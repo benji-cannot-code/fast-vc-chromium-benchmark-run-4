c32332a2d355b979aa860a3fc3c36f7d69a436f2
Akihiro Ota <akihiroota@chromium.org>
2024-11-19 19:49:53 +0000

    FaceGaze: Unify preference listeners
    Currently there are three preference listeners: one in facegaze.ts,
    mouse_controller.ts, and gesture_handler.ts. This makes the code
    cluttered since each has to initialize/teardown the listener.
    
    This CL unifies them behind the listener in facegaze.ts, overall
    simplifying the code.
    
    Bug: N/A
    Change-Id: I48c63b3e84bad7cb112b945c72152b662c4a98e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032182
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385146}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/mouse_controller.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/mouse_controller.ts
@@ -10,14 +10,11 @@ import {TestImportManager} from '/common/testing/test_import_manager.js';
 import type {FaceLandmarkerResult} from '/third_party/mediapipe/vision.js';
 
 import {BubbleController} from './bubble_controller.js';
-import {PrefNames} from './pref_names.js';
 import {ScrollModeController} from './scroll_mode_controller.js';
 
 import ScreenRect = chrome.accessibilityPrivate.ScreenRect;
 import ScreenPoint = chrome.accessibilityPrivate.ScreenPoint;
 import SyntheticMouseEventButton = chrome.accessibilityPrivate.SyntheticMouseEventButton;
-
-type PrefObject = chrome.settingsPrivate.PrefObject;
 
 // A ScreenPoint represents an integer screen coordinate, whereas
 // a FloatingPoint2D represents a (x, y) floating point number
@@ -43,8 +40,6 @@ export class MouseController {
   private onMouseMovedHandler_: EventHandler;
   private onMouseDraggedHandler_: EventHandler;
   private screenBounds_: ScreenRect|undefined;
-
-  private prefsListener_: ((prefs: PrefObject[]) => void);
 
   // These values will be updated when prefs are received in init_().
   private targetBufferSize_ = MouseController.DEFAULT_BUFFER_SIZE;
@@ -102,7 +97,6 @@ export class MouseController {
     this.landmarkWeights_.set(LandmarkType.RIGHT_TEMPLE, 0.0336);
     this.landmarkWeights_.set(LandmarkType.ROTATION, 0.3960);
 
-    this.prefsListener_ = prefs => this.updateFromPrefs_(prefs);
     this.init();
   }
 
@@ -125,8 +119,6 @@ export class MouseController {
 
   async start(): Promise<void> {
     this.paused_ = false;
-    chrome.settingsPrivate.getAllPrefs(prefs => this.updateFromPrefs_(prefs));
-    chrome.settingsPrivate.onPrefsChanged.addListener(this.prefsListener_);
 
     // TODO(b/309121742): Handle display bounds changed.
     const screens = await new Promise<ScreenRect[]>((resolve) => {
@@ -346,7 +338,6 @@ export class MouseController {
     this.lastMouseMovedTime_ = 0;
     this.buffer_ = [];
     this.paused_ = false;
-    chrome.settingsPrivate.onPrefsChanged.removeListener(this.prefsListener_);
   }
 
   togglePaused(): void {
@@ -487,45 +478,6 @@ export class MouseController {
     return multiply * sig;
   }
 
-  private updateFromPrefs_(prefs: PrefObject[]): void {
-    prefs.forEach(pref => {
-      switch (pref.key) {
-        case PrefNames.SPD_UP:
-          if (pref.value !== undefined) {
-            this.speedUpChanged_(pref.value);
-          }
-          break;
-        case PrefNames.SPD_DOWN:
-          if (pref.value !== undefined) {
-            this.speedDownChanged_(pref.value);
-          }
-          break;
-        case PrefNames.SPD_LEFT:
-          if (pref.value !== undefined) {
-            this.speedLeftChanged_(pref.value);
-          }
-          break;
-        case PrefNames.SPD_RIGHT:
-          if (pref.value !== undefined) {
-            this.speedRightChanged_(pref.value);
-          }
-          break;
-        case PrefNames.CURSOR_USE_ACCELERATION:
-          if (pref.value !== undefined) {
-            this.useCursorAccelerationChanged_(pref.value);
-          }
-          break;
-        case PrefNames.VELOCITY_THRESHOLD:
-          if (pref.value !== undefined) {
-            this.velocityThresholdChanged_(pref.value);
-          }
-          break;
-        default:
-          return;
-      }
-    });
-  }
-
   private calcVelocityThreshold_(): void {
     // Threshold is a function of speed. Threshold increases as speed increases
     // because it's easier to move the mouse accidentally at high mouse speeds.
@@ -577,31 +529,31 @@ export class MouseController {
     }
   }
 
-  private speedUpChanged_(speed: number): void {
+  speedUpChanged(speed: number): void {
     this.spdUp_ = speed;
     this.calcVelocityThreshold_();
   }
 
-  private speedDownChanged_(speed: number): void {
+  speedDownChanged(speed: number): void {
     this.spdDown_ = speed;
     this.calcVelocityThreshold_();
   }
 
-  private speedLeftChanged_(speed: number): void {
+  speedLeftChanged(speed: number): void {
     this.spdLeft_ = speed;
     this.calcVelocityThreshold_();
   }
 
-  private speedRightChanged_(speed: number): void {
+  speedRightChanged(speed: number): void {
     this.spdRight_ = speed;
     this.calcVelocityThreshold_();
   }
 
-  private useCursorAccelerationChanged_(useAcceleration: boolean): void {
+  useCursorAccelerationChanged(useAcceleration: boolean): void {
     this.useMouseAcceleration_ = useAcceleration;
   }
 
-  private velocityThresholdChanged_(threshold: number): void {
+  velocityThresholdChanged(threshold: number): void {
     // Ensure threshold factor is a decimal value.
     this.velocityThresholdFactor_ =
         threshold / MouseController.MAX_VELOCITY_THRESHOLD_PREF_VALUE;
