fa969c33e0326b2827b85d71e91162c132c32e30
Emily Stark <estark@google.com>
2024-11-19 03:58:07 +0000

    Add metrics for chrome.contentSettings extension usage, take 2
    https://chromium-review.googlesource.com/c/chromium/src/+/5814282
    added metrics to try to measure extensions that were granting
    themselves content settings. However, granting settings via a
    'chrome-extension://<id>' URL is not allowed; it's only through a
    wildcard scheme that an extension can grant itself content
    settings. This CL revises the metrics to record when an extension uses
    a content settings pattern that matches its own origin.
    
    OBSOLETE_HISTOGRAMS=Extensions.ContentSettings.[Primary|Secondary]PatternChromeExtensionScheme are replaced with [Primary|Secondary]PatternMatchesExtensionOrigin
    
    Bug: 40086360
    Change-Id: I92bc7065fc935c674f323a1c1e7641f922faaa79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026952
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384752}

--- extensions/browser/api/content_settings/content_settings_store_unittest.cc
+++ extensions/browser/api/content_settings/content_settings_store_unittest.cc
@@ -352,13 +352,12 @@ TEST_F(ContentSettingsStoreTest, RemoveEmbedded) {
   store()->RemoveObserver(&observer);
 }
 
-TEST_F(ContentSettingsStoreTest, ChromeExtensionSchemeMetrics) {
+TEST_F(ContentSettingsStoreTest, ChromeExtensionOriginMetrics) {
   base::HistogramTester histogram_tester;
   content_settings::ContentSettingsRegistry::GetInstance();
   std::string extension_id(32, 'a');
   ContentSettingsPattern chrome_extension_pattern =
-      ContentSettingsPattern::FromString(
-          "chrome-extension://peoadpeiejnhkmpaakpnompolbglelel/");
+      ContentSettingsPattern::FromString("*://" + extension_id + "/*");
   ContentSettingsPattern https_pattern =
       ContentSettingsPattern::FromString("https://example.test/");
 
@@ -368,7 +367,7 @@ TEST_F(ContentSettingsStoreTest, ChromeExtensionSchemeMetrics) {
       ContentSettingsType::COOKIES, CONTENT_SETTING_ALLOW,
       ChromeSettingScope::kRegular);
   histogram_tester.ExpectUniqueSample(
-      "Extensions.ContentSettings.PrimaryPatternChromeExtensionScheme",
+      "Extensions.ContentSettings.PrimaryPatternMatchesExtensionOrigin",
       content_settings_uma_util::ContentSettingTypeToHistogramValue(
           ContentSettingsType::COOKIES),
       1);
@@ -379,7 +378,7 @@ TEST_F(ContentSettingsStoreTest, ChromeExtensionSchemeMetrics) {
       ContentSettingsType::IMAGES, CONTENT_SETTING_ALLOW,
       ChromeSettingScope::kRegular);
   histogram_tester.ExpectUniqueSample(
-      "Extensions.ContentSettings.SecondaryPatternChromeExtensionScheme",
+      "Extensions.ContentSettings.SecondaryPatternMatchesExtensionOrigin",
       content_settings_uma_util::ContentSettingTypeToHistogramValue(
           ContentSettingsType::IMAGES),
       1);
