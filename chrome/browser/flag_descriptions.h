47a7b5fab67eace7900ca4a3d54d4faa6a02ed22
Jordan Bayles <jophba@chromium.org>
2024-11-19 00:52:46 +0000

    [Cast Streaming] Add new cast-streaming-media-video-encoder feature
    This patch adds a new media feature for enabling the new
    media::VideoEncoder based implementation for Cast Streaming.
    
    This series adds a new MediaVideoEncoderWrapper class that fulfills
    the media::cast::VideoEncoder API and wraps media::VideoEncoder. This
    can be enabled for software encoding using a new flag,
    cast-streaming-media-video-encoder.
    
    Googlers may view the related design doc at:
    http://go/cast-streaming-media-videoencoder
    
    Publicly published design documentation available at:
    https://tinyurl.com/cast-streaming-mvew
    
    Bug: 282984511
    Change-Id: Id7b62d8fa935734639848f66648ec4e4f91ac22a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033350
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384688}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1179,6 +1179,9 @@ extern const char kCastStreamingHardwareVp8Description[];
 extern const char kCastStreamingHardwareVp9Name[];
 extern const char kCastStreamingHardwareVp9Description[];
 
+extern const char kCastStreamingMediaVideoEncoderName[];
+extern const char kCastStreamingMediaVideoEncoderDescription[];
+
 extern const char kCastStreamingPerformanceOverlayName[];
 extern const char kCastStreamingPerformanceOverlayDescription[];
 
