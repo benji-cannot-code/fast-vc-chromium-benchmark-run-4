3799e1759009711c80a9bd2e2e9b250927b123db
Shu Yang <shuyng@google.com>
2024-11-19 16:56:27 +0000

    Avoid crash when pdf page url fails to decode
    Bug: 379770426
    Change-Id: I2560c2d21609d5bb5af6c7d713d6409b4fad25c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033086
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385024}

--- chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfUtils.java
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfUtils.java
@@ -178,6 +178,9 @@ public class PdfUtils {
     }
 
     static String getFileNameFromUrl(String url, String defaultTitle) {
+        if (url == null) {
+            return defaultTitle;
+        }
         Uri uri = Uri.parse(url);
         String scheme = uri.getScheme();
         assert scheme != null;
@@ -203,6 +206,9 @@ public class PdfUtils {
     }
 
     static String getFilePathFromUrl(String url) {
+        if (url == null) {
+            return null;
+        }
         GURL gurl = new GURL(url);
         if (getPdfPageTypeInternal(gurl, false) == PdfPageType.LOCAL) {
             return url;
