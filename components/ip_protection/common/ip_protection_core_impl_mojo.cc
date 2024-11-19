7dd6c51dbc1c1646b06c1aea4f4bcd7913d3efc5
Dustin J. Mitchell <djmitche@chromium.org>
2024-11-19 17:40:02 +0000

    Use a fetcher in token manager
    This updates the IpProtectionTokenManager to use a fetcher, adding
    a fetcher implementation that makes an IPC call to CoreHost (via the
    config getter, for now).
    
    The `IpProtectionCoreHost` type relies directly on the direct fetcher.
    
    Bug: 369103603
    Change-Id: I26662a1a47cefd30d10466ba0e13a8466895dd21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000459
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Ciara McMullin <ciaramcmullin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385052}

--- components/ip_protection/common/ip_protection_core_impl_mojo.cc
+++ components/ip_protection/common/ip_protection_core_impl_mojo.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ip_protection/common/ip_protection_proxy_config_mojo_fetcher.h"
 #include "components/ip_protection/common/ip_protection_token_manager.h"
 #include "components/ip_protection/common/ip_protection_token_manager_impl.h"
+#include "components/ip_protection/common/ip_protection_token_mojo_fetcher.h"
 #include "net/base/features.h"
 #include "net/base/proxy_chain.h"
 #include "net/base/proxy_server.h"
@@ -35,10 +36,12 @@ MakeTokenManagerMap(
     scoped_refptr<IpProtectionConfigGetter> ip_protection_config_getter) {
   std::map<ProxyLayer, std::unique_ptr<IpProtectionTokenManager>> managers;
   for (ProxyLayer proxy_layer : {ProxyLayer::kProxyA, ProxyLayer::kProxyB}) {
-    managers.insert({proxy_layer,
-                     std::make_unique<IpProtectionTokenManagerImpl>(
-                         ip_protection_core,
-                         std::move(ip_protection_config_getter), proxy_layer)});
+    managers.insert(
+        {proxy_layer, std::make_unique<IpProtectionTokenManagerImpl>(
+                          ip_protection_core,
+                          std::make_unique<IpProtectionTokenMojoFetcher>(
+                              ip_protection_config_getter),
+                          proxy_layer)});
   }
   return managers;
 }
