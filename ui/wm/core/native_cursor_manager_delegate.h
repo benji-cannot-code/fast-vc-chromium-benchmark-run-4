f28b0f3528561ab9674a08092fec19450ab33cf9
Jiahe Zhang <jiahe.zhang@intel.com>
2024-11-19 02:43:02 +0000

    Add Cursor Visibility Management for Mouse-less Systems on Windows
    Currently the cursor is always considered visible by default at startup
    and manage the visibility based on related events. However, if the
    cursor is actually invisible at Chomium startup, Chromium will
    unexpectedly render mouse effects (e.g. hover). This might happens on
    systems where no mouse is installed or cursor is suppressed by the OS.
    
    As Windows provides some capabilities to read/monitor the cursor
    status, this CL:
    
    - Uses GetCursorInfo() to initialize the visibility at startup and
      monitor the change via an event hook.
    - Ignores the ShowCursor/HideCursor requests if cursor is hidden by the
      OS.
    
    Bug: 376174073
    Change-Id: Ic1be96ec77c834fb0cee064ea87c4f9340461b26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5573002
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Jiahe Zhang <jiahe.zhang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1384735}

--- ui/wm/core/native_cursor_manager_delegate.h
+++ ui/wm/core/native_cursor_manager_delegate.h
@@ -35,6 +35,7 @@ class COMPONENT_EXPORT(UI_WM) NativeCursorManagerDelegate {
   virtual void CommitCursorSize(ui::CursorSize cursor_size) = 0;
   virtual void CommitMouseEventsEnabled(bool enabled) = 0;
   virtual void CommitSystemCursorSize(const gfx::Size& cursor_size) = 0;
+  virtual void CommitSystemCursorVisibility(bool visible) = 0;
 };
 
 }  // namespace wm
