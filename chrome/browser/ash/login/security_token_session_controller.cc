404a788bd872806a9693b138793a481f425aff8f
Georg Neis <neis@chromium.org>
2024-11-19 01:05:10 +0000

    Remove Lacros keep-alive request from SecurityTokenSessionController
    This is part of removing Lacros.
    
    Change-Id: Icafc93ce366947a1efd686810711cab2aa82e908
    Bug: b:365741912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6029832
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384693}

--- chrome/browser/ash/login/security_token_session_controller.cc
+++ chrome/browser/ash/login/security_token_session_controller.cc
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "base/trace_event/trace_event.h"
-#include "chrome/browser/ash/crosapi/browser_manager.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/login/lock/screen_locker.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browser_process.h"
@@ -214,7 +212,6 @@ SecurityTokenSessionController::SecurityTokenSessionController(
       &observed_extensions_);
   UpdateNotificationPref();
   behavior_ = GetBehaviorFromPrefAndSessionState();
-  UpdateKeepAlive();
   pref_change_registrar_.Init(local_state_);
   base::RepeatingClosure behavior_pref_changed_callback =
       base::BindRepeating(&SecurityTokenSessionController::UpdateBehavior,
@@ -382,23 +379,12 @@ void SecurityTokenSessionController::MaybeDisplayLoginScreenNotification() {
 void SecurityTokenSessionController::UpdateBehavior() {
   Behavior previous_behavior = behavior_;
   behavior_ = GetBehaviorFromPrefAndSessionState();
-  UpdateKeepAlive();
   if (behavior_ == Behavior::kIgnore) {
     Reset();
   } else if (previous_behavior == Behavior::kIgnore) {
     // Request all available certificates to ensure that all required
     // certificates are still present.
     certificate_provider_->GetCertificates(base::DoNothing());
-  }
-}
-
-void SecurityTokenSessionController::UpdateKeepAlive() {
-  if (behavior_ == Behavior::kIgnore ||
-      crosapi::browser_util::IsAshWebBrowserEnabled()) {
-    keep_alive_.reset();
-  } else if (!keep_alive_) {
-    keep_alive_ = crosapi::BrowserManager::Get()->KeepAlive(
-        crosapi::BrowserManager::Feature::kSmartCardSessionController);
   }
 }
 
--- chrome/browser/ash/login/security_token_session_controller.h
+++ chrome/browser/ash/login/security_token_session_controller.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
-#include "chrome/browser/ash/crosapi/browser_manager_scoped_keep_alive.h"
 #include "chrome/browser/certificate_provider/certificate_provider_service.h"
 #include "chrome/browser/extensions/forced_extensions/force_installed_tracker.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -106,7 +105,6 @@ class SecurityTokenSessionController
   bool ShouldApplyPolicyInCurrentSessionState() const;
   Behavior GetBehaviorFromPrefAndSessionState() const;
   void UpdateBehavior();
-  void UpdateKeepAlive();
   void UpdateNotificationPref();
 
   void ExtensionProvidesAllRequiredCertificates(
@@ -130,7 +128,6 @@ class SecurityTokenSessionController
       nullptr;
   extensions::ForceInstalledTracker extensions_tracker_;
   const raw_ptr<session_manager::SessionManager> session_manager_;
-  std::unique_ptr<crosapi::BrowserManagerScopedKeepAlive> keep_alive_;
   base::ScopedObservation<session_manager::SessionManager,
                           session_manager::SessionManagerObserver>
       session_manager_observation_{this};
