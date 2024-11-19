486c3caa55c4604a98139f8bb8dbfeb1e9ccfcd4
Clark DuVall <cduvall@chromium.org>
2024-11-19 23:29:57 +0000

    Remove browser side timeout logic for on-device model
    This timeout was causing the model to be disabled too often, and was not
    catching real issues. The backend still has a timeout that should
    prevent cases where the model is not making progress.
    
    Bug: 378703860
    Change-Id: Ic59540dbd5fcc709f60099a0295dfcc41278ab33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035965
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385313}

--- components/optimization_guide/core/model_execution/on_device_model_access_controller.h
+++ components/optimization_guide/core/model_execution/on_device_model_access_controller.h
@@ -38,9 +38,6 @@ class OnDeviceModelAccessController {
   // Called if using the gpu is blocked.
   void OnGpuBlocked();
 
-  // Called if the session times out.
-  void OnSessionTimedOut();
-
   bool ShouldValidateModel(std::string_view model_version);
   void OnValidationFinished(OnDeviceModelValidationResult result);
 
@@ -57,7 +54,6 @@ class OnDeviceModelAccessController {
   raw_ref<PrefService> pref_service_;
   bool is_gpu_blocked_ = false;
   base::Time next_attempt_time_after_crash_ = base::Time::Now();
-  base::Time next_attempt_time_after_timeout_ = base::Time::Now();
 };
 
 }  // namespace optimization_guide
--- components/optimization_guide/core/model_execution/on_device_model_service_controller.cc
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller.cc
@@ -467,12 +467,6 @@ void OnDeviceModelServiceController::OnDeviceModelClient::
   }
 }
 
-void OnDeviceModelServiceController::OnDeviceModelClient::OnSessionTimedOut() {
-  if (controller_) {
-    controller_->access_controller_->OnSessionTimedOut();
-  }
-}
-
 scoped_refptr<const OnDeviceModelFeatureAdapter>
 OnDeviceModelServiceController::GetFeatureAdapter(
     ModelBasedCapabilityKey feature) {
