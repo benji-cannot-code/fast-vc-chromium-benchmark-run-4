855383dd72842121f415af29443db25bf8659a4b
Calder Kitagawa <ckitagawa@chromium.org>
2024-11-19 22:01:37 +0000

    [Tab Model] Use TabRemover API for TabModelSelector#closeTab
    TabModelSelector#closeTab has some additional logic to force close a
    tab if it is currently closing or ignore tabs that have already closed.
    Rather than expose this complexity to callers it is nicer to keep this
    function for the few places that need it.
    
    In order to ensure the callers "do the right thing". The API has been
    changed to match TabRemover#closeTab.
    
    Bug: 345854441
    Change-Id: I5b0b338995a00525f3b5ec4c400f3364583bd7fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035627
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1385246}

--- chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarContainerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarContainerTest.java
@@ -37,6 +37,7 @@ import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.prefetch.settings.PreloadPagesSettingsBridge;
 import org.chromium.chrome.browser.prefetch.settings.PreloadPagesState;
 import org.chromium.chrome.browser.profiles.ProfileManager;
+import org.chromium.chrome.browser.tabmodel.TabClosureParams;
 import org.chromium.chrome.browser.ui.messages.infobar.SimpleConfirmInfoBarBuilder;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
@@ -278,8 +279,16 @@ public class InfoBarContainerTest {
                     infoBar.onCloseButtonClicked();
                     sActivityTestRule
                             .getActivity()
-                            .getTabModelSelector()
-                            .closeTab(sActivityTestRule.getActivity().getActivityTab());
+                            .getCurrentTabModel()
+                            .getTabRemover()
+                            .closeTabs(
+                                    TabClosureParams.closeTab(
+                                                    sActivityTestRule
+                                                            .getActivity()
+                                                            .getActivityTab())
+                                            .allowUndo(false)
+                                            .build(),
+                                    /* allowDialog= */ false);
                 });
 
         infobarListener.dismissedCallback.waitForCallback(0, 1);
--- chrome/android/javatests/src/org/chromium/chrome/browser/screenshot_monitor/ScreenshotTabObserverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/screenshot_monitor/ScreenshotTabObserverTest.java
@@ -23,6 +23,7 @@ import org.chromium.base.test.util.Matchers;
 import org.chromium.base.test.util.UserActionTester;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabClosureParams;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
@@ -53,6 +54,16 @@ public class ScreenshotTabObserverTest {
                 (Runnable) () -> mObserver = ScreenshotTabObserver.from(mTab));
     }
 
+    private void closeCurrentTab() {
+        sActivityTestRule
+                .getActivity()
+                .getCurrentTabModel()
+                .getTabRemover()
+                .closeTabs(
+                        TabClosureParams.closeTab(mTab).allowUndo(false).build(),
+                        /* allowDialog= */ false);
+    }
+
     @Test
     @MediumTest
     public void testScreenshotUserCounts() {
@@ -76,10 +87,7 @@ public class ScreenshotTabObserverTest {
         int count = callbackHelper.getCallCount();
 
         mObserver.onScreenshotTaken();
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    sActivityTestRule.getActivity().getTabModelSelector().closeTab(mTab);
-                });
+        ThreadUtils.runOnUiThreadBlocking(this::closeCurrentTab);
         callbackHelper.waitForCallback(count);
 
         histogramWatcher.assertExpected("Should be one page with one snapshot reported.");
@@ -96,10 +104,7 @@ public class ScreenshotTabObserverTest {
 
         mObserver.onScreenshotTaken();
         mObserver.onScreenshotTaken();
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    sActivityTestRule.getActivity().getTabModelSelector().closeTab(mTab);
-                });
+        ThreadUtils.runOnUiThreadBlocking(this::closeCurrentTab);
         callbackHelper.waitForCallback(count);
 
         histogramWatcher.assertExpected("Should be one page with two snapshots reported.");
@@ -115,10 +120,7 @@ public class ScreenshotTabObserverTest {
 
         mObserver.onScreenshotTaken();
         mObserver.onActionPerformedAfterScreenshot(ScreenshotTabObserver.SCREENSHOT_ACTION_SHARE);
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    sActivityTestRule.getActivity().getTabModelSelector().closeTab(mTab);
-                });
+        ThreadUtils.runOnUiThreadBlocking(this::closeCurrentTab);
         callbackHelper.waitForCallback(count);
 
         histogramWatcher.assertExpected(
