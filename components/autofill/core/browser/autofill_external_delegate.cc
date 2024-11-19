2dd885f71e9a3d12af34314c22d2e4b6a4a3bf39
Timofey Chudakov <tchudakov@google.com>
2024-11-19 10:36:59 +0000

    Remove address deletion dialog handling from AED.
    AED - AutofillExternalDelegate.
    
    Reason: feature is being removed from Chrome.
    
    Bug: 378835293
    Change-Id: I560c0876eeb9ec7f32a7f5e6355249a422be8616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018306
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384850}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -749,7 +749,6 @@ void AutofillExternalDelegate::DidSelectSuggestion(
     case SuggestionType::kComposeSavedStateNotification:
     case SuggestionType::kCreateNewPlusAddress:
     case SuggestionType::kDatalistEntry:
-    case SuggestionType::kDeleteAddressProfile:
     case SuggestionType::kDevtoolsTestAddressByCountry:
     case SuggestionType::kDevtoolsTestAddresses:
     case SuggestionType::kEditAddressProfile:
@@ -785,6 +784,7 @@ void AutofillExternalDelegate::DidSelectSuggestion(
     case SuggestionType::kFillPassword:
     case SuggestionType::kPredictionImprovementsFeedback:
     case SuggestionType::kViewPasswordDetails:
+    case SuggestionType::kDeleteAddressProfile:
       NOTREACHED();  // Should be handled elsewhere.
   }
 }
@@ -805,7 +805,6 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
     case SuggestionType::kFillFullEmail:
     case SuggestionType::kAddressFieldByFieldFilling:
     case SuggestionType::kEditAddressProfile:
-    case SuggestionType::kDeleteAddressProfile:
     case SuggestionType::kDevtoolsTestAddressEntry:
       DidAcceptAddressSuggestion(suggestion, metadata);
       break;
@@ -972,6 +971,7 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
     case SuggestionType::kViewPasswordDetails:
     case SuggestionType::kPredictionImprovementsLoadingState:
     case SuggestionType::kPredictionImprovementsError:
+    case SuggestionType::kDeleteAddressProfile:
       NOTREACHED();  // Should be handled elsewhere.
   }
   // Note that some suggestion types return early.
@@ -1151,20 +1151,6 @@ void AutofillExternalDelegate::ShowEditAddressProfileDialog(
   }
 }
 
-void AutofillExternalDelegate::ShowDeleteAddressProfileDialog(
-    const std::string& guid) {
-  const AutofillProfile* profile = manager_->client()
-                                       .GetPersonalDataManager()
-                                       ->address_data_manager()
-                                       .GetProfileByGUID(guid);
-  if (profile) {
-    manager_->client().ShowDeleteAddressProfileDialog(
-        *profile,
-        base::BindOnce(&AutofillExternalDelegate::OnDeleteDialogClosed,
-                       GetWeakPtr(), guid));
-  }
-}
-
 void AutofillExternalDelegate::OnAddressEditorClosed(
     AutofillClient::AddressPromptUserDecision decision,
     base::optional_ref<const AutofillProfile> edited_profile) {
@@ -1176,21 +1162,6 @@ void AutofillExternalDelegate::OnAddressEditorClosed(
     }
     CHECK(edited_profile.has_value());
     adm.UpdateProfile(edited_profile.value());
-    return;
-  }
-  manager_->driver().RendererShouldTriggerSuggestions(query_field_.global_id(),
-                                                      GetReopenTriggerSource());
-}
-
-void AutofillExternalDelegate::OnDeleteDialogClosed(const std::string& guid,
-                                                    bool user_accepted_delete) {
-  if (user_accepted_delete) {
-    AddressDataManager& adm =
-        manager_->client().GetPersonalDataManager()->address_data_manager();
-    if (!adm_observation_.IsObserving()) {
-      adm_observation_.Observe(&adm);
-    }
-    adm.RemoveProfile(guid);
     return;
   }
   manager_->driver().RendererShouldTriggerSuggestions(query_field_.global_id(),
@@ -1570,10 +1541,6 @@ void AutofillExternalDelegate::DidAcceptAddressSuggestion(
       break;
     case SuggestionType::kEditAddressProfile:
       ShowEditAddressProfileDialog(
-          suggestion.GetPayload<Suggestion::Guid>().value());
-      break;
-    case SuggestionType::kDeleteAddressProfile:
-      ShowDeleteAddressProfileDialog(
           suggestion.GetPayload<Suggestion::Guid>().value());
       break;
     case SuggestionType::kDevtoolsTestAddressEntry: {
