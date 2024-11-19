51bbdd4b601bb3694af6cc9b9702026cda13adab
Fredrik Söderquist <fs@opera.com>
2024-11-19 22:44:40 +0000

    spanification: Use span StringImpl::CreateUninitialized in WebGL code
    Replace uses of the pointer overload of StringImpl::CreateUninitialized
    with the span overload. Switch GLStringQuery::Run() to use StringBuffer
    because it's a better fit.
    
    Bug: 351564777
    Change-Id: I4dc1701c4279ce76cdc6cf184b7bd506c600c59f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035446
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1385279}

--- third_party/blink/renderer/modules/webgl/gl_string_query.h
+++ third_party/blink/renderer/modules/webgl/gl_string_query.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/memory/raw_ptr.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
+#include "third_party/blink/renderer/platform/wtf/text/string_buffer.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/khronos/GLES2/gl2ext.h"
 
@@ -74,15 +75,14 @@ class GLStringQuery {
     Traits::LengthFunction(gl_, id, &length);
     if (!length)
       return WTF::g_empty_string;
-    LChar* log_ptr;
-    scoped_refptr<WTF::StringImpl> name_impl =
-        WTF::StringImpl::CreateUninitialized(length, log_ptr);
     GLsizei returned_length = 0;
-    Traits::LogFunction(gl_, id, length, &returned_length, log_ptr);
+    StringBuffer<LChar> log_buffer(length);
+    Traits::LogFunction(gl_, id, length, &returned_length,
+                        log_buffer.Span().data());
     // The returnedLength excludes the null terminator. If this check wasn't
     // true, then we'd need to tell the returned String the real length.
     DCHECK_EQ(returned_length + 1, length);
-    return String(std::move(name_impl));
+    return String::Adopt(log_buffer);
   }
 
  private:
