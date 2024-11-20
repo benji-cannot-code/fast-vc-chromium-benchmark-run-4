ad945d16b1a437620c7df0f4e6c087677a7de870
Jeroen Dhollander <jeroendh@google.com>
2024-11-20 13:35:22 +0000

    Auto-start WeeklyIntervalTimer upon creation
    Previously the caller needed to explicitly start the timer, but there
    really is no point to one of these timers without running them.
    
    Fixed: b:328421429
    Change-Id: I9946aaea9cddb91d9010d890d2100137e3a4d9a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018304
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385616}

--- chrome/browser/ash/app_mode/auto_sleep/device_weekly_scheduled_suspend_controller.cc
+++ chrome/browser/ash/app_mode/auto_sleep/device_weekly_scheduled_suspend_controller.cc
@@ -194,10 +194,6 @@ void DeviceWeeklyScheduledSuspendController::
       base::BindRepeating(
           &DeviceWeeklyScheduledSuspendController::OnWeeklyIntervalStart,
           weak_factory_.GetWeakPtr()));
-
-  for (const auto& timer : device_suspension_timers_) {
-    timer->ScheduleTimer();
-  }
 }
 
 void DeviceWeeklyScheduledSuspendController::OnWeeklyIntervalStart(
