db090a35d9d76db5cf73bea165218757d1465410
Moteva <moteva@google.com>
2024-11-20 01:40:45 +0000

    CrOS Settings: cleanup revamp in personalization cc files
    Bug: b/370837128
    Change-Id: If3040a7f9f34995903318dfa2b9d2bd02b473ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036387
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385386}

--- chrome/browser/ui/webui/ash/settings/pages/personalization/personalization_hub_handler.cc
+++ chrome/browser/ui/webui/ash/settings/pages/personalization/personalization_hub_handler.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/settings/pages/personalization/personalization_hub_handler.h"
 
-#include "ash/constants/ash_features.h"
 #include "ash/constants/personalization_entry_point.h"
 #include "base/functional/bind.h"
 #include "chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_metrics.h"
