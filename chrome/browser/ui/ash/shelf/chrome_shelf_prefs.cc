85e45a7581c0c12a78a3899643f7c041d4e615f4
Antonio Gomes <tonikitoo@igalia.com>
2024-11-20 10:43:57 +0000

    Remove app_constants::kLacrosAppId and IsLacrosEnabled() use from chrome_shelf_prefs.cc
    After the termination of the Lacros project, their use isn't needed
    anymore.
    
    R=hidehiko@chromium.org, neis@chromium.org
    
    Bug: 378932949, 37397153
    Change-Id: I9c0c67b57f1f3dafc01ac7e2196d391618061d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032139
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1385547}

--- chrome/browser/ui/ash/shelf/chrome_shelf_prefs.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_prefs.cc
@@ -98,7 +98,6 @@ syncer::StringOrdinal GetAdjacentPosition(
       continue;
     }
     if (exclude_chrome && (item_id == app_constants::kChromeAppId ||
-                           item_id == app_constants::kLacrosAppId ||
                            item_id == app_constants::kAshDebugBrowserAppId)) {
       continue;
     }
@@ -171,8 +170,7 @@ void EnsurePinnedOrMakeFirst(
     app_list::AppListSyncableService* syncable_service) {
   // This piece prevents accidental side-effects to the SetPinPosition() call
   // below.
-  CHECK(app_id == app_constants::kChromeAppId ||
-        app_id == app_constants::kLacrosAppId);
+  CHECK_EQ(app_id, app_constants::kChromeAppId);
   syncer::StringOrdinal position = syncable_service->GetPinPosition(app_id);
   if (!position.IsValid()) {
     position = CreateFirstPinPosition(syncable_service);
@@ -552,21 +550,6 @@ std::vector<ash::ShelfID> ChromeShelfPrefs::GetPinnedAppsFromSync(
     PinPreloadApps();
   }
 
-  // If Lacros is enabled and allowed for this user type, ensure the Lacros icon
-  // is pinned. Lacros doesn't support multi-signin, so only add the icon for
-  // the primary user.
-  if (crosapi::browser_util::IsLacrosEnabled() &&
-      ash::ProfileHelper::IsPrimaryProfile(profile_)) {
-    syncer::StringOrdinal lacros_position =
-        syncable_service->GetPinPosition(app_constants::kLacrosAppId);
-    if (!lacros_position.IsValid()) {
-      // If Lacros isn't already pinned, add it to the right of the Chrome icon.
-      InsertPinsAfterChromeAndBeforeFirstPinnedApp(
-          syncable_service, {{app_constants::kLacrosAppId}},
-          /*is_policy_initiated=*/false);
-    }
-  }
-
   std::vector<std::string> policy_delta_remove_from_shelf;
 
   std::vector<PinInfo> pin_infos;
@@ -576,11 +559,6 @@ std::vector<ash::ShelfID> ChromeShelfPrefs::GetPinnedAppsFromSync(
   for (const auto& [item_id, sync_item] : syncable_service->sync_items()) {
     // A null ordinal means the item has been unpinned.
     if (!sync_item->item_pin_ordinal.IsValid()) {
-      continue;
-    }
-
-    // kChromeAppId is the only valid sync ID for the browser.
-    if (item_id == app_constants::kLacrosAppId) {
       continue;
     }
 
