558aa56a060db482a1b303e332cb2e63df0efb04
Guillem Perez <guiperez@google.com>
2024-11-20 17:30:24 +0000

    Update the allowlist for AI data collection.
    The extension nfdaijodggdcjengofmbibbkcnopmikg is no longer needed. The extension bgbpcgpcobgjpnpiginpidndjpggappi is needed.
    
    Change-Id: Ic397372418a0000529cb1ed1bafe4944fa74b86e
    Bug: 377129777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011876
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385730}

--- chrome/browser/ai/ai_data_keyed_service_browsertest.cc
+++ chrome/browser/ai/ai_data_keyed_service_browsertest.cc
@@ -83,7 +83,7 @@ class AiDataKeyedServiceBrowserTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(AiDataKeyedServiceBrowserTest,
                        AllowlistedExtensionList) {
   std::vector<std::string> expected_allowlisted_extensions = {
-      "hpkopmikdojpadgmioifjjodbmnjjjca", "nfdaijodggdcjengofmbibbkcnopmikg"};
+      "hpkopmikdojpadgmioifjjodbmnjjjca", "bgbpcgpcobgjpnpiginpidndjpggappi"};
 
   EXPECT_EQ(AiDataKeyedService::GetAllowlistedExtensions(),
             expected_allowlisted_extensions);
@@ -225,7 +225,7 @@ class AiDataKeyedServiceBrowserTestWithBlocklistedExtensions
 IN_PROC_BROWSER_TEST_F(AiDataKeyedServiceBrowserTestWithBlocklistedExtensions,
                        BlockedExtensionList) {
   std::vector<std::string> expected_allowlisted_extensions = {
-      "nfdaijodggdcjengofmbibbkcnopmikg"};
+      "bgbpcgpcobgjpnpiginpidndjpggappi"};
 
   EXPECT_EQ(AiDataKeyedService::GetAllowlistedExtensions(),
             expected_allowlisted_extensions);
@@ -252,7 +252,7 @@ IN_PROC_BROWSER_TEST_F(
   std::vector<std::string> expected_allowlisted_extensions = {
       "1234",
       "hpkopmikdojpadgmioifjjodbmnjjjca",
-      "nfdaijodggdcjengofmbibbkcnopmikg",
+      "bgbpcgpcobgjpnpiginpidndjpggappi",
   };
 
   EXPECT_EQ(AiDataKeyedService::GetAllowlistedExtensions(),
@@ -277,7 +277,7 @@ class AiDataKeyedServiceBrowserTestWithAllowAndBlock
 IN_PROC_BROWSER_TEST_F(AiDataKeyedServiceBrowserTestWithAllowAndBlock,
                        AllowAndBlock) {
   std::vector<std::string> expected_allowlisted_extensions = {
-      "hpkopmikdojpadgmioifjjodbmnjjjca", "nfdaijodggdcjengofmbibbkcnopmikg"};
+      "hpkopmikdojpadgmioifjjodbmnjjjca", "bgbpcgpcobgjpnpiginpidndjpggappi"};
 
   EXPECT_EQ(AiDataKeyedService::GetAllowlistedExtensions(),
             expected_allowlisted_extensions);
