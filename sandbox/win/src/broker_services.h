f28ad2a4aa046c78b0e25fc58b14d33351a34fce
Emily Andrews <emiled@microsoft.com>
2024-11-20 19:16:07 +0000

    Update BrokerServices Interface Destructor to be virtual
    Interfaces typically have virtual destructors. There's no reason to not
    have this be a virtual destructor either.
    
    Bug: 40238155
    Change-Id: I0d3420a369045d2bfa785575d7286d0dc3473ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001107
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Emily Andrews <emiled@microsoft.com>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385801}

--- sandbox/win/src/broker_services.h
+++ sandbox/win/src/broker_services.h
@@ -44,7 +44,7 @@ class BrokerServicesBase final : public BrokerServices,
   BrokerServicesBase(const BrokerServicesBase&) = delete;
   BrokerServicesBase& operator=(const BrokerServicesBase&) = delete;
 
-  ~BrokerServicesBase();
+  ~BrokerServicesBase() override;
 
   // BrokerServices interface.
   ResultCode Init(std::unique_ptr<BrokerServicesDelegate> delegate) override;
