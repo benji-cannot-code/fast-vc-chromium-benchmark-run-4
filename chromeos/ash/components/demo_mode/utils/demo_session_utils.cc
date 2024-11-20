887a38b5cb929d7a0648945d1dd3991a1d2adb12
Xiqi Ruan <xiqiruan@google.com>
2024-11-20 02:05:51 +0000

    demo_mode: Send clean up request with last device id
    Persist sign in scope device id as session id to local state and send
    with clean up request in next session. This device id cannot be used to
    set up demo account for a second time. It will be used as a session
    identifier on server side.
    
    Bug: 370808139
    Change-Id: I10d6a0995f7796eea287481cc0630b2eefeff6ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967676
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385398}

--- chromeos/ash/components/demo_mode/utils/demo_session_utils.cc
+++ chromeos/ash/components/demo_mode/utils/demo_session_utils.cc
@@ -36,6 +36,8 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 
   // Demo login controller prefs:
   registry->RegisterStringPref(prefs::kDemoAccountGaiaId, std::string());
+  registry->RegisterStringPref(prefs::kDemoModeSessionIdentifier,
+                               std::string());
 }
 
 }  // namespace ash::demo_mode
