1c472186a382c7fbeb6c8e04312c4c7497db279b
Fredrik Söderquist <fs@opera.com>
2024-11-20 09:59:56 +0000

    spanification: StringImpl::StripMatchedCharacters
    Factor out a StrippedMatchedCharactersRange() helper, and reuse that in
    said function and StringImpl::LengthWithStrippedWhiteSpace().
    
    Bug: 351564777
    Change-Id: Ie92b6e25793f0db54a0c3efe6c362e096e770f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032235
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385530}

--- third_party/blink/renderer/platform/wtf/text/string_impl.h
+++ third_party/blink/renderer/platform/wtf/text/string_impl.h
@@ -404,7 +404,7 @@ class WTF_EXPORT StringImpl {
 
   scoped_refptr<StringImpl> Truncate(wtf_size_t length);
 
-  unsigned LengthWithStrippedWhiteSpace() const;
+  wtf_size_t LengthWithStrippedWhiteSpace() const;
 
   scoped_refptr<StringImpl> StripWhiteSpace();
   scoped_refptr<StringImpl> StripWhiteSpace(IsWhiteSpaceFunctionPtr);
@@ -587,8 +587,6 @@ class WTF_EXPORT StringImpl {
                                     const UChar* replacement,
                                     wtf_size_t replacement_length);
 
-  template <class UCharPredicate>
-  unsigned LengthWithStrippedMatchedCharacters(UCharPredicate) const;
   template <class UCharPredicate>
   scoped_refptr<StringImpl> StripMatchedCharacters(UCharPredicate);
   template <typename CharType, class UCharPredicate>
