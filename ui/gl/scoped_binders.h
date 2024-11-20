a83c84d787b87eb17df93604dbff0e7ecfa5110c
Michael Wilson <mjwilson@chromium.org>
2024-11-20 19:52:51 +0000

    [Code Health] Remove RAW_PTR_EXCLUSION for gl::ScopedVertexAttribArray
    Use AsEphemeralRawAddr() because:
    
    A) We are calling third-party function glGetVertexAttribPointerv
       (can’t change the function signature)
    
    B) The caller doesn’t store the void** anywhere
       (alleviating lifetime management concerns).
    
    Pinpoint results:
    https://pinpoint-dot-chromeperf.appspot.com/job/160b1792210000
    https://pinpoint-dot-chromeperf.appspot.com/job/14a4a5bc210000
    
    Bug: 346694210
    Change-Id: I6c7442f9115414ae032c5814e89e966983ab1f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024164
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385834}

--- ui/gl/scoped_binders.h
+++ ui/gl/scoped_binders.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "ui/gl/gl_export.h"
 
 namespace gl {
@@ -115,9 +114,7 @@ class GL_EXPORT ScopedVertexAttribArray {
   int type_;
   int normalized_;
   int stride_;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION void* pointer_;
+  raw_ptr<void> pointer_;
 };
 
 class GL_EXPORT ScopedBufferBinder {
