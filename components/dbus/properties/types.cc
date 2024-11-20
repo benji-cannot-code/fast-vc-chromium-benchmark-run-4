1f72c305580a471be46cd5df45172be365e200e3
Tom Anderson <thomasanderson@chromium.org>
2024-11-20 18:29:08 +0000

    Add UntypedDbusContainer
    This is required to implement DbusType::Read().
    
    R=thestig
    
    Change-Id: I22a5b961ef0de628fdfcd3b59377e5640ac326d5
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036611
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385764}

--- components/dbus/properties/types.cc
+++ components/dbus/properties/types.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/notreached.h"
 #include "dbus/message.h"
 
 DbusType::~DbusType() = default;
@@ -15,6 +16,9 @@ bool DbusType::operator==(const DbusType& other) const {
   if (!TypeMatches(other)) {
     return false;
   }
+  // Dynamic types should be casted to static types before comparison.
+  CHECK(!IsUntyped());
+  CHECK(!other.IsUntyped());
   return IsEqual(other);
 }
 
@@ -22,9 +26,43 @@ bool DbusType::operator!=(const DbusType& other) const {
   return !(*this == other);
 }
 
+bool DbusType::IsUntyped() const {
+  return false;
+}
+
 bool DbusType::TypeMatches(const DbusType& other) const {
   return GetSignatureDynamic() == other.GetSignatureDynamic();
 }
+
+namespace detail {
+
+UntypedDbusContainer::UntypedDbusContainer() = default;
+
+UntypedDbusContainer::UntypedDbusContainer(
+    UntypedDbusContainer&& other) noexcept = default;
+UntypedDbusContainer& UntypedDbusContainer::operator=(
+    UntypedDbusContainer&& other) noexcept = default;
+UntypedDbusContainer::~UntypedDbusContainer() = default;
+
+void UntypedDbusContainer::Write(dbus::MessageWriter* writer) const {
+  // Dynamic types are read from variants. Only static types should be written.
+  NOTREACHED();
+}
+
+std::string UntypedDbusContainer::GetSignatureDynamic() const {
+  return signature_;
+}
+
+bool UntypedDbusContainer::IsEqual(const DbusType& other_type) const {
+  // Dynamic types should be casted to static types before comparison.
+  NOTREACHED();
+}
+
+bool UntypedDbusContainer::IsUntyped() const {
+  return true;
+}
+
+}  // namespace detail
 
 DbusVariant::DbusVariant() = default;
 
