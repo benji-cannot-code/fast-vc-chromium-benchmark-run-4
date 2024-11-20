68ed4b9f87a6823b3a9938558f21a37f87eff9f2
ssid <ssid@chromium.org>
2024-11-20 19:43:36 +0000

    [data sharing] Add config API for updating preview image
    When preview image is fetched, the join/share ui is updated with the
    image.
    
    Bug: 354773630
    Change-Id: Ia0caf5f19cc32c8aaa9e770b2c18bf7bb7baa3f5
    Low-Coverage-Reason: prototype code will be replaced by service delegate.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6031929
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385826}

---
+++ components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/DataSharingPreviewDataConfig.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.data_sharing.configs;
+
+import android.graphics.Bitmap;
+
+/** Preview data for images in UI flows */
+public final class DataSharingPreviewDataConfig {
+    private final Bitmap mTabGroupPreviewImage;
+
+    private DataSharingPreviewDataConfig(Bitmap tabGroupPreviewImage) {
+        this.mTabGroupPreviewImage = tabGroupPreviewImage;
+    }
+
+    public Bitmap getTabGroupPreviewImage() {
+        return mTabGroupPreviewImage;
+    }
+
+    // Builder class
+    public static class Builder {
+        private Bitmap mTabGroupPreviewImage;
+
+        /** The preview image bitmap */
+        public Builder setTabGroupPreviewImage(Bitmap tabGroupPreviewImage) {
+            this.mTabGroupPreviewImage = tabGroupPreviewImage;
+            return this;
+        }
+
+        public DataSharingPreviewDataConfig build() {
+            return new DataSharingPreviewDataConfig(mTabGroupPreviewImage);
+        }
+    }
+}
