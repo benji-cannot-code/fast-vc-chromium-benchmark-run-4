3826dfc9277ebc49de809f870d44e2c1b82b5e6d
Robert Phillips <robertphillips@google.com>
2024-11-20 18:55:23 +0000

    Use PrecompileContext for Precompilation
    The graphite::Context isn't thread-safe so shouldn't be passed around for threaded precompilation. The new PrecompileContext *is* thread-safe.
    
    This also addresses the earlier issues about when the graphite::Context is deleted. The PrecompileContext will hold on to the resources it needs until Precompilation is complete.
    
    Low-Coverage-Reason: TESTS_ARE_DISABLED
    Bug: skbug.com/358074434
    Change-Id: Ief142786cfa52d73f6cb7ee051f2752ad773bcab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038657
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385783}

--- gpu/command_buffer/service/shared_context_state.cc
+++ gpu/command_buffer/service/shared_context_state.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/gpu/ganesh/gl/GrGLDirectContext.h"
 #include "third_party/skia/include/gpu/ganesh/mock/GrMockTypes.h"
 #include "third_party/skia/include/gpu/graphite/Context.h"
+#include "third_party/skia/include/gpu/graphite/PrecompileContext.h"
 #include "third_party/skia/include/gpu/graphite/precompile/PaintOptions.h"
 #include "third_party/skia/include/gpu/graphite/precompile/Precompile.h"
 #include "ui/gl/gl_bindings.h"
@@ -104,7 +105,8 @@ size_t MaxNumSkSurface() {
 
 // TODO: crbug.com/376667859 - passing the graphite::Context to here isn't safe.
 // Correct when a suitable Skia object is added.
-void PerformPrecompilation(skgpu::graphite::Context* context) {
+void PerformPrecompilation(
+    std::unique_ptr<skgpu::graphite::PrecompileContext> precompileContext) {
   constexpr skgpu::graphite::RenderPassProperties kProps = {
       skgpu::graphite::DepthStencilFlags::kDepth, kBGRA_8888_SkColorType,
       /* requiresMSAA= */ false};
@@ -113,7 +115,7 @@ void PerformPrecompilation(skgpu::graphite::Context* context) {
   skgpu::graphite::PaintOptions paintOptions;
   paintOptions.setBlendModes({SkBlendMode::kSrcOver});
 
-  Precompile(context, paintOptions,
+  Precompile(precompileContext.get(), paintOptions,
              skgpu::graphite::DrawTypeFlags::kBitmapText_Mask, {&kProps, 1});
 }
 
@@ -122,13 +124,17 @@ void InitiatePrecompilation(skgpu::graphite::Context* context) {
       base::TaskPriority::BEST_EFFORT,
       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN};
 
+  std::unique_ptr<skgpu::graphite::PrecompileContext> precompileContext =
+      context->makePrecompileContext();
+
   // TODO: crbug.com/358074434 - need to determine the actual delay or initiate
   // precompilation at first idle
   constexpr base::TimeDelta precompile_wait = base::Seconds(1);
 
   base::ThreadPool::PostDelayedTask(
       FROM_HERE, precompile_traits,
-      base::BindOnce(&PerformPrecompilation, context), precompile_wait);
+      base::BindOnce(&PerformPrecompilation, std::move(precompileContext)),
+      precompile_wait);
 }
 
 // Creates a Graphite recorder, supplying it with a GraphiteImageProvider.
