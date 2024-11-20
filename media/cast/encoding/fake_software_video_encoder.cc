4379dc580cb95ee3e30f809dd413bc24b007b5ee
Mikihito Matsuura <mikt@google.com>
2024-11-20 05:57:10 +0000

    Revert "[Cast Streaming] Use is_key_frame instead of dependency type."
    This reverts commit cec749f1338152dfd98bd70944c10c6a806e31ad.
    
    Reason for revert: Failure https://crbug.com/379758206 is consistent,
    tentatively reverting the change so that the author can take time to
    fix the issue.
    
    Original change's description:
    > [Cast Streaming] Use is_key_frame instead of dependency type.
    >
    > This patch just removes the Dependency enum in favor of
    > using a boolean argument, aligning the legacy API with the new media
    > VideoEncoder API. The unused encoder_bitrate is also removed.
    >
    > Googlers may view the related design doc at:
    > http://go/cast-streaming-media-videoencoder
    >
    > Publicly published design documentation available at:
    > https://tinyurl.com/cast-streaming-mvew
    >
    > Bug: 282984511
    > Change-Id: I2fb22b672f68d145d10bc9e091707151ae9c4016
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020961
    > Commit-Queue: Jordan Bayles <jophba@chromium.org>
    > Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1384570}
    
    Bug: 282984511
    Change-Id: I63c0557f1b75bc21b977f41e8e09ebf9c0971fcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6034479
    Owners-Override: Mikihito Matsuura <mikt@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385458}

--- media/cast/encoding/fake_software_video_encoder.cc
+++ media/cast/encoding/fake_software_video_encoder.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cast/common/rtp_time.h"
 #include "media/cast/common/sender_encoded_frame.h"
 #include "media/cast/constants.h"
+#include "third_party/openscreen/src/cast/streaming/public/encoded_frame.h"
+
+using Dependency = openscreen::cast::EncodedFrame::Dependency;
 
 namespace media::cast {
 
@@ -43,11 +46,12 @@ void FakeSoftwareVideoEncoder::Encode(
   }
 
   encoded_frame->frame_id = frame_id_++;
-  encoded_frame->is_key_frame = next_frame_is_key_;
   if (next_frame_is_key_) {
+    encoded_frame->dependency = Dependency::kKeyFrame;
     encoded_frame->referenced_frame_id = encoded_frame->frame_id;
     next_frame_is_key_ = false;
   } else {
+    encoded_frame->dependency = Dependency::kDependent;
     encoded_frame->referenced_frame_id = encoded_frame->frame_id - 1;
   }
   encoded_frame->rtp_timestamp =
@@ -56,7 +60,7 @@ void FakeSoftwareVideoEncoder::Encode(
 
   const auto values =
       base::Value::Dict()
-          .Set("key", encoded_frame->is_key_frame)
+          .Set("key", encoded_frame->dependency == Dependency::kKeyFrame)
           .Set("ref", static_cast<int>(
                           encoded_frame->referenced_frame_id.lower_32_bits()))
           .Set("id", static_cast<int>(encoded_frame->frame_id.lower_32_bits()))
@@ -70,7 +74,7 @@ void FakeSoftwareVideoEncoder::Encode(
   encoded_frame->data = base::HeapArray<uint8_t>::CopiedFrom(
       base::as_bytes(base::span(raw_data)));
 
-  if (encoded_frame->is_key_frame) {
+  if (encoded_frame->dependency == Dependency::kKeyFrame) {
     encoded_frame->encoder_utilization = 1.0;
     encoded_frame->lossiness = 6.0;
   } else {
