7224e586fc993d2842f291682fd845a84ee05b32
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-11-20 04:02:06 +0000

    [UIA] Expose the Expanded/Collapsed state for submenu MenuItemViews
    This CL fixes an issue with Narrator and NVDA not announcing whether
    menu items have a submenu. Exposing the expanded and collapsed state
    fixes the bug, and brings the experience on par with Microsoft Edge.
    
    Bug: 379886423
    Change-Id: I538c18955a8380e1c6b6e055e820c5d46e9cd0b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032159
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385432}

--- ui/views/controls/menu/menu_item_view.cc
+++ ui/views/controls/menu/menu_item_view.cc
@@ -434,6 +434,7 @@ SubmenuView* MenuItemView::CreateSubmenu() {
   submenu_ = std::make_unique<SubmenuView>(/*parent=*/this);
   submenu_->SetProperty(kElementIdentifierKey, submenu_id_);
   UpdateAccessibleHasPopup();
+  UpdateAccessibleExpandedCollapsedState();
 #if BUILDFLAG(IS_MAC)
   // All MenuItemViews of Type kSubMenu have a respective SubmenuView.
   // However, in the Views hierarchy, this SubmenuView is not a child of the
@@ -901,6 +902,7 @@ MenuItemView::MenuItemView(MenuItemView* parent,
 
   UpdateAccessibleSelection();
   UpdateAccessibleKeyShortcuts();
+  UpdateAccessibleExpandedCollapsedState();
 }
 
 void MenuItemView::PrepareForRun(bool has_mnemonics, bool show_mnemonics) {
@@ -1643,6 +1645,19 @@ void MenuItemView::UpdateAccessibleName() {
             GetScrollViewContainerViewAccessibility()) {
       scrollview_accessibility->SetName(accessible_name);
     }
+  }
+}
+
+void MenuItemView::UpdateAccessibleExpandedCollapsedState() {
+  if (type_ != Type::kSubMenu && type_ != Type::kActionableSubMenu) {
+    GetViewAccessibility().RemoveExpandCollapseState();
+    return;
+  }
+
+  if (SubmenuIsShowing()) {
+    GetViewAccessibility().SetIsExpanded();
+  } else {
+    GetViewAccessibility().SetIsCollapsed();
   }
 }
 
--- ui/views/controls/menu/submenu_view.cc
+++ ui/views/controls/menu/submenu_view.cc
@@ -502,6 +502,8 @@ void SubmenuView::ShowAt(const MenuHost::InitParams& init_params) {
   // Fire kMenuPopupStart for each menu/submenu that is shown.
   NotifyAccessibilityEvent(ax::mojom::Event::kMenuPopupStart, true);
 
+  GetMenuItem()->UpdateAccessibleExpandedCollapsedState();
+
   // Announce if the menu/submenu is empty.
   if (GetRowCount() == 0) {
     GetViewAccessibility().AnnouncePolitely(
@@ -522,6 +524,7 @@ void SubmenuView::Close() {
   if (host_) {
     host_->DestroyMenuHost();
     host_ = nullptr;
+    GetMenuItem()->UpdateAccessibleExpandedCollapsedState();
   }
 }
 
@@ -543,6 +546,7 @@ void SubmenuView::Hide() {
       NotifyAccessibilityEvent(ax::mojom::Event::kMenuPopupEnd, true);
 
     host_->HideMenuHost();
+    GetMenuItem()->UpdateAccessibleExpandedCollapsedState();
   }
 
   if (scroll_animator_->is_scrolling())
