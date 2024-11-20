8d4624374cf778e2d1b127ab85b816ec4a9b6121
Noémie St-Onge <noemies@google.com>
2024-11-20 20:12:41 +0000

    [iOS] Handle double taps on the passkey welcome screen primary button
    The primary action button of the passkey welcome screen (especially the
    one leading to the enrollment process), can be tapped multiple times
    before the action is executed and the next screen is presented. On the
    second tap, `_primaryButtonAction` is `nil` resulting in a crash.
    
    Another (an probably more common) scenario where the primary button
    can be tapped more than once is when the enrollment keychain UI (shown
    when tapping the primary button the first time) is dismissed with a
    down swipe, leaving the passkey welcome screen at the top of the
    navigation stack again. The button then becomes available an can be
    tapped a second time, but crashes as `_primaryButtonAction` has become
    `nil`.
    
    This CL fixes the issue by only executing the block if the current
    passkey welcome screen is still at the top of the navigation stack
    (i.e., additional taps, if any, are ignored), and by allowing
    `_primaryButtonAction` to be executed more than once by not setting it
    to `nil` after it's first used.
    
    Fixed: 379172052
    Change-Id: I5df4f1769330dcc4b0568eab880f89a8780831e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037963
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385853}

--- ios/chrome/credential_provider_extension/ui/passkey_welcome_screen_view_controller.mm
+++ ios/chrome/credential_provider_extension/ui/passkey_welcome_screen_view_controller.mm
@@ -240,10 +240,12 @@ NSString* GetPrimaryButtonTitle(PasskeyWelcomeScreenPurpose purpose) {
 }
 
 - (void)didTapPrimaryActionButton {
-  ProceduralBlock primaryButtonAction = _primaryButtonAction;
-  _primaryButtonAction = nil;
+  if (self.navigationController.topViewController != self) {
+    return;
+  }
 
-  primaryButtonAction();
+  CHECK(_primaryButtonAction);
+  _primaryButtonAction();
 }
 
 - (void)didTapSecondaryActionButton {
