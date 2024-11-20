63b906344dd0539806ab809149cec8ee5ee3a1d6
Steinar H. Gunderson <sesse@chromium.org>
2024-11-20 09:21:11 +0000

    Unbreak the build.
    Bug: 40286814
    Change-Id: Id0284db996120ad39598799c437cbde841dd64d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033675
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385507}

--- third_party/blink/renderer/core/html/forms/menu_list_inner_element.cc
+++ third_party/blink/renderer/core/html/forms/menu_list_inner_element.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/forms/menu_list_inner_element.h"
 
 #include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
+#include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
