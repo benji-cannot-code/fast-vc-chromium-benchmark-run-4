61e213f26859f5235ebf38f071c5781ed2433467
Mikihito Matsuura <mikt@google.com>
2024-11-20 05:54:41 +0000

    Revert "Add timing histograms and trace events to sql database ops"
    This reverts commit 0fae0abfd2c94989b8220e2d05b93d90999a732f.
    
    Reason for revert: Race access to base::ScopedMockElapsedTimersForTest
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20TSan%20Tests/92754/overview
    
    Original change's description:
    > Add timing histograms and trace events to sql database ops
    >
    > This CL adds a few histograms:
    > - Sql.Statement.StepTime.{DatabaseTag}: time spent in each
    >   single `Step()` call
    > - Sql.Statement.ExecutionTime.{DatabaseTag}: total time
    >   spent in `Step()` within the lifetime of each statement
    > - Sql.Database.Success.OpenInternalTime.{DatabaseTag}: time
    >   spent in `Database::OpenInternal()`
    > - Sql.Database.Success.SqliteOpenTime.{DatabaseTag}: time taken
    >   by the `sqlite3_open_v2()` call within `Database`
    >
    > It also adds a few trace events:
    > - "Database::OpenInternal sqlite3_open_v2" directly around
    >   the `sqlite3_open_v2()` call
    > - "Database::Statement", spanning the time spent stepping for
    >   each statement. These are emitted in new tracks named
    >   "Database: {Tag}" parented on the track of the thread calling
    >   `Step()`. These events also contain the associated prepared
    >   statements (without bound parameters).
    >
    > Bug: 40949392
    > Change-Id: I2de4a0359e68961f58d5e7f0434b5c56292626d1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6019886
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    > Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    > Reviewed-by: Ayu Ishii <ayui@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1385191}
    
    Bug: 40949392
    Change-Id: Ie5af7e4ef64d26e868d40cda803ed376b4c84763
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6034678
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mikihito Matsuura <mikt@google.com>
    Auto-Submit: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385457}

--- sql/statement.h
+++ sql/statement.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -310,8 +309,6 @@ class COMPONENT_EXPORT(SQL) Statement {
   bool step_called_ GUARDED_BY_CONTEXT(sequence_checker_) = false;
   bool run_called_ GUARDED_BY_CONTEXT(sequence_checker_) = false;
 #endif  // DCHECK_IS_ON()
-
-  std::optional<base::TimeDelta> time_spent_stepping_ = std::nullopt;
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
