4ff5d14095414abb2986a09f461daa87d570e265
renkens <renkens@google.com>
2024-11-20 15:19:47 +0000

    feat chrome/browser & chromeos/constants: remove the expired flag
    Bug: 361119457
    Change-Id: I51721070df992e56fd4187a5666af7c4707b6bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020927
    Commit-Queue: Daniil Hrybchuk <renkens@google.com>
    Reviewed-by: Jason Lin <lxj@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385665}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4392,9 +4392,6 @@ extern const char kTetheringExperimentalFunctionalityDescription[];
 #endif  // #if BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-extern const char kExperimentalWebAppStoragePartitionIsolationName[];
-extern const char kExperimentalWebAppStoragePartitionIsolationDescription[];
-
 extern const char kBlinkExtensionName[];
 extern const char kBlinkExtensionDescription[];
 
