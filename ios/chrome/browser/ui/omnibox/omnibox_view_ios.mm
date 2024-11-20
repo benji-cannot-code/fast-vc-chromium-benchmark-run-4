25f48da37865b4fafe5332250fc90a86c31ec7d4
Christian Xu <christianxu@google.com>
2024-11-20 09:25:22 +0000

    [iOS][LO][RemoveSelection] Remove selection UI when removing thumbnail
    Currently, thumbnail removal is only effective when the user accepts an
    omnibox suggestion.
    
    This CL immediately removes the selection UI when the thumbnail is
    removed.
    - Moves thumbnailRemoved state from omnibox to lens overlay.
    - Hides lens selection UI when the thumbnail is removed.
    
    Bug: 375194155
    Change-Id: Ic1f0a154e4526575d3531803cf63d420c7a22b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6003154
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385511}

--- ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
@@ -90,7 +90,6 @@ void OmniboxViewIOS::OnReceiveClipboardURLForOpenMatch(
   AutocompleteController* autocomplete_controller =
       controller()->autocomplete_controller();
 
-  AcceptThumbnailEdits();
   OmniboxPopupSelection selection(autocomplete_controller->InjectAdHocMatch(
       autocomplete_controller->clipboard_provider()->NewClipboardURLMatch(
           url)));
@@ -120,7 +119,6 @@ void OmniboxViewIOS::OnReceiveClipboardTextForOpenMatch(
     return;
   }
 
-  AcceptThumbnailEdits();
   OmniboxPopupSelection selection(
       controller()->autocomplete_controller()->InjectAdHocMatch(
           new_match.value()));
@@ -155,7 +153,6 @@ void OmniboxViewIOS::OnReceiveImageMatchForOpenMatch(
   if (!optional_match) {
     return;
   }
-  AcceptThumbnailEdits();
   OmniboxPopupSelection selection(
       controller()->autocomplete_controller()->InjectAdHocMatch(
           optional_match.value()));
@@ -199,7 +196,6 @@ void OmniboxViewIOS::SetCaretPos(size_t caret_pos) {
 void OmniboxViewIOS::RevertAll() {
   ignore_popup_updates_ = true;
   OmniboxView::RevertAll();
-  RevertThumbnailEdits();
   ignore_popup_updates_ = false;
 }
 
@@ -520,7 +516,6 @@ void OmniboxViewIOS::OnAccept() {
   base::RecordAction(UserMetricsAction("IOS.Omnibox.AcceptDefaultSuggestion"));
 
   if (model()) {
-    AcceptThumbnailEdits();
     // The omnibox edit model doesn't support accepting input with no text.
     // Delegate the call to the client instead.
     if (OmniboxClient* client = controller()->client();
@@ -737,7 +732,6 @@ void OmniboxViewIOS::OnSelectedMatchForOpening(
   if (index >= autocomplete_controller->result().size() ||
       autocomplete_controller->result().match_at(index).destination_url !=
           match.destination_url) {
-    AcceptThumbnailEdits();
     OmniboxPopupSelection selection(
         autocomplete_controller->InjectAdHocMatch(match));
     model()->OpenSelection(selection, match_selection_timestamp, disposition);
@@ -772,7 +766,6 @@ void OmniboxViewIOS::OnSelectedMatchForOpening(
       return;
     }
   }
-  AcceptThumbnailEdits();
   model()->OpenSelection(OmniboxPopupSelection(index),
                          match_selection_timestamp, disposition);
 }
@@ -780,43 +773,30 @@ void OmniboxViewIOS::OnSelectedMatchForOpening(
 #pragma mark - Thumbnail
 
 void OmniboxViewIOS::SetThumbnailImage(UIImage* image) {
-  thumbnail_image_before_edit_ = image;
-  thumbnail_deleted_ = NO;
   [consumer_ setThumbnailImage:image];
   if (popup_provider_) {
     popup_provider_->SetHasThumbnail(image != nil);
   }
 }
 
-void OmniboxViewIOS::AcceptThumbnailEdits() {
-  if (thumbnail_deleted_) {
-    base::RecordAction(UserMetricsAction("Mobile.OmniboxThumbnail.Deleted"));
-    thumbnail_image_before_edit_ = nil;
-    thumbnail_deleted_ = NO;
-    if (OmniboxClient* client = controller()->client()) {
-      client->OnThumbnailRemoved();
-    }
-  }
-}
-
-void OmniboxViewIOS::RevertThumbnailEdits() {
-  if (thumbnail_deleted_) {
-    [consumer_ setThumbnailImage:thumbnail_image_before_edit_];
-    thumbnail_deleted_ = NO;
-    if (popup_provider_) {
-      popup_provider_->SetHasThumbnail(thumbnail_image_before_edit_ != nil);
-    }
-  }
-}
-
 void OmniboxViewIOS::RemoveThumbnail() {
-  thumbnail_deleted_ = YES;
+  base::RecordAction(UserMetricsAction("Mobile.OmniboxThumbnail.Deleted"));
+  // Update the client state.
+  if (OmniboxClient* client = controller()->client()) {
+    client->OnThumbnailRemoved();
+  }
+  // Update the UI.
   [consumer_ setThumbnailImage:nil];
   if (popup_provider_) {
     popup_provider_->SetHasThumbnail(false);
   }
+  // Generate new results.
   if (model()) {
-    model()->UpdateInput(/*has_selected_text=*/false,
-                         /*prevent_inline_autocomplete=*/true);
+    if (field_.userText.length) {
+      model()->UpdateInput(/*has_selected_text=*/false,
+                           /*prevent_inline_autocomplete=*/true);
+    } else {
+      CloseOmniboxPopup();
+    }
   }
 }
