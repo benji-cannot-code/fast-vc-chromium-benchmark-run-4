d569f98256484040b29f42da147d1f4cc3808ec9
Finnur Thorarinsson <finnur@chromium.org>
2024-11-20 11:41:37 +0000

    [NavCapture]: Migrate away from Find[Installed]AppWithUrlInScopes.
    This is an intermediary no-op step where we switch calls to
    Find[Installed]AppWithUrlInScopes over to calls to
    FindBestAppWithUrlInScopes (which is what these functions do
    internally).
    
    This change is for directory:
    chrome/browser/apps
    
    Planned (but beyond the scope of this CL) is an effort to
    evaluate all call sites of FindBestAppWithUrlInScopes for
    correctness and introduce new helpers, that hopefully will
    be more resistant to incorrect use.
    
    Design doc: https://docs.google.com/document/d/14JPgYUVX8ECAMdXlp6GwXP-ucXnIIPXjTfxWt-WINLo/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=skuhne@chromium.org
    
    Bug: 379827962
    Change-Id: Ic1063fb9def7335fd3011fb7668343e1fc4fcd01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033318
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385572}

--- chrome/browser/apps/digital_goods/util.cc
+++ chrome/browser/apps/digital_goods/util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
+#include "chrome/browser/web_applications/proto/web_app_install_state.pb.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "content/public/browser/document_user_data.h"
@@ -55,8 +56,14 @@ std::string GetScope(content::RenderFrameHost* render_frame_host) {
   }
 
   const web_app::WebAppRegistrar& registrar = provider->registrar_unsafe();
-  std::optional<webapps::AppId> app_id = registrar.FindAppWithUrlInScope(
-      render_frame_host->GetMainFrame()->GetLastCommittedURL());
+  // TODO(crbug.com/379827962): Evaluate call sites of FindBestAppWithUrlInScope
+  // for correctness.
+  std::optional<webapps::AppId> app_id = registrar.FindBestAppWithUrlInScope(
+      render_frame_host->GetMainFrame()->GetLastCommittedURL(),
+      {
+          web_app::proto::InstallState::INSTALLED_WITH_OS_INTEGRATION,
+          web_app::proto::InstallState::INSTALLED_WITHOUT_OS_INTEGRATION,
+      });
   if (!app_id) {
     return std::string();
   }
