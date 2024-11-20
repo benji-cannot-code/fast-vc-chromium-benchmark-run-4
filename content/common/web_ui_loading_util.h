6a8451e28f702e39561ea5c175b6f2647b56fb1b
Alex Yang <aycyang@chromium.org>
2024-11-20 19:50:29 +0000

    Move WebUI HTTP request header byte range handling to a shared location
    In a future commit, this code will be re-used in the implementation of
    an in-renderer resource loader, which will not be located in
    //content/browser.
    
    Bug: 362511750
    Change-Id: I1e93b762005756b61d503540537eba348f0430fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033597
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385830}

---
+++ content/common/web_ui_loading_util.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_COMMON_WEB_UI_LOADING_UTIL_H_
+#define CONTENT_COMMON_WEB_UI_LOADING_UTIL_H_
+
+#include "base/types/expected.h"
+#include "content/common/content_export.h"
+#include "net/http/http_byte_range.h"
+#include "net/http/http_request_headers.h"
+
+namespace content {
+
+enum class GetRequestedRangeError {
+  kNoRanges,
+  kMultipleRanges,
+  kParseFailed,
+};
+
+// Get the requested byte range in the request headers if present. For
+// simplicity, only single byte ranges are considered valid input. If there are
+// zero or multiple byte ranges, an error is returned. This is deemed
+// sufficient for WebUI content.
+CONTENT_EXPORT base::expected<net::HttpByteRange, GetRequestedRangeError>
+GetRequestedRange(const net::HttpRequestHeaders& headers);
+
+}  // namespace content
+
+#endif  // CONTENT_COMMON_WEB_UI_LOADING_UTIL_H_
