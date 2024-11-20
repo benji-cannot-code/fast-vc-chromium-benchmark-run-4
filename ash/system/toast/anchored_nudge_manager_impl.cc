a71a4b57c55b4b45efdcefbda1f17de5dd60b1d3
Xiaodan Zhu <zxdan@chromium.org>
2024-11-20 23:54:06 +0000

    birch: dangling ptr on removing first chip with nudge
    When the privacy nudage is anchored on the first Glanceables chip,
    removing the chip will make the raw ptr `anchor_view_` in
    AnchorViewWidgetObserver dangling. Actually, we don't need to cache
    the anchor view pointer since the only use is to get its widget but
    the widget pointer is cached in `active_widget_`. Remove `anchor_view_`
    resolved the issue.
    
    Test: BirchBarAnimationTest.NoCrashOnRemovingFirstChipWithPrivacyNudge
    Bug: b:380086558
    Change-Id: I07814304a54c2351c23dc5db144579d741d5272a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037372
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385961}

--- ash/system/toast/anchored_nudge_manager_impl.cc
+++ ash/system/toast/anchored_nudge_manager_impl.cc
@@ -198,7 +198,6 @@ class AnchoredNudgeManagerImpl::AnchorViewWidgetObserver
                            views::View* anchor_view,
                            AnchoredNudgeManagerImpl* anchored_nudge_manager)
       : anchored_nudge_(anchored_nudge),
-        anchor_view_(anchor_view),
         anchored_nudge_manager_(anchored_nudge_manager) {
     DCHECK(anchor_view->GetWidget());
     active_widget_ = anchor_view->GetWidget();
@@ -225,7 +224,6 @@ class AnchoredNudgeManagerImpl::AnchorViewWidgetObserver
   void OnWidgetDestroying(views::Widget* widget) override {
     widget->RemoveObserver(this);
     active_widget_ = nullptr;
-    anchor_view_ = nullptr;
     anchored_nudge_ = nullptr;
   }
 
@@ -234,16 +232,14 @@ class AnchoredNudgeManagerImpl::AnchorViewWidgetObserver
     const std::string id = anchored_nudge_->id();
     // Make sure the nudge bubble no longer observes the anchor view.
     anchored_nudge_->SetAnchorView(nullptr);
-    anchor_view_->GetWidget()->RemoveObserver(this);
+    active_widget_->RemoveObserver(this);
     active_widget_ = nullptr;
-    anchor_view_ = nullptr;
     anchored_nudge_ = nullptr;
     anchored_nudge_manager_->Cancel(id);
   }
 
   // Owned by the views hierarchy.
   raw_ptr<AnchoredNudge> anchored_nudge_;
-  raw_ptr<views::View> anchor_view_;
   raw_ptr<views::Widget> active_widget_;
 
   // `AnchorViewWidgetObserver` is guaranteed to not outlive
