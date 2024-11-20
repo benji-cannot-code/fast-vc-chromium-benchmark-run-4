59518c99fa9a177babd78f6554115aca8e4e1735
conniekxu <conniekxu@chromium.org>
2024-11-20 05:33:29 +0000

    coral: Nudge the name for the newly created coral desk
    Nudge the name for the newly created coral desk. When there're multiple
    displays, we already nudge the name on the display on which the pressed
    birch chip button is. To achieve this, we passed the birch chip button
    being pressed to the callback function.
    
    Screen record:
    https://screencast.googleplex.com/cast/NTY3MDA1Nzk2MjExMDk3Nnw4ODU0MGI2YS01Yw
    
    Bug=b:379894669
    
    Change-Id: I77f976663055fecf42c82d511ba872c575b5b79d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035786
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385454}

--- ash/birch/birch_coral_item.h
+++ ash/birch/birch_coral_item.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class BirchChipButtonBase;
+
 class ASH_EXPORT BirchCoralItem : public BirchItem {
  public:
   BirchCoralItem(const std::u16string& coral_title,
@@ -29,6 +31,9 @@ class ASH_EXPORT BirchCoralItem : public BirchItem {
   BirchCoralItem& operator=(const BirchCoralItem&);
   bool operator==(const BirchCoralItem& rhs) const;
   ~BirchCoralItem() override;
+
+  // The callback function for the press on the given `birch_chip_button`.
+  void LaunchGroup(BirchChipButtonBase* birch_chip_button);
 
   const base::Token& group_id() const { return group_id_; }
 
--- ash/wm/coral/coral_controller.cc
+++ ash/wm/coral/coral_controller.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_histogram_enums.h"
 #include "ash/wm/mru_window_tracker.h"
-#include "ash/wm/overview/overview_session.h"
 #include "ash/wm/snap_group/snap_group.h"
 #include "ash/wm/snap_group/snap_group_controller.h"
 #include "base/command_line.h"
