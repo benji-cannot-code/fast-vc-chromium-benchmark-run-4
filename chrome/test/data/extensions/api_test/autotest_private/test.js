f0142f2cb13ff82b8fe00eacd1f44421082e10ca
Georg Neis <neis@chromium.org>
2024-11-20 01:54:01 +0000

    Delete obsolete autotestPrivate.getLacrosInfo
    The Tast library that was using this API was removed in
    https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/6012261.
    
    Bug: b:373972275
    Change-Id: I5504dfd00a1ebcf6972d243ab4d4074cb5898018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022570
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385394}

--- chrome/test/data/extensions/api_test/autotest_private/test.js
+++ chrome/test/data/extensions/api_test/autotest_private/test.js
@@ -1772,28 +1772,6 @@ var systemWebAppsTests = [
   },
 ]
 
-    var lacrosEnabledTests = [
-      function checkLacrosInfoFields() {
-        chrome.autotestPrivate.getLacrosInfo(
-            chrome.test.callbackPass(function(lacrosInfo) {
-              chrome.test.assertEq(typeof lacrosInfo, 'object');
-              chrome.test.assertTrue(lacrosInfo.hasOwnProperty('state'));
-              chrome.test.assertTrue(lacrosInfo.hasOwnProperty('isKeepAlive'));
-              chrome.test.assertTrue(lacrosInfo.hasOwnProperty('lacrosPath'));
-              chrome.test.assertTrue(lacrosInfo.hasOwnProperty('mode'));
-            }));
-      },
-      function checkLacrosInfoFieldValue() {
-        chrome.autotestPrivate.getLacrosInfo(
-            chrome.test.callbackPass(function(lacrosInfo) {
-              chrome.test.assertEq('Unavailable', lacrosInfo['state']);
-              chrome.test.assertTrue(lacrosInfo['isKeepAlive']);
-              chrome.test.assertEq('', lacrosInfo['lacrosPath']);
-              chrome.test.assertEq('Only', lacrosInfo['mode']);
-            }));
-      },
-    ]
-
     var test_suites = {
       'default': defaultTests,
       'arcEnabled': arcEnabledTests,
@@ -1809,7 +1787,6 @@ var systemWebAppsTests = [
       'isFeatureEnabled': isFeatureEnabledTests,
       'holdingSpace': holdingSpaceTests,
       'systemWebApps': systemWebAppsTests,
-      'lacrosEnabled': lacrosEnabledTests,
       'launcherSearchBoxState': launcherSearchBoxStateTests,
       'isFieldTrialActive': isFieldTrialActiveTests,
       'clearAllowedPref': clearAllowedPrefTests,
