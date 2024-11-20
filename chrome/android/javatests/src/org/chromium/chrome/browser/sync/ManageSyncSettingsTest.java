1bcbe0f44567e8aca67b4bf68ec726c26426a5d3
Duong Dac <ddac@chromium.org>
2024-11-20 12:48:41 +0000

    [gardening] Disable testSigninSettingsTopAvatarWithNoName
    Bug: 380024812
    Change-Id: I4c2f5bd9eb9fd2081dee17dc7b56bdce68f7338a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037913
    Auto-Submit: Duong Dac <ddac@chromium.org>
    Owners-Override: Duong Dac <ddac@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385603}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
@@ -1118,6 +1118,7 @@ public class ManageSyncSettingsTest {
 
     @Test
     @LargeTest
+    @DisabledTest(message = "crbug.com/380024812")
     @Feature({"Sync", "RenderTest"})
     @EnableFeatures({ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS})
     public void testSigninSettingsTopAvatarWithNoName() throws Exception {
