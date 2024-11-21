ee5f44b2a73ca80bb15e6fee7649c44862be0262
Ken Buchanan <kenrb@chromium.org>
2024-11-21 23:07:28 +0000

    [WebAuthn] Improve categorization of enclave transaction errors
    Recorded metrics are showing that many enclave transactions are failing
    with decryption errors. `Transact::OnData()` doesn't explicitly check
    for WebSocket errors, so this could be dropped connections that are
    being miscategorized.
    
    This adds a new error type for WebSocket errors. It also adds a
    histogram that reports the size of the received ciphertext when a
    decryption error is encountered, in case the guess about it being
    dropped connections is wrong.
    
    Unrelated cleanup: This also makes the websocket data callback
    stop using optional, since it never returns a nullopt, and the
    transaction code wasn't checking for it.
    
    Change-Id: I874c22977d1438f3be92a59641ffc17435bd1069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040007
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386515}

--- device/fido/enclave/transact.h
+++ device/fido/enclave/transact.h
@@ -35,8 +35,9 @@ enum class EnclaveTransactionResult {
   kDecryptionFailed = 5,
   kParseFailure = 6,
   kOtherError = 7,
+  kWebSocketError = 8,
 
-  kMaxValue = kOtherError,
+  kMaxValue = kWebSocketError,
 };
 
 enum class TransactError {
@@ -54,6 +55,8 @@ enum class TransactError {
   kHandshakeFailed,
   kSigningFailed,
   kUnknownServiceError,
+  kWebSocketError,
+
   // kOther mainly encompasses invalid responses received from the service.
   kOther,
 };
