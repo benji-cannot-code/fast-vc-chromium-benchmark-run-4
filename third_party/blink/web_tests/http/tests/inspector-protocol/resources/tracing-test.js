e0c011f7331f726fde82f9e007176fa51babd40e
Stefan Zager <szager@chromium.org>
2024-11-21 23:12:09 +0000

    Suppress devtools frame timing trace events for UI compositor
    This also fixes the test for these events. Previously, the trace event
    queries in the test would get some results from the UI compositor.
    Because the test didn't check the process ID of events, it would get a
    mix of events from the browser UI compositor, the blink compositor for
    the test host page (which also runs in the browser process) and the
    test content page (which runs in a separate renderer process). Because
    each process uses its own monotonically increasing counter for
    assigning frameSeqId, frameId, and layerTreeId, these valuse are very
    likely to be duplicated across independent compositors.
    
    Since the intention seems to be to verify events coming from the test
    content page, this test now correctly identifies the renderer process
    and filters trace events accordingly.
    
    Bug: chromium:380109520
    Change-Id: Ia3628e607935c31656701e639ba96c711e5a601c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039321
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386518}

--- third_party/blink/web_tests/http/tests/inspector-protocol/resources/tracing-test.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/tracing-test.js
@@ -126,7 +126,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var events = this.findEvents(name, ph, condition);
     if (events.length)
       return events[0];
-    throw new Error("Couldn't find event " + name + " / " + ph + "\n\n in " + JSON.stringify(this._devtoolsEvents, null, 2));
+    this._testRunner.log("Couldn't find event " + name + " / " + ph + ".");
+    return null;
   }
 
   filterEvents(callback) {
