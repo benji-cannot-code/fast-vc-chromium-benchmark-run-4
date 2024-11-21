0402c6444ae5b9fef2df4bb0e82a1d4d9bd39203
Peter Kasting <pkasting@chromium.org>
2024-11-21 18:36:25 +0000

    Remove `StringPrintfNonConstexpr()`: content/
    This is discouraged due to unsafety.
    
    Bug: none
    Change-Id: Ib74f30a0652083bac7cbcd0b041bdbcdf9d4ff5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037414
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386333}

--- content/browser/network/trust_token_parameters_browsertest.cc
+++ content/browser/network/trust_token_parameters_browsertest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/strings/escape.h"
-#include "base/strings/stringprintf.h"
+#include "base/strings/strcat.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "content/public/browser/network_service_util.h"
@@ -157,16 +157,13 @@ IN_PROC_BROWSER_TEST_P(TrustTokenParametersBrowsertest,
 
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
-  EXPECT_TRUE(
-      ExecJs(shell(),
-             base::StringPrintfNonConstexpr(
-                 JsReplace("let request = new XMLHttpRequest();"
-                           "request.open($1, $2);"
-                           "request.setPrivateToken(%s);"
-                           "request.send();",
-                           "GET", trust_token_url)
-                     .c_str(),
-                 expected_params_and_serialization.serialized_params.c_str())));
+  EXPECT_TRUE(ExecJs(
+      shell(),
+      base::StrCat({"let request = new XMLHttpRequest(); ",
+                    JsReplace("request.open(\"GET\", $1);", trust_token_url),
+                    "request.setPrivateToken(",
+                    expected_params_and_serialization.serialized_params,
+                    "); request.send();"})));
 
   monitor.WaitForUrls();
   std::optional<network::ResourceRequest> request =
