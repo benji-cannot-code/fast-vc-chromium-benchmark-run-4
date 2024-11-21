7a88e1d9ec805993ddc736295b346659082e5713
Louis Romero <lpromero@google.com>
2024-11-21 19:04:56 +0000

    Revert "Add EG tests for Join flow"
    This reverts commit 47d881517b6da935d7aef28d4f1db6f2649ce4df.
    
    Reason for revert: Breaks ios-fieldtrial-rel: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-fieldtrial-rel/34745/overview
    
    Original change's description:
    > Add EG tests for Join flow
    >
    > This CL adds two tests for Shared Tab Groups when joining a group from a
    > share URL.
    >
    > Bug: 379657889
    > Change-Id: I154fa4c115a48149d50f747f1d22eee415b18cad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6031882
    > Auto-Submit: Louis Romero <lpromero@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Louis Romero <lpromero@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1385634}
    
    Bug: 379657889
    Change-Id: Ib6a90151d44a48a9284584508d1192fd29375eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037929
    Commit-Queue: Louis Romero <lpromero@google.com>
    Owners-Override: Ali Juma <ajuma@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386356}

--- ios/chrome/browser/share_kit/model/test_share_kit_service.h
+++ ios/chrome/browser/share_kit/model/test_share_kit_service.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/saved_tab_groups/public/types.h"
 #import "ios/chrome/browser/share_kit/model/share_kit_service.h"
 
-namespace data_sharing {
-class DataSharingService;
-}  // namespace data_sharing
-
 namespace tab_groups {
 class TabGroupSyncService;
 }  // namespace tab_groups
@@ -23,8 +19,7 @@ class TabGroupSyncService;
 // Test implementation of the ShareKitService.
 class TestShareKitService : public ShareKitService {
  public:
-  TestShareKitService(data_sharing::DataSharingService* data_sharing_service,
-                      tab_groups::TabGroupSyncService* sync_service);
+  TestShareKitService(tab_groups::TabGroupSyncService* sync_service);
   TestShareKitService(const TestShareKitService&) = delete;
   TestShareKitService& operator=(const TestShareKitService&) = delete;
   ~TestShareKitService() override;
@@ -43,15 +38,11 @@ class TestShareKitService : public ShareKitService {
   id<ShareKitAvatarPrimitive> AvatarImage(
       ShareKitAvatarConfiguration* config) override;
 
-  // KeyedService.
-  void Shutdown() override;
-
  private:
   // Sets the `collab_id` for the given `tab_group_id`.
   void SetTabGroupCollabID(tab_groups::LocalTabGroupID tab_group_id,
                            NSString* collab_id);
 
-  raw_ptr<data_sharing::DataSharingService> data_sharing_service_;
   raw_ptr<tab_groups::TabGroupSyncService> sync_service_;
   base::WeakPtrFactory<TestShareKitService> weak_pointer_factory_{this};
 };
