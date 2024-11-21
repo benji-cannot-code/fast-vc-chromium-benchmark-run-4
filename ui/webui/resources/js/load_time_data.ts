016f104449741cd250e2d7f432575f9a2046aa23
dpapad <dpapad@chromium.org>
2024-11-21 01:39:37 +0000

    Migrate components/neterror/resources/neterror.js to TypeScript.
    This is part of the effort to migrate this code off of jstemplate and
    into Lit eventually.
    
    Bug: 378692755,373951324
    Change-Id: I68fd5ca7926e14dd3c4617a66219d9f774797f34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036257
    Reviewed-by: Edward Jung <edwardjung@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386008}

--- ui/webui/resources/js/load_time_data.ts
+++ ui/webui/resources/js/load_time_data.ts
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from './assert.js';
 
-interface LoadTimeDataRaw {
+export interface LoadTimeDataRaw {
   [key: string]: any;
 }
 
