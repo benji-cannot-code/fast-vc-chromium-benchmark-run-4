3be7d7557d78afef66b7f84c4433fed6f34ffcc6
Fiorella Barrientos Villalta <fiorellab@google.com>
2024-11-21 23:57:52 +0000

    Onboard enum for PasswordChangeSubmission to ModelExecution Feature
    Bug: 378838341
    Change-Id: Ia8dd4a6725946e47dad965f89f4f84c241f969c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039892
    Commit-Queue: Fiorella Barrientos Villalta <fiorellab@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386544}

--- components/optimization_guide/core/model_execution/model_execution_features.cc
+++ components/optimization_guide/core/model_execution/model_execution_features.cc
@@ -167,6 +167,7 @@ std::optional<proto::OptimizationTarget> GetOptimizationTargetForCapability(
     case ModelBasedCapabilityKey::kWallpaperSearch:
     case ModelBasedCapabilityKey::kTextSafety:
     case ModelBasedCapabilityKey::kBlingPrototyping:
+    case ModelBasedCapabilityKey::kPasswordChangeSubmission:
       return std::nullopt;
   }
 }
--- components/optimization_guide/core/model_execution/model_execution_util.cc
+++ components/optimization_guide/core/model_execution/model_execution_util.cc
@@ -55,6 +55,7 @@ void SetExecutionRequest(
     case ModelBasedCapabilityKey::kTextSafety:
     case ModelBasedCapabilityKey::kTest:
     case ModelBasedCapabilityKey::kBlingPrototyping:
+    case ModelBasedCapabilityKey::kPasswordChangeSubmission:
       // Do not log requests for these features.
       return;
   }
@@ -96,6 +97,7 @@ void SetExecutionResponse(ModelBasedCapabilityKey feature,
     case ModelBasedCapabilityKey::kTextSafety:
     case ModelBasedCapabilityKey::kTest:
     case ModelBasedCapabilityKey::kBlingPrototyping:
+    case ModelBasedCapabilityKey::kPasswordChangeSubmission:
       // Do not log responses for these features.
       return;
   }
