0f67a3251021c9f405b460756500c22f5e90ccdc
Mahesh Machavolu <mahesh.ma@samsung.com>
2024-11-21 13:33:00 +0000

    Rename methods in Direct writing module
    This CL modifies method names to satisfy Chromium guidelines
    and takes care of presubmit warnings.
    
    Signed-off-by: Mahesh Machavolu <mahesh.ma@samsung.com>
    Change-Id: Idd7ca148b6ac8bc86f9521bf7c4bf4a9692046dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6041097
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386194}

--- components/stylus_handwriting/android/java/src/org/chromium/components/stylus_handwriting/DirectWritingTrigger.java
+++ components/stylus_handwriting/android/java/src/org/chromium/components/stylus_handwriting/DirectWritingTrigger.java
@@ -31,7 +31,7 @@ import org.chromium.content_public.browser.WebContents;
  * {@link StylusWritingHandler} to receive messages about stylus writing events.
  */
 class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
-    private static final String TAG = "DWTrigger";
+    private static final String TAG = "DwTrigger";
 
     private DirectWritingServiceBinder mBinder = new DirectWritingServiceBinder();
     private DirectWritingServiceConfiguration mConfig = new DirectWritingServiceConfiguration();
@@ -72,7 +72,7 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
      */
     @Override
     public void onWebContentsChanged(Context context, WebContents webContents) {
-        updateDWSettings(context);
+        updateDwSettings(context);
         webContents.setStylusWritingHandler(this);
         mStylusWritingImeCallback = webContents.getStylusWritingImeCallback();
         mCallback.setImeCallback(mStylusWritingImeCallback);
@@ -117,7 +117,7 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
             }
         } else {
             // Stop recognition and hide DW toolbar as focused node is not editable.
-            hideDWToolbar();
+            hideDwToolbar();
             onStopRecognition(/* motionEvent= */ null, /* editableBounds= */ null, currentView);
         }
 
@@ -153,9 +153,9 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
         return false;
     }
 
-    private void updateDWServiceStatus(Context context) {
+    private void updateDwServiceStatus(Context context) {
         mDwServiceEnabled = isDirectWritingServiceEnabled(context);
-        Log.i(TAG, "updateDWServiceStatus() : isEnabled = " + mDwServiceEnabled);
+        Log.i(TAG, "updateDwServiceStatus() : isEnabled = " + mDwServiceEnabled);
     }
 
     /**
@@ -164,15 +164,15 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
      * @param context current context
      */
     @VisibleForTesting
-    void updateDWSettings(Context context) {
-        boolean wasDWEnabled = mDwServiceEnabled;
-        updateDWServiceStatus(context);
-        if (!wasDWEnabled && mDwServiceEnabled) {
-            onDWServiceEnabled();
+    void updateDwSettings(Context context) {
+        boolean wasDwEnabled = mDwServiceEnabled;
+        updateDwServiceStatus(context);
+        if (!wasDwEnabled && mDwServiceEnabled) {
+            onDwServiceEnabled();
         }
     }
 
-    private void onDWServiceEnabled() {
+    private void onDwServiceEnabled() {
         // Create IDirectWritingServiceCallbackImpl instance when DW setting is changed to
         // enabled. Platform Crash occurs if it is created when DW setting is not enabled.
         if (mCallback != null) return;
@@ -199,7 +199,7 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
     public void onFocusChanged(boolean hasFocus) {
         if (!hasFocus) {
             // Hide DW toolbar and Stop Recognition when View focus is lost.
-            hideDWToolbar();
+            hideDwToolbar();
             onStopRecognition(/* motionEvent= */ null, /* editableBounds= */ null);
         }
     }
@@ -207,9 +207,9 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
     @Override
     public void updateHandlerState(Context context, boolean hasWindowFocus) {
         if (hasWindowFocus) {
-            updateDWSettings(context);
+            updateDwSettings(context);
         } else {
-            hideDWToolbar();
+            hideDwToolbar();
         }
         if (!mDwServiceEnabled) return;
         mBinder.handleWindowFocusChanged(context, hasWindowFocus);
@@ -316,7 +316,7 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
         } else {
             // Hide the DW toolbar when stylus is not being used.
             if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-                hideDWToolbar();
+                hideDwToolbar();
             }
         }
         return false;
@@ -519,8 +519,8 @@ class DirectWritingTrigger implements StylusWritingHandler, StylusApiOption {
         mStylusUpEvent = null;
     }
 
-    private void hideDWToolbar() {
+    private void hideDwToolbar() {
         if (!mDwServiceEnabled) return;
-        mBinder.hideDWToolbar();
+        mBinder.hideDwToolbar();
     }
 }
