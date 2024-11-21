86e23af7db3f46a7e5cd02ad43acefd8a47d6723
Vaibhav Raheja <vraheja@google.com>
2024-11-21 17:18:49 +0000

    arc: Send ARCVM's serial number to Keymint
    We need to send ARCVM's serial number to KeyMint
    so that it could be used to be included
    in the ARC Attestation Certificate if requested
    by an Android app.
    
    This CL adds the code which gets the serial number
    from ARC Session Manager and then sends it
    to ARC KeyMint through Cert Store instance.
    
    ChromiumOS companion CL - https://crrev.com/c/5704981
    
    BUG=b:372711104
    TEST= Local testing. Serial number is visible in KeyMint.
    
    Change-Id: I954fcf14f86707c70bd90005c84538b98f497d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036948
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Vaibhav Raheja <vraheja@chromium.org>
    Reviewed-by: Yao Li <yaohuali@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386289}

--- chrome/browser/ash/arc/session/arc_session_manager.cc
+++ chrome/browser/ash/arc/session/arc_session_manager.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
@@ -794,6 +795,14 @@ void ArcSessionManager::SetUserInfo() {
 void ArcSessionManager::TrimVmMemory(TrimVmMemoryCallback callback,
                                      int page_limit) {
   arc_session_runner_->TrimVmMemory(std::move(callback), page_limit);
+}
+
+std::string ArcSessionManager::GetSerialNumberForKeyMint() {
+  DCHECK(arc::IsArcVmEnabled());
+  if (!arc_salt_on_disk_.has_value()) {
+    arc_salt_on_disk_ = ReadSaltOnDisk(base::FilePath(kArcSaltPath));
+  }
+  return GetSerialNumber();
 }
 
 std::string ArcSessionManager::GetSerialNumber() const {
