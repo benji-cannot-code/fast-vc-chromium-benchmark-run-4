8ddef43df065a01ae064dc326e7339f30e80fe0a
Steven Holte <holte@google.com>
2024-11-21 07:28:51 +0000

    Remove support for feature configs in base model.
    * Always get the feature config from AdaptationMetadata instead.
    * Adjusts tests to provide configs through AdaptationMetadata.
    * Standardize FakeBaseModelAsset
      * Provide data at construction like other Fake assets
      * Create a fake weights file, and use it in fake_service.
      * Changed how tests look for updated base model.
    * Avoid setting can_skip_text_safety in tests, and actually provide a
      safety config instead.
      * Defines StandardAssets and updates most test to use them.
      * Some tests expectations change due to this mostly around
        streaming responses / repeat detection.
      * Removed some assertions around these from tests that didn't care.
      * Continued to use UnsafeComposeConfig in tests focused on those.
    * Reorganized ModelExecutionSuccess tests slightly, since the
      adaptation case is not the special one anymore.
    * Updated some tests to use GetFinalStatus instead of RunUntilIdle.
    
    Bug: 376754799
    Change-Id: Ifbb4b5577b1237efce6925ece04eb37fe4a4224f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000181
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Auto-Submit: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386084}

--- services/on_device_model/public/cpp/test_support/fake_service.h
+++ services/on_device_model/public/cpp/test_support/fake_service.h
@@ -72,7 +72,6 @@ struct FakeOnDeviceServiceSettings final {
 class FakeOnDeviceSession final : public mojom::Session {
  public:
   explicit FakeOnDeviceSession(FakeOnDeviceServiceSettings* settings,
-                               const std::string& adaptation_model_weight,
                                FakeOnDeviceModel* model);
   ~FakeOnDeviceSession() override;
 
@@ -112,6 +111,7 @@ class FakeOnDeviceSession final : public mojom::Session {
 class FakeOnDeviceModel : public mojom::OnDeviceModel {
  public:
   struct Data {
+    std::string base_weight = "";
     std::string adaptation_model_weight = "";
   };
   explicit FakeOnDeviceModel(FakeOnDeviceServiceSettings* settings,
@@ -134,6 +134,8 @@ class FakeOnDeviceModel : public mojom::OnDeviceModel {
   void AddSession(
       mojo::PendingReceiver<on_device_model::mojom::Session> receiver,
       std::unique_ptr<FakeOnDeviceSession> session);
+
+  const Data& data() const { return data_; }
 
  private:
   raw_ptr<FakeOnDeviceServiceSettings> settings_;
