12741d3aa78a39b10bb991efece078103771849d
Aaron Leventhal <aleventhal@google.com>
2024-11-21 23:38:54 +0000

    [A11y] Update global attribute list used for role=presentation exclusion
    The list of global attributes that undoes role=none/presentation was
    outdated. It was missing aria-braillelabel, and aria-grabbed was
    deprecated long ago.
    
    Also updated test to improve source/expectation line correlation
    by adding a class name to each line.
    
    Bug: none
    Change-Id: Idd6cef43882b8f80794b9135fb0de1f257b7351f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039004
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386537}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -5477,6 +5477,7 @@ bool DoesUndoRolePresentation(const AtomicString& name) {
       HashSet<AtomicString>, aria_global_properties,
       ({
         AtomicString("ARIA-ATOMIC"),
+        AtomicString("ARIA-BRAILLELABEL"),
         AtomicString("ARIA-BRAILLEROLEDESCRIPTION"),
         AtomicString("ARIA-BUSY"),
         AtomicString("ARIA-CONTROLS"),
@@ -5485,7 +5486,6 @@ bool DoesUndoRolePresentation(const AtomicString& name) {
         AtomicString("ARIA-DESCRIPTION"),
         AtomicString("ARIA-DETAILS"),
         AtomicString("ARIA-FLOWTO"),
-        AtomicString("ARIA-GRABBED"),
         AtomicString("ARIA-KEYSHORTCUTS"),
         AtomicString("ARIA-LABEL"),
         AtomicString("ARIA-LABELEDBY"),
