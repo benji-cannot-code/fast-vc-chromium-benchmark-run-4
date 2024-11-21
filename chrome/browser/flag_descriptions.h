808697385ed76d758e5e3fbcec1b06ca681f244e
Ho Cheung <hocheung@chromium.org>
2024-11-21 08:25:18 +0000

    [history_clusters] Remove history-journeys-show-all-clusters WebUI flag
    But functional code paths are preserved.
    
    Fixed: 330368471
    Change-Id: I38a0f2e09a84346d1fc98cffce136647176661e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040752
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386098}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1351,9 +1351,6 @@ extern const char kJavascriptHarmonyShippingDescription[];
 extern const char kJourneysName[];
 extern const char kJourneysDescription[];
 
-extern const char kJourneysShowAllClustersName[];
-extern const char kJourneysShowAllClustersDescription[];
-
 extern const char kJumpStartOmniboxName[];
 extern const char kJumpStartOmniboxDescription[];
 
