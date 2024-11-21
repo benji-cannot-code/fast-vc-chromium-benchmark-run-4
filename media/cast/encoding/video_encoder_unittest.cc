bea417a42e1ec3f888401392957f5cc61e253473
Jordan Bayles <jophba@chromium.org>
2024-11-21 00:19:04 +0000

    Reland "[Cast Streaming] Use is_key_frame instead of dependency type."
    This reverts commit d7899d7e0a627f9ca68c40fbdf9fc662e18c30db.
    
    Reason for revert: fixed unitialized value.
    
    Original change's description:
    > Revert "[Cast Streaming] Use is_key_frame instead of dependency type."
    >
    > This reverts commit cec749f1338152dfd98bd70944c10c6a806e31ad.
    >
    > Reason for revert: Failure https://crbug.com/379758206 is consistent,
    > tentatively reverting the change so that the author can take time to
    > fix the issue.
    >
    > Original change's description:
    > > [Cast Streaming] Use is_key_frame instead of dependency type.
    > >
    > > This patch just removes the Dependency enum in favor of
    > > using a boolean argument, aligning the legacy API with the new media
    > > VideoEncoder API. The unused encoder_bitrate is also removed.
    > >
    > > Googlers may view the related design doc at:
    > > http://go/cast-streaming-media-videoencoder
    > >
    > > Publicly published design documentation available at:
    > > https://tinyurl.com/cast-streaming-mvew
    > >
    > > Bug: 282984511
    > > Change-Id: I2fb22b672f68d145d10bc9e091707151ae9c4016
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020961
    > > Commit-Queue: Jordan Bayles <jophba@chromium.org>
    > > Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1384570}
    >
    > Bug: 282984511
    > Change-Id: I63c0557f1b75bc21b977f41e8e09ebf9c0971fcd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6034479
    > Owners-Override: Mikihito Matsuura <mikt@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Mikihito Matsuura <mikt@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1385458}
    
    Bug: 282984511
    Change-Id: Ie089fc3669f7a38949f79f49a474424c0e2ea4e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039279
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385974}

--- media/cast/encoding/video_encoder_unittest.cc
+++ media/cast/encoding/video_encoder_unittest.cc
@@ -65,8 +65,6 @@ class VideoEncoderTest
         std::vector<base::test::FeatureRef>{});
   }
 
-  ~VideoEncoderTest() override = default;
-
   void SetUp() final {
     VideoCodec codec = GetParam().first;
     codec_params_->codec = codec;
@@ -82,7 +80,7 @@ class VideoEncoderTest
     }
   }
 
-  void TearDown() final {
+  ~VideoEncoderTest() override {
     video_encoder_.reset();
     RunTasksAndAdvanceClock();
   }
@@ -276,13 +274,10 @@ TEST_P(VideoEncoderTest, EncodesVariedFrameSizes) {
     ASSERT_NE(expectation, expectations.end());
     EXPECT_EQ(expectation->second, encoded_frame->rtp_timestamp);
 
-    if (encoded_frame->dependency ==
-        openscreen::cast::EncodedFrame::Dependency::kKeyFrame) {
+    if (encoded_frame->is_key_frame) {
       EXPECT_EQ(encoded_frame->frame_id, encoded_frame->referenced_frame_id);
       last_key_frame_id = encoded_frame->frame_id;
     } else {
-      EXPECT_EQ(openscreen::cast::EncodedFrame::Dependency::kDependent,
-                encoded_frame->dependency);
       EXPECT_GT(encoded_frame->frame_id, encoded_frame->referenced_frame_id);
       // There must always be a KEY frame before any DEPENDENT ones.
       ASSERT_FALSE(last_key_frame_id.is_null());
@@ -296,7 +291,6 @@ TEST_P(VideoEncoderTest, EncodesVariedFrameSizes) {
     if (is_testing_software_vp8_encoder()) {
       ASSERT_TRUE(std::isfinite(encoded_frame->encoder_utilization));
       EXPECT_LE(0.0, encoded_frame->encoder_utilization);
-      EXPECT_LE(0, encoded_frame->encoder_bitrate);
       ASSERT_TRUE(std::isfinite(encoded_frame->lossiness));
       EXPECT_LE(0.0, encoded_frame->lossiness);
     }
