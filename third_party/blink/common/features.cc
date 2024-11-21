4dd3862e536497e7ba9d8604226a9792ba8701f1
Ashish Kumar <ashishkum@microsoft.com>
2024-11-21 10:14:17 +0000

    Update navigation logic to open all files dropped in web content
    This CL updates the navigation logic for dropping URLs or files in
    the web content. Currently, when multiple files are dropped in web
    content, only one of them opens in a new tab. After this change, all
    files will be opened in new tabs when multiple files are dropped.
    The first tab out of the newly opened tabs will be in focus, which
    aligns with the current behavior of dropping multiple files in the
    browser tab strip.
    
    Bug: 40927333
    Change-Id: Ib250ac35929cc8e074769afbb06cf9d7426d889c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920063
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ashish Kumar <ashishkum@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1386142}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1917,6 +1917,10 @@ BASE_FEATURE(kNoThrottlingVisibleAgent,
              "NoThrottlingVisibleAgent",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kOpenAllUrlsOrFilesOnDrop,
+             "OpenAllUrlsOrFilesOnDrop",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 BASE_FEATURE(kOptimizeLoadingDataUrls,
              "OptimizeLoadingDataUrls",
              base::FEATURE_ENABLED_BY_DEFAULT);
