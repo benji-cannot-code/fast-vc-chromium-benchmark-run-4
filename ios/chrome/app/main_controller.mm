e2f49f74fdf6a1605d30731bec808665568eb278
Sylvain Defresne <sdefresne@chromium.org>
2024-11-21 07:15:13 +0000

    [ios] Move RLZ library init to MainController
    The RLZ library should be initialised with the first profile that
    is fully loaded so move the code performing the initialisation to
    MainController when the first ProfileState reach kProfileLoaded.
    
    Bug: 325257407
    Change-Id: I9a2a49d0166d2697652fb945351c5b420f864403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038773
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386082}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -139,6 +139,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/public/provider/chrome/browser/user_feedback/user_feedback_api.h"
 #import "ios/web/public/webui/web_ui_ios_controller_factory.h"
 #import "net/base/apple/url_conversions.h"
+#import "rlz/buildflags/buildflags.h"
 #import "services/network/public/cpp/shared_url_loader_factory.h"
 #import "ui/base/device_form_factor.h"
 
@@ -148,7 +149,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IOS_ENABLE_SANDBOX_DUMP)
 #import "ios/chrome/app/dump_documents_statistics.h"
-#endif  // BUILDFLAG(IOS_ENABLE_SANDBOX_DUMP)
+#endif
+
+#if BUILDFLAG(ENABLE_RLZ)
+#import "components/rlz/rlz_tracker.h"                        // nogncheck
+#import "ios/chrome/browser/rlz/rlz_tracker_delegate_impl.h"  // nogncheck
+#endif
 
 namespace {
 
@@ -946,7 +952,13 @@ void BeginMemoryExperimentationAfterDelay() {
 
       case ProfileInitStage::kLoadProfile:
       case ProfileInitStage::kMigrateStorage:
+        // Nothing to do.
+        break;
+
       case ProfileInitStage::kProfileLoaded:
+        [self scheduleRLZInitWithProfile:profileState.profile];
+        break;
+
       case ProfileInitStage::kPrepareUI:
       case ProfileInitStage::kUIReady:
       case ProfileInitStage::kFirstRun:
@@ -1131,6 +1143,10 @@ void BeginMemoryExperimentationAfterDelay() {
     return;
   }
 #endif  // BUILDFLAG(FAST_APP_TERMINATE_ENABLED)
+
+#if BUILDFLAG(ENABLE_RLZ)
+  rlz::RLZTracker::CleanupRlz();
+#endif
 
   _chromeMain.reset();
 }
@@ -1505,6 +1521,26 @@ void BeginMemoryExperimentationAfterDelay() {
   ios::provider::ScheduleAppDistributionNotifications(URLLoaderFactory,
                                                       isFirstRun);
   ios::provider::InitializeFirebase(installDate, isFirstRun);
+}
+
+// Schedules the initialization of the RLZ library with a give profile. This
+// method must only be called once. If this is the first run of the app, it
+// will record the installation event.
+- (void)scheduleRLZInitWithProfile:(ProfileIOS*)profile {
+#if BUILDFLAG(ENABLE_RLZ)
+  DCHECK(profile);
+  PrefService* prefs = profile->GetPrefs();
+
+  // Negative delay means to send ping immediately after first recorded search.
+  const int pingDelay = prefs->GetInteger(FirstRun::GetPingDelayPrefName());
+  rlz::RLZTracker::SetRlzDelegate(std::make_unique<RLZTrackerDelegateImpl>());
+  rlz::RLZTracker::InitRlzDelayed(
+      FirstRun::IsChromeFirstRun(), pingDelay < 0,
+      base::Milliseconds(abs(pingDelay)),
+      RLZTrackerDelegateImpl::IsGoogleDefaultSearch(profile),
+      RLZTrackerDelegateImpl::IsGoogleHomepage(profile),
+      RLZTrackerDelegateImpl::IsGoogleInStartpages(profile));
+#endif
 }
 
 // Records launch metrics when the application and all initial profiles have
