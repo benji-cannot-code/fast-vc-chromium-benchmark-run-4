38894ef6bf89a1ae5d31857305ea743fc5acef34
Maks Orlovich <morlovich@chromium.org>
2024-11-21 01:35:39 +0000

    Cleanup some unsafe_buffers from net/disk_cache/simple
    This is mostly tests and some interfacing with Pickle & File in
    SimpleIndexFile... and also using HeapArray rather than discouraged
    unique_ptr<T[]>
    
    I am not sure what to do with more of impl --- the "stream index is 0...2" invariant isn't very visible in the type system... though even the array in simple_file_tracker where it's indexed by going from a class enum to int isn't clear how to deal with, either.
    
    Bug: 40285824
    
    Change-Id: Iccd181bc09c05c7926289486297d9fb098897d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032800
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386006}

--- net/disk_cache/simple/simple_file_tracker_unittest.cc
+++ net/disk_cache/simple/simple_file_tracker_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <memory>
 #include <string>
 #include <string_view>
@@ -106,10 +101,8 @@ TEST_F(SimpleFileTrackerTest, Basic) {
     SimpleFileTracker::FileHandle borrow_1 = file_tracker_.Acquire(
         &ops, entry.get(), SimpleFileTracker::SubFile::FILE_1);
 
-    EXPECT_EQ(static_cast<int>(msg_0.size()),
-              borrow_0->Write(0, msg_0.data(), msg_0.size()));
-    EXPECT_EQ(static_cast<int>(msg_1.size()),
-              borrow_1->Write(0, msg_1.data(), msg_1.size()));
+    EXPECT_EQ(msg_0.size(), borrow_0->Write(0, base::as_byte_span(msg_0)));
+    EXPECT_EQ(msg_1.size(), borrow_1->Write(0, base::as_byte_span(msg_1)));
 
     // For stream 0 do release/close, for stream 1 do close/release --- where
     // release happens when borrow_{0,1} go out of scope
@@ -156,10 +149,8 @@ TEST_F(SimpleFileTrackerTest, Collision) {
     SimpleFileTracker::FileHandle borrow2 = file_tracker_.Acquire(
         &ops, entry2.get(), SimpleFileTracker::SubFile::FILE_0);
 
-    EXPECT_EQ(static_cast<int>(msg.size()),
-              borrow->Write(0, msg.data(), msg.size()));
-    EXPECT_EQ(static_cast<int>(msg2.size()),
-              borrow2->Write(0, msg2.data(), msg2.size()));
+    EXPECT_EQ(msg.size(), borrow->Write(0, base::as_byte_span(msg)));
+    EXPECT_EQ(msg2.size(), borrow2->Write(0, base::as_byte_span(msg2)));
   }
   file_tracker_.Close(entry.get(), SimpleFileTracker::SubFile::FILE_0);
   file_tracker_.Close(entry2.get(), SimpleFileTracker::SubFile::FILE_0);
@@ -208,7 +199,7 @@ TEST_F(SimpleFileTrackerTest, PointerStability) {
   // Make sure the FileHandle lent out doesn't get screwed up as we update
   // the state (and potentially move the underlying base::File object around).
   const int kEntries = 8;
-  SyncEntryPointer entries[kEntries] = {
+  std::array<SyncEntryPointer, kEntries> entries = {
       MakeSyncEntry(1), MakeSyncEntry(1), MakeSyncEntry(1), MakeSyncEntry(1),
       MakeSyncEntry(1), MakeSyncEntry(1), MakeSyncEntry(1), MakeSyncEntry(1),
   };
@@ -234,8 +225,7 @@ TEST_F(SimpleFileTrackerTest, PointerStability) {
                              std::move(file_n));
     }
 
-    EXPECT_EQ(static_cast<int>(msg.size()),
-              borrow->Write(0, msg.data(), msg.size()));
+    EXPECT_EQ(msg.size(), borrow->Write(0, base::as_byte_span(msg)));
   }
 
   for (const auto& entry : entries)
@@ -312,7 +302,7 @@ TEST_F(SimpleFileTrackerTest, OverLimit) {
   // still open, so no change in stats after.
   SimpleFileTracker::FileHandle borrow_last = file_tracker_.Acquire(
       &ops, entries[kEntries - 1].get(), SimpleFileTracker::SubFile::FILE_0);
-  EXPECT_EQ(1, borrow_last->Write(0, "L", 1));
+  EXPECT_EQ(1u, borrow_last->Write(0, base::byte_span_from_cstring("L")));
 
   histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
                                      disk_cache::FD_LIMIT_CLOSE_FILE,
@@ -332,7 +322,7 @@ TEST_F(SimpleFileTrackerTest, OverLimit) {
     if (i != 2) {
       EXPECT_TRUE(borrow.IsOK());
       char c = static_cast<char>(i);
-      EXPECT_EQ(1, borrow->Write(0, &c, 1));
+      EXPECT_EQ(1u, borrow->Write(0, base::byte_span_from_ref(c)));
     } else {
       EXPECT_FALSE(borrow.IsOK());
     }
@@ -365,7 +355,7 @@ TEST_F(SimpleFileTrackerTest, OverLimit) {
     char expected = static_cast<char>(i);
     if (i != 2) {
       EXPECT_TRUE(borrow.IsOK());
-      EXPECT_EQ(1, borrow->Read(0, &read, 1));
+      EXPECT_EQ(1, borrow->Read(0, base::byte_span_from_ref(read)));
       EXPECT_EQ(expected, read);
     } else {
       EXPECT_FALSE(borrow.IsOK());
@@ -384,7 +374,7 @@ TEST_F(SimpleFileTrackerTest, OverLimit) {
 
   // Read from the last one, too. Should still be fine.
   char read;
-  EXPECT_EQ(1, borrow_last->Read(0, &read, 1));
+  EXPECT_EQ(1, borrow_last->Read(0, base::byte_span_from_ref(read)));
   EXPECT_EQ('L', read);
 
   for (const auto& entry : entries)
--- net/disk_cache/simple/simple_test_util.h
+++ net/disk_cache/simple/simple_test_util.h
@@ -3,16 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef NET_DISK_CACHE_SIMPLE_SIMPLE_TEST_UTIL_H_
 #define NET_DISK_CACHE_SIMPLE_SIMPLE_TEST_UTIL_H_
 
 #include <stddef.h>
 
+#include <array>
 #include <string>
 
 #include "base/functional/callback.h"
@@ -42,7 +38,7 @@ class ImmutableArray {
   }
 
  private:
-  T data_[size];
+  std::array<T, size> data_;
 };
 
 // Creates a corrupt file to be used in tests.
