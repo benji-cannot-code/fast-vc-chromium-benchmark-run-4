7567f6a90295ba386ab890b5d7d6a5781d16aa4d
Florian Leimgruber <fleimgruber@google.com>
2024-11-21 16:01:38 +0000

    Remove AddressDataManager::AddChangeCallback()
    The function is unused. (It recently became unused due to the Mullet
    removal)
    
    Bug: 40100455
    Change-Id: I995ad81b3531aef5b04eb1bcd9970232816a2265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039237
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1386255}

--- components/autofill/core/browser/address_data_manager.cc
+++ components/autofill/core/browser/address_data_manager.cc
@@ -144,10 +144,6 @@ void AddressDataManager::RemoveObserver(AddressDataManager::Observer* obs) {
   observers_.RemoveObserver(obs);
 }
 
-void AddressDataManager::AddChangeCallback(base::OnceClosure callback) {
-  change_callbacks_.push_back(std::move(callback));
-}
-
 void AddressDataManager::OnAutofillChangedBySync(syncer::DataType data_type) {
   if (data_type == syncer::DataType::AUTOFILL_PROFILE ||
       data_type == syncer::DataType::CONTACT_INFO) {
@@ -581,14 +577,11 @@ AddressDataManager::GetAddressSuggestionStrikeDatabase() const {
 }
 
 void AddressDataManager::NotifyObservers() {
-  if (!IsAwaitingPendingAddressChanges()) {
-    for (Observer& o : observers_) {
-      o.OnAddressDataChanged();
-    }
-    for (base::OnceClosure& callback : change_callbacks_) {
-      std::move(callback).Run();
-    }
-    change_callbacks_.clear();
+  if (IsAwaitingPendingAddressChanges()) {
+    return;
+  }
+  for (Observer& o : observers_) {
+    o.OnAddressDataChanged();
   }
 }
 
--- components/autofill/core/browser/address_data_manager_unittest.cc
+++ components/autofill/core/browser/address_data_manager_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/gmock_callback_support.h"
-#include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/test/with_feature_override.h"
@@ -1188,13 +1187,6 @@ TEST_F(AddressDataManagerTest, AutofillSyncToggleAvailableInTransportMode) {
   EXPECT_FALSE(address_data_manager().IsAutofillSyncToggleAvailable());
 }
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-
-TEST_F(AddressDataManagerTest, ChangeCallbackIsTriggeredOnAddedProfile) {
-  ::testing::StrictMock<base::MockOnceClosure> callback;
-  EXPECT_CALL(callback, Run);
-  address_data_manager().AddChangeCallback(callback.Get());
-  AddProfileToAddressDataManager(test::GetFullProfile());
-}
 
 }  // namespace
 
