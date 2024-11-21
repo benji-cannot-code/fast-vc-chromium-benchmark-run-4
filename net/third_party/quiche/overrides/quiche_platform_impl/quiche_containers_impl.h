4eb138b2ae19b81e6b52078fd77fa556b6f38749
David Benjamin <davidben@chromium.org>
2024-11-21 19:57:37 +0000

    Remove QuicheInlinedVectorImpl from quiche_containers_impl.h
    QUICHE no longer uses this.
    
    Change-Id: I1116e169eff339bd2ce594d0078274f953d2c5cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038798
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386401}

--- net/third_party/quiche/overrides/quiche_platform_impl/quiche_containers_impl.h
+++ net/third_party/quiche/overrides/quiche_platform_impl/quiche_containers_impl.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace quiche {
 
-// TODO(wub): Switch to absl::InlinedVector once it is allowed.
-template <typename T, size_t N, typename A = std::allocator<T>>
-using QuicheInlinedVectorImpl = std::vector<T, A>;
-
 template <typename Key, typename Compare>
 using QuicheSmallOrderedSetImpl = base::flat_set<Key, Compare>;
 
