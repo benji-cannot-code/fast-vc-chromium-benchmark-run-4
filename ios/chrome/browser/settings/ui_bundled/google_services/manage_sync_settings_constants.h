c9f30f753054b379c95c70a5591b1fb9be2ceb1c
Christian Xu <christianxu@google.com>
2024-11-21 11:32:00 +0000

    [CodeHealth][iOS] Prepare ui/settings move to settings/ui_bundled
    Bug: 324233364
    Change-Id: Idef9a146edf89a151bb589994b3862d7576f271a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038115
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386164}

---
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_constants.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_MANAGE_SYNC_SETTINGS_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_MANAGE_SYNC_SETTINGS_CONSTANTS_H_
+
+#import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_MANAGE_SYNC_SETTINGS_CONSTANTS_H_
