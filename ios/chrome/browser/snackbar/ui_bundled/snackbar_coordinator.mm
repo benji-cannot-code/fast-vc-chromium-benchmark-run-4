44c55e4f269a9cb8c45338471cc58d51d2a4dd49
Maksim Moskvitin <mmoskvitin@google.com>
2024-11-21 13:53:44 +0000

    [Code Health][iOS] Move ios/c/b/ui/snackbar to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 40223315, 324233268
    Change-Id: If6f917c0be799a3270b7ecd7251bd3c2840213ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038259
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1386205}

--- ios/chrome/browser/snackbar/ui_bundled/snackbar_coordinator.mm
+++ ios/chrome/browser/snackbar/ui_bundled/snackbar_coordinator.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/snackbar/snackbar_coordinator.h"
+#import "ios/chrome/browser/snackbar/ui_bundled/snackbar_coordinator.h"
 
 #import <MaterialComponents/MaterialSnackbar.h>
 
