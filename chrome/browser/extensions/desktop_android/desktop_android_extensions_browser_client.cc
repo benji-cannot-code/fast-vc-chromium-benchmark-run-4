7167781adbb99810307603cf57acda9d8e5143ef
Jenny Zhang <jennyz@chromium.org>
2024-11-22 00:51:05 +0000

    Add OTR profile support for DAExtensionsBrowserClient.
    In `DesktopAndroidExtensionsBrowserClient`, ensure `GetContextRedirectedToOriginal()` returns the original
    browser context for Off-The-Record (OTR) contexts. This
    fixes the regression causing
    `CookiesFetcherRestoreUtilBrowserTest` failure.
    
    Bug: 380314905
    Change-Id: I2f108d64f310b050fa6593881beacb6e06576cd4
    Cq-Include-Trybots: luci.chromium.try:android-desktop-arm64-compile-rel,android-desktop-x64-compile-rel,android-desktop-14-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039478
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386567}

--- chrome/browser/extensions/desktop_android/desktop_android_extensions_browser_client.cc
+++ chrome/browser/extensions/desktop_android/desktop_android_extensions_browser_client.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/desktop_android/desktop_android_runtime_api_delegate.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/profiles/profile_selections.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -111,7 +112,10 @@ BrowserContext* DesktopAndroidExtensionsBrowserClient::GetOriginalContext(
 content::BrowserContext*
 DesktopAndroidExtensionsBrowserClient::GetContextRedirectedToOriginal(
     content::BrowserContext* context) {
-  return context;
+  return ProfileSelections::Builder()
+      .WithRegular(ProfileSelection::kRedirectedToOriginal)
+      .Build()
+      .ApplyProfileSelection(Profile::FromBrowserContext(context));
 }
 
 content::BrowserContext*
