98c42df06436fd484588ba0f30b1ff317164673b
Ian Struiksma <ianstruiksma@google.com>
2024-11-22 10:39:07 +0000

    [Captured Sites] Always set phone number via PHONE_HOME_WHOLE_NUMBER.
    Other fields in PHONE group are not allowed to be set. In case a site has a different PHONE stored in the json profile, switch to always set via PHONE_HOME_WHOLE_NUMBER.
    
    Change-Id: I56788cfd00530ad64a42abac00d29f720d1c567c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040947
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ian Struiksma <ianstruiksma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1386705}

--- chrome/browser/autofill/captured_sites_test_utils.cc
+++ chrome/browser/autofill/captured_sites_test_utils.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/autofill_type.h"
+#include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/test_autofill_clock.h"
@@ -863,6 +864,13 @@ bool ProfileDataController::AddAutofillProfileInfo(
     ADD_FAILURE() << "Unable to recognize autofill field type '" << field_type
                   << "'!";
     return false;
+  }
+
+  // PhoneNumber only allows PHONE_HOME_WHOLE_NUMBER to be set directly, so if
+  // any other phone type is set, convert it to PHONE_HOME_WHOLE_NUMBER.
+  if (GroupTypeOfFieldType(type.value()) == autofill::FieldTypeGroup::kPhone &&
+      type.value() != autofill::FieldType::PHONE_HOME_WHOLE_NUMBER) {
+    type = autofill::FieldType::PHONE_HOME_WHOLE_NUMBER;
   }
 
   if (base::StartsWith(field_type, "HTML_TYPE_CREDIT_CARD_",
