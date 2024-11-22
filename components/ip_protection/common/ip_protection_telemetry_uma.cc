954888e630dfe63a11dc9db72a39ccb3ad2ae265
Dustin J. Mitchell <djmitche@chromium.org>
2024-11-22 21:25:32 +0000

    Refactor an IP Protection histogram that wasn't included in telemetry
    This was added at about the same time all of the other histograms were
    moving to IpProtectionTelemetry, so it didn't get moved.
    
    Change-Id: I881d10676b5ed99a867dfadf341c1c69140d836e
    Bug: 330844356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037575
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387057}

--- components/ip_protection/common/ip_protection_telemetry_uma.cc
+++ components/ip_protection/common/ip_protection_telemetry_uma.cc
@@ -194,6 +194,11 @@ void IpProtectionTelemetryUma::TokenBatchGenerationComplete(
       "NetworkService.IpProtection.TokenBatchGenerationTime", duration);
 }
 
+void IpProtectionTelemetryUma::TryGetAuthTokensError(uint32_t hash) {
+  base::UmaHistogramSparse("NetworkService.IpProtection.TryGetAuthTokensErrors",
+                           hash);
+}
+
 void IpProtectionTelemetryUma::GeoChangeTokenPresence(bool tokens_present) {
   base::UmaHistogramBoolean(
       "NetworkService.IpProtection.GeoChangeTokenPresence", tokens_present);
