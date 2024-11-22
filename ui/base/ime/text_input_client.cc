404e42f2f9049b6b8d9d6122f89dc4c7490e44ff
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2024-11-22 20:19:47 +0000

    Implement GetProximateCharacter *Bounds and *IndexFromPoint
    Implementing the RenderWidgetHostViewAura version of
    GetProximateCharacterBounds and GetProximateCharacterIndexFromPoint
    when pulling from the "proximate" character bounds cache.
    
    GetProximateCharacterBounds is meant to satisfy requirements
    outlined for ITextStoreACP::GetTextExt.
    
    If all character bounding boxes in the range [`acp_start`, `acp_end`)
    are available, combines them into one bounding box that encapsulates
    the entire range, otherwise returns `std::nullopt` which the caller
    handles by throwing `TS_E_NOLAYOUT` since it's unclear whether the
    provided range went out-of-bounds for the actual text range or only
    the cached "proximate" range.
    
    Chromium's `GetTextExt` implementation currently doesn't support the
    OUT parameter `clipped`, and so far doesn't seem to be necessary for
    StylusHandwritingWin. This can be added in a separate CL if any issues
    come up related to that parameter.
    
    GetProximateCharacterIndexFromPoint is meant to satisfy requirements
    outlined for ITextStoreACP::GetACPFromPoint.
    
    Depending on which `ui::IndexFromPointFlags` is provided, finds an
    appropriate character offset from the "proximate" character bounds
    cache, or returns `std::nullopt` if one cannot be determined which the
    caller handles by throwing `TS_E_INVALIDPOINT`.
    
    Bug: 355578906
    Change-Id: Ia4c81dcd0be7676c5639da2a8d39c1c113cc7b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871295
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1387028}

--- ui/base/ime/text_input_client.cc
+++ ui/base/ime/text_input_client.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ime/text_input_client.h"
 
+#include <iomanip>
+#include <ios>
+#include <ostream>
 #include <string_view>
 
 namespace ui {
@@ -50,5 +53,31 @@ ui::TextInputClient::EditingContext TextInputClient::GetTextEditingContext() {
   return {};
 }
 #endif
+
+#if BUILDFLAG(IS_WIN)
+std::ostream& operator<<(std::ostream& os, ui::IndexFromPointFlags flags) {
+  switch (flags) {
+    case ui::IndexFromPointFlags::kNone:
+      os << "None";
+      break;
+    case ui::IndexFromPointFlags::kNearestToContainedPoint:
+      os << "NearestToContainedPoint";
+      break;
+    case ui::IndexFromPointFlags::kNearestToUncontainedPoint:
+      os << "NearestToUncontainedPoint";
+      break;
+    case ui::IndexFromPointFlags::kNearestToPoint:
+      os << "NearestToPoint";
+      break;
+    default:
+      using T = std::underlying_type_t<ui::IndexFromPointFlags>;
+      static_assert(sizeof(T) <= sizeof(uint32_t));
+      os << "Unknown(0x" << std::setfill('0') << std::setw(sizeof(T) * 2)
+         << std::hex << static_cast<uint32_t>(flags) << ")";
+      break;
+  }
+  return os;
+}
+#endif  // BUILDFLAG(IS_WIN)
 
 }  // namespace ui
