74684c98dfcff60454203c9fba2be2671b80b98c
Juan Mojica <juanmojica@google.com>
2024-11-22 00:41:07 +0000

    [companion] Remove some leftover companion code.
    Seems these files were able to stay in the codebase despite my removal
    CLs. These files were already removed from BUILD files and should
    just be dead code.
    
    Change-Id: I33ad02b75a7451c390d84f8ebbdca2c0eb2c74cd
    Bug: b:348678854
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039496
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386563}

--- chrome/browser/ui/views/lens/lens_side_panel_helper.cc
+++ chrome/browser/ui/views/lens/lens_side_panel_helper.cc
@@ -21,11 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace lens {
 
-bool ShouldPageBeVisible(const GURL& url) {
-  return lens::IsValidLensResultUrl(url) || !lens::IsLensUrl(url) ||
-         !lens::features::GetEnableLensHtmlRedirectFix();
-}
-
 views::Widget* OpenLensRegionSearchInstructions(
     Browser* browser,
     base::OnceClosure close_callback,
