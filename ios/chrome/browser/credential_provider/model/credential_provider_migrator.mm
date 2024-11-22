cf5a1cff69791e6635d732f61f2fd0404d2cd69d
Alexis Hetu <sugoi@chromium.org>
2024-11-22 22:42:52 +0000

    [iOS] Add passkey migration metrics
    This CL adds metrics for passkeys being created and updated from
    the CPE.
    
    Bug: 377499026
    Change-Id: I9937e8e85bf470b97def25a83bbddd8d8619ae88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038668
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387100}

--- ios/chrome/browser/credential_provider/model/credential_provider_migrator.mm
+++ ios/chrome/browser/credential_provider/model/credential_provider_migrator.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/credential_provider/model/credential_provider_migrator.h"
 
+#import "base/metrics/histogram_functions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/password_manager/core/browser/password_form.h"
 #import "components/password_manager/core/browser/password_store/password_store_interface.h"
@@ -22,6 +23,20 @@ NSErrorDomain const kCredentialProviderMigratorErrorDomain =
 typedef enum : NSInteger {
   CredentialProviderMigratorErrorAlreadyRunning,
 } CredentialProviderMigratorErrors;
+
+// Name of the passkey migration related histogram.
+static constexpr char kPasskeysIOSMigration[] = "Passkeys.IOSMigration";
+
+// Values of the UMA Passkeys.IOSMigration histogram. These values are persisted
+// to logs. Entries should not be renumbered and numeric values should never be
+// reused.
+enum class PasskeysMigration {
+  // New passkey from the CPE migrated to Chrome.
+  kPasskeyCreated = 0,
+  // Existing passkey used by the CPE updated in Chrome.
+  kPasskeyUpdated = 1,
+  kMaxValue = kPasskeyUpdated
+};
 
 @interface CredentialProviderMigrator () {
   // Passkey store.
@@ -110,11 +125,15 @@ typedef enum : NSInteger {
           _passkeyStore->UpdatePasskeyTimestamp(
               credentialId, base::Time::FromDeltaSinceWindowsEpoch(
                                 base::Microseconds(credential.lastUsedTime)));
+          base::UmaHistogramEnumeration(kPasskeysIOSMigration,
+                                        PasskeysMigration::kPasskeyUpdated);
         }
       } else {
         sync_pb::WebauthnCredentialSpecifics passkey =
             PasskeyFromCredential(credential);
         _passkeyStore->CreatePasskey(passkey);
+        base::UmaHistogramEnumeration(kPasskeysIOSMigration,
+                                      PasskeysMigration::kPasskeyCreated);
       }
     } else {
       password_manager::PasswordForm form =
