ad3bf96eb822bdb8aeee202ba2249228c4acb008
Rob Schonberger <robsc@chromium.org>
2024-11-22 02:29:54 +0000

    soda: Add timer for retry to SODA.
    Add a multiplicative backoff timer to SODA installation, so that any
    error attempts a reinstall. The backoff timer defaults to a max delay
    of 32 seconds.
    
    Ideally, DLC handles intermittent errors internally, but we do this to
    avoid cases where DLC may not yet be available.
    
    
    Bug: b:369008001,b:372340605
    Change-Id: I002764ee60feb751f9b3d500b8b1b87b4ce67ca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889071
    Reviewed-by: Andrew Moylan <amoylan@google.com>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1386601}

--- components/soda/soda_installer_impl_chromeos.h
+++ components/soda/soda_installer_impl_chromeos.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
+#include "base/timer/timer.h"
 #include "chromeos/ash/components/dbus/dlcservice/dlcservice_client.h"
 #include "components/soda/soda_installer.h"
 
@@ -69,6 +70,7 @@ class COMPONENT_EXPORT(SODA_INSTALLER) SodaInstallerImplChromeOS
   void OnSodaInstalled(
       const base::Time start_time,
       const ash::DlcserviceClient::InstallResult& install_result);
+  void OnSodaInstallRetry();
   void OnLanguageInstalled(
       const LanguageCode language_code,
       const std::string language_name,
@@ -85,6 +87,10 @@ class COMPONENT_EXPORT(SODA_INSTALLER) SodaInstallerImplChromeOS
   void OnDlcUninstalled(std::string_view dlc_id, std::string_view err);
 
   double soda_progress_ = 0.0;
+
+  double soda_backoff_seconds_ = 1.0;
+  // timer only used for soda install retries.
+  base::OneShotTimer soda_install_retry_timer_;
 
   base::FilePath soda_lib_path_;
 
