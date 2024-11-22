2a18b4c062b202647bc7a9582afd371503c5030e
Ben Pastene <bpastene@chromium.org>
2024-11-22 18:23:29 +0000

    utr: Plumb exit code when using python's exectuable-dir magic
    The magic here lets one invoke utr via `vpython3 tools/utr ...`. But
    since it was missing a "sys.exit()" call, all invocations (even
    failures) exited zero. So this fixes that.
    
    Bug: None
    Change-Id: Id714c4ebd708af00294b0628471f8c0e01c8dec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038508
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386942}

--- tools/utr/__main__.py
+++ tools/utr/__main__.py
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Allows the UTR to be invoked like `python3 ./tools/utr ...`
 """
 
+import sys
+
 import run
 
-run.main()
+sys.exit(run.main())
