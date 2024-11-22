decf2bcd555dd389ff3626a399863dffa45e352d
Maggie Jennings <mjenn@google.com>
2024-11-22 21:12:09 +0000

    [Mode B] Add histogram for redirect to /cookies from /trackingProtection
    Bug: b:333527273
    Change-Id: I1d6081857bf0d7dc31217e995e0b1fb3d950c2b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035828
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1387054}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
 #include "base/ranges/algorithm.h"
@@ -7262,6 +7261,11 @@ bool ChromeContentBrowserClient::HandleWebUI(
     GURL::Replacements replacements;
     replacements.SetPathStr(chrome::kCookiesSubPagePath);
     *url = url->ReplaceComponents(replacements);
+    base::UmaHistogramBoolean("Settings.Cookies.TrackingProtectionRedirect",
+                              true);
+  } else if (url->path() == chrome::kCookiesSubPagePath) {
+    base::UmaHistogramBoolean("Settings.Cookies.TrackingProtectionRedirect",
+                              false);
   }
 #endif
 
