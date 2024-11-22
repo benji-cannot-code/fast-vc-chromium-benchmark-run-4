3c2b9d5bedeb74415b314c95ffdbcb93c7f81f99
Dale Curtis <dalecurtis@chromium.org>
2024-11-22 21:03:10 +0000

    Remove a couple of stable feature flags
    This removes MediaCapabilitiesDynamicRange and SvgCrossOriginAttribute
    which went stable some time ago.
    
    Change-Id: I709aa8c016596bd362425496a61b9f9a863e4bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6044380
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387050}

--- content/browser/media/media_capabilities_browsertest.cc
+++ content/browser/media/media_capabilities_browsertest.cc
@@ -64,8 +64,6 @@ class MediaCapabilitiesTest : public ContentBrowserTest {
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
                                     "MediaCapabilitiesSpatialAudio");
-    command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
-                                    "MediaCapabilitiesDynamicRange");
   }
 
   std::string CanDecodeAudio(std::string_view config_type,
