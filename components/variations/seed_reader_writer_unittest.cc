6e600c7a1c5da356c3a30b7b6a306fc362cb1446
Cristian Lopez <cristiandlopez@google.com>
2024-11-22 17:23:00 +0000

    Plumb entropy providers in CrOS early boot.
    This CL plumbs entropy providers to seed stores, enabling CrOS clients
    to participate in the seed files experiment during the early boot code
    path. The CL also removes artifacts related to these clients
    previously not being able to join the treatment group. This was
    previously not safe as there was the potential for a mismatch in a
    client's treatment group during early boot and normal Chrome sessions.
    This could have lead to a situation in which clients use a local-state-
    based seed during early boot despite possibly being in the treatment
    group when using Chrome. Now however, the client will be able to join
    the same group in both code paths.
    
    Bug: 379327745
    Change-Id: If5b9c79bdf57c3dac4208cc8225cc67195011fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033352
    Commit-Queue: Cristian Lopez <cristiandlopez@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386898}

--- components/variations/seed_reader_writer_unittest.cc
+++ components/variations/seed_reader_writer_unittest.cc
@@ -233,12 +233,6 @@ TEST_P(SeedReaderWriterSeedFilesGroupTest, WriteSeed) {
   std::string seed_file_data;
   ASSERT_TRUE(base::ReadFileToString(temp_seed_file_path_, &seed_file_data));
   EXPECT_EQ(seed_file_data, compressed_seed);
-// TODO(crbug.com/379327745): Remove build flag directive when early boot can
-// participate in treatment group behavior.
-#if BUILDFLAG(IS_CHROMEOS)
-  EXPECT_EQ(local_state_.GetString(GetParam().seed_pref),
-            base64_compressed_seed);
-#endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
 // Verifies that a seed is cleared from a seed file for clients in the SeedFiles
@@ -256,12 +250,7 @@ TEST_P(SeedReaderWriterSeedFilesGroupTest, ClearSeed) {
   // Create and store seed.
   const std::string compressed_seed = CreateCompressedVariationsSeed();
   ASSERT_TRUE(base::WriteFile(temp_seed_file_path_, compressed_seed));
-// TODO(crbug.com/379327745): Remove build flag directive when early boot can
-// participate in treatment group behavior.
-#if BUILDFLAG(IS_CHROMEOS)
-  local_state_.SetString(GetParam().seed_pref,
-                         base::Base64Encode(compressed_seed));
-#endif  // BUILDFLAG(IS_CHROMEOS)
+
   // Clear seed and force write.
   seed_reader_writer.ClearSeed();
   timer_.Fire();
@@ -271,11 +260,6 @@ TEST_P(SeedReaderWriterSeedFilesGroupTest, ClearSeed) {
   std::string seed_file_data;
   ASSERT_TRUE(base::ReadFileToString(temp_seed_file_path_, &seed_file_data));
   EXPECT_THAT(seed_file_data, IsEmpty());
-// TODO(crbug.com/379327745): Remove build flag directive when early boot can
-// participate in treatment group behavior.
-#if BUILDFLAG(IS_CHROMEOS)
-  EXPECT_THAT(local_state_.GetString(GetParam().seed_pref), IsEmpty());
-#endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
 // Verifies clients in SeedFiles group read seeds from the seed file.
@@ -294,12 +278,6 @@ TEST_P(SeedReaderWriterSeedFilesGroupTest, ReadSeedFileBasedSeed) {
       GetParam().seed_pref, GetParam().channel, entropy_providers_.get(),
       file_writer_thread_.task_runner());
 
-// TODO(crbug.com/379327745): Remove build flag directive when early boot can
-// participate in treatment group behavior.
-#if !BUILDFLAG(IS_CHROMEOS)
-  // Ensure local state seed is cleared.
-  EXPECT_THAT(local_state_.GetString(GetParam().seed_pref), IsEmpty());
-#endif  // !BUILDFLAG(IS_CHROMEOS)
   // Ensure seed data loaded from seed file.
   ASSERT_EQ(StoredSeed::StorageFormat::kCompressed,
             seed_reader_writer.GetSeedData().storage_format);
@@ -327,12 +305,6 @@ TEST_P(SeedReaderWriterSeedFilesGroupTest, ReadEmptySeedFile) {
       GetParam().seed_pref, GetParam().channel, entropy_providers_.get(),
       file_writer_thread_.task_runner());
 
-// TODO(crbug.com/379327745): Remove build flag directive when early boot can
-// participate in treatment group behavior.
-#if !BUILDFLAG(IS_CHROMEOS)
-  // Ensure local state seed is cleared.
-  EXPECT_THAT(local_state_.GetString(GetParam().seed_pref), IsEmpty());
-#endif  // !BUILDFLAG(IS_CHROMEOS)
   histogram_tester.ExpectUniqueSample(
       base::StrCat(
           {"Variations.SeedFileRead.",
@@ -362,12 +334,6 @@ TEST_P(SeedReaderWriterSeedFilesGroupTest, ReadMissingSeedFile) {
       GetParam().seed_pref, GetParam().channel, entropy_providers_.get(),
       file_writer_thread_.task_runner());
 
-// TODO(crbug.com/379327745): Remove build flag directive when early boot can
-// participate in treatment group behavior.
-#if !BUILDFLAG(IS_CHROMEOS)
-  // Ensure local state seed is cleared.
-  EXPECT_THAT(local_state_.GetString(GetParam().seed_pref), IsEmpty());
-#endif  // !BUILDFLAG(IS_CHROMEOS)
   // Ensure read failed due to seed file not existing.
   histogram_tester.ExpectUniqueSample(
       base::StrCat(
