cf48630232d1a2a411fb4b48e8066ac09bf86437
Istvan Nagy <iscsi@google.com>
2024-11-22 14:09:24 +0000

    [Hgh5] Add PIN support to the Multidevice setup
    Bug: b:379941470, b:310227881
    Change-Id: I7c2f3d5b3ea79a9bf688a8caa3b9e9a3207811b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038836
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386790}

--- ash/webui/common/resources/multidevice_setup/password_page.js
+++ ash/webui/common/resources/multidevice_setup/password_page.js
@@ -8,6 +8,7 @@ import './ui_page.js';
 import '//resources/ash/common/cr_elements/cr_input/cr_input.js';
 import '//resources/ash/common/cr.m.js';
 
+import {WebUIListenerBehavior} from '//resources/ash/common/web_ui_listener_behavior.js';
 import {Polymer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BrowserProxy, BrowserProxyImpl} from './multidevice_setup_browser_proxy.js';
@@ -20,6 +21,7 @@ Polymer({
 
   behaviors: [
     UiPageContainerBehavior,
+    WebUIListenerBehavior,
   ],
 
   properties: {
@@ -76,6 +78,30 @@ Polymer({
       value: '',
     },
 
+    /** @private {boolean} */
+    authenticateByPin_: {
+      type: Boolean,
+      value: false,
+    },
+
+    /** @private {string} */
+    passwordPageHeader_: {
+      type: String,
+      value: '',
+    },
+
+    /** @private {string} */
+    authPrompt_: {
+      type: String,
+      value: '',
+    },
+
+    /** @private {string} */
+    authError_: {
+      type: String,
+      value: '',
+    },
+
     /** @private {!QuickUnlockPrivate} */
     quickUnlockPrivate_: {
       type: Object,
@@ -123,7 +149,12 @@ Polymer({
     this.browserProxy_.getProfileInfo().then((profileInfo) => {
       this.profilePhotoUrl_ = profileInfo.profilePhotoUrl;
       this.email_ = profileInfo.email;
+      this.authenticateByPin_ = profileInfo.authenticateByPin;
+      this.updateUiForAuthFactor_();
     });
+
+    this.addWebUIListener(
+        'multidevice_setup.switchToPinAuth', this.switchToPinAuth_.bind(this));
   },
 
   /** Overridden from UiPageContainerBehavior. */
@@ -176,4 +207,24 @@ Polymer({
     return this.passwordInvalid_ || !this.inputValue_ ||
         this.waitingForPasswordCheck_;
   },
+
+  /** @private */
+  switchToPinAuth_() {
+    this.authenticateByPin_ = true;
+    this.updateUiForAuthFactor_();
+  },
+
+  /** @private */
+  updateUiForAuthFactor_() {
+    if (this.authenticateByPin_) {
+      this.passwordPageHeader_ = this.i18n('passwordPageHeaderForPIN');
+      this.authPrompt_ = this.i18n('enterPIN');
+      this.authError_ = this.i18n('wrongPIN');
+    } else {
+      this.passwordPageHeader_ = this.i18n('passwordPageHeader');
+      this.authPrompt_ = this.i18n('enterPassword');
+      this.authError_ = this.i18n('wrongPassword');
+    }
+  },
+
 });
