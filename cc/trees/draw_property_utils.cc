cdd691cb1ef4470f83d38f37b9b9858828b9c547
Xianzhu Wang <wangxianzhu@chromium.org>
2024-11-22 16:23:13 +0000

    Remove unused parameters after crrev.com/c/6031938
    Bug: 40071326
    Change-Id: I2c7d9db5370eace3fe707cb8552da48eae845bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042892
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386858}

--- cc/trees/draw_property_utils.cc
+++ cc/trees/draw_property_utils.cc
@@ -360,21 +360,21 @@ bool HasSingularTransform(int transform_tree_index, const TransformTree& tree) {
 
 int LowestCommonAncestor(int clip_id_1,
                          int clip_id_2,
-                         const ClipTree* clip_tree) {
-  const ClipNode* clip_node_1 = clip_tree->Node(clip_id_1);
-  const ClipNode* clip_node_2 = clip_tree->Node(clip_id_2);
+                         const ClipTree& clip_tree) {
+  const ClipNode* clip_node_1 = clip_tree.Node(clip_id_1);
+  const ClipNode* clip_node_2 = clip_tree.Node(clip_id_2);
   while (clip_node_1->id != clip_node_2->id) {
-    if (clip_node_1->id < clip_node_2->id)
-      clip_node_2 = clip_tree->parent(clip_node_2);
-    else
-      clip_node_1 = clip_tree->parent(clip_node_1);
+    if (clip_node_1->id < clip_node_2->id) {
+      clip_node_2 = clip_tree.parent(clip_node_2);
+    } else {
+      clip_node_1 = clip_tree.parent(clip_node_1);
+    }
   }
   return clip_node_1->id;
 }
 
 void UpdateRenderSurfaceCommonAncestorClip(LayerImpl* layer,
-                                           const ClipTree* clip_tree,
-                                           EffectTree* effect_tree) {
+                                           const ClipTree& clip_tree) {
   RenderSurfaceImpl* render_surface = layer->render_target();
   CHECK(render_surface);
   int clip_id = layer->clip_tree_index();
@@ -615,8 +615,7 @@ gfx::Transform ScreenSpaceTransformInternal(LayerType* layer,
   return xform;
 }
 
-void SetSurfaceClipRect(const ClipNode* parent_clip_node,
-                        PropertyTrees* property_trees,
+void SetSurfaceClipRect(PropertyTrees* property_trees,
                         RenderSurfaceImpl* render_surface) {
   if (!render_surface->is_clipped()) {
     render_surface->SetClipRect(gfx::Rect());
@@ -910,9 +909,7 @@ void ComputeSurfaceDrawProperties(PropertyTrees* property_trees,
           render_surface->TransformTreeIndex(),
           render_surface->EffectTreeIndex()));
 
-  const ClipNode* clip_node =
-      property_trees->clip_tree().Node(render_surface->ClipTreeIndex());
-  SetSurfaceClipRect(clip_node, property_trees, render_surface);
+  SetSurfaceClipRect(property_trees, render_surface);
 }
 
 void AddSurfaceToRenderSurfaceList(RenderSurfaceImpl* render_surface,
@@ -1359,9 +1356,7 @@ void ComputeDrawPropertiesOfVisibleLayers(const LayerImplList* layer_list,
   for (LayerImpl* layer : *layer_list) {
     layer->draw_properties().opacity =
         LayerDrawOpacity(layer, property_trees->effect_tree());
-    UpdateRenderSurfaceCommonAncestorClip(
-        layer, &property_trees->clip_tree(),
-        &property_trees->effect_tree_mutable());
+    UpdateRenderSurfaceCommonAncestorClip(layer, property_trees->clip_tree());
   }
 
   // Compute clips and visible rects
