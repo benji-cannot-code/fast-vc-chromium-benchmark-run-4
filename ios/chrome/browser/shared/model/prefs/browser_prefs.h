fd382317c68e2046c8eaa4c3a41fe29273df3216
Sylvain Defresne <sdefresne@chromium.org>
2024-11-22 14:56:01 +0000

    [ios] Allow migrating the preferences asynchronously
    Migrating the browser_sync preferences is performing IO operations
    which means that it will add another asynchronous step in loading
    the preferences.
    
    Bug: 335597804
    Change-Id: If163e5e610fac5c3b29e1ca86100fe422fa17665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038126
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386816}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.h
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.h
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class PrefRegistrySimple;
 class PrefService;
 
-namespace base {
-class FilePath;
-}
 namespace user_prefs {
 class PrefRegistrySyncable;
 }
@@ -33,8 +30,7 @@ void MigrateObsoleteLocalStatePrefs(PrefService* prefs);
 // time, long deprecated prefs should be removed as new ones are added, but this
 // call should never go away (even if it becomes an empty call for some time) as
 // it should remain *the* place to drop deprecated profile's prefs at.
-void MigrateObsoleteProfilePrefs(const base::FilePath& state_path,
-                                 PrefService* prefs);
+void MigrateObsoleteProfilePrefs(PrefService* prefs);
 
 // Migrate/cleanup deprecated prefs from the standard NSUserDefault store. Over
 // time, long deprecated prefs should be removed as new ones are added, but this
