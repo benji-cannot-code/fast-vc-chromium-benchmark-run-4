c58efe0d1ba463f2ddebe42fc919793168b6261d
Sylvain Defresne <sdefresne@chromium.org>
2024-11-22 14:16:51 +0000

    Add async version of MaybeMigrateSyncingUserToSignedIn(...)
    When creating a profile asynchronously, it may not be allowed to
    perform IO operations. Add an asynchronous variant that perform
    the IO on a background sequence and invoke a closure when done.
    
    Update unit tests to check both variants.
    
    Bug: 335597804
    Change-Id: Ibc6c50ad51b8675598de8a3f31c240fa4ebce7e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042145
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386793}

--- components/browser_sync/sync_to_signin_migration.h
+++ components/browser_sync/sync_to_signin_migration.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_BROWSER_SYNC_SYNC_TO_SIGNIN_MIGRATION_H_
 
 #include "base/feature_list.h"
+#include "base/functional/callback.h"
 #include "components/sync/base/data_type.h"
 
 namespace base {
@@ -45,6 +46,13 @@ SyncToSigninMigrationDataTypeDecision GetSyncToSigninMigrationDataTypeDecision(
 // KeyedServices are created.
 void MaybeMigrateSyncingUserToSignedIn(const base::FilePath& profile_path,
                                        PrefService* pref_service);
+
+// Asynchronous version of MaybeMigrateSyncingUserToSignedIn() that can be
+// used in context where accessing the IO is forbidden. When the migration is
+// complete `closure` will be invoked asynchronously on the current sequence.
+void MaybeMigrateSyncingUserToSignedInAsync(const base::FilePath& profile_path,
+                                            PrefService* pref_service,
+                                            base::OnceClosure closure);
 
 // Returns whether the current primary account was migrated from "syncing" to
 // "signed-in" via MaybeMigrateSyncingUserToSignedIn().
