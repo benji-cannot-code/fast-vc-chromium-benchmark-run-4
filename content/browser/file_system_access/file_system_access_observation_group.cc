79f10aad6c3b6e1b3741a0f0083993ac4c0cb5fd
Nathan Memmott <memmott@chromium.org>
2024-11-22 23:18:54 +0000

    Change Observers: Make Observation Groups keyed by Storage Key
    Observation Groups were previously keyed only be Scope. This makes it
    so that they are keyed by both Scope and Storage Key.
    
    This is in preparation to implement change observers limits. There
    should be no functional change.
    
    Bug: 338457523
    Change-Id: I7c1d8b4bef68a31ff5f2da0fe71d5dcaa302e4be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033039
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387124}

--- content/browser/file_system_access/file_system_access_observation_group.cc
+++ content/browser/file_system_access/file_system_access_observation_group.cc
@@ -53,9 +53,12 @@ void FileSystemAccessObservationGroup::Observer::NotifyOfChanges(
 
 FileSystemAccessObservationGroup::FileSystemAccessObservationGroup(
     FileSystemAccessWatcherManager& watcher_manager,
+    blink::StorageKey storage_key,
     FileSystemAccessWatchScope scope,
     base::PassKey<FileSystemAccessWatcherManager> pass_key)
-    : scope_(std::move(scope)), watcher_manager_(watcher_manager) {
+    : storage_key_(std::move(storage_key)),
+      scope_(std::move(scope)),
+      watcher_manager_(watcher_manager) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   watcher_manager_->AddObserver(this);
@@ -87,7 +90,7 @@ void FileSystemAccessObservationGroup::RemoveObserver(Observer* observation) {
   // destroy this when there are no more observations.
   if (observations_.empty()) {
     // `this` is destroyed after `RemoveObservationGroup` is called.
-    watcher_manager_->RemoveObservationGroup(scope_);
+    watcher_manager_->RemoveObservationGroup(storage_key_, scope_);
   }
 }
 
