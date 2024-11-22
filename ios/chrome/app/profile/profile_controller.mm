1bd8a9d58c8e3bd860a3b42dadeaf995c6232bf6
Louis Romero <lpromero@google.com>
2024-11-22 14:16:11 +0000

    Add EG tests for Join flow
    This CL adds two tests for Shared Tab Groups when joining a group
    from a share URL.
    
    This reverts commit 6d6b7192299f76b6a5210b3f08b995d058b92430 and is a
    reland of commit 47d881517b6da935d7aef28d4f1db6f2649ce4df
    
    Bug: 379657889
    Change-Id: Idd19ca625365f894792ee0f38dd582cb65a28fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038125
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386792}

--- ios/chrome/app/profile/profile_controller.mm
+++ ios/chrome/app/profile/profile_controller.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/profile/profile_state_observer.h"
 #import "ios/chrome/app/profile/search_engine_choice_profile_agent.h"
 #import "ios/chrome/app/spotlight/spotlight_manager.h"
+#import "ios/chrome/browser/collaboration/model/features.h"
 #import "ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h"
 #import "ios/chrome/browser/credential_provider/model/credential_provider_buildflags.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_profile_agent.h"
@@ -61,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
 #import "ios/chrome/browser/share_extension/model/share_extension_service.h"
 #import "ios/chrome/browser/share_extension/model/share_extension_service_factory.h"
+#import "ios/chrome/browser/share_kit/model/share_kit_service_factory.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state_observer.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
@@ -487,6 +489,11 @@ void FlushCookieStoreOnIOThread(
   // Ensure that the tab group sync services are created to observe updates.
   if (IsTabGroupSyncEnabled()) {
     tab_groups::TabGroupSyncServiceFactory::GetForProfile(profile);
+  }
+
+  // Ensure that the ShareKit service is created to handle Join Group requests.
+  if (IsSharedTabGroupsJoinEnabled(profile)) {
+    ShareKitServiceFactory::GetForProfile(profile);
   }
 
   [self attachProfileAgents];
