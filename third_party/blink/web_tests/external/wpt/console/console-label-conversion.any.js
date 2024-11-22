f9b571fda4db5818c9f5e301edc64441ef5a96a1
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-11-22 08:10:25 +0000

    Import wpt@c22e0c51fc43acee1555bcf3e8096fd564352fc8
    https://github.com/web-platform-tests/wpt/compare/8c7ff922c...c22e0c51f
    
    Using wpt-import in Chromium 75805f5c7e31358b3c04f3db7b4f5386421ab040.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kozyatinskiy@chromium.org:
      external/wpt/console
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ic2224a1d192d29bf7e279d18b3155e9ac6e72b1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042676
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1386658}

--- third_party/blink/web_tests/external/wpt/console/console-label-conversion.any.js
+++ third_party/blink/web_tests/external/wpt/console/console-label-conversion.any.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,shadowrealm
+
 "use strict";
 // https://console.spec.whatwg/org/#counting
 // https://console.spec.whatwg/org/#timing
