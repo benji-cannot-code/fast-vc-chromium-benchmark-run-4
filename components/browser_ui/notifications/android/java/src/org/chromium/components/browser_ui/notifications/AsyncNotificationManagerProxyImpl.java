67d56ddb673d659dc073ad930fb6eef8d7c9ea90
Min Qin <qinmin@chromium.org>
2024-11-23 00:30:49 +0000

    Fix an issue that trace event is recorded on a wrong thread
    TraceEvent.scoped() will start recording the trace right away on
    the same thread it is called. So for tasks that are posted to
    another thread, we need to call it on that thread.
    
    Change-Id: Iad5a849f2c58bf49404d507722a4144a24ec38d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039840
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1387169}

--- components/browser_ui/notifications/android/java/src/org/chromium/components/browser_ui/notifications/AsyncNotificationManagerProxyImpl.java
+++ components/browser_ui/notifications/android/java/src/org/chromium/components/browser_ui/notifications/AsyncNotificationManagerProxyImpl.java
@@ -36,7 +36,7 @@ import java.util.stream.Collectors;
     @Override
     public void areNotificationsEnabled(Callback<Boolean> callback) {
         runAsyncAndReply(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.areNotificationsEnabled"),
+                "AsyncNotificationManagerProxyImpl.areNotificationsEnabled",
                 () -> mNotificationManager.areNotificationsEnabled(),
                 callback);
     }
@@ -44,43 +44,42 @@ import java.util.stream.Collectors;
     @Override
     public void cancel(int id) {
         runAsync(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.cancel(id)"),
+                "AsyncNotificationManagerProxyImpl.cancel(id)",
                 () -> mNotificationManager.cancel(id));
     }
 
     @Override
     public void cancel(String tag, int id) {
         runAsync(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.cancel(tag, id)"),
+                "AsyncNotificationManagerProxyImpl.cancel(tag, id)",
                 () -> mNotificationManager.cancel(tag, id));
     }
 
     @Override
     public void cancelAll() {
         runAsync(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.cancelAll"),
+                "AsyncNotificationManagerProxyImpl.cancelAll",
                 () -> mNotificationManager.cancelAll());
     }
 
     @Override
     public void createNotificationChannel(NotificationChannel channel) {
         runAsync(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.createNotificationChannel"),
+                "AsyncNotificationManagerProxyImpl.createNotificationChannel",
                 () -> mNotificationManager.createNotificationChannel(channel));
     }
 
     @Override
     public void createNotificationChannelGroup(NotificationChannelGroup channelGroup) {
         runAsync(
-                TraceEvent.scoped(
-                        "AsyncNotificationManagerProxyImpl.createNotificationChannelGroup"),
+                "AsyncNotificationManagerProxyImpl.createNotificationChannelGroup",
                 () -> mNotificationManager.createNotificationChannelGroup(channelGroup));
     }
 
     @Override
     public void getNotificationChannels(Callback<List<NotificationChannel>> callback) {
         runAsyncAndReply(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.getNotificationChannels"),
+                "AsyncNotificationManagerProxyImpl.getNotificationChannels",
                 () -> mNotificationManager.getNotificationChannels(),
                 callback);
     }
@@ -88,7 +87,7 @@ import java.util.stream.Collectors;
     @Override
     public void getNotificationChannelGroups(Callback<List<NotificationChannelGroup>> callback) {
         runAsyncAndReply(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.getNotificationChannelGroups"),
+                "AsyncNotificationManagerProxyImpl.getNotificationChannelGroups",
                 () -> mNotificationManager.getNotificationChannelGroups(),
                 callback);
     }
@@ -96,15 +95,14 @@ import java.util.stream.Collectors;
     @Override
     public void deleteNotificationChannel(String id) {
         runAsync(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.deleteNotificationChannel"),
+                "AsyncNotificationManagerProxyImpl.deleteNotificationChannel",
                 () -> mNotificationManager.deleteNotificationChannel(id));
     }
 
     @Override
     public void deleteAllNotificationChannels(Function<String, Boolean> func) {
         runAsync(
-                TraceEvent.scoped(
-                        "AsyncNotificationManagerProxyImpl.deleteAllNotificationChannels"),
+                "AsyncNotificationManagerProxyImpl.deleteAllNotificationChannels",
                 () -> {
                     for (NotificationChannel channel :
                             mNotificationManager.getNotificationChannels()) {
@@ -125,7 +123,7 @@ import java.util.stream.Collectors;
         }
 
         runAsync(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.notify"),
+                "AsyncNotificationManagerProxyImpl.notify",
                 () ->
                         mNotificationManager.notify(
                                 notification.getMetadata().tag,
@@ -136,7 +134,7 @@ import java.util.stream.Collectors;
     @Override
     public void getNotificationChannel(String channelId, Callback<NotificationChannel> callback) {
         runAsyncAndReply(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.getNotificationChannel"),
+                "AsyncNotificationManagerProxyImpl.getNotificationChannel",
                 () -> mNotificationManager.getNotificationChannel(channelId),
                 callback);
     }
@@ -144,8 +142,7 @@ import java.util.stream.Collectors;
     @Override
     public void deleteNotificationChannelGroup(String groupId) {
         runAsync(
-                TraceEvent.scoped(
-                        "AsyncNotificationManagerProxyImpl.deleteNotificationChannelGroup"),
+                "AsyncNotificationManagerProxyImpl.deleteNotificationChannelGroup",
                 () -> mNotificationManager.deleteNotificationChannelGroup(groupId));
     }
 
@@ -153,7 +150,7 @@ import java.util.stream.Collectors;
     public void getActiveNotifications(
             Callback<List<? extends StatusBarNotificationProxy>> callback) {
         runAsyncAndReply(
-                TraceEvent.scoped("AsyncNotificationManagerProxyImpl.getActiveNotifications"),
+                "AsyncNotificationManagerProxyImpl.getActiveNotifications",
                 () ->
                         mNotificationManager.getActiveNotifications().stream()
                                 .map((sbn) -> new StatusBarNotificationAdaptor(sbn))
@@ -162,10 +159,11 @@ import java.util.stream.Collectors;
     }
 
     /** Helper method to run an runnable inside a scoped event in background. */
-    private void runAsync(TraceEvent scopedEvent, Runnable runnable) {
+    @SuppressWarnings("NoDynamicStringsInTraceEventCheck")
+    private void runAsync(String eventName, Runnable runnable) {
         AsyncTask.SERIAL_EXECUTOR.execute(
                 () -> {
-                    try (scopedEvent) {
+                    try (TraceEvent te = TraceEvent.scoped(eventName)) {
                         runnable.run();
                     } catch (Exception e) {
                         Log.e(TAG, "unable to run a runnable.", e);
@@ -177,12 +175,12 @@ import java.util.stream.Collectors;
      * Helper method to run an runnable inside a scoped event in background, and executes callback
      * on the ui thread.
      */
-    private <T> void runAsyncAndReply(
-            TraceEvent scopedEvent, Callable<T> callable, Callback callback) {
+    @SuppressWarnings("NoDynamicStringsInTraceEventCheck")
+    private <T> void runAsyncAndReply(String eventName, Callable<T> callable, Callback callback) {
         new AsyncTask<T>() {
             @Override
             protected T doInBackground() {
-                try (scopedEvent) {
+                try (TraceEvent te = TraceEvent.scoped(eventName)) {
                     try {
                         return callable.call();
                     } catch (Exception e) {
