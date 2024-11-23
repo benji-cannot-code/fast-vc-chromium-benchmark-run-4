7baebe644440eeb2b1c26eaca7eeb44472cd059d
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-11-23 00:20:43 +0000

    [Extensions] Allow extensions to update script worlds to default world
    Extensions can inject in multiple user script worlds, which are
    specified by different user script world (string) IDs. If an ID isn't
    specified, the extension injects in the default user script world ID.
    However, this creates an issue with trying to update a registered
    script to inject in the default world after initially registered with
    a non-default world:
    * Specifying any non-null value indicates a non-default world
    * Specifying a null value will be dropped by the extension bindings
    
    To enable this, allow extensions to use the empty string ('') as an
    indicator that a script should use the default world. The API layer
    converts the empty string to nullopt for use elsewhere in the
    extensions system.
    
    Bug: 331680187
    Change-Id: I4b0ef064884dc5fac814c0680134c278d0b28fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036255
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387164}

--- extensions/browser/user_script_world_configuration_manager.cc
+++ extensions/browser/user_script_world_configuration_manager.cc
@@ -107,6 +107,7 @@ void UserScriptWorldConfigurationManager::SetUserScriptWorldInfo(
     const std::optional<std::string>& world_id,
     std::optional<std::string> csp,
     bool enable_messaging) {
+  CHECK(!world_id || !world_id->empty());
   // Persist world configuratation in ExtensionPrefs.
   ExtensionPrefs::ScopedDictionaryUpdate update(
       extension_prefs_, extension.id(), kUserScriptsWorldsConfiguration.name);
@@ -131,6 +132,7 @@ void UserScriptWorldConfigurationManager::SetUserScriptWorldInfo(
 void UserScriptWorldConfigurationManager::ClearUserScriptWorldInfo(
     const Extension& extension,
     const std::optional<std::string>& world_id) {
+  CHECK(!world_id || !world_id->empty());
   ExtensionPrefs::ScopedDictionaryUpdate update(
       extension_prefs_, extension.id(), kUserScriptsWorldsConfiguration.name);
   std::unique_ptr<prefs::DictionaryValueUpdate> update_dict = update.Get();
@@ -152,6 +154,7 @@ mojom::UserScriptWorldInfoPtr
 UserScriptWorldConfigurationManager::GetUserScriptWorldInfo(
     const ExtensionId& extension_id,
     const std::optional<std::string>& world_id) {
+  CHECK(!world_id || !world_id->empty());
   const base::Value::Dict* worlds_configuration =
       extension_prefs_->ReadPrefAsDictionary(extension_id,
                                              kUserScriptsWorldsConfiguration);
