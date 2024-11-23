3ee72842a10bd2ba4ef67b1ce54fd24e686bfd18
Zhe Li <zheliooo@google.com>
2024-11-23 01:33:05 +0000

    [TabGroupShareTablet] implement java logic to update avatar on shared group title
    Bug: 362314403
    Change-Id: Ib7f6e03148718e72468fc16c603fc4eb835dc5e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938002
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1387189}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/TabGroupContextMenuCoordinatorUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/TabGroupContextMenuCoordinatorUnitTest.java
@@ -70,7 +70,7 @@ import java.util.List;
 
 /** Unit tests for {@link TabGroupContextMenuCoordinator}. */
 @RunWith(BaseRobolectricTestRunner.class)
-@EnableFeatures({ChromeFeatureList.TAB_STRIP_GROUP_CONTEXT_MENU})
+@EnableFeatures({ChromeFeatureList.TAB_STRIP_GROUP_CONTEXT_MENU, ChromeFeatureList.DATA_SHARING})
 public class TabGroupContextMenuCoordinatorUnitTest {
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
@@ -142,7 +142,6 @@ public class TabGroupContextMenuCoordinatorUnitTest {
     }
 
     @Test
-    @EnableFeatures(ChromeFeatureList.DATA_SHARING)
     @Feature("Tab Strip Group Context Menu")
     public void testListMenuItems() {
         // Build custom view first to setup menu view.
@@ -237,7 +236,6 @@ public class TabGroupContextMenuCoordinatorUnitTest {
     }
 
     @Test
-    @EnableFeatures(ChromeFeatureList.DATA_SHARING)
     @Feature("Tab Strip Group Context Menu")
     public void testCollaborationMenuItems_Owner() {
         ModelList modelList = new ModelList();
@@ -264,7 +262,6 @@ public class TabGroupContextMenuCoordinatorUnitTest {
     }
 
     @Test
-    @EnableFeatures(ChromeFeatureList.DATA_SHARING)
     @Feature("Tab Strip Group Context Menu")
     public void testCollaborationMenuItems_Member() {
         ModelList modelList = new ModelList();
