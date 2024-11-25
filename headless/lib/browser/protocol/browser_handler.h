c7e8f635fec4434f45c1c4d39802cdb79a1cbccd
Alex Rudenko <alexrudenko@chromium.org>
2024-11-25 09:55:01 +0000

    De-alias protocol::Maybe in //headless
    Bug: 40276207
    Change-Id: I1b62e2c45614755dead020cf20003939f972760e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038303
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387493}

--- headless/lib/browser/protocol/browser_handler.h
+++ headless/lib/browser/protocol/browser_handler.h
@@ -30,7 +30,7 @@ class BrowserHandler : public DomainHandler, public Browser::Backend {
 
   // Browser::Backend implementation
   Response GetWindowForTarget(
-      Maybe<std::string> target_id,
+      std::optional<std::string> target_id,
       int* out_window_id,
       std::unique_ptr<Browser::Bounds>* out_bounds) override;
   Response GetWindowBounds(
@@ -40,7 +40,8 @@ class BrowserHandler : public DomainHandler, public Browser::Backend {
   Response SetWindowBounds(
       int window_id,
       std::unique_ptr<Browser::Bounds> out_bounds) override;
-  Response SetDockTile(Maybe<std::string> label, Maybe<Binary> image) override;
+  Response SetDockTile(std::optional<std::string> label,
+                       std::optional<Binary> image) override;
 
  private:
   raw_ptr<HeadlessBrowserImpl> browser_;
