71f48cd49814e047fe4ae607a741a46c3da45e0b
Nancy Xiao <nancylanxiao@google.com>
2024-11-25 20:55:15 +0000

    Gate FCM service initialization by experiment flag
    This is the first step to remove FCM from CloudBinaryUploadService
    
    Bug: b/360113709
    Change-Id: I3991aec1ab391c8c6da68cf1932f5d1728e80fbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048086
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1387784}

--- chrome/browser/safe_browsing/cloud_content_scanning/cloud_binary_upload_service_browsertest.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/cloud_binary_upload_service_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/test_future.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/enterprise/connectors/analysis/content_analysis_features.h"
 #include "chrome/browser/enterprise/connectors/connectors_service.h"
 #include "chrome/browser/enterprise/connectors/test/management_context_mixin.h"
 #include "chrome/browser/enterprise/connectors/test/test_constants.h"
@@ -125,7 +126,8 @@ class TestCloudBinaryUploadService : public CloudBinaryUploadService {
     // Since paste requests only receive sync responses, they don't use FCM and
     // shouldn't have a notification token.
     if (connector_ ==
-        enterprise_connectors::AnalysisConnector::BULK_DATA_ENTRY) {
+            enterprise_connectors::AnalysisConnector::BULK_DATA_ENTRY ||
+        enterprise_connectors::IsStopRegisterFcmEnabled()) {
       ASSERT_TRUE(request->fcm_notification_token().empty());
     } else {
       ASSERT_EQ(request->fcm_notification_token(), kTestInstanceId);
@@ -173,20 +175,31 @@ class TestRequest : public CloudBinaryUploadService::Request {
 
 class CloudBinaryUploadServiceRequestValidationBrowserTest
     : public MixinBasedInProcessBrowserTest,
-      public testing::WithParamInterface<ManagementContextDeviceRequest> {
+      public testing::WithParamInterface<
+          std::tuple<ManagementContextDeviceRequest, bool>> {
  public:
   CloudBinaryUploadServiceRequestValidationBrowserTest()
       : management_mixin_(
             enterprise_connectors::test::ManagementContextMixin::Create(
                 &mixin_host_,
                 this,
-                management_context())) {}
-
-  enterprise_connectors::test::ManagementContext management_context() const {
-    return GetParam().context;
+                management_context())) {
+    is_stop_register_fcm_enabled()
+        ? scoped_feature_list_.InitAndEnableFeature(
+              enterprise_connectors::kStopRegisterFcmEnabled)
+        : scoped_feature_list_.InitAndDisableFeature(
+              enterprise_connectors::kStopRegisterFcmEnabled);
   }
 
-  bool profile_request() const { return GetParam().profile_request; }
+  enterprise_connectors::test::ManagementContext management_context() const {
+    return std::get<0>(GetParam()).context;
+  }
+
+  bool profile_request() const {
+    return std::get<0>(GetParam()).profile_request;
+  }
+
+  bool is_stop_register_fcm_enabled() const { return std::get<1>(GetParam()); }
 
   void SetUpOnMainThread() override {
     CloudBinaryUploadServiceFactory::GetInstance()->SetTestingFactory(
@@ -274,7 +287,9 @@ IN_PROC_BROWSER_TEST_P(CloudBinaryUploadServiceRequestValidationBrowserTest,
 IN_PROC_BROWSER_TEST_P(CloudBinaryUploadServiceRequestValidationBrowserTest,
                        FileAttach) {
   set_connector(enterprise_connectors::AnalysisConnector::FILE_ATTACHED);
-  set_valid_fcm(true);
+  if (!is_stop_register_fcm_enabled()) {
+    set_valid_fcm(true);
+  }
 
   enterprise_connectors::CloudAnalysisSettings cloud_settings;
   cloud_settings.analysis_url = GURL(kTestUrl);
@@ -301,7 +316,9 @@ IN_PROC_BROWSER_TEST_P(CloudBinaryUploadServiceRequestValidationBrowserTest,
 IN_PROC_BROWSER_TEST_P(CloudBinaryUploadServiceRequestValidationBrowserTest,
                        FileDownload) {
   set_connector(enterprise_connectors::AnalysisConnector::FILE_DOWNLOADED);
-  set_valid_fcm(true);
+  if (!is_stop_register_fcm_enabled()) {
+    set_valid_fcm(true);
+  }
 
   enterprise_connectors::CloudAnalysisSettings cloud_settings;
   cloud_settings.analysis_url = GURL(kTestUrl);
@@ -328,7 +345,9 @@ IN_PROC_BROWSER_TEST_P(CloudBinaryUploadServiceRequestValidationBrowserTest,
 IN_PROC_BROWSER_TEST_P(CloudBinaryUploadServiceRequestValidationBrowserTest,
                        Print) {
   set_connector(enterprise_connectors::AnalysisConnector::PRINT);
-  set_valid_fcm(true);
+  if (!is_stop_register_fcm_enabled()) {
+    set_valid_fcm(true);
+  }
 
   enterprise_connectors::CloudAnalysisSettings cloud_settings;
   cloud_settings.analysis_url = GURL(kTestUrl);
@@ -354,6 +373,7 @@ IN_PROC_BROWSER_TEST_P(CloudBinaryUploadServiceRequestValidationBrowserTest,
 
 INSTANTIATE_TEST_SUITE_P(All,
                          CloudBinaryUploadServiceRequestValidationBrowserTest,
-                         testing::ValuesIn(kTestCases));
+                         testing::Combine(testing::ValuesIn(kTestCases),
+                                          testing::Bool()));
 
 }  // namespace safe_browsing
