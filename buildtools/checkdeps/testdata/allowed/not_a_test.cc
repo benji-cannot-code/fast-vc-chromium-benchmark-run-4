801fd1deeb3940874f0b9611651ac28099fac533
Andrew Grieve <agrieve@chromium.org>
2024-11-25 22:22:05 +0000

    Reland "checkdeps: Teach it about new_usages_require_review"
    This reverts commit 142fc1efd6a3a2634a1e054c31c349be61034430.
    
    Reason for reland: Account for paths outside of base_directory
    
    Original change's description:
    > Revert "checkdeps: Teach it about new_usages_require_review"
    >
    > This reverts commit 20ff3edd59380b820cea9e613a7d3774325e8073.
    >
    > Reason for revert: Breaking in V8
    > File "/usr/local/google/home/leszeks/v8/v8/buildtools/checkdeps/builddeps.py", line 373, in _IterSelfAndParentDirectories
    >     assert parent != current_dir_name, (
    >            ^^^^^^^^^^^^^^^^^^^^^^^^^^
    > AssertionError: /usr/local/google/home/leszeks/third_party/inspector_protocol/crdtp, /usr/local/google/home/leszeks/v8/v8, /
    >
    >
    > Original change's description:
    > > checkdeps: Teach it about new_usages_require_review
    > >
    > > This will cause checkdeps to fail if an include is added to a directory
    > > that lists new_usages_require_review=True in its DEPS, but contains
    > > include_dirs entries only for ancestor directories.
    > >
    > > Bug: 365797506
    > > Change-Id: Ib6ccaf6d65bb869ff13f8f3c7722146fc7ad5317
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953099
    > > Reviewed-by: Nico Weber <thakis@chromium.org>
    > > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: David Benjamin <davidben@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1386478}
    >
    > Bug: 365797506
    > Change-Id: I14e1f734359ad87927a6557a6ff2687acc0fe063
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043338
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1386859}
    
    Bug: 365797506
    Change-Id: I302d78a800686716a49eadcd736b4cc53540192c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049694
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387859}

--- buildtools/checkdeps/testdata/allowed/not_a_test.cc
+++ buildtools/checkdeps/testdata/allowed/not_a_test.cc
@@ -4,3 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "buildtools/checkdeps/testdata/disallowed/teststuff/bad.h"
+
+// Test that directories outside of base_directory do not cause issues.
+#include "/does_not_exist.h"
