13a153fad79f27a07beda9be4112e97cbfd66b32
Peter Kasting <pkasting@chromium.org>
2024-11-25 00:01:25 +0000

    Cleanups from stricter span usage of size_t, pt. 2: chrome
    More use of first(), compile-time sizes, size_t, etc.
    
    Bug: none
    Change-Id: Iaa7319f85c219004577186fa01cc0d735dcf8063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6045610
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387395}

--- chrome/utility/safe_browsing/mac/read_stream.cc
+++ chrome/utility/safe_browsing/mac/read_stream.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/notreached.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/posix/eintr_wrapper.h"
 
 namespace safe_browsing {
@@ -54,7 +55,8 @@ bool MemoryReadStream::Read(base::span<uint8_t> buf, size_t* bytes_read) {
   }
 
   *bytes_read = std::min(buf.size(), bytes_remaining);
-  buf.first(*bytes_read).copy_from(byte_buf_.subspan(offset_, *bytes_read));
+  buf.first(*bytes_read)
+      .copy_from(byte_buf_.subspan(static_cast<size_t>(offset_), *bytes_read));
   offset_ += *bytes_read;
   return true;
 }
@@ -73,9 +75,7 @@ off_t MemoryReadStream::Seek(off_t offset, int whence) {
     default:
       NOTREACHED();
   }
-  if (static_cast<size_t>(offset_) >= byte_buf_.size()) {
-    offset_ = byte_buf_.size();
-  }
+  offset_ = std::min(base::checked_cast<size_t>(offset_), byte_buf_.size());
   return offset_;
 }
 
