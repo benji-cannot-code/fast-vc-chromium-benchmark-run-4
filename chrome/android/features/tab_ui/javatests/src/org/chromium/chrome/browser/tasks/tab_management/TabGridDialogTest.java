a3992f51385dc78f2d0f08c8b6e796dee1f05b7a
Zhe Li <zheliooo@google.com>
2024-11-25 21:58:50 +0000

    fix stale CollaborationService when profile changes and re-enable test
    Bug:380823191
    
    Change-Id: I8788e1977cbe8100c7542d02462e197e35dde440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6047342
    Reviewed-by: Neil Coronado <nemco@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1387836}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -2027,7 +2027,6 @@ public class TabGridDialogTest {
     @Test
     @MediumTest
     @EnableAnimations
-    @DisabledTest(message = "https://crbug.com/380823191")
     public void testCreateIncognitoGroupAndCloseAllTabsInDialogTwice_Bug354745444() {
         ChromeTabbedActivity cta = sActivityTestRule.getActivity();
         boolean incognito = true;
