e58ed922ac0a5d2386fb6584511fc9c40b809243
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-11-25 09:27:36 +0000

    [iOS] Create youtube incognito provider
    This CL implement the methods of the provider in ios_internal.
    
     =>   crrev.com/c/6031551 : create app_mode provider
      -   crrev.com/i/7834666 : implement app_mode provider
    
    Bug: 374934680
    Change-Id: Ic2310fe3776b6e566967e2b26043eb5c7adcece4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6031551
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1387481}

---
+++ ios/chrome/test/providers/application_mode_fetcher/test_application_mode_fetcher.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "base/functional/bind.h"
+#import "base/location.h"
+#import "base/task/sequenced_task_runner.h"
+#import "ios/public/provider/chrome/browser/application_mode_fetcher/application_mode_fetcher_api.h"
+#import "url/gurl.h"
+
+namespace ios::provider {
+
+void FetchApplicationMode(const GURL& url,
+                          NSString* appID,
+                          AppModeFetchingCallback callback) {
+  // TODO(crbug.com/374934680): Add a factory to test different callback
+  // configurations.
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), false));
+}
+
+}  // namespace ios::provider
