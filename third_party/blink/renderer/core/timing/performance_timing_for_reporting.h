231fdf8b0b9941435d9addb28612ef174cd2a562
Keyou <qqkillyou@gmail.com>
2024-11-25 17:24:34 +0000

    Make PopulateLargestContentfulPaintDetailsForReporting private
    The PerformanceTimingForReporting::
    PopulateLargestContentfulPaintDetailsForReporting method should be
    private rather than public.
    
    Bug: 375944001
    Change-Id: Ic1081e69d2e98ea92ad8c3535fd6ccbb38a1b0f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5971274
    Commit-Queue: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387687}

--- third_party/blink/renderer/core/timing/performance_timing_for_reporting.h
+++ third_party/blink/renderer/core/timing/performance_timing_for_reporting.h
@@ -99,10 +99,6 @@ class CORE_EXPORT PerformanceTimingForReporting final
   LargestContentfulPaintDetailsForReporting
   SoftNavigationLargestContentfulPaintDetailsForMetrics() const;
 
-  LargestContentfulPaintDetailsForReporting
-  PopulateLargestContentfulPaintDetailsForReporting(
-      const LargestContentfulPaintDetails& timing) const;
-
   // The time at which the frame is first eligible for painting due to not
   // being throttled. A zero value indicates throttling.
   uint64_t FirstEligibleToPaint() const;
@@ -189,6 +185,9 @@ class CORE_EXPORT PerformanceTimingForReporting final
   InteractiveDetector* GetInteractiveDetector() const;
   std::optional<base::TimeDelta> MonotonicTimeToPseudoWallTime(
       const std::optional<base::TimeTicks>&) const;
+  LargestContentfulPaintDetailsForReporting
+  PopulateLargestContentfulPaintDetailsForReporting(
+      const LargestContentfulPaintDetails& timing) const;
 
   bool cross_origin_isolated_capability_;
 };
