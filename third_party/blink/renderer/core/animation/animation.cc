fdb740b9282f1258cb5b418b7dc5f360c430af58
David Awogbemila <awogbemila@chromium.org>
2024-11-25 23:43:39 +0000

    Rename Animation.progress to Animation.overallProgress
    In line with the CSS working group resolution in #8799[1], this patch
    renames Animation.progress in to Animation.overallProgress in Blink.
    
    [1] https://github.com/w3c/csswg-drafts/issues/8799#issuecomment-2461097047
    
    Bug: 40914396
    Change-Id: Ie7605f59d99314158b77e9672dbe98a564d2d1e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049834
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387916}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -636,7 +636,7 @@ std::optional<AnimationTimeDelta> Animation::UnlimitedCurrentTime() const {
              : CalculateCurrentTime();
 }
 
-std::optional<double> Animation::progress() const {
+std::optional<double> Animation::overallProgress() const {
   std::optional<AnimationTimeDelta> current_time = CurrentTimeInternal();
   if (!effect() || !current_time) {
     return std::nullopt;
