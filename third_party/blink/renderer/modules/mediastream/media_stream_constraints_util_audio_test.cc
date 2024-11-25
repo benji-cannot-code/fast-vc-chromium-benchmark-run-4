08b2f195aa81223d9fe5ccf57214f4a230eb7773
Lionel Koenig Gélas <lionelk@google.com>
2024-11-25 13:10:56 +0000

    Reland "Remove googHighpassFilter constraints"
    This reverts commit 34a3ad0a32d42e6299d2d392090db010874e69ba.
    
    And updates
    `third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_audio.cc`.
    
    Reason for revert: Fixing missed code removals
    
    Original change's description:
    > Revert "Remove googHighpassFilter constraints"
    >
    > This reverts commit 4a7eeb8ca7af7fce0715a999b0e5a1287413fb64.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5299838418681856
    >
    > Sample build with failed test: https://ci.chromium.org/b/8730745112830651793
    > Affected test(s):
    > [ninja://fuchsia_web/webengine:web_engine_integration_tests/WebEngineIntegrationMediaTest.MicrophoneAccess_WithoutPermission](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Ffuchsia_web%2Fwebengine:web_engine_integration_tests%2FWebEngineIntegrationMediaTest.MicrophoneAccess_WithoutPermission?q=VHash%3Ac2c3ce6fb4e5dcea)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5299838418681856&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6035607&type=BUG
    >
    > Original change's description:
    > > Remove googHighpassFilter constraints
    > >
    > > This removes googHighpassFilter constraints. It let WebRTC
    > > AudioProcessing module manage the high pass filter.
    > >
    > > Bug: chromium:40610693
    > > Change-Id: I06b72d70ca16fd88e924a3280f5c3d9267113cf1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035607
    > > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Commit-Queue: Lionel Koenig Gélas <lionelk@google.com>
    > > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > > Reviewed-by: Per Åhgren <peah@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1385595}
    > >
    >
    > Bug: chromium:40610693
    > Change-Id: I92e402fcf1f0f62365bbe4e3f5a9a0bf55219cd7
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039294
    > Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    > Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    > Owners-Override: Shunya Shishido <sisidovski@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1386017}
    
    Bug: chromium:40610693
    Change-Id: Ia4b38a73649143d453d0a0a7de223e9cca11edbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040158
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Lionel Koenig Gélas <lionelk@google.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387557}

--- third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_audio_test.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_audio_test.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/testing/sim/sim_test.h"
 #include "third_party/blink/renderer/modules/mediastream/local_media_stream_audio_source.h"
 #include "third_party/blink/renderer/modules/mediastream/media_constraints.h"
+#include "third_party/blink/renderer/modules/mediastream/media_stream_constraints_util.h"
 #include "third_party/blink/renderer/modules/mediastream/mock_constraint_factory.h"
 #include "third_party/blink/renderer/modules/mediastream/processed_local_audio_source.h"
 #include "third_party/blink/renderer/modules/webrtc/webrtc_audio_device_impl.h"
@@ -93,8 +94,7 @@ class MediaStreamConstraintsUtilAudioTestBase : public SimTest {
   bool IsDeviceCapture() { return GetMediaStreamSource().empty(); }
   static AudioPropertiesBoolMembers GetAudioProcessingProperties() {
     return {&AudioProcessingProperties::auto_gain_control,
-            &AudioProcessingProperties::noise_suppression,
-            &AudioProcessingProperties::goog_highpass_filter};
+            &AudioProcessingProperties::noise_suppression};
   }
 
   blink::mojom::MediaStreamType GetMediaStreamType() {
@@ -206,10 +206,6 @@ class MediaStreamConstraintsUtilAudioTestBase : public SimTest {
                   &AudioProcessingProperties::noise_suppression)) {
       EXPECT_TRUE(properties.noise_suppression);
     }
-    if (!Contains(exclude_audio_properties,
-                  &AudioProcessingProperties::goog_highpass_filter)) {
-      EXPECT_TRUE(properties.goog_highpass_filter);
-    }
   }
 
   void CheckBoolDefaultsContentCapture(
@@ -234,10 +230,6 @@ class MediaStreamConstraintsUtilAudioTestBase : public SimTest {
     if (!Contains(exclude_audio_properties,
                   &AudioProcessingProperties::noise_suppression)) {
       EXPECT_FALSE(properties.noise_suppression);
-    }
-    if (!Contains(exclude_audio_properties,
-                  &AudioProcessingProperties::goog_highpass_filter)) {
-      EXPECT_FALSE(properties.goog_highpass_filter);
     }
   }
 
@@ -318,7 +310,6 @@ class MediaStreamConstraintsUtilAudioTestBase : public SimTest {
               properties.echo_cancellation_type);
     EXPECT_TRUE(properties.auto_gain_control);
     EXPECT_TRUE(properties.noise_suppression);
-    EXPECT_TRUE(properties.goog_highpass_filter);
 
     // The following are not audio processing.
     EXPECT_EQ(GetMediaStreamSource() != blink::kMediaStreamSourceDesktop,
@@ -612,7 +603,6 @@ TEST_P(MediaStreamConstraintsUtilAudioTest, SingleBoolConstraint) {
       kAudioProcessingConstraints = {
           &MediaTrackConstraintSetPlatform::auto_gain_control,
           &MediaTrackConstraintSetPlatform::noise_suppression,
-          &MediaTrackConstraintSetPlatform::goog_highpass_filter,
       };
 
   ASSERT_EQ(GetAudioProcessingProperties().size(),
@@ -1320,8 +1310,6 @@ TEST_P(MediaStreamConstraintsUtilAudioTest, EchoCancellationWithWebRtc) {
             IsDeviceCapture() ? value : false;
         EXPECT_EQ(enable_webrtc_audio_processing, properties.auto_gain_control);
         EXPECT_EQ(enable_webrtc_audio_processing, properties.noise_suppression);
-        EXPECT_EQ(enable_webrtc_audio_processing,
-                  properties.goog_highpass_filter);
 
         // The following are not audio processing.
         EXPECT_EQ(GetMediaStreamSource() != blink::kMediaStreamSourceDesktop,
@@ -1376,7 +1364,6 @@ TEST_P(MediaStreamConstraintsUtilAudioTest, EchoCancellationWithSystem) {
                   properties.echo_cancellation_type);
         EXPECT_EQ(value, properties.auto_gain_control);
         EXPECT_EQ(value, properties.noise_suppression);
-        EXPECT_EQ(value, properties.goog_highpass_filter);
 
         // The following are not audio processing.
         EXPECT_EQ(GetMediaStreamSource() != blink::kMediaStreamSourceDesktop,
@@ -1397,7 +1384,6 @@ TEST_P(MediaStreamConstraintsUtilAudioTest,
       kAudioProcessingConstraints = {
           &MediaTrackConstraintSetPlatform::auto_gain_control,
           &MediaTrackConstraintSetPlatform::noise_suppression,
-          &MediaTrackConstraintSetPlatform::goog_highpass_filter,
       };
 
   ASSERT_EQ(GetAudioProcessingProperties().size(),
@@ -1521,32 +1507,32 @@ TEST_P(MediaStreamConstraintsUtilAudioTest, AdvancedCompatibleConstraints) {
 // applied.
 TEST_P(MediaStreamConstraintsUtilAudioTest,
        AdvancedConflictingMiddleConstraints) {
-  constraint_factory_.AddAdvanced().goog_highpass_filter.SetExact(true);
+  constraint_factory_.AddAdvanced().noise_suppression.SetExact(true);
   auto& advanced2 = constraint_factory_.AddAdvanced();
-  advanced2.goog_highpass_filter.SetExact(false);
+  advanced2.noise_suppression.SetExact(false);
   auto result = SelectSettings();
   EXPECT_TRUE(result.HasValue());
   CheckProcessingType(result);
   CheckDeviceDefaults(result);
-  CheckBoolDefaults({}, {&AudioProcessingProperties::goog_highpass_filter},
+  CheckBoolDefaults({}, {&AudioProcessingProperties::noise_suppression},
                     result);
   CheckEchoCancellationTypeDefault(result);
-  EXPECT_TRUE(result.audio_processing_properties().goog_highpass_filter);
+  EXPECT_TRUE(result.audio_processing_properties().noise_suppression);
 }
 
 // Test that an advanced constraint set that contradicts a previous constraint
 // set with a boolean constraint is ignored.
 TEST_P(MediaStreamConstraintsUtilAudioTest, AdvancedConflictingLastConstraint) {
-  constraint_factory_.AddAdvanced().goog_highpass_filter.SetExact(true);
+  constraint_factory_.AddAdvanced().noise_suppression.SetExact(true);
   auto result = SelectSettings();
   EXPECT_TRUE(result.HasValue());
   CheckProcessingType(result);
   CheckDeviceDefaults(result);
-  CheckBoolDefaults({}, {&AudioProcessingProperties::goog_highpass_filter},
+  CheckBoolDefaults({}, {&AudioProcessingProperties::noise_suppression},
                     result);
   CheckEchoCancellationTypeDefault(result);
   // The fourth advanced set is ignored because it contradicts the second set.
-  EXPECT_TRUE(result.audio_processing_properties().goog_highpass_filter);
+  EXPECT_TRUE(result.audio_processing_properties().noise_suppression);
 }
 
 // NoDevices tests verify that the case with no devices is handled correctly.
@@ -1641,7 +1627,6 @@ TEST_P(MediaStreamConstraintsUtilAudioTest, SourceWithAudioProcessing) {
           EchoCancellationType::kEchoCancellationDisabled;
       properties.auto_gain_control = !properties.auto_gain_control;
       properties.noise_suppression = !properties.noise_suppression;
-      properties.goog_highpass_filter = !properties.goog_highpass_filter;
     }
 
     std::unique_ptr<ProcessedLocalAudioSource> source =
@@ -1653,7 +1638,6 @@ TEST_P(MediaStreamConstraintsUtilAudioTest, SourceWithAudioProcessing) {
         kAudioProcessingConstraints = {
             &MediaTrackConstraintSetPlatform::auto_gain_control,
             &MediaTrackConstraintSetPlatform::noise_suppression,
-            &MediaTrackConstraintSetPlatform::goog_highpass_filter,
         };
     ASSERT_EQ(kAudioProcessingConstraints.size(),
               GetAudioProcessingProperties().size());
@@ -1662,7 +1646,7 @@ TEST_P(MediaStreamConstraintsUtilAudioTest, SourceWithAudioProcessing) {
       constraint_factory_.Reset();
       (constraint_factory_.basic().*kAudioProcessingConstraints[i])
           .SetExact(properties.*GetAudioProcessingProperties()[i]);
-      auto result = SelectSettingsAudioCapture(
+      blink::AudioCaptureSettings result = SelectSettingsAudioCapture(
           source.get(), constraint_factory_.CreateMediaConstraints());
       EXPECT_TRUE(result.HasValue());
 
--- third_party/blink/renderer/modules/mediastream/user_media_client_test.cc
+++ third_party/blink/renderer/modules/mediastream/user_media_client_test.cc
@@ -1115,7 +1115,6 @@ TEST_F(UserMediaClientTest, DefaultConstraintsPropagate) {
             properties.echo_cancellation_type);
   EXPECT_TRUE(properties.auto_gain_control);
   EXPECT_TRUE(properties.noise_suppression);
-  EXPECT_TRUE(properties.goog_highpass_filter);
 
   EXPECT_TRUE(video_capture_settings.HasValue());
   EXPECT_EQ(video_capture_settings.Width(),
@@ -1167,7 +1166,6 @@ TEST_F(UserMediaClientTest, DefaultTabCapturePropagate) {
             properties.echo_cancellation_type);
   EXPECT_FALSE(properties.auto_gain_control);
   EXPECT_FALSE(properties.noise_suppression);
-  EXPECT_FALSE(properties.goog_highpass_filter);
 
   EXPECT_TRUE(video_capture_settings.HasValue());
   EXPECT_EQ(video_capture_settings.Width(), blink::kDefaultScreenCastWidth);
@@ -1217,7 +1215,6 @@ TEST_F(UserMediaClientTest, DefaultDesktopCapturePropagate) {
             properties.echo_cancellation_type);
   EXPECT_FALSE(properties.auto_gain_control);
   EXPECT_FALSE(properties.noise_suppression);
-  EXPECT_FALSE(properties.goog_highpass_filter);
 
   EXPECT_TRUE(video_capture_settings.HasValue());
   EXPECT_EQ(video_capture_settings.Width(), blink::kDefaultScreenCastWidth);
@@ -1280,7 +1277,6 @@ TEST_F(UserMediaClientTest, NonDefaultAudioConstraintsPropagate) {
             properties.echo_cancellation_type);
   EXPECT_FALSE(properties.auto_gain_control);
   EXPECT_FALSE(properties.noise_suppression);
-  EXPECT_FALSE(properties.goog_highpass_filter);
 }
 
 TEST_F(UserMediaClientTest, CreateWithMandatoryInvalidAudioDeviceId) {
