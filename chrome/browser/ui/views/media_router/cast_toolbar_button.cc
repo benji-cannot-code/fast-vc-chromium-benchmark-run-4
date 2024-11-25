5ed33d5267d60a3f3a2368a04470260499eb4512
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-11-25 17:28:44 +0000

    [a11y] Don't assume every ToolbarButton shows a menu as primary action
    This CL fixes a bug where the "Invoke" accessible action was not
    available for any ToolbarButton. The problem was that all ToolbarButton
    were setting the accessible HasPopup property to kMenu, indicating to
    accessibility APIs that they were not invocable and only supported the
    expand/collapse actions.
    
    This was certainly not true of all ToolbarButtons: the back/forward
    buttons, the reload button, and others, don't show a menu as a primary
    action. Instead, we're now only setting the HasPopup to kMenu on
    ToolbarButtons that do have one.
    
    Note: Some ToolbarButtons have a menu as a secondary action that can
    be triggered with the context menu key, shortcut, or right click. This
    CL doesn't break that -- those context menus are still accessible.
    
    Fixed: 378787479
    Change-Id: Ifa007d9794b3828137435aabab6a0e84b6af6a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032161
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387690}

--- chrome/browser/ui/views/media_router/cast_toolbar_button.cc
+++ chrome/browser/ui/views/media_router/cast_toolbar_button.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icon_types.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/controls/button/button_controller.h"
 
@@ -83,6 +84,10 @@ CastToolbarButton::CastToolbarButton(
   DCHECK(GetActionController());
   GetActionController()->AddObserver(this);
   SetVisible(GetActionController()->ShouldEnableAction());
+
+  if (menu_model()) {
+    GetViewAccessibility().SetHasPopup(ax::mojom::HasPopup::kMenu);
+  }
 }
 
 CastToolbarButton::~CastToolbarButton() {
