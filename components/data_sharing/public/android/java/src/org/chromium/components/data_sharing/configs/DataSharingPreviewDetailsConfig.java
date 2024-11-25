d7b1d40b97f836ac92ebf227e0cdf0a8395c2ba6
ssid <ssid@chromium.org>
2024-11-25 21:29:02 +0000

    [data sharing] Add config api for preview details
    Fetch all favicons and update the session with the images for preview.
    
    Low-Coverage-Reason: EXPERIMENTAL_CODE
    Bug: 380113830
    Change-Id: I60257f7faf1da8b62b51f0b64cb6ced63e35070b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043108
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387812}

---
+++ components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/DataSharingPreviewDetailsConfig.java
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.data_sharing.configs;
+
+import android.graphics.Bitmap;
+
+import java.util.List;
+
+/** Preview details images in join flow */
+public final class DataSharingPreviewDetailsConfig {
+    private final List<TabPreview> mTabPreviews;
+
+    /** Preview entry for a tab in the details view */
+    public static final class TabPreview {
+        // The display URL for the tab, see TabPreview.displayUrl.
+        public final String displayUrl;
+        // Favicon for the tab, can be null.
+        public final Bitmap favicon;
+
+        public TabPreview(String displayUrl, Bitmap favicon) {
+            this.displayUrl = displayUrl;
+            this.favicon = favicon;
+        }
+    }
+
+    private DataSharingPreviewDetailsConfig(List<TabPreview> tabPreviews) {
+        this.mTabPreviews = tabPreviews;
+    }
+
+    public List<TabPreview> getTabPreviews() {
+        return mTabPreviews;
+    }
+
+    // Builder class
+    public static class Builder {
+        private List<TabPreview> mTabPreviews;
+
+        /** The list of tab previews. */
+        public Builder setTabPreviews(List<TabPreview> tabPreviews) {
+            this.mTabPreviews = tabPreviews;
+            return this;
+        }
+
+        public DataSharingPreviewDetailsConfig build() {
+            return new DataSharingPreviewDetailsConfig(mTabPreviews);
+        }
+    }
+}
