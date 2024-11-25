8f7b63c43a52acafe4cbf006e56ec1db2d1086c3
Anantanarayanan G Iyengar <aiyengar@nvidia.com>
2024-11-25 22:00:41 +0000

    Don't DCHECK in WebMediaPlayerMSCompositor for LowLatencyVideoRendererAlgorithm frame drops
    In WebMediaPlayerMSCompositor::EnqueueFrame, the assertion ensuring the sizeof pending_frames_info_ matches the frames queued in rendering_frame_buffer_ does not apply to the LowLatencyVideoRendererAlgorithm.
    
    The LowLatencyVideoRendererAlgorithm does not drop frames during
    EnqueueFrame and instead handles frame dropping during Render. This behavior differs from the default VideoRendererAlgorithm, which
    can drop frames during both EnqueueFrame and Render.
    
    To address this, the code now skips the DCHECK when the rendering algorithm is LowLatency. The LowLatency algorithm is activated
    when the maximum_composition_delay_in_frames field is set in the
    video frame metadata
    (computed in VideoReceiveStream2::UpdatePlayoutDelays()).
    
    Changes:
    1. Skip the DCHECK in WebMediaPlayerMSCompositor::EnqueueFrame if the
       low latency algorithm is enabled.
    2. Added a getter for the renderer algorithm in the
       VideoRendererAlgorithmWrapper class.
    
    BUG=380487896
    
    Change-Id: I7ac9044b27d1aca7183b7880c38219bc64e3bcf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043766
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387838}

--- third_party/blink/renderer/modules/mediastream/video_renderer_algorithm_wrapper.h
+++ third_party/blink/renderer/modules/mediastream/video_renderer_algorithm_wrapper.h
@@ -22,6 +22,8 @@ namespace blink {
 
 class VideoRendererAlgorithmWrapper {
  public:
+  enum RendererAlgorithm { kDefault, kLowLatency };
+
   VideoRendererAlgorithmWrapper(
       const media::TimeSource::WallClockTimeCB& wall_clock_time_cb,
       media::MediaLog* media_log);
@@ -47,9 +49,9 @@ class VideoRendererAlgorithmWrapper {
 
   bool NeedsReferenceTime() const;
 
- private:
-  enum RendererAlgorithm { kDefault, kLowLatency };
+  RendererAlgorithm renderer_algorithm() const { return renderer_algorithm_; }
 
+ private:
   const media::TimeSource::WallClockTimeCB wall_clock_time_cb_;
   raw_ptr<media::MediaLog> media_log_;
   RendererAlgorithm renderer_algorithm_;
