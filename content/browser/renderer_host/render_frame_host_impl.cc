b5493f859d9e29c5d6ae177e9110f48797d346d8
Dave Tapuska <dtapuska@chromium.org>
2024-11-25 22:39:48 +0000

    [MPArch guest view] Enable load progress events for guest view
    Plumb the load progress events to the GuestPageHolder.
    
    Bug: 40202416
    Change-Id: I3c93b47aa4bfee5136e96c9bf5d6825650531ec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6044347
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387877}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -7739,9 +7739,7 @@ void RenderFrameHostImpl::DidChangeLoadProgress(double load_progress) {
 
   GetPage().set_load_progress(load_progress);
 
-  // Only dispatch LoadProgressChanged for the primary main frame.
-  if (IsInPrimaryMainFrame())
-    delegate_->DidChangeLoadProgressForPrimaryMainFrame();
+  delegate_->DidChangeLoadProgressForMainFrame(this);
 }
 
 void RenderFrameHostImpl::DidFinishLoad(const GURL& validated_url) {
