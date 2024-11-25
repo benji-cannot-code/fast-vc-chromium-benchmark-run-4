f4e79441bdc6e12555492870002aa248f82283e6
Daniel Cheng <dcheng@chromium.org>
2024-11-25 10:22:36 +0000

    Fix ContentBrowserClient lifetime in AlternativeErrorPageOverrideInfoBrowserTest
    Code in //content should be able to assume that a ContentBrowserClient
    doesn't disappear from underneath it. Tests that want to do this need to
    carefully manage state to avoid violating assumptions, e.g. the tests in
    ssl_browsertest.cc override ContentBrowserClient, but also make sure to
    close all browser windows before a test ends.
    
    However, in this case, the test does not even need to override
    ContentBrowserClient, so just use a test-only hook to call into the
    functionality under test. This avoids causing random failures in
    seemingly-unrelated code.
    
    Bug: 380222876
    Change-Id: I960112207e5e0bcc016caea67330a573cd7e2bbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043770
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387501}

--- chrome/browser/web_applications/alternative_error_page_override_info_browsertest.cc
+++ chrome/browser/web_applications/alternative_error_page_override_info_browsertest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
@@ -36,17 +35,17 @@ class AlternativeErrorPageOverrideInfoBrowserTest
   // alternative error page function.
   content::mojom::AlternativeErrorPageOverrideInfoPtr GetErrorPageInfo(
       std::string_view html) {
-    ChromeContentBrowserClient browser_client;
-    content::ScopedContentBrowserClientSetting setting(&browser_client);
 
     const GURL app_url = embedded_test_server()->GetURL(html);
     web_app::NavigateViaLinkClickToURLAndWait(browser(), app_url);
     web_app::test::InstallPwaForCurrentUrl(browser());
     content::BrowserContext* context = browser()->profile();
 
-    return browser_client.GetAlternativeErrorPageOverrideInfo(
-        app_url, /*render_frame_host=*/nullptr, context,
-        net::ERR_INTERNET_DISCONNECTED);
+    return content::GetContentClientForTesting()
+        ->browser()
+        ->GetAlternativeErrorPageOverrideInfo(
+            app_url, /*render_frame_host=*/nullptr, context,
+            net::ERR_INTERNET_DISCONNECTED);
   }
 
  protected:
@@ -70,17 +69,16 @@ class AlternativeErrorPageOverrideInfoBrowserTest
 // Testing url outside the scope of an installed app.
 IN_PROC_BROWSER_TEST_F(AlternativeErrorPageOverrideInfoBrowserTest,
                        NoManifest) {
-  ChromeContentBrowserClient browser_client;
-  content::ScopedContentBrowserClientSetting setting(&browser_client);
-
   ASSERT_TRUE(embedded_test_server()->Start());
   const GURL app_url = embedded_test_server()->GetURL("/simple.html");
   content::BrowserContext* context = browser()->profile();
 
   content::mojom::AlternativeErrorPageOverrideInfoPtr info =
-      browser_client.GetAlternativeErrorPageOverrideInfo(
-          app_url, /*render_frame_host=*/nullptr, context,
-          net::ERR_INTERNET_DISCONNECTED);
+      content::GetContentClientForTesting()
+          ->browser()
+          ->GetAlternativeErrorPageOverrideInfo(
+              app_url, /*render_frame_host=*/nullptr, context,
+              net::ERR_INTERNET_DISCONNECTED);
 
   // Expect mojom struct to be null.
   EXPECT_FALSE(info);
@@ -132,9 +130,6 @@ IN_PROC_BROWSER_TEST_F(AlternativeErrorPageOverrideInfoBrowserTest,
 // has no title
 IN_PROC_BROWSER_TEST_F(AlternativeErrorPageOverrideInfoBrowserTest,
                        ManifestWithNoAppShortNameOrAppNameOrTitle) {
-  ChromeContentBrowserClient browser_client;
-  content::ScopedContentBrowserClientSetting setting(&browser_client);
-
   ASSERT_TRUE(embedded_test_server()->Start());
   const GURL app_url = embedded_test_server()->GetURL("/title1.html");
   web_app::NavigateViaLinkClickToURLAndWait(browser(), app_url);
@@ -142,9 +137,11 @@ IN_PROC_BROWSER_TEST_F(AlternativeErrorPageOverrideInfoBrowserTest,
   content::BrowserContext* context = browser()->profile();
 
   content::mojom::AlternativeErrorPageOverrideInfoPtr info =
-      browser_client.GetAlternativeErrorPageOverrideInfo(
-          app_url, /*render_frame_host=*/nullptr, context,
-          net::ERR_INTERNET_DISCONNECTED);
+      content::GetContentClientForTesting()
+          ->browser()
+          ->GetAlternativeErrorPageOverrideInfo(
+              app_url, /*render_frame_host=*/nullptr, context,
+              net::ERR_INTERNET_DISCONNECTED);
 
   // Expect mojom struct customized with HTML page title.
   EXPECT_TRUE(info);
@@ -156,8 +153,6 @@ IN_PROC_BROWSER_TEST_F(AlternativeErrorPageOverrideInfoBrowserTest,
 IN_PROC_BROWSER_TEST_F(AlternativeErrorPageOverrideInfoBrowserTest,
                        ManifestWithIcon) {
   ASSERT_TRUE(embedded_test_server()->Start());
-  ChromeContentBrowserClient browser_client;
-  content::ScopedContentBrowserClientSetting setting(&browser_client);
   const GURL app_url = embedded_test_server()->GetURL(
       "/banners/"
       "manifest_test_page.html?manifest=manifest_one_icon.json");
@@ -177,9 +172,11 @@ IN_PROC_BROWSER_TEST_F(AlternativeErrorPageOverrideInfoBrowserTest,
           });
   WebAppIconWaiter(profile, app_id.value()).Wait();
   content::mojom::AlternativeErrorPageOverrideInfoPtr info =
-      browser_client.GetAlternativeErrorPageOverrideInfo(
-          app_url, /*render_frame_host=*/nullptr, profile,
-          net::ERR_INTERNET_DISCONNECTED);
+      content::GetContentClientForTesting()
+          ->browser()
+          ->GetAlternativeErrorPageOverrideInfo(
+              app_url, /*render_frame_host=*/nullptr, profile,
+              net::ERR_INTERNET_DISCONNECTED);
 
   // Expect mojom struct with everything (except the icon) filled out.
   EXPECT_TRUE(info);
