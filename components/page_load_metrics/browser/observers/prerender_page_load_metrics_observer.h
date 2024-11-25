2bf310e5a270edea795f2fa7f32721f60985f85b
Salma Elmahallawy <esalma@google.com>
2024-11-25 22:58:18 +0000

    Revert "Prerender: Add histogram to measure the duration between prerender activation and DOMContentLoaded event."
    This reverts commit cf2f8fcb9f2c91aef3732b70a031147f23df5e47.
    
    Reason for revert: PrerenderPageLoadMetricsObserverBrowserTest.DomLoadedAfterActivation
    failing on Step "browser_tests (retry shards) on Ubuntu-22.04" failing on builder "chrome/ci/linux-chromeos-chrome"
    
    Failure:
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FPrerenderPageLoadMetricsObserverBrowserTest.DomLoadedAfterActivation?q=VHASH%3A6d0a43807ac672d9
    
    Original change's description:
    > Prerender: Add histogram to measure the duration between prerender activation and DOMContentLoaded event.
    >
    > Now we're relying on DOMContentLoaded to perform layout and early
    > lifecycle update optimization. Unfortunately, it may not be a good
    > timing to do so:
    > 1. on some sites this event is usually deferred till activation.
    > 2. we may not have enough time to reach this stage.
    >
    > This CL adds a histogram to understand the percentage of pages that can perform the early lifecycle update. We also need to put this data
    > to prerender page load ukm.
    >
    > Bug: 40240492, 40243375, 379030045
    > Change-Id: Ic3d441f468449506a54dcc206e6c919d44e008ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036707
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1387633}
    
    Bug: 40240492, 40243375, 379030045
    Change-Id: I6768990f508ecad57016bacaf0c66290d22a1322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050098
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Owners-Override: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1387891}

--- components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.h
+++ components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
-#include "base/time/time.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
 #include "content/public/browser/preloading_trigger_type.h"
 
@@ -90,9 +89,6 @@ class PrerenderPageLoadMetricsObserver
   // Records loading status for an activated and loaded page.
   void MaybeRecordMainResourceLoadStatus();
 
-  void MaybeRecordDocumentLoadMetrics(
-      const page_load_metrics::mojom::PageLoadTiming& timing);
-
   // Helper function to concatenate the histogram name, the trigger type and the
   // embedder histogram suffix when the trigger type is kEmbedder.
   std::string AppendSuffix(const std::string& histogram_name) const;
@@ -105,9 +101,6 @@ class PrerenderPageLoadMetricsObserver
 
   // Set when the main resource of the main frame finishes loading.
   std::optional<net::Error> main_resource_load_status_;
-
-  // Updated upon activation.
-  std::optional<base::TimeDelta> navigation_to_activation_time_;
 
   // The type to trigger prerendering.
   std::optional<content::PreloadingTriggerType> trigger_type_;
