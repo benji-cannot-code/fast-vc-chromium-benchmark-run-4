226dcb33bde78cdd9156092827f2e20a8f74532a
Justin Green <greenjustin@google.com>
2024-11-25 22:03:19 +0000

    fake_libva_driver: Fix vaGetDisplayDRM in headless mode
    Currently, the libva fake backend still needs a DRM render node in order
    to function. It does not actually do anything with it, it just needs a
    valid file descriptor for which drmGetNodeTypeFromFd() won't return an
    error. This is a problem for truly headless systems that don't have any
    kind of graphics capability. So, as a workaround, we simply clobber
    drmGetNodeTypeFromFd() and make sure it returns DRM_NODE_RENDER.
    
    Test: Ran cros-codecs ccdec using the new .so files.
    Bug: b:376063687
    Change-Id: Ieedc8117248a750c4acd9e4abbeae84389ff287c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048937
    Reviewed-by: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Commit-Queue: Justin Green <greenjustin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1387841}

--- media/gpu/vaapi/test/fake_libva_driver/fake_gbm.cc
+++ media/gpu/vaapi/test/fake_libva_driver/fake_gbm.cc
@@ -24,6 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PAGE_SIZE 4096
 #define ALIGN(x, y) (((x + y - 1) / y) * y)
 
+#define DRM_NODE_RENDER 2
+
+int drmGetNodeTypeFromFd(int fd) {
+  return DRM_NODE_RENDER;
+}
+
 struct gbm_bo {
   struct gbm_import_fd_modifier_data meta;
 };
