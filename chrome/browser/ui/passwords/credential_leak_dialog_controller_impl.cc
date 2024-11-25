eb69fc09c717511ab8c32afe03fbb83bcb3e742d
Viktor Semeniuk <vsemeniuk@google.com>
2024-11-25 14:55:38 +0000

    Start password change upon clicking action button from a leak dialog
    This change allows to trigger password change from existing leak
    dialog. This is done temporarily until a new UI is implemented.
    
    Bug: 375565247
    Change-Id: Ic2c18bed71a8c1512dc9c2740d2495586ad92499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022838
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1387599}

--- chrome/browser/ui/passwords/credential_leak_dialog_controller_impl.cc
+++ chrome/browser/ui/passwords/credential_leak_dialog_controller_impl.cc
@@ -23,6 +23,9 @@ CredentialLeakDialogControllerImpl::CredentialLeakDialogControllerImpl(
       leak_dialog_traits_(CreateDialogTraits(details.leak_type)),
       url_(std::move(details.origin)),
       username_(std::move(details.username)),
+      password_(std::move(details.password)),
+      change_password_supported_(
+          password_manager::IsPasswordChangeSupported(details.leak_type)),
       metrics_recorder_(std::move(metrics_recorder)) {}
 
 CredentialLeakDialogControllerImpl::~CredentialLeakDialogControllerImpl() {
@@ -52,6 +55,9 @@ void CredentialLeakDialogControllerImpl::OnAcceptDialog() {
         LeakDialogDismissalReason::kClickedCheckPasswords);
     delegate_->NavigateToPasswordCheckup(
         password_manager::PasswordCheckReferrer::kPasswordBreachDialog);
+  } else if (change_password_supported_) {
+    // TODO(crbug.com/375564659): Remove once a new dedicated dialog is added.
+    delegate_->ChangePassword(url_, username_, password_);
   } else {
     metrics_recorder_->LogLeakDialogTypeAndDismissalReason(
         LeakDialogDismissalReason::kClickedOk);
