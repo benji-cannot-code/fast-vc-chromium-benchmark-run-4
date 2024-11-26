296a1fd0da0ad1bee033bfe844480e51e7ac1c84
Stepan Khapugin <stkhapugin@chromium.org>
2024-11-26 13:45:51 +0000

    [iOS] Clean up OmniboxLockIconEnabled flag.
    Fixed: 356623953
    Change-Id: I596f676eeaaaff9d460ee62eece44f9e9ff0b557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049096
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388194}

--- ios/chrome/browser/omnibox/ui_bundled/omnibox_ui_features.cc
+++ ios/chrome/browser/omnibox/ui_bundled/omnibox_ui_features.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/omnibox/common/omnibox_features.h"
 #import "ui/base/device_form_factor.h"
 
-BASE_FEATURE(kOmniboxLockIconEnabled,
-             "OmniboxLockIconEnabled",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kOmniboxActionsInSuggest,
              "OmniboxIOSActionsInSuggest",
              base::FEATURE_ENABLED_BY_DEFAULT);
