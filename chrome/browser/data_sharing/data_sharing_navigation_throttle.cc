eb99a4a52dedd2b7cdcb33680bbca26fca86c137
Min Qin <qinmin@chromium.org>
2024-11-26 01:10:27 +0000

    Require user gesture to prompt user to join a group
    Bug: 380509009
    Change-Id: I906eda01a69f24d43196b8978918dddc78086a98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039480
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387967}

--- chrome/browser/data_sharing/data_sharing_navigation_throttle.cc
+++ chrome/browser/data_sharing/data_sharing_navigation_throttle.cc
@@ -54,6 +54,10 @@ DataSharingNavigationThrottle::CheckIfShouldIntercept() {
     return PROCEED;
   }
 
+  if (!navigation_handle()->HasUserGesture()) {
+    return PROCEED;
+  }
+
   DataSharingService* data_sharing_service =
       DataSharingServiceFactory::GetForProfile(Profile::FromBrowserContext(
           navigation_handle()->GetWebContents()->GetBrowserContext()));
