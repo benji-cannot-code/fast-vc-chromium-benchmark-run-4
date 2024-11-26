844c2195a75366f337cbf3f489749ecb8e7f20b4
jhtin <jhtin@google.com>
2024-11-26 00:29:40 +0000

    specialized features: Add manta account capabilities check.
    Checks user account information like whether the user is a minor (will
    only be enabled if not a minor).
    
    Bug: b:367881438
    Change-Id: Iba284684acbf5d76cbc6a3d7422d0a23f255e128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038912
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Jon Htin <jhtin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1387935}

--- chromeos/ash/components/specialized_features/feature_access_checker_unittest.cc
+++ chromeos/ash/components/specialized_features/feature_access_checker_unittest.cc
@@ -15,8 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/signin/public/base/consent_level.h"
+#include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
+#include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -197,6 +200,48 @@ TEST_F(
       base::ToVector(
           FeatureAccessChecker(config, pref_, *GetIdentityManager()).Check()),
       IsEmpty());
+}
+
+TEST_F(FeatureAccessCheckerTest, MantaAccountCapabilitiesCheckPass) {
+  AccountInfo account = identity_test_environment_.MakePrimaryAccountAvailable(
+      "someone@gmail.com", signin::ConsentLevel::kSignin);
+  AccountCapabilitiesTestMutator mutator(&account.capabilities);
+  mutator.set_can_use_manta_service(true);
+  signin::UpdateAccountInfoForAccount(
+      identity_test_environment_.identity_manager(), account);
+  FeatureAccessConfig config = DefaultConfig();
+  config.requires_manta_account_capabilities = true;
+
+  EXPECT_THAT(
+      base::ToVector(
+          FeatureAccessChecker(config, pref_, *GetIdentityManager()).Check()),
+      IsEmpty());
+}
+
+TEST_F(FeatureAccessCheckerTest, MantaAccountCapabilitiesCheckFailIfFalse) {
+  AccountInfo account = identity_test_environment_.MakePrimaryAccountAvailable(
+      "someone@gmail.com", signin::ConsentLevel::kSignin);
+  AccountCapabilitiesTestMutator mutator(&account.capabilities);
+  mutator.set_can_use_manta_service(false);
+  FeatureAccessConfig config = DefaultConfig();
+  config.requires_manta_account_capabilities = true;
+
+  EXPECT_THAT(
+      base::ToVector(
+          FeatureAccessChecker(config, pref_, *GetIdentityManager()).Check()),
+      ElementsAre(kMantaAccountCapabilitiesCheckFailed));
+}
+
+TEST_F(FeatureAccessCheckerTest, MantaAccountCapabilitiesCheckFailIfUnset) {
+  identity_test_environment_.MakePrimaryAccountAvailable(
+      "someone@gmail.com", signin::ConsentLevel::kSignin);
+  FeatureAccessConfig config = DefaultConfig();
+  config.requires_manta_account_capabilities = true;
+
+  EXPECT_THAT(
+      base::ToVector(
+          FeatureAccessChecker(config, pref_, *GetIdentityManager()).Check()),
+      ElementsAre(kMantaAccountCapabilitiesCheckFailed));
 }
 
 }  // namespace
