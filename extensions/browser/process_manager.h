9d0c2440fcccc0fad7cb580bd3be1d2d021beb9c
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-11-26 00:58:25 +0000

    [Extensions Cleanup] Remove EventPageTracker
    This class was only ever implemented by ProcessManager, and had two
    methods WakeEventPage() and IsEventPageSuspended().
    
    IsEventPageSuspended() was only used in a single test file, and just
    checks that there is no background host for an extension. This can be
    trivially done by checking GetBackgroundHostForExtension() (which is
    what the implementation did).
    
    WakeEventPage() is called by numerous sites, but all of them do so by
    accessing the ProcessManager. No sites accept or use the class as the
    EventPageTracker, so having a separate class that the ProcessManager
    inherits from is unnecessary.
    
    Bug: None
    Change-Id: Ie83d9b5eb83dcb458a29ab51a46ea33797203af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6044582
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387960}

--- extensions/browser/process_manager.h
+++ extensions/browser/process_manager.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/service_worker_external_request_result.h"
 #include "content/public/browser/service_worker_external_request_timeout_type.h"
 #include "extensions/browser/activity.h"
-#include "extensions/browser/event_page_tracker.h"
 #include "extensions/browser/extension_host_observer.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "extensions/browser/service_worker/worker_id.h"
@@ -56,7 +55,6 @@ class ProcessManagerObserver;
 // track of split-mode extensions only.
 class ProcessManager : public KeyedService,
                        public ExtensionRegistryObserver,
-                       public EventPageTracker,
                        public content::DevToolsAgentHostObserver,
                        public content::RenderProcessHostObserver,
                        public ExtensionHostObserver {
@@ -235,10 +233,17 @@ class ProcessManager : public KeyedService,
   // Called on shutdown to close our extension hosts.
   void CloseBackgroundHosts();
 
-  // EventPageTracker implementation.
-  bool IsEventPageSuspended(const ExtensionId& extension_id) override;
+  // Wakes an extension's event page from a suspended state and calls
+  // `callback` after it is reactivated.
+  //
+  // `callback` will be passed true if the extension was reactivated
+  // successfully, or false if an error occurred.
+  //
+  // Returns true if a wake operation was scheduled successfully,
+  // or false if the event page was already awake.
+  // Callback will be run asynchronously if true, and never run if false.
   bool WakeEventPage(const ExtensionId& extension_id,
-                     base::OnceCallback<void(bool)> callback) override;
+                     base::OnceCallback<void(bool)> callback);
 
   // Sets the time in milliseconds that an extension event page can
   // be idle before it is shut down; must be > 0.
