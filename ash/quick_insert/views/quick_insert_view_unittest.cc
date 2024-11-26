f34cae5a4a9529cbe1bcf16304f1cef74bd5f012
Darren Shen <shend@chromium.org>
2024-11-26 06:01:52 +0000

    Keep the query the same when toggling the GIF button.
    Bug: b:368442959
    Change-Id: Ic33a82915caa79faea336bc565eb809a3bf3a064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038482
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388048}

--- ash/quick_insert/views/quick_insert_view_unittest.cc
+++ ash/quick_insert/views/quick_insert_view_unittest.cc
@@ -3265,6 +3265,7 @@ TEST_F(QuickInsertViewTest, CheckingGifButtonWithQueryShowsGifSearchResults) {
               /*has_more_results=*/false),
       });
 
+  EXPECT_EQ(query, u"a");
   EXPECT_TRUE(
       quick_insert_view->search_results_view_for_testing().GetVisible());
   EXPECT_THAT(
@@ -3311,6 +3312,7 @@ TEST_F(QuickInsertViewTest,
               /*has_more_results=*/false),
       });
 
+  EXPECT_EQ(query, u"a");
   EXPECT_TRUE(
       quick_insert_view->search_results_view_for_testing().GetVisible());
   EXPECT_THAT(
@@ -3351,6 +3353,7 @@ TEST_F(QuickInsertViewTest, UncheckingGifButtonSearchesNormally) {
                                           /*has_more_results=*/false),
       });
 
+  EXPECT_EQ(query, u"a");
   EXPECT_TRUE(
       quick_insert_view->search_results_view_for_testing().GetVisible());
   EXPECT_THAT(
