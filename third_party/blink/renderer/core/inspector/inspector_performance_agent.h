05ee46cc31903d6a7eccfdfa30589eb69af4e648
Alex Rudenko <alexrudenko@chromium.org>
2024-11-26 10:16:02 +0000

    De-alias protocol::Maybe in blink
    Bug: 40276207
    Change-Id: I1fe9a0189c4f98adcaf7bb912d2cdcdb0ff39308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049634
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388127}

--- third_party/blink/renderer/core/inspector/inspector_performance_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_performance_agent.h
@@ -43,7 +43,7 @@ class CORE_EXPORT InspectorPerformanceAgent final
   void Restore() override;
 
   // Performance protocol domain implementation.
-  protocol::Response enable(Maybe<String> time_domain) override;
+  protocol::Response enable(std::optional<String> time_domain) override;
   protocol::Response disable() override;
   protocol::Response setTimeDomain(const String& time_domain) override;
   protocol::Response getMetrics(
