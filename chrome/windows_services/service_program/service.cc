cdd681d4937dea900ad60a37d1c37eb07e2810f0
Greg Thompson <grt@chromium.org>
2024-11-26 16:30:54 +0000

    [windows services] Use atomic for cross-thread state
    A global variable is used to make a pointer to the Service instance
    available to the service main function. Use a std::atomic for this to
    ensure that writes to it are visible to all threads.
    
    Bug: 380943791
    Change-Id: I65239258ccabf14a511c575a26c74867bcc4344a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048759
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388270}

--- chrome/windows_services/service_program/service.cc
+++ chrome/windows_services/service_program/service.cc
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sddl.h>
 #include <wrl/module.h>
 
+#include <atomic>
 #include <string_view>
 #include <type_traits>
 #include <utility>
 
 #include "base/auto_reset.h"
+#include "base/check.h"
 #include "base/check_deref.h"
 #include "base/command_line.h"
 #include "base/containers/heap_array.h"
@@ -26,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-Service* g_instance = nullptr;
+std::atomic<Service*> g_instance(nullptr);
 
 // Command line switch "--console" runs the service interactively for debugging
 // purposes.
@@ -40,11 +42,15 @@ constexpr wchar_t kWindowsServiceName[] = L"";
 }  // namespace
 
 Service::Service(ServiceDelegate& delegate) : delegate_(delegate) {
-  CHECK_EQ(std::exchange(g_instance, this), nullptr);
+  Service* expected = nullptr;
+  CHECK(g_instance.compare_exchange_strong(expected, this,
+                                           std::memory_order_relaxed));
 }
 
 Service::~Service() {
-  CHECK_EQ(std::exchange(g_instance, nullptr), this);
+  Service* expected = this;
+  CHECK(g_instance.compare_exchange_strong(expected, nullptr,
+                                           std::memory_order_relaxed));
 }
 
 bool Service::InitWithCommandLine(const base::CommandLine* command_line) {
@@ -142,7 +148,7 @@ void Service::UnregisterClassObjects(base::HeapArray<DWORD>& cookies) {
 }
 
 Service& Service::GetInstance() {
-  return CHECK_DEREF(g_instance);
+  return CHECK_DEREF(g_instance.load(std::memory_order_relaxed));
 }
 
 int Service::RunAsService() {
