eedb006324971d0562918639d67e94aad4cb1168
Michelle Chen <michellegc@google.com>
2024-11-26 03:54:47 +0000

    scanner: Add methods to paint glow animation around capture region.
    Add painting and invalidation logic to show a glow animation around the
    capture region.
    
    This CL should not introduce any user visible changes, since a capture
    mode session currently never calls StartGlowAnimation. The logic to
    start / pause / remove the glow animation will be handled in followups.
    
    Bug: b:374381937
    Change-Id: I620a4119fceb7878a8f041ce97fc07c055c33376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6034071
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388024}

--- ash/capture_mode/capture_mode_session.h
+++ ash/capture_mode/capture_mode_session.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display_observer.h"
 #include "ui/events/event.h"
 #include "ui/events/event_handler.h"
+#include "ui/gfx/animation/animation_delegate.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/views/widget/unique_widget_ptr.h"
 #include "ui/views/widget/widget.h"
@@ -76,7 +77,8 @@ class ASH_EXPORT CaptureModeSession
       public aura::WindowObserver,
       public display::DisplayObserver,
       public FolderSelectionDialogController::Delegate,
-      public ui::ColorProviderSourceObserver {
+      public ui::ColorProviderSourceObserver,
+      public gfx::AnimationDelegate {
  public:
   // Centralized place to control the events, observe windows and create the
   // capture mode needed widgets including `capture_mode_bar_widget_`,
@@ -240,6 +242,9 @@ class ASH_EXPORT CaptureModeSession
 
   // ui::ColorProviderSourceObserver:
   void OnColorProviderChanged() override;
+
+  // gfx::AnimationDelegate:
+  void AnimationProgressed(const gfx::Animation* animation) override;
 
  private:
   friend class CaptureModeSettingsTestApi;
