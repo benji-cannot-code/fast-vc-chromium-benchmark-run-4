53512dcf4c72133a66a4094a94a589a351a996a7
Eleanor Lee <eleanorlee@google.com>
2024-11-26 18:25:55 +0000

    Remove #announceForAccessibility in TabModalPresenter
    announceForAccessibility is discouraged and should be used sparingly.
    TabModalPresenter uses #announceForAccessibility to announce the appearance of the dialog.
    This fix removes #announceForAccessibility from TabModalPresenter.
    
    Demo of #announceForAccessibility:
    https://screencast.googleplex.com/cast/NTYwMDcyNTAyNjQ3MTkzNnwyYzk3MWVmYy03Nw
    
    Demo after removing #announceForAccessibility:
    https://screencast.googleplex.com/cast/NjM3Mjk3MDIxMjYyMjMzNnw4MWI5NDhkOC1iNA
    
    Notice that the red disruptive announcement is now gone when the dialog appears, but the user experience remains the same.
    
    Bug: 376744135
    Change-Id: Id04df76f213b70ad5b5df393b71382dd226ed67a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050760
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388346}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/TabModalPresenter.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/TabModalPresenter.java
@@ -182,7 +182,6 @@ public abstract class TabModalPresenter extends ModalDialogManager.Presenter {
      */
     public void updateContainerHierarchy(boolean toFront) {
         if (toFront) {
-            mDialogView.announceForAccessibility(getContentDescription(getDialogModel()));
             mDialogView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
             mDialogView.requestFocus();
             if (mFocusDialog) {
