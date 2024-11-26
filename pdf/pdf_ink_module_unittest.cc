a25952e6a397c47f7f483fc141e993e61829a2dd
Andy Phan <andyphan@chromium.org>
2024-11-26 01:24:12 +0000

    [PDF Ink Signatures] Add ApplyStrokeWithMouseAtMouseDownPoint() in tests
    Add helper method `ApplyStrokeWithWithMouseAtMouseDownPoint()` for
    testing. This is a shortcut method for applying a stroke with mouse at a
    point on the PDF page and is easier to read than using
    `ApplyStrokeWithMouseAtPoints()` directly.
    
    This method will be used more frequently in future metrics tests.
    
    Bug: 380433757
    Change-Id: I82800524490b0be85d8a29e300565797043cbfb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6044010
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387977}

--- pdf/pdf_ink_module_unittest.cc
+++ pdf/pdf_ink_module_unittest.cc
@@ -761,6 +761,11 @@ class PdfInkModuleStrokeTest : public PdfInkModuleTest {
     }
   }
 
+  void ApplyStrokeWithMouseAtMouseDownPoint() {
+    ApplyStrokeWithMouseAtPoints(
+        kMouseDownPoint, base::span_from_ref(kMouseDownPoint), kMouseDownPoint);
+  }
+
   void ApplyStrokeWithTouchAtPoints(
       base::span<const gfx::PointF> touch_start_points,
       std::vector<base::span<const gfx::PointF>> all_touch_move_points,
@@ -1161,8 +1166,7 @@ TEST_F(PdfInkModuleStrokeTest, EraseStroke) {
 
   // Stroke with the eraser tool.
   SelectEraserToolOfSize(3.0f);
-  ApplyStrokeWithMouseAtPoints(
-      kMouseDownPoint, base::span_from_ref(kMouseDownPoint), kMouseDownPoint);
+  ApplyStrokeWithMouseAtMouseDownPoint();
 
   // Now there are no visible strokes left.
   EXPECT_TRUE(VisibleStrokeInputPositions().empty());
@@ -1171,8 +1175,7 @@ TEST_F(PdfInkModuleStrokeTest, EraseStroke) {
   EXPECT_THAT(updated_thumbnail_page_indices, ElementsAre(0, 0));
 
   // Stroke again. The stroke that have already been erased should stay erased.
-  ApplyStrokeWithMouseAtPoints(
-      kMouseDownPoint, base::span_from_ref(kMouseDownPoint), kMouseDownPoint);
+  ApplyStrokeWithMouseAtMouseDownPoint();
 
   // Still no visible strokes.
   EXPECT_TRUE(VisibleStrokeInputPositions().empty());
@@ -1190,8 +1193,7 @@ TEST_F(PdfInkModuleStrokeTest, EraseOnPageWithoutStrokes) {
 
   // Stroke with the eraser tool when there are no strokes on the page.
   SelectEraserToolOfSize(3.0f);
-  ApplyStrokeWithMouseAtPoints(
-      kMouseDownPoint, base::span_from_ref(kMouseDownPoint), kMouseDownPoint);
+  ApplyStrokeWithMouseAtMouseDownPoint();
 
   // Verify there are still no visible strokes and StrokeFinished() never got
   // called.
@@ -1413,8 +1415,7 @@ TEST_F(PdfInkModuleStrokeTest, EraseStrokeWithTouch) {
   EXPECT_THAT(updated_thumbnail_page_indices, ElementsAre(0, 0));
 
   // Stroke again with the mouse gets the same results.
-  ApplyStrokeWithMouseAtPoints(
-      kMouseDownPoint, base::span_from_ref(kMouseDownPoint), kMouseDownPoint);
+  ApplyStrokeWithMouseAtMouseDownPoint();
 
   // Still no visible strokes.
   EXPECT_TRUE(VisibleStrokeInputPositions().empty());
