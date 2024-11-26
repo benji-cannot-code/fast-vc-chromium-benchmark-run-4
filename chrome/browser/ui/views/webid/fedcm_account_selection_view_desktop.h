226f88a323d416a6b14bcbff7ae6428b6f6aabcf
Erik Chen <erikchen@chromium.org>
2024-11-26 22:14:54 +0000

    Simplify FedCmAccountSelectionView (part 12/N)
    This CL switches FedCm to use CLIENT_OWNS_WIDGET along with synchronous
    destruction. This fixes production bugs related to the widget being
    asynchronously destroyed after the FedCmAccountSelectionView is
    destroyed.
    
    Bug: 377803489
    Change-Id: Ia2780da849086bc5b9d675c25bca7c61eae87431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043775
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388489}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.h
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/views/input_event_activation_protector.h"
-#include "ui/views/widget/widget_observer.h"
 
 using IdentityProviderDataPtr = scoped_refptr<content::IdentityProviderData>;
 using IdentityRequestAccountPtr =
@@ -47,8 +46,7 @@ class TabInterface;
 class FedCmAccountSelectionView : public AccountSelectionView,
                                   public FedCmModalDialogView::Observer,
                                   content::WebContentsObserver,
-                                  public PictureInPictureOcclusionObserver,
-                                  public views::WidgetObserver {
+                                  public PictureInPictureOcclusionObserver {
  public:
   // safe_zone_diameter/icon_size as defined in
   // https://www.w3.org/TR/appmanifest/#icon-masks
@@ -200,6 +198,12 @@ class FedCmAccountSelectionView : public AccountSelectionView,
   void WillDetach(tabs::TabInterface* tab,
                   tabs::TabInterface::DetachReason reason);
 
+  // Synchronously closes dialog_widget_.
+  // This method can result in synchronous destruction of `this`.
+  // Public for testing.
+  // TODO(https://crbug.com/377803489): Make private again.
+  void CloseWidget(views::Widget::ClosedReason reason);
+
  protected:
   friend class FedCmAccountSelectionViewBrowserTest;
 
@@ -224,10 +228,12 @@ class FedCmAccountSelectionView : public AccountSelectionView,
   // Widget to control the dialog i.e. hide, show, add observer etc.
   // TODO(https://crbug.com/377803489): Make private again.
   // Protected for testing.
-  base::WeakPtr<views::Widget> dialog_widget_;
+  std::unique_ptr<views::Widget> dialog_widget_;
 
   // This view controls the contents of the dialog_widget_. Conceptually there
-  // should be a view if and only if there is a widget.
+  // is a view if and only if there is a widget. The two are constructed
+  // together and destroyed together. `dialog_widget_` owns
+  // `account_selection_view_`.
   // Protected for testing.
   raw_ptr<AccountSelectionViewBase> account_selection_view_;
 
@@ -353,9 +359,6 @@ class FedCmAccountSelectionView : public AccountSelectionView,
     kMaxValue = kTapScrim
   };
 
-  // views::WidgetObserver:
-  void OnWidgetDestroying(views::Widget* widget) override;
-
   // Called when the tab's WebContents is discarded.
   void WillDiscardContents(tabs::TabInterface* tab,
                            content::WebContents* old_contents,
@@ -376,14 +379,9 @@ class FedCmAccountSelectionView : public AccountSelectionView,
   // Returns the SheetType to be used for metrics reporting.
   AccountSelectionView::SheetType GetSheetType();
 
-  // Notify the delegate that the widget was closed with reason
-  // `dismiss_reason`.
-  void OnDismiss(
-      content::IdentityRequestDialogController::DismissReason dismiss_reason);
-
-  // Resets `account_selection_view_`. Typically, to recreate it later to show a
-  // different kind of dialog. Virtual for testing purposes.
-  virtual void MaybeResetAccountSelectionView();
+  // Closes widget if it exists and never notifies.
+  // Virtual for testing purposes.
+  virtual void CloseWidgetNoNotify();
 
   // Returns whether an IDP sign-in pop-up window is currently open.
   bool IsIdpSigninPopupOpen();
