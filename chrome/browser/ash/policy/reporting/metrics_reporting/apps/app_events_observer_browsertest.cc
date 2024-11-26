5f3ea1736de651624f31e19f67ce77866a76134e
Awad Osman <awado@google.com>
2024-11-26 18:40:01 +0000

    [Code Health] Clean up kEnableAppEventsObserver feature flag
    This feature has been enabled by default for quite a while and the
    OWNER has confirmed we can remove it.
    
    Bug: 362247074
    Change-Id: I4f53b1fd57f7ed3aa02a6f10709a84729c0a9069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6052431
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388353}

--- chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_events_observer_browsertest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_events_observer_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_ash.h"
@@ -95,8 +94,7 @@ bool IsMetricEventOfType(MetricEventType metric_event_type,
 class AppEventsObserverBrowserTest
     : public ::policy::DevicePolicyCrosBrowserTest {
  protected:
-  AppEventsObserverBrowserTest()
-      : scoped_feature_list_(kEnableAppEventsObserver) {
+  AppEventsObserverBrowserTest() {
     crypto_home_mixin_.MarkUserAsExisting(affiliation_mixin_.account_id());
     crypto_home_mixin_.ApplyAuthConfig(
         affiliation_mixin_.account_id(),
@@ -145,7 +143,6 @@ class AppEventsObserverBrowserTest
   ::policy::DevicePolicyCrosTestHelper test_helper_;
   ::policy::AffiliationMixin affiliation_mixin_{&mixin_host_, &test_helper_};
   ::ash::CryptohomeMixin crypto_home_mixin_{&mixin_host_};
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(AppEventsObserverBrowserTest, PRE_ReportInstalledApp) {
