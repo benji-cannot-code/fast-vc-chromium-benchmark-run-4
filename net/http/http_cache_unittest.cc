5b5f913ba7a33ebed61be206180338618f49b869
Tsuyoshi Horo <horo@chromium.org>
2024-11-26 08:54:52 +0000

    Prioritize main frame navigation resources in Simple Cache
    Before this CL, SimpleCache used a generic eviction policy for the HTTP
    cache, removing resources based on a formula that combines their size
    and time since last access.
    
    ```
    sort_value = (time since last access in seconds) * (size in bytes + 512)
    ```
    
    This policy doesn't distinguish between resource types.
    
    This CL prioritizes main frame navigation resources in Simple Cache. If
    a main frame navigation resource has been used within the last day, its
    `sort_value` is divided by 10. This makes it less likely to be evicted
    from the cache.
    
    To keep the hint for the prioritization in memory, this CL utilizes the
    in-memory byte hint field introduced for https://crbug.com/40524133.
    
    Note that we set the prioritization flag HINT_HIGH_PRIORITY even when we
    set HINT_UNUSABLE_PER_CACHING_HEADERS. It is because we want to increase
    the cache hit rate of back forward navigation. But the back forward
    navigation request has the SKIP_CACHE_VALIDATION flag. So the unusable
    entry with the HINT_UNUSABLE_PER_CACHING_HEADERS flag can be used for
    the back forward navigation request.
    
    This prioritization is implemented behind SimpleCachePrioritizedCaching
    feature flag which is disabled by default.
    
    Bug: 380957432
    Change-Id: If133f2138eb64f8089c07ea482755c618a137bc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048384
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388097}

--- net/http/http_cache_unittest.cc
+++ net/http/http_cache_unittest.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_status_flags.h"
 #include "net/cert/x509_certificate.h"
 #include "net/disk_cache/disk_cache.h"
+#include "net/disk_cache/memory_entry_data_hints.h"
 #include "net/http/http_byte_range.h"
 #include "net/http/http_cache_transaction.h"
 #include "net/http/http_request_headers.h"
@@ -14104,6 +14105,29 @@ TEST_F(HttpCacheTest, SecurityHeadersAreCopiedToConditionalizedResponse) {
       "cross-origin");
 
   EXPECT_EQ(304, response.headers->response_code());
+}
+
+// This test verifies that the PrioritizeCaching flag is not set by default.
+TEST_F(HttpCacheTest, PrioritizeCachingFlagNotSetByDefault) {
+  MockHttpCache cache;
+  ScopedMockTransaction transaction(kSimpleGET_Transaction);
+  MockHttpRequest request(transaction);
+  RunTransactionTestWithRequest(cache.http_cache(), transaction, request,
+                                nullptr);
+  EXPECT_EQ(cache.backend()->GetEntryInMemoryData(request.CacheKey()), 0);
+}
+
+// This test verifies that the PrioritizeCaching flag is set for main frame
+// navigation requests.
+TEST_F(HttpCacheTest, PrioritizeCachingFlagSetForMainFrameNavigationRequest) {
+  MockHttpCache cache;
+  ScopedMockTransaction transaction(kSimpleGET_Transaction);
+  MockHttpRequest request(transaction);
+  request.is_main_frame_navigation = true;
+  RunTransactionTestWithRequest(cache.http_cache(), transaction, request,
+                                nullptr);
+  EXPECT_EQ(cache.backend()->GetEntryInMemoryData(request.CacheKey()),
+            HINT_HIGH_PRIORITY);
 }
 
 }  // namespace net
