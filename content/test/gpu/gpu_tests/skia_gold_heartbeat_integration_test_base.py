7abc5bef1088a61a9c0a158692b5c589a0c8b830
Brian Sheedy <bsheedy@chromium.org>
2024-11-26 18:13:20 +0000

    Scale pixel test timeouts for ASan
    Automatically scales pixel and expected color message loop timeouts for
    ASan tests to account for the extra slowness ASan can introduce.
    
    Bug: 380101867
    Change-Id: I2b2b85d0be2fa34353608c156360439551aa2ca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050898
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388339}

--- content/test/gpu/gpu_tests/skia_gold_heartbeat_integration_test_base.py
+++ content/test/gpu/gpu_tests/skia_gold_heartbeat_integration_test_base.py
@@ -28,6 +28,7 @@ DEFAULT_HEARTBEAT_TIMEOUT = 15
 SLOW_HEARTBEAT_MULTIPLIER = 8
 DEFAULT_CONTROLLER_MESSAGE_TIMEOUT = 5
 SLOW_CONTROLLER_MESSAGE_MULTIPLIER = 5
+ASAN_MESSAGE_LOOP_MULTIPLIER = 1.5
 
 DEFAULT_GLOBAL_TIMEOUT = 300
 
@@ -240,6 +241,7 @@ class SkiaGoldHeartbeatIntegrationTestBase(sgitb.SkiaGoldIntegrationTestBase):
           instead of the ones associated with the primary tab.
       loop_state: The LoopState to use. Will be modified in place.
     """
+    test_timeout = test_timeout * self._GetMessageLoopTimeoutMultiplier()
     tab = tab_data.tab
     websocket_server = tab_data.websocket_server
     start_time = time.time()
@@ -297,6 +299,12 @@ class SkiaGoldHeartbeatIntegrationTestBase(sgitb.SkiaGoldIntegrationTestBase):
         logging.warning('Could not retrieve messages from test page.')
 
   # pylint: enable=too-many-branches
+
+  def _GetMessageLoopTimeoutMultiplier(self) -> float:
+    multiplier = 1
+    if self._is_asan:
+      multiplier = ASAN_MESSAGE_LOOP_MULTIPLIER
+    return multiplier
 
   def _GetHeartbeatTimeout(self) -> int:
     multiplier = 1
