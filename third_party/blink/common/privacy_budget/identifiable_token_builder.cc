0dbed9481142a393b9fe1f76acd7e9bca550b310
Peter Kasting <pkasting@chromium.org>
2024-11-26 18:49:52 +0000

    Remove usage of base::make_span(): third_party/blink/common
    Replace with span() CTAD use, or more targeted helpers.
    
    Bug: 341907909
    Change-Id: Ifaae90fb3c08da42f8e4f547f59b74c17261d649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050620
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388357}

--- third_party/blink/common/privacy_budget/identifiable_token_builder.cc
+++ third_party/blink/common/privacy_budget/identifiable_token_builder.cc
@@ -125,7 +125,7 @@ void IdentifiableTokenBuilder::DigestBlock(ConstFullBlockSpan block) {
   // study has more-or-less accepted for some metrics and is dealt with during
   // the analysis phase.
   chaining_value_ =
-      base::legacy::CityHash64WithSeed(base::make_span(block), chaining_value_);
+      base::legacy::CityHash64WithSeed(base::span(block), chaining_value_);
 }
 
 size_t IdentifiableTokenBuilder::PartialSize() const {
@@ -136,7 +136,7 @@ size_t IdentifiableTokenBuilder::PartialSize() const {
 IdentifiableTokenBuilder::ConstFullBlockSpan
 IdentifiableTokenBuilder::TakeCompletedBlock() {
   DCHECK(position_ == partial_.end());
-  auto buffer = base::make_span(partial_);
+  auto buffer = base::span(partial_);
   position_ = partial_.begin();
   return buffer;
 }
