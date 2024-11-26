421a04e88380e612ce7a9f7f46128aed88e811f4
Evelynn Kaplan <evelynn.kaplan@microsoft.com>
2024-11-26 21:42:18 +0000

    [views-ax] Migrate accessible name in ash::LogoutButtonTray
    This CL is the fifth of a multi-CL set that will migrate how
    TrayBackgroundView and its derived classes set their accessible names.
    
    This CL migrates the accessible name setter for ash::LogoutButtonTray
    from being calculated only when requested in
    TrayBackgroundView::GetAccessibleNodeData ("pull" system) to
    updating the value in the ViewAccessibility cache whenever it
    changes ("push"). The accessible name of LogoutButtonTray is dependent
    on the text of a child button View, which can be changed by other
    Views. This CL updates the LogoutButtonTray's accessible name whenever
    the value of the button_'s text changes by adding a callback. This CL
    also adds a relevant test.
    
    This CL sets up LogoutButtonTray in preparation for a future CL where
    TrayBackgroundView::GetAccessibleNodeData will be deleted.
    
    As part of the ViewsAX project, we are removing GetAccessibleNodeData
    from Views across chromium:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit
    
    Future CLs will handle more complex cases where the accessible
    name for the derived class needs to be updated dynamically.
    The final CL will remove TrayBackgroundView::GetAccessibleNode data
    entirely.
    
    Bug: 325137417
    Change-Id: I973c609fe07ad9df877af793a0a91756581ccef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033673
    Commit-Queue: Evelynn Kaplan <evelynn.kaplan@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388469}

--- ash/system/session/logout_button_tray.cc
+++ ash/system/session/logout_button_tray.cc
@@ -50,6 +50,7 @@ LogoutButtonTray::LogoutButtonTray(Shelf* shelf)
   set_use_bounce_in_animation(false);
 
   SetFocusBehavior(FocusBehavior::NEVER);
+  SubscribeCallbacksForAccessibility();
 }
 
 LogoutButtonTray::~LogoutButtonTray() {
@@ -91,6 +92,11 @@ void LogoutButtonTray::OnActiveUserPrefServiceChanged(PrefService* prefs) {
   UpdateLogoutDialogDuration();
 }
 
+std::u16string LogoutButtonTray::GetLoginStatusString() {
+  LoginStatus login_status = shelf()->GetStatusAreaWidget()->login_status();
+  return user::GetLocalizedSignOutStringForStatus(login_status, false);
+}
+
 void LogoutButtonTray::OnThemeChanged() {
   TrayBackgroundView::OnThemeChanged();
   const auto* color_provider = GetColorProvider();
@@ -121,10 +127,6 @@ void LogoutButtonTray::HideBubbleWithView(const TrayBubbleView* bubble_view) {}
 
 void LogoutButtonTray::HideBubble(const TrayBubbleView* bubble_view) {}
 
-std::u16string LogoutButtonTray::GetAccessibleNameForTray() {
-  return button_->GetText();
-}
-
 void LogoutButtonTray::HandleLocaleChange() {
   UpdateButtonTextAndImage();
 }
@@ -137,16 +139,13 @@ void LogoutButtonTray::UpdateVisibility() {
 }
 
 void LogoutButtonTray::UpdateButtonTextAndImage() {
-  LoginStatus login_status = shelf()->GetStatusAreaWidget()->login_status();
-  const std::u16string title =
-      user::GetLocalizedSignOutStringForStatus(login_status, false);
   if (shelf()->IsHorizontalAlignment()) {
-    button_->SetText(title);
+    button_->SetText(GetLoginStatusString());
     button_->SetImageModel(views::Button::STATE_NORMAL, ui::ImageModel());
     button_->SetMinSize(gfx::Size(0, kTrayItemSize));
   } else {
     button_->SetText(std::u16string());
-    button_->GetViewAccessibility().SetName(title);
+    button_->GetViewAccessibility().SetName(GetLoginStatusString());
     button_->SetImageModel(
         views::Button::STATE_NORMAL,
         ui::ImageModel::FromVectorIcon(
@@ -156,6 +155,22 @@ void LogoutButtonTray::UpdateButtonTextAndImage() {
     button_->SetMinSize(gfx::Size(kTrayItemSize, kTrayItemSize));
   }
   UpdateVisibility();
+}
+
+void LogoutButtonTray::OnButtonTextChangedCallback(
+    ax::mojom::StringAttribute attribute,
+    const std::optional<std::string>& name) {
+  if (name.has_value()) {
+    GetViewAccessibility().SetName(name.value());
+  }
+}
+
+void LogoutButtonTray::SubscribeCallbacksForAccessibility() {
+  button_text_changed_subscription_ =
+      button_->GetViewAccessibility().AddStringAttributeChangedCallback(
+          ax::mojom::StringAttribute::kName,
+          base::BindRepeating(&LogoutButtonTray::OnButtonTextChangedCallback,
+                              base::Unretained(this)));
 }
 
 void LogoutButtonTray::ButtonPressed() {
