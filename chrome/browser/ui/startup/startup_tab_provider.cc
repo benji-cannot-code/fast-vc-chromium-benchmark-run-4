011e415e940e9f14bbef082e4374fdc2d967e6c7
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-11-26 18:26:55 +0000

    Skip chrome://welcome requested from startup tabs
    Required to be able to fully unlaunch this deprecated UI
    
    Bug: 380397532
    Change-Id: Id674a493e2597b86ba4b89cea022cf8543bc8f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6031887
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388347}

--- chrome/browser/ui/startup/startup_tab_provider.cc
+++ chrome/browser/ui/startup/startup_tab_provider.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_util.h"
 #include "base/threading/thread_restrictions.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
 #include "components/url_formatter/url_fixer.h"
 #include "content/public/browser/child_process_security_policy.h"
+#include "content/public/common/url_constants.h"
 #include "net/base/url_util.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -275,6 +277,21 @@ StartupTabs StartupTabProviderImpl::GetInitialPrefsTabsForState(
     for (GURL url : first_run_tabs) {
       if (url.host_piece() == kNewTabUrlHost) {
         url = GURL(chrome::kChromeUINewTabURL);
+      }
+      if (url.SchemeIs(content::kChromeUIScheme) &&
+          (url.host_piece() == chrome::kChromeUIWelcomeHost
+#if BUILDFLAG(IS_WIN)
+           || url.host_piece() == chrome::kChromeUIWelcomeWin10Host
+#endif
+           )) {
+        // These URLs are still referenced from some of the installers. As
+        // these chrome UIs are removed, avoid opening tabs pointing to
+        // invalid pages.
+        // TODO(crbug.com/379999327): Cleanup this block around Chrome M143
+        // or if it stops being reached.
+        base::UmaHistogramBoolean("Startup.StartupTabs.IsWelcomePageSkipped",
+                                  true);
+        continue;
       }
       tabs.emplace_back(url);
     }
