67fe483dbcecd182ba825eba373df357d4d03301
Tanu Jain <tanujain@microsoft.com>
2024-11-26 03:02:43 +0000

    [Editing] Fire selectionchange event on TextControl for backspace input
    The selectionchange event does not get fired when the backspace key
    deletes text inside a textcontrol element. According to the
    specification [1], when an input or textarea element provide a text
    selection and its selection changes (in either extent or direction),
    the user agent must schedule a selectionchange event on the element.
    This change ensures that the event will be scheduled as per the
    specification.
    
    [1] https://w3c.github.io/selection-api/#selectionchange-event
    
    Bug: 41321247
    Change-Id: Id6307b9baaee6480ba607ae8576959e2635e7b16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012498
    Commit-Queue: Tanu Jain <tanujain@microsoft.com>
    Reviewed-by: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388005}

--- third_party/blink/renderer/core/editing/selection_editor.cc
+++ third_party/blink/renderer/core/editing/selection_editor.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/editing/selection_adjuster.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/html/forms/text_control_element.h"
 
 namespace blink {
 
@@ -160,9 +161,18 @@ void SelectionEditor::DidFinishTextChange(const Position& new_anchor,
   selection_.anchor_ = new_anchor;
   selection_.focus_ = new_focus;
   selection_.ResetDirectionCache();
+
+  // See: https://w3c.github.io/selection-api/#selectionchange-event
   if (RuntimeEnabledFeatures::ScheduleSelectionChangeOnBackspaceEnabled()) {
-    GetDocument().ScheduleSelectionchangeEvent();
+    TextControlElement* text_control =
+        EnclosingTextControl(GetSelectionInDOMTree().Anchor());
+    if (text_control && !text_control->IsInShadowTree()) {
+      text_control->ScheduleSelectionchangeEvent();
+    } else {
+      GetDocument().ScheduleSelectionchangeEvent();
+    }
   }
+
   MarkCacheDirty();
   DidFinishDOMMutation();
 }
