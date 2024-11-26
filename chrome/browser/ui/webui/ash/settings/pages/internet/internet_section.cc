e536ac119ef950e02078ecca7eb18fcf90b1c7b5
Wes Okuhara <wesokuhara@google.com>
2024-11-26 23:37:35 +0000

    CrOS Settings: Cleanup revamp code in internet section
    The revamp feature is fully launched and any feature-related code can
    be cleaned up.
    
    Bug: b:370837149
    Test: browser_tests
    Change-Id: I8c752be754b0e68901e5ec88264ef9e6cd3533ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050601
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388531}

--- chrome/browser/ui/webui/ash/settings/pages/internet/internet_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/internet/internet_section.cc
@@ -782,9 +782,6 @@ InternetSection::InternetSection(Profile* profile,
 InternetSection::~InternetSection() = default;
 
 void InternetSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
-  const bool isRevampEnabled =
-      ash::features::IsOsSettingsRevampWayfindingEnabled();
-
   webui::LocalizedString kLocalizedStrings[] = {
       {"deviceInfoPopupMenuItemTitle",
        IDS_SETTINGS_DEVICE_INFO_POPUP_MENU_ITEM_TITLE},
@@ -845,8 +842,7 @@ void InternetSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
       {"knownNetworksAll", IDS_SETTINGS_INTERNET_KNOWN_NETWORKS_ALL},
       {"knownNetworksButton", IDS_SETTINGS_INTERNET_KNOWN_NETWORKS_BUTTON},
       {"knownNetworksMessage",
-       isRevampEnabled ? IDS_OS_SETTINGS_REVAMP_INTERNET_KNOWN_NETWORKS_MESSAGE
-                       : IDS_SETTINGS_INTERNET_KNOWN_NETWORKS_MESSAGE},
+       IDS_OS_SETTINGS_REVAMP_INTERNET_KNOWN_NETWORKS_MESSAGE},
       {"knownNetworksPreferred",
        IDS_SETTINGS_INTERNET_KNOWN_NETWORKS_PREFFERED},
       {"knownNetworksMenuAddPreferred",
@@ -936,8 +932,7 @@ void InternetSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
        IDS_SETTINGS_INTERNET_NETWORK_SECTION_PROXY_ACCESSIBILITY_LABEL},
       {"networkShared", IDS_SETTINGS_INTERNET_NETWORK_SHARED},
       {"networkSharedOwner",
-       isRevampEnabled ? IDS_OS_SETTINGS_REVAMP_INTERNET_NETWORK_SHARED_OWNER
-                       : IDS_SETTINGS_INTERNET_NETWORK_SHARED_OWNER},
+       IDS_OS_SETTINGS_REVAMP_INTERNET_NETWORK_SHARED_OWNER},
       {"networkSharedNotOwner", IDS_SETTINGS_INTERNET_NETWORK_SHARED_NOT_OWNER},
       {"networkVpnBuiltin", IDS_NETWORK_TYPE_VPN_BUILTIN},
       {"networkOutOfRange", IDS_SETTINGS_INTERNET_WIFI_NETWORK_OUT_OF_RANGE},
