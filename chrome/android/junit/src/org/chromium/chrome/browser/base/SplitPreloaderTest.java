22d135661ce1d254090493ace0102928ef4db78f
Sam Maier <smaier@chromium.org>
2024-11-26 15:16:11 +0000

    isolated splits: always use Application to create context for split
    In order to avoid deadlock, we have to split out the normal
    App.createContextForSplit into 2 functions, one which does not wait,
    which is the one we use in the preload, and one which does wait for the
    preload to finish, which we use for everything else (and which should be functionally unchanged).
    
    Low-Coverage-Reason: not adding tests for application class
    Bug: 378723187
    Change-Id: I8e33b0292d653fa885aff1368a93517655cf393f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043188
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388231}

--- chrome/android/junit/src/org/chromium/chrome/browser/base/SplitPreloaderTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/base/SplitPreloaderTest.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
+import org.chromium.base.BundleUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -90,7 +91,7 @@ public class SplitPreloaderTest {
         }
     }
 
-    private static class OnCompleteTracker implements SplitPreloader.OnComplete {
+    private static class PreloadHooksTracker implements SplitPreloader.PreloadHooks {
         private SplitContext mBackgroundContext;
         private SplitContext mUiContext;
 
@@ -104,6 +105,11 @@ public class SplitPreloaderTest {
         public void runInUiThread(Context context) {
             assertNull(mUiContext);
             mUiContext = (SplitContext) context;
+        }
+
+        @Override
+        public Context createIsolatedSplitContext(String name) {
+            return BundleUtils.createIsolatedSplitContext(name);
         }
 
         public SplitContext getBackgroundContext() {
@@ -154,7 +160,7 @@ public class SplitPreloaderTest {
     public void testPreload_withOnComplete_splitInstalled() {
         initSplits(SPLIT_A);
 
-        OnCompleteTracker tracker = new OnCompleteTracker();
+        PreloadHooksTracker tracker = new PreloadHooksTracker();
         mPreloader.preload(SPLIT_A, tracker);
         mPreloader.wait(SPLIT_A);
 
@@ -170,7 +176,7 @@ public class SplitPreloaderTest {
     public void testPreload_multipleWaitCalls() {
         initSplits(SPLIT_A);
 
-        OnCompleteTracker tracker = new OnCompleteTracker();
+        PreloadHooksTracker tracker = new PreloadHooksTracker();
         mPreloader.preload(SPLIT_A, tracker);
         mPreloader.wait(SPLIT_A);
         mPreloader.wait(SPLIT_A);
@@ -186,10 +192,10 @@ public class SplitPreloaderTest {
     public void testPreload_withOnComplete_multipleSplitsInstalled() {
         initSplits(SPLIT_A, SPLIT_B);
 
-        OnCompleteTracker trackerA = new OnCompleteTracker();
+        PreloadHooksTracker trackerA = new PreloadHooksTracker();
         mPreloader.preload(SPLIT_A, trackerA);
 
-        OnCompleteTracker trackerB = new OnCompleteTracker();
+        PreloadHooksTracker trackerB = new PreloadHooksTracker();
         mPreloader.preload(SPLIT_B, trackerB);
         mPreloader.wait(SPLIT_A);
         mPreloader.wait(SPLIT_B);
@@ -219,7 +225,7 @@ public class SplitPreloaderTest {
         CallbackHelper helper = new CallbackHelper();
         mPreloader.preload(
                 SPLIT_A,
-                new SplitPreloader.OnComplete() {
+                new SplitPreloader.PreloadHooks() {
                     @Override
                     public void runImmediatelyInBackgroundThread(Context context) {
                         backgroundContextHolder[0] = context;
@@ -229,6 +235,11 @@ public class SplitPreloaderTest {
                     @Override
                     public void runInUiThread(Context context) {
                         uiContextHolder[0] = context;
+                    }
+
+                    @Override
+                    public Context createIsolatedSplitContext(String name) {
+                        return BundleUtils.createIsolatedSplitContext(name);
                     }
                 });
         helper.waitForOnly();
