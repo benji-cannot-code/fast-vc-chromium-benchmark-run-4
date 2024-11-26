c75a3bf14b295f4c2ee7904b06e28a2537d7665f
Jun Zou <junzou@chromium.org>
2024-11-26 00:35:22 +0000

    Disable the message "Preparing AI" when model is not available
    Bug: 380137667
    Change-Id: I1ab9c729b4cfad4115ed3efb2a35bc6e0bf6461d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049436
    Auto-Submit: Jun Zou <junzou@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Jun Zou <junzou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1387939}

--- ui/webui/resources/cr_components/history_embeddings/history_embeddings.ts
+++ ui/webui/resources/cr_components/history_embeddings/history_embeddings.ts
@@ -235,12 +235,11 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
       case AnswerStatus.kExecutionCanceled:
       case AnswerStatus.kUnanswerable:
       case AnswerStatus.kFiltered:
+      case AnswerStatus.kModelUnavailable:
         // Still loading or answer section is not displayed.
         return undefined;
       case AnswerStatus.kSuccess:
         return this.searchResult_.answer;
-      case AnswerStatus.kModelUnavailable:
-        return this.i18n('historyEmbeddingsAnswererErrorModelUnavailable');
       case AnswerStatus.kExecutionFailure:
         return this.i18n('historyEmbeddingsAnswererErrorTryAgain');
       default:
@@ -320,8 +319,7 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
       return false;
     }
 
-    return this.searchResult_.answerStatus === AnswerStatus.kModelUnavailable ||
-        this.searchResult_.answerStatus === AnswerStatus.kExecutionFailure;
+    return this.searchResult_.answerStatus === AnswerStatus.kExecutionFailure;
   }
 
   private onFeedbackSelectedOptionChanged_(
@@ -511,7 +509,8 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
       // loading state to show.
       return this.searchResult_.answerStatus !== AnswerStatus.kUnspecified &&
           this.searchResult_.answerStatus !== AnswerStatus.kUnanswerable &&
-          this.searchResult_.answerStatus !== AnswerStatus.kFiltered;
+          this.searchResult_.answerStatus !== AnswerStatus.kFiltered &&
+          this.searchResult_.answerStatus !== AnswerStatus.kModelUnavailable;
     }
   }
 
