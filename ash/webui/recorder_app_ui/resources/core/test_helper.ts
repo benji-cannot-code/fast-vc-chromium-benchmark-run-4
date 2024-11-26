6d511ecd7f10c4c4dafb3a1aaee59e1d00f02af3
hsuanling <hsuanling@google.com>
2024-11-26 08:47:21 +0000

    recorder: Set transcription language in test helper
    Set the transcription language when configures settings so that
    transcription language won't be null if transcription is force enabled.
    
    Bug: b:378796006
    Test: tast run $DUT recorderapp.RecordingMeetPower.record_transcription
    Change-Id: I52d4d461a014d93a852d786359d8b5fc12358752
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6047786
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Jennifer Ling <hsuanling@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388091}

--- ash/webui/recorder_app_ui/resources/core/test_helper.ts
+++ ash/webui/recorder_app_ui/resources/core/test_helper.ts
@@ -87,6 +87,8 @@ export class TestHelper {
       }
       if (config.transcriptionForceEnabled) {
         s.transcriptionEnabled = TranscriptionEnableState.ENABLED;
+        // TODO: b/380800050 - Support testing different languages.
+        s.transcriptionLanguage = LanguageCode.EN_US;
       }
     });
   }
@@ -108,6 +110,7 @@ export class TestHelper {
         durationMs: durationMs,
         recordedAt: Date.now(),
         powers: powers,
+        // TODO: b/380800050 - Support testing different languages.
         transcription: tokens !== undefined ?
           new Transcription(tokens, LanguageCode.EN_US) :
           null,
@@ -120,7 +123,7 @@ export class TestHelper {
    * Installs the model used for transcription.
    */
   static installTranscriptionModel(): void {
-    // TODO(hsuanling): Support testing different languages
+    // TODO: b/380800050 - Support testing different languages.
     void usePlatformHandler().installSoda(LanguageCode.EN_US);
   }
 
@@ -130,7 +133,7 @@ export class TestHelper {
    * @return Boolean indicating if the transcription model is installed.
    */
   static isTranscriptionModelInstalled(): boolean {
-    // TODO(hsuanling): Support testing different languages
+    // TODO: b/380800050 - Support testing different languages.
     const state = usePlatformHandler().getSodaState(LanguageCode.EN_US).value;
     return state.kind === 'installed';
   }
