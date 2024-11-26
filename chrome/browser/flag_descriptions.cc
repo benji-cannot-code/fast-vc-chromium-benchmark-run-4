d48d0cceaf61aa34cf9ff9a9918dc4dd59adfc64
Yifan Luo <lyf@google.com>
2024-11-26 14:27:24 +0000

    [Site Settings] Add feature flag for radio-buttons.
    New Chrome feature flag "permission-site-settings-radio-button"
    is introduced, because we are aimed to replace the toggles with radio
    buttons for site settings.
    
    For CPSS related features, we plan to use two separate radio button
    groups in parallel other than nested radio buttons.
    
    Bug: 41494473
    Change-Id: Icb711ce3dc179258f17b1c35725f9c0cd29a5838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993495
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388211}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3046,6 +3046,11 @@ const char kPermissionQuietChipDescription[] =
     "right-hand side address bar icon for quiet permission prompts. Requires "
     "chrome://flags/#quiet-notification-prompts to be enabled.";
 
+const char kPermissionSiteSettingsRadioButtonName[] =
+    "Permission radio buttons in Site Settings";
+const char kPermissionSiteSettingsRadioButtonDescription[] =
+    "Enables radio buttons for permissions in SiteSettings";
+
 const char kShowRelatedWebsiteSetsPermissionGrantsName[] =
     "Show permission grants from Related Website Sets";
 const char kShowRelatedWebsiteSetsPermissionGrantsDescription[] =
