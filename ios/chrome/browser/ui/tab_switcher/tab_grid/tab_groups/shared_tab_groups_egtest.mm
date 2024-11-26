20eb5a63e3480433d47b86f1af4f80944873807b
Ewann Pelle <ewannpv@chromium.org>
2024-11-26 13:49:57 +0000

    Adds missing flag
    bug: 375564771
    Change-Id: Ia0d331e5fd61984be692744684470f0db68792cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049002
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388198}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/shared_tab_groups_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/shared_tab_groups_egtest.mm
@@ -63,6 +63,7 @@ AppLaunchConfiguration SharedTabGroupAppLaunchConfiguration(
   config.features_enabled.push_back(kTabGroupsIPad);
   config.features_enabled.push_back(kModernTabStrip);
   config.features_enabled.push_back(kTabGroupSync);
+  config.features_enabled.push_back(kTabGroupIndicator);
   config.features_enabled.push_back(shared_tab_group_flavor);
   // Add the flag to use FakeTabGroupSyncService.
   config.additional_args.push_back(
