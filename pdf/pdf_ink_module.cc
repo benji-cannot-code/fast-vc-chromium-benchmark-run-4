9009ac7115cf302a7539a1c18fce2e523c5c41c4
Andy Phan <andyphan@chromium.org>
2024-11-26 22:13:59 +0000

    [PDF Ink Signatures] Add stroke brush size metric
    Add a metric to track the brush sizes of draw strokes and erase strokes.
    
    Bug: 380433757
    Change-Id: I8006fe955c8646c3cd953c5024d48ca09a45fa9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6044360
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388488}

--- pdf/pdf_ink_module.cc
+++ pdf/pdf_ink_module.cc
@@ -537,7 +537,7 @@ bool PdfInkModule::FinishStroke(const gfx::PointF& position,
   bool undo_redo_success = undo_redo_model_.FinishDraw();
   CHECK(undo_redo_success);
 
-  ReportDrawStroke(state.brush_type);
+  ReportDrawStroke(state.brush_type, GetDrawingBrush().ink_brush());
 
   // Reset `state` now that the stroke operation is done.
   state.inputs.clear();
@@ -609,7 +609,7 @@ bool PdfInkModule::FinishEraseStroke(const gfx::PointF& position) {
       client_->UpdateThumbnail(page_index);
     }
 
-    ReportEraseStroke();
+    ReportEraseStroke(eraser_size_);
   }
 
   // Reset `state` now that the erase operation is done.
