d81476e30686a2180c9aa6d9de7d36e95491fc61
Tomasz Jurkiewicz <tju@google.com>
2024-11-26 13:40:55 +0000

    Ignore checking test format if the repository is not avaialable.
    NO_IFTTT=False positive
    
    Bug: 373667523
    Change-Id: I269f61f8cc445836a40da529acd655a84fdca655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049000
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388190}

--- components/supervised_user/test_support/account_repository_internal_unittest.cc
+++ components/supervised_user/test_support/account_repository_internal_unittest.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace supervised_user {
 namespace {
 TEST(AccountRepositoryInternalTest, ProdConfigIsParseable) {
+  if (!test_accounts::IsDefaultAccountRepositoryAvailable()) {
+    GTEST_SKIP() << "Default repository not available";
+  }
+
   TestAccountRepository repository;
   std::optional<test_accounts::Family> family =
       repository.GetRandomFamilyByFeature(test_accounts::Feature::REGULAR);
