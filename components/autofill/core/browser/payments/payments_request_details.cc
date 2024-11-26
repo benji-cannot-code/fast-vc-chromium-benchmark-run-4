43bfd901010eca3358afd981b1c100315e54b59d
Viplav Kadam <viplavkadam@google.com>
2024-11-26 22:21:14 +0000

    [BNPL][Chrome] Add GetDetailsForCreateBnplPaymentInstrumentRequest call
    This CL implements the
    GetDetailsForCreateBnplPaymentInstrumentRequest and integrates it
    with the Payments Network Interface. This request is used to determine
    whether the user meets the conditions required to link a BNPL partner.
    
    Bug: 378518543, 356443046
    Change-Id: Ide589f43b72f82cfb7debb7a5e51dea138055072
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042731
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388496}

--- components/autofill/core/browser/payments/payments_request_details.cc
+++ components/autofill/core/browser/payments/payments_request_details.cc
@@ -158,6 +158,17 @@ SelectChallengeOptionRequestDetails::SelectChallengeOptionRequestDetails(
 SelectChallengeOptionRequestDetails::~SelectChallengeOptionRequestDetails() =
     default;
 
+UpdateVirtualCardEnrollmentRequestDetails::
+    UpdateVirtualCardEnrollmentRequestDetails() = default;
+UpdateVirtualCardEnrollmentRequestDetails::
+    UpdateVirtualCardEnrollmentRequestDetails(
+        const UpdateVirtualCardEnrollmentRequestDetails&) = default;
+UpdateVirtualCardEnrollmentRequestDetails&
+UpdateVirtualCardEnrollmentRequestDetails::operator=(
+    const UpdateVirtualCardEnrollmentRequestDetails&) = default;
+UpdateVirtualCardEnrollmentRequestDetails::
+    ~UpdateVirtualCardEnrollmentRequestDetails() = default;
+
 GetDetailsForEnrollmentRequestDetails::GetDetailsForEnrollmentRequestDetails() =
     default;
 GetDetailsForEnrollmentRequestDetails::GetDetailsForEnrollmentRequestDetails(
@@ -183,15 +194,23 @@ UploadCardResponseDetails& UploadCardResponseDetails::operator=(
     UploadCardResponseDetails&&) = default;
 UploadCardResponseDetails::~UploadCardResponseDetails() = default;
 
-UpdateVirtualCardEnrollmentRequestDetails::
-    UpdateVirtualCardEnrollmentRequestDetails() = default;
-UpdateVirtualCardEnrollmentRequestDetails::
-    UpdateVirtualCardEnrollmentRequestDetails(
-        const UpdateVirtualCardEnrollmentRequestDetails&) = default;
-UpdateVirtualCardEnrollmentRequestDetails&
-UpdateVirtualCardEnrollmentRequestDetails::operator=(
-    const UpdateVirtualCardEnrollmentRequestDetails&) = default;
-UpdateVirtualCardEnrollmentRequestDetails::
-    ~UpdateVirtualCardEnrollmentRequestDetails() = default;
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails::
+    GetDetailsForCreateBnplPaymentInstrumentRequestDetails() = default;
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails::
+    GetDetailsForCreateBnplPaymentInstrumentRequestDetails(
+        const GetDetailsForCreateBnplPaymentInstrumentRequestDetails&) =
+        default;
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails&
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails::operator=(
+    const GetDetailsForCreateBnplPaymentInstrumentRequestDetails& other) =
+    default;
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails::
+    GetDetailsForCreateBnplPaymentInstrumentRequestDetails(
+        GetDetailsForCreateBnplPaymentInstrumentRequestDetails&&) = default;
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails&
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails::operator=(
+    GetDetailsForCreateBnplPaymentInstrumentRequestDetails&&) = default;
+GetDetailsForCreateBnplPaymentInstrumentRequestDetails::
+    ~GetDetailsForCreateBnplPaymentInstrumentRequestDetails() = default;
 
 }  // namespace autofill::payments
