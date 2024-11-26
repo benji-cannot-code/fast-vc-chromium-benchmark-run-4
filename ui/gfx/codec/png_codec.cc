f3f76f9c45e95cb793396310d4fba55b0b9cb0c1
Lukasz Anforowicz <lukasza@chromium.org>
2024-11-26 18:44:39 +0000

    [rust png] `#include` `SkPngRustDecoder.h` from the new location.
    http://review.skia.org/922436 has moved `SkPngRustDecoder.h` from
    `experimental/rust_png/` into `experimental/rust_png/decoder/`
    directory.  This CL updates the Chromium `#include`s to use the new
    location.  The old location will be deleted in
    http://review.skia.org/924317.
    
    Bug: chromium:379312510
    Change-Id: I68c44768109e4f52a8a679b54122cae5926dc382
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050534
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Urvang Joshi <urvang@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388354}

--- ui/gfx/codec/png_codec.cc
+++ ui/gfx/codec/png_codec.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 
 #if BUILDFLAG(SKIA_BUILD_RUST_PNG)
-#include "third_party/skia/experimental/rust_png/SkPngRustDecoder.h"
+#include "third_party/skia/experimental/rust_png/decoder/SkPngRustDecoder.h"
 #endif
 
 namespace gfx {
