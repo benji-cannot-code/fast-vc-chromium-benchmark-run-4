77462e3d3f964e91ad1dd1eec01ed7d5ce679302
Alex Rudenko <alexrudenko@chromium.org>
2024-11-27 09:50:21 +0000

    De-alias protocol::Maybe in chrome/
    Bug: 40276207
    Change-Id: I2ef8ee106cd49d4da94429e1369637845ed85bcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049109
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388703}

--- chrome/browser/devtools/protocol/cast_handler_unittest.cc
+++ chrome/browser/devtools/protocol/cast_handler_unittest.cc
@@ -97,7 +97,7 @@ class CastHandlerTest : public ChromeRenderViewHostTestHarness {
               }
               return true;
             }));
-    handler_->Enable(protocol::Maybe<std::string>());
+    handler_->Enable(std::nullopt);
   }
 
   std::unique_ptr<CastHandler> handler_;
