a76abc8b52caca0a887bce144d387fa8dbb5fac4
Maksim Sisov <msisov@igalia.com>
2024-11-27 10:05:56 +0000

    ozone/wayland: remove zcr stylus code
    This supported lacros and is based on the downstream protocol.
    
    Bug: 374244479, 373971535
    Change-Id: Ied2eaccf3b894aded7dbe6ed58fe40d461486a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048997
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388718}

--- ui/ozone/platform/wayland/test/test_touch.h
+++ ui/ozone/platform/wayland/test/test_touch.h
@@ -17,8 +17,6 @@ namespace wl {
 
 extern const struct wl_touch_interface kTestTouchImpl;
 
-class TestZcrTouchStylus;
-
 class TestTouch : public ServerObject {
  public:
   explicit TestTouch(wl_resource* resource);
@@ -27,14 +25,6 @@ class TestTouch : public ServerObject {
   TestTouch& operator=(const TestTouch&) = delete;
 
   ~TestTouch() override;
-
-  void set_touch_stylus(TestZcrTouchStylus* touch_stylus) {
-    touch_stylus_ = touch_stylus;
-  }
-  TestZcrTouchStylus* touch_stylus() const { return touch_stylus_; }
-
- private:
-  raw_ptr<TestZcrTouchStylus, DanglingUntriaged> touch_stylus_ = nullptr;
 };
 
 }  // namespace wl
--- ui/ozone/platform/wayland/test/test_wayland_server_thread.h
+++ ui/ozone/platform/wayland/test/test_wayland_server_thread.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/test/test_viewporter.h"
 #include "ui/ozone/platform/wayland/test/test_wp_linux_drm_syncobj.h"
 #include "ui/ozone/platform/wayland/test/test_wp_pointer_gestures.h"
-#include "ui/ozone/platform/wayland/test/test_zcr_stylus.h"
 #include "ui/ozone/platform/wayland/test/test_zcr_text_input_extension.h"
 #include "ui/ozone/platform/wayland/test/test_zwp_linux_explicit_synchronization.h"
 #include "ui/ozone/platform/wayland/test/test_zwp_text_input_manager.h"
@@ -237,7 +236,6 @@ class TestWaylandServerThread : public base::Thread,
   TestZXdgOutputManager zxdg_output_manager_;
   MockXdgShell xdg_shell_;
   ::testing::NiceMock<MockZcrColorManagerV1> zcr_color_manager_v1_;
-  TestZcrStylus zcr_stylus_;
   TestZcrTextInputExtensionV1 zcr_text_input_extension_v1_;
   TestZwpTextInputManagerV1 zwp_text_input_manager_v1_;
   TestZwpTextInputManagerV3 zwp_text_input_manager_v3_;
