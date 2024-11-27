3bd2f2b815662b08429a0f6eeadf5fea07648766
Paul Adedeji <pauladedeji@google.com>
2024-11-27 21:42:21 +0000

    [wallpaper search] Support showing only 3 tiles of results.
    The backend currently only returns 3 search results, so
    this CL:
    1. Changes the loading gradient to only show 3 tiles
    2. Removes the requirement for two rows of results, by removing
    the empty tile logic from wallpaper search results.
    
    UX requirements are detailed in the bug's description.
    
    2 results*: screen/5WLmXsoa8zuQCt6
    3 results: screencast/cast/NjYzMzkwMjg5NzYyNzEzNnw5ZjdmYzVkOS05Yg
    4 results*: screencast/cast/NTYxOTE1NDAwOTcxODc4NHw1OWQ4ZjIwZi1jMg
    
    *faked backend response to return more or less images
    
    Change-Id: Id308bea1f0ae0f5561379c1fd5f26551cbee3911
    Bug: 379922063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039996
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389058}

--- chrome/browser/resources/side_panel/customize_chrome/wallpaper_search/wallpaper_search.ts
+++ chrome/browser/resources/side_panel/customize_chrome/wallpaper_search/wallpaper_search.ts
@@ -150,7 +150,6 @@ export class WallpaperSearchElement extends WallpaperSearchElementBase {
       descriptorD_: {type: Array},
       errorState_: {type: Object},
       emptyHistoryContainers_: {type: Object},
-      emptyResultContainers_: {type: Object},
       expandedCategories_: {type: Object},
       loading_: {type: Boolean},
       history_: {type: Array},
@@ -181,7 +180,6 @@ export class WallpaperSearchElement extends WallpaperSearchElementBase {
   private descriptors_: Descriptors|null = null;
   protected descriptorD_: string[] = DESCRIPTOR_D_VALUE.map(value => value.hex);
   protected emptyHistoryContainers_: number[] = [];
-  protected emptyResultContainers_: number[] = [];
   private errorCallback_: (() => void)|undefined;
   protected errorState_: ErrorState|null = null;
   private expandedCategories_: {[categoryIndex: number]: boolean} = {};
@@ -711,7 +709,6 @@ export class WallpaperSearchElement extends WallpaperSearchElementBase {
     this.selectedDescriptorA_ = selectedDescriptorA;
     this.loading_ = true;
     this.results_ = [];
-    this.emptyResultContainers_ = [];
     announcer.announce(this.i18n('wallpaperSearchLoadingA11yMessage'));
     const descriptors: ResultDescriptors = {
       subject: this.selectedDescriptorA_!,
@@ -742,7 +739,6 @@ export class WallpaperSearchElement extends WallpaperSearchElementBase {
           }
           recordStatusChange(status);
           this.selectedFeedbackOption_ = CrFeedbackOption.UNSPECIFIED;
-          this.emptyResultContainers_ = this.calculateEmptyTiles(results);
         }
       }
     } else {
@@ -806,10 +802,6 @@ export class WallpaperSearchElement extends WallpaperSearchElementBase {
 
   protected shouldShowFeedbackButtons_() {
     return !this.loading_ && this.results_.length > 0;
-  }
-
-  protected shouldShowGrid_(): boolean {
-    return this.results_.length > 0 || this.emptyResultContainers_.length > 0;
   }
 }
 
