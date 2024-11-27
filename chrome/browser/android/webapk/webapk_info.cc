62e90871052983b25d981395d598d906851776da
Sorin Jianu <sorin@chromium.org>
2024-11-27 20:06:47 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/android/webapk
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=yfriedman@chromium.org
    
    Bug: 381119547
    Change-Id: If22fa1e954326fd8d883354da301d3885d71973a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054127
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389021}

--- chrome/browser/android/webapk/webapk_info.cc
+++ chrome/browser/android/webapk/webapk_info.cc
@@ -54,7 +54,7 @@ WebApkInfo::WebApkInfo(std::string name,
       is_backing_browser(is_backing_browser),
       update_status(std::move(update_status)) {}
 
-WebApkInfo::~WebApkInfo() {}
+WebApkInfo::~WebApkInfo() = default;
 
 WebApkInfo& WebApkInfo::operator=(WebApkInfo&& rhs) noexcept = default;
 WebApkInfo::WebApkInfo(WebApkInfo&& other) noexcept = default;
--- chrome/browser/android/webapk/webapk_installer_browsertest.cc
+++ chrome/browser/android/webapk/webapk_installer_browsertest.cc
@@ -101,12 +101,12 @@ class TestWebApkInstaller : public WebApkInstaller {
 // Runs the WebApkInstaller installation process/update and blocks till done.
 class WebApkInstallerRunner {
  public:
-  WebApkInstallerRunner() {}
+  WebApkInstallerRunner() = default;
 
   WebApkInstallerRunner(const WebApkInstallerRunner&) = delete;
   WebApkInstallerRunner& operator=(const WebApkInstallerRunner&) = delete;
 
-  ~WebApkInstallerRunner() {}
+  ~WebApkInstallerRunner() = default;
 
   void RunInstallWebApk(std::unique_ptr<WebApkInstaller> installer,
                         content::WebContents* web_contents,
@@ -160,7 +160,7 @@ class WebApkInstallerRunner {
 // synchronously.
 class UpdateRequestStorer {
  public:
-  UpdateRequestStorer() {}
+  UpdateRequestStorer() = default;
 
   UpdateRequestStorer(const UpdateRequestStorer&) = delete;
   UpdateRequestStorer& operator=(const UpdateRequestStorer&) = delete;
@@ -225,7 +225,7 @@ class WebApkInstallerBrowserTest : public AndroidBrowserTest {
       std::unique_ptr<net::test_server::HttpResponse>(void)>
       WebApkResponseBuilder;
 
-  WebApkInstallerBrowserTest() {}
+  WebApkInstallerBrowserTest() = default;
 
   WebApkInstallerBrowserTest(const WebApkInstallerBrowserTest&) = delete;
   WebApkInstallerBrowserTest& operator=(const WebApkInstallerBrowserTest&) =
