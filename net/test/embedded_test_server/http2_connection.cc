7eea2c7b839cf4633b02bfb520285a10f4d18077
Nidhi Jaju <nidhijaju@chromium.org>
2024-11-27 06:23:30 +0000

    Roll src/net/third_party/quiche/src/ 18e5a7319..7c4b99323 (10 commits)
    https://quiche.googlesource.com/quiche.git/+log/18e5a7319873..7c4b993237be
    
    $ git log 18e5a7319..7c4b99323 --date=short --no-merges --format='%ad %ae %s'
    2024-11-25 vasilvv Fix standalone build.
    2024-11-25 birenroy Adds unit test cases covering the case where a content-length overshoot causes a PROTOCOL_ERROR.
    2024-11-25 birenroy Migrates adapter unit tests away from deprecated SubmitRequest/SubmitResponse methods.
    2024-11-25 wub Set client CID in the packets sent by QuicBufferedPacketStore.
    2024-11-25 birenroy Removes DataFrameSource arguments from Http2Adapter methods and implementations.
    2024-11-25 birenroy Removes DataFrameSource interface methods and related utilities.
    2024-11-25 quiche-dev Enabling rolled out flags.
    2024-11-25 birenroy Removes DataFrameSource-based DATA frame manipulation from OgHttp2Session.
    2024-11-25 vasilvv Move MoqtSessionPeer into its own file.
    2024-11-25 birenroy Removes DataFrameSource-based DATA frame manipulation from NgHttp2Adapter.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Remove DataFrameSource calls
    
    Change-Id: Ia8a00552aa5e80aa7c111acaf84acfca1d428391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6051276
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388646}

--- net/test/embedded_test_server/http2_connection.cc
+++ net/test/embedded_test_server/http2_connection.cc
@@ -66,9 +66,9 @@ class Http2Connection::ResponseDelegate : public HttpResponseDelegate {
   void SendResponseHeaders(HttpStatusCode status,
                            const std::string& status_reason,
                            const base::StringPairs& headers) override {
-    connection_->adapter()->SubmitResponse(
-        stream_id_, GenerateHeaders(status, headers), nullptr,
-        /*end_stream=*/false);
+    connection_->adapter()->SubmitResponse(stream_id_,
+                                           GenerateHeaders(status, headers),
+                                           /*end_stream=*/false);
     connection_->SendIfNotProcessing();
   }
 
@@ -114,9 +114,9 @@ class Http2Connection::ResponseDelegate : public HttpResponseDelegate {
                                    const std::string& contents) override {
     chunks_.push(std::move(contents));
     last_frame_ = true;
-    connection_->adapter()->SubmitResponse(
-        stream_id_, GenerateHeaders(status, headers), nullptr,
-        /*end_stream=*/false);
+    connection_->adapter()->SubmitResponse(stream_id_,
+                                           GenerateHeaders(status, headers),
+                                           /*end_stream=*/false);
     connection_->SendIfNotProcessing();
   }
   base::WeakPtr<ResponseDelegate> GetWeakPtr() {
