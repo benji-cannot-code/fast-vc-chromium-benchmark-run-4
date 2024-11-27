587e4b62d6591fb6baee6332207ec60b818e6b91
David Maunder <davidjm@google.com>
2024-11-27 22:28:15 +0000

    Fix price tracking promo on refresh in magic stack
    There has been an INFO logged whereby the price tracking promo
    is attempted to be inserted to the magic stack after it has already
    been inserted. It has been observed these occur when the user
    refreshes the page. This means it's possible there will be multiple
    callbacks in flight for the most recent subscriptions / corresponding
    image and favicon. This CL prevents the mediator from calling
    the magic stack framework multiple times when subscription / image
    / favicon data is found (unless the mediator is reset).
    
    Bug: 375332991
    Change-Id: I8285864b22602c30ba383efe964c74e60501d509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050672
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389082}

--- ios/chrome/browser/ui/content_suggestions/price_tracking_promo/price_tracking_promo_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/price_tracking_promo/price_tracking_promo_mediator.mm
@@ -89,6 +89,7 @@ void LogOptInFlowHistogram(PriceTrackingPromoOptInFlow opt_in_flow) {
   PrefChangeRegistrar _prefChangeRegistrar;
   raw_ptr<FaviconLoader> _faviconLoader;
   bool _faviconCallbackCalledOnce;
+  bool _subscriptionDataFound;
 }
 
 - (instancetype)
@@ -140,6 +141,7 @@ void LogOptInFlowHistogram(PriceTrackingPromoOptInFlow opt_in_flow) {
 
 - (void)reset {
   _faviconCallbackCalledOnce = false;
+  _subscriptionDataFound = false;
   _priceTrackingPromoItem = nil;
 }
 
@@ -148,6 +150,7 @@ void LogOptInFlowHistogram(PriceTrackingPromoOptInFlow opt_in_flow) {
     return;
   }
   _faviconCallbackCalledOnce = false;
+  _subscriptionDataFound = false;
   __weak PriceTrackingPromoMediator* weakSelf = self;
   GetAllPriceTrackedBookmarks(
       _shoppingService, _bookmarkModel,
@@ -325,7 +328,7 @@ void LogOptInFlowHistogram(PriceTrackingPromoOptInFlow opt_in_flow) {
   if (most_recent_subscription_product_image_url.is_empty()) {
     _priceTrackingPromoItem = [[PriceTrackingPromoItem alloc] init];
     _priceTrackingPromoItem.commandHandler = self;
-    [self.delegate newSubscriptionAvailable];
+    [self onNewSubscriptionAvailable];
   } else {
     // If we have an image, fetch it and display the price tracking promo
     // with that image.
@@ -400,6 +403,16 @@ void LogOptInFlowHistogram(PriceTrackingPromoOptInFlow opt_in_flow) {
   if (attributes.faviconImage && !attributes.usesDefaultImage) {
     self->_priceTrackingPromoItem.faviconImage = attributes.faviconImage;
   }
+  [self onNewSubscriptionAvailable];
+}
+
+- (void)onNewSubscriptionAvailable {
+  // Prevent magic stack framework being called more than once in the event
+  // of multiple requests in a short period (e.g. refresh).
+  if (_subscriptionDataFound) {
+    return;
+  }
+  _subscriptionDataFound = true;
   [self.delegate newSubscriptionAvailable];
 }
 
