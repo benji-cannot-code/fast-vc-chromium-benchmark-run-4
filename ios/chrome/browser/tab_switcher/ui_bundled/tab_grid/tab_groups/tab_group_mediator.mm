e71cff9b0123be5f64c9fc96b51ac577fa883ec7
Ewann Pelle <ewannpv@chromium.org>
2024-11-27 09:48:15 +0000

    [iOS][SK] Add face pile config properties
    Adds properties allowing us to hide the face pile and customize the
    size of the avatar icons.
    
    Bug: 379285053
    Change-Id: I04c3b4aa3bb870ecf833bf393c983ce12cd75f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049750
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388701}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_mediator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_mediator.mm
@@ -45,6 +45,11 @@ using ScopedTabGroupSyncObservation =
     base::ScopedObservation<tab_groups::TabGroupSyncService,
                             tab_groups::TabGroupSyncService::Observer>;
 
+namespace {
+// The preferred size in points for the avatar icons.
+constexpr CGFloat kFacePileAvatarSize = 24;
+}  // namespace
+
 @implementation TabGroupMediator {
   // The service to observe.
   raw_ptr<tab_groups::TabGroupSyncService> _tabGroupSyncService;
@@ -528,6 +533,8 @@ using ScopedTabGroupSyncObservation =
   ShareKitFacePileConfiguration* config =
       [[ShareKitFacePileConfiguration alloc] init];
   config.collabID = savedCollabID;
+  config.showsEmptyState = YES;
+  config.avatarSize = kFacePileAvatarSize;
   [_groupConsumer setFacePileViewController:_shareKitService->FacePile(config)];
 }
 
