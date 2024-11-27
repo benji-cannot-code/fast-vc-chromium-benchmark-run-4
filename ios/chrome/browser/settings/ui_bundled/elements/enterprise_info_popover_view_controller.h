7282221401805fb5d17865d04fec98539d140817
Christian Xu <christianxu@google.com>
2024-11-27 15:56:03 +0000

    [Code Health][iOS][Settings] Move elements to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    There are too many files in ios/chrome/browser/ui/settings, this CL
    is moving part of it. Following CLs will move the rest.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 324233364
    Change-Id: I64f5119ec0f9651e60a7822ab0fb5351f6e7011e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054358
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388847}

--- ios/chrome/browser/settings/ui_bundled/elements/enterprise_info_popover_view_controller.h
+++ ios/chrome/browser/settings/ui_bundled/elements/enterprise_info_popover_view_controller.h
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_ELEMENTS_ENTERPRISE_INFO_POPOVER_VIEW_CONTROLLER_H_
-#define IOS_CHROME_BROWSER_UI_SETTINGS_ELEMENTS_ENTERPRISE_INFO_POPOVER_VIEW_CONTROLLER_H_
+#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_ELEMENTS_ENTERPRISE_INFO_POPOVER_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_ELEMENTS_ENTERPRISE_INFO_POPOVER_VIEW_CONTROLLER_H_
 
-#import "ios/chrome/browser/ui/settings/elements/info_popover_view_controller.h"
+#import "ios/chrome/browser/settings/ui_bundled/elements/info_popover_view_controller.h"
 
 // Static popover presenting the information of the enterprise.
 @interface EnterpriseInfoPopoverViewController : InfoPopoverViewController
@@ -43,4 +43,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_ELEMENTS_ENTERPRISE_INFO_POPOVER_VIEW_CONTROLLER_H_
+#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_ELEMENTS_ENTERPRISE_INFO_POPOVER_VIEW_CONTROLLER_H_
--- ios/chrome/browser/settings/ui_bundled/elements/supervised_user_info_popover_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/elements/supervised_user_info_popover_view_controller.mm
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/settings/elements/supervised_user_info_popover_view_controller.h"
+#import "ios/chrome/browser/settings/ui_bundled/elements/supervised_user_info_popover_view_controller.h"
 
 #import "base/apple/foundation_util.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/supervised_user/core/common/supervised_user_constants.h"
+#import "ios/chrome/browser/settings/ui_bundled/elements/elements_constants.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
-#import "ios/chrome/browser/ui/settings/elements/elements_constants.h"
 #import "ios/chrome/common/string_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
