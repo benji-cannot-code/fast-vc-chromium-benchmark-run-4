f215ae02c32fc4da8a14978217bf06eb3ab4b0bf
Hidehiko Abe <hidehiko@chromium.org>
2024-11-27 09:59:27 +0000

    Get rid of ENUM macro from InstallSource.
    The ENUM macro does not provide any critical feature.
    Inline the code, preferring less macro tricks.
    
    BUG=None
    TEST=Tryjob
    
    Change-Id: Ic9b37d473078d103aeb8eb784b3f2dcf6f4f6363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049499
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388712}

--- components/services/app_service/public/cpp/app_update.cc
+++ components/services/app_service/public/cpp/app_update.cc
@@ -661,7 +661,7 @@ std::ostream& operator<<(std::ostream& out, const AppUpdate& app) {
   }
 
   out << "InstallReason: " << app.InstallReason() << std::endl;
-  out << "InstallSource: " << EnumToString(app.InstallSource()) << std::endl;
+  out << "InstallSource: " << app.InstallSource() << std::endl;
 
   out << "PolicyId: " << base::JoinString(app.PolicyIds(), ", ") << std::endl;
 
