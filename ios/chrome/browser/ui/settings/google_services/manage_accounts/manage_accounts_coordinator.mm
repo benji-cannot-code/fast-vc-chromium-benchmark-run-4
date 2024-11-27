cd0799d16d00174fde25b66dfb613eb2e1e7bd6f
Jood <jood@google.com>
2024-11-27 12:47:53 +0000

    [iOS] Pop manage accounts view correctly when viewed from web
    This CL fixes the crash by re-adding a removed line to init
    _closeSettingsOnAddAccount, when viewed from web.
    
    Later, _closeSettingsOnAddAccount should be renamed to reflect its use
    better.
    
    Fixed: 380497331
    Change-Id: I767e52972e83524f3aa09ad49a8d9291c300b1ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054029
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388762}

--- ios/chrome/browser/ui/settings/google_services/manage_accounts/manage_accounts_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_accounts/manage_accounts_coordinator.mm
@@ -98,6 +98,7 @@ using signin_metrics::PromoAction;
                            (BOOL)closeSettingsOnAddAccount {
   if ((self = [self initWithBaseViewController:navigationController
                                        browser:browser])) {
+    _closeSettingsOnAddAccount = closeSettingsOnAddAccount;
     _baseNavigationController = navigationController;
   }
   return self;
