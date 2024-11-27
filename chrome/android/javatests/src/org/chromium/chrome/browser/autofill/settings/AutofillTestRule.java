dd913916163b2aa36513715bb51732ba1f70573b
Qihui Zhao <qihuizhao@google.com>
2024-11-27 00:32:44 +0000

    Show Ewallet accounts on financial management page
    This CL does the followings:
    1. Add eWallet pref
    2. Show eWallet accounts on financial management page along
    with Pix if available.
    
    Screencast:
    https://drive.google.com/file/d/1ZHcmtK6UMBCNrFvCccYnSrBW7iFkqFVE/view?usp=sharing
    https://drive.google.com/file/d/1P4Z7J2YaZ80Nlq3HmgVrk51QtDlJc0Ov/view?usp=sharing
    
    Screenshot:
    https://screenshot.googleplex.com/9byS3anSQwoFZXD
    https://screenshot.googleplex.com/nAoqLB3FuRBJHFS
    
    Mock:
    https://screenshot.googleplex.com/5ZeP6gdZnGXrTMj
    https://screenshot.googleplex.com/9NCT8JZoajviFqD
    
    
    Bug: 40280186
    Change-Id: Id07748e8a2f8f67358426ccd5b5f9953f6e1ce46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6005371
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388555}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillTestRule.java
@@ -16,6 +16,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.chrome.browser.autofill.editors.EditorDialogView;
 import org.chromium.chrome.browser.autofill.editors.EditorObserverForTest;
 import org.chromium.chrome.test.ChromeBrowserTestRule;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 
 import java.util.List;
 import java.util.concurrent.TimeoutException;
@@ -133,6 +134,16 @@ class AutofillTestRule extends ChromeBrowserTestRule
                             .dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, keycode));
                 });
         mClickUpdate.waitForCallback(callCount);
+    }
+
+    protected void clickOnPreferenceAndWait(ChromeSwitchPreference preference)
+            throws TimeoutException {
+        int callCount = mFragmentShown.getCallCount();
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    preference.performClick();
+                });
+        mFragmentShown.waitForCallback(callCount);
     }
 
     protected void setEditorDialogAndWait(EditorDialogView editorDialog) throws TimeoutException {
--- components/autofill/core/common/autofill_prefs.h
+++ components/autofill/core/common/autofill_prefs.h
@@ -147,6 +147,8 @@ inline constexpr char kAutofillUsingVirtualViewStructure[] =
 inline constexpr char kAutofillThirdPartyPasswordManagersAllowed[] =
     "autofill.third_party_password_managers_allowed";
 inline constexpr char kFacilitatedPaymentsPix[] = "facilitated_payments.pix";
+inline constexpr char kFacilitatedPaymentsEwallet[] =
+    "facilitated_payments.ewallet";
 #endif  // BUILDFLAG(IS_ANDROID)
 
 // The maximum value for the
@@ -228,6 +230,10 @@ bool UsesVirtualViewStructureForAutofill(const PrefService* prefs);
 void SetFacilitatedPaymentsPix(PrefService* prefs, bool value);
 
 bool IsFacilitatedPaymentsPixEnabled(const PrefService* prefs);
+
+void SetFacilitatedPaymentsEwallet(PrefService* prefs, bool value);
+
+bool IsFacilitatedPaymentsEwalletEnabled(const PrefService* prefs);
 
 }  // namespace autofill::prefs
 
