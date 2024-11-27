e50d3ceed97cbea132afda1405f85404a71d0f78
Mike West <mkwst@chromium.org>
2024-11-27 11:40:27 +0000

    [Identity-Digest] Matching content.
    This patch adds `IdentityDigestHeader::DoesMatch()`, which allows an
    `Identity-Digest` header to be compared against a `SgementedBuffer`
    for eventual use in the future to enforce the header's constraint
    on resources prior to execution.
    
    This is part of a chain of CLs implementing this feature:
    
    1.  https://crrev.com/c/5961577
    2.  [You are here] https://crrev.com/c/5961839
    3.  https://crrev.com/c/5966593
    
    Bug: 375343417
    Change-Id: I536cc7cbdc0e7858c7167d89faed06f465305a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5961839
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388743}

--- third_party/blink/renderer/platform/loader/identity_digest.h
+++ third_party/blink/renderer/platform/loader/identity_digest.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/integrity_metadata.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 
+namespace WTF {
+class SegmentedBuffer;
+}  // namespace WTF
+
 namespace blink {
 
 class HTTPHeaderMap;
@@ -28,6 +32,10 @@ class PLATFORM_EXPORT IdentityDigest {
   static std::optional<IdentityDigest> Create(const HTTPHeaderMap&);
 
   const IntegrityMetadataSet& digests() const { return digests_; }
+
+  // Validates |data| against all parsed digests, returning `true` if all match,
+  // and `false` otherwise.
+  bool DoesMatch(WTF::SegmentedBuffer* data);
 
  private:
   IdentityDigest() = default;
