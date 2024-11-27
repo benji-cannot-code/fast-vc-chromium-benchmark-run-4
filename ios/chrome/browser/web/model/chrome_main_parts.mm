65b8cce0c3187a2fd3fd52bd6dfefa095680ea37
Joe Mason <joenotcharles@google.com>
2024-11-27 12:55:34 +0000

    Reland "Register SyntheticHeapProfilingConfiguration trial on ios"
    This is a reland of commit a51d14a6f2aabc0be44bfeb768e46da543272d74
    
    It wraps all references to HeapProfilerController in a
    USE_ALLOCATOR_SHIM check, since the profiler relies on the allocator
    shim on iOS. Fixes a build failure on the ios-catalyst builder.
    
    Original change's description:
    > Register SyntheticHeapProfilingConfiguration trial on ios
    >
    > Copies the field trial setup from
    > ChromeBrowserMainParts::StartMetricsRecording(), which is used on other
    > platforms.
    >
    > Bug: 358435071
    > Change-Id: Iee6b836b44229a384488d4a447b45a8dfbffbb59
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026809
    > Auto-Submit: Joe Mason <joenotcharles@google.com>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1386864}
    
    Bug: 358435071
    Change-Id: I973ee0d32690874486a4da783aa40e9ccb886282
    Cq-Include-Trybots: luci.chromium.try:ios-catalyst
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049899
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388768}

--- ios/chrome/browser/web/model/chrome_main_parts.mm
+++ ios/chrome/browser/web/model/chrome_main_parts.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import <string>
+
 #import "base/allocator/partition_alloc_support.h"
 #import "base/check_op.h"
 #import "base/feature_list.h"
@@ -46,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/variations/field_trial_config/field_trial_util.h"
 #import "components/variations/service/variations_service.h"
 #import "components/variations/synthetic_trial_registry.h"
+#import "components/variations/synthetic_trials.h"
 #import "components/variations/synthetic_trials_active_group_id_provider.h"
 #import "components/variations/variations_crash_keys.h"
 #import "components/variations/variations_ids_provider.h"
@@ -82,6 +85,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if DCHECK_IS_ON()
 #import "ui/display/screen_base.h"
+#endif
+
+#if PA_BUILDFLAG(USE_ALLOCATOR_SHIM)
+#import "components/heap_profiling/in_process/heap_profiler_controller.h"
 #endif
 
 namespace {
@@ -416,6 +423,22 @@ void IOSChromeMainParts::SetupMetrics() {
 }
 
 void IOSChromeMainParts::StartMetricsRecording() {
+  // Register synthetic field trial for the sampling profiler configuration
+  // that was already chosen.
+#if PA_BUILDFLAG(USE_ALLOCATOR_SHIM)
+  // HeapProfilerController is only built when the allocator shim is enabled.
+  std::string trial_name, group_name;
+  auto* heap_profiler_controller =
+      heap_profiling::HeapProfilerController::GetInstance();
+  if (heap_profiler_controller &&
+      heap_profiler_controller->GetSyntheticFieldTrial(trial_name,
+                                                       group_name)) {
+    IOSChromeMetricsServiceAccessor::RegisterSyntheticFieldTrial(
+        trial_name, group_name,
+        variations::SyntheticTrialAnnotationMode::kCurrentLog);
+  }
+#endif
+
   // TODO(crbug.com/40894426) Add an EG2 test for cloned install detection.
   application_context_->GetMetricsService()->CheckForClonedInstall();
   application_context_->GetMetricsServicesManager()->UpdateUploadPermissions(
