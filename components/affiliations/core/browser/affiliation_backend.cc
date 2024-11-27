93e54899a934c071caa11c5eb824393a479f18fc
Norge Vizcay <vizcay@google.com>
2024-11-27 16:19:39 +0000

    Support fetching affiliation PSL extension list on Android.
    Bug: 369774706
    Change-Id: I60d8831194a4b662ba2db7214b3ce3815714f91d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032755
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388859}

--- components/affiliations/core/browser/affiliation_backend.cc
+++ components/affiliations/core/browser/affiliation_backend.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/affiliations/core/browser/affiliation_backend.h"
 
 #include <stdint.h>
+
 #include <algorithm>
 #include <utility>
 #include <vector>
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/affiliations/core/browser/affiliation_fetcher_interface.h"
 #include "components/affiliations/core/browser/affiliation_utils.h"
 #include "components/affiliations/core/browser/facet_manager.h"
+#include "components/affiliations/core/browser/features.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace affiliations {
@@ -419,10 +421,11 @@ bool AffiliationBackend::OnCanSendNetworkRequest() {
   // TODO(crbug.com/40858918): There is no need to request psl extension every
   // time, find a better way of caching it.
 #if BUILDFLAG(IS_ANDROID)
-  // psl_extension_list isn't needed on Android because the OS API will apply
-  // it..
-  fetcher_->StartRequest(requested_facet_uris,
-                         {.branding_info = true, .psl_extension_list = false});
+  const bool is_android_enabled =
+      base::FeatureList::IsEnabled(features::kAffiliationsGroupInfoEnabled);
+  fetcher_->StartRequest(
+      requested_facet_uris,
+      {.branding_info = true, .psl_extension_list = is_android_enabled});
 #else
   fetcher_->StartRequest(requested_facet_uris,
                          {.branding_info = true, .psl_extension_list = true});
