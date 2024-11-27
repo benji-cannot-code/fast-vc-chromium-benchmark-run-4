610ac6ced04e71895841024bc43056707611f50a
Michelle Chen <michellegc@google.com>
2024-11-27 00:17:06 +0000

    scanner: Show glow animation when Scanner actions are being fetched.
    1. Start a glow animation around the capture region when starting to
       fetch Scanner actions (either from a Sunfish session or when the
       smart actions button is pressed from a normal capture mode session).
    2. After smart actions have been fetched, stop animating the glow and
       instead keep it at the minimum glow level.
    3. When selecting a new region, remove the glow effect.
    
    Follow ups will handle removing the glow effect in other situations
    where Scanner processing becomes invalid (e.g. after a behaviour change
    or capture mode change).
    
    Bug: b:374381937
    Change-Id: I3dbeecacb87d4365451fb7078321bfed369bfd99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043037
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388550}

--- ash/capture_mode/null_capture_mode_session.cc
+++ ash/capture_mode/null_capture_mode_session.cc
@@ -134,7 +134,7 @@ ActionButtonView* NullCaptureModeSession::AddActionButton(
   return nullptr;
 }
 
-void NullCaptureModeSession::AddScannerActionButtons(
+void NullCaptureModeSession::OnScannerActionsFetched(
     std::vector<ScannerActionViewModel> scanner_actions) {}
 
 void NullCaptureModeSession::OnTextDetected() {}
