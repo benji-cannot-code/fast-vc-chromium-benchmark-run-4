1565b895112ad7aa00c2f37641daa2b3d329e651
Kevin McNee <mcnee@chromium.org>
2024-11-27 17:29:00 +0000

    Disable OnDeviceModelServiceControllerTest.ModelValidationNewModelVersion on Mac
    The test is flaky on Mac.
    
    Bug: 380229867
    Change-Id: Ibaa1977f73b308c2e6a13d2bf32e79846835ebe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056057
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Owners-Override: Kevin McNee <mcnee@google.com>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388908}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller_unittest.cc
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/cxx23_to_underlying.h"
 #include "base/types/expected.h"
 #include "base/uuid.h"
+#include "build/build_config.h"
 #include "components/optimization_guide/core/model_execution/feature_keys.h"
 #include "components/optimization_guide/core/model_execution/model_execution_features.h"
 #include "components/optimization_guide/core/model_execution/model_execution_prefs.h"
@@ -3389,7 +3390,15 @@ TEST_F(OnDeviceModelServiceControllerTest,
   EXPECT_TRUE(CreateSession());
 }
 
-TEST_F(OnDeviceModelServiceControllerTest, ModelValidationNewModelVersion) {
+// TODO(crbug.com/380229867): Flaky on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_ModelValidationNewModelVersion \
+  DISABLED_ModelValidationNewModelVersion
+#else
+#define MAYBE_ModelValidationNewModelVersion ModelValidationNewModelVersion
+#endif
+TEST_F(OnDeviceModelServiceControllerTest,
+       MAYBE_ModelValidationNewModelVersion) {
   base::test::ScopedFeatureList feature_list;
   feature_list.InitWithFeaturesAndParameters(
       {{features::kOnDeviceModelValidation,
