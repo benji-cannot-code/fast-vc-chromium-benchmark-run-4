60e8babda57641e0cea827a16b575b48c1daefe3
Radu Nitescu <radunitescu@google.com>
2024-11-27 16:10:34 +0000

    Revert "[iOS] Remove some scene state window usage in Lens"
    This reverts commit 749e9db92249c008db32d1d4f38b3df1c943a910.
    
    Reason for revert: Breaking the gesture recognizer for panning
    
    Original change's description:
    > [iOS] Remove some scene state window usage in Lens
    >
    > Bug: 376667510
    > Change-Id: Ibf0f589577720b39680f7dc2795145e3d6fec649
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032618
    > Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    > Reviewed-by: Radu Nitescu <radunitescu@google.com>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1388777}
    
    Bug: 376667510
    Change-Id: I24ec5f89be898714bd334c97c634c7bd2a8fbd64
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054723
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1388853}

--- ios/chrome/browser/shared/coordinator/scene/scene_state.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_state.mm
@@ -134,10 +134,6 @@ ContentVisibility ContentVisibilityForIncognito(BOOL isIncognito) {
   return self.window.accessibilityIdentifier;
 }
 
-- (NSArray<UIGestureRecognizer*>*)windowGestureRecognizers {
-  return self.window.gestureRecognizers;
-}
-
 - (UIViewController*)rootViewController {
   return [self.window rootViewController];
 }
