420dc24fba936a6b005eac1ce5fea8102a22e077
Georg Neis <neis@chromium.org>
2024-11-27 09:35:48 +0000

    ash: Remove GetBrowserInformation from desk template feature
    It was only needed for Lacros.
    
    Bug: b:365741912
    Change-Id: Ic7b3ecc7c8f00d52896173b15185a0120dbc922e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048232
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388696}

--- chrome/browser/ash/crosapi/browser_manager.h
+++ chrome/browser/ash/crosapi/browser_manager.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/session_manager/session_manager_client.h"
 #include "chromeos/ash/components/standalone_browser/lacros_selection.h"
 #include "chromeos/crosapi/mojom/browser_service.mojom.h"
-#include "chromeos/crosapi/mojom/desk_template.mojom.h"
 #include "components/component_updater/component_updater_service.h"
 #include "components/policy/core/common/cloud/cloud_policy_core.h"
 #include "components/policy/core/common/cloud/cloud_policy_refresh_scheduler_observer.h"
@@ -92,10 +91,6 @@ class BrowserManager : public session_manager::SessionManagerObserver,
 
   ~BrowserManager() override;
 
-  // Returns true if Lacros is in running state.
-  // Virtual for testing.
-  virtual bool IsRunning() const;
-
   // NOTE on callbacks:
   // An action's callback (e.g. the last parameter to NewWindowForDetachingTab
   // below) will never be invoked with a CreationResult value of
@@ -133,14 +128,6 @@ class BrowserManager : public session_manager::SessionManagerObserver,
   // Lacros via BrowserLoader::Unload, which also deletes the user data
   // directory.
   virtual void InitializeAndStartIfNeeded();
-
-  using GetBrowserInformationCallback =
-      base::OnceCallback<void(crosapi::mojom::DeskTemplateStatePtr)>;
-  // Gets URLs and active indices of the tab strip models from the Lacros
-  // browser window.
-  // Virtual for testing.
-  virtual void GetBrowserInformation(const std::string& window_unique_id,
-                                     GetBrowserInformationCallback callback);
 
   void AddObserver(BrowserManagerObserver* observer);
   void RemoveObserver(BrowserManagerObserver* observer);
--- chrome/browser/ash/crosapi/desk_template_ash.cc
+++ chrome/browser/ash/crosapi/desk_template_ash.cc
@@ -10,38 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crosapi {
 
-DeskTemplateAsh::Call::Call(
-    uint32_t serial,
-    const std::string& window_unique_id,
-    uint32_t remote_count,
-    base::OnceCallback<void(crosapi::mojom::DeskTemplateStatePtr)> callback)
-    : serial(serial),
-      window_unique_id(window_unique_id),
-      remote_count(remote_count),
-      callback(std::move(callback)) {}
-
-DeskTemplateAsh::Call::~Call() = default;
-
 DeskTemplateAsh::DeskTemplateAsh() = default;
 DeskTemplateAsh::~DeskTemplateAsh() = default;
 
 void DeskTemplateAsh::BindReceiver(
     mojo::PendingReceiver<mojom::DeskTemplate> pending_receiver) {
   receivers_.Add(this, std::move(pending_receiver));
-}
-
-void DeskTemplateAsh::GetBrowserInformation(
-    const std::string& window_unique_id,
-    base::OnceCallback<void(crosapi::mojom::DeskTemplateStatePtr)> callback) {
-  const auto current_serial = serial_++;
-  calls_.emplace_back(current_serial, window_unique_id, remotes_.size(),
-                      std::move(callback));
-  for (const auto& remote : remotes_) {
-    remote->GetBrowserInformation(
-        current_serial, window_unique_id,
-        base::BindOnce(&DeskTemplateAsh::OnGetBrowserInformationFromRemote,
-                       weak_factory_.GetWeakPtr()));
-  }
 }
 
 void DeskTemplateAsh::CreateBrowserWithRestoredData(
@@ -71,31 +45,6 @@ void DeskTemplateAsh::GetFaviconImage(
 void DeskTemplateAsh::AddDeskTemplateClient(
     mojo::PendingRemote<mojom::DeskTemplateClient> client) {
   remotes_.Add(mojo::Remote<mojom::DeskTemplateClient>(std::move(client)));
-}
-
-void DeskTemplateAsh::OnGetBrowserInformationFromRemote(
-    uint32_t serial,
-    const std::string& window_unique_id,
-    mojom::DeskTemplateStatePtr state) {
-  auto call_it = calls_.begin();
-  while (call_it != calls_.end()) {
-    if (call_it->window_unique_id == window_unique_id &&
-        call_it->serial == serial) {
-      break;
-    }
-    ++call_it;
-  }
-  if (call_it == calls_.end()) {
-    DCHECK(state.is_null());
-    return;
-  }
-  Call& call = *call_it;
-  DCHECK(call.remote_count > 0);
-  --call.remote_count;
-  if (call.remote_count == 0 || !state.is_null()) {
-    std::move(call.callback).Run(std::move(state));
-    calls_.erase(call_it);
-  }
 }
 
 }  // namespace crosapi
