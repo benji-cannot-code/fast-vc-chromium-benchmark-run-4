cc8f7523993fa89853ddf12c48797a561a3d2ec1
Nasser Al-shawwa <alshawwa@chromium.org>
2024-11-27 18:06:27 +0000

    Implement WatermarkTextContainer as WebContentsUserData
    Attaching the watermark text to the web contents in this way
    allows us to render it in the browser process outside of any
    ongoing navigations. This is needed when we need to retrieve
    the string for printing.
    
    Bug: 377892051
    Change-Id: Ic0bda2319653466e6964d48b589ff4aa713e94b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050100
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388926}

---
+++ components/enterprise/watermarking/content/watermark_text_container.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/watermarking/content/watermark_text_container.h"
+
+namespace enterprise_watermark {
+
+WatermarkTextContainer::WatermarkTextContainer(content::WebContents* contents)
+    : content::WebContentsUserData<WatermarkTextContainer>(*contents) {}
+
+void WatermarkTextContainer::SetWatermarkText(std::string watermark_text) {
+  watermark_text_ = std::move(watermark_text);
+}
+
+const std::string& WatermarkTextContainer::watermark_text() const {
+  return watermark_text_;
+}
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(WatermarkTextContainer);
+
+}  // namespace enterprise_watermark
---
+++ components/enterprise/watermarking/content/watermark_text_container.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_WATERMARKING_CONTENT_WATERMARK_TEXT_CONTAINER_H_
+#define COMPONENTS_ENTERPRISE_WATERMARKING_CONTENT_WATERMARK_TEXT_CONTAINER_H_
+
+#include "content/public/browser/web_contents_user_data.h"
+
+namespace enterprise_watermark {
+
+class WatermarkTextContainer
+    : public content::WebContentsUserData<WatermarkTextContainer> {
+ public:
+  explicit WatermarkTextContainer(content::WebContents* contents);
+  ~WatermarkTextContainer() override = default;
+
+  void SetWatermarkText(std::string watermark_text);
+  const std::string& watermark_text() const;
+
+ private:
+  friend class content::WebContentsUserData<WatermarkTextContainer>;
+
+  std::string watermark_text_;
+  WEB_CONTENTS_USER_DATA_KEY_DECL();
+};
+
+}  // namespace enterprise_watermark
+
+#endif  // COMPONENTS_ENTERPRISE_WATERMARKING_CONTENT_WATERMARK_TEXT_CONTAINER_H_
