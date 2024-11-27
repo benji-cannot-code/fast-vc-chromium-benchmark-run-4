58ad5c3ae116137aed14d2b1dcb5190d9b025cce
Antonio Gomes <tonikitoo@igalia.com>
2024-11-27 11:09:17 +0000

    Remove LacrosAppWindowObserver from desks_client.*
    This class was used to store app ids for lacros windows so
    that when a lacros window is part of a saved desk, it can figure
    out the app id (if any).
    This functionality isn't needed anymore after the Lacros project
    termination.
    
    R=hidehiko@chromium.org, neis@chromium.org
    
    Bug: 373971535, 381109480
    Change-Id: I636577cf717e1fcaa43a79a94b31ead33a8fe280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6053909
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388733}

--- chrome/browser/ui/ash/desks/desks_client.h
+++ chrome/browser/ui/ash/desks/desks_client.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/core/session_id.h"
 
 class DesksTemplatesAppLaunchHandler;
-class LacrosAppWindowObserver;
 class Profile;
 
 namespace ash {
@@ -201,10 +200,6 @@ class DesksClient : public ash::SessionObserver {
   std::optional<DesksClient::DeskActionError> SwitchDesk(
       const base::Uuid& desk_uuid);
 
-  // If `window` is a lacros window that has an app id, return it.
-  std::optional<std::string> GetAppIdForLacrosWindow(
-      aura::Window* window) const;
-
  private:
   class LaunchPerformanceTracker;
   class DeskEventObserver;
@@ -288,9 +283,6 @@ class DesksClient : public ash::SessionObserver {
 
   // Wrapper desk model to house both desk types backend storage.
   std::unique_ptr<desks_storage::DeskModelWrapper> saved_desk_storage_manager_;
-
-  // Monitors lacros app windows for use in saved desks.
-  std::unique_ptr<LacrosAppWindowObserver> lacros_app_window_observer_;
 
   // The stored JSON values of preconfigured desk templates
   base::flat_map<AccountId, std::string> preconfigured_desk_templates_json_;
