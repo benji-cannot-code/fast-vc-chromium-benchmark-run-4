349a00f43db03931982540a9611636d2fe47e077
Christian Xu <christianxu@google.com>
2024-11-27 13:13:08 +0000

    [Code Health][iOS][Settings] Move content_settings to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    There are too many files in ios/chrome/browser/ui/settings, this CL
    is moving part of it. Following CLs will move the rest.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 324233364
    Change-Id: I09e5c7420ff8e01b8b11ab462de3d10b3d4772c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049639
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388780}

---
+++ ios/chrome/browser/settings/ui_bundled/content_settings/content_settings_table_view_controller.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_CONTENT_SETTINGS_CONTENT_SETTINGS_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_CONTENT_SETTINGS_CONTENT_SETTINGS_TABLE_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/browser/ui/settings/settings_controller_protocol.h"
+#import "ios/chrome/browser/ui/settings/settings_root_table_view_controller.h"
+
+class Browser;
+
+// Controller for the UI that allows the user to change content settings like
+// blocking popups.
+@interface ContentSettingsTableViewController
+    : SettingsRootTableViewController <SettingsControllerProtocol>
+
+// The designated initializer. `browser` must not be null.
+- (instancetype)initWithBrowser:(Browser*)browser NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithStyle:(UITableViewStyle)style NS_UNAVAILABLE;
+@end
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_CONTENT_SETTINGS_CONTENT_SETTINGS_TABLE_VIEW_CONTROLLER_H_
