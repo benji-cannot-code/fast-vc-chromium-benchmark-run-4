b023dfc059e10f7f34fa79de409f171afd4bc096
Keren Zhu <kerenzhu@chromium.org>
2024-11-27 01:15:02 +0000

    Use scoped feature in WebUIJSErrorReportingTest
    This test suite requires feature kWebUIJSErrorReportingExtended to be
    enabled. This CL uses a scoped feature in the test suite so that it
    won't be affected by changes in the feature default or the field trial
    testing config.
    
    Bug: 374696525
    Change-Id: I3cf383a7cd152b200a2e0ae95f40bbc579c72e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050004
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388572}

--- content/browser/webui/web_ui_main_frame_observer.cc
+++ content/browser/webui/web_ui_main_frame_observer.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crash/content/browser/error_reporting/js_error_report_processor.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/webui/web_ui_impl.h"
-#include "content/common/features.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
