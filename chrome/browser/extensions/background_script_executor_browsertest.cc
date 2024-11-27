389b82d97d56fe4df6b3fd2d979093f32c4677d3
Jenny Zhang <jennyz@chromium.org>
2024-11-27 23:17:59 +0000

    [Desktop Android CRX] Enable BackgroundScriptExecutorBrowserTest.
    This cl enables extension browser tests for BackgroundScriptExecutor
    to ensure `BackgroundScriptExecutor::ExecuteScript`
    work in service worker for regular profile on desktop android.
    
    Note: `BackgroundScriptExecutor::ExecuteScript` seems not working
    with incognito browser context on desktop android. It fails to
    execute the same script for incognito context when running
    `OffscreenApiTest.IncognitoModeHandling_SplitMode`.
    
    Bug: 356905053
    Change-Id: Ide5955969f51ab1093056b3f0467ec62559bdad3
    Cq-Include-Trybots: luci.chromium.try:android-desktop-14-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6052360
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389103}

--- chrome/browser/extensions/background_script_executor_browsertest.cc
+++ chrome/browser/extensions/background_script_executor_browsertest.cc
@@ -3,19 +3,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "extensions/browser/background_script_executor.h"
+
 #include "base/test/bind.h"
 #include "base/test/values_test_util.h"
-#include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/test/browser_test.h"
-#include "extensions/browser/background_script_executor.h"
 #include "extensions/browser/script_executor.h"
+#include "extensions/common/extension.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/test_extension_dir.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/extensions/extension_platform_browsertest.h"
+#else
+#include "chrome/browser/extensions/extension_browsertest.h"
+#endif
+
 namespace extensions {
 
+#if BUILDFLAG(IS_ANDROID)
+using BackgroundScriptExecutorBrowserTest = ExtensionPlatformBrowserTest;
+#else
 using BackgroundScriptExecutorBrowserTest = ExtensionBrowserTest;
+#endif
 
 // Tests the ability to run JS in an extension-registered service worker.
 IN_PROC_BROWSER_TEST_F(BackgroundScriptExecutorBrowserTest,
@@ -60,6 +71,8 @@ IN_PROC_BROWSER_TEST_F(BackgroundScriptExecutorBrowserTest,
   }
 }
 
+#if !BUILDFLAG(IS_ANDROID)
+// Only manifest V3 and above is supported on desktop android.
 // Tests the ability to run JS in an extension background page.
 IN_PROC_BROWSER_TEST_F(BackgroundScriptExecutorBrowserTest,
                        ExecuteScriptInBackgroundPage) {
@@ -141,5 +154,6 @@ IN_PROC_BROWSER_TEST_F(BackgroundScriptExecutorBrowserTest,
         value, base::test::IsJson(R"({"testFlag":"flag","userGesture":true})"));
   }
 }
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace extensions
