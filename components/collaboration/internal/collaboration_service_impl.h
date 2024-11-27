a9ba9475dff687f43d8e3b9118c81b11e3b1afe2
Hailey Wang <haileywang@google.com>
2024-11-27 06:01:07 +0000

    [Collaboration] Add signin status observation
    Bug: 380120486
    Change-Id: I76aef92541a63389c9936b720b912ab2eb11a4b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043545
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388639}

--- components/collaboration/internal/collaboration_service_impl.h
+++ components/collaboration/internal/collaboration_service_impl.h
@@ -11,15 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "components/collaboration/public/collaboration_service.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/signin/public/identity_manager/primary_account_change_event.h"
 #include "components/sync/service/sync_service_observer.h"
 
 namespace data_sharing {
 class DataSharingService;
 }  // namespace data_sharing
-
-namespace signin {
-class IdentityManager;
-}  // namespace signin
 
 namespace syncer {
 class SyncService;
@@ -34,7 +32,8 @@ class CollaborationController;
 
 // The internal implementation of the CollborationService.
 class CollaborationServiceImpl : public CollaborationService,
-                                 public syncer::SyncServiceObserver {
+                                 public syncer::SyncServiceObserver,
+                                 public signin::IdentityManager::Observer {
  public:
   CollaborationServiceImpl(
       tab_groups::TabGroupSyncService* tab_group_sync_service,
@@ -57,6 +56,16 @@ class CollaborationServiceImpl : public CollaborationService,
   void OnStateChanged(syncer::SyncService* sync) override;
   void OnSyncShutdown(syncer::SyncService* sync) override;
 
+  // IdentityManager::Observer implementation.
+  void OnPrimaryAccountChanged(
+      const signin::PrimaryAccountChangeEvent& event_details) override;
+  void OnRefreshTokenUpdatedForAccount(
+      const CoreAccountInfo& account_info) override;
+  void OnRefreshTokenRemovedForAccount(
+      const CoreAccountId& account_id) override;
+  void OnIdentityManagerShutdown(
+      signin::IdentityManager* identity_manager) override;
+
   // For testing.
   const std::map<data_sharing::GroupToken,
                  std::unique_ptr<CollaborationController>>&
@@ -67,10 +76,15 @@ class CollaborationServiceImpl : public CollaborationService,
 
  private:
   SyncStatus GetSyncStatus();
+  SigninStatus GetSigninStatus();
+  void RefreshSigninStatus();
 
   ServiceStatus current_status_;
   base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
       sync_observer_{this};
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observer_{this};
 
   // Service providing information about tabs and tab groups.
   const raw_ptr<tab_groups::TabGroupSyncService> tab_group_sync_service_;
