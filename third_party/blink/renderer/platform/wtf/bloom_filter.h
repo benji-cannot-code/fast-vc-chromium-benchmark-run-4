ff949508f315503562984f90f0575c08084b7404
keyou <qqkillyou@gmail.com>
2024-11-27 09:53:18 +0000

    spanification: //third_party/blink/renderer/bindings/core/v8/ [part 3]
    1. Change V8CrowdsourcedCompileHintsConsumer::SetData to accept span.
    2. Change WTF::BloomFilter::GetRawData to return span type.
    
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: I2e6a0eb1565152375ad848e5aff078bf46173151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049234
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388706}

--- third_party/blink/renderer/platform/wtf/bloom_filter.h
+++ third_party/blink/renderer/platform/wtf/bloom_filter.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_BLOOM_FILTER_H_
 
 #include "base/check_op.h"
+#include "base/containers/span.h"
 #include "base/dcheck_is_on.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
@@ -67,7 +68,7 @@ class BloomFilter {
     return memcmp(a.bit_array_, b.bit_array_, a.kBitArrayMemorySize) == 0;
   }
 
-  unsigned* GetRawData() { return bit_array_; }
+  base::span<unsigned> GetRawData() { return base::span(bit_array_); }
 
  private:
   using BitArrayUnit = unsigned;
