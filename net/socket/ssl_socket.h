2417745013b229ccd24469346c95b61a601079c6
Elly <ellyjones@chromium.org>
2024-11-27 21:01:48 +0000

    net: spanify SSLSocket::ExportKeyingMaterial
    This change migrates the interface from:
    
      bool ExportKeyingMaterial(std::string_view label,
                                bool has_context,
                                std::string_view context,
                                unsigned char* out,
                                unsigned int outlen);
    to:
    
      bool ExportKeyingMaterial(
          std::string_view label,
          std::optional<base::span<const uint8_t>> context,
          base::span<uint8_t> out);
    
    which makes two errors impossible:
    * Passing an out buffer with the wrong length
    * Passing a context but !has_context, or !context but has_context
    
    All the call sites are migrated in this change as well.
    
    Bug: None
    Change-Id: I81a88f92178ec5023e8e4ea54a434a41585dc058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6052679
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389042}

--- net/socket/ssl_socket.h
+++ net/socket/ssl_socket.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string_view>
 
+#include "base/containers/span.h"
 #include "net/base/net_export.h"
 #include "net/socket/stream_socket.h"
 
@@ -16,19 +17,27 @@ namespace net {
 // SSLSocket interface defines method that are common between client
 // and server SSL sockets.
 class NET_EXPORT SSLSocket : public StreamSocket {
-public:
- ~SSLSocket() override = default;
+ public:
+  ~SSLSocket() override = default;
 
- // Exports data derived from the SSL master-secret (see RFC 5705).
- // If |has_context| is false, uses the no-context construction from the
- // RFC and |context| is ignored.  The call will fail with an error if
- // the socket is not connected or the SSL implementation does not
- // support the operation.
- virtual int ExportKeyingMaterial(std::string_view label,
-                                  bool has_context,
-                                  std::string_view context,
-                                  unsigned char* out,
-                                  unsigned int outlen) = 0;
+  // Exports data derived from the SSL master-secret (see RFC 5705).  The call
+  // will fail with an error if the socket is not connected or the SSL
+  // implementation does not support the operation. Note that |label| is
+  // required (per RFC 5705 section 4) to be ASCII and subclasses enforce this
+  // requirement.
+  //
+  // Note that in TLS < 1.3, passing std::nullopt for context produces a
+  // different result from passing a populated option containing an empty span.
+  // TLS 1.3 did away with this distinction and passing std::nullopt has the
+  // same behavior as passing base::span(). See RFC 5705 section 4 for TLS <
+  // 1.3 and RFC 8446 section 7.5 for TLS 1.3.
+  //
+  // Once we drop support for TLS < 1.3 (some day...) the context argument here
+  // can cease being optional.
+  virtual int ExportKeyingMaterial(
+      std::string_view label,
+      std::optional<base::span<const uint8_t>> context,
+      base::span<uint8_t> out) = 0;
 };
 
 }  // namespace net
