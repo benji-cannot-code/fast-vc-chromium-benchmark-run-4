c8348786f258efce1703fdaa7d6aed5195e90d04
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-11-27 01:30:46 +0000

    Reland "[Extensions] Nullify ProcessManager::site_instance_"
    This relands commit 0e35ed6882463b74f18c8735747d3702f254190a.
    
    This was reverted because of an issue where a MimeHandlerViewGuest
    had a RenderFrameHost swap, which meant that handles in the test
    were invalidated, leading to UAFs (in the test only). However, since
    other code may be relying on the same type of behavior, fix this by
    going back to using a direct SiteInstance in the MimeHandlerViewGuest.
    
    ----- Original CL Description -----
    This CL is based off nasko@'s CL at
    https://chromium-review.googlesource.com/c/chromium/src/+/5142350.
    
    For various historical reasons, there's a "primordial" shared
    SiteInstance used for most extension hosts that's owned by the
    ProcessManager. This should no longer be necessary, and has some
    potentially undesirable side effects.
    
    This CL sets that SiteInstance to null if the corresponding
    feature kRemoveCoreSiteInstance is enabled. This is effectively the
    same as removing the member, since the SiteInstance is now only used
    in the creation of ExtensionHosts' WebContents, which can accept a
    null SiteInstance (the same as passing no SiteInstance).
    
    This requires adjusting a number of callsites, primarily those around
    ExtensionHost creation, in order to provide them with a BrowserContxt
    member directly. In the future, once we're sure this change is safe,
    we will remove the SiteInstance member from ProcessManager entirely.
    
    In addition to plumbing, this required:
    - Changing where we set a temporary zoom level for extension popups
      to be immediately before we're ready to commit, rather than in the
      constructor; this is necessary since the primary RenderFrameHost
      will be swapped between these points.
    - Updating how a few tests wait for readiness (to use more correct
      methods, rather than relying on racy conditions)
    - Updating IsExtensionIdle() to look for a process entry in the
      ProcessMap instead of checking the SiteInstance from the
      ProcessManager.
    
    Bug: 334991035 
    Change-Id: Ib682f8dee02985bbef32be83e17fe3d42323d257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6041245
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388577}

--- chrome/browser/extensions/extension_util.cc
+++ chrome/browser/extensions/extension_util.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extension_util.h"
 #include "extensions/browser/pref_names.h"
+#include "extensions/browser/process_map.h"
 #include "extensions/browser/renderer_startup_helper.h"
 #include "extensions/browser/user_script_manager.h"
 #include "extensions/common/extension.h"
@@ -266,6 +267,7 @@ bool IsExtensionIdle(const std::string& extension_id,
   }
 
   ProcessManager* process_manager = ProcessManager::Get(context);
+  ProcessMap* process_map = ProcessMap::Get(context);
   for (std::vector<std::string>::const_iterator i = ids_to_check.begin();
        i != ids_to_check.end();
        i++) {
@@ -274,14 +276,16 @@ bool IsExtensionIdle(const std::string& extension_id,
     if (host)
       return false;
 
-    scoped_refptr<content::SiteInstance> site_instance =
-        process_manager->GetSiteInstanceForURL(
-            Extension::GetBaseURLFromExtensionId(id));
-    if (site_instance && site_instance->HasProcess())
+    if (!process_manager->GetRenderFrameHostsForExtension(id).empty()) {
       return false;
+    }
 
-    if (!process_manager->GetRenderFrameHostsForExtension(id).empty())
+    // TODO(devlin): We can probably remove the checks above (for background
+    // hosts and frame hosts). If an extension has any active frames, it should
+    // have a dedicated process.
+    if (process_map->ExtensionHasProcess(id)) {
       return false;
+    }
   }
   return true;
 }
