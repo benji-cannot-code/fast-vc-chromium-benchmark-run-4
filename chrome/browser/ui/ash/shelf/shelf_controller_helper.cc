2772b999bd34dfd84bee91a01697193c533af5cd
renkens <renkens@google.com>
2024-11-27 10:24:50 +0000

    Removed unused metrics: shortcut_metrics.
    OBSOLETE_HISTOGRAMS=The metrics are expiring. All related code has been deleted (https://crbug.com/353891727, https://crbug.com/338393239).
    
    Bug: 374762810
    Change-Id: I74ed1ffb90b6496fffa8bd8e13a870c65cd32ab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048836
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Daniil Hrybchuk <renkens@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388724}

--- chrome/browser/ui/ash/shelf/shelf_controller_helper.cc
+++ chrome/browser/ui/ash/shelf/shelf_controller_helper.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/launch_utils.h"
-#include "chrome/browser/apps/app_service/metrics/shortcut_metrics.h"
 #include "chrome/browser/apps/app_service/package_id_util.h"
 #include "chrome/browser/apps/app_service/promise_apps/promise_app.h"
 #include "chrome/browser/apps/app_service/promise_apps/promise_app_registry_cache.h"
