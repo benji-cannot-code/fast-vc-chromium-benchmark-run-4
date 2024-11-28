4d87147e67ab9243fc4a2f55da37c0ebc16cfc38
Sorin Jianu <sorin@chromium.org>
2024-11-28 01:41:51 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/ash/sparky
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=tby@chromium.org
    
    Bug: 381119547
    Change-Id: I1b586233cd0aafb6dc9e56012a87fa6a6e376c8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055830
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389155}

--- chrome/browser/ash/sparky/storage/simple_size_calculator.cc
+++ chrome/browser/ash/sparky/storage/simple_size_calculator.cc
@@ -43,7 +43,7 @@ SimpleSizeCalculator::SimpleSizeCalculator(
     const CalculationType& calculation_type)
     : calculation_type_(calculation_type) {}
 
-SimpleSizeCalculator::~SimpleSizeCalculator() {}
+SimpleSizeCalculator::~SimpleSizeCalculator() = default;
 
 void SimpleSizeCalculator::StartCalculation() {
   if (calculating_) {
