7616ec95e1696d7fcec0da6f7ef3d8c929c9211b
Hiroki Nakagawa <nhiroki@chromium.org>
2024-11-28 15:45:12 +0000

    SearchPrefetch: Remove kEnsureSearchPrefetchServiceOnInterceptor
    kEnsureSearchPrefetchServiceOnInterceptor has been enabled by default
    for a while. The flag should no longer be necessary.
    
    Fixed: 40269568, 364445584
    Change-Id: I75ac14ce553bf1f4e1ad1a38ea76bc62368614d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055103
    Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389434}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/callback.h"
-#include "chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_factory.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader.h"
@@ -47,14 +46,7 @@ SearchPrefetchService* GetSearchPrefetchService(
   if (!profile) {
     return nullptr;
   }
-  // If the feature is enabled, ensure SearchPrefetchService so that the
-  // navigation can consult the search prefetch cache regardless of if
-  // SearchPrefetchService has been accessed before this line, for example,
-  // during browser startup.
-  if (base::FeatureList::IsEnabled(kEnsureSearchPrefetchServiceOnInterceptor)) {
-    return SearchPrefetchServiceFactory::GetForProfile(profile);
-  }
-  return SearchPrefetchServiceFactory::GetForProfileIfExists(profile);
+  return SearchPrefetchServiceFactory::GetForProfile(profile);
 }
 
 void SearchPrefetchRequestHandler(
