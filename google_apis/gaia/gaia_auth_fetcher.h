31df5bf9df6438efdfbdd030b2315d38e9a86080
Alex Ilin <alexilin@chromium.org>
2024-11-28 14:49:07 +0000

    [DBSC] Decrypt cookies from OAuthMultilogin response if needed
    OAuthMultilogin responses containing a "token_binding_directed_response"
    key (the value doesn't matter) are encrypted to an ephemeral key that
    was passed in token_binding_assertion.
    
    Upon receiving such a response, Chrome should decrypt all cookie values
    with this ephemeral key.
    
    A cookie must be decrypted before building a CanonicalCookie, which
    happens in the OAuthMultiloginResult constructor.
    
    Bug: 372648645
    Change-Id: I0e42e50683e24caf74b3588f26b36b86c13c58a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055160
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1389393}

--- google_apis/gaia/gaia_auth_fetcher.h
+++ google_apis/gaia/gaia_auth_fetcher.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_auth_consumer.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+#include "google_apis/gaia/oauth_multilogin_result.h"
 #include "net/base/net_errors.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/mojom/fetch_api.mojom.h"
@@ -125,7 +126,9 @@ class COMPONENT_EXPORT(GOOGLE_APIS) GaiaAuthFetcher {
   void StartOAuthMultilogin(
       gaia::MultiloginMode mode,
       const std::vector<gaia::MultiloginAccountAuthCredentials>& accounts,
-      const std::string& external_cc_result);
+      const std::string& external_cc_result,
+      OAuthMultiloginResult::CookieDecryptor cookie_decryptor =
+          base::NullCallback());
 
   // Starts a request to list the accounts in the GAIA cookie.
   void StartListAccounts();
@@ -267,6 +270,9 @@ class COMPONENT_EXPORT(GOOGLE_APIS) GaiaAuthFetcher {
   std::unique_ptr<network::SimpleURLLoader> url_loader_;
   GURL original_url_;
   std::string request_body_;
+
+  // Only populated in Multilogin requests.
+  OAuthMultiloginResult::CookieDecryptor oauth_multilogin_cookie_decryptor_;
 
   bool fetch_pending_ = false;
 
--- google_apis/gaia/oauth_multilogin_result.h
+++ google_apis/gaia/oauth_multilogin_result.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/component_export.h"
+#include "base/functional/callback.h"
+#include "base/functional/callback_helpers.h"
 #include "base/gtest_prod_util.h"
 #include "base/values.h"
 #include "google_apis/gaia/gaia_auth_util.h"
@@ -65,6 +67,9 @@ OAuthMultiloginResponseStatus ParseOAuthMultiloginResponseStatus(
 
 class COMPONENT_EXPORT(GOOGLE_APIS) OAuthMultiloginResult {
  public:
+  using CookieDecryptor =
+      base::RepeatingCallback<std::string(std::string_view)>;
+
   struct FailedAccount {
     std::string gaia_id;
 
@@ -77,7 +82,12 @@ class COMPONENT_EXPORT(GOOGLE_APIS) OAuthMultiloginResult {
   // Parses cookies and status from JSON response. Maps status to
   // GoogleServiceAuthError::State values or sets error to
   // UNEXPECTED_SERVER_RESPONSE if JSON string cannot be parsed.
-  OAuthMultiloginResult(const std::string& raw_data, int http_response_code);
+  // `cookie_decryptor` is optional and used only if the JSON response contains
+  // "token_binding_directed_response" object.
+  OAuthMultiloginResult(
+      const std::string& raw_data,
+      int http_response_code,
+      const CookieDecryptor& cookie_decryptor = base::NullCallback());
 
   explicit OAuthMultiloginResult(OAuthMultiloginResponseStatus status);
   OAuthMultiloginResult(const OAuthMultiloginResult& other);
@@ -95,7 +105,9 @@ class COMPONENT_EXPORT(GOOGLE_APIS) OAuthMultiloginResult {
   FRIEND_TEST_ALL_PREFIXES(OAuthMultiloginResultTest,
                            ParseRealResponseFromGaia_2021_10);
 
-  void TryParseCookiesFromValue(const base::Value::Dict& json_value);
+  void TryParseCookiesFromValue(
+      const base::Value::Dict& json_value,
+      const CookieDecryptor& decryptor = base::NullCallback());
 
   // If `status_` is `kInvalidTokens` or `kRetryWithTokenBindingChallenge`, the
   // response is expected to have a list of failed accounts for which tokens are
