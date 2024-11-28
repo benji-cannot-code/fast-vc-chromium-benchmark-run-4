08aaee546d5d885935e082f75cb5404661b9b072
Andrew Grieve <agrieve@chromium.org>
2024-11-28 03:27:21 +0000

    Android: Add directory and 3pp script for NullAway
    Bug: 40657579
    Change-Id: I1e62a1e33a0587e1082581393694db5a7ad3c93d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6045401
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389190}

---
+++ third_party/android_build_tools/nullaway/print_version.sh
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+DIR_SRC_ROOT=$(pwd)/
+DIR_SRC_ROOT=${DIR_SRC_ROOT%/src/*}/src
+SUBDIR=$(cd $(dirname $0); pwd)
+SUBDIR="${SUBDIR#$DIR_SRC_ROOT/}"
+CIPD_PACKAGE=chromium/$SUBDIR
+
+exec $DIR_SRC_ROOT/build/3pp_common/print_cipd_version.py \
+    --subdir "$SUBDIR/cipd" \
+    --cipd-package "$CIPD_PACKAGE" \
+    "$@"
