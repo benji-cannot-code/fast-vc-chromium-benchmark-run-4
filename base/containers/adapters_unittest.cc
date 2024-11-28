3721efcb0eec85ae49da0918293f2e4e9d7af2cd
Daniel Cheng <dcheng@chromium.org>
2024-11-28 08:04:31 +0000

    Introduce `base::RangeAsRvalues()` adapter
    This adapter wraps a range, exposing its elements as rvalues. Using the
    resulting range as an input range will move from (i.e. consume) the
    original values.
    
    Bug: 378623811
    Change-Id: I2122610a973e3ce6fe15cf4a3658e9aa9f270a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024072
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389261}

--- base/containers/adapters_unittest.cc
+++ base/containers/adapters_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/span.h"
+#include "base/containers/to_vector.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -144,6 +145,24 @@ TEST(AdaptersTest, ReversedConst) {
   for (int i : base::Reversed(cv)) {
     EXPECT_EQ(++j, i);
   }
+}
+
+TEST(AdaptersTest, RangeAsRvalues) {
+  std::vector<std::unique_ptr<int>> v;
+  v.push_back(std::make_unique<int>(1));
+  v.push_back(std::make_unique<int>(2));
+  v.push_back(std::make_unique<int>(3));
+
+  auto v2 = base::ToVector(base::RangeAsRvalues(std::move(v)));
+  EXPECT_EQ(1, *v2[0]);
+  EXPECT_EQ(2, *v2[1]);
+  EXPECT_EQ(3, *v2[2]);
+
+  // The old vector should be consumed. The standard guarantees that a
+  // moved-from std::unique_ptr will be null.
+  EXPECT_EQ(nullptr, v[0]);  // NOLINT(bugprone-use-after-move)
+  EXPECT_EQ(nullptr, v[1]);  // NOLINT(bugprone-use-after-move)
+  EXPECT_EQ(nullptr, v[2]);  // NOLINT(bugprone-use-after-move)
 }
 
 }  // namespace
