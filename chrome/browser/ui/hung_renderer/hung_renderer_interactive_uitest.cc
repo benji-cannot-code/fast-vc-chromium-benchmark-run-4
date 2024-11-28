7058a2aa7c759eec4e97e306a01a94af46cd63fb
Sorin Jianu <sorin@chromium.org>
2024-11-28 04:42:01 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/ui/hung_renderer
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=bsep@chromium.org
    
    Bug: 381119547
    Change-Id: I91b9978cb26acba8eea8856153822654b1f62027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056070
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389205}

--- chrome/browser/ui/hung_renderer/hung_renderer_interactive_uitest.cc
+++ chrome/browser/ui/hung_renderer/hung_renderer_interactive_uitest.cc
@@ -21,13 +21,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class HungRendererNavigationTest : public InProcessBrowserTest {
  public:
-  HungRendererNavigationTest() {}
+  HungRendererNavigationTest() = default;
 
   HungRendererNavigationTest(const HungRendererNavigationTest&) = delete;
   HungRendererNavigationTest& operator=(const HungRendererNavigationTest&) =
       delete;
 
-  ~HungRendererNavigationTest() override {}
+  ~HungRendererNavigationTest() override = default;
 
  protected:
   void SetUpOnMainThread() override {
