c729d1a9a78220efb4505a56c58cd2ed9f46ac1c
Timofey Chudakov <tchudakov@google.com>
2024-11-28 13:21:23 +0000

    Trigger plus address HaTS surveys via AutofillClient [2/4].
    This CL moves logic to trigger plus address HaTS surveys via
    the ChromeAutofillClient.
    
    Bug: 348139343
    Change-Id: Iffa74116ca60d1936c709600e76092cbc3ac8145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6043711
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1389359}

--- chrome/browser/keyboard_accessory/android/address_accessory_controller_impl_unittest.cc
+++ chrome/browser/keyboard_accessory/android/address_accessory_controller_impl_unittest.cc
@@ -114,6 +114,10 @@ class MockAutofillClient : public TestContentAutofillClient {
               GetLastCommittedPrimaryMainFrameOrigin,
               (),
               (const, override));
+  MOCK_METHOD(void,
+              TriggerPlusAddressUserPerceptionSurvey,
+              (plus_addresses::hats::SurveyType),
+              (override));
 };
 
 class MockAutofillDriver : public TestContentAutofillDriver {
@@ -580,6 +584,9 @@ TEST_F(AddressAccessoryControllerTest, FillsPlusAddressSuggestion) {
               ApplyFieldAction(mojom::FieldActionType::kReplaceAll,
                                mojom::ActionPersistence::kFill, field_id,
                                plus_address));
+  EXPECT_CALL(autofill_client(), TriggerPlusAddressUserPerceptionSurvey(
+                                     plus_addresses::hats::SurveyType::
+                                         kFilledPlusAddressViaManualFallack));
   controller()->OnFillingTriggered(
       field_id, AccessorySheetField::Builder()
                     .SetSuggestionType(AccessorySuggestionType::kPlusAddress)
