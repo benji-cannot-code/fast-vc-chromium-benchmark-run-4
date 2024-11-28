b8522e62b9ed9c821e2fc8679f078fbd27ee273c
Henrik Boström <hbos@chromium.org>
2024-11-28 16:01:24 +0000

    Delete ThermalThrottling/ThermalState UMA metrics that have expired.
    These signals are no longer used for UMA collection reasons (expired
    histograms, no interest in extending them). They are still collected by
    the browser process for PowerThermalObserver and wired up to
    MediaStreamPowerLogger for logging purposes but the *UmaListeners can be
    deleted and PeerConnectionTracker code can be partially cleaned up:
    - OnSpeedLimitChange: Deleted as it was only used for UMA.
    - OnThermalStateChange: Kept since it is used by ThermalResource (for
      now?) but the forwarding to the UMA listener can be deleted.
    
    OBSOLETE_HISTOGRAMS=Deleting obsolete WebRTC.PeerConnection.ThermalThrottling* and WebRTC.PeerConnection.ThermalState*
    
    Bug: chromium:369136239, chromium:364019566
    Change-Id: I39d139968e1144a2214cf9a10067096cc165e118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037694
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Evan Shrubsole <eshr@google.com>
    Auto-Submit: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389444}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.h
@@ -23,9 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/peerconnection/media_stream_track_metrics.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver_impl.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_rtp_sender_impl.h"
-#include "third_party/blink/renderer/modules/peerconnection/speed_limit_uma_listener.h"
 #include "third_party/blink/renderer/modules/peerconnection/thermal_resource.h"
-#include "third_party/blink/renderer/modules/peerconnection/thermal_uma_listener.h"
 #include "third_party/blink/renderer/modules/peerconnection/transceiver_state_surfacer.h"
 #include "third_party/blink/renderer/modules/peerconnection/webrtc_media_stream_track_adapter_map.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -212,8 +210,6 @@ class MODULES_EXPORT RTCPeerConnectionHandler {
   // Virtual for testing purposes.
   virtual void OnThermalStateChange(
       mojom::blink::DeviceThermalState thermal_state);
-  // Invoked when a new CPU speed limit is received from the OS.
-  virtual void OnSpeedLimitChange(int32_t speed_limit);
 
   // Start recording an event log.
   void StartEventLog(int output_period_ms);
@@ -284,10 +280,6 @@ class MODULES_EXPORT RTCPeerConnectionHandler {
                            int error_code,
                            const String& error_text);
   void OnInterestingUsage(int usage_pattern);
-
-  // Protected for testing.
-  ThermalUmaListener* thermal_uma_listener() const;
-  SpeedLimitUmaListener* speed_limit_uma_listener() const;
 
  private:
   // Record info about the first SessionDescription from the local and
@@ -428,15 +420,6 @@ class MODULES_EXPORT RTCPeerConnectionHandler {
   // The Thermal Resource is lazily instantiated on platforms where thermal
   // signals are supported.
   scoped_refptr<ThermalResource> thermal_resource_;
-  // ThermalUmaListener is only tracked on peer connection that add a track.
-  std::unique_ptr<ThermalUmaListener> thermal_uma_listener_;
-  mojom::blink::DeviceThermalState last_thermal_state_ =
-      mojom::blink::DeviceThermalState::kUnknown;
-  // Last received OS speed limit.
-  int32_t last_speed_limit_ = mojom::blink::kSpeedLimitMax;
-  // SpeedLimitUmaListener is only tracked on peer connection that add an audio
-  // or video track.
-  std::unique_ptr<SpeedLimitUmaListener> speed_limit_uma_listener_;
 
   // Record info about the first SessionDescription from the local and
   // remote side to record UMA stats once both are set.  We only check
