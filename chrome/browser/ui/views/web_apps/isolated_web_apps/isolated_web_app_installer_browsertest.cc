3b3755b6782da2014b4b443bc64d74f9a051de8f
Finnur Thorarinsson <finnur@chromium.org>
2024-11-28 12:07:33 +0000

    [NavCapture]: WebAppRegistrar migration part 2a.
    This change is for directory/directories:
    chrome/browser/ui/views/web_apps/isolated_web_apps
    
    In the first (previous) part, a few functions from WebAppRegistrar
    were migrated over to IsInstallState, in an effort to flush out
    incorrect uses. This CL changes some of those call sites (mostly
    tests) to GetInstallSite, which is a more appropriate API
    (especially for tests).
    
    This CL was uploaded by git cl split.
    
    R=simonha@google.com
    
    DD: https://docs.google.com/document/d/14JPgYUVX8ECAMdXlp6GwXP-ucXnIIPXjTfxWt-WINLo/edit?usp=sharing
    Bug: 379827962
    Change-Id: Iba251b67f6d8d455eb68c761af9a5132692606f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055463
    Reviewed-by: Simon Hangl <simonha@google.com>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389337}

--- chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_browsertest.cc
+++ chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_browsertest.cc
@@ -109,8 +109,7 @@ IN_PROC_BROWSER_TEST_F(IsolatedWebAppInstallerBrowserTest,
   ASSERT_TRUE(child_widget);
 
   // App is not installed.
-  ASSERT_FALSE(provider().registrar_unsafe().IsInstallState(
-      app_id, {proto::InstallState::INSTALLED_WITH_OS_INTEGRATION}));
+  ASSERT_TRUE(provider().registrar_unsafe().IsNotInRegistrar(app_id));
 
   AcceptDialogAndAwaitDestruction(child_widget);
 
@@ -120,8 +119,8 @@ IN_PROC_BROWSER_TEST_F(IsolatedWebAppInstallerBrowserTest,
       IsolatedWebAppInstallerModel::Step::kInstallSuccess);
 
   // App is installed.
-  ASSERT_TRUE(provider().registrar_unsafe().IsInstallState(
-      app_id, {proto::InstallState::INSTALLED_WITH_OS_INTEGRATION}));
+  ASSERT_EQ(proto::InstallState::INSTALLED_WITH_OS_INTEGRATION,
+            provider().registrar_unsafe().GetInstallState(app_id));
 
   AcceptDialogAndContinue(main_widget);
 
