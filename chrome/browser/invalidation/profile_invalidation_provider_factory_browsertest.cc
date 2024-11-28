3c644ba0ce128ead466f0568c56c08e963cc1e1e
Sorin Jianu <sorin@chromium.org>
2024-11-28 16:08:01 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/invalidation
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=asumaneev@google.com
    
    Bug: 381119547
    Change-Id: Iab6edf943eea9a04582e6fefbe2fe761dbf92f07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055832
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389447}

--- chrome/browser/invalidation/profile_invalidation_provider_factory_browsertest.cc
+++ chrome/browser/invalidation/profile_invalidation_provider_factory_browsertest.cc
@@ -46,10 +46,10 @@ class ProfileInvalidationProviderFactoryTestBase : public InProcessBrowserTest {
 };
 
 ProfileInvalidationProviderFactoryTestBase::
-    ProfileInvalidationProviderFactoryTestBase() {}
+    ProfileInvalidationProviderFactoryTestBase() = default;
 
 ProfileInvalidationProviderFactoryTestBase::
-    ~ProfileInvalidationProviderFactoryTestBase() {}
+    ~ProfileInvalidationProviderFactoryTestBase() = default;
 
 bool ProfileInvalidationProviderFactoryTestBase::
     CanConstructProfileInvalidationProvider(Profile* profile) {
@@ -73,10 +73,10 @@ class ProfileInvalidationProviderFactoryLoginScreenBrowserTest
 };
 
 ProfileInvalidationProviderFactoryLoginScreenBrowserTest::
-    ProfileInvalidationProviderFactoryLoginScreenBrowserTest() {}
+    ProfileInvalidationProviderFactoryLoginScreenBrowserTest() = default;
 
 ProfileInvalidationProviderFactoryLoginScreenBrowserTest::
-    ~ProfileInvalidationProviderFactoryLoginScreenBrowserTest() {}
+    ~ProfileInvalidationProviderFactoryLoginScreenBrowserTest() = default;
 
 void ProfileInvalidationProviderFactoryLoginScreenBrowserTest::SetUpCommandLine(
     base::CommandLine* command_line) {
@@ -109,10 +109,10 @@ class ProfileInvalidationProviderFactoryGuestBrowserTest
 };
 
 ProfileInvalidationProviderFactoryGuestBrowserTest::
-    ProfileInvalidationProviderFactoryGuestBrowserTest() {}
+    ProfileInvalidationProviderFactoryGuestBrowserTest() = default;
 
 ProfileInvalidationProviderFactoryGuestBrowserTest::
-    ~ProfileInvalidationProviderFactoryGuestBrowserTest() {}
+    ~ProfileInvalidationProviderFactoryGuestBrowserTest() = default;
 
 void ProfileInvalidationProviderFactoryGuestBrowserTest::SetUpCommandLine(
     base::CommandLine* command_line) {
