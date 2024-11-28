73f3eebb1de9135b93608437b3b9500bca69802b
Andrii Natiahlyi <natiahlyi@google.com>
2024-11-28 10:46:54 +0000

    Enable unsafe buffer warnings in password_change_ui.cc
    Bug: 40285824
    Change-Id: I5cd6513f70a9674fe4efe9d943a416d0ad59444c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054363
    Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Andrey Davydov <andreydav@google.com>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389321}

--- chrome/browser/ui/webui/ash/in_session_password_change/password_change_ui.cc
+++ chrome/browser/ui/webui/ash/in_session_password_change/password_change_ui.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/browser/ui/webui/ash/in_session_password_change/password_change_ui.h"
 
 #include <memory>
@@ -110,13 +105,11 @@ PasswordChangeUI::PasswordChangeUI(content::WebUI* web_ui)
   source->AddString("hostedHeader", GetHostedHeaderText(password_change_url));
   source->UseStringsJs();
 
-  source->AddResourcePaths(
-      base::make_span(kPasswordChangeResources, kPasswordChangeResourcesSize));
+  source->AddResourcePaths(base::span(kPasswordChangeResources));
   source->SetDefaultResource(IDR_PASSWORD_CHANGE_PASSWORD_CHANGE_APP_HTML);
 
   // Add Gaia Authenticator resources
-  source->AddResourcePaths(
-      base::make_span(kGaiaAuthHostResources, kGaiaAuthHostResourcesSize));
+  source->AddResourcePaths(base::span(kGaiaAuthHostResources));
 }
 
 PasswordChangeUI::~PasswordChangeUI() = default;
@@ -161,8 +154,7 @@ ConfirmPasswordChangeUI::ConfirmPasswordChangeUI(content::WebUI* web_ui)
 
   source->UseStringsJs();
 
-  source->AddResourcePaths(
-      base::make_span(kPasswordChangeResources, kPasswordChangeResourcesSize));
+  source->AddResourcePaths(base::span(kPasswordChangeResources));
   source->SetDefaultResource(
       IDR_PASSWORD_CHANGE_CONFIRM_PASSWORD_CHANGE_APP_HTML);
 
@@ -207,8 +199,7 @@ UrgentPasswordExpiryNotificationUI::UrgentPasswordExpiryNotificationUI(
 
   source->UseStringsJs();
 
-  source->AddResourcePaths(
-      base::make_span(kPasswordChangeResources, kPasswordChangeResourcesSize));
+  source->AddResourcePaths(base::span(kPasswordChangeResources));
   source->SetDefaultResource(
       IDR_PASSWORD_CHANGE_URGENT_PASSWORD_EXPIRY_NOTIFICATION_HTML);
 
