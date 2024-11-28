42eeccc503c5c8d495073ae7155dd6a672d2c678
Sohail Rajdev <sorajdev@microsoft.com>
2024-11-28 13:40:31 +0000

    Truncate long extension names in settings overridden dialog
    This CL truncates long extension names when they are displayed on the
    settings overridden dialog. This dialog is shown when extensions
    override the new tab page or the browser search engine for the first
    time.
    
    Long names can break the UI (e.g. other UI elements can go out of the
    screen). In the worst case, it can trick the user into doing something
    unintentional. Thus, we truncate the extension name before displaying
    it.
    
    Before:
    - https://ibb.co/p3TKw1m
    - https://ibb.co/QFVdKYX
    
    After:
    - https://ibb.co/CW6WJkZ
    - https://ibb.co/stGWb4f
    
    Bug: 40063885
    Change-Id: I2ba42e2bcc6d1a7bc88ea5e8f2e7940a1e5bd87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054657
    Commit-Queue: Sohail Rajdev <sorajdev@microsoft.com>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389368}

--- chrome/browser/ui/extensions/settings_overridden_params_providers.cc
+++ chrome/browser/ui/extensions/settings_overridden_params_providers.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/branding_buildflags.h"
+#include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/extension_web_ui.h"
 #include "chrome/browser/extensions/settings_api_helpers.h"
 #include "chrome/browser/profiles/profile.h"
@@ -206,7 +207,7 @@ std::optional<ExtensionSettingsOverriddenDialog::Params> GetNtpOverriddenParams(
 
   std::u16string dialog_message = l10n_util::GetStringFUTF16(
       IDS_EXTENSION_NTP_OVERRIDDEN_DIALOG_BODY_GENERIC,
-      base::UTF8ToUTF16(extension->name().c_str()));
+      extensions::util::GetFixupExtensionNameForUIDisplay(extension->name()));
 
   return ExtensionSettingsOverriddenDialog::Params(
       extension->id(), preference_name, histogram_name, std::move(dialog_title),
@@ -296,7 +297,7 @@ GetSearchOverriddenParams(Profile* profile) {
   }
   std::u16string dialog_message = l10n_util::GetStringFUTF16(
       IDS_EXTENSION_SEARCH_OVERRIDDEN_DIALOG_BODY_GENERIC, formatted_search_url,
-      base::UTF8ToUTF16(extension->name().c_str()));
+      extensions::util::GetFixupExtensionNameForUIDisplay(extension->name()));
 
   return ExtensionSettingsOverriddenDialog::Params(
       extension->id(), preference_name, histogram_name, std::move(dialog_title),
