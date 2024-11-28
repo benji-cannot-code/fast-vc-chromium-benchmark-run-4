fb7fab730f190273f7e0d5169b15008e608f5732
Xiqi Ruan <xiqiruan@google.com>
2024-11-28 00:07:26 +0000

    demo_mode: Re-launch demo mode app on Device idle for 90s
    Re-launch demo mode app on device idle for 90s for demo account sign in
    session. Currently we are using PowerManagementIdleSettings to handling
    idle action in demo mode, however there is no easy way to introduce a
    customize action (i.e. close windows, delete files, launch apps...) via
    this policy. Replace with the specific demo mode idle handler in sign-in
    experience.
    
    Bug: 380941084
    Change-Id: I1193d8944f9867996e44f077dbf2e8272ddd86bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050903
    Reviewed-by: Haifan Wang <wanghaifan@google.com>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389123}

--- chrome/browser/ash/login/demo_mode/demo_session.h
+++ chrome/browser/ash/login/demo_mode/demo_session.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_multi_source_observation.h"
 #include "base/scoped_observation.h"
 #include "base/values.h"
+#include "chrome/browser/ash/login/demo_mode/demo_mode_idle_handler.h"
 #include "chrome/browser/ash/login/demo_mode/demo_mode_window_closer.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "components/component_updater/ash/component_manager_ash.h"
@@ -240,6 +241,10 @@ class DemoSession : public session_manager::SessionManagerObserver,
 
   // Keep track of which app has been installed in demo mode.
   std::set<std::string> installed_app_;
+
+  // Handle device idle action for demo mode. Affect both MGS and demo account
+  // sessions. Constructed while demo app is available.
+  std::unique_ptr<DemoModeIdleHandler> idle_handler_;
 
   base::WeakPtrFactory<DemoSession> weak_ptr_factory_{this};
 };
