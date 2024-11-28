34d9192ead7ddf0ee1197384b5dc58731fcb695c
Olesia Marukhno <olesiamarukhno@google.com>
2024-11-28 08:59:56 +0000

    [cleanup] Clean up RichHoverButton constructor
    Clean up RichHoverButton constructor params:
    * remove |tooltip_text| and set it separately when needed
    * deprecate |secondary_text| which is only used by "Last visited" button
    in page info
    
    |secondary_text| was previously used by page info "Cookies" button but
    UI changed since then.
    
    "Last visited" feature was never launched, we have to consider cleaning up the code. For now, move the timestamp from secondary label to subtitle, matching all other buttons.
    
    Bug: 381215941
    Change-Id: I0830d9ebf826585d934d06f388578e3d58e263d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054072
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1389277}

--- chrome/browser/ui/views/controls/rich_hover_button.cc
+++ chrome/browser/ui/views/controls/rich_hover_button.cc
@@ -68,8 +68,6 @@ RichHoverButton::RichHoverButton(
     views::Button::PressedCallback callback,
     const ui::ImageModel& main_image_icon,
     const std::u16string& title_text,
-    const std::u16string& secondary_text,
-    const std::u16string& tooltip_text,
     const std::u16string& subtitle_text,
     std::optional<ui::ImageModel> action_image_icon,
     std::optional<ui::ImageModel> state_icon)
@@ -94,14 +92,10 @@ RichHoverButton::RichHoverButton(
       // Column for title.
       .AddColumn(views::LayoutAlignment::kStretch,
                  views::LayoutAlignment::kCenter, 1.0f,
-                 views::TableLayout::ColumnSize::kUsePreferred, 0, 0)
-      .AddPaddingColumn(views::TableLayout::kFixedSize, icon_label_spacing)
-      // Column for |secondary_text|.
-      .AddColumn(views::LayoutAlignment::kEnd, views::LayoutAlignment::kStretch,
-                 views::TableLayout::kFixedSize,
                  views::TableLayout::ColumnSize::kUsePreferred, 0, 0);
 
   if (state_icon.has_value()) {
+    has_state_icon_ = true;
     table_layout
         // Column for |state_icon|.
         ->AddPaddingColumn(views::TableLayout::kFixedSize, icon_label_spacing)
@@ -135,16 +129,7 @@ RichHoverButton::RichHoverButton(
   title_ = AddChildView(std::move(title_label));
   title_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   title_->SetCanProcessEventsWithinSubtree(false);
-
-  auto secondary_label = std::make_unique<views::Label>(
-      std::u16string(), views::style::CONTEXT_LABEL,
-      views::style::STYLE_SECONDARY);
-  secondary_label->SetHorizontalAlignment(gfx::ALIGN_RIGHT);
-  secondary_label_ = AddChildView(std::move(secondary_label));
-
   title_->SetTextStyle(views::style::STYLE_BODY_3_MEDIUM);
-  secondary_label_->SetTextStyle(views::style::STYLE_BODY_5);
-  secondary_label_->SetEnabledColorId(ui::kColorLabelForegroundSecondary);
 
   // State icon is optional and column is created only when it is set.
   if (state_icon.has_value()) {
@@ -160,13 +145,11 @@ RichHoverButton::RichHoverButton(
 
   if (!title_text.empty()) {
     SetTitleText(title_text);
-    if (!secondary_text.empty())
-      SetSecondaryText(secondary_text);
   }
 
   if (!subtitle_text.empty()) {
     table_layout->AddRows(1, views::TableLayout::kFixedSize);
-    AddChildView(std::make_unique<views::View>());
+    AddChildView(std::make_unique<views::View>());  // main icon column
     auto subtitle = std::make_unique<views::Label>(
         subtitle_text, views::style::CONTEXT_LABEL,
         views::style::STYLE_SECONDARY);
@@ -178,14 +161,12 @@ RichHoverButton::RichHoverButton(
     subtitle_->SetMultiLine(true);
     subtitle_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
     subtitle_->SetAutoColorReadabilityEnabled(false);
-    AddChildView(std::make_unique<views::View>());
-    AddChildView(std::make_unique<views::View>());
+    AddFillerViews();
   }
 
   SetBorder(views::CreateEmptyBorder(layout_provider->GetInsetsMetric(
       ChromeInsetsMetric::INSETS_PAGE_INFO_HOVER_BUTTON)));
 
-  SetTooltipText(tooltip_text);
   UpdateAccessibleName();
 
   DeprecatedLayoutImmediately();
@@ -194,12 +175,6 @@ RichHoverButton::RichHoverButton(
 void RichHoverButton::SetTitleText(const std::u16string& title_text) {
   DCHECK(title_);
   title_->SetText(title_text);
-  UpdateAccessibleName();
-}
-
-void RichHoverButton::SetSecondaryText(const std::u16string& secondary_text) {
-  DCHECK(secondary_label_);
-  secondary_label_->SetText(secondary_text);
   UpdateAccessibleName();
 }
 
@@ -217,10 +192,9 @@ template <typename T>
 T* RichHoverButton::AddCustomSubtitle(std::unique_ptr<T> custom_view) {
   static_cast<views::TableLayout*>(GetLayoutManager())
       ->AddRows(1, views::TableLayout::kFixedSize);
-  AddChildView(std::make_unique<views::View>());
+  AddChildView(std::make_unique<views::View>());  // main icon column
   auto* view = AddChildView(std::move(custom_view));
-  AddChildView(std::make_unique<views::View>());
-  AddChildView(std::make_unique<views::View>());
+  AddFillerViews();
   return view;
 }
 
@@ -233,16 +207,19 @@ const views::Label* RichHoverButton::GetSubTitleViewForTesting() const {
 }
 
 void RichHoverButton::UpdateAccessibleName() {
-  const std::u16string title_text =
-      secondary_label_ == nullptr
-          ? title_->GetText()
-          : base::JoinString({title_->GetText(), secondary_label_->GetText()},
-                             u" ");
+  const std::u16string title_text = title_->GetText();
   const std::u16string accessible_name =
       subtitle_ == nullptr
           ? title_text
           : base::JoinString({title_text, subtitle_->GetText()}, u"\n");
   HoverButton::GetViewAccessibility().SetName(accessible_name);
+}
+
+void RichHoverButton::AddFillerViews() {
+  if (has_state_icon_) {
+    AddChildView(std::make_unique<views::View>());
+  }
+  AddChildView(std::make_unique<views::View>());
 }
 
 gfx::Size RichHoverButton::CalculatePreferredSize(
