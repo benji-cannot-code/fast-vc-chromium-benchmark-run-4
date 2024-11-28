cf1c03d36667c389fbeecaf4b28d25a2871a0ada
Fredrik Söderquist <fs@opera.com>
2024-11-28 22:27:20 +0000

    Simplify a couple of DOMUint8Array to Vector<uint8_t> appends
    Change-Id: I8d465cf90f3a1eed9e93756899b109a1ed9b8ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054215
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389559}

--- third_party/blink/renderer/modules/direct_sockets/tcp_readable_stream_wrapper_unittest.cc
+++ third_party/blink/renderer/modules/direct_sockets/tcp_readable_stream_wrapper_unittest.cc
@@ -81,8 +81,7 @@ class StreamCreator : public GarbageCollected<StreamCreator> {
       ADD_FAILURE() << "chunk is not an Uint8Array";
       return ret;
     }
-    ret.Append(static_cast<uint8_t*>(value->Data()),
-               static_cast<wtf_size_t>(value->byteLength()));
+    ret.AppendSpan(value->ByteSpan());
     return ret;
   }
 
