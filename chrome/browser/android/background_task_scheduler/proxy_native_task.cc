779a797e4ab0d01c184090cf87e50730edcd2e10
Sorin Jianu <sorin@chromium.org>
2024-11-28 10:07:05 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    ['chrome/browser/android/background_task_scheduler', 'chrome/browser/push_messaging']
    
    This is a mechanical change. There are a few instances of empty user-defined
    constructors and destructors left in the /chrome code which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: 381119547
    Change-Id: I9d041c3ef36fd175ea98c362d2dad8d675f0e418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055935
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1389306}

--- chrome/browser/android/background_task_scheduler/proxy_native_task.cc
+++ chrome/browser/android/background_task_scheduler/proxy_native_task.cc
@@ -50,7 +50,7 @@ ProxyNativeTask::ProxyNativeTask(
       task_params_(std::move(task_params)),
       finish_callback_(std::move(finish_callback)) {}
 
-ProxyNativeTask::~ProxyNativeTask() {}
+ProxyNativeTask::~ProxyNativeTask() = default;
 
 void ProxyNativeTask::Destroy(JNIEnv* env,
                               const JavaParamRef<jobject>& jcaller) {
