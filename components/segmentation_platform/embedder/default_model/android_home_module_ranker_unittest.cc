b7c2578cb45bd8359af60f4a42d1954ea522a36a
Xi Han <hanxi@google.com>
2024-11-28 00:09:07 +0000

    [AppIntegration] Rank Auxiliary search module in magic stack.
    Use the biggest weight for the auxiliary search module card to make it
    shown as the first module if possible.
    
    Bug: 380086456
    Change-Id: I065196f85ab375c490b22b063695085c206e5685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056386
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389124}

--- components/segmentation_platform/embedder/default_model/android_home_module_ranker_unittest.cc
+++ components/segmentation_platform/embedder/default_model/android_home_module_ranker_unittest.cc
@@ -22,7 +22,7 @@ class AndroidHomeModuleRankerTest : public DefaultModelTestBase {
     DefaultModelTestBase::SetUp();
     bool isAndroidHomeModuleRankerV2Enabled = base::FeatureList::IsEnabled(
         features::kSegmentationPlatformAndroidHomeModuleRankerV2);
-    input_size = isAndroidHomeModuleRankerV2Enabled ? 12 : 8;
+    input_size = isAndroidHomeModuleRankerV2Enabled ? 15 : 10;
   }
 
   void TearDown() override { DefaultModelTestBase::TearDown(); }
@@ -41,8 +41,9 @@ TEST_F(AndroidHomeModuleRankerTest, ExecuteModelWithInputForAllModules) {
 
   EXPECT_FALSE(ExecuteWithInput(/*inputs=*/{}));
   std::vector<float> input(input_size, 0);
-  ExpectClassifierResults(input, {kPriceChange, kSingleTab,
-                                  kTabResumptionForAndroidHome, kSafetyHub});
+  ExpectClassifierResults(
+      input, {kPriceChange, kSingleTab, kTabResumptionForAndroidHome,
+              kSafetyHub, kAuxiliarySearch});
 }
 
 TEST_F(AndroidHomeModuleRankerTest,
@@ -56,8 +57,10 @@ TEST_F(AndroidHomeModuleRankerTest,
   input[2] = 1;
   input[4] = 1;
   input[6] = 1;
-  ExpectClassifierResults(input, {kSafetyHub, kPriceChange, kSingleTab,
-                                  kTabResumptionForAndroidHome});
+  input[8] = 1;
+  ExpectClassifierResults(input,
+                          {kSafetyHub, kPriceChange, kSingleTab,
+                           kTabResumptionForAndroidHome, kAuxiliarySearch});
 }
 
 TEST_F(AndroidHomeModuleRankerTest,
@@ -71,8 +74,10 @@ TEST_F(AndroidHomeModuleRankerTest,
   input[3] = 1;
   input[5] = 1;
   input[7] = 1;
-  ExpectClassifierResults(input, {kSingleTab, kTabResumptionForAndroidHome,
-                                  kPriceChange, kSafetyHub});
+  input[9] = 1;
+  ExpectClassifierResults(
+      input, {kAuxiliarySearch, kSingleTab, kTabResumptionForAndroidHome,
+              kPriceChange, kSafetyHub});
 }
 
 }  // namespace segmentation_platform
