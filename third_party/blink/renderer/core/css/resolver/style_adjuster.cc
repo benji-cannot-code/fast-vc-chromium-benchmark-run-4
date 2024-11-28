78dfe12ec002e95d9b6262bb5e2a06e5fcbfb412
Morten Stenshorne <mstensho@chromium.org>
2024-11-28 14:07:49 +0000

    [carousel] Blockify ::scroll-marker-group.
    Given the nature of scroll marker groups, being populated after layout
    of the scrollable container and all, requiring containment, means that
    we cannot allow them to be inline. We *could* allow them to be
    layout-block, for instance, but for now, just blockify them, since
    that's a well-known concept (whereas there's no such thing as
    inlineblockification...).
    
    scroll-marker-group-add-dynamic.html, as it was, would pass with this
    change, since 'display' would now always be 'block'. Change it to 'flex'
    -> 'block' to keep it failing, since we still don't handle scroll marker
    group re-attachment correctly.
    
    Bug: 380623037
    Change-Id: I7d1790d32cce6e80ff1a0b16c40c5bfd0e066602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054082
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389380}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -743,6 +743,10 @@ void StyleAdjuster::AdjustStyleForDisplay(
     }
   }
 
+  if (builder.StyleType() == kPseudoIdScrollMarkerGroup) {
+    builder.SetDisplay(EquivalentBlockDisplay(builder.Display()));
+  }
+
   if (builder.Display() == EDisplay::kBlock) {
     return;
   }
