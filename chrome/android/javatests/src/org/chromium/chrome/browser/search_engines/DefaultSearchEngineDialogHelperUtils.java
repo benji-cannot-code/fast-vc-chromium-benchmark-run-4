635216f575f3ec163fc07225efaeffe1accfacf8
Samar <samarchehade@google.com>
2024-11-28 10:39:27 +0000

    [UNOp2][Android] Enable Uno by default in the FRE integration tests.
    Bug: 41493794
    Fixed: 346755013
    Change-Id: Ia192e133db4e2a9e649edd7f67012202189fd5f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937809
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389315}

--- chrome/android/javatests/src/org/chromium/chrome/browser/search_engines/DefaultSearchEngineDialogHelperUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/search_engines/DefaultSearchEngineDialogHelperUtils.java
@@ -32,6 +32,7 @@ public class DefaultSearchEngineDialogHelperUtils {
         CriteriaHelper.pollUiThread(
                 () -> {
                     ViewGroup options = rootView.findViewById(OPTION_LAYOUT_ID);
+                    Criteria.checkThat(options, Matchers.notNullValue());
                     Criteria.checkThat(options.getChildCount(), Matchers.greaterThan(0));
                 });
 
@@ -43,7 +44,7 @@ public class DefaultSearchEngineDialogHelperUtils {
                     sSelectedEngine = (String) options.getChildAt(0).getTag();
                 });
 
-        // Wait for the OK button to be clicakble.
+        // Wait for the OK button to be clickable.
         CriteriaHelper.pollUiThread(
                 () -> {
                     View view = rootView.findViewById(OK_BUTTON_ID);
