a52be744ddd447898bec87edff87aa18cdda0172
Christian Xu <christianxu@google.com>
2024-11-28 11:50:47 +0000

    [Code Health][iOS][Settings] Move google_services to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    There are too many files in ios/chrome/browser/ui/settings, this CL
    is moving part of it. Following CLs will move the rest.
    
    R=gambard
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 324233364
    Change-Id: I9b92de8ff75496895f713bb79067e106ade0c599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054209
    Reviewed-by: Jood Hajeer <jood@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389333}

---
+++ ios/chrome/browser/settings/ui_bundled/google_services/bulk_upload/bulk_upload_consumer.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_BULK_UPLOAD_BULK_UPLOAD_CONSUMER_H_
+#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_BULK_UPLOAD_BULK_UPLOAD_CONSUMER_H_
+
+#import <UIKit/UIKit.h>
+
+#import <vector>
+
+@class BulkUploadViewItem;
+
+// Consumer for the BulkUpload view controller
+@protocol BulkUploadConsumer <NSObject>
+
+// Sets the enabled state of the button.
+- (void)setValidationButtonEnabled:(BOOL)enabled;
+
+// Update the view controller about new bookmark data.
+- (void)updateViewWithViewItems:(NSArray<BulkUploadViewItem*>*)viewItems;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_BULK_UPLOAD_BULK_UPLOAD_CONSUMER_H_
--- ios/chrome/browser/ui/authentication/account_menu/account_menu_mediator_delegate.h
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_mediator_delegate.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import "ios/chrome/browser/settings/ui_bundled/google_services/sync_error_settings_command_handler.h"
 #import "ios/chrome/browser/shared/public/commands/show_signin_command.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_constants.h"
-#import "ios/chrome/browser/ui/settings/google_services/sync_error_settings_command_handler.h"
 
 @class AccountMenuMediator;
 @class AuthenticationFlow;
