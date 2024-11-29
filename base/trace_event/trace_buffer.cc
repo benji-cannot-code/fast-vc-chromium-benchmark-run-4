605d26c2f6fcc9a7e1e331370e4f1552f2d63aa8
Luc Nguyen <lucnguyen@google.com>
2024-11-29 16:50:47 +0000

    [Health] Change unique_ptr<[]> to HeapArray in TraceBufferRingBuffer
    Use a type that tracks its size. Could also use a vector, but that's
    re-sizeable so not 100% semantically correct here.
    
    No-op.
    
    Bug: 40285824
    Change-Id: I6b0bd7404dc46e2b06f6fe38da83df28b6d1b39f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6046867
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389851}

--- base/trace_event/trace_buffer.cc
+++ base/trace_event/trace_buffer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/containers/heap_array.h"
 #include "base/functional/bind.h"
 #include "base/trace_event/heap_profiler.h"
 #include "base/trace_event/trace_event_impl.h"
@@ -27,11 +28,8 @@ class TraceBufferRingBuffer : public TraceBuffer {
  public:
   TraceBufferRingBuffer(size_t max_chunks)
       : max_chunks_(max_chunks),
-        recyclable_chunks_queue_(new size_t[queue_capacity()]),
-        queue_head_(0),
-        queue_tail_(max_chunks),
-        current_iteration_index_(0),
-        current_chunk_seq_(1) {
+        recyclable_chunks_queue_(HeapArray<size_t>::Uninit(queue_capacity())),
+        queue_tail_(max_chunks) {
     chunks_.reserve(max_chunks);
     for (size_t i = 0; i < max_chunks; ++i)
       recyclable_chunks_queue_[i] = i;
@@ -150,12 +148,12 @@ class TraceBufferRingBuffer : public TraceBuffer {
   size_t max_chunks_;
   std::vector<std::unique_ptr<TraceBufferChunk>> chunks_;
 
-  std::unique_ptr<size_t[]> recyclable_chunks_queue_;
-  size_t queue_head_;
+  HeapArray<size_t> recyclable_chunks_queue_;
+  size_t queue_head_ = 0;
   size_t queue_tail_;
 
-  size_t current_iteration_index_;
-  uint32_t current_chunk_seq_;
+  size_t current_iteration_index_ = 0;
+  uint32_t current_chunk_seq_ = 1;
 };
 
 class TraceBufferVector : public TraceBuffer {
