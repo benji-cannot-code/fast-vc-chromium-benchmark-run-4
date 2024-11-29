51d512196b71565bd08238c3df920fba6cc570da
Radu Nitescu <radunitescu@google.com>
2024-11-29 13:22:41 +0000

    [iOS][LO] Refactor results bottom sheet presentation
    A new presenter class is introduced. This class contains the
    presentation and detents management logic. This presenter is decoupled
    from the selection UI.
    
    This is mostly a mechanical refactor with no logic changes
    
    Fixed: 375605280
    Change-Id: I154de2424d3531c5d31f7dc000ca2d824698d04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054221
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389758}

--- ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay.mm
+++ ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay.mm
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return NO;
 }
 
-- (BOOL)isPanningSelectionUI {
-  return NO;
-}
-
 - (void)setQueryText:(NSString*)text clearSelection:(BOOL)clearSelection {
   _currentQueryText = text;
   [self sendNewResult];
