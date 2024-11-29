b4dba3e92f99529f8b6cd31154539b0ec495264a
Ken Buchanan <kenrb@chromium.org>
2024-11-29 16:43:15 +0000

    [WebAuthn] IDL and initial plumbing for challengeUrl
    This adds a runtime flag, an IDL change and the mojom plumbing for
    the challengeUrl field.
    
    Bug: 381219428
    Change-Id: I06338598c04a52a41f15bcfaa8fd139d6369a123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054688
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389847}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -164,6 +164,7 @@ BASE_FEATURE(kDigitalCredentialsHybridLinking,
              "DigitalCredentialsHybridLinking",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Net yet enabled by default.
 BASE_FEATURE(kWebAuthnPasskeyUpgrade,
              "WebAuthenticationPasskeyUpgrade",
              base::FEATURE_DISABLED_BY_DEFAULT);
