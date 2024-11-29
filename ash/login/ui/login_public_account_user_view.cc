52f61192f601aab95f76874eb76fbef428be4ec5
Istvan Nagy <iscsi@google.com>
2024-11-29 07:06:18 +0000

    Set the same alignment for managed and non managed accounts.
    Change-Id: I3a3c5a0424b09a733edb19f4398edff2c89659ad
    Bug: b:377260634, b:371156639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054629
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389650}

--- ash/login/ui/login_public_account_user_view.cc
+++ ash/login/ui/login_public_account_user_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/login/ui/arrow_button_view.h"
 #include "ash/login/ui/hover_notifier.h"
+#include "ash/login/ui/login_constants.h"
 #include "ash/login/ui/login_display_style.h"
 #include "ash/login/ui/views_utils.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -167,6 +168,7 @@ gfx::Size LoginPublicAccountUserView::CalculatePreferredSize(
   // Make sure we are at least as big as the user view. If we do not do this the
   // view will be below minimum size when no auth methods are displayed.
   size.SetToMax(user_view_->GetPreferredSize());
+  size.set_height(std::max(login::kMinimiumBigUserViewHeightDp, size.height()));
   return size;
 }
 
