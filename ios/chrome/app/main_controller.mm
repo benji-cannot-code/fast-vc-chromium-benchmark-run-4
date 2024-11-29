1a3a16af93b0a8f18a89d2fb308a6727a3ab48a9
Sylvain Defresne <sdefresne@chromium.org>
2024-11-29 09:10:49 +0000

    [ios] Use random name when creating the first profile
    Stop using kIOSChromeInitialProfile name in MainController when
    creating the first profile. This way, all profiles created will
    be named after a valid UUID.
    
    Bug: 333865629
    Change-Id: I7e2c471a9fa211dd583b4032247ebcd095606236
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054248
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389681}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -1757,7 +1757,7 @@ void BeginMemoryExperimentationAfterDelay() {
   if (profileName.empty()) {
     profileName = localState->GetString(prefs::kLastUsedProfile);
     if (profileName.empty()) {
-      profileName = kIOSChromeInitialProfile;
+      profileName = manager->ReserveNewProfileName();
       localState->SetString(prefs::kLastUsedProfile, profileName);
     }
   }
