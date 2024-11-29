cbc94717c1c62e1482da1f3d8cf840f70c7ec84b
Maksim Moskvitin <mmoskvitin@google.com>
2024-11-29 09:38:42 +0000

    [DataSharing] Pass consistency tokens to the SDK
    Since we pass consistency tokens for every requested group, they are
    bundled together in ReadGroupParams. This would require small migration
    away from the old `group_ids` field, but it seems cleaner long term
    (comparing to new repeated field with consistency tokens).
    
    Bug: 369856322
    Change-Id: Ie9a34ead22a9ca85ea8a5536662a8287823db677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049668
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389693}

--- components/data_sharing/internal/partial_failure_sdk_delegate_wrapper_unittest.cc
+++ components/data_sharing/internal/partial_failure_sdk_delegate_wrapper_unittest.cc
@@ -15,6 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace data_sharing {
 namespace {
 
+data_sharing_pb::ReadGroupsParams CreateReadGroupsParams(
+    const std::vector<GroupId>& group_ids) {
+  data_sharing_pb::ReadGroupsParams read_groups_params;
+  for (const GroupId& group_id : group_ids) {
+    read_groups_params.add_group_ids(group_id.value());
+    data_sharing_pb::ReadGroupsParams::GroupParams* group_params =
+        read_groups_params.add_group_params();
+    group_params->set_group_id(group_id.value());
+  }
+  return read_groups_params;
+}
+
 class PartialFailureSDKDelegateWrapperTest : public testing::Test {
  public:
   PartialFailureSDKDelegateWrapperTest()
@@ -58,11 +70,9 @@ TEST_F(PartialFailureSDKDelegateWrapperTest, ShouldReadSingleGroup) {
   const GroupId group_id =
       actual_sdk_delegate().AddGroupAndReturnId(display_name);
 
-  data_sharing_pb::ReadGroupsParams params;
-  params.add_group_ids(group_id.value());
-
   base::expected<data_sharing_pb::ReadGroupsResult, absl::Status>
-      read_groups_result = ReadGroupsViaWrapper(params);
+      read_groups_result =
+          ReadGroupsViaWrapper(CreateReadGroupsParams({group_id}));
 
   ASSERT_TRUE(read_groups_result.has_value());
   ASSERT_EQ(read_groups_result->group_data_size(), 1);
@@ -77,12 +87,9 @@ TEST_F(PartialFailureSDKDelegateWrapperTest, ShouldReadMultipleGroups) {
   const GroupId group_id2 =
       actual_sdk_delegate().AddGroupAndReturnId(display_name2);
 
-  data_sharing_pb::ReadGroupsParams params;
-  params.add_group_ids(group_id1.value());
-  params.add_group_ids(group_id2.value());
-
   base::expected<data_sharing_pb::ReadGroupsResult, absl::Status>
-      read_groups_result = ReadGroupsViaWrapper(params);
+      read_groups_result =
+          ReadGroupsViaWrapper(CreateReadGroupsParams({group_id1, group_id2}));
 
   ASSERT_TRUE(read_groups_result.has_value());
   ASSERT_EQ(read_groups_result->group_data_size(), 2);
@@ -96,12 +103,9 @@ TEST_F(PartialFailureSDKDelegateWrapperTest, ShouldHandlePartialFailure) {
       actual_sdk_delegate().AddGroupAndReturnId(display_name1);
   const GroupId group_id2 = GroupId("non_existent_group_id");
 
-  data_sharing_pb::ReadGroupsParams params;
-  params.add_group_ids(group_id1.value());
-  params.add_group_ids(group_id2.value());
-
   base::expected<data_sharing_pb::ReadGroupsResult, absl::Status>
-      read_groups_result = ReadGroupsViaWrapper(params);
+      read_groups_result =
+          ReadGroupsViaWrapper(CreateReadGroupsParams({group_id1, group_id2}));
 
   ASSERT_TRUE(read_groups_result.has_value());
   ASSERT_EQ(read_groups_result->group_data_size(), 1);
