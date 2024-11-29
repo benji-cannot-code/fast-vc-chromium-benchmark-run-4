0ef0e0c3e69966aa5f2d69d277b0ebcf46b1a496
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-11-29 16:49:12 +0000

    Incorporate perfetto changes to base/tracing/stdlib/chrome/
    Normally base/tracing/stdlib/chrome/ should be the source of truth
    and copied into perfetto. But some changes were made directly into the
    perfetto repository. This CL reconciles the Chromium copy.
    
    Patchset 1 is pure application of the command below.
    ```
    cp -r \
      third_party/perfetto/src/trace_processor/perfetto_sql/stdlib/chrome \
      base/tracing/stdlib
    ```
    
    Other patchsets include fixes to make presubmit happy, among them:
    - Convert some JOINID back to LONG, as detailed in
      https://crbug.com/380890280#comment2
    - Revert the removal of columns
      chrome_graphics_pipeline_surface_frame_steps.task_start_time_ts and
      chrome_graphics_pipeline_display_frame_steps.task_start_time_ts. (For
      some reason, the perfetto version of the file never had these?!)
    
    The contents of
    base/tracing/stdlib/chrome/ will be copied to perfetto in a separate CL.
    Some column types could be improved (e.g. use TIMESTAMP instead of LONG)
    but we leave that for future CLs.
    
    
    PERFETTO_TESTS=perfetto_diff_tests passes in the latest patchset
    
    Bug: 380890280
    Change-Id: I5dd9cfbfd10732f71adc04b4c0a980f5912b3ded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6059838
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389850}

--- base/tracing/stdlib/chrome/scroll_jank/scroll_jank_v3_cause.sql
+++ base/tracing/stdlib/chrome/scroll_jank/scroll_jank_v3_cause.sql
@@ -13,9 +13,9 @@ RETURNS TABLE(
   -- Alias for `slice.type`.
   type STRING,
   -- Alias for `slice.ts`.
-  ts LONG,
+  ts TIMESTAMP,
   -- Alias for `slice.dur`.
-  dur LONG,
+  dur DURATION,
   -- Alias for `slice.category`.
   category LONG,
   -- Alias for `slice.name`.
@@ -29,7 +29,7 @@ RETURNS TABLE(
   -- Alias for `slice.arg_set_id`.
   arg_set_id LONG,
   -- Alias for `slice.thread_ts`.
-  thread_ts LONG,
+  thread_ts TIMESTAMP,
   -- Alias for `slice.thread_dur`.
   thread_dur LONG
 ) AS
