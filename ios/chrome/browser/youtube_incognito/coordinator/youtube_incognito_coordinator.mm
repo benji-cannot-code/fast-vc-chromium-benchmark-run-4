a8fc7aa9b696e54b471a72408e86692958dd2547
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-11-29 12:27:24 +0000

    [iOS] Youtube Incognito coordinator -- Placeholder
    This CL introduces the coordinator of the Youtube Incognito feature. It
    will be used in the scene controller, to display a youtube incognito
    interstitial once a youtube link is opened in incognito.
    
    Bug: 374935670
    Change-Id: Ibb815c43fd5926b77ac60f16e0d6686b4dda1c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054709
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1389736}

---
+++ ios/chrome/browser/youtube_incognito/coordinator/youtube_incognito_coordinator.mm
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/youtube_incognito/coordinator/youtube_incognito_coordinator.h"
+
+@implementation YoutubeIncognitoCoordinator
+
+- (void)start {
+  // TODO(crbug.com/374935670): Present the corresponding view controller when
+  // available, and start the coordinator.
+}
+
+- (void)stop {
+  // TODO(crbug.com/374935670): Dimiss the view controller and stop the
+  // coordinator.
+}
+
+@end
