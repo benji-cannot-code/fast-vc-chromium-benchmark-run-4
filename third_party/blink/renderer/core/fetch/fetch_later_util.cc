09a3d39a085e4def81ab2f11c22be602872b892b
Ming-Ying Chung <mych@chromium.org>
2024-11-29 09:39:15 +0000

    Move `ComputeFetchLaterLoadPriority()` into a standalone util file.
    There is no functional change in this CL.
    
    Subsequent CLs will add more FetchLater-specific util functions in this
    new file which will be used outside `fetch/`.
    
    Bug: 40276121
    Change-Id: I29297fe3cbd115f6974069a40e16c84ed2003381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056930
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389694}

---
+++ third_party/blink/renderer/core/fetch/fetch_later_util.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/fetch/fetch_later_util.h"
+
+#include "third_party/blink/renderer/platform/loader/fetch/resource_request_utils.h"
+
+namespace blink {
+
+ResourceLoadPriority ComputeFetchLaterLoadPriority(
+    const FetchParameters& params) {
+  // FetchLater's ResourceType is ResourceType::kRaw, which should default to
+  // ResourceLoadPriority::kHigh priority. See also TypeToPriority() in
+  // resource_fetcher.cc
+  return AdjustPriorityWithPriorityHintAndRenderBlocking(
+      ResourceLoadPriority::kHigh, kFetchLaterResourceType,
+      params.GetResourceRequest().GetFetchPriorityHint(),
+      params.GetRenderBlockingBehavior());
+  // TODO(crbug.com/40276121): Apply kLow when
+  // IsSubframeDeprioritizationEnabled.
+}
+
+}  // namespace blink
---
+++ third_party/blink/renderer/core/fetch/fetch_later_util.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_FETCH_LATER_UTIL_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_FETCH_LATER_UTIL_H_
+
+#include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
+#include "third_party/blink/renderer/platform/loader/fetch/resource.h"
+#include "third_party/blink/renderer/platform/loader/fetch/resource_load_priority.h"
+
+namespace blink {
+
+// The ResourceType of FetchLater requests.
+inline constexpr ResourceType kFetchLaterResourceType = ResourceType::kRaw;
+
+// Computes resource loader priority for a FetchLater request.
+ResourceLoadPriority CORE_EXPORT
+ComputeFetchLaterLoadPriority(const FetchParameters& params);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_FETCH_LATER_UTIL_H_
