708457667286927642419d5eb56fe369aff7cf6b
Sorin Jianu <sorin@chromium.org>
2024-11-29 06:11:39 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/ash/crosapi
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=skuhne@chromium.org
    
    Bug: 381119547
    Change-Id: I0a2c0e1fefbccff699fdbf31414e49d360f05258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6053858
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389641}

--- chrome/browser/ash/crosapi/login_screen_storage_ash.cc
+++ chrome/browser/ash/crosapi/login_screen_storage_ash.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crosapi {
 
-LoginScreenStorageAsh::LoginScreenStorageAsh() {}
+LoginScreenStorageAsh::LoginScreenStorageAsh() = default;
 LoginScreenStorageAsh::~LoginScreenStorageAsh() = default;
 
 void LoginScreenStorageAsh::BindReceiver(
