6f4e18f63daa14cb5d20c00e4502257ca2221cd3
Ryan Sultanem <rsult@google.com>
2024-11-29 14:40:23 +0000

    Revert "[ProfilePicker] Remove ProfileSwitch check for sync"
    This reverts commit a11913f80def85902c1404fd072a173779c9c90a.
    
    Reason for revert: The fix introduced an unexpected behavior when
    signing in with an account that is already signed in in another
    profile - when not part of the profile creation flow. A login
    ui error is shown that is not expected and also hitting a `DCHECK`.
    
    Original change's description:
    > [ProfilePicker] Remove ProfileSwitch check for sync
    >
    > Bug: 40237154
    > Change-Id: If7c4f31d10d0c8f6573765b2eb5c257a9c09cf4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049381
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Ryan Sultanem <rsult@google.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1388726}
    
    Bug: 40237154
    Fixed: 381407891
    Change-Id: I3a57ac9991779a2ef6152faeb3b7854126eb586a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054138
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389784}

--- chrome/browser/ui/webui/signin/profile_picker_handler.h
+++ chrome/browser/ui/webui/signin/profile_picker_handler.h
@@ -67,6 +67,7 @@ class ProfilePickerHandler : public content::WebUIMessageHandler,
   friend class ProfilePickerHandlerTest;
   friend class ProfilePickerHandlerInUserProfileTest;
   friend class ProfilePickerCreationFlowBrowserTest;
+  friend class ProfilePickerEnterpriseCreationFlowBrowserTest;
   friend class StartupBrowserCreatorPickerInfobarTest;
   friend class SupervisedProfilePickerHideGuestModeTest;
   FRIEND_TEST_ALL_PREFIXES(ProfilePickerHandlerInUserProfileTest,
@@ -77,10 +78,10 @@ class ProfilePickerHandler : public content::WebUIMessageHandler,
   FRIEND_TEST_ALL_PREFIXES(ProfilePickerCreationFlowBrowserTest,
                            DeleteProfileFromOwnTab);
   FRIEND_TEST_ALL_PREFIXES(
-      ProfilePickerEnterpriseCreationFlowWithSyncParamBrowserTest,
+      ProfilePickerEnterpriseCreationFlowBrowserTest,
       CreateSignedInProfileSigninAlreadyExists_ConfirmSwitch);
   FRIEND_TEST_ALL_PREFIXES(
-      ProfilePickerEnterpriseCreationFlowWithSyncParamBrowserTest,
+      ProfilePickerEnterpriseCreationFlowBrowserTest,
       CreateSignedInProfileSigninAlreadyExists_CancelSwitch);
 
   void HandleMainViewInitialize(const base::Value::List& args);
