b7fc7f00e9f548bdefbe34cf9d888c9fc40cc2eb
Sorin Jianu <sorin@chromium.org>
2024-11-29 16:52:40 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    ['chrome/browser/android', 'chrome/browser/send_tab_to_self']
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=wenyufu@chromium.org
    
    Bug: 381119547
    Change-Id: I1d2b8243c722d3629eae294182e83eee7b80e51f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056521
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389852}

--- chrome/browser/android/devtools_manager_delegate_android.cc
+++ chrome/browser/android/devtools_manager_delegate_android.cc
@@ -38,7 +38,7 @@ class ClientProxy : public content::DevToolsAgentHostClient {
   ClientProxy(const ClientProxy&) = delete;
   ClientProxy& operator=(const ClientProxy&) = delete;
 
-  ~ClientProxy() override {}
+  ~ClientProxy() override = default;
 
   void DispatchProtocolMessage(DevToolsAgentHost* agent_host,
                                base::span<const uint8_t> message) override {
@@ -70,7 +70,7 @@ class TabProxyDelegate : public content::DevToolsExternalAgentProxyDelegate {
   TabProxyDelegate(const TabProxyDelegate&) = delete;
   TabProxyDelegate& operator=(const TabProxyDelegate&) = delete;
 
-  ~TabProxyDelegate() override {}
+  ~TabProxyDelegate() override = default;
 
   void Attach(content::DevToolsExternalAgentProxy* proxy) override {
     proxies_[proxy] = std::make_unique<ClientProxy>(proxy);
--- chrome/browser/send_tab_to_self/receiving_ui_handler_registry.cc
+++ chrome/browser/send_tab_to_self/receiving_ui_handler_registry.cc
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace send_tab_to_self {
 
-ReceivingUiHandlerRegistry::ReceivingUiHandlerRegistry() {}
-ReceivingUiHandlerRegistry::~ReceivingUiHandlerRegistry() {}
+ReceivingUiHandlerRegistry::ReceivingUiHandlerRegistry() = default;
+ReceivingUiHandlerRegistry::~ReceivingUiHandlerRegistry() = default;
 
 // static
 ReceivingUiHandlerRegistry* ReceivingUiHandlerRegistry::GetInstance() {
