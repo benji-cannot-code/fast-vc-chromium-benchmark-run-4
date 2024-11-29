f0be8561b7fc2a2226fd72c752f8774b9f24c96b
Sorin Jianu <sorin@chromium.org>
2024-11-29 06:10:21 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/ui/passwords
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=vasilii@chromium.org
    
    Bug: 381119547
    Change-Id: I52931630a962b99e11e6700c3f0924fa9bc27317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055191
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389640}

--- chrome/browser/ui/passwords/manage_passwords_icon_view.h
+++ chrome/browser/ui/passwords/manage_passwords_icon_view.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // An interface for updating the passwords icon in the location bar.
 class ManagePasswordsIconView {
  public:
-  ManagePasswordsIconView() {}
+  ManagePasswordsIconView() = default;
 
   ManagePasswordsIconView(const ManagePasswordsIconView&) = delete;
   ManagePasswordsIconView& operator=(const ManagePasswordsIconView&) = delete;
