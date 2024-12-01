e52c3797c3678ba15f71e36b5599a88189cb1766
Peter Kasting <pkasting@chromium.org>
2024-12-01 05:51:55 +0000

    Remove usage of base::make_span(): chrome/browser/ui/
    Replace with span() CTAD use, or more targeted helpers.
    
    Bug: 341907909
    Change-Id: Icdfe86c67dd9b61438407d2fa172f155759534e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6060346
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390057}

--- chrome/browser/ui/tabs/organization/tab_declutter_controller.cc
+++ chrome/browser/ui/tabs/organization/tab_declutter_controller.cc
@@ -309,7 +309,7 @@ bool TabDeclutterController::HasNewUnusedTabsForNudge(
 
   // Check for new unused duplicate tabs.
   for (auto& [url, tabs] : duplicate_tabs) {
-    if (IsNewTabDetectedForNudge(base::make_span(tabs))) {
+    if (IsNewTabDetectedForNudge(base::span(tabs))) {
       return true;
     }
   }
