c40bbe5d144f214b03d44efb1f4cf8693f1d1d22
Koji Ishii <kojii@chromium.org>
2024-12-01 23:00:22 +0000

    [break-iterator] Remove the runtime flag `BreakIteratorHyphenMinus`
    This flag shipped in M131, no reports so far.
    
    Bug: 371615505
    Change-Id: I88a1065e63fb5ec42ff7c4857809d81390af680a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056714
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390131}

--- third_party/blink/renderer/platform/text/text_break_iterator.cc
+++ third_party/blink/renderer/platform/text/text_break_iterator.cc
@@ -278,7 +278,7 @@ struct LazyLineBreakIterator::Context {
           return IsASCIIAlphanumeric(last_last_ch) ? FastBreakResult::kCanBreak
                                                    : FastBreakResult::kNoBreak;
         }
-      } else if (RuntimeEnabledFeatures::BreakIteratorHyphenMinusEnabled()) {
+      } else {
         // Defer to the Unicode algorithm to take more context into account.
         return FastBreakResult::kUnknown;
       }
