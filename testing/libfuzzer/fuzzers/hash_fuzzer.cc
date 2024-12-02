38a3fbc120a1461cd3542ef72995ea305ba48950
Peter Kasting <pkasting@chromium.org>
2024-12-02 09:28:50 +0000

    Remove usage of base::make_span(): testing/
    Replace with span() CTAD use, or more targeted helpers.
    
    Bug: 341907909
    Change-Id: Ib55f593051d66064dde8619187d6fc71189eaa35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6057027
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390217}

--- testing/libfuzzer/fuzzers/hash_fuzzer.cc
+++ testing/libfuzzer/fuzzers/hash_fuzzer.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::PersistentHash(base::make_span(data, size));
-  base::FastHash(base::make_span(data, size));
+  base::PersistentHash(base::span(data, size));
+  base::FastHash(base::span(data, size));
   return 0;
 }
