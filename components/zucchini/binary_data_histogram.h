7e3659adfe5d72a0babbd0e23dd12ef97e794afa
Luc Nguyen <lucnguyen@google.com>
2024-12-02 16:40:10 +0000

    Revert "[Health] Change unique_ptr<[]> to HeapArray in Zucchini::BinaryDataHistogram"
    This reverts commit 6f3f22396fb6638614fad550b6b4b6b0d97076c3.
    
    Reason: HeapArray adds overhead that is unacceptable.
    
    Also adds a comment explaining this.
    
    Bug: 381332105
    Change-Id: I6eca022657c4eab151e65dc4e0958f9f9f7fdfc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062543
    Auto-Submit: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390374}

--- components/zucchini/binary_data_histogram.h
+++ components/zucchini/binary_data_histogram.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <memory>
 #include <string>
 
-#include "base/containers/heap_array.h"
 #include "components/zucchini/buffer_view.h"
 
 namespace zucchini {
@@ -65,7 +65,7 @@ class BinaryDataHistogram {
   // Attempts to compute the histogram, returns true iff successful.
   bool Compute(ConstBufferView region);
 
-  bool IsValid() const { return !histogram_.empty(); }
+  bool IsValid() const { return static_cast<bool>(histogram_); }
 
   // Returns distance to another histogram (heuristics). If two binaries are
   // identical then their histogram distance is 0. However, the converse is not
@@ -82,8 +82,9 @@ class BinaryDataHistogram {
 
   // 2^16 buckets holding counts of all 2-byte sequences in the data. The counts
   // are stored as signed values to simplify computing the distance between two
-  // histograms.
-  base::HeapArray<int32_t> histogram_;
+  // histograms. Note that HeapArray is intentionally not used due to its
+  // overhead being unacceptable (see crbug.com/381332105).
+  std::unique_ptr<int32_t[]> histogram_;
 };
 
 }  // namespace zucchini
