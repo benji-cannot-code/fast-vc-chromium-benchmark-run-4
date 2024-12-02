42ce86e5ed1e07cec8f79f9e4b18b0573fd68e5d
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-12-02 06:14:33 +0000

    Introduce ServiceWorkerClient::GetFrameTreeNodeTypeStringBeforeCommit
    To move the logic accessing `FrameTreeNodeId` from
    `ServiceWorkerMainResourceLoader::GetFrameTreeNodeTypeString()`
    and remove a use of
    `ServiceWorkerMainResourceLoader::frame_tree_node_id_`.
    
    Bug: 368025734
    Change-Id: I31c4dc5a21bf625636942a5b862375a075c84e87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871257
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390183}

--- content/browser/service_worker/service_worker_main_resource_loader.cc
+++ content/browser/service_worker/service_worker_main_resource_loader.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "content/browser/loader/navigation_url_loader.h"
 #include "content/browser/loader/response_head_update_params.h"
-#include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/service_worker/service_worker_client.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
@@ -137,6 +136,8 @@ ServiceWorkerMainResourceLoader::ServiceWorkerMainResourceLoader(
       frame_tree_node_id_(frame_tree_node_id),
       is_browser_startup_completed_(
           GetContentClient()->browser()->IsBrowserStartupComplete()),
+      frame_tree_node_type_(
+          service_worker_client_->GetFrameTreeNodeTypeStringBeforeCommit()),
       find_registration_start_time_(std::move(find_registration_start_time)),
       fetch_event_client_id_(std::move(fetch_event_client_id)) {
   TRACE_EVENT_WITH_FLOW0(
@@ -160,16 +161,6 @@ ServiceWorkerMainResourceLoader::ServiceWorkerMainResourceLoader(
           "ServiceWorkerIsStopped.WaitingForWarmUp",
           core->IsWaitingForWarmUp(active_worker->key()));
     }
-  }
-
-  FrameTreeNode* frame_tree_node =
-      FrameTreeNode::GloballyFindByID(frame_tree_node_id_);
-  if (!frame_tree_node) {
-    frame_tree_node_type_ = FrameTreeNodeType::kUnknown;
-  } else {
-    frame_tree_node_type_ = frame_tree_node->IsOutermostMainFrame()
-                                ? FrameTreeNodeType::kOutermostMainFrame
-                                : FrameTreeNodeType::kNotOutermostMainFrame;
   }
 
   response_head_->load_timing.request_start = base::TimeTicks::Now();
@@ -1229,14 +1220,7 @@ ServiceWorkerMainResourceLoader::GetInitialServiceWorkerStatusString() {
 }
 
 std::string ServiceWorkerMainResourceLoader::GetFrameTreeNodeTypeString() {
-  switch (frame_tree_node_type_) {
-    case FrameTreeNodeType::kOutermostMainFrame:
-      return "OutermostMainFrame";
-    case FrameTreeNodeType::kNotOutermostMainFrame:
-      return "NotOutermostMainFrame";
-    case FrameTreeNodeType::kUnknown:
-      return "Unknown";
-  }
+  return frame_tree_node_type_;
 }
 
 void ServiceWorkerMainResourceLoader::RecordFindRegistrationTiming(
