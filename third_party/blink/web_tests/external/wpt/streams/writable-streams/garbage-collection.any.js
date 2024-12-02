73eaacbda77e1fdaa58dd8c8e046887c2d9bde21
Adam Rice <ricea@chromium.org>
2024-12-02 07:29:13 +0000

    Additional GC wpts for streams
    Add tests to verify that when the stream and reader/writer are
    unreferenced and only the promise returned by read() or write() is
    retained, the promise is still resolved correctly.
    
    Change-Id: I2b38bbda8c0e9fbdba12235a978991adb8e589e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6051620
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390197}

---
+++ third_party/blink/web_tests/external/wpt/streams/writable-streams/garbage-collection.any.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker,shadowrealm
+// META: script=/common/gc.js
+'use strict';
+
+promise_test(async () => {
+
+  let written = false;
+  const promise = (() => {
+    const rs = new WritableStream({
+      write() {
+        written = true;
+      }
+    });
+    const writer = rs.getWriter();
+    return writer.write('something');
+  })();
+  await garbageCollect();
+  await promise;
+  assert_true(written);
+
+}, 'A WritableStream and its writer should not be garbage collected while there is a write promise pending');
