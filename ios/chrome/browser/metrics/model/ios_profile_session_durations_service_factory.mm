de5f41f01f817206dacbda7168c9236be1f0cde5
Sylvain Defresne <sdefresne@chromium.org>
2024-12-02 16:26:10 +0000

    [ios] Convert IOSProfileSessionDurationsServiceFactory to PKSFIOS
    Convert IOSProfileSessionDurationsServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363215628
    Change-Id: Ib10e5c3a16cb4676c2a38e8a20084a78cf53c758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063244
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390362}

--- ios/chrome/browser/metrics/model/ios_profile_session_durations_service_factory.mm
+++ ios/chrome/browser/metrics/model/ios_profile_session_durations_service_factory.mm
@@ -8,10 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/sync/service/sync_service.h"
 #import "ios/chrome/browser/metrics/model/ios_profile_session_durations_service.h"
-#import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
@@ -19,8 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // static
 IOSProfileSessionDurationsService*
 IOSProfileSessionDurationsServiceFactory::GetForProfile(ProfileIOS* profile) {
-  return static_cast<IOSProfileSessionDurationsService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()
+      ->GetServiceForProfileAs<IOSProfileSessionDurationsService>(
+          profile, /*create=*/true);
 }
 
 // static
@@ -30,11 +29,15 @@ IOSProfileSessionDurationsServiceFactory::GetInstance() {
   return instance.get();
 }
 
+// Session time in incognito is counted towards the session time in the
+// regular profile. That means that for a user that is signed in and syncing
+// in their regular profile and that is browsing in incognito profile,
+// Chromium will record the session time as being signed in and syncing.
 IOSProfileSessionDurationsServiceFactory::
     IOSProfileSessionDurationsServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "IOSProfileSessionDurationsService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("IOSProfileSessionDurationsService",
+                                    ProfileSelection::kRedirectedInIncognito,
+                                    TestingCreation::kNoServiceForTests) {
   DependsOn(SyncServiceFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
 }
@@ -52,19 +55,4 @@ IOSProfileSessionDurationsServiceFactory::BuildServiceInstanceFor(
       IdentityManagerFactory::GetForProfile(profile);
   return std::make_unique<IOSProfileSessionDurationsService>(
       sync_service, profile->GetPrefs(), identity_manager);
-}
-
-web::BrowserState*
-IOSProfileSessionDurationsServiceFactory::GetBrowserStateToUse(
-    web::BrowserState* context) const {
-  // Session time in incognito is counted towards the session time in the
-  // regular profile. That means that for a user that is signed in and syncing
-  // in their regular profile and that is browsing in incognito profile,
-  // Chromium will record the session time as being signed in and syncing.
-  return GetBrowserStateRedirectedInIncognito(context);
-}
-
-bool IOSProfileSessionDurationsServiceFactory::ServiceIsNULLWhileTesting()
-    const {
-  return true;
 }
