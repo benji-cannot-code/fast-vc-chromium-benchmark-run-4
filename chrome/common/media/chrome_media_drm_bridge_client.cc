38e955cb1de5fd0de1010aa727ec90596591ec5c
Sorin Jianu <sorin@chromium.org>
2024-12-02 18:09:17 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/common/media
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=dalecurtis@chromium.org
    
    Bug: 381119547
    Change-Id: Ibf70581438b4e7756911094a3702b3fe8db6cb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056892
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390452}

--- chrome/common/media/chrome_media_drm_bridge_client.cc
+++ chrome/common/media/chrome_media_drm_bridge_client.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/media/chrome_media_drm_bridge_client.h"
 
-ChromeMediaDrmBridgeClient::ChromeMediaDrmBridgeClient() {}
+ChromeMediaDrmBridgeClient::ChromeMediaDrmBridgeClient() = default;
 
-ChromeMediaDrmBridgeClient::~ChromeMediaDrmBridgeClient() {}
+ChromeMediaDrmBridgeClient::~ChromeMediaDrmBridgeClient() = default;
 
 media::MediaDrmBridgeDelegate*
 ChromeMediaDrmBridgeClient::GetMediaDrmBridgeDelegate(
