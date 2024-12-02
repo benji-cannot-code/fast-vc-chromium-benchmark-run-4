a007c9edb07cefe49ac9921c0f28c2c5d4b6bf8f
Ginny Huang <ginnyhuang@chromium.org>
2024-12-02 20:23:22 +0000

    Revert "[iOS][Signin]HistorySyncPopupCoordinator should allow history"
    This reverts commit 437c7fd2e737efd6d7936835e7fa16b398a7260e.
    
    Reason for revert: Failing ios-fieldtrial-rel.
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-fieldtrial-rel/35263/test-results?sortby=&groupby=
    
    Original change's description:
    > [iOS][Signin]HistorySyncPopupCoordinator should allow history
    >
    > In this CL, HistorySyncPopupCoordinator ’s start method can’t request
    > to stop itself.
    > Instead, the owners are in charge of checking that it’s possible to request history.
    >
    > This start/stop cycle was making the next CL more complex than needed, once the interrupt step is gone.
    >
    > The way this CL was created is, for all allocation of the
    > HistorySyncPopupCoordinator, to first check whether it’s permissible
    > to start it. And otherwise to behave as `[self
    > historySyncPopupCoordinatorSelf:child
    > didFinishWithResult:SigninCoordinatorResultDisabled]`.
    >
    > As the child does not actually exists and this method call would
    > usually CHECK the child value and stop the child, this method is not
    > actually used.
    >
    > Bug: 381444097
    > Change-Id: Ic25d21be4cd2771bcc9fb970264d0cf2e8a75746
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062824
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1390421}
    
    Bug: 381444097
    Change-Id: Iba9f20bfa01c77f2ef26c2b3f616b22333a68a9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063905
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Owners-Override: Ginny Huang <ginnyhuang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1390547}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_popup_coordinator.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_popup_coordinator.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#import "base/check_op.h"
 #import "base/memory/raw_ptr.h"
 #import "base/metrics/user_metrics.h"
 #import "components/signin/public/base/signin_metrics.h"
@@ -18,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
 #import "ios/chrome/browser/ui/authentication/authentication_ui_util.h"
 #import "ios/chrome/browser/ui/authentication/history_sync/history_sync_coordinator.h"
-#import "ios/chrome/browser/ui/authentication/history_sync/history_sync_utils.h"
 
 @interface HistorySyncPopupCoordinator () <
     HistorySyncCoordinatorDelegate,
@@ -71,9 +69,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _authenticationService = AuthenticationServiceFactory::GetForProfile(profile);
   syncer::SyncService* syncService = SyncServiceFactory::GetForProfile(profile);
   // Check if History Sync Opt-In should be skipped.
-  CHECK_EQ(history_sync::GetSkipReason(syncService, _authenticationService,
-                                       profile->GetPrefs(), _isOptional),
-           history_sync::HistorySyncSkipReason::kNone);
+  HistorySyncSkipReason skipReason = [HistorySyncCoordinator
+      getHistorySyncOptInSkipReason:syncService
+              authenticationService:_authenticationService
+                        prefService:profile->GetPrefs()
+              isHistorySyncOptional:_isOptional];
+  if (skipReason != HistorySyncSkipReason::kNone) {
+    [HistorySyncCoordinator recordHistorySyncSkipMetric:skipReason
+                                            accessPoint:_accessPoint];
+    [self.delegate historySyncPopupCoordinator:self
+                           didFinishWithResult:SigninCoordinatorResultDisabled];
+    return;
+  }
 
   _navigationController =
       [[UINavigationController alloc] initWithNavigationBarClass:nil
