c3e4b94e81e8059ac66883989aa85691f456ab14
André Bianchessi <abianchessi@chromium.org>
2024-12-02 17:29:17 +0000

    Inject model into ToolbarController
    To solve crbug.com/359863838, we'll make the ToolbarController [1] observe the PinnedToolbarActionsModel [2]. "OnActionsChanged", the ToolbarController will reorder the responsive_elements_ so that the ActionIds are always properly sorted.
    
    This CL is no-op on its own because it's only doing the required plumbing for the PinnedToolbarActionsModel to be passed to the constructor. On follow-up CLs, I'll actually use that model to make the ToolbarController observe it and re-order the responsive_elements when needed.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/toolbar/toolbar_controller.h;l=119;drc=89f0fbf01198959eb2dc66fd26f2fa1753eb526e
    
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.h;l=22;drc=7ada3acfe99856f4da3531d71e33672bbb8c3a60
    
    Bug: 359863838
    Change-Id: Idb5bbc837bb3e88aadf8c4c24086f3dc36ba183c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039195
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: André Bianchessi <abianchessi@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390419}

--- chrome/browser/ui/views/toolbar/toolbar_controller.cc
+++ chrome/browser/ui/views/toolbar/toolbar_controller.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_actions.h"
+#include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.h"
 #include "chrome/browser/ui/toolbar_controller_util.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_action_callback.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_enums.h"
@@ -125,7 +126,8 @@ ToolbarController::ToolbarController(
     int element_flex_order_start,
     views::View* toolbar_container_view,
     OverflowButton* overflow_button,
-    ToolbarController::PinnedActionsDelegate* pinned_actions_delegate)
+    ToolbarController::PinnedActionsDelegate* pinned_actions_delegate,
+    PinnedToolbarActionsModel* pinned_actions_model)
     : responsive_elements_(responsive_elements),
       element_flex_order_start_(element_flex_order_start),
       toolbar_container_view_(toolbar_container_view),
