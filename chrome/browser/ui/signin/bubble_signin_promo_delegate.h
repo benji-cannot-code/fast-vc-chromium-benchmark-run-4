803c9c42f0097e93529074cdb717a92e69342ccd
Sylvain Defresne <sdefresne@chromium.org>
2024-12-02 16:45:37 +0000

    Make BubbleSignInPromoDelegate non-copyable
    BubbleSignInPromoDelegate is a pure virtual class and thus should
    not be copyable/moveable.
    
    Followup to https://crrev.com/c/6056127.
    
    Bug: none
    Change-Id: I12e25c7ffb665ba634f30645ea56cfa875dbda4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6053698
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390379}

--- chrome/browser/ui/signin/bubble_signin_promo_delegate.h
+++ chrome/browser/ui/signin/bubble_signin_promo_delegate.h
@@ -11,7 +11,12 @@ struct AccountInfo;
 // Delegate for the bubble sign in promo view.
 class BubbleSignInPromoDelegate {
  public:
+  BubbleSignInPromoDelegate() = default;
   virtual ~BubbleSignInPromoDelegate() = default;
+
+  BubbleSignInPromoDelegate(BubbleSignInPromoDelegate&) = delete;
+  BubbleSignInPromoDelegate& operator=(const BubbleSignInPromoDelegate&) =
+      delete;
 
   // Informs the delegate to sign in |account| or to present
   // the browser sign-in page if |account| is empty.
