2c8c2f3d82f0fa49818331815c7e1a2774e4a887
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2024-12-02 09:37:35 +0000

    Listen to onFinishingTabClosure instead of tabClosureCommitted to avoid leaking memory
    Tab closed by JS or other non-undoable sources doesn't fire
    tabClosureCommitted, while onFinishingTabClosure fires for all closure
    (see TabModelImpl.finalizeTabClosure).
    
    Listen to the latter to avoid memory leak.
    
    Remove the call to super since it's a noop for onFinishingTabClosure.
    
    Bug: 381210500
    Change-Id: I9b4fe77a6fd30afad4658d7fd963cec28be66285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6051446
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390218}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
@@ -219,8 +219,7 @@ class ManualFillingMediator
                     }
 
                     @Override
-                    public void tabClosureCommitted(Tab tab) {
-                        super.tabClosureCommitted(tab);
+                    public void onFinishingTabClosure(Tab tab) {
                         mObservedTabs.remove(tab);
                         tab.removeObserver(
                                 mTabObserver); // Fails silently if observer isn't registered.
