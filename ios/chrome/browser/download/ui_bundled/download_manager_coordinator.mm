f3573cf92d72a165ca4dc0b15a0abdd45413cb23
Quentin Pubert <qpubert@google.com>
2024-12-02 16:17:16 +0000

    [iOS] Cleanup Save to Drive flag
    This CL removes references to the kIOSSaveToDrive flag which is enabled
    by default, as well as the flag itself.
    
    LegacyDownloadManagerViewController and associated unit tests can be
    removed and DownloadManagerCoordinatorTest is fixed to work with its
    replacement DownloadManagerViewController.
    
    Change-Id: I4be65f5b330ec5cf3625d4365bab035c5759224e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054085
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390353}

--- ios/chrome/browser/download/ui_bundled/download_manager_coordinator.mm
+++ ios/chrome/browser/download/ui_bundled/download_manager_coordinator.mm
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/download/ui_bundled/download_manager_view_controller.h"
 #import "ios/chrome/browser/download/ui_bundled/download_manager_view_controller_delegate.h"
 #import "ios/chrome/browser/download/ui_bundled/download_manager_view_controller_protocol.h"
-#import "ios/chrome/browser/download/ui_bundled/legacy_download_manager_view_controller.h"
 #import "ios/chrome/browser/download/ui_bundled/unopened_downloads_tracker.h"
 #import "ios/chrome/browser/drive/model/drive_service_factory.h"
 #import "ios/chrome/browser/drive/model/upload_task.h"
@@ -121,9 +120,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                       object:nil];
 
   BOOL isIncognito = self.browser->GetProfile()->IsOffTheRecord();
-  _viewController = base::FeatureList::IsEnabled(kIOSSaveToDrive)
-                        ? [[DownloadManagerViewController alloc] init]
-                        : [[LegacyDownloadManagerViewController alloc] init];
+  _viewController = [[DownloadManagerViewController alloc] init];
   _viewController.delegate = self;
   _viewController.layoutGuideCenter = LayoutGuideCenterForBrowser(self.browser);
   _viewController.incognito = isIncognito;
@@ -134,7 +131,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_viewController setFullscreenController:fullscreenController];
   }
 
-  if (base::FeatureList::IsEnabled(kIOSSaveToDrive)) {
     _mediator.SetIsIncognito(isIncognito);
     ProfileIOS* profile = self.browser->GetProfile();
     _mediator.SetIdentityManager(
@@ -142,7 +138,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _mediator.SetDriveService(
         drive::DriveServiceFactory::GetForProfile(profile));
     _mediator.SetPrefService(profile->GetPrefs());
-  }
 
   _mediator.SetDownloadTask(_downloadTask);
   _mediator.SetConsumer(_viewController);
@@ -391,7 +386,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)downloadManagerViewControllerDidOpenInDriveApp:
     (UIViewController*)controller {
-  CHECK(base::FeatureList::IsEnabled(kIOSSaveToDrive));
   UploadTask* uploadTask = _mediator.GetUploadTask();
   if (!uploadTask) {
     // While it should not be possible that uploadTask is nil at this point,
@@ -514,9 +508,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       "Download.IOSDownloadFileUIGoogleDrive",
       DownloadFileUIGoogleDrive::GoogleDriveInstalledAfterDisplay,
       DownloadFileUIGoogleDrive::Count);
-  if (base::FeatureList::IsEnabled(kIOSSaveToDrive)) {
-    _mediator.SetGoogleDriveAppInstalled(true);
-  }
+  _mediator.SetGoogleDriveAppInstalled(true);
   _mediator.UpdateConsumer();
 }
 
@@ -533,12 +525,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     };
   }
   [_storeKitCoordinator start];
-  if (!base::FeatureList::IsEnabled(kIOSSaveToDrive) &&
-      [_viewController respondsToSelector:@selector
-                       (setInstallDriveButtonVisible:animated:)]) {
-    [_viewController setInstallDriveButtonVisible:NO animated:YES];
-  }
-
   [[InstallationNotifier sharedInstance]
       registerForInstallationNotifications:self
                               withSelector:@selector(didInstallGoogleDriveApp)
--- ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_egtest.mm
+++ ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_egtest.mm
@@ -143,7 +143,6 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
 
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration configuration;
-  configuration.features_enabled.push_back(kIOSSaveToDrive);
   if ([self isRunningTest:@selector(testCanRetryDownloadToDrive)]) {
     const std::string commandLineSwitch =
         std::string(kTestDriveFileUploaderCommandLineSwitch);
