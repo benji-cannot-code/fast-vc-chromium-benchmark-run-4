b65717705404f3e21111df62f8ee1cfbf7e65cd6
Sorin Jianu <sorin@chromium.org>
2024-12-02 16:55:54 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/password_manager/android
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=friedrichh@chromium.org
    
    Bug: 381119547
    Change-Id: Ie5cda91f4d3b701d8d16adf7fcca2447190eff65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056214
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1390385}

--- chrome/browser/password_manager/android/password_manager_lifecycle_helper.cc
+++ chrome/browser/password_manager/android/password_manager_lifecycle_helper.cc
@@ -5,4 +5,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/password_manager/android/password_manager_lifecycle_helper.h"
 
-PasswordManagerLifecycleHelper::~PasswordManagerLifecycleHelper() {}
+PasswordManagerLifecycleHelper::~PasswordManagerLifecycleHelper() = default;
