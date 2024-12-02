7e8b708f3349cd71bb211363460b64644b9c797b
Will Yeager <wyeager@google.com>
2024-12-02 16:13:06 +0000

    Only download Xcode in the iOS test runner if LUCI_CONTEXT is present
    Bug: 362906088
    Change-Id: Id8ca4394bb1399c5b92334b99be564ac4b423d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6030451
    Reviewed-by: Yue She <yueshe@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Will Yeager <wyeager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390349}

--- ios/build/bots/scripts/run_test.py
+++ ios/build/bots/scripts/run_test.py
@@ -16,6 +16,7 @@ import run
 import result_sink_util
 from test_runner import SimulatorNotFoundError, TestRunner
 from xcodebuild_runner import SimulatorParallelTestRunner
+import xcode_util
 import test_runner_errors
 import test_runner_test
 
@@ -471,6 +472,7 @@ class RunnerInstallXcodeTest(test_runner_test.TestCase):
     self.runner = run.Runner()
 
     self.mock(self.runner, 'parse_args', lambda _: None)
+    self.mock(xcode_util, 'is_local_run', lambda: False)
     self.runner.args = mock.MagicMock()
     # Make run() choose xcodebuild_runner.SimulatorParallelTestRunner as tr.
     self.runner.args.xcode_parallelization = True
