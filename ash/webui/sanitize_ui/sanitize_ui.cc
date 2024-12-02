a2f6694183bdd25dc51948d3313b2ab976af93d7
Andrii Natiahlyi <natiahlyi@google.com>
2024-12-02 14:51:58 +0000

    Enable unsafe buffer warnings in sanitize_ui.cc
    Bug: 40285824
    Change-Id: I4f7ff7d7530b1cf778b1f60ec0852fc696aee4a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054017
    Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390303}

--- ash/webui/sanitize_ui/sanitize_ui.cc
+++ ash/webui/sanitize_ui/sanitize_ui.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ash/webui/sanitize_ui/sanitize_ui.h"
 
 #include "ash/constants/ash_features.h"
@@ -94,8 +89,7 @@ SanitizeDialogUI::SanitizeDialogUI(
   html_source->UseStringsJs();
   html_source->EnableReplaceI18nInJS();
 
-  const auto resources =
-      base::make_span(kAshSanitizeAppResources, kAshSanitizeAppResourcesSize);
+  const auto resources = base::span(kAshSanitizeAppResources);
   html_source->AddResourcePaths(resources);
   html_source->AddResourcePath("", IDR_ASH_SANITIZE_APP_INDEX_HTML);
   html_source->AddResourcePath("test_loader.html", IDR_WEBUI_TEST_LOADER_HTML);
