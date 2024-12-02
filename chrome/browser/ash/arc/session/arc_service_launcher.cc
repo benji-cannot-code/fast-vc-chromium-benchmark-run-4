8df0e10f63950de071eb754cb5ecd636e411b21b
Ting-Hao Lin <tinghaolin@google.com>
2024-12-02 16:31:25 +0000

    chrome: fix ARC image check after restart
    Ensure consistent ARCVM DLC image availability
    check after a browser restart. Previously,
    the session didn't go through UserSessionManager
    after a crash, causing the image path check to
    be skipped and trigger a crash.
    
    This commit adds the check in ArcServiceLauncher
    initialization, before profile setup.
    GetArcStatusForProfile() can return a
    consistent result.
    
    BUG=b:375451772
    TEST=run ChromeArcUtilTest unittest
    TEST=deploy Chrome and verify arcvm availability
    
    Change-Id: I8b4f78b3820315724e840ce06dd3df5b6a82a88e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039872
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: TingHao Lin <tinghaolin@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390368}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -39,7 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/volume_mounter/arc_volume_mounter_bridge.h"
 #include "ash/components/arc/wake_lock/arc_wake_lock_bridge.h"
 #include "ash/constants/ash_features.h"
+#include "ash/constants/ash_switches.h"
 #include "base/check_op.h"
+#include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/task/sequenced_task_runner.h"
@@ -141,6 +143,18 @@ std::unique_ptr<ArcSessionManager> CreateArcSessionManager(
                                              std::move(delegate));
 }
 
+void CheckArcvmDlcImageStatus() {
+  base::FilePath arc_vm_dlc_image_path(
+      "/opt/google/vms/android/system.raw.img");
+  // Check if the ARCVM DLC image exists before calling
+  // GetArcStatusForProfile(). This blocks the main thread but is necessary to
+  // ensure arc availability is consistent, especially during Ash Chrome
+  // restarts. The check only occurs when the arcvm_dlc USE flag is enabled,
+  // which is currently specific to the Reven board.
+  bool is_arcvm_dlc_image_available = base::PathExists(arc_vm_dlc_image_path);
+  arc::SetArcvmDlcImageStatus(is_arcvm_dlc_image_available);
+}
+
 }  // namespace
 
 ArcServiceLauncher::ArcServiceLauncher(
@@ -185,6 +199,12 @@ void ArcServiceLauncher::Initialize() {
                      weak_factory_.GetWeakPtr()),
       kTpmOwnershipCheckDelay);
 #else
+  if (arc::IsArcVmDlcEnabled() &&
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          ash::switches::kLoginUser)) {
+    CheckArcvmDlcImageStatus();
+  }
+
   arc_session_manager_->ExpandPropertyFilesAndReadSalt();
 #endif  // BUILDFLAG(USE_ARC_PROTECTED_MEDIA)
 }
