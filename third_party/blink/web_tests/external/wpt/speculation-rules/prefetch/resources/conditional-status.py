451289046b6d8107371b497c4e5e826cedfcb5c5
Domenic Denicola <domenic@chromium.org>
2024-12-02 09:00:14 +0000

    Ship PrefetchFollowsNormalCacheSemantics
    This removes the "5-minute rule" for prefetches, where they ignore
    normal HTTP cache semantics for 5 minutes and instead stay cached in the
    HTTP cache. This applies to <link rel=prefetch>, NoStatePrefetch (i.e.
    <link rel=prerender>), and speculation rules prefetch.
    
    Note that for speculation rules prefetch, because there is the separate
    in-memory speculation rules prefetch cache, 2xx responses without
    caching headers will still be stored in that in-memory cache. They just
    won't be HTTP cached, in a way that was counter to the website's
    expressed preferences.
    
    Shipping this was approved back in May 2023, with an accompanying Finch
    study to check for regressions [1]. However, we forgot to actually
    change the default.
    
    This is of new urgency now because of the impact on speculation rules
    prefetch, which is is causing web developer pain. A web platform test is
    included for the specific painful scenario, which fails before this
    change and passes afterward.
    
    Most of this CL consists of test updates: many of our tests relied on
    not needing caching headers to make <link rel=prefetch> or
    <link rel=prerender> work, and so do things like assume there will never
    be a second request to those URLs. The general fix is to apply caching
    headers to the resources in question, as that represents the intended
    real-world usage of <link rel=prefetch>. (And, to a lesser extent, the
    nonstandard and hopefully-one-day-removed <link rel=prerender>.)
    
    Notably, the WPT prefetch-document.html was failing in all browsers
    before this, including in Firefox which has a highly spec-conformant
    implementation. It should now be passing.
    
    PrerendererImplBrowserTestPrefetchAhead.PrefetchSuccessPrefetchMatchResolverTimeoutPrerenderFailure
    was disabled, since it appears to never have been working as expected,
    with the HTTP caching hiding the real problem. Issue 372851198 tracks
    fixing that test.
    
    Tests specifically for the 5-minute rule were deleted.
    
    [1]: https://groups.google.com/a/chromium.org/g/blink-dev/c/Zdo71C0k9C0
    
    Bug: 40232065, 381099745, 356624471, 372851198
    Change-Id: I7d42c43efd89486587d8b7fc8ddd4412b31ef0bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056830
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390213}

---
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/conditional-status.py
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import json
+import os.path
+from wptserve.pipes import template
+
+def main(request, response):
+  response.headers.set(b"Content-Type", b"text/html")
+
+  prefetch = request.headers.get("Sec-Purpose", b"").decode("utf-8").startswith("prefetch")
+
+  response.content = template(
+    request,
+    open(os.path.join(os.path.dirname(__file__), "executor.sub.html"), "rb").read())
+
+  if prefetch:
+    response.status = 503
+    response.content += b"<body>503"
+  else:
+    response.status = 200
+    response.content += b"<body>200"
--- third_party/blink/web_tests/http/tests/misc/resources/prefetch-purpose.php
+++ third_party/blink/web_tests/http/tests/misc/resources/prefetch-purpose.php
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+header("Cache-Control: max-age=3600");
 echo "Purpose: " . $_SERVER["HTTP_PURPOSE"];
 ?>
 
