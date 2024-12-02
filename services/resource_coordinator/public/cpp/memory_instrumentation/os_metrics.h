2f49983925b666a23024e806bd8abc86e702077a
Thomas Lukaszewicz <tluk@chromium.org>
2024-12-02 18:41:56 +0000

    Revert "[memory] Move process memory info to base"
    This reverts commit 6a0ed21330f202eae185ea827ae19f51f0537d82.
    
    Reason for revert:
    Speculatively reverting due to Linux MSan bot failures on
    RenderFrameHostManagerTest.RenderViewInitAfterNewProxyAndProcessKill
    
    First failing build
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/52273/overview
    
    Original change's description:
    > [memory] Move process memory info to base
    >
    > This will be re-used in a follow-up in SystemMetricsSampler.
    > This should also replaced copied code in blink::MemoryUsageMonitor.
    >
    > This changes Linux's FillOSMemoryDump from GetResidentAndSharedPagesFromStatmFile() (proc/pid/statm) to ReadProcFileToTrimmedStringPairs() (proc/pid/status)
    >
    > This drops supports for pre MAC_OS_X_VERSION_10_11 missing
    > phys_footprint: https://issues.chromium.org/issues/40669926
    >
    > Change-Id: I2002e69abbe852e82d0d1cb37017e77602538d6b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039432
    > Reviewed-by: Primiano Tucci <primiano@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Owners-Override: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1390345}
    
    Change-Id: I352a3dc8e876eee98ee39cf68555dad2938169f5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063460
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390473}

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics.h
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/gtest_prod_util.h"
 #include "base/process/process_handle.h"
-#include "base/process/process_metrics.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "build/build_config.h"
 #include "services/resource_coordinator/public/mojom/memory_instrumentation/memory_instrumentation.mojom.h"
@@ -41,22 +40,19 @@ namespace memory_instrumentation {
 class COMPONENT_EXPORT(
     RESOURCE_COORDINATOR_PUBLIC_MEMORY_INSTRUMENTATION) OSMetrics {
  public:
-  // Fills |dump| with memory information about |handle|. See class comments for
-  // restrictions on |handle|. |dump.platform_private_footprint| must be
-  // allocated before calling this function. If |handle| is null, the handle of
-  // the current process is used
-  static bool FillOSMemoryDump(base::ProcessHandle handle,
-                               mojom::RawOSMemDump* dump);
+  // Fills |dump| with memory information about |pid|. See class comments for
+  // restrictions on |pid|. |dump.platform_private_footprint| must be allocated
+  // before calling this function. If |pid| is null, the pid of the current
+  // process is used
+  static bool FillOSMemoryDump(base::ProcessId pid, mojom::RawOSMemDump* dump);
 #if BUILDFLAG(IS_APPLE)
-  static bool FillOSMemoryDump(base::ProcessHandle handle,
-                               base::PortProvider* port_provider,
-                               mojom::RawOSMemDump* dump);
+  static bool FillOSMemoryDumpFromTaskPort(mach_port_t task_port,
+                                           mojom::RawOSMemDump* dump);
 #endif
-  static bool FillProcessMemoryMaps(base::ProcessHandle,
+  static bool FillProcessMemoryMaps(base::ProcessId,
                                     mojom::MemoryMapOption,
                                     mojom::RawOSMemDump*);
-  static std::vector<mojom::VmRegionPtr> GetProcessMemoryMaps(
-      base::ProcessHandle);
+  static std::vector<mojom::VmRegionPtr> GetProcessMemoryMaps(base::ProcessId);
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
   static void SetProcSmapsForTesting(FILE*);
@@ -72,13 +68,8 @@ class COMPONENT_EXPORT(
                            MemoryMaps);
 
 #if BUILDFLAG(IS_MAC)
-  static std::vector<mojom::VmRegionPtr> GetProcessModules(base::ProcessHandle);
+  static std::vector<mojom::VmRegionPtr> GetProcessModules(base::ProcessId);
 #endif
-
-#if !BUILDFLAG(IS_APPLE)
-  static base::expected<base::ProcessMemoryInfo, base::ProcessUsageError>
-  GetMemoryInfo(base::ProcessHandle handle);
-#endif  // !BUILDFLAG(IS_APPLE)
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
   // Provides information on the dump state of resident pages. These values are
