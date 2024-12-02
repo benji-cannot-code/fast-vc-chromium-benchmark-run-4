858af34dd6ae334d9449819ca1e40b91817fbe19
Yunke Zhou <yunkez@google.com>
2024-12-02 14:56:59 +0000

    Enable kPasswordlessGaiaForConsumers by default
    Bug: b:238181338
    Change-Id: I4e75bb9cea78cdc5d8bead8ba049cb453f85e881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056419
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390305}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2283,7 +2283,7 @@ BASE_FEATURE(kParentAccessJelly,
 // (This feature is only available for consumer users)
 BASE_FEATURE(kPasswordlessGaiaForConsumers,
              "PasswordlessGaiaForConsumers",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables a notification warning users that their Thunderbolt device is not
 // supported on their CrOS device.
