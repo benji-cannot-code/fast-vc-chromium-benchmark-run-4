e44ccbf5983b47615e070e34936ba2d0b2d0a957
Peter Kasting <pkasting@chromium.org>
2024-12-02 17:32:05 +0000

    Fully define mojom::AccessibilityFileLoader before using.
    Structs used as template params should usually be fully-defined, or
    strange ODR violations can happen.
    
    Bug: none
    Change-Id: If7e9b61ed713706c23cbdab70771e2f1fcdfc101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6058762
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390422}

--- services/accessibility/features/v8_manager.h
+++ services/accessibility/features/v8_manager.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/accessibility/features/bindings_isolate_holder.h"
 #include "services/accessibility/public/mojom/accessibility_service.mojom-forward.h"
 #include "services/accessibility/public/mojom/automation.mojom-forward.h"
-#include "services/accessibility/public/mojom/file_loader.mojom-forward.h"
+#include "services/accessibility/public/mojom/file_loader.mojom.h"
 #include "third_party/blink/public/mojom/devtools/devtools_agent.mojom.h"
 #include "v8-persistent-handle.h"
 #include "v8-script.h"
