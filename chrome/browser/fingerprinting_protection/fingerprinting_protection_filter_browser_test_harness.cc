1b69f26a14b52090e0539196d72d577f13ae7bfa
Tanushree Bisht <tanub@google.com>
2024-12-02 19:11:27 +0000

    [FPF] Add Android support for set of existing FPF browser tests.
    Moves fingerprinting_protection_filter_browsertest.cc and
    fingerprinting_protection_filter_ruleset_browsertest.cc to
    platform_browser_tests target so that they run on both Desktop
    and Android.
    
    Also some modifications + helpers / guarding behavior in
    fingerprinting_protection_filter_browsertest via ifendifs so that
    some tests only run on Desktop.
    
    Bug: 379336042
    Change-Id: If1ef36635406ff301f05643c835e35c2dc14e0e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049903
    Reviewed-by: Martin Verde <thesalsa@google.com>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Tanushree Bisht <tanub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390492}

--- chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browser_test_harness.cc
+++ chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browser_test_harness.cc
@@ -5,10 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browser_test_harness.h"
 
+#include "chrome/test/base/chrome_test_utils.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.h"
 #include "components/subresource_filter/content/browser/test_ruleset_publisher.h"
 #include "components/subresource_filter/core/common/test_ruleset_creator.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/ui_test_utils.h"
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 namespace fingerprinting_protection_filter {
 
@@ -27,6 +33,13 @@ FingerprintingProtectionFilterBrowserTest::
 
 void FingerprintingProtectionFilterBrowserTest::SetUpOnMainThread() {
   SubresourceFilterSharedBrowserTest::SetUpOnMainThread();
+#if !BUILDFLAG(IS_ANDROID)
+  embedded_test_server()->ServeFilesFromSourceDirectory(
+      "components/test/data/subresource_filter");
+#else
+  embedded_test_server()->ServeFilesFromSourceDirectory(
+      "chrome/test/data/fingerprinting_protection");
+#endif  // !BUILDFLAG(IS_ANDROID)
   // Allow derived classes to start the server on their own.
 }
 
@@ -58,6 +71,16 @@ void FingerprintingProtectionFilterBrowserTest::AssertUrlContained(
     const GURL& sub_url) {
   ASSERT_NE(full_url.spec().find(std::string(sub_url.spec())),
             std::string::npos);
+}
+
+bool FingerprintingProtectionFilterBrowserTest::NavigateToDestination(
+    const GURL& url) {
+#if !BUILDFLAG(IS_ANDROID)
+  return ui_test_utils::NavigateToURL(browser(), url);
+#else
+  return content::NavigateToURL(chrome_test_utils::GetActiveWebContents(this),
+                                url);
+#endif  // !BUILDFLAG(IS_ANDROID)
 }
 
 // ============= FingerprintingProtectionFilterDryRunBrowserTest ==============
--- chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browsertest.cc
+++ chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browsertest.cc
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browser_test_harness.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_constants.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.h"
 #include "components/subresource_filter/core/browser/subresource_filter_features_test_support.h"
@@ -23,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/ui_test_utils.h"
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 // TODO(https://crbug.com/358371545): Add browser tests for Histogram population
 // for histograms that relate to time measurements as well as renderer
@@ -48,7 +51,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterBrowserTest,
   base::HistogramTester histogram_tester;
   ukm::TestAutoSetUkmRecorder test_ukm_recorder;
 
-  GURL url(GetTestUrl(kTestFrameSetPath));
+  GURL url(GetTestUrl(kMultiPlatformTestFrameSetPath));
 
   // Disallow loading child frame documents that in turn would end up
   // loading included_script.js, unless the document is loaded from an allowed
@@ -56,7 +59,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterBrowserTest,
   // disallowing a load only after the first redirect.
   ASSERT_NO_FATAL_FAILURE(
       SetRulesetToDisallowURLsWithPathSuffix("included_script.html"));
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+  ASSERT_TRUE(NavigateToDestination(url));
 
   const std::vector<const char*> kSubframeNames{"one", "two", "three"};
   const std::vector<bool> kExpectOnlySecondSubframe{false, true, false};
@@ -66,8 +69,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterBrowserTest,
 
   // Now navigate the first subframe to an allowed URL and ensure that the load
   // successfully commits and the frame gets restored (no longer collapsed).
-  GURL allowed_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_allowed_script.html"));
+  GURL allowed_subdocument_url(GetTestUrl("frame_with_allowed_script.html"));
   NavigateFrame(kSubframeNames[0], allowed_subdocument_url);
 
   const std::vector<bool> kExpectFirstAndSecondSubframe{true, true, false};
@@ -77,7 +79,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterBrowserTest,
 
   // Navigate the first subframe to a document that does not load the probe JS.
   GURL allowed_empty_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_no_subresources.html"));
+      GetTestUrl("frame_with_no_subresources.html"));
   NavigateFrame(kSubframeNames[0], allowed_empty_subdocument_url);
 
   // Finally, navigate the first subframe to an allowed URL that redirects to a
@@ -85,104 +87,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterBrowserTest,
   // collapsed.
   const char kAllowedDomain[] = "allowed.com";
   GURL disallowed_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_included_script.html"));
-  GURL redirect_to_disallowed_subdocument_url(embedded_test_server()->GetURL(
-      kAllowedDomain, "/server-redirect?" + disallowed_subdocument_url.spec()));
-  NavigateFrame(kSubframeNames[0], redirect_to_disallowed_subdocument_url);
-
-  ASSERT_NO_FATAL_FAILURE(ExpectParsedScriptElementLoadedStatusInFrames(
-      kSubframeNames, kExpectOnlySecondSubframe));
-
-  content::RenderFrameHost* frame = FindFrameByName(kSubframeNames[0]);
-  const auto last_committed_url = frame->GetLastCommittedURL();
-
-  ASSERT_TRUE(frame);
-  AssertUrlContained(last_committed_url,
-                     redirect_to_disallowed_subdocument_url);
-  AssertUrlContained(last_committed_url, disallowed_subdocument_url);
-
-  ExpectFramesIncludedInLayout(kSubframeNames, kExpectOnlySecondSubframe);
-
-  // Check test UKM recorder contains event with expected metrics.
-  const auto& entries = test_ukm_recorder.GetEntriesByName(
-      ukm::builders::FingerprintingProtection::kEntryName);
-  // 1 entry for every frame_with_included_script.html (2 from initial load, 1
-  // from redirect)
-  EXPECT_EQ(3u, entries.size());
-  for (const ukm::mojom::UkmEntry* entry : entries) {
-    test_ukm_recorder.ExpectEntryMetric(
-        entry, ukm::builders::FingerprintingProtection::kActivationDecisionName,
-        static_cast<int64_t>(
-            subresource_filter::ActivationDecision::ACTIVATED));
-    EXPECT_FALSE(test_ukm_recorder.EntryHasMetric(
-        entry, ukm::builders::FingerprintingProtection::kDryRunName));
-  }
-
-  histogram_tester.ExpectBucketCount(
-      ActivationDecisionHistogramName,
-      subresource_filter::ActivationDecision::ACTIVATED, 1);
-  histogram_tester.ExpectBucketCount(
-      ActivationLevelHistogramName,
-      subresource_filter::mojom::ActivationLevel::kEnabled, 1);
-  histogram_tester.ExpectTotalCount(kSubresourceLoadsTotalForPage, 1);
-  histogram_tester.ExpectTotalCount(kSubresourceLoadsEvaluatedForPage, 1);
-  histogram_tester.ExpectTotalCount(kSubresourceLoadsMatchedRulesForPage, 1);
-  histogram_tester.ExpectTotalCount(kSubresourceLoadsDisallowedForPage, 1);
-}
-
-IN_PROC_BROWSER_TEST_F(
-    FingerprintingProtectionFilterEnabledInIncognitoBrowserTest,
-    SubframeDocumentLoadFiltering) {
-  // Close normal browser and switch the test's browser instance to an incognito
-  // instance.
-  Browser* incognito = CreateIncognitoBrowser(browser()->profile());
-  CloseBrowserSynchronously(browser());
-  SelectFirstBrowser();
-  ASSERT_EQ(browser(), incognito);
-
-  // TODO(https://crbug.com/358371545): Test console messaging for subframe
-  // blocking once its implementation is resolved.
-  base::HistogramTester histogram_tester;
-  ukm::TestAutoSetUkmRecorder test_ukm_recorder;
-
-  GURL url(GetTestUrl(kTestFrameSetPath));
-
-  // Disallow loading child frame documents that in turn would end up
-  // loading included_script.js, unless the document is loaded from an allowed
-  // (not in the blocklist) domain. This enables the third part of this test
-  // disallowing a load only after the first redirect.
-  ASSERT_NO_FATAL_FAILURE(
-      SetRulesetToDisallowURLsWithPathSuffix("included_script.html"));
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
-
-  const std::vector<const char*> kSubframeNames{"one", "two", "three"};
-  const std::vector<bool> kExpectOnlySecondSubframe{false, true, false};
-  ASSERT_NO_FATAL_FAILURE(ExpectParsedScriptElementLoadedStatusInFrames(
-      kSubframeNames, kExpectOnlySecondSubframe));
-  ExpectFramesIncludedInLayout(kSubframeNames, kExpectOnlySecondSubframe);
-
-  // Now navigate the first subframe to an allowed URL and ensure that the load
-  // successfully commits and the frame gets restored (no longer collapsed).
-  GURL allowed_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_allowed_script.html"));
-  NavigateFrame(kSubframeNames[0], allowed_subdocument_url);
-
-  const std::vector<bool> kExpectFirstAndSecondSubframe{true, true, false};
-  ASSERT_NO_FATAL_FAILURE(ExpectParsedScriptElementLoadedStatusInFrames(
-      kSubframeNames, kExpectFirstAndSecondSubframe));
-  ExpectFramesIncludedInLayout(kSubframeNames, kExpectFirstAndSecondSubframe);
-
-  // Navigate the first subframe to a document that does not load the probe JS.
-  GURL allowed_empty_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_no_subresources.html"));
-  NavigateFrame(kSubframeNames[0], allowed_empty_subdocument_url);
-
-  // Finally, navigate the first subframe to an allowed URL that redirects to a
-  // disallowed URL, and verify that the navigation gets blocked and the frame
-  // collapsed.
-  const char kAllowedDomain[] = "allowed.com";
-  GURL disallowed_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_included_script.html"));
+      GetTestUrl("frame_with_included_script.html"));
   GURL redirect_to_disallowed_subdocument_url(embedded_test_server()->GetURL(
       kAllowedDomain, "/server-redirect?" + disallowed_subdocument_url.spec()));
   NavigateFrame(kSubframeNames[0], redirect_to_disallowed_subdocument_url);
@@ -234,7 +139,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterDryRunBrowserTest,
   base::HistogramTester histogram_tester;
   ukm::TestAutoSetUkmRecorder test_ukm_recorder;
 
-  GURL url(GetTestUrl(kTestFrameSetPath));
+  GURL url(GetTestUrl(kMultiPlatformTestFrameSetPath));
 
   // Would disallow loading child frame documents that in turn would end up
   // loading included_script.js, unless the document is loaded from an allowed
@@ -244,7 +149,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterDryRunBrowserTest,
   ASSERT_NO_FATAL_FAILURE(
       SetRulesetToDisallowURLsWithPathSuffix("included_script.html"));
 
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+  ASSERT_TRUE(NavigateToDestination(url));
 
   const std::vector<const char*> kSubframeNames{"one", "two", "three"};
   const std::vector<bool> kExpectAllSubframes{true, true, true};
@@ -254,17 +159,17 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterDryRunBrowserTest,
 
   // Now navigate the first subframe to an allowed URL and ensure that the load
   // successfully commits.
-  GURL allowed_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_allowed_script.html"));
+  GURL allowed_subdocument_url(GetTestUrl("frame_with_allowed_script.html"));
   NavigateFrame(kSubframeNames[0], allowed_subdocument_url);
 
   ASSERT_NO_FATAL_FAILURE(ExpectParsedScriptElementLoadedStatusInFrames(
       kSubframeNames, kExpectAllSubframes));
   ExpectFramesIncludedInLayout(kSubframeNames, kExpectAllSubframes);
 
-  // Navigate the first subframe to a document that does not load the probe JS.
+  // Navigate the first subframe to a document that does not load the probe
+  // JS.
   GURL allowed_empty_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_no_subresources.html"));
+      GetTestUrl("frame_with_no_subresources.html"));
   NavigateFrame(kSubframeNames[0], allowed_empty_subdocument_url);
 
   // Finally, navigate the first subframe to an allowed URL that redirects to a
@@ -272,7 +177,7 @@ IN_PROC_BROWSER_TEST_F(FingerprintingProtectionFilterDryRunBrowserTest,
   // blocked and the frame doesn't collapse under dry run mode.
   const char kAllowedDomain[] = "allowed.com";
   GURL disallowed_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_included_script.html"));
+      GetTestUrl("frame_with_included_script.html"));
   GURL redirect_to_disallowed_subdocument_url(embedded_test_server()->GetURL(
       kAllowedDomain, "/server-redirect?" + disallowed_subdocument_url.spec()));
   NavigateFrame(kSubframeNames[0], redirect_to_disallowed_subdocument_url);
@@ -335,13 +240,13 @@ IN_PROC_BROWSER_TEST_F(
   ASSERT_TRUE(embedded_test_server()->Start());
   base::HistogramTester histogram_tester;
 
-  GURL url(GetTestUrl(kTestFrameSetPath));
+  GURL url(GetTestUrl(kMultiPlatformTestFrameSetPath));
 
   // Disallow loading child frame documents that in turn would end up
   // loading included_script.js.
   ASSERT_NO_FATAL_FAILURE(
       SetRulesetToDisallowURLsWithPathSuffix("included_script.html"));
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+  ASSERT_TRUE(NavigateToDestination(url));
 
   const std::vector<const char*> kSubframeNames{"one", "two", "three"};
   const std::vector<bool> kExpectOnlySecondSubframe{false, true, false};
@@ -351,8 +256,7 @@ IN_PROC_BROWSER_TEST_F(
 
   // Now navigate the first subframe to an allowed URL and ensure that the load
   // successfully commits and the frame gets restored (no longer collapsed).
-  GURL allowed_subdocument_url(
-      GetTestUrl("subresource_filter/frame_with_allowed_script.html"));
+  GURL allowed_subdocument_url(GetTestUrl("frame_with_allowed_script.html"));
   NavigateFrame(kSubframeNames[0], allowed_subdocument_url);
 
   const std::vector<bool> kExpectFirstAndSecondSubframe{true, true, false};
@@ -377,6 +281,107 @@ IN_PROC_BROWSER_TEST_F(
   // FP performance measurements from DocumentSubresourceFilter. Currently, the
   // codepath is not triggered in FP browser tests because requests from
   // localhost are ignored in RendererUrlLoaderThrottle.
+}
+
+// TODO(https://crbug.com/379336042): The following tests cannot be included for
+// Android because of the usage of `Browser` (its header cannot be included for
+// Android targets). See if there is a potential workaround.
+#if !BUILDFLAG(IS_ANDROID)
+
+IN_PROC_BROWSER_TEST_F(
+    FingerprintingProtectionFilterEnabledInIncognitoBrowserTest,
+    SubframeDocumentLoadFiltering) {
+  // Close normal browser and switch the test's browser instance to an incognito
+  // instance.
+  Browser* incognito = CreateIncognitoBrowser(browser()->profile());
+  CloseBrowserSynchronously(browser());
+  SelectFirstBrowser();
+  ASSERT_EQ(browser(), incognito);
+
+  // TODO(https://crbug.com/358371545): Test console messaging for subframe
+  // blocking once its implementation is resolved.
+  base::HistogramTester histogram_tester;
+  ukm::TestAutoSetUkmRecorder test_ukm_recorder;
+
+  GURL url(GetTestUrl(kTestFrameSetPath));
+
+  // Disallow loading child frame documents that in turn would end up
+  // loading included_script.js, unless the document is loaded from an allowed
+  // (not in the blocklist) domain. This enables the third part of this test
+  // disallowing a load only after the first redirect.
+  ASSERT_NO_FATAL_FAILURE(
+      SetRulesetToDisallowURLsWithPathSuffix("included_script.html"));
+  ASSERT_TRUE(NavigateToDestination(url));
+
+  const std::vector<const char*> kSubframeNames{"one", "two", "three"};
+  const std::vector<bool> kExpectOnlySecondSubframe{false, true, false};
+  ASSERT_NO_FATAL_FAILURE(ExpectParsedScriptElementLoadedStatusInFrames(
+      kSubframeNames, kExpectOnlySecondSubframe));
+  ExpectFramesIncludedInLayout(kSubframeNames, kExpectOnlySecondSubframe);
+
+  // Now navigate the first subframe to an allowed URL and ensure that the load
+  // successfully commits and the frame gets restored (no longer collapsed).
+  GURL allowed_subdocument_url(GetTestUrl("frame_with_allowed_script.html"));
+  NavigateFrame(kSubframeNames[0], allowed_subdocument_url);
+
+  const std::vector<bool> kExpectFirstAndSecondSubframe{true, true, false};
+  ASSERT_NO_FATAL_FAILURE(ExpectParsedScriptElementLoadedStatusInFrames(
+      kSubframeNames, kExpectFirstAndSecondSubframe));
+  ExpectFramesIncludedInLayout(kSubframeNames, kExpectFirstAndSecondSubframe);
+
+  // Navigate the first subframe to a document that does not load the probe JS.
+  GURL allowed_empty_subdocument_url(
+      GetTestUrl("frame_with_no_subresources.html"));
+  NavigateFrame(kSubframeNames[0], allowed_empty_subdocument_url);
+
+  // Finally, navigate the first subframe to an allowed URL that redirects to a
+  // disallowed URL, and verify that the navigation gets blocked and the frame
+  // collapsed.
+  const char kAllowedDomain[] = "allowed.com";
+  GURL disallowed_subdocument_url(
+      GetTestUrl("frame_with_included_script.html"));
+  GURL redirect_to_disallowed_subdocument_url(embedded_test_server()->GetURL(
+      kAllowedDomain, "/server-redirect?" + disallowed_subdocument_url.spec()));
+  NavigateFrame(kSubframeNames[0], redirect_to_disallowed_subdocument_url);
+
+  ASSERT_NO_FATAL_FAILURE(ExpectParsedScriptElementLoadedStatusInFrames(
+      kSubframeNames, kExpectOnlySecondSubframe));
+
+  content::RenderFrameHost* frame = FindFrameByName(kSubframeNames[0]);
+  const auto last_committed_url = frame->GetLastCommittedURL();
+
+  ASSERT_TRUE(frame);
+  AssertUrlContained(last_committed_url,
+                     redirect_to_disallowed_subdocument_url);
+  AssertUrlContained(last_committed_url, disallowed_subdocument_url);
+
+  ExpectFramesIncludedInLayout(kSubframeNames, kExpectOnlySecondSubframe);
+
+  // Check test UKM recorder contains event with expected metrics.
+  const auto& entries = test_ukm_recorder.GetEntriesByName(
+      ukm::builders::FingerprintingProtection::kEntryName);
+  // 1 entry for every frame_with_included_script.html (2 from initial load, 1
+  // from redirect)
+  EXPECT_EQ(3u, entries.size());
+  for (const ukm::mojom::UkmEntry* entry : entries) {
+    test_ukm_recorder.ExpectEntryMetric(
+        entry, ukm::builders::FingerprintingProtection::kActivationDecisionName,
+        static_cast<int64_t>(
+            subresource_filter::ActivationDecision::ACTIVATED));
+    EXPECT_FALSE(test_ukm_recorder.EntryHasMetric(
+        entry, ukm::builders::FingerprintingProtection::kDryRunName));
+  }
+
+  histogram_tester.ExpectBucketCount(
+      ActivationDecisionHistogramName,
+      subresource_filter::ActivationDecision::ACTIVATED, 1);
+  histogram_tester.ExpectBucketCount(
+      ActivationLevelHistogramName,
+      subresource_filter::mojom::ActivationLevel::kEnabled, 1);
+  histogram_tester.ExpectTotalCount(kSubresourceLoadsTotalForPage, 1);
+  histogram_tester.ExpectTotalCount(kSubresourceLoadsEvaluatedForPage, 1);
+  histogram_tester.ExpectTotalCount(kSubresourceLoadsMatchedRulesForPage, 1);
+  histogram_tester.ExpectTotalCount(kSubresourceLoadsDisallowedForPage, 1);
 }
 
 class
@@ -452,5 +457,7 @@ IN_PROC_BROWSER_TEST_F(
   histogram_tester.ExpectTotalCount(kEvaluationTotalWallDurationForPage, 1);
   histogram_tester.ExpectTotalCount(kEvaluationTotalCPUDurationForPage, 1);
 }
+
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace fingerprinting_protection_filter
