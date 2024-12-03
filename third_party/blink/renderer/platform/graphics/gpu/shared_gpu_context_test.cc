cb76f2889541047d1635ac46afbee7a393d0e604
Colin Blundell <blundell@chromium.org>
2024-12-03 07:54:10 +0000

    [Blink] Update CRP::CreateSIProvider() calls [4]
    Transition them from the deprecated variant that takes in SkImageInfo to
    the new variant that takes in the exploded fields. This CL tackles the
    remaining callsites in tests.
    
    Bug: 371227617
    Change-Id: I9e35d499a1a752c3f7b9d1c78732a5e8afdbaa05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062199
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390834}

--- third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context_test.cc
+++ third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context_test.cc
@@ -218,8 +218,8 @@ TEST_F(BadSharedGpuContextTest, AccelerateImageBufferSurfaceCreationFails) {
   // return a nullptr provider
   std::unique_ptr<CanvasResourceProvider> resource_provider =
       CanvasResourceProvider::CreateSharedImageProvider(
-          SkImageInfo::MakeN32Premul(10, 10),
-          cc::PaintFlags::FilterQuality::kLow,
+          gfx::Size(10, 10), kN32_SkColorType, kPremul_SkAlphaType,
+          SkColorSpace::MakeSRGB(), cc::PaintFlags::FilterQuality::kLow,
           CanvasResourceProvider::ShouldInitialize::kNo,
           SharedGpuContext::ContextProviderWrapper(), RasterMode::kGPU,
           gpu::SharedImageUsageSet());
@@ -245,8 +245,8 @@ TEST_F(SharedGpuContextTestViz, AccelerateImageBufferSurfaceAutoRecovery) {
   EXPECT_FALSE(SharedGpuContext::IsValidWithoutRestoring());
   std::unique_ptr<CanvasResourceProvider> resource_provider =
       CanvasResourceProvider::CreateSharedImageProvider(
-          SkImageInfo::MakeN32Premul(10, 10),
-          cc::PaintFlags::FilterQuality::kLow,
+          gfx::Size(10, 10), kN32_SkColorType, kPremul_SkAlphaType,
+          SkColorSpace::MakeSRGB(), cc::PaintFlags::FilterQuality::kLow,
           CanvasResourceProvider::ShouldInitialize::kNo,
           SharedGpuContext::ContextProviderWrapper(), RasterMode::kGPU,
           gpu::SharedImageUsageSet());
