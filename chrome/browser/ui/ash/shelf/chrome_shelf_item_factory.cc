df9e5c52c4f553e0180d44c3ca8d32e2ed38ff33
Maksim Sisov <msisov@igalia.com>
2024-12-03 09:14:10 +0000

    Remove standalone_browser_extension_app* code
    This used to support lacros apps.
    
    Bug: 373971535, 373972275
    Change-Id: Ib869fb35dd3cfb8336a2650629ccd72dd9a02cc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042144
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1390871}

--- chrome/browser/ui/ash/shelf/chrome_shelf_item_factory.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_item_factory.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/shelf/arc_playstore_shortcut_shelf_item_controller.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
 #include "chrome/browser/ui/ash/shelf/shelf_controller_helper.h"
-#include "chrome/browser/ui/ash/shelf/standalone_browser_extension_app_shelf_item_controller.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/services/app_service/public/cpp/app_types.h"
