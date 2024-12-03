25e0bbebd9299106a93c1e46d983e0b1d791daec
Marijn Kruisselbrink <mek@chromium.org>
2024-12-03 22:20:57 +0000

    [AppShims] Fix app shim creation on non-stable channel with ad-hoc signing.
    When adhoc signed app shims are enabled, web_app_shortcut_copier
    is used to copy the generated app shim to its final location.
    The way web_app_shortcut_copier tries to setup a mojo connection
    to the app that launched it relies on base::apple::BaseBundleID to
    return the same value in the browser and helper process. However
    unless a process explicitly sets this value, it will return the
    bundle ID of stable channel. As such breaking creation of app
    shims from anything other than chromium builds or chrome stable.
    
    Fix this by getting the bundle ID from chrome::OuterAppBundle in
    web_app_shortcut_copier; this should return the correct bundle
    as long as we override the framework bundle path correctly (which
    we do).
    
    Bug: 382015346
    Change-Id: I7323261b48070ed1dd7c5169863b409f00f7e60b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6067566
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Mark Rowe <markrowe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391280}

--- base/apple/mach_port_rendezvous.h
+++ base/apple/mach_port_rendezvous.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/ios_buildflags.h"
 
 #if BUILDFLAG(IS_MAC)
-#include "base/apple/scoped_dispatch_object.h"
 #include "base/environment.h"
 #include "base/mac/process_requirement.h"
 #endif
@@ -226,18 +225,7 @@ class BASE_EXPORT MachPortRendezvousServerMac final
   MachPortRendezvousServerMac();
   ~MachPortRendezvousServerMac() override;
 
-  struct ClientData {
-    ClientData();
-    ClientData(ClientData&&);
-    ~ClientData();
-
-    // A DISPATCH_SOURCE_TYPE_PROC / DISPATCH_PROC_EXIT dispatch source. When
-    // the source is triggered, it calls OnClientExited().
-    apple::ScopedDispatchObject<dispatch_source_t> exit_watcher;
-
-    MachPortsForRendezvous ports;
-    std::optional<mac::ProcessRequirement> requirement;
-  };
+  struct ClientData;
 
   // Returns the `ClientData` for `pid`, creating it if necessary.
   // It will be cleaned up automatically when `pid` exits.
