efaed730974283528612fc62d4ecd78dc1528c1a
Zhe Li <zheliooo@google.com>
2024-12-03 22:47:50 +0000

    [Gardener] disable CreatorCoordinator and creatorMediator tests
    Bug: 381939495
    Change-Id: Ied52368c14d2be31cc5afe598f0519ebeca0d9e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6067846
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Owners-Override: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391298}

--- chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorMediatorTest.java
+++ chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorMediatorTest.java
@@ -17,6 +17,7 @@ import android.view.View;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -52,6 +53,7 @@ import org.chromium.url.JUnitTestGURLs;
 
 /** Tests for {@link CreatorMediator}. */
 @RunWith(BaseRobolectricTestRunner.class)
+@Ignore("Entire test class fails in multiple builds. crbug.com/381939495")
 public class CreatorMediatorTest {
     @Mock private WebFeedBridge.Natives mWebFeedBridgeJniMock;
     @Mock private FeedSurfaceRendererBridge.Natives mFeedSurfaceRendererBridgeJniMock;
