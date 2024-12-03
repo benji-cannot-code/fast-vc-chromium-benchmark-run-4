9f4392093d2bfae65f02c907efa19cd4c8595aa8
Paul Jensen <pauljensen@chromium.org>
2024-12-03 21:26:17 +0000

    Reland "Enable 5 Protected Audience and Fenced Frame features in Mode A/B"
    This reverts commit b550c350a6ac01554b5d25c999d8d58b9389f289.
    
    Reason for revert: Turned out to not be the cause of b/381980101 and
     b/381977849 as they continued after the revert, and look to be WPT
     cross-talk for which mitigations are already in progress (e.g.
     https://crrev.com/1388378)
    
    Original change's description:
    > Revert "Enable 5 Protected Audience and Fenced Frame features in Mode A/B"
    >
    > This reverts commit 7f52df769dac418870a2d2a8fcd3f7c1f114405b.
    >
    > Reason for revert: suspected culprit for b/381980101 and b/381977849
    >
    > Bug: 381980101, 381977849
    >
    > Original change's description:
    > > Enable 5 Protected Audience and Fenced Frame features in Mode A/B
    > >
    > > With the end of the Chrome-facilitated testing period, enable these features
    > > in Modes A and B to provide a more consistent developer experience and
    > > decrease the need for feature detection:
    > >  * PA Real Time Reporting
    > >  * PA multi-bid support
    > >  * PA deprecatedReplaceInURN via auction config
    > >  * PA multiple bidding contexts optimization
    > >  * FF allow cross-origin subframes to send reportEvent() beacons
    > >
    > > This was announced here:
    > >  https://groups.google.com/a/chromium.org/g/blink-dev/c/WgGGPElrG9o
    > >
    > > Change-Id: I7d26fb1ea908837557bf60cc8762245c409d0160
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049396
    > > Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    > > Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    > > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > > Commit-Queue: Nasko Oskov <nasko@chromium.org>
    > > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1390666}
    >
    > Change-Id: I4cf77749fb95b8de9cdc412c674f7edb9c481edd
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062886
    > Auto-Submit: Florian Jacky <fjacky@chromium.org>
    > Reviewed-by: Balazs Engedy <engedy@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Florian Jacky <fjacky@chromium.org>
    > Commit-Queue: Balazs Engedy <engedy@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1390874}
    
    Bug: 381980101, 381977849
    Change-Id: Ia963eac7362df95567f48fd8c0a4c07127df73d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6065414
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Owners-Override: Victor Tan <victortan@chromium.org>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391243}

--- third_party/blink/renderer/core/html/fenced_frame/fence.cc
+++ third_party/blink/renderer/core/html/fenced_frame/fence.cc
@@ -120,10 +120,7 @@ void Fence::reportEventToDestinationEnum(const FenceEvent* event,
   }
   if (event->crossOriginExposed() &&
       !base::FeatureList::IsEnabled(
-          blink::features::
-              kFencedFramesCrossOriginEventReportingUnlabeledTraffic) &&
-      !base::FeatureList::IsEnabled(
-          blink::features::kFencedFramesCrossOriginEventReportingAllTraffic)) {
+          blink::features::kFencedFramesCrossOriginEventReporting)) {
     exception_state.ThrowTypeError(
         "'crossOriginExposed' is not supported with reportEvent().");
     return;
@@ -197,10 +194,7 @@ void Fence::reportEventToDestinationURL(const FenceEvent* event,
   }
   if (event->crossOriginExposed() &&
       !base::FeatureList::IsEnabled(
-          blink::features::
-              kFencedFramesCrossOriginEventReportingUnlabeledTraffic) &&
-      !base::FeatureList::IsEnabled(
-          blink::features::kFencedFramesCrossOriginEventReportingAllTraffic)) {
+          blink::features::kFencedFramesCrossOriginEventReporting)) {
     exception_state.ThrowTypeError(
         "'crossOriginExposed' is not supported with reportEvent().");
     return;
