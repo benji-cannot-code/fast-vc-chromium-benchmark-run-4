5aa9d789d1ff6d16011497bc551cf40e6850ccb8
Dan Harrington <harringtond@chromium.org>
2024-12-03 22:13:44 +0000

    Move glic api impl to api directory
    to be shared with downstream
    
    Bug: 378715663
    
    Change-Id: Ib0ae0ed9df7224626c2eeef520ba0d62d6b0a7d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063835
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391276}

--- chrome/browser/resources/glic/glic_api/post_message_transport.ts
+++ chrome/browser/resources/glic/glic_api/post_message_transport.ts
@@ -11,7 +11,7 @@ import type {HostRequestTypes, WebClientRequestTypes} from './request_types.js';
 // even if the web client is recreated.
 
 // Requests sent over postMessage have this structure.
-interface RequestMessage {
+declare interface RequestMessage {
   // Present for any Glic request message.
   glicRequest: true;
   // The type of request.
@@ -25,7 +25,7 @@ interface RequestMessage {
 
 // Responses sent over postMessage have this structure. Responses are messages
 // sent in response to a `RequestMessage`.
-interface ResponseMessage {
+declare interface ResponseMessage {
   // The type of request.
   type: string;
   // The round-tripped `RequestMessage.requestId`.
@@ -35,7 +35,7 @@ interface ResponseMessage {
 }
 
 // Something that has postMessage() - probably a window or WindowProxy.
-interface PostMessageSender {
+declare interface PostMessageSender {
   postMessage(message: any, targetOrigin: string, transfer?: Transferable[]):
       void;
 }
