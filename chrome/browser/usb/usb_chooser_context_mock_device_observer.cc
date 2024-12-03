9d245482b7cad9961e3dee84550a94960fd37102
Sorin Jianu <sorin@chromium.org>
2024-12-03 04:19:24 +0000

    [chrome] Default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    chrome/browser/usb
    
    This is a mechanical change. There are a few instances of empty
    user-defined constructors and destructors left in the /chrome code
    which can be defaulted.
    
    This CL was uploaded by git cl split.
    
    R=reillyg@chromium.org
    
    Bug: 381119547
    Change-Id: I0c2dacf3c4cf1c5eae40d0bbc21db65893e61648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056218
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1390790}

--- chrome/browser/usb/usb_chooser_context_mock_device_observer.cc
+++ chrome/browser/usb/usb_chooser_context_mock_device_observer.cc
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/device/public/mojom/usb_device.mojom.h"
 
-MockDeviceObserver::MockDeviceObserver() {}
+MockDeviceObserver::MockDeviceObserver() = default;
 
-MockDeviceObserver::~MockDeviceObserver() {}
+MockDeviceObserver::~MockDeviceObserver() = default;
