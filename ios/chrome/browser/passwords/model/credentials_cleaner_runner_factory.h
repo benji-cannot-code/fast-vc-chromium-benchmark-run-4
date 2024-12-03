305176cf258a74b96313976b4fbeb1aad57929f3
Sylvain Defresne <sdefresne@chromium.org>
2024-12-03 10:55:29 +0000

    [ios] Convert CredentialsCleanerRunnerFactory to PKSFIOS
    Convert CredentialsCleanerRunnerFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363215065
    Change-Id: I193668e96f50afd61078963681b89221ad9741c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6064240
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390901}

--- ios/chrome/browser/passwords/model/credentials_cleaner_runner_factory.h
+++ ios/chrome/browser/passwords/model/credentials_cleaner_runner_factory.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_PASSWORDS_MODEL_CREDENTIALS_CLEANER_RUNNER_FACTORY_H_
 
 #include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 
@@ -16,7 +16,7 @@ class CredentialsCleanerRunner;
 }  // namespace password_manager
 
 // Creates instances of CredentialsCleanerRunner per Profile.
-class CredentialsCleanerRunnerFactory : public BrowserStateKeyedServiceFactory {
+class CredentialsCleanerRunnerFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static CredentialsCleanerRunnerFactory* GetInstance();
   static password_manager::CredentialsCleanerRunner* GetForProfile(
