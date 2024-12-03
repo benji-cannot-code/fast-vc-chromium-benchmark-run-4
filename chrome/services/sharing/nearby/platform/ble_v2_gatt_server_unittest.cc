59689fc9fac5f7b11e7368aa547d7d4a53b7203a
Peter Kasting <pkasting@chromium.org>
2024-12-03 00:27:13 +0000

    Remove usage of base::make_span(): chrome/
    Replace with span() CTAD use, or more targeted helpers.
    
    Bug: 341907909
    Change-Id: If2d722766687ab2b404e953281e6911504b203db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6060345
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390702}

--- chrome/services/sharing/nearby/platform/ble_v2_gatt_server_unittest.cc
+++ chrome/services/sharing/nearby/platform/ble_v2_gatt_server_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/sharing/nearby/platform/ble_v2_gatt_server.h"
 
 #include <memory>
+#include <string_view>
 
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
@@ -32,8 +33,8 @@ const device::BluetoothUUID kCharacteristicUuid1 =
     device::BluetoothUUID("00001101-0000-1000-8000-00805f9b34fb");
 const device::BluetoothUUID kCharacteristicUuid2 =
     device::BluetoothUUID("00001102-0000-1000-8000-00805f9b34fc");
-const std::string kNewCharacteristicValue = "123456";
-const int kPartialBufferOffset = 2;
+constexpr char kNewCharacteristicValue[] = "123456";
+constexpr int kPartialBufferOffset = 2;
 
 class FakeGattService : public nearby::chrome::BleV2GattServer::GattService {
  public:
@@ -291,8 +292,7 @@ TEST_F(BleV2GattServerTest,
   EXPECT_FALSE(read_result->is_error_code());
   EXPECT_TRUE(read_result->is_data());
   EXPECT_EQ(kNewCharacteristicValue,
-            base::as_string_view(
-                base::as_chars(base::make_span(read_result->get_data()))));
+            base::as_string_view(base::span(read_result->get_data())));
   histogram_tester_.ExpectBucketCount(
       "Nearby.Connections.BleV2.GattServer.OnLocalCharacteristicRead.Result",
       /*bucket: success=*/1, 1);
@@ -476,9 +476,9 @@ TEST_F(
   auto read_result = future.Take();
   EXPECT_FALSE(read_result->is_error_code());
   EXPECT_TRUE(read_result->is_data());
-  EXPECT_EQ(kNewCharacteristicValue.substr(kPartialBufferOffset),
-            base::as_string_view(
-                base::as_chars(base::make_span(read_result->get_data()))));
+  EXPECT_EQ(
+      std::string_view(kNewCharacteristicValue).substr(kPartialBufferOffset),
+      base::as_string_view(base::span(read_result->get_data())));
 }
 
 TEST_F(BleV2GattServerTest,
