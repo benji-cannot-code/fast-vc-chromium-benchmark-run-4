f51fe803e0c3e6ad15dd8ffccce3fdb26f837a5f
Marc Treib <treib@chromium.org>
2024-12-03 18:44:29 +0000

    [iOS] PriceTrackingPromoMediatorTest: Create profile via manager
    This CL updates the setup of PriceTrackingPromoMediatorTest so that the
    TestProfileIOS is created via TestProfileManagerIOS, instead of
    directly. This ensures that profile-related info in
    ProfileAttributesStorageIOS is initialized properly, which is required
    if kSeparateProfilesForManagedAccounts is enabled.
    
    Bug: 379077722
    Change-Id: I67f3bf61bdc60b49291be408dd951dca84dd5dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6058397
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391131}

--- ios/chrome/browser/ui/content_suggestions/price_tracking_promo/price_tracking_promo_mediator_unittest.mm
+++ ios/chrome/browser/ui/content_suggestions/price_tracking_promo/price_tracking_promo_mediator_unittest.mm
@@ -54,7 +54,7 @@ class PriceTrackingPromoMediatorTest : public PlatformTest {
         IOSChromeFaviconLoaderFactory::GetInstance(),
         IOSChromeFaviconLoaderFactory::GetDefaultFactory());
 
-    profile_ = std::move(builder).Build();
+    profile_ = profile_manager_.AddProfileWithBuilder(std::move(builder));
     auth_service_ = AuthenticationServiceFactory::GetForProfile(profile_.get());
     identity_ = [FakeSystemIdentity fakeIdentity1];
     FakeSystemIdentityManager* system_identity_manager =
@@ -130,7 +130,7 @@ class PriceTrackingPromoMediatorTest : public PlatformTest {
   web::WebTaskEnvironment task_environment_;
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   TestProfileManagerIOS profile_manager_;
-  std::unique_ptr<TestProfileIOS> profile_;
+  raw_ptr<TestProfileIOS> profile_;
   raw_ptr<AuthenticationService> auth_service_;
   id<SystemIdentity> identity_;
   std::unique_ptr<commerce::MockShoppingService> shopping_service_;
