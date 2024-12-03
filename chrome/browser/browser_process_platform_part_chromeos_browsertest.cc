c7535428073ef318be42b19350f9c7f88c661a41
Georg Neis <neis@chromium.org>
2024-12-03 02:19:20 +0000

    Delete chromeos/startup/*
    This code was used for Lacros and is no longer needed.
    Also delete the --lacros-mojo-socket-for-testing and
    --cros-startup-data-fd switches.
    
    Bug: b:365741912
    Change-Id: Id7476a775a8f8a2660a47fdc8c4cf2beea2ba98b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6057130
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390758}

--- chrome/browser/browser_process_platform_part_chromeos_browsertest.cc
+++ chrome/browser/browser_process_platform_part_chromeos_browsertest.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "chromeos/startup/browser_init_params.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/signin/public/base/signin_switches.h"
--- chrome/browser/extensions/api/preference/preference_api.cc
+++ chrome/browser/extensions/api/preference/preference_api.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/chromeos/extensions/controlled_pref_mapping.h"
-#include "chromeos/startup/browser_params_proxy.h"
 #endif
 
 using extensions::mojom::APIPermissionID;
