452e81e8c29fe4640e678b3e1697f1dcb8e415ce
Maryla <maryla@google.com>
2024-12-03 23:00:57 +0000

    Add missing BlinkFuzzerTestSupport to fuzz tests
    Bug: 381910341
    Change-Id: I3117ebd273558abf6de1dcafe969bd158aa276a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062916
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391306}

--- third_party/blink/renderer/platform/image-decoders/avif/crabbyavif_image_decoder_fuzzer.cc
+++ third_party/blink/renderer/platform/image-decoders/avif/crabbyavif_image_decoder_fuzzer.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static BlinkFuzzerTestSupport test_support;
   FuzzedDataProvider fdp(data, size);
   FuzzDecoder(DecoderType::kCrabbyAvifDecoder, fdp);
   return 0;
