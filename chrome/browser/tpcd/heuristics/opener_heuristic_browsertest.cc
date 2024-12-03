5c0d096d1a97efe7e6cec4a586a57d023de8b092
Peter Kasting <pkasting@chromium.org>
2024-12-03 01:10:07 +0000

    Take sample values as ints, not TimeDeltas.
    This avoids some awkward conversion code at many callsites.
    
    While touching this, made nonfunctional style changes to comply with
    https://google.github.io/styleguide/cppguide.html#Preincrement_and_Predecrement
    and https://google.github.io/styleguide/cppguide.html#Constant_Names.
    
    Bug: none
    Low-Coverage-Reason: TRIVIAL_CHANGE modifying arg types
    Change-Id: I1c2ebfe6560a59e2c9b1125f0b801ddb48d8b4f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6060943
    Reviewed-by: Joshua Hood <jdh@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390722}

--- chrome/browser/tpcd/heuristics/opener_heuristic_browsertest.cc
+++ chrome/browser/tpcd/heuristics/opener_heuristic_browsertest.cc
@@ -1026,9 +1026,8 @@ IN_PROC_BROWSER_TEST_P(OpenerHeuristicInteractionTypesBrowserTest,
             final_url);
   // The time between *popup_url* committing and the click.
   EXPECT_EQ(entries[0].metrics["SecondsSinceCommitted"],
-            Bucketize3PCDHeuristicTimeDelta(
-                base::Minutes(2), base::Minutes(3),
-                base::BindRepeating(&base::TimeDelta::InSeconds)));
+            Bucketize3PCDHeuristicSample(base::Minutes(2).InSeconds(),
+                                         base::Minutes(3).InSeconds()));
   // The user clicked on *final_url*, which was the third URL.
   EXPECT_EQ(entries[0].metrics["UrlIndex"], 3);
   EXPECT_EQ(entries[0].metrics["InteractionType"],
@@ -1183,9 +1182,8 @@ IN_PROC_BROWSER_TEST_F(OpenerHeuristicBrowserTest,
             popup_url);
   // The uncommitted navigation was ignored. UrlIndex is still 1.
   EXPECT_EQ(entries[0].metrics["SecondsSinceCommitted"],
-            Bucketize3PCDHeuristicTimeDelta(
-                base::Minutes(2), base::Minutes(3),
-                base::BindRepeating(&base::TimeDelta::InSeconds)));
+            Bucketize3PCDHeuristicSample(base::Minutes(2).InSeconds(),
+                                         base::Minutes(3).InSeconds()));
   EXPECT_EQ(entries[0].metrics["UrlIndex"], 1);
 }
 
