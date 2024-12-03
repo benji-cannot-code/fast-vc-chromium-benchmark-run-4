ffe9281fa28b0b6743a747f999925668a342d15d
Brando Socarras <brandosocarras@google.com>
2024-12-03 22:23:21 +0000

    [Quick Share v2] Set Your devices visibility from quick settings
    When Quick Share v2 is enabled:
    
    Select 'Your devices' visibility option in the Quick Share quick
    settings detailed view to put the device in 'Your devices' Quick Share
    visibility.
    
    Screencast
    ----------
    http://screencast/cast/NDgxMjg3NTY4NzI2NDI1NnxlZTkzMDEyOS0xYg
    
    Tested: utest in this, verified manually via DUT.
    Bug: b/323206955
    Change-Id: Ia25205be752e367a38506571b72e01695d15089c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6064541
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391283}

--- ash/system/nearby_share/nearby_share_detailed_view_impl_unittest.cc
+++ ash/system/nearby_share/nearby_share_detailed_view_impl_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/style/switch.h"
 #include "ash/system/tray/fake_detailed_view_delegate.h"
+#include "ash/system/tray/hover_highlight_view.h"
 #include "ash/test/ash_test_base.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
@@ -41,6 +42,11 @@ class NearbyShareDetailedViewImplTest : public AshTestBase {
   Switch* GetQuickShareToggle() const {
     CHECK(detailed_view_);
     return detailed_view_->toggle_switch_;
+  }
+
+  HoverHighlightView* GetYourDevicesRow() const {
+    CHECK(detailed_view_);
+    return detailed_view_->your_devices_row_;
   }
 
   size_t GetCloseBubbleCallCount() const {
@@ -112,6 +118,20 @@ TEST_F(NearbyShareDetailedViewImplTest, QuickShareV2_ToggleQuickShareOff) {
   LeftClickOn(quick_share_toggle);
   EXPECT_FALSE(quick_share_toggle->GetIsOn());
   EXPECT_FALSE(test_delegate_->IsEnabled());
+}
+
+TEST_F(NearbyShareDetailedViewImplTest, QuickShareV2_ToggleYourDevices) {
+  test_delegate_->SetEnabled(true);
+  test_delegate_->SetVisibility(
+      ::nearby_share::mojom::Visibility::kAllContacts);
+  SetUpDetailedView();
+  EXPECT_EQ(::nearby_share::mojom::Visibility::kAllContacts,
+            test_delegate_->GetVisibility());
+
+  HoverHighlightView* your_devices_row = GetYourDevicesRow();
+  LeftClickOn(your_devices_row);
+  EXPECT_EQ(::nearby_share::mojom::Visibility::kYourDevices,
+            test_delegate_->GetVisibility());
 }
 
 }  // namespace ash
