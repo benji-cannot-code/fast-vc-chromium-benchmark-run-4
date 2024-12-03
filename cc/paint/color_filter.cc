08b3682f4c64cdcd90bfcdfa8df543b6b54992d0
Maks Orlovich <morlovich@chromium.org>
2024-12-03 15:28:42 +0000

    [CodeHealth] Spanify WriteData in paint_op_writer.cc
    Fixed: 326458765
    Change-Id: If3e04cbd1a579f2a0f1bfbce34ae2b9cce9f0c54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5596530
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391002}

--- cc/paint/color_filter.cc
+++ cc/paint/color_filter.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check_op.h"
+#include "base/compiler_specific.h"
 #include "base/memory/values_equivalent.h"
 #include "cc/paint/paint_op_reader.h"
 #include "cc/paint/paint_op_writer.h"
@@ -99,10 +100,12 @@ class TableColorFilter : public ColorFilter {
     return PaintOpWriter::SerializedSizeOfBytes(256 * 4);
   }
   void SerializeData(PaintOpWriter& writer) const override {
-    writer.WriteData(256, table_->alphaTable());
-    writer.WriteData(256, table_->redTable());
-    writer.WriteData(256, table_->greenTable());
-    writer.WriteData(256, table_->blueTable());
+    // SAFETY: the various SkColorTable::...table() methods always return
+    // 256-byte arrays.
+    writer.WriteData(UNSAFE_BUFFERS(base::span(table_->alphaTable(), 256u)));
+    writer.WriteData(UNSAFE_BUFFERS(base::span(table_->redTable(), 256u)));
+    writer.WriteData(UNSAFE_BUFFERS(base::span(table_->greenTable(), 256u)));
+    writer.WriteData(UNSAFE_BUFFERS(base::span(table_->blueTable(), 256u)));
   }
 
  private:
--- cc/paint/paint_op_writer.h
+++ cc/paint/paint_op_writer.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bits.h"
+#include "base/containers/span.h"
 #include "base/memory/aligned_memory.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/stack_allocated.h"
@@ -210,7 +211,7 @@ class CC_PAINT_EXPORT PaintOpWriter {
                                     size_t serialized_size);
 
   // Write a sequence of arbitrary bytes.
-  void WriteData(size_t bytes, const void* input);
+  void WriteData(base::span<const uint8_t> data);
 
   // Returns the size of successfully written data, including paddings for
   // alignment.
@@ -356,7 +357,7 @@ class CC_PAINT_EXPORT PaintOpWriter {
     requires(std::is_trivially_copyable_v<T>)
   void Write(const std::vector<T>& vec) {
     WriteSize(vec.size());
-    WriteData(vec.size() * sizeof(T), vec.data());
+    WriteData(base::as_byte_span(vec));
   }
 
   template <typename T, typename... Args>
