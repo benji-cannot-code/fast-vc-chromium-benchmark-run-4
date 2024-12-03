6b7cc1c022d825cf2c39c3753e500bc1cc1fd78d
Georg Neis <neis@chromium.org>
2024-12-03 03:26:35 +0000

    Remove UI parts of Lacros-extension controlled proxy settings
    Remove obsolete Lacros leftovers related to ChromeOS network proxy
    settings controlled by an extension in Lacros. This CL focuses on
    the UI only, a followup CL will clean up the backend code.
    
    No change in behavior or look intended.
    
    Bug: b:365741912
    Change-Id: I7b1336c39fbfbcb291f960b1064089c8041c4e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6051606
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390780}

--- chrome/browser/ui/webui/extension_control_handler.cc
+++ chrome/browser/ui/webui/extension_control_handler.cc
@@ -14,13 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/disable_reason.h"
 #include "extensions/browser/extension_system.h"
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "base/strings/strcat.h"
-#include "chrome/browser/ash/crosapi/browser_manager.h"
-#include "chrome/browser/ui/browser_navigator_params.h"
-#include "chrome/common/webui_url_constants.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
 ExtensionControlHandler::ExtensionControlHandler() = default;
 ExtensionControlHandler::~ExtensionControlHandler() = default;
 
@@ -29,14 +22,6 @@ void ExtensionControlHandler::RegisterMessages() {
       "disableExtension",
       base::BindRepeating(&ExtensionControlHandler::HandleDisableExtension,
                           base::Unretained(this)));
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  web_ui()->RegisterMessageCallback(
-      "openExtensionPageInLacros",
-      base::BindRepeating(
-          &ExtensionControlHandler::HandleOpenExtensionPageInLacros,
-          base::Unretained(this)));
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
 void ExtensionControlHandler::HandleDisableExtension(
@@ -50,17 +35,3 @@ void ExtensionControlHandler::HandleDisableExtension(
   extension_service->DisableExtension(
       extension_id, extensions::disable_reason::DISABLE_USER_ACTION);
 }
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-void ExtensionControlHandler::HandleOpenExtensionPageInLacros(
-    const base::Value::List& args) {
-  CHECK_EQ(args.size(), 1u);
-  const std::string& extension_id = args[0].GetString();
-  GURL url(
-      base::StrCat({chrome::kChromeUIExtensionsURL, "?id=", extension_id}));
-  CHECK(url.is_valid());
-
-  crosapi::BrowserManager::Get()->SwitchToTab(
-      url, NavigateParams::PathBehavior::RESPECT);
-}
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
