a92815d6f313509d6f2ff7d5511ace683bdd274b
Florian Jacky <fjacky@chromium.org>
2024-12-03 13:45:08 +0000

    Revert "Update avatar badge background color in sign-in interception bubbles"
    This reverts commit de0e29d561febd33ed35c81eb11bb765188492a6.
    
    Reason for revert: Likely culprit for test failures, see bug
    
    Bug: 382027496
    
    
    Original change's description:
    > Update avatar badge background color in sign-in interception bubbles
    >
    > The background color of the avatar badge is set to match the
    > the header image color of the container it belongs to.
    > This change applies to the badge for both supervised users (kite
    > icon) and enterprise users (domain icon) that are displayed in the
    > sign-in interception bubble.
    >
    > The color of the badge icon is also dynamically set and derived
    > from the profile highlight color using profile_colors_util's
    > `GetProfileForegroundIconColor`.
    >
    > Screenshot of the result:
    > https://screenshot.googleplex.com/78QHcreAZGuMon3
    > https://screenshot.googleplex.com/Ak5tNPn2CAa7Vsz
    > https://screenshot.googleplex.com/6HntySVXAMPNciA (dark)
    >
    > Bug: 379892923
    > Change-Id: I6953ec4ce195895fed7a4c675657f7b147b3d67a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062800
    > Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1390899}
    
    Bug: 379892923
    Change-Id: If3b927d5d7cc1845c0dcef7f40b1560cbc6f88c8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063589
    Auto-Submit: Florian Jacky <fjacky@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1390950}

--- chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_browser_proxy.ts
+++ chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_browser_proxy.ts
@@ -30,8 +30,6 @@ export interface InterceptionParameters {
   primaryAccount: AccountInfo;
   useV2Design: boolean;
   showManagedDisclaimer: boolean;
-  interceptedProfileBadgeColor: string;
-  primaryProfileBadgeColor: string;
 }
 
 export interface ChromeSigninInterceptionParameters {
