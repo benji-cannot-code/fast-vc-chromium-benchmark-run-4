d8bc092381f00aa3a05ed8a7f620cf3deb3e1e9d
Tom Anderson <thomasanderson@chromium.org>
2024-12-03 22:27:42 +0000

    Create systemd unit before initializing GlobalShortcuts
    This is required so the desktop gets the proper application ID
    for the shortcuts session.
    
    R=thestig
    
    Change-Id: Id2bde6a5059e1a7f3ba8d867ccd3b4b79a3201ad
    Bug: 40759171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055266
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391288}

--- chrome/browser/extensions/global_shortcut_listener_linux.h
+++ chrome/browser/extensions/global_shortcut_listener_linux.h
@@ -21,7 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace dbus_xdg {
 class Request;
-}
+enum class SystemdUnitStatus;
+}  // namespace dbus_xdg
 
 namespace extensions {
 
@@ -115,6 +116,8 @@ class GlobalShortcutListenerLinux : public GlobalShortcutListener {
                          const std::string& signal_name,
                          bool success);
 
+  void OnSystemdUnitStarted(dbus_xdg::SystemdUnitStatus status);
+
   void OnServiceStarted(std::optional<bool> service_started);
 
   void CreateSession(SessionMapPair& pair);
@@ -125,7 +128,7 @@ class GlobalShortcutListenerLinux : public GlobalShortcutListener {
   scoped_refptr<dbus::Bus> bus_;
   raw_ptr<dbus::ObjectProxy> global_shortcuts_proxy_ = nullptr;
 
-  // Whether the GlboalShortcuts service is available, or nullopt if the status
+  // Whether the GlobalShortcuts service is available, or nullopt if the status
   // is not yet known.
   std::optional<bool> service_started_;
 
