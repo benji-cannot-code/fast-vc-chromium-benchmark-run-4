055629cc0911a791e78b82e66af62d57e6051155
Peter Kasting <pkasting@chromium.org>
2024-12-03 02:21:10 +0000

    Remove usage of base::make_span(): device/
    Replace with span() CTAD use, or more targeted helpers.
    
    Bug: 341907909
    Change-Id: Icb380803cbef70d2505c7a6ed12db295d82d56c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6058973
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1390760}

--- device/fido/cable/fido_ble_frames_fuzzer.cc
+++ device/fido/cable/fido_ble_frames_fuzzer.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/fido_constants.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* raw_data, size_t size) {
-  auto data_span = base::make_span(raw_data, size);
+  auto data_span = base::span(raw_data, size);
   std::vector<uint8_t> data(data_span.begin(), data_span.end());
 
   {
--- device/fido/cros/authenticator.cc
+++ device/fido/cros/authenticator.cc
@@ -215,7 +215,7 @@ void ChromeOSAuthenticator::OnMakeCredentialResponse(
 
   std::optional<AuthenticatorData> authenticator_data =
       AuthenticatorData::DecodeAuthenticatorData(
-          base::as_bytes(base::make_span(response->authenticator_data())));
+          base::as_byte_span(response->authenticator_data()));
   if (!authenticator_data) {
     FIDO_LOG(ERROR) << "Authenticator data corrupted.";
     std::move(callback).Run(MakeCredentialStatus::kAuthenticatorResponseInvalid,
@@ -223,8 +223,8 @@ void ChromeOSAuthenticator::OnMakeCredentialResponse(
     return;
   }
 
-  std::optional<cbor::Value> statement_map = cbor::Reader::Read(
-      base::as_bytes(base::make_span(response->attestation_statement())));
+  std::optional<cbor::Value> statement_map =
+      cbor::Reader::Read(base::as_byte_span(response->attestation_statement()));
   if (!statement_map ||
       statement_map.value().type() != cbor::Value::Type::MAP) {
     FIDO_LOG(ERROR) << "Attestation statement is not a CBOR map.";
@@ -338,7 +338,7 @@ void ChromeOSAuthenticator::OnGetAssertionResponse(
 
   std::optional<AuthenticatorData> authenticator_data =
       AuthenticatorData::DecodeAuthenticatorData(
-          base::as_bytes(base::make_span(assertion.authenticator_data())));
+          base::as_byte_span(assertion.authenticator_data()));
   if (!authenticator_data) {
     FIDO_LOG(ERROR) << "Authenticator data corrupted.";
     std::move(callback).Run(GetAssertionStatus::kAuthenticatorResponseInvalid,
