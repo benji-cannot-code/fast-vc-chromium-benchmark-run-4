051f75b6cbca1347919af623ae8dc5cf64b216a6
Scott Yoder <scottyoder@google.com>
2024-12-03 22:29:22 +0000

    [ios] Fix overscroll actions on the NTP
    This fixes a few issues with the use of overscroll actions:
     * switches back to initializing the OverscrollActionsController right
       away on the NTP so that the UIPanGestureRecognizer can be started
       before the first drag event
     * rounds a float value used to determine if a pull event has been
       started. On the NTP, this was sometimes causing the overscroll action
       controller to never notice that a pull had ended so that it could
       start again.
     * stops the code from creating multiple pan gesture recognizers. This
       was happening every time the OverscrollActionsController was
       instantiated on the NTP.
    
    This change also removes some unused code (scheduleInvalidate method).
    
    Fixed: 382033412
    Change-Id: I347c2c0b5a60679f67198ca1241f17e3a09c7ab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6067033
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391290}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_view_controller.mm
@@ -522,11 +522,7 @@ const CGFloat kFeedContainerExtraHeight = 500;
   self.collectionView.clipsToBounds = NO;
 
   [self.overscrollActionsController invalidate];
-
-  // Only re-configure `overscrollActionsController`.
-  if (self.overscrollActionsController) {
-    [self configureOverscrollActionsController];
-  }
+  [self configureOverscrollActionsController];
 
   // Update NTP collection view constraints to ensure the layout adapts to
   // changes in feed visibility.
@@ -844,10 +840,6 @@ const CGFloat kFeedContainerExtraHeight = 500;
   // now replaced. In these cases, ignore the scroll event.
   if (scrollView != self.collectionView) {
     return;
-  }
-
-  if (!self.overscrollActionsController) {
-    [self configureOverscrollActionsController];
   }
 
   // User has interacted with the surface, so it is safe to assume that a saved
--- ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_controller.mm
+++ ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_controller.mm
@@ -167,9 +167,6 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
   // Records if a transition to the overscroll state ACTION_READY was made.
   // This is used to record a cancel gesture.
   BOOL _didTransitionToActionReady;
-  // Records that the controller will be dismissed at the end of the current
-  // animation. No new action should be started.
-  BOOL _shouldInvalidate;
   // Store the set of notifications that did increment the overscroll actions
   // lock. It is used in order to enforce the fact that the lock should only be
   // incremented/decremented once for a given notification.
@@ -332,14 +329,6 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
   return gInstanceCount;
 }
 
-- (void)scheduleInvalidate {
-  if (self.overscrollState == OverscrollState::NO_PULL_STARTED) {
-    [self invalidate];
-  } else {
-    _shouldInvalidate = YES;
-  }
-}
-
 - (void)invalidate {
   [self clear];
   [self stopBounce];
@@ -395,7 +384,7 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
   CGFloat topMargin = 0;
   if (!_webViewProxy)
     topMargin = self.scrollView.safeAreaInsets.top;
-  if (contentOffsetFromExpandedHeader >= 0) {
+  if (round(contentOffsetFromExpandedHeader) >= 0) {
     // Record initial content offset and dispatch delegate on state change.
     self.overscrollState = OverscrollState::NO_PULL_STARTED;
   } else {
@@ -680,6 +669,10 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
 }
 
 - (void)setup {
+  if (self.panGestureRecognizer) {
+    return;
+  }
+
   UIPanGestureRecognizer* panGesture;
   // Workaround a bug occurring when Speak Selection is enabled.
   // See crbug.com/699655.
@@ -839,9 +832,6 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
       self.panPointScreenOrigin = CGPointZero;
       [self resetScrollViewTopContentInset];
       self.disablingFullscreen = NO;
-      if (_shouldInvalidate) {
-        [self invalidate];
-      }
     } break;
     case OverscrollState::STARTED_PULLING: {
       if (!self.overscrollActionView.superview && self.scrollViewDragged) {
@@ -961,9 +951,6 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
 #pragma mark - Bounce dynamic
 
 - (void)startBounceWithInitialVelocity:(CGPoint)velocity {
-  if (_shouldInvalidate) {
-    return;
-  }
   [self stopBounce];
   CADisplayLink* dpLink =
       [CADisplayLink displayLinkWithTarget:self
@@ -1040,9 +1027,6 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
 
 - (void)overscrollActionsViewDidTapTriggerAction:
     (OverscrollActionsView*)overscrollActionsView {
-  if (_shouldInvalidate) {
-    return;
-  }
   [self.overscrollActionView displayActionAnimation];
   [self
       recordMetricForTriggeredAction:self.overscrollActionView.selectedAction];
