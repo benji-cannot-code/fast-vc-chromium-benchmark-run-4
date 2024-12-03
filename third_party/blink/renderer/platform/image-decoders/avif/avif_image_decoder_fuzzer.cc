748c0f92ddd2db50a19c2923a5b76dfd0f1d26fd
Maryla <maryla@google.com>
2024-12-03 09:36:10 +0000

    Fuzz AvifDecoder and CrabbyAvifDecoder constructor parameters.
    Change-Id: Id22c2c2b9181e67c10e9f67615cf66f64d30f282
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039162
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Maryla Ustarroz-Calonge <maryla@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390877}

--- third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder_fuzzer.cc
+++ third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder_fuzzer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder.h"
 
+#include <fuzzer/FuzzedDataProvider.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/color_behavior.h"
 #include "third_party/blink/renderer/platform/image-decoders/image_decoder.h"
+#include "third_party/blink/renderer/platform/image-decoders/image_decoder_fuzzer_utils.h"
 #include "third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
@@ -19,29 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::unique_ptr<ImageDecoder> CreateAVIFDecoder() {
-  // TODO(crbug.com/323934468): Initialize decoder settings dynamically using
-  // fuzzer input.
-  return std::make_unique<AVIFImageDecoder>(
-      ImageDecoder::kAlphaPremultiplied, ImageDecoder::kDefaultBitDepth,
-      ColorBehavior::kTransformToSRGB, cc::AuxImage::kDefault,
-      ImageDecoder::kNoDecodedImageByteLimit,
-      ImageDecoder::AnimationOption::kPreferAnimation);
-}
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  static BlinkFuzzerTestSupport test_support;
-  test::TaskEnvironment task_environment;
-  auto buffer = SharedBuffer::Create(data, size);
-  auto decoder = CreateAVIFDecoder();
-  constexpr static bool kAllDataReceived = true;
-  decoder->SetData(buffer.get(), kAllDataReceived);
-  for (wtf_size_t frame = 0; frame < decoder->FrameCount(); ++frame) {
-    decoder->DecodeFrameBufferAtIndex(frame);
-    if (decoder->Failed()) {
-      return 0;
-    }
-  }
+  FuzzedDataProvider fdp(data, size);
+  FuzzDecoder(DecoderType::kAvifDecoder, fdp);
   return 0;
 }
 
