fd550aeb963b4eb9ec37db3af594b22075609f72
Andrew Grieve <agrieve@chromium.org>
2024-12-03 22:31:00 +0000

    Android: Add custom annotations for use by NullAway (e.g. @Nullable)
    Based on the ones from jspecify & nullaway. Using our own copies so that
    cronet does not need to worry about handling extra deps.
    
    See: https://github.com/uber/NullAway/wiki/Supported-Annotations
    
    Bug: 40657579
    Change-Id: Ic8c9670329d9eaf141a569b881a64f5818725e2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6065417
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391292}

---
+++ build/android/java/src/org/chromium/build/annotations/NullMarked.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.build.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** Ensures the annotated class or method is checked by NullAway. */
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.CLASS)
+public @interface NullMarked {}
