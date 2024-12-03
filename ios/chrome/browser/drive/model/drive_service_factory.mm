7f8f090625ae146dce9491358145462ca764e3a8
Federica Germinario <fedegermi@google.com>
2024-12-03 13:39:43 +0000

    Use ProfileKeyedServiceFactoryIOS in DriveServiceFactory
    Convert DriveServiceFactory to use ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215734
    Change-Id: I9c7f7733ad19ad11f944a40b0e766d906035e36b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062963
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390947}

--- ios/chrome/browser/drive/model/drive_service_factory.mm
+++ ios/chrome/browser/drive/model/drive_service_factory.mm
@@ -6,12 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/drive/model/drive_service_factory.h"
 
 #import "base/feature_list.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "ios/chrome/app/tests_hook.h"
 #import "ios/chrome/browser/drive/model/drive_service.h"
 #import "ios/chrome/browser/drive/model/drive_service_configuration.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
-#import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
@@ -22,8 +20,8 @@ namespace drive {
 
 // static
 DriveService* DriveServiceFactory::GetForProfile(ProfileIOS* profile) {
-  return static_cast<DriveService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<DriveService>(profile,
+                                                             /*create=*/true);
 }
 
 // static
@@ -33,9 +31,8 @@ DriveServiceFactory* DriveServiceFactory::GetInstance() {
 }
 
 DriveServiceFactory::DriveServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "DriveService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("DriveService",
+                                    ProfileSelection::kRedirectedInIncognito) {
   DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(ChromeAccountManagerServiceFactory::GetInstance());
 }
@@ -50,9 +47,8 @@ std::unique_ptr<KeyedService> DriveServiceFactory::BuildServiceInstanceFor(
     return overridden_drive_service;
   }
 
-  ApplicationContext* application_context = GetApplicationContext();
   drive::DriveServiceConfiguration configuration{};
-  configuration.sso_service = application_context->GetSingleSignOnService();
+  configuration.sso_service = GetApplicationContext()->GetSingleSignOnService();
   ProfileIOS* profile = ProfileIOS::FromBrowserState(context);
   configuration.pref_service = profile->GetPrefs();
   configuration.identity_manager =
@@ -60,11 +56,6 @@ std::unique_ptr<KeyedService> DriveServiceFactory::BuildServiceInstanceFor(
   configuration.account_manager_service =
       ChromeAccountManagerServiceFactory::GetForProfile(profile);
   return ios::provider::CreateDriveService(configuration);
-}
-
-web::BrowserState* DriveServiceFactory::GetBrowserStateToUse(
-    web::BrowserState* context) const {
-  return GetBrowserStateRedirectedInIncognito(context);
 }
 
 }  // namespace drive
