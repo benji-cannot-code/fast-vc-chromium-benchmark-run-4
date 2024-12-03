993d426e11c1014ff56b8f2c421a52ac1a4daae0
Federica Germinario <fedegermi@google.com>
2024-12-03 13:51:28 +0000

    Use ProfileKeyedServiceFactoryIOS in AboutSigninInternalsFactory
    Convert AboutSigninInternalsFactory to ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215738
    Change-Id: Ie0710622368efdf3230949509f384d05eef58d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063588
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390954}

--- ios/chrome/browser/signin/model/about_signin_internals_factory.cc
+++ ios/chrome/browser/signin/model/about_signin_internals_factory.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/signin/model/about_signin_internals_factory.h"
 
-#include <utility>
-
-#include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/signin/core/browser/about_signin_internals.h"
 #include "ios/chrome/browser/shared/model/profile/profile_ios.h"
@@ -20,22 +16,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ios {
 
 AboutSigninInternalsFactory::AboutSigninInternalsFactory()
-    : BrowserStateKeyedServiceFactory(
-          "AboutSigninInternals",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("AboutSigninInternals") {
   DependsOn(ios::AccountReconcilorFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(SigninClientFactory::GetInstance());
   DependsOn(SigninErrorControllerFactory::GetInstance());
 }
 
-AboutSigninInternalsFactory::~AboutSigninInternalsFactory() {}
+AboutSigninInternalsFactory::~AboutSigninInternalsFactory() = default;
 
 // static
 AboutSigninInternals* AboutSigninInternalsFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<AboutSigninInternals*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<AboutSigninInternals>(
+      profile, /*create=*/true);
 }
 
 // static
--- ios/chrome/browser/signin/model/about_signin_internals_factory.h
+++ ios/chrome/browser/signin/model/about_signin_internals_factory.h
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SIGNIN_MODEL_ABOUT_SIGNIN_INTERNALS_FACTORY_H_
 #define IOS_CHROME_BROWSER_SIGNIN_MODEL_ABOUT_SIGNIN_INTERNALS_FACTORY_H_
 
-#import <memory>
-
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class AboutSigninInternals;
 class ProfileIOS;
@@ -17,14 +15,10 @@ class ProfileIOS;
 namespace ios {
 // Singleton that owns all AboutSigninInternals and associates them with browser
 // states.
-class AboutSigninInternalsFactory : public BrowserStateKeyedServiceFactory {
+class AboutSigninInternalsFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static AboutSigninInternals* GetForProfile(ProfileIOS* profile);
   static AboutSigninInternalsFactory* GetInstance();
-
-  AboutSigninInternalsFactory(const AboutSigninInternalsFactory&) = delete;
-  AboutSigninInternalsFactory& operator=(const AboutSigninInternalsFactory&) =
-      delete;
 
  private:
   friend class base::NoDestructor<AboutSigninInternalsFactory>;
