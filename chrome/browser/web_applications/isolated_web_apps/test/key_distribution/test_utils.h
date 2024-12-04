1109bd86f4ceea5bb1c88c16de92bf7a955cbb0d
Andrew Rayskiy <greengrape@google.com>
2024-12-04 14:05:10 +0000

    iwa: Improve the testing infra around component updater
    Bug: 380815149
    Change-Id: I8a3a380f596f06cd80be76efed4f2b91f193ace7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070252
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391627}

--- chrome/browser/web_applications/isolated_web_apps/test/key_distribution/test_utils.h
+++ chrome/browser/web_applications/isolated_web_apps/test/key_distribution/test_utils.h
@@ -17,6 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app::test {
 
+struct IwaComponentMetadata {
+  base::Version version;
+  bool is_preloaded;
+};
+
 // Synchronously updates the key distribution info provider with data from
 // `path`.
 base::expected<void, IwaComponentUpdateError> UpdateKeyDistributionInfo(
@@ -53,9 +58,10 @@ InstallIwaKeyDistributionComponent(
     const std::string& web_bundle_id,
     std::optional<base::span<const uint8_t>> expected_key);
 
-// Synchronously registers the preloaded component with the component updater.
-base::expected<void, IwaComponentUpdateError>
-RegisterPreloadedIwaKeyDistributionComponent();
+// Synchronously registers the component with the component updater and waits
+// for the component updater to pick up the on-disk data in its folder.
+base::expected<IwaComponentMetadata, IwaComponentUpdateError>
+RegisterIwaKeyDistributionComponentAndWaitForLoad();
 
 }  // namespace web_app::test
 
