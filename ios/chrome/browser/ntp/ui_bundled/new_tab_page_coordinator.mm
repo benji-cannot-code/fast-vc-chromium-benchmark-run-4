25099d4e80c8035998b5e7e2e469c506475c99b3
adamta <adamta@google.com>
2024-12-04 19:19:00 +0000

    [iOS Fixit] Enable NTP a11y elements when customization menu dismissed
    Improved a11y enabling/disabling during Home Customization menu
    navigation, fixing issue where the NTP's a11y elements would remain
    disabled.
    
    Fixed: 364569640
    Change-Id: I01f3ec6cb14d96356c1f38c8f99e3d6d5e25d39a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6069160
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391784}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -2013,7 +2013,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!_customizationCoordinator) {
     return;
   }
-  [self.NTPViewController dismissViewControllerAnimated:YES completion:nil];
   [_customizationCoordinator stop];
   _customizationCoordinator = nil;
 }
