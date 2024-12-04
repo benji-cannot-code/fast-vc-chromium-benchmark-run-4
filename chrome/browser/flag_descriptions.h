209d2cfd6945cd2c2d46908d3b28fed5515c76fc
Mariam Ali <alimariam@google.com>
2024-12-04 12:58:34 +0000

    CBD Timeframe Experiment: Clean up
    Bug: 40283307
    Change-Id: Ie5fbe8e736e680dc4164ea7b70471db91e269eaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062890
    Auto-Submit: Mariam Ali <alimariam@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391613}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1803,9 +1803,6 @@ extern const char kPreloadingOnPerformancePageDescription[];
 extern const char kPrerender2Name[];
 extern const char kPrerender2Description[];
 
-extern const char kCbdTimeframeRequiredName[];
-extern const char kCbdTimeframeRequiredDescription[];
-
 extern const char kPriceChangeModuleName[];
 extern const char kPriceChangeModuleDescription[];
 
