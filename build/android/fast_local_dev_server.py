83be686045cc1eac416f2afc89b6d8be27fa61c4
Mohamed Heikal <mheikal@chromium.org>
2024-12-04 03:36:21 +0000

    Add new line to remote printed output in fast_local_dev_server
    Also fixes race condition in test by only finishing a task when all
    actions related to it are done.
    
    Bug: 370589852, 382113315
    Change-Id: I618a300550aed41eda3b9b840d89ac97cc9dfe3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6067686
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391437}

--- build/android/fast_local_dev_server.py
+++ build/android/fast_local_dev_server.py
@@ -423,7 +423,6 @@ class Task:
     This method should only be run once per task. Avoid modifying the task so
     that this method does not need locking."""
 
-    TaskStats.complete_task(build_id=self.build_id)
     delete_stamp = False
     status_string = 'FINISHED'
     if self._terminated:
@@ -448,7 +447,10 @@ class Task:
       if self.remote_print:
         # Add emoji to show that output is from the build server.
         preamble = [f'⏩ {line}' for line in preamble]
-        self.tty.write('\n'.join(preamble + [stdout]))
+        remote_message = '\n'.join(preamble + [stdout])
+        # Add a new line at start of message to clearly delineate from previous
+        # output/text already on the remote tty we are printing to.
+        self.tty.write(f'\n{remote_message}')
         self.tty.flush()
     set_status(f'{status_string} {self.name}',
                quiet=self.options.quiet,
@@ -464,6 +466,7 @@ class Task:
       # about the mtime that is recorded in its database at the time the
       # original action finished.
       pass
+    TaskStats.complete_task(build_id=self.build_id)
 
 
 def _handle_add_task(data, current_tasks: Dict[Tuple[str, str], Task], options):
