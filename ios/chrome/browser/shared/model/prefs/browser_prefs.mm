b4ee5659e2ec133231ea827c826c203b0ee3f8ca
Federica Germinario <fedegermi@google.com>
2024-12-04 15:57:09 +0000

    [ios] Remove kPrivacySandboxManuallyControlled from BrowserPrefs
    The key was deprecated in (M119):
    https://chromium-review.googlesource.com/c/chromium/src/+/4878776
    
    Bug: none
    Change-Id: I83482d1599a508df92a8229c64fbc9c1eadaf684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068369
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391688}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -130,12 +130,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Deprecated 09/2023.
-// Synced boolean that indicates if a user has manually toggled the settings
-// associated with the PrivacySandboxSettings feature.
-inline constexpr char kPrivacySandboxManuallyControlled[] =
-    "privacy_sandbox.manually_controlled";
-
 // Deprecated 10/2023.
 // Boolean whether has requested sync to be enabled. This is set early in the
 // sync setup flow, after the user has pressed "turn on sync" but before they
@@ -983,7 +977,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(prefs::kIosParcelTrackingOptInPromptSwipedDown,
                                 false);
 
-  registry->RegisterBooleanPref(kPrivacySandboxManuallyControlled, false);
   // Register prefs used to skip too frequent History Sync Opt-In prompt.
   history_sync::RegisterProfilePrefs(registry);
 
@@ -1203,9 +1196,6 @@ void MigrateObsoleteProfilePrefs(PrefService* prefs) {
   invalidation::InvalidatorRegistrarWithMemory::ClearDeprecatedPrefs(prefs);
   invalidation::PerUserTopicSubscriptionManager::ClearDeprecatedPrefs(prefs);
   invalidation::FCMInvalidationService::ClearDeprecatedPrefs(prefs);
-
-  // Added 09/2023.
-  prefs->ClearPref(kPrivacySandboxManuallyControlled);
 
   NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
   // Added 09/2023.
