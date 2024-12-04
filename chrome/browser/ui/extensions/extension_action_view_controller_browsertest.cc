63f6d841c3e93282208bbc4eca319f80f0d47a98
EmiliaPaz <emiliapaz@chromium.org>
2024-12-04 17:11:35 +0000

    [Extensions cleanup] Remove unused include in action controller test
    Bug: n/a
    Change-Id: I69a04d7a9804b2aba9979963b47f194b65f04906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068588
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391732}

--- chrome/browser/ui/extensions/extension_action_view_controller_browsertest.cc
+++ chrome/browser/ui/extensions/extension_action_view_controller_browsertest.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/permissions/site_permissions_helper.h"
 #include "chrome/browser/extensions/test_extension_system.h"
 #include "chrome/browser/extensions/user_script_listener.h"
-#include "chrome/browser/ui/extensions/extension_action_test_helper.h"
 #include "chrome/browser/ui/extensions/extensions_container.h"
 #include "chrome/browser/ui/extensions/icon_with_badge_image_source.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
