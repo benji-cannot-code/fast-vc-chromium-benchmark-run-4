acdaa37bceeaea75cd8675bd292c37fc568fb785
Thomas Guilbert <tguilbert@chromium.org>
2024-12-04 20:04:37 +0000

    Revert "Reland "Update avatar badge background color in sign-in interception bubbles""
    This reverts commit 480deeaa0b60ac72d5aabebc2164f333ebabee00.
    
    Reason for revert: Causing ASAN/LSAN test failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/129774/test-results?sortby=&groupby=
    
    Original change's description:
    > Reland "Update avatar badge background color in sign-in interception bubbles"
    >
    > This reverts commit 53b601431b57a4236f9b1e75d53959f548ad74c1.
    >
    > Reason for revert: No code changes necessary.
    > I have now approved the new reference images for the failing pixel tests. Triggering mega CQ & the win-rel builder that mirrors the failing builder Win10 Tests x64.
    >
    > Original change's description:
    > > Revert "Update avatar badge background color in sign-in interception bubbles"
    > >
    > > This reverts commit de0e29d561febd33ed35c81eb11bb765188492a6.
    > >
    > > Reason for revert: Likely culprit for test failures, see bug
    > >
    > > Bug: 382027496
    > >
    > >
    > > Original change's description:
    > > > Update avatar badge background color in sign-in interception bubbles
    > > >
    > > > The background color of the avatar badge is set to match the
    > > > the header image color of the container it belongs to.
    > > > This change applies to the badge for both supervised users (kite
    > > > icon) and enterprise users (domain icon) that are displayed in the
    > > > sign-in interception bubble.
    > > >
    > > > The color of the badge icon is also dynamically set and derived
    > > > from the profile highlight color using profile_colors_util's
    > > > `GetProfileForegroundIconColor`.
    > > >
    > > > Screenshot of the result:
    > > > https://screenshot.googleplex.com/78QHcreAZGuMon3
    > > > https://screenshot.googleplex.com/Ak5tNPn2CAa7Vsz
    > > > https://screenshot.googleplex.com/6HntySVXAMPNciA (dark)
    > > >
    > > > Bug: 379892923
    > > > Change-Id: I6953ec4ce195895fed7a4c675657f7b147b3d67a
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062800
    > > > Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    > > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > > Reviewed-by: David Roger <droger@chromium.org>
    > > > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1390899}
    > >
    > > Bug: 379892923
    > > Change-Id: If3b927d5d7cc1845c0dcef7f40b1560cbc6f88c8
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063589
    > > Auto-Submit: Florian Jacky <fjacky@chromium.org>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Owners-Override: Florian Jacky <fjacky@chromium.org>
    > > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/main@{#1390950}
    >
    > Bug: 382027496
    > Bug: 379892923
    > Change-Id: I24a7c331367dcc314a6dfab262d4ff5fab8e9856
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063592
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1391733}
    
    Bug: 382027496
    Bug: 379892923
    Change-Id: I87fc9c03911361d02b02400f645510e38a4c9a2e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070759
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Owners-Override: Thomas Guilbert <tguilbert@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1391806}

--- chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.ts
+++ chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.ts
@@ -64,8 +64,6 @@ export class DiceWebSigninInterceptAppElement extends
     primaryAccount: {pictureUrl: '', avatarBadge: ''},
     useV2Design: false,
     showManagedDisclaimer: false,
-    interceptedProfileBadgeColor: '',
-    primaryProfileBadgeColor: '',
   };
   protected acceptButtonClicked_: boolean = false;
   private diceWebSigninInterceptBrowserProxy_:
@@ -106,10 +104,6 @@ export class DiceWebSigninInterceptAppElement extends
     this.style.setProperty(
         '--primary-profile-color', parameters.primaryProfileColor);
     this.style.setProperty('--header-text-color', parameters.headerTextColor);
-    this.style.setProperty(
-      '--intercepted-profile-avatar-badge-color', parameters.interceptedProfileBadgeColor);
-    this.style.setProperty(
-      '--primary-profile-avatar-badge-color', parameters.primaryProfileBadgeColor);
   }
 
   protected sanitizeInnerHtml_(text: string): TrustedHTML {
--- chrome/test/data/webui/signin/test_dice_web_signin_intercept_browser_proxy.ts
+++ chrome/test/data/webui/signin/test_dice_web_signin_intercept_browser_proxy.ts
@@ -35,8 +35,6 @@ export class TestDiceWebSigninInterceptBrowserProxy extends TestBrowserProxy
       primaryAccount: {pictureUrl: '', avatarBadge: ''},
       useV2Design: false,
       showManagedDisclaimer: false,
-      interceptedProfileBadgeColor: '',
-      primaryProfileBadgeColor: '',
     };
 
     this.chromeSigninInterceptionParameters_ = {
