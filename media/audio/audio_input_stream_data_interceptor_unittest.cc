60bfa00393c4185e9972130e469831b57a10580b
Ho Cheung <hocheung@chromium.org>
2024-12-04 00:42:51 +0000

    Revert "Remove underscores from test names to follow GTest conventions //media." #10
    This reverts commit dd89c95a58d44e443e12c5d2e4f4a7cdc9cd1151.
    
    Reason: https://groups.google.com/a/chromium.org/g/chromium-dev/c/KxzyXKv7adQ
    
    Based on the discussion here, I have decided to terminate this work.
    
    This CL was uploaded by git cl split.
    
    R=jophba@chromium.org
    
    Bug: 377144451
    Change-Id: I59f2d9814338415445002c0fbb7b8a25f5e59caa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6066119
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391375}

--- media/audio/audio_input_stream_data_interceptor_unittest.cc
+++ media/audio/audio_input_stream_data_interceptor_unittest.cc
@@ -253,7 +253,7 @@ TEST(AudioInputStreamDataInterceptorTest,
   TestSetAutomaticGainControl(false, false);
 }
 
-TEST(AudioInputStreamDataInterceptorTest, GetAutomaticGainControlTrue) {
+TEST(AudioInputStreamDataInterceptorTest, GetAutomaticGainControl_True) {
   MockDebugRecorderFactory factory;
   StrictMock<MockStream> stream;
   AudioInputStream* interceptor = new AudioInputStreamDataInterceptor(
@@ -269,7 +269,7 @@ TEST(AudioInputStreamDataInterceptorTest, GetAutomaticGainControlTrue) {
   interceptor->Close();
 }
 
-TEST(AudioInputStreamDataInterceptorTest, GetAutomaticGainControlFalse) {
+TEST(AudioInputStreamDataInterceptorTest, GetAutomaticGainControl_False) {
   MockDebugRecorderFactory factory;
   StrictMock<MockStream> stream;
   AudioInputStream* interceptor = new AudioInputStreamDataInterceptor(
@@ -285,7 +285,7 @@ TEST(AudioInputStreamDataInterceptorTest, GetAutomaticGainControlFalse) {
   interceptor->Close();
 }
 
-TEST(AudioInputStreamDataInterceptorTest, IsMutedTrue) {
+TEST(AudioInputStreamDataInterceptorTest, IsMuted_True) {
   MockDebugRecorderFactory factory;
   StrictMock<MockStream> stream;
   AudioInputStream* interceptor = new AudioInputStreamDataInterceptor(
@@ -301,7 +301,7 @@ TEST(AudioInputStreamDataInterceptorTest, IsMutedTrue) {
   interceptor->Close();
 }
 
-TEST(AudioInputStreamDataInterceptorTest, IsMutedFalse) {
+TEST(AudioInputStreamDataInterceptorTest, IsMuted_False) {
   MockDebugRecorderFactory factory;
   StrictMock<MockStream> stream;
   AudioInputStream* interceptor = new AudioInputStreamDataInterceptor(
