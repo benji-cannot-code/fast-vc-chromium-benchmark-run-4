02686e2c49045a96c84d18679e49a5f7a61f1fd3
Awad Osman <awado@google.com>
2024-12-04 18:37:01 +0000

    [Code Health] Clean up kProjectorTranscriptChapterTitle feature flag
    This feature has been stale for quite a while and the OWNER confirmed
    that it can be removed.
    
    Bug: 362247373
    Change-Id: I97912b3aa7a6101185350af881713e601a483811
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062314
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391769}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2458,11 +2458,6 @@ BASE_FEATURE(kProjectorRedirectToPwa,
 // Controls whether projector V2 is enabled.
 BASE_FEATURE(kProjectorV2, "ProjectorV2", base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Controls whether the transcript chapter title feature is enabled.
-BASE_FEATURE(kProjectorTranscriptChapterTitle,
-             "ProjectorTranscriptChapterTitle",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // Controls whether to use USM for serverside speech recognition for projector.
 BASE_FEATURE(kProjectorUseUSMForS3,
              "ProjectorUseUSMForS3",
@@ -4366,10 +4361,6 @@ bool IsProjectorRedirectToPwaEnabled() {
 
 bool IsProjectorV2Enabled() {
   return base::FeatureList::IsEnabled(kProjectorV2);
-}
-
-bool IsProjectorTranscriptChapterTitleEnabled() {
-  return base::FeatureList::IsEnabled(kProjectorTranscriptChapterTitle);
 }
 
 bool IsProjectorUseUSMForS3Enabled() {
