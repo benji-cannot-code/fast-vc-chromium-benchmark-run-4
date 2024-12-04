12af461e4a5dd41a379805a63f9f70286bac433d
Andy Phan <andyphan@chromium.org>
2024-12-04 01:35:08 +0000

    [CodeHealth] Remove allow_unsafe_buffers in chrome/browser/ui/webui pt 2
    After https://crrev.com/c/6060912 and https://crrev.com/c/6060911, many
    of the unsafe buffers were removed. Remove the allow_unsafe_buffers
    pragma from files that no longer need it.
    
    There are still some files that have unsafe buffers. They can be handled
    in a separate CL.
    
    Change-Id: I75fbfa00e9f5e0eb2f9a0971dedd871d627924da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6064582
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391401}

--- chrome/browser/ui/webui/policy/policy_ui.cc
+++ chrome/browser/ui/webui/policy/policy_ui.cc
@@ -1,10 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 // Copyright 2012 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
