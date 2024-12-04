3c5349dd3ddbe57d2de5e842ab0cedc0e86f6cd4
Federica Germinario <fedegermi@google.com>
2024-12-04 15:52:19 +0000

    Use ProfileKeyedServiceFactoryIOS in PasswordManagerLogRouterFactory
    Convert PasswordManagerLogRouterFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215059
    Change-Id: I59175cfabe045e7b241b05ecc0113f115f283aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070426
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391684}

--- ios/chrome/browser/passwords/model/password_manager_log_router_factory.h
+++ ios/chrome/browser/passwords/model/password_manager_log_router_factory.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_PASSWORDS_MODEL_PASSWORD_MANAGER_LOG_ROUTER_FACTORY_H_
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 
@@ -18,15 +18,10 @@ class LogRouter;
 namespace ios {
 // Singleton that owns all PasswordStores and associates them with
 // profile.
-class PasswordManagerLogRouterFactory : public BrowserStateKeyedServiceFactory {
+class PasswordManagerLogRouterFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static autofill::LogRouter* GetForProfile(ProfileIOS* profile);
   static PasswordManagerLogRouterFactory* GetInstance();
-
-  PasswordManagerLogRouterFactory(const PasswordManagerLogRouterFactory&) =
-      delete;
-  PasswordManagerLogRouterFactory& operator=(
-      const PasswordManagerLogRouterFactory&) = delete;
 
  private:
   friend class base::NoDestructor<PasswordManagerLogRouterFactory>;
