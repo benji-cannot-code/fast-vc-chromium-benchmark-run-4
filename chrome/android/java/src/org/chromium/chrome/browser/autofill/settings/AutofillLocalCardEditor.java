3698b5a6fa6046319c907fa2c134c0fb84ffe743
Darwin Yang <darwinyang@google.com>
2024-12-04 19:54:07 +0000

    [autofill] Fix talkback misreading the card number in payment settings
    Insert separators to the card number from a scan before adding it to the
    card number field. This prevents an unintentional change to the text
    field causing talkback to read the card number multiple times.
    
    Fixed: 382077682,b/382074145
    Change-Id: I0b4b933cea9348c883f04c5ec77ecf6d824db976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6066347
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1391803}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalCardEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalCardEditor.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.autofill.settings;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -438,12 +439,18 @@ public class AutofillLocalCardEditor extends AutofillCreditCardEditor
     @Override
     public void onScanCompleted(
             String cardHolderName, String cardNumber, int expirationMonth, int expirationYear) {
+        // Create a new card if it doesn't already exist.
         if (mCard == null) {
             mCard =
                     PersonalDataManagerFactory.getForProfile(getProfile())
                             .getCreditCardForNumber(cardNumber);
-        } else if (!TextUtils.isEmpty(cardNumber)) {
-            mCard.setNumber(cardNumber);
+        }
+
+        if (!TextUtils.isEmpty(cardNumber)) {
+            // Reformat the card number for the text field.
+            SpannableStringBuilder cardNumberAsEditable = new SpannableStringBuilder(cardNumber);
+            CreditCardNumberFormattingTextWatcher.insertSeparators(cardNumberAsEditable);
+            mCard.setNumber(cardNumberAsEditable.toString());
         }
 
         if (!TextUtils.isEmpty(cardHolderName)) {
@@ -451,7 +458,7 @@ public class AutofillLocalCardEditor extends AutofillCreditCardEditor
         }
 
         if (expirationMonth != 0) {
-            // Zero pad the month to 2 digits
+            // Zero pad the month to 2 digits.
             mCard.setMonth(String.format(Locale.getDefault(), "%02d", expirationMonth));
         }
 
