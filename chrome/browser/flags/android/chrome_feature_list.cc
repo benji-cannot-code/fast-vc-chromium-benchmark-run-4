9c7c615764b88d1b24442210de7777f9af9661bc
Joel Tan-Aristy <jtanaristy@google.com>
2024-12-04 01:37:49 +0000

    [AutomotiveToolbar] Enable AutomotiveFullscreenToolbarImprovements by default
    Bug: 364691879
    Change-Id: Iee136789c7d81793e7e135a29e97e6f2cde21e4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068927
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391403}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -529,7 +529,7 @@ BASE_FEATURE(kAsyncNotificationManager,
 
 BASE_FEATURE(kAutomotiveFullscreenToolbarImprovements,
              "AutomotiveFullscreenToolbarImprovements",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAuxiliarySearchDonation,
              "AuxiliarySearchDonation",
