9b0a74ac34ce553ffaef9bc7caa215f79d2a0c44
Andrew Grieve <agrieve@chromium.org>
2024-12-04 22:43:50 +0000

    Add parameters to EnsuresNonNull, EnsuresNonNullIf, RequiresNonNull
    Missing these as an oversight.
    
    Bug: 40657579
    Change-Id: Ib7eb72f8818e64d51f17d582e51ba438dc0c9357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068631
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391901}

--- build/android/java/src/org/chromium/build/annotations/RequiresNonNull.java
+++ build/android/java/src/org/chromium/build/annotations/RequiresNonNull.java
@@ -17,4 +17,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.CLASS)
-public @interface RequiresNonNull {}
+public @interface RequiresNonNull {
+    /** List of fields that must be non-null before the method. */
+    String[] value();
+}
