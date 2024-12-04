af29231c591425a15b1a831884fdb685791a0476
Baran Erfani <baranerf@google.com>
2024-12-04 23:13:34 +0000

    BorderView class skeleton
    BorderView class will be responsible for drawing, animating, and styling
    the border. This CL only defines a skeleton for this class to provide a
    starting point for further implementation.
    
    Bug: 381424324
    Change-Id: Id9efd6ea6822ae39d48da41f8ea2ef048ad0fa58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6069952
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391923}

---
+++ chrome/browser/ui/views/glic/border/border_view.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/glic/border/border_view.h"
+
+namespace glic {
+
+BorderView::BorderView() = default;
+
+BorderView::~BorderView() = default;
+
+void BorderView::OnPaint(gfx::Canvas* canvas) {
+  // Paint onto `canvas`.
+  // Call `SchedulePaint()` if the animation hasn't finished.
+}
+
+void BorderView::OnChildViewAdded(views::View* observed_view,
+                                  views::View* child) {
+  // Use this API to make sure our border view is always the z-top-most of the
+  // contents_web_view_ of the browser view.
+}
+
+void BorderView::OnAnimationStep(base::TimeTicks timestamp) {
+  // Update the border style based on`timestamp` and the motion curve(s).
+}
+
+void BorderView::OnCompositingShuttingDown(ui::Compositor* compositor) {}
+
+// TODO(crbug.com/381424645): Implementation.
+void BorderView::StartAnimation() {}
+
+void BorderView::CancelAnimation() {}
+
+}  // namespace glic
