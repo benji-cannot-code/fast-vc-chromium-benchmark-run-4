1df506b518d09e87af5cce1281f1b70aced7dd3a
Tsuyoshi Horo <horo@chromium.org>
2024-12-04 09:55:46 +0000

    [built-in-ai] Implement AITranslatorCapabilities
    This CL implements AITranslatorCapabilities with `available` attribute
    and `languagePairAvailable()` method.
    
    AITranslatorCapabilities.languagePairAvailable() method is a synchronous
    method which returns the capabilities of a language pair. But
    unfortunately, the logic of calculating the capabilities of each
    language pair is very complicated because of the
    TranslationAPIAcceptLanguagesCheck and
    TranslationAPILimitLanguagePackCount features which were introduced
    for protecting the user's privacy during the Origin Trial experiment.
    
    It is possible to calculate the capabilities of each language pair
    synchronously in Blink, if we send the state of these two features, the
    user's accept languages, the list of supported language packs and the
    list of installed language packs to Blink, and implement the
    calculation logic in Blink duplicating the logic in the browser
    process. But this makes the mojo IPC and Blink code too specialized to
    Chrome's translation library. This makes it hard for other embedder to
    implement the Translation API. So we decided to implement a more generic
    solution.
    
    The solution is to introduce language categories and an availability
    matrix. The all supported languages are divided into several categories
    based on the user's accept languages and the installed language packs,
    and whether the language is popular or not. And then we create an
    availability matrix which represents the capabilities of each language
    pair based on the language categories.
    
    The availability matrix is a 2D array of TranslationAvailability which
    represents the capabilities of each language pair based on the language
    categories.
    
    The browser process sends the language categories and the availability
    matrix to Blink. So the languagePairAvailable() method in Blink only
    needs to return the corresponding element in the availability matrix
    according to the given source and target language categories.
    
    
    Note: This CL was originally created by yuzus@. horo@ took over this
    work. This CL's PS1 is the clone of the original CL
    https://crrev.com/c/5963884/32 .
    
    Bug: 378992891
    Change-Id: I50e7434b6cfac4da4cc0040d81188ebf98f01ce3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038986
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391552}

--- chrome/browser/policy/test/translator_api_policy_browsertest.cc
+++ chrome/browser/policy/test/translator_api_policy_browsertest.cc
@@ -19,7 +19,9 @@ using on_device_translation::LanguagePackKey;
 using on_device_translation::MockComponentManager;
 using on_device_translation::TestCanTranslate;
 using on_device_translation::TestCreateTranslator;
+using on_device_translation::TestLanguagePairAvailable;
 using on_device_translation::TestSimpleTranslationWorks;
+using on_device_translation::TestTranslatorCapabilitiesAvailable;
 
 namespace policy {
 
@@ -84,6 +86,8 @@ IN_PROC_BROWSER_TEST_F(TranslatorAPIPolicyTest, DefaultAllowed) {
   NavigateToEmptyPage();
   TestSimpleTranslationWorks(browser(), "en", "ja");
   TestCanTranslate(browser(), "en", "ja", "readily");
+  TestLanguagePairAvailable(browser(), "en", "ja", "readily");
+  TestTranslatorCapabilitiesAvailable(browser(), "readily");
 }
 
 // Test that set the policy to false will disallow the API.
@@ -94,6 +98,8 @@ IN_PROC_BROWSER_TEST_F(TranslatorAPIPolicyTest, Disallow) {
                        "NotSupportedError: Unable to create translator for the "
                        "given source and target language.");
   TestCanTranslate(browser(), "en", "ja", "no");
+  TestLanguagePairAvailable(browser(), "en", "ja", "no");
+  TestTranslatorCapabilitiesAvailable(browser(), "no");
 }
 
 // Test that set the policy to true will allow the API.
@@ -102,6 +108,8 @@ IN_PROC_BROWSER_TEST_F(TranslatorAPIPolicyTest, Allow) {
   SetTranslatorAPIAllowedPolicy(true);
   TestSimpleTranslationWorks(browser(), "en", "ja");
   TestCanTranslate(browser(), "en", "ja", "readily");
+  TestLanguagePairAvailable(browser(), "en", "ja", "readily");
+  TestTranslatorCapabilitiesAvailable(browser(), "readily");
 }
 
 // Test that the policy can be dynamically refreshed.
