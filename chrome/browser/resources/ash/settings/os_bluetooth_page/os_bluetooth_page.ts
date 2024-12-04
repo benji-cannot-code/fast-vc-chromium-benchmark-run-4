f916c92dc95bf7a65f878f22c98bf378f001ca7d
Wes Okuhara <wesokuhara@google.com>
2024-12-04 23:35:14 +0000

    CrOS Settings: Cleanup revamp code in bluetooth page
    Bug: b:370837462
    Test: browser_test
    Change-Id: I6063d2c3251cf01567c8b6de52ab84d7e0af3adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6066790
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391943}

--- chrome/browser/resources/ash/settings/os_bluetooth_page/os_bluetooth_page.ts
+++ chrome/browser/resources/ash/settings/os_bluetooth_page/os_bluetooth_page.ts
@@ -27,7 +27,6 @@ import type {BluetoothSystemProperties} from 'chrome://resources/mojo/chromeos/a
 import {BluetoothSystemState, SystemPropertiesObserverReceiver} from 'chrome://resources/mojo/chromeos/ash/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom-webui.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {isRevampWayfindingEnabled} from '../common/load_time_booleans.js';
 import {Section} from '../mojom-webui/routes.mojom-webui.js';
 
 import type {OsBluetoothDevicesSubpageBrowserProxy} from './os_bluetooth_devices_subpage_browser_proxy.js';
@@ -66,14 +65,6 @@ export class SettingsBluetoothPageElement extends
        * visibility in the subpage.
        */
       showSavedDevicesLoadingIndicators_: Boolean,
-
-      isRevampWayfindingEnabled_: {
-        type: Boolean,
-        value() {
-          return isRevampWayfindingEnabled();
-        },
-        readOnly: true,
-      },
     };
   }
 
@@ -83,7 +74,6 @@ export class SettingsBluetoothPageElement extends
   private shouldShowPairingDialog_: boolean;
   private systemProperties_: BluetoothSystemProperties;
   private systemPropertiesObserverReceiver_: SystemPropertiesObserverReceiver;
-  private isRevampWayfindingEnabled_: boolean;
 
   constructor() {
     super();
