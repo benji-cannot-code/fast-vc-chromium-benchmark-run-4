334bcc3996ab940c1102834e80d56e1e93456496
Sharon Yang <yangsharon@chromium.org>
2024-12-04 22:56:37 +0000

    Improve tracing in SiteInstanceImpl::LockProcessIfNeeded
    It seems this tracing event was moved over when the code was somewhere
    else. Rename the event and fields to more accurately represent what it's
    currently tracing.
    
    Test: Tracing only change
    Bug: 382303496
    Change-Id: I0d4a5c4e26290585d2ce587009799bd1b4c538a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6071449
    Auto-Submit: Sharon Yang <yangsharon@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391911}

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+using perfetto::protos::pbzero::ChromeTrackEvent;
+
 namespace {
 
 // Constant used to mark two call sites that must always agree on whether
@@ -1403,6 +1405,8 @@ bool SiteInstanceImpl::HasEffectiveURL(BrowserContext* browser_context,
 }
 
 void SiteInstanceImpl::LockProcessIfNeeded() {
+  TRACE_EVENT("navigation", "SiteInstanceImpl::LockProcessIfNeeded",
+              ChromeTrackEvent::kSiteInstance, this);
   RenderProcessHost* process = site_instance_group_->process();
   ChildProcessSecurityPolicyImpl* policy =
       ChildProcessSecurityPolicyImpl::GetInstance();
@@ -1443,8 +1447,10 @@ void SiteInstanceImpl::LockProcessIfNeeded() {
       // strong protection. If only some sites are isolated, we need
       // additional logic to prevent the non-isolated sites from requesting
       // resources for isolated sites. https://crbug.com/509125
-      TRACE_EVENT2("navigation", "RenderProcessHost::SetProcessLock", "site id",
-                   id_.value(), "lock", lock_to_set.ToString());
+      TRACE_EVENT_INSTANT(
+          "navigation",
+          "SiteInstanceImpl::LockProcessIfNeeded_set_process_lock", "lock",
+          lock_to_set.ToString());
       process->SetProcessLock(GetIsolationContext(), lock_to_set);
     } else if (process_lock != lock_to_set) {
       // We should never attempt to reassign a different origin lock to a
