e3f0c4344d1205d077bbcf81ba345529b06cb855
Sylvain Defresne <sdefresne@chromium.org>
2024-12-04 11:19:38 +0000

    [ios] Convert IOSChromeSafetyCheckManagerFactory to PKSFIOS
    Convert IOSChromeSafetyCheckManagerFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217876
    Change-Id: I5ff7c1067733eb7f1992a0ae82cc9d1cd39bec22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070387
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391587}

--- ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_factory.h
+++ ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_factory.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class IOSChromeSafetyCheckManager;
 class KeyedService;
@@ -18,16 +18,11 @@ class ProfileIOS;
 // Singleton that owns all IOSChromeSafetyCheckManager(s) and associates them
 // with profiles.
 class IOSChromeSafetyCheckManagerFactory
-    : public BrowserStateKeyedServiceFactory {
+    : public ProfileKeyedServiceFactoryIOS {
  public:
   static IOSChromeSafetyCheckManager* GetForProfile(ProfileIOS* profile);
   static IOSChromeSafetyCheckManagerFactory* GetInstance();
   static TestingFactory GetDefaultFactory();
-
-  IOSChromeSafetyCheckManagerFactory(
-      const IOSChromeSafetyCheckManagerFactory&) = delete;
-  IOSChromeSafetyCheckManagerFactory& operator=(
-      const IOSChromeSafetyCheckManagerFactory&) = delete;
 
  private:
   friend class base::NoDestructor<IOSChromeSafetyCheckManagerFactory>;
@@ -37,8 +32,6 @@ class IOSChromeSafetyCheckManagerFactory
 
   // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
