a9d79fa25b465e0c5719aa78580d7eb02359b278
Christian Xu <christianxu@google.com>
2024-12-04 14:47:44 +0000

    [iOS][LO] Destroy lens UI on stop
    Bug: None
    Change-Id: I1a4ae83701b149c99fb9cbce1ac79611b6287f70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070255
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391638}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -262,6 +262,7 @@ const int kExpectedExitAnimationCount = 2;
   if (Browser* browser = self.browser) {
     [browser->GetCommandDispatcher() stopDispatchingToTarget:self];
   }
+  [self destroyLensUI:NO reason:lens::LensOverlayDismissalSource::kTabClosed];
 
   [super stop];
 }
