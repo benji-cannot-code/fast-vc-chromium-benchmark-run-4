ecb12d271f59df6c84e25eb3ff189c862b1a05ef
Federica Germinario <fedegermi@google.com>
2024-12-04 15:41:30 +0000

    Use ProfileKeyedServiceFactoryIOS in BookmarkModelFactory
    Convert BookmarkModelFactory to use ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215619
    Change-Id: I75df8d79358f215a49c445e8a0620e2c54a92307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063327
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391677}

--- ios/chrome/browser/bookmarks/model/bookmarks_utils.cc
+++ ios/chrome/browser/bookmarks/model/bookmarks_utils.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/prefs/pref_service.h"
-#include "ios/chrome/browser/bookmarks/model/bookmark_model_factory.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_storage_type.h"
 #include "ios/chrome/browser/shared/model/prefs/pref_names.h"
 
