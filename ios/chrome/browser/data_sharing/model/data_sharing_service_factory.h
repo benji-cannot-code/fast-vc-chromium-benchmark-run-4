368761229c2ea06ec1afbbe5768b87a9d5244e6b
Federica Germinario <fedegermi@google.com>
2024-12-04 15:23:58 +0000

    Use ProfileKeyedServiceFactoryIOS in DataSharingServiceFactory
    Convert DataSharingServiceFactory to use ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363213829
    Change-Id: Id634cf4c009437e8e1079ce90864a2fb10a0b5eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063585
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391663}

--- ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h
+++ ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_DATA_SHARING_MODEL_DATA_SHARING_SERVICE_FACTORY_H_
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 
@@ -16,14 +16,11 @@ namespace data_sharing {
 class DataSharingService;
 
 // Factory for DataSharingService.
-class DataSharingServiceFactory : public BrowserStateKeyedServiceFactory {
+class DataSharingServiceFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static DataSharingService* GetForProfile(ProfileIOS* profile);
 
   static DataSharingServiceFactory* GetInstance();
-
-  DataSharingServiceFactory(DataSharingServiceFactory&) = delete;
-  DataSharingServiceFactory& operator=(DataSharingServiceFactory&) = delete;
 
   // Returns the default factory used to build DataSharingService. Can be
   // registered with SetTestingFactory to use real instances during testing.
@@ -37,8 +34,6 @@ class DataSharingServiceFactory : public BrowserStateKeyedServiceFactory {
 
   // BrowserStateKeyedServiceFactory:
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
