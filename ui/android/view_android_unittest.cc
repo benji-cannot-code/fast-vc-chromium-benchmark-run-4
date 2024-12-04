2c9b41b127cff14b783f597525915a7ca690c887
Theo Cristea <theocristea@google.com>
2024-12-04 18:23:22 +0000

    Add `ViewAndroidObserver::OnDelegateSet`
    `ViewAndroidDelegate` can be null if accessed too early. This
    observer method will allow other objects to know when they can start
    accessing the `ViewAndroidDelegate`.
    
    More details on the bug.
    
    Bug: 378837881
    Change-Id: I793a2fb9fa5aac9854318d166f5eb3196f5f2e31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6017943
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1391762}

--- ui/android/view_android_unittest.cc
+++ ui/android/view_android_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/android/event_forwarder.h"
+#include "ui/android/test_view_android_delegate.h"
 #include "ui/android/view_android.h"
 #include "ui/android/view_android_observer.h"
 #include "ui/android/window_android.h"
@@ -279,7 +280,7 @@ TEST(ViewAndroidTest, ChecksMultipleEventForwarders) {
 
 class Observer : public ViewAndroidObserver {
  public:
-  Observer() : attached_(false), destroyed_(false) {}
+  Observer() = default;
 
   void OnAttachedToWindow() override { attached_ = true; }
 
@@ -287,8 +288,11 @@ class Observer : public ViewAndroidObserver {
 
   void OnViewAndroidDestroyed() override { destroyed_ = true; }
 
-  bool attached_;
-  bool destroyed_;
+  void OnDelegateSet() override { delegate_set_ = true; }
+
+  bool attached_ = false;
+  bool destroyed_ = false;
+  bool delegate_set_ = false;
 };
 
 TEST(ViewAndroidTest, Observer) {
@@ -332,6 +336,12 @@ TEST(ViewAndroidTest, Observer) {
     // View, upon addition to a tree in the attached state, should be notified.
     top.AddChild(&bottom);
     EXPECT_TRUE(bottom_observer.attached_);
+
+    // Observer is notified about the new delegate.
+    TestViewAndroidDelegate view_android_delegate;
+    EXPECT_FALSE(top_observer.delegate_set_);
+    view_android_delegate.SetupTestDelegate(&top);
+    EXPECT_TRUE(top_observer.delegate_set_);
 
     // Views in a tree all get notified of 'detached' event.
     top.RemoveFromParent();
