727377c8374fe3fe18fbe8edc243bc2c9a3de9c1
Alison Gale <agale@chromium.org>
2024-12-04 17:32:48 +0000

    Reduce flakiness by using page with auto-focusing input
    Attempt to reduce the flakiness by changing which page this test uses
    to one that has an auto focusing input.
    
    Updated CheckTabIsDiscarded to include a descriptive string so
    failures are easier to identify.
    
    Change-Id: Iadf0d38c1b53bc022ee208c2e40694a5a7e5c309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6064163
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391738}

--- chrome/browser/ui/performance_controls/test_support/memory_saver_interactive_test_mixin.h
+++ chrome/browser/ui/performance_controls/test_support/memory_saver_interactive_test_mixin.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <concepts>
 
+#include "base/strings/strcat.h"
 #include "base/test/bind.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/performance_controls/test_support/memory_saver_browser_test_mixin.h"
@@ -34,10 +35,12 @@ class MemorySaverInteractiveTestMixin : public MemorySaverBrowserTestMixin<T> {
       const MemorySaverInteractiveTestMixin&) = delete;
 
   auto CheckTabIsDiscarded(int tab_index, bool is_discarded) {
-    return T::Check([=, this]() {
-      return MemorySaverBrowserTestMixin<T>::IsTabDiscarded(tab_index) ==
-             is_discarded;
-    });
+    return T::Check(
+        [=, this]() {
+          return MemorySaverBrowserTestMixin<T>::IsTabDiscarded(tab_index) ==
+                 is_discarded;
+        },
+        base::StrCat({"Tab ", is_discarded ? "is" : "isn't", " discarded."}));
   }
 
   auto TryDiscardTab(int tab_index) {
