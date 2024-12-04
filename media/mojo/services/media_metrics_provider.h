ccb80876f79b9a5403a67fa491a27ec3bdde6473
Ted Meyer <tmathmeyer@chromium.org>
2024-12-04 21:28:15 +0000

    Add DemuxerType to WebMediaPlayerState UKM
    This will allow separation of playbacks based on demuxer, so that it is
    easier to see the differences between ManifestDemuxer (HLS) vs others,
    or ManifestDemuxer vs MediaUrlDemuxer.
    
    UKM privacy doc: go/DemuxerType-UKM-PD
    
    Bug: 40057824
    Change-Id: Ie8521f85cbdaa85c2864180eb84231d484eeb5d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6052619
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391853}

--- media/mojo/services/media_metrics_provider.h
+++ media/mojo/services/media_metrics_provider.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "media/base/container_names.h"
+#include "media/base/demuxer.h"
 #include "media/base/pipeline_status.h"
 #include "media/base/renderer_factory_selector.h"
 #include "media/base/timestamp_constants.h"
@@ -115,6 +116,7 @@ class MEDIA_MOJO_EXPORT MediaMetricsProvider
   void SetContainerName(
       container_names::MediaContainerName container_name) override;
   void SetRendererType(RendererType renderer_type) override;
+  void SetDemuxerType(DemuxerType renderer_type) override;
   void SetKeySystem(const std::string& key_system) override;
   void SetHasWaitingForKey() override;
   void SetIsHardwareSecure() override;
@@ -166,6 +168,7 @@ class MEDIA_MOJO_EXPORT MediaMetricsProvider
   std::optional<MediaInfo> media_info_;
 
   RendererType renderer_type_ = RendererType::kRendererImpl;
+  DemuxerType demuxer_type_ = DemuxerType::kUnknownDemuxer;
   std::string key_system_;
   bool has_waiting_for_key_ = false;
   bool is_hardware_secure_ = false;
