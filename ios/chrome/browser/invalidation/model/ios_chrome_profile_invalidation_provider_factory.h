243f3771ba871c16d18a54442dba744f5843f82a
Sylvain Defresne <sdefresne@chromium.org>
2024-12-04 13:31:49 +0000

    [ios] Convert IOSChromeProfileInvalidationProviderFactory to PKSFIOS
    Convert IOSChromeProfileInvalidationProviderFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217875
    Change-Id: If8535d49e74d478277f564c1150302d31cc35f4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070167
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391616}

--- ios/chrome/browser/invalidation/model/ios_chrome_profile_invalidation_provider_factory.h
+++ ios/chrome/browser/invalidation/model/ios_chrome_profile_invalidation_provider_factory.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 
@@ -20,16 +20,11 @@ class ProfileInvalidationProvider;
 // A BrowserContextKeyedServiceFactory to construct InvalidationServices wrapped
 // in ProfileInvalidationProviders.
 class IOSChromeProfileInvalidationProviderFactory
-    : public BrowserStateKeyedServiceFactory {
+    : public ProfileKeyedServiceFactoryIOS {
  public:
   static invalidation::ProfileInvalidationProvider* GetForProfile(
       ProfileIOS* profile);
   static IOSChromeProfileInvalidationProviderFactory* GetInstance();
-
-  IOSChromeProfileInvalidationProviderFactory(
-      const IOSChromeProfileInvalidationProviderFactory&) = delete;
-  IOSChromeProfileInvalidationProviderFactory& operator=(
-      const IOSChromeProfileInvalidationProviderFactory&) = delete;
 
  private:
   friend class base::NoDestructor<IOSChromeProfileInvalidationProviderFactory>;
--- ios/chrome/browser/invalidation/model/ios_chrome_profile_invalidation_provider_factory.mm
+++ ios/chrome/browser/invalidation/model/ios_chrome_profile_invalidation_provider_factory.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/gcm_driver/instance_id/instance_id_profile_service.h"
 #import "components/invalidation/impl/profile_identity_provider.h"
 #import "components/invalidation/profile_invalidation_provider.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/pref_registry/pref_registry_syncable.h"
 #import "components/prefs/pref_registry.h"
 #import "ios/chrome/browser/gcm/model/instance_id/ios_chrome_instance_id_profile_service_factory.h"
@@ -30,8 +29,9 @@ using invalidation::ProfileInvalidationProvider;
 invalidation::ProfileInvalidationProvider*
 IOSChromeProfileInvalidationProviderFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<ProfileInvalidationProvider*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()
+      ->GetServiceForProfileAs<invalidation::ProfileInvalidationProvider>(
+          profile, /*create=*/true);
 }
 
 // static
@@ -44,9 +44,7 @@ IOSChromeProfileInvalidationProviderFactory::GetInstance() {
 
 IOSChromeProfileInvalidationProviderFactory::
     IOSChromeProfileInvalidationProviderFactory()
-    : BrowserStateKeyedServiceFactory(
-          "InvalidationService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("InvalidationService") {
   DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(IOSChromeGCMProfileServiceFactory::GetInstance());
   DependsOn(IOSChromeInstanceIDProfileServiceFactory::GetInstance());
