cdeb811ee519f4ffdd71a2fd85234587ab22227b
Tomasz Wiszkowski <ender@google.com>
2024-12-04 23:09:59 +0000

    Disable UrlBar NTP⬌Tab expansion animation when switching tabs.
    This logic exposes a bug in the current animation implementation,
    where the Microphone and Camera icons linger briefly as the UrlBar
    contracts when switching from the NTP and loaded tab using the Tab
    group accessory.
    
    This change partially reverts https://crrev.com/c/6023931, which
    introduced the logic being removed.
    
    Change-Id: Ie1bc5790db5215cd0498c0c174691b7d95ae5265
    Fixed: b:381339470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6071448
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391919}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2202,7 +2202,7 @@ public class ToolbarPhone extends ToolbarLayout
                     }
                 });
         mUrlFocusLayoutAnimator.start();
-        if (mLocationBar.shouldShortCircuitFocusAnimation(hasFocus)) {
+        if (!hasFocus) {
             TraceEvent.instant("ToolbarPhone.ShortCircuitUnfocusAnimation");
             mUrlFocusLayoutAnimator.end();
         }
