8d38995e4b0788dc6429f78b9b478991ad57f458
Viktor Semeniuk <vsemeniuk@google.com>
2024-12-05 16:39:53 +0000

    Enable UseAsyncOsCryptInLoginDatabase by default
    Bug: 345691490
    Change-Id: I62dd1e42b80f03e6867a295ebc514b8faa415fa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075065
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392322}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -172,7 +172,7 @@ BASE_FEATURE(kUsernameFirstFlowWithIntermediateValuesVoting,
 
 BASE_FEATURE(kUseAsyncOsCryptInLoginDatabase,
              "UseAsyncOsCryptInLoginDatabase",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseNewEncryptionMethod,
              "UseNewEncryptionMethod",
