b4c9b1199fd35fe37f2e71dec4d717bb99b8a358
Marijn Kruisselbrink <mek@chromium.org>
2024-12-05 18:45:35 +0000

    Reland "[AppShims] Fix app shim creation on non-stable channel with ad-hoc signing."
    This is a reland of commit d972a88a18664b4b33845e3f71a01f7124d5305b,
    with a fix for the fuzzer compile failure.
    
    Original change's description:
    > [AppShims] Fix app shim creation on non-stable channel with ad-hoc signing.
    >
    > When adhoc signed app shims are enabled, web_app_shortcut_copier
    > is used to copy the generated app shim to its final location.
    > The way web_app_shortcut_copier tries to setup a mojo connection
    > to the app that launched it relies on base::apple::BaseBundleID to
    > return the same value in the browser and helper process. However
    > unless a process explicitly sets this value, it will return the
    > bundle ID of stable channel. As such breaking creation of app
    > shims from anything other than chromium builds or chrome stable.
    >
    > Fix this by getting the bundle ID from chrome::OuterAppBundle in
    > web_app_shortcut_copier; this should return the correct bundle
    > as long as we override the framework bundle path correctly (which
    > we do).
    >
    > Bug: 382015346
    > Change-Id: I7323261b48070ed1dd7c5169863b409f00f7e60b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6067566
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    > Reviewed-by: Mark Rowe <markrowe@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1391280}
    
    Bug: 382015346
    Change-Id: I63f298386d6a7b69088ff32bfe31a15636fbb564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068586
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392416}

--- base/apple/mach_port_rendezvous.h
+++ base/apple/mach_port_rendezvous.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/ios_buildflags.h"
 
 #if BUILDFLAG(IS_MAC)
-#include "base/apple/scoped_dispatch_object.h"
 #include "base/environment.h"
 #include "base/mac/process_requirement.h"
 #endif
@@ -207,6 +206,8 @@ class BASE_EXPORT MachPortRendezvousServerMac final
   // registered.
   Lock& GetLock() LOCK_RETURNED(lock_) { return lock_; }
 
+  void ClearClientDataForTesting() EXCLUSIVE_LOCKS_REQUIRED(GetLock());
+
  protected:
   // Returns the registered collection of ports for the specified `audit_token`.
   // `std::nullopt` indicates that the client is not authorized to retrieve the
@@ -226,18 +227,7 @@ class BASE_EXPORT MachPortRendezvousServerMac final
   MachPortRendezvousServerMac();
   ~MachPortRendezvousServerMac() override;
 
-  struct ClientData {
-    ClientData();
-    ClientData(ClientData&&);
-    ~ClientData();
-
-    // A DISPATCH_SOURCE_TYPE_PROC / DISPATCH_PROC_EXIT dispatch source. When
-    // the source is triggered, it calls OnClientExited().
-    apple::ScopedDispatchObject<dispatch_source_t> exit_watcher;
-
-    MachPortsForRendezvous ports;
-    std::optional<mac::ProcessRequirement> requirement;
-  };
+  struct ClientData;
 
   // Returns the `ClientData` for `pid`, creating it if necessary.
   // It will be cleaned up automatically when `pid` exits.
