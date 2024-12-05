c0e03124035145e1c09d30c54865ab76d04f6f1f
Marlon Facey <mfacey@chromium.org>
2024-12-05 16:23:18 +0000

    [Code Health] Remove omnibox http header feature
    This feature is no longer in use.
    
    Bug: 356623137, 40689845
    Change-Id: I67cadfb19165b61dca2a9bfac9ad7c1bdeadb39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038794
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392311}

--- components/variations/net/variations_http_headers.cc
+++ components/variations/net/variations_http_headers.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
@@ -15,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "components/google/core/common/google_util.h"
-#include "components/variations/net/omnibox_http_headers.h"
 #include "components/variations/variations_features.h"
 #include "components/variations/variations_ids_provider.h"
 #include "net/base/isolation_info.h"
@@ -251,8 +249,6 @@ class VariationsHeaderHelper {
   VariationsHeaderHelper& operator=(const VariationsHeaderHelper&) = delete;
 
   bool AppendHeaderIfNeeded(const GURL& url, InIncognito incognito) {
-    AppendOmniboxOnDeviceSuggestionsHeaderIfNeeded(url, resource_request_);
-
     // Note the criteria for attaching client experiment headers:
     // 1. We only transmit to Google owned domains which can evaluate
     // experiments.
@@ -399,11 +395,6 @@ bool ShouldAppendVariationsHeaderForTesting(
 void UpdateCorsExemptHeaderForVariations(
     network::mojom::NetworkContextParams* params) {
   params->cors_exempt_header_list.push_back(kClientDataHeader);
-
-  if (base::FeatureList::IsEnabled(kReportOmniboxOnDeviceSuggestionsHeader)) {
-    params->cors_exempt_header_list.push_back(
-        kOmniboxOnDeviceSuggestionsHeader);
-  }
 }
 
 }  // namespace variations
--- content/renderer/variations_render_thread_observer.cc
+++ content/renderer/variations_render_thread_observer.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
-#include "components/variations/net/omnibox_url_loader_throttle.h"
 #include "components/variations/net/variations_url_loader_throttle.h"
 #include "content/public/renderer/render_thread.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_registry.h"
@@ -56,7 +55,6 @@ VariationsRenderThreadObserver::~VariationsRenderThreadObserver() = default;
 void VariationsRenderThreadObserver::AppendThrottleIfNeeded(
     const url::Origin& top_frame_origin,
     std::vector<std::unique_ptr<blink::URLLoaderThrottle>>* throttles) {
-  variations::OmniboxURLLoaderThrottle::AppendThrottleIfNeeded(throttles);
 
   variations::mojom::VariationsHeadersPtr variations_headers =
       GetVariationsData()->GetVariationsHeaders();
