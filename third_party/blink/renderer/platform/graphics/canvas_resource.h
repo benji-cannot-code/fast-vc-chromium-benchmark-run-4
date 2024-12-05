1b978bf10593753e80ff96ab849b81a25344eab4
Colin Blundell <blundell@chromium.org>
2024-12-05 15:34:39 +0000

    [Blink] Have ExternalCanvasResource take in `is_overlay_candidate`
    Next step to removnig ExternalCanvasResource needing to take in the
    TransferableResource. A followup will push this out to
    ExternalCanvasResource::Create().
    
    Bug: 352263194
    Change-Id: I85eb024cc0477bff792f676f82409caeddb555eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072807
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392291}

--- third_party/blink/renderer/platform/graphics/canvas_resource.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource.h
@@ -469,9 +469,7 @@ class PLATFORM_EXPORT ExternalCanvasResource final : public CanvasResource {
   }
 
  private:
-  bool IsOverlayCandidate() const final {
-    return transferable_resource_.is_overlay_candidate;
-  }
+  bool IsOverlayCandidate() const final { return is_overlay_candidate_; }
   bool UsesAcceleratedRaster() const final { return true; }
   const gpu::SyncToken GetSyncTokenWithOptionalVerification(
       bool needs_verified_token) override;
@@ -483,6 +481,7 @@ class PLATFORM_EXPORT ExternalCanvasResource final : public CanvasResource {
 
   ExternalCanvasResource(scoped_refptr<gpu::ClientSharedImage> client_si,
                          const viz::TransferableResource& transferable_resource,
+                         bool is_overlay_candidate,
                          viz::ReleaseCallback out_callback,
                          base::WeakPtr<WebGraphicsContext3DProviderWrapper>,
                          base::WeakPtr<CanvasResourceProvider>);
@@ -491,6 +490,7 @@ class PLATFORM_EXPORT ExternalCanvasResource final : public CanvasResource {
   const base::WeakPtr<WebGraphicsContext3DProviderWrapper>
       context_provider_wrapper_;
   viz::TransferableResource transferable_resource_;
+  bool is_overlay_candidate_ = false;
   viz::ReleaseCallback release_callback_;
   bool is_origin_clean_ = true;
   bool resource_is_lost_ = false;
