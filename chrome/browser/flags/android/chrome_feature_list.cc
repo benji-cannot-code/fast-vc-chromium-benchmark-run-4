ddc6dd8729ff39d91153e06edbde35c6cefa234a
Aldo Culquicondor <acondor@chromium.org>
2024-12-05 20:13:53 +0000

    Screenshot native pages using hardware acceleration
    For now, limited to asynchronous requests, which are the transitions from a content page to a native page.
    This is based on the code removed in crrev.com/c/5116547. Several simplifications were possible as we only
    need to snapshot once. The ImageReader is recreated every time, to make sure we always process the image from the latest draw request.
    
    While rendering on the GPU is faster than on the CPU, there is a cost transferring the bitmap. However, the synchronous part (capturing the draw instructions) is up to 30x faster than CPU rendering, as measured on a Pixel tablet. The drawing and transferring is done asynchronously.
    
    Low-Coverage-Reason: HARD_TO_TEST Interacts with hardware rendering. Covered by render tests.
    Bug: 376519305
    Change-Id: I849d257d7aad31e185c6514064d46e57d8c492b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032814
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392486}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -281,6 +281,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kMayLaunchUrlUsesSeparateStoragePartition,
     &kMostVisitedTilesReselect,
     &kMultiInstanceApplicationStatusCleanup,
+    &kNativePageTransitionHardwareCapture,
     &kNavBarColorMatchesTabBackground,
     &kNewTabSearchEngineUrlAndroid,
     &kNewTabPageAndroidTriggerForPrerender2,
@@ -831,6 +832,10 @@ BASE_FEATURE(kMostVisitedTilesReselect,
 
 BASE_FEATURE(kMultiInstanceApplicationStatusCleanup,
              "MultiInstanceApplicationStatusCleanup",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kNativePageTransitionHardwareCapture,
+             "NativePageTransitionHardwareCapture",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNavBarColorMatchesTabBackground,
