23f398a4c7ae293031c6998d777622d82b356f3a
Hira Mahmood <hiramahmood@google.com>
2024-12-05 19:36:16 +0000

    [iOS] Fix layout constraint conflicts in FeedHeaderViewController
    This CL fixes some conflicting constraints that are flooding the debug
    console.
    
    More specifically, the conflicting constraints are:
     1. segmentedControl.centerX = container.centerX and
     2. segmentedControl.trailingAnchor <= container.leadingAnchor
    
    The second is not needed anymore, so this CL removes it.
    
    Bug: 381032249
    Change-Id: I209c4a68d3dd86a69c8e0820146a218b69a59974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072789
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392453}

--- ios/chrome/browser/ntp/ui_bundled/feed_header_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/feed_header_view_controller.mm
@@ -596,12 +596,7 @@ NSInteger kFeedSymbolPointSize = 17;
                        constant:kButtonHorizontalMargin],
   ]];
 
-  if (IsHomeCustomizationEnabled()) {
-    [self.feedHeaderConstraints addObjectsFromArray:@[
-      [self.segmentedControl.trailingAnchor
-          constraintLessThanOrEqualToAnchor:self.container.leadingAnchor],
-    ]];
-  } else {
+  if (!IsHomeCustomizationEnabled()) {
     [self.feedHeaderConstraints addObjectsFromArray:@[
       [self.segmentedControl.trailingAnchor
           constraintLessThanOrEqualToAnchor:self.managementButton.leadingAnchor
