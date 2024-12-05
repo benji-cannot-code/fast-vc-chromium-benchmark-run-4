b0c71d4c3401f747ee53778439351e935256ce9f
Tanmoy Mollik <triploblastic@google.com>
2024-12-05 11:31:21 +0000

    [Android] Add ntp promo show logic in signin promo coordinator
    The logic is imported from SyncPromoController#canShowNtpPromo()
    
    Bug: 327387704
    Change-Id: Ia9eb096398d8df2ac09f9cc46d9c0fd04934b28a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033740
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392178}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/signin_promo/SigninPromoMediator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/signin_promo/SigninPromoMediator.java
@@ -91,7 +91,7 @@ final class SigninPromoMediator
     }
 
     boolean canShowPromo() {
-        return !mMaxImpressionReached && mDelegate.canShowPromo();
+        return !mMaxImpressionReached && mDelegate.canShowPromo(getVisibleAccount());
     }
 
     /** Implements {@link IdentityManager.Observer} */
