27c1aff36954cec31d478d449043fa7bcfd356be
Colin Blundell <blundell@chromium.org>
2024-12-05 07:48:23 +0000

    [Blink] Get alpha type in OffscreenCanvas via dedicated getter
    Builds on [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6068469
    
    Bug: 371227617
    Change-Id: I8e22706649e99e2b6294f1b47a84ec5eff4eb70a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070259
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392102}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -555,9 +555,10 @@ CanvasResourceProvider* OffscreenCanvas::GetOrCreateResourceProvider() {
   }
 
   const SkColorInfo resource_info = GetRenderingContextSkColorInfo();
+  const SkAlphaType alpha_type = GetRenderingContextAlphaType();
   if (use_shared_image) {
     provider = CanvasResourceProvider::CreateSharedImageProvider(
-        Size(), resource_info.colorType(), resource_info.alphaType(),
+        Size(), resource_info.colorType(), alpha_type,
         resource_info.refColorSpace(),
         CanvasResourceProvider::ShouldInitialize::kCallClear,
         SharedGpuContext::ContextProviderWrapper(),
@@ -568,7 +569,7 @@ CanvasResourceProvider* OffscreenCanvas::GetOrCreateResourceProvider() {
     base::WeakPtr<CanvasResourceDispatcher> dispatcher_weakptr =
         GetOrCreateResourceDispatcher()->GetWeakPtr();
     provider = CanvasResourceProvider::CreateSharedBitmapProvider(
-        Size(), resource_info.colorType(), resource_info.alphaType(),
+        Size(), resource_info.colorType(), alpha_type,
         resource_info.refColorSpace(),
         CanvasResourceProvider::ShouldInitialize::kCallClear,
         SharedGpuContext::SharedImageInterfaceProvider(), this);
@@ -582,7 +583,7 @@ CanvasResourceProvider* OffscreenCanvas::GetOrCreateResourceProvider() {
     // another type of resource prover above is a sign that the graphics
     // pipeline is in a bad state (e.g. gpu process crashed, out of memory)
     provider = CanvasResourceProvider::CreateBitmapProvider(
-        Size(), resource_info.colorType(), resource_info.alphaType(),
+        Size(), resource_info.colorType(), alpha_type,
         resource_info.refColorSpace(),
         CanvasResourceProvider::ShouldInitialize::kCallClear, this);
   }
