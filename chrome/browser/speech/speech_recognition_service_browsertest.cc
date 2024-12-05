f3a3ad9f30f98f6ce9267f1814aea365e99ec79e
Evan Liu <evliu@google.com>
2024-12-05 22:15:58 +0000

    Fix SpeechRecognitionServiceTest
    This CL fixes the SpeechRecognitionServiceTest that was broken by crrev.com/c/6038937
    
    Fix: 382541692
    Change-Id: Ie567f76ffae7dafd6a945c53dce17a78274611f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075430
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Auto-Submit: Evan Liu <evliu@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392571}

--- chrome/browser/speech/speech_recognition_service_browsertest.cc
+++ chrome/browser/speech/speech_recognition_service_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/memory/unsafe_shared_memory_region.h"
 #include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/sync_socket.h"
@@ -93,8 +94,7 @@ class TestStreamFactory : public audio::FakeStreamFactory {
     base::SyncSocket socket1, socket2;
     base::SyncSocket::CreatePair(&socket1, &socket2);
     std::move(created_callback)
-        .Run({std::in_place,
-              base::ReadOnlySharedMemoryRegion::Create(kShMemSize).region,
+        .Run({std::in_place, base::UnsafeSharedMemoryRegion::Create(kShMemSize),
               mojo::PlatformHandle(socket1.Take())},
              false /*initially muted*/, base::UnguessableToken::Create());
   }
