9bed310c605282e8abed5d2e29b4d05b9f9f6a13
Benjamin Williams <bwwilliams@google.com>
2024-12-05 18:44:13 +0000

    [iOS] Fix Docking Promo Lottie spacing on iPad
    This CL addresses an issue where the Docking Promo Lottie animation on
    iPad requires scrolling to view all the text.
    
    To resolve this, the Lottie spacing is adjusted with a -24px offset as
    requested by the design team. This ensures all instructional text is
    visible by default without the need for scrolling.
    
    Before:
    https://screenshot.googleplex.com/6NT2e96ttoUe63U.png
    
    After:
    https://screenshot.googleplex.com/9JAuri7ZgRQHvMk.png
    
    Before (multi-window):
    https://screenshot.googleplex.com/C7WaADLkj9VSQA3.png
    
    After (multi-window):
    https://screenshot.googleplex.com/6ivTRwWsoYPBxsU.png
    
    After (multi-window with 1/3 split):
    https://screenshot.googleplex.com/BGkyHyBT96txv3S.png
    
    Fixed: 331260956
    Change-Id: I4c5f4ba1e106e6acdc318fca637baa7484269efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063508
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392414}

--- ios/chrome/browser/docking_promo/ui/animated_promo_view_controller.mm
+++ ios/chrome/browser/docking_promo/ui/animated_promo_view_controller.mm
@@ -9,13 +9,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 #import "ios/public/provider/chrome/browser/lottie/lottie_animation_api.h"
 #import "ios/public/provider/chrome/browser/lottie/lottie_animation_configuration.h"
 
 namespace {
+
+// Spacing applied at the top of the alert screen if there is no navigation bar.
 constexpr CGFloat kCustomSpacingAtTopIfNoNavigationBar = 32;
+
+// Spacing applied after the image when no animation is shown.
 constexpr CGFloat kCustomSpacingAfterImageWithoutAnimation = 0;
+
+// Default spacing applied between elements in the alert screen layout.
 constexpr CGFloat kCustomSpacing = 8;
+
+// Offset to raise the alertScreen's top anchor for devices with a regular
+// size class.
+constexpr CGFloat kCustomTopOffsetForRegularSizeClass = -24;
+
 }  // namespace
 
 @interface AnimatedPromoViewController ()
@@ -71,13 +83,9 @@ constexpr CGFloat kCustomSpacing = 8;
 
   if (@available(iOS 17, *)) {
     NSArray<UITrait>* traits = TraitCollectionSetForTraits(
-        @[ UITraitUserInterfaceStyle.class, UITraitVerticalSizeClass.class ]);
-    __weak __typeof(self) weakSelf = self;
-    UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
-                                     UITraitCollection* previousCollection) {
-      [weakSelf updateUIOnTraitChange:previousCollection];
-    };
-    [self registerForTraitChanges:traits withHandler:handler];
+        @[ UITraitVerticalSizeClass.class, UITraitUserInterfaceStyle.class ]);
+    [self registerForTraitChanges:traits
+                       withAction:@selector(updateUIOnTraitChange)];
   }
 }
 
@@ -90,7 +98,7 @@ constexpr CGFloat kCustomSpacing = 8;
     return;
   }
 
-  [self updateUIOnTraitChange:previousTraitCollection];
+  [self updateUIOnTraitChange];
 }
 #endif
 
@@ -154,13 +162,21 @@ constexpr CGFloat kCustomSpacing = 8;
 // Called when the screen rotates, or in the initial layout.
 - (void)updateAlertScreenTopAnchorConstraint {
   _alertScreenTopAnchorConstraint.active = NO;
+
+  CGFloat topOffset = 0;
+  if (IsRegularXRegularSizeClass(_alertScreen)) {
+    topOffset = kCustomTopOffsetForRegularSizeClass;
+  }
+
   if ([self shouldShowAnimation]) {
     _alertScreenTopAnchorConstraint = [_alertScreen.view.topAnchor
-        constraintEqualToAnchor:self.view.centerYAnchor];
+        constraintEqualToAnchor:self.view.centerYAnchor
+                       constant:topOffset];
   } else {
     _alertScreenTopAnchorConstraint = [_alertScreen.view.topAnchor
         constraintEqualToAnchor:self.view.topAnchor];
   }
+
   _alertScreenTopAnchorConstraint.active = YES;
 }
 
@@ -221,7 +237,7 @@ constexpr CGFloat kCustomSpacing = 8;
 
 // Called when the device is rotated or dark mode is enabled/disabled. (Un)Hide
 // the animations accordingly.
-- (void)updateUIOnTraitChange:(UITraitCollection*)previousTraitCollection {
+- (void)updateUIOnTraitChange {
   BOOL darkModeEnabled =
       (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark);
   BOOL hidden = ![self shouldShowAnimation];
