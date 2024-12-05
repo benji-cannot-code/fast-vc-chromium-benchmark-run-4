0986f7fbdd6e9870fb0f117b2d60257a40f5a387
Sophie Wen <sophiewen@google.com>
2024-12-05 03:49:33 +0000

    sunfish: Remove gen204 latency logs
    This CL removes the latency ping logs from the Capture Mode
    LensOverlayQueryController.
    
    Bug: b:362363034
    Change-Id: I1acd4358d119bb4ead8ac27ac5c2812faa291866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6064525
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392062}

--- chrome/browser/ui/ash/capture_mode/chrome_capture_mode_delegate.h
+++ chrome/browser/ui/ash/capture_mode/chrome_capture_mode_delegate.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lens/core/mojom/text.mojom.h"
 #include "chrome/browser/screen_ai/public/optical_character_recognizer.h"
 #include "chrome/browser/ui/ash/capture_mode/lens_overlay_query_controller.h"
-#include "chrome/browser/ui/lens/lens_overlay_gen204_controller.h"
 #include "chromeos/ash/components/drivefs/mojom/drivefs.mojom-forward.h"
 #include "components/drive/file_errors.h"
 #include "components/lens/proto/server/lens_overlay_response.pb.h"
@@ -205,8 +204,6 @@ class ChromeCaptureModeDelegate : public ash::CaptureModeDelegate {
   SEQUENCE_CHECKER(sequence_checker_);
 
   std::unique_ptr<LensOverlayQueryController> lens_overlay_query_controller_;
-
-  std::unique_ptr<lens::LensOverlayGen204Controller> gen204_controller_;
 
   base::WeakPtrFactory<ChromeCaptureModeDelegate> weak_ptr_factory_{this};
 };
