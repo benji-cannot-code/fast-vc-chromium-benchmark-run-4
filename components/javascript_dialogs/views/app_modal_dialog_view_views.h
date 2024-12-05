3d362a22afac20e32424bc0de5fb0b2ad03fbce2
Maksim Sisov <msisov@igalia.com>
2024-12-05 08:35:14 +0000

    Remove lacros support code from //components/javascript_dialogs
    Also renamed IS_CHROMEOS_ASH to IS_CHROMEOS as lacros is deprecated and
    usage of IS_CHROMEOS_ASH is discouraged
    
    Bug: 375523816, 373972275, 373971535
    Change-Id: I1639cb4ce1443b5898142dc2c3fef59edefe7395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070428
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1392116}

--- components/javascript_dialogs/views/app_modal_dialog_view_views.h
+++ components/javascript_dialogs/views/app_modal_dialog_view_views.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
-#include "build/chromeos_buildflags.h"
 #include "components/javascript_dialogs/app_modal_dialog_view.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/views/window/dialog_delegate.h"
@@ -21,10 +20,6 @@ class MessageBoxView;
 namespace javascript_dialogs {
 
 class AppModalDialogController;
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-class LayerDimmer;
-#endif  // IS_CHROMEOS_LACROS
 
 class AppModalDialogViewViews : public AppModalDialogView,
                                 public views::DialogDelegate {
@@ -56,10 +51,6 @@ class AppModalDialogViewViews : public AppModalDialogView,
 
  private:
   std::unique_ptr<AppModalDialogController> controller_;
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  std::unique_ptr<LayerDimmer> layerDimmer_;
-#endif  // IS_CHROMEOS_LACROS
 
   // The message box view whose commands we handle.
   raw_ptr<views::MessageBoxView> message_box_view_;
