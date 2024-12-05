8784f564bfd9b19c50ae90569d6f48f4e674cb63
Stephen Chenney <schenney@chromium.org>
2024-12-05 16:09:51 +0000

    Add UseCounter for custom properties in ::selection
    Adding this in anticipation of changing the spec, to assess just
    how common it is (probably very common).
    
    Bug: 381125910
    Change-Id: Ie6a69b4cacf788a164dd4338547a2c2c7edf6aba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6069547
    Auto-Submit: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392307}

--- third_party/blink/renderer/core/css/properties/longhands/custom_property.cc
+++ third_party/blink/renderer/core/css/properties/longhands/custom_property.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/property_registration.h"
 #include "third_party/blink/renderer/core/css/property_registry.h"
 #include "third_party/blink/renderer/core/css/resolver/style_builder_converter.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 
 namespace blink {
@@ -119,15 +120,19 @@ void CustomProperty::ApplyInherit(StyleResolverState& state) const {
 void CustomProperty::ApplyValue(StyleResolverState& state,
                                 const CSSValue& value,
                                 ValueMode value_mode) const {
+  ComputedStyleBuilder& builder = state.StyleBuilder();
+  DCHECK(!value.IsCSSWideKeyword());
+
   // Highlight Pseudos do not allow custom property definitions.
   // Properties are copied from the originating element when the
   // style is created.
   if (state.UsesHighlightPseudoInheritance()) {
+    if (builder.StyleType() == kPseudoIdSelection) {
+      UseCounter::Count(state.GetDocument(),
+                        WebFeature::kSelectionCustomProperty);
+    }
     return;
   }
-
-  ComputedStyleBuilder& builder = state.StyleBuilder();
-  DCHECK(!value.IsCSSWideKeyword());
 
   builder.SetHasVariableDeclaration();
 
