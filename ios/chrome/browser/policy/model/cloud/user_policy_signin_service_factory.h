5f392ac2173237b0ef29f84cbdc17ee5dc0554f6
Federica Germinario <fedegermi@google.com>
2024-12-05 17:57:16 +0000

    Use ProfileKeyedServiceFactoryIOS in UserPolicySigninServiceFactory
    Convert UserPolicySigninServiceFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363216130
    Change-Id: I0f87488358d5141dcf4b64f41288dc379205ca17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068779
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392377}

--- ios/chrome/browser/policy/model/cloud/user_policy_signin_service_factory.h
+++ ios/chrome/browser/policy/model/cloud/user_policy_signin_service_factory.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_POLICY_MODEL_CLOUD_USER_POLICY_SIGNIN_SERVICE_FACTORY_H_
 
 #include "base/memory/singleton.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 
@@ -26,7 +26,7 @@ class UserPolicySigninService;
 // Warning: ONLY use the service when Enterprise Policy is enabled where
 // the policy system objects are enabled (eg. the BrowserPolicyConnector object
 // was instantiated).
-class UserPolicySigninServiceFactory : public BrowserStateKeyedServiceFactory {
+class UserPolicySigninServiceFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   // Returns an instance of the UserPolicySigninServiceFactory singleton.
   static UserPolicySigninServiceFactory* GetInstance();
@@ -40,11 +40,6 @@ class UserPolicySigninServiceFactory : public BrowserStateKeyedServiceFactory {
   static void SetDeviceManagementServiceForTesting(
       DeviceManagementService* device_management_service);
 
-  UserPolicySigninServiceFactory(const UserPolicySigninServiceFactory&) =
-      delete;
-  UserPolicySigninServiceFactory& operator=(
-      const UserPolicySigninServiceFactory&) = delete;
-
  protected:
   // BrowserStateKeyedServiceFactory implementation:
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
@@ -53,8 +48,6 @@ class UserPolicySigninServiceFactory : public BrowserStateKeyedServiceFactory {
       user_prefs::PrefRegistrySyncable* registry) override;
 
  private:
-  bool ServiceIsCreatedWithBrowserState() const override;
-  bool ServiceIsNULLWhileTesting() const override;
 
   friend struct base::DefaultSingletonTraits<UserPolicySigninServiceFactory>;
 
--- ios/chrome/browser/policy/model/cloud/user_policy_signin_service_factory.mm
+++ ios/chrome/browser/policy/model/cloud/user_policy_signin_service_factory.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/policy/model/cloud/user_policy_signin_service_factory.h"
 
 #import "base/memory/ref_counted.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/policy/core/browser/browser_policy_connector.h"
 #import "components/policy/core/common/policy_pref_names.h"
 #import "components/pref_registry/pref_registry_syncable.h"
@@ -27,19 +26,19 @@ policy::DeviceManagementService* g_device_management_service_for_testing = NULL;
 namespace policy {
 
 UserPolicySigninServiceFactory::UserPolicySigninServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "UserPolicySigninService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("UserPolicySigninService",
+                                    ServiceCreation::kCreateWithProfile,
+                                    TestingCreation::kNoServiceForTests) {
   DependsOn(IdentityManagerFactory::GetInstance());
 }
 
-UserPolicySigninServiceFactory::~UserPolicySigninServiceFactory() {}
+UserPolicySigninServiceFactory::~UserPolicySigninServiceFactory() = default;
 
 // static
 UserPolicySigninService* UserPolicySigninServiceFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<UserPolicySigninService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<UserPolicySigninService>(
+      profile, /*create=*/true);
 }
 
 // static
@@ -80,18 +79,6 @@ UserPolicySigninServiceFactory::BuildServiceInstanceFor(
 void UserPolicySigninServiceFactory::RegisterBrowserStatePrefs(
     user_prefs::PrefRegistrySyncable* user_prefs) {
   user_prefs->RegisterInt64Pref(policy_prefs::kLastPolicyCheckTime, 0);
-}
-
-bool UserPolicySigninServiceFactory::ServiceIsCreatedWithBrowserState() const {
-  // When Enterprise Policy is enabled, initialize the UserPolicySigninService
-  // early when creating the BrowserState. This will make sure that the user
-  // polices are fetched if there is no cache at startup when the account is
-  // already syncing and eligible for user policy.
-  return true;
-}
-
-bool UserPolicySigninServiceFactory::ServiceIsNULLWhileTesting() const {
-  return true;
 }
 
 }  // namespace policy
