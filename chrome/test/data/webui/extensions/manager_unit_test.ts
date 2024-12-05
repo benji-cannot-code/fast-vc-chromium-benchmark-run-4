aadc8fed1f5548ad0340892a2707a531139cc0f5
rbpotter <rbpotter@chromium.org>
2024-12-05 23:32:17 +0000

    CrLitElement: Fix property to attribute mapping for numbers in names
    To match Polymer, map properties like prop2WithMor3Text to attributes
    like prop2-with-mor3-text not prop2with-mor3text.
    
    This fixes a broken binding in the extensions page, where the attribute
    binding for isMv2DeprecationNoticeDismissed was not working, since
    it used an attribute name of is-mv2-deprecation-notice-dismissed (like
    the Polymer version).
    
    Bug: 382310352
    Change-Id: I8d507c3ff1cb4cfd5eee771cdbe649be06bd37f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073008
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392620}

--- chrome/test/data/webui/extensions/manager_unit_test.ts
+++ chrome/test/data/webui/extensions/manager_unit_test.ts
@@ -199,7 +199,7 @@ suite('ExtensionManagerUnitTest', function() {
         assertEquals(newDescription, content.textContent!.trim());
       });
 
-  test('ProfileSettings', function() {
+  test('ProfileSettings', async () => {
     assertFalse(manager.inDevMode);
 
     service.profileStateChangedTarget.callListeners({inDeveloperMode: true});
@@ -213,6 +213,18 @@ suite('ExtensionManagerUnitTest', function() {
 
     service.profileStateChangedTarget.callListeners({canLoadUnpacked: false});
     assertFalse(manager.canLoadUnpacked);
+
+    service.profileStateChangedTarget.callListeners(
+        {isMv2DeprecationNoticeDismissed: true});
+    assertTrue(manager.isMv2DeprecationNoticeDismissed);
+    await microtasksFinished();
+    assertTrue(manager.$['items-list'].isMv2DeprecationNoticeDismissed);
+
+    service.profileStateChangedTarget.callListeners(
+        {isMv2DeprecationNoticeDismissed: false});
+    assertFalse(manager.isMv2DeprecationNoticeDismissed);
+    await microtasksFinished();
+    assertFalse(manager.$['items-list'].isMv2DeprecationNoticeDismissed);
   });
 
   test('Uninstall', async () => {
