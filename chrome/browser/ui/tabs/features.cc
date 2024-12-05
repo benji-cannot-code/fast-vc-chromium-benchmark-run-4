2110cc75b1902e56845067b6c3cd8a16edecb182
Emily Shack <emshack@chromium.org>
2024-12-05 00:01:40 +0000

    Disable position setting if combo button is enabled
    Tab search position setting is irrelevant for the combo button, which
    cannot be repositioned.
    
    Bug: 382321257
    Change-Id: I711fe5543a64f5bd3050ef5a7501114301b03c28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6071608
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1391960}

--- chrome/browser/ui/tabs/features.cc
+++ chrome/browser/ui/tabs/features.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "chrome/browser/buildflags.h"
+#include "chrome/common/chrome_features.h"
 
 namespace tabs {
 
@@ -47,6 +48,11 @@ BASE_FEATURE(kTabStripCollectionStorage,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool CanShowTabSearchPositionSetting() {
+  // The combo button, which includes tab search, is always on the right side
+  // and cannot be repositioned.
+  if (features::IsTabstripComboButtonEnabled()) {
+    return false;
+  }
 // Mac and other platforms will always have the tab search position in the
 // correct location, cros/linux/win git the user the option to change.
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
