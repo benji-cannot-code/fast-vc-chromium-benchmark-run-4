a900ff84b9a0aa866ffaa2aed26e7092a864f16f
Radu Nitescu <radunitescu@google.com>
2024-12-05 14:17:42 +0000

    [iOS][LO] Move consent_presenter to ui directory
    This is where all the rest of the presenters live
    
    Bug: None
    Change-Id: I3876b9d01600a01add13500debb71974c6f2f2dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075061
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392246}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_consent_presenter.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_consent_presenter.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/lens_overlay/coordinator/lens_overlay_consent_presenter.h"
+#import "ios/chrome/browser/lens_overlay/ui/lens_overlay_consent_presenter.h"
 
 #import "base/check.h"
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_detents_manager.h"
