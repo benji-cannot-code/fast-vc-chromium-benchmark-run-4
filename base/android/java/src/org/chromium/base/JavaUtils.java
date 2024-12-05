4c48a377c6a010bd111653432cf04d2ae940b781
Andrew Grieve <agrieve@chromium.org>
2024-12-05 02:46:56 +0000

    Android: Remove enable_nullaway GN variable
    It's too cumbersome to opt-in on a per-target basis. Rely on @NullMarked
    instead.
    
    Bug: 40657579
    Change-Id: I5662ce4d2bc8d3b9f70707a3a5c877c9985b8f65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068275
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392045}

--- base/android/java/src/org/chromium/base/JavaUtils.java
+++ base/android/java/src/org/chromium/base/JavaUtils.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base;
 
+import org.chromium.build.annotations.NullMarked;
+
+@NullMarked
 public class JavaUtils {
     private JavaUtils() {}
 
--- base/android/java/src/org/chromium/base/Log.java
+++ base/android/java/src/org/chromium/base/Log.java
@@ -9,6 +9,7 @@ import androidx.annotation.Nullable;
 
 import org.chromium.build.BuildConfig;
 import org.chromium.build.annotations.AlwaysInline;
+import org.chromium.build.annotations.NullMarked;
 
 import java.util.Locale;
 
@@ -21,6 +22,7 @@ import java.util.Locale;
  *
  * <p>Usage documentation: {@code //docs/android_logging.md}.
  */
+@NullMarked
 public class Log {
     /** Convenience property, same as {@link android.util.Log#ASSERT}. */
     public static final int ASSERT = android.util.Log.ASSERT;
