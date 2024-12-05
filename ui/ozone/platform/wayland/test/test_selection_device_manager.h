2af025e7c9fb26b1b1367bd1a2cc2d5c0b90a06a
Devon Loehr <dloehr@google.com>
2024-12-05 18:30:39 +0000

    Rewrite raw_ptr in InterfaceInfo
    This change replaces the C++ pointers in InterfaceInfo with raw_ptrs as part of the MiraclePtr project.
    
    Bug: 346694445
    Change-Id: Ifab9b6b002b9783f72e770bd37bee1ef26cc67ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062310
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392404}

--- ui/ozone/platform/wayland/test/test_selection_device_manager.h
+++ ui/ozone/platform/wayland/test/test_selection_device_manager.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/threading/thread.h"
 #include "ui/ozone/platform/wayland/test/global_object.h"
@@ -47,12 +46,8 @@ class TestSelectionDeviceManager : public GlobalObject {
   };
 
   struct InterfaceInfo {
-    // This field is not a raw_ptr<> because it was filtered by the rewriter
-    // for: #global-scope
-    RAW_PTR_EXCLUSION const struct wl_interface* interface;
-    // This field is not a raw_ptr<> because it was filtered by the rewriter
-    // for: #global-scope
-    RAW_PTR_EXCLUSION const void* implementation;
+    raw_ptr<const struct wl_interface> interface;
+    raw_ptr<const void> implementation;
     uint32_t version;
   };
 
