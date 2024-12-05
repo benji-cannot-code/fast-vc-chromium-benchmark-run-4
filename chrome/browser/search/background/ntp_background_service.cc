24f00b053a140a696b862cce05aef19828a3218c
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-12-05 15:13:48 +0000

    Remove hardcoded chrome://welcome NTP backgrounds
    The chrome://welcome page has been removed from the repo already, and
    this background allowlist is not used anymore
    
    Bug: 40253961
    Change-Id: Ibc591425c9ce9f69c8a61b94455c768f9074878c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070328
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392276}

--- chrome/browser/search/background/ntp_background_service.cc
+++ chrome/browser/search/background/ntp_background_service.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/search/background/ntp_background.pb.h"
-#include "chrome/browser/search/background/ntp_backgrounds.h"
 #include "components/search/ntp_features.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_context.h"
@@ -662,12 +661,6 @@ void NtpBackgroundService::RemoveObserver(
 }
 
 bool NtpBackgroundService::IsValidBackdropUrl(const GURL& url) const {
-  for (auto& ntp_background : GetNtpBackgrounds()) {
-    if (ntp_background == url) {
-      return true;
-    }
-  }
-
   for (auto& image : collection_images_) {
     if (image.image_url == url)
       return true;
