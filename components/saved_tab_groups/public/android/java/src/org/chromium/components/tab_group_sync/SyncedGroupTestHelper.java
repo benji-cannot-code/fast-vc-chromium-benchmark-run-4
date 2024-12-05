70a3e9ab1d9b8c47aebfc51aaacd57ac85515619
Sky Malice <skym@chromium.org>
2024-12-05 15:57:19 +0000

    Plumb sync group id through sharing notification.
    The sync id must be used instead of any local id, as closed groups do
    not have a local ids.
    
    Bug: 369186228
    Change-Id: Ic1bb3f4ad08ff519f13449d57e2306c9d9fe77eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6052772
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392301}

--- components/saved_tab_groups/public/android/java/src/org/chromium/components/tab_group_sync/SyncedGroupTestHelper.java
+++ components/saved_tab_groups/public/android/java/src/org/chromium/components/tab_group_sync/SyncedGroupTestHelper.java
@@ -5,13 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.tab_group_sync;
 
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.when;
 
+import org.mockito.ArgumentMatcher;
+
+import org.chromium.base.Token;
 import org.chromium.url.GURL;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -78,9 +83,26 @@ public class SyncedGroupTestHelper {
         return group;
     }
 
+    /**
+     * @param syncId The sync id of the group, when not unique it will overwrite previous mocks.
+     * @param tabGroupId The local id of the group.
+     * @return A new group object.
+     */
+    public SavedTabGroup newTabGroup(String syncId, Token tabGroupId) {
+        SavedTabGroup group = newTabGroup(syncId);
+        when(mTabGroupSyncService.getGroup(argThat(matchesTabGroupToken(tabGroupId))))
+                .thenReturn(group);
+        return group;
+    }
+
     /** Removes the tab group from mocked responses. */
     public void removeTabGroup(String syncId) {
         mTabGroupSyncIdSet.remove(syncId);
         when(mTabGroupSyncService.getGroup(syncId)).thenReturn(null);
+    }
+
+    private ArgumentMatcher<LocalTabGroupId> matchesTabGroupToken(Token tabGroupId) {
+        return localTabGroupId ->
+                localTabGroupId != null && Objects.equals(localTabGroupId.tabGroupId, tabGroupId);
     }
 }
