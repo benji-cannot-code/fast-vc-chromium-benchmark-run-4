15fb5297f51aa227f002d5060495aaaec6d52034
Mariam Ali <alimariam@google.com>
2024-12-05 10:49:38 +0000

    CBD Timeframe Experiment: Remove unused prefs
    Bug: 40283307, 337781204
    Change-Id: I2cc3dd67cf9d5d1fa489167d62bb1ab588b78fe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6069932
    Auto-Submit: Mariam Ali <alimariam@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392163}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1179,6 +1179,12 @@ inline constexpr char kHatsPrivacyHubPostLaunchCycleEndTs[] =
     "hats_privacy_hub_postlaunch_end_timestamp";
 #endif
 
+// Deprecated 12/2024.
+inline constexpr char kDeleteTimePeriodV2[] =
+    "browser.clear_data.time_period_v2";
+inline constexpr char kDeleteTimePeriodV2Basic[] =
+    "browser.clear_data.time_period_v2_basic";
+
 // Register local state used only for migration (clearing or moving to a new
 // key).
 void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
@@ -1674,6 +1680,10 @@ void RegisterProfilePrefsForMigration(
   registry->RegisterBooleanPref(kHatsPrivacyHubPostLaunchIsSelected, false);
   registry->RegisterInt64Pref(kHatsPrivacyHubPostLaunchCycleEndTs, 0);
 #endif
+
+  // Deprecated 12/2024.
+  registry->RegisterIntegerPref(kDeleteTimePeriodV2, -1);
+  registry->RegisterIntegerPref(kDeleteTimePeriodV2Basic, -1);
 }
 
 void ClearSyncRequestedPrefAndMaybeMigrate(PrefService* profile_prefs) {
@@ -3041,6 +3051,10 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
   profile_prefs->ClearPref(kHatsPrivacyHubPostLaunchIsSelected);
   profile_prefs->ClearPref(kHatsPrivacyHubPostLaunchCycleEndTs);
 #endif
+
+  // Added 12/2024.
+  profile_prefs->ClearPref(kDeleteTimePeriodV2);
+  profile_prefs->ClearPref(kDeleteTimePeriodV2Basic);
 
   // Please don't delete the following line. It is used by PRESUBMIT.py.
   // END_MIGRATE_OBSOLETE_PROFILE_PREFS
