037f4a73e27f4314bb50c303a44c95272ce593f9
Monica Basta <msalama@chromium.org>
2024-12-05 13:03:26 +0000

    [B4B-D] Enforce selection in context menu is not empty
    It is meaningless to creat context menu with empty nodes. This CL
    enforces selection to be not empty, nor null or have repeated nodes.
    
    Bug: 369304373
    Change-Id: I59b331dba9ceb61fc1554ac7a99302b70e11653a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054791
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392206}

--- chrome/browser/ui/bookmarks/bookmark_context_menu_controller.cc
+++ chrome/browser/ui/bookmarks/bookmark_context_menu_controller.cc
@@ -135,6 +135,20 @@ bool IsSelectionPermanentBookmarkFolder(
   return false;
 }
 
+// Check selection is not empty, nodes are not null nor repeated.
+void CheckSelectionIsValid(
+    const std::vector<raw_ptr<const BookmarkNode, VectorExperimental>>&
+        selection) {
+  CHECK(!selection.empty());
+  // Nodes must be not null.
+  CHECK(base::ranges::all_of(selection,
+                             [](const BookmarkNode* node) { return node; }));
+
+  // Check not repeated nodes.
+  std::set<const BookmarkNode*> nodes_set(selection.begin(), selection.end());
+  CHECK_EQ(selection.size(), nodes_set.size());
+}
+
 }  // namespace
 
 BookmarkContextMenuController::BookmarkContextMenuController(
@@ -156,6 +170,8 @@ BookmarkContextMenuController::BookmarkContextMenuController(
       new_nodes_parent_(GetParentForNewNodes(selection)) {
   DCHECK(profile_);
   DCHECK(bookmark_service_->loaded());
+  CheckSelectionIsValid(selection);
+  CHECK(new_nodes_parent_);
   menu_model_ = std::make_unique<ui::SimpleMenuModel>(this);
   bookmark_service_->bookmark_model()->AddObserver(this);
 
@@ -174,10 +190,6 @@ BookmarkContextMenuController::GetParentForNewNodes(
   if (selection.empty()) {
     return nullptr;
   }
-
-  // Check valid node selection.
-  CHECK(base::ranges::all_of(selection,
-                             [](const BookmarkNode* node) { return node; }));
 
   const BookmarkNode* parent = nullptr;
   if (IsSelectionPermanentBookmarkFolder(selection)) {
@@ -405,7 +417,6 @@ void BookmarkContextMenuController::ExecuteCommand(int id, int event_flags) {
               &title)) {
         break;
       }
-      CHECK(new_nodes_parent_);
       BookmarkEditor::Show(parent_window_, profile_,
                            BookmarkEditor::EditDetails::AddNodeInFolder(
                                BookmarkUIOperationsHelperMergedSurfaces(
@@ -420,7 +431,6 @@ void BookmarkContextMenuController::ExecuteCommand(int id, int event_flags) {
       base::RecordAction(
           UserMetricsAction("BookmarkBar_ContextMenu_NewFolder"));
 
-      CHECK(new_nodes_parent_);
       BookmarkEditor::Show(parent_window_, profile_,
                            BookmarkEditor::EditDetails::AddFolder(
                                BookmarkUIOperationsHelperMergedSurfaces(
@@ -465,7 +475,6 @@ void BookmarkContextMenuController::ExecuteCommand(int id, int event_flags) {
     case IDC_BOOKMARK_MANAGER: {
       if (selection_.size() == 1 ||
           IsSelectionPermanentBookmarkFolder(selection_)) {
-        CHECK(new_nodes_parent_);
         chrome::ShowBookmarkManagerForNode(
             browser_, BookmarkUIOperationsHelperMergedSurfaces(
                           bookmark_service_, new_nodes_parent_.get())
@@ -492,10 +501,6 @@ void BookmarkContextMenuController::ExecuteCommand(int id, int event_flags) {
       break;
 
     case IDC_PASTE: {
-      if (!new_nodes_parent_) {
-        return;
-      }
-
       BookmarkUIOperationsHelperMergedSurfaces(bookmark_service_,
                                                new_nodes_parent_.get())
           .PasteFromClipboard(GetIndexForNewNodes());
@@ -629,7 +634,7 @@ bool BookmarkContextMenuController::IsCommandIdEnabled(int command_id) const {
 
     case IDC_BOOKMARK_BAR_NEW_FOLDER:
     case IDC_BOOKMARK_BAR_ADD_NEW_BOOKMARK:
-      return can_edit && new_nodes_parent_;
+      return can_edit;
 
     case IDC_BOOKMARK_BAR_ALWAYS_SHOW:
       return !prefs->IsManagedPreference(bookmarks::prefs::kShowBookmarkBar);
--- chrome/browser/ui/webui/side_panel/bookmarks/bookmarks_page_handler.cc
+++ chrome/browser/ui/webui/side_panel/bookmarks/bookmarks_page_handler.cc
@@ -170,8 +170,10 @@ std::unique_ptr<BookmarkContextMenu> ContextMenuFromNodes(
     }
   }
 
-  return std::make_unique<BookmarkContextMenu>(
-      browser, embedder, bookmarks, source, shopping_list_controller);
+  return bookmarks.empty() ? nullptr
+                           : std::make_unique<BookmarkContextMenu>(
+                                 browser, embedder, bookmarks, source,
+                                 shopping_list_controller);
 }
 
 }  // namespace
