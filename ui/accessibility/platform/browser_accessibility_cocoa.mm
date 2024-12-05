c79e22084c79f736da070a3d746a878023e4f66e
Alexander Surkov <asurkov@igalia.com>
2024-12-05 20:16:57 +0000

    [AX Mac] migrate columns attribute
    Implement accessibilityColumns methods, and hide its
    NSAccessibility attribute counterparts under a flag.
    
    Test: `out/.../content_browsertests \
       --gtest_filter="*AccessibilityColumns*"`
    
    Test: `out/.../accessibility_unittests \
       --gtest_filter="*AccessibilityColumns*"`
    
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:mac-fieldtrial-tester
    
    Bug: 363275809
    Change-Id: I4727382a75d9582ad8b02698003e49016e41d5b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021308
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392488}

--- ui/accessibility/platform/browser_accessibility_cocoa.mm
+++ ui/accessibility/platform/browser_accessibility_cocoa.mm
@@ -662,6 +662,7 @@ bool ui::IsNSRange(id value) {
   return nil;
 }
 
+// LINT.IfChange(accessibilityColumns)
 - (NSArray*)columns {
   if (![self instanceActive])
     return nil;
@@ -672,6 +673,7 @@ bool ui::IsNSRange(id value) {
   }
   return ret;
 }
+// LINT.ThenChange(ui/accessibility/platform/ax_platform_node_cocoa.mm:accessibilityColumns)
 
 - (BrowserAccessibility*)containingTable {
   BrowserAccessibility* table = _owner;
