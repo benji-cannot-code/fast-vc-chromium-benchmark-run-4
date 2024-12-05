a40b54536614c1452fe40a50c79c886b9c1cc460
Sam Chen <chensa@google.com>
2024-12-05 06:27:48 +0000

    SafetyService: return if arc isn't connected during session creation
    This fixes the bug that chrome crashes when we call
    CreateOnDeviceSafetySession before arc is ready.
    
    BUG=b:357479669
    TEST=build, confirms chrome doesn't crash
    
    Change-Id: Ic22f69cccb8271365579ed8941acee8f729e9cc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6066140
    Reviewed-by: Yi Chou <yich@google.com>
    Reviewed-by: Howard Yang <hcyang@google.com>
    Commit-Queue: Sam Chen <chensa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392085}

--- chromeos/ash/services/cros_safety/cros_safety_service.cc
+++ chromeos/ash/services/cros_safety/cros_safety_service.cc
@@ -84,6 +84,7 @@ void CrosSafetyService::CreateOnDeviceSafetySession(
            ->IsConnected()) {
     std::move(callback).Run(
         cros_safety::mojom::GetOnDeviceSafetySessionResult::kGenericError);
+    return;
   }
 
   auto* on_device_safety_instance = ARC_GET_INSTANCE_FOR_METHOD(
@@ -93,6 +94,7 @@ void CrosSafetyService::CreateOnDeviceSafetySession(
   if (!on_device_safety_instance) {
     std::move(callback).Run(
         cros_safety::mojom::GetOnDeviceSafetySessionResult::kGenericError);
+    return;
   }
 
   on_device_safety_instance->GetArcSafetySession(
