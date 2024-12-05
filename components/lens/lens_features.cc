7151475a954730a877de088be59da0277a904991
Juan Mojica <juanmojica@google.com>
2024-12-05 01:41:06 +0000

    Remove Lens fullscreen search feature.
    This feature is unused. Also remove kLensSearchOptimizations as it
    also appears to be unused.
    
    Change-Id: I386727a19744c98345757db293b2d063ca768abd
    Bug: b:378583426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073024
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392027}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -19,10 +19,6 @@ BASE_FEATURE(kLensStandalone,
              "LensStandalone",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kLensSearchOptimizations,
-             "LensSearchOptimizations",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kEnableLatencyLogging,
              "LensImageLatencyLogging",
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -367,9 +363,6 @@ constexpr base::FeatureParam<std::string> kPreconnectKeyForLens{
 constexpr base::FeatureParam<bool> kShouldIssueProcessPrewarmingForLens{
     &kLensStandalone, "lens-issue-process-prewarming", true};
 
-const base::FeatureParam<bool> kEnableLensFullscreenSearch{
-    &kLensSearchOptimizations, "enable-lens-fullscreen-search", false};
-
 bool GetEnableLatencyLogging() {
   return base::FeatureList::IsEnabled(kEnableLatencyLogging) &&
          base::FeatureList::IsEnabled(kLensStandalone);
@@ -381,12 +374,6 @@ std::string GetHomepageURLForLens() {
 
 bool GetEnableLensHtmlRedirectFix() {
   return kEnableLensHtmlRedirectFix.Get();
-}
-
-bool IsLensFullscreenSearchEnabled() {
-  return base::FeatureList::IsEnabled(kLensStandalone) &&
-         base::FeatureList::IsEnabled(kLensSearchOptimizations) &&
-         kEnableLensFullscreenSearch.Get();
 }
 
 bool GetShouldIssuePreconnectForLens() {
