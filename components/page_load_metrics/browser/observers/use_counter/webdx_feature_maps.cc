f8d5b859a28337a1b8475f33bb3c1c9ed825ae00
Thomas Guilbert <tguilbert@chromium.org>
2024-12-05 21:15:11 +0000

    Add RemotePlayback WebDXFeatures counter
    Tie the `remote` getter on the HTMLMediaElement to a WebDXFeatures
    counter to track RemotePlayback usage.
    
    Bug: 366475997
    Change-Id: I1fc54cad4f606ce78e55703781d729eabe2df03d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072473
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392525}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -276,6 +276,8 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kFedCm, WebDXFeature::kFedcm},
           {WebFeature::kWebOTP, WebDXFeature::kWebOtp},
           {WebFeature::kPreservesPitch, WebDXFeature::kPreservesPitch},
+          {WebFeature::kV8HTMLMediaElement_Remote_AttributeGetter,
+           WebDXFeature::kRemotePlayback},
       }};
 
   return *kMap;
