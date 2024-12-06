affdd772b9c83fbac6c65cf45763e4074dc3562b
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-12-06 13:42:09 +0000

    Spanification: Use std::array when size is explicit.
    To keep the rewrite closer to the original:
    - Use std::to_array when the size was unspecified.
    - Use std::array when the size was specified.
    
    Context:
    https://chromium-review.googlesource.com/c/chromium/src/+/6043703/comment/c0a0bc98_b272ac98/
    
    Bug: 378069401
    Change-Id: Ic6f1f58eb0f1e7a2ef31c191524929e59643c751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048881
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392859}

--- tools/clang/spanify/tests/structs-with-inline-arrays-original.cc
+++ tools/clang/spanify/tests/structs-with-inline-arrays-original.cc
@@ -38,7 +38,7 @@ void fct() {
   // struct TestCases {
   //   int val;
   // };
-  // const auto kTestCases = std::to_array<TestCases, 4>({{1}, {2}, {3}, {4}});
+  // const std::array<TestCases, 4> kTestCases = {{{1}, {2}, {3}, {4}}};
   const struct {
     int val;
   } kTestCases[4] = {{1}, {2}, {3}, {4}};
@@ -48,7 +48,7 @@ void fct() {
   // struct GTestCases {
   //   int val;
   // };
-  // const auto gTestCases = std::to_array<GTestCases, 4>({{1}, {2}, {3}, {4}});
+  // const std::array<GTestCases, 4> gTestCases = {{{1}, {2}, {3}, {4}}};
   const struct {
     int val;
   } gTestCases[4] = {{1}, {2}, {3}, {4}};
@@ -58,7 +58,7 @@ void fct() {
   // struct Knights {
   //   int val;
   // };
-  // const auto knights = std::to_array<Knights, 4>({{1}, {2}, {3}, {4}});
+  // const std::array<Knights, 4> knights = {{{1}, {2}, {3}, {4}}};
   const struct {
     int val;
   } knights[4] = {{1}, {2}, {3}, {4}};
