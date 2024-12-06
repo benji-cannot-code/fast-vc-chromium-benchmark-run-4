fa0ac126da84682692789ad2420dae90136c48a4
Maksim Moskvitin <mmoskvitin@google.com>
2024-12-06 00:39:01 +0000

    [DataSharing] Deprecate async ReadGroup() API
    Hopefully, this will reduce the likelihood of new usages.
    
    Bug: 370897286
    Change-Id: If33e3c890a4a84c86fd151a6255c0fbc876f94a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062681
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392649}

--- components/data_sharing/internal/empty_data_sharing_service.cc
+++ components/data_sharing/internal/empty_data_sharing_service.cc
@@ -52,7 +52,7 @@ EmptyDataSharingService::GetPossiblyRemovedGroupMember(
   return std::nullopt;
 }
 
-void EmptyDataSharingService::ReadGroup(
+void EmptyDataSharingService::ReadGroupDeprecated(
     const GroupId& group_id,
     base::OnceCallback<void(const GroupDataOrFailureOutcome&)> callback) {}
 
--- components/data_sharing/test_support/mock_data_sharing_service.h
+++ components/data_sharing/test_support/mock_data_sharing_service.h
@@ -40,7 +40,7 @@ class MockDataSharingService : public DataSharingService {
                std::optional<GroupMemberPartialData>(const GroupId&,
                                                      const GaiaId&));
   MOCK_METHOD2(
-      ReadGroup,
+      ReadGroupDeprecated,
       void(const GroupId&,
            base::OnceCallback<void(const GroupDataOrFailureOutcome&)>));
   MOCK_METHOD2(
