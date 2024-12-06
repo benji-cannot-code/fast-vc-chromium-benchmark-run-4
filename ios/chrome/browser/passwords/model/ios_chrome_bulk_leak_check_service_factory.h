c19b50f7e288c50141d4cbe809f9bbd3639bf962
Sylvain Defresne <sdefresne@chromium.org>
2024-12-06 15:50:28 +0000

    [ios] Convert IOSChromeBulkLeakCheckServiceFactory to PKSFIOS
    Convert IOSChromeBulkLeakCheckServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363215809
    Change-Id: If1222b30149962c20efa42b2b9d31ae61875e0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6076642
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392919}

--- ios/chrome/browser/passwords/model/ios_chrome_bulk_leak_check_service_factory.h
+++ ios/chrome/browser/passwords/model/ios_chrome_bulk_leak_check_service_factory.h
@@ -6,12 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_PASSWORDS_MODEL_IOS_CHROME_BULK_LEAK_CHECK_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_PASSWORDS_MODEL_IOS_CHROME_BULK_LEAK_CHECK_SERVICE_FACTORY_H_
 
-#import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-#import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
-
-class ProfileIOS;
-enum class ServiceAccessType;
+#include "base/no_destructor.h"
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 namespace password_manager {
 class BulkLeakCheckServiceInterface;
@@ -20,7 +16,7 @@ class BulkLeakCheckServiceInterface;
 // Singleton that owns all BulkLeakCheckServices and associates them with
 // profile.
 class IOSChromeBulkLeakCheckServiceFactory
-    : public BrowserStateKeyedServiceFactory {
+    : public ProfileKeyedServiceFactoryIOS {
  public:
   static password_manager::BulkLeakCheckServiceInterface* GetForProfile(
       ProfileIOS* profile);
@@ -32,6 +28,7 @@ class IOSChromeBulkLeakCheckServiceFactory
   IOSChromeBulkLeakCheckServiceFactory();
   ~IOSChromeBulkLeakCheckServiceFactory() override;
 
+  // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* context) const override;
 };
