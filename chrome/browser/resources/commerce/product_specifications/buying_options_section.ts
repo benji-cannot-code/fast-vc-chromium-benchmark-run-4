d558b344ca61228bd11ea8273ffc1eca5ef751e4
Yue Zhang <yuezhanggg@chromium.org>
2024-12-06 16:54:44 +0000

    [Compare] Add metrics for actions on column
    Metrics are captured here: https://docs.google.com/document/d/1PEHRGGJeOGhfbWt2o26f1Olhk11mtm2ZPrWKLll0fBE/edit?resourcekey=0-qB8Ai06NJW_Pn7tcSlsmmQ&tab=t.0#bookmark=id.5sze3zjayjta
    
    Bug: 382500529
    Change-Id: I21b2cdec1a7ccfb825551b379935869b680d27b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074092
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392967}

--- chrome/browser/resources/commerce/product_specifications/buying_options_section.ts
+++ chrome/browser/resources/commerce/product_specifications/buying_options_section.ts
@@ -48,6 +48,8 @@ export class BuyingOptionsSectionElement extends CrLitElement {
 
   protected openJackpotUrl_() {
     OpenWindowProxyImpl.getInstance().openUrl(this.jackpotUrl);
+    chrome.metricsPrivate.recordUserAction(
+        'Commerce.Compare.BuyingOptionsClicked');
   }
 }
 
