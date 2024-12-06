854b06ddf64aa90fe8aeea9b3087d0d444de573a
Slobodan Pejic <slobodan@chromium.org>
2024-12-06 19:11:21 +0000

    [gardener] Revert "Enable FluentScrollbar in field trial testing config."
    This reverts commit 2a0f4d66531b6fff2b0a36cb9a485464fe470cea.
    
    Reason for revert: Seeing builder failures on external/wpt/screen-orientation/lock-unlock-check.html, this change is a suspected culprit because it touches third_party/blink/web_tests/flag-specific/disable-site-isolation-trials/external/wpt/screen-orientation/lock-unlock-check-expected.txt.
    
    Original change's description:
    > Enable FluentScrollbar in field trial testing config.
    >
    > Bug: 40213017
    > Change-Id: I09a19329ad505c7d70ad4c4e0ae76617aa7a37c3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054690
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Commit-Queue: Steve Kobes <skobes@chromium.org>
    > Reviewed-by: Andy Phan <andyphan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1392958}
    
    Bug: 40213017
    Change-Id: I114c2151f641448272e9487c922f8257fe421146
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6077804
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1393054}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -118,7 +118,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/test/test_clipboard.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/native_theme/native_theme_features.h"
 #include "url/gurl.h"
 
 #if defined(TOOLKIT_VIEWS) && !BUILDFLAG(IS_MAC)
@@ -1408,7 +1407,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionScrollTest,
                                        /*command=*/false);
   ASSERT_NO_FATAL_FAILURE(scroll_waiter.Wait());
 #if BUILDFLAG(IS_WIN)
-  const int kFirstPosition = ui::IsFluentScrollbarEnabled() ? 917 : 915;
+  constexpr int kFirstPosition = 915;
 #elif BUILDFLAG(IS_CHROMEOS)
   constexpr int kFirstPosition = 937;
 #else
@@ -1427,7 +1426,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionScrollTest,
                                        /*command=*/false);
   ASSERT_NO_FATAL_FAILURE(scroll_waiter.Wait());
 #if BUILDFLAG(IS_WIN)
-  const int kSecondPosition = ui::IsFluentScrollbarEnabled() ? 1834 : 1831;
+  constexpr int kSecondPosition = 1831;
 #elif BUILDFLAG(IS_CHROMEOS)
   constexpr int kSecondPosition = 1875;
 #else
