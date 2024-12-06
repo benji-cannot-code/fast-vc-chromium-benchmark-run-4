1ecbb2c1103e91947ae4361b540f072b1ec62bbd
Andrew Grieve <agrieve@chromium.org>
2024-12-06 16:20:28 +0000

    JNI Zero: Add @Nullable support
    * Parsing of TYPE_USE @Nullable
    * Looking for @NullMarked
    * Adding for @NullUnmarked to outputs
    
    Bug: 40657579
    Change-Id: I52f24761de32158ab0427674449e730d8e037f91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073010
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392940}

---
+++ third_party/jni_zero/java/src/org/jni_zero/internal/Nullable.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.jni_zero.internal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** JNI Zero's @Nullable (to avoid a dep on jspecify). */
+@Target(ElementType.TYPE_USE)
+@Retention(RetentionPolicy.CLASS)
+public @interface Nullable {}
---
+++ third_party/jni_zero/test/java/src/org/jni_zero/NullMarked.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.jni_zero;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface NullMarked {}
