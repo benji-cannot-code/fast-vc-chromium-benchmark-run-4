77240bc36818b8f309782eb3a30eb06ac159fbbe
David Black <dmblack@google.com>
2024-12-06 19:45:47 +0000

    Tidy up holding space tray accessible name.
    This CL tidies up some recent changes to holding space tray accessible
    name and updates tests so that they test prod behavior and not test api
    behavior.
    
    Change-Id: Ic20d0a84c902d36d629d04d63f4c26c1177cd49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040005
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393076}

--- ash/system/holding_space/holding_space_tray.h
+++ ash/system/holding_space/holding_space_tray.h
@@ -114,8 +114,6 @@ class ASH_EXPORT HoldingSpaceTray : public TrayBackgroundView,
   }
 
  private:
-  friend class HoldingSpaceTestApi;
-
   // TrayBubbleView::Delegate:
   std::u16string GetAccessibleNameForBubble() override;
   bool ShouldEnableExtraKeyboardAccessibility() override;
