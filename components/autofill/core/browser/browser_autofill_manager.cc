05c7eb497db9e664ad08444ed66074590df6783b
Christoph Schwering <schwering@google.com>
2024-12-06 16:40:54 +0000

    [Autofill] Rename some functions, variables in VotesUploader
    This CL renames some variables and functions for more consistency
    in VotesUploader:
    
    - `OnSubmissionFieldTypesDetermined()` --> OnFieldTypesDetermined()`
      Reason: the meaning of "submission" is unclear, especially since
      the function is also called with `is_submission = false`.
    
    - `FlushPendingVotes()` --> FlushQueuedVotes()`
      Reason: avoid two concepts for the same thing; originally I had
      thought I'd need the term "pending" for asynchronous tasks.
    
    - `QueuedVote::callback` --> `QueuedVote::upload_vote`
      Reason: more descriptive.
    
    - `interaction_time` --> `initial_interaction_timestamp`
      `submittion_time` --> `submission_timestamp`
      Reason: consistency.
    
    - `source_id` --> `ukm_source_id`
      Reason: consistency.
    
    - `queued_vote_uploads_` --> `queued_votes_`
      Reason: consistency with the type name.
    
    - `form_structure` --> `form`
      Reason: reduce noise; there are no non-FormStructure forms in
      VotesUploader.
    
    Bug: 374086145
    Change-Id: I05e4454055509f802cbbccd6cf382e887bad5cbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074473
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392952}

--- components/autofill/core/browser/browser_autofill_manager.cc
+++ components/autofill/core/browser/browser_autofill_manager.cc
@@ -623,7 +623,7 @@ BrowserAutofillManager::~BrowserAutofillManager() {
   for (const auto& [form_id, form_structure] : form_structures()) {
     ProcessFieldLogEventsInForm(*form_structure);
   }
-  votes_uploader_->FlushPendingVotes();
+  votes_uploader_->FlushQueuedVotes();
 
   client().GetSingleFieldFillRouter().CancelPendingQueries();
 }
@@ -2193,7 +2193,7 @@ void BrowserAutofillManager::Reset() {
     ProcessFieldLogEventsInForm(*form_structure);
   }
   ProcessPendingFormForUpload();
-  votes_uploader_->FlushPendingVotes();
+  votes_uploader_->FlushQueuedVotes();
   DCHECK(!pending_form_data_);
 
   four_digit_combinations_in_dom_.clear();
