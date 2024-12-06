581138854f32cb243af79884eeaaf89f8b7192be
Anna Tsvirchkova <atsvirchkova@google.com>
2024-12-06 17:08:51 +0000

    [Sheriff] Disable flaky test
    UrlOverridingTest#testNavigationFromXHRCallbackAndShortTimeout is flaky
    on android-12l-x64-dbg-tests and a couple of table bots.
    
    Bug: 373333486, 382632437
    Change-Id: I799bf582aca059150e58bb6db9b9f8a8c65968bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6076170
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392976}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -825,7 +825,8 @@ public class UrlOverridingTest {
 
     @Test
     @SmallTest
-    @DisableIf.Build(sdk_equals = Build.VERSION_CODES.S, message = "crbug.com/373333486")
+    @DisableIf.Device(DeviceFormFactor.TABLET)
+    @DisableIf.Build(sdk_equals = Build.VERSION_CODES.S_V2, message = "crbug.com/373333486")
     public void testNavigationFromXHRCallbackAndShortTimeout() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
         loadUrlAndWaitForIntentUrl(
@@ -971,6 +972,8 @@ public class UrlOverridingTest {
 
     @Test
     @SmallTest
+    @DisableIf.Device(DeviceFormFactor.TABLET)
+    @DisableIf.Build(sdk_equals = Build.VERSION_CODES.S_V2, message = "crbug.com/382632437")
     public void testOpenWindowFromSvgUserGesture() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
         TestParams params =
