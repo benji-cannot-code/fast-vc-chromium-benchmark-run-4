0741d73fbf0583d1b5b555942ff4bda2b7b23775
Sylvain Defresne <sdefresne@chromium.org>
2024-12-06 09:40:57 +0000

    [ios] Convert IOSChromeTabRestoreServiceFactory to PKSFIOS
    Convert IOSChromeTabRestoreServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217877
    Change-Id: Ic096ae73535d19b4f6064aed91e706a8b2982ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070386
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392773}

--- ios/chrome/browser/sessions/model/ios_chrome_tab_restore_service_factory.h
+++ ios/chrome/browser/sessions/model/ios_chrome_tab_restore_service_factory.h
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SESSIONS_MODEL_IOS_CHROME_TAB_RESTORE_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_SESSIONS_MODEL_IOS_CHROME_TAB_RESTORE_SERVICE_FACTORY_H_
 
-#import <memory>
+#include <memory>
 
-#import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "base/no_destructor.h"
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 
@@ -19,8 +19,7 @@ class TabRestoreService;
 
 // Singleton that owns all TabRestoreServices and associates them with
 // ProfileIOS.
-class IOSChromeTabRestoreServiceFactory
-    : public BrowserStateKeyedServiceFactory {
+class IOSChromeTabRestoreServiceFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static sessions::TabRestoreService* GetForProfile(ProfileIOS* profile);
   static IOSChromeTabRestoreServiceFactory* GetInstance();
@@ -28,11 +27,6 @@ class IOSChromeTabRestoreServiceFactory
   // Returns the default factory used to build TabRestoreServices. Can be
   // registered with SetTestingFactory to use real instances during testing.
   static TestingFactory GetDefaultFactory();
-
-  IOSChromeTabRestoreServiceFactory(const IOSChromeTabRestoreServiceFactory&) =
-      delete;
-  IOSChromeTabRestoreServiceFactory& operator=(
-      const IOSChromeTabRestoreServiceFactory&) = delete;
 
  private:
   friend class base::NoDestructor<IOSChromeTabRestoreServiceFactory>;
@@ -43,7 +37,6 @@ class IOSChromeTabRestoreServiceFactory
   // BrowserStateKeyedServiceFactory:
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* context) const override;
-  bool ServiceIsNULLWhileTesting() const override;
 };
 
 #endif  // IOS_CHROME_BROWSER_SESSIONS_MODEL_IOS_CHROME_TAB_RESTORE_SERVICE_FACTORY_H_
