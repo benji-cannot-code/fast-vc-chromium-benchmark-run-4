50792c9e22c659859150aede7f854ca30efd8962
Vidhan Jain <vidhanj@google.com>
2024-12-06 12:49:05 +0000

    Move remaining strings from the finch config
    Bug: 382209806
    Change-Id: Ie2f1b595686cd983b2a0a9048bf6e17c37e79437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074537
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392841}

--- chrome/browser/ui/webui/settings/shared_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/shared_settings_localized_strings_provider.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/google/core/common/google_util.h"
 #include "components/live_caption/caption_util.h"
+#include "components/plus_addresses/features.h"
+#include "components/plus_addresses/grit/plus_addresses_strings.h"
 #include "components/soda/constants.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/sync/base/features.h"
@@ -242,11 +244,14 @@ void AddSharedSyncPageStrings(content::WebUIDataSource* html_source) {
   html_source->AddString(
       "encryptWithSyncPassphraseLabel",
       l10n_util::GetStringFUTF8(
-          IDS_SETTINGS_ENCRYPT_WITH_SYNC_PASSPHRASE_LABEL,
+          base::FeatureList::IsEnabled(
+              plus_addresses::features::kPlusAddressesEnabled)
+              ? IDS_SETTINGS_ENCRYPT_WITH_SYNC_PASSPHRASE_INCLUDING_PLUS_ADDRESS_LABEL
+              : IDS_SETTINGS_ENCRYPT_WITH_SYNC_PASSPHRASE_LABEL,
 #if BUILDFLAG(IS_CHROMEOS_ASH)
           GetHelpUrlWithBoard(chrome::kSyncEncryptionHelpURL)));
 #else
-                              chrome::kSyncEncryptionHelpURL));
+          chrome::kSyncEncryptionHelpURL));
 #endif
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   html_source->AddBoolean(
