96eb1563ba02231809c5e8aded4be129e14d7c61
Xi Han <hanxi@google.com>
2024-12-06 21:40:55 +0000

    [AppIntegration] Save user's response once the setting is changed.
    Currently, we only save the user's response when the user interacts
    with the opt-in card on the magic stack. However, if the user changed
    the setting of "sharing tabs with OS" without clicking the opt-in
    card, the opt-in card with old content of the default enabled state
    will still shown.
    
    In this CL, we save the user's response in the SharedPreference once
    the setting of "sharing tabs with OS" is changed. This prevents
    showing the opt-in card on the magic stack after the default setting
    is changed.
    
    Bug: 380086456
    Change-Id: Ia387e16a26dbc48262957bb5d99576260a6d7568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072959
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393162}

--- chrome/browser/auxiliary_search/junit/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchConfigManagerUnitTest.java
+++ chrome/browser/auxiliary_search/junit/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchConfigManagerUnitTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.auxiliary_search;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
@@ -19,8 +20,11 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import org.chromium.base.shared_preferences.SharedPreferencesManager;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.auxiliary_search.AuxiliarySearchConfigManager.ShareTabsWithOsStateListener;
+import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
+import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
 
 /** Unit tests for {@link AuxiliarySearchConfigManager}. */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -46,9 +50,16 @@ public class AuxiliarySearchConfigManagerUnitTest {
         assertEquals(1, mConfigManager.getObserverListSizeForTesting());
 
         mConfigManager.notifyShareTabsStateChanged(true);
+        SharedPreferencesManager prefManager = ChromeSharedPreferences.getInstance();
+        assertTrue(
+                prefManager.readBoolean(
+                        ChromePreferenceKeys.AUXILIARY_SEARCH_MODULE_USER_RESPONDED, false));
+
         verify(mListener).onConfigChanged(eq(true));
 
         mConfigManager.removeListener(mListener);
         assertEquals(0, mConfigManager.getObserverListSizeForTesting());
+
+        prefManager.removeKey(ChromePreferenceKeys.AUXILIARY_SEARCH_MODULE_USER_RESPONDED);
     }
 }
