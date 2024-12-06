9da7f8845ebcf13a7c582caff9451c9bde647ef8
Charles Meng <charlesmeng@google.com>
2024-12-06 20:36:00 +0000

    Add test for glic status tray icon existing
    Augmented the list of status icons in the status tray with their types
    so that tests can query if a status icon of a particular type exists.
    Added a test for checking the presence of the glic status tray icon.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE changed the way somethings are accessed in status_tray_win.cc
    Bug: 378139555
    Change-Id: I09457fc2891c7549a04ccb9aa832b74307755dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6071667
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393115}

--- chrome/browser/glic/launcher/glic_background_mode_manager_browsertest.cc
+++ chrome/browser/glic/launcher/glic_background_mode_manager_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/status_icons/status_tray.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -30,7 +31,7 @@ class GlicBackgroundModeManagerBrowserTest : public InProcessBrowserTest {
   base::test::ScopedFeatureList feature_list_;
 };
 
-// Checks that modifying the pref propagates to KeepAliveRegistry
+// Checks that modifying the pref propagates to KeepAliveRegistry.
 IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest, KeepAlive) {
   auto* keep_alive_registry = KeepAliveRegistry::GetInstance();
   ASSERT_FALSE(
@@ -38,13 +39,29 @@ IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest, KeepAlive) {
 
   g_browser_process->local_state()->SetBoolean(prefs::kGlicLauncherEnabled,
                                                true);
-  EXPECT_EQ(true, keep_alive_registry->IsOriginRegistered(
-                      KeepAliveOrigin::GLIC_LAUNCHER));
+  EXPECT_TRUE(
+      keep_alive_registry->IsOriginRegistered(KeepAliveOrigin::GLIC_LAUNCHER));
 
   g_browser_process->local_state()->SetBoolean(prefs::kGlicLauncherEnabled,
                                                false);
-  EXPECT_EQ(false, keep_alive_registry->IsOriginRegistered(
-                       KeepAliveOrigin::GLIC_LAUNCHER));
+  EXPECT_FALSE(
+      keep_alive_registry->IsOriginRegistered(KeepAliveOrigin::GLIC_LAUNCHER));
+}
+
+// Checks that the status icon exists when the pref is enabled.
+IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest, StatusIcon) {
+  ASSERT_FALSE(g_browser_process->status_tray()->HasStatusIconOfTypeForTesting(
+      StatusTray::StatusIconType::GLIC_ICON));
+
+  g_browser_process->local_state()->SetBoolean(prefs::kGlicLauncherEnabled,
+                                               true);
+  EXPECT_TRUE(g_browser_process->status_tray()->HasStatusIconOfTypeForTesting(
+      StatusTray::StatusIconType::GLIC_ICON));
+
+  g_browser_process->local_state()->SetBoolean(prefs::kGlicLauncherEnabled,
+                                               false);
+  EXPECT_FALSE(g_browser_process->status_tray()->HasStatusIconOfTypeForTesting(
+      StatusTray::StatusIconType::GLIC_ICON));
 }
 
 }  // namespace
--- chrome/browser/status_icons/status_tray.cc
+++ chrome/browser/status_icons/status_tray.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/status_icons/status_tray.h"
 
+#include <memory>
+#include <utility>
+
+#include "base/ranges/algorithm.h"
 #include "chrome/browser/status_icons/status_icon.h"
 
 StatusTray::~StatusTray() {
@@ -17,13 +21,13 @@ StatusIcon* StatusTray::CreateStatusIcon(StatusIconType type,
   if (!icon)
     return nullptr;
 
-  status_icons_.push_back(std::move(icon));
-  return status_icons_.back().get();
+  status_icons_.emplace_back(std::move(icon), type);
+  return status_icons_.back().icon.get();
 }
 
 void StatusTray::RemoveStatusIcon(StatusIcon* icon) {
   for (auto iter = status_icons_.begin(); iter != status_icons_.end(); ++iter) {
-    if (iter->get() == icon) {
+    if (iter->icon.get() == icon) {
       status_icons_.erase(iter);
       return;
     }
@@ -31,5 +35,21 @@ void StatusTray::RemoveStatusIcon(StatusIcon* icon) {
   NOTREACHED();
 }
 
-StatusTray::StatusTray() {
+bool StatusTray::HasStatusIconOfTypeForTesting(StatusIconType type) const {
+  return base::ranges::any_of(status_icons_,
+                              [type](const StatusIconWithType& status_icon) {
+                                return status_icon.type == type;
+                              });
 }
+
+StatusTray::StatusIconWithType::StatusIconWithType(
+    std::unique_ptr<StatusIcon> status_icon,
+    StatusIconType status_icon_type)
+    : icon(std::move(status_icon)), type(status_icon_type) {}
+StatusTray::StatusIconWithType::StatusIconWithType(
+    StatusIconWithType&& other) noexcept = default;
+StatusTray::StatusIconWithType& StatusTray::StatusIconWithType::operator=(
+    StatusIconWithType&& other) noexcept = default;
+StatusTray::StatusIconWithType::~StatusIconWithType() = default;
+
+StatusTray::StatusTray() = default;
