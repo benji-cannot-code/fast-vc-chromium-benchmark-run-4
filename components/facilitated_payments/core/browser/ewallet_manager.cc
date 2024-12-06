92b5551006cd65343ed41eaac451146bee0a551a
Qihui Zhao <qihuizhao@google.com>
2024-12-06 17:16:03 +0000

    Refactor PaymentLinkValidator::IsValid to GetScheme
    This CL does the followings:
    1. Add enum Scheme to return scheme of the given payment url.
    2. Refactor PaymentLinkValidator::IsValid into
    PaymentLinkValidator::GetScheme to get the Scheme.
    
    Design Doc: go/ewallet-in-chrome-metrics
    
    Bug: 40280186
    Change-Id: I2e7d64e9886e4cfd2f735821ec0ad1300e7bcfa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6065363
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392980}

--- components/facilitated_payments/core/browser/ewallet_manager.cc
+++ components/facilitated_payments/core/browser/ewallet_manager.cc
@@ -49,7 +49,8 @@ void EwalletManager::TriggerEwalletPushPayment(const GURL& payment_link_url,
     return;
   }
 
-  if (!PaymentLinkValidator().IsValid(payment_link_url.spec())) {
+  if (PaymentLinkValidator().GetScheme(payment_link_url) ==
+      PaymentLinkValidator::Scheme::kInvalid) {
     return;
   }
 
