74c9b9a2c974348b850fbc7c40fbc726762b76d7
Federica Germinario <fedegermi@google.com>
2024-12-06 14:36:01 +0000

    Use ProfileKeyedServiceFactoryIOS in ProviderStateServiceFactory
    Convert ProviderStateServiceFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215060
    Change-Id: I56d822129647c48bf6fe196249a7aea83c554346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074642
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392878}

--- ios/chrome/browser/autocomplete/model/provider_state_service_factory.mm
+++ ios/chrome/browser/autocomplete/model/provider_state_service_factory.mm
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/autocomplete/model/provider_state_service_factory.h"
 
-#import <memory>
-
-#import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/omnibox/browser/provider_state_service.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/web/public/browser_state.h"
@@ -18,8 +14,8 @@ namespace ios {
 // static
 ProviderStateService* ProviderStateServiceFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<ProviderStateService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<ProviderStateService>(
+      profile, /*create=*/true);
 }
 
 // static
@@ -29,9 +25,7 @@ ProviderStateServiceFactory* ProviderStateServiceFactory::GetInstance() {
 }
 
 ProviderStateServiceFactory::ProviderStateServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "ProviderStateService",
-          BrowserStateDependencyManager::GetInstance()) {}
+    : ProfileKeyedServiceFactoryIOS("ProviderStateService") {}
 
 ProviderStateServiceFactory::~ProviderStateServiceFactory() = default;
 
