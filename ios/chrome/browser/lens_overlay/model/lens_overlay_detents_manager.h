8731293404609725ac64e0c69b47b60bb4146747
Radu Nitescu <radunitescu@google.com>
2024-12-06 15:16:24 +0000

    [iOS][LO] Decouple and group UI under a single target
    Model code should be independent of the UI. This CL removes the
    dependencies to `lens_overlay/ui/` code and groups all ui related
    targets under a single proxy target.
    
    Bug: None
    Change-Id: Ic80df490bf852d5f1a7727c0453d46cc5ad8ae06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074531
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392893}

--- ios/chrome/browser/lens_overlay/model/lens_overlay_detents_manager.h
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_detents_manager.h
@@ -8,13 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_sheet_detent_state.h"
-#import "ios/chrome/browser/lens_overlay/ui/lens_overlay_bottom_sheet_presentation_delegate.h"
 
 @protocol LensOverlayDetentsChangeObserver;
 
 // Manages the detents for a given bottom sheet.
-@interface LensOverlayDetentsManager
-    : NSObject <LensOverlayBottomSheetPresentationDelegate>
+@interface LensOverlayDetentsManager : NSObject
 
 @property(nonatomic, weak) id<LensOverlayDetentsChangeObserver> observer;
 
