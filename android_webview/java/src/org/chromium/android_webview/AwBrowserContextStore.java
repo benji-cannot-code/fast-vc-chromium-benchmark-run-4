b492ed82e4ca1ec1a8449b96639a58de68b653ea
Wayne Jackson Jr. <wbjacksonjr@google.com>
2024-12-06 12:43:02 +0000

    [webview][tracing] Add Tracing to ProfileStore & Internals
    Bug: 363946909
    Change-Id: I45ac213cca6cbca9a16bc8b2a3bf2d0fb9971646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6069934
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392840}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserContextStore.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserContextStore.java
@@ -9,6 +9,7 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.android_webview.common.Lifetime;
+import org.chromium.base.TraceEvent;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,9 @@ public class AwBrowserContextStore {
      * <p>Name must be non-null and valid Unicode.
      */
     public static AwBrowserContext getNamedContext(String name, boolean createIfNeeded) {
-        return AwBrowserContextStoreJni.get().getNamedContextJava(name, createIfNeeded);
+        try (TraceEvent event = TraceEvent.scoped("WebView.ProfileStore.GET_NAMED_CONTEXT")) {
+            return AwBrowserContextStoreJni.get().getNamedContextJava(name, createIfNeeded);
+        }
     }
 
     /**
@@ -50,16 +53,20 @@ public class AwBrowserContextStore {
      */
     public static boolean deleteNamedContext(String name)
             throws IllegalArgumentException, IllegalStateException {
-        final String defaultContextName = AwBrowserContextJni.get().getDefaultContextName();
-        if (name.equals(defaultContextName)) {
-            throw new IllegalArgumentException("Cannot delete the default profile");
+        try (TraceEvent event = TraceEvent.scoped("WebView.ProfileStore.DELETE_NAMED_CONTEXT")) {
+            final String defaultContextName = AwBrowserContextJni.get().getDefaultContextName();
+            if (name.equals(defaultContextName)) {
+                throw new IllegalArgumentException("Cannot delete the default profile");
+            }
+            return AwBrowserContextStoreJni.get().deleteNamedContext(name);
         }
-        return AwBrowserContextStoreJni.get().deleteNamedContext(name);
     }
 
     /** List all contexts. */
     public static List<String> listAllContexts() {
-        return Arrays.asList(AwBrowserContextStoreJni.get().listAllContexts());
+        try (TraceEvent event = TraceEvent.scoped("WebView.ProfileStore.LIST_ALL_CONTEXTS")) {
+            return Arrays.asList(AwBrowserContextStoreJni.get().listAllContexts());
+        }
     }
 
     /**
@@ -68,7 +75,10 @@ public class AwBrowserContextStore {
      * <p>Name must be non-null and valid Unicode.
      */
     public static boolean checkNamedContextExists(String name) {
-        return AwBrowserContextStoreJni.get().checkNamedContextExists(name);
+        try (TraceEvent event =
+                TraceEvent.scoped("WebView.ProfileStore.CHECKED_NAMED_CONTEXT_EXISTS")) {
+            return AwBrowserContextStoreJni.get().checkNamedContextExists(name);
+        }
     }
 
     @NativeMethods
