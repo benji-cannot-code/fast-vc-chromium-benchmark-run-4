104d32982d67918215ff55fca4a9b128a47019f3
Igor Ruvinov <igorruvinov@chromium.org>
2024-12-06 21:51:04 +0000

    Convert CallbackListSubscription in CloudApProviderWin to a vector.
    When multiple tabs which require SSO open simultaneously, there is a
    chance that CloudApProviderWin receives multiple SSO auth data fetch
    requests before it has had a chance to finish fetching data.
    
    Before this change, if a new callback was added to GetDataCallbackList
    when there was already an existing callback, the previous callback's
    CallbackListSubscription was replaced with the subscription from the
    current callback. The destruction of the previous callback's
    subscription resulted in that callback being cancelled and never
    receiving the SSO auth data. In practice, that tab would hang because
    its navigation throttle would never resume the deferred navigation.
    
    Placing the subscriptions in a vector eliminates the overriding of
    subscriptions and allows multiple callbacks to be run with the SSO auth
    data.
    
    Bug: 350320168, 40934568
    Change-Id: I2b7aa9d255cdc53367db921db296fbe51ae69547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6077789
    Auto-Submit: Igor Ruvinov <igorruvinov@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393168}

--- chrome/browser/enterprise/platform_auth/cloud_ap_provider_win.h
+++ chrome/browser/enterprise/platform_auth/cloud_ap_provider_win.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ENTERPRISE_PLATFORM_AUTH_CLOUD_AP_PROVIDER_WIN_H_
 
 #include <optional>
+#include <vector>
 
 #include "base/callback_list.h"
 #include "base/gtest_prod_util.h"
@@ -69,9 +70,9 @@ class CloudApProviderWin : public PlatformAuthProvider {
       base::OnceCallbackList<void(net::HttpRequestHeaders)>;
   GetDataCallbackList on_get_data_callback_list_;
 
-  // Subscription for auth data requests. Guarantees that the corresponding
+  // Subscriptions for auth data requests. Guarantees that the corresponding
   // callbacks are run on destruction.
-  base::CallbackListSubscription get_data_subscription_;
+  std::vector<base::CallbackListSubscription> get_data_subscriptions_;
 };
 
 }  // namespace enterprise_auth
