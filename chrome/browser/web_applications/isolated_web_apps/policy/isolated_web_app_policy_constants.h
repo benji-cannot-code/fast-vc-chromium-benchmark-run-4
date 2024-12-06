11f8809822f71a786176b568ba0f0ac7d847b492
Magdalena Skarbinska <mskarbinska@google.com>
2024-12-06 12:19:34 +0000

    iwa: Add allow_downgrades to IWA policy
    This change introduces allow_downgrades field to IsolatedWebAppInstallForceList policy. It is a prerequisite for downgrade feature for IWAs, in which it is possible to update an app to a lower version (explicitly specified by admin).
    
    Bug: b:365048785
    Change-Id: I5ff2fb4ca6673e53e81484b45d32663640820bec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032616
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392834}

--- chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_constants.h
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_constants.h
@@ -12,6 +12,7 @@ namespace web_app {
 extern const char kPolicyUpdateManifestUrlKey[];
 extern const char kPolicyWebBundleIdKey[];
 extern const char kPolicyUpdateChannelKey[];
+extern const char kPolicyAllowDowngradesKey[];
 extern const char kPolicyPinnedVersionKey[];
 
 }  // namespace web_app
