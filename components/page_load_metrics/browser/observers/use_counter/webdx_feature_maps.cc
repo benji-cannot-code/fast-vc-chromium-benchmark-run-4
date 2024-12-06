59e2cab662a1a19f5bd205324eb71e2b13876636
Joey Arhar <jarhar@chromium.org>
2024-12-06 23:08:43 +0000

    Add WebDXFeature for CloseWatcher
    https://web-platform-dx.github.io/web-features-explorer/features/closewatcher/
    
    Change-Id: I83f2556cee77fa4cf9e8277af1628ce5864b5096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072053
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393211}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -57,6 +57,8 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kElementCheckVisibility,
            WebDXFeature::kElementCheckVisibility},
           {WebFeature::kHTMLUnsafeMethods, WebDXFeature::kParseHtmlUnsafe},
+          {WebFeature::kCloseWatcherScriptConstructor,
+           WebDXFeature::kClosewatcher},
           {WebFeature::kHTMLSearchElement, WebDXFeature::kSearch},
           {WebFeature::kDialogElement, WebDXFeature::kDialog},
           {WebFeature::kV8DocumentPictureInPicture_RequestWindow_Method,
