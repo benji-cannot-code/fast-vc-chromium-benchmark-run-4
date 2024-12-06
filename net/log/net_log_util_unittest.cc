95c2c83309fd3b8a9c16459864fd1dc863fe830f
Kenichi Ishibashi <bashi@chromium.org>
2024-12-06 06:48:49 +0000

    netlog: Include OK in netError constants
    This is a preparation for updating the NetLog viewer. Usually zero
    value of "net_error" represents OK. Add "OK" to the constants.
    
    Bug: 346835898
    Change-Id: I898c588ea833e98f77687ec7b082754b2c65b611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6065838
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392731}

--- net/log/net_log_util_unittest.cc
+++ net/log/net_log_util_unittest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_test_util.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace net {
@@ -41,6 +42,13 @@ namespace {
 // Make sure GetNetConstants doesn't crash.
 TEST(NetLogUtil, GetNetConstants) {
   base::Value constants(GetNetConstants());
+}
+
+TEST(NetLogUtil, GetNetConstantsOkInNetError) {
+  base::Value constants(GetNetConstants());
+  std::optional<int> ok_value =
+      constants.GetDict().FindDict("netError")->FindInt("net::OK");
+  EXPECT_THAT(ok_value, testing::Optional(0));
 }
 
 // Make sure GetNetInfo doesn't crash when called on contexts with and without
