66fca39748178c60bd505da9ebda354a9f2542d9
Sylvain Defresne <sdefresne@chromium.org>
2024-12-06 17:35:09 +0000

    [ios] Convert MailtoHandlerServiceFactory to PKSFIOS
    Convert MailtoHandlerServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363215631
    Change-Id: I7e8058d0a120e0e2eb57d29f8a859a43b84c3ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074725
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392987}

--- ios/chrome/browser/mailto_handler/model/mailto_handler_service_factory.h
+++ ios/chrome/browser/mailto_handler/model/mailto_handler_service_factory.h
@@ -9,14 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class MailtoHandlerService;
-class ProfileIOS;
 
 // Singleton that owns all MailtoHandlerServices and associates them with
 // profiles.
-class MailtoHandlerServiceFactory : public BrowserStateKeyedServiceFactory {
+class MailtoHandlerServiceFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static MailtoHandlerService* GetForProfile(ProfileIOS* profile);
   static MailtoHandlerServiceFactory* GetInstance();
@@ -29,8 +28,6 @@ class MailtoHandlerServiceFactory : public BrowserStateKeyedServiceFactory {
 
   // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
--- ios/chrome/browser/mailto_handler/model/mailto_handler_service_factory.mm
+++ ios/chrome/browser/mailto_handler/model/mailto_handler_service_factory.mm
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/mailto_handler/model/mailto_handler_service_factory.h"
 
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "ios/chrome/browser/mailto_handler/model/mailto_handler_configuration.h"
 #import "ios/chrome/browser/mailto_handler/model/mailto_handler_service.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
-#import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
@@ -18,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // static
 MailtoHandlerService* MailtoHandlerServiceFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<MailtoHandlerService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<MailtoHandlerService>(
+      profile, /*create=*/true);
 }
 
 // static
@@ -29,9 +27,8 @@ MailtoHandlerServiceFactory* MailtoHandlerServiceFactory::GetInstance() {
 }
 
 MailtoHandlerServiceFactory::MailtoHandlerServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "MailtoHandlerService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("MailtoHandlerService",
+                                    ProfileSelection::kRedirectedInIncognito) {
   DependsOn(ChromeAccountManagerServiceFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
 }
@@ -55,9 +52,4 @@ MailtoHandlerServiceFactory::BuildServiceInstanceFor(
   config.singleSignOnService = application_context->GetSingleSignOnService();
 
   return ios::provider::CreateMailtoHandlerService(config);
-}
-
-web::BrowserState* MailtoHandlerServiceFactory::GetBrowserStateToUse(
-    web::BrowserState* context) const {
-  return GetBrowserStateRedirectedInIncognito(context);
 }
