78b8e056dbfa39cf9dd0956f87dee9851686873d
Christoph Schwering <schwering@google.com>
2024-12-06 20:43:51 +0000

    [Autofill] Send votes on driver state change
    This CL emits votes cast by a BAM when the BAM's driver's LifecycleState
    changes. There are three reasons for this change.
    
    Firstly, the CL ensures that destroying or resetting one BAM does not
    flush the votes of another BAM. This used to be guaranteed, but we lost
    the guarantee by moving VotesUploader to AutofillClient in the dependent
    CL crrev.com/c/6049265.
    
    Secondly, the CL flushes votes already when a frame becomes inactive,
    i.e., enters the BFcache. This part of the CL is behind a Finch flag.
    Before this CL, when a bfcached frame stayed around until the
    WebContents was destroyed, no votes could be sent because the
    AutofillCrowdsourcingManager would be destroyed with the WebContents.
    
    Thirdly, the dependency on AutofillDriver instead of BAM is another
    small step towards making BAM per-tab.
    
    Bug: 374086145, 40284887
    Change-Id: I875dc91f2489d7370eefe04e9869bc6e0f248f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6061418
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393122}

--- components/autofill/core/browser/browser_autofill_manager.cc
+++ components/autofill/core/browser/browser_autofill_manager.cc
@@ -623,8 +623,6 @@ BrowserAutofillManager::~BrowserAutofillManager() {
   for (const auto& [form_id, form_structure] : form_structures()) {
     ProcessFieldLogEventsInForm(*form_structure);
   }
-  client().GetVotesUploader().FlushQueuedVotes();
-
   client().GetSingleFieldFillRouter().CancelPendingQueries();
 }
 
@@ -2216,7 +2214,6 @@ void BrowserAutofillManager::Reset() {
     ProcessFieldLogEventsInForm(*form_structure);
   }
   ProcessPendingFormForUpload();
-  client().GetVotesUploader().FlushQueuedVotes();
   DCHECK(!pending_form_data_);
 
   four_digit_combinations_in_dom_.clear();
