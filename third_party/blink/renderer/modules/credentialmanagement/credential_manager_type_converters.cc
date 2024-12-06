ef7f1bebd5c6fdbacf660cdcf7e7945d21d5423e
Slobodan Pejic <slobodan@chromium.org>
2024-12-06 16:50:56 +0000

    Reland "Sign SPC payment assertions with a browser bound key."
    This is a reland of commit 3eb0d07ac739ff3d54caab7f68089b95504ef82b with
    tests restricted to devices with StrongBox API support.
    
    Original change's description:
    > Before this change invoking secure payment confirmation invoke the
    > internal authenticator with payment options and no browser bound
    > signature was returned. After this change invoking secure payment
    > confirmation creates a browser bound key pair and signs over
    > the client data.
    >
    > This change is behind flag
    > chrome://flags/#enable-secure-payment-confirmation-browser-bound-key
    >
    >  BrowserBoundKeyStoreTest javatests are not taken into account.
    >
    > Low-Coverage-Reason: COVERAGE_UNDERREPORTED The
    > Bug: 377278827, b:378707928
    > Change-Id: I5c58f870567ede4dddab302761e9b755fbb0ed5b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001035
    > Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    > Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    > Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1391872}
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED The BrowserBoundKeyStoreTest javatests are not taken into account.
    Bug: 377278827, b:378707928
    Change-Id: I816950c875bb50a4c3e5e06eee0a1c6424290ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075266
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392961}

--- third_party/blink/renderer/modules/credentialmanagement/credential_manager_type_converters.cc
+++ third_party/blink/renderer/modules/credentialmanagement/credential_manager_type_converters.cc
@@ -20,7 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_client_outputs.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_large_blob_inputs.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_large_blob_outputs.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_payment_browser_bound_signature.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_payment_inputs.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_payment_outputs.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_prf_inputs.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_prf_outputs.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_authentication_extensions_prf_values.h"
@@ -247,6 +249,11 @@ TypeConverter<blink::AuthenticationExtensionsClientOutputs*,
         ConvertTo<blink::AuthenticationExtensionsSupplementalPubKeysOutputs*>(
             extensions->supplemental_pub_keys));
   }
+  if (extensions->payment) {
+    extension_outputs->setPayment(
+        ConvertTo<blink::AuthenticationExtensionsPaymentOutputs*>(
+            extensions->payment));
+  }
   if (extensions->echo_prf) {
     auto* prf_outputs = blink::AuthenticationExtensionsPRFOutputs::Create();
     if (extensions->prf_results) {
@@ -283,6 +290,33 @@ TypeConverter<blink::AuthenticationExtensionsSupplementalPubKeysOutputs*,
       blink::AuthenticationExtensionsSupplementalPubKeysOutputs::Create();
   spk_outputs->setSignatures(std::move(signatures));
   return spk_outputs;
+}
+
+// static
+blink::AuthenticationExtensionsPaymentOutputs*
+TypeConverter<blink::AuthenticationExtensionsPaymentOutputs*,
+              blink::mojom::blink::AuthenticationExtensionsPaymentResponsePtr>::
+    Convert(
+        const blink::mojom::blink::AuthenticationExtensionsPaymentResponsePtr&
+            payment_response) {
+  auto* payment_outputs =
+      blink::AuthenticationExtensionsPaymentOutputs::Create();
+  if (!payment_response->browser_bound_signatures.empty()) {
+    blink::HeapVector<blink::Member<
+        blink::AuthenticationExtensionsPaymentBrowserBoundSignature>>
+        signatures;
+    signatures.reserve(payment_response->browser_bound_signatures.size());
+    for (const auto& mojo_signature :
+         payment_response->browser_bound_signatures) {
+      auto* browser_bound_signature =
+          blink::AuthenticationExtensionsPaymentBrowserBoundSignature::Create();
+      browser_bound_signature->setSignatureOutput(
+          blink::DOMArrayBuffer::Create(std::move(mojo_signature)));
+      signatures.push_back(std::move(browser_bound_signature));
+    }
+    payment_outputs->setBrowserBoundSignatures(signatures);
+  }
+  return payment_outputs;
 }
 
 // static
