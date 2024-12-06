eb5b151072934971b6ff87eca4a01e33a5dd4e0b
caott <caott@google.com>
2024-12-06 22:21:40 +0000

    Check if browser locked fullscreen when handling press and hold esc.
    This fixes a regression with the "press and hold Esc" feature and forces it to check for locked fullscreen to prevent users from exiting this mode.
    
    Bug: b:382339104
    Change-Id: I2ced8cad0cfee90169b3e9c1f747e84dfa84d604
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073474
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Luke Cao <caott@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393187}

--- chrome/browser/ui/exclusive_access/fullscreen_controller_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller_browsertest.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "ash/wm/window_pin_util.h"
+#endif
+
 using content::WebContents;
 using ui::PAGE_TRANSITION_TYPED;
 using FullscreenControllerTest = ExclusiveAccessTest;
@@ -561,6 +565,35 @@ IN_PROC_BROWSER_TEST_F(FullscreenControllerPressAndHoldEscTest,
   WaitAndVerifyFullscreenState(/*browser_fullscreen=*/false,
                                /*tab_fullscreen=*/false);
 }
+
+#if BUILDFLAG(IS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(FullscreenControllerPressAndHoldEscTest,
+                       NotExitBrowserLockedFullscreenOnPressEsc) {
+  // Enter browser locked fullscreen.
+  PinWindow(browser()->window()->GetNativeWindow(), /*trusted=*/true);
+  ASSERT_FALSE(IsWindowFullscreenForTabOrPending());
+
+  // Short-press Esc key won't exit browser locked fullscreen.
+  SendEscapeToExclusiveAccessManager(/*is_key_down=*/true);
+  SendEscapeToExclusiveAccessManager(/*is_key_down=*/false);
+  EXPECT_TRUE(IsFullscreenForBrowser());
+}
+
+IN_PROC_BROWSER_TEST_F(FullscreenControllerPressAndHoldEscTest,
+                       NotExitBrowserLockedFullscreenOnPressAndHoldEsc) {
+  // Enter browser locked fullscreen.
+  PinWindow(browser()->window()->GetNativeWindow(), /*trusted=*/true);
+  ASSERT_FALSE(IsWindowFullscreenForTabOrPending());
+
+  // Press-and-hold Esc will not exit browser locked fullscreen.
+  {
+    base::TestMockTimeTaskRunner::ScopedContext scoped_context(task_runner());
+    SendEscapeToExclusiveAccessManager(/*is_key_down=*/true);
+    task_runner()->FastForwardBy(base::Seconds(2));
+  }
+  EXPECT_TRUE(IsFullscreenForBrowser());
+}
+#endif
 
 IN_PROC_BROWSER_TEST_F(FullscreenControllerPressAndHoldEscTest,
                        ExitBrowserFullscreenOnMultipleEscKeyDown) {
