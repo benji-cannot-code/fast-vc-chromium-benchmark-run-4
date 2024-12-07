181aa5c17f7a698d424f9c8f57121e3241f79764
Dustin J. Mitchell <djmitche@chromium.org>
2024-12-07 02:02:23 +0000

    Roll libc: 0.2.164 => 0.2.167 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * libc: 0.2.164 => 0.2.167
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * libc@0.2.167: crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: Ifec5a2e321b0538848499988a2e499f48473ada1
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072828
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393270}

--- third_party/rust/chromium_crates_io/vendor/libc-0.2.164/src/unix/linux_like/linux/musl/b32/x86/align.rs
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-s_no_extra_traits! {
-    #[allow(missing_debug_implementations)]
-    #[repr(align(8))]
-    pub struct max_align_t {
-        priv_: [f64; 3]
-    }
-}
---
+++ third_party/rust/chromium_crates_io/vendor/libc-0.2.167/tests/const_fn.rs
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#[cfg(target_os = "linux")]
+const _FOO: libc::c_uint = unsafe { libc::CMSG_SPACE(1) };
+//^ if CMSG_SPACE is not const, this will fail to compile
