af5732092a2e73a22f7d8de5d588b9a7a7c3fc5b
Wan-Teh Chang <wtc@google.com>
2024-12-07 01:03:59 +0000

    Roll src/third_party/dav1d/libdav1d/ f8d2620d8..d242c47b4 (4 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/f8d2620d82dc..d242c47b437c
    
    $ git log f8d2620d8..d242c47b4 --date=short --no-merges --format='%ad %ae %s'
    2024-12-02 ltrudeau Replace Av1Block with pal_sz in read_pal_indices
    2024-12-02 gramner Explicitly use uint8_t for the order_palette() scratch buffer
    2024-11-28 victorien flush: Reset f->task_thread.error
    2024-11-26 wtc Fix ClangTidy misc-include-cleaner warnings
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=eugene@chromium.org
    
    Change-Id: I10d3a82785d932567981d3a7a9e85c2360fcecbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074233
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393252}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.0-42-gf8d2620"
+#define DAV1D_VERSION "1.5.0-46-gd242c47"
