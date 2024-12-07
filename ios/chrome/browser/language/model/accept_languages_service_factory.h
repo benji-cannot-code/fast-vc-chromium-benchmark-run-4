dec5c86efadf04b8021fe9991394b8889782a05f
Federica Germinario <fedegermi@google.com>
2024-12-07 01:59:26 +0000

    Use ProfileKeyedServiceFactoryIOS in AcceptLanguagesServiceFactory
    Convert AcceptLanguagesServiceFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215062
    Change-Id: I9f855f2304cbc589dbb729a64ec912987f06ec8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074881
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393269}

--- ios/chrome/browser/language/model/accept_languages_service_factory.h
+++ ios/chrome/browser/language/model/accept_languages_service_factory.h
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_LANGUAGE_MODEL_ACCEPT_LANGUAGES_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_LANGUAGE_MODEL_ACCEPT_LANGUAGES_SERVICE_FACTORY_H_
 
-#import <memory>
-
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 
@@ -19,14 +17,10 @@ class AcceptLanguagesService;
 
 // AcceptLanguagesServiceFactory is a way to associate an
 // AcceptLanguagesService instance to a Profile.
-class AcceptLanguagesServiceFactory : public BrowserStateKeyedServiceFactory {
+class AcceptLanguagesServiceFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static language::AcceptLanguagesService* GetForProfile(ProfileIOS* profile);
   static AcceptLanguagesServiceFactory* GetInstance();
-
-  AcceptLanguagesServiceFactory(const AcceptLanguagesServiceFactory&) = delete;
-  AcceptLanguagesServiceFactory& operator=(
-      const AcceptLanguagesServiceFactory&) = delete;
 
  private:
   friend class base::NoDestructor<AcceptLanguagesServiceFactory>;
@@ -36,8 +30,6 @@ class AcceptLanguagesServiceFactory : public BrowserStateKeyedServiceFactory {
 
   // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
