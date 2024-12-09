e7c6d7a8ee0b6ee78c251898ab5808c0f4eab15e
Sylvain Defresne <sdefresne@chromium.org>
2024-12-09 17:20:06 +0000

    [ios] Convert IOSPasswordRequirementsServiceFactory to PKSFIOS
    Convert IOSPasswordRequirementsServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217656
    Change-Id: I4267929efcaeba9282fa062d173456eb1da23a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6079417
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393720}

--- ios/chrome/browser/passwords/model/ios_password_requirements_service_factory.cc
+++ ios/chrome/browser/passwords/model/ios_password_requirements_service_factory.cc
@@ -6,19 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/passwords/model/ios_password_requirements_service_factory.h"
 
 #include "base/no_destructor.h"
-#include "components/keyed_service/core/service_access_type.h"
-#include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/password_manager/core/browser/password_requirements_service.h"
 #include "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 // static
 password_manager::PasswordRequirementsService*
-IOSPasswordRequirementsServiceFactory::GetForProfile(
-    ProfileIOS* profile,
-    ServiceAccessType access_type) {
-  return static_cast<password_manager::PasswordRequirementsService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+IOSPasswordRequirementsServiceFactory::GetForProfile(ProfileIOS* profile) {
+  return GetInstance()
+      ->GetServiceForProfileAs<password_manager::PasswordRequirementsService>(
+          profile, /*create=*/true);
 }
 
 // static
@@ -29,9 +26,8 @@ IOSPasswordRequirementsServiceFactory::GetInstance() {
 }
 
 IOSPasswordRequirementsServiceFactory::IOSPasswordRequirementsServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "PasswordRequirementsServiceFactory",
-          BrowserStateDependencyManager::GetInstance()) {}
+    : ProfileKeyedServiceFactoryIOS("PasswordRequirementsServiceFactory",
+                                    TestingCreation::kNoServiceForTests) {}
 
 IOSPasswordRequirementsServiceFactory::
     ~IOSPasswordRequirementsServiceFactory() {}
@@ -41,8 +37,4 @@ IOSPasswordRequirementsServiceFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
   return password_manager::CreatePasswordRequirementsService(
       context->GetSharedURLLoaderFactory());
-}
-
-bool IOSPasswordRequirementsServiceFactory::ServiceIsNULLWhileTesting() const {
-  return true;
 }
