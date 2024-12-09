ac8ec6e5dcc05e22d609975b95464d6693ea04a2
Kevin Graney <kmg@google.com>
2024-12-09 22:39:03 +0000

    Cleanup shouldShowV2 logic in WebUI
    Bug: 382508072
    Change-Id: I49992b7cae1c1ddae6b18205c8b8d03bc8a96393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6081711
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393932}

--- chrome/browser/resources/privacy_sandbox/privacy_sandbox_dialog_mixin.ts
+++ chrome/browser/resources/privacy_sandbox/privacy_sandbox_dialog_mixin.ts
@@ -24,10 +24,9 @@ export const PrivacySandboxDialogMixin = dedupingMixin(
         wasScrolledToBottom: boolean = true;
 
         private didStartWithScrollbar_: boolean = false;
-        private shouldShowV2_: boolean = loadTimeData.getBoolean(
-            'isPrivacySandboxAdsApiUxEnhancementsEnabled');
         private wasScrolledToBottomResolver_: PromiseResolver<void>;
         private moreButtonInitialized_: PromiseResolver<void>;
+        private shouldShowV2_: boolean;
 
         static get properties() {
           return {
@@ -35,7 +34,22 @@ export const PrivacySandboxDialogMixin = dedupingMixin(
               type: Boolean,
               observer: 'onWasScrolledToBottomChange_',
             },
+
+            /**
+             * If true, the Ads API UX Enhancement should be shown.
+             */
+            shouldShowV2_: {
+              type: Boolean,
+              value: () => {
+                return loadTimeData.getBoolean(
+                    'isPrivacySandboxAdsApiUxEnhancementsEnabled');
+              },
+            },
           };
+        }
+
+        shouldShowV2(): boolean {
+          return this.shouldShowV2_;
         }
 
         onConsentLearnMoreExpandedChanged(
@@ -186,7 +200,7 @@ export const PrivacySandboxDialogMixin = dedupingMixin(
 
             const buttonRowHeight = 64;
             let lastTextElementId = '#lastTextElement';
-            if (this.shouldShowV2_ &&
+            if (this.shouldShowV2() &&
                 scrollable.querySelector('#lastTextElementV2')) {
               lastTextElementId = '#lastTextElementV2';
             }
@@ -262,6 +276,9 @@ export const PrivacySandboxDialogMixin = dedupingMixin(
 
 export interface PrivacySandboxDialogMixinInterface {
   wasScrolledToBottom: boolean;
+
+  // Returns true if the Ads API UX Enhancement should be shown.
+  shouldShowV2(): boolean;
 
   onConsentLearnMoreExpandedChanged(newValue: boolean, oldValue: boolean): void;
   onNoticeLearnMoreExpandedChanged(newValue: boolean, oldValue: boolean): void;
