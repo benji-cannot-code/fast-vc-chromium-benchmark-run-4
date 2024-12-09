4a50171cfc23af67d6127411e792fab157cd9606
Mason Freed <masonf@chromium.org>
2024-12-09 19:38:51 +0000

    Remove the CreateInputShadowTreeDuringLayout flag
    This flag landed in M124 and can (very) safely be removed.
    
    Fixed: 325613706
    Change-Id: I93c577e1cc8b03d7646e57933df288ca2fca9056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6078138
    Reviewed-by: Traian Captan <tcaptan@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393819}

--- third_party/blink/renderer/core/html/forms/file_input_type.cc
+++ third_party/blink/renderer/core/html/forms/file_input_type.cc
@@ -401,8 +401,6 @@ Node* FileInputType::FileStatusElement() const {
 }
 
 void FileInputType::DisabledAttributeChanged() {
-  DCHECK(RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled() ||
-         IsShadowHost(GetElement()));
   if (Element* button = UploadButton()) {
     button->SetBooleanAttribute(html_names::kDisabledAttr,
                                 GetElement().IsDisabledFormControl());
@@ -410,8 +408,6 @@ void FileInputType::DisabledAttributeChanged() {
 }
 
 void FileInputType::MultipleAttributeChanged() {
-  DCHECK(RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled() ||
-         IsShadowHost(GetElement()));
   if (Element* button = UploadButton()) {
     button->setAttribute(
         html_names::kValueAttr,
--- third_party/blink/renderer/core/html/forms/html_input_element.cc
+++ third_party/blink/renderer/core/html/forms/html_input_element.cc
@@ -380,9 +380,6 @@ void HTMLInputElement::HandleBlurEvent() {
 }
 
 void HTMLInputElement::setType(const AtomicString& type) {
-  if (!RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled()) {
-    EnsureShadowSubtree();
-  }
   setAttribute(html_names::kTypeAttr, type);
 }
 
@@ -405,8 +402,7 @@ void HTMLInputElement::InitializeTypeInParsing() {
   if (!default_value.IsNull())
     input_type_->WarnIfValueIsInvalid(default_value);
 
-  if (!RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled() ||
-      input_type_view_->HasCreatedShadowSubtree()) {
+  if (input_type_view_->HasCreatedShadowSubtree()) {
     input_type_view_->UpdateView();
   } else {
     input_type_view_->set_needs_update_view_in_create_shadow_subtree(true);
@@ -493,9 +489,7 @@ void HTMLInputElement::UpdateType(const AtomicString& type_attribute_value) {
   // No need for CreateShadowSubtreeIfNeeded() to call UpdateView() as we'll
   // do that later on in this function (and calling UpdateView() here is
   // problematic as state hasn't fully been updated).
-  if (RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled()) {
-    input_type_view_->set_needs_update_view_in_create_shadow_subtree(false);
-  }
+  input_type_view_->set_needs_update_view_in_create_shadow_subtree(false);
   input_type_view_->CreateShadowSubtreeIfNeeded(true);
 
   UpdateWillValidateCache();
@@ -868,7 +862,6 @@ void HTMLInputElement::ParseAttribute(
     input_type_->WarnIfValueIsInvalidAndElementIsVisible(value);
     input_type_->InRangeChanged();
     if (input_type_view_->HasCreatedShadowSubtree() ||
-        !RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled() ||
         !input_type_view_->NeedsShadowSubtree()) {
       input_type_view_->ValueAttributeChanged();
     } else {
@@ -1771,8 +1764,7 @@ Node::InsertionNotificationRequest HTMLInputElement::InsertedInto(
     if (!Form()) {
       AddToRadioButtonGroup();
     }
-    if (RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled() &&
-        !input_type_view_->HasCreatedShadowSubtree() &&
+    if (!input_type_view_->HasCreatedShadowSubtree() &&
         input_type_view_->NeedsShadowSubtree()) {
       scheduled_create_shadow_tree_ = true;
       GetDocument().ScheduleShadowTreeCreation(*this);
@@ -1781,10 +1773,6 @@ Node::InsertionNotificationRequest HTMLInputElement::InsertedInto(
   ResetListAttributeTargetObserver();
   LogAddElementIfIsolatedWorldAndInDocument("input", html_names::kTypeAttr,
                                             html_names::kFormactionAttr);
-  if (!RuntimeEnabledFeatures::CreateInputShadowTreeDuringLayoutEnabled()) {
-    EventDispatchForbiddenScope::AllowUserAgentEvents allow_events;
-    input_type_view_->CreateShadowSubtreeIfNeeded();
-  }
   return kInsertionShouldCallDidNotifySubtreeInsertions;
 }
 
