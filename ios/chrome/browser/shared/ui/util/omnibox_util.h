db42d803e2b18ac9c8ebdb23ab226486c98b8830
Christian Xu <christianxu@google.com>
2024-12-09 16:21:04 +0000

    [iOS][Omnibox] Remove unused util
    Remove undefined function that was missed during previous cleanup.
    
    Bug: None
    Change-Id: I2b9122bc237c441c1635847787eb229056ddf632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6076637
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393696}

--- ios/chrome/browser/shared/ui/util/omnibox_util.h
+++ ios/chrome/browser/shared/ui/util/omnibox_util.h
@@ -9,10 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 class Browser;
-@class LayoutGuideCenter;
 
 /// Returns true if the omnibox is at the bottom in the current layout.
-bool IsCurrentLayoutBottomOmnibox(LayoutGuideCenter* layout_guide_center);
 bool IsCurrentLayoutBottomOmnibox(Browser* browser);
 
 #endif  // IOS_CHROME_BROWSER_SHARED_UI_UTIL_OMNIBOX_UTIL_H_
