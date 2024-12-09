d359226fab892ea57ba2b819263db74a0c0f528e
Federica Germinario <fedegermi@google.com>
2024-12-09 15:20:38 +0000

    Use ProfileKeyedServiceFactoryIOS in IOSChromeAffiliationServiceFactory
    Convert IOSChromeAffiliationServiceFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363217343
    Change-Id: I8306da4dca8148a9328a9aee25381927b9fe01a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6076560
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393658}

--- ios/chrome/browser/affiliations/model/ios_chrome_affiliation_service_factory.h
+++ ios/chrome/browser/affiliations/model/ios_chrome_affiliation_service_factory.h
@@ -7,16 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_AFFILIATIONS_MODEL_IOS_CHROME_AFFILIATION_SERVICE_FACTORY_H_
 
 #include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ProfileIOS;
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 namespace affiliations {
 class AffiliationService;
 }
 
 class IOSChromeAffiliationServiceFactory
-    : public BrowserStateKeyedServiceFactory {
+    : public ProfileKeyedServiceFactoryIOS {
  public:
   static IOSChromeAffiliationServiceFactory* GetInstance();
   static affiliations::AffiliationService* GetForProfile(ProfileIOS* profile);
@@ -29,7 +27,6 @@ class IOSChromeAffiliationServiceFactory
 
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(web::BrowserState*) const override;
 };
 
 #endif  // IOS_CHROME_BROWSER_AFFILIATIONS_MODEL_IOS_CHROME_AFFILIATION_SERVICE_FACTORY_H_
--- ios/chrome/browser/affiliations/model/ios_chrome_affiliation_service_factory.mm
+++ ios/chrome/browser/affiliations/model/ios_chrome_affiliation_service_factory.mm
@@ -8,16 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 #import <utility>
 
-#import "base/no_destructor.h"
 #import "base/not_fatal_until.h"
 #import "base/task/sequenced_task_runner.h"
 #import "base/task/thread_pool.h"
 #import "components/affiliations/core/browser/affiliation_constants.h"
 #import "components/affiliations/core/browser/affiliation_service_impl.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
-#import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "services/network/public/cpp/shared_url_loader_factory.h"
 
@@ -38,9 +34,8 @@ IOSChromeAffiliationServiceFactory::GetForProfile(ProfileIOS* profile) {
 }
 
 IOSChromeAffiliationServiceFactory::IOSChromeAffiliationServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "AffiliationService",
-          BrowserStateDependencyManager::GetInstance()) {}
+    : ProfileKeyedServiceFactoryIOS("AffiliationService",
+                                    ProfileSelection::kRedirectedInIncognito) {}
 
 IOSChromeAffiliationServiceFactory::~IOSChromeAffiliationServiceFactory() =
     default;
@@ -61,9 +56,4 @@ IOSChromeAffiliationServiceFactory::BuildServiceInstanceFor(
           affiliations::kAffiliationDatabaseFileName));
 
   return affiliation_service;
-}
-
-web::BrowserState* IOSChromeAffiliationServiceFactory::GetBrowserStateToUse(
-    web::BrowserState* context) const {
-  return GetBrowserStateRedirectedInIncognito(context);
 }
