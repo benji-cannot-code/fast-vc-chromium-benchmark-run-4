b783d04746b29c320368dadadc7c393a874212b7
Vasiliy Telezhnikov <vasilyt@chromium.org>
2024-12-09 21:56:58 +0000

    Remove AcceleratedStaticBitmapImage::is_origin_top_left_
    Class can be created in two ways:
    * CreateFromCanvasSharedImage. In this case variable is always set from
      client shared image. [1]
    * CreateFromExternalSharedImage. In this case we set from
      IsOriginTopLeft of the source image [2], which would come either from
      CreateFromCanvasSharedImage (and so origin would be from SharedImage)
      or from another CreateFromExternalSharedImage (and so on).
    
    We already do send ClientSharedImage::surface_origin over mojo and the
    flag always match it, so we can remove it.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.cc;drc=8226afdf7956354dfcfd0d0e8645bb066d99cc3b;l=75
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/messaging/blink_transferable_message_mojom_traits.cc;drc=5103ac6102b144aff2bb3b45fe525521ecf3320d;l=58
    
    Bug: 378688985
    Change-Id: I063e1c548e19ec52643226e980c6043959892ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073149
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393907}

--- third_party/blink/renderer/core/messaging/blink_transferable_message_mojom_traits.cc
+++ third_party/blink/renderer/core/messaging/blink_transferable_message_mojom_traits.cc
@@ -56,7 +56,7 @@ ToSerializedAcceleratedImage(
       blink::mojom::blink::SerializedStaticBitmapImage::NewAcceleratedImage(
           blink::AcceleratedImageInfo{
               shared_image->Export(), cloned_image->GetSyncToken(),
-              cloned_image->GetSkImageInfo(), cloned_image->IsOriginTopLeft(),
+              cloned_image->GetSkImageInfo(),
               cloned_image->SupportsDisplayCompositing(),
               cloned_image->IsOverlayCandidate(),
               WTF::BindOnce(&blink::StaticBitmapImage::UpdateSyncToken,
