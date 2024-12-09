00ac35c44bf589da98b6e6abbe79e1617bc3f8af
msiem <msiem@chromium.org>
2024-12-09 22:53:35 +0000

    [PWA]: Create was_shortcut_app_ bool for apps
    Creating a was_shortcut_app_ field to track whether an app was a
    shortcut app prior to migrating into a diy app.
    
    This is going to help maintain a record of all apps that were
    previously treated as shortcut apps, in case they need to be
    migrated to any OS specific implementations.
    
    More information: go/shortcut-diy-app-migration-cros
    
    Bug: 380894044
    Change-Id: I0ec07a57c5b44f7b15904cb478f13b29acaa2f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075414
    Commit-Queue: May Siem <msiem@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393945}

--- chrome/browser/web_applications/test/web_app_test_utils.cc
+++ chrome/browser/web_applications/test/web_app_test_utils.cc
@@ -1107,6 +1107,8 @@ std::unique_ptr<WebApp> CreateRandomWebApp(CreateRandomWebAppParams params) {
   app->SetSupportedLinksOfferDismissCount(random.next_uint());
 
   app->SetIsDiyApp(random.next_bool());
+
+  app->SetWasShortcutApp(random.next_bool());
   return app;
 }
 
