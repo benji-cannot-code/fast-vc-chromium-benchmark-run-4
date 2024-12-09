e765ab892b67fd029944963e5284796053c63595
Weizhong Xia <weizhong@google.com>
2024-12-09 22:41:31 +0000

    Regenerate certificates for WPT
    Also removed the certificates for signed exchange as those tests
    are now running exclusively with Wptrunner, and the related certs
    are managed at upstream [1].
    
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/chrome_spki_certs.py;l=8
    
    Bug: 382650353
    Change-Id: I161a4587064a23a0c9e795a1d030995e43b60a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075845
    Reviewed-by: Theo Cristea <theocristea@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393936}

--- third_party/blink/web_tests/http/tests/loading/sxg/resources/README.md
+++ third_party/blink/web_tests/http/tests/loading/sxg/resources/README.md
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 To regenerate the key/certificate pair for the signed-exchanges in this
-directory, run `generate-test-certs.sh`.
+directory, run `generate-test-certs.sh`. Please make sure openssl is in
+the path.
 
 The signed exchange files in this directory are generated using
 `generate-test-sxgs.sh`. it requires command-line tools in the
--- third_party/blink/web_tests/http/tests/loading/sxg/resources/generate-test-certs.sh
+++ third_party/blink/web_tests/http/tests/loading/sxg/resources/generate-test-certs.sh
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 set -e
 
 topdir=$(git rev-parse --show-toplevel)
-cd $topdir/third_party/blink/tools/blinkpy/third_party/wpt/certs
+cd $topdir/third_party/wpt_tools/certs
 
 openssl ecparam -out 127.0.0.1.sxg.key -name prime256v1 -genkey;
 openssl req -new -sha256 -key 127.0.0.1.sxg.key -out 127.0.0.1.sxg.csr \
@@ -16,5 +16,5 @@ openssl req -new -sha256 -key 127.0.0.1.sxg.key -out 127.0.0.1.sxg.csr \
   -config 127.0.0.1.sxg.cnf;
 openssl x509 -req -days 3650 \
   -in 127.0.0.1.sxg.csr -extfile 127.0.0.1.sxg.ext \
-  -CA cacert.pem -CAkey cakey.pem -passin pass:web-platform-tests \
+  -CA cacert.pem -CAkey cacert.key -passin pass:web-platform-tests \
   -set_serial 3 -out 127.0.0.1.sxg.pem
