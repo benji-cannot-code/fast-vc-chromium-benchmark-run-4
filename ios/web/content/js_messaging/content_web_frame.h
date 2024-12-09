add0c965d652a4564a2f367033665042e889034c
Gyuyoung Kim <gyuyoung@igalia.com>
2024-12-09 16:36:14 +0000

    [iOS Blink] Add ContentWebFrame::GetSecurityOrigin
    As https://crrev.com/c/6071350 added back GetSecurityOrigin
    method to WebFrame, this CL also implements GetSecurityOrigin
    method in ContentWebFrame.
    
    Bug: 40260077
    Change-Id: Id3ccf7ceb6682a898fe5690c3195eeb06533dce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074173
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393704}

--- ios/web/content/js_messaging/content_web_frame.h
+++ ios/web/content/js_messaging/content_web_frame.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/js_messaging/web_frame.h"
 #import "ios/web/public/web_state_observer.h"
 #import "url/gurl.h"
+#import "url/origin.h"
 
 namespace content {
 class RenderFrameHost;
@@ -44,6 +45,7 @@ class ContentWebFrame : public WebFrame,
   std::string GetFrameId() const override;
   bool IsMainFrame() const override;
   GURL GetSecurityOriginDeprecated() const override;
+  url::Origin GetSecurityOrigin() const override;
   BrowserState* GetBrowserState() override;
   base::WeakPtr<WebFrame> AsWeakPtr() override;
 
