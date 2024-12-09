76ae090ce2904cdc0d2be83bd2a257ba9031d937
dljames <dljames@chromium.org>
2024-12-09 19:14:13 +0000

    [SharedTabGroups] Only present ungroup option for owners
    Only the owner of a shared tab group will have the ungroup option in
    the TabGroupEditorBubbleView.
    
    This means members will only be able to hide and leave the shared group.
    
    Non-shared group functionality is preserved.
    
    No test is added since this is extremely hard to test.
    
    Ideally, we would have a test where we share a group from one profile to another in order to verify which options show up in the editor bubble.
    
    Additionally, we don't have a way to inject arbitrary data into the
    DataSharingService or CollaborationService at the moment so even if we set up the test framework we will still be missing some pieces.
    
    Change-Id: I7588cc9595ef960270b0c435c0282ba4f47cc0cd
    Bug: 382754499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075432
    Commit-Queue: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393798}

--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/collaboration/collaboration_service_factory.h"
 #include "chrome/browser/data_sharing/data_sharing_service_factory.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -60,10 +61,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/collaboration/public/collaboration_service.h"
 #include "components/data_sharing/public/features.h"
+#include "components/data_sharing/public/group_data.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/saved_tab_groups/public/features.h"
 #include "components/saved_tab_groups/public/saved_tab_group.h"
+#include "components/saved_tab_groups/public/types.h"
 #include "components/tab_groups/tab_group_color.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
@@ -330,7 +334,10 @@ void TabGroupEditorBubbleView::RebuildMenuContents() {
   } else if (tab_groups::IsTabGroupsSaveV2Enabled()) {
     menu_items_.push_back(AddChildView(BuildNewTabInGroupButton()));
     menu_items_.push_back(AddChildView(BuildMoveGroupToNewWindowButton()));
-    menu_items_.push_back(AddChildView(BuildUngroupButton()));
+
+    if (OwnsGroup()) {
+      menu_items_.push_back(AddChildView(BuildUngroupButton()));
+    }
 
     if (CanShareGroups()) {
       menu_items_.push_back(AddChildView(BuildManageSharedGroupButton()));
@@ -381,8 +388,6 @@ void TabGroupEditorBubbleView::UpdateMenuContentsVisibility() {
       case TAB_GROUP_HEADER_CXMENU_MOVE_GROUP_TO_NEW_WINDOW: {
         button->SetVisible(CanMoveGroupToNewWindow());
         break;
-      }
-      default: {
       }
     }
   }
@@ -607,9 +612,9 @@ const std::u16string TabGroupEditorBubbleView::GetSaveToggleAccessibleName()
 }
 
 bool TabGroupEditorBubbleView::CanSaveGroups() const {
-  return (browser_->profile()->IsRegularProfile() &&
-          tab_groups::SavedTabGroupUtils::GetServiceForProfile(
-              browser_->profile()));
+  return browser_->profile()->IsRegularProfile() &&
+         tab_groups::SavedTabGroupUtils::GetServiceForProfile(
+             browser_->profile());
 }
 
 bool TabGroupEditorBubbleView::CanShareGroups() const {
@@ -617,6 +622,37 @@ bool TabGroupEditorBubbleView::CanShareGroups() const {
          tab_groups::IsTabGroupsSaveV2Enabled() &&
          base::FeatureList::IsEnabled(
              data_sharing::features::kDataSharingFeature);
+}
+
+bool TabGroupEditorBubbleView::OwnsGroup() const {
+  tab_groups::TabGroupSyncService* tab_group_service =
+      tab_groups::SavedTabGroupUtils::GetServiceForProfile(browser_->profile());
+  if (!tab_group_service) {
+    return true;
+  }
+
+  const std::optional<tab_groups::SavedTabGroup> maybe_saved_group =
+      tab_group_service->GetGroup(group_);
+  if (!maybe_saved_group.has_value()) {
+    return true;
+  }
+
+  std::optional<tab_groups::CollaborationId> collaboration_id =
+      maybe_saved_group->collaboration_id();
+  if (!collaboration_id) {
+    return true;
+  }
+
+  collaboration::CollaborationService* collaboration_service =
+      collaboration::CollaborationServiceFactory::GetForProfile(
+          browser_->profile());
+  if (!collaboration_service) {
+    return true;
+  }
+
+  data_sharing::GroupId group_id(collaboration_id->value());
+  return data_sharing::MemberRole::kOwner ==
+         collaboration_service->GetCurrentUserRoleForGroup(group_id);
 }
 
 bool TabGroupEditorBubbleView::IsGroupSaved() const {
