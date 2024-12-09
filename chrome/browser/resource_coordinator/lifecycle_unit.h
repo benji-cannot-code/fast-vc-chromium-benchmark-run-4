db7db1cf760050aa171719efda175826857e106d
Patrick Monette <pmonette@chromium.org>
2024-12-09 16:26:10 +0000

    Remove "TabManager.LifecycleStateChange" UKM
    This UKM is no longer recorded, and is not needed anymore.
    
    Change-Id: I999a11ea7386fb39f53940f1fd936da91cc25bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6077589
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393699}

--- chrome/browser/resource_coordinator/lifecycle_unit.h
+++ chrome/browser/resource_coordinator/lifecycle_unit.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/decision_details.h"
 #include "chrome/browser/resource_coordinator/lifecycle_unit_state.mojom-forward.h"
 #include "content/public/browser/visibility.h"
-#include "services/metrics/public/cpp/ukm_source_id.h"
 
 namespace resource_coordinator {
 
@@ -159,10 +158,6 @@ class LifecycleUnit {
   // Adds/removes an observer to this LifecycleUnit.
   virtual void AddObserver(LifecycleUnitObserver* observer) = 0;
   virtual void RemoveObserver(LifecycleUnitObserver* observer) = 0;
-
-  // Returns the UKM source ID associated with this LifecycleUnit, if it has
-  // one.
-  virtual ukm::SourceId GetUkmSourceId() const = 0;
 };
 
 using LifecycleUnitSet =
