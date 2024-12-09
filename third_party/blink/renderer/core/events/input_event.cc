b61aab3e3da1b536b9ad942fdeeced20009cde7f
David Baron <dbaron@chromium.org>
2024-12-09 23:41:31 +0000

    Propagate Range exceptions through InputEvent::Create.
    When InputEvent's constructor is given StaticRange objects, converting
    those objects to Range objects might throw an exception.  Currently in
    those cases we have a DCHECK failure in DCHECK-enabled builds, and store
    a default-initialized range (pointing to the Document) in non-DCHECK
    builds.
    
    This instead propagates those exceptions through the InputEvent
    constructor.
    
    It's not really clear what the spec requires since the spec isn't clear
    about the processing model for this InputEvent constructor; in
    particular the fact that we store these ranges as Range rather than
    StaticRange is unclear in the spec.
    
    This is a behavior change, but only for an obscure corner of the spec
    (manually-initialized events) that didn't work correctly before.
    
    The added test fails without the change (crashing in DCHECK-enabled
    builds, and failing in non-DCHECK-enabled builds).
    
    Fixed: 377955857
    Change-Id: Ia74d2194d3da78853f2ac4862b78b074579f9d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039813
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393973}

--- third_party/blink/renderer/core/events/input_event.cc
+++ third_party/blink/renderer/core/events/input_event.cc
@@ -109,8 +109,27 @@ bool InputTypeIsCancelable(InputEvent::InputType input_type) {
 
 }  // anonymous namespace
 
+/* static */ InputEvent* InputEvent::Create(const AtomicString& type,
+                                            const InputEventInit* initializer,
+                                            ExceptionState& exception_state) {
+  InputEvent* result;
+  if (RuntimeEnabledFeatures::InputEventConstructorThrowsEnabled()) {
+    CHECK(!exception_state.HadException());
+    result =
+        MakeGarbageCollected<InputEvent>(type, initializer, exception_state);
+    if (exception_state.HadException()) {
+      return nullptr;
+    }
+  } else {
+    result =
+        MakeGarbageCollected<InputEvent>(type, initializer, IGNORE_EXCEPTION);
+  }
+  return result;
+}
+
 InputEvent::InputEvent(const AtomicString& type,
-                       const InputEventInit* initializer)
+                       const InputEventInit* initializer,
+                       ExceptionState& exception_state)
     : UIEvent(type, initializer) {
   // TODO(ojan): We should find a way to prevent conversion like
   // String->enum->String just in order to use initializer.
@@ -126,7 +145,7 @@ InputEvent::InputEvent(const AtomicString& type,
   if (!initializer->hasTargetRanges())
     return;
   for (const auto& range : initializer->targetRanges())
-    ranges_.push_back(range->toRange());
+    ranges_.push_back(range->toRange(exception_state));
 }
 
 InputEvent::InputEvent(const AtomicString& type,
@@ -143,7 +162,7 @@ InputEvent::InputEvent(const AtomicString& type,
       is_composing_(is_composing == kIsComposing) {
   if (ranges) {
     for (const auto& range : *ranges) {
-      ranges_.push_back(range->toRange());
+      ranges_.push_back(range->toRange(ASSERT_NO_EXCEPTION));
     }
   }
 }
