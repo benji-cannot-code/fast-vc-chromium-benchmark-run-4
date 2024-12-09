e006f84b32f7a513a4d24e0a3450bd10ebabda32
Guohui Deng <guohuideng@microsoft.com>
2024-12-09 23:17:39 +0000

    Save some DocumentLoader values in NavigationTiming.
    The purpose is that the values will remain available after the document
    is removed.
    
    Bug: 40793421
    Change-Id: Ia54a63e5a6964acf5f1c051c349d9a68d20ccf6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6065040
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Commit-Queue: Guohui Deng <guohuideng@microsoft.com>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393958}

--- third_party/blink/renderer/core/timing/performance_navigation_timing.h
+++ third_party/blink/renderer/core/timing/performance_navigation_timing.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_NAVIGATION_TIMING_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_NAVIGATION_TIMING_H_
 
+#include "services/network/public/mojom/url_response_head.mojom.h"
 #include "third_party/blink/public/mojom/back_forward_cache_not_restored_reasons.mojom-blink.h"
 #include "third_party/blink/public/mojom/timing/resource_timing.mojom-blink-forward.h"
 #include "third_party/blink/public/web/web_navigation_type.h"
@@ -85,6 +86,9 @@ class CORE_EXPORT PerformanceNavigationTiming final
 
   NotRestoredReasons* BuildNotRestoredReasons(
       const mojom::blink::BackForwardCacheNotRestoredReasonsPtr& reasons) const;
+
+  const network::mojom::NavigationDeliveryType navigation_delivery_type_;
+  const WebNavigationType navigation_type_;
 
   Member<DocumentTimingValues> document_timing_values_;
   Member<DocumentLoadTimingValues> document_load_timing_values_;
