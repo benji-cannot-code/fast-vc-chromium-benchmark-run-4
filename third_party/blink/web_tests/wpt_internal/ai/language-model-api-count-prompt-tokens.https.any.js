b26c17cdc62ceeeb72a457e1068e8073e00ef3f7
Tsuyoshi Horo <horo@chromium.org>
2024-12-09 00:12:47 +0000

    Remove the workaround for crbug.com/362676838
    There was an issue that any ongoing sessions will be aborted once the
    language detection model is loaded. To avoid the issue,
    workaround-for-362676838.js was introduced.
    
    The issue has been fixed. Also workaround-for-362676838.js is creating a
    AIWriter which doesn't work with the latest base model.
    
    So this CL removes the workaround.
    
    
    Bug: 362676838, 378549335
    Change-Id: I761c03c3c653ec6c7903601f65f462cf7a53b4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074176
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393466}

--- third_party/blink/web_tests/wpt_internal/ai/language-model-api-count-prompt-tokens.https.any.js
+++ third_party/blink/web_tests/wpt_internal/ai/language-model-api-count-prompt-tokens.https.any.js
@@ -1,6 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: script=resources/workaround-for-362676838.js
-
 promise_test(async t => {
   // Make sure the prompt api is enabled.
   assert_true(!!ai);
