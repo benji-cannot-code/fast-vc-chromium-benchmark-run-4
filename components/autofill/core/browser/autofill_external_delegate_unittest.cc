010a0cd93ddc1d810d8daf748f0111316922e4c5
Jihad Hanna <jihadghanna@google.com>
2024-12-09 15:36:30 +0000

    Rename PredictionImprovementsPayload
    Low-Coverage-Reason: TRIVIAL_CHANGE the CL only renames a class.
    Bug: 375569869
    Change-Id: If24d4e65eaad19921f974e37bff2b73491a6c0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080294
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393665}

--- components/autofill/core/browser/autofill_external_delegate_unittest.cc
+++ components/autofill/core/browser/autofill_external_delegate_unittest.cc
@@ -1266,7 +1266,7 @@ TEST_F(AutofillExternalDelegateTest,
 }
 
 // Tests that on acceptance of a `kFillPredictionImprovements` suggestion with
-// `Suggestion::PredictionImprovementsPayload` payload, the full form is filled
+// `Suggestion::AutofillAiPayload` payload, the full form is filled
 // accordingly.
 TEST_F(AutofillExternalDelegateTest,
        DidAcceptFillPredictionImprovementsFillsFullForm) {
@@ -1285,7 +1285,7 @@ TEST_F(AutofillExternalDelegateTest,
       /*update_datalist=*/false);
   Suggestion fill_suggestion =
       Suggestion(u"Autocomplete", SuggestionType::kFillAutofillAi);
-  fill_suggestion.payload = Suggestion::PredictionImprovementsPayload(
+  fill_suggestion.payload = Suggestion::AutofillAiPayload(
       {{field_to_fill->global_id(), value_to_fill}}, {});
 
   std::vector<FormFieldData> filled_fields;
