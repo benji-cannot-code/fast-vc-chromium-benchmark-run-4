865465a0aa340d8ed88a9b7758206bad0b21d32d
Clark DuVall <cduvall@chromium.org>
2024-12-09 20:16:21 +0000

    Add image input to chrome://on-device-internals
    This adds the ability to upload images to the internals page along with
    a few other cosmetic fixes.
    
    Bug: 377772887
    Change-Id: I2719d0362cbd09d09441bcad2e99c24e8ebd8001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075721
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393842}

--- chrome/browser/ui/webui/on_device_internals/on_device_internals_page_handler.h
+++ chrome/browser/ui/webui/on_device_internals/on_device_internals_page_handler.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/on_device_model/public/cpp/buildflags.h"
 #include "services/on_device_model/public/cpp/model_assets.h"
+#include "services/on_device_model/public/mojom/on_device_model.mojom.h"
 #include "services/on_device_model/public/mojom/on_device_model_service.mojom.h"
 
 class OptimizationGuideKeyedService;
@@ -55,6 +56,8 @@ class OnDeviceInternalsPageHandler : public mojom::OnDeviceInternalsPageHandler,
       GetEstimatedPerformanceClassCallback callback) override;
   void GetOnDeviceInternalsData(
       GetOnDeviceInternalsDataCallback callback) override;
+  void DecodeBitmap(mojo_base::BigBuffer image_buffer,
+                    DecodeBitmapCallback callback) override;
 
   // optimization_guide::OptimizationGuideLogger::Observer:
   void OnLogMessageAdded(base::Time event_time,
@@ -63,10 +66,16 @@ class OnDeviceInternalsPageHandler : public mojom::OnDeviceInternalsPageHandler,
                          int source_line,
                          const std::string& message) override;
 
+  void LoadAdaptation(
+      mojo::PendingReceiver<on_device_model::mojom::OnDeviceModel> model,
+      LoadModelCallback callback,
+      on_device_model::mojom::LoadModelResult result);
+
   mojo::Receiver<mojom::OnDeviceInternalsPageHandler> receiver_;
   mojo::Remote<mojom::OnDeviceInternalsPage> page_;
 
   mojo::Remote<Service> service_;
+  mojo::Remote<on_device_model::mojom::OnDeviceModel> base_model_;
 
   // Logger to receive the debug logs from the optimization guide service. Not
   // owned. Guaranteed to outlive |this|, since the logger is owned by the
