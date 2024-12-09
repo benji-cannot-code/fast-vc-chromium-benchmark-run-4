4507ecd3e49ace9b090c5bd579507a3580b8249c
Andy Phan <andyphan@chromium.org>
2024-12-09 20:40:53 +0000

    [CodeHealth] Use std::array in ash/public/cpp/accelerators.h
    Use std::array for all the arrays in ash/public/cpp/accelerators.h.
    Then, all array accesses will have safer bounds checking, and the length
    variables can be removed.
    
    Now that the length variables are removed, replace the for loops that
    use the lengths with for-each loops.
    
    This change is necessary to remove the allow_unsafe_buffers pragma from
    affected files in a future CL.
    
    Change-Id: If677929cfc98f8ff7dcb87408ff829fc14a9ed20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075565
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393854}

--- ash/accelerators/accelerator_controller_unittest.cc
+++ ash/accelerators/accelerator_controller_unittest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ime/ime_controller_impl.h"
 #include "ash/ime/test_ime_controller_client.h"
 #include "ash/media/media_controller_impl.h"
+#include "ash/public/cpp/accelerator_actions.h"
 #include "ash/public/cpp/accelerators.h"
 #include "ash/public/cpp/arc_game_controls_flag.h"
 #include "ash/public/cpp/ash_prefs.h"
@@ -2385,8 +2386,9 @@ TEST_F(PreferredReservedAcceleratorsTest, AcceleratorsWithPinned) {
 
 TEST_F(AcceleratorControllerTest, DisallowedAtModalWindow) {
   std::set<AcceleratorAction> all_actions;
-  for (size_t i = 0; i < kAcceleratorDataLength; ++i)
-    all_actions.insert(kAcceleratorData[i].action);
+  for (const AcceleratorData& data : kAcceleratorData) {
+    all_actions.insert(data.action);
+  }
   std::set<AcceleratorAction> all_debug_actions;
   for (size_t i = 0; i < kDebugAcceleratorDataLength; ++i)
     all_debug_actions.insert(kDebugAcceleratorData[i].action);
@@ -2509,8 +2511,7 @@ TEST_F(AcceleratorControllerTest, DisallowedWithNoWindow) {
   for (size_t i = 0; i < kActionsNeedingWindowLength; ++i)
     actions_needing_window.insert(kActionsNeedingWindow[i]);
   std::map<AcceleratorAction, ui::Accelerator> accelerators_needing_window;
-  for (size_t i = 0; i < kAcceleratorDataLength; ++i) {
-    const auto& accelerator_data = kAcceleratorData[i];
+  for (const AcceleratorData& accelerator_data : kAcceleratorData) {
     auto iter = actions_needing_window.find(accelerator_data.action);
     if (!base::Contains(actions_needing_window, accelerator_data.action)) {
       continue;
