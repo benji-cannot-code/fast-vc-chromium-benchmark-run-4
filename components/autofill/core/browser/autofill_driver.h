443bed3b1b061fed7076ef3ebed2191a89cbe04f
Christoph Schwering <schwering@google.com>
2024-12-09 12:43:36 +0000

    [Autofill] DCHECK AutofillDriver::LifecycleState transitions
    This CL adds a DCHECK to validate that the diagram of
    AutofillDriver::LifecycleState is correct.
    
    Bug: 354043640
    Change-Id: I4b4377b2fd444f02ca178a08f2215fa22d30f926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6078264
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393604}

--- components/autofill/core/browser/autofill_driver.h
+++ components/autofill/core/browser/autofill_driver.h
@@ -70,12 +70,16 @@ class AutofillDriver {
   //
   // The possible transitions are:
   //
-  //   ╭───────────────────────────╮
-  //   │                           ▼
-  // kInactive ◄──► kActive ──► kPendingDeletion
-  //   ▲                ▲
-  //   │                ╰─────► kPendingReset
-  //   ╰──────────────────────► kPendingReset
+  // LINT.IfChange(LifecycleStateChanges)
+  //
+  //     ╭───────────────────────────╮
+  //     │                           ▼
+  //   kInactive ◄──► kActive ──► kPendingDeletion
+  //     ▲                ▲
+  //     │                ╰─────► kPendingReset
+  //     ╰──────────────────────► kPendingReset
+  //
+  // LINT.ThenChange(autofill_driver.cc:LifecycleStateChanges)
   //
   // The initial state is kInactive.
   //
@@ -122,10 +126,7 @@ class AutofillDriver {
   // - notify observers,
   // - destruct the driver.
   void SetLifecycleState(LifecycleState new_state,
-                         base::PassKey<AutofillDriverFactory> pass_key) {
-    DCHECK_NE(lifecycle_state_, new_state);
-    lifecycle_state_ = new_state;
-  }
+                         base::PassKey<AutofillDriverFactory> pass_key);
 
   // Returns the uniquely identifying frame token.
   virtual LocalFrameToken GetFrameToken() const = 0;
@@ -335,6 +336,10 @@ class AutofillDriver {
   friend class AutofillDriverTestApi;
 
   LifecycleState lifecycle_state_ = LifecycleState::kInactive;
+
+#if DCHECK_IS_ON()
+  LifecycleState previous_lifecycle_state_ = LifecycleState::kInactive;
+#endif
 };
 
 }  // namespace autofill
