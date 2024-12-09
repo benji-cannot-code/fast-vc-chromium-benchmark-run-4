d0ca71a1271db983995a32e96248452dd45044fe
Stephen McGruer <smcgruer@chromium.org>
2024-12-09 07:50:21 +0000

    [Autofill] Remove two usages of test::GetFullServerCard
    These tests are for VCN enrollment flows, which are in production
    triggered with either kMaskedServerCard or kLocalCard credit
    cards, not kFullServerCard.
    
    Bug: 40939195
    Change-Id: Id405991f6f71116c81d66630756a6a38f0eb6e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073384
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393531}

--- chrome/browser/ui/views/autofill/payments/virtual_card_enroll_bubble_views_interactive_uitest.cc
+++ chrome/browser/ui/views/autofill/payments/virtual_card_enroll_bubble_views_interactive_uitest.cc
@@ -65,7 +65,7 @@ class VirtualCardEnrollBubbleViewsInteractiveUiTest
   }
 
   void CreateVirtualCardEnrollmentFields() {
-    CreditCard credit_card = test::GetFullServerCard();
+    CreditCard credit_card = test::GetCreditCard();
     LegalMessageLines google_legal_message = {
         TestLegalMessageLine("google_test_legal_message")};
     LegalMessageLines issuer_legal_message = {
