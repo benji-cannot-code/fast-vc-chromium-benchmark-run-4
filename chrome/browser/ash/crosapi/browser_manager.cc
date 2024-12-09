8b6780f72788647ec4b008b32996aff9ccdc4cce
Georg Neis <neis@chromium.org>
2024-12-09 04:40:35 +0000

    ash: Remove BrowserManagerObserver
    - Remove SessionControllerClientImpl's use of BrowserManagerObserver.
      The session info no longer depend on Lacros (in particular,
      ash::AddUserSessionPolicy::ERROR_LACROS_RUNNING does not exist
      anymore).
    
    - Delete BrowserManagerObserver.
    
    This is a step towards removing BrowserManager and other
    Lacros-related code.
    
    Bug: b:365741912
    Change-Id: If1623e1335395ccea6da9a733d92db9b2c3bbf67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072911
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393500}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -223,14 +223,6 @@ void BrowserManager::InitializeAndStartIfNeeded() {
   ClearLacrosData();
 }
 
-void BrowserManager::AddObserver(BrowserManagerObserver* observer) {
-  observers_.AddObserver(observer);
-}
-
-void BrowserManager::RemoveObserver(BrowserManagerObserver* observer) {
-  observers_.RemoveObserver(observer);
-}
-
 void BrowserManager::Shutdown() {
   shutdown_requested_ = true;
 }
@@ -240,10 +232,6 @@ void BrowserManager::SetState(State state) {
     return;
   }
   state_ = state;
-
-  for (auto& observer : observers_) {
-    observer.OnStateChanged();
-  }
 }
 
 void BrowserManager::ClearLacrosData() {
--- chrome/browser/ash/crosapi/browser_manager.h
+++ chrome/browser/ash/crosapi/browser_manager.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
-#include "chrome/browser/ash/crosapi/browser_manager_observer.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/crosapi/crosapi_id.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
@@ -83,9 +82,6 @@ class BrowserManager : public session_manager::SessionManagerObserver,
   // directory.
   virtual void InitializeAndStartIfNeeded();
 
-  void AddObserver(BrowserManagerObserver* observer);
-  void RemoveObserver(BrowserManagerObserver* observer);
-
   // Notifies the BrowserManager that it should prepare for shutdown. This is
   // called in the early stages of ash shutdown to give Lacros sufficient time
   // for a graceful exit.
@@ -137,7 +133,7 @@ class BrowserManager : public session_manager::SessionManagerObserver,
     // or disabled.
     UNAVAILABLE,
   };
-  // Changes |state| value and potentially notify observers of the change.
+  // Changes |state| value.
   void SetState(State state);
 
  private:
@@ -192,8 +188,6 @@ class BrowserManager : public session_manager::SessionManagerObserver,
   // Called as a callback to `RemoveLacrosUserDataDir()`. `cleared` is set to
   // true if the directory existed and was removed successfully.
   void OnLacrosUserDataDirRemoved(bool cleared);
-
-  base::ObserverList<BrowserManagerObserver> observers_;
 
   // NOTE: The state is exposed to tests via autotest_private.
   State state_ = State::NOT_INITIALIZED;
