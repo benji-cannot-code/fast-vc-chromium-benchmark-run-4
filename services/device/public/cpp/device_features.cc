6d07fac9ce9cb7a4c936c0ce600436344b63b823
Alvin Ji <alvinji@chromium.org>
2024-12-09 23:07:13 +0000

    geolocation: Introduce kHybridPlatform2 mode for LocationProviderManager
    This CL introduces a new mode, `kHybridPlatform2`, to the
    `LocationProviderManager`. This mode extends the existing fallback
    mechanism, which was previously triggered only by a Wi-Fi disabled
    error, to now be triggered by *any* error from the platform location
    provider.
    
    The goal is to help the `MacPlatformLocationProvider` study and
    isolate the server-side metrics degradation issue. This will help us
    understand whether the issue is related to the inherent lower
    reliability of the platform  location provider compared to the network
    location provider.
    
    The following changes were made:
    * Added `kHybridPlatform2` to the `LocationProviderManagerMode` enum in geolocation_internals.mojom.
    * Updated `LocationProviderManager` to support the new mode, including starting with the platform location provider and falling back to the network location provider upon encountering any error.
    * Added `kHybridPlatform2` as a feature variation in about_flags.cc for testing and experimentation.
    * Updated unit tests to cover the new mode and its fallback behavior.
    
    Bug: 382062436
    Change-Id: I53e1c8bf895268777c7a793759b27314ec384c46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074232
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393952}

--- services/device/public/cpp/device_features.cc
+++ services/device/public/cpp/device_features.cc
@@ -84,6 +84,8 @@ const base::FeatureParam<device::mojom::LocationProviderManagerMode>::Option
          "PlatformOnly"},
         {device::mojom::LocationProviderManagerMode::kHybridPlatform,
          "HybridPlatform"},
+        {device::mojom::LocationProviderManagerMode::kHybridPlatform2,
+         "HybridPlatform2"},
 };
 
 const base::FeatureParam<device::mojom::LocationProviderManagerMode>
