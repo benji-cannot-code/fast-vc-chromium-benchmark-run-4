21eda580030a2a38b33f9a1b5e9bd6cc6da21e26
Federica Germinario <fedegermi@google.com>
2024-12-09 15:24:06 +0000

    Use ProfileKeyedServiceFactoryIOS in DomainDiversityReporterFactory
    Convert DomainDiversityReporterFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215067
    Change-Id: Ice27188f493a095b7bc5d0b51580bb15a0153b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075076
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393660}

--- ios/chrome/browser/history/model/domain_diversity_reporter_factory.h
+++ ios/chrome/browser/history/model/domain_diversity_reporter_factory.h
@@ -6,13 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_HISTORY_MODEL_DOMAIN_DIVERSITY_REPORTER_FACTORY_H_
 #define IOS_CHROME_BROWSER_HISTORY_MODEL_DOMAIN_DIVERSITY_REPORTER_FACTORY_H_
 
-#include <memory>
-
 #include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class DomainDiversityReporter;
-class ProfileIOS;
 
 namespace user_prefs {
 class PrefRegistrySyncable;
@@ -20,7 +17,7 @@ class PrefRegistrySyncable;
 
 // Singleton that creates all DomainDiversityReporter instances and associates
 // them with BrowserState.
-class DomainDiversityReporterFactory : public BrowserStateKeyedServiceFactory {
+class DomainDiversityReporterFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static DomainDiversityReporter* GetForProfile(ProfileIOS* profile);
 
@@ -38,11 +35,6 @@ class DomainDiversityReporterFactory : public BrowserStateKeyedServiceFactory {
 
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* browser_state) const override;
-
-  web::BrowserState* GetBrowserStateToUse(
-      web::BrowserState* browser_state) const override;
-  bool ServiceIsNULLWhileTesting() const override;
-  bool ServiceIsCreatedWithBrowserState() const override;
 };
 
 #endif  // IOS_CHROME_BROWSER_HISTORY_MODEL_DOMAIN_DIVERSITY_REPORTER_FACTORY_H_
