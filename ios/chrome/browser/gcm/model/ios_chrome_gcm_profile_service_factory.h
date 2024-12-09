d055266b00c4713ab6b4ab4035ff9feefd01851f
Sylvain Defresne <sdefresne@chromium.org>
2024-12-09 15:51:29 +0000

    [ios] Convert IOSChromeGCMProfileServiceFactory to PKSFIOS
    Convert IOSChromeGCMProfileServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363215810
    Change-Id: Id7132f1367f8d7acf59ba7944c2644a345679aea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074958
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393676}

--- ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.h
+++ ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.h
@@ -10,9 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <string>
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ProfileIOS;
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 namespace gcm {
 class GCMProfileService;
@@ -20,16 +18,10 @@ class GCMProfileService;
 
 // Singleton that owns all GCMProfileService and associates them with
 // profiles.
-class IOSChromeGCMProfileServiceFactory
-    : public BrowserStateKeyedServiceFactory {
+class IOSChromeGCMProfileServiceFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static gcm::GCMProfileService* GetForProfile(ProfileIOS* profile);
   static IOSChromeGCMProfileServiceFactory* GetInstance();
-
-  IOSChromeGCMProfileServiceFactory(const IOSChromeGCMProfileServiceFactory&) =
-      delete;
-  IOSChromeGCMProfileServiceFactory& operator=(
-      const IOSChromeGCMProfileServiceFactory&) = delete;
 
   // Returns a string like "com.chrome.ios" that should be used as the GCM
   // category when an app_id is sent as a subtype instead of as a category. This
--- ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.mm
+++ ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.h"
 
 #import "base/functional/bind.h"
-#import "base/memory/ptr_util.h"
 #import "base/memory/ref_counted.h"
 #import "base/no_destructor.h"
 #import "base/task/sequenced_task_runner.h"
@@ -14,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "build/branding_buildflags.h"
 #import "components/gcm_driver/gcm_client_factory.h"
 #import "components/gcm_driver/gcm_profile_service.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
@@ -59,8 +57,8 @@ void RequestProxyResolvingSocketFactory(
 // static
 gcm::GCMProfileService* IOSChromeGCMProfileServiceFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<gcm::GCMProfileService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<gcm::GCMProfileService>(
+      profile, /*create=*/true);
 }
 
 // static
@@ -80,9 +78,7 @@ std::string IOSChromeGCMProfileServiceFactory::GetProductCategoryForSubtypes() {
 }
 
 IOSChromeGCMProfileServiceFactory::IOSChromeGCMProfileServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "GCMProfileService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("GCMProfileService") {
   DependsOn(IdentityManagerFactory::GetInstance());
 }
 
@@ -105,7 +101,6 @@ IOSChromeGCMProfileServiceFactory::BuildServiceInstanceFor(
       GetApplicationContext()->GetNetworkConnectionTracker(), ::GetChannel(),
       GetProductCategoryForSubtypes(),
       IdentityManagerFactory::GetForProfile(profile),
-      base::WrapUnique(new gcm::GCMClientFactory),
-      web::GetUIThreadTaskRunner({}), web::GetIOThreadTaskRunner({}),
-      blocking_task_runner);
+      std::make_unique<gcm::GCMClientFactory>(), web::GetUIThreadTaskRunner({}),
+      web::GetIOThreadTaskRunner({}), blocking_task_runner);
 }
