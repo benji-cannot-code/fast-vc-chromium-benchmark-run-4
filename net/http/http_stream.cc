f4918c535ea83d8626eb336f28738a069d06cd47
Keita Suzuki <suzukikeita@chromium.org>
2024-12-09 00:27:20 +0000

    Update `GetQuicErrorDetails` method to `GetQuicConnectionDetails`
    This CL expands `GetQuicErrorDetails` to `GetQuicConnectionDetails` to
    expand its use to general Quic Connection information including the
    connection migration related information. We also add
    `QuicConnectionDetails` struct to carry the information around.
    
    Bug: 379516116
    Change-Id: I6a91ef0de555367230000c1204cfb0d51960b392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6051119
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393471}

--- net/http/http_stream.cc
+++ net/http/http_stream.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-std::optional<HttpStream::QuicErrorDetails> HttpStream::GetQuicErrorDetails()
-    const {
+std::optional<HttpStream::QuicConnectionDetails>
+HttpStream::GetQuicConnectionDetails() const {
   return std::nullopt;
 }
 
--- net/http/http_stream.h
+++ net/http/http_stream.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "net/base/completion_once_callback.h"
+#include "net/base/connection_migration_information.h"
 #include "net/base/idempotency.h"
 #include "net/base/net_error_details.h"
 #include "net/base/net_errors.h"
@@ -209,7 +210,7 @@ class NET_EXPORT_PRIVATE HttpStream {
   // before any requests are made.
   virtual std::string_view GetAcceptChViaAlps() const = 0;
 
-  // Represents detailed QUIC errors returned by GetQuicErrorDetails().
+  // Represents detailed QUIC errors stored in `QuicConnectionDetails`.
   struct QuicErrorDetails {
     // Internal connection error of the stream.
     quic::QuicErrorCode connection_error = quic::QUIC_NO_ERROR;
@@ -221,11 +222,17 @@ class NET_EXPORT_PRIVATE HttpStream {
     uint64_t ietf_application_error = 0;
   };
 
+  // Represents details for QUIC connections.
+  struct QuicConnectionDetails {
+    QuicErrorDetails error;
+    ConnectionMigrationInformation connection_migration_info;
+  };
+
   // If `this` is using a QUIC stream, returns error details of the QUIC stream.
   // Otherwise returns nullopt. Detailed QUIC errors are only available after
   // the stream has been initialized. Use PopulateNetErrorDetails() for errors
   // that happened during the initialization.
-  virtual std::optional<QuicErrorDetails> GetQuicErrorDetails() const;
+  virtual std::optional<QuicConnectionDetails> GetQuicConnectionDetails() const;
 };
 
 }  // namespace net
