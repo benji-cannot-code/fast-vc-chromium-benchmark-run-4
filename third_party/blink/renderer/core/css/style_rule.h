33b14e76674bebfdc25c8df616610c7d6bfe2971
Anders Hartvoll Ruud <andruud@chromium.org>
2024-12-09 14:29:34 +0000

    [css-nesting] Implement CSSScopeRule::SetPreludeText by rule replacement
    CSSScopeRule::SetPreludeText is a function that replaces the prelude
    of some @scope rule. It is not web-exposed (i.e. not bound to any JS
    API), but it's used by Devtools to edit @scope rules dynamically.
    This function is currently implemented as modifying the contents
    of the StyleRuleScope in-place, which is an approach we're moving
    away from (see other work related to Issue 363019837).
    
    This CL instead implements the mutation as replacing the whole
    StyleRuleScope held by CSSScopeRule. This fixes nullptr-crashes
    for invalid preludes, but there should otherwise not be any behavior
    change.
    
    Bug: 363019837
    Change-Id: Iefdc96b334c26723ad4164045f741d64825fde01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6069931
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393632}

--- third_party/blink/renderer/core/css/style_rule.h
+++ third_party/blink/renderer/core/css/style_rule.h
@@ -52,7 +52,6 @@ class CascadeLayer;
 class CSSRule;
 class CSSStyleSheet;
 class ExecutionContext;
-class StyleSheetContents;
 
 class CORE_EXPORT StyleRuleBase : public GarbageCollected<StyleRuleBase> {
  public:
@@ -413,13 +412,6 @@ class CORE_EXPORT StyleRuleScope : public StyleRuleGroup {
   void TraceAfterDispatch(blink::Visitor*) const;
 
   const StyleScope& GetStyleScope() const { return *style_scope_; }
-
-  void SetPreludeText(const ExecutionContext*,
-                      String,
-                      CSSNestingType,
-                      StyleRule* parent_rule_for_nesting,
-                      StyleSheetContents* style_sheet);
-
  private:
   Member<const StyleScope> style_scope_;
 };
