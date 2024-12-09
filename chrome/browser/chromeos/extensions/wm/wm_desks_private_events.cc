403506c8f3e7eaf01009608443d67d2729cc148c
Georg Neis <neis@chromium.org>
2024-12-09 02:51:38 +0000

    ash: Remove bogus Lacros includes
    These includes should have been behind
    `#if BUILDFLAG(IS_CHROMEOS_LACROS)'. Since Lacros is not supported
    anymore we can simply delete them.
    
    [Submitting manually to unblock the tree.]
    
    Change-Id: I30f663b52b36ff2e50727ff0aaef47f31a088756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6076362
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Georg Neis <neis@chromium.org>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393482}

--- chrome/browser/chromeos/extensions/wm/wm_desks_private_events.cc
+++ chrome/browser/chromeos/extensions/wm/wm_desks_private_events.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/wm_desks_private.h"
-#include "chromeos/lacros/lacros_service.h"
 #include "content/public/browser/browser_context.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/browser/event_router.h"
--- chrome/browser/chromeos/printing/print_preview/print_preview_webcontents_manager_browsertest.cc
+++ chrome/browser/chromeos/printing/print_preview/print_preview_webcontents_manager_browsertest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chromeos/crosapi/mojom/print_preview_cros.mojom.h"
-#include "chromeos/lacros/lacros_service.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
