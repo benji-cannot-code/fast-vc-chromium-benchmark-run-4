61b7c660a2f4b765911f431ca8ed11b74b3c5821
Adrian Taylor <adetaylor@chromium.org>
2024-12-09 11:23:21 +0000

    page_load_in_process_fuzzer: disregard unexpected
    Some aspect of mutation appears able to put unexpected values in this
    proto field. Reject these test cases instead of crashing.
    
    Cq-Include-Trybots: luci.chromium.try:linux-centipede-asan-rel
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel
    Cq-Include-Trybots: luci.chromium.try:win-libfuzzer-asan-rel
    Bug: 376503843
    Change-Id: I735628d51a8a1471b7114011bdb0a651c6bd8bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6076645
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Auto-Submit: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393581}

--- chrome/test/fuzzing/page_load_in_process_fuzzer.cc
+++ chrome/test/fuzzing/page_load_in_process_fuzzer.cc
@@ -190,7 +190,7 @@ int PageLoadInProcessFuzzer::Fuzz(
         test_url = https_test_server2_.GetURL(path);
         break;
       default:
-        LOG(FATAL) << "Unexpected proto value for which server";
+        return -1;
     }
   }
 
