ef3d5430ba11ac24b65272c3076e10f4f41e5a1a
Sunggook Chue <sunggch@microsoft.com>
2024-12-10 20:32:14 +0000

    When stopping or disabling a remote audio track, do not disable clones.
    Cloned audio tracks in WebRTC share the same track interface as
    the original track, which serves as the source of the stream.
    
    When the track interface is disabled through a single audio track,
    it affects all tracks that share the same interface.
    
    The solution is to remove this dependency on the shared track interface
    when disabling or stopping an individual track.
    
    Bug: 40849402
    Change-Id: Ib4c54337cd10797449e856b420396038a45d30da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6052891
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394493}

--- content/test/data/media/webrtc_test_audio.js
+++ content/test/data/media/webrtc_test_audio.js
@@ -17,7 +17,7 @@ async function ensureAudioPlaying(audioTrack) {
 
 // Uses WebAudio's analyser of the on peerConnection's media stream to find
 // out whether audio is muted on the connection.
-async function ensureSilence(audioTrack) {
+async function ensureAudioSilence(audioTrack) {
   const result = await ensureSilenceOrPlayingForAudioTrack(
       audioTrack, /*checkPlaying*/ false);
   assertTrue(result);
@@ -46,8 +46,6 @@ async function ensureSilenceOrPlayingForAudioTrack(audioTrack, checkPlaying) {
 
   console.log('checkPlaying: ' + checkPlaying);
 
-  assertEquals(audioTrack.readyState, 'live');
-
   // Configure the analyser
   analyser.fftSize = 512;
   const dataArray = new Uint8Array(analyser.frequencyBinCount);
@@ -56,9 +54,8 @@ async function ensureSilenceOrPlayingForAudioTrack(audioTrack, checkPlaying) {
   mediaStreamSource.connect(analyser);
 
   return new Promise((resolve) => {
+    let detectionCount = 0;
     let attempts = 0;
-    let silentCount = 0;
-
     const checkSilence = () => {
       analyser.getByteFrequencyData(dataArray);
 
@@ -67,22 +64,28 @@ async function ensureSilenceOrPlayingForAudioTrack(audioTrack, checkPlaying) {
       const average = sum / dataArray.length;
       const decibels = 20 * Math.log10(average / 255);
 
-      if (decibels < threshold) {
-        silentCount++;
+      if (checkPlaying && decibels > threshold) {
+        detectionCount++;
+      } else if (!checkPlaying && decibels < threshold) {
+        detectionCount++;
       }
 
+      if (detectionCount == 1) {
+        attempts = 0;
+      }
       attempts++;
 
       console.log(
-          'decibels: ' + decibels + ' silentCount: ' + silentCount,
-          'attempts: ' + attempts);
+          'decibels: ' + decibels + ' detectionCount: ' + detectionCount);
 
-      if (silentCount === maxAttempts) {
-        console.log('Silence detected consistently.');
-        close(!checkPlaying);
-      } else if (attempts >= maxAttempts) {
-        console.log('Playing detected consistently.');
-        close(checkPlaying);
+      if (detectionCount === maxAttempts) {
+        // Once silence or playing is detected, it keeps the state.
+        assertEquals(attempts, maxAttempts);
+
+        console.log(
+            checkPlaying ? 'Playing detected consistently.' :
+                           'Silence detected consistently.');
+        close();
       }
     };
 
@@ -96,11 +99,11 @@ async function ensureSilenceOrPlayingForAudioTrack(audioTrack, checkPlaying) {
       resolve(false);
     };
 
-    function close(result) {
+    function close() {
       audioContext.close().then(() => {
         console.log('AudioContext closed.');
         clearInterval(intervalId);
-        resolve(result);
+        resolve(true);
       });
     }
   });
