34c0ab1e265dde0cdba7e4b33478a6c87e993015
Sylvain Defresne <sdefresne@chromium.org>
2024-12-10 16:06:57 +0000

    [ios] Convert HostContentSettingsMapFactory to PKSFIOS
    Convert HostContentSettingsMapFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363215620
    Change-Id: I6338a42df5ef419a6a725e5525a4028fad238ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085195
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394326}

--- ios/chrome/browser/content_settings/model/host_content_settings_map_factory.cc
+++ ios/chrome/browser/content_settings/model/host_content_settings_map_factory.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
-#include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/prefs/pref_service.h"
-#include "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/shared/model/profile/profile_ios.h"
 
 namespace ios {
@@ -17,8 +15,9 @@ namespace ios {
 // static
 HostContentSettingsMap* HostContentSettingsMapFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<HostContentSettingsMap*>(
-      GetInstance()->GetServiceForBrowserState(profile, true).get());
+  return GetInstance()
+      ->GetServiceForProfileAs<HostContentSettingsMap>(profile, /*create=*/true)
+      .get();
 }
 
 // static
@@ -28,11 +27,11 @@ HostContentSettingsMapFactory* HostContentSettingsMapFactory::GetInstance() {
 }
 
 HostContentSettingsMapFactory::HostContentSettingsMapFactory()
-    : RefcountedBrowserStateKeyedServiceFactory(
+    : RefcountedProfileKeyedServiceFactoryIOS(
           "HostContentSettingsMap",
-          BrowserStateDependencyManager::GetInstance()) {}
+          ProfileSelection::kOwnInstanceInIncognito) {}
 
-HostContentSettingsMapFactory::~HostContentSettingsMapFactory() {}
+HostContentSettingsMapFactory::~HostContentSettingsMapFactory() = default;
 
 bool HostContentSettingsMapFactory::ServiceIsRequiredForContextInitialization()
     const {
@@ -45,25 +44,14 @@ bool HostContentSettingsMapFactory::ServiceIsRequiredForContextInitialization()
 scoped_refptr<RefcountedKeyedService>
 HostContentSettingsMapFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
-  ProfileIOS* profile = ProfileIOS::FromBrowserState(context);
-  if (profile->IsOffTheRecord()) {
-    // If off-the-record, retrieve the host content settings map of the parent
-    // profile to ensure the preferences have been migrated.
-    GetForProfile(profile->GetOriginalProfile());
-  }
-
   // TODO(crbug.com/40130635): Set restore_session to whether or not the phone
   // has been reset, which would mirror iOS's cookie store.
+  ProfileIOS* profile = ProfileIOS::FromBrowserState(context);
   const bool is_off_the_record = profile->IsOffTheRecord();
   const bool should_record_metrics = !is_off_the_record;
   return base::MakeRefCounted<HostContentSettingsMap>(
-      profile->GetPrefs(), is_off_the_record, false /* store_last_modified */,
-      false /*restore_session*/, should_record_metrics);
-}
-
-web::BrowserState* HostContentSettingsMapFactory::GetBrowserStateToUse(
-    web::BrowserState* context) const {
-  return GetBrowserStateOwnInstanceInIncognito(context);
+      profile->GetPrefs(), is_off_the_record, /*store_last_modified=*/false,
+      /*restore_session=*/false, should_record_metrics);
 }
 
 }  // namespace ios
--- ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h
+++ ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h
@@ -8,23 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/ref_counted.h"
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/refcounted_browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/refcounted_profile_keyed_service_factory_ios.h"
 
 class HostContentSettingsMap;
-class ProfileIOS;
 
 namespace ios {
+
 // Singleton that owns all HostContentSettingsMaps and associates them with
 // profiles.
 class HostContentSettingsMapFactory
-    : public RefcountedBrowserStateKeyedServiceFactory {
+    : public RefcountedProfileKeyedServiceFactoryIOS {
  public:
   static HostContentSettingsMap* GetForProfile(ProfileIOS* profile);
   static HostContentSettingsMapFactory* GetInstance();
-
-  HostContentSettingsMapFactory(const HostContentSettingsMapFactory&) = delete;
-  HostContentSettingsMapFactory& operator=(
-      const HostContentSettingsMapFactory&) = delete;
 
  private:
   friend class base::NoDestructor<HostContentSettingsMapFactory>;
@@ -35,8 +31,6 @@ class HostContentSettingsMapFactory
   // BrowserStateKeyedServiceFactory implementation.
   bool ServiceIsRequiredForContextInitialization() const override;
   scoped_refptr<RefcountedKeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
