14dc81e9e817ab6e332b954e04a8e42824a8ff34
Noam Rosenthal <nrosenthal@chromium.org>
2024-12-10 17:06:19 +0000

    LCP: Clean up `firstAnimatedFrameTime`
    This timing is baked into the `startTime`, and only has residual
    code, and spuriously exposed in toJSON.
    
    Bug: 381270287
    Change-Id: I5b6c2eb920a715ae03c162e1d791cbed96e50f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6084898
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394358}

--- third_party/blink/renderer/core/timing/largest_contentful_paint.h
+++ third_party/blink/renderer/core/timing/largest_contentful_paint.h
@@ -24,7 +24,6 @@ class CORE_EXPORT LargestContentfulPaint final : public PerformanceEntry {
                          DOMHighResTimeStamp render_time,
                          uint64_t size,
                          DOMHighResTimeStamp load_time,
-                         DOMHighResTimeStamp first_animated_frame_time,
                          const AtomicString& id,
                          const String& url,
                          Element* element,
@@ -38,9 +37,6 @@ class CORE_EXPORT LargestContentfulPaint final : public PerformanceEntry {
   uint64_t size() const { return size_; }
   DOMHighResTimeStamp renderTime() const { return render_time_; }
   DOMHighResTimeStamp loadTime() const { return load_time_; }
-  DOMHighResTimeStamp firstAnimatedFrameTime() const {
-    return first_animated_frame_time_;
-  }
   const AtomicString& id() const { return id_; }
   const String& url() const { return url_; }
   Element* element() const;
@@ -53,7 +49,6 @@ class CORE_EXPORT LargestContentfulPaint final : public PerformanceEntry {
   uint64_t size_;
   DOMHighResTimeStamp render_time_;
   DOMHighResTimeStamp load_time_;
-  DOMHighResTimeStamp first_animated_frame_time_;
   AtomicString id_;
   String url_;
   WeakMember<Element> element_;
