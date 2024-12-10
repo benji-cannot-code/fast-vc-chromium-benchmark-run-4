8e46ba328f205e5e7dbceb784b28fd02f7b34c81
Frank Liberato <liberato@chromium.org>
2024-12-10 22:56:42 +0000

    [pip] Don't exit pip while screen off / keyguard on
    Android can get into a bad state if one exits pip while the screen
    is off or the keyguard is on.  When this happens, the next attempt
    to switch to landscape mode will cause the window contents to
    disappear.  Instead, we delay exiting pip in this case, and exit
    once the device becomes interactible again.
    
    Bug: 376727750
    Change-Id: Iadb24d759d371ed68e7540ed4ede9dc8de6a65b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963216
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1394543}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -957,6 +957,10 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         }
 
         mRootUiCoordinator.restoreUiState(getSavedInstanceState());
+
+        if (mFullscreenVideoPictureInPictureController != null) {
+            mFullscreenVideoPictureInPictureController.onStart();
+        }
     }
 
     /**
@@ -1098,7 +1102,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         }
         if (mFullscreenVideoPictureInPictureController != null) {
             Log.i(TAG, "onResumeWithNative: exiting picture in picture if needed");
-            mFullscreenVideoPictureInPictureController.onFrameworkExitedPictureInPicture();
+            mFullscreenVideoPictureInPictureController.onResume();
         }
 
         getManualFillingComponent().onResume();
@@ -1230,10 +1234,6 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
 
     @Override
     public void onStopWithNative() {
-        if (mFullscreenVideoPictureInPictureController != null) {
-            mFullscreenVideoPictureInPictureController.onStop();
-            mFullscreenVideoPictureInPictureController = null;
-        }
         super.onStopWithNative();
         endUmaSession();
     }
@@ -1531,6 +1531,12 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             }
             compositorViewHolder.shutDown();
             mCompositorViewHolderSupplier.set(null);
+        }
+
+        // crbug.com/352365937: Let the pip controller clear up to prevent a leak.
+        if (mFullscreenVideoPictureInPictureController != null) {
+            mFullscreenVideoPictureInPictureController.onDestroy();
+            mFullscreenVideoPictureInPictureController = null;
         }
 
         onDestroyInternal();
