cb552fb26ca5c16ac05ac6f771f2a709b58c0af9
Andrew Grieve <agrieve@chromium.org>
2024-12-10 21:50:29 +0000

    Android: Add NullUtil.assumeNonNull()
    This will tell NullAway that we promise a type is non-null.
    See: https://github.com/uber/NullAway/wiki/Suppressing-Warnings#downcasting
    
    Bug: 40657579
    Change-Id: I671dfedcf7da8e57361017b6a3d6f1f5144974a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085726
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394524}

---
+++ build/android/java/src/org/chromium/build/NullUtil.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.build;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.NullUnmarked;
+import org.chromium.build.annotations.Nullable;
+
+/** Misc methods related to build tools. */
+@NullMarked
+public class NullUtil {
+    private NullUtil() {}
+
+    /**
+     * Tell NullAway that the given parameter is non-null.
+     *
+     * <p>Prefer this over "assert foo != null;", since asserts will lead to different crash
+     * signatures in Canary vs non-Canary (since asserts enabled only on Canary).
+     *
+     * <p>For when a runtime check is meritted, use Objects.requireNonNull().
+     *
+     * <p>See: https://github.com/uber/NullAway/wiki/Suppressing-Warnings#downcasting
+     */
+    @NullUnmarked
+    public static <T> T assumeNonNull(@Nullable T object) {
+        return object;
+    }
+}
