89f3fdc931fa3e6de9f0d67288dff61ebbd36e14
Fredrik Söderquist <fs@opera.com>
2024-12-10 11:07:37 +0000

    spanification: WebData: Add span constructor
    Update most users of the WebData(pointer, size) constructor to use it.
    
    Bug: 351564777
    Change-Id: I451a718aec54f41968443747613802e809ac1cfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080539
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394187}

--- third_party/blink/public/platform/web_data.h
+++ third_party/blink/public/platform/web_data.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_DATA_H_
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_DATA_H_
 
+#include "base/containers/span.h"
 #include "third_party/blink/public/platform/web_common.h"
 #include "third_party/blink/public/platform/web_private_ptr.h"
 #include "third_party/blink/public/platform/web_vector.h"
@@ -53,6 +54,8 @@ class BLINK_PLATFORM_EXPORT WebData {
 
   WebData() = default;
 
+  explicit WebData(base::span<const uint8_t> data)
+      : WebData(reinterpret_cast<const char*>(data.data()), data.size()) {}
   WebData(const char* data, size_t size) { Assign(data, size); }
 
   template <int N>
