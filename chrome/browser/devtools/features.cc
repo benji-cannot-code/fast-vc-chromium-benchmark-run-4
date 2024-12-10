57329a8ce18e1bbf98bba62c18a7dcd7468b8837
Ergün Erdoğmuş <ergunsh@chromium.org>
2024-12-10 14:59:23 +0000

    [DevTools] Add a feature flag for showing animation styles in styles tab
    Bug: 349566291
    Change-Id: I0328d05c4327caeaa938dd037aa02b503bb34d09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6084996
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394266}

--- chrome/browser/devtools/features.cc
+++ chrome/browser/devtools/features.cc
@@ -118,4 +118,9 @@ BASE_FEATURE(kDevToolsVeLogging,
 const base::FeatureParam<bool> kDevToolsVeLoggingTesting{
     &kDevToolsVeLogging, "testing", /*default_value=*/false};
 
+// Whether showing animation styles in the styles tab is enabled.
+BASE_FEATURE(kDevToolsAnimationStylesInStylesTab,
+             "DevToolsAnimationStylesInStylesTab",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
