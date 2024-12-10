57e643e953c1bfe27ad7b002ba8db944f4e11dc4
Brandon Wylie <wylieb@chromium.org>
2024-12-10 17:47:21 +0000

    [hub] Fix black screen flash for switch-to-tab animation
    Bug: 381325000
    Change-Id: I5cd447454fa9b736246568d8878bd24a6d200ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6081855
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394380}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
@@ -853,8 +853,8 @@ public class SearchActivity extends AsyncInitializationActivity
     }
 
     private void bringTabToFront(Tab tab) {
-        IntentHandler.bringTabToFront(tab);
         finish(TerminationReason.BRING_TAB_TO_FRONT, /* loadUrlParams= */ null);
+        IntentHandler.bringTabToFront(tab);
     }
 
     /* package */ void setLocationBarCoordinatorForTesting(LocationBarCoordinator coordinator) {
