cfb4a757ca585b9e2a242a2ec0c9617de122e110
Sylvain Defresne <sdefresne@chromium.org>
2024-12-10 12:30:02 +0000

    [ios] Convert SupervisedUserMetricsServiceFactory to PKSFIOS
    Convert SupervisedUserMetricsServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363218195
    Change-Id: I7b82c3ef8b1b92ee02375109923efbefd4d1789a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6076177
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394208}

--- ios/chrome/browser/supervised_user/model/supervised_user_metrics_service_factory.h
+++ ios/chrome/browser/supervised_user/model/supervised_user_metrics_service_factory.h
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_SUPERVISED_USER_MODEL_SUPERVISED_USER_METRICS_SERVICE_FACTORY_H_
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ProfileIOS;
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 namespace supervised_user {
 class SupervisedUserMetricsService;
@@ -18,16 +16,11 @@ class SupervisedUserMetricsService;
 // Singleton that owns SupervisedUserMetricsService object and associates
 // them with ProfileIOS.
 class SupervisedUserMetricsServiceFactory
-    : public BrowserStateKeyedServiceFactory {
+    : public ProfileKeyedServiceFactoryIOS {
  public:
   static supervised_user::SupervisedUserMetricsService* GetForProfile(
       ProfileIOS* profile);
   static SupervisedUserMetricsServiceFactory* GetInstance();
-
-  SupervisedUserMetricsServiceFactory(
-      const SupervisedUserMetricsServiceFactory&) = delete;
-  SupervisedUserMetricsServiceFactory& operator=(
-      const SupervisedUserMetricsServiceFactory&) = delete;
 
  private:
   friend class base::NoDestructor<SupervisedUserMetricsServiceFactory>;
