ea59fdca594781e224270018947c015e39bd1832
Daniel Murphy <dmurph@chromium.org>
2024-12-10 20:31:04 +0000

    [PWA/NavCapture] Disable navigation capturing in incognito
    R=mek@chromium.org
    
    Fixed: 383216501
    Change-Id: I769b48a10dfa3762805e42dcb14fa6d00a42a87c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085126
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394491}

--- chrome/browser/ui/web_applications/web_app_launch_utils.cc
+++ chrome/browser/ui/web_applications/web_app_launch_utils.cc
@@ -1466,7 +1466,7 @@ ClientModeAndBrowser GetEffectiveClientModeAndBrowserForCapturing(
 AppNavigationResult MaybeHandleAppNavigation(const NavigateParams& params) {
   Profile* profile = params.initiating_profile;
 
-  if (!AreWebAppsEnabled(profile) ||
+  if (!AreWebAppsUserInstallable(profile) ||
       Browser::GetCreationStatusForProfile(profile) !=
           Browser::CreationStatus::kOk ||
       !params.url.is_valid()) {
@@ -1898,7 +1898,7 @@ void OnWebAppNavigationAfterWebContentsCreation(
     // capturing wasn't enabled for the navigation.
     return;
   }
-  CHECK(AreWebAppsEnabled(params.initiating_profile));
+  CHECK(AreWebAppsUserInstallable(params.initiating_profile));
   CHECK(!(params.force_open_pwa_window && params.open_pwa_window_if_possible));
 
   std::optional<webapps::AppId> first_navigation_app_id =
