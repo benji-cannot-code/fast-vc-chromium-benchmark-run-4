289ec9d996d79edf61552dbc2c588e25fd11c10d
Mickey Burks <mickeyburks@chromium.org>
2024-12-10 18:22:34 +0000

    [TabGroupSync] Add callback list for collaboration message changes
    This callback list is notified after messages are set or cleared for
    this tab. This will be used for the page action since it needs to
    update itself while being displayed.
    
    Bug: 383310609
    Change-Id: Ib83d45c8049bbe6f9ab979e914fc56a1c84608cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085855
    Reviewed-by: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394401}

--- chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_tab_data_unittest.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_tab_data_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_tab_data.h"
 
+#include "base/test/mock_callback.h"
 #include "components/collaboration/public/messaging/message.h"
 #include "components/data_sharing/public/group_data.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -72,6 +73,26 @@ TEST_F(CollaborationMessagingTabDataTest, CanSetAndClearData) {
   EXPECT_EQ(tab_data().collaboration_event(), CollaborationEvent::TAB_UPDATED);
 
   tab_data().ClearMessage(message2);
+  EXPECT_FALSE(tab_data().HasMessage());
+}
+
+TEST_F(CollaborationMessagingTabDataTest, NotifiesListeners) {
+  EXPECT_FALSE(tab_data().HasMessage());
+
+  auto message = CreateMessage("User", "URL", CollaborationEvent::TAB_ADDED);
+
+  base::MockCallback<CollaborationMessagingTabData::CallbackList::CallbackType>
+      cb;
+  auto subscription = tab_data().RegisterMessageChangedCallback(cb.Get());
+
+  // Callback is called when message is set.
+  EXPECT_CALL(cb, Run);
+  tab_data().SetMessage(message);
+  EXPECT_TRUE(tab_data().HasMessage());
+
+  // Callback is called again when message is cleared.
+  EXPECT_CALL(cb, Run);
+  tab_data().ClearMessage(message);
   EXPECT_FALSE(tab_data().HasMessage());
 }
 
