b37909eca79c07e3cc5ec683ea95fd5ffdbd737f
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-12-10 09:09:15 +0000

    Spanify: Fix CanElideBracesForStdArrayInitialization
    While rewriting Chrome again, I observed the tool is eliding some braces
    wrongly. The previous logic about not containing directly InitListExpr
    is not good enough, as some expression are implicitly wrapping them.
    For instance, the CXXConstructExpr is wrapping the InitListExpr.
    
    For instance, a std::vector<int>[2] can be initialized with:
    {
      {1, 2, 3},
      {4, 5, 6},
    }
    
    InitListExpr 0x560cc0182e00 'std::vector<int>[2]'
    |-CXXConstructExpr 0x560cc0182f30 'std::vector<int>':'class std::vector<int>' 'void (initializer_list<value_type>, const allocator_type &)' list std::initializer_list
    | |-CXXStdInitializerListExpr 0x560cc0182ec0 'initializer_list<value_type>':'class std::initializer_list<int>'
    | | `-MaterializeTemporaryExpr 0x560cc0182ea8 'const int[3]' xvalue
    | |   `-InitListExpr 0x560cc0182e50 'const int[3]'
    | |     |-IntegerLiteral 0x560cc0182c40 'int' 1
    | |     |-IntegerLiteral 0x560cc0182c60 'int' 2
    | |     `-IntegerLiteral 0x560cc0182c80 'int' 3
    
    This patch fixes the issue by checking the first character of the
    element not to be '{'. This is easier to think about instead of covering
    the whole llvm clang hierarchy.
    
    Bug: None
    Change-Id: I399743ab6126272f1147d084d02349ce21712c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6079411
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394144}

--- tools/clang/spanify/tests/array-with-init-list-original.cc
+++ tools/clang/spanify/tests/array-with-init-list-original.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <string>
+#include <vector>
 
 namespace {
 
@@ -165,4 +166,30 @@ void test_brace_elision_computed_size() {
   // std::array<Aggregate, size> buffer = {{{1, 2}, {3, 4}}};
   Aggregate buffer[size] = {{1, 2}, {3, 4}};
   std::ignore = buffer[0];
+}
+
+void test_with_nested_vector() {
+  // Expected rewrite:
+  // auto data = std::to_array<std::vector<int>>({
+  //     {1, 2, 3},
+  //     {4, 5, 6},
+  // });
+  std::vector<int> data[] = {
+      {1, 2, 3},
+      {4, 5, 6},
+  };
+  std::ignore = data[0];
+}
+
+void test_with_nested_vector_const_size() {
+  // Expected rewrite:
+  // std::array<std::vector<int>, 2> data = {{
+  //    {1, 2, 3},
+  //    {4, 5, 6},
+  // }};
+  std::vector<int> data[2] = {
+      {1, 2, 3},
+      {4, 5, 6},
+  };
+  std::ignore = data[0];
 }
