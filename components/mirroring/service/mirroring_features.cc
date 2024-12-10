b9e2f2c2d68cbbc26c2cf15496278f60294368c0
Alexandra Reimers <areimers@chromium.org>
2024-12-10 08:08:32 +0000

    Code Health: Clean up stale base::Feature "CastDisableLetterboxing"
    This feature has been disabled by default for a long time and has no Finch. Let's clean it up.
    
    Bug: 356236272, 250963833
    Change-Id: I9dfeebb0e629c236c7ed3faea7552f8b26b24d31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6079573
    Commit-Queue: Alexandra Reimers <areimers@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394127}

--- components/mirroring/service/mirroring_features.cc
+++ components/mirroring/service/mirroring_features.cc
@@ -7,15 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mirroring {
 namespace features {
-
-// The mirroring sender has the ability to letterbox video frames to match the
-// aspect ratio of the reciever's display.  However, receivers can handle
-// variable aspect ratio video so this is not needed any more.
-// TODO(crbug.com/1363512):  Remove support for sender side letterboxing.
-BASE_FEATURE(kCastDisableLetterboxing,
-             "CastDisableLetterboxing",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // The mirroring service previously used a model name filter before even
 // attempting to query the receiver for media remoting support. This
 // flag disables this behavior and queries all devices for remoting support.
