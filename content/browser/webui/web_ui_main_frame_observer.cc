21ce7096ab6aff5b36e192dc033b6b9c772eaa58
Keren Zhu <kerenzhu@chromium.org>
2024-12-10 19:12:24 +0000

    Revert "Add histogram WebUI.ShownUrl"
    This reverts commit a7f989bbf272622d112b7ff8afdd371099ab3180.
    
    Reason for revert: this histogram might log Tab Search that is not
    actually shown. See https://crbug.com/375209495#comment12.
    
    Per dpapad@ suggestion, reverting this histogram first. The reland will
    be at https://crrev.com/c/6067108.
    
    Original change's description:
    > Add histogram WebUI.ShownUrl
    >
    > WebUI.ShownUrl is a new histogram to track when a WebUI is actually
    > displayed to the user. This complements the existing histogram
    > WebUI.CreatedForUrl which logs WebUI object creation and is being skewed
    > by the preloading.
    >
    > WebUI.ShownUrl is logged once on a WebContents'
    > DidFirstVisuallyNonEmptyPaint(). It does not log a WebUI if it is not
    > shown (e.g. opened in a background tab, preloaded but not evetually
    > shown).
    >
    > Bug: 375209495
    > Change-Id: I5794cb5262e1fbc7e066c463aa257f7a18f4bc8e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5984696
    > Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Elly FJ <ellyjones@chromium.org>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1378482}
    
    Bug: 375209495
    Change-Id: I215ea74f8fa032eb72adc9c2e6436e6637c5d9a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075410
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394436}

--- content/browser/webui/web_ui_main_frame_observer.cc
+++ content/browser/webui/web_ui_main_frame_observer.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crash/content/browser/error_reporting/js_error_report_processor.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/webui/web_ui_impl.h"
-#include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -177,33 +176,15 @@ void WebUIMainFrameObserver::ReadyToCommitNavigation(
   web_ui_->GetController()->WebUIReadyToCommitNavigation(
       web_ui_->GetRenderFrameHost());
 
-  GetContentClient()->browser()->LogWebUICreated(GetUrlForLogging());
-  pending_non_empty_paint_ = true;
-
+  GURL site_url =
+      web_ui_->GetRenderFrameHost()->GetSiteInstance()->GetSiteURL();
+  GetContentClient()->browser()->LogWebUIUrl(site_url);
   MaybeEnableWebUIJavaScriptErrorReporting(navigation_handle);
 }
 
 void WebUIMainFrameObserver::PrimaryPageChanged(Page& page) {
   web_ui_->DisallowJavascriptOnAllHandlers();
   web_ui_->GetController()->WebUIPrimaryPageChanged(page);
-}
-
-void WebUIMainFrameObserver::DidFirstVisuallyNonEmptyPaint() {
-  // Ignore paint events if we are not expecting one. They could come
-  // from frames that are not associated with this WebUI.
-  if (!pending_non_empty_paint_) {
-    return;
-  }
-  pending_non_empty_paint_ = false;
-
-  GetContentClient()->browser()->LogWebUIShown(GetUrlForLogging());
-}
-
-const GURL& WebUIMainFrameObserver::GetUrlForLogging() const {
-  // This returns the actual WebUI url which can differ from the visible
-  // URL (e.g. chrome://newtab can be rewrote to chrome://new-tab-page or
-  // chrome://new-tab-page-third-party)
-  return web_ui_->GetRenderFrameHost()->GetSiteInstance()->GetSiteURL();
 }
 
 }  // namespace content
