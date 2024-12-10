34e205be4b7fe9472a15a76eb2e5d99f24338149
Alexandra Reimers <areimers@chromium.org>
2024-12-10 10:39:44 +0000

    Code Health: Clean up stale base::Feature "ConversionMeasurementAPIAlternativeUsage"
    This feature has been enabled by default for a long time. Let's clean up the flag.
    
    Bug: 356236449
    Change-Id: Iccf447b2cdbd87dc890d8d111f2eb461d09670aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080351
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Alexandra Reimers <areimers@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394178}

--- components/embedder_support/origin_trials/features.cc
+++ components/embedder_support/origin_trials/features.cc
@@ -17,9 +17,4 @@ namespace embedder_support {
 BASE_FEATURE(kOriginTrialsSampleAPIThirdPartyAlternativeUsage,
              "OriginTrialsSampleAPIThirdPartyAlternativeUsage",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kConversionMeasurementAPIAlternativeUsage,
-             "ConversionMeasurementAPIAlternativeUsage",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 }  // namespace embedder_support
