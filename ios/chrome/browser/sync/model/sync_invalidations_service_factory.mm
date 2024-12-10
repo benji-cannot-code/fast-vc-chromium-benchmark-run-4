a7444b16c39bedfd101fdf34482fa42647f32d01
Sylvain Defresne <sdefresne@chromium.org>
2024-12-10 12:17:20 +0000

    [ios] Convert SyncInvalidationsServiceFactory to PKSFIOS
    Convert SyncInvalidationsServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217671
    Change-Id: I4a96b0e92281eb41d632f7dcdadc9c8552b3a819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080340
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394203}

--- ios/chrome/browser/sync/model/sync_invalidations_service_factory.mm
+++ ios/chrome/browser/sync/model/sync_invalidations_service_factory.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/no_destructor.h"
 #import "components/gcm_driver/gcm_profile_service.h"
 #import "components/gcm_driver/instance_id/instance_id_profile_service.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/sync/invalidations/sync_invalidations_service_impl.h"
 #import "ios/chrome/browser/gcm/model/instance_id/ios_chrome_instance_id_profile_service_factory.h"
 #import "ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.h"
@@ -17,8 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // static
 syncer::SyncInvalidationsService*
 SyncInvalidationsServiceFactory::GetForProfile(ProfileIOS* profile) {
-  return static_cast<syncer::SyncInvalidationsService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()
+      ->GetServiceForProfileAs<syncer::SyncInvalidationsService>(
+          profile, /*create=*/true);
 }
 
 // static
@@ -29,9 +29,7 @@ SyncInvalidationsServiceFactory::GetInstance() {
 }
 
 SyncInvalidationsServiceFactory::SyncInvalidationsServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "SyncInvalidationsService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("SyncInvalidationsService") {
   DependsOn(IOSChromeGCMProfileServiceFactory::GetInstance());
   DependsOn(IOSChromeInstanceIDProfileServiceFactory::GetInstance());
 }
