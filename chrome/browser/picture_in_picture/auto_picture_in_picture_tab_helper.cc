a8c52cf0e36ee17bee6bedef6f834127c63498b0
Benjamin Keen <bkeen@google.com>
2024-12-10 00:45:14 +0000

    Skip media engagement check if content setting is set to allow
    As per T&S requirements, this CL updates AutoPiP tab helper to skip
    media engagement score checks when content setting is set to allow.
    
    This CL also includes a minor cleanup that updates
    `ScheduleUrlSafetyCheck` to use the existing
    `GetPrimaryMainRoutedFrame` method, rather than duplicating that logic.
    
    Bug: 368058093
    Change-Id: Ic11dac9105c6f5b731863422ee858229eea6e022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6081515
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394006}

--- chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper.cc
+++ chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper.cc
@@ -312,7 +312,7 @@ bool AutoPictureInPictureTabHelper::MeetsVideoPlaybackConditions() const {
   }
 
   return has_audio_focus_ && is_playing_ && WasRecentlyAudible() &&
-         has_safe_url_ && MeetsEngagementScore() &&
+         has_safe_url_ && MeetsMediaEngagementConditions() &&
          has_sufficiently_visible_video_;
 }
 
@@ -331,7 +331,12 @@ bool AutoPictureInPictureTabHelper::WasRecentlyAudible() const {
   return audible_helper->WasRecentlyAudible();
 }
 
-bool AutoPictureInPictureTabHelper::MeetsEngagementScore() const {
+bool AutoPictureInPictureTabHelper::MeetsMediaEngagementConditions() const {
+  // Skip checking media engagement when content setting is set to allow.
+  if (GetCurrentContentSetting() == CONTENT_SETTING_ALLOW) {
+    return true;
+  }
+
   if (!media_engagement_service_) {
     return false;
   }
@@ -395,8 +400,8 @@ void AutoPictureInPictureTabHelper::ScheduleUrlSafetyCheck() {
   CHECK(g_browser_process);
   CHECK(g_browser_process->safe_browsing_service());
 
-  auto* rfh = content::MediaSession::Get(web_contents())->GetRoutedFrame();
-  if (!rfh || !rfh->IsInPrimaryMainFrame()) {
+  std::optional<content::RenderFrameHost*> rfh = GetPrimaryMainRoutedFrame();
+  if (!rfh) {
     return;
   }
 
@@ -411,7 +416,8 @@ void AutoPictureInPictureTabHelper::ScheduleUrlSafetyCheck() {
                             async_tasks_weak_factory_.GetWeakPtr()));
   }
 
-  safe_browsing_checker_client_->CheckUrlSafety(rfh->GetLastCommittedURL());
+  safe_browsing_checker_client_->CheckUrlSafety(
+      rfh.value()->GetLastCommittedURL());
 }
 
 void AutoPictureInPictureTabHelper::EnsureAutoPipSettingHelper() {
