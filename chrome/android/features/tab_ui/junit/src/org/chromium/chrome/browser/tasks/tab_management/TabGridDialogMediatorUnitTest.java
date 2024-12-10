ccb27176eb48f8862b3ef52af638956c3bdd9533
Tommy Nyquist <nyquist@chromium.org>
2024-12-10 16:54:41 +0000

    [messaging] Only query for dirty tabs for tab grid message
    Before this CL, we query for all data when looking for changes to a tab
    group, by looking for currently dirty persistent messages.
    
    This ends up including both DIRTY_TAB and CHIP type of messages for each
    dirty tab, which leads to double-counting of every tab.
    
    This CL specifies that we only care about the DIRTY_TAB (dirty blue dot)
    for each tab, making the count correct.
    
    When the original CL for this functionality landed, the backend was not
    possible to use for testing, so we were not sure how the query should
    work: https://chromium-review.googlesource.com/c/chromium/src/+/5909992
    
    R=skym@chromium.org
    
    Bug: 345856643, 369188289
    Change-Id: If4eebc09a2f903f5d9f0ddd12f75fe70f69064b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6081517
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394352}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediatorUnitTest.java
@@ -104,6 +104,7 @@ import org.chromium.components.collaboration.messaging.CollaborationEvent;
 import org.chromium.components.collaboration.messaging.MessageAttribution;
 import org.chromium.components.collaboration.messaging.MessagingBackendService;
 import org.chromium.components.collaboration.messaging.PersistentMessage;
+import org.chromium.components.collaboration.messaging.PersistentNotificationType;
 import org.chromium.components.collaboration.messaging.TabGroupMessageMetadata;
 import org.chromium.components.collaboration.messaging.TabMessageMetadata;
 import org.chromium.components.data_sharing.DataSharingService;
@@ -125,6 +126,7 @@ import org.chromium.url.GURL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /** Tests for {@link TabGridDialogMediator}. */
 @SuppressWarnings({"ArraysAsListWithZeroOrOneArgument", "ResultOfMethodCallIgnored"})
@@ -1817,6 +1819,8 @@ public class TabGridDialogMediatorUnitTest {
         for (int i = 0; i < removed; i++) {
             messageList.add(makePersistentMessage(CollaborationEvent.TAB_REMOVED));
         }
-        when(mMessagingBackendService.getMessagesForGroup(any(), any())).thenReturn(messageList);
+        when(mMessagingBackendService.getMessagesForGroup(
+                        any(), eq(Optional.of(PersistentNotificationType.DIRTY_TAB))))
+                .thenReturn(messageList);
     }
 }
