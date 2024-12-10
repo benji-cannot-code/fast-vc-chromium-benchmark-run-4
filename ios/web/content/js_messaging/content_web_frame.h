693322c76188d92a0e45fd3c38021c07cef84869
Mike Dougherty <michaeldo@chromium.org>
2024-12-10 21:34:15 +0000

    Remove ContentWebFrame::GetSecurityOriginDeprecated
    Bug: 40260077
    Change-Id: Iad78a2e3677d1626dd6f088c3085f4f64526e70e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085722
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394518}

--- ios/web/content/js_messaging/content_web_frame.h
+++ ios/web/content/js_messaging/content_web_frame.h
@@ -44,7 +44,6 @@ class ContentWebFrame : public WebFrame,
   WebFrameInternal* GetWebFrameInternal() override;
   std::string GetFrameId() const override;
   bool IsMainFrame() const override;
-  GURL GetSecurityOriginDeprecated() const override;
   url::Origin GetSecurityOrigin() const override;
   BrowserState* GetBrowserState() override;
   base::WeakPtr<WebFrame> AsWeakPtr() override;
