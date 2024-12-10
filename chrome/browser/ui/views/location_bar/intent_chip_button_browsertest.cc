dedc26d63e665d43d7d4458faf189e3169fe8f03
Dibyajyoti Pal <dibyapal@google.com>
2024-12-10 15:44:12 +0000

    [PWA] Speculative fix for IntentPicker bubble view tests on Mac
    This CL provides a speculative fix for the IntentPicker bubble view
    tests on Mac by waiting for the threadpool instance to finish running
    all tasks before progressing, since the intent picker logic uses the
    threadpool to get Mac application information using universal links.
    
    Also removes the duplicate IconVisibilityAfterTabSwitching test that
    is implemented under IntentPickerIconBrowserTest.
    
    Bug: 319217054
    Change-Id: Idb9b685ed43d357fb5336a857f4f9f752d77ca81
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6081386
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394305}

--- chrome/browser/ui/views/location_bar/intent_chip_button_browsertest.cc
+++ chrome/browser/ui/views/location_bar/intent_chip_button_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/views/location_bar/intent_chip_button.h"
+
 #include <memory>
 #include <utility>
 
@@ -10,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
 #include "base/scoped_observation.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -27,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/test/test_browser_ui.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
-#include "chrome/browser/ui/views/location_bar/intent_chip_button.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h"
@@ -90,6 +92,13 @@ class IntentChipButtonBrowserTest
         browser()->tab_strip_model()->GetActiveWebContents());
     tab_helper->SetIconUpdateCallbackForTesting(
         intent_picker_done.GetCallback());
+    // On Mac, updating the icon requires asynchronous work that is done on the
+    // threadpool (see `WebAppsIntentPickerDelegate::FindAllAppsForUrl()` for
+    // more information). Flushing the thread pool thus helps prevent flakiness
+    // in tests.
+#if BUILDFLAG(IS_MAC)
+    base::ThreadPoolInstance::Get()->FlushForTesting();
+#endif  // BUILDFLAG(IS_MAC)
     action();
     if (HasFailure()) {
       return testing::AssertionFailure();
@@ -200,33 +209,6 @@ IN_PROC_BROWSER_TEST_P(IntentChipButtonBrowserTest,
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), out_of_scope_url));
 
   EXPECT_FALSE(GetIntentChip()->GetVisible());
-}
-
-// TODO(crbug.com/41488032): This test is flaky on Mac.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_IconVisibilityAfterTabSwitching \
-  DISABLED_IconVisibilityAfterTabSwitching
-#else
-#define MAYBE_IconVisibilityAfterTabSwitching IconVisibilityAfterTabSwitching
-#endif
-IN_PROC_BROWSER_TEST_P(IntentChipButtonBrowserTest,
-                       MAYBE_IconVisibilityAfterTabSwitching) {
-  const GURL in_scope_url =
-      https_server().GetURL(GetAppUrlHost(), GetInScopeUrlPath());
-  const GURL out_of_scope_url =
-      https_server().GetURL(GetAppUrlHost(), GetOutOfScopeUrlPath());
-
-  OmniboxChipButton* intent_chip_button = GetIntentChip();
-
-  OpenNewTab(in_scope_url);
-  EXPECT_TRUE(intent_chip_button->GetVisible());
-  OpenNewTab(out_of_scope_url);
-  EXPECT_FALSE(intent_chip_button->GetVisible());
-
-  chrome::SelectPreviousTab(browser());
-  EXPECT_TRUE(intent_chip_button->GetVisible());
-  chrome::SelectNextTab(browser());
-  EXPECT_FALSE(intent_chip_button->GetVisible());
 }
 
 IN_PROC_BROWSER_TEST_P(IntentChipButtonBrowserTest,
