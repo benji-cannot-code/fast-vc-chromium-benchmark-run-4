e956bfd7006b08f0f8e357fc9d31fca7755e4f71
Weizhong Xia <weizhong@google.com>
2024-12-10 22:46:46 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 6a035397d6fc64d82db054c21cb531592748362e
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I2a37ee753f14010ebe87733789e522b42236b4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6086006
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394541}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/headless_shell.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/headless_shell.py
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from .base import require_arg
 from .base import get_timeout_multiplier   # noqa: F401
-from .chrome import ChromeBrowser, debug_args, executor_kwargs  # noqa: F401
+from .chrome import ChromeBrowser, debug_args  # noqa: F401
+from .chrome import executor_kwargs as chrome_executor_kwargs
 from ..executors.base import WdspecExecutor  # noqa: F401
 from ..executors.executorchrome import (  # noqa: F401
     ChromeDriverCrashTestExecutor,
@@ -40,6 +41,17 @@ def browser_kwargs(logger, test_type, run_info_data, config, **kwargs):
             "webdriver_binary": kwargs["webdriver_binary"],
             "webdriver_args": kwargs.get("webdriver_args"),
             "debug_info": kwargs["debug_info"]}
+
+
+def executor_kwargs(logger, test_type, test_environment, run_info_data, subsuite,
+                    **kwargs):
+    executor_kwargs = chrome_executor_kwargs(logger, test_type, test_environment, run_info_data,
+                                             subsuite, **kwargs)
+    chrome_options = executor_kwargs["capabilities"]["goog:chromeOptions"]
+    # Defaultly enable SiteIsolation in headless shell
+    if "--disable-site-isolation-trials" not in chrome_options["args"]:
+        chrome_options["args"].append("--site-per-process")
+    return executor_kwargs
 
 
 def env_extras(**kwargs):
