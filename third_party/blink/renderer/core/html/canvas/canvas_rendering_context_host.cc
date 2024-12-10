3c1f8c8fd574c999b2129fa9e609b85775d91056
Colin Blundell <blundell@chromium.org>
2024-12-10 10:19:56 +0000

    [Blink] Add CanvasRenderingContext::GetSkColorType()
    Next step in breaking down the usage of SkColorInfo in
    CanvasRenderingContext(Host).
    
    Bug: 371227617
    Change-Id: Iace98564a5674bc6f2c595ce0bfce8f12f1c2aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6082472
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394173}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
@@ -346,7 +346,8 @@ SkAlphaType CanvasRenderingContextHost::GetRenderingContextAlphaType() const {
 }
 
 SkColorType CanvasRenderingContextHost::GetRenderingContextSkColorType() const {
-  return GetRenderingContextSkColorInfo().colorType();
+  return RenderingContext() ? RenderingContext()->GetSkColorType()
+                            : kN32_SkColorType;
 }
 
 sk_sp<SkColorSpace>
