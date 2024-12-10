b54536fea77f102aa79e527a172f3010e1b6dfed
Gauthier Ambard <gambard@chromium.org>
2024-12-10 16:03:49 +0000

    [iOS] Fix SharedTabGroup join tests
    Tests were failing because the root view controller wasn't right.
    This is a real bug, so it updates the presentation of the join flow to
    dismiss all presented modals before trying to present the join flow on
    top of the root view controller.
    
    Bug: 381850856
    Change-Id: I5bfef3b1bbc223b9806d155ad732938f0369cac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080540
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394322}

--- ios/chrome/browser/data_sharing/model/data_sharing_ui_delegate_ios.mm
+++ ios/chrome/browser/data_sharing/model/data_sharing_ui_delegate_ios.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/data_sharing/model/data_sharing_ui_delegate_ios.h"
 
+#import "base/functional/callback_helpers.h"
 #import "base/notimplemented.h"
 #import "components/collaboration/public/collaboration_service.h"
 #import "ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate.h"
@@ -13,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/share_kit/model/share_kit_service.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/public/commands/application_commands.h"
+#import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios_share_url_interception_context.h"
 #import "url/gurl.h"
 
@@ -41,18 +44,26 @@ void DataSharingUIDelegateIOS::HandleShareURLIntercepted(
     return;
   }
 
-  UIViewController* baseViewController =
-      browser->GetSceneState().rootViewController;
+  id<ApplicationCommands> applicationHandler =
+      HandlerForProtocol(browser->GetCommandDispatcher(), ApplicationCommands);
 
-  while (baseViewController.presentedViewController) {
-    baseViewController = baseViewController.presentedViewController;
-  }
+  [applicationHandler
+      dismissModalDialogsWithCompletion:
+          base::CallbackToBlock(base::BindOnce(
+              &DataSharingUIDelegateIOS::OnJoinFlowReadyToBePresented,
+              weak_ptr_factory_.GetWeakPtr(), url, browser))];
+}
+
+void DataSharingUIDelegateIOS::OnJoinFlowReadyToBePresented(GURL url,
+                                                            Browser* browser) {
+  UIViewController* base_view_controller =
+      browser->GetSceneState().rootViewController;
 
   std::unique_ptr<IOSCollaborationControllerDelegate> delegate =
       std::make_unique<IOSCollaborationControllerDelegate>(
           std::make_unique<CollaborationFlowConfigurationJoin>(
               share_kit_service_, url, browser->GetCommandDispatcher(),
-              baseViewController));
+              base_view_controller));
   collaboration_service_->StartJoinFlow(std::move(delegate), url);
 }
 
