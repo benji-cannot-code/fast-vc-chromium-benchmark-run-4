da7552b54ac9893cf9d79c0c15a499405d71b5f5
Joe Mason <joenotcharles@google.com>
2024-12-10 19:02:25 +0000

    Use OnBeforeFrameNodeAdded in CPUMeasurementMonitor
    Removes the workarounds in ResourceAttribution that skip the node
    being added.
    
    Bug: 40640034
    Change-Id: Id132f5b7ebb782cfd018f06a98ad652beac0630e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075413
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394432}

--- components/performance_manager/resource_attribution/cpu_measurement_monitor.h
+++ components/performance_manager/resource_attribution/cpu_measurement_monitor.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/graph/page_node.h"
 #include "components/performance_manager/public/graph/process_node.h"
 #include "components/performance_manager/public/graph/worker_node.h"
-#include "components/performance_manager/public/resource_attribution/attribution_helpers.h"
 #include "components/performance_manager/public/resource_attribution/cpu_measurement_delegate.h"
 #include "components/performance_manager/public/resource_attribution/origin_in_browsing_instance_context.h"
 #include "components/performance_manager/public/resource_attribution/query_results.h"
@@ -99,7 +98,12 @@ class CPUMeasurementMonitor
   void RecordMemoryMetrics();
 
   // FrameNode::Observer:
-  void OnFrameNodeAdded(const FrameNode* frame_node) override;
+  void OnBeforeFrameNodeAdded(
+      const FrameNode* frame_node,
+      const FrameNode* pending_parent_frame_node,
+      const PageNode* pending_page_node,
+      const ProcessNode* pending_process_node,
+      const FrameNode* pending_parent_or_outer_document_or_embedder) override;
   void OnBeforeFrameNodeRemoved(const FrameNode* frame_node) override;
   void OnOriginChanged(
       const FrameNode* frame_node,
@@ -115,7 +119,9 @@ class CPUMeasurementMonitor
                          base::TaskPriority previous_value) override;
 
   // WorkerNode::Observer:
-  void OnWorkerNodeAdded(const WorkerNode* worker_node) override;
+  void OnBeforeWorkerNodeAdded(
+      const WorkerNode* worker_node,
+      const ProcessNode* pending_process_node) override;
   void OnBeforeWorkerNodeRemoved(const WorkerNode* worker_node) override;
   void OnBeforeClientFrameAdded(const WorkerNode* worker_node,
                                 const FrameNode* client_frame_node) override;
@@ -183,8 +189,7 @@ class CPUMeasurementMonitor
   // MeasureAndDistributeCPUUsage(). Adds the estimated CPU usage of each frame
   // and worker since the last time the process was measured to
   // `measurement_results_`. `graph_change` is the event that triggered the
-  // measurement or NoGraphChange if it wasn't triggered due to a graph topology
-  // change.
+  // measurement or NoGraphChange if it wasn't triggered due to a graph change.
   void UpdateCPUMeasurements(const ProcessNode* process_node,
                              GraphChange graph_change = NoGraphChange());
 
@@ -197,8 +202,7 @@ class CPUMeasurementMonitor
 
   // Adds the new measurements in `measurement_deltas` to
   // `measurement_results_`. `graph_change` is the event that triggered the
-  // measurement or NoGraphChange if it wasn't triggered due to a graph topology
-  // change.
+  // measurement or NoGraphChange if it wasn't triggered due to a graph change.
   void ApplyMeasurementDeltas(
       const std::map<ResourceContext, CPUTimeResult>& measurement_deltas,
       GraphChange graph_change = NoGraphChange());
--- components/performance_manager/resource_attribution/graph_change.h
+++ components/performance_manager/resource_attribution/graph_change.h
@@ -10,13 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/task/task_traits.h"
-#include "components/performance_manager/public/graph/process_node.h"
-#include "components/performance_manager/resource_attribution/performance_manager_aliases.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "url/origin.h"
 
 namespace performance_manager {
 class Node;
+class ProcessNode;
 }
 
 namespace resource_attribution {
@@ -24,18 +23,6 @@ namespace resource_attribution {
 // Graph changes that can affect resource measurement distribution.
 // These are all passed on the stack so don't need to use raw_ptr.
 struct NoGraphChange {};
-
-struct GraphChangeAddFrame {
-  explicit GraphChangeAddFrame(const FrameNode* node) : frame_node(node) {}
-
-  raw_ptr<const FrameNode> frame_node;
-};
-
-struct GraphChangeAddWorker {
-  explicit GraphChangeAddWorker(const WorkerNode* node) : worker_node(node) {}
-
-  raw_ptr<const WorkerNode> worker_node;
-};
 
 // Not technically a graph change, but modifies the distribution of FrameNode
 // and WorkerNode measurements to OriginInBrowsingInstanceContexts the same way
@@ -65,8 +52,6 @@ struct GraphChangeUpdateProcessPriority {
 };
 
 using GraphChange = absl::variant<NoGraphChange,
-                                  GraphChangeAddFrame,
-                                  GraphChangeAddWorker,
                                   GraphChangeUpdateOrigin,
                                   GraphChangeUpdateProcessPriority>;
 
