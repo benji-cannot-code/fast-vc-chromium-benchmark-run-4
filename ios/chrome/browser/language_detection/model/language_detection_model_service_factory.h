8f29147afd4446e8ecf3a6f3699ee763f3077193
Sylvain Defresne <sdefresne@chromium.org>
2024-12-10 15:38:57 +0000

    [ios] Convert LanguageDetectionModelServiceFactory to PKSFIOS
    Convert LanguageDetectionModelServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217485
    Change-Id: I051f6c5ff3f05d71e8f246795321fe19681716c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080440
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394303}

--- ios/chrome/browser/language_detection/model/language_detection_model_service_factory.h
+++ ios/chrome/browser/language_detection/model/language_detection_model_service_factory.h
@@ -9,25 +9,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ProfileIOS;
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 namespace language_detection {
 class LanguageDetectionModelService;
 }
 
+// Owns all LanguageDetectionModelService instances and associte them to
+// profiles.
 class LanguageDetectionModelServiceFactory
-    : public BrowserStateKeyedServiceFactory {
+    : public ProfileKeyedServiceFactoryIOS {
  public:
   static language_detection::LanguageDetectionModelService* GetForProfile(
       ProfileIOS* profile);
   static LanguageDetectionModelServiceFactory* GetInstance();
-
-  LanguageDetectionModelServiceFactory(
-      const LanguageDetectionModelServiceFactory&) = delete;
-  LanguageDetectionModelServiceFactory& operator=(
-      const LanguageDetectionModelServiceFactory&) = delete;
 
  private:
   friend class base::NoDestructor<LanguageDetectionModelServiceFactory>;
@@ -37,8 +32,6 @@ class LanguageDetectionModelServiceFactory
 
   // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
