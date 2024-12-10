4d610e1825a98fddeaeddf8c110447618e5b984a
Zhengzheng Liu <zhzhliu@chromium.org>
2024-12-10 18:29:57 +0000

    Chromium Task Manager - Search Bar
    Description : This implement the search bar UI for chromium task
    manager, including the search icon, text field, clear button and
    corresponding layout, size settings.
    
    A follow up CL will be wrapped up to hook it up with the search filter
    logic.
    
    Screenshot:
    https://screenshot.googleplex.com/AT4CieX3xTnN25u
    Demo:
    https://drive.google.com/file/d/1i9T_pfKxP9O6cW1DN3H2HIMuQXelHfKx/view?usp=sharing&resourcekey=0-Opt1_uh0YO6IUK5S91sk7w
    
    Bug: b:366255692
    Change-Id: I59a8c1a90eb54adcff55cc13b24da16eed87219e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6051110
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Vincent Chiang <vincentchiang@chromium.org>
    Commit-Queue: Zhengzheng Liu <zhzhliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394408}

--- chrome/browser/ui/views/task_manager_view.h
+++ chrome/browser/ui/views/task_manager_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/task_manager/task_manager_metrics_recorder.h"
 #include "chrome/browser/ui/task_manager/task_manager_table_model.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/task_manager_search_bar_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/models/table_model.h"
 #include "ui/base/mojom/dialog_button.mojom.h"
@@ -142,7 +143,8 @@ class TaskManagerView : public TableViewDelegate,
 
   // Creates all corresponding subcomponents for the header.
   std::unique_ptr<views::View> CreateTabbedPane();
-  std::unique_ptr<views::Textfield> CreateSearchBar(const gfx::Insets& margins);
+  std::unique_ptr<views::View> CreateSearchBar(
+      const ChromeLayoutProvider* provider);
   std::unique_ptr<views::MdTextButton> CreateEndProcessButton(
       const gfx::Insets& margins);
   std::unique_ptr<views::Separator> CreateSeparator(const gfx::Insets& margins);
