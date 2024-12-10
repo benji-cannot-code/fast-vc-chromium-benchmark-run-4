433e12d1722bdb1a3bbf200eab792303b77e9424
Angela Yoeurng <yoangela@chromium.org>
2024-12-10 00:39:43 +0000

    [omnibox] Update @gemini a11y VO string and placeholder text
    Currently, the screen reader string says "Search Gemini...".  This CL
    adds a special exception for @gemini to read "Ask Gemini, ...".
    
    The placeholder text is also updated to "Enter a question".
    
    Bug: 377960896
    Change-Id: I5d9a7ced7d2a27e9c9c8532a554527e7ac400819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6066348
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393999}

--- components/omnibox/browser/omnibox_edit_model.cc
+++ components/omnibox/browser/omnibox_edit_model.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_prepopulate_data.h"
 #include "components/search_engines/template_url_service.h"
+#include "components/search_engines/template_url_starter_pack_data.h"
 #include "components/strings/grit/components_strings.h"
 #include "net/cookies/cookie_util.h"
 #include "third_party/icu/source/common/unicode/ubidi.h"
@@ -2037,8 +2038,11 @@ std::u16string OmniboxEditModel::GetPopupAccessibilityLabelForCurrentSelection(
           controller_->client()->GetTemplateURLService(), false);
       std::u16string replacement_string =
           turl ? turl->short_name() : match.contents;
-      return l10n_util::GetStringFUTF16(IDS_ACC_KEYWORD_MODE,
-                                        replacement_string);
+      int message_id = (turl && turl->starter_pack_id() ==
+                                    TemplateURLStarterPackData::kGemini)
+                           ? IDS_ACC_ASK_KEYWORD_MODE
+                           : IDS_ACC_KEYWORD_MODE;
+      return l10n_util::GetStringFUTF16(message_id, replacement_string);
     }
     case OmniboxPopupSelection::FOCUSED_BUTTON_ACTION:
       // When pedal button is focused, the autocomplete suggestion isn't
