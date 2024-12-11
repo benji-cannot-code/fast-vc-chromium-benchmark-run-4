446b25d546afe705f05ea5538ce8820b38b02645
Katie Dektar <katie@chromium.org>
2024-12-11 15:31:16 -0800

    [A11y] Highlight keyboard focus works in native views
    Refactors native view focus following code to share with
    AccessibilityHighlightController, so that it may also highlight
    the native view with keyboard focus.
    
    Bug:b:259374133
    Test:Manual for FocusRingController, new for AccessibilityHighlightController
    
    Change-Id: Ieb3893d27f3ff5b0db5c9bb0292f5ea93e4c89ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089511
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395139}

--- ash/accessibility/ui/accessibility_highlight_controller.cc
+++ ash/accessibility/ui/accessibility_highlight_controller.cc
@@ -59,6 +59,11 @@ AccessibilityHighlightController::~AccessibilityHighlightController() {
   controller->HideCaretRing();
   controller->HideCursorRing();
 
+  if (native_focus_watcher_) {
+    native_focus_watcher_->RemoveObserver(this);
+    native_focus_watcher_.reset();
+  }
+
   GetSharedInputMethod()->RemoveObserver(this);
   Shell::Get()->cursor_manager()->RemoveObserver(this);
   Shell::Get()->RemovePreTargetHandler(this);
@@ -66,6 +71,15 @@ AccessibilityHighlightController::~AccessibilityHighlightController() {
 
 void AccessibilityHighlightController::HighlightFocus(bool focus) {
   focus_ = focus;
+  if (focus_) {
+    if (!native_focus_watcher_) {
+      native_focus_watcher_ = std::make_unique<NativeFocusWatcher>();
+      native_focus_watcher_->AddObserver(this);
+    }
+    native_focus_watcher_->SetEnabled(true);
+  } else if (native_focus_watcher_) {
+    native_focus_watcher_->SetEnabled(false);
+  }
   UpdateFocusAndCaretHighlights();
 }
 
@@ -135,6 +149,15 @@ void AccessibilityHighlightController::OnCaretBoundsChanged(
 void AccessibilityHighlightController::OnCursorVisibilityChanged(
     bool is_visible) {
   UpdateCursorHighlight();
+}
+
+void AccessibilityHighlightController::OnNativeFocusChanged(
+    const gfx::Rect& bounds_in_screen) {
+  SetFocusHighlightRect(bounds_in_screen);
+}
+
+void AccessibilityHighlightController::OnNativeFocusCleared() {
+  SetFocusHighlightRect(gfx::Rect());
 }
 
 bool AccessibilityHighlightController::IsCursorVisible() {
