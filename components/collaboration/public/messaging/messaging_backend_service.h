8845e8bdddfc5cae45cfc6d4bc285e8d03cbf999
Mickey Burks <mickeyburks@chromium.org>
2024-12-11 12:01:01 -0800

    [TabGroupSync] Create observer service for MessagingBackendService
    This observer depends on the MessagingBackendService and adds itself
    as an observer upon construction. When the backend service
    initializes, the observer queries for previously undelivered messages.
    
    Bug: 382725947
    Change-Id: Ia3298170e3f7fceee69e644740dd83b9c3db78ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073544
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1395021}

--- components/collaboration/public/messaging/messaging_backend_service.h
+++ components/collaboration/public/messaging/messaging_backend_service.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/observer_list_types.h"
+#include "base/scoped_observation_traits.h"
 #include "base/supports_user_data.h"
 #include "components/collaboration/public/messaging/activity_log.h"
 #include "components/collaboration/public/messaging/message.h"
@@ -90,5 +91,26 @@ class MessagingBackendService : public KeyedService,
 };
 
 }  // namespace collaboration::messaging
+
+namespace base {
+template <>
+struct ScopedObservationTraits<
+    collaboration::messaging::MessagingBackendService,
+    collaboration::messaging::MessagingBackendService::
+        PersistentMessageObserver> {
+  static void AddObserver(
+      collaboration::messaging::MessagingBackendService* source,
+      collaboration::messaging::MessagingBackendService::
+          PersistentMessageObserver* observer) {
+    source->AddPersistentMessageObserver(observer);
+  }
+  static void RemoveObserver(
+      collaboration::messaging::MessagingBackendService* source,
+      collaboration::messaging::MessagingBackendService::
+          PersistentMessageObserver* observer) {
+    source->RemovePersistentMessageObserver(observer);
+  }
+};
+}  // namespace base
 
 #endif  // COMPONENTS_COLLABORATION_PUBLIC_MESSAGING_MESSAGING_BACKEND_SERVICE_H_
