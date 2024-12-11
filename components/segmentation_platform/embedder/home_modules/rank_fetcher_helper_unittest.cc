4da05e7cdd11946cf51241f358c4caf8096e58e5
ssid <ssid@chromium.org>
2024-12-11 01:36:35 +0000

    [magic stack] Fill in inputs that were not passed in
    The modules may be disabled. The model does not handle individual module
    flags and ranks all. So, include invalid inputs for modules so the model
    returns results. The client code will filter out any modules not
    enabled. So, it will not affect any ranking. There is a risk of useful
    inputs that were not passed in.
    
    Bug: 382803396
    Change-Id: I0e46d91e3dbff9cbd4f6cb5b7472fa00029eb54f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085398
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394625}

--- components/segmentation_platform/embedder/home_modules/rank_fetcher_helper_unittest.cc
+++ components/segmentation_platform/embedder/home_modules/rank_fetcher_helper_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/segmentation_platform/embedder/home_modules/rank_fetcher_helper.h"
 
+#include "base/memory/scoped_refptr.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
@@ -13,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/embedder/home_modules/ephemeral_home_module_backend.h"
 #include "components/segmentation_platform/public/constants.h"
 #include "components/segmentation_platform/public/features.h"
+#include "components/segmentation_platform/public/input_context.h"
 #include "components/segmentation_platform/public/prediction_options.h"
 #include "components/segmentation_platform/public/result.h"
 #include "components/segmentation_platform/public/testing/mock_segmentation_platform_service.h"
+#include "components/segmentation_platform/public/types/processed_value.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -27,6 +30,11 @@ using testing::SizeIs;
 
 namespace segmentation_platform::home_modules {
 namespace {
+
+constexpr char kInput1[] = "input1";
+constexpr char kInput2[] = "input_freshness_2";
+constexpr char kInput3[] = "input_freshness_3";
+constexpr char kInput4[] = "input4";
 
 class RankFetcherHelperTest : public testing::Test {
  public:
@@ -44,12 +52,14 @@ class RankFetcherHelperTest : public testing::Test {
 
   void TearDown() override {}
 
-  ClassificationResult FetchRank(RankFetcherHelper& rank_fetcher_helper) {
+  ClassificationResult FetchRank(RankFetcherHelper& rank_fetcher_helper,
+                                 scoped_refptr<InputContext> inputs =
+                                     base::MakeRefCounted<InputContext>()) {
     base::test::TestFuture<const ClassificationResult&> result_future;
 
     rank_fetcher_helper.GetHomeModulesRank(
         &segmentation_service_, PredictionOptions(false, false, false),
-        /*input_context=*/nullptr, result_future.GetCallback());
+        /*input_context=*/inputs, result_future.GetCallback());
     return result_future.Get();
   }
 
@@ -88,6 +98,15 @@ class RankFetcherHelperTest : public testing::Test {
             result.result.add_result(it.second);
           }
           std::move(callback).Run(result);
+        }));
+  }
+
+  void ReturnInputKeys(int count) {
+    EXPECT_CALL(segmentation_service_, GetInputKeysForModel(_, _))
+        .Times(count)
+        .WillRepeatedly(Invoke([&](const std::string& segmentation_key,
+                                   InputContextKeysCallback callback) {
+          std::move(callback).Run({kInput1, kInput2, kInput3, kInput4});
         }));
   }
 
@@ -137,6 +156,7 @@ TEST_F(RankFetcherHelperTest, GetHomeModulesRankEnabled) {
   RankFetcherHelper rank_fetcher_helper;
 
   ReturnHomeModulesResult(module_ranks);
+  ReturnInputKeys(1);
 
   ClassificationResult result = FetchRank(rank_fetcher_helper);
 
@@ -150,15 +170,32 @@ TEST_F(RankFetcherHelperTest, MergeResultsAndRunCallback) {
                                                {"EphemeralModule2", 0.5}};
   RankFetcherHelper rank_fetcher_helper;
 
+  ReturnInputKeys(2);
   ReturnHomeModulesResult(module_ranks);
   ReturnEphermeralResult(ephemeral_ranks);
 
-  ClassificationResult result = FetchRank(rank_fetcher_helper);
+  scoped_refptr<InputContext> input_context =
+      base::MakeRefCounted<InputContext>();
+  input_context->metadata_args.emplace(
+      kInput1, processing::ProcessedValue::FromFloat(1));
+  input_context->metadata_args.emplace(
+      kInput3, processing::ProcessedValue::FromFloat(3));
+  ClassificationResult result = FetchRank(rank_fetcher_helper, input_context);
 
   EXPECT_EQ(result.status, PredictionStatus::kSucceeded);
   std::vector<std::string> expected_result = {
       "EphemeralModule1", "module1", "module3", "module2", "EphemeralModule2"};
   EXPECT_EQ(result.ordered_labels, expected_result);
+
+  EXPECT_NEAR(input_context->GetMetadataArgument(kInput1)->float_val, 1, 0.01);
+  EXPECT_NEAR(input_context->GetMetadataArgument(kInput3)->float_val, 3, 0.01);
+
+#if BUILDFLAG(IS_ANDROID)
+  // On Android the freshness inputs get backfilled if missing since modules can
+  // be disabled.
+  EXPECT_NEAR(input_context->GetMetadataArgument(kInput2)->float_val, -1, 0.01);
+  EXPECT_FALSE(input_context->GetMetadataArgument(kInput4));
+#endif
 }
 
 TEST_F(RankFetcherHelperTest, EphemeralCardsEmpty) {
@@ -166,6 +203,7 @@ TEST_F(RankFetcherHelperTest, EphemeralCardsEmpty) {
   std::map<std::string, float> ephemeral_ranks;
   RankFetcherHelper rank_fetcher_helper;
 
+  ReturnInputKeys(2);
   ReturnHomeModulesResult(module_ranks);
   ReturnEphermeralResult(ephemeral_ranks);
 
@@ -179,6 +217,7 @@ TEST_F(RankFetcherHelperTest, MergeResultsAndRunCallback_Failed) {
   std::vector<std::string> module_ranks = {"module1", "module3", "module2"};
   RankFetcherHelper rank_fetcher_helper;
 
+  ReturnInputKeys(2);
   ReturnHomeModulesResult(module_ranks);
   ReturnEphermeralFailure();
 
