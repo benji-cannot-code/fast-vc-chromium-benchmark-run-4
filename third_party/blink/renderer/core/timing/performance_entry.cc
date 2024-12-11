6921f3f999cbde0f0e87ab7b537fb00b330563ad
David Baron <dbaron@chromium.org>
2024-12-11 07:34:54 -0800

    Make PerformanceEntry::presentationTime return std::optional<> to match its IDL signature.
    Bug: 40275482
    Change-Id: I92ec4f80c9d46b27d8c815a12bba33f23ffad72b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075761
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394837}

--- third_party/blink/renderer/core/timing/performance_entry.cc
+++ third_party/blink/renderer/core/timing/performance_entry.cc
@@ -169,7 +169,7 @@ DOMHighResTimeStamp PerformanceEntry::paintTime() const {
   CHECK(RuntimeEnabledFeatures::PaintTimingMixinEnabled());
   return paint_timing_info_ ? paint_timing_info_->paint_time : 0;
 }
-DOMHighResTimeStamp PerformanceEntry::presentationTime() const {
+std::optional<DOMHighResTimeStamp> PerformanceEntry::presentationTime() const {
   CHECK(RuntimeEnabledFeatures::PaintTimingMixinEnabled());
   return paint_timing_info_ ? paint_timing_info_->presentation_time : 0;
 }
