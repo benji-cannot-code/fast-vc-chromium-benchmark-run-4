b0599fb905391888ad4e3ac250707021de95a5f4
Arthur Milchior <arthurmilchior@chromium.org>
2024-12-11 05:00:53 -0800

    [iOS]Remove KeyFetchedCallback
    This alias is not used in ios_internal anymore.
    
    This is part of the chain:
    * crrev.com/c/5943126
    * crrev.com/i/7757070
    + crrev.com/c/5943103
    
    
    Bug: 355042392
    Change-Id: I31d6442dae7996f1bdd52174f1a2f80a6619366e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943103
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394778}

--- ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_mediator_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_mediator_unittest.mm
@@ -65,7 +65,7 @@ class MockTrustedVaultClientBackend : public TrustedVaultClientBackend {
               FetchKeys,
               (id<SystemIdentity> identity,
                trusted_vault::SecurityDomainId security_domain_id,
-               KeyFetchedCallback completion),
+               KeysFetchedCallback completion),
               (override));
   MOCK_METHOD(void,
               MarkLocalKeysAsStale,
