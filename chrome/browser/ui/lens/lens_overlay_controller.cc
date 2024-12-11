360c7b9ff04399e4edd07c90e332e72dbb8db470
Nihar Majmudar <niharm@google.com>
2024-12-11 14:20:00 -0800

    [lensoverlay cleanup] Set search performed inside RecordTimeToFirstInteraction.
    Change-Id: I6d7f11f423b7a6810cee2d5331d685f381c0dd80
    Bug: 380276543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6086015
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395100}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -2417,7 +2417,6 @@ void LensOverlayController::DoLensRequest(
   results_side_panel_coordinator_->RegisterEntryAndShow();
   RecordTimeToFirstInteraction(
       lens::LensOverlayFirstInteractionType::kRegionSelect);
-  search_performed_in_session_ = true;
   state_ = State::kOverlayAndResults;
   MaybeLaunchSurvey();
 }
@@ -2595,7 +2594,6 @@ void LensOverlayController::IssueTextSelectionRequestInner(
   results_side_panel_coordinator_->RegisterEntryAndShow();
   RecordTimeToFirstInteraction(
       lens::LensOverlayFirstInteractionType::kTextSelect);
-  search_performed_in_session_ = true;
   state_ = State::kOverlayAndResults;
   MaybeLaunchSurvey();
 }
@@ -2649,7 +2647,6 @@ void LensOverlayController::IssueSearchBoxRequest(
   RecordContextualSearchboxTimeToInteractionAfterNavigation();
   RecordTimeToFirstInteraction(
       lens::LensOverlayFirstInteractionType::kSearchbox);
-  search_performed_in_session_ = true;
 
   // Do not attempt to contextualize if CSB is disabled or if the user is not in
   // the contextual search flow (aka, issues an image request already).
@@ -2817,6 +2814,7 @@ void LensOverlayController::RecordTimeToFirstInteraction(
   lens::RecordTimeToFirstInteraction(invocation_source_,
                                      time_to_first_interaction,
                                      interaction_type, source_id);
+  search_performed_in_session_ = true;
 }
 
 void LensOverlayController::
