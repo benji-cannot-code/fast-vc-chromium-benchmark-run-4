6b7a4235ff455b8d6aa4580cc7363ce17144ce7a
Michael Wojcicka <mwoj@google.com>
2024-12-11 14:06:38 -0800

    Task Manager Refresh - Add API to remove TabbedPaneTabStrip divider
    - There is a 1 pixel divider which is drawn underneath the
      TabbedPaneTabStrip.
    
    - There are use cases where a custom divider is preferred instead of the
      built-in one, such as when recreating the "All Tabs / Organize Tabs"
      styling from WebUI in Views (which seems to have a depth of 2 or 3px).
    
    - Our use case is drawing a divider which spans the entire length of the
      parent container. Drawing the one provided by this component creates a
      1px inconsistency, since we draw our own below it.
    
    Screenshot Demo (hard to see since the difference is 1px):
    Original: https://screenshot.googleplex.com/46JHrg7xPJAyFgP
    Without Divider: https://screenshot.googleplex.com/9kkHeqq2mpzRNMf
    
    Bug: b:366251764
    Change-Id: I4baeb83c85cd2625f95c704c99f861acb93bc5f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056014
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Yichen Zhou <yichenz@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395088}

--- ui/views/controls/tabbed_pane/tabbed_pane.cc
+++ ui/views/controls/tabbed_pane/tabbed_pane.cc
@@ -667,6 +667,11 @@ void TabbedPaneTabStrip::UpdateAccessibleName() {
   }
 }
 
+void TabbedPaneTabStrip::SetDrawTabDivider(bool draw) {
+  draw_tab_divider_ = draw;
+  SchedulePaint();
+}
+
 void TabbedPaneTabStrip::OnPaintBorder(gfx::Canvas* canvas) {
   // Do not draw border line in kHighlight mode.
   if (GetStyle() == TabbedPane::TabStripStyle::kHighlight) {
@@ -692,8 +697,11 @@ void TabbedPaneTabStrip::OnPaintBorder(gfx::Canvas* canvas) {
     rect = gfx::Rect(max_cross_axis - kUnselectedBorderThickness, 0,
                      kUnselectedBorderThickness, height());
   }
-  canvas->FillRect(rect,
-                   GetColorProvider()->GetColor(ui::kColorTabContentSeparator));
+
+  if (draw_tab_divider_) {
+    canvas->FillRect(
+        rect, GetColorProvider()->GetColor(ui::kColorTabContentSeparator));
+  }
 
   TabbedPaneTab* tab = GetSelectedTab();
   if (!tab) {
@@ -910,6 +918,10 @@ void TabbedPane::UpdateAccessibleName() {
   } else {
     GetViewAccessibility().RemoveName();
   }
+}
+
+void TabbedPane::SetDrawTabDivider(bool draw) {
+  tab_strip_->SetDrawTabDivider(draw);
 }
 
 TabbedPaneTab* TabbedPane::GetSelectedTab() {
