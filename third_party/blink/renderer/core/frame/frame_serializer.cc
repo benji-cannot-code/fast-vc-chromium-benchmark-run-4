a3b98d72a57837324187e20a4fe019153a4ae695
Fredrik Söderquist <fs@opera.com>
2024-12-11 09:42:55 +0000

    spanification: Use span-based SharedBuffer interfaces
    Switch from SegmentedBuffer::Append(ptr, size) to Append(span) and
    SharedBuffer::Create(ptr, size) to Create(span).
    
    Bug: 351564777
    Change-Id: I959d9f5ba4e2b56d26a48e98fd9343a67411b717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6084736
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394728}

--- third_party/blink/renderer/core/frame/frame_serializer.cc
+++ third_party/blink/renderer/core/frame/frame_serializer.cc
@@ -1174,9 +1174,8 @@ function main(metadata) {
             text_string, WTF::kCSSEncodedEntitiesForUnencodables);
       }
 
-      resource_serializer_->AddToResources(
-          String("text/css"), SharedBuffer::Create(text.c_str(), text.length()),
-          url);
+      resource_serializer_->AddToResources(String("text/css"),
+                                           SharedBuffer::Create(text), url);
     }
 
     // Sub resources need to be serialized even if the CSS definition doesn't
@@ -1374,9 +1373,8 @@ void FrameSerializer::SerializeFrame(
 
     std::string frame_html =
         document.Encoding().Encode(text, WTF::kEntitiesForUnencodables);
-    resource_serializer->AddMainResource(
-        document.SuggestedMIMEType(),
-        SharedBuffer::Create(frame_html.c_str(), frame_html.length()), url);
+    resource_serializer->AddMainResource(document.SuggestedMIMEType(),
+                                         SharedBuffer::Create(frame_html), url);
     resource_serializer->Finish(std::move(callback));
   }
 }
--- third_party/blink/renderer/platform/network/network_utils.cc
+++ third_party/blink/renderer/platform/network/network_utils.cc
@@ -83,7 +83,7 @@ std::tuple<int, ResourceResponse, scoped_refptr<SharedBuffer>> ParseDataURL(
   if (result != net::OK)
     return std::make_tuple(result, ResourceResponse(), nullptr);
 
-  auto buffer = SharedBuffer::Create(data_string.data(), data_string.size());
+  auto buffer = SharedBuffer::Create(data_string);
   // The below code is the same as in
   // `CreateResourceForTransparentPlaceholderImage()`.
   ResourceResponse response;
