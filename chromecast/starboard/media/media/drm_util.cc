32cd0e0e38c1b0d683f776087de13ca0b48db3fe
Antonio Rivera <antoniori@google.com>
2024-12-11 11:59:22 -0800

    Fix a compile error for starboard code.
    ../../chromecast/starboard/media/media/drm_util.cc:23:15: error: constexpr variable 'kMaxIvLength' must be initialized by a constant expression
    
    Bug: None
    
    Test: Built core_runtime_starboard
    Change-Id: Ic14d77147822bd24cab23b08c1249e1b91be9ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6081685
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395019}

--- chromecast/starboard/media/media/drm_util.cc
+++ chromecast/starboard/media/media/drm_util.cc
@@ -13,18 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 namespace media {
 
-// Returns the length of an array.
-template <typename T, size_t n>
-static constexpr size_t ArrayLength(const T (&)[n]) {
-  return n;
-}
-
 // Rather than hard-coding values here, we simply read the length of the
 // relevant arrays in StarboardDrmSampleInfo.
-constexpr int kMaxIvLength = ArrayLength(
-    static_cast<StarboardDrmSampleInfo*>(nullptr)->initialization_vector);
-constexpr int kMaxIdLength =
-    ArrayLength(static_cast<StarboardDrmSampleInfo*>(nullptr)->identifier);
+constexpr int kMaxIvLength =
+    std::size(StarboardDrmSampleInfo{}.initialization_vector);
+constexpr int kMaxIdLength = std::size(StarboardDrmSampleInfo{}.identifier);
 
 DrmInfoWrapper::DrmInfoWrapper() = default;
 
