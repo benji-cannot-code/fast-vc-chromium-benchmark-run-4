4c64347b6d958e15ce1a3ad60cff8aa6fbc33e78
David Roger <droger@chromium.org>
2024-12-11 05:08:20 -0800

    [Uno-D] Fix DiceExplicitSigninRollbackBrowserTest.RollbackSigninPending
    There was a race condition, because the account is signed out only after
    the tokens are loaded. Added an explicit waiting step to the test to fix
    the flakiness.
    
    Fixed: 382186954
    Change-Id: Ic84fbb87298ca8a71d53bc245a9f5bc988e53df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6082191
    Auto-Submit: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1394782}

--- chrome/browser/signin/dice_browsertest.cc
+++ chrome/browser/signin/dice_browsertest.cc
@@ -1384,6 +1384,7 @@ IN_PROC_BROWSER_TEST_F(DiceExplicitSigninRollbackBrowserTest, PRE_Rollback) {
 IN_PROC_BROWSER_TEST_F(DiceExplicitSigninRollbackBrowserTest, Rollback) {
   Profile* profile = browser()->profile();
   // The user is now signed in implicitly.
+  signin::WaitForRefreshTokensLoaded(GetIdentityManager());
   ASSERT_EQ(signin::GetPrimaryAccountConsentLevel(GetIdentityManager()),
             signin::ConsentLevel::kSignin);
   ASSERT_TRUE(gaia::AreEmailsSame(
@@ -1437,6 +1438,8 @@ IN_PROC_BROWSER_TEST_F(DiceExplicitSigninRollbackBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(DiceExplicitSigninRollbackBrowserTest,
                        RollbackSigninPending) {
+  // The account is signed out after tokens are loaded.
+  signin::WaitForRefreshTokensLoaded(GetIdentityManager());
   // After rollback, a signin pending state should transition to signed out.
   EXPECT_EQ(signin::GetPrimaryAccountConsentLevel(GetIdentityManager()),
             std::nullopt);
@@ -1469,6 +1472,7 @@ IN_PROC_BROWSER_TEST_F(DiceExplicitSigninRollbackBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(DiceExplicitSigninRollbackBrowserTest,
                        RollbackWebSigninOnly) {
+  signin::WaitForRefreshTokensLoaded(GetIdentityManager());
   signin::TriggerListAccount(GetIdentityManager(), test_url_loader_factory());
   // After rollback, Chrome would be implicitly signed in.
   EXPECT_EQ(signin::GetPrimaryAccountConsentLevel(GetIdentityManager()),
