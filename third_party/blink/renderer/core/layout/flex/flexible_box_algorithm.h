7630a81bdacbb9f2b03494463129c58351532987
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-12-11 10:04:32 -0800

    [flex] Remove final dependencies of FlexibleBoxAlgorithm::flex_lines_
    There was only two remaining places where this was called.
    1) FlexLayoutAlgorithm::CheckFlexLines - this wasn't really checking
       anything useful anymore - just remove.
    2) FlexibleBoxAlgorithm::FlexItemAtIndex - instead of looking up the
       line, just store the original item-index on the NGFlexItem, and use
       that to directly lookup the FlexItem.
    
    There should be no behaviour change.
    
    Change-Id: I9123789e3cbfd7c80caee609d17d24c11a754cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085806
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394923}

--- third_party/blink/renderer/core/layout/flex/flexible_box_algorithm.h
+++ third_party/blink/renderer/core/layout/flex/flexible_box_algorithm.h
@@ -76,6 +76,7 @@ class FlexItem {
   //   |min_max_cross_sizes| does include cross_axis_border_padding.
   FlexItem(const FlexibleBoxAlgorithm*,
            const ComputedStyle& style,
+           wtf_size_t item_index,
            unsigned main_axis_auto_margin_count,
            LayoutUnit flex_base_content_size,
            MinMaxSizes min_max_main_sizes,
@@ -123,7 +124,8 @@ class FlexItem {
 
   LayoutUnit MarginBoxAscent(bool is_last_baseline, bool is_wrap_reverse) const;
 
-  LayoutUnit CrossAxisOffset(const NGFlexLine&, LayoutUnit cross_axis_size);
+  LayoutUnit CrossAxisOffset(const NGFlexLine&,
+                             LayoutUnit cross_axis_size) const;
 
   static LayoutUnit AlignmentOffset(LayoutUnit available_free_space,
                                     ItemPosition position,
@@ -134,6 +136,8 @@ class FlexItem {
 
   const FlexibleBoxAlgorithm* algorithm_;
   Member<const ComputedStyle> style_;
+
+  const wtf_size_t item_index_;
   const float flex_grow_;
   const float flex_shrink_;
   const unsigned main_axis_auto_margin_count_;
@@ -342,7 +346,7 @@ class CORE_EXPORT FlexibleBoxAlgorithm {
       const StyleContentAlignmentData&,
       unsigned number_of_items);
 
-  FlexItem* FlexItemAtIndex(wtf_size_t line_index, wtf_size_t item_index) const;
+  const FlexItem& FlexItemAtIndex(wtf_size_t item_index) const;
 
   static LayoutUnit GapBetweenItems(const ComputedStyle& style,
                                     LogicalSize percent_resolution_sizes);
