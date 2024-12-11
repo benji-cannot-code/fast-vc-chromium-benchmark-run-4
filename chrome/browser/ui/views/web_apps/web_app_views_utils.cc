4f5ff75ff3966d427ab7ad02e06be288393d9160
Finnur Thorarinsson <finnur@chromium.org>
2024-12-11 12:43:18 -0800

    [PWA]: Install/Uninstall -- wrap names that are too long.
    The dialogs should be resilient to app names that are
    extra long. The decision was to allow app names to wrap
    to the next line, but to elide after that.
    
    This also includes minor UI polish, as directed by the UX
    team.
    
    Bug: 367401496, 383523099
    Change-Id: Ia86ede77bc6beca2991e1fa57e8231216e284bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089150
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395048}

--- chrome/browser/ui/views/web_apps/web_app_views_utils.cc
+++ chrome/browser/ui/views/web_apps/web_app_views_utils.cc
@@ -11,11 +11,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
+constexpr int kMaxLinesForNameLabel = 2;
+
 std::unique_ptr<views::Label> CreateNameLabel(const std::u16string& name) {
   auto name_label = std::make_unique<views::Label>(
       name, views::style::CONTEXT_DIALOG_BODY_TEXT,
       views::style::TextStyle::STYLE_PRIMARY);
   name_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  name_label->SetMultiLine(kMaxLinesForNameLabel > 1);
+  name_label->SetMaxLines(kMaxLinesForNameLabel);
   name_label->SetElideBehavior(gfx::ELIDE_TAIL);
   return name_label;
 }
