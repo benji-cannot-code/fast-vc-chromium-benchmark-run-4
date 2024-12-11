e54484d6c2aa9e3c638187a72a8592b0c30df08c
Fredrik Söderquist <fs@opera.com>
2024-12-11 14:36:44 -0800

    spanification: Remove unsafe SharedBuffer/SegmentedBuffer interfaces
    Removes SharedBuffer::Create(const {unsigned,} char*, <size>) and
    SegmentedBuffer::Append(const {unsigned,}... char*, <size>).
    
    Bug: 351564777
    Change-Id: I6fed5f0577bc372f7979b88d74e9856b6a59aa27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088038
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1395115}

--- third_party/blink/renderer/platform/mhtml/mhtml_fuzzer.cc
+++ third_party/blink/renderer/platform/mhtml/mhtml_fuzzer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/containers/span.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
 #include "third_party/blink/renderer/platform/mhtml/archive_resource.h"
 #include "third_party/blink/renderer/platform/mhtml/mhtml_parser.h"
@@ -19,7 +20,10 @@ namespace blink {
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static BlinkFuzzerTestSupport test_support = BlinkFuzzerTestSupport();
   blink::test::TaskEnvironment task_environment;
-  MHTMLParser mhtml_parser(SharedBuffer::Create(data, size));
+
+  // SAFETY: Just wrapping the input from libFuzzer in a span.
+  auto data_span = UNSAFE_BUFFERS(base::span(data, size));
+  MHTMLParser mhtml_parser(SharedBuffer::Create(data_span));
   HeapVector<Member<ArchiveResource>> mhtml_archives =
       mhtml_parser.ParseArchive();
   mhtml_archives.clear();
