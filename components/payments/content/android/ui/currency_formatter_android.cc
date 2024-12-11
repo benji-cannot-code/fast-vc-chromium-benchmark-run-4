44d4d0b937712195ded539cf1a4b0bad29abd67b
Rouslan Solomakhin <rouslan@chromium.org>
2024-12-11 14:11:28 -0800

    [Payments] Create the ui:jni_headers and ui:java build targets
    This patch creates two targets:
    1. //components/payments/content/android/ui:java
    2. //components/payments/content/android/ui:jni_headers
    
    The CurrencyFormatter.java file is moved into these targets.
    Since the CurrencyFormatter.java is used only for UI purposes,
    this patch decouples a part of the PaymentRequest UI from the rest
    of the core PaymentRequest implementation on Android.
    
    The patch updates several targets that use CurrencyFormatter.java
    to point to the new build target.
    
    Design sketch: go/payment-request-android-code-structure
    
    Fixed: b:382282941
    Change-Id: I4ff718bcffafd6d1ae6eeaed2a6d54f85e5b71c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072547
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395092}

--- components/payments/content/android/ui/currency_formatter_android.cc
+++ components/payments/content/android/ui/currency_formatter_android.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/payments/core/currency_formatter.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "components/payments/content/android/jni_headers/CurrencyFormatter_jni.h"
+#include "components/payments/content/android/ui/jni_headers/CurrencyFormatter_jni.h"
 
 namespace payments {
 namespace {
