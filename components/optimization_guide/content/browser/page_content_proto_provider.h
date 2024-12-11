014a27b781e8f5627b387948216b1d149c757253
Khushal Sagar <khushalsagar@chromium.org>
2024-12-11 08:06:57 -0800

    components: Move AIPageContent capture to components.
    Refactor the code to generate the AIPageContent for a given WebContents
    to components/optimization_guide. Also move the browsertests.
    
    Bug: 377496304
    Change-Id: I005e9377163996664b28ac5d113be5eae8d2b76b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6071949
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1394859}

---
+++ components/optimization_guide/content/browser/page_content_proto_provider.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTENT_PROTO_PROVIDER_H_
+#define COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTENT_PROTO_PROVIDER_H_
+
+#include "base/functional/callback.h"
+#include "components/optimization_guide/proto/features/model_prototyping.pb.h"
+
+namespace content {
+class WebContents;
+}
+
+namespace optimization_guide {
+
+// Provides AIPageContent representation for the primary page displayed in a
+// WebContents.
+using OnAIPageContentDone = base::OnceCallback<void(
+    std::optional<optimization_guide::proto::AnnotatedPageContent>)>;
+void GetAIPageContent(content::WebContents* web_contents,
+                      OnAIPageContentDone done_callback);
+
+}  // namespace optimization_guide
+
+#endif  // COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTENT_PROTO_PROVIDER_H_
