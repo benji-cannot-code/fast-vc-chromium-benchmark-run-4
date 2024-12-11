b7be7058b30c4cf637701056122391ebe6d2822b
LK Kupoluyi <lkupo@chromium.org>
2024-12-11 11:36:20 -0800

    [MouseKeys] Move mouse key settings to it's own subpage
    * Migrated mouse key setting code and tests to new mouse key subpage
    * Added mouse key search concept
    
    screen-recording: https://screencast.googleplex.com/cast/NTE3MzAwMzEyNDk5ODE0NHxiYTcwNWQ5Ni03NQ
    
    Mouse keys row hover: https://screencast.googleplex.com/cast/NTMxODEzNzA0OTI1MTg0MHw5ZTlkNjc2Zi0xOQ
    
    Bug: 380053252
    Change-Id: Ifceb0c1ac6a77aaf0a97b138c70a8e0e1308bf1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6051102
    Commit-Queue: Jesulayomi Kupoluyi <lkupo@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395012}

--- chrome/browser/resources/ash/settings/os_settings_routes.ts
+++ chrome/browser/resources/ash/settings/os_settings_routes.ts
@@ -199,6 +199,7 @@ export interface OsSettingsRoutes extends MinimumRoutes {
   MANAGE_ACCESSIBILITY: Route;
   MANAGE_FACEGAZE_SETTINGS: Route;
   MANAGE_ISOLATED_WEB_APPS: Route;
+  MANAGE_MOUSE_KEYS_SETTINGS: Route;
   MANAGE_SWITCH_ACCESS_SETTINGS: Route;
   MANAGE_TTS_SETTINGS: Route;
   MULTIDEVICE: Route;
@@ -500,6 +501,9 @@ export function createRoutes(): OsSettingsRoutes {
   r.MANAGE_FACEGAZE_SETTINGS = createSubpage(
       r.A11Y_CURSOR_AND_TOUCHPAD, routesMojom.FACE_GAZE_SETTINGS_SUBPAGE_PATH,
       Subpage.kFaceGazeSettings);
+  r.MANAGE_MOUSE_KEYS_SETTINGS = createSubpage(
+      r.A11Y_CURSOR_AND_TOUCHPAD, routesMojom.MOUSE_KEYS_SETTINGS_SUBPAGE_PATH,
+      Subpage.kMouseKeysSettings);
 
   // Privacy and Security section.
   r.OS_PRIVACY = createSection(
--- chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_section.cc
@@ -62,6 +62,7 @@ using ::chromeos::settings::mojom::kDisplayAndMagnificationSubpagePath;
 using ::chromeos::settings::mojom::kFaceGazeSettingsSubpagePath;
 using ::chromeos::settings::mojom::kKeyboardAndTextInputSubpagePath;
 using ::chromeos::settings::mojom::kManageAccessibilitySubpagePath;
+using ::chromeos::settings::mojom::kMouseKeysSettingsSubpagePath;
 using ::chromeos::settings::mojom::kSelectToSpeakSubpagePath;
 using ::chromeos::settings::mojom::kSwitchAccessOptionsSubpagePath;
 using ::chromeos::settings::mojom::kTextToSpeechPagePath;
@@ -528,6 +529,18 @@ base::span<const SearchConcept> GetA11yFaceGazeSearchConcepts() {
         IDS_OS_SETTINGS_TAG_A11Y_FACEGAZE_ALT2,
         IDS_OS_SETTINGS_TAG_A11Y_FACEGAZE_ALT3,
         IDS_OS_SETTINGS_TAG_A11Y_FACEGAZE_ALT4}},
+  });
+  return tags;
+}
+
+base::span<const SearchConcept> GetA11yMouseKeysSearchConcepts() {
+  static constexpr auto tags = std::to_array<SearchConcept>({
+      {IDS_OS_SETTINGS_TAG_A11Y_MOUSE_KEYS,
+       mojom::kMouseKeysSettingsSubpagePath,
+       mojom::SearchResultIcon::kA11y,
+       mojom::SearchResultDefaultRank::kMedium,
+       mojom::SearchResultType::kSetting,
+       {.setting = mojom::Setting::kMouseKeysEnabled}},
   });
   return tags;
 }
@@ -1930,6 +1943,13 @@ void AccessibilitySection::RegisterHierarchy(
       mojom::Subpage::kFaceGazeSettings, mojom::SearchResultIcon::kA11y,
       mojom::SearchResultDefaultRank::kMedium,
       mojom::kFaceGazeSettingsSubpagePath);
+
+  // Mouse keys settings.
+  generator->RegisterTopLevelSubpage(
+      IDS_OS_SETTINGS_ACCESSIBILITY_MOUSE_KEYS_LABEL,
+      mojom::Subpage::kMouseKeysSettings, mojom::SearchResultIcon::kA11y,
+      mojom::SearchResultDefaultRank::kMedium,
+      mojom::kMouseKeysSettingsSubpagePath);
 }
 
 void AccessibilitySection::OnVoicesChanged() {
@@ -2029,6 +2049,10 @@ void AccessibilitySection::UpdateSearchTags() {
 
   if (IsAccessibilitySlowKeysEnabled()) {
     updater.AddSearchTags(GetA11ySlowKeysSearchConcepts());
+  }
+
+  if (IsAccessibilityMouseKeysEnabled()) {
+    updater.AddSearchTags(GetA11yMouseKeysSearchConcepts());
   }
 
   if (IsAccessibilityDisableTouchpadEnabled()) {
