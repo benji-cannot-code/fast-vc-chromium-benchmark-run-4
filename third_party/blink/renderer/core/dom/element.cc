15e5d1fe346675d29adcb3037ca05a120420ca33
Daniil Sakhapov <sakhapov@chromium.org>
2024-12-11 07:34:51 +0000

    [carousel] Make ::scroll-button()s succeeding box siblings of scroller
    They need to be succeeding for anchor positioning to work correctly.
    The spec will be updated later.
    
    Bug: 370067113
    Change-Id: I9c812fcbd82100f7d059119179780f6b7b1bc43d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6084582
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394699}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -3338,10 +3338,6 @@ void Element::AttachLayoutTree(AttachContext& context) {
   }
 
   AttachPseudoElement(kPseudoIdScrollMarkerGroupBefore, context);
-  AttachPseudoElement(kPseudoIdScrollUpButton, context);
-  AttachPseudoElement(kPseudoIdScrollLeftButton, context);
-  AttachPseudoElement(kPseudoIdScrollRightButton, context);
-  AttachPseudoElement(kPseudoIdScrollDownButton, context);
 
   AttachContext children_context(context);
   LayoutObject* layout_object = nullptr;
@@ -3371,6 +3367,10 @@ void Element::AttachLayoutTree(AttachContext& context) {
   }
   children_context.use_previous_in_flow = true;
 
+  AttachPseudoElement(kPseudoIdScrollUpButton, context);
+  AttachPseudoElement(kPseudoIdScrollLeftButton, context);
+  AttachPseudoElement(kPseudoIdScrollRightButton, context);
+  AttachPseudoElement(kPseudoIdScrollDownButton, context);
   AttachPseudoElement(kPseudoIdScrollMarkerGroupAfter, context);
 
   if (skipped_container_descendants &&
