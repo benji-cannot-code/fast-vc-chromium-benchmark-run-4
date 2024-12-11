590117a79f03780d41f55b33a764e4d9392d71f1
Xi Han <hanxi@google.com>
2024-12-11 15:49:40 -0800

    [AppIntegration] Add a feature flag for showing the notice card.
    In this CL, we add a new flag for showing the notice card on the magic
    stack. The existing feature params "force_show_card" and
    "show_third_party_card" are with this feature flag now.
    Screenshot of chrome://flags page:
    https://drive.google.com/file/d/1TDMsSNTgUvw60pqBBtatH9bEQFAmDN_q/view?usp=drive_link
    
    Bug: 380086456
    Change-Id: I415ebedef351c7513483975e812335d7397d1a09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089873
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395149}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -75,6 +75,9 @@ extern const char kAllowLegacyMV2ExtensionsDescription[];
 extern const char kAndroidAppIntegrationName[];
 extern const char kAndroidAppIntegrationDescription[];
 
+extern const char kAndroidAppIntegrationModuleName[];
+extern const char kAndroidAppIntegrationModuleDescription[];
+
 extern const char kAndroidAppIntegrationV2Name[];
 extern const char kAndroidAppIntegrationV2Description[];
 
