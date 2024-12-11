3f3805edbc1df0e33508cefc0394e61656381418
rbpotter <rbpotter@chromium.org>
2024-12-11 14:01:47 -0800

    Internal debugging pages interstitial: Add link to control
    Add a message to the debugging pages disabled interstitial explaining
    to developers how to enable such pages. The message includes a link to
    chrome://chrome-urls/#internal-debug-pages, so that developers can
    easily follow it to the button that enables these pages.
    
    Bug: 379889249
    Change-Id: I3884efd7d5b38a9d390c18e7cffa96637381085f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6086795
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1395084}

--- chrome/browser/ui/webui/internal_debug_pages_disabled/internal_debug_pages_disabled_ui.cc
+++ chrome/browser/ui/webui/internal_debug_pages_disabled/internal_debug_pages_disabled_ui.cc
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/internal_debug_pages_disabled/internal_debug_pages_disabled_ui.h"
 
+#include "base/strings/strcat.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/internal_debug_pages_disabled_resources.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace {
 
@@ -19,6 +23,11 @@ void CreateAndAddHTMLSource(Profile* profile, const std::string& host_name) {
       content::WebUIDataSource::CreateAndAdd(profile, host_name);
   source->AddLocalizedString("pageHeading",
                              IDS_INTERNAL_DEBUG_PAGES_DISABLED_HEADING);
+  std::u16string body =
+      l10n_util::GetStringFUTF16(IDS_INTERNAL_DEBUG_PAGES_DISABLED_BODY,
+                                 base::StrCat({chrome::kChromeUIChromeURLsURL16,
+                                               u"#internal-debug-pages"}));
+  source->AddString("pageBody", body);
 
   source->AddResourcePath("", IDR_INTERNAL_DEBUG_PAGES_DISABLED_APP_HTML);
 }
