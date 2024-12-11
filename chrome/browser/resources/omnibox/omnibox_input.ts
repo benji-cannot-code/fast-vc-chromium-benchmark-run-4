424f15a9725279eea7cf4b5375ab417f6ce889a1
dpapad <dpapad@chromium.org>
2024-12-11 12:31:12 -0800

    Remove @ts-ignore:next-line when using CSS import attribute.
    While TypeScript has added support for import attributes per [1],
    it is still not entirely clear how to resolve CSS modules [2]. Using a
    workaround from [3] where a proposal is discussed instead of resorting
    to using `@ts-ignore:next-line` comments to suppress errors.
    
    [1] https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-3.html#import-attributes
    [2] https://github.com/microsoft/TypeScript/issues/46689
    [2] https://github.com/microsoft/TypeScript/issues/46135#issuecomment-2310885452t
    
    Bug: None
    Change-Id: Ia2315d35dbca7761441b1694e4446b12d0532fc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6086700
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395040}

--- chrome/browser/resources/omnibox/omnibox_input.ts
+++ chrome/browser/resources/omnibox/omnibox_input.ts
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {PageClassification} from './omnibox.mojom-webui.js';
 import {OmniboxElement} from './omnibox_element.js';
-/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
-// @ts-ignore:next-line
 import sheet from './omnibox_input.css' with {type : 'css'};
 
 export interface QueryInputs {
