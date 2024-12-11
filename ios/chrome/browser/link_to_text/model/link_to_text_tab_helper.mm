1dcbc4eb2d6f1e74dae9476a82f7315277eb28f5
Olivier ROBIN <olivierrobin@google.com>
2024-12-11 07:04:26 -0800

    Remove DUMP_WILL_BE_NOTREACHED in LinkToTextTabHelper
    This the iOS18.2 fix for edit menu, buildWithBuilder is now
    called when web content (game, video,...) is in fullscreen
    and in that case, the textInputView cannot be found.
    
    As the dump was also triggered without the fix, and as it
    is handled gracefully, remove the dump.
    
    Fixed: 382547336
    Change-Id: I53b8145332016b783ef68a01c507983e151b400e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088119
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394826}

--- ios/chrome/browser/link_to_text/model/link_to_text_tab_helper.mm
+++ ios/chrome/browser/link_to_text/model/link_to_text_tab_helper.mm
@@ -98,7 +98,6 @@ bool LinkToTextTabHelper::ShouldOffer() {
 
   if (!textInputView) {
     LogShouldOfferResult(ShouldOfferResult::kTextInputNotFound);
-    DUMP_WILL_BE_NOTREACHED();
     return false;
   }
 
