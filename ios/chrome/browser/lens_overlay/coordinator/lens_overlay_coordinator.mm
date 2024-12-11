d5958cd76356ccf06fc3f6e4daae7db9f6852747
Radu Nitescu <radunitescu@google.com>
2024-12-11 10:33:36 +0000

    [iOS][LO] Don't retain utility factories in coordinator
    Bug: None
    Change-Id: Id52277b111039a37745bc403fc291d52174fe44e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075007
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394744}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -135,12 +135,6 @@ const int kExpectedExitAnimationCount = 2;
   /// Consent dialog presenter.
   LensOverlayConsentPresenter* _lensOverlayConsentPresenter;
 
-  /// Factory for the  actions in the overflow menu.
-  LensOverlayOverflowMenuFactory* _overflowMenuFactory;
-
-  /// Configuration factory.
-  LensOverlayConfigurationFactory* _lensConfigurationFactory;
-
   /// Network issue alert presenter.
   LensOverlayNetworkIssueAlertPresenter* _networkIssueAlertPresenter;
 
@@ -200,12 +194,18 @@ const int kExpectedExitAnimationCount = 2;
     return;
   }
 
-  LensConfiguration* config = [_lensConfigurationFactory
+  LensOverlayConfigurationFactory* lensConfigurationFactory =
+      [[LensOverlayConfigurationFactory alloc] init];
+  LensConfiguration* config = [lensConfigurationFactory
       configurationForEntrypoint:entrypoint
                          profile:self.browser->GetProfile()];
+
+  LensOverlayOverflowMenuFactory* overflowMenuFactory =
+      [[LensOverlayOverflowMenuFactory alloc] initWithBrowser:self.browser];
+
   NSArray<UIAction*>* additionalMenuItems = @[
-    [_overflowMenuFactory openUserActivityAction],
-    [_overflowMenuFactory learnMoreAction],
+    [overflowMenuFactory openUserActivityAction],
+    [overflowMenuFactory learnMoreAction],
   ];
 
   _selectionViewController = ios::provider::NewChromeLensOverlay(
@@ -286,16 +286,10 @@ const int kExpectedExitAnimationCount = 2;
            object:nil];
 
   _associatedTabHelper = [self activeTabHelper];
-  CHECK(_associatedTabHelper, kLensOverlayNotFatalUntil);
 
   _metricsRecorder = [[LensOverlayMetricsRecorder alloc]
       initWithEntrypoint:entrypoint
       associatedWebState:_associatedTabHelper->GetWebState()];
-
-  _overflowMenuFactory =
-      [[LensOverlayOverflowMenuFactory alloc] initWithBrowser:self.browser];
-
-  _lensConfigurationFactory = [[LensOverlayConfigurationFactory alloc] init];
 
   // The instance that creates the Lens UI designates itself as the command
   // handler for the associated tab.
