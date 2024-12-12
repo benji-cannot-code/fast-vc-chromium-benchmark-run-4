f3807bdfc7fcce94e89b3e33a5013dc39ff7560d
Amanda Lin Dietz <aldietz@google.com>
2024-12-12 14:13:55 -0800

    [FaceGaze] Add confirmation dialogs for feature/cursor/actions buttons
    * Show standard confirmation dialog when feature is toggled off.
    * Show standard confirmation dialog when cursor control is toggled off.
    * Show standard confirmation dialog when actions are toggled off.
    * To enable user to use FaceGaze to interact with the dialog,
    add a set of sentinel prefs to indicate when users have toggled
    the button off.
    * To enable JS to force feature off without showing a dialog,
    add a sentinel pref for showing a confirmation dialog that is
    set to false when FaceGaze is forcing the feature off.
    * Update all JS to only interact with the set of sentinel prefs.
    * Add tests.
    * Leave cursor/actions strings as translateable=false for now.
    
    Design doc:
    https://docs.google.com/document/d/1ULHXI_QK6ET1Inyrm_9DrIpiecVFc3Z7kyyv6cs0oJ0/edit?usp=sharing
    
    Screenshots:
    https://screenshot.googleplex.com/3f6ir9GbJC7zd5q
    https://screenshot.googleplex.com/ra4nbokD8K4Cpub
    https://screenshot.googleplex.com/6fd2gzZvx8Gmg3W
    
    Change-Id: Iccb932657e9613cef670f2638e5dfa0794d0e3db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042086
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395618}

--- ash/accessibility/accessibility_controller.h
+++ ash/accessibility/accessibility_controller.h
@@ -56,6 +56,7 @@ namespace ash {
 class AccessibilityConfirmationDialog;
 class AccessibilityControllerClient;
 class AccessibilityEventRewriter;
+class AccessibilityFeatureDisableDialog;
 class AccessibilityHighlightController;
 class AccessibilityObserver;
 enum class AccessibilityPanelState;
@@ -645,6 +646,12 @@ class ASH_EXPORT AccessibilityController
       std::optional<int> timeout_seconds = std::nullopt);
   gfx::Rect GetConfirmationDialogBoundsInScreen();
 
+  // Shows a dialog to disable a feature with the given text, and calls the
+  // relevant callback when the dialog is accepted or cancelled.
+  void ShowFeatureDisableDialog(int window_title_text_id,
+                                base::OnceClosure on_accept_callback,
+                                base::OnceClosure on_cancel_callback);
+
   void PreviewFlashNotification() const;
 
   // SessionObserver:
@@ -679,6 +686,9 @@ class ASH_EXPORT AccessibilityController
   }
   AccessibilityConfirmationDialog* GetConfirmationDialogForTest() {
     return confirmation_dialog_.get();
+  }
+  AccessibilityFeatureDisableDialog* GetFeatureDisableDialogForTest() {
+    return disable_dialog_.get();
   }
 
   bool enable_chromevox_volume_slide_gesture() {
@@ -717,6 +727,9 @@ class ASH_EXPORT AccessibilityController
       base::RepeatingCallback<void(AccessibilityToastType)> callback);
 
   void AddShowConfirmationDialogCallbackForTesting(
+      base::RepeatingCallback<void()> callback);
+
+  void AddFeatureDisableDialogCallbackForTesting(
       base::RepeatingCallback<void()> callback);
 
   bool VerifyFeaturesDataForTesting();
@@ -813,6 +826,12 @@ class ASH_EXPORT AccessibilityController
   void OnDisableTouchpadDialogAccepted();
   void OnDisableTouchpadDialogDismissed();
   void ExternalDeviceConnected();
+
+  void OnFaceGazeSentinelChanged(const std::string& sentinel_pref,
+                                 const std::string& behavior_pref);
+  void OnFaceGazeDisableDialogClosed(const std::string& sentinel_pref,
+                                     const std::string& behavior_pref,
+                                     bool dialog_accepted);
 
   void RecordSelectToSpeakSpeechDuration(SelectToSpeakState old_state,
                                          SelectToSpeakState new_state);
@@ -912,6 +931,11 @@ class ASH_EXPORT AccessibilityController
 
   base::RepeatingCallback<void()>
       show_confirmation_dialog_callback_for_testing_;
+
+  // The current AccessibilityFeatureDisableDialog, if one exists.
+  base::WeakPtr<AccessibilityFeatureDisableDialog> disable_dialog_;
+
+  base::RepeatingCallback<void()> show_disable_dialog_callback_for_testing_;
 
   base::Time select_to_speak_speech_start_time_;
 
