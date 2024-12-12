85a426a16c0174e3f66604718e9e39544c4caf1f
Sophie Wen <sophiewen@google.com>
2024-12-11 17:32:34 -0800

    sunfish: Add search results panel container
    This CL adds the kShellWindowId_CaptureModeSearchResultsPanel
    container to stack the panel above the capture mode UI and below the
    drag and tooltip container while capture mode is in session.
    
    Design: https://docs.google.com/document/d/1NEpVEis2SzkCrySSNfdsMz3jDGmlezdPmy2lt-CNCM0/edit?usp=sharing
    
    Test: Added in follow-up
    Bug: b:377352005
    Change-Id: Ibbf61e12964c5db76dabf027f5f8dc8374446797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088678
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395198}

--- ash/root_window_controller.cc
+++ ash/root_window_controller.cc
@@ -244,6 +244,7 @@ void ReparentAllWindows(aura::Window* src, aura::Window* dst) {
       kShellWindowId_SystemModalContainer,
       kShellWindowId_LockSystemModalContainer,
       kShellWindowId_MenuContainer,
+      kShellWindowId_CaptureModeSearchResultsPanel,
       kShellWindowId_LiveCaptionContainer,
       kShellWindowId_OverlayContainer,
   };
@@ -1319,6 +1320,12 @@ void RootWindowController::CreateContainers() {
                       lock_screen_related_containers);
   ::wm::SetChildWindowVisibilityChangesAnimated(menu_container);
   menu_container->SetProperty(::wm::kUsesScreenCoordinatesKey, true);
+
+  aura::Window* panel_container = CreateContainer(
+      kShellWindowId_CaptureModeSearchResultsPanel,
+      "SearchResultsPanelContainer", lock_screen_related_containers);
+  ::wm::SetChildWindowVisibilityChangesAnimated(panel_container);
+  panel_container->SetProperty(::wm::kUsesScreenCoordinatesKey, true);
 
   aura::Window* accessibility_bubble_container = CreateContainer(
       kShellWindowId_AccessibilityBubbleContainer,
