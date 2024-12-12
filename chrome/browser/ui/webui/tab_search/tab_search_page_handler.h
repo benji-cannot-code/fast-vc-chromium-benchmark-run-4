d92f78787f75b46599e6c2f0f6eeb67763bd291b
Shibalik Mohapatra <shibalik@chromium.org>
2024-12-12 10:46:27 -0800

    [TabStripDedupe] Add listeners for duplicate tabs.
    This CL adds listeners for group changes, pinned tab changes and detaching of the duplicate tabs. It also implements the exclude feature for url.
    
    
    Change-Id: Iea61ee549bd8351a45c3cd395433456f5defdc77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089640
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1395508}

--- chrome/browser/ui/webui/tab_search/tab_search_page_handler.h
+++ chrome/browser/ui/webui/tab_search/tab_search_page_handler.h
@@ -176,6 +176,11 @@ class TabSearchPageHandler
     return stale_tabs_;
   }
 
+  std::map<GURL, std::vector<tabs::TabInterface*>>
+  duplicate_tabs_for_testing() {
+    return duplicate_tabs_;
+  }
+
   void SetTabDeclutterControllerForTesting(
       tabs::TabDeclutterController* tab_declutter_controller);
 
@@ -190,6 +195,8 @@ class TabSearchPageHandler
   // previously added to the ProfileData will not be added more than once by
   // leveraging DedupKey comparisons.
   typedef std::tuple<GURL, std::optional<base::Token>> DedupKey;
+
+  enum class UnusedTabType { kInactive, kDuplicate };
 
   // Encapsulates tab details to facilitate performing an action on a tab.
   struct TabDetails {
@@ -268,15 +275,23 @@ class TabSearchPageHandler
   GetMojoDuplicateTabs();
 
   void UnregisterTabCallbacks();
-  void RegisterTabDeclutterCallbacks(tabs::TabInterface* tab);
+  void RegisterInactiveTabDeclutterCallbacks(tabs::TabInterface* tab);
+  void RegisterDuplicateTabDeclutterCallbacks(tabs::TabInterface* tab);
+
   void OnStaleTabDidEnterForeground(tabs::TabInterface* tab);
-  void OnStaleTabWillDetach(tabs::TabInterface* tab,
-                            tabs::TabInterface::DetachReason reason);
-  void OnStaleTabPinnedStateChanged(tabs::TabInterface* tab,
-                                    bool new_pinned_state);
-  void OnStaleTabGroupChanged(tabs::TabInterface* tab,
-                              std::optional<tab_groups::TabGroupId> new_group);
+
+  void OnUnusedTabWillDetach(tabs::TabInterface* tab,
+                             tabs::TabInterface::DetachReason reason,
+                             UnusedTabType type);
+  void OnUnusedTabPinnedStateChanged(tabs::TabInterface* tab,
+                                     bool new_pinned_state,
+                                     UnusedTabType type);
+  void OnUnusedTabGroupChanged(tabs::TabInterface* tab,
+                               std::optional<tab_groups::TabGroupId> new_group,
+                               UnusedTabType type);
+
   void RemoveStaleTab(tabs::TabInterface* tab);
+  void RemoveDuplicateTab(tabs::TabInterface* tab);
 
   // Called when the browser window context for this WebUI has changed.
   void BrowserWindowInterfaceChanged();
@@ -328,8 +343,12 @@ class TabSearchPageHandler
 
   std::vector<tabs::TabInterface*> stale_tabs_;
   std::map<GURL, std::vector<tabs::TabInterface*>> duplicate_tabs_;
+
   std::map<tabs::TabInterface*, std::vector<base::CallbackListSubscription>>
-      tab_declutter_subscriptions_map_;
+      inactive_tab_subscriptions_map_;
+
+  std::map<tabs::TabInterface*, std::vector<base::CallbackListSubscription>>
+      duplicate_tab_subscriptions_map_;
 
   base::ScopedObservation<TabOrganizationService, TabOrganizationObserver>
       tab_organization_observation_{this};
