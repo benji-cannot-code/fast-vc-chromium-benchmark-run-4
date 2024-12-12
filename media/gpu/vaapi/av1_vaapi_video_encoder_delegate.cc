60d508d501ebb8f25c72349f6f74f17c4dff3918
Takuto Ikuta <tikuta@chromium.org>
2024-12-12 02:26:16 -0800

    media: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: Ic43945122c725e1309ba1ddd462910c69825aafe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090651
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395323}

--- media/gpu/vaapi/av1_vaapi_video_encoder_delegate.cc
+++ media/gpu/vaapi/av1_vaapi_video_encoder_delegate.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vaapi/av1_vaapi_video_encoder_delegate.h"
 
 #include <bit>
+#include <bitset>
 #include <utility>
 
 #include "base/bits.h"
