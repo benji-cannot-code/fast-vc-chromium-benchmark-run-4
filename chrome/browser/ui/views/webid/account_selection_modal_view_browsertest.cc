3755055b38e032c04aec4cc7fe496791ceb2ce23
Erik Chen <erikchen@chromium.org>
2024-12-11 17:56:08 -0800

    Simplify FedCmAccountSelectionView (part 18/N)
    This CL fixes fedCM's incorrect use of focus.
    
    Only controls (such as buttons) can be focused. Prior to this CL, fedCM
    was trying to focus labels (such as the title of the dialog) in order to
    get screenreaders to announce the presence of the dialog.
    
    This is wrong. The correct approach is to set the title of the window to
    announce the dialog. Furthermore, screenreaders use a virtual cursor to
    navigate text elements. This is different from, and should not be
    confused with focus.
    
    See http://screencast/cast/NTk5MjQyNDc2ODc5ODcyMHw3ZTY4MTg0NC0yMA for a
    demo of this CL with voiceover on macOS.
    
    Change-Id: Ia60fd10fbe46a42d321253a3c6a0f6add55d8143
    Bug: 377803489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074069
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395207}

--- chrome/browser/ui/views/webid/account_selection_modal_view_browsertest.cc
+++ chrome/browser/ui/views/webid/account_selection_modal_view_browsertest.cc
@@ -263,9 +263,6 @@ class AccountSelectionModalViewTest : public DialogBrowserTest,
     views::Label* title_view = static_cast<views::Label*>(header_children[1]);
     ASSERT_TRUE(title_view);
     EXPECT_EQ(title_view->GetText(), kTitleSignIn);
-    if (should_focus_title_) {
-      EXPECT_EQ(dialog()->GetInitiallyFocusedView(), title_view);
-    }
 
     if (!is_loading_dialog) {
       // Check body text.
@@ -274,10 +271,6 @@ class AccountSelectionModalViewTest : public DialogBrowserTest,
       EXPECT_EQ(body_view->GetText(), kBodySignIn);
       EXPECT_EQ(body_view->GetVisible(), expect_visible_body_label_);
     }
-
-    // After the first header check, the consecutive header checks do not
-    // necessarily have to focus on the title.
-    should_focus_title_ = false;
   }
 
   void CheckButtonRow(views::View* button_row,
@@ -678,7 +671,6 @@ class AccountSelectionModalViewTest : public DialogBrowserTest,
 
  private:
   bool expect_visible_body_label_{true};
-  bool should_focus_title_{true};
   ui::ImageModel idp_brand_icon_;
   scoped_refptr<network::SharedURLLoaderFactory>
       test_shared_url_loader_factory_;
--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
@@ -98,6 +98,7 @@ void FedCmAccountSelectionView::ShowDialogWidget() {
   }
 
   input_protector_->VisibilityChanged(true);
+  GetDialogWidget()->widget_delegate()->SetCanActivate(true);
   GetDialogWidget()->Show();
   if (dialog_type_ == DialogType::MODAL) {
     scoped_ignore_input_events_ =
@@ -124,7 +125,6 @@ void FedCmAccountSelectionView::ShowDialogWidget() {
   }
 #endif  // IS_MAC
 
-  GetDialogWidget()->widget_delegate()->SetCanActivate(true);
   if (accounts_widget_shown_callback_) {
     std::move(accounts_widget_shown_callback_).Run();
   }
@@ -834,19 +834,12 @@ void FedCmAccountSelectionView::UpdateDialogPosition() {
         static_cast<AccountSelectionBubbleView*>(account_selection_view_);
     GetDialogWidget()->SetBounds(bubble->GetBubbleBounds());
   } else {
-    auto* modal =
-        static_cast<AccountSelectionModalView*>(account_selection_view_);
-
     constrained_window::UpdateWebContentsModalDialogPosition(
         GetDialogWidget(),
         web_modal::WebContentsModalDialogManager::FromWebContents(
             account_selection_view_->web_contents())
             ->delegate()
             ->GetWebContentsModalDialogHost());
-
-    if (accessibility_state_utils::IsScreenReaderEnabled()) {
-      modal->GetInitiallyFocusedView()->RequestFocus();
-    }
   }
 }
 
