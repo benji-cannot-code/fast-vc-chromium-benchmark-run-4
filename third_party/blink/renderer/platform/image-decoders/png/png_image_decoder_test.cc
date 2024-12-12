31cbed1f8be82d3ff22231f6639e088067b1fbc7
Lukasz Anforowicz <lukasza@chromium.org>
2024-12-12 06:17:39 -0800

    [rust png] Don't call `SetEmbeddedColorProfile` if `IgnoresColorSpace`.
    https://crrev.com/c/6013129 modified `SkiaImageDecoderBase::OnSetData`
    to unconditionally propagate the color profile from `SkCodec` into
    `blink::ImageDecoder`.  This may hit a `DCHECK(!IgnoresColorSpace())` in
    `ImageDecoder::SetEmbeddedColorProfile` - this CL fixes this by also
    doing this check in `SkiaImageDecoderBase`.  This problem was originally
    discovered via https://crrev.com/c/6085801/2 and
    `DataDecoderBrowserTest.DecodeImage` from `content_browsertests`:
    https://luci-milo.appspot.com/ui/inv/build-8728891287741805441/test-results?q=DataDecoderBrowserTest.DecodeImage&sortby=&groupby=
    
    Bug: chromium:376758571
    Bug: chromium:383580201
    Change-Id: I3cf22f152b3e71ec63715abd0849ffea07910346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089367
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395381}

--- third_party/blink/renderer/platform/image-decoders/png/png_image_decoder_test.cc
+++ third_party/blink/renderer/platform/image-decoders/png/png_image_decoder_test.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/buildflags.h"
 #include "skia/rusty_png_feature.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/platform/graphics/color_behavior.h"
 #include "third_party/blink/renderer/platform/image-decoders/image_decoder_test_helpers.h"
 #include "third_party/blink/renderer/platform/image-decoders/png/png_decoder_factory.h"
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
@@ -51,10 +52,16 @@ namespace blink {
 namespace {
 
 std::unique_ptr<ImageDecoder> CreatePNGDecoder(
-    ImageDecoder::AlphaOption alpha_option) {
+    ImageDecoder::AlphaOption alpha_option,
+    ColorBehavior color_behavior) {
   return CreatePngImageDecoder(alpha_option, ImageDecoder::kDefaultBitDepth,
-                               ColorBehavior::kTransformToSRGB,
+                               color_behavior,
                                ImageDecoder::kNoDecodedImageByteLimit);
+}
+
+std::unique_ptr<ImageDecoder> CreatePNGDecoder(
+    ImageDecoder::AlphaOption alpha_option) {
+  return CreatePNGDecoder(alpha_option, ColorBehavior::kTransformToSRGB);
 }
 
 std::unique_ptr<ImageDecoder> CreatePNGDecoder() {
@@ -1734,6 +1741,20 @@ TEST_P(PNGTests, cicp) {
   const skcms_ICCProfile* png_profile = transform->SrcProfile();
   ASSERT_TRUE(png_profile);
   EXPECT_TRUE(skcms_TransferFunction_isPQish(&png_profile->trc[0].parametric));
+}
+
+TEST_P(PNGTests, IgnoringColorProfile) {
+  const char* png_file = "/images/resources/cicp_pq.png";
+  scoped_refptr<SharedBuffer> data = ReadFileToSharedBuffer(png_file);
+  ASSERT_TRUE(data);
+
+  auto decoder = CreatePNGDecoder(ImageDecoder::kAlphaNotPremultiplied,
+                                  ColorBehavior::kIgnore);
+  decoder->SetData(data.get(), true);
+  auto* frame = decoder->DecodeFrameBufferAtIndex(0);
+  ASSERT_TRUE(frame);
+  ASSERT_FALSE(decoder->Failed());
+  ASSERT_FALSE(decoder->HasEmbeddedColorProfile());
 }
 
 TEST_P(PNGTests, HDRMetadata) {
