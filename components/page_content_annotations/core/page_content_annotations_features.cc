ac617eb2dceae50461a8bd099b12f1e5da1ae3c8
Nathan Memmott <memmott@chromium.org>
2024-12-12 10:06:36 -0800

    Code Health: Clean up stale PageVisibilityBatchAnnotations
    Fixed: 356236726
    Change-Id: I45c7e3161f8a0440439f7b1eb2c7dd602a110fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089854
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395487}

--- components/page_content_annotations/core/page_content_annotations_features.cc
+++ components/page_content_annotations/core/page_content_annotations_features.cc
@@ -116,10 +116,6 @@ BASE_FEATURE(kPageVisibilityPageContentAnnotations,
              "PageVisibilityPageContentAnnotations",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kPageVisibilityBatchAnnotations,
-             "PageVisibilityBatchAnnotations",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kPageContentAnnotationsValidation,
              "PageContentAnnotationsValidation",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -205,10 +201,6 @@ double NoiseProbabilityForRAPPORMetrics() {
   return std::max(0.0, std::min(1.0, GetFieldTrialParamByFeatureAsDouble(
                                          kPageContentAnnotations,
                                          "noise_prob_for_rappor_metrics", .5)));
-}
-
-bool PageVisibilityBatchAnnotationsEnabled() {
-  return base::FeatureList::IsEnabled(kPageVisibilityBatchAnnotations);
 }
 
 size_t AnnotateVisitBatchSize() {
