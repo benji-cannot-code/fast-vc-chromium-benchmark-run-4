8d81d67d8c75d3b31fd79336d9e3c6bfbb38d77c
Emily Andrews <emiled@microsoft.com>
2024-12-12 12:33:48 -0800

    Update SpareRenderProcessHostManager::GetSpareIds to use ChildProcessId
    Using int32_t for the child process id can be fraught with peril.
    Developers can mistake this for the PID. Bugs can be created by passing
    in an invalid value by mistake. Using a strongly typed ChildProcessId
    instead will allow the compiler to catch errors.
    
    
    This change updates SpareRenderProcessHostManager::GetSpareIds and it's
    users to use ChildProcessId.
    
    Bug: 379869738
    Change-Id: Ie91e798b5c7d5a0eb064695f352097054e9a50e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042587
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Emily Andrews <emiled@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1395572}

--- chrome/browser/ui/webui/top_chrome/webui_contents_warmup_level_recorder.h
+++ chrome/browser/ui/webui/top_chrome/webui_contents_warmup_level_recorder.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/top_chrome/webui_contents_warmup_level.h"
+#include "content/public/browser/child_process_id.h"
 
 namespace content {
 
@@ -31,7 +32,7 @@ struct WebUIContentsWarmupLevelPreCondition {
   // The IDs of the spare processes before the contents is created. This can be
   // empty when the memory pressure is high or a spare process creation is
   // delayed.
-  std::vector<int> spare_process_ids;
+  std::vector<content::ChildProcessId> spare_process_ids;
 
   // Use WeakPtr because the preloaded contents could be destroyed, e.g. after
   // a request of contents under a different browser context.
--- content/public/browser/spare_render_process_host_manager.h
+++ content/public/browser/spare_render_process_host_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/observer_list_types.h"
 #include "content/common/content_export.h"
+#include "content/public/browser/child_process_id.h"
 
 namespace content {
 
@@ -49,7 +50,7 @@ class CONTENT_EXPORT SpareRenderProcessHostManager {
   // spare before the navigation. Using `GetSpares()` and comparing pointers
   // wouldn't work because there is always a chance that a RPH is destroyed and
   // a new one is created reusing the same address).
-  virtual std::vector<int> GetSpareIds() = 0;
+  virtual std::vector<ChildProcessId> GetSpareIds() = 0;
 
   // Possibly start an unbound, spare RenderProcessHost. A subsequent creation
   // of a RenderProcessHost with a matching browser_context may use this
