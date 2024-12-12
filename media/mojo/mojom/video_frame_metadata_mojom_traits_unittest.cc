2a5b44a1e9900638fc58ee866862521706673fb5
Alex Gough <ajgo@chromium.org>
2024-12-11 16:20:12 -0800

    Rename media/mojo/mojom testonly mojom
    Testonly mojom do not need security review if they
    are named .test-mojom.
    
    Bug: 383380971
    Change-Id: I835495356c13f8d58d0eb0a4459079b3571098fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089801
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395167}

--- media/mojo/mojom/video_frame_metadata_mojom_traits_unittest.cc
+++ media/mojo/mojom/video_frame_metadata_mojom_traits_unittest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
-#include "media/mojo/mojom/traits_test_service.mojom.h"
+#include "media/mojo/mojom/traits_test_service.test-mojom.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
--- media/mojo/mojom/video_frame_mojom_traits_unittest.cc
+++ media/mojo/mojom/video_frame_mojom_traits_unittest.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/color_plane_layout.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_frame_layout.h"
-#include "media/mojo/mojom/traits_test_service.mojom.h"
+#include "media/mojo/mojom/traits_test_service.test-mojom.h"
 #include "media/video/fake_gpu_memory_buffer.h"
 #include "mojo/public/cpp/bindings/message.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
