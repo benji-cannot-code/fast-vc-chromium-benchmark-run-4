6bf8b963b0b72ad195bc82126efd331acad84409
Michelle Chen <michellegc@google.com>
2024-12-11 16:13:45 -0800

    scanner: Close capture mode UI when Scanner action button is pressed.
    From the latest UX, we would like to close the Scanner session UI when a
    Scanner action button is pressed and then complete the action in the
    background (with a progress notification separate to capture mode).
    
    Followups will clean up other parts of capture mode that are no longer
    needed, e.g. action button disabled state, loading spinner.
    
    Bug: b:378023303
    Change-Id: I63b424f7eb4266d3bccaf6e7e91e4ddcdb312473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074411
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395162}

--- ash/scanner/scanner_action_view_model.h
+++ ash/scanner/scanner_action_view_model.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/scanner/scanner_action_handler.h"
 #include "ash/scanner/scanner_unpopulated_action.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
 
 namespace gfx {
@@ -41,7 +42,10 @@ class ASH_EXPORT ScannerActionViewModel {
 
   // Executes this action, running the provided callback with a success value
   // when the execution finishes.
-  void ExecuteAction(ScannerCommandCallback action_finished_callback) const;
+  // TODO: crbug.com/378023303 - Remove `action_finished_callback` since it's no
+  // longer needed for the new action progress UI.
+  void ExecuteAction(ScannerCommandCallback action_finished_callback =
+                         base::DoNothing()) const;
 
  private:
   ScannerUnpopulatedAction unpopulated_action_;
