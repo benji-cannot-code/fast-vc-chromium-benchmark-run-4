7f3bfccf69d14f46494879ec8bb8780ca49c220d
Kenichi Ishibashi <bashi@chromium.org>
2024-12-11 16:34:19 -0800

    HttpStreamPool: Handle slow-but-success IPEndPoint
    Before this CL, AttemptManager didn't use IPEndPoints that were
    considered slow but succeeded. Since the slow timeout is relatively
    short (currently the delay is 250ms), such endpoints would be fairly
    common. We should use these endpoints when there is no slow-fast
    endpoints or unattempted endpoints.
    
    Introduce IPEndPointState to manage a state of an endpoint and
    merge `failed_ip_endpoints_` and `slow_ip_endpoints_` into a single
    map.
    
    Bug: 346835898
    Change-Id: I1524ce548ed8c1c736f32e884fee10bd384e1829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073170
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395176}

--- net/http/http_stream_pool_attempt_manager.h
+++ net/http/http_stream_pool_attempt_manager.h
@@ -47,10 +47,33 @@ class HttpNetworkSession;
 class NetLog;
 class HttpStreamKey;
 
-// Maintains in-flight Jobs. Peforms DNS resolution.
+// Drives connection attempts for a single destination.
+//
+// Maintains multiple in-flight Jobs for a single destination keyed by
+// HttpStreamKey. Peforms DNS resolution and manages connection attempts.
+// Delegates QUIC connection attempts to QuicTask. Upon successful HttpStream
+// creations or fatal error occurrence, notify jobs of success or failure.
+//
+// Created by an HttpStreamPool::Group when new connection attempts are needed
+// and destroyed when all jobs, in-flight attempts, and the QuicTask are
+// completed.
 class HttpStreamPool::AttemptManager
     : public HostResolver::ServiceEndpointRequest::Delegate {
  public:
+  // The state of an IPEndPoint. There is no success state. The absence of a
+  // state for an endpoint means that we haven't yet attempted to connect to the
+  // endpoint, or that a connection to the endpoint was successfully completed
+  // and was not slow. Public for testing.
+  enum class IPEndPointState {
+    // The endpoint has failed.
+    kFailed,
+    // The endpoint is considered slow and haven't timed out yet.
+    kSlowAttempting,
+    // The endpoint was slow to connect, but the connection establishment
+    // completed successfully.
+    kSlowSucceeded,
+  };
+
   // Time to delay connection attempts more than one when the destination is
   // known to support HTTP/2, to avoid unnecessary socket connection
   // establishments. See https://crbug.com/718576
@@ -161,12 +184,17 @@ class HttpStreamPool::AttemptManager
   // Retrieves information on the current state of `this` as a base::Value.
   base::Value::Dict GetInfoAsValue();
 
+  MultiplexedSessionCreationInitiator
+  CalculateMultiplexedSessionCreationInitiator();
+
   std::optional<int> GetQuicTaskResultForTesting() { return quic_task_result_; }
 
   void SetIsFailingForTest(bool is_failing) { is_failing_ = is_failing; }
 
-  MultiplexedSessionCreationInitiator
-  CalculateMultiplexedSessionCreationInitiator();
+  const std::map<IPEndPoint, IPEndPointState>& ip_endpoint_states_for_testing()
+      const {
+    return ip_endpoint_states_;
+  }
 
  private:
   // Represents failure of connection attempts. Used to notify job of completion
@@ -202,6 +230,8 @@ class HttpStreamPool::AttemptManager
 
   static std::string_view TcpBasedAttemptStateToString(
       TcpBasedAttemptState state);
+
+  static std::string_view IPEndPointStateToString(IPEndPointState state);
 
   const HttpStreamKey& stream_key() const;
 
@@ -254,9 +284,11 @@ class HttpStreamPool::AttemptManager
   void MaybeAttemptQuic();
 
   // Attempts connections if there are pending jobs and IPEndPoints that
-  // haven't failed. If `max_attempts` is given, attempts connections up to
+  // haven't failed. If `exclude_ip_endpoint` is given, exclude the IPEndPoint
+  // from attempts. If `max_attempts` is given, attempts connections up to
   // `max_attempts`.
   void MaybeAttemptConnection(
+      std::optional<IPEndPoint> exclude_ip_endpoint = std::nullopt,
       std::optional<size_t> max_attempts = std::nullopt);
 
   // Returns true if there are pending jobs and the pool and the group
@@ -286,9 +318,13 @@ class HttpStreamPool::AttemptManager
   // Helper method to calculate pending jobs/preconnects.
   size_t PendingCountInternal(size_t pending_count) const;
 
-  std::optional<IPEndPoint> GetIPEndPointToAttempt();
+  // Returns an IPEndPoint to attempt a connection. If `exclude_ip_endpoint` is
+  // given, exclude the endpoint.
+  std::optional<IPEndPoint> GetIPEndPointToAttempt(
+      std::optional<IPEndPoint> exclude_ip_endpoint = std::nullopt);
   std::optional<IPEndPoint> FindPreferredIPEndpoint(
-      const std::vector<IPEndPoint>& ip_endpoints);
+      const std::vector<IPEndPoint>& ip_endpoints,
+      std::optional<IPEndPoint> exclude_ip_endpoint = std::nullopt);
 
   // Calculate the failure kind to notify jobs of failure. Used to call one of
   // the job's methods.
@@ -457,12 +493,9 @@ class HttpStreamPool::AttemptManager
 
   // When true, try to use IPv6 for the next attempt first.
   bool prefer_ipv6_ = true;
-  // Updated when a stream attempt failed. Used to calculate next IPEndPoint to
-  // attempt.
-  std::set<IPEndPoint> failed_ip_endpoints_;
-  // Updated when a stream attempt is considered slow. Used to calculate next
-  // IPEndPoint to attempt.
-  std::set<IPEndPoint> slow_ip_endpoints_;
+  // Updated when a stream attempt is completed or considered slow. Used to
+  // calculate next IPEndPoint to attempt.
+  std::map<IPEndPoint, IPEndPointState> ip_endpoint_states_;
 
   // The current state of TCP/TLS connection attempts.
   TcpBasedAttemptState tcp_based_attempt_state_ =
