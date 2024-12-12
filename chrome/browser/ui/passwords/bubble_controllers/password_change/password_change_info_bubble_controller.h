a7aff72d3f433ffd0dc346da31e7c43a4db1a99f
Anna Tsvirchkova <atsvirchkova@google.com>
2024-12-12 03:26:19 -0800

    Introduce change password view & controller
    These view & controller don't contain any logic yet and are not wired to
    be displayed.
    
    Bug: 381053884
    Change-Id: I2d9d5fb0e6a0dd4a1bd95946f246f36275aebe62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070189
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395342}

---
+++ chrome/browser/ui/passwords/bubble_controllers/password_change/password_change_info_bubble_controller.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PASSWORDS_BUBBLE_CONTROLLERS_PASSWORD_CHANGE_PASSWORD_CHANGE_INFO_BUBBLE_CONTROLLER_H_
+#define CHROME_BROWSER_UI_PASSWORDS_BUBBLE_CONTROLLERS_PASSWORD_CHANGE_PASSWORD_CHANGE_INFO_BUBBLE_CONTROLLER_H_
+
+#include <string>
+
+#include "chrome/browser/ui/passwords/bubble_controllers/password_bubble_controller_base.h"
+
+// Controller for the views informing the user about the password change flow
+// state.
+class PasswordChangeInfoBubbleController : public PasswordBubbleControllerBase {
+ public:
+  explicit PasswordChangeInfoBubbleController(
+      base::WeakPtr<PasswordsModelDelegate> delegate);
+
+  ~PasswordChangeInfoBubbleController() override;
+
+  // PasswordBubbleControllerBase methods:
+  std::u16string GetTitle() const override;
+  void ReportInteractions() override;
+};
+
+#endif  // CHROME_BROWSER_UI_PASSWORDS_BUBBLE_CONTROLLERS_PASSWORD_CHANGE_PASSWORD_CHANGE_INFO_BUBBLE_CONTROLLER_H_
