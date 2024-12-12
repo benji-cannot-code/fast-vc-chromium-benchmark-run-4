d0404a30dbb6a5535b4bda6f307ae4cac3df53f6
Jenny Zhang <jennyz@chromium.org>
2024-12-11 16:10:46 -0800

    Fix ChromeSiteIsolationPolicyTest test for desktop android.
    Since sxtensions are enabled on desktop android build,
    the extension related logic needs to be enabled in the test
    in order to meet the test expectations.
    
    This cl fixes the failed test on desktop android buid, and only
    affects tests.
    
    Bug: 383592423
    Change-Id: I4bf281258af6f86f0da19b8909fb2490b6cc33f2
    Cq-Include-Trybots: luci.chromium.try:android-desktop-14-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090054
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395160}

--- chrome/browser/site_isolation/chrome_site_isolation_policy_unittest.cc
+++ chrome/browser/site_isolation/chrome_site_isolation_policy_unittest.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "extensions/common/extension_urls.h"
 #endif
 
@@ -122,7 +122,7 @@ TEST_F(ChromeSiteIsolationPolicyTest, IsolatedOriginsContainChromeOrigins) {
     expected_embedder_origins.push_back(GaiaUrls::GetInstance()->gaia_origin());
   }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   expected_embedder_origins.push_back(
       url::Origin::Create(extension_urls::GetWebstoreLaunchURL()));
   expected_embedder_origins.push_back(
