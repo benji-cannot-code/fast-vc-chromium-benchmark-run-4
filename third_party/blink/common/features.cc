e9e09aeb5e9454b61e4cc59868aee1b4651fec33
Adam Psarouthakis <psarouthakis@google.com>
2024-12-12 14:06:14 -0800

    [Code Health Rotation] Clean Up StylusPointerAdjustment
    As part of the code health rotation stale feature flags should be
    cleaned up. Feature was launched and the launch bug can be found here
    https://crbug.com/40258680.
    
    Bug: 362247471
    Change-Id: I2ee0830e22c7d10b0cf86b27d36d2f590663c0cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055980
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395614}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2582,10 +2582,6 @@ BASE_FEATURE(kSubSampleWindowProxyUsageMetrics,
              "SubSampleWindowProxyUsageMetrics",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kStylusPointerAdjustment,
-             "StylusPointerAdjustment",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kStylusRichGestures,
              "StylusRichGestures",
              base::FEATURE_ENABLED_BY_DEFAULT);
