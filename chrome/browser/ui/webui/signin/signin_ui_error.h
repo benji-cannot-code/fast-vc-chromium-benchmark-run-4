903cc89c3c054fc9398c24e74feba711a1912b0c
Takuto Ikuta <tikuta@chromium.org>
2024-12-11 19:20:47 -0800

    chrome: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: I96da4ab77846403e087c4b1f1a22cb8fd40c6107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090631
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395234}

--- chrome/browser/ui/webui/signin/signin_ui_error.h
+++ chrome/browser/ui/webui/signin/signin_ui_error.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_SIGNIN_SIGNIN_UI_ERROR_H_
 
 #include <string>
+#include <utility>
 
 #include "base/files/file_path.h"
 #include "build/build_config.h"
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_PREF_NAMES_H_
 #define CHROME_COMMON_PREF_NAMES_H_
 
+#include <stddef.h>
+
 #include <iterator>
 
 #include "build/branding_buildflags.h"
