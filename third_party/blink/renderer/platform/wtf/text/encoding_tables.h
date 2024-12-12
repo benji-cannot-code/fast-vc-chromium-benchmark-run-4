7cb13a129897794ad6f9173950856b2cce3a00f4
Takuto Ikuta <tikuta@chromium.org>
2024-12-11 19:24:26 -0800

    third_party/blink: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: I2e88be6768d3cd58caa408b1b4981decb42245a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089335
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395235}

--- third_party/blink/renderer/platform/wtf/text/encoding_tables.h
+++ third_party/blink/renderer/platform/wtf/text/encoding_tables.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <iterator>
 #include <optional>
+#include <type_traits>
 #include <utility>
 
 #include "base/dcheck_is_on.h"
