031f3d9e14577d9735acc5a9c56a4a3f41ad912f
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-12-12 15:35:24 -0800

    [flex] Split FlexItem into its own file.
    As above. Makes FlexItem a struct with public members, and renames a
    few of the members.
    
    There should be no behaviour change.
    
    Change-Id: I08a01de6380d8dfbb91e60f54abfd96090349d55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091933
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395650}

--- third_party/blink/renderer/core/layout/flex/line_flexer.h
+++ third_party/blink/renderer/core/layout/flex/line_flexer.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_FLEX_LINE_FLEXER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_FLEX_LINE_FLEXER_H_
 
-#include "third_party/blink/renderer/core/layout/flex/flexible_box_algorithm.h"
+#include "third_party/blink/renderer/core/layout/flex/flex_item.h"
 #include "third_party/blink/renderer/platform/geometry/layout_unit.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
