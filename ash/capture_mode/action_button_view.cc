ed7654c30eff6c1bf81747f30219abe4aed9952f
Michelle Chen <michellegc@google.com>
2024-12-11 18:15:04 -0800

    scanner: Remove loading throbber from action button.
    UX has decided that we will close capture mode and show a progress
    notification while a Scanner action is executing.
    
    So, we can now remove the temporary action button throbber added in
    CL:5999863.
    
    Bug: b:378023303
    Change-Id: I0815820de315708b5553f39efe1bc3c2c8a37e68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073295
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395217}

--- ash/capture_mode/action_button_view.cc
+++ ash/capture_mode/action_button_view.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_id.h"
 #include "ash/style/system_shadow.h"
 #include "ash/style/typography.h"
-#include "base/check.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
@@ -28,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/controls/throbber.h"
 #include "ui/views/highlight_border.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/widget/widget.h"
@@ -51,9 +49,6 @@ constexpr int kActionButtonRadius = 18;
 
 // The size of the icon in an action button.
 constexpr int kActionButtonIconSize = 20;
-
-// The diameter of the loading throbber in an action button.
-constexpr int kActionButtonThrobberDiameter = 20;
 
 }  // namespace
 
@@ -87,10 +82,6 @@ ActionButtonView::ActionButtonView(views::Button::PressedCallback callback,
 
   SetAccessibleName(text);
   UpdateColorsAndIcon();
-
-  throbber_ = AddChildView(
-      std::make_unique<views::Throbber>(kActionButtonThrobberDiameter));
-  throbber_->SetVisible(false);
 }
 
 ActionButtonView::~ActionButtonView() = default;
@@ -131,25 +122,6 @@ void ActionButtonView::OnThemeChanged() {
 void ActionButtonView::OnEnabledChanged() {
   views::Button::OnEnabledChanged();
   UpdateColorsAndIcon();
-}
-
-void ActionButtonView::StateChanged(ButtonState old_state) {
-  if (!show_throbber_when_pressed_) {
-    return;
-  }
-
-  CHECK(throbber_);
-  const ButtonState button_state = GetState();
-  if (button_state == ButtonState::STATE_PRESSED) {
-    throbber_->SetVisible(true);
-    throbber_->Start();
-  } else if (button_state == STATE_NORMAL) {
-    // The button state is `STATE_PRESSED` then `STATE_DISABLED` while the
-    // action is in progress, but may become `STATE_NORMAL` again if the action
-    // is not successfully executed. If that happens, hide the loading throbber.
-    throbber_->Stop();
-    throbber_->SetVisible(false);
-  }
 }
 
 void ActionButtonView::UpdateColorsAndIcon() {
