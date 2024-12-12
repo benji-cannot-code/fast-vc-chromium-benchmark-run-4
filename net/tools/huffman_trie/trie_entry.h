d20ce1c41964c1afc0337e40789ef240483ae8ca
Takuto Ikuta <tikuta@chromium.org>
2024-12-11 19:14:11 -0800

    net: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: I8c2b0e1d962d3a1e1a9dd00d7f938eb5a138a5de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090372
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395231}

--- net/tools/huffman_trie/trie_entry.h
+++ net/tools/huffman_trie/trie_entry.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_TOOLS_HUFFMAN_TRIE_TRIE_ENTRY_H_
 #define NET_TOOLS_HUFFMAN_TRIE_TRIE_ENTRY_H_
 
+#include <stdint.h>
+
 #include <memory>
 #include <string>
 #include <vector>
--- net/tools/transport_security_state_generator/spki_hash.h
+++ net/tools/transport_security_state_generator/spki_hash.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_TOOLS_TRANSPORT_SECURITY_STATE_GENERATOR_SPKI_HASH_H_
 #define NET_TOOLS_TRANSPORT_SECURITY_STATE_GENERATOR_SPKI_HASH_H_
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include <string_view>
