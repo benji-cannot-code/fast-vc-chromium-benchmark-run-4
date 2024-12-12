44a2780e0a686e1d4a146a843ac819dcfc001ade
Siye Liu <siliu@microsoft.com>
2024-12-11 20:47:08 -0800

    Add usercounter for writingsuggestions feature and add to WebDXFeatures
    Bug: 366475997
    Change-Id: I37d67170f4bf631531a2481eafaff3cebf792ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088675
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Siye Liu <siliu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1395254}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -292,6 +292,8 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kReferrerPolicyHeader, WebDXFeature::kReferrerPolicy},
           {WebFeature::kHTMLAnchorElementReferrerPolicyAttribute,
            WebDXFeature::kReferrerPolicy},
+          {WebFeature::kHTMLElementWritingSuggestions,
+           WebDXFeature::kWritingSuggestions},
           {WebFeature::kHTMLIFrameElementReferrerPolicyAttribute,
            WebDXFeature::kReferrerPolicy},
           {WebFeature::kHTMLImageElementReferrerPolicyAttribute,
