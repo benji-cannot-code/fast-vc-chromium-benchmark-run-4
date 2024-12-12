8e88281f6e717473c14cd41e50b67c0ca9e2e37f
Mark Schillaci <mschillaci@google.com>
2024-12-12 12:51:41 -0800

    Add histogram to track time for first accessibility focus in Android
    This CL tracks the amount of time it takes for a single instance of
    WebContentsAccessibilityImpl to start its native initialization, until
    the same instance first gives a node accessibility focus. We only
    track from native initialization onward because that is the first
    signal that an AT is running. We restrict the recording to TalkBack,
    since we know that it always tries to focus the root node, but other
    ATs may not ever send an accessibility focus action, which would
    skew the metrics. We only record this once per instance, since there
    can only be one first focus, and we set the scale to 5 seconds max
    with the expectation of changing this if needed after initial data
    gives a sense of an accurate scale for the value.
    
    AX-Relnotes: N/A
    Bug: 383387881
    Change-Id: I5d3a1b1ba834975b5f5c96d612cde873248c14f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090216
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395580}

--- ui/accessibility/android/java/src/org/chromium/ui/accessibility/AccessibilityState.java
+++ ui/accessibility/android/java/src/org/chromium/ui/accessibility/AccessibilityState.java
@@ -64,6 +64,9 @@ public class AccessibilityState {
     public static final String AUTOFILL_COMPAT_ACCESSIBILITY_SERVICE_ID =
             "android/com.android.server.autofill.AutofillCompatAccessibilityService";
 
+    public static final String TALKBACK_SERVICE_ID =
+            "com.google.android.marvin.talkback/com.google.android.marvin.talkback.TalkBackService";
+
     // Constant value to multiply animation timeouts by for pre-Q Android versions.
     private static final int ANIMATION_TIMEOUT_MULTIPLIER = 2;
 
@@ -791,6 +794,18 @@ public class AccessibilityState {
     }
 
     /**
+     * Returns true when one of the running services is TalkBack based on list of service ids. This
+     * should not be used commonly, and is meant for identifying state while tracking metrics for
+     * performance improvements.
+     */
+    public static boolean isTalkBackEnabled() {
+        if (!sInitialized) updateAccessibilityServices();
+        return sServiceIds != null
+                && !sServiceIds.isEmpty()
+                && sServiceIds.contains(TALKBACK_SERVICE_ID);
+    }
+
+    /**
      * Return a bitmask containing the union of all event types that running accessibility services
      * listen to.
      */
@@ -1181,6 +1196,13 @@ public class AccessibilityState {
         if (!sInitialized) initializeForTesting();
 
         sEnabledServiceStringForTesting = enabledServiceString;
+    }
+
+    public static void setServiceIdsForTesting(String newServiceId) {
+        if (!sInitialized) initializeForTesting();
+
+        sServiceIds = new ArrayList<String>();
+        sServiceIds.add(newServiceId);
     }
 
     private static void initializeForTesting() {
