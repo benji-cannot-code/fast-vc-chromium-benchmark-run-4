f3a9fdacf374834dcfb47f48eef03bfa4d1df2ff
Takuto Ikuta <tikuta@chromium.org>
2024-12-12 02:27:00 -0800

    ui: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: If76633a69f857128f72ccb9ac2d14c6f87864bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088717
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395324}

--- ui/gfx/animation/keyframe/timing_function.h
+++ ui/gfx/animation/keyframe/timing_function.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_ANIMATION_KEYFRAME_TIMING_FUNCTION_H_
 #define UI_GFX_ANIMATION_KEYFRAME_TIMING_FUNCTION_H_
 
+#include <stddef.h>
+
 #include <memory>
 #include <vector>
 
