e7161fb1577863b5e6643df99f95df3d9b0a42b9
Mathijs Affourtit <maffourtit@google.com>
2024-12-12 11:19:58 -0800

    Reland "Reland "Move Libunwindstack initialization out of DFM""
    This is a reland of commit 85a6a02aca408915d944aea0ec8674c4cc2a0fee, following the fix in: https://chromium-review.googlesource.com/c/chromium/src/+/6088003
    
    Original change's description:
    > Reland "Move Libunwindstack initialization out of DFM"
    >
    > This is a reland of commit d51b5329e39f84aa48e7c62c4635822b855b5622
    >
    > Original change's description:
    > > Move Libunwindstack initialization out of DFM
    > >
    > > Previously Libunwindstack was loaded through the stack_unwinder DFM. However Libunwindstack is sufficiently small that we can load it directly. This enables using Libunwindstack without loading the DFM, which facilitates enabling SSM on Android stable for arm64.
    > >
    > > Change-Id: I0989e3581eb858b6bce15100236f66f3f4b71200
    > > Bug: 375463618
    > > Binary-Size: Expected size increase, due to moving libunwindstack out of DFM into main APK.
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937826
    > > Commit-Queue: Mathijs Affourtit <maffourtit@google.com>
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1393052}
    >
    > Bug: 375463618
    > Binary-Size: Expected size increase, due to moving libunwindstack out of DFM into main APK.
    > Change-Id: Ia23176fac7a273fe1c872dfa3f0a6e78f27fd497
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080247
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Mathijs Affourtit <maffourtit@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1394445}
    
    Bug: 375463618
    Change-Id: Ie002a36f16b0e2b3987e05f9f821c6058cf69f18
    Binary-Size: Expected size increase, due to moving libunwindstack out of DFM into main APK.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090581
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Mathijs Affourtit <maffourtit@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1395526}

--- base/profiler/native_unwinder_android.h
+++ base/profiler/native_unwinder_android.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/base_export.h"
 #include "base/memory/raw_ptr.h"
 #include "base/profiler/unwinder.h"
 #include "third_party/libunwindstack/src/libunwindstack/include/unwindstack/DexFiles.h"
@@ -23,7 +24,7 @@ class NativeUnwinderAndroidMemoryRegionsMapImpl;
 // Implementation of unwindstack::Memory that restricts memory access to a stack
 // buffer, used by NativeUnwinderAndroid. While unwinding, only memory accesses
 // within the stack should be performed to restore registers.
-class UnwindStackMemoryAndroid : public unwindstack::Memory {
+class BASE_EXPORT UnwindStackMemoryAndroid : public unwindstack::Memory {
  public:
   UnwindStackMemoryAndroid(uintptr_t stack_ptr, uintptr_t stack_top);
   ~UnwindStackMemoryAndroid() override;
@@ -36,8 +37,9 @@ class UnwindStackMemoryAndroid : public unwindstack::Memory {
 };
 
 // Native unwinder implementation for Android, using libunwindstack.
-class NativeUnwinderAndroid : public Unwinder,
-                              public ModuleCache::AuxiliaryModuleProvider {
+class BASE_EXPORT NativeUnwinderAndroid
+    : public Unwinder,
+      public ModuleCache::AuxiliaryModuleProvider {
  public:
   // Creates maps object from /proc/self/maps for use by NativeUnwinderAndroid.
   // Since this is an expensive call, the maps object should be re-used across
