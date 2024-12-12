bce26303654d8571af0fc171673cef38e76e699b
Takuto Ikuta <tikuta@chromium.org>
2024-12-11 21:39:49 -0800

    cc: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: Iad5cc9a276da830f7c6b127aecc7a9dae0d62734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090672
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395266}

--- cc/input/main_thread_scrolling_reason.h
+++ cc/input/main_thread_scrolling_reason.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_INPUT_MAIN_THREAD_SCROLLING_REASON_H_
 #define CC_INPUT_MAIN_THREAD_SCROLLING_REASON_H_
 
+#include <stdint.h>
+
 #include <memory>
 #include <string>
+
 #include "cc/cc_export.h"
 
 namespace base {
