986874499d1a2769150f4cff302ab0e58d8176d2
Sean Kau <skau@chromium.org>
2024-12-12 05:01:33 -0800

    Depend on ChildUserService directly for chrome/browser/ash/policy/status_collector
    The fascade for the factory causes a circular dependency in
    child_accounts. Remove it and source the impl from the source
    (ChildUserServiceFactory).
    
    This CL was uploaded by git cl split.
    
    R=hendrich@chromium.org
    
    Bug: b:383378298
    Change-Id: Ida8f991208127f08c0995ab2c4331602807d35fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085867
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395360}

--- chrome/browser/ash/policy/status_collector/child_status_collector.cc
+++ chrome/browser/ash/policy/status_collector/child_status_collector.cc
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/values.h"
+#include "chrome/browser/ash/child_accounts/child_user_service.h"
+#include "chrome/browser/ash/child_accounts/child_user_service_factory.h"
+#include "chrome/browser/ash/child_accounts/time_limits/app_activity_report_interface.h"
 #include "chrome/browser/ash/policy/status_collector/child_activity_storage.h"
 #include "chrome/browser/ash/policy/status_collector/status_collector_state.h"
 #include "chrome/browser/browser_process.h"
@@ -212,7 +215,7 @@ void ChildStatusCollector::OnAppActivityReportSubmitted() {
   DCHECK(last_report_params_);
   if (last_report_params_->anything_reported) {
     ash::app_time::AppActivityReportInterface* app_activity_reporting =
-        ash::app_time::AppActivityReportInterface::Get(profile_);
+        ash::ChildUserServiceFactory::GetForBrowserContext(profile_);
     DCHECK(app_activity_reporting);
     app_activity_reporting->AppActivityReportSubmitted(
         last_report_params_->generation_time);
@@ -291,7 +294,7 @@ bool ChildStatusCollector::GetActivityTimes(
 bool ChildStatusCollector::GetAppActivity(
     em::ChildStatusReportRequest* status) {
   ash::app_time::AppActivityReportInterface* app_activity_reporting =
-      ash::app_time::AppActivityReportInterface::Get(profile_);
+      ash::ChildUserServiceFactory::GetForBrowserContext(profile_);
   DCHECK(app_activity_reporting);
 
   last_report_params_ =
