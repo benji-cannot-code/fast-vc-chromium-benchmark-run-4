45cfb4e0d474d16429f105974d3b8aba66e62519
Jihad Hanna <jihadghanna@google.com>
2024-12-12 15:59:28 -0800

    Remove FormTracker::Observer
    In an effort to remove submission logic from AutofillAgent and move it
    to FormTracker, this CL removes the FormTracker::Observer class since
    AutofillAgent is the only observer and no further observers are
    anticipated. FormTracker can access the agent directly through the
    raw_ref variable stored in it.
    
    Bug: 40281981
    Change-Id: Ic5618fa9d72d4ccb806c9bd5397b95cc89135b36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088430
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395668}

--- components/autofill/content/renderer/form_tracker.cc
+++ components/autofill/content/renderer/form_tracker.cc
@@ -125,18 +125,6 @@ FormTracker::~FormTracker() {
   ResetLastInteractedElements();
 }
 
-void FormTracker::AddObserver(Observer* observer) {
-  DCHECK(observer);
-  DCHECK_CALLED_ON_VALID_SEQUENCE(form_tracker_sequence_checker_);
-  observers_.AddObserver(observer);
-}
-
-void FormTracker::RemoveObserver(Observer* observer) {
-  DCHECK(observer);
-  DCHECK_CALLED_ON_VALID_SEQUENCE(form_tracker_sequence_checker_);
-  observers_.RemoveObserver(observer);
-}
-
 void FormTracker::AjaxSucceeded() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(form_tracker_sequence_checker_);
   submission_triggering_events_.xhr_succeeded = true;
@@ -171,11 +159,11 @@ void FormTracker::TextFieldDidChange(const WebFormControlElement& element) {
   unsafe_render_frame()
       ->GetWebFrame()
       ->GetTaskRunner(blink::TaskType::kInternalUserInteraction)
-      ->PostTask(FROM_HERE, base::BindRepeating(
-                                &FormTracker::FormControlDidChangeImpl,
-                                weak_ptr_factory_.GetWeakPtr(),
-                                form_util::GetFieldRendererId(element),
-                                Observer::SaveFormReason::kTextFieldChanged));
+      ->PostTask(FROM_HERE,
+                 base::BindRepeating(&FormTracker::FormControlDidChangeImpl,
+                                     weak_ptr_factory_.GetWeakPtr(),
+                                     form_util::GetFieldRendererId(element),
+                                     SaveFormReason::kTextFieldChanged));
 }
 
 void FormTracker::SelectControlDidChange(const WebFormControlElement& element) {
@@ -192,7 +180,7 @@ void FormTracker::SelectControlDidChange(const WebFormControlElement& element) {
                  base::BindRepeating(&FormTracker::FormControlDidChangeImpl,
                                      weak_ptr_factory_.GetWeakPtr(),
                                      form_util::GetFieldRendererId(element),
-                                     Observer::SaveFormReason::kSelectChanged));
+                                     SaveFormReason::kSelectChanged));
 }
 
 void FormTracker::ElementDisappeared(const blink::WebElement& element) {
@@ -251,9 +239,8 @@ void FormTracker::TrackAutofilledElement(const WebFormControlElement& element) {
   TrackElement(mojom::SubmissionSource::DOM_MUTATION_AFTER_AUTOFILL);
 }
 
-void FormTracker::FormControlDidChangeImpl(
-    FieldRendererId element_id,
-    Observer::SaveFormReason change_source) {
+void FormTracker::FormControlDidChangeImpl(FieldRendererId element_id,
+                                           SaveFormReason change_source) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(form_tracker_sequence_checker_);
   WebFormControlElement element =
       form_util::GetFormControlByRendererId(element_id);
@@ -269,9 +256,7 @@ void FormTracker::FormControlDidChangeImpl(
   } else {
     UpdateLastInteractedElement(form_util::GetFieldRendererId(element));
   }
-  for (auto& observer : observers_) {
-    observer.OnProvisionallySaveForm(form_element, element, change_source);
-  }
+  agent_->OnProvisionallySaveForm(form_element, element, change_source);
 }
 
 void FormTracker::DidCommitProvisionalLoad(ui::PageTransition transition) {
@@ -333,11 +318,8 @@ void FormTracker::WillSendSubmitEvent(const WebFormElement& form) {
     // document the reason, otherwise remove.
     UpdateLastInteractedElement(form_util::GetFormRendererId(form));
   }
-  for (auto& observer : observers_) {
-    observer.OnProvisionallySaveForm(
-        form, blink::WebFormControlElement(),
-        Observer::SaveFormReason::kWillSendSubmitEvent);
-  }
+  agent_->OnProvisionallySaveForm(form, blink::WebFormControlElement(),
+                                  SaveFormReason::kWillSendSubmitEvent);
 }
 
 void FormTracker::WillSubmitForm(const WebFormElement& form) {
@@ -363,8 +345,7 @@ void FormTracker::OnDestruct() {
 void FormTracker::FireFormSubmitted(const blink::WebFormElement& form) {
   base::UmaHistogramEnumeration(kSubmissionSourceHistogram,
                                 SubmissionSource::FORM_SUBMISSION);
-  for (auto& observer : observers_)
-    observer.OnFormSubmitted(form);
+  agent_->OnFormSubmitted(form);
   if (!base::FeatureList::IsEnabled(features::kAutofillFixFormTracking)) {
     ResetLastInteractedElements();
   }
@@ -374,9 +355,7 @@ void FormTracker::FireProbablyFormSubmitted() {
   if (IsTracking()) {
     base::UmaHistogramEnumeration(kSubmissionSourceHistogram,
                                   SubmissionSource::PROBABLY_FORM_SUBMITTED);
-    for (auto& observer : observers_) {
-      observer.OnProbablyFormSubmitted();
-    }
+    agent_->OnProbablyFormSubmitted();
     if (!base::FeatureList::IsEnabled(features::kAutofillFixFormTracking)) {
       ResetLastInteractedElements();
     }
@@ -387,9 +366,7 @@ void FormTracker::FireInferredFormSubmission(SubmissionSource source) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(form_tracker_sequence_checker_);
   if (IsTracking()) {
     base::UmaHistogramEnumeration(kSubmissionSourceHistogram, source);
-    for (auto& observer : observers_) {
-      observer.OnInferredFormSubmission(source);
-    }
+    agent_->OnInferredFormSubmission(source);
     if (source != SubmissionSource::DOM_MUTATION_AFTER_AUTOFILL ||
         !base::FeatureList::IsEnabled(
             features::kAutofillAcceptDomMutationAfterAutofillSubmission)) {
--- components/autofill/content/renderer/form_tracker_browsertest.cc
+++ components/autofill/content/renderer/form_tracker_browsertest.cc
@@ -18,16 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 namespace {
 
-class MockFormTrackerObserver : public FormTracker::Observer {
+class MockFormTracker : public FormTracker {
  public:
-  // Not a mock method so that gmock ignores calls to this method.
-  void OnProvisionallySaveForm(const blink::WebFormElement&,
-                               const blink::WebFormControlElement&,
-                               SaveFormReason) override {}
-
-  MOCK_METHOD0(OnProbablyFormSubmitted, void());
-  MOCK_METHOD1(OnFormSubmitted, void(const blink::WebFormElement&));
-  MOCK_METHOD1(OnInferredFormSubmission, void(mojom::SubmissionSource));
+  using FormTracker::FormTracker;
+  MOCK_METHOD((void),
+              FireInferredFormSubmission,
+              (mojom::SubmissionSource),
+              (override));
 };
 
 class FormTrackerTest : public test::AutofillRendererTest,
@@ -48,6 +45,20 @@ class FormTrackerTest : public test::AutofillRendererTest,
         features.begin() + GetParam(), features.end());
     scoped_feature_list_.InitWithFeatures(enabled_features, disabled_features);
   }
+
+  void SetUp() override {
+    test::AutofillRendererTest::SetUp();
+    test_api(autofill_agent())
+        .set_form_tracker(std::make_unique<MockFormTracker>(
+            GetMainRenderFrame(), FormTracker::UserGestureRequired(true),
+            autofill_agent()));
+  }
+
+  MockFormTracker& form_tracker() {
+    return static_cast<MockFormTracker&>(
+        test_api(autofill_agent()).form_tracker());
+  }
+
   blink::WebFormControlElement GetFormControlById(const std::string& id) {
     return GetMainFrame()
         ->GetDocument()
@@ -72,23 +83,20 @@ TEST_P(FormTrackerTest, FormlessXHRThenHide) {
 
   blink::WebFormControlElement input1 = GetFormControlById("input1");
 
-  testing::StrictMock<MockFormTrackerObserver> observer;
-  test_api(autofill_agent()).form_tracker().AddObserver(&observer);
-
   GetMainFrame()->NotifyUserActivation(
       blink::mojom::UserActivationNotificationType::kTest);
   ExecuteJavaScriptForTests("document.getElementById('input1').focus();");
-  test_api(autofill_agent()).form_tracker().TextFieldDidChange(input1);
+  form_tracker().TextFieldDidChange(input1);
 
   task_environment_.RunUntilIdle();
 
-  test_api(autofill_agent()).form_tracker().AjaxSucceeded();
+  form_tracker().AjaxSucceeded();
   task_environment_.RunUntilIdle();
   // FormTracker should not think there is a submission because the <input>s are
   // still visible.
 
   // FormTracker should detect a submission after the <input>s are hidden.
-  EXPECT_CALL(observer, OnInferredFormSubmission).Times(1);
+  EXPECT_CALL(form_tracker(), FireInferredFormSubmission).Times(1);
   ExecuteJavaScriptForTests(
       R"(document.getElementById('input1').style.display = 'none';
          document.getElementById('input2').style.display = 'none';)");
@@ -104,13 +112,10 @@ TEST_P(FormTrackerTest, FormlessHideThenXhr) {
 
   blink::WebFormControlElement input1 = GetFormControlById("input1");
 
-  testing::StrictMock<MockFormTrackerObserver> observer;
-  test_api(autofill_agent()).form_tracker().AddObserver(&observer);
-
   GetMainFrame()->NotifyUserActivation(
       blink::mojom::UserActivationNotificationType::kTest);
   ExecuteJavaScriptForTests("document.getElementById('input1').focus();");
-  test_api(autofill_agent()).form_tracker().TextFieldDidChange(input1);
+  form_tracker().TextFieldDidChange(input1);
   task_environment_.RunUntilIdle();
 
   ExecuteJavaScriptForTests(
@@ -122,8 +127,8 @@ TEST_P(FormTrackerTest, FormlessHideThenXhr) {
   // done any XHRs.
 
   // FormTracker should detect a submission when the XHR succeeds.
-  EXPECT_CALL(observer, OnInferredFormSubmission).Times(1);
-  test_api(autofill_agent()).form_tracker().AjaxSucceeded();
+  EXPECT_CALL(form_tracker(), FireInferredFormSubmission).Times(1);
+  form_tracker().AjaxSucceeded();
   task_environment_.RunUntilIdle();
 }
 
