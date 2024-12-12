7063bde9412913a7df49ca9a35a91f2be8fa8056
Takuto Ikuta <tikuta@chromium.org>
2024-12-11 20:02:46 -0800

    gpu: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: I08fdf2d75d7e0072f90e96a6aa4a6aee8047af6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090770
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1395244}

--- gpu/config/webgpu_blocklist_impl.cc
+++ gpu/config/webgpu_blocklist_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/config/webgpu_blocklist_impl.h"
 
+#include <array>
 #include <sstream>
 #include <string_view>
 
