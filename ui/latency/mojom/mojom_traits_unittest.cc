554690ce9a2d9e670d76d3a760dd0d3bd75d8055
Alex Gough <ajgo@chromium.org>
2024-12-12 12:50:41 -0800

    Rename test helper mojoms in //ui/latency/mojom
    Testonly mojoms can have extension test-mojom and do not
    need security review in future.
    
    Bug: 383380971
    Change-Id: I9b3ee60c7c9f208b50af9bbc4d4c6f7b494958db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089859
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395579}

--- ui/latency/mojom/mojom_traits_unittest.cc
+++ ui/latency/mojom/mojom_traits_unittest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/latency/mojom/latency_info_mojom_traits.h"
-#include "ui/latency/mojom/traits_test_service.mojom.h"
+#include "ui/latency/mojom/traits_test_service.test-mojom.h"
 
 namespace ui {
 
