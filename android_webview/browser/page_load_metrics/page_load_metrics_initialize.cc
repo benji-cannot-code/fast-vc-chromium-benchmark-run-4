c47a14e9cd9280566997aa73171a920867a1dc27
Nick Armstrong <narms@google.com>
2024-12-11 17:53:30 -0800

    Add a histogram to track page load results in WebView
    This histogram will be used to track the success rate of page loads in WebView.
    
    BUG=372336009
    
    Change-Id: Ia67d2748eeb1a5893f879be480543e214eabe4c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5976965
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Nick Armstrong <narms@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395204}

--- android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/page_load_metrics/aw_gws_page_load_metrics_observer.h"
 #include "android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.h"
+#include "android_webview/browser/page_load_metrics/service_level_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/page_load_metrics/browser/observers/abandoned_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/third_party_metrics_observer.h"
@@ -66,6 +67,7 @@ void PageLoadMetricsEmbedder::RegisterObservers(
   tracker->AddObserver(std::make_unique<AbandonedPageLoadMetricsObserver>());
   tracker->AddObserver(std::make_unique<GWSAbandonedPageLoadMetricsObserver>());
   tracker->AddObserver(std::make_unique<AwGWSPageLoadMetricsObserver>());
+  tracker->AddObserver(std::make_unique<ServiceLevelPageLoadMetricsObserver>());
 }
 
 bool PageLoadMetricsEmbedder::IsNewTabPageUrl(const GURL& url) {
