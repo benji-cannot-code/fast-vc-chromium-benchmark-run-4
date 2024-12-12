084c1e4ee7eb85bda26f75b7c22a01a71021cd02
Min Qin <qinmin@chromium.org>
2024-12-12 14:24:39 -0800

    Fix a crash when converting alternative title to utf16
    The alternative title is utf8 and html escaped. So the correct method
    to convert it to utf16 string then call UnescapeForHTML.
    
    Bug: 383727398
    Change-Id: I9a297316ee775ef0a66b8e1bffa31d325020fbcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092211
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395623}

--- components/saved_tab_groups/internal/tab_group_sync_service_impl.cc
+++ components/saved_tab_groups/internal/tab_group_sync_service_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/observer_list.h"
+#include "base/strings/escape.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/uuid.h"
@@ -79,7 +80,8 @@ void UpdateTabTitleIfNeeded(
     if (page_entities_metadata.has_value() &&
         !page_entities_metadata->alternative_title().empty()) {
       use_url_as_title = false;
-      title = base::ASCIIToUTF16(page_entities_metadata->alternative_title());
+      title = base::UnescapeForHTML(
+          base::UTF8ToUTF16(page_entities_metadata->alternative_title()));
     }
   }
   stats::RecordSharedGroupTitleSanitization(use_url_as_title, type);
