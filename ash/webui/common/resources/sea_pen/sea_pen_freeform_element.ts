cd5bcc6b61fd203347af66a726182e57447e9733
Erica Lee <ericamlee@google.com>
2024-12-12 12:04:28 -0800

    p13n: fix a11y for freeform tabs
    Added keypress access and roles for the freeform tabs.
    
    https://screencast.googleplex.com/cast/NDcyNTg0NTA4NzIyMzgwOHw4YzhhZWQyNS1lYw
    
    BUG=b:366264535
    TEST=browser_tests *SeaPenFreeform*
    
    Change-Id: I9f9a57f1964d111edf49dddfc6b76a8e60b1a8a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090094
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395554}

--- ash/webui/common/resources/sea_pen/sea_pen_freeform_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_freeform_element.ts
@@ -13,6 +13,7 @@ import 'chrome://resources/ash/common/personalization/personalization_shared_ico
 import 'chrome://resources/ash/common/personalization/wallpaper.css.js';
 
 import {assertNotReached} from 'chrome://resources/js/assert.js';
+import {IronA11yKeysElement} from 'chrome://resources/polymer/v3_0/iron-a11y-keys/iron-a11y-keys.js';
 
 import {FreeformTab, SeaPenSamplePrompt} from './constants.js';
 import {MantaStatusCode, SeaPenQuery} from './sea_pen.mojom-webui.js';
@@ -21,6 +22,15 @@ import {logSamplePromptShuffleClicked, logSeaPenFreeformTabClicked} from './sea_
 import {WithSeaPenStore} from './sea_pen_store.js';
 import {SEA_PEN_SAMPLES} from './sea_pen_untranslated_constants.js';
 import {isArrayEqual, shuffle} from './sea_pen_utils.js';
+
+export interface SeaPenFreeformElement {
+  $: {
+    samplePromptsTab: HTMLElement,
+    resultsTab: HTMLElement,
+    tabContainer: HTMLElement,
+    tabKeys: IronA11yKeysElement,
+  }
+}
 
 export class SeaPenFreeformElement extends WithSeaPenStore {
   static get is() {
@@ -68,6 +78,7 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
         state => state.thumbnailResponseStatusCode);
     this.updateFromStore();
     this.shuffleSamplePrompts_();
+    this.$.tabKeys.target = this.$.tabContainer;
   }
 
   /** Invoked on tab selected. */
@@ -86,6 +97,22 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
     logSeaPenFreeformTabClicked(this.freeformTab_);
   }
 
+  /** Handle keyboard navigation. */
+  private onTabKeysPressed_(e: CustomEvent) {
+    const focusedElement = this.shadowRoot!.activeElement;
+    // Remove focus state of focused tab.
+    focusedElement?.removeAttribute('tabindex');
+
+    const nextTab = this.getOtherTab_(focusedElement)
+    if (nextTab) {
+      // Add focus state for next tab.
+      nextTab.setAttribute('tabindex', '0');
+      nextTab.focus();
+    }
+
+    e.detail.keyboardEvent.preventDefault();
+  }
+
   private onSeaPenQueryChanged_(query: SeaPenQuery|null) {
     // Update selected tab to Results tab once a freeform query search starts.
     // Otherwise, stay in Sample Prompts tab.
@@ -100,10 +127,10 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
     }
   }
 
-  private isTabContainerEnabled_(
+  private isTabContainerHidden_(
       query: SeaPenQuery,
       thumbnailResponseStatusCode: MantaStatusCode|null): boolean {
-    return !!query?.textQuery || !!thumbnailResponseStatusCode;
+    return !query?.textQuery && !thumbnailResponseStatusCode;
   }
 
   private isSamplePromptsTabSelected_(tab: FreeformTab): boolean {
@@ -112,6 +139,24 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
 
   private isResultsTabSelected_(tab: FreeformTab): boolean {
     return tab === FreeformTab.RESULTS;
+  }
+
+  private getSamplePromptsTabIndex_(tab: FreeformTab): string {
+    return this.isSamplePromptsTabSelected_(tab) ? '0' : '-1'
+  }
+
+  private getResultsTabIndex_(tab: FreeformTab): string {
+    return this.isResultsTabSelected_(tab) ? '0' : '-1'
+  }
+
+  private getOtherTab_(element: Element|null): HTMLElement|null {
+    if (element === this.$.resultsTab) {
+      return this.$.samplePromptsTab;
+    }
+    if (element === this.$.samplePromptsTab) {
+      return this.$.resultsTab;
+    }
+    return null;
   }
 
   private onShuffleClicked_(): void {
