fc4e66f10e7158ee7b502b3c65fe56d7f158033e
Ken Okada <kenoss@chromium.org>
2024-12-11 21:21:37 -0800

    Revert "[lensoverlay] Reenable SelectionOverlay tests"
    This reverts commit 4c3feaba028fa065fd135f95f97a1908b15a0a45.
    
    Reason for revert: Still flaky https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/123768/overview
    
    Original change's description:
    > [lensoverlay] Reenable SelectionOverlay tests
    >
    > The tests were disabled due to flakiness. They stayed disabled for so
    > long that they eventually became broken. I fixed the tests and am
    > reenabling. If they are still flaky, I will fix ASAP.
    >
    > One test that verified the user can drag a post selection corner over a
    > word bounding box was failing because the feature was broken, so I fixed
    > the feature rather than changing the test.
    >
    > Change-Id: I349c4c32c350c840b9374b5f069a62d7aeda2517
    > Fixed: 383376596
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085839
    > Reviewed-by: Juan Mojica <juanmojica@google.com>
    > Commit-Queue: Duncan Mercer <mercerd@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1395026}
    
    Change-Id: I8d39881fee10a88c56d0b11db4f2258ebe19a641
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088655
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395261}

--- chrome/test/data/webui/lens/overlay/selection_overlay_test.ts
+++ chrome/test/data/webui/lens/overlay/selection_overlay_test.ts
@@ -106,8 +106,6 @@ suite('SelectionOverlay', function() {
       ]),
     ]);
     callbackRouterRemote.textReceived(text);
-    await flushTasks();
-
     const semanticEventArgs = await testBrowserProxy.handler.getArgs(
         'recordLensOverlaySemanticEvent');
     const semanticEvent = semanticEventArgs[semanticEventArgs.length - 1];
@@ -115,7 +113,7 @@ suite('SelectionOverlay', function() {
     await waitAfterNextRender(selectionOverlayElement);
   }
 
-  async function addWordsWithTranslations() {
+  function addWordsWithTranslations() {
     const text = createText([
       createParagraph(
           [
@@ -160,7 +158,7 @@ suite('SelectionOverlay', function() {
     return flushTasks();
   }
 
-  async function addObjects() {
+  function addObjects() {
     objects =
         [
           {x: 80, y: 20, width: 25, height: 10},
@@ -205,6 +203,7 @@ suite('SelectionOverlay', function() {
             SemanticEvent.kTextGleamsViewEnd, penultimateSemanticEvent);
       });
 
+  // <if expr="not chromeos_lacros">
   test(
       'verify that starting a drag on a word does not trigger region search',
       async () => {
@@ -226,6 +225,7 @@ suite('SelectionOverlay', function() {
         assertEquals(
             0, testBrowserProxy.handler.getCallCount('issueLensRegionRequest'));
       });
+  // </if>
 
   test(
       `verify that starting a drag off a word and continuing onto a word triggers region search`,
@@ -288,7 +288,7 @@ suite('SelectionOverlay', function() {
                 .regionSelectionCanvas.height);
       });
 
-  test(
+    test(
       'verify object selection canvas resizes when selection overlay resizes',
       async () => {
         selectionOverlayElement.style.display = 'block';
@@ -319,15 +319,15 @@ suite('SelectionOverlay', function() {
                 .objectSelectionCanvas.height);
       });
 
-  test(
+    test(
       `verify that text respond to taps, even when an object is underneath`,
       async () => {
         await Promise.all([addWords(), addObjects()]);
 
         await simulateClick(selectionOverlayElement, {x: 80, y: 20});
 
-        const textQuery = await testBrowserProxy.handler.whenCalled(
-            'issueTextSelectionRequest');
+        const textQuery =
+            await testBrowserProxy.handler.whenCalled('issueTextSelectionRequest');
         assertDeepEquals('test', textQuery);
         assertEquals(
             0, testBrowserProxy.handler.getCallCount('issueLensRegionRequest'));
@@ -365,6 +365,7 @@ suite('SelectionOverlay', function() {
         verifyRegionRequest(expectedRect, /*expectedIsClick=*/ false);
       });
 
+  // <if expr="not chromeos_lacros">
   test(
       'verify that region search over text triggers detected text options',
       async () => {
@@ -513,6 +514,7 @@ suite('SelectionOverlay', function() {
         assertTrue(selectionOverlayElement
                        .getShowSelectedRegionContextMenuForTesting());
       });
+  // </if>
 
   test('verify that region search triggers post selection', async () => {
     await simulateDrag(
@@ -659,7 +661,7 @@ suite('SelectionOverlay', function() {
     const wordElBoundingBox = wordEl.getBoundingClientRect();
     await simulateDrag(
         selectionOverlayElement, {
-          x: wordElBoundingBox.left + (wordElBoundingBox.width / 2),
+          x: wordElBoundingBox.left + (wordElBoundingBox.width / 3),
           y: wordElBoundingBox.top + (wordElBoundingBox.height / 2),
         },
         {
@@ -685,7 +687,7 @@ suite('SelectionOverlay', function() {
     const wordElBoundingBox = wordEl.getBoundingClientRect();
     await simulateDrag(
         selectionOverlayElement, {
-          x: wordElBoundingBox.left + (wordElBoundingBox.width / 2),
+          x: wordElBoundingBox.left + (wordElBoundingBox.width / 3),
           y: wordElBoundingBox.top + (wordElBoundingBox.height / 2),
         },
         {
@@ -731,7 +733,7 @@ suite('SelectionOverlay', function() {
         const wordElBoundingBox = wordEl.getBoundingClientRect();
         await simulateDrag(
             selectionOverlayElement, {
-              x: wordElBoundingBox.left + (wordElBoundingBox.width / 2),
+              x: wordElBoundingBox.left + (wordElBoundingBox.width / 3),
               y: wordElBoundingBox.top + (wordElBoundingBox.height / 2),
             },
             {
@@ -864,8 +866,8 @@ suite('SelectionOverlay', function() {
       });
 
   test(
-      `verify that post selection corners are draggable over text and
-      objects`, async () => {
+      `verify that post selection corners are draggable over text and objects`,
+      async () => {
         await Promise.all([addWords(), addObjects()]);
         // Add the post selection to have top left corner overlap with text
         // and objects
@@ -894,7 +896,6 @@ suite('SelectionOverlay', function() {
         };
         verifyRegionRequest(expectedRect, /*expectedIsClick=*/ false);
       });
-
   test(
       'verify that completing a drag calls closePreselectionBubble',
       async () => {
