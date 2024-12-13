1e6198150b4df7291ffea46e31c4a5a4c0bf9504
Leonard Grey <lgrey@chromium.org>
2024-12-13 07:54:27 -0800

    Mac: undo confirm quit if quit is interrupted
    This adds a general mechanism (the `cancel` method) and hooks it up to
    two specific cases:
    - The user choosing to not quit if downloads are in progress
    - The user choosing to not leave from a `beforeunload` pop-up
    
    The test is kind of weak, but given the challenges with nested run
    loops (and uh...trying to not actually quit) it's the best I could
    come up with.
    
    Bug: 380188563, 40604617
    Change-Id: Iac247f78a83eb54cbb9692324b694bc68ecce6be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092004
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395969}

--- chrome/browser/ui/cocoa/confirm_quit_panel_controller.h
+++ chrome/browser/ui/cocoa/confirm_quit_panel_controller.h
@@ -32,6 +32,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // instructions on how to quit.
 - (void)dismissPanel;
 
+// Completely back out of the process, making all windows visible again.
+// Called when the quit was aborted *after* confirmations (for example, due to
+// pending downloads or `beforeunload`).
+- (void)cancel;
+
+// Hides windows and set state as if we had run `runModalLoop` and received
+// a key up from the user.
+- (void)simulateQuitForTesting;
+
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_CONFIRM_QUIT_PANEL_CONTROLLER_H_
