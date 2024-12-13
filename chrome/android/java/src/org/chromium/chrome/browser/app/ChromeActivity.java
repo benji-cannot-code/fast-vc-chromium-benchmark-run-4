439268755127c565165a548bc7bbe488e924bd20
Shu Yang <shuyng@google.com>
2024-12-13 15:09:34 -0800

    Record two metrics for Android
    Android.Pdf.AssistContent.IsWorkProfile:
    Records whether we are running in a work profile when AssistContent is
    generated successfully for pdf pages.
    
    Android.Pdf.AssistContent.IsEnterpriseInfoCached:
    Records whether the device enterprise info has been cached when getting
    it synchronously for pdf pages.
    
    Bug: 383028979
    Change-Id: I2f19d6f98da03160d5c4ce818c4aa7f7610942fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088009
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396233}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -1471,6 +1471,8 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
                     && tab.getNativePage() instanceof PdfPage pdfPage) {
                 EnterpriseInfo.OwnedState state =
                         EnterpriseInfo.getInstance().getDeviceEnterpriseInfoSync();
+                RecordHistogram.recordBooleanHistogram(
+                        "Android.Pdf.AssistContent.IsEnterpriseInfoCached", state != null);
                 if (state == null) return;
                 String structuredData = pdfPage.requestAssistContent(state.mProfileOwned);
                 if (structuredData != null) {
