dca214b28f64869d45154e5f0d8464f3097d6935
James Cook <jamescook@chromium.org>
2024-12-13 14:41:40 -0800

    lacros cleanup: Delete calls to ExtensionInfoPrivate
    Lacros is sunset and is no longer built or tested. Remove lacros-only
    code that called the ExtensionInfoPrivate crosapi.
    
    The crosapi will need to be removed in a separate CL as its
    implementation is still used by ash-chrome.
    
    Bug: 373971535
    Change-Id: I71f1987a218b930e80daf71ff059aad743bc0b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088432
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396216}

--- chrome/browser/chromeos/extensions/info_private/info_private_api.cc
+++ chrome/browser/chromeos/extensions/info_private/info_private_api.cc
@@ -13,18 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
-#include "extensions/browser/extension_function.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ash/crosapi/crosapi_ash.h"
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "chrome/browser/ash/crosapi/extension_info_private_ash.h"
-#endif
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chromeos/crosapi/mojom/extension_info_private.mojom.h"  // nogncheck
-#include "chromeos/lacros/lacros_service.h"
-#endif
+#include "extensions/browser/extension_function.h"
 
 namespace {
 
@@ -33,17 +25,6 @@ const char kPropertyTimezone[] = "timezone";
 
 // Property not found error message.
 const char kPropertyNotFound[] = "Property '*' does not exist.";
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-mojo::Remote<crosapi::mojom::ExtensionInfoPrivate>*
-GetExtensionInfoPrivateRemote() {
-  auto* lacros_service = chromeos::LacrosService::Get();
-  if (lacros_service->IsAvailable<crosapi::mojom::ExtensionInfoPrivate>()) {
-    return &(lacros_service->GetRemote<crosapi::mojom::ExtensionInfoPrivate>());
-  }
-  return nullptr;
-}
-#endif
 
 }  // namespace
 
@@ -65,19 +46,10 @@ ExtensionFunction::ResponseAction ChromeosInfoPrivateGetFunction::Run() {
   }
   auto callback =
       base::BindOnce(&ChromeosInfoPrivateGetFunction::RespondWithResult, this);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   crosapi::CrosapiManager::Get()
       ->crosapi_ash()
       ->extension_info_private_ash()
       ->GetSystemProperties(std::move(property_names), std::move(callback));
-#else
-  auto* remote = GetExtensionInfoPrivateRemote();
-  if (!remote) {
-    return RespondNow(Error("ExtensionInfoPrivate unavailable."));
-  }
-  (*remote)->GetSystemProperties(std::move(property_names),
-                                 std::move(callback));
-#endif
   return RespondLater();
 }
 
@@ -98,18 +70,10 @@ ExtensionFunction::ResponseAction ChromeosInfoPrivateSetFunction::Run() {
     EXTENSION_FUNCTION_VALIDATE(args().size() >= 2);
     EXTENSION_FUNCTION_VALIDATE(args()[1].is_string());
     const std::string& param_value = args()[1].GetString();
-#if BUILDFLAG(IS_CHROMEOS_ASH)
     crosapi::CrosapiManager::Get()
         ->crosapi_ash()
         ->extension_info_private_ash()
         ->SetTimezone(param_value);
-#else
-    auto* remote = GetExtensionInfoPrivateRemote();
-    if (!remote) {
-      return RespondNow(Error("ExtensionInfoPrivate unavailable."));
-    }
-    (*remote)->SetTimezone(param_value);
-#endif
     return RespondNow(NoArguments());
   }
 
@@ -119,18 +83,10 @@ ExtensionFunction::ResponseAction ChromeosInfoPrivateSetFunction::Run() {
 
   auto callback =
       base::BindOnce(&ChromeosInfoPrivateSetFunction::RespondWithResult, this);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   crosapi::CrosapiManager::Get()
       ->crosapi_ash()
       ->extension_info_private_ash()
       ->SetBool(param_name_, param_value, std::move(callback));
-#else
-  auto* remote = GetExtensionInfoPrivateRemote();
-  if (!remote) {
-    return RespondNow(Error("ExtensionInfoPrivate unavailable."));
-  }
-  (*remote)->SetBool(param_name_, param_value, std::move(callback));
-#endif
 
   return RespondLater();
 }
@@ -153,18 +109,10 @@ ExtensionFunction::ResponseAction
 ChromeosInfoPrivateIsTabletModeEnabledFunction::Run() {
   auto callback = base::BindOnce(
       &ChromeosInfoPrivateIsTabletModeEnabledFunction::RespondWithResult, this);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   crosapi::CrosapiManager::Get()
       ->crosapi_ash()
       ->extension_info_private_ash()
       ->IsTabletModeEnabled(std::move(callback));
-#else
-  auto* remote = GetExtensionInfoPrivateRemote();
-  if (!remote) {
-    return RespondNow(Error("ExtensionInfoPrivate unavailable."));
-  }
-  (*remote)->IsTabletModeEnabled(std::move(callback));
-#endif
   return RespondLater();
 }
 
@@ -181,11 +129,7 @@ ChromeosInfoPrivateIsRunningOnLacrosFunction::
 
 ExtensionFunction::ResponseAction
 ChromeosInfoPrivateIsRunningOnLacrosFunction::Run() {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   return RespondNow(WithArguments(false));
-#else
-  return RespondNow(WithArguments(true));
-#endif
 }
 
 }  // namespace extensions
