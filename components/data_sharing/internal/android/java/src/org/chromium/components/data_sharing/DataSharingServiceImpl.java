f21526319f365b63211d4f94a4ca2c4817920e97
ssid <ssid@chromium.org>
2024-12-13 11:18:59 -0800

    Add a testing setter for preview data
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL tests are in clank
    Bug: 379703994
    Change-Id: I703c16960dba88a7d2f9c08ca5eb094abdd4c822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6093269
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396090}

--- components/data_sharing/internal/android/java/src/org/chromium/components/data_sharing/DataSharingServiceImpl.java
+++ components/data_sharing/internal/android/java/src/org/chromium/components/data_sharing/DataSharingServiceImpl.java
@@ -10,6 +10,7 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.Callback;
+import org.chromium.base.ResettersForTesting;
 import org.chromium.base.UserDataHost;
 import org.chromium.url.GURL;
 
@@ -116,6 +117,10 @@ public class DataSharingServiceImpl implements DataSharingService {
     @Override
     public void getSharedEntitiesPreview(
             GroupToken groupToken, Callback<SharedDataPreviewOrFailureOutcome> callback) {
+        if (sSharedEntitiesPreviewForTesting != null) {
+            callback.onResult(sSharedEntitiesPreviewForTesting);
+            return;
+        }
         DataSharingServiceImplJni.get()
                 .getSharedEntitiesPreview(
                         mNativePtr, groupToken.collaborationId, groupToken.accessToken, callback);
@@ -124,6 +129,15 @@ public class DataSharingServiceImpl implements DataSharingService {
     @Override
     public DataSharingUIDelegate getUiDelegate() {
         return DataSharingServiceImplJni.get().getUiDelegate(mNativePtr);
+    }
+
+    private static SharedDataPreviewOrFailureOutcome sSharedEntitiesPreviewForTesting;
+
+    /** Sets a test preview data to return for all preview requests. */
+    public static void setSharedEntitiesPreviewForTesting(
+            SharedDataPreviewOrFailureOutcome preview) {
+        sSharedEntitiesPreviewForTesting = preview;
+        ResettersForTesting.register(() -> sSharedEntitiesPreviewForTesting = null);
     }
 
     @CalledByNative
