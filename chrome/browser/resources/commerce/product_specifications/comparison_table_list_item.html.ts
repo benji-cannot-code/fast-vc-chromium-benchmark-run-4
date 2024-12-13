62d4e758cd08652783dee48a481ddd39ffe2f5af
Chase Hartsell <chazzy@chromium.org>
2024-12-13 09:27:52 -0800

    [Compare] Add comparison table list item element
    This patch adds the comparison table list item element that will be
    displayed in the list of comparison tables next to the empty table.
    This element displays a table's name, URL, number of items, and first
    product image.
    
    http://screen/7NVXPYDfNiXfAk4
    
    Bug: 377939401
    Change-Id: Iceeb7c29f411a9efde3239e150497e5b1abd2d3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090390
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396015}

---
+++ chrome/browser/resources/commerce/product_specifications/comparison_table_list_item.html.ts
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ComparisonTableListItemElement} from './comparison_table_list_item.js';
+
+export function getHtml(this: ComparisonTableListItemElement) {
+  // clang-format off
+  return html`
+  <div id="itemContainer">
+    <cr-url-list-item
+        id="item"
+        size="large"
+        title="${this.getTitle_()}"
+        url="${this.tableUrl_.url}"
+        description="${this.tableUrl_.url}"
+        .imageUrls="${this.imageUrl ? [this.imageUrl?.url] : []}"
+        @click="${() => this.fire('comparison-table-list-item-click', {
+          uuid: this.uuid,
+        })}">
+      <div id="numItems" slot="badges">${this.numItemsString_}</div>
+    </cr-url-list-item>
+  </div>`;
+  // clang-format on
+}
