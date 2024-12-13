177d2a0f5a7adb6372f7297e04373020a4700236
Austin Sullivan <asully@chromium.org>
2024-12-13 10:20:39 -0800

    webnn: Assert readTensor reads the correct number of bytes
    A successful WebNNTensor::ReadTensor mojo call will always return a
    buffer whose size exactly matches the size of the tensor. If a buffer
    of a different size is returned, it indicates a logic error on the
    //services side
    
    Change-Id: I88cf2232df56d878db3362e478d89f979c7cc09e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092914
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396051}

--- third_party/blink/renderer/modules/ml/webnn/ml_tensor.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_tensor.cc
@@ -174,6 +174,9 @@ void MLTensor::OnDidReadTensor(
         read_tensor_error.message);
     return;
   }
+
+  CHECK_EQ(result->get_buffer().size(), descriptor_.PackedByteLength());
+
   resolver->Resolve(DOMArrayBuffer::Create(result->get_buffer()));
 
   RecordReadTensorTime(std::move(read_tensor_timer));
@@ -200,6 +203,8 @@ void MLTensor::OnDidReadTensorByob(
     resolver->RejectWithTypeError("Buffer was detached.");
     return;
   }
+
+  CHECK_EQ(result->get_buffer().size(), descriptor_.PackedByteLength());
 
   // It is safe to write into `dst_data` even though it was not transferred
   // because this method is called in a task which runs on same thread where
