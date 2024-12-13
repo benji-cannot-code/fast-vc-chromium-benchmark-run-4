62e37db38aaceb5117a6cd6bec275f3039786556
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-12-13 06:06:01 -0800

    [iOS] Youtube incognito - UI Sheet configuration
    The aim of this CL is to configure the youtube incognito sheet
    
    --Title animation demo: https://drive.google.com/file/d/1zFWk4yclW_I591u_Gc_oo5_FLzjKnp4R/view?usp=sharing&resourcekey=0-4tKtmzRGx9JucFHT2U7w6g
    
    --Landscape view: https://drive.google.com/file/d/1LUyK4AihkgMjpZSP-_SvYrnMvF93iuma/view?usp=sharing&resourcekey=0-xET0FKbF-wawxNK8oU928g
    
    Bug: 374934559
    Change-Id: I9ac3f2b4ad6aff46f5a1637257ee9a50ba145869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054217
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395918}

--- ios/chrome/browser/shared/ui/symbols/symbol_configurations.mm
+++ ios/chrome/browser/shared/ui/symbols/symbol_configurations.mm
@@ -22,6 +22,10 @@ const CGFloat kSettingsRootSymbolImagePointSize = 18;
 
 const CGFloat kCloudSlashSymbolPointSize = 20;
 
+NSString* const kLargeIncognitoBackgroundColor = kGrey700Color;
+
+NSString* const kLargeIncognitoForegroundColor = kGrey100Color;
+
 NSArray<UIColor*>* SmallIncognitoPalette() {
   return @[
     [UIColor colorNamed:kGrey400Color], [UIColor colorNamed:kGrey100Color]
@@ -30,7 +34,8 @@ NSArray<UIColor*>* SmallIncognitoPalette() {
 
 NSArray<UIColor*>* LargeIncognitoPalette() {
   return @[
-    [UIColor colorNamed:kGrey100Color], [UIColor colorNamed:kGrey700Color]
+    [UIColor colorNamed:kLargeIncognitoForegroundColor],
+    [UIColor colorNamed:kLargeIncognitoBackgroundColor]
   ];
 }
 
