99703c0cfc74cc5f675cd8b7d6d61edb649e5df2
Paul Adedeji <pauladedeji@google.com>
2024-12-13 12:56:44 -0800

    [ntp promo] Tidy up test suites for both promos.
    This CL tries to streamline parts of these test suites that
    decrease their readability by lowering code duplication and
    adding comments describing intended behavior.
    
    Change-Id: I41f5b2b745874ba87c77626a7384d40530d556f8
    Bug: 383068576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090055
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396155}

--- chrome/test/data/webui/new_tab_page/mobile_promo_test.ts
+++ chrome/test/data/webui/new_tab_page/mobile_promo_test.ts
@@ -19,6 +19,7 @@ import {installMock} from './test_support.js';
 
 suite('MobilePromoTest', () => {
   let newTabPageHandler: TestMock<PageHandlerRemote>;
+  let mobilePromo: MobilePromoElement;
 
   setup(() => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
@@ -28,8 +29,7 @@ suite('MobilePromoTest', () => {
     void newTabPageHandler;
   });
 
-  async function createMobilePromo(hasQrCode: boolean):
-      Promise<MobilePromoElement> {
+  async function createMobilePromo(hasQrCode: boolean) {
     if (hasQrCode) {
       newTabPageHandler.setResultFor(
           'getMobilePromoQrCode', Promise.resolve({qrCode: 'abc'}));
@@ -37,13 +37,23 @@ suite('MobilePromoTest', () => {
       newTabPageHandler.setResultFor(
           'getMobilePromoQrCode', Promise.resolve({qrCode: ''}));
     }
-    const mobilePromo = document.createElement('ntp-mobile-promo');
+    mobilePromo = document.createElement('ntp-mobile-promo');
     document.body.appendChild(mobilePromo);
 
     await eventToPromise('qr-code-changed', mobilePromo);
     await microtasksFinished();
+  }
 
-    return mobilePromo;
+  async function createMobilePromoWithQrCode() {
+    await createMobilePromo(/*hasQrCode=*/ true);
+  }
+
+  async function clickDismissPromoButton(mobilePromo: MobilePromoElement) {
+    const parts = mobilePromo.$.titleAndDismissContainer.children;
+    assertEquals(2, parts.length);
+    const dismissPromoButton = parts[1] as HTMLElement;
+    dismissPromoButton.click();
+    await microtasksFinished();
   }
 
   function assertHasQrCode(
@@ -62,57 +72,39 @@ suite('MobilePromoTest', () => {
 
   test('render hasQrCode=true', async () => {
     const hasQrCode = true;
-    const mobilePromo = await createMobilePromo(hasQrCode);
+    await createMobilePromo(hasQrCode);
     assertHasQrCode(hasQrCode, mobilePromo);
   });
 
   test('render hasQrCode=false', async () => {
     const hasQrCode = false;
-    const mobilePromo = await createMobilePromo(hasQrCode);
+    await createMobilePromo(hasQrCode);
     assertHasQrCode(hasQrCode, mobilePromo);
   });
 
   test('mobile promo dismisses when dismiss is clicked', async () => {
-    newTabPageHandler.setResultFor(
-        'getMobilePromoQrCode', Promise.resolve({qrCode: 'abc'}));
-    const mobilePromo = await createMobilePromo(true);
+    await createMobilePromoWithQrCode();
     assertTrue(isVisible(mobilePromo.$.promoContainer));
     assertFalse(mobilePromo.$.dismissPromoButtonToast.open);
 
-    const parts = mobilePromo.$.titleAndDismissContainer.children;
-    assertEquals(2, parts.length);
-
-    const dismissPromoButton = parts[1] as HTMLElement;
-    dismissPromoButton.click();
-
-    await microtasksFinished();
+    await clickDismissPromoButton(mobilePromo);
 
     assertFalse(isVisible(mobilePromo.$.promoContainer));
     assertTrue(mobilePromo.$.dismissPromoButtonToast.open);
     assertEquals(1, newTabPageHandler.getCallCount('onDismissMobilePromo'));
   });
 
-  test('mobile promo dismissed can undo', async () => {
-    newTabPageHandler.setResultFor(
-        'getMobilePromoQrCode', Promise.resolve({qrCode: 'abc'}));
-    const mobilePromo = await (createMobilePromo(true));
-    assertTrue(isVisible(mobilePromo.$.promoContainer));
+  test('mobile promo dismissal can be undone', async () => {
+    // Dismiss mobile promo.
+    await createMobilePromoWithQrCode();
     assertFalse(mobilePromo.$.dismissPromoButtonToast.open);
-
-    const parts = mobilePromo.$.titleAndDismissContainer.children;
-    assertEquals(2, parts.length);
-
-    const dismissPromoButton = parts[1] as HTMLElement;
-    dismissPromoButton.click();
-
-    await microtasksFinished();
-
+    await clickDismissPromoButton(mobilePromo);
     assertFalse(isVisible(mobilePromo.$.promoContainer));
     assertTrue(mobilePromo.$.dismissPromoButtonToast.open);
-    assertEquals(1, newTabPageHandler.getCallCount('onDismissMobilePromo'));
+    assertEquals(0, newTabPageHandler.getCallCount('onUndoDismissMobilePromo'));
 
+    // Undo dismissal via toast.
     mobilePromo.$.undoDismissPromoButton.click();
-
     await microtasksFinished();
 
     assertTrue(isVisible(mobilePromo.$.promoContainer));
@@ -121,17 +113,11 @@ suite('MobilePromoTest', () => {
   });
 
   test('restores promo if undo command is fired via keyboard', async () => {
-    newTabPageHandler.setResultFor(
-        'getMobilePromoQrCode', Promise.resolve({qrCode: 'abc'}));
-    const mobilePromo = await (createMobilePromo(true));
-
-    const parts = mobilePromo.$.titleAndDismissContainer.children;
-    assertEquals(2, parts.length);
-    const dismissPromoButton = parts[1] as HTMLElement;
-    dismissPromoButton.click();
-    await microtasksFinished();
-
+    // Dismiss mobile promo.
+    await createMobilePromoWithQrCode();
+    await clickDismissPromoButton(mobilePromo);
     assertEquals(0, newTabPageHandler.getCallCount('onUndoDismissMobilePromo'));
+
     // Simulate 'ctrl+z' key combo (or meta+z for Mac).
     keyDownOn(document.documentElement, 0, isMac ? 'meta' : 'ctrl', 'z');
     await microtasksFinished();
@@ -142,9 +128,7 @@ suite('MobilePromoTest', () => {
   });
 
   test('ignores undo command if no promo was blocklisted', async () => {
-    newTabPageHandler.setResultFor(
-        'getMobilePromoQrCode', Promise.resolve({qrCode: 'abc'}));
-    await (createMobilePromo(true));
+    await createMobilePromoWithQrCode();
 
     // Simulate 'ctrl+z' key combo (or meta+z for Mac).
     keyDownOn(document.documentElement, 0, isMac ? 'meta' : 'ctrl', 'z');
