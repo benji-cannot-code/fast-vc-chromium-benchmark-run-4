59edf97618fca4a077e87129dc6281f5168eb41f
mlcui <mlcui@google.com>
2024-12-13 00:18:45 -0800

    scanner: Add fake strings to feedback WebUI
    These will be replaced with finalised, localised strings at a later
    date.
    
    Bug: b:379974095
    Change-Id: I6a6a210004bb8659eabf16e90ea688b3545078cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073207
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395807}

--- ash/webui/scanner_feedback_ui/resources/app.ts
+++ ash/webui/scanner_feedback_ui/resources/app.ts
@@ -5,9 +5,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '//resources/cros_components/orca_feedback/orca-feedback.js';
 
+import type {OrcaFeedback} from '//resources/cros_components/orca_feedback/orca-feedback.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './app.html.js';
+
+// `StringSource` is not exported from orca-feedback.js, so get a reference to
+// it via `OrcaFeedback`.
+type StringSource = NonNullable<OrcaFeedback['stringSource']>;
+
+const STRING_SOURCE: StringSource = {
+  getAriaLabelForLink(linkText: string): string {
+    return `ARIA LABEL FOR ${linkText}`;
+  },
+  MSG_FEEDBACK_TITLE: 'FEEDBACK TITLE',
+  MSG_FEEDBACK_SUBTITLE: 'FEEDBACK SUBTITLE',
+  MSG_FEEDBACK_QUESTION: 'FEEDBACK QUESTION',
+  MSG_FEEDBACK_QUESTION_PLACEHOLDER: 'FEEDBACK QUESTION PLACEHOLDER',
+  MSG_OFFENSIVE_OR_UNSAFE: 'OFFENSIVE OR UNSAFE',
+  MSG_FACTUALLY_INCORRECT: 'FACTUALLY INCORRECT',
+  MSG_LEGAL_ISSUE: 'LEGAL ISSUE',
+  getFeedbackDisclaimer(
+      linkPolicyBegin: string, linkPolicyEnd: string,
+      linkTermsOfServiceBegin: string, linkTermsOfServiceEnd: string): string {
+    return `FEEDBACK DISCLAIMER ${linkPolicyBegin}POLICY${linkPolicyEnd} ${
+        linkTermsOfServiceBegin}TERMS OF SERVICE${linkTermsOfServiceEnd}`;
+  },
+  MSG_PRIVACY_POLICY: 'PRIVACY POLICY',
+  MSG_TERMS_OF_SERVICE: 'TERMS OF SERVICE',
+  MSG_CANCEL: 'CANCEL',
+  MSG_SEND: 'SEND'
+};
 
 export class ScannerFeedbackAppElement extends PolymerElement {
   static get is() {
@@ -17,6 +45,15 @@ export class ScannerFeedbackAppElement extends PolymerElement {
   static get template() {
     return getTemplate();
   }
+
+  static get properties() {
+    return {
+      // Private properties:
+      stringSource: Object,
+    };
+  }
+
+  private readonly stringSource = STRING_SOURCE;
 }
 
 declare global {
