cc03bc67f9bc72fcf9a274e9cb00ba9f0a73611b
Takuto Ikuta <tikuta@chromium.org>
2024-12-12 19:31:36 -0800

    extensions: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40440396
    Change-Id: I86e0006e8276e0488d820cbb5646bff13623e7cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090673
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395748}

--- extensions/common/stack_frame.h
+++ extensions/common/stack_frame.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_COMMON_STACK_FRAME_H_
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include <memory>
 #include <string>
