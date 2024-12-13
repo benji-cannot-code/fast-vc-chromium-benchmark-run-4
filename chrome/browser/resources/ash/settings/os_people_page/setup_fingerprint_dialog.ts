fb9f6271e640c4780a09764c5834c110c615efd1
Istvan Nagy <iscsi@google.com>
2024-12-13 02:52:40 -0800

    Fix setup fingerprint dialog behaviour on refresh.
    Handle the fingerprint enrollment dialog reload (e.g. CTRL+R).
    
    Bug: b:376917869, b:381410414
    Change-Id: I1e742b352206c70451e352040639f06ad97a7ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6058389
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395855}

--- chrome/browser/resources/ash/settings/os_people_page/setup_fingerprint_dialog.ts
+++ chrome/browser/resources/ash/settings/os_people_page/setup_fingerprint_dialog.ts
@@ -132,6 +132,8 @@ export class SettingsSetupFingerprintDialogElement extends
     this.addWebUiListener(
         'on-fingerprint-scan-received', this.onScanReceived_.bind(this));
     this.addWebUiListener('on-screen-locked', this.onScreenLocked_.bind(this));
+    window.addEventListener('beforeunload', () => this.cancelCurrentEnroll());
+
     this.$.arc.reset();
     this.browserProxy_.startEnroll(this.authToken);
     this.$.dialog.showModal();
@@ -139,6 +141,7 @@ export class SettingsSetupFingerprintDialogElement extends
 
   override disconnectedCallback(): void {
     this.cancelCurrentEnroll();
+    this.closeDialog();
   }
 
   private cancelCurrentEnroll(): void {
