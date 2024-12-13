b34be43ebc9f1ba6ba70cb4d7289ea25e9b6a8e5
Rune Lillesveen <futhark@chromium.org>
2024-12-13 03:55:47 -0800

    Add a feature for disabling getComputedStyle outside flat tree
    Bug: 40571175
    Change-Id: Ie8d12f5549b19f5ba9090ee8dbc9472afb6568be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088788
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395872}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -7851,6 +7851,9 @@ const ComputedStyle* Element::EnsureComputedStyle(
       (filter_root &&
        !filter_root->ComputedStyleRef().IsEnsuredOutsideFlatTree());
   if (!is_in_flat_tree) {
+    if (!RuntimeEnabledFeatures::GetComputedStyleOutsideFlatTreeEnabled()) {
+      return nullptr;
+    }
     filter_root = nullptr;
   }
 
