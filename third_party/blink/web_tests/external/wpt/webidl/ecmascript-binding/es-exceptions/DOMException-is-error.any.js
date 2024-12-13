8aa580b994ce9db520714db57706bab621207514
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-12-13 03:16:45 -0800

    Import wpt@09c6697ce28e621d192ef78f324dc2f05454412c
    https://github.com/web-platform-tests/wpt/compare/d91cef737...09c6697ce
    
    Using wpt-import in Chromium de6fc1bf82cdbdacdc3f51787cd9b618d97ab5ac.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org:
      external/wpt/infrastructure
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I28e40aec1c7e39e7db7e474095f99e849f713cd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090504
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1395860}

---
+++ third_party/blink/web_tests/external/wpt/webidl/ecmascript-binding/es-exceptions/DOMException-is-error.any.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,shadowrealm
+
+'use strict';
+
+test(function() {
+  // https://github.com/tc39/proposal-is-error/issues/9
+  // https://github.com/whatwg/webidl/pull/1421
+  assert_true(Error.isError(new DOMException()));
+});
