f75d23826eb6813ebc4487e28666f4adc2dc7ef8
Andreea Costinas <acostinas@google.com>
2024-12-13 07:13:36 -0800

    Revert "[iOS] Youtube incognito - UI Sheet configuration"
    This reverts commit 0e962d81925371f27fbf16230d8093beb46030d1.
    
    Reason for revert: Causes check_static_initializers failures. See bug 384019592.
    
    Original change's description:
    > [iOS] Youtube incognito - UI Sheet configuration
    >
    > The aim of this CL is to configure the youtube incognito sheet
    >
    > --Title animation demo: https://drive.google.com/file/d/1zFWk4yclW_I591u_Gc_oo5_FLzjKnp4R/view?usp=sharing&resourcekey=0-4tKtmzRGx9JucFHT2U7w6g
    >
    > --Landscape view: https://drive.google.com/file/d/1LUyK4AihkgMjpZSP-_SvYrnMvF93iuma/view?usp=sharing&resourcekey=0-xET0FKbF-wawxNK8oU928g
    >
    > Bug: 374934559
    > Change-Id: I9ac3f2b4ad6aff46f5a1637257ee9a50ba145869
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054217
    > Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1395918}
    
    Bug: 384019592
    Change-Id: I073dfb5c872bf91958b943430ba7fce2c162cd6d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088882
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Andreea Costinas <acostinas@google.com>
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Auto-Submit: Andreea Costinas <acostinas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395946}

--- ios/chrome/browser/shared/ui/symbols/symbol_configurations.mm
+++ ios/chrome/browser/shared/ui/symbols/symbol_configurations.mm
@@ -22,10 +22,6 @@ const CGFloat kSettingsRootSymbolImagePointSize = 18;
 
 const CGFloat kCloudSlashSymbolPointSize = 20;
 
-NSString* const kLargeIncognitoBackgroundColor = kGrey700Color;
-
-NSString* const kLargeIncognitoForegroundColor = kGrey100Color;
-
 NSArray<UIColor*>* SmallIncognitoPalette() {
   return @[
     [UIColor colorNamed:kGrey400Color], [UIColor colorNamed:kGrey100Color]
@@ -34,8 +30,7 @@ NSArray<UIColor*>* SmallIncognitoPalette() {
 
 NSArray<UIColor*>* LargeIncognitoPalette() {
   return @[
-    [UIColor colorNamed:kLargeIncognitoForegroundColor],
-    [UIColor colorNamed:kLargeIncognitoBackgroundColor]
+    [UIColor colorNamed:kGrey100Color], [UIColor colorNamed:kGrey700Color]
   ];
 }
 
