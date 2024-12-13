044fc4a05c3f05c97203de113a7c2fa360c4b19a
Colin Blundell <blundell@chromium.org>
2024-12-13 07:57:05 -0800

    [//cc] Rename VizLayers base::Feature to TreesInViz
    Provide added clarity as to what this base::Feature is controlling.
    
    Bug: 369883417
    Change-Id: I605ef6a2eeed289a8c0009e7c536c733e8734e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6087932
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395970}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -156,7 +156,7 @@ const base::FeatureParam<std::string> kScrollEventDispatchMode(
     "mode",
     kScrollEventDispatchModeDispatchScrollEventsImmediately);
 
-BASE_FEATURE(kVizLayers, "VizLayers", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kTreesInViz, "TreesInViz", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSendExplicitDecodeRequestsImmediately,
              "SendExplicitDecodeRequestsImmediately",
