ecd5110eaf28402543ca0dc78435f18665a28e76
Eliot Courtney <edcourtney@google.com>
2024-12-12 19:00:07 -0800

    Fix UserMediaScreenCapturing flag.
    There are two conflicting definitions of UserMediaScreenCapturing, one
    of which is not used.
    
    This CL deletes the unused one and replicates the changes in
    crrev.com/c/6055844 to the correct flag.
    
    Bug: b/352186941
    Test: CQ
    Change-Id: I727b8299402f8055f4d1e74ed98edd3ef3cc86e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088298
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395737}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -68,7 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/visited_url_ranking/public/features.h"
 #include "components/viz/common/features.h"
 #include "components/webapps/browser/features.h"
-#include "content/public/common/buildflags.h"
 #include "content/public/common/content_features.h"
 #include "media/base/media_switches.h"
 #include "services/device/public/cpp/device_features.h"
@@ -1088,15 +1087,6 @@ BASE_FEATURE(kUpdateCompositorForSurfaceControl,
 BASE_FEATURE(kUseLibunwindstackNativeUnwinderAndroid,
              "UseLibunwindstackNativeUnwinderAndroid",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kUserMediaScreenCapturing,
-             "UserMediaScreenCapturing",
-#if BUILDFLAG(ENABLE_SCREEN_CAPTURE)
-             base::FEATURE_ENABLED_BY_DEFAULT
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-);
 
 BASE_FEATURE(kVoiceSearchAudioCapturePolicy,
              "VoiceSearchAudioCapturePolicy",
