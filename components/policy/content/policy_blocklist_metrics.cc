60d7adc5ee59f8f431abdfb2d9e143dd272de6a0
Takashi Toyoshima <toyoshim@chromium.org>
2024-12-13 09:26:51 -0800

    Change PolicyBlocklistMetrics to use a void* user data key
    Any unique number can be used for the key, but it seems using
    a pointer that points itself is a well-known best practice.
    
    Change-Id: Iaec41ee4105d4e96e63ee9e2c1c00a9781861c86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075419
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396014}

--- components/policy/content/policy_blocklist_metrics.cc
+++ components/policy/content/policy_blocklist_metrics.cc
@@ -10,17 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 
 namespace {
-constexpr char kUserDataKey[] = "PolicyBlocklistMetrics";
+const void* const kPolicyBlocklistMetricsDataKey = &kPolicyBlocklistMetricsDataKey;
 }
 
 // static
 PolicyBlocklistMetrics* PolicyBlocklistMetrics::Get(
     content::NavigationHandle& handle) {
-  return static_cast<PolicyBlocklistMetrics*>(handle.GetUserData(kUserDataKey));
+  return static_cast<PolicyBlocklistMetrics*>(
+      handle.GetUserData(kPolicyBlocklistMetricsDataKey));
 }
 
 // static
 void PolicyBlocklistMetrics::Create(content::NavigationHandle& handle) {
-  handle.SetUserData(kUserDataKey,
+  handle.SetUserData(kPolicyBlocklistMetricsDataKey,
                      base::WrapUnique(new PolicyBlocklistMetrics()));
 }
