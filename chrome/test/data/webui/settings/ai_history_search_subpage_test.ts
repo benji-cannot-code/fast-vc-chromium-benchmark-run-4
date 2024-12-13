8ae50ac60b25a6da1f5336fa40a25d115d4f2892
Zaina Al-Mashni <zalmashni@google.com>
2024-12-13 15:34:28 -0800

    [Synpase] Update strings and icons for subpages
    History Search: https://screenshot.googleplex.com/BUV4bUEUZh3gPdc
    History Search with Answers:
    Tab Organizer: https://screenshot.googleplex.com/BXJciafQ7Zt4gA7
    
    https: //screenshot.googleplex.com/559oMTfqmtVs4Ci
    Compare: https://screenshot.googleplex.com/4xnpJd4SbeANA9J
    Compose: https://screenshot.googleplex.com/3bgRhRNKUtTTtLu
    Bug: 362225975
    Change-Id: Iadb9a6b41e687cbeaf482535ea900b81b4d79a3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091594
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396243}

--- chrome/test/data/webui/settings/ai_history_search_subpage_test.ts
+++ chrome/test/data/webui/settings/ai_history_search_subpage_test.ts
@@ -138,7 +138,8 @@ suite('HistorySearchSubpage', function() {
     assertFalse(checkVisibility('#linkoutTextWithAnswers'));
     assertTrue(checkVisibility('#whenOnPageContentText'));
     assertFalse(checkVisibility('#whenOnPageContentTextWithAnswers'));
-    assertFalse(checkVisibility('#whenOnLogStartItem'));
+    assertFalse(checkVisibility('#whenOnRecallInfoWithAnswers'));
+    assertTrue(checkVisibility('#whenOnLogStartItem'));
     assertTrue(checkVisibility('#considerDataEncryptedText'));
     assertFalse(checkVisibility('#considerDataEncryptedTextWithAnswers'));
     assertFalse(checkVisibility('#considerOutDatedItem'));
@@ -149,6 +150,7 @@ suite('HistorySearchSubpage', function() {
     assertTrue(checkVisibility('#linkoutTextWithAnswers'));
     assertFalse(checkVisibility('#whenOnPageContentText'));
     assertTrue(checkVisibility('#whenOnPageContentTextWithAnswers'));
+    assertTrue(checkVisibility('#whenOnRecallInfoWithAnswers'));
     assertTrue(checkVisibility('#whenOnLogStartItem'));
     assertFalse(checkVisibility('#considerDataEncryptedText'));
     assertTrue(checkVisibility('#considerDataEncryptedTextWithAnswers'));
