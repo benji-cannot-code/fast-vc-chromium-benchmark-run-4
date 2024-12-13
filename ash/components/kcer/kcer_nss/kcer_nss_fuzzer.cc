30456aa60abd5a5cad63acfbe96dbf77a4cdcdff
Michael Ershov <miersh@google.com>
2024-12-13 02:01:34 -0800

    test/cert_builder: Allow invalid URLs
    Allow passing invalid URLs into the test CertBuilder for a wider
    range of possible test scenarios. At the moment the builder fails
    with "NOTREACHED hit" if the passed URL is not "is_valid()".
    
    Add a new method that explicitly accepts arbitrary strings to make it
    independent of the GURL implementation and use it to fix kcer_nss_fuzzer.
    
    Fixed: https://issues.chromium.org/382526735
    Test: Run kcer_nss_fuzzer with the reproducer testcase from the bug
    Change-Id: Ic95108587a2723350524b3462c4fac2158168aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080375
    Auto-Submit: Michael Ershov <miersh@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395834}

--- ash/components/kcer/kcer_nss/kcer_nss_fuzzer.cc
+++ ash/components/kcer/kcer_nss/kcer_nss_fuzzer.cc
@@ -292,10 +292,6 @@ inline std::vector<uint8_t> CertGenerator::GetBytes() {
   return data_provider_->ConsumeBytes<uint8_t>(length);
 }
 
-inline GURL CertGenerator::GetGurl() {
-  return GURL(data_provider_->ConsumeRandomLengthString());
-}
-
 inline net::IPAddress CertGenerator::GetIpAddress() {
   bool use_ip4 = GetBool();
   if (use_ip4) {
@@ -418,13 +414,13 @@ void CertGenerator::GenerateCert() {
                                               excluded_dns_names);
   }
   if (GetBool()) {
-    std::vector<GURL> ca_issuers_urls;
+    std::vector<std::string> ca_issuers_urls;
     while (GetBool()) {
-      ca_issuers_urls.push_back(GetGurl());
+      ca_issuers_urls.push_back(GetString());
     }
-    std::vector<GURL> ocsp_urls;
+    std::vector<std::string> ocsp_urls;
     while (GetBool()) {
-      ocsp_urls.push_back(GetGurl());
+      ocsp_urls.push_back(GetString());
     }
     cert_builder_->SetCaIssuersAndOCSPUrls(ca_issuers_urls, ocsp_urls);
   }
