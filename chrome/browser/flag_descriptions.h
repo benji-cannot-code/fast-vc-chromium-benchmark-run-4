3431ceac558a77076f2c0269d98a12703fdc1af7
Brandon Fong <bjfong@google.com>
2024-12-13 10:43:41 -0800

    [declutter] Expose the archive duplicate tabs flag on about flags
    Expose the archive duplicate tabs flag on about flags.
    
    Bug: 383870027
    Change-Id: I05dde5d527983ef3762320184409b9db94041920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092293
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396065}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2504,6 +2504,9 @@ extern const char kAndroidTabDeclutterDescription[];
 extern const char kAndroidTabDeclutterArchiveAllButActiveTabName[];
 extern const char kAndroidTabDeclutterArchiveAllButActiveTabDescription[];
 
+extern const char kAndroidTabDeclutterArchiveDuplicateTabsName[];
+extern const char kAndroidTabDeclutterArchiveDuplicateTabsDescription[];
+
 extern const char kAppSpecificHistoryName[];
 extern const char kAppSpecificHistoryDescription[];
 
