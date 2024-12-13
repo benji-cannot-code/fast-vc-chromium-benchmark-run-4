58a664cebebaca152f9e0b532912f9007d5e1717
François Degros <fdegros@chromium.org>
2024-12-12 21:27:57 -0800

    Improve StorageHandler::IsEligibleForAndroidStorage()
    Ensure that mounted archives and network drives are not eligible for
    Android storage.
    
    BUG=b:271805482
    TEST=unit_tests --gtest_filter=StorageHandlerTest.*
    
    Change-Id: I5b59db5b9e20679a83368f24018a42b41077a1ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090379
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Momoko Hattori <momohatt@chromium.org>
    Commit-Queue: Momoko Hattori <momohatt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395764}

--- chrome/browser/ui/webui/ash/settings/pages/storage/device_storage_handler.h
+++ chrome/browser/ui/webui/ash/settings/pages/storage/device_storage_handler.h
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/session/arc_session_manager_observer.h"
 #include "chrome/browser/ui/webui/ash/settings/calculator/size_calculator.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
+#include "chromeos/ash/components/dbus/cros_disks/cros_disks_client.h"
 #include "chromeos/ash/components/disks/disk_mount_manager.h"
-#include "third_party/re2/src/re2/re2.h"
 
 class Profile;
 
@@ -100,9 +100,9 @@ class StorageHandler : public ::settings::SettingsPageUIHandler,
   // Updates list of external storages.
   void UpdateExternalStorages();
 
-  // Returns true if the volume from |source_path| can be used as Android
+  // Returns true if the volume from |mount_point| can be used as Android
   // storage.
-  bool IsEligibleForAndroidStorage(std::string source_path);
+  bool IsEligibleForAndroidStorage(const MountPoint& mount_point);
 
   // Update encryption type whenever it is fetched.
   void OnGetVaultProperties(
@@ -131,7 +131,6 @@ class StorageHandler : public ::settings::SettingsPageUIHandler,
   base::ScopedObservation<arc::ArcSessionManager,
                           arc::ArcSessionManagerObserver>
       arc_observation_{this};
-  const re2::RE2 special_volume_path_pattern_;
 
   base::WeakPtrFactory<StorageHandler> weak_ptr_factory_{this};
 };
