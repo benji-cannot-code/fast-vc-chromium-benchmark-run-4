d8a1ba92484b86651585d0240fabd6c389896f12
Jeremie Boulic <jboulic@chromium.org>
2024-12-12 22:58:29 -0800

    [Files Trash] Add UMAs for auto cleanup
    Bug: b:328659479
    Change-Id: Idf049d7d404476a28b927e459dc8b6aeb8b0c529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090685
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395779}

--- chrome/browser/ash/file_manager/trash_auto_cleanup.cc
+++ chrome/browser/ash/file_manager/trash_auto_cleanup.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/barrier_callback.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
+#include "base/metrics/histogram_functions_internal_overloads.h"
 #include "base/system/sys_info.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
@@ -16,6 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace file_manager::trash {
 
 namespace {
+
+constexpr char kCleanupFileCountMetricName[] =
+    "FileBrowser.TrashAutoCleanup.FileCount";
+constexpr char kCleanupErrorsMetricName[] =
+    "FileBrowser.TrashAutoCleanup.Errors";
+constexpr char kCleanupTimeMetricName[] = "FileBrowser.TrashAutoCleanup.Time";
 
 // Enumerates Trash info files (supported .Trash/info/ locations) and returns
 // the list of trashinfo files corresponding to the trash entries to delete.
@@ -36,11 +43,16 @@ std::vector<base::FilePath> GetTrashInfoFilesToDeleteOnBlockingThread(
                       base::File::FLAG_OPEN | base::File::FLAG_READ);
       if (!file.IsValid()) {
         ++invalid_file_counter;
+        base::UmaHistogramEnumeration(kCleanupErrorsMetricName,
+                                      AutoCleanupError::kInvalidTrashInfoFile);
         continue;
       }
       base::File::Info info;
       if (!file.GetInfo(&info)) {
         ++file_get_info_failed_counter;
+        base::UmaHistogramEnumeration(
+            kCleanupErrorsMetricName,
+            AutoCleanupError::kFailedToGetTrashInfoFileModifiedTime);
         continue;
       }
       if (now - info.last_modified >= kMaxTrashAge) {
@@ -61,6 +73,8 @@ std::vector<base::FilePath> GetTrashInfoFilesToDeleteOnBlockingThread(
     LOG(ERROR) << "Could not get info from " << file_get_info_failed_counter
                << " trashinfo files";
   }
+  base::UmaHistogramCounts1000(kCleanupFileCountMetricName,
+                               trash_info_paths_to_delete.size());
   return trash_info_paths_to_delete;
 }
 
@@ -70,7 +84,12 @@ bool DeleteOldTrashFilesOnBlockingThread(
   for (const ParsedTrashInfoData& trash_info_data : to_delete) {
     if (!base::DeleteFile(trash_info_data.trashed_file_path) ||
         !base::DeleteFile(trash_info_data.trash_info_path)) {
+      base::UmaHistogramEnumeration(kCleanupErrorsMetricName,
+                                    AutoCleanupError::kFailedToDeleteTrashFile);
       success = false;
+    } else {
+      base::UmaHistogramEnumeration(kCleanupErrorsMetricName,
+                                    AutoCleanupError::kSuccessfullyDeleted);
     }
   }
   return success;
@@ -103,9 +122,10 @@ std::unique_ptr<TrashAutoCleanup> TrashAutoCleanup::Create(Profile* profile) {
 }
 
 void TrashAutoCleanup::Init() {
-  cleanup_timer_.Start(FROM_HERE, kCleanupCheckInterval,
-                       base::BindRepeating(&TrashAutoCleanup::StartCleanup,
-                                           weak_ptr_factory_.GetWeakPtr()));
+  cleanup_repeating_timer_.Start(
+      FROM_HERE, kCleanupCheckInterval,
+      base::BindRepeating(&TrashAutoCleanup::StartCleanup,
+                          weak_ptr_factory_.GetWeakPtr()));
 }
 
 void TrashAutoCleanup::StartCleanup() {
@@ -124,6 +144,7 @@ void TrashAutoCleanup::StartCleanup() {
     return;
   }
   last_cleanup_time_ = base::Time::Now();
+  cleanup_start_time_ = base::TimeTicks::Now();
 
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE,
@@ -170,10 +191,9 @@ void TrashAutoCleanup::OnTrashInfoFilesParsed(
   for (auto& trash_info_data_or_error : parsed_data_or_error) {
     if (!trash_info_data_or_error.has_value()) {
       ++parse_error_counter;
-      if (cleanup_done_closure_for_test_) {
-        std::move(cleanup_done_closure_for_test_)
-            .Run(AutoCleanupResult::kTrashInfoParsingError);
-      }
+      base::UmaHistogramEnumeration(
+          kCleanupErrorsMetricName,
+          AutoCleanupError::kFailedToParseTrashInfoFile);
       continue;
     }
     to_delete.push_back(std::move(trash_info_data_or_error.value()));
@@ -181,6 +201,10 @@ void TrashAutoCleanup::OnTrashInfoFilesParsed(
   if (parse_error_counter) {
     LOG(ERROR) << "Failed to parse " << parse_error_counter
                << " trash info files";
+    if (cleanup_done_closure_for_test_) {
+      std::move(cleanup_done_closure_for_test_)
+          .Run(AutoCleanupResult::kTrashInfoParsingError);
+    }
   }
   if (to_delete.empty()) {
     return;
@@ -202,6 +226,8 @@ void TrashAutoCleanup::OnCleanupDone(bool success) {
                                          : AutoCleanupResult::kDeletionError;
     std::move(cleanup_done_closure_for_test_).Run(result);
   }
+  base::UmaHistogramTimes(kCleanupTimeMetricName,
+                          base::TimeTicks::Now() - cleanup_start_time_);
 }
 
 void TrashAutoCleanup::SetCleanupDoneCallbackForTest(
