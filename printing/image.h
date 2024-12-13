1f049138d73a5125478446afb58d6cd660fcd72e
Lei Zhang <thestig@chromium.org>
2024-12-13 09:45:33 -0800

    Fix some nits in //printing code
    Fix nits to prepare for allow_unsafe_buffers pragma removal in //printing:
    
    - Move Image::pixel_at() implementation into image.cc, to avoid having
      too much code in the header.
    - Move allow_unsafe_buffers pragma into image.cc as well.
    - Break up DCHECKs in pixel_at() to make it easier to see which
      condition failed.
    - Update DCHECKs to CHECKs in pixel_at().
    - Remove an unnecessary else keyword in MetafileSkia::SaveTo().
    - Move some pointer arithmetic code in emf_win.cc into separate
      functions.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE Just moving existing code around.
    Change-Id: Ib1e50b6bc872cc1747ff2bedd00d825aa24de775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092357
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396026}

--- printing/image.h
+++ printing/image.h
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef PRINTING_IMAGE_H_
 #define PRINTING_IMAGE_H_
 
@@ -16,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/check.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace printing {
@@ -42,13 +36,7 @@ class Image {
     return color & 0xFFFFFF;  // Strip out alpha channel.
   }
 
-  uint32_t pixel_at(int x, int y) const {
-    DCHECK(x >= 0 && x < size_.width());
-    DCHECK(y >= 0 && y < size_.height());
-    const uint32_t* data = reinterpret_cast<const uint32_t*>(&*data_.begin());
-    const uint32_t* data_row = data + y * row_length_ / sizeof(uint32_t);
-    return Color(data_row[x]);
-  }
+  uint32_t pixel_at(int x, int y) const;
 
  private:
   // Pixel dimensions of the image.
