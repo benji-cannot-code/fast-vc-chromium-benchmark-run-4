eaa4270709df59072a1bf0afdea33c04061290cd
kenoss <kenoss@chromium.org>
2024-12-13 00:23:49 -0800

    PrefetchNewWaitLoop: Don't set prefetch status on cookie change if already failure
    This CL prevents to set PrefetchStatus in `OnDetectedCookiesChange2()`
    if PrefetchStatus is already set as failure.
    
    For more details, see
    https://docs.google.com/document/d/1G48SaWbdOy1yNBT1wio2IHVuUtddF5VLFsT6BRSYPMI/edit?tab=t.hpkotaxo7tfh#heading=h.woaoy8erwx63
    
    Bug: 375333786
    Change-Id: I6e4aca6913af04f3dd9966be949734e596d672b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090757
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395810}

--- content/browser/preloading/prefetch/prefetch_match_resolver.cc
+++ content/browser/preloading/prefetch/prefetch_match_resolver.cc
@@ -239,7 +239,7 @@ void PrefetchMatchResolver2::FindPrefetchInternal(
       case PrefetchContainer::ServableState::kServable:
         if (candidate.second->prefetch_container->CreateReader()
                 .HaveDefaultContextCookiesChanged()) {
-          UnblockForCookiesChanged();
+          UnblockForCookiesChanged(candidate.second->prefetch_container->key());
           return;
         }
         break;
@@ -398,7 +398,7 @@ void PrefetchMatchResolver2::OnDeterminedHead(
   }
 
   if (prefetch_container.CreateReader().HaveDefaultContextCookiesChanged()) {
-    UnblockForCookiesChanged();
+    UnblockForCookiesChanged(prefetch_container.key());
     return;
   }
 
@@ -487,7 +487,8 @@ void PrefetchMatchResolver2::MaybeUnblockForUnmatch(
   // It still waits for other `PrefetchContainer`s.
 }
 
-void PrefetchMatchResolver2::UnblockForCookiesChanged() {
+void PrefetchMatchResolver2::UnblockForCookiesChanged(
+    const PrefetchContainer::Key& key) {
   // Unregister remaining candidates as not served, with calling
   // `PrefetchContainer::OnDetectedCookiesChange2()`.
   for (auto& prefetch_key : Keys(candidates_)) {
@@ -499,7 +500,9 @@ void PrefetchMatchResolver2::UnblockForCookiesChanged() {
 
     UnregisterCandidate(prefetch_key, /*is_served=*/false);
 
-    prefetch_container.OnDetectedCookiesChange2();
+    prefetch_container.OnDetectedCookiesChange2(
+        /*is_unblock_for_cookies_changed_triggered_by_this_prefetch_container*/
+        prefetch_key == key);
   }
 
   UnblockForNoCandidates();
