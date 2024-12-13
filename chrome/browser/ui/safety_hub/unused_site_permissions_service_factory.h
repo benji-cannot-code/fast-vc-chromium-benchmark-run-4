645dde132133d3cb8cecd0a6dc55b025a47e32e9
Filipa Senra <fsenra@google.com>
2024-12-13 05:34:39 -0800

    [Desktop][SafetyHub] Create services on start up
    We want to create the SafetyHub services with proactive checks on start
    up so these checks can already be done for all users in the background
    without needing for users to navigate to the three dot menu.
    
    Fixed: 370775505
    Change-Id: I09d449b130036af7bcd72af989050bcacbcb6cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088380
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395907}

--- chrome/browser/ui/safety_hub/unused_site_permissions_service_factory.h
+++ chrome/browser/ui/safety_hub/unused_site_permissions_service_factory.h
@@ -42,9 +42,7 @@ class UnusedSitePermissionsServiceFactory : public ProfileKeyedServiceFactory {
   std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 
-#if BUILDFLAG(IS_ANDROID)
   bool ServiceIsCreatedWithBrowserContext() const override;
-#endif  // BUILDFLAG(ANDROID)
 };
 
 #endif  // CHROME_BROWSER_UI_SAFETY_HUB_UNUSED_SITE_PERMISSIONS_SERVICE_FACTORY_H_
