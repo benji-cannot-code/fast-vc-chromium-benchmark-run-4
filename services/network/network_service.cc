0834e0c62ec4fa855774b8746a8c0eb04980c4bd
Nathan Memmott <memmott@chromium.org>
2024-12-13 15:07:28 -0800

    Code Health: Clean up stale InhibitLoadingStateUpdate
    Fixed: 362247096
    Change-Id: I7576af9ae101f59b05f16c1845604a95ec90cbd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6082134
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396231}

--- services/network/network_service.cc
+++ services/network/network_service.cc
@@ -86,7 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/crash_keys.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/initiator_lock_compatibility.h"
-#include "services/network/public/cpp/load_info_util.h"
 #include "services/network/public/cpp/network_switches.h"
 #include "services/network/public/cpp/parsed_headers.h"
 #include "services/network/public/mojom/key_pinning.mojom.h"
