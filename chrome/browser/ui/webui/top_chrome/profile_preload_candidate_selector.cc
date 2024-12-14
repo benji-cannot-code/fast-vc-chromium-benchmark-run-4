2554dd096f33990a934d867ba46dcfcdbb1d825e
Keren Zhu <kerenzhu@chromium.org>
2024-12-13 18:14:16 -0800

    Ignore disabled WebUI when choosing profile preload candidate
    When a WebUI is disabled, we should not include it in the preload
    candidate list. This has caused crash because navigating to a
    disabled WebUI results in the page landed with an empty URL which
    does not have a corresponding WebUIConfig.
    
    Bug: 383937390, 381723414
    Change-Id: I2ae30c5258f15f880758555ae4c77f4574effe2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092607
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396300}

--- chrome/browser/ui/webui/top_chrome/profile_preload_candidate_selector.cc
+++ chrome/browser/ui/webui/top_chrome/profile_preload_candidate_selector.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/top_chrome/per_profile_webui_tracker.h"
+#include "chrome/browser/ui/webui/top_chrome/top_chrome_webui_config.h"
 #include "components/site_engagement/content/site_engagement_service.h"
 #include "url/gurl.h"
 
@@ -49,6 +50,13 @@ std::optional<GURL> ProfilePreloadCandidateSelector::GetURLToPreload(
   for (const GURL& url : preloadable_urls_) {
     // Skip URLs that are present under the profile.
     if (webui_tracker_->ProfileHasWebUI(profile, url.spec())) {
+      continue;
+    }
+
+    // Skip if this URL is disabled. This is seen in History Clusters side panel
+    // where some locales are not supported.
+    auto* config = TopChromeWebUIConfig::From(profile, url);
+    if (!config || !config->IsWebUIEnabled(profile)) {
       continue;
     }
 
