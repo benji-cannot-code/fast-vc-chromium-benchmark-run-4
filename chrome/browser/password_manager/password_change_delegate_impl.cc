41e2ef1377532dec7fbf13712cd87c8be3486c01
Anna Tsvirchkova <atsvirchkova@google.com>
2024-12-14 05:13:07 -0800

    Set and return correct states in PasswordChangeDelegate
    Bug: 381053884
    Change-Id: I4bdb5a4dc5a9ba45a1e18578e36f51b622c076be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088780
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396356}

--- chrome/browser/password_manager/password_change_delegate_impl.cc
+++ chrome/browser/password_manager/password_change_delegate_impl.cc
@@ -122,7 +122,7 @@ bool PasswordChangeDelegateImpl::IsPasswordChangeOngoing(
 
 PasswordChangeDelegate::State PasswordChangeDelegateImpl::GetCurrentState()
     const {
-  return State::kWaitingForChangePasswordForm;
+  return current_state_;
 }
 
 void PasswordChangeDelegateImpl::Stop() {
@@ -137,8 +137,7 @@ void PasswordChangeDelegateImpl::SuccessfulSubmissionDetected(
     // update.
     form_manager_->OnUpdateUsernameFromPrompt(username_);
     form_manager_->Save();
-    // TODO(crbug.com/375565171): Transition to a password successfully updated
-    // state.
+    UpdateState(State::kPasswordSuccessfullyChanged);
   }
 }
 
