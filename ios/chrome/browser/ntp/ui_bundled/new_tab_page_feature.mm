755a8fed29d37a79c351635f7616dfd4491c2c32
ginnnnnnny <ginnyhuang@chromium.org>
2024-12-16 10:05:16 -0800

    [iOS][follow-entry] Add feature flag for following entry points work
    This CL adds the initial feature flag for work on the new following feed
    entry points. Currently, only enabled/disabled groups are supported, but
    as I progress on creating treatment groups to tweak other UI components
    on NTP, parameters will be added and more variations will be included in
    about_flags.mm.
    
    Bug: 359325090
    Change-Id: Ibce5524709a98d5ea3390c22d944ce6d4228b5b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094267
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396822}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.mm
@@ -50,6 +50,10 @@ BASE_FEATURE(kIdentityDiscAccountMenu,
              "IdentityDiscAccountMenu",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kIOSNewFollowingFeedEntryPoints,
+             "IOSNewFollowingFeedEntryPoints",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 #pragma mark - Feature parameters
 
 const char kDiscoverFeedSRSReconstructedTemplatesEnabled[] =
@@ -100,4 +104,9 @@ bool IsSignedOutViewDemotionEnabled() {
 
 bool IsiPadFeedGhostCardsEnabled() {
   return base::FeatureList::IsEnabled(kEnableiPadFeedGhostCards);
+}
+
+bool IsNewFollowingFeedEntryPointsEnabled() {
+  return IsFollowUIUpdateEnabled() &&
+         base::FeatureList::IsEnabled(kIOSNewFollowingFeedEntryPoints);
 }
