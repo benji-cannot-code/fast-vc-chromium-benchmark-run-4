a2eec179f215385cd6595c6a6455a4dc1953be25
Mike Wittman <wittman@chromium.org>
2024-12-16 15:19:11 -0800

    [sampling profiler] Rationalize 32/64-bit Android unwinder configuration
    With the move of the libunwinstack library from the dynamic feature
    module into Chrome code, we now only need to depend on the DFM
    for the unwind resource, which is only used by the 32-bit Chrome
    unwinder. This CL effects that change and also restricts the remaining
    headers and logic to either 32-bit, 64-bit, or both, to reflect the
    actual architecture-specific dependencies.
    
    Bug: 380487894, 375463618
    Change-Id: I1b9571057e772286834ae7dcbfbe8c8538c650ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6087128
    Reviewed-by: Rodney Ding <rodneyding@google.com>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397038}

--- chrome/common/profiler/core_unwinders_android_unittest.cc
+++ chrome/common/profiler/core_unwinders_android_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/profiler/core_unwinders.h"
 
 #include "base/command_line.h"
+#include "base/debug/debugging_buildflags.h"
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/profiler/profiler_buildflags.h"
