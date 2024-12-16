a8f041f98c479bbeba08daf29c4c7111d37e8cd2
Takuto Ikuta <tikuta@chromium.org>
2024-12-15 23:42:10 -0800

    third_party/ipcz: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40263312
    Change-Id: I5a10b292da08c8a3a93f417beef270619224ca61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090604
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396594}

--- third_party/ipcz/src/reference_drivers/memfd_memory.h
+++ third_party/ipcz/src/reference_drivers/memfd_memory.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IPCZ_SRC_REFERENCE_DRIVERS_MEMORY_H_
 #define IPCZ_SRC_REFERENCE_DRIVERS_MEMORY_H_
 
+#include <stdint.h>
+
 #include "reference_drivers/file_descriptor.h"
 #include "third_party/abseil-cpp/absl/types/span.h"
 
