2d927375075a37aab7f8d3cba307cef24ac4a5e6
Darren Shen <shend@chromium.org>
2024-12-16 15:41:42 -0800

    Delete c/b/ash/input_method/field_trial
    Not used.
    
    Bug: b:245020074
    Change-Id: Ib161fcce3da7de33d0464d47802fda830bf00f0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091058
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397047}

--- chrome/browser/ash/input_method/autocorrect_manager.cc
+++ chrome/browser/ash/input_method/autocorrect_manager.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/assistive_window_properties.h"
 #include "chrome/browser/ash/input_method/autocorrect_enums.h"
 #include "chrome/browser/ash/input_method/autocorrect_prefs.h"
-#include "chrome/browser/ash/input_method/field_trial.h"
 #include "chrome/browser/ash/input_method/suggestion_enums.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
