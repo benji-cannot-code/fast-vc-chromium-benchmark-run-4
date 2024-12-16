84f5191698ccbe9a5f41aafd4787fea55a6bc46f
David Pennington <dpenning@chromium.org>
2024-12-16 11:18:15 -0800

    Update TabInterface naming to be more explicit about active state.
    The "Foregrounded" naming of the TabInterface isnt explicit about the
    state that the tab is in. In order to fix this, ive renamed to Active
    and also added a "Visible" state which will provide whether the
    webcontents is shown or not. currently they are the same, however
    this may change in the future and some features want to know if the
    content is displayed, not if its the active tab.
    
    Change-Id: I6ea3a279e99446b45b36e411b7b161a7fc0538b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092356
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1396864}

--- chrome/browser/enterprise/data_protection/data_protection_navigation_controller.cc
+++ chrome/browser/enterprise/data_protection/data_protection_navigation_controller.cc
@@ -50,7 +50,7 @@ DataProtectionNavigationController::DataProtectionNavigationController(
     return;
   }
   Observe(tab_interface->GetContents());
-  tab_subscriptions_.push_back(tab_interface_->RegisterDidEnterForeground(
+  tab_subscriptions_.push_back(tab_interface_->RegisterDidActivate(
       base::BindRepeating(&DataProtectionNavigationController::TabForegrounded,
                           weak_ptr_factory_.GetWeakPtr())));
   tab_subscriptions_.push_back(
@@ -117,7 +117,7 @@ void DataProtectionNavigationController::
   }
 
   // If the tab is in the background, do nothing.
-  if (!tab_interface_->IsInForeground()) {
+  if (!tab_interface_->IsActivated()) {
     return;
   }
 
@@ -180,7 +180,7 @@ void DataProtectionNavigationController::ApplyDataProtectionSettings(
   }
 
   // If the tab is in the background, do nothing.
-  if (!tab_interface_->IsInForeground()) {
+  if (!tab_interface_->IsActivated()) {
     return;
   }
 
@@ -227,7 +227,7 @@ void DataProtectionNavigationController::
   // Note that steps #5 and #6 are racy but the final outcome is correct
   // regardless of the order in which they execute.
 
-  if (!tab_interface_->IsInForeground()) {
+  if (!tab_interface_->IsActivated()) {
     return;
   }
 
