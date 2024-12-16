85f43cd05bd71e856139186568baef232f505c1b
Vishwas Uppoor <vishwasuppoor@google.com>
2024-12-16 08:11:34 -0800

    [PIX] Log histogram if purchase action could not be invoked
    If Chrome was unable to invoke purchase action, log PayflowExitedReason
    histogram.
    
    go/pix-in-chrome-metrics-revised
    
    Bug: 377126728, 367751320
    Change-Id: Ia2b939212c38e682bf454bdad7ff83c3988b5f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092076
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396764}

--- components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
+++ components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
@@ -206,7 +206,8 @@ INSTANTIATE_TEST_SUITE_P(
                     PayflowExitedReason::kActionTokenNotAvailable,
                     PayflowExitedReason::kUserLoggedOut,
                     PayflowExitedReason::kFopSelectorClosedNotByUser,
-                    PayflowExitedReason::kFopSelectorClosedByUser));
+                    PayflowExitedReason::kFopSelectorClosedByUser,
+                    PayflowExitedReason::kPurchaseActionCouldNotBeInvoked));
 
 class FacilitatedPaymentsMetricsUkmTest : public testing::Test {
  public:
