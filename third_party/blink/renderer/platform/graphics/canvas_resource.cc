89825ea134c7944f0bb29eeb9e0fe2aa2dd4fe2c
Colin Blundell <blundell@chromium.org>
2024-12-16 06:56:09 -0800

    [Blink] Fold ExternalCanvasResource::GenOrFlushSyncToken() into caller
    No functional changes.
    
    Bug: 352263194
    Change-Id: Ia6eade9d4b2a6b1c371350b1092dc262d86d6557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088879
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396711}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -856,12 +856,6 @@ scoped_refptr<StaticBitmapImage> ExternalCanvasResource::Bitmap() {
 const gpu::SyncToken
 ExternalCanvasResource::GetSyncTokenWithOptionalVerification(
     bool needs_verified_token) {
-  GenOrFlushSyncToken();
-  return sync_token_;
-}
-
-void ExternalCanvasResource::GenOrFlushSyncToken() {
-  TRACE_EVENT0("blink", "ExternalCanvasResource::GenOrFlushSyncToken");
   // This method is expected to be used both in WebGL and WebGPU, that's why it
   // uses InterfaceBase.
   if (!sync_token_.HasData()) {
@@ -870,7 +864,8 @@ void ExternalCanvasResource::GenOrFlushSyncToken() {
       interface->GenSyncTokenCHROMIUM(sync_token_.GetData());
   } else if (!sync_token_.verified_flush()) {
     // The offscreencanvas usage needs the sync_token to be verified in order to
-    // be able to use it by the compositor.
+    // be able to use it by the compositor. This is why this method produces a
+    // verified token even if `needs_verified_token` is false.
     int8_t* token_data = sync_token_.GetData();
     auto* interface = InterfaceBase();
     DCHECK(interface);
@@ -878,6 +873,8 @@ void ExternalCanvasResource::GenOrFlushSyncToken() {
     interface->VerifySyncTokensCHROMIUM(&token_data, 1);
     sync_token_.SetVerifyFlush();
   }
+
+  return sync_token_;
 }
 
 base::WeakPtr<WebGraphicsContext3DProviderWrapper>
