382f52ed86539b63608dadf6b838a7b8c718bb8c
Mohamed Amir Yosef <mamir@chromium.org>
2024-12-16 10:53:40 -0800

    [DC] Handle caBLE events during cross-device flow for DC
    This CL handles different events to display different UI to the user
    when the phone is connecting or done connecting already to the phone.
    
    The actual UI will be added in a follow-up CL.
    
    This CL only adds the infrastructure for that.
    
    Bug: 384423219
    Change-Id: I7dcade50176b07b2fa9480abba35022f726f9762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098250
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396846}

--- chrome/browser/digital_credentials/digital_identity_provider_desktop.h
+++ chrome/browser/digital_credentials/digital_identity_provider_desktop.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_DIGITAL_CREDENTIALS_DIGITAL_IDENTITY_PROVIDER_DESKTOP_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/ui/views/digital_credentials/digital_identity_bluetooth_manual_dialog_controller.h"
 #include "chrome/browser/ui/views/digital_credentials/digital_identity_multi_step_dialog.h"
 #include "content/public/browser/cross_device_request_info.h"
@@ -58,6 +59,11 @@ class DigitalIdentityProviderDesktop : public content::DigitalIdentityProvider {
   void OnEvent(const std::string& qr_url,
                content::digital_credentials::cross_device::Event);
 
+  // caBLE events notify when the user has started the transaction on their
+  // phone. This method updates the desktop UI to inform about the current state
+  // or to instruct the user to complete the action on the phone.
+  void OnCableEvent(device::cablev2::Event event);
+
   // Called when the transaction is finished (successfully or not).
   void OnFinished(
       base::expected<content::digital_credentials::cross_device::Response,
@@ -75,6 +81,17 @@ class DigitalIdentityProviderDesktop : public content::DigitalIdentityProvider {
   // Called when the user clicks a button on the dialog requesting Bluetooth
   // power.
   void OnUserRequestedBluetoothPowerOn();
+
+  // Called upon receiving a BLE advert from the phone which starts the
+  // connection between the phone and the desktop via the tunnel service.
+  void ShowConnectingToPhoneDialog();
+
+  // Called when the tunnel connection is established in which case the user
+  // should follow the instruction on the phone.
+  void ShowContinueStepsOnThePhoneDialog();
+
+  // Called when `cable_connecting_dialog_timer_` completes.
+  void OnCableConnectingTimerComplete();
 
   // Called when the request has failed, possibly as a result of the user
   // canceling the dialog.
@@ -101,6 +118,16 @@ class DigitalIdentityProviderDesktop : public content::DigitalIdentityProvider {
   std::unique_ptr<DigitalIdentityMultiStepDialog> dialog_;
 
   DigitalIdentityCallback callback_;
+
+  // cable_connecting_dialog_timer_ is started when we start displaying
+  // the "connecting..." dialog for a caBLE connection. To avoid flashing the
+  // UI, the dialog won't be automatically replaced until this timer completes.
+  base::OneShotTimer cable_connecting_dialog_timer_;
+
+  // cable_connecting_ready_to_advance_ is set to true if we are ready to
+  // advance the "connecting" dialog but are waiting for
+  // `cable_connecting_dialog_timer_` to complete.
+  bool cable_connecting_ready_to_advance_ = false;
 
   base::WeakPtrFactory<DigitalIdentityProviderDesktop> weak_ptr_factory_{this};
 };
