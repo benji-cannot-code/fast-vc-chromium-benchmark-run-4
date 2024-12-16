17a3ae41848b03950203d9a99b04acfedeb83d4b
Ashley Newson <ashleynewson@chromium.org>
2024-12-16 10:09:57 -0800

    [Aw WebExposed] Enable and rebaseline WebView WebExposed tests
    The tests are rebaselined to reflect the current state of the code. This
    change makes no statement as to whether an interface SHOULD be present
    in WebView's API surface, and no actual API changes are introduced as
    part of this change.
    
    This enables WebView's updated WebExposed tests, introducing them on the
    CQ to ensure ongoing changes continue to be tracked in expectations.
    
    Bug: 381090604
    Change-Id: I8aa5079af9fd28513734f3cec72d6efc15f8285c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056025
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396825}

--- android_webview/javatests/src/org/chromium/android_webview/test/WebExposedTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/WebExposedTest.java
@@ -32,7 +32,6 @@ import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.UrlUtils;
 
 import java.io.File;
@@ -162,7 +161,6 @@ public class WebExposedTest extends AwParameterizedTest {
         "enable-experimental-web-platform-features",
         "enable-blink-test-features",
     })
-    @DisabledTest(message = "crbug.com/381090604 - requires new baseline")
     public void testGlobalInterfaceListingUnstable() throws Exception {
         doTestGlobalInterfaceListing("");
     }
@@ -170,7 +168,6 @@ public class WebExposedTest extends AwParameterizedTest {
     @Test
     @LargeTest
     @CommandLineFlags.Add({"disable-field-trial-config"})
-    @DisabledTest(message = "crbug.com/381090604 - requires new baseline")
     public void testGlobalInterfaceListingStable() throws Exception {
         doTestGlobalInterfaceListing("virtual/stable/");
     }
