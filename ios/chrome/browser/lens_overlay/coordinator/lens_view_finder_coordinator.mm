9db003f5ad029721cdb4129460007af108f3a09a
Radu Nitescu <radunitescu@google.com>
2024-12-16 08:46:46 -0800

    [iOS][LU] Refactor the custom transitioning delegate for LVF
    Bug: 379824813
    Change-Id: I0c7f63d7fbd57cf8a8e8536ee1a3b509ea92693d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088167
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396783}

--- ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_configuration_factory.h"
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_entrypoint.h"
+#import "ios/chrome/browser/lens_overlay/ui/lens_view_finder_transition_manager.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
@@ -15,6 +16,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/open_lens_input_selection_command.h"
 #import "ios/chrome/browser/shared/public/commands/search_image_with_lens_command.h"
 #import "ios/public/provider/chrome/browser/lens/lens_api.h"
+
+namespace {
+
+// Maps the presentation style to transition type.
+LensViewFinderTransition TransitionFromPresentationStyle(
+    LensInputSelectionPresentationStyle style) {
+  switch (style) {
+    case LensInputSelectionPresentationStyle::SlideFromLeft:
+      return LensViewFinderTransitionSlideFromLeft;
+    case LensInputSelectionPresentationStyle::SlideFromRight:
+      return LensViewFinderTransitionSlideFromRight;
+  }
+}
+
+}  // namespace
 
 @interface LensViewFinderCoordinator () <LensCommands,
                                          ChromeLensControllerDelegate>
@@ -26,6 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // The user interface to be presented.
   __weak UIViewController* _lensViewController;
+
+  // Manages the presenting & dismissal of the LVF user interface.
+  LensViewFinderTransitionManager* _transitionManager;
 }
 
 @synthesize baseViewController = _baseViewController;
@@ -64,10 +83,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   LensConfiguration* configuration = [configurationFactory
       configurationForLensEntrypoint:command.entryPoint
                              profile:self.browser->GetProfile()];
+
+  _transitionManager = [[LensViewFinderTransitionManager alloc]
+      initWithLVFTransitionType:TransitionFromPresentationStyle(
+                                    command.presentationStyle)];
+
   _lensController = ios::provider::NewChromeLensController(configuration);
   _lensController.delegate = self;
 
   _lensViewController = _lensController.inputSelectionViewController;
+  _lensViewController.transitioningDelegate = _transitionManager;
   _lensViewController.modalPresentationStyle =
       UIModalPresentationOverCurrentContext;
   _lensViewController.modalTransitionStyle =
