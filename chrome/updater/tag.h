998c2dcbaee5e4840a86f16fc4dbe15d2c5e4565
Takuto Ikuta <tikuta@chromium.org>
2024-12-16 00:24:14 -0800

    chrome: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40263312
    Change-Id: I7b693edd8f0bf8694aa969739a2dd9290e44148a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091060
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396601}

--- chrome/updater/tag.h
+++ chrome/updater/tag.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 #include <string>
 #include <string_view>
+#include <utility>
 #include <vector>
 
 #include "base/files/file_path.h"
