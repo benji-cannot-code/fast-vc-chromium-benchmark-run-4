2fd3b4e52de4326b52d7e924482f8bdc466c5045
Daniel White <danieltwhite@google.com>
2024-12-16 08:23:21 -0800

    [iOS] Deprecate UITextItemInteraction for TableViewIllustratedEmptyView
    Since UITextItemInteraction is deprecated in iOS 17, was updated to use
    the new alternative system,
    textView:primaryActionForTextItem:defaultAction.
    
    Fixed: 349398175
    Change-Id: Idc0d9fdcd86e4574bdcca720d229caa2779eb750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5919832
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396773}

--- ios/chrome/browser/shared/ui/table_view/table_view_illustrated_empty_view.mm
+++ ios/chrome/browser/shared/ui/table_view/table_view_illustrated_empty_view.mm
@@ -118,6 +118,7 @@ NSAttributedString* GetAttributedMessage(NSString* message) {
 
 #pragma mark - UITextViewDelegate
 
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (BOOL)textView:(UITextView*)textView
     shouldInteractWithURL:(NSURL*)URL
                   inRange:(NSRange)characterRange
@@ -125,6 +126,18 @@ NSAttributedString* GetAttributedMessage(NSString* message) {
   [self.delegate tableViewIllustratedEmptyView:self didTapSubtitleLink:URL];
 
   return NO;
+}
+#endif
+
+- (UIAction*)textView:(UITextView*)textView
+    primaryActionForTextItem:(UITextItem*)textItem
+               defaultAction:(UIAction*)defaultAction API_AVAILABLE(ios(17.0)) {
+  __weak __typeof(self) weakSelf = self;
+  NSURL* URL = textItem.link;
+  return [UIAction actionWithHandler:^(UIAction* action) {
+    [weakSelf.delegate tableViewIllustratedEmptyView:weakSelf
+                                  didTapSubtitleLink:URL];
+  }];
 }
 
 - (void)textViewDidChangeSelection:(UITextView*)textView {
