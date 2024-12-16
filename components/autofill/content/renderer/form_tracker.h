b9a138686653db77052f2bab7d06cf3e63e9ecee
Friedrich Horschig <friedrichh@chromium.org>
2024-12-16 01:35:28 -0800

    [Android][3PPWM] Setter in FormTracker for UserGestureRequired
    In order to change the requirement for user gestures, make the injected
    dependency a setter. This allows to change the reuqirement when the
    config is changed with https://crrev.com/c/6088777.
    
    The default remains on "true" so the stricter default is not impaired by
    delayed initialization calls.
    
    Bug: 383068786
    Change-Id: I2c96a6e315c4576d2c620abe291754e976761caf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088698
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396617}

--- components/autofill/content/renderer/form_tracker.h
+++ components/autofill/content/renderer/form_tracker.h
@@ -84,7 +84,6 @@ class FormTracker : public content::RenderFrameObserver,
   using UserGestureRequired =
       base::StrongAlias<class UserGestureRequiredTag, bool>;
   explicit FormTracker(content::RenderFrame* render_frame,
-                       UserGestureRequired user_gesture_required,
                        AutofillAgent& agent);
 
   FormTracker(const FormTracker&) = delete;
@@ -108,6 +107,11 @@ class FormTracker : public content::RenderFrameObserver,
   void UpdateLastInteractedElement(
       absl::variant<FormRendererId, FieldRendererId> element_id);
   void ResetLastInteractedElements();
+
+  // Set whether a user gesture is required to accept text changes. If
+  // `user_gesture_required` is false, text changes without user gestures are
+  // discarded.
+  void SetUserGestureRequired(UserGestureRequired user_gesture_required);
 
   FormRef last_interacted_form() const { return last_interacted_.form; }
 
@@ -176,7 +180,7 @@ class FormTracker : public content::RenderFrameObserver,
   void ElementWasHiddenOrRemoved(mojom::SubmissionSource source);
 
   // Whether a user gesture is required to pass on text field change events.
-  const UserGestureRequired user_gesture_required_;
+  UserGestureRequired user_gesture_required_ = UserGestureRequired(true);
 
   struct {
     FormRef form;
