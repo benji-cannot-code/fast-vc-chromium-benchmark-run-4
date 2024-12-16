c15219278d7ab8fe242307c405c5091e5b2c66fc
Harsh Shukla <hvs@google.com>
2024-12-16 12:49:41 -0800

    Merge virtual card authentication response handling logic with OnRiskBasedAuthenticationResponseReceived().
    Bug: 40934051
    Change-Id: I041e9fa18e6f1d5d39e633619fe6b6749ed34173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6027276
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Harsh Shukla <hvs@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396937}

--- components/autofill/core/browser/payments/test_authentication_requester.h
+++ components/autofill/core/browser/payments/test_authentication_requester.h
@@ -68,9 +68,6 @@ class TestAuthenticationRequester
   void OnRiskBasedAuthenticationResponseReceived(
       const CreditCardRiskBasedAuthenticator::RiskBasedAuthenticationResponse&
           response) override;
-  void OnVirtualCardRiskBasedAuthenticationResponseReceived(
-      payments::PaymentsAutofillClient::PaymentsRpcResult result,
-      const payments::UnmaskResponseDetails& response_details) override;
 
   base::WeakPtr<TestAuthenticationRequester> GetWeakPtr();
 
@@ -79,10 +76,6 @@ class TestAuthenticationRequester
   std::optional<bool> did_succeed() { return did_succeed_; }
 
   std::u16string number() { return number_; }
-
-  payments::UnmaskResponseDetails response_details() const {
-    return response_details_;
-  }
 
   CreditCardRiskBasedAuthenticator::RiskBasedAuthenticationResponse&
   risk_based_authentication_response() {
@@ -107,9 +100,6 @@ class TestAuthenticationRequester
 
   // The card number returned from On*AuthenticationComplete().
   std::u16string number_;
-
-  // Unmask response returned from UnmaskCard request.
-  payments::UnmaskResponseDetails response_details_;
 
   // Authentication response returned from CreditCardRiskBasedAuthenticator.
   CreditCardRiskBasedAuthenticator::RiskBasedAuthenticationResponse
