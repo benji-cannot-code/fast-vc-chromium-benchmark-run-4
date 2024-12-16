3edcadda9d388535cb9272f94784acbcc51039e0
Alex Gough <ajgo@chromium.org>
2024-12-15 23:16:23 -0800

    Add trace for IME during startup, and enhance StartProcessWithAccess
    On Windows there is a 5ms gap[0] during startup traces between
    BrowserMainLoop::PostCreateMainMessageLoop &
    BrowserMainLoop::CreateStartupTasks. This CL adds an annotation
    to ui::InitializeInputMethod to fill this gap[1].
    
    For StartProcessWithAccess we can annotate this
    with the sandbox delegate's tag which is quick to obtain and
    always available. This makes it easier to see which process
    is being created in these traces.
    
    [0] https://ui.perfetto.dev/#!/?s=60a6ea3da5fd33c0506f3c2efa6766fb83ef23e4
    [1] https://ui.perfetto.dev/#!/?s=4162f79bf5da02557e13e0d98f91778c686f1154
    
    Bug: N/A
    Change-Id: I42869a6dd9f6ed223b302c8ec8a0e19eeebad45c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094412
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396588}

--- ui/base/ime/init/input_method_initializer.cc
+++ ui/base/ime/init/input_method_initializer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 
 #if defined(USE_AURA) && BUILDFLAG(IS_LINUX)
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 void InitializeInputMethod() {
+  TRACE_EVENT0("startup", "ui::InitializeInputMethod");
 #if BUILDFLAG(IS_WIN)
   TSFBridge::Initialize();
 #endif
