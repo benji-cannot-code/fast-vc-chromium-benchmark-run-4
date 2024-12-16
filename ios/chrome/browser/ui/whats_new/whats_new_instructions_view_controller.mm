9bb7e015290e763466a6f46cf22d9e64177e2719
Ibrahim Kanouche <kanouche@google.com>
2024-12-16 09:28:07 -0800

    [iOS] Fix close button color on "What's New" instructions view.
    Issue: The close button on the "What's New" instructions view was blue instead of grey.
    
    Change: Updated the close button to use a grey dismiss icon by applying a custom color palette to the symbol using SymbolWithPalette.
    
    Fix: This change ensures the close button appears grey as intended and request by UX.
    
    Bug: 353819242
    
    Screenshot: https://screenshot.googleplex.com/APQMZXFGGn8TYA8
    
    Change-Id: I33abf798fff65c2416433c53b9a561a659b7576f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090495
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1396805}

--- ios/chrome/browser/ui/whats_new/whats_new_instructions_view_controller.mm
+++ ios/chrome/browser/ui/whats_new/whats_new_instructions_view_controller.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/values.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
 #import "ios/chrome/common/ui/instruction_view/instruction_view.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
@@ -77,8 +78,11 @@ NSString* const kWhatsNewInstructionsLabelAccessibilityIdentifier =
   _alertScreen.titleView = self.titleLabel;
   _alertScreen.actionHandler = self.actionHandler;
   _alertScreen.showDismissBarButton = YES;
-  _alertScreen.customDismissBarButtonImage =
-      DefaultSymbolWithPointSize(kXMarkCircleFillSymbol, kDismissSymbolSize);
+  UIImage* xmarkSymbol = SymbolWithPalette(
+      DefaultSymbolWithPointSize(kXMarkCircleFillSymbol, kDismissSymbolSize),
+      @[ [UIColor colorNamed:kGrey600Color] ]);
+  _alertScreen.customDismissBarButtonImage = xmarkSymbol;
+
   _alertScreen.topAlignedLayout = YES;
 
   [NSLayoutConstraint activateConstraints:@[
