9a1066e4aedbb2dacd3183005c176d3789a29925
Baran Erfani <baranerf@google.com>
2024-12-16 13:58:03 -0800

    Implemented OnPaint() in BorderView class
    OnPaint() draws a simple border around the web contents.
    
    
    Bug: 383773288
    Change-Id: Ia7aa9a8d18cc59655d9f7e1a0df127df69823d62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092605
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396985}

--- chrome/browser/ui/views/glic/border/border_view.cc
+++ chrome/browser/ui/views/glic/border/border_view.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/compositor/layer.h"
+#include "ui/gfx/canvas.h"
 #include "ui/views/view_class_properties.h"
 
 namespace glic {
@@ -51,8 +53,15 @@ BorderView::BorderView() = default;
 BorderView::~BorderView() = default;
 
 void BorderView::OnPaint(gfx::Canvas* canvas) {
-  // Paint onto `canvas`.
-  // Call `SchedulePaint()` if the animation hasn't finished.
+  // TODO(baranerf): Modify this to a variable width when adding animation.
+  constexpr static int kBorderWidth = 5;
+
+  views::View::OnPaint(canvas);
+  cc::PaintFlags flags;
+  flags.setStyle(cc::PaintFlags::kStroke_Style);
+  flags.setColor(GetColorProvider()->GetColor(ui::kColorSysPrimary));
+  flags.setStrokeWidth(kBorderWidth);
+  canvas->DrawRect(GetContentsBounds(), flags);
 }
 
 void BorderView::OnChildViewAdded(views::View* observed_view,
@@ -67,10 +76,23 @@ void BorderView::OnAnimationStep(base::TimeTicks timestamp) {
 
 void BorderView::OnCompositingShuttingDown(ui::Compositor* compositor) {}
 
-// TODO(crbug.com/381424645): Implementation.
-void BorderView::StartAnimation() {}
+void BorderView::StartAnimation() {
+  CHECK(!animation_ongoing_);
+  animation_ongoing_ = true;
+  SetBoundsRect(GetVisibleBounds());
+  SetPaintToLayer();
+  layer()->SetFillsBoundsOpaquely(false);
+}
 
-void BorderView::CancelAnimation() {}
+void BorderView::CancelAnimation() {
+  if (!animation_ongoing_) {
+    return;
+  }
+  animation_ongoing_ = false;
+  // `DestroyLayer()` schedules another paint to repaint the affected area by
+  // the destroyed layer.
+  DestroyLayer();
+}
 
 BEGIN_METADATA(BorderView)
 END_METADATA
--- chrome/browser/ui/views/glic/border/border_view.h
+++ chrome/browser/ui/views/glic/border/border_view.h
@@ -55,6 +55,9 @@ class BorderView : public views::View,
   void StartAnimation();
 
   void CancelAnimation();
+
+ private:
+  bool animation_ongoing_ = false;
 };
 
 }  // namespace glic
