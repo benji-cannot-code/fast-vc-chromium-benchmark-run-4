684022c7c36980196593fa02d0e3ee1b565c6093
Lukasz Anforowicz <lukasza@chromium.org>
2024-12-17 12:34:39 -0800

    Roll prost: 0.13.3 => 0.13.4 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * prost: 0.13.3 => 0.13.4
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * prost@0.13.4: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I9e59115a46c0886cdd17738a21c484d372ab539a
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099062
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397520}

--- third_party/rust/chromium_crates_io/vendor/prost-0.13.4/src/encoding/varint.rs
+++ third_party/rust/chromium_crates_io/vendor/prost-0.13.4/src/encoding/varint.rs
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 use core::cmp::min;
+use core::num::NonZeroU64;
 
 use ::bytes::{Buf, BufMut};
 
@@ -26,8 +27,10 @@ pub fn encode_varint(mut value: u64, buf: &mut impl BufMut) {
 #[inline]
 pub fn encoded_len_varint(value: u64) -> usize {
     // Based on [VarintSize64][1].
-    // [1]: https://github.com/google/protobuf/blob/3.3.x/src/google/protobuf/io/coded_stream.h#L1301-L1309
-    ((((value | 1).leading_zeros() ^ 63) * 9 + 73) / 64) as usize
+    // [1]: https://github.com/protocolbuffers/protobuf/blob/v28.3/src/google/protobuf/io/coded_stream.h#L1744-L1756
+    // Safety: value | 1 is non-zero.
+    let log2value = unsafe { NonZeroU64::new_unchecked(value | 1) }.ilog2();
+    ((log2value * 9 + (64 + 9)) / 64) as usize
 }
 
 /// Decodes a LEB128-encoded variable length integer from the buffer.
