469376ccdc2f89c59f2c1c664f2f53b3e03263c9
Colin Blundell <blundell@chromium.org>
2024-12-17 01:54:32 -0800

    [Blink] Streamline GpuCanvasContext::GetSkColorType()
    Move the underlying logic into that method and have
    CanvasRenderingContextSkColorInfo() call it rather than the other way
    around.
    
    Bug: 371227617
    Change-Id: I669e556803796603d2b12d48404eaed1550be1af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097981
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397195}

--- third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
@@ -108,9 +108,7 @@ SkColorInfo GPUCanvasContext::CanvasRenderingContextSkColorInfo() const {
     return SkColorInfo(kN32_SkColorType, kPremul_SkAlphaType,
                        SkColorSpace::MakeSRGB());
   }
-  return SkColorInfo(viz::ToClosestSkColorType(
-                         /*gpu_compositing=*/true, swap_buffers_->Format()),
-                     GetAlphaType(), GetSkColorSpace());
+  return SkColorInfo(GetSkColorType(), GetAlphaType(), GetSkColorSpace());
 }
 
 SkAlphaType GPUCanvasContext::GetAlphaType() const {
@@ -120,7 +118,11 @@ SkAlphaType GPUCanvasContext::GetAlphaType() const {
 }
 
 SkColorType GPUCanvasContext::GetSkColorType() const {
-  return CanvasRenderingContextSkColorInfo().colorType();
+  if (!swap_buffers_) {
+    return kN32_SkColorType;
+  }
+  return viz::ToClosestSkColorType(
+      /*gpu_compositing=*/true, swap_buffers_->Format());
 }
 
 sk_sp<SkColorSpace> GPUCanvasContext::GetSkColorSpace() const {
