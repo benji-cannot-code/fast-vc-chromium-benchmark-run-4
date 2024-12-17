b514e14740375ef344c8b602623c8bac4e913f18
Hailey Wang <haileywang@google.com>
2024-12-17 13:52:17 -0800

    [Collaboration] Give GroupToken when showing join dialog
    The GroupToken is needed to add a member to the collaboration group.
    
    Change-Id: I2cc84b8181302fc0a735684b3c51076da0668367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094352
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397562}

--- ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate_unittest.mm
+++ ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate_unittest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/test/mock_callback.h"
+#import "components/data_sharing/public/group_data.h"
 #import "ios/chrome/browser/collaboration/model/ios_collaboration_flow_configuration.h"
 #import "ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h"
 #import "ios/chrome/browser/share_kit/model/share_kit_service_factory.h"
@@ -151,7 +152,8 @@ TEST_F(IOSCollaborationControllerDelegateTest, ShowJoinDialog) {
   base::MockCallback<CollaborationControllerDelegate::ResultCallback>
       completion_callback;
   data_sharing::SharedDataPreview preview_data;
-  delegate_->ShowJoinDialog(preview_data, completion_callback.Get());
+  delegate_->ShowJoinDialog(data_sharing::GroupToken(), preview_data,
+                            completion_callback.Get());
   EXPECT_TRUE(base_view_controller_.presentedViewController);
   // The callback is not expected to be called, as it is called when the
   // given ShareKit flow returns, i.e. when the presented view controller is
