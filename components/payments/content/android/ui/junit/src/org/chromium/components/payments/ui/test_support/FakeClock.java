2f588ebd8727cf574065803ad217ff4af5ae1c98
Rouslan Solomakhin <rouslan@chromium.org>
2024-12-17 09:12:17 -0800

    [Payments] Move InputProtector and FakeClock into the UI build target.
    In order to decouple the PaymentRequest UI code from the core
    PaymentRequest logic, this patch moves the InputProtector.java and
    FakeClock.java files:
    
    From build targets:
      //components/payments/content/android:java
      //components/payments/content/android:payments_java_test_support
    
    To build targets:
      //components/payments/content/android/ui:java
      //components/payments/content/android/ui:payments_java_test_support
    
    From packages:
      org.chromium.components.payments
      org.chromium.components.payments.test_support
    
    To packages:
      org.chromium.components.payments.ui
      org.chromium.components.payments.ui.test_support
    
    Design sketch: go/payment-request-android-code-structure
    
    Bug: 383336411
    Change-Id: I75c5ab5c31b4691a5d66e8d658ddee512c869802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6084662
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Kavita Soni <kavitasoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397365}

--- components/payments/content/android/ui/junit/src/org/chromium/components/payments/ui/test_support/FakeClock.java
+++ components/payments/content/android/ui/junit/src/org/chromium/components/payments/ui/test_support/FakeClock.java
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.components.payments.test_support;
+package org.chromium.components.payments.ui.test_support;
 
-import org.chromium.components.payments.InputProtector;
+import org.chromium.components.payments.ui.InputProtector;
 
 /** An InputProtector.Clock implementation used by tests to simulate advancing the time. */
 public class FakeClock implements InputProtector.Clock {
