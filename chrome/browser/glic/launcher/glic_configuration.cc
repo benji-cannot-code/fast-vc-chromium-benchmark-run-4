975447c53b62a7d3902e683d5d2c404cac39fe88
David Bokan <bokan@chromium.org>
2024-12-17 11:30:08 -0800

    Move prefs to glic_pref_names.h
    There's going to be many more of these prefs over time so it'd be more
    convenient to keep them all scoped within the project (as well as being
    the preferred approach in the prefs documentation [1])
    
    [1] https://chromium.googlesource.com/chromium/src/+/refs/heads/main/chrome/browser/prefs/README.md#adding-a-new-pref
    
    Bug: 382743392
    Change-Id: I09d03f643f43419f2a58be9be8f1c9537fc24688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091732
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397471}

--- chrome/browser/glic/launcher/glic_configuration.cc
+++ chrome/browser/glic/launcher/glic_configuration.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/common/pref_names.h"
+#include "chrome/browser/glic/glic_pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "ui/base/accelerators/accelerator.h"
