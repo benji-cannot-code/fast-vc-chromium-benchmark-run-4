0f93b8e1555ee24f61ed6e05ae80a2e51eda4129
Salma Elmahallawy <esalma@google.com>
2024-12-17 14:49:17 -0800

    Remove the dead function TemplateURLService::IsUnscopedModeExtensionId
    The function is only used in tests, and not expected to be used
    anymore.
    
    Bug: 378539031
    Change-Id: I60f281905736dcbd861feca637a0a2450cec21cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098922
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397605}

--- chrome/browser/search_engines/template_url_service_unittest.cc
+++ chrome/browser/search_engines/template_url_service_unittest.cc
@@ -858,12 +858,10 @@ TEST_P(TemplateURLServiceTest, AddAndRemoveExtensionIdWithUnscopedMode) {
 
   // Register an extension with unscoped mode allowed.
   model()->AddToUnscopedModeExtensionIds("id");
-  EXPECT_TRUE(model()->IsUnscopedModeExtensionId("id"));
   EXPECT_TRUE(model()->GetUnscopedModeExtensionIds().contains("id"));
 
   // Remove the registered extension.
   model()->RemoveFromUnscopedModeExtensionIdsIfPresent("id");
-  EXPECT_FALSE(model()->IsUnscopedModeExtensionId("id"));
   EXPECT_FALSE(model()->GetUnscopedModeExtensionIds().contains("id"));
 }
 
@@ -878,7 +876,6 @@ TEST_P(TemplateURLServiceTest, ExtensionWithUnscopedModeRegisteredCorrectly) {
   EXPECT_TRUE(extension_with_permission);
   EXPECT_EQ(extension_with_permission,
             model()->GetTemplateURLForKeyword(u"keyword"));
-  EXPECT_TRUE(model()->IsUnscopedModeExtensionId("id"));
   EXPECT_TRUE(model()->GetUnscopedModeExtensionIds().contains("id"));
 
   // Remove the registered extension.
@@ -887,7 +884,6 @@ TEST_P(TemplateURLServiceTest, ExtensionWithUnscopedModeRegisteredCorrectly) {
   const TemplateURL* removed_extension =
       model()->GetTemplateURLForKeyword(u"keyword");
   EXPECT_FALSE(removed_extension);
-  EXPECT_FALSE(model()->IsUnscopedModeExtensionId("id"));
   EXPECT_FALSE(model()->GetUnscopedModeExtensionIds().contains("id"));
 
   // Register an extension again without allowing unscoped mode.
@@ -898,7 +894,6 @@ TEST_P(TemplateURLServiceTest, ExtensionWithUnscopedModeRegisteredCorrectly) {
   EXPECT_TRUE(extension_without_permission);
   EXPECT_EQ(extension_without_permission,
             model()->GetTemplateURLForKeyword(u"keyword"));
-  EXPECT_FALSE(model()->IsUnscopedModeExtensionId("id"));
   EXPECT_FALSE(model()->GetUnscopedModeExtensionIds().contains("id"));
 }
 
