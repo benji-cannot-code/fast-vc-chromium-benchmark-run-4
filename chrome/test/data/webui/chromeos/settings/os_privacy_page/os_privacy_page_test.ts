c72388c7c8ecbf9003f68c0aa4aa1844f97c712b
Wes Okuhara <wesokuhara@google.com>
2024-12-17 09:27:31 -0800

    CrOS Settings: Remove revamp code from privacy page
    Bug: b:370837076
    Test: browser_tests
    Change-Id: Ib9ebd95bdc31e6bbc714d69029c016759e5a66bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098058
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397376}

--- chrome/test/data/webui/chromeos/settings/os_privacy_page/os_privacy_page_test.ts
+++ chrome/test/data/webui/chromeos/settings/os_privacy_page/os_privacy_page_test.ts
@@ -25,7 +25,7 @@ const CROS_SETTING_PREF_NAME = 'cros.device.peripheral_data_access_enabled';
 const LOCAL_STATE_PREF_NAME =
     'settings.local_state_device_pci_data_access_enabled';
 
-suite('with isRevampWayfindingEnabled set to true', () => {
+suite('Deep linking in old sync page', () => {
   let privacyPage: OsSettingsPrivacyPageElement;
   let browserProxy: TestPeripheralDataAccessBrowserProxy;
   let syncBrowserProxy: SyncBrowserProxy&TestSyncBrowserProxy;
@@ -39,9 +39,6 @@ suite('with isRevampWayfindingEnabled set to true', () => {
   }
 
   setup(() => {
-    loadTimeData.overrideValues({
-      isRevampWayfindingEnabled: true,
-    });
     const testRouter = createRouterForTesting();
     Router.resetInstanceForTesting(testRouter);
 
