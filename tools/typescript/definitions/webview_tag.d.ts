51536e55c9b1699b4d04f4c962c882b1eaba5868
Courtney Wong <courtneywong@chromium.org>
2024-12-17 12:40:15 -0800

    graduation: Add ChromeWebView to Graduation webview user agent string
    This allows the Graduation app to load the embedded Takeout Transfer
    endpoint (takeout.com/embedded/transfer).
    
    Bug: b:383177535
    Change-Id: I44222918f14c14045593ae3256b9b86ede953eb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092246
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397524}

--- tools/typescript/definitions/webview_tag.d.ts
+++ tools/typescript/definitions/webview_tag.d.ts
@@ -276,6 +276,8 @@ declare global {
         insertCSS(details: InjectDetails,
             callback?: (results: any[]) => void): void;
         terminate(): void;
+        getUserAgent(): string;
+        setUserAgentOverride(userAgent: string): void;
       }
 
       export enum ZoomMode {
