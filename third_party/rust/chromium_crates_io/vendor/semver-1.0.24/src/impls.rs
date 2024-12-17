ea165c2268ba08e866164baf072532f002d73623
Lukasz Anforowicz <lukasza@chromium.org>
2024-12-17 12:29:06 -0800

    Roll semver: 1.0.23 => 1.0.24 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * semver: 1.0.23 => 1.0.24
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * semver@1.0.24: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I06ce49088cefa93a3ab383987bc722d81461156b
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098569
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397515}

--- third_party/rust/chromium_crates_io/vendor/semver-1.0.24/src/impls.rs
+++ third_party/rust/chromium_crates_io/vendor/semver-1.0.24/src/impls.rs
@@ -51,8 +51,11 @@ impl PartialOrd for BuildMetadata {
 
 impl Ord for Prerelease {
     fn cmp(&self, rhs: &Self) -> Ordering {
+        if self.identifier.ptr_eq(&rhs.identifier) {
+            return Ordering::Equal;
+        }
+
         match self.is_empty() {
-            true if rhs.is_empty() => return Ordering::Equal,
             // A real release compares greater than prerelease.
             true => return Ordering::Greater,
             // Prerelease compares less than the real release.
@@ -106,6 +109,10 @@ impl Ord for Prerelease {
 
 impl Ord for BuildMetadata {
     fn cmp(&self, rhs: &Self) -> Ordering {
+        if self.identifier.ptr_eq(&rhs.identifier) {
+            return Ordering::Equal;
+        }
+
         let lhs = self.as_str().split('.');
         let mut rhs = rhs.as_str().split('.');
 
