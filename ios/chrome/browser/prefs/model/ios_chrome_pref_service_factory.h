573672d7cc6896fd4b185542f982a8183b4fceda
Ankush Singh <ankushkush@google.com>
2024-12-17 03:58:25 -0800

    [ios] Migrate to new account preferences implementation
    Design doc(sorry, Googlers only):
    https://docs.google.com/document/d/1M674sp04yvPM6eTZNJSr30rS7pj4ZPlMgjYBAFLKi4c/edit?usp=sharing
    
    This migrates the existing two JSON pref files implementation to a
    single JSON pref file wherein we maintain the local and the account
    preference values in the same file.
    
    Bug: 346508597
    Change-Id: I257ce3d4774c9f2d7b5679b38516ab6224518d89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743501
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397236}

--- ios/chrome/browser/prefs/model/ios_chrome_pref_service_factory.h
+++ ios/chrome/browser/prefs/model/ios_chrome_pref_service_factory.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 
 class PrefRegistry;
@@ -31,6 +32,9 @@ class PrefServiceSyncable;
 namespace user_prefs {
 class PrefRegistrySyncable;
 }
+
+extern const base::FilePath::CharType kPreferencesFilename[];
+extern const base::FilePath::CharType kAccountPreferencesFilename[];
 
 // Factory methods that create and initialize a new instance of a PrefService
 // for Chrome on iOS with the applicable PrefStores. The `pref_filename` points
