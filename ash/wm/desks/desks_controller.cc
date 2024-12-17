01147e6c7de5821b676850b3fb1f021b00467ac1
Sammie Quon <sammiequon@chromium.org>
2024-12-17 11:26:13 -0800

    coral: Create new desk when clicking a saved group
    Test: manual
    Test: ash_unittests CoralSavedGroupTest.LaunchSavedGroup
    Bug: 365839564
    Change-Id: I0cda06af44643c3dc19893a041d4c1ad6bfa5e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099115
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397467}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -1221,9 +1221,8 @@ Desk* DesksController::CreateNewDeskForSavedDesk(
       NewDesk(DesksCreationRemovalSource::kFloatingWorkspace);
       break;
     case DeskTemplateType::kCoral:
-      // TODO(crbug.com/371447150): Create a new desk with a new creation
-      // source.
-      return nullptr;
+      NewDesk(DesksCreationRemovalSource::kCoral);
+      break;
     case DeskTemplateType::kUnknown:
       return nullptr;
   }
