80ebb3d3024119a1b92fa8909962e3164506ea9d
Joey Arhar <jarhar@chromium.org>
2024-12-17 15:11:37 -0800

    Remove SelectPopupLessUpdates flag
    This has been enabled by default long enough to remove
    
    Change-Id: I6e4b167a76a224eb4eb76f56cc97f436534ec8de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102253
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397613}

--- third_party/blink/renderer/core/html/forms/select_type.cc
+++ third_party/blink/renderer/core/html/forms/select_type.cc
@@ -778,12 +778,10 @@ void MenuListSelectType::ShowPopup(PopupMenu::ShowEventType type) {
   // wrapping the <option>s.
   // We also need to update style before calling OpenPopupMenu in order to avoid
   // an expensive call to popup_->UpdateFromElement in DidRecalcStyle.
-  if (RuntimeEnabledFeatures::SelectPopupLessUpdatesEnabled()) {
-    SetNativePopupIsVisible(true);
-    if (RuntimeEnabledFeatures::CSSPseudoOpenEnabled()) {
-      select_->GetDocument().UpdateStyleAndLayoutForNode(
-          select_, DocumentUpdateReason::kPagePopup);
-    }
+  SetNativePopupIsVisible(true);
+  if (RuntimeEnabledFeatures::CSSPseudoOpenEnabled()) {
+    select_->GetDocument().UpdateStyleAndLayoutForNode(
+        select_, DocumentUpdateReason::kPagePopup);
   }
 
   if (!popup_) {
@@ -791,18 +789,8 @@ void MenuListSelectType::ShowPopup(PopupMenu::ShowEventType type) {
         *document.GetFrame(), *select_);
   }
   if (!popup_) {
-    if (RuntimeEnabledFeatures::SelectPopupLessUpdatesEnabled()) {
-      SetNativePopupIsVisible(false);
-    }
+    SetNativePopupIsVisible(false);
     return;
-  }
-
-  if (!RuntimeEnabledFeatures::SelectPopupLessUpdatesEnabled()) {
-    SetNativePopupIsVisible(true);
-    if (RuntimeEnabledFeatures::CSSPseudoOpenEnabled()) {
-      select_->GetDocument().UpdateStyleAndLayoutForNode(
-          select_, DocumentUpdateReason::kPagePopup);
-    }
   }
 
   ObserveTreeMutation();
