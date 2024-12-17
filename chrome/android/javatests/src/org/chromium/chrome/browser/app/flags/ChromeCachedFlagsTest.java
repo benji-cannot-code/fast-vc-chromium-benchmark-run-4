880fac35e0566e5aa05d222a7312d42b88a5bb08
Martin Kong <martinkong@google.com>
2024-12-17 11:40:25 -0800

    Change the sAndroidAppIntegration cached flag's default value to true
    Change the sAndroidAppIntegration cached flag's default value to true,
    so that it matches the default value in the native code.
    Changing the default value of a CachedFlag in the Java code is a
    functional change for the first Chrome run when flags aren't yet cached.
    
    Bug: 380113169
    Change-Id: I09cd13f5430b96e6ec9e224c19d09746b412cf19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097698
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397479}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
@@ -53,7 +53,6 @@ public class ChromeCachedFlagsTest {
     // DO NOT ADD FLAGS TO THIS LIST.
     private static final Set<CachedFlag> BASELINE =
             Set.of(
-                    ChromeFeatureList.sAndroidAppIntegration,
                     ChromeFeatureList.sCctFeatureUsage,
                     ChromeFeatureList.sCctGoogleBottomBar,
                     ChromeFeatureList.sCollectAndroidFrameTimelineMetrics,
