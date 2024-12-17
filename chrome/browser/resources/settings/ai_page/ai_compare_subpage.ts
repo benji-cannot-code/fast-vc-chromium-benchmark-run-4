bef939a775fada1bc39e368dd73768c1ffd00253
Zaina Al-Mashni <zalmashni@google.com>
2024-12-17 10:34:14 -0800

    [Synpase] Add disabled by policy indicator to subpages
    Add an indicator to the subpages that is visibly if they are disabled by
    policy. Disabling the controls in the subpages will be handled in a
    follow up.
    
    Screenshot: https://screenshot.googleplex.com/5gxuWq9b6gYrXAV
    Bug: 362225975
    Change-Id: I65d5db503d63225b0a0e62badbcbf2dbaef618a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088874
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397430}

--- chrome/browser/resources/settings/ai_page/ai_compare_subpage.ts
+++ chrome/browser/resources/settings/ai_page/ai_compare_subpage.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import './ai_policy_indicator.js';
 
 import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
--- chrome/browser/resources/settings/ai_page/offer_writing_help_page.ts
+++ chrome/browser/resources/settings/ai_page/offer_writing_help_page.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './ai_policy_indicator.js';
 import '../controls/settings_toggle_button.js';
 
 import {I18nMixin} from '//resources/cr_elements/i18n_mixin.js';
