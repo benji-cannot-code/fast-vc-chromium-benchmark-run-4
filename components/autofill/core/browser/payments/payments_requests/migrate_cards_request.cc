425e7cc5302f8aa2ca0b52fcef0df141e24cf547
Ivan Murashov <ivan.murashov@lge.com>
2024-12-17 07:29:53 -0800

    IWYU: Add missing include for base::StringPrintf usage in //components/autofill
    In some files the base::StringPrintf is used without corresponding
    include.
    Added missing include for base::StringPrintf usage in
    //components/autofill.
    
    Bug: none
    Change-Id: Ib67ed337b7f8d9125589f2acb698796100e47697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098190
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397307}

--- components/autofill/core/browser/payments/payments_requests/migrate_cards_request.cc
+++ components/autofill/core/browser/payments/payments_requests/migrate_cards_request.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/json/json_writer.h"
 #include "base/strings/escape.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/payments/local_card_migration_manager.h"
 
