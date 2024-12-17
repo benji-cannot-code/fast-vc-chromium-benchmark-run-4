ad2a4707ead8a4a721364ba31b892ca230031e13
Colin Blundell <blundell@chromium.org>
2024-12-17 10:29:52 -0800

    [//gpu] Make SII::CreateSharedImageForSoftwareCompositor() pure virtual
    All production subclasses now have implementations. We will give TestSII
    an implementation only when this method actually gets exercised in
    tests.
    
    Bug: 379996128
    Change-Id: I37b51dd986c7635d078d0056d2507968be19f4b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098254
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397425}

--- gpu/command_buffer/client/shared_image_interface.cc
+++ gpu/command_buffer/client/shared_image_interface.cc
@@ -86,12 +86,6 @@ scoped_refptr<ClientSharedImage> SharedImageInterface::CreateSharedImage(
 }
 
 scoped_refptr<ClientSharedImage>
-SharedImageInterface::CreateSharedImageForSoftwareCompositor(
-    const SharedImageInfo& si_info) {
-  NOTREACHED();
-}
-
-scoped_refptr<ClientSharedImage>
 SharedImageInterface::AddReferenceToSharedImage(
     const SyncToken& sync_token,
     const Mailbox& mailbox,
