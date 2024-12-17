f2dcfac9cf5d6bc01ab6123c886a775565acb6d4
Neil Coronado <nemco@google.com>
2024-12-17 14:04:56 -0800

    [TabGroupReorder] Foreground the reordering tabs
    Foreground the reordering tabs.
    -The group indicator is not yet foregrounded. This will be done in a
     followup where we'll also add the bg shape.
    
    Bug: 376068489
    Change-Id: I49b933d3559e2c3154f1d6a79f949100bffc1e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092911
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397574}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutView.java
@@ -74,6 +74,7 @@ public abstract class StripLayoutView implements VirtualView {
     private boolean mVisible = true;
     private boolean mCollapsed;
     private boolean mIsIncognito;
+    private boolean mIsForegrounded;
 
     // A11y variables.
     private String mAccessibilityDescription = "";
@@ -250,6 +251,18 @@ public abstract class StripLayoutView implements VirtualView {
      */
     public void setIncognito(boolean state) {
         mIsIncognito = state;
+    }
+
+    /** Returns {@code true} if the view is foregrounded for reorder, {@code false} otherwise. */
+    public boolean isForegrounded() {
+        return mIsForegrounded;
+    }
+
+    /**
+     * @param isForegrounded Whether or not the given view should be foregrounded for reorder.
+     */
+    void setIsForegrounded(boolean isForegrounded) {
+        mIsForegrounded = isForegrounded;
     }
 
     /**
--- chrome/browser/android/compositor/layer/tab_handle_layer.cc
+++ chrome/browser/android/compositor/layer/tab_handle_layer.cc
@@ -269,6 +269,10 @@ void TabHandleLayer::SetProperties(
   }
 }
 
+bool TabHandleLayer::foreground() {
+  return foreground_;
+}
+
 scoped_refptr<cc::slim::Layer> TabHandleLayer::layer() {
   return layer_;
 }
