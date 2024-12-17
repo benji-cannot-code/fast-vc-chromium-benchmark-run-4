01e8807359dc38199aca534fd148749d2caac78c
Adrian Taylor <adetaylor@chromium.org>
2024-12-17 03:07:57 -0800

    ATSPI fuzzer: avoid database conflicts
    Centipede can run several Chromes at once. Occasionally, they got into a
    race condition setting up tables for the database.
    
    Cq-Include-Trybots: luci.chromium.try:linux-centipede-asan-rel
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel
    Cq-Include-Trybots: luci.chromium.try:win-libfuzzer-asan-rel
    Change-Id: I37c7b5a15679bac93fbffb88ce4df2a7fd4f5463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6101190
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Auto-Submit: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397220}

--- chrome/test/fuzzing/atspi_in_process_fuzzer.cc
+++ chrome/test/fuzzing/atspi_in_process_fuzzer.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/database.h"
 #include "sql/statement.h"
 #include "sql/test/test_helpers.h"
+#include "sql/transaction.h"
 #include "testing/libfuzzer/libfuzzer_exports.h"
 #include "ui/accessibility/platform/inspect/ax_inspect_scenario.h"
 #include "ui/accessibility/platform/inspect/ax_inspect_utils_auralinux.h"
@@ -1070,6 +1071,9 @@ Database::Database() {
   db_path = db_path.AppendASCII("atspi_in_process_fuzzer_controls.db");
   CHECK(db_->Open(db_path));
   CHECK(db_->Execute("PRAGMA foreign_keys = ON"));
+  // Atomically delete and create tables
+  sql::Transaction transaction(db_.get());
+  CHECK(transaction.Begin());
   // Delete some tables from older versions of this fuzzer
   DropTableIfExists("roles");
   DropTableIfExists("names");
@@ -1098,6 +1102,7 @@ Database::Database() {
         "controlsv4(id) ON DELETE CASCADE,  FOREIGN KEY(action_id) REFERENCES "
         "actionsv2(id) ON DELETE CASCADE, unique(control_id, action_id))"));
   }
+  CHECK(transaction.Commit());
 }
 
 void Database::DropTableIfExists(const std::string& table_name) {
