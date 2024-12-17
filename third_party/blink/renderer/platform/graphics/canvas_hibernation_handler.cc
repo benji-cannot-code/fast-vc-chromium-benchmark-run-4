47f58794611a5d9c1195e3100edaeaba95e034dc
Ivan Murashov <ivan.murashov@lge.com>
2024-12-16 23:27:05 -0800

    IWYU: Add missing include for base::StringPrintf usage in //third_party/blink/renderer/platform/graphics
    In some files the base::StringPrintf is used without corresponding
    include.
    Added missing include for base::StringPrintf usage in
    //third_party/blink/renderer/platform/graphics.
    
    Bug: none
    Change-Id: Ic1db3f1600245fb0c6190b83fb39abfcd0a58815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097974
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1397160}

--- third_party/blink/renderer/platform/graphics/canvas_hibernation_handler.cc
+++ third_party/blink/renderer/platform/graphics/canvas_hibernation_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/memory/post_delayed_memory_reduction_task.h"
+#include "base/strings/stringprintf.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/memory_dump_request_args.h"
--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_helpers.h"
 #include "base/memory/read_only_shared_memory_region.h"
+#include "base/strings/stringprintf.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
