535a7e94c780ca864ffab5a0a9afdae0001cb2c9
Wes Okuhara <wesokuhara@google.com>
2024-12-16 16:10:07 -0800

    CrOS Settings: Remove revamp flag from routes and router
    Bug: b:326488045
    Test: browser_tests
    Change-Id: I823a00f9c72974c99257bf33df8606d00ee45930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098315
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397065}

--- chrome/test/data/webui/chromeos/settings/os_settings_ui/os_settings_ui_page_visibility_revamp_test.ts
+++ chrome/test/data/webui/chromeos/settings/os_settings_ui/os_settings_ui_page_visibility_revamp_test.ts
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview
  * Suite of tests for page visibility in the CrOS Settings UI.
  *
- * - These tests expect the OsSettingsRevampWayfinding feature flag to be
- *   enabled.
  * - This suite is separated into a dedicated file to mitigate test timeouts
  *   since the element is very large.
  */
@@ -139,9 +137,6 @@ suite('<os-settings-ui> page visibility', () => {
   suiteSetup(async () => {
     browserProxy = new TestAccountManagerBrowserProxy();
     AccountManagerBrowserProxyImpl.setInstanceForTesting(browserProxy);
-    assertTrue(
-        loadTimeData.getBoolean('isRevampWayfindingEnabled'),
-        'This suite expects OsSettingsRevampWayfinding to be enabled.');
     loadTimeData.overrideValues({
       isKerberosEnabled: true,  // Simulate kerberos route exists
     });
