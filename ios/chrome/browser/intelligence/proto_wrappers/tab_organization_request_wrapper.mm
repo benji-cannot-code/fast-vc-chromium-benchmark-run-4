8895b0b4822a29705070d36231f8eaccd35ccd36
Nicolas MacBeth <nicolasmacbeth@google.com>
2024-12-17 10:48:56 -0800

    [iOS][PageContext] Use cached screenshots instead of capturing new ones
    Using cached screenshots instead of capturing new ones in PageContext
    proto population. If the webstate is currently visible however, we
    force update the screenshot in case the user was still scrolling when
    the last screenshot was created.
    
    Bug: 379911543
    Change-Id: I8e50cbcaca84764a06b27e8801d3ad51de893a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089375
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397441}

--- ios/chrome/browser/intelligence/proto_wrappers/tab_organization_request_wrapper.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/tab_organization_request_wrapper.mm
@@ -116,6 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         })];
 
     [pageContextWrapper setShouldGetSnapshot:YES];
+    [pageContextWrapper setShouldForceUpdateMissingSnapshots:YES];
 
     // Hold references to each PageContextWrapper to keep them alive during
     // their async work.
