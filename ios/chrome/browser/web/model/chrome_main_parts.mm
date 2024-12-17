52440992a93de035cc977e04fe636d90ed2bd237
Sylvain Defresne <sdefresne@chromium.org>
2024-12-17 11:15:40 -0800

    [ios] Force deterministic initialization of OSCrypt
    OSCrypt is lazily initialized the first time it is accessed. There
    is suspicion that by delaying the loading of ProfileIOS, the first
    call to OSCrypt may happen to late, causing failures in accessing
    the key from the OS.
    
    Add an explicit initialization of OSCrypt from IOSChromeMainParts
    where the ProfileIOS where previously loaded to replicate how the
    library was previously initialized (before the introduction of the
    multi-profile support).
    
    Bug: 383661630
    Change-Id: I38c4041d85421ba04a70daa5499cb983c034404b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097195
    Commit-Queue: Will Harris <wfh@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397461}

--- ios/chrome/browser/web/model/chrome_main_parts.mm
+++ ios/chrome/browser/web/model/chrome_main_parts.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/task/sequenced_task_runner.h"
 #import "base/task/single_thread_task_runner.h"
+#import "base/task/task_traits.h"
 #import "base/task/thread_pool.h"
 #import "base/time/default_tick_clock.h"
 #import "build/blink_buildflags.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/metrics/metrics_service.h"
 #import "components/metrics_services_manager/metrics_services_manager.h"
 #import "components/open_from_clipboard/clipboard_recent_content.h"
+#import "components/os_crypt/sync/os_crypt.h"
 #import "components/prefs/json_pref_store.h"
 #import "components/prefs/pref_service.h"
 #import "components/previous_session_info/previous_session_info.h"
@@ -103,6 +105,14 @@ void SetProtectionLevel(const base::FilePath& file_path, id level) {
                                                  forKey:NSURLFileProtectionKey
                                                   error:&error];
   DCHECK(protection_set) << base::SysNSStringToUTF8(error.localizedDescription);
+}
+
+// Initializes OSCrypt.
+void EnsureOSCryptInitialized() {
+  // There is no public API to initialize OSCrypt. It is performed one the
+  // first call to the library, so call `OSCrypt::IsEncryptionAvailable()`
+  // and discard the result to perform the initialisation.
+  std::ignore = OSCrypt::IsEncryptionAvailable();
 }
 
 }  // namespace
@@ -291,6 +301,12 @@ void IOSChromeMainParts::PreMainMessageLoopRun() {
   base::ThreadPool::PostTask(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
       base::BindOnce(&FirstRun::LoadSentinelInfo));
+
+  // Force the initialisation of the OSCrypt library early in the application
+  // startup sequence. See https://crbug.com/383661630 for why this is needed.
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
+      base::BindOnce(&EnsureOSCryptInitialized));
 
   // ContentSettingsPattern need to be initialized before creating the
   // ProfileIOS.
