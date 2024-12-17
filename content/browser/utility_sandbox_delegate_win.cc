ba43d9ac1d7a074aabb50773f48a3c8ce80db92a
Stefan Smolen <ssmole@microsoft.com>
2024-12-17 14:43:30 -0800

    Filter the environment variables of kService* sandboxes on Windows
    This change adds a `WinSboxFilterServiceEnvironment` feature which
    applies the existing `SetFilterEnvironment` mitigation used by renderer
    sandbox to `kService` and `kServiceWithJit` sandboxes. The primary
    motivation to pick these sandboxes is because they also apply the
    `AddWin32kLockdownPolicy` mitigation, and they are highly sandboxed.
    
    Bug: 384549643
    Change-Id: I7f54f00a06136532a83b4c9c4b00cefed9e1d4a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097220
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Stefan Smolen <ssmole@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1397602}

--- content/browser/utility_sandbox_delegate_win.cc
+++ content/browser/utility_sandbox_delegate_win.cc
@@ -355,6 +355,9 @@ bool UtilitySandboxedProcessLauncherDelegate::InitializeConfig(
     if (result != sandbox::SBOX_ALL_OK) {
       return false;
     }
+
+    config->SetFilterEnvironment(base::FeatureList::IsEnabled(
+        sandbox::policy::features::kWinSboxFilterServiceEnvironment));
   }
 
   if (sandbox_type_ == sandbox::mojom::Sandbox::kService) {
