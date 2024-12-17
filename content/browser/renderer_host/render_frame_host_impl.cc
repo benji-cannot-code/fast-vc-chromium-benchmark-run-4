dfc62ecaa01ebbbae808517420c647f01cd29694
Ari Chivukula <arichiv@chromium.org>
2024-12-17 10:30:29 -0800

    Clean up stale base::Feature "DisableFrameNameUpdateOnNonCurrentRenderFrameHost"
    This has been enabled by default since 2022:
    https://chromium-review.googlesource.com/c/chromium/src/+/3574576
    Let's clean it up!
    
    Fixed: 356624362
    Change-Id: I02a0abbe3e78fc6069fa1c995bc1d70d387dec5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097739
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Harkiran Bolaria <hbolaria@google.com>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397426}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -330,9 +330,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace features {
-BASE_FEATURE(kDisableFrameNameUpdateOnNonCurrentRenderFrameHost,
-             "DisableFrameNameUpdateOnNonCurrentRenderFrameHost",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Evict when accessibility events occur while in back/forward cache.
 // Disabling on all platforms since https://crbug.com/1341507 has been addressed
@@ -7355,9 +7352,7 @@ void RenderFrameHostImpl::DidChangeName(const std::string& name,
   // now occur in RenderFrameHostImpl (and by extension, BrowsingContextState),
   // ensure that invalid updates (i.e. when in the BackForwardCache or in a
   // pending deletion state) are not applied.
-  if ((IsInBackForwardCache() || IsPendingDeletion()) &&
-      base::FeatureList::IsEnabled(
-          features::kDisableFrameNameUpdateOnNonCurrentRenderFrameHost)) {
+  if (IsInBackForwardCache() || IsPendingDeletion()) {
     return;
   }
   if (GetParent() != nullptr) {
