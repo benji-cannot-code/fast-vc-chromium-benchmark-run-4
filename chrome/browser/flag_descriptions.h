e94f9442f9121f201017d4414510675236f595cd
Shibalik Mohapatra <shibalik@chromium.org>
2024-12-17 14:32:55 -0800

    [TabstripDedupe] Add tab deduplication to chrome://flags
    Change-Id: I29d19fdf334f079403c6c43e89a80196c2923c0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099215
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397593}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3154,6 +3154,9 @@ extern const char kTabReorganizationDividerDescription[];
 extern const char kTabstripDeclutterName[];
 extern const char kTabstripDeclutterDescription[];
 
+extern const char kTabstripDedupeName[];
+extern const char kTabstripDedupeDescription[];
+
 extern const char kIOSPromoRefreshedPasswordBubbleName[];
 extern const char kIOSPromoRefreshedPasswordBubbleDescription[];
 
