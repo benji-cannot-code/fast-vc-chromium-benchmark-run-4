b84c31969e122a76446fd18b0ec0c66138214d9b
Taiyo Mizuhashi <taiyo@chromium.org>
2024-12-17 20:10:22 -0800

    Prefetch: Eliminate unnecessary ResourceRequest::TrustedParams.copy
    After this CL, OnGotPrefetchToServe will receive GURL, instead of
    network::ResourceRequest. This avoids the copy of latter when passing it via base::BindOnce, which makes the prefetch matching faster.
    
    Bug: n/a
    Change-Id: Ib97bcbf901c4ac37fae496bd7b4aee2281aea0d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6100573
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397720}

--- content/browser/preloading/prefetch/prefetch_url_loader_helper.h
+++ content/browser/preloading/prefetch/prefetch_url_loader_helper.h
@@ -14,11 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// Checks if `prefetch_container` can be used for `tentative_resource_request`,
-// and starts checking `PrefetchOriginProber` if needed.
+// Checks if `prefetch_container` can be used for the url of intercepted
+// `tentative_resource_request`, and starts checking `PrefetchOriginProber` if
+// needed.
 void CONTENT_EXPORT OnGotPrefetchToServe(
     FrameTreeNodeId frame_tree_node_id,
-    const network::ResourceRequest& tentative_resource_request,
+    const GURL& tentative_resource_request_url,
     base::OnceCallback<void(PrefetchContainer::Reader)> get_prefetch_callback,
     PrefetchContainer::Reader reader);
 
