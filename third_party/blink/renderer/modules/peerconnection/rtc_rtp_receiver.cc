0b72d472954681f7383c16e99bdabbe6f203189b
Ivan Murashov <ivan.murashov@lge.com>
2024-12-18 10:12:56 -0800

    IWYU: Add missing include for base::StringPrintf usage in //third_party/blink/renderer/modules/peerconnection
    In some files the base::StringPrintf is used without corresponding
    include.
    Added missing include for base::StringPrintf usage in
    //third_party/blink/renderer/modules/peerconnection.
    
    Bug: none
    Change-Id: I6adea1ac70a514a88db66ba145e4459a47b16860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097973
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398052}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver.h"
 
 #include "base/numerics/safe_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "base/synchronization/lock.h"
 #include "base/task/single_thread_task_runner.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
