d8016d7656b72ea123d39f26b1e43e15031598e5
Din Nezametdinov <shaikhitdin@google.com>
2024-12-18 01:45:51 -0800

    [CodeHealth] Clean-up InfoCardAcknowledgementTracking feature flag
    Clean-up flag related code.
    
    I am not sure if ordering in `CreateFeedQueryRequest` matters, left it
    as is.
    
    Bug: 356236380
    Change-Id: Iac0fc9cf874e7049fda0eaef816910df87db4979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098014
    Commit-Queue: Din Nezametdinov <shaikhitdin@google.com>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397804}

--- components/feed/core/v2/proto_util.cc
+++ components/feed/core/v2/proto_util.cc
@@ -144,7 +144,8 @@ feedwire::Request CreateFeedQueryRequest(
 #if BUILDFLAG(IS_ANDROID)
         Capability::SYNC_STRING_REMOVAL,
 #endif
-        Capability::SPORTS_IN_GAME_UPDATE}) {
+        Capability::SPORTS_IN_GAME_UPDATE,
+        Capability::INFO_CARD_ACKNOWLEDGEMENT_TRACKING}) {
     feed_request.add_client_capability(capability);
   }
 
@@ -173,11 +174,6 @@ feedwire::Request CreateFeedQueryRequest(
 
   if (base::FeatureList::IsEnabled(kFeedSignedOutViewDemotion)) {
     feed_request.add_client_capability(Capability::ON_DEVICE_VIEW_HISTORY);
-  }
-
-  if (base::FeatureList::IsEnabled(kInfoCardAcknowledgementTracking)) {
-    feed_request.add_client_capability(
-        Capability::INFO_CARD_ACKNOWLEDGEMENT_TRACKING);
   }
 
   if (base::FeatureList::IsEnabled(kSyntheticCapabilities)) {
