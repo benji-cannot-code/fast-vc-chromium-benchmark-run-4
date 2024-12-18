a7743de0540d14dc921cc1316ed20712000ba12d
Dibyajyoti Pal <dibyapal@google.com>
2024-12-18 14:18:00 -0800

    [PWA] Retire WebAppRegistrar::IsShortcutApp()
    This CL should be the last step of cleaning up any last remaining
    references to `WebAppRegistrar::IsShortcutApp()`. It should be safe
    to remove this API, since this API has worked wrongfully for the
    past few years (crbug.com/40240353) without anything breaking. A lot
    of the test APIs about shortcut creation have been dead code, so
    it should be safe to remove.
    
    This can be landed once the shortcut to DIY app migration is complete.
    
    Bug: 380451541
    Change-Id: Ie67a559a86cc5f054b21eb9b5cc82e1f56994d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092229
    Reviewed-by: May Siem <msiem@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398238}

--- chrome/browser/web_applications/web_app_registrar.h
+++ chrome/browser/web_applications/web_app_registrar.h
@@ -157,8 +157,6 @@ class WebAppRegistrar {
   // CHECK-fail if `allowed_states` is empty.
   // The 'best' criteria is a combination of the following:
   // - The length of the scope of the potential controlling app.
-  // - If the app is a shortcut app or not.
-  // TODO(crbug.com/341316725): Remove shortcut apps.
   std::optional<webapps::AppId> FindBestAppWithUrlInScope(
       const GURL& url,
       std::initializer_list<proto::InstallState> allowed_states) const;
@@ -395,10 +393,6 @@ class WebAppRegistrar {
   int GetUrlInAppScopeScore(const std::string& url_spec,
                             const webapps::AppId& app_id) const;
 
-  // Returns whether the app is a shortcut app (as opposed to a PWA).
-  // TODO(crbug.com/341316725): Remove shortcut apps.
-  bool IsShortcutApp(const webapps::AppId& app_id) const;
-
   // Returns whether the app is pending successful navigation in order to
   // complete installation via the ExternallyManagedAppManager.
   bool IsPlaceholderApp(const webapps::AppId& app_id,
@@ -457,13 +451,12 @@ class WebAppRegistrar {
       const std::string& partition_name);
 
   // Returns if the given app_id would ever be eligible to capture links in
-  // its scope. This returns false for apps that aren't installed or for
-  // "Create Shortcut..." apps.
+  // its scope. This returns false for apps that aren't installed.
   bool CanCaptureLinksInScope(const webapps::AppId& app_id) const;
 
   // Returns true if a web app is set to be the default app to
-  // capture links by the user. If an app is not locally installed or is a
-  // shortcut, this returns false.
+  // capture links by the user. If an app is not locally installed, this returns
+  // false.
   bool CapturesLinksInScope(const webapps::AppId& app_id) const;
 
   // Searches for all apps that can control this url, and chooses the best one
