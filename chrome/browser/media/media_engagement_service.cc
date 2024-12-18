ed78f5d252bb98164fe16b9b3dce455e409f7308
Benjamin Keen <bkeen@google.com>
2024-12-17 19:39:25 -0800

    Update HasHighEngagement to also check the MediaEngagementPreloadedList
    This change updates the media engagement service HasHighEngagement
    method to also check the MediaEngagementPreloadedList.
    
    If the media engagement score is low, HasHighEngagement will return
    true if:
      * The preload media engagement feature is enabled
      * The number of visits is less than the number of visits required to
        have an MEI score
      * And, the origin is present in the list
    
    The resulting HasHighEngagement method is also reused by
    MediaEngagementContentsObserver::ReadyToCommitNavigation, to maintain
    the shared logic in a single place.
    
    Finally, the AutoPiP tab helper is updated to perform the URL scheme
    check before the video conferencing/media playback checks, and ignores
    media engagement for URLs with file scheme. This is done to maintain
    the HasHighEngagement invariant.
    
    Bug: 368058093
    Change-Id: I5cbd043afbbd8ce424108ff921e057463dfaa8ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098060
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397711}

--- chrome/browser/media/media_engagement_service.cc
+++ chrome/browser/media/media_engagement_service.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/media/media_engagement_contents_observer.h"
+#include "chrome/browser/media/media_engagement_preloaded_list.h"
 #include "chrome/browser/media/media_engagement_score.h"
 #include "chrome/browser/media/media_engagement_service_factory.h"
 #include "chrome/browser/preloading/prefetch/no_state_prefetch/chrome_no_state_prefetch_contents_delegate.h"
@@ -257,7 +258,26 @@ double MediaEngagementService::GetEngagementScore(
 
 bool MediaEngagementService::HasHighEngagement(
     const url::Origin& origin) const {
-  return CreateEngagementScore(origin).high_score();
+  MediaEngagementScore score = CreateEngagementScore(origin);
+  bool has_high_engagement = score.high_score();
+  if (has_high_engagement) {
+    return true;
+  }
+
+  if (base::FeatureList::IsEnabled(media::kMediaEngagementHTTPSOnly)) {
+    DCHECK(!has_high_engagement || (origin.scheme() == url::kHttpsScheme));
+  }
+
+  if (!base::FeatureList::IsEnabled(media::kPreloadMediaEngagementData)) {
+    return false;
+  }
+
+  if (score.visits() >= MediaEngagementScore::GetScoreMinVisits()) {
+    return false;
+  }
+
+  return MediaEngagementPreloadedList::GetInstance()->CheckOriginIsPresent(
+      origin);
 }
 
 std::map<url::Origin, double> MediaEngagementService::GetScoreMapForTesting()
