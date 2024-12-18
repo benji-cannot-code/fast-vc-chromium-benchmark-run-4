63bacce2515e74559ebc2df22c47001af7b74cc7
Ivan Murashov <ivan.murashov@lge.com>
2024-12-18 07:28:55 -0800

    IWYU: Add missing include for base::StringPrintf usage in //components/fingerprinting_protection_filters
    In some files the base::StringPrintf is used without corresponding
    include.
    Added missing include for base::StringPrintf usage in
    //components/fingerprinting_protection_filters.
    
    Bug: none
    Change-Id: I287d8e36f50c2d16afddd6be332a621f6fe88ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097434
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Martin Verde <thesalsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397933}

--- components/fingerprinting_protection_filter/renderer/renderer_agent.cc
+++ components/fingerprinting_protection_filter/renderer/renderer_agent.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/stringprintf.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_constants.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.h"
 #include "components/fingerprinting_protection_filter/mojom/fingerprinting_protection_filter.mojom.h"
