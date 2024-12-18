67e67643940ddb3f947ba40a466db8f338496d75
Mikel Astiz <mastiz@chromium.org>
2024-12-18 14:41:09 -0800

    Update tests to avoid implicit conversion between GaiaId and std::string
    Class GaiaId was introduced to improve type safety and avoid strings
    being used, which is more error-prone.
    
    Temporarily, implicit conversion between GaiaId and std::string was
    allowed, until all occurrences relying on it are migrated. This patch
    updates tests under chrome/browser/ash/accessibility.
    
    This CL was uploaded by git cl split.
    
    R=dtseng@chromium.org
    
    Bug: 380416867
    Change-Id: I281eb990a024c7118b368a2e06d43c2adde87826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6106293
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398256}

--- chrome/browser/ash/accessibility/magnification_manager_browsertest.cc
+++ chrome/browser/ash/accessibility/magnification_manager_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ash/accessibility/magnification_manager.h"
+
 #include <string>
 
 #include "ash/accessibility/magnifier/fullscreen_magnifier_controller.h"
@@ -12,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
-#include "chrome/browser/ash/accessibility/magnification_manager.h"
 #include "chrome/browser/ash/login/helper.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/session_manager/core/session_manager.h"
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/test/browser_test.h"
+#include "google_apis/gaia/gaia_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
@@ -176,7 +178,7 @@ class MagnificationManagerTest : public InProcessBrowserTest {
   }
 
   const AccountId test_account_id_ =
-      AccountId::FromUserEmailGaiaId(kTestUserName, kTestUserGaiaId);
+      AccountId::FromUserEmailGaiaId(kTestUserName, GaiaId(kTestUserGaiaId));
 };
 
 IN_PROC_BROWSER_TEST_F(MagnificationManagerTest, PRE_LoginOffToOff) {
