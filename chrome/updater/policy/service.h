2c51901573c615e4829b6e8c510d2d3f32f3e225
Xiaoling Bao <xiaolingbao@chromium.org>
2024-12-17 16:57:41 -0800

    Handle policy fetch error during force install/update.
    Roughly speaking, with this CL, all non-self-updates/installs are blocked if policies can't be obtained.
    
    Current implementation does not include a customized UI message for
    policy fetch error. Instead, it's just shows the generic update error
    (update fail to start) with extra code `kErrorPolicyFetchFailed`.
    
    This implementation relies on the assumption that a policy fetch
    is done before the policy value is queried.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Bug: 382328658
    Change-Id: Ice73abd434bf5f7bc630b28401ebf50138f0d25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6077922
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397665}

--- chrome/updater/policy/service.h
+++ chrome/updater/policy/service.h
@@ -150,6 +150,9 @@ class PolicyService : public base::RefCountedThreadSafe<PolicyService> {
   // Enterprise Core (formerly Chrome Enterprise Cloud Management).
   void IsCloudManaged(base::OnceCallback<void(bool)> callback) const;
 
+  // Returns the last policy fetch result.
+  std::optional<int> LastFetchResult() const { return last_fetch_result_; }
+
  protected:
   virtual ~PolicyService();
 
@@ -187,6 +190,9 @@ class PolicyService : public base::RefCountedThreadSafe<PolicyService> {
   // Also contains a named map indexed by `source()` for all the policy
   // managers.
   PolicyManagers policy_managers_;
+
+  // Holds the last policy fetch result.
+  std::optional<int> last_fetch_result_;
 
   const scoped_refptr<ExternalConstants> external_constants_;
 
