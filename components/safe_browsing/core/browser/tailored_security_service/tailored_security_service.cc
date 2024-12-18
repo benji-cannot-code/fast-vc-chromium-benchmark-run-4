d067be7c5c748f240d36e3ae970b9c78ae67b0d6
thefrog <thefrog@chromium.org>
2024-12-18 14:43:26 -0800

    Add network result histogram for tailored security service
    This also changes the default unset response code from -1 to 0. This
    matches other safe_browsing network result histogram implementations and has no effect on the existing usages of the response code. This change
    is needed in this CL because -1 is a valid net error value (IO_PENDING),
    and we want to avoid the default case logging a real value.
    
    Histogram SafeBrowsing.TailoredSecurityService.OAuthTokenResponseCode is
    removed. It is now redundant due to the new histogram.
    
    New histogram:
     - SafeBrowsing.TailoredSecurityService.OAuthTokenNetworkResult
    Removed histogram:
     - SafeBrowsing.TailoredSecurityService.OAuthTokenResponseCode
    
    OBSOLETE_HISTOGRAM[SafeBrowsing.TailoredSecurityService.OAuthTokenResponseCode]=Replaced by SafeBrowsing.TailoredSecurityService.OAuthTokenNetworkResult.
    
    Bug: 383994656
    Change-Id: I61a5b6dcb88c1687c165e1f686677444afc7dc6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102287
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398259}

--- components/safe_browsing/core/browser/tailored_security_service/tailored_security_service.cc
+++ components/safe_browsing/core/browser/tailored_security_service/tailored_security_service.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/features.h"
 #include "components/safe_browsing/core/common/safe_browsing_policy_handler.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
+#include "components/safe_browsing/core/common/utils.h"
 #include "components/signin/public/identity_manager/access_token_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_access_token_fetcher.h"
@@ -39,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/base/url_util.h"
 #include "net/http/http_status_code.h"
-#include "net/http/http_util.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
@@ -158,18 +158,16 @@ class RequestImpl : public TailoredSecurityService::Request {
   void Shutdown() override {}
 
   void OnSimpleLoaderComplete(std::unique_ptr<std::string> response_body) {
-    response_code_ = -1;
+    response_code_ = 0;
     if (simple_url_loader_->ResponseInfo() &&
         simple_url_loader_->ResponseInfo()->headers) {
       response_code_ =
           simple_url_loader_->ResponseInfo()->headers->response_code();
     }
+    RecordHttpResponseOrErrorCode(
+        "SafeBrowsing.TailoredSecurityService.OAuthTokenNetworkResult",
+        simple_url_loader_->NetError(), response_code_);
     simple_url_loader_.reset();
-
-    UMA_HISTOGRAM_CUSTOM_ENUMERATION(
-        "SafeBrowsing.TailoredSecurityService.OAuthTokenResponseCode",
-        net::HttpUtil::MapStatusCodeForHistogram(response_code_),
-        net::HttpUtil::GetStatusCodesForHistogram());
 
     // If the response code indicates that the token might not be valid,
     // invalidate the token and try again.
