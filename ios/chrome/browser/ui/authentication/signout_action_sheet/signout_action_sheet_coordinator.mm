1a527b9e17feefc290b391f46adfbc4b6345d2ff
Jood <jood@google.com>
2024-12-18 02:00:19 -0800

    [iOS] Implement ChangeProfileSignoutContinuation executeWithBrowser
    This CL:
    * Passes signout completion block to ChangeProfileSignoutContinuation.
    * Implements the sign out logic in executeWithBrowser
    * Calls the new helper in multi-profile case from SignoutActionSheet
    * These changes are feature gated, only when
    kSeparateProfilesForManagedAccounts is enabled.
    * Later CLs will migrate other (existing) signout callers, to unify the
    code path.
    
    Bug: 375605174
    Change-Id: I8634de2c2e5ba4fde1669c0f7637a1d7a93616ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088117
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397812}

--- ios/chrome/browser/ui/authentication/signout_action_sheet/signout_action_sheet_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signout_action_sheet/signout_action_sheet_coordinator.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sync/model/enterprise_utils.h"
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
 #import "ios/chrome/browser/ui/authentication/authentication_ui_util.h"
+#import "ios/chrome/browser/ui/authentication/signin/signin_utils.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
@@ -593,9 +594,20 @@ typedef NS_ENUM(NSUInteger, SignedInUserState) {
   [self preventUserInteraction];
   id<SnackbarCommands> snackbarCommandsHandler = HandlerForProtocol(
       self.browser->GetCommandDispatcher(), SnackbarCommands);
+  // Prepare the signout snackbar before account switching.
   // The snackbar message might be nil if the snackbar is not needed.
   MDCSnackbarMessage* snackbarMessage = [self signoutSnackbarMessage];
+
   __weak __typeof(self) weakSelf = self;
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+    signin::MultiProfileSignOut(self.browser, _signout_source_metric,
+                                forceClearData, _forceSnackbarOverToolbar,
+                                snackbarMessage, ^{
+                                  [weakSelf signOutDidFinish];
+                                });
+    return;
+  }
+
   BOOL forceSnackbarOverToolbar = _forceSnackbarOverToolbar;
   self.authenticationService->SignOut(_signout_source_metric, forceClearData, ^{
     // The snackbar should be displayed even if self has been deallocated.
