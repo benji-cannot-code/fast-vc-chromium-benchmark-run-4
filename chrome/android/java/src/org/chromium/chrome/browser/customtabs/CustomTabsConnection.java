249b2b875f9faf097a2a5e055e1cf997871cd9e2
Jinsuk Kim <jinsukkim@chromium.org>
2024-12-18 10:33:13 -0800

    [CCT] Hook up CustomTabsConnection#isEphemeralBrowsingSupported
    Hook up a new API to be supported in AndroidX that informs the
    availability of the feature Ephemeral Browsing.
    
    AndroidX CL: https://android-review.git.corp.google.com/c/platform/frameworks/support/+/3417184
    
    Bug: crbug.com/384548523
    Change-Id: I1163d3a99dfdb33bcff713e7c44c68f2169f8295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6101702
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398072}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -2154,6 +2154,11 @@ public class CustomTabsConnection {
         return mClientManager.getEngagementSignalsCallbackForSession(session) != null;
     }
 
+    /** Whether Ephemeral Browsing is supported. */
+    public boolean isEphemeralBrowsingSupported(Bundle extras) {
+        return ChromeFeatureList.sCctEphemeralMode.isEnabled();
+    }
+
     /** Whether a CustomTabs instance should include interactive Omnibox. */
     public boolean shouldEnableOmniboxForIntent(BrowserServicesIntentDataProvider intentData) {
         return false;
