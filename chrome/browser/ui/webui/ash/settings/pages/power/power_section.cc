84029a6299c48f3c90cacd7ec5e276c5bbebfb73
Wes Okuhara <wesokuhara@google.com>
2024-12-18 10:29:08 -0800

    CrOS Settings: Remove revamp code in system preferences subsections
    Bug: b:370836652, b:384064212
    Test: browser_tests
    Change-Id: I8bb9f69cfbc22181c08c9613b98c8e97399b9d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102131
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398066}

--- chrome/browser/ui/webui/ash/settings/pages/power/power_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/power/power_section.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::settings {
 
 namespace mojom {
-using ::chromeos::settings::mojom::kDeviceSectionPath;
 using ::chromeos::settings::mojom::kPowerSubpagePath;
 using ::chromeos::settings::mojom::kSystemPreferencesSectionPath;
 using ::chromeos::settings::mojom::Section;
@@ -151,9 +150,6 @@ PowerSection::~PowerSection() {
 }
 
 void PowerSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
-  const bool kIsRevampEnabled =
-      ash::features::IsOsSettingsRevampWayfindingEnabled();
-
   webui::LocalizedString kPowerStrings[] = {
       {"calculatingPower", IDS_SETTINGS_POWER_SOURCE_CALCULATING},
       {"powerAdaptiveChargingLabel",
@@ -169,15 +165,11 @@ void PowerSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
       {"powerIdleWhileChargingAriaLabel",
        IDS_SETTINGS_POWER_IDLE_WHILE_CHARGING_ARIA_LABEL},
       {"powerInactiveWhilePluggedInLabel",
-       kIsRevampEnabled
-           ? IDS_OS_SETTINGS_REVAMP_POWER_INACTIVE_WHILE_PLUGGED_IN_LABEL
-           : IDS_SETTINGS_POWER_IDLE_WHILE_CHARGING_LABEL},
+       IDS_OS_SETTINGS_REVAMP_POWER_INACTIVE_WHILE_PLUGGED_IN_LABEL},
       {"powerIdleWhileOnBatteryAriaLabel",
        IDS_SETTINGS_POWER_IDLE_WHILE_ON_BATTERY_ARIA_LABEL},
       {"powerInactiveWhileOnBatteryLabel",
-       kIsRevampEnabled
-           ? IDS_OS_SETTINGS_REVAMP_POWER_INACTIVE_WHILE_ON_BATTERY_LABEL
-           : IDS_SETTINGS_POWER_IDLE_WHILE_ON_BATTERY_LABEL},
+       IDS_OS_SETTINGS_REVAMP_POWER_INACTIVE_WHILE_ON_BATTERY_LABEL},
       {"powerLidShutDownLabel", IDS_SETTINGS_POWER_LID_CLOSED_SHUT_DOWN_LABEL},
       {"powerLidSignOutLabel", IDS_SETTINGS_POWER_LID_CLOSED_SIGN_OUT_LABEL},
       {"powerLidSleepLabel", IDS_SETTINGS_POWER_LID_CLOSED_SLEEP_LABEL},
@@ -215,14 +207,9 @@ int PowerSection::GetSectionNameMessageId() const {
 }
 
 mojom::Section PowerSection::GetSection() const {
-  // Note: This is a subsection that exists under Device or System Preferences.
-  // This section will no longer exist under the Device section once the
-  // OsSettingsRevampWayfinding feature is fully launched.
   // This is not a top-level section and does not have a respective declaration
   // in chromeos::settings::mojom::Section.
-  return ash::features::IsOsSettingsRevampWayfindingEnabled()
-             ? mojom::Section::kSystemPreferences
-             : mojom::Section::kDevice;
+  return mojom::Section::kSystemPreferences;
 }
 
 mojom::SearchResultIcon PowerSection::GetSectionIcon() const {
@@ -230,9 +217,7 @@ mojom::SearchResultIcon PowerSection::GetSectionIcon() const {
 }
 
 const char* PowerSection::GetSectionPath() const {
-  return ash::features::IsOsSettingsRevampWayfindingEnabled()
-             ? mojom::kSystemPreferencesSectionPath
-             : mojom::kDeviceSectionPath;
+  return mojom::kSystemPreferencesSectionPath;
 }
 
 bool PowerSection::LogMetric(mojom::Setting setting, base::Value& value) const {
--- chrome/browser/ui/webui/ash/settings/pages/storage/storage_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/storage/storage_section.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <array>
 
-#include "ash/constants/ash_features.h"
 #include "base/containers/span.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::settings {
 
 namespace mojom {
-using ::chromeos::settings::mojom::kDeviceSectionPath;
 using ::chromeos::settings::mojom::kExternalStorageSubpagePath;
 using ::chromeos::settings::mojom::kStorageSubpagePath;
 using ::chromeos::settings::mojom::kSystemPreferencesSectionPath;
@@ -81,17 +79,13 @@ StorageSection::StorageSection(Profile* profile,
 StorageSection::~StorageSection() = default;
 
 void StorageSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
-  const bool kIsRevampEnabled =
-      ash::features::IsOsSettingsRevampWayfindingEnabled();
-
   webui::LocalizedString kStorageStrings[] = {
       {"storageExternal", IDS_SETTINGS_STORAGE_EXTERNAL},
       {"storageExternalStorageEmptyListHeader",
        IDS_SETTINGS_STORAGE_EXTERNAL_STORAGE_EMPTY_LIST_HEADER},
       {"storageExternalStorageListHeader",
        IDS_SETTINGS_STORAGE_EXTERNAL_STORAGE_LIST_HEADER},
-      {"storageItemApps", kIsRevampEnabled ? IDS_OS_SETTINGS_STORAGE_ITEM_APPS
-                                           : IDS_SETTINGS_STORAGE_ITEM_APPS},
+      {"storageItemApps", IDS_OS_SETTINGS_STORAGE_ITEM_APPS},
       {"storageItemOffline", IDS_SETTINGS_STORAGE_ITEM_OFFLINE},
       {"storageItemAvailable", IDS_SETTINGS_STORAGE_ITEM_AVAILABLE},
       {"storageItemCrostini", IDS_SETTINGS_STORAGE_ITEM_CROSTINI},
@@ -159,14 +153,9 @@ int StorageSection::GetSectionNameMessageId() const {
 }
 
 mojom::Section StorageSection::GetSection() const {
-  // Note: This is a subsection that exists under Device or System Preferences.
-  // This section will no longer exist under the Device section once the
-  // OsSettingsRevampWayfinding feature is fully launched.
   // This is not a top-level section and does not have a respective declaration
   // in chromeos::settings::mojom::Section.
-  return ash::features::IsOsSettingsRevampWayfindingEnabled()
-             ? mojom::Section::kSystemPreferences
-             : mojom::Section::kDevice;
+  return mojom::Section::kSystemPreferences;
 }
 
 mojom::SearchResultIcon StorageSection::GetSectionIcon() const {
@@ -174,9 +163,7 @@ mojom::SearchResultIcon StorageSection::GetSectionIcon() const {
 }
 
 const char* StorageSection::GetSectionPath() const {
-  return ash::features::IsOsSettingsRevampWayfindingEnabled()
-             ? mojom::kSystemPreferencesSectionPath
-             : mojom::kDeviceSectionPath;
+  return mojom::kSystemPreferencesSectionPath;
 }
 
 bool StorageSection::LogMetric(mojom::Setting setting,
