8a7e1ef64806667097205c3c2a342d9406b27d4a
Zhe Li <zheliooo@google.com>
2024-12-18 13:28:28 -0800

    Clean up notification bubble impl
    Bug: 348728489
    Change-Id: I8288dc2a357a7dd78aa90d4f03eaa0de34db0eb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097144
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398207}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/LayerTitleCache.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/LayerTitleCache.java
@@ -84,16 +84,6 @@ public class LayerTitleCache {
                 res.getDimensionPixelSize(R.dimen.tab_title_favicon_end_padding);
         mSharedGroupAvatarPaddingPx =
                 res.getDimensionPixelSize(R.dimen.tablet_shared_group_avatar_padding);
-        mNativeLayerTitleCache =
-                LayerTitleCacheJni.get()
-                        .init(
-                                LayerTitleCache.this,
-                                fadeWidthPx,
-                                faviconStartPaddingPx,
-                                faviconEndPaddingPx,
-                                R.drawable.spinner,
-                                R.drawable.spinner_white,
-                                mResourceManager);
         mFaviconSize = res.getDimensionPixelSize(R.dimen.compositor_tab_title_favicon_size);
         mStandardTitleBitmapFactory = new TitleBitmapFactory(context, false);
         mDarkTitleBitmapFactory = new TitleBitmapFactory(context, true);
@@ -105,8 +95,24 @@ public class LayerTitleCache {
         mBubbleOffset =
                 res.getDimensionPixelSize(R.dimen.compositor_tab_title_favicon_bubble_offset);
         mBubbleBorderColor =
-                TabUiThemeUtil.getTabStripBackgroundColorForActivityState(context, false, false);
+                TabUiThemeUtil.getTabStripBackgroundColorForActivityState(
+                        context, /* isIncognito= */ false, /* isActivityFocused= */ true);
         mBubbleFillColor = TabUiThemeProvider.getTabBubbleFillColor(context);
+        mNativeLayerTitleCache =
+                LayerTitleCacheJni.get()
+                        .init(
+                                LayerTitleCache.this,
+                                fadeWidthPx,
+                                faviconStartPaddingPx,
+                                faviconEndPaddingPx,
+                                R.drawable.spinner,
+                                R.drawable.spinner_white,
+                                mBubbleInnerCircleSize,
+                                mBubbleOuterCircleSize,
+                                mBubbleOffset,
+                                mBubbleFillColor,
+                                mBubbleBorderColor,
+                                mResourceManager);
     }
 
     /** Destroys the native reference. */
@@ -197,12 +203,7 @@ public class LayerTitleCache {
                             title.getFaviconResId(),
                             isDarkTheme,
                             isRtl,
-                            showBubble,
-                            mBubbleInnerCircleSize,
-                            mBubbleOuterCircleSize,
-                            mBubbleOffset,
-                            mBubbleFillColor,
-                            mBubbleBorderColor);
+                            showBubble);
         }
         return titleString;
     }
@@ -391,12 +392,7 @@ public class LayerTitleCache {
                         ResourcesCompat.ID_NULL,
                         false,
                         false,
-                        false,
-                        0,
-                        0,
-                        0,
-                        0,
-                        0);
+                        false);
     }
 
     public void removeGroupTitle(int rootId) {
@@ -499,6 +495,11 @@ public class LayerTitleCache {
                 int faviconEndPadding,
                 int spinnerResId,
                 int spinnerIncognitoResId,
+                int tabBubbleInnerDimension,
+                int tabBubbleOuterDimension,
+                int bubbleOffset,
+                @ColorInt int tabBubbleInnerColor,
+                @ColorInt int tabBubbleOuterColor,
                 ResourceManager resourceManager);
 
         void destroy(long nativeLayerTitleCache);
@@ -511,12 +512,7 @@ public class LayerTitleCache {
                 int faviconResId,
                 boolean isIncognito,
                 boolean isRtl,
-                boolean showBubble,
-                int tabBubbleInnerDimension,
-                int tabBubbleOuterDimension,
-                int bubbleOffset,
-                @ColorInt int tabBubbleInnerColor,
-                @ColorInt int tabBubbleOuterColor);
+                boolean showBubble);
 
         void updateGroupLayer(
                 long nativeLayerTitleCache,
