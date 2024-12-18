25b57ac7d5a5c887052e6c9401c9b6542931034d
Benoît Lizé <lizeb@chromium.org>
2024-12-18 09:59:42 -0800

    [cc] Add about:flags entry for main frame update throttling
    The feature has landed, but we want to allow easy back to back tests on
    some sites, to ease testing. Add an about:flags entry to enable this.
    
    Bug: 379678455
    Change-Id: I16f2ced0088dd02df1e48b1186df828f06f83fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6084997
    Auto-Submit: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398046}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2179,6 +2179,9 @@ extern const char kAutofillThirdPartyModeContentProviderDescription[];
 extern const char kThreadedScrollPreventRenderingStarvationName[];
 extern const char kThreadedScrollPreventRenderingStarvationDescription[];
 
+extern const char kThrottleMainTo60HzName[];
+extern const char kThrottleMainTo60HzDescription[];
+
 extern const char kTintCompositedContentName[];
 extern const char kTintCompositedContentDescription[];
 
