702bd3297a0274fbc8a6ba0b819b18a266900bf8
Fergal Daly <fergal@chromium.org>
2024-12-17 22:06:08 -0800

    Delete the old language detection API.
    Bug: 349927087
    Change-Id: I41458da3cf03e78ec9c25fe3fddea343c3e982d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054825
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397739}

--- third_party/blink/renderer/modules/on_device_translation/translation.h
+++ third_party/blink/renderer/modules/on_device_translation/translation.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 
 namespace blink {
-class LanguageDetector;
 class LanguageTranslator;
 class V8TranslationAvailability;
 
@@ -43,12 +42,6 @@ class Translation final : public ScriptWrappable,
   ScriptPromise<LanguageTranslator> createTranslator(
       ScriptState* script_state,
       TranslationLanguageOptions* options,
-      ExceptionState& exception_state);
-  ScriptPromise<V8TranslationAvailability> canDetect(
-      ScriptState* script_state,
-      ExceptionState& exception_state);
-  ScriptPromise<LanguageDetector> createDetector(
-      ScriptState* script_state,
       ExceptionState& exception_state);
 
  private:
