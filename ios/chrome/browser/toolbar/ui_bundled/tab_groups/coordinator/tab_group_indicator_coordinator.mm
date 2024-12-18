9cf7d493cb03a484b6e5f459193f6fbfdcce81bb
Gauthier Ambard <gambard@chromium.org>
2024-12-18 05:29:23 -0800

    [iOS] Use the Collab Service's Manage flow
    This CL uses the Collab Service to show the manage flow instead of
    directly using ShareKitService.
    
    Bug: 384512074
    Change-Id: Ic104409a0c0438ad2f88ba1f69fd749719c75542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098156
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397866}

--- ios/chrome/browser/toolbar/ui_bundled/tab_groups/coordinator/tab_group_indicator_coordinator.mm
+++ ios/chrome/browser/toolbar/ui_bundled/tab_groups/coordinator/tab_group_indicator_coordinator.mm
@@ -216,7 +216,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                          anchorPoint:anchorPoint];
 }
 
-- (void)shareTabGroup:(const TabGroup*)tabGroup {
+- (void)shareOrManageTabGroup:(const TabGroup*)tabGroup {
   Browser* browser = self.browser;
   collaboration::CollaborationService* collaborationService =
       collaboration::CollaborationServiceFactory::GetForProfile(
