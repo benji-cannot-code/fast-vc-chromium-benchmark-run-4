64226b18a7ae5a95ced9c9eb3a56ff19b016c873
Mike Wittman <wittman@chromium.org>
2024-12-18 10:23:13 -0800

    Reland "[sampling profiler] Rationalize 32/64-bit Android unwinder configuration"
    This is a reland of commit f360e41bdf8de1a33b9e266ddbf96b693930b1af
    
    Fixes the test failures seen in https://crrev.com/c/6096776 and
    simplifies the code to only be concerned with prerequisites for
    arm32.
    
    Original change's description:
    > [sampling profiler] Rationalize 32/64-bit Android unwinder configuration
    >
    > With the move of the libunwinstack library from the dynamic feature
    > module into Chrome code, we now only need to depend on the DFM
    > for the unwind resource, which is only used by the 32-bit Chrome
    > unwinder. This CL effects that change and also restricts the remaining
    > headers and logic to either 32-bit, 64-bit, or both, to reflect the
    > actual architecture-specific dependencies.
    >
    > Bug: 380487894, 375463618
    > Change-Id: I1b9571057e772286834ae7dcbfbe8c8538c650ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6087128
    > Reviewed-by: Rodney Ding <rodneyding@google.com>
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1397038}
    
    Bug: 380487894, 375463618
    Change-Id: I3e0c4f3eebc4607dc7f4fe43c8fd3381dac03053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102033
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398062}

--- chrome/common/profiler/core_unwinders_android_unittest.cc
+++ chrome/common/profiler/core_unwinders_android_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/profiler/core_unwinders.h"
 
 #include "base/command_line.h"
+#include "base/debug/debugging_buildflags.h"
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/profiler/profiler_buildflags.h"
@@ -109,14 +110,15 @@ TEST(UnwindPrerequisitesTest, AreUnwindPrerequisitesAvailable) {
     raw_ptr<UnwindPrerequisitesDelegate> delegate;
     bool are_unwind_prerequisites_expected;
   } test_cases[] = {
-    // Android unwinders require the presence of the unwinder module.
+#if ARM32_UNWINDING_SUPPORTED
+      // 32-bit Chrome Android unwinder requires the presence of the unwinder
+      // module.
     {version_info::Channel::CANARY, &false_mock_delegate, false},
     {version_info::Channel::DEV, &false_mock_delegate, false},
     {version_info::Channel::BETA, &false_mock_delegate, false},
     {version_info::Channel::STABLE, &false_mock_delegate, false},
     {version_info::Channel::UNKNOWN, &false_mock_delegate, false},
 
-#if UNWINDING_SUPPORTED
     {version_info::Channel::CANARY, &true_mock_delegate, true},
     {version_info::Channel::DEV, &true_mock_delegate, true},
     {version_info::Channel::BETA, &true_mock_delegate, true},
@@ -131,20 +133,28 @@ TEST(UnwindPrerequisitesTest, AreUnwindPrerequisitesAvailable) {
     {version_info::Channel::STABLE, &true_mock_delegate, true},
     {version_info::Channel::UNKNOWN, &true_mock_delegate, true},
 #endif  // defined(OFFICIAL_BUILD) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-#else   // UNWINDING_SUPPORTED
-    // Unwinding on any other platforms is not currently supported for Android.
-    {version_info::Channel::CANARY, &true_mock_delegate, false},
-    {version_info::Channel::DEV, &true_mock_delegate, false},
-    {version_info::Channel::BETA, &true_mock_delegate, false},
-    {version_info::Channel::STABLE, &true_mock_delegate, false},
-    {version_info::Channel::UNKNOWN, &true_mock_delegate, false},
-#endif  // UNWINDING_SUPPORTED
+#else   // ARM32_UNWINDING_SUPPORTED
+    {version_info::Channel::CANARY, &false_mock_delegate, true},
+    {version_info::Channel::DEV, &false_mock_delegate, true},
+    {version_info::Channel::BETA, &false_mock_delegate, true},
+    {version_info::Channel::STABLE, &false_mock_delegate, true},
+    {version_info::Channel::UNKNOWN, &false_mock_delegate, true},
+
+    {version_info::Channel::CANARY, &true_mock_delegate, true},
+    {version_info::Channel::DEV, &true_mock_delegate, true},
+    {version_info::Channel::BETA, &true_mock_delegate, true},
+    {version_info::Channel::STABLE, &true_mock_delegate, true},
+    {version_info::Channel::UNKNOWN, &true_mock_delegate, true},
+#endif  // ARM32_UNWINDING_SUPPORTED
   };
 
   for (const auto& test_case : test_cases) {
     EXPECT_EQ(
         AreUnwindPrerequisitesAvailable(test_case.channel, test_case.delegate),
-        test_case.are_unwind_prerequisites_expected);
+        test_case.are_unwind_prerequisites_expected)
+        << "Channel " << static_cast<int>(test_case.channel) << ", "
+        << (test_case.delegate == &false_mock_delegate ? "false_mock_delegate"
+                                                       : "true_mock_delegate");
   }
 }
 
