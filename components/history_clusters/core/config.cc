60a3ccabdffb6a4abd659152c856bc699d94a65b
Ari Chivukula <arichiv@chromium.org>
2024-12-18 10:00:56 -0800

    Clean up stale base::Feature "JourneysPersistCachesToPrefs"
    This has been enabled by default since 2023:
    https://chromium-review.googlesource.com/c/chromium/src/+/4860161
    Let's clean it up!
    
    Fixed: 362247544
    Change-Id: Ife22d8a57117d3c9c43e380fb30a3e2c64a031cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6101737
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398048}

--- components/history_clusters/core/config.cc
+++ components/history_clusters/core/config.cc
@@ -289,9 +289,6 @@ Config::Config() {
     should_show_all_clusters_unconditionally_on_prominent_ui_surfaces =
         base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kShouldShowAllClustersOnProminentUiSurfaces);
-
-    persist_caches_to_prefs =
-        base::FeatureList::IsEnabled(internal::kJourneysPersistCachesToPrefs);
   }
 }
 
--- components/history_clusters/core/history_clusters_service.cc
+++ components/history_clusters/core/history_clusters_service.cc
@@ -107,8 +107,7 @@ HistoryClustersService::HistoryClustersService(
     TemplateURLService* template_url_service,
     optimization_guide::OptimizationGuideDecider* optimization_guide_decider,
     PrefService* prefs)
-    : persist_caches_to_prefs_(GetConfig().persist_caches_to_prefs),
-      is_journeys_feature_flag_enabled_(
+    : is_journeys_feature_flag_enabled_(
           GetConfig().is_journeys_enabled_no_locale_check &&
           IsApplicationLocaleSupportedByJourneys(application_locale)),
       history_service_(history_service),
@@ -553,7 +552,7 @@ void HistoryClustersService::PopulateClusterKeywordCache(
 }
 
 void HistoryClustersService::LoadCachesFromPrefs() {
-  if (!pref_service_ || !persist_caches_to_prefs_) {
+  if (!pref_service_) {
     return;
   }
 
@@ -588,7 +587,7 @@ void HistoryClustersService::LoadCachesFromPrefs() {
 }
 
 void HistoryClustersService::WriteShortCacheToPrefs() {
-  if (!pref_service_ || !persist_caches_to_prefs_) {
+  if (!pref_service_) {
     return;
   }
 
@@ -606,7 +605,7 @@ void HistoryClustersService::WriteShortCacheToPrefs() {
 }
 
 void HistoryClustersService::WriteAllCacheToPrefs() {
-  if (!pref_service_ || !persist_caches_to_prefs_) {
+  if (!pref_service_) {
     return;
   }
 
