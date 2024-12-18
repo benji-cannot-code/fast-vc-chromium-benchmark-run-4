a05971bf8c01d519c1a698af0e2f6958b17324de
thefrog <thefrog@chromium.org>
2024-12-18 09:55:08 -0800

    Revert "Remove "CHECK" version of LogMessage constructor"
    This reverts commit 1eb84b989d4cf7c8145fefe2b1b390a31c2e19a3.
    
    Reason for revert: To revert https://crrev.com/c/6095761. (Please see
    crbug.com/384553537 for more details.)
    
    Original change's description:
    > Remove "CHECK" version of LogMessage constructor
    >
    > This reimplements a few macros to use base::Location-based CheckErrors
    > which better aligns with other CHECK macros.
    >
    > Bug: None
    > Change-Id: Ia1390e364bb8b56a739ccb6890eccae1553522dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088726
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1397694}
    
    Bug: 384553537
    Change-Id: Ia83796d2280e86da3b5ea55ed1710e5f364d8c20
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105075
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: thefrog <thefrog@chromium.org>
    Owners-Override: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398043}

--- third_party/blink/renderer/core/editing/commands/editing_state.h
+++ third_party/blink/renderer/core/editing/commands/editing_state.h
@@ -70,8 +70,7 @@ class NoEditingAbortChecker final {
   STACK_ALLOCATED();
 
  public:
-  explicit NoEditingAbortChecker(
-      const base::Location& location = base::Location::Current());
+  NoEditingAbortChecker(const char* file, int line);
   NoEditingAbortChecker(const NoEditingAbortChecker&) = delete;
   NoEditingAbortChecker& operator=(const NoEditingAbortChecker&) = delete;
   ~NoEditingAbortChecker();
@@ -80,7 +79,8 @@ class NoEditingAbortChecker final {
 
  private:
   EditingState editing_state_;
-  const base::Location location_;
+  const char* const file_;
+  int const line_;
 };
 
 // If a function with EditingState* argument should not be aborted,
@@ -88,7 +88,8 @@ class NoEditingAbortChecker final {
 //    fooFunc(...., ASSERT_NO_EDITING_ABORT);
 // It causes an assertion failure If DCHECK_IS_ON() and the function was aborted
 // unexpectedly.
-#define ASSERT_NO_EDITING_ABORT (NoEditingAbortChecker().GetEditingState())
+#define ASSERT_NO_EDITING_ABORT \
+  (NoEditingAbortChecker(__FILE__, __LINE__).GetEditingState())
 #else
 #define ASSERT_NO_EDITING_ABORT (IgnorableEditingAbortState().GetEditingState())
 #endif
