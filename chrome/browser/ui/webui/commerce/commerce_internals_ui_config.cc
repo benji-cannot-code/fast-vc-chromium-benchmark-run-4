048553d18b7f62043720ddb5340d170dc103aac3
Teresa Mao <temao@chromium.org>
2024-12-18 01:20:40 -0800

    WebUI: Move chrome/browser/ui/webui/webui_util.* to ui/webui.
    - Introduce //ui/webui/webui_util.* and move all functions to the new
      location. Note that the existing
      chrome/browser/ui/webui/webui_util.* will be removed once all the uses
      have been migrated to the new files.
    - Delegate the implementation of chrome/browser/ui/webui/webui_util.h
      to ui/webui/webui_util.h.
    - Update CommerceInternalsUIConfig and HistoryClustersInternalsUIConfig
      to use SetupWebUIDataSource() from ui/webui/webui_util.h and eliminate
      the wrapper callback in these two WebUI configs.
    - Replace outdated IS_CHROMEOS_LACROS with IS_CHROMEOS.
    
    The motivation for this migration is to allow WebUIs residing outside of
    chrome/ to leverage functionalities in webui_util.cc.
    
    Bug: 40745251
    Change-Id: I919e4d925804ca9aad67d3e3ab5c3be4138315a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089862
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397788}

--- chrome/browser/ui/webui/commerce/commerce_internals_ui_config.cc
+++ chrome/browser/ui/webui/commerce/commerce_internals_ui_config.cc
@@ -7,22 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/commerce/shopping_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/webui/webui_util.h"
 #include "components/commerce/content/browser/commerce_internals_ui.h"
 #include "components/commerce/core/commerce_constants.h"
-#include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_ui_data_source.h"
 
 namespace commerce {
-
-void SetUpWebUIDataSource(content::WebUI* web_ui,
-                          const char* web_ui_host,
-                          base::span<const webui::ResourcePath> resources,
-                          int default_resource) {
-  content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
-      web_ui->GetWebContents()->GetBrowserContext(), web_ui_host);
-  webui::SetupWebUIDataSource(source, resources, default_resource);
-}
 
 CommerceInternalsUIConfig::CommerceInternalsUIConfig()
     : WebUIConfig(content::kChromeUIScheme,
@@ -36,8 +24,6 @@ CommerceInternalsUIConfig::CreateWebUIController(content::WebUI* web_ui,
   Profile* profile = Profile::FromWebUI(web_ui);
   return std::make_unique<CommerceInternalsUI>(
       web_ui,
-      base::BindOnce(&SetUpWebUIDataSource, web_ui,
-                     commerce::kChromeUICommerceInternalsHost),
       commerce::ShoppingServiceFactory::GetForBrowserContext(profile));
 }
 
--- ui/webui/webui_util.cc
+++ ui/webui/webui_util.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/webui_util.h"
+#include "ui/webui/webui_util.h"
 
 #include <string>
 
@@ -50,7 +50,7 @@ void SetJSModuleDefaults(content::WebUIDataSource* source) {
         "img-src chrome://resources chrome://theme chrome://image "
         "chrome://favicon2 chrome://app-icon chrome://extension-icon "
         "chrome://fileicon "
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
         "chrome://chromeos-asset chrome://userimage "
 #endif
         "blob: data: 'self';");
