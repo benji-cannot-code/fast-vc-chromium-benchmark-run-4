38728b7b046799311acfd705803941dfeb11d408
Aga Wronska <agawronska@chromium.org>
2024-12-18 12:24:27 -0800

    pac: Skip parent access code in re-auth flow
    First phase of deprecating parent access code in re-auth flow.
    The code is no longer needed, because current re-auth flow does
    not allow to add a new user to the device.
    Request for the code during re-auth was also causing issues with
    code configuration updates,
    
    Bug: b:382497324
    Change-Id: Ia5e74aef7445a7e2a6c6408b7043583169e742be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098925
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398156}

--- chrome/browser/ash/child_accounts/parent_access_code/parent_access_service_browsertest.cc
+++ chrome/browser/ash/child_accounts/parent_access_code/parent_access_service_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ash/child_accounts/parent_access_code/parent_access_service.h"
+
 #include <map>
 #include <memory>
 #include <string>
@@ -12,13 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/json/json_writer.h"
 #include "chrome/browser/ash/child_accounts/parent_access_code/config_source.h"
-#include "chrome/browser/ash/child_accounts/parent_access_code/parent_access_service.h"
 #include "chrome/browser/ash/child_accounts/parent_access_code/parent_access_test_utils.h"
+#include "chrome/browser/ash/login/test/feature_parameter_interface.h"
 #include "chrome/browser/ash/login/test/logged_in_user_mixin.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/ash/policy/core/user_policy_test_helper.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "components/account_id/account_id.h"
 #include "components/prefs/pref_service.h"
@@ -26,8 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace ash {
-namespace parent_access {
+namespace ash::parent_access {
+
+const auto kAllFeatureStates = FeatureAsParameterInterface<1>::Generator(
+    {&::features::kSkipParentAccessCodeForReauth});
 
 // Stores information about results of the access code validation.
 struct CodeValidationResults {
@@ -66,7 +71,9 @@ class TestParentAccessServiceObserver : public ParentAccessService::Observer {
   const AccountId account_id_;
 };
 
-class ParentAccessServiceTest : public MixinBasedInProcessBrowserTest {
+class ParentAccessServiceTest
+    : public MixinBasedInProcessBrowserTest,
+      public ::testing::WithParamInterface<FeatureStateArray<1>> {
  public:
   ParentAccessServiceTest()
       : test_observer_(std::make_unique<TestParentAccessServiceObserver>(
@@ -134,7 +141,7 @@ class ParentAccessServiceTest : public MixinBasedInProcessBrowserTest {
   std::unique_ptr<TestParentAccessServiceObserver> test_observer_;
 };
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoConfigAvailable) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, NoConfigAvailable) {
   auto test_value = test_values_.begin();
   EXPECT_EQ(ParentCodeValidationResult::kNoConfig,
             ValidateAccessCode(test_value->second, test_value->first));
@@ -142,7 +149,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoConfigAvailable) {
   ExpectResults(0, 1);
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoValidConfigAvailable) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, NoValidConfigAvailable) {
   std::vector<AccessCodeConfig> old_configs;
   old_configs.emplace_back(GetInvalidTestConfig());
   UpdatePolicy(PolicyFromConfigs(GetInvalidTestConfig(), GetInvalidTestConfig(),
@@ -155,7 +162,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoValidConfigAvailable) {
   ExpectResults(0, 1);
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, ValidationWithFutureConfig) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, ValidationWithFutureConfig) {
   std::vector<AccessCodeConfig> old_configs;
   old_configs.emplace_back(GetInvalidTestConfig());
   UpdatePolicy(PolicyFromConfigs(GetDefaultTestConfig(), GetInvalidTestConfig(),
@@ -168,7 +175,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, ValidationWithFutureConfig) {
   ExpectResults(1, 0);
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, ValidationWithCurrentConfig) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, ValidationWithCurrentConfig) {
   std::vector<AccessCodeConfig> old_configs;
   old_configs.emplace_back(GetInvalidTestConfig());
   UpdatePolicy(PolicyFromConfigs(GetInvalidTestConfig(), GetDefaultTestConfig(),
@@ -181,7 +188,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, ValidationWithCurrentConfig) {
   ExpectResults(1, 0);
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, ValidationWithOldConfig) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, ValidationWithOldConfig) {
   std::vector<AccessCodeConfig> old_configs;
   old_configs.emplace_back(GetInvalidTestConfig());
   old_configs.emplace_back(GetDefaultTestConfig());
@@ -195,7 +202,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, ValidationWithOldConfig) {
   ExpectResults(1, 0);
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, MultipleValidationAttempts) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, MultipleValidationAttempts) {
   AccessCodeValues::iterator test_value = test_values_.begin();
 
   // No config - validation should fail.
@@ -221,7 +228,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, MultipleValidationAttempts) {
   ExpectResults(test_values_.size(), 2);
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoObserver) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, NoObserver) {
   ParentAccessService::Get().RemoveObserver(test_observer_.get());
 
   UpdatePolicy(
@@ -234,7 +241,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoObserver) {
   ExpectResults(0, 0);
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoAccountId) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, NoAccountId) {
   ParentAccessService::Get().RemoveObserver(test_observer_.get());
 
   UpdatePolicy(
@@ -247,7 +254,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, NoAccountId) {
                 EmptyAccountId(), test_value->second, test_value->first));
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, InvalidAccountId) {
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest, InvalidAccountId) {
   ParentAccessService::Get().RemoveObserver(test_observer_.get());
 
   UpdatePolicy(
@@ -261,7 +268,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest, InvalidAccountId) {
                 other_child, test_value->second, test_value->first));
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest,
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest,
                        ChildDeviceOwner_IsApprovalRequired) {
   auto* const user_manager =
       static_cast<FakeChromeUserManager*>(user_manager::UserManager::Get());
@@ -287,8 +294,8 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest,
   // Login screen.
   EXPECT_TRUE(
       ParentAccessService::IsApprovalRequired(SupervisedAction::kAddUser));
-  EXPECT_TRUE(
-      ParentAccessService::IsApprovalRequired(SupervisedAction::kReauth));
+  EXPECT_EQ(::features::IsParentAccessCodeForReauthEnabled(),
+            ParentAccessService::IsApprovalRequired(SupervisedAction::kReauth));
   // In session, because child user is logged in the test fixture.
   EXPECT_TRUE(ParentAccessService::IsApprovalRequired(
       SupervisedAction::kUnlockTimeLimits));
@@ -298,7 +305,7 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest,
       SupervisedAction::kUpdateTimezone));
 }
 
-IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest,
+IN_PROC_BROWSER_TEST_P(ParentAccessServiceTest,
                        RegularDeviceOwner_IsApprovalRequired) {
   auto* const user_manager =
       static_cast<FakeChromeUserManager*>(user_manager::UserManager::Get());
@@ -337,5 +344,9 @@ IN_PROC_BROWSER_TEST_F(ParentAccessServiceTest,
       SupervisedAction::kUpdateTimezone));
 }
 
-}  // namespace parent_access
-}  // namespace ash
+INSTANTIATE_TEST_SUITE_P(All,
+                         ParentAccessServiceTest,
+                         testing::ValuesIn(kAllFeatureStates),
+                         FeatureAsParameterInterface<1>::ParamInfoToString);
+
+}  // namespace ash::parent_access
