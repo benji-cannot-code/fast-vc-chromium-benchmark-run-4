53aa306e14b397868505598a529a728be5bfdece
Henrique Nakashima <hnakashima@chromium.org>
2024-12-18 13:17:23 -0800

    [Android] @NullMarked: media_router/b/a:cast_options_provider_java
    Bug: 40657579
    Change-Id: Id4e671e65d49a0f70aa1d1bf50c1a2ce50c879de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6106676
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398194}

--- components/media_router/browser/android/java/src/org/chromium/components/media_router/caf/CastOptionsProvider.java
+++ components/media_router/browser/android/java/src/org/chromium/components/media_router/caf/CastOptionsProvider.java
@@ -12,9 +12,13 @@ import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
 
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+
 import java.util.List;
 
 /** {@link OptionsProvider} implementation for Chrome MR. */
+@NullMarked
 public class CastOptionsProvider implements OptionsProvider {
     @Override
     public CastOptions getCastOptions(Context context) {
@@ -28,7 +32,7 @@ public class CastOptionsProvider implements OptionsProvider {
     }
 
     @Override
-    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+    public @Nullable List<SessionProvider> getAdditionalSessionProviders(Context context) {
         return null;
     }
 }
