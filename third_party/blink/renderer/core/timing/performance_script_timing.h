55bd9667f5c6cd8097e759650098688d546d8f40
Noam Rosenthal <nrosenthal@chromium.org>
2024-12-18 07:46:08 -0800

    LoAF: convert monotonic time to DOM time at entry creation
    This corresponds to how other entries work and to the spec, where the
    monotonic->double conversion happens at initialization.
    
    This should not be web-observable, except for in edge cases where two
    calls to the API would produce different results because of coarsening
    randomness.
    
    Bug: 384756620
    Change-Id: Ifef181a39fffe49f1fb3ff81fdae332ffb332280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097466
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397946}

--- third_party/blink/renderer/core/timing/performance_script_timing.h
+++ third_party/blink/renderer/core/timing/performance_script_timing.h
@@ -37,7 +37,7 @@ class PerformanceScriptTiming final : public PerformanceEntry {
   const AtomicString& entryType() const override;
   PerformanceEntryType EntryTypeEnum() const override;
 
-  DOMHighResTimeStamp executionStart() const;
+  DOMHighResTimeStamp executionStart() const { return execution_start_; }
   DOMHighResTimeStamp forcedStyleAndLayoutDuration() const;
   DOMHighResTimeStamp pauseDuration() const;
   LocalDOMWindow* window() const;
@@ -52,10 +52,9 @@ class PerformanceScriptTiming final : public PerformanceEntry {
  private:
   void BuildJSONValue(V8ObjectBuilder&) const override;
   DOMHighResTimeStamp ToMonotonicTime(base::TimeTicks) const;
-  base::TimeTicks time_origin_;
-  bool cross_origin_isolated_capability_;
   Member<ScriptTimingInfo> info_;
   V8ScriptWindowAttribution::Enum window_attribution_;
+  DOMHighResTimeStamp execution_start_;
 };
 
 }  // namespace blink
