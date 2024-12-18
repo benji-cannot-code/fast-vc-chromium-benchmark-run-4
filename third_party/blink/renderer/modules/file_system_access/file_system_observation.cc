6123ccba11eb5c435e27b5104f3a1f18c9f4b7bf
Nathan Memmott <memmott@chromium.org>
2024-12-18 11:47:37 -0800

    Change Observers: Make FileSystemChangeRecord a dictionary
    It is still being discussed on the spec PR whether the
    FileSystemChangeRecord should be a dictionary or an interface. To leave
    our options open, this switches it to a dictionary for now since we can
    switch to an interface without breaking anything but not vice versa.
    
    Fixed: 378924423
    Change-Id: I73427d32b132cdf4a0af518be775220b3c0792b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099067
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398122}

--- third_party/blink/renderer/modules/file_system_access/file_system_observation.cc
+++ third_party/blink/renderer/modules/file_system_access/file_system_observation.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/file_system_access/file_system_observation.h"
 
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/modules/file_system_access/file_system_change_record.h"
 #include "third_party/blink/renderer/modules/file_system_access/file_system_handle.h"
 #include "third_party/blink/renderer/modules/file_system_access/file_system_observation_collection.h"
 #include "third_party/blink/renderer/modules/file_system_access/file_system_observer.h"
