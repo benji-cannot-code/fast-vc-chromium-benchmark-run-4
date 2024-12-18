a368526ac7524661c7483436a5e4712c979380cc
Jérôme Lebel <jlebel@chromium.org>
2024-12-18 06:18:46 -0800

    [iOS] Remove -AuthenticationFlow.userDecisionCompletion
    -AuthenticationFlow.userDecisionCompletion property is not used
    anymore.
    
    Change-Id: Id2cee75be5133cd4f68e69ebdf0fea0eda9ff90c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105710
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397890}

--- ios/chrome/browser/ui/authentication/authentication_flow.mm
+++ ios/chrome/browser/ui/authentication/authentication_flow.mm
@@ -480,11 +480,7 @@ BOOL IsIdentityInCoreAccountInfos(
 }
 
 - (void)multiProfileSignIn {
-  if (self.userDecisionCompletion) {
-    self.userDecisionCompletion();
-  }
   ProfileIOS* profile = [self originalProfile];
-
   signin::IdentityManager* identityManager =
       IdentityManagerFactory::GetForProfile(profile);
   ChromeAccountManagerService* accountManagerService =
