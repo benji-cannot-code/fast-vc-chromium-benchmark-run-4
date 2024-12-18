3fce49707ec0f34993997e824e6ba67abf274ea4
Thomas Lukaszewicz <tluk@chromium.org>
2024-12-18 11:46:25 -0800

    [ntp] Support NTP loads in app contexts
    Currently it is possible for NTP loads to occur in app contexts.
    
    Consequently it is possible for NTP loads to start without a hosting
    TabModel, and for the NTP to be moved between window types (normal<>app
    browser transitions).
    
    This CL ensures the NTP will not crash and correctly function in both
    of the above mentioned cases. The CustomizeChromeSidePanelController
    tab feature has also been transitioned to leverage the new WebUI
    embedding context utils.
    
    Handling for these the above cases of absent and changing TabModels
    can be removed once the NTP is disallowed from loading in app
    browsers.
    
    Bug: 378475391
    Change-Id: Ibf466cf2ee29e8f2f1ec4c1f5367470a6bd4dbcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040425
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398120}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search/background/ntp_custom_background_service_observer.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_observer.h"
-#include "chrome/browser/ui/tabs/public/tab_interface.h"
 #include "chrome/browser/ui/webui/metrics_reporter/metrics_reporter.h"
 #include "chrome/browser/ui/webui/new_tab_page/new_tab_page.mojom.h"
 #include "chrome/common/webui_url_constants.h"
@@ -251,10 +250,6 @@ class NewTabPageUI
   // Called when the NTP (re)loads. Sets mutable load time data.
   void OnLoad();
 
-  // Called when the tab will be detached.
-  void TabWillDetach(tabs::TabInterface* tab,
-                     tabs::TabInterface::DetachReason reason);
-
   std::unique_ptr<NewTabPageHandler> page_handler_;
   mojo::Receiver<new_tab_page::mojom::PageHandlerFactory>
       page_factory_receiver_;
@@ -278,7 +273,6 @@ class NewTabPageUI
   std::unique_ptr<page_image_service::ImageServiceHandler>
       image_service_handler_;
   raw_ptr<Profile> profile_;
-  raw_ptr<tabs::TabInterface> tab_;
   raw_ptr<ThemeService> theme_service_;
   raw_ptr<NtpCustomBackgroundService> ntp_custom_background_service_;
   base::ScopedObservation<NtpCustomBackgroundService,
@@ -295,9 +289,6 @@ class NewTabPageUI
   std::unique_ptr<MicrosoftAuthPageHandler> microsoft_auth_handler_;
   std::unique_ptr<OutlookCalendarPageHandler> outlook_calendar_handler_;
   PrefChangeRegistrar pref_change_registrar_;
-
-  // Holds subscriptions for TabInterface callbacks.
-  std::vector<base::CallbackListSubscription> tab_subscriptions_;
 
   base::WeakPtrFactory<NewTabPageUI> weak_ptr_factory_{this};
 
