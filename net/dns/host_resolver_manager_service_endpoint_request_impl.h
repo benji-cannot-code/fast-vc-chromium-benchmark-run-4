871867e812985740aefc7f780df9f5e300be17c2
Kenichi Ishibashi <bashi@chromium.org>
2024-12-18 13:12:17 -0800

    dns: Refactor HostResolverManager::ServiceEndpointRequestImpl
    This is a preparation for adding a feature to allow using stale DNS
    results by using ServiceEndpointRequest:
    * Add a state for resolving locally
    * Add a helper method to convert HostCache::Entry to ServiceEndpoints
    
    No behavior changes.
    
    Bug: 383174960
    
    Change-Id: I0e4fd71c34676f3e99ff1cce03e773f9c21c654a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088517
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398190}

--- net/dns/host_resolver_manager_service_endpoint_request_impl.h
+++ net/dns/host_resolver_manager_service_endpoint_request_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_DNS_HOST_RESOLVER_MANAGER_SERVICE_ENDPOINT_REQUEST_IMPL_H_
 #define NET_DNS_HOST_RESOLVER_MANAGER_SERVICE_ENDPOINT_REQUEST_IMPL_H_
 
+#include <deque>
 #include <optional>
 #include <set>
 #include <string>
@@ -80,12 +81,14 @@ class HostResolverManager::ServiceEndpointRequestImpl
     kNone,
     kCheckIPv6Reachability,
     kCheckIPv6ReachabilityComplete,
+    kDoResolveLocally,
     kStartJob,
   };
 
   int DoLoop(int rv);
   int DoCheckIPv6Reachability();
   int DoCheckIPv6ReachabilityComplete(int rv);
+  int DoResolveLocally();
   int DoStartJob();
 
   void OnIOComplete(int rv);
@@ -125,6 +128,11 @@ class HostResolverManager::ServiceEndpointRequestImpl
 
   // Set when the endpoint results are finalized.
   std::optional<FinalizedResult> finalized_result_;
+
+  // These fields are calculated by DoResolveLocally() and consumed by
+  // DoStartJob().
+  std::optional<JobKey> job_key_;
+  std::deque<TaskType> tasks_;
 
   // Set when a job is associated with `this`. Must be valid unless
   // `resolve_context_` becomes invalid. Cleared when the endpoints are
