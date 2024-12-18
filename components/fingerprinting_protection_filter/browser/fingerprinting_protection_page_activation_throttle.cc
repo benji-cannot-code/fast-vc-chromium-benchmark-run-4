81f691afd464b43a657cbceb8dc5eab1e5d0da3a
Adam Abeshouse <aabeshouse@google.com>
2024-12-18 11:25:40 -0800

    [FPF] Disable FPF activation based on breakage exception
    In previous CLs, we have implemented a feature flag param,
    `refresh_heuristic_exception_threshold`, which, when nonzero,
    causes updating a profile pref when a user refreshes a site
    multiple times in the same WebContents (tab). With this CL,
    we complete the implementation of that feature by disabling
    FPF on that site in that case.
    
    UMAs to track latency and usage of the feature will be added
    in an upcoming CL.
    
    Bug: 372669423
    Change-Id: I61bf2cac1c94dfef759b89d4b232a79ce705f83e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5966957
    Commit-Queue: Adam Abeshouse <aabeshouse@google.com>
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Martin Verde <thesalsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398105}

--- components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.cc
+++ components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/fingerprinting_protection_filter/browser/fingerprinting_protection_web_contents_helper.h"
+#include "components/fingerprinting_protection_filter/common/fingerprinting_protection_breakage_exception.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_constants.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.h"
 #include "components/subresource_filter/content/shared/browser/utils.h"
@@ -73,6 +74,14 @@ FingerprintingProtectionPageActivationThrottle::GetActivation() const {
     // Feature flag enabled, but disabled by feature param.
     return {.level = ActivationLevel::kDisabled,
             .decision = ActivationDecision::ACTIVATION_DISABLED};
+  }
+
+  if (features::IsFingerprintingProtectionRefreshHeuristicExceptionEnabled(
+          is_incognito_) &&
+      HasBreakageException(navigation_handle()->GetURL(), *prefs_)) {
+    // Disabled by breakage exception.
+    return {.level = ActivationLevel::kDisabled,
+            .decision = ActivationDecision::URL_ALLOWLISTED};
   }
 
   if (features::kActivationLevel.Get() == ActivationLevel::kDryRun) {
