d3c550b9c60f5d2c21b5d1da44c552606cbc3333
Mikel Astiz <mastiz@chromium.org>
2024-12-18 14:54:06 -0800

    Update tests to avoid implicit conversion between GaiaId and std::string
    Class GaiaId was introduced to improve type safety and avoid strings
    being used, which is more error-prone.
    
    Temporarily, implicit conversion between GaiaId and std::string was
    allowed, until all occurrences relying on it are migrated. This patch
    updates tests under chrome/browser/ash/plugin_vm.
    
    This CL was uploaded by git cl split.
    
    R=joelhockey@chromium.org
    
    Bug: 380416867
    Change-Id: I0cafa2e8feb53de761c5fc1937a05779311822fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105634
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398263}

--- chrome/browser/ash/plugin_vm/plugin_vm_test_helper.cc
+++ chrome/browser/ash/plugin_vm/plugin_vm_test_helper.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "components/user_manager/scoped_user_manager.h"
+#include "google_apis/gaia/gaia_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace plugin_vm {
@@ -158,7 +159,7 @@ void PluginVmTestHelper::SetPolicyRequirementsToAllowPluginVm() {
 void PluginVmTestHelper::SetUserRequirementsToAllowPluginVm() {
   // User for the profile should be affiliated with the device.
   const AccountId account_id(AccountId::FromUserEmailGaiaId(
-      testing_profile_->GetProfileUserName(), "id"));
+      testing_profile_->GetProfileUserName(), GaiaId("id")));
   auto user_manager = std::make_unique<ash::FakeChromeUserManager>();
   auto* user = user_manager->AddUserWithAffiliationAndTypeAndProfile(
       account_id, true, user_manager::UserType::kRegular, testing_profile_);
