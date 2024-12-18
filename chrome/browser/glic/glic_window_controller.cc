3d1de9d2109a45e8ae3397840df43f99d08b18d6
Sana Akbani <sanaakbani@google.com>
2024-12-18 11:57:59 -0800

    Fix null glic widget when calling AttachToBrowser
    Bug: 380094949
    Change-Id: Ieaa03b330fb89ac99335a052e64321489aa16c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099756
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398132}

--- chrome/browser/glic/glic_window_controller.cc
+++ chrome/browser/glic/glic_window_controller.cc
@@ -97,6 +97,7 @@ void GlicWindowController::Show(views::View* glic_button_view) {
 
   int padding;
   gfx::Point top_right_point;
+  bool should_attach_to_browser = false;
 
   if (!glic_button_view) {
     // Right now this only detects whether the glic widget is summoned from the
@@ -111,6 +112,7 @@ void GlicWindowController::Show(views::View* glic_button_view) {
     // window.
     top_right_point = GetTopRightPositionForAttachedWindow(glic_button_view);
     padding = GetLayoutConstant(TAB_STRIP_PADDING);
+    should_attach_to_browser = true;
   }
 
   widget_ = glic::GlicView::CreateWidget(
@@ -120,6 +122,16 @@ void GlicWindowController::Show(views::View* glic_button_view) {
   glic_widget_observer_ =
       std::make_unique<GlicWidgetObserver>(this, widget_.get());
   widget_->Show();
+
+  if (should_attach_to_browser) {
+    views::Widget* glic_button_widget = glic_button_view->GetWidget();
+    Browser* browser =
+        chrome::FindBrowserWithWindow(glic_button_widget->GetNativeWindow());
+    AttachToBrowser(browser, glic_button_widget);
+  } else {
+    MaybeCreateHolderWindowAndReparent();
+  }
+
   window_event_observer_ =
       std::make_unique<WindowEventObserver>(this, GetGlicView());
   // Set the draggable area to the top bar of the window, by default.
@@ -139,10 +151,6 @@ gfx::Point GlicWindowController::GetTopRightPositionForAttachedWindow(
   // point of the button.
   gfx::Point top_right_bounds =
       glic_button_view->GetBoundsInScreen().top_right();
-  views::Widget* glic_button_widget = glic_button_view->GetWidget();
-  Browser* browser =
-      chrome::FindBrowserWithWindow(glic_button_widget->GetNativeWindow());
-  AttachToBrowser(browser, glic_button_widget);
 
   return top_right_bounds;
 }
