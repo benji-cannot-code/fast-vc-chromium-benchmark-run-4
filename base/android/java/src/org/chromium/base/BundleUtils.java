a2d8bd00ae3381a6c1d293671cdf956bd70d11e4
Andrew Grieve <agrieve@chromium.org>
2024-12-18 11:17:47 -0800

    Android: Remove @NullUnmarked from BundleUtils.java
    Missed these on the first pass...
    
    Bug: 40657579
    Change-Id: I5402ee5b104c3d646c80ec74c49e639ef30413d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102133
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1398095}

--- base/android/java/src/org/chromium/base/BundleUtils.java
+++ base/android/java/src/org/chromium/base/BundleUtils.java
@@ -29,7 +29,6 @@ import org.jni_zero.JniType;
 
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
-import org.chromium.build.annotations.NullUnmarked;
 import org.chromium.build.annotations.Nullable;
 
 import java.lang.reflect.Field;
@@ -85,7 +84,6 @@ public class BundleUtils {
         ResettersForTesting.register(() -> sIsBundle = oldVal);
     }
 
-    @NullUnmarked
     @RequiresApi(api = Build.VERSION_CODES.O)
     private static @Nullable String getSplitApkPath(String splitName) {
         ApplicationInfo appInfo = ContextUtils.getApplicationContext().getApplicationInfo();
@@ -94,7 +92,7 @@ public class BundleUtils {
             return null;
         }
         int idx = Arrays.binarySearch(splitNames, splitName);
-        return idx < 0 ? null : assumeNonNull(appInfo.splitSourceDirs)[idx];
+        return idx < 0 ? null : appInfo.splitSourceDirs[idx];
     }
 
     /**
@@ -346,7 +344,6 @@ public class BundleUtils {
     private static class SplitCompatClassLoader extends ClassLoader {
         private static final String TAG = "SplitCompatClassLoader";
 
-        @NullUnmarked
         public SplitCompatClassLoader() {
             // The chrome split classloader if the chrome split exists, otherwise
             // the base module class loader.
