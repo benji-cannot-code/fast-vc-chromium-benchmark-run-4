c08eac43b93026fd96805c6106a99abb3c2cadfe
Michael Wilson <mjwilson@chromium.org>
2024-12-18 15:31:37 -0800

    [WebAudio] Don't pull more data than is in the FIFO when in bypass mode
    The call to Render() may be interrupted if the device is stopping.  In
    this case, we can report an underrun when we try to pull the requested
    amount of data.  But it's not really a performance-related underrun,
    so instead we should only return the data that is available.
    
    Bug: 376672971
    Change-Id: If45fe9f3727bf4aac4a52f9ed8f9b5beba587b24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089758
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398285}

--- third_party/blink/renderer/platform/audio/audio_destination.h
+++ third_party/blink/renderer/platform/audio/audio_destination.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/audio/audio_destination_uma_reporter.h"
 #include "third_party/blink/renderer/platform/audio/audio_io_callback.h"
 #include "third_party/blink/renderer/platform/audio/media_multi_channel_resampler.h"
+#include "third_party/blink/renderer/platform/audio/push_pull_fifo.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -147,6 +148,10 @@ class PLATFORM_EXPORT AudioDestination final
   // RealtimeAudioDestinationHandler::SetSinkDescriptor, which can be invoked
   // from the constructor of AudioContext and AudioContext.setSinkId() method.
   media::OutputDeviceStatus MaybeCreateSinkAndGetStatus();
+
+  const PushPullFIFOStateForTest GetPushPullFIFOStateForTest() {
+    return fifo_->GetStateForTest();
+  }
 
  private:
   explicit AudioDestination(AudioIOCallback&,
