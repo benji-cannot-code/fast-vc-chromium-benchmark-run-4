c36baecb18b355fb6b9e13da395f21699e8f9e97
Etienne Pierre-doray <etiennep@chromium.org>
2024-12-19 09:59:37 -0800

    [tracing] Delete unused android code
    Bug: 40833600
    Change-Id: Ie386b7cabbbb0db31d5540ff8b5a8b5d1b09091b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108089
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398678}

--- base/trace_event/memory_dump_manager.cc
+++ base/trace_event/memory_dump_manager.cc
@@ -40,10 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_ANDROID)
 #include "base/trace_event/java_heap_dump_provider_android.h"
 
-#if BUILDFLAG(CAN_UNWIND_WITH_CFI_TABLE)
-#include "base/trace_event/cfi_backtrace_android.h"
-#endif
-
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
