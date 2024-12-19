440114b9cbbead6d5d9344300ca320af9e93a1d7
François Beaufort <beaufort.francois@gmail.com>
2024-12-19 09:51:19 -0800

    webgpu: Add experimental GPUAdapter featureLevel attribute
    This CL adds the GPUAdapter featureLevel attribute behind the
    WebGPU Experimental Features flag, disabled by default.
    
    Spec PR: https://github.com/gpuweb/gpuweb/pull/5012
    
    Bug: 366151404, 382291443
    Change-Id: Iea06fc16ab4ae3cda59acddf78e14281f32b8f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088045
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1398673}

--- third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
@@ -148,6 +148,8 @@ GPUAdapter::GPUAdapter(
   adapter_type_ = info.adapterType;
   backend_type_ = info.backendType;
   is_compatibility_mode_ = info.compatibilityMode;
+  // TODO(crbug.com/382291443): Report feature level from wgpu::Adapter.
+  feature_level_ = info.compatibilityMode ? "compatibility" : "core";
 
   // TODO(crbug.com/359418629): Report xr compatibility in GetInfo()
   is_xr_compatible_ = options->xrCompatible();
@@ -248,6 +250,10 @@ bool GPUAdapter::SupportsMultiPlanarFormats() const {
 
 bool GPUAdapter::isCompatibilityMode() const {
   return is_compatibility_mode_;
+}
+
+String GPUAdapter::featureLevel() const {
+  return feature_level_;
 }
 
 void GPUAdapter::OnRequestDeviceCallback(
--- third_party/blink/renderer/modules/webgpu/gpu_adapter.h
+++ third_party/blink/renderer/modules/webgpu/gpu_adapter.h
@@ -48,6 +48,7 @@ class GPUAdapter final : public ScriptWrappable, DawnObject<wgpu::Adapter> {
   wgpu::BackendType backendType() const;
   bool SupportsMultiPlanarFormats() const;
   bool isCompatibilityMode() const;
+  String featureLevel() const;
 
   ScriptPromise<GPUDevice> requestDevice(ScriptState* script_state,
                                          GPUDeviceDescriptor* descriptor);
@@ -81,6 +82,7 @@ class GPUAdapter final : public ScriptWrappable, DawnObject<wgpu::Adapter> {
   wgpu::AdapterType adapter_type_;
   bool is_consumed_ = false;
   bool is_compatibility_mode_;
+  String feature_level_ = "";
   bool is_xr_compatible_ = false;
   Member<GPUSupportedLimits> limits_;
   Member<GPUSupportedFeatures> features_;
