d3a0cf26659a2d380a56688efc4a8178108dfbf7
vikas soni <vikassoni@chromium.org>
2024-12-19 09:18:46 -0800

    [SIPool] Remove SharedImagePoolId from SharedImageBacking constructor.
    Remove SharedImagePoolId from SharedImageBacking constructor.
    SharedImagePoolId will be set via SetSharedImagePoolId() method instead.
    
    Bug: 383466770
    Change-Id: Idb51f5d1f759e8e0244fb4c0e937c58937a646a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105076
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398640}

--- gpu/command_buffer/service/shared_image/shared_image_backing.h
+++ gpu/command_buffer/service/shared_image/shared_image_backing.h
@@ -130,8 +130,7 @@ class GPU_GLES2_EXPORT SharedImageBacking {
       std::string debug_label,
       size_t estimated_size,
       bool is_thread_safe,
-      std::optional<gfx::BufferUsage> buffer_usage = std::nullopt,
-      std::optional<SharedImagePoolId> pool_id = std::nullopt);
+      std::optional<gfx::BufferUsage> buffer_usage = std::nullopt);
 
   virtual ~SharedImageBacking();
 
@@ -470,8 +469,7 @@ class GPU_GLES2_EXPORT ClearTrackingSharedImageBacking
       std::string debug_label,
       size_t estimated_size,
       bool is_thread_safe,
-      std::optional<gfx::BufferUsage> buffer_usage = std::nullopt,
-      std::optional<SharedImagePoolId> pool_id = std::nullopt);
+      std::optional<gfx::BufferUsage> buffer_usage = std::nullopt);
 
   gfx::Rect ClearedRect() const override;
   void SetClearedRect(const gfx::Rect& cleared_rect) override;
