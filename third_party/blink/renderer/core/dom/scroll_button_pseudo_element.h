6223ed70d2a97bd02c1aa7d66853b6e3bf32b131
Tsuyoshi Horo <horo@chromium.org>
2024-12-18 20:56:53 -0800

    Revert "[carousel] Improve scroll buttons scrolling code"
    This reverts commit 52658a3e5092c402f62ef524b1831155fe317e4f.
    
    Reason for revert: Made tests flaky.
    Bug: 385029329
    
    Original change's description:
    > [carousel] Improve scroll buttons scrolling code
    >
    > This CL fixes the problem that zoom affects the scroll amount by reusing
    > scrolling logic from scroll manager code that is also used for page up
    > and page down buttons handling.
    >
    > Fixed: 383552806
    > Change-Id: I581c91b99acf78159728b03e7f3173c6d1686a50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097334
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Auto-Submit: Daniil Sakhapov <sakhapov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1398125}
    
    Change-Id: I45427c5a24386ea2fa80b4981dd13f215f61d4b5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108297
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398407}

--- third_party/blink/renderer/core/dom/scroll_button_pseudo_element.h
+++ third_party/blink/renderer/core/dom/scroll_button_pseudo_element.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_SCROLL_BUTTON_PSEUDO_ELEMENT_H_
 
 #include "third_party/blink/renderer/core/dom/pseudo_element.h"
-#include "third_party/blink/renderer/core/input/scroll_manager.h"
 #include "third_party/blink/renderer/core/scroll/scroll_snapshot_client.h"
 
 namespace blink {
@@ -38,7 +37,9 @@ class ScrollButtonPseudoElement : public PseudoElement,
   bool UpdateSnapshotInternal();
 
   bool enabled_ = true;
-  Member<ScrollManager> scroll_manager_;
+  // As per https://drafts.csswg.org/css-overflow-5/#scroll-buttons
+  // defines a "page" size to be 85% of the scrollport size.
+  static constexpr double PageSizePercent = 0.85;
 };
 
 template <>
