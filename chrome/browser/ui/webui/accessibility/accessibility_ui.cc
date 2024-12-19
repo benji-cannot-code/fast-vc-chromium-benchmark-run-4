c3252c74f9cd0b3ddfb91103597954e664af7f03
Alexandra Reimers <areimers@chromium.org>
2024-12-19 02:13:48 -0800

    Code Health: Clean up stale base::Feature "AccessibilityTreeForViews"
    This feature is disabled by default for a long time and has no Finch config. Let's clean up the dead code.
    
    AX-Relnotes: n/a.
    Bug: 356625214
    Change-Id: I78e7e58f3ca9a2ab24424cab4aac638f2008a5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6104174
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Alexandra Reimers <areimers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398468}

--- chrome/browser/ui/webui/accessibility/accessibility_ui.cc
+++ chrome/browser/ui/webui/accessibility/accessibility_ui.cc
@@ -86,11 +86,9 @@ static const char kTreeField[] = "tree";
 static const char kTypeField[] = "type";
 static const char kUrlField[] = "url";
 static const char kValueField[] = "value";
-static const char kWidgetsField[] = "widgets";
 static const char kApiTypeField[] = "apiType";
 
 #if defined(USE_AURA) && !BUILDFLAG(IS_CHROMEOS_ASH)
-static const char kWidgetIdField[] = "widgetId";
 static const char kWidget[] = "widget";
 #endif
 
@@ -105,7 +103,6 @@ static const char kPDFPrinting[] = "pdfPrinting";
 static const char kScreenReader[] = "screenreader";
 static const char kShowOrRefreshTree[] = "showOrRefreshTree";
 static const char kText[] = "text";
-static const char kViewsAccessibility[] = "viewsAccessibility";
 static const char kWeb[] = "web";
 
 // Possible global flag values
@@ -180,19 +177,6 @@ base::Value::Dict BuildTargetDescriptor(Browser* browser) {
 }
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-#if defined(USE_AURA) && !BUILDFLAG(IS_CHROMEOS_ASH)
-base::Value::Dict BuildTargetDescriptor(views::Widget* widget) {
-  base::Value::Dict widget_data;
-  widget_data.Set(kNameField, widget->widget_delegate()->GetWindowTitle());
-  widget_data.Set(kTypeField, kWidget);
-
-  // Use the Widget's root view ViewAccessibility's unique ID for lookup.
-  int id = widget->GetRootView()->GetViewAccessibility().GetUniqueId();
-  widget_data.Set(kWidgetIdField, id);
-  return widget_data;
-}
-#endif  // defined(USE_AURA) && !BUILDFLAG(IS_CHROMEOS_ASH)
-
 bool ShouldHandleAccessibilityRequestCallback(const std::string& path) {
   return path == kTargetsDataFile;
 }
@@ -231,10 +215,6 @@ void HandleAccessibilityRequestCallback(
 
   // The "pdfPrinting" flag is independent of the others.
   data.Set(kPDFPrinting, pdf_printing ? kOn : kOff);
-
-  // The "Top Level Widgets" section is only relevant if views accessibility is
-  // enabled.
-  data.Set(kViewsAccessibility, features::IsAccessibilityTreeForViewsEnabled());
 
   std::string pref_api_type =
       pref->GetString(prefs::kShownAccessibilityApiType);
@@ -310,19 +290,6 @@ void HandleAccessibilityRequestCallback(
   }
 #endif  // !BUILDFLAG(IS_ANDROID)
   data.Set(kBrowsersField, std::move(browser_list));
-
-  base::Value::List widgets_list;
-#if defined(USE_AURA) && !BUILDFLAG(IS_CHROMEOS_ASH)
-  if (features::IsAccessibilityTreeForViewsEnabled()) {
-    views::WidgetAXTreeIDMap& manager_map =
-        views::WidgetAXTreeIDMap::GetInstance();
-    const std::vector<views::Widget*> widgets = manager_map.GetWidgets();
-    for (views::Widget* widget : widgets) {
-      widgets_list.Append(BuildTargetDescriptor(widget));
-    }
-  }
-#endif  // defined(USE_AURA) && !BUILDFLAG(IS_CHROMEOS_ASH)
-  data.Set(kWidgetsField, std::move(widgets_list));
 
   std::string json_string;
   base::JSONWriter::Write(data, &json_string);
@@ -754,31 +721,6 @@ void AccessibilityUIMessageHandler::RequestWidgetsTree(
   AddPropertyFilters(property_filters, allow_empty,
                      AXPropertyFilter::ALLOW_EMPTY);
   AddPropertyFilters(property_filters, deny, AXPropertyFilter::DENY);
-
-  if (features::IsAccessibilityTreeForViewsEnabled()) {
-    int widget_id = *data.FindInt(kWidgetIdField);
-    views::WidgetAXTreeIDMap& manager_map =
-        views::WidgetAXTreeIDMap::GetInstance();
-    const std::vector<views::Widget*> widgets = manager_map.GetWidgets();
-    for (views::Widget* widget : widgets) {
-      int current_id =
-          widget->GetRootView()->GetViewAccessibility().GetUniqueId();
-      if (current_id == widget_id) {
-        ui::AXTreeID tree_id = manager_map.GetWidgetTreeID(widget);
-        DCHECK_NE(tree_id, ui::AXTreeIDUnknown());
-        std::unique_ptr<ui::AXTreeFormatter> formatter(
-            content::AXInspectFactory::CreateBlinkFormatter());
-        std::string tree_dump =
-            formatter->DumpInternalAccessibilityTree(tree_id, property_filters);
-
-        base::Value::Dict result(BuildTargetDescriptor(widget));
-        result.Set(kTreeField, tree_dump);
-        AllowJavascript();
-        FireWebUIListener(request_type, result);
-        return;
-      }
-    }
-  }
 
   base::Value::Dict result;
   result.Set(kTypeField, kWidget);
