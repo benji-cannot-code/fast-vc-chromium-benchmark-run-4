543b5536da98e29d6d81d3b795e06a924c12a3c8
Lokbondo Kung <lokokung@google.com>
2024-12-19 09:12:28 -0800

    [dawn] Add ifdefs to use new logging callback types and entry points.
    Bug: 369445924
    Change-Id: Id562567f514a9094a6e2cb232a2944d638dc6792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6107731
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Loko Kung <lokokung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398629}

--- gpu/command_buffer/service/dawn_context_provider.cc
+++ gpu/command_buffer/service/dawn_context_provider.cc
@@ -410,6 +410,23 @@ class DawnSharedContext : public base::RefCountedThreadSafe<DawnSharedContext>,
   }
 
   // Provided to wgpu::Device as logging callback.
+#ifdef WGPU_BREAKING_CHANGE_LOGGING_CALLBACK_TYPE
+  static void LogInfo(wgpu::LoggingType type, wgpu::StringView message) {
+    std::string_view view = {message.data, message.length};
+    switch (type) {
+      case wgpu::LoggingType::Warning:
+        LOG(WARNING) << view;
+        break;
+      case wgpu::LoggingType::Error:
+        LOG(ERROR) << view;
+        SetDawnErrorCrashKey(view);
+        base::debug::DumpWithoutCrashing();
+        break;
+      default:
+        break;
+    }
+  }
+#else
   static void LogInfo(WGPULoggingType type,
                       WGPUStringView message,
                       void*) {
@@ -427,6 +444,7 @@ class DawnSharedContext : public base::RefCountedThreadSafe<DawnSharedContext>,
         break;
     }
   }
+#endif
 
   ~DawnSharedContext() override;
 
@@ -470,7 +488,11 @@ DawnSharedContext::~DawnSharedContext() {
       base::trace_event::MemoryDumpManager::GetInstance()
           ->UnregisterDumpProvider(this);
     }
+#ifdef WGPU_BREAKING_CHANGE_LOGGING_CALLBACK_TYPE
+    device_.SetLoggingCallback([](wgpu::LoggingType, wgpu::StringView) {});
+#else
     device_.SetLoggingCallback(nullptr, nullptr);
+#endif
     // Destroy the device now so that the lost callback, which references this
     // class, is fired now before we clean up the rest of this class.
     device_.Destroy();
@@ -490,9 +512,14 @@ bool DawnSharedContext::Initialize(
   // instance doesn't exit the GPU process.
   // LogInfo will be used to receive instance level errors. For example failures
   // of loading libraries, initializing backend, etc
-  instance_ = webgpu::DawnInstance::Create(
-      &platform_, gpu_preferences, webgpu::SafetyLevel::kUnsafe,
-      &DawnSharedContext::LogInfo, nullptr);
+  instance_ = webgpu::DawnInstance::Create(&platform_, gpu_preferences,
+                                           webgpu::SafetyLevel::kUnsafe,
+#ifdef WGPU_BREAKING_CHANGE_LOGGING_CALLBACK_TYPE
+                                           &DawnSharedContext::LogInfo);
+#else
+                                           &DawnSharedContext::LogInfo,
+                                           nullptr);
+#endif
 
   std::vector<const char*> enabled_toggles =
       GetEnabledToggles(backend_type, force_fallback_adapter, gpu_preferences);
@@ -661,7 +688,11 @@ bool DawnSharedContext::Initialize(
     return false;
   }
 
+#ifdef WGPU_BREAKING_CHANGE_LOGGING_CALLBACK_TYPE
+  device_.SetLoggingCallback(&DawnSharedContext::LogInfo);
+#else
   device_.SetLoggingCallback(&DawnSharedContext::LogInfo, nullptr);
+#endif
 
   backend_type_ = backend_type;
   is_vulkan_swiftshader_adapter_ =
