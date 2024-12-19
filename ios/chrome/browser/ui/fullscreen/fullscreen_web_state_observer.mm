e2cb0ffeb068a2f68f716f03356893998af85770
“Prasanaa” <prasanaa@google.com>
2024-12-19 01:52:26 -0800

    Remove unused imports from `fullscreen_web_state_observer.mm`
    Change-Id: I0190c95dd3ded79c092a13b619eb2f50a262746a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091595
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398461}

--- ios/chrome/browser/ui/fullscreen/fullscreen_web_state_observer.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_web_state_observer.mm
@@ -7,11 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check_op.h"
 #import "base/ios/ios_util.h"
-#import "ios/chrome/browser/ui/fullscreen/fullscreen_content_adjustment_util.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_mediator.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_model.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_web_view_proxy_observer.h"
-#import "ios/chrome/browser/ui/fullscreen/scoped_fullscreen_disabler.h"
 #import "ios/public/provider/chrome/browser/fullscreen/fullscreen_api.h"
 #import "ios/web/common/url_util.h"
 #import "ios/web/public/navigation/navigation_context.h"
