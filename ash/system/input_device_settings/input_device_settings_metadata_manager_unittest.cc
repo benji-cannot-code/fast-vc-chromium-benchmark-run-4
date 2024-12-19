f30c218bd0bf41db54584e8f825f39a8df3c0e32
Mikel Astiz <mastiz@chromium.org>
2024-12-19 13:35:24 -0800

    Update tests to avoid implicit conversion between GaiaId and std::string
    Class GaiaId was introduced to improve type safety and avoid strings
    being used, which is more error-prone.
    
    Temporarily, implicit conversion between GaiaId and std::string was
    allowed, until all occurrences relying on it are migrated. This patch
    updates tests under ash/system/input_device_settings.
    
    This CL was uploaded by git cl split.
    
    R=michaelcheco@google.com
    
    Bug: 380416867
    Change-Id: I20847f17c75643a505d5963376caa6cb0562b2ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105214
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398839}

--- ash/system/input_device_settings/input_device_settings_metadata_manager_unittest.cc
+++ ash/system/input_device_settings/input_device_settings_metadata_manager_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/strings/string_util.h"
 #include "base/test/bind.h"
+#include "google_apis/gaia/gaia_id.h"
 
 namespace ash {
 
@@ -27,7 +28,7 @@ constexpr char kExpectedDataUri[] =
     "ABzDhkxyVHpUmRRoAmpABJ+eiyP8AAAAASUVORK5CYII=";
 
 const AccountId account_1 =
-    AccountId::FromUserEmailGaiaId("user@example.com", "123");
+    AccountId::FromUserEmailGaiaId("user@example.com", GaiaId("123"));
 
 }  // namespace
 
