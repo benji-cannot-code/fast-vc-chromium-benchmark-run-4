bc875ccadc789cdc1c8ede76367bd723fe8fb2e8
Joemer Ramos <joemerramos@chromium.org>
2024-12-19 07:55:58 -0800

    [iOS] Fix compile failures in Tab Organization Wrapper for iOS
    After crrev.com/c/6100523 landed, logic in
    tab_organization_request_wrapper.mm broke. This CL fixes the namespace
    issue.
    
    Bug: 384090989
    Change-Id: I03668f0600e90fa60976185d74d7727d0ce98619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109479
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398581}

--- ios/chrome/browser/intelligence/proto_wrappers/tab_organization_request_wrapper.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/tab_organization_request_wrapper.mm
@@ -105,8 +105,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     PageContextWrapper* pageContextWrapper = [[PageContextWrapper alloc]
           initWithWebState:webState
         completionCallback:base::BindOnce(^(
-                               std::unique_ptr<optimization_guide::proto::
-                                                   features::PageContext>
+                               std::unique_ptr<
+                                   optimization_guide::proto::PageContext>
                                    page_context) {
           [weakSelf asyncWorkCompleteForPageContext:std::move(page_context)
                                       associatedTab:tab];
