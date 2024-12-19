6da980b70546dc9dd775fd22ddffcaf06cbc4452
Mikel Astiz <mastiz@chromium.org>
2024-12-19 10:14:32 -0800

    Update tests to avoid implicit conversion between GaiaId and std::string
    Class GaiaId was introduced to improve type safety and avoid strings
    being used, which is more error-prone.
    
    Temporarily, implicit conversion between GaiaId and std::string was
    allowed, until all occurrences relying on it are migrated. This patch
    updates tests under chrome/browser/ash/growth.
    
    This CL was uploaded by git cl split.
    
    R=llin@chromium.org
    
    Bug: 380416867
    Change-Id: I7c79df599575d99b46b65e3722f5870753fa578d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105694
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398693}

--- chrome/browser/ash/growth/campaigns_manager_session_unittest.cc
+++ chrome/browser/ash/growth/campaigns_manager_session_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "content/public/test/browser_task_environment.h"
+#include "google_apis/gaia/gaia_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -96,7 +97,7 @@ class CampaignsManagerSessionTest : public testing::Test {
   // Creates a test user with a testing profile and logs in.
   TestingProfile* LoginUser() {
     const AccountId account_id(
-        AccountId::FromUserEmailGaiaId("test@test.com", "test_user"));
+        AccountId::FromUserEmailGaiaId("test@test.com", GaiaId("test_user")));
     fake_user_manager_->AddUser(account_id);
 
     auto prefs =
