6c6f467b17353be269e4e9e2b0ddec3c27d7fd16
Eshwar Stalin <estalin@chromium.org>
2024-12-18 16:15:50 -0800

    Code cleanup of glic background mode manager
    Supporting background mode manager even when the platform doesn't
    support global hotkey listener. This can happen on platforms like
    wayland.
    
    Bug: 377805727
    Change-Id: Ic589730aafc1326c8864c0ef27049490e0ca7615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6100532
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398317}

--- chrome/browser/glic/launcher/glic_background_mode_manager_browsertest.cc
+++ chrome/browser/glic/launcher/glic_background_mode_manager_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/extensions/global_shortcut_listener.h"
 #include "chrome/browser/glic/glic_pref_names.h"
 #include "chrome/browser/glic/launcher/glic_configuration.h"
 #include "chrome/browser/global_features.h"
@@ -28,7 +29,8 @@ namespace glic {
 class GlicBackgroundModeManagerBrowserTest : public InProcessBrowserTest {
  public:
   void SetUp() override {
-    feature_list_.InitAndEnableFeature(features::kGlic);
+    feature_list_.InitWithFeatures(
+        {features::kGlic, features::kTabstripComboButton}, {});
     InProcessBrowserTest::SetUp();
   }
 
@@ -37,6 +39,12 @@ class GlicBackgroundModeManagerBrowserTest : public InProcessBrowserTest {
     // browser process from closing which causes the test to hang.
     g_browser_process->local_state()->SetBoolean(prefs::kGlicLauncherEnabled,
                                                  false);
+  }
+
+  bool IsHotkeySupported() {
+    auto* const global_shortcut_listener =
+        extensions::GlobalShortcutListener::GetInstance();
+    return global_shortcut_listener != nullptr;
   }
 
   void RegisterHotkey(ui::Accelerator updated_hotkey) {
@@ -88,6 +96,9 @@ IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest, StatusIcon) {
 
 IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest,
                        UpdateHotkeyWhileEnabled) {
+  if (!IsHotkeySupported()) {
+    GTEST_SKIP() << "Test does not apply to this platform.";
+  }
   g_browser_process->local_state()->SetBoolean(prefs::kGlicLauncherEnabled,
                                                true);
   GlicBackgroundModeManager* const manager =
@@ -101,6 +112,9 @@ IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest,
                        UpdateHotkeyWhileDisabled) {
+  if (!IsHotkeySupported()) {
+    GTEST_SKIP() << "Test does not apply to this platform.";
+  }
   PrefService* const pref_service = g_browser_process->local_state();
   ASSERT_FALSE(pref_service->GetBoolean(prefs::kGlicLauncherEnabled));
   GlicBackgroundModeManager* const manager =
@@ -120,6 +134,9 @@ IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(GlicBackgroundModeManagerBrowserTest,
                        RegisterInvalidAccelerator) {
+  if (!IsHotkeySupported()) {
+    GTEST_SKIP() << "Test does not apply to this platform.";
+  }
   g_browser_process->local_state()->SetBoolean(prefs::kGlicLauncherEnabled,
                                                true);
   GlicBackgroundModeManager* const manager =
