7be8aacc826d17157782544a1651e4080f88b271
Henrique Nakashima <hnakashima@chromium.org>
2024-12-19 15:46:24 -0800

    [Android] Disable testClickDisabledForNonAcceptableAutofillSuggestions
    Flaky across multiple bots.
    
    Bug: 385200981,374102854
    Change-Id: I3255d4831e877bf665005747361a2f2bbdf6b5b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112537
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398918}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryViewTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryViewTest.java
@@ -72,6 +72,7 @@ import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.autofill.PersonalDataManagerFactory;
@@ -804,6 +805,7 @@ public class KeyboardAccessoryViewTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/385200981")
     public void testClickDisabledForNonAcceptableAutofillSuggestions() throws InterruptedException {
         AtomicReference<Boolean> clickRecorded = new AtomicReference<>(false);
         ThreadUtils.runOnUiThreadBlocking(
