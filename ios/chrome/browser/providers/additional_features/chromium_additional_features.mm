a641c77a4a2e070a7570c816eab2240fcd87428f
ginnnnnnny <ginnyhuang@chromium.org>
2024-12-19 10:14:42 -0800

    [iOS] Add new API for shutdown [1/3]
    This series of CL adds a shutdown method to
    AdditionalFeaturesController. This is to make sure that when the app
    is terminated, the controller object is properly cleaned up before
    everything is deallocated by the OS.
    
    Follow up CLs:
    crrev.com/i/7902280 (should only be landed after this CL lands)
    crrev.com/c/6102294 (should only be landed after the above CL lands)
    
    Bug: 384569776
    Change-Id: I2aac4c3060307533ce3be6e44166e99a8c72d87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102349
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398694}

--- ios/chrome/browser/providers/additional_features/chromium_additional_features.mm
+++ ios/chrome/browser/providers/additional_features/chromium_additional_features.mm
@@ -17,6 +17,7 @@ class ChromiumAdditionalFeaturesController final
  public:
   inline void RegisterFeatureList(base::FeatureList* featureList) final {}
   inline void FeatureListDidCompleteSetup() final {}
+  inline void ShutDown() final {}
 };
 
 }  // namespace
