809b9774527a35b38f1cb644683e1407a4aa6eba
Jacob Stanley <jacobstanley@google.com>
2024-12-18 20:40:54 -0800

    [Ads Api UX Enhancements] Notice EEA - Clank
    Add PromptAction metrics for the two separate more info dropdowns.
    
    Bug: 379337243
    Change-Id: I4fbae5fac44c0e01439bc10732936b7371d15200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6083092
    Reviewed-by: Christine Ying <chrstne@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398404}

--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogTest.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogTest.java
@@ -455,7 +455,10 @@ public final class PrivacySandboxDialogTest {
         onViewWaiting(withId(R.id.privacy_sandbox_notice_title), true);
         // Click on the site suggested ads expanding section and verify it worked correctly.
         tryClickOn(withId(R.id.site_suggested_ads_dropdown_element));
-        // TODO(crbug.com/379337243): Add Site Suggested Ads more info opened metrics
+        assertEquals(
+                "Last dialog action",
+                PromptAction.NOTICE_SITE_SUGGESTED_ADS_MORE_INFO_OPENED,
+                (int) mFakePrivacySandboxBridge.getLastPromptAction());
 
         onView(withId(R.id.privacy_sandbox_notice_eea_site_suggested_ads_dropdown))
                 .inRoot(isDialog())
@@ -464,7 +467,10 @@ public final class PrivacySandboxDialogTest {
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()));
         tryClickOn(withId(R.id.site_suggested_ads_dropdown_element));
-        // TODO(crbug.com/379337243): Add Site Suggested Ads more info closed metrics
+        assertEquals(
+                "Last dialog action",
+                PromptAction.NOTICE_SITE_SUGGESTED_ADS_MORE_INFO_CLOSED,
+                (int) mFakePrivacySandboxBridge.getLastPromptAction());
         onView(withId(R.id.privacy_sandbox_notice_eea_site_suggested_ads_dropdown))
                 .check(doesNotExist());
     }
@@ -479,7 +485,10 @@ public final class PrivacySandboxDialogTest {
         onViewWaiting(withId(R.id.privacy_sandbox_notice_title), true);
         // Click on the Ad Measurement expanding section and verify it worked correctly.
         tryClickOn(withId(R.id.ad_measurement_dropdown_element));
-        // TODO(crbug.com/379337243): Add Ad Measurement more info opened metrics
+        assertEquals(
+                "Last dialog action",
+                PromptAction.NOTICE_ADS_MEASUREMENT_MORE_INFO_OPENED,
+                (int) mFakePrivacySandboxBridge.getLastPromptAction());
 
         onView(withId(R.id.privacy_sandbox_notice_eea_ad_measurement_dropdown))
                 .inRoot(isDialog())
@@ -488,7 +497,10 @@ public final class PrivacySandboxDialogTest {
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()));
         tryClickOn(withId(R.id.ad_measurement_dropdown_element));
-        // TODO(crbug.com/379337243): Add Ad Measurement more info closed metrics
+        assertEquals(
+                "Last dialog action",
+                PromptAction.NOTICE_ADS_MEASUREMENT_MORE_INFO_CLOSED,
+                (int) mFakePrivacySandboxBridge.getLastPromptAction());
         onView(withId(R.id.privacy_sandbox_notice_eea_ad_measurement_dropdown))
                 .check(doesNotExist());
     }
