a41939235a6f1723eaa1782fb25ffa163813b0a1
Frank Liberato <liberato@chromium.org>
2024-12-19 10:18:42 -0800

    [pip] Don't send raw ptrs in intents
    This CL replaces the raw ptr in the PictureInPictureActivity intents
    with UnguessableTokens instead.  This includes both the launch
    intent and the remote action intents.
    
    The native side now manages the lookup between tokens and pointers,
    with the ctor and dtor ensuring that the map is up to date.  The
    java side still caches the raw pointer, but the native side is
    careful to also clear that before being destroyed.
    
    Change-Id: I1792bbc3b1d0f1462666633b302ce36115ef6e65
    Bug: 379168169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088735
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398697}

--- chrome/browser/ui/android/overlay/overlay_window_android.h
+++ chrome/browser/ui/android/overlay/overlay_window_android.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/memory/raw_ptr.h"
 #include "base/timer/timer.h"
+#include "base/unguessable_token.h"
 #include "content/public/browser/overlay_window.h"
 #include "third_party/blink/public/mojom/mediasession/media_session.mojom.h"
 #include "ui/android/window_android.h"
@@ -33,8 +34,9 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
       content::VideoPictureInPictureWindowController* controller);
   ~OverlayWindowAndroid() override;
 
-  void OnActivityStart(
+  static OverlayWindowAndroid* OnActivityStart(
       JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& token,
       const base::android::JavaParamRef<jobject>& obj,
       const base::android::JavaParamRef<jobject>& jwindow_android);
   void Destroy(JNIEnv* env);
@@ -86,6 +88,10 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
       const std::vector<media_session::MediaImage>& images) override {}
   void SetSurfaceId(const viz::SurfaceId& surface_id) override;
 
+  void Initialize(JNIEnv* env,
+                  const base::android::JavaParamRef<jobject>& obj,
+                  const base::android::JavaParamRef<jobject>& jwindow_android);
+
  private:
   // Notify PictureInPictureActivity that visible actions have changed.
   void MaybeNotifyVisibleActionsChanged();
@@ -96,7 +102,8 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
       bool is_visible);
   void CloseInternal();
 
-  // A weak reference to Java PictureInPictureActivity object.
+  base::UnguessableToken token_{base::UnguessableToken::Create()};
+
   JavaObjectWeakGlobalRef java_ref_;
   raw_ptr<ui::WindowAndroid> window_android_;
   raw_ptr<thin_webview::android::CompositorView> compositor_view_;
