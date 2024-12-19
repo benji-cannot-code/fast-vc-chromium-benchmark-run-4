19846091b06033a761ff540f747a2477d160926b
David Bertoni <dbertoni@chromium.org>
2024-12-18 16:35:51 -0800

    [Extensions] Update SW extension modification code.
    Most test cases uses a single JS file, so rather than generate a
    file and use importScripts to import the original script, it's
    trivial to write the manifest using the original script name.
    
    Change-Id: I7abab750d8c710e91eec3e9981d369728331e532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102346
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398330}

--- chrome/test/data/extensions/api_test/runtime/privileged/test.js
+++ chrome/test/data/extensions/api_test/runtime/privileged/test.js
@@ -10,7 +10,7 @@ var succeed = chrome.test.succeed;
 
 const isServiceWorker = ('ServiceWorkerGlobalScope' in self);
 const extensionId = 'iegclhlplifhodhkoafiokenjoapiobj';
-const serviceWorkerScriptName = 'generated_service_worker__.js';
+const serviceWorkerScriptName = 'test.js';
 
 function checkIsDefined(prop) {
   if (!chrome.runtime) {
