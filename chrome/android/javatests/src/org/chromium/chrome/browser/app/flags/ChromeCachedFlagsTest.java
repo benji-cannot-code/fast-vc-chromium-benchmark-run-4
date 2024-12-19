81502cfad2ca006e26156a65c43915408a45711f
Martin Kong <martinkong@google.com>
2024-12-19 07:12:13 -0800

    Enable sSearchInCCT in tests
    Change the sSearchInCCT cached flag's defaultValueInTests,
    so that it matches the value in fieldtrial_testing_config.json.
    
    Bug: 380113169
    Change-Id: I3e3eba5d846b84f2e848b44129bb051806b234e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6106571
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1398551}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
@@ -57,7 +57,6 @@ public class ChromeCachedFlagsTest {
                     ChromeFeatureList.sPostGetMyMemoryStateToBackground,
                     ChromeFeatureList.sPrefetchBrowserInitiatedTriggers,
                     ChromeFeatureList.sSafetyHubMagicStack,
-                    ChromeFeatureList.sSearchInCCT,
                     ChromeFeatureList.sTabGroupPaneAndroid,
                     ChromeFeatureList.sTabResumptionModuleAndroid,
                     ChromeFeatureList.sTabStateFlatBuffer,
