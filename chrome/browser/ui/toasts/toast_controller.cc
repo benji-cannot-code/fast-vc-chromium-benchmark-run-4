b17bcf2ec9d2589fa4aca37353ca9b2f01ff6f99
Alison Gale <agale@chromium.org>
2024-12-19 09:16:26 -0800

    Make sure the actionable toast timeout applies to the dismiss menu
    Bug: 383135477
    Change-Id: I5bc63daf2fa6e2cf2e2f971ca16c261df2ea390d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6107881
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1398636}

--- chrome/browser/ui/toasts/toast_controller.cc
+++ chrome/browser/ui/toasts/toast_controller.cc
@@ -46,6 +46,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/fullscreen_util_mac.h"
 #endif
 
+namespace {
+bool ShouldAddDismissMenuOptions(const ToastSpecification* spec) {
+  return base::FeatureList::IsEnabled(toast_features::kToastRefinements) &&
+         !spec->has_close_button() && !spec->has_menu();
+}
+
+}  // namespace
+
 ToastParams::ToastParams(ToastId id) : toast_id(id) {}
 ToastParams::ToastParams(ToastParams&& other) noexcept = default;
 ToastParams& ToastParams::operator=(ToastParams&& other) noexcept = default;
@@ -229,10 +237,13 @@ void ToastController::ShowToast(ToastParams params) {
   CHECK_EQ(current_toast_spec->has_menu(), !!params.menu_model);
 
   currently_showing_toast_id_ = params.toast_id;
+  const bool is_actionable =
+      current_toast_spec->action_button_string_id().has_value() ||
+      current_toast_spec->has_menu() ||
+      ShouldAddDismissMenuOptions(current_toast_spec);
   base::TimeDelta timeout =
-      current_toast_spec->action_button_string_id().has_value()
-          ? toast_features::kToastTimeout.Get()
-          : toast_features::kToastWithoutActionTimeout.Get();
+      is_actionable ? toast_features::kToastTimeout.Get()
+                    : toast_features::kToastWithoutActionTimeout.Get();
 
   toast_close_timer_.Start(
       FROM_HERE, timeout,
@@ -285,8 +296,9 @@ void ToastController::CreateToast(ToastParams params,
 
   if (params.menu_model) {
     toast_view->AddMenu(std::move(params.menu_model));
-  } else if (base::FeatureList::IsEnabled(toast_features::kToastRefinements) &&
-             !spec->has_close_button()) {
+  }
+
+  if (ShouldAddDismissMenuOptions(spec)) {
     toast_view->AddMenu(
         std::make_unique<ToastDismissMenuModel>(params.toast_id));
   }
