07507fe0805e21e90bc99164aa572b06a3949bd1
Sorin Jianu <sorin@chromium.org>
2024-12-19 10:49:48 -0800

    [safe browsing] fix obsolete comment re member initialization
    Change-Id: I789e7eef433ab5ac72f20903b871a4809a24e332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6107757
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398716}

--- chrome/browser/safe_browsing/incident_reporting/incident_report_uploader_impl.cc
+++ chrome/browser/safe_browsing/incident_reporting/incident_report_uploader_impl.cc
@@ -63,11 +63,6 @@ constexpr net::NetworkTrafficAnnotationTag
 
 }  // namespace
 
-// This is initialized here rather than in the class definition due to an
-// "extension" in MSVC that defies the standard.
-// static
-const int IncidentReportUploaderImpl::kTestUrlFetcherId = 47;
-
 IncidentReportUploaderImpl::~IncidentReportUploaderImpl() {
 }
 
