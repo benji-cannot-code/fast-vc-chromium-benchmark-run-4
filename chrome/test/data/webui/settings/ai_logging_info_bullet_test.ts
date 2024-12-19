6df7fda1b6febf07c947a531b81ac9f953a14f3f
dpapad <dpapad@chromium.org>
2024-12-19 15:05:07 -0800

    Settings: Add a couple missing "import" keywords in tests.
    Bug: None
    Change-Id: If2b976ccc3cbe698beb7642e34b56b420c7e7803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111917
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398901}

--- chrome/test/data/webui/settings/ai_logging_info_bullet_test.ts
+++ chrome/test/data/webui/settings/ai_logging_info_bullet_test.ts
@@ -3,13 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'chrome://settings/settings.js';
+import 'chrome://settings/settings.js';
 
 import type {SettingsAiLoggingInfoBullet} from 'chrome://settings/settings.js';
 import {loadTimeData, ModelExecutionEnterprisePolicyValue} from 'chrome://settings/settings.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
-
-import {assertEquals, assertTrue, assertFalse} from 'chrome://webui-test/chai_assert.js';
 
 type PrefObject = chrome.settingsPrivate.PrefObject;
 
