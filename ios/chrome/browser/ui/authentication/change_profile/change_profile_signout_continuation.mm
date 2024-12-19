5953b1db8099c2b7472397b7157fa6dad4c8b566
Jood <jood@google.com>
2024-12-19 10:07:54 -0800

    [iOS] Use currentBrowserProvider in ChangeProfileSignoutContinuation
    Bug: 375605174
    Change-Id: I5212d4a9902179c81042b6afef8aa5540bb1a790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105353
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1398687}

--- ios/chrome/browser/ui/authentication/change_profile/change_profile_signout_continuation.mm
+++ ios/chrome/browser/ui/authentication/change_profile/change_profile_signout_continuation.mm
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)executeWithSceneState:(SceneState*)sceneState
                    completion:(ProceduralBlock)completion {
   Browser* browser =
-      sceneState.browserProviderInterface.mainBrowserProvider.browser;
+      sceneState.browserProviderInterface.currentBrowserProvider.browser;
   CHECK(browser);
 
   id<SnackbarCommands> snackbarCommandsHandler =
