f8feb882b4c289631942f4ef9a7739cc25706b88
ginnnnnnny <ginnyhuang@chromium.org>
2024-12-19 12:44:42 -0800

    [iOS] Add new API for shutdown [3/3]
    Cleaning up CL for the series
    
    crrev.com/c/6102349 (should be landed before this CL lands)
    crrev.com/i/7902280 (should only be landed after this CL is rolled
    into Chromium)
    
    Fixed: 384569776
    Change-Id: I41c68e5f4e042316eb3f9ca01eaf934db5c417ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102294
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398799}

--- ios/public/provider/chrome/browser/additional_features/additional_features_controller.mm
+++ ios/public/provider/chrome/browser/additional_features/additional_features_controller.mm
@@ -8,7 +8,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 AdditionalFeaturesController::AdditionalFeaturesController() = default;
 
 AdditionalFeaturesController::~AdditionalFeaturesController() = default;
-
-void AdditionalFeaturesController::ShutDown() {
-  // TODO(crbug.com/384569776): Remove this block.
-}
