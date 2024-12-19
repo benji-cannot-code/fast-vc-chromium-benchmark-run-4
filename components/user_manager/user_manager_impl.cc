51df974d71c2b53c1a9b4b0ad3e7dcf5bdb5ec5d
Hidehiko Abe <hidehiko@chromium.org>
2024-12-19 09:16:12 -0800

    Move stub image initialization to User's ctor.
    Currently it is set as a part of user log-in, but it should make
    more sense to set it up on user creation.
    Historically, it was done so, but with code change histories
    the timing gets diverged. This CL restores it for simplicity.
    
    BUG=278643115
    TEST=Tryjob
    
    Change-Id: I06910831e60ed1e88e07111b61b0fa64969b431e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109996
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398635}

--- components/user_manager/user_manager_impl.cc
+++ components/user_manager/user_manager_impl.cc
@@ -50,8 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_type.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/resource/resource_bundle.h"
-#include "ui/chromeos/resources/grit/ui_chromeos_resources.h"
 #include "ui/gfx/image/image_skia.h"
 
 namespace user_manager {
@@ -84,12 +82,6 @@ UserType GetStoredUserType(const base::Value::Dict& prefs_user_types,
     return UserType::kRegular;
   }
   return static_cast<UserType>(int_user_type);
-}
-
-std::unique_ptr<UserImage> CreateStubImage() {
-  return std::make_unique<user_manager::UserImage>(
-      *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
-          IDR_LOGIN_DEFAULT_USER));
 }
 
 bool IsDeviceLocalAccountChanged(
@@ -1461,8 +1453,6 @@ User* UserManagerImpl::FindUserInListAndModify(const AccountId& account_id) {
 void UserManagerImpl::GuestUserLoggedIn() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto* user = User::CreateGuestUser(GuestAccountId());
-  user->SetStubImage(CreateStubImage(), UserImage::Type::kInvalid,
-                     /*is_loading=*/false);
   user_storage_.emplace_back(user);
   active_user_ = user;
 }
@@ -1533,9 +1523,6 @@ void UserManagerImpl::PublicAccountUserLoggedIn(user_manager::User* user) {
 
 void UserManagerImpl::KioskAppLoggedIn(user_manager::User* user) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  user->SetStubImage(CreateStubImage(), UserImage::Type::kInvalid,
-                     /*is_loading=*/false);
   active_user_ = user;
 }
 
