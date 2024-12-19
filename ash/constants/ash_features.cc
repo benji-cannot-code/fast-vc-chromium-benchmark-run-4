fa54f5dc21bf4c14b52756e58385a896308656ff
Daseul Lee <dslee@chromium.org>
2024-12-19 08:15:36 -0800

    [CodeHealth] Clean up NearbyKeepAliveFix flag.
    Bug: 356234776
    Change-Id: I4a32df60f18508ee92486841133b6ee2b5b73751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102127
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398597}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1889,12 +1889,6 @@ BASE_FEATURE(kMultiZoneRgbKeyboard,
              "MultiZoneRgbKeyboard",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Enables Nearby Connections to specificy KeepAlive interval and timeout while
-// also making the Nearby Connections WebRTC defaults longer.
-BASE_FEATURE(kNearbyKeepAliveFix,
-             "NearbyKeepAliveFix",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables Nearby Presence for scanning and discovery of nearby devices.
 BASE_FEATURE(kNearbyPresence,
              "NearbyPresence",
@@ -4072,10 +4066,6 @@ bool AreEphemeralNetworkPoliciesEnabled() {
 
 bool CanEphemeralNetworkPoliciesBeEnabledByPolicy() {
   return base::FeatureList::IsEnabled(kEphemeralNetworkPoliciesEnabledPolicy);
-}
-
-bool IsNearbyKeepAliveFixEnabled() {
-  return base::FeatureList::IsEnabled(kNearbyKeepAliveFix);
 }
 
 bool IsNearbyPresenceEnabled() {
