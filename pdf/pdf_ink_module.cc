185b2b53b2611bb218be3b9a7ff9da8bf7bdbe99
Alan Screen <awscreen@chromium.org>
2024-12-19 14:37:54 -0800

    [PDF Ink Signatures] Finish in-progress stroke on tool type change
    A user can simultaneously apply different modes of input (such as mouse
    + keyboard, or mouse + touchscreen) to cause a change in tool state in
    the middle of a draw or erase stroke.  Such actions should not cause
    the PDF viewer to crash.
    
    Avoid this crash by automatically finishing the in-progress stroke at
    the time of the tool type change.  The event which would normally
    signal to finish the stroke is now considered unhandled since the
    stroke already finished.
    
    Bug: 381908888
    Change-Id: Ia5690e271886d82864fd155bd09c00da7b9d7a37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6103525
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1398878}

--- pdf/pdf_ink_module.cc
+++ pdf/pdf_ink_module.cc
@@ -459,7 +459,7 @@ bool PdfInkModule::StartStroke(const gfx::PointF& position,
   // compensate for missed input events.
   CHECK(!state.input_last_event.has_value());
   state.input_last_event =
-      DrawingStrokeState::EventDetails{position, timestamp};
+      DrawingStrokeState::EventDetails{position, timestamp, tool_type};
 
   return true;
 }
@@ -487,7 +487,7 @@ bool PdfInkModule::ContinueStroke(const gfx::PointF& position,
     // If `position` is outside the page, and so was `last_position`, then just
     // update `last_input_event` and treat the event as handled.
     state.input_last_event =
-        DrawingStrokeState::EventDetails{position, timestamp};
+        DrawingStrokeState::EventDetails{position, timestamp, tool_type};
     return true;
   }
 
@@ -509,7 +509,7 @@ bool PdfInkModule::ContinueStroke(const gfx::PointF& position,
     // Remember `position` and `timestamp` for use in the next event and treat
     // event as handled.
     state.input_last_event =
-        DrawingStrokeState::EventDetails{position, timestamp};
+        DrawingStrokeState::EventDetails{position, timestamp, tool_type};
     return true;
   }
 
@@ -538,7 +538,7 @@ bool PdfInkModule::ContinueStroke(const gfx::PointF& position,
 
   // Remember `position` and `timestamp` for use in the next event.
   state.input_last_event =
-      DrawingStrokeState::EventDetails{position, timestamp};
+      DrawingStrokeState::EventDetails{position, timestamp, tool_type};
 
   return true;
 }
@@ -810,10 +810,33 @@ void PdfInkModule::HandleSetAnnotationBrushMessage(
 
   const std::string& brush_type_string = *data->FindString("type");
   if (brush_type_string == "eraser") {
+    if (is_drawing_stroke()) {
+      DrawingStrokeState& state = drawing_stroke_state();
+      if (state.start_time.has_value()) {
+        // PdfInkModule is currently drawing a stroke.  Finish that before
+        // transitioning, using the last known input.
+        CHECK(state.input_last_event.has_value());
+        const DrawingStrokeState::EventDetails& input_last_event =
+            state.input_last_event.value();
+        FinishStroke(input_last_event.position, input_last_event.timestamp,
+                     input_last_event.tool_type);
+      }
+    }
+
     current_tool_state_.emplace<EraserState>();
     eraser_size_ = size;
     MaybeSetCursor();
     return;
+  }
+
+  if (is_erasing_stroke()) {
+    EraserState& state = erasing_stroke_state();
+    if (state.erasing) {
+      // An erasing stroke is in-progress.  Finish that off before
+      // transitioning, using the last known input.
+      CHECK(state.input_last_event_position.has_value());
+      FinishEraseStroke(state.input_last_event_position.value());
+    }
   }
 
   // All brush types except the eraser should have a color and size.
