6fee8b05a67f971ba76008cc44471476dc4758a9
Maggie Chen <magchen@chromium.org>
2024-12-20 13:37:43 -0800

    Add the support for sticky position in DynamicSafeAreaInsetsSupportedByCC
    Now both fixed and sticky position work in the path of
    moved_by_safe_area_bottom in CC for the feature
    DynamicSafeAreaInsetsSupportedByCC.
    
    Bug: 373980016
    
    Change-Id: I8e7f6771125cbfaaa9097f3894e0b09d92eed916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094022
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399362}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -815,6 +815,11 @@ void FragmentPaintPropertyTreeBuilder::UpdateStickyTranslation() {
                     CompositorElementIdNamespace::kStickyTranslation);
           }
           state.sticky_constraint = std::move(constraint);
+
+          if (object_.StyleRef().IsBottomRelativeToSafeAreaInset()) {
+            state.direct_compositing_reasons |=
+                CompositingReason::kAffectedBySafeAreaBottom;
+          }
         }
       }
 
