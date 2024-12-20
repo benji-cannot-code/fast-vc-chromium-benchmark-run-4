261cb986f7ac959e6f6f14313932aa061395b9be
Dana Fried <dfried@chromium.org>
2024-12-20 14:35:24 -0800

    [User Education] Add UE2.5 precondition for conflicts with known notices
    Copies over code from 2.0 into a precondition - no additional changes.
    (See BrowserFeaturePromoController20::CanShowPromoForElement().)
    
    Note: this is very difficult to test as it would require triggering the
    prompts in question, which are very situational.
    
    Low-Coverage-Reason: HARD_TO_TEST
    Bug: 369887098
    Change-Id: I417e4e296af62229ffc3fa4b5463ab12f2d53218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108103
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399386}

--- chrome/browser/ui/views/user_education/impl/browser_feature_promo_preconditions.h
+++ chrome/browser/ui/views/user_education/impl/browser_feature_promo_preconditions.h
@@ -18,6 +18,8 @@ DECLARE_FEATURE_PROMO_PRECONDITION_IDENTIFIER_VALUE(
     kToolbarNotCollapsedPrecondition);
 DECLARE_FEATURE_PROMO_PRECONDITION_IDENTIFIER_VALUE(
     kBrowserNotClosingPrecondition);
+DECLARE_FEATURE_PROMO_PRECONDITION_IDENTIFIER_VALUE(
+    kNoCriticalNoticeShowingPrecondition);
 
 // Requires that the window a promo will be shown in is active.
 class WindowActivePrecondition
@@ -69,6 +71,23 @@ class BrowserNotClosingPrecondition
  public:
   explicit BrowserNotClosingPrecondition(BrowserView& browser_view);
   ~BrowserNotClosingPrecondition() override;
+
+  // FeaturePromoPreconditionBase:
+  user_education::FeaturePromoResult CheckPrecondition(
+      ComputedData& data) const override;
+
+ private:
+  const raw_ref<BrowserView> browser_view_;
+};
+
+// Certain critical notices do not (yet) use the Product Messaging Service.
+// TODO(https://crbug.com/324785292): When the migration is done, remove this
+// precondition.
+class NoCriticalNoticeShowingPrecondition
+    : public user_education::FeaturePromoPreconditionBase {
+ public:
+  explicit NoCriticalNoticeShowingPrecondition(BrowserView& browser_view);
+  ~NoCriticalNoticeShowingPrecondition() override;
 
   // FeaturePromoPreconditionBase:
   user_education::FeaturePromoResult CheckPrecondition(
