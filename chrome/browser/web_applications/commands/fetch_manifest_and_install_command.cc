42aad0bac72b5e7d1bc09b32ef1918e7a80db8ab
Daniel Murphy <dmurph@chromium.org>
2024-12-19 17:06:26 -0800

    [PWA] Fix visibility flakiness on mac
    R=mek@chromium.org
    
    Change-Id: I17b8efedd197a06dff4ffc56d010b69c1997b566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112697
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398960}

--- chrome/browser/web_applications/commands/fetch_manifest_and_install_command.cc
+++ chrome/browser/web_applications/commands/fetch_manifest_and_install_command.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/auto_reset.h"
+#include "base/check_is_test.h"
 #include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
@@ -165,7 +167,19 @@ bool IsShortcutCreated(WebAppRegistrar& registrar,
   return os_state->has_shortcut();
 }
 
+static bool& ShouldBypassVisibilityChecks() {
+  static bool g_bypass_visibility_checking = false;
+  return g_bypass_visibility_checking;
+}
+
 }  // namespace
+
+// static
+base::AutoReset<bool>
+FetchManifestAndInstallCommand::BypassVisibilityCheckForTesting() {
+  CHECK_IS_TEST();
+  return base::AutoReset<bool>(&ShouldBypassVisibilityChecks(), true);
+}
 
 FetchManifestAndInstallCommand::FetchManifestAndInstallCommand(
     webapps::WebappInstallSource install_surface,
@@ -224,7 +238,8 @@ void FetchManifestAndInstallCommand::StartWithLock(
     return;
   }
 
-  if (web_contents()->GetVisibility() != content::Visibility::VISIBLE) {
+  if (web_contents()->GetVisibility() != content::Visibility::VISIBLE &&
+      !ShouldBypassVisibilityChecks()) {
     Abort(webapps::InstallResultCode::kCancelledDueToMainFrameNavigation);
     return;
   }
@@ -286,6 +301,9 @@ void FetchManifestAndInstallCommand::DidFinishNavigation(
 
 void FetchManifestAndInstallCommand::OnVisibilityChanged(
     content::Visibility visibility) {
+  if (ShouldBypassVisibilityChecks()) {
+    return;
+  }
   if (visibility == content::Visibility::VISIBLE) {
     return;
   }
