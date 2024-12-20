53077e57ccc6d0a3b4c262954b8976ff60501a5e
Andrew Grieve <agrieve@chromium.org>
2024-12-20 07:58:50 -0800

    Android: Add @Contract annotation for use with NullAway
    Bug: 40657579
    Change-Id: I4cf6eca0a96dccfbe51ccff24f8c40ce562d18bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6107639
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399184}

---
+++ build/android/java/src/org/chromium/build/annotations/Contract.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.build.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * See: https://github.com/uber/NullAway/wiki/Supported-Annotations#contracts
+ *
+ * <p>Not directly using NullAway's annotations so that Cronet does not need the extra dep.
+ *
+ * <pre>
+ * Examples:
+ * // The contract is: "If the parameter is null, the method will return false".
+ * // NullAway infers nullness from the inverse: Returning "true" means the parameter is non-null.
+ * @Contract("null -> false")
+ * // Returning false means the second parameter is non-null.
+ * @Contract("_, null -> true")
+ * </pre>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface Contract {
+    String value();
+}
