e57d22d433a691e3835f0f1898e7d5d5b80afcb5
Thiabaud Engelbrecht <thiabaud@google.com>
2024-12-20 09:51:34 -0800

    [pre_freeze] Remove features
    This has been launched for a while, so we can safely remove the feature.
    
    Bug: 375432350
    Change-Id: Iee8ec3b01a635fd5ed3e3e2f91ed2ab527bb4518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108511
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399244}

--- base/android/pre_freeze_background_memory_trimmer.cc
+++ base/android/pre_freeze_background_memory_trimmer.cc
@@ -189,14 +189,6 @@ bool IsMadvisePageoutSupported() {
 
 }  // namespace
 
-BASE_FEATURE(kOnPreFreezeMemoryTrim,
-             "OnPreFreezeMemoryTrim",
-             FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kIsTrimMemoryBackgroundCritical,
-             "IsTrimMemoryBackgroundCritical",
-             FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kShouldFreezeSelf,
              "ShouldFreezeSelf",
              FEATURE_DISABLED_BY_DEFAULT);
@@ -387,14 +379,6 @@ void PreFreezeBackgroundMemoryTrimmer::PostDelayedBackgroundTaskInternal(
   DCHECK(SupportsModernTrim());
 
   RegisterPrivateMemoryFootprintMetric();
-
-  if (!base::FeatureList::IsEnabled(kOnPreFreezeMemoryTrim)) {
-    task_runner->PostDelayedTask(
-        from_here,
-        BindOnce(std::move(task), MemoryReductionTaskContext::kDelayExpired),
-        delay);
-    return;
-  }
 
   PostDelayedBackgroundTaskModern(task_runner, from_here, std::move(task),
                                   delay);
@@ -758,14 +742,12 @@ bool PreFreezeBackgroundMemoryTrimmer::SupportsModernTrim() {
 
 // static
 bool PreFreezeBackgroundMemoryTrimmer::ShouldUseModernTrim() {
-  return SupportsModernTrim() &&
-         base::FeatureList::IsEnabled(kOnPreFreezeMemoryTrim);
+  return SupportsModernTrim();
 }
 
 // static
 bool PreFreezeBackgroundMemoryTrimmer::IsTrimMemoryBackgroundCritical() {
-  return SupportsModernTrim() &&
-         base::FeatureList::IsEnabled(kIsTrimMemoryBackgroundCritical);
+  return SupportsModernTrim();
 }
 
 // static
--- base/android/pre_freeze_background_memory_trimmer.h
+++ base/android/pre_freeze_background_memory_trimmer.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base::android {
 class MemoryPurgeManagerAndroid;
 
-BASE_EXPORT BASE_DECLARE_FEATURE(kOnPreFreezeMemoryTrim);
 BASE_EXPORT BASE_DECLARE_FEATURE(kShouldFreezeSelf);
 
 // Starting from Android U, apps are frozen shortly after being backgrounded
