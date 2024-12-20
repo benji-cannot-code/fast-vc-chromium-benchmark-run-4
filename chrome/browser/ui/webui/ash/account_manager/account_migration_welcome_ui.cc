65c69058d4432332df59d4fea18226b60cd97ca7
dpapad <dpapad@chromium.org>
2024-12-20 11:03:02 -0800

    WebUI: Remove more ui/resources/grit/webui_resources.h includes.
    These includes are not needed, since they are not actually used in these
    files.
    
    This is in preparation of moving ui/resources/grit/webui_resources.h to
    ui/webui/resources/grit/webui_resources.h, to reveal hidden
    ui/webui/ dependencies across the codebase.
    
    Bug: 385086925
    Change-Id: I2b18a6b2850baa1a617f9b98890c85d1032343a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110234
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399279}

--- chrome/browser/ui/webui/ash/account_manager/account_migration_welcome_ui.cc
+++ chrome/browser/ui/webui/ash/account_manager/account_migration_welcome_ui.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_data_source.h"
 #include "net/base/url_util.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/resources/grit/webui_resources.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
 #include "ui/webui/webui_util.h"
--- chrome/browser/ui/webui/ash/emoji/emoji_ui.cc
+++ chrome/browser/ui/webui/ash/emoji/emoji_ui.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/display/screen.h"
-#include "ui/resources/grit/webui_resources.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/webui/color_change_listener/color_change_handler.h"
 #include "ui/webui/webui_util.h"
