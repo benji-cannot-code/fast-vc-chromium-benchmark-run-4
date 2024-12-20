a1b191a50279b628f7766c24de48c4204483020b
Claire Chambers <clchambers@microsoft.com>
2024-12-20 11:09:19 -0800

    Add README.md for native paint worklet animations
    Change-Id: I0d08baf89a095b90e92cb6701d96bb781d882dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074070
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Claire Chambers <clchambers@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1399283}

--- third_party/blink/renderer/core/css/native_paint_image_generator.h
+++ third_party/blink/renderer/core/css/native_paint_image_generator.h
@@ -13,11 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// See README.md in modules/csspaint/nativepaint for more detail in how this
+// fits in the overall design. This class is used as a proxy for native
+// paint definitions, with its subclasses *PaintImageGenerator.
 class CORE_EXPORT NativePaintImageGenerator
     : public GarbageCollected<NativePaintImageGenerator> {
  public:
   virtual ~NativePaintImageGenerator() = default;
 
+  // See comment in NativeCssPaintDefinition::GetAnimationForProperty
   virtual Animation* GetAnimationIfCompositable(const Element* element) = 0;
 
   virtual void Shutdown() = 0;
