c6c1e39aa832326941cda48b37184fd6e8a7ec2c
Hailey Wang <haileywang@google.com>
2024-12-20 12:21:54 -0800

    [Collaboration] Add JNI layer for Android delegate
    Bug: 383402890
    Change-Id: I55179346161b2aba67d7a416574545209eb13ac4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092210
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399321}

--- components/collaboration/public/collaboration_controller_delegate.h
+++ components/collaboration/public/collaboration_controller_delegate.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "components/data_sharing/public/group_data.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/jni_android.h"
+#endif  // BUILDFLAG(IS_ANDROID)
+
 namespace collaboration {
 
 // The class responsible for controlling actions on platform specific UI
@@ -16,10 +20,12 @@ namespace collaboration {
 class CollaborationControllerDelegate {
  public:
   struct ErrorInfo {
+    // GENERATED_JAVA_ENUM_PACKAGE: (
+    //   org.chromium.components.collaboration)
     enum class Type {
-      kUnknown,
+      kUnknown = 0,
       // Show the generic error dialog.
-      kGenericError,
+      kGenericError = 1,
     };
 
     explicit ErrorInfo(Type type) : type(type) {}
@@ -29,10 +35,12 @@ class CollaborationControllerDelegate {
     bool operator==(const ErrorInfo& other) const { return type == other.type; }
   };
 
+  // GENERATED_JAVA_ENUM_PACKAGE: (
+  //   org.chromium.components.collaboration)
   enum class Outcome {
-    kSuccess,
-    kFailure,
-    kCancel,
+    kSuccess = 0,
+    kFailure = 1,
+    kCancel = 2,
   };
 
   CollaborationControllerDelegate() = default;
@@ -82,6 +90,11 @@ class CollaborationControllerDelegate {
 
   // Focus the UI screen associated with the current delegate instance.
   virtual void PromoteCurrentScreen() = 0;
+
+#if BUILDFLAG(IS_ANDROID)
+  // Returns the Java object of the CollaborationControllerDelegate.
+  virtual base::android::ScopedJavaLocalRef<jobject> GetJavaObject();
+#endif  // BUILDFLAG(IS_ANDROID)
 };
 
 }  // namespace collaboration
