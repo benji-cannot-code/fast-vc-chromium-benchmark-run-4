169b9cebff144a83534319b93ce0a91c7ef67fd8
Nihar Majmudar <niharm@google.com>
2024-12-20 12:23:10 -0800

    [lensoverlay] Update placeholder when side panel searchbox is CSB.
    Placeholder should say "Ask about this page" in the side panel
    searchbox when it's contextual.
    
    Also updates search icon color, size, and thumbnail positioning.
    
    Change-Id: I4c1cc13d29b7024c71b4dba6136d5f1356b660c7
    Bug: 384843434, 385003230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111786
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399323}

--- ui/webui/resources/cr_components/searchbox/searchbox.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.ts
@@ -184,9 +184,10 @@ export class SearchboxElement extends SearchboxElementBase {
         value: false,
       },
 
-      placeholderText_: {
+      placeholderText: {
         type: String,
-        computed: `computePlaceholderText_(showThumbnail)`,
+        reflectToAttribute: true,
+        notify: true,
       },
 
       /** Searchbox default icon (i.e., Google G icon or the search loupe). */
@@ -270,7 +271,7 @@ export class SearchboxElement extends SearchboxElementBase {
   private lastInput_: Input;
   private lastQueriedInput_: string|null;
   private pastedInInput_: boolean;
-  private placeholderText_: string;
+  private placeholderText: string;
   private searchboxIcon_: string;
   private searchboxVoiceSearchEnabled_: boolean;
   private searchboxLensSearchEnabled_: boolean;
@@ -803,6 +804,9 @@ export class SearchboxElement extends SearchboxElementBase {
   }
 
   private computePlaceholderText_(): string {
+    if (this.placeholderText) {
+      return this.placeholderText;
+    }
     return this.showThumbnail ? this.i18n('searchBoxHintMultimodal') :
                                 this.i18n('searchBoxHint');
   }
