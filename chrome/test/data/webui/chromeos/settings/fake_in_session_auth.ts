de28b1a3402da8cae4656a106d4fb0a798e66669
Brittany Hartmire <bhartmire@google.com>
2024-12-20 15:28:53 -0800

    [High5]: Fix auth modals in settings
    Recording: https://drive.google.com/file/d/1Er62YCqGYHgRHGXxxMrHJZwcka5ebmxr/view
    
    on atlas with pixel8. Created new mojo interface fake to inject in
    existing mocha tests.
    
    Test: manual test with PIN setup, password setup, Google account setup
    Change-Id: Ic5827c82053d52d0eaeefaac3bf3b87107cd993b
    Bug: b:382116381, b:382116951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6081997
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399410}

---
+++ chrome/test/data/webui/chromeos/settings/fake_in_session_auth.ts
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Fake implementation of InSessionAuth for testing.
+ */
+
+import {RequestTokenReply} from 'chrome://resources/mojo/chromeos/components/in_session_auth/mojom/in_session_auth.mojom-webui.js';
+
+/**
+ * Fake implementation of TimeDelta used inside the InSessionAuth token reply
+ * timeout.
+ */
+class TimeDelta {
+  microseconds: bigint;
+
+  constructor(microseconds: bigint) {
+    this.microseconds = microseconds;
+  }
+}
+
+/**
+ * Fake implementation of InSessionAuthInterface
+ */
+export class FakeInSessionAuth {
+  tokenReply_: RequestTokenReply|null;
+
+  constructor() {
+    this.tokenReply_ = { token: 'token', timeout: new TimeDelta(BigInt(1000)) }
+  }
+
+  requestToken(): {reply: null|RequestTokenReply} {
+    return {reply: this.tokenReply_};
+  }
+
+  checkToken(): boolean {
+    return true;
+  }
+
+  invalidateToken() {
+    this.tokenReply_ = null;
+  }
+}
