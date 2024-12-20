2879d6c4159d7026864a76a48343280280b490e8
Sam Maier <smaier@chromium.org>
2024-12-20 14:31:36 -0800

    Android: add nullability checks to vr
    Bug: 40657579
    Change-Id: I12022f85b74130eea9c5fb3bdf6e9b45146897df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6115019
    Owners-Override: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399381}

--- components/webxr/android/java/src/org/chromium/components/webxr/WebXrAndroidFeatureMap.java
+++ components/webxr/android/java/src/org/chromium/components/webxr/WebXrAndroidFeatureMap.java
@@ -8,7 +8,10 @@ package org.chromium.components.webxr;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.build.annotations.NullMarked;
+
 @JNINamespace("webxr")
+@NullMarked
 public final class WebXrAndroidFeatureMap {
     /** Convenience method to check if OpenXR should be enabled due to complex state. */
     public static boolean isOpenXrEnabled() {
