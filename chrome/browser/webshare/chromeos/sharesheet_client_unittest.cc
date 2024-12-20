c97da8abef72a7fb2753d2d04482b4415ff27614
James Cook <jamescook@chromium.org>
2024-12-19 16:02:20 -0800

    lacros cleanup: Remove lacros code from SharesheetClient
    Lacros has sunset and is no longer built or tested. The sharesheet mojo
    crosapi is being removed in another CL. Remove the calling code from
    SharesheetClient and clean up IS_CHROMEOS_ASH/LACROS macros.
    
    Bug: 373971535
    Change-Id: Ic38ee12c8fce57e7477d7fbe215307365ff89861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112300
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398926}

--- chrome/browser/webshare/chromeos/sharesheet_client_unittest.cc
+++ chrome/browser/webshare/chromeos/sharesheet_client_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sharesheet/sharesheet_types.h"
 #include "chrome/browser/webshare/prepare_directory_task.h"
@@ -26,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/web_contents_tester.h"
 #include "third_party/blink/public/mojom/webshare/webshare.mojom.h"
 #include "url/gurl.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/ash/file_manager/path_util.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 namespace webshare {
 
