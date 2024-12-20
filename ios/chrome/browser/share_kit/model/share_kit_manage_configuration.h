ac1b6c03689f47a8af09d3a864ef347ee6b380b2
Gauthier Ambard <gambard@chromium.org>
2024-12-20 07:24:03 -0800

    [iOS] Add a completion block for the manage flow
    Bug: 384512074
    Change-Id: I9053a48fc0bfd57d0e3be21ae5c228e15bfe3156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110598
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399170}

--- ios/chrome/browser/share_kit/model/share_kit_manage_configuration.h
+++ ios/chrome/browser/share_kit/model/share_kit_manage_configuration.h
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Application commands handler.
 @property(nonatomic, weak) id<ApplicationCommands> applicationHandler;
 
+// Executed when the manage flow ended.
+@property(nonatomic, copy) void (^completionBlock)(BOOL succeeded);
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_SHARE_KIT_MODEL_SHARE_KIT_MANAGE_CONFIGURATION_H_
