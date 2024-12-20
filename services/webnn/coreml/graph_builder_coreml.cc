1bb644d2f13b11384f408549ad6d36dc5d7949ca
Phillis Tang <phillis@chromium.org>
2024-12-20 15:08:04 -0800

    webnn: add wpt for bidirectional lstm
    Fixes some minor bugs with CoreML bidrection implementation.
    
    Also adjusted ULP for this complex op to 3, this is same as gru.
    
    Bug: 360052663
    Change-Id: Ie15ab15a359be70e219fd61ee0d3a3fea21f3405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6103076
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399398}

--- services/webnn/coreml/graph_builder_coreml.cc
+++ services/webnn/coreml/graph_builder_coreml.cc
@@ -3348,7 +3348,7 @@ GraphBuilderCoreml::AddOperationForLstm(
 
   static constexpr char kForwardDirection[] = "forward";
   static constexpr char kBackwardDirection[] = "reverse";
-  static constexpr char kBothDirection[] = "both";
+  static constexpr char kBiDirectional[] = "bidirectional";
 
   uint32_t num_of_directions =
       direction == mojom::RecurrentNetworkDirection::kBoth ? 2 : 1;
@@ -3440,7 +3440,7 @@ GraphBuilderCoreml::AddOperationForLstm(
       direction_param_value = kBackwardDirection;
       break;
     case mojom::RecurrentNetworkDirection::kBoth:
-      direction_param_value = kBothDirection;
+      direction_param_value = kBiDirectional;
       break;
   }
 
@@ -3613,10 +3613,11 @@ GraphBuilderCoreml::AddOperationForLstm(
   } else {
     // Else, the first output of CoreML lstm is the output of the last step with
     // shape [1, batchSize, hiddenSize].
-    ASSIGN_OR_RETURN(uint64_t unused_second_output,
-                     GenerateInternalOperandInfo(
-                         data_type, base::span<const uint32_t>(
-                                        {1, batch_size, hidden_size})));
+    ASSIGN_OR_RETURN(
+        uint64_t unused_second_output,
+        GenerateInternalOperandInfo(
+            data_type, base::span<const uint32_t>(
+                           {1, batch_size, num_of_directions * hidden_size})));
     PopulateNamedValueType(unused_second_output, *op->add_outputs());
   }
 
