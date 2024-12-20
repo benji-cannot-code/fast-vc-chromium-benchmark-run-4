2f7e2c16ff4560e61809f572bc690cdf93bcb046
Finnur Thorarinsson <finnur@chromium.org>
2024-12-20 13:12:44 -0800

    [PWA]: Migrate away from IsInstallState (part 3a)
    This handles some of the cases, mostly in our web_applications
    production code (tests have been migrated).
    
    Bug: 381832688
    Change-Id: I2897f31ad992e165fffde909514fe1634f2f3265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6093867
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399346}

--- chrome/browser/web_applications/ash/migrations/adobe_express_oem_to_default_migration.cc
+++ chrome/browser/web_applications/ash/migrations/adobe_express_oem_to_default_migration.cc
@@ -14,11 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web_app::migrations {
 
 void MigrateAdobeExpressFromOemInstallToDefault(WebAppSyncBridge* sync_bridge) {
-  if (!sync_bridge->registrar().IsInstallState(
-          ash::kAdobeExpressAppId,
-          {proto::InstallState::SUGGESTED_FROM_ANOTHER_DEVICE,
-           proto::InstallState::INSTALLED_WITHOUT_OS_INTEGRATION,
-           proto::InstallState::INSTALLED_WITH_OS_INTEGRATION})) {
+  if (sync_bridge->registrar().IsNotInRegistrar(ash::kAdobeExpressAppId)) {
     return;
   }
 
