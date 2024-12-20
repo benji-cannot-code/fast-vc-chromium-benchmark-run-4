60357128a6ae1169f7fe6c40dd0d9676d69a33e4
Neil Coronado <nemco@google.com>
2024-12-20 13:14:06 -0800

    [TabGroupReorder] Fix reordered group divider height
    Fix reordered group divider height.
    -Remove original_x/y for foregrounded tabs.
    -Additionally, remove unused SetOpacity calls.
    
    Bug: 376067968
    Change-Id: I52dbff07688ba53e0f370f3e500f29aeca85ce30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108657
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399349}

--- chrome/browser/android/compositor/layer/tab_handle_layer.cc
+++ chrome/browser/android/compositor/layer/tab_handle_layer.cc
@@ -56,12 +56,6 @@ void TabHandleLayer::SetProperties(
   }
 
   y += top_margin;
-  float original_x = x;
-  float original_y = y;
-  if (foreground_) {
-    x = 0;
-    y = 0;
-  }
 
   bool is_rtl = l10n_util::IsLayoutRtl();
 
@@ -97,7 +91,6 @@ void TabHandleLayer::SetProperties(
   }
 
   if (title_layer) {
-    title_layer->setOpacity(1.0f);
     unsigned expected_children = 4;
     title_layer_ = title_layer->layer();
     if (tab_->children().size() < expected_children) {
@@ -128,14 +121,6 @@ void TabHandleLayer::SetProperties(
   tab_outline_->SetBounds(tab_bounds);
   tab_outline_->SetBorder(
       tab_handle_outline_resource->Border(tab_outline_->bounds()));
-
-  if (foreground_) {
-    decoration_tab_->SetPosition(gfx::PointF(original_x, original_y));
-    tab_outline_->SetPosition(gfx::PointF(original_x, original_y));
-  } else {
-    decoration_tab_->SetPosition(gfx::PointF(0, 0));
-    tab_outline_->SetPosition(gfx::PointF(0, 0));
-  }
 
   // Display the tab outline only for the currently selected tab in group when
   // TabGroupIndicator is enabled.
@@ -176,11 +161,7 @@ void TabHandleLayer::SetProperties(
     start_divider_->SetBounds(divider_resource->size());
     int divider_x =
         is_rtl ? width - divider_width - divider_offset_x : divider_offset_x;
-    if (foreground_) {
-      divider_x += original_x;
-    }
     start_divider_->SetPosition(gfx::PointF(divider_x, divider_y));
-    start_divider_->SetOpacity(1.0f);
   }
 
   if (!is_end_divider_visible) {
@@ -191,11 +172,7 @@ void TabHandleLayer::SetProperties(
     end_divider_->SetBounds(divider_resource->size());
     int divider_x =
         is_rtl ? divider_offset_x : width - divider_width - divider_offset_x;
-    if (foreground_) {
-      divider_x += original_x;
-    }
     end_divider_->SetPosition(gfx::PointF(divider_x, divider_y));
-    end_divider_->SetOpacity(1.0f);
   }
 
   if (title_layer) {
@@ -209,10 +186,6 @@ void TabHandleLayer::SetProperties(
     int title_x = is_rtl ? padding_left + close_width : padding_left;
     title_layer->setBounds(
         gfx::Size(width - padding_right - padding_left - close_width, height));
-    if (foreground_) {
-      title_x += original_x;
-      title_y += original_y;
-    }
     title_layer->layer()->SetPosition(gfx::PointF(title_x, title_y));
     if (is_loading) {
       title_layer->SetIsLoading(true);
@@ -241,10 +214,6 @@ void TabHandleLayer::SetProperties(
 
     int close_x = is_rtl ? padding_left - close_button_padding
                          : width - padding_right - close_width;
-    if (foreground_) {
-      close_x += original_x;
-      close_y += original_y;
-    }
 
     float background_left_offset =
         (close_button_background_resource->size().width() -
@@ -286,6 +255,9 @@ TabHandleLayer::TabHandleLayer(LayerTitleCache* layer_title_cache)
   tab_->AddChild(tab_outline_);
   tab_->AddChild(close_button_hover_highlight_);
   close_button_hover_highlight_->AddChild(close_button_);
+
+  decoration_tab_->SetPosition(gfx::PointF(0, 0));
+  tab_outline_->SetPosition(gfx::PointF(0, 0));
 
   // The divider is added as a separate child so its opacity can be controlled
   // separately from the other tab items.
