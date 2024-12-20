9a1d0bb30b247a701ab7f0dcc4d77c761b81e53a
Celeste Pan <celestepan@microsoft.com>
2024-12-20 08:20:27 -0800

    [Masonry] Introduce parsing for masonry-direction.
    This CL introduces the parsing for the CSS property masonry-direction.
    
    This is part of: https://chromestatus.com/feature/5149560434589696
    
    The properties are behind the CSSMasonryLayout feature flag. The spec
    definition for masonry-direction can be found here:
    https://tabatkins.github.io/specs/css-masonry/#masonry-direction.
    
    Bug: 343257585
    Change-Id: I9835985037cd9e24107eb7caaf2898c0a16ce2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099065
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Celeste Pan <celestepan@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1399192}

--- third_party/blink/renderer/core/css/css_property_equality.cc
+++ third_party/blink/renderer/core/css/css_property_equality.cc
@@ -518,6 +518,8 @@ bool CSSPropertyEquality::PropertiesEqual(const PropertyHandle& property,
       return a.MarkerStartResource() == b.MarkerStartResource();
     case CSSPropertyID::kMaskType:
       return a.MaskType() == b.MaskType();
+    case CSSPropertyID::kMasonryDirection:
+      return a.MasonryDirection() == b.MasonryDirection();
     case CSSPropertyID::kMasonryFill:
       return a.MasonryFill() == b.MasonryFill();
     case CSSPropertyID::kMasonrySlack:
