7dcc44d9693527e8696646aa966409ba128bdf20
Ankush Singh <ankushkush@google.com>
2024-12-20 01:44:38 -0800

    [search-engines] Update initial sync to keep account data separate
    All changes are behind kSeparateLocalAndAccountSearchEngines flag.
    
    This updates TemplateURLService::MergeDataAndStartSyncing to keep
    account data separate from local data and avoiding an actual merge.
    
    Bug: 374903497
    Change-Id: I34149819c5d07521dc1db5442aca418f802d2cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097771
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399064}

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/search_engines_switches.h"
 #include "components/search_engines/search_terms_data.h"
 #include "components/search_engines/template_url_data.h"
+#include "components/sync/base/features.h"
 #include "components/url_formatter/url_formatter.h"
 #include "google_apis/google_api_keys.h"
 #include "net/base/mime_util.h"
@@ -2203,8 +2204,10 @@ const TemplateURLData& TemplateURL::data() const {
 }
 
 TemplateURLData& TemplateURL::active_data() {
-  CHECK(local_data_);
-  return *local_data_;
+  CHECK(local_data_ || base::FeatureList::IsEnabled(
+                           syncer::kSeparateLocalAndAccountSearchEngines));
+  // TODO(crbug.com/374903497): Implement conflict resolution.
+  return local_data_ ? *local_data_ : *account_data_;
 }
 
 void TemplateURL::set_short_name(const std::u16string& short_name) {
