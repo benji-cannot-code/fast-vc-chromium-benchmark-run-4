125ce9f5c5c420945156e1d0c398c43ed3f37c32
Anthony Vallée-Dubois <anthonyvd@google.com>
2024-12-20 08:37:12 -0800

    Move SQL histograms into their own histograms.xml file
    There's quite a bit of metrics work currently happening in the sql/
    code, so it makes sense for the folks working on it to own the
    associated histograms (and also reduces review load on storage/
    histograms owners).
    
    Change-Id: I7998faa2526c3abfbd8db9d322f12699cec0792c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109794
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399202}

--- sql/database.h
+++ sql/database.h
@@ -303,7 +303,7 @@ class COMPONENT_EXPORT(SQL) Database {
         // NOTREACHED() is invalid in a consteval context so compilation will
         // fail iff the string is invalid.
         NOTREACHED() << "Invalid database tag. Did you add it to the "
-                        "DatabaseTag variant in storage/histograms.xml?";
+                        "DatabaseTag variant in sql/histograms.xml?";
       }
     }
 
