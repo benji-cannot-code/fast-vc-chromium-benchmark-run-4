32bfb9df2e9e545213da7e9758a9289de3405c9c
Sammie Quon <sammiequon@chromium.org>
2024-12-20 09:08:07 -0800

    coral: Remove coral saved group flag
    It was temporary as we did not show something that clearly did not work.
    It works mostly now and can be part of the coral flag.
    
    Test: none
    Bug: 365839564
    Change-Id: If59380e14c1dfe5c2e7d15096ac9fd3ee1ffcae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112794
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399223}

--- ash/wm/desks/templates/saved_desk_library_view.cc
+++ ash/wm/desks/templates/saved_desk_library_view.cc
@@ -309,7 +309,7 @@ SavedDeskLibraryView::SavedDeskLibraryView() {
   // Create grids depending on which features are enabled.
   if (saved_desk_util::AreDesksTemplatesEnabled()) {
     auto group_contents = GetLabelAndGridGroupContents();
-    if (features::IsCoralSavedDeskFeatureEnabled()) {
+    if (features::IsCoralFeatureEnabled()) {
       grid_labels_.push_back(group_contents->AddChildView(
           MakeGridLabel(IDS_ASH_DESKS_TEMPLATES_LIBRARY_TEMPLATES_GRID_LABEL)));
     }
@@ -321,7 +321,7 @@ SavedDeskLibraryView::SavedDeskLibraryView() {
   }
   if (saved_desk_util::ShouldShowSavedDesksOptions()) {
     auto group_contents = GetLabelAndGridGroupContents();
-    if (features::IsCoralSavedDeskFeatureEnabled()) {
+    if (features::IsCoralFeatureEnabled()) {
       grid_labels_.push_back(group_contents->AddChildView(MakeGridLabel(
           IDS_ASH_DESKS_TEMPLATES_LIBRARY_SAVE_AND_RECALL_GRID_LABEL)));
     }
@@ -331,9 +331,9 @@ SavedDeskLibraryView::SavedDeskLibraryView() {
 
     scroll_contents->AddChildView(std::move(group_contents));
   }
-  if (features::IsCoralSavedDeskFeatureEnabled()) {
+  if (features::IsCoralFeatureEnabled()) {
     auto group_contents = GetLabelAndGridGroupContents();
-    if (features::IsCoralSavedDeskFeatureEnabled()) {
+    if (features::IsCoralFeatureEnabled()) {
       grid_labels_.push_back(group_contents->AddChildView(
           MakeGridLabel(IDS_ASH_DESKS_TEMPLATES_LIBRARY_CORAL_GRID_LABEL)));
     }
@@ -572,7 +572,7 @@ std::optional<gfx::Rect> SavedDeskLibraryView::GetDeskPreviewBoundsForLaunch(
 }
 
 void SavedDeskLibraryView::UpdateGridLabels() {
-  if (!features::IsCoralSavedDeskFeatureEnabled()) {
+  if (!features::IsCoralFeatureEnabled()) {
     return;
   }
 
