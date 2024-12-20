59e4d0e88e926bc2d387045ddf6b924d2f55c894
Andrew Grieve <agrieve@chromium.org>
2024-12-20 09:43:49 -0800

    Fix missing @Nullable on formatUrlForSecurityDisplay()
    Bug: 385146869
    Change-Id: Ia7adae7b40cacb1931161e80a2e7298c983f8922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108092
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399239}

--- components/url_formatter/android/java/src/org/chromium/components/url_formatter/UrlFormatter.java
+++ components/url_formatter/android/java/src/org/chromium/components/url_formatter/UrlFormatter.java
@@ -13,6 +13,7 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.url.GURL;
 import org.chromium.url.Origin;
 
@@ -232,7 +233,7 @@ public final class UrlFormatter {
      * @deprecated Please use {@link #formatUrlForSecurityDisplay(GURL, int)} instead.
      */
     @Deprecated
-    public static String formatUrlForSecurityDisplay(String uri, @SchemeDisplay int schemeDisplay) {
+    public static String formatUrlForSecurityDisplay(@Nullable String uri, @SchemeDisplay int schemeDisplay) {
         return UrlFormatterJni.get().formatStringUrlForSecurityDisplay(uri, schemeDisplay);
     }
 
@@ -257,6 +258,6 @@ public final class UrlFormatter {
 
         String formatOriginForSecurityDisplay(Origin origin, @SchemeDisplay int schemeDisplay);
 
-        String formatStringUrlForSecurityDisplay(String url, @SchemeDisplay int schemeDisplay);
+        String formatStringUrlForSecurityDisplay(@Nullable String url, @SchemeDisplay int schemeDisplay);
     }
 }
