4a3d3a4ef29b3a65f9e2c66b0d3f60ab4f089f39
Mikel Astiz <mastiz@chromium.org>
2024-12-20 15:16:04 -0800

    Update tests to avoid implicit conversion between GaiaId and std::string
    Class GaiaId was introduced to improve type safety and avoid strings
    being used, which is more error-prone.
    
    Temporarily, implicit conversion between GaiaId and std::string was
    allowed, until all occurrences relying on it are migrated. This patch
    updates tests under ash/webui/eche_app_ui.
    
    This CL was uploaded by git cl split.
    
    R=nayebi@google.com
    
    Bug: 380416867
    Change-Id: I5290fbaefe3886ab2642fd366b8dfacd204dfa11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105216
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399400}

--- ash/webui/eche_app_ui/eche_app_manager_unittest.cc
+++ ash/webui/eche_app_ui/eche_app_manager_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/testing_pref_service.h"
 #include "device/bluetooth/dbus/bluez_dbus_manager.h"
 #include "device/bluetooth/dbus/fake_bluetooth_debug_manager_client.h"
+#include "google_apis/gaia/gaia_id.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -147,7 +148,7 @@ class EcheAppManagerTest : public AshTestBase {
         SystemInfo::Builder()
             .SetDeviceName(kFakeDeviceName)
             .SetBoardName(kFakeBoardName)
-            .SetGaiaId(kFakeGaiaId)
+            .SetGaiaId(GaiaId(kFakeGaiaId))
             .SetDeviceType(kFakeDeviceType)
             .Build(),
         fake_phone_hub_manager_.get(), fake_device_sync_client_.get(),
