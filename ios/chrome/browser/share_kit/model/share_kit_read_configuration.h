5973cf799c5b0dc8ad82d562aa04d24e44907625
Gauthier Ambard <gambard@chromium.org>
2024-12-20 07:24:36 -0800

    [iOS] Remove deprecated groupID
    No longer used.
    
    Bug: 384512074
    Change-Id: I1d1b72d28994be02c0474133e77b75a413697456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108809
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399171}

--- ios/chrome/browser/share_kit/model/share_kit_read_configuration.h
+++ ios/chrome/browser/share_kit/model/share_kit_read_configuration.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The configuration to read a single group.
 @interface ShareKitReadGroupParamConfiguration : NSObject
 
-// The ID of the group to be read.
-// TODO(crbug.com/384512074): Remove this once all clients switched to collabID.
-@property(nonatomic, copy) NSString* groupID;
-
 // The collab ID of the group to be read, also sometimes called groupID.
 @property(nonatomic, copy) NSString* collabID;
 
