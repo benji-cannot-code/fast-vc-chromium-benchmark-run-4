c422a0edc5fe97615137be966466ab6c73831bc2
Mark Schillaci <mschillaci@google.com>
2024-12-20 18:00:57 -0800

    Add metric to track element type navigation in Android accessibility
    This CL adds a new metric to track the type of elements that a user
    navigates a page by when using the next/previous html element navigation
    type (e.g. link, heading, paragraph, etc). We split the metric with
    TalkBack running alone, not running, or running with a different unknown
    accessibility service. The metric should give us an indication of the
    types of navigations most commonly used, where we need to optimize for
    upcoming AXMode projects, and if apps besides TalkBack are using this
    API.
    
    NO_IFTTT=Initial commit/addition, seems to be a false positive.
    
    AX-Relnotes: N/A
    Bug: 385186714
    Change-Id: I7fa8e324be547cfbfc92cb08f23256f07d5e1542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112433
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399462}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
@@ -65,6 +65,7 @@ import android.content.ReceiverCallNotAllowedException;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.Pair;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
@@ -1461,6 +1462,7 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
             boolean forwards,
             boolean canWrap,
             boolean setSequentialFocus) {
+        Pair<Boolean, Boolean> talkbackEnabledState = AccessibilityState.getTalkBackEnabledState();
         int id =
                 WebContentsAccessibilityImplJni.get()
                         .findElementType(
@@ -1469,7 +1471,9 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
                                 elementType,
                                 forwards,
                                 canWrap,
-                                elementType.isEmpty());
+                                elementType.isEmpty(),
+                                talkbackEnabledState.first,
+                                talkbackEnabledState.second);
         if (id == 0) return false;
 
         if (setSequentialFocus) {
@@ -2277,7 +2281,9 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
                 String elementType,
                 boolean forwards,
                 boolean canWrapToLastElement,
-                boolean useDefaultPredicate);
+                boolean useDefaultPredicate,
+                boolean isTalkbackEnabled,
+                boolean isOnlyTalkbackEnabled);
 
         void setTextFieldValue(long nativeWebContentsAccessibilityAndroid, int id, String newValue);
 
