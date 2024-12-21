c6a81bf9c8786c8bbb2760ca74430163558e527e
Charles Hager <clhager@google.com>
2024-12-20 19:46:02 -0800

    Add AndroidDataControlsDialog
    This change adds the AndroidDataControlsDialog for managing the UI related to the enterprise data controls on Android. For now, the UI is rudimentary and only consists of a title, label / description, and an OK button - this will be updated later when the desired UX is finalized and approved.
    
    Rudimentary UI using modal dialog:
    
    Copy (WARN):
    https://screenshot.googleplex.com/6RRb8qAJDfPhQHG.png
    
    Copy (BLOCK):
    https://screenshot.googleplex.com/C8Jn7ZeevSXRoG9.png
    
    Paste (WARN):
    https://screenshot.googleplex.com/AurneaGuj5UMafK.png
    
    Paste (BLOCK):
    https://screenshot.googleplex.com/7AUstyVhstynF7Z.png
    
    Bug: 382710261
    Change-Id: Ie37ec9ea12e87e1c9271588b77396e369e8623e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099238
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399486}

---
+++ chrome/browser/enterprise/data_controls/android_data_controls_dialog_factory.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_ANDROID_DATA_CONTROLS_DIALOG_FACTORY_H_
+#define CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_ANDROID_DATA_CONTROLS_DIALOG_FACTORY_H_
+
+#include "components/enterprise/data_controls/core/browser/data_controls_dialog_factory.h"
+
+namespace data_controls {
+
+class AndroidDataControlsDialogFactory : public DataControlsDialogFactory {
+ public:
+  static AndroidDataControlsDialogFactory* GetInstance();
+
+  AndroidDataControlsDialogFactory() = default;
+  virtual ~AndroidDataControlsDialogFactory() = default;
+
+ private:
+  DataControlsDialog* CreateDialog(
+      DataControlsDialog::Type type,
+      content::WebContents* web_contents,
+      base::OnceCallback<void(bool bypassed)> callback) override;
+};
+
+}  // namespace data_controls
+
+#endif  // CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_ANDROID_DATA_CONTROLS_DIALOG_FACTORY_H_
