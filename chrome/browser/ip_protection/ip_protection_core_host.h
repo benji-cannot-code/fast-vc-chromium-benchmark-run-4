179476930edc1a159466a17ba74d8353870885a3
Andrew Williams <awillia@chromium.org>
2024-12-23 11:33:10 -0800

    Reland "[IP Protection] Disable by default for managed browsers/devices"
    This is a reland of commit 7f3f09b6510fbf47b9d76c0e46f8e6cdfaf777f8
    
    Some of the IP Protection tests were failing on certain bots that are
    managed devices (causing IP Protection to be disabled by default).
    Updates the IP Protection code to expose an
    ShouldDisableIpProtectionForManagedForTesting method that we can use
    to account for this in the tests.
    
    Original change's description:
    > [IP Protection] Disable by default for managed browsers/devices
    >
    > Disable IP Protection by defaulted for managed browsers or managed
    > devices, and update the existing enterprise policy text to reflect this.
    >
    > Bug: 41496985
    > Change-Id: Ibc95af529819eac6ee4a4c3d668f676ee5a9f779
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021115
    > Reviewed-by: Abhi Patel <abhipatel@chromium.org>
    > Reviewed-by: Artyom Chen <artyomchen@google.com>
    > Commit-Queue: Andrew Williams <awillia@chromium.org>
    > Reviewed-by: Yann Dago <ydago@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1398890}
    
    Bug: 41496985
    Change-Id: I51527974b99130ca76a47c7a6418965884621075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111922
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Owners-Override: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Abhi Patel <abhipatel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399853}

--- chrome/browser/ip_protection/ip_protection_core_host.h
+++ chrome/browser/ip_protection/ip_protection_core_host.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ip_protection/common/ip_protection_telemetry.h"
 #include "components/ip_protection/common/ip_protection_token_direct_fetcher.h"
 #include "components/ip_protection/mojom/core.mojom.h"
+#include "components/policy/core/common/management/management_service.h"
 #include "components/privacy_sandbox/tracking_protection_settings.h"
 #include "components/privacy_sandbox/tracking_protection_settings_observer.h"
 #include "components/signin/public/identity_manager/access_token_info.h"
@@ -57,6 +58,7 @@ class IpProtectionCoreHost
   IpProtectionCoreHost(
       signin::IdentityManager* identity_manager,
       privacy_sandbox::TrackingProtectionSettings* tracking_protection_settings,
+      policy::ManagementService* management_service,
       PrefService* pref_service,
       Profile* profile);
 
@@ -111,6 +113,10 @@ class IpProtectionCoreHost
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       std::unique_ptr<quiche::BlindSignAuthInterface> bsa);
 
+  // Returns whether IP Protection should be disabled for managed users and/or
+  // devices, for testing.
+  bool ShouldDisableIpProtectionForManagedForTesting();
+
  private:
   friend class IpProtectionCoreHostTest;
   FRIEND_TEST_ALL_PREFIXES(IpProtectionCoreHostIdentityBrowserTest,
@@ -164,6 +170,10 @@ class IpProtectionCoreHost
   // available) or extend them (if not).
   void AccountStatusChanged(bool account_available);
 
+  // Returns whether IP Protection should be disabled for managed users and/or
+  // devices.
+  bool ShouldDisableIpProtectionForManaged();
+
   // The object used to get an OAuth token. `identity_manager_` will be set to
   // nullptr after `Shutdown()` is called, but will otherwise be non-null.
   raw_ptr<signin::IdentityManager> identity_manager_;
@@ -172,12 +182,14 @@ class IpProtectionCoreHost
   // is called, but will otherwise be non-null.
   raw_ptr<privacy_sandbox::TrackingProtectionSettings>
       tracking_protection_settings_;
+  // Used to check whether the browser is being managed. Will be set to nullptr
+  // after `Shutdown()`, but will otherwise be non-null.
+  raw_ptr<policy::ManagementService> management_service_;
   // Used to request the state of the IP Protection user setting. Will be set to
   // nullptr after `Shutdown()` is called.
   raw_ptr<PrefService> pref_service_;
-  // The `Profile` object associated with this
-  // `IpProtectionCoreHost()`. Will be reset to nullptr after
-  // `Shutdown()` is called.
+  // The `Profile` object associated with this `IpProtectionCoreHost()`. Will be
+  // reset to nullptr after `Shutdown()` is called.
   // NOTE: If this is used in any `GetForProfile()` call, ensure that there is a
   // corresponding dependency (if needed) registered in the factory class.
   raw_ptr<Profile> profile_;
