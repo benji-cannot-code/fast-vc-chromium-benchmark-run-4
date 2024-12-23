015df011c376909f683132e65aca2f616c4e8e0c
Karol Sygiet <sygiet@google.com>
2024-12-23 08:21:15 -0800

    [Japan] Prevent updates when profiles are semantically the same
    This CL fixes a bug where the Update profile bubble was shown for
    alternative name, after the profile submitted used a different
    character set from the one already saved.
    
    Now alternative names will be considered different(and thus triggering
    the Update bubble) only if the semantic value of them is different.
    
    Bug: 381222780
    Change-Id: Ifa13ea0f0479b6c06d3e0980c5161d893c3969aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111574
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399805}

--- components/autofill/core/browser/data_model/autofill_profile_comparator.cc
+++ components/autofill/core/browser/data_model/autofill_profile_comparator.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/borrowed_transliterator.h"
 #include "components/autofill/core/browser/data_model/contact_info.h"
 #include "components/autofill/core/browser/data_quality/autofill_data_util.h"
+#include "components/autofill/core/browser/field_type_utils.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/geo/autofill_country.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
@@ -667,6 +668,20 @@ bool AutofillProfileComparator::ProfilesHaveDifferentSettingsVisibleValues(
   // Return true if at least one value corresponding to the settings visible
   // types is different between the two profiles.
   return std::ranges::any_of(p1.GetUserVisibleTypes(), [&](FieldType type) {
+    if (IsAlternativeNameType(type) &&
+        base::FeatureList::IsEnabled(
+            features::kAutofillSupportPhoneticNameForJP)) {
+      // Consider two alternative names that differ only in the character set
+      // equal.
+      return p1.GetNameInfo()
+                 .GetStructuredAlternativeName()
+                 .GetValueForComparisonForType(
+                     type, p2.GetNameInfo().GetStructuredAlternativeName()) !=
+             p2.GetNameInfo()
+                 .GetStructuredAlternativeName()
+                 .GetValueForComparisonForType(
+                     type, p1.GetNameInfo().GetStructuredAlternativeName());
+    }
     return p1.GetInfo(type, app_locale) != p2.GetInfo(type, app_locale);
   });
 }
