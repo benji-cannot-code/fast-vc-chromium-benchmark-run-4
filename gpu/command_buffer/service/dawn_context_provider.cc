a4249f333920f238a1f27a1240c834138a095964
Le Hoang Quyen <lehoangquyen@chromium.org>
2024-12-23 12:21:49 -0800

    Graphite: Enable FlexibleTextureViews feature.
    It's needed for using multiplanar textures in Dawn/Compat Mode.
    
    Bug: 382071071
    Change-Id: I98c02fb56041474d091461104b0c28c491c08e60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108541
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Auto-Submit: Quyen Le <lehoangquyen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399869}

--- gpu/command_buffer/service/dawn_context_provider.cc
+++ gpu/command_buffer/service/dawn_context_provider.cc
@@ -211,6 +211,7 @@ std::vector<wgpu::FeatureName> GetRequiredFeatures(
       wgpu::FeatureName::DawnLoadResolveTexture,
       wgpu::FeatureName::DawnPartialLoadResolveTexture,
       wgpu::FeatureName::DawnTexelCopyBufferRowAlignment,
+      wgpu::FeatureName::FlexibleTextureViews,
   };
 
   for (auto feature : kOptionalFeatures) {
