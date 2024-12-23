cee815567fb338cf7811a50ab46143bf9b3d92b0
Martin Kong <martinkong@google.com>
2024-12-23 12:21:39 -0800

    Enable sTabStateFlatBuffer in tests
    Change the sTabStateFlatBuffer cached flag's defaultValueInTests,
    so that it matches the value in fieldtrial_testing_config.json.
    
    Bug: 380113169
    Change-Id: Idceed4bc698bc7f962e2d07abcee6979d398e0b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6106816
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1399868}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
@@ -59,7 +59,6 @@ public class ChromeCachedFlagsTest {
                     ChromeFeatureList.sSafetyHubMagicStack,
                     ChromeFeatureList.sTabGroupPaneAndroid,
                     ChromeFeatureList.sTabResumptionModuleAndroid,
-                    ChromeFeatureList.sTabStateFlatBuffer,
                     ChromeFeatureList.sTraceBinderIpc,
                     OmniboxFeatures.sAndroidHubSearch);
 
