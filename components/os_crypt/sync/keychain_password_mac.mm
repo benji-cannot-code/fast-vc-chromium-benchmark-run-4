b1a7d4f43f24d4a0ba7d054b72dfd198bf9986c7
Vasilii Sukhanov <vasilii@chromium.org>
2024-12-23 09:47:02 -0800

    Introduce DumpWithoutCrashing to see a callstack for the failed OSCrypt on iOS.
    Bug: 383661630
    Change-Id: I7d9c5e5632b0281480a94566c9b29c3225e36fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110242
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Owners-Override: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399825}

--- components/os_crypt/sync/keychain_password_mac.mm
+++ components/os_crypt/sync/keychain_password_mac.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/osstatus_logging.h"
 #include "base/apple/scoped_cftyperef.h"
 #include "base/base64.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/rand_util.h"
@@ -103,5 +104,10 @@ std::string KeychainPassword::GetPassword() const {
 
   OSSTATUS_LOG(ERROR, error) << "Keychain lookup failed";
   base::UmaHistogramSparse("OSCrypt.Mac.FindGenericPasswordError", error);
+  // TODO(crbug.com/383661630): revert this change once enough data is
+  // collected.
+  if (error == errSecInteractionNotAllowed) {
+    base::debug::DumpWithoutCrashing();
+  }
   return std::string();
 }
