16e7a18b16779bdfae0826c6ac564b3e654a54ba
Adrian Taylor <adetaylor@chromium.org>
2024-12-23 17:11:11 -0800

    Roll serde_json: 1.0.133 => 1.0.134 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * serde_json: 1.0.133 => 1.0.134
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * serde_json@1.0.134: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I6d5a21740a469be6d096dc829b53f72599a0f7ac
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110719
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399945}

--- third_party/rust/chromium_crates_io/vendor/serde_json-1.0.134/src/raw.rs
+++ third_party/rust/chromium_crates_io/vendor/serde_json-1.0.134/src/raw.rs
@@ -120,7 +120,7 @@ pub struct RawValue {
 }
 
 impl RawValue {
-    fn from_borrowed(json: &str) -> &Self {
+    const fn from_borrowed(json: &str) -> &Self {
         unsafe { mem::transmute::<&str, &RawValue>(json) }
     }
 
@@ -149,7 +149,7 @@ impl ToOwned for RawValue {
 
 impl Default for Box<RawValue> {
     fn default() -> Self {
-        RawValue::from_borrowed("null").to_owned()
+        RawValue::NULL.to_owned()
     }
 }
 
@@ -169,6 +169,13 @@ impl Display for RawValue {
 }
 
 impl RawValue {
+    /// A constant RawValue with the JSON value `null`.
+    pub const NULL: &'static RawValue = RawValue::from_borrowed("null");
+    /// A constant RawValue with the JSON value `true`.
+    pub const TRUE: &'static RawValue = RawValue::from_borrowed("true");
+    /// A constant RawValue with the JSON value `false`.
+    pub const FALSE: &'static RawValue = RawValue::from_borrowed("false");
+
     /// Convert an owned `String` of JSON data to an owned `RawValue`.
     ///
     /// This function is equivalent to `serde_json::from_str::<Box<RawValue>>`
