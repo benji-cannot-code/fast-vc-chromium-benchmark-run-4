9361b2410029face74c104226e2d49fce2c9f92b
An Sung <ansung@google.com>
2024-12-24 11:24:07 -0800

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 32f43fa72ba90b4269b5c485909210761cb70b20
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Iefbe7a66430485c85cbfd8f6bf0a29a4f09ba8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6120838
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: An Sung <ansung@google.com>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400077}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/wpttest.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/wpttest.py
@@ -217,7 +217,8 @@ class Test(ABC):
     long_timeout = 60  # seconds
 
     def __init__(self, url_base, tests_root, url, inherit_metadata, test_metadata,
-                 timeout=None, path=None, protocol="http", subdomain=False, pac=None):
+          timeout=None, path=None, protocol="http", subdomain=False, pac=None,
+          testdriver_features=None):
         self.url_base = url_base
         self.tests_root = tests_root
         self.url = url
@@ -225,6 +226,7 @@ class Test(ABC):
         self._test_metadata = test_metadata
         self.timeout = timeout if timeout is not None else self.default_timeout
         self.path = path
+        self.testdriver_features = testdriver_features
         self.subdomain = subdomain
         self.environment = {"url_base": url_base,
                             "protocol": protocol,
@@ -483,9 +485,10 @@ class TestharnessTest(Test):
 
     def __init__(self, url_base, tests_root, url, inherit_metadata, test_metadata,
                  timeout=None, path=None, protocol="http", testdriver=False,
-                 jsshell=False, scripts=None, subdomain=False, pac=None):
+                 jsshell=False, scripts=None, subdomain=False, pac=None,
+                 testdriver_features=None):
         Test.__init__(self, url_base, tests_root, url, inherit_metadata, test_metadata, timeout,
-                      path, protocol, subdomain, pac)
+                      path, protocol, subdomain, pac, testdriver_features)
 
         self.testdriver = testdriver
         self.jsshell = jsshell
@@ -495,6 +498,7 @@ class TestharnessTest(Test):
     def from_manifest(cls, manifest_file, manifest_item, inherit_metadata, test_metadata):
         timeout = cls.long_timeout if manifest_item.timeout == "long" else cls.default_timeout
         pac = manifest_item.pac
+        testdriver_features = manifest_item.testdriver_features
         testdriver = manifest_item.testdriver if hasattr(manifest_item, "testdriver") else False
         jsshell = manifest_item.jsshell if hasattr(manifest_item, "jsshell") else False
         script_metadata = manifest_item.script_metadata or []
@@ -507,6 +511,7 @@ class TestharnessTest(Test):
                    test_metadata,
                    timeout=timeout,
                    pac=pac,
+                   testdriver_features=testdriver_features,
                    path=os.path.join(manifest_file.tests_root, manifest_item.path),
                    protocol=server_protocol(manifest_item),
                    testdriver=testdriver,
