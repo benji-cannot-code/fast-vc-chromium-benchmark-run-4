acf34c82b7257504f3ef15534b7b735026237df2
yingyingma <yingying.ma@intel.com>
2024-12-23 18:18:04 -0800

    Convert av1 builder to safer constructs
    Refactor av1 bitstream builder and make the access safer.
    
    Bug: 40285824
    Change-Id: Id4a4f08948540cd807aded7b1b6f2d604e9f4e04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6119049
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Yingying Ma <yingying.ma@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1399959}

--- media/gpu/av1_builder.cc
+++ media/gpu/av1_builder.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "media/gpu/av1_builder.h"
 
 #include <iterator>
@@ -42,9 +37,9 @@ AV1BitstreamBuilder AV1BitstreamBuilder::BuildSequenceHeaderOBU(
       ret.Write(1, 4);  // Spatial layer 1 should be decoded.
       ret.Write((1 << (seq_hdr.operating_points_cnt_minus_1 + 1 - i)) - 1, 8);
     }
-    ret.Write(seq_hdr.level[i], 5);
-    if (seq_hdr.level[i] > 7) {
-      ret.WriteBool(seq_hdr.tier[i]);
+    ret.Write(seq_hdr.level.at(i), 5);
+    if (seq_hdr.level.at(i) > 7) {
+      ret.WriteBool(seq_hdr.tier.at(i));
     }
   }
 
@@ -160,10 +155,10 @@ AV1BitstreamBuilder AV1BitstreamBuilder::BuildFrameHeaderOBU(
     for (uint32_t i = 0; i < libgav1::kMaxSegments; i++) {
       for (uint32_t j = 0; j < libgav1::kSegmentFeatureMax; j++) {
         bool feature_enabled = (i < pic_hdr.segment_number &&
-                                (pic_hdr.feature_mask[i] & (1u << j)));
+                                (pic_hdr.feature_mask.at(i) & (1u << j)));
         ret.WriteBool(feature_enabled);
         if (feature_enabled) {
-          int delta_q = pic_hdr.feature_data[i][j];
+          int delta_q = pic_hdr.feature_data.at(i).at(j);
           ret.WriteBool(delta_q < 0);  // Sign bit.
           if (delta_q < 0) {
             delta_q += 2 * (1 << 8);
@@ -179,9 +174,9 @@ AV1BitstreamBuilder AV1BitstreamBuilder::BuildFrameHeaderOBU(
   }
 
   // Pack loop filter parameters.
-  ret.Write(pic_hdr.filter_level[0], 6);
-  ret.Write(pic_hdr.filter_level[1], 6);
-  if (pic_hdr.filter_level[0] || pic_hdr.filter_level[1]) {
+  ret.Write(pic_hdr.filter_level.at(0), 6);
+  ret.Write(pic_hdr.filter_level.at(1), 6);
+  if (pic_hdr.filter_level.at(0) || pic_hdr.filter_level.at(1)) {
     ret.Write(pic_hdr.filter_level_u, 6);
     ret.Write(pic_hdr.filter_level_v, 6);
   }
@@ -194,10 +189,10 @@ AV1BitstreamBuilder AV1BitstreamBuilder::BuildFrameHeaderOBU(
     ret.Write(2, 2);         // Set CDEF damping minus 3 to 5 - 3.
     ret.Write(3, 2);         // Set cdef_bits to 3.
     for (size_t i = 0; i < (1 << num_planes); i++) {
-      ret.Write(pic_hdr.cdef_y_pri_strength[i], 4);
-      ret.Write(pic_hdr.cdef_y_sec_strength[i], 2);
-      ret.Write(pic_hdr.cdef_uv_pri_strength[i], 4);
-      ret.Write(pic_hdr.cdef_uv_sec_strength[i], 2);
+      ret.Write(pic_hdr.cdef_y_pri_strength.at(i), 4);
+      ret.Write(pic_hdr.cdef_y_sec_strength.at(i), 2);
+      ret.Write(pic_hdr.cdef_uv_pri_strength.at(i), 4);
+      ret.Write(pic_hdr.cdef_uv_sec_strength.at(i), 2);
     }
   }
   ret.WriteBool(true);  // TxMode TX_MODE_SELECT.
