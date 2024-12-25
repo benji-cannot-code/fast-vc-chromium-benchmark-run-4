fc71a516296285e38fc1bfcb2b98f7219d214e9b
Lingqi Chi <lingqi@chromium.org>
2024-12-24 18:39:06 -0800

    [gardener] disable RecentActivityBubbleDialogViewUnitTest.ShowsAllTypes on Mac
    Example:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/33705/overview
    
    Bug: 383361891
    Change-Id: I6c784bcd948806907c6df10f34d6baccbb815951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110022
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400110}

--- chrome/browser/ui/views/tabs/recent_activity_bubble_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/tabs/recent_activity_bubble_dialog_view_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/tabs/recent_activity_bubble_dialog_view.h"
 
+#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_tab_data.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
@@ -212,7 +213,14 @@ IN_PROC_BROWSER_TEST_F(RecentActivityBubbleDialogViewUnitTest,
   ShowAndVerifyUi();
 }
 
-IN_PROC_BROWSER_TEST_F(RecentActivityBubbleDialogViewUnitTest, ShowsAllTypes) {
+// TODO(https://crbug.com/383361891): Failing on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_ShowsAllTypes DISABLED_ShowsAllTypes
+#else
+#define MAYBE_ShowsAllTypes ShowsAllTypes
+#endif
+IN_PROC_BROWSER_TEST_F(RecentActivityBubbleDialogViewUnitTest,
+                       MAYBE_ShowsAllTypes) {
   auto activity_log = CreateMockActivityLogWithAllTypes();
   ShowLog(activity_log);
 
