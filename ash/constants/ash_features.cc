1eca0ca0b168de8dca65844add142b357c62878d
Daseul Lee <dslee@chromium.org>
2024-12-26 10:53:39 -0800

    [CodeHealth] Clean up LockScreenInlineReply flag.
    Bug: 356234193
    Change-Id: I501cd246fff86d3c9f1991559d7d50dd48c09e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102091
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400320}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1765,12 +1765,6 @@ BASE_FEATURE(kLockScreenHideSensitiveNotificationsSupport,
              "LockScreenHideSensitiveNotificationsSupport",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables inline reply on notifications on the lock screen.
-// This option is effective when |kLockScreenNotification| is enabled.
-BASE_FEATURE(kLockScreenInlineReply,
-             "LockScreenInlineReply",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // Enables notifications on the lock screen.
 BASE_FEATURE(kLockScreenNotifications,
              "LockScreenNotifications",
@@ -3973,10 +3967,6 @@ bool AreGameDashboardUtilitiesEnabled() {
 
 bool IsAppLaunchShortcutEnabled() {
   return base::FeatureList::IsEnabled(kAppLaunchShortcut);
-}
-
-bool IsLockScreenInlineReplyEnabled() {
-  return base::FeatureList::IsEnabled(kLockScreenInlineReply);
 }
 
 bool IsLockScreenNotificationsEnabled() {
