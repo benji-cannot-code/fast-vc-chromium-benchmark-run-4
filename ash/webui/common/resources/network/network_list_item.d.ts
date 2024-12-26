836a12d879ddf7cfe7244fbcd9e7f4e705d6f3af
xiangdong kong <xiangdongkong@google.com>
2024-12-26 15:23:22 -0800

    typescript: Convert network_list.js to TypeScript
    Bug: 367734360
    Change-Id: I44ddd68a86acc7ecfaa7caabeadfec31be5bdfd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050138
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1400378}

--- ash/webui/common/resources/network/network_list_item.d.ts
+++ ash/webui/common/resources/network/network_list_item.d.ts
@@ -3,4 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export {};
+import {LegacyElementMixin} from '//resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
+
+interface NetworkListItemElement extends LegacyElementMixin, HTMLElement {}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'network-list-item': NetworkListItemElement;
+  }
+}
+
+export {NetworkListItemElement};
