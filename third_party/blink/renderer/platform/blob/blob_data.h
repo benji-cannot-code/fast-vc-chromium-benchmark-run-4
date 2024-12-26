af25ad1607e59521dee81eae46ea092b4df24631
Evan Stade <estade@google.com>
2024-12-26 11:01:46 -0800

    Blobs: remove dead code.
    Bug: none
    Change-Id: I96c236f907236fd22a10b9b3a56bb204744a36e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6120153
    Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400324}

--- third_party/blink/renderer/platform/blob/blob_data.h
+++ third_party/blink/renderer/platform/blob/blob_data.h
@@ -56,14 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 
-namespace network {
-namespace mojom {
-namespace blink {
-class DataPipeGetter;
-}  // namespace blink
-}  // namespace mojom
-}  // namespace network
-
 namespace blink {
 namespace mojom {
 namespace blink {
@@ -218,7 +210,6 @@ class PLATFORM_EXPORT BlobDataHandle
 
   mojo::PendingRemote<mojom::blink::Blob> CloneBlobRemote();
   void CloneBlobRemote(mojo::PendingReceiver<mojom::blink::Blob>);
-  mojo::PendingRemote<network::mojom::blink::DataPipeGetter> AsDataPipeGetter();
 
   void ReadAll(mojo::ScopedDataPipeProducerHandle,
                mojo::PendingRemote<mojom::blink::BlobReaderClient>);
