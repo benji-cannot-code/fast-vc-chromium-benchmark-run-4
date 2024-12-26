df336074eaf9c4282e5092ab6863a6559c4b7d9f
Andrew Williams <awillia@chromium.org>
2024-12-26 08:01:48 -0800

    [IP Protection] ip_protection_core_host.h cleanup
    Minor clean-up according to
    https://google.github.io/styleguide/cppguide.html#Declaration_Order
    
    Doesn't change any functionality or content.
    
     #cleanup
    
    Bug: None
    Change-Id: Ic187d0e6a73f31ccd2f5485088e460daf6d1a4c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6107873
    Reviewed-by: Ciara McMullin <ciaramcmullin@google.com>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400292}

--- chrome/browser/ip_protection/ip_protection_core_host.h
+++ chrome/browser/ip_protection/ip_protection_core_host.h
@@ -124,6 +124,12 @@ class IpProtectionCoreHost
   FRIEND_TEST_ALL_PREFIXES(IpProtectionCoreHostUserSettingBrowserTest,
                            OnIpProtectionEnabledChanged);
 
+  // Creating a generic callback in order for `RequestOAuthToken()` to work for
+  // `TryGetAuthTokens()` and `GetProxyConfig()`.
+  using RequestOAuthTokenInternalCallback =
+      base::OnceCallback<void(GoogleServiceAuthError error,
+                              signin::AccessTokenInfo access_token_info)>;
+
   // Set up `ip_protection_proxy_config_fetcher_` and
   // `ip_protection_token_fetcher_` if
   // not already initialized. This accomplishes lazy loading of these components
@@ -154,12 +160,6 @@ class IpProtectionCoreHost
       ip_protection::TryGetAuthTokensResult result,
       std::optional<base::TimeDelta> duration = std::nullopt);
 
-  // Creating a generic callback in order for `RequestOAuthToken()` to work for
-  // `TryGetAuthTokens()` and `GetProxyConfig()`.
-  using RequestOAuthTokenInternalCallback =
-      base::OnceCallback<void(GoogleServiceAuthError error,
-                              signin::AccessTokenInfo access_token_info)>;
-
   // Calls the IdentityManager asynchronously to request the OAuth token for the
   // logged in user. This method must only be called when
   // `CanRequestOAuthToken()` returns true.
@@ -173,6 +173,22 @@ class IpProtectionCoreHost
   // Returns whether IP Protection should be disabled for managed users and/or
   // devices.
   bool ShouldDisableIpProtectionForManaged();
+
+  // Instruct the `IpProtectionConfigCache()`(s) in the Network Service to
+  // ignore any previously sent `try_again_after` times.
+  void InvalidateNetworkContextTryAgainAfterTime();
+
+  // IdentityManager::Observer:
+  void OnPrimaryAccountChanged(
+      const signin::PrimaryAccountChangeEvent& event) override;
+  void OnErrorStateOfRefreshTokenUpdatedForAccount(
+      const CoreAccountInfo& account_info,
+      const GoogleServiceAuthError& error,
+      signin_metrics::SourceForRefreshTokenOperation token_operation_source)
+      override;
+
+  // TrackingProtectionSettingsObserver:
+  void OnIpProtectionEnabledChanged() override;
 
   // The object used to get an OAuth token. `identity_manager_` will be set to
   // nullptr after `Shutdown()` is called, but will otherwise be non-null.
@@ -193,22 +209,6 @@ class IpProtectionCoreHost
   // NOTE: If this is used in any `GetForProfile()` call, ensure that there is a
   // corresponding dependency (if needed) registered in the factory class.
   raw_ptr<Profile> profile_;
-
-  // Instruct the `IpProtectionConfigCache()`(s) in the Network Service to
-  // ignore any previously sent `try_again_after` times.
-  void InvalidateNetworkContextTryAgainAfterTime();
-
-  // IdentityManager::Observer:
-  void OnPrimaryAccountChanged(
-      const signin::PrimaryAccountChangeEvent& event) override;
-  void OnErrorStateOfRefreshTokenUpdatedForAccount(
-      const CoreAccountInfo& account_info,
-      const GoogleServiceAuthError& error,
-      signin_metrics::SourceForRefreshTokenOperation token_operation_source)
-      override;
-
-  // TrackingProtectionSettingsObserver:
-  void OnIpProtectionEnabledChanged() override;
 
   std::unique_ptr<ip_protection::IpProtectionProxyConfigDirectFetcher>
       ip_protection_proxy_config_fetcher_;
