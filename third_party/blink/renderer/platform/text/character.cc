e2cf2e07da3daf4ac450517f5e990887c41102fa
Kent Tamura <tkent@chromium.org>
2024-12-26 01:47:08 -0800

    text-emphasis: Do not paint emphasis marks on punctuations
    * Do not paint emphasis marks on punctuations
    * Do not use IDEOGRAPHIC FULL STOP for text-combine placeholder
      It's a punctuation.  Use HIRAGANA LETTER A instead.
    * Do not paint emphasis marks on generated ellipsis symbols just in case
      It's always a punctuation now. But we might support arbitrary string
      in the future.
    
    * Update text-emphasis-punctuation-1.html so that it uses Ahem font
      Without this change, the test had pixel glitches on Chromium.
    
    The new behavior matches to Firefox.
    
    https://drafts.csswg.org/css-text-decor/#text-emphasis-style-property
    
    Bug: 359420112
    Change-Id: If93ef6190e739a0cf64d3becb24c41191dbe1100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108987
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400253}

--- third_party/blink/renderer/platform/text/character.cc
+++ third_party/blink/renderer/platform/text/character.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/synchronization/lock.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/character_property_data.h"
 #include "third_party/blink/renderer/platform/text/icu_error.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
@@ -249,6 +250,7 @@ bool Character::CanReceiveTextEmphasis(UChar32 c) {
 
   // Additional word-separator characters listed in CSS Text Level 3 Editor's
   // Draft 3 November 2010.
+  // https://www.w3.org/TR/css-text-3/#word-separator
   if (c == kEthiopicWordspaceCharacter ||
       c == kAegeanWordSeparatorLineCharacter ||
       c == kAegeanWordSeparatorDotCharacter ||
@@ -256,6 +258,22 @@ bool Character::CanReceiveTextEmphasis(UChar32 c) {
       c == kTibetanMarkIntersyllabicTshegCharacter ||
       c == kTibetanMarkDelimiterTshegBstarCharacter)
     return false;
+
+  if (!RuntimeEnabledFeatures::TextEmphasisNoPunctuationEnabled()) {
+    return true;
+  }
+  // Punctuation
+  if (category &
+      (WTF::unicode::kPunctuation_Dash | WTF::unicode::kPunctuation_Open |
+       WTF::unicode::kPunctuation_Close | WTF::unicode::kPunctuation_Connector |
+       WTF::unicode::kPunctuation_Other |
+       WTF::unicode::kPunctuation_InitialQuote |
+       WTF::unicode::kPunctuation_FinalQuote)) {
+    return false;
+  }
+  // TODO(layout-dev): css/css-text-decor/text-emphasis-punctuation-3.html
+  // requires implementation for the following rule in the specification:
+  // > do not NFKD normalize to any of the following symbols:
 
   return true;
 }
