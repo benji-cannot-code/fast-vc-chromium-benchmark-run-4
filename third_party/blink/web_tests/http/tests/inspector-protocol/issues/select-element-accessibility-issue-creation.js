a3a4101f1a7567f56d295232a88ab28477378dc4
Ana Sollano Kim <ansollan@microsoft.com>
2024-12-26 15:52:04 -0800

    [Select Element Accessibility Issue] Report DevTools issue, part 1 (Chromium)
    This CL reports a new <select> accessibility issue whenever the content
    model of the customizable <select> isn't followed. There are five types
    of issues, described here
    https://gist.github.com/scottaohara/9f05b6470b314f955e11e3ce9d7615b7.
    Tests are added to verify behavior.
    
    Doc: https://docs.google.com/document/d/1XZGVjR8uBkgNt6Uhg7E09Veo0fZUgh6eHJQPfLEYe_k/edit?usp=sharing
    
    Bug: 354032791, 347890366
    Change-Id: I264d3d6f59b6b1c1dbcecaf8da2e75243b157af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6079873
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Ana Sollano Kim <ansollan@microsoft.com>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400384}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/select-element-accessibility-issue-creation.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+    const { session, dp } = await testRunner.startBlank(
+    'Verifies that the <select> accessibility issue is created when the content ' +
+    'model is not followed.');
+    await dp.Audits.enable();
+
+    let issues = [];
+    let count = 0;
+    let eventReceived = null;
+    let allEventsReceived = new Promise(resolve => eventReceived = () => {
+      if (++count == 1) resolve();
+    });
+
+    dp.Audits.onceIssueAdded(issue => {
+      if (issue.params.issue.code !== 'SelectElementAccessibilityIssue') {
+        return;
+      }
+      const details = issue.params.issue.details.selectElementAccessibilityIssueDetails;
+      if (!Number.isInteger(details.nodeId)) {
+        testRunner.log("Error: nodeId is not an integer." + details.nodeId);
+      }
+      issues.push(issue.params);
+      eventReceived();
+    });
+
+    await session.navigate('../resources/disallowed-select-element-descendants.html');
+    await allEventsReceived;
+
+    issues.forEach(issue => testRunner.log(issue, "Inspector issue: "));
+    testRunner.completeTest();
+})
