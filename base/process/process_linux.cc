bba69545a9bc033604625ccd6606b291fc0579e4
Shintaro Kawamura <kawasin@google.com>
2024-12-25 23:41:33 -0800

    Use flattened cpu cgroups on ChromeOS
    Nested cpu cgroup has an overhead on task scheduling.
    
    Also 10 as the cpu.share for /chrome_renderer/background only works
    between chrome renderers. If there is no active foreground renderers but
    active background renderers, it consumes CPU resources from other system
    processes and browser processes as if foreground renderers.
    
    The new cpu cgroup is added by https://crrev.com/c/6109599.
    
    In FlattenCpuCgroups:unified_cpu_cgroup/true experiment, chrome puts
    foreground renderer processes to /sys/fs/cgroup/cpu/ui cgroup which is
    shared with other chrome processes (e.g. browser/gpu processes).
    
    The unified cpu cgroup showed better input latency because high priority
    threads in the browser process can run over the lower priority threads
    in foreground renderer processes.
    
    The access to /sys/fs/cgroup/cpu/ui is configured by
    https://crrev.com/c/6108548.
    
    
    Bug: 385656021
    Test: manual
    Change-Id: If79d7f88402b80971e81f312d8b7d139be7f8409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111015
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Reviewed-by: Charles Dick <cwd@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400242}

--- base/process/process_linux.cc
+++ base/process/process_linux.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/process/process_handle.h"
 #include "base/process/process_priority_delegate.h"
 #include "base/strings/strcat.h"
@@ -52,6 +53,20 @@ BASE_FEATURE(kOneGroupPerRenderer,
 #else
              FEATURE_DISABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
+BASE_FEATURE(kFlattenCpuCgroups,
+             "FlattenCpuCgroups",
+             FEATURE_DISABLED_BY_DEFAULT);
+
+// If FlattenCpuCgroupsUnified parameter is enabled, foreground renderer
+// processes uses /sys/fs/cgroup/cpu/ui cgroup instead of
+// /sys/fs/cgroup/cpu/chrome_renderers sharing the cpu cgroup with the browser
+// process and others.
+BASE_FEATURE_PARAM(bool,
+                   kFlattenCpuCgroupsUnified,
+                   &kFlattenCpuCgroups,
+                   "unified_cpu_cgroup",
+                   false);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 namespace {
@@ -73,6 +88,9 @@ const char kControlPath[] = "/sys/fs/cgroup/cpu%s/cgroup.procs";
 const char kFullRendererCgroupRoot[] = "/sys/fs/cgroup/cpu/chrome_renderers";
 const char kForeground[] = "/chrome_renderers/foreground";
 const char kBackground[] = "/chrome_renderers/background";
+const char kForegroundExperiment[] = "/chrome_renderers";
+const char kForegroundUnifiedExperiment[] = "/ui";
+const char kBackgroundExperiment[] = "/chrome_renderers_background";
 const char kProcPath[] = "/proc/%d/cgroup";
 const char kUclampMinFile[] = "cpu.uclamp.min";
 const char kUclampMaxFile[] = "cpu.uclamp.max";
@@ -111,8 +129,17 @@ struct CGroups {
   std::string uclamp_max;
 
   CGroups() {
-    foreground_file = FilePath(StringPrintf(kControlPath, kForeground));
-    background_file = FilePath(StringPrintf(kControlPath, kBackground));
+    if (FeatureList::IsEnabled(kFlattenCpuCgroups)) {
+      foreground_file =
+          FilePath(StringPrintf(kControlPath, kFlattenCpuCgroupsUnified.Get()
+                                                  ? kForegroundUnifiedExperiment
+                                                  : kForegroundExperiment));
+      background_file =
+          FilePath(StringPrintf(kControlPath, kBackgroundExperiment));
+    } else {
+      foreground_file = FilePath(StringPrintf(kControlPath, kForeground));
+      background_file = FilePath(StringPrintf(kControlPath, kBackground));
+    }
     enabled = PathIsCGroupFileSystem(foreground_file) &&
               PathIsCGroupFileSystem(background_file);
 
