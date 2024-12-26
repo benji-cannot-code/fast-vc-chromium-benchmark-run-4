ffe2c2ef64183c311e564d9bb1bdfa9e5ad9a599
Jeffrey Young <cowmoo@google.com>
2024-12-26 11:12:27 -0800

    jelly: rm jellyroll feature and references
    Already fully launched since https://crrev.com/c/4630070 in M117.
    
    Bug: 291622042
    Change-Id: I9da4dc1229ef84fa90478f4037e4e0cdc24478ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6115234
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400329}

--- chromeos/constants/chromeos_features.h
+++ chromeos/constants/chromeos_features.h
@@ -160,7 +160,6 @@ bool IsGeminiAppPreinstallFeatureManagementEnabled();
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 bool IsGeminiAppPreinstallEnabled();
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool IsJellyEnabled();
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool IsJellyrollEnabled();
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool IsMahiEnabled();
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool IsMahiSendingUrl();
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool IsMahiManagedEnabled();
