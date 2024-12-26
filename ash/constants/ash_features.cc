4ff4c67530ea3d96c9f9bc0ff70807d1809bbe30
Wes Okuhara <wesokuhara@google.com>
2024-12-26 14:01:17 -0800

    CrOS Settings: Remove revamp load time boolean
    Bug: b:302374851
    Test: browser_tests
    Change-Id: Ie485acb87c452967a98b3148b35adbb7a7b6901d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6125714
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400364}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -4180,11 +4180,6 @@ bool IsOsSettingsDeprecateDnsDialogEnabled() {
   return base::FeatureList::IsEnabled(kOsSettingsDeprecateDnsDialog);
 }
 
-bool IsOsSettingsRevampWayfindingEnabled() {
-  // TODO(crbug.com/370836442): remove after all code cleanup is done.
-  return true;
-}
-
 bool IsOsSyncConsentRevampEnabled() {
   return base::FeatureList::IsEnabled(kOsSyncConsentRevamp);
 }
