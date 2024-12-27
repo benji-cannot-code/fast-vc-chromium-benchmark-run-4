c56d16d4492773e772cd831eaa57ecd7ee1599a9
Wes Okuhara <wesokuhara@google.com>
2024-12-27 15:25:34 -0800

    CrOS Settings: Remove "revamp" from icon names
    Bug: b:302374851
    Test: Manually verify UI icons
    Change-Id: Iba11e09c37e2f80a3bc7b72be0bfcac2e6ef607f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6128038
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1400596}

--- chrome/browser/resources/ash/settings/os_settings_search_box/os_search_result_row.ts
+++ chrome/browser/resources/ash/settings/os_settings_search_box/os_search_result_row.ts
@@ -673,7 +673,7 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
     const settingsSearchResult = this.searchResult as SettingsSearchResult;
     switch (settingsSearchResult.icon) {
       case SearchResultIcon.kA11y:
-        return 'os-settings:accessibility-revamp';
+        return 'os-settings:accessibility';
       case SearchResultIcon.kAndroid:
         return 'os-settings:android';
       case SearchResultIcon.kAppsParentalControls:
@@ -739,7 +739,7 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
       case SearchResultIcon.kGoogleDrive:
         return 'os-settings:google-drive';
       case SearchResultIcon.kGooglePlay:
-        return 'os-settings:google-play-revamp';
+        return 'os-settings:google-play';
       case SearchResultIcon.kGraduation:
         return 'os-settings:graduation';
       case SearchResultIcon.kHearing:
@@ -761,7 +761,7 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
       case SearchResultIcon.kLaptop:
         return 'os-settings:laptop-chromebook';
       case SearchResultIcon.kLock:
-        return 'os-settings:lock-revamp';
+        return 'os-settings:lock';
       case SearchResultIcon.kMagicBoost:
         return 'os-settings:magic-boost';
       case SearchResultIcon.kMicrophone:
@@ -806,7 +806,7 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
       case SearchResultIcon.kReset:
         return 'os-settings:startup';
       case SearchResultIcon.kRestore:
-        return 'os-settings:restore-revamp';
+        return 'os-settings:restore';
       case SearchResultIcon.kScanner:
         return 'os-settings:device-scan';
       case SearchResultIcon.kSearch:
@@ -822,7 +822,7 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
       case SearchResultIcon.kStylus:
         return 'os-settings:device-stylus';
       case SearchResultIcon.kSync:
-        return 'os-settings:sync-revamp';
+        return 'os-settings:sync';
       case SearchResultIcon.kSystemPreferences:
         return 'os-settings:system-preferences';
       case SearchResultIcon.kTextToSpeech:
