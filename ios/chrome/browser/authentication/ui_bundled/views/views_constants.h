ed1e06db6ced812317e752e2065f40b8679ba689
Stepan Khapugin <stkhapugin@chromium.org>
2024-12-27 05:11:26 -0800

    [Code Health] Prepare move of i/c/b/ui/authentication to ui_bundled 1/3
    Prepare forwarding headers to update the downstream includes.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 324232855
    Change-Id: Ifc762f5e849ada7691d751fd1d42e731da1a2c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6120296
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Owners-Override: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400472}

---
+++ ios/chrome/browser/authentication/ui_bundled/views/views_constants.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_VIEWS_VIEWS_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_VIEWS_VIEWS_CONSTANTS_H_
+
+#import "ios/chrome/browser/ui/authentication/views/views_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_VIEWS_VIEWS_CONSTANTS_H_
