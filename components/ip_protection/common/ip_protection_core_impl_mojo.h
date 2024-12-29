399a8eb5c61e3fad411d681850a624e8b1e252c5
Aykut Bulut <aykutb@google.com>
2024-12-28 18:23:30 -0800

    Fix typo in forward declaration
    Fix typo in forward declaration in components/ip_protection/common/ip_protection_core_impl_mojo.h
    
    Things work since the same classes are forward declared in components/ip_protection/common/ip_protection_core_impl.h
    
    This should be fixed anyway.
    
    Fixed: 386355761
    Change-Id: I3b3fbdc5d88cca4e84d8e10584290b4efe27d460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6120058
    Commit-Queue: Aykut Bulut <aykutb@google.com>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400735}

--- components/ip_protection/common/ip_protection_core_impl_mojo.h
+++ components/ip_protection/common/ip_protection_core_impl_mojo.h
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ip_protection {
 
 class IpProtectionCoreHostRemote;
-class IpProtectionProxyConfigManaager;
-class IpProtectionTokenManaager;
+class IpProtectionProxyConfigManager;
+class IpProtectionTokenManager;
 
 // The Mojo implementation of IpProtectionCore, providing methods for CoreHost
 // to call on the core, and supporting initialization.
