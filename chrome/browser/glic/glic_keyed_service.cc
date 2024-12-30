bcc4d535ca46be02dd4a50c5fb35a204ea81a6d9
David Bokan <bokan@chromium.org>
2024-12-30 12:20:26 -0800

    Fix incognito mode crash
    GlicKeyedService is not created when in an incognito profile due to
    the fact that the factory inherits from ProfileKeyedServiceFactory[1].
    This causes crashes in several code paths that assume the
    GlicKeyedService is non-null.
    
    This CL updates those paths to check for EnabledForProfile. This
    prevents creating any related UI as well as prevents registering a
    handler for chrome://glic.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/profiles/profile_keyed_service_factory.h;l=45;drc=24386822ca7fb24c3785efb4d709f137c1b8f786
    
    Bug: 386790391
    Change-Id: Ie5b53f375dfaf84bbdfbd14637d3b5183323b3fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6115638
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Owners-Override: David Bokan <bokan@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400988}

--- chrome/browser/glic/glic_keyed_service.cc
+++ chrome/browser/glic/glic_keyed_service.cc
@@ -40,7 +40,7 @@ void GlicKeyedService::LaunchUI(views::View* glic_button_view) {
   // Glic may be disabled for certain user profiles (the user is browsing in
   // incognito or guest mode, policy, etc). In those cases, the entry points to
   // this method should already have been removed.
-  DCHECK(GlicEnabling::IsEnabledForProfile(
+  CHECK(GlicEnabling::IsEnabledForProfile(
       Profile::FromBrowserContext(browser_context_)));
 
   profile_manager_->OnUILaunching(this);
