42dee397b4e0766a44d4da3cc36227a569fa5eaf
Andrei Nedoluzhko <nedol@google.com>
2024-12-30 02:28:36 -0800

    Print Preview: Fork destination.ts for ChromeOS
    This file has a lot of ChromeOS specific logic. Supporting per-printer
    job options will add even more logic for ChromeOS only, thus forking
    this file.
    
    BYPASS_LARGE_CHANGE_WARNING=Refactoring, not possible to split
    
    Bug: b:374066702
    Change-Id: I18d28ca8e8845d047a2b82c68ea9a68d5efe46d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110440
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Andrei Nedoluzhko <nedol@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400878}

--- chrome/browser/resources/print_preview/data/local_parsers.ts
+++ chrome/browser/resources/print_preview/data/local_parsers.ts
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {assertNotReached} from 'chrome://resources/js/assert.js';
 import {isChromeOS} from 'chrome://resources/js/platform.js';
 
+// <if expr="not is_chromeos">
 import type {DestinationOptionalParams} from './destination.js';
 import {Destination, DestinationOrigin, PrinterType} from './destination.js';
-
+// </if>
 // <if expr="is_chromeos">
-import {DestinationProvisionalType} from './destination.js';
+import type {DestinationOptionalParams} from './destination_cros.js';
+import {Destination, DestinationOrigin, DestinationProvisionalType, PrinterType} from './destination_cros.js';
 import type {PrinterStatus} from './printer_status_cros.js';
 import type {ManagedPrintOptions} from './managed_print_options_cros.ts';
 // </if>
--- chrome/browser/resources/print_preview/native_layer.ts
+++ chrome/browser/resources/print_preview/native_layer.ts
@@ -6,7 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {sendWithPromise} from 'chrome://resources/js/cr.js';
 
 import type {Cdd} from './data/cdd.js';
+// <if expr="not is_chromeos">
 import type {PrinterType} from './data/destination.js';
+// </if>
+// <if expr="is_chromeos">
+import type {PrinterType} from './data/destination_cros.js';
+// </if>
 import type {LocalDestinationInfo} from './data/local_parsers.js';
 import type {MeasurementSystemUnitType} from './data/measurement_system.js';
 
