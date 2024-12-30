f703f772c99f0620b8bc199ddf3fa8817cc53ddd
Stepan Khapugin <stkhapugin@chromium.org>
2024-12-30 05:59:21 -0800

    [iOS][LO] Don't do things when Lens Overlay Coordinator stops.
    Adds some defensive checks for when the coordinator is stopped.
    Also adds some nil-checks in termsOfServiceAccepted to avoid bad access.
    
    Fixed: 384923111
    Change-Id: I75508d40ab3645ed1af004da4d5ce52b9dfecf25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108451
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1400909}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -122,6 +122,9 @@ const int kExpectedExitAnimationCount = 2;
   /// Indicates the Lens Overlay is in the exit flow.
   BOOL _isExiting;
 
+  /// Indicates this coordinator has received the `stop` call.
+  BOOL _isStopped;
+
   /// Command handler for loadQueryCommands.
   id<LoadQueryCommands> _loadQueryHandler;
 
@@ -259,6 +262,8 @@ const int kExpectedExitAnimationCount = 2;
 }
 
 - (void)stop {
+  _isStopped = YES;
+
   if (Browser* browser = self.browser) {
     [browser->GetCommandDispatcher() stopDispatchingToTarget:self];
   }
@@ -348,6 +353,15 @@ const int kExpectedExitAnimationCount = 2;
 }
 
 - (void)onContainerViewControllerPresented {
+  // In some situations this coordinator shouldn't do anything because it's
+  // already being torn down. Just do minimal clean up and return.
+  if (_isStopped || _isExiting) {
+    if (_associatedTabHelper) {
+      _associatedTabHelper->ReleaseSnapshotAuxiliaryWindows();
+    }
+    return;
+  }
+
   // Start the selection UI only when the container is presented. This avoids
   // results being reported before the container is fully shown.
   if ([self termsOfServiceAccepted]) {
@@ -705,6 +719,11 @@ const int kExpectedExitAnimationCount = 2;
 }
 
 - (BOOL)termsOfServiceAccepted {
+  if (!self.browser || !self.browser->GetProfile() ||
+      !!self.browser->GetProfile()->GetPrefs()) {
+    return NO;
+  }
+
   return self.browser->GetProfile()->GetPrefs()->GetBoolean(
       prefs::kLensOverlayConditionsAccepted);
 }
