35710ad32b5067c32bbf5b3bd2421499212a7226
Duncan Mercer <mercerd@google.com>
2024-12-30 09:01:09 -0800

    Reland "[lensoverlay] Fix ViewportImageBoundingBoxes"
    This is a reland of commit 9ea0636a8516e513e173f4849423a5e3804fcfb1
    
    The reason the first test was failing was because the object the test
    was checking was being std::move(), causing the object to no longer
    exist when the tests checked it. I changed the test to make a copy
    of the object to avoid race issues.
    
    Original change's description:
    > [lensoverlay] Fix ViewportImageBoundingBoxes
    >
    > I believe this test started failing once State::kStartingWebUI became
    > an async process. Changing the test to wait for State::kOverlay to
    > guarantee that the screenshot and the region boxes exist. Its
    > possible this test is still flaky on some builders, so I will monitor
    > and fix ASAP if not fixed on all builders.
    >
    > Bugs: 377950416
    > Change-Id: I029cfae0589ae826ff420ed4a9453a4593b689ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6117707
    > Reviewed-by: Juan Mojica <juanmojica@google.com>
    > Commit-Queue: Duncan Mercer <mercerd@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1400524}
    
    Change-Id: Id08d440e62abc46ec665d334610ec212378abb23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6127811
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1400934}

--- chrome/browser/ui/lens/test_lens_overlay_query_controller.h
+++ chrome/browser/ui/lens/test_lens_overlay_query_controller.h
@@ -135,6 +135,11 @@ class TestLensOverlayQueryController : public LensOverlayQueryController {
 
   const GURL& last_sent_page_url() const { return last_sent_page_url_; }
 
+  const std::vector<lens::mojom::CenterRotatedBoxPtr>&
+  last_sent_significant_region_boxes() const {
+    return last_sent_significant_region_boxes_;
+  }
+
   const std::optional<lens::mojom::UserAction>& last_user_action() const {
     return last_user_action_;
   }
@@ -292,6 +297,10 @@ class TestLensOverlayQueryController : public LensOverlayQueryController {
 
   // The last page url sent by the query controller.
   GURL last_sent_page_url_;
+
+  // The last significant region boxes sent to the query controller.
+  std::vector<lens::mojom::CenterRotatedBoxPtr>
+      last_sent_significant_region_boxes_;
 
   // The last user action sent by the query controller.
   std::optional<lens::mojom::UserAction> last_user_action_;
