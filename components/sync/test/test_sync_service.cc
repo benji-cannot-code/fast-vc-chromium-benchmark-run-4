51253591c83ac3936234efad7898ee783156b3eb
Amelie Schneider <amelies@google.com>
2024-12-31 02:42:43 -0800

    [UNO] Infra - Use sync service for sign in promo data migration
    Currently, the autofill sign in promos are using data type specific
    functions for migrating the data after a sign in.
    This prepares a function in the sync service, both for enabling
    account storage and migrating the data. In the following CLs, the
    `move_callback` passed to the `AutofillBubbleSignInPromoController`
    will be removed.
    
    Bug: 384457734
    
    Change-Id: I3d285e7b8f868bbd48dad2b19dd41b5014996537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098414
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1401098}

--- components/sync/test/test_sync_service.cc
+++ components/sync/test/test_sync_service.cc
@@ -432,8 +432,11 @@ void TestSyncService::GetLocalDataDescriptions(
 void TestSyncService::TriggerLocalDataMigration(DataTypeSet types) {}
 
 void TestSyncService::TriggerLocalDataMigration(
-    std::map<DataType, std::vector<syncer::LocalDataItemModel::DataId>> items) {
-}
+    std::map<DataType, std::vector<LocalDataItemModel::DataId>> items) {}
+
+void TestSyncService::SelectTypeAndMigrateLocalDataItemsWhenActive(
+    DataType data_type,
+    std::vector<LocalDataItemModel::DataId> items) {}
 
 void TestSyncService::SetTriggerRefreshCallback(
     const base::RepeatingCallback<void(syncer::DataTypeSet)>&
