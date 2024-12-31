ba125b1fecfb7d92a0be946647d2bed6f1aa009f
Kaan Alsan <alsan@chromium.org>
2024-12-30 17:33:36 -0800

    Implement method to show page actions only if not already pinned
    This CL adds a `ShowIfNotPinned(ActionId)` method to
    `PageActionActionController`, which requests to show a page action
    only if it isn't already pinned. This is will be used for some page
    actions (e.g. Translate), which avoid showing the page action if it's
    already pinned.
    
    This is implemented by adding a reference to
    `PinnedToolbarActionsModel` to `PageActionController`. We could take
    this one step further by observing the model and showing/hiding page
    actions in response to (un)pinning, but this is out of scope for now (no
    different from current page actions behaviour).
    
    Change-Id: Ic6e1edc86aa9245fc68591a13ab941dd7b90d772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6113671
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401038}

--- chrome/browser/ui/tabs/tab_features.cc
+++ chrome/browser/ui/tabs/tab_features.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_delegate.h"
+#include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.h"
 #include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_translate_action_listener.h"
 #include "chrome/browser/ui/views/page_action/action_ids.h"
 #include "chrome/browser/ui/views/page_action/page_action_controller.h"
@@ -151,9 +152,11 @@ void TabFeatures::Init(TabInterface& tab, Profile* profile) {
               tab_group_sync_service, &tab);
     }
 
+    const auto* pinned_actions_model = PinnedToolbarActionsModel::Get(profile);
+    CHECK(pinned_actions_model);
     page_action_controller_ =
-        std::make_unique<page_actions::PageActionController>();
-    CHECK(tab.GetBrowserWindowInterface()->GetActions());
+        std::make_unique<page_actions::PageActionController>(
+            pinned_actions_model);
     page_action_controller_->Initialize(std::vector<actions::ActionId>(
         page_actions::kActionIds.begin(), page_actions::kActionIds.end()));
 
