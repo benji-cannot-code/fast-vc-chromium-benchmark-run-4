9a89cf8b02a5ba9d622c5c6dac59275e854e0eb2
Mark Cogan <marq@google.com>
2024-12-31 06:09:15 -0800

    Disable SearchEngineSettingsNonEEATestCase/testChangeSearchEngine
    Test is failing/crashing on device.
    
    Bug: 386926969
    Change-Id: I4d7a1eb4c97b1b7358abb2cca67cc8ff4220626a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108754
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401120}

--- ios/chrome/browser/settings/ui_bundled/search_engine_settings_eea_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/search_engine_settings_eea_egtest.mm
@@ -33,7 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Tests that when changing the default search engine, the URL used for the
 // search is updated.
-- (void)testChangeSearchEngine {
+// TODO(crbug.com/386926969): Test failing on tablet-device and phone-device.
+- (void)DISABLED_testChangeSearchEngine {
   [self startHTTPServer];
   [self addURLRewriter];
 
