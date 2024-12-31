11d2970916ea0006728f5c11bbcb278f055f49fc
Louis Romero <lpromero@google.com>
2024-12-31 06:09:37 -0800

    Fix Objective-C Style / ExplicitOwnership warning
    Fix style guide checker warning for tab strip view controller property
    in TabStripCoordinator.
    
    Bug: none
    Change-Id: I23eb8f055b22ed1badb72f8b84b1c1096280327c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6132584
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401121}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/coordinator/tab_strip_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/coordinator/tab_strip_coordinator.mm
@@ -63,8 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) TabStripMediator* mediator;
 // Helper providing context menu for tab strip items.
 @property(nonatomic, strong) TabStripContextMenuHelper* contextMenuHelper;
-
-@property TabStripViewController* tabStripViewController;
+// The view controller for the tab strip.
+@property(nonatomic, strong) TabStripViewController* tabStripViewController;
 
 @end
 
