b9b8acb85bea99763c06bad17a23b22d5eb79cba
Yiren Wang <yrw@chromium.org>
2024-12-30 17:31:55 -0800

    Add the interface SpeechRecognitionPhraseList
    Add the IDL files behind the flag MediaStreamTrackWebSpeech and allow
    an empty list
    
    Bug: 381349238
    Change-Id: I6ab8ac980512cb52ce66964c2faf1f1c4fe82030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6128697
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401037}

---
+++ third_party/blink/renderer/modules/speech/speech_recognition_phrase_list.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/speech/speech_recognition_phrase_list.h"
+
+namespace blink {
+
+SpeechRecognitionPhraseList* SpeechRecognitionPhraseList::Create() {
+  return MakeGarbageCollected<SpeechRecognitionPhraseList>();
+}
+
+void SpeechRecognitionPhraseList::Trace(Visitor* visitor) const {
+  visitor->Trace(phrases_);
+  ScriptWrappable::Trace(visitor);
+}
+
+SpeechRecognitionPhrase* SpeechRecognitionPhraseList::item(
+    uint64_t index) const {
+  if (index < 0 || index >= length()) {
+    return nullptr;
+  }
+  return phrases_[index].Get();
+}
+
+void SpeechRecognitionPhraseList::addItem(SpeechRecognitionPhrase* item) {
+  phrases_.push_back(item);
+}
+
+}  // namespace blink
