6afe9e3cb8ec387893942eeff0533da31abd25f8
Slobodan Pejic <slobodan@chromium.org>
2025-01-02 06:58:17 -0800

    Load the browser bound key on secure payment confirmation.
    When creating a payment assertion, load the stored browser bound key id
    for the relevant credential and relying party. Before this change secure
    payment confirmation would create a new browser bound key. After this
    change secure payment confirmation uses the browser bound key that was
    previously created (e.g. the browser bound key from credential creation).
    
    This change is behind the flag,
    chrome://flags/#enable-secure-payment-confirmation-browser-bound-key
    
    Bug: 377278827, b:378708356
    Change-Id: Ia902e9256e4c86de3b2878cfb1c8047df772f7cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6101779
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401411}

--- components/payments/content/secure_payment_confirmation_app.h
+++ components/payments/content/secure_payment_confirmation_app.h
@@ -58,6 +58,7 @@ class SecurePaymentConfirmationApp : public PaymentApp,
       const std::u16string& payment_instrument_label,
       std::unique_ptr<SkBitmap> payment_instrument_icon,
       std::vector<uint8_t> credential_id,
+      std::optional<std::vector<uint8_t>> browser_bound_key_id,
       const url::Origin& merchant_origin,
       base::WeakPtr<PaymentRequestSpec> spec,
       mojom::SecurePaymentConfirmationRequestPtr request,
@@ -111,6 +112,11 @@ class SecurePaymentConfirmationApp : public PaymentApp,
   void SetBrowserBoundKeyStoreForTesting(
       std::unique_ptr<BrowserBoundKeyStore> key_store);
 
+#if BUILDFLAG(IS_ANDROID)
+  const std::optional<std::vector<uint8_t>>& GetBrowserBoundKeyIdForTesting()
+      const;
+#endif  // BUILDFLAG(IS_ANDROID)
+
  private:
   void OnGetAssertion(
       base::WeakPtr<Delegate> delegate,
@@ -126,6 +132,7 @@ class SecurePaymentConfirmationApp : public PaymentApp,
   const std::u16string payment_instrument_label_;
   const std::unique_ptr<SkBitmap> payment_instrument_icon_;
   const std::vector<uint8_t> credential_id_;
+  const std::optional<std::vector<uint8_t>> browser_bound_key_id_;
   const url::Origin merchant_origin_;
   const base::WeakPtr<PaymentRequestSpec> spec_;
   const mojom::SecurePaymentConfirmationRequestPtr request_;
