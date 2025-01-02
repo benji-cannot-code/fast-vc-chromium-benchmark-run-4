a6ede56f4abdf91e6ceb01065274949f8414a6ef
Marijn Kruisselbrink <mek@chromium.org>
2025-01-02 11:10:35 -0800

    [PWA] Fix IncompatibleAppShim integration test.
    Unfortunately this test broke while being unintentionally disabled.
    Fix the test by adding a flag to allow the LaunchFromAppShim call
    inside the LaunchFromPlatformShortcut call to fail, as in this
    specific test the initial launch will fail, even though the eventual
    app launch still succeeds.
    
    Bug: 386749842
    Change-Id: If38ff1663c28abc25d362f7593a8a60445327ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6133859
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401517}

--- chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac.cc
@@ -69,7 +69,7 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegration, CorruptAppShim) {
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegration, IncompatibleAppShim) {
   // Install and close the PWA.
-  helper_.CreateShortcut(Site::kStandalone, WindowOptions::kWindowed);
+  helper_.InstallMenuOption(Site::kStandalone);
   helper_.CheckWindowCreated();
   helper_.ClosePwa();
   // Now mess up the shortcut.
@@ -85,7 +85,8 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegration, IncompatibleAppShim) {
   web_app::startup::SetStartupDoneCallbackForTesting(base::BindOnce(
       []() { apps::TakeShimStartupDoneCallbackForTesting().Run(); }));
 
-  helper_.LaunchFromPlatformShortcut(Site::kStandalone);
+  helper_.LaunchFromPlatformShortcut(Site::kStandalone,
+                                     /*allow_shim_failure=*/true);
   helper_.CheckWindowCreated();
 }
 
