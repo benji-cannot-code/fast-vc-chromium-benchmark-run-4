f4c5a3743de3939a6b7878fbdafce93759f9caee
Dana Fried <dfried@chromium.org>
2025-01-02 15:08:52 -0800

    Add trigger for IPH promo
    This adds an in-code trigger to the glic promo which was already added.
    The promo will attempt to trigger periodically if:
     - The page is valid
     - The promo is eligible to show
     - The promo has not already been shown
    
    Clicking on the entrypoint will close an open promo and mark the feature
    as used, preventing it from showing again in the future.
    
    Detailed descriptions of the behavior and why this approach was
    chosen as a first pass for the trigger are in the code itself.
    
    Tests will require situations where glic is active and may require mocking a significant number of systems. This change should be hand-
    tested first and then regression tests added when we know we actually
    want to go with this behavior.
    
    TODO: If focus manager operates before FRE, switch to using that for
    triggering. This bug will remain open until the correct approach is
    settled (either the current one here, or the proposed update).
    
    Low-Coverage-Reason: HARD_TO_TEST
    Bug: 379340988
    Change-Id: Ic48a6be93f826ea2c618d84051d07897ff648332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6118993
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401617}

--- chrome/browser/glic/glic_tab_indicator_helper.h
+++ chrome/browser/glic/glic_tab_indicator_helper.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_GLIC_GLIC_TAB_INDICATOR_HELPER_H_
 #define CHROME_BROWSER_GLIC_GLIC_TAB_INDICATOR_HELPER_H_
 
+#include <memory>
+
 #include "base/callback_list.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
@@ -26,6 +28,8 @@ class GlicTabIndicatorHelper {
   ~GlicTabIndicatorHelper();
 
  private:
+  class PromoHelper;
+
   // Sets the last focused tab to `contents`.
   void SetLastFocusedTab(const content::WebContents* contents);
 
@@ -38,6 +42,7 @@ class GlicTabIndicatorHelper {
   const raw_ref<BrowserWindowInterface> browser_;
   base::WeakPtr<const content::WebContents> last_focused_tab_;
   base::CallbackListSubscription change_subscription_;
+  const std::unique_ptr<PromoHelper> promo_helper_;
 };
 
 }  // namespace glic
