e0febc211ec31ebe8e75734e092ed73f2a736906
Wan-Teh Chang <wtc@google.com>
2025-01-02 13:21:06 -0800

    Roll src/third_party/libaom/source/libaom/ f74eae54f..433be28b4 (3 commits)
    https://aomedia.googlesource.com/aom.git/+log/f74eae54f3be..433be28b4f4f
    
    $ git log f74eae54f..433be28b4 --date=short --no-merges --format='%ad %ae %s'
    2024-12-18 jianj Define opaque struct of RC class for C
    2024-12-20 jzern cmake: add float-cast-overflow w/ubsan
    2024-12-20 wtc Don't check size limit in aom_realloc_frame_buffer
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=eugene@chromium.org
    
    Bug: b:307414544
    Change-Id: I799672ded4cfe483d93287a8ea80248c007d8f2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6138128
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401578}

--- third_party/libaom/source/config/config/aom_version.h
+++ third_party/libaom/source/config/config/aom_version.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (c) 2024, Alliance for Open Media. All rights reserved.
+ * Copyright (c) 2025, Alliance for Open Media. All rights reserved.
  *
  * This source code is subject to the terms of the BSD 2 Clause License and
  * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 11
 #define VERSION_PATCH 0
-#define VERSION_EXTRA "157-gf74eae54f3"
+#define VERSION_EXTRA "160-g433be28b4f"
 #define VERSION_PACKED \
   ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "3.11.0-157-gf74eae54f3"
-#define VERSION_STRING " 3.11.0-157-gf74eae54f3"
+#define VERSION_STRING_NOSP "3.11.0-160-g433be28b4f"
+#define VERSION_STRING " 3.11.0-160-g433be28b4f"
 #endif  // AOM_VERSION_H_
