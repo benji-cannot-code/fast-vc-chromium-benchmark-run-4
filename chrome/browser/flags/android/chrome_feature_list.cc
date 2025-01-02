134dd093d52525c484095d13e5accfbbfba70ee2
Vishwas Uppoor <vishwasuppoor@google.com>
2025-01-02 09:57:54 -0800

    [Metadata] Cleanup finch flag
    The AutofillImageFetcher is being used by all payment types to fetch
    image assets. AutofillEnableCardArtServerSideStretching was an
    optimization that only applied to credit card image assets.
    
    Cleanup AutofillEnableCardArtServerSideStretching without rolling out.
    
    Fixed: 356236072
    Change-Id: If67af25a9c33e74fe4edb0b32f8b2934d56a4c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112600
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401479}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -92,7 +92,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &autofill::features::kAutofillEnableRankingFormulaAddressProfiles,
     &autofill::features::kAutofillEnableRankingFormulaCreditCards,
     &autofill::features::kAutofillEnableNewCardArtAndNetworkImages,
-    &autofill::features::kAutofillEnableCardArtServerSideStretching,
     &autofill::features::kAutofillEnableVirtualCardMetadata,
     &autofill::features::kAutofillEnableCardArtImage,
     &autofill::features::kAutofillEnableCardBenefitsForAmericanExpress,
