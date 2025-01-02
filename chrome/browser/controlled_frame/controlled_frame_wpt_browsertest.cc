630d67db051ad61b77dbd9b57dcf2162fb5576cf
Zelin Liu <zelin@chromium.org>
2025-01-02 13:06:52 -0800

    controlledframe: Re-enable WebRequest tests & monitor onErrorOccurred
    onErrorOccurred gets triggered in the CI causing flakiness but it's not
    reproducible in local runs. Adding a temporary monitor to see what the
    errors are.
    
    Bug: 386380410
    Change-Id: I646e13cd1fdaf6bc1edae562a9885747ca58c034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6137042
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401568}

--- chrome/browser/controlled_frame/controlled_frame_wpt_browsertest.cc
+++ chrome/browser/controlled_frame/controlled_frame_wpt_browsertest.cc
@@ -28,8 +28,8 @@ namespace controlled_frame {
 
 namespace {
 
-// TODO(b/386380410): Re-enable event_handlers.window.js and
-// webrequest_event_handlers.window.js once they no longer timeout.
+// TODO(crbug.com/386380410): Re-enable event_handlers.window.js, once it no
+// longer times out.
 const auto kTestFiles = testing::Values("add_content_scripts.window.js",
                                         "camera.window.js",
                                         "client_hints_user_agent.window.js",
@@ -37,7 +37,8 @@ const auto kTestFiles = testing::Values("add_content_scripts.window.js",
                                         "new_window.window.js",
                                         "no_callback.window.js",
                                         "scheme.window.js",
-                                        "user_agent_override.window.js");
+                                        "user_agent_override.window.js",
+                                        "webrequest_event_handlers.window.js");
 
 constexpr char kTestDirectory[] = "chrome/test/data/controlled_frame";
 constexpr char kTestHarnessPath[] =
