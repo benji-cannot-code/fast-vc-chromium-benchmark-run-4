420bef50e1a3d67bc70f6034c2158e6687c9cb3b
Anna Tsvirchkova <atsvirchkova@google.com>
2025-01-02 03:11:24 -0800

    [Sheriff] Disable SensitiveContentTest#testSwipingBetweenTabsIsSensitive
    The test looks quite stable on tablets but fails on phone devices, so
    disabling it only on phones.
    
    Bug: 378742136
    Change-Id: Ic91f6d77ca2f47d0974aeb0dc40dfc5602f11668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6122580
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401358}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
@@ -32,6 +32,7 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.LargeTest;
 import androidx.test.filters.MediumTest;
 
+import org.chromium.base.test.util.DisableIf;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -450,6 +451,7 @@ public class SensitiveContentTest {
     @Test
     @LargeTest
     @EnableFeatures(SensitiveContentFeatures.SENSITIVE_CONTENT_WHILE_SWITCHING_TABS)
+    @DisableIf.Device(DeviceFormFactor.PHONE) // crbug.com/378742136
     public void testSwipingBetweenTabsIsSensitive() {
         // Set up.
         final View contentContainer =
