ff933a2e875c68443d590f5747f56f94347c05e9
Abigail Katcoff <abigailkatcoff@chromium.org>
2025-01-02 09:15:09 -0800

    Protected Audience: fix HttpCachedTrustedBiddingSignalsAge metric
    The request time on the response head corresponds with the original
    request time (for the validated and cached entries), not the new
    request time, so this metric wasn't being recorded as expected. Do not
    record for the "updated" case because we do not appear to have the
    relevant fields for it.
    
    OBSOLETE_HISTOGRAM[Ads.InterestGroup.Auction.HttpCachedTrustedBiddingSignalsAge]=replaced by HttpCachedTrustedBiddingSignalsAge2
    
    Bug: 383579272
    Change-Id: I223c3c666f12662b13dc32fcc228f238f42778ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6116043
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Orr Bernstein <orrb@google.com>
    Commit-Queue: Abigail Katcoff <abigailkatcoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401463}

--- content/services/auction_worklet/public/cpp/auction_downloader.cc
+++ content/services/auction_worklet/public/cpp/auction_downloader.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "base/unguessable_token.h"
 #include "base/values.h"
@@ -183,18 +184,6 @@ void WriteTraceTiming(const net::LoadTimingInfo& timing,
   dict.Add("pushEnd", timing.push_end.since_origin().InSecondsF());
 }
 
-bool IsCachedResponse(const network::mojom::URLResponseHead& response_head) {
-  if (response_head.was_fetched_via_cache) {
-    return true;
-  }
-
-  // was_fetched_via_cache only includes the cases where the entry was fresh
-  // or was validated. Include `response_head.original_response_time
-  // < response_head.response_time` so that we include the cases the entry
-  // was updated as well.
-  return response_head.original_response_time < response_head.response_time;
-}
-
 }  // namespace
 
 AuctionDownloader::AuctionDownloader(
@@ -264,8 +253,9 @@ AuctionDownloader::AuctionDownloader(
   simple_url_loader_->SetOnRedirectCallback(base::BindRepeating(
       &AuctionDownloader::OnRedirect, base::Unretained(this)));
 
-  simple_url_loader_->SetOnResponseStartedCallback(base::BindRepeating(
-      &AuctionDownloader::OnResponseStarted, base::Unretained(this)));
+  simple_url_loader_->SetOnResponseStartedCallback(
+      base::BindRepeating(&AuctionDownloader::OnResponseStarted,
+                          base::Unretained(this), base::Time::Now()));
 
   simple_url_loader_->SetTimeoutDuration(kRequestTimeout);
 
@@ -410,6 +400,7 @@ std::optional<std::string> AuctionDownloader::CheckResponseAllowed(
 }
 
 void AuctionDownloader::OnResponseStarted(
+    base::Time request_time,
     const GURL& final_url,
     const network::mojom::URLResponseHead& response_head) {
   if (network_events_delegate_ != nullptr) {
@@ -483,11 +474,11 @@ void AuctionDownloader::OnResponseStarted(
 
   // Record the cached response's age if there was an entry in the cache.
   if (is_trusted_bidding_signals_kvv1_download_ &&
-      IsCachedResponse(response_head) &&
-      response_head.original_response_time < response_head.request_time) {
+      response_head.was_fetched_via_cache &&
+      response_head.original_response_time < request_time) {
     base::UmaHistogramTimes(
-        "Ads.InterestGroup.Auction.HttpCachedTrustedBiddingSignalsAge",
-        response_head.request_time - response_head.original_response_time);
+        "Ads.InterestGroup.Auction.HttpCachedTrustedBiddingSignalsAge2",
+        request_time - response_head.original_response_time);
   }
 
   if (response_started_callback_) {
