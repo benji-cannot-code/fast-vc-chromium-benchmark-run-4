4e31951836356f7d72fe6ea2e59db2b3082cfe1f
Mikel Astiz <mastiz@chromium.org>
2025-01-02 02:37:28 -0800

    Update tests to avoid implicit conversion between GaiaId and std::string
    Class GaiaId was introduced to improve type safety and avoid strings
    being used, which is more error-prone.
    
    Temporarily, implicit conversion between GaiaId and std::string was
    allowed, until all occurrences relying on it are migrated. This patch
    updates tests under chrome/browser/ash/floating_workspace.
    
    This CL was uploaded by git cl split.
    
    R=aprilzhou@google.com
    
    Bug: 380416867
    Change-Id: I8ae1130e033ae17160737fefd97d84e48e137f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105633
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401349}

--- chrome/browser/ash/floating_workspace/floating_workspace_service_unittest.cc
+++ chrome/browser/ash/floating_workspace/floating_workspace_service_unittest.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/fake_user_manager.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "content/public/test/browser_task_environment.h"
+#include "google_apis/gaia/gaia_id.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
@@ -421,7 +422,7 @@ class FloatingWorkspaceServiceTest : public testing::Test {
                                       /*is_child=*/false);
     CoreAccountInfo account_info;
     account_info.email = kTestAccount;
-    account_info.gaia = "gaia";
+    account_info.gaia = GaiaId("gaia");
     account_info.account_id = CoreAccountId::FromGaiaId(account_info.gaia);
     test_sync_service_.SetSignedIn(signin::ConsentLevel::kSync, account_info);
 
@@ -2304,7 +2305,7 @@ class FloatingWorkspaceServiceMultiUserTest
                                       /*is_child=*/false);
     CoreAccountInfo account_info;
     account_info.email = kTestAccount2;
-    account_info.gaia = "gaia2";
+    account_info.gaia = GaiaId("gaia2");
     account_info.account_id = CoreAccountId::FromGaiaId(account_info.gaia);
     test_sync_service()->SetSignedIn(signin::ConsentLevel::kSync, account_info);
     fake_desk_sync_service2_ =
