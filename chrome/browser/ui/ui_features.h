809b694ba99c9d1611c5825d4f6d16cdc53c7d54
Kaan Alsan <alsan@chromium.org>
2025-01-02 12:32:33 -0800

    Code Health: Clean up stale StopLoadingAnimationForHiddenWindow
    Bug: 356235463
    Change-Id: Idb26ed302b62b3606fb4085a45fb32c0a9ef6b33
    Fixed: 356235463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6133675
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Alex Attar <aattar@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401553}

--- chrome/browser/ui/ui_features.h
+++ chrome/browser/ui/ui_features.h
@@ -245,8 +245,6 @@ BASE_DECLARE_FEATURE(kViewsFirstRunDialog);
 BASE_DECLARE_FEATURE(kViewsJSAppModalDialog);
 #endif
 
-BASE_DECLARE_FEATURE(kStopLoadingAnimationForHiddenWindow);
-
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 BASE_DECLARE_FEATURE(kUsePortalAccentColor);
 #endif
