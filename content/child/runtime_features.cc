3a9d4d8549c907fd7fa703e6337d0228223609ed
Abigail Katcoff <abigailkatcoff@chromium.org>
2025-01-02 10:02:26 -0800

    Topics: Sending Topics with image requests
    Send the Topics request header on image requests that specify the "browsingTopics" attribute.
    
    Use much of the existing plumbing work from the fetch() API. Adapt many
    of the tests for the fetch() API to also run for <img> attribute.
    
    PR: https://github.com/patcg-individual-drafts/topics/pull/357
    
    Bug: 382532794
    
    Change-Id: Id5975006a0c842df8765c49470c5f30bca7586c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6067268
    Commit-Queue: Abigail Katcoff <abigailkatcoff@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401483}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -396,6 +396,9 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
            kSetOnlyIfOverridden},
           {"TopicsDocumentAPI",
            raw_ref(features::kPrivacySandboxAdsAPIsM1Override)},
+          {"TopicsImgAPI", raw_ref(features::kPrivacySandboxAdsAPIsOverride),
+           kSetOnlyIfOverridden},
+          {"TopicsImgAPI", raw_ref(features::kPrivacySandboxAdsAPIsM1Override)},
           {"TouchTextEditingRedesign",
            raw_ref(features::kTouchTextEditingRedesign)},
           {"TrustedTypesFromLiteral",
@@ -584,6 +587,7 @@ void ResolveInvalidConfigurations() {
         << blink::features::kBrowsingTopics.name << " in addition.";
     WebRuntimeFeatures::EnableTopicsAPI(false);
     WebRuntimeFeatures::EnableTopicsDocumentAPI(false);
+    WebRuntimeFeatures::EnableTopicsImgAPI(false);
   } else {
     if (!base::FeatureList::IsEnabled(
             blink::features::kBrowsingTopicsDocumentAPI)) {
--- third_party/blink/renderer/core/html/parser/html_preload_scanner.cc
+++ third_party/blink/renderer/core/html/parser/html_preload_scanner.cc
@@ -370,6 +370,11 @@ class TokenPreloadScanner::StartTagScanner {
       request->SetSharedStorageWritableOptedIn(true);
     }
 
+    if (browsing_topics_attr_set_) {
+      DCHECK(is_img);
+      request->SetBrowsingTopicsEligible(true);
+    }
+
     return request;
   }
 
@@ -452,6 +457,8 @@ class TokenPreloadScanner::StartTagScanner {
       attributionsrc_attr_set_ = true;
     } else if (Match(attribute_name, html_names::kSharedstoragewritableAttr)) {
       shared_storage_writable_opted_in_ = true;
+    } else if (Match(attribute_name, html_names::kBrowsingtopicsAttr)) {
+      browsing_topics_attr_set_ = true;
     } else if (use_data_src_attr_match_for_image_ &&
                Match(attribute_name, html_names::kDataSrcAttr) &&
                img_src_url_.IsNull()) {
@@ -819,6 +826,7 @@ class TokenPreloadScanner::StartTagScanner {
   const HashSet<String>* disabled_image_types_;
   bool attributionsrc_attr_set_ = false;
   bool shared_storage_writable_opted_in_ = false;
+  bool browsing_topics_attr_set_ = false;
   std::optional<float> resource_width_;
   std::optional<float> resource_height_;
   features::LcppPreloadLazyLoadImageType preload_lazy_load_image_type_;
