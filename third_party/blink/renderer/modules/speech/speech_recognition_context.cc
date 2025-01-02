95d2b34ed9d484c91e449e9fb0cb66dc8587949d
Yiren Wang <yrw@chromium.org>
2025-01-02 14:14:48 -0800

    Add the interface SpeechRecognitionContext
    Add the IDL files behind the flag MediaStreamTrackWebSpeech
    
    Bug: 381349238
    Change-Id: I94a1bff952adf6bbed2e7deb90de9de7a2a165e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6136150
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401596}

---
+++ third_party/blink/renderer/modules/speech/speech_recognition_context.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/speech/speech_recognition_context.h"
+
+namespace blink {
+
+SpeechRecognitionContext* SpeechRecognitionContext::Create(
+    SpeechRecognitionPhraseList* phrases) {
+  return MakeGarbageCollected<SpeechRecognitionContext>(phrases);
+}
+
+SpeechRecognitionContext::SpeechRecognitionContext(
+    SpeechRecognitionPhraseList* phrases)
+    : phrases_(phrases) {}
+
+void SpeechRecognitionContext::Trace(Visitor* visitor) const {
+  visitor->Trace(phrases_);
+  ScriptWrappable::Trace(visitor);
+}
+
+}  // namespace blink
