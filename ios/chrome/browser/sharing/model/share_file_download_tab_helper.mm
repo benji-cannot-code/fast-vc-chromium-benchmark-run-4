d1ccf22a6704b06960ec14598c7615558f37a5e1
Cheick Cisse <cheickcisse@google.com>
2025-01-02 07:05:07 -0800

    [iOS][DownloadRestrictions] Remove download options from the share menu
    This CL removes the download options from the sharing menu when download
    restrictions are enabled.
    
    Screen recording": https://drive.google.com/file/d/1TPtSPHJcWW6JoydlD6eQtdTqn3IwUrkf/view?usp=sharing
    
    Bug: chromium:383772883
    Change-Id: Id89f11e0ef2d34ba81c54a6c2cb241dd07e305eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6104179
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401413}

--- ios/chrome/browser/sharing/model/share_file_download_tab_helper.mm
+++ ios/chrome/browser/sharing/model/share_file_download_tab_helper.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
 #import "base/strings/sys_string_conversions.h"
+#import "ios/chrome/browser/download/model/download_manager_tab_helper.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/navigation/navigation_context.h"
@@ -166,6 +167,10 @@ std::u16string ShareFileDownloadTabHelper::GetFileNameSuggestion() {
 // static
 bool ShareFileDownloadTabHelper::ShouldDownload(web::WebState* web_state) {
   if (!web_state) {
+    return false;
+  }
+
+  if (DownloadManagerTabHelper::ShouldRestrictDownloadToFile(web_state)) {
     return false;
   }
 
--- ios/chrome/browser/sharing/ui_bundled/sharing_coordinator_unittest.mm
+++ ios/chrome/browser/sharing/ui_bundled/sharing_coordinator_unittest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/bookmarks/browser/bookmark_node.h"
 #import "components/bookmarks/test/bookmark_test_helpers.h"
 #import "ios/chrome/browser/bookmarks/model/bookmark_ios_unit_test_support.h"
+#import "ios/chrome/browser/download/model/download_manager_tab_helper.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
@@ -99,6 +100,7 @@ TEST_F(SharingCoordinatorTest, Start_ShareCurrentPage) {
       std::make_unique<web::FakeNavigationManager>());
   test_web_state->SetCurrentURL(test_url);
   test_web_state->SetBrowserState(browser_->GetProfile());
+  DownloadManagerTabHelper::CreateForWebState(test_web_state.get());
 
   auto frames_manager = std::make_unique<web::FakeWebFramesManager>();
   web::FakeWebFramesManager* frames_manager_ptr = frames_manager.get();
