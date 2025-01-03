6794fb23c279a410f46cb6f79c9bd1c9f699de5d
Peilin Wang <peilinwang@google.com>
2025-01-03 13:25:52 -0800

    [EdgeToEdge] Fix bug for caching chin color
    The color was cached while the view is not visible, so if the color was
    updated to a different color when the view was visible, then the
    incorrect cached color would be applied.
    
    Bug: 387301946
    Change-Id: Ice396843a765dea6312f81afa802d6d2d4c32fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6142291
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1401945}

--- chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeBottomChinMediator.java
+++ chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeBottomChinMediator.java
@@ -204,8 +204,8 @@ class EdgeToEdgeBottomChinMediator
 
     @Override
     public void onNavigationBarColorChanged(int color) {
+        mNavigationBarColor = color;
         if (!isVisible()) {
-            mNavigationBarColor = color;
             return;
         }
 
@@ -215,8 +215,8 @@ class EdgeToEdgeBottomChinMediator
 
     @Override
     public void onNavigationBarDividerChanged(int dividerColor) {
+        mDividerColor = dividerColor;
         if (!isVisible()) {
-            mDividerColor = dividerColor;
             return;
         }
 
@@ -298,5 +298,13 @@ class EdgeToEdgeBottomChinMediator
     @Override
     public void clearOffsetTag() {
         mModel.set(OFFSET_TAG, null);
+    }
+
+    public int getDividerColorForTesting() {
+        return mDividerColor;
+    }
+
+    public int getNavigationBarColorForTesting() {
+        return mNavigationBarColor;
     }
 }
--- chrome/browser/ui/android/edge_to_edge/internal/junit/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeBottomChinMediatorTest.java
+++ chrome/browser/ui/android/edge_to_edge/internal/junit/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeBottomChinMediatorTest.java
@@ -126,9 +126,17 @@ public class EdgeToEdgeBottomChinMediatorTest {
 
         mMediator.onNavigationBarColorChanged(Color.BLUE);
         assertEquals("The color should have been updated to blue.", Color.BLUE, mModel.get(COLOR));
+        assertEquals(
+                "The cached color should have been updated to blue.",
+                Color.BLUE,
+                mMediator.getNavigationBarColorForTesting());
 
         mMediator.onNavigationBarColorChanged(Color.RED);
         assertEquals("The color should have been updated to red.", Color.RED, mModel.get(COLOR));
+        assertEquals(
+                "The cached color should have been updated to red.",
+                Color.RED,
+                mMediator.getNavigationBarColorForTesting());
 
         // scroll view offscreen
         mMediator.onBrowserControlsOffsetUpdate(mModel.get(HEIGHT), false);
@@ -152,6 +160,10 @@ public class EdgeToEdgeBottomChinMediatorTest {
 
         mMediator.onNavigationBarDividerChanged(Color.WHITE);
         assertEquals(
+                "The cached divider color should have been updated to WHITE.",
+                Color.WHITE,
+                mMediator.getDividerColorForTesting());
+        assertEquals(
                 "The divider color should have been updated to WHITE.",
                 Color.WHITE,
                 mModel.get(DIVIDER_COLOR));
@@ -161,6 +173,10 @@ public class EdgeToEdgeBottomChinMediatorTest {
                 "The divider color should have been updated to TRANSPARENT.",
                 Color.TRANSPARENT,
                 mModel.get(DIVIDER_COLOR));
+        assertEquals(
+                "The cached divider color should have been updated to TRANSPARENT.",
+                Color.TRANSPARENT,
+                mMediator.getDividerColorForTesting());
 
         // scroll view offscreen
         mMediator.onBrowserControlsOffsetUpdate(mModel.get(HEIGHT), false);
