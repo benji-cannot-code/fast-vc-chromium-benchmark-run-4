a6caac83d7aba07a86e4cfd6635b860c21a11063
Eshwar Stalin <estalin@chromium.org>
2025-01-03 13:41:20 -0800

    Fixing issue where deleting tab groups locally doesn't delete it from sync
    When deleting tab groups locally, there was a regression where it wasn't
    being treated as a local removal in the model. This results in the saved
    tab group bridge not issuing a deletion command to remove it from sync.
    This also keeps both V1 and V2 implementations consistent.
    
    Bug: 386987509
    Change-Id: I28d761123f99b9a71c27e4c791347723123392f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6138133
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401954}

--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
@@ -838,8 +838,9 @@ void TabGroupEditorBubbleView::DeleteGroupPressed() {
               kDeleteGroup);
     }
   } else {
+    CHECK(saved_group->local_group_id().has_value());
+    tab_group_service->RemoveGroup(saved_group->local_group_id().value());
     DeleteGroupFromTabstrip();
-    tab_group_service->RemoveGroup(saved_group->saved_guid());
   }
   GetWidget()->Close();
 }
