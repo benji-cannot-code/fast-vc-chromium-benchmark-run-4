8f8de4793b250186dee24521d111a60417d99c49
Eric Sum <esum@google.com>
2025-01-03 12:02:20 -0800

    Use box layout for WindowMiniViewHeaderView.
    Box layout is ~10x faster than flex layout in this use case.
    For most real-world cases, the difference is probably imperceptible,
    but the tast test in the bug surfaced this difference.
    
    Video recordings of this tast test before and after this change
    are below.
    Before:
    http://screencast/cast/NDUyMjE2NTc1OTExNTI2NHw3M2FhYTQ1Ny0zYw
    After:
    http://screencast/cast/NTY2MTc5NTM2Nzc4MDM1Mnw5OTBkMDg3YS1mNA
    
          Ran ui.SplitViewResizePerf tast test
          Ran with --ui-show-paint-rects to ensure there's not overpainting.
    
    Bug: b:385148375
    Test: Tested both short and long window labels.
    Change-Id: Ie1417bb1a71b6b6589b80eab6aec3a63fe6e9f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6137996
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401919}

--- ash/wm/overview/overview_item_view.cc
+++ ash/wm/overview/overview_item_view.cc
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/animation_builder.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/layout/flex_layout_types.h"
-#include "ui/views/layout/flex_layout_view.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 
@@ -94,11 +92,6 @@ OverviewItemView::OverviewItemView(
 
   close_button_->SetPaintToLayer();
   close_button_->layer()->SetFillsBoundsOpaquely(false);
-  close_button_->SetProperty(
-      views::kFlexBehaviorKey,
-      views::FlexSpecification().WithOrder(
-          WindowMiniViewHeaderView::IconLabelFlexPriorities::
-              kIconOrCloseButton));
   views::InkDrop::Get(close_button_)
       ->SetMode(views::InkDropHost::InkDropMode::ON_NO_GESTURE_HANDLER);
   close_button_->GetViewAccessibility().SetName(
