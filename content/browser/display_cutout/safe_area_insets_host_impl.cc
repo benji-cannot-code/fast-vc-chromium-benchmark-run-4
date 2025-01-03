d5dfa4f4c91f45bd111f28b96f52bcdca3258baa
Wenyu Fu <wenyufu@chromium.org>
2025-01-03 14:07:50 -0800

    Integrate ComplexSafeAreaConstraintChanged signal
    Bug: 385221500
    Change-Id: I1c2c4e8a0d919f2caf10a42ef50cca99104103c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143053
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401961}

--- content/browser/display_cutout/safe_area_insets_host_impl.cc
+++ content/browser/display_cutout/safe_area_insets_host_impl.cc
@@ -107,8 +107,9 @@ void SafeAreaInsetsHostImpl::ViewportFitChangedForFrame(
 void SafeAreaInsetsHostImpl::ComplexSafeAreaConstraintChangedForFrame(
     RenderFrameHost* rfh,
     bool has_constraint) {
-  DCHECK(rfh);
-  web_contents_impl_->NotifySafeAreaConstraintChanged(has_constraint);
+  if (rfh == ActiveRenderFrameHost()) {
+    web_contents_impl_->NotifySafeAreaConstraintChanged(has_constraint);
+  }
 }
 
 void SafeAreaInsetsHostImpl::MaybeActiveRenderFrameHostChanged() {
