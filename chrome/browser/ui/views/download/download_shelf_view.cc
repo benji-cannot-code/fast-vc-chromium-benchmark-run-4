2a5d376041d84d68ed97c0dc17566ad5a6d2bc46
Caroline Rising <corising@chromium.org>
2025-01-03 14:26:29 -0800

    Create build file for c/b/ui/views/download/
    This CL shouldn't change behavior. This is needed for changes to
    downloads which make the download button pinnable in the toolbar and
    will add a new controller to BrowserWindowFeatures. Note
    DownloadToolbarButtonView is not included in the new BUILD target since
    it would create a new circular dependency with toolbar and those files
    should be deleted after the downloads toolbar pinning work is complete
    and launched.
    
    Bug: 323962334
    Change-Id: Ic3d8a2c1f2dea154a0c7c69a559933a0a05e5461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143253
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401971}

--- chrome/browser/ui/views/download/download_shelf_view.cc
+++ chrome/browser/ui/views/download/download_shelf_view.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/download/download_item_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/download/public/common/download_item.h"
 #include "components/strings/grit/components_strings.h"
