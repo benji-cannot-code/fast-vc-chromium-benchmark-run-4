bb7a1efa20730c370ef3adacccb357e38c92df4c
Boris Sazonov <bsazonov@chromium.org>
2025-01-03 06:20:54 -0800

    [Signin][Android] Expose AccountManagedStatusFinder to Java
    Creates a Java bridge for AccountManagedStatusFinder.
    
    Bug: 348373729
    Change-Id: I472f3b6cb4c52fc47c836be7d7037d20eadb51c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109046
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1401795}

--- components/signin/public/identity_manager/account_managed_status_finder.h
+++ components/signin/public/identity_manager/account_managed_status_finder.h
@@ -41,6 +41,10 @@ class AccountManagedStatusFinder : public signin::IdentityManager::Observer {
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
   //
+  // This enum is also used in Java.
+  // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.components.signin.identitymanager
+  // GENERATED_JAVA_CLASS_NAME_OVERRIDE: AccountManagedStatusFinderOutcome
+  //
   // LINT.IfChange(AccountManagedStatusFinderOutcome)
   enum class Outcome {
     // Check isn't complete yet.
@@ -90,6 +94,12 @@ class AccountManagedStatusFinder : public signin::IdentityManager::Observer {
   void OnRefreshTokensLoaded() override;
   void OnIdentityManagerShutdown(
       signin::IdentityManager* identity_manager) override;
+
+#if BUILDFLAG(IS_ANDROID)
+  // Implementation for JNI methods.
+  void DestroyNativeObject(JNIEnv* env);
+  jint GetOutcomeFromNativeObject(JNIEnv* env) const;
+#endif
 
  private:
   void OnTimeoutReached();
