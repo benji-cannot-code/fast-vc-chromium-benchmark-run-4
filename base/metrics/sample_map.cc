a473610ecf51af1f7e7beac069f7a50cb51c64c9
Peter Kasting <pkasting@chromium.org>
2025-01-03 11:01:36 -0800

    Move SampleMap's iterator to its own header.
    This is in preparation for refactoring it to share code with
    PersistentSampleMap's iterator. Just a move and rename in this CL, more
    refactors to come.
    
    Bug: 364987728
    Change-Id: I90909b72681d07101a718994ed63373f5548c02d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6134707
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401893}

--- base/metrics/sample_map.cc
+++ base/metrics/sample_map.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/check.h"
+#include "base/metrics/sample_map_iterator.h"
 #include "base/numerics/safe_conversions.h"
 
 namespace base {
@@ -15,54 +16,19 @@ namespace base {
 typedef HistogramBase::Count Count;
 typedef HistogramBase::Sample Sample;
 
-namespace {
-
-// An iterator for going through a SampleMap. The logic here is identical
-// to that of the iterator for PersistentSampleMap but with different data
-// structures. Changes here likely need to be duplicated there.
-template <typename T, typename I>
-class IteratorTemplate : public SampleCountIterator {
- public:
-  explicit IteratorTemplate(T& sample_counts)
-      : iter_(sample_counts.begin()), end_(sample_counts.end()) {
-    SkipEmptyBuckets();
-  }
-
-  ~IteratorTemplate() override;
-
-  // SampleCountIterator:
-  bool Done() const override { return iter_ == end_; }
-  void Next() override {
-    DCHECK(!Done());
-    ++iter_;
-    SkipEmptyBuckets();
-  }
-  void Get(HistogramBase::Sample* min,
-           int64_t* max,
-           HistogramBase::Count* count) override;
-
- private:
-  void SkipEmptyBuckets() {
-    while (!Done() && iter_->second == 0) {
-      ++iter_;
-    }
-  }
-
-  I iter_;
-  const I end_;
-};
-
 typedef std::map<HistogramBase::Sample, HistogramBase::Count> SampleToCountMap;
-typedef IteratorTemplate<const SampleToCountMap,
+typedef SampleMapIterator<const SampleToCountMap,
                          SampleToCountMap::const_iterator>
-    SampleMapIterator;
+    NonExtractingSampleMapIterator;
 
 template <>
-SampleMapIterator::~IteratorTemplate() = default;
+NonExtractingSampleMapIterator::~SampleMapIterator() = default;
 
 // Get() for an iterator of a SampleMap.
 template <>
-void SampleMapIterator::Get(Sample* min, int64_t* max, Count* count) {
+void NonExtractingSampleMapIterator::Get(Sample* min,
+                                         int64_t* max,
+                                         Count* count) {
   DCHECK(!Done());
   *min = iter_->first;
   *max = strict_cast<int64_t>(iter_->first) + 1;
@@ -74,11 +40,11 @@ void SampleMapIterator::Get(Sample* min, int64_t* max, Count* count) {
   *count = iter_->second;
 }
 
-typedef IteratorTemplate<SampleToCountMap, SampleToCountMap::iterator>
+typedef SampleMapIterator<SampleToCountMap, SampleToCountMap::iterator>
     ExtractingSampleMapIterator;
 
 template <>
-ExtractingSampleMapIterator::~IteratorTemplate() {
+ExtractingSampleMapIterator::~SampleMapIterator() {
   // Ensure that the user has consumed all the samples in order to ensure no
   // samples are lost.
   DCHECK(Done());
@@ -98,8 +64,6 @@ void ExtractingSampleMapIterator::Get(Sample* min, int64_t* max, Count* count) {
   *count = iter_->second;
   iter_->second = 0;
 }
-
-}  // namespace
 
 SampleMap::SampleMap() : SampleMap(0) {}
 
@@ -135,7 +99,7 @@ Count SampleMap::TotalCount() const {
 }
 
 std::unique_ptr<SampleCountIterator> SampleMap::Iterator() const {
-  return std::make_unique<SampleMapIterator>(sample_counts_);
+  return std::make_unique<NonExtractingSampleMapIterator>(sample_counts_);
 }
 
 std::unique_ptr<SampleCountIterator> SampleMap::ExtractingIterator() {
