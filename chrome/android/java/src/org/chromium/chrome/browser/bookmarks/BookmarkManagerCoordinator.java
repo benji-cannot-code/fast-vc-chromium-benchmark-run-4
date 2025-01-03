5e8704dfcb091dd12c721e33cc641dcf4c0c3442
Menghan Yang <myuu@google.com>
2025-01-03 03:13:28 -0800

    [Android][UNO2FF] Implements bookmarks sync promo
    This CL adds the new bookmarks sync promo for signed-in users.
    
    It updates BookmarkSigninPromoDelegate to provide sign-in or
    bookmark sync promo content according to a private promo type
    property.
    
    Also, it moves the bookmark promo visibility logic from canShowPromo()
    to a new refreshPromoState() method, which is called each time the
    SigninPromoMediator is notified of events it observes. The promo
    content is refreshed if the promo state changes. (the implementation
    for other access points will be aligned in following CLs)
    
    String and tests are updated accordingly.
    
    See https://crbug.com/328402030#comment4 for recording.
    
    Bug: 328402030
    Change-Id: If0c6c11071d1cb04a9c414e7b4d9a0db780df7f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088207
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401745}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
@@ -33,8 +33,10 @@ import org.chromium.chrome.browser.commerce.ShoppingServiceFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.settings.SettingsNavigationFactory;
 import org.chromium.chrome.browser.signin.SigninAndHistorySyncActivityLauncherImpl;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
+import org.chromium.chrome.browser.sync.settings.ManageSyncSettings;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.ui.native_page.BasicNativePage;
 import org.chromium.chrome.browser.ui.signin.signin_promo.BookmarkSigninPromoDelegate;
@@ -108,6 +110,7 @@ public class BookmarkManagerCoordinator
 
     private final ObservableSupplierImpl<Boolean> mBackPressStateSupplier =
             new ObservableSupplierImpl<>();
+    private final Context mContext;
     private final ViewGroup mMainView;
     private final SelectableListLayout<BookmarkId> mSelectableListLayout;
     private final RecyclerView mRecyclerView;
@@ -144,6 +147,7 @@ public class BookmarkManagerCoordinator
             Profile profile,
             BookmarkUiPrefs bookmarkUiPrefs,
             @Nullable Runnable bookmarkOpenedCallback) {
+        mContext = context;
         mProfile = profile;
         mImageFetcher =
                 ImageFetcherFactory.createImageFetcher(
@@ -266,7 +270,8 @@ public class BookmarkManagerCoordinator
                                     context,
                                     mProfile,
                                     SigninAndHistorySyncActivityLauncherImpl.get(),
-                                    mMediator::onPromoVisibilityChange));
+                                    mMediator::onPromoVisibilityChange,
+                                    this::openSettings));
             dragReorderableRecyclerViewAdapter.registerType(
                     ViewType.PERSONALIZED_SIGNIN_PROMO,
                     mSigninPromoCoordinator::buildPromoView,
@@ -522,5 +527,13 @@ public class BookmarkManagerCoordinator
 
     public BookmarkUiPrefs getBookmarkUiPrefsForTesting() {
         return mBookmarkUiPrefs;
+    }
+
+    private void openSettings() {
+        SettingsNavigationFactory.createSettingsNavigation()
+                .startSettings(
+                        mContext,
+                        ManageSyncSettings.class,
+                        ManageSyncSettings.createArguments(false));
     }
 }
