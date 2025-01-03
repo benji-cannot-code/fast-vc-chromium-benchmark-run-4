d2cf0344d9a8e96799c3e3124d98d4b6037def76
Federica Germinario <fedegermi@google.com>
2025-01-03 07:46:16 -0800

    [ios] Remove legacy code from app_state+Testing.h
    The removed code was never called.
    The code was removed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/5920429
    
    Bug: none
    Change-Id: I13fa7ca9ac7e899d703fe6dd314572c99faf445d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6141593
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401820}

--- ios/chrome/app/application_delegate/app_state+Testing.h
+++ ios/chrome/app/application_delegate/app_state+Testing.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Redefined internally as readwrite.
 @property(nonatomic, assign) AppInitStage initStage;
 
-- (void)completeUIInitialization;
-
 // Updates the value of `initStage` without any of the associated checks or
 // logic.
 - (void)updateInitStage:(AppInitStage)initStage;
