a14fa018293ee3c43fdca8c7b4c843a85217c13f
Kaan Alsan <alsan@chromium.org>
2025-01-03 14:10:04 -0800

    Drive BrowserView loading animations from the compositor
    Bug: 40816290
    Change-Id: I82fbae3ebceb6b902a225ec99a35c086af15ddc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6121697
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401963}

--- chrome/browser/ui/views/frame/browser_view_interactive_uitest.cc
+++ chrome/browser/ui/views/frame/browser_view_interactive_uitest.cc
@@ -392,7 +392,7 @@ IN_PROC_BROWSER_TEST_F(BrowserViewTest,
   }
 
   EXPECT_TRUE(browser()->tab_strip_model()->TabsNeedLoadingUI());
-  EXPECT_FALSE(browser_view()->IsLoadingAnimationRunningForTesting());
+  EXPECT_FALSE(browser_view()->IsLoadingAnimationRunning());
 
   {
     base::RunLoop run_loop;
@@ -405,7 +405,7 @@ IN_PROC_BROWSER_TEST_F(BrowserViewTest,
   }
 
   EXPECT_TRUE(browser()->tab_strip_model()->TabsNeedLoadingUI());
-  EXPECT_TRUE(browser_view()->IsLoadingAnimationRunningForTesting());
+  EXPECT_TRUE(browser_view()->IsLoadingAnimationRunning());
 
   // Now block for the navigation to complete.
   navigation_watcher.Wait();
