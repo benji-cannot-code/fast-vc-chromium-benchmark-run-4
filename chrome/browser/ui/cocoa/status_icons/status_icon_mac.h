6249dfc3af8c9c88cfd7a9afb9c99a2c248df758
Alison Gale <agale@chromium.org>
2025-01-03 13:40:37 -0800

    Render glic context menu hotkey for all cases on MacOS
    When kGlicStatusIconOpenMenuWithSecondaryClick is false, the context
    menu is created before the hotkey is set. It is also possible that
    the hotkey will be updated after the context menu is created. For that
    reason we need a new method that will refresh the context menu.
    
    Bug: 386803748
    Change-Id: I111ed23bcd5b466102ec42eb89597266102a4997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6138876
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401953}

--- chrome/browser/ui/cocoa/status_icons/status_icon_mac.h
+++ chrome/browser/ui/cocoa/status_icons/status_icon_mac.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/gtest_prod_util.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/status_icons/desktop_notification_balloon.h"
 #include "chrome/browser/status_icons/status_icon.h"
 #include "chrome/browser/status_icons/status_tray.h"
@@ -19,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class NSStatusItem;
 @class StatusItemController;
 
-class StatusIconMac : public StatusIcon {
+class StatusIconMac : public StatusIcon, public StatusIconMenuModel::Observer {
  public:
   StatusIconMac();
 
@@ -37,6 +38,9 @@ class StatusIconMac : public StatusIcon {
                       const message_center::NotifierId& notifier_id) override;
   void SetOpenMenuWithSecondaryClick(
       bool open_menu_with_secondary_click) override;
+
+  // StatusIconMenuModel::Observer overrides:
+  void OnMenuStateChanged() override;
 
   bool HasStatusIconMenu();
 
@@ -85,9 +89,11 @@ class StatusIconMac : public StatusIcon {
   bool open_menu_with_secondary_click_ = false;
 
   // Stores the menu model so that it can be created later in
-  // CreateAndOpenMenu(). Only used when open_menu_with_secondary_click_ is
-  // true.
-  raw_ptr<ui::MenuModel> menu_model_ = nullptr;
+  // CreateAndOpenMenu() or so its observers can be cleaned up.
+  raw_ptr<StatusIconMenuModel> menu_model_ = nullptr;
+
+  base::ScopedObservation<StatusIconMenuModel, StatusIconMenuModel::Observer>
+      observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_COCOA_STATUS_ICONS_STATUS_ICON_MAC_H_
