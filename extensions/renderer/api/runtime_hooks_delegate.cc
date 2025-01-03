6f4763d333e5c99bbca34b19ccf4640cfec9da93
Oliver Dunk <oliverdunk@chromium.org>
2025-01-03 14:25:03 -0800

    [Extensions] Support query parameters with `use_dynamic_url`
    Adds support for query parameters when calling `chrome.runtime.getURL`
    for a web accessible resource with `use_dynamic_url` set to true.
    Previously, this would cause the URL to be generated without the dynamic
    ID and it would therefore fail to load in contexts where web accessible
    resources restrictions apply.
    
    Bug: 375973466
    Change-Id: Id9c5f7970cdc4ea76b460f88ecb55e983d516e47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967237
    Commit-Queue: Oliver Dunk <oliverdunk@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401969}

--- extensions/renderer/api/runtime_hooks_delegate.cc
+++ extensions/renderer/api/runtime_hooks_delegate.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/containers/span.h"
+#include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/v8_value_converter.h"
@@ -162,6 +163,15 @@ v8::LocalVector<v8::Value> MassageRequestUpdateCheckResults(
   return v8::LocalVector<v8::Value>(isolate, {status, details});
 }
 
+// Constructs a URL string from an `id` and `path`. `id` is directly used as
+// the host and can be a static or dynamic (GUID) identifier.
+GURL UrlFromPathAndId(const std::string& id, const std::string& path) {
+  std::string maybe_slash = !path.empty() && path[0] == '/' ? "" : "/";
+  std::string url = base::StrCat(
+      {kExtensionScheme, url::kStandardSchemeSeparator, id, maybe_slash, path});
+  return GURL(url);
+}
+
 }  // namespace
 
 RuntimeHooksDelegate::RuntimeHooksDelegate(
@@ -180,22 +190,25 @@ RequestResult RuntimeHooksDelegate::GetURL(
   v8::Isolate* isolate = script_context->isolate();
   std::string path = gin::V8ToString(isolate, arguments[0]);
   const auto* extension = script_context->extension();
-  bool use_dynamic_url = false;
-  if (base::FeatureList::IsEnabled(
-          extensions_features::kExtensionDynamicURLRedirection)) {
-    use_dynamic_url =
-        WebAccessibleResourcesInfo::ShouldUseDynamicUrl(extension, path);
-  }
-  std::string id = use_dynamic_url ? extension->guid() : extension->id();
 
-  RequestResult result(RequestResult::HANDLED);
-  std::string url = base::StringPrintf(
-      "chrome-extension://%s%s%s", id.c_str(),
-      !path.empty() && path[0] == '/' ? "" : "/", path.c_str());
+  GURL url = UrlFromPathAndId(extension->id(), path);
   // GURL considers any possible path valid. Since the argument is only appended
   // as part of the path, there should be no way this could conceivably fail.
-  DCHECK(GURL(url).is_valid());
-  result.return_value = gin::StringToV8(isolate, url);
+  DCHECK(url.is_valid());
+
+  if (base::FeatureList::IsEnabled(
+          extensions_features::kExtensionDynamicURLRedirection)) {
+    if (WebAccessibleResourcesInfo::ShouldUseDynamicUrl(extension,
+                                                        url.path())) {
+      GURL::Replacements replacements;
+      replacements.SetHostStr(extension->guid());
+      url = url.ReplaceComponents(replacements);
+    }
+  }
+
+  RequestResult result(RequestResult::HANDLED);
+  DCHECK(url.is_valid());
+  result.return_value = gin::StringToV8(isolate, url.spec());
   return result;
 }
 
