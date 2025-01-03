354913c64278ecb555c41d4118244495600f93be
Wan-Teh Chang <wtc@google.com>
2025-01-03 10:11:57 -0800

    Roll src/third_party/dav1d/libdav1d/ 2ba57aa53..b129d9f2c (3 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/2ba57aa53589..b129d9f2cb89
    
    $ git log 2ba57aa53..b129d9f2c --date=short --no-merges --format='%ad %ae %s'
    2024-12-24 martin mc: Reduce stack use in {put,prep}_scaled_{bilin,8tap}
    2024-12-26 brad riscv: Fix building on non-Linux OS's
    2024-12-27 jamrial obu: don't print warnings for Metadata OBUs of types "Unregistered user private"
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=eugene@chromium.org
    
    Change-Id: Id470edef9074987d4ca9328b574caa4401205319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6137593
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401873}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.0-50-g2ba57aa"
+#define DAV1D_VERSION "1.5.0-53-gb129d9f"
