6983a2ef212cccd3ffdda83c43699541a4768107
Karol Sygiet <sygiet@google.com>
2025-01-03 06:42:05 -0800

    [Japan] Change row icon in alternative name update bubble
    Before this change, the alternative name was being displayed as part
    of the envelope address. After this CL, it will be in it's own row with
    the proper icon.
    
    Bug: 359768803
    Change-Id: Ia99f0469c7eec9b29688280586e8172f28d16f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6119952
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1401800}

--- components/autofill/core/browser/ui/addresses/autofill_address_util_unittest.cc
+++ components/autofill/core/browser/ui/addresses/autofill_address_util_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/uuid.h"
+#include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/geo/address_i18n.h"
 #include "components/autofill/core/browser/test_utils/autofill_test_utils.h"
 #include "components/autofill/core/common/autofill_features.h"
@@ -207,6 +208,21 @@ TEST_F(AddressFormattingTest,
   EXPECT_THAT(GetProfileDifferenceForUi(profile1, profile2, "en-US"),
               ElementsAre(ProfileValueDifference{NAME_FULL, u"John H. Doe",
                                                  u"John Doe"}));
+}
+
+TEST_F(AddressFormattingTest,
+       GetEnvelopeStyleAddressHasDiffereceInUiWhenAlternativeFullnameDiffers) {
+  base::test::ScopedFeatureList feature_list{
+      features::kAutofillSupportPhoneticNameForJP};
+  AutofillProfile profile1 = test::GetFullProfile();
+  profile1.SetInfo(ALTERNATIVE_FULL_NAME, u"", "ja-JP");
+
+  AutofillProfile profile2 = profile1;
+  profile2.SetInfo(ALTERNATIVE_FULL_NAME, u"やまもと あおい", "ja-JP");
+
+  EXPECT_THAT(GetProfileDifferenceForUi(profile1, profile2, "ja-JP"),
+              ElementsAre(ProfileValueDifference{ALTERNATIVE_FULL_NAME, u"",
+                                                 u"やまもと あおい"}));
 }
 
 TEST_F(AddressFormattingTest,
