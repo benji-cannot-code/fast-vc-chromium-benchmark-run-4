5397f732b10c427f0bed700830083d0a0a97b978
Vignesh Shenvi <vshenvi@google.com>
2025-01-02 16:04:42 -0800

    Close unmanaged Boca SWA instances
    This change configures the OnTask window tracker to observe the browser
    list outside active OnTask sessions to pre-emptively close unmanaged
    Boca instances, especially those that can be spawned through
    non-conventional flows.
    
    Bug: b:377997889
    Change-Id: Ibb773020c80be10dc4db19c6d83c9f2e7f34984d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6041246
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Reviewed-by: Luke Cao <caott@google.com>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401638}

--- chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker_unittest.cc
+++ chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker_unittest.cc
@@ -94,9 +94,9 @@ class FakeOnTaskNotificationsManagerDelegate
 // browser test, right now it's very hard to tell when does tab strip update
 // happens.
 class OnTaskLockedSessionWindowTrackerTest : public BrowserWithTestWindowTest {
- public:
+ protected:
   void SetUp() override {
-    // TODO(crbug.com/36741761):Change this to reguar mock when remigrated to
+    // TODO(crbug.com/36741761): Change this to regular mock when remigrated to
     // browser test.
     ON_CALL(boca_window_observer_, OnActiveTabChanged(_))
         .WillByDefault(Return());
@@ -126,7 +126,7 @@ class OnTaskLockedSessionWindowTrackerTest : public BrowserWithTestWindowTest {
           auto on_task_blocklist = std::make_unique<OnTaskBlocklist>(
               std::move(url_blocklist_manager));
           return std::make_unique<LockedSessionWindowTracker>(
-              std::move(on_task_blocklist));
+              std::move(on_task_blocklist), context);
         }));
     ASSERT_TRUE(base::test::RunUntil([&]() {
       return (LockedSessionWindowTrackerFactory::GetForBrowserContext(
@@ -142,6 +142,7 @@ class OnTaskLockedSessionWindowTrackerTest : public BrowserWithTestWindowTest {
             ash::boca::OnTaskNotificationsManager::CreateForTest(
                 std::move(fake_notifications_delegate)));
   }
+
   void TearDown() override {
     task_environment()->RunUntilIdle();
 
@@ -677,6 +678,11 @@ TEST_F(OnTaskLockedSessionWindowTrackerTest,
 
 TEST_F(OnTaskLockedSessionWindowTrackerTest,
        NewBrowserWindowsDontOpenDuringLockedFullscreen) {
+  // Set up relevant features for browser instance observation.
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatures(
+      /*enabled_features=*/{ash::features::kBoca, ash::features::kBocaConsumer},
+      /*disabled_features=*/{});
   CreateWindowTrackerServiceForTesting();
   auto* const window_tracker =
       LockedSessionWindowTrackerFactory::GetForBrowserContext(profile());
@@ -718,6 +724,11 @@ TEST_F(OnTaskLockedSessionWindowTrackerTest,
 }
 
 TEST_F(OnTaskLockedSessionWindowTrackerTest, NewBrowserPopupIsRegistered) {
+  // Set up relevant features for browser instance observation.
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatures(
+      /*enabled_features=*/{ash::features::kBoca, ash::features::kBocaConsumer},
+      /*disabled_features=*/{});
   CreateWindowTrackerServiceForTesting();
   auto* const window_tracker =
       LockedSessionWindowTrackerFactory::GetForBrowserContext(profile());
@@ -733,6 +744,11 @@ TEST_F(OnTaskLockedSessionWindowTrackerTest, NewBrowserPopupIsRegistered) {
 }
 
 TEST_F(OnTaskLockedSessionWindowTrackerTest, BrowserClose) {
+  // Set up relevant features for browser instance observation.
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatures(
+      /*enabled_features=*/{ash::features::kBoca, ash::features::kBocaConsumer},
+      /*disabled_features=*/{});
   CreateWindowTrackerServiceForTesting();
   auto* const window_tracker =
       LockedSessionWindowTrackerFactory::GetForBrowserContext(profile());
--- chromeos/ash/components/boca/on_task/on_task_session_manager_unittest.cc
+++ chromeos/ash/components/boca/on_task/on_task_session_manager_unittest.cc
@@ -84,7 +84,7 @@ class OnTaskSystemWebAppManagerMock : public OnTaskSystemWebAppManager {
               (override));
   MOCK_METHOD(void,
               PrepareSystemWebAppWindowForOnTask,
-              (SessionID window_id),
+              (SessionID window_id, bool close_bundle_content),
               (override));
   MOCK_METHOD(SessionID, GetActiveTabID, (), (override));
   MOCK_METHOD(void, SwitchToTab, (SessionID tab_id), (override));
@@ -219,7 +219,8 @@ TEST_F(OnTaskSessionManagerTest,
       session_manager_->active_tab_tracker(), session_manager_.get()};
   Sequence s;
   EXPECT_CALL(*system_web_app_manager_ptr_,
-              PrepareSystemWebAppWindowForOnTask(kWindowId))
+              PrepareSystemWebAppWindowForOnTask(kWindowId,
+                                                 /*close_bundle_content=*/true))
       .Times(1)
       .InSequence(s);
   EXPECT_CALL(
@@ -688,7 +689,7 @@ TEST_F(OnTaskSessionManagerTest, OnAppReloadWithNoActiveWindow) {
   EXPECT_CALL(*system_web_app_manager_ptr_, GetActiveSystemWebAppWindowID())
       .WillOnce(Return(SessionID::InvalidValue()));
   EXPECT_CALL(*system_web_app_manager_ptr_,
-              PrepareSystemWebAppWindowForOnTask(_))
+              PrepareSystemWebAppWindowForOnTask(_, _))
       .Times(0);
   session_manager_->OnAppReloaded();
 }
@@ -720,8 +721,8 @@ TEST_F(OnTaskSessionManagerTest, RestoreTabsOnAppReload) {
       SetWindowTrackerForSystemWebAppWindow(kWindowId, kWindowObservers))
       .Times(AtLeast(1));
   EXPECT_CALL(*system_web_app_manager_ptr_,
-              PrepareSystemWebAppWindowForOnTask(kWindowId))
-      .Times(1)
+              PrepareSystemWebAppWindowForOnTask(kWindowId, _))
+      .Times(AtLeast(1))
       .InSequence(s);
   EXPECT_CALL(*system_web_app_manager_ptr_,
               CreateBackgroundTabWithUrl(
@@ -769,8 +770,8 @@ TEST_F(OnTaskSessionManagerTest, LockWindowOnAppReload) {
       SetWindowTrackerForSystemWebAppWindow(kWindowId, kWindowObservers))
       .Times(AtLeast(1));
   EXPECT_CALL(*system_web_app_manager_ptr_,
-              PrepareSystemWebAppWindowForOnTask(kWindowId))
-      .Times(1)
+              PrepareSystemWebAppWindowForOnTask(kWindowId, _))
+      .Times(AtLeast(1))
       .InSequence(s);
   EXPECT_CALL(*system_web_app_manager_ptr_, SetPinStateForSystemWebAppWindow(
                                                 /*pinned=*/true, kWindowId))
