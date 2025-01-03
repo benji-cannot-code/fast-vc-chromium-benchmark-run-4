5713ba97ddc043aa1953a81cebe170a67abbac6f
Chris Lu <thegreenfrog@chromium.org>
2025-01-03 14:42:38 -0800

    [ios] Never add MVTs if config has no items
    This check is done in -setMostVisitedTilesConfig:, it should match
    in -viewDidLoad, so that an empty MVT module is not inserted.
    
    Fixed: 387173584
    Change-Id: If916034fa52dc75ac5d90b9f89a5ac763e6ff2db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143216
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1401975}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -87,7 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                      : -kBottomMagicStackPadding)]
   ]];
 
-  if (_mostVisitedTileConfig) {
+  if (_mostVisitedTileConfig.mostVisitedItems.count > 0) {
     CHECK(!_mostVisitedTileConfig.inMagicStack);
     [self createAndInsertMostVisitedModule];
   }
