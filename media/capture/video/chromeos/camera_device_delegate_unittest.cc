02d3579acc354aabcc6c5311d1f1f09f398abe6f
vikas soni <vikassoni@chromium.org>
2025-01-03 00:15:14 -0800

    Reland "[MappableSI] Convert CameraBufferFactory to use MappableSI."
    This is reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/5758193
    after bug fix via
    https://chromium-review.googlesource.com/c/chromium/src/+/5901567 and
    https://chromium-review.googlesource.com/c/chromium/src/+/6072273.
    
    Refactor CameraBufferFactory::ResolveStreamBufferFormat() to create
    MappableSI instead of GpuMemoryBuffer.
    
    This is ChromeOS only change.
    
    Follow up CLs will convert rest of CrOs media capture code
    to use MappableSI.
    
    Bug: 40264379, 361010222
    Change-Id: Ifed3cbb6a1e26cd1ad1d219b777f86ea8aed47b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020760
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401714}

--- media/capture/video/chromeos/camera_device_delegate_unittest.cc
+++ media/capture/video/chromeos/camera_device_delegate_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "components/viz/test/test_context_provider.h"
 #include "media/capture/video/chromeos/camera_buffer_factory.h"
 #include "media/capture/video/chromeos/camera_device_context.h"
 #include "media/capture/video/chromeos/camera_hal_delegate.h"
@@ -170,6 +171,9 @@ class CameraDeviceDelegateTest : public ::testing::Test {
   void SetUp() override {
     VideoCaptureDeviceFactoryChromeOS::SetGpuBufferManager(
         &mock_gpu_memory_buffer_manager_);
+    test_sii_ = base::MakeRefCounted<gpu::TestSharedImageInterface>();
+    test_sii_->UseTestGMBInSharedImageCreationWithBufferUsage();
+    VideoCaptureDeviceFactoryChromeOS::SetSharedImageInterface(test_sii_);
     camera_hal_delegate_ = std::make_unique<CameraHalDelegate>(ui_task_runner_);
     if (!camera_hal_delegate_->Init()) {
       LOG(ERROR) << "Failed to initialize CameraHalDelegate";
@@ -185,6 +189,7 @@ class CameraDeviceDelegateTest : public ::testing::Test {
 
   void TearDown() override {
     VideoCaptureDeviceFactoryChromeOS::SetGpuBufferManager(nullptr);
+    VideoCaptureDeviceFactoryChromeOS::SetSharedImageInterface(nullptr);
     camera_device_delegate_.reset();
     camera_hal_delegate_.reset();
     task_environment_.RunUntilIdle();
@@ -428,22 +433,34 @@ class CameraDeviceDelegateTest : public ::testing::Test {
         .WillOnce(
             Invoke(this, &CameraDeviceDelegateTest::ConfigureFakeStreams));
 
-    EXPECT_CALL(mock_gpu_memory_buffer_manager_,
-                CreateGpuMemoryBuffer(
-                    _, gfx::BufferFormat::YUV_420_BIPLANAR,
-                    gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE,
-                    gpu::kNullSurfaceHandle, nullptr))
-        .Times(1)
-        .WillOnce(Invoke(&unittest_internal::MockGpuMemoryBufferManager::
-                             CreateFakeGpuMemoryBuffer));
-    EXPECT_CALL(
-        mock_gpu_memory_buffer_manager_,
-        CreateGpuMemoryBuffer(_, gfx::BufferFormat::R_8,
-                              gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE,
-                              gpu::kNullSurfaceHandle, nullptr))
-        .Times(AtMost(1))
-        .WillOnce(Invoke(&unittest_internal::MockGpuMemoryBufferManager::
-                             CreateFakeGpuMemoryBuffer));
+    // CameraBufferFactory::ResolveStreamBufferFormat() is now using
+    // ::CreateSharedImage() instead of ::CreateGpuMemoryBuffer(). Hence adding
+    // some expectations here.
+    EXPECT_CALL(*test_sii_,
+                DoCreateSharedImage(
+                    _, viz::MultiPlaneFormat::kNV12, gpu::kNullSurfaceHandle,
+                    gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE))
+        .Times(1);
+    EXPECT_CALL(*test_sii_,
+                DoCreateSharedImage(
+                    _, viz::SinglePlaneFormat::kR_8, gpu::kNullSurfaceHandle,
+                    gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE))
+        .Times(AtMost(1));
+    EXPECT_CALL(*test_sii_,
+                DoCreateSharedImage(
+                    gfx::Size(kJpegMaxBufferSize, 1),
+                    viz::SinglePlaneFormat::kR_8, gpu::kNullSurfaceHandle,
+                    gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE))
+        .Times(AtMost(1));
+
+    // Note that ::CreateGpuMemoryBuffer() is currently being used by
+    // StreamBufferManager.
+    ON_CALL(mock_gpu_memory_buffer_manager_,
+            CreateGpuMemoryBuffer(_, gfx::BufferFormat::R_8,
+                                  gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE,
+                                  gpu::kNullSurfaceHandle, nullptr))
+        .WillByDefault(Invoke(&unittest_internal::MockGpuMemoryBufferManager::
+                                  CreateFakeGpuMemoryBuffer));
     EXPECT_CALL(mock_gpu_memory_buffer_manager_,
                 CreateGpuMemoryBuffer(
                     gfx::Size(kDefaultWidth, kDefaultHeight),
@@ -451,14 +468,6 @@ class CameraDeviceDelegateTest : public ::testing::Test {
                     gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE,
                     gpu::kNullSurfaceHandle, nullptr))
         .Times(1)
-        .WillOnce(Invoke(&unittest_internal::MockGpuMemoryBufferManager::
-                             CreateFakeGpuMemoryBuffer));
-    EXPECT_CALL(mock_gpu_memory_buffer_manager_,
-                CreateGpuMemoryBuffer(
-                    gfx::Size(kJpegMaxBufferSize, 1), gfx::BufferFormat::R_8,
-                    gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE,
-                    gpu::kNullSurfaceHandle, nullptr))
-        .Times(AtMost(1))
         .WillOnce(Invoke(&unittest_internal::MockGpuMemoryBufferManager::
                              CreateFakeGpuMemoryBuffer));
   }
@@ -555,6 +564,7 @@ class CameraDeviceDelegateTest : public ::testing::Test {
   testing::StrictMock<unittest_internal::MockCameraModule> mock_camera_module_;
   testing::NiceMock<unittest_internal::MockVendorTagOps> mock_vendor_tag_ops_;
   unittest_internal::MockGpuMemoryBufferManager mock_gpu_memory_buffer_manager_;
+  scoped_refptr<gpu::TestSharedImageInterface> test_sii_;
 
   testing::StrictMock<MockCameraDevice> mock_camera_device_;
   mojo::Receiver<cros::mojom::Camera3DeviceOps> mock_camera_device_receiver_;
