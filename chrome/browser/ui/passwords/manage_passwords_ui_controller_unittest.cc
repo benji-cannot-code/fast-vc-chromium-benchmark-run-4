f5bf851e5bc3d17454d2a4d4fbc715c6b16f88bd
Allen Bauer <kylixrd@chromium.org>
2025-01-06 08:39:11 -0800

    Reland "Convert Credential leak dialog to use CLIENT_OWNS_WIDGET ownership model."
    This is a reland of commit 7d3d61d6a3985c1dc89824c3d8ca2ff52c4e4cd4
    
    Several other general fixes were put into place that make sure this CL works. The DialogDelegate handles Widget destruction better when accept/cancal callbacks are invoked.  There is better tracking of the Widget lifetime to know when to re-enable input events on the tab. The TabDialogManager now correctly positions the tab modal dialog just below the Omnibox.
    
    Ancestor CLs are here:
    https://chromium-review.googlesource.com/c/chromium/src/+/6094046
    https://chromium-review.googlesource.com/c/chromium/src/+/6105333
    
    Original change's description:
    > Convert Credential leak dialog to use CLIENT_OWNS_WIDGET ownership model.
    >
    > Change-Id: I1edf89d3c6cee2485a2e7097097a439533c6d10d
    > Bug: 338254375
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056146
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1395380}
    
    Bug: 338254375
    Change-Id: Ic5c8b519199e28599c7ba81b0c17b0bbb3e455f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098309
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402400}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
@@ -85,6 +85,9 @@ using ::testing::Return;
 using ::testing::ReturnRef;
 using ::testing::SaveArg;
 
+namespace views {
+class Widget;
+}
 namespace {
 
 MATCHER_P3(MatchesLoginAndURL,
@@ -117,7 +120,7 @@ class CredentialManagementDialogPromptMock : public AccountChooserPrompt,
 class PasswordLeakDialogMock : public CredentialLeakPrompt {
  public:
   MOCK_METHOD(void, ShowCredentialLeakPrompt, (), (override));
-  MOCK_METHOD(void, ControllerGone, (), (override));
+  MOCK_METHOD(views::Widget*, GetWidgetForTesting, (), (override));
 };
 
 class TestManagePasswordsIconView : public ManagePasswordsIconView {
@@ -174,7 +177,7 @@ class TestManagePasswordsUIController : public ManagePasswordsUIController {
               CreateAutoSigninPrompt,
               (CredentialManagerDialogController*),
               (override));
-  MOCK_METHOD(CredentialLeakPrompt*,
+  MOCK_METHOD(std::unique_ptr<CredentialLeakPrompt>,
               CreateCredentialLeakPrompt,
               (CredentialLeakDialogController*),
               (override));
@@ -1657,11 +1660,12 @@ TEST_F(ManagePasswordsUIControllerTest, SaveBubbleAfterLeakCheck) {
   EXPECT_TRUE(controller()->opened_automatic_bubble());
 
   // Leak detection dialog hides the bubble.
-  PasswordLeakDialogMock dialog_prompt;
+  auto dialog_prompt = std::make_unique<PasswordLeakDialogMock>();
   CredentialLeakDialogController* dialog_controller = nullptr;
+  EXPECT_CALL(*dialog_prompt, ShowCredentialLeakPrompt);
   EXPECT_CALL(*controller(), CreateCredentialLeakPrompt)
-      .WillOnce(DoAll(SaveArg<0>(&dialog_controller), Return(&dialog_prompt)));
-  EXPECT_CALL(dialog_prompt, ShowCredentialLeakPrompt);
+      .WillOnce(DoAll(SaveArg<0>(&dialog_controller),
+                      Return(std::move(dialog_prompt))));
   controller()->OnCredentialLeak(password_manager::LeakedPasswordDetails(
       password_manager::CreateLeakType(password_manager::IsSaved(false),
                                        password_manager::IsReused(false),
@@ -1679,7 +1683,6 @@ TEST_F(ManagePasswordsUIControllerTest, SaveBubbleAfterLeakCheck) {
           Return(base::span<const password_manager::InteractionsStats>()));
 
   // Close the dialog.
-  EXPECT_CALL(dialog_prompt, ControllerGone);
   EXPECT_CALL(*controller(), OnUpdateBubbleAndIconVisibility());
   dialog_controller->OnAcceptDialog();
 
@@ -1699,11 +1702,12 @@ TEST_F(ManagePasswordsUIControllerTest,
   EXPECT_FALSE(controller()->opened_automatic_bubble());
 
   // Leak detection dialog hides the bubble.
-  PasswordLeakDialogMock dialog_prompt;
+  auto dialog_prompt = std::make_unique<PasswordLeakDialogMock>();
   CredentialLeakDialogController* dialog_controller = nullptr;
+  EXPECT_CALL(*dialog_prompt, ShowCredentialLeakPrompt);
   EXPECT_CALL(*controller(), CreateCredentialLeakPrompt)
-      .WillOnce(DoAll(SaveArg<0>(&dialog_controller), Return(&dialog_prompt)));
-  EXPECT_CALL(dialog_prompt, ShowCredentialLeakPrompt);
+      .WillOnce(DoAll(SaveArg<0>(&dialog_controller),
+                      Return(std::move(dialog_prompt))));
   controller()->OnCredentialLeak(password_manager::LeakedPasswordDetails(
       password_manager::CreateLeakType(password_manager::IsSaved(false),
                                        password_manager::IsReused(false),
@@ -1718,7 +1722,6 @@ TEST_F(ManagePasswordsUIControllerTest,
   EXPECT_CALL(*form_manager_ptr, IsBlocklisted()).WillOnce(Return(true));
 
   // Close the dialog.
-  EXPECT_CALL(dialog_prompt, ControllerGone);
   EXPECT_CALL(*controller(), OnUpdateBubbleAndIconVisibility());
   dialog_controller->OnAcceptDialog();
 
@@ -1736,11 +1739,13 @@ TEST_F(ManagePasswordsUIControllerTest, UpdateBubbleAfterLeakCheck) {
   EXPECT_TRUE(controller()->opened_automatic_bubble());
 
   // Leak detection dialog hides the bubble.
-  PasswordLeakDialogMock dialog_prompt;
+  auto dialog_prompt = std::make_unique<PasswordLeakDialogMock>();
+  auto* dialog_prompt_ptr = dialog_prompt.get();
   CredentialLeakDialogController* dialog_controller = nullptr;
   EXPECT_CALL(*controller(), CreateCredentialLeakPrompt)
-      .WillOnce(DoAll(SaveArg<0>(&dialog_controller), Return(&dialog_prompt)));
-  EXPECT_CALL(dialog_prompt, ShowCredentialLeakPrompt);
+      .WillOnce(DoAll(SaveArg<0>(&dialog_controller),
+                      Return(std::move(dialog_prompt))));
+  EXPECT_CALL(*dialog_prompt_ptr, ShowCredentialLeakPrompt);
   controller()->OnCredentialLeak(password_manager::LeakedPasswordDetails(
       password_manager::CreateLeakType(password_manager::IsSaved(true),
                                        password_manager::IsReused(false),
@@ -1751,7 +1756,6 @@ TEST_F(ManagePasswordsUIControllerTest, UpdateBubbleAfterLeakCheck) {
   EXPECT_FALSE(controller()->opened_automatic_bubble());
 
   // Close the dialog.
-  EXPECT_CALL(dialog_prompt, ControllerGone);
   EXPECT_CALL(*controller(), OnUpdateBubbleAndIconVisibility());
   dialog_controller->OnAcceptDialog();
 
--- chrome/browser/ui/views/passwords/credential_leak_dialog_view.h
+++ chrome/browser/ui/views/passwords/credential_leak_dialog_view.h
@@ -18,8 +18,7 @@ class WebContents;
 
 class CredentialLeakDialogController;
 
-class CredentialLeakDialogView : public views::DialogDelegateView,
-                                 public CredentialLeakPrompt {
+class CredentialLeakDialogView : public views::DialogDelegateView {
   METADATA_HEADER(CredentialLeakDialogView, views::DialogDelegateView)
 
  public:
@@ -29,17 +28,16 @@ class CredentialLeakDialogView : public views::DialogDelegateView,
   CredentialLeakDialogView& operator=(const CredentialLeakDialogView&) = delete;
   ~CredentialLeakDialogView() override;
 
-  // CredentialsLeakedPrompt:
-  void ShowCredentialLeakPrompt() override;
-  void ControllerGone() override;
+  // Sets up the child views.
+  void InitWindow();
+
+  CredentialLeakDialogController* controller() { return controller_; }
+  content::WebContents* web_contents() { return web_contents_; }
 
  private:
   // views::DialogDelegateView:
   void AddedToWidget() override;
   std::u16string GetWindowTitle() const override;
-
-  // Sets up the child views.
-  void InitWindow();
 
   // A weak pointer to the controller.
   raw_ptr<CredentialLeakDialogController> controller_ = nullptr;
