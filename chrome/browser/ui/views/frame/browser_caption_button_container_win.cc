6b77e88c622ba9984cffb208d945ccab7ef8bed0
Peter Kasting <pkasting@chromium.org>
2025-01-06 10:14:24 -0800

    Avoid including <windows.h> in scoped_gdi_object.h.
    Instead, add a .cc file and define what's necessary there. This avoids
    transient includes of windows.h, which is very heavyweight.
    
    Bug: 364987728
    Change-Id: Ieab791e48351c1469b5ae39567ea44a4c576773a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6136685
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402483}

--- chrome/browser/ui/views/frame/browser_caption_button_container_win.cc
+++ chrome/browser/ui/views/frame/browser_caption_button_container_win.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/frame/browser_caption_button_container_win.h"
 
+#include <windows.h>
+
 #include <memory>
 
 #include "chrome/browser/ui/frame/window_frame_util.h"
