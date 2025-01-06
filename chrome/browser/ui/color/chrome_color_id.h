f5f61fc5f20e5e18ce8ee3a3d2c492684fb37aac
Matt Jones <mdjones@chromium.org>
2025-01-06 10:35:13 -0800

    [compare] Add text preview and favicons to citations
    This patch adds page title, favicon, and text snippet  to the tooltip
    that shows when hovering over a citation. The styles of the citation
    have also been updated to resemble a card rather than a typical
    tooltip. Example: https://screenshot.googleplex.com/A8ugZoTAZUMX6Nx
    
    A few supporting changes have been made to the handler, namely adding
    a utility to handle conversion of UrlInfo.
    
    Bug: 379300527
    Change-Id: Ife5dc1922973b957a4ad131aac33f83f457541f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092105
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402499}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -425,6 +425,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   /* Product Specifications colors */ \
   E_CPONLY(kColorProductSpecificationsButtonBackground) \
   E_CPONLY(kColorProductSpecificationsCitationBackground) \
+  E_CPONLY(kColorProductSpecificationsCitationPopupBackground) \
+  E_CPONLY(kColorProductSpecificationsCitationPopupText) \
+  E_CPONLY(kColorProductSpecificationsCitationPopupTitle) \
   E_CPONLY(kColorProductSpecificationsComparisonTableListBackground) \
   E_CPONLY(kColorProductSpecificationsDetailChipBackground) \
   E_CPONLY(kColorProductSpecificationsDisclosureBackground) \
