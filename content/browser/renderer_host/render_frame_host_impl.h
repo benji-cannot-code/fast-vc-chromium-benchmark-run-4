981e12acb0ff3daf632355a53a383befaa94aefe
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2025-01-06 07:48:38 -0800

    [views-ax] Implement BrowserAccessibility::IsWebContent
    BrowserAccessibility::IsWebContent used to always return true. This had
    to change since we'll soon have a BrowserAccessibility tree of views.
    
    Bug: 325137417
    Change-Id: Ic7592a608725e01a32301a9f28c5febfcab2dac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6135342
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1402373}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -733,6 +733,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   bool AccessibilityIsRootFrame() const override;
   bool ShouldSuppressAXLoadComplete() override;
   WebContentsAccessibility* AccessibilityGetWebContentsAccessibility() override;
+  bool AccessibilityIsWebContentSource() override;
 
   // ui::AXNodeIdDelegate:
   ui::AXPlatformNodeId GetOrCreateAXNodeUniqueId(
--- ui/accessibility/platform/ax_platform_tree_manager_delegate.h
+++ ui/accessibility/platform/ax_platform_tree_manager_delegate.h
@@ -134,6 +134,10 @@ class COMPONENT_EXPORT(AX_PLATFORM) AXPlatformTreeManagerDelegate {
   virtual content::WebContentsAccessibility*
   AccessibilityGetWebContentsAccessibility() = 0;
 
+  // Returns true if the current accessibility tree is for web content, false if
+  // it's for Views.
+  virtual bool AccessibilityIsWebContentSource() = 0;
+
  protected:
   AXPlatformTreeManagerDelegate() = default;
 };
