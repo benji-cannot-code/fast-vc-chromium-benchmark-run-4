6f066cc4f75c21c61a38d0d6546cbf13df57538c
Martin Duke <martinduke@google.com>
2025-01-06 15:33:13 -0800

    Enable kReportEcn by default.
    Experiment report at go/ecn-measurement
    
    R=martinduke@google.com
    
    Change-Id: I1ef230050b4ee7a43aa6d1f4fc45b8e1ace4e903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6067995
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Commit-Queue: Martin Duke <martinduke@google.com>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402698}

--- net/base/features.cc
+++ net/base/features.cc
@@ -527,7 +527,7 @@ BASE_FEATURE(kSpdyHeadersToHttpResponseUseBuilder,
              "SpdyHeadersToHttpResponseUseBuilder",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kReportEcn, "ReportEcn", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kReportEcn, "ReportEcn", base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseNewAlpsCodepointHttp2,
              "UseNewAlpsCodepointHttp2",
