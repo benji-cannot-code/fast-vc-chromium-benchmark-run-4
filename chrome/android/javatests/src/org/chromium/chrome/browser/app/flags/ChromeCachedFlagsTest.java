47f5c639d188a970026fd211a4bc59b618f8d471
Nafis Abedin <nafisabedin@google.com>
2025-01-06 12:01:11 -0800

    [Binder] Add a field trial for TraceBinderIpc
    Adding a field trial for Binder IPC Tracing on Android so that the
    value matches defaultValueInTests.
    
    Bug: 380113169
    Change-Id: I852e6774890f2fe6506035d7045a39bc538feaf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6149988
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402567}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
@@ -57,7 +57,6 @@ public class ChromeCachedFlagsTest {
                     ChromeFeatureList.sPostGetMyMemoryStateToBackground,
                     ChromeFeatureList.sPrefetchBrowserInitiatedTriggers,
                     ChromeFeatureList.sSafetyHubMagicStack,
-                    ChromeFeatureList.sTraceBinderIpc,
                     OmniboxFeatures.sAndroidHubSearch);
 
     /**
