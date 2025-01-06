8e3cb7ea73ce1668722f1795c510b7345576cf22
Shu Yang <shuyng@google.com>
2025-01-06 14:26:00 -0800

    Add integration test for PDF page
    Bug: 355049488
    Change-Id: Iee0661dac01c205fe187b7514d98f9d70e3f6f17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6137595
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402658}

--- chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -60,7 +61,7 @@ public class PdfCoordinator {
     /** Uri of the pdf document. Generated when the pdf is ready to load. */
     private Uri mUri;
 
-    private ChromePdfViewerFragment mChromePdfViewerFragment;
+    @VisibleForTesting ChromePdfViewerFragment mChromePdfViewerFragment;
 
     private int mFindInPageCount;
 
