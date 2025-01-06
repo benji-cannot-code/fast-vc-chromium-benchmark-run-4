87765cfffebd33c9314a451a767fb78366a3e909
Erica Kovac <ekovac@google.com>
2025-01-06 08:35:08 -0800

    [FedCM] Create Lightweight FedCM flag and feature.
    Bug: 365798407
    Change-Id: Icb5f94e4db8c4101ac7cff8acf9a118374d8474e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109952
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Erica Kovac <ekovac@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402398}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1104,6 +1104,9 @@ extern const char kFedCmDelegationDescription[];
 extern const char kFedCmIdPRegistrationName[];
 extern const char kFedCmIdPRegistrationDescription[];
 
+extern const char kFedCmLightweightModeName[];
+extern const char kFedCmLightweightModeDescription[];
+
 extern const char kFedCmMetricsEndpointName[];
 extern const char kFedCmMetricsEndpointDescription[];
 
--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -221,6 +221,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
           {wf::EnableFedCmIdpSigninStatus,
            raw_ref(features::kFedCmIdpSigninStatusEnabled),
            kSetOnlyIfOverridden},
+          {wf::EnableFedCmLightweightMode,
+           raw_ref(features::kFedCmLightweightMode), kDefault},
           {wf::EnableGamepadMultitouch,
            raw_ref(features::kEnableGamepadMultitouch)},
           {wf::EnableSharedStorageAPI,
