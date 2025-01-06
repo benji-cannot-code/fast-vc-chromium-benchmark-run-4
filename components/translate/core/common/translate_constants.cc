2137c9a4a6b14db2cc343f773c2dd30988fcbb94
Fergal Daly <fergal@chromium.org>
2025-01-05 23:52:23 -0800

    Move kUnknownLanguageCode into language_detection.
    This leaves an alias in translate. https://crrev.com/c/6048141 will be split by owners to migrate all users and eventually delete the alias.
    
    Bug: 380786760
    Change-Id: Id18b38596e97eab952ea87d912b6e7b265703412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6113295
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402282}

--- components/translate/core/common/translate_constants.cc
+++ components/translate/core/common/translate_constants.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace translate {
 
-const char* const kUnknownLanguageCode = "und";
-
 const int kDesktopPartialTranslateTextSelectionMaxCharacters = 500;
 
 const int kDesktopPartialTranslateBubbleShowDelayMs = 500;
