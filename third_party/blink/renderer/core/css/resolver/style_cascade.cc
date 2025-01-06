48df72601ab2b276e7148db297979e7b3a139d2b
Victor Miura <vmiura@chromium.org>
2025-01-06 08:29:45 -0800

    Unify naming around ComputedStyle HasEnv* fields.
    Also take advantage of HasEnvSafeAreaInsetBottom field to optimize
    ApplyIsBottomRelativeToSafeAreaInset implementation.
    
    Bug: 385215539
    Change-Id: Ib8f98c5429986a11c63f6d3da0e316b9ff6134d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6144925
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Victor Miura <vmiura@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402394}

--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -1595,7 +1595,7 @@ bool StyleCascade::ResolveEnvInto(CSSParserTokenStream& stream,
   AtomicString variable_name = ConsumeVariableName(stream);
 
   if (variable_name == "safe-area-inset-bottom") {
-    state_.StyleBuilder().SetReferencesSafeAreaInsetBottom(true);
+    state_.StyleBuilder().SetHasEnvSafeAreaInsetBottom();
     state_.GetDocument()
         .GetStyleEngine()
         .SetNeedsToUpdateComplexSafeAreaConstraints();
@@ -1820,7 +1820,8 @@ void StyleCascade::ApplyUnresolvedEnv() {
 }
 
 void StyleCascade::ApplyIsBottomRelativeToSafeAreaInset() {
-  if (!map_.NativeBitset().Has(CSSPropertyID::kBottom)) {
+  if (!state_.StyleBuilder().HasEnvSafeAreaInsetBottom() ||
+      !map_.NativeBitset().Has(CSSPropertyID::kBottom)) {
     return;
   }
 
