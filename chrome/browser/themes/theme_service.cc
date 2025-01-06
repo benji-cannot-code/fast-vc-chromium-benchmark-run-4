d5816c6f37a96a291d97b2b88ea5a3222daaff33
Ankush Singh <ankushkush@google.com>
2025-01-06 09:29:08 -0800

    [themes] Fix ThemePrefsMigrationShouldReadPrefsTest test suite
    This CL fixes the following bugs in
    ThemePrefsMigrationShouldReadPrefsTest test suite:
    
    1. Two ThemeService and ThemeSyncableService instances existed at the
    same time. This is because TestingProfile also creates an instance of
    ThemeService, which further creates an instance of ThemeSyncableService.
    2. Feature list is initialized after the creation of ThemeService. This
    makes ThemeService listen to the old syncing prefs, thus producing
    unexpected behaviors.
    
    Bug: 356148174
    Change-Id: Id12e5278ace78e8803357683b19aa19a013e3c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112211
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402441}

--- chrome/browser/themes/theme_service.cc
+++ chrome/browser/themes/theme_service.cc
@@ -1033,3 +1033,7 @@ bool ThemeService::DisableExtension(const std::string& extension_id) {
   }
   return false;
 }
+
+void ThemeService::ResetThemeSyncableServiceForTest() {
+  theme_syncable_service_.reset();
+}
