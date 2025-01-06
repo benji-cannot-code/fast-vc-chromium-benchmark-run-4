4fab329bbb194fde0b172cd99bd560c4d4839f92
dpapad <dpapad@chromium.org>
2025-01-06 12:10:02 -0800

    WebUI: Leverage build_webui() in chrome://webrtc-internals.
    This is in preparation of removing a JS minification step happening
    within Grit on Android builds, as Grit should not be responsible for
    performing minification.
    
    Bug: 340278433
    Change-Id: I5f6a1df8b24a34be3b806c137419546b7c628d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6147566
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402576}

--- tools/typescript/validate_tsconfig.py
+++ tools/typescript/validate_tsconfig.py
@@ -168,6 +168,7 @@ def validateJavaScriptAllowed(source_dir, out_dir, is_ios):
       # TODO(crbug.com/373951324): Migrate offline dino game to TypeScript.
       'components/neterror/resources',
       'components/policy/resources/webui',
+      'content/browser/webrtc/resources',
       'ui/webui/resources/js',
       'ui/webui/resources/mojo',
 
