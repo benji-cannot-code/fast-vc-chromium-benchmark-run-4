be6d73a038373ed7ba7ba8bb019a91ddd9b8a180
Adam Langley <agl@chromium.org>
2025-01-06 15:35:33 -0800

    webauthn: add (disabled) support for enclave attestation validation.
    The enclave service returns an attestation with each handshake. It might
    well be worthwhile in the future to enforce minimum firmware versions
    since older firmwares have known issues.
    
    This CL adds code that can validate an attestation and return the
    CPU/PSP firmware versions that were in use when the enclave process
    started. It is disabled at the moment since a) I think we want to make a
    breaking change to the enclave attestation format and, b) we need to
    establish and enforce minimum firmware versions in the code, but we'll
    need to confirm that with the operators of the SEV machines.
    
    Change-Id: I859a646421cf863242fa82ba0c20517cad67c57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6133457
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402702}

--- device/fido/enclave/transact.cc
+++ device/fido/enclave/transact.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/fido/enclave/transact.h"
 
+#include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/cbor/writer.h"
 #include "components/device_event_log/device_event_log.h"
 #include "device/fido/cable/v2_handshake.h"
+#include "device/fido/enclave/attestation.h"
 #include "device/fido/enclave/enclave_protocol_utils.h"
 #include "device/fido/enclave/enclave_websocket_client.h"
 #include "device/fido/features.h"
@@ -199,14 +201,27 @@ struct Transaction : base::RefCounted<Transaction> {
       return false;
     }
 
-    // `response` may contain arbitrary extra data, which is ignored. In
-    // the future this will contain attestation information.
+    auto attestation =
+        response.subspan(cablev2::HandshakeInitiator::kResponseSize);
     response = response.first<cablev2::HandshakeInitiator::kResponseSize>();
 
     cablev2::HandshakeResult result = handshake_.ProcessResponse(response);
     if (!result) {
       FIDO_LOG(ERROR) << "Enclave handshake failed";
       return false;
+    }
+
+    if (base::FeatureList::IsEnabled(device::kWebAuthnEnclaveAttestation)) {
+      auto attestation_result =
+          ProcessAttestation(attestation, /*handshake_hash=*/result->second);
+      if (!attestation_result.has_value()) {
+        FIDO_LOG(ERROR) << "Attestation checking failed: "
+                        << attestation_result.error();
+        return false;
+      }
+      // TODO: establish minimum firmware versions and enforce that
+      // `attestation_result` meets that bar. Likely want an UMA to measure
+      // attestation failure rates (which should be zero).
     }
 
     crypter_ = std::move(result->first);
