4a1596cc282407d77ae01481a99e43d52799818f
S. Ganesh <ganesh@chromium.org>
2025-01-06 14:24:50 -0800

    updater: fix the metainstaller to exit with the installer exit code
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED integration tests
    Fixed: 385151236
    Change-Id: Idedf9dbc73d083d5b852add217d44162a2fb4777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143978
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402656}

--- chrome/updater/app/app_install_win.cc
+++ chrome/updater/app/app_install_win.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
+#include "base/ranges/algorithm.h"
 #include "base/sequence_checker.h"
 #include "base/strings/escape.h"
 #include "base/strings/strcat.h"
@@ -880,8 +881,30 @@ void AppInstallControllerImpl::RunUI() {
   if (!callback_) {
     return;
   }
-  main_task_runner_->PostTask(FROM_HERE,
-                              base::BindOnce(std::move(callback_), kErrorOk));
+
+  main_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback_), [&]() {
+        if (!observer_completion_info_) {
+          return kErrorNoObserverCompletionInfo;
+        }
+
+        if (observer_completion_info_->completion_code !=
+            CompletionCodes::COMPLETION_CODE_ERROR) {
+          return kErrorOk;
+        }
+
+        if (observer_completion_info_->apps_info.empty()) {
+          return kErrorNoApps;
+        }
+
+        return base::ranges::max_element(
+                   observer_completion_info_->apps_info,
+                   [](const auto& app_info1, const auto& app_info2) {
+                     return GetPriority(app_info1.completion_code) <
+                            GetPriority(app_info2.completion_code);
+                   })
+            ->error_code;
+      }()));
 }
 
 void AppInstallControllerImpl::DoExit() {
