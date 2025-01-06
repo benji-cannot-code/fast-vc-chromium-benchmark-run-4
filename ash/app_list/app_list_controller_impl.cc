1ebc620f8f6066fdf03f27ed3ae39820178d7453
Sophie Wen <sophiewen@google.com>
2025-01-06 14:40:30 -0800

    sunfish: Disable nudge if kAshNoNudges switch is on
    This CL disables the Sunfish launcher nudge if kAshNoNudges is on.
    
    Bug: b:385643386
    Change-Id: I608b02cb81f7a28a1f22d7550b4fdea61dbf12cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6150109
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402666}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/capture_mode/capture_mode_constants.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
+#include "ash/constants/ash_switches.h"
 #include "ash/keyboard/ui/keyboard_ui_controller.h"
 #include "ash/public/cpp/app_list/app_list_client.h"
 #include "ash/public/cpp/app_list/app_list_controller_observer.h"
@@ -66,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_util.h"
 #include "base/barrier_closure.h"
 #include "base/callback_list.h"
+#include "base/command_line.h"
 #include "base/containers/adapters.h"
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
@@ -2103,6 +2105,11 @@ void AppListControllerImpl::SetSunfishNudgeDisabledForTest(bool is_disabled) {
 void AppListControllerImpl::MaybeShowSunfishLauncherNudge(
     views::View* launcher_button) {
   if (g_sunfish_nudge_disabled_for_test) {
+    return;
+  }
+
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kAshNoNudges)) {
     return;
   }
 
