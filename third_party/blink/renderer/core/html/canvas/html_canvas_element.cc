0e2e0b3c3b6b9007486982748f0799fb87765f60
Colin Blundell <blundell@chromium.org>
2025-01-07 01:34:11 -0800

    [Blink] Have HTMLCanvasElement check alpha type directly for opacity
    HTMLCanvasElement currently checks for opacity via calling CanvasRenderingContext::CanvasRenderingContextSkColorInfo::isOpaque().
    SkColorInfo::isOpaque() returns true if either the alpha type is opaque
    or the color type is always opaque [1]. However, the color type used by
    CanvasRenderingContext can only be RGBA_8888, BGRA_8888 or RGBA_F16
    (note that N32 *is* either RGBA_8888 or BGRA_8888):
    * It comes from [2] for (Offscreen)Canvas2D
    * It comes from [3] for ImageBitmap
    * It comes from [4] for WebGL
    * It comes from [5] for WebGPU
    
    RGBA_8888/BGRA_8888/RGBA_F16 are (naturally) *not* always-opaque color
    types [6][7][8].  Hence, we can replace this check by directly checking
    whether the alpha type is opaque.
    
    This also enables removing
    CanvasRenderingContext::CanvasRenderingContextSkColorInfo() altogether.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/include/core/SkImageInfo.h;l=149-152;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_color_params.cc;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27;l=67
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context_base.h;l=45;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc;l=5622;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27
    [5] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/webgpu_swap_buffer_provider.cc;l=21;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27;bpv=1;bpt=1
    [6] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/src/core/SkImageInfo.cpp;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27;l=49
    [7] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/src/core/SkImageInfoPriv.h;l=15-46;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27
    [8] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/include/core/SkColor.h;drc=dd73812bb7bca5b8fb5105127573bcfd4f226a27;l=248
    
    Bug: 371227617
    Change-Id: I9276a3edbc03b8cbc1e20a0c2058c2fd16d83f8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097342
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402844}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -937,8 +937,7 @@ void HTMLCanvasElement::NotifyListenersCanvasChanged() {
   }
 
   const bool context_color_is_opaque =
-      context_ ? context_->CanvasRenderingContextSkColorInfo().isOpaque()
-               : false;
+      context_ ? SkAlphaTypeIsOpaque(context_->GetAlphaType()) : false;
 
   for (CanvasDrawListener* listener : listeners_) {
     if (!listener->NeedsNewFrame())
