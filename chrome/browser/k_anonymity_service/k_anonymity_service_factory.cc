d374b8f36eb8e22aec8f2a47eb1729077d334819
Muyao Xu <muyaoxu@google.com>
2025-01-07 11:48:00 -0800

    [Code Health] Clean up stale base::Feature "KAnonymityService"
    Bug: 356235647
    Change-Id: I53fe73e1d727a601b798674635f3e49a35ed1837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097838
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1403159}

--- chrome/browser/k_anonymity_service/k_anonymity_service_factory.cc
+++ chrome/browser/k_anonymity_service/k_anonymity_service_factory.cc
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 ProfileSelections BuildKAnonymityServiceProfileSelections() {
-  if (!base::FeatureList::IsEnabled(features::kKAnonymityService))
-    return ProfileSelections::BuildNoProfilesSelected();
   return ProfileSelections::Builder()
       .WithRegular(ProfileSelection::kOwnInstance)
       .WithGuest(ProfileSelection::kOwnInstance)
