a5213c1adf6e0342ed74477fa4c96effb8aaabef
Dustin J. Mitchell <djmitche@chromium.org>
2025-01-07 11:13:30 -0800

    Roll quote: 1.0.37 => 1.0.38 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * quote: 1.0.37 => 1.0.38
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * quote@1.0.38: crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: I3ec6345b0fdc03cac6071b7d6643d5f5ffb32cfb
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6149189
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403131}

--- third_party/rust/chromium_crates_io/vendor/quote-1.0.38/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/quote-1.0.38/src/lib.rs
@@ -93,12 +93,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! [prettyplease]: https://github.com/dtolnay/prettyplease
 
 // Quote types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/quote/1.0.37")]
+#![doc(html_root_url = "https://docs.rs/quote/1.0.38")]
 #![allow(
     clippy::doc_markdown,
     clippy::missing_errors_doc,
     clippy::missing_panics_doc,
     clippy::module_name_repetitions,
+    clippy::needless_lifetimes,
     // false positive https://github.com/rust-lang/rust-clippy/issues/6983
     clippy::wrong_self_convention,
 )]
