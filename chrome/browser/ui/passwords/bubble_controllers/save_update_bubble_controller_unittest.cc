5565cbc2b1a4dd97f97b2599feaf5ddf8d3d33ee
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-01-07 04:36:00 -0800

    Remove passwords account storage promo from save bubble
    Unblocked by launching ExplicitBrowserSigninUIOnDesktop [1] (note
    the flag was not launched on ChromeOS, but ChromeOS never showed
    the promo because the user is always syncing).
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/password_manager/core/browser/features/password_manager_features_util_desktop.cc;l=384;drc=e35c86a08334445bec5b81cdc7a8b8ffa67529f8
    
    Bug: 374293518
    Change-Id: I3c55730f91429466e27e9e17cb7a7a2004a2e0cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953264
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402900}

--- chrome/browser/ui/passwords/bubble_controllers/save_update_bubble_controller_unittest.cc
+++ chrome/browser/ui/passwords/bubble_controllers/save_update_bubble_controller_unittest.cc
@@ -316,8 +316,6 @@ TEST_F(SaveUpdateBubbleControllerTest, ClickSaveInLocalStore) {
                                         pending_password().password_value));
   EXPECT_CALL(*delegate(), NeverSavePassword()).Times(0);
   EXPECT_CALL(*delegate(), OnNopeUpdateClicked()).Times(0);
-  EXPECT_CALL(*delegate(), AuthenticateUserForAccountStoreOptInAndSavePassword)
-      .Times(0);
   controller()->OnSaveClicked();
   DestroyModelExpectReason(password_manager::metrics_util::CLICKED_ACCEPT);
 }
@@ -331,7 +329,6 @@ TEST_F(SaveUpdateBubbleControllerTest, ClickSaveInAccountStoreWhileOptedIn) {
   PretendPasswordWaiting();
 
   EXPECT_FALSE(controller()->IsCurrentStateUpdate());
-  EXPECT_FALSE(controller()->IsAccountStorageOptInRequiredBeforeSave());
 
   EXPECT_CALL(*mock_smart_bubble_stats_store(),
               RemoveSiteStats(GURL(kSiteOrigin).DeprecatedGetOriginAsURL()));
@@ -340,31 +337,6 @@ TEST_F(SaveUpdateBubbleControllerTest, ClickSaveInAccountStoreWhileOptedIn) {
                                         pending_password().password_value));
   EXPECT_CALL(*delegate(), NeverSavePassword()).Times(0);
   EXPECT_CALL(*delegate(), OnNopeUpdateClicked()).Times(0);
-  EXPECT_CALL(*delegate(), AuthenticateUserForAccountStoreOptInAndSavePassword)
-      .Times(0);
-  controller()->OnSaveClicked();
-  DestroyModelExpectReason(password_manager::metrics_util::CLICKED_ACCEPT);
-}
-
-TEST_F(SaveUpdateBubbleControllerTest, ClickSaveInAccountStoreWhileNotOptedIn) {
-  ON_CALL(*password_feature_manager(), GetDefaultPasswordStore)
-      .WillByDefault(
-          Return(password_manager::PasswordForm::Store::kAccountStore));
-  ON_CALL(*password_feature_manager(), IsOptedInForAccountStorage)
-      .WillByDefault(Return(false));
-  PretendPasswordWaiting();
-
-  EXPECT_FALSE(controller()->IsCurrentStateUpdate());
-  EXPECT_TRUE(controller()->IsAccountStorageOptInRequiredBeforeSave());
-
-  EXPECT_CALL(*mock_smart_bubble_stats_store(),
-              RemoveSiteStats(GURL(kSiteOrigin).DeprecatedGetOriginAsURL()));
-  EXPECT_CALL(*delegate(), SavePassword).Times(0);
-  EXPECT_CALL(*delegate(), NeverSavePassword()).Times(0);
-  EXPECT_CALL(*delegate(), OnNopeUpdateClicked()).Times(0);
-  EXPECT_CALL(*delegate(), AuthenticateUserForAccountStoreOptInAndSavePassword(
-                               pending_password().username_value,
-                               pending_password().password_value));
   controller()->OnSaveClicked();
   DestroyModelExpectReason(password_manager::metrics_util::CLICKED_ACCEPT);
 }
@@ -380,7 +352,6 @@ TEST_F(SaveUpdateBubbleControllerTest, ClickUpdateWhileNotOptedIn) {
   PretendUpdatePasswordWaiting();
 
   EXPECT_TRUE(controller()->IsCurrentStateUpdate());
-  EXPECT_FALSE(controller()->IsAccountStorageOptInRequiredBeforeSave());
 
   EXPECT_CALL(*mock_smart_bubble_stats_store(),
               RemoveSiteStats(GURL(kSiteOrigin).DeprecatedGetOriginAsURL()));
@@ -388,8 +359,6 @@ TEST_F(SaveUpdateBubbleControllerTest, ClickUpdateWhileNotOptedIn) {
                                         pending_password().password_value));
   EXPECT_CALL(*delegate(), NeverSavePassword()).Times(0);
   EXPECT_CALL(*delegate(), OnNopeUpdateClicked()).Times(0);
-  EXPECT_CALL(*delegate(), AuthenticateUserForAccountStoreOptInAndSavePassword)
-      .Times(0);
   controller()->OnSaveClicked();
   DestroyModelExpectReason(password_manager::metrics_util::CLICKED_ACCEPT);
 }
