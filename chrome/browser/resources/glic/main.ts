967570a61c376097e5acd89ff7bd17c36bf364d0
Dan Harrington <harringtond@chromium.org>
2025-01-07 15:43:37 -0800

    Add GlicDevelopmentCookies feature
    When it's enabled, google login cookies are copied from
    the main partition into the glic webview partition, which
    allows for certain development workflows. This is only
    intended for use by @google accounts.
    
    Bug: 379949671
    Change-Id: I424012896f5e324658337c9f93cb40d09fc4dcc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6138361
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403288}

--- chrome/browser/resources/glic/main.ts
+++ chrome/browser/resources/glic/main.ts
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '/strings.m.js';
 
+import {loadTimeData} from '//resources/js/load_time_data.js';
+
 import {BrowserProxyImpl} from './browser_proxy.js';
 import {GlicApiHost} from './glic_api_impl/glic_api_host.js';
 
@@ -65,4 +67,8 @@ class GlicAppHostManager {
 
 // Blocking on cookie syncing here introduces latency, we should consider ways
 // to avoid it.
-browserProxy.handler.syncWebviewCookies().then(() => new GlicAppHostManager());
+browserProxy.handler.syncWebviewCookies().then(() => {
+  // Load the web client only after cookie sync is complete.
+  webview.src = loadTimeData.getString('glicGuestURL');
+  new GlicAppHostManager();
+});
