458391231c058ff048d31764063b422c50510487
Sarah Krakowiak Criel <skrakowi@chromium.org>
2025-01-07 07:41:20 -0800

    Create histogram for suspicious notification interactions
    See go/spamish-dd for details.
    
    Bug: 382475678
    Change-Id: I5bfe1bc0a8b00607537f6a43d697bce70122e92c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6138578
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402989}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridge.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.notifications;
 
+import static org.chromium.chrome.browser.notifications.SuspiciousNotificationWarningUtils.recordSuspiciousNotificationWarningInteractions;
 import static org.chromium.components.content_settings.PrefNames.NOTIFICATIONS_VIBRATE_ENABLED;
 
 import android.app.Notification;
@@ -42,6 +43,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.browserservices.TrustedWebActivityClient;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
+import org.chromium.chrome.browser.notifications.SuspiciousNotificationWarningUtils.SuspiciousNotificationWarningInteractions;
 import org.chromium.chrome.browser.notifications.channels.SiteChannelsManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileManager;
@@ -263,6 +265,8 @@ public class NotificationPlatformBridge {
             restoreNotificationBackups(
                     attributes,
                     NotificationConstants.EXTRA_NOTIFICATION_BACKUP_FOR_SUSPICIOUS_VERDICT);
+            recordSuspiciousNotificationWarningInteractions(
+                    SuspiciousNotificationWarningInteractions.SHOW_ORIGINAL_NOTIFICATION);
             return false;
         }
 
@@ -833,6 +837,9 @@ public class NotificationPlatformBridge {
                                                     timestamp,
                                                     silent,
                                                     notification.getNotification()));
+                                    recordSuspiciousNotificationWarningInteractions(
+                                            SuspiciousNotificationWarningInteractions
+                                                    .WARNING_SHOWN);
                                 } else {
                                     mNotificationManager.notify(notification);
                                 }
