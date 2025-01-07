1eea472acde73ed3d68bb4d1f7c102895b36e79f
Elly <ellyjones@chromium.org>
2025-01-06 17:14:15 -0800

    cdm: fix unsafe buffers in cenc_utils
    This change removes all unsafe buffer uses in cenc_utils and their unit
    tests. To do that, it:
    
    * Migrates the cenc_utils functions from taking const vector<uint8_t>&
      to taking span<const uint8_t>
    * Migrates all the test data from uint8_t[] to std::array<uint8_t>
    * Removes a lot of now-unnecessary vector copies
    * Includes cenc_utils_unittest.cc in builds with !proprietary_codecs -
      cenc_utils.cc is included in these builds, so the tests should be as
      well, and these tests don't depend on proprietary codecs.
    * Since the test fixture for CencUtilsTest now has no state and does no
      setup, deletes the test fixture altogether and makes the tests
      fixtureless.
    
    Bug: 383606739
    Change-Id: Ib87c1dfd9764e9fe878bcdba3e3399fadc5d4e84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088948
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402747}

--- media/cdm/cenc_utils.h
+++ media/cdm/cenc_utils.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/containers/span.h"
 #include "media/base/media_export.h"
 #include "media/cdm/json_web_key.h"
 
@@ -17,7 +18,7 @@ namespace media {
 
 // Validate that |input| is a set of concatenated 'pssh' boxes and the sizes
 // match. Returns true if |input| looks valid, false otherwise.
-MEDIA_EXPORT bool ValidatePsshInput(const std::vector<uint8_t>& input);
+MEDIA_EXPORT bool ValidatePsshInput(base::span<const uint8_t> input);
 
 // Gets the Key Ids from the first 'pssh' box for the Common System ID among one
 // or more concatenated 'pssh' boxes. Returns true if a matching box is found
@@ -28,7 +29,7 @@ MEDIA_EXPORT bool ValidatePsshInput(const std::vector<uint8_t>& input);
 // 2. Only PSSH boxes are allowed in |input|. Any other boxes in |pssh_boxes|
 //    will result in false being returned.
 MEDIA_EXPORT bool GetKeyIdsForCommonSystemId(
-    const std::vector<uint8_t>& pssh_boxes,
+    base::span<const uint8_t> pssh_boxes,
     KeyIdList* key_ids);
 
 // Gets the data field from the first 'pssh' box containing |system_id|.
@@ -39,8 +40,8 @@ MEDIA_EXPORT bool GetKeyIdsForCommonSystemId(
 //    box will be set in |pssh_data|.
 // 2. Only PSSH boxes are allowed in |input|. Any other boxes in |pssh_boxes|
 //    will result in false being returned.
-MEDIA_EXPORT bool GetPsshData(const std::vector<uint8_t>& input,
-                              const std::vector<uint8_t>& system_id,
+MEDIA_EXPORT bool GetPsshData(base::span<const uint8_t> input,
+                              base::span<const uint8_t> system_id,
                               std::vector<uint8_t>* pssh_data);
 
 }  // namespace media
