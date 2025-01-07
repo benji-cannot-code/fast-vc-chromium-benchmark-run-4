aab99e9b80edf030a0274000187d64c837813296
Jérôme Lebel <jlebel@chromium.org>
2025-01-07 12:02:02 -0800

    [iOS][MIM2] Cleanup in Signin.AccountType.SigninConsent metric
    Add LogSigninWithAccountType() in signin_metrics.h to record the
    metric with SigninAccountType enum.
    
    Change-Id: I07dea9b42d750bd85b4535b268ab74cd06722274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148133
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403172}

--- ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_unittest.mm
+++ ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_unittest.mm
@@ -54,14 +54,6 @@ NSString* const kFakeDMToken = @"fake_dm_token";
 NSString* const kFakeClientID = @"fake_client_id";
 NSString* const kFakeUserAffiliationID = @"fake_user_affiliation_id";
 
-// Duplicated from
-// ios/chrome/browser/authentication/ui_bundled/authentication_flow.mm, which
-// is fine since the enum values should never be renumbered.
-enum class SigninAccountType {
-  kRegular = 0,
-  kManaged = 1,
-};
-
 class AuthenticationFlowTest : public PlatformTest {
  protected:
   void SetUp() override {
@@ -268,8 +260,9 @@ TEST_F(AuthenticationFlowTest, TestSignInSimple) {
 
   SignIn(identity1_, signin_metrics::AccessPoint::ACCESS_POINT_START_PAGE);
 
-  histogram_tester_.ExpectUniqueSample("Signin.AccountType.SigninConsent",
-                                       SigninAccountType::kRegular, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Signin.AccountType.SigninConsent",
+      signin_metrics::SigninAccountType::kRegular, 1);
 }
 
 // Tests a Sign In of a normal account on the same profile using a navigatiion
@@ -285,8 +278,9 @@ TEST_F(AuthenticationFlowTest, TestSignInNewScreen) {
 
   SignIn(identity1_, signin_metrics::AccessPoint::ACCESS_POINT_START_PAGE);
 
-  histogram_tester_.ExpectUniqueSample("Signin.AccountType.SigninConsent",
-                                       SigninAccountType::kRegular, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Signin.AccountType.SigninConsent",
+      signin_metrics::SigninAccountType::kRegular, 1);
 }
 
 // Tests the fetch managed status failure case.
@@ -326,8 +320,9 @@ TEST_F(AuthenticationFlowTest,
 
   SignIn(managed_identity1_,
          signin_metrics::AccessPoint::ACCESS_POINT_SUPERVISED_USER);
-  histogram_tester_.ExpectUniqueSample("Signin.AccountType.SigninConsent",
-                                       SigninAccountType::kManaged, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Signin.AccountType.SigninConsent",
+      signin_metrics::SigninAccountType::kManaged, 1);
   EXPECT_EQ(1, managed_confirmation_dialog_shown_count_);
 }
 
@@ -353,8 +348,9 @@ TEST_F(AuthenticationFlowTest,
 
   SignIn(managed_identity1_,
          signin_metrics::AccessPoint::ACCESS_POINT_SUPERVISED_USER);
-  histogram_tester_.ExpectUniqueSample("Signin.AccountType.SigninConsent",
-                                       SigninAccountType::kManaged, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Signin.AccountType.SigninConsent",
+      signin_metrics::SigninAccountType::kManaged, 1);
   EXPECT_EQ(0, managed_confirmation_dialog_shown_count_);
 }
 
