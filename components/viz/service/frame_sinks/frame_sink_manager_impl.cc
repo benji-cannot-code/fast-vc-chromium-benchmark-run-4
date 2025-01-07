39b3d4c280b8cf1177ddd9b039cf0e24494c66ab
Aman Verma <amanvr@google.com>
2025-01-07 07:14:37 -0800

    [InputVizard]: Filter input events in Viz based on render process block state.
    This CL introduces the ability to allow filtering input events at the
    RenderInputRouter::FilterInputEvent [1] level in Viz with InputVizard.
    RenderProcessHostImpl observes and notifies the HostFrameSinkManager
    with the FrameSinkIds of corresponding RenderWidgetHosts whose block
    state has changed. The FrameSinkIds are then sent to InputManager (in
    Viz) to update the |is_blocked_| state being tracked in
    RenderInputRouterDelegateImpl using FrameSinkManager mojo interface.
    
    This ensures that input events are not delivered to a blocked renderer.
    This block state update in browser was already async (for ex. for
    JavaScript dialog boxes), since it gets notified from renderer via mojo
    pipe. Extending this to include input filtering in Viz should have
    minimal impact on performance.
    
    [1]:
    https://source.chromium.org/chromium/chromium/src/+/main:components/input/render_input_router.cc;l=281;drc=616d60fca655937c2b730db94fd32d37ddff3bb5
    
    Bug: b:377625588
    Change-Id: I3343b17f23ca1ddf3fc6847092089d7cdb9cca9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080515
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402978}

--- components/viz/service/frame_sinks/frame_sink_manager_impl.cc
+++ components/viz/service/frame_sinks/frame_sink_manager_impl.cc
@@ -1201,6 +1201,13 @@ void FrameSinkManagerImpl::SetupRenderInputRouterDelegateConnection(
       std::move(rir_delegate_receiver));
 }
 
+void FrameSinkManagerImpl::NotifyRendererBlockStateChanged(
+    bool blocked,
+    const std::vector<FrameSinkId>& render_input_routers) {
+  input_manager_->NotifyRendererBlockStateChanged(blocked,
+                                                  render_input_routers);
+}
+
 void FrameSinkManagerImpl::RequestBeginFrameForGpuService(bool toggle) {
   if (root_begin_frame_source_ && gpu_service_) {
     if (toggle) {
--- components/viz/service/input/input_manager.h
+++ components/viz/service/input/input_manager.h
@@ -136,6 +136,9 @@ class VIZ_SERVICE_EXPORT InputManager
       mojo::PendingReceiver<input::mojom::RenderInputRouterDelegate>
           rir_delegate_receiver);
 
+  void NotifyRendererBlockStateChanged(bool blocked,
+                                       const std::vector<FrameSinkId>& rirs);
+
   input::RenderInputRouter* GetRenderInputRouterFromFrameSinkId(
       const FrameSinkId& id);
 
