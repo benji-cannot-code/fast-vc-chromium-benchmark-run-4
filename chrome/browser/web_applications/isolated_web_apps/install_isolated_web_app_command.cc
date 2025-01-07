9ca1633b5e6a8889f9ba9183df5966547d3d5e1a
Robert Ferens <rferens@google.com>
2025-01-07 04:14:01 -0800

    Make debug log for InstallIsolatedWebAppCommandSuccess readable
    Previously it printed unprintable characters and looked messy, e.g:
    ```
    InstallIsolatedWebAppCommandSuccess {
     "installed_version": "1.0.0",
     "location": "{\n   \"proxy\": {\n      \"proxy_url\": \"http://127.0.0.1:46607\"\n   }\n}\n"
    }
    ```
    
    Fixed: 387452482
    Change-Id: I17418cb4264e15ca40debd6c92c1ae5373debdb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6133122
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Robert Ferens <rferens@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402892}

--- chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.cc
+++ chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.cc
@@ -73,7 +73,7 @@ std::ostream& operator<<(std::ostream& os,
             << base::Value::Dict()
                    .Set("installed_version",
                         success.installed_version.GetString())
-                   .Set("location", base::ToString(success.location));
+                   .Set("location", success.location.ToDebugValue());
 }
 
 std::ostream& operator<<(std::ostream& os,
