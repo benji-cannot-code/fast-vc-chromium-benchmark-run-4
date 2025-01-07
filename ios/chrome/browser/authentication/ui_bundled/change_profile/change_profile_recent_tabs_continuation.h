b8f04fe5832c375633217130daec7899ff9b1b70
Federica Germinario <fedegermi@google.com>
2025-01-07 06:52:33 -0800

    [ios] Add continuation for recent tabs
    Add continuation to open recent tabs after switching profile.
    
    Fixed: 374283051
    Change-Id: I0b355a335fee3ae06e60bb6a9cf9c0c51ede29ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6127389
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402961}

---
+++ ios/chrome/browser/authentication/ui_bundled/change_profile/change_profile_recent_tabs_continuation.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_CHANGE_PROFILE_CHANGE_PROFILE_RECENT_TABS_CONTINUATION_H_
+#define IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_CHANGE_PROFILE_CHANGE_PROFILE_RECENT_TABS_CONTINUATION_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/app/change_profile_continuation.h"
+
+@interface ChangeProfileRecentTabsContinuation
+    : NSObject <ChangeProfileContinuation>
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_CHANGE_PROFILE_CHANGE_PROFILE_RECENT_TABS_CONTINUATION_H_
