879968a8d88f81ea0682a1b10911603b396086dd
Anna Tsvirchkova <atsvirchkova@google.com>
2025-01-07 07:03:32 -0800

    Add privacy notice bubble
    The privacy notice bubble is displayed before starting the password change flow. Only after accepting the notice, the flow will start. This CL wires the "Accept" button to start the flow.
    
    Bug: 381053884
    Change-Id: I94dafcf7e6cba7ed3260357aa0c2614fb4ede189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6131860
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402967}

---
+++ chrome/browser/ui/passwords/bubble_controllers/password_change/privacy_notice_bubble_view_controller.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/bubble_controllers/password_change/privacy_notice_bubble_view_controller.h"
+
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/ui/passwords/passwords_model_delegate.h"
+
+PrivacyNoticeBubbleViewController::PrivacyNoticeBubbleViewController(
+    base::WeakPtr<PasswordsModelDelegate> delegate)
+    : PasswordBubbleControllerBase(
+          delegate,
+          password_manager::metrics_util::UIDisplayDisposition::
+              PASSWORD_CHANGE_BUBBLE),
+      password_change_delegate_(
+          delegate_->GetPasswordChangeDelegate()->AsWeakPtr()) {}
+
+PrivacyNoticeBubbleViewController::~PrivacyNoticeBubbleViewController() {
+  OnBubbleClosing();
+}
+
+std::u16string PrivacyNoticeBubbleViewController::GetTitle() const {
+  // TODO(crbug.com/381053884): Add string.
+  return u"Lorem ipsum";
+}
+
+void PrivacyNoticeBubbleViewController::ReportInteractions() {
+  // TODO(crbug.com/381053884): Report metrics.
+}
+
+void PrivacyNoticeBubbleViewController::AcceptNotice() {
+  password_change_delegate_->OnPrivacyNoticeAccepted();
+}
--- chrome/browser/ui/views/passwords/password_change/password_change_view_factory.cc
+++ chrome/browser/ui/views/passwords/password_change/password_change_view_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "chrome/browser/password_manager/password_change_delegate.h"
 #include "chrome/browser/ui/views/passwords/password_change/password_change_info_bubble_view.h"
+#include "chrome/browser/ui/views/passwords/password_change/privacy_notice_view.h"
 
 PasswordBubbleViewBase* CreatePasswordChangeBubbleView(
     PasswordChangeDelegate* delegate,
@@ -16,11 +17,12 @@ PasswordBubbleViewBase* CreatePasswordChangeBubbleView(
     views::View* anchor_view) {
   switch (delegate->GetCurrentState()) {
       // TODO (crbug.com/375564659): Implement views for each state.
+    case PasswordChangeDelegate::State::kWaitingForAgreement:
+      return new PrivacyNoticeView(web_contents, anchor_view);
     case PasswordChangeDelegate::State::kWaitingForChangePasswordForm:
     case PasswordChangeDelegate::State::kChangingPassword:
       return new PasswordChangeInfoBubbleView(web_contents, anchor_view,
                                               delegate->GetCurrentState());
-    case PasswordChangeDelegate::State::kWaitingForAgreement:
     case PasswordChangeDelegate::State::kPasswordSuccessfullyChanged:
     case PasswordChangeDelegate::State::kPasswordChangeFailed:
       NOTIMPLEMENTED();
