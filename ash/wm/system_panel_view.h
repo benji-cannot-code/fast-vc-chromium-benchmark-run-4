289b825b88062ce5eca96a5e6e3d9cd13ae97748
Sophie Wen <sophiewen@google.com>
2025-01-07 12:00:48 -0800

    sunfish: Only end session on right-click events
    This CL fixes an issue where Sunfish session would be ended even on
    drag. Since it is unknown on the mouse press event whether it will be
    a mouse release or drag event, this modifies the session to only be
    ended on a right-click to also address b:377352005.
    
    Demo: https://b.corp.google.com/issues/385156981#comment2
    
    Bug: b:385156981
    Test: added
    Change-Id: Ifcb4f7e0c05b603a0780852c2bd44651f49e399d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111328
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403170}

--- ash/wm/system_panel_view.h
+++ ash/wm/system_panel_view.h
@@ -37,6 +37,7 @@ class ASH_EXPORT SystemPanelView : public views::View {
                              ui::LocatedEvent* event);
 
    private:
+    friend class SystemPanelView;
     // Whether a drag is currently in progress.
     bool is_dragging_ = false;
 
@@ -53,6 +54,8 @@ class ASH_EXPORT SystemPanelView : public views::View {
   SystemPanelView(const SystemPanelView&) = delete;
   SystemPanelView& operator=(const SystemPanelView&) = delete;
   ~SystemPanelView() override;
+
+  bool IsDragging() const;
 
   // views::View:
   void OnMouseEvent(ui::MouseEvent* event) override;
