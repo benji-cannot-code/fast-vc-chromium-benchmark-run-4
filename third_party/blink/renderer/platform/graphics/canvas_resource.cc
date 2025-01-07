0fcd2e48698531633427378e9ffbba6cadd7e8e4
Colin Blundell <blundell@chromium.org>
2025-01-07 01:49:16 -0800

    [Blink] Change CanvasResourceSharedBitmap validity
    CanvasResourceSharedBitmap currently implements IsValid() by checking
    whether the WritableSharedMemoryMapping that it is holding is valid.
    This mapping will be valid on construction by definition as the
    CreateSharedImage() call from which it is populated crashes if it can't
    create a valid mapping. Hence, the only way that it can become invalid
    is due to being cleared in NotifyResourceLost().
    
    In preparation for changing CanvasResourceSharedBitmap to hold only a
    ClientSharedImage that internally holds the SharedMemoryMapping, this CL
    changes this flow to clear the ClientSI in NotifyResourceLost() and
    check for whether the ClientSI is present in IsValid(). All usages of
    the ClientSharedImage via GetSharedImage() will be entered only if the
    resource is valid (e.g., CanvasResourceSharedImage also implements
    IsValid() by checking whether its ClientSI is present).
    
    This CL is a slight behavioral change as it means that we will be
    dropping the ClientSI in NotifyResourceLost() rather than when the
    CanvasResourceSharedBitmap is subsequently destroyed. This behavioral
    change does not seem problematic for the reason mentioned above.
    
    Bug: 379996128
    Change-Id: I02879fa04874214277b3861d69e2d6e4be178cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097446
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402847}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -291,7 +291,7 @@ CanvasResourceSharedBitmap::~CanvasResourceSharedBitmap() {
 }
 
 bool CanvasResourceSharedBitmap::IsValid() const {
-  return shared_mapping_.IsValid();
+  return !!shared_image_;
 }
 
 scoped_refptr<StaticBitmapImage> CanvasResourceSharedBitmap::Bitmap() {
@@ -332,9 +332,9 @@ scoped_refptr<CanvasResourceSharedBitmap> CanvasResourceSharedBitmap::Create(
 }
 
 void CanvasResourceSharedBitmap::NotifyResourceLost() {
-  // Release our reference to the shared memory mapping since the resource can
-  // no longer be safely recycled and this memory is needed for copy-on-write.
-  shared_mapping_ = {};
+  // Release our reference to the SharedImage since the resource can
+  // no longer be safely recycled and its memory is needed for copy-on-write.
+  shared_image_.reset();
 }
 
 void CanvasResourceSharedBitmap::UploadSoftwareRenderingResults(
