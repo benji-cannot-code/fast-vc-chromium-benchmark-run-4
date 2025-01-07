f1e9450e940ca6cf0d5c6b090b8d3c9d2ef88cc8
Hsincheng Hou <dihimono@google.com>
2025-01-06 17:09:56 -0800

    Reland "Diagnostics: enable split modifier keyboard in diagnostics app."
    This is a reland of commit 3d34cc12858adb0a993f444dedb4dc42f9f55fd0
    
    The previous commit is missing the default definition of the newly added attributes. This commit updates `input_device_information.h` and add in the default value.
    
    The default value in header can fix the MSAN issue as the unit tests used the default construtor of InputDeviceInformation.
    
    This commit also updates the condition of excluding keyboard diagnostics into a single if clause in `input_data_provide.cc`.
    
    Original change's description:
    > Diagnostics: enable split modifier keyboard in diagnostics app.
    >
    > Enable keyboard diagnostics in app if the corresponding cros_config
    > has value (`bottom_left_layout` and `bottom_right_layout`).
    >
    > example screenshots:
    > https://screenshot.googleplex.com/9VrrENfTUKWjpQe.png
    > https://screenshot.googleplex.com/7u96cWqeyCg7Qsh.png
    >
    > Bug: 380002430
    > TEST: ash_unittests and browser_tests --gtest_filter="Diagnostics*"
    > Change-Id: Ia4f17930e718bd9634bf5e5b3f47c2cbfc3acb4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109418
    > Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    > Reviewed-by: Denny Huang <dennyh@google.com>
    > Commit-Queue: Hsincheng Hou <dihimono@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1401070}
    
    Bug: 380002430
    Change-Id: I300ad16b034de33e5977cf5a630ff6259b85a35f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6145817
    Reviewed-by: Denny Huang <dennyh@google.com>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Byron Lee <byronlee@chromium.org>
    Commit-Queue: Hsincheng Hou <dihimono@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402745}

--- ash/webui/diagnostics_ui/backend/input/input_device_information.h
+++ ash/webui/diagnostics_ui/backend/input/input_device_information.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::diagnostics {
 
+template <typename T>
+T GetLayoutFromFile(const base::FilePath& file_path,
+                    const std::map<std::string, T>& layout_mapping);
+
 // Wrapper for tracking several pieces of information about an evdev-backed
 // device.
 class InputDeviceInformation {
@@ -34,6 +38,9 @@ class InputDeviceInformation {
   // Keyboard-only fields:
   ui::KeyboardCapability::DeviceType keyboard_type;
   ui::KeyboardCapability::KeyboardTopRowLayout keyboard_top_row_layout;
+  mojom::BottomLeftLayout bottom_left_layout = mojom::BottomLeftLayout::kUnknown;
+  mojom::BottomRightLayout bottom_right_layout = mojom::BottomRightLayout::kUnknown;
+  mojom::NumpadLayout numpad_layout = mojom::NumpadLayout::kUnknown;
   std::vector<uint32_t> keyboard_scan_codes;
 };
 
