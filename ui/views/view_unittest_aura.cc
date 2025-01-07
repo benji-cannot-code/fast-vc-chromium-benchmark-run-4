461c57efd84cc89fef650b2cc58f0019768c1f45
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-01-07 11:27:33 -0800

    Fix bug in `Widget::ShouldDescendIntoChildForEventHandling()`
    When finding the event target, the WindowEventTargeter checks if it
    should explore the subtree of a window. Part of the logic is delegated
    to the aura::WindowDelegate (i.e NativeWidgetAura). NativeWidgetAura
    further delegates the work to views::Widget.
    
    Widget iterates through its views with layers with a z-order in mind
    to figure out if we should explore the subtree. However
    `BuildViewsWithLayers()` used to build the z-order sensitive views
    had a bug: It was calling `view->children()` which does not always
    return the child views in correct z-order.
    
    Bug: b:385257216
    Change-Id: Iae51c65d8b9008afb2b552fbb52cea8012757ddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143114
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403149}

--- ui/views/view_unittest_aura.cc
+++ ui/views/view_unittest_aura.cc
@@ -56,7 +56,7 @@ class ViewAuraTest : public ViewsTestBase {
   ~ViewAuraTest() override = default;
 
   const View::Views& GetViewsWithLayers(Widget* widget) {
-    return widget->GetViewsWithLayers();
+    return widget->GetViewsWithLayersInZOrder();
   }
 };
 
