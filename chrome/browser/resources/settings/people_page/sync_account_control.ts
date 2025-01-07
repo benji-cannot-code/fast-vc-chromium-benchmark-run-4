8e301c29b529e3b141549fccae4c2f8c13280247
Liza Bipin <mlbipin@google.com>
2025-01-07 09:56:22 -0800

    Remove signout button from dropdown in chrome://settings
    * This cl will remove the signout button from the dropdown from the
    account aware state in the settings page.
    
    * This cl also removes the header from the signed in and syncing page.
    
    * Screenshots in bug.
    
    Fixed: 387535426, 388216327
    Change-Id: Ia067a6bc70a18722d77a0563f7f848b7f156bd48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148184
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1403073}

--- chrome/browser/resources/settings/people_page/sync_account_control.ts
+++ chrome/browser/resources/settings/people_page/sync_account_control.ts
@@ -440,11 +440,11 @@ export class SettingsSyncAccountControlElement extends
     }
 
     switch (this.syncStatus.signedInState) {
-      case SignedInState.SYNCING:
       case SignedInState.SIGNED_IN:
       case SignedInState.SIGNED_OUT:
       case SignedInState.WEB_ONLY_SIGNED_IN:
         return false;
+      case SignedInState.SYNCING:
       case SignedInState.SIGNED_IN_PAUSED:
         return true;
     }
@@ -535,6 +535,11 @@ export class SettingsSyncAccountControlElement extends
     return (this.isSyncing_() || this.storedAccounts_.length > 0) &&
         this.syncStatus.signedInState !== SignedInState.WEB_ONLY_SIGNED_IN;
   }
+
+  private shouldHideSignoutDropdownButton_(): boolean {
+    return loadTimeData.getBoolean('isImprovedSettingsUIOnDesktopEnabled');
+  }
+
 
   private onErrorButtonClick_() {
     const router = Router.getInstance();
