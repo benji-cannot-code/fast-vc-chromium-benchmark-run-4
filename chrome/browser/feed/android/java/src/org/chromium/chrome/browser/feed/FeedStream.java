d4b021b6128922e0523a5e293b7982fbea01bd71
Menghan Yang <myuu@google.com>
2025-01-07 15:30:54 -0800

    [Android][Signin] Remove deprecated sign-in flow from the feed
    Given UNO Phase 2 is launched & REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS
    is enabled by default, this CL removes the call to the old sync
    consent sign-in flow from the feed-related entry points.
    
    It also adds the CreatorActionDelegateImplTest test suite and a
    test for the sign-in flow's launch in the creator surface for the
    previously missing coverage.
    
    Bug: 350461111
    Change-Id: Ib9edf3f7d1c5714d71750c16e69841f71bb884eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108748
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Menghan Yang <myuu@google.com>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403281}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/FeedStream.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/FeedStream.java
@@ -341,16 +341,6 @@ public class FeedStream implements Stream {
         }
 
         @Override
-        public void showSyncConsentPrompt() {
-            if (ChromeFeatureList.isEnabled(
-                    ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS)) {
-                startSigninFlow();
-            } else {
-                mActionDelegate.showSyncConsentActivity(SigninAccessPoint.NTP_FEED_BOTTOM_PROMO);
-            }
-        }
-
-        @Override
         public void startSigninFlow() {
             mActionDelegate.startSigninFlow(SigninAccessPoint.NTP_FEED_BOTTOM_PROMO);
         }
@@ -358,9 +348,7 @@ public class FeedStream implements Stream {
         @Override
         public void showSignInInterstitial() {
             mActionDelegate.showSignInInterstitial(
-                    SigninAccessPoint.NTP_FEED_CARD_MENU_PROMO,
-                    mBottomSheetController,
-                    mWindowAndroid);
+                    SigninAccessPoint.NTP_FEED_CARD_MENU_PROMO, mBottomSheetController);
         }
 
         @Override
