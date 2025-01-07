1b635dc9d35710eabfb4248e34630ed5c31733d3
David Sanders <dsanders11@ucsbalum.com>
2025-01-06 16:42:43 -0800

    Remove unused include in //base/synchronization/waitable_event.h
    Became unused after https://chromium-review.googlesource.com/c/chromium/src/+/6097198
    
    Bug: 40318405
    Change-Id: I96d86387339943baf9e04780eb381fdd73be6f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6133682
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402732}

--- base/synchronization/waitable_event.h
+++ base/synchronization/waitable_event.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/ref_counted.h"
 #elif BUILDFLAG(IS_POSIX) || BUILDFLAG(IS_FUCHSIA)
-#include <list>
 #include <utility>
 
 #include "base/memory/ref_counted.h"
--- media/capture/video/android/video_capture_device_android.h
+++ media/capture/video/android/video_capture_device_android.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include <list>
 #include <string>
 
 #include "base/android/scoped_java_ref.h"
