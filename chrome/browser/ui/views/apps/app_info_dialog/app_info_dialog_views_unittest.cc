23bc6748ec8b2367bdd5a1d84665a714c609c9d8
Mitsuru Oshima <oshima@chromium.org>
2025-01-07 10:45:59 -0800

    Don't run DestroyedOtherProfileDoesNotCloseDialog on Cros
    It is not possible to destroy profile on the fly on CrOS, and
    I'm going to let impossible path crash.
    
    Bug: None
    Change-Id: Id041caa624bcfdf6c624c2309e0fc9bce751c43d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6151335
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403109}

--- chrome/browser/ui/views/apps/app_info_dialog/app_info_dialog_views_unittest.cc
+++ chrome/browser/ui/views/apps/app_info_dialog/app_info_dialog_views_unittest.cc
@@ -285,7 +285,6 @@ TEST_F(AppInfoDialogViewsTest, DestroyedProfileClosesDialog) {
 
   EXPECT_FALSE(widget_);
 }
-#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 // Tests that the dialog does not close when a different profile is destroyed.
 TEST_F(AppInfoDialogViewsTest, DestroyedOtherProfileDoesNotCloseDialog) {
@@ -305,6 +304,7 @@ TEST_F(AppInfoDialogViewsTest, DestroyedOtherProfileDoesNotCloseDialog) {
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(widget_);
 }
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 // Tests that clicking the View in Store link opens a browser tab and closes the
 // dialog cleanly.
