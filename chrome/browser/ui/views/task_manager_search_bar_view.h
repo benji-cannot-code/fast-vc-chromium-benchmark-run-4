993b49489834a986d5eb215a97c09d6250da16ab
Zhengzheng Liu <zhzhliu@chromium.org>
2025-01-06 16:42:07 -0800

    Chromium Task Manager - Update search bar background on mouse hover
    Description: The current hovor behavior fails to propagate from the
    search bar to the input textfield. This CL fixes this issue so the
    background color get updated correctly.
    
    Video demo : https://drive.google.com/file/d/1UHQ4uG8c3wWsm4Dj1-U1CS63o8l6RHN3/view?usp=sharing&resourcekey=0-G_yDokESRTi7mRrFHy-vBg
    Bug: b:383858712
    
    Change-Id: I5b82deab17953cdf41f34ac30ab5b1a2eb99bb0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112093
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Zhengzheng Liu <zhzhliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402731}

--- chrome/browser/ui/views/task_manager_search_bar_view.h
+++ chrome/browser/ui/views/task_manager_search_bar_view.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace task_manager {
 class TaskManagerSearchBarView : public views::View,
+                                 public views::FocusChangeListener,
                                  public views::TextfieldController {
   METADATA_HEADER(TaskManagerSearchBarView, views::View)
 
@@ -58,10 +59,14 @@ class TaskManagerSearchBarView : public views::View,
 
   // views::View:
   void OnThemeChanged() override;
-
-  // views::View:
   void OnMouseEntered(const ui::MouseEvent& event) override;
   void OnMouseExited(const ui::MouseEvent& event) override;
+  void AddedToWidget() override;
+  void RemovedFromWidget() override;
+
+  // views::FocusChangeListener:
+  void OnWillChangeFocus(View* focused_before, View* focused_now) override;
+  void OnDidChangeFocus(View* focused_before, View* focused_now) override;
 
   // views::TextfieldController:
   bool HandleKeyEvent(views::Textfield* sender,
@@ -76,6 +81,9 @@ class TaskManagerSearchBarView : public views::View,
   // Updates related fields on the Textfield.
   void UpdateTextfield();
 
+  // Update background color for the search bar based on its hover status.
+  void UpdateBackground();
+
  private:
   void OnInputChanged();
   void OnClearPressed();
@@ -87,6 +95,9 @@ class TaskManagerSearchBarView : public views::View,
 
   raw_ptr<views::Textfield> input_ = nullptr;
   raw_ptr<views::Button> clear_ = nullptr;
+
+  // Indicate if the search bar is hovered on or not.
+  bool is_hovered_ = false;
 
   base::CallbackListSubscription input_changed_subscription_;
   base::OneShotTimer input_change_notification_timer_;
