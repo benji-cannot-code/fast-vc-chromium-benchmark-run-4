4de38a35bffc76bb566272b13c2f88ea931fa137
Ayu Ishii <ayui@chromium.org>
2025-01-08 14:53:45 -0800

    CodeHealth: Clean up stale base::Feature KioskEnableImeButton
    Bug: 356234743
    Change-Id: I855b3793cf0ded77ade9d26d886eb69176b258b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111310
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403874}

--- ash/system/accessibility/floating_accessibility_controller_unittest.cc
+++ ash/system/accessibility/floating_accessibility_controller_unittest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/strings/stringprintf.h"
-#include "base/test/scoped_feature_list.h"
 #include "ui/compositor/layer.h"
 #include "ui/views/accessibility/view_accessibility.h"
 
@@ -49,11 +48,6 @@ class FloatingAccessibilityControllerTest : public AshTestBase {
 
   AccessibilityController* accessibility_controller() {
     return Shell::Get()->accessibility_controller();
-  }
-
-  void TearDown() override {
-    AshTestBase::TearDown();
-    features_.Reset();
   }
 
   FloatingAccessibilityController* controller() {
@@ -214,18 +208,7 @@ class FloatingAccessibilityControllerTest : public AshTestBase {
   std::u16string GetAccessibleNameForBubble() {
     return controller()->GetAccessibleNameForBubble();
   }
-
- protected:
-  base::test::ScopedFeatureList features_;
 };
-
-TEST_F(FloatingAccessibilityControllerTest, ImeButtonNotShowWhenDisabled) {
-  features_.InitAndDisableFeature(features::kKioskEnableImeButton);
-
-  SetUpVisibleMenu();
-
-  EXPECT_FALSE(IsButtonVisible(FloatingAccessibilityView::ButtonId::kIme));
-}
 
 TEST_F(FloatingAccessibilityControllerTest, ImeButtonShownWhenEnabled) {
   SetUpVisibleMenu();
