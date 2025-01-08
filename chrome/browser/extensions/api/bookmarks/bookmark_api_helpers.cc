5d2c70dbd1a1dfab6d9ed929496b94b3f3d6f463
James Lee <ljjlee@google.com>
2025-01-08 04:27:49 -0800

    Expose node types via bookmarks extension API
    See proposal at https://github.com/w3c/webextensions/issues/732.
    
    Bug: 383970322
    Change-Id: I008475b1c81a82e8da1dab61ca5588b5504bd107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075236
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403507}

--- chrome/browser/extensions/api/bookmarks/bookmark_api_helpers.cc
+++ chrome/browser/extensions/api/bookmarks/bookmark_api_helpers.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/bookmarks/bookmarks_error_constants.h"
 #include "chrome/common/extensions/api/bookmarks.h"
 #include "components/bookmarks/browser/bookmark_model.h"
+#include "components/bookmarks/browser/bookmark_node.h"
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/bookmarks/common/bookmark_metrics.h"
 #include "components/bookmarks/managed/managed_bookmark_service.h"
@@ -86,6 +87,20 @@ void PopulateBookmarkTreeNode(
   if (!node->date_added().is_null()) {
     out_bookmark_tree_node->date_added =
         node->date_added().InMillisecondsSinceUnixEpoch();
+  }
+
+  if (node->type() == BookmarkNode::Type::BOOKMARK_BAR) {
+    out_bookmark_tree_node->folder_type =
+        api::bookmarks::FolderType::kBookmarksBar;
+  } else if (node->type() == BookmarkNode::Type::OTHER_NODE) {
+    out_bookmark_tree_node->folder_type =
+        api::bookmarks::FolderType::kOther;
+  } else if (node->type() == BookmarkNode::Type::MOBILE) {
+    out_bookmark_tree_node->folder_type =
+        api::bookmarks::FolderType::kMobile;
+  } else if (node == managed->managed_node()) {
+    out_bookmark_tree_node->folder_type =
+        api::bookmarks::FolderType::kManaged;
   }
 
   if (bookmarks::IsDescendantOf(node, managed->managed_node())) {
