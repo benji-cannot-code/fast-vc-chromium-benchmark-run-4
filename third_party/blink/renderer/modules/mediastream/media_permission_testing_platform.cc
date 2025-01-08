a60b5dc9ee9efd4fe9968b077bd397186cb2b50c
Guido Urdaneta <guidou@chromium.org>
2025-01-08 08:27:06 -0800

    [MediaDevices] Check permission before firing devicechange event
    Bug: 387583503
    Change-Id: Iffb85a335719c263555bb4a2630da90cb756189d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6152197
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403590}

---
+++ third_party/blink/renderer/modules/mediastream/media_permission_testing_platform.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/mediastream/media_permission_testing_platform.h"
+
+#include <memory>
+
+#include "media/base/media_permission.h"
+
+namespace blink {
+
+MediaPermissionTestingPlatform::MediaPermissionTestingPlatform(
+    std::unique_ptr<media::MediaPermission> media_permission)
+    : media_permission_(std::move(media_permission)) {}
+
+media::MediaPermission*
+MediaPermissionTestingPlatform::GetWebRTCMediaPermission(WebLocalFrame*) {
+  return media_permission_.get();
+}
+
+}  // namespace blink
