3eb3bac8e01476ba259d5a9c85261c7061ea9f48
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-01-08 10:40:35 -0800

    Import wpt@42a44d890d5437994ad9f9513379ad1b1c7cdb05
    https://github.com/web-platform-tests/wpt/compare/3567a8981...42a44d890
    
    Using wpt-import in Chromium c9c8f39ec7bf39a0761cb3f67ec6b84b246bbcd4.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kojii@chromium.org:
      external/wpt/css/css-writing-modes
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I8510a1cbf369251f70ef2b19f2caca6590f7c420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6156797
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1403691}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/network/response_started/response_started_cached.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/network/response_started/response_started_cached.py
@@ -213,8 +213,6 @@ async def test_cached_revalidate(
     )
 
 
-
-
 @pytest.mark.asyncio
 async def test_page_with_cached_link_stylesheet(
     bidi_session,
@@ -543,10 +541,12 @@ async def test_page_with_cached_script_javascript(
         wait="complete",
     )
 
-    # Expect three events, one for the document and two for script javascript files.
+    # Expect two or three events, one for the document and the rest for javascript files.
+    # If the browser uses memory caching there may be only single request for the javascript files,
+    # see issue https://github.com/whatwg/html/issues/6110.
     wait = AsyncPoll(bidi_session, timeout=2)
-    await wait.until(lambda _: len(events) >= 7)
-    assert len(events) == 7
+    await wait.until(lambda _: len(events) >= 6)
+    assert len(events) >= 6
 
     # Assert only cached events after reload.
     cached_events = events[4:]
@@ -561,11 +561,12 @@ async def test_page_with_cached_script_javascript(
         expected_request={"method": "GET", "url": cached_script_js_url},
         expected_response={"url": cached_script_js_url, "fromCache": True},
     )
-    assert_response_event(
-        cached_events[2],
-        expected_request={"method": "GET", "url": cached_script_js_url},
-        expected_response={"url": cached_script_js_url, "fromCache": True},
-    )
+    if len(events) > 6:
+        assert_response_event(
+            cached_events[2],
+            expected_request={"method": "GET", "url": cached_script_js_url},
+            expected_response={"url": cached_script_js_url, "fromCache": True},
+        )
 
 
 @pytest.mark.asyncio
