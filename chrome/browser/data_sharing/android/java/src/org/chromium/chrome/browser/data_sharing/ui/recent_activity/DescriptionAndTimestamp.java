2fbfc40d062643d6d379f1f0108e924fc8abf6bf
Shakti Sahu <shaktisahu@chromium.org>
2025-01-08 10:29:58 -0800

    [Recent Activity] Elide description and timestamp
    The recent activity list items should show description, separator
    and timestamp in a single line. If the description string is too
    long, it should be elided at the end, but timestamp should be
    fully shown spanning up to the right edge of the parent container.
    On the otherhand, if the description string is too short, the
    timestamp will not span upto the right edge.
    It's difficult to achieve purely using android layout. So, it
    was rendered on the runtime by measuring the view length and
    subtracting the length required by the timestamp and separator.
    
    Also the existing title_and_description_layout.xml isn't reusable
    any more. Hence this CL makes a custom layout instead of reusing it.
    
    Screenshot: https://screenshot.googleplex.com/6pUicLcpy7NkPMc
    
    Bug: 387304832
    Change-Id: I3ce047c1dd11f385a8c42f23f9042b9763d51707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6155776
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1403683}

---
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/ui/recent_activity/DescriptionAndTimestamp.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.data_sharing.ui.recent_activity;
+
+/** Information required to render the description and timestamp line in the recent activity row. */
+class DescriptionAndTimestamp {
+    /** The description text which is the left half of the description line. */
+    public String description;
+
+    /* The separator string between description and timestamp. */
+    public String separator;
+
+    /* The timestamp text. */
+    public String timestamp;
+
+    /* The resource ID for the full description line.*/
+    public int descriptionFullTextResId;
+}
