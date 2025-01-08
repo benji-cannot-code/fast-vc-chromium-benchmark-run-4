e47fdde286e0262731dbf146fc2a21ac5296957b
Patrick Monette <pmonette@chromium.org>
2025-01-08 11:57:52 -0800

    Add PerformanceManager::GetGraph()
    Now that the performance manager lives on the main thread, it is safe
    to directly access it instead of using CallOnGraph.
    
    Change-Id: I8613416cc20eb8d4ecb1c8d3e3802be1e4595cc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102576
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403747}

--- components/performance_manager/performance_manager.cc
+++ components/performance_manager/performance_manager.cc
@@ -28,6 +28,11 @@ PerformanceManager::PerformanceManager() = default;
 PerformanceManager::~PerformanceManager() = default;
 
 // static
+Graph* PerformanceManager::GetGraph() {
+  return PerformanceManagerImpl::GetGraphImpl();
+}
+
+// static
 void PerformanceManager::CallOnGraph(const base::Location& from_here,
                                      base::OnceClosure callback) {
   DCHECK(callback);
