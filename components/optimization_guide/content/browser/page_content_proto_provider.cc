afb7b5e83c76ba9a411794dfee759a2925d97945
Khushal Sagar <khushalsagar@chromium.org>
2025-01-08 11:07:46 -0800

    optimization_guide: Add latency metrics for AnnotatedPageContent
    Bug: 387991723
    Change-Id: I72ae6c435a35dd0c684be4c4cc40a785ce157581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6149134
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403709}

--- components/optimization_guide/content/browser/page_content_proto_provider.cc
+++ components/optimization_guide/content/browser/page_content_proto_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/content/browser/page_content_proto_provider.h"
 
 #include "base/functional/concurrent_closures.h"
+#include "base/metrics/histogram_macros.h"
 #include "components/optimization_guide/content/browser/page_content_proto_util.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
@@ -49,6 +50,7 @@ std::optional<optimization_guide::RenderFrameInfo> GetRenderFrameInfo(
 }
 
 void OnGotAIPageContentForAllFrames(
+    base::TimeTicks start_time,
     content::GlobalRenderFrameHostToken main_frame_token,
     std::unique_ptr<optimization_guide::AIPageContentMap> page_content_map,
     OnAIPageContentDone done_callback) {
@@ -56,6 +58,8 @@ void OnGotAIPageContentForAllFrames(
   if (optimization_guide::ConvertAIPageContentToProto(
           main_frame_token, *page_content_map,
           base::BindRepeating(&GetRenderFrameInfo), &proto)) {
+    UMA_HISTOGRAM_TIMES("OptimizationGuide.AIPageContent.TotalLatency",
+                        base::TimeTicks::Now() - start_time);
     std::move(done_callback).Run(std::move(proto));
     return;
   }
@@ -114,7 +118,7 @@ void GetAIPageContent(content::WebContents* web_contents,
 
   std::move(concurrent)
       .Done(base::BindOnce(
-          &OnGotAIPageContentForAllFrames,
+          &OnGotAIPageContentForAllFrames, base::TimeTicks::Now(),
           web_contents->GetPrimaryMainFrame()->GetGlobalFrameToken(),
           std::move(page_content_map), std::move(done_callback)));
 }
