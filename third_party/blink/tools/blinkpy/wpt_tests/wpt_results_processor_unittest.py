53e1c379c56868a5d2f64ee75e5661f508956bb2
Jonathan Lee <jonathanjlee@google.com>
2025-01-07 17:09:48 -0800

    [wptrunner] Generate `{actual,expected}_text` with trailing newline
    `run_{web,wpt}_tests.py` should produce the exact same testharness
    output so that an `-expected.txt` generated by one runs expectedly with
    the other (either locally or a test moved between CI suites).
    
    Most testharness `-expected.txt` in the tree already include the
    trailing newline, so standardize in that direction, not without a
    trailing newline.
    
    Fixed: 383572679
    Change-Id: Id01776f614919eeabeb6eb7ec50a190ac1609192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6152660
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1403329}

--- third_party/blink/tools/blinkpy/wpt_tests/wpt_results_processor_unittest.py
+++ third_party/blink/tools/blinkpy/wpt_tests/wpt_results_processor_unittest.py
@@ -392,6 +392,7 @@ class WPTResultsProcessorTest(LoggingTestCase):
                 This is a testharness.js-based test.
                 [FAIL] fail
                 Harness: the test ran to completion.
+
                 """))
 
     def test_report_subtest_unexpected_fail(self):
@@ -601,6 +602,7 @@ class WPTResultsProcessorTest(LoggingTestCase):
                 [PRECONDITION_FAILED] subtest
                   actual-message
                 Harness: the test ran to completion.
+
                 """))
         self.assertEqual(
             self.fs.read_text_file(
@@ -612,6 +614,7 @@ class WPTResultsProcessorTest(LoggingTestCase):
                 [FAIL] subtest
                   expected-message
                 Harness: the test ran to completion.
+
                 """))
         diff_lines = self.fs.read_text_file(
             self.fs.join('/mock-checkout', 'out', 'Default',
@@ -635,6 +638,7 @@ class WPTResultsProcessorTest(LoggingTestCase):
                 This is a testharness.js-based test.
                 [FAIL] subtest
                 Harness: the test ran to completion.
+
                 """))
         with self.fs.patch_builtins():
             self._event(
@@ -655,6 +659,7 @@ class WPTResultsProcessorTest(LoggingTestCase):
                 This is a testharness.js-based test.
                 Harness Error. harness_status.status = 1 , harness_status.message = Uncaught SyntaxError: Unexpected token ')'
                 Harness: the test ran to completion.
+
                 """))
         self.assertEqual(
             self.fs.read_text_file(
@@ -665,6 +670,7 @@ class WPTResultsProcessorTest(LoggingTestCase):
                 This is a testharness.js-based test.
                 [FAIL] subtest
                 Harness: the test ran to completion.
+
                 """))
 
     def test_extract_text_all_pass(self):
