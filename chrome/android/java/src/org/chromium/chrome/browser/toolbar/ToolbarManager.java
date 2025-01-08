705a8127098eb2475d6ffebc98707385bdbd2c6f
Lijin Shen <lazzzis@google.com>
2025-01-08 10:42:27 -0800

    [DNT] Add histogram to record navigations between chrome native pages
    Add histogram to record navigations between chrome native pages.
    
    Bug: 377489768
    Change-Id: Icee9c286a766a8f1cdb75a5e0abbd28a141aacbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6149613
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1403693}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -1197,6 +1197,10 @@ public class ToolbarManager
                             Tab tab, NavigationHandle navigationHandle) {
                         assert tab == mLocationBarModel.getTab();
                         mStartNavDuringOngoingGesture |= mBackGestureInProgress;
+                        BackPressMetrics.recordNavigateBetweenChromeNativePages(
+                                UrlUtilities.isChromeNativeUrl(tab.getUrl())
+                                        && UrlUtilities.isChromeNativeUrl(
+                                                navigationHandle.getUrl()));
                         onBackPressStateChanged();
                         mLocationBarModel.notifyDidStartNavigation(
                                 navigationHandle.isSameDocument());
