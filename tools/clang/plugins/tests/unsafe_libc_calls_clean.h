0e11d936c095a6db4676b0d3c83a78be81fd84a4
Tom Sepez <tsepez@chromium.org>
2025-01-08 14:55:25 -0800

    Introduce pragma allow_unsafe_libc_calls to clang plugin.
    This new pragma will suppress forthcoming libc call warnings while
    still enforcing the current unsafe-buffers warnings in files.
    
    Update tests scripts to invoke the plugin using the libc checking
    flags, and add tests for these cases.
    
    Change-Id: Ide7fe1f82a929c1b3fcb304b6ccb809e01b2c676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6155196
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403875}

---
+++ tools/clang/plugins/tests/unsafe_libc_calls_clean.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_CLANG_PLUGINS_TESTS_UNSAFE_LIBC_CALLS_CLEAN_H_
+#define TOOLS_CLANG_PLUGINS_TESTS_UNSAFE_LIBC_CALLS_CLEAN_H_
+
+#include <string.h>
+
+void* clean_bad_memcpy(int* i, unsigned s) {
+  return memcpy(i, &s, sizeof(s));  // Should generate a warning.
+}
+
+void* clean_guarded_bad_memcpy(int* i, unsigned s) {
+  return UNSAFE_BUFFERS(memcpy(i, &s, sizeof(s))); // Guarded so no warning.
+}
+
+#endif  // TOOLS_CLANG_PLUGINS_TESTS_UNSAFE_LIBC_CALLS_CLEAN_H_
