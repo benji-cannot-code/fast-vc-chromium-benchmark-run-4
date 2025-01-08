0d9ceb4006d41ade6f7888381d470b9756f6adb7
Vishwas Uppoor <vishwasuppoor@google.com>
2025-01-08 12:04:42 -0800

    [PIX] Auto-detection cleanup - Phase 6
    Cleanup Pix detection feature flags.
    
    Bug: 362780542, 362781719
    Change-Id: Ia1b3edd0d1df3a5cb0a30451f224d75d49ba5594
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6146055
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1403756}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2623,12 +2623,6 @@ extern const char kEducationalTipModuleDescription[];
 extern const char kEnableCommandLineOnNonRootedName[];
 extern const char kEnableCommandLineOnNoRootedDescription[];
 
-extern const char kEnablePixDetectionName[];
-extern const char kEnablePixDetectionDescription[];
-
-extern const char kEnablePixDetectionOnDomContentLoadedName[];
-extern const char kEnablePixDetectionOnDomContentLoadedDescription[];
-
 extern const char kEnableClipboardDataControlsAndroidName[];
 extern const char kEnableClipboardDataControlsAndroidDescription[];
 
--- components/facilitated_payments/core/features/features.cc
+++ components/facilitated_payments/core/features/features.cc
@@ -7,18 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments::facilitated {
 
-// When enabled, Chrome will detect PIX codes on allow-listed merchant websites.
-BASE_FEATURE(kEnablePixDetection,
-             "EnablePixDetection",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-// When enabled, Chrome will use `WebContentsObserver::DOMContentLoaded` event
-// as the trigger for PIX code detection instead of
-// `WebContentsObserver::DidDinishLoad`.
-BASE_FEATURE(kEnablePixDetectionOnDomContentLoaded,
-             "EnablePixDetectionOnDomContentLoaded",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // When enabled, Chrome will offer to pay with accounts supporting Pix.
 BASE_FEATURE(kEnablePixPayments,
              "EnablePixPayments",
