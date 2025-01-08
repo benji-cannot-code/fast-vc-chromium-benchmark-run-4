cf83eacc357b20aa241eba404b4b1ab5c6c05dbe
Tzarial <zork@google.com>
2025-01-08 10:16:37 -0800

    [Read anything] Reload images when toggling links.
    Toggling links recreates nodes that are links, resulting in images
    needed to be reloaded.
    
    Fixed: 361846125
    Change-Id: I91a20123336152ebbebc4b8bb9467edc06750709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6156115
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Tzarial <zork@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403678}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -856,6 +856,7 @@ export class AppElement extends AppElementBase {
     if (shouldRehighlightCurrentNodes && originallyHadHighlights) {
       this.highlightCurrentGranularity(chrome.readingMode.getCurrentText());
     }
+    this.loadImages_();
   }
 
   protected updateImages_() {
