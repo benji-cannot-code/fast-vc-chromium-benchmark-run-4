cbeefddcdc351ed81d60bbcf5e7275ffbf5a8332
Stepan Khapugin <stkhapugin@chromium.org>
2025-01-08 09:45:16 -0800

    Revert "[iOS][LO] Load related searches in bottom sheet and reset selection"
    This reverts commit 9036501137b419fc1fe11d418547f3ae0bcd7d94.
    
    Reason for revert: Introduces a crash
    
    Original change's description:
    > [iOS][LO] Load related searches in bottom sheet and reset selection
    >
    > Bug: 368016564
    > Change-Id: Ie6bfe6cab7e5d41085d27650b27519900a69ef77
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097933
    > Reviewed-by: Radu Nitescu <radunitescu@google.com>
    > Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Jason Hu <hujasonx@google.com>
    > Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1402316}
    
    Bug: 368016564
    Change-Id: I9fa90e99522b00c696a566bb5a2bd98554b7396b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148140
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1403655}

--- ios/chrome/browser/lens_overlay/coordinator/lens_result_page_mediator_unittest.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_result_page_mediator_unittest.mm
@@ -57,11 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _openInNewTabRequested = YES;
 }
 
-- (void)lensResultPageWillLoadNonLensSRP:(NSString*)queryText
-                                     url:(const GURL&)destinationURL {
-  // NO-OP
-}
-
 @end
 
 @interface LensResultPageMediator (Testing)
