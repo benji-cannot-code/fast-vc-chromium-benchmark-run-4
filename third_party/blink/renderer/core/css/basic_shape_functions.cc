755e2e0b5c501d8dd93b98161b27b951e39310dc
Noam Rosenthal <nrosenthal@chromium.org>
2025-01-08 05:55:29 -0800

    CSS shape() - support close
    Bug: 384870259
    Change-Id: I450f26f45ed09bfa1fd8eefc611edc68ce8ec562
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6157135
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403541}

--- third_party/blink/renderer/core/css/basic_shape_functions.cc
+++ third_party/blink/renderer/core/css/basic_shape_functions.cc
@@ -126,6 +126,8 @@ StyleShape::Segment ShapeCommandToShapeSegment(
               CSSValueIDToShapeSegmentOrigin(command.GetEndPointOrigin()),
           .end_point = StyleBuilderConverter::ConvertPosition(
               state, command.GetEndPoint())};
+    case CSSValueID::kClose:
+      return StyleShape::Segment{.type = StyleShape::Segment::kClose};
     default:
       NOTREACHED();
   }
@@ -146,6 +148,9 @@ const cssvalue::CSSShapeCommand* ShapeSegmentToShapeCommand(
           CSSValueID::kLine,
           ShapeSegmentOriginToCSSValueID(segment.end_point_origin),
           LengthPointToCSSValue(segment.end_point, zoom));
+    case blink::StyleShape::Segment::Type::kClose:
+      return MakeGarbageCollected<const cssvalue::CSSShapeCommand>(
+          CSSValueID::kClose);
   }
 }
 
--- third_party/blink/renderer/core/style/style_shape.cc
+++ third_party/blink/renderer/core/style/style_shape.cc
@@ -50,6 +50,9 @@ void StyleShape::ResolvePath(Path& path, const gfx::SizeF& box_size) const {
                      : SVGPathSegType::kPathSegLineToRel,
              .target_point = PointForLengthPoint(segment.end_point, box_size)});
         break;
+      case Segment::Type::kClose:
+        builder.EmitSegment({.command = SVGPathSegType::kPathSegClosePath});
+        break;
     }
   }
 }
