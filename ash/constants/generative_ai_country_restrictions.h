3227173cfee86991b1b90a8e6d33a72d7230fb14
Howard Yang <hcyang@google.com>
2025-01-07 17:43:39 -0800

    Move GenAI location allowlist to ash/constants
    The allowlist used in the editor feature can be used by other GenAI
    features. We want to put it in ash/constants header so that other
    features can use that helper function more conveniently.
    
    Bug: b:385832801
    Test: CQ
    Change-Id: I1b4f125c2451b749c16d8a3ad4cfe465b3739879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6128739
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Howard Yang <hcyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1403339}

---
+++ ash/constants/generative_ai_country_restrictions.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_CONSTANTS_GENERATIVE_AI_COUNTRY_RESTRICTIONS_H_
+#define ASH_CONSTANTS_GENERATIVE_AI_COUNTRY_RESTRICTIONS_H_
+
+#include <string>
+
+#include "base/component_export.h"
+
+namespace ash {
+
+// If the country code isn't recognized, false will be returned.
+COMPONENT_EXPORT(ASH_CONSTANTS)
+bool IsGenerativeAiAllowedForCountry(std::string_view country_code);
+
+}  // namespace ash
+
+#endif  // ASH_CONSTANTS_GENERATIVE_AI_COUNTRY_RESTRICTIONS_H_
