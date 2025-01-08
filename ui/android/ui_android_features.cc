36cf5f6b3f2456bfc66a6e16a3207c5d4cfd9ebb
Min Qin <qinmin@chromium.org>
2025-01-08 14:27:14 -0800

    Disable Caller permission check due to framework issues
    The permission check is returning false on some Android builds,
    we suspect it is caused by either framework or photo pickers.
    This CL temporarily disables the permission check, so photo
    upload will work for those Android builds
    
    Bug: 383610801,388432387,281539662
    Change-Id: I0950c2ae1c1d9ae38d403ac3749005c198afb48d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6158591
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403856}

--- ui/android/ui_android_features.cc
+++ ui/android/ui_android_features.cc
@@ -44,7 +44,7 @@ BASE_FEATURE(kSendTouchMovesToEventForwarderObservers,
 
 BASE_FEATURE(kCheckIntentCallerPermission,
              "CheckIntentCallerPermission",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDisablePhotoPickerForVideoCapture,
              "DisablePhotoPickerForVideoCapture",
