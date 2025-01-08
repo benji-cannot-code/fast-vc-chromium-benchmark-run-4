18cb40d6caf3076477bcc4ed6dbb1a5610fd512c
Tom Sepez <tsepez@chromium.org>
2025-01-08 14:46:33 -0800

    Remove unused pragma check_unsafe_buffers from clang plugin.
    This is now enforced by default, so there is no need to opt in.
    
    Change-Id: Iabfe9683b8b685b30dde71173c9994f3c1af2ca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6155215
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403870}

--- tools/clang/plugins/tests/unsafe_buffers.cpp
+++ tools/clang/plugins/tests/unsafe_buffers.cpp
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "unsafe_buffers_not_clean_dir/clean_header.h"
 #include "unsafe_buffers_not_clean_dir/not_checked_header.h"
 #include "unsafe_buffers_not_clean_dir/not_clean_header.h"
-#include "unsafe_buffers_not_clean_dir/opt_in_header.h"
 #include "unsafe_buffers_opt_out.h"
 
 int main() {
