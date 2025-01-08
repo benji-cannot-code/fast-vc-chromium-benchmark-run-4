3d720d27f9fd669a139efa246d6a62c37ee2d723
Miyoung shin <myid.shin@igalia.com>
2025-01-07 21:50:35 -0800

    Resolve DCHECK failure when running CustomizeToolbarHandlerTest.*
    The global action map will occur DCHECK failure if it has duplicate
    values. The action map's values are added when initializing in
    CustomizeToolbarHandlerTest::SetUp, but this is problematic because
    the previous values will remain when the next test is run in the same
    process. So, this patch fixes this issue by resetting the action map
    in TearDown().
    
    Bug: 387892109
    Change-Id: I6c46c8f5c927fb9ba5f7e6c2378e9aa56abbfbab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6144673
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1403401}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler_unittest.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler_unittest.cc
@@ -130,6 +130,7 @@ class CustomizeToolbarHandlerTest : public BrowserWithTestWindowTest {
     pinned_toolbar_actions_model_observer_ = nullptr;
     handler_.reset();
     mock_pinned_toolbar_actions_model_ = nullptr;
+    actions::ActionIdMap::ResetMapsForTesting();
 
     BrowserWithTestWindowTest::TearDown();
   }
