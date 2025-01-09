4c436c5b1f195233fb389734e068828eb1b0dd04
Khushal Sagar <khushalsagar@chromium.org>
2025-01-09 10:11:00 -0800

    blink: Remove redundant content nodes from visibility:hidden
    Remove subtrees which are hidden via `visibility: hidden`. This is an
    inherited property so in the common case it hides the entire subtree
    when set on a node. But the property can be overridden by descendants.
    So we still walk the subtree for nodes with `visibility: hidden`. The
    layout structure for the ancestor chain is preserved if it has visible
    content, even if the ancestors themselves are hidden.
    
    Fixed: 384950058
    Change-Id: I8b68f12e874be4392d739daa8751654736904b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6152798
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404276}

--- third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
+++ third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
@@ -208,9 +208,9 @@ std::optional<DOMNodeId> GetNodeId(const LayoutObject& object) {
   return DOMNodeIds::IdForNode(node);
 }
 
-bool ShouldSkipContent(const LayoutObject& object) {
+bool IsVisible(const LayoutObject& object) {
   // Don't add content when node is invisible.
-  return object.Style()->Visibility() != EVisibility::kVisible;
+  return object.Style()->Visibility() == EVisibility::kVisible;
 }
 
 bool ShouldSkipSubtree(const LayoutObject& object) {
@@ -253,7 +253,7 @@ void ProcessTextNode(const LayoutText& layout_text,
                      mojom::blink::AIPageContentAttributes& attributes,
                      const ComputedStyle& document_style) {
   attributes.attribute_type = mojom::blink::AIPageContentAttributeType::kText;
-  CHECK(!ShouldSkipContent(layout_text));
+  CHECK(IsVisible(layout_text));
 
   auto text_style = mojom::blink::AIPageContentTextStyle::New();
   text_style->text_size = GetTextSize(*layout_text.Style(), document_style);
@@ -269,7 +269,7 @@ void ProcessImageNode(const LayoutImage& layout_image,
                       mojom::blink::AIPageContentAttributes& attributes,
                       const ComputedStyle& document_style) {
   attributes.attribute_type = mojom::blink::AIPageContentAttributeType::kImage;
-  CHECK(!ShouldSkipContent(layout_image));
+  CHECK(IsVisible(layout_image));
 
   if (DynamicTo<LayoutMedia>(layout_image)) {
     return;
@@ -292,7 +292,7 @@ void ProcessAnchorNode(const HTMLAnchorElement& anchor_element,
                        mojom::blink::AIPageContentAttributes& attributes,
                        const ComputedStyle& document_style) {
   attributes.attribute_type = mojom::blink::AIPageContentAttributeType::kAnchor;
-  if (ShouldSkipContent(*anchor_element.GetLayoutObject())) {
+  if (!IsVisible(*anchor_element.GetLayoutObject())) {
     return;
   }
 
@@ -308,7 +308,7 @@ void ProcessTableNode(const LayoutTable& layout_table,
                       mojom::blink::AIPageContentAttributes& attributes,
                       const ComputedStyle& document_style) {
   attributes.attribute_type = mojom::blink::AIPageContentAttributeType::kTable;
-  if (ShouldSkipContent(layout_table)) {
+  if (!IsVisible(layout_table)) {
     return;
   }
 
@@ -350,7 +350,7 @@ void ProcessTableRowNode(const LayoutTableRow& layout_table_row,
                          const ComputedStyle& document_style) {
   attributes.attribute_type =
       mojom::blink::AIPageContentAttributeType::kTableRow;
-  if (ShouldSkipContent(layout_table_row)) {
+  if (!IsVisible(layout_table_row)) {
     return;
   }
 
@@ -480,20 +480,24 @@ mojom::blink::AIPageContentPtr AIPageContentAgent::GetAIPageContentSync()
   return page_content;
 }
 
-void AIPageContentAgent::WalkChildren(
+bool AIPageContentAgent::WalkChildren(
     const LayoutObject& object,
     mojom::blink::AIPageContentNode& content_node,
     const ComputedStyle& document_style) const {
   if (object.ChildPrePaintBlockedByDisplayLock()) {
-    return;
+    return false;
   }
 
+  bool has_visible_content = false;
   for (auto* child = object.SlowFirstChild(); child;
        child = child->NextSibling()) {
     if (ShouldSkipSubtree(*child)) {
       continue;
     }
 
+    has_visible_content |= IsVisible(*child);
+
+    bool child_has_visible_content = false;
     auto child_content_node = MaybeGenerateContentNode(*child, document_style);
     if (child_content_node &&
         child_content_node->content_attributes->attribute_type ==
@@ -502,18 +506,26 @@ void AIPageContentAgent::WalkChildren(
     } else {
       auto& node_for_child =
           child_content_node ? *child_content_node : content_node;
-      WalkChildren(*child, node_for_child, document_style);
+      child_has_visible_content =
+          WalkChildren(*child, node_for_child, document_style);
+      has_visible_content |= child_has_visible_content;
     }
 
-    if (child_content_node) {
+    const bool should_add_node_for_child =
+        IsVisible(*child) || child_has_visible_content;
+    if (should_add_node_for_child && child_content_node) {
       content_node.children_nodes.emplace_back(std::move(child_content_node));
     }
   }
+
+  return has_visible_content;
 }
 
 void AIPageContentAgent::ProcessIframe(
     const LayoutIFrame& object,
     mojom::blink::AIPageContentNode& content_node) const {
+  CHECK(IsVisible(object));
+
   content_node.content_attributes->attribute_type =
       mojom::blink::AIPageContentAttributeType::kIframe;
 
@@ -533,6 +545,10 @@ void AIPageContentAgent::ProcessIframe(
     auto child_content_node = MaybeGenerateContentNode(
         *child_layout_view, *child_layout_view->Style());
     CHECK(child_content_node);
+
+    // We could consider removing an iframe with no visible content. But this is
+    // likely not common and should be done in the browser so it's consistently
+    // done for local and remote frames.
     WalkChildren(*child_layout_view, *child_content_node,
                  *child_layout_view->Style());
     content_node.children_nodes.emplace_back(std::move(child_content_node));
@@ -552,20 +568,25 @@ mojom::blink::AIPageContentNodePtr AIPageContentAgent::MaybeGenerateContentNode(
   // requires it.
   auto* element = DynamicTo<HTMLElement>(object.GetNode());
   if (const auto* iframe = GetIFrame(object)) {
+    // If the `iframe` is invisible, it's Document can't override this and must
+    // also be invisible.
+    if (!IsVisible(object)) {
+      return nullptr;
+    }
     ProcessIframe(*iframe, *content_node);
   } else if (object.IsLayoutView()) {
     attributes.attribute_type = mojom::blink::AIPageContentAttributeType::kRoot;
   } else if (object.IsText()) {
     // Since text is a leaf node, do not create a content node if should skip
     // content.
-    if (ShouldSkipContent(object)) {
+    if (!IsVisible(object)) {
       return nullptr;
     }
     ProcessTextNode(To<LayoutText>(object), attributes, document_style);
   } else if (object.IsLayoutImage()) {
     // Since image is a leaf node, do not create a content node if should skip
     // content.
-    if (ShouldSkipContent(object)) {
+    if (!IsVisible(object)) {
       return nullptr;
     }
     ProcessImageNode(To<LayoutImage>(object), attributes, document_style);
