d71e9904afaf80b747faa285db97ea5351915221
EmiliaPaz <emiliapaz@chromium.org>
2025-01-09 08:23:01 -0800

    [Extensions] Use util methods in scripting API sub frames test
    Bug: n/a
    Change-Id: Ie7913498fd3c1768180bd0cc46616251354ee519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6125313
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404209}

--- chrome/test/data/extensions/api_test/scripting/sub_frames/worker.js
+++ chrome/test/data/extensions/api_test/scripting/sub_frames/worker.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {getSingleTab} from '/_test_resources/test_util/tabs_util.js';
+import {findDocumentIdWithHostname, findFrameIdWithHostname, findFrameWithHostname, getFramesInTab, getSingleTab} from '/_test_resources/test_util/tabs_util.js';
 
 function injectedFunction() {
   return location.href;
@@ -15,32 +15,6 @@ function getAccessError(url) {
   return `Error: Cannot access contents of url "${url}". ` +
       'Extension manifest must request permission ' +
       'to access this host.';
-}
-
-// Returns all frames in the given tab.
-async function getFramesInTab(tabId) {
-  const frames = await chrome.webNavigation.getAllFrames({tabId: tabId});
-  chrome.test.assertTrue(frames.length > 0);
-  return frames;
-}
-
-// Returns the frame with the given `hostname`.
-function findFrameWithHostname(frames, hostname) {
-  const frame = frames.find(frame => {
-    return (new URL(frame.url)).hostname == hostname;
-  });
-  chrome.test.assertTrue(!!frame, 'No frame with hostname: ' + hostname);
-  return frame;
-}
-
-// Returns the ID of the frame with the given `hostname`.
-function findFrameIdWithHostname(frames, hostname) {
-  return findFrameWithHostname(frames, hostname).frameId;
-}
-
-// Returns the ID of the document with the given `hostname`.
-function findDocumentIdWithHostname(frames, hostname) {
-  return findFrameWithHostname(frames, hostname).documentId;
 }
 
 chrome.test.runTests([
