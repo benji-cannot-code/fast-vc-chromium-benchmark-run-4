2973cb35592b122fca9ef3f6ff56578c45d73c74
François Doray <fdoray@chromium.org>
2025-01-09 09:17:51 -0800

    Disable UrgentPageDiscardingPolicy in BFCachePolicyBrowserTest.
    Instead of disabling PageDiscarder. This is because when
    DiscardAttemptMarker is ignored, PageDiscardingHelper
    will attempt to discard the same tab in a loop (failing each time to
    discard because PageDiscarded is disabled).
    
    Bug: 365722775
    Change-Id: I0dc20c58cf64a4388eb91a9e20168d1059bfb03d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164285
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404242}

--- chrome/browser/performance_manager/policies/bfcache_policy_browsertest.cc
+++ chrome/browser/performance_manager/policies/bfcache_policy_browsertest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/memory_pressure_listener.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
-#include "chrome/browser/performance_manager/mechanisms/page_discarder.h"
+#include "chrome/browser/performance_manager/policies/urgent_page_discarding_policy.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -124,7 +124,7 @@ class BFCachePolicyBrowserTest
     host_resolver()->AddRule("*", "127.0.0.1");
     ASSERT_TRUE(embedded_test_server()->Start());
     // Disable tab discarding to avoid interference.
-    mechanism::PageDiscarder::DisableForTesting();
+    policies::UrgentPageDiscardingPolicy::DisableForTesting();
   }
 
  protected:
