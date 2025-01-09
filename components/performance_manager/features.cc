602ace72630e8e6f67b3488a5bd9bd1ec2b22934
Keita Suzuki <suzukikeita@chromium.org>
2025-01-09 02:59:51 -0800

    Make SeamlessRenderFrameSwap feature to enable by default
    This CL updates the `SeamlessRenderFrameSwap` feature to be enabled
    by default.
    
    Report (internal): https://docs.google.com/document/d/1Ql-3YuXQGHOJ4lQZ-DduufCgssMOPTqjvQ_rbEG98Uc/edit?resourcekey=0-Gko_YNdPm8IKHre_4XjRMw&tab=t.0#heading=h.53acks0nurd
    Thread (internal): http://g/search-chrome/PYGUUO0PZcA/zI_VPtqbCAAJ
    
    Change-Id: Ide9ea68b23d2ff7c0c4ed7b9a053f2a56268e8f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161274
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404099}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -195,7 +195,7 @@ BASE_FEATURE(kResourceAttributionIncludeOrigins,
 
 BASE_FEATURE(kSeamlessRenderFrameSwap,
              "SeamlessRenderFrameSwap",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUnimportantFramesPriority,
              "UnimportantFramesPriority",
