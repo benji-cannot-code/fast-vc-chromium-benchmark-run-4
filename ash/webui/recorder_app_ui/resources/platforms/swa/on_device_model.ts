56214ae5b239cb3b01c49925dd6f154759c52cfb
yuanchieh <yuanchieh@google.com>
2025-01-08 21:39:33 -0800

    recorder: chunk content before input into T&S model
    To prevent out-of-memory errors when processing long input sequences
    with the T&S model (observed with inputs exceeding 8k tokens),
    this change implements content chunking.
    
    Instead of processing the entire input at once, the content is divided
    into smaller chunks, and safety is verified individually for each chunk.
    This approach mitigates excessive memory consumption by the T&S model,
    particularly for longer inputs.
    
    Testing on Navi with 11k tokens input, safety check execution time after chunking is around 2 seconds.
    
    BUG=b:385078550
    TEST=manual; test on brya with Xss model & rauru with Xs model
    
    Change-Id: I3bd1ebe2031fd8603d0deaa2fab30de1e21445ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108466
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Yuan-Chieh Cheng <yuanchieh@google.com>
    Reviewed-by: Jennifer Ling <hsuanling@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404001}

--- ash/webui/recorder_app_ui/resources/platforms/swa/on_device_model.ts
+++ ash/webui/recorder_app_ui/resources/platforms/swa/on_device_model.ts
@@ -17,6 +17,9 @@ import {
   assertExists,
   assertNotReached,
 } from '../../core/utils/assert.js';
+import {
+  chunkContentByWord,
+} from '../../core/utils/utils.js';
 
 import {PlatformHandler} from './handler.js';
 import {
@@ -42,6 +45,13 @@ function parseResponse(res: string): string {
 
 // The minimum transcript token length for title generation and summarization.
 const MIN_TOKEN_LENGTH = 200;
+
+// The maximum content input length for T&S model.
+// Based on tests, 1k input tokens is the most efficient.
+// According to the Gemini tokenizer documentation
+// (https://ai.google.dev/gemini-api/docs/tokens), 100 tokens are roughly
+// equivalent to 60-80 English words, chose 700 since it is average.
+const MAX_TS_MODEL_INPUT_WORD_LENGTH = 700;
 
 abstract class OnDeviceModel<T> implements Model<T> {
   constructor(
@@ -141,17 +151,27 @@ abstract class OnDeviceModel<T> implements Model<T> {
   private async contentIsUnsafe(
     content: string,
     safetyFeature: SafetyFeature,
+    language: LanguageCode,
   ): Promise<boolean> {
-    const {safetyInfo} = await this.remote.classifyTextSafety(content);
-    if (safetyInfo === null) {
-      return false;
+    // Split the content into chunks due to model performance considerations.
+    const contentChunks =
+      chunkContentByWord(content, MAX_TS_MODEL_INPUT_WORD_LENGTH, language);
+    for (const chunk of contentChunks) {
+      const {safetyInfo} = await this.remote.classifyTextSafety(chunk);
+      if (safetyInfo === null) {
+        continue;
+      }
+
+      const {isSafe} = await this.pageRemote.validateSafetyResult(
+        safetyFeature,
+        chunk,
+        safetyInfo,
+      );
+      if (!isSafe) {
+        return true;
+      }
     }
-    const {isSafe} = await this.pageRemote.validateSafetyResult(
-      safetyFeature,
-      content,
-      safetyInfo,
-    );
-    return !isSafe;
+    return false;
   }
 
   close(): void {
@@ -184,20 +204,25 @@ abstract class OnDeviceModel<T> implements Model<T> {
     responseSafetyFeature: SafetyFeature,
     fields: Record<string, string>,
     session: SessionRemote,
+    language: LanguageCode,
   ): Promise<ModelResponse<string>> {
     const prompt = await this.formatInput(formatFeature, fields);
     if (prompt === null) {
       console.error('formatInput returns null, wrong model?');
       return {kind: 'error', error: ModelResponseError.GENERAL};
     }
-    if (await this.contentIsUnsafe(prompt, requestSafetyFeature)) {
+    if (await this.contentIsUnsafe(prompt, requestSafetyFeature, language)) {
       return {kind: 'error', error: ModelResponseError.UNSAFE};
     }
     const response = await this.executeRaw(prompt, session);
     if (response.kind === 'error') {
       return response;
     }
-    if (await this.contentIsUnsafe(response.result, responseSafetyFeature)) {
+    if (await this.contentIsUnsafe(
+          response.result,
+          responseSafetyFeature,
+          language,
+        )) {
       return {kind: 'error', error: ModelResponseError.UNSAFE};
     }
     return {kind: 'success', result: response.result};
@@ -228,6 +253,7 @@ export class SummaryModel extends OnDeviceModel<string> {
         bullet_points_request: bulletPointsRequest,
       },
       session,
+      language,
     );
     // TODO(pihsun): `Result` monadic helper class?
     if (resp.kind === 'error') {
@@ -261,7 +287,7 @@ export class TitleSuggestionModel extends OnDeviceModel<string[]> {
   // For title suggestion, model input only needs transcription.
   override async executeInRemoteSession(
     content: string,
-    _: LanguageCode,
+    language: LanguageCode,
     session: SessionRemote,
   ): Promise<ModelResponse<string[]>> {
     const resp = await this.formatAndExecute(
@@ -270,6 +296,7 @@ export class TitleSuggestionModel extends OnDeviceModel<string[]> {
       SafetyFeature.kAudioTitleResponse,
       {transcription: content},
       session,
+      language,
     );
     if (resp.kind === 'error') {
       return resp;
