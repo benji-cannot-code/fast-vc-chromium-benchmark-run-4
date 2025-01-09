c74a3dc861f16848184b2550b308c7b181865f90
Hans Wennborg <hans@chromium.org>
2025-01-09 08:34:09 -0800

    Revert "Introduce pragma allow_unsafe_libc_calls to clang plugin."
    This reverts commit d812c37e096cb28f0de23cccd0896b7abeee73a3.
    
    Reason for revert:
    Broke the plugin tests on mac, see bug.
    
    Original change's description:
    > Introduce pragma allow_unsafe_libc_calls to clang plugin.
    >
    > This new pragma will suppress forthcoming libc call warnings while
    > still enforcing the current unsafe-buffers warnings in files.
    >
    > Update tests scripts to invoke the plugin using the libc checking
    > flags, and add tests for these cases.
    >
    > Change-Id: Ide7fe1f82a929c1b3fcb304b6ccb809e01b2c676
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6155196
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Tom Sepez <tsepez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1403875}
    
    Bug: 388616196
    Change-Id: If0b450d24770bcd34bad817eb4f23c93dda475df
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164005
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1404215}

--- tools/clang/plugins/tests/unsafe_buffers.cpp
+++ tools/clang/plugins/tests/unsafe_buffers.cpp
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "unsafe_buffers_not_clean_dir/not_checked_header.h"
 #include "unsafe_buffers_not_clean_dir/not_clean_header.h"
 #include "unsafe_buffers_opt_out.h"
-#include "unsafe_libc_calls_clean.h"
-#include "unsafe_libc_calls_not_clean.h"
-#include "unsafe_libc_calls_opt_out.h"
 
 int main() {
   call_unsafe_stuff();
