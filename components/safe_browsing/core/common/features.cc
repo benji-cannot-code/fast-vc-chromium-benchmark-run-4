c9c01a3d5ea0d8a47117890011b5aa8694ce1ef6
Seung Jae Lim <andysjlim@chromium.org>
2025-01-09 14:19:27 -0800

    Trigger LLM when LlamaForcedTriggerInfo has intelligent_scan to true
    Bug: 388290797
    Change-Id: Ifffad5b78f61c09950f1f061acb6033c75ffd590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164463
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404433}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -57,6 +57,10 @@ BASE_FEATURE(kClientSideDetectionKillswitch,
              "ClientSideDetectionKillswitch",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kClientSideDetectionLlamaForcedTriggerInfoForScamDetection,
+             "ClientSideDetectionLlamaForcedTriggerInfoForScamDetection",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kClientSideDetectionKeyboardPointerLockRequest,
              "ClientSideDetectionKeyboardPointerLockRequest",
              base::FEATURE_ENABLED_BY_DEFAULT);
