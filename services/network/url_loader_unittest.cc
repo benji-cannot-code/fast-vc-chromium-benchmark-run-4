a4ea1e12ba3d337340bb2746e609a2295542b368
Anthony Vallée-Dubois <anthonyvd@google.com>
2025-01-09 06:59:30 -0800

    Attempt to deflake URLLoaderTest.ResourceSchedulerIntegration
    Bug: 333723898
    Change-Id: Ifdfdd64f55881f05d09cb7a5a840d127108e02e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6158178
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404163}

--- services/network/url_loader_unittest.cc
+++ services/network/url_loader_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/gtest_util.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
@@ -3929,13 +3930,7 @@ TEST_F(URLLoaderTest, CertStatusOnResponse) {
 }
 
 // Verifies if URLLoader works well with ResourceScheduler.
-// TODO(crbug.com/333723898): enable this test.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_ResourceSchedulerIntegration DISABLED_ResourceSchedulerIntegration
-#else
-#define MAYBE_ResourceSchedulerIntegration ResourceSchedulerIntegration
-#endif
-TEST_F(URLLoaderTest, MAYBE_ResourceSchedulerIntegration) {
+TEST_F(URLLoaderTest, ResourceSchedulerIntegration) {
   // ResourceScheduler limits the number of connections for the same host
   // by 6.
   constexpr int kRepeat = 6;
@@ -3972,12 +3967,12 @@ TEST_F(URLLoaderTest, MAYBE_ResourceSchedulerIntegration) {
     loaders.emplace_back(std::move(url_loader), std::move(loader_remote));
   }
 
-  base::RunLoop().RunUntilIdle();
-  for (const auto& pair : loaders) {
-    URLLoader* loader = pair.first.get();
-    ASSERT_NE(loader, nullptr);
-    EXPECT_EQ(net::LOAD_STATE_WAITING_FOR_RESPONSE, loader->GetLoadState());
-  }
+  ASSERT_TRUE(base::test::RunUntil([&]() {
+    return std::ranges::all_of(loaders, [](const auto& pair) {
+      return pair.first->GetLoadState() == net::LOAD_STATE_WAITING_FOR_RESPONSE;
+    });
+  })) << "Timeout waiting for all loaders to be in state "
+         "net::LOAD_STATE_WAITING_FOR_RESPONSE";
 
   mojo::PendingRemote<mojom::URLLoader> loader_remote;
   std::unique_ptr<URLLoader> loader = URLLoaderOptions().MakeURLLoader(
