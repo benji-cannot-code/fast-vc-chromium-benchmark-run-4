24a8d3b0d00a59db33a7af4f05ed7eabd89fb8fe
Greg Thompson <grt@chromium.org>
2025-01-08 23:20:30 -0800

    [system tracing] Add a flag for WindowsSystemTracing
    This allows users to opt-in to running the system tracing service when
    capturing traces. It is a convenience over manually adding
    "--enable-features=WindowsSystemTracing" to the browser's command line.
    
    Bug: 41497361
    Change-Id: I5e10535f3fb2969a70c7710d111070caa680d927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148131
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404028}

---
+++ chrome/browser/tracing/tracing_features.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_TRACING_TRACING_FEATURES_H_
+#define CHROME_BROWSER_TRACING_TRACING_FEATURES_H_
+
+#include "base/feature_list.h"
+#include "build/build_config.h"
+
+#if BUILDFLAG(IS_WIN)
+// A Feature to selectively enable connecting to the Windows system tracing
+// service when the tracing service is started.
+BASE_DECLARE_FEATURE(kWindowsSystemTracing);
+#endif  // BUILDFLAG(IS_WIN)
+
+#endif  // CHROME_BROWSER_TRACING_TRACING_FEATURES_H_
