1819f4b00031bd270da2f09490554bbe9c9abd15
Sean Kau <skau@chromium.org>
2025-01-09 15:37:13 -0800

    Remove DEPS on chrome/common
    Dependencies are not needed. Additionally, clean up the BUILD.
    
    Bug: b:346593904
    Change-Id: I5618ef855fdb9fa6e5f1d7635736d8809d9a4a09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6159249
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404479}

--- chrome/browser/ash/child_accounts/family_user_parental_control_metrics_unittest.cc
+++ chrome/browser/ash/child_accounts/family_user_parental_control_metrics_unittest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/child_accounts/time_limits/app_types.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/prefs/scoped_user_pref_update.h"
