2e37e5f4781c1c2c8edb916e47571c8ced54a876
Zekun Jiang <zekunjiang@google.com>
2025-01-09 11:02:46 -0800

    Add optimization guide decider to contextual cueing helper.
    Bug: 388304924
    Change-Id: Ic78b849c269241628a3d7ec77ef59c2368cfcda8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6153335
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404310}

--- chrome/browser/contextual_cueing/contextual_cueing_helper.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_features.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
+#include "chrome/browser/profiles/profile.h"
+#include "components/optimization_guide/core/optimization_guide_decider.h"
+#include "components/optimization_guide/core/optimization_metadata.h"
+#include "components/optimization_guide/proto/icon_view_metadata.pb.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
@@ -15,15 +21,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace contextual_cueing {
 
 ContextualCueingHelper::ContextualCueingHelper(
-    content::WebContents* web_contents)
+    content::WebContents* web_contents,
+    optimization_guide::OptimizationGuideDecider* decider)
     : content::WebContentsObserver(web_contents),
-      content::WebContentsUserData<ContextualCueingHelper>(*web_contents) {}
+      content::WebContentsUserData<ContextualCueingHelper>(*web_contents),
+      optimization_guide_decider_(decider) {
+  optimization_guide_decider_->RegisterOptimizationTypes(
+      {optimization_guide::proto::OPTIMIZATION_GUIDE_ICON_VIEW});
+}
 
 ContextualCueingHelper::~ContextualCueingHelper() = default;
 
 // content::WebContentsObserver
-void ContextualCueingHelper::DidFinishNavigation(
-    content::NavigationHandle* navigation_handle) {}
+void ContextualCueingHelper::DocumentOnLoadCompletedInPrimaryMainFrame() {
+  optimization_guide::OptimizationMetadata metadata;
+  auto decision = optimization_guide_decider_->CanApplyOptimization(
+      web_contents()->GetLastCommittedURL(),
+      optimization_guide::proto::OPTIMIZATION_GUIDE_ICON_VIEW, &metadata);
+  if (decision != optimization_guide::OptimizationGuideDecision::kTrue ||
+      metadata.empty()) {
+    return;
+  }
+
+  auto parsed = metadata.ParsedMetadata<
+      optimization_guide::proto::OptimizationGuideIconViewMetadata>();
+  // TODO(crbug.com/388305688): trigger cueing UI.
+}
 
 // static
 std::unique_ptr<ContextualCueingHelper>
@@ -32,8 +55,17 @@ ContextualCueingHelper::MaybeCreateForWebContents(
   if (!base::FeatureList::IsEnabled(contextual_cueing::kContextualCueing)) {
     return nullptr;
   }
+
+  Profile* profile =
+      Profile::FromBrowserContext(web_contents->GetBrowserContext());
+  auto* optimization_guide_decider =
+      OptimizationGuideKeyedServiceFactory::GetForProfile(profile);
+  if (!optimization_guide_decider) {
+    return nullptr;
+  }
+
   return base::WrapUnique<ContextualCueingHelper>(
-      new ContextualCueingHelper(web_contents));
+      new ContextualCueingHelper(web_contents, optimization_guide_decider));
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(ContextualCueingHelper);
--- chrome/browser/contextual_cueing/contextual_cueing_helper.h
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.h
@@ -6,13 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CONTEXTUAL_CUEING_CONTEXTUAL_CUEING_HELPER_H_
 #define CHROME_BROWSER_CONTEXTUAL_CUEING_CONTEXTUAL_CUEING_HELPER_H_
 
-#include "base/component_export.h"
 #include "base/observer_list.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
+namespace optimization_guide {
+class OptimizationGuideDecider;
+}  // namespace optimization_guide
+
 namespace contextual_cueing {
-class COMPONENT_EXPORT(CONTEXTUAL_CUEING) ContextualCueingHelper
+
+class ContextualCueingHelper
     : public content::WebContentsObserver,
       public content::WebContentsUserData<ContextualCueingHelper> {
  public:
@@ -26,13 +30,16 @@ class COMPONENT_EXPORT(CONTEXTUAL_CUEING) ContextualCueingHelper
   ~ContextualCueingHelper() override;
 
   // content::WebContentsObserver
-  void DidFinishNavigation(
-      content::NavigationHandle* navigation_handle) override;
+  void DocumentOnLoadCompletedInPrimaryMainFrame() override;
 
  private:
-  explicit ContextualCueingHelper(content::WebContents* contents);
+  ContextualCueingHelper(content::WebContents* contents,
+                         optimization_guide::OptimizationGuideDecider* decider);
 
-  friend WebContentsUserData;
+  raw_ptr<optimization_guide::OptimizationGuideDecider>
+      optimization_guide_decider_ = nullptr;
+
+  friend WebContentsUserData<ContextualCueingHelper>;
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
 
