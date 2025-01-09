df4e1aa14deb87c726fa2e92397a43f5cd5de3df
Peter Kvitek <kvitekp@chromium.org>
2025-01-09 15:33:44 -0800

    [headless] Use larger Target.createTarget() test window size
    https://crrev.com/c/6154346 introduced target window positioning
    support to Chrome, however, the test was failing on some platforms
    due to unexpected width. It turns out that the test requested width
    which was smaller than Chrome's minimum window width constraint.
    
    This CL changes the test to requests target window width large
    enough to satisfy minimum window size constrains.
    
    Bug: 388310031
    Change-Id: I8c6ba668e3098395f2d2129dddb3b341d1537683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164607
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404476}

--- chrome/browser/headless/test/data/protocol/sanity/create-target-position.js
+++ chrome/browser/headless/test/data/protocol/sanity/create-target-position.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                        'url': 'about:blank',
                        'left': 100,
                        'top': 200,
-                       'width': 300,
+                       'width': 600,
                        'height': 400,
                        'newWindow': true
                      })).result;
