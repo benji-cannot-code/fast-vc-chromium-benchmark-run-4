7bed4ffe0bb0d51609ad61b2db271f242b62e504
dmblack <dmblack@google.com>
2025-01-09 15:28:32 -0800

    Update how `ScopedBookmarkModelObservation` fetches child nodes.
    Previously `ScopedBookmarkModelObservation` fetched child nodes using
    `BookmarkBridge#getBookmarksForFolder()`. Now, it fetches child nodes
    using `#getChildIds()` and `#getBookmarkById()`. This circumvents an
    issue where `#getBookmarksForFolder()` would fail to return children of
    the local bookmark bar node when the user is not signed in.
    
    Bug: 347632437
    Change-Id: I9daa43c58efa37c6523e0e514684916caeaf7b41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143036
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404472}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ScopedBookmarkModelObservation.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ScopedBookmarkModelObservation.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.components.bookmarks.BookmarkItem;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -127,7 +128,12 @@ public class ScopedBookmarkModelObservation extends BookmarkModelObserver {
 
     @Override
     public void bookmarkModelChanged() {
-        mObserver.onBookmarkItemsChanged(mId, mModel.getBookmarksForFolder(mFolderId));
+        final List<BookmarkItem> items = new ArrayList<>();
+        for (final BookmarkId itemId : mModel.getChildIds(mFolderId)) {
+            final BookmarkItem item = mModel.getBookmarkById(itemId);
+            if (item != null) items.add(item);
+        }
+        mObserver.onBookmarkItemsChanged(mId, items);
     }
 
     @Override
