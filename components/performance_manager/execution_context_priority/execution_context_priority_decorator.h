ec8f170912dfdd6a598bf907048e65bd457f6a5a
Patrick Monette <pmonette@chromium.org>
2025-01-09 08:18:04 -0800

    [PM] Priority voting: Remove downvoters
    This functionality is currently unused as the only downvoter is behind
    a flag and there is no plan to actually use it in the futur.
    
    If this is ever needed again, we can easily add it back.
    
    Bug: 388544210
    Change-Id: I5c7b56185df8d52a017d8ac76aede2d8d061f114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6158469
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404206}

--- components/performance_manager/execution_context_priority/execution_context_priority_decorator.h
+++ components/performance_manager/execution_context_priority/execution_context_priority_decorator.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "components/performance_manager/execution_context_priority/max_vote_aggregator.h"
-#include "components/performance_manager/execution_context_priority/override_vote_aggregator.h"
 #include "components/performance_manager/execution_context_priority/root_vote_observer.h"
 #include "components/performance_manager/execution_context_priority/voter_base.h"
 #include "components/performance_manager/public/graph/graph.h"
@@ -45,10 +44,6 @@ class ExecutionContextPriorityDecorator final : public GraphOwned {
   // Takes in the aggregated votes and applies them to the execution contexts in
   // the graph.
   RootVoteObserver root_vote_observer_;
-
-  // Used to cast a negative vote that overrides the vote from
-  // |max_vote_aggregator_|.
-  OverrideVoteAggregator override_vote_aggregator_;
 
   // Aggregates all the votes from the voters.
   MaxVoteAggregator max_vote_aggregator_;
