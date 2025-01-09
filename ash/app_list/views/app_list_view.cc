75c35d124545577a43e4e566985bd0533df010fb
Michael Cui <mlcui@google.com>
2025-01-08 22:53:42 -0800

    app_list: Use `ButtonFocusSkipper` in `AppListView`
    `ButtonFocusSkipper` was previously used in `AppListBubbleView` to skip
    the Assistant and (recently) Sunfish button when using up/down arrow key
    navigation, but was not used in `AppListView`.
    
    Introduce `ButtonFocusSkipper` to `AppListView` in the same manner. This
    fixes unexpected keyboard navigation in tablet mode, and fixes test
    failures in `AppListKeyboardControllerTest` once Scanner is enabled by
    default.
    
    Bug: b:381965299
    Change-Id: I6a6a2100d255fe95f70c1941a2c51d218b14941b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161879
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404020}

--- ash/app_list/views/app_list_view.cc
+++ ash/app_list/views/app_list_view.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/app_list_folder_view.h"
 #include "ash/app_list/views/app_list_main_view.h"
 #include "ash/app_list/views/apps_container_view.h"
+#include "ash/app_list/views/button_focus_skipper.h"
 #include "ash/app_list/views/contents_view.h"
 #include "ash/app_list/views/paged_apps_grid_view.h"
 #include "ash/app_list/views/search_box_view.h"
@@ -249,6 +250,11 @@ void AppListView::InitContents() {
       std::make_unique<SearchBoxView>(app_list_main_view.get(), delegate_,
                                       /*is_app_list_bubble=*/false);
   search_box_view->InitializeForFullscreenLauncher();
+
+  // Skip the assistant and Sunfish buttons on arrow up/down in app list.
+  button_focus_skipper_ = std::make_unique<ButtonFocusSkipper>(this);
+  button_focus_skipper_->AddButton(search_box_view->sunfish_button());
+  button_focus_skipper_->AddButton(search_box_view->assistant_button());
 
   // Assign |app_list_main_view_| and |search_box_view_| here since they are
   // accessed during Init().
