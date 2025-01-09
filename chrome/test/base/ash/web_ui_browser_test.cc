3d666f026f0ed738679c6e334f9eb7e0331e0665
Georg Neis <neis@chromium.org>
2025-01-08 17:45:05 -0800

    Remove a use of obsolete crosapi::browser_util::IsLacrosEnabled
    Bug: b:365741912
    Change-Id: I5070c1a9881dd8610f44127f7abdf556cefb3372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6156638
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403947}

--- chrome/test/base/ash/web_ui_browser_test.cc
+++ chrome/test/base/ash/web_ui_browser_test.cc
@@ -24,12 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_switches.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/values.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -392,14 +389,6 @@ void BaseWebUIBrowserTest::SetUpOnMainThread() {
   }
 
   logging::SetLogMessageHandler(&LogHandler);
-
-  if (crosapi::browser_util::IsLacrosEnabled() && browser() == nullptr) {
-    // Create a new Ash browser window so test code using browser() can work
-    // even when Lacros is the only browser.
-    // TODO(crbug.com/40270051): Remove uses of browser() from such tests.
-    chrome::NewEmptyWindow(ProfileManager::GetActiveUserProfile());
-    SelectFirstBrowser();
-  }
 
   // For tests that run on the login screen, there is no Browser during
   // SetUpOnMainThread() so skip adding the chrome://webui-test data source.
