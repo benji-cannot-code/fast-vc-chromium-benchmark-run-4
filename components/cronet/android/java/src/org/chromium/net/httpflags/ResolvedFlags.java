f7ed4b93fc6c103fd906e929e35f37c073445df9
Mohannad Farrag <aymanm@google.com>
2025-01-09 06:40:49 -0800

    Add functionality to allow manipulating flags during tests
    This should make it much easier to integrate httpFlags with the current
    test infrastructure by allowing setting specific values to httpflags
    via an annotation.
    
    Added assertions to ensure that batching does not mess up the httpflags
    behaviour. This will safely fail the test if the assumed flags is
    different from applied flags.
    
    Bug: 387983175
    Change-Id: I2b66ce45d9577d5e7d4248f2504ada92acdcae46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148573
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404132}

--- components/cronet/android/java/src/org/chromium/net/httpflags/ResolvedFlags.java
+++ components/cronet/android/java/src/org/chromium/net/httpflags/ResolvedFlags.java
@@ -171,6 +171,11 @@ public final class ResolvedFlags {
             checkType(Type.BYTES);
             return (ByteString) mValue;
         }
+
+        @Override
+        public String toString() {
+            return mValue.toString();
+        }
     }
 
     private final Map<String, Value> mFlags;
