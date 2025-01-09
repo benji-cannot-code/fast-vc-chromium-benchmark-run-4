533f7ebfb6ec6cd55cfa72885fbaaebbf58ef290
Ayu Ishii <ayui@chromium.org>
2025-01-09 15:04:32 -0800

    Reland "WebSQL: Remove Inspector Database Agent"
    This is a reland of commit ef09d20723dce6faeeac10d8868db0240a9176de
    
    Original change's description:
    > WebSQL: Remove Inspector Database Agent
    >
    > Cleaning up dead code that is no longer used. WebSQL was removed
    > in M119 in all platforms except WebView, and DevTools code for
    > WebSQL was removed in:
    > https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5335172
    >
    > Change-Id: Ifb0ff5a98fccdf662e53eaf29a519df81e44d91b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6139214
    > Commit-Queue: Ayu Ishii <ayui@chromium.org>
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1403243}
    
    Change-Id: I1ed57f4cb127f09dd3dee1fccc01768a9fb5dae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6154490
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404459}

--- third_party/blink/renderer/core/inspector/devtools_session.h
+++ third_party/blink/renderer/core/inspector/devtools_session.h
@@ -41,7 +41,6 @@ class InspectorCacheStorageAgent;
 class InspectorDOMAgent;
 class InspectorDOMDebuggerAgent;
 class InspectorDOMSnapshotAgent;
-class InspectorDatabaseAgent;
 class InspectorEmulationAgent;
 class InspectorIOAgent;
 class InspectorLogAgent;
@@ -145,7 +144,6 @@ class CORE_EXPORT DevToolsSession : public GarbageCollected<DevToolsSession>,
                               std::is_same<T, InspectorDOMAgent>,
                               std::is_same<T, InspectorDOMDebuggerAgent>,
                               std::is_same<T, InspectorDOMSnapshotAgent>,
-                              std::is_same<T, InspectorDatabaseAgent>,
                               std::is_same<T, InspectorEmulationAgent>,
                               std::is_same<T, InspectorIOAgent>,
                               std::is_same<T, InspectorLogAgent>,
