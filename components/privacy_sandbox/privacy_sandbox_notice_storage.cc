986c0baef2558cfa6cd479d51fb21a41cf3baad2
Christine Ying <chrstne@google.com>
2025-01-09 12:47:38 -0800

    Add histograms to determine N&C NoticeShown mismatch
    Change-Id: I29829e51bc42f591ed8821d47d1cb2402ac178a1
    Bug: 388521278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6159884
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404380}

--- components/privacy_sandbox/privacy_sandbox_notice_storage.cc
+++ components/privacy_sandbox/privacy_sandbox_notice_storage.cc
@@ -318,6 +318,15 @@ void PrivacySandboxNoticeStorage::SetNoticeShown(PrefService* pref_service,
     SetSchemaVersion(pref_service, notice);
     base::UmaHistogramBoolean(
         base::StrCat({"PrivacySandbox.Notice.NoticeShown.", notice}), true);
+    base::UmaHistogramBoolean(
+        base::StrCat(
+            {"PrivacySandbox.Notice.NoticeShownForFirstTime.", notice}),
+        true);
+  } else {
+    base::UmaHistogramBoolean(
+        base::StrCat(
+            {"PrivacySandbox.Notice.NoticeShownForFirstTime.", notice}),
+        false);
   }
 
   // Always set notice last shown.
@@ -326,6 +335,7 @@ void PrivacySandboxNoticeStorage::SetNoticeShown(PrefService* pref_service,
       base::TimeToValue(notice_shown_time));
 }
 
+// TODO(chrstne): Create new histograms for migration.
 void PrivacySandboxNoticeStorage::MigratePrivacySandboxNoticeData(
     PrefService* pref_service,
     const PrivacySandboxNoticeData& input,
