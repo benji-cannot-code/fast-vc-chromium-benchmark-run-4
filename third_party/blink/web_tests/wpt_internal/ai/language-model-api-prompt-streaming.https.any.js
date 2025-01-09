10abe1afcd93fa4dffcd662d96088c8022e5261b
Tsuyoshi Horo <horo@chromium.org>
2025-01-08 16:12:44 -0800

    Fix prompt streaming WPT
    https://crrev.com/c/6002539 changed the behavior of promptStreaming() to
    return the result chunk-by-chunk. But the existing test only checks the
    last chunk. So if the last chunk is an empty string. The
    `assert_true(result.length > 0);` check fails.
    
    This CL fixes this problem by concatenating the result string.
    
    Bug: 378549335
    Change-Id: I1f99fa08661ea313f24a72ff411398abb62efe2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6158504
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403911}

--- third_party/blink/web_tests/wpt_internal/ai/language-model-api-prompt-streaming.https.any.js
+++ third_party/blink/web_tests/wpt_internal/ai/language-model-api-prompt-streaming.https.any.js
@@ -23,7 +23,9 @@ promise_test(async t => {
     if (done) {
       break;
     }
-    result = value;
+    if (value) {
+      result += value;
+    }
   }
-  assert_true(result.length > 0);
+  assert_true(result.length > 0, "The result should not be empty.");
 });
