ef797cd0e0764e27bb6c1ef1c016fdec5aca50f9
Alexis Hetu <sugoi@chromium.org>
2025-01-09 11:28:11 -0800

    Move logic to convert PRF inputs to salts into the PRFInput class
    This CL moves the logic to make PRF salts into the PRFInput class.
    This will allow the code to hash PRF values into salts to be
    reusable across platforms.
    
    Bug: 355045497
    Change-Id: I1ec4668b77ad6622744a221cc891dc1522371b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6163871
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404336}

--- device/fido/prf_input.cc
+++ device/fido/prf_input.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/cbor/values.h"
 #include "device/fido/fido_constants.h"
 #include "device/fido/fido_parsing_utils.h"
+#include "third_party/boringssl/src/include/openssl/sha.h"
 
 namespace device {
 
@@ -19,6 +20,23 @@ bool CBORToPRFValue(const cbor::Value& v, std::array<uint8_t, 32>* out) {
     return false;
   }
   return fido_parsing_utils::ExtractArray(v.GetBytestring(), 0, out);
+}
+
+// HashPRFValue hashes a PRF evaluation point with a fixed prefix in order to
+// separate the set of points that a website can evaluate. See
+// https://w3c.github.io/webauthn/#prf-extension.
+std::array<uint8_t, 32> HashPRFValue(base::span<const uint8_t> value) {
+  constexpr char kPrefix[] = "WebAuthn PRF";
+
+  SHA256_CTX ctx;
+  SHA256_Init(&ctx);
+  // This deliberately includes the terminating NUL.
+  SHA256_Update(&ctx, kPrefix, sizeof(kPrefix));
+  SHA256_Update(&ctx, value.data(), value.size());
+
+  std::array<uint8_t, 32> digest;
+  SHA256_Final(digest.data(), &ctx);
+  return digest;
 }
 }  // namespace
 
@@ -63,6 +81,13 @@ cbor::Value::MapValue PRFInput::ToCBOR() const {
                 std::vector<uint8_t>(this->salt2->begin(), this->salt2->end()));
   }
   return ret;
+}
+
+void PRFInput::HashInputsIntoSalts() {
+  this->salt1 = HashPRFValue(this->input1);
+  if (this->input2) {
+    this->salt2 = HashPRFValue(*(this->input2));
+  }
 }
 
 }  // namespace device
