a2b86d679e20f2e118e1722f91ec24931c4424fb
Michael Cui <mlcui@google.com>
2025-01-08 22:54:02 -0800

    app_list: Fix SunfishButtonLauncherTest not toggling Scanner features
    This fixes test failures when Scanner features are enabled by default.
    
    Bug: b:381965299
    Change-Id: I6a6a2100be524614c1773b0b33a88378aaa58010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161217
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404021}

--- ash/app_list/views/search_box_view_unittest.cc
+++ ash/app_list/views/search_box_view_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <string_view>
 #include <utility>
+#include <vector>
 
 #include "ash/app_list/app_list_controller_impl.h"
 #include "ash/app_list/app_list_presenter_impl.h"
@@ -1454,10 +1455,17 @@ class SunfishLauncherButtonTest : public AshTestBase,
                                   public testing::WithParamInterface<bool> {
  public:
   SunfishLauncherButtonTest() {
+    std::vector<base::test::FeatureRef> sunfish_features = {
+        features::kSunfishFeature,
+        features::kScannerUpdate,
+        features::kScannerDogfood,
+    };
     if (IsSunfishEnabled()) {
-      scoped_feature_list_.InitAndEnableFeature(features::kSunfishFeature);
+      scoped_feature_list_.InitWithFeatures(
+          /*enabled_features=*/sunfish_features, /*disabled_features=*/{});
     } else {
-      scoped_feature_list_.InitAndDisableFeature(features::kSunfishFeature);
+      scoped_feature_list_.InitWithFeatures(
+          /*enabled_features=*/{}, /*disabled_features=*/sunfish_features);
     }
   }
   ~SunfishLauncherButtonTest() override = default;
