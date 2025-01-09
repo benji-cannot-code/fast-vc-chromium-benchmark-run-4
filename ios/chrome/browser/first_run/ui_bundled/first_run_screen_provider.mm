3ad0ff0873fc99b0905a0023fbb9e55cfc8343b5
Hira Mahmood <hiramahmood@google.com>
2025-01-09 08:02:13 -0800

    [iOS] Update FRE sequence for feature kUpdatedFirstRunSequence
    This CL:
    * Updates the sequence of the FRE screens depending on the state of
      feature kUpdatedFirstRunSequence.
    * Updates tests that interact with the FRE screens.
    
    In a later CL, the ToS disclaimer will be conditionally added to the DB
    screen. EGTests will also be added for the feature in a later CL, once
    the UI is fully updated.
    
    Bug: 379306137
    Change-Id: Ib36c4a9633f2d0b996335c2b9d8f9d19cdbf88a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148293
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404187}

--- ios/chrome/browser/first_run/ui_bundled/first_run_screen_provider.mm
+++ ios/chrome/browser/first_run/ui_bundled/first_run_screen_provider.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/notreached.h"
 #import "ios/chrome/app/tests_hook.h"
+#import "ios/chrome/browser/first_run/ui_bundled/features.h"
 #import "ios/chrome/browser/screen/ui_bundled/screen_provider+protected.h"
 #import "ios/chrome/browser/screen/ui_bundled/screen_type.h"
 #import "ios/chrome/browser/search_engine_choice/model/search_engine_choice_util.h"
@@ -14,22 +15,53 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/browser/signin/model/chrome_account_manager_service.h"
+#import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
 #import "ios/public/provider/chrome/browser/signin/choice_api.h"
 
 @implementation FirstRunScreenProvider
 
-- (instancetype)initForProfile:(ProfileIOS*)profile {
++ (NSArray*)firstRunScreenSequenceForProfile:(ProfileIOS*)profile {
   NSMutableArray* screens = [NSMutableArray array];
-  [screens addObject:@(kSignIn)];
-  [screens addObject:@(kHistorySync)];
 
-  if (ShouldDisplaySearchEngineChoiceScreen(
-          *profile, /*is_first_run_entrypoint=*/true,
-          /*app_started_via_external_intent=*/false)) {
-    [screens addObject:@(kChoice)];
+  first_run::UpdatedFRESequenceVariationType variationType =
+      first_run::GetUpdatedFRESequenceVariation(profile);
+  BOOL hasIdentities =
+      ChromeAccountManagerServiceFactory::GetForProfile(profile)
+          ->HasIdentities();
+
+  switch (variationType) {
+    case first_run::UpdatedFRESequenceVariationType::kDisabled:
+      [screens addObject:@(kSignIn)];
+      [screens addObject:@(kHistorySync)];
+      if (ShouldDisplaySearchEngineChoiceScreen(
+              *profile, /*is_first_run_entrypoint=*/true,
+              /*app_started_via_external_intent=*/false)) {
+        [screens addObject:@(kChoice)];
+      }
+      [screens addObject:@(kDefaultBrowserPromo)];
+      break;
+    case first_run::UpdatedFRESequenceVariationType::kDBPromoFirst:
+      [screens addObject:@(kDefaultBrowserPromo)];
+      [screens addObject:@(kSignIn)];
+      [screens addObject:@(kHistorySync)];
+      break;
+    case first_run::UpdatedFRESequenceVariationType::kRemoveSignInSync:
+      if (hasIdentities) {
+        [screens addObject:@(kSignIn)];
+        [screens addObject:@(kHistorySync)];
+      }
+      [screens addObject:@(kDefaultBrowserPromo)];
+      break;
+    case first_run::UpdatedFRESequenceVariationType::
+        kDBPromoFirstAndRemoveSignInSync:
+      [screens addObject:@(kDefaultBrowserPromo)];
+      if (hasIdentities) {
+        [screens addObject:@(kSignIn)];
+        [screens addObject:@(kHistorySync)];
+      }
+      break;
   }
-
-  [screens addObject:@(kDefaultBrowserPromo)];
 
   DockingPromoDisplayTriggerArm experimentArm =
       DockingPromoExperimentTypeEnabled();
@@ -40,7 +72,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   [screens addObject:@(kStepsCompleted)];
-  return [super initWithScreens:screens];
+  return screens;
+}
+
+- (instancetype)initForProfile:(ProfileIOS*)profile {
+  return [super
+      initWithScreens:[[self class] firstRunScreenSequenceForProfile:profile]];
 }
 
 @end
