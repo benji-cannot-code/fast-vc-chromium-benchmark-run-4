c1d9bafa8b28a9beee7557748b8bdf0670c5d98f
Xiaodan Zhu <zxdan@chromium.org>
2025-01-09 11:43:50 -0800

    coral: maintain the second restore chip after restoring the first one
    Currently, when user restores a desk from one of the two restore chips
    and goes back to the Overview. The restore content will likely to form
    an in-session group such that another restore group will be cleared.
    To fix it, we set all the desks created/restore by Coral as modified
    by Coral and compare if the generated in-session group is same as the
    content on the desk modified by Coral. If it is, we will not clear the
    restore group and show the in-session group.
    
    Test: CoralFakeBackendBrowserTest.RetentSecondRestoreChip
    Bug: b:382549155
    Change-Id: I76e042564cad84880bc93bd3c6c2245e21bb632a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6152760
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404352}

--- chrome/browser/ui/ash/birch/birch_test_util.cc
+++ chrome/browser/ui/ash/birch/birch_test_util.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/overview/birch/birch_chip_button_base.h"
 #include "ash/wm/overview/overview_grid_test_api.h"
+#include "base/containers/flat_tree.h"
 #include "base/run_loop.h"
 #include "components/prefs/pref_service.h"
 
@@ -37,9 +38,12 @@ BirchChipButtonBase* GetBirchChipButton() {
   return test_api.GetBirchChips()[0];
 }
 
-size_t GetBirchChipsNum() {
+size_t GetCoralChipsNum() {
   aura::Window* root = Shell::GetPrimaryRootWindow();
-  return OverviewGridTestApi(root).GetBirchChips().size();
+  return base::ranges::count_if(
+      OverviewGridTestApi(root).GetBirchChips(), [](const auto& chip) {
+        return chip->GetItem()->GetType() == BirchItemType::kCoral;
+      });
 }
 
 void DisableAllDataTypePrefsExcept(std::vector<std::string_view> exceptions) {
