bdd0ec1b8f790aa94b5e13963c5d82f0d51d6f44
Takuto Ikuta <tikuta@chromium.org>
2025-01-09 09:12:31 -0800

    sandbox: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40263312
    Change-Id: If37d8e3e1c94f0b1fc6a9c054e8150e01b71ff6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6162135
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404237}

--- sandbox/linux/services/namespace_sandbox.cc
+++ sandbox/linux/services/namespace_sandbox.cc
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/services/syscall_wrappers.h"
 #include "sandbox/linux/system_headers/linux_signal.h"
 
+#if defined(LIBC_GLIBC)
+#include <pthread.h>
+#endif
+
 namespace sandbox {
 
 namespace {
