1e1c144c9f861caa8d1d69137bcbbead67e3d392
Ben Pastene <bpastene@chromium.org>
2025-01-09 15:58:10 -0800

    Revert "Remove unused include in //base/memory/ref_counted.h"
    This reverts commit 8b3d9ecfd37ba214eaed0cb58bd0b50ef5848130.
    
    Reason for revert: compile error around ABSL_MUST_USE_RESULT:
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-cast-arm-rel/5514/infra
    """
    In file included from ../../chromecast/media/cma/backend/android/audio_sink_android.cc:5:
    ../../chromecast/media/cma/backend/android/audio_sink_android.h:127:45: error: expected ';' at end of declaration list
      127 |               AudioContentType content_type) ABSL_MUST_USE_RESULT;
    
    """
    
    Original change's description:
    > Remove unused include in //base/memory/ref_counted.h
    >
    > Became unused after https://chromium-review.googlesource.com/c/chromium/src/+/3614476
    >
    > Bug: 40318405
    > Change-Id: I7400816622584a38ef3cbd421efe0dce396c72e0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6129106
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    > Cr-Commit-Position: refs/heads/main@{#1404468}
    
    Bug: 40318405
    Change-Id: Ib6eb4e09e1694fc99bab22a214ecb06ba10389d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164334
    Auto-Submit: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ben Pastene <bpastene@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1404480}

--- base/memory/ref_counted.h
+++ base/memory/ref_counted.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/threading/thread_collision_warner.h"
 #include "build/build_config.h"
+#include "third_party/abseil-cpp/absl/utility/utility.h"
 
 namespace base {
 namespace subtle {
