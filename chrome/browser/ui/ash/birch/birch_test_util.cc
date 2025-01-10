17abc6ec7fbe16b23c905652a58249932bc20590
Adam Rice <ricea@chromium.org>
2025-01-10 00:30:55 -0800

    Revert "coral: maintain the second restore chip after restoring the first one"
    This reverts commit b0a7182412b0f186d807eea5b99d9eafbf0ee081.
    
    Reason for revert: CoralFakeBackendBrowserTest.RetentSecondRestoreChip is failing on builder "chromium/ci/linux-chromeos-dbg". See https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/38578/overview
    
    
    Original change's description:
    > coral: maintain the second restore chip after restoring the first one
    >
    > Currently, when user restores a desk from one of the two restore chips
    > and goes back to the Overview. The restore content will likely to form
    > an in-session group such that another restore group will be cleared.
    > To fix it, we set all the desks created/restore by Coral as modified
    > by Coral and compare if the generated in-session group is same as the
    > content on the desk modified by Coral. If it is, we will not clear the
    > restore group and show the in-session group.
    >
    > Test: CoralFakeBackendBrowserTest.RetentSecondRestoreChip
    > Bug: b:382549155
    > Change-Id: I76e042564cad84880bc93bd3c6c2245e21bb632a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6152760
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    > Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1404352}
    
    Bug: b:382549155
    Change-Id: I75c29a0f1e65a2e3e265b8b59ad6704a57950b94
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166922
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Owners-Override: Adam Rice <ricea@google.com>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1404642}

--- chrome/browser/ui/ash/birch/birch_test_util.cc
+++ chrome/browser/ui/ash/birch/birch_test_util.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/overview/birch/birch_chip_button_base.h"
 #include "ash/wm/overview/overview_grid_test_api.h"
-#include "base/containers/flat_tree.h"
 #include "base/run_loop.h"
 #include "components/prefs/pref_service.h"
 
@@ -38,12 +37,9 @@ BirchChipButtonBase* GetBirchChipButton() {
   return test_api.GetBirchChips()[0];
 }
 
-size_t GetCoralChipsNum() {
+size_t GetBirchChipsNum() {
   aura::Window* root = Shell::GetPrimaryRootWindow();
-  return base::ranges::count_if(
-      OverviewGridTestApi(root).GetBirchChips(), [](const auto& chip) {
-        return chip->GetItem()->GetType() == BirchItemType::kCoral;
-      });
+  return OverviewGridTestApi(root).GetBirchChips().size();
 }
 
 void DisableAllDataTypePrefsExcept(std::vector<std::string_view> exceptions) {
