04bb71e8bb6c370eae3a8b1fdc992ac4ad9d4692
Thomas Nguyen <tungnh@chromium.org>
2025-01-10 01:38:36 -0800

    [PEPC] Add PEPC flag
    Enable PEPC as a flag of chrome://flags to simplify testing PEPC on
    Android.
    
    Fixed: 385090952
    Change-Id: I9d8d4c8281b84b1b045dcc87c56e4d0baa6edaec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110455
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404665}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1635,6 +1635,9 @@ extern const char kOverscrollHistoryNavigationDescription[];
 extern const char kPageContentAnnotationsName[];
 extern const char kPageContentAnnotationsDescription[];
 
+extern const char kPageEmbeddedPermissionControlName[];
+extern const char kPageEmbeddedPermissionControlDescription[];
+
 extern const char kPageContentAnnotationsPersistSalientImageMetadataName[];
 extern const char
     kPageContentAnnotationsPersistSalientImageMetadataDescription[];
