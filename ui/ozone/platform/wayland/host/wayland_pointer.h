49a4bc53d58b5d137e8b79495db925946012d000
Takuto Ikuta <tikuta@chromium.org>
2025-01-09 22:44:27 -0800

    ui: add missing includes for the build with use_libcxx_modules
    This is to fix build error when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40263312
    Change-Id: I7a2973c8ce3072a2efb90959bba06499f3bea3e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164428
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404613}

--- ui/ozone/platform/wayland/host/wayland_pointer.h
+++ ui/ozone/platform/wayland/host/wayland_pointer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_OZONE_PLATFORM_WAYLAND_HOST_WAYLAND_POINTER_H_
 
 #include <cstdint>
+#include <optional>
 
 #include "base/memory/raw_ptr.h"
 #include "ui/events/event_constants.h"
