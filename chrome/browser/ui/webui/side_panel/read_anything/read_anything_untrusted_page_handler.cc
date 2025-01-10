a675564daceb911ce356341ba0e62ac8b82e1f5c
Fergal Daly <fergal@chromium.org>
2025-01-10 00:23:50 -0800

    Switch use of translate::kUnknownLanguageCode to language_detection::
    This is a no-op refactor.
    
    This is split from https://crrev.com/c/6048141 and covers
    
    chrome/browser/ui/translate
    
    This CL was uploaded by git cl split.
    
    R=xiangxiao@google.com
    
    Bug: 380786760
    Change-Id: I388ddb47f58d037d02df2498e64ffd81414547e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161982
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404639}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.cc
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.cc
@@ -34,11 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/language/core/browser/language_model.h"
 #include "components/language/core/browser/language_model_manager.h"
 #include "components/language/core/common/locale_util.h"
+#include "components/language_detection/core/constants.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "components/translate/core/browser/language_state.h"
 #include "components/translate/core/browser/translate_driver.h"
-#include "components/translate/core/common/translate_constants.h"
 #include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/scoped_accessibility_mode.h"
@@ -881,7 +881,8 @@ void ReadAnythingUntrustedPageHandler::OnActiveAXTreeIDChanged() {
 void ReadAnythingUntrustedPageHandler::SetLanguageCode(
     const std::string& code) {
   const std::string& language_code =
-      (code.empty() || code == translate::kUnknownLanguageCode) ? "" : code;
+      (code.empty() || code == language_detection::kUnknownLanguageCode) ? ""
+                                                                         : code;
   // Only send the language code if it's a new language.
   if (language_code != current_language_code_) {
     current_language_code_ = language_code;
