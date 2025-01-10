a05c56e2ae4cc2ac0d35956f53782852946c4aff
Valerie Young <spectranaut@igalia.com>
2025-01-09 16:02:50 -0800

    [Braille IME] Convert to TypeScript
    Bug: 378856317
    Change-Id: Ibff5c639ffb19c9c819155de0e1b449ff13d01b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6096613
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1404488}

--- chrome/browser/resources/chromeos/accessibility/braille_ime/main.ts
+++ chrome/browser/resources/chromeos/accessibility/braille_ime/main.ts
@@ -3,10 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {BrailleIme} from './braille_ime.js';
+
 /**
  * The Braille IME object.  Attached to the globalThis object for ease of
  * debugging.
- * @type {BrailleIme}
  */
+declare global {
+  var ime: BrailleIme;
+}
+
 globalThis.ime = new BrailleIme();
 globalThis.ime.init();
