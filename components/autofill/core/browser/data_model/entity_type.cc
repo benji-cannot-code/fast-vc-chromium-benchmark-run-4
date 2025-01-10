99d00a7e37660ace171889113f1e5ed858543365
Christoph Schwering <schwering@google.com>
2025-01-10 07:24:48 -0800

    [Autofill] Add Entity transpiler
    This CL adds a transpiler and the basic classes for Autofill AI schemas.
    
    Low-Coverage-Reason: OTHER The lines in question are trivial.
    Bug: 388590912
    Change-Id: I6fa2477f31f5b5f3140197910256cf0267f9876c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6159067
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404775}

---
+++ components/autofill/core/browser/data_model/entity_type.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/data_model/entity_type.h"
+
+#include "base/types/cxx23_to_underlying.h"
+
+namespace autofill {
+
+FieldType AttributeTypeNameToFieldType(AttributeTypeName a) {
+  switch (a) {
+    case AttributeTypeName::kPassportName:
+      return NAME_FULL;
+    default:
+      return NO_SERVER_DATA;
+  }
+  NOTREACHED();
+}
+
+EntityType AttributeType::entity_type() const {
+  return EntityType(AttributeTypeNameToEntityTypeName(name_));
+}
+
+std::ostream& operator<<(std::ostream& os, EntityType t) {
+  return os << base::to_underlying(t.name());
+}
+
+std::ostream& operator<<(std::ostream& os, AttributeType t) {
+  return os << base::to_underlying(t.name());
+}
+
+}  // namespace autofill
