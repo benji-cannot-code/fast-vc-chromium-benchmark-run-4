0089ed4cd2bc66341549629da9c41c268d944e0c
Georg Neis <neis@chromium.org>
2025-01-09 18:55:41 -0800

    Remove some obsolete Lacros-related code
    Delete chrome/browser/chromeos/crosapi/* as well as most
    functions from chrome/browser/ash/crosapi/crosapi_util.*.
    
    Bug: b:365741912
    Change-Id: I608ff13f2229751379de243ad176dd20c7059a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6162374
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404562}

--- chrome/browser/ash/crosapi/crosapi_util.h
+++ chrome/browser/ash/crosapi/crosapi_util.h
@@ -6,21 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_CROSAPI_CROSAPI_UTIL_H_
 #define CHROME_BROWSER_ASH_CROSAPI_CROSAPI_UTIL_H_
 
-#include <string_view>
-#include <vector>
-
-#include "base/containers/flat_map.h"
-#include "base/containers/span.h"
-#include "base/files/platform_file.h"
-#include "base/files/scoped_file.h"
-#include "base/token.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
-#include "chromeos/crosapi/mojom/crosapi.mojom.h"
-#include "components/policy/core/common/cloud/cloud_policy_core.h"
-#include "components/policy/core/common/cloud/component_cloud_policy_service.h"
-#include "components/user_manager/user.h"
-#include "url/gurl.h"
-
 class Profile;
 
 // These methods are used by ash-chrome.
@@ -30,20 +15,6 @@ namespace browser_util {
 // Checks for the given profile if the user is affiliated or belongs to the
 // sign-in profile.
 bool IsSigninProfileOrBelongsToAffiliatedUser(Profile* profile);
-
-// Returns the UUID and version for all tracked interfaces. Exposed for testing.
-const base::flat_map<base::Token, uint32_t>& GetInterfaceVersions();
-
-// Returns the CloudPolicyCore for the given user.
-policy::CloudPolicyCore* GetCloudPolicyCoreForUser(
-    const user_manager::User& user);
-
-// Returns the ComponentCloudPolicyService for the given user.
-policy::ComponentCloudPolicyService* GetComponentCloudPolicyServiceForUser(
-    const user_manager::User& user);
-
-// Returns the list of Ash capabilities to publish to Lacros.
-base::span<const std::string_view> GetAshCapabilities();
 
 }  // namespace browser_util
 }  // namespace crosapi
