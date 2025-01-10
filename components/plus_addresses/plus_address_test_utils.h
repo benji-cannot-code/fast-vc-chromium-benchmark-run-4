723f79cb9354680fc189165b7d7041ad9974a92a
Norge Vizcay <vizcay@google.com>
2025-01-10 02:36:28 -0800

    Move plus address matchers to shared utils folder.
    The functions are moved to allow them to be reused over other text
    fixtures.
    
    Bug: 388862958
    Change-Id: Ib5caef05db519e9dcd5079abef23e5285621f2c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6163644
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404680}

--- components/plus_addresses/plus_address_test_utils.h
+++ components/plus_addresses/plus_address_test_utils.h
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_TEST_UTILS_H_
 
 #include <string>
+#include <vector>
 
 #include "base/time/time.h"
 #include "base/values.h"
 #include "components/plus_addresses/plus_address_types.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace plus_addresses::test {
 
@@ -69,6 +72,18 @@ HandleRequestToPlusAddressWithSuccess(
 base::Value CreatePreallocatedPlusAddress(
     base::Time end_of_life,
     std::string address = "some@plus.com");
+
+// Matches a fill plus address suggestion.
+testing::Matcher<autofill::Suggestion> EqualsFillPlusAddressSuggestion(
+    std::string_view address);
+
+// Matches a one-item vector with a standalone creation suggestion.
+testing::Matcher<std::vector<autofill::Suggestion>>
+IsSingleCreatePlusAddressSuggestion();
+
+// Matches a one-item vector with a fill plus address suggestion.
+testing::Matcher<std::vector<autofill::Suggestion>>
+IsSingleFillPlusAddressSuggestion(std::string_view address);
 
 }  // namespace plus_addresses::test
 
