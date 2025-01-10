6dff6c3aba0292e4c758628c662d7b5589ff2b82
Achuith Bhandarkar <achuith@chromium.org>
2025-01-10 10:22:25 -0800

    Replace PressKeySync with EventGenerator in OSFeedbackApp tests
    BUG=40268116
    
    Change-Id: I8169c4dabf4c172ae1886cd538875e015c34fdf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166481
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404884}

--- chrome/browser/ash/system_web_apps/apps/os_feedback_app_integration_browsertest.cc
+++ chrome/browser/ash/system_web_apps/apps/os_feedback_app_integration_browsertest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/webapps/browser/install_result_code.h"
 #include "content/public/test/browser_test.h"
@@ -28,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/display/screen.h"
 #include "ui/display/test/display_manager_test_api.h"
+#include "ui/events/event_constants.h"
+#include "ui/events/test/event_generator.h"
 #include "ui/gfx/geometry/rect.h"
 #include "url/gurl.h"
 
@@ -90,10 +91,10 @@ class OSFeedbackAppIntegrationTest : public ash::SystemWebAppIntegrationTest {
     EXPECT_EQ(nullptr, FindFeedbackAppBrowser());
   }
 
-  void SendKeyPressAltShiftI(Browser* browser) {
-    ASSERT_TRUE(ui_test_utils::SendKeyPressSync(
-        browser, ui::VKEY_I, /* control= */ false, /* shift= */ true,
-        /* alt= */ true, /* command= */ false));
+  void SendKeyPressAltShiftI() {
+    ui::test::EventGenerator generator(ash::Shell::GetPrimaryRootWindow());
+    generator.PressKeyAndModifierKeys(ui::VKEY_I,
+                                      ui::EF_SHIFT_DOWN | ui::EF_ALT_DOWN);
   }
 
   GURL feedback_url_;
@@ -123,7 +124,7 @@ IN_PROC_BROWSER_TEST_P(OSFeedbackAppIntegrationTest, OpenFeedbackByHotKey) {
   content::TestNavigationObserver navigation_observer(feedback_url_);
   navigation_observer.StartWatchingNewWebContents();
   // Try to press keyboard shortcut to open Feedback app.
-  SendKeyPressAltShiftI(browser());
+  SendKeyPressAltShiftI();
   navigation_observer.Wait();
 
   ExpectFeedbackAppLaunched(old_url);
@@ -146,7 +147,7 @@ IN_PROC_BROWSER_TEST_P(OSFeedbackAppIntegrationTest, UserFeedbackNotAllowed) {
   ExpectNoFeedbackAppLaunched(old_url);
 
   // Try to press keyboard shortcut to open Feedback app.
-  SendKeyPressAltShiftI(browser());
+  SendKeyPressAltShiftI();
 
   ExpectNoFeedbackAppLaunched(old_url);
 }
