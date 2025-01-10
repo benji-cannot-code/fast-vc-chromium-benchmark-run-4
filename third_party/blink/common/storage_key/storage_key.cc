11ab7da77537a5a68efde692d4ec7e5ed530cb5a
Peter Kasting <pkasting@chromium.org>
2025-01-10 15:50:47 -0800

    Switch StorageKey to C++20 comparisons: <=> and ==, not <, ==, and !=
    Allows more std::ranges use with StorageKey.
    
    Bug: 386918226
    Change-Id: If8bfc72625959f7e3eb28ca9744eafd03a460c9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6163829
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405069}

--- third_party/blink/common/storage_key/storage_key.cc
+++ third_party/blink/common/storage_key/storage_key.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 #include <string>
 #include <string_view>
-#include <tuple>
 
 #include "base/feature_list.h"
 #include "base/ranges/algorithm.h"
@@ -851,24 +850,6 @@ bool StorageKey::ExactMatchForTesting(const StorageKey& other) const {
              other.ancestor_chain_bit_if_third_party_enabled_ &&
          this->top_level_site_if_third_party_enabled_ ==
              other.top_level_site_if_third_party_enabled_;
-}
-
-bool operator==(const StorageKey& lhs, const StorageKey& rhs) {
-  return std::tie(lhs.origin_, lhs.top_level_site_, lhs.nonce_,
-                  lhs.ancestor_chain_bit_) ==
-         std::tie(rhs.origin_, rhs.top_level_site_, rhs.nonce_,
-                  rhs.ancestor_chain_bit_);
-}
-
-bool operator!=(const StorageKey& lhs, const StorageKey& rhs) {
-  return !(lhs == rhs);
-}
-
-bool operator<(const StorageKey& lhs, const StorageKey& rhs) {
-  return std::tie(lhs.origin_, lhs.top_level_site_, lhs.nonce_,
-                  lhs.ancestor_chain_bit_) <
-         std::tie(rhs.origin_, rhs.top_level_site_, rhs.nonce_,
-                  rhs.ancestor_chain_bit_);
 }
 
 std::ostream& operator<<(std::ostream& ostream, const StorageKey& sk) {
