2295358a75ba7318d44be5fed68e3856d757bbd9
Michelle Chen <michellegc@google.com>
2025-01-09 20:21:39 -0800

    scanner: Add fade in animation for Scanner action buttons.
    Add fade in animation for Scanner action buttons (i.e. buttons for
    actions fetched after pressing the smart actions button or selecting a
    region in Sunfish mode).
    
    We may also add fade in animations for other action buttons (e.g. copy
    text) as followup.
    
    Bug: b:388141749
    Change-Id: Ie560a0961c93ee2acac552a92b1ada360448e838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161935
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Elijah Hewer <hewer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404580}

--- ash/capture_mode/action_button_view.h
+++ ash/capture_mode/action_button_view.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/button/button.h"
 
+namespace base {
+class TimeDelta;
+}  // namespace base
+
 namespace gfx {
 struct VectorIcon;
 }  // namespace gfx
@@ -52,6 +56,9 @@ class ASH_EXPORT ActionButtonView : public views::Button {
   // Collapses the action button, hiding its label so that only the icon
   // shows.
   void CollapseToIconButton();
+
+  // Fades in the action button from fully transparent to fully opaque.
+  void PerformFadeInAnimation(base::TimeDelta fade_in_duration);
 
   const views::ImageView* image_view_for_testing() const { return image_view_; }
   const views::Label* label_for_testing() const { return label_; }
