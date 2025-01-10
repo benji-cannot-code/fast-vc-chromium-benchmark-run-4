e8542d7f673599f263f20d9d24d17f07a18a6ce0
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-01-10 12:47:57 -0800

    Remove RoundedDisplay feature flag
    Bug: b:346432542
    Change-Id: Idf9158910b439165e393915629d959e208c83e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036610
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404964}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1891,9 +1891,6 @@ extern const char kRetainOmniboxOnFocusName[];
 extern const char kRetainOmniboxOnFocusDescription[];
 #endif  // BUILDFLAG(IS_ANDROID)
 
-extern const char kRoundedDisplay[];
-extern const char kRoundedDisplayDescription[];
-
 extern const char kRoundedWindows[];
 extern const char kRoundedWindowsDescription[];
 
