34d3460bb55d0ddc590e026e2deffd16cd5acb24
sangwoo ko <sangwoo108@chromium.org>
2025-01-10 13:51:42 -0800

    Pass ImageModel to ImageView::SetImage() in ash/system/network
    The ImageSkia version of ImageView::SetImage() is deprecated.
    There's no intended behavior changes with this patch.
    
    This CL was uploaded by git cl split.
    
    R=khorimoto@chromium.org
    
    Bug: 40137576
    Change-Id: I1bc5fb7746d7d666d79cbd0108b0791d581e798e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166601
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Auto-Submit: SangWoo Ko <sangwoo108@chromium.org>
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404998}

--- ash/system/network/network_tray_view.cc
+++ ash/system/network/network_tray_view.cc
@@ -86,7 +86,7 @@ void NetworkTrayView::NetworkListChanged() {
 
 void NetworkTrayView::UpdateIcon(bool tray_icon_visible,
                                  const gfx::ImageSkia& image) {
-  image_view()->SetImage(image);
+  image_view()->SetImage(ui::ImageModel::FromImageSkia(image));
   SetVisible(tray_icon_visible);
   SchedulePaint();
 }
