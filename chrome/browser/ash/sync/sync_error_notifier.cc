12290e72311614e448f9677ea90a9efa98b98bb1
Georg Neis <neis@chromium.org>
2025-01-09 22:15:27 -0800

    ash: Remove Lacros leftovers from c/b/ash/sync
    Change-Id: I99466ecdc0dae842d4fdc82c6c210114469f7d76
    Bug: b:365741912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166820
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Auto-Submit: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404604}

--- chrome/browser/ash/sync/sync_error_notifier.cc
+++ chrome/browser/ash/sync/sync_error_notifier.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_common.h"
 #include "chrome/browser/notifications/notification_display_service.h"
@@ -22,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
-#include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
 #include "chrome/common/url_constants.h"
@@ -61,14 +59,7 @@ void ShowSyncSetup(Profile* profile) {
     return;
   }
 
-  if (crosapi::browser_util::IsLacrosEnabled()) {
-    chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
-        profile, chromeos::settings::mojom::kSyncSetupSubpagePath);
-  } else {
-    // TODO(crbug.com/40210838): remove this once it's not possible to use ash
-    // as a primary browser.
-    chrome::ShowSettingsSubPageForProfile(profile, chrome::kSyncSetupSubPage);
-  }
+  chrome::ShowSettingsSubPageForProfile(profile, chrome::kSyncSetupSubPage);
 }
 
 void TriggerSyncKeyRetrieval(Profile* profile) {
