4ea94fa289feaeccfa73a90fe6d6815f1fdf59dc
Ameur Hosni <ameurhosni@google.com>
2025-01-10 05:06:31 -0800

    [IOS][LO] Customize side swipe for lens overlay page
    Display the lens overlay snapshot when swiping back to a webpage where
    LO was invoked.
    
    doc: http://shortn/_2GMhTm66lt
    
    Bug: 380863351
    Change-Id: Id3aa019649fcdeb75a6158121831e29ea4ab76f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085341
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1404722}

---
+++ ios/chrome/browser/side_swipe/ui_bundled/horizontal_pan_gesture_handler.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SIDE_SWIPE_UI_BUNDLED_HORIZONTAL_PAN_GESTURE_HANDLER_H_
+#define IOS_CHROME_BROWSER_SIDE_SWIPE_UI_BUNDLED_HORIZONTAL_PAN_GESTURE_HANDLER_H_
+
+@class SideSwipeGestureRecognizer;
+
+// Animates the target view in response to given side swipe gesture recognizers.
+@protocol HorizontalPanGestureHandler <NSObject>
+
+// UIView that is animated in response to side swipe gestures.
+@property(nonatomic, weak) UIView* targetView;
+
+// Update views for latest gesture, and call completion blocks whether
+// `threshold` is met.
+- (void)handleHorizontalPan:(SideSwipeGestureRecognizer*)gesture
+     onOverThresholdCompletion:(void (^)(void))onOverThresholdCompletion
+    onUnderThresholdCompletion:(void (^)(void))onUnderThresholdCompletion;
+
+// Performs an animation on the view that simulates a swipe in `direction` and
+// call `completion` when the animation completes.
+- (void)animateHorizontalPanWithDirection:
+            (UISwipeGestureRecognizerDirection)direction
+                        completionHandler:(void (^)(void))completion;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SIDE_SWIPE_UI_BUNDLED_HORIZONTAL_PAN_GESTURE_HANDLER_H_
--- ios/chrome/browser/side_swipe/ui_bundled/side_swipe_navigation_view.h
+++ ios/chrome/browser/side_swipe/ui_bundled/side_swipe_navigation_view.h
@@ -8,31 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/side_swipe/ui_bundled/horizontal_pan_gesture_handler.h"
+
 @class SideSwipeGestureRecognizer;
 
 // Accessory view showing forward or back arrow while the main target view
 // is dragged from side to side.
-@interface SideSwipeNavigationView : UIView
-
-@property(nonatomic, weak) UIView* targetView;
+@interface SideSwipeNavigationView : UIView <HorizontalPanGestureHandler>
 
 // Initialize with direction.
 - (instancetype)initWithFrame:(CGRect)frame
                 withDirection:(UISwipeGestureRecognizerDirection)direction
                   canNavigate:(BOOL)canNavigate
                         image:(UIImage*)image;
-
-// Update views for latest gesture, and call completion blocks whether
-// `threshold` is met.
-- (void)handleHorizontalPan:(SideSwipeGestureRecognizer*)gesture
-     onOverThresholdCompletion:(void (^)(void))onOverThresholdCompletion
-    onUnderThresholdCompletion:(void (^)(void))onUnderThresholdCompletion;
-
-// Performs an animation on the view that simulates a swipe in `direction` and
-// call `completion` when the animation completes.
-- (void)animateHorizontalPanWithDirection:
-            (UISwipeGestureRecognizerDirection)direction
-                        completionHandler:(void (^)(void))completion;
 
 @end
 
