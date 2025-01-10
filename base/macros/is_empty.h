5205b44e1e4f79355f26869b76e14d69c8eb9388
Peter Boström <pbos@chromium.org>
2025-01-09 17:10:52 -0800

    Reimplement BASE_IS_EMPTY() to work with >1 args
    Before this change providing >1 arguments would resolve to the second
    argument. I.e. BASE_IS_EMPTY(a, b) would expand to b instead of 0 or 1.
    
    With this implementation BASE_IS_EMPTY() either expands to 0 or 1 and is
    a lot simpler due to its use of now-available __VA_OPT__.
    
    Bug: None
    Change-Id: Idbe9ac313bfd06cb8ab819ec8e821e8a6814a7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161936
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404523}

--- base/macros/is_empty.h
+++ base/macros/is_empty.h
@@ -7,11 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_MACROS_IS_EMPTY_H_
 
 // A macro that substitutes with 1 if called without arguments, otherwise 0.
-#define BASE_IS_EMPTY(...) BASE_INTERNAL_IS_EMPTY_EXPANDED(__VA_ARGS__)
-#define BASE_INTERNAL_IS_EMPTY_EXPANDED(...) \
-  BASE_INTERNAL_IS_EMPTY_INNER(_, ##__VA_ARGS__)
-#define BASE_INTERNAL_IS_EMPTY_INNER(...) \
-  BASE_INTERNAL_IS_EMPTY_INNER_EXPANDED(__VA_ARGS__, 0, 1)
-#define BASE_INTERNAL_IS_EMPTY_INNER_EXPANDED(e0, e1, is_empty, ...) is_empty
+#define BASE_IS_EMPTY(...) BASE_INTERNAL_IS_##__VA_OPT__(NON_)##EMPTY
+
+#define BASE_INTERNAL_IS_EMPTY 1
+#define BASE_INTERNAL_IS_NON_EMPTY 0
 
 #endif  // BASE_MACROS_IS_EMPTY_H_
