aca078e3c6041b13800913fcf49cf4b42e02e016
harthuang <harthuang@google.com>
2025-01-10 14:31:14 -0800

    Add UMA histogram to track exo surface file usage
    We will assign true each time a code path has
    been executed.
    
    Bug: 382422294
    Change-Id: Ib1901229bceee97f2825b0cff73b060a1fe2d819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6072378
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Hart Huang <harthuang@google.com>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405021}

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/not_fatal_until.h"
 #include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
@@ -1702,6 +1703,7 @@ void Surface::AppendContentsToFrame(const gfx::PointF& parent_to_root_px,
                                     bool needs_full_damage,
                                     std::optional<float> device_scale_factor,
                                     viz::CompositorFrame* frame) {
+  UMA_HISTOGRAM_BOOLEAN("Graphics.Exo.Surface.AppendContentsToFrame", true);
   const std::unique_ptr<viz::CompositorRenderPass>& render_pass =
       frame->render_pass_list.back();
   gfx::PointF parent_to_root_dp = gfx::ScalePoint(
@@ -1828,6 +1830,7 @@ void Surface::AppendContentsToFrame(const gfx::PointF& parent_to_root_px,
     if (current_resource_.id) {
       frame->resource_list.push_back(current_resource_);
     }
+    UMA_HISTOGRAM_BOOLEAN("Graphics.Exo.Surface.Occluded", true);
     return;
   }
 
@@ -1872,6 +1875,7 @@ void Surface::AppendContentsToFrame(const gfx::PointF& parent_to_root_px,
       // Draw quad is only needed if buffer is not fully transparent.
 
       if (requires_texture_draw_quad) {
+        UMA_HISTOGRAM_BOOLEAN("Graphics.Exo.Surface.TextureDrawQuad", true);
         viz::TextureDrawQuad* texture_quad =
             render_pass->CreateAndAppendDrawQuad<viz::TextureDrawQuad>();
         texture_quad->SetNew(quad_state, quad_rect, quad_rect,
@@ -1924,6 +1928,7 @@ void Surface::AppendContentsToFrame(const gfx::PointF& parent_to_root_px,
           damage_rect_px = gfx::RectF();
         }
       } else {
+        UMA_HISTOGRAM_BOOLEAN("Graphics.Exo.Surface.TileDrawQuad", true);
         viz::TileDrawQuad* tile_quad =
             render_pass->CreateAndAppendDrawQuad<viz::TileDrawQuad>();
         // TODO(crbug.com/40229946): Support AA quads coming from exo.
@@ -1940,6 +1945,7 @@ void Surface::AppendContentsToFrame(const gfx::PointF& parent_to_root_px,
     }
     frame->resource_list.push_back(current_resource_);
   } else if (state_.basic_state.alpha != 0.0f) {
+    UMA_HISTOGRAM_BOOLEAN("Graphics.Exo.Surface.SolidColorDrawQuad", true);
     const viz::SharedQuadState* quad_state = AppendOrCreateSharedQuadState(
         viz::DrawQuad::Material::kSolidColor, state_.basic_state.alpha,
         render_pass, quad_to_target_transform, quad_rect, msk, quad_clip_rect,
