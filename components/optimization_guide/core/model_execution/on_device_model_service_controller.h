faad9477166b70635a8129ae54562490749f650d
Steven Holte <holte@google.com>
2025-01-10 16:18:45 -0800

    Split OnDeviceState to OnDeviceExecution and OnDeviceContext.
    Seperates the on-device state into two distinct objects.
    
    * OnDeviceContext owns a session that is populated with context
      * Replaces ContextProcessor.
      * Now owns session and OnDeviceOptions
    * OnDeviceExecution owns the state for an ongoing session.
      * Gets a Clone of ContextProcessor's session.
      * Gets a copy of OnDeviceOptions.
    
    Some logic changes:
     * Score handles some cases that were previously errors.
     * Logging is slightly different when execution substition is invalid.
    
    Change-Id: Id309352927e5cb9f35b9898c15f04eeae78a623a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6160578
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1405082}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller.h
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller.h
@@ -144,7 +144,7 @@ class OnDeviceModelServiceController
   }
 
  private:
-  class OnDeviceModelClient final : public SessionImpl::OnDeviceModelClient {
+  class OnDeviceModelClient final : public OnDeviceOptions::Client {
    public:
     OnDeviceModelClient(
         ModelBasedCapabilityKey feature,
@@ -153,7 +153,7 @@ class OnDeviceModelServiceController
         base::optional_ref<const on_device_model::AdaptationAssetPaths>
             adaptation_assets);
     ~OnDeviceModelClient() override;
-    std::unique_ptr<SessionImpl::OnDeviceModelClient> Clone() const override;
+    std::unique_ptr<OnDeviceOptions::Client> Clone() const override;
     bool ShouldUse() override;
     mojo::Remote<on_device_model::mojom::OnDeviceModel>& GetModelRemote()
         override;
