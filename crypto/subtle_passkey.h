e998321e38fb0b80c3164d58a517f434e13ca254
Tom Anderson <thomasanderson@chromium.org>
2025-01-10 17:49:19 -0800

    Add FreedesktopSecretKeyProvider
    This adds an async Linux KeyProvider that's compatible with the current
    synchronous implementation. This is required in cases where
    SecretPortalKeyProvider is unavailable, which is described in [1].
    
    FallbackSecretKeyProvider is added for decrypting v10 (fixed password)
    ciphertexts.
    
    [1] https://docs.google.com/document/d/1ccFfyNaFtLNp6KypY5mjANtFcIOYPdSPBu8fLRmzsD0/edit?usp=sharing
    
    R=thestig
    
    Bug: 40086962
    Change-Id: I1e01522ee3c5f45ab0aaf6fe11f22ff0844c249c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112164
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405113}

--- crypto/subtle_passkey.h
+++ crypto/subtle_passkey.h
@@ -24,6 +24,10 @@ namespace chromeos::onc {
 crypto::SubtlePassKey MakeCryptoPassKey();
 }
 
+namespace os_crypt_async {
+class FreedesktopSecretKeyProvider;
+}
+
 class OSCryptImpl;
 
 namespace crypto {
@@ -58,9 +62,10 @@ class CRYPTO_EXPORT SubtlePassKey final {
   // arbitrary (possibly attacker-supplied) PBKDF2 parameters.
   friend SubtlePassKey chromeos::onc::MakeCryptoPassKey();
 
-  // This class uses custom PBKDF2 parameters and has to keep doing so for
+  // These classes use custom PBKDF2 parameters and have to keep doing so for
   // compatibility with existing persisted data.
   friend class ::OSCryptImpl;
+  friend class os_crypt_async::FreedesktopSecretKeyProvider;
 };
 
 }  // namespace crypto
