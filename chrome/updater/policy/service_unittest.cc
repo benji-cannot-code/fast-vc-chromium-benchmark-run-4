7379d3e3c1b6a6503cf0131008de484418a145d3
S. Ganesh <ganesh@chromium.org>
2025-01-10 16:24:51 -0800

    updater: always take policy critsec when creating group policy manager
    Bug: 388608937
    Change-Id: Icae5cd630e68cadcc7197e2a47e24382882bacd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168647
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405088}

--- chrome/updater/policy/service_unittest.cc
+++ chrome/updater/policy/service_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/task_environment.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/enterprise_companion/global_constants.h"
@@ -772,6 +773,7 @@ TEST_P(PolicyServiceAreUpdatesSuppressedNowTest, TestCases) {
 
 #if BUILDFLAG(IS_WIN)
 TEST(PolicyService, CreateManagers) {
+  base::test::TaskEnvironment environment;
   registry_util::RegistryOverrideManager registry_overrides;
   ASSERT_NO_FATAL_FAILURE(
       registry_overrides.OverrideRegistry(HKEY_LOCAL_MACHINE));
@@ -781,7 +783,7 @@ TEST(PolicyService, CreateManagers) {
                            OmahaSettingsClientProto>();
   auto dm_policy = base::MakeRefCounted<DMPolicyManager>(*omaha_settings, true);
   PolicyManagers managers =
-      CreateManagers(false, CreateExternalConstants(), dm_policy);
+      CreateManagers(CreateExternalConstants(), dm_policy);
   EXPECT_EQ(managers.size(), size_t{4});
   EXPECT_EQ(managers[0]->source(), "DictValuePolicy");
   EXPECT_EQ(managers[1]->source(), "Group Policy");
@@ -792,7 +794,7 @@ TEST(PolicyService, CreateManagers) {
                         Wow6432(KEY_WRITE));
   EXPECT_EQ(ERROR_SUCCESS,
             key.WriteValue(L"CloudPolicyOverridesPlatformPolicy", 1));
-  managers = CreateManagers(false, CreateExternalConstants(), dm_policy);
+  managers = CreateManagers(CreateExternalConstants(), dm_policy);
   EXPECT_EQ(managers.size(), size_t{4});
   EXPECT_EQ(managers[0]->source(), "DictValuePolicy");
   EXPECT_EQ(managers[1]->source(), "Device Management");
@@ -806,7 +808,7 @@ TEST(PolicyService, CreateManagers) {
                            OmahaSettingsClientProto>();
   auto dm_policy = base::MakeRefCounted<DMPolicyManager>(*omaha_settings, true);
   PolicyManagers managers =
-      CreateManagers(false, CreateExternalConstants(), dm_policy);
+      CreateManagers(CreateExternalConstants(), dm_policy);
   EXPECT_EQ(managers.size(), size_t{4});
   EXPECT_EQ(managers[0]->source(), "DictValuePolicy");
   EXPECT_EQ(managers[1]->source(), "Device Management");
@@ -820,7 +822,7 @@ TEST(PolicyService, CreateManagers) {
                            OmahaSettingsClientProto>();
   auto dm_policy = base::MakeRefCounted<DMPolicyManager>(*omaha_settings, true);
   PolicyManagers managers =
-      CreateManagers(false, CreateExternalConstants(), dm_policy);
+      CreateManagers(CreateExternalConstants(), dm_policy);
   EXPECT_EQ(managers.size(), size_t{3});
   EXPECT_EQ(managers[0]->source(), "DictValuePolicy");
   EXPECT_EQ(managers[1]->source(), "Device Management");
