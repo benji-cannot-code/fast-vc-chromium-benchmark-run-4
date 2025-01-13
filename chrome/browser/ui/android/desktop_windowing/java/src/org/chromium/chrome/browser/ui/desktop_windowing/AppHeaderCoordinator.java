d7f43ac87d67a2d779154fc4538108f8a009f901
Aishwarya Rajesh <aishwaryarj@google.com>
2025-01-13 15:30:31 -0800

    Reland "[TSLO] Bottom-pad the root view in a DW with non-zero nav bar insets"
    This is a reland of commit b4bc3672747d7ed4f66ec655b22dde2d72e2147f.
    Reland includes fix for regression reported in crbug.com/388210259 -
    consume nav bar insets in DeferredIMEWindowInsetApplicationCallback
    only when the ime is visible.
    
    Original change's description:
    > [TSLO] Bottom-pad the root view in a DW with non-zero nav bar insets
    >
    > Since Chrome is drawn E2E in a desktop window to customize the app
    > header, it is possible that bottom Chrome UI is drawn behind the nav
    > bar when such a window overlaps with the nav bar.
    > This CL bottom-pads the root view so that such UI is properly offset
    > from the bottom in this case.
    >
    > Before: https://screenshot.googleplex.com/8ow5TDivM69eoCq
    > After: https://screenshot.googleplex.com/Jn8TVGuDigiLeKj
    >
    > Bug: 382578519
    > Change-Id: I51c34a381ed10fc47aad4085eb3ea6e031dfecc5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6141934
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1402572}
    
    Bug: 382578519
    Change-Id: Id9158ae1228ea4518b9cf9832e584a8ee6d330d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171222
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405785}

--- chrome/browser/ui/android/desktop_windowing/java/src/org/chromium/chrome/browser/ui/desktop_windowing/AppHeaderCoordinator.java
+++ chrome/browser/ui/android/desktop_windowing/java/src/org/chromium/chrome/browser/ui/desktop_windowing/AppHeaderCoordinator.java
@@ -80,6 +80,7 @@ public class AppHeaderCoordinator
             DesktopWindowHeuristicResult.UNKNOWN;
     private @WindowingMode int mWindowingMode = WindowingMode.UNKNOWN;
     private int mKeyboardInset;
+    private int mNavBarInset;
 
     /**
      * Instantiate the coordinator to handle drawing the tab strip into the captionBar area.
@@ -111,7 +112,7 @@ public class AppHeaderCoordinator
         mRootView = rootView;
         mBrowserControlsVisibilityDelegate = browserControlsVisibilityDelegate;
         mInsetObserver = insetObserver;
-        mInsetObserver.addInsetsConsumer(this, InsetConsumerSource.APP_HEADER_COORDINATOR_IME);
+        mInsetObserver.addInsetsConsumer(this, InsetConsumerSource.APP_HEADER_COORDINATOR_BOTTOM);
         mInsetsController = mRootView.getWindowInsetsController();
         mActivityLifecycleDispatcher = activityLifecycleDispatcher;
         mActivityLifecycleDispatcher.register(this);
@@ -314,7 +315,10 @@ public class AppHeaderCoordinator
     private boolean maybeUpdateRootViewBottomPadding() {
         int rootViewBottomPadding = mRootView.getPaddingBottom();
         // Pad the root view with IME bottom insets only if E2E is active.
-        int bottomInset = FALSE.equals(mEdgeToEdgeStateProvider.get()) ? 0 : mKeyboardInset;
+        int bottomInset =
+                FALSE.equals(mEdgeToEdgeStateProvider.get())
+                        ? 0
+                        : Math.max(mKeyboardInset, mNavBarInset);
 
         // If the root view is padded as needed already, return early.
         if (rootViewBottomPadding == bottomInset) return bottomInset != 0;
@@ -359,12 +363,15 @@ public class AppHeaderCoordinator
     public WindowInsetsCompat onApplyWindowInsets(
             @NonNull View view, @NonNull WindowInsetsCompat windowInsetsCompat) {
         mKeyboardInset = windowInsetsCompat.getInsets(WindowInsetsCompat.Type.ime()).bottom;
+        mNavBarInset =
+                windowInsetsCompat.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom;
         boolean resizedRootView = maybeUpdateRootViewBottomPadding();
         if (!resizedRootView) return windowInsetsCompat;
 
         // Consume IME insets if the root view has been adjusted.
         return new WindowInsetsCompat.Builder(windowInsetsCompat)
                 .setInsets(WindowInsetsCompat.Type.ime(), Insets.NONE)
+                .setInsets(WindowInsetsCompat.Type.navigationBars(), Insets.NONE)
                 .build();
     }
 }
