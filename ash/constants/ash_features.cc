03633792cc96a5114905e62a65fbbf5a24ccbd07
Moteva <moteva@google.com>
2025-01-13 14:06:24 -0800

    CrOS Settings: cleanup OsSettingsDeprecateDnsDialog
    remove deprecated feature flag.
    
    Bug: 384773223
    Test:
    browser_test --gtest_filter="*OsPrivacyPageSecureDns*"
    
    Change-Id: I443b2c4bd61b49a3ba7c82ba74623af77f663e34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6165804
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1405741}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2219,12 +2219,6 @@ BASE_FEATURE(kOsFeedbackDialog,
              "OsFeedbackDialog",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Whether the DNS dialog in should be deprecated in Security and Privacy
-// Settings page when the user toggles off the DNS button.
-BASE_FEATURE(kOsSettingsDeprecateDnsDialog,
-             "OsSettingsDeprecateDnsDialog",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables Jelly colors and components to appear in the Parent Access Widget
 // if jelly-colors is also enabled.
 BASE_FEATURE(kParentAccessJelly,
@@ -4164,10 +4158,6 @@ bool IsOobeSplitModifierKeyboardInfoEnabled() {
 
 bool IsOsFeedbackDialogEnabled() {
   return base::FeatureList::IsEnabled(kOsFeedbackDialog);
-}
-
-bool IsOsSettingsDeprecateDnsDialogEnabled() {
-  return base::FeatureList::IsEnabled(kOsSettingsDeprecateDnsDialog);
 }
 
 bool IsOsSyncConsentRevampEnabled() {
