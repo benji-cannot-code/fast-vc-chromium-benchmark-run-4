3c649be6346ab72cfbff762f57cc0540b3b390ba
Duncan Mercer <mercerd@google.com>
2025-01-13 10:26:42 -0800

    Reland "[lensoverlay] Only show ghost loader if autocomplete is loading"
    This is a reland of commit 023e6388f0e240ab94dcdd99ace77ab84e7cccaf
    
    The original CL forgot to set isSearchboxFocused as a variable
    to computeShowGhostLoader after it was added back to that function
    after a CL comment. This caused the ghost loader to not show
    when the searchbox was focused via mouse click.
    
    Original change's description:
    > [lensoverlay] Only show ghost loader if autocomplete is loading
    >
    > Change the ghost loader to only show if there is a pending
    > autocomplete request. This better aligns the ghost loader to only be
    > showing when suggestions are actually being generated.
    >
    > Bug: 386808320
    > Change-Id: I52f2d50cc53fcfbfaab1d74b4446f5ecae48fa5b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6153216
    > Commit-Queue: Duncan Mercer <mercerd@google.com>
    > Reviewed-by: Nihar Majmudar <niharm@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1403729}
    
    Bug: 386808320
    Change-Id: If16eeaa795e26dc047c1cefbf66ee1878a1a9104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6167947
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1405599}

--- chrome/browser/resources/lens/overlay/lens_overlay_app.ts
+++ chrome/browser/resources/lens/overlay/lens_overlay_app.ts
@@ -143,8 +143,12 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
       },
       showGhostLoader: {
         type: Boolean,
-        computed: `computeShowGhostLoader(isSearchboxFocused,
-            suppressGhostLoader)`,
+        computed:
+            `computeShowGhostLoader(
+                isSearchboxFocused,
+                autocompleteRequestStarted,
+                showErrorState,
+                suppressGhostLoader)`,
         reflectToAttribute: true,
       },
       showErrorState: {
@@ -165,6 +169,8 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
   suppressGhostLoader: boolean;
   // Whether the ghost loader should show its error state.
   showErrorState: boolean;
+  // Whether this is an in flight request to autocomplete.
+  private autocompleteRequestStarted: boolean = false;
   // Whether the translate button is enabled.
   private isTranslateButtonEnabled: boolean;
   // Whether the image has finished rendering.
@@ -279,6 +285,9 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
       }
     });
     this.eventTracker_.add(
+        document, 'query-autocomplete',
+        this.handleQueryAutocomplete.bind(this));
+    this.eventTracker_.add(
         document, 'pointermove', this.updateCursorPosition.bind(this));
     this.eventTracker_.add(this.$.searchbox, 'mousedown', () => {
       this.suppressGhostLoader = false;
@@ -340,6 +349,11 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
     this.searchboxBoundingClientRectObserver.observe(this.$.selectionOverlay);
   }
 
+  // Called when the searchbox requests autocomplete suggestions.
+  private handleQueryAutocomplete() {
+    this.autocompleteRequestStarted = true;
+  }
+
   private focusShimmerOnSearchbox() {
     const suggestionsContainer = this.$.searchbox.getSuggestionsElement();
     const areSuggestionsShowing =
@@ -376,6 +390,7 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
 
   private handleSearchboxBlurred() {
     this.isSearchboxFocused = false;
+    this.autocompleteRequestStarted = false;
     this.showErrorState = false;
     this.$.translateButtonContainer.classList.add('searchbox-unfocused');
 
@@ -433,7 +448,13 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
   }
 
   private computeShowGhostLoader(): boolean {
-    return this.isSearchboxFocused && !this.suppressGhostLoader;
+    if (this.suppressGhostLoader) {
+      return false;
+    }
+    // Show the ghost loader if there is focus on the searchbox, and there is
+    // autcomplete is loading or if autocomplete failed.
+    return this.isSearchboxFocused &&
+        (this.autocompleteRequestStarted || this.showErrorState);
   }
 
   private suppressGhostLoader_() {
