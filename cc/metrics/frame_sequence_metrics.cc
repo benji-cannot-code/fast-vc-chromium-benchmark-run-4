01bad010624ffeafa906635f88fce77c708ff6a2
Sam Fortiner <samfort@microsoft.com>
2025-01-13 10:19:20 -0800

    Spanification: Fix FrameSequenceMetrics
    The needed C-style array to std::array conversation was recently
    handled via https://crrev.com/1404094 which enables removing the
    allow_unsafe_buffers pragma. This change also adds a couple qualifiers
    that clarify array usage expectations.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE adding static constexpr to variable
    Bug: 351564777
    Change-Id: I90869c800e9b232455632effa107814ec9e8be55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6114863
    Commit-Queue: Sam Fortiner <samfort@microsoft.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405589}

--- cc/metrics/frame_sequence_metrics.cc
+++ cc/metrics/frame_sequence_metrics.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "cc/metrics/frame_sequence_metrics.h"
 
 #include <array>
@@ -565,7 +560,7 @@ void FrameSequenceMetrics::TraceData::Advance(base::TimeTicks start_timestamp,
   // Use different names, because otherwise the trace-viewer shows the slices in
   // the same color, and that makes it difficult to tell the traces apart from
   // each other.
-  auto trace_names =
+  static constexpr auto trace_names =
       std::to_array<const char*>({"Frame", "Frame ", "Frame   "});
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN_WITH_TIMESTAMP0(
       "cc,benchmark", trace_names[++this->frame_count % 3],
