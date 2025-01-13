dc41ddb4221daebb49f33d025e593cf5e294e624
Chase Hartsell <chazzy@chromium.org>
2025-01-13 11:37:15 -0800

    [Compare] Fix header menu delete option
    This patch fixes an issue where the header menu delete option did not
    work, as the function it called expected a UUID instead of an event.
    The app test for this has been updated to explicitly click the delete
    button on the menu to ensure that the option works as intended.
    This patch also reorders some app functions to keep them near related
    functions.
    
    Bug: 389554076
    Change-Id: Iff457355361d5f500e0e47ca2180a89224372d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6170342
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405639}

--- chrome/test/data/webui/commerce/product_specifications/app_test.ts
+++ chrome/test/data/webui/commerce/product_specifications/app_test.ts
@@ -1809,7 +1809,13 @@ suite('AppTest', () => {
 
     const uuid =
         shoppingServiceApi.getArgs('addProductSpecificationsSet')[0][2];
-    appElement.$.header.dispatchEvent(new CustomEvent('delete-click'));
+    const header = appElement.$.header;
+    header.$.menuButton.click();
+    const menu = header.$.menu.$.menu;
+    const menuItemButton = menu.get().querySelector<HTMLElement>('#delete');
+    assertTrue(!!menuItemButton);
+    menuItemButton.click();
+    await flushTasks();
 
     assertEquals(
         1, shoppingServiceApi.getCallCount('deleteProductSpecificationsSet'));
