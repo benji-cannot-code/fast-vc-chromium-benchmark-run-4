17fd5bc7e5195fd7389809a763ad89a03c593d89
Gauthier Ambard <gambard@chromium.org>
2025-01-13 08:20:17 -0800

    [iOS] Tentative fix for Orchestrator crash
    The previous CL fixed it for updateUIToExpandedState, apply the same fix
    to updateUIToContractedState.
    The amount of crashes in M132 seemed to have diminished.
    
    Fixed: 369988988
    Change-Id: Ib16e0fd178125b5c1824985cf8016fadc66f032d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168853
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1405533}

--- ios/chrome/browser/orchestrator/ui_bundled/omnibox_focus_orchestrator.mm
+++ ios/chrome/browser/orchestrator/ui_bundled/omnibox_focus_orchestrator.mm
@@ -359,17 +359,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         delay:0
         options:UIViewAnimationCurveEaseInOut
         animations:^{
-          [UIView addKeyframeWithRelativeStartTime:0
-                                  relativeDuration:relativeDurationAnimation1
-                                        animations:^{
-                                          [self contraction];
-                                        }];
-          [UIView
-              addKeyframeWithRelativeStartTime:relativeDurationAnimation1
-                              relativeDuration:1 - relativeDurationAnimation1
-                                    animations:^{
-                                      [self.toolbarAnimatee showControlButtons];
-                                    }];
+          BOOL isLowerThan17 = !base::ios::IsRunningOnOrLater(17, 0, 0);
+          BOOL isHigherThan17_2 = base::ios::IsRunningOnOrLater(17, 2, 0);
+          if (isLowerThan17 || isHigherThan17_2) {
+            [UIView addKeyframeWithRelativeStartTime:0
+                                    relativeDuration:relativeDurationAnimation1
+                                          animations:^{
+                                            [self contraction];
+                                          }];
+            [UIView
+                addKeyframeWithRelativeStartTime:relativeDurationAnimation1
+                                relativeDuration:1 - relativeDurationAnimation1
+                                      animations:^{
+                                        [self.toolbarAnimatee
+                                                showControlButtons];
+                                      }];
+          } else {
+            // This is a workaround for a crash that is mostly happening on
+            // iOS 17.0-17.1. See crbug.com/369988988.
+            [self contraction];
+            [self.toolbarAnimatee showControlButtons];
+          }
         }
         completion:^(BOOL finished) {
           [self.toolbarAnimatee hideCancelButton];
