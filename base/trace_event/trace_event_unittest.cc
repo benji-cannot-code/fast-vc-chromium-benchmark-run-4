e835c018554cf34449b0747f7afcb636a97865df
Mikhail Khokhlov <khokhlov@google.com>
2025-01-13 07:48:34 -0800

    Disable 2 flaky trace event tests on Mac
    Bug: 325641473
    Change-Id: I906a5bc556b3861eb00ddecc3f3acdbbfe878842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6162637
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405516}

--- base/trace_event/trace_event_unittest.cc
+++ base/trace_event/trace_event_unittest.cc
@@ -1332,7 +1332,13 @@ class MyData : public ConvertableToTraceFormat {
   }
 };
 
-TEST_F(TraceEventTestFixture, ConvertableTypes) {
+// crbug.com/325641473: Flaky on Mac due to missing last event.
+#if BUILDFLAG(IS_APPLE)
+#define MAYBE_ConvertableTypes DISABLED_ConvertableTypes
+#else
+#define MAYBE_ConvertableTypes ConvertableTypes
+#endif
+TEST_F(TraceEventTestFixture, MAYBE_ConvertableTypes) {
   TraceLog::GetInstance()->SetEnabled(
       TraceConfig(kRecordAllCategoryFilter, ""));
 
@@ -1425,7 +1431,13 @@ TEST_F(TraceEventTestFixture, ConvertableTypes) {
   ASSERT_TRUE(convertable_dict);
 }
 
-TEST_F(TraceEventTestFixture, PrimitiveArgs) {
+// crbug.com/325641473: Flaky on Mac due to missing last event.
+#if BUILDFLAG(IS_APPLE)
+#define MAYBE_PrimitiveArgs DISABLED_PrimitiveArgs
+#else
+#define MAYBE_PrimitiveArgs PrimitiveArgs
+#endif
+TEST_F(TraceEventTestFixture, MAYBE_PrimitiveArgs) {
   TraceLog::GetInstance()->SetEnabled(
       TraceConfig(kRecordAllCategoryFilter, ""));
 
