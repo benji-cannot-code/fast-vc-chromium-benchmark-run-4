47b469edc075338caa8a26d54611b019a9341b09
Khushal Sagar <khushalsagar@chromium.org>
2025-01-13 12:06:45 -0800

    blink: Remove throttling override for AIPageContent extraction.
    The override was added to ensure we have a clean lifecycle state for
    offscreen iframes which might be throttled during rendering. But it's
    unnecessary since throttling is applied explicitly only during the
    rendering task. The default is for all iframes to be unthrottled when
    a lifecycle update is triggered.
    
    Bug: 389096819
    Change-Id: I39bfa65ddd554408c9f92ee4f655cbfff1f542f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171839
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405672}

--- third_party/blink/renderer/core/frame/local_frame_view.h
+++ third_party/blink/renderer/core/frame/local_frame_view.h
@@ -159,20 +159,6 @@ class CORE_EXPORT LocalFrameView final
     virtual void DidFinishLayout() {}
   };
 
-  // Enables forcing lifecycle updates for throttled frames.
-  class CORE_EXPORT DisallowThrottlingScope {
-    STACK_ALLOCATED();
-
-   public:
-    explicit DisallowThrottlingScope(const LocalFrameView& frame_view);
-    DisallowThrottlingScope(const DisallowThrottlingScope&) = delete;
-    DisallowThrottlingScope& operator=(const DisallowThrottlingScope&) = delete;
-    ~DisallowThrottlingScope() = default;
-
-   private:
-    base::AutoReset<bool> value_;
-  };
-
   explicit LocalFrameView(LocalFrame&);
   LocalFrameView(LocalFrame&, const gfx::Size& initial_size);
   ~LocalFrameView() override;
@@ -886,6 +872,19 @@ class CORE_EXPORT LocalFrameView final
     AllowThrottlingScope(const AllowThrottlingScope&) = delete;
     AllowThrottlingScope& operator=(const AllowThrottlingScope&) = delete;
     ~AllowThrottlingScope() = default;
+
+   private:
+    base::AutoReset<bool> value_;
+  };
+
+  class CORE_EXPORT DisallowThrottlingScope {
+    STACK_ALLOCATED();
+
+   public:
+    explicit DisallowThrottlingScope(const LocalFrameView& frame_view);
+    DisallowThrottlingScope(const DisallowThrottlingScope&) = delete;
+    DisallowThrottlingScope& operator=(const DisallowThrottlingScope&) = delete;
+    ~DisallowThrottlingScope() = default;
 
    private:
     base::AutoReset<bool> value_;
