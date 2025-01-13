7316ed8f69fc03ca24453fb49dc503faf2538e10
Michelle Chen <michellegc@google.com>
2025-01-12 17:19:27 -0800

    scanner: Move button transition logic into ActionButtonContainerView.
    Move the smart actions button transition logic into
    ActionButtonContainerView.
    
    Bug: b:388939375, b:388141749
    Change-Id: Ia49ca077550716f3fc83a1dea2e6c13273f0a2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166684
    Reviewed-by: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1405342}

--- ash/capture_mode/action_button_container_view.h
+++ ash/capture_mode/action_button_container_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/capture_mode/capture_mode_types.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/view.h"
@@ -42,6 +43,22 @@ class ASH_EXPORT ActionButtonContainerView : public views::View {
                                     const gfx::VectorIcon* icon,
                                     ActionButtonRank rank,
                                     ActionButtonViewID id);
+
+  // Starts performing the button transition triggered after pressing the smart
+  // actions button. This will fade out existing action buttons, remove the
+  // smart actions button, then animate in new icon buttons to replace the old
+  // copy text and search buttons.
+  void StartSmartActionsButtonTransition();
+
+ private:
+  // Called when the smart actions button has faded out, to start the transition
+  // to new buttons. See `StartSmartActionsButtonTransition()`.
+  void OnSmartActionsButtonFadedOut();
+
+  // Enables or disables events on the action button container widget.
+  void SetWidgetEventsEnabled(bool enabled);
+
+  base::WeakPtrFactory<ActionButtonContainerView> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
