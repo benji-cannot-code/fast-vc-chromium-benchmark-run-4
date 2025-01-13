50247c883040ba5f836d7be010645d938e471c99
Sergiy Belozorov <sergiyb@chromium.org>
2025-01-13 06:39:26 -0800

    Always allow powerwash before ownership is taken
    Bug: b:377477484
    Change-Id: Idf3900b7e939e3adfed9ef380751fe2ecd4b1699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6170435
    Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1405492}

--- chrome/browser/ash/login/screens/reset_screen_unittest.cc
+++ chrome/browser/ash/login/screens/reset_screen_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/screens/reset_screen.h"
 
 #include "ash/constants/ash_switches.h"
-#include "base/test/scoped_command_line.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/ash/policy/enrollment/auto_enrollment_type_checker.h"
 #include "chrome/browser/ash/settings/scoped_cros_settings_test_helper.h"
@@ -48,10 +47,6 @@ class ResetScreenTest : public testing::Test {
   // Configure policy
   void SetPowerwashAllowedByPolicy(bool allowed);
 
-  // Configure VPD values
-  void SetFreOn();
-  void SetFreOff();
-
  private:
   ScopedCrosSettingsTestHelper cros_settings_test_helper_;
   system::ScopedFakeStatisticsProvider fake_statistics_provider_;
@@ -80,16 +75,6 @@ void ResetScreenTest::SetPowerwashAllowedByPolicy(bool allowed) {
   cros_settings_test_helper_.Set(kDevicePowerwashAllowed, base::Value(allowed));
 }
 
-void ResetScreenTest::SetFreOn() {
-  fake_statistics_provider_.SetMachineStatistic(
-      ash::system::kCheckEnrollmentKey, "1");
-}
-
-void ResetScreenTest::SetFreOff() {
-  fake_statistics_provider_.SetMachineStatistic(
-      ash::system::kCheckEnrollmentKey, "0");
-}
-
 TEST_F(ResetScreenTest, CheckPowerwashAllowedConsumerOwned) {
   SetConsumerOwned();
   ExpectPowerwashAllowed(true);
@@ -105,18 +90,8 @@ TEST_F(ResetScreenTest, CheckPowerwashAllowedOnEnrolledDevice) {
   ExpectPowerwashAllowed(false);
 }
 
-// TODO(b/353731379): Remove when removing legacy state determination code.
 TEST_F(ResetScreenTest, CheckPowerwashAllowedNotOwned) {
-  base::test::ScopedCommandLine command_line;
-  command_line.GetProcessCommandLine()->AppendSwitchASCII(
-      ash::switches::kEnterpriseEnableUnifiedStateDetermination,
-      policy::AutoEnrollmentTypeChecker::kUnifiedStateDeterminationNever);
   SetUnowned();
-
-  SetFreOn();
-  ExpectPowerwashAllowed(false);
-
-  SetFreOff();
   ExpectPowerwashAllowed(true);
 }
 
