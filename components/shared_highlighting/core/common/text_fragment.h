4959e2bfa7129fc6d061d05857cec4bd105bbbd1
Lei Zhang <thestig@chromium.org>
2025-01-13 10:07:11 -0800

    Reduce copying in shared_highlighting::TextFragment::FromEscapedString()
    Replace std::string usage with std::string_view to break a string into
    its components without duplicating the string content.
    
    Change-Id: Ibb9dbdb257923fab505dc28dfe4bd0a2e5e3417d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164333
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405579}

--- components/shared_highlighting/core/common/text_fragment.h
+++ components/shared_highlighting/core/common/text_fragment.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 #include <string>
+#include <string_view>
 
 #include "base/values.h"
 
@@ -33,7 +34,7 @@ class TextFragment {
   // [prefix-,]textStart[,textEnd][,-suffix]
   // Returns |std::nullopt| if parsing failed.
   static std::optional<TextFragment> FromEscapedString(
-      std::string escaped_string);
+      std::string_view escaped_string);
 
   // Returns a TextFragment instance created from a dictionary |value|
   // containing the right set of values. The values stored in |value| must be
