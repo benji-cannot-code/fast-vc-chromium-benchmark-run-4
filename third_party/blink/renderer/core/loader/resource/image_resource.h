d8126e21ec90c5fc66b62ad21ead528ef60ef7e8
Stefan Zager <szager@chromium.org>
2025-01-13 10:32:33 -0800

    Never speculatively decode an img element with zero dimensions
    Previously we would speculatively decode an in-viewport img element with
    0x0 size, using the image resource's intrinsic dimensions as the decode
    target size. The reasoning was that if the img has auto-sizing in both
    dimensions and hasn't run layout since the image resource's intrinsic
    size became known, it was very likely that on the next layout the image
    would be sized to its intrinsic size.
    
    However, that's not always a correct assumption and it could have
    undesirable consequences. If the img element is sized much smaller than
    its intrinsic size, we would waste memory by decoding to the intrinsic
    size. Decoding at full size and downsampling can also result in minor
    differences in pixel output, which can cause failures in pixel-based
    tests and might catch the attention of eagle-eyed developers. These
    pixel diffs would be non-deterministic, since speculative decoding is
    inherently timing sensitive.
    
    With this CL, if an <img> has 0x0 size then we will not start a
    speculative decode for it. We will still start speculative decodes for
    images that have zero area but one non-zero dimension.
    
    Bug: chromium:378623805
    Change-Id: I5f14f3f5abe5ed2831644396eb345feceea211d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166227
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405606}

--- third_party/blink/renderer/core/loader/resource/image_resource.h
+++ third_party/blink/renderer/core/loader/resource/image_resource.h
@@ -102,6 +102,7 @@ class CORE_EXPORT ImageResource final
   void UpdateResourceInfoFromObservers() override;
   std::pair<ResourcePriority, ResourcePriority> PriorityFromObservers()
       const override;
+  bool HasNonDegenerateSizeForDecode() const override;
 
   // MultipartImageResourceParser::Client
   void OnePartInMultipartReceived(const ResourceResponse&) final;
