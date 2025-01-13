fafe25b4bd130cfb5351b690938da6e6126057bb
Roman Arora <romanarora@chromium.org>
2025-01-13 14:20:30 -0800

    [ntp][tab-resumption] Add remote visits flag variation
    Fixed: 389742559
    Change-Id: Idd0c794e36faa6f2706deea2dd2d870414d1c948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169184
    Auto-Submit: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405749}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1994,6 +1994,9 @@ const FeatureEntry::FeatureParam
     kNtpMostRelevantTabResumptionModuleRemoteTabData[] = {
         {ntp_features::kNtpMostRelevantTabResumptionModuleDataParam, "2"}};
 const FeatureEntry::FeatureParam
+    kNtpMostRelevantTabResumptionModuleRemoteVisitsData[] = {
+        {ntp_features::kNtpMostRelevantTabResumptionModuleDataParam, "2,4"}};
+const FeatureEntry::FeatureParam
     kNtpMostRelevantTabResumptionModuleAllHistoryRemoteTabData[] = {
         {ntp_features::kNtpMostRelevantTabResumptionModuleDataParam, "2,3,4"}};
 const FeatureEntry::FeatureParam
@@ -2021,6 +2024,9 @@ const FeatureEntry::FeatureVariation
          std::size(kNtpMostRelevantTabResumptionModuleTabData), nullptr},
         {"- Remote Tabs Only", kNtpMostRelevantTabResumptionModuleRemoteTabData,
          std::size(kNtpMostRelevantTabResumptionModuleRemoteTabData), nullptr},
+        {"- Remote Visits", kNtpMostRelevantTabResumptionModuleRemoteVisitsData,
+         std::size(kNtpMostRelevantTabResumptionModuleRemoteVisitsData),
+         nullptr},
         {"- All History, Remote Tabs",
          kNtpMostRelevantTabResumptionModuleAllHistoryRemoteTabData,
          std::size(kNtpMostRelevantTabResumptionModuleAllHistoryRemoteTabData),
