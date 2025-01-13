f6dec2cb0cdab8751044b0ce13fc0135c620dd26
Kevin McNee <mcnee@chromium.org>
2025-01-13 13:19:58 -0800

    Remove unnecessary include from latency_info.h
    This file already forward declares EventContext, and the declaration is
    enough.
    
    This include is costly according to
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html#view=edges
    
    Bug: 40318405
    Change-Id: Idb9f1e52efd1727b972b0744a4ac4bd3a57f1bdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169227
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405709}

--- ui/latency/latency_info.h
+++ ui/latency/latency_info.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/tracing/protos/chrome_track_event.pbzero.h"
 #include "build/blink_buildflags.h"
 #include "build/build_config.h"
-#include "third_party/perfetto/include/perfetto/tracing/event_context.h"
 #include "third_party/perfetto/protos/perfetto/trace/track_event/chrome_latency_info.pbzero.h"
 
 #if BUILDFLAG(USE_BLINK)
