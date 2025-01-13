c7095a83aec3411ce1011c6e98ab8db1fc328985
Patrick Monette <pmonette@chromium.org>
2025-01-13 10:53:34 -0800

    Fix Side Panel priority inversion
    Some side panel are hidden from view until their contents is fully
    loaded. This has the side effect of lowering the priority of the
    hosting renderer.
    
    This change adds a new PriorityVoter that increase the priority of
    the main frame of the Side Panel contents until it is made visible.
    
    Bug: 388544210
    Change-Id: I4dd8381de5d9f255801f25e83958d0f706d2f2ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148515
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405623}

---
+++ chrome/browser/performance_manager/side_panel_loading_policy.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/performance_manager/public/side_panel_loading_policy.h"
+
+#include "base/functional/bind.h"
+#include "chrome/browser/performance_manager/execution_context_priority/side_panel_loading_voter.h"
+#include "components/performance_manager/public/graph/graph.h"
+#include "components/performance_manager/public/performance_manager.h"
+#include "content/public/browser/web_contents.h"
+
+namespace performance_manager::execution_context_priority {
+
+void MarkAsSidePanel(content::WebContents* web_contents) {
+  PerformanceManager::CallOnGraph(
+      FROM_HERE,
+      base::BindOnce(
+          [](base::WeakPtr<PageNode> page_node, Graph* graph) {
+            CHECK(page_node);
+            auto* voter = graph->GetRegisteredObjectAs<
+                execution_context_priority::SidePanelLoadingVoter>();
+            CHECK(voter);
+
+            voter->MarkAsSidePanel(page_node.get());
+          },
+          PerformanceManager::GetPrimaryPageNodeForWebContents(web_contents)));
+}
+
+}  // namespace performance_manager::execution_context_priority
