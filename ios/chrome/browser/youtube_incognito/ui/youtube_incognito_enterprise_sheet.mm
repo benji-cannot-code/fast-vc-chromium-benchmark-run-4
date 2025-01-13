311ff8cb4c4b55618a7f6c0a98373393d54d5d68
Elmehdi Rahmaoui <erahmaoui@google.com>
2025-01-13 07:31:42 -0800

    [iOS] Youtube Incognito feature strings
    Bug: 388455294
    Change-Id: I5884ce83846dadc3b9f4607b20592b15f3a4566c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6157724
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405511}

--- ios/chrome/browser/youtube_incognito/ui/youtube_incognito_enterprise_sheet.mm
+++ ios/chrome/browser/youtube_incognito/ui/youtube_incognito_enterprise_sheet.mm
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/youtube_incognito/ui/youtube_incognito_sheet_delegate.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
-#import "ios/chrome/grit/ios_branded_strings.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
 namespace {
 
@@ -36,13 +37,15 @@ CGFloat const kIconSize = 32;
   self.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
   self.aboveTitleView = [self enterpriseTitleView];
 
-  // TODO(crbug.com/374935670): Add a11y strings.
-  self.titleString = @"TODO Incognito Mode Is Unavailable";
+  self.titleString =
+      l10n_util::GetNSString(IDS_IOS_YOUTUBE_INCOGNITO_ENTERPRISE_TITLE);
   self.secondaryTitleString =
-      @"TODO Your organization turned off private browsing.";
+      l10n_util::GetNSString(IDS_IOS_YOUTUBE_INCOGNITO_ENTERPRISE_SUBTITLE);
   self.subtitleString = self.URLText;
-  self.primaryActionString = @"TODO Open Site Anyway";
-  self.secondaryActionString = @"TODO cancel";
+  self.primaryActionString = l10n_util::GetNSString(
+      IDS_IOS_YOUTUBE_INCOGNITO_ENTERPRISE_PRIMARY_BUTTON_TITLE);
+  self.secondaryActionString = l10n_util::GetNSString(
+      IDS_IOS_YOUTUBE_INCOGNITO_ENTERPRISE_SECONDARY_BUTTON_TITLE);
   self.customSpacing = kVerticalSpacing;
   self.titleTextStyle = UIFontTextStyleTitle3;
   self.scrollEnabled = YES;
