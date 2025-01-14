131c3401cd1c207534ff0845a274d315689fa116
Georg Neis <neis@chromium.org>
2025-01-13 16:18:40 -0800

    ash: Remove Lacros leftovers from input_method code
    No change in behavior.
    
    Change-Id: I6a7ae6bb13a92a814c217aa72a480818f1bd7b40
    Bug: b:365741912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166465
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405806}

--- chrome/browser/ash/input_method/assistive_suggester_unittest.cc
+++ chrome/browser/ash/input_method/assistive_suggester_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "base/time/time.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/input_method/assistive_suggester_client_filter.h"
 #include "chrome/browser/ash/input_method/assistive_suggester_switch.h"
 #include "chrome/browser/ash/input_method/fake_suggestion_handler.h"
