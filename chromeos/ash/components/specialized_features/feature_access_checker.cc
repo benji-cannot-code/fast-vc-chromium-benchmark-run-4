fcf314a4166b3b746bcf9fe655789e590d5f6ed7
Michael Cui <mlcui@google.com>
2025-01-14 15:30:41 -0800

    Do not store pointer to variations service in feature access checker
    This caused various browser tests to fail as the browser process'
    variations service is destroyed before any keyed services, including
    `ScannerKeyedService`.
    
    Instead, "lazily" obtain the variation service pointer by using a
    callback instead.
    
    Bug: b:381965299
    Change-Id: I6a6a21001c6ebf093041f634f8715400cd77e40b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171924
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Jon Htin <jhtin@google.com>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406407}

--- chromeos/ash/components/specialized_features/feature_access_checker.cc
+++ chromeos/ash/components/specialized_features/feature_access_checker.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/specialized_features/feature_access_checker.h"
 
+#include <utility>
+
 #include "base/command_line.h"
 #include "base/hash/sha1.h"
 #include "chromeos/components/kiosk/kiosk_utils.h"
@@ -30,11 +32,13 @@ FeatureAccessChecker::FeatureAccessChecker(
     FeatureAccessConfig config,
     PrefService* prefs,
     signin::IdentityManager* identity_manager,
-    variations::VariationsService* variations_service)
+    VariationsServiceCallback variations_service_callback)
     : config_(config),
       prefs_(prefs),
       identity_manager_(identity_manager),
-      variations_service_(variations_service) {}
+      variations_service_callback_(std::move(variations_service_callback)) {}
+
+FeatureAccessChecker::~FeatureAccessChecker() = default;
 
 FeatureAccessChecker::FeatureAccessChecker() = default;
 
@@ -104,9 +108,13 @@ FeatureAccessFailureSet FeatureAccessChecker::Check() const {
   }
 
   if (!config_.country_codes.empty()) {
-    if (variations_service_ == nullptr ||
+    variations::VariationsService* variations_service =
+        variations_service_callback_.is_null()
+            ? nullptr
+            : variations_service_callback_.Run();
+    if (variations_service == nullptr ||
         !base::Contains(config_.country_codes,
-                        variations_service_->GetLatestCountry())) {
+                        variations_service->GetLatestCountry())) {
       failures.Put(kCountryCheckFailed);
     }
   }
