6c60b10167d24e602efd8119a033d5101710bb9c
Timofey Chudakov <tchudakov@google.com>
2025-01-14 06:57:08 -0800

    [Android] Enable testAutofillClickFirstSuggestion per Test Reviver.
    Fixed: 339692413
    Change-Id: Id213cab02e77d6210aa2931374c64df0caf95093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169620
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406067}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillUnitTest.java
@@ -23,7 +23,6 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.autofill.AutofillDelegate;
 import org.chromium.components.autofill.AutofillPopup;
@@ -188,7 +187,6 @@ public class AutofillUnitTest {
     @Test
     @SmallTest
     @Feature({"autofill"})
-    @DisabledTest(message = "https://crbug.com/1338184")
     public void testAutofillClickFirstSuggestion() {
         AutofillSuggestion[] suggestions = createTwoAutofillSuggestionArray();
         openAutofillPopupAndWaitUntilReady(suggestions);
