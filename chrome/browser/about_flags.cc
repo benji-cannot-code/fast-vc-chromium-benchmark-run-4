edfeeb7b48933aff8f7d11ed68cfd2535502283e
Sven Zheng <svenzheng@google.com>
2025-01-14 11:45:32 -0800

    Android: Support feedback with variations state
    When user file a feedback report, also attach Chrome variations state.
    This would be useful for Chrome developers to reproduce an issue that
    was caused by experiments.
    
    When 'Include system logs' is checked, and the experiment is on, user
    can see 'variations_state' tab in the 'System information' under
    'View the information we collect'. And it will be uploaded if user
    send the feedback.
    
    On the backend, it will be saved as variations_state.gz, which is a
    gzipped, base64 encoded, encrypted variations state string. A tool at
    http://go/finch-listnr will be used to decrypt the data.
    This is different with desktop implementations where we upload a bin
    file. 2 reasons:
    1 Any data go through the API will show up in 'System information'.
    So binary data will show as garbled text. Also see the internal cl
    crrev.com/i/7929308, the API addPsbdForSilentFeedback mimeType can
    only be (text/plain, text/html). That's why base64 is used.
    2 All data is gzipped. We can not control this behavior.
    
    Creates a new static library for 'variations_associated_data' to avoid
    dependency cycle. There is a cycle that is hard to avoid:
    variations_associated_data_android.cc
    (//components/variations:variations) -> variations_command_line.h/cc
    variations_command_line.cc -> field_trial_util.h/cc
    field_trial_util.cc -> //components/variations:variations
    Unless we move all 3 files to //components/variations:variations, it's
    hard to avoid. So move variations_associated_data_android.cc to a new
    target.
    
    Design doc: http://go/finch-listnr-design.
    Related internal cl: http://crrev.com/i/7929308
    
    Test: Tested locally that the uploaded file can be decrypted with dev key.
    Bug: 373470046
    Low-Coverage-Reason: TRIVIAL_CHANGE simple interface definition
    Change-Id: Idb7d3553de65a249a8abe0b4978e07d20ea07e28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6114490
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406269}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4303,7 +4303,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"feedback-include-variations",
      flag_descriptions::kFeedbackIncludeVariationsName,
      flag_descriptions::kFeedbackIncludeVariationsDescription,
-     kOsWin | kOsLinux | kOsMac,
+     kOsWin | kOsLinux | kOsMac | kOsAndroid,
      FEATURE_VALUE_TYPE(variations::kFeedbackIncludeVariations)},
 #endif
     {"ui-disable-partial-swap", flag_descriptions::kUiPartialSwapName,
