5572085e6b8cb90d2ed4bd346d3e6f4769d4a6c7
Norman Bintang <normanbt@chromium.org>
2025-01-13 16:37:33 -0800

    vc-ui: Call OnThemeChanged manually
    Call button_container_ and mic_indicator_'s OnThemeChanged manually to
    force update the color.
    
    Bug: 389769280
    Change-Id: I2aad373e153d8232cfb4c40fe48641e20639cea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173854
    Commit-Queue: Norman Bintang <normanbt@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Auto-Submit: Norman Bintang <normanbt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405817}

--- ash/system/video_conference/bubble/title_view.cc
+++ ash/system/video_conference/bubble/title_view.cc
@@ -109,8 +109,9 @@ MicTestButton::MicTestButton() {
   auto* background_layer = background_view_->layer();
   background_layer->SetRoundedCornerRadius(gfx::RoundedCornersF(16));
 
-  AddChildView(std::make_unique<MicTestButtonContainer>(base::BindRepeating(
-      &MicTestButton::OnMicTestButtonClicked, base::Unretained(this))));
+  button_container_ =
+      AddChildView(std::make_unique<MicTestButtonContainer>(base::BindRepeating(
+          &MicTestButton::OnMicTestButtonClicked, base::Unretained(this))));
 }
 
 void MicTestButton::OnThemeChanged() {
@@ -121,6 +122,7 @@ void MicTestButton::OnThemeChanged() {
           ? cros_tokens::kCrosSysSystemPrimaryContainer
           : cros_tokens::kCrosSysSystemOnBase);
   background_view_->layer()->SetColor(color);
+  button_container_->OnThemeChanged();
 }
 
 void MicTestButton::OnMicTestButtonClicked(const ui::Event& event) {
@@ -221,6 +223,7 @@ void MicTestButtonContainer::OnThemeChanged() {
                       : cros_tokens::kCrosSysOnSurface;
   sidetone_icon_->SetImage(
       ui::ImageModel::FromVectorIcon(kVideoConferenceSidetoneIcon, color_id));
+  mic_indicator_->OnThemeChanged();
 }
 
 MicTestButtonContainer::~MicTestButtonContainer() = default;
