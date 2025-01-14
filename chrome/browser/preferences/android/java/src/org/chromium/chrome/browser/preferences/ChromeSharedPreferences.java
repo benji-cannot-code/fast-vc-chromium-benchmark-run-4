18bb1d2d2605ddd207e9734fb25a592a75c37a10
Andrew Grieve <agrieve@chromium.org>
2025-01-14 12:29:11 -0800

    Android: Rename @AssumeNonNull -> @OptimizeAsNonNull
    So as to not be confused with having a meaning for NullAway.
    
    Bug: 40657579
    Change-Id: Ib7aa4e3849ca2753906ac40c5ac029284a63bcf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174943
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406299}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromeSharedPreferences.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromeSharedPreferences.java
@@ -11,7 +11,7 @@ import org.jni_zero.JNINamespace;
 import org.chromium.base.shared_preferences.PreferenceKeyRegistry;
 import org.chromium.base.shared_preferences.SharedPreferencesManager;
 import org.chromium.build.BuildConfig;
-import org.chromium.build.annotations.AssumeNonNull;
+import org.chromium.build.annotations.OptimizeAsNonNull;
 
 @JNINamespace("android::shared_preferences")
 public class ChromeSharedPreferences {
@@ -27,7 +27,7 @@ public class ChromeSharedPreferences {
     /**
      * @return The //base SharedPreferencesManager singleton.
      */
-    @AssumeNonNull
+    @OptimizeAsNonNull
     @CalledByNative
     public static SharedPreferencesManager getInstance() {
         return SharedPreferencesManager.getInstanceForRegistry(REGISTRY);
