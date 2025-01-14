d4ed86ef14c7cfa4b17322e7ca46ed41c196b634
sangwoo ko <sangwoo108@chromium.org>
2025-01-14 10:49:50 -0800

    Pass ImageModel to ImageView::SetImage() in ash/game_dashboard
    The ImageSkia version of ImageView::SetImage() is deprecated.
    There's no intended behavior changes with this patch.
    
    This CL was uploaded by git cl split.
    
    R=phshah@chromium.org
    
    Bug: 40137576
    Change-Id: I60160829100c45419cce8814281a7dd92c4997cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171611
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Auto-Submit: SangWoo Ko <sangwoo108@chromium.org>
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406223}

--- ash/game_dashboard/game_dashboard_network_view.cc
+++ ash/game_dashboard/game_dashboard_network_view.cc
@@ -50,7 +50,7 @@ void GameDashboardNetworkView::UpdateNetworkStateHandlerIcon() {
 
   const bool image_exists = !image.isNull();
   if (image_exists) {
-    SetImage(image);
+    SetImage(ui::ImageModel::FromImageSkia(image));
   }
   SetVisible(image_exists);
 
