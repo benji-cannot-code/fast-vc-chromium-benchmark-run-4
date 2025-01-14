c0d7e9bdb794369d7847e305766b227f1cc229e9
Mingyu Lei <leimy@chromium.org>
2025-01-13 21:19:57 -0800

    Make responder able to handle response in both streaming modes
    The non-streaming responder is an adapter of the streaming mojo
    interface and it's used by prompt API, summarizer and writer/rewriter.
    
    This CL adds an additional field in the `OnStreaming()` callback to
    indicate whether the responder should append or replace the latest
    chunk.
    
    Note that we need to support both mode since not all the built-in AI
    APIs are using the chunk-by-chunk streaming mode, but this will be
    migrated soon. By that time, we should remove the flag.
    
    Bug: 388508408
    Change-Id: I80c2c9377c0f9044cf9e57d721dd413a95e8c805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161375
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405896}

--- chrome/browser/ai/ai_summarizer_unittest.cc
+++ chrome/browser/ai/ai_summarizer_unittest.cc
@@ -175,9 +175,14 @@ TEST_F(AISummarizerUnitTest, SummarizeSuccess) {
   AITestUtils::MockModelStreamingResponder mock_responder;
 
   base::RunLoop run_loop;
-  EXPECT_CALL(mock_responder, OnStreaming(_))
+  EXPECT_CALL(mock_responder, OnStreaming(_, _))
       .WillOnce(testing::Invoke(
-          [&](const std::string& text) { EXPECT_THAT(text, "Test output"); }));
+          [&](const std::string& text,
+              blink::mojom::ModelStreamingResponderAction action) {
+            EXPECT_THAT(text, "Test output");
+            EXPECT_EQ(action,
+                      blink::mojom::ModelStreamingResponderAction::kReplace);
+          }));
 
   EXPECT_CALL(mock_responder, OnCompletion(_))
       .WillOnce(testing::Invoke(
@@ -255,10 +260,14 @@ TEST_F(AISummarizerUnitTest, MultipleSummarizeWithOptions) {
     AITestUtils::MockModelStreamingResponder mock_responder;
 
     base::RunLoop run_loop;
-    EXPECT_CALL(mock_responder, OnStreaming(_))
-        .WillOnce(testing::Invoke([&](const std::string& text) {
-          EXPECT_THAT(text, "Test output1");
-        }));
+    EXPECT_CALL(mock_responder, OnStreaming(_, _))
+        .WillOnce(testing::Invoke(
+            [&](const std::string& text,
+                blink::mojom::ModelStreamingResponderAction action) {
+              EXPECT_THAT(text, "Test output1");
+              EXPECT_EQ(action,
+                        blink::mojom::ModelStreamingResponderAction::kReplace);
+            }));
 
     EXPECT_CALL(mock_responder, OnCompletion(_))
         .WillOnce(testing::Invoke(
@@ -282,10 +291,14 @@ TEST_F(AISummarizerUnitTest, MultipleSummarizeWithOptions) {
     AITestUtils::MockModelStreamingResponder mock_responder;
 
     base::RunLoop run_loop;
-    EXPECT_CALL(mock_responder, OnStreaming(_))
-        .WillOnce(testing::Invoke([&](const std::string& text) {
-          EXPECT_THAT(text, "Test output2");
-        }));
+    EXPECT_CALL(mock_responder, OnStreaming(_, _))
+        .WillOnce(testing::Invoke(
+            [&](const std::string& text,
+                blink::mojom::ModelStreamingResponderAction action) {
+              EXPECT_THAT(text, "Test output2");
+              EXPECT_EQ(action,
+                        blink::mojom::ModelStreamingResponderAction::kReplace);
+            }));
 
     EXPECT_CALL(mock_responder, OnCompletion(_))
         .WillOnce(testing::Invoke(
--- content/browser/ai/echo_ai_language_model.cc
+++ content/browser/ai/echo_ai_language_model.cc
@@ -20,6 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+namespace {
+constexpr char kResponsePrefix[] =
+    "On-device model is not available in Chromium, this API is just echoing "
+    "back the input:\n";
+}
+
 EchoAILanguageModel::EchoAILanguageModel() = default;
 
 EchoAILanguageModel::~EchoAILanguageModel() = default;
@@ -33,11 +39,6 @@ void EchoAILanguageModel::DoMockExecution(
     return;
   }
 
-  const std::string response =
-      "On-device model is not available in Chromium, this API is just echoing "
-      "back the input:\n" +
-      input;
-
   if (input.size() > EchoAIManagerImpl::kMaxContextSizeInTokens) {
     responder->OnError(blink::mojom::ModelStreamingResponseStatus::
                            kErrorPromptRequestTooLarge);
@@ -49,7 +50,10 @@ void EchoAILanguageModel::DoMockExecution(
     responder->OnContextOverflow();
   }
   current_tokens_ += input.size();
-  responder->OnStreaming(response);
+  responder->OnStreaming(kResponsePrefix,
+                         blink::mojom::ModelStreamingResponderAction::kAppend);
+  responder->OnStreaming(input,
+                         blink::mojom::ModelStreamingResponderAction::kAppend);
   responder->OnCompletion(
       blink::mojom::ModelExecutionContextInfo::New(current_tokens_));
 }
