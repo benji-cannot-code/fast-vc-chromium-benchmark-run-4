8ff2597cc0a501af919cfabf5374675bbb53a3d1
Hidehiko Abe <hidehiko@chromium.org>
2025-01-13 17:04:32 -0800

    Migrate more methods in FakeUserManager to the real one.
    To keep the fake behavior closer to the real one.
    User{,Non}CryptohomeDataEphemeral related ones will be done
    in following CLs.
    
    BUG=278643115
    TEST=Tryjob
    
    Change-Id: I542c2469d95c8f4275433e928eab0daec5e70a87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171751
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405836}

--- chrome/browser/ash/floating_workspace/floating_workspace_service_unittest.cc
+++ chrome/browser/ash/floating_workspace/floating_workspace_service_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/floating_workspace/floating_workspace_metrics_util.h"
 #include "chrome/browser/ash/floating_workspace/floating_workspace_service_factory.h"
 #include "chrome/browser/ash/floating_workspace/floating_workspace_util.h"
+#include "chrome/browser/ash/settings/scoped_cros_settings_test_helper.h"
 #include "chrome/browser/notifications/notification_display_service_tester.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
@@ -406,6 +407,8 @@ class FloatingWorkspaceServiceTest : public testing::Test {
 
   void SetUp() override {
     chromeos::PowerManagerClient::InitializeFake();
+    cros_settings_test_helper_ =
+        std::make_unique<ScopedCrosSettingsTestHelper>();
     ash::AshTestHelper::InitParams params;
     ash_test_helper_.SetUp(std::move(params));
     profile_manager_ = std::make_unique<TestingProfileManager>(
@@ -469,7 +472,9 @@ class FloatingWorkspaceServiceTest : public testing::Test {
     profile_ = nullptr;
     profile_manager_ = nullptr;
     mock_desks_client_ = nullptr;
+    fake_user_manager_.Reset();
     ash_test_helper_.TearDown();
+    cros_settings_test_helper_.reset();
     chromeos::PowerManagerClient::Shutdown();
   }
 
@@ -485,6 +490,7 @@ class FloatingWorkspaceServiceTest : public testing::Test {
   std::unique_ptr<NetworkHandlerTestHelper> network_handler_test_helper_;
   std::unique_ptr<apps::AppRegistryCache> cache_;
   AccountId account_id_;
+  std::unique_ptr<ScopedCrosSettingsTestHelper> cros_settings_test_helper_;
   AshTestHelper ash_test_helper_;
   std::unique_ptr<TestingProfileManager> profile_manager_;
   std::unique_ptr<MockDesksClient> mock_desks_client_;
