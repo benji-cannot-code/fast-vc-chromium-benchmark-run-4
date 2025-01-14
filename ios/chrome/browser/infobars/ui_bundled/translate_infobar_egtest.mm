909beac8293d2f3d819cb388d53f04ef93f17447
Fergal Daly <fergal@chromium.org>
2025-01-14 08:56:18 -0800

    Switch use of translate::kUnknownLanguageCode to language_detection::
    This is a no-op refactor.
    
    This is split from https://crrev.com/c/6048141 and covers
    
    chrome/browser/ui/translate
    
    This CL was uploaded by git cl split.
    
    R=thegreenfrog@chromium.org
    
    Bug: 380786760
    Change-Id: I6dc5ddd56ceda6adcfa35f15c43a7f0522f4dc0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6161941
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406130}

--- ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
+++ ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/stringprintf.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
+#import "components/language_detection/core/constants.h"
 #import "components/translate/core/browser/translate_pref_names.h"
-#import "components/translate/core/common/translate_constants.h"
 #import "components/translate/core/common/translate_util.h"
 #import "ios/chrome/browser/badges/ui_bundled/badge_constants.h"
 #import "ios/chrome/browser/infobars/ui_bundled/banners/infobar_banner_constants.h"
@@ -316,7 +316,7 @@ void TestResponseProvider::GetLanguageResponse(
   [self assertContentLanguage:@"it"
              htmlRootLanguage:@"de"
               adoptedLanguage:base::SysUTF8ToNSString(
-                                  translate::kUnknownLanguageCode)];
+                                  language_detection::kUnknownLanguageCode)];
 }
 
 // Tests that hidden text is not considered during detection.
@@ -334,7 +334,7 @@ void TestResponseProvider::GetLanguageResponse(
   [self assertContentLanguage:@""
              htmlRootLanguage:@""
               adoptedLanguage:base::SysUTF8ToNSString(
-                                  translate::kUnknownLanguageCode)];
+                                  language_detection::kUnknownLanguageCode)];
 }
 
 // Tests that language detection is still performed when the page specifies the
@@ -379,7 +379,7 @@ void TestResponseProvider::GetLanguageResponse(
   [self assertContentLanguage:@""
              htmlRootLanguage:@""
               adoptedLanguage:base::SysUTF8ToNSString(
-                                  translate::kUnknownLanguageCode)];
+                                  language_detection::kUnknownLanguageCode)];
 
   // Resets state before triggering a new round of language detection.
   [TranslateAppInterface resetLanguageDetectionTabHelperObserver];
