9c2e20646bdfd6fc9823783b019ab3e928a82053
David Sanders <dsanders11@ucsbalum.com>
2025-01-14 10:21:20 -0800

    Remove unused include in array_buffer_contents.h
    Became unused after https://chromium-review.googlesource.com/c/chromium/src/+/1186333
    
    Bug: 40318405
    Change-Id: I4870fa28d7b965c7ec3a40c7eb3c14a62a5d6180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174317
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406197}

--- third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer_contents.h
+++ third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer_contents.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/memory/platform_shared_memory_region.h"
 #include "base/memory/scoped_refptr.h"
-#include "partition_alloc/page_allocator.h"
 #include "partition_alloc/partition_alloc_constants.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
