6a3ff86cc3ede3513b5a83142090d1db4488058a
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2025-01-14 10:03:12 -0800

    [views-ax] Reland tab group header tooltip text refactor
    CL:6154289 reverted the TabGroupHeader changes from CL:6027039,
    this was due to as a side effect of the revert needed for bug 388331744,
    we would hit a dcheck unless these TabGroupHeader changes were reverted
    too.
    
    One of the owners let us know that the `OnGroupChanged` logic
    was not actually needed, since tab group headers can't change
    group affiliation after creation. The removal of this
    logic was all we needed to fix the DCHECK, as this ensured
    that setting the role would happen before updating the tooltip
    text (which set the accessible description), which
    fixes the issue.
    
    With this CL,
    I can not repro the bug in 388331744 anymore, nor the DCHECK.
    
    Bug: 325137417, 378724151, 388331744
    Change-Id: I11aba1218508bf490441edfd790336ca3348210c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168227
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406187}

--- chrome/browser/ui/views/tabs/tab_group_header.h
+++ chrome/browser/ui/views/tabs/tab_group_header.h
@@ -59,8 +59,9 @@ class TabGroupHeader : public TabSlotView,
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
   TabSlotView::ViewType GetTabSlotViewType() const override;
   TabSizeInfo GetTabSizeInfo() const override;
-  std::u16string GetTooltipText(const gfx::Point& p) const override;
   gfx::Rect GetAnchorBoundsInScreen() const override;
+
+  void UpdateTooltipText();
 
   // views::ContextMenuController:
   void ShowContextMenuForViewImpl(
