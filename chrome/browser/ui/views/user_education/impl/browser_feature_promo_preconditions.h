b244b7b90e98d2bce71c67dead1841414e4272bf
Dana Fried <dfried@chromium.org>
2025-01-14 08:35:01 -0800

    [User Education] Minor fixes to browser promo preconditions
    This adds a safeguard for preconditions created before the browser
    window is fully initialized, as well as some additional documentation
    and renames an incorrectly-named test.
    
    Low-Coverage-Reason: HARD_TO_TEST
    Bug: 369887098
    Change-Id: Iafdd8f75c9ae4f80a4ae32931a67b407cda4fb7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173474
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406119}

--- chrome/browser/ui/views/user_education/impl/browser_feature_promo_preconditions.h
+++ chrome/browser/ui/views/user_education/impl/browser_feature_promo_preconditions.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ref.h"
+#include "base/scoped_observation.h"
 #include "base/time/clock.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
@@ -19,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/events/event_observer.h"
 #include "ui/views/event_monitor.h"
+#include "ui/views/view.h"
+#include "ui/views/view_observer.h"
 
 DECLARE_FEATURE_PROMO_PRECONDITION_IDENTIFIER_VALUE(kWindowActivePrecondition);
 DECLARE_FEATURE_PROMO_PRECONDITION_IDENTIFIER_VALUE(
@@ -58,6 +61,10 @@ class OmniboxNotOpenPrecondition
   const raw_ref<const BrowserView> browser_view_;
 };
 
+// Trying to show an IPH when the toolbar is collapsed (Responsive Toolbar) is
+// inadvisable, as the anchor may not be present, important UI for the feature
+// might not be present (including tutorial elements), and the UI might be too
+// small to properly accommodate a help bubble.
 class ToolbarNotCollapsedPrecondition
     : public user_education::FeaturePromoPreconditionBase {
  public:
@@ -110,7 +117,8 @@ class NoCriticalNoticeShowingPrecondition
 // Don't show heavyweight notices while the user is typing.
 class UserNotActivePrecondition
     : public user_education::FeaturePromoPreconditionBase,
-      public ui::EventObserver {
+      public ui::EventObserver,
+      public views::ViewObserver {
  public:
   explicit UserNotActivePrecondition(
       BrowserView& browser_view,
@@ -122,13 +130,21 @@ class UserNotActivePrecondition
       ComputedData& data) const override;
 
  private:
+  void CreateEventMonitor();
+
   // ui::EventObserver:
   void OnEvent(const ui::Event& event) override;
+
+  // views::ViewObserver:
+  void OnViewAddedToWidget(views::View* observed_view) override;
+  void OnViewIsDeleting(views::View* observed_view) override;
 
   const raw_ref<BrowserView> browser_view_;
   const raw_ref<const user_education::UserEducationTimeProvider> time_provider_;
   std::unique_ptr<views::EventMonitor> event_monitor_;
   base::Time last_active_time_;
+  base::ScopedObservation<views::View, views::ViewObserver>
+      browser_view_observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_USER_EDUCATION_IMPL_BROWSER_FEATURE_PROMO_PRECONDITIONS_H_
--- chrome/browser/ui/views/user_education/impl/browser_feature_promo_preconditions_interactive_uitest.cc
+++ chrome/browser/ui/views/user_education/impl/browser_feature_promo_preconditions_interactive_uitest.cc
@@ -314,11 +314,11 @@ IN_PROC_BROWSER_TEST_F(BrowserNotClosingPreconditionUiTest,
           .SetMustRemainVisible(false));
 }
 
-class BrowserNotActivePreconditionUiTest
+class UserNotActivePreconditionUiTest
     : public BrowserFeaturePromoPreconditionsUiTest {
  public:
-  BrowserNotActivePreconditionUiTest() = default;
-  ~BrowserNotActivePreconditionUiTest() override = default;
+  UserNotActivePreconditionUiTest() = default;
+  ~UserNotActivePreconditionUiTest() override = default;
 
   void SetUpOnMainThread() override {
     BrowserFeaturePromoPreconditionsUiTest::SetUpOnMainThread();
@@ -368,13 +368,13 @@ class BrowserNotActivePreconditionUiTest
   std::unique_ptr<ui::test::EventGenerator> event_generator_;
 };
 
-IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest, ReturnsSuccess) {
+IN_PROC_BROWSER_TEST_F(UserNotActivePreconditionUiTest, ReturnsSuccess) {
   RunTestSequence(
       WaitForShow(kBrowserViewElementId),
       CheckPrecondResult(user_education::FeaturePromoResult::Success()));
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest,
+IN_PROC_BROWSER_TEST_F(UserNotActivePreconditionUiTest,
                        ReturnsBlockedAfterMouseClick) {
   RunTestSequence(
       WaitForShow(kBrowserViewElementId),
@@ -396,7 +396,7 @@ IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest,
 #define MAYBE_ReturnsBlockedAfterMouseHoverOverTabstrip \
   ReturnsBlockedAfterMouseHoverOverTabstrip
 #endif
-IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest,
+IN_PROC_BROWSER_TEST_F(UserNotActivePreconditionUiTest,
                        MAYBE_ReturnsBlockedAfterMouseHoverOverTabstrip) {
   RunTestSequence(
       WaitForShow(kBrowserViewElementId),
@@ -409,7 +409,7 @@ IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest,
       CheckPrecondResult(user_education::FeaturePromoResult::Success()));
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest,
+IN_PROC_BROWSER_TEST_F(UserNotActivePreconditionUiTest,
                        ReturnsSuccessWhenHoveringOutsideTopContainer) {
   gfx::Point start;
   gfx::Point finish;
@@ -436,7 +436,7 @@ IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest,
       CheckPrecondResult(user_education::FeaturePromoResult::Success()));
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserNotActivePreconditionUiTest,
+IN_PROC_BROWSER_TEST_F(UserNotActivePreconditionUiTest,
                        ReturnsBlockedAfterKeyPress) {
   RunTestSequence(
       WaitForShow(kBrowserViewElementId), Check([this]() {
