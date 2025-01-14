b7df4afd322e6430a13bdcdcec1563fc96049177
Ming-Ying Chung <mych@chromium.org>
2025-01-14 01:02:49 -0800

    [fetch-later] Update "Reserve deferred-fetch quota" implementation
    This updates the [fetch-later][3] feature which is in OT. However, no
    real prod impact as changes are gated behind a flag.
    
    This CL updates the implementation of the [algorithm][1], which is the
    `GetContainerDeferredFetchPolicyOnNavigation()` method:
    
    - Move its call site into `FrameLoader::StartNavigation()` to match the
      [Step 22. in the new html spec PR][2]. As a result, now it needs to
      take additional arg `to_url` as the navigation just starts.
    - Allow it to trigger `PermissionsPolicy::InheritedValueForFeature()`,
      which is necessary for its quota calculation.
    
    [1]: https://whatpr.org/fetch/1647.html#reserve-deferred-fetch-quota
    [2]: https://whatpr.org/html/10903/d1c086a...0e0afb3/browsing-the-web.html#beginning-navigation
    [3]: https://developer.chrome.com/blog/fetch-later-api-origin-trial
    
    Bug: 40276121
    Change-Id: If5423428c3f4a059761bf6e89d788ad2130aaf67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6162375
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405954}

--- third_party/blink/renderer/core/html/html_frame_owner_element.cc
+++ third_party/blink/renderer/core/html/html_frame_owner_element.cc
@@ -427,12 +427,12 @@ void HTMLFrameOwnerElement::UpdateRequiredPolicy() {
   }
 }
 
-void HTMLFrameOwnerElement::UpdateDeferredFetchPolicy() {
+void HTMLFrameOwnerElement::UpdateDeferredFetchPolicy(const KURL& to_url) {
   if (!IsFetchLaterUseDeferredFetchPolicyEnabled()) {
     return;
   }
   frame_policy_.deferred_fetch_policy =
-      GetContainerDeferredFetchPolicyOnNavigation(this);
+      FetchLaterUtil::GetContainerDeferredFetchPolicyOnNavigation(this, to_url);
 
   if (ContentFrame()) {
     auto* frame = GetDocument().GetFrame();
