bd47dc838cd0358a483e2ce84d88cbdf16add0e7
Tom Anderson <thomasanderson@chromium.org>
2025-01-13 19:05:24 -0800

    Create secret and collection if needed in FreedesktopSecretKeyProvider
    * Secret creation is necessary on first run.
    * Create the default collection if one does not already exist. This is
      necessary to match the behavior of the old libsecret backend.
    
    R=thestig
    
    Bug: 40086962
    Change-Id: I26a47f921148d213f39588d62ea89df74d35d7e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111477
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405868}

--- components/os_crypt/async/browser/freedesktop_secret_key_provider.h
+++ components/os_crypt/async/browser/freedesktop_secret_key_provider.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/ref_counted_memory.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/types/expected.h"
@@ -93,8 +94,10 @@ class FreedesktopSecretKeyProvider : public KeyProvider {
       "org.freedesktop.Secret.Session";
 
   static constexpr char kMethodReadAlias[] = "ReadAlias";
+  static constexpr char kMethodCreateCollection[] = "CreateCollection";
   static constexpr char kMethodGetSecret[] = "GetSecret";
   static constexpr char kMethodOpenSession[] = "OpenSession";
+  static constexpr char kMethodCreateItem[] = "CreateItem";
   static constexpr char kMethodUnlock[] = "Unlock";
   static constexpr char kMethodClose[] = "Close";
   static constexpr char kMethodSearchItems[] = "SearchItems";
@@ -124,8 +127,10 @@ class FreedesktopSecretKeyProvider : public KeyProvider {
   static constexpr char kLabelProperty[] = "Label";
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  constexpr static char kKeyName[] = "Chrome Safe Storage";
   static constexpr char kAppName[] = "chrome";
 #else
+  constexpr static char kKeyName[] = "Chromium Safe Storage";
   static constexpr char kAppName[] = "chromium";
 #endif
 
@@ -134,6 +139,9 @@ class FreedesktopSecretKeyProvider : public KeyProvider {
       base::expected<DbusObjectPath, ErrorDetail> collection_path);
   void OnGetCollectionLabelResponse(
       base::expected<DbusVariant, ErrorDetail> variant);
+  void OnCreateCollection(
+      base::expected<DbusParameters<DbusObjectPath, DbusObjectPath>,
+                     ErrorDetail> create_collection_reply);
   void OnUnlock(
       base::expected<DbusParameters<DbusArray<DbusObjectPath>, DbusObjectPath>,
                      ErrorDetail> unlocked_collection);
@@ -145,6 +153,11 @@ class FreedesktopSecretKeyProvider : public KeyProvider {
 
   void UnlockDefaultCollection();
   void OpenSession();
+  void CreateItem(scoped_refptr<base::RefCountedMemory> secret);
+  void OnCreateItem(
+      scoped_refptr<base::RefCountedMemory> secret,
+      base::expected<DbusParameters<DbusObjectPath, DbusObjectPath>,
+                     ErrorDetail> created_item);
   void DeriveKeyFromSecret(base::span<const uint8_t> secret);
   void FinalizeSuccess(Encryptor::Key key);
   void FinalizeFailure(InitStatus status, ErrorDetail detail);
