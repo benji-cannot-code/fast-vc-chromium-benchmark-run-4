ae2009165c0b80d61dbbe49dfa9d536c3b8d3d1f
Peter Kasting <pkasting@chromium.org>
2025-01-14 07:36:40 -0800

    Misc. IWYU fixes
    Bug: 386918226
    Change-Id: I6ed8f5c25dfd2292cbd79245f9e6cff360a3bac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164374
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406095}

--- components/content_settings/core/browser/content_settings_uma_util.cc
+++ components/content_settings/core/browser/content_settings_uma_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/fixed_flat_map.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
+#include "base/ranges/functional.h"
 #include "base/strings/strcat.h"
 #include "components/content_settings/core/common/content_settings.h"
 
