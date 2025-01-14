29564941ac4105ff9cc3cc3379c82ea3cfc8262e
Neil Coronado <nemco@google.com>
2025-01-14 12:27:59 -0800

    [TabGroupReorder] Attach tabs and group titles to separate layers
    Attach tabs and group titles to separate layers.
    -Relevant in child CL where we can't enforce the z-indexing of the
     layers. Instead, we need a different parent layer altogether to ensure
     that the foregrounded tabs appear on top of the foregrounded group
     titles (and therefore in front of the reorder backing shape).
    
    Bug: 376068489
    Change-Id: Idc8233eb4db9ed71ebbe77c634f1bce223ac2deb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173107
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406297}

--- chrome/browser/android/compositor/scene_layer/tab_strip_scene_layer.cc
+++ chrome/browser/android/compositor/scene_layer/tab_strip_scene_layer.cc
@@ -32,6 +32,8 @@ TabStripSceneLayer::TabStripSceneLayer(JNIEnv* env,
       group_ui_parent_layer_(cc::slim::Layer::Create()),
       scrollable_strip_layer_(cc::slim::Layer::Create()),
       foreground_layer_(cc::slim::Layer::Create()),
+      foreground_tabs_(cc::slim::Layer::Create()),
+      foreground_group_titles_(cc::slim::Layer::Create()),
       new_tab_button_(cc::slim::UIResourceLayer::Create()),
       new_tab_button_background_(cc::slim::UIResourceLayer::Create()),
       left_fade_(cc::slim::UIResourceLayer::Create()),
@@ -60,10 +62,15 @@ TabStripSceneLayer::TabStripSceneLayer(JNIEnv* env,
   group_ui_parent_layer_->SetIsDrawable(true);
   scrollable_strip_layer_->SetIsDrawable(true);
   foreground_layer_->SetIsDrawable(true);
+  foreground_tabs_->SetIsDrawable(true);
+  foreground_group_titles_->SetIsDrawable(true);
   tab_strip_layer_->SetIsDrawable(true);
+
   tab_strip_layer_->AddChild(group_ui_parent_layer_);
   tab_strip_layer_->AddChild(scrollable_strip_layer_);
   tab_strip_layer_->AddChild(foreground_layer_);
+  foreground_layer_->AddChild(foreground_group_titles_);
+  foreground_layer_->AddChild(foreground_tabs_);
 
   tab_strip_layer_->AddChild(left_fade_);
   tab_strip_layer_->AddChild(right_fade_);
@@ -423,7 +430,7 @@ void TabStripSceneLayer::PutStripTabLayer(
 
   if (foreground != layer->foreground()) {
     if (foreground) {
-      foreground_layer_->AddChild(layer->layer());
+      foreground_tabs_->AddChild(layer->layer());
     } else {
       scrollable_strip_layer_->AddChild(layer->layer());
     }
@@ -487,7 +494,7 @@ void TabStripSceneLayer::PutGroupIndicatorLayer(
   // Foreground if needed.
   if (foreground != layer->foreground()) {
     if (foreground) {
-      foreground_layer_->AddChild(layer->layer());
+      foreground_group_titles_->AddChild(layer->layer());
     } else {
       group_ui_parent_layer_->AddChild(layer->layer());
     }
