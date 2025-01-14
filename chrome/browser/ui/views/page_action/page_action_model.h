decbf9f5b85bba9ae01768081fc6619f86e71669
Christopher Grant <cjgrant@chromium.org>
2025-01-14 10:02:15 -0800

    Page Actions: Create a compound ActionItem setter on the model
    This lets PageActionController push new ActionItem state into the model
    in a single call, yielding a single notification to model observers.
    
    This is tested indirectly through existing controller and view tests.
    crbug.com/388527536 tracks adding explicit coverage on PageActionModel.
    
    Bug: 388264699
    Change-Id: I73e8333016db1741457c332f0a9483ec38e2c589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173169
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406186}

--- chrome/browser/ui/views/page_action/page_action_model.h
+++ chrome/browser/ui/views/page_action/page_action_model.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/pass_key.h"
 #include "ui/base/models/image_model.h"
 
+namespace actions {
+class ActionItem;
+}  // namespace actions
+
 namespace page_actions {
 
 class PageActionController;
@@ -28,21 +32,20 @@ class PageActionModel {
   void AddObserver(PageActionModelObserver* observer);
   void RemoveObserver(PageActionModelObserver* observer);
 
+  // Applies any relevant ActionItem properties to the model, including
+  // visibility, text and icon properties.
+  void SetActionItemProperties(base::PassKey<PageActionController>,
+                               const actions::ActionItem* action_item);
+
   void SetShowRequested(base::PassKey<PageActionController>, bool requested);
-  void SetActionItemEnabled(base::PassKey<PageActionController>, bool enabled);
-  void SetActionItemVisible(base::PassKey<PageActionController>, bool visible);
   void SetOverrideText(base::PassKey<PageActionController>,
                        const std::optional<std::u16string>& override_text);
+
   // The model distills all visibility properties into a single result.
   bool GetVisible() const;
 
-  void SetImage(const ui::ImageModel& image);
   const ui::ImageModel& GetImage() const;
-
-  void SetText(const std::u16string& text);
   const std::u16string GetText() const;
-
-  void SetTooltipText(const std::u16string& tooltip);
   const std::u16string GetTooltipText() const;
 
  private:
