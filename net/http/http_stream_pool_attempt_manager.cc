c260af0d1a3491c8ef85fb5a900eb90ac79ede64
Kenichi Ishibashi <bashi@chromium.org>
2025-01-13 20:41:18 -0800

    HttpStreamPool: Add some histograms for QUIC/SPDY session usage
    This CL add three histograms:
    * Record time to find an existing QUIC session
    * Record time to find an existing SPDY session
    * Record time to establish a new SPDY session
    
    These histogram would be useful to adjust some delay timers to
    reduce spurious stream attempts that would be discarded after
    creating/using a QUIC/SPDY session.
    
    Bug: 384759483
    Change-Id: I3787518c025a60203033b729fe49209f070e5316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166383
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1405888}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -988,6 +988,9 @@ bool HttpStreamPool::AttemptManager::
           quic_task_result_ = OK;
           quic_task_.reset();
         }
+        base::UmaHistogramTimes(
+            "Net.HttpStreamPool.ExistingQuicSessionFoundTime",
+            base::TimeTicks::Now() - dns_resolution_start_time_);
         HandleQuicSessionReady(
             StreamSocketCloseReason::kUsingExistingQuicSession);
         // Use PostTask() because we could reach here from RequestStream()
@@ -1015,6 +1018,9 @@ bool HttpStreamPool::AttemptManager::
             spdy_session_key(), endpoint,
             service_endpoint_request_->GetDnsAliasResults());
     if (spdy_session_) {
+      base::UmaHistogramTimes(
+          "Net.HttpStreamPool.ExistingSpdySessionFoundTime",
+          base::TimeTicks::Now() - dns_resolution_start_time_);
       HandleSpdySessionReady(
           StreamSocketCloseReason::kUsingExistingSpdySession);
       // Use PostTask() because we could reach here from RequestStream()
@@ -1819,6 +1825,10 @@ void HttpStreamPool::AttemptManager::OnInFlightAttemptComplete(
           stream_key().destination(), stream_key().network_anonymization_key(),
           /*supports_spdy=*/true);
     }
+
+    base::UmaHistogramTimes(
+        "Net.HttpStreamPool.NewSpdySessionEstablishTime",
+        base::TimeTicks::Now() - in_flight_attempt->start_time());
 
     HandleSpdySessionReady(StreamSocketCloseReason::kSpdySessionCreated);
     CreateSpdyStreamAndNotify();
