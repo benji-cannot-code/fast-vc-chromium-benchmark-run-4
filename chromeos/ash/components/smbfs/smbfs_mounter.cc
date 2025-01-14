dc496b3b889a61748918d3a5575234df31e31c61
Andrea Orru <andreaorru@chromium.org>
2025-01-14 04:31:00 -0800

    ipcz+cros: non-broker to non-broker connection to smbfs
    As part of the effort of bringing ipcz to ChromeOS, we need to
    support a few instances in which processes that are non-brokers
    connect to each other.
    
    To do that, ipcz requires using invitation flags defined upstream in
    https://crrev.com/c/4554178 to support this use case.
    
    BUG=b:41494930
    TEST=CQ
    
    Change-Id: I5f46d04785305bd3cd483e43c277e2bf149ec907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171472
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406016}

--- chromeos/ash/components/smbfs/smbfs_mounter.cc
+++ chromeos/ash/components/smbfs/smbfs_mounter.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/strcat.h"
 #include "chromeos/components/mojo_bootstrap/pending_connection_manager.h"
+#include "mojo/core/configuration.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 
@@ -187,6 +188,10 @@ void SmbFsMounter::OnMountDone(
 
 void SmbFsMounter::OnIpcChannel(base::ScopedFD mojo_fd) {
   DCHECK(mojo_fd.is_valid());
+  if (!mojo::core::GetConfiguration().is_broker_process) {
+    bootstrap_invitation_.set_extra_flags(
+        MOJO_SEND_INVITATION_FLAG_SHARE_BROKER);
+  }
   mojo::OutgoingInvitation::Send(
       std::move(bootstrap_invitation_), base::kNullProcessHandle,
       mojo::PlatformChannelEndpoint(mojo::PlatformHandle(std::move(mojo_fd))));
