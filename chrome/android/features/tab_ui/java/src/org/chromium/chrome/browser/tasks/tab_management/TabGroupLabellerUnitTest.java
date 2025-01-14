34ae8970a3d00127bb70ceac5f52610e96723fc9
Calder Kitagawa <ckitagawa@chromium.org>
2025-01-14 08:54:30 -0800

    [Data Sharing] Rework TabGroupLabeller IDs
    Tabs in TabListMediator are represented by the last shown tab ID in the
    tab group. Update the TabGroupLabeller to use this same type of ID for
    label updates so it is compatible. Previously the TabGroupLabeller was
    using Root ID instead.
    
    Longer term TAB_ID should be split to TAB_ID for tabs and TAB_GROUP_ID
    (token) for groups to make this less of an issue. However, until time
    for a much bigger refactor is available this is the best we can do.
    
    Tested locally in addition to updating unit tests.
    
    Fixed: 387509285
    Change-Id: Ica47804248435299bf69b2bdaf0df4defab4416e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172860
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406129}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupLabellerUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupLabellerUnitTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,6 +52,8 @@ public class TabGroupLabellerUnitTest {
     @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     private static final Token GROUP_ID1 = new Token(1L, 11L);
+    private static final int ROOT_ID1 = 0;
+    private static final int TAB_ID0 = 0;
     private static final int TAB_ID1 = 1;
 
     @Mock private Profile mProfile;
@@ -72,7 +75,8 @@ public class TabGroupLabellerUnitTest {
         MessagingBackendServiceFactory.setForTesting(mMessagingBackendService);
         mContext = ApplicationProvider.getApplicationContext();
         mTabGroupModelFilterSupplier.set(mTabGroupModelFilter);
-        when(mTabGroupModelFilter.getRootIdFromStableId(GROUP_ID1)).thenReturn(TAB_ID1);
+        when(mTabGroupModelFilter.getRootIdFromStableId(GROUP_ID1)).thenReturn(ROOT_ID1);
+        when(mTabGroupModelFilter.getGroupLastShownTabId(ROOT_ID1)).thenReturn(TAB_ID1);
         mTabGroupLabeller =
                 new TabGroupLabeller(
                         mProfile, mTabListNotificationHandler, mTabGroupModelFilterSupplier);
@@ -124,6 +128,7 @@ public class TabGroupLabellerUnitTest {
     @Test
     public void testShowAll_WrongTabModel() {
         when(mTabGroupModelFilter.getRootIdFromStableId(any())).thenReturn(Tab.INVALID_TAB_ID);
+        when(mTabGroupModelFilter.getGroupLastShownTabId(anyInt())).thenReturn(Tab.INVALID_TAB_ID);
         List<PersistentMessage> messageList = List.of(makeStandardMessage());
         when(mMessagingBackendService.getMessages(any())).thenReturn(messageList);
 
