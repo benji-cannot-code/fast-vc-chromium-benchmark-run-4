2da8b00a96c0b898f3cfa851d31c2c3942ffa1c8
David Sanders <dsanders11@ucsbalum.com>
2025-01-14 10:59:55 -0800

    Remove unused include in autofill_webdata_service.h
    Bug: 40318405
    Change-Id: I558cf4a8e2c85c3edf7bdfc3d2923d4615bc271d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6165607
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406229}

--- components/autofill/core/browser/webdata/autofill_webdata_service.h
+++ components/autofill/core/browser/webdata/autofill_webdata_service.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/webdata/common/web_data_results.h"
 #include "components/webdata/common/web_data_service_base.h"
 #include "components/webdata/common/web_data_service_consumer.h"
-#include "components/webdata/common/web_database.h"
 
 class WebDatabaseService;
 
