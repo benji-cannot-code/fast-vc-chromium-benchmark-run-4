ce5b1a740730f5f34d77beb33a9e3d9ea7cabe06
Clark DuVall <cduvall@chromium.org>
2025-01-15 11:08:01 -0800

    Add crash keys to debug crbug.com/342226953
    It appears the GPU blocklist logic isn't quite working as intended in
    some rare cases, so use crash keys to get more info.
    
    Bug: 342226953
    Change-Id: I670373d3b6d725c57cea6ff59cdff53aeb2d594e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177633
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406845}

--- services/on_device_model/ml/gpu_blocklist.cc
+++ services/on_device_model/ml/gpu_blocklist.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/string_number_conversions.h"
+#include "components/crash/core/common/crash_key.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "gpu/config/webgpu_blocklist_impl.h"
 #include "third_party/dawn/include/dawn/webgpu_cpp.h"
@@ -44,11 +46,22 @@ GpuBlockedReason IsGpuBlockedInternal(const ChromeMLAPI& api) {
     bool is_blocklisted_cpu_adapter;
   };
 
+  static crash_reporter::CrashKeyString<256> blocklist_key(
+      "ChromeML-blocklist");
+  blocklist_key.Set(kGpuBlockList.Get());
+
   QueryData query_data;
   if (!api.QueryGPUAdapter(
           [](WGPUAdapter cAdapter, void* data) {
             wgpu::Adapter adapter(cAdapter);
             auto* query_data = static_cast<QueryData*>(data);
+
+            wgpu::AdapterInfo info;
+            adapter.GetInfo(&info);
+            static crash_reporter::CrashKeyString<32> device_key(
+                "ChromeML-device");
+            device_key.Set(base::NumberToString(info.vendorID) + ":" +
+                           base::NumberToString(info.deviceID));
 
             query_data->blocklisted =
                 gpu::IsWebGPUAdapterBlocklisted(
@@ -62,8 +75,6 @@ GpuBlockedReason IsGpuBlockedInternal(const ChromeMLAPI& api) {
                     })
                     .blocked;
             if (query_data->blocklisted) {
-              wgpu::AdapterInfo info;
-              adapter.GetInfo(&info);
               query_data->is_blocklisted_cpu_adapter =
                   info.adapterType == wgpu::AdapterType::CPU;
             }
