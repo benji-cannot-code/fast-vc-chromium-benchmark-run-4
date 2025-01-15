b2d5c1015bfb86c8d88f6530b1683ba1b23898f5
Colin Blundell <blundell@chromium.org>
2025-01-15 06:40:35 -0800

    [//gpu] Remove kSWVideoFrameAddScanoutUsageOnlyIfSupportedBySharedImage
    Shipped in 131 [1], and there have been no reported problems.
    
    [1] https://chromiumdash.appspot.com/commit/f5d51971005346d347302bebca21235e85d12096
    
    Bug: 330865436
    Change-Id: I5d3128f7637522dd9bc1d43a6f52beae9a939c45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176745
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406693}

--- gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory.cc
@@ -53,10 +53,6 @@ bool SupportScanout() {
     return true;
   }
   if (!base::FeatureList::IsEnabled(
-          features::kSWVideoFrameAddScanoutUsageOnlyIfSupportedBySharedImage)) {
-    return true;
-  }
-  if (!base::FeatureList::IsEnabled(
           features::kViewTreeHostAddScanoutUsageOnlyIfSupportedBySharedImage)) {
     return true;
   }
--- gpu/config/gpu_finch_features.h
+++ gpu/config/gpu_finch_features.h
@@ -86,8 +86,6 @@ GPU_EXPORT BASE_DECLARE_FEATURE(
 GPU_EXPORT BASE_DECLARE_FEATURE(
     kRoundedDisplayAddScanoutUsageOnlyIfSupportedBySharedImage);
 GPU_EXPORT BASE_DECLARE_FEATURE(
-    kSWVideoFrameAddScanoutUsageOnlyIfSupportedBySharedImage);
-GPU_EXPORT BASE_DECLARE_FEATURE(
     kViewTreeHostAddScanoutUsageOnlyIfSupportedBySharedImage);
 
 GPU_EXPORT BASE_DECLARE_FEATURE(kConditionallySkipGpuChannelFlush);
