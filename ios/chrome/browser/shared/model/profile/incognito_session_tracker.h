510b6093d3225120b650cd7a52b0141fcf5f35b8
Sylvain Defresne <sdefresne@chromium.org>
2025-01-15 11:39:45 -0800

    [ios] Move incognito_session_tracker* files
    Move the files to //ios/chrome/browser/shared/model/profile where they
    belong since ChromeBrowserState has been renamed to ProfileIOS.
    
    Fixed: 358584545
    Change-Id: I1fa7bd662a26aaae8e92247d82f044140f23c44b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173880
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406867}

--- ios/chrome/browser/shared/model/profile/incognito_session_tracker.h
+++ ios/chrome/browser/shared/model/profile/incognito_session_tracker.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_STATE_INCOGNITO_SESSION_TRACKER_H_
-#define IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_STATE_INCOGNITO_SESSION_TRACKER_H_
+#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_INCOGNITO_SESSION_TRACKER_H_
+#define IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_INCOGNITO_SESSION_TRACKER_H_
 
 #import "base/callback_list.h"
 #import "base/containers/flat_map.h"
@@ -77,4 +77,4 @@ class IncognitoSessionTracker final : public ProfileManagerObserverIOS {
   SEQUENCE_CHECKER(sequence_checker_);
 };
 
-#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_STATE_INCOGNITO_SESSION_TRACKER_H_
+#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_INCOGNITO_SESSION_TRACKER_H_
