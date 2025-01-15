ee1c146c7042cd15f4302bbd845f3dec9b07040a
Moe Ahmadi <mahmadi@chromium.org>
2025-01-15 11:49:17 -0800

    [passage-embeddings] Keep track of embedder running state
    Note that EmbedderRunning() can have false positives. It will falsely
    report true while requests are queued in the pipe before the embedder
    is connected. However that state is expected to be short-lived.
    
    Bug: 389692830
    Change-Id: I4c754cbe71c3381c453e4ccfbb50a7c9e83ac7dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169658
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406874}

--- components/passage_embeddings/passage_embeddings_service_controller.h
+++ components/passage_embeddings/passage_embeddings_service_controller.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSAGE_EMBEDDINGS_PASSAGE_EMBEDDINGS_SERVICE_CONTROLLER_H_
 #define COMPONENTS_PASSAGE_EMBEDDINGS_PASSAGE_EMBEDDINGS_SERVICE_CONTROLLER_H_
 
+#include <vector>
+
 #include "base/types/optional_ref.h"
 #include "components/optimization_guide/core/model_info.h"
 #include "components/optimization_guide/proto/passage_embeddings_model_metadata.pb.h"
@@ -40,6 +42,9 @@ class PassageEmbeddingsServiceController {
   // Returns true if this service controller is ready for embeddings generation.
   bool EmbedderReady();
 
+  // Returns true if the embedder is currently running.
+  bool EmbedderRunning();
+
   // Returns the metadata about the embeddings model. This is only valid when
   // EmbedderReady() returns true.
   EmbedderMetadata GetEmbedderMetadata();
@@ -60,6 +65,10 @@ class PassageEmbeddingsServiceController {
   mojo::Remote<mojom::PassageEmbeddingsService> service_remote_;
 
  private:
+  // uint64_t is large enough to never overflow.
+  using RequestId = uint64_t;
+  RequestId next_request_id_ = 0;
+
   // Called when the model files on disks are opened and ready to be sent to
   // the service.
   void LoadModelsToService(
@@ -68,6 +77,11 @@ class PassageEmbeddingsServiceController {
 
   // Called when an attempt to load models to service finishes.
   void OnLoadModelsResult(bool success);
+
+  // Called when an attempt to generate embeddings finishes.
+  void OnGotEmbeddings(RequestId request_id,
+                       GetEmbeddingsCallback callback,
+                       std::vector<mojom::PassageEmbeddingsResultPtr> results);
 
   // Version of the embeddings model.
   int64_t model_version_;
@@ -80,6 +94,9 @@ class PassageEmbeddingsServiceController {
   base::FilePath sp_model_path_;
 
   mojo::Remote<mojom::PassageEmbedder> embedder_remote_;
+
+  // Pending requests to generate embeddings.
+  std::vector<RequestId> pending_requests_;
 
   // Used to generate weak pointers to self.
   base::WeakPtrFactory<PassageEmbeddingsServiceController> weak_ptr_factory_{
