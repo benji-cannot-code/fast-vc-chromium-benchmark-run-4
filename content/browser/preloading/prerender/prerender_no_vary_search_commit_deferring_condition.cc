aa4ae5e039804c093798828f91f9c7936ab9a090
Lingqi Chi <lingqi@chromium.org>
2025-01-14 20:41:22 -0800

    [Prerender] record deferring duration of PrerenderNoVarySearchCommitDeferringCondition
    Traces show that it could be slow to update a URL. This CL adds a
    metric to measure the latency.
    
    Bug: 41494389
    Change-Id: I51e62b242e34faf8997a5b2653872fad8ae438ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164958
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406508}

--- content/browser/preloading/prerender/prerender_no_vary_search_commit_deferring_condition.cc
+++ content/browser/preloading/prerender/prerender_no_vary_search_commit_deferring_condition.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/preloading/prerender/prerender_no_vary_search_commit_deferring_condition.h"
 
+#include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
+#include "base/time/time.h"
 #include "content/browser/preloading/prerender/prerender_host.h"
 #include "content/browser/renderer_host/frame_tree.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
@@ -49,6 +51,19 @@ PrerenderNoVarySearchCommitDeferringCondition::MaybeCreate(
 
   return base::WrapUnique(new PrerenderNoVarySearchCommitDeferringCondition(
       navigation_request, candidate_prerender_frame_tree_node_id.value()));
+}
+
+// static
+void PrerenderNoVarySearchCommitDeferringCondition::OnUrlUpdated(
+    base::TimeTicks defer_start_time,
+    std::string histogram_suffix,
+    base::OnceClosure resume) {
+  // Resume the prerender activation.
+  base::TimeDelta time_delta = base::TimeTicks::Now() - defer_start_time;
+  base::UmaHistogramTimes(
+      "Navigation.Prerender.NoVarySearchCommitDeferTime" + histogram_suffix,
+      time_delta);
+  std::move(resume).Run();
 }
 
 PrerenderNoVarySearchCommitDeferringCondition::
@@ -109,7 +124,12 @@ PrerenderNoVarySearchCommitDeferringCondition::WillCommitNavigation(
             .IsSameOriginWith(GetNavigationHandle().GetURL()));
   prerender_host.GetPrerenderedMainFrameHost()
       ->GetAssociatedLocalFrame()
-      ->UpdatePrerenderURL(GetNavigationHandle().GetURL(), std::move(resume));
+      ->UpdatePrerenderURL(
+          GetNavigationHandle().GetURL(),
+          base::BindOnce(
+              PrerenderNoVarySearchCommitDeferringCondition::OnUrlUpdated,
+              base::TimeTicks::Now(), prerender_host.GetHistogramSuffix(),
+              std::move(resume)));
   // Defer the prerender activation until the ongoing prerender main frame
   // changes the URL.
   return Result::kDefer;
