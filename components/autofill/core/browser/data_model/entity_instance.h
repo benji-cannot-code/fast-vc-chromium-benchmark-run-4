e014a0b60e893d4f8749cd15742f03e27e227f83
Christoph Schwering <schwering@google.com>
2025-01-14 23:35:18 -0800

    [Autofill] Remove EntityInstance::synced()
    Sync support isn't in scope for now, and when it it is, synced() can
    also be inferred from the sync metadata.
    
    Bug: 388590912
    Change-Id: Ie773a54c7a9934e3b260a47a2b68d06c26575679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175124
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406541}

--- components/autofill/core/browser/data_model/entity_instance.h
+++ components/autofill/core/browser/data_model/entity_instance.h
@@ -113,8 +113,7 @@ class EntityInstance final {
                                 AttributeInstance::CompareByType> attributes,
                  base::Uuid guid,
                  std::string nickname,
-                 base::Time date_modified,
-                 bool synced);
+                 base::Time date_modified);
 
   EntityInstance(const EntityInstance&);
   EntityInstance& operator=(const EntityInstance&);
@@ -147,9 +146,6 @@ class EntityInstance final {
   // The latest time the instance, including any of its attributes, was edited.
   base::Time date_modified() const { return date_modified_; }
 
-  // Indicates if the instance is synced.
-  bool synced() const { return synced_; }
-
  private:
   EntityType type_;
   base::flat_set<AttributeInstance, AttributeInstance::CompareByType>
@@ -157,7 +153,6 @@ class EntityInstance final {
   base::Uuid guid_;
   std::string nickname_;
   base::Time date_modified_;
-  bool synced_ = false;
 };
 
 std::ostream& operator<<(std::ostream& os, const AttributeInstance& a);
