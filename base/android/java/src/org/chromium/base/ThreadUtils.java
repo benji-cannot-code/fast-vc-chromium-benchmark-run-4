8ec05d1b3504e104505c54ac61efc3c5c3ff6f7f
Andrew Grieve <agrieve@chromium.org>
2025-01-15 08:01:06 -0800

    Android: Fix @Nullable of ThreadUtils & PostTask Callables
    Bug: 389129271
    Change-Id: I017b16dee7f287e022c0edf7ee0aed058ab49cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175632
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406730}

--- base/android/java/src/org/chromium/base/ThreadUtils.java
+++ base/android/java/src/org/chromium/base/ThreadUtils.java
@@ -192,7 +192,7 @@ public class ThreadUtils {
      * @param c The Callable to run
      * @return The result of the callable
      */
-    public static <T> T runOnUiThreadBlocking(Callable<T> c) {
+    public static <T extends @Nullable Object> T runOnUiThreadBlocking(Callable<T> c) {
         return PostTask.runSynchronously(TaskTraits.UI_DEFAULT, c);
     }
 
@@ -203,7 +203,7 @@ public class ThreadUtils {
      * @param task The FutureTask to run
      * @return The queried task (to aid inline construction)
      */
-    public static <T> FutureTask<T> runOnUiThread(FutureTask<T> task) {
+    public static <T extends @Nullable Object> FutureTask<T> runOnUiThread(FutureTask<T> task) {
         PostTask.runOrPostTask(TaskTraits.UI_DEFAULT, task);
         return task;
     }
@@ -225,7 +225,7 @@ public class ThreadUtils {
      * @param task The FutureTask to run
      * @return The queried task (to aid inline construction)
      */
-    public static <T> FutureTask<T> postOnUiThread(FutureTask<T> task) {
+    public static <T extends @Nullable Object> FutureTask<T> postOnUiThread(FutureTask<T> task) {
         PostTask.postTask(TaskTraits.UI_DEFAULT, task);
         return task;
     }
