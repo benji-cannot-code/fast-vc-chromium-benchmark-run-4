d8c889d474b80801717e5fdcf3174df64a044691
Michael Cui <mlcui@google.com>
2025-01-15 13:41:53 -0800

    scanner: add dogfood fieldtrial config
    This is a cherry-pick of https://crrev.com/c/6000203 with pixel tests
    updated for the new default behaviour.
    
    Bug: b:369272173
    Change-Id: I6a6a2100176f6329a6ed76b2a25299e2179dc93e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174276
    Commit-Queue: David Vallet <dvallet@google.com>
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406956}

--- ash/app_list/views/app_list_view_pixeltest.cc
+++ ash/app_list/views/app_list_view_pixeltest.cc
@@ -268,7 +268,7 @@ TEST_P(AppListViewPixelRTLTest, Basics) {
   UseFixedPlaceholderTextAndHideCursor(
       GetAppListTestHelper()->GetSearchBoxView());
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_basics", 13, GetAppListTestHelper()->GetBubbleView(),
+      "bubble_launcher_basics", 14, GetAppListTestHelper()->GetBubbleView(),
       GetPrimaryShelf()->navigation_widget()));
 }
 
@@ -289,7 +289,7 @@ TEST_P(AppListViewPixelRTLTest, GradientZone) {
                                 /*position=*/20);
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_gradient_zone", 13,
+      "bubble_launcher_gradient_zone", 14,
       GetAppListTestHelper()->GetBubbleView(),
       GetPrimaryShelf()->navigation_widget()));
 }
@@ -342,7 +342,7 @@ TEST_P(AppListViewLauncherSearchIphTest, Basic) {
 
   UseFixedPlaceholderTextAndHideCursor(search_box_view);
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "launcher_search_iph", /*revision_number=*/5, search_box_view));
+      "launcher_search_iph", /*revision_number=*/6, search_box_view));
 }
 
 class AppListViewTabletPixelTest
@@ -380,7 +380,7 @@ INSTANTIATE_TEST_SUITE_P(RTL,
 // Verifies the default layout for tablet mode launcher.
 TEST_P(AppListViewTabletPixelTest, Basic) {
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "tablet_launcher_basics", 14,
+      "tablet_launcher_basics", 15,
       GetAppListTestHelper()->GetAppsContainerView()));
 }
 
@@ -401,7 +401,7 @@ TEST_P(AppListViewTabletPixelTest, TopGradientZone) {
   generator->MoveTouchBy(0, -40);
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "tablet_launcher_top_gradient_zone", 12,
+      "tablet_launcher_top_gradient_zone", 13,
       GetAppListTestHelper()->GetAppsContainerView()));
 }
 
@@ -422,7 +422,7 @@ TEST_P(AppListViewTabletPixelTest, BottomGradientZone) {
   generator->MoveTouchBy(0, -90);
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "tablet_launcher_bottom_gradient_zone", 14,
+      "tablet_launcher_bottom_gradient_zone", 15,
       GetAppListTestHelper()->GetAppsContainerView()));
 }
 
