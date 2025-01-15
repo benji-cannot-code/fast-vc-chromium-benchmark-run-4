826121d5c864e081bbd80def7ced1bed01fd7b6b
Nicolas Dossou-Gbete <dgn@chromium.org>
2025-01-15 03:26:18 -0800

    rcaps: Extract RegionalCapabilitiesService to its component
    We move regional capabilities logic outside of the search engines
    component, starting with the implementation of
    SearchEngineChoiceService::GetCountryId().
    
    In follow-up CLs we will:
    - make the service a proper profile-keyed service instead of the
      current shortcut that makes it an implementation detail of
      SearchEngineChoiceService
    - remove more related APIs and implementation from the
      search_engines component (e.g. search_engines::IsEeaChoiceCountry)
    - improve the way the country ID is obtained on various platforms,
      especially Android.
    
    NO_IFTTT=b/390108284
    
    Bug: 388792357
    Change-Id: I77aab41cec9fdcebd76ba10771e9aec728d63ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6105431
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1406616}

--- chrome/browser/search_engines/template_url_service_test_util.cc
+++ chrome/browser/search_engines/template_url_service_test_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/scoped_testing_local_state.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/regional_capabilities/regional_capabilities_test_utils.h"
 #include "components/search_engines/keyword_table.h"
 #include "components/search_engines/keyword_web_data_service.h"
 #include "components/search_engines/search_engine_choice/search_engine_choice_service.h"
@@ -170,6 +171,8 @@ TemplateURLServiceTestUtil::TemplateURLServiceTestUtil(
   search_engine_choice_service_ =
       std::make_unique<search_engines::SearchEngineChoiceService>(
           *profile_->GetPrefs(), local_state_,
+          regional_capabilities::CreateServiceWithFakeClient(
+              *profile_->GetPrefs()),
           /*is_profile_eligible_for_dse_guest_propagation=*/false);
 
   ResetModel(false);
--- components/search_engines/search_engines_test_environment.cc
+++ components/search_engines/search_engines_test_environment.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "components/metrics/metrics_pref_names.h"
+#include "components/regional_capabilities/regional_capabilities_test_utils.h"
 #include "components/search_engines/search_engine_choice/search_engine_choice_service.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_service_test_util.h"
@@ -30,19 +31,12 @@ SearchEnginesTestEnvironment::SearchEnginesTestEnvironment(const Deps& deps) {
     local_state_ = owned_local_state_.get();
     local_state_->registry()->RegisterBooleanPref(
         metrics::prefs::kMetricsReportingEnabled, true);
-
-    // This is needed to prevent the code in
-    // `SearchEngineChoiceService::GetCountryIdInternal` from calling the
-    // Android specific code that requires JNI initialization.
-    pref_service_->SetInteger(country_codes::kCountryIDAtInstall,
-                              country_codes::CountryCharsToCountryID('U', 'S'));
   }
 
   search_engine_choice_service_ = std::make_unique<SearchEngineChoiceService>(
-      *pref_service_, local_state_
-      ,
-      /*is_profile_eligible_for_dse_guest_propagation=*/false
-  );
+      *pref_service_, local_state_,
+      regional_capabilities::CreateServiceWithFakeClient(*pref_service_),
+      /*is_profile_eligible_for_dse_guest_propagation=*/false);
 
   template_url_service_ = std::make_unique<TemplateURLService>(
       *pref_service_, *search_engine_choice_service_,
