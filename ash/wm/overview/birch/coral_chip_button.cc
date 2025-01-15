34b44f78f32a09643cb1e30f90f7f5ad65e7478a
Xiaodan Zhu <zxdan@chromium.org>
2025-01-15 11:26:03 -0800

    coral: UI strings
    This cl adds and adjusts the strings in coral and enables translation.
    
    Bug: b:388047163, b:385204376, b:385203508, b:385203922
    Change-Id: I35bffecb63330a967531dd176107841720b76998
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174302
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406856}

--- ash/wm/overview/birch/coral_chip_button.cc
+++ ash/wm/overview/birch/coral_chip_button.cc
@@ -43,15 +43,7 @@ CoralChipButton::~CoralChipButton() = default;
 void CoralChipButton::OnSelectionWidgetVisibilityChanged() {
   CHECK(tab_app_selection_widget_);
   UpdateRoundedCorners(tab_app_selection_widget_->IsVisible());
-
-  views::View* chevron_button = addon_view();
-
-  CHECK(chevron_button);
-  views::AsViewClass<IconButton>(chevron_button)
-      ->SetTooltipText(l10n_util::GetStringUTF16(
-          tab_app_selection_widget_->IsVisible()
-              ? IDS_ASH_BIRCH_CORAL_ADDON_SELECTOR_SHOWN
-              : IDS_ASH_BIRCH_CORAL_ADDON_SELECTOR_HIDDEN));
+  UpdateAddonButtonTooltip();
 }
 
 void CoralChipButton::ShutdownSelectionWidget() {
@@ -86,6 +78,10 @@ void CoralChipButton::UpdateTitle(
   }
 
   SetAccessibleName(item_->GetAccessibleName());
+
+  if (chevron_button_) {
+    UpdateAddonButtonTooltip();
+  }
 }
 
 void CoralChipButton::Init(BirchItem* item) {
@@ -115,9 +111,11 @@ void CoralChipButton::Init(BirchItem* item) {
 
   auto button = birch_bar_util::CreateCoralAddonButton(
       std::move(callback), vector_icons::kCaretUpIcon);
-  button->SetTooltipText(
-      l10n_util::GetStringUTF16(IDS_ASH_BIRCH_CORAL_ADDON_SELECTOR_HIDDEN));
+  button->SetTooltipText(l10n_util::GetStringFUTF16(
+      IDS_ASH_BIRCH_CORAL_ADDON_SELECTOR_HIDDEN, item_->title()));
+  chevron_button_ = button.get();
   SetAddon(std::move(button));
+  UpdateAddonButtonTooltip();
 }
 
 void CoralChipButton::ExecuteCommand(int command_id, int event_flags) {
@@ -231,6 +229,15 @@ void CoralChipButton::DestroyBorderAnimation() {
     stop_border_animation_timer_.Stop();
     RemoveChildViewT(std::exchange(rainbow_border_animated_image_, nullptr));
   }
+}
+
+void CoralChipButton::UpdateAddonButtonTooltip() {
+  CHECK(chevron_button_);
+  chevron_button_->SetTooltipText(l10n_util::GetStringFUTF16(
+      tab_app_selection_widget_ && tab_app_selection_widget_->IsVisible()
+          ? IDS_ASH_BIRCH_CORAL_ADDON_SELECTOR_SHOWN
+          : IDS_ASH_BIRCH_CORAL_ADDON_SELECTOR_HIDDEN,
+      item_->title()));
 }
 
 BEGIN_METADATA(CoralChipButton)
