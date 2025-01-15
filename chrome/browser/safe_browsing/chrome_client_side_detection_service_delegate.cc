a1f946f907e36837cde25537276f462d377177bc
Seung Jae Lim <andysjlim@chromium.org>
2025-01-15 13:22:55 -0800

    [CLAMS] Remove model availability observer at CSD service destruction
    Bug: 380928557, 388547765
    Change-Id: I0968e8a3862081c67dc1aebc264600928fd8cc72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175810
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406945}

--- chrome/browser/safe_browsing/chrome_client_side_detection_service_delegate.cc
+++ chrome/browser/safe_browsing/chrome_client_side_detection_service_delegate.cc
@@ -45,7 +45,9 @@ ChromeClientSideDetectionServiceDelegate::
     : profile_(profile) {}
 
 ChromeClientSideDetectionServiceDelegate::
-    ~ChromeClientSideDetectionServiceDelegate() = default;
+    ~ChromeClientSideDetectionServiceDelegate() {
+  StopListeningToOnDeviceModelUpdate();
+}
 
 PrefService* ChromeClientSideDetectionServiceDelegate::GetPrefs() {
   if (profile_) {
