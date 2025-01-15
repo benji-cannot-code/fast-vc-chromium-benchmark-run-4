8b15731fc8e898c0c3a6023958d12c95b7e6d395
Avi Drissman <avi@chromium.org>
2025-01-15 13:19:40 -0800

    Remove unused `item_height` from external popup menus
    This traces back to https://codereview.chromium.org/4078003/
    which used it in https://chromium.googlesource.com/chromium/src/+/caf706f870d1d9d576277110ab1df022b2756203/webkit/glue/webmenurunner_mac.mm
    to figure out a click location, but this is long-obsolete.
    
    Bug: none
    Change-Id: Ief6aea89eb11436ec7a07e70a927fbcfff803017
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6178000
    Reviewed-by: David Baron <dbaron@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406940}

--- content/browser/renderer_host/popup_menu_helper_ios.mm
+++ content/browser/renderer_host/popup_menu_helper_ios.mm
@@ -40,7 +40,6 @@ PopupMenuHelper::~PopupMenuHelper() {
 
 void PopupMenuHelper::ShowPopupMenu(
     const gfx::Rect& bounds,
-    int item_height,
     double item_font_size,
     int selected_item,
     std::vector<blink::mojom::MenuItemPtr> items,
--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -8678,7 +8678,6 @@ void RenderFrameHostImpl::BubbleLogicalScrollInParentFrame(
 void RenderFrameHostImpl::ShowPopupMenu(
     mojo::PendingRemote<blink::mojom::PopupMenuClient> popup_client,
     const gfx::Rect& bounds,
-    int32_t item_height,
     double font_size,
     int32_t selected_item,
     std::vector<blink::mojom::MenuItemPtr> menu_items,
@@ -8729,9 +8728,8 @@ void RenderFrameHostImpl::ShowPopupMenu(
   gfx::Rect transformed_bounds(transformed_point.x(), transformed_point.y(),
                                bounds.width(), bounds.height());
   view->ShowPopupMenu(this, std::move(popup_client), transformed_bounds,
-                      item_height, font_size, selected_item,
-                      std::move(menu_items), right_aligned,
-                      allow_multiple_selection);
+                      font_size, selected_item, std::move(menu_items),
+                      right_aligned, allow_multiple_selection);
 #endif
 }
 
