aa55be97e59e5f7a4cab9892ac1f3301a22117f1
Ryan Sultanem <rsult@google.com>
2025-01-15 12:46:54 -0800

    [ProfilePicker][Glic] Implement the Learn More button
    The Learn More text is currently part of a bigger string. Therefore the
    click even is added dynamically when the page is loaded by connecting
    the part of the text through the class tag to override the click event.
    
    On that event, the handler is notified to load the last used profile,
    open or use a browser with it and load the "Sign in to Chrome" page from
    the Help Center.
    
    The page cannot be loaded directly, since the Picker uses the System
    Profile which cannot be used to open a Browser.
    
    The picker is not expected to be closed after this action.
    
    Bug: b:385726690
    Change-Id: I79a311f0bb69f1db94f52368c68f066643d2ec2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168854
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406918}

--- chrome/browser/resources/signin/profile_picker/manage_profiles_browser_proxy.ts
+++ chrome/browser/resources/signin/profile_picker/manage_profiles_browser_proxy.ts
@@ -159,6 +159,12 @@ export interface ManageProfilesBrowserProxy {
    * by +/-1 depending on the change direction.
    */
   updateProfileOrder(fromIndex: number, toIndex: number): void;
+
+  /**
+   * Loads the last used profile; opens/uses a browser and open the "Sign in to
+   * Chrome" Help center page. Does not close the Picker.
+   */
+  onLearnMoreClicked(): void;
 }
 
 /** @implements {ManageProfilesBrowserProxy} */
@@ -237,6 +243,10 @@ export class ManageProfilesBrowserProxyImpl {
 
   updateProfileOrder(fromIndex: number, toIndex: number) {
     chrome.send('updateProfileOrder', [fromIndex, toIndex]);
+  }
+
+  onLearnMoreClicked(): void {
+    chrome.send('onLearnMoreClicked');
   }
 
   static getInstance(): ManageProfilesBrowserProxy {
--- chrome/test/data/webui/signin/test_manage_profiles_browser_proxy.ts
+++ chrome/test/data/webui/signin/test_manage_profiles_browser_proxy.ts
@@ -14,14 +14,26 @@ export class TestManageProfilesBrowserProxy extends TestBrowserProxy implements
 
   constructor() {
     super([
-      'initializeMainView', 'launchGuestProfile',
-      'openManageProfileSettingsSubPage', 'launchSelectedProfile',
-      'askOnStartupChanged', 'getNewProfileSuggestedThemeInfo',
-      'getProfileThemeInfo', 'removeProfile', 'getProfileStatistics',
-      'closeProfileStatistics', 'selectNewAccount', 'continueWithoutAccount',
-      'setProfileName', 'recordSignInPromoImpression', 'getAvailableIcons',
-      'getSwitchProfile', 'confirmProfileSwitch', 'cancelProfileSwitch',
+      'initializeMainView',
+      'launchGuestProfile',
+      'openManageProfileSettingsSubPage',
+      'launchSelectedProfile',
+      'askOnStartupChanged',
+      'getNewProfileSuggestedThemeInfo',
+      'getProfileThemeInfo',
+      'removeProfile',
+      'getProfileStatistics',
+      'closeProfileStatistics',
+      'selectNewAccount',
+      'continueWithoutAccount',
+      'setProfileName',
+      'recordSignInPromoImpression',
+      'getAvailableIcons',
+      'getSwitchProfile',
+      'confirmProfileSwitch',
+      'cancelProfileSwitch',
       'updateProfileOrder',
+      'onLearnMoreClicked',
     ]);
 
     this.profileThemeInfo = {
@@ -159,5 +171,9 @@ export class TestManageProfilesBrowserProxy extends TestBrowserProxy implements
 
   updateProfileOrder(fromIndex: number, toIndex: number) {
     this.methodCalled('updateProfileOrder', [fromIndex, toIndex]);
+  }
+
+  onLearnMoreClicked(): void {
+    this.methodCalled('onLearnMoreClicked');
   }
 }
