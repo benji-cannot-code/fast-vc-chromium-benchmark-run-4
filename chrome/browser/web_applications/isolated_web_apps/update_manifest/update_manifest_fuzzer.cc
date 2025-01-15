ead7b6b2d57cd1e025600ee9d7f987987cf9b04f
Patryk Chodur <pchodur@google.com>
2025-01-15 05:36:48 -0800

    Fix UpdateManifestCanSuccessfullyParseAnyString fuzz test
    The CluzzterFuzz found an issue with the
    UpdateManifestCanSuccessfullyParseAnyString test within the
    UpdateManifestFuzzTest suite. There are 2 problems here.
    
    When we parse the URL in ParseAndValidateSrc, we need CommandLine for
    the current process to be available. Because it was not initialised, the
    test crashed. This was not caught earlier, because our test case uses
    "url" key in dictionary, but should have used "src". The result was that
    the "src" attribute was never read and we just skipped "url".
    
    Bug: 386688712
    Change-Id: I667be11011630bd3fb2e14acfb59634fe4683a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172341
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Patryk Chodur <pchodur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406666}

--- chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fuzzer.cc
+++ chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fuzzer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest.h"
 
 #include "base/at_exit.h"
+#include "base/command_line.h"
 #include "base/i18n/icu_util.h"
 #include "base/json/json_reader.h"
 #include "base/logging.h"
@@ -28,6 +29,9 @@ class Environment {
     // ICU is as of 2024-11-28 reachable from here so we need to initialize ICU
     // to avoid hitting NOTREACHED()s. See https://crbug.com/381129857.
     CHECK(base::i18n::InitializeICU());
+    // Parsing URLs requires command line to be available.
+    // See https://crbug.com/386688712
+    base::CommandLine::Init(/*argc=*/0, /*argv=*/nullptr);
   }
   // Required by ICU integration according to several other fuzzer environments.
   // TODO(pbos): Consider breaking out this fuzzer environment to something
@@ -50,15 +54,15 @@ FUZZ_TEST(UpdateManifestFuzzTest, UpdateManifestCanSuccessfullyParseAnyString)
                   "versions": [
                     {
                       "version": "1.0.0",
-                      "url": "https://example.com/bundle.swbn"
+                      "src": "https://example.com/bundle.swbn"
                     },
                     {
                       "version": "1.0.3",
-                      "url": "bundle.swbn"
+                      "src": "bundle.swbn"
                     },
                     {
                       "version": "1.0.0",
-                      "url": "https://example.com/bundle2.swbn"
+                      "src": "https://example.com/bundle2.swbn"
                     }
                   ]
                 })"),
@@ -66,7 +70,7 @@ FUZZ_TEST(UpdateManifestFuzzTest, UpdateManifestCanSuccessfullyParseAnyString)
                   "versions": [
                     {
                       "version": "1.0.0",
-                      "url": "https://example.com/bundle.swbn",
+                      "src": "https://example.com/bundle.swbn",
                       "blah": 123
                     }
                   ]
@@ -75,7 +79,7 @@ FUZZ_TEST(UpdateManifestFuzzTest, UpdateManifestCanSuccessfullyParseAnyString)
                   "versions": [
                     {
                       "version": "1.0.0",
-                      "url": "https://example.com/bundle.swbn",
+                      "src": "https://example.com/bundle.swbn",
                       "channels": ["test", "stable", "test"]
                     }
                   ]
@@ -93,7 +97,7 @@ FUZZ_TEST(UpdateManifestFuzzTest, UpdateManifestCanSuccessfullyParseAnyString)
                   "versions": [
                     {
                       "version": "1.0.0",
-                      "url": "https://example.com/bundle.swbn",
+                      "src": "https://example.com/bundle.swbn",
                       "channels": ["test", "stable", "test"]
                     }
                   ]
