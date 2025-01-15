48b8a464b9adbd766bec76d3779c12afc7b34e96
Yann Dago <ydago@chromium.org>
2025-01-15 10:19:33 -0800

    Remove disallow-managed-profile-signout flag
    Bug: 40269992
    Change-Id: I50855598829f647aa99756add06afc80c97c184e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018919
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406823}

--- chrome/browser/resources/settings/people_page/sync_account_control.ts
+++ chrome/browser/resources/settings/people_page/sync_account_control.ts
@@ -547,8 +547,7 @@ export class SettingsSyncAccountControlElement extends
       return false;
     }
 
-    if (loadTimeData.getBoolean('turnOffSyncAllowedForManagedProfiles') &&
-        this.syncStatus.domain) {
+    if (this.syncStatus.domain) {
       return false;
     }
 
