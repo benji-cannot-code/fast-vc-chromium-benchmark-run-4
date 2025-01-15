6bfe236268f0f8cec35fd3791fc98e65d160700d
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-01-15 03:21:34 -0800

    Make WillCreateURLLoaderFactory mocks strict in prefetch tests
    To avoid gmock warnings,
    by ignoring unrelated `WillCreateURLLoaderFactory` calls with
    `kDocumentSubResource` or `kNavigation`.
    
    Bug: 359802755
    Change-Id: I006dea69c3d57c1bcb4d7033ed5982981808b41d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176579
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406614}

--- content/browser/preloading/prefetch/prefetch_network_context_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_network_context_unittest.cc
@@ -36,8 +36,8 @@ class PrefetchNetworkContextTest : public RenderViewHostTestHarness {
   void SetUp() override {
     RenderViewHostTestHarness::SetUp();
 
-    test_content_browser_client_ =
-        std::make_unique<ScopedMockContentBrowserClient>();
+    test_content_browser_client_ = std::make_unique<
+        ::testing::StrictMock<ScopedMockContentBrowserClient>>();
 
     prefetch_service_ = std::make_unique<PrefetchService>(browser_context());
   }
--- content/browser/preloading/prefetch/prefetch_test_util_internal.cc
+++ content/browser/preloading/prefetch/prefetch_test_util_internal.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+using ::testing::_;
+
 namespace {
 
 // Make some broadly reasonable redirect info.
@@ -442,6 +444,19 @@ void PrefetchTestURLLoaderClient::OnDataComplete() {
 
 ScopedMockContentBrowserClient::ScopedMockContentBrowserClient() {
   old_browser_client_ = SetBrowserClientForTesting(this);
+
+  // Ignore `WillCreateURLLoaderFactory(kDocumentSubResource)` calls (triggered
+  // by e.g. `RenderFrameHostImpl::CommitNavigation()`) to suppress gmock
+  // warning/failures. This is safe to ignore, because prefetch-related tests
+  // are only for navigational prefetch and don't care about unrelated
+  // subresource URLLoaderFactories.
+  EXPECT_CALL(
+      *this,
+      WillCreateURLLoaderFactory(
+          _, _, _,
+          ContentBrowserClient::URLLoaderFactoryType::kDocumentSubResource, _,
+          _, _, _, _, _, _, _, _, _))
+      .Times(::testing::AnyNumber());
 }
 
 ScopedMockContentBrowserClient::~ScopedMockContentBrowserClient() {
