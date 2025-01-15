4273c9a041bfc36a012ecb4c412967409cc6bfcc
Alexander Surkov <asurkov@igalia.com>
2025-01-15 06:56:53 -0800

    [AX Mac] implement accessibilityPerformConfirm placeholder
    Provide empty implementation for accessibilityPerformConfirm. Not yet
    implemented in WebKit.
    
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:mac-fieldtrial-tester
    
    Bug: 363275809
    Change-Id: I96d6c539dee5cc99d4bee0f66ffbc82daaaf2316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173637
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1406699}

--- ui/accessibility/platform/ax_platform_node_cocoa.mm
+++ ui/accessibility/platform/ax_platform_node_cocoa.mm
@@ -327,6 +327,7 @@ const ui::CocoaActionList& GetCocoaActionListForTesting() {
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     dict = @{
+      @"accessibilityPerformConfirm" : NSAccessibilityConfirmAction,
       @"accessibilityPerformPress" : NSAccessibilityPressAction,
       @"accessibilityPerformShowMenu" : NSAccessibilityShowMenuAction,
       @"accessibilityPerformDecrement" : NSAccessibilityDecrementAction,
@@ -1454,6 +1455,11 @@ const ui::CocoaActionList& GetCocoaActionListForTesting() {
     return NO;
   }
   return [self performAction:ax::mojom::Action::kIncrement];
+}
+
+- (BOOL)accessibilityPerformConfirm {
+  // Placeholder for the future. Needs to implement Return press key action.
+  return NO;
 }
 
 - (NSMutableArray*)internalAccessibilityAttributeNames {
