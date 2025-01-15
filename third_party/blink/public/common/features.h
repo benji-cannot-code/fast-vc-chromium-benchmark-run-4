a3f43fd3e854e3cbb9e4817a4c3ce8f091721f2a
Minoru Chikamune <chikamune@chromium.org>
2025-01-14 18:33:35 -0800

    Clean-up: Remove kServiceWorkerStorageSuppressPostTask flag [1]
    kServiceWorkerStorageSuppressPostTask feature has been enabled by
    default. This CL removes the feature flag, and clean-up the surrounding
    logic.
    
    Bug: 40242807
    Change-Id: Ie4c777df2fdf04b5f64125bb9d9b831eb1c04a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166903
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406481}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -1646,8 +1646,6 @@ BLINK_COMMON_EXPORT extern const base::FeatureParam<bool>
 BLINK_COMMON_EXPORT extern const base::FeatureParam<bool>
     kSpeculativeServiceWorkerWarmUpOnIdleTimeout;
 
-BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kServiceWorkerStorageSuppressPostTask);
-
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kServiceWorkerSyntheticResponse);
 
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE_PARAM(
