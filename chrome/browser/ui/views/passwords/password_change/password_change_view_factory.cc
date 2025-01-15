a5b11f81434302d76dc1088f41a50606bac4d524
Viktor Semeniuk <vsemeniuk@google.com>
2025-01-15 03:45:41 -0800

    Add dialog which is displayed when change password form can't be found
    This CL adds a dedicated dialog for kChangePasswordFormNotFound state
    with corresponding view and controller.
    
    Mock: https://screenshot.googleplex.com/ABPvfEPBhPNPz8Z
    Impl: https://screenshot.googleplex.com/AZW3WhYMjfL9urQ
    
    Bug: 381055148
    Change-Id: I247c6ef4fbba085fcf42ca3b030d041ce39d60b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6170446
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406621}

--- chrome/browser/ui/views/passwords/password_change/password_change_view_factory.cc
+++ chrome/browser/ui/views/passwords/password_change/password_change_view_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "chrome/browser/password_manager/password_change_delegate.h"
 #include "chrome/browser/ui/views/passwords/password_change/failed_password_change_view.h"
+#include "chrome/browser/ui/views/passwords/password_change/no_password_change_form_view.h"
 #include "chrome/browser/ui/views/passwords/password_change/password_change_info_bubble_view.h"
 #include "chrome/browser/ui/views/passwords/password_change/privacy_notice_view.h"
 #include "chrome/browser/ui/views/passwords/password_change/successful_password_change_view.h"
@@ -30,8 +31,7 @@ PasswordBubbleViewBase* CreatePasswordChangeBubbleView(
     case PasswordChangeDelegate::State::kPasswordChangeFailed:
       return new FailedPasswordChangeView(web_contents, anchor_view);
     case PasswordChangeDelegate::State::kChangePasswordFormNotFound:
-      NOTIMPLEMENTED();
-      break;
+      return new NoPasswordChangeFormView(web_contents, anchor_view);
   }
   NOTREACHED();
 }
