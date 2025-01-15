0aaaff86cde664909f86ed04bb0dc476f06cd012
Zhibo Wang <zhibo1.wang@intel.com>
2025-01-15 00:13:34 -0800

    Create unittests for D3D12VideoEncodeDelegate
    This CL tests the basic features of D3D12VideoEncodeDelegate,
    introduces common test methods into a base class in the header file for
    future codec delegates, fixes several bugs and makes necessary code
    adjustments to ensure the tests pass.
    
    Bug: 40275246
    Change-Id: I1aca31366db87fe024494d9759802fef609c495f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6151962
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Zhibo Wang <zhibo1.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1406559}

--- media/gpu/windows/d3d12_video_encoder_wrapper.h
+++ media/gpu/windows/d3d12_video_encoder_wrapper.h
@@ -12,32 +12,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "media/base/encoder_status.h"
+#include "media/gpu/media_gpu_export.h"
 #include "media/gpu/windows/d3d12_fence.h"
 #include "media/gpu/windows/d3d12_helpers.h"
 
 namespace media {
 
-class D3D12VideoEncoderWrapper {
+class MEDIA_GPU_EXPORT D3D12VideoEncoderWrapper {
  public:
   D3D12VideoEncoderWrapper(
       Microsoft::WRL::ComPtr<ID3D12VideoEncoder> video_encoder,
       Microsoft::WRL::ComPtr<ID3D12VideoEncoderHeap> video_encoder_heap);
-  ~D3D12VideoEncoderWrapper();
+  virtual ~D3D12VideoEncoderWrapper();
 
-  bool Initialize();
+  virtual bool Initialize();
 
   // Do the encode and wait for the completion of the encoding.
-  EncoderStatus Encode(
+  virtual EncoderStatus Encode(
       const D3D12_VIDEO_ENCODER_ENCODEFRAME_INPUT_ARGUMENTS& input_arguments,
       const D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE& reconstructed_picture);
 
   // Get the number of bytes written to the bitstream buffer or log the encoding
   // error.
-  EncoderStatus::Or<uint64_t> GetEncodedBitstreamWrittenBytesCount() const;
+  virtual EncoderStatus::Or<uint64_t> GetEncodedBitstreamWrittenBytesCount()
+      const;
 
   // Readback bitstream from GPU memory to |data|. The |size| must be no greater
   // than the value returned by |GetEncodedBitstreamWrittenBytesCount()|.
-  EncoderStatus ReadbackBitstream(base::span<uint8_t> data) const;
+  virtual EncoderStatus ReadbackBitstream(base::span<uint8_t> data) const;
 
  private:
   Microsoft::WRL::ComPtr<ID3D12VideoEncoder> video_encoder_;
