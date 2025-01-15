e959893b9ef2972eb4f9422b69c7ae4032f4093e
Chris Harrelson <chrishtr@google.com>
2025-01-15 15:08:53 -0800

    Remove some unused variables.
    Change-Id: Idd2ee523d0dd90337fc526652a162c0e87577e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175801
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406983}

--- android_webview/javatests/src/org/chromium/android_webview/test/AndroidScrollIntegrationTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AndroidScrollIntegrationTest.java
@@ -489,8 +489,6 @@ public class AndroidScrollIntegrationTest extends AwParameterizedTest {
                 GraphicsTestUtils.dipScaleForContext(testContainerView.getContext());
         final int targetScrollXCss = 132;
         final int targetScrollYCss = 243;
-        final int targetScrollXPix = (int) Math.floor(targetScrollXCss * deviceDIPScale);
-        final int targetScrollYPix = (int) Math.floor(targetScrollYCss * deviceDIPScale);
 
         final int maxScrollXCss = 101;
         final int maxScrollYCss = 201;
@@ -870,8 +868,6 @@ public class AndroidScrollIntegrationTest extends AwParameterizedTest {
         final ScrollTestContainerView testContainerView =
                 (ScrollTestContainerView)
                         mActivityTestRule.createAwTestContainerViewOnMainSync(contentsClient);
-        final OverScrollByCallbackHelper overScrollByCallbackHelper =
-                testContainerView.getOverScrollByCallbackHelper();
         final AwContents awContents = testContainerView.getAwContents();
         AwActivityTestRule.enableJavaScriptOnUiThread(awContents);
 
@@ -1009,7 +1005,6 @@ public class AndroidScrollIntegrationTest extends AwParameterizedTest {
         // Drag scroll.
         final CallbackHelper onScrollToCallbackHelper =
                 testContainerView.getOnScrollToCallbackHelper();
-        final int scrollToCallCount = onScrollToCallbackHelper.getCallCount();
         final int dragSteps = 10;
         final int dragStepSize = 24;
         final int targetScrollYPix = dragStepSize * dragSteps;
