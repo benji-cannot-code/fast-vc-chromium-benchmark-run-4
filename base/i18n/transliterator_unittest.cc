49abdd52ed87e3803db7825326504dee55a0ba38
Frank Tang <ftang@chromium.org>
2025-01-15 00:15:56 -0800

    Move to use base::i18n::Transliterator
    Avoid direct usage of icu Transliterator
    and use base::i18n::Transliterator so later we can switch to rust
    implementation in ICU4X easily.
    Remove unused include in idn_spoof_checker.cc
    
    Bug: 388821484
    Change-Id: Iecc3b987212b75c73c612bfaac17ff66d601131b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173955
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Auto-Submit: Frank Tang <ftang@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1406562}

--- base/i18n/transliterator_unittest.cc
+++ base/i18n/transliterator_unittest.cc
@@ -14,7 +14,7 @@ namespace base::i18n {
 TEST(TransliteratorTest, LowerCorrect) {
   std::unique_ptr<base::i18n::Transliterator> transliterator(
       base::i18n::CreateTransliterator("Lower"));
-  ASSERT_TRUE(transliterator != nullptr);
+  ASSERT_TRUE(transliterator);
   std::u16string text(u"ÎÑŢÉRÑÅŢÎÖÑÅĻÎŽÅŢÎÖÑ");
   std::u16string result = transliterator->Transliterate(text);
   EXPECT_EQ(result, u"îñţérñåţîöñåļîžåţîöñ");
@@ -23,7 +23,7 @@ TEST(TransliteratorTest, LowerCorrect) {
 TEST(TransliteratorTest, LatinASCIICorrect) {
   std::unique_ptr<base::i18n::Transliterator> transliterator(
       base::i18n::CreateTransliterator("Latin-ASCII"));
-  ASSERT_TRUE(transliterator != nullptr);
+  ASSERT_TRUE(transliterator);
   std::u16string text(u"ÎÑŢÉRÑÅŢÎÖÑÅĻÎŽÅŢÎÖÑ");
   std::u16string result = transliterator->Transliterate(text);
   EXPECT_EQ(result, u"INTERNATIONALIZATION");
@@ -32,7 +32,7 @@ TEST(TransliteratorTest, LatinASCIICorrect) {
 TEST(TransliteratorTest, LowerLatinASCIICorrect) {
   std::unique_ptr<base::i18n::Transliterator> transliterator(
       base::i18n::CreateTransliterator("Lower;Latin-ASCII"));
-  ASSERT_TRUE(transliterator != nullptr);
+  ASSERT_TRUE(transliterator);
   std::u16string text(u"ÎÑŢÉRÑÅŢÎÖÑÅĻÎŽÅŢÎÖÑ");
   std::u16string result = transliterator->Transliterate(text);
   EXPECT_EQ(result, u"internationalization");
--- components/url_formatter/spoof_checks/idn_spoof_checker.cc
+++ components/url_formatter/spoof_checks/idn_spoof_checker.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/icu/source/common/unicode/schriter.h"
 #include "third_party/icu/source/common/unicode/unistr.h"
 #include "third_party/icu/source/i18n/unicode/regex.h"
-#include "third_party/icu/source/i18n/unicode/translit.h"
 #include "third_party/icu/source/i18n/unicode/uspoof.h"
 #include "url/url_features.h"
 
