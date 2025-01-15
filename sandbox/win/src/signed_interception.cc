a65a225f5680bda0024abe2c9d4e9b8785e77859
Alex Gough <ajgo@chromium.org>
2025-01-14 18:36:01 -0800

    Revert "Reduce scope of unsafe-buffers in signed_interception.cc"
    This reverts commit 5a7afce638ddbac50d76bd5762c70ac3ea8c3512.
    
    Reason for revert: pinpoint fails to start a browser for some
    benchmarking runs - it is not yet clear what is not working but applying
    the revert allows pinpoint to build and run benchmarks again. See
    https://pinpoint-dot-chromeperf.appspot.com/job/1720deaf210000.
    
    > Reduce scope of unsafe-buffers in signed_interception.cc
    > Moves a fake-memset from a header into a call to ntdll!memset,
    > which must be added so that it is available. ntdll!memset is
    > marked as an unsafe-buffers operation. We cannot directly use a
    > span copy as that might use memset from the CRT which may not be
    > initialized when this dll-loading hook might be called.
    > Bug: 351564777
    > Change-Id: I6b957e59d09d4585875ef5176fe0ede167146435
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6151234
    > Commit-Queue: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1404411}
    
    Bug: 351564777
    Change-Id: I10d555df0b8120a5a8a52bb78875ec1d924adf79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175544
    Reviewed-by: Arthur Wang <wuwang@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406485}

--- sandbox/win/src/signed_interception.cc
+++ sandbox/win/src/signed_interception.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ntstatus.h>
 #include <stdint.h>
 
-#include "base/compiler_specific.h"
 #include "sandbox/win/src/crosscall_client.h"
 #include "sandbox/win/src/ipc_tags.h"
 #include "sandbox/win/src/policy_params.h"
@@ -82,9 +81,7 @@ TargetNtCreateSection(NtCreateSectionFunction orig_CreateSection,
 
     // Avoid memset inserted by -ftrivial-auto-var-init=pattern on debug builds.
     STACK_UNINITIALIZED CrossCallReturn answer;
-    // SAFETY cannot use {} constructor as this code runs too early and might
-    // introduce a call to the CRT's memset. Instead use ntdll memset.
-    UNSAFE_BUFFERS(GetNtExports()->memset(&answer, 0, sizeof(answer)));
+    Memset(&answer, 0, sizeof(answer));
 
     answer.nt_status = STATUS_INVALID_IMAGE_HASH;
     SharedMemIPCClient ipc(memory);
