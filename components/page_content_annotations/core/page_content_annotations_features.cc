a2577f9117fd551b1f354040f6683449fa5d6d98
Khushal Sagar <khushalsagar@chromium.org>
2025-01-15 08:50:41 -0800

    page_content_annotations: Add finch config to assess page content proto
    Add a finch config to trigger collection of AnnotatedPageContent for
    each page load. A couple of follow up changes pending before starting
    the study:
    
    1. Cover same-document navigations.
    2. Add a metric to measure the size of the proto and compare it against
       innerText.
    
    Bug: 389096819
    Change-Id: Ibb20662498f237a96915fe9fbec8fda9676e1874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169167
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1406759}

--- components/page_content_annotations/core/page_content_annotations_features.cc
+++ components/page_content_annotations/core/page_content_annotations_features.cc
@@ -104,6 +104,15 @@ bool IsSupportedCountryForFeature(const std::string& country_code,
       });
 }
 
+const base::FeatureParam<base::TimeDelta> kAnnotatedPageContentCaptureDelay{
+    &kAnnotatedPageContentExtraction, "capture_delay", base::Seconds(1)};
+
+const base::FeatureParam<bool> kAnnotatedPageContentIncludeGeometry{
+    &kAnnotatedPageContentExtraction, "include_geometry", false};
+
+const base::FeatureParam<bool> kAnnotatedPageContentOnCriticalPath{
+    &kAnnotatedPageContentExtraction, "on_critical_path", false};
+
 }  // namespace
 
 // Enables page content to be annotated.
@@ -136,6 +145,10 @@ BASE_FEATURE(kPageContentAnnotationsPersistSalientImageMetadata,
 BASE_FEATURE(kExtractRelatedSearchesFromPrefetchedZPSResponse,
              "ExtractRelatedSearchesFromPrefetchedZPSResponse",
              enabled_by_default_desktop_only);
+
+BASE_FEATURE(kAnnotatedPageContentExtraction,
+             "AnnotatedPageContentExtraction",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 base::TimeDelta PCAServiceWaitForTitleDelayDuration() {
   return base::Milliseconds(GetFieldTrialParamByFeatureAsInt(
@@ -254,6 +267,18 @@ size_t MaxRelatedSearchesCacheSize() {
   return GetFieldTrialParamByFeatureAsInt(
       kExtractRelatedSearchesFromPrefetchedZPSResponse,
       "max_related_searches_cache_size", 10);
+}
+
+bool IsAnnotatedPageContentOnCriticalPath() {
+  return kAnnotatedPageContentOnCriticalPath.Get();
+}
+
+base::TimeDelta GetAnnotatedPageContentCaptureDelay() {
+  return kAnnotatedPageContentCaptureDelay.Get();
+}
+
+bool ShouldAnnotatedPageContentIncludeGeometry() {
+  return kAnnotatedPageContentIncludeGeometry.Get();
 }
 
 }  // namespace page_content_annotations::features
