9e944afe297c25f5fddb98dd55f3f4abd74cc319
Yue Zhang <yuezhanggg@chromium.org>
2025-01-15 15:14:53 -0800

    [CodeHealth] Remove UTM for discount in cart/quest
    Bug: 383565980
    Change-Id: I9df8e048b357aab4ace9d5aa97f9462c9e6f24b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173651
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406993}

--- components/commerce/core/commerce_utils.cc
+++ components/commerce/core/commerce_utils.cc
@@ -22,27 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace commerce {
-bool UrlContainsDiscountUtmTag(const GURL& url) {
-  std::string utm_source;
-  std::string utm_medium;
-  std::string utm_campaign;
-  if (!net::GetValueForKeyInQuery(url, commerce::kUTMSourceLabel,
-                                  &utm_source)) {
-    return false;
-  }
-  if (!net::GetValueForKeyInQuery(url, commerce::kUTMMediumLabel,
-                                  &utm_medium)) {
-    return false;
-  }
-  if (!net::GetValueForKeyInQuery(url, commerce::kUTMCampaignLabel,
-                                  &utm_campaign)) {
-    return false;
-  }
-  return utm_source == commerce::kUTMSourceValue &&
-         utm_medium == commerce::kUTMMediumValue &&
-         utm_campaign == commerce::kUTMCampaignValueForDiscounts;
-}
-
 GURL GetProductSpecsTabUrl(const std::vector<GURL>& urls) {
   auto urls_list = base::Value::List();
 
