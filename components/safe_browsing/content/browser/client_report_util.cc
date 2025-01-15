2fe0269280689f21b3dd0c7b09b5a0bec5bfc956
Nwokedi Idika <nwokedi@chromium.org>
2025-01-15 08:01:41 -0800

    Extend IsReportable to CrCA's chrome:// Test URLs
    Bug: 342093068
    Change-Id: Ife0f857d8475ae60287947d027914e7da9aaa80a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174962
    Commit-Queue: Nwokedi Idika <nwokedi@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406731}

--- components/safe_browsing/content/browser/client_report_util.cc
+++ components/safe_browsing/content/browser/client_report_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/content/browser/client_report_util.h"
 
 #include "components/safe_browsing/content/browser/content_unsafe_resource_util.h"
+#include "components/safe_browsing/core/common/web_ui_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_entry.h"
 
@@ -167,7 +168,9 @@ GetSecurityInterstitialInteractionFromCommand(
 
 bool IsReportableUrl(const GURL& url) {
   // TODO(panayiotis): also skip internal urls.
-  return url.SchemeIs("http") || url.SchemeIs("https");
+  // chrome://safe-browsing/match?type=${THREAT_TYPE} is used for local tests.
+  return url.SchemeIs("http") || url.SchemeIs("https") ||
+         safe_browsing::IsSafeBrowsingWebUIUrl(url);
 }
 
 GURL GetPageUrl(const security_interstitials::UnsafeResource& resource) {
