4839c118c432885e48a85a324c8e90a364afcec2
Kamil Jarosz <kjarosz@google.com>
2025-01-15 06:33:25 -0800

    metrics: Add a histogram for navigation to FCP in Incognito
    This patch adds a histogram
    PageLoad.PaintTiming.NavigationToFirstContentfulPaint.Incognito,
    which is an incognito-only version of the metric
    PageLoad.PaintTiming.NavigationToFirstContentfulPaint.
    
    Bug: 378052451
    Change-Id: I06449ed7a5156752c10a816bf27d09d51c6c4923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6122584
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406686}

--- components/page_load_metrics/browser/page_load_metrics_embedder_base.cc
+++ components/page_load_metrics/browser/page_load_metrics_embedder_base.cc
@@ -47,7 +47,8 @@ void PageLoadMetricsEmbedderBase::RegisterCommonObservers(
 
   tracker->AddObserver(
       std::make_unique<BackForwardCachePageLoadMetricsObserver>());
-  tracker->AddObserver(std::make_unique<UmaPageLoadMetricsObserver>());
+  tracker->AddObserver(std::make_unique<UmaPageLoadMetricsObserver>(
+      IsIncognito(tracker->GetWebContents())));
   tracker->AddObserver(std::make_unique<UseCounterPageLoadMetricsObserver>());
   tracker->AddObserver(std::make_unique<EarlyHintsPageLoadMetricsObserver>());
   tracker->AddObserver(std::make_unique<FencedFramesPageLoadMetricsObserver>());
