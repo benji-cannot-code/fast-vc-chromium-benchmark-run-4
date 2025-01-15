71c230e8743fc3f3553af72ee6423a19edc37c22
Florian Leimgruber <fleimgruber@google.com>
2025-01-15 03:50:39 -0800

    Remove unused FormFieldParser::ParseField() overload
    Due to a recent refactoring (http://shortn/_O6FpRsMAs8), the function is
    unused.
    
    Bug: 40100455
    Change-Id: I9abf6b7fbfb1361522199197c19406b753315abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172567
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406623}

--- components/autofill/core/browser/form_parsing/form_field_parser.cc
+++ components/autofill/core/browser/form_parsing/form_field_parser.cc
@@ -512,13 +512,14 @@ FormFieldParser::FieldMatchesMatchPatternRef(
 bool FormFieldParser::ParseField(
     ParsingContext& context,
     AutofillScanner* scanner,
-    base::span<const MatchPatternRef> patterns,
-    std::optional<FieldAndMatchInfo>* match,
     const char* regex_name,
+    std::optional<FieldAndMatchInfo>* match,
     MatchParams (*projection)(const MatchParams&)) {
   if (scanner->IsEnd()) {
     return false;
   }
+  base::span<const MatchPatternRef> patterns =
+      GetMatchPatterns(regex_name, context.page_language, context.pattern_file);
   AutofillField* field = scanner->Cursor();
   if (std::optional<MatchInfo> match_info = FieldMatchesMatchPatternRef(
           context, patterns, *field, regex_name, {projection})) {
@@ -529,18 +530,6 @@ bool FormFieldParser::ParseField(
     return true;
   }
   return false;
-}
-
-// static
-bool FormFieldParser::ParseField(
-    ParsingContext& context,
-    AutofillScanner* scanner,
-    const char* regex_name,
-    std::optional<FieldAndMatchInfo>* match,
-    MatchParams (*projection)(const MatchParams&)) {
-  base::span<const MatchPatternRef> patterns =
-      GetMatchPatterns(regex_name, context.page_language, context.pattern_file);
-  return ParseField(context, scanner, patterns, match, regex_name, projection);
 }
 
 // static
