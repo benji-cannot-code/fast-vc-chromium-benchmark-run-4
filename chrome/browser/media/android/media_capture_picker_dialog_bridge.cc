3ab1354e520ef30e9f5357558e30a4c132a234b7
Eliot Courtney <edcourtney@google.com>
2025-01-14 23:57:20 -0800

    Reland "Add audio share option to Android tab sharing"
    This is a reland of commit d8c3a8e62997eef7bcef41cd07633562d9e5fd07
    
    Original change's description:
    > Add audio share option to Android tab sharing
    >
    > This CL adds a switch for enabling or disabling audio sharing during tab
    > sharing to the tab selection UI.
    >
    > Bug: b/352186941
    > Test: CQ + manual testing
    > Change-Id: Id4495c93d3b6af07283d2f091cda1756b3acc259
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108977
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    > Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1405869}
    
    Bug: b/352186941
    Change-Id: I741e37cf9bb3523fef0a6d4f78382781f05ae7b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171633
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406547}

--- chrome/browser/media/android/media_capture_picker_dialog_bridge.cc
+++ chrome/browser/media/android/media_capture_picker_dialog_bridge.cc
@@ -34,6 +34,7 @@ MediaCapturePickerDialogBridge::~MediaCapturePickerDialogBridge() {
 void MediaCapturePickerDialogBridge::Show(
     content::WebContents* web_contents,
     const std::u16string& app_name,
+    bool request_audio,
     MediaCapturePickerDialogCallback callback) {
   CHECK(web_contents);
   CHECK(callback_.is_null());
@@ -48,12 +49,14 @@ void MediaCapturePickerDialogBridge::Show(
   }
 
   Java_MediaCapturePickerDialogBridge_showDialog(
-      env, java_object_, window_android->GetJavaObject(), app_name);
+      env, java_object_, window_android->GetJavaObject(), app_name,
+      request_audio);
 }
 
 void MediaCapturePickerDialogBridge::OnResult(
     JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& java_web_contents) {
+    const base::android::JavaParamRef<jobject>& java_web_contents,
+    bool audio_share) {
   content::DesktopMediaID desktop_media_id;
 
   // If no web contents was selected to capture, return a null DesktopMediaID.
@@ -69,6 +72,7 @@ void MediaCapturePickerDialogBridge::OnResult(
                 ->GetProcess()
                 ->GetDeprecatedID(),
             web_contents->GetPrimaryMainFrame()->GetRoutingID()));
+    desktop_media_id.audio_share = audio_share;
   }
   std::move(callback_).Run(desktop_media_id);
 }
