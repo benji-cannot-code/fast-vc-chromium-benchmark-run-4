99d9f52a5b1d12c77864a0fd086522e99aeba583
Darwin Yang <darwinyang@google.com>
2025-01-15 11:23:17 -0800

    [autofill] Integrate GetBnplPaymentInstrumentForFetchingVcnRequest
    Integrate the GetBnplPaymentInstrumentForFetchingVcnRequest with
    PaymentsNetworkInterface::GetBnplPaymentInstrumentForFetchingVcn().
    
    Renamed other BNPL test classes to
    PaymentsNetworkInterfaceTestParameterizePaymentsRpcResults
    as the only difference between the original test class and them
    is parametrizing the payment RPC results. This allows the test class
    to be more reusable for instead of specific BNPL methods.
    
    This change also includes a lot of formatting fixes from running
    `git cl format` on files with existing formatting issues.
    
    Bug: 385131002,b:378518641,b:356443046
    Change-Id: I5d51e3e17d114f4ce7439c08518ca2c069e91bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168288
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406855}

--- components/autofill/core/browser/payments/payments_network_interface.cc
+++ components/autofill/core/browser/payments/payments_network_interface.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/account_info_getter.h"
 #include "components/autofill/core/browser/payments/client_behavior_constants.h"
 #include "components/autofill/core/browser/payments/payments_requests/create_bnpl_payment_instrument_request.h"
+#include "components/autofill/core/browser/payments/payments_requests/get_bnpl_payment_instrument_for_fetching_vcn_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/get_card_upload_details_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/get_details_for_create_bnpl_payment_instrument_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/get_details_for_enrollment_request.h"
@@ -63,8 +64,9 @@ PaymentsNetworkInterface::PaymentsNetworkInterface(
 PaymentsNetworkInterface::~PaymentsNetworkInterface() = default;
 
 void PaymentsNetworkInterface::Prepare() {
-  if (access_token_.empty())
+  if (access_token_.empty()) {
     StartTokenFetch(false);
+  }
 }
 
 void PaymentsNetworkInterface::GetUnmaskDetails(
@@ -231,7 +233,10 @@ void PaymentsNetworkInterface::GetBnplPaymentInstrumentForFetchingVcn(
     GetBnplPaymentInstrumentForFetchingVcnRequestDetails request_details,
     base::OnceCallback<void(PaymentsAutofillClient::PaymentsRpcResult,
                             const BnplFetchVcnResponseDetails&)> callback) {
-  // TODO(crbug.com/378518641): Implement GetBnplPaymentInstrument() in payments
-  // network interface.
+  IssueRequest(std::make_unique<GetBnplPaymentInstrumentForFetchingVcnRequest>(
+      request_details,
+      /*full_sync_enabled=*/
+      account_info_getter_->IsSyncFeatureEnabledForPaymentsServerMetrics(),
+      std::move(callback)));
 }
 }  // namespace autofill::payments
