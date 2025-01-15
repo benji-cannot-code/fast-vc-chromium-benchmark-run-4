35c080c0b91b7fe5570bb9fd2508f1137e7c4523
Takuto Ikuta <tikuta@chromium.org>
2025-01-15 00:00:11 -0800

    components: fix missing includes for protobuf roll
    Bug: 40263312
    Change-Id: I49c85989887cd0eb0eb1cb3fd0326d2bbb215ec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172020
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406549}

--- components/commerce/core/product_specifications/product_specifications_sync_bridge.h
+++ components/commerce/core/product_specifications/product_specifications_sync_bridge.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_COMMERCE_CORE_PRODUCT_SPECIFICATIONS_PRODUCT_SPECIFICATIONS_SYNC_BRIDGE_H_
 #define COMPONENTS_COMMERCE_CORE_PRODUCT_SPECIFICATIONS_PRODUCT_SPECIFICATIONS_SYNC_BRIDGE_H_
 
+#include <set>
+
 #include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
--- components/omnibox/browser/unscoped_extension_provider_delegate.h
+++ components/omnibox/browser/unscoped_extension_provider_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OMNIBOX_BROWSER_UNSCOPED_EXTENSION_PROVIDER_DELEGATE_H_
 #define COMPONENTS_OMNIBOX_BROWSER_UNSCOPED_EXTENSION_PROVIDER_DELEGATE_H_
 
+#include <set>
 #include <string>
 
 #include "components/omnibox/browser/autocomplete_input.h"
