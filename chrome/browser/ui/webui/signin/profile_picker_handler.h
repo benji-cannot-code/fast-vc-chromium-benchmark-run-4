6617536abc06e1aeb891f4704877b6e7a0e32428
Ryan Sultanem <rsult@google.com>
2025-01-16 09:59:17 -0800

    [ProfilePicker][Glic] Implement profile entry filtering in the handler
    Filtering is based on the information present in the
    ProfilesAttributeEntry that was filled when the user signed in or when
    the refresh tokens/account capabilities are refreshed.
    
    This information controls whether the profile is allowed to use Glic and
    therefore be one of the choices in the Picker.
    
    Bug: b:388211126
    Change-Id: I6333be4b619fa40952f417af4636bacb40a015c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175120
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1407404}

--- chrome/browser/ui/webui/signin/profile_picker_handler.h
+++ chrome/browser/ui/webui/signin/profile_picker_handler.h
@@ -48,7 +48,7 @@ class ProfilePickerHandler : public content::WebUIMessageHandler,
                              public content::WebContentsObserver,
                              public ProfileAttributesStorage::Observer {
  public:
-  ProfilePickerHandler();
+  explicit ProfilePickerHandler(bool is_glic_version);
 
   ProfilePickerHandler(const ProfilePickerHandler&) = delete;
   ProfilePickerHandler& operator=(const ProfilePickerHandler&) = delete;
@@ -167,6 +167,8 @@ class ProfilePickerHandler : public content::WebUIMessageHandler,
   // Returns the list of profiles in the same order as when the picker
   // was first shown.
   std::vector<ProfileAttributesEntry*> GetProfileAttributes();
+
+  const bool is_glic_version_;
 
   // Observes changes to profile attributes, and notifies the WebUI.
   base::ScopedObservation<ProfileAttributesStorage,
--- chrome/browser/ui/webui/signin/profile_picker_handler_unittest.cc
+++ chrome/browser/ui/webui/signin/profile_picker_handler_unittest.cc
@@ -69,8 +69,9 @@ void VerifyProfileEntry(const base::Value::Dict& dict,
 
 class ProfilePickerHandlerTest : public testing::Test {
  public:
-  ProfilePickerHandlerTest()
-      : profile_manager_(TestingBrowserProcess::GetGlobal()) {
+  explicit ProfilePickerHandlerTest(bool is_glic_version = false)
+      : is_glic_version_(is_glic_version),
+        profile_manager_(TestingBrowserProcess::GetGlobal()) {
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
     scoped_feature_list_.InitAndEnableFeature(
         supervised_user::kHideGuestModeForSupervisedUsers);
@@ -80,7 +81,7 @@ class ProfilePickerHandlerTest : public testing::Test {
   void SetUp() override {
     ASSERT_TRUE(profile_manager_.SetUp());
 
-    handler_ = std::make_unique<ProfilePickerHandler>();
+    handler_ = std::make_unique<ProfilePickerHandler>(is_glic_version_);
     web_ui_profile_ = GetWebUIProfile();
     web_ui_.set_web_contents(
         web_contents_factory_.CreateWebContents(web_ui_profile_));
@@ -169,6 +170,8 @@ class ProfilePickerHandlerTest : public testing::Test {
   ProfilePickerHandler* handler() { return handler_.get(); }
 
  private:
+  const bool is_glic_version_;
+
   content::BrowserTaskEnvironment task_environment_;
   TestingProfileManager profile_manager_;
   content::TestWebContentsFactory web_contents_factory_;
@@ -482,4 +485,22 @@ TEST_F(ProfilePickerHandlerTest, UpdateProfileOrder) {
         entries_to_names(storage->GetAllProfilesAttributesSortedForDisplay()),
         expected_profile_order_names);
   }
+}
+
+class ProfilePickerHandlerGlicVersionTest : public ProfilePickerHandlerTest {
+ public:
+  ProfilePickerHandlerGlicVersionTest()
+      : ProfilePickerHandlerTest(/*is_glic_version=*/true) {}
+};
+
+TEST_F(ProfilePickerHandlerGlicVersionTest, FilteringProfileEntries) {
+  ProfileAttributesEntry* eligible_1 = CreateTestingProfile("E1");
+  eligible_1->SetIsGlicEligible(true);
+  ProfileAttributesEntry* eligible_2 = CreateTestingProfile("E2");
+  eligible_2->SetIsGlicEligible(true);
+
+  ProfileAttributesEntry* ineligible_1 = CreateTestingProfile("I1");
+  ineligible_1->SetIsGlicEligible(false);
+
+  InitializeMainViewAndVerifyProfileList({eligible_1, eligible_2});
 }
