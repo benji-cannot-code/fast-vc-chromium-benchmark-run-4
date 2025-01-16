007dad186b14e50ce89441e7b03a4b681e74ebe6
Adam Norberg <norberg@google.com>
2025-01-16 11:28:04 -0800

    Expect an updater .zip in CRURegistration rather than a helper .app.
    This uses `tar` to unzip the `.zip` because `unzip`'s CLI is alarmingly
    inconsistent; `tar` is a lot more predictable.
    
    Embedding an app in another app is hard to do (mostly for getting the
    symlinks and signing right). Embedding a .zip as a resource is much
    easier, but we have to extract the .zip.
    
    This makes no effort to figure out if it needs to spend the time
    extracting the zip and installing the updater; it would be polite to
    the user to only bother doing this if we actually need to install
    anything. Checking for an already-installed updater and skipping the
    installation is an option for another patch, this one has enough
    going on already.
    
    Bug: 341790927
    Change-Id: I88c8f0a1534b1750085017f6463f9bc8a92e19d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169049
    Commit-Queue: Adam Norberg <norberg@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407471}

---
+++ chrome/updater/mac/client_lib/CRURegistrationPaths.m
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Foundation/Foundation.h>
+
+#import "CRURegistration.h"
+
+NSString* const CRUArchiveBasename = @(CRU_PRODUCT_FULLNAME_STRING);
+NSString* const CRUInstallerAppBasename = @(CRU_PRODUCT_FULLNAME_STRING);
