f3e6892d39fb01c545cd97991fc4f2a295f87424
Steinar H. Gunderson <sesse@chromium.org>
2025-01-16 02:18:17 -0800

    Move the customizable select resources into the main stylesheets.
    Now what we have @supports blink-feature(), there's no need to keep
    these separate in code and add them after-the-fact lazily to the main
    ruleset; it only adds complexity.
    
    Ideally, we'd like to get rid of adding to RuleSet after-the-fact
    entirely (it means we have to keep support for uncompacting them),
    but we currently cannot do that due to printing. Also, we cannot
    do this for PEPC since features in origin trials are not supported.
    
    Bug: 41483940
    Change-Id: I691290c5447f7f020d2045113edb0bc8f8157a31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6170442
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407202}

--- third_party/blink/renderer/core/css/css_default_style_sheets.h
+++ third_party/blink/renderer/core/css/css_default_style_sheets.h
@@ -85,12 +85,6 @@ class CSSDefaultStyleSheets final
 
   StyleSheetContents* DefaultStyleSheet() { return default_style_sheet_.Get(); }
   StyleSheetContents* QuirksStyleSheet() { return quirks_style_sheet_.Get(); }
-  StyleSheetContents* CustomizableSelectStyleSheet() {
-    return customizable_select_style_sheet_.Get();
-  }
-  StyleSheetContents* CustomizableSelectForcedColorsStyleSheet() {
-    return customizable_select_forced_colors_style_sheet_.Get();
-  }
   StyleSheetContents* SvgStyleSheet() { return svg_style_sheet_.Get(); }
   StyleSheetContents* MathmlStyleSheet() { return mathml_style_sheet_.Get(); }
   StyleSheetContents* MediaControlsStyleSheet() {
@@ -179,8 +173,6 @@ class CSSDefaultStyleSheets final
   Member<StyleSheetContents> permission_element_style_sheet_;
   Member<StyleSheetContents> text_track_style_sheet_;
   Member<StyleSheetContents> fullscreen_style_sheet_;
-  Member<StyleSheetContents> customizable_select_style_sheet_;
-  Member<StyleSheetContents> customizable_select_forced_colors_style_sheet_;
   Member<StyleSheetContents> marker_style_sheet_;
   Member<StyleSheetContents> scroll_button_style_sheet_;
   Member<StyleSheetContents> forced_colors_style_sheet_;
