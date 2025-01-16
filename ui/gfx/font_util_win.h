bd2423b65c4f4609b3564fd3049965e5097520db
Kurt Catti-Schmidt <kschmi@microsoft.com>
2025-01-16 12:22:32 -0800

    [Text Contrast] Set contrast at compile time
    The `IncreaseWindowsTextContrast` feature flag that sets text
    contrast to 1.0 at runtime has been enabled-by-default since
    version 132. This change enables the new value (1.0) at compile
    time. This change also removes the "IncreaseWindowsTextContrast"
    feature flag, as it will have no effect when the contrast value is
    fixed at compile time.
    
    The advantage of setting this value at compile time is a slight
    memory reduction, as Skia will no longer need multiple text contrast
    lookup tables.
    
    There were several tests that needed to be updated - a few
    reference images needed updates and some additional tests needed
    `will-change: transform` added on both test and -expected
    files so they both consistently apply LCDText behaviors.
    
    
    Change-Id: I7f62348b32ff1b295a6beef496f8c6b0322d28df
    Bug: 40037626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6150028
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407508}

--- ui/gfx/font_util_win.h
+++ ui/gfx/font_util_win.h
@@ -17,7 +17,6 @@ class FontUtilWin {
   static float ClampGamma(float value);
   static float GetContrastFromRegistry();
   static float GetGammaFromRegistry();
-  static float TextGammaContrast();
 };
 
 }  // namespace gfx
