806cfc9d58aac1b2d549436f366f8d7e74a91704
vikas soni <vikassoni@chromium.org>
2025-01-16 09:50:31 -0800

    [SIPool] Implement a service side counterpart of SharedImagePool.
    Implement a service side counterpart of SharedImagePool called
    SharedImagePoolService. Each client side pool in the renderer/browser
    process will have this corresponding service side pool object in the
    GPU process.
    
    SharedImagePoolService is responsible for sending gpu signals from GPU
    process to client process via SharedImagePoolClientInterface. It will
    be updated in the future to add more functionality like doing some
    post processing on incoming request to create shared images.
    
    This CL only implements the SharedImagePoolService. Additional plumbing
    to send GPU signals from GPU process to renderer/browser process, i.e,
    making the remote and receiver connection will be done by future CLs.
    
    Bug: 334764455
    Change-Id: I5bdcb3a1c7fdb10226acaf3d9108d41027e827f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108719
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407394}

---
+++ gpu/command_buffer/service/shared_image/shared_image_pool_service.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/command_buffer/service/shared_image/shared_image_pool_service.h"
+
+namespace gpu {
+
+SharedImagePoolService::SharedImagePoolService(
+    SharedImagePoolId pool_id,
+    mojo::PendingRemote<mojom::SharedImagePoolClientInterface> client_remote,
+    SharedImageFactory* factory)
+    : pool_id_(pool_id),
+      client_remote_(std::move(client_remote)),
+      factory_(std::move(factory)) {}
+
+SharedImagePoolService::~SharedImagePoolService() = default;
+
+void SharedImagePoolService::NotifyClearPool() {
+  if (client_remote_.is_bound()) {
+    client_remote_->OnClearPool();
+  }
+}
+
+}  // namespace gpu
