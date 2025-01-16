5aeab5382944fa656fa376cc151b51f9a6492bed
EmiliaPaz <emiliapaz@chromium.org>
2025-01-16 07:02:50 -0800

    [Extensions] Request entry label flex horizontal
    Change the request entry label flex behavior to only expand
    horizontally. This fixes a bug where the request entry label would flex unbounded both vertically and horizontally, causing a wrong layout.
    
    Screenshots:
    - Before:
    https://drive.google.com/file/d/1n7AsC8BW1aUOa0xxAQ1P2_In_J6jsv6r/view?usp=sharing
    - After:
    https://drive.google.com/file/d/1s15HuXqwoK1DFRL0G_Cl7oCK99rzAJJO/view?usp=sharing
    
    Bug: 385271864
    Change-Id: Iacb849d69cfe970a6171ecd182b7c9cb2d8ded7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177990
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407299}

--- chrome/browser/ui/views/extensions/extensions_menu_main_page_view.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view.cc
@@ -390,6 +390,7 @@ void ExtensionsMenuMainPageView::AddOrUpdateExtensionRequestingAccess(
                   .SetHorizontalAlignment(gfx::ALIGN_LEFT)
                   .SetProperty(views::kFlexBehaviorKey,
                                views::FlexSpecification(
+                                   views::LayoutOrientation::kHorizontal,
                                    views::MinimumFlexSizeRule::kScaleToZero,
                                    views::MaximumFlexSizeRule::kUnbounded)),
               views::Builder<views::MdTextButton>()
