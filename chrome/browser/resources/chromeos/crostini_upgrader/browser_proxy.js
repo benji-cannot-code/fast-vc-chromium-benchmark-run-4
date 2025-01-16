c6a9ff391b1adf5a06ca3c4522199a20ab0ca3e7
dpapad <dpapad@chromium.org>
2025-01-16 00:01:21 -0800

    CrOS: Migrate chrome://crostini-upgrader off of "lite" Mojo bindings.
    Bug: 40169715
    Change-Id: I7a8a543c5cb6818bcb923c10de11b56d6129e2ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175184
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407154}

--- chrome/browser/resources/chromeos/crostini_upgrader/browser_proxy.js
+++ chrome/browser/resources/chromeos/crostini_upgrader/browser_proxy.js
@@ -3,18 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(lxj): use es6 module when it is ready crbug/1004256
-import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
-import './crostini_upgrader.mojom-lite.js';
+import {PageCallbackRouter, PageHandlerFactory, PageHandlerRemote} from './crostini_upgrader.mojom-webui.js';
 
 export class BrowserProxy {
   constructor() {
-    /** @type {ash.crostiniUpgrader.mojom.PageCallbackRouter} */
-    this.callbackRouter = new ash.crostiniUpgrader.mojom.PageCallbackRouter();
-    /** @type {ash.crostiniUpgrader.mojom.PageHandlerRemote} */
-    this.handler = new ash.crostiniUpgrader.mojom.PageHandlerRemote();
+    /** @type {PageCallbackRouter} */
+    this.callbackRouter = new PageCallbackRouter();
+    /** @type {PageHandlerRemote} */
+    this.handler = new PageHandlerRemote();
 
-    const factory = ash.crostiniUpgrader.mojom.PageHandlerFactory.getRemote();
+    const factory = PageHandlerFactory.getRemote();
     factory.createPageHandler(
         this.callbackRouter.$.bindNewPipeAndPassRemote(),
         this.handler.$.bindNewPipeAndPassReceiver());
