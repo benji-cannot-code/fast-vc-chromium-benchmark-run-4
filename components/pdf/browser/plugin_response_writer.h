9d8053dd0390a1256fcec03c76cfde7a3ede66ba
Andy Phan <andyphan@chromium.org>
2025-01-15 16:23:27 -0800

    [OOPIF PDF] Add COEP: credentialless header to PDF content when needed
    When the original PDF navigation has a COEP: credentialless header, copy
    the header for the PDF content frame navigation.
    
    The original fix for OOPIF PDFs not loading with COEP headers only fixed
    the issue for COEP: require-corp headers (https://crrev.com/c/5778839).
    This CL extends the fix to work for PDFs with COEP: credentialless
    headers, too.
    
    Fixed: 390162930
    Change-Id: I5386280bd537e90a9ee995d7070a8ca5470396b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176406
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407024}

--- components/pdf/browser/plugin_response_writer.h
+++ components/pdf/browser/plugin_response_writer.h
@@ -49,7 +49,7 @@ class PluginResponseWriter final {
   std::string body_;
   mojo::Remote<network::mojom::URLLoaderClient> client_;
   std::unique_ptr<mojo::DataPipeProducer> producer_;
-  bool require_corp_ = false;
+  std::string coep_header_;
 };
 
 }  // namespace pdf
