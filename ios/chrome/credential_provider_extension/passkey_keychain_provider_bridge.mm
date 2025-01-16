8360f934e2a02222b6131ec1a281b1c85d2cf149
Alexis Hetu <sugoi@chromium.org>
2025-01-16 13:07:09 -0800

    [iOS] Change DecryptPrivateKey to return all credential secrets
    Changed DecryptPrivateKey to DecryptCredentialSecrets, because we
    will need access to both the private key and the hmac secret.
    
    Bug: 355045497
    Change-Id: I8cfe4986d14958fa16e65b81b3458df8d45edd08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172367
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407537}

--- ios/chrome/credential_provider_extension/passkey_keychain_provider_bridge.mm
+++ ios/chrome/credential_provider_extension/passkey_keychain_provider_bridge.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/credential_provider_extension/passkey_keychain_provider_bridge.h"
 
 #import "base/functional/callback.h"
+#import "components/sync/protocol/webauthn_credential_specifics.pb.h"
 #import "ios/chrome/credential_provider_extension/passkey_util.h"
 
 typedef void (^CheckEnrolledCompletionBlock)(BOOL is_enrolled, NSError* error);
@@ -31,9 +32,12 @@ NSArray<NSData*>* GetSecurityDomainSecret(
 bool ContainsValidKey(const PasskeyKeychainProvider::SharedKeyList keys,
                       id<Credential> credential) {
   NSArray<NSData*>* security_domain_secrets = GetSecurityDomainSecret(keys);
-  std::string private_key =
-      DecryptPrivateKey(credential, security_domain_secrets);
-  return !private_key.empty();
+
+  // DecryptCredentialSecrets will succeed if and only if there's at least one
+  // valid key.
+  std::optional<sync_pb::WebauthnCredentialSpecifics_Encrypted> encrypted =
+      DecryptCredentialSecrets(credential, security_domain_secrets);
+  return encrypted.has_value();
 }
 
 }  // namespace
--- ios/chrome/credential_provider_extension/passkey_util.h
+++ ios/chrome/credential_provider_extension/passkey_util.h
@@ -9,7 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <AuthenticationServices/AuthenticationServices.h>
 #import <Foundation/Foundation.h>
 
+#import <optional>
 #import <string>
+
+namespace sync_pb {
+class WebauthnCredentialSpecifics_Encrypted;
+}  // namespace sync_pb
 
 @protocol Credential;
 
@@ -21,10 +26,13 @@ enum class UserVerificationPreference {
   kOther,
 };
 
-// Decrypts the credential's private key. Can be used to verify if any of the
-// security_domain_secrets from the provided array is valid.
-std::string DecryptPrivateKey(id<Credential> credential,
-                              NSArray<NSData*>* security_domain_secrets);
+// Decrypts the credential's secrets, like the private key and the hmac secret.
+// Can be used to verify if any of the security_domain_secrets from the provided
+// array is valid. If the decryption is successful, the results will be stored
+// in the provided `credential_secrets` structure.
+std::optional<sync_pb::WebauthnCredentialSpecifics_Encrypted>
+DecryptCredentialSecrets(id<Credential> credential,
+                         NSArray<NSData*>* security_domain_secrets);
 
 // On a success, returns a newly created passkey.
 // Returns nil otherwise.
