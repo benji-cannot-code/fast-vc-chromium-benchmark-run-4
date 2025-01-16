281b2a6915466a0cbe60bbbf98772b1fd0592db6
Kristi Saney <kristislee@google.com>
2025-01-16 11:59:38 -0800

    [Read aloud] Reduce flakiness of LinksToggledIntegration
    I can't actually reproduce the flakiness locally but I am seeing it a
    bit on LUCI. Hoping this helps and will continue to investigate.
    
    Bug: 40927698
    Change-Id: I2a7c972319a78c0760d15cbb843d68a54fbc2481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173959
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1407496}

--- chrome/test/data/webui/side_panel/read_anything/links_toggled_integration_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/links_toggled_integration_test.ts
@@ -127,6 +127,7 @@ suite('LinksToggledIntegration', () => {
           emitEvent(app, ToolbarEvent.NEXT_GRANULARITY);
           await microtasksFinished();
         }
+        return microtasksFinished();
       });
 
       test('container has links again', () => {
@@ -136,8 +137,9 @@ suite('LinksToggledIntegration', () => {
   });
 
   suite('after speech pauses', () => {
-    setup(() => {
+    setup(async () => {
       app.playSpeech();
+      await microtasksFinished();
       app.stopSpeech(PauseActionSource.BUTTON_CLICK);
       return microtasksFinished();
     });
@@ -185,9 +187,11 @@ suite('LinksToggledIntegration', () => {
     });
 
     suite('after speech pauses', () => {
-      setup(() => {
+      setup(async () => {
         app.playSpeech();
+        await microtasksFinished();
         app.stopSpeech(PauseActionSource.BUTTON_CLICK);
+        return microtasksFinished();
       });
 
       test('container does not have links', () => {
