a11ededc076f86ea18e960c8c8199ae7ca46f403
Richard Chui <richui@chromium.org>
2025-01-16 15:57:36 -0800

    focus-mode: fix ending moment nudge focus issue
    Fix the order of how the focus mode ending moment nudge is closed in
    respect to opening the ending moment bubble. Previously it was hidden
    after we showed the bubble, but this caused issues in the focus
    controller activating an existing open window. By showing the bubble
    after hiding the nudge, we can make sure that the bubble is focused
    correctly and doesn't get closed.
    
    Bug: b:384586824
    Test: manual, added new test
    Change-Id: I25d027290297407f965821761d2ccd0e5eee79f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164614
    Commit-Queue: Richard Chui <richui@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407626}

--- ash/system/focus_mode/focus_mode_controller.cc
+++ ash/system/focus_mode/focus_mode_controller.cc
@@ -458,7 +458,7 @@ void FocusModeController::ResetFocusSession() {
   }
 }
 
-void FocusModeController::OnEndingBubbleShown() {
+void FocusModeController::OnEndingBubbleShowing() {
   if (!in_ending_moment()) {
     return;
   }
