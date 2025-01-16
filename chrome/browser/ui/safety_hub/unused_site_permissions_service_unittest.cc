8089c30fc94f26d76f53afb5bca5df0815fc9f4d
Peter Kasting <pkasting@chromium.org>
2025-01-16 09:22:23 -0800

    Port to/improve compat with std::ranges::: chrome/
    Bug: 386918226
    Change-Id: Iba5c04c5be0b092990335cf5c43835ae3a55a10b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177197
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1407376}

--- chrome/browser/ui/safety_hub/unused_site_permissions_service_unittest.cc
+++ chrome/browser/ui/safety_hub/unused_site_permissions_service_unittest.cc
@@ -60,6 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+using ::testing::UnorderedElementsAre;
+
 namespace {
 
 const char url1[] = "https://example1.com:443";
@@ -1234,19 +1236,11 @@ TEST_P(UnusedSitePermissionsServiceTest, ResultToFromDict) {
   base::Value::Dict dict = result->ToDictValue();
   auto* revoked_origins_list = dict.FindList(kUnusedSitePermissionsResultKey);
   if (ShouldSetupUnusedSites() && ShouldSetupAbusiveNotificationSites()) {
-    EXPECT_EQ(3U, revoked_origins_list->size());
-    EXPECT_EQ(url1, revoked_origins_list->front().GetString());
-    EXPECT_TRUE(base::Contains(*revoked_origins_list, url1));
-    EXPECT_TRUE(base::Contains(*revoked_origins_list, url2));
-    EXPECT_TRUE(base::Contains(*revoked_origins_list, url3));
+    EXPECT_THAT(*revoked_origins_list, UnorderedElementsAre(url1, url2, url3));
   } else if (ShouldSetupUnusedSites()) {
-    EXPECT_EQ(2U, revoked_origins_list->size());
-    EXPECT_TRUE(base::Contains(*revoked_origins_list, url1));
-    EXPECT_TRUE(base::Contains(*revoked_origins_list, url2));
+    EXPECT_THAT(*revoked_origins_list, UnorderedElementsAre(url1, url2));
   } else if (ShouldSetupAbusiveNotificationSites()) {
-    EXPECT_EQ(2U, revoked_origins_list->size());
-    EXPECT_TRUE(base::Contains(*revoked_origins_list, url2));
-    EXPECT_TRUE(base::Contains(*revoked_origins_list, url3));
+    EXPECT_THAT(*revoked_origins_list, UnorderedElementsAre(url2, url3));
   }
 }
 
--- chrome/utility/importer/bookmarks_file_importer.cc
+++ chrome/utility/importer/bookmarks_file_importer.cc
@@ -68,7 +68,7 @@ bool CanImportURL(const GURL& url) {
       }
     }
 
-    if (base::Contains(chrome::ChromeDebugURLs(), fixed_url)) {
+    if (base::Contains(chrome::ChromeDebugURLs(), fixed_url.spec())) {
       return true;
     }
 
