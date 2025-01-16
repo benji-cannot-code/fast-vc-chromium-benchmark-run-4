116d12798e6d695d03396a883f3578c036722093
James Cook <jamescook@chromium.org>
2025-01-15 16:25:32 -0800

    extensions: Port chrome_settings_override test to desktop Android
    The settings_override API only exists on Win and Mac, but the manifest
    key is compiled for all platforms. There's a test to verify that the
    API is not allowed on other platforms. Enable that test on desktop
    Android.
    
    Bug: 390015399
    Change-Id: I57f2dae935dac7f958bce5d9315b9af4b324d832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168801
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407025}

--- chrome/browser/extensions/api/settings_overrides/settings_overrides_browsertest.cc
+++ chrome/browser/extensions/api/settings_overrides/settings_overrides_browsertest.cc
@@ -4,16 +4,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <memory>
+#include <string>
+#include <vector>
 
+#include "build/build_config.h"
+#include "content/public/test/browser_test.h"
+#include "extensions/common/extension.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/extensions/extension_platform_browsertest.h"
+#else
 #include "base/command_line.h"
 #include "base/containers/to_vector.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
-#include "build/build_config.h"
 #include "chrome/browser/extensions/api/settings_overrides/settings_overrides_api.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_service.h"
-#include "chrome/browser/prefs/session_startup_pref.h"
+#include "chrome/browser/prefs/session_startup_pref.h"  // nogncheck
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
@@ -31,12 +39,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_prepopulate_data.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/version_info/version_info.h"
-#include "content/public/test/browser_test.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/mock_external_provider.h"
 #include "extensions/browser/test_extension_registry_observer.h"
 #include "extensions/common/feature_switch.h"
 #include "extensions/common/features/feature_channel.h"
+#endif
 
 namespace extensions {
 
@@ -327,7 +335,15 @@ IN_PROC_BROWSER_TEST_F(ExtensionsDisabledWithSettingsOverrideAPI,
 }
 
 #else
-IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, SettingsOverridesDisallowed) {
+
+#if BUILDFLAG(IS_ANDROID)
+using ExtensionSettingsOverrideTest = ExtensionPlatformBrowserTest;
+#else
+using ExtensionSettingsOverrideTest = ExtensionBrowserTest;
+#endif
+
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsOverrideTest,
+                       SettingsOverridesDisallowed) {
   const extensions::Extension* extension =
       LoadExtension(test_data_dir_.AppendASCII("settings_override"),
                     {.ignore_manifest_warnings = true});
@@ -337,7 +353,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, SettingsOverridesDisallowed) {
                         "is not allowed for specified platform."),
             extension->install_warnings().front().message);
 }
-#endif
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 
 }  // namespace
 }  // namespace extensions
