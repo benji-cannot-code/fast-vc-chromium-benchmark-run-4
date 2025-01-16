ac68a43fa05be6392e5bc0310722d628cfff1398
sangwoo ko <sangwoo108@chromium.org>
2025-01-16 11:50:46 -0800

    Pass ImageModel to ImageView::SetImage() in ash/system/power
    The ImageSkia version of ImageView::SetImage() is deprecated.
    There's no intended behavior changes with this patch.
    
    This CL was uploaded by git cl split.
    
    R=jiamingc@chromium.org
    
    Bug: 40137576
    Change-Id: I3214676006c055d8612fa511595ad7acb839a147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166602
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Auto-Submit: SangWoo Ko <sangwoo108@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407488}

--- ash/system/power/tray_power.cc
+++ ash/system/power/tray_power.cc
@@ -104,8 +104,9 @@ void PowerTrayView::UpdateLabelOrImageViewColor(bool active) {
   PowerStatus::BatteryImageInfo info =
       PowerStatus::Get()->GenerateBatteryImageInfo(icon_fg_color);
 
-  image_view()->SetImage(PowerStatus::GetBatteryImage(
-      info, kUnifiedTrayBatteryIconSize, GetColorProvider()));
+  image_view()->SetImage(
+      ui::ImageModel::FromImageSkia(PowerStatus::GetBatteryImage(
+          info, kUnifiedTrayBatteryIconSize, GetColorProvider())));
 }
 
 void PowerTrayView::OnPowerStatusChanged() {
