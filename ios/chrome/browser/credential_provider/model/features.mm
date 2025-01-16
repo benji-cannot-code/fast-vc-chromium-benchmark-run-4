5f78567edad8834cc3565a70b1637c40029d58cd
Tommy Martino <tmartino@google.com>
2025-01-16 09:29:16 -0800

    [iOS] Disable CPE Performance Improvements on trunk
    This is a kill switch CL.
    
    Bug: 388494593
    Change-Id: Ib0a17d67e654ed7b0e37ba00a8391337c80e9318
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6178246
    Auto-Submit: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407381}

--- ios/chrome/browser/credential_provider/model/features.mm
+++ ios/chrome/browser/credential_provider/model/features.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 BASE_FEATURE(kCredentialProviderPerformanceImprovements,
              "CredentialProviderPerformanceImprovements",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsCPEPerformanceImprovementsEnabled() {
   return base::FeatureList::IsEnabled(
