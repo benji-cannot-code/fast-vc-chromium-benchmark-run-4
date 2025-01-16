1ba27fef0ec2aaf19b8f4e4354290caeb660c70a
Takuto Ikuta <tikuta@chromium.org>
2025-01-15 19:11:48 -0800

    chrome: replace std::variant with absl::variant
    std::variant is currently banned.
    https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/c++-features.md#std_variant-banned
    
    This is to fix build error too when we set use_libcxx_modules=true in
    chromium build.
    
    Bug: 40263312
    Change-Id: I3bcb65d1f8624070bd3adcdeae761900cdd1cd16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172678
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407093}

--- chrome/browser/ui/tabs/tab_strip_model_observer.h
+++ chrome/browser/ui/tabs/tab_strip_model_observer.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/core/session_id.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 #include "ui/base/models/list_selection_model.h"
 
@@ -198,7 +199,7 @@ class TabStripModelChange {
   void WriteIntoTrace(perfetto::TracedValue context) const;
 
  private:
-  using Delta = std::variant<Insert, Remove, Move, Replace>;
+  using Delta = absl::variant<Insert, Remove, Move, Replace>;
 
   TabStripModelChange(Type type, Delta delta);
 
