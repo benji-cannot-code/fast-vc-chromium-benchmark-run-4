42fb7d515671d0640dfce0987d1f8a447824e146
Elmehdi Rahmaoui <erahmaoui@google.com>
2025-01-16 02:09:42 -0800

    [iOS] Youtube Incognito flag variation
    This CL adds a variation to the Youtube Incognito flag, to choose
    whether to handle the fetching errors with incognito interstitial or not.
    
    Bug: 389048721
    Change-Id: I54c9d807c71f19d8a449acfd2be1307c7b662285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172627
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1407200}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1269,6 +1269,19 @@ const FeatureEntry::FeatureVariation kUpdatedFirstRunSequenceVariations[] = {
      kUpdatedFirstRunSequenceArm3, std::size(kUpdatedFirstRunSequenceArm3),
      nullptr}};
 
+const FeatureEntry::FeatureParam
+    kYoutubeIncognitoErrorHandlingWithoutIncognitoInterstitial[] = {
+        {kYoutubeIncognitoErrorHandlingWithoutIncognitoInterstitialParam,
+         "true"},
+};
+
+const FeatureEntry::FeatureVariation kYoutubeIncognitoVariations[] = {
+    {"Error handling without Incognito Interstitial",
+     kYoutubeIncognitoErrorHandlingWithoutIncognitoInterstitial,
+     std::size(kYoutubeIncognitoErrorHandlingWithoutIncognitoInterstitial),
+     nullptr},
+};
+
 // To add a new entry, add to the end of kFeatureEntries. There are four
 // distinct types of entries:
 // . ENABLE_DISABLE_VALUE: entry is either enabled, disabled, or uses the
@@ -2203,7 +2216,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flags_ui::kOsIos, FEATURE_VALUE_TYPE(kChromeStartupParametersAsync)},
     {"ios-youtube-incognito", flag_descriptions::kYoutubeIncognitoName,
      flag_descriptions::kYoutubeIncognitoDescription, flags_ui::kOsIos,
-     FEATURE_VALUE_TYPE(kYoutubeIncognito)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(kYoutubeIncognito,
+                                    kYoutubeIncognitoVariations,
+                                    "IOSYoutubeIncognito")},
     {"lens-overlay-enable-location-bar-entrypoint",
      flag_descriptions::kLensOverlayEnableLocationBarEntrypointName,
      flag_descriptions::kLensOverlayEnableLocationBarEntrypointDescription,
