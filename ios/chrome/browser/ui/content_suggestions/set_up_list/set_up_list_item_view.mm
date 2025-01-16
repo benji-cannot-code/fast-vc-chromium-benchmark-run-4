c2c3894600270d48996d0db7efbb29d7170f852f
Briana McClure <bmcclure@google.com>
2025-01-16 12:36:13 -0800

    [iOS] Update the Magic Stack card for dynamic font sizes.
    Update the Magic Stack to dynamically adjust the card height based on
    the user's font size.
    
    Updated Magic Stack: go/magic-stack-polish
    
    Fixed: 378773890
    Change-Id: I79f0e99f44fc69a24f8d9d522ad9e3ad471ff929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6065038
    Auto-Submit: Briana McClure <bmcclure@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407515}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_item_view.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_item_view.mm
@@ -36,7 +36,7 @@ namespace {
 constexpr CGFloat kPadding = 15;
 
 // The spacing between the title and description labels.
-constexpr CGFloat kTextSpacing = 5;
+constexpr CGFloat kTextSpacing = 2;
 constexpr CGFloat kCompactTextSpacing = 0;
 
 // The duration of the icon / label crossfade animation.
@@ -153,12 +153,6 @@ struct ViewConfig {
           kTextSpacing,
       };
     }
-    if (@available(iOS 17, *)) {
-      NSArray<UITrait>* traits = TraitCollectionSetForTraits(
-          @[ UITraitPreferredContentSizeCategory.class ]);
-      [self registerForTraitChanges:traits
-                         withAction:@selector(hideDescriptionOnTraitChange)];
-    }
   }
   return self;
 }
@@ -183,11 +177,6 @@ struct ViewConfig {
   [super traitCollectionDidChange:previousTraitCollection];
   if (@available(iOS 17, *)) {
     return;
-  }
-
-  if (previousTraitCollection.preferredContentSizeCategory !=
-      self.traitCollection.preferredContentSizeCategory) {
-    [self hideDescriptionOnTraitChange];
   }
 }
 #endif
@@ -299,6 +288,8 @@ struct ViewConfig {
   textStack.axis = UILayoutConstraintAxisVertical;
   textStack.translatesAutoresizingMaskIntoConstraints = NO;
   textStack.spacing = _config.text_spacing;
+  textStack.maximumContentSizeCategory =
+      UIContentSizeCategoryAccessibilityMedium;
 
   // Add a horizontal stack to contain the icon(s) and the text stack.
   NSArray* arrangedSubviews = putIconInSquareBackground
@@ -359,6 +350,10 @@ struct ViewConfig {
   if (_complete) {
     label.attributedText = Strikethrough(label.text);
   }
+  // Set height constraint. Applicable for larger text.
+  [NSLayoutConstraint activateConstraints:@[ [label.heightAnchor
+                                              constraintEqualToConstant:26] ]];
+
   [label
       setContentCompressionResistancePriority:UILayoutPriorityDefaultLow
                                       forAxis:UILayoutConstraintAxisVertical];
@@ -441,16 +436,6 @@ struct ViewConfig {
     case SetUpListItemType::kFollow:
       return set_up_list::kFollowItemID;
   }
-}
-
-// Hides `_description` if the font size category is larger than
-// extra-extra-large.
-- (void)hideDescriptionOnTraitChange {
-  _description.hidden = self.traitCollection.preferredContentSizeCategory >
-                        UIContentSizeCategoryExtraExtraLarge;
-  // Force a layout since the size of text components may have changed.
-  [self setNeedsLayout];
-  [self layoutIfNeeded];
 }
 
 #pragma mark - Private methods (animation helpers)
