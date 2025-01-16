ac1b79535ff4e4c93c19e62faa782dbd47899d8a
Thomas Lukaszewicz <tluk@chromium.org>
2025-01-16 10:05:07 -0800

    Add RenderProcessFlags::kDisallowV8FeatureFlagOverrides
    This CL adds a new RenderProcessFlag that allows configuring
    the RenderProcessHost to disallow v8 feature flag overrides.
    
    Whether a RPH qualifies for this treatment is decided on in
    ChromeContentBrowserClient, determined using the
    process_lock_url from the RPH's associated SiteInstance.
    
    The configuration is exposed to renderers via the
    "--disallow-v8-feature-flag-overrides" command line flag.
    
    
    This is intended to only apply to top-chrome WebUI renderers behind
    the kBundledWebUICodeCache flag. This is necessary specifically for
    build-time optimized WebUI as these optimizations require the v8 flag
    configuration present at build-time to match the configuration
    present at run-time.
    
    The SpareRenderer optimization is less relevant to the top-chrome
    WebUI as the process hosting these is started proactively shortly
    after startup with WebUI preloading.
    
    
    This change follows the same pattern as crrev.com/c/5689960 that
    added an option to disable V8 optimizations.
    
    See bug and linked DD for details.
    
    Bug: 388577282
    Change-Id: I89674c700f90f0557ed18e17de995101f9cc5989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6134286
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407411}

--- content/public/test/mock_render_process_host.cc
+++ content/public/test/mock_render_process_host.cc
@@ -205,6 +205,10 @@ bool MockRenderProcessHost::AreV8OptimizationsDisabled() {
   return false;
 }
 
+bool MockRenderProcessHost::DisallowV8FeatureFlagOverrides() {
+  return false;
+}
+
 bool MockRenderProcessHost::IsPdf() {
   return false;
 }
