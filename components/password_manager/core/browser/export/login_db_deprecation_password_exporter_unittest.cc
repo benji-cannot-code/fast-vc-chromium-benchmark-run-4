5354cf48bfe984d3e60a34cb60df2da1ea28cfc5
Ioana Pandele <ioanap@chromium.org>
2025-01-16 08:24:07 -0800

    [UPMLoginDbSunset] Set the export as finished for empty db
    This change sets the export as finished even if the db is empty or
    there are no autofillable passwords (there might be blocklisted
    entries in the db, which are not exportable).
    
    The pref is used to signal readiness for login db deprecation. It
    will be temporarily used to decide which store backend to instantiate.
    
    The pref should also be reset when the feature flag is off, to ensure
    that clients who might jump between feaure-enabled and
    feature-disabled states perform the export once the feature is enabled
    completely.
    
    Bug: 378650395
    Change-Id: I4cc382825c93f77aeba9940a53d67207c042eb82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174934
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407339}

--- components/password_manager/core/browser/export/login_db_deprecation_password_exporter_unittest.cc
+++ components/password_manager/core/browser/export/login_db_deprecation_password_exporter_unittest.cc
@@ -110,7 +110,7 @@ class LoginDbDeprecationPasswordExporterTest : public testing::Test {
   base::ScopedTempDir temp_dir_;
 };
 
-TEST_F(LoginDbDeprecationPasswordExporterTest, DoesntExportIfNoPasswords) {
+TEST_F(LoginDbDeprecationPasswordExporterTest, DoesntCreateFileIfNoPasswords) {
   base::HistogramTester histogram_tester;
   // No passwords in the backend.
   exporter()->Start(password_store(), task_env_.QuitClosure());
@@ -122,6 +122,8 @@ TEST_F(LoginDbDeprecationPasswordExporterTest, DoesntExportIfNoPasswords) {
   histogram_tester.ExpectUniqueSample(
       kExportResultHistogram, LoginDbDeprecationExportResult::kNoPasswords, 1);
   histogram_tester.ExpectTotalCount(kExportLatencyHistogram, 0);
+  EXPECT_TRUE(
+      pref_service()->GetBoolean(prefs::kUpmUnmigratedPasswordsExported));
 }
 
 TEST_F(LoginDbDeprecationPasswordExporterTest, ExportFailedToFetchPasswords) {
