c10fb391988afd87b4b10c244d2bb5a2496e1df7
Daniel Rubery <drubery@chromium.org>
2025-01-16 06:34:51 -0800

    Remove debug #include from registration_fetcher
    This was accidentally included during debugging.
    
    Change-Id: If94478694faf98ab2432cbc4a02f936210907c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6178310
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407276}

--- net/device_bound_sessions/registration_fetcher.cc
+++ net/device_bound_sessions/registration_fetcher.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/debug/stack_trace.h"
 #include "components/unexportable_keys/background_task_priority.h"
 #include "components/unexportable_keys/unexportable_key_service.h"
 #include "net/base/io_buffer.h"
