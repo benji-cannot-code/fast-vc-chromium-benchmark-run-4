959abccb0e11b5053cf3f226adc1deae0e30d9dc
Benjamin Gordon <bmgordon@chromium.org>
2025-01-15 16:04:23 -0800

    Remove document_scan::ScanFirstPage
    ScanFirstPage was used to implement the documentScan.scan extension
    function.  That function now uses the regular internal API directly, so
    ScanFirstPage is no longer needed.
    
    Bug: 317056194
    Test: tast run documentscanapi.Scan
    Change-Id: Id299eda63ffab71f287b6e7d3cb9c48c6a53c437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172818
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Nathan Muggli <nmuggli@google.com>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407012}

--- chrome/browser/ash/crosapi/document_scan_ash.h
+++ chrome/browser/ash/crosapi/document_scan_ash.h
@@ -24,8 +24,6 @@ class DocumentScanAsh : public mojom::DocumentScan {
 
   // crosapi::mojom::DocumentScan:
   void GetScannerNames(GetScannerNamesCallback callback) override;
-  void ScanFirstPage(const std::string& scanner_name,
-                     ScanFirstPageCallback callback) override;
   void GetScannerList(const std::string& client_id,
                       mojom::ScannerEnumFilterPtr filter,
                       GetScannerListCallback callback) override;
