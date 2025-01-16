09cf892aa28ba22c1d8d4987a47cfd915d833dcd
Brandon Fong <bjfong@google.com>
2025-01-16 12:56:07 -0800

    [Tab Group Parity] Migrate parity related tab switcher tests to PT
    Migrate the parity related tab switcher layout tests to public transit
    in order to address flakiness and improve coverage.
    
    Bug: 360393681, 365903259, 353463207
    Change-Id: I6f5834acff582df2b8abffc5ed8fc311f3b5c646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173635
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407527}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherGroupCardFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherGroupCardFacility.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.test.transit.hub;
 
 import androidx.annotation.Nullable;
+
 import org.chromium.base.test.transit.Elements;
 import org.chromium.chrome.test.transit.tabmodel.TabGroupExistsCondition;
 import org.chromium.chrome.test.transit.tabmodel.TabGroupUtil;
@@ -39,7 +40,7 @@ public class TabSwitcherGroupCardFacility extends TabSwitcherCardFacility {
     public TabSwitcherGroupCardFacility(@Nullable Integer cardIndex, List<Integer> tabIdsToGroup, String title) {
         super(
                 cardIndex,
-                title.equals(DEFAULT_N_TABS_TITLE)
+                title.equals(DEFAULT_N_TABS_TITLE) || title.isEmpty()
                         ? TabGroupUtil.getNumberOfTabsString(tabIdsToGroup.size())
                         : title);
         assert !tabIdsToGroup.isEmpty();
