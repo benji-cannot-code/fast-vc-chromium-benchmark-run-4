4e03d3723de7b3ea48703b35ac8bb4af5d474b26
Michael Cui <mlcui@google.com>
2025-01-15 22:20:56 -0800

    scanner: Add function to convert actions into JSON
    This will be used to serialise actions into human-readable strings for
    senders of feedback (users) and receivers of feedback.
    
    This is required as Chromium's lite runtime for Protobuf does not
    support descriptors and, by extension, text format.
    
    Bug: b:367882164
    Change-Id: I6a6a2100a3db3a7ae0fff624278f1d24d8c911ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171776
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1407136}

---
+++ ash/scanner/scanner_feedback.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SCANNER_SCANNER_FEEDBACK_H_
+#define ASH_SCANNER_SCANNER_FEEDBACK_H_
+
+#include "ash/ash_export.h"
+#include "base/values.h"
+
+namespace manta::proto {
+class ScannerAction;
+}
+
+namespace ash {
+
+// Converts a `manta::proto::ScannerAction` into an "externally tagged"
+// `base::Value::Dict` for use in filing feedback.
+// Required as Chromium's lite runtime for Protobuf does not support descriptors
+// and, by extension, text format.
+ASH_EXPORT base::Value::Dict ScannerActionToDict(
+    manta::proto::ScannerAction action);
+
+}  // namespace ash
+
+#endif  // ASH_SCANNER_SCANNER_FEEDBACK_H_
