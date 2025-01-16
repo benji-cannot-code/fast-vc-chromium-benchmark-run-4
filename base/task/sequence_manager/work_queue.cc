4485fa338bc022b44b7e89f098688081431613bd
Etienne Pierre-doray <etiennep@chromium.org>
2025-01-16 07:28:41 -0800

    [task] Use deque in sequence manager
    LazilyDeallocatedDeque increases peak memory usage by
    allocating a single segment in MaybeShrinkQueue(), causing
    OOM.
    Based on Sp3, it doesn't have a performance advantage vs std::deque.
    
    Change-Id: I71814dd550564ca988f09eb79fefdd5eaaa9cc8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177877
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407314}

--- base/task/sequence_manager/work_queue.cc
+++ base/task/sequence_manager/work_queue.cc
@@ -213,9 +213,6 @@ Task WorkQueue::TakeTaskFromWorkQueue() {
       // right thing.
       task_queue_->TakeImmediateIncomingQueueTasks(&tasks_);
     }
-    // Since the queue is empty, now is a good time to consider reducing it's
-    // capacity if we're wasting memory.
-    tasks_.MaybeShrinkQueue();
   }
 
   DCHECK(work_queue_sets_);
@@ -269,9 +266,6 @@ bool WorkQueue::RemoveAllCanceledTasksFromFront() {
         // right thing.
         task_queue_->TakeImmediateIncomingQueueTasks(&tasks_);
       }
-      // Since the queue is empty, now is a good time to consider reducing it's
-      // capacity if we're wasting memory.
-      tasks_.MaybeShrinkQueue();
     }
     // If we have a valid |heap_handle_| (i.e. we're not blocked by a fence or
     // disabled) then |work_queue_sets_| needs to be told.
@@ -331,10 +325,6 @@ bool WorkQueue::RemoveFence() {
     return true;
   }
   return false;
-}
-
-void WorkQueue::MaybeShrinkQueue() {
-  tasks_.MaybeShrinkQueue();
 }
 
 void WorkQueue::PopTaskForTesting() {
