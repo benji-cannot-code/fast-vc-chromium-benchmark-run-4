8c528aeb93e818c8d6f46e0e99dd77d1b6d5aa37
Charles Meng <charlesmeng@google.com>
2025-01-16 13:43:03 -0800

    Use 2x resolution icon for status items on Mac
    The glic status icon is blurry on retina displays due to using a 1x
    bitmap. Convert gfx::ImageSkia to NSImage through gfx::Image instead.
    
    Side by side: https://screenshot.googleplex.com/3t5wvweqSdbr7zV.png
    
    Fixed: 390255023
    Change-Id: Iaa365f964016d9aedd719896ae4e72ec8a8907bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177602
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407559}

--- chrome/browser/ui/cocoa/status_icons/status_icon_mac.mm
+++ chrome/browser/ui/cocoa/status_icons/status_icon_mac.mm
@@ -15,8 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/browser/status_icons/status_tray.h"
-#include "skia/ext/skia_utils_mac.h"
-#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_skia.h"
 #import "ui/menus/cocoa/menu_controller.h"
 #include "ui/message_center/public/cpp/notifier_id.h"
@@ -167,7 +166,7 @@ NSStatusItem* StatusIconMac::item() {
 
 void StatusIconMac::SetImage(const gfx::ImageSkia& image) {
   if (!image.isNull()) {
-    NSImage* ns_image = skia::SkBitmapToNSImage(*image.bitmap());
+    NSImage* ns_image = gfx::Image(image).AsNSImage();
     if (ns_image) {
       item().button.image = ns_image;
     }
