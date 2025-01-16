33774db373f309f94a58266c45002e174c3e690b
David Bokan <bokan@chromium.org>
2025-01-16 08:55:00 -0800

    Reland: Block Glic WebUI URL based on policy
    chrome://glic should not be available if Glic is disabled via policy.
    
    Reland note: Originally landed in https://crrev.com/c/6172873. Diff from
    PS1 to see changes from original CL. Issue is that the WebUI is now
    disabled using GlicEnabling::IsEnabledForProfile which checks both kGlic
    and kTabstripComboButton. The failing test was only enabling kGlic so it
    was now blocking the Glic WebUI.
    
    Bug: 382722218
    Change-Id: I63444d357e0458ed4a714294183edafd84c75df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175993
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407352}

--- chrome/test/data/webui/webview/webui_webview_browsertest.cc
+++ chrome/test/data/webui/webview/webui_webview_browsertest.cc
@@ -47,6 +47,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class WebUIWebViewBrowserTest : public WebUIMochaBrowserTest {
  public:
+  WebUIWebViewBrowserTest() {
+#if BUILDFLAG(ENABLE_GLIC)
+    // Required to enable chrome://glic.
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{features::kGlic, features::kTabstripComboButton},
+        /*disabled_features=*/{});
+#endif
+  }
+
   void SetUpOnMainThread() override {
     base::FilePath test_data_dir;
     base::PathService::Get(chrome::DIR_TEST_DATA, &test_data_dir);
@@ -119,10 +128,7 @@ class WebUIWebViewBrowserTest : public WebUIMochaBrowserTest {
             false);
   }
 
-#if BUILDFLAG(ENABLE_GLIC)
-  // Required to enable chrome://glic.
-  base::test::ScopedFeatureList scoped_feature_list_{{ features::kGlic }};
-#endif
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Checks that hiding and showing the WebUI host page doesn't break guests in
