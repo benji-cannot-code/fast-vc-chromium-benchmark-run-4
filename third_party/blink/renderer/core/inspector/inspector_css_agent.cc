641684890632542038b3a2e22bb78e64a064678b
Ergün Erdoğmuş <ergunsh@chromium.org>
2025-01-17 07:07:23 -0800

    [Inspector] Fix showing animations for inactive SDAs
    Inactive SDAs are registered as animations
    with `running` animation state and they have pending play.
    
    See the example in here: https://www.bram.us/2023/09/16/solved-by-css-scroll-driven-animations-detect-if-an-element-can-scroll-or-not/
    
    The inactive SDA should not show up in the Styles tab.
    
    You can test out this feature with this flag after 134.0.6955.0+:
    `--enable-features="DevToolsAnimationStylesInStylesTab"`
    
    Bug: 349566291
    Change-Id: I6a82dd98d0a035bad2913021db1032d4435cb9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172305
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407891}

--- third_party/blink/renderer/core/inspector/inspector_css_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_css_agent.cc
@@ -3613,8 +3613,9 @@ InspectorCSSAgent::BuildArrayForCSSAnimationStyleList(Element* element) {
   HeapVector<Member<Animation>> animations;
   for (const auto& entry : element_animations->Animations()) {
     Animation& animation = *entry.key;
-    // We only include CSS animations & WAAPI animations here.
-    if (animation.IsCSSTransition()) {
+    AnimationEffect* effect = animation.effect();
+    // We only include active CSS animations & WAAPI animations here.
+    if (animation.IsCSSTransition() || !effect || !effect->IsInEffect()) {
       continue;
     }
 
--- third_party/blink/web_tests/inspector-protocol/css/css-get-animated-styles.js
+++ third_party/blink/web_tests/inspector-protocol/css/css-get-animated-styles.js
@@ -42,6 +42,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   animation: 1s --color infinite;
 }
 
+#sda-non-scrolling {
+  width: 120px;
+  height: 120px;
+  animation: --color linear;
+  animation-timeline: scroll();
+}
+
+#animation-start-delay {
+  width: 120px;
+  height: 120px;
+  animation: --color linear;
+  animation-start-delay: 100s;
+}
+
 @keyframes --color {
   from {
     color: red;
@@ -69,6 +83,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   <div class='parent'>
     <div id='element'>Text</div>
     <div id='element-for-pseudo'></div>
+    <div id='sda-non-scrolling'></div>
+    <div id='animation-start-delay'></div>
   </div>
 </div>
 `,
@@ -213,6 +229,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   logCondition(
       'The color property is animated',
       animationStylesForPseudoWithoutParentAnimations[0].style.cssProperties[0].name === 'color');
+
+  testRunner.log('\nScroll driven animations in a non-scrollable container');
+  const sdaNodeId = nodeTracker.nodes().find(
+    node => DOMHelper.attributes(node).get('id') === 'sda-non-scrolling').nodeId;
+  const {
+    result: {
+      animationStyles: animationStylesForNonScrollingSda,
+    }
+  } = await dp.CSS.getAnimatedStylesForNode({nodeId: sdaNodeId});
+  logCondition(
+    'There is no animation', animationStylesForNonScrollingSda.length === 0);
+
+  testRunner.log('\nAnimations with `animation-start-delay` before the delay passes');
+  const animationStartDelayNodeId = nodeTracker.nodes().find(
+    node => DOMHelper.attributes(node).get('id') === 'animation-start-delay').nodeId;
+  const {
+    result: {
+      animationStyles: animationStartDelayAnimationStyles,
+    }
+  } = await dp.CSS.getAnimatedStylesForNode({nodeId: animationStartDelayNodeId});
+  logCondition(
+    'There is no animation', animationStartDelayAnimationStyles.length === 0);
 
   testRunner.completeTest();
   function logCondition(text, condition) {
