55b9bbcd22bcd1efb55f81850d60702b28bc302b
Jun Ishiguro <junis@google.com>
2025-01-16 16:59:41 -0800

    Move chrome/browser/chromeos/quickoffice
    ... to chromeos/ash/components/quickoffice
    
    Bug: b:362650458
    Change-Id: Ic72d1fd0d4d5253e4199940c2d695da40a129bab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174766
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407661}

--- chromeos/ash/components/quickoffice/quickoffice_prefs.cc
+++ chromeos/ash/components/quickoffice/quickoffice_prefs.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/quickoffice/quickoffice_prefs.h"
+#include "chromeos/ash/components/quickoffice/quickoffice_prefs.h"
 
 #include "components/pref_registry/pref_registry_syncable.h"
 
--- chromeos/ash/components/quickoffice/quickoffice_prefs.h
+++ chromeos/ash/components/quickoffice/quickoffice_prefs.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_QUICKOFFICE_QUICKOFFICE_PREFS_H_
-#define CHROME_BROWSER_CHROMEOS_QUICKOFFICE_QUICKOFFICE_PREFS_H_
+#ifndef CHROMEOS_ASH_COMPONENTS_QUICKOFFICE_QUICKOFFICE_PREFS_H_
+#define CHROMEOS_ASH_COMPONENTS_QUICKOFFICE_QUICKOFFICE_PREFS_H_
 
 namespace user_prefs {
 class PrefRegistrySyncable;
@@ -21,4 +21,4 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
 }  // namespace quickoffice
 
-#endif  // CHROME_BROWSER_CHROMEOS_QUICKOFFICE_QUICKOFFICE_PREFS_H_
+#endif  // CHROMEOS_ASH_COMPONENTS_QUICKOFFICE_QUICKOFFICE_PREFS_H_
