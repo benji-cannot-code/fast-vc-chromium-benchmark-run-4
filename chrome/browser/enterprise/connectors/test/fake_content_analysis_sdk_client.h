3751a686d4f2f15f9d7ba428a60e937c93231a39
Evan Stade <estade@google.com>
2025-01-17 06:02:04 -0800

    Mechanical changes to enable forthcoming protobuf roll
    Bug: 328417294
    Change-Id: Ie124c669e7283de79e000d2049af6cfdaa2e59ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6178597
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407864}

--- chrome/browser/enterprise/connectors/test/fake_content_analysis_sdk_client.h
+++ chrome/browser/enterprise/connectors/test/fake_content_analysis_sdk_client.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_TEST_FAKE_CONTENT_ANALYSIS_SDK_CLIENT_H_
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_TEST_FAKE_CONTENT_ANALYSIS_SDK_CLIENT_H_
 
+#include <map>
+
 #include "base/synchronization/lock.h"
 #include "third_party/content_analysis_sdk/src/browser/include/content_analysis/sdk/analysis_client.h"
 
