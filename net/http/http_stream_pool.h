5b5188bcdc998b244cc815551848e4f6b6cde70d
Kenichi Ishibashi <bashi@chromium.org>
2025-01-16 16:19:30 -0800

    HttpStreamPool: Introduce a feature param to control stream attempt delay
    The parameter specifies when to start the stream attempt delay timer.
    This is a follow-up of crrev.com/c/6169787.
    
    Bug: 384759483
    Change-Id: Id5fb0430e1807203c6670b394a9bb6afcaef576b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176622
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407640}

--- net/http/http_stream_pool.h
+++ net/http/http_stream_pool.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "net/base/completion_once_callback.h"
+#include "net/base/features.h"
 #include "net/base/net_export.h"
 #include "net/base/network_anonymization_key.h"
 #include "net/base/network_change_notifier.h"
@@ -50,6 +51,16 @@ class NET_EXPORT_PRIVATE HttpStreamPool
   enum class RespectLimits {
     kRespect,
     kIgnore,
+  };
+
+  // Specify when to start the stream attempt delay timer.
+  enum class StreamAttemptDelayBehavior {
+    // Starts the stream attempt delay timer on the first service endpoint
+    // update.
+    kStartTimerOnFirstEndpointUpdate,
+    // Start the stream attempt delay timer when the first QUIC endpoint is
+    // attempted.
+    kStartTimerOnFirstQuicAttempt,
   };
 
   // Observes events on the HttpStreamPool and may intercept preconnects. Used
@@ -106,9 +117,18 @@ class NET_EXPORT_PRIVATE HttpStreamPool
       "max_stream_per_group";
   static constexpr std::string_view kConnectionAttemptDelayParamName =
       "connection_attempt_delay";
+  static constexpr std::string_view kStreamAttemptDelayBehaviorParamName =
+      "stream_attempt_delay_behavior";
   static constexpr std::string_view kVerboseNetLogParamName = "verbose_netlog";
   static constexpr std::string_view kConsistencyCheckParamName =
       "consistency_check";
+
+  static constexpr inline auto kStreamAttemptDelayBehaviorOptions =
+      std::to_array<base::FeatureParam<StreamAttemptDelayBehavior>::Option>(
+          {{StreamAttemptDelayBehavior::kStartTimerOnFirstEndpointUpdate,
+            "first_endpoint_update"},
+           {StreamAttemptDelayBehavior::kStartTimerOnFirstQuicAttempt,
+            "first_quic_attempt"}});
 
   class NET_EXPORT_PRIVATE Job;
   class NET_EXPORT_PRIVATE JobController;
@@ -117,6 +137,9 @@ class NET_EXPORT_PRIVATE HttpStreamPool
 
   // The time to wait between connection attempts.
   static base::TimeDelta GetConnectionAttemptDelay();
+
+  // Returns when to start the stream attempt delay timer.
+  static StreamAttemptDelayBehavior GetStreamAttemptDelayBehavior();
 
   explicit HttpStreamPool(HttpNetworkSession* http_network_session,
                           bool cleanup_on_ip_address_change = true);
--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -963,6 +963,11 @@ void HttpStreamPool::AttemptManager::ProcessServiceEndpointChanges() {
     CHECK(in_flight_attempts_.empty()) << info;
     return;
   }
+
+  if (GetStreamAttemptDelayBehavior() ==
+      StreamAttemptDelayBehavior::kStartTimerOnFirstEndpointUpdate) {
+    MaybeRunStreamAttemptDelayTimer();
+  }
   MaybeCalculateSSLConfig();
   MaybeAttemptQuic();
   MaybeAttemptConnection();
