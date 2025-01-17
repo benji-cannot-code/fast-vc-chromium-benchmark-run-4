b00fde930d30c9f6587bba282856cd96ab4d74a8
Xiyuan Xia <xiyuan@chromium.org>
2025-01-17 14:44:07 -0800

    Revert "editor: show editor ui on context menu card before consent"
    This reverts commit 5f10f8bf2f29487cb3c4b55272ac8ef0f83b5ca8.
    
    Reason for revert: b/390661006 
    
    Causing `unit_tests` to crash on startup on `linux-chromeos-chrome-preuprev` bot.
    
    Original change's description:
    > editor: show editor ui on context menu card before consent
    >
    > In case the magic boost revamp flag is ON, we stop using the promo card
    > for Editor. Instead, we show the Rewrite/Write UI. To determine between
    > Rewrite or Write UI, we use the TextSelectionMode introduced from the
    > previous CL.
    >
    > Bug: b:388964690
    > Change-Id: I2f6ef11c70291dd365ee83f50059c90c79b9ee60
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171640
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1407842}
    
    Bug: b:388964690
    Bug: b:390661006
    Change-Id: Id2638128f95f24fff49b99cc0c54bdc3d3548478
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6183152
    Auto-Submit: Xiyuan Xia <xiyuan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1408179}

--- chrome/browser/ui/ash/editor_menu/editor_menu_card_context.cc
+++ chrome/browser/ui/ash/editor_menu/editor_menu_card_context.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/editor_menu/editor_menu_card_context.h"
 
-#include "ash/constants/ash_features.h"
 #include "chrome/browser/ui/ash/editor_menu/editor_menu_strings.h"
 #include "chrome/browser/ui/ash/editor_menu/utils/text_and_image_mode.h"
 
@@ -19,38 +18,32 @@ EditorMenuCardContext::EditorMenuCardContext(const EditorMenuCardContext&) =
 EditorMenuCardContext::~EditorMenuCardContext() = default;
 
 TextAndImageMode EditorMenuCardContext::text_and_image_mode() const {
-  // TODO: b:389553095 - With the introduction of EditorTextSelectionMode,
-  // merge kRewrite and kWrite into a single enum value.
-  switch (editor_mode_) {
-    case EditorMode::kRewrite:
-    case EditorMode::kWrite:
-    case EditorMode::kConsentNeeded:
-      if (editor_mode_ == EditorMode::kConsentNeeded &&
-          !ash::features::IsMagicBoostRevampEnabled()) {
-        return TextAndImageMode::kPromoCard;
-      }
-      if (lobster_mode_ == LobsterMode::kBlocked) {
-        return text_selection_mode_ ==
-                       EditorMenuCardTextSelectionMode::kHasSelection
-                   ? TextAndImageMode::kEditorRewriteOnly
-                   : TextAndImageMode::kEditorWriteOnly;
-      }
-      return text_selection_mode_ ==
-                     EditorMenuCardTextSelectionMode::kHasSelection
-                 ? TextAndImageMode::kEditorRewriteAndLobster
-                 : TextAndImageMode::kEditorWriteAndLobster;
-
-    case EditorMode::kSoftBlocked:
-    case EditorMode::kHardBlocked:
-      if (lobster_mode_ == LobsterMode::kBlocked) {
-        return TextAndImageMode::kBlocked;
-      }
-
-      return text_selection_mode_ ==
-                     EditorMenuCardTextSelectionMode::kHasSelection
-                 ? TextAndImageMode::kLobsterWithSelectedText
-                 : TextAndImageMode::kLobsterWithNoSelectedText;
+  if (lobster_mode_ == LobsterMode::kBlocked) {
+    if (editor_mode_ == EditorMode::kRewrite) {
+      return TextAndImageMode::kEditorRewriteOnly;
+    }
+    if (editor_mode_ == EditorMode::kWrite) {
+      return TextAndImageMode::kEditorWriteOnly;
+    }
+    if (editor_mode_ == EditorMode::kConsentNeeded) {
+      return TextAndImageMode::kPromoCard;
+    }
+    return TextAndImageMode::kBlocked;
   }
+
+  if (editor_mode_ == EditorMode::kRewrite) {
+    return TextAndImageMode::kEditorRewriteAndLobster;
+  }
+  if (editor_mode_ == EditorMode::kWrite) {
+    return TextAndImageMode::kEditorWriteAndLobster;
+  }
+  if (editor_mode_ == EditorMode::kConsentNeeded) {
+    return TextAndImageMode::kPromoCard;
+  }
+  if (lobster_mode_ == LobsterMode::kNoSelectedText) {
+    return TextAndImageMode::kLobsterWithNoSelectedText;
+  }
+  return TextAndImageMode::kLobsterWithSelectedText;
 }
 
 bool EditorMenuCardContext::consent_status_settled() const {
