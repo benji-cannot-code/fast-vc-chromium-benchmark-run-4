261984d85d71522345f38284bb41165bab4bb32f
Maggie Chen <magchen@chromium.org>
2025-01-17 11:56:24 -0800

    Improve Interaction-to-Next-Paint for the kVSyncAlignedPresent feature
    With kVSyncAlignedPresent enabled, all frames are delayed and
    committed to CoreAnimation in the next VSync to improve smoothness.
    This CL allows frames that don't handle interactions or animations to
    commit immediately without waiting. The goal is to prevent INP
    (Interaction to Next Paint) regression when a frame doesn't have a
    smoothness issue. There is no benefit in doing VSync aligned
    presentation for those frames.
    
    The implementation of this CL is put behind the feature flag
    kPresentationDelayForInteractiveFrames.
    
    Bug: 330771325
    Change-Id: Ie6876fd85209c92725fbee26cbf39629cbd40e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143459
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408073}

--- ui/gfx/frame_data.h
+++ ui/gfx/frame_data.h
@@ -39,6 +39,10 @@ struct FrameData {
   // fall out of delegated mode.
   gfx::CALayerResult ca_layer_error_code = gfx::kCALayerSuccess;
 #endif
+
+#if BUILDFLAG(IS_MAC)
+  bool is_handling_interaction_or_animation = false;
+#endif
 };
 
 }  // namespace gfx
