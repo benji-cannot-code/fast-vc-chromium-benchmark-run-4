c07c1bb3303dd78d1f6fbe2c9c4d801f6b6cc53c
Emily Stark <estark@google.com>
2025-01-16 23:14:49 -0800

    Add metrics for mixed content on localhost
    Bug: 40062531
    Change-Id: I2f18c413a2a8e75bc6003c3d709cbf1826f041e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176599
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407747}

---
+++ third_party/blink/web_tests/wpt_internal/security/mixed_content/resources/urls.sub.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+LOCALHOST_HTTP_ORIGIN = "http://localhost:{{ports[http][0]}}";
+HTTP_ORIGIN = "http://{{hosts[][www]}}:{{ports[http][0]}}";
+HTTPS_ORIGIN = "https://{{hosts[][www]}}:{{ports[https][0]}}";
