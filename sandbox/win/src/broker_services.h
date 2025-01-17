7f37d57bb7b321424593c0067674c3f51c546ab1
Alex Gough <ajgo@chromium.org>
2025-01-17 12:37:07 -0800

    Add histograms to diagnose a CsrssLockdown issue
    This CL adds histograms to diagnose errors that occur when
    thread creation is proxied by sandbox IPCs. As the sandbox
    code lives in chrome.exe it cannot directly log histograms
    so we add plumbing via the existing BrokerServicesDelegate
    which is always registered to allow these to be logged via
    the correct module.
    
    Bug: 389729365
    Change-Id: I8037e5d8152119ff5e890076e04de5fd694a91e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180965
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408094}

--- sandbox/win/src/broker_services.h
+++ sandbox/win/src/broker_services.h
@@ -75,6 +75,9 @@ class BrokerServicesBase final : public BrokerServices,
   void SetBrokerServicesDelegateForTesting(
       std::unique_ptr<BrokerServicesDelegate> delegate);
 
+  // Gets helper to log histograms from within the exe.
+  BrokerServicesDelegate* GetMetricsDelegate();
+
   static void FreezeTargetConfigForTesting(TargetConfig* config);
 
  private:
