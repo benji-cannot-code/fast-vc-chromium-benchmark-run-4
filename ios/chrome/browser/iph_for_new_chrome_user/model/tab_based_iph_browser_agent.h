e4f89c6c27a33ae872832e2739824f7ac76f298e
Benjamin Williams <bwwilliams@google.com>
2025-01-17 15:44:44 -0800

    [iOS] Observe ReadingListModel changes for Reminder Notifications IPH
    The TabBasedIPHBrowserAgent now implements the ReadingListModelObserver
    interface to listen for relevant reading list changes.
    
    Specifically, when a user manually adds an entry to the reading list,
    the TabBasedIPHBrowserAgent will be able to (eventually) trigger a UI
    command to display the Reminder Notifications Bubble IPH.
    
    This CL extends the groundwork for the Reminder Notifications feature by
    enabling the TabBasedIPHBrowserAgent to listen to the necessary reading
    list events.
    
    This addresses another part of the requirements for enabling the
    Reminder Notifications feature on iOS.
    
    Fixed: 390475544
    Change-Id: I547082efbfafeba6702522664afe5012f499844b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180083
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408214}

--- ios/chrome/browser/iph_for_new_chrome_user/model/tab_based_iph_browser_agent.h
+++ ios/chrome/browser/iph_for_new_chrome_user/model/tab_based_iph_browser_agent.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #import "base/scoped_observation.h"
 #import "components/bookmarks/browser/base_bookmark_model_observer.h"
+#import "components/reading_list/core/reading_list_model_observer.h"
 #import "ios/chrome/browser/shared/model/browser/browser_observer.h"
 #import "ios/chrome/browser/shared/model/browser/browser_user_data.h"
 #import "ios/chrome/browser/shared/model/web_state_list/active_web_state_observation_forwarder.h"
@@ -19,9 +20,14 @@ class BookmarkModel;
 class BookmarkNode;
 }  // namespace bookmarks
 
+namespace reading_list {
+enum EntrySource;
+}  // namespace reading_list
+
 class Browser;
 @class CommandDispatcher;
 @protocol HelpCommands;
+class ReadingListModel;
 class UrlLoadingNotifierBrowserAgent;
 class WebStateList;
 
@@ -34,6 +40,7 @@ class Tracker;
 class TabBasedIPHBrowserAgent : public bookmarks::BaseBookmarkModelObserver,
                                 public BrowserUserData<TabBasedIPHBrowserAgent>,
                                 public BrowserObserver,
+                                public ReadingListModelObserver,
                                 public UrlLoadingObserver,
                                 public web::WebStateObserver {
  public:
@@ -82,6 +89,13 @@ class TabBasedIPHBrowserAgent : public bookmarks::BaseBookmarkModelObserver,
   // BrowserObserver
   void BrowserDestroyed(Browser* browser) override;
 
+  // ReadingListModelObserver
+  void ReadingListModelLoaded(const ReadingListModel* model) override;
+  void ReadingListModelBeingShutdown(const ReadingListModel* model) override;
+  void ReadingListDidAddEntry(const ReadingListModel* model,
+                              const GURL& url,
+                              reading_list::EntrySource source) override;
+
   // UrlLoadingObserver
   void TabDidLoadUrl(const GURL& url,
                      ui::PageTransition transition_type) override;
@@ -110,6 +124,12 @@ class TabBasedIPHBrowserAgent : public bookmarks::BaseBookmarkModelObserver,
   // longer react to bookmark modifications.
   void StopObservingBookmarkModel();
 
+  // Stops observing changes to the reading list model. This is typically
+  // called when `TabBasedIPHBrowserAgent` is being destroyed, when reactions
+  // to reading list modifications are no longer needed, or when the reading
+  // list model itself is being destroyed.
+  void StopObservingReadingListModel();
+
   // For all IPH features managed by this class, resets their tracker variables
   // to `false`, and remove currently displaying IPH views from the view.
   void ResetFeatureStatesAndRemoveIPHViews();
@@ -129,6 +149,16 @@ class TabBasedIPHBrowserAgent : public bookmarks::BaseBookmarkModelObserver,
   base::ScopedObservation<bookmarks::BookmarkModel,
                           bookmarks::BaseBookmarkModelObserver>
       bookmark_model_observation_{this};
+  // `ReadingListModel` instance providing access to the reading list. May be
+  // `nullptr`.
+  raw_ptr<ReadingListModel> reading_list_model_ = nullptr;
+  // Tracks whether the reading list model has finished loading. Until the model
+  // is fully loaded, it is unsafe to use or interact with it.
+  bool reading_list_model_loaded_ = false;
+  // Automatically removes this observer from the reading list model when
+  // destroyed.
+  base::ScopedObservation<ReadingListModel, ReadingListModelObserver>
+      reading_list_model_observation_{this};
   // Observer for URL loading.
   raw_ptr<UrlLoadingNotifierBrowserAgent> url_loading_notifier_;
   // Command dispatcher for the browser; used to retrieve help handler.
