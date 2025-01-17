e96eecb8572b8478c3d16a6fc477e73c0bc61cc7
Colin Blundell <blundell@chromium.org>
2025-01-17 01:44:23 -0800

    [Blink] Rename RecycledBitmapCount()
    Updates to be consistent with other renames in this code.
    
    Bug: 40064122
    Change-Id: If0a4458e5550729c2d099f0a965e0df13d66111a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177738
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407791}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test_helpers.h
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test_helpers.h
@@ -487,7 +487,9 @@ class DrawingBufferForTests : public DrawingBuffer {
 
   raw_ptr<bool> live_;
 
-  int RecycledBitmapCount() { return recycled_software_resources_.size(); }
+  int RecycledSoftwareResourceCount() {
+    return recycled_software_resources_.size();
+  }
 };
 
 }  // blink
