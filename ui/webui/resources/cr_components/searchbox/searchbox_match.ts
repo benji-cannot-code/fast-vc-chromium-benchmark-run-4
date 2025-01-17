febe32fcce788dfc9fdbe6e850fece60aa995d1e
dpapad <dpapad@chromium.org>
2025-01-17 07:59:54 -0800

    WebUI: Cleanup Polymer properties in cr_components/searchbox.
     - Declare Polymer properties as class members, where such declarations
       were missing.
     - Delete a few usused Polymer propreties.
    
    Bug: 389737066
    Change-Id: I07e9bc37ab7f6f9604a400538dda627842553307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181914
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1407928}

--- ui/webui/resources/cr_components/searchbox/searchbox_match.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox_match.ts
@@ -124,11 +124,6 @@ export class SearchboxMatchElement extends PolymerElement {
         value: -1,
       },
 
-      renderType: {
-        type: String,
-        reflectToAttribute: true,
-      },
-
       showThumbnail: {
         type: Boolean,
         reflectToAttribute: true,
@@ -186,11 +181,13 @@ export class SearchboxMatchElement extends PolymerElement {
   override ariaLabel: string;
   hasAction: boolean;
   hasImage: boolean;
+  isEntitySuggestion: boolean;
+  isRichSuggestion: boolean;
   match: AutocompleteMatch;
   matchIndex: number;
-  searchboxConsistentRowHeight: boolean;
   sideType: SideType;
-  private actionIsVisible_: boolean;
+  showThumbnail: boolean;
+  private isLensSearchbox_: boolean;
   private contentsHtml_: TrustedHTML;
   private descriptionHtml_: TrustedHTML;
   private removeButtonAriaLabel_: string;
