a62ac07f6d21f201ceb28a5ac160a15eaeed022b
Peter Kvitek <kvitekp@chromium.org>
2025-01-16 17:56:46 -0800

    [headless] Change string window state representation to enum class
    This CL replaces string window state representation with more
    appropriate enum class. There are no changes in behavior expected.
    
    Change-Id: Ib46c61dbe084b927c1c03b5a268169193464c9d8
    Cq-Include-Trybots: luci.chromium.try:mac12-tests,mac13-tests,mac14-tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175359
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407683}

--- headless/lib/browser/headless_web_contents_impl.h
+++ headless/lib/browser/headless_web_contents_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_window_tree_host.h"
 #include "headless/public/headless_export.h"
 #include "headless/public/headless_web_contents.h"
+#include "headless/public/headless_window_state.h"
 
 class SkBitmap;
 
@@ -66,10 +67,10 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents {
     return window_tree_host_.get();
   }
   int window_id() const { return window_id_; }
-  const std::string& window_state() const { return window_state_; }
+  HeadlessWindowState window_state() const { return window_state_; }
 
   // Set the WebContent's platform window state and visibility.
-  void SetWindowState(const std::string& state);
+  void SetWindowState(HeadlessWindowState window_state);
 
   // Set bounds of WebContent's platform window.
   void SetBounds(const gfx::Rect& bounds);
@@ -94,7 +95,7 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents {
       std::unique_ptr<content::WebContents> web_contents);
 
   void InitializeWindow(const gfx::Rect& bounds,
-                        const std::string& window_state);
+                        HeadlessWindowState window_state);
 
   uint64_t begin_frame_sequence_number_ =
       viz::BeginFrameArgs::kStartingFrameNumber;
@@ -104,7 +105,7 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents {
   std::unique_ptr<Delegate> web_contents_delegate_;
   std::unique_ptr<HeadlessWindowTreeHost> window_tree_host_;
   int window_id_ = 0;
-  std::string window_state_;
+  HeadlessWindowState window_state_ = HeadlessWindowState::kNormal;
   std::unique_ptr<content::WebContents> const web_contents_;
 
   class PendingFrame;
