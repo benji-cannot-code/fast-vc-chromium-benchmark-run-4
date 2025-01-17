6facb53be6d19a2a30d8effcab99dee6e1f26803
Ioana Pandele <ioanap@chromium.org>
2025-01-17 01:36:55 -0800

    Revert "[Ads CCT Notice] Launch the treatment surveys"
    This reverts commit ce5d535a3b67a6044783331b2f501fac78b76a8b.
    
    Reason for revert: This seems to have caused PrivacySandboxSurveyControllerIntegrationTest to be very flaky. The test has then caused multiple consecutive build failures on 
    automotive.
    
    Original change's description:
    > [Ads CCT Notice] Launch the treatment surveys
    >
    > Bug: 379930582
    > Change-Id: I9dca678353500d7760b77a8f837f48c944f41830
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164105
    > Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Abe Boujane <boujane@google.com>
    > Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    > Commit-Queue: Alvin Lam <alvingo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1407507}
    
    Bug: 379930582, 390561083
    Change-Id: I298c0f6aea0fc09751e24dab3f9ccac33269be46
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179206
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407787}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
@@ -33,7 +33,6 @@ public class PrivacySandboxDialogController {
     private static boolean sDisableAnimations;
     private static boolean sDisableEEANoticeForTesting;
     private static LoadUrlParams sThinWebViewLoadUrlParams;
-    private static Runnable sOnDialogDismissedRunnable;
 
     public static boolean shouldShowPrivacySandboxDialog(Profile profile, int surfaceType) {
         assert profile != null;
@@ -125,9 +124,6 @@ public class PrivacySandboxDialogController {
                                 surfaceType,
                                 profile,
                                 activityWindowAndroid);
-                if (sOnDialogDismissedRunnable != null) {
-                    dialog.setOnDismissListener(d -> sOnDialogDismissedRunnable.run());
-                }
                 dialog.show();
                 sDialog = new WeakReference<>(dialog);
                 return true;
@@ -168,16 +164,9 @@ public class PrivacySandboxDialogController {
                         new PrivacySandboxDialogNoticeEEA(
                                 context, privacySandboxBridge, surfaceType);
             }
-            if (sOnDialogDismissedRunnable != null) {
-                dialog.setOnDismissListener(d -> sOnDialogDismissedRunnable.run());
-            }
             dialog.show();
             sDialog = new WeakReference<>(dialog);
         }
-    }
-
-    public static void setOnDialogDismissRunnable(Runnable runnable) {
-        sOnDialogDismissedRunnable = runnable;
     }
 
     static Dialog getDialogForTesting() {
