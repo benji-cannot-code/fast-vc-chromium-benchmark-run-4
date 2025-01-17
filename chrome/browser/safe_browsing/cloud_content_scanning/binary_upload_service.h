b0fc2e940d1c5614c94007fd7f664f5fa8d07cb4
David Sanders <dsanders11@ucsbalum.com>
2025-01-17 13:24:49 -0800

    Remove unused include in binary_upload_service.h
    Bug: 40318405
    Change-Id: Ie473f7926f08c5c0f60e933d039843f4470b4ac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181489
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408119}

--- chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SAFE_BROWSING_CLOUD_CONTENT_SCANNING_BINARY_UPLOAD_SERVICE_H_
 
 #include "base/functional/bind.h"
+#include "base/functional/callback.h"
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/weak_ptr.h"
 #include "base/types/id_type.h"
@@ -14,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/common/proto/connectors.pb.h"
 #include "components/enterprise/connectors/core/analysis_settings.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "services/network/public/cpp/resource_request.h"
 #include "url/gurl.h"
 
 class Profile;
