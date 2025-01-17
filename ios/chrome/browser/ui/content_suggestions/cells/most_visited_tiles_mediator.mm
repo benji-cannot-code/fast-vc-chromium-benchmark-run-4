5a252de2b4754a5e0439627d203933ac124f2dcd
ginnnnnnny <ginnyhuang@chromium.org>
2025-01-17 14:56:34 -0800

    [iOS][feed-positioning] Support long press on MVT
    This CL adds long press actions for the most visited tiles module when
    it resides in the magic stack. Also adds strings to support the context
    menu.
    
    Bug: 390262388,390261118
    Change-Id: I99c16a1ad55e13ff43422c1ea6ab46603747c427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179664
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408185}

--- ios/chrome/browser/ui/content_suggestions/cells/most_visited_tiles_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/most_visited_tiles_mediator.mm
@@ -147,6 +147,10 @@ const CGFloat kMagicStackMostVisitedFaviconMinimalSize = 18;
   _mostVisitedSites->Refresh();
 }
 
+- (void)disableModule {
+  _prefService->SetBoolean(prefs::kHomeCustomizationMostVisitedEnabled, false);
+}
+
 - (MostVisitedTilesConfig*)mostVisitedTilesConfig {
   return _mostVisitedConfig;
 }
--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -855,6 +855,9 @@ using segmentation_platform::TipIdentifier;
 
 - (void)neverShowModuleType:(ContentSuggestionsModuleType)type {
   switch (type) {
+    case ContentSuggestionsModuleType::kMostVisited:
+      [_mostVisitedTilesMediator disableModule];
+      break;
     case ContentSuggestionsModuleType::kTabResumption:
       [_tabResumptionMediator disableModule];
       break;
