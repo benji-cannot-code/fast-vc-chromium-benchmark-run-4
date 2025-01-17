6965e7873c8031a9cac7862814890729a8bcad24
Brando Socarras <brandosocarras@google.com>
2025-01-17 15:23:23 -0800

    [Quick Share v2] Set Your devices viz on devics in Selected contacts viz
    In Quick Share v2, the 'Selected contacts' visibility is deprecated.
    Devices with 'Selected contacts' enabled will be switched to 'Your
    devices' visibility.
    
    Tested: verified selected contacts -> your devices via DUT. utest in this.
    Bug: 323206955
    Change-Id: I9027c10170164d63336d62122fddf9a52c376b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174607
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408203}

--- chrome/browser/nearby_sharing/nearby_share_settings_unittest.cc
+++ chrome/browser/nearby_sharing/nearby_share_settings_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/nearby_sharing/common/nearby_share_features.h"
 #include "chrome/browser/nearby_sharing/common/nearby_share_prefs.h"
 #include "chrome/browser/nearby_sharing/local_device_data/fake_nearby_share_local_device_data_manager.h"
@@ -117,6 +118,7 @@ class NearbyShareSettingsTest : public ::testing::Test {
   FakeNearbyShareSettingsObserver observer_;
   std::unique_ptr<NearbyShareSettings> nearby_share_settings_;
   std::unique_ptr<NearbyShareSettingsAsyncWaiter> nearby_share_settings_waiter_;
+  base::test::ScopedFeatureList feature_list_;
 };
 
 TEST_F(NearbyShareSettingsTest, GetAndSetEnabled) {
@@ -343,4 +345,19 @@ TEST_F(NearbyShareSettingsTest, GetAndSetAllowedContacts) {
 
   settings_waiter()->GetAllowedContacts(&allowed_contacts);
   EXPECT_EQ(0u, allowed_contacts.size());
+}
+
+TEST_F(NearbyShareSettingsTest, QuickShareV2_SomeContacts_ToYourDevices) {
+  feature_list_.InitAndEnableFeature(chromeos::features::kQuickShareV2);
+  pref_service_.SetInteger(
+      prefs::kNearbySharingBackgroundVisibilityName,
+      static_cast<int>(nearby_share::mojom::Visibility::kSelectedContacts));
+
+  // Since some contacts -> your devices occurs in constructor, new
+  // NearbyShareSettings object must be created after QuickShareV2 is enabled.
+  NearbyShareSettings nearby_share_settings(&pref_service_,
+                                            &local_device_data_manager_);
+
+  EXPECT_EQ(nearby_share_settings.GetVisibility(),
+            nearby_share::mojom::Visibility::kYourDevices);
 }
