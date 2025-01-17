c9cd18eb1f0c467d3cb3867949e69092b748a343
Jingping Sun <jings@microsoft.com>
2025-01-17 10:47:20 -0800

    Use RemoveEntryById to erase the entry when restore recent closed tabs
    This CL will use the method RemoveEntryById to remove the entry from
    entries list of tab restore. Because the iterator may be invaild after
    the time-consuming operation.
    
    Change-Id: I70fc581f41fea1a9f88d496b92d367458b77f865
    Bug: 390075226
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176858
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408035}

--- components/sessions/core/tab_restore_service_helper.cc
+++ components/sessions/core/tab_restore_service_helper.cc
@@ -693,7 +693,10 @@ std::vector<LiveTab*> TabRestoreServiceHelper::RestoreEntryById(
 
         if (window.tabs.empty()) {
           // Remove the entry if there is nothing left to restore.
-          entries_.erase(entry_iterator);
+          // The entries_ may by changed after the tabs restored and the
+          // entry_iterator may be no longer valid. So call RemoveEntryById here
+          // instead of entries_.erase(entry_iterator).
+          RemoveEntryById(id);
         }
       }
 
@@ -746,7 +749,10 @@ std::vector<LiveTab*> TabRestoreServiceHelper::RestoreEntryById(
             CHECK(ValidateGroup(group));
             group.tabs.erase(group.tabs.begin() + i);
             if (group.tabs.empty()) {
-              entries_.erase(entry_iterator);
+              // The entries_ may by changed after the tabs restored and the
+              // entry_iterator may be no longer valid. So call RemoveEntryById
+              // here instead of entries_.erase(entry_iterator).
+              RemoveEntryById(id);
             }
 
             break;
@@ -760,7 +766,10 @@ std::vector<LiveTab*> TabRestoreServiceHelper::RestoreEntryById(
   }
 
   if (entry_id_matches_restore_id) {
-    entries_.erase(entry_iterator);
+    // The entries_ may by changed after the tabs restored and the
+    // entry_iterator may be no longer valid. So call RemoveEntryById here
+    // instead of entries_.erase(entry_iterator).
+    RemoveEntryById(id);
   }
 
   restoring_ = false;
