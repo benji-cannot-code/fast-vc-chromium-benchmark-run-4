4d02a0c63745545b6e3c6fdc9a2b7fb9a3632fa1
David Roger <droger@chromium.org>
2025-01-17 09:17:19 -0800

    [Enterprise] Management header clickable with HoverButton
    The target is:
    - chrome://management for managed accounts
    - https://familylink.google.com/setting/resource/94 for kids
    
    This CL overrides the layout manager of the HoverButton so that
    it looks centered.
    
    Screenshot: https://screenshot.googleplex.com/7RYzUSwMPmKKGPr
    Recording: http://go/scrcast/NTc2MDI0NjE4NTE5NzU2OHwzNDg0NjVkNi1lMw
    
    This CL also fixes a bug where the subtitle had the wrong color.
    
    Change-Id: I1a1a85641a145908ce5702a1538e11f4df32ecdf
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169925
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407981}

--- chrome/browser/ui/views/profiles/profile_menu_view.h
+++ chrome/browser/ui/views/profiles/profile_menu_view.h
@@ -62,6 +62,7 @@ class ProfileMenuView : public ProfileMenuViewBase {
   std::u16string GetAccessibleWindowTitle() const override;
 
   // Button/link actions.
+  void OnProfileManagementButtonClicked();
   void OnManageGoogleAccountButtonClicked();
   void OnPasswordsButtonClicked();
   void OnCreditCardsButtonClicked();
