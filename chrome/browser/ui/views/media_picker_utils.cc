28d4562996ff8e05fccb8885004aafbcc7034890
Keren Zhu <kerenzhu@chromium.org>
2025-01-17 22:02:03 -0800

    Remove modal type for parentless media selector dialog
    The media selector dialog used window-modal type without a parent. This
    causes a native titlebar to show which covers the content area.
    
    Fixed: 385704985
    Change-Id: I4286695b9df8a3e254f332047df4c7f6b75c6010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177123
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408296}

--- chrome/browser/ui/views/media_picker_utils.cc
+++ chrome/browser/ui/views/media_picker_utils.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/constrained_window/constrained_window_views.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/views/widget/widget.h"
-#include "ui/views/widget/widget_delegate.h"
 #include "ui/views/window/dialog_delegate.h"
 
 bool IsMediaPickerModalWindow(content::WebContents* web_contents) {
@@ -21,7 +20,7 @@ bool IsMediaPickerModalWindow(content::WebContents* web_contents) {
 
 views::Widget* CreateMediaPickerDialogWidget(Browser* browser,
                                              content::WebContents* web_contents,
-                                             views::WidgetDelegate* delegate,
+                                             views::DialogDelegate* delegate,
                                              gfx::NativeWindow context,
                                              gfx::NativeView parent) {
   // If |web_contents| is not a background page then the picker will be shown
@@ -37,11 +36,12 @@ views::Widget* CreateMediaPickerDialogWidget(Browser* browser,
     widget =
         constrained_window::ShowWebModalDialogViews(delegate, web_contents);
   } else {
-#if BUILDFLAG(IS_MAC)
-    // On Mac, ModalType::kChild with a null parent isn't allowed - fall back to
-    // ModalType::kWindow.
-    delegate->SetModalType(ui::mojom::ModalType::kWindow);
-#endif
+    // This becomes a top-level window if it does not have a parent.
+    // Top-level windows should usually be draggable.
+    if (!parent) {
+      delegate->set_draggable(true);
+      delegate->SetModalType(ui::mojom::ModalType::kNone);
+    }
     widget =
         views::DialogDelegate::CreateDialogWidget(delegate, context, parent);
     widget->Show();
