9820a47798db4390439d3ea624fe756d7ac54d21
Sirisha Kavuluru <skavuluru@google.com>
2025-01-17 18:24:32 -0800

    [TabStripRefactor] Refactor mergeToTabGroupOnDrop to delegate
    Bug: 381285152
    Change-Id: I5493ccd3c1a4dffafeacd359be57a446c03cf4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180199
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408269}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabDragSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabDragSource.java
@@ -364,7 +364,7 @@ public class TabDragSource implements View.OnDragListener {
         }
         mStripLayoutHelperSupplier
                 .get()
-                .prepareForTabDrop(xPx * mPxToDp, mLastXDp, isDragSource, isDraggedTabIncognito());
+                .handleDragEnter(xPx * mPxToDp, mLastXDp, isDragSource, isDraggedTabIncognito());
         return true;
     }
 
@@ -373,7 +373,7 @@ public class TabDragSource implements View.OnDragListener {
         float yDp = yPx * mPxToDp;
         mStripLayoutHelperSupplier
                 .get()
-                .dragForTabDrop(
+                .handleDragWithin(
                         LayoutManagerImpl.time(),
                         xDp,
                         yDp,
@@ -384,9 +384,7 @@ public class TabDragSource implements View.OnDragListener {
 
     private boolean onDrop(DragEvent dropEvent) {
         StripLayoutHelper helper = mStripLayoutHelperSupplier.get();
-        int destinationTabId = helper.getTabDropId();
         helper.stopReorderMode();
-
         if (isDragSource()) {
             DragDropMetricUtils.recordTabReorderStripWithDragDrop(mUmaState.mDragEverLeftStrip);
             return true;
@@ -414,10 +412,10 @@ public class TabDragSource implements View.OnDragListener {
                     mTabModelSelector.getModel(tabBeingDragged.isIncognito()).getCount());
             showDroppedDifferentModelToast(mWindowAndroid.getContext().get());
         } else {
+            // Reparent tab at drop index and merge to group on destination if needed.
             int tabIndex = helper.getTabIndexForTabDrop(dropEvent.getX() * mPxToDp);
             mMultiInstanceManager.moveTabToWindow(getActivity(), tabBeingDragged, tabIndex);
-            helper.mergeToGroupForTabDropIfNeeded(
-                    destinationTabId, tabBeingDragged.getId(), tabIndex);
+            helper.maybeMergeToGroupOnDrop(tabBeingDragged.getId(), tabIndex);
         }
         DragDropMetricUtils.recordTabDragDropType(
                 DragDropType.TAB_STRIP_TO_TAB_STRIP,
@@ -525,7 +523,7 @@ public class TabDragSource implements View.OnDragListener {
                 mShadowView.expand();
             }
         }
-        mStripLayoutHelperSupplier.get().clearForTabDrop(isDragSource(), isDraggedTabIncognito());
+        mStripLayoutHelperSupplier.get().handleDragExit(isDragSource(), isDraggedTabIncognito());
         return true;
     }
 
