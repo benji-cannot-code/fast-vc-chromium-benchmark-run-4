80d63dbc4b4320d654d2049d62531c7bbd0cc2ac
elabadysayed <elabadysayed@chromium.org>
2025-01-20 10:29:32 -0800

    Enable PostGetMyMemoryState CachedFlag
    The feature is now enabled by default but we missed enabling the
    CachedFlag. This CL change the default value of the CachedFlag to true
    to align with the feature.
    
    Bug: 380113169
    Change-Id: Ic4e81a49b4c39459c3133fd22c452983ffcd333d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168863
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Sayed Elabady <elabadysayed@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408738}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
@@ -53,7 +53,6 @@ public class ChromeCachedFlagsTest {
     private static final Set<CachedFlag> BASELINE =
             Set.of(
                     ChromeFeatureList.sDownloadsMigrateToJobsAPI,
-                    ChromeFeatureList.sPostGetMyMemoryStateToBackground,
                     ChromeFeatureList.sPrefetchBrowserInitiatedTriggers,
                     ChromeFeatureList.sSafetyHubMagicStack);
 
