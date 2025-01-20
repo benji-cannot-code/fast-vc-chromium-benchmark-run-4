698c0f3d8ff819e6ea719c05e8d5cb6a640a7c00
David Sanders <dsanders11@ucsbalum.com>
2025-01-20 00:34:54 -0800

    Remove unused include in //ui/gfx/color_space.h
    Bug: 40318405
    Change-Id: Iefa9ef0ac1b70cbde9f127f8829fba3adc688091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6184818
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408534}

--- gpu/command_buffer/service/shared_image/dawn_fallback_image_representation.cc
+++ gpu/command_buffer/service/shared_image/dawn_fallback_image_representation.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h"
 #include "gpu/config/gpu_finch_features.h"
+#include "third_party/skia/include/core/SkColorSpace.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 
 namespace gpu {
