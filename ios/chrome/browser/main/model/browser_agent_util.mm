3fa96f63cf8446b6c635a84b94a3c9d485a6d7a5
Sylvain Defresne <sdefresne@chromium.org>
2025-01-20 03:43:26 -0800

    [ios] Remove TabParentingGlobalObserver and helper classes
    Fixed: 384034042
    Change-Id: I56a2b132d9b6aae8bc1bd28b013c624cf1076f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177761
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408590}

--- ios/chrome/browser/main/model/browser_agent_util.mm
+++ ios/chrome/browser/main/model/browser_agent_util.mm
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/tab_insertion/model/tab_insertion_browser_agent.h"
 #import "ios/chrome/browser/tabs/model/closing_web_state_observer_browser_agent.h"
 #import "ios/chrome/browser/tabs/model/synced_window_delegate_browser_agent.h"
-#import "ios/chrome/browser/tabs/model/tab_parenting_browser_agent.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_browser_agent.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_notifier_browser_agent.h"
 #import "ios/chrome/browser/view_source/model/view_source_browser_agent.h"
@@ -70,7 +69,6 @@ void AttachBrowserAgents(Browser* browser) {
   // LensBrowserAgent must be created before WebNavigationBrowserAgent.
   LensBrowserAgent::CreateForBrowser(browser);
   WebNavigationBrowserAgent::CreateForBrowser(browser);
-  TabParentingBrowserAgent::CreateForBrowser(browser);
 
   if (!browser_is_off_record) {
     ClosingWebStateObserverBrowserAgent::CreateForBrowser(browser);
