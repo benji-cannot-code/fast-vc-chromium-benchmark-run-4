7e2f6ebe2bd6ae8d231334e40745c5ce6f543feb
Nicola Tommasi <tommasin@chromium.org>
2025-01-20 07:34:10 -0800

    [CodeHealth] Clean up dep from content_settings to page_info
    It was added for async canned page_info, but the async canned helper
    code has been deleted. Removing the dependency as it's not needed
    anymore.
    
    Bug: 40251822
    Change-Id: I6ff1188cce33f7648606b34496cded4494247e66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181817
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408673}

--- components/content_settings/browser/page_specific_content_settings.cc
+++ components/content_settings/browser/page_specific_content_settings.cc
@@ -51,10 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "url/origin.h"
 
-#if !BUILDFLAG(IS_ANDROID)
-#include "components/page_info/core/features.h"
-#endif
-
 using content::BrowserThread;
 using StorageType =
     content_settings::mojom::ContentSettingsManager::StorageType;
