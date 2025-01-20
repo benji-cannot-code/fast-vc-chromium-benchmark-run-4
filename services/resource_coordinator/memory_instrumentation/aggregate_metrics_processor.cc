4aab9aaf0e66d0a28581aaddc4c9ea89fe18a926
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2025-01-20 03:19:02 -0800

    Reduce allocation spam in native code resident size calculation.
    This code is inserting ~10k elements into a set, which is a lot of
    malloc calls due to the use of std::set.
    
    This can be done efficiently instead using a std::vector since the
    elements are inserted in ascending order.
    
    Bug: 390083086
    Test: dumping native-library-resident-pages.txt works
    Change-Id: Icc343ce4de6d1cba93dff1bec27c57dd2c349c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174708
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408581}

--- services/resource_coordinator/memory_instrumentation/aggregate_metrics_processor.cc
+++ services/resource_coordinator/memory_instrumentation/aggregate_metrics_processor.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/resource_coordinator/memory_instrumentation/aggregate_metrics_processor.h"
 
-#include <set>
 #include <string>
 #include <vector>
 
@@ -26,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-void LogNativeCodeResidentPages(const std::set<size_t>& accessed_pages_set) {
+void LogNativeCodeResidentPages(base::span<size_t> accessed_pages_set) {
   // |SUPPORTS_CODE_ORDERING| can only be enabled on Android.
   const auto kResidentPagesPath = base::FilePath(
       "/data/local/tmp/chrome/native-library-resident-pages.txt");
@@ -74,12 +73,13 @@ mojom::AggregatedMetricsPtr ComputeGlobalNativeCodeResidentMemoryKb(
     }
   }
 
-  // |accessed_pages_set| will be ~40kB on 32 bit mode and ~80kB on 64 bit mode.
-  std::set<size_t> accessed_pages_set;
+  std::vector<size_t> accessed_pages_set;
+  // The typical size of this set is ~10k entries.
+  accessed_pages_set.reserve(10240);
   for (size_t i = 0; i < common_map.size(); i++) {
     for (int j = 0; j < 8; j++) {
       if (common_map[i] & (1 << j))
-        accessed_pages_set.insert(i * 8 + j);
+        accessed_pages_set.push_back(i * 8 + j);
     }
   }
 
