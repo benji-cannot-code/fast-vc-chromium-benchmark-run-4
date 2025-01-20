bbc5496a4dc594c85bf3c571a94b8234d47bf4e8
Cheick Cisse <cheickcisse@google.com>
2025-01-20 11:06:01 -0800

    [iOS][DefaultBrowser] Add feature flag for non-modal promo migration
    This CL adds a feature flag for the non-modal promo migration
    
    Change-Id: Ida657e95c60c6f00b96a264ab5e79d6348cd541e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6164767
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408745}

---
+++ ios/chrome/browser/default_browser/model/features.mm
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/default_browser/model/features.h"
+
+BASE_FEATURE(kNonModalPromoMigration,
+             "NonModalPromoMigration",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+bool IsNonModalPromoMigrationEnabled() {
+  return base::FeatureList::IsEnabled(kNonModalPromoMigration);
+}
