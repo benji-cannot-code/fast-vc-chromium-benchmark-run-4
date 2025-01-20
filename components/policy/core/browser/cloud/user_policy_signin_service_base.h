44a45ffbe52464b7b3950ddebac968bc60591081
Artem Sumaneev <asumaneev@google.com>
2025-01-20 03:09:04 -0800

    [policy] Migrate UserPolicySigninServiceBase to use scoped observations
    This is a tentative fix for the crash when the CloudPolicyService is
    being observed upon destruction.
    
    Scoped observation will ensure that the observer is removed when needed.
    
    Bug: b:40915114
    Change-Id: I12ed56752ce4ed3b2b161f2118782cd391837a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172385
    Auto-Submit: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408578}

--- components/policy/core/browser/cloud/user_policy_signin_service_base.h
+++ components/policy/core/browser/cloud/user_policy_signin_service_base.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
@@ -264,6 +265,12 @@ class POLICY_EXPORT UserPolicySigninServiceBase
   // Callback to start the delayed registration. Cancelled when the service is
   // shut down.
   base::CancelableOnceCallback<void()> registration_callback_;
+
+  base::ScopedObservation<CloudPolicyClient, CloudPolicyClient::Observer>
+      cloud_policy_client_observation_{this};
+
+  base::ScopedObservation<CloudPolicyService, CloudPolicyService::Observer>
+      cloud_policy_service_observation_{this};
 
   base::WeakPtrFactory<UserPolicySigninServiceBase> weak_factory_{this};
 
