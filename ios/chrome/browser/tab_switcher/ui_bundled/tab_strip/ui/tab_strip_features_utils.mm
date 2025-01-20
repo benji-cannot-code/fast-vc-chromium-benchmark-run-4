255192b2708ecf4923f0ca7d1b2e958d74ef29d3
Gauthier Ambard <gambard@chromium.org>
2025-01-20 07:23:34 -0800

    [iOS] Enable ModernTabStrip by default
    Bug: None
    Change-Id: I26c3d52a50f47e40cdf648bffaadbf0e76250504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181894
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408667}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/ui/tab_strip_features_utils.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/ui/tab_strip_features_utils.mm
@@ -12,10 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation TabStripFeaturesUtils
 
 + (BOOL)isModernTabStripNewTabButtonDynamic {
-  CHECK(IsModernTabStripOrRaccoonEnabled());
-  std::string feature_param = base::GetFieldTrialParamValueByFeature(
-      kModernTabStrip, kModernTabStripParameterName);
-  return feature_param != kModernTabStripNTBStaticParam;
+  return YES;
 }
 
 + (BOOL)isModernTabStripWithTabGroups {
@@ -23,53 +20,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 + (BOOL)hasCloserNTB {
-  return GetFieldTrialParamByFeatureAsBool(kModernTabStrip,
-                                           kModernTabStripCloserNTB, false);
+  return YES;
 }
 
 + (BOOL)hasDarkerBackground {
-  return GetFieldTrialParamByFeatureAsBool(
-      kModernTabStrip, kModernTabStripDarkerBackground, false);
+  return NO;
 }
 
 + (BOOL)hasDarkerBackgroundV3 {
-  return GetFieldTrialParamByFeatureAsBool(
-      kModernTabStrip, kModernTabStripDarkerBackgroundV3, false);
+  return YES;
 }
 
 + (BOOL)hasNoNTBBackground {
-  return GetFieldTrialParamByFeatureAsBool(
-      kModernTabStrip, kModernTabStripNTBNoBackground, false);
+  return NO;
 }
 
 + (BOOL)hasBlackBackground {
-  return GetFieldTrialParamByFeatureAsBool(
-      kModernTabStrip, kModernTabStripBlackBackground, false);
+  return NO;
 }
 
 + (BOOL)hasBiggerNTB {
-  return GetFieldTrialParamByFeatureAsBool(kModernTabStrip,
-                                           kModernTabStripBiggerNTB, false);
+  return YES;
 }
 
 + (BOOL)hasCloseButtonsVisible {
-  return GetFieldTrialParamByFeatureAsBool(
-      kModernTabStrip, kModernTabStripCloseButtonsVisible, false);
+  return NO;
 }
 
 + (BOOL)hasHighContrastInactiveTabs {
-  return GetFieldTrialParamByFeatureAsBool(
-      kModernTabStrip, kModernTabStripInactiveTabsHighContrast, false);
+  return YES;
 }
 
 + (BOOL)hasHighContrastNTB {
-  return GetFieldTrialParamByFeatureAsBool(
-      kModernTabStrip, kModernTabStripHighContrastNTB, false);
+  return NO;
 }
 
 + (BOOL)hasDetachedTabs {
-  return GetFieldTrialParamByFeatureAsBool(kModernTabStrip,
-                                           kModernTabStripDetachedTabs, false);
+  return NO;
 }
 
 @end
