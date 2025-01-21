a621261ad4b63113997002b256b4cb7addd567cf
Koji Ishii <kojii@chromium.org>
2025-01-20 19:56:22 -0800

    [shape-cache] Stop including `hb.h` in `font_features.h`
    To improve the ShapeCache, the logic in `font_features.h` is
    needed in `core`, but it wasn't possible because `hb.h` isn't
    visible for `core`.
    
    This patch has no behavior changes.
    
    Bug: 40253357
    Change-Id: If3c607e11aaf2baec2b39733d55cda1becaf6e29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174132
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408843}

--- third_party/blink/renderer/platform/fonts/shaping/font_features_test.cc
+++ third_party/blink/renderer/platform/fonts/shaping/font_features_test.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/fonts/shaping/font_features.h"
 
+#include <hb.h>
+
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
