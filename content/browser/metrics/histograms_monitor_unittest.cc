473ae8fcec04828d033d54e8ac44e7a71b7a9a3c
Andrzej Fiedukowicz <afie@google.com>
2025-01-21 09:56:26 -0800

    Allow chrome://histograms selecting histogram while monitoring
    In crbug.com/40864008 it was discussed that selecting histogram while in
    monitoring mode isn't really supported because as soon as new data comes
    the selection gets invalidated. The only way to monitor single histogram
    as to select them before getting into monitoring mode.
    
    This attempts to fix that by doing the following:
    
    1. Make `StartMonitoring` ignore the query taking the baseline of ALL
    the histograms even if one is preselected. This is done to allow
    selection change without changing a base point.
    
    2. Make `GetDiff` not ignore the query returning only the histogram that
    is actually requested by a frontend page.
    
    3. Don't force `requestHistograms` when hash selection changes during
    monitoring mode. This mixes monitoring data which are diff from baseline
    with a global histogram data. With monitoring mode the updated data will
    be pulled with the next fetch anyway (within 1s).
    
    4. Link header to `#` allowing to reset selection (aka. `query`) without
    refreshing the whole page and therefore the baseline. This allows
    changing selection live and opens potential for selecting multiple
    histograms.
    
    There is a small risk of increased memory footprint for users doing a
    lot of selective monitoring as each of them will now keep the whole
    "baseline" set of histograms. However it seems like an extremely obscure
    use case and the footprint is not enormous anyway O(kB).
    
    Tested:
    
     * Updated and added new coverage to automated testing
     * Checked the functionality with local build on gLinux
    
    Change-Id: I8fbf6e7fc138e2639e2d5ea5dff2d48a5666075a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175086
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Andrzej Fiedukowicz <afie@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409115}

--- content/browser/metrics/histograms_monitor_unittest.cc
+++ content/browser/metrics/histograms_monitor_unittest.cc
@@ -13,6 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+namespace {
+
+constexpr std::string kEmptyFilter = "";
+
+}  // namespace
+
 class HistogramsMonitorTest : public testing::Test {
  public:
   HistogramsMonitorTest()
@@ -27,17 +33,17 @@ TEST_F(HistogramsMonitorTest, StartMonitoringThenGetDiff) {
       "MonitorHistogram1", 1, 1000, 10, base::HistogramBase::kNoFlags);
   histogram1->Add(30);
   HistogramsMonitor monitor;
-  monitor.StartMonitoring(/*query=*/"");
+  monitor.StartMonitoring();
 
   // Get diff immediately should return nothing.
-  base::Value::List diff = monitor.GetDiff();
+  base::Value::List diff = monitor.GetDiff(kEmptyFilter);
   ASSERT_EQ(diff.size(), 0ull);
 
   // Add more data to histogram.
   histogram1->Add(30);
   histogram1->Add(40);
 
-  diff = monitor.GetDiff();
+  diff = monitor.GetDiff(kEmptyFilter);
   ASSERT_EQ(diff.size(), 1ull);
   std::string* header1 = diff[0].GetDict().FindString("header");
   EXPECT_EQ(*header1,
@@ -47,7 +53,7 @@ TEST_F(HistogramsMonitorTest, StartMonitoringThenGetDiff) {
   base::HistogramBase* histogram2 = base::Histogram::FactoryGet(
       "MonitorHistogram2", 1, 1000, 10, base::HistogramBase::kNoFlags);
   histogram2->Add(50);
-  diff = monitor.GetDiff();
+  diff = monitor.GetDiff(kEmptyFilter);
   ASSERT_EQ(diff.size(), 2ull);
   std::string* header2 = diff[1].GetDict().FindString("header");
   EXPECT_EQ(*header2,
@@ -61,12 +67,12 @@ TEST_F(HistogramsMonitorTest, StartMonitoringWithQueryThenGetDiff) {
       "MonitorHistogram1", 1, 1000, 10, base::HistogramBase::kNoFlags);
   histogram1->Add(30);
   HistogramsMonitor monitor;
-  monitor.StartMonitoring(/*query=*/"MonitorHistogram1");
+  monitor.StartMonitoring();
 
   base::HistogramBase* histogram2 = base::Histogram::FactoryGet(
       "MonitorHistogram2", 1, 1000, 10, base::HistogramBase::kNoFlags);
   histogram2->Add(50);
-  base::Value::List diff = monitor.GetDiff();
+  base::Value::List diff = monitor.GetDiff("MonitorHistogram1");
   ASSERT_EQ(diff.size(), 0ull);
 }
 
@@ -79,24 +85,83 @@ TEST_F(HistogramsMonitorTest, CaseInsensitiveQuery) {
   histogram1->Add(30);
 
   HistogramsMonitor monitor;
-  monitor.StartMonitoring(/*query=*/"histogram1");
+  monitor.StartMonitoring();
 
   base::HistogramBase* histogram2 = base::Histogram::FactoryGet(
       "MonitorHistogram2", 1, 1000, 10, base::HistogramBase::kNoFlags);
   histogram2->Add(50);
 
   // The query shouldn't match "MonitorHistogram2".
-  base::Value::List diff = monitor.GetDiff();
+  base::Value::List diff = monitor.GetDiff("histogram1");
   ASSERT_EQ(diff.size(), 0ull);
 
   histogram1->Add(10);
 
   // The query should match "MonitorHistogram1", since it's case insensitive.
-  diff = monitor.GetDiff();
+  diff = monitor.GetDiff("histogram1");
   ASSERT_EQ(diff.size(), 1ull);
   std::string* header2 = diff[0].GetDict().FindString("header");
   EXPECT_EQ(*header2,
             "Histogram: MonitorHistogram1 recorded 1 samples, mean = 10.0");
+}
+
+TEST_F(HistogramsMonitorTest, MonitoringBaselineDoesntChangeWithFilter) {
+  base::StatisticsRecorder::ForgetHistogramForTesting("MonitorHistogram1");
+  base::StatisticsRecorder::ForgetHistogramForTesting("MonitorHistogram2");
+  base::HistogramBase* histogram1 = base::Histogram::FactoryGet(
+      "MonitorHistogram1", 1, 1000, 10, base::HistogramBase::kNoFlags);
+  histogram1->Add(30);
+
+  base::HistogramBase* histogram2 = base::Histogram::FactoryGet(
+      "MonitorHistogram2", 1, 1000, 10, base::HistogramBase::kNoFlags);
+  histogram2->Add(50);
+  HistogramsMonitor monitor;
+
+  monitor.StartMonitoring();
+
+  // Get diff immediately should return nothing.
+  base::Value::List diff = monitor.GetDiff(kEmptyFilter);
+  ASSERT_EQ(diff.size(), 0ull);
+
+  // Add more data to histogram.
+  histogram1->Add(30);
+  histogram1->Add(40);
+
+  histogram2->Add(20);
+
+  // Filter the query to only return "MonitorHistogram1", the baseline should
+  // not change.
+  diff = monitor.GetDiff("MonitorHistogram1");
+  ASSERT_EQ(diff.size(), 1ull);
+  std::string* header1 = diff[0].GetDict().FindString("header");
+  EXPECT_EQ(*header1,
+            "Histogram: MonitorHistogram1 recorded 2 samples, mean = 35.0");
+
+
+  // Add more data to histogram2 and expect both samples after baseline to be
+  // returned.
+  histogram2->Add(10);
+  diff = monitor.GetDiff("MonitorHistogram2");
+  ASSERT_EQ(diff.size(), 1ull);
+  std::string* header2 = diff[0].GetDict().FindString("header");
+  EXPECT_EQ(*header2,
+            "Histogram: MonitorHistogram2 recorded 2 samples, mean = 15.0");
+
+  // Add more data to histogram1 and expect that both histograms are still
+  // returned for empty filter as they changed compared to the baseline.
+  histogram1->Add(50);
+  diff = monitor.GetDiff(kEmptyFilter);
+  ASSERT_EQ(diff.size(), 2ull);
+
+  std::vector<std::string> headers;
+  headers.push_back(*diff[0].GetDict().FindString("header"));
+  headers.push_back(*diff[1].GetDict().FindString("header"));
+  EXPECT_THAT(headers,
+              testing::UnorderedElementsAre(
+                  "Histogram: MonitorHistogram1 recorded 3 samples, "
+                  "mean = 40.0",
+                  "Histogram: MonitorHistogram2 recorded 2 samples, "
+                  "mean = 15.0"));
 }
 
 }  // namespace content
--- content/browser/resources/histograms/histograms_internals.ts
+++ content/browser/resources/histograms/histograms_internals.ts
@@ -250,6 +250,14 @@ function downloadHistograms() {
   }
 }
 
+function handleHashChange() {
+  // In monitoring mode the updated histograms will be fetched by fetchDiff()
+  // within 1s of changing URL and the proper filter will be applied.
+  if (!inMonitoringMode) {
+    requestHistograms();
+  }
+}
+
 /**
  * Load the initial list of histograms.
  */
@@ -267,4 +275,4 @@ document.addEventListener('DOMContentLoaded', function() {
 /**
  * Reload histograms when the "#abc" in "chrome://histograms/#abc" changes.
  */
-window.onhashchange = requestHistograms;
+window.onhashchange = handleHashChange;
