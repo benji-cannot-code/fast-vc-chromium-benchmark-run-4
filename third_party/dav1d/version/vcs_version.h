20abfde8892866517d2a823525f728acc23c4ec7
Wan-Teh Chang <wtc@google.com>
2025-01-21 11:28:46 -0800

    Roll src/third_party/dav1d/libdav1d/ edb16889d..42b2b24fb (2 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/edb16889d16f..42b2b24fb881
    
    $ git log edb16889d..42b2b24fb --date=short --no-merges --format='%ad %ae %s'
    2024-12-15 jb Update NEWS for 1.5.1
    2025-01-10 wtc Include <string.h> for memcpy()
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org
    
    Change-Id: I4a3ec9779ef908c1e0cd3d3f16678defdc2515b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187921
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409185}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.0-54-gedb1688"
+#define DAV1D_VERSION "1.5.1-0-g42b2b24"
