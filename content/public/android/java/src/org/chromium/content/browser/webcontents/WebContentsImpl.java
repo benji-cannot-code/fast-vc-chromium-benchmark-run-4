d328e9abedc2e3138450cea855eed1076b82c50b
Aldo Culquicondor <acondor@chromium.org>
2025-01-21 08:04:26 -0800

    Reland "Update support for forward transition animations via onResume"
    This is a reland of commit be2fd01a3178f177080f569974c76e7d1ebe6d73
    
    NavigationTransitionsTest now avoid screenshots during backward navigations
    while in gesture mode. Tests needed to be adapted.
    
    The tests are skipped on emulators.
    Tested in
    https://ci.chromium.org/ui/inv/u-acondor-2025-01-20-15-18-00-8070fed3a83c063d/test-results?sortby=&groupby=&q=NavigationTransitionsTest
    
    Original change's description:
    > Update support for forward transition animations via onResume
    >
    > Instead of querying the java web contents every time there is a
    > navigation, react to events to update the capability in the WebContents.
    >
    > This will allow to communicate the navigation mode to the renderers only
    > when the mode changes.
    >
    > Bug: 384026829
    > Change-Id: Ia4a417887f275da28051cbb2ae4ee70d04905eda
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172592
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1408089}
    
    Bug: 384026829
    Change-Id: I51393c64e7ec72d8b8a5bbab19f99f59ec280f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6184552
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409025}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -1263,6 +1263,12 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
                 .captureContentAsBitmapForTesting(mNativeWebContentsAndroid, callback);
     }
 
+    @Override
+    public void setSupportsForwardTransitionAnimation(boolean supports) {
+        WebContentsImplJni.get()
+                .setSupportsForwardTransitionAnimation(mNativeWebContentsAndroid, supports);
+    }
+
     @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
     @NativeMethods
     public interface Natives {
@@ -1479,5 +1485,7 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
 
         void captureContentAsBitmapForTesting(
                 long nativeWebContentsAndroid, Callback<Bitmap> callback);
+
+        void setSupportsForwardTransitionAnimation(long nativeWebContentsAndroid, boolean enabled);
     }
 }
--- content/public/browser/web_contents_delegate.cc
+++ content/public/browser/web_contents_delegate.cc
@@ -450,10 +450,6 @@ bool WebContentsDelegate::IsWaitingForPointerLockPrompt(
 }
 
 #if BUILDFLAG(IS_ANDROID)
-bool WebContentsDelegate::SupportsForwardTransitionAnimation() {
-  return true;
-}
-
 SkBitmap WebContentsDelegate::MaybeCopyContentAreaAsBitmapSync() {
   return SkBitmap();
 }
