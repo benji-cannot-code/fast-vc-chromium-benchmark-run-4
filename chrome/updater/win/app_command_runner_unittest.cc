11688a133b341ab858ab07f536037919794aeace
Sorin Jianu <sorin@chromium.org>
2025-01-21 11:17:02 -0800

    updater: fix unsafe buffer in app_command_runner_unittest.cc
    Change-Id: I696996ac4dd816ed521b0668cf53156775ece565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6183402
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409176}

--- chrome/updater/win/app_command_runner_unittest.cc
+++ chrome/updater/win/app_command_runner_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/updater/win/app_command_runner.h"
 
 #include <windows.h>
@@ -21,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/path_service.h"
@@ -352,7 +349,11 @@ TEST_P(AppCommandFormatComponentsAndCommandLineTest, TestCases) {
                          << "' which unexpectedly did not parse to a single "
                          << "argument.";
 
-  EXPECT_EQ(argv_handle.get()[1], GetParam().substitutions[0])
+  // SAFETY: the unsafe buffer is present because ::CommandLineToArgvW call. The
+  // code is safe because `num_args` is checked as a size_t value.
+  UNSAFE_BUFFERS(const base::span<wchar_t*> safe_argv_handle{
+      argv_handle.get(), base::checked_cast<size_t>(num_args)});
+  EXPECT_EQ(safe_argv_handle[1], GetParam().substitutions[0])
       << "substitution '" << GetParam().substitutions[0]
       << "' gave command line '" << cmd << "' which did not parse back to the "
       << "original substitution";
