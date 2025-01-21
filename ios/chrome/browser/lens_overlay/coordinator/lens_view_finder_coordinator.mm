d7d1d81bb2721f28e3721fe4a80ed049802e4427
Radu Nitescu <radunitescu@google.com>
2025-01-21 09:14:11 -0800

    [iOS][LU] Lock LVF in portrait in the prototype
    Bug: None
    Change-Id: I6560e14a6f946c38294ef699286342f8049db14d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6163473
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409089}

--- ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.h"
 
+#import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_configuration_factory.h"
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_entrypoint.h"
 #import "ios/chrome/browser/lens_overlay/ui/lens_view_finder_transition_manager.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
@@ -15,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/lens_overlay_commands.h"
 #import "ios/chrome/browser/shared/public/commands/open_lens_input_selection_command.h"
 #import "ios/chrome/browser/shared/public/commands/search_image_with_lens_command.h"
+#import "ios/chrome/browser/ui/device_orientation/scoped_force_portrait_orientation.h"
 #import "ios/public/provider/chrome/browser/lens/lens_api.h"
 
 namespace {
@@ -49,6 +53,9 @@ const CGFloat kBottomCornerRadius = 108.0;
 
   // Manages the presenting & dismissal of the LVF user interface.
   LensViewFinderTransitionManager* _transitionManager;
+
+  /// Forces the device orientation in portrait mode.
+  std::unique_ptr<ScopedForcePortraitOrientation> _scopedForceOrientation;
 }
 
 @synthesize baseViewController = _baseViewController;
@@ -68,6 +75,7 @@ const CGFloat kBottomCornerRadius = 108.0;
 
 - (void)stop {
   [self.browser->GetCommandDispatcher() stopDispatchingToTarget:self];
+  [self lockOrientationPortrait:NO];
   [super stop];
 }
 
@@ -163,10 +171,36 @@ const CGFloat kBottomCornerRadius = 108.0;
   [self exitLensViewFinder];
 }
 
+- (void)lensControllerWillAppear:
+    (id<ChromeLensViewFinderController>)lensController {
+  [self lockOrientationPortrait:YES];
+}
+
+- (void)lensControllerWillDisappear:
+    (id<ChromeLensViewFinderController>)lensController {
+  [self lockOrientationPortrait:NO];
+}
+
 #pragma mark - Private
+
 - (void)exitLensViewFinder {
   if (self.baseViewController.presentedViewController == _lensViewController) {
     [self.baseViewController dismissViewControllerAnimated:YES completion:nil];
+  }
+}
+
+- (void)lockOrientationPortrait:(BOOL)portraitLock {
+  if (!portraitLock) {
+    _scopedForceOrientation = nil;
+    return;
+  }
+
+  SceneState* sceneState = self.browser->GetSceneState();
+  if (!self.browser) {
+    return;
+  }
+  if (AppState* appState = sceneState.profileState.appState) {
+    _scopedForceOrientation = ForcePortraitOrientationOnIphone(appState);
   }
 }
 
