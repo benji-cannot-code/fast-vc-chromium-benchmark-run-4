19a9d39fa9761bd5b0af6429e2ad7b96fbbb8d13
Lukasz Anforowicz <lukasza@chromium.org>
2025-01-21 14:02:52 -0800

    Roll bitflags: 2.6.0 => 2.8.0 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * bitflags: 2.6.0 => 2.8.0
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * bitflags@2.8.0: crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: I5bb66229566ddff69daa2a7e253bd9fc6d310ba5
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6186243
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409274}

--- third_party/rust/chromium_crates_io/vendor/bitflags-2.8.0/src/tests.rs
+++ third_party/rust/chromium_crates_io/vendor/bitflags-2.8.0/src/tests.rs
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 mod all;
+mod bitflags_match;
 mod bits;
 mod complement;
 mod contains;
@@ -22,7 +23,9 @@ mod iter;
 mod parser;
 mod remove;
 mod symmetric_difference;
+mod truncate;
 mod union;
+mod unknown;
 
 bitflags! {
     #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
---
+++ third_party/rust/chromium_crates_io/vendor/bitflags-2.8.0/src/tests/truncate.rs
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+use super::*;
+
+use crate::Flags;
+
+#[test]
+fn cases() {
+    case(
+        TestFlags::ABC | TestFlags::from_bits_retain(1 << 3),
+        TestFlags::ABC,
+    );
+
+    case(TestZero::empty(), TestZero::empty());
+
+    case(TestZero::all(), TestZero::all());
+
+    case(
+        TestFlags::from_bits_retain(1 << 3) | TestFlags::all(),
+        TestFlags::all(),
+    );
+}
+
+#[track_caller]
+fn case<T: Flags + std::fmt::Debug>(mut before: T, after: T)
+where
+    T: std::fmt::Debug + PartialEq + Copy,
+{
+    before.truncate();
+    assert_eq!(before, after, "{:?}.truncate()", before);
+}
