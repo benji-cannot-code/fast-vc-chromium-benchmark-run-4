f250b7533b435b5c6e613439241c2f2c02d63bfb
Andrew Grieve <agrieve@chromium.org>
2025-01-21 15:02:24 -0800

    Android: Enable -parameters flag in javac
    Makes Error Prone's ParameterName check work for calls across target
    boundaries.
    
    Bug: 40286681
    Change-Id: I5c9c6e3314783bbd9445d4a6832e50ad9c3b5360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185925
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409318}

--- build/android/gyp/compile_java.py
+++ build/android/gyp/compile_java.py
@@ -711,6 +711,8 @@ def main(argv):
 
   javac_args = [
       '-g',
+      # Required for Error Prone's /* paramName= */ check.
+      '-parameters',
       # Jacoco does not currently support a higher value.
       '--release',
       '17',
