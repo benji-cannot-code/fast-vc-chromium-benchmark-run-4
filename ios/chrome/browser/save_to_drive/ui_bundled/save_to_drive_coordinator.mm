7fee7387cc38784b43803d3ddbe3c9fec3451fd6
Arthur Milchior <arthurmilchior@chromium.org>
2025-01-21 07:20:26 -0800

    [Signin]Rename AccessPoint enum entries
    See go/access_point_shorter to see how those patches were generated.
    
    This allows to remove at least 13 characters per line and avoid the
    redundant double "access point" when the entry is used.
    
    This CL was uploaded by git cl split.
    
    R=qpubert@google.com
    
    R: droger
    Bug: 389192722
    Change-Id: I2d417429df6bb2e1209164a866d15e94a1bc9008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185458
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408981}

--- ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_coordinator.mm
+++ ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_coordinator.mm
@@ -136,8 +136,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ShowSigninCommand* addAccountCommand = [[ShowSigninCommand alloc]
       initWithOperation:AuthenticationOperation::kAddAccount
                identity:nil
-            accessPoint:signin_metrics::AccessPoint::
-                            ACCESS_POINT_SAVE_TO_DRIVE_IOS
+            accessPoint:signin_metrics::AccessPoint::kSaveToDriveIOS
             promoAction:signin_metrics::PromoAction::
                             PROMO_ACTION_NO_SIGNIN_PROMO
              completion:^(SigninCoordinatorResult result,
