d5b594cfc80e715b0aa0d41f8219c4054b2ecf58
Arthur Milchior <arthurmilchior@chromium.org>
2025-01-21 04:28:21 -0800

    [Signin]Rename AccessPoint enum entries
    See go/access_point_shorter to see how those patches were generated.
    
    This allows to remove at least 13 characters per line and avoid the
    redundant double "access point" when the entry is used.
    
    This CL was uploaded by git cl split.
    
    R=fsenra@google.com
    
    R: droger
    Bug: 389192722
    Change-Id: I1ea3c6cd3d60556b7dc51dc202600a77a85530f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187065
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408963}

--- ios/chrome/browser/settings/ui_bundled/clear_browsing_data/clear_browsing_data_manager_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/clear_browsing_data/clear_browsing_data_manager_unittest.mm
@@ -205,8 +205,7 @@ TEST_F(ClearBrowsingDataManagerTest, TestModelSignedInSyncOff) {
   test_sync_service_->SetSignedIn(signin::ConsentLevel::kSignin);
 
   AuthenticationServiceFactory::GetForProfile(profile_.get())
-      ->SignIn(fake_identity(),
-               signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS);
+      ->SignIn(fake_identity(), signin_metrics::AccessPoint::kSettings);
 
   [manager_ loadModel:model_];
 
@@ -355,8 +354,7 @@ TEST_F(ClearBrowsingDataManagerTest, TestOnPreferenceChanged) {
 
 TEST_F(ClearBrowsingDataManagerTest, TestGoogleDSETextSignedIn) {
   AuthenticationServiceFactory::GetForProfile(profile_.get())
-      ->SignIn(fake_identity(),
-               signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS);
+      ->SignIn(fake_identity(), signin_metrics::AccessPoint::kSettings);
 
   [manager_ loadModel:model_];
 
@@ -386,8 +384,7 @@ TEST_F(ClearBrowsingDataManagerTest, TestGoogleDSETextSignedOut) {
 
 TEST_F(ClearBrowsingDataManagerTest, TestPrepopulatedTextSignedIn) {
   AuthenticationServiceFactory::GetForProfile(profile_.get())
-      ->SignIn(fake_identity(),
-               signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS);
+      ->SignIn(fake_identity(), signin_metrics::AccessPoint::kSettings);
 
   // Set DSE to one from "prepoulated list".
   const std::string kEngineP1Name = "prepopulated-1";
@@ -453,8 +450,7 @@ TEST_F(ClearBrowsingDataManagerTest, TestPrepopulatedTextSignedOut) {
 
 TEST_F(ClearBrowsingDataManagerTest, TestCustomTextSignedIn) {
   AuthenticationServiceFactory::GetForProfile(profile_.get())
-      ->SignIn(fake_identity(),
-               signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS);
+      ->SignIn(fake_identity(), signin_metrics::AccessPoint::kSettings);
 
   // Set DSE to a be fully custom.
   const std::string kEngineC1Name = "custom-1";
