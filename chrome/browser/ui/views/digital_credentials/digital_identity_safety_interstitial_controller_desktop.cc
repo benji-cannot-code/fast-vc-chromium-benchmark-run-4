823efa461851270a9b846c39ddb5bfd4aec78ffc
Mohamed Amir Yosef <mamir@chromium.org>
2025-01-21 14:15:21 -0800

    [DC] Button in the DC API interstitial should have the same prominence
    After discussing with UX, it has been decided that both buttons in the
    interstitial should have the same prominence.
    
    Bug: 384423219
    Change-Id: Ic517c803d542b6deb1e91a93012f5e34c3e95613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182132
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409281}

--- chrome/browser/ui/views/digital_credentials/digital_identity_safety_interstitial_controller_desktop.cc
+++ chrome/browser/ui/views/digital_credentials/digital_identity_safety_interstitial_controller_desktop.cc
@@ -125,7 +125,7 @@ void DigitalIdentitySafetyInterstitialControllerDesktop::ShowInterstitialImpl(
                          DigitalIdentityInterstitialClosedReason::kOkButton),
           DialogButton::Params()
               .SetLabel(positive_button_label)
-              .SetStyle(ui::ButtonStyle::kText)
+              .SetStyle(ui::ButtonStyle::kDefault)
               .SetEnabled(positive_button_enabled))
       .AddCancelButton(
           base::BindOnce(
@@ -135,8 +135,8 @@ void DigitalIdentitySafetyInterstitialControllerDesktop::ShowInterstitialImpl(
               DigitalIdentityInterstitialClosedReason::kCancelButton),
           DialogButton::Params()
               .SetLabel(negative_button_label)
-              .SetStyle(ui::ButtonStyle::kProminent))
-      .OverrideDefaultButton(ui::mojom::DialogButton::kCancel)
+              .SetStyle(ui::ButtonStyle::kDefault))
+      .OverrideDefaultButton(ui::mojom::DialogButton::kNone)
       .SetDialogDestroyingCallback(base::BindOnce(
           &DigitalIdentitySafetyInterstitialControllerDesktop::OnDialogClosed,
           weak_ptr_factory_.GetWeakPtr(),
