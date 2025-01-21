93b549eaa1d29cf24dac0a843bbd4aed8251a0ec
Abigail Katcoff <abigailkatcoff@chromium.org>
2025-01-21 11:10:33 -0800

    PA: Use new thread selector in bidder worklet
    Use a new thread selector implemented in a previous CL in an
    effort to balance the load of tasks between threads on the bidder
    worklet and improve auction latency.
    
    Bug: 390444489
    Change-Id: I460a75fb7c4b1866e0756fd460a8f0f08c7d4196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180198
    Commit-Queue: Abigail Katcoff <abigailkatcoff@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409170}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -18614,7 +18614,9 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest, ExecutionModeGroupByOrigin) {
     }
   )";
 
-  const int kNumGroups = 10;  // as many ads in each group, too.
+  // Keep this number relatively low so that the groups don't get divided
+  // among multiple threads.
+  const int kNumGroups = 3;  // as many ads in each group, too.
   GURL test_url =
       embedded_https_test_server().GetURL("a.test", "/page_with_iframe.html");
   ASSERT_TRUE(NavigateToURL(shell(), test_url));
@@ -18652,7 +18654,7 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest, ExecutionModeGroupByOrigin) {
                 .Build()));
   }
 
-  EXPECT_EQ(embedded_https_test_server().GetURL("c.test", "/echo?10"),
+  EXPECT_EQ(embedded_https_test_server().GetURL("c.test", "/echo?3"),
             RunAuctionAndWaitForUrl(JsReplace(
                 R"({
                   seller: $1,
