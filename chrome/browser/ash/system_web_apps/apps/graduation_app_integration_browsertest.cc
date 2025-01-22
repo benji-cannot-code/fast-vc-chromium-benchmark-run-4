ed43b2aa115807da3c7cb9381891b7c076f247ae
Courtney Wong <courtneywong@chromium.org>
2025-01-22 10:25:07 -0800

    graduation: Update app name to "Content transfer" in strings
    Bug: b:391434594
    Change-Id: Id1d6edb97e713eec77ae434c42f2b64e48479c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6186118
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409788}

--- chrome/browser/ash/system_web_apps/apps/graduation_app_integration_browsertest.cc
+++ chrome/browser/ash/system_web_apps/apps/graduation_app_integration_browsertest.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr char kContentTransferAppName[] = "Content Transfer";
+constexpr char kContentTransferAppName[] = "Content transfer";
 constexpr char kFromChromeInternalHistogramName[] =
     "Apps.DefaultAppLaunch.FromChromeInternal";
 
