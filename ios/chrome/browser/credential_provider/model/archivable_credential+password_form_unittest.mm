285a2869542069657b8dd1452d3b78d59ff0b67b
Sylvain Defresne <sdefresne@chromium.org>
2025-01-22 09:45:40 -0800

    [cleanup] clang-format //ios/chrome/browser/credential_provider
    Produced by:
      git ls-files \
          ios/chrome/browser/credential_provider/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I25e4df1ddc5dc2664ebf98de54883bd322c0d5c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182579
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409755}

--- ios/chrome/browser/credential_provider/model/archivable_credential+password_form_unittest.mm
+++ ios/chrome/browser/credential_provider/model/archivable_credential+password_form_unittest.mm
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/credential_provider/model/archivable_credential+password_form.h"
-
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
 #import "components/password_manager/core/browser/password_form.h"
+#import "ios/chrome/browser/credential_provider/model/archivable_credential+password_form.h"
 #import "testing/gtest_mac.h"
 #import "testing/platform_test.h"
 #import "url/gurl.h"
