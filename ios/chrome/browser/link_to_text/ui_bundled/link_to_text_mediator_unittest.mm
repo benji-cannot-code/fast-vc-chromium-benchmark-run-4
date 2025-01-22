c0ad908b08a768e22021514ac5b10e1cead0d582
Sylvain Defresne <sdefresne@chromium.org>
2025-01-22 11:05:59 -0800

    [cleanup] clang-format //ios/chrome/browser/link_to_text
    Produced by:
      git ls-files ios/chrome/browser/link_to_text/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I4094cc193e8c7912f78d8154c3adfced5f3b8aa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190469
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409816}

--- ios/chrome/browser/link_to_text/ui_bundled/link_to_text_mediator_unittest.mm
+++ ios/chrome/browser/link_to_text/ui_bundled/link_to_text_mediator_unittest.mm
@@ -41,11 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
 
+using base::test::ios::kWaitForJSCompletionTimeout;
+using base::test::ios::WaitUntilConditionOrTimeout;
+using shared_highlighting::LinkGenerationError;
 using shared_highlighting::TextFragment;
 using web::FakeWebState;
-using base::test::ios::WaitUntilConditionOrTimeout;
-using base::test::ios::kWaitForJSCompletionTimeout;
-using shared_highlighting::LinkGenerationError;
 
 namespace {
 const CGFloat kCaretWidth = 4.0;
