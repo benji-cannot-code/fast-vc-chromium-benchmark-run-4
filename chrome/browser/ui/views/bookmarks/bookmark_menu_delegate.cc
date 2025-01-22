742851d4b8174171a36c4ac645f78cf39d8ca51c
Kaan Alsan <alsan@chromium.org>
2025-01-22 14:47:49 -0800

    Reland "Prevent AppMenu from closing after bookmark DnDs"
    This is a reland of commit 51374db9bb3b115fdfae82eccc5e8d2f8f3d708f
    
    This reland disabled the test on Mac-ARM64. The test is failing not
    because of an issue with this change, but rather that the Mac DND
    handler does not execute the "drop" portion of the DND, even though the
    correct Drop data is being set.
    
    > Prevent AppMenu from closing after bookmark DnDs
    > BookmarkMenuDelegate now supports updating menu views when there are
    > updates to the model, which means we don't have to close the menu on
    > each update anymore.
    > This CL prevents AppMenu from closing when bookmarks nodes are moved,
    > allowing the menu views to be updated by the delegate.
    > Change-Id: I7791b86ac33458e7fd96e93c20cc968450f0e0c2
    > Bug: 40868588
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148304
    > Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Kaan Alsan <alsan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1406920}
    
    Bug: 40868588
    Change-Id: Ibdffae7f8b82589fd7c821e3f9c1fbf0bbc923c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179525
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409982}

--- chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/resources/grit/ui_resources.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/menu_button.h"
+#include "ui/views/controls/menu/menu_controller.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/menu_separator.h"
 #include "ui/views/controls/menu/submenu_view.h"
@@ -642,8 +643,18 @@ void BookmarkMenuDelegate::BookmarkNodeMoved(const BookmarkNode* old_parent,
   if (old_parent_menu) {
     old_parent_menu->ChildrenChanged();
   }
-  if (new_parent_menu && !new_parent->HasAncestor(old_parent)) {
-    new_parent_menu->ChildrenChanged();
+
+  if (new_parent_menu) {
+    if (!new_parent->HasAncestor(old_parent)) {
+      new_parent_menu->ChildrenChanged();
+    }
+
+    // Open the new parent menu if the model was changed from a drag.
+    views::MenuController* new_menu_controller =
+        new_parent_menu ? new_parent_menu->GetMenuController() : nullptr;
+    if (new_menu_controller && new_menu_controller->drag_in_progress()) {
+      new_menu_controller->SelectItemAndOpenSubmenu(new_parent_menu);
+    }
   }
 }
 
