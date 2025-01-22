d398c377045a2b4dfbd43e81ccb3c020671f783b
Nicolás Peña <npm@chromium.org>
2025-01-22 14:41:32 -0800

    [FedCM] Never fetch client metadata from registered IDPs
    Bug: 346572117
    Change-Id: I36c3896b63d6bf7459141a4c36c230221e44e88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6191508
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409977}

--- content/browser/webid/federated_auth_request_impl_unittest.cc
+++ content/browser/webid/federated_auth_request_impl_unittest.cc
@@ -1842,6 +1842,7 @@ TEST_F(FederatedAuthRequestImplTest, WellKnownNotInListButRegistered) {
       .WillOnce(Return(registry));
 
   RunAuthTest(requestParameters, kExpectationSuccess, config);
+  EXPECT_FALSE(DidFetch(FetchedEndpoint::CLIENT_METADATA));
 }
 
 // Test that the well-known file has too many provider urls.
