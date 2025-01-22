55a9fdd575a17170e9c5dfc9f068e13bb3372f79
Federica Germinario <fedegermi@google.com>
2025-01-22 09:09:59 -0800

    [ios] Remove code cleaning legacy prefs
    Pref kTabResumptionLastOpenedTabURLPref was migrated to ProfilePrefs
    one year ago.
    This CL removes the migration code and updates tests (removing logic
    testing the migration code).
    
    Bug: none
    Change-Id: Idda750ae292ec62e6f5c463ad656cbc56ddb44a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182153
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409721}

--- ios/chrome/browser/shared/model/prefs/browser_prefs_unittest.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs_unittest.mm
@@ -163,8 +163,6 @@ TEST_F(BrowserPrefsTest, VerifyLocalStatePrefsMigration) {
                          list_example.Clone());
   local_state()->SetString(prefs::kIosSafetyCheckManagerPasswordCheckResult,
                            "Example");
-  local_state()->SetString(
-      tab_resumption_prefs::kTabResumptionLastOpenedTabURLPref, "Example");
   local_state()->SetDict(prefs::kIosPreRestoreAccountInfo,
                          dict_example.Clone());
 
@@ -185,13 +183,6 @@ TEST_F(BrowserPrefsTest, VerifyLocalStatePrefsMigration) {
             NameForSafetyCheckState(PasswordSafetyCheckState::kDefault));
   EXPECT_EQ(local_state()->GetString(
                 prefs::kIosSafetyCheckManagerPasswordCheckResult),
-            "Example");
-
-  EXPECT_EQ(pref_service()->GetString(
-                tab_resumption_prefs::kTabResumptionLastOpenedTabURLPref),
-            std::string());
-  EXPECT_EQ(local_state()->GetString(
-                tab_resumption_prefs::kTabResumptionLastOpenedTabURLPref),
             "Example");
 
   EXPECT_EQ(pref_service()->GetDict(prefs::kIosPreRestoreAccountInfo).size(),
@@ -220,13 +211,6 @@ TEST_F(BrowserPrefsTest, VerifyLocalStatePrefsMigration) {
   EXPECT_EQ(local_state()->GetString(
                 prefs::kIosSafetyCheckManagerPasswordCheckResult),
             NameForSafetyCheckState(PasswordSafetyCheckState::kDefault));
-
-  EXPECT_EQ(pref_service()->GetString(
-                tab_resumption_prefs::kTabResumptionLastOpenedTabURLPref),
-            "Example");
-  EXPECT_EQ(local_state()->GetString(
-                tab_resumption_prefs::kTabResumptionLastOpenedTabURLPref),
-            std::string());
 
   EXPECT_EQ(pref_service()->GetDict(prefs::kIosPreRestoreAccountInfo),
             dict_example);
