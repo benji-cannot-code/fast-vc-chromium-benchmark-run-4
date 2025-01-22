d59573bb2054a6a7bcedcaf82a3b0c3e3219691d
Ricky Yang <rickyyang@google.com>
2025-01-22 09:50:38 -0800

    Fix Talkback unlabeled color in color picker
    The color Red reads unlabeled in color picker. This CL fixes the incorrect view handling and makes it read the color correctly.
    
    AX-Relnotes: The Red color now reads red in Talkback
    Bug: 335708958
    Change-Id: If1db024b35757ec3e0eca136c4e2ae2151969069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185742
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Ricky Yang <rickyyang@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1409759}

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/delegate/ColorPickerViewBinder.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/delegate/ColorPickerViewBinder.java
@@ -65,6 +65,8 @@ public class ColorPickerViewBinder {
                             model.get(ColorPickerSuggestionProperties.ONCLICK)
                                     .onResult(model.get(ColorPickerSuggestionProperties.INDEX)));
         } else {
+            suggestionView.setContentDescription(model.get(ColorPickerSuggestionProperties.LABEL));
+            suggestionView.setSelected(model.get(ColorPickerSuggestionProperties.IS_SELECTED));
             suggestionView.setAccessibilityDelegate(
                     new View.AccessibilityDelegate() {
                         @Override
@@ -78,9 +80,6 @@ public class ColorPickerViewBinder {
                                             1,
                                             1,
                                             false));
-                            info.setSelected(
-                                    model.get(ColorPickerSuggestionProperties.IS_SELECTED));
-                            info.setText(model.get(ColorPickerSuggestionProperties.LABEL));
                         }
                     });
         }
