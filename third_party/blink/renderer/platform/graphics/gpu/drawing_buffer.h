c31961011d084c85ddf10f2396ea9a533f4f57b0
Colin Blundell <blundell@chromium.org>
2025-01-22 00:41:50 -0800

    [Blink] Rename DrawingBuffer software rendering test utils
    These utils still refer to SharedBitmap, which is now out of date.
    
    Bug: 40064122
    Change-Id: I33de20e57938661e2d18d30b4faac0ba997cc0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187069
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409510}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h
@@ -342,7 +342,7 @@ class PLATFORM_EXPORT DrawingBuffer : public cc::TextureLayerClient,
 
   bool Initialize(const gfx::Size&, bool use_multisampling);
 
-  void SetSharedImageInterfaceProviderForBitmapTest(
+  void SetSharedImageInterfaceProviderForSoftwareRenderingTest(
       std::unique_ptr<WebGraphicsSharedImageInterfaceProvider> sii_provider);
 
   struct SoftwareResource {
--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test_helpers.h
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test_helpers.h
@@ -422,7 +422,7 @@ class DrawingBufferForTests : public DrawingBuffer {
   static scoped_refptr<DrawingBufferForTests> Create(
       std::unique_ptr<WebGraphicsContext3DProvider> context_provider,
       std::unique_ptr<TestWebGraphicsSharedImageInterfaceProvider>
-          shared_image_interface_provider_for_shared_bitmap,
+          shared_image_interface_provider_for_sw,
       const Platform::GraphicsInfo& graphics_info,
       DrawingBuffer::Client* client,
       const gfx::Size& size,
@@ -439,8 +439,8 @@ class DrawingBufferForTests : public DrawingBuffer {
       drawing_buffer->BeginDestruction();
       return nullptr;
     }
-    drawing_buffer->SetSharedImageInterfaceProviderForBitmapTest(
-        std::move(shared_image_interface_provider_for_shared_bitmap));
+    drawing_buffer->SetSharedImageInterfaceProviderForSoftwareRenderingTest(
+        std::move(shared_image_interface_provider_for_sw));
 
     return drawing_buffer;
   }
