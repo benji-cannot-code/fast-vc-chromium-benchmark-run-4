cd3a14a649065480ac24a90d4023b5266ba3a1be
Colin Blundell <blundell@chromium.org>
2025-01-22 00:16:01 -0800

    [//cc] Remove unneeded stub GPU backings
    These are now all empty.
    
    Bug: 40064122
    Change-Id: Ie5756a2722fcd1b871a0886435ec8db7dc730ba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187140
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409503}

--- cc/test/fake_raster_buffer_provider.cc
+++ cc/test/fake_raster_buffer_provider.cc
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-class StubGpuBacking : public ResourcePool::GpuBacking {
-};
-
 FakeRasterBufferProviderImpl::FakeRasterBufferProviderImpl() = default;
 
 FakeRasterBufferProviderImpl::~FakeRasterBufferProviderImpl() = default;
@@ -27,7 +24,7 @@ FakeRasterBufferProviderImpl::AcquireBufferForRaster(
     bool depends_on_at_raster_decodes,
     bool depends_on_hardware_accelerated_jpeg_candidates,
     bool depends_on_hardware_accelerated_webp_candidates) {
-  auto backing = std::make_unique<StubGpuBacking>();
+  auto backing = std::make_unique<ResourcePool::GpuBacking>();
   backing->shared_image = gpu::ClientSharedImage::CreateForTesting();
   resource.set_gpu_backing(std::move(backing));
   return nullptr;
