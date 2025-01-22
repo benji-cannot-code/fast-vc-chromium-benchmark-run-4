a208014514ada05d6651fb439f27cf67e382b681
Sylvain Defresne <sdefresne@chromium.org>
2025-01-22 09:18:45 -0800

    [cleanup] clang-format //ios/chrome/browser/crash_report
    Produced by:
      git ls-files ios/chrome/browser/crash_report/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I8fbdc702a249f4c018d37b2c58fbbf2e609e196b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6189126
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409728}

--- ios/chrome/browser/crash_report/model/crash_loop_detection_util_unittest.mm
+++ ios/chrome/browser/crash_report/model/crash_loop_detection_util_unittest.mm
@@ -3,9 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "ios/chrome/browser/crash_report/model/crash_loop_detection_util.h"
+
 #import <Foundation/Foundation.h>
 
-#import "ios/chrome/browser/crash_report/model/crash_loop_detection_util.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
 
