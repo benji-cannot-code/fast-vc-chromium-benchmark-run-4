1b68ce5c8fd7b2e7d87fbda9fb770849c0adfb25
dpapad <dpapad@chromium.org>
2025-01-22 09:59:34 -0800

    WebUI: Migrate cr_components/history_embeddings/ to Lit.
    Bug: 40943652
    Change-Id: Ia886d2fa919fa9891093f3392615b74f609f7d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6186381
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409766}

--- ui/webui/resources/cr_components/history_embeddings/filter_chips.html.ts
+++ ui/webui/resources/cr_components/history_embeddings/filter_chips.html.ts
@@ -1,4 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {FilterChipsElement} from './filter_chips.js';
+
+export function getHtml(this: FilterChipsElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
 <select id="showByGroupSelectMenu" class="md-select"
     aria-label="${this.i18n('historyEmbeddingsShowByLabel')}"
     .value="${this.showResultsByGroup}"
@@ -25,3 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </cr-chip>
   `)}
 </div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
