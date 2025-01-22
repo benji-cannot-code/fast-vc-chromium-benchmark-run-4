349396bcbefe10f3622ff22ac8e83790af8d393f
Liza Bipin <mlbipin@google.com>
2025-01-22 09:07:40 -0800

    Update account dropdown in settings to be read correctly by screenreader
    Bug: 390544936
    Change-Id: Ia9fcc358a756d14ecf0b5b9e74a32bc1ef2b6677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182575
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409719}

--- chrome/browser/resources/settings/people_page/sync_account_control.ts
+++ chrome/browser/resources/settings/people_page/sync_account_control.ts
@@ -610,11 +610,23 @@ export class SettingsSyncAccountControlElement extends
     if (actionMenu) {
       actionMenu.close();
     }
+
+    const menuAnchor =
+        this.shadowRoot!.querySelector<HTMLElement>('#dropdown-arrow');
+    if (menuAnchor) {
+      menuAnchor.setAttribute('aria-expanded', 'false');
+    }
   }
 
   private onSignoutClick_() {
     this.syncBrowserProxy_.signOut(false /* deleteProfile */);
     this.shadowRoot!.querySelector('cr-action-menu')!.close();
+
+    const menuAnchor =
+        this.shadowRoot!.querySelector<HTMLElement>('#dropdown-arrow');
+    if (menuAnchor) {
+      menuAnchor.setAttribute('aria-expanded', 'false');
+    }
   }
 
   private onSyncButtonClick_() {
@@ -645,6 +657,7 @@ export class SettingsSyncAccountControlElement extends
         this.shadowRoot!.querySelector<HTMLElement>('#dropdown-arrow');
     assert(anchor);
     actionMenu.showAt(anchor);
+    anchor.setAttribute('aria-expanded', 'true');
   }
 
   private onShouldShowAvatarRowChange_() {
@@ -653,6 +666,12 @@ export class SettingsSyncAccountControlElement extends
     const actionMenu = this.shadowRoot!.querySelector('cr-action-menu');
     if (!this.shouldShowAvatarRow_ && actionMenu && actionMenu.open) {
       actionMenu.close();
+    }
+
+    const menuAnchor =
+        this.shadowRoot!.querySelector<HTMLElement>('#dropdown-arrow');
+    if (menuAnchor) {
+      menuAnchor.setAttribute('aria-expanded', 'false');
     }
   }
 
