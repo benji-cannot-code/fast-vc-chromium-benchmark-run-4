5d7fe8233365c6c7770ee0dccda4475a4709ecb1
Saifuddin Hitawala <hitawala@chromium.org>
2025-01-22 12:40:00 -0800

    [xr] Remove unused HandleBackgroundImage method
    While looking through usages of MailboxHolder came across
    HandleBackgroundImage method that is no longer used. Remove the method.
    
    Bug: 383309406
    Change-Id: I007d60d8e58f6d3a805fa2a53992d7dde8202b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6186113
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409901}

--- third_party/blink/renderer/modules/xr/xr_webgl_layer.h
+++ third_party/blink/renderer/modules/xr/xr_webgl_layer.h
@@ -83,10 +83,6 @@ class XRWebGLLayer final : public XRLayer, public XRWebGLLayerClient {
   void OnFrameEnd() override;
   void OnResize() override;
 
-  // Called from XRSession::OnFrame handler. Params are background texture
-  // mailbox holder and its size respectively.
-  void HandleBackgroundImage(const gpu::MailboxHolder&, const gfx::Size&) {}
-
   void Trace(Visitor*) const override;
 
  private:
