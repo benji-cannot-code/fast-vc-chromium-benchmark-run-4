04db8ec6c0ede8e3ef722eb5faf4f868193abfa3
Ananya <ananyaseelam@google.com>
2025-01-22 12:19:39 -0800

    [search aggregator] Add Debouncing
    This CL adds debouncing to the EnterpriseSearchAggregatorProvider.
    
    Bug: 380653020
    Change-Id: I20d6716fa61540ee2c98fc384f9419e3d39e981e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187585
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409885}

--- components/omnibox/browser/enterprise_search_aggregator_provider.h
+++ components/omnibox/browser/enterprise_search_aggregator_provider.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OMNIBOX_BROWSER_ENTERPRISE_SEARCH_AGGREGATOR_PROVIDER_H_
 #define COMPONENTS_OMNIBOX_BROWSER_ENTERPRISE_SEARCH_AGGREGATOR_PROVIDER_H_
 
+#include <memory>
 #include <string>
 
 #include "base/memory/raw_ptr.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_provider.h"
 
 class AutocompleteProviderClient;
+class AutocompleteProviderDebouncer;
 
 class EnterpriseSearchAggregatorProvider : public AutocompleteProvider {
  public:
@@ -37,8 +39,14 @@ class EnterpriseSearchAggregatorProvider : public AutocompleteProvider {
                                 const std::u16string& title,
                                 const std::u16string& additional_text);
 
+  // Called by `debouncer_`, queued when `Start()` is called.
+  void Run();
+
   // Owned by AutocompleteController.
   const raw_ptr<AutocompleteProviderClient> client_;
+
+  // Used to ensure that we don't send multiple requests in quick succession.
+  std::unique_ptr<AutocompleteProviderDebouncer> debouncer_;
 };
 
 #endif  // COMPONENTS_OMNIBOX_BROWSER_ENTERPRISE_SEARCH_AGGREGATOR_PROVIDER_H_
