cc40753e288c661ef84e9f84560c13b620725a14
Ethan Jimenez <ethavar@microsoft.com>
2025-01-22 12:20:48 -0800

    [Masonry] Refactor GridRangeBuilder constructors
    Previously we had two `GridRangeBuilder` constructors that had the same
    logic but different input parameters for testing purposes.
    
    This CL centralizes the logic into a single private constructor that is
    callable by the public one, while introducing the use of new helper
    methods for querying either the grid or masonry tracks.
    
    This change shouldn't have any behavioral impact.
    
    Bug: 343257585
    Change-Id: If194369f904751cbd9cc4d5ff816689d831b0eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181499
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1409887}

--- third_party/blink/renderer/core/layout/grid/grid_track_collection.h
+++ third_party/blink/renderer/core/layout/grid/grid_track_collection.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class GridLineResolver;
-
 // |GridTrackCollectionBase| provides an implementation for some shared
 // functionality on grid collections, specifically binary search on the
 // collection to get the range that contains a specific grid line.
@@ -89,8 +87,8 @@ class CORE_EXPORT GridRangeBuilder {
   GridRangeBuilder() = delete;
 
   GridRangeBuilder(const ComputedStyle& grid_style,
-                   const GridLineResolver& line_resolver,
                    GridTrackSizingDirection track_direction,
+                   wtf_size_t auto_repetitions,
                    wtf_size_t start_offset);
 
   // Ensures that after FinalizeRanges is called, a range will start at the
@@ -123,15 +121,15 @@ class CORE_EXPORT GridRangeBuilder {
     wtf_size_t* grid_item_range_index_to_cache;
   };
 
-  // This constructor is used exclusively in testing.
   GridRangeBuilder(const NGGridTrackList& explicit_tracks,
                    const NGGridTrackList& implicit_tracks,
-                   wtf_size_t auto_repetitions);
+                   wtf_size_t auto_repetitions,
+                   wtf_size_t start_offset = 0);
 
   wtf_size_t auto_repetitions_;
   wtf_size_t start_offset_;
 
-  bool must_sort_grid_lines_ : 1;
+  bool must_sort_grid_lines_{false};
 
   // Stores the grid's explicit and implicit tracks.
   const NGGridTrackList& explicit_tracks_;
