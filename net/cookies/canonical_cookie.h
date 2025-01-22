a622b0022ea5dd5a3fb809a805551b80a718f1fd
Anthony Vallée-Dubois <anthonyvd@google.com>
2025-01-22 13:35:04 -0800

    Subsample some CanonicalCookie histograms
    These histograms are currently recorded once for each included cookie,
    on every read attempt. This results in unnecessarily large data
    volumes, and recording histograms has a non-zero runtime cost. Because
    this code is in the critical path of navigation (it's executed for
    every resource request), it's preferable to subsample the metrics and
    avoid paying this cost.
    
    Bug: 390451503
    Change-Id: If430a12529ef83794c935e40394ba1b8697a0c27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182837
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409942}

--- net/cookies/canonical_cookie.h
+++ net/cookies/canonical_cookie.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/gtest_prod_util.h"
+#include "base/rand_util.h"
 #include "base/time/time.h"
 #include "base/types/pass_key.h"
 #include "crypto/process_bound_string.h"
@@ -404,6 +405,7 @@ class NET_EXPORT CanonicalCookie : public CookieBase {
   base::Time last_update_date_;
   CookiePriority priority_{COOKIE_PRIORITY_MEDIUM};
   CookieSourceType source_type_{CookieSourceType::kUnknown};
+  base::MetricsSubSampler metrics_subsampler_;
 };
 
 // Used to pass excluded cookie information when it's possible that the
