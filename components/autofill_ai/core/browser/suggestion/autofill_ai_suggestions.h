b88c7863ad0641e43ecb5be5ae02dfc5ff0abb9a
Bruno Braga <brunobraga@google.com>
2025-01-22 02:57:09 -0800

    [AutofillAi] Generate new AutofillAi filling suggestions
    Bug: 389629573
    Change-Id: I019971bf4e039fb359cd5d68581c59a945a19633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179442
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409560}

--- components/autofill_ai/core/browser/suggestion/autofill_ai_suggestions.h
+++ components/autofill_ai/core/browser/suggestion/autofill_ai_suggestions.h
@@ -8,12 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/containers/span.h"
 #include "components/autofill_ai/core/browser/suggestion/autofill_ai_model_executor.h"
 
 namespace autofill {
 
+class EntityInstance;
 class FormData;
 class FormFieldData;
+class FormStructure;
 struct Suggestion;
 
 }  // namespace autofill
@@ -23,8 +26,8 @@ namespace autofill_ai {
 class AutofillAiClient;
 
 // Returns true if the type of `autofill_suggestion` should not be added to
-// prediction improvements or if `autofill_suggestion` likely matches the cached
-// prediction improvements.
+// AutofillAi or if `autofill_suggestion` likely matches the cached
+// AutofillAi.
 // TODO(crbug.com/376016081): Move to anonymous namespace.
 bool ShouldSkipAutofillSuggestion(
     AutofillAiClient& client,
@@ -38,8 +41,10 @@ std::vector<autofill::Suggestion> CreateTriggerSuggestions();
 // Creates the animated suggestion shown while improved predictions are loaded.
 std::vector<autofill::Suggestion> CreateLoadingSuggestions();
 
-// Creates filling suggestions listing the ones for prediction improvements
-// first and `autofill_suggestions` afterwards.
+// Creates filling suggestions listing the ones for AutofillAi
+// first and `autofill_suggestions` afterwards. This method should only be
+// called when the feature `kAutofillAi` feature flag is enabled.
+// TODO(crbug.com/391276120): Delete this method once clean up starts.
 std::vector<autofill::Suggestion> CreateFillingSuggestions(
     AutofillAiClient& client,
     const AutofillAiModelExecutor::PredictionsByGlobalId& cache,
@@ -47,7 +52,15 @@ std::vector<autofill::Suggestion> CreateFillingSuggestions(
     const autofill::FormFieldData& field,
     const std::vector<autofill::Suggestion>& autofill_suggestions);
 
-// Creates a suggestion shown when retrieving prediction improvements wasn't
+// Creates filling suggestions using `autofill::EntityInstance`s.
+// Should be called only when `AutofillAiWithStructuredData` feature flag is
+// enabled.
+std::vector<autofill::Suggestion> CreateFillingSuggestionsV2(
+    const autofill::FormStructure& form,
+    const autofill::FormFieldData& triggering_field,
+    base::span<const autofill::EntityInstance> entities);
+
+// Creates a suggestion shown when retrieving AutofillAi wasn't
 // successful.
 std::vector<autofill::Suggestion> CreateErrorSuggestions();
 
