e12bf4a197cd7e453dfc07b3b276d4ebebeff6cd
Piotr Kotynia <piotrkotynia@google.com>
2025-01-22 05:49:19 -0800

    Remove FormGroup inheritance from Iban
    This change removes the inheritance of FormGroup from Iban and makes
    Iban a standalone class.
    
    The FormGroup inheritance was unnecessary and caused confusion as Iban
    doesn't really use a `FormGroup` base class. Iban doesn't use
    verification status and `SetRawInfo` method was only a wrapper over
    `set_value`.
    
    Bug: 40100455
    Change-Id: I1b30d45c24f2e5892fee7d116d23641a48928f26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187390
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409606}

--- components/autofill/core/browser/data_model/iban.h
+++ components/autofill/core/browser/data_model/iban.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/types/strong_alias.h"
 #include "components/autofill/core/browser/autofill_type.h"
-#include "components/autofill/core/browser/data_model/form_group.h"
 #include "components/autofill/core/browser/data_model/usage_history_information.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 
@@ -20,8 +19,8 @@ namespace autofill {
 
 struct PaymentsMetadata;
 
-// A form group that stores IBAN information.
-class Iban : public FormGroup {
+// A class storing IBAN information.
+class Iban {
  public:
   using Guid = base::StrongAlias<class GuidTag, std::string>;
   using InstrumentId = base::StrongAlias<class InstrumentIdTag, int64_t>;
@@ -142,7 +141,7 @@ class Iban : public FormGroup {
   explicit Iban(const InstrumentId& instrument_id);
 
   Iban(const Iban&);
-  ~Iban() override;
+  ~Iban();
 
   Iban& operator=(const Iban& iban);
 
@@ -174,16 +173,6 @@ class Iban : public FormGroup {
 
   PaymentsMetadata GetMetadata() const;
   bool SetMetadata(const PaymentsMetadata& metadata);
-
-  std::u16string GetInfo(FieldType type,
-                         const std::string& app_locale) const override;
-  std::u16string GetInfo(const AutofillType& type,
-                         const std::string& app_locale) const override;
-  std::u16string GetRawInfo(FieldType type) const override;
-  void SetRawInfoWithVerificationStatus(FieldType type,
-                                        const std::u16string& value,
-                                        VerificationStatus status) override;
-  void GetSupportedTypes(FieldTypeSet* supported_types) const override;
 
   // Returns true if there are no values (field types) set.
   bool IsEmpty(const std::string& app_locale) const;
