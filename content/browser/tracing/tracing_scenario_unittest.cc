fd11dad87198f36c666f8f4874ef6c7ff9cc7848
Etienne Pierre-doray <etiennep@chromium.org>
2025-01-22 10:46:25 -0800

    [tracing] Fix TracingScenarioTest
    Since the startup sessions don't get adopted in tests (we don't
    start real tracing session), disable startup tracing to avoid leaks.
    
    bug: 391438101
    Change-Id: I40c25fe152e8fe32729dfa04604153a1e8f7a7e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190287
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409801}

--- content/browser/tracing/tracing_scenario_unittest.cc
+++ content/browser/tracing/tracing_scenario_unittest.cc
@@ -250,7 +250,7 @@ class TracingScenarioForTesting : public TracingScenario {
                         delegate,
                         /*enable_privacy_filter=*/false,
                         /*is_local_scenario=*/true,
-                        /*request_startup_tracing=*/true) {
+                        /*request_startup_tracing=*/false) {
     EXPECT_TRUE(Initialize(config, false));
   }
 
@@ -389,13 +389,7 @@ TEST_F(TracingScenarioTest, StartStop) {
             tracing_scenario.current_state());
 }
 
-// TODO(crbug.com/391438101): Re-enable this test
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_NestedStartStop DISABLED_NestedStartStop
-#else
-#define MAYBE_NestedStartStop NestedStartStop
-#endif
-TEST_F(TracingScenarioTest, MAYBE_NestedStartStop) {
+TEST_F(TracingScenarioTest, NestedStartStop) {
   TracingScenarioForTesting tracing_scenario(
       ParseScenarioConfigFromText(kDefaultConfig), &delegate);
 
@@ -668,13 +662,7 @@ TEST_F(TracingScenarioTest, StopUpload) {
             tracing_scenario.current_state());
 }
 
-// TODO(crbug.com/391438101): Re-enable this test
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_NestedUpload DISABLED_NestedUpload
-#else
-#define MAYBE_NestedUpload NestedUpload
-#endif
-TEST_F(TracingScenarioTest, MAYBE_NestedUpload) {
+TEST_F(TracingScenarioTest, NestedUpload) {
   TracingScenarioForTesting tracing_scenario(
       ParseScenarioConfigFromText(kDefaultConfig), &delegate);
 
@@ -763,13 +751,7 @@ TEST_F(NestedTracingScenarioTest, Upload) {
             tracing_scenario.current_state());
 }
 
-// TODO(crbug.com/391438101): Re-enable this test
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_StopUpload DISABLED_StopUpload
-#else
-#define MAYBE_StopUpload StopUpload
-#endif
-TEST_F(NestedTracingScenarioTest, MAYBE_StopUpload) {
+TEST_F(NestedTracingScenarioTest, StopUpload) {
   NestedTracingScenarioForTesting tracing_scenario(
       ParseNestedScenarioConfigFromText(kDefaultNestedConfig), &delegate);
 
