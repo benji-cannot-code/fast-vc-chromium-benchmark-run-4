0a0dd7914ae908de27658ed7573c8d4717520487
Adam Walls <avvall@google.com>
2025-01-22 15:29:34 -0800

    [WV] Removing NOTREACHED to prevent AwComponentInstallerPolicy crash.
    Recent spikes in crashes have shown that the OnCustomUninstall code
    path for AwComponentInstallerPolicy is unexpectedly reached. The
    NOTREACHED call moved from silent errors to actually crashing on
    non-debug builds. This removes the call to NOTREACHED until
    the unexpected calls to the uninstall method can be stopped.
    
    Test: N/A
    Bug: 390586616
    Change-Id: Idb0b4592546d9e451f951d28bfcb85fd748c0518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6191091
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Adam Walls <avvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410002}

--- android_webview/nonembedded/component_updater/aw_component_installer_policy.cc
+++ android_webview/nonembedded/component_updater/aw_component_installer_policy.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/logging.h"
-#include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
@@ -40,8 +39,9 @@ std::string GetVersionDirName(const uint32_t sequence_number,
 AwComponentInstallerPolicy::AwComponentInstallerPolicy() = default;
 
 void AwComponentInstallerPolicy::OnCustomUninstall() {
-  // Uninstallation isn't supported in WebView.
-  NOTREACHED();
+  // TODO(crbug.com/391694564): Investigate why this is called and how to
+  // prevent it, since uninstallation isn't supported in WebView.
+  return;
 }
 
 // Copy the components file from `install_dir` to the serving directory of the
