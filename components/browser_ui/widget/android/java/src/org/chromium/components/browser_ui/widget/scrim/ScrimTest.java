3257b336ad7942dcb3fe92d0e867293aed20b22e
Sky Malice <skym@chromium.org>
2025-01-22 09:37:22 -0800

    Add composite color suppliers to scrim.
    Additionally migrated SigninAccountPickerCoordinator to use the new
    supplier instead of the old bespoke accessor.
    
    Bug: 371034867
    Change-Id: I55a08971f5e4b3e5465eab5d7b9e8f813a6bf47e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187924
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409749}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/scrim/ScrimTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/scrim/ScrimTest.java
@@ -80,6 +80,9 @@ public class ScrimTest {
 
     private final PayloadCallbackHelper<Integer> mScrimColorCallbackHelper =
             new PayloadCallbackHelper<>();
+    private final PayloadCallbackHelper<Integer> mStatusBarColorHelper =
+            new PayloadCallbackHelper<>();
+    private final PayloadCallbackHelper<Integer> mNavBarColorHelper = new PayloadCallbackHelper<>();
     private final PayloadCallbackHelper<Float> mStatusFractionCallback =
             new PayloadCallbackHelper<>();
     private final PayloadCallbackHelper<Float> mNavFractionCallback = new PayloadCallbackHelper<>();
@@ -117,6 +120,12 @@ public class ScrimTest {
                             .getFullScrimColorSupplier()
                             .addObserver(mScrimColorCallbackHelper::notifyCalled);
                     mScrimCoordinator
+                            .getStatusBarColorSupplier()
+                            .addObserver(mStatusBarColorHelper::notifyCalled);
+                    mScrimCoordinator
+                            .getNavigationBarColorSupplier()
+                            .addObserver(mNavBarColorHelper::notifyCalled);
+                    mScrimCoordinator
                             .getStatusBarScrimFractionSupplier()
                             .addObserver(mStatusFractionCallback::notifyCalled);
                     mScrimCoordinator
@@ -136,6 +145,8 @@ public class ScrimTest {
                 });
         // Wait for all the posted initial observations come back before test cases start.
         mScrimColorCallbackHelper.getOnlyPayloadBlocking();
+        mStatusBarColorHelper.getOnlyPayloadBlocking();
+        mNavBarColorHelper.getOnlyPayloadBlocking();
         mStatusFractionCallback.getOnlyPayloadBlocking();
         mNavFractionCallback.getOnlyPayloadBlocking();
     }
@@ -331,12 +342,16 @@ public class ScrimTest {
     @SmallTest
     @Feature({"Scrim"})
     public void testAffectsStatusBar_enabled() throws TimeoutException {
-        int callCount = mStatusFractionCallback.getCallCount();
+        int fractionCallCount = mStatusFractionCallback.getCallCount();
+        int colorCallCount = mStatusBarColorHelper.getCallCount();
         showScrim(buildModel(true, true, Color.RED), false);
         assertEquals(
                 1f,
-                mStatusFractionCallback.getPayloadByIndexBlocking(callCount).floatValue(),
+                mStatusFractionCallback.getPayloadByIndexBlocking(fractionCallCount).floatValue(),
                 MathUtils.EPSILON);
+        assertEquals(
+                Color.RED,
+                mStatusBarColorHelper.getPayloadByIndexBlocking(colorCallCount).intValue());
     }
 
     @Test
@@ -365,7 +380,8 @@ public class ScrimTest {
     @SmallTest
     @Feature({"Scrim"})
     public void testAffectsNavigationBar_enabled() throws TimeoutException {
-        int callCount = mNavFractionCallback.getCallCount();
+        int fractionCallCount = mNavFractionCallback.getCallCount();
+        int colorCallCount = mNavBarColorHelper.getCallCount();
         PropertyModel model =
                 ThreadUtils.runOnUiThreadBlocking(
                         () -> {
@@ -374,14 +390,17 @@ public class ScrimTest {
                                     .with(CLICK_DELEGATE, mClickDelegate)
                                     .with(VISIBILITY_CALLBACK, mVisibilityChangeCallback)
                                     .with(AFFECTS_NAVIGATION_BAR, true)
+                                    .with(BACKGROUND_COLOR, Color.RED)
                                     .build();
                         });
         showScrim(model, false);
 
         assertEquals(
                 1f,
-                mNavFractionCallback.getPayloadByIndexBlocking(callCount).floatValue(),
+                mNavFractionCallback.getPayloadByIndexBlocking(fractionCallCount).floatValue(),
                 MathUtils.EPSILON);
+        assertEquals(
+                Color.RED, mNavBarColorHelper.getPayloadByIndexBlocking(colorCallCount).intValue());
     }
 
     @Test
