323078464fb7258b3846cce48a5fa82d3ed18cb2
Chuong Ho <hdchuong@google.com>
2025-01-21 18:14:08 -0800

    lobster: add autotest API that overrides Lobster provider response
    The Tast test for Lobster requires mocking the response. This CL adds an
    auto test API to send a signal to switch to in-memory response provider
    in Lobster.
    
    Bug: b:391244188
    Change-Id: I3762a70ae6960538b0178607e947a8616d53a491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179038
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409417}

--- chrome/browser/ash/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/ash/extensions/autotest_private/autotest_private_api.cc
@@ -115,6 +115,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/guest_os/guest_os_registry_service.h"
 #include "chrome/browser/ash/guest_os/guest_os_registry_service_factory.h"
 #include "chrome/browser/ash/input_method/editor_mediator_factory.h"
+#include "chrome/browser/ash/lobster/lobster_service.h"
+#include "chrome/browser/ash/lobster/lobster_service_provider.h"
 #include "chrome/browser/ash/login/lock/screen_locker.h"
 #include "chrome/browser/ash/login/wizard_context.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_installer.h"
@@ -6333,6 +6335,32 @@ AutotestPrivateIsInputMethodReadyForTestingFunction::Run() {
       ash::IMEBridge::Get()->GetCurrentEngineHandler();
   return RespondNow(
       WithArguments(engine && engine->IsReadyForTesting()));  // IN-TEST
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// AutotestPrivateOverrideLobsterResponseForTestingFunction
+//////////////////////////////////////////////////////////////////////////////
+
+AutotestPrivateOverrideLobsterResponseForTestingFunction::
+    AutotestPrivateOverrideLobsterResponseForTestingFunction() = default;
+
+AutotestPrivateOverrideLobsterResponseForTestingFunction::
+    ~AutotestPrivateOverrideLobsterResponseForTestingFunction() = default;
+
+ExtensionFunction::ResponseAction
+AutotestPrivateOverrideLobsterResponseForTestingFunction::Run() {
+  if (!ash::features::IsLobsterEnabled()) {
+    return RespondNow(WithArguments(false));
+  }
+
+  LobsterService* lobster_service = LobsterServiceProvider::GetForProfile(
+      ash::ProfileHelper::Get()->GetProfileByUser(
+          user_manager::UserManager::Get()->GetActiveUser()));
+
+  return RespondNow(WithArguments(
+      lobster_service != nullptr
+          ? lobster_service->OverrideLobsterImageProviderForTesting()
+          : false));  // IN-TEST
 }
 
 ///////////////////////////////////////////////////////////////////////////////
