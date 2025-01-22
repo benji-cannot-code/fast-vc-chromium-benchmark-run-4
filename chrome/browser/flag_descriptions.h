f18fba734979ab746b3067e4e9417d73b4974fb0
Shakti Sahu <shaktisahu@chromium.org>
2025-01-21 23:43:13 -0800

    Cleanup feature flag DownloadsMigrateToJobsAPI
    This CL removes the feature DownloadsMigrateToJobsAPI and the
    corresponding cached flag.
    
    Bug: 387328232
    Change-Id: Id635863d1193fc220ce75bfae9b49da4f7a94fba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6183875
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409489}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -934,9 +934,6 @@ extern const char kEnableManagedConfigurationWebApiDescription[];
 extern const char kDownloadNotificationServiceUnifiedAPIName[];
 extern const char kDownloadNotificationServiceUnifiedAPIDescription[];
 
-extern const char kDownloadsMigrateToJobsAPIName[];
-extern const char kDownloadsMigrateToJobsAPIDescription[];
-
 extern const char kEnablePerfettoSystemTracingName[];
 extern const char kEnablePerfettoSystemTracingDescription[];
 
