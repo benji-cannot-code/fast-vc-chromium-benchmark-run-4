1b70dc86f8b8d16aeae69f6b96927b055cf48292
Zaina Al-Mashni <zalmashni@google.com>
2025-01-22 03:17:17 -0800

    [Quick Delete] Add CBD HaTS survey flag
    Bug: 40255099
    Change-Id: I75155a2d893d4bc8db24dd2bfc82d2e74e68ba78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187323
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409568}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -247,6 +247,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kChromeSharePageInfo,
     &kChromeSurveyNextAndroid,
     &kClankStartupLatencyInjection,
+    &kClearBrowsingDataAndroidSurvey,
     &kCommandLineOnNonRooted,
     &kContextMenuTranslateWithGoogleLens,
     &kContextMenuSysUiMatchesActivity,
@@ -719,6 +720,10 @@ BASE_FEATURE(kChromeSurveyNextAndroid,
 
 BASE_FEATURE(kClankStartupLatencyInjection,
              "ClankStartupLatencyInjection",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kClearBrowsingDataAndroidSurvey,
+             "ClearBrowsingDataAndroidSurvey",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCommandLineOnNonRooted,
--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -87,6 +87,7 @@ BASE_DECLARE_FEATURE(kChromeShareScreenshot);
 BASE_DECLARE_FEATURE(kChromeSharingHubLaunchAdjacent);
 BASE_DECLARE_FEATURE(kChromeSurveyNextAndroid);
 BASE_DECLARE_FEATURE(kClankStartupLatencyInjection);
+BASE_DECLARE_FEATURE(kClearBrowsingDataAndroidSurvey);
 BASE_DECLARE_FEATURE(kCommandLineOnNonRooted);
 BASE_DECLARE_FEATURE(kContextMenuSysUiMatchesActivity);
 BASE_DECLARE_FEATURE(kContextMenuTranslateWithGoogleLens);
