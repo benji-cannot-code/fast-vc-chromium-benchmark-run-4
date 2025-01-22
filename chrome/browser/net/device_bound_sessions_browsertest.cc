0bfadea3e007eee3342b2876ccb61acd2293170e
Daniel Rubery <drubery@chromium.org>
2025-01-21 17:23:14 -0800

    Fix license in device_bound_sessions_browsertest.cc
    It's missing a period, which sets off the pre-upload checks. This CL
    also gives net/device_bound_sessions OWNERS ownership over this test
    file.
    
    Change-Id: I2c584b593fa1306d8a581898f1279bf2471b46bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179109
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409397}

--- chrome/browser/net/device_bound_sessions_browsertest.cc
+++ chrome/browser/net/device_bound_sessions_browsertest.cc
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file
+// found in the LICENSE file.
 
 #include "base/functional/callback.h"
 #include "base/json/json_writer.h"
