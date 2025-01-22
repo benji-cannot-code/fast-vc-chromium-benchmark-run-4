a1912149643a67a39105cd682ede518f2cf1cab0
Georg Neis <neis@chromium.org>
2025-01-21 17:03:20 -0800

    policy: Remove Lacros fields from DeviceStatusReportRequest
    Namely lacros_browser_report and is_lacros_primary_browser.
    Lacros is gone, so they are obsolete.
    
    Bug: b:354842935
    Change-Id: Iad42d2839c2df74aaec3ce718f7e4c2004aaf6d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166585
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409392}

--- chrome/browser/ash/policy/status_collector/device_status_collector_browsertest.cc
+++ chrome/browser/ash/policy/status_collector/device_status_collector_browsertest.cc
@@ -1810,7 +1810,6 @@ TEST_F(DeviceStatusCollectorTest, VersionInfo) {
   // Expect the version info to be reported by default.
   GetStatus();
   EXPECT_TRUE(device_status_.has_browser_version());
-  EXPECT_TRUE(device_status_.has_is_lacros_primary_browser());
   EXPECT_TRUE(device_status_.has_channel());
   EXPECT_TRUE(device_status_.has_os_version());
   EXPECT_TRUE(device_status_.has_firmware_version());
@@ -1824,7 +1823,6 @@ TEST_F(DeviceStatusCollectorTest, VersionInfo) {
       ->set_status(::tpm_manager::STATUS_DBUS_ERROR);
   GetStatus();
   EXPECT_TRUE(device_status_.has_browser_version());
-  EXPECT_TRUE(device_status_.has_is_lacros_primary_browser());
   EXPECT_TRUE(device_status_.has_channel());
   EXPECT_TRUE(device_status_.has_os_version());
   EXPECT_TRUE(device_status_.has_firmware_version());
@@ -1841,7 +1839,6 @@ TEST_F(DeviceStatusCollectorTest, VersionInfo) {
       ash::kReportDeviceVersionInfo, false);
   GetStatus();
   EXPECT_FALSE(device_status_.has_browser_version());
-  EXPECT_FALSE(device_status_.has_is_lacros_primary_browser());
   EXPECT_FALSE(device_status_.has_channel());
   EXPECT_FALSE(device_status_.has_os_version());
   EXPECT_FALSE(device_status_.has_firmware_version());
@@ -1852,7 +1849,6 @@ TEST_F(DeviceStatusCollectorTest, VersionInfo) {
       ash::kReportDeviceVersionInfo, true);
   GetStatus();
   EXPECT_TRUE(device_status_.has_browser_version());
-  EXPECT_TRUE(device_status_.has_is_lacros_primary_browser());
   EXPECT_TRUE(device_status_.has_channel());
   EXPECT_TRUE(device_status_.has_os_version());
   EXPECT_TRUE(device_status_.has_firmware_version());
