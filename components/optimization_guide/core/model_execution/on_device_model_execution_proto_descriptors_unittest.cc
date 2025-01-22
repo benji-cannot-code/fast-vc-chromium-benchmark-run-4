84604f2f47f29cb2f3dded6e1c74e33531d23cba
Steven Holte <holte@google.com>
2025-01-22 15:54:07 -0800

    Add generated methods for modifying message fields.
    Bug: 390713803
    
    Change-Id: Ic88c878d09548e404e23a5f5820e484ced78ec38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182706
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1410015}

--- components/optimization_guide/core/model_execution/on_device_model_execution_proto_descriptors_unittest.cc
+++ components/optimization_guide/core/model_execution/on_device_model_execution_proto_descriptors_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_execution/on_device_model_execution_proto_descriptors.h"
 
 #include "components/optimization_guide/proto/features/compose.pb.h"
+#include "components/optimization_guide/proto/features/tab_organization.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace optimization_guide {
@@ -22,6 +23,31 @@ TEST(ProtoDescriptorsTest, SetStringFieldError) {
   proto::ComposeRequest req;
   EXPECT_EQ(SetProtoField(req.mutable_generate_params(), 999, "foo"),
             ProtoStatus::kError);
+}
+
+TEST(ProtoDescriptorsTest, MutableMessageField) {
+  proto::ComposeRequest req;
+  EXPECT_EQ(SetProtoField(GetProtoMutableMessage(&req, 7), 1, "foo"),
+            ProtoStatus::kOk);
+  EXPECT_EQ(req.mutable_generate_params()->user_input(), "foo");
+}
+
+TEST(ProtoDescriptorsTest, MutableMessageError) {
+  proto::ComposeRequest req;
+  EXPECT_EQ(GetProtoMutableMessage(&req, 999), nullptr);
+}
+
+TEST(ProtoDescriptorsTest, MutableRepeatedMessageField) {
+  proto::TabOrganizationRequest req;
+  EXPECT_EQ(AddProtoMessage(&req, 1), 1);
+  SetProtoField(GetProtoMutableRepeatedMessage(&req, 1, 0), 2, "mytitle");
+  EXPECT_EQ(req.tabs(0).title(), "mytitle");
+}
+
+TEST(ProtoDescriptorsTest, MutableRepeatedMessageError) {
+  proto::TabOrganizationRequest req;
+  EXPECT_EQ(AddProtoMessage(&req, 999), 0);
+  EXPECT_EQ(GetProtoMutableRepeatedMessage(&req, 1, 0), nullptr);
 }
 
 }  // namespace
