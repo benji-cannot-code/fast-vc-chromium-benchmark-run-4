966f97cb36df57d2e11b44e285ec801b95e47d3a
Riley Tatum <rtatum@google.com>
2025-01-23 11:07:26 -0800

    [NTP][Enterprise] Make MicrosoftAuthService observable
    Makes the MicrosoftAuthServiceObserver so that the New Tab Page
    can observe changes in auth state later.
    
    Bug: 386390488
    Change-Id: Ie673ccfa9199490c54c2f573853d3afac8acd0aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6184448
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410470}

--- chrome/browser/new_tab_page/microsoft_auth/microsoft_auth_service.h
+++ chrome/browser/new_tab_page/microsoft_auth/microsoft_auth_service.h
@@ -8,8 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/observer_list.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ui/webui/ntp_microsoft_auth/ntp_microsoft_auth_untrusted_ui.mojom.h"
 #include "components/keyed_service/core/keyed_service.h"
+
+class MicrosoftAuthServiceObserver;
 
 // Manages microsoft auth access token and its status for the Desktop NTP.
 class MicrosoftAuthService : public KeyedService {
@@ -24,12 +28,18 @@ class MicrosoftAuthService : public KeyedService {
   std::string GetAccessToken();
   virtual new_tab_page::mojom::AuthState GetAuthState();
 
+  // Adds/Removes MicrosoftAuthServiceObserver observers.
+  virtual void AddObserver(MicrosoftAuthServiceObserver* observer);
+  void RemoveObserver(MicrosoftAuthServiceObserver* observer);
+
  private:
   void CheckAccessTokenExpiration();
+  void NotifyObservers();
 
   new_tab_page::mojom::AccessTokenPtr access_token_ =
       new_tab_page::mojom::AccessToken::New();
   new_tab_page::mojom::AuthState state_ = new_tab_page::mojom::AuthState::kNone;
+  base::ObserverList<MicrosoftAuthServiceObserver> observers_;
 };
 
 #endif  // CHROME_BROWSER_NEW_TAB_PAGE_MICROSOFT_AUTH_MICROSOFT_AUTH_SERVICE_H_
--- chrome/browser/new_tab_page/microsoft_auth/microsoft_auth_service_unittest.cc
+++ chrome/browser/new_tab_page/microsoft_auth/microsoft_auth_service_unittest.cc
@@ -6,12 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/new_tab_page/microsoft_auth/microsoft_auth_service.h"
 
 #include "chrome/browser/new_tab_page/microsoft_auth/microsoft_auth_service_factory.h"
+#include "chrome/browser/new_tab_page/microsoft_auth/microsoft_auth_service_observer.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
+
+class MockMicrosoftAuthServiceObserver : public MicrosoftAuthServiceObserver {
+ public:
+  MOCK_METHOD0(OnAuthStateUpdated, void());
+};
 
 std::unique_ptr<TestingProfile> MakeTestingProfile() {
   TestingProfile::Builder profile_builder;
@@ -29,10 +35,15 @@ class MicrosoftAuthServiceTest : public testing::Test {
 
   ~MicrosoftAuthServiceTest() override = default;
 
+  void SetUp() override { scoped_observation_.Observe(auth_service_); }
+
+  void TearDown() override { scoped_observation_.Reset(); }
+
   MicrosoftAuthService& auth_service() { return *auth_service_; }
   content::BrowserTaskEnvironment& task_environment() {
     return task_environment_;
   }
+  MockMicrosoftAuthServiceObserver& observer() { return observer_; }
 
  private:
   // NOTE: The initialization order of these members matters.
@@ -40,9 +51,13 @@ class MicrosoftAuthServiceTest : public testing::Test {
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   std::unique_ptr<TestingProfile> profile_;
   raw_ptr<MicrosoftAuthService> auth_service_;
+  MockMicrosoftAuthServiceObserver observer_;
+  base::ScopedObservation<MicrosoftAuthService, MicrosoftAuthServiceObserver>
+      scoped_observation_{&observer_};
 };
 
 TEST_F(MicrosoftAuthServiceTest, SetAccessToken) {
+  EXPECT_CALL(observer(), OnAuthStateUpdated).Times(1);
   new_tab_page::mojom::AccessTokenPtr access_token =
       new_tab_page::mojom::AccessToken::New();
   access_token->token = "1234";
@@ -52,6 +67,15 @@ TEST_F(MicrosoftAuthServiceTest, SetAccessToken) {
   EXPECT_EQ(auth_service().GetAccessToken(), "1234");
   EXPECT_EQ(auth_service().GetAuthState(),
             new_tab_page::mojom::AuthState::kSuccess);
+}
+
+TEST_F(MicrosoftAuthServiceTest, GetExpiredAccessToken) {
+  EXPECT_CALL(observer(), OnAuthStateUpdated).Times(2);
+  new_tab_page::mojom::AccessTokenPtr access_token =
+      new_tab_page::mojom::AccessToken::New();
+  access_token->token = "1234";
+  access_token->expiration = base::Time::Now() + base::Minutes(5);
+  auth_service().SetAccessToken(std::move(access_token));
 
   // Wait for 30 seconds before expiration.
   task_environment().AdvanceClock(base::Minutes(4.5));
@@ -65,7 +89,16 @@ TEST_F(MicrosoftAuthServiceTest, GetEmptyAccessToken) {
   EXPECT_TRUE(auth_service().GetAccessToken().empty());
 }
 
+TEST_F(MicrosoftAuthServiceTest, SetAuthStateError) {
+  EXPECT_CALL(observer(), OnAuthStateUpdated).Times(1);
+  auth_service().SetAuthStateError();
+
+  EXPECT_EQ(auth_service().GetAuthState(),
+            new_tab_page::mojom::AuthState::kError);
+}
+
 TEST_F(MicrosoftAuthServiceTest, GetAuthState) {
+  EXPECT_CALL(observer(), OnAuthStateUpdated).Times(2);
   EXPECT_EQ(auth_service().GetAuthState(),
             new_tab_page::mojom::AuthState::kNone);
 
