3b55be3e71d835daccbe9f421df202458488ea34
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 06:14:01 -0800

    [cleanup] clang-format //ios/chrome/browser/ntp_tiles
    Produced by:
      git ls-files ios/chrome/browser/ntp_tiles/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I581a0d0d214ffcc5536d902548c6547102acfd72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190389
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410290}

--- ios/chrome/browser/ntp_tiles/model/most_visited_sites_observer_bridge.h
+++ ios/chrome/browser/ntp_tiles/model/most_visited_sites_observer_bridge.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Observes MostVisitedSites events from Objective-C. To use as a
 // ntp_tiles::MostVisitedSites::Observer, wrap in a
 // MostVisitedSitesObserverBridge.
-@protocol MostVisitedSitesObserving<NSObject>
+@protocol MostVisitedSitesObserving <NSObject>
 
 // Invoked by ntp_tiles::MostVisitedSites::Observer::OnMostVisitedURLsAvailable.
 - (void)onMostVisitedURLsAvailable:
