d6b055d0ad336b6d5025f157f7737c5ea67d0ec2
Kim Jablonski <kimjablonski@google.com>
2025-01-23 05:06:28 -0800

    Add DwaRenderFrameObserver to enable Dwa recorder calls on page loads.
    DWA needs to upload the events it records to enable metric collection. In accordance with the Chrome Privacy team requirements, we upload all collected metrics whenever a tab finishes loading. Specifically, DWA adds events in each page load to a collection of "page load events", this is done to prevent the ability of deducing a user's browsing history by combining metrics from different web pages in the same collection.
    
    Bug: 369473036
    Change-Id: I2a0cad4e6b6ec1369ea1534e6498bc2fb95d8175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012619
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Kim Jablonski <kimjablonski@google.com>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410258}

--- chrome/browser/ui/tabs/tab_features.cc
+++ chrome/browser/ui/tabs/tab_features.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon/content/content_favicon_driver.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.h"
 #include "components/image_fetcher/core/image_fetcher_service.h"
+#include "components/metrics/content/dwa_web_contents_observer.h"
 #include "components/permissions/permission_indicators_tab_data.h"
 
 #if BUILDFLAG(ENABLE_GLIC)
@@ -144,6 +145,10 @@ void TabFeatures::Init(TabInterface& tab, Profile* profile) {
 
     privacy_sandbox_tab_observer_ =
         std::make_unique<privacy_sandbox::PrivacySandboxTabObserver>(
+            tab.GetContents());
+
+    dwa_web_contents_observer_ =
+        std::make_unique<metrics::DwaWebContentsObserver>(
             tab.GetContents());
 
     tab_groups::TabGroupSyncService* tab_group_sync_service =
@@ -285,6 +290,13 @@ void TabFeatures::WillDiscardContents(tabs::TabInterface* tab,
     privacy_sandbox_tab_observer_.reset();
     privacy_sandbox_tab_observer_ =
         std::make_unique<privacy_sandbox::PrivacySandboxTabObserver>(
+            tab->GetContents());
+  }
+
+  if (dwa_web_contents_observer_) {
+    dwa_web_contents_observer_.reset();
+    dwa_web_contents_observer_ =
+        std::make_unique<metrics::DwaWebContentsObserver>(
             tab->GetContents());
   }
 
---
+++ components/metrics/content/dwa_web_contents_observer.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/metrics/content/dwa_web_contents_observer.h"
+
+#include "components/metrics/dwa/dwa_recorder.h"
+
+namespace metrics {
+
+DwaWebContentsObserver::DwaWebContentsObserver(
+    content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents),
+      content::WebContentsUserData<DwaWebContentsObserver>(*web_contents) {}
+
+DwaWebContentsObserver::~DwaWebContentsObserver() = default;
+
+void DwaWebContentsObserver::DidStopLoading() {
+  dwa::DwaRecorder::Get()->OnPageLoad();
+}
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(DwaWebContentsObserver);  // nocheck
+
+}  // namespace metrics
