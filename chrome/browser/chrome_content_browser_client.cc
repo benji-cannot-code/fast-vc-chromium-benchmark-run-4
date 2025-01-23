7248c549334116e0d69eee394f147815a8ce4c3b
Nwokedi Idika <nwokedi@chromium.org>
2025-01-23 06:12:14 -0800

    [URT] Guard ReferringAppInfo Use with ESB Enablement
    Bug: b/265345610
    Change-Id: Id4888096902f996867026cb7e205f2629880e8ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6191111
    Commit-Queue: Nwokedi Idika <nwokedi@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410289}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -5883,7 +5883,8 @@ ChromeContentBrowserClient::MaybeCreateSafeBrowsingURLLoaderThrottle(
   std::optional<safe_browsing::internal::ReferringAppInfo> referring_app_info =
       std::nullopt;
 #if BUILDFLAG(IS_ANDROID)
-  if (base::FeatureList::IsEnabled(
+  if (safe_browsing::IsEnhancedProtectionEnabled(*profile->GetPrefs()) &&
+      base::FeatureList::IsEnabled(
           safe_browsing::kAddReferringAppInfoToProtegoPings)) {
     WebContents* web_contents = wc_getter.Run();
     if (web_contents) {
