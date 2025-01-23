af914341a7c2e0eab73cc0e181bab0b04da9b425
rajendrant <rajendrant@chromium.org>
2025-01-23 15:00:50 -0800

    Change contextual cueing opt type
    Manual tested that nudge is shown with new opt type.
    
    Change-Id: I7f6c19d161670888bacd9cbbc2558b14d79a16e9
    Bug: 389746704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194078
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410576}

--- chrome/browser/contextual_cueing/contextual_cueing_helper.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_execution/model_execution_features_controller.h"
 #include "components/optimization_guide/core/optimization_guide_decider.h"
 #include "components/optimization_guide/core/optimization_metadata.h"
-#include "components/optimization_guide/proto/icon_view_metadata.pb.h"
+#include "components/optimization_guide/proto/contextual_cueing_metadata.pb.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
@@ -29,6 +29,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace contextual_cueing {
 
+namespace {
+
+// Returns whether the `config` matches all the current cueing condition.
+bool DidMatchCueingConditions(
+    const optimization_guide::proto::GlicCueingConfiguration& config) {
+  for (const auto& condition : config.conditions()) {
+    switch (condition.signal()) {
+      case optimization_guide::proto::
+          CONTEXTUAL_CUEING_CLIENT_SIGNAL_UNSPECIFIED:
+      case optimization_guide::proto::
+          CONTEXTUAL_CUEING_CLIENT_SIGNAL_PDF_PAGE_COUNT:
+      case optimization_guide::proto::
+          CONTEXTUAL_CUEING_CLIENT_SIGNAL_CONTENT_LENGTH_WORD_COUNT:
+        // TODO: crbug.com/389751174 - Implement checking the client signals.
+        return false;
+    }
+  }
+  return true;
+}
+
+}  // namespace
+
 ContextualCueingHelper::ContextualCueingHelper(
     content::WebContents* web_contents,
     OptimizationGuideKeyedService* ogks)
@@ -36,7 +58,7 @@ ContextualCueingHelper::ContextualCueingHelper(
       content::WebContentsUserData<ContextualCueingHelper>(*web_contents),
       optimization_guide_keyed_service_(ogks) {
   optimization_guide_keyed_service_->RegisterOptimizationTypes(
-      {optimization_guide::proto::OPTIMIZATION_GUIDE_ICON_VIEW});
+      {optimization_guide::proto::GLIC_CONTEXTUAL_CUEING});
 }
 
 ContextualCueingHelper::~ContextualCueingHelper() = default;
@@ -57,13 +79,19 @@ void ContextualCueingHelper::DocumentOnLoadCompletedInPrimaryMainFrame() {
   optimization_guide::OptimizationMetadata metadata;
   auto decision = optimization_guide_keyed_service_->CanApplyOptimization(
       web_contents()->GetLastCommittedURL(),
-      optimization_guide::proto::OPTIMIZATION_GUIDE_ICON_VIEW, &metadata);
+      optimization_guide::proto::GLIC_CONTEXTUAL_CUEING, &metadata);
   if (decision == optimization_guide::OptimizationGuideDecision::kTrue &&
       !metadata.empty()) {
     auto parsed = metadata.ParsedMetadata<
-        optimization_guide::proto::OptimizationGuideIconViewMetadata>();
-    if (parsed->has_cue_label()) {
-      last_navigation_cue_label_ = parsed->cue_label();
+        optimization_guide::proto::GlicContextualCueingMetadata>();
+
+    for (const auto& config : parsed->cueing_configurations()) {
+      if (!config.has_cue_label()) {
+        continue;
+      }
+      if (DidMatchCueingConditions(config)) {
+        last_navigation_cue_label_ = config.cue_label();
+      }
     }
   }
 
