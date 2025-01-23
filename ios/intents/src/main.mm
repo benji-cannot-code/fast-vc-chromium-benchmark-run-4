1288c724e2e083e357def2b04465de0ef9daa21d
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 04:25:20 -0800

    [cleanup] clang-format //ios/intents
    Produced by:
      git ls-files ios/intents/'*'.{cc,h,mm,m}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: If020f4e04240855f62bc782e837c31ed90e8614f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190394
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410240}

--- ios/intents/src/main.mm
+++ ios/intents/src/main.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import <UIKit/UIKit.h>
+
 #import "ios/intents/src/AppDelegate.h"
 
 int main(int argc, char** argv) {
