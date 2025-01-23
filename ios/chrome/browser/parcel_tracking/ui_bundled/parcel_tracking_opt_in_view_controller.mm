436e22bda761f2baaadecf4b58a400ddbc4892c0
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 06:00:29 -0800

    [cleanup] clang-format //ios/chrome/browser/parcel_tracking
    Produced by:
      git ls-files \
          ios/chrome/browser/parcel_tracking/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I40cf5567ae6721e8e4398c13136ddee0765ec8f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190807
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410282}

--- ios/chrome/browser/parcel_tracking/ui_bundled/parcel_tracking_opt_in_view_controller.mm
+++ ios/chrome/browser/parcel_tracking/ui_bundled/parcel_tracking_opt_in_view_controller.mm
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/notreached.h"
 #import "ios/chrome/browser/parcel_tracking/parcel_tracking_opt_in_status.h"
+#import "ios/chrome/browser/parcel_tracking/ui_bundled/parcel_tracking_opt_in_view_controller_delegate.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_item.h"
-#import "ios/chrome/browser/parcel_tracking/ui_bundled/parcel_tracking_opt_in_view_controller_delegate.h"
 #import "ios/chrome/common/string_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
