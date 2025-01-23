6edd35772de6b0801d141bf82bbb94cbbd0267a8
ahmedmoussa <ahmedmoussa@google.com>
2025-01-23 11:05:24 -0800

    Remove SetConfiguration() from VideoEffectsManager mojo Interface
    Currently, SetConfiguration() is only being used from the browser
    process. So, a mojom connection is not needed.
    
    This would make VideoEffectsManager as a readonly interface, allowing
    its usage with services that shouldn't have edit access to
    configurations (e.g. video capture service, effects service).
    
    Going to rename as ReadonlyVideoEffectsManager in a follow-up CL.
    
    Bug: 377532863
    Change-Id: I4fb8f4ffd53605ea51a2584fdffa5ac40e516f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181011
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410467}

--- media/capture/video/video_capture_device_client_unittest.cc
+++ media/capture/video/video_capture_device_client_unittest.cc
@@ -71,9 +71,6 @@ std::unique_ptr<VideoCaptureJpegDecoder> ReturnNullPtrAsJpecDecoder() {
 
 class FakeVideoEffectsManagerImpl : public media::mojom::VideoEffectsManager {
   void GetConfiguration(GetConfigurationCallback callback) override {}
-  void SetConfiguration(
-      media::mojom::VideoEffectsConfigurationPtr configuration,
-      SetConfigurationCallback callback) override {}
   void AddObserver(
       ::mojo::PendingRemote<media::mojom::VideoEffectsConfigurationObserver>
           observer) override {}
