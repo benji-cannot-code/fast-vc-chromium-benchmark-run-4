b7c8685eb1df364a751ec5c9b8f9f2feeb39e9ec
Rouslan Solomakhin <rouslan@chromium.org>
2025-01-23 07:51:26 -0800

    Move the SPC no-credentials UI to a Chrome build target
    Before this patch, core PaymentRequest build target depended on the SPC
    (Secure Payment Confirmation) no-credentials UI, which prevented build
    targets that cannot have UI (e.g., WebView) from depending on the core
    PaymentRequest build target.
    
    This patch moves the SPC no-credentials UI from the core PaymentRequest
    build target to the Chrome-specific implementation of PaymentRequest.
    
    After this patch, core PaymentRequest code is more decoupled from the UI
    code.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED The refactored code path is covered by `android_browsertests` but the coverage bots do not pick that up.
    Fixed: 383334934
    Change-Id: I8001a8ffdeac253cf5988b3d87b94c442cb1d9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6191688
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410349}

--- chrome/test/android/test_support/src/org/chromium/chrome/test_support/PaymentRequestTestBridge.java
+++ chrome/test/android/test_support/src/org/chromium/chrome/test_support/PaymentRequestTestBridge.java
@@ -330,7 +330,7 @@ public class PaymentRequestTestBridge {
         if (authnUi != null) return authnUi.cancelForTest();
 
         SecurePaymentConfirmationNoMatchingCredController noMatchingUi =
-                PaymentRequestService.getSecurePaymentConfirmationNoMatchingCredUiForTesting();
+                getSecurePaymentConfirmationNoMatchingCredUi();
         if (noMatchingUi != null) {
             noMatchingUi.closeForTest();
             return true;
@@ -344,7 +344,7 @@ public class PaymentRequestTestBridge {
         SecurePaymentConfirmationAuthnController authnUi = getSecurePaymentConfirmationAuthnUi();
         if (authnUi != null) return authnUi.optOutForTest();
         SecurePaymentConfirmationNoMatchingCredController noMatchingUi =
-                PaymentRequestService.getSecurePaymentConfirmationNoMatchingCredUiForTesting();
+                getSecurePaymentConfirmationNoMatchingCredUi();
         if (noMatchingUi != null) return noMatchingUi.optOutForTest();
         return false;
     }
@@ -356,6 +356,17 @@ public class PaymentRequestTestBridge {
                         PaymentRequestService.getBrowserPaymentRequestForTesting();
         return chromeService != null
                 ? chromeService.getSecurePaymentConfirmationAuthnUiForTesting()
+                : null;
+    }
+
+    @Nullable
+    private static SecurePaymentConfirmationNoMatchingCredController
+            getSecurePaymentConfirmationNoMatchingCredUi() {
+        ChromePaymentRequestService chromeService =
+                (ChromePaymentRequestService)
+                        PaymentRequestService.getBrowserPaymentRequestForTesting();
+        return chromeService != null
+                ? chromeService.getSecurePaymentConfirmationNoMatchingCredUiForTesting()
                 : null;
     }
 
