08297b2941c413ced9acb1654f0f1319c2a210b3
Amelie Schneider <amelies@google.com>
2025-01-23 12:55:19 -0800

    [UNO][A11y] Fix sign in promo accessibility announcement
    Changes the screen reader to announce that there is a dialog which is
    currently out of focus when the address sign in promo opens. Pressing
    Alt + Shift + A will focus the sign in button.
    
    Bug: 390641444
    Change-Id: I992afb8c0f35e8a80ea97faed602b7aa31159c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194733
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410514}

--- chrome/browser/ui/views/autofill/autofill_bubble_handler_impl.cc
+++ chrome/browser/ui/views/autofill/autofill_bubble_handler_impl.cc
@@ -207,7 +207,7 @@ AutofillBubbleBase* AutofillBubbleHandlerImpl::ShowAddressSignInPromo(
   }
 
   views::BubbleDialogDelegateView::CreateBubble(bubble);
-  bubble->ShowForReason(LocationBarBubbleDelegateView::USER_GESTURE);
+  bubble->ShowForReason(LocationBarBubbleDelegateView::AUTOMATIC);
   return bubble;
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
