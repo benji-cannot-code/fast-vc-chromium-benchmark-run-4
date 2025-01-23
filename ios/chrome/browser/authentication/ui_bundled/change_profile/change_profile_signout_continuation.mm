029d667bcb52223da2af328fecec9d95c9cc7ee4
Jood <jood@google.com>
2025-01-23 09:44:10 -0800

    [iOS] Fix crash when signing out from incognito
    Use GetOriginalProfile in MultiProfileSignout to make sure it works in
    incognito.
    
    Before this CL, signing out from incognito crashes in M133, as
    MultiProfileSignout is used.
    
    This CL needs to be merged back into M133.
    
    Bug: 391863244
    Change-Id: I2345cdc6f76c5f66e620cc27fff8cf513ed9e48c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190595
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410422}

--- ios/chrome/browser/authentication/ui_bundled/change_profile/change_profile_signout_continuation.mm
+++ ios/chrome/browser/authentication/ui_bundled/change_profile/change_profile_signout_continuation.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider_interface.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/public/commands/snackbar_commands.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
@@ -59,9 +60,12 @@ void ChangeProfileSignoutContinuation(
           .Then(std::move(signout_completion))
           .Then(std::move(closure));
 
+  // TODO(crbug.com/391863244): Long-term fix, the code that create the
+  // continuation should ensure that the method is not called with an incognito
+  // Browser.
   AuthenticationService* authentication_service =
-      AuthenticationServiceFactory::GetForProfile(browser->GetProfile());
-
+      AuthenticationServiceFactory::GetForProfile(
+          browser->GetProfile()->GetOriginalProfile());
   authentication_service->SignOut(signout_source_metric, force_clear_data,
                                   base::CallbackToBlock(std::move(completion)));
 
