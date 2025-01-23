1750bd788b48f15d412e40cb56b71b7c8482e5f8
sbingler <bingler@chromium.org>
2025-01-23 09:00:48 -0800

    Propagate is_top_level_nav to ShouldUpgradeToSSL
    Expose is_top_level_nav through ShouldUpgradeToSSL to its callers.
    
    As part of this CL preconnect HSTS upgrades will be disabled whenever
    kHstsTopLevelNavigationsOnly is active. This is a tentative decision
    and may be adjusted in future once we have more metrics data regarding
    preconnect's HSTS usage.
    
    The metric's collection is also modified to only occur when the feature
    is disabled. Since it's never been enabled yet this is effectively a
    no-op on metric collection and doesn't affect existing data.
    
    Bug: 40725781
    Change-Id: I68342c3792c3e119c44a44bfc401e3b89fe89d44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6186117
    Reviewed-by: mmenke <mmenke@chromium.org>
    Auto-Submit: Steven Bingler <bingler@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410390}

--- net/url_request/url_request_http_job_unittest.cc
+++ net/url_request/url_request_http_job_unittest.cc
@@ -1168,10 +1168,12 @@ TEST_F(URLRequestHttpJobTest, HSTSInternalRedirectTest) {
   // Setup HSTS state.
   context_->transport_security_state()->AddHSTS(
       "upgrade.test", base::Time::Now() + base::Seconds(10), true);
-  ASSERT_TRUE(
-      context_->transport_security_state()->ShouldUpgradeToSSL("upgrade.test"));
+  // Setting `is_top_level_nav` true prevents the upgrade from being blocked by
+  // kHstsTopLevelNavigationsOnly.
+  ASSERT_TRUE(context_->transport_security_state()->ShouldUpgradeToSSL(
+      "upgrade.test", /*is_top_level_nav=*/true));
   ASSERT_FALSE(context_->transport_security_state()->ShouldUpgradeToSSL(
-      "no-upgrade.test"));
+      "no-upgrade.test", /*is_top_level_nav=*/true));
 
   struct TestCase {
     const char* url;
@@ -1269,8 +1271,10 @@ TEST_F(URLRequestHttpJobTest, ShouldBypassHSTS) {
   // Setup HSTS state.
   context_->transport_security_state()->AddHSTS(
       "upgrade.test", base::Time::Now() + base::Seconds(30), true);
-  ASSERT_TRUE(
-      context_->transport_security_state()->ShouldUpgradeToSSL("upgrade.test"));
+  // Setting `is_top_level_nav` true prevents the upgrade from being blocked by
+  // kHstsTopLevelNavigationsOnly.
+  ASSERT_TRUE(context_->transport_security_state()->ShouldUpgradeToSSL(
+      "upgrade.test", /*is_top_level_nav=*/true));
 
   struct TestCase {
     const char* url;
@@ -1559,8 +1563,10 @@ TEST_F(URLRequestHttpJobTest, ShouldBypassHSTSResponseAndConnectionNotReused) {
   // The host of all EmbeddedTestServer URLs is 127.0.0.1.
   context->transport_security_state()->AddHSTS(
       "127.0.0.1", base::Time::Now() + base::Seconds(30), true);
-  ASSERT_TRUE(
-      context->transport_security_state()->ShouldUpgradeToSSL("127.0.0.1"));
+  // Setting `is_top_level_nav` true prevents the upgrade from being blocked by
+  // kHstsTopLevelNavigationsOnly.
+  ASSERT_TRUE(context->transport_security_state()->ShouldUpgradeToSSL(
+      "127.0.0.1", /*is_top_level_nav=*/true));
 
   GURL::Replacements replace_scheme;
   replace_scheme.SetSchemeStr("https");
@@ -1650,8 +1656,10 @@ TEST_F(URLRequestHttpJobTest, HSTSInternalRedirectCallback) {
   auto context = CreateTestURLRequestContextBuilder()->Build();
   context->transport_security_state()->AddHSTS(
       "127.0.0.1", base::Time::Now() + base::Seconds(10), true);
-  ASSERT_TRUE(
-      context->transport_security_state()->ShouldUpgradeToSSL("127.0.0.1"));
+  // Setting `is_top_level_nav` true prevents the upgrade from being blocked by
+  // kHstsTopLevelNavigationsOnly.
+  ASSERT_TRUE(context->transport_security_state()->ShouldUpgradeToSSL(
+      "127.0.0.1", /*is_top_level_nav=*/true));
 
   GURL::Replacements replace_scheme;
   replace_scheme.SetSchemeStr("http");
