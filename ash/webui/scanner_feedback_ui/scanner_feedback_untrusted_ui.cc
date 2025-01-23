6fc65c42a00918c65e6cad32539a604edef77f5b
Michael Cui <mlcui@google.com>
2025-01-22 20:35:32 -0800

    scanner: Add strings to feedback dialog
    Fixed: b:391035831
    Change-Id: I6a6a21004dc73841be4aacb02c7ad00478dc71f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6192308
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Auto-Submit: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410110}

--- ash/webui/scanner_feedback_ui/scanner_feedback_untrusted_ui.cc
+++ ash/webui/scanner_feedback_ui/scanner_feedback_untrusted_ui.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "base/unguessable_token.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -179,6 +180,21 @@ ScannerFeedbackUntrustedUI::ScannerFeedbackUntrustedUI(content::WebUI* web_ui)
   untrusted_source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::StyleSrc,
       "style-src-elem 'self' theme;");
+  untrusted_source->AddLocalizedStrings(
+      {{"linkAriaLabel", IDS_MAKO_FEEDBACK_LINK_ARIA_LABEL},
+       {"title", IDS_MAKO_FEEDBACK_TITLE},
+       {"subtitle", IDS_MAKO_FEEDBACK_SUBTITLE},
+       {"question", IDS_MAKO_FEEDBACK_QUESTION},
+       {"questionPlaceholder", IDS_MAKO_FEEDBACK_QUESTION_PLACEHOLDER},
+       {"offensiveOrUnsafe", IDS_MAKO_FEEDBACK_OFFENSIVE_OR_UNSAFE},
+       {"factuallyIncorrect", IDS_MAKO_FEEDBACK_FACTUALLY_INCORRECT},
+       {"legalIssue", IDS_MAKO_FEEDBACK_LEGAL_ISSUE},
+       {"feedbackDisclaimer", IDS_MAKO_FEEDBACK_FEEDBACK_DISCLAIMER},
+       {"privacyPolicy", IDS_MAKO_FEEDBACK_PRIVACY_POLICY},
+       {"termsOfService", IDS_MAKO_FEEDBACK_TERMS_OF_SERVICE},
+       {"cancel", IDS_MAKO_FEEDBACK_CANCEL},
+       {"send", IDS_MAKO_FEEDBACK_SEND}});
+  untrusted_source->UseStringsJs();
 
   untrusted_source->SetRequestFilter(
       base::BindRepeating(&ShouldHandleRequest, browser_context->GetWeakPtr()),
