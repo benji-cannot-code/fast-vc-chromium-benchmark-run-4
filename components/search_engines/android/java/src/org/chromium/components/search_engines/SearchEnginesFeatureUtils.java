33660bc1604bc06003a001404a84e7fad5d62d09
Nicolas Dossou-Gbete <dgn@chromium.org>
2025-01-23 03:05:39 -0800

    clay: Force-disable verbose logging
    Follow-ups will actually remove the unreachable code.
    
    Bug: 391570180
    Change-Id: I4819c43c17e7455570e11cc8d354535d6efec284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190395
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410213}

--- components/search_engines/android/java/src/org/chromium/components/search_engines/SearchEnginesFeatureUtils.java
+++ components/search_engines/android/java/src/org/chromium/components/search_engines/SearchEnginesFeatureUtils.java
@@ -46,7 +46,8 @@ public final class SearchEnginesFeatureUtils {
      * <p>This param is surfaced in {@code chrome://flags}.
      */
     public static boolean clayBlockingEnableVerboseLogging() {
-        return clayBlockingFeatureParamAsBoolean("enable_verbose_logging", true);
+        // TODO(crbug.com/391570180): Finish cleaning up this flag once usage is removed.
+        return false;
     }
 
     /**
