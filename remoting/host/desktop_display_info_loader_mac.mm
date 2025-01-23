877455dc86500f3349e77ca774734910032b7314
Lambros Lambrou <lambroslambrou@chromium.org>
2025-01-23 10:46:21 -0800

    [remoting] macOS implementation of display_name.
    This sets the VideoTrackLayout display_name to the NSScreen's
    localizedName property.
    
    Bug: 391566723
    Change-Id: I0d8a9080a84a3395628edd7187dce88504e885b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6191941
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410460}

--- remoting/host/desktop_display_info_loader_mac.mm
+++ remoting/host/desktop_display_info_loader_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Cocoa/Cocoa.h>
 
 #include "base/check.h"
+#include "base/strings/sys_string_conversions.h"
 
 namespace remoting {
 
@@ -41,11 +42,14 @@ DesktopDisplayInfo DesktopDisplayInfoLoaderMac::GetCurrentDisplayInfo() {
         static_cast<CGDirectDisplayID>([device[@"NSScreenNumber"] intValue]);
 
     NSRect bounds = screen.frame;
-    int x = bounds.origin.x;
-    int y = bounds.origin.y;
-    int height = bounds.size.height;
-
+    int32_t x = bounds.origin.x;
+    int32_t y = bounds.origin.y;
+    uint32_t width = bounds.size.width;
+    uint32_t height = bounds.size.height;
+    uint32_t dpi = kDefaultScreenDpi * screen.backingScaleFactor;
     bool is_default = false;
+    std::string display_name = base::SysNSStringToUTF8(screen.localizedName);
+
     if (i == 0) {
       DCHECK(x == 0);
       DCHECK(y == 0);
@@ -53,17 +57,11 @@ DesktopDisplayInfo DesktopDisplayInfoLoaderMac::GetCurrentDisplayInfo() {
       main_display_height = height;
     }
 
-    DisplayGeometry info;
-    info.id = id;
-    info.x = x;
     // Convert origin from lower left to upper left (based on main display).
-    info.y = main_display_height - y - height;
-    info.width = bounds.size.width;
-    info.height = height;
-    info.dpi = (int)(kDefaultScreenDpi * screen.backingScaleFactor);
-    info.bpp = 24;
-    info.is_default = is_default;
-    result.AddDisplay(info);
+    y = main_display_height - y - height;
+
+    result.AddDisplay({id, x, y, width, height, dpi,
+                       /*bpp=*/ 24, is_default, display_name});
   }
   return result;
 }
