d17b43b4b92e1e339e1990ca37efd17318cb7948
dpapad <dpapad@chromium.org>
2025-01-23 00:07:13 -0800

    WebUI: Use `click()` instead of `new Event('click')` in tests.
    The former is more robust as it closer simulates user input, for example
    it takes into account whether an element is disabled, which can surface
    more errors during tests. It is also the dominant pattern in WebUI
    tests, as well as shorter.
    
    This is in preparation of catching these cases via ESLint's
    no-restricted-syntax in a follow-up.
    
    Bug: 40519637
    Change-Id: I3fa8212e90578f3e1e879f811af81f2d7658500f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6188893
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410148}

--- chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_test.ts
@@ -119,9 +119,10 @@ suite('WallpaperSearchTest', () => {
       const learnMoreLink =
           wallpaperSearchElement.shadowRoot!.querySelector<HTMLAnchorElement>(
               '#disclaimer a')!;
-      const clickEvent = new Event('click', {cancelable: true});
-      learnMoreLink.dispatchEvent(clickEvent);
+      const whenClick = eventToPromise('click', learnMoreLink);
+      learnMoreLink.click();
       await handler.whenCalled('openHelpArticle');
+      const clickEvent = await whenClick;
       assertTrue(clickEvent.defaultPrevented);
     });
 
