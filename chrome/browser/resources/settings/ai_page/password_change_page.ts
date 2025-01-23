bf55c52c56820c2601eaf0496f57120a091b178a
Viktor Semeniuk <vsemeniuk@google.com>
2025-01-23 02:54:52 -0800

    Add password change subpage to settings
    This CL adds an entry point for password change feature. Subpage
    itself isn't yet implemented.
    
    Bug: 391131625
    Change-Id: I172d2c4f5d4b3b4dde0b75cf6d5b392f6000eb05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177284
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410205}

---
+++ chrome/browser/resources/settings/ai_page/password_change_page.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './password_change_page.html.js';
+
+export class PasswordChangeSubpageElement extends PolymerElement {
+  static get is() {
+    return 'settings-password-change-subpage';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'settings-password-change-subpage': PasswordChangeSubpageElement;
+  }
+}
+
+customElements.define(
+    PasswordChangeSubpageElement.is, PasswordChangeSubpageElement);
