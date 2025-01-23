13834049c2c1999fa07b25613a98b3d19465f609
Alison Gale <agale@chromium.org>
2025-01-23 15:50:40 -0800

    Add regression test for toast crash
    Confirmed that this test crashes with the fix reverted.
    
    Bug: 391270241
    Change-Id: I2869b3e9fcdc98ff44444586078e0a5792ff0853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194609
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1410613}

--- chrome/browser/ui/toasts/toast_controller_interactive_ui_test.cc
+++ chrome/browser/ui/toasts/toast_controller_interactive_ui_test.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/interactive_test.h"
 #include "ui/events/keycodes/keyboard_codes.h"
+#include "ui/gfx/animation/animation_test_api.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/controls/button/button.h"
@@ -578,4 +579,28 @@ IN_PROC_BROWSER_TEST_F(ToastControllerInteractiveTest,
           ->GetContentsWebView()
           ->GetBoundsInScreen();
   EXPECT_TRUE(web_view_bounds.Contains(toast_bounds));
+}
+
+// Regression test for http://crbug.com/383898425
+IN_PROC_BROWSER_TEST_F(ToastControllerInteractiveTest,
+                       HandlesReducedAnimation) {
+  ToastController* const toast_controller = GetToastController();
+  {
+    // Show toast while rich animations are disabled.
+    const gfx::AnimationTestApi::RenderModeResetter disable_rich_animations =
+        gfx::AnimationTestApi::SetRichAnimationRenderMode(
+            gfx::Animation::RichAnimationRenderMode::FORCE_DISABLED);
+    gfx::Animation::SetPrefersReducedMotionForTesting(true);
+    EXPECT_TRUE(gfx::Animation::PrefersReducedMotion());
+    EXPECT_TRUE(
+        toast_controller->MaybeShowToast(ToastParams(ToastId::kLinkCopied)));
+  }
+  {
+    // Animate out toast (due to new toast showing) after enabling animations.
+    const gfx::AnimationTestApi::RenderModeResetter disable_rich_animations =
+        gfx::AnimationTestApi::SetRichAnimationRenderMode(
+            gfx::Animation::RichAnimationRenderMode::FORCE_ENABLED);
+    EXPECT_TRUE(
+        toast_controller->MaybeShowToast(ToastParams(ToastId::kLinkCopied)));
+  }
 }
