7eb32f2c06ea8fef04bf7a41449f97b0c01b97fa
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 04:06:38 -0800

    [cleanup] clang-format //ios/chrome/browser/screen_time
    Produced by:
      git ls-files ios/chrome/browser/screen_time/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I20dcd8732d8c042433250fb611823ee8e4508459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190392
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410236}

--- ios/chrome/browser/screen_time/ui_bundled/screen_time_coordinator.mm
+++ ios/chrome/browser/screen_time/ui_bundled/screen_time_coordinator.mm
@@ -56,8 +56,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Private properties
 
 - (void)setMediator:(ScreenTimeMediator*)mediator {
-  if (_mediator == mediator)
+  if (_mediator == mediator) {
     return;
+  }
 
   if (_mediator) {
     [_mediator disconnect];
@@ -73,8 +74,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setScreenTimeViewController:
     (ScreenTimeViewController*)screenTimeViewController {
-  if (_screenTimeViewController == screenTimeViewController)
+  if (_screenTimeViewController == screenTimeViewController) {
     return;
+  }
 
   if (_screenTimeViewController) {
     [_screenTimeViewController willMoveToParentViewController:nil];
--- ios/chrome/browser/screen_time/ui_bundled/screen_time_mediator.mm
+++ ios/chrome/browser/screen_time/ui_bundled/screen_time_mediator.mm
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/screen_time/ui_bundled/screen_time_mediator.h"
 
+#import "ios/chrome/browser/screen_time/ui_bundled/screen_time_consumer.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/web_state_list/active_web_state_observation_forwarder.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer_bridge.h"
-#import "ios/chrome/browser/screen_time/ui_bundled/screen_time_consumer.h"
 #import "ios/web/public/web_state.h"
 #import "ios/web/public/web_state_observer_bridge.h"
 #import "net/base/apple/url_conversions.h"
