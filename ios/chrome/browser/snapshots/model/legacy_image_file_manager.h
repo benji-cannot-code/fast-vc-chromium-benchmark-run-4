d2915c2f7022b0eecb20fe262c89ab4893d5c8cf
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 07:04:59 -0800

    [cleanup] clang-format //ios/chrome/browser/snapshots
    Produced by:
      git ls-files ios/chrome/browser/snapshots/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: Ic86d2b6c8f847a30c9d07cf5bd713fd2002ae397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190889
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410319}

--- ios/chrome/browser/snapshots/model/legacy_image_file_manager.h
+++ ios/chrome/browser/snapshots/model/legacy_image_file_manager.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SNAPSHOTS_MODEL_LEGACY_IMAGE_FILE_MANAGER_H_
 #define IOS_CHROME_BROWSER_SNAPSHOTS_MODEL_LEGACY_IMAGE_FILE_MANAGER_H_
 
-#include <vector>
-
 #import <UIKit/UIKit.h>
+
+#include <vector>
 
 #import "base/functional/callback_forward.h"
 
--- ios/chrome/browser/snapshots/model/legacy_snapshot_storage.h
+++ ios/chrome/browser/snapshots/model/legacy_snapshot_storage.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SNAPSHOTS_MODEL_LEGACY_SNAPSHOT_STORAGE_H_
 #define IOS_CHROME_BROWSER_SNAPSHOTS_MODEL_LEGACY_SNAPSHOT_STORAGE_H_
 
-#include <vector>
-
 #import <UIKit/UIKit.h>
+
+#include <vector>
 
 class SnapshotID;
 namespace base {
