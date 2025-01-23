16780e6a2c927a16366862219faf655461c0cc87
Ivana Žužić <izuzic@google.com>
2025-01-23 04:54:10 -0800

    [Android][3PPWM] Add an integration test for the ContentProvider
    An integration should be able to catch more bugs than a unit test.
    An example is stated in a comment in crrev.com/c/6110456: there is no
    guarantee that Chrome flags can already be checked when the
    ContentProvider starts.
    
    Bug: 384485045
    Change-Id: I7d6b8c32af695b6f179f13ab07c95c8d8b4974a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6189118
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410252}

---
+++ chrome/browser/autofill/android/javatest/src/org/chromium/chrome/browser/autofill/AutofillThirdPartyModeContentProviderIntegrationTest.java
@@ -0,0 +1,99 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill;
+
+import static android.content.ContentResolver.SCHEME_CONTENT;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import static org.chromium.chrome.browser.autofill.AutofillThirdPartyModeContentProvider.AUTOFILL_THIRD_PARTY_MODE_ACTIONS_URI_PATH;
+import static org.chromium.chrome.browser.autofill.AutofillThirdPartyModeContentProvider.AUTOFILL_THIRD_PARTY_MODE_COLUMN;
+import static org.chromium.chrome.browser.autofill.AutofillThirdPartyModeContentProvider.createContentUri;
+
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.filters.MediumTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+
+/** Integration tests for the {@link AutofillThirdPartyModeContentProvider}. */
+@RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.PER_CLASS)
+@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+public class AutofillThirdPartyModeContentProviderIntegrationTest {
+    @Test
+    @MediumTest
+    public void testQueryThirdPartyModeOff() {
+        AutofillClientProviderUtils.setThirdPartyModePref(false);
+
+        Cursor cursor =
+                ApplicationProvider.getApplicationContext()
+                        .getContentResolver()
+                        .query(
+                                createContentUri(),
+                                new String[] {AUTOFILL_THIRD_PARTY_MODE_COLUMN},
+                                null,
+                                null,
+                                null);
+
+        cursor.moveToFirst();
+        int index = cursor.getColumnIndex(AUTOFILL_THIRD_PARTY_MODE_COLUMN);
+
+        // 0 means that the third party mode was off.
+        assertEquals(0, cursor.getInt(index));
+    }
+
+    @Test
+    @MediumTest
+    public void testQueryThirdPartyModeOn() {
+        AutofillClientProviderUtils.setThirdPartyModePref(true);
+
+        Cursor cursor =
+                ApplicationProvider.getApplicationContext()
+                        .getContentResolver()
+                        .query(
+                                createContentUri(),
+                                new String[] {AUTOFILL_THIRD_PARTY_MODE_COLUMN},
+                                null,
+                                null,
+                                null);
+
+        cursor.moveToFirst();
+        int index = cursor.getColumnIndex(AUTOFILL_THIRD_PARTY_MODE_COLUMN);
+
+        // 1 means that the third party mode was off.
+        assertEquals(1, cursor.getInt(index));
+    }
+
+    @Test
+    @MediumTest
+    public void testQueryWrongUrl() {
+        AutofillClientProviderUtils.setThirdPartyModePref(false);
+        Uri uri =
+                new Uri.Builder()
+                        .scheme(SCHEME_CONTENT)
+                        .authority(
+                                ApplicationProvider.getApplicationContext().getPackageName()
+                                        + ".NonExistentContentProvider")
+                        .path(AUTOFILL_THIRD_PARTY_MODE_ACTIONS_URI_PATH)
+                        .build();
+
+        Cursor cursor =
+                ApplicationProvider.getApplicationContext()
+                        .getContentResolver()
+                        .query(uri, null, null, null, null);
+        assertNull("Cursor should be null", cursor);
+    }
+}
