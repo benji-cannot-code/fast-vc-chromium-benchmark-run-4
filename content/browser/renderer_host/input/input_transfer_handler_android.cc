8ae8e4695bf6edc6efdbe7e66871f8645f2c5de2
Kartar Singh <kartarsingh@google.com>
2025-01-23 03:14:59 -0800

    Reland "Implement `canTransferInputToViz` method in InputTransferHandler."
    This reverts commit b74c9d1332b86cee4bd0fbc8526d34d7f8df1c15.
    
    Reason for revert: Relanding after fixing test failure.
    
    Original change's description:
    > Revert "Implement `canTransferInputToViz` method in InputTransferHandler."
    >
    > This reverts commit 9cac2cccdc9fde2d95a2fcc48a4d37987f0bdb0a.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5171646110367744
    >
    > Sample build with failed test: https://ci.chromium.org/b/8725208713731069633
    > Affected test(s):
    > [ninja://content/test:content_browsertests/InputOnVizBrowserTest.TransfersStateOnTouchDown](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fcontent%2Ftest:content_browsertests%2FInputOnVizBrowserTest.TransfersStateOnTouchDown?q=VHash%3A8f1d589e583b7f27)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5171646110367744&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6158625&type=BUG
    >
    > Original change's description:
    > > Implement `canTransferInputToViz` method in InputTransferHandler.
    > >
    > > We are by default transferring input except in following cases:
    > > - `mVizToken` is null: To handle cases where browser hasn't received the
    > >   viz side token yet, and a touch sequence might have started. This will
    > >   also cover the case of split window where Viz would never set viz
    > >   token for second window since we are only using a single
    > >   AInputReceiver for now due to an Android platform bug which prevents
    > >   us from calling AInputReceiver_release.
    > > - When text selection is active or IME is active, this is done to
    > >   prevent ordering issues between input and text selection/IME related
    > >   mojo methods.
    > > - When page is at top, this is added to allow overscroll/refresh effect
    > >   to be handled on Browser.
    > >
    > > P.S. We are also changing point where `input_transfer_handler_`
    > > intercepts events in RenderWidgetHostViewAndroid, this is to resolve
    > > crash happening due transferred events sitting in
    > > TouchDispositionGestureFilter and acks being expected to come for them.
    > > Call to `gesture_provider_.OnTouchEvent` was pushing events to
    > > TouchDispositionGestureFilter.
    > >
    > > i.e. input coming from system server directly instead of being injected
    > > to views.
    > >
    > > Low-Coverage-Reason: HARD_TO_TEST Requires testing touch transfer APIs
    > > Bug: 370506271
    > > Change-Id: Ic71e1b49fd15fc47fce3b80948f5cd9a4ef5ab58
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6158625
    > > Commit-Queue: Kartar Singh <kartarsingh@google.com>
    > > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > > Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1408677}
    > >
    >
    > Bug: 370506271
    > Change-Id: I686a8860353b0ff3b71509d1aba8aa8aa6bb3b9c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185955
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Reviewed-by: Lan Wei <lanwei@chromium.org>
    > Owners-Override: Lan Wei <lanwei@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1408769}
    
    Bug: 370506271
    Change-Id: Iafd8c6ed597c7e6067ceca2466c846f54e2bfdf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185322
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410220}

--- content/browser/renderer_host/input/input_transfer_handler_android.cc
+++ content/browser/renderer_host/input/input_transfer_handler_android.cc
@@ -42,6 +42,8 @@ InputTransferHandlerAndroid::InputTransferHandlerAndroid(
   CHECK(input::IsTransferInputToVizSupported());
 }
 
+InputTransferHandlerAndroid::InputTransferHandlerAndroid() = default;
+
 InputTransferHandlerAndroid::~InputTransferHandlerAndroid() = default;
 
 bool InputTransferHandlerAndroid::OnTouchEvent(const ui::MotionEvent& event) {
@@ -68,6 +70,10 @@ bool InputTransferHandlerAndroid::OnTouchEvent(const ui::MotionEvent& event) {
   }
 
   if (event.GetAction() != ui::MotionEvent::Action::DOWN) {
+    return false;
+  }
+
+  if (event.GetToolType() != ui::MotionEvent::ToolType::FINGER) {
     return false;
   }
 
