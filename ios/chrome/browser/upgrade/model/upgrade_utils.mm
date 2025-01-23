8141cd4a53fb5e8d43a99cec6a802ab6ef2f6d25
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 02:58:33 -0800

    [cleanup] clang-format //ios/chrome/browser/{upgrade,ui/upgrade}
    Produced by:
      git ls-files ios/chrome/browser/{upgrade,ui/}/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I9122d7ffa9d43a077943d8cdde1d45eed0c50726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190929
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410208}

--- ios/chrome/browser/upgrade/model/upgrade_utils.mm
+++ ios/chrome/browser/upgrade/model/upgrade_utils.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/upgrade/model/upgrade_utils.h"
 
 #import <Foundation/Foundation.h>
+
 #import "ios/chrome/browser/upgrade/model/upgrade_constants.h"
 
 bool IsAppUpToDate() {
