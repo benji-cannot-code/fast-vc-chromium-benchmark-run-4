4a64fd21cb1141dc24239d961c48e853de6150da
Wenyu Fu <wenyufu@chromium.org>
2025-01-23 11:17:37 -0800

    [E2E] Mark Bottom chin not capable of coloring status bar
    Temporarily mark bottom chin not capable. Can be removed once we direct
    status bar coloring to the e2e layout in CTA.
    
    Bug: 390262712, 390689060
    Change-Id: I7cb1b96168822573b6a4833405733a586f00023e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6191942
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410474}

--- chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeBottomChinCoordinator.java
+++ chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeBottomChinCoordinator.java
@@ -110,6 +110,11 @@ public class EdgeToEdgeBottomChinCoordinator implements Destroyable, SystemBarCo
     // SystemBarColorHelper
 
     @Override
+    public boolean canSetStatusBarColor() {
+        return false;
+    }
+
+    @Override
     public void setStatusBarColor(int color) {}
 
     @Override
