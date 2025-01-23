895e1293fe99d18097791b18418ab1dbb994411f
Raphael Kubo da Costa <kubo@igalia.com>
2025-01-23 13:10:23 -0800

    iossim: Remove workarounds for older XCode versions
    Remove workarounds for XCode 10 and 11 added in
    https://crrev.com/c/1966433, https://crrev.com/c/1972465 and
    https://crrev.com/c/1642508.
    
    Change-Id: I7e451d630d7f15c43bf5266106b9f639a58bce9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181886
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <kubo@igalia.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410518}

--- testing/iossim/iossim.mm
+++ testing/iossim/iossim.mm
@@ -132,13 +132,9 @@ typedef enum {
 NSArray* Runtimes(NSDictionary* simctl_list) {
   NSMutableArray* runtimes = [simctl_list[@"runtimes"] mutableCopy];
   for (NSDictionary* runtime in simctl_list[@"runtimes"]) {
-    BOOL available =
-        [runtime[@"availability"] isEqualToString:@"(available)"] ||
-        runtime[@"isAvailable"];
-
     if (![runtime[@"identifier"]
             hasPrefix:@"com.apple.CoreSimulator.SimRuntime.iOS"] ||
-        !available) {
+        !runtime[@"isAvailable"]) {
       [runtimes removeObject:runtime];
     }
   }
@@ -211,7 +207,6 @@ NSString* GetDeviceBySDKAndName(NSDictionary* simctl_list,
                                 NSString* device_name,
                                 NSString* sdk_version) {
   NSString* sdk = nil;
-  NSString* name = nil;
   // Get runtime identifier based on version property to handle
   // cases when version and identifier are not the same,
   // e.g. below identifer is *13-2 but version is 13.2.2
@@ -224,7 +219,6 @@ NSString* GetDeviceBySDKAndName(NSDictionary* simctl_list,
   for (NSDictionary* runtime in Runtimes(simctl_list)) {
     if ([runtime[@"version"] isEqualToString:sdk_version]) {
       sdk = runtime[@"identifier"];
-      name = runtime[@"name"];
       break;
     }
   }
@@ -234,11 +228,6 @@ NSString* GetDeviceBySDKAndName(NSDictionary* simctl_list,
     exit(kExitInvalidArguments);
   }
   NSArray* devices = [simctl_list[@"devices"] objectForKey:sdk];
-  if (devices == nil || devices.count == 0) {
-    // Specific for XCode 10.1 and lower,
-    // where name from 'runtimes' uses as a key in 'devices'.
-    devices = [simctl_list[@"devices"] objectForKey:name];
-  }
   for (NSDictionary* device in devices) {
     if ([device[@"name"] isEqualToString:device_name]) {
       return device[@"udid"];
