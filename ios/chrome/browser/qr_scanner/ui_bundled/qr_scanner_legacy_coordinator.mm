1b28dd948b500a3bacb07f2084f1d7dd29a5722f
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 05:08:24 -0800

    [cleanup] clang-format //ios/chrome/browser/qr_scanner
    Produced by:
      git ls-files ios/chrome/browser/qr_scanner/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I235203c40930448768e2e4b06c58b10424d93a85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190888
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410259}

--- ios/chrome/browser/qr_scanner/ui_bundled/qr_scanner_legacy_coordinator.mm
+++ ios/chrome/browser/qr_scanner/ui_bundled/qr_scanner_legacy_coordinator.mm
@@ -86,8 +86,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                               completion:^{
                                                 sceneState.QRScannerVisible =
                                                     NO;
-                                                if (completion)
+                                                if (completion) {
                                                   completion();
+                                                }
                                               }];
   self.viewController = nil;
 }
