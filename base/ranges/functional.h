60df76d6acd58917da35816854d7a89f822080a8
Peter Kasting <pkasting@chromium.org>
2025-01-23 12:33:52 -0800

    Make base::ranges equivalent to std::ranges.
    This prevents backsliding while rewriting the codebase to explicitly
    spell the latter.
    
    Bug: 386918226
    Binary-Size: Unlikely actionable, OKed by agrieve@
    Change-Id: I1c5dd4e46802d3f1ae880ca28898de5d9b11287e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169820
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410505}

--- base/ranges/functional.h
+++ base/ranges/functional.h
@@ -7,18 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_RANGES_FUNCTIONAL_H_
 
 #include <functional>
-#include <type_traits>
-#include <utility>
 
-namespace base {
+namespace base::ranges {
 
-namespace ranges {
-
-// Simplified implementations of C++20's std::ranges comparison function
-// objects. As opposed to the std::ranges implementation, these versions do not
-// constrain the passed-in types.
-//
-// Reference: https://wg21.link/range.cmp
 using equal_to = std::equal_to<>;
 using not_equal_to = std::not_equal_to<>;
 using greater = std::greater<>;
@@ -26,8 +17,6 @@ using less = std::less<>;
 using greater_equal = std::greater_equal<>;
 using less_equal = std::less_equal<>;
 
-}  // namespace ranges
-
-}  // namespace base
+}  // namespace base::ranges
 
 #endif  // BASE_RANGES_FUNCTIONAL_H_
