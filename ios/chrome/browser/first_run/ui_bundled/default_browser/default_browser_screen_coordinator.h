638b807ee274f2a58bb49812335a5521357cfc71
Briana McClure <bmcclure@google.com>
2025-01-23 13:57:18 -0800

    [iOS] Implement the Animated FRE Default Browser Promo Experiment
    Implement the animated Default Browser promo in the FRE.
    
    Bug: 389933017
    Change-Id: Icd88dfe86ab8f836216b19ad369d5419dfa6b39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175682
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Auto-Submit: Briana McClure <bmcclure@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410543}

--- ios/chrome/browser/first_run/ui_bundled/default_browser/default_browser_screen_coordinator.h
+++ ios/chrome/browser/first_run/ui_bundled/default_browser/default_browser_screen_coordinator.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @protocol FirstRunScreenDelegate;
 
-// Coordinator to present the default browser screen.
+// Coordinator to present the Default Browser Promo.
 @interface DefaultBrowserScreenCoordinator
     : ChromeCoordinator <PromoStyleViewControllerDelegate>
 
