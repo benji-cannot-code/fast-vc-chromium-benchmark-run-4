438a7544bc064a6542264bb6e003a2e8d96f8023
Shunya Shishido <sisidovski@chromium.org>
2025-01-22 18:44:54 -0800

    Add thread checker to ServiceWorkerData
    This CL adds thread checker to extensions::ServiceWorkerData so that we
    ensure this is running on the same thread. ServiceWorkerData instance is
    managed by `service_worker_data`, which is specified with `thread_local`
    in WorkerThreadDispatcher.
    
    In crbug.com/389971360, we see there are some crashes when
    `ServiceWorkerData::GetServiceWorkerHost()` is called. This happens by
    hitting CHECK for `bindings_system_`, which is unlikely to be null
    because it's passed in the constructor as a unique pointer and no
    explicit deletion in the class.
    
    This also adds CHECK if `WorkerThreadDispatcher::GetServiceWorkerData()`
    is not null in `DidStartServiceWorkerContextOnWorkerThread()`.
    
    We don't set the thread checker for the caller, WorkerThreadDispatcher
    because it can be run on multiple service worker threads.
    
    Bug: 389971360
    Change-Id: Ibfcbd4e71a55d7305a9f388347f59516d9569261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179044
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410082}

--- extensions/renderer/service_worker_data.h
+++ extensions/renderer/service_worker_data.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/threading/thread_checker.h"
 #include "base/unguessable_token.h"
 #include "extensions/common/mojom/automation_registry.mojom.h"
 #include "extensions/common/mojom/event_dispatcher.mojom.h"
@@ -109,6 +110,8 @@ class ServiceWorkerData
   mojo::AssociatedRemote<mojom::RendererAutomationRegistry>
       renderer_automation_registry_remote_;
   mojo::AssociatedReceiver<mojom::ServiceWorker> receiver_{this};
+
+  THREAD_CHECKER(thread_checker_);
 
   base::WeakPtrFactory<ServiceWorkerData> weak_ptr_factory_{this};
 };
