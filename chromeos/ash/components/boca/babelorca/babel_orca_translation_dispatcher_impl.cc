ac684962ab9933a1c6628fd58ef3df9cb888eac0
Avynn Donaghe <avynn@google.com>
2025-01-23 13:17:49 -0800

    [BabelOrca] Move Translation Dispatcher To Component
    moves the translation dispatcher impl for Babel Orca to the
    boca/babel_orca component. This is done because the impl does
    not require any browser dependencies.
    
    Bug: 377691979
    Change-Id: If1a13081c994ec06db641fab59220be397708a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6137653
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410527}

--- chromeos/ash/components/boca/babelorca/babel_orca_translation_dispatcher_impl.cc
+++ chromeos/ash/components/boca/babelorca/babel_orca_translation_dispatcher_impl.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2024 The Chromium Authors
+// Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/boca/babelorca/babel_orca_translation_dispatcher_impl.h"
+#include "chromeos/ash/components/boca/babelorca/babel_orca_translation_dispatcher_impl.h"
 
 #include <memory>
 #include <string>
