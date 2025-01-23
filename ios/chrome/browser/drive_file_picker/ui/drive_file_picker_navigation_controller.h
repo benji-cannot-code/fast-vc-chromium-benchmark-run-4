dace325c8ad1634db2ca528eda4660a44c2a73bd
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 06:42:26 -0800

    [cleanup] clang-format //ios/chrome/browser/drive_file_picker
    Produced by:
      git ls-files ios/chrome/browser/drive_file_picker/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I29e5417be62fb4f56e4b594418abcc5dfceb79e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190207
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410307}

--- ios/chrome/browser/drive_file_picker/ui/drive_file_picker_navigation_controller.h
+++ ios/chrome/browser/drive_file_picker/ui/drive_file_picker_navigation_controller.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // user's Drive folders.
 @interface DriveFilePickerNavigationController : UINavigationController
 
-
 @end
 
 #endif  // IOS_CHROME_BROWSER_DRIVE_FILE_PICKER_UI_DRIVE_FILE_PICKER_NAVIGATION_CONTROLLER_H_
