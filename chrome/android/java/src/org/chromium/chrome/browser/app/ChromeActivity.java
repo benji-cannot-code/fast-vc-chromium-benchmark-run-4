4fc863f41cf1f83b6cc015bf54de29bbcaaf3920
Charles Hager <clhager@google.com>
2025-01-23 13:20:05 -0800

    Rename shouldContentFitWindow to wrapContentWithEdgeToEdgeLayout
    `contentFitsWindow` is a bit overloaded - renaming #shouldContentFitWindow to #wrapContentWithEdgeToEdgeLayout to make it a bit more explicit what this method is signaling.
    
    Bug: 389790022
    Change-Id: I8487f9983594ee2387ee8b51dbad7d3c8defee7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6191556
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Charles Hager <clhager@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1410529}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -1726,7 +1726,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     }
 
     @Override
-    protected boolean shouldContentFitWindowInsets() {
+    protected boolean wrapContentWithEdgeToEdgeLayout() {
         return EdgeToEdgeUtils.isEdgeToEdgeEverywhereEnabled()
                 && !EdgeToEdgeUtils.isEdgeToEdgeBottomChinEnabled()
                 && !EdgeToEdgeUtils.isEdgeToEdgeWebOptInEnabled();
