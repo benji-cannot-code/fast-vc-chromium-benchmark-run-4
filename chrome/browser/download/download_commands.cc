dd9d2de7db344658a1914981dbfeecbb51fe24d0
David Black <dmblack@google.com>
2025-01-23 15:55:09 -0800

    Write filename to clipboard when copying image downloads.
    Previously, the file was decoded and written as a bitmap image to the
    clipboard. This meant that GIFs, for example, wouldn't be pasted in the
    same format as the original. Now, the filename will also be written to
    the clipboard. This will allow the file contents to be pasted in its
    original format.
    
    Note that even though writing filenames to the clipboard is not
    dependent on successful image decoding, we still block on image
    decoding: (1) so as not to change circumstances under which an image is
    available, and (2) so as to continue writing to the clipboard buffer in
    a single atomic write operation.
    
    Also note that this new behavior is wrapped in a kill switch in the
    unlikely event that there are unintended consequences of this change.
    
    Bug: 388405834
    Change-Id: Ie1d346803a84b39329aff1be18a36a53db9f3244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187922
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410620}

--- chrome/browser/download/download_commands.cc
+++ chrome/browser/download/download_commands.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_ui_model.h"
 #include "chrome/browser/image_decoder/image_decoder.h"
 #include "chrome/common/url_constants.h"
+#include "components/download/public/common/download_features.h"
 #include "components/google/core/common/google_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/url_util.h"
@@ -50,15 +51,19 @@ class ImageClipboardCopyManager : public ImageDecoder::ImageRequest {
   ImageClipboardCopyManager& operator=(const ImageClipboardCopyManager&) =
       delete;
 
-  static void Start(const base::FilePath& file_path,
+  static void Start(const base::FilePath file_path,
+                    const base::FilePath file_name_to_report_user,
                     base::SequencedTaskRunner* task_runner) {
-    new ImageClipboardCopyManager(file_path, task_runner);
+    new ImageClipboardCopyManager(
+        std::move(file_path), std::move(file_name_to_report_user), task_runner);
   }
 
  private:
-  ImageClipboardCopyManager(const base::FilePath& file_path,
+  ImageClipboardCopyManager(const base::FilePath file_path,
+                            const base::FilePath file_name_to_report_user,
                             base::SequencedTaskRunner* task_runner)
-      : file_path_(file_path) {
+      : file_path_(std::move(file_path)),
+        file_name_to_report_user_(std::move(file_name_to_report_user)) {
     // Constructor must be called in the UI thread.
     DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
@@ -97,8 +102,14 @@ class ImageClipboardCopyManager : public ImageDecoder::ImageRequest {
     ui::ScopedClipboardWriter scw(ui::ClipboardBuffer::kCopyPaste);
     scw.Reset();
 
-    if (!decoded_image.empty() && !decoded_image.isNull())
+    if (!decoded_image.empty() && !decoded_image.isNull()) {
+      if (base::FeatureList::IsEnabled(
+              download::features::kCopyImageFilenameToClipboard)) {
+        scw.WriteFilenames(ui::FileInfosToURIList(
+            {ui::FileInfo(file_path_, file_name_to_report_user_)}));
+      }
       scw.WriteImage(decoded_image);
+    }
 
     delete this;
   }
@@ -117,6 +128,7 @@ class ImageClipboardCopyManager : public ImageDecoder::ImageRequest {
   }
 
   const base::FilePath file_path_;
+  const base::FilePath file_name_to_report_user_;
 };
 
 }  // namespace
@@ -204,14 +216,15 @@ void DownloadCommands::CopyFileAsImageToClipboard() {
   if (!model_->HasSupportedImageMimeType())
     return;
 
-  base::FilePath file_path = model_->GetFullPath();
-
   if (!task_runner_) {
     task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
         {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
          base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
   }
-  ImageClipboardCopyManager::Start(file_path, task_runner_.get());
+
+  ImageClipboardCopyManager::Start(model_->GetFullPath(),
+                                   model_->GetFileNameToReportUser(),
+                                   task_runner_.get());
 }
 
 bool DownloadCommands::CanBeCopiedToClipboard() const {
