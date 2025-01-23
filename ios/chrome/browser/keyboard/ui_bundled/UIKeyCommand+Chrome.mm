a870ce3084c6d8db56b9a65015b94e1f7bf6316f
Sylvain Defresne <sdefresne@chromium.org>
2025-01-23 03:05:30 -0800

    [cleanup] clang-format //ios/chrome/browser/keyboard
    Produced by:
      git ls-files ios/chrome/browser/keyboard/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: I32904d248f3f0e7f20ce1e3118b8477fd861f767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190527
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410212}

--- ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.mm
+++ ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.mm
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.h"
-
 #import "ios/chrome/browser/keyboard/ui_bundled/key_command_actions.h"
 
 namespace {
--- ios/chrome/browser/keyboard/ui_bundled/menu_builder.mm
+++ ios/chrome/browser/keyboard/ui_bundled/menu_builder.mm
@@ -19,8 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (void)buildMainMenuWithBuilder:(id<UIMenuBuilder>)builder {
   // Only configure the builder for the main command system, not contextual
   // menus.
-  if (builder.system != UIMenuSystem.mainSystem)
+  if (builder.system != UIMenuSystem.mainSystem) {
     return;
+  }
 
   // File
   UIMenu* fileMenu = [UIMenu menuWithChildren:@[
