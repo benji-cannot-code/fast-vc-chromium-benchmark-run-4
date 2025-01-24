47998bab44a094cb5fb1ed54d2cdc3572ec5847b
Ciara McMullin <ciaramcmullin@google.com>
2025-01-24 09:21:28 -0800

    [CNAME Cloaking] Fix up DNS aliases when processing DNS transaction results
    Design Doc: go/ipp-cname-cloaking
    
    Bug: 390686664
    Change-Id: I1da4a6ba67084d8be8d8b2d25147c534fc7073e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182331
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Ciara McMullin <ciaramcmullin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410992}

--- net/dns/dns_task_results_manager.cc
+++ net/dns/dns_task_results_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/features.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_errors.h"
+#include "net/dns/dns_alias_utility.h"
 #include "net/dns/host_resolver.h"
 #include "net/dns/host_resolver_dns_task.h"
 #include "net/dns/host_resolver_internal_result.h"
@@ -145,8 +146,13 @@ void DnsTaskResultsManager::ProcessDnsTransactionResults(
     }
   }
 
+  // Track whether new aliases are added.
+  bool aliases_updated = false;
+
   for (const auto& result : results) {
-    aliases_.insert(result->domain_name());
+    auto [unused_1_, updated_domain_name] =
+        aliases_.insert(result->domain_name());
+    aliases_updated |= updated_domain_name;
 
     switch (result->type()) {
       case HostResolverInternalResult::Type::kData: {
@@ -184,10 +190,13 @@ void DnsTaskResultsManager::ProcessDnsTransactionResults(
 
         break;
       }
-      case net::HostResolverInternalResult::Type::kAlias:
-        aliases_.insert(result->AsAlias().alias_target());
+      case net::HostResolverInternalResult::Type::kAlias: {
+        auto [unused_2_, updated_alias] =
+            aliases_.insert(result->AsAlias().alias_target());
+        aliases_updated |= updated_alias;
 
         break;
+      }
       case net::HostResolverInternalResult::Type::kError:
         // Need to update endpoints when AAAA response is NODATA but A response
         // has at least one valid address.
@@ -209,6 +218,11 @@ void DnsTaskResultsManager::ProcessDnsTransactionResults(
 
         break;
     }
+  }
+
+  // Only fix up aliases if new ones were added.
+  if (aliases_updated) {
+    aliases_ = dns_alias_utility::FixUpDnsAliases(aliases_);
   }
 
   const bool waiting_for_aaaa_response =
@@ -285,7 +299,7 @@ void DnsTaskResultsManager::UpdateEndpoints() {
       endpoint.ipv6_endpoints = per_domain_result->ipv6_endpoints;
       new_endpoints.emplace_back(std::move(endpoint));
     } else {
-      for (const auto& [_, metadata] : per_domain_result->metadatas) {
+      for (const auto& [unused_, metadata] : per_domain_result->metadatas) {
         ServiceEndpoint endpoint;
         endpoint.ipv4_endpoints = per_domain_result->ipv4_endpoints;
         endpoint.ipv6_endpoints = per_domain_result->ipv6_endpoints;
@@ -335,7 +349,7 @@ void DnsTaskResultsManager::UpdateEndpoints() {
 }
 
 bool DnsTaskResultsManager::HasIpv4Addresses() {
-  for (const auto& [_, per_domain_result] : per_domain_results_) {
+  for (const auto& [unused_, per_domain_result] : per_domain_results_) {
     if (!per_domain_result->ipv4_endpoints.empty()) {
       return true;
     }
