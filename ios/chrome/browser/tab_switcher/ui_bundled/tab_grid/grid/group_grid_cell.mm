187077d8fec84fcaa0cc490f4addfd6346cfd746
Daniel White <danieltwhite@google.com>
2025-01-24 08:02:09 -0800

    [iOS] Add build guard around deprecated API
    This CL adds a build guard around the -traitCollectionDidChange
    function. This function has been deprecated in iOS 17 and will be
    removed once Chrome's minimum deployment target is bumped.
    
    Fixed: 391365398
    Change-Id: I438bb9c2ea573b66c47f09b113d1d08c632852e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194836
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410942}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/group_grid_cell.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/group_grid_cell.mm
@@ -174,6 +174,7 @@ const CGFloat kTopBarLargeInset = 20;
 
 #pragma mark - UIView
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
 
@@ -191,6 +192,7 @@ const CGFloat kTopBarLargeInset = 20;
     [self updateTopBarConstraints];
   }
 }
+#endif
 
 - (void)didMoveToWindow {
   if (self.theme == GridThemeLight) {
