a26efc67e39851a573f2482e37e814c80a378cb8
Abhi Patel <abhipatel@chromium.org>
2025-01-24 08:13:35 -0800

    Update `IpProtectionCore` to store MdlType
    The core object for IPP is instantiated per `NetworkContext`. In order
    to correctly check if requests require IPP, the core must be aware of
    what version of the MDL (incognito vs regular) to check. This change
    adds a state stored in the core to keep track of which MDL to check in
    for requests.
    
    This change simply adds the mechanisms required to store the MDL type
    state directly within the IPP core object. It should not currently be
    set. This should not cause any functional change in the logic. A
    followup cl will be written that correctly initializes the core from
    the `NetworkContext`.
    
    Change-Id: I0559e647916d4a48d41d59c45840419193d512a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187189
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Commit-Queue: Abhi Patel <abhipatel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410946}

--- components/ip_protection/common/ip_protection_core_impl.h
+++ components/ip_protection/common/ip_protection_core_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "components/ip_protection/common/ip_protection_core.h"
+#include "components/ip_protection/common/ip_protection_data_types.h"
 #include "net/base/network_change_notifier.h"
 
 namespace net {
@@ -43,7 +44,8 @@ class IpProtectionCoreImpl
           ip_protection_proxy_config_manager,
       std::map<ProxyLayer, std::unique_ptr<IpProtectionTokenManager>>
           ip_protection_token_managers,
-      bool is_ip_protection_enabled);
+      bool is_ip_protection_enabled,
+      bool use_regular_mdl = false);
   ~IpProtectionCoreImpl() override;
 
   // IpProtectionCore implementation.
@@ -91,6 +93,8 @@ class IpProtectionCoreImpl
       ipp_token_managers_;
 
   bool is_ip_protection_enabled_;
+
+  MdlType mdl_type_;
 
   // If true, this class will try to connect to IP Protection proxies via QUIC.
   // Once this value becomes false, it stays false until a network change or
