a7b9822b33e4302e454d30b9dff3c366dfc67911
Sylvain Defresne <sdefresne@chromium.org>
2025-01-24 05:03:06 -0800

    [cleanup] clang-format //ios/chrome/browser/enterprise
    Produced by:
      git ls-files ios/chrome/browser/enterprise/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: Ib4ae3ab407208daaa7304261376bcb8792ae68a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6189890
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410853}

--- ios/chrome/browser/enterprise/model/idle/action.mm
+++ ios/chrome/browser/enterprise/model/idle/action.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/callback_list.h"
 #import "base/check_is_test.h"
-#import "base/memory/raw_ptr.h"
 #import "base/containers/flat_map.h"
 #import "base/containers/flat_set.h"
 #import "base/functional/bind.h"
