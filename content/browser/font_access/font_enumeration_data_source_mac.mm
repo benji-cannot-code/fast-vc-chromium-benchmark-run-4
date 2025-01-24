def83d256c1d7c5d0f8524b3b4240c04c25749d1
Evan Stade <estade@google.com>
2025-01-24 11:36:19 -0800

    More IWYU for protobuf roll
    Bug: 328417294
    Change-Id: I40b5fb3d58969c7005d765732d631cd7ee5a07d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196478
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411084}

--- content/browser/font_access/font_enumeration_data_source_mac.mm
+++ content/browser/font_access/font_enumeration_data_source_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <CoreFoundation/CoreFoundation.h>
 #import <CoreText/CoreText.h>
 
+#include <set>
 #include <string>
 
 #include "base/apple/bridging.h"
