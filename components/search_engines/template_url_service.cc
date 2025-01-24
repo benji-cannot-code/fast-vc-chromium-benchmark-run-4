3431ed89b62dd54bb1472fe5439eba43fcb226a7
Ankush Singh <ankushkush@google.com>
2025-01-24 04:28:09 -0800

    [search-engines] Account data separation in incremental sync update
    This CL implements account data separation in
    TemplateURLService::ProcessSyncChanges(), which handles the incoming
    incremental sync updates.
    
    Bug: 374903497
    Change-Id: Iadaa6ef0a36b5524fdd4179368fa0746b66230e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172227
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410841}

--- components/search_engines/template_url_service.cc
+++ components/search_engines/template_url_service.cc
@@ -1636,7 +1636,10 @@ std::optional<syncer::ModelError> TemplateURLService::ProcessSyncChanges(
         "ProcessSyncChanges failed on ChangeType " +
         syncer::SyncChange::ChangeTypeToString(iter->change_type());
     if (iter->change_type() == syncer::SyncChange::ACTION_DELETE) {
-      if (!existing_turl) {
+      if (!existing_turl ||
+          (base::FeatureList::IsEnabled(
+               syncer::kSeparateLocalAndAccountSearchEngines) &&
+           !existing_turl->GetAccountData())) {
         // Can't DELETE a non-existent engine.
         error = syncer::ModelError(FROM_HERE, error_msg);
         continue;
@@ -1654,7 +1657,12 @@ std::optional<syncer::ModelError> TemplateURLService::ProcessSyncChanges(
       //
       // In the past, we tried re-creating the deleted TemplateURL, but it was
       // likely a source of duplicate search engine entries. crbug.com/1022775
-      if (existing_turl != GetDefaultSearchProvider()) {
+      if (base::FeatureList::IsEnabled(
+              syncer::kSeparateLocalAndAccountSearchEngines) &&
+          existing_turl->GetLocalData()) {
+        Update(existing_turl, TemplateURL(*existing_turl->GetLocalData()));
+        MaybeUpdateDSEViaPrefs(existing_turl);
+      } else if (existing_turl != GetDefaultSearchProvider()) {
         Remove(existing_turl);
       } else {
         postponed_deleted_default_engine_guid_ = existing_turl->sync_guid();
@@ -1676,7 +1684,12 @@ std::optional<syncer::ModelError> TemplateURLService::ProcessSyncChanges(
       TemplateURLData data(turl->data());
       data.id = kInvalidTemplateURLID;
 
-      TemplateURL* added = Add(std::make_unique<TemplateURL>(data));
+      // If flag is enabled, add `data` as account data member instead.
+      TemplateURL* added =
+          base::FeatureList::IsEnabled(
+              syncer::kSeparateLocalAndAccountSearchEngines)
+              ? Add(std::make_unique<TemplateURL>(std::nullopt, data))
+              : Add(std::make_unique<TemplateURL>(data));
       if (added) {
         MaybeUpdateDSEViaPrefs(added);
       }
