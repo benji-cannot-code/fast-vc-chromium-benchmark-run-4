88f1bc243b2b64737797914c503be995da05f292
Erik Chen <erikchen@chromium.org>
2025-01-24 09:23:18 -0800

    views: Create Widget::Reparent.
    Previously, Widget::SetParent() was a private method that only hooked up
    the widget bits without touching the native-widget bits. This CL renames
    that method to HandleNativeWidgetReparented, and makes a public
    Reparent. This CL hooks up glic to use the simpler method.
    
    Change-Id: Ic977533debdbd4b35b972d37bac89f44cfda57be
    Bug: 391414831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180146
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410993}

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -374,7 +374,7 @@ void Widget::ReparentNativeView(gfx::NativeView native_view,
   Widget* parent_widget =
       new_parent ? GetWidgetForNativeView(new_parent) : nullptr;
   if (child_widget) {
-    child_widget->SetParent(parent_widget);
+    child_widget->HandleNativeWidgetReparented(parent_widget);
   }
 }
 
@@ -650,6 +650,13 @@ bool Widget::HasRemovalsObserver(const WidgetRemovalsObserver* observer) const {
 
 bool Widget::GetAccelerator(int cmd_id, ui::Accelerator* accelerator) const {
   return false;
+}
+
+void Widget::Reparent(Widget* parent) {
+  gfx::NativeView child_view = GetNativeView();
+  gfx::NativeView parent_view = parent->GetNativeView();
+  internal::NativeWidgetPrivate::ReparentNativeView(child_view, parent_view);
+  HandleNativeWidgetReparented(parent);
 }
 
 void Widget::ViewHierarchyChanged(const ViewHierarchyChangedDetails& details) {
@@ -1881,7 +1888,7 @@ void Widget::OnNativeWidgetDestroyed() {
 
 void Widget::OnNativeWidgetParentChanged(gfx::NativeView parent) {
   Widget* parent_widget = parent ? GetWidgetForNativeView(parent) : nullptr;
-  SetParent(parent_widget);
+  HandleNativeWidgetReparented(parent_widget);
 }
 
 gfx::Size Widget::GetMinimumSize() const {
@@ -2474,7 +2481,7 @@ void Widget::SetInitialBoundsForFramelessWindow(const gfx::Rect& bounds) {
   }
 }
 
-void Widget::SetParent(Widget* parent) {
+void Widget::HandleNativeWidgetReparented(Widget* parent) {
   if (parent == parent_.get()) {
     return;
   }
