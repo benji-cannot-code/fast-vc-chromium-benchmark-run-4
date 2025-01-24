a99d27a174670e3c521e718197481251371166a6
Siddhartha S <ssid@chromium.org>
2025-01-24 12:29:26 -0800

    Reorder android epehemral cards
    Search module gets higher priroity since others show at kLast
    
    Change-Id: Icc870bfaee74b2e06f3d95cd3120b2759ff769ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6199120
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Auto-Submit: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411120}

--- components/segmentation_platform/embedder/home_modules/home_modules_card_registry.cc
+++ components/segmentation_platform/embedder/home_modules/home_modules_card_registry.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace segmentation_platform::home_modules {
 
 #if BUILDFLAG(IS_ANDROID)
+const char kAuxiliarySearchPromoImpressionCounterPref[] =
+    "ephemeral_pref_counter.auxiliary_search_promo_counter";
+const char kAuxiliarySearchPromoInteractedPref[] =
+    "ephemeral_pref_interacted.auxiliary_search_promo_interacted";
 const char kDefaultBrowserPromoImpressionCounterPref[] =
     "ephemeral_pref_counter.default_browser_promo_counter";
 const char kDefaultBrowserPromoInteractedPref[] =
@@ -51,10 +55,6 @@ const char kQuickDeletePromoImpressionCounterPref[] =
     "ephemeral_pref_counter.quick_delete_promo_counter";
 const char kQuickDeletePromoInteractedPref[] =
     "ephemeral_pref_interacted.quick_delete_promo_interacted";
-const char kAuxiliarySearchPromoImpressionCounterPref[] =
-    "ephemeral_pref_counter.auxiliary_search_promo_counter";
-const char kAuxiliarySearchPromoInteractedPref[] =
-    "ephemeral_pref_interacted.auxiliary_search_promo_interacted";
 #endif
 
 namespace {
@@ -189,6 +189,8 @@ void HomeModulesCardRegistry::RegisterProfilePrefs(
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
+  registry->RegisterIntegerPref(kAuxiliarySearchPromoImpressionCounterPref, 0);
+  registry->RegisterBooleanPref(kAuxiliarySearchPromoInteractedPref, false);
   registry->RegisterIntegerPref(kDefaultBrowserPromoImpressionCounterPref, 0);
   registry->RegisterBooleanPref(kDefaultBrowserPromoInteractedPref, false);
   registry->RegisterIntegerPref(kTabGroupPromoImpressionCounterPref, 0);
@@ -197,8 +199,6 @@ void HomeModulesCardRegistry::RegisterProfilePrefs(
   registry->RegisterBooleanPref(kTabGroupSyncPromoInteractedPref, false);
   registry->RegisterIntegerPref(kQuickDeletePromoImpressionCounterPref, 0);
   registry->RegisterBooleanPref(kQuickDeletePromoInteractedPref, false);
-  registry->RegisterIntegerPref(kAuxiliarySearchPromoImpressionCounterPref, 0);
-  registry->RegisterBooleanPref(kAuxiliarySearchPromoInteractedPref, false);
 #endif
 }
 
@@ -414,6 +414,12 @@ void HomeModulesCardRegistry::CreateAllCards() {
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
+  int auxiliary_search_promo_count =
+      profile_prefs_->GetInteger(kAuxiliarySearchPromoImpressionCounterPref);
+  if (AuxiliarySearchPromo::IsEnabled(auxiliary_search_promo_count)) {
+    all_cards_by_priority_.push_back(std::make_unique<AuxiliarySearchPromo>());
+  }
+
   int default_browser_promo_count =
       profile_prefs_->GetInteger(kDefaultBrowserPromoImpressionCounterPref);
   if (DefaultBrowserPromo::IsEnabled(default_browser_promo_count)) {
@@ -442,11 +448,6 @@ void HomeModulesCardRegistry::CreateAllCards() {
         std::make_unique<QuickDeletePromo>(profile_prefs_));
   }
 
-  int auxiliary_search_promo_count =
-      profile_prefs_->GetInteger(kAuxiliarySearchPromoImpressionCounterPref);
-  if (AuxiliarySearchPromo::IsEnabled(auxiliary_search_promo_count)) {
-    all_cards_by_priority_.push_back(std::make_unique<AuxiliarySearchPromo>());
-  }
 #endif
   InitializeAfterAddingCards();
 }
