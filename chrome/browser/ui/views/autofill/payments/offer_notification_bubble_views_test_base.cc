9d870a85c3770819d87f9624bca6b7cbdf3e3706
Alison Gale <agale@chromium.org>
2025-01-24 14:57:30 -0800

    Add missing includes of profile.h
    Discovered while cleaning up unrelated features.
    
    Bug: 390650448
    Change-Id: I07d147305387ccb8c85c1759bf079fa1be9b315c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198165
    Commit-Queue: Alison Gale <agale@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411185}

--- chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_test_base.cc
+++ chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_test_base.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autofill/autofill_uitest_util.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
--- chrome/browser/ui/views/extensions/extension_installed_bubble_view.cc
+++ chrome/browser/ui/views/extensions/extension_installed_bubble_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_sync_util.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/platform_util.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/signin_ui_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
