64f5274a929749b98be516ccd900e9d7e623e364
Dana Fried <dfried@chromium.org>
2025-01-24 10:08:44 -0800

    Move interactive test API to use new inner webcontents logic
    This refactors the interactive glic test to use the new inner
    WebContents instrumentation introduced in Kombucha to avoid having to
    use unreliable JS injection through the chrome.webviewTag API.
    
    It also opens up mock glic to be used freely in interactive tests with
    the full Kombucha API and not just a few one-off injection methods that
    previously had to be hand-rolled.
    
    For example, the close button can now be closed with:
      ClickElement(kGlicContentsElementId, kPathToMockGlicCloseButton)
    
    ExecuteJs and EvaluateJs can also be used directly on the glic
    contents as well.
    
    Change-Id: Id81eab0d4b8a845f7153cbbaeaef7dba015106e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190717
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411021}

--- chrome/browser/glic/interactive_glic_test.cc
+++ chrome/browser/glic/interactive_glic_test.cc
@@ -11,14 +11,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace glic::test {
 
+namespace internal {
+
+DEFINE_STATE_IDENTIFIER_VALUE(GlicInitializedStateObserver,
+                              kGlicInitializedState);
 GlicInitializedStateObserver::GlicInitializedStateObserver(
     const GlicWindowController& controller)
     : PollingStateObserver(
           [&controller]() { return controller.web_client() != nullptr; }) {}
 GlicInitializedStateObserver::~GlicInitializedStateObserver() = default;
 
-DEFINE_ELEMENT_IDENTIFIER_VALUE(kInstrumentedGlicWebContentsElementId);
-DEFINE_STATE_IDENTIFIER_VALUE(GlicInitializedStateObserver,
-                              kGlicInitializedState);
+}  // namespace internal
+
+DEFINE_ELEMENT_IDENTIFIER_VALUE(kGlicHostElementId);
+DEFINE_ELEMENT_IDENTIFIER_VALUE(kGlicContentsElementId);
+
+const InteractiveBrowserTestApi::DeepQuery kPathToMockGlicCloseButton = {
+    "#closebn"};
 
 }  // namespace glic::test
