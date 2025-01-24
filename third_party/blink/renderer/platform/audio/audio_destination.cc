d5ef69ee2fe2ff5ca4e0a024da4d024a0654c154
Olga Sharonova <olka@chromium.org>
2025-01-24 10:24:45 -0800

    Revert "WebAudio: avoid a logical race during destination start"
    This reverts commit 3edd3a7e61e10d95fea40c5cdc8ad3bd6a97e825.
    
    Reason for revert: Exploratory revert
    Bug: 391234244
    
    Original change's description:
    > WebAudio: avoid a logical race during destination start
    >
    > Update the device state before starting the rendering thread, to avoid
    > inserting silence in the beginning of the stream.
    >
    > Bug: 389961185
    > Change-Id: I17ab7f2bc0507c39302a291bbcf5bc0186f99fd4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171715
    > Auto-Submit: Olga Sharonova <olka@chromium.org>
    > Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    > Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1407396}
    
    Bug: 389961185
    Change-Id: I114285d3022d2ffaa1b6465c607b72a1821f57f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196743
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1411031}

--- third_party/blink/renderer/platform/audio/audio_destination.cc
+++ third_party/blink/renderer/platform/audio/audio_destination.cc
@@ -283,8 +283,8 @@ void AudioDestination::Start() {
   if (device_state_ != DeviceState::kStopped) {
     return;
   }
-  SetDeviceState(DeviceState::kRunning);
   web_audio_device_->Start();
+  SetDeviceState(DeviceState::kRunning);
 }
 
 void AudioDestination::Stop() {
@@ -325,8 +325,8 @@ void AudioDestination::Resume() {
   if (device_state_ != DeviceState::kPaused) {
     return;
   }
-  SetDeviceState(DeviceState::kRunning);
   web_audio_device_->Resume();
+  SetDeviceState(DeviceState::kRunning);
 }
 
 void AudioDestination::SetWorkletTaskRunner(
@@ -356,8 +356,8 @@ void AudioDestination::StartWithWorkletTaskRunner(
   }
 
   SetWorkletTaskRunner(worklet_task_runner);
-  SetDeviceState(DeviceState::kRunning);
   web_audio_device_->Start();
+  SetDeviceState(DeviceState::kRunning);
 }
 
 bool AudioDestination::IsPlaying() {
