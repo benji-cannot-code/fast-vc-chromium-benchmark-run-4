69329eb6448ee222ee798aef6db14539df42b721
Omer Katz <omerkatz@chromium.org>
2025-01-23 16:27:59 -0800

    [perf] Update jetstream2 benchmark to v2.2 by default
    Bug: 40870775
    Change-Id: I57d91374e3206264ce2489192229639501186014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194273
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410629}

--- tools/perf/page_sets/speedometer2_pages.py
+++ tools/perf/page_sets/speedometer2_pages.py
@@ -133,5 +133,5 @@ class Speedometer21Story(_Speedometer2Story):
   NAME = 'Speedometer21'
 
 
-class Speedometer2Story(Speedometer20Story):
+class Speedometer2Story(Speedometer21Story):
   NAME = 'Speedometer2'
