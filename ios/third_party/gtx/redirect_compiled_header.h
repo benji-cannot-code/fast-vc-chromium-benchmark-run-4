3bfe14bc296795c4e25b43760ffcb3d73c4e777f
Evan Stade <estade@google.com>
2025-01-24 08:43:10 -0800

    GTX: change workaround to avoid using pre-compiled proto header
    The previous version of the workaround relied on the headers' macros
    being the same. This version should work with both current protobuf and
    later protobuf, which has a different header macro.
    
    Bug: 328417294
    Change-Id: I8be5a5bf20959b6368759c37a6c3f92d3ab61ec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196576
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410966}

---
+++ ios/third_party/gtx/redirect_compiled_header.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This will point at the generated file.
+#include "gtx.pb.h"
+
+// Prevent including checked-in gtx.pb.h. It's impossible to redirect includes
+// of the pre-built gtx.pb.h because compilers always use the current file's
+// path as the highest-precedence include dir.
+#ifndef GOOGLE_PROTOBUF_INCLUDED_gtx_2eproto
+#define GOOGLE_PROTOBUF_INCLUDED_gtx_2eproto
+#endif
