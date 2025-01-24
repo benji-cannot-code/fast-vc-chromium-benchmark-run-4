b09c4d556fb89afe33f270e407dd625abcaebe8d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-01-24 12:55:38 -0800

    Import wpt@d52c34a22d7409be629f26e0d95cb659c8d94384
    https://github.com/web-platform-tests/wpt/compare/33c6f8490...d52c34a22
    
    Using wpt-import in Chromium 127a874742f7f2b94fcbada2905404c05ebd0f38.
    With Chromium commits locally applied on WPT:
      78a2e0cd09 "Prevent :active/:hover on select when interacting [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kojii@chromium.org, mstensho@chromium.org:
      external/wpt/css/CSS2
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Iba852021c78b4773ab6c6f496f19b9228a59d78a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197718
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411132}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/session/subscribe/subscription_id.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/session/subscribe/subscription_id.py
@@ -2,8 +2,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import pytest
 import uuid
 
-@pytest.mark.asyncio
+pytestmark = pytest.mark.asyncio
+
+
 async def test_subscribe_subscription_id(subscribe_events):
     result = await subscribe_events(events=["browsingContext"])
-    assert isinstance(result['subscription'], str)
-    uuid.UUID(hex=result['subscription'])
+    assert isinstance(result["subscription"], str)
+    uuid.UUID(hex=result["subscription"])
+
+
+async def test_subscribe_twice(subscribe_events):
+    result_1 = await subscribe_events(events=["browsingContext"])
+    result_2 = await subscribe_events(events=["script"])
+
+    assert result_1["subscription"] != result_2["subscription"]
