1564fc630222a38d99d3f41d1eb793bf6ba323d3
Niko Tsirakis <ntsirakis@google.com>
2025-01-24 14:07:27 -0800

    artemis: Refactor internal buffer size checks
    This commit aims to significantly reduce the maximum
    potential memory consumption in Artemis. There are
    two main changes:
    
    * Track the current buffer size based on bytes, not
    line count. Prior to this commit, we were approximating
    buffer size by line count to simplify the check, but
    this may have resulted in larger-than-expected buffers
    in practice. See crbug.com/385089952.
    
    * Halt data collection if we've reached our max size.
    Prior to this commit, local data sources would purge old
    data in the event that their internal buffers grew too
    large. This leads to unnecessary data loss. Instead, simply
    halt data collection until data starts being consumed again.
    This allows us to drop our max buffer size significantly.
    
    Bug: b:388014312
    
    Change-Id: I8bda95d2ccfc432bbafcca0bcc861026c8000d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6184565
    Auto-Submit: Niko Tsirakis <ntsirakis@google.com>
    Commit-Queue: Niko Tsirakis <ntsirakis@google.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411167}

--- chrome/browser/ash/chromebox_for_meetings/artemis/local_data_source.h
+++ chrome/browser/ash/chromebox_for_meetings/artemis/local_data_source.h
@@ -18,11 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::cfm {
 
-// Maximum lines that can be in the internal buffer before we start
-// purging older records. In the working case, we should never hit
-// this limit, but we may reach it if we're unable to enqueue logs
+// Maximum bytes that can be in the internal buffer before we halt
+// collecting data temporarily. In the working case, we should never
+// hit this limit, but we may reach it if we're unable to enqueue logs
 // via Fetch() for whatever reason (eg a network outage).
-inline constexpr int kMaxInternalBufferSize = 50000;  // ~7Mb
+inline constexpr int kMaxInternalBufferSize = 500000;  // 500Kb
 
 class LocalDataSource : public mojom::DataSource {
  public:
@@ -105,6 +105,9 @@ class LocalDataSource : public mojom::DataSource {
   // Contains a chain of the most recent data. Will be returned
   // at the next call to Fetch().
   std::deque<std::string> data_buffer_;
+
+  // Current size of data buffer in bytes.
+  size_t data_buffer_size_ = 0;
 
   // Redaction tool for PII redaction
   redaction::RedactionTool redactor_;
