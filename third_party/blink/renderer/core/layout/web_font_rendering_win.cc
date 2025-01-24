548ca31bdee066b587c84d3488b59b827b3aae0a
Rune Lillesveen <futhark@chromium.org>
2025-01-24 02:46:04 -0800

    Remove CrossThreadCopier for FontDescription
    Was added for Parallel Text Shaping which has been removed.
    
    Also removed an out-dated TODO.
    
    Change-Id: I1064e0e8afd92962abc3ef7d7c648115679971f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194123
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410803}

--- third_party/blink/renderer/core/layout/web_font_rendering_win.cc
+++ third_party/blink/renderer/core/layout/web_font_rendering_win.cc
@@ -29,7 +29,6 @@ WebFontPrewarmer* WebFontRendering::GetFontPrewarmer() {
 void WebFontRendering::SetFontRenderingClient(
     WebFontRenderingClient* rendering_client) {
   FontCache::SetFontPrewarmer(rendering_client);
-  // TODO(yosin): Call `FontThreadPool::SetFontRenderingClient()`.
 }
 
 // static
--- third_party/blink/renderer/platform/fonts/font_description.h
+++ third_party/blink/renderer/platform/fonts/font_description.h
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/typesetting_features.h"
 #include "third_party/blink/renderer/platform/text/layout_locale.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
-#include "third_party/blink/renderer/platform/wtf/cross_thread_copier.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/skia/include/core/SkFontStyle.h"
@@ -600,12 +599,6 @@ struct HashTraits<blink::FontDescription>
   static blink::FontDescription EmptyValue() {
     return blink::FontDescription::CreateHashTableEmptyValue();
   }
-};
-
-template <>
-struct CrossThreadCopier<blink::FontDescription>
-    : public CrossThreadCopierPassThrough<blink::FontDescription> {
-  STATIC_ONLY(CrossThreadCopier);
 };
 
 }  // namespace WTF
