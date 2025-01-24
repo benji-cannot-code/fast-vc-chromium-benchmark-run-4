9108a737839be53af58f14a1422ae1a2e56f2ac7
Anna Tsvirchkova <atsvirchkova@google.com>
2025-01-24 00:55:15 -0800

    Introduce new leak warning bubble
    This CL implements the new credential leak warning as a bubble (because
    it needs to be displayed near the password icon). It wires the button
    click to start the password change flow. Showing this bubble instead of
    our normal leak check dialog will be done in the follow-up CL.
    
    Bug: 381053354
    Change-Id: Iae919af956702fcb6dee79873134b594444cf504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179880
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410766}

--- chrome/browser/ui/views/passwords/password_change/password_change_ui_browsertest.cc
+++ chrome/browser/ui/views/passwords/password_change/password_change_ui_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/chrome_password_change_service.h"
 #include "chrome/browser/password_manager/password_change_service_factory.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/passwords/manage_passwords_ui_controller.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/passwords/password_bubble_view_base.h"
 #include "chrome/test/base/ui_test_utils.h"
