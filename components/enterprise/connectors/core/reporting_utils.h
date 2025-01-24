a61702e1264e3b1aaa0496c67de9af31c6462c69
Nancy Xiao <nancylanxiao@google.com>
2025-01-24 05:50:27 -0800

    Add the remaining event getters
    Bug: 390419758
    Change-Id: I54d9336538397e3c85b9292938618c804ecd1532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194173
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410871}

--- components/enterprise/connectors/core/reporting_utils.h
+++ components/enterprise/connectors/core/reporting_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/common/proto/synced/browser_events.pb.h"
 #include "components/enterprise/connectors/core/common.h"
 #include "url/gurl.h"
+#include "url/scheme_host_port.h"
 
 namespace enterprise_connectors {
 
@@ -31,6 +32,25 @@ GetPasswordReuseEvent(const GURL& url,
 
 chrome::cros::reporting::proto::SafeBrowsingPasswordChangedEvent
 GetPasswordChangedEvent(const std::string& user_name);
+
+chrome::cros::reporting::proto::LoginEvent GetLoginEvent(
+    const GURL& url,
+    bool is_federated,
+    const url::SchemeHostPort& federated_origin,
+    const std::u16string& username);
+
+chrome::cros::reporting::proto::SafeBrowsingInterstitialEvent
+GetInterstitialEvent(const GURL& url,
+                     const std::string& reason,
+                     int net_error_code,
+                     bool clicked_through,
+                     EventResult event_result);
+
+chrome::cros::reporting::proto::BrowserCrashEvent GetBrowserCrashEvent(
+    const std::string& channel,
+    const std::string& version,
+    const std::string& report_id,
+    const std::string& platform);
 
 }  // namespace enterprise_connectors
 
