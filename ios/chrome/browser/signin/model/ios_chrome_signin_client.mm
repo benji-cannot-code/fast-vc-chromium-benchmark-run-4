742b5cd1596974a1c23292aad5d06673246a3689
Arthur Milchior <arthurmilchior@chromium.org>
2025-01-23 19:13:37 -0800

    [Signin]Remove a unused parameter
    Bug: none
    Change-Id: Iea335394f89f76860dc0e1b32df99ed804f30a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6192341
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1410703}

--- ios/chrome/browser/signin/model/ios_chrome_signin_client.mm
+++ ios/chrome/browser/signin/model/ios_chrome_signin_client.mm
@@ -102,8 +102,6 @@ void IOSChromeSigninClient::OnPrimaryAccountChanged(
       break;
     case signin::PrimaryAccountChangeEvent::Type::kSet:
       CHECK(event_details.GetSetPrimaryAccountAccessPoint().has_value());
-      signin_metrics::AccessPoint access_point =
-          event_details.GetSetPrimaryAccountAccessPoint().value();
 
       size_t tabs_count = 0;
 
@@ -113,7 +111,7 @@ void IOSChromeSigninClient::OnPrimaryAccountChanged(
         tabs_count += browser->GetWebStateList()->count();
       }
 
-      signin_metrics::RecordOpenTabCountOnSignin(
-          access_point, signin::ConsentLevel::kSignin, tabs_count);
+      signin_metrics::RecordOpenTabCountOnSignin(signin::ConsentLevel::kSignin,
+                                                 tabs_count);
   }
 }
