01ab3c4cb62fcde920710643aa9ab1c52517cac0
Jacob Stanley <jacobstanley@google.com>
2025-01-24 07:18:14 -0800

    [Ads API UX Enhancements] Creating V2 of footer
    Both site suggested ads and ad topics settings pages have an updated
    footer with this feature enabled, where it doesn't have the link to the help center anymore. This CL also updates IDS_SETTINGS_TOPICS_PAGE_FOOTER_V2 and IDS_SETTINGS_FLEDGE_PAGE_FOOTER_V2 by adding the `aria-description` to the string itself and passing in IDS_SETTINGS_OPENS_IN_NEW_TAB from the C++ side rather than querying the DOM and adding it on the typescript side. This update to the strings does not require a new screenshot since it does not change visibly.
    
    Screenshots: https://screenshot.googleplex.com/hoQ3oDiDN5NaNfn (Ad
    Topics) https://screenshot.googleplex.com/BdgzYd6P7RJ9vvA (Site
    Suggested Ads)
    
    Bug: 389923263
    Change-Id: Ic0c276b658a27dd5864c6c87e9e03af956cb6c62
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6183106
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Auto-Submit: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410907}

--- chrome/browser/resources/settings/privacy_sandbox/privacy_sandbox_fledge_subpage.ts
+++ chrome/browser/resources/settings/privacy_sandbox/privacy_sandbox_fledge_subpage.ts
@@ -29,7 +29,6 @@ import {getTemplate} from './privacy_sandbox_fledge_subpage.html.js';
 export interface SettingsPrivacySandboxFledgeSubpageElement {
   $: {
     fledgeToggle: SettingsToggleButtonElement,
-    footer: HTMLElement,
   };
 }
 
@@ -159,10 +158,6 @@ export class SettingsPrivacySandboxFledgeSubpageElement extends
 
     this.privacySandboxBrowserProxy_.getFledgeState().then(
         state => this.onFledgeStateChanged_(state));
-
-    this.$.footer.querySelectorAll('a').forEach(
-        link =>
-            link.setAttribute('aria-description', this.i18n('opensInNewTab')));
   }
 
   private isFledgePrefManaged_(): boolean {
