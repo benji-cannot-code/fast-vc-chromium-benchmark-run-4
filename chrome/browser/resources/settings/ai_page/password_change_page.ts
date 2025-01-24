7a9b2dbd89b3360d691528dd7b68cc2075f26bd8
Viktor Semeniuk <vsemeniuk@google.com>
2025-01-24 02:35:22 -0800

    Add password change subpage implementation
    This CL adds content to password change subpage.
    
    Bug: 391131625
    Change-Id: I96956f06d30538aa6090070c0fbc67ab2997ff44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187467
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410799}

--- chrome/browser/resources/settings/ai_page/password_change_page.ts
+++ chrome/browser/resources/settings/ai_page/password_change_page.ts
@@ -3,6 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
+import '../settings_columned_section.css.js';
+import '../settings_shared.css.js';
+import '../privacy_icons.html.js';
+import '../icons.html.js';
+
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './password_change_page.html.js';
