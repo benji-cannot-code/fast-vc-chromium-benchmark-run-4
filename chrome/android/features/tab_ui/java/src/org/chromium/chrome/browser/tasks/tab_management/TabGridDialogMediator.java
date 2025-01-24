3195cf531a31b3bd37bb3191f04a41f1a939e60a
Calder Kitagawa <ckitagawa@chromium.org>
2025-01-24 12:18:07 -0800

    [Data Sharing] Dismiss all messages when recent activity is shown
    Recent activity showing is effectively "acking" of all of the messages
    at once. When showing it from the tab grid dialog dismiss all the
    associated messages for the current group.
    
    Fixed: 391946725
    Change-Id: I0e7369fa1179880ccc1a2ebfd1d0f2b3adc76e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196779
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1411113}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
@@ -960,6 +960,7 @@ public class TabGridDialogMediator
         } else if (menuId == R.id.recent_activity) {
             RecordUserAction.record("TabGridDialogMenu.RecentActivity");
             mDataSharingTabManager.showRecentActivity(mActivity, collaborationId);
+            dismissAllDirtyTabMessagesForCurrentGroup();
         } else if (menuId == R.id.close_tab_group || menuId == R.id.delete_tab_group) {
             boolean hideTabGroups = menuId == R.id.close_tab_group;
             if (hideTabGroups) {
@@ -1313,6 +1314,14 @@ public class TabGridDialogMediator
         }
     }
 
+    private void dismissAllDirtyTabMessagesForCurrentGroup() {
+        @Nullable Token tabGroupId = getCurrentTabGroupId();
+        if (mMessagingBackendService != null && tabGroupId != null) {
+            mMessagingBackendService.clearDirtyTabMessagesForGroup(
+                    EitherGroupId.createLocalId(new LocalTabGroupId(tabGroupId)));
+        }
+    }
+
     private void showOrUpdateCollaborationActivityMessageCard() {
         @Nullable Token currentTabGroupId = getCurrentTabGroupId();
         if (currentTabGroupId == null) {
@@ -1344,13 +1353,11 @@ public class TabGridDialogMediator
                             mActivity,
                             this::showRecentActivityOrDismissActivityMessageCard,
                             (unused) -> {
-                                @Nullable Token tabGroupId = getCurrentTabGroupId();
-                                if (mMessagingBackendService != null && tabGroupId != null) {
-                                    mMessagingBackendService.clearDirtyTabMessagesForGroup(
-                                            EitherGroupId.createLocalId(
-                                                    new LocalTabGroupId(tabGroupId)));
-                                }
+                                // TODO(crbug.com/391946087): this shouldn't be required once
+                                // clearDirtyTabMessagesForCurrentGroup is fixed.
                                 removeCollaborationActivityMessageCard();
+
+                                dismissAllDirtyTabMessagesForCurrentGroup();
                             });
         }
         mCollaborationActivityPropertyModel.updateDescriptionText(
@@ -1369,8 +1376,11 @@ public class TabGridDialogMediator
         if (TabShareUtils.isCollaborationIdValid(collaborationId)) {
             mDataSharingTabManager.showRecentActivity(mActivity, collaborationId);
         } else {
+            // TODO(crbug.com/391946087): this shouldn't be required once
+            // clearDirtyTabMessagesForCurrentGroup is fixed.
             removeCollaborationActivityMessageCard();
         }
+        dismissAllDirtyTabMessagesForCurrentGroup();
     }
 
     private void updateUngroupBarText(int tabCount) {
