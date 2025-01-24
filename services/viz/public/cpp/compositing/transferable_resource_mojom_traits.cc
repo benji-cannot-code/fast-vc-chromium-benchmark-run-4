890edc0f4845a494d1e7d32c2e96fe18629d3ccd
Maggie Chen <magchen@chromium.org>
2025-01-23 16:30:22 -0800

    Delete class SharedBitmapId
    Now the legacy SharedBitmapId is replaced by Mailbox id.
    
    Bug: 40064122
    Change-Id: Id5f0780fe2427216d3deb72bf08725a0150979fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172595
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410633}

--- services/viz/public/cpp/compositing/transferable_resource_mojom_traits.cc
+++ services/viz/public/cpp/compositing/transferable_resource_mojom_traits.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/common/mailbox_mojom_traits.h"
 #include "gpu/ipc/common/sync_token_mojom_traits.h"
 #include "services/viz/public/cpp/compositing/resource_id_mojom_traits.h"
-#include "services/viz/public/cpp/compositing/shared_bitmap_id_mojom_traits.h"
 #include "services/viz/public/cpp/compositing/shared_image_format_mojom_traits.h"
 #include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
 #include "ui/gfx/mojom/color_space_mojom_traits.h"
