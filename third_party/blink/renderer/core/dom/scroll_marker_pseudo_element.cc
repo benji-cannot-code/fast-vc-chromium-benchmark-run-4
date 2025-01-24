e08fbe8555e22f20ec21ef6dda7f51fd30ddf5f0
David Awogbemila <awogbemila@chromium.org>
2025-01-24 08:20:50 -0800

    [carousel] Keep active scroll-marker in view
    A scroll-marker-group can be laid out so that it can't visibly fit
    all its scroll-markers. In such cases where the scroll-marker-group
    is scrollable, it should automatically be scrolled so as to keep its
    active scroll-marker within its scrollport.
    
    Bug: 388545127
    Change-Id: Iefe860f5a65716f5c5d8f49c55c20960b6b201af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171382
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410950}

--- third_party/blink/renderer/core/dom/scroll_marker_pseudo_element.cc
+++ third_party/blink/renderer/core/dom/scroll_marker_pseudo_element.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_block.h"
 #include "third_party/blink/renderer/core/scroll/scroll_alignment.h"
 #include "third_party/blink/renderer/core/scroll/scroll_into_view_util.h"
+#include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/keyboard_codes.h"
 
 namespace blink {
@@ -70,6 +71,30 @@ void ScrollMarkerPseudoElement::SetSelected(bool value) {
   }
   is_selected_ = value;
   PseudoStateChanged(CSSSelector::kPseudoTargetCurrent);
+  if (is_selected_ && scroll_marker_group_) {
+    LayoutBox* group_box = scroll_marker_group_->GetLayoutBox();
+    LayoutObject* marker_object = GetLayoutObject();
+    if (!group_box || !marker_object) {
+      return;
+    }
+    ScrollableArea* group_scroller = group_box->GetScrollableArea();
+    if (group_scroller) {
+      // AbsoluteBoundingBoxRectForScrollIntoView detects that this is a
+      // scroll-marker pseudo and returns the rect of the originating element.
+      // Since what we want is the rect of the scroll-marker itself, we use
+      // AbsoluteBoundingBoxRectHandlingEmptyInline directly.
+      PhysicalRect rect =
+          marker_object->AbsoluteBoundingBoxRectHandlingEmptyInline();
+      PhysicalBoxStrut scroll_margin =
+          marker_object->Style()->ScrollMarginStrut();
+      mojom::blink::ScrollIntoViewParamsPtr params =
+          scroll_into_view_util::CreateScrollIntoViewParams(
+              ScrollAlignment::ToEdgeIfNeeded(),
+              ScrollAlignment::ToEdgeIfNeeded());
+      params->behavior = group_box->Style()->GetScrollBehavior();
+      group_scroller->ScrollIntoView(rect, scroll_margin, params);
+    }
+  }
 }
 
 void ScrollMarkerPseudoElement::AttachLayoutTree(AttachContext& context) {
