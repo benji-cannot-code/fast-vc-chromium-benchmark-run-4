c0c667a4ec3fdbebf0967a1e592b6ca514d0fc2d
Sylvain Defresne <sdefresne@chromium.org>
2025-01-24 03:48:41 -0800

    [cleanup] clang-format //ios/chrome/browser/tips_notifications
    Produced by:
      git ls-files \
          ios/chrome/browser/tips_notifications/'*'.{mm,cc,m,h}|\
          xargs clang-format -i
    
    Bug: none
    Change-Id: Ie93b3ad9284919b564fa0e579b77c33be5314392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190692
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410824}

--- ios/chrome/browser/tips_notifications/model/tips_notification_client.h
+++ ios/chrome/browser/tips_notifications/model/tips_notification_client.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 #import <UserNotifications/UserNotifications.h>
+
 #import <optional>
 
 #import "components/prefs/pref_change_registrar.h"
