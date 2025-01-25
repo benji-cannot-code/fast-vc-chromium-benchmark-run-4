b48b283717f00bf5acb95fc7e0b6bfd083313d80
Timofey Chudakov <tchudakov@google.com>
2025-01-25 02:42:45 -0800

    [Android] Remove getSortingCode from AutofillProfile.
    This patch does not change code behavior. See the attached bug
    description for the motivation of the refactoring.
    
    Bug: 392199375
    Change-Id: I703e9f0e86c2d255325fe102b67bbe4afaaa5ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198285
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411307}

--- chrome/android/junit/src/org/chromium/chrome/browser/payments/AddressEditorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/payments/AddressEditorTest.java
@@ -338,7 +338,7 @@ public class AddressEditorTest {
 
         validateTextField(
                 editorFields.get(6),
-                profile.getSortingCode(),
+                profile.getInfo(FieldType.ADDRESS_HOME_SORTING_CODE),
                 FieldType.ADDRESS_HOME_SORTING_CODE,
                 /* label= */ "sorting code label",
                 /* isRequired= */ false,
@@ -745,7 +745,7 @@ public class AddressEditorTest {
         assertEquals("", profile.getInfo(FieldType.ADDRESS_HOME_DEPENDENT_LOCALITY));
         assertEquals("Google", profile.getInfo(FieldType.COMPANY_NAME));
         assertEquals("90291", profile.getInfo(FieldType.ADDRESS_HOME_ZIP));
-        assertEquals("", profile.getSortingCode());
+        assertEquals("", profile.getInfo(FieldType.ADDRESS_HOME_SORTING_CODE));
     }
 
     @Test
--- chrome/browser/autofill/android/junit/src/org/chromium/chrome/browser/autofill/editors/AddressEditorTest.java
+++ chrome/browser/autofill/android/junit/src/org/chromium/chrome/browser/autofill/editors/AddressEditorTest.java
@@ -400,7 +400,7 @@ public class AddressEditorTest {
 
         validateTextField(
                 editorFields.get(6),
-                profile.getSortingCode(),
+                profile.getInfo(FieldType.ADDRESS_HOME_SORTING_CODE),
                 FieldType.ADDRESS_HOME_SORTING_CODE,
                 /* label= */ "sorting code label",
                 /* isRequired= */ false,
@@ -1164,7 +1164,7 @@ public class AddressEditorTest {
         assertEquals("", profile.getInfo(FieldType.ADDRESS_HOME_DEPENDENT_LOCALITY));
         assertEquals("Google", profile.getInfo(FieldType.COMPANY_NAME));
         assertEquals("90291", profile.getInfo(FieldType.ADDRESS_HOME_ZIP));
-        assertEquals("", profile.getSortingCode());
+        assertEquals("", profile.getInfo(FieldType.ADDRESS_HOME_SORTING_CODE));
     }
 
     @Test
