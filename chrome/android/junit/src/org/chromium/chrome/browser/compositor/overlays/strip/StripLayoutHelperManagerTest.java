7424b4eec4b46c43ca2be35eda2fea7b8dbdce82
Zhe Li <zheliooo@google.com>
2025-01-25 11:28:35 -0800

    [TabGroupShareTablet] Show notification bubble iph on tab strip
    - Enqueue showIph runnable to a list and only attempt to show iph when the target rect area becomes fully visible, otherwise retry in next #updateLayout.
    
    IPH on group title
    LTR: https://screenshot.googleplex.com/T28xWsKQHzhr4vk
    RTL: https://screenshot.googleplex.com/9NJnRkXDUeYuvjw
    
    IPH on tab favicon:
    LTR: https://screenshot.googleplex.com/6dMkJQgsYVUKtrL
    RTL: https://screenshot.googleplex.com/3HwBX8vVtCSZsCx
    
    Bug: 348728701
    Change-Id: I6874cc69f1872a90c421546f82b25cb9d84c5f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6153479
    Reviewed-by: Neil Coronado <nemco@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411351}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManagerTest.java
@@ -70,6 +70,7 @@ import org.chromium.chrome.browser.compositor.overlays.strip.StripLayoutHelperMa
 import org.chromium.chrome.browser.compositor.scene_layer.TabStripSceneLayer;
 import org.chromium.chrome.browser.compositor.scene_layer.TabStripSceneLayerJni;
 import org.chromium.chrome.browser.data_sharing.DataSharingTabManager;
+import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.layouts.LayoutType;
 import org.chromium.chrome.browser.layouts.animation.CompositorAnimationHandler;
@@ -97,6 +98,7 @@ import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.scrim.ScrimProperties;
 import org.chromium.components.collaboration.CollaborationService;
 import org.chromium.components.collaboration.ServiceStatus;
+import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.ui.base.LocalizationUtils;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.dragdrop.DragAndDropDelegate;
@@ -142,6 +144,7 @@ public class StripLayoutHelperManagerTest {
     @Mock private DataSharingTabManager mDataSharingTabManager;
     @Mock private CollaborationService mCollaborationService;
     @Mock private ServiceStatus mServiceStatus;
+    @Mock private Tracker mTracker;
     @Captor private ArgumentCaptor<List<Rect>> mSystemExclusionRectCaptor;
 
     private StripLayoutHelperManager mStripLayoutHelperManager;
@@ -188,6 +191,7 @@ public class StripLayoutHelperManagerTest {
     private void initializeTest() {
         when(mTabGroupModelFilterProvider.getTabGroupModelFilter(anyBoolean()))
                 .thenReturn(mTabGroupModelFilter);
+        when(mTabModelSelector.getModel(anyBoolean())).thenReturn(mStandardTabModel);
         when(mTabGroupModelFilter.getTabModel()).thenReturn(mStandardTabModel);
         when(mTabModelSelector.getTabGroupModelFilterProvider())
                 .thenReturn(mTabGroupModelFilterProvider);
@@ -202,6 +206,7 @@ public class StripLayoutHelperManagerTest {
         mToolbarPrimaryColor = SemanticColorUtils.getToolbarBackgroundPrimary(mActivity);
         when(mToolbarManager.getTabStripHeightSupplier()).thenReturn(mTabStripHeightSupplier);
         when(mToolbarManager.getPrimaryColor()).thenReturn(mToolbarPrimaryColor);
+        TrackerFactory.setTrackerForTests(mTracker);
 
         mStripLayoutHelperManager =
                 new StripLayoutHelperManager(
