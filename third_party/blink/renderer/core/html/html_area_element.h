131fbf27f0c7203144fc16bdd1d244a97b14b94e
Mason Freed <masonf@chromium.org>
2025-01-27 11:54:00 -0800

    Only support interesttarget on valid links (with hrefs) [8/N]
    Since links (<a>, <area>, SVG <a>) aren't interactive/focusable
    unless they have an href attribute, interesttarget should also
    not work on links without hrefs.
    
    Bug: 326681249
    Change-Id: I3063d8e3654807da069731c66d1899e0e8ba06c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6199080
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411799}

--- third_party/blink/renderer/core/html/html_area_element.h
+++ third_party/blink/renderer/core/html/html_area_element.h
@@ -72,8 +72,6 @@ class CORE_EXPORT HTMLAreaElement final : public HTMLAnchorElementBase {
                               const FocusOptions*) override;
   void SetFocused(bool, mojom::blink::FocusType) override;
 
-  Element* interestTargetElement() override;
-
   enum Shape { kDefault, kPoly, kRect, kCircle };
   void InvalidateCachedPath();
 
