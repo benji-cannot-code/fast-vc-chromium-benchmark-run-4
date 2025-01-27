995a18718cd6d6b1b490a73967d73b46c9b3d962
Andrzej Fiedukowicz <afie@google.com>
2025-01-27 08:15:27 -0800

    Add test coverage for histogram PRESUBMIT checks
    This was done to ensure no regressions in crbug.com/40281617 but while
    doing that I realized that there is a lot of ground to cover and
    potential controversies in how to actually cover this with tests so for
    the sake of keeping CLs relatively small I think it makes sense to
    dedicate this one to simply covering existing functionality with tests.
    
    Main changes done here:
    
    1. Add `PRESUBMIT_test.py` for histogram that adds a basic pass/fails
    scenarios for the 3 presubmit checks we have.
    
    2. Add 2 extra versions of `.*histogram.xml` within `test_data` with
    comments describing what each of them representing.
    
    3. Add basic injectability by exposing internal versions of check
    methods in `PRESUBMIT.py` as a lot of those checks relying on various
    types of globals - it was a way to inject things like xml paths, or
    actually not skip the whole checks where the data comes from `test_data`
    directory when testing this functionality.
    
    4. Make `CheckWebViewHistogramsAllowlist` accept histograms_files as
    params to allow dependency on global test enabling testing.
    
    Probably the biggest controversy is using PRESUBMIT_test_mocks which
    come from top level of the repo. AFAICT there are no official mocks
    available and there is a prior art to reusing top level ones for example
    (but not limited to):
    https://source.chromium.org/chromium/chromium/src/+/main:base/allocator/partition_allocator/PRESUBMIT_test.py;l=12-17
    
    Tested: This whole PR is adding test coverage, but on top of that I
    validated that presubmits are still properly passing IRL by checking
    that they pass on prod version of histograms.xml and then tripping each
    of them intentionally with local changes.
    
    Bug: 391532700
    Change-Id: I23a4c29ac8a3dc77abb84b06cdd5864921b6eedf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187542
    Commit-Queue: Andrzej Fiedukowicz <afie@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411638}

--- android_webview/java/res/raw/histograms_allowlist_check.py
+++ android_webview/java/res/raw/histograms_allowlist_check.py
@@ -17,7 +17,7 @@ def get_histograms_allowlist_content(src_path):
         return [line.rstrip() for line in file]
 
 
-def CheckWebViewHistogramsAllowlist(src_path, output_api):
+def CheckWebViewHistogramsAllowlist(src_path, output_api, histograms_files):
     """Checks that histograms_allowlist.txt contains valid histograms.
     src_path should point to chromium/src
     """
@@ -25,8 +25,7 @@ def CheckWebViewHistogramsAllowlist(src_path, output_api):
     sys.path.append(histograms_path)
     import print_histogram_names
 
-    all_histograms = print_histogram_names.get_names(
-        print_histogram_names.histogram_xml_files())
+    all_histograms = print_histogram_names.get_names(histograms_files)
 
     histograms_allowlist = get_histograms_allowlist_content(src_path)
 
