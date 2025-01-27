f66ae51c3f19600cf658dabd06ac2d58abc0c931
Rushan Suleymanov <rushans@google.com>
2025-01-27 07:25:23 -0800

    Remove code for accounts from GCM
    Account tracking was disabled in crrev.com/c/4516239, this CL removes
    the remaining code from some GCM files.
    
    This is a part of a chain of CLs to remove account tracking from the
    gcm_driver component.
    
    Bug: 40067875
    Change-Id: I649d25a74e333d34fc179ec97767d2a33492d67b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197084
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411618}

--- google_apis/gcm/engine/gcm_store.h
+++ google_apis/gcm/engine/gcm_store.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
-#include <set>
 #include <string>
 #include <vector>
 
@@ -58,7 +57,6 @@ class GCM_EXPORT GCMStore {
     std::map<std::string, std::string> gservices_settings;
     std::string gservices_digest;
     base::Time last_checkin_time;
-    std::set<std::string> last_checkin_accounts;
     AccountMappings account_mappings;
     base::Time last_token_fetch_time;
     std::map<std::string, int> heartbeat_intervals;
@@ -128,7 +126,6 @@ class GCM_EXPORT GCMStore {
 
   // Sets last device's checkin information.
   virtual void SetLastCheckinInfo(const base::Time& time,
-                                  const std::set<std::string>& accounts,
                                   UpdateCallback callback) = 0;
 
   // G-service settings handling.
--- google_apis/gcm/engine/gcm_store_impl.h
+++ google_apis/gcm/engine/gcm_store_impl.h
@@ -83,7 +83,6 @@ class GCM_EXPORT GCMStoreImpl : public GCMStore {
 
   // Sets last device's checkin information.
   void SetLastCheckinInfo(const base::Time& time,
-                          const std::set<std::string>& accounts,
                           UpdateCallback callback) override;
 
   // G-service settings handling.
