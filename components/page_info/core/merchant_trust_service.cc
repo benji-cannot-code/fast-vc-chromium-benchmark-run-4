ebf5a97008a87a023d3d4083c43e265672739fc9
Nicola Tommasi <tommasin@chromium.org>
2025-01-27 15:14:22 -0800

    [Merchant Trust] Record User Actions for Merchant Trust interactions
    This CL adds support for recording UserActions along with Histograms.
    
    NO_IFTTT=Changes are reflected into the histogram
    
    Bug: 378681205
    Change-Id: I5ed93ccc2a7570f9438c1c20ac61b9eba03888a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198325
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411935}

--- components/page_info/core/merchant_trust_service.cc
+++ components/page_info/core/merchant_trust_service.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/user_metrics.h"
+#include "base/strings/strcat.h"
 #include "components/commerce/core/proto/merchant_trust.pb.h"
 #include "components/optimization_guide/core/hints_processing_util.h"
 #include "components/optimization_guide/core/optimization_guide_decider.h"
@@ -23,6 +25,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "net/base/url_util.h"
 #include "url/gurl.h"
+
+namespace {
+
+std::string GetUserActionString(
+    page_info::MerchantTrustInteraction interaction) {
+  switch (interaction) {
+    case page_info::MerchantTrustInteraction::kPageInfoRowShown:
+      return "PageInfoRowSeen";
+    case page_info::MerchantTrustInteraction::kBubbleOpenedFromPageInfo:
+      return "BubbleOpenedFromPageInfo";
+    case page_info::MerchantTrustInteraction::kSidePanelOpened:
+      return "SidePanelOpened";
+    case page_info::MerchantTrustInteraction::kBubbleClosed:
+      return "BubbleClosed";
+    case page_info::MerchantTrustInteraction::kSidePanelClosed:
+      return "SidePanelClosed";
+  }
+}
+
+std::string GetFamiliarityString(page_info::MerchantFamiliarity familiarity) {
+  switch (familiarity) {
+    case page_info::MerchantFamiliarity::kFamiliar:
+      return "FamiliarSite";
+    case page_info::MerchantFamiliarity::kNotFamiliar:
+      return "UnfamiliarSite";
+  }
+}
+
+}  // namespace
 
 namespace page_info {
 using OptimizationGuideDecision = optimization_guide::OptimizationGuideDecision;
@@ -195,13 +226,17 @@ void MerchantTrustService::RecordMerchantTrustInteraction(
           ? MerchantFamiliarity::kFamiliar
           : MerchantFamiliarity::kNotFamiliar;
 
-  auto histogram_name =
-      std::string("Security.PageInfo.MerchantTrustInteraction.") +
-      (merchant_familiarity == MerchantFamiliarity::kFamiliar
-           ? "FamiliarSite"
-           : "UnfamiliarSite");
+  std::string histogram_name = base::StrCat(
+      {"Security.PageInfo.MerchantTrustInteraction.",
+       GetFamiliarityString(merchant_familiarity)});
+
+  std::string user_action_string =
+      base::StrCat({"MerchantTrust.",
+                    GetUserActionString(interaction), ".",
+                    GetFamiliarityString(merchant_familiarity)});
 
   base::UmaHistogramEnumeration(histogram_name, interaction);
+  base::RecordAction(base::UserMetricsAction(user_action_string.c_str()));
 }
 
 }  // namespace page_info
--- components/page_info/core/merchant_trust_service_unittest.cc
+++ components/page_info/core/merchant_trust_service_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/simple_test_clock.h"
 #include "base/test/task_environment.h"
@@ -351,10 +352,15 @@ TEST_F(MerchantTrustServiceTest, ExperimentSurveyDisabled) {
 
 TEST_F(MerchantTrustServiceTest, RecordMerchantTrustInteractionFamiliarSite) {
   base::HistogramTester t;
+  base::UserActionTester user_action_tester;
+
   EXPECT_CALL(*delegate(), GetSiteEngagementScore(_))
       .WillOnce(Return(kMerchantFamiliarityThreshold));
   service()->RecordMerchantTrustInteraction(
       GURL("https://foo.com"), MerchantTrustInteraction::kPageInfoRowShown);
+
+  EXPECT_EQ(1, user_action_tester.GetActionCount(
+      "MerchantTrust.PageInfoRowSeen.FamiliarSite"));
   t.ExpectUniqueSample(
       "Security.PageInfo.MerchantTrustInteraction.FamiliarSite",
       MerchantTrustInteraction::kPageInfoRowShown, 1);
@@ -362,11 +368,16 @@ TEST_F(MerchantTrustServiceTest, RecordMerchantTrustInteractionFamiliarSite) {
 
 TEST_F(MerchantTrustServiceTest, RecordMerchantTrustInteractionUnfamiliarSite) {
   base::HistogramTester t;
+  base::UserActionTester user_action_tester;
+
   EXPECT_CALL(*delegate(), GetSiteEngagementScore(_))
       .WillOnce(Return(kMerchantFamiliarityThreshold - 0.1));
   service()->RecordMerchantTrustInteraction(
       GURL("https://foo.com"),
       MerchantTrustInteraction::kBubbleOpenedFromPageInfo);
+
+  EXPECT_EQ(1, user_action_tester.GetActionCount(
+      "MerchantTrust.BubbleOpenedFromPageInfo.UnfamiliarSite"));
   t.ExpectUniqueSample(
       "Security.PageInfo.MerchantTrustInteraction.UnfamiliarSite",
       MerchantTrustInteraction::kBubbleOpenedFromPageInfo, 1);
