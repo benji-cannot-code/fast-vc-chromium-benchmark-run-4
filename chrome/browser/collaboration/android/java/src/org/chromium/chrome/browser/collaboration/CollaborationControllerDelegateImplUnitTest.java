929707feecd8e62ef1c2d10e7f58344d60bf1783
Hailey Wang <haileywang@google.com>
2025-01-27 14:22:48 -0800

    [Collaboration] Implement onUrlReadyToShare
    Bug: 383882763
    Change-Id: I55058bee992d14d3b41eefe92b3a1ba0720f1385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6188032
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411906}

--- chrome/browser/collaboration/android/java/src/org/chromium/chrome/browser/collaboration/CollaborationControllerDelegateImplUnitTest.java
+++ chrome/browser/collaboration/android/java/src/org/chromium/chrome/browser/collaboration/CollaborationControllerDelegateImplUnitTest.java
@@ -26,6 +26,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import org.chromium.base.Callback;
 import org.chromium.base.Token;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.data_sharing.DataSharingTabManager;
@@ -47,6 +48,7 @@ import org.chromium.components.tab_group_sync.TabGroupSyncService;
 import org.chromium.ui.base.TestActivity;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.base.WindowAndroid.IntentCallback;
+import org.chromium.url.GURL;
 
 /** Unit test for {@link CollaborationControllerDelegateImpl} */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -62,6 +64,7 @@ public class CollaborationControllerDelegateImplUnitTest {
     @Mock private WindowAndroid mWindowAndroid;
     @Mock private SigninAndHistorySyncActivityLauncher mSigninAndHistorySyncActivityLauncher;
     @Mock private TabGroupSyncService mTabGroupSyncService;
+    @Mock private Callback<Boolean> mCloseScreenCallback;
 
     @Mock
     private CollaborationControllerDelegateImpl.Natives
@@ -290,6 +293,7 @@ public class CollaborationControllerDelegateImplUnitTest {
         String collaborationId = "collaborationId";
         String accessToken = "accessToken";
         String title = "title";
+        GURL url = new GURL("url");
 
         SavedTabGroup savedGroup = new SavedTabGroup();
         savedGroup.localId = localId;
@@ -309,12 +313,26 @@ public class CollaborationControllerDelegateImplUnitTest {
                         .setGroupId(collaborationId)
                         .setAccessToken(accessToken)
                         .build();
-        createCallbackCaptor.getValue().onGroupCreatedWithWait(groupData, null);
+
+        createCallbackCaptor.getValue().onGroupCreatedWithWait(groupData, mCloseScreenCallback);
         verify(mCollaborationControllerDelegateImplNativeMock)
                 .runResultWithGroupTokenCallback(
                         eq(Outcome.SUCCESS),
                         eq(collaborationId),
                         eq(accessToken),
                         eq(resultCallback));
+
+        ArgumentCaptor<Callback<Boolean>> onFinishCallback =
+                ArgumentCaptor.forClass(Callback.class);
+        mCollaborationControllerDelegateImpl.onUrlReadyToShare(
+                collaborationId, url, resultCallback);
+        verify(mDataSharingTabManager)
+                .showShareSheet(
+                        eq(mActivity), eq(collaborationId), eq(url), onFinishCallback.capture());
+
+        onFinishCallback.getValue().onResult(true);
+        verify(mCollaborationControllerDelegateImplNativeMock)
+                .runResultCallback(eq(Outcome.SUCCESS), eq(resultCallback));
+        verify(mCloseScreenCallback).onResult(true);
     }
 }
