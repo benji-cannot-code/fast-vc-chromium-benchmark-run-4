a024a221ccf760d067755fae3249e5d617c070e9
Saifuddin Hitawala <hitawala@chromium.org>
2025-01-27 09:49:56 -0800

    [viz] Explode MailboxHolder in ImageContext
    As part of removing MailboxHolder, this change explodes MailboxHolder
    to its contents in ImageContext, and updates all the constructors and
    creation sites to pass in the contents instead.
    
    Bug: 383309406
    Change-Id: I6cf8b7038df4deba4b8e8b4b9481be53be72612b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6202021
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411702}

--- components/viz/service/display_embedder/image_context_impl.h
+++ components/viz/service/display_embedder/image_context_impl.h
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/aggregated_render_pass.h"
 #include "components/viz/common/resources/shared_image_format.h"
 #include "components/viz/service/display/external_use_client.h"
-#include "gpu/command_buffer/common/mailbox_holder.h"
+#include "gpu/command_buffer/common/mailbox.h"
+#include "gpu/command_buffer/common/sync_token.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_representation.h"
 #include "gpu/ipc/common/vulkan_ycbcr_info.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
@@ -46,7 +47,9 @@ namespace viz {
 // SkiaOutputSurfaceImplOnGpu. {Begin,End}Access is called from the GPU thread.
 class ImageContextImpl final : public ExternalUseClient::ImageContext {
  public:
-  ImageContextImpl(const gpu::MailboxHolder& mailbox_holder,
+  ImageContextImpl(const gpu::Mailbox& mailbox,
+                   const gpu::SyncToken& sync_token,
+                   uint32_t texture_target,
                    const gfx::Size& size,
                    SharedImageFormat format,
                    bool maybe_concurrent_reads,
