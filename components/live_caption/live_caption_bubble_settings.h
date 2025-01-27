d3937d15a67cec9c889faa8a2559bde2a14cafae
Ahmed Nasr <anasr@google.com>
2025-01-27 10:22:00 -0800

    Remove extra include from live_caption_bubble_settings
    Change-Id: I30eb35407c024dee972b1f1305bd481a04d796b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198802
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411725}

--- components/live_caption/live_caption_bubble_settings.h
+++ components/live_caption/live_caption_bubble_settings.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "caption_bubble_settings.h"
 #include "components/live_caption/caption_bubble_settings.h"
 
 class PrefChangeRegistrar;
