fc61160848ce6b2076fde21aa44dd0bfbe8d38aa
Marc Treib <treib@chromium.org>
2025-01-27 08:41:56 -0800

    [iOS] Introduce feature UseAccountListFromIdentityManager
    We want to migrate from a bunch of (iOS-specific)
    ChromeAccountManagerService APIs to corresponding (cross-platform)
    IdentityManager APIs, see linked bug. The changes to do this were
    already implemented, behind the feature flag
    SeparateProfilesForManagedAccounts which also does a bunch of other
    things.
    
    For an easier and cleaner rollout, this CL introduces a new feature
    specifically for the API change. It also converts one usage over to the
    new feature, as an example.
    
    Bug: 377467350
    Change-Id: I933418ce54926c6f5c789e67c6b511af3a3865be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190593
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411655}

--- ios/chrome/browser/account_picker/ui_bundled/account_picker_confirmation/account_picker_confirmation_screen_mediator.mm
+++ ios/chrome/browser/account_picker/ui_bundled/account_picker_confirmation/account_picker_confirmation_screen_mediator.mm
@@ -143,7 +143,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeAccountManagerServiceObserver
 
 - (void)identityListChanged {
-  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+  if (IsUseAccountListFromIdentityManagerEnabled()) {
     // Listening to `onAccountsOnDeviceChanged` instead.
     return;
   }
@@ -151,7 +151,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)identityUpdated:(id<SystemIdentity>)identity {
-  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+  if (IsUseAccountListFromIdentityManagerEnabled()) {
     // Listening to `onExtendedAccountInfoUpdated` instead.
     return;
   }
@@ -167,7 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark -  IdentityManagerObserver
 
 - (void)onAccountsOnDeviceChanged {
-  if (!AreSeparateProfilesForManagedAccountsEnabled()) {
+  if (!IsUseAccountListFromIdentityManagerEnabled()) {
     // Listening to `identityListChanged` instead.
     return;
   }
@@ -175,7 +175,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)onExtendedAccountInfoUpdated:(const AccountInfo&)info {
-  if (!AreSeparateProfilesForManagedAccountsEnabled()) {
+  if (!IsUseAccountListFromIdentityManagerEnabled()) {
     // Listening to `identityUpdated` instead.
     return;
   }
