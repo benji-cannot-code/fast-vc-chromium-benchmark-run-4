28efc29303908fba6cd931a9e4e60c143450866d
Colin Blundell <blundell@chromium.org>
2025-01-27 04:45:28 -0800

    [Blink] Add CanvasRenderingContextHost getter for SharedImageFormat
    Will be used to convert callsites of
    CRCHost::GetRenderingContextSkColorType() to instead operate on the
    SharedImageFormat. A bit down the line, we will swap the layering so
    that the SkColorType getter is on top of the SharedImageFormat getter
    (and then hopefully goes away entirely).
    
    Bug: 371227617
    Change-Id: Ief61768c671531d6d8c67a21f31edbe09e5b5afa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198502
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411570}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
+#include "components/viz/common/resources/shared_image_format_utils.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/config/gpu_feature_info.h"
 #include "third_party/blink/public/common/features.h"
@@ -354,6 +355,12 @@ SkAlphaType CanvasRenderingContextHost::GetRenderingContextAlphaType() const {
 SkColorType CanvasRenderingContextHost::GetRenderingContextSkColorType() const {
   return RenderingContext() ? RenderingContext()->GetSkColorType()
                             : kN32_SkColorType;
+}
+
+viz::SharedImageFormat CanvasRenderingContextHost::GetRenderingContextFormat()
+    const {
+  return viz::SkColorTypeToSinglePlaneSharedImageFormat(
+      GetRenderingContextSkColorType());
 }
 
 sk_sp<SkColorSpace>
--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -2137,6 +2137,15 @@ _CONFIG = [
     },
     {
         'paths': [
+            'third_party/blink/renderer/core/html/canvas',
+        ],
+        'allowed': [
+            'viz::SharedImageFormat',
+            'viz::SkColorTypeToSinglePlaneSharedImageFormat',
+        ],
+    },
+    {
+        'paths': [
             'third_party/blink/renderer/platform/graphics/view_transition_element_id.h'
         ],
         'allowed': ['cc::ViewTransitionElementId'],
