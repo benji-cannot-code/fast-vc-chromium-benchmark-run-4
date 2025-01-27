e43aef0ab7e1b4564134ef7936df17b787fec7c2
rbpotter <rbpotter@chromium.org>
2025-01-27 11:12:36 -0800

    iOS: Add new chrome://chrome-urls and InternalOnlyUisPref feature
    This CL implements the InternalOnlyUisPref feature behind the feature
    flag on iOS. The CL is already implemented for other platforms. The
    feature consists of:
    - New chrome://chrome-urls page, separate from chrome://about, with
      a separate section for URLs identified as internal debugging tools
    - Internal debugging tool pages are behind a pref. When the pref is
      disabled, loading one of the internal debug pages displays an
      interstitial explaining that the pages are disabled.
    - The interstitial also contains a link to the location on the
      chrome://chrome-urls where a button can be clicked to enable the
      pages.
    
    The feature flag is currently disabled by default.
    
    Bug: 379889249
    Change-Id: Ifee64896fa49efeed0866f7830c69019e42f6c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174606
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411766}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/update_client/update_client.h"
 #import "components/variations/service/variations_service.h"
 #import "components/web_resource/web_resource_pref_names.h"
+#import "components/webui/chrome_urls/pref_names.h"
 #import "ios/chrome/app/spotlight/spotlight_util.h"
 #import "ios/chrome/app/variations_app_state_agent.h"
 #import "ios/chrome/browser/authentication/ui_bundled/history_sync/history_sync_utils.h"
@@ -421,6 +422,7 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   autofill::prefs::RegisterLocalStatePrefs(registry);
   breadcrumbs::RegisterPrefs(registry);
   ProfileAttributesStorageIOS::RegisterPrefs(registry);
+  chrome_urls::RegisterPrefs(registry);
   flags_ui::PrefServiceFlagsStorage::RegisterPrefs(registry);
   signin::IdentityManager::RegisterLocalStatePrefs(registry);
   IOSChromeMetricsServiceClient::RegisterPrefs(registry);
