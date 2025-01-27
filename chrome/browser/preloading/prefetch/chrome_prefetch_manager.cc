e5a5496d22ae88358543bd2d76e775530ba9f98d
Taiyo Mizuhashi <taiyo@chromium.org>
2025-01-26 18:03:13 -0800

    CCT Prefetch: Do early return when the flags are disabled on ChromePrefetchmanager
    - CCT Prefetch API requires two flags to be enabled:
      kCCTNavigationalPrefetch, kPrefetchBrowserInitiatedTriggers
    - The API usage is guarded on [1] (Top of the API call. Uses Java side
      CachedFlag. If disabled then early return) and CHECK-ed on [2] (Middle
      of the API call on C++ side).
    - However, if the state of the Java's CachedFlag and the C++ side's flag
      are not synchronized, the CHECK on [2] will be violated when the
      former is enabled and latter is disabled. (Please see
      crbug.com/390302058)
    
    After this CL, [2] does early return instead of CHECK, avoiding crash in
    the above situation.
    
    No behavior changes. The feature is guarded on either [1] or [2].
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/src/org/chromium/chrome/browser/WarmupManager.java;l=659;drc=9d39df1e6478e012e73ddff8dd356401ba312e60
    
    [2]
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/preloading/prefetch/chrome_prefetch_manager.cc;l=36;drc=26f86c6b36848b2ea3fb322dbf6b6901bfcc50c8,
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/preloading/prefetch/chrome_prefetch_manager.cc;l=75;drc=26f86c6b36848b2ea3fb322dbf6b6901bfcc50c8
    
    Bug: 390302058, 40288091
    Change-Id: Id49cd3bb0f76df766946e9d2e616c2688d11db59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196896
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411464}

--- chrome/browser/preloading/prefetch/chrome_prefetch_manager.cc
+++ chrome/browser/preloading/prefetch/chrome_prefetch_manager.cc
@@ -34,9 +34,12 @@ void ChromePrefetchManager::StartPrefetchFromCCT(
     const GURL& prefetch_url,
     bool use_prefetch_proxy,
     const std::optional<url::Origin>& referring_origin) {
-  CHECK(
-      base::FeatureList::IsEnabled(chrome::android::kCCTNavigationalPrefetch));
-
+  if (!base::FeatureList::IsEnabled(
+          chrome::android::kCCTNavigationalPrefetch) ||
+      !base::FeatureList::IsEnabled(
+          features::kPrefetchBrowserInitiatedTriggers)) {
+    return;
+  }
   auto* preloading_data =
       content::PreloadingData::GetOrCreateForWebContents(&GetWebContents());
 
@@ -72,9 +75,6 @@ void ChromePrefetchManager::StartPrefetchFromCCT(
 #endif  // BUILDFLAG(IS_ANDROID)
 
 ChromePrefetchManager::ChromePrefetchManager(content::WebContents* web_contents)
-    : content::WebContentsUserData<ChromePrefetchManager>(*web_contents) {
-  CHECK(base::FeatureList::IsEnabled(
-      features::kPrefetchBrowserInitiatedTriggers));
-}
+    : content::WebContentsUserData<ChromePrefetchManager>(*web_contents) {}
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(ChromePrefetchManager);
