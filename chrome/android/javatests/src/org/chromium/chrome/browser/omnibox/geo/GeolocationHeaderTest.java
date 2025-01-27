0bf7315563a616bb3790448e0adbc93f6bf50215
Matthew Jones <mdjones@chromium.org>
2025-01-27 14:35:15 -0800

    [gardener] Disable flaky test: testGeolocationHeaderPrimingDisabledOsPermissionBlocked
    Bug: 392607758
    Change-Id: I708d1210dce3891d495513c249ba77bfab3f351d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196483
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411913}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeaderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeaderTest.java
@@ -23,6 +23,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -217,6 +218,7 @@ public class GeolocationHeaderTest {
     @SmallTest
     @Feature({"Location"})
     @RequiresRestart(value = "Needs to reset cached geolocation from previous tests")
+    @DisabledTest(message = "Flaky. See crbug.com/392607758")
     public void testGeolocationHeaderPrimingDisabledOsPermissionBlocked() {
         setPermission(ContentSettingValues.ALLOW);
         LocationSettingsTestUtil.setSystemLocationSettingEnabled(false);
