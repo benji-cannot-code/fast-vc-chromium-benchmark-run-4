a6b3c23d56c12ac66b1e1569c2a3656316743e8b
Wenyu Fu <wenyufu@chromium.org>
2025-01-27 14:59:29 -0800

    [E2E] Do not consider device supported configuration
    "isSupportedConfiguration" is used for to create bottom chin, and many places other places to enable safe area insets behaviors. Do not consider device a "supported configuration" when bottom chin
    is disabled, but edge to edge everywhere is turned on.
    
    Bug: 391535238
    Change-Id: I1feac58038914adabe9c18efcd0f4202cef82f70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6199511
    Reviewed-by: Charles Hager <clhager@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411926}

--- chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeControllerFactory.java
+++ chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeControllerFactory.java
@@ -154,7 +154,7 @@ public class EdgeToEdgeControllerFactory {
             return false;
         }
 
-        return EdgeToEdgeUtils.isEnabled()
+        return EdgeToEdgeUtils.isEdgeToEdgeBottomChinEnabled()
                 && !DeviceFormFactor.isNonMultiDisplayContextOnTablet(activity)
                 && !BuildInfo.getInstance().isAutomotive
                 // TODO(https://crbug.com/325356134) use UiUtils#isGestureNavigationMode instead.
