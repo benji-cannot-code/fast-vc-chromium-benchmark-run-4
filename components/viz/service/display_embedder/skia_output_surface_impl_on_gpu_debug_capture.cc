3e5e5f684ebac00641b2a55e27dc60b6e2320a42
Saifuddin Hitawala <hitawala@chromium.org>
2025-01-27 09:28:56 -0800

    [viz] Add mailbox, texture target accessors on ImageContext
    As part of removing MailboxHolder, this change adds accessors for
    mailbox and texture target on ImageContext which can then be used
    instead of accessing them through mailbox_holder().
    
    Bug: 383309406
    Change-Id: I3e943aed5435cf4e527151901efe9435755d842b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6201982
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411686}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu_debug_capture.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu_debug_capture.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display_embedder/skia_output_surface_dependency.h"
 #include "components/viz/service/display_embedder/skia_render_copy_results.h"
 #include "gpu/command_buffer/common/mailbox.h"
-#include "gpu/command_buffer/common/mailbox_holder.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_factory.h"
 #include "gpu/vulkan/buildflags.h"
@@ -96,8 +95,8 @@ void AttemptDebuggerBufferCapture(
     return;
   }
 
-  auto representation = representation_factory->ProduceSkia(
-      context->mailbox_holder().mailbox, context_state);
+  auto representation =
+      representation_factory->ProduceSkia(context->mailbox(), context_state);
 
   if (!representation) {
     DLOG(ERROR) << "Failed to make produce skia representation.";
@@ -147,7 +146,7 @@ void AttemptDebuggerBufferCapture(
   si_info->surface_origin = context->origin();
   si_info->alpha_type = context->alpha_type();
   si_info->usage = gpu::SHARED_IMAGE_USAGE_DISPLAY_READ;
-  si_info->mailbox = context->mailbox_holder().mailbox;
+  si_info->mailbox = context->mailbox();
 
   if (auto* graphite_context = context_state->graphite_context()) {
     // SkImage/SkSurface asyncRescaleAndReadPixels methods won't be implemented
