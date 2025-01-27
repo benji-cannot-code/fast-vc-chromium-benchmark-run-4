48435c0dfacd8027630b1f169ec7d6bcabbd7867
Xiqi Ruan <xiqiruan@google.com>
2025-01-27 10:53:57 -0800

    demomode: Log server side error
    Add system log for setup or clean up demo account.
    
    Bug: 392176322
    Change-Id: I995e43c1b341e3aa67057f62f7ce39cb4bf1b3b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197577
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411746}

--- chrome/browser/ash/login/demo_mode/demo_login_controller.h
+++ chrome/browser/ash/login/demo_mode/demo_login_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_LOGIN_DEMO_MODE_DEMO_LOGIN_CONTROLLER_H_
 #define CHROME_BROWSER_ASH_LOGIN_DEMO_MODE_DEMO_LOGIN_CONTROLLER_H_
 
+#include <optional>
 #include <string>
 
 #include "base/functional/callback.h"
