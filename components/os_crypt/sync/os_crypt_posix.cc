3b12ccc25b3423916a996a7a87da211094e3f2fa
Will Harris <wfh@chromium.org>
2025-01-27 14:41:57 -0800

    Flip posix OSCrypt to return 'true' for IsEncryptionAvailable
    Encryption is available on posix, and Encrypt and Decrypt
    operations work fine.
    
    This CL fixes the glitch, and also disables hash verification
    for the keyword table since flipping this to return true will
    cause hash verification to start failing as the hash was never
    generated during migration on these platforms.
    
    BUG=391843295
    
    Change-Id: I4a65d8a37784d2306e68df973cce0ea848db261d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194802
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411916}

--- components/os_crypt/sync/os_crypt_posix.cc
+++ components/os_crypt/sync/os_crypt_posix.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <memory>
+#include <optional>
 
 #include "base/check.h"
 #include "base/metrics/histogram_functions.h"
@@ -29,6 +30,9 @@ constexpr std::array<uint8_t, crypto::aes_cbc::kBlockSize> kIv{
     ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
     ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
 };
+
+std::optional<bool> g_encryption_available_for_testing;
+
 // clang-format on
 
 // Prefix for cypher text returned by obfuscation version.  We prefix the
@@ -37,6 +41,14 @@ constexpr std::array<uint8_t, crypto::aes_cbc::kBlockSize> kIv{
 constexpr char kObfuscationPrefix[] = "v10";
 
 }  // namespace
+
+namespace OSCrypt {
+
+void SetEncryptionAvailableForTesting(std::optional<bool> available) {
+  g_encryption_available_for_testing = available;
+}
+
+}  // namespace OSCrypt
 
 bool OSCrypt::EncryptString16(const std::u16string& plaintext,
                               std::string* ciphertext) {
@@ -109,7 +121,7 @@ bool OSCrypt::DecryptString(const std::string& ciphertext,
 
 // static
 bool OSCrypt::IsEncryptionAvailable() {
-  return false;
+  return g_encryption_available_for_testing.value_or(true);
 }
 
 // static
