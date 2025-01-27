112eff61f8d22f380cacc3ef40d87f68a0533aff
Adem Derinel <derinel@google.com>
2025-01-27 04:52:22 -0800

    Include gtest from //testing in prediction_service_unittest.cc
    Bug: 392523690
    Change-Id: I918d97fc4e9501b56017716156c22900d027bf39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6170703
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411571}

--- components/permissions/prediction_service/prediction_service_unittest.cc
+++ components/permissions/prediction_service/prediction_service_unittest.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/test/test_url_loader_factory.h"
-#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
 // Helper common requests and responses. All of these are for the NOTIFICATION
