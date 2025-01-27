ffc9100161aa29088029021ad003b71141c5791a
David Sanders <dsanders11@ucsbalum.com>
2025-01-27 11:24:32 -0800

    Remove unused include in //base/observer_list.h
    Bug: 40318405
    Change-Id: Ib7f423beac4a2976047ca44e9d61331704811da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6201410
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1411780}

--- base/observer_list.h
+++ base/observer_list.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/dcheck_is_on.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/notreached.h"
 #include "base/observer_list_internal.h"
 #include "base/ranges/algorithm.h"
