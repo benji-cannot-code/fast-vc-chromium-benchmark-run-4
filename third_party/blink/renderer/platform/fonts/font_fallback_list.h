437e083a748e4d9226fdf1f1131d9f270d0585fc
Koji Ishii <kojii@chromium.org>
2025-01-27 14:51:43 -0800

    [shape-cache] Cache `FontFeatures`
    This patch caches `FontFeatures` in `FontFallbackList`,
    reducing `FontFeatures::Initialize` for each shaping to for
    each `Font`.
    
    The new ShapeCache can use the cached `FontFeatures`.
    
    This patch should have no behavior changes.
    
    Bug: 377832797
    Change-Id: I7c12bd40a3e049da43742d73721cd9b4a22f13dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196113
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411920}

--- third_party/blink/renderer/platform/fonts/font_fallback_list.h
+++ third_party/blink/renderer/platform/fonts/font_fallback_list.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/fallback_list_composite_key.h"
 #include "third_party/blink/renderer/platform/fonts/font_cache.h"
 #include "third_party/blink/renderer/platform/fonts/font_selector.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/font_features.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/ng_shape_cache.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_cache.h"
 #include "third_party/blink/renderer/platform/fonts/simple_font_data.h"
@@ -101,6 +102,8 @@ class PLATFORM_EXPORT FontFallbackList
   }
   const FontData* FontDataAt(const FontDescription&, unsigned index);
 
+  const FontFeatures& GetFontFeatures(const FontDescription&);
+
   bool CanShapeWordByWord(const FontDescription&);
 
   void SetCanShapeWordByWordForTesting(bool b) {
@@ -130,12 +133,15 @@ class PLATFORM_EXPORT FontFallbackList
   const Member<FontSelector> font_selector_;
   int family_index_ = 0;
   const uint16_t generation_;
+  FontFeatures font_features_;
+
   bool has_loading_fallback_ : 1 = false;
   bool has_custom_font_ : 1 = false;
   bool can_shape_word_by_word_ : 1 = false;
   bool can_shape_word_by_word_computed_ : 1 = false;
   bool is_invalid_ : 1 = false;
   bool nullify_primary_font_data_for_test_ : 1 = false;
+  bool is_font_features_computed_ : 1 = false;
 
   Member<NGShapeCache> ng_shape_cache_;
   Member<ShapeCache> shape_cache_;
