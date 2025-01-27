188107698d91817f619e92baee518a78364f719d
Ian Clelland <iclelland@chromium.org>
2025-01-27 10:39:32 -0800

    Clear error timeout when offline message appears while loading glic window
    Bug: 379164689
    Change-Id: I725538564110cebff008fc01459bd0010c21abcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6200466
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411732}

--- chrome/browser/resources/glic/glic_app_controller.ts
+++ chrome/browser/resources/glic/glic_app_controller.ts
@@ -197,6 +197,8 @@ export class GlicAppController {
     if (online) {
       this.beginLoadingSequence();
     } else {
+      clearTimeout(this.maxWaitTimer);
+      this.maxWaitTimer = undefined;
       this.destroyWebview();
       this.showPanel('offlinePanel');
     }
