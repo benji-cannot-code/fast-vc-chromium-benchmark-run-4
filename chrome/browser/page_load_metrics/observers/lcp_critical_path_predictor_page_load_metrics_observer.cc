ab3d03ae8222d363276da12ed0953d07180a0faa
Minoru Chikamune <chikamune@chromium.org>
2025-01-26 22:59:51 -0800

    Bugfix of ImageLoadingPriority.ConfidenceOf{LCP,NonLCP}
    This CL is a bugfix of ImageLoadingPriority.ConfidenceOf{LCP,NonLCP}
    histograms. In the previous CL, I accidentally recorded the confidence
    value between 0 and 1. But I had to multiply 100.
    
    OBSOLETE_HISTOGRAM[PageLoad.Clients.LCPP.ImageLoadingPriority.ConfidenceOfLCP]=Replaced by PageLoad.Clients.LCPP.ImageLoadingPriority.ConfidenceOfLCP2
    OBSOLETE_HISTOGRAM[PageLoad.Clients.LCPP.ImageLoadingPriority.ConfidenceOfNonLCP]=Replaced by PageLoad.Clients.LCPP.ImageLoadingPriority.ConfidenceOfNonLCP2
    
    Bug: 40063266
    Change-Id: I1fe46fc6bf677631b46f3f91a68a2bfd6300a3f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197682
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411487}

--- chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.cc
@@ -44,9 +44,9 @@ const char kHistogramLCPPImageLoadingPriorityFrequencyOfLCP[] =
 const char kHistogramLCPPImageLoadingPriorityFrequencyOfNonLCP[] =
     HISTOGRAM_PREFIX "ImageLoadingPriority.FrequencyOfNonLCP";
 const char kHistogramLCPPImageLoadingPriorityConfidenceOfLCP[] =
-    HISTOGRAM_PREFIX "ImageLoadingPriority.ConfidenceOfLCP";
+    HISTOGRAM_PREFIX "ImageLoadingPriority.ConfidenceOfLCP2";
 const char kHistogramLCPPImageLoadingPriorityConfidenceOfNonLCP[] =
-    HISTOGRAM_PREFIX "ImageLoadingPriority.ConfidenceOfNonLCP";
+    HISTOGRAM_PREFIX "ImageLoadingPriority.ConfidenceOfNonLCP2";
 }  // namespace internal
 
 namespace {
@@ -152,11 +152,11 @@ void MaybeReportLcpElementLocatorUMA(
     if (lcp_element_locator == actual_lcp_element_locator) {
       base::UmaHistogramPercentage(
           internal::kHistogramLCPPImageLoadingPriorityConfidenceOfLCP,
-          confidence);
+          100 * confidence);
     } else {
       base::UmaHistogramPercentage(
           internal::kHistogramLCPPImageLoadingPriorityConfidenceOfNonLCP,
-          confidence);
+          100 * confidence);
     }
   }
 }
