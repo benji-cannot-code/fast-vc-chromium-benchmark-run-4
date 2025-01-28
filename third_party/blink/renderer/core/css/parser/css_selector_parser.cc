22c5950974ab1303f4139a9ca60a0d27207c1c29
Steinar H. Gunderson <sesse@chromium.org>
2025-01-28 02:53:55 -0800

    Fix a DCHECK when parsing malformed aN+b selectors.
    Fixed: 392667948
    Change-Id: I959dc591cb247f8acfacd4282d6cdd42d35b2d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6204522
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412193}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.cc
@@ -2147,7 +2147,7 @@ bool CSSSelectorParser::ConsumeANPlusB(CSSParserTokenStream& stream,
     return true;
   }
 
-  const CSSParserToken& b = stream.Consume();
+  CSSParserToken b = stream.Peek();
   if (b.GetType() != kNumberToken ||
       b.GetNumericValueType() != kIntegerValueType) {
     return false;
@@ -2156,6 +2156,7 @@ bool CSSSelectorParser::ConsumeANPlusB(CSSParserTokenStream& stream,
     return false;
   }
   result.second = ClampTo<int>(b.NumericValue());
+  stream.Consume();
   if (sign == kMinusSign) {
     // Negating minimum integer returns itself, instead return max integer.
     if (result.second == std::numeric_limits<int>::min()) [[unlikely]] {
