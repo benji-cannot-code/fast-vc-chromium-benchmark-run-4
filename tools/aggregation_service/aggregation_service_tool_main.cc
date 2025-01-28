e266e42250986dff2e49d57f06b2d4c0306049c0
Peter Kasting <pkasting@chromium.org>
2025-01-27 20:14:53 -0800

    [cleanup] Replace base::ranges with std::ranges: tools/
    Done entirely with `git grep` and `sed` + `git cl format`, no
    hand-editing.
    
    Bug: 386918226
    Change-Id: Ia68292338e695331aa48816ae82a9265855f9afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6202989
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412076}

--- tools/aggregation_service/aggregation_service_tool_main.cc
+++ tools/aggregation_service/aggregation_service_tool_main.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <algorithm>
 #include <iterator>
 #include <string>
 #include <string_view>
@@ -11,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
