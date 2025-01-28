2347340f767e62573c0fedc5e14ec461f836c8c7
Erik Chen <erikchen@chromium.org>
2025-01-28 15:15:54 -0800

    fedcm: Delete diagnostic DumpWithoutCrashing.
    It no longer occurs in the wild.
    
    Change-Id: If8b52f276f52fcfc9cde0adf281897cfb56c950e
    Bug: 377803489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6208675
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412592}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.h"
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -83,15 +82,6 @@ FedCmAccountSelectionView::~FedCmAccountSelectionView() {
 }
 
 void FedCmAccountSelectionView::ShowDialogWidget() {
-  Browser* browser = chrome::FindBrowserWithTab(web_contents());
-  if (browser &&
-      browser->tab_strip_model()->GetActiveWebContents() != web_contents()) {
-    // This is unexpected since we should never reach this codepath when the
-    // WebContents is not the active one. Dump to get debug info on when this
-    // happens.
-    base::debug::DumpWithoutCrashing();
-  }
-
   // Initialize InputEventActivationProtector to handle potentially unintended
   // input events. Do not override `input_protector_` set by
   // SetInputEventActivationProtectorForTesting().
