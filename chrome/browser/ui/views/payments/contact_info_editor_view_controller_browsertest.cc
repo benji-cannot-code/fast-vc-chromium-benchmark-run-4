700a6a7c28aa95c507f0bcbd993e294b6ef9034d
Jan Keitel <jkeitel@google.com>
2025-01-27 23:02:28 -0800

    Add AddressDataChangedWaiter
    This class has the same purpose as the PersonalDataChangedWaiter - it
    just listens only to address-related change events. This is another
    step towards eventually removing PDM.
    
    To illustrate that the class works, this CL replaces the new class
    in ContactInfoEditorViewControllerBrowsertest.
    
    Bug: 40100455
    Change-Id: I21a2a3569e2b17e21e9190044a25cba6d81f00b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197087
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412122}

--- chrome/browser/ui/views/payments/contact_info_editor_view_controller_browsertest.cc
+++ chrome/browser/ui/views/payments/contact_info_editor_view_controller_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/payments/payment_request_browsertest_base.h"
 #include "chrome/browser/ui/views/payments/payment_request_dialog_view_ids.h"
 #include "components/autofill/core/browser/data_manager/addresses/address_data_manager.h"
+#include "components/autofill/core/browser/data_manager/addresses/address_data_manager_test_utils.h"
 #include "components/autofill/core/browser/data_manager/personal_data_manager.h"
 #include "components/autofill/core/browser/test_utils/autofill_test_utils.h"
 #include "content/public/test/browser_test.h"
@@ -21,11 +22,11 @@ namespace payments {
 
 namespace {
 
-const char16_t kNameFull[] = u"Kirby Puckett";
-const char16_t kPhoneNumber[] = u"6515558946";
-const char16_t kPhoneNumberInvalid[] = u"123";
-const char16_t kEmailAddress[] = u"kirby@example.test";
-const char16_t kEmailAddressInvalid[] = u"kirby";
+constexpr char16_t kNameFull[] = u"Kirby Puckett";
+constexpr char16_t kPhoneNumber[] = u"6515558946";
+constexpr char16_t kPhoneNumberInvalid[] = u"123";
+constexpr char16_t kEmailAddress[] = u"kirby@example.test";
+constexpr char16_t kEmailAddressInvalid[] = u"kirby";
 
 std::string GetLocale() {
   return g_browser_process->GetApplicationLocale();
@@ -42,13 +43,7 @@ std::string GetLocale() {
   PaymentRequestContactInfoEditorTest
 #endif
 
-class MAYBE_PaymentRequestContactInfoEditorTest
-    : public PaymentRequestBrowserTestBase {
- protected:
-  MAYBE_PaymentRequestContactInfoEditorTest() = default;
-
-  PersonalDataLoadedObserverMock personal_data_observer_;
-};
+using MAYBE_PaymentRequestContactInfoEditorTest = PaymentRequestBrowserTestBase;
 
 IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest, HappyPath) {
   // Installs two apps so that the Payment Request UI will be shown.
@@ -73,21 +68,14 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest, HappyPath) {
   SetEditorTextfieldValue(kPhoneNumber, autofill::PHONE_HOME_WHOLE_NUMBER);
   SetEditorTextfieldValue(kEmailAddress, autofill::EMAIL_ADDRESS);
 
-  autofill::PersonalDataManager* personal_data_manager = GetDataManager();
-  personal_data_manager->AddObserver(&personal_data_observer_);
-
   // Wait until the web database has been updated and the notification sent.
-  base::RunLoop data_loop;
-  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-      .WillOnce(QuitMessageLoop(&data_loop));
+  autofill::AddressDataChangedWaiter waiter(address_data_manager());
   ClickOnDialogViewAndWait(DialogViewID::EDITOR_SAVE_BUTTON);
-  data_loop.Run();
+  std::move(waiter).Wait();
 
-  personal_data_manager->RemoveObserver(&personal_data_observer_);
-  ASSERT_EQ(1UL,
-            personal_data_manager->address_data_manager().GetProfiles().size());
+  ASSERT_EQ(1UL, address_data_manager()->GetProfiles().size());
   const autofill::AutofillProfile* profile =
-      personal_data_manager->address_data_manager().GetProfiles()[0];
+      address_data_manager()->GetProfiles()[0];
   DCHECK(profile);
 
   EXPECT_EQ(kNameFull, profile->GetInfo(autofill::NAME_FULL, GetLocale()));
@@ -126,24 +114,16 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest,
   SetEditorTextfieldValue(kPhoneNumber, autofill::PHONE_HOME_WHOLE_NUMBER);
   SetEditorTextfieldValue(kEmailAddress, autofill::EMAIL_ADDRESS);
 
-  autofill::PersonalDataManager* personal_data_manager = GetDataManager();
-  personal_data_manager->AddObserver(&personal_data_observer_);
-
   // Wait until the web database has been updated and the notification sent.
-  base::RunLoop data_loop;
-  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-      .WillOnce(QuitMessageLoop(&data_loop));
   views::View* editor_sheet = dialog_view()->GetViewByID(
       static_cast<int>(DialogViewID::CONTACT_INFO_EDITOR_SHEET));
   EXPECT_TRUE(editor_sheet->AcceleratorPressed(
       ui::Accelerator(ui::VKEY_RETURN, ui::EF_NONE)));
-  data_loop.Run();
+  autofill::AddressDataChangedWaiter(address_data_manager()).Wait();
 
-  personal_data_manager->RemoveObserver(&personal_data_observer_);
-  ASSERT_EQ(1UL,
-            personal_data_manager->address_data_manager().GetProfiles().size());
+  ASSERT_EQ(1UL, address_data_manager()->GetProfiles().size());
   const autofill::AutofillProfile* profile =
-      personal_data_manager->address_data_manager().GetProfiles()[0];
+      address_data_manager()->GetProfiles()[0];
   DCHECK(profile);
 
   EXPECT_EQ(kNameFull, profile->GetInfo(autofill::NAME_FULL, GetLocale()));
@@ -192,21 +172,14 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest, Validation) {
   EXPECT_FALSE(IsEditorTextfieldInvalid(autofill::PHONE_HOME_WHOLE_NUMBER));
   EXPECT_FALSE(IsEditorTextfieldInvalid(autofill::EMAIL_ADDRESS));
 
-  autofill::PersonalDataManager* personal_data_manager = GetDataManager();
-  personal_data_manager->AddObserver(&personal_data_observer_);
-
   // Wait until the web database has been updated and the notification sent.
-  base::RunLoop data_loop;
-  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-      .WillOnce(QuitMessageLoop(&data_loop));
+  autofill::AddressDataChangedWaiter waiter(address_data_manager());
   ClickOnDialogViewAndWait(DialogViewID::EDITOR_SAVE_BUTTON);
-  data_loop.Run();
+  std::move(waiter).Wait();
 
-  personal_data_manager->RemoveObserver(&personal_data_observer_);
-  ASSERT_EQ(1UL,
-            personal_data_manager->address_data_manager().GetProfiles().size());
+  ASSERT_EQ(1UL, address_data_manager()->GetProfiles().size());
   const autofill::AutofillProfile* profile =
-      personal_data_manager->address_data_manager().GetProfiles()[0];
+      address_data_manager()->GetProfiles()[0];
   DCHECK(profile);
 
   EXPECT_EQ(kNameFull, profile->GetInfo(autofill::NAME_FULL, GetLocale()));
@@ -227,8 +200,6 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest,
                     &b_method_name);
 
   NavigateTo("/payment_request_contact_details_test.html");
-  autofill::PersonalDataManager* personal_data_manager = GetDataManager();
-  personal_data_manager->AddObserver(&personal_data_observer_);
 
   autofill::AutofillProfile incomplete_profile(
       autofill::i18n_model_definition::kLegacyHierarchyCountryCode);
@@ -254,17 +225,13 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest,
   SetEditorTextfieldValue(kEmailAddress, autofill::EMAIL_ADDRESS);
 
   // Wait until the web database has been updated and the notification sent.
-  base::RunLoop save_data_loop;
-  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-      .WillOnce(QuitMessageLoop(&save_data_loop));
+  autofill::AddressDataChangedWaiter waiter(address_data_manager());
   ClickOnDialogViewAndWait(DialogViewID::EDITOR_SAVE_BUTTON);
-  save_data_loop.Run();
+  std::move(waiter).Wait();
 
-  personal_data_manager->RemoveObserver(&personal_data_observer_);
-  ASSERT_EQ(1UL,
-            personal_data_manager->address_data_manager().GetProfiles().size());
+  ASSERT_EQ(1UL, address_data_manager()->GetProfiles().size());
   const autofill::AutofillProfile* profile =
-      personal_data_manager->address_data_manager().GetProfiles()[0];
+      address_data_manager()->GetProfiles()[0];
   DCHECK(profile);
 
   EXPECT_EQ(kNameFull, profile->GetInfo(autofill::NAME_FULL, GetLocale()));
@@ -285,8 +252,6 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest,
                     &b_method_name);
 
   NavigateTo("/payment_request_contact_details_test.html");
-  autofill::PersonalDataManager* personal_data_manager = GetDataManager();
-  personal_data_manager->AddObserver(&personal_data_observer_);
 
   autofill::AutofillProfile incomplete_profile(
       autofill::i18n_model_definition::kLegacyHierarchyCountryCode);
@@ -320,17 +285,13 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest,
   SetEditorTextfieldValue(kEmailAddress, autofill::EMAIL_ADDRESS);
 
   // Wait until the web database has been updated and the notification sent.
-  base::RunLoop save_data_loop;
-  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-      .WillOnce(QuitMessageLoop(&save_data_loop));
+  autofill::AddressDataChangedWaiter waiter(address_data_manager());
   ClickOnDialogViewAndWait(DialogViewID::EDITOR_SAVE_BUTTON);
-  save_data_loop.Run();
+  std::move(waiter).Wait();
 
-  personal_data_manager->RemoveObserver(&personal_data_observer_);
   autofill::AutofillProfile* profile =
       request->state()->selected_contact_profile();
   DCHECK(profile);
-
   EXPECT_EQ(u"16515558946",
             profile->GetInfo(autofill::PHONE_HOME_WHOLE_NUMBER, GetLocale()));
   EXPECT_EQ(kEmailAddress,
@@ -366,17 +327,11 @@ IN_PROC_BROWSER_TEST_F(MAYBE_PaymentRequestContactInfoEditorTest,
   SetEditorTextfieldValue(kPhoneNumber, autofill::PHONE_HOME_WHOLE_NUMBER);
   SetEditorTextfieldValue(kEmailAddress, autofill::EMAIL_ADDRESS);
 
-  autofill::PersonalDataManager* personal_data_manager = GetDataManager();
-  personal_data_manager->AddObserver(&personal_data_observer_);
-
-  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged()).Times(0);
   ClickOnDialogViewAndWait(DialogViewID::EDITOR_SAVE_BUTTON);
 
-  personal_data_manager->RemoveObserver(&personal_data_observer_);
   // In incognito, the profile should be available in contact_profiles but it
   // shouldn't be saved to the PersonalDataManager.
-  ASSERT_EQ(0UL,
-            personal_data_manager->address_data_manager().GetProfiles().size());
+  ASSERT_EQ(0UL, address_data_manager()->GetProfiles().size());
   PaymentRequest* request = GetPaymentRequests().front();
   EXPECT_EQ(1U, request->state()->contact_profiles().size());
   EXPECT_EQ(request->state()->contact_profiles().back(),
--- chrome/browser/ui/views/payments/payment_request_browsertest_base.h
+++ chrome/browser/ui/views/payments/payment_request_browsertest_base.h
@@ -32,8 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/payments/payment_request.mojom-forward.h"
 
 namespace autofill {
+class AddressDataManager;
 class AutofillProfile;
 class CreditCard;
+class PaymentsDataManager;
 }  // namespace autofill
 
 namespace content {
@@ -183,6 +185,8 @@ class PaymentRequestBrowserTestBase
   const std::vector<PaymentRequest*> GetPaymentRequests();
 
   autofill::PersonalDataManager* GetDataManager();
+  autofill::AddressDataManager* address_data_manager();
+  autofill::PaymentsDataManager* payments_data_manager();
   // Adds the various models to the database, waiting until the personal data
   // manager notifies that they are added.
   // NOTE: If no use_count is specified on the models and multiple items are
