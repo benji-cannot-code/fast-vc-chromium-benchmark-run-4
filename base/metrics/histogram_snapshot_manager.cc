0ea1d989d667181100af2dc2a19b374ae1f0fe38
Alexei Svitkine <asvitkine@chromium.org>
2025-01-28 15:13:28 -0800

    Return early in PrepareSamples() when a histogram has no samples.
    This has no real behavior change, as a delta of an empty histogram is equivalent to an empty histogram. However, it allows skipping corruption checks in the case when there are no samples, which can be expensive due to the crc32 over the bucket ranges.
    
    Bug: 360262188
    Change-Id: Idf3faefe19a749636f3f7ab4b36aa811022b5b7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6209648
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412591}

--- base/metrics/histogram_snapshot_manager.cc
+++ base/metrics/histogram_snapshot_manager.cc
@@ -50,6 +50,9 @@ void HistogramSnapshotManager::PrepareFinalDelta(
 void HistogramSnapshotManager::PrepareSamples(const HistogramBase* histogram,
                                               const HistogramSamples& samples) {
   DCHECK(histogram_flattener_);
+  if (samples.TotalCount() <= 0) {
+    return;
+  }
 
   // Crash if we detect that our histograms have been overwritten.  This may be
   // a fair distance from the memory smasher, but we hope to correlate these
@@ -84,9 +87,7 @@ void HistogramSnapshotManager::PrepareSamples(const HistogramBase* histogram,
     return;
   }
 
-  if (samples.TotalCount() > 0) {
-    histogram_flattener_->RecordDelta(*histogram, samples);
-  }
+  histogram_flattener_->RecordDelta(*histogram, samples);
 }
 
 }  // namespace base
