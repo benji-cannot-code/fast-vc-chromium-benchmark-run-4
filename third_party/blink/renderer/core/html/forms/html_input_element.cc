d162fa617728a0c9fda7398766ce29254bddf604
David Baron <dbaron@chromium.org>
2025-01-28 12:41:55 -0800

    Add feature flag for two DOM insertion performance improvements.
    This adds a feature flag for the performance improvements from:
    https://crrev.com/5d85ef800f321bf1cbe7eaee53354529c5a9a0ee
    https://crrev.com/a508e23b19ac422c82f3325a9d0cd3881ea000a8
    both so that we have a killswitch if needed, and so that we can measure
    their benefits.
    
    Change-Id: I20939aa105c8f554db7389deedfaff19b65ef534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6208788
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412503}

--- third_party/blink/renderer/core/html/forms/html_input_element.cc
+++ third_party/blink/renderer/core/html/forms/html_input_element.cc
@@ -2272,6 +2272,9 @@ void HTMLInputElement::AdjustStyle(ComputedStyleBuilder& builder) {
 
 void HTMLInputElement::DidNotifySubtreeInsertionsToDocument() {
   input_type_view_->ListAttributeTargetChanged();
+  if (!RuntimeEnabledFeatures::DOMInsertionFasterEnabled()) {
+    PseudoStateChanged(CSSSelector::kPseudoHasDatalist);
+  }
 }
 
 AXObject* HTMLInputElement::PopupRootAXObject() {
