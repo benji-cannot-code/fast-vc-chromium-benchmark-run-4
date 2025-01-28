1a97f1478227f0849d6911208733a7039445afc1
Henrique Nakashima <hnakashima@chromium.org>
2025-01-28 11:46:51 -0800

    [Android] Port testHideMenuOnToggleOverview to Public Transit
    The new test is simpler and batched. The restriction to phones was
    also removed.
    
    In the framework, implement Transition.runTriggerOnUiThreadOption()
    to avoid nested lambdas as Triggers like here.
    
    Lastly, delete TabbedAppMenuTest#testMenuNewTab() since it's covered by
    TabbedAppMenuPTTest#testOpenNewTab().
    
    Bug: 354278364,330938767
    Change-Id: I91e4f95e76862e8b52ea91bb4eded9132bf5a779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6202902
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412455}

--- base/test/android/javatests/src/org/chromium/base/test/transit/Transition.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Transition.java
@@ -8,6 +8,7 @@ package org.chromium.base.test.transit;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.Log;
+import org.chromium.base.ThreadUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -124,7 +125,11 @@ public abstract class Transition {
         if (mTrigger != null) {
             Log.i(TAG, "%s: will run trigger", toDebugString());
             try {
-                mTrigger.triggerTransition();
+                if (mOptions.mRunTriggerOnUiThread) {
+                    ThreadUtils.runOnUiThread(mTrigger::triggerTransition);
+                } else {
+                    mTrigger.triggerTransition();
+                }
                 Log.i(TAG, "%s: finished running trigger", toDebugString());
             } catch (Throwable e) {
                 throw TravelException.newTravelException(
@@ -213,6 +218,11 @@ public abstract class Transition {
         return builder.build();
     }
 
+    /** Convenience method equivalent to newOptions().withRunTriggerOnUiThread().build(). */
+    public static TransitionOptions runTriggerOnUiThreadOption() {
+        return newOptions().withRunTriggerOnUiThread().build();
+    }
+
     /** Options to configure the Transition. */
     public static class TransitionOptions {
 
@@ -221,6 +231,7 @@ public abstract class Transition {
         long mTimeoutMs;
         int mTries = 1;
         boolean mPossiblyAlreadyFulfilled;
+        boolean mRunTriggerOnUiThread;
 
         private TransitionOptions() {}
 
@@ -262,6 +273,12 @@ public abstract class Transition {
             /** The Transition's Conditions might already be all fulfilled before the Trigger. */
             public Builder withPossiblyAlreadyFulfilled() {
                 mPossiblyAlreadyFulfilled = true;
+                return this;
+            }
+
+            /** Run the {@link Trigger} on the UI Thread. */
+            public Builder withRunTriggerOnUiThread() {
+                mRunTriggerOnUiThread = true;
                 return this;
             }
         }
