314918f80b20747f51aa1321a4fb2b8bb1543212
Ramin Halavati <rhalavati@google.com>
2025-01-28 09:36:26 -0800

    Update minimum expected version of ChromeScreenAI library.
    This is a long overdue CL to update the minimum supported version of
    ChromeScreenAI library. It is changed to the current version to force
    all clients (hence forth) to use the latest version that include SSE3
    fixes in it.
    
    AX-Relnotes: n/a
    Bug: 378472917
    Change-Id: I5ba98da7a543f50084ae8e218a7b0cee23d82789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6206851
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412356}

--- chrome/browser/screen_ai/screen_ai_install_state.cc
+++ chrome/browser/screen_ai/screen_ai_install_state.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const int kScreenAICleanUpDelayInDays = 30;
-const char kMinExpectedVersion[] = "124.2";
+const char kMinExpectedVersion[] = "127.13";
 
 bool IsDeviceCompatible() {
 #if BUILDFLAG(IS_LINUX)
