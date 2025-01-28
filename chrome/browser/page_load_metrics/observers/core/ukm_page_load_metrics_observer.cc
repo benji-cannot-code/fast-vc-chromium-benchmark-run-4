c314151eb251f01fec0b25bf0ccc7bc64b339ec5
Kamil Jarosz <kjarosz@google.com>
2025-01-28 01:38:12 -0800

    metrics: Add a histogram for max CLS in Incognito
    This patch adds a histogram
    PageLoad.LayoutInstability.MaxCumulativeShiftScore.SessionWindow.Gap1000ms.Max5000ms2.Incognito,
    which is an incognito-only version of the metric
    PageLoad.LayoutInstability.MaxCumulativeShiftScore.SessionWindow.Gap1000ms.Max5000ms2.
    
    Bug: 378052451
    Change-Id: Ifdeb95410e2a01f59c700df8db2e68a3f40862b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179894
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412174}

--- chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.cc
@@ -79,6 +79,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using page_load_metrics::PageVisitFinalStatus;
 
+namespace internal {
+
+const char
+    kHistogramLayoutInstabilityMaxCumulativeShiftScoreSessionWindowGap1000msMax5000ms2
+        [] = "PageLoad.LayoutInstability.MaxCumulativeShiftScore.SessionWindow."
+             "Gap1000ms.Max5000ms2";
+const char
+    kHistogramLayoutInstabilityMaxCumulativeShiftScoreSessionWindowGap1000msMax5000ms2Incognito
+        [] = "PageLoad.LayoutInstability.MaxCumulativeShiftScore.SessionWindow."
+             "Gap1000ms.Max5000ms2.Incognito";
+
+}  // namespace internal
+
 namespace {
 
 const char kOfflinePreviewsMimeType[] = "multipart/related";
@@ -214,17 +227,19 @@ int64_t CalculateLCPEntropyBucket(double bpp) {
 
 // static
 std::unique_ptr<page_load_metrics::PageLoadMetricsObserver>
-UkmPageLoadMetricsObserver::CreateIfNeeded() {
+UkmPageLoadMetricsObserver::CreateIfNeeded(bool is_incognito) {
   if (!ukm::UkmRecorder::Get()) {
     return nullptr;
   }
   return std::make_unique<UkmPageLoadMetricsObserver>(
-      g_browser_process->network_quality_tracker());
+      g_browser_process->network_quality_tracker(), is_incognito);
 }
 
 UkmPageLoadMetricsObserver::UkmPageLoadMetricsObserver(
-    network::NetworkQualityTracker* network_quality_tracker)
-    : network_quality_tracker_(network_quality_tracker) {
+    network::NetworkQualityTracker* network_quality_tracker,
+    bool is_incognito)
+    : network_quality_tracker_(network_quality_tracker),
+      is_incognito_(is_incognito) {
   DCHECK(network_quality_tracker_);
 }
 
@@ -1313,11 +1328,19 @@ void UkmPageLoadMetricsObserver::ReportLayoutStability() {
     builder
         .SetLayoutInstability_MaxCumulativeShiftScore_SessionWindow_Gap1000ms_Max5000ms(
             page_load_metrics::LayoutShiftUkmValue(*cwv_cls_value));
+    auto sample = page_load_metrics::LayoutShiftUmaValue10000(*cwv_cls_value);
     base::UmaHistogramCustomCounts(
-        "PageLoad.LayoutInstability.MaxCumulativeShiftScore.SessionWindow."
-        "Gap1000ms.Max5000ms2",
-        page_load_metrics::LayoutShiftUmaValue10000(*cwv_cls_value), 1, 24000,
-        50);
+        internal::
+            kHistogramLayoutInstabilityMaxCumulativeShiftScoreSessionWindowGap1000msMax5000ms2,
+        sample, 1, 24000, 50);
+
+    if (is_incognito_) {
+      base::UmaHistogramCustomCounts(
+          internal::
+              kHistogramLayoutInstabilityMaxCumulativeShiftScoreSessionWindowGap1000msMax5000ms2Incognito,
+          sample, 1, 24000, 50);
+    }
+
     // The pseudo metric of PageLoad.LayoutInstability.MaxCumulativeShiftScore.
     // SessionWindow.Gap1000ms.Max5000ms2.
     // Only used to assess field trial data quality.
