5c68cfa889b26215b8d31f4140f2e2505acfb500
Timofey Chudakov <tchudakov@google.com>
2025-01-28 06:03:23 -0800

    [HaTS] Adjust kDidChoosePlusAddressOverEmail survey trigger condition.
    After aligning with the UX team, it was decided that the
    kDidChoosePlusAddressOverEmail survey should be triggered even if
    the popup doesn't have other alternative email addresses offered.
    
    Bug: 348139343
    Change-Id: I06fe56856444cb126a67874ebff215035b66ef65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198873
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412257}

--- components/autofill/core/browser/ui/autofill_external_delegate_unittest.cc
+++ components/autofill/core/browser/ui/autofill_external_delegate_unittest.cc
@@ -1302,13 +1302,10 @@ TEST_F(AutofillExternalDelegatePlusAddressTest,
 
   EXPECT_CALL(client(), ShowAutofillSuggestions(
                             PopupOpenArgsAre(SuggestionVectorIdsAre(
-                                SuggestionType::kAddressEntry,
                                 SuggestionType::kFillExistingPlusAddress)),
                             _));
   const std::u16string plus_address = u"test+plus@test.example";
   std::vector<Suggestion> suggestions;
-  suggestions.emplace_back(/*main_text=*/u"example@gmail.com",
-                           SuggestionType::kAddressEntry);
   suggestions.emplace_back(/*main_text=*/plus_address,
                            SuggestionType::kFillExistingPlusAddress);
   // This function tests the filling of existing plus addresses, which is why
@@ -1323,7 +1320,7 @@ TEST_F(AutofillExternalDelegatePlusAddressTest,
                          HasQueriedFormId(), HasQueriedFieldId(), plus_address,
                          SuggestionType::kFillExistingPlusAddress,
                          std::optional(EMAIL_ADDRESS)));
-  external_delegate().DidSelectSuggestion(suggestions[1]);
+  external_delegate().DidSelectSuggestion(suggestions[0]);
   EXPECT_CALL(client(), HideAutofillSuggestions(
                             SuggestionHidingReason::kAcceptSuggestion));
   EXPECT_CALL(plus_address_delegate(),
@@ -1342,7 +1339,7 @@ TEST_F(AutofillExternalDelegatePlusAddressTest,
                          HasQueriedFormId(), HasQueriedFieldId(), plus_address,
                          SuggestionType::kFillExistingPlusAddress,
                          std::optional(EMAIL_ADDRESS)));
-  external_delegate().DidAcceptSuggestion(suggestions[1],
+  external_delegate().DidAcceptSuggestion(suggestions[0],
                                           SuggestionPosition{.row = 0});
 }
 
