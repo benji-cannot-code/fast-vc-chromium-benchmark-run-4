d107f30c8e5691c7bfbdf075a323aa318a3ef146
Sergiy Belozorov <sergiyb@chromium.org>
2025-01-28 09:30:16 -0800

    Report ownership check status during enrollment recovery
    Bug: 367711432
    Change-Id: Icac096389e3d94e51d5da6178dcd8c5598ee08de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823372
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412353}

--- chrome/browser/ash/policy/enrollment/enrollment_state_fetcher_unittest.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_state_fetcher_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "build/branding_buildflags.h"
-#include "chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.h"
 #include "chrome/browser/ash/policy/enrollment/auto_enrollment_state.h"
 #include "chrome/browser/ash/policy/enrollment/auto_enrollment_type_checker.h"
 #include "chrome/browser/ash/policy/enrollment/enrollment_test_helper.h"
@@ -29,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/server_backed_state/server_backed_device_state.h"
 #include "chrome/browser/ash/policy/server_backed_state/server_backed_state_keys_broker.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
+#include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/ash/components/install_attributes/stub_install_attributes.h"
 #include "chromeos/ash/components/system/fake_statistics_provider.h"
@@ -217,9 +217,7 @@ class EnrollmentStateFetcherTest : public testing::Test {
     command_line_.GetProcessCommandLine()->AppendSwitchASCII(
         ash::switches::kEnterpriseEnableUnifiedStateDetermination,
         AutoEnrollmentTypeChecker::kUnifiedStateDeterminationAlways);
-
-    DeviceCloudPolicyManagerAsh::RegisterPrefs(local_state_.registry());
-    EnrollmentStateFetcher::RegisterPrefs(local_state_.registry());
+    RegisterLocalState(local_state_.registry());
 
     statistics_provider_.SetMachineStatistic(ash::system::kSerialNumberKey,
                                              kTestSerialNumber);
@@ -349,8 +347,7 @@ TEST_F(EnrollmentStateFetcherTest, RegisterPrefs) {
   TestingPrefServiceSimple local_state;
   auto* registry = local_state.registry();
 
-  DeviceCloudPolicyManagerAsh::RegisterPrefs(registry);
-  EnrollmentStateFetcher::RegisterPrefs(registry);
+  RegisterLocalState(registry);
 
   const base::Value* value;
   auto defaults = registry->defaults();
@@ -362,6 +359,8 @@ TEST_F(EnrollmentStateFetcherTest, RegisterPrefs) {
   ASSERT_TRUE(
       defaults->GetValue(prefs::kEnrollmentPsmDeterminationTime, &value));
   EXPECT_EQ(value->GetString(), "0");
+  ASSERT_TRUE(defaults->GetValue(prefs::kEnrollmentRecoveryRequired, &value));
+  EXPECT_EQ(value->GetBool(), false);
 }
 
 TEST_F(EnrollmentStateFetcherTest, DisabledViaSwitches) {
