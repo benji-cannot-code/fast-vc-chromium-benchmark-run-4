d8a1381de588d2721add144d8c9dce3359e0daf6
Aliona DANGLA <alionadangla@google.com>
2025-01-28 07:46:03 -0800

    [iOS] Add TabGroupMediatorDelegate
    Bug: 378880292
    Change-Id: I4fd01983be82ebf9541b8e436b35c8bee38a0f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196746
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412301}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_coordinator.mm
@@ -49,6 +49,7 @@ constexpr CGFloat kTabGroupBackgroundElementDurationFactor = 0.75;
 @interface TabGroupCoordinator () <
     GridViewControllerDelegate,
     SharedTabGroupUserEducationCoordinatorDelegate,
+    TabGroupMediatorDelegate,
     TabGroupPresentationCommands>
 @end
 
@@ -122,6 +123,7 @@ constexpr CGFloat kTabGroupBackgroundElementDurationFactor = 0.75;
   _mediator.tabGroupsHandler = HandlerForProtocol(
       self.browser->GetCommandDispatcher(), TabGroupsCommands);
   _mediator.tabGridIdleStatusHandler = self.tabGridIdleStatusHandler;
+  _mediator.tabGroupDelegate = self;
 
   _tabContextMenuHelper = [[TabContextMenuHelper alloc]
              initWithProfile:browser->GetProfile()
@@ -398,6 +400,16 @@ constexpr CGFloat kTabGroupBackgroundElementDurationFactor = 0.75;
     (SharedTabGroupUserEducationCoordinator*)coordinator {
   [_userEducationCoordinator stop];
   _userEducationCoordinator = nil;
+}
+
+#pragma mark - TabGroupMediatorDelegate
+
+- (void)tabGroupMediatorCloseLastTabAsOwner:(TabGroupMediator*)mediator {
+  CHECK_EQ(_mediator, mediator);
+}
+
+- (void)tabGroupMediatorCloseLastTabAsMember:(TabGroupMediator*)mediator {
+  CHECK_EQ(_mediator, mediator);
 }
 
 #pragma mark - Private
