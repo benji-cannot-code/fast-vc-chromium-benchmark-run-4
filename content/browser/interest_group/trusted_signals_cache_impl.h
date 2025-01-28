f82d7952cba01ef108aec396c6ef1616c3e6c344
Matt Menke <mmenke@chromium.org>
2025-01-28 10:34:17 -0800

    [Protected Audiences] Add cleanup timer for unused entries.
    This adds a timer to clean up entries that have been in the trusted
    signals KVv2 cache for more than 10 minutes without being used. The
    current value is arbitrary - we'll likely want to experiment with it
    once we have consumers. The timer is relatively short because entries
    are stored in memory. We may well end up with an even shorter time,
    long term.
    
    This code has a min and max time for when the cleanup task will
    remove each entry (subject to CPU resources to run the cleanup task).
    This means if 100 entries are added in a short time, only one task may
    run and clean up all entries. Unclear if this is worth the
    investment, though the logic to do that doesn't add much complexity
    to production code - it mostly makes tests more complicated.
    
    The timer does not currently take into account cache entry's TTL as
    provided by the server. In the future, we may want to make that
    change, but we may need to think about how to do so (e.g., if we set
    the timer based on the first to expire, we'd need to search for it,
    or sort the LRU list differently - but the latter would affect
    eviction, too. Or we could set it only on the last entry, but then
    expired entries might be cached too long, etc).
    
    Bug: 333445540
    Change-Id: I4e14bddf3d4c4652d4492d58b4bcbab5ac258bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196289
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412410}

--- content/browser/interest_group/trusted_signals_cache_impl.h
+++ content/browser/interest_group/trusted_signals_cache_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "base/types/optional_ref.h"
 #include "base/unguessable_token.h"
 #include "base/values.h"
@@ -191,6 +192,24 @@ class CONTENT_EXPORT TrustedSignalsCacheImpl
   static constexpr size_t kMaxCacheSizeBytes = 10 * 1024 * 1024;
 
   static constexpr size_t kNonceCacheSize = 50;
+
+  // If data for a compression group has no outstanding Handle for this amount
+  // of time, it will be removed to save memory. Entries are guaranteed to be
+  // destroyed between `kMinUnusedCleanupTime` and `kMaxUnusedCleanupTime` times
+  // - the logic records when the minimum time is when a compression group is
+  // added to the LruList, but the shared-across-groups cleanup timer is set to
+  // run after the expiration time of the next group to expire plus the
+  // `kCleanupInterval` has passed, and then cleans up all entries that are past
+  // the `kMinUnusedCleanupTime`. This avoids potentially running a bunch of
+  // very short timers when there are a lot of entries that expire at about the
+  // same time. Unclear if the extra complexity is worth it.
+  //
+  // None of this logic currently takes into account when a compression group's
+  // TTL from the server has been reached.
+  static constexpr base::TimeDelta kMinUnusedCleanupTime = base::Minutes(10);
+  static constexpr base::TimeDelta kCleanupInterval = base::Seconds(30);
+  static constexpr base::TimeDelta kMaxUnusedCleanupTime =
+      kMinUnusedCleanupTime + kCleanupInterval;
 
   // If StartFetch() isn't called on any handle for a request that has been
   // around this long, automatically call SetFetchCanStart() for the fetch.
@@ -609,6 +628,14 @@ class CONTENT_EXPORT TrustedSignalsCacheImpl
   base::UnguessableToken GetNetworkPartitionNonce(
       const NetworkPartitionNonceKey& network_partition_nonce_key);
 
+  // Starts `cleanup_lru_timer_` if it's not already running and `lru_list_` is
+  // not empty.
+  void MaybeStartCleanupTimer();
+
+  // Removes up all entries in `lru_list_` that have passed their scheduled
+  // removal time.
+  void Cleanup();
+
   // Virtual for testing.
   virtual std::unique_ptr<TrustedSignalsFetcher> CreateFetcher();
 
@@ -656,6 +683,8 @@ class CONTENT_EXPORT TrustedSignalsCacheImpl
 
   // See LruList for details.
   LruList lru_list_;
+
+  base::OneShotTimer cleanup_timer_;
 };
 
 }  // namespace content
