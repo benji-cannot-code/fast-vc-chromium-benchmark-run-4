ab951694ba09a649e4e4a23847317ed53d5e2b82
Adam Abeshouse <aabeshouse@google.com>
2025-01-28 12:09:54 -0800

    [FPF] Check tracking protection settings in GetActivation
    When the FP toggle in Tracking Protection settings is visible, its value
    should determine whether FPP is active. With this CL we check that
    setting when deciding whether to activate.
    
    GetActivation is the best place for this check because Tracking
    Protection settings should take immediate effect, and not require
    restarting the browser or opening a new tab.
    
    As part of this change, we also add the check for the
    `FingerprintingProtectionUx` flag to the other flag check functions so
    that core filter objects are also created when
    `FingerprintingProtectionUx` is enabled even when the other FPP flags
    are not.
    
    Hand-tested with all flag combinations of FingerprintingProtectionUx,
    EnableFingerprintingProtectionFilter,
    EnableFingerprintingProtectionFilterInIncognito.
    
    Bug: 390395545
    Change-Id: Ieb0c284750a879c0470cae6b4ed1f0fc6218297c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180250
    Commit-Queue: Adam Abeshouse <aabeshouse@google.com>
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Martin Verde <thesalsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412478}

--- components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.cc
+++ components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/rand_util.h"
+#include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/subresource_filter/core/mojom/subresource_filter.mojom.h"
 #include "fingerprinting_protection_filter_features.h"
 
@@ -27,13 +28,17 @@ BASE_FEATURE(kEnableFingerprintingProtectionFilterInIncognito,
 bool IsFingerprintingProtectionFeatureEnabled() {
   return base::FeatureList::IsEnabled(kEnableFingerprintingProtectionFilter) ||
          base::FeatureList::IsEnabled(
-             kEnableFingerprintingProtectionFilterInIncognito);
+             kEnableFingerprintingProtectionFilterInIncognito) ||
+         base::FeatureList::IsEnabled(
+             privacy_sandbox::kFingerprintingProtectionUx);
 }
 
 bool IsFingerprintingProtectionEnabledForIncognitoState(bool is_incognito) {
   if (is_incognito) {
     return base::FeatureList::IsEnabled(
-        kEnableFingerprintingProtectionFilterInIncognito);
+               kEnableFingerprintingProtectionFilterInIncognito) ||
+           base::FeatureList::IsEnabled(
+               privacy_sandbox::kFingerprintingProtectionUx);
   }
   return base::FeatureList::IsEnabled(kEnableFingerprintingProtectionFilter);
 }
