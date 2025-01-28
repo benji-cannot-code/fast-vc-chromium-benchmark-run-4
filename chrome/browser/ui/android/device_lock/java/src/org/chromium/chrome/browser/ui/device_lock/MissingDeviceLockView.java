b202fa5650b3855763ad9f2c021859a3d8bbca04
Menghan Yang <myuu@google.com>
2025-01-28 10:06:54 -0800

    [Android] Update Automotive device lock dialogs UI
    This CL updates the profile lock dialogs UI when UNO_FOR_AUTO flag is
    enabled. It also moves the `signin_header_animation_background` color
    from the app-level module to the `ui/android:ui_java_resources` module
    so it can be accessed programmatically from the `device_lock` module.
    
    See https://crbug.com/367329904#comment13 for screenshots.
    
    Bug: 367329904
    Change-Id: I9e694ce2861592b80cc5531ab30c479e9a8c65bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198187
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Menghan Yang <myuu@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412386}

--- chrome/browser/ui/android/device_lock/java/src/org/chromium/chrome/browser/ui/device_lock/MissingDeviceLockView.java
+++ chrome/browser/ui/android/device_lock/java/src/org/chromium/chrome/browser/ui/device_lock/MissingDeviceLockView.java
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.device_lock;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,6 +19,7 @@ import org.chromium.components.browser_ui.widget.DualControlLayout;
 import org.chromium.components.browser_ui.widget.DualControlLayout.ButtonType;
 import org.chromium.components.signin.SigninFeatureMap;
 import org.chromium.components.signin.SigninFeatures;
+import org.chromium.ui.base.ViewUtils;
 
 /**
  * View shown to a user who has removed the device lock to inform them that their private data will
@@ -72,11 +75,26 @@ public class MissingDeviceLockView extends LinearLayout {
         mButtonBar = findViewById(R.id.dual_control_button_bar);
         mButtonBar.addView(mContinueButton);
         mButtonBar.addView(mCreateDeviceLockButton);
+
+        ImageView illustration = findViewById(R.id.missing_device_lock_illustration);
+        int illustrationTopMargin;
         if (SigninFeatureMap.isEnabled(SigninFeatures.UNO_FOR_AUTO)) {
+            illustration.setBackgroundColor(Color.TRANSPARENT);
+            illustrationTopMargin = ViewUtils.dpToPx(getContext(), 16);
             mButtonBar.setAlignment(DualControlLayout.DualControlLayoutAlignment.END);
         } else {
+            illustration.setBackgroundColor(
+                    getContext().getColor(R.color.signin_header_animation_background));
+            illustrationTopMargin = 0;
             mButtonBar.setAlignment(DualControlLayout.DualControlLayoutAlignment.APART);
         }
+        MarginLayoutParams illustrationParams = (MarginLayoutParams) illustration.getLayoutParams();
+        illustrationParams.setMargins(
+                illustrationParams.leftMargin,
+                illustrationTopMargin,
+                illustrationParams.rightMargin,
+                illustrationParams.bottomMargin);
+        illustration.setLayoutParams(illustrationParams);
     }
 
     TextView getTitle() {
