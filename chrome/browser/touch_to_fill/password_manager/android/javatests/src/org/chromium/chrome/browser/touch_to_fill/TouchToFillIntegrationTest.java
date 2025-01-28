d82e1087a107bda61f2eb7dbe391cef1d5e5df7e
Ashley Newson <ashleynewson@chromium.org>
2025-01-28 10:20:48 -0800

    [Gardener] Disable testConsumesGenericMotionEventsToPreventMouseClicksThroughSheet for Android 14
    This test has a 100% failure rate on many Android 14 bots.
    
    Bug: 392886105
    Change-Id: I2d4034cbc4fbb5f15297debe2532a323b8918aa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6206817
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>
    Owners-Override: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412395}

--- chrome/browser/touch_to_fill/password_manager/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
+++ chrome/browser/touch_to_fill/password_manager/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
@@ -40,6 +40,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -132,6 +133,7 @@ public class TouchToFillIntegrationTest {
 
     @Test
     @MediumTest
+    @DisableIf.Build(sdk_equals = 34)
     public void testConsumesGenericMotionEventsToPreventMouseClicksThroughSheet() {
         runOnUiThreadBlocking(
                 () -> {
