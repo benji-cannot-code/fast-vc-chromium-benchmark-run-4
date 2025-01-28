95a7562196b3107f3cf3466e9f84529a4c8a109c
Ken Buchanan <kenrb@chromium.org>
2025-01-28 09:58:18 -0800

    [WebAuthn] Sync with SDS when renewing PIN
    This triggers a sync with the Security Domain Service when the Enclave
    Manager is performing a PIN renewal, since it appears the operation is
    failing due to being out of sync.
    
    Bug: b/391435614
    Change-Id: I8fcb5f3958ae37273564980a868b918186a15351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6188456
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412377}

--- chrome/browser/webauthn/enclave_manager.cc
+++ chrome/browser/webauthn/enclave_manager.cc
@@ -1543,8 +1543,12 @@ class EnclaveManager::StateMachine {
       }
 
       is_pin_renewal_ = true;
-      state_ = State::kDownloadingRecoveryKeyStoreKeys;
-      DownloadRecoveryKeyStoreKeys();
+      if (base::FeatureList::IsEnabled(
+              device::kSyncSecurityDomainBeforePINRenewal)) {
+        SyncWithSecurityDomain();
+      } else {
+        DownloadRecoveryKeyStoreKeys();
+      }
       return;
     }
 
@@ -1985,6 +1989,12 @@ class EnclaveManager::StateMachine {
       return;
     }
 
+    if (is_pin_renewal_) {
+      // The PIN isn't being changed, so no need to hash.
+      DownloadRecoveryKeyStoreKeys();
+      return;
+    }
+
     state_ = State::kHashingPIN;
     HashPIN(action_->set_pin.empty() ? std::move(action_->updated_pin)
                                      : std::move(action_->set_pin));
@@ -2002,7 +2012,6 @@ class EnclaveManager::StateMachine {
     }
     wrapped_pin_proto_ = hashed_pin_->ToWrappedPIN(generation);
 
-    state_ = State::kDownloadingRecoveryKeyStoreKeys;
     DownloadRecoveryKeyStoreKeys();
   }
 
@@ -2588,6 +2597,7 @@ class EnclaveManager::StateMachine {
   }
 
   void DownloadRecoveryKeyStoreKeys() {
+    state_ = State::kDownloadingRecoveryKeyStoreKeys;
     cert_xml_loader_ = FetchURL(
         manager_->url_loader_factory_.get(),
         device::enclave::kRecoveryKeyStoreCertFileURL,
