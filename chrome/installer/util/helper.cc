ca74f3efec0ca7650bbfa1285a2bb6140a1282d9
Greg Thompson <grt@chromium.org>
2025-01-28 00:17:40 -0800

    [etw tracing] Add helpers for registering the service
    This CL adds helper functions that the browser can eventually use to
    prompt the user to register or unregister the Windows system tracing
    service (e.g., via a button in chrome://traces-internals/).
    
    Bug: 41497361
    Change-Id: I8bff793f59bd29cca655d9579bc1a4ff606815c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182269
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412141}

--- chrome/installer/util/helper.cc
+++ chrome/installer/util/helper.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/install_static/install_util.h"
 #include "chrome/installer/util/initial_preferences.h"
 #include "chrome/installer/util/initial_preferences_constants.h"
+#include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/installation_state.h"
 #include "chrome/installer/util/util_constants.h"
 
@@ -205,6 +206,21 @@ base::FilePath FindInstallPath(bool system_install,
     }
   }
   return {};
+}
+
+bool IsCurrentProcessInstalled() {
+  // Get the directory in which the product is installed as per its registration
+  // with the updater.
+  const base::FilePath install_dir = GetInstalledDirectory(
+      /*system_install=*/!InstallUtil::IsPerUserInstall());
+
+  if (install_dir.empty()) {
+    return false;  // No product installed at this level and mode.
+  }
+
+  // Return true if the current process resides within that directory.
+  const base::FilePath this_exe = base::PathService::CheckedGet(base::FILE_EXE);
+  return install_dir.IsParent(this_exe);
 }
 
 }  // namespace installer.
