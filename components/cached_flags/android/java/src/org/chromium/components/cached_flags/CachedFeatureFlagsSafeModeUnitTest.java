cd5e94c73fda06dac5d5a72284ded9e127c7a245
Martin Kong <martinkong@google.com>
2025-01-28 10:43:47 -0800

    Make about_flags changes require only one restart
    Currently, when the user sets a flag in chrome://flags, they need to
    perform two restarts for the change to apply. In the first restart,
    the changes are propagated from the native side to the Java side
    on startup after the native library is loaded. In the second restart,
    the Java side returns the correct flag values on startup before
    the native library is loaded.
    
    In order to fix this problem, I propagate the changes from the
    native side to the Java side immediately after the user sets the flag
    in chrome://flags by making appropriate JNI calls. This way,
    the Java side is aware of the change before the first restart,
    so that it can return the correct value after the first restart.
    
    This CL fixes the problem for cases where the user is setting the
    flag to "Enabled" or "Disabled". A followup CL will address the cases
    where the user is setting the flag to "Default".
    
    Bug: 40132529
    Change-Id: Ie636279c774f725391e103cd89ea295b11ce2be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6159150
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412419}

--- components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFeatureFlagsSafeModeUnitTest.java
+++ components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFeatureFlagsSafeModeUnitTest.java
@@ -465,7 +465,7 @@ public class CachedFeatureFlagsSafeModeUnitTest {
                 .thenReturn(STRING_PARAM_NATIVE_1);
         CachedFlagUtils.cacheNativeFlags(List.of(Arrays.asList(mCrashyFeature, mOkFeature)));
         CachedFlagUtils.cacheFeatureParams(
-                Arrays.asList(mBoolParam, mIntParam, mDoubleParam, mStringParam));
+                List.of(Arrays.asList(mBoolParam, mIntParam, mDoubleParam, mStringParam)));
 
         clearMemory();
         // Cached values became true(crashy)/true/native1.
@@ -697,7 +697,7 @@ public class CachedFeatureFlagsSafeModeUnitTest {
 
         CachedFlagUtils.cacheNativeFlags(List.of(Arrays.asList(mCrashyFeature, mOkFeature)));
         CachedFlagUtils.cacheFeatureParams(
-                Arrays.asList(mBoolParam, mIntParam, mDoubleParam, mStringParam));
+                List.of(Arrays.asList(mBoolParam, mIntParam, mDoubleParam, mStringParam)));
 
         CachedFlagsSafeMode.getInstance().onEndCheckpoint();
         mExecutorRule.runAllBackgroundAndUi();
--- components/flags_ui/flags_state.h
+++ components/flags_ui/flags_state.h
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_span.h"
 #include "base/values.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "components/cached_flags/android/jni_delegate.h"
+#endif
+
 namespace flags_ui {
 
 // Internal functionality exposed for tests.
@@ -275,6 +279,12 @@ class FlagsState {
                           const std::string& name,
                           int platform_mask) const;
 
+  // Stores the flags in both FlagsStorage and SharedPreferences.
+  // Call FlagsStorage.SetFlags() to store the flags in FlagsStorage.
+  // Make the appropriate JNI calls to store the flags in SharedPreferences.
+  void SetFlags(FlagsStorage* flags_storage,
+                const std::set<std::string>& flags) const;
+
   const base::raw_span<const FeatureEntry> feature_entries_;
 
   bool needs_restart_;
@@ -287,6 +297,17 @@ class FlagsState {
   // Delegate used for embedders to control display and application of flags.
   // May be null.
   raw_ptr<Delegate> delegate_;
+
+#if BUILDFLAG(IS_ANDROID)
+  // Delegate used by internal code to make JNI calls.
+  std::unique_ptr<cached_flags::JniDelegate> jni_delegate_;
+
+ public:
+  void SetJniDelegateForTesting(
+      std::unique_ptr<cached_flags::JniDelegate> delegate) {
+    jni_delegate_ = std::move(delegate);
+  }
+#endif
 };
 
 }  // namespace flags_ui
