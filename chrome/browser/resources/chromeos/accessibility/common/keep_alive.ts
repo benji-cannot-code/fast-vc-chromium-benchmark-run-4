105e7bb1500e25c6283b4300a9d9b79dec4a488e
Valerie Young <spectranaut@igalia.com>
2025-01-29 13:16:17 -0800

    Accessibility extensions: add KeepAlive class to common
    In the migration of ChromeOS accessibility extensions to manifest v3,
    some extensions will need to keep there service worker alive
    indefinitely. This method for keeping the service worker alive
    was recommended in the migration documents:
    https://developer.chrome.com/docs/extensions/develop/migrate/to-service-workers
    
    Bug: 384675323
    Change-Id: I9668452efeb5be20b3773ddd879ab7835b8575af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196486
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413131}

---
+++ chrome/browser/resources/chromeos/accessibility/common/keep_alive.ts
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Extends the service worker lifetime indefinitely by calling
+ * chrome.runtime.getPlatformInfo() every 20 seconds.
+ */
+
+export class KeepAlive {
+  static instance?: KeepAlive;
+  private interval?: number;
+
+  static init(): void {
+    if (!KeepAlive.instance) {
+      KeepAlive.instance = new KeepAlive();
+      KeepAlive.instance.startHeartbeat();
+    }
+  }
+
+  static stop(): void {
+    if (KeepAlive.instance) {
+      clearInterval(KeepAlive.instance.interval);
+      delete KeepAlive.instance;
+    }
+  }
+
+  private runHeartbeat(): void {
+    chrome.runtime.getPlatformInfo();
+  }
+
+  private startHeartbeat(): void {
+    // Run the heartbeat once.
+    this.runHeartbeat();
+
+    // Then again every 20 seconds.
+    this.interval = setInterval(() => this.runHeartbeat(), 20 * 1000);
+  }
+}
