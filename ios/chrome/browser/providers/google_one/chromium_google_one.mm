0336a86fb6e56fe97f3b9f5352c90b986e3a9068
Olivier ROBIN <olivierrobin@google.com>
2025-01-29 13:31:48 -0800

    Create GoogleOne provider
    Create the provider API for GoogleOne.
    This is part of a 4 CL change
    
    => crrev.com/c/6187393 : Create the new API
     - cl/716205930 : create the internal class
     - crrev.com/i/7971741 : implement the new provider
     - crrev.com/c/6197979 : use the new provider
    
    Change-Id: Idd1194d9e06903bc5550f18daefe57af9df048e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187393
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413141}

---
+++ ios/chrome/browser/providers/google_one/chromium_google_one.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+
+#import "ios/public/provider/chrome/browser/google_one/google_one_api.h"
+
+namespace ios {
+namespace provider {
+
+id<GoogleOneController> CreateGoogleOneController() {
+  return nil;
+}
+
+}  // namespace provider
+}  // namespace ios
