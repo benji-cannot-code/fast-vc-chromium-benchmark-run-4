2174b5644deeab4bba8473faab91f174a8b70602
Nathan Memmott <memmott@chromium.org>
2025-01-29 09:23:22 -0800

    Language Detector: Reject when called with aborted signal
    Rejects the promise returned by AILanguageDetector.detect() when the
    signal passed in is already aborted.
    
    Bug: 391713293
    Change-Id: Icbeb5648458397e92fa7ec0ba8e14fd31ddd1754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210161
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412982}

--- third_party/blink/renderer/modules/ai/on_device_translation/ai_language_detector.cc
+++ third_party/blink/renderer/modules/ai/on_device_translation/ai_language_detector.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/ai/on_device_translation/ai_language_detector.h"
 
+#include "third_party/blink/renderer/modules/ai/exception_helpers.h"
+
 namespace blink {
 
 AILanguageDetector::AILanguageDetector(
@@ -21,11 +23,15 @@ ScriptPromise<IDLSequence<LanguageDetectionResult>> AILanguageDetector::detect(
     const WTF::String& input,
     AILanguageDetectorDetectOptions* options,
     ExceptionState& exception_state) {
-  // TODO(crbug.com/349927087): Take `options` into account.
   if (!script_state->ContextIsValid()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
                                       "The execution context is not valid.");
     return ScriptPromise<IDLSequence<LanguageDetectionResult>>();
+  }
+
+  AbortSignal* signal = options->getSignalOr(nullptr);
+  if (HandleAbortSignal(signal, script_state, exception_state)) {
+    return EmptyPromise();
   }
 
   auto* resolver = MakeGarbageCollected<
