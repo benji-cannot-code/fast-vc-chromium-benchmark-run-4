bfa7274ffa3a52f585a2550657a928326b517b2b
S. Ganesh <ganesh@chromium.org>
2025-01-29 13:06:11 -0800

    updater: always fetch policies in the server
    This CL makes it the responsibility of the server to call FetchPolicies.
    This makes life easy for the many clients, such as AppInstall, legacy
    COM, UpdaterUtil, browser fetcher, etc, and allows the server to
    uniformly enforce policy regardless of client.
    
    Fixed: 392981095
    Change-Id: I952fdcb3f835f5689466d130cf33af6e1a27e57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210937
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413119}

--- chrome/updater/update_service_impl_impl.cc
+++ chrome/updater/update_service_impl_impl.cc
@@ -632,6 +632,20 @@ bool IsPathOnReadOnlyMount(const base::FilePath& path) {
 #endif  // BUILDFLAG(IS_MAC)
 }
 
+void FetchPoliciesDone(
+    base::OnceCallback<void(base::OnceCallback<void(UpdateService::Result)>)>
+        fetch_policies_done,
+    base::OnceCallback<void(UpdateService::Result)> callback,
+    int result) {
+  if (result != kErrorOk) {
+    LOG(ERROR) << "FetchPolicies failed: " << result;
+    std::move(callback).Run(UpdateService::Result::kFetchPoliciesFailed);
+    return;
+  }
+
+  std::move(fetch_policies_done).Run(std::move(callback));
+}
+
 }  // namespace
 
 UpdateServiceImplImpl::UpdateServiceImplImpl(scoped_refptr<Configurator> config)
@@ -930,9 +944,14 @@ void UpdateServiceImplImpl::CheckForUpdate(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::MakeRefCounted<FindUnregisteredAppsTask>(config_, GetUpdaterScope())
-      ->Run(base::BindOnce(&UpdateServiceImplImpl::CheckForUpdateImpl, this,
-                           app_id, priority, policy_same_version_update,
-                           language, state_update, std::move(callback)));
+      ->Run(base::BindOnce(
+          &UpdateServiceImplImpl::FetchPolicies, this,
+          base::BindOnce(
+              &FetchPoliciesDone,
+              base::BindOnce(&UpdateServiceImplImpl::CheckForUpdateImpl, this,
+                             app_id, priority, policy_same_version_update,
+                             language, state_update),
+              std::move(callback))));
 }
 
 void UpdateServiceImplImpl::CheckForUpdateImpl(
@@ -984,10 +1003,14 @@ void UpdateServiceImplImpl::Update(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::MakeRefCounted<FindUnregisteredAppsTask>(config_, GetUpdaterScope())
-      ->Run(base::BindOnce(&UpdateServiceImplImpl::UpdateImpl, this, app_id,
-                           install_data_index, priority,
-                           policy_same_version_update, language, state_update,
-                           std::move(callback)));
+      ->Run(base::BindOnce(
+          &UpdateServiceImplImpl::FetchPolicies, this,
+          base::BindOnce(&FetchPoliciesDone,
+                         base::BindOnce(&UpdateServiceImplImpl::UpdateImpl,
+                                        this, app_id, install_data_index,
+                                        priority, policy_same_version_update,
+                                        language, state_update),
+                         std::move(callback))));
 }
 
 void UpdateServiceImplImpl::UpdateImpl(
@@ -1079,10 +1102,14 @@ void UpdateServiceImplImpl::Install(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::MakeRefCounted<FindUnregisteredAppsTask>(config_, GetUpdaterScope())
-      ->Run(base::BindOnce(&UpdateServiceImplImpl::InstallImpl, this,
-                           registration, client_install_data,
-                           install_data_index, priority, language, state_update,
-                           std::move(callback)));
+      ->Run(base::BindOnce(
+          &UpdateServiceImplImpl::FetchPolicies, this,
+          base::BindOnce(&FetchPoliciesDone,
+                         base::BindOnce(&UpdateServiceImplImpl::InstallImpl,
+                                        this, registration, client_install_data,
+                                        install_data_index, priority, language,
+                                        state_update),
+                         std::move(callback))));
 }
 
 void UpdateServiceImplImpl::InstallImpl(
@@ -1174,10 +1201,14 @@ void UpdateServiceImplImpl::RunInstaller(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::MakeRefCounted<FindUnregisteredAppsTask>(config_, GetUpdaterScope())
-      ->Run(base::BindOnce(&UpdateServiceImplImpl::RunInstallerImpl, this,
-                           app_id, installer_path, install_args, install_data,
-                           install_settings, language, state_update,
-                           std::move(callback)));
+      ->Run(base::BindOnce(
+          &UpdateServiceImplImpl::FetchPolicies, this,
+          base::BindOnce(
+              &FetchPoliciesDone,
+              base::BindOnce(&UpdateServiceImplImpl::RunInstallerImpl, this,
+                             app_id, installer_path, install_args, install_data,
+                             install_settings, language, state_update),
+              std::move(callback))));
 }
 
 void UpdateServiceImplImpl::RunInstallerImpl(
