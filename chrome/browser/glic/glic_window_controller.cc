1d79b73e943c6c37ac3cff599839290a96af46fa
David Bienvenu <davidbienvenu@chromium.org>
2025-01-29 11:39:34 -0800

    glic: Require a mouse drag of 10 pixels before starting a drag
    This fixes various issues with unintentional drags starting, and an
    issue clicking on contents of the glic window when it isn't active
    losing the mouse click.
    
    Bug: 387296426
    Low-Coverage-Reason: HARD_TO_TEST
    Change-Id: Id13c7016e2f6553999097297fff450a1528a5bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198579
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413067}

--- chrome/browser/glic/glic_window_controller.cc
+++ chrome/browser/glic/glic_window_controller.cc
@@ -92,6 +92,7 @@ class GlicWindowController::WindowEventObserver : public ui::EventObserver {
     if (event.type() == ui::EventType::kMousePressed) {
       mouse_down_in_draggable_area_ =
           glic_view_->IsPointWithinDraggableArea(mouse_location);
+      initial_press_loc_ = mouse_location;
     }
     if (event.type() == ui::EventType::kMouseReleased &&
         event.AsMouseEvent()->IsRightMouseButton() &&
@@ -101,17 +102,21 @@ class GlicWindowController::WindowEventObserver : public ui::EventObserver {
     if (event.type() == ui::EventType::kMouseReleased ||
         event.type() == ui::EventType::kMouseExited) {
       mouse_down_in_draggable_area_ = false;
+      initial_press_loc_ = gfx::Point();
     }
 
     // Window should only be dragged if a corresponding mouse drag event was
     // initiated in the draggable area.
     if (mouse_down_in_draggable_area_ &&
-        event.type() == ui::EventType::kMouseDragged) {
+        event.type() == ui::EventType::kMouseDragged &&
+        glic_window_controller_->ShouldStartDrag(initial_press_loc_,
+                                                 mouse_location)) {
       glic_window_controller_->HandleWindowDragWithOffset(
           mouse_location.OffsetFromOrigin());
     }
   }
 
+ private:
   raw_ptr<glic::GlicWindowController> glic_window_controller_;
   raw_ptr<glic::GlicView> glic_view_;
   std::unique_ptr<views::EventMonitor> event_monitor_;
@@ -119,6 +124,9 @@ class GlicWindowController::WindowEventObserver : public ui::EventObserver {
   // Tracks whether the mouse is pressed and was initially within a draggable
   // area of the window.
   bool mouse_down_in_draggable_area_;
+
+  // Tracks the initial kMousePressed location of a potential drag.
+  gfx::Point initial_press_loc_;
 };
 
 GlicWindowController::GlicWindowController(Profile* profile)
@@ -748,6 +756,17 @@ void GlicWindowController::ShowTitleBarContextMenuAt(gfx::Point event_loc) {
   views::ShowSystemMenuAtScreenPixelLocation(views::HWNDForView(GetGlicView()),
                                              event_loc);
 #endif  // BUILDFLAG(IS_WIN)
+}
+
+bool GlicWindowController::ShouldStartDrag(const gfx::Point& initial_press_loc,
+                                           const gfx::Point& mouse_location) {
+  // Determine if the mouse has moved beyond a minimum elasticity distance
+  // in any direction from the starting point.
+  static const int kMinimumDragDistance = 10;
+  int x_offset = abs(mouse_location.x() - initial_press_loc.x());
+  int y_offset = abs(mouse_location.y() - initial_press_loc.y());
+  return sqrt(pow(static_cast<float>(x_offset), 2) +
+              pow(static_cast<float>(y_offset), 2)) > kMinimumDragDistance;
 }
 
 void GlicWindowController::HandleWindowDragWithOffset(
