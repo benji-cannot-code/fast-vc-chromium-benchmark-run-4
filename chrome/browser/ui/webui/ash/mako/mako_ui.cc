423a03c1529cd98803af3cc8b1d32369bc679cb5
Chuong Ho <hdchuong@google.com>
2025-01-28 23:31:15 -0800

    lobster: fix chromeos-chrome build failures
    This CL fixes the build failures by:
    * Forward declare the orca::mojom::EditorClient class
    * Move the import of "orca_service.mojom.h" from its header file
    to its cpp file.
    
    Bug: b:392755229
    Change-Id: I1076993034bbff2582f1a6d61dbdc155e78de621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6211437
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412732}

--- chrome/browser/ui/webui/ash/mako/mako_ui.cc
+++ chrome/browser/ui/webui/ash/mako/mako_ui.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/top_chrome/untrusted_top_chrome_web_ui_controller.h"
 #include "chrome/grit/orca_resources.h"
 #include "chrome/grit/orca_resources_map.h"
+#include "chromeos/ash/services/orca/public/mojom/orca_service.mojom.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
