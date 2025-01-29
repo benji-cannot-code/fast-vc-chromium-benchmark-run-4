a9e2466e7fc2d56bfad1f581417f71024a61456e
Greg Thompson <grt@chromium.org>
2025-01-28 16:31:55 -0800

    [security_interstitials] Fix dangling raw_ptr<WebContents>
    SecurityInterstitialControllerClient and SecurityInterstitialPage each
    hold a pointer to a WebContents that they outlive. Use base::WeakPtr<>
    rather than raw_ptr<> for these members, as they should not be
    referenced during destruction.
    
    Bug: none
    Change-Id: Id9be30997e46dabd3479528c46c26589d4383658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197423
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412626}

--- components/security_interstitials/content/security_interstitial_controller_client.h
+++ components/security_interstitials/content/security_interstitial_controller_client.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SECURITY_INTERSTITIALS_CONTENT_SECURITY_INTERSTITIAL_CONTROLLER_CLIENT_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "components/security_interstitials/core/controller_client.h"
 #include "url/gurl.h"
 
@@ -57,9 +58,10 @@ class SecurityInterstitialControllerClient
  protected:
   // security_interstitials::ControllerClient overrides.
   const std::string GetExtendedReportingPrefName() const override;
-  raw_ptr<content::WebContents> web_contents_;
+  content::WebContents* web_contents() { return &*web_contents_; }
 
  private:
+  base::WeakPtr<content::WebContents> web_contents_;
   raw_ptr<PrefService> prefs_;
   const std::string app_locale_;
   // The default safe page we should go to if there is no previous page to go
--- components/security_interstitials/content/security_interstitial_page.h
+++ components/security_interstitials/content/security_interstitial_page.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "url/gurl.h"
@@ -94,7 +94,7 @@ class SecurityInterstitialPage {
   // The WebContents with which this interstitial page is
   // associated. Not available in ~SecurityInterstitialPage, since it
   // can be destroyed before this class is destroyed.
-  raw_ptr<content::WebContents> web_contents_;
+  base::WeakPtr<content::WebContents> web_contents_;
   const GURL request_url_;
   // Whether the interstitial should create a view.
   bool create_view_;
