babbf38a11496bba0a6601482310e9ae0f5e0ac7
Tsuyoshi Horo <horo@chromium.org>
2025-01-29 01:03:48 -0800

    Add source_type.mojom in preparation for usage in URLResponseHead
    This CL introduces a new mojom file and traits files for SourceType
    in preparation for using `mojom::SourceType` in `URLResponseHead`.
    
    `mojom::SourceType` is currently used only in `URLRequest`.
    A URLResponseHead will contain an array of `mojom::SourceType` which
    will list the content encoding types the renderer should apply. See the
    WIP CL [1] and the design doc in the crbug [2] for more details.
    
    This CL should not introduce any behavior change.
    
    [1]: https://crrev.com/c/391950057
    [2]: https://crbug.com/391950057
    
    Bug: 391950057
    Change-Id: I0546c112996267cc498ce39486f72a4df3a29150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6205888
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412771}

--- services/network/public/cpp/url_request_mojom_traits.h
+++ services/network/public/cpp/url_request_mojom_traits.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/network_isolation_key_mojom_traits.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/resource_request_body.h"
+#include "services/network/public/cpp/source_type_mojom_traits.h"
 #include "services/network/public/mojom/attribution.mojom-forward.h"
 #include "services/network/public/mojom/chunked_data_pipe_getter.mojom.h"
 #include "services/network/public/mojom/client_security_state.mojom-forward.h"
@@ -45,14 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/mojom/url_gurl_mojom_traits.h"
 
 namespace mojo {
-
-template <>
-struct COMPONENT_EXPORT(NETWORK_CPP_BASE)
-    EnumTraits<network::mojom::SourceType, net::SourceStream::SourceType> {
-  static network::mojom::SourceType ToMojom(net::SourceStream::SourceType type);
-  static bool FromMojom(network::mojom::SourceType in,
-                        net::SourceStream::SourceType* out);
-};
 
 template <>
 struct COMPONENT_EXPORT(NETWORK_CPP_BASE)
