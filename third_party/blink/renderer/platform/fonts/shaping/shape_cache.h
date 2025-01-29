9a9ec30ec7bcfdc47f855b7a60b26ecbacddd2f4
Kent Tamura <tkent@chromium.org>
2025-01-29 01:21:47 -0800

    Remove an unnecessary argument of ShapeCache::Add()
    We just passed an default instance as the second argument. We can
    create it in AddSlowCase().
    
    This CL has no behavior changes.
    
    Bug: 389726691
    Change-Id: I562c4d7c18ff9472f9a2bdf367e77cbf08952a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6204483
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412778}

--- third_party/blink/renderer/platform/fonts/shaping/shape_cache.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_cache.h
@@ -117,11 +117,11 @@ class ShapeCache : public GarbageCollected<ShapeCache> {
     visitor->Trace(short_string_map_);
   }
 
-  ShapeCacheEntry* Add(const TextRun& run, ShapeCacheEntry entry) {
+  ShapeCacheEntry* Add(const TextRun& run) {
     if (run.length() > SmallStringKey::Capacity())
       return nullptr;
 
-    return AddSlowCase(run, std::move(entry));
+    return AddSlowCase(run);
   }
 
   void ClearIfVersionChanged(unsigned version) {
@@ -152,7 +152,7 @@ class ShapeCache : public GarbageCollected<ShapeCache> {
   }
 
  private:
-  ShapeCacheEntry* AddSlowCase(const TextRun& run, ShapeCacheEntry entry) {
+  ShapeCacheEntry* AddSlowCase(const TextRun& run) {
     bool is_new_entry;
     ShapeCacheEntry* value;
     if (run.length() == 1) {
@@ -162,7 +162,7 @@ class ShapeCache : public GarbageCollected<ShapeCache> {
       if (run.Direction() == TextDirection::kRtl)
         key |= (1u << 31);
       SingleCharMap::AddResult add_result =
-          single_char_map_.insert(key, std::move(entry));
+          single_char_map_.insert(key, ShapeCacheEntry());
       is_new_entry = add_result.is_new_entry;
       value = &add_result.stored_value->value;
     } else {
@@ -173,7 +173,7 @@ class ShapeCache : public GarbageCollected<ShapeCache> {
         small_string_key = SmallStringKey(run.Span16(), run.Direction());
       }
       SmallStringMap::AddResult add_result =
-          short_string_map_.insert(small_string_key, std::move(entry));
+          short_string_map_.insert(small_string_key, ShapeCacheEntry());
       is_new_entry = add_result.is_new_entry;
       value = &add_result.stored_value->value;
     }
