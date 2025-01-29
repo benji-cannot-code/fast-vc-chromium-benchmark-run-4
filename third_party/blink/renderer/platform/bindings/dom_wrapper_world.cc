1f54569e202bf001108ee59510e0f5ccb1bd2ea7
Nate Chapin <japhet@chromium.org>
2025-01-29 12:40:16 -0800

    Remove unused V8ObjectDataStore and MultiWorldsV8Reference
    Change-Id: I970e37547160bf362072ba9e3d18fd9e02c890f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210517
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413105}

--- third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
+++ third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "third_party/blink/public/platform/web_isolated_world_info.h"
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
-#include "third_party/blink/renderer/platform/bindings/v8_object_data_store.h"
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
@@ -117,8 +116,7 @@ DOMWrapperWorld::DOMWrapperWorld(PassKey,
       world_id_(world_id),
       dom_data_store_(
           MakeGarbageCollected<DOMDataStore>(isolate,
-                                             is_default_world_of_isolate)),
-      v8_object_data_store_(MakeGarbageCollected<V8ObjectDataStore>()) {
+                                             is_default_world_of_isolate)) {
   switch (world_type_) {
     case WorldType::kMain:
       // The main world is managed separately from worldMap(). See worldMap().
@@ -315,7 +313,6 @@ bool DOMWrapperWorld::ClearWrapperInAnyNonInlineStorageWorldIfEqualTo(
 
 void DOMWrapperWorld::Trace(Visitor* visitor) const {
   visitor->Trace(dom_data_store_);
-  visitor->Trace(v8_object_data_store_);
 }
 
 }  // namespace blink
