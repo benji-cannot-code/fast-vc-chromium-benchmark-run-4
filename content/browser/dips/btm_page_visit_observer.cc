65c51412a1d69739cce5771666dcc030c81d37c7
Svend Larsen <svend@chromium.org>
2025-01-29 11:55:31 -0800

    [BTM] Report page visit duration from BtmPageVisitObserver
    I computed duration using base::Time (wall-clock time) rather than
    base::TimeTicks (monotonically increasing counter) because, although
    wall-clock time can produce surprising results like negative durations,
    the implementation notes for base::TimeTicks say that it's not
    recommended for long durations due to platform-dependent implementation
    details, such as whether the counter increments while the process is
    asleep. I think a low but consistent rate of wall-clock weirdness is
    preferable to skewing visit durations by platform.
    
    Design: go/btm-page-visit-observer
    
    Bug: 390201794
    Change-Id: I920a2734f30c0b092d955fedb9600a0b485156e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6208809
    Commit-Queue: Svend Larsen <svend@chromium.org>
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413082}

--- content/browser/dips/btm_page_visit_observer.cc
+++ content/browser/dips/btm_page_visit_observer.cc
@@ -109,6 +109,10 @@ void BtmPageVisitObserver::DidFinishNavigation(
     return;
   }
 
+  base::Time now = clock_->Now();
+  current_page_.visit_duration = last_page_change_time_.has_value()
+                                     ? (now - last_page_change_time_.value())
+                                     : base::TimeDelta();
   auto [navigation, final_url_cookie_access] =
       state->CreateNavigationInfo(*navigation_handle);
   // Don't report the visit right away; put it in the pending queue and wait a
@@ -123,6 +127,7 @@ void BtmPageVisitObserver::DidFinishNavigation(
 
   current_page_ = BtmPageVisitInfo{navigation_handle->GetURL(),
                                    IsWrite(final_url_cookie_access)};
+  last_page_change_time_ = now;
 }
 
 void BtmPageVisitObserver::ReportVisit() {
