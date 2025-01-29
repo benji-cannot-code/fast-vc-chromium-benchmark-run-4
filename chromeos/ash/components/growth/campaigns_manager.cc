0abef0e1e69e6900edfd13ed36cc4fe9a4450736
ThuHuong <thv@chromium.org>
2025-01-29 15:25:20 -0800

    growth: record event before trigger campaigns.
    Record all queued events before triggering the campaigns selection.
    
    Bug: crbug.com/392927978
    Change-Id: Ic636cd6d78e3d51dbf97cd886e0f38f58220ecf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216339
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Thu Huong Vu <thv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413214}

--- chromeos/ash/components/growth/campaigns_manager.cc
+++ chromeos/ash/components/growth/campaigns_manager.cc
@@ -459,8 +459,8 @@ void CampaignsManager::OnCampaignsLoaded(
 
   campaigns_loaded_ = true;
 
-  std::move(load_callback).Run();
   RecordQueuedEventsAndMaybeTrigger();
+  std::move(load_callback).Run();
   NotifyCampaignsLoaded();
 }
 
