58d1b6f2c6ebee9e6121e5d206952ba5d8be7cf8
Kenichi Ishibashi <bashi@chromium.org>
2025-01-28 16:51:36 -0800

    dns: Allow stale DNS results while refreshing
    This CL allows a ServiceEndpointRequest to provide stale endpoint
    results as intermediate results while refreshing them, when the cache
    usage is ALLOWED. If the cache usage is ALLOWED, the request calls
    OnServiceEndpointsUpdated() if there are stale results and starts
    (or is associated with) a HostResolverManager::Job to refresh them.
    Also adds two methods to ServiceEndpointRequest so that a client of
    a request can check staleness about the current endpoints.
    
    Bug: 383174960
    Change-Id: I6b60bc2fccd22434d97c45387c8e2fa2ef0dca39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6095760
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412639}

--- net/dns/host_resolver.cc
+++ net/dns/host_resolver.cc
@@ -135,6 +135,12 @@ class FailingServiceEndpointRequestImpl
     return ResolveErrorInfo(error_);
   }
 
+  const HostCache::EntryStaleness* GetStaleInfo() const override {
+    return nullptr;
+  }
+
+  bool IsStaleWhileRefresing() const override { return false; }
+
   void ChangeRequestPriority(RequestPriority priority) override {}
 
  private:
@@ -322,6 +328,13 @@ std::unique_ptr<HostResolver> HostResolver::Factory::CreateStandaloneResolver(
 }
 
 HostResolver::ResolveHostParameters::ResolveHostParameters() = default;
+
+HostResolver::ResolveHostParameters::ResolveHostParameters(
+    const ResolveHostParameters&) = default;
+
+HostResolver::ResolveHostParameters&
+HostResolver::ResolveHostParameters::operator=(const ResolveHostParameters&) =
+    default;
 
 HostResolver::~HostResolver() = default;
 
