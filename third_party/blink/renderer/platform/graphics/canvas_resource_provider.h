5b8407d5312a8017e6980be5a5fa6aa68d0c90d4
Colin Blundell <blundell@chromium.org>
2025-01-29 00:09:17 -0800

    [Blink] Have CRP::CreatePassThroughProvider() take in SharedImageFormat
    Next step in converting Canvas' usage of SkColorType to
    SharedImageFormat.
    
    Note: The change in canvas_rendering_context_host.cc is a no-op due to
    the same reasoning as in [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6198862
    
    Bug: 371227617
    Change-Id: I97f276b44fa14d5ab33660a1cd7d46c1db46ad23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6203882
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412747}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -147,7 +147,7 @@ class PLATFORM_EXPORT CanvasResourceProvider
 
   static std::unique_ptr<CanvasResourceProvider> CreatePassThroughProvider(
       gfx::Size size,
-      SkColorType sk_color_type,
+      viz::SharedImageFormat format,
       SkAlphaType alpha_type,
       const gfx::ColorSpace& color_space,
       base::WeakPtr<WebGraphicsContext3DProviderWrapper>,
