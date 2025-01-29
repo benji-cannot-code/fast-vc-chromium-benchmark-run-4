c4334b7d740fed32cdf6fa1bf335753561e5a70f
Shu Yang <shuyng@google.com>
2025-01-28 18:01:13 -0800

    Disable testIncognitoTabSwitcherBecomesSensitive
    Bug: 392980546
    Change-Id: I1469c7b30683174fef0a920c15e4cc5d9870fc2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210348
    Auto-Submit: Shu Yang <shuyng@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Owners-Override: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412671}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
@@ -45,6 +45,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -299,6 +300,7 @@ public class SensitiveContentTest {
     // This test also tests if metrics are recorded properly.
     @Test
     @LargeTest
+    @DisabledTest(message = "See https://crbug.com/392980546")
     @EnableFeatures(SensitiveContentFeatures.SENSITIVE_CONTENT_WHILE_SWITCHING_TABS)
     public void testIncognitoTabSwitcherBecomesSensitive() {
         final String histogram =
