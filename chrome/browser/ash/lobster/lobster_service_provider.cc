c87e64b1643e8b95a8efed2be09528cb33afb8cd
Jun Ishiguro <junis@google.com>
2025-01-28 23:40:34 -0800

    Remove unused includes from chrome/browser/ash/lobster
    This removes #include "chrome/browser/browser_process.h" as it's unused.
    
    Bug: 369689076
    Change-Id: I293a40653cf4c98abe07ea583816da495aab853b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210700
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412736}

--- chrome/browser/ash/lobster/lobster_service_provider.cc
+++ chrome/browser/ash/lobster/lobster_service_provider.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "chrome/browser/ash/lobster/lobster_service.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/manta/manta_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
