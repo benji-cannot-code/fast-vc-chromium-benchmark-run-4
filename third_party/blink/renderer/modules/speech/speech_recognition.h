c3be9c4d91c2b08740e3ea6e2484c1b1a411f906
Yiren Wang <yrw@chromium.org>
2025-01-30 11:45:18 -0800

    Add UpdateContext() to SpeechRecognition IDL
    Add behind a new flag WebSpeechRecognitionContext which also enables
    SpeechRecognitionContext
    
    Bug: 388626991
    Change-Id: I26e57bd89fb6d3102edb773f832dc96d2654e414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210342
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413649}

--- third_party/blink/renderer/modules/speech/speech_recognition.h
+++ third_party/blink/renderer/modules/speech/speech_recognition.h
@@ -100,6 +100,7 @@ class MODULES_EXPORT SpeechRecognition final
   void start(MediaStreamTrack*, ExceptionState&);
   void stopFunction();
   void abort();
+  void updateContext(SpeechRecognitionContext*, ExceptionState&);
   ScriptPromise<IDLBoolean> onDeviceWebSpeechAvailable(ScriptState*,
                                                        const String& lang,
                                                        ExceptionState&);
