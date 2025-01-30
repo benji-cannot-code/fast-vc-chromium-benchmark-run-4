ca27f1fcd353bcb5ccfa6488653dbfebb5fa69f5
Xianzhu Wang <wangxianzhu@chromium.org>
2025-01-29 16:25:12 -0800

    Don't pass cull rect to cc if it doesn't cover the scrollport
    If a scrolling contents rect is affected by ancestor clips and doesn't
    cover the scrollport, cc paint checkerboarding code can't distinguish
    the case form paint checkerboarding situation, causing over-reporting.
    
    Bug: 391919458
    Change-Id: I5a3c43fd064fa64e6a6c1c10ba8fffccc0bb84cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6215670
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413239}

--- third_party/blink/renderer/core/paint/box_painter.cc
+++ third_party/blink/renderer/core/paint/box_painter.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/paint/scrollable_area_painter.h"
+#include "ui/gfx/geometry/rect_conversions.h"
 
 namespace blink {
 
@@ -97,6 +98,16 @@ void BoxPainter::RecordScrollHitTestData(
     gfx::Rect cull_rect = fragment->GetContentsCullRect().Rect();
     if (cull_rect.Contains(properties->Scroll()->ContentsRect())) {
       cull_rect = CullRect::Infinite().Rect();
+    } else {
+      // Don't pass the cull rect if it doesn't cover the container rect
+      // because cc can't distinguish the case from paint checkerboarding.
+      gfx::Rect cull_rect_in_container_space = gfx::ToEnclosingRect(
+          gfx::RectF(cull_rect) +
+          properties->ScrollTranslation()->Get2dTranslation());
+      if (!cull_rect_in_container_space.Contains(
+              properties->Scroll()->ContainerRect())) {
+        cull_rect = CullRect::Infinite().Rect();
+      }
     }
     paint_controller.RecordScrollHitTestData(
         background_client, DisplayItem::kScrollHitTest,
