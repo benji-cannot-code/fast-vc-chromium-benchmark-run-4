cdbbbc6c515c9b950d534b8525fef80621499ba2
Aishwarya Rajesh <aishwaryarj@google.com>
2025-01-30 15:41:21 -0800

    [TSLO] Force update height transition in desktop window
    Height transition runs in a desktop window solely to update the strip
    top padding. This CL ensures that the transition is not blocked in a
    desktop window when transition tokens are in use so that the padding is
    updated as needed. To make this fully WAI, the tab modal container
    layout should be updated when the top (or bottom) controls height
    changes as a result of such a transition; there is no adverse impact to
    the FiP toolbar as a result of the forced height transition.
    
    Before:
    https://screencast.googleplex.com/cast/NjEwNTA1ODMwNjQyNDgzMnw1OWM5ZTU1Yy0wOA
    
    After:
    https://screencast.googleplex.com/cast/NDU0NzIzOTMwODQyNzI2NHwyNGI3MTVjYy00ZA
    
    Bug: 391437438
    Change-Id: I415616e7e89ebbe417278e5cbf42c76d311404c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6199261
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1413810}

--- chrome/android/java/src/org/chromium/chrome/browser/modaldialog/ChromeTabModalPresenter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/modaldialog/ChromeTabModalPresenter.java
@@ -163,15 +163,7 @@ public class ChromeTabModalPresenter extends TabModalPresenter
 
     @Override
     protected void showDialogContainer() {
-        if (mShouldUpdateContainerLayoutParams) {
-            MarginLayoutParams params = (MarginLayoutParams) getDialogContainer().getLayoutParams();
-            params.topMargin =
-                    getContainerTopMargin(
-                            mActivity.getResources(), mBrowserControlsVisibilityManager);
-            params.bottomMargin = mBottomControlsHeight;
-            getDialogContainer().setLayoutParams(params);
-            mShouldUpdateContainerLayoutParams = false;
-        }
+        maybeUpdateDialogLayout();
 
         // Don't show the dialog container before browser controls are guaranteed fully visible.
         if (BrowserControlsUtils.areBrowserControlsFullyVisible(
@@ -258,11 +250,13 @@ public class ChromeTabModalPresenter extends TabModalPresenter
             int bottomControlsHeight, int bottomControlsMinHeight) {
         mBottomControlsHeight = bottomControlsHeight;
         mShouldUpdateContainerLayoutParams = true;
+        maybeUpdateDialogLayout();
     }
 
     @Override
     public void onTopControlsHeightChanged(int topControlsHeight, int topControlsMinHeight) {
         mShouldUpdateContainerLayoutParams = true;
+        maybeUpdateDialogLayout();
     }
 
     @Override
@@ -338,6 +332,18 @@ public class ChromeTabModalPresenter extends TabModalPresenter
 
     private boolean areRendererInputEventsIgnored() {
         return mActiveTab.getWebContents().getMainFrame().areInputEventsIgnored();
+    }
+
+    private void maybeUpdateDialogLayout() {
+        if (mShouldUpdateContainerLayoutParams && getDialogContainer() != null) {
+            MarginLayoutParams params = (MarginLayoutParams) getDialogContainer().getLayoutParams();
+            params.topMargin =
+                    getContainerTopMargin(
+                            mActivity.getResources(), mBrowserControlsVisibilityManager);
+            params.bottomMargin = mBottomControlsHeight;
+            getDialogContainer().setLayoutParams(params);
+            mShouldUpdateContainerLayoutParams = false;
+        }
     }
 
     ViewGroup getContainerParentForTest() {
