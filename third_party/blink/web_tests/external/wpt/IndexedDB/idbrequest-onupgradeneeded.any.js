cf7e263511361c92425b2bc5f5d3100e5cf025bb
Rahul Singh <rahsin@microsoft.com>
2025-01-29 19:06:57 -0800

    IDB WPTs: Extend idbrequest WPTs to run on workers
    This set of IndexedDB WPTs currently only run in a window environment.
    This change extends them to also run in dedicated, shared, and service
    worker environments.
    
    Bug: 41455766
    Change-Id: I19b3e1fe6e4342529970edb906b39be505702388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6214757
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1413284}

--- third_party/blink/web_tests/external/wpt/IndexedDB/idbrequest-onupgradeneeded.any.js
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbrequest-onupgradeneeded.any.js
@@ -1,25 +1,22 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<!doctype html>
-<meta charset=utf-8>
-<title>IndexedDB: UpgradeNeeded Tests</title>
-<meta name="help" href="https://w3c.github.io/IndexedDB/#commit-transaction">
-<meta name="help" href="https://w3c.github.io/IndexedDB/#dom-idbfactory-open">
-<script src="/resources/testharness.js"></script>
-<script src="/resources/testharnessreport.js"></script>
-<script>
+// META: title=IDBRequest onupgradeneeded tests
+// META: global=window,worker
 
-function upgradeneeded_test(upgrade_func, success_func, error_func, description) {
+'use strict';
+
+function upgradeneeded_test(
+    upgrade_func, success_func, error_func, description) {
   async_test(function(t) {
-    var dbName = 'db' + self.location.pathname + '-' + description;
-    var delete_request = indexedDB.deleteDatabase(dbName);
+    let dbName = 'db' + self.location.pathname + '-' + description;
+    let delete_request = indexedDB.deleteDatabase(dbName);
     delete_request.onerror = t.unreached_func('deleteDatabase should not fail');
     delete_request.onsuccess = t.step_func(function() {
-      var open_request = indexedDB.open(dbName);
+      let open_request = indexedDB.open(dbName);
 
       open_request.onupgradeneeded = t.step_func(function() {
         t.add_cleanup(function() {
           if (open_request.result) {
-            open_request.result.close(),
+            open_request.result.close();
             indexedDB.deleteDatabase(dbName);
           }
         });
@@ -29,7 +26,9 @@ function upgradeneeded_test(upgrade_func, success_func, error_func, description)
         success_func(t, open_request);
       });
       if (error_func) {
-        open_request.onerror = function() { error_func(t, open_request); };
+        open_request.onerror = function() {
+          error_func(t, open_request);
+        };
       } else {
         open_request.onerror = t.unreached_func('open failed');
       }
@@ -38,12 +37,12 @@ function upgradeneeded_test(upgrade_func, success_func, error_func, description)
 }
 
 (function() {
-  var order = [];
-  upgradeneeded_test(
+let order = [];
+upgradeneeded_test(
     function upgrade(t, request) {
       order.push('Upgrade');
-      var db = request.result;
-      var deleteRequest = indexedDB.deleteDatabase(db.name);
+      let db = request.result;
+      let deleteRequest = indexedDB.deleteDatabase(db.name);
       deleteRequest.onsuccess = t.step_func(function() {
         assert_array_equals(order, ['Upgrade', 'Open Success']);
         t.done();
@@ -51,24 +50,22 @@ function upgradeneeded_test(upgrade_func, success_func, error_func, description)
       deleteRequest.onerror = t.unreached_func('delete failed');
     },
     function success(t, request) {
-      var db = request.result;
+      let db = request.result;
       db.close();
       order.push('Open Success');
     },
-    null,
-    'indexedDB.delete called from upgradeneeded handler'
-  );
+    null, 'indexedDB.delete called from upgradeneeded handler');
 }());
 
 (function() {
-  var order = [];
-  upgradeneeded_test(
+let order = [];
+upgradeneeded_test(
     function upgrade(t, request) {
       order.push('Upgrade');
       request.transaction.abort();
       order.push('Upgrade Transaction Aborted');
-      var db = request.result;
-      var deleteRequest = indexedDB.deleteDatabase(db.name);
+      let db = request.result;
+      let deleteRequest = indexedDB.deleteDatabase(db.name);
       deleteRequest.onsuccess = t.step_func(function() {
         assert_array_equals(
             order, ['Upgrade', 'Upgrade Transaction Aborted', 'Open Error']);
@@ -83,17 +80,16 @@ function upgradeneeded_test(upgrade_func, success_func, error_func, description)
       assert_array_equals(order, ['Upgrade', 'Upgrade Transaction Aborted']);
       order.push('Open Error');
     },
-    'Abort transaction before deleting database in upgradeneeded handler'
-  );
+    'Abort transaction before deleting database in upgradeneeded handler');
 }());
 
 (function() {
-  var order = [];
-  upgradeneeded_test(
+let order = [];
+upgradeneeded_test(
     function upgrade(t, request) {
       order.push('Upgrade');
-      var db = request.result;
-      var deleteRequest = indexedDB.deleteDatabase(db.name);
+      let db = request.result;
+      let deleteRequest = indexedDB.deleteDatabase(db.name);
       request.transaction.abort();
       order.push('Upgrade Transaction Aborted');
       deleteRequest.onsuccess = t.step_func(function() {
@@ -110,21 +106,20 @@ function upgradeneeded_test(upgrade_func, success_func, error_func, description)
       assert_array_equals(order, ['Upgrade', 'Upgrade Transaction Aborted']);
       order.push('Open Error');
     },
-    'Abort transaction after deleting database in upgradeneeded event handler'
-  );
+    'Abort transaction after deleting database in upgradeneeded event handler');
 }());
 
 (function() {
-  var order = [];
-  upgradeneeded_test(
+let order = [];
+upgradeneeded_test(
     function upgrade(t, request) {
       order.push('Upgrade');
-      var db = request.result;
+      let db = request.result;
       db.createObjectStore('store');
       request.transaction.oncomplete = t.step_func(function() {
         order.push('Upgrade transaction complete');
-        var txn = db.transaction('store', 'readwrite');
-        var store = txn.objectStore('store');
+        let txn = db.transaction('store', 'readwrite');
+        let store = txn.objectStore('store');
         store.put('value', 'key');
         txn.oncomplete = t.step_func(function() {
           assert_array_equals(
@@ -139,9 +134,5 @@ function upgradeneeded_test(upgrade_func, success_func, error_func, description)
     function success(t, request) {
       order.push('Open Success');
     },
-    null,
-    'transaction oncomplete ordering relative to open request onsuccess'
-  );
+    null, 'transaction oncomplete ordering relative to open request onsuccess');
 }());
-
-</script>
---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbrequest_result.any.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=IDBRequest.result
+// META: global=window,worker
+// META: script=resources/support.js
+
+'use strict';
+
+async_test(t => {
+  let open = createdb(t);
+  open.onupgradeneeded = t.step_func(e => {
+    let db = e.target.result;
+    db.createObjectStore('store');
+  });
+  open.onsuccess = t.step_func(e => {
+    let db = e.target.result;
+    let request =
+        db.transaction('store', 'readonly').objectStore('store').get(0);
+
+    assert_equals(request.readyState, 'pending');
+    assert_throws_dom(
+        'InvalidStateError', () => request.result,
+        'IDBRequest.result should throw if request is pending');
+    t.done();
+  });
+}, 'IDBRequest.result throws if ready state is pending');
