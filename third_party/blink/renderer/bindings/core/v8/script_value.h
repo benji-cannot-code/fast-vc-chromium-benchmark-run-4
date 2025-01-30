39d82bbfd499e5a21eb97e527ec348a25793012a
Michael Lippautz <mlippautz@chromium.org>
2025-01-30 11:53:43 -0800

    bindings: Slimmer ScriptValue
    ScriptValue is a primitive that is used ubiquitously all over the
    place. The Isolate pointer in there was barely used and we can also
    store the Isolate in the DOM world which is a much more coarse grained
    concept.
    
    S3 seems noisy on this change but it seems generally viable to do this.
    
    Summary:
    👍 TodoMVC-JavaScript-ES5                      [ -0.5%,  -0.1%]
    👍 TodoMVC-JavaScript-ES6-Webpack-Complex-DOM  [ -0.6%,  -0.1%]
    👍 TodoMVC-Vue                                 [ -0.5%,  -0.1%]
    👍 TodoMVC-WebComponents                       [ -0.4%,  -0.0%]
    
    Change-Id: I5d4f8780c0b4cf3b288bb4509c79d53660df64c2
    Fixed: 393153134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6212565
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413657}

--- third_party/blink/renderer/bindings/core/v8/script_value.h
+++ third_party/blink/renderer/bindings/core/v8/script_value.h
@@ -69,9 +69,7 @@ class CORE_EXPORT ScriptValue final {
   ScriptValue() = default;
 
   ScriptValue(v8::Isolate* isolate, v8::Local<v8::Value> value)
-      : isolate_(isolate), value_(isolate, value) {
-    DCHECK(isolate_);
-  }
+      : value_(isolate, value) {}
 
   ~ScriptValue() {
     // Reset() below eagerly cleans up Oilpan-internal book-keeping data
@@ -96,11 +94,10 @@ class CORE_EXPORT ScriptValue final {
 
   ScriptValue(const ScriptValue& value) = default;
 
-  // TODO(riakf): Use this GetIsolate() only when doing DCHECK inside
-  // ScriptValue.
+  // Prefer getting the Isolate from ScriptState or similar.
   v8::Isolate* GetIsolate() const {
-    DCHECK(isolate_);
-    return isolate_;
+    DCHECK(!IsEmpty());
+    return value_.GetIsolate();
   }
 
   ScriptValue& operator=(const ScriptValue& value) = default;
@@ -150,7 +147,6 @@ class CORE_EXPORT ScriptValue final {
   bool IsEmpty() const { return value_.IsEmpty(); }
 
   void Clear() {
-    isolate_ = nullptr;
     value_.Reset();
   }
 
@@ -168,7 +164,6 @@ class CORE_EXPORT ScriptValue final {
   void Trace(Visitor* visitor) const { visitor->Trace(value_); }
 
  private:
-  v8::Isolate* isolate_ = nullptr;
   WorldSafeV8Reference<v8::Value> value_;
 };
 
