7fcb2eb97e57df96125c2d1a78335ba8a4ce10ef
Finnur Thorarinsson <finnur@chromium.org>
2025-01-30 12:06:14 -0800

    [NavCapture]: Add ChromeOs test coverage for v2-DefaultOff.
    Target(s): chrome/browser/ui/webui/app_management
    
    This change adds NavCapture v2-DefaultOff coverage for existing
    ChromeOS tests that were testing v1-DefaultOff.
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 370548596
    Change-Id: Icfdf5e77fb0c596e480b21ad958ff8ff5e1fb826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216756
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413664}

--- chrome/browser/ui/webui/app_management/app_management_page_handler_unittest.cc
+++ chrome/browser/ui/webui/app_management/app_management_page_handler_unittest.cc
@@ -944,7 +944,8 @@ TEST_P(AppManagementPageHandlerArcTest, SetAppLocale) {
 INSTANTIATE_TEST_SUITE_P(
     ,
     AppManagementPageHandlerArcTest,
-    testing::Values(apps::test::LinkCapturingFeatureVersion::kV1DefaultOff),
+    testing::Values(apps::test::LinkCapturingFeatureVersion::kV1DefaultOff,
+                    apps::test::LinkCapturingFeatureVersion::kV2DefaultOff),
     apps::test::LinkCapturingVersionToString);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
@@ -952,7 +953,8 @@ INSTANTIATE_TEST_SUITE_P(
     ,
     AppManagementPageHandlerTestBase,
 #if BUILDFLAG(IS_CHROMEOS)
-    testing::Values(apps::test::LinkCapturingFeatureVersion::kV1DefaultOff)
+    testing::Values(apps::test::LinkCapturingFeatureVersion::kV1DefaultOff,
+                    apps::test::LinkCapturingFeatureVersion::kV2DefaultOff)
 #else
     testing::Values(apps::test::LinkCapturingFeatureVersion::kV2DefaultOff,
                     apps::test::LinkCapturingFeatureVersion::kV2DefaultOn)
