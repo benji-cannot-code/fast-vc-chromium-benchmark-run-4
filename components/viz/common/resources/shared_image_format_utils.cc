cd61e019456b43d9b5c3a30d9b703c8d933742c9
Saifuddin Hitawala <hitawala@chromium.org>
2025-01-30 10:30:26 -0800

    [viz] Remove ToClosestSkColorTypeDeprecated
    All usages of ToClosestSkColorTypeDeprecated should now be converted
    to use ToClosestSkColorType. Remove ToClosestSkColorTypeDeprecated.
    
    Bug: 383575394
    Change-Id: Ieb02533254aa52646fa0122994dd28604f15818c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216967
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413598}

--- components/viz/common/resources/shared_image_format_utils.cc
+++ components/viz/common/resources/shared_image_format_utils.cc
@@ -16,22 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace viz {
 
-SkColorType ToClosestSkColorTypeDeprecated(bool gpu_compositing,
-                                           SharedImageFormat format) {
-  CHECK(format.is_single_plane());
-
-  if (!gpu_compositing) {
-    // TODO(crbug.com/41472025): Remove this assumption and have clients tag
-    // resources with the correct format.
-    // In software compositing we lazily use RGBA_8888 throughout the system,
-    // but actual pixel encodings are the native skia bit ordering, which can be
-    // RGBA or BGRA.
-    return kN32_SkColorType;
-  }
-
-  return ToClosestSkColorType(format);
-}
-
 SkColorType ToClosestSkColorType(SharedImageFormat format) {
   CHECK(format.is_single_plane());
 
