0856be8acd74f20a4deea37cd80d57ef8e96a196
Dan Polanco <polardz@google.com>
2025-01-30 10:46:37 -0800

    [New Tab Animation] Update foreground animation in GTS
    Adds rounded corners to improve the current animation.
    This change is behind the 'Show New Tab Animations' flag.
    
    LTR:http://go/scrcast/NjI5OTc2NzYyODg5MDExMnw1YWIyMGVlNy1iMw
    RTL:http://go/scrcast/NjUxMjQ5MzYwMDk2NDYwOHw0YTlhMWNjYS1hMw
    
    Bug: 40933120
    Change-Id: Iec3b8361a6b4f96789549a166ab48ad05794553e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6208550
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1413607}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneBase.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneBase.java
@@ -401,7 +401,7 @@ public abstract class TabSwitcherPaneBase implements Pane, TabSwitcher, TabSwitc
                     initialRect.offset(-leftOffset, -initialTopOffset);
                     finalRect.offset(-leftOffset, -finalTopOffset);
                     animationDataSupplier.set(
-                            new ShrinkExpandAnimationData(
+                            ShrinkExpandAnimationData.createHubShrinkExpandAnimationData(
                                     initialRect,
                                     finalRect,
                                     initialTopCornerRadius,
