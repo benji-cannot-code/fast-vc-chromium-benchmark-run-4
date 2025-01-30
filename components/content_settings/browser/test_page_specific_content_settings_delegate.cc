dd49a64eab983395b6630f103acb277a4c918edc
Elias Klim <elklm@chromium.org>
2025-01-30 05:38:37 -0800

    Suppress the media blocked indicator for PiP window
    This CL fixes a bug when a PiP window tries to use camera/mic permission
    and we display a blocked activity indicator. The PiP window does not
    support blocked activity indicator, so we should not show it.
    
    Bug: 384770168
    Change-Id: If3199c935c997ea37312de0acf00d8553bcf1f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198789
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413445}

--- components/content_settings/browser/test_page_specific_content_settings_delegate.cc
+++ components/content_settings/browser/test_page_specific_content_settings_delegate.cc
@@ -64,4 +64,9 @@ bool TestPageSpecificContentSettingsDelegate::IsFrameAllowlistedForJavaScript(
   return false;
 }
 
+bool TestPageSpecificContentSettingsDelegate::IsPiPWindow(
+    content::WebContents* web_contents) {
+  return false;
+}
+
 }  // namespace content_settings
