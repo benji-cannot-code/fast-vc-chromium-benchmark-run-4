1bd60bdab798c386a076fced6cf7fbc279ad9dc1
Mustafa Emre Acer <meacer@chromium.org>
2025-01-30 09:16:11 -0800

    [CSP] Add a use counter for unsafe-hashes
    This CL adds a use counter for script-src directives that have
    unsafe-hashes. Other directives can also have unsafe-hashes, but for
    now we only record it for script-src.
    
    Bug: 392657736
    Change-Id: I7940d5048d0c25382cf7a7df05cb9449b981cb05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6204362
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413556}

--- third_party/blink/renderer/core/frame/csp/content_security_policy.cc
+++ third_party/blink/renderer/core/frame/csp/content_security_policy.cc
@@ -212,6 +212,13 @@ static String StripURLForUseInReport(const SecurityOrigin* security_origin,
   return stripped_url.GetString();
 }
 
+bool HasScriptUnsafeHashes(
+    const network::mojom::blink::ContentSecurityPolicy& policy) {
+  CSPOperativeDirective directive = CSPDirectiveListOperativeDirective(
+      policy, network::mojom::CSPDirectiveName::ScriptSrc);
+  return directive.source_list && directive.source_list->allow_unsafe_hashes;
+}
+
 }  // namespace
 
 bool ContentSecurityPolicy::IsNonceableElement(const Element* element) {
@@ -335,6 +342,10 @@ void ContentSecurityPolicy::ReportUseCounters(
                                   ReportingDisposition::kSuppressReporting,
                                   kWillNotThrowException, g_empty_string)) {
       Count(WebFeature::kCSPWithUnsafeEval);
+    }
+
+    if (HasScriptUnsafeHashes(*policy)) {
+      Count(WebFeature::kCSPWithUnsafeHashes);
     }
 
     // We consider a policy to be "reasonably secure" if it:
