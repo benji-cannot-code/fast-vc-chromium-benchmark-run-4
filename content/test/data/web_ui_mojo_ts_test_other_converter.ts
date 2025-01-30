c656ed244282777f63f86a5ee7d527759b0d7e2b
Fred Shih <ffred@chromium.org>
2025-01-30 15:40:45 -0800

    Add a copy step for converters to the mojom rule
    By itself, this copy rule is not all that useful. ATM it's only helpful
    in ensuring that converters are copied to the correct destination so
    that relative path import works correctly.
    
    An upcoming CL will hook this into the build_webui BUILD rule which
    would make it a lot more useful.
    
    Change-Id: Ie02bf3db1e098d6038fd9c7d64a6e46234fb0748
    Bug: 40615900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6178410
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413809}

---
+++ content/test/data/web_ui_mojo_ts_test_other_converter.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {MappedDictType} from './web_ui_mojo_ts_test_other_mapped_types.js';
+import {MappedDictDataView, MappedDictTypeMapper} from './web_ui_ts_test_other_types.test-mojom-converters.js';
+
+export class MappedDictConverter implements
+    MappedDictTypeMapper<MappedDictType> {
+  data(dict: MappedDictType): {[key: string]: string} {
+    const converted: {[key: string]: string} = {};
+    for (let k of dict.keys()) {
+      converted[k] = dict.get(k) || '';
+    }
+    return converted;
+  }
+
+  convert(view: MappedDictDataView): MappedDictType {
+    const converted = new Map<string, string>();
+    for (let k in view.data()) {
+      converted.set(k, view.data()[k]!);
+    }
+    return converted;
+  }
+}
+
---
+++ content/test/data/web_ui_mojo_ts_test_simple_converter.ts
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {SimpleMappedTypeDataView, SimpleMappedTypeTypeMapper} from './web_ui_ts_test.test-mojom-converters.js';
+
+export class SimpleTypeConverter implements SimpleMappedTypeTypeMapper<string> {
+  value(mappedType: string): string {
+    return mappedType;
+  }
+
+  convert(dataView: SimpleMappedTypeDataView): string {
+    return dataView.value();
+  }
+}
