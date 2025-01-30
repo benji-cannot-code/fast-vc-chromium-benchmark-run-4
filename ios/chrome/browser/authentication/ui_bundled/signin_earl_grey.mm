e7dfc0c898d8e03fb162e3c6636d79ee7587b506
Ankush Singh <ankushkush@google.com>
2025-01-30 01:24:15 -0800

    [ios] Wait for transport active in SigninEarlGrey signinWithFakeIdentity
    This CL introduces a new test util
    signinAndWaitForSyncTransportStateActive to allow a wait for the sync
    transport state to become active, which otherwise most of the tests had
    to call by themselves.
    
    Fixed: 383027526
    Change-Id: I94d12b24e1a6789b61af03b05c308cb3015bf5ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6207098
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413365}

--- ios/chrome/browser/authentication/ui_bundled/signin_earl_grey.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin_earl_grey.mm
@@ -80,6 +80,11 @@ using base::test::ios::WaitUntilConditionOrTimeout;
   [self verifySignedInWithFakeIdentity:identity];
 }
 
+- (void)signinAndWaitForSyncTransportStateActive:(FakeSystemIdentity*)identity {
+  [self signinWithFakeIdentity:identity];
+  [ChromeEarlGrey waitForSyncTransportStateActiveWithTimeout:base::Seconds(10)];
+}
+
 - (void)signinAndEnableLegacySyncFeature:(FakeSystemIdentity*)identity {
   [SigninEarlGreyAppInterface signinAndEnableLegacySyncFeature:identity];
   [self verifyPrimaryAccountWithEmail:identity.userEmail
