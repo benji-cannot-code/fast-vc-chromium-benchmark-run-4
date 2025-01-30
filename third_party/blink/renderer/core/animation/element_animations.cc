cfff380c5e0e4d9c9de49b0b3979a4fb13e07ada
Claire Chambers <clchambers@microsoft.com>
2025-01-30 12:16:19 -0800

    [CompositeClipPathAnimations] Add lifecycle CHECKs for status
    The composited clip path paint status has been a persistent paint point,
    causing crashes. This change adds some CHECKs to constrain the bugs
    we're seeing in the wild.
    
    Change-Id: I27b067633645afca276c0ba93e58421b619b1e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173961
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Claire Chambers <clchambers@microsoft.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413673}

--- third_party/blink/renderer/core/animation/element_animations.cc
+++ third_party/blink/renderer/core/animation/element_animations.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/animation/element_animations.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "third_party/blink/renderer/core/css/css_property_equality.h"
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
@@ -97,6 +98,14 @@ bool ElementAnimations::HasCompositedPaintWorkletAnimation() {
 void ElementAnimations::RecalcCompositedStatusForKeyframeChange(
     Element& element,
     Animation::NativePaintWorkletReasons properties) {
+  if ((element.GetDocument().Lifecycle().GetState() !=
+       DocumentLifecycle::kInStyleRecalc) &&
+      (element.GetDocument().Lifecycle().GetState() !=
+       DocumentLifecycle::kInPerformLayout)) {
+    DCHECK(false) << "RecalcCompositedStatusForKeyframeChange must not be "
+                  << "called outside of style/layout.";
+    base::debug::DumpWithoutCrashing();
+  }
   if (!element.GetLayoutObject()) {
     return;
   }
@@ -122,6 +131,18 @@ void ElementAnimations::RecalcCompositedStatusForKeyframeChange(
 }
 
 void ElementAnimations::RecalcCompositedStatus(Element* element) {
+  // Must not run during paint or pre-paint. Can be run post-paint via JS,
+  // during stop due to detach, and post-layout from the post style animation
+  // update.
+  if ((element->GetDocument().Lifecycle().GetState() ==
+       DocumentLifecycle::kInPrePaint) ||
+      (element->GetDocument().Lifecycle().GetState() ==
+       DocumentLifecycle::kInPaint)) {
+    DCHECK(false) << "Composited status must not be reset during "
+                  << "prepaint/paint";
+    base::debug::DumpWithoutCrashing();
+  }
+
   Animation::NativePaintWorkletReasons reasons = Animation::kNoPaintWorklet;
   for (auto& entry : Animations()) {
     if (entry.key->CalculateAnimationPlayState() ==
