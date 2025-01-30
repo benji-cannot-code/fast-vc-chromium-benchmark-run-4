d9e95fd294f48bf00d1d96b77ee0f6893c7a329c
Di Wu <diwux@google.com>
2025-01-29 20:53:31 -0800

    [Ash] Delete no longer needed dependencies on //c/b/a/crosapi
    This CL deletes two more dependencies on //c/b/a/crosapi from feature
    code in Ash. The deletion was made possible thanks to Lacros sunset.
    
    Bug: 354710097
    Test: tryjob
    Change-Id: I07842679450840252be46c260c66ae8a2182aca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217559
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413307}

--- chrome/browser/ash/scalable_iph/scalable_iph_delegate_impl.cc
+++ chrome/browser/ash/scalable_iph/scalable_iph_delegate_impl.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ash/arc/arc_util.h"
-#include "chrome/browser/ash/crosapi/crosapi_util.h"
 #include "chrome/browser/ash/phonehub/phone_hub_manager_factory.h"
 #include "chrome/browser/ash/printing/synced_printers_manager.h"
 #include "chrome/browser/ash/printing/synced_printers_manager_factory.h"
