1c8808121df554af89231815dfeeb0b513054867
Ritika Gupta <ritikagup@google.com>
2025-01-30 15:58:54 -0800

    [Gardener] Disable the failing testReadingListFolderShown test in ReadingListTest.
    Bug:393441283
    
    Change-Id: Idfc73448d54101eca38ea90859dc654215bf3040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6220096
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Owners-Override: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1413825}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/ReadingListTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/ReadingListTest.java
@@ -354,6 +354,7 @@ public class ReadingListTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/393441283")
     public void testReadingListFolderShown() throws Exception {
         SigninPromoCoordinator.disablePromoForTesting();
         BookmarkPromoHeader.forcePromoVisibilityForTesting(false);
