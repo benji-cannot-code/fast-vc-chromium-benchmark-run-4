391e2b4ab2ea8aac38a299434854cc0f528bd3f9
Michael Ershov <miersh@google.com>
2025-01-30 06:23:16 -0800

    kcer_nss_fuzzer: Allow some certs to not be imported
    Fixed: b:392928933
    Test: Reproducer Testcase from the bug
    Change-Id: I399022c5ccbf6796625e5341f32bf4a4acc9c85a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218207
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1413458}

--- chromeos/ash/components/kcer/kcer_nss/kcer_nss_fuzzer.cc
+++ chromeos/ash/components/kcer/kcer_nss/kcer_nss_fuzzer.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_task_environment.h"
 #include "crypto/hash.h"
 #include "net/cert/x509_util.h"
+#include "net/cert/x509_util_nss.h"
 #include "net/test/cert_builder.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -923,6 +924,15 @@ void KcerFuzzer::RunImportX509Cert() {
   if (!base::Contains(available_tokens_, token)) {
     ASSERT_FALSE(import_waiter.Get().has_value());
     EXPECT_EQ(import_waiter.Get().error(), Error::kTokenIsNotAvailable);
+    return;
+  }
+
+  if (import_waiter.Get().error() == Error::kInvalidCertificate) {
+    // `CertBuilder` can generate certs that are not accepted by NSS. Double
+    // check that the cert indeed wasn't supposed to be imported and continue.
+    EXPECT_TRUE(net::x509_util::CreateCERTCertificateListFromBytes(
+                    cert->cert_span(), net::X509Certificate::FORMAT_AUTO)
+                    .empty());
     return;
   }
 
