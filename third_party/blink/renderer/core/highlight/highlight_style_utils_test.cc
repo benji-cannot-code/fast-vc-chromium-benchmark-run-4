064de48b5c180905d12ad5ad5e8ded1b7d0b24ba
Stephen Chenney <schenney@chromium.org>
2025-01-29 17:59:25 -0800

    Allow custom properties in highlight pseudos
    This is a workaround for the use case of custom properties
    defined and used in the same rule for ::selection pseudos.
    The use case arises from tooling, particularly Tailwind CSS,
    and it caused a stable regression when Highlight Inheritance
    was launched.
    
    This change restores the original behavior in that highlight
    pseudos can use custom properties defined in the highlight
    itself. The custom properties are not inherited through the
    highlight inheritance chain, so this change does not result in
    confusion about the source of custom properties when Highlight
    Inheritance is enabled: the properties still come from the
    originating element and then the highlight pseudo itself, never
    it's parent highlight.
    
    CSS Spec PR: https://github.com/w3c/csswg-drafts/pull/11528
    
    Fixed: 381125910
    Change-Id: I0f89e6b8ad96d097ce1e2b39c179a270d472991f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6053860
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413271}

--- third_party/blink/renderer/core/highlight/highlight_style_utils_test.cc
+++ third_party/blink/renderer/core/highlight/highlight_style_utils_test.cc
@@ -350,8 +350,8 @@ TEST_F(HighlightStyleUtilsTest, CustomPropertyInheritance) {
       :root {
         --root-color: green;
       }
-      ::selection {
-        /* This rule should not apply */
+      :root::selection {
+        /* Should not affect div::selection */
         --selection-color: blue;
       }
       div::selection {
