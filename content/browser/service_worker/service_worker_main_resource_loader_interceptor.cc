da58e8800c0117a368804b5e9f4829d91423e850
Shunya Shishido <sisidovski@chromium.org>
2025-01-30 07:45:21 -0800

    [SyntheticResponse] Fake service worker registration
    This CL adds the fake service worker database that is returned if
    SyntheticResponse is enabled, and the request URL is matched to the url
    in the allowlist provided from the feature param.
    
    The fake service worker is basically do nothing, but this is necessary
    to handle the navigation with the service worker classes in the browser
    process e.g. ServiceWorkerMainResourceLoader.
    
    Since SyntheticResponse will be implemented on the service worker infra,
    we need this fake service worker registration. After the experiment we
    have a plan to remove this from the code base and give developers a way
    to control on their side via service workers.
    
    Fuchsia-Binary-Size: Increase is temporary.
    Bug: 352578800
    Change-Id: I491735ed6ebdf8bb7d357f1f779d5d964ddbb1e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6093911
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413493}

--- content/browser/service_worker/service_worker_main_resource_loader_interceptor.cc
+++ content/browser/service_worker/service_worker_main_resource_loader_interceptor.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_client.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
+#include "content/browser/service_worker/service_worker_loader_helpers.h"
 #include "content/browser/service_worker/service_worker_main_resource_handle.h"
 #include "content/browser/service_worker/service_worker_object_host.h"
 #include "content/public/common/content_client.h"
@@ -247,11 +248,17 @@ void ServiceWorkerMainResourceLoaderInterceptor::MaybeCreateLoader(
 
   // If we know there's no service worker for the storage key, let's skip asking
   // the storage to check the existence.
+  //
+  // crbug.com/352578800: `MaybeHasRegistrationForStorageKey()` doesn't reflect
+  // the fake registration initially. If the URL is eligible for
+  // SyntheticResponse, do not skip service worker.
   bool skip_service_worker =
       skip_service_worker_ ||
       !OriginCanAccessServiceWorkers(tentative_resource_request.url) ||
-      !handle_->context_wrapper()->MaybeHasRegistrationForStorageKey(
-          handle_->service_worker_client()->key());
+      !(handle_->context_wrapper()->MaybeHasRegistrationForStorageKey(
+            handle_->service_worker_client()->key()) ||
+        service_worker_loader_helpers::IsEligibleForSyntheticResponse(
+            tentative_resource_request.url));
 
   // Create and start the handler for this request. It will invoke the loader
   // callback or fallback callback.
