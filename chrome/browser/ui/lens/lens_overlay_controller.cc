8995768c5b25e57ff70c2f69ebafcbb7c39c6c36
Lei Zhang <thestig@chromium.org>
2025-01-30 14:05:58 -0800

    [lensoverlay] Use ENABLE_PDF build flag more consistently
    Make conditionals more consistent, so the Lens code builds with GN
    enable_pdf=false.
    
    Bug: 393263509
    Change-Id: Iebac8209042a7d63d1c5e2ca430c7f87c821652c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219778
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413752}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -1806,11 +1806,13 @@ void LensOverlayController::UpdatePageContextualization(
     SuppressGhostLoader();
   }
 
+#if BUILDFLAG(ENABLE_PDF)
   // If the new page is a PDF, fetch the text from the page to be used as early
   // suggest signals.
   if (content_type == lens::MimeType::kPdf) {
     FetchVisiblePageIndexAndGetPartialPdfText(page_count.value_or(0));
   }
+#endif
 
   lens_overlay_query_controller_->SendPageContentUpdateRequest(
       initialization_data_->page_content_bytes_,
@@ -2044,6 +2046,7 @@ void LensOverlayController::InitializeOverlay(
   InitializeOverlayUI(*initialization_data_);
   base::UmaHistogramBoolean("Lens.Overlay.Shown", true);
 
+#if BUILDFLAG(ENABLE_PDF)
   // If PDF content was extracted from the page, fetch the text from the PDF to
   // be used as early suggest signals.
   if (initialization_data_->page_content_type_ == lens::MimeType::kPdf &&
@@ -2052,6 +2055,7 @@ void LensOverlayController::InitializeOverlay(
     FetchVisiblePageIndexAndGetPartialPdfText(
         initialization_data_->pdf_page_count_.value());
   }
+#endif
 
   // If the StartQueryFlow optimization is enabled, the page contents will not
   // be sent with the initial image request, so we need to send it here.
