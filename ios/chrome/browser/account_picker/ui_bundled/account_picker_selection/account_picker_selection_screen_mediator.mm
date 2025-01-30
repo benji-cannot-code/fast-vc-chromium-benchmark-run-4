df0a1eba1b116a49650a4fb0560f7f107aaf1a80
Marc Treib <treib@chromium.org>
2025-01-30 02:54:29 -0800

    [iOS] Migrate last (overlooked) places to accounts-on-device
    All iOS UIs were previously (behind a flag) updated to new
    accounts-on-device APIs (see linked bug), but it turns out I had
    overlooked one situation where a class internally calls its own
    observer method.
    This CL updates 3 instances of this. Hopefully this concludes the
    migration!
    
    Bug: 377467350
    Change-Id: I78d5a144540cd8243afa05a03bcc0e908d5f79c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6196972
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413392}

--- ios/chrome/browser/account_picker/ui_bundled/account_picker_selection/account_picker_selection_screen_mediator.mm
+++ ios/chrome/browser/account_picker/ui_bundled/account_picker_selection/account_picker_selection_screen_mediator.mm
@@ -76,8 +76,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   id<SystemIdentity> previousSelectedIdentity = _selectedIdentity;
   _selectedIdentity = identity;
-  [self identityUpdated:previousSelectedIdentity];
-  [self identityUpdated:_selectedIdentity];
+  [self handleIdentityUpdated:previousSelectedIdentity];
+  [self handleIdentityUpdated:_selectedIdentity];
 }
 
 #pragma mark - Private
