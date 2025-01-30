4b14b4ba2ae8bc8fee7815df87fb8bf8b941e65c
Ian Wells <iwells@chromium.org>
2025-01-30 11:32:24 -0800

    Update glic Detach() method animation
    Bug: 392110704, 388922182
    Change-Id: I1425c1336072bded9a982efd81a4878f56f79c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210518
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1413638}

--- chrome/browser/glic/glic_window_controller.h
+++ chrome/browser/glic/glic_window_controller.h
@@ -177,6 +177,7 @@ class GlicWindowController : public views::WidgetObserver {
     kOpenAnimation,
     kWaitingForGlicToLoad,
     kOpen,
+    kDetaching,
     kClosingToReopenDetached,
     kCloseAnimation,
   };
@@ -217,6 +218,9 @@ class GlicWindowController : public views::WidgetObserver {
 
   // Finishes closing off the widget after running the closing animation.
   void CloseFinish(bool reopen_detached);
+
+  // Called when the Detach() animation ends.
+  void DetachFinished();
 
   // Causes an immediate close (eg, for during shutdown).
   void ForceClose();
