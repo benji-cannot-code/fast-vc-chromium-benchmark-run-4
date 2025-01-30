5c7ac3ccf268f478176c0de3f7018a39dda36db3
Shelley Vohr <shelley.vohr@gmail.com>
2025-01-30 15:10:58 -0800

    Ensure default path filled for KDialog
    This PR ensures that SelectFileDialog on Linux correctly fills the Name
    field when a default path is provided. Existing logic ensured that the
    path command line argument was never empty, but if it's a relative path
    it won't be correctly prefilled.
    
    The added logic ensures that it's an absolute path created by appending
    the relative path to PWD if necessary.
    
    After the fix: https://github.com/user-attachments/assets/49ffa657-6d39-4357-adf3-e1a10c26bafa
    
    Bug: N/A
    Change-Id: If6056c2b9b22379ffc592c0cdec2f4a4ee69c913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219304
    Reviewed-by: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413795}

--- ui/shell_dialogs/select_file_dialog_linux_kde.cc
+++ ui/shell_dialogs/select_file_dialog_linux_kde.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/command_line.h"
+#include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
@@ -414,10 +415,16 @@ void SelectFileDialogLinuxKde::GetKDialogCommandLine(
   }
   command_line->AppendSwitch(type);
   // The path should never be empty. If it is, set it to PWD.
-  if (path.empty())
-    command_line->AppendArgPath(base::FilePath("."));
-  else
+  auto pwd = base::FilePath(".");
+  if (path.empty()) {
+    command_line->AppendArgPath(pwd);
+  } else if (path.IsAbsolute()) {
     command_line->AppendArgPath(path);
+  } else {
+    // KDialog won't set the default name in the Name field for relative paths.
+    auto abs_path = base::MakeAbsoluteFilePathNoResolveSymbolicLinks(path);
+    command_line->AppendArgPath(abs_path.value_or(pwd));
+  }
   // Depending on the type of the operation we need, get the path to the
   // file/folder and set up mime type filters.
   if (file_operation)
