84e1bfd7db76d280f579ff699fa83efc6542f469
Palak Agarwal <agpalak@chromium.org>
2025-01-30 03:35:16 -0800

    Add zoom level to media_stream_track_impl
    This change makes the zoom level for a captured tab available to us in
    MediaStreamTrackImpl. This value would be used in the follow-up change
    to get the correct logical resolution of a captured tab.
    
    Bug: 383946052
    Change-Id: I968db981157482673cf64971e0cc8f95cac7ce19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185884
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413407}

--- third_party/blink/renderer/modules/mediastream/media_stream_track_impl.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_track_impl.h
@@ -154,6 +154,8 @@ class MODULES_EXPORT MediaStreamTrackImpl : public MediaStreamTrack,
 
   void Trace(Visitor*) const override;
 
+  std::optional<int> GetZoomLevelForTesting() const { return zoom_level_; }
+
  protected:
   // Given a partially built MediaStreamTrackImpl, finishes the job of making it
   // into a clone of |this|.
@@ -174,7 +176,7 @@ class MODULES_EXPORT MediaStreamTrackImpl : public MediaStreamTrack,
   void SourceChangedCaptureConfiguration() override;
   void SourceChangedCaptureHandle() override;
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
-  void SourceChangedZoomLevel(int) override {}
+  void SourceChangedZoomLevel(int) override;
 #endif
   void PropagateTrackEnded();
 
@@ -219,6 +221,7 @@ class MODULES_EXPORT MediaStreamTrackImpl : public MediaStreamTrack,
   WeakMember<ExecutionContext> execution_context_;
   HeapHashSet<WeakMember<MediaStreamTrack::Observer>> observers_;
   bool muted_ = false;
+  std::optional<int> zoom_level_;
   MediaConstraints constraints_;
   std::optional<bool> suppress_local_audio_playback_setting_;
   Member<V8UnionMediaStreamTrackAudioStatsOrMediaStreamTrackVideoStats> stats_;
