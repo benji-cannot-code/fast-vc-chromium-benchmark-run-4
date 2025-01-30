1fa2776502d82e5edbdae23102fac3c7b95472c6
Andrey Kosyakov <caseq@chromium.org>
2025-01-30 10:33:53 -0800

    Revert "Suppress the media blocked indicator for PiP window"
    This reverts commit 36dda004f031604d5904d578a5a8dc453dd12edb.
    
    Reason for revert: tests fail consistently on win11-arm64-dbg-tests, sample failure:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/win11-arm64-dbg-tests/4610/test-results?sortby=&groupby=
    
    Original change's description:
    > Suppress the media blocked indicator for PiP window
    >
    > This CL fixes a bug when a PiP window tries to use camera/mic permission
    > and we display a blocked activity indicator. The PiP window does not
    > support blocked activity indicator, so we should not show it.
    >
    > Bug: 384770168
    > Change-Id: If3199c935c997ea37312de0acf00d8553bcf1f7a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198789
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Commit-Queue: Elias Klim <elklm@chromium.org>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1413445}
    
    Bug: 384770168
    Change-Id: I5e68f8d7070abb9b9003028249c89ea59692bf54
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219171
    Owners-Override: Andrey Kosyakov <caseq@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413601}

--- components/content_settings/browser/test_page_specific_content_settings_delegate.cc
+++ components/content_settings/browser/test_page_specific_content_settings_delegate.cc
@@ -64,9 +64,4 @@ bool TestPageSpecificContentSettingsDelegate::IsFrameAllowlistedForJavaScript(
   return false;
 }
 
-bool TestPageSpecificContentSettingsDelegate::IsPiPWindow(
-    content::WebContents* web_contents) {
-  return false;
-}
-
 }  // namespace content_settings
