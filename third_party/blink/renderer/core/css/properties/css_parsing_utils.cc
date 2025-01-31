31eddad1b18d87b530d0fe1b644dbb41cd9da53b
Aaron Krajeski <aaronhk@chromium.org>
2025-01-31 08:41:56 -0800

    Add remaining color spaces for interpolation
    Display P3, Rec2020, ProPhoto RGB and a98 RGB are specified in CSS color
    4 as predefined color spaces but had not yet been implemented as color
    spaces for interpolation.
    
    https://www.w3.org/TR/css-color-4/#predefined
    
    Skia change: https://skia-review.googlesource.com/c/skia/+/942876
    
    Primaries for ProPhotoRGB and transfer functions for ProPhotoRGB and a98
    RGB have been added to Skia, so we can remove them from our extensions
    file and reference Skia's copies instead.
    
    Bug: 383270111
    Change-Id: Ibd84bd7ae34f9f2b7e397bae7752b922f2fddec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6202302
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414169}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -2766,18 +2766,6 @@ static CSSValue* ConsumeDeprecatedRadialGradient(
              : nullptr;
 }
 
-static void MaybeLogUnsupportedGradientInterpolationSpaceWarning(
-    const CSSParserContext& context,
-    Color::ColorSpace color_space) {
-  if (const auto* document = context.GetDocument()) {
-    document->AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
-        mojom::blink::ConsoleMessageSource::kRecommendation,
-        mojom::blink::ConsoleMessageLevel::kWarning,
-        Color::ColorSpaceToString(color_space) +
-            " is not yet supported as a gradient interpolation space."));
-  }
-}
-
 static CSSValue* ConsumeRadialGradient(CSSParserTokenStream& stream,
                                        const CSSParserContext& context,
                                        cssvalue::CSSGradientRepeat repeating) {
@@ -2887,11 +2875,6 @@ static CSSValue* ConsumeRadialGradient(CSSParserTokenStream& stream,
           vertical_size, repeating, cssvalue::kCSSRadialGradient);
 
   if (has_color_space) {
-    if (Color::IsUndefinedColorSpaceForGradientInterpolation(color_space)) {
-      MaybeLogUnsupportedGradientInterpolationSpaceWarning(context,
-                                                           color_space);
-      return nullptr;
-    }
     result->SetColorInterpolationSpace(color_space, hue_interpolation_method);
     context.Count(WebFeature::kCSSColorGradientColorSpace);
   }
@@ -2960,11 +2943,6 @@ static CSSValue* ConsumeLinearGradient(
           end_x, end_y, nullptr, nullptr, angle, repeating, gradient_type);
 
   if (has_color_space) {
-    if (Color::IsUndefinedColorSpaceForGradientInterpolation(color_space)) {
-      MaybeLogUnsupportedGradientInterpolationSpaceWarning(context,
-                                                           color_space);
-      return nullptr;
-    }
     result->SetColorInterpolationSpace(color_space, hue_interpolation_method);
     context.Count(WebFeature::kCSSColorGradientColorSpace);
   }
@@ -3017,11 +2995,6 @@ static CSSValue* ConsumeConicGradient(CSSParserTokenStream& stream,
       center_x, center_y, from_angle, repeating);
 
   if (has_color_space) {
-    if (Color::IsUndefinedColorSpaceForGradientInterpolation(color_space)) {
-      MaybeLogUnsupportedGradientInterpolationSpaceWarning(context,
-                                                           color_space);
-      return nullptr;
-    }
     result->SetColorInterpolationSpace(color_space, hue_interpolation_method);
     context.Count(WebFeature::kCSSColorGradientColorSpace);
   }
