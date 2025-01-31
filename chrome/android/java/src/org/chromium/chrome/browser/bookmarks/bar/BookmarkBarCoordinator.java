3c504d401bae0dba95964ad534cab50ec254d37f
dmblack <dmblack@google.com>
2025-01-30 18:30:35 -0800

    Populate bookmark bar with items in clank.
    This CL populates the bookmark bar with items in clank. It does so by:
    * Creating a new RecyclerView backed container for rendering items
    * Creating a new ListModel for representing the items to be rendered
    * Updating the ListModel in response to BookmarkBarItemsProvider events
    
    Design: http://shortn/_bSnnOXZtVR
    Screenshot: http://shortn/_blaN4eI9Pt
    
    Still TODO:
    * Render item favicons
    * Handle item clicks
    
    Bug: 347632437
    Change-Id: I20c52653509785190d26e718a5826305d5b06829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194085
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1413886}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
@@ -6,21 +6,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.bookmarks.bar;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.view.ViewStub;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.fullscreen.BrowserControlsManager;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
+import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
 
 /** Coordinator for the bookmark bar which provides users with bookmark access from top chrome. */
 public class BookmarkBarCoordinator {
 
+    private final SimpleRecyclerViewAdapter mItemsAdapter;
     private final BookmarkBarMediator mMediator;
     private final BookmarkBar mView;
 
@@ -49,6 +55,21 @@ public class BookmarkBarCoordinator {
                 mView.findViewById(R.id.bookmark_bar_all_bookmarks_button),
                 BookmarkBarButtonViewBinder::bind);
 
+        // Bind adapter/model and initialize view for bookmark bar items.
+        final var itemsModel = new ModelList();
+        mItemsAdapter = new SimpleRecyclerViewAdapter(itemsModel);
+        mItemsAdapter.registerType(
+                BookmarkBarUtils.ViewType.ITEM,
+                this::inflateBookmarkBarButton,
+                BookmarkBarButtonViewBinder::bind);
+        final RecyclerView itemsContainer = mView.findViewById(R.id.bookmark_bar_items_container);
+        itemsContainer.setAdapter(mItemsAdapter);
+        itemsContainer.setLayoutManager(new BookmarkBarItemsLayoutManager(activity));
+
+        // NOTE: Scrolling isn't supported and items rarely change so item view caching is disabled.
+        itemsContainer.getRecycledViewPool().setMaxRecycledViews(BookmarkBarUtils.ViewType.ITEM, 0);
+        itemsContainer.setItemViewCacheSize(0);
+
         // Bind view/model for bookmark bar and instantiate mediator.
         final var model = new PropertyModel.Builder(BookmarkBarProperties.ALL_KEYS).build();
         mMediator =
@@ -57,6 +78,7 @@ public class BookmarkBarCoordinator {
                         allBookmarksButtonModel,
                         browserControlsManager,
                         heightChangeCallback,
+                        itemsModel,
                         model,
                         profileSupplier);
         PropertyModelChangeProcessor.create(model, mView, BookmarkBarViewBinder::bind);
@@ -64,6 +86,7 @@ public class BookmarkBarCoordinator {
 
     /** Destroys the bookmark bar coordinator. */
     public void destroy() {
+        mItemsAdapter.destroy();
         mMediator.destroy();
         mView.destroy();
     }
@@ -73,5 +96,11 @@ public class BookmarkBarCoordinator {
      */
     public ObservableSupplier<Integer> getHeightSupplier() {
         return mMediator.getHeightSupplier();
+    }
+
+    private @NonNull BookmarkBarButton inflateBookmarkBarButton(@NonNull ViewGroup parent) {
+        return (BookmarkBarButton)
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.bookmark_bar_button, parent, false);
     }
 }
