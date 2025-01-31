c4209222fd95f40383f12544ed5fef13333bb957
Anthony Cui <cuianthony@google.com>
2025-01-31 15:38:55 -0800

    Pass current hotkey configuration to glic fre web client
    This implements a URL-based messaging from the WebUI to the FRE app, described here https://docs.google.com/document/d/18M03ETfTK8DKaLEIOWXrRXBlaP-iiL8x9X5O1yjToQM/edit?tab=t.0
    
    Bug: 383620463
    Change-Id: Ib34478cbfd17450ad0a94eea56126a7f203532de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6220657
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414443}

--- chrome/browser/glic/glic_ui.cc
+++ chrome/browser/glic/glic_ui.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "chrome/browser/glic/fre_util.h"
 #include "chrome/browser/glic/glic_enabling.h"
 #include "chrome/browser/glic/glic_fre_page_handler.h"
 #include "chrome/browser/glic/glic_page_handler.h"
@@ -74,6 +75,9 @@ GlicUI::GlicUI(content::WebUI* web_ui) : ui::MojoWebUIController(web_ui) {
 
   // Set up guest URL via cli flag or default to finch param value.
   source->AddString("glicGuestURL", GetGuestURL().spec());
+
+  // Add current global hotkey configuration.
+  source->AddString("glicHotkeyString", GetHotkeyString());
 
   // Set up loading notice timeout values.
   source->AddInteger("preLoadingTimeMs", features::kGlicPreLoadingTimeMs.Get());
