ab1e1c66d8c3cbc5e19cb890aad9cb026befcd8e
Dan McArdle <dmcardle@chromium.org>
2025-01-31 06:08:12 -0800

    PrivateAggregation: Delete base::Feature for 100 contributions
    This CL deletes the base::Feature that increased Protected Audience
    callers' default number of contributions per report from 20 to 100:
    PrivateAggregationApi100ContributionsForProtectedAudience.
    
    Fixed: 389897604
    Change-Id: I50f444696207944e6b5b52cb662d793215771718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218025
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414066}

--- content/browser/private_aggregation/private_aggregation_host.cc
+++ content/browser/private_aggregation/private_aggregation_host.cc
@@ -10,11 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <bit>
-#include <iterator>
+#include <map>
 #include <memory>
 #include <optional>
 #include <set>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -22,12 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
-#include "base/containers/contains.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
+#include "base/location.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/numerics/clamped_math.h"
@@ -42,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/aggregation_service/aggregation_coordinator_utils.h"
 #include "content/browser/aggregation_service/aggregatable_report.h"
 #include "content/browser/private_aggregation/private_aggregation_budget_key.h"
-#include "content/browser/private_aggregation/private_aggregation_budgeter.h"
 #include "content/browser/private_aggregation/private_aggregation_caller_api.h"
 #include "content/browser/private_aggregation/private_aggregation_features.h"
 #include "content/browser/private_aggregation/private_aggregation_manager.h"
@@ -227,8 +227,7 @@ PrivateAggregationHost::GetEffectiveMaxContributions(
   // These constants define the maximum number of contributions that can go in
   // an `AggregatableReport` after merging.
   static constexpr size_t kMaxContributionsSharedStorage = 20;
-  static constexpr size_t kMaxContributionsProtectedAudience = 20;
-  static constexpr size_t kMaxContributionsProtectedAudienceIncreased = 100;
+  static constexpr size_t kMaxContributionsProtectedAudience = 100;
   static constexpr size_t kMaxContributionsWhenCustomized = 1000;
 
   if (requested_max_contributions.has_value()) {
@@ -246,10 +245,7 @@ PrivateAggregationHost::GetEffectiveMaxContributions(
     case PrivateAggregationCallerApi::kSharedStorage:
       return kMaxContributionsSharedStorage;
     case PrivateAggregationCallerApi::kProtectedAudience:
-      return base::FeatureList::IsEnabled(
-                 kPrivateAggregationApi100ContributionsForProtectedAudience)
-                 ? kMaxContributionsProtectedAudienceIncreased
-                 : kMaxContributionsProtectedAudience;
+      return kMaxContributionsProtectedAudience;
   }
   NOTREACHED();
 }
