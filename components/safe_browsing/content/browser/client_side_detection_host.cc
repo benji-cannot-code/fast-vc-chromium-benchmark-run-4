0dc65d64b7e05f337d7bcb51635bf0aca83ebac1
Seung Jae Lim <andysjlim@chromium.org>
2025-01-31 05:45:30 -0800

    Log the allowlist matches in CSPP on fullscreen API calls
    Server side CL: http://cl/721079156
    
    The purpose of this logging serves to understand the prevalence of
    non-allowlisted sites that use the fullscreen API.
    
    Bug: 392929910
    Change-Id: Id9438419e2a539935f4c60ce9ff48eba986eba33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6214114
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414053}

--- components/safe_browsing/content/browser/client_side_detection_host.cc
+++ components/safe_browsing/content/browser/client_side_detection_host.cc
@@ -126,6 +126,8 @@ std::string GetRequestTypeName(
       return "PointerLockRequested";
     case safe_browsing::ClientSideDetectionType::VIBRATION_API:
       return "VibrationApi";
+    case safe_browsing::ClientSideDetectionType::FULLSCREEN_API:
+      return "FullscreenApi";
   }
 }
 
@@ -316,6 +318,10 @@ class ClientSideDetectionHost::ShouldClassifyUrlRequest {
       base::UmaHistogramEnumeration(
           "SBClientPhishing.PreClassificationCheckResult", reason,
           PreClassificationCheckResult::NO_CLASSIFY_MAX);
+      base::UmaHistogramEnumeration(
+          "SBClientPhishing.PreClassificationCheckResult." +
+              GetRequestTypeName(phishing_detection_request_type_),
+          reason, PreClassificationCheckResult::NO_CLASSIFY_MAX);
       if (base::FeatureList::IsEnabled(
               kClientSideDetectionDebuggingMetadataCache) &&
           host_ && host_->delegate_->GetPrefs() &&
@@ -394,6 +400,12 @@ class ClientSideDetectionHost::ShouldClassifyUrlRequest {
 
     if (phishing_reason !=
         PreClassificationCheckResult::NO_CLASSIFY_NO_DATABASE_MANAGER) {
+      if (phishing_detection_request_type_ ==
+          ClientSideDetectionType::FULLSCREEN_API) {
+        base::UmaHistogramBoolean(
+            "SBClientPhishing.MatchCSDAllowlistOnFullscreenApi",
+            match_allowlist);
+      }
       // This check is also for logging purposes although the CSD allowlist
       // could be matched or not checked at all. Once it completes,
       // preclassification check will continue.
@@ -440,6 +452,16 @@ class ClientSideDetectionHost::ShouldClassifyUrlRequest {
     if (phishing_reason == NO_CLASSIFY_MAX && ShouldAcceptHCAllowlist()) {
       phishing_reason =
           PreClassificationCheckResult::NO_CLASSIFY_MATCH_HC_ALLOWLIST;
+    }
+
+    if (phishing_detection_request_type_ ==
+        ClientSideDetectionType::FULLSCREEN_API) {
+      // The purpose of triggering preclassification for fullscreen API to have
+      // an initial assessment on how often we'll be hitting the allowlist and
+      // triggering the classification. We will not go further than checking for
+      // this metric for now.
+      DontClassifyForPhishing(
+          PreClassificationCheckResult::NO_CLASSIFY_ALLOWLIST_METRIC);
     }
 
     CheckCache(phishing_reason);
@@ -761,6 +783,20 @@ void ClientSideDetectionHost::VibrationRequested() {
       ClientSideDetectionFeatureCache::FromWebContents(web_contents());
   if (!feature_cache_map->WasVibrationClassificationTriggered(current_url_)) {
     MaybeStartPreClassification(ClientSideDetectionType::VIBRATION_API);
+  }
+}
+
+void ClientSideDetectionHost::DidToggleFullscreenModeForTab(
+    bool entered_fullscreen,
+    bool will_cause_resize) {
+  // We do not check for entered_fullscreen, because although the user may never
+  // successfully enter fullscreen, we want to proceed with the
+  // preclassification check. Instead, we check for |last_fullscreen_url|
+  // instead which is updated per fresh committed URL on the RenderFrameHost.
+  content::RenderFrameHost* rfh = web_contents()->GetPrimaryMainFrame();
+  if (rfh->GetLastCommittedURL() != last_fullscreen_url_) {
+    last_fullscreen_url_ = rfh->GetLastCommittedURL();
+    MaybeStartPreClassification(ClientSideDetectionType::FULLSCREEN_API);
   }
 }
 
