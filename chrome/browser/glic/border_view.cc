382054d77b0b37648b8fa6b587a3e89c83afa060
William Liu <liuwilliam@chromium.org>
2025-01-31 10:39:16 -0800

    Reenable tests
    Bug: 393468180
    Change-Id: I967e1114f469181bbe28214d1d2ccc887f2ddeaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218029
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414257}

--- chrome/browser/glic/border_view.cc
+++ chrome/browser/glic/border_view.cc
@@ -362,6 +362,10 @@ void BorderView::StartAnimation() {
   compositor_ = compositor;
   compositor_->AddAnimationObserver(this);
   compositor_->AddObserver(this);
+
+  if (tester_) [[unlikely]] {
+    tester_->AnimationStarted();
+  }
 }
 
 void BorderView::CancelAnimation() {
@@ -411,6 +415,9 @@ void BorderView::ResetEmphasisAndReplay() {
   }
   first_emphasis_frame_ = base::TimeTicks{};
   SchedulePaint();
+  if (tester_) [[unlikely]] {
+    tester_->EmphasisRestarted();
+  }
 }
 
 float BorderView::GetOpacity(base::TimeTicks timestamp) const {
