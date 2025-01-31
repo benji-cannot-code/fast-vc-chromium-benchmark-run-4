2f093a17127e9fb05b678773525de2833c4f67bb
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-01-30 16:35:37 -0800

    Set background color for ShelfShutdownConfirmationBubble
    Bug: b:393394752
    Change-Id: I7e4fb3e1589862661663b559c9bd64d16ca836d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216780
    Reviewed-by: Vincent Chiang <vincentchiang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413843}

--- ash/shelf/shelf_shutdown_confirmation_bubble.cc
+++ ash/shelf/shelf_shutdown_confirmation_bubble.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shelf/shelf_shutdown_confirmation_bubble.h"
 
+#include "ash/public/cpp/shelf_config.h"
 #include "ash/shelf/login_shelf_button.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
@@ -189,6 +190,7 @@ void ShelfShutdownConfirmationBubble::OnThemeChanged() {
       AshColorProvider::ContentLayerType::kButtonLabelColor);
   cancel_->SetEnabledTextColors(button_color);
   confirm_->SetEnabledTextColors(button_color);
+  set_color(ShelfConfig::Get()->GetDefaultShelfColor(GetWidget()));
 }
 
 std::u16string ShelfShutdownConfirmationBubble::GetAccessibleWindowTitle()
