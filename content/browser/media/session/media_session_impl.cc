04d35cf9d422a6c15912c9398c198e03a512c845
Benjamin Keen <bkeen@google.com>
2025-01-31 06:49:53 -0800

    Record time during which PiP windows have been open for a MediaSession
    This CL expands the MediaSessionUmaHelper to record the time during
    which Picture in Picture windows have been open during a MediaSession.
    Which will help analyze the impact and/or user satisfaction with the
    media playback heuristics.
    
    Bug: 368058093
    Change-Id: Ia2c143209a3f0ab6ee247186475de425d7b1a07e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6204145
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414085}

--- content/browser/media/session/media_session_impl.cc
+++ content/browser/media/session/media_session_impl.cc
@@ -388,6 +388,7 @@ void MediaSessionImpl::MediaPictureInPictureChanged(
     bool is_picture_in_picture) {
   RebuildAndNotifyMediaSessionInfoChanged();
   RebuildAndNotifyActionsChanged();
+  uma_helper_.OnMediaPictureInPictureChanged(is_picture_in_picture);
 }
 
 void MediaSessionImpl::RenderFrameHostStateChanged(
@@ -1567,6 +1568,7 @@ void MediaSessionImpl::OnServiceCreated(MediaSessionServiceImpl* service) {
 }
 
 void MediaSessionImpl::OnServiceDestroyed(MediaSessionServiceImpl* service) {
+  uma_helper_.OnServiceDestroyed();
   services_.erase(service->GetRenderFrameHostId());
 
   if (routed_service_ == service)
