45fc03a01ba2cb13ef59be48061a55b7401df8ee
David Awogbemila <awogbemila@chromium.org>
2025-01-31 04:48:21 -0800

    Parse animation-trigger shorthand
    For reference: the spec is currently a work-in-progress[1] but this
    comment[2] describes the API.
    
    [1] https://github.com/w3c/csswg-drafts/pull/10128
    [2] https://github.com/w3c/csswg-drafts/issues/8942#issuecomment-1853533616
    
    Bug: 390314945
    Change-Id: I3b273f4565efddd2fd48d407102f9f1376d816dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6207949
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414034}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.h
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.h
@@ -83,6 +83,7 @@ using IsResetOnlyFunction = bool (*)(CSSPropertyID);
 using IsPositionKeyword = bool (*)(CSSValueID);
 
 constexpr size_t kMaxNumAnimationLonghands = 12;
+constexpr size_t kMaxNumAnimationTriggerLonghands = 6;
 
 void Complete4Sides(CSSValue* side[4]);
 
@@ -367,6 +368,13 @@ CSSValue* ConsumeSingleTimelineName(CSSParserTokenStream&,
                                     const CSSParserContext&);
 CSSValue* ConsumeSingleTimelineInset(CSSParserTokenStream&,
                                      const CSSParserContext&);
+
+bool ConsumeAnimationTriggerShorthand(const StylePropertyShorthand&,
+                                      HeapVector<Member<CSSValueList>, kMaxNumAnimationTriggerLonghands>&,
+                                      CSSParserTokenStream&,
+                                      const CSSParserContext&);
+
+CSSValue* GetImpliedRangeEnd(const CSSValue* start_range);
 
 void AddBackgroundValue(CSSValue*& list, const CSSValue*);
 CSSValue* ConsumeBackgroundAttachment(CSSParserTokenStream&);
