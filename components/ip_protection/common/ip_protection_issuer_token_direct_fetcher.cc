dab14f07f9078aaeb2031744657ed9ed5e979da4
Ryan Kalla <ryankalla@google.com>
2025-01-31 13:25:24 -0800

    Add exponential backoff for failed Issuer Token fetches.
    The TryGetIssuerTokensResult now has a `try_again_after` field which is
    set on failures and indicates when the next fetch should occur. The
    Issuer Token Manager (to be added later) will receive the
    `try_again_after` time and schedule the next token fetch.
    
    Bug: 391357128
    Change-Id: Iacf16f0ad4af7a1e74b313f544fa60424ebac2ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6208968
    Reviewed-by: Aykut Bulut <aykutb@google.com>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Commit-Queue: Ryan Kalla <ryankalla@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414373}

--- components/ip_protection/common/ip_protection_issuer_token_direct_fetcher.cc
+++ components/ip_protection/common/ip_protection_issuer_token_direct_fetcher.cc
@@ -139,6 +139,15 @@ IpProtectionIssuerTokenDirectFetcher::~IpProtectionIssuerTokenDirectFetcher() =
 
 void IpProtectionIssuerTokenDirectFetcher::TryGetIssuerTokens(
     TryGetIssuerTokensCallback callback) {
+  // If we are not able to call `RetrieveIssuerToken` yet, return early.
+  if (no_get_issuer_tokens_until_ > base::Time::Now()) {
+    std::move(callback).Run(
+        std::nullopt,
+        TryGetIssuerTokensResult{TryGetIssuerTokensStatus::kRequestBackedOff,
+                                 net::OK, no_get_issuer_tokens_until_});
+    return;
+  }
+
   retriever_.RetrieveIssuerToken(base::BindOnce(
       &IpProtectionIssuerTokenDirectFetcher::OnGetIssuerTokenCompleted,
       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
@@ -205,11 +214,18 @@ void IpProtectionIssuerTokenDirectFetcher::OnGetIssuerTokenCompleted(
     base::expected<std::optional<std::string>, int> response) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
+  if (!response.has_value() || !response.value().has_value()) {
+    // Apply exponential backoff to these sorts of failures.
+    no_get_issuer_tokens_until_ =
+        base::Time::Now() + next_get_issuer_tokens_backoff_;
+    next_get_issuer_tokens_backoff_ *= 2;
+  }
+
   if (!response.has_value()) {
     std::move(callback).Run(
-        std::nullopt,
-        TryGetIssuerTokensResult{TryGetIssuerTokensStatus::kNetNotOk,
-                                 response.error()});
+        std::nullopt, TryGetIssuerTokensResult{
+                          TryGetIssuerTokensStatus::kNetNotOk, response.error(),
+                          no_get_issuer_tokens_until_});
     // TODO(crbug.com/391358904): add failure metrics using response.error()
     // before returning.
     return;
@@ -219,7 +235,7 @@ void IpProtectionIssuerTokenDirectFetcher::OnGetIssuerTokenCompleted(
     std::move(callback).Run(
         std::nullopt,
         TryGetIssuerTokensResult{TryGetIssuerTokensStatus::kNetOkNullResponse,
-                                 net::OK});
+                                 net::OK, no_get_issuer_tokens_until_});
     // TODO(crbug.com/391358904): add failure metrics using response.error()
     // before returning.
     return;
@@ -242,6 +258,9 @@ void IpProtectionIssuerTokenDirectFetcher::OnGetIssuerTokenCompleted(
                             TryGetIssuerTokensResult{status, net::OK});
     return;
   }
+
+  // Cancel any backoff on success.
+  ClearBackoffTimer();
 
   // TODO(crbug.com/391358904): add success metrics before returning.
   TryGetIssuerTokensOutcome outcome;
@@ -296,6 +315,18 @@ IpProtectionIssuerTokenDirectFetcher::ValidateIssuerTokenResponse(
     }
   }
   return TryGetIssuerTokensStatus::kSuccess;
+}
+
+void IpProtectionIssuerTokenDirectFetcher::AccountStatusChanged(
+    bool account_available) {
+  if (account_available) {
+    ClearBackoffTimer();
+  }
+}
+
+void IpProtectionIssuerTokenDirectFetcher::ClearBackoffTimer() {
+  no_get_issuer_tokens_until_ = base::Time();
+  next_get_issuer_tokens_backoff_ = kGetIssuerTokensFailureTimeout;
 }
 
 }  // namespace ip_protection
