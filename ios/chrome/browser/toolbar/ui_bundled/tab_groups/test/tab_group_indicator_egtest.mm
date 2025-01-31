fe7a8fc5e0c2985f6ec343734b3224dc899a97ed
Ewann Pelle <ewannpv@chromium.org>
2025-01-31 07:04:16 -0800

    [iOS] Remove unused parameter
    Bug: None
    Change-Id: I4d96f78fe24235970b91550582bc4897016ff183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216685
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414099}

--- ios/chrome/browser/toolbar/ui_bundled/tab_groups/test/tab_group_indicator_egtest.mm
+++ ios/chrome/browser/toolbar/ui_bundled/tab_groups/test/tab_group_indicator_egtest.mm
@@ -162,7 +162,7 @@ void ShareTabGroupAndOpenMenu(
 }
 
 // Joins a tab group and open the tab group indicator menu.
-void JoinTabGroupAndOpenMenu(net::test_server::EmbeddedTestServer* testServer) {
+void JoinTabGroupAndOpenMenu() {
   [TabGroupAppInterface mockSharedEntitiesPreview];
   GURL joinGroupURL = data_sharing::GetDataSharingUrl(data_sharing::GroupToken(
       data_sharing::GroupId("resources%2F3be"), "CggHBicxA_slvx"));
@@ -577,7 +577,7 @@ void JoinTabGroupAndOpenMenu(net::test_server::EmbeddedTestServer* testServer) {
     EARL_GREY_TEST_SKIPPED(@"On iPad, the tab group indicator is not displayed "
                            @"if the tab strip is visible.");
   }
-  JoinTabGroupAndOpenMenu(self.testServer);
+  JoinTabGroupAndOpenMenu();
 
   // Verify that the delete button is not available.
   [[EarlGrey selectElementWithMatcher:DeleteGroupButton()]
