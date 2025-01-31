340f16359ee017892d682424452d7c0a3ae295d6
Radu Nitescu <radunitescu@google.com>
2025-01-31 06:08:27 -0800

    [iOS][LU] Make Lens Unification dependent on Lens Overlay being enabled
    Fixed: 393155714
    Change-Id: I6b0270a6f0ff87a653089ad48bb588b30b9d82b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218493
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414067}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_availability.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_availability.mm
@@ -37,7 +37,8 @@ bool IsLensOverlaySameTabNavigationEnabled() {
 }
 
 bool IsLVFUnifiedExperienceEnabled() {
-  return base::FeatureList::IsEnabled(kEnableLensViewFinderUnifiedExperience);
+  return IsLensOverlayAvailable() &&
+         base::FeatureList::IsEnabled(kEnableLensViewFinderUnifiedExperience);
 }
 
 LensOverlayOnboardingTreatment GetLensOverlayOnboardingTreatment() {
