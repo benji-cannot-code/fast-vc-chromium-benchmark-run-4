33ed4a48c1113a345de3fdac1b476e272456bbeb
Bryan Nguyen <nguyenbryan@google.com>
2025-01-31 12:23:03 -0800

    [lensoverlay] Add chrome://flags entry for open in new tab.
    Bug: b:393166874
    Change-Id: Ieae4ccf379a66f17ccda2cebd3153c13dfdad20e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6221566
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414330}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1398,6 +1398,9 @@ extern const char kLensOverlayContextualSearchboxDescription[];
 extern const char kLensOverlayImageContextMenuActionsName[];
 extern const char kLensOverlayImageContextMenuActionsDescription[];
 
+extern const char kLensOverlaySidePanelOpenInNewTabName[];
+extern const char kLensOverlaySidePanelOpenInNewTabDescription[];
+
 extern const char kLensOverlayTranslateButtonName[];
 extern const char kLensOverlayTranslateButtonDescription[];
 
