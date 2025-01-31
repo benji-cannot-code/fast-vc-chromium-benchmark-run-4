beefab6fc3ac38fd7775509192162b089c5e3e10
Kavita Soni <kavitasoni@chromium.org>
2025-01-31 08:16:02 -0800

    Add strike count to SaveCreditCardOptions.
    This change adds strike count(number of times a card has been denied
    for upload) to SaveCreditCardOptions. This will be used to log credit
    card metrics with strike count information in future CL.
    
    Bug: 391930612
    Change-Id: Ieb92880e923ffafc058a9dad46a791c2e3d94784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6207929
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Kavita Soni <kavitasoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414149}

--- components/autofill/core/browser/payments/payments_autofill_client.h
+++ components/autofill/core/browser/payments/payments_autofill_client.h
@@ -174,6 +174,11 @@ class PaymentsAutofillClient : public RiskDataLoader {
       return *this;
     }
 
+    SaveCreditCardOptions& with_num_strikes(const int strikes) {
+      num_strikes = strikes;
+      return *this;
+    }
+
     SaveCreditCardOptions& with_card_save_type(CardSaveType b) {
       card_save_type = b;
       return *this;
@@ -185,6 +190,7 @@ class PaymentsAutofillClient : public RiskDataLoader {
     bool has_multiple_legal_lines = false;
     bool has_same_last_four_as_server_card_but_different_expiration_date =
         false;
+    std::optional<int> num_strikes;
     CardSaveType card_save_type = CardSaveType::kCardSaveOnly;
   };
 
