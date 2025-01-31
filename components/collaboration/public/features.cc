6723b909c24b23315aa2495f1d7ae1faca194f2d
Hailey Wang <haileywang@google.com>
2025-01-31 15:26:39 -0800

    [Collaboration] Enable collaboration flow on Android by default
    Change-Id: I58ca13f82d8318bfe4dfb855dacd3e0e205aa22e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6222177
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414438}

--- components/collaboration/public/features.cc
+++ components/collaboration/public/features.cc
@@ -20,6 +20,6 @@ BASE_FEATURE(kCollaborationMessagingDatabase,
 
 BASE_FEATURE(kCollaborationFlowAndroid,
              "CollaborationFlowAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace collaboration::features
