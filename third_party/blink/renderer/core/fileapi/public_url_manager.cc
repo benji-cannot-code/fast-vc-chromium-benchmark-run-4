6bd6e6fb53681a0a9ee66c63f5fa1d845e15c418
Janice Liu <janiceliu@chromium.org>
2025-01-31 12:24:53 -0800

    [Blob URL] Partition Blob URL Subframe Navigations
    `noopener` is enforced for Blob URL navigations, but navigations that
    aren't top-level should be partitioned to align with Firefox and Safari.
    
    Bug: 381211737
    Change-Id: I8a97dc4355b230db7d1fd3f6f45246bf13d14a90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172828
    Commit-Queue: Janice Liu <janiceliu@chromium.org>
    Auto-Submit: Janice Liu <janiceliu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Janice Liu <janiceliu@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414331}

--- third_party/blink/renderer/core/fileapi/public_url_manager.cc
+++ third_party/blink/renderer/core/fileapi/public_url_manager.cc
@@ -273,9 +273,10 @@ void PublicURLManager::Resolve(
       WTF::BindOnce(metrics_callback, WrapPersistent(GetExecutionContext())));
 }
 
-void PublicURLManager::Resolve(
+void PublicURLManager::ResolveForNavigation(
     const KURL& url,
-    mojo::PendingReceiver<mojom::blink::BlobURLToken> token_receiver) {
+    mojo::PendingReceiver<mojom::blink::BlobURLToken> token_receiver,
+    bool is_top_level_navigation) {
   if (is_stopped_)
     return;
 
@@ -292,7 +293,7 @@ void PublicURLManager::Resolve(
   };
 
   GetBlobURLStore().ResolveForNavigation(
-      url, std::move(token_receiver),
+      url, std::move(token_receiver), is_top_level_navigation,
       WTF::BindOnce(metrics_callback, WrapPersistent(GetExecutionContext())));
 }
 
