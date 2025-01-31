f468fc82b74d17b95c04d4d3eff415ce224b57fa
manukh <manukh@chromium.org>
2025-01-30 17:09:39 -0800

    [omnibox][spark] Cache during ongoing requests and after error responses
    - Keep cached matches while running new request. If new request returns
      an error, keep cached matches until next successful request. This
      prevents the popup from showing 0 matches in between requests that
      show 8 matches.
    
    - When new response returns successfully, even if it contains 0 results,
      clear cached matches. This prevents showing matches that we know no
      longer match the input.
    
    - Add `AutocompleteController` as provider listener.
    
    - Sanitize match strings.
    
    - Add some tests
    
    Bug: 380631004, 380642574
    Change-Id: I2dfee2c2e64bed90bc6990365d4f58f5301dd152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219170
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Alex Chen <alexwchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413857}

--- components/omnibox/browser/enterprise_search_aggregator_provider.cc
+++ components/omnibox/browser/enterprise_search_aggregator_provider.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match_classification.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
+#include "components/omnibox/browser/autocomplete_provider_listener.h"
 #include "components/omnibox/browser/remote_suggestions_service.h"
 #include "components/omnibox/browser/search_suggestion_parser.h"
 #include "components/omnibox/common/omnibox_feature_configs.h"
@@ -40,11 +41,14 @@ std::string ptr_to_string(const std::string* ptr) {
 }  // namespace
 
 EnterpriseSearchAggregatorProvider::EnterpriseSearchAggregatorProvider(
-    AutocompleteProviderClient* client)
+    AutocompleteProviderClient* client,
+    AutocompleteProviderListener* listener)
     : AutocompleteProvider(
           AutocompleteProvider::TYPE_ENTERPRISE_SEARCH_AGGREGATOR),
       client_(client),
-      debouncer_(std::make_unique<AutocompleteProviderDebouncer>(true, 300)) {}
+      debouncer_(std::make_unique<AutocompleteProviderDebouncer>(true, 300)) {
+  AddListener(listener);
+}
 
 EnterpriseSearchAggregatorProvider::~EnterpriseSearchAggregatorProvider() =
     default;
@@ -109,14 +113,8 @@ void EnterpriseSearchAggregatorProvider::Run() {
   CHECK(template_url->policy_origin() ==
         TemplateURLData::PolicyOrigin::kSearchAggregator);
 
-  // Clear old matches from the last response.
-  matches_.clear();
-
-  auto match = CreateMatch(
-      input_, template_url->keyword(), SuggestionType::CONTENT, true, 1500,
-      "https://wikipedia.org", u"Your document", u"Last edited Feb 25");
-  matches_.push_back(match);
-  NotifyListeners(/*updated_matches=*/true);
+  // Don't clear `matches_` until a new successful response is ready to replace
+  // them.
 
   client_->GetRemoteSuggestionsService(/*create_if_necessary=*/true)
       ->CreateEnterpriseSearchAggregatorSuggestionsRequest(
@@ -145,6 +143,8 @@ void EnterpriseSearchAggregatorProvider::RequestCompleted(
       UpdateResults(SearchSuggestionParser::ExtractJsonData(
           source, std::move(response_body)));
 
+  // Keep showing old `matches_` if received an error response.
+
   loader_.reset();
   done_ = true;
   NotifyListeners(/*updated_matches=*/updated_matches);
@@ -158,6 +158,10 @@ bool EnterpriseSearchAggregatorProvider::UpdateResults(
     return false;
   }
 
+  // Clear old matches if received a successful response, even if the response
+  // is empty.
+  matches_.clear();
+
   // Fill `matches_` with the new server matches.
   ParseEnterpriseSearchAggregatorSearchResults(*response);
 
@@ -166,9 +170,10 @@ bool EnterpriseSearchAggregatorProvider::UpdateResults(
 
 void EnterpriseSearchAggregatorProvider::
     ParseEnterpriseSearchAggregatorSearchResults(const base::Value& root_val) {
+  auto adjusted_input = input_;
   const TemplateURL* template_url =
       AutocompleteInput::GetSubstitutingTemplateURLForInput(
-          client_->GetTemplateURLService(), &input_);
+          client_->GetTemplateURLService(), &adjusted_input);
 
   // Parse the results.
   const base::Value::List* queryResults =
@@ -300,12 +305,12 @@ AutocompleteMatch EnterpriseSearchAggregatorProvider::CreateMatch(
   match.destination_url = GURL(url);
   match.fill_into_edit = base::UTF8ToUTF16(url);
 
-  match.description = description;
+  match.description = AutocompleteMatch::SanitizeString(description);
   match.description_class = ClassifyTermMatches(
       FindTermMatches(input.text(), match.description),
       match.description.size(), ACMatchClassification::MATCH,
       ACMatchClassification::NONE);
-  match.contents = contents;
+  match.contents = AutocompleteMatch::SanitizeString(contents);
   match.contents_class = ClassifyTermMatches(
       FindTermMatches(input.text(), match.contents), match.contents.size(),
       ACMatchClassification::MATCH | ACMatchClassification::URL,
