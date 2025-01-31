bb0690a2411eb046c78c7fc71214ec964ac6d014
Riley Tatum <rtatum@google.com>
2025-01-31 12:31:27 -0800

    [NTP][Enterprise] Enable Microsoft card flags by default
    Enabling Microsoft cards by default because they are still disabled
    by default by policy. These flags will be used as a kill switch and
    the policies for actually enabling for testing.
    
    Change-Id: Ia9ddac3b2cb48615ed47768d02971114aa727a79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6221206
    Auto-Submit: Riley Tatum <rtatum@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414337}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -180,7 +180,7 @@ BASE_FEATURE(kNtpOneGoogleBar,
 // If enabled, outlook calendar module will be shown.
 BASE_FEATURE(kNtpOutlookCalendarModule,
              "NtpOutlookCalendarModule",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, Google Photos module will be shown.
 BASE_FEATURE(kNtpPhotosModule,
@@ -221,7 +221,7 @@ BASE_FEATURE(kNtpSafeBrowsingModule,
 // If enabled, sharepoint module will be shown.
 BASE_FEATURE(kNtpSharepointModule,
              "NtpSharepointModule",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, shortcuts will be shown.
 // This is a kill switch. Keep indefinitely.
@@ -282,7 +282,7 @@ BASE_FEATURE(kNtpMobilePromo,
 // If enabled, the Microsoft Authentication module will be shown.
 BASE_FEATURE(kNtpMicrosoftAuthenticationModule,
              "NtpMicrosoftAuthenticationModule",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, the OGB loader will request for the async bar parts payload type.
 BASE_FEATURE(kNtpOneGoogleBarAsyncBarParts,
