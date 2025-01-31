cb699c51513b6407ed82d6fa288fbf80b808a3c7
Greg Thompson <grt@chromium.org>
2025-01-31 12:16:54 -0800

    [windows] Add trace events around ObjectWatcher's thread pool ops
    To help us evaluate the real cost of RegisterWaitForSingleObject and
    UnregisterWaitEx.
    
    Bug: none
    Change-Id: I3a73f50ca10ac3c1f918f49f1d281d64bf7d15d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216928
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414324}

--- base/win/object_watcher.cc
+++ base/win/object_watcher.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/threading/thread_restrictions.h"
+#include "base/trace_event/base_tracing.h"
 
 namespace base {
 namespace win {
@@ -53,17 +54,22 @@ bool ObjectWatcher::StopWatching() {
   // INVALID_HANDLE_VALUE to wait on all callback functions seemlingly waits
   // on other callbacks in the threadpool; not just callbacks from
   // RegisterWaitForSingleObject.
-  WaitableEvent event;
-  if (!UnregisterWaitEx(wait_object_, event.handle())) {
-    // ERROR_IO_PENDING is not a fatal error; see
-    // https://learn.microsoft.com/en-us/windows/win32/sync/unregisterwaitex.
-    if (const auto error = ::GetLastError(); error != ERROR_IO_PENDING) {
-      DPLOG(FATAL) << "UnregisterWaitEx failed";
-      return false;
+  {
+    // Measure the total cost of calling UnregisterWaitEx, including creation of
+    // and waiting on the event.
+    TRACE_EVENT("base", "UnregisterWaitEx");
+    WaitableEvent event;
+    if (!UnregisterWaitEx(wait_object_, event.handle())) {
+      // ERROR_IO_PENDING is not a fatal error; see
+      // https://learn.microsoft.com/en-us/windows/win32/sync/unregisterwaitex.
+      if (const auto error = ::GetLastError(); error != ERROR_IO_PENDING) {
+        DPLOG(FATAL) << "UnregisterWaitEx failed";
+        return false;
+      }
     }
+    // Wait for unregistration to complete.
+    event.Wait();
   }
-  // Wait for unregistration to complete.
-  event.Wait();
   Reset();
   return true;
 }
@@ -122,6 +128,7 @@ bool ObjectWatcher::StartWatchingInternal(HANDLE object,
                             base::UnsafeDanglingUntriaged(delegate));
   object_ = object;
 
+  TRACE_EVENT("base", "RegisterWaitForSingleObject");
   if (!RegisterWaitForSingleObject(&wait_object_, object, DoneWaiting, this,
                                    INFINITE, wait_flags)) {
     DPLOG(FATAL) << "RegisterWaitForSingleObject failed";
