2d123d47b63767542a9f0b1fcf38971a7b7ec8d1
Eric Sum <esum@google.com>
2025-01-31 10:10:23 -0800

    Enable OverviewSessionInitOptimizations by default.
    It's time to launch it.
    
    Bug: b:362835104
    Change-Id: I0b7a3de66a3b08c5d999352bf532ec14681270f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6200896
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414230}

--- ash/wm/desks/desks_unittests.cc
+++ ash/wm/desks/desks_unittests.cc
@@ -9752,6 +9752,7 @@ TEST_P(DeskBarTest, NewDeskButton) {
 
     EXPECT_TRUE(desks_controller->CanRemoveDesks());
     EXPECT_THAT(desks_controller->GetNumberOfDesks(), i);
+    views::test::RunScheduledLayout(desk_bar_view);
   }
 
   // The new desk button should be disabled.
