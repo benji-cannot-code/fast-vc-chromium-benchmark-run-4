b23f5029c5de53194dbed487f71aba3016664af7
Kent Tamura <tkent@chromium.org>
2025-01-30 22:10:33 -0800

    Stop using TextRunPaintInfo for DrawText().
    These functions refer to only the TextRun field.
    This CL has no behavior changes.
    
    Bug: 389726691
    Change-Id: I25985c94ea7a1af683a78548a34acc629e9c288d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217383
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413921}

--- third_party/blink/renderer/platform/graphics/graphics_context.h
+++ third_party/blink/renderer/platform/graphics/graphics_context.h
@@ -72,7 +72,6 @@ class PaintController;
 class Path;
 class StrokeData;
 class StyledStrokeData;
-struct TextRunPaintInfo;
 
 // Tiling parameters for the DrawImageTiled() method.
 struct ImageTilingInfo {
@@ -404,7 +403,7 @@ class PLATFORM_EXPORT GraphicsContext {
                 const AutoDarkMode& auto_dark_mode);
 
   void DrawEmphasisMarks(const Font&,
-                         const TextRunPaintInfo&,
+                         const TextRun&,
                          const AtomicString& mark,
                          const gfx::PointF&,
                          const AutoDarkMode& auto_dark_mode);
@@ -415,7 +414,7 @@ class PLATFORM_EXPORT GraphicsContext {
                          const AutoDarkMode& auto_dark_mode);
 
   void DrawBidiText(const Font&,
-                    const TextRunPaintInfo&,
+                    const TextRun&,
                     const gfx::PointF&,
                     const AutoDarkMode& auto_dark_mode);
 
