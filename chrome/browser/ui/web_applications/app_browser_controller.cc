f8d2c9c1d15459f9fc081079c8812c20cc023a5a
Marijn Kruisselbrink <mek@chromium.org>
2025-01-31 11:15:36 -0800

    Refactor GetEffectiveClientModeAndBrowserForCapturing
    This splits up the old GetAppHostForCapturing helper method
    into 3 separate methods for the various bits of functionality
    it was used for:
    - AppBrowserController::FindNoOpenerTabForWebApp, to find an existing
      tab to navigate or focus for a navigate or focus existing launch mode.
    - A helper FindNormalBrowser method that returns the most recently
      active "normal" browser in a given profile.
    - A bug fix for the existing AppBrowserController::FindForWebApp
      method, to return the most recently active non-popup app browser
      for a given app id.
    
    These 3 pieces are then combined back together in a new
    GetEffectiveClientModeAndBrowser method now as part of the
    NavigationCapturingProcess class. This does mean that in some
    cases we're iterating over all the browsers multiple times
    (first to see if there is a tab to capture a focus/navigate existing
    navigation, and if there was none a second one to figure out what
    browser to open the new tab in), I think this makes the code more
    readable and easier to extend to add support for tabbed mode.
    
    Finally, the other place where the old GetEffectiveClientMode
    method was used, in the intent-picker focus/navigate existing code,
    we call FindNoOpenerTabForWebApp directly, as we're always in the
    same branch of the old method in this case.
    
    Bug: 390257745
    Change-Id: I64bd3d7f9a1a9a7e687e3c533dd5e3cee58d0ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6213068
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414285}

--- chrome/browser/ui/web_applications/app_browser_controller.cc
+++ chrome/browser/ui/web_applications/app_browser_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "base/memory/values_equivalent.h"
 #include "base/strings/escape.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/trace_event.h"
@@ -105,14 +106,11 @@ bool AppBrowserController::IsForWebApp(const Browser* browser,
 // static
 Browser* AppBrowserController::FindForWebApp(const Profile& profile,
                                              const webapps::AppId& app_id) {
-  const BrowserList* browser_list = BrowserList::GetInstance();
-  for (auto it = browser_list->begin_browsers_ordered_by_activation();
-       it != browser_list->end_browsers_ordered_by_activation(); ++it) {
-    Browser* browser = *it;
+  for (Browser* browser : BrowserList::GetInstance()->OrderedByActivation()) {
     if (browser->IsAttemptingToCloseBrowser() || browser->IsBrowserClosing()) {
       continue;
     }
-    if (browser->type() == Browser::TYPE_POPUP) {
+    if (!browser->is_type_app()) {
       continue;
     }
     if (browser->profile() != &profile) {
@@ -124,6 +122,49 @@ Browser* AppBrowserController::FindForWebApp(const Profile& profile,
     return browser;
   }
   return nullptr;
+}
+
+// static
+std::optional<AppBrowserController::BrowserAndTabIndex>
+AppBrowserController::FindTopLevelBrowsingContextForWebApp(
+    const Profile& profile,
+    const webapps::AppId& app_id,
+    Browser::Type browser_type) {
+  for (Browser* browser : BrowserList::GetInstance()->OrderedByActivation()) {
+    if (browser->IsAttemptingToCloseBrowser() || browser->IsBrowserClosing()) {
+      continue;
+    }
+    if (browser->type() != browser_type) {
+      continue;
+    }
+    if (browser->profile() != &profile) {
+      continue;
+    }
+    if (IsWebApp(browser) && !IsForWebApp(browser, app_id)) {
+      continue;
+    }
+    // The active web contents should have preference if it is in scope.
+    content::WebContents* active_contents =
+        browser->tab_strip_model()->GetActiveWebContents();
+    if (browser->tab_strip_model()->active_index() != TabStripModel::kNoTab) {
+      if (base::ValuesEquivalent(&app_id,
+                                 WebAppTabHelper::GetAppId(active_contents)) &&
+          !active_contents->HasOpener()) {
+        return {{browser, browser->tab_strip_model()->active_index()}};
+      }
+    }
+    // Otherwise, use the first one for the app.
+    for (int i = 0; i < browser->tab_strip_model()->count(); ++i) {
+      content::WebContents* contents =
+          browser->tab_strip_model()->GetWebContentsAt(i);
+      if (base::ValuesEquivalent(&app_id,
+                                 WebAppTabHelper::GetAppId(contents)) &&
+          !contents->HasOpener()) {
+        return {{browser, i}};
+      }
+    }
+  }
+  return std::nullopt;
 }
 
 // static
