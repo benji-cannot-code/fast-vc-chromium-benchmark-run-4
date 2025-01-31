81f8ecb4f2fd23f4ec40a4d59c8a8b635b0c9814
Saifuddin Hitawala <hitawala@chromium.org>
2025-01-31 11:54:29 -0800

    [media] Remove unused Capabilities param in readback util method
    Remove unused Capabilities param in readback util method
    
    Bug: 343011436
    Change-Id: Icca3f0eed225f459a558d6179142d5298b5411d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216329
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414311}

--- media/base/video_util.cc
+++ media/base/video_util.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/shared_image_format.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/raster_interface.h"
-#include "gpu/command_buffer/common/capabilities.h"
 #include "media/base/limits.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_frame_pool.h"
@@ -707,7 +706,6 @@ bool I420CopyWithPadding(const VideoFrame& src_frame, VideoFrame* dst_frame) {
 scoped_refptr<VideoFrame> ReadbackTextureBackedFrameToMemorySync(
     VideoFrame& txt_frame,
     gpu::raster::RasterInterface* ri,
-    const gpu::Capabilities& caps,
     VideoFramePool* pool) {
   DCHECK(ri);
 
@@ -740,7 +738,7 @@ scoped_refptr<VideoFrame> ReadbackTextureBackedFrameToMemorySync(
     gfx::Rect src_rect(0, 0, txt_frame.columns(plane), txt_frame.rows(plane));
     if (!ReadbackTexturePlaneToMemorySync(txt_frame, plane, src_rect,
                                           result->writable_data(plane),
-                                          result->stride(plane), ri, caps)) {
+                                          result->stride(plane), ri)) {
       return nullptr;
     }
   }
@@ -752,8 +750,7 @@ bool ReadbackTexturePlaneToMemorySync(VideoFrame& src_frame,
                                       gfx::Rect& src_rect,
                                       uint8_t* dest_pixels,
                                       size_t dest_stride,
-                                      gpu::raster::RasterInterface* ri,
-                                      const gpu::Capabilities& caps) {
+                                      gpu::raster::RasterInterface* ri) {
   DCHECK(ri);
 
   bool result = ReadbackTexturePlaneToMemorySyncOOP(
