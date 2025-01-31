62c82fec402f74a1cc8fb5e587220cc2ec2c7eae
Christian Dullweber <dullweber@chromium.org>
2025-01-31 03:02:39 -0800

    [synapse] Add aria-label to ai learn more link
    Construct the learn more URL for managed AI features such that it has
    an aria-label attribute and a aria-description which tell what the
    page behind the link is about and that it opens in a new tab.
    
    Skip-Translation-Screenshots-Check: True
    Bug: 392656269, 378868667
    Change-Id: Iac49f64f9d121265c40dfdfb8a79d063df7b854a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218927
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414001}

--- chrome/browser/resources/settings/ai_page/ai_info_card.ts
+++ chrome/browser/resources/settings/ai_page/ai_info_card.ts
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import '../settings_shared.css.js';
 
-import {sanitizeInnerHtml} from 'chrome://resources/js/parse_html_subset.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {loadTimeData} from '../i18n_setup.js';
@@ -32,16 +31,10 @@ export class SettingsAiInfoCardElement extends PolymerElement {
         type: String,
         computed: 'computeIcon3_()',
       },
-
-      sublabel3_: {
-        type: TrustedHTML,
-        computed: 'computeSublabel3_()',
-      },
     };
   }
 
   private icon3_: string;
-  private sublabel3_: TrustedHTML;
 
   private isManaged_(): boolean {
     return loadTimeData.getBoolean('isManaged');
@@ -50,13 +43,6 @@ export class SettingsAiInfoCardElement extends PolymerElement {
   private computeIcon3_(): string {
     return this.isManaged_() ? loadTimeData.getString('managedByIcon') :
                                'settings20:account-box';
-  }
-
-  private computeSublabel3_(): TrustedHTML {
-    return sanitizeInnerHtml(
-        this.isManaged_() ?
-            loadTimeData.getString('aiPageMainManagedSublabel3') :
-            loadTimeData.getString('aiPageMainSublabel3'));
   }
 }
 
