ec59a7cf81cd528adcade5e6e33c3db11e070660
Chuong Ho <hdchuong@google.com>
2025-01-30 16:29:43 -0800

    lobster: add a flag that forces to show disclaimer screen again
    For easy testing of the disclaimer UI, this CL adds a flag to force
    showing the disclaimer screen every time the lobster is triggered.
    
    Bug: b:348279617
    Change-Id: I0ad77aca79a4089d1b213fe8e78111679b9c1570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217561
    Reviewed-by: Jon Htin <jhtin@google.com>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413839}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -591,6 +591,8 @@ BASE_DECLARE_FEATURE(kLobsterQuickInsertZeroState);
 COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kLobsterFeedback);
 COMPONENT_EXPORT(ASH_CONSTANTS)
+BASE_DECLARE_FEATURE(kLobsterAlwaysShowDisclaimerForTesting);
+COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kLobsterRightClickMenu);
 COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kFeatureManagementLobster);
@@ -1212,6 +1214,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLauncherSearchControlEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLinkCrossDeviceDogfoodFeedbackEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLinkCrossDeviceInternalsEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLobsterEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLobsterAlwaysShowDisclaimerForTesting();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsLockScreenHideSensitiveNotificationsSupported();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLockScreenNotificationsEnabled();
