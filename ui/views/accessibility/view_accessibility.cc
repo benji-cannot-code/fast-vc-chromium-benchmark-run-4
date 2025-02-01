e1fd6da842f06047ee9ce416e421cfd49a51c89a
Dileep Maurya <dileepmaurya@microsoft.com>
2025-01-31 22:17:57 -0800

    Reland "[views-ax] Migrating kValue attributes to new system in Slider and QuickSettingsSlider views"
    https://issues.chromium.org/issues/383390902 was introduced due to
    kValue attribute change in QuickSettingsSlider where ChromeVox reading
    "Brightness Slider Percentage QuickSettings" instead of "QuickSettings"
    when the focus is on the status bar.
    
    This issue occurred because of unnecessary accessibility events of
    Brightness slider was getting triggered while QuickSettingsSlider gets
    AddedToWidget. To fix this we update the kValue in the cache when
    "value_" changes and stopped triggering the kValueChanged accessibility
    event.
    
    Original change description
    
    > This CL migrates the kValue attribute in slider.cc and
    > quick_settings_slider.cc files where we move setting of
    > ax::mojom::StringAttribute::kValue from pull-based
    > architecture to push based architecture. This involves
    > removing the explicit instances of kValue from the view's
    > GetAccessibleNodeData and instead use(or directly push) these
    > properties in the view accessibility cache.
    > This CL is part of the ViewsAX project:
    > https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    > Bug: 325137417
    > Change-Id: Ia31e698ca49069ee36b482817c861417393663ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857352
    > Commit-Queue: Dileep Maurya <dileepmaurya@microsoft.com>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Reviewed-by: Jacques Newman <janewman@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1375452}
    >
    
    Bug: 383390902
    Change-Id: I706b4a1458f97092c41cf8e14d4ce693e380fd50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6204484
    Commit-Queue: Dileep Maurya <dileepmaurya@microsoft.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1414541}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -1051,9 +1051,10 @@ void ViewAccessibility::SetValue(const std::string& value) {
   }
   data_.AddStringAttribute(ax::mojom::StringAttribute::kValue, value);
 
-  OnStringAttributeChanged(ax::mojom::StringAttribute::kValue, value);
-
-  NotifyEvent(ax::mojom::Event::kValueChanged, true);
+  if (ready_to_notify_events_) {
+    OnStringAttributeChanged(ax::mojom::StringAttribute::kValue, value);
+    NotifyEvent(ax::mojom::Event::kValueChanged, true);
+  }
 }
 
 void ViewAccessibility::SetValue(const std::u16string& value) {
