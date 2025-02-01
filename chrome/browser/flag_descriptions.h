f2eaf1d901177b33e624d5e1d937cab18fd0def9
henrika <henrika@google.com>
2025-02-01 03:26:20 -0800

    Adds support for system AEC flag on Windows
    This CL:
    
    Checks the feature flag EnforceSystemEchoCancellation on Windows and
    adds AudioParameters::ECHO_CANCELLER to the input device parameters if
    the flag is enabled during device enumeration.
    
    Checks audio parameters for AudioParameters::ECHO_CANCELLER when an
    input stream is created and sets a boolean if it was enabled.
    
    Applies an experimental System AEC mode on Windows 24H2 and above if
    AudioParameters::ECHO_CANCELLER was set.
    
    As a result: it is now possible to enable a flag and thereby activate a
    system AEC on Windows 24H2 and above.
    
    The CL also adds support for the EnforceSystemEchoCancellation flag in
    chrome://flags for Mac AND Windows:
    
    The new UI flag looks like this:
    https://screenshot.googleplex.com/7Y5YkJwA7TN6jy5
    
    Support for the command-line flag has already landed in
    https://chromium-review.googlesource.com/c/chromium/src/+/5953258
    
    Bug: 393098901
    Bug: 375523826
    Change-Id: I4faa8ada4c2f714323c952be2626702ea6d3e137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218470
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414567}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3296,6 +3296,9 @@ extern const char kBlockRootWindowAccessibleNameChangeEventDescription[];
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 
+extern const char kEnforceSystemEchoCancellationName[];
+extern const char kEnforceSystemEchoCancellationDescription[];
+
 extern const char kLocationProviderManagerName[];
 extern const char kLocationProviderManagerDescription[];
 
