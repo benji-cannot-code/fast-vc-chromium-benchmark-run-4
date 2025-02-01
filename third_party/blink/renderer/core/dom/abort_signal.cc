c62ca4545702f796a58767b4632bf8bec9722852
Nate Chapin <japhet@chromium.org>
2025-01-31 23:18:51 -0800

    Reland "Ensure AbortSignal.throwIfAborted doesn't modify the message of an abort error"
    This reverts commit 74320221ad24d0050a8baa031ca1d0e998ea7fc6.
    
    Reason for revert: Not the cause of issues.chromium.org/issues/393009392
    
    Original change's description:
    > Revert "Ensure AbortSignal.throwIfAborted doesn't modify the message of an abort error"
    >
    > This reverts commit 0e0ef178e05e3f81b0de04c8f0197fe3ab608f41.
    >
    > Reason for revert: Possible culprit for windows builder closure, see https://g-issues.chromium.org/issues/393009392.
    >
    > Original change's description:
    > > Ensure AbortSignal.throwIfAborted doesn't modify the message of an abort error
    > >
    > > Fixed: 392413688
    > > Change-Id: I3639c573cd7349eac1f9b09c427a5a52bccdda72
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6209470
    > > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > > Commit-Queue: Nate Chapin <japhet@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1412605}
    >
    > Change-Id: Ifb785338a85e77e845a677940fc1353b74757b0f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6211537
    > Commit-Queue: Patti Lor <patricialor@chromium.org>
    > Owners-Override: Patti Lor <patricialor@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Patti Lor <patricialor@chromium.org>
    > Auto-Submit: Patti Lor <patricialor@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1412694}
    
    Change-Id: Ic42d7631d3ebe6a1c902e0763fdc3335f5262b33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6221802
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414549}

--- third_party/blink/renderer/core/dom/abort_signal.cc
+++ third_party/blink/renderer/core/dom/abort_signal.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/time/time.h"
 #include "third_party/blink/public/platform/task_type.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/core/dom/abort_signal_composition_manager.h"
 #include "third_party/blink/renderer/core/dom/abort_signal_composition_type.h"
@@ -185,6 +186,7 @@ ScriptValue AbortSignal::reason(ScriptState* script_state) const {
 void AbortSignal::throwIfAborted(v8::Isolate* isolate) const {
   if (!aborted())
     return;
+  OmitExceptionContextInformation omit(isolate);
   V8ThrowException::ThrowException(isolate, abort_reason_.V8Value());
 }
 
