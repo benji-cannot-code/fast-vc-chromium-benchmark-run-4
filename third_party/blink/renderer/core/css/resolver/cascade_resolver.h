f11768ea69285584fc9e36ba44742ba9b387c384
Anders Hartvoll Ruud <andruud@chromium.org>
2025-02-01 17:33:09 -0800

    [functions] Use AtomicString for attributes in CycleElem
    The attribute name is already an AtomicString at both relevant
    call sites. This reduces indirection noise when digging the value
    out of the variant, and removes any concerns regarding the safety
    of holding on to a raw pointer here.
    
    This is a minor cleanup to prepare for detecting cycles
    for local variables in @function rules.
    
    Bug: 325504770
    Change-Id: Icebadcffb624bd4a513a8ef46dc9b7714e45412f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190029
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414622}

--- third_party/blink/renderer/core/css/resolver/cascade_resolver.h
+++ third_party/blink/renderer/core/css/resolver/cascade_resolver.h
@@ -34,7 +34,7 @@ class CORE_EXPORT CascadeResolver {
   STACK_ALLOCATED();
 
  public:
-  using CycleElem = absl::variant<const CSSProperty*, const String*>;
+  using CycleElem = absl::variant<const CSSProperty*, AtomicString>;
   // TODO(crbug.com/985047): Probably use a HashMap for this.
   using CycleStack = Vector<CycleElem, 8>;
 
@@ -42,7 +42,7 @@ class CORE_EXPORT CascadeResolver {
   // In other words, once a property is locked, locking it again would form
   // a cycle, and is therefore an error.
   bool IsLocked(const CSSProperty&) const;
-  bool IsLocked(const String& attribute) const;
+  bool IsLocked(const AtomicString& attribute) const;
 
   // Returns the property we're currently applying.
   const CSSProperty* CurrentProperty() const {
@@ -91,7 +91,7 @@ class CORE_EXPORT CascadeResolver {
 
    public:
     AutoLock(const CSSProperty&, CascadeResolver&);
-    AutoLock(const String& attribute, CascadeResolver&);
+    AutoLock(const AtomicString& attribute, CascadeResolver&);
     ~AutoLock();
 
    private:
@@ -115,7 +115,7 @@ class CORE_EXPORT CascadeResolver {
   // The marked range of the stack shrinks during ~AutoLock, such that we won't
   // be InCycle whenever we move outside that of that range.
   bool DetectCycle(const CSSProperty&);
-  bool DetectCycle(const String& attribute);
+  bool DetectCycle(const AtomicString& attribute);
   bool DetectCycle(wtf_size_t index);
   // Returns true whenever the CascadeResolver is in a cycle state.
   // This DOES NOT detect cycles; the caller must call DetectCycle first.
@@ -124,7 +124,7 @@ class CORE_EXPORT CascadeResolver {
   // CSSPropertyName), or kNotFound if the property (name) is not present in
   // stack_.
   wtf_size_t Find(const CSSProperty&) const;
-  wtf_size_t Find(const String& attribute) const;
+  wtf_size_t Find(const AtomicString& attribute) const;
 
   CycleStack stack_;
   // If we're in a cycle, cycle_start_ is the index of the stack_ item that
