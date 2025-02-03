54b163f56932965a8025f8214bd1a328c11e6ced
Lei Zhang <thestig@chromium.org>
2025-02-03 11:27:53 -0800

    Remove Windows-only include in cross-platform web_applications code
    Remove a spurious include of uninstallation_via_os_settings.h, which is
    Windows-only.
    
    Change-Id: I1b2c73c06f76e160b45f5b6cfd5bab6fc902f3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6222974
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415087}

--- chrome/browser/web_applications/os_integration/uninstallation_via_os_settings_sub_manager.cc
+++ chrome/browser/web_applications/os_integration/uninstallation_via_os_settings_sub_manager.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
-#include "chrome/browser/win/uninstallation_via_os_settings.h"
 
 namespace web_app {
 
