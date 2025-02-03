a5304817fd8599baeb7ec80b8cab116b6d981434
Olivier ROBIN <olivierrobin@google.com>
2025-02-03 07:35:27 -0800

    Update GoogleOneAPI to take a config at creation
    Follow up on crrev.com/c/6187393: Update the creation method to take a
    config object
    
     - crrev.com/c/6187393 : Create the new API
     - cl/716205930 : create the internal class
    => crrev.com/c/6217062 : Update the new API
     - crrev.com/i/7971741 : implement the new provider
     - crrev.com/c/6197979 : use the new provider
    
    Change-Id: I80c6ca0c3efade11475da8e165c9d006f26b242d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217062
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414922}

--- ios/chrome/browser/providers/google_one/chromium_google_one.mm
+++ ios/chrome/browser/providers/google_one/chromium_google_one.mm
@@ -7,10 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/public/provider/chrome/browser/google_one/google_one_api.h"
 
+@implementation GoogleOneConfiguration
+
+@end
+
 namespace ios {
 namespace provider {
 
-id<GoogleOneController> CreateGoogleOneController() {
+id<GoogleOneController> CreateGoogleOneController(
+    GoogleOneConfiguration* configuration) {
   return nil;
 }
 
--- ios/chrome/test/providers/google_one/test_google_one.h
+++ ios/chrome/test/providers/google_one/test_google_one.h
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol GoogleOneControllerFactory
 
 // Create a GoogleOneController.
-- (id<GoogleOneController>)createController;
+- (id<GoogleOneController>)createControllerWithConfiguration:
+    (GoogleOneConfiguration*)configuration;
 
 @end
 
