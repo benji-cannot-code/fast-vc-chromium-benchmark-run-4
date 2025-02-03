11386c224a117e3fd88aa1f59fb7d75bd011f917
Emily Shack <emshack@chromium.org>
2025-02-03 10:23:39 -0800

    [SxS] Notify BrowserView of split view creation
    Adds an observer method for when a split view is created. BrowserView
    then listens for this and stores the TabInterfaces that are part of the
    split view, so that both on split view creation and on active tab change
    it can update the contents view appropriately. Currently just has
    a placeholder for actually updating the contents view, which will be
    done in a follow-up. Like the previous CL, this is hacky and subject to
    change, but will enable prototyping for now.
    
    Bug: 391894786
    Change-Id: Ib9092de64d6e95b82733e045d99acd07246d239b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216803
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415032}

--- chrome/browser/ui/tabs/test/mock_tab_interface.h
+++ chrome/browser/ui/tabs/test/mock_tab_interface.h
@@ -71,6 +71,7 @@ class MockTabInterface : public testing::NiceMock<TabInterface> {
               (override));
   MOCK_METHOD(TabFeatures*, GetTabFeatures, (), (override));
   MOCK_METHOD(bool, IsPinned, (), (const override));
+  MOCK_METHOD(bool, IsSplit, (), (const override));
   MOCK_METHOD(std::optional<tab_groups::TabGroupId>,
               GetGroup,
               (),
