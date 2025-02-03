9f6c3583441ac39a855b876402a5e730f1464457
Minoru Chikamune <chikamune@chromium.org>
2025-02-02 22:42:33 -0800

    Bugfix: Fix wrong overlap of PageLoad.Clients.LCPP.*.PerConfidence*
    Before this CL, CalculateScoreFromConfidenceAndTotalFrequency() function
    returned the following value.
    
    10 * normalized_confidence_0_to_10 + normalized_total_frequency_0_to_10
    
    But this was wrong because we can't distinguish between 10 * 0 + 10 and
    10 * 1 + 0. The correct formula had to be the following.
    
    10 * normalized_confidence_0_to_9 + normalized_total_frequency_0_to_9
    
    By restricting the upper value this way, we can resolve the
    overlap. This CL fixes this problem.
    
    Bug: 40063266
    Change-Id: Ic0e00d732e22e6e3022ea0a2c2b8e396fa012886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217465
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414755}

--- chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.cc
@@ -184,12 +184,11 @@ const char* ConvertConfidenceToSuffix(double confidence) {
 
 int CalculateScoreFromConfidenceAndTotalFrequency(double confidence,
                                                   double total_frequency) {
-  int normalized_confidence_0_to_10 =
-      static_cast<int>(std::floor(std::clamp(10.0 * confidence, 0.0, 10.0)));
-  int normalized_total_frequency_0_to_10 = static_cast<int>(
-      std::floor(std::clamp(total_frequency / 10.0, 0.0, 10.0)));
-  return 10 * normalized_confidence_0_to_10 +
-         normalized_total_frequency_0_to_10;
+  int normalized_confidence_0_to_9 =
+      static_cast<int>(std::floor(std::clamp(10.0 * confidence, 0.0, 9.9)));
+  int normalized_total_frequency_0_to_9 = static_cast<int>(
+      std::floor(std::clamp(total_frequency / 10.0, 0.0, 9.9)));
+  return 10 * normalized_confidence_0_to_9 + normalized_total_frequency_0_to_9;
 }
 
 void MaybeReportConfidenceUMAs(
@@ -244,7 +243,8 @@ void MaybeReportConfidenceUMAs(
               HISTOGRAM_PREFIX
               "ImageLoadingPriority"
               ".TotalFrequencyOfActualPositive"
-              ".PerConfidence",
+              ".PerConfidence"
+              ".2",
               CalculateScoreFromConfidenceAndTotalFrequency(confidence,
                                                             total_frequency));
         };
@@ -261,7 +261,8 @@ void MaybeReportConfidenceUMAs(
               HISTOGRAM_PREFIX
               "ImageLoadingPriority"
               ".TotalFrequencyOfActualNegative"
-              ".PerConfidence",
+              ".PerConfidence"
+              ".2",
               CalculateScoreFromConfidenceAndTotalFrequency(confidence,
                                                             total_frequency));
         };
@@ -412,14 +413,15 @@ void MaybeReportConfidenceUMAs(
               HISTOGRAM_PREFIX
               "Subresource"
               ".TotalFrequencyOfActualPositive"
-              ".PerConfidence",
+              ".PerConfidence"
+              ".2",
               CalculateScoreFromConfidenceAndTotalFrequency(confidence,
                                                             total_frequency));
           base::UmaHistogramPercentage(
               base::StrCat({HISTOGRAM_PREFIX "Subresource"
                                              ".TotalFrequencyOfActualPositive"
                                              ".PerConfidence",
-                            is_same_site ? ".SameSite" : ".CrossSite"}),
+                            is_same_site ? ".SameSite" : ".CrossSite", ".2"}),
               CalculateScoreFromConfidenceAndTotalFrequency(confidence,
                                                             total_frequency));
         };
@@ -443,14 +445,15 @@ void MaybeReportConfidenceUMAs(
               HISTOGRAM_PREFIX
               "Subresource"
               ".TotalFrequencyOfActualNegative"
-              ".PerConfidence",
+              ".PerConfidence"
+              ".2",
               CalculateScoreFromConfidenceAndTotalFrequency(confidence,
                                                             total_frequency));
           base::UmaHistogramPercentage(
               base::StrCat({HISTOGRAM_PREFIX "Subresource"
                                              ".TotalFrequencyOfActualNegative"
                                              ".PerConfidence",
-                            is_same_site ? ".SameSite" : ".CrossSite"}),
+                            is_same_site ? ".SameSite" : ".CrossSite", ".2"}),
               CalculateScoreFromConfidenceAndTotalFrequency(confidence,
                                                             total_frequency));
         };
