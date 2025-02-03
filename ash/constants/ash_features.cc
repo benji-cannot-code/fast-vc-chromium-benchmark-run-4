4de2404fa7e74a89e24b9028fffc9df6826476ad
Jason Thai <jasontt@chromium.org>
2025-02-03 10:05:26 -0800

    p13n: add SeaPenPromptRewrite flag
    BUG=b:393364111
    TEST=Manually compile chrome
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ia0f032624153a196b8218b63bed483e3f9f49556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6221578
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415016}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2550,6 +2550,11 @@ BASE_FEATURE(kSeaPenDemoMode,
              "SeaPenDemoMode",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enables sea pen prompt rewrite feature.
+BASE_FEATURE(kSeaPenPromptRewrite,
+             "SeaPenPromptRewrite",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables sea pen feature with next templates.
 BASE_FEATURE(kSeaPenUseExptTemplate,
              "SeaPenUseExptTemplate",
@@ -4339,6 +4344,11 @@ bool IsScannerEnabled() {
 
 bool IsSeaPenDemoModeEnabled() {
   return IsSeaPenEnabled() && base::FeatureList::IsEnabled(kSeaPenDemoMode);
+}
+
+bool IsSeaPenPromptRewriteEnabled() {
+  return IsSeaPenTextInputEnabled() &&
+         base::FeatureList::IsEnabled(kSeaPenPromptRewrite);
 }
 
 bool IsSeaPenEnabled() {
