07c28e503a6f0c3f603eab44ec6a74ac7c55f11b
Christian Xu <christianxu@google.com>
2025-02-03 09:55:45 -0800

    [iOS][LO] Add CHECK to ensure non-empty vectors
    Add CHECK to ensure that the vector is non empty before the pop. The
    vector is already checked in CanGoBack which is called before these
    methods.
    https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/lens_overlay/model/lens_overlay_navigation_manager.mm;l=94-102;drc=3deaac799dc39f0ba14200463fd31bba49eb72a6
    
    The CHECK is added to rule out this possibility during crash
    investigation.
    
    Bug: None
    Change-Id: Iadbc3d02874ea10627c91b2a3ab3dbe6dde670b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226800
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1415009}

--- ios/chrome/browser/lens_overlay/model/lens_overlay_navigation_manager.mm
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_navigation_manager.mm
@@ -185,6 +185,8 @@ void LensOverlayNavigationManager::OnNavigationListUpdate() const {
 }
 
 void LensOverlayNavigationManager::GoToPreviousSubNavigation() {
+  CHECK(!lens_navigation_items_.empty());
+  CHECK(!lens_navigation_items_.back()->sub_navigations().empty());
   std::vector<LensSubNavigationItem>& sub_navigation =
       lens_navigation_items_.back()->sub_navigations();
   // Removes current sub navigation.
@@ -197,6 +199,7 @@ void LensOverlayNavigationManager::GoToPreviousSubNavigation() {
 }
 
 void LensOverlayNavigationManager::GoToPreviousLensNavigation() {
+  CHECK(!lens_navigation_items_.empty());
   // Remove the current lens navigation.
   lens_navigation_items_.pop_back();
   LensResultItem& previous_item = *lens_navigation_items_.back();
