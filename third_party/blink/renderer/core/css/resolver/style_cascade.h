baf57e338822f879c6a2cf88be7801bac94be5a0
Munira Tursunova <moonira@google.com>
2025-02-03 04:28:44 -0800

    Add support for inline if function
    Current implementation compares computed custom property's value with
    a raw query specified value. This might cause some of the queries with
    registered custom properties to be evaluated incorrectly. See failing
    test in external/wpt/css/css-values/if-style-query.html.
    
    Comparison with computed query specified value to be done in the next
    CL.
    
    Bug: 346977961
    Change-Id: I998cf13836796c65a618c08385bfa5e93d010c3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179820
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414842}

--- third_party/blink/renderer/core/css/resolver/style_cascade.h
+++ third_party/blink/renderer/core/css/resolver/style_cascade.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_property_name.h"
 #include "third_party/blink/renderer/core/css/css_property_value.h"
+#include "third_party/blink/renderer/core/css/kleene_value.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_token.h"
 #include "third_party/blink/renderer/core/css/parser/css_tokenizer.h"
 #include "third_party/blink/renderer/core/css/properties/css_bitset.h"
@@ -42,6 +43,7 @@ class CSSValue;
 class CSSVariableData;
 class CustomProperty;
 class MatchResult;
+class MediaQueryFeatureExpNode;
 class StyleResolverState;
 
 namespace cssvalue {
@@ -449,9 +451,19 @@ class CORE_EXPORT StyleCascade {
                        const CSSParserContext&,
                        TokenSequence&);
   bool ResolveAutoBaseInto(CSSParserTokenStream&,
-                                           CascadeResolver&,
-                                           const CSSParserContext&,
-                                           TokenSequence&);
+                           CascadeResolver&,
+                           const CSSParserContext&,
+                           TokenSequence&);
+  bool ResolveIfInto(CSSParserTokenStream&,
+                     CascadeResolver&,
+                     const CSSParserContext&,
+                     TokenSequence&);
+
+  bool EvalIfCondition(CSSParserTokenStream& stream,
+                       CascadeResolver& resolver,
+                       const CSSParserContext& context);
+  KleeneValue EvalIfStyleFeature(const MediaQueryFeatureExpNode&,
+                                 CascadeResolver&);
 
   // NOTE: The FunctionContext object must be the _caller's_ function context,
   // not the one the function itself sets up. This is because it is used to
