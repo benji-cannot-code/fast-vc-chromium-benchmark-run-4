4e4244abc6512b1054c1401b962f311b398b52bb
Sam Davis Omekara <samomekarajr@microsoft.com>
2025-02-03 15:56:10 -0800

    [Gap Decorations]: Parse `column-rule-break` property
    This CL introduces parsing for the `column-rule-break` property which
    controls the behavior of breaking gap decorations within gap into
    segments, see: https://drafts.csswg.org/css-gaps-1/#break.
    
    Bug: 357648037
    Change-Id: I7201d9113654e689d2dfcba3981e9138c3dfc60d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216649
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1415251}

--- third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
@@ -1238,6 +1238,10 @@ bool CSSParserFastPaths::IsValidKeywordPropertyAndValue(
       return value_id == CSSValueID::kAuto ||
              value_id == CSSValueID::kOptimizespeed ||
              value_id == CSSValueID::kOptimizequality;
+    case CSSPropertyID::kColumnRuleBreak:
+      return value_id == CSSValueID::kNone ||
+             value_id == CSSValueID::kSpanningItem ||
+             value_id == CSSValueID::kIntersection;
     case CSSPropertyID::kDirection:
       return value_id == CSSValueID::kLtr || value_id == CSSValueID::kRtl;
     case CSSPropertyID::kDominantBaseline:
@@ -1744,6 +1748,7 @@ CSSBitset CSSParserFastPaths::handled_by_keyword_fast_paths_properties_{{
     CSSPropertyID::kColorInterpolation,
     CSSPropertyID::kColorInterpolationFilters,
     CSSPropertyID::kColorRendering,
+    CSSPropertyID::kColumnRuleBreak,
     CSSPropertyID::kDirection,
     CSSPropertyID::kDominantBaseline,
     CSSPropertyID::kEmptyCells,
