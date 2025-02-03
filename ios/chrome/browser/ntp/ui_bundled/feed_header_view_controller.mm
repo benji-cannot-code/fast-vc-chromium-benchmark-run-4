af46681c6c25daa5dbcbe297ae77f7d64c90d171
Ginny Huang <ginnyhuang@chromium.org>
2025-02-03 08:50:27 -0800

    Revert "[iOS][follow-entry] Hide the segment control in feed header selectively"
    This reverts commit b6d6a5987e939fd36325d36d4029eac70f822ec2.
    
    Reason for revert: Project Cancelled
    
    Original change's description:
    > [iOS][follow-entry] Hide the segment control in feed header selectively
    >
    > This CL removes the following feed from the feed wrapper view
    > controller, as well as the segment control to select it in the feed
    > header view when `IsNewFollowingFeedEntryPointsEnabled()` is enabled.
    >
    > The following feed will later be displayed in a separate full-screen
    > sheet.
    >
    > Bug: 359325090
    > Change-Id: Ie1d75a0e4e42bea92da58d6d3461a04de48f4e2a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099000
    > Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Adam Arcaro <adamta@google.com>
    > Commit-Queue: Adam Arcaro <adamta@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1397985}
    
    Bug: 359325090
    Change-Id: I418013cee3e442233743d72222757cf7f40f32d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6220112
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414964}

--- ios/chrome/browser/ntp/ui_bundled/feed_header_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/feed_header_view_controller.mm
@@ -139,7 +139,7 @@ NSInteger kFeedSymbolPointSize = 17;
 - (void)viewWillLayoutSubviews {
   [super viewWillLayoutSubviews];
 
-  if ([self isFollowingEntryPointInFeedHeader]) {
+  if ([self.feedControlDelegate isFollowingFeedAvailable]) {
     [self updateSegmentedControlFont:self.segmentedControl];
   } else {
     UIFont* font = [self fontForTitleLabel];
@@ -166,14 +166,14 @@ NSInteger kFeedSymbolPointSize = 17;
 #pragma mark - Public
 
 - (CGFloat)feedHeaderHeight {
-  return [self isFollowingEntryPointInFeedHeader]
+  return [self.feedControlDelegate isFollowingFeedAvailable]
              ? kDiscoverFeedHeaderHeightWithFollowing
              : kDiscoverFeedHeaderHeightWithoutFollowing;
 }
 
 - (CGFloat)customSearchEngineViewHeight {
   return [self.NTPDelegate isGoogleDefaultSearchEngine] ||
-                 ![self isFollowingEntryPointInFeedHeader]
+                 ![self.feedControlDelegate isFollowingFeedAvailable]
              ? 0
              : kCustomSearchEngineLabelHeight;
 }
@@ -182,7 +182,7 @@ NSInteger kFeedSymbolPointSize = 17;
   if (!self.viewLoaded) {
     return;
   }
-  if (![self isFollowingEntryPointInFeedHeader]) {
+  if (![self.feedControlDelegate isFollowingFeedAvailable]) {
     [self.titleLabel setText:[self feedHeaderTitleText]];
     [self.titleLabel setNeedsDisplay];
     return;
@@ -200,7 +200,7 @@ NSInteger kFeedSymbolPointSize = 17;
   // When feed visibility changes, the menu content is recreated.
   [self.feedMenuHandler configureManagementMenu:self.managementButton];
 
-  if (![self isFollowingEntryPointInFeedHeader]) {
+  if (![self.feedControlDelegate isFollowingFeedAvailable]) {
     [self.titleLabel setText:[self feedHeaderTitleText]];
     [self.titleLabel setNeedsDisplay];
     return;
@@ -260,15 +260,8 @@ NSInteger kFeedSymbolPointSize = 17;
 
 #pragma mark - Private
 
-// If `YES`, the following fead will be displayed within the feed wrapper view
-// controller when the user requests it using `self.segmentedControl`.
-- (BOOL)isFollowingEntryPointInFeedHeader {
-  return [self.feedControlDelegate isFollowingFeedAvailable] &&
-         !IsNewFollowingFeedEntryPointsEnabled();
-}
-
 - (void)configureHeaderViews {
-  if ([self isFollowingEntryPointInFeedHeader]) {
+  if ([self.feedControlDelegate isFollowingFeedAvailable]) {
     if ([self.feedControlDelegate shouldFeedBeVisible]) {
       [self addViewsForVisibleFeed];
     } else {
@@ -340,7 +333,7 @@ NSInteger kFeedSymbolPointSize = 17;
   managementButton.accessibilityLabel =
       l10n_util::GetNSString(IDS_IOS_DISCOVER_FEED_MENU_ACCESSIBILITY_LABEL);
 
-  if ([self isFollowingEntryPointInFeedHeader]) {
+  if ([self.feedControlDelegate isFollowingFeedAvailable]) {
     buttonConfiguration.image =
         DefaultSymbolTemplateWithPointSize(kMenuSymbol, kFeedSymbolPointSize);
     managementButton.clipsToBounds = YES;
@@ -359,7 +352,7 @@ NSInteger kFeedSymbolPointSize = 17;
 
 // Configures and returns the feed header's sorting button.
 - (UIButton*)createSortButton {
-  DCHECK([self isFollowingEntryPointInFeedHeader]);
+  DCHECK([self.feedControlDelegate isFollowingFeedAvailable]);
 
   UIButton* sortButton = [[UIButton alloc] init];
 
@@ -500,7 +493,7 @@ NSInteger kFeedSymbolPointSize = 17;
 
   CGFloat totalHeaderHeight =
       [self feedHeaderHeight] + [self customSearchEngineViewHeight];
-  totalHeaderHeight += [self isFollowingEntryPointInFeedHeader]
+  totalHeaderHeight += [self.feedControlDelegate isFollowingFeedAvailable]
                            ? kTopVerticalPaddingFollowing
                            : kTopVerticalPadding;
   // Anchor container.
@@ -531,7 +524,7 @@ NSInteger kFeedSymbolPointSize = 17;
     ]];
   }
 
-  if ([self isFollowingEntryPointInFeedHeader]) {
+  if ([self.feedControlDelegate isFollowingFeedAvailable]) {
     // Anchor views based on the feed being visible or hidden.
     if ([self.feedControlDelegate shouldFeedBeVisible]) {
       [self anchorSegmentedControl];
@@ -694,7 +687,7 @@ NSInteger kFeedSymbolPointSize = 17;
 
 // The title text for the Discover feed header based on user prefs.
 - (NSString*)feedHeaderTitleText {
-  DCHECK(![self isFollowingEntryPointInFeedHeader]);
+  DCHECK(![self.feedControlDelegate isFollowingFeedAvailable]);
 
   // Set the title based on the default search engine.
   NSString* feedHeaderTitleText =
