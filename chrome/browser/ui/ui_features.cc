075b1d6c09c7463dd2f9790a80791ae2ec8f173e
Christian Biesinger <cbiesinger@chromium.org>
2025-02-03 08:48:45 -0800

    [FedCM] Enable FedCmContinueWithoutName
    To make the Continue button just say "Continue" when the
    IDP did not send a given_name instead of falling back
    to "Continue as Fullname".
    
    As discussed in
    https://chat.google.com/room/AAAA2v7vEk0/LRlM0TItHgI
    
    Fixed: 382086283
    Change-Id: Id7d560768af60d55cc03382f12ed539bf9f101bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6225861
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414961}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -415,6 +415,6 @@ BASE_FEATURE(kCompositorLoadingAnimations,
 
 BASE_FEATURE(kFedCmContinueWithoutName,
              "FedCmContinueWithoutName",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace features
