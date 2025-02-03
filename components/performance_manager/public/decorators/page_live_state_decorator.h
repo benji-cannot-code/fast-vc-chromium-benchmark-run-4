9e8ae0c37a660d1dca8eee4900ea8a7619c2a66d
David Sanders <dsanders11@ucsbalum.com>
2025-02-03 12:40:37 -0800

    Remove unused include in page_live_state_decorator.h
    Also removes a forward declaration which has no effect since the header
    is being included.
    
    Bug: 40318405
    Change-Id: I28503870cc3bebb671a57dd2bd7a2c931f8c5161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181693
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415131}

--- components/performance_manager/public/decorators/page_live_state_decorator.h
+++ components/performance_manager/public/decorators/page_live_state_decorator.h
@@ -13,12 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/graph/node_data_describer.h"
 #include "components/performance_manager/public/graph/page_node.h"
-#include "content/public/browser/web_contents_observer.h"
+#include "content/public/browser/web_contents_capability_type.h"
 #include "url/gurl.h"
-
-namespace content {
-class WebContents;
-}  // namespace content
 
 namespace performance_manager {
 
