1cf4e7c955c8e5b1e7e12c407d09fe3fc70b3c7f
Lei Zhang <thestig@chromium.org>
2025-02-03 11:04:40 -0800

    Make ChromeMetricsServiceAccessor metrics service binding PPAPI-only
    Make ChromeMetricsServiceAccessor::BindMetricsServiceReceiver()
    PPAPI-only, as it is only used from PepperUMAHost, not from plugins code
    in general. In turn, make chrome/common/metrics.mojom PPAPI-only.
    
    Also clean up some Ash/LaCros buildflags along the way.
    
    Change-Id: Ic08268dace6cb07828315dedea08b0ad08a8eb30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6222529
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415069}

--- chrome/browser/metrics/chrome_metrics_service_accessor.cc
+++ chrome/browser/metrics/chrome_metrics_service_accessor.cc
@@ -7,19 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string_view>
 
-#include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/common/buildflags.h"
 #include "chrome/common/pref_names.h"
 #include "components/metrics/metrics_service.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_thread.h"
-#include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "ppapi/buildflags/buildflags.h"
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "ash/constants/ash_features.h"
-#include "chrome/browser/metrics/per_user_state_manager_chromeos.h"
+#if BUILDFLAG(ENABLE_PPAPI)
+#include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #endif
 
 namespace {
@@ -81,7 +77,7 @@ void ChromeMetricsServiceAccessor::SetForceIsMetricsReportingEnabledPrefLookup(
       value);
 }
 
-#if BUILDFLAG(ENABLE_PLUGINS)
+#if BUILDFLAG(ENABLE_PPAPI)
 // static
 void ChromeMetricsServiceAccessor::BindMetricsServiceReceiver(
     mojo::PendingReceiver<chrome::mojom::MetricsService> receiver) {
@@ -95,4 +91,4 @@ void ChromeMetricsServiceAccessor::BindMetricsServiceReceiver(
   };
   mojo::MakeSelfOwnedReceiver(std::make_unique<Thunk>(), std::move(receiver));
 }
-#endif  // BUILDFLAG(ENABLE_PLUGINS)
+#endif  // BUILDFLAG(ENABLE_PPAPI)
