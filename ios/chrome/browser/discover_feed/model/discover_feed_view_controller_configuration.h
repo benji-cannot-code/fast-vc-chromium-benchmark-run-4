f2ef70e2f3c812cb33a8fbb4682d82fa095c6583
Ginny Huang <ginnyhuang@chromium.org>
2025-02-03 08:51:11 -0800

    Revert "[iOS][follow-entry] Add feed control delegate as a VC config property"
    This reverts commit 188bdab5627090d986982351840c1451a67ad336.
    
    Reason for revert: Project cancelled
    
    Original change's description:
    > [iOS][follow-entry] Add feed control delegate as a VC config property
    >
    > This enables the discover action delegate to handle selected feed
    > change, therefore open the following feed overlay when the user taps
    > the entry points.
    >
    > Bug: 359325090
    > Change-Id: I2cae06753994eb927cbe08a5447400f792708c43
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102296
    > Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    > Commit-Queue: Adam Arcaro <adamta@google.com>
    > Reviewed-by: Adam Arcaro <adamta@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1397981}
    
    Bug: 359325090
    Change-Id: I54a581a2c1364121c4249a3f35e3fac8745a7341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217197
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414965}

--- ios/chrome/browser/discover_feed/model/discover_feed_view_controller_configuration.h
+++ ios/chrome/browser/discover_feed/model/discover_feed_view_controller_configuration.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 @protocol DiscoverFeedManageDelegate;
 @protocol DiscoverFeedPreviewDelegate;
-@protocol FeedControlDelegate;
 @protocol FeedSignInPromoDelegate;
 @protocol UIScrollViewDelegate;
 
@@ -33,9 +32,6 @@ class Browser;
 
 // FeedSignInPromoDelegate used by Discover Feed ViewController.
 @property(nonatomic, weak) id<FeedSignInPromoDelegate> signInPromoDelegate;
-
-// FeedControlDelegate used by Discover Feed ViewController.
-@property(nonatomic, weak) id<FeedControlDelegate> controlDelegate;
 
 @end
 
