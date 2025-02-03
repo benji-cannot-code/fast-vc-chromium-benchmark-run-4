79883ad200d1780845db203d1a15cb985ce4902c
Sirisha Kavuluru <skavuluru@google.com>
2025-02-03 12:10:54 -0800

    [TabStrip][Peripherals] Fix NPE when hovering with mouse with MSB not initialized
    Bug: 40933186
    Change-Id: Ie05c711e2dd525f4f5fc889c99529dd04343b04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226362
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1415112}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
@@ -544,7 +544,9 @@ public class StripLayoutHelperManager
                     mTooltipManager = new TooltipManager((FrameLayout) view, () -> mTopPadding);
                     mNormalHelper.getNewTabButton().setTooltipManager(mTooltipManager);
                     mIncognitoHelper.getNewTabButton().setTooltipManager(mTooltipManager);
-                    mModelSelectorButton.setTooltipManager(mTooltipManager);
+                    if (mModelSelectorButton != null) {
+                        mModelSelectorButton.setTooltipManager(mTooltipManager);
+                    }
                 });
 
         if (tabModelStartupInfoSupplier != null) {
@@ -664,6 +666,9 @@ public class StripLayoutHelperManager
         mModelSelectorButton.setAccessibilityDescription(
                 context.getString(R.string.accessibility_tabstrip_btn_incognito_toggle_standard),
                 context.getString(R.string.accessibility_tabstrip_btn_incognito_toggle_incognito));
+        if (mTooltipManager != null) {
+            mModelSelectorButton.setTooltipManager(mTooltipManager);
+        }
     }
 
     /** Cleans up internal state. */
