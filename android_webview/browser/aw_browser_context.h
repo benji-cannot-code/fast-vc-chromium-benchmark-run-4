3b4e3b94f903b1977c17044b91a7b445b0a6f823
elabadysayed <elabadysayed@chromium.org>
2025-02-03 04:38:10 -0800

    [AW][1/N] Add PrefetchManager for WebView
    This is the first part of a series of CLs to add handle for TTL and
    maxPrefetch in WebView.
    
    This CL adds an empty PrefetchManager that delegates the Prefetch method
    call from AwBrowserContext to BrowserContext. It doesn't change the
    logic nor adds any extra logic, the upcoming CLs will do so.
    
    Bug: 376014072
    Change-Id: Ib299d92848d78df8c3a1d91591f365384279a1d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181899
    Reviewed-by: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414846}

--- android_webview/browser/aw_browser_context.h
+++ android_webview/browser/aw_browser_context.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/aw_ssl_host_state_delegate.h"
 #include "android_webview/browser/file_system_access/aw_file_system_access_permission_context.h"
 #include "android_webview/browser/network_service/aw_proxy_config_monitor.h"
+#include "android_webview/browser/prefetch/aw_prefetch_manager.h"
 #include "base/android/jni_weak_ref.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/compiler_specific.h"
@@ -245,6 +246,8 @@ class AwBrowserContext : public content::BrowserContext,
   //
   // In generally, use GetCookieManager() rather than using this directly.
   std::unique_ptr<CookieManager> cookie_manager_;
+
+  std::unique_ptr<AwPrefetchManager> prefetch_manager_;
 
   // The IO thread client that should be used by service workers.
   base::android::ScopedJavaGlobalRef<jobject> sw_io_thread_client_;
---
+++ android_webview/browser/prefetch/aw_prefetch_manager.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/prefetch/aw_prefetch_manager.h"
+
+#include "content/public/browser/browser_context.h"
+
+namespace android_webview {
+
+AwPrefetchManager::AwPrefetchManager(content::BrowserContext* browser_context)
+    : browser_context_(*browser_context) {}
+
+AwPrefetchManager::~AwPrefetchManager() = default;
+
+void AwPrefetchManager::StartBrowserPrefetchRequest(
+    const GURL& url,
+    bool javascript_enabled,
+    std::optional<net::HttpNoVarySearchData> no_vary_search_hint,
+    const net::HttpRequestHeaders& additional_headers,
+    std::unique_ptr<content::PrefetchRequestStatusListener>
+        request_status_listener) {
+  // TODO(elabadysayed): Handle TTL and maxPrefetches.
+
+  browser_context_->StartBrowserPrefetchRequest(
+      url, javascript_enabled, std::move(no_vary_search_hint),
+      additional_headers, std::move(request_status_listener));
+}
+
+}  // namespace android_webview
