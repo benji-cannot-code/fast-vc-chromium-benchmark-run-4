0a15b65d3bb9e882ff14e30f3b9c427f708687a7
Rune Lillesveen <futhark@chromium.org>
2025-02-03 10:50:46 -0800

    Inert attribute should not cause html inertness
    Per spec the inert attribute should set inertness through a UA style
    rule: [inert] { interactivity:inert }. That means inertness can be
    escaped in the subtree by setting interactivity:auto.
    
    Since inertness can be escaped, and inert UA style can be mixed with
    interactivity set through author styling, the kAXInertElement and
    kAXInertSubtree difference no longer makes sense. This CL introduces a
    new kAXInertStyle, but currently maps to "inertElement" for devtools
    since devtools currently does not know about "inertStyle".
    
    This CL no longer considers inertness inside display:none or display
    locked subtrees for which ComputedStyle is null, because the existing
    code that looks up InertRoot(), ignores any styles that would have
    modified the effect of the inert attribute if styles were computed for
    those elements.
    
    Change-Id: Ibee1d47b8a504025b6afb2f2d753e72ba0f6b444
    Bug: 370065759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217059
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415056}

--- third_party/blink/renderer/modules/accessibility/ax_enums.h
+++ third_party/blink/renderer/modules/accessibility/ax_enums.h
@@ -113,6 +113,7 @@ enum AXIgnoredReason {
   kAXEmptyText,
   kAXInertElement,
   kAXInertSubtree,
+  kAXInertStyle,  // Node is made inert by interactivity:inert
   kAXLabelContainer,
   kAXLabelFor,
   kAXNotRendered,
