38e2a525141bbf3d0e66cf17dba5958f042ffed0
Jennifer Serrano <jennserrano@google.com>
2025-02-03 15:00:48 -0800

    [ntp][enterpise] Finalize Microsoft module strings
    Bug: 372724129
    Change-Id: Id8a6456e8360ce67097f7b9a3b7f1a061df1ad2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227224
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1415228}

--- chrome/browser/resources/new_tab_page/modules/v2/file_suggestion/microsoft_files_module.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/file_suggestion/microsoft_files_module.ts
@@ -74,10 +74,7 @@ export class MicrosoftFilesModuleElement extends
         {
           action: 'disable',
           icon: 'modules:block',
-          // TODO(crbug.com/372724129): Rename `modulesDriveDisableButtonTextV2`
-          // to accommodate both sharepoint and drive modules, or replace it
-          // with a sharepoint-specific string.
-          text: this.i18n('modulesDriveDisableButtonTextV2'),
+          text: this.i18n('modulesMicrosoftFilesDisableButtonText'),
         },
         {
           action: 'signout',
@@ -106,7 +103,7 @@ export class MicrosoftFilesModuleElement extends
       detail: {
         message: loadTimeData.getStringF(
             'disableModuleToastMessage',
-            loadTimeData.getString('modulesSharepointName')),
+            loadTimeData.getString('modulesMicrosoftFilesName')),
       },
     });
     this.dispatchEvent(disableEvent);
@@ -119,7 +116,9 @@ export class MicrosoftFilesModuleElement extends
       bubbles: true,
       composed: true,
       detail: {
-        message: this.i18n('modulesOutlookCalendarDismissToastMessage'),
+        message: loadTimeData.getStringF(
+            'dismissModuleToastMessage',
+            loadTimeData.getString('modulesFilesSentence')),
         restoreCallback: () => this.handler_.restoreModule(),
       },
     }));
--- chrome/test/data/webui/new_tab_page/modules/v2/file_suggestion/microsoft_files_module_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/v2/file_suggestion/microsoft_files_module_test.ts
@@ -19,10 +19,11 @@ import {installMock} from '../../../test_support.js';
 suite('MicrosoftFilesModule', () => {
   let childDocument: TestMock<MicrosoftAuthUntrustedDocumentRemote>;
   let handler: TestMock<MicrosoftFilesPageHandlerRemote>;
-  const modulesSharepointName = 'SharePoint';
+  const modulesMicrosoftFilesName = 'SharePoint and OneDrive files';
 
   setup(async () => {
-    loadTimeData.overrideValues({modulesSharepointName: modulesSharepointName});
+    loadTimeData.overrideValues(
+        {modulesMicrosoftFilesName: modulesMicrosoftFilesName});
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     handler = installMock(
         MicrosoftFilesPageHandlerRemote,
@@ -90,7 +91,7 @@ suite('MicrosoftFilesModule', () => {
     // Assert.
     const event: DisableModuleEvent = await whenFired;
     assertEquals(
-        ('You won\'t see ' + modulesSharepointName + ' on this page again'),
+        ('You won\'t see ' + modulesMicrosoftFilesName + ' on this page again'),
         event.detail.message);
   });
 
@@ -129,7 +130,7 @@ suite('MicrosoftFilesModule', () => {
     assertTrue(isVisible(microsoftFilesModule.$.moduleHeaderElementV2));
     assertEquals(
         microsoftFilesModule.$.moduleHeaderElementV2.headerText,
-        modulesSharepointName);
+        modulesMicrosoftFilesName);
   });
 
   test('module not created when there are no files', async () => {
@@ -161,8 +162,7 @@ suite('MicrosoftFilesModule', () => {
     dismissButton.click();
 
     const event: DismissModuleInstanceEvent = await whenFired;
-    // TODO(crbug.com/372724129): Update dismiss message.
-    assertEquals('Outlook Calendar hidden', event.detail.message);
+    assertEquals('Files hidden', event.detail.message);
     assertTrue(!!event.detail.restoreCallback);
     assertEquals(1, handler.getCallCount('dismissModule'));
 
