b9b5d296361012a8fbc3827fc3ecd4b354552d2d
Garima Chadha <garimachadha@microsoft.com>
2025-02-03 01:22:30 -0800

    IDB WPTs: Extend idbobjectstore related tests to workers
    The update modifies idbobjectstore related WPTs to run not
    only in window environments but also on dedicated, service, and shared
    workers.
    
    Change-Id: Icc13423f5b76f5a79bc60f7f496312d465f90577
    Bug: 41455766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197783
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Garima Chadha <garimachadha@microsoft.com>
    Auto-Submit: Garima Chadha <garimachadha@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1414801}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbobjectstore-getAll-enforcerange.any.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: title=IndexedDB: IDBObjectStore getAll() uses [EnforceRange]
+// META: script=resources/support.js
+
+// Spec: https://w3c.github.io/IndexedDB/#object-store-interface
+
+'use strict';
+
+indexeddb_test(
+    (t, db) => {
+      const store = db.createObjectStore('store');
+    },
+    (t, db) => {
+      const tx = db.transaction('store', 'readonly');
+      const store = tx.objectStore('store');
+      [NaN, Infinity, -Infinity, -1, -Number.MAX_SAFE_INTEGER].forEach(
+          count => {
+            assert_throws_js(TypeError, () => {
+              store.getAll(null, count);
+            }, `getAll with count ${count} count should throw TypeError`);
+          });
+      t.done();
+    },
+    `IDBObjectStore.getAll() uses [EnforceRange]`);
