f72889d9db25537adaef02504e4adca227d4c874
Will Harris <wfh@chromium.org>
2025-02-04 14:41:53 -0800

    Reland "Add ability for OSCrypt Async decrypt to signal temporary failure"
    This is a reland of commit 6de49a0d1a20f7ff3b9fcb415aa85a3f55da7084
    
    Two issues are resolved in this reland, which can be inspected
    by comparing PS1 with PS6:
    
    1. Previously if data were encrypted with the header of a Key
    Provider that signalled it was temporarily unavailable, then
    Decrypt would return failure immediately, rather than attempting
    to ask OSCrypt Sync to decrypt the data. This meant that if there
    were cases of OSCrypt Sync compatible Key Providers returning
    temporary failure, the fallback to OSCrypt Sync would not
    correctly happen. This never happened in production since the only
    OSCrypt Sync compatible key provider was the DPAPI key provider
    on Windows, which was always consistent with the Sync code (so they
    would both succeed or both fail).
    
    2. Some platforms return `false` for IsEncryptionAvailable from
    OSCrypt Sync, despite encryption (and decryption) being available
    e.g. Linux can return `false` for IsEncryptionAvailable if only
    the v10 (fixed key basic store) decryption is available. This meant
    that if running with --password-store=basic or an unsupported
    Window Manager, OSCrypt Sync would never be called for decrypts
    and they would fail.
    
    This re-land fixes both of these issues.
    
    The first one is tested by a CL landed previously in
    crrev.com/c/6221797, since this is a behavior that worked correctly
    before this change and regressed.
    
    The second issue has been tested manually and by the
    `linux-ip-protection` bot, which runs with basic (v10) password store.
    
    Original change's description:
    > Add ability for OSCrypt Async decrypt to signal temporary failure
    >
    > This CL adds a new public API on the Encryptor's decrypt operation to
    > allow callers to determine whether a failure is permanent or temporary.
    >
    > This is returned via the `temporarily_unavailable` flag to Decrypt.
    >
    > A temporary failure might be something such as keychain locked (on
    > macOS) or a user booting into safe mode (Windows) but is platform
    > defined and controlled by the key provider when returning a key, or by
    > OSCrypt Sync's IsEncryptionAvailable when there are no supported key
    > providers.
    >
    > The Key Provider interface is extended to replace the std::optional with
    > a base::expected that allows indicating whether the failure to provide
    > is a key is transient or not i.e. is the key permanently lost, or just
    > temporarily unavailable.
    >
    > Encryptor::Decrypt operations are then augmented to take this into
    > account when returning a failure.
    >
    > Existing key providers are updated to return the new type. On Windows,
    > DPAPI and App-Bound return permanent failures if they never generated
    > key, otherwise they return temporary failures.
    >
    > In practice, since DPAPI key provider re-generates a key (and discards
    > the current one) upon a DPAPI failure, failures will always end up being
    > permanent. However App-Bound failures will be more indicative of real
    > failure state.
    >
    > Tests are added to OSCrypt Async to verify the new functionality and a
    > new test is added in App-Bound browsertest to simulate a temporarily
    > non-functional service correctly returning this as a temporary failure.
    >
    > BUG=389016528
    >
    > Change-Id: Id8e34cbf0343cbb37008243985ac555e5041ca81
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608336
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1412642}
    
    Bug: 389016528
    Change-Id: Id6cd0db7e3418ca728b8fed92046ee015c63291c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6221251
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415844}

--- components/os_crypt/async/browser/os_crypt_async.h
+++ components/os_crypt/async/browser/os_crypt_async.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
+#include "base/types/expected.h"
 #include "components/os_crypt/async/browser/key_provider.h"
 #include "components/os_crypt/async/common/encryptor.h"
 
@@ -77,7 +78,7 @@ class COMPONENT_EXPORT(OS_CRYPT_ASYNC) OSCryptAsync {
   void CallbackHelper(InitCallback callback, Encryptor::Option option) const;
   void HandleKey(ProviderIterator current,
                  const std::string& tag,
-                 std::optional<Encryptor::Key> key);
+                 base::expected<Encryptor::Key, KeyProvider::KeyError> key);
 
   std::unique_ptr<Encryptor> encryptor_instance_
       GUARDED_BY_CONTEXT(sequence_checker_);
