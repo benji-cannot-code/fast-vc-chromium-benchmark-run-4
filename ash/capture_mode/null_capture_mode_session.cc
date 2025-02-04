ec2bb88b42486c2454e7a20020ed0c1cb326eb56
Michelle Chen <michellegc@google.com>
2025-02-03 18:42:25 -0800

    scanner: Show error in action container if response contains error.
    If an error occurred while trying to fetch Scanner actions, show the
    error message in the action container.
    
    Bug: b:378582420
    Change-Id: Idef24afde70ad2d6962d03fe4f504419df98cf42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6228435
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415340}

--- ash/capture_mode/null_capture_mode_session.cc
+++ ash/capture_mode/null_capture_mode_session.cc
@@ -137,7 +137,7 @@ ActionButtonView* NullCaptureModeSession::AddActionButton(
 void NullCaptureModeSession::AddSmartActionsButton() {}
 
 void NullCaptureModeSession::OnScannerActionsFetched(
-    std::vector<ScannerActionViewModel> scanner_actions) {}
+    ScannerSession::FetchActionsResponse actions_response) {}
 
 void NullCaptureModeSession::InitInternal() {
   layer()->SetName("NullCaptureModeSession");
