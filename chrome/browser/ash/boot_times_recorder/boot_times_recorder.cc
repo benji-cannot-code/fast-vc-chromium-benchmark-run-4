cedde52336418876c974320b2a05fe76134c1d14
Eriko Kurimoto <elkurin@google.com>
2025-02-03 23:11:35 -0800

    boot_times_recorder: Pass local_state OnChromeProcessStart
    This CL lets BootTimeRecorder::OnChromeProcessStart obtain local_state
    as args so that we can remove g_browser_process access.
    
    This is a part of ash refactoring.
    
    Bug: b:369689752 , b:393222697
    Change-Id: Ib9884962434aaa81c8ddd9302ce6cabecab8bec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6229167
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415392}

--- chrome/browser/ash/boot_times_recorder/boot_times_recorder.cc
+++ chrome/browser/ash/boot_times_recorder/boot_times_recorder.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/time/time.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/pref_names.h"
@@ -54,6 +53,12 @@ const std::string GetTabUrl(RenderWidgetHost* rwh) {
     }
   }
   return std::string();
+}
+
+// Clear saved logout-started metric in Local State. This method is called when
+// logout-state was written to file.
+void ClearLogoutStartedLastPreference(PrefService& local_state) {
+  local_state.ClearPref(prefs::kLogoutStartedLast);
 }
 
 }  // namespace
@@ -129,16 +134,9 @@ void BootTimesRecorder::AddLoginTimeMarkerWithURL(const char* marker_name,
   LoginEventRecorder::Get()->AddLoginTimeMarkerWithURL(marker_name, url, false);
 }
 
-// static
-void BootTimesRecorder::ClearLogoutStartedLastPreference() {
-  PrefService* local_state = g_browser_process->local_state();
-  local_state->ClearPref(prefs::kLogoutStartedLast);
-}
-
-void BootTimesRecorder::OnChromeProcessStart() {
-  PrefService* local_state = g_browser_process->local_state();
+void BootTimesRecorder::OnChromeProcessStart(PrefService& local_state) {
   const std::string logout_started_last_str =
-      local_state->GetString(prefs::kLogoutStartedLast);
+      local_state.GetString(prefs::kLogoutStartedLast);
   if (logout_started_last_str.empty())
     return;
 
@@ -159,7 +157,7 @@ void BootTimesRecorder::OnChromeProcessStart() {
 
   if (logout_started_last >= uptime) {
     // Reboot happened.
-    ClearLogoutStartedLastPreference();
+    ClearLogoutStartedLastPreference(local_state);
     return;
   }
 
@@ -167,7 +165,7 @@ void BootTimesRecorder::OnChromeProcessStart() {
   constexpr char kLogoutStarted[] = "logout-started";
   logout_started_last_stats.RecordStatsWithCallback(
       kLogoutStarted, /*write_flag_file=*/true,
-      base::BindOnce(&BootTimesRecorder::ClearLogoutStartedLastPreference));
+      base::BindOnce(&ClearLogoutStartedLastPreference, std::ref(local_state)));
 }
 
 void BootTimesRecorder::OnLogoutStarted(PrefService* state) {
