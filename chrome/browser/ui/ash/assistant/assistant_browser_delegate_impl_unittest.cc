32b0676d738582ffb4727b331d2eb69a75f01736
Hidehiko Abe <hidehiko@chromium.org>
2025-02-03 17:01:38 -0800

    Destroy AshTestHelper in AshTestBase::TearDown
    The instance is created in AshTestHelper::SetUp(), so destroy it in
    AshTestBase::TearDown.
    This provides a way to run some code after AshTestHelper's destruction
    in inheriting fixtures in latter CLs.
    
    There's two edge cases we cannot handle easily.
    Screen() instance is updated in ash::Shell()'s tear down.
    
    (Testing)ProfileManager needs to be destroyed "during" TearDown
    to follow the production behavior (specifically, after destroying
    ash::Shell(), but before SessionManager/UserManager).
    As a workaround, this CL adds a callback to AshTestBase invoked just
    before destroying AshTestHelper.
    
    BUG=278643115
    TEST=Tryjob
    
    Change-Id: I19fa00ee46fb9b7a494c08af084156b2df0a3dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6224054
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415300}

--- chrome/browser/ui/ash/assistant/assistant_browser_delegate_impl_unittest.cc
+++ chrome/browser/ui/ash/assistant/assistant_browser_delegate_impl_unittest.cc
@@ -77,6 +77,11 @@ class AssistantBrowserDelegateImplTest : public ChromeAshTestBase {
             std::make_unique<web_app::FakeWebAppDatabaseFactory>());
   }
 
+  void TearDown() override {
+    delegate_.reset();
+    ChromeAshTestBase::TearDown();
+  }
+
  protected:
   Profile* profile() const { return profile_.get(); }
 
