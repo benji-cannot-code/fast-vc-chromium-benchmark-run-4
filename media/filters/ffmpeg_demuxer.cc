138a56dcde03e7b1bb2e2ba8b914b842a10c8ce1
Eugene Zemtsov <eugene@chromium.org>
2025-02-04 15:17:44 -0800

    media: Report a demuxer error when bitstream conversion fails
    Also this CL treats zero-sized conversion output as a failure.
    Previously zero sized decoder buffers caused crashes in
    accelerated decoders.
    
    Bug: 393652851
    Change-Id: I01966c6164e4b6e0a0fcfd319f19da7331d6faa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6228769
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415862}

--- media/filters/ffmpeg_demuxer.cc
+++ media/filters/ffmpeg_demuxer.cc
@@ -411,7 +411,8 @@ void FFmpegDemuxerStream::EnqueuePacket(ScopedAVPacket packet) {
   // Convert the packet if there is a bitstream filter.
   if (bitstream_converter_ &&
       !bitstream_converter_->ConvertPacket(packet.get())) {
-    DVLOG(1) << "Format conversion failed.";
+    demuxer_->NotifyDemuxerError(DEMUXER_ERROR_BITSTREAM_CONVERSION_FAILED);
+    return;
   }
 #endif
 
