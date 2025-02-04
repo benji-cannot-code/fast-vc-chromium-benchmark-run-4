472d45844b8ac62414a0e6d5a6014efa04e8d2bc
Qingxin Wu <qingxinwu@google.com>
2025-02-04 06:49:53 -0800

    Prepare allowing real time reports not noised based on settings.
    Currently it keeps the same noise with the same epsilon. Also change the
    report version to 2, and add a new field 'flipProbability' to the
    report.
    
    Bug: 392148872
    
    Change-Id: I661127eb4bd39d111faf8ed1e81d931bdfc9644b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6202741
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1415547}

--- content/browser/interest_group/interest_group_real_time_report_util.h
+++ content/browser/interest_group/interest_group_real_time_report_util.h
@@ -17,13 +17,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// Calculate flip probability that'll be used in Rappor(). `epsilon` is a
+// measure of the privacy loss (higher is less private).
+CONTENT_EXPORT double CalculateFlipProbability(double epsilon);
+
 // Basic RAPPOR noises each coordinate of the bit vector (of size `num_buckets`)
-// independently, and it is parameterized by epsilon, a measure of privacy loss.
+// independently. `flip_probability` is calculated from an epsilon which is
+// a measure of privacy loss. When `flip_probability` is 0, no noise is added.
 // See the explainer for more details:
 // https://github.com/WICG/turtledove/blob/main/PA_real_time_monitoring.md#histogram-contributions-and-the-rappor-noise-algorithm
 // CONTENT_EXPORT for testing.
 CONTENT_EXPORT std::vector<uint8_t> Rappor(std::optional<int32_t> maybe_bucket,
-                                           double epsilon,
+                                           double flip_probability,
                                            int num_buckets);
 
 // Randomly select one contribution from `contributions` and return its index if
@@ -41,7 +46,8 @@ CalculateRealTimeReportingHistograms(
     std::map<
         url::Origin,
         std::vector<auction_worklet::mojom::RealTimeReportingContributionPtr>>
-        contributions);
+        contributions,
+    double flip_probability);
 
 // Get the destination of sending real time report.
 CONTENT_EXPORT GURL GetRealTimeReportDestination(const url::Origin& origin);
