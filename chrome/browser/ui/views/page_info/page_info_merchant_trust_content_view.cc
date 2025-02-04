97371e8dacfb4b42a12690c34345cd5b9c178659
Olesia Marukhno <olesiamarukhno@google.com>
2025-02-04 07:03:39 -0800

    [merchant trust] Adjust footer padding
    Adjust the footer button padding to match the mock closer.
    
    Bug: 394275285
    Change-Id: I4db98e8c8a0c4b31c1267f13d9142d41bac3b519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6225219
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415555}

--- chrome/browser/ui/views/page_info/page_info_merchant_trust_content_view.cc
+++ chrome/browser/ui/views/page_info/page_info_merchant_trust_content_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/label_button.h"
@@ -185,6 +186,9 @@ PageInfoMerchantTrustContentView::CreateHatsButton() {
       views::CreateThemedSolidBackground(ui::kColorSysNeutralContainer));
   hats_button->SetProperty(views::kElementIdentifierKey, kHatsButtonId);
   hats_button->SetVisible(false);
+  hats_button->SetBorder(
+      views::CreateEmptyBorder(ChromeLayoutProvider::Get()->GetInsetsMetric(
+          INSETS_PAGE_INFO_FOOTER_BUTTON)));
   return hats_button;
 }
 
