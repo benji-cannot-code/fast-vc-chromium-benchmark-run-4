e2a6f492e1ffe431c033891bd44025f961877e61
Samuel Huang <huangs@chromium.org>
2025-02-04 08:45:43 -0800

    Revert "Remove a trailing zero byte that was never used for anything."
    This reverts commit 4d8021584a95f10c2072074f28e6d224989c8eab.
    
    Reason for revert: Suspecting of causing AddressSanitizer: stack-buffer-overflow.
    
    Original change's description:
    > Remove a trailing zero byte that was never used for anything.
    >
    > Change-Id: I98d2e5d8344c4fe7b14d827ae47ab771f25447c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6225749
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1415544}
    
    Change-Id: I68153bf0a1adf1fde3af343ac50bddb99280f451
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6229712
    Auto-Submit: Samuel Huang <huangs@chromium.org>
    Owners-Override: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1415597}

--- third_party/blink/renderer/core/css/parser/css_property_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_property_parser.cc
@@ -263,6 +263,7 @@ static inline bool QuasiLowercaseIntoBuffer(const UChar* src,
     }
     dst[i] = ToASCIILower(c);
   }
+  dst[length] = '\0';
   return true;
 }
 
@@ -340,7 +341,7 @@ static CSSPropertyID UnresolvedCSSPropertyID(
     return CSSPropertyID::kInvalid;
   }
 
-  char buffer[kMaxCSSPropertyNameLength];
+  char buffer[kMaxCSSPropertyNameLength + 1];  // 1 for null character
   if (!QuasiLowercaseIntoBuffer(property_name, length, buffer)) {
     return CSSPropertyID::kInvalid;
   }
@@ -386,7 +387,7 @@ CSSPropertyID UnresolvedCSSPropertyID(const ExecutionContext* execution_context,
 template <typename CharacterType>
 static CSSValueID CssValueKeywordID(const CharacterType* value_keyword,
                                     unsigned length) {
-  char buffer[kMaxCSSValueKeywordLength];
+  char buffer[kMaxCSSValueKeywordLength + 1];  // 1 for null character
   if (!QuasiLowercaseIntoBuffer(value_keyword, length, buffer)) {
     return CSSValueID::kInvalid;
   }
