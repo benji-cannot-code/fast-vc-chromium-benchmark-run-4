a04442760b852f17511134271845105019bccfb7
Arthur Milchior <arthurmilchior@chromium.org>
2025-02-04 04:02:33 -0800

    [iOS]Introduce FeedSignInPromoSource
    Originally, there was a need for different code for the feed depending
    on whether we need to only sign-in the user or also enable sync. Now
    that sync is disabled, the name has no more meaning.
    
    There remains only two differences. The histograms logged are not the
    same. And, if there are no identities on the device, the bottom feed
    sign the user in as soon as the account is added instead of asking for a
    confirmation the user intended to sign-in with the added account.
    
    As far as I can tell by using the app, the only reason for this second
    difference is that, if there are no account on the device, the user is
    not invited to sign-in from the context menu, meaning we could not even
    consider using the instant sign-in flow.
    
    This is part of the chain
    + cl/722808388
    > crrev.com/c/6226065
    + crrev.com/i/8002548
    + crrev.com/c/6227561
    + cl/722808735
    
    Bug: 394148687
    Change-Id: Ia0901675dd7a8e6f242747a697683268ce56a2c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226065
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415489}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -1154,42 +1154,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - FeedSignInPromoDelegate
 
 - (void)showSignInPromoUI {
-  if (![self isSignInAllowed]) {
-    [self showSignInDisableMessage];
-    [self.feedMetricsRecorder recordShowSignInRelatedUIWithType:
-                                  feed::FeedSignInUI::kShowSignInDisableToast];
-    return;
-  }
-  if (_showAccountMenuInProgress || _showSigninCommandInProgress) {
-    return;
-  }
-
-  BOOL hasUserIdentities = [self hasIdentitiesOnDevice];
-  id<ApplicationCommands> handler = HandlerForProtocol(
-      self.browser->GetCommandDispatcher(), ApplicationCommands);
-  __weak __typeof(self) weakSelf = self;
-  _showSigninCommandInProgress = YES;
-  ShowSigninCommand* command = [[ShowSigninCommand alloc]
-      initWithOperation:AuthenticationOperation::kSigninOnly
-               identity:nil
-            accessPoint:signin_metrics::AccessPoint::kNtpFeedCardMenuPromo
-            promoAction:signin_metrics::PromoAction::
-                            PROMO_ACTION_NO_SIGNIN_PROMO
-             completion:^(SigninCoordinatorResult result,
-                          id<SystemIdentity> completionIdentity) {
-               [weakSelf showSigninCommandDidFinish];
-             }];
-  [handler showSignin:command baseViewController:self.NTPViewController];
-  [self.feedMetricsRecorder recordShowSignInRelatedUIWithType:
-                                feed::FeedSignInUI::kShowSignInOnlyFlow];
-  [self.feedMetricsRecorder recordShowSignInOnlyUIWithUserId:hasUserIdentities];
-  signin_metrics::RecordSigninUserActionForAccessPoint(
-      signin_metrics::AccessPoint::kNtpFeedCardMenuPromo);
+  [self showSignInUIFromSource:FeedSignInCommandSourceCardMenu];
 }
 
 - (void)showSignInUI {
-  // Both possible flows (sign-in only and sign-in + sync) involve sign-in. So
-  // they shouldn't be offered if sign-in is disallowed.
+  [self showSignInUIFromSource:FeedSignInCommandSourceBottom];
+}
+
+- (void)showSignInUIFromSource:(FeedSignInPromoSource)source {
+  // This flow shouldn't be offered if sign-in is disallowed.
+  // In theory, the flow should not even have been offered to the user.
   if (![self isSignInAllowed]) {
     [self showSignInDisableMessage];
     [self.feedMetricsRecorder recordShowSyncnRelatedUIWithType:
@@ -1199,19 +1173,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (_showAccountMenuInProgress || _showSigninCommandInProgress) {
     return;
   }
+  BOOL hasUserIdentities = [self hasIdentitiesOnDevice];
 
+  signin_metrics::AccessPoint accessPoint =
+      signin_metrics::AccessPoint::kNtpFeedCardMenuPromo;
   id<ApplicationCommands> handler = HandlerForProtocol(
       self.browser->GetCommandDispatcher(), ApplicationCommands);
   // If there are 0 identities, kInstantSignin requires less taps.
-  auto operation = [self hasIdentitiesOnDevice]
-                       ? AuthenticationOperation::kSigninOnly
-                       : AuthenticationOperation::kInstantSignin;
+  AuthenticationOperation operation =
+      (hasUserIdentities) ? AuthenticationOperation::kSigninOnly
+                          : AuthenticationOperation::kInstantSignin;
+  switch (source) {
+    case FeedSignInCommandSourceBottom:
+      // TODO(crbug.com/40066051): Strictly speaking this should record a bucket
+      // other than kShowSyncFlow. But I don't think we care too much about this
+      // particular histogram, just rename the bucket after launch.
+      [self.feedMetricsRecorder
+          recordShowSyncnRelatedUIWithType:feed::FeedSyncPromo::kShowSyncFlow];
+      break;
+    case FeedSignInCommandSourceCardMenu:
+      accessPoint = signin_metrics::AccessPoint::kNtpFeedBottomPromo;
+      [self.feedMetricsRecorder recordShowSignInRelatedUIWithType:
+                                    feed::FeedSignInUI::kShowSignInOnlyFlow];
+      [self.feedMetricsRecorder
+          recordShowSignInOnlyUIWithUserId:hasUserIdentities];
+      break;
+  }
   __weak __typeof(self) weakSelf = self;
   _showSigninCommandInProgress = YES;
   ShowSigninCommand* command = [[ShowSigninCommand alloc]
       initWithOperation:operation
                identity:nil
-            accessPoint:signin_metrics::AccessPoint::kNtpFeedBottomPromo
+            accessPoint:accessPoint
             promoAction:signin_metrics::PromoAction::
                             PROMO_ACTION_NO_SIGNIN_PROMO
              completion:^(SigninCoordinatorResult result,
@@ -1219,13 +1212,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                [weakSelf showSigninCommandDidFinish];
              }];
   [handler showSignin:command baseViewController:self.NTPViewController];
-  // TODO(crbug.com/40066051): Strictly speaking this should record a bucket
-  // other than kShowSyncFlow. But I don't think we care too much about this
-  // particular histogram, just rename the bucket after launch.
-  [self.feedMetricsRecorder
-      recordShowSyncnRelatedUIWithType:feed::FeedSyncPromo::kShowSyncFlow];
-  signin_metrics::RecordSigninUserActionForAccessPoint(
-      signin_metrics::AccessPoint::kNtpFeedBottomPromo);
+  signin_metrics::RecordSigninUserActionForAccessPoint(accessPoint);
 }
 
 #pragma mark - FeedWrapperViewControllerDelegate
