da2594dba6009d47d1fd283b61e7787b49a3f115
Anders Hartvoll Ruud <andruud@chromium.org>
2025-02-04 03:55:19 -0800

    [functions] Substitute var() via attr() locally
    In other words, when substituting an attribute like "var(--x)",
    the referenced "--x" must respect the local context: it can either
    refer to a local variable, and argument, or a custom property.
    
    https://drafts.csswg.org/css-mixins-1/#locally-substitute-a-var
    
    Bug: 325504770
    Change-Id: I68a1d4cecc173ebe17692087edaa1014eab27466
    Fixed: 391570175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190814
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415486}

--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -1367,7 +1367,8 @@ bool StyleCascade::ResolveTokensInto(CSSParserTokenStream& stream,
                RuntimeEnabledFeatures::CSSAdvancedAttrFunctionEnabled()) {
       CSSParserTokenStream::BlockGuard guard(stream);
       state_.StyleBuilder().SetHasAttrFunction();
-      success &= ResolveAttrInto(stream, resolver, context, out);
+      success &=
+          ResolveAttrInto(stream, resolver, context, function_context, out);
     } else if (token.FunctionId() ==
                CSSValueID::kInternalAutoBase) {
       CSSParserTokenStream::BlockGuard guard(stream);
@@ -1847,6 +1848,7 @@ bool StyleCascade::ResolveEnvInto(CSSParserTokenStream& stream,
 bool StyleCascade::ResolveAttrInto(CSSParserTokenStream& stream,
                                    CascadeResolver& resolver,
                                    const CSSParserContext& context,
+                                   FunctionContext* function_context,
                                    TokenSequence& out) {
   AtomicString local_name = ConsumeVariableName(stream);
   using Attribute = CascadeResolver::Attribute;
@@ -1870,10 +1872,9 @@ bool StyleCascade::ResolveAttrInto(CSSParserTokenStream& stream,
   if (!attribute_value.IsNull() && !attr_type->IsString()) {
     TokenSequence substituted_attribute_token_sequence;
     CSSParserTokenStream attribute_value_stream(attribute_value);
-    if (!ResolveTokensInto(attribute_value_stream, resolver, context,
-                           /* function_context */ nullptr,
-                           /* stop_type */ kEOFToken,
-                           substituted_attribute_token_sequence)) {
+    if (!ResolveTokensInto(
+            attribute_value_stream, resolver, context, function_context,
+            /* stop_type */ kEOFToken, substituted_attribute_token_sequence)) {
       return false;
     }
     substituted_attribute_value =
@@ -1889,8 +1890,7 @@ bool StyleCascade::ResolveAttrInto(CSSParserTokenStream& stream,
     stream.ConsumeWhitespace();
 
     TokenSequence fallback;
-    if (!ResolveTokensInto(stream, resolver, context,
-                           /* function_context */ nullptr,
+    if (!ResolveTokensInto(stream, resolver, context, function_context,
                            /* stop_type */ kEOFToken, fallback)) {
       return false;
     }
