7135d36f880519820b61b51a35c6fa9d0848a21e
Ritika Gupta <ritikagup@google.com>
2025-02-04 08:44:24 -0800

    [Data Sharing] Cleanup LearnMoreAboutSharedTabGroupsClicked method.
    Bug: b/386835161
    Change-Id: I5b5b90767830b156dfe60af972f59069a8876aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226762
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415596}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -1326,11 +1326,6 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
             }
 
             @Override
-            public void openLearnMoreSharedTabGroupsPage(Context context, GURL gurl) {
-                CustomTabActivity.showInfoPage(context, gurl.getSpec());
-            }
-
-            @Override
             public void openUrlInChromeCustomTab(Context context, GURL gurl) {
                 CustomTabActivity.showInfoPage(context, gurl.getSpec());
             }
