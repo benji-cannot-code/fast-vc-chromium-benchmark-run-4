090679e438089a5691d3707dfac3e8d43525b25b
Duong Dac <ddac@google.com>
2025-02-04 09:54:36 -0800

    [iOS] Fix race condition when the PACP bottom sheet auto-dismisses
    The parent access approval callback might fire after its owning
    coordinator is destroyed on bottom sheet dismissal.
    
    Bug: 384514294
    Change-Id: I651326f15b1f55d15286ec9fe20c77dd64d2f556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6225758
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1415640}

--- ios/chrome/browser/supervised_user/coordinator/parent_access_coordinator.mm
+++ ios/chrome/browser/supervised_user/coordinator/parent_access_coordinator.mm
@@ -92,10 +92,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (supervised_user::LocalApprovalResult)result {
   id<ParentAccessCommands> handler = HandlerForProtocol(
       self.browser->GetCommandDispatcher(), ParentAccessCommands);
-  [handler hideParentAccessBottomSheet];
   if (_callback) {
     std::move(_callback).Run(result);
   }
+
+  // Dismiss the parent access bottom sheet, which will also stop this
+  // coordinator.
+  [handler hideParentAccessBottomSheet];
 }
 
 #pragma mark - ParentAccessMediatorDelegate
