f0bcfba3f30d4dfe93e9a44dad03cbf1b22b1d37
Boris Sazonov <bsazonov@chromium.org>
2025-02-04 07:02:39 -0800

    [Android] Support TYPE_USE for IntDefs generated from C++ enums
    Allows using IntDefs generated from C++ enums in generic arguments. If
    @CppEnum is an enum generated via java_cpp_enum:
    
    Prior to this CL:
    Callback<@CppEnum Integer> callback; // Doesn't compile.
    
    After this CL:
    Callback<@CppEnum Integer> callback; // Allowed.
    
    Bug: None
    Change-Id: If386fea19f68351dfa9eeef86449c800e04157d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226410
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415554}

--- build/android/gyp/java_cpp_enum_tests.py
+++ build/android/gyp/java_cpp_enum_tests.py
@@ -45,12 +45,15 @@ package some.package;
 
 import androidx.annotation.IntDef;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @IntDef({
     ClassName.E1, ClassName.E2
 })
+@Target(ElementType.TYPE_USE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface ClassName {
   /**
@@ -108,6 +111,7 @@ public @interface ClassName {
       expected = """@IntDef(%s{
     EnumName.ZERO, EnumName.ONE
 })
+@Target(ElementType.TYPE_USE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface EnumName {
   int ZERO = 1 << 0;
