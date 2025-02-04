8f943a4e24c84cadde24b4a1412d834cfefef332
Maksim Sisov <msisov@igalia.com>
2025-02-03 23:30:40 -0800

    Introduce getJobStatus in printing API
    This CL is based on https://crrev.com/c/5079456 by nedol@google.com
    
    Introduce getJobStatus method in chrome.printing API. This is based on
    the non-persistent cache, which stores up-to 10k jobs, which are
    possible to get statuses of. The cache evicts 50 oldest jobs once the
    threshold is reached.
    
    Please see the comments and the "Implementation" section in the document
    to understand better why this number was chosen.
    
    API modification approved proposal: http://go/printing-get-job-status
    
    Test: Added new tests to PrintingAPIHandlerUnittest
    Bug: 41485740
    Change-Id: I0eeb574baa0a542a3e83ec914aa33cdbfedb474c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190458
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1415396}

---
+++ chrome/test/data/extensions/api_test/printing/get_print_job_status.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+    chrome.printing.getJobStatus('invalid_job_id', status => {
+      chrome.test.assertEq(null, status);
+    });
+
+    const url = 'http://localhost:' + config.testServer.port + '/pdf/test.pdf';
+    submitJob('id', 'test job', url, response => {
+      chrome.test.assertNe(undefined, response);
+      chrome.test.assertNe(undefined, response.status);
+      chrome.test.assertEq(chrome.printing.SubmitJobStatus.OK, response.status);
+      chrome.test.assertNe(undefined, response.jobId);
+
+      chrome.printing.getJobStatus('id', status => {
+        chrome.test.assertEq(chrome.printing.JobStatus.COMPLETED, status);
+
+        chrome.test.notifyPass();
+      });
+    });
+  });
