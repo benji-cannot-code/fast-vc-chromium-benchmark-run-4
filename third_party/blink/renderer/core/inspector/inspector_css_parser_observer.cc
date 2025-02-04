57546c93472e076cf56b173f79ae591ad51dc960
David Sanders <dsanders11@ucsbalum.com>
2025-02-04 14:28:06 -0800

    Remove unused include in style_rule_keyframe.h
    Bug: 40318405
    Change-Id: I37442b071c58ccc4284c043e3dd99a5615ecfbf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6229970
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415829}

--- third_party/blink/renderer/core/inspector/inspector_css_parser_observer.cc
+++ third_party/blink/renderer/core/inspector/inspector_css_parser_observer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/parser/css_parser.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/execution_context/security_context.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
 #include "third_party/blink/renderer/core/inspector/inspector_audits_issue.h"
 
