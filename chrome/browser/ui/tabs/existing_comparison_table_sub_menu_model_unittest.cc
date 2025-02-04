5a54b9fc2eb044d7ca7a01f1c2b85d8775a4517a
Chase Hartsell <chazzy@chromium.org>
2025-02-04 10:46:05 -0800

    [Compare] Enable confirmation toast by default
    Now that the toast framework is enabled by default, the confirmation
    toast does not need to be guarded behind a separate flag. The overall
    Compare feature is still guarded behind a flag, so these toasts won't
    appear unless Compare is enabled and triggers one.
    
    Bug: 393382378
    Change-Id: I578c2c7bcd203df46ff9ad01ffcad709b1bbbc88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219727
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415673}

--- chrome/browser/ui/tabs/existing_comparison_table_sub_menu_model_unittest.cc
+++ chrome/browser/ui/tabs/existing_comparison_table_sub_menu_model_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/existing_comparison_table_sub_menu_model.h"
 
 #include "chrome/test/base/browser_with_test_window_test.h"
+#include "components/commerce/core/commerce_feature_list.h"
 #include "components/commerce/core/commerce_utils.h"
 #include "components/commerce/core/mojom/product_specifications.mojom.h"
 #include "components/commerce/core/pref_names.h"
@@ -28,6 +29,8 @@ class ExistingComparisonTableSubMenuModelTest
     : public BrowserWithTestWindowTest {
  public:
   void SetUp() override {
+    test_features_.InitAndEnableFeature(commerce::kProductSpecifications);
+
     BrowserWithTestWindowTest::SetUp();
 
     product_specs_service =
@@ -39,6 +42,7 @@ class ExistingComparisonTableSubMenuModelTest
   TabStripModel* tab_strip() { return browser()->tab_strip_model(); }
 
  protected:
+  base::test::ScopedFeatureList test_features_;
   std::unique_ptr<MockProductSpecificationsService> product_specs_service;
 
   const std::vector<ProductSpecificationsSet> kProductSpecsSets = {
