94877918d2430b0db76c56ec0751036ee935202e
André Bianchessi <abianchessi@chromium.org>
2025-02-04 10:02:31 -0800

    Get WebContents inside Pwa Install callback
    This CL removes the WebContents from the callback that will be used to
    register the PWA Install ActionItem in browser_actions [1]. This is
    necessary because when we create the callback (in browser_actions) it
    can't be bound to the WebContents at that time, as they must refer to
    the WebContents of the current web page when the page action is
    executed.
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/browser_actions.cc;l=156;drc=4ab8f6b03230ecfe44eab58069bac992d0dca80e
    
    Bug: 376283433
    Change-Id: Ifae95687741572fd564b98b9f56051a334db3fc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219845
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: André Bianchessi <abianchessi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415646}

--- chrome/browser/ui/views/page_action/pwa_install.cc
+++ chrome/browser/ui/views/page_action/pwa_install.cc
@@ -19,11 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(crbug.com/376283433): Migrate the tests from
 // https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
-void ShowPwaInstallDialog(Browser* browser,
-                          content::WebContents* web_contents) {
+void ShowPwaInstallDialog(Browser* browser) {
   CHECK(browser);
 
   base::RecordAction(base::UserMetricsAction("PWAInstallIcon"));
+
+  content::WebContents* const web_contents =
+      browser->tab_strip_model()->GetActiveWebContents();
+  CHECK(web_contents);
 
   // Close PWA install IPH if it is showing.
   web_app::PwaInProductHelpState iph_state =
--- chrome/browser/ui/views/page_action/pwa_install.h
+++ chrome/browser/ui/views/page_action/pwa_install.h
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_PWA_INSTALL_H_
 
 #include "chrome/browser/ui/browser.h"
-#include "content/public/browser/web_contents.h"
 
-void ShowPwaInstallDialog(Browser* browser, content::WebContents* web_contents);
+void ShowPwaInstallDialog(Browser* browser);
 
 #endif  // CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_PWA_INSTALL_H_
