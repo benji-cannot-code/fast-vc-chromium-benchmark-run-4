b619c4342f9cfe45faab4620c4daee8e71018dc7
Saifuddin Hitawala <hitawala@chromium.org>
2025-02-04 10:00:16 -0800

    [mediastream] Use MappableSI in WebMediaPlayerMSTest
    Convert WebMediaPlayerMSTest to create a video frame through
    WrapMappableSharedImage rather than WrapExternalGpuMemoryBuffer.
    This makes the necessary changes to also create a client shared image
    through the test shared image interface.
    
    Bug: 393315130
    Change-Id: I1f3d7ad8ae76dcf28e1221259300595f7d15fb3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227461
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415645}

--- third_party/blink/renderer/modules/mediastream/web_media_player_ms_test.cc
+++ third_party/blink/renderer/modules/mediastream/web_media_player_ms_test.cc
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "cc/layers/layer.h"
+#include "gpu/command_buffer/client/client_shared_image.h"
+#include "gpu/command_buffer/client/test_shared_image_interface.h"
 #include "media/base/media_content_type.h"
 #include "media/base/media_util.h"
 #include "media/base/test_helpers.h"
@@ -538,6 +540,7 @@ class WebMediaPlayerMSTest
         surface_layer_bridge_(
             std::make_unique<NiceMock<MockSurfaceLayerBridge>>()),
         submitter_(std::make_unique<NiceMock<MockWebVideoFrameSubmitter>>()),
+        test_sii_(base::MakeRefCounted<gpu::TestSharedImageInterface>()),
         layer_set_(false),
         rendering_(false),
         background_rendering_(false) {
@@ -671,6 +674,7 @@ class WebMediaPlayerMSTest
       surface_layer_bridge_ptr_ = nullptr;
   raw_ptr<NiceMock<MockWebVideoFrameSubmitter>, DanglingUntriaged>
       submitter_ptr_ = nullptr;
+  scoped_refptr<gpu::TestSharedImageInterface> test_sii_;
   bool enable_surface_layer_for_video_ = false;
   base::TimeTicks deadline_min_;
   base::TimeTicks deadline_max_;
@@ -1751,11 +1755,20 @@ TEST_P(WebMediaPlayerMSTest, OnContextLost) {
   compositor_->OnContextLost();
   EXPECT_EQ(non_gpu_frame, compositor_->GetCurrentFrame());
 
-  std::unique_ptr<gfx::GpuMemoryBuffer> gmb =
-      std::make_unique<media::FakeGpuMemoryBuffer>(
-          frame_size, gfx::BufferFormat::YUV_420_BIPLANAR);
-  auto gpu_frame = media::VideoFrame::WrapExternalGpuMemoryBuffer(
-      gfx::Rect(frame_size), frame_size, std::move(gmb), base::TimeDelta());
+  test_sii_->UseTestGMBInSharedImageCreationWithBufferUsage();
+  // Setting some default usage in order to get a mappable shared image.
+  const auto si_usage = gpu::SHARED_IMAGE_USAGE_CPU_WRITE_ONLY |
+                        gpu::SHARED_IMAGE_USAGE_DISPLAY_READ;
+  // Create a mappable shared image.
+  auto shared_image = test_sii_->CreateSharedImage(
+      {viz::MultiPlaneFormat::kNV12, frame_size, gfx::ColorSpace(),
+       gpu::SharedImageUsageSet(si_usage), "WebMediaPlayerMSTest"},
+      gpu::kNullSurfaceHandle, gfx::BufferUsage::GPU_READ);
+  auto gpu_frame = media::VideoFrame::WrapMappableSharedImage(
+      std::move(shared_image), test_sii_->GenVerifiedSyncToken(),
+      base::NullCallback(), gfx::Rect(frame_size), frame_size,
+      base::TimeDelta());
+
   compositor_->EnqueueFrame(gpu_frame, true);
   base::RunLoop().RunUntilIdle();
   // frame with gpu resource should be reset if context is lost
