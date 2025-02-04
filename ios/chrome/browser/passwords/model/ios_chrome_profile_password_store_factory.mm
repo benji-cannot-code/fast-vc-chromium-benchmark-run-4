ac1a332b8ad94da9dd653614cf3b0b025926689d
Mikel Astiz <mastiz@chromium.org>
2025-02-04 02:13:34 -0800

    [sync] Remove WipeModelUponSyncDisabledBehavior::kOnceIfTrackingMetadata
    See linked bug for detailed background and rationale. The TL;DR is that
    this codepath is quite risky, hard to maintain and benefits a very small
    number of iOS users. It is time to clean it up, as the consequences for
    the few users affected are not too bad either (potential duplication of
    data, that they can presumably fix using the available batch upload
    flows).
    
    OBSOLETE_HISTOGRAM[Sync.DataTypeClearedOnce]=Logic cleaned up
    
    Change-Id: I9443ae55d5aea15075ceb5a155c739ba22bcce8b
    Fixed: 393165673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218787
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415455}

--- ios/chrome/browser/passwords/model/ios_chrome_profile_password_store_factory.mm
+++ ios/chrome/browser/passwords/model/ios_chrome_profile_password_store_factory.mm
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/password_manager/core/browser/password_store/password_store_built_in_backend.h"
 #import "components/password_manager/core/browser/password_store_factory_util.h"
 #import "components/password_manager/core/common/password_manager_features.h"
-#import "components/signin/public/identity_manager/tribool.h"
-#import "components/sync/model/wipe_model_upon_sync_disabled_behavior.h"
 #import "components/sync/service/sync_service.h"
 #import "ios/chrome/browser/affiliations/model/ios_chrome_affiliation_service_factory.h"
 #import "ios/chrome/browser/passwords/model/credentials_cleaner_runner_factory.h"
@@ -35,17 +33,6 @@ namespace {
 
 using affiliations::AffiliationService;
 using password_manager::AffiliatedMatchHelper;
-
-// Returns what the profile store should do when sync is disabled, that is,
-// whether passwords might need to be deleted.
-syncer::WipeModelUponSyncDisabledBehavior
-GetWipeModelUponSyncDisabledBehaviorForProfileStore() {
-  if (IsFirstSessionAfterDeviceRestore() != signin::Tribool::kTrue) {
-    return syncer::WipeModelUponSyncDisabledBehavior::kNever;
-  }
-
-  return syncer::WipeModelUponSyncDisabledBehavior::kOnceIfTrackingMetadata;
-}
 
 }  // namespace
 
@@ -103,7 +90,7 @@ IOSChromeProfilePasswordStoreFactory::BuildServiceInstanceFor(
       base::MakeRefCounted<password_manager::PasswordStore>(
           std::make_unique<password_manager::PasswordStoreBuiltInBackend>(
               std::move(login_db),
-              GetWipeModelUponSyncDisabledBehaviorForProfileStore(),
+              syncer::WipeModelUponSyncDisabledBehavior::kNever,
               profile->GetPrefs(), os_crypt_async));
 
   AffiliationService* affiliation_service =
--- ios/chrome/browser/reading_list/model/reading_list_model_factory.mm
+++ ios/chrome/browser/reading_list/model/reading_list_model_factory.mm
@@ -14,30 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/reading_list/core/dual_reading_list_model.h"
 #import "components/reading_list/core/reading_list_model_impl.h"
 #import "components/reading_list/core/reading_list_model_storage_impl.h"
-#import "components/signin/public/identity_manager/tribool.h"
 #import "components/sync/base/storage_type.h"
 #import "components/sync/model/data_type_store_service.h"
 #import "components/sync/model/wipe_model_upon_sync_disabled_behavior.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/system_flags.h"
-#import "ios/chrome/browser/signin/model/signin_util.h"
 #import "ios/chrome/browser/sync/model/data_type_store_service_factory.h"
 #import "ios/web/public/thread/web_thread.h"
-
-namespace {
-
-// Returns what the local-or-syncable instance should do when sync is disabled,
-// that is, whether reading list entries might need to be deleted.
-syncer::WipeModelUponSyncDisabledBehavior
-GetWipeModelUponSyncDisabledBehaviorForSyncableModel() {
-  if (IsFirstSessionAfterDeviceRestore() != signin::Tribool::kTrue) {
-    return syncer::WipeModelUponSyncDisabledBehavior::kNever;
-  }
-
-  return syncer::WipeModelUponSyncDisabledBehavior::kOnceIfTrackingMetadata;
-}
-
-}  // namespace
 
 // static
 ReadingListModel* ReadingListModelFactory::GetForProfile(ProfileIOS* profile) {
@@ -79,7 +62,7 @@ std::unique_ptr<KeyedService> ReadingListModelFactory::BuildServiceInstanceFor(
   auto reading_list_model_for_local_storage =
       std::make_unique<ReadingListModelImpl>(
           std::move(local_storage), syncer::StorageType::kUnspecified,
-          GetWipeModelUponSyncDisabledBehaviorForSyncableModel(),
+          syncer::WipeModelUponSyncDisabledBehavior::kNever,
           base::DefaultClock::GetInstance());
 
   syncer::OnceDataTypeStoreFactory store_factory_for_account_storage =
