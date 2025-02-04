bbfa428a72b6b2742c0b4f99846d1b1cc359803e
Rahul Singh <rahsin@microsoft.com>
2025-02-04 14:04:09 -0800

    IDB WPTs: Extend interleaved-cursor WPTs to run on workers
    This set of IndexedDB WPTs currently only run in a window environment.
    This change extends them to also run in dedicated, shared, and service
    worker environments.
    
    Bug: 41455766
    Change-Id: I8e984b4fb51293d70659e657c449f2dfb9697610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6220542
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1415813}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/interleaved-cursors-large.any.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=IndexedDB: Interleaved iteration of multiple cursors
+// META: global=window,worker
+// META: script=resources/support-promises.js
+// META: script=resources/interleaved-cursors-common.js
+// META: timeout=long
+
+'use strict';
+
+cursorTest(250);
