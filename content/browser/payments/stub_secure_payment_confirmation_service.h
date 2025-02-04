ae2dfc3ee0f399219d822542e371b81e7500b1f1
Stephen McGruer <smcgruer@chromium.org>
2025-02-04 14:35:05 -0800

    [SPC] Add isSecurePaymentConfirmationIsAvailable behind a flag (2/N)
    This CL adds the core of the implementation of PaymentRequest.
    isSecurePaymentConfirmationIsAvailable on both the renderer and browser
    sides, as well as mojo piping between the two.
    
    There are a few pieces still left to tackle in future changes:
    
    - The browser side doesn't currently check for the `payment` permission
      policy. This is checked by the renderer so for the happy path it
      works correctly, but for completeness the browser should check too.
    - The added blink unittest doesn't yet test the permission policy
      checking.
    - This CL also revealed a dangling raw_ptr in
      secure_payment_confirmation_service_unittest.cc, which previously
      went unnoticed because Android doesn't have dangling pointer checks.
    
    Intent to prototype and ship:
    https://groups.google.com/u/1/a/chromium.org/g/blink-dev/c/1pYa8CTib2s/m/1CbbBNHYAgAJ
    
    DanglingUntriaged-notes: Fixing in follow-up patch
    Bug: 40258712
    Change-Id: I2cd965a2cdd50217709ece60d685b2a9692a4772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194175
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415837}

--- content/browser/payments/stub_secure_payment_confirmation_service.h
+++ content/browser/payments/stub_secure_payment_confirmation_service.h
@@ -36,6 +36,10 @@ class StubSecurePaymentConfirmationService
       const StubSecurePaymentConfirmationService&) = delete;
 
   // mojom::SecurePaymentConfirmationService:
+  void IsSecurePaymentConfirmationAvailable(
+      IsSecurePaymentConfirmationAvailableCallback callback) override;
+
+  // mojom::SecurePaymentConfirmationService:
   void StorePaymentCredential(const std::vector<uint8_t>& credential_id,
                               const std::string& rp_id,
                               const std::vector<uint8_t>& user_id,
--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -45,6 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/html_iframe_element.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
+#include "third_party/blink/renderer/modules/credentialmanagement/credential_manager_proxy.h"
+#include "third_party/blink/renderer/modules/credentialmanagement/scoped_promise_resolver.h"
 #include "third_party/blink/renderer/modules/event_target_modules_names.h"
 #include "third_party/blink/renderer/modules/payments/payment_address.h"
 #include "third_party/blink/renderer/modules/payments/payment_method_change_event.h"
@@ -97,7 +99,6 @@ const char kGooglePlayBillingMethod[] = "https://play.google.com/billing";
 const char kUnknownCurrency[] = "ZZZ";
 const char kAppStoreBillingLabelPlaceHolder[] = "AppStoreBillingPlaceHolder";
 const char kSecurePaymentConfirmationMethod[] = "secure-payment-confirmation";
-
 }  // namespace
 
 namespace mojo {
@@ -818,6 +819,12 @@ void RecordActivationlessShow(ExecutionContext* execution_context,
   }
 }
 
+void OnIsSecurePaymentConfirmationAvailableResponse(
+    std::unique_ptr<ScopedPromiseResolver> scoped_resolver,
+    bool is_available) {
+  auto* resolver = scoped_resolver->Release()->DowncastTo<IDLBoolean>();
+  resolver->Resolve(is_available);
+}
 }  // namespace
 
 // static
@@ -827,8 +834,24 @@ ScriptPromise<IDLBoolean> PaymentRequest::isSecurePaymentConfirmationAvailable(
       MakeGarbageCollected<ScriptPromiseResolver<IDLBoolean>>(script_state);
   auto promise = resolver->Promise();
 
-  // TODO(crbug.com/40258712): Implement isSecurePaymentConfirmationAvailable
-  resolver->Resolve(false);
+  if (!RuntimeEnabledFeatures::SecurePaymentConfirmationEnabled(
+          ExecutionContext::From(script_state))) {
+    resolver->Resolve(false);
+    return promise;
+  }
+
+  if (!ExecutionContext::From(script_state)
+           ->IsFeatureEnabled(
+               network::mojom::PermissionsPolicyFeature::kPayment)) {
+    resolver->Resolve(false);
+    return promise;
+  }
+
+  CredentialManagerProxy::From(script_state)
+      ->SecurePaymentConfirmationService()
+      ->IsSecurePaymentConfirmationAvailable(
+          WTF::BindOnce(&OnIsSecurePaymentConfirmationAvailableResponse,
+                        std::make_unique<ScopedPromiseResolver>(resolver)));
 
   return promise;
 }
