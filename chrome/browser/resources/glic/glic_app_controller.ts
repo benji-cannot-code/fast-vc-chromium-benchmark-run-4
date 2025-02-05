3023ae5934cc6580490024cbdc96c7340476781d
Ian Vollick <vollick@chromium.org>
2025-02-04 17:56:34 -0800

    Connect to webviewTag exit/unresponsive events
    Manually tested by invoking the event handlers in a debugger.
    
    credit: mcnee@ for pointing out that these could be used here.
    
    Bug: 389120288,384044638,390479880
    Change-Id: Ib0c91486687c5dd8342a6a5b39585a470ac50882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6220875
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415954}

--- chrome/browser/resources/glic/glic_app_controller.ts
+++ chrome/browser/resources/glic/glic_app_controller.ts
@@ -89,6 +89,8 @@ export class GlicAppController implements PageInterface {
     this.contentLoaded = this.contentLoaded.bind(this);
     this.onNewWindow = this.onNewWindow.bind(this);
     this.onPermissionRequest = this.onPermissionRequest.bind(this);
+    this.onUnresponsive = this.onUnresponsive.bind(this);
+    this.onExit = this.onExit.bind(this);
 
     this.webview = this.createWebView();
 
@@ -123,6 +125,16 @@ export class GlicAppController implements PageInterface {
   private onPermissionRequest(e: any): void {
     if (e.permission === 'media' || e.permission === 'geolocation') {
       e.request.allow();
+    }
+  }
+
+  private onUnresponsive(): void {
+    this.setState(WebUiState.kUnresponsive);
+  }
+
+  private onExit(e: any): void {
+    if (e.reason !== 'normal') {
+      this.setState(WebUiState.kError);
     }
   }
 
@@ -188,6 +200,17 @@ export class GlicAppController implements PageInterface {
         onEnter: () => {
           $.guestPanel.classList.toggle('show-header', false);
           this.showPanel('guestPanel');
+        },
+      },
+    ],
+    [
+      WebUiState.kUnresponsive,
+      {
+        onEnter: () => {
+          this.destroyWebview();
+          // TODO(crbug.com/394162784): Create an unresponsive UI and permit
+          // transitioning back to being responsive.
+          this.showPanel('errorPanel');
         },
       },
     ],
@@ -272,6 +295,8 @@ export class GlicAppController implements PageInterface {
     webview.addEventListener('contentload', this.contentLoaded);
     webview.addEventListener('newwindow', this.onNewWindow);
     webview.addEventListener('permissionrequest', this.onPermissionRequest);
+    webview.addEventListener('unresponsive', this.onUnresponsive);
+    webview.addEventListener('exit', this.onExit);
 
     return webview;
   }
