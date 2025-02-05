2bbf3e69d8b65fa4e4bb7cc38a7fdb838306dcf2
Hidehiko Abe <hidehiko@chromium.org>
2025-02-05 09:36:45 -0800

    Remove is_current_user_ephemeral_regular_users_.
    The condition can be inferred by looking at whether the user
    is in the persisted user list.
    The key in this CL is to remove one "current" concept as preparation
    to move the caller of the function out from UserManager::UserLoggedIn.
    
    BUG=278643115
    TEST=Tryjob
    
    Change-Id: Ib29742c2e22eefe8d17c476422ebd21dcfc45d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6232326
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416250}

--- chrome/browser/ash/login/users/fake_chrome_user_manager.cc
+++ chrome/browser/ash/login/users/fake_chrome_user_manager.cc
@@ -277,7 +277,6 @@ void FakeChromeUserManager::UserLoggedIn(const AccountId& account_id,
     active_user_ =
         AddEphemeralUser(account_id, user_manager::UserType::kRegular);
     SetIsCurrentUserNew(true);
-    is_current_user_ephemeral_regular_user_ = true;
   }
 
   NotifyOnLogin();
--- components/user_manager/user_manager_impl.cc
+++ components/user_manager/user_manager_impl.cc
@@ -341,11 +341,10 @@ UserManagerImpl::EnsuredUser UserManagerImpl::EnsureUser(
         break;
       }
 
+      // Ensure User is created.
       if (is_ephemeral) {
         user = AddEphemeralUser(account_id, user_type);
-        is_current_user_ephemeral_regular_user_ = true;
       } else {
-        // Ensure User is created.
         user = AddGaiaUser(account_id, user_type);
       }
       break;
@@ -1041,8 +1040,8 @@ bool UserManagerImpl::IsUserNonCryptohomeDataEphemeral(
 
   // Even though device-local accounts might be ephemeral (e.g. kiosk accounts),
   // non-cryptohome data of device-local accounts should be non-ephemeral.
-  if (const User* user = FindUser(account_id);
-      user && user->IsDeviceLocalAccount()) {
+  const User* user = FindUser(account_id);
+  if (user && user->IsDeviceLocalAccount()) {
     return false;
   }
 
@@ -1051,9 +1050,12 @@ bool UserManagerImpl::IsUserNonCryptohomeDataEphemeral(
   //    policy was enabled.
   //    - or -
   // b) The user logged into any other account type.
-  if (IsUserLoggedIn() && (account_id == GetActiveUser()->GetAccountId()) &&
-      (is_current_user_ephemeral_regular_user_ ||
-       !IsLoggedInAsUserWithGaiaAccount())) {
+  // TODO(crbug.com/278643115): The first condition may be redundant, because
+  // we already check it in UserExistsInList above.
+  if (user && user == active_user_ &&
+      ((user->GetType() == UserType::kRegular &&
+        std::ranges::find(persisted_users_, user) == persisted_users_.end()) ||
+       !user->HasGaiaAccount())) {
     return true;
   }
 
