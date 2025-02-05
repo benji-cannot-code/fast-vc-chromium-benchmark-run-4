5755407db8fc3d687b565030684de440122b7f98
stluong <stluong@chromium.org>
2025-02-05 09:57:25 -0800

    Add support for Ctrl+Alt shortcuts for CrShortcutInput
    Currently, the CrShortcutInput element prevent users from setting
    Ctrl+Alt shortcuts. This modifies the CrShortcutInput element so that
    it can optionally support Ctrl+Alt shortcuts.
    
    This CL also fixes the bug where setting Command+Alt shortcuts should
    behave similarly to Ctrl+Alt because the Command key is automatically
    converted to ctrl in the backend to accommodate shortcuts saved to
    profile prefs and used on non-Mac devices.
    
    Bug: 391366158
    Change-Id: I69517adac9d7a51e7f124ab2c99473555f8fff65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6231351
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416265}

--- ui/webui/resources/cr_components/cr_shortcut_input/cr_shortcut_util.ts
+++ ui/webui/resources/cr_components/cr_shortcut_input/cr_shortcut_util.ts
@@ -99,7 +99,7 @@ export function keystrokeToString(e: KeyboardEvent): string {
   if (e.ctrlKey) {
     output.push('Ctrl');
   }
-  if (!e.ctrlKey && e.altKey) {
+  if (e.altKey) {
     output.push('Alt');
   }
   if (e.shiftKey) {
