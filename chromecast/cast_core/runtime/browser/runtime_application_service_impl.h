29855898fcc83ebb7bc9345918fdfcddc1836107
Erin Yan <yiranyan@google.com>
2025-02-05 13:27:34 -0800

    Pass through ac3/eac3 support to the browser.
    Mojo pipe for ApplicationMediaCapabilities was broken with the browser
    decoupling. This lets CastContentRendererClient receive the
    OnSupportedBitstreamAudioCodecsChanged events again. In upstream
    chromium, we will use AudioManagerAndroid to directly set support.
    
    Bug: internal 371469701
    Test: Cast Dolby Digital Plus (E-AC3) content.
    
    Change-Id: I59f4c3f8df507ed69853e0e97be6722028991c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216724
    Reviewed-by: Vigen Issahhanjan <vigeni@google.com>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Erin Yan <yiranyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416433}

--- chromecast/cast_core/runtime/browser/runtime_application_service_impl.h
+++ chromecast/cast_core/runtime/browser/runtime_application_service_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/task/sequenced_task_runner.h"
+#include "chromecast/browser/application_media_capabilities.h"
 #include "chromecast/browser/cast_content_window.h"
 #include "chromecast/browser/cast_web_view.h"
 #include "chromecast/cast_core/grpc/grpc_server.h"
@@ -89,6 +90,8 @@ class RuntimeApplicationServiceImpl : public cast_receiver::EmbedderApplication,
   void SetVisibility(cast::common::Visibility::Type state);
   void SetMediaBlocking(cast::common::MediaState::Type state);
 
+  void SetApplicationMediaCapabilities();
+
   // Called on an error is hit during running of cast mirroring or remoting.
   void OnStreamingApplicationError(cast_receiver::Status status);
 
@@ -142,6 +145,8 @@ class RuntimeApplicationServiceImpl : public cast_receiver::EmbedderApplication,
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   raw_ref<CastWebService> web_service_;
+
+  shell::ApplicationMediaCapabilities app_media_capabilities_;
 
   // The WebView associated with the window in which the Cast application is
   // displayed.
