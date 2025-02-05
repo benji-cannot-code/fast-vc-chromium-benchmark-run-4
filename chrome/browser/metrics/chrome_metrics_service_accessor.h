ffc88f50e3841c0b78f854ae1e093525f0f6ba14
Lei Zhang <thestig@chromium.org>
2025-02-05 11:32:22 -0800

    Rename chrome/common/metrics.mojom to ppapi_metrics.mojom
    Be more specific about what this Mojo interface is used for. Inside the
    .mojom file, also rename chrome::mojom::MetricsService to
    PpapiMetricsService.
    
    Change-Id: I4e96d49c115d2940b49afb2eb36117e2f0dea905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227679
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416354}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_PPAPI)
-#include "chrome/common/metrics.mojom.h"
+#include "chrome/common/ppapi_metrics.mojom.h"
 #endif
 
 class BrowserProcessImpl;
@@ -256,9 +256,9 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   static void SetForceIsMetricsReportingEnabledPrefLookup(bool value);
 
 #if BUILDFLAG(ENABLE_PPAPI)
-  // Provides an implementation of chrome::mojom::MetricsService.
-  static void BindMetricsServiceReceiver(
-      mojo::PendingReceiver<chrome::mojom::MetricsService> receiver);
+  // Provides an implementation of chrome::mojom::PpapiMetricsService.
+  static void BindPpapiMetricsServiceReceiver(
+      mojo::PendingReceiver<chrome::mojom::PpapiMetricsService> receiver);
 #endif  // BUILDFLAG(ENABLE_PPAPI)
 };
 
