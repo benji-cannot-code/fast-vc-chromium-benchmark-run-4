f8c75119d37bc45aa5d893e036dcfa623d7896d7
Anna Tsvirchkova <atsvirchkova@google.com>
2025-02-05 08:12:02 -0800

    [Sheriff] Disable more EmbeddedPermissionPromptTest
    The tests are consistently failing on
    android-automotive-12l-x64-rel-tests.
    
    Bug: 394097674
    Change-Id: I0c72e25fbbb7ccdcd81e0e120937153b2bdab7d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6233882
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416177}

--- chrome/android/javatests/src/org/chromium/chrome/browser/permissions/EmbeddedPermissionPromptTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/permissions/EmbeddedPermissionPromptTest.java
@@ -209,6 +209,7 @@ public class EmbeddedPermissionPromptTest {
     @MediumTest
     @Features.EnableFeatures({PermissionsAndroidFeatureList.PERMISSION_ELEMENT})
     @Features.DisableFeatures(PermissionsAndroidFeatureList.ONE_TIME_PERMISSION)
+    @DisabledTest(message = "crbug.com/394097674")
     public void testAskPromptTextWithoutOneTime() throws Exception {
         String[] requestablePermission =
                 new String[] {
@@ -264,6 +265,7 @@ public class EmbeddedPermissionPromptTest {
         PermissionsAndroidFeatureList.ONE_TIME_PERMISSION,
         PermissionsAndroidFeatureList.PERMISSION_ELEMENT
     })
+    @DisabledTest(message = "crbug.com/394097674")
     public void testPreviouslyDeniedPromptTextWithOneTime() throws Exception {
         String[] requestablePermission =
                 new String[] {
@@ -289,6 +291,7 @@ public class EmbeddedPermissionPromptTest {
     @MediumTest
     @Features.EnableFeatures(PermissionsAndroidFeatureList.PERMISSION_ELEMENT)
     @Features.DisableFeatures(PermissionsAndroidFeatureList.ONE_TIME_PERMISSION)
+    @DisabledTest(message = "crbug.com/394097674")
     public void testPreviouslyDeniedPromptTextWithoutOneTime() throws Exception {
         String[] requestablePermission =
                 new String[] {
