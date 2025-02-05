4bf306e9e20decb330f80d0f548accb17be891e9
Thomas Guilbert <tguilbert@chromium.org>
2025-02-04 16:04:24 -0800

    [CodeHealth] Remove AudioBus::channel() usage (part 5)
    This CL removes some uses of AudioBus::channel(), in favor of the safer
    AudioBus::channel_span().
    
    Bug: 373960632
    Change-Id: Ia3071763af9ec4f4b19eeaa8daa679150783a0fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166440
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415893}

--- media/base/converting_audio_fifo_unittest.cc
+++ media/base/converting_audio_fifo_unittest.cc
@@ -3,13 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "media/base/converting_audio_fifo.h"
 
+#include <algorithm>
 #include <memory>
 
 #include "media/base/audio_bus.h"
@@ -73,10 +69,7 @@ class ConvertingAudioFifoTest
 
   void PushFramesWithValue(AudioParameters params, float value) {
     auto audio_bus = AudioBus::Create(params);
-    float* channel = audio_bus->channel(0);
-    for (int i = 0; i < audio_bus->frames(); ++i) {
-      channel[i] = value;
-    }
+    std::ranges::fill(audio_bus->channel_span(0), value);
   }
 
   int min_number_input_frames_needed() {
@@ -282,7 +275,7 @@ TEST_F(ConvertingAudioFifoTest, Push_MoreThanEnoughFrames_IsFifoOrder) {
   while (fifo()->HasOutput()) {
     // Get the first value of the output.
     auto* output = fifo()->PeekOutput();
-    float current_value = output->channel(0)[0];
+    float current_value = output->channel_span(0)[0];
     EXPECT_GT(current_value, last_value);
     last_value = current_value;
   }
