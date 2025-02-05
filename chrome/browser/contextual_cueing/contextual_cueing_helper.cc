52a9e7974927a15241581bc938943a1ba669fd83
Sophie Chang <sophiechang@chromium.org>
2025-02-05 10:33:23 -0800

    Add contextual cueing nudge decision event
    Privacy doc here: https://docs.google.com/document/d/1LhnQ-2KDDdJMZ6eLieNr2EVlW1SP0ArhBfDFjUcCT0A/edit?resourcekey=0-bopXA880-mmI41RWLdrFtQ&tab=t.0#heading=h.k5jx6iluw4yt
    
    Bug: 393434401
    Change-Id: I1fa4af0456e71c42ee78d78b1f8c3361285b8d43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6231553
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416288}

--- chrome/browser/contextual_cueing/contextual_cueing_helper.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.cc
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/contextual_cueing_metadata.pb.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
@@ -37,9 +39,10 @@ namespace contextual_cueing {
 
 class ScopedNudgeDecisionRecorder {
  public:
-  explicit ScopedNudgeDecisionRecorder(
-      optimization_guide::proto::OptimizationType optimization_type)
-      : optimization_type_(optimization_type) {}
+  ScopedNudgeDecisionRecorder(
+      optimization_guide::proto::OptimizationType optimization_type,
+      ukm::SourceId source_id)
+      : optimization_type_(optimization_type), source_id_(source_id) {}
   ~ScopedNudgeDecisionRecorder() {
     CHECK_NE(nudge_decision_, NudgeDecision::kUnknown);
     base::UmaHistogramEnumeration(
@@ -47,6 +50,12 @@ class ScopedNudgeDecisionRecorder {
             optimization_guide::GetStringNameForOptimizationType(
                 optimization_type_),
         nudge_decision_);
+
+    auto* ukm_recorder = ukm::UkmRecorder::Get();
+    ukm::builders::ContextualCueing_NudgeDecision(source_id_)
+        .SetNudgeDecision(static_cast<int64_t>(nudge_decision_))
+        .SetOptimizationType(optimization_type_)
+        .Record(ukm_recorder->Get());
   }
 
   void set_nudge_decision(NudgeDecision nudge_decision) {
@@ -57,6 +66,7 @@ class ScopedNudgeDecisionRecorder {
 
  private:
   optimization_guide::proto::OptimizationType optimization_type_;
+  ukm::SourceId source_id_;
   NudgeDecision nudge_decision_ = NudgeDecision::kUnknown;
 };
 
@@ -109,7 +119,8 @@ void ContextualCueingHelper::DocumentOnLoadCompletedInPrimaryMainFrame() {
 
   std::unique_ptr<ScopedNudgeDecisionRecorder> recorder =
       std::make_unique<ScopedNudgeDecisionRecorder>(
-          optimization_guide::proto::GLIC_CONTEXTUAL_CUEING);
+          optimization_guide::proto::GLIC_CONTEXTUAL_CUEING,
+          web_contents()->GetPrimaryMainFrame()->GetPageUkmSourceId());
   optimization_guide::OptimizationMetadata metadata;
   auto decision = optimization_guide_keyed_service_->CanApplyOptimization(
       web_contents()->GetLastCommittedURL(),
