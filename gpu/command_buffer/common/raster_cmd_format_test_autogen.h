6383fff9b570bc549b72b4e16ea1e763afae43d9
Tom Sepez <tsepez@chromium.org>
2025-02-05 11:01:07 -0800

    Suppress unsafe libc call warnings in generated gpu files.
    Update a python script to add unsafe libc call suppression to a list of
    generated files, then run the scripts which use it to regenerate the
    files.
    
    This is similar to what was done in the past to suppress unsafe buffers
    warnings, and helps prepare us for eventual enforcement of these
    stricter warnings.
    
    Bug: 390223051
    Change-Id: I29898d1d7f5d9b010481e8b48cb26f37f6b56674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6230978
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416324}

--- gpu/command_buffer/common/raster_cmd_format_test_autogen.h
+++ gpu/command_buffer/common/raster_cmd_format_test_autogen.h
@@ -9,6 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
+#pragma allow_unsafe_libc_calls
+#endif
+
 // This file contains unit tests for raster commands
 // It is included by raster_cmd_format_test.cc
 
