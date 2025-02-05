149d76a6aa0506cad66b655991ce234ee401402b
Colin Blundell <blundell@chromium.org>
2025-02-05 02:50:03 -0800

    [Blink] Remove CanvasResourceSI check for context provider being lost
    Now that CanvasResourceSharedImage::UploadSoftwareRenderingResult() no
    longer calls into ContextProviderWrapper()'s SharedImageInterface
    directly, there is no need for the method to early-out if the wrapper
    isn't present:
    
    * If the ClientSharedImage's SII is gone then the ClientSI is orphaned
    * In that case it either can be still mapped or can't (there are no
      cases where it can't currently, but we'd make Map() fail in that
      case, which this code handles).
    * The call to ClientSI::BackingWasExternallyUpdated() is a safe no-op
      in that case as well as it will short-circuit out if the ClientSI's
      SII isn't present
    
    Bug: 379996128
    Change-Id: I40f11fc9cf85538093e07dda11b6b4c5c1a2f1ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6224413
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416074}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -649,9 +649,6 @@ void CanvasResourceSharedImage::UploadSoftwareRenderingResults(
 
   const sk_sp<SkImage>& image = sk_surface->makeImageSnapshot();
 
-  if (!ContextProviderWrapper()) {
-    return;
-  }
   std::unique_ptr<gpu::ClientSharedImage::ScopedMapping> mapping =
       GetClientSharedImage()->Map();
   if (!mapping) {
