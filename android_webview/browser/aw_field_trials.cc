ecb178cdce3781ee867d7c857ac0bb6720124f38
Juston Li <justonli@google.com>
2025-02-05 09:01:08 -0800

    Disable VulkanFromANGLE on WebView
    Sharing ANGLE's Vulkan queue is not supported on WebView and causes
    crashes.
    
    Bug: 393957039
    Change-Id: I7c6c5144b9eb5c05471c1a89e4c7576eacea1c4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6232401
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Juston Li <justonli@google.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416217}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features_generated.h"
 #include "ui/android/ui_android_features.h"
 #include "ui/gl/gl_features.h"
+#include "ui/gl/gl_switches.h"
 
 namespace internal {
 
@@ -299,4 +300,7 @@ void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
 
   // Disable Topics on WebView.
   aw_feature_overrides.DisableFeature(blink::features::kBrowsingTopics);
+
+  // Sharing ANGLE's Vulkan queue is not supported on WebView.
+  aw_feature_overrides.DisableFeature(::features::kVulkanFromANGLE);
 }
