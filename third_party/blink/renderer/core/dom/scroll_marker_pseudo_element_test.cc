4a4f32bad75b78dda60e439a3b5a294ab4d390da
Rune Lillesveen <futhark@chromium.org>
2025-02-05 14:48:27 -0800

    Add a use counter for nested ::scroll-markers
    Use count the case where there are two ::scroll-markers attached to a
    ::scroll-marker-group whose originating elements have an ancestor /
    descendant relationship.
    
    ::column::scroll-markers are ignored.
    
    Bug: 393100181
    Change-Id: I654c5aa21daac67c9f512f23a7691d07f308b112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6235330
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416482}

---
+++ third_party/blink/renderer/core/dom/scroll_marker_pseudo_element_test.cc
@@ -0,0 +1,62 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/dom/scroll_marker_pseudo_element.h"
+
+#include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/testing/page_test_base.h"
+
+namespace blink {
+
+using ScrollMarkerPseudoElementTest = PageTestBase;
+
+TEST_F(ScrollMarkerPseudoElementTest, NoNestedMarkersCount) {
+  SetBodyContent(R"HTML(
+    <style>
+      #scroller {
+        overflow: scroll;
+        width: 100px;
+        height: 100px;
+        scroll-marker-group: after;
+      }
+      .marker::scroll-marker {
+        content: "";
+      }
+    </style>
+    <div id="scroller">
+      <div class="marker"></div>
+      <div class="marker"></div>
+    </div>
+  )HTML");
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_FALSE(GetDocument().IsUseCounted(WebFeature::kNestedScrollMarkers));
+}
+
+TEST_F(ScrollMarkerPseudoElementTest, NestedMarkersCounted) {
+  SetBodyContent(R"HTML(
+    <style>
+      #scroller {
+        overflow: scroll;
+        width: 100px;
+        height: 100px;
+        scroll-marker-group: after;
+      }
+      .marker::scroll-marker {
+        content: "";
+      }
+    </style>
+    <div id="scroller">
+      <div class="marker">
+        <div class="marker"></div>
+      </div>
+    </div>
+  )HTML");
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_TRUE(GetDocument().IsUseCounted(WebFeature::kNestedScrollMarkers));
+}
+
+}  // namespace blink
