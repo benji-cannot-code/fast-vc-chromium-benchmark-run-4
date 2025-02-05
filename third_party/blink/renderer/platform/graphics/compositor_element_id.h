92b87fdcd98d3893ab734b3be173399f0c57819b
Vladimir Levin <vmpstr@chromium.org>
2025-02-05 07:26:02 -0800

    cc/paint: Move ElementId enum namespaces to cc
    This patch moves element id enum namespace to cc, exposing them to
    blink. The reason for this is that cc will need
    to introduce a new namespace for render pass generation when a render
    surface generates two render passes (in view transition capture
    phase).
    
    R=pdr@chromium.org
    
    Bug: 358119267
    Change-Id: Ic179d2837fd9c4870b89a256ca97262e32a916cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219355
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416141}

--- third_party/blink/renderer/platform/graphics/compositor_element_id.h
+++ third_party/blink/renderer/platform/graphics/compositor_element_id.h
@@ -15,40 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-const int kCompositorNamespaceBitCount = 5;
+const int kCompositorNamespaceBitCount = cc::kElementIdNamespaceBitCount;
 
 // The functions in this header requires cc::ElementId::InternalValue to be
 // uint64_t.
 static_assert(std::is_same_v<cc::ElementId::InternalValue, uint64_t>);
 
-enum class CompositorElementIdNamespace {
-  kPrimary,
-  kUniqueObjectId,
-  kScroll,
-  kStickyTranslation,
-  kAnchorPositionScrollTranslation,
-  kPrimaryEffect,
-  kPrimaryTransform,
-  kEffectFilter,
-  kEffectMask,
-  kEffectClipPath,
-  kScaleTransform,
-  kRotateTransform,
-  kTranslateTransform,
-  kVerticalScrollbar,
-  kHorizontalScrollbar,
-  kScrollCorner,
-  kViewTransitionSubframeRoot,
-  kViewTransitionElement,
-  kElementCapture,
-  kPlaceElement,
-  kDOMNodeId,
-  // The following values are for internal usage only.
-  kMax = kDOMNodeId,
-  // A sentinel to indicate the maximum representable namespace id
-  // (the maximum is one less than this value).
-  kMaxRepresentable = 1 << kCompositorNamespaceBitCount
-};
+using CompositorElementIdNamespace = cc::ElementIdNamespace;
 
 static_assert(CompositorElementIdNamespace::kMax <
               CompositorElementIdNamespace::kMaxRepresentable);
