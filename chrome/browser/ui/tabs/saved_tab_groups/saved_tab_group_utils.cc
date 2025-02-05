86bee230f59d201032b3ebc5cd36e8d78f35e05d
David Pennington <dpenning@chromium.org>
2025-02-05 11:26:10 -0800

    [SharedTabGroups] Show Avatars in the Manage Sharing Row
    This CL rebuilds the manage sharing row in the TabGroupEditorBubble
    to show multiple avatars in a face pile to the right of the row title.
    it does this by querying for member state from the data sharing
    service then updating stored images and showing image views with
    negative margins to overlap.
    
    This CL adds a complex row that is similar to HoverButton, but
    allows for the specific view content thats expected for the manage row.
    
    If there are more than 3 members, we show an overflow count with a +.
    
    menu rows was renamed to "simple menu rows" since it already
    represents that.
    
    Another change made here is that i remove the network queried clip,
    since it doesnt seem to work as expected on the page action. a
    followup CL will add the clipping to the page action.
    
    
    Bug: 391870579, 391870310, 391871178
    Change-Id: I390ae730478376818bf87eedbe8d660af55e9212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6203271
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416349}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
 
 #include <numeric>
+#include <optional>
 #include <unordered_set>
 
 #include "base/functional/bind.h"
@@ -752,6 +753,28 @@ bool SavedTabGroupUtils::IsOwnerOfSharedTabGroup(Profile* profile,
           data_sharing::GroupId(collaboration_id.value().value()));
 
   return data_sharing::MemberRole::kOwner == member_role;
+}
+
+// static
+std::vector<data_sharing::GroupMember>
+SavedTabGroupUtils::GetMembersOfSharedTabGroup(
+    Profile* profile,
+    const tab_groups::CollaborationId& collaboration_id) {
+  collaboration::CollaborationService* collaboration_service =
+      collaboration::CollaborationServiceFactory::GetForProfile(profile);
+  if (!collaboration_service) {
+    return {};
+  }
+
+  const std::optional<data_sharing::GroupData> group_data =
+      collaboration_service->GetGroupData(
+          data_sharing::GroupId(collaboration_id.value()));
+
+  if (!group_data.has_value()) {
+    return {};
+  }
+
+  return group_data->members;
 }
 
 // static
