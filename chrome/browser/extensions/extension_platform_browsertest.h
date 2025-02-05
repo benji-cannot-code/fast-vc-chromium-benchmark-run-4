38977b5223bdbd9c93af91d524065997a598cb9d
James Cook <jamescook@chromium.org>
2025-02-05 12:00:06 -0800

    extensions: Add chrome.system.network API to desktop Android
    This API is deprecated (it was for platform apps) but is still in use
    by allowlisted extensions, including an extension we need for corp
    (see bugs for which one).
    
    In addition to adding the API, migrate its API test from a shell-based
    test to a chrome-based browser test. In earlier design discussions
    we decided we don't want to port app_shell to Android.
    
    This requires moving a ScopedFeatureChannel from the subclass
    ExtensionBrowserTest to the parent ExtensionPlatformBrowserTest
    because this API is otherwise blocked by feature permissions. (The API
    allowed for extensions on dev channel, but only for apps and
    allowlisted extensions on stable.)
    
    Bug: 394367691, 388287861
    Change-Id: I4907f4031da65a32f0509f465ad1e9e98deacac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6230463
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416370}

--- chrome/browser/extensions/extension_platform_browsertest.h
+++ chrome/browser/extensions/extension_platform_browsertest.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_protocols.h"
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_id.h"
+#include "extensions/common/features/feature_channel.h"
 
 class Profile;
 
@@ -161,6 +162,15 @@ class ExtensionPlatformBrowserTest : public PlatformBrowserTest {
   class TestTabModel;
   std::unique_ptr<TestTabModel> tab_model_;
 #endif
+
+  // Used for setting the default scoped current channel for extension browser
+  // tests to UNKNOWN (trunk), in order to enable channel restricted features.
+  // TODO(crbug.com/40261741): We should remove this and have the current
+  // channel respect what is defined on the builder. If a test requires a
+  // specific channel for a channel restricted feature, it should be defining
+  // its own scoped channel override. As this stands, it means we don't really
+  // have non-trunk coverage for most extension browser tests.
+  ScopedCurrentChannel current_channel_;
 };
 
 }  // namespace extensions
