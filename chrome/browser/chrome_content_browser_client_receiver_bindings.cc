7a0fd380a48c910c4e7faca65b724426f4150a81
Lei Zhang <thestig@chromium.org>
2025-02-04 17:03:13 -0800

    Split chrome_browser_interface_binders.cc in two
    Move the WebUI portion of chrome_browser_interface_binders.cc into
    chrome_browser_interface_binders_webui.cc. Then the build system can
    compile the different .cc files in parallel, and incrementally build any
    particular .cc file faster.
    
    Change-Id: I5a1e2304fbf1b1a26083422f9c36a2c81b7ecd1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6222587
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415929}

--- chrome/browser/chrome_content_browser_client_receiver_bindings.cc
+++ chrome/browser/chrome_content_browser_client_receiver_bindings.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_browser_interface_binders.h"
+#include "chrome/browser/chrome_browser_interface_binders_webui.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/chrome_content_browser_client_parts.h"
 #include "chrome/browser/content_settings/content_settings_manager_delegate.h"
