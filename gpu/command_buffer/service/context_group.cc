e8fad4716610f2f17037f9850b329e55b28d4980
Zhenyao Mo <zmo@chromium.org>
2025-02-05 07:55:43 -0800

    Remove |supports_passthrough_decoders| from ContextGroup constructor.
    It is always true now.
    
    TEST=gpu_unittests
    
    Bug: 394148793
    Change-Id: I3ac03952b08fc58ed678494e2ae8fee34aeda97e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226892
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416161}

--- gpu/command_buffer/service/context_group.cc
+++ gpu/command_buffer/service/context_group.cc
@@ -70,7 +70,6 @@ DisallowedFeatures AdjustDisallowedFeatures(
 
 ContextGroup::ContextGroup(
     const GpuPreferences& gpu_preferences,
-    bool supports_passthrough_command_decoders,
     std::unique_ptr<MemoryTracker> memory_tracker,
     ShaderTranslatorCache* shader_translator_cache,
     FramebufferCompletenessCache* framebuffer_completeness_cache,
@@ -129,8 +128,7 @@ ContextGroup::ContextGroup(
       shared_image_manager_(shared_image_manager) {
   DCHECK(discardable_manager);
   DCHECK(feature_info_);
-  use_passthrough_cmd_decoder_ = supports_passthrough_command_decoders &&
-                                 gpu_preferences_.use_passthrough_cmd_decoder;
+  use_passthrough_cmd_decoder_ = gpu_preferences_.use_passthrough_cmd_decoder;
 }
 
 gpu::ContextResult ContextGroup::Initialize(
