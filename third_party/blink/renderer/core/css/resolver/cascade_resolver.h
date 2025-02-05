fce2e249fa144a9b6e1eb49868a21161aca947a4
Anders Hartvoll Ruud <andruud@chromium.org>
2025-02-05 08:31:13 -0800

    [functions] Detect cycles between <dashed-functions>
    This is quite similar to CL:6190327: we just extend CycleElem
    to accommodate yet another type.
    
    Unfortunately we won't be able to keep using absl::variable when
    we support tree-scoped references, since we'll need to store
    GarbageCollected type at that point. But that's a problem for later.
    
    Bug: 325504770
    Change-Id: If1a51e5c7ee2d95a7070953c6f72dcf3601ae6a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190588
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416195}

--- third_party/blink/renderer/core/css/resolver/cascade_resolver.h
+++ third_party/blink/renderer/core/css/resolver/cascade_resolver.h
@@ -37,7 +37,9 @@ class CORE_EXPORT CascadeResolver {
  public:
   using Attribute = base::StrongAlias<class AttributeTag, AtomicString>;
   using LocalVariable = base::StrongAlias<class LocalVariableTag, AtomicString>;
-  using CycleElem = absl::variant<const CSSProperty*, Attribute, LocalVariable>;
+  using Function = base::StrongAlias<class FunctionTag, AtomicString>;
+  using CycleElem =
+      absl::variant<const CSSProperty*, Attribute, LocalVariable, Function>;
   // TODO(crbug.com/985047): Probably use a HashMap for this.
   using CycleStack = Vector<CycleElem, 8>;
 
@@ -47,6 +49,7 @@ class CORE_EXPORT CascadeResolver {
   bool IsLocked(const CSSProperty&) const;
   bool IsLocked(const Attribute&) const;
   bool IsLocked(const LocalVariable&) const;
+  bool IsLocked(const Function&) const;
 
   // Returns the property we're currently applying.
   const CSSProperty* CurrentProperty() const {
@@ -97,6 +100,7 @@ class CORE_EXPORT CascadeResolver {
     AutoLock(const CSSProperty&, CascadeResolver&);
     AutoLock(const Attribute&, CascadeResolver&);
     AutoLock(const LocalVariable&, CascadeResolver&);
+    AutoLock(const Function&, CascadeResolver&);
     ~AutoLock();
 
    private:
@@ -122,6 +126,7 @@ class CORE_EXPORT CascadeResolver {
   bool DetectCycle(const CSSProperty&);
   bool DetectCycle(const Attribute&);
   bool DetectCycle(const LocalVariable&);
+  bool DetectCycle(const Function&);
   bool DetectCycle(wtf_size_t index);
   // Returns true whenever the CascadeResolver is in a cycle state.
   // This DOES NOT detect cycles; the caller must call DetectCycle first.
@@ -132,6 +137,7 @@ class CORE_EXPORT CascadeResolver {
   wtf_size_t Find(const CSSProperty&) const;
   wtf_size_t Find(const Attribute&) const;
   wtf_size_t Find(const LocalVariable&) const;
+  wtf_size_t Find(const Function&) const;
 
   CycleStack stack_;
   // If we're in a cycle, cycle_start_ is the index of the stack_ item that
--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -1550,6 +1550,13 @@ bool StyleCascade::ResolveFunctionInto(StringView function_name,
                                        TokenSequence& out) {
   state_.StyleBuilder().SetAffectedByCSSFunction();
 
+  AtomicString function_name_atomic(function_name);
+  using Function = CascadeResolver::Function;
+  if (resolver.DetectCycle(Function(function_name_atomic))) {
+    return false;
+  }
+  CascadeResolver::AutoLock lock(Function(function_name_atomic), resolver);
+
   // TODO(sesse): Deal with tree-scoped references.
   StyleRuleFunction* function = nullptr;
   if (GetDocument().GetScopedStyleResolver()) {
@@ -1660,7 +1667,13 @@ bool StyleCascade::ResolveFunctionInto(StringView function_name,
       .locals = {},  // Populated by ApplyLocalVariables.
       .unresolved_locals = unresolved_locals,
       .parent = function_context};
+
   ApplyLocalVariables(resolver, context, local_function_context);
+
+  // Applying local variables may place this function in a cycle.
+  if (resolver.InCycle()) {
+    return false;
+  }
 
   const CSSValue* ret_value = ResolveFunctionExpression(
       unresolved_result->VariableDataValue()->OriginalText(),
