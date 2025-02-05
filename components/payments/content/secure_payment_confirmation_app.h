1b6fe470de08aed832bfb39d3c6cb18a9caa36af
Slobodan Pejic <slobodan@chromium.org>
2025-02-05 12:09:38 -0800

    Recreate browser bound key on SPC assertion.
    Before this change, if a browser bound keys was not present at secure
    payment assertion then no browser bound key would be included in the
    secure payment confirmation assertion.  This change moves binding of
    browser bound keys to passkeys to a new class PasskeyBrowserBinding and
    uses it in secure payment confirmation to recreate browser bound keys.
    After this change, a new browser bound key is created if needed at
    payment assertion.
    
    Browser bound signatures are currently behind the flag,
    chrome://flags/#enable-secure-payment-confirmation-browser-bound-key
    
    Bug: 377278827, b:387275359
    Change-Id: Idf98ca310bbde7d63f48bbed0c8628fdab69e7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187265
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416378}

--- components/payments/content/secure_payment_confirmation_app.h
+++ components/payments/content/secure_payment_confirmation_app.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
-#include "components/payments/content/browser_binding/browser_bound_key.h"
-#include "components/payments/content/browser_binding/browser_bound_key_store.h"
 #include "components/payments/content/payment_app.h"
 #include "components/payments/content/secure_payment_confirmation_controller.h"
 #include "content/public/browser/global_routing_id.h"
@@ -36,6 +34,8 @@ class WebContents;
 
 namespace payments {
 
+class BrowserBoundKey;
+class PasskeyBrowserBinder;
 class PaymentRequestSpec;
 
 // These values are persisted to logs. Entries should not be renumbered and
@@ -58,7 +58,7 @@ class SecurePaymentConfirmationApp : public PaymentApp,
       const std::u16string& payment_instrument_label,
       std::unique_ptr<SkBitmap> payment_instrument_icon,
       std::vector<uint8_t> credential_id,
-      std::optional<std::vector<uint8_t>> browser_bound_key_id,
+      std::unique_ptr<PasskeyBrowserBinder> passkey_browser_binder,
       const url::Origin& merchant_origin,
       base::WeakPtr<PaymentRequestSpec> spec,
       mojom::SecurePaymentConfirmationRequestPtr request,
@@ -108,15 +108,13 @@ class SecurePaymentConfirmationApp : public PaymentApp,
   std::u16string issuer_label() const { return issuer_label_; }
   SkBitmap issuer_icon() const { return issuer_icon_; }
 
-  void SetBrowserBoundKeyStoreForTesting(
-      std::unique_ptr<BrowserBoundKeyStore> key_store);
-
-#if BUILDFLAG(IS_ANDROID)
-  const std::optional<std::vector<uint8_t>>& GetBrowserBoundKeyIdForTesting()
-      const;
-#endif  // BUILDFLAG(IS_ANDROID)
+  PasskeyBrowserBinder* GetPasskeyBrowserBinderForTesting();
 
  private:
+  void OnGetBrowserBoundKey(
+      base::WeakPtr<Delegate> delegate,
+      blink::mojom::PublicKeyCredentialRequestOptionsPtr options,
+      std::unique_ptr<BrowserBoundKey> browser_bound_key);
   void OnGetAssertion(
       base::WeakPtr<Delegate> delegate,
       blink::mojom::AuthenticatorStatus status,
@@ -131,12 +129,11 @@ class SecurePaymentConfirmationApp : public PaymentApp,
   const std::u16string payment_instrument_label_;
   const std::unique_ptr<SkBitmap> payment_instrument_icon_;
   const std::vector<uint8_t> credential_id_;
-  const std::optional<std::vector<uint8_t>> browser_bound_key_id_;
   const url::Origin merchant_origin_;
   const base::WeakPtr<PaymentRequestSpec> spec_;
   const mojom::SecurePaymentConfirmationRequestPtr request_;
   std::unique_ptr<webauthn::InternalAuthenticator> authenticator_;
-  std::unique_ptr<BrowserBoundKeyStore> browser_bound_key_store_;
+  std::unique_ptr<PasskeyBrowserBinder> passkey_browser_binder_;
   std::unique_ptr<BrowserBoundKey> browser_bound_key_;
   std::string challenge_;
   blink::mojom::GetAssertionAuthenticatorResponsePtr response_;
--- components/payments/content/secure_payment_confirmation_app_factory.h
+++ components/payments/content/secure_payment_confirmation_app_factory.h
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments {
 
+#if BUILDFLAG(IS_ANDROID)
+class BrowserBoundKeyStore;
+#endif  // BUILDFLAG(IS_ANDROID)
 struct SecurePaymentConfirmationCredential;
 
 class SecurePaymentConfirmationAppFactory : public PaymentAppFactory,
@@ -30,6 +33,11 @@ class SecurePaymentConfirmationAppFactory : public PaymentAppFactory,
 
   // PaymentAppFactory:
   void Create(base::WeakPtr<Delegate> delegate) override;
+
+#if BUILDFLAG(IS_ANDROID)
+  void SetBrowserBoundKeyStoreForTesting(
+      std::unique_ptr<BrowserBoundKeyStore> key_store);
+#endif  // BUILDFLAG(IS_ANDROID)
 
  private:
   struct Request;
@@ -65,11 +73,12 @@ class SecurePaymentConfirmationAppFactory : public PaymentAppFactory,
 
   // Called once all icons are downloaded and their respective SkBitmaps have
   // been set into the Request.
-  void DidDownloadAllIcons(
-      std::optional<std::vector<uint8_t>> browser_bound_key_id,
-      std::unique_ptr<Request> request);
+  void DidDownloadAllIcons(std::unique_ptr<Request> request);
 
   std::map<WebDataServiceBase::Handle, std::unique_ptr<Request>> requests_;
+#if BUILDFLAG(IS_ANDROID)
+  std::unique_ptr<BrowserBoundKeyStore> browser_bound_key_store_for_testing_;
+#endif  // BUILDFLAG(IS_ANDROID)
   base::WeakPtrFactory<SecurePaymentConfirmationAppFactory> weak_ptr_factory_{
       this};
 };
