ad443a1f535e27d03dd72b4f366134c1491c5696
Emily Andrews <emiled@microsoft.com>
2025-02-04 19:19:26 -0800

    Fix mojo typescript generation to take into account type nesting
    This change updates the mojom_ts_generator.py to fix an issue where
    imports of Interface types nested in arrays and other containers
    would be missing from the .ts file for WebUI files.
    
    Also adds a test case to validate the changes and comments to help
    explain the logic
    
    Bug: 393183858
    Change-Id: I39ed13af22f5f12da2253c12c38a1a1f44d98a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218839
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Emily Andrews <emiled@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1415970}

--- content/test/data/web_ui_mojo_ts_test.ts
+++ content/test/data/web_ui_mojo_ts_test.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {StringDictType, TestNode} from './web_ui_mojo_ts_test_mapped_types.js';
 import {OptionalNumericsStruct, TestEnum, WebUITsMojoTestCache} from './web_ui_ts_test.test-mojom-webui.js';
+import {StringWrapper} from './web_ui_ts_test_types.test-mojom-webui.js';
 
 const TEST_DATA: Array<{url: string, contents: string}> = [
   { url: 'https://google.com/', contents: 'i am in fact feeling lucky' },
@@ -36,10 +37,13 @@ async function doTest(): Promise<boolean> {
   const cache = WebUITsMojoTestCache.getRemote();
   for (const entry of TEST_DATA) {
     cache.put({ url: entry.url }, entry.contents);
+    let stringWrapper = StringWrapper.getRemote();
+    stringWrapper.putString(entry.contents);
+    cache.addStringWrapper(stringWrapper);
   }
 
   const {items} = await cache.getAll();
-  if (items.length != TEST_DATA.length) {
+  if (items.length !== TEST_DATA.length) {
     return false;
   }
 
@@ -53,6 +57,23 @@ async function doTest(): Promise<boolean> {
       return false;
     }
     if (entries[entry.url] != entry.contents) {
+      return false;
+    }
+  }
+
+  const {stringWrapperList} = await cache.getStringWrapperList();
+  if (stringWrapperList.length !== TEST_DATA.length) {
+    return false;
+  }
+
+  let stringsInList = [];
+  for (const stringWrapper of stringWrapperList) {
+    let {item} = await stringWrapper.getString();
+    stringsInList.push(item);
+  }
+
+  for (const entry of TEST_DATA) {
+    if (!stringsInList.includes(entry.contents)) {
       return false;
     }
   }
