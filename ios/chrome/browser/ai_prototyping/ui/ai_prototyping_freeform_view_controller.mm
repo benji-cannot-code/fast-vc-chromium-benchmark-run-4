68e290a37d7f832cad04af985ed1781de45a23b6
adamta <adamta@google.com>
2025-02-05 12:01:12 -0800

    [iOS] Move AI prototyping submit buttons enabling
    Instead of re-enabling the submit buttons when the result text is
    updated, now they're re-enabled when the response is received. This
    allows us to update the result field without affecting the button
    state.
    
    Bug: 392901832
    Change-Id: Id94f5a60301d4298227ea1ee04a51d906eb15b3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6234632
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416371}

--- ios/chrome/browser/ai_prototyping/ui/ai_prototyping_freeform_view_controller.mm
+++ ios/chrome/browser/ai_prototyping/ui/ai_prototyping_freeform_view_controller.mm
@@ -263,14 +263,8 @@ using optimization_guide::proto::BlingPrototypingRequest_ModelEnum_Name;
 
 - (void)updateResponseField:(NSString*)response {
   _responseContainer.text = response;
-
-  // Enable the submit buttons, as the request has resolved.
-  [self enableSubmitButtons];
 }
 
-#pragma mark - Private
-
-// Enable submit buttons, and style the accordingly.
 - (void)enableSubmitButtons {
   _serverSideSubmitButton.enabled = YES;
   _serverSideSubmitButton.backgroundColor = [UIColor colorNamed:kBlueColor];
@@ -281,16 +275,7 @@ using optimization_guide::proto::BlingPrototypingRequest_ModelEnum_Name;
 #endif  // BUILDFLAG(BUILD_WITH_INTERNAL_OPTIMIZATION_GUIDE)
 }
 
-// Disable submit buttons, and style the accordingly.
-- (void)disableSubmitButtons {
-  _serverSideSubmitButton.enabled = NO;
-  _serverSideSubmitButton.backgroundColor =
-      [UIColor colorNamed:kDisabledTintColor];
-
-  _onDeviceSubmitButton.enabled = NO;
-  _onDeviceSubmitButton.backgroundColor =
-      [UIColor colorNamed:kDisabledTintColor];
-}
+#pragma mark - Private
 
 // Returns a container for a text field in the menu.
 - (UIView*)textFieldContainer {
@@ -302,6 +287,17 @@ using optimization_guide::proto::BlingPrototypingRequest_ModelEnum_Name;
       [[UIColor colorNamed:kTextPrimaryColor] CGColor];
   container.layer.borderWidth = kBorderWidth;
   return container;
+}
+
+// Disable submit buttons, and style them accordingly.
+- (void)disableSubmitButtons {
+  _serverSideSubmitButton.enabled = NO;
+  _serverSideSubmitButton.backgroundColor =
+      [UIColor colorNamed:kDisabledTintColor];
+
+  _onDeviceSubmitButton.enabled = NO;
+  _onDeviceSubmitButton.backgroundColor =
+      [UIColor colorNamed:kDisabledTintColor];
 }
 
 // Creates the menu for the model picker button.
--- ios/chrome/browser/ai_prototyping/ui/ai_prototyping_tab_organization_view_controller.mm
+++ ios/chrome/browser/ai_prototyping/ui/ai_prototyping_tab_organization_view_controller.mm
@@ -134,9 +134,11 @@ using optimization_guide::proto::
 
 - (void)updateResponseField:(NSString*)response {
   _responseContainer.text = response;
+}
 
-  // Re-enable the submit button as the query has resolved.
-  [self enableSubmitButton];
+- (void)enableSubmitButtons {
+  _groupTabsButton.enabled = YES;
+  _groupTabsButton.backgroundColor = [UIColor colorNamed:kBlueColor];
 }
 
 #pragma mark - Private
@@ -238,12 +240,6 @@ using optimization_guide::proto::
     default:
       NOTREACHED();
   }
-}
-
-// Enable submit button, and style the accordingly.
-- (void)enableSubmitButton {
-  _groupTabsButton.enabled = YES;
-  _groupTabsButton.backgroundColor = [UIColor colorNamed:kBlueColor];
 }
 
 // Disable submit button, and style the accordingly.
