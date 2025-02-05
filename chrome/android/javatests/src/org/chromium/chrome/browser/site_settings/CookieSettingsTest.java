39775004b4d5df3134bf1e4e85783e4da858c61c
Maggie Jennings <mjenn@google.com>
2025-02-05 08:10:52 -0800

    [CCM] Strings for settings pt. 1 on Clank
    go/ccm-unification-prd
    UX deck:
    https://docs.google.com/presentation/d/1UKLg5dgH0_3o1-0f38GemYCXs33pbr8WjrnZB90lxFQ/present?slide=13
    
    Bug: b:370008370
    Change-Id: I0f47000c47fac8e9d8e1a8177eb48b10a1e35196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180239
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416175}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/CookieSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/CookieSettingsTest.java
@@ -7,7 +7,9 @@ package org.chromium.chrome.browser.site_settings;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -27,6 +29,9 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.Features.DisableFeatures;
+import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.settings.SettingsActivity;
 import org.chromium.chrome.browser.settings.SettingsActivityTestRule;
@@ -44,6 +49,7 @@ import java.io.IOException;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @Batch(Batch.PER_CLASS)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@EnableFeatures({ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO})
 public class CookieSettingsTest {
     @Rule
     public SettingsActivityTestRule<SingleCategorySettings> mSettingsActivityTestRule =
@@ -66,10 +72,72 @@ public class CookieSettingsTest {
         mSettingsActivity = mSettingsActivityTestRule.startSettingsActivity(fragmentArgs);
     }
 
+    // TODO(crbug.com/370008370): Remove once AlwaysBlock3pcsIncognito launched.
+    @Test
+    @SmallTest
+    @DisableFeatures({ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO})
+    public void shouldDisplayBlockThirdPartyIncognitoDescriptionWhenAuxButtonClicked()
+            throws IOException {
+        onView(withId(R.id.block_third_party_incognito_with_aux)).perform(click());
+        onView(
+                        allOf(
+                                withId(R.id.expand_arrow),
+                                isDescendantOfA(withId(R.id.block_third_party_incognito_with_aux))))
+                .perform(click());
+        onView(
+                        withText(
+                                R.string
+                                        .website_settings_category_cookie_block_third_party_incognito_subtitle))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.website_settings_category_cookie_subpage_incognito_bullet_two))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    @SmallTest
+    public void shouldDisplayAllowDescriptionWhenAuxButtonClicked() throws IOException {
+        onView(withId(R.id.block_third_party_incognito_with_aux)).perform(click());
+        onView(
+                        allOf(
+                                withId(R.id.expand_arrow),
+                                isDescendantOfA(withId(R.id.block_third_party_incognito_with_aux))))
+                .perform(click());
+        onView(withText(R.string.settings_cookies_block_third_party_settings_allow_bullet_one))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.settings_cookies_block_third_party_settings_allow_bullet_two))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.settings_cookies_block_third_party_settings_allow_bullet_three))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    @SmallTest
+    public void shouldDisplayBlockDescriptionAndRwsToggleWhenAuxButtonClicked() throws IOException {
+        onView(withId(R.id.block_third_party_with_aux)).perform(click());
+        onView(
+                        allOf(
+                                withId(R.id.expand_arrow),
+                                isDescendantOfA(withId(R.id.block_third_party_with_aux))))
+                .perform(click());
+        onView(withText(R.string.website_settings_category_cookie_block_third_party_subtitle))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.website_settings_category_cookie_subpage_bullet_one))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.website_settings_category_cookie_subpage_bullet_two))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.website_settings_category_cookie_rws_toggle_title))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.website_settings_category_cookie_rws_toggle_description))
+                .check(matches(isDisplayed()));
+    }
+
+    // TODO(crbug.com/370008370): Remove once AlwaysBlock3pcsIncognito launched.
     @Test
     @SmallTest
     @Feature({"RenderTest"})
-    public void testRenderCookieFPSSubpage() throws IOException {
+    @DisableFeatures({ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO})
+    public void renderBlockThirdPartyDescriptionAndRwsToggleWhenAuxButtonClicked()
+            throws IOException {
         onView(withId(R.id.block_third_party_with_aux)).perform(click());
         onView(
                         allOf(
@@ -79,7 +147,59 @@ public class CookieSettingsTest {
 
         mRenderTestRule.render(
                 getRootView(R.string.website_settings_category_cookie_block_third_party_subtitle),
-                "settings_cookie_fps_subpage");
+                "settings_cookie_rws_subpage_block_third_party");
+    }
+
+    // TODO(crbug.com/370008370): Remove once AlwaysBlock3pcsIncognito launched.
+    @Test
+    @SmallTest
+    @Feature({"RenderTest"})
+    @DisableFeatures({ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO})
+    public void renderBlockThirdPartyIncognitoDescriptionWhenAuxButtonClicked() throws IOException {
+        onView(withId(R.id.block_third_party_incognito_with_aux)).perform(click());
+        onView(
+                        allOf(
+                                withId(R.id.expand_arrow),
+                                isDescendantOfA(withId(R.id.block_third_party_incognito_with_aux))))
+                .perform(click());
+
+        mRenderTestRule.render(
+                getRootView(
+                        R.string
+                                .website_settings_category_cookie_block_third_party_incognito_subtitle),
+                "settings_cookie_rws_subpage_block_third_party_incognito");
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"RenderTest"})
+    public void renderAllowDescriptionWhenAuxButtonClicked() throws IOException {
+        onView(withId(R.id.block_third_party_incognito_with_aux)).perform(click());
+        onView(
+                        allOf(
+                                withId(R.id.expand_arrow),
+                                isDescendantOfA(withId(R.id.block_third_party_incognito_with_aux))))
+                .perform(click());
+
+        mRenderTestRule.render(
+                getRootView(R.string.website_settings_category_cookie_allow_third_party_subtitle),
+                "settings_cookie_rws_subpage_allow");
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"RenderTest"})
+    public void renderBlockDescriptionAndRwsToggleWhenAuxButtonClicked() throws IOException {
+        onView(withId(R.id.block_third_party_with_aux)).perform(click());
+        onView(
+                        allOf(
+                                withId(R.id.expand_arrow),
+                                isDescendantOfA(withId(R.id.block_third_party_with_aux))))
+                .perform(click());
+
+        mRenderTestRule.render(
+                getRootView(R.string.website_settings_category_cookie_block_third_party_subtitle),
+                "settings_cookie_rws_subpage_block");
     }
 
     private View getRootView(int text) {
