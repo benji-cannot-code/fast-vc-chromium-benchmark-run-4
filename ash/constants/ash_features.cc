143d41177ff91da39aa29e75d483770684dbd368
Kazuki Takise <takise@google.com>
2025-02-05 19:22:14 -0800

    Clean up enable-pip-tuck
    The feature was enabled on M130, and has been stable for a couple
    of milestones.
    
    BUG=b:394327675
    TEST=build
    
    Change-Id: I6dfe1e26764ff5ffc79d77a040b0099067ce49b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6232621
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416575}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2316,9 +2316,6 @@ BASE_FEATURE(kPhoneHubShortQuickActionPodsTitles,
 // Enables GIF search in Picker.
 BASE_FEATURE(kPickerGifs, "PickerGifs", base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Enables or disables the tuck feature for Picture in Picture.
-BASE_FEATURE(kPipTuck, "PipTuck", base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables or disables the preference of using constant frame rate for camera
 // when streaming.
 BASE_FEATURE(kPreferConstantFrameRate,
@@ -4204,10 +4201,6 @@ bool IsPhoneHubShortQuickActionPodsTitlesEnabled() {
 
 bool IsPinAutosubmitBackfillFeatureEnabled() {
   return base::FeatureList::IsEnabled(kQuickUnlockPinAutosubmitBackfill);
-}
-
-bool IsPipTuckEnabled() {
-  return base::FeatureList::IsEnabled(kPipTuck);
 }
 
 bool IsPrinterPreviewCrosAppEnabled() {
