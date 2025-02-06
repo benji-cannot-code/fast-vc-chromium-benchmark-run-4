9202c6d2f941d0564616fa6b0f1c52d1bfb6cdb1
Christopher Grant <cjgrant@chromium.org>
2025-02-06 06:39:21 -0800

    Page Actions: Support hiding suggestion chip
    A previous change added support for showing a page action's chip state.
    Some features need to show their chip temporarily, so we need a Hide
    equivalent.
    
    Change-Id: I6be51921634d991dadbf2ea1c20c70b92bf1689f
    Bug: 376283619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6238517
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416761}

--- chrome/browser/ui/views/page_action/mock_page_action_model.h
+++ chrome/browser/ui/views/page_action/mock_page_action_model.h
@@ -42,7 +42,7 @@ class MockPageActionModel : public PageActionModelInterface {
               (override));
   MOCK_METHOD(void,
               SetShowSuggestionChip,
-              (base::PassKey<PageActionController>),
+              (base::PassKey<PageActionController>, bool show),
               (override));
   MOCK_METHOD(void,
               SetHasPinnedIcon,
--- chrome/browser/ui/views/page_action/page_action_controller_unittest.cc
+++ chrome/browser/ui/views/page_action/page_action_controller_unittest.cc
@@ -37,6 +37,7 @@ const std::u16string kAnotherNewText = u"Another New Text";
 const std::u16string kTooltip = u"Tooltip";
 
 using ::actions::ActionItem;
+using ::testing::_;
 
 using TestPageActionModelObservation =
     ::base::ScopedObservation<PageActionModelInterface,
@@ -416,15 +417,14 @@ TEST_F(PageActionControllerMockModelTest, SetAndClearOverrideText) {
 
   // Set the text override.
   EXPECT_CALL(models().Get(kActionItemId),
-              SetOverrideText(testing::_, std::optional<std::u16string>(kText)))
+              SetOverrideText(_, std::optional<std::u16string>(kText)))
       .Times(1);
 
   controller().OverrideText(kActionItemId, kText);
 
   // Clear the text override.
-  EXPECT_CALL(
-      models().Get(kActionItemId),
-      SetOverrideText(testing::_, std::optional<std::u16string>(std::nullopt)))
+  EXPECT_CALL(models().Get(kActionItemId),
+              SetOverrideText(_, std::optional<std::u16string>(std::nullopt)))
       .Times(1);
   controller().ClearOverrideText(0);
 }
@@ -434,8 +434,7 @@ TEST_F(PageActionControllerMockModelTest, TabActivation) {
   tab_interface().Deactivate();
   controller().Initialize(tab_interface(), {kActionItemId});
 
-  EXPECT_CALL(models().Get(kActionItemId), SetTabActive(testing::_, true))
-      .Times(1);
+  EXPECT_CALL(models().Get(kActionItemId), SetTabActive(_, true)).Times(1);
   tab_interface().Activate();
 }
 
@@ -444,17 +443,21 @@ TEST_F(PageActionControllerMockModelTest, TabDeactivation) {
   tab_interface().Activate();
   controller().Initialize(tab_interface(), {kActionItemId});
 
-  EXPECT_CALL(models().Get(kActionItemId), SetTabActive(testing::_, false))
-      .Times(1);
+  EXPECT_CALL(models().Get(kActionItemId), SetTabActive(_, false)).Times(1);
   tab_interface().Deactivate();
 }
 
 TEST_F(PageActionControllerMockModelTest, ShowSuggestionChip) {
   constexpr int kActionItemId = 0;
   controller().Initialize(tab_interface(), {kActionItemId});
-  EXPECT_CALL(models().Get(kActionItemId), SetShowSuggestionChip(testing::_))
+
+  EXPECT_CALL(models().Get(kActionItemId), SetShowSuggestionChip(_, true))
       .Times(1);
   controller().ShowSuggestionChip(kActionItemId);
+
+  EXPECT_CALL(models().Get(kActionItemId), SetShowSuggestionChip(_, false))
+      .Times(1);
+  controller().HideSuggestionChip(kActionItemId);
 }
 
 }  // namespace
