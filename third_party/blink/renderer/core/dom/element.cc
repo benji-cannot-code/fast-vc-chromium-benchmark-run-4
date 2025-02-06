a03cac7e1758dac948a0510292489ae0f217f450
Daniil Sakhapov <sakhapov@chromium.org>
2025-02-06 05:43:32 -0800

    [carousel] Don't create ::scroll-marker without ::scroll-marker-group
    As per spec, when ::scroll-marker's nearest ancestor scroll container
    scroll container has a computed scroll-marker-group property that is
    not none, the pseudo-element generates a box attached as a child of
    the ::scroll-marker-group pseudo-element’s generated box on its
    nearest ancestor scroll container.
    
    So, we shouldn't create ::scroll-marker without ::scroll-marker-group.
    For this, style recalc context will keep track of if there is any
    element with scroll-marker-group property.
    
    Fixed: 383552420
    Change-Id: I7217f87677d3a6c470056eae8773ba6ae3b4c749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6229916
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416737}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4072,6 +4072,9 @@ void Element::RecalcStyle(const StyleRecalcChange change,
   }
 
   if (const ComputedStyle* style = GetComputedStyle()) {
+    child_recalc_context
+        .has_scroller_ancestor_with_scroll_marker_group_property |=
+        style->IsScrollContainer() && !style->ScrollMarkerGroupNone();
     if (style->CanMatchSizeContainerQueries(*this)) {
       // IsSuppressed() means we are at the root of a container subtree called
       // from UpdateStyleAndLayoutTreeForContainer(). If so, we can not skip
@@ -8198,6 +8201,12 @@ const ComputedStyle* Element::EnsureOwnComputedStyle(
     return nullptr;
   }
 
+  if (pseudo_element_specifier == kPseudoIdScrollMarker &&
+      !style_recalc_context
+           .has_scroller_ancestor_with_scroll_marker_group_property) {
+    return nullptr;
+  }
+
   if (const ComputedStyle* pseudo_element_style =
           element_style->GetCachedPseudoElementStyle(pseudo_element_specifier,
                                                      pseudo_argument)) {
@@ -8589,6 +8598,11 @@ PseudoElement* Element::UpdatePseudoElement(
 
   if (change.ShouldUpdatePseudoElement(*element)) {
     bool generate_pseudo = CanGeneratePseudoElement(pseudo_id);
+    if (pseudo_id == kPseudoIdScrollMarker &&
+        !style_recalc_context
+             .has_scroller_ancestor_with_scroll_marker_group_property) {
+      generate_pseudo = false;
+    }
     if (generate_pseudo) {
       if (auto* cache = GetDocument().ExistingAXObjectCache()) {
         cache->RemoveSubtree(this, /*remove_root*/ false);
@@ -8625,6 +8639,11 @@ PseudoElement* Element::CreatePseudoElementIfNeeded(
     const StyleRecalcContext& style_recalc_context,
     const AtomicString& view_transition_name) {
   if (!CanGeneratePseudoElement(pseudo_id)) {
+    return nullptr;
+  }
+  if (pseudo_id == kPseudoIdScrollMarker &&
+      !style_recalc_context
+           .has_scroller_ancestor_with_scroll_marker_group_property) {
     return nullptr;
   }
   if (pseudo_id == kPseudoIdFirstLetter) {
