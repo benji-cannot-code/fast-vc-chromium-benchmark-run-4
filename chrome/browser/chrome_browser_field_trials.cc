fe8321b97f5b60f21cf5033219d070644706c28c
Alison Gale <agale@chromium.org>
2025-02-06 09:58:09 -0800

    Reland "Clean up default prompt synthetic trial"
    This reverts commit 35519982f2c710da12003c9e7d7f52dba8e824c5.
    
    Reason for revert: Not a culprit for the original failure
    
    The original test is still failing even after the revert landed:
    https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan/34123/overview
    
    Original change's description:
    > Revert "Clean up default prompt synthetic trial"
    >
    > This reverts commit bf67d6a4f6baf7d7e602131a78d4d8d09af2a8aa.
    >
    > Reason for revert:
    > This looks causing the continuous All/StartupBrowserCreatorPickerTest.TestSetup/7 failure on win.
    > https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan
    >
    > When I checked the first failure, the blamelist has this CL:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan/34112/blamelist
    >
    > I assume it has something to do with the test:
    > https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/startup/;bpv=1
    >
    > Let me speculatively revert.
    >
    > Original change's description:
    > > Clean up default prompt synthetic trial
    > >
    > > Bug: 390650448
    > > Change-Id: I250878f3916400a2b2fc80a95170edd5deb3ba0b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227225
    > > Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    > > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > > Commit-Queue: Alison Gale <agale@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1416363}
    >
    > Bug: 390650448
    > Change-Id: Ibfc1aed207d52195541ff7af79a42119725360d1
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6238637
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Owners-Override: Yoshisato Yanagisawa <yyanagisawa@google.com>
    > Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1416633}
    
    Bug: 390650448
    Change-Id: I356a2a419993289ccb9ad0f079af1a62270d5469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6240120
    Commit-Queue: Alison Gale <agale@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416870}

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -46,10 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/startup/startup.h"  // nogncheck
 #endif
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_trial.h"
-#endif
-
 #if BUILDFLAG(IS_LINUX)
 #include "base/nix/xdg_util.h"
 #include "ui/base/ui_base_features.h"
@@ -130,10 +126,6 @@ void ChromeBrowserFieldTrials::RegisterSyntheticTrials() {
         kBackgroundThreadPoolTrial, group_name);
   }
 #endif  // BUILDFLAG(IS_ANDROID)
-
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS)
-  DefaultBrowserPromptTrial::EnsureStickToDefaultBrowserPromptCohort();
-#endif
 }
 
 #if BUILDFLAG(IS_LINUX)
