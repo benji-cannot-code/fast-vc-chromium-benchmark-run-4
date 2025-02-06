38c99a4659052e4882961b5aae37696f75579d68
David Sanders <dsanders11@ucsbalum.com>
2025-02-06 06:18:15 -0800

    Remove unused include in deep_scanning_utils.h
    Bug: 40318405
    Change-Id: I8b2b714a593a26862519818ec1214b994b5ccee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237965
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416747}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
 #include "components/enterprise/common/proto/connectors.pb.h"
-#include "components/enterprise/connectors/core/reporting_utils.h"
+#include "components/enterprise/connectors/core/common.h"
 #include "url/gurl.h"
 
 class Profile;
