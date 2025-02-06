3a5cef49a0c10574b5185fa4cc596d1a63afcace
Ramin Halavati <rhalavati@google.com>
2025-02-06 09:41:40 -0800

    Do not prune "InLineTextBox"es in proto generation for Screen2x.
    "InLineTextBox"es are not needed in Screen2x main content extraction,
    and to make the behavior consistent for all of its clients, the library
    preprocesses and removes them itself.
    
    The following changes are made accordingly:
     1. Pruning code is removed.
     2. Unittest is updated to cover the change.
     3. Minimum required version of ChromeScreenAI library is updated to
        127.15 which includes the required preprocessing.
    
    AX-Relnotes: n/a
    Bug: 359853518
    Change-Id: Id9ec3f3ab655861062e16e04a5b429bbb4a41aaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6233388
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416863}

--- chrome/browser/screen_ai/screen_ai_install_state.cc
+++ chrome/browser/screen_ai/screen_ai_install_state.cc
@@ -23,8 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
+// See crbug.com/393349281 and crbug.com/359853518
+const char kMinExpectedVersion[] = "127.15";
 const int kScreenAICleanUpDelayInDays = 30;
-const char kMinExpectedVersion[] = "127.9";
 
 bool IsDeviceCompatible() {
 #if BUILDFLAG(IS_LINUX)
