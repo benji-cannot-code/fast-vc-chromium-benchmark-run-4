b90646493d90247c6edf7007b42884761c7db79e
Radu Nitescu <radunitescu@google.com>
2025-02-06 09:32:25 -0800

    [iOS][LO] Add flag to force enable landscape support
    Change-Id: I7cae6fb33d1f8e0f2503eccc875624de0876d04a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6238238
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416854}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -241,6 +241,10 @@ BASE_FEATURE(kLensOverlayEnableIPadCompatibility,
              "EnableLensOverlayForceIPadSupport",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kLensOverlayEnableLandscapeCompatibility,
+             "EnableLensOverlayLandscapeSupport",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kLensOverlayEnableLocationBarEntrypoint,
              "LensOverlayEnableLocationBarEntrypoint",
              base::FEATURE_ENABLED_BY_DEFAULT);
