15936a13da600de5e512cc785777fc41fcbc19d3
Kouhei Ueno <kouhei@chromium.org>
2025-02-06 03:14:58 -0800

    Update a client-hints+prefetch comment and downgrade CHECK
    Bug: 394716357
    Change-Id: Ib9385f49febbffad0732cfcb0a0a94ffa5a65324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239081
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416698}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/not_fatal_until.h"
 #include "base/state_transitions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
@@ -229,7 +230,8 @@ bool SearchPrefetchRequest::StartPrefetchRequest(Profile* profile) {
   // `SearchPrefetchService::MaybePrefetchURL()` should be already prohibiting
   // this.
   CHECK(profile->GetPrefs() &&
-        profile->GetPrefs()->GetBoolean(prefs::kWebKitJavascriptEnabled));
+            profile->GetPrefs()->GetBoolean(prefs::kWebKitJavascriptEnabled),
+        base::NotFatalUntil::M136);
 
   AddClientHintsHeadersToPrefetchNavigation(
       prefetch_origin, &(resource_request->headers), profile,
