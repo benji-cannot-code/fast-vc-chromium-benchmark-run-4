57bcc560d0827966bac8e98b4749844b852165ba
Zhe Li <zheliooo@google.com>
2025-02-06 11:32:50 -0800

    [TabGroupDragDrop] add TabGroupMetadata.java
    Bug: 383125556
    Change-Id: I7e4e3551d6e458785bb9ce9807ac0b74df710da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6228159
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416934}

--- base/android/java/src/org/chromium/base/Token.java
+++ base/android/java/src/org/chromium/base/Token.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import com.google.errorprone.annotations.DoNotMock;
@@ -21,6 +24,9 @@ import org.chromium.build.annotations.NullMarked;
 @JNINamespace("base::android")
 @DoNotMock("This is a simple value object.")
 public final class Token extends TokenBase {
+    private static final String KEY_LOW = "low";
+    private static final String KEY_HIGH = "high";
+
     /** Returns a new random token using the native implementation. */
     public static Token createRandom() {
         return TokenJni.get().createRandom();
@@ -56,6 +62,28 @@ public final class Token extends TokenBase {
     @Override
     public String toString() {
         return String.format("%016X%016X", mHigh, mLow);
+    }
+
+    /**
+     * Converts this Token's data into a bundle {@link Bundle}.
+     *
+     * @return the bundle {@link Bundle} stores the Token's high and low values.
+     */
+    public Bundle toBundle() {
+        Bundle bundle = new Bundle();
+        bundle.putLong(KEY_HIGH, getHigh());
+        bundle.putLong(KEY_LOW, getLow());
+        return bundle;
+    }
+
+    /**
+     * @param bundle Bundle to be parsed.
+     * @return the deserialized Token object or null if the bundle is invalid.
+     */
+    public static @Nullable Token maybeCreateFromBundle(@Nullable Bundle bundle) {
+        if (bundle == null) return null;
+        if (!bundle.containsKey(KEY_HIGH) || !bundle.containsKey(KEY_LOW)) return null;
+        return new Token(bundle.getLong(KEY_HIGH), bundle.getLong(KEY_LOW));
     }
 
     @NativeMethods
