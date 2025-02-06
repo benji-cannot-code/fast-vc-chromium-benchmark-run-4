be54546a4f84e421ed706bb486e2f98d0e3ce623
Garima Chadha <garimachadha@microsoft.com>
2025-02-06 10:33:42 -0800

    IDB WPTs: Extend idbobjectstore_opencursor related tests to workers
    The update modifies idbobjectstore_opencursor related WPTs to run not
    only in window environments but also on dedicated, service, and shared
    workers.
    
    Change-Id: Ibc1d96253b65182cf94ce4769389b127149d89e4
    Bug: 41455766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6201393
    Auto-Submit: Garima Chadha <garimachadha@microsoft.com>
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1416897}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbobjectstore_index.any.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: title=IDBObjectStore.index() - returns an index
+// META: script=resources/support.js
+
+'use strict';
+
+async_test(t => {
+  let db;
+
+  let open_rq = createdb(t);
+  open_rq.onupgradeneeded = t.step_func(e => {
+    db = e.target.result;
+
+    db.createObjectStore('store').createIndex('index', 'indexedProperty');
+  });
+
+  open_rq.onsuccess = t.step_func(e => {
+    let index =
+        db.transaction('store', 'readonly').objectStore('store').index('index');
+
+    assert_true(index instanceof IDBIndex, 'instanceof IDBIndex');
+    t.done();
+  });
+});
---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbobjectstore_openCursor_invalid.any.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: title=IDBObjectStore.openCursor() - invalid
+// META: script=resources/support.js
+
+'use strict';
+
+indexeddb_test(
+    function(t, db, tx) {
+      let objStore = db.createObjectStore('test');
+      objStore.createIndex('index', '');
+
+      objStore.add('data', 1);
+      objStore.add('data2', 2);
+    },
+    function(t, db, tx) {
+      let idx =
+          db.transaction('test', 'readonly').objectStore('test').index('index');
+
+      assert_throws_dom('DataError', function() {
+        idx.openCursor({lower: 'a'});
+      });
+
+      assert_throws_dom('DataError', function() {
+        idx.openCursor({lower: 'a', lowerOpen: false});
+      });
+
+      assert_throws_dom('DataError', function() {
+        idx.openCursor(
+            {lower: 'a', lowerOpen: false, upper: null, upperOpen: false});
+      });
+
+      t.done();
+    },
+    'IDBObjectStore.openCursor() - invalid - pass something other than number');
