4c7b445ee7c44d66e416b95d5aa7c4bc8aa94e66
Yuheng Huang <yuhengh@chromium.org>
2025-02-06 14:30:21 -0800

    Toast controller: fix global toast not dismissed
    This happens on any global scoped toast. ClearTabScopedToasts() for
    global scoped toast.
    
    Bug: 394906012
    Change-Id: I769d3f00c7502ac7ee6aec6cfcdaacb96dcb9edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6240085
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417023}

--- chrome/browser/ui/toasts/toast_controller.cc
+++ chrome/browser/ui/toasts/toast_controller.cc
@@ -346,7 +346,6 @@ void ToastController::OnFullscreenStateChanged() {
 }
 
 void ToastController::ClearTabScopedToasts() {
-  toast_close_timer_.Stop();
   if (next_toast_params_.has_value()) {
     const ToastId toast_id = next_toast_params_.value().toast_id;
     const ToastSpecification* const specification =
@@ -361,6 +360,7 @@ void ToastController::ClearTabScopedToasts() {
       !toast_registry_
            ->GetToastSpecification(currently_showing_toast_id_.value())
            ->is_global_scope()) {
+    toast_close_timer_.Stop();
     CloseToast(toasts::ToastCloseReason::kAbort);
   }
 }
