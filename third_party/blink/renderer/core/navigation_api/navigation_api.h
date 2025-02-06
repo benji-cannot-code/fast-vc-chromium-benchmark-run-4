b406bfaa83f9fe9c90a17903c09d11d6078326e5
Nate Chapin <japhet@chromium.org>
2025-02-06 11:10:31 -0800

    Navigation API: window.stop() after-transition traversal should actually block the traversal
    If a traversal is intercepted by the navigate event, and the
    intercept() sets { commit: 'after-transition' } (which defers
    commit), and the navigation is aborted via window.stop() or the UI
    stop button before the traversal is committed, the browser session
    history and the navigation API view of the session history will get
    out of sync (the browser will have its offset as though the
    traversal committed, but the commit didn't happen and the navigation
    API does not update its current position).
    
    Fixed: 394772469
    Change-Id: I2ab67c7d6b5f0116dde98c5196e0ff0822a2c15e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227767
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416923}

--- third_party/blink/renderer/core/navigation_api/navigation_api.h
+++ third_party/blink/renderer/core/navigation_api/navigation_api.h
@@ -147,7 +147,7 @@ class CORE_EXPORT NavigationApi final : public EventTarget {
       const mojom::blink::NavigationApiHistoryEntryPtr&);
   void PopulateKeySet();
   void UpdateActivation(HistoryItem* previous_entry, WebFrameLoadType);
-  void AbortOngoingNavigation(ScriptState*);
+  void AbortOngoingNavigation(ScriptState*, CancelNavigationReason);
   void DidFinishOngoingNavigation();
   void DidFailOngoingNavigation(ScriptValue);
 
