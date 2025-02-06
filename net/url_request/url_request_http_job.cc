b85386166b004e929ebc142b0fd0d74474f687b0
thefrog <thefrog@chromium.org>
2025-02-06 07:41:11 -0800

    Add some helper comments for DBSC in url_request_http_job.cc
    Change-Id: I0824237b550fd3d4b895fb75e53a1a1165951c86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237154
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416794}

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -939,8 +939,8 @@ void URLRequestHttpJob::SetCookieHeaderAndStart(
   if (service) {
     std::optional<device_bound_sessions::Session::Id> id =
         service->GetAnySessionRequiringDeferral(request_);
-    // If the request needs to be deferred while waiting for refresh,
-    // do not start the transaction at this time.
+    // If the request needs to be deferred while waiting for refresh, do not
+    // start the transaction at this time. This may also kick off a refresh.
     if (id) {
       service->DeferRequestForRefresh(
           request_, *id,
@@ -1149,6 +1149,9 @@ void URLRequestHttpJob::ProcessDeviceBoundSessionsHeader() {
     return;
   }
 
+  // If response header Sec-Session-Registration is present and configured
+  // appropriately, trigger a registration request per header value to attempt
+  // to create a new session.
   const auto& request_url = request_->url();
   auto* headers = GetResponseHeaders();
   std::vector<device_bound_sessions::RegistrationFetcherParam> params =
@@ -1160,6 +1163,10 @@ void URLRequestHttpJob::ProcessDeviceBoundSessionsHeader() {
         request_->isolation_info(), request_->net_log(), request_->initiator());
   }
 
+  // If response header Sec-Session-Challenge is present and configured
+  // appropriately, for each header value, store the challenge in advance for
+  // the next relevant refresh request that gets triggered. This is to help
+  // avoid a round-trip for when the next refresh request is required.
   std::vector<device_bound_sessions::SessionChallengeParam> challenge_params =
       device_bound_sessions::SessionChallengeParam::CreateIfValid(request_url,
                                                                   headers);
