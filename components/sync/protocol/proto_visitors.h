acb83de89af2f26580299cfc12b117e48afd7660
Viplav Kadam <viplavkadam@google.com>
2025-02-06 12:37:18 -0800

    [BNPL][Chrome] Update PaymentInstrumentCreationOption proto
    This CL enhances flexibility by replacing BnplIssuerDetails with
    BnplCreationOption in PaymentInstrumentCreationOption proto.
    BnplCreationOption and BnplIssuerDetails were originally designed as
    separate protos. During initial implementation, however, they were
    combined due to identical proto representations and the expectation
    of no near-term divergence. Recently, the need to add a field for the
    issuer (Klarna) has required us to separate the protos again to enable
    independent evolution.
    
    Bug: 393457903
    Change-Id: I96359df6dde6fc5ab9aeb4a10ff1ae6c479d659c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219811
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416968}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -336,6 +336,11 @@ VISIT_PROTO_FIELDS(const sync_pb::AutofillWalletSpecifics& proto) {
   VISIT(payment_instrument_creation_option);
 }
 
+VISIT_PROTO_FIELDS(const sync_pb::BnplCreationOption& proto) {
+  VISIT(issuer_id);
+  VISIT_REP(eligible_price_range);
+}
+
 VISIT_PROTO_FIELDS(const sync_pb::BnplIssuerDetails& proto) {
   VISIT(issuer_id);
   VISIT_REP(eligible_price_range);
