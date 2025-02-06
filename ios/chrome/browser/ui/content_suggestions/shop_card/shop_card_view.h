a0c61ad173a1842b7c2e8864fd61cab2de33c275
Gloria Fang <gloriafang@google.com>
2025-02-06 11:55:30 -0800

    Add mediator and view for ShopCard
    Bug: 374170663
    Change-Id: Ied62c1e99cf07800118f3f8f1a281be59ffbb211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6232968
    Auto-Submit: Gloria Fang <gloriafang@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416945}

---
+++ ios/chrome/browser/ui/content_suggestions/shop_card/shop_card_view.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SHOP_CARD_SHOP_CARD_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SHOP_CARD_SHOP_CARD_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+@protocol ShopCardCommands;
+@class ShopCardItem;
+
+@interface ShopCardModuleView : UIView
+
+- (instancetype)initWithFrame;
+
+// Configures this view with `config`.
+- (void)configureView:(ShopCardItem*)config;
+
+// Command handler for user events.
+@property(nonatomic, weak) id<ShopCardCommands> commandHandler;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SHOP_CARD_SHOP_CARD_VIEW_H_
---
+++ ios/chrome/browser/ui/content_suggestions/shop_card/shop_card_view.mm
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/content_suggestions/shop_card/shop_card_view.h"
+
+#import "ios/chrome/browser/ui/content_suggestions/shop_card/shop_card_data.h"
+#import "ios/chrome/browser/ui/content_suggestions/shop_card/shop_card_item.h"
+
+@implementation ShopCardModuleView {
+}
+
+- (instancetype)initWithFrame {
+  self = [super initWithFrame:CGRectZero];
+  return self;
+}
+
+- (void)configureView:(ShopCardItem*)config {
+  if (config.shopCardData.shopCardItemType ==
+      ShopCardItemType::kPriceDropOnTab) {
+    // TODO: crbug.com/394638800 - render correct view when data available
+  } else if (config.shopCardData.shopCardItemType ==
+             ShopCardItemType::kReviews) {
+    // TODO: crbug.com/394638800 - render correct view when data available
+  }
+}
+
+@end
