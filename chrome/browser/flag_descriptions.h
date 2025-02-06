3376df805708ee9c7c908a0bfb127b9e16a47591
Juan Mojica <juanmojica@google.com>
2025-02-06 10:26:02 -0800

    [lensoverlay] Add simplified selection flag to chrome://flags.
    This flag currently does nothing.
    
    Change-Id: Idd873be50988808406db8774be863bad46b5c1c0
    Bug: 394375754
    Fixed: 394375754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6236717
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416891}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1348,6 +1348,9 @@ extern const char kLensOverlayImageContextMenuActionsDescription[];
 extern const char kLensOverlaySidePanelOpenInNewTabName[];
 extern const char kLensOverlaySidePanelOpenInNewTabDescription[];
 
+extern const char kLensOverlaySimplifiedSelectionName[];
+extern const char kLensOverlaySimplifiedSelectionDescription[];
+
 extern const char kLensOverlayTranslateButtonName[];
 extern const char kLensOverlayTranslateButtonDescription[];
 
