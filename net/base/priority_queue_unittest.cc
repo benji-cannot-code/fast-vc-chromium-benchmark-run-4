5d0e6bdc2011d02e2774ee2d6bd2b3c36c42f03e
Matt Menke <mmenke@chromium.org>
2025-02-06 15:15:44 -0800

    Remove unsafe buffers pragmas from some files in net/base.
    Of these files, only priority_queue_unittest.cc still needed its
    pragma, and it was a one-line fix.
    
    Bug: 390223051, 40284755
    Change-Id: Id5e7783efba7306307c91148d304c9f7ddda07f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237321
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417046}

--- net/base/priority_queue_unittest.cc
+++ net/base/priority_queue_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "net/base/priority_queue.h"
 
 #include <array>
@@ -93,7 +88,7 @@ class PriorityQueueTest : public testing::TestWithParam<size_t> {
 
  protected:
   PriorityQueue<int> queue_;
-  PriorityQueue<int>::Pointer pointers_[kNumElements];
+  std::array<PriorityQueue<int>::Pointer, kNumElements> pointers_;
 };
 
 TEST_P(PriorityQueueTest, AddAndClear) {
