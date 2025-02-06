1a3124d2adb3405bf71c276c0c2b355ddfaada26
Shuhei Takahashi <nya@chromium.org>
2025-02-05 16:29:34 -0800

    extensions: Enable activityLogPrivate API for desktop Android
    Bug: 392777363
    Cq-Include-Trybots: luci.chromium.try:android-desktop-15-x64-rel
    Change-Id: I31f770bae86e1391265f5801d90c86ad8b7f5369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6232012
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416529}

--- chrome/browser/extensions/activity_log/activity_log.cc
+++ chrome/browser/extensions/activity_log/activity_log.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/preloading/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
