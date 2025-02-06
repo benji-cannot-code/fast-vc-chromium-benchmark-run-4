7d9f844aaf86357ca81d3636f8c49c581bdcc94f
Ginny Huang <ginnyhuang@chromium.org>
2025-02-06 01:16:41 -0800

    Remove obsolete NotFatalUntil in BrowserCoordinator
    Original issue was fixed by https://crrev.com/c/1157772 and verified in
    M133. Replace DWC with `CHECK` to be alerted for future issues.
    
    Change-Id: I9a67767edc0d498dd9232ae985f2344a904209d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6235826
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1416658}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -2309,11 +2309,7 @@ enum class ToolbarKind {
 #pragma mark - BrowserViewVisibilityConsumer
 
 - (void)browserViewDidChangeVisibility {
-  // TODO(crbug.com/377763682): This is a temporary fix to avoid a crash.
-  if (!self.browser) {
-    NOTREACHED(base::NotFatalUntil::M133);
-    return;
-  }
+  CHECK(self.browser);
   raw_ptr<TabBasedIPHBrowserAgent> tabBasedIPHBrowserAgent =
       TabBasedIPHBrowserAgent::FromBrowser(self.browser);
   if (!tabBasedIPHBrowserAgent) {
