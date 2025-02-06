5ce5651e23955fdcd10a023111cf3d249b502511
Michelle Chen <michellegc@google.com>
2025-02-06 15:21:15 -0800

    scanner: Update icon for action container error view.
    Use the error icon specified in b:378582420#comment1.
    
    Bug: b:378582420
    Change-Id: I7ba4bb4ce3758f945b8eaee29ce91f44b28677cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6238198
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1417051}

--- ash/capture_mode/action_button_container_view.cc
+++ ash/capture_mode/action_button_container_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/capture_mode/capture_mode_session_focus_cycler.h"
 #include "ash/capture_mode/capture_mode_types.h"
 #include "ash/capture_mode/capture_mode_util.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/system_shadow.h"
 #include "ash/style/typography.h"
@@ -43,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/link.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
-#include "ui/views/vector_icons.h"
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/view_utils.h"
@@ -105,15 +105,16 @@ ActionButtonContainerView::ErrorView::ErrorView()
       this, kErrorViewCornerRadius,
       views::HighlightBorder::Type::kHighlightBorderNoShadow);
 
-  AddChildView(views::Builder<views::ImageView>()
-                   .SetPreferredSize(gfx::Size(kErrorViewLeadingIconSize,
-                                               kErrorViewLeadingIconSize))
-                   .SetImage(ui::ImageModel::FromVectorIcon(
-                       views::kInfoIcon, cros_tokens::kCrosSysSecondary))
-                   .SetProperty(views::kMarginsKey,
-                                gfx::Insets::TLBR(
-                                    0, 0, 0, kErrorViewLeadingIconRightPadding))
-                   .Build());
+  AddChildView(
+      views::Builder<views::ImageView>()
+          .SetPreferredSize(
+              gfx::Size(kErrorViewLeadingIconSize, kErrorViewLeadingIconSize))
+          .SetImage(ui::ImageModel::FromVectorIcon(
+              kCaptureModeActionErrorIcon, cros_tokens::kCrosSysSecondary))
+          .SetProperty(
+              views::kMarginsKey,
+              gfx::Insets::TLBR(0, 0, 0, kErrorViewLeadingIconRightPadding))
+          .Build());
 
   AddChildView(
       views::Builder<views::Label>()
