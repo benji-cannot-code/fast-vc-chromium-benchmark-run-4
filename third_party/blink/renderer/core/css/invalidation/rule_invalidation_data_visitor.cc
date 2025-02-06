5df9bcbfc32792caa62b552e5af084aea4981d80
Mason Freed <masonf@chromium.org>
2025-02-06 09:47:09 -0800

    Fix an invalidation issue with `:has-interest` [11/N]
    Previously, showing interest via the keyboard on multiple elements
    resulted in all of them still showing `:has-interest` styles. The
    problem was that in addition to PseudoStateChanged(), it is also
    required to call SetNeedsStyleRecalc(), always.
    
    This CL also fixes up two cases where an interest invoker is re-hovered
    while it still has interest. Tests are added for both such cases.
    
    Bug: 326681249
    Change-Id: Ia545096c2f53442fe6433c3416592c1742d98181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219717
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416866}

--- third_party/blink/renderer/core/css/invalidation/rule_invalidation_data_visitor.cc
+++ third_party/blink/renderer/core/css/invalidation/rule_invalidation_data_visitor.cc
@@ -1677,6 +1677,7 @@ RuleInvalidationDataVisitor<VisitorType>::InvalidationSetForSimpleSelector(
       case CSSSelector::kPseudoSelectorFragmentAnchor:
       case CSSSelector::kPseudoActiveViewTransition:
       case CSSSelector::kPseudoActiveViewTransitionType:
+      case CSSSelector::kPseudoHasInterest:
       case CSSSelector::kPseudoHasSlotted:
         return EnsurePseudoInvalidationSet(selector.GetPseudoType(), type,
                                            position, in_nth_child);
