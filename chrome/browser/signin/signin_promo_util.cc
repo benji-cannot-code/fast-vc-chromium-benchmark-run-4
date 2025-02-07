a1048f21727afdb4d766b8fc18010fb7a8e4d54c
Liza Bipin <mlbipin@google.com>
2025-02-07 01:37:12 -0800

    Reland "Remove kExplicitBrowserSigninUIOnDesktop from c/b/signin"
    This is a reland of commit 329c177c0f1e0ddf2c10a50698285ba1002b030f
    CloseBrowserWhileInitializingSyncConfirmation has been disabled as
    it is flaky. This test will be re-enabled in a follow up CL
    
    Original change's description:
    > Remove kExplicitBrowserSigninUIOnDesktop from c/b/signin
    >
    > * Removes both the flag and uses of
    > IsExplicitBrowserSigninUIOnDesktopEnabled()
    >
    > Bug: b:344531408
    > Change-Id: I052178bed35bf467359f8089d82be3562e20dfcf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6214210
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Liza Bipin <mlbipin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1414026}
    
    Bug: b:344531408
    Change-Id: Ib0327f7c982e6b681bc7ed7bd14d859c0f8996bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239777
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1417211}

--- chrome/browser/signin/signin_promo_util.cc
+++ chrome/browser/signin/signin_promo_util.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/base/signin_prefs.h"
-#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
 #include "net/base/network_change_notifier.h"
@@ -117,8 +116,7 @@ bool ShouldShowSignInPromoCommon(Profile& profile,
     return false;
   }
 
-  // Only show the promo if explicit browser signin is enabled.
-  return switches::IsExplicitBrowserSigninUIOnDesktopEnabled();
+  return true;
 }
 
 bool ShouldShowPromoBasedOnImpressionCount(Profile& profile,
@@ -250,10 +248,6 @@ bool ShouldShowPasswordSignInPromo(Profile& profile) {
 bool ShouldShowAddressSignInPromo(Profile& profile,
                                   const autofill::AutofillProfile& address) {
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
-  // Remove this once enabled by default.
-  if (!switches::IsImprovedSigninUIOnDesktopEnabled()) {
-    return false;
-  }
 
   if (!ShouldShowSignInPromoCommon(profile,
                                    AutofillSignInPromoType::kAddress)) {
