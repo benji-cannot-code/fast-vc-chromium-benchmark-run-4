e915432445fcd6f45d52b05dbe5e414e60ff7d50
jhtin <jhtin@google.com>
2025-02-06 20:35:39 -0800

    scanner: Rename CanShowConsentScreenEntryPoints to CanShowUI
    Since all UI can normally be shown without consent acceptance, this
    makes it more obvious what the result of the boolean is.
    
    For any UI that requires consent acceptance, we should be using
    CanStartSession()
    
    Bug: b:381767664
    Change-Id: I34298e67546a51084c720d4b0c6889a63ee2b3b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6231831
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Jon Htin <jhtin@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417150}

--- ash/scanner/scanner_controller.cc
+++ ash/scanner/scanner_controller.cc
@@ -343,7 +343,7 @@ void ScannerController::OnActiveUserSessionChanged(
   command_delegate_ = nullptr;
 }
 
-bool ScannerController::CanShowConsentScreenEntryPoints() {
+bool ScannerController::CanShowUi() {
   ScannerProfileScopedDelegate* profile_scoped_delegate =
       delegate_->GetProfileScopedDelegate();
 
