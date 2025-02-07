37f7065cab6d0d4374943f0056222e9987be9df4
Oleksandr Tara <otara@google.com>
2025-02-07 02:23:52 -0800

    Set request criticality in Affiliation requests
    Tested the change locally and the criticality is propagated as intended: https://screenshot.googleplex.com/BLhnrxk87mmeAEL.
    
    Bug: 371938601
    Change-Id: Idca02feaadb8a109ad491c61e2006d2cce138ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239794
    Auto-Submit: Oleksandr Tara <otara@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1417229}

--- components/affiliations/core/browser/hash_affiliation_fetcher_unittest.cc
+++ components/affiliations/core/browser/hash_affiliation_fetcher_unittest.cc
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/gmock_move_support.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "components/affiliations/core/browser/affiliation_api.pb.h"
 #include "components/affiliations/core/browser/affiliation_service_impl.h"
 #include "components/affiliations/core/browser/affiliation_utils.h"
-#include "components/affiliations/core/browser/affiliation_api.pb.h"
 #include "components/affiliations/core/browser/mock_affiliation_fetcher_delegate.h"
 #include "components/variations/scoped_variations_ids_provider.h"
 #include "crypto/sha2.h"
@@ -129,6 +129,9 @@ void HashAffiliationFetcherTest::VerifyRequestPayload(
       "application/x-protobuf",
       intercepted_headers_.GetHeader(net::HttpRequestHeaders::kContentType)
           .value_or(std::string()));
+  EXPECT_EQ("CgIIAQ==",
+            intercepted_headers_.GetHeader("x-goog-ext-174067345-bin")
+                .value_or(std::string()));
   EXPECT_THAT(actual_hash_prefixes,
               testing::UnorderedElementsAreArray(expected_hash_prefixes));
   EXPECT_EQ(request.mask().change_password_info(),
