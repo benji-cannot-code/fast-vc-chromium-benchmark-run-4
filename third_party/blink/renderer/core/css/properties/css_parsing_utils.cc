89d7345119d450111d626d9d006508be96313697
CGQAQ <m.jason.liu@gmail.com>
2025-02-07 00:32:27 -0800

    Allow gradient to have one single stop
    Allow gradients with a single color stop and 0-1 positions as per
    css-images-4 says.
    Affected gradients are:
      - linear-gradient
      - -webkit-linear-gradient
      - repeating-linear-gradient
      - -webkit-repeating-linear-gradient
      - radial-gradient
      - -webkit-radial-gradient
      - repeating-radial-gradient
      - -webkit-repeating-radial-gradient
      - conic-gradient
      - repeating-conic-gradient
    
    Bug: 344620213
    Ref: https://drafts.csswg.org/css-images-3/#color-stop-syntax
    Ref: https://github.com/w3c/csswg-drafts/issues/10092
    Ref: https://bugzilla.mozilla.org/show_bug.cgi?id=1900530
    Change-Id: I589497e9e333cf0ea099d06609c03873550c1422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197018
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417190}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -2714,8 +2714,8 @@ static bool ConsumeGradientColorStops(CSSParserTokenStream& stream,
     return false;
   }
 
-  // Must have 2 or more stops to be valid.
-  return gradient->StopCount() >= 2;
+  // Must have 1 or more stops to be valid.
+  return gradient->StopCount() >= 1;
 }
 
 static CSSValue* ConsumeDeprecatedRadialGradient(
