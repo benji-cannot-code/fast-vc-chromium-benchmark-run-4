a31dac1929cfed3e304cee3973f2c4fea7fd7034
Mounir Lamouri <mounir@meta.com>
2025-02-07 11:07:11 -0800

    ImageReaderGLOwner: fix tests always expecting 3 max images.
    When features::IncreaseBufferCountForHighFrameRate() returns true, the
    value will be 5 instead of 3. This is fixing the tests to be more
    flexible.
    
    Bug: None
    Change-Id: Ifbc3c40ac164d52e66cb496ce707e3b86ebd27b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6241994
    Commit-Queue: Mounir Lamouri <mounir@meta.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417463}

--- gpu/command_buffer/service/image_reader_gl_owner_unittest.cc
+++ gpu/command_buffer/service/image_reader_gl_owner_unittest.cc
@@ -206,7 +206,7 @@ TEST_F(ImageReaderGLOwnerSecureSurfaceControlTest, MaxImageExpectation) {
     return;
   EXPECT_EQ(static_cast<ImageReaderGLOwner*>(image_reader_.get())
                 ->max_images_for_testing(),
-            3);
+            features::IncreaseBufferCountForHighFrameRate() ? 5 : 3);
 }
 
 class ImageReaderGLOwnerInsecureSurfaceControlTest
@@ -224,7 +224,7 @@ TEST_F(ImageReaderGLOwnerInsecureSurfaceControlTest, MaxImageExpectation) {
     return;
   EXPECT_EQ(static_cast<ImageReaderGLOwner*>(image_reader_.get())
                 ->max_images_for_testing(),
-            3);
+            features::IncreaseBufferCountForHighFrameRate() ? 5 : 3);
 }
 
 }  // namespace gpu
