d98eb3e43799cfe4461b840dcca2e338db106d32
Ivan Murashov <ivan.murashov@lge.com>
2025-02-06 20:47:24 -0800

    IWYU: Add missing include for std::optional usage in lens_overlay_request_id_generator.h
    In the CL https://crrev.com/c/6048364 used std::optional in the
    chrome/browser/ui/lens/lens_overlay_request_id_generator.h, but
    corresponding include was missed.
    Added missing include for std::optional usage.
    
    Bug: 41455655
    Change-Id: I8a0cec0fb7af745de53e7555f054d6299b905f0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6236984
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1417154}

--- chrome/browser/ui/lens/lens_overlay_request_id_generator.h
+++ chrome/browser/ui/lens/lens_overlay_request_id_generator.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_LENS_LENS_OVERLAY_REQUEST_ID_GENERATOR_H_
 #define CHROME_BROWSER_UI_LENS_LENS_OVERLAY_REQUEST_ID_GENERATOR_H_
 
+#include <optional>
+
 #include "third_party/lens_server_proto/lens_overlay_routing_info.pb.h"
 #include "third_party/lens_server_proto/lens_overlay_server.pb.h"
 #include "third_party/lens_server_proto/lens_overlay_service_deps.pb.h"
