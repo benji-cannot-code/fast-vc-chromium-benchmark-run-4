242c68a0e2d2c7ec95d5f3aeccf5750d8131d534
Matthew Andrews <mattandr@microsoft.com>
2025-02-07 07:02:39 -0800

    Shared image handling for blink::VideoEncoder
    This change allows blink::VideoEncoder to supply shared image backed
    frames to a supporting VideoEncodeAccelerator without readback.  In
    addition to checking for support from the VEA, VideoEncoder also
    needs to verify the sync token so it can go across process.
    
    As part of this, VideoFrame's UpdateAcquireSyncToken function is updated
    to be a simple thread-safe setter for acquire_sync_token_.  This allows
    VideoEncoder to update the sync token with the verified token.  Per
    Vasiliy's comments on a previous change, waiting for the sync token is
    unnecessary here because it is not possible to generate a new sync
    token without waiting on the previous one.
    
    Bug: 40162806
    Change-Id: I3b2b5b99a7417b3db104b67480870e31d3fc3ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6077294
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Commit-Queue: Matthew Andrews <mattandr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1417315}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.h
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_color_space.h"
 #include "media/base/video_encoder.h"
 #include "media/base/video_frame_pool.h"
+#include "media/video/video_encoder_info.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_encoded_video_chunk_output_callback.h"
 #include "third_party/blink/renderer/modules/webcodecs/encoder_base.h"
@@ -191,6 +192,9 @@ class MODULES_EXPORT VideoEncoder : public EncoderBase<VideoEncoderTraits> {
   // The color space corresponding to the last emitted output. Used to update
   // emitted VideoDecoderConfig when necessary.
   gfx::ColorSpace last_output_color_space_;
+
+  // Latest VideoEncoderInfo reported by encoder
+  media::VideoEncoderInfo encoder_info_;
 };
 
 }  // namespace blink
