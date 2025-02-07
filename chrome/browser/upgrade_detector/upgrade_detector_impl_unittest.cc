a51df2e6cbed369745b4f8020ba57522fd2189a5
Greg Thompson <grt@chromium.org>
2025-02-07 06:57:10 -0800

    [upgrade_detector] Fix crash in unit tests for official builds
    The network time tracker (which the outdated build detector uses to get
    the current time) can't be used from unit tests, so suppress the
    detector.
    
    Fixed: 394952791
    Change-Id: I61515f69604e15ad0ea466419bb0a06527a45764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244277
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417311}

--- chrome/browser/upgrade_detector/upgrade_detector_impl_unittest.cc
+++ chrome/browser/upgrade_detector/upgrade_detector_impl_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/google/google_brand.h"
 #include "chrome/browser/upgrade_detector/installed_version_poller.h"
 #include "chrome/browser/upgrade_detector/upgrade_observer.h"
 #include "chrome/common/pref_names.h"
@@ -202,6 +203,9 @@ class UpgradeDetectorImplTest : public ::testing::Test {
   }
 
  private:
+  // Override the brand code so that the test appears to be a non-organic brand
+  // in order to suppress the outdated build detector.
+  google_brand::BrandForTesting non_organic_{"BBBB"};
   content::BrowserTaskEnvironment task_environment_;
   ScopedTestingLocalState scoped_local_state_;
   InstalledVersionPoller::ScopedDisableForTesting scoped_poller_disabler_;
