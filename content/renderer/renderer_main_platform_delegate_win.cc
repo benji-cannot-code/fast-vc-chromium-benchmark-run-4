a4f734292c82880caba224805bb0a4c84c1555f0
Alex Gough <ajgo@chromium.org>
2025-02-07 11:35:29 -0800

    Hook dwrite.__imp_GetUserDefaultLCID for Csrss Lockdown
    Dwrite tracing functions call GetUserDefaultLCID() which breaks
    when Csrss Lockdown is enabled. This CL adds an import table (IAT)
    hook to dwrite during renderer warmup when Csrss lockdown is
    enabled.
    
    As dwrite imports GetUserDefaultLCID from an apiset we must
    attempt to hook all versions of the apiset. So far two of the
    three published versions of the apiset have been observed in
    dwrite.dll in the wild.
    
    This CL adds tests of the warmup infrastructure, both this
    hook, and randomness warmup, to ensure they continue to work
    in future versions of Windows.
    
    Bug: 384336650
    Change-Id: Ia015117fc5c51d16f4cfdcc1daade408c257226f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226669
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417488}

--- content/renderer/renderer_main_platform_delegate_win.cc
+++ content/renderer/renderer_main_platform_delegate_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "content/child/dwrite_font_proxy/dwrite_font_proxy_init_impl_win.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/renderer/render_thread.h"
 #include "content/renderer/render_thread_impl.h"
+#include "sandbox/policy/features.h"
 #include "sandbox/policy/switches.h"
 #include "sandbox/policy/win/sandbox_warmup.h"
 #include "sandbox/win/src/sandbox.h"
@@ -73,6 +75,12 @@ bool RendererMainPlatformDelegate::EnableSandbox() {
 
   if (target_services) {
     sandbox::policy::WarmupRandomnessInfrastructure();
+
+    if (base::FeatureList::IsEnabled(
+            sandbox::policy::features::kEnableCsrssLockdown)) {
+      bool hooked = sandbox::policy::HookDwriteGetUserDefaultLCID();
+      base::UmaHistogramBoolean("Process.Sandbox.DwriteHookStatus", hooked);
+    }
 
     target_services->LowerToken();
     return true;
