a169355e787dfa5fed71aab6a6cfdeacf5b0cc8f
S. Ganesh <ganesh@chromium.org>
2025-02-06 18:38:38 -0800

    updater: Add an integration test with `lang` for the metainstaller UI
    Bug: 385004395
    Change-Id: I58fad7e9acb08c86f4b8c546d6c717e0783db859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6241382
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417130}

--- chrome/updater/test/integration_test_commands_system.cc
+++ chrome/updater/test/integration_test_commands_system.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/base64.h"
 #include "base/base_paths.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
@@ -656,7 +657,8 @@ class IntegrationTestCommandsSystem : public IntegrationTestCommands {
     base::CommandLine helper_command(path);
     helper_command.AppendSwitch(command_switch);
     for (const Param& param : params) {
-      helper_command.AppendSwitchASCII(param.name, param.value);
+      helper_command.AppendSwitchASCII(param.name,
+                                       base::Base64Encode(param.value));
     }
 
     // Avoids the test runner banner about test debugging.
