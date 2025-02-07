936a7eb6549413a8843271efff8f776974d6f805
Christian Xu <christianxu@google.com>
2025-02-07 11:11:28 -0800

    [iOS][Omnibox] Add MobileOmniboxClipboardToURL to actions.xml
    Add MobileOmniboxClipboardToURL in actions.xml.
    
    Fixed: 326989399
    Change-Id: Ib2b9c4e0b257832bda529d7e30465e44c4094e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6234989
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417467}

--- ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
+++ ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
@@ -109,8 +109,6 @@ using base::UserMetricsAction;
   AutocompleteMatch matchCopy = match;
 
   if (matchCopy.type == AutocompleteMatchType::CLIPBOARD_URL) {
-    // TODO(crbug.com/326989399): MobileOmniboxClipboardToURL action is not
-    // defined in actions.xml
     base::RecordAction(UserMetricsAction("MobileOmniboxClipboardToURL"));
     UMA_HISTOGRAM_LONG_TIMES_100(
         "MobileOmnibox.PressedClipboardSuggestionAge",
