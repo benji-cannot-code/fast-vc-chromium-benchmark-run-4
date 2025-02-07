42cff46b4851699bdc4ef9d8beb0eab6e4da4e3b
Ivan Murashov <ivan.murashov@lge.com>
2025-02-06 20:51:09 -0800

    IWYU: Add missing include for base::StringPrintf usage in ntp_microsoft_auth_untrusted_ui.cc
    In the file ntp_microsoft_auth_untrusted_ui.cc the method
    base::StringPrintf is used without corresponding include.
    Added missing include.
    
    Bug: none
    Change-Id: I4b0199a74941ce9be95bd51ebc044e63fc0c607e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239624
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1417155}

--- chrome/browser/ui/webui/ntp_microsoft_auth/ntp_microsoft_auth_untrusted_ui.cc
+++ chrome/browser/ui/webui/ntp_microsoft_auth/ntp_microsoft_auth_untrusted_ui.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ntp_microsoft_auth/ntp_microsoft_auth_untrusted_ui.h"
 
+#include "base/strings/stringprintf.h"
 #include "chrome/browser/new_tab_page/new_tab_page_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h"
