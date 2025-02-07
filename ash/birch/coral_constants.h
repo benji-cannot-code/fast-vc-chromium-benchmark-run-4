7d3c952048d274b17df9b27c720607e46c008e1c
Xiaodan Zhu <zxdan@chromium.org>
2025-02-06 19:20:39 -0800

    coral: filter the tab and app with an empty title in the request
    We observed empty titled tab items in the selector and since the
    selector item is using the title as its accessibility name, the empty
    title may cause crash.
    
    Currently, we decide to filter the items with empty titled entities and
    add metrics to observe the frequency.
    
    Test: manual test
    Bug: b:393665095
    Change-Id: Ie8584d856f734be2e6ab85d6ed797526a956c25a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6236695
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417137}

--- ash/birch/coral_constants.h
+++ ash/birch/coral_constants.h
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
 enum class ASH_EXPORT CoralSource {
   kUnknown,    // the initial unset source value.
   kPostLogin,  // the source of the item is from post-login coral response.
   kInSession,  // the source of the item is from Overview session response.
+  kMaxValue = kInSession,
 };
 
 }  // namespace ash
