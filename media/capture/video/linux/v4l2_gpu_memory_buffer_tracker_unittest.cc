e8b8f5b3f780dc689bfe484e70c6d7924293ad78
vikas soni <vikassoni@chromium.org>
2025-02-06 16:23:01 -0800

    [MappableSI] Remove MockGpuMemoryBufferManager.
    Remove MockGpuMemoryBufferManager and all of its usages.
    It is no longer used by any clients or tests since all
    were converted to use MappableSI instead of GMB.
    
    Bug: 394919676
    Change-Id: I8453de6c8fa8bdcc31b40fd7aabbca2406ab4538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237071
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417081}

--- media/capture/video/linux/v4l2_gpu_memory_buffer_tracker_unittest.cc
+++ media/capture/video/linux/v4l2_gpu_memory_buffer_tracker_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "components/viz/test/test_context_provider.h"
-#include "media/capture/video/mock_gpu_memory_buffer_manager.h"
 #include "media/capture/video/video_capture_gpu_channel_host.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
