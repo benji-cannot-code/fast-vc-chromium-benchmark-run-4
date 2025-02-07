25cd1ff9a69ff4dd0ed9eedf4715eb17109f5ccb
Ashutosh Singhal <macinashutosh@google.com>
2025-02-07 10:13:14 -0800

    Ensure 'MessageBox::Core::Show()' must be called only once
    Bug: b:390164423
    Change-Id: Ief2d5c9a77a4eb9250192980438ef53f54b915c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6243797
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1417433}

--- remoting/host/chromeos/message_box.cc
+++ remoting/host/chromeos/message_box.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <utility>
 
+#include "base/check.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -65,6 +66,8 @@ class MessageBox::Core : public views::DialogDelegateView {
   ResultCallback result_callback_;
   raw_ptr<MessageBox> message_box_;
 
+  bool is_shown_ = false;
+
   // Owned by the native widget hierarchy.
   raw_ptr<views::MessageBoxView> message_box_view_;
 };
@@ -109,6 +112,9 @@ MessageBox::Core::Core(const std::u16string& title_label,
 }
 
 void MessageBox::Core::Show(gfx::NativeView parent) {
+  CHECK(!is_shown_) << "Show() should only be called once.";
+  is_shown_ = true;
+
   // The widget is owned by the NativeWidget.  See  comments in widget.h.
   views::Widget* widget =
       CreateDialogWidget(/* delegate=*/this,
