d09f5bc544a6179fce1460b087c2729eb5614538
S. Ganesh <ganesh@chromium.org>
2025-02-07 18:00:12 -0800

    updater: do not pre-register app in AppWebImpl if app exists in registry
    With r1410268, apps are registered automatically if they exist in the
    registry, making a pre-registration redundant, and causing unwanted
    side-effects, so this CL skips pre-registration if the app already
    exists in the registry.
    
    Bug: 41494128,391634935
    Change-Id: Ibcb8f53eb0378a0514fb6b9720a97f9abc42d7ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6245528
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417673}

--- chrome/updater/app/server/win/com_classes_legacy.cc
+++ chrome/updater/app/server/win/com_classes_legacy.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
+#include "base/win/registry.h"
 #include "base/win/scoped_bstr.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/scoped_variant.h"
@@ -59,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/win/setup/setup_util.h"
 #include "chrome/updater/win/ui/l10n_util.h"
 #include "chrome/updater/win/ui/resources/updater_installer_strings.h"
+#include "chrome/updater/win/win_constants.h"
 #include "components/update_client/protocol_definition.h"
 #include "components/update_client/update_client.h"
 
@@ -414,6 +416,19 @@ class AppWebImpl : public IDispatchImpl<IAppWeb> {
 
     if (!is_install_) {
       VLOG(1) << __func__ << ": !is_install_, app not pre-registered";
+      return S_OK;
+    }
+
+    const HKEY root = UpdaterScopeToHKeyRoot(GetUpdaterScope());
+    if (base::win::RegKey(root, GetAppClientsKey(app_id).c_str(),
+                          Wow6432(KEY_QUERY_VALUE))
+            .HasValue(kRegValuePV)) {
+      VLOG(1) << __func__ << ": app is already registered";
+
+      // Always update ap.
+      if (!ap.empty()) {
+        SetRegistryKey(root, GetAppClientStateKey(app_id), kRegValueAP, ap);
+      }
       return S_OK;
     }
 
