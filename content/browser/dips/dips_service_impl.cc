bb5872ecc3e59946286390be63265ea2fff26549
Andrew Liu <liu@chromium.org>
2025-02-10 07:26:27 -0800

    Track metrics around cookie partition keys with uncanonical hosts
    When creating cookie partition keys for BTM, using an uncanonical host
    will cause Mojo deserialization to fail. It seems unusual that an
    uncanonical host would actually reach the point to be deleted, so we
    filter it out for safety and track a metric around it.
    
    Bug: 393088777
    Change-Id: I91f68da8b257f204d9a85c43070ba6b9bf650aff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244983
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418136}

--- content/browser/dips/dips_service_impl.cc
+++ content/browser/dips/dips_service_impl.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <set>
+#include <string>
 #include <string_view>
 #include <vector>
 
 #include "base/check_deref.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
@@ -125,11 +127,14 @@ inline void UmaHistogramDeletion(BtmCookieMode mode, BtmDeletionAction action) {
 
 inline void UmaHistogramSiteToClearDomainLength(
     std::string const& site_to_clear,
-    bool is_partition_key_serializable) {
+    bool is_partition_key_serializable,
+    bool is_canonical_host) {
   base::UmaHistogramSparse(
-      is_partition_key_serializable
-          ? "Privacy.DIPS.DeletionDomainLength.Serializable"
-          : "Privacy.DIPS.DeletionDomainLength.NonSerializable",
+      is_canonical_host
+          ? is_partition_key_serializable
+                ? "Privacy.DIPS.DeletionDomainLength.Serializable"
+                : "Privacy.DIPS.DeletionDomainLength.NonSerializable"
+          : "Privacy.DIPS.DeletionDomainLength.NonCanonical",
       site_to_clear.length());
 }
 
@@ -148,6 +153,10 @@ net::CookiePartitionKeyCollection CookiePartitionKeyCollectionForSites(
       for (auto ancestorChainBit :
            {net::CookiePartitionKey::AncestorChainBit::kSameSite,
             net::CookiePartitionKey::AncestorChainBit::kCrossSite}) {
+        bool is_canonical_host =
+            url::Origin::UnsafelyCreateTupleOriginWithoutNormalization(
+                scheme, site, port)
+                .has_value();
         auto key = net::CookiePartitionKey::FromStorageKeyComponents(
             net::SchemefulSite(
                 url::Origin::CreateFromNormalizedTuple(scheme, site, port)),
@@ -157,12 +166,27 @@ net::CookiePartitionKeyCollection CookiePartitionKeyCollectionForSites(
           // Track metrics around the length of cleared domains and whether they
           // form valid cookie partition keys.
           //
-          // TODO(crbug.com/393088777): Remove this histogram once crashes have
-          // resolved.
-          UmaHistogramSiteToClearDomainLength(site, key->IsSerializeable());
-          // Only collect serializeable cookie partition keys. BTM does not
-          // support wiping opaque sites.
-          if (key->IsSerializeable()) {
+          // TODO(crbug.com/393088777): Remove this histogram and the crash
+          // dumps once crashes have resolved.
+          UmaHistogramSiteToClearDomainLength(site, key->IsSerializeable(),
+                                              is_canonical_host);
+          // Collect crash dumps if the cookie partition key is not
+          // serializeable or the host is not canonical. This should not happen
+          // in practice, and in the case it does, it'll cause deserialization
+          // problems down the line.
+          std::string crash_value =
+              "len=" + base::NumberToString(site.length()) + " " + site;
+          if (!key->IsSerializeable()) {
+            SCOPED_CRASH_KEY_STRING1024("BtmCookiePartitionKey",
+                                        "nonserializeable", crash_value);
+            base::debug::DumpWithoutCrashing();
+          }
+          if (!is_canonical_host) {
+            SCOPED_CRASH_KEY_STRING1024("BtmCookiePartitionKey", "noncanonical",
+                                        crash_value);
+            base::debug::DumpWithoutCrashing();
+          }
+          if (key->IsSerializeable() && is_canonical_host) {
             keys.push_back(*key);
           }
         }
