5004b5dfefe94234cb8f4ee15b05bab1de50a651
Jihad Hanna <jihadghanna@google.com>
2025-02-10 05:32:13 -0800

    Use FieldRendererId instead of FieldRef in AutofillAgent previews
    Autofill currently stores the previewed elements as references to DOM
    elements. This is suboptimal because it allows autofill to hold
    references to elements that get disconnected from the DOM.
    
    There's a feature that is meant to fix this, and there has been two
    attempts at rolling it out that failed due to unexpected regressions in
    submission detection.
    
    Since we never saw any filling regression, I believe that this change is
    safe.
    
    Bug: 40056157
    Change-Id: Id22103ceaefdceca39237d465bc3fdce468097d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239772
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418083}

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -1063,12 +1063,11 @@ void AutofillAgent::ApplyFieldsAction(
                                          action_persistence,
                                          field_data_manager()),
             {},
-            [](const std::pair<FieldRef, WebAutofillState>& filled_field)
+            [](const std::pair<FieldRendererId, WebAutofillState>& filled_field)
                 -> std::pair<FieldRendererId, FormRendererId> {
               WebFormControlElement element =
-                  form_util::GetFormControlByRendererId(
-                      filled_field.first.GetId());
-              return {filled_field.first.GetId(),
+                  form_util::GetFormControlByRendererId(filled_field.first);
+              return {filled_field.first,
                       element ? form_util::GetFormRendererId(
                                     element.GetOwningFormForAutofill())
                               : FormRendererId()};
@@ -1155,9 +1154,10 @@ void AutofillAgent::ClearPreviewedForm() {
 
   std::vector<std::pair<WebFormControlElement, WebAutofillState>>
       previewed_elements;
-  for (const auto& [previewed_element, prior_autofill_state] :
+  for (const auto& [previewed_element_id, prior_autofill_state] :
        previewed_elements_) {
-    if (WebFormControlElement field = previewed_element.GetField()) {
+    if (WebFormControlElement field =
+            form_util::GetFormControlByRendererId(previewed_element_id)) {
       previewed_elements.emplace_back(field, prior_autofill_state);
     }
   }
@@ -1207,7 +1207,7 @@ void AutofillAgent::ApplyFieldAction(
                 << "Previewing replacement of selection is not implemented";
             break;
           case mojom::FieldActionType::kReplaceAll:
-            previewed_elements_.emplace_back(form_control,
+            previewed_elements_.emplace_back(field_id,
                                              form_control.GetAutofillState());
             form_control.SetSuggestedValue(WebString::FromUTF16(value));
             break;
