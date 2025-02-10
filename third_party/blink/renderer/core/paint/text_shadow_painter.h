4ce5804c93bc083ea0272a9aa6840dcb46752877
Fredrik Söderquist <fs@opera.com>
2025-02-10 08:47:20 -0800

    Revert "Fixed purely physical mapping bug for vertical text and decorations."
    This reverts commit 5cf5660d7abb5c2de0b43156c382eadba9c2abe6.
    
    Reason for revert: Caused performance issues
    
    Original change's description:
    > Fixed purely physical mapping bug for vertical text and decorations.
    >
    > This fix was achieved by replacing `DrawLooper` with a filter and implementing the translation of the shadow offset using the value of writing mode.
    >
    > Additionally, this CL is a portion of the fix discussed in a previous CL regarding text-shadow (please refer to the discussion at https://chromium-review.googlesource.com/c/chromium/src/+/5380302).
    >
    > Therefore, a fix for the painting order of text and decoration shadows will follow.
    >
    > Bug: 40829719
    > Change-Id: I9186e1673d14ba419a791efb09060c6f876d5b8d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484626
    > Reviewed-by: Fredrik Söderquist <fs@opera.com>
    > Reviewed-by: Stephen Chenney <schenney@chromium.org>
    > Commit-Queue: Stephen Chenney <schenney@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1398959}
    
    Bug: 40829719
    Bug: 394893130
    Bug: 391914237
    Change-Id: I2c3528d58f6ac8d98b6289c313db574271127fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249857
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Daniel Yip <danielyip@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418182}

--- third_party/blink/renderer/core/paint/text_shadow_painter.h
+++ third_party/blink/renderer/core/paint/text_shadow_painter.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_TEXT_SHADOW_PAINTER_H_
 
 #include "third_party/blink/renderer/core/paint/text_paint_style.h"
-#include "third_party/blink/renderer/core/paint/text_painter.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_context.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
@@ -22,12 +21,11 @@ class ScopedTextShadowPainter {
 
  public:
   ScopedTextShadowPainter(GraphicsContext& context,
-                          const TextPaintStyle& text_style,
-                          const bool is_horizontal) {
+                          const TextPaintStyle& text_style) {
     if (!text_style.shadow) {
       return;
     }
-    ApplyShadowList(context, text_style, is_horizontal);
+    ApplyShadowList(context, text_style);
   }
   ~ScopedTextShadowPainter() {
     if (context_) {
@@ -37,9 +35,7 @@ class ScopedTextShadowPainter {
   bool HasEffectiveShadow() const { return context_; }
 
  private:
-  void ApplyShadowList(GraphicsContext&,
-                       const TextPaintStyle&,
-                       const bool is_horizontal);
+  void ApplyShadowList(GraphicsContext&, const TextPaintStyle&);
 
   GraphicsContext* context_ = nullptr;
 };
@@ -50,23 +46,16 @@ enum class TextShadowPaintPhase {
 };
 
 template <typename PaintProc>
-void PaintWithTextShadow(
-    PaintProc paint_proc,
-    GraphicsContext& context,
-    const TextPaintStyle& text_style,
-    const bool is_horizontal,
-    const TextPainter::ShadowMode shadow_mode =
-        TextPainter::ShadowMode::kBothShadowsAndTextProper) {
-  if (text_style.shadow &&
-      shadow_mode != TextPainter::ShadowMode::kTextProperOnly) {
-    ScopedTextShadowPainter shadow_painter(context, text_style, is_horizontal);
+void PaintWithTextShadow(PaintProc paint_proc,
+                         GraphicsContext& context,
+                         const TextPaintStyle& text_style) {
+  if (text_style.shadow) {
+    ScopedTextShadowPainter shadow_painter(context, text_style);
     if (shadow_painter.HasEffectiveShadow()) {
       paint_proc(TextShadowPaintPhase::kShadow);
     }
   }
-  if (shadow_mode != TextPainter::ShadowMode::kShadowsOnly) {
-    paint_proc(TextShadowPaintPhase::kForeground);
-  }
+  paint_proc(TextShadowPaintPhase::kForeground);
 }
 
 }  // namespace blink
