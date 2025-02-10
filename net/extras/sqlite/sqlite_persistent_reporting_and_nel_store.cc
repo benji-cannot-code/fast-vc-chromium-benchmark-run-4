ecbefe77ee9d34d50503be83ba24005412ad3890
Etienne Bergeron <etienneb@chromium.org>
2025-02-10 15:26:50 -0800

    [fix] Remove multilines SQL statements
    This CL is splitting into two separate calls the SQL queries
    that contains multi-statements.
    
    Bug: 394891084
    Change-Id: I17ccef1509df18891612358b3a88a391c1b71fdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237670
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418329}

--- net/extras/sqlite/sqlite_persistent_reporting_and_nel_store.cc
+++ net/extras/sqlite/sqlite_persistent_reporting_and_nel_store.cc
@@ -717,8 +717,8 @@ SQLitePersistentReportingAndNelStore::Backend::DoMigrateDatabaseSchema() {
       return std::nullopt;
 
     // Migrate NEL policies table.
-    if (!db()->Execute("DROP TABLE IF EXISTS nel_policies_old; "
-                       "ALTER TABLE nel_policies RENAME TO nel_policies_old")) {
+    if (!db()->Execute("DROP TABLE IF EXISTS nel_policies_old") ||
+        !db()->Execute("ALTER TABLE nel_policies RENAME TO nel_policies_old")) {
       return std::nullopt;
     }
     if (!CreateV2NelPoliciesSchema(db()))
@@ -744,8 +744,8 @@ SQLitePersistentReportingAndNelStore::Backend::DoMigrateDatabaseSchema() {
       return std::nullopt;
 
     // Migrate Reporting endpoints table.
-    if (!db()->Execute("DROP TABLE IF EXISTS reporting_endpoints_old; "
-                       "ALTER TABLE reporting_endpoints RENAME TO "
+    if (!db()->Execute("DROP TABLE IF EXISTS reporting_endpoints_old") ||
+        !db()->Execute("ALTER TABLE reporting_endpoints RENAME TO "
                        "reporting_endpoints_old")) {
       return std::nullopt;
     }
@@ -766,8 +766,8 @@ SQLitePersistentReportingAndNelStore::Backend::DoMigrateDatabaseSchema() {
       return std::nullopt;
 
     // Migrate Reporting endpoint groups table.
-    if (!db()->Execute("DROP TABLE IF EXISTS reporting_endpoint_groups_old; "
-                       "ALTER TABLE reporting_endpoint_groups RENAME TO "
+    if (!db()->Execute("DROP TABLE IF EXISTS reporting_endpoint_groups_old") ||
+        !db()->Execute("ALTER TABLE reporting_endpoint_groups RENAME TO "
                        "reporting_endpoint_groups_old")) {
       return std::nullopt;
     }
