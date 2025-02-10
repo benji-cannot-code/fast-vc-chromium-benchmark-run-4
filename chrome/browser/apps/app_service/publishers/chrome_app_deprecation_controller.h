958107674471db5dc9a0dfcbc21011143f2da051
Giovanni Pezzino <giovax@google.com>
2025-02-10 08:59:33 -0800

    Notify users of deprecation of user-installed Chrome Apps
    User-installed Chrome Apps will stop working in M139. This CL adds a
    notification every time a user-installed app is launched, so customers
    are nudged to find an alternative app.
    
    Bug: crbug.com/379264433
    Test: tools/autotest.py chrome/browser/apps/app_service/notifications_browsertest.cc
    Change-Id: Icff062f00870cc3649ef20796df3e77bc52757a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216877
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Giovanni Pezzino <giovax@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418191}

---
+++ chrome/browser/apps/app_service/publishers/chrome_app_deprecation_controller.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHERS_CHROME_APP_DEPRECATION_CONTROLLER_H_
+#define CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHERS_CHROME_APP_DEPRECATION_CONTROLLER_H_
+
+#include <string_view>
+
+class Profile;
+
+// This namespace collects all the methods that control when to enforce the
+// multiple stages of the Chrome Apps deprecation for user installed apps, Kiosk
+// sessions and managed users.
+namespace apps::chrome_app_deprecation {
+
+bool IsLaunchAllowed(std::string_view app_id, Profile* profile);
+
+void AddAppToAllowlistForTesting(std::string_view app_id);
+
+}  // namespace apps::chrome_app_deprecation
+
+#endif  // CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHERS_CHROME_APP_DEPRECATION_CONTROLLER_H_
--- chrome/browser/apps/app_service/publishers/extension_apps_chromeos.cc
+++ chrome/browser/apps/app_service/publishers/extension_apps_chromeos.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_service/publishers/extension_apps_chromeos.h"
 
+#include <cstddef>
 #include <memory>
 #include <optional>
 #include <string>
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/apps/app_service/menu_util.h"
 #include "chrome/browser/apps/app_service/metrics/app_service_metrics.h"
+#include "chrome/browser/apps/app_service/publishers/chrome_app_deprecation_controller.h"
 #include "chrome/browser/apps/app_service/publishers/extension_apps_util.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ash/app_list/extension_app_utils.h"
@@ -1066,6 +1068,10 @@ void ExtensionAppsChromeOs::RegisterInstance(extensions::AppWindow* app_window,
 
 content::WebContents* ExtensionAppsChromeOs::LaunchImpl(
     AppLaunchParams&& params) {
+  if (!chrome_app_deprecation::IsLaunchAllowed(params.app_id, profile())) {
+    return nullptr;
+  }
+
   AppLaunchParams params_for_restore(
       params.app_id, params.container, params.disposition, params.launch_source,
       params.display_id, params.launch_files, params.intent);
