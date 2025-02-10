698336b7199008e9e52c88343ccc0a400f64be1e
James Sun <jamessun@google.com>
2025-02-10 12:38:48 -0800

    [Amount Extraction][Browser] Load BNPL allowlist to AmountExtractionManager
    This CL let AmountExtractionManager reads in the allowlist hosted by
    AutofillOptimizationGuide and check whether the checkout page that
    user is visiting hits the BNPL allowlist or not. And only trigger the
    amount search if the allowlist covers the host.
    
    Bug: 394911170
    Change-Id: Ib17b113ef91b0a5f76a7e659b0bbb979049c9910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6240436
    Commit-Queue: James Sun <jamessun@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418300}

--- components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
@@ -878,8 +878,12 @@ class MockAutofillDriver : public TestAutofillDriver {
 class MockAmountExtractionManager : public payments::AmountExtractionManager {
  public:
   explicit MockAmountExtractionManager(TestBrowserAutofillManager* test_manager)
-      : AmountExtractionManager(
-            static_cast<BrowserAutofillManager*>(test_manager)) {}
+      : AmountExtractionManager(test_manager) {
+    ON_CALL(*static_cast<MockAutofillOptimizationGuide*>(
+                test_manager->client().GetAutofillOptimizationGuide()),
+            IsEligibleForBuyNowPayLater)
+        .WillByDefault(Return(true));
+  }
 
   MOCK_METHOD(void, TriggerCheckoutAmountExtraction, (), (override));
 };
@@ -2794,7 +2798,6 @@ TEST_F(BrowserAutofillManagerTest,
        ShouldNotTriggerAmountExtraction_IfNonCreditCardFormIsClicked) {
   base::test::ScopedFeatureList scoped_feature_list{
       features::kAutofillEnableAmountExtractionDesktop};
-
   // Set up our form data.
   FormData form = CreateTestAddressFormData();
   FormsSeen({form});
@@ -2840,7 +2843,6 @@ TEST_F(BrowserAutofillManagerTest,
        ShouldNotTriggerAmountExtraction_IfAutofillDisabled) {
   base::test::ScopedFeatureList scoped_feature_list{
       features::kAutofillEnableAmountExtractionDesktop};
-
   // Set up our form data.
   FormData form =
       CreateTestCreditCardFormData(/*is_https=*/true, /*use_month_type=*/false);
