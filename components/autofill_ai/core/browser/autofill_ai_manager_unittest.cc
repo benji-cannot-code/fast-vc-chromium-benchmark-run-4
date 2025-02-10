ee5708e071c291191599af7368deceeed024adf8
Jan Keitel <jkeitel@google.com>
2025-02-10 01:40:52 -0800

    Rename AutofillAiManager::GetSuggestionsV2
    Since the old "GetSuggestions()" method no longer exist, we can drop
    the V2-suffix from GetSuggestionsV2.
    
    Bug: 395039350
    Change-Id: Ibb19bbc7927027d447d7e31c5351799b84613dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244459
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418021}

--- components/autofill_ai/core/browser/autofill_ai_manager_unittest.cc
+++ components/autofill_ai/core/browser/autofill_ai_manager_unittest.cc
@@ -246,9 +246,9 @@ TEST_F(AutofillAiManagerTest,
       autofill::test::GetPassportEntityInstance());
 
   base::test::TestFuture<std::vector<autofill::Suggestion>> suggestions;
-  manager().GetSuggestionsV2(
-      form.global_id(), form.fields().front().global_id(),
-      /*is_manual_fallback=*/false, suggestions.GetCallback());
+  manager().GetSuggestions(form.global_id(), form.fields().front().global_id(),
+                           /*is_manual_fallback=*/false,
+                           suggestions.GetCallback());
   EXPECT_THAT(suggestions.Take(), ElementsAre(HasType(kFillAutofillAi)));
 }
 
@@ -272,9 +272,9 @@ TEST_F(
       get_suggestions_callback;
 
   base::test::TestFuture<std::vector<autofill::Suggestion>> suggestions;
-  manager().GetSuggestionsV2(
-      form.global_id(), form.fields().front().global_id(),
-      /*is_manual_fallback=*/true, suggestions.GetCallback());
+  manager().GetSuggestions(form.global_id(), form.fields().front().global_id(),
+                           /*is_manual_fallback=*/true,
+                           suggestions.GetCallback());
   EXPECT_THAT(suggestions.Take(),
               ElementsAre(HasType(kAutofillAiLoadingState)));
 }
@@ -301,9 +301,9 @@ TEST_F(
       get_suggestions_callback;
 
   base::test::TestFuture<std::vector<autofill::Suggestion>> suggestions;
-  manager().GetSuggestionsV2(
-      form.global_id(), form.fields().front().global_id(),
-      /*is_manual_fallback=*/true, suggestions.GetCallback());
+  manager().GetSuggestions(form.global_id(), form.fields().front().global_id(),
+                           /*is_manual_fallback=*/true,
+                           suggestions.GetCallback());
   EXPECT_THAT(suggestions.Take(), ElementsAre(HasType(kFillAutofillAi)));
 }
 
