a318be07d2c52c780697127e96ead92e58d25d3c
Anthony Vallée-Dubois <anthonyvd@google.com>
2025-02-10 12:29:09 -0800

    Support web test font sideloading for FontDataManager
    Web tests depend on Ahem being available as a system font. To achieve
    this, the DWriteFontProxy sideloads Ahem in its internal collection.
    
    This doesn't provide Ahem to the browser process' underlying font
    manager, which makes it so FontDataManager can't see Ahem in web tests.
    
    This CL sideloads the font in the browser's dwrite font manager, which
    is used by the FontDataManager. This allows web tests that depend on
    Ahem to pass when using FontDataManager.
    
    Bug: None
    Change-Id: Id56da6ac99070a4ac7f65436b86e68cd5c6de320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6250577
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418296}

--- ui/gfx/win/direct_write.h
+++ ui/gfx/win/direct_write.h
@@ -12,9 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/component_export.h"
+#include "base/files/file_path.h"
 
 namespace gfx {
 namespace win {
+
+COMPONENT_EXPORT(GFX) void SideLoadFontForTesting(base::FilePath path);
 
 COMPONENT_EXPORT(GFX) void InitializeDirectWrite();
 
