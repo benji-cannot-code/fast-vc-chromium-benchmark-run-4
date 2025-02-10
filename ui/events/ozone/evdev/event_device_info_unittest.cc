c565da8d348b859bdb774aac568a3a9f7a4a839b
Jonathan Denose <jdenose@google.com>
2025-02-10 11:26:14 -0800

    Revert "ozone: evdev: Add reven-specific internal usb list"
    This reverts commit c8ce009fc70efda146e4e44c8380f82137bb14bd.
    
    Reason for revert: Caused the regression resulting in b/393775027.
    
    The customer has an AIO with an internal USB touchscreen which was
    correctly identified as internal by crrev/c/5980254. However, already in
    production they have an external touchscreen with the same vid/pid as
    the AIO's internal touchscreen. This was causing the external
    touchscreen to be mapped as internal which led to a loss of
    touchscreen functionality on the external touchscreens in production.
    Reverting this change will restore functionality to the devices
    already in production.
    
    Original change's description:
    > ozone: evdev: Add reven-specific internal usb list
    >
    > Devices running ChromeOS Flex containing internal usb touchscreens
    > are behaving like external touchscreens. This CL creates a
    > Flex-specific list of devices which will now be handled as internal
    > input devices instead of as external usb devices.
    >
    > Bug: 361471892
    > Test: Verify touch events are processed on the correct display when
    > an external display is connected.
    > Test: out/Default/events_unittests
    >
    > Change-Id: I6104a4fa83b28676a1df56d16081432a3d6ed82a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980254
    > Reviewed-by: Harry Cutts <hcutts@chromium.org>
    > Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    > Commit-Queue: Jonathan Denose <jdenose@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1378643}
    
    Bug: 361471892,393775027
    Change-Id: I338c92ea835684dd3d1a9981e2248dfea9a54b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6235020
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Commit-Queue: Jonathan Denose <jdenose@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418284}

--- ui/events/ozone/evdev/event_device_info_unittest.cc
+++ ui/events/ozone/evdev/event_device_info_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/ozone/evdev/event_device_info.h"
 
-#include "base/command_line.h"
 #include "base/format_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
@@ -14,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/events/ozone/evdev/event_device_test_util.h"
 #include "ui/events/ozone/evdev/event_device_util.h"
-
-#if BUILDFLAG(IS_CHROMEOS)
-#include "ash/constants/ash_switches.h"  // nogncheck
-#endif
 
 namespace ui {
 
@@ -584,17 +579,5 @@ TEST(EventDeviceInfoTest, RexHeatmapTouchScreen) {
   EXPECT_FALSE(devinfo.HasStylusSwitch());
   EXPECT_TRUE(devinfo.SupportsHeatmap());
 }
-
-#if BUILDFLAG(IS_CHROMEOS)
-TEST(EventDeviceInfoTest, RevenAdvantechInternalUsbTouchscreen) {
-  EventDeviceInfo devinfo;
-  base::CommandLine::ForCurrentProcess()->AppendSwitch(
-      ash::switches::kRevenBranding);
-  EXPECT_TRUE(
-      CapabilitiesToDeviceInfo(kAdvantechUsbInternalTouchscreen, &devinfo));
-
-  EXPECT_EQ(ui::InputDeviceType::INPUT_DEVICE_INTERNAL, devinfo.device_type());
-}
-#endif
 
 }  // namespace ui
