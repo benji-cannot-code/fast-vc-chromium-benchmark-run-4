7a67d15d8edff3a10e755843f398f77d4f62d541
Asami Doi <asamidoi@chromium.org>
2025-02-10 03:31:30 -0800

    [ios] Add helper function to create fake shared tab groups
    This CL adds a helper function `+prepareFakeSharedTabGroups:` to add
    shared tab groups from the test. Also adds a test case to make sure
    that the groups are visible in the tab grid and have the correct
    context menu.
    
    Bug: 389986364
    Change-Id: I77dbfc7a74ff4570d5579d6de8388d5061a234c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218482
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418048}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_app_interface.h
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_app_interface.h
@@ -16,6 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Creates and saves `numberOfGroups` synced tab groups.
 + (void)prepareFakeSyncedTabGroups:(NSInteger)numberOfGroups;
 
+// Creates and saves `numberOfGroups` shared tab groups. A user with
+// `fakeIdentity1` joins the group as a member and a user with `fakeIdentity2`
+// joins the group as an owner.
++ (void)prepareFakeSharedTabGroups:(NSInteger)numberOfGroups;
+
 // Removes a group at `index`.
 + (void)removeAtIndex:(unsigned int)index;
 
