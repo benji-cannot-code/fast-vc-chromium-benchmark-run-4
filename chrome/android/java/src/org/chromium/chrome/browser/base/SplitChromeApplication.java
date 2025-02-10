2b961b4d93742a8495d2dc82f600579ccd0e84b4
Sam Maier <smaier@chromium.org>
2025-02-10 11:15:29 -0800

    Fix UnsatisfiedLinkError message in race condition
    Instead of seeing UnsatisfiedLinkError crashes, we are seeing
    NullPointerExceptions on SplitCompatActivity.getImpl, as the supplier is
    null. I believe this is because, in some cases with our earlier native
    loading, we can trigger the UnsatisfiedLinkError before
    attachBaseContext finishes. To fix this, we can just set the supplier
    before calling super.attachBaseContext.
    
    Bug: 395261363
    Change-Id: Id1b18ccbecdad9598c2ebe1360b7deb6e8402b5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249979
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418277}

--- chrome/android/java/src/org/chromium/chrome/browser/base/SplitChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/base/SplitChromeApplication.java
@@ -63,7 +63,6 @@ public class SplitChromeApplication extends SplitCompatApplication {
 
     @Override
     protected void attachBaseContext(Context context) {
-        super.attachBaseContext(context);
         if (isBrowserProcess()) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 DexFixer.setHasIsolatedSplits(true);
@@ -77,6 +76,9 @@ public class SplitChromeApplication extends SplitCompatApplication {
         } else {
             setImplSupplier(() -> createNonBrowserApplication());
         }
+        // We need to call setImplSupplier before continuing attachBaseContext. See
+        // crbug.com/395261363 for details.
+        super.attachBaseContext(context);
     }
 
     private static String getPreloadClassName(String split) {
