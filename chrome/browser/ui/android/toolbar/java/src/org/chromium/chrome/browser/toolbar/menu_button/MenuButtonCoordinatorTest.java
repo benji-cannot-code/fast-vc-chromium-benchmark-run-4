8030c5740988d5b4ca6ddb59da951669083e7501
Sirisha Kavuluru <skavuluru@google.com>
2025-02-11 11:32:19 -0800

    [Toolbar][Refactor] Move setting tooltip text to xml
    Change-Id: I5ade1a71c5c52817eae3bac91a21ab9446a25850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226432
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418797}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/menu_button/MenuButtonCoordinatorTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/menu_button/MenuButtonCoordinatorTest.java
@@ -2,11 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2020 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 package org.chromium.chrome.browser.toolbar.menu_button;
 
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -14,22 +12,18 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.widget.ImageButton;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.robolectric.Robolectric;
 import org.robolectric.annotation.LooperMode;
 
-import org.chromium.base.CallbackUtils;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.browser_controls.BrowserStateBrowserControlsVisibilityDelegate;
 import org.chromium.chrome.browser.theme.ThemeColorProvider;
 import org.chromium.chrome.browser.toolbar.R;
-import org.chromium.chrome.browser.toolbar.top.ToolbarTablet;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuButtonHelper;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuCoordinator;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuHandler;
@@ -43,13 +37,14 @@ import java.lang.ref.WeakReference;
 @RunWith(BaseRobolectricTestRunner.class)
 @LooperMode(LooperMode.Mode.LEGACY)
 public class MenuButtonCoordinatorTest {
+
     @Mock private BrowserStateBrowserControlsVisibilityDelegate mControlsVisibilityDelegate;
-    @Mock private Activity mActivityMock;
+    @Mock private Activity mActivity;
     @Mock private MenuButtonCoordinator.SetFocusFunction mFocusFunction;
     @Mock private AppMenuCoordinator mAppMenuCoordinator;
     @Mock private AppMenuHandler mAppMenuHandler;
     @Mock private AppMenuButtonHelper mAppMenuButtonHelper;
-    MenuButton mMenuButton;
+    @Mock MenuButton mMenuButton;
     @Mock ImageButton mImageButton;
     @Mock private AppMenuPropertiesDelegate mAppMenuPropertiesDelegate;
     @Mock private Runnable mRequestRenderRunnable;
@@ -61,20 +56,10 @@ public class MenuButtonCoordinatorTest {
     private MenuUiState mMenuUiState;
     private OneshotSupplierImpl<AppMenuCoordinator> mAppMenuSupplier;
     private MenuButtonCoordinator mMenuButtonCoordinator;
-    private Activity mActivity;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-
-        // Get menu button.
-        mActivity = Robolectric.buildActivity(Activity.class).setup().get();
-        mActivity.setTheme(R.style.Theme_BrowserUI_DayNight);
-        ToolbarTablet toolbarTablet =
-                (ToolbarTablet)
-                        mActivity.getLayoutInflater().inflate(R.layout.toolbar_tablet, null);
-        mMenuButton = spy(toolbarTablet.findViewById(R.id.menu_button_wrapper));
-
         doReturn(mAppMenuHandler).when(mAppMenuCoordinator).getAppMenuHandler();
         doReturn(mAppMenuButtonHelper).when(mAppMenuHandler).createAppMenuButtonHelper();
         doReturn(mAppMenuPropertiesDelegate)
@@ -82,18 +67,16 @@ public class MenuButtonCoordinatorTest {
                 .getAppMenuPropertiesDelegate();
         mAppMenuSupplier = new OneshotSupplierImpl<>();
         mMenuUiState = new MenuUiState();
-        doReturn(mMenuButton).when(mActivityMock).findViewById(R.id.menu_button_wrapper);
+        doReturn(mMenuButton).when(mActivity).findViewById(R.id.menu_button_wrapper);
         doReturn(mImageButton).when(mMenuButton).getImageButton();
-        doReturn(mResources).when(mActivityMock).getResources();
+        doReturn(mResources).when(mActivity).getResources();
         doReturn(10)
                 .when(mResources)
                 .getDimensionPixelSize(R.dimen.toolbar_url_focus_translation_x);
-        doReturn(mActivity.getResources().getString(R.string.accessibility_toolbar_btn_menu))
-                .when(mResources)
-                .getString(R.string.accessibility_toolbar_btn_menu);
-        doReturn(new WeakReference<>(mActivityMock)).when(mWindowAndroid).getActivity();
+        doReturn(new WeakReference<>(mActivity)).when(mWindowAndroid).getActivity();
         doReturn(mKeyboardDelegate).when(mWindowAndroid).getKeyboardDelegate();
 
+        // clang-format off
         mMenuButtonCoordinator =
                 new MenuButtonCoordinator(
                         mAppMenuSupplier,
@@ -105,8 +88,9 @@ public class MenuButtonCoordinatorTest {
                         () -> false,
                         mThemeColorProvider,
                         () -> null,
-                        CallbackUtils.emptyRunnable(),
+                        () -> {},
                         R.id.menu_button_wrapper);
+        // clang-format on
     }
 
     @Test
@@ -119,13 +103,5 @@ public class MenuButtonCoordinatorTest {
         mMenuButtonCoordinator.destroy();
         mMenuButtonCoordinator.onEnterKeyPress();
         verify(mAppMenuButtonHelper, times(1)).onEnterKeyPress(mImageButton);
-    }
-
-    @Test
-    public void testHoverTooltipText() {
-        Assert.assertEquals(
-                "Tooltip text for Menu button is not as expected",
-                mActivity.getResources().getString(R.string.accessibility_toolbar_btn_menu),
-                mMenuButton.getTooltipText());
     }
 }
