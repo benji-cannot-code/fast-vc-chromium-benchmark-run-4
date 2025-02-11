c01172af30c497c56c47cf6253d6e56965a40f0e
Shanthanu Bhardwaj <xanth@google.com>
2025-02-11 08:03:12 -0800

    Add a proto based upload API to realtime connector reporting client
    - This uses the UploadEventsRequest proto to call the proto based UploadSecurityEvent functions in CloudPolicyClient
    - Change GetLocalIpAddresses() to a std::vector from a base::Value::List  so we can reuse it in the deprecated and new codepaths
    - Add tests for the new upload call stack
    
    Change-Id: I22fff5e45648130caeee56e9045ef16db8cb73ce
    Bug: 394403600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237878
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418668}

--- chrome/browser/profiles/reporting_util.h
+++ chrome/browser/profiles/reporting_util.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
 #include "components/enterprise/common/proto/connectors.pb.h"
+#include "components/enterprise/common/proto/upload_request_response.pb.h"
 
 class Profile;
 
@@ -20,6 +21,11 @@ namespace reporting {
 // returns corresponding info to a Device, Browser and Profile protos defined in
 // google3/google/internal/chrome/reporting/v1/chromereporting.proto.
 base::Value::Dict GetContext(Profile* profile);
+
+// Creates and returns an UploadEventsRequest proto with the Device, Browser and
+// Profile fields set.
+::chrome::cros::reporting::proto::UploadEventsRequest CreateUploadEventsRequest(
+    Profile* profile);
 
 // Fetches the same information as GetContext, but in a protobuf instead of a
 // Value.
