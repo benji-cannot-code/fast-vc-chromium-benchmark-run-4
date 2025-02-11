29955213983119f574e7d062bc03473bb7839498
Sandor Major <sandormajor@chromium.org>
2025-02-10 16:38:00 -0800

    Update comment to point to the correct template
    Bug: 382291442
    Change-Id: I5c65b73f4294d8a81fcb764564da9d0c618800f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6248600
    Commit-Queue: Sandor «Alex» Major <sandormajor@chromium.org>
    Auto-Submit: Sandor «Alex» Major <sandormajor@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418366}

--- third_party/blink/common/permissions_policy/permissions_policy_features_generated.h
+++ third_party/blink/common/permissions_policy/permissions_policy_features_generated.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/permissions_policy/permissions_policy_features.h"
 
-// Headers for the generated code from permissions_policy_features.cc.tmpl.
+// Headers for the generated code from
+// permissions_policy_features_generated.cc.tmpl.
 namespace blink {
 
 PermissionsPolicyFeatureList GetBasePermissionsPolicyFeatureList();
