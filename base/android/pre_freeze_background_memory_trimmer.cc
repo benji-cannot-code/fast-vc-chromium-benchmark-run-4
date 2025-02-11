5582d6fd0efb32dd8fedf6631e31ddc5cea7c561
Thiabaud Engelbrecht <thiabaud@google.com>
2025-02-10 20:58:18 -0800

    [self_freeze] Clean up locking annotations
    This CL fixes the locking annotations to always refer to the same lock,
    regardless of whether we are in a static function or not. This has no
    effect on functionality, but allows the checker to catch more issues
    with locking.
    
    Change-Id: I41e712984afc23bb0836dea7aadad1d592ba6b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6234610
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418472}

--- base/android/pre_freeze_background_memory_trimmer.cc
+++ base/android/pre_freeze_background_memory_trimmer.cc
@@ -223,7 +223,7 @@ void PreFreezeBackgroundMemoryTrimmer::RecordMetrics() {
   // determine the current process, which is used for the names of metrics
   // below.
   CHECK(base::CommandLine::InitializedForCurrentProcess());
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   if (metrics_.size() != values_before_.size()) {
     UmaHistogramEnumeration("Memory.PreFreeze2.RecordMetricsFailureType",
                             MetricsFailure::kSizeMismatch);
@@ -404,7 +404,7 @@ void PreFreezeBackgroundMemoryTrimmer::PostDelayedBackgroundTaskModern(
     return;
   }
 
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   PostDelayedBackgroundTaskModernHelper(std::move(task_runner), from_here,
                                         std::move(task), delay);
 }
@@ -425,7 +425,7 @@ PreFreezeBackgroundMemoryTrimmer::PostDelayedBackgroundTaskModernHelper(
 // static
 void PreFreezeBackgroundMemoryTrimmer::RegisterMemoryMetric(
     const PreFreezeMetric* metric) {
-  base::AutoLock locker(Instance().lock_);
+  base::AutoLock locker(lock());
   Instance().RegisterMemoryMetricInternal(metric);
 }
 
@@ -443,7 +443,7 @@ void PreFreezeBackgroundMemoryTrimmer::RegisterMemoryMetricInternal(
 // static
 void PreFreezeBackgroundMemoryTrimmer::UnregisterMemoryMetric(
     const PreFreezeMetric* metric) {
-  base::AutoLock locker(Instance().lock_);
+  base::AutoLock locker(lock());
   Instance().UnregisterMemoryMetricInternal(metric);
 }
 
@@ -467,7 +467,7 @@ bool PreFreezeBackgroundMemoryTrimmer::SelfCompactionIsSupported() {
 // static
 bool PreFreezeBackgroundMemoryTrimmer::ShouldContinueSelfCompaction(
     base::TimeTicks self_compaction_started_at) {
-  base::AutoLock locker(Instance().lock_);
+  base::AutoLock locker(lock());
   return Instance().self_compaction_last_cancelled_ <
          self_compaction_started_at;
 }
@@ -519,7 +519,7 @@ void PreFreezeBackgroundMemoryTrimmer::StartSelfCompaction(
     base::TimeTicks started_at) {
   TRACE_EVENT0("base", "StartSelfCompaction");
   {
-    base::AutoLock locker(lock_);
+    base::AutoLock locker(lock());
     process_compacted_metadata_.emplace(
         "PreFreezeBackgroundMemoryTrimmer.ProcessCompacted",
         /*is_compacted=*/1, base::SampleMetadataScope::kProcess);
@@ -553,7 +553,7 @@ void PreFreezeBackgroundMemoryTrimmer::MaybeCancelSelfCompaction() {
 }
 
 void PreFreezeBackgroundMemoryTrimmer::MaybeCancelSelfCompactionInternal() {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   process_compacted_metadata_.reset();
   self_compaction_last_cancelled_ = base::TimeTicks::Now();
 }
@@ -666,7 +666,7 @@ void PreFreezeBackgroundMemoryTrimmer::OnSelfFreeze() {
 }
 
 void PreFreezeBackgroundMemoryTrimmer::OnSelfFreezeInternal() {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   if (base::FeatureList::IsEnabled(kShouldFreezeSelf)) {
     RunPreFreezeTasks();
   }
@@ -708,13 +708,13 @@ void PreFreezeBackgroundMemoryTrimmer::RunPreFreezeTasks() {
     // (1) To avoid holding it too long while running all the background tasks.
     // (2) To prevent a deadlock if the |background_task| needs to acquire the
     //     lock (e.g. to post another task).
-    base::AutoUnlock unlocker(lock_);
+    base::AutoUnlock unlocker(lock());
     BackgroundTask::RunNow(std::move(background_task));
   }
 }
 
 void PreFreezeBackgroundMemoryTrimmer::OnPreFreezeInternal() {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   PostMetricsTasksIfModern();
 
   if (!ShouldUseModernTrim()) {
@@ -732,13 +732,13 @@ void PreFreezeBackgroundMemoryTrimmer::UnregisterBackgroundTask(
 
 void PreFreezeBackgroundMemoryTrimmer::UnregisterBackgroundTaskInternal(
     BackgroundTask* timer) {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   std::erase_if(background_tasks_, [&](auto& t) { return t.get() == timer; });
 }
 
 // static
 void PreFreezeBackgroundMemoryTrimmer::RegisterPrivateMemoryFootprintMetric() {
-  base::AutoLock locker(Instance().lock_);
+  base::AutoLock locker(lock());
   static base::NoDestructor<PrivateMemoryFootprintMetric> pmf_metric;
   if (!PrivateMemoryFootprintMetric::did_register_) {
     PrivateMemoryFootprintMetric::did_register_ = true;
@@ -769,39 +769,39 @@ void PreFreezeBackgroundMemoryTrimmer::SetSupportsModernTrimForTesting(
 
 // static
 void PreFreezeBackgroundMemoryTrimmer::ClearMetricsForTesting() {
-  base::AutoLock locker(Instance().lock_);
+  base::AutoLock locker(lock());
   Instance().metrics_.clear();
   PrivateMemoryFootprintMetric::did_register_ = false;
 }
 
 bool PreFreezeBackgroundMemoryTrimmer::DidRegisterTasksForTesting() const {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   return metrics_.size() != 0;
 }
 
 size_t
 PreFreezeBackgroundMemoryTrimmer::GetNumberOfPendingBackgroundTasksForTesting()
     const {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   return background_tasks_.size();
 }
 
 size_t PreFreezeBackgroundMemoryTrimmer::GetNumberOfKnownMetricsForTesting()
     const {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   return metrics_.size();
 }
 
 size_t PreFreezeBackgroundMemoryTrimmer::GetNumberOfValuesBeforeForTesting()
     const {
-  base::AutoLock locker(lock_);
+  base::AutoLock locker(lock());
   return values_before_.size();
 }
 
 // static
 void PreFreezeBackgroundMemoryTrimmer::
     ResetSelfCompactionLastCancelledForTesting() {
-  base::AutoLock locker(Instance().lock_);
+  base::AutoLock locker(lock());
   Instance().self_compaction_last_cancelled_ = base::TimeTicks::Min();
 }
 
