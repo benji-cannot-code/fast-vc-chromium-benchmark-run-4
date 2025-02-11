78c96903ce52c8031ad0d31430ffb2b000258451
Christian Xu <christianxu@google.com>
2025-02-11 03:09:44 -0800

    [iOS][Omnibox][OR] Move omnibox position observation to controller
    Move omnibox position observation from OmniboxPopupMediator to
    OmniboxAutocompleteController.
    
    Bug: 390409678
    Change-Id: I7b79ecb99a3fba18f282ee80200cfbc1f3888158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249321
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418551}

--- ios/chrome/browser/omnibox/ui_bundled/popup/omnibox_popup_mediator.mm
+++ ios/chrome/browser/omnibox/ui_bundled/popup/omnibox_popup_mediator.mm
@@ -48,9 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/omnibox/ui_bundled/popup/popup_swift.h"
 #import "ios/chrome/browser/omnibox/ui_bundled/popup/remote_suggestions_service_observer_bridge.h"
 #import "ios/chrome/browser/omnibox/ui_bundled/popup/row/actions/suggest_action.h"
-#import "ios/chrome/browser/shared/model/application_context/application_context.h"
-#import "ios/chrome/browser/shared/model/prefs/pref_backed_boolean.h"
-#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
 #import "ios/chrome/browser/shared/public/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
@@ -69,8 +66,7 @@ const CGFloat kOmniboxIconSize = 16;
 const NSUInteger kMaxSuggestTileTypePosition = 15;
 }  // namespace
 
-@interface OmniboxPopupMediator () <BooleanObserver,
-                                    PedalSectionExtractorDelegate>
+@interface OmniboxPopupMediator () <PedalSectionExtractorDelegate>
 
 // FET reference.
 @property(nonatomic, assign) feature_engagement::Tracker* tracker;
@@ -107,10 +103,6 @@ const NSUInteger kMaxSuggestTileTypePosition = 15;
   std::unique_ptr<RemoteSuggestionsServiceObserverBridge>
       _remoteSuggestionsServiceObserverBridge;
 
-  /// Preferred omnibox position, logged in omnibox logs.
-  metrics::OmniboxEventProto::OmniboxPosition _preferredOmniboxPosition;
-  /// Pref tracking if bottom omnibox is enabled.
-  PrefBackedBoolean* _bottomOmniboxEnabled;
   /// Holds cached images keyed by their URL. The cache is purged when the popup
   /// is closed.
   NSCache<NSString*, UIImage*>* _cachedImages;
@@ -139,23 +131,11 @@ const NSUInteger kMaxSuggestTileTypePosition = 15;
     _remoteSuggestionsService = remoteSuggestionsService;
     _tracker = tracker;
     _cachedImages = [[NSCache alloc] init];
-    // This is logged only when `IsBottomOmniboxAvailable`.
-    _preferredOmniboxPosition = metrics::OmniboxEventProto::UNKNOWN_POSITION;
-
-    _bottomOmniboxEnabled = [[PrefBackedBoolean alloc]
-        initWithPrefService:GetApplicationContext()->GetLocalState()
-                   prefName:prefs::kBottomOmnibox];
-    [_bottomOmniboxEnabled setObserver:self];
-    // Initialize to the correct value.
-    [self booleanDidChange:_bottomOmniboxEnabled];
   }
   return self;
 }
 
 - (void)disconnect {
-  [_bottomOmniboxEnabled stop];
-  [_bottomOmniboxEnabled setObserver:nil];
-  _bottomOmniboxEnabled = nil;
   if (_remoteSuggestionsServiceObserverBridge) {
     self.remoteSuggestionsService->RemoveObserver(
         _remoteSuggestionsServiceObserverBridge.get());
@@ -419,21 +399,6 @@ const NSUInteger kMaxSuggestTileTypePosition = 15;
       base::UmaHistogramExactLinear("Omnibox.SuggestTiles.SelectedTileType",
                                     tileType, SuggestTileType::kCount);
       return;
-    }
-  }
-}
-
-#pragma mark - Boolean Observer
-
-- (void)booleanDidChange:(id<ObservableBoolean>)observableBoolean {
-  if (observableBoolean == _bottomOmniboxEnabled) {
-    _preferredOmniboxPosition =
-        _bottomOmniboxEnabled.value
-            ? metrics::OmniboxEventProto::BOTTOM_POSITION
-            : metrics::OmniboxEventProto::TOP_POSITION;
-    if (self.autocompleteController) {
-      self.autocompleteController->SetSteadyStateOmniboxPosition(
-          _preferredOmniboxPosition);
     }
   }
 }
