f688d944c8aef8358ebc80d74bbb453e5c31347d
Daniel Cheng <dcheng@chromium.org>
2025-02-11 11:23:51 -0800

    Use std::string_view to hold filename in ui::ClassMetaData
    This is always constructed from `__FILE__` so there's no need to
    allocate storage. One slightly tricky bit is the trim helpers in
    string_util.h output a std::string rather than a std::string_view.
    
    A nice future cleanup might be to see if there's a way to change the
    trim APIs to take and return a std::string_view via use of the
    LIFETIME_BOUND annotations.
    
    Change-Id: I6c5fcffd1516766129fbcc8ddc444836fd0d30fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6246351
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418782}

--- ui/base/metadata/metadata_types.h
+++ ui/base/metadata/metadata_types.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_list.h"
+#include "base/compiler_specific.h"
 #include "base/component_export.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
@@ -95,7 +96,7 @@ class MemberMetaDataBase;
 class COMPONENT_EXPORT(UI_BASE_METADATA) ClassMetaData {
  public:
   ClassMetaData();
-  ClassMetaData(std::string file, int line);
+  ClassMetaData(std::string_view file LIFETIME_BOUND, int line);
   ClassMetaData(const ClassMetaData&) = delete;
   ClassMetaData& operator=(const ClassMetaData&) = delete;
   virtual ~ClassMetaData();
@@ -105,7 +106,7 @@ class COMPONENT_EXPORT(UI_BASE_METADATA) ClassMetaData {
       const {
     return members_;
   }
-  const std::string& file() const { return file_; }
+  const std::string_view file() const { return file_; }
   const int& line() const { return line_; }
   const std::string& GetUniqueName() const;
   void AddMemberData(std::unique_ptr<MemberMetaDataBase> member_data);
@@ -179,7 +180,7 @@ class COMPONENT_EXPORT(UI_BASE_METADATA) ClassMetaData {
   mutable std::string unique_name_;
   std::vector<raw_ptr<MemberMetaDataBase, VectorExperimental>> members_;
   raw_ptr<ClassMetaData> parent_class_meta_data_ = nullptr;
-  std::string file_;
+  const std::string_view file_;
   const int line_ = 0;
 };
 
