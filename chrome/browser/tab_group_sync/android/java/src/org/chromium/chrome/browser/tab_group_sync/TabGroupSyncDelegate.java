f5b464355c2fb7ff42531c9be8bde1d3d1b84fc1
Shakti Sahu <shaktisahu@chromium.org>
2025-02-11 10:11:14 -0800

    [TG Sync] Add delegate method to get a tab title from tab model
    Messaging backend needs the tab's current title in case an active
    tab is remotely updated / deleted. However, we don't keep the
    title info in SavedTabGroupTab as we treat title to be same as
    URL for shared tab groups for various reasons.
    Hence the best option is to find out the tab's title from the
    tab model. This CL adds a delegate API in TabGroupSyncService
    to get the title from live tab.
    
    Bug: 379699409
    Change-Id: Id60f10c882465bcb973aa591ea9c2a977322d5dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6248937
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418748}

--- chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncDelegate.java
+++ chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncDelegate.java
@@ -10,6 +10,8 @@ import androidx.annotation.NonNull;
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabWindowManager;
 
@@ -44,6 +46,18 @@ public class TabGroupSyncDelegate implements TabWindowManager.Observer {
     @CalledByNative
     void destroy() {
         mTabWindowManager.removeObserver(this);
+    }
+
+    @CalledByNative
+    private String getTabTitle(int tabId) {
+        for (TabModelSelector tabModelSelector : mTabWindowManager.getAllTabModelSelectors()) {
+            TabModel tabModel = tabModelSelector.getModel(/* incognito= */ false);
+            Tab tab = tabModel.getTabById(tabId);
+            if (tab != null) {
+                return tab.getTitle();
+            }
+        }
+        return "";
     }
 
     @Override
