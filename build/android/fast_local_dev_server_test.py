b6f09ec8c8371040da677db72ac7e3df36199e73
Mohamed Heikal <mheikal@chromium.org>
2025-02-11 14:33:40 -0800

    Refactor build server to group build related work into a class
    There is now a Build object/class that manages build specific work like
    keeping track of build level statistics as well as creating and writing
    to build logfiles.
    
    Logfiles should no longer be leaked since we now can tell when a build
    is "done" and close the logfile.
    
    Title string is now based on global stats rather than per build to
    better handle the case when two concurrent builds are updating the same
    terminal title.
    
    Added a global OptionsManager so as to not have to pipe the quiet option
    everywhere.
    
    Change-Id: I010982dc863ab45508957997c4ed8ad26902a4bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249304
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418922}

--- build/android/fast_local_dev_server_test.py
+++ build/android/fast_local_dev_server_test.py
@@ -44,13 +44,20 @@ def pollServer():
     return False
 
 
+def shouldSkip():
+  if os.environ.get('ALLOW_EXTERNAL_BUILD_SERVER', None):
+    return False
+  return pollServer()
+
+
 def callServer(args, check=True):
   return subprocess.run([str(server_utils.SERVER_SCRIPT.absolute())] + args,
                         cwd=pathlib.Path(__file__).parent,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.STDOUT,
                         check=check,
-                        text=True)
+                        text=True,
+                        timeout=3)
 
 
 @contextlib.contextmanager
@@ -82,10 +89,7 @@ class ServerStartedTest(unittest.TestCase):
     self._build_id = None
 
   def setUp(self):
-    if pollServer():
-      # TODO(mheikal): Support overriding the standard named pipe for
-      # communicating with the server so that we can run an instance just for
-      # this test even if a real one is running.
+    if shouldSkip():
       self.skipTest("Cannot run test when server already running.")
     self._process = subprocess.Popen(
         [server_utils.SERVER_SCRIPT.absolute(), '--exit-on-idle', '--quiet'],
@@ -269,6 +273,10 @@ class ServerStartedTest(unittest.TestCase):
 
 
 class ServerNotStartedTest(unittest.TestCase):
+
+  def setUp(self):
+    if pollServer():
+      self.skipTest("Cannot run test when server already running.")
 
   def testWaitForBuildServerCall(self):
     proc_result = callServer(['--wait-for-build', 'invalid-build-id'])
