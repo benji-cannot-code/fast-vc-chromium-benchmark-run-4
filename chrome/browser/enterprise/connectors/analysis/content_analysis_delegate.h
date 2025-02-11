8e740be44fefad9fec3923154b50d90327cfcda0
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-02-10 16:28:36 -0800

    Create `ContentAnalysisInfo` interface
    This CL creates a generic data accessor implemented by
    `ContentAnalysisDelegate` that is used in `PrepareRequest`. This is done
    in anticipation of a broader refactor of `ContentAnalysisDelegate` where
    that method will be removed and instead used through new
    `RequestHandlerBase` sub-classes for print and paste.
    
    Bug: 40839522
    Change-Id: I3f7a2ab0d018e41bfb8b084623714f1a42911183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249720
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418358}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/enterprise/connectors/analysis/content_analysis_delegate_base.h"
+#include "chrome/browser/enterprise/connectors/analysis/content_analysis_info.h"
 #include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
@@ -87,7 +88,8 @@ class StringAnalysisRequest
 //     safe_browsing::ContentAnalysisDelegate::CreateForWebContents(
 //         contents, std::move(data), base::BindOnce(...));
 //   }
-class ContentAnalysisDelegate : public ContentAnalysisDelegateBase {
+class ContentAnalysisDelegate : public ContentAnalysisDelegateBase,
+                                public ContentAnalysisInfo {
  public:
   // Used as an input to CreateForWebContents() to describe what data needs
   // deeper scanning.  Any members can be empty.
@@ -285,6 +287,16 @@ class ContentAnalysisDelegate : public ContentAnalysisDelegateBase {
 
   void SetPageWarningForTesting(ContentAnalysisResponse page_response);
 
+  // ContentAnalysisInfo:
+  const AnalysisSettings& settings() const override;
+  int user_action_requests_count() const override;
+  std::string tab_title() const override;
+  std::string user_action_id() const override;
+  std::string email() const override;
+  std::string url() const override;
+  const GURL& tab_url() const override;
+  ContentAnalysisRequest::Reason reason() const override;
+
  protected:
   ContentAnalysisDelegate(content::WebContents* web_contents,
                           Data data,
@@ -424,6 +436,11 @@ class ContentAnalysisDelegate : public ContentAnalysisDelegateBase {
   // reporting.
   std::string GetContentTransferMethod() const;
 
+  // Returns `true` if `data_` contains corresponding data to be scanned, and
+  // when that data isn't too large to be exempt from scanning.
+  bool text_request_required() const;
+  bool image_request_required() const;
+
   // The Profile corresponding to the pending scan request(s).
   raw_ptr<Profile> profile_ = nullptr;
 
@@ -458,9 +475,6 @@ class ContentAnalysisDelegate : public ContentAnalysisDelegateBase {
   // Stores the scanned page's size since it moves from `data_` to be uploaded.
   // TODO(crbug.com/40839522): Move to PageRequestHandler.
   int64_t page_size_bytes_ = 0;
-
-  // Stores the total number of requests associated with one user action.
-  int64_t total_requests_count_ = 0;
 
   // Set to true once the scan of text has completed.  If the scan request has
   // no text requiring deep scanning, this is set to true immediately.
