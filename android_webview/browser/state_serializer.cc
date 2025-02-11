1b838a37cb25bb856f9cb22dd827240c16fb992a
Peter E Conn <peconn@chromium.org>
2025-02-11 07:11:39 -0800

    Reverse order of WebView saveState.
    We want to be able to truncate WebView's saveState - omitting earlier
    entries in the navigation history if it has grown too long. Given that
    we don't know the size of a NavigationEntry before serializing it,
    reversing the order of entries will allow us to truncate the list
    without doing any extra copies.
    
    Actually truncating the list will come in a follow up CL.
    
    Bug: 389076708
    Change-Id: Ic15def057d64284af7987dc91e0c5bea0e2e3d1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6252737
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418633}

--- android_webview/browser/state_serializer.cc
+++ android_webview/browser/state_serializer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/state_serializer.h"
 
+#include <algorithm>
 #include <memory>
 #include <string>
 #include <vector>
@@ -36,7 +37,7 @@ namespace android_webview {
 
 namespace {
 
-const uint32_t AW_STATE_VERSION = internal::AW_STATE_VERSION_DATA_URL;
+const uint32_t AW_STATE_VERSION = internal::AW_STATE_VERSION_REVERSE_ENTRIES;
 
 // The production implementation of NavigationHistory and NavigationHistorySink,
 // backed by a NavigationController.
@@ -87,9 +88,15 @@ bool RestoreFromPickle(base::PickleIterator* iterator,
 namespace internal {
 
 void WriteToPickle(NavigationHistory& history, base::Pickle* pickle) {
+  WriteToPickle(AW_STATE_VERSION, history, pickle);
+}
+
+void WriteToPickle(uint32_t state_version,
+                   NavigationHistory& history,
+                   base::Pickle* pickle) {
   DCHECK(pickle);
 
-  internal::WriteHeaderToPickle(pickle);
+  internal::WriteHeaderToPickle(state_version, pickle);
 
   const int entry_count = history.GetEntryCount();
   const int selected_entry = history.GetCurrentEntry();
@@ -101,8 +108,18 @@ void WriteToPickle(NavigationHistory& history, base::Pickle* pickle) {
 
   pickle->WriteInt(entry_count);
   pickle->WriteInt(selected_entry);
+
   for (int i = 0; i < entry_count; ++i) {
-    internal::WriteNavigationEntryToPickle(*history.GetEntryAtIndex(i), pickle);
+    // Since AW_STATE_VERSION_REVERSE_ENTRIES, entries are written in reverse
+    // order. This allows (in an upcoming change, see
+    // http://crbug.com/389076708) us to easily drop the earlier navigation
+    // entries if the state is getting too big.
+    int index = state_version < AW_STATE_VERSION_REVERSE_ENTRIES
+                    ? i
+                    : (entry_count - i - 1);
+
+    internal::WriteNavigationEntryToPickle(
+        state_version, *history.GetEntryAtIndex(index), pickle);
   }
 
   // Please update AW_STATE_VERSION and IsSupportedVersion() if serialization
@@ -133,7 +150,8 @@ uint32_t RestoreHeaderFromPickle(base::PickleIterator* iterator) {
 
 bool IsSupportedVersion(uint32_t state_version) {
   return state_version == internal::AW_STATE_VERSION_INITIAL ||
-         state_version == internal::AW_STATE_VERSION_DATA_URL;
+         state_version == internal::AW_STATE_VERSION_DATA_URL ||
+         state_version == internal::AW_STATE_VERSION_REVERSE_ENTRIES;
 }
 
 void WriteNavigationEntryToPickle(content::NavigationEntry& entry,
@@ -220,6 +238,10 @@ bool RestoreFromPickle(base::PickleIterator* iterator,
             state_version, iterator, entries[i].get(), context.get())) {
       return false;
     }
+  }
+
+  if (state_version >= AW_STATE_VERSION_REVERSE_ENTRIES) {
+    std::reverse(entries.begin(), entries.end());
   }
 
   // |web_contents| takes ownership of these entries after this call.
