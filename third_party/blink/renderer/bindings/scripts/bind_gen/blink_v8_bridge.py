a0c25eb0f428b8f8e7bc9d53bd3858afbc24d096
Andrey Kosyakov <caseq@chromium.org>
2025-02-10 16:32:41 -0800

    Allow overriding webidl union names with human- and filesystem- friendly ids
    Introduce third_party/blink/renderer/bindings/union_name_map.conf to allow
    specifying custom generated union names.
    
    Bug: 394123703
    Change-Id: Ic3f124d5c5486462990690cd3aeceda897b062f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6250938
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418361}

--- third_party/blink/renderer/bindings/scripts/bind_gen/blink_v8_bridge.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/blink_v8_bridge.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import web_idl
 
 from . import name_style
+from .union_name_mapper import UnionNameMapper
 from .code_node import FormatNode
 from .code_node import Likeliness
 from .code_node import SymbolDefinitionNode
@@ -38,6 +39,9 @@ def blink_class_name(idl_definition):
             idl_definition.element_type.
             type_name_with_extended_attribute_key_values)
     elif isinstance(idl_definition, web_idl.Union):
+        # See if there are overrides.
+        if class_name := UnionNameMapper.instance().class_name(idl_definition):
+            return class_name
         # Technically this name is not guaranteed to be unique because
         # (X or sequence<Y or Z>) and (X or Y or sequence<Z>) have the same
         # name, but it's highly unlikely to cause a conflict in the actual use
