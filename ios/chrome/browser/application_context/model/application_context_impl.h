78229cba66e96c8d53d8589ffbae11a8ef38d67a
Sylvain Defresne <sdefresne@chromium.org>
2025-02-11 02:56:18 -0800

    [ios] Fix a shutdown crash in GCMProfileService & GCMDriver
    Use BindPostTask(...) and base::WeakPtr<...> to ensure that the
    thread hopping for the callback passed to GCMProfileService and
    to GCMDriver do not risk calling method on deleted objects.
    
    Bug: 393549084
    Change-Id: I90f417cb975f191a7b172f9590a5c082bfbfe01f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249020
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418543}

--- ios/chrome/browser/application_context/model/application_context_impl.h
+++ ios/chrome/browser/application_context/model/application_context_impl.h
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "ios/chrome/browser/shared/model/application_context/application_context.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "services/network/public/mojom/proxy_resolving_socket.mojom.h"
 
 namespace base {
 class CommandLine;
@@ -120,6 +123,11 @@ class ApplicationContextImpl : public ApplicationContext {
   // Create the gcm driver.
   void CreateGCMDriver();
 
+  // Requests a network::mojom::ProxyResolvingSocketFactory.
+  void RequestProxyResolvingSocketFactory(
+      mojo::PendingReceiver<network::mojom::ProxyResolvingSocketFactory>
+          receiver);
+
   SEQUENCE_CHECKER(sequence_checker_);
 
   // Used internally for tracking whether the call to StartTearDown() has
@@ -181,6 +189,9 @@ class ApplicationContextImpl : public ApplicationContext {
   scoped_refptr<optimization_guide::OnDeviceModelServiceController>
       on_device_model_service_controller_;
 #endif  // BUILD_WITH_INTERNAL_OPTIMIZATION_GUIDE
+
+  // Must be the last member variable.
+  base::WeakPtrFactory<ApplicationContextImpl> weak_ptr_factory_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_APPLICATION_CONTEXT_MODEL_APPLICATION_CONTEXT_IMPL_H_
