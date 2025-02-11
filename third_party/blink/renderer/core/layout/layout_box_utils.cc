c666b03625fa5bc224b8ce5f726a69ec4f39caca
Morten Stenshorne <mstensho@chromium.org>
2025-02-10 22:47:12 -0800

    Move LayoutBox::SkipContainingBlockForPercentHeightCalculation.
    Move it into LayoutInputNode. It was only used there, via an awkward
    LayoutBox-friendship in LayoutBoxUtils, which is now no longer needed.
    
    Change-Id: I9105346f3d5c3ed3bfce509fe79dad719087da42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244300
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418485}

--- third_party/blink/renderer/core/layout/layout_box_utils.cc
+++ third_party/blink/renderer/core/layout/layout_box_utils.cc
@@ -19,11 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool LayoutBoxUtils::SkipContainingBlockForPercentHeightCalculation(
-    const LayoutBlock* cb) {
-  return LayoutBox::SkipContainingBlockForPercentHeightCalculation(cb);
-}
-
 LayoutUnit LayoutBoxUtils::InlineSize(const LayoutBox& box) {
   DCHECK_GT(box.PhysicalFragmentCount(), 0u);
 
--- third_party/blink/renderer/core/layout/layout_box_utils.h
+++ third_party/blink/renderer/core/layout/layout_box_utils.h
@@ -12,7 +12,6 @@ namespace blink {
 
 class BlockBreakToken;
 class LayoutBox;
-class LayoutBlock;
 class LayoutPoint;
 class PhysicalBoxFragment;
 struct PhysicalOffset;
@@ -23,9 +22,6 @@ class LayoutBoxUtils {
   STATIC_ONLY(LayoutBoxUtils);
 
  public:
-  static bool SkipContainingBlockForPercentHeightCalculation(
-      const LayoutBlock* cb);
-
   static LayoutUnit InlineSize(const LayoutBox& box);
 
   // The total block size of all fragments.
