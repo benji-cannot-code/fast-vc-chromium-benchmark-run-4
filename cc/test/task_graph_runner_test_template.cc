606a3626edb6b544b2dfb5ead05a1a689f97089c
Sorin Jianu <sorin@chromium.org>
2025-02-11 09:20:59 -0800

    cc: use begin/end instead of + std::size in //cc
    This resolves some unsafe buffers issues as well.
    
    Bug: 392116519
    Change-Id: I998d3d6df453320a097f2c0f03033f8f238a9811
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249206
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418703}

--- cc/test/task_graph_runner_test_template.cc
+++ cc/test/task_graph_runner_test_template.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-const int TaskGraphRunnerTestBase::kNamespaceCount;
+TaskGraphRunnerTestBase::TaskGraphRunnerTestBase() = default;
+TaskGraphRunnerTestBase::~TaskGraphRunnerTestBase() = default;
 
 void TaskGraphRunnerTestBase::SetTaskGraphRunner(
     TaskGraphRunner* task_graph_runner) {
--- cc/test/task_graph_runner_test_template.h
+++ cc/test/task_graph_runner_test_template.h
@@ -3,16 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef CC_TEST_TASK_GRAPH_RUNNER_TEST_TEMPLATE_H_
 #define CC_TEST_TASK_GRAPH_RUNNER_TEST_TEMPLATE_H_
 
-#include "cc/raster/task_graph_runner.h"
-
+#include <array>
 #include <vector>
 
 #include "base/functional/bind.h"
@@ -20,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/threading/simple_thread.h"
 #include "cc/raster/task_category.h"
+#include "cc/raster/task_graph_runner.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace cc {
@@ -51,7 +46,8 @@ class TaskGraphRunnerTestBase {
     bool has_external_dependency;
   };
 
-  TaskGraphRunnerTestBase() {}
+  TaskGraphRunnerTestBase();
+  ~TaskGraphRunnerTestBase();
 
   void SetTaskGraphRunner(TaskGraphRunner* task_graph_runner);
   void ResetIds(int namespace_index);
@@ -64,7 +60,7 @@ class TaskGraphRunnerTestBase {
   void ScheduleTasks(int namespace_index, const std::vector<TaskInfo>& tasks);
   void ExternalDependencyCompletedForTask(int namespace_index, int task_index);
 
-  static const int kNamespaceCount = 3;
+  static constexpr int kNamespaceCount = 3;
 
  protected:
   class FakeTaskImpl : public Task {
@@ -106,8 +102,8 @@ class TaskGraphRunnerTestBase {
     ~FakeDependentTaskImpl() override {}
   };
 
-  raw_ptr<TaskGraphRunner> task_graph_runner_;
-  NamespaceToken namespace_token_[kNamespaceCount];
+  raw_ptr<TaskGraphRunner> task_graph_runner_ = nullptr;
+  std::array<NamespaceToken, kNamespaceCount> namespace_token_;
   Task::Vector tasks_[kNamespaceCount];
   Task::Vector dependents_[kNamespaceCount];
   std::vector<unsigned> run_task_ids_[kNamespaceCount];
@@ -124,8 +120,9 @@ class TaskGraphRunnerTest : public TaskGraphRunnerTestBase,
     delegate_.StartTaskGraphRunner();
     SetTaskGraphRunner(delegate_.GetTaskGraphRunner());
 
-    for (int i = 0; i < kNamespaceCount; ++i)
-      namespace_token_[i] = task_graph_runner_->GenerateNamespaceToken();
+    for (NamespaceToken& namespace_token : namespace_token_) {
+      namespace_token = task_graph_runner_->GenerateNamespaceToken();
+    }
   }
   void TearDown() override {
     delegate_.StopTaskGraphRunner();
@@ -327,8 +324,8 @@ TYPED_TEST_P(SingleThreadTaskGraphRunnerTest, Priority) {
         TaskInfo(i, 0u, 2u, 1u, 0u, 1u, false),  // Priority 1
         TaskInfo(i, 1u, 3u, 1u, 0u, 0u, false)   // Priority 0
     };
-    this->ScheduleTasks(i,
-                        std::vector<TaskInfo>(tasks, tasks + std::size(tasks)));
+    this->ScheduleTasks(
+        i, std::vector<TaskInfo>(std::begin(tasks), std::end(tasks)));
   }
 
   for (int i = 0; i < kNamespaceCount; ++i) {
