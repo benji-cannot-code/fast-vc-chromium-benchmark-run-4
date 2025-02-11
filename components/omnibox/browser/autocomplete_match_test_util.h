8de750a54fcbab208ad9c6e3370e0a3be05e2f18
Christian Xu <christianxu@google.com>
2025-02-11 06:00:10 -0800

    [Omnibox] Add CreateActionInSuggestMatch test helper
    Add helper to create an action in suggest autocomplete match.
    
    Bug: None
    Change-Id: I1e707616645aab0a1e14b960a8954d6c547fe0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6253017
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418598}

--- components/omnibox/browser/autocomplete_match_test_util.h
+++ components/omnibox/browser/autocomplete_match_test_util.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "third_party/omnibox_proto/answer_type.pb.h"
+#include "third_party/omnibox_proto/entity_info.pb.h"
 
 AutocompleteMatch CreateAutocompleteMatch(std::string name,
                                           AutocompleteMatchType::Type type,
@@ -26,6 +27,10 @@ AutocompleteMatch CreateSearchMatch(std::u16string contents = u"text");
 AutocompleteMatch CreateStarterPackMatch(std::u16string keyword);
 
 AutocompleteMatch CreateFeaturedEnterpriseSearch(std::u16string keyword);
+
+AutocompleteMatch CreateActionInSuggestMatch(
+    std::u16string description,
+    std::vector<omnibox::ActionInfo::ActionType> action_types);
 
 AutocompleteMatch CreateSearchMatch(std::string name,
                                     bool allowed_to_be_default_match,
