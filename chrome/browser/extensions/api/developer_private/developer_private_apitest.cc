3f9299222840ffb26801a8c414a7955db3b7f846
Keigo Oka <oka@google.com>
2025-02-10 20:56:11 -0800

    extensions: Factor out DeveloperPrivateEventRouter and DeveloperPrivateAPI
    * Move DeveloperPrivateEventRouter to its own file. This file also
      contains two utility functions used from multiple places in the
      extensions::util namespace.
    * Move API function declarations and implementations in a separate file
      (developer_private_functions.h/cc). We update the .idl file
      accordingly to refer to this file.
    * Now the developer_private_api.h/cc only contains the
      DeveloperPrivateAPI class.
    
    This is a pure refactoring to ease piecemeal migration of the APIs
    on Android.
    
    Bug: 392777363
    Change-Id: I8353f98ad2a8c5293b357f305d06f31b57917fd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6243013
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418471}

--- chrome/browser/extensions/api/developer_private/developer_private_apitest.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_apitest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/devtools/devtools_window_testing.h"
-#include "chrome/browser/extensions/api/developer_private/developer_private_api.h"
+#include "chrome/browser/extensions/api/developer_private/developer_private_functions.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/extensions/manifest_v2_experiment_manager.h"
