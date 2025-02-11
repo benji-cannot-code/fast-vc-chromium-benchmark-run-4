5717d5c9001cda7eff02e07cd0e071c35732f1d4
Janice Liu <janiceliu@chromium.org>
2025-02-11 14:32:50 -0800

    [Blob URL] Add DevTools Issues for Fetching Partitioned Blob URL
    This DevTools Issues indicates when a Blob URL fetch was blocked due to
    storage partitioning. A separate CL will be needed to add these issues
    to the DevTools issue panel in the frontend.
    
    Bug: 372883697
    
    Change-Id: Ia5e1a55d3d09930b0923e7ef810a2601054b4bed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216461
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Janice Liu <janiceliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418921}

--- storage/browser/blob/blob_url_registry.cc
+++ storage/browser/blob/blob_url_registry.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/features.h"
 #include "storage/browser/blob/blob_url_store_impl.h"
 #include "storage/browser/blob/blob_url_utils.h"
+#include "third_party/blink/public/mojom/devtools/inspector_issue.mojom.h"
 #include "url/gurl.h"
 
 namespace storage {
@@ -34,13 +35,15 @@ void BlobUrlRegistry::AddReceiver(
     const url::Origin& renderer_origin,
     int render_process_host_id,
     mojo::PendingAssociatedReceiver<blink::mojom::BlobURLStore> receiver,
-    base::RepeatingClosure partitioned_fetch_failure_closure,
+    base::RepeatingCallback<void(const GURL&,
+                                 blink::mojom::PartitioningBlobURLInfo)>
+        partitioning_blob_url_closure,
     bool partitioning_disabled_by_policy) {
   mojo::ReceiverId receiver_id = frame_receivers_.Add(
       std::make_unique<storage::BlobURLStoreImpl>(
           storage_key, renderer_origin, render_process_host_id, AsWeakPtr(),
           storage::BlobURLValidityCheckBehavior::DEFAULT,
-          std::move(partitioned_fetch_failure_closure),
+          std::move(partitioning_blob_url_closure),
           partitioning_disabled_by_policy),
       std::move(receiver));
 
