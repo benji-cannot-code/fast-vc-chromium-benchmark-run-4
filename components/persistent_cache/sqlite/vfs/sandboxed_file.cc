cd5f7d93542c43e63f9b6ef397cbd5818ce39228
Olivier Li <olivierli@chromium.org>
2025-02-11 08:35:53 -0800

    [persistent cache]: Add sandboxed files vfs.
    This creates a sql::SandboxedVfs::Delegate implementation that can map a
    set of files to labels to allow sqlite to use these labels directly
    without needing to know about the file paths.
    
    This is necessary to the creation of an SQLite backend that can be used
    in sandboxed processes.
    
    Bug: 377475540
    Change-Id: I477489c4c9293528be4d3bffd4420ed1d726ed43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6214070
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418684}

---
+++ components/persistent_cache/sqlite/vfs/sandboxed_file.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/persistent_cache/sqlite/vfs/sandboxed_file.h"
+
+namespace persistent_cache {
+
+SandboxedFile::SandboxedFile(base::File file, AccessRights access_rights)
+    : underlying_file_(std::move(file)), access_rights_(access_rights) {}
+SandboxedFile::~SandboxedFile() = default;
+
+SandboxedFile::SandboxedFile(SandboxedFile&& other) = default;
+SandboxedFile& SandboxedFile::operator=(SandboxedFile&& other) = default;
+
+SandboxedFile SandboxedFile::Copy() const {
+  return SandboxedFile(DuplicateUnderlyingFile(), access_rights_);
+}
+
+base::File SandboxedFile::DuplicateUnderlyingFile() const {
+  return underlying_file_.Duplicate();
+}
+
+}  // namespace persistent_cache
