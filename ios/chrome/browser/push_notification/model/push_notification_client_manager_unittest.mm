c80e3cbc38b468614f9f888183587f1189496ff9
Scott Yoder <scottyoder@google.com>
2025-02-11 08:13:12 -0800

    [ios] Enable IOSTipsNotifications by default
    Bug: 41490310
    Change-Id: I4356ef05523f941adc8f1e2e1448afa75ecfa04c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6248267
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418673}

--- ios/chrome/browser/push_notification/model/push_notification_client_manager_unittest.mm
+++ ios/chrome/browser/push_notification/model/push_notification_client_manager_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/task_environment.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_client.h"
 #import "ios/chrome/browser/push_notification/model/test_push_notification_client.h"
+#import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #import "testing/platform_test.h"
 
 namespace {
@@ -50,6 +51,7 @@ class PushNotificationClientManagerTest : public PlatformTest {
   const scoped_refptr<base::SequencedTaskRunner> main_task_runner_{
       base::SequencedTaskRunner::GetCurrentDefault()};
 
+  IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   std::unique_ptr<PushNotificationClientManager> manager_ =
       std::make_unique<PushNotificationClientManager>(main_task_runner_);
 };
--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -957,7 +957,7 @@ BASE_FEATURE(kInactiveNavigationAfterAppLaunchKillSwitch,
 
 BASE_FEATURE(kIOSTipsNotifications,
              "IOSTipsNotifications",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const char kIOSTipsNotificationsUnknownTriggerTimeParam[] =
     "unknown_trigger_time";
