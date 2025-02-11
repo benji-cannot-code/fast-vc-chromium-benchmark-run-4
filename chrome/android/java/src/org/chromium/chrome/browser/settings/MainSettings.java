353245dd357df13f08cbb8066aa46c6a0117e27d
Yue Zhang <yuezhanggg@chromium.org>
2025-02-10 16:34:33 -0800

    Revert "Enable Bottom Toolbar preference on folded Foldable devices."
    This reverts commit fda728c4340494f544d3455b3af232ba33b5bfaa.
    
    Reason for revert: Causing failure on several android bots. See b/395725506 for more details.
    
    Original change's description:
    > Enable Bottom Toolbar preference on folded Foldable devices.
    >
    > Change-Id: Ibe456abc0a93ee81b0b9767887072c19fdf2b9ca
    > Fixed: b:383310106
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244914
    > Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1418319}
    
    Change-Id: Ia51ec323f9a8857e1bc78353dbccdf054baf6665
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6251339
    Auto-Submit: Yue Zhang <yuezhanggg@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418364}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
@@ -8,7 +8,6 @@ package org.chromium.chrome.browser.settings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -73,7 +72,6 @@ import org.chromium.components.signin.AccountManagerFacadeProvider;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.sync.SyncService;
 import org.chromium.components.user_prefs.UserPrefs;
-import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 
 import java.util.HashMap;
@@ -195,13 +193,6 @@ public class MainSettings extends ChromeBaseSettingsFragment
     public void onResume() {
         super.onResume();
         updatePreferences();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Ensure the preference disabled state is reflected when device is folded or unfolded.
-        updateAddressBarPreference();
     }
 
     private void createPreferences() {
@@ -527,16 +518,7 @@ public class MainSettings extends ChromeBaseSettingsFragment
     }
 
     private void updateAddressBarPreference() {
-        // Similar to ToolbarPositionController#isToolbarPositionCustomizationEnabled(), except
-        // - no CCT checks (settings are not accessible from CCTs),
-        // - showing on Foldables in unfolded (open) state.
-        boolean showSetting =
-                ChromeFeatureList.sAndroidBottomToolbar.isEnabled()
-                        && (BuildInfo.getInstance().isFoldable
-                                || !DeviceFormFactor.isNonMultiDisplayContextOnTablet(
-                                        getContext()));
-
-        if (showSetting) {
+        if (ToolbarPositionController.isToolbarPositionCustomizationEnabled(getContext(), false)) {
             Preference addressBarPreference = addPreferenceIfAbsent(PREF_ADDRESS_BAR);
             addressBarPreference.setSummary(ToolbarPositionController.getToolbarPositionResId());
         } else {
