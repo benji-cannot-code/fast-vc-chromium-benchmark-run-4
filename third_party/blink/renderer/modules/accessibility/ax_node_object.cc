0dcd07ef1fac3922bb0f902ba5c631b346c36ca8
Addison Luh <aluh@chromium.org>
2025-02-10 18:41:34 -0800

    [a11y] Add UMA for kSetValue usage
    Adds the `Accessibility.SetValue.Role` histogram to measure whether set
    value is being used by ATs. This will help determine whether to
    deprecate the kSetValue enum.
    
    Bug: b:394684977
    Change-Id: Ic392f148b120bf13f87f20c2ee725cfa62eb8ca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237720
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Addison Luh <aluh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418449}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/containers/contains.h"
 #include "base/containers/fixed_flat_set.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/numerics/safe_conversions.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/input/web_keyboard_event.h"
@@ -4708,11 +4709,15 @@ bool AXNodeObject::OnNativeSetSelectedAction(bool selected) {
 }
 
 bool AXNodeObject::OnNativeSetValueAction(const String& string) {
-  if (!GetNode() || !GetNode()->IsElementNode())
+  base::UmaHistogramEnumeration("Accessibility.SetValue.Role", RoleValue());
+
+  if (!GetNode() || !GetNode()->IsElementNode()) {
     return false;
+  }
   const LayoutObject* layout_object = GetLayoutObject();
-  if (!layout_object || !layout_object->IsBoxModelObject())
+  if (!layout_object || !layout_object->IsBoxModelObject()) {
     return false;
+  }
 
   auto* html_input_element = DynamicTo<HTMLInputElement>(*GetNode());
   if (html_input_element && layout_object->IsTextField()) {
