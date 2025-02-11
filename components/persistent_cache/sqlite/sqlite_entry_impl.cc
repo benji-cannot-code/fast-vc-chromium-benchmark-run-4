fd7546fcb520a85a8688bda2dfec5459d98603cd
Olivier Li <olivierli@chromium.org>
2025-02-11 08:42:35 -0800

    [persistent cache]: Add SqliteEntryImpl.
    This adds an sqlite specific implementation of persistent_cache::Entry.
    This initial implementation copies whatever content it is given into an
    internal buffer. This is a minimally viable implementation that incurs
    the copy cost but avoids having to deal with manual lifetime management
    of the value.
    
    Bug: 377475540
    Change-Id: Ic78ed5eb066fdb9129a4d64e985304f583ee5350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219858
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418691}

---
+++ components/persistent_cache/sqlite/sqlite_entry_impl.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/persistent_cache/sqlite/sqlite_entry_impl.h"
+
+#include <cstdint>
+
+#include "base/containers/span.h"
+
+namespace persistent_cache {
+
+SqliteEntryImpl::SqliteEntryImpl(std::string&& content)
+    : content_(std::move(content)) {}
+
+SqliteEntryImpl::~SqliteEntryImpl() = default;
+
+base::span<const uint8_t> SqliteEntryImpl::GetContentSpan() const {
+  return base::as_byte_span(content_);
+}
+
+}  // namespace persistent_cache
