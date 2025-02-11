987327ef429bd910af892440bd39d425f07a2743
Avi Drissman <avi@chromium.org>
2025-02-10 17:38:22 -0800

    Centralize the compatibility shimming for services
    There are two places that Services are handled (Views and Web) and
    both have to work around the same compatibility bugs. Centralize the
    handling for the bugs.
    
    This also generalizes the compatibility workaround. Will this help
    with Writing Tools? Unknown, but generalizing the workaround can't
    hurt.
    
    Bug: 385170286
    Change-Id: I8e575daa0e8f4d64edfb5a9ad1890d23c54f401c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6245493
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418420}

--- ui/base/cocoa/appkit_utils.mm
+++ ui/base/cocoa/appkit_utils.mm
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ui/base/cocoa/appkit_utils.h"
 
+#import <Cocoa/Cocoa.h>
+#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
+
 #include <cmath>
 
 #include "base/mac/mac_util.h"
@@ -31,6 +34,37 @@ bool ForceClickInvokesQuickLook() {
 
 bool IsCGFloatEqual(CGFloat a, CGFloat b) {
   return std::fabs(a - b) <= std::numeric_limits<CGFloat>::epsilon();
+}
+
+UTType* UTTypeForServicesType(NSString* type) {
+  if (!type) {
+    return nil;
+  }
+
+  // `type` is either a modern UTType identifier, or an obsolete Pboard type.
+  // Try one, then the other.
+  UTType* uttype = [UTType typeWithIdentifier:type];
+  if (uttype) {
+    return uttype;
+  }
+
+  // The UTType API provides two UTTagClass values: UTTagClassFilenameExtension
+  // and UTTagClassMIMEType, but there are other internal ones. This is one of
+  // the private internal tag classes, the tag class that corresponds to the
+  // deprecated kUTTagClassNSPboardType.
+  return [UTType typeWithTag:type
+                    tagClass:@"com.apple.nspboard-type"
+            conformingToType:nil];
+}
+
+NSSet<UTType*>* UTTypesForServicesTypeArray(NSArray* types) {
+  NSMutableSet* result = [NSMutableSet set];
+  for (NSString* type in types) {
+    if (UTType* uttype = UTTypeForServicesType(type)) {
+      [result addObject:uttype];
+    }
+  }
+  return result;
 }
 
 }  // namespace ui
