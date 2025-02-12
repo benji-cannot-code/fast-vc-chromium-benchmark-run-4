0cd030140def08727281f162982e83565f9b56be
Anna Tsvirchkova <atsvirchkova@google.com>
2025-02-12 06:53:46 -0800

    Fix PasswordBubbleControllerBase::OnBubbleClosing crash
    The problem is that UpdateBubbleAndIconVisibility is triggered as a
    result of chain of calls from SuccessfulPasswordChangeView's destructor.
    UpdateBubbleAndIconVisibility contains the call to close the current
    bubble (if any), and the problem is that the current bubble is half
    destroyed already. To avoid this behavior, the global static variable
    g_manage_passwords_bubble_ needs to be reset earlier. Now we do that in
    PasswordBubbleViewBase::~PasswordBubbleViewBase.
    This CL resets g_manage_passwords_bubble_ in window closing callback
    before triggering OnBubbleHidden.
    
    Bug: 395820295
    Change-Id: I3e0ea68330fcb45625646220e626714fff11fd3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6253282
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419263}

--- chrome/browser/ui/views/passwords/password_bubble_view_base.cc
+++ chrome/browser/ui/views/passwords/password_bubble_view_base.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/passwords/password_bubble_view_base.h"
 
+#include "base/functional/bind.h"
 #include "base/notreached.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -105,6 +106,8 @@ void PasswordBubbleViewBase::ShowBubble(content::WebContents* web_contents,
   views::BubbleDialogDelegateView::CreateBubble(g_manage_passwords_bubble_);
 
   g_manage_passwords_bubble_->ShowForReason(reason);
+  g_manage_passwords_bubble_->RegisterWindowClosingCallback(
+      base::BindOnce([]() { g_manage_passwords_bubble_ = nullptr; }));
 
   if (features::IsToolbarPinningEnabled()) {
     auto* passwords_action_item = actions::ActionManager::Get().FindAction(
@@ -254,9 +257,7 @@ PasswordBubbleViewBase::PasswordBubbleViewBase(
 }
 
 PasswordBubbleViewBase::~PasswordBubbleViewBase() {
-  if (g_manage_passwords_bubble_ == this) {
-    g_manage_passwords_bubble_ = nullptr;
-  }
+  CHECK(this != g_manage_passwords_bubble_);
   // It is possible in tests for |browser_| not to exist.
   if (features::IsToolbarPinningEnabled() && browser_) {
     auto* passwords_action_item = actions::ActionManager::Get().FindAction(
