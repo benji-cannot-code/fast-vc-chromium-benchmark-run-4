917c24e33d81a9f45004998998fb2bb9c894b4a8
François Degros <fdegros@chromium.org>
2025-02-11 19:18:33 -0800

    Files app: Remove the files-kernel-drivers feature flag
    This reverts commit 51295a1e5809210864f8e83b554041d825f9d720.
    This reverts commit d9c79fb69830014942b68985685e11871dc5c752.
    This reverts commit e39d738fafd0335716b93d5b34f7b5618a22c14e.
    
    BUG=b:364409158
    TEST=chromeos_unittests --gtest_filter=CrosDisksClientTest.*
    
    Change-Id: I5d583ae460c5953a9b60cfe3dab16f0da188d7e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187569
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419068}

--- chromeos/ash/components/dbus/cros_disks/cros_disks_client.cc
+++ chromeos/ash/components/dbus/cros_disks/cros_disks_client.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <utility>
 
-#include "ash/constants/ash_features.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/files/file_path.h"
@@ -890,12 +889,6 @@ std::vector<std::string> CrosDisksClient::ComposeMountOptions(
   if (!mount_label.empty()) {
     options.push_back(base::StrCat({"mountlabel=", mount_label}));
   }
-
-  // TODO(b/364409158) Remove with files-kernel-drivers feature flag.
-  options.push_back(base::StrCat(
-      {"prefer-driver=",
-       base::FeatureList::IsEnabled(features::kFilesKernelDrivers) ? "kernel"
-                                                                   : "fuse"}));
 
   return options;
 }
