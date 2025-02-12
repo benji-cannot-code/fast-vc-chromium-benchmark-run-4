dd5570f5ea95164272a3b3ae4467734b17096c59
Jérôme Lebel <jlebel@chromium.org>
2025-02-12 05:51:53 -0800

    [iOS][MIM2] Remove unused AuthenticationFlowDelegate protocol
    This protocol was used the owner of AuthenticationFlow as blocking
    the window instead of view.
    When AuthenticationFlow needed to display a dialog, the window had to
    be unblocked so the user could validate the dialog.
    
    AuthenticationFlow.delegate's methods are not called anymore by
    AuthenticationFlow.
    
    AuthenticationFlow is used by AccountMenuCoordinator,
    ConsistencyPromoSigninCoordinator, InstantSigninCoordinator and
    SigninScreenCoordinator.
    None of them are block the window anymore.
    
    For those 2 reasons, AuthenticationFlowDelegate is not needed anymore.
    
    Bug: 375604649
    Change-Id: Idc4b8b8737f188eb2c732e544ac69ddd79263a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6253694
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419240}

--- ios/chrome/browser/authentication/ui_bundled/signin/instant_signin/instant_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/instant_signin/instant_signin_coordinator.mm
@@ -26,8 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 
-@interface InstantSigninCoordinator () <AuthenticationFlowDelegate,
-                                        IdentityChooserCoordinatorDelegate,
+@interface InstantSigninCoordinator () <IdentityChooserCoordinatorDelegate,
                                         InstantSigninMediatorDelegate>
 
 @end
@@ -192,16 +191,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
-#pragma mark - AuthenticationFlowDelegate
-
-- (void)didPresentDialog {
-  [self removeActivityOverlay];
-}
-
-- (void)didDismissDialog {
-  [self showActivityOverlay];
-}
-
 #pragma mark - IdentityChooserCoordinatorDelegate
 
 - (void)identityChooserCoordinatorDidClose:
@@ -286,7 +275,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                       accessPoint:self.accessPoint
                                 postSignInActions:postSigninActions
                          presentingViewController:self.baseViewController];
-  authenticationFlow.delegate = self;
   authenticationFlow.precedingHistorySync = YES;
   [_mediator startSignInOnlyFlowWithAuthenticationFlow:authenticationFlow];
 }
