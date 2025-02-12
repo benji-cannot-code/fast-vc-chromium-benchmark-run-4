0d20857bc08d73ae0b68bac99fd1bf866f9f4431
Christian Dullweber <dullweber@chromium.org>
2025-02-12 04:54:15 -0800

    Reland "Re-enable IncognitoTabLauncherTest"
    This is a reland of commit 6165df3c402a12c9c1d70d4dde52f1e68fd25e38
    Disable tests on automotive since they failed there last time.
    
    Original change's description:
    > Re-enable IncognitoTabLauncherTest
    >
    > Test Reviver thinks that these tests are fine.
    >
    > Bug: 40783511, 40789038, 355035028
    > Change-Id: I0cd50cbe3d9feb355dc3c3edd785f19f1d385e47
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098913
    > Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Mohamad Arab <arabm@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1397308}
    
    Bug: 40783511, 40789038, 355035028
    Change-Id: Ie29b47f5590722af19cc74b2ba575f2cde23c3dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6225942
    Commit-Queue: Mohamad Arab <arabm@google.com>
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419222}

--- chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoTabLauncherTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoTabLauncherTest.java
@@ -10,7 +10,6 @@ import static org.mockito.Mockito.doReturn;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.browser.customtabs.CustomTabsIntent;
@@ -28,10 +27,10 @@ import org.mockito.Mockito;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabsConnection;
 import org.chromium.chrome.browser.customtabs.CustomTabsTestUtils;
@@ -40,6 +39,7 @@ import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.components.externalauth.ExternalAuthUtils;
+import org.chromium.ui.test.util.DeviceRestriction;
 
 import java.util.concurrent.TimeoutException;
 
@@ -47,9 +47,6 @@ import java.util.concurrent.TimeoutException;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @EnableFeatures({ChromeFeatureList.ALLOW_NEW_INCOGNITO_TAB_INTENTS})
-@DisableIf.Build(
-        sdk_is_greater_than = Build.VERSION_CODES.O,
-        message = "Flaky, see crbug.com/1246132")
 public class IncognitoTabLauncherTest {
     @Rule
     public final ChromeTabbedActivityTestRule mActivityRule = new ChromeTabbedActivityTestRule();
@@ -77,7 +74,7 @@ public class IncognitoTabLauncherTest {
     @Test
     @Feature("Incognito")
     @MediumTest
-    @DisabledTest(message = "crbug.com/1237504")
+    @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO})
     public void testLaunchIncognitoNewTab() throws TimeoutException {
         ChromeTabbedActivity activity = launchIncognitoTab(false);
         assertIncognitoTabLaunched(activity, false);
@@ -86,7 +83,7 @@ public class IncognitoTabLauncherTest {
     @Test
     @Feature("Incognito")
     @MediumTest
-    @DisabledTest(message = "crbug.com/1237504")
+    @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO})
     public void testLaunchIncognitoNewTab_omniboxFocused_enabled_thirdParty()
             throws TimeoutException {
         ChromeTabbedActivity activity = launchIncognitoTab(false);
