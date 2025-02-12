ecb5ffde938bbc65e33e598fddf0d5a6d7613f70
David Sanders <dsanders11@ucsbalum.com>
2025-02-12 10:46:19 -0800

    Remove unused include in compose_metrics.h
    Bug: 40318405
    Change-Id: I5f54c2f40768746f10ef70e649fbc637be5b95f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6254187
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419402}

--- components/compose/core/browser/compose_metrics.h
+++ components/compose/core/browser/compose_metrics.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "components/compose/core/browser/compose_enums.mojom.h"
-#include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
 namespace base {
