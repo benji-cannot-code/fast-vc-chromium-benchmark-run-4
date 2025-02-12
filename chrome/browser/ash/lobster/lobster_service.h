a57d42852cddfe187329a6cb211b6afe95c73169
Chuong Ho <hdchuong@google.com>
2025-02-11 21:58:30 -0800

    lobster: reset previously declined consent when switching on settings
    Bug: b:395537359
    Change-Id: I3861fb9774ef598aea830f963eb6bd563d299202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6256237
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419100}

--- chrome/browser/ash/lobster/lobster_service.h
+++ chrome/browser/ash/lobster/lobster_service.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/lobster/lobster_system_state_provider.h"
 #include "components/account_id/account_id.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/prefs/pref_change_registrar.h"
+#include "components/prefs/pref_service.h"
 
 namespace manta {
 class SnapperProvider;
@@ -85,6 +87,8 @@ class LobsterService : public KeyedService, public LobsterEventSink {
   ash::LobsterBubbleCoordinator bubble_coordinator_;
 
   std::unique_ptr<LobsterInsertion> queued_insertion_;
+
+  PrefChangeRegistrar pref_change_registrar_;
 };
 
 #endif  // CHROME_BROWSER_ASH_LOBSTER_LOBSTER_SERVICE_H_
