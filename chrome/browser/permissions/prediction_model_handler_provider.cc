527b52540046385f0adee58cf7fad068743e2b71
Elias Klim <elklm@chromium.org>
2025-02-12 10:44:56 -0800

    [PermissionsAI] Do not init model handler if feature is disabled
    Bug: 395130841
    Change-Id: I91a12d9375862dc0763d4332a5bf798a3f9c8fbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6257858
    Auto-Submit: Elias Klim <elklm@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419401}

--- chrome/browser/permissions/prediction_model_handler_provider.cc
+++ chrome/browser/permissions/prediction_model_handler_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/permissions/genai_model_handler.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
+#include "components/permissions/features.h"
 #include "components/permissions/request_type.h"
 
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
@@ -21,23 +22,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace permissions {
 
 PredictionModelHandlerProvider::PredictionModelHandlerProvider(
-    OptimizationGuideKeyedService* optimization_guide)
-    : genai_model_handler_(
-          std::make_unique<GenAiModelHandler>(optimization_guide))
+    OptimizationGuideKeyedService* optimization_guide) {
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
-      ,
-      notification_prediction_model_handler_(
-          std::make_unique<PredictionModelHandler>(
-              optimization_guide,
-              optimization_guide::proto::OptimizationTarget::
-                  OPTIMIZATION_TARGET_NOTIFICATION_PERMISSION_PREDICTIONS)),
-      geolocation_prediction_model_handler_(
-          std::make_unique<PredictionModelHandler>(
-              optimization_guide,
-              optimization_guide::proto::OptimizationTarget::
-                  OPTIMIZATION_TARGET_GEOLOCATION_PERMISSION_PREDICTIONS))
+  notification_prediction_model_handler_ =
+      std::make_unique<PredictionModelHandler>(
+          optimization_guide,
+          optimization_guide::proto::OptimizationTarget::
+              OPTIMIZATION_TARGET_NOTIFICATION_PERMISSION_PREDICTIONS);
+
+  geolocation_prediction_model_handler_ =
+      std::make_unique<PredictionModelHandler>(
+          optimization_guide,
+          optimization_guide::proto::OptimizationTarget::
+              OPTIMIZATION_TARGET_GEOLOCATION_PERMISSION_PREDICTIONS);
 #endif  // BUILDFLAG(BUILD_WITH_TFLITE_LIB)
-{
+
+  if (base::FeatureList::IsEnabled(permissions::features::kPermissionsAIv1)) {
+    genai_model_handler_ =
+        std::make_unique<GenAiModelHandler>(optimization_guide);
+  }
 }
 
 PredictionModelHandlerProvider::~PredictionModelHandlerProvider() = default;
