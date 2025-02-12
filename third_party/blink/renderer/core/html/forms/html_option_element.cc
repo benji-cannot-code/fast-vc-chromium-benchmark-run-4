04037226b5a3b2ddf74a94f0388732fea8c8fbef
Joey Arhar <jarhar@chromium.org>
2025-02-12 08:03:46 -0800

    Don't render whitespace option label attributes
    This makes us match the spec and firefox's behavior.
    Context: https://github.com/whatwg/html/issues/10955
    
    Change-Id: I982c5413220e04ab5c93a46cc44981cb90083caa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6215230
    Reviewed-by: Traian Captan <tcaptan@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419307}

--- third_party/blink/renderer/core/html/forms/html_option_element.cc
+++ third_party/blink/renderer/core/html/forms/html_option_element.cc
@@ -157,6 +157,19 @@ bool HTMLOptionElement::MatchesEnabledPseudoClass() const {
 }
 
 String HTMLOptionElement::DisplayLabel() const {
+  if (RuntimeEnabledFeatures::OptionLabelAttributeWhitespaceEnabled()) {
+    // If the label attribute is set and is not an empty string, then use its
+    // value. Otherwise, use inner text.
+    String label_attr = String(FastGetAttribute(html_names::kLabelAttr));
+    if (!label_attr.empty()) {
+      return label_attr.StripWhiteSpace(IsHTMLSpace<UChar>)
+          .SimplifyWhiteSpace(IsHTMLSpace<UChar>);
+    }
+    return CollectOptionInnerText()
+        .StripWhiteSpace(IsHTMLSpace<UChar>)
+        .SimplifyWhiteSpace(IsHTMLSpace<UChar>);
+  }
+
   String label_attr = String(FastGetAttribute(html_names::kLabelAttr))
     .StripWhiteSpace(IsHTMLSpace<UChar>).SimplifyWhiteSpace(IsHTMLSpace<UChar>);
   String inner_text = CollectOptionInnerText()
@@ -164,7 +177,7 @@ String HTMLOptionElement::DisplayLabel() const {
   // FIXME: The following treats an element with the label attribute set to
   // the empty string the same as an element with no label attribute at all.
   // Is that correct? If it is, then should the label function work the same
-  // way? https://github.com/whatwg/html/issues/10955
+  // way?
   return label_attr.empty() ? inner_text : label_attr;
 }
 
