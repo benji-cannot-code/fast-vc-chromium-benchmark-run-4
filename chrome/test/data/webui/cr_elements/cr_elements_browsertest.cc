3eb1133cf1d6aeb4bdcb06f9443cbea827ca8093
rbpotter <rbpotter@chromium.org>
2025-02-12 14:03:03 -0800

    WebUI: Run relevant cr_elements_browsertests on Android
    Add cr_elements tests to Android builds, for all cr_elements that are
    included on Android and have browsertests. The tests added are for:
    
    CrLitElement (tests that depend on and test integration with Polymer
                  are excluded since Polymer is not included on Android)
    cr-button
    cr-tree
    cr-ripple and CrRippleMixin
    cr-splitter
    
    Note that cr-tabs is included on Android, but is tested via an
    interactive_ui_test. Adding interactive_ui_tests to Android builds to
    run this test will be done in a later CL.
    
    Bug: 40108641
    Change-Id: I4448fc96da192ffe8b832eb24d9c6c62285c0935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6253805
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419541}

--- chrome/test/data/webui/cr_elements/cr_elements_browsertest.cc
+++ chrome/test/data/webui/cr_elements/cr_elements_browsertest.cc
@@ -9,12 +9,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef WebUIMochaBrowserTest CrElementsTest;
 
-IN_PROC_BROWSER_TEST_F(CrElementsTest, CrA11yAnnouncer) {
-  RunTest("cr_elements/cr_a11y_announcer_test.js", "mocha.run()");
-}
-
 IN_PROC_BROWSER_TEST_F(CrElementsTest, CrButton) {
   RunTest("cr_elements/cr_button_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(CrElementsTest, CrRipple) {
+  RunTest("cr_elements/cr_ripple_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(CrElementsTest, CrRippleMixin) {
+  RunTest("cr_elements/cr_ripple_mixin_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(CrElementsTest, CrSplitter) {
+  RunTest("cr_elements/cr_splitter_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(CrElementsTest, CrTree) {
+  RunTest("cr_elements/cr_tree_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(CrElementsTest, CrLitElement) {
+  RunTest("cr_elements/cr_lit_element_test.js", "mocha.run()");
+}
+
+#if !BUILDFLAG(IS_ANDROID)
+IN_PROC_BROWSER_TEST_F(CrElementsTest, CrA11yAnnouncer) {
+  RunTest("cr_elements/cr_a11y_announcer_test.js", "mocha.run()");
 }
 
 IN_PROC_BROWSER_TEST_F(CrElementsTest, CrChip) {
@@ -108,14 +129,6 @@ IN_PROC_BROWSER_TEST_F(CrElementsTest, CrRadioButton) {
   RunTest("cr_elements/cr_radio_button_test.js", "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(CrElementsTest, CrRipple) {
-  RunTest("cr_elements/cr_ripple_test.js", "mocha.run()");
-}
-
-IN_PROC_BROWSER_TEST_F(CrElementsTest, CrRippleMixin) {
-  RunTest("cr_elements/cr_ripple_mixin_test.js", "mocha.run()");
-}
-
 IN_PROC_BROWSER_TEST_F(CrElementsTest, CrRippleMixinPolymer) {
   RunTest("cr_elements/cr_ripple_mixin_polymer_test.js", "mocha.run()");
 }
@@ -146,10 +159,6 @@ IN_PROC_BROWSER_TEST_F(CrElementsTest, CrSelectableMixin) {
 
 IN_PROC_BROWSER_TEST_F(CrElementsTest, CrSlider) {
   RunTest("cr_elements/cr_slider_test.js", "mocha.run()");
-}
-
-IN_PROC_BROWSER_TEST_F(CrElementsTest, CrSplitter) {
-  RunTest("cr_elements/cr_splitter_test.js", "mocha.run()");
 }
 
 IN_PROC_BROWSER_TEST_F(CrElementsTest, CrToast) {
@@ -188,10 +197,6 @@ IN_PROC_BROWSER_TEST_F(CrElementsTest, CrTooltip) {
   RunTest("cr_elements/cr_tooltip_test.js", "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(CrElementsTest, CrTree) {
-  RunTest("cr_elements/cr_tree_test.js", "mocha.run()");
-}
-
 IN_PROC_BROWSER_TEST_F(CrElementsTest, WebUiListenerMixin) {
   RunTest("cr_elements/web_ui_listener_mixin_test.js", "mocha.run()");
 }
@@ -208,10 +213,6 @@ IN_PROC_BROWSER_TEST_F(CrElementsTest, StoreClient) {
   RunTest("cr_elements/store_client_test.js", "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(CrElementsTest, CrLitElement) {
-  RunTest("cr_elements/cr_lit_element_test.js", "mocha.run()");
-}
-
 IN_PROC_BROWSER_TEST_F(CrElementsTest, CrLoadingGradient) {
   RunTest("cr_elements/cr_loading_gradient_test.js", "mocha.run()");
 }
@@ -219,3 +220,4 @@ IN_PROC_BROWSER_TEST_F(CrElementsTest, CrLoadingGradient) {
 IN_PROC_BROWSER_TEST_F(CrElementsTest, CrFeedbackButtons) {
   RunTest("cr_elements/cr_feedback_buttons_test.js", "mocha.run()");
 }
+#endif  // !BUILDFLAG(IS_ANDROID)
