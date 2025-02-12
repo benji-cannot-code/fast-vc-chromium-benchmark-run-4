8396aead953c51e2a676a2d604144414c0e8b869
Gyuyoung Kim <gyuyoung@igalia.com>
2025-02-12 01:57:17 -0800

    Add dummy Clipboard::Create for tvOS
    tvOS cannot share the iOS or macOS clipboard implementation due to the
    lack of supported APIs. Therefore, we add clipboard_tvos.mm, which
    contains no implementation, to enable building a content shell on tvOS.
    
    Bug: 391914246
    Change-Id: I238ba1c9695e74fbcff063d067de8805302193af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6248377
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1419174}

---
+++ ui/base/clipboard/clipboard_tvos.mm
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/notreached.h"
+#include "ui/base/clipboard/clipboard.h"
+
+namespace ui {
+
+Clipboard* Clipboard::Create() {
+  // TODO(crbug.com/391914246): Implement the Clipboard when tvOS actually
+  // requires the functionality.
+  NOTREACHED();
+}
+
+}  // namespace ui
