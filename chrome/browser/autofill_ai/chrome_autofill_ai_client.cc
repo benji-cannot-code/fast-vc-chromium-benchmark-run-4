2a2d954fd32edb7f94edf16a7da112dc7d7c6969
Bruno Braga <brunobraga@google.com>
2025-02-12 07:36:32 -0800

    [AutofillAi] Clean up save dialog obsolete features and UX
    1. Remove the footer
    2. Remove feedback functionality
    3. Remove learn more link functionality
    
    Bug: 389629676
    Change-Id: I8597a447f71b5e318040adc5ce7f739aef3892f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6257547
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419291}

--- chrome/browser/autofill_ai/chrome_autofill_ai_client.cc
+++ chrome/browser/autofill_ai/chrome_autofill_ai_client.cc
@@ -212,8 +212,7 @@ void ChromeAutofillAiClient::ShowSaveAutofillAiBubble(
   if (auto* controller = autofill_ai::SaveAutofillAiDataController::GetOrCreate(
           &*web_contents_)) {
     controller->OfferSave(std::move(entity),
-                          std::move(prompt_acceptance_callback),
-                          base::DoNothing());
+                          std::move(prompt_acceptance_callback));
     return;
   }
 #endif  // !BUILDFLAG(IS_ANDROID)
