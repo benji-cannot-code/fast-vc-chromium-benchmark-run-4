9ba39a1967fa70e07b8849f9a6723b2e6dc39fbd
Matt Menke <mmenke@chromium.org>
2025-02-11 16:02:37 -0800

    Fix unsafe buffer usage in http_network_transaction.cc.
    Bug: 40284755
    Change-Id: I862db6e0c7174c10d6407bf86eed17319b63f04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6254679
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418973}

--- net/http/http_network_transaction.h
+++ net/http/http_network_transaction.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <array>
 #include <memory>
 #include <optional>
 #include <string>
@@ -367,8 +368,8 @@ class NET_EXPORT_PRIVATE HttpNetworkTransaction
   static void SetProxyInfoInResponse(const ProxyInfo& proxy_info,
                                      HttpResponseInfo* response_info);
 
-  scoped_refptr<HttpAuthController>
-      auth_controllers_[HttpAuth::AUTH_NUM_TARGETS];
+  std::array<scoped_refptr<HttpAuthController>, HttpAuth::AUTH_NUM_TARGETS>
+      auth_controllers_;
 
   // Whether this transaction is waiting for proxy auth, server auth, or is
   // not waiting for any auth at all. |pending_auth_target_| is read and
