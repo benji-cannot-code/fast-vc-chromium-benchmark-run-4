d43d5d8d5db9ab9d8f9e44f36ff9a18cb39082b3
Yann Dago <ydago@chromium.org>
2025-02-12 07:47:31 -0800

    [MIM2]Do not show management confirmation after it has been accepted
    If a managed profile that has already seen the management confirmation
    and accepted it should never see it again.
    
    Bug: 395086006
    Change-Id: Ib332d3a5990143c9024a1496cd910b0d80dfc2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6243097
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419295}

--- ios/chrome/browser/authentication/ui_bundled/authentication_ui_util.mm
+++ ios/chrome/browser/authentication/ui_bundled/authentication_ui_util.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -177,23 +178,22 @@ BOOL ShouldShowManagedConfirmationForHostedDomain(
     return NO;
   }
 
-  if (HasMachineLevelPolicies()) {
+  // With multi-profiles, the user must be reminded of who is managing the new
+  // profile.
+  if (HasMachineLevelPolicies() &&
+      !AreSeparateProfilesForManagedAccountsEnabled()) {
     // Don't show the dialog if the browser has already machine level policies
     // as the user already knows that their browser is managed.
     return NO;
   }
 
-  if (access_point == signin_metrics::AccessPoint::kAccountMenu &&
-      base::FeatureList::IsEnabled(kIdentityDiscAccountMenu)) {
-    // Only show the dialog once per account, when switching from the Account
-    // Menu.
-    signin::GaiaIdHash gaia_id_hash =
-        signin::GaiaIdHash::FromGaiaId(GaiaId(gaia_id));
-    const base::Value* already_seen = syncer::GetAccountKeyedPrefValue(
-        prefs, prefs::kSigninHasAcceptedManagementDialog, gaia_id_hash);
-    if (already_seen && already_seen->GetIfBool().value_or(false)) {
-      return NO;
-    }
+  signin::GaiaIdHash gaia_id_hash =
+      signin::GaiaIdHash::FromGaiaId(GaiaId(gaia_id));
+  const base::Value* already_seen = syncer::GetAccountKeyedPrefValue(
+      prefs, prefs::kSigninHasAcceptedManagementDialog, gaia_id_hash);
+
+  if (already_seen && already_seen->GetIfBool().value_or(false)) {
+    return NO;
   }
 
   // Show the dialog if User Policy is enabled.
