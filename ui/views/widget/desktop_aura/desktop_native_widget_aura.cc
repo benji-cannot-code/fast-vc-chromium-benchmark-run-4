79911e9a8aa63ddbb7ba466c3b2a20ab5a60e826
Sahir Vellani <sahir.vellani@microsoft.com>
2025-02-12 11:01:56 -0800

    Use the frame color as opaque compositor background instead of white
    When in dark mode, users have complained about a momentary white flash
    when they are maximizing the browser window. The source of the white
    flash is a window sized draw quad that is presented prior to the first
    contentful compositor frame being submitted to the GPU process - note
    that this quad is actually created and submitted when the window is
    being minimized (see AppendQuadsToFillScreen). Since there are no tiles
    to be rendered, the quad is one color, which is set in
    DesktopNativeWidgetAura::UpdateWindowTransparency.
    
    This change replaces White for opaque backgrounds with whatever the
    frame color is - which already accounts for the browser color mode.
    
    Bug: 40872233
    Change-Id: I09995e868cf37b7ed0532a4b6a17739f9e3b2340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6245732
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419413}

--- ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
+++ ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/owned_window_anchor.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/base/ui_base_types.h"
+#include "ui/color/color_id.h"
 #include "ui/color/color_provider_key.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/layer.h"
@@ -537,7 +538,9 @@ void DesktopNativeWidgetAura::UpdateWindowTransparency() {
 
   auto* window_tree_host = desktop_window_tree_host_->AsWindowTreeHost();
   window_tree_host->compositor()->SetBackgroundColor(
-      transparent ? SK_ColorTRANSPARENT : SK_ColorWHITE);
+      transparent
+          ? SK_ColorTRANSPARENT
+          : GetWidget()->GetColorProvider()->GetColor(ui::kColorFrameActive));
   window_tree_host->window()->SetTransparent(transparent);
 
   content_window_->SetTransparent(transparent);
