a62c0a7b8727aebe969a676e00edb7f2a46ea13d
Joe Downing <joedow@google.com>
2025-02-11 20:25:32 -0800

    Removing pre-alpha Cloud provisioning path
    I am going to implement an updated Cloud provisioning path which will
    use the default service account associated with the Compute Engine
    Instance however before I start work I wanted to clean up the pre-
    alpha code which was used last year as that will greatly reduce the
    risk of regressions in the new codepath. The pre-alpha path can
    be removed as the only consumer of it has indicated they can pin
    the M134 host package if needed.
    
    Bug: 388886766
    Change-Id: Icc60229ddab12f798b90f8c1e9c205bf910cc217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6254843
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419081}

--- remoting/base/cloud_service_client.h
+++ remoting/base/cloud_service_client.h
@@ -35,10 +35,6 @@ struct NetworkTrafficAnnotationTag;
 
 namespace remoting {
 
-namespace apis::v1 {
-class ProvisionGceInstanceResponse;
-}  // namespace apis::v1
-
 class ProtobufHttpStatus;
 
 // A service client that communicates with the directory service.
@@ -52,9 +48,6 @@ class CloudServiceClient {
       const ProtobufHttpStatus&,
       std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
                           GenerateIceConfigResponse>)>;
-  using LegacyProvisionGceInstanceCallback = base::OnceCallback<void(
-      const ProtobufHttpStatus&,
-      std::unique_ptr<apis::v1::ProvisionGceInstanceResponse>)>;
   using ProvisionGceInstanceCallback = base::OnceCallback<void(
       const ProtobufHttpStatus&,
       std::unique_ptr<
@@ -75,10 +68,6 @@ class CloudServiceClient {
       std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
                           VerifySessionTokenResponse>)>;
 
-  // TODO: joedow - Remove the single param c'tor when we no longer support the
-  // legacy provisioning path.
-  explicit CloudServiceClient(
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
   // Used for creating a service client to call the Remoting Cloud API using
   // the |api_key| provided.
   CloudServiceClient(
@@ -94,14 +83,6 @@ class CloudServiceClient {
 
   CloudServiceClient(const CloudServiceClient&) = delete;
   CloudServiceClient& operator=(const CloudServiceClient&) = delete;
-
-  // TODO: joedow - Remove the legacy codepath once the new flow is working.
-  void LegacyProvisionGceInstance(
-      const std::string& owner_email,
-      const std::string& display_name,
-      const std::string& public_key,
-      const std::optional<std::string>& existing_directory_id,
-      LegacyProvisionGceInstanceCallback callback);
 
   void ProvisionGceInstance(
       const std::string& owner_email,
