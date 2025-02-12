4e7711f9932e017ce13df341aa7ce814930bf42a
Jiacheng Guo <gjc@google.com>
2025-02-12 00:30:13 -0800

    Check nullity of the completion callback in BackgroundLoaderOffliner when the render process is gone
    Bug: 40073298
    Change-Id: Icb6798a2e131d0173f1a3a181244ccef6a8b8146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6242499
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419147}

--- chrome/browser/offline_pages/background_loader_offliner.cc
+++ chrome/browser/offline_pages/background_loader_offliner.cc
@@ -251,7 +251,7 @@ void BackgroundLoaderOffliner::DocumentOnLoadCompletedInPrimaryMainFrame() {
 
 void BackgroundLoaderOffliner::PrimaryMainFrameRenderProcessGone(
     base::TerminationStatus status) {
-  if (pending_request_) {
+  if (pending_request_ && completion_callback_) {
     SavePageRequest request(*pending_request_.get());
     switch (status) {
       case base::TERMINATION_STATUS_OOM:
