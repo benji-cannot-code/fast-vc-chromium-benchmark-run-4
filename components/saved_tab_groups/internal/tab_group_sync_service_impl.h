dd5368483740031eb61f7d1ebe2ce11bedd20dcc
Min Qin <qinmin@chromium.org>
2025-02-12 10:59:48 -0800

    Fix an issue that tab group is duplicated on Android
    When a tab group is migrating to shared, an original guid field is set
    on that tab group. However, that guid field is never cleared even after
    the migration is over. This makes the
    UpdateTransitionedSavedTabGroupsList() method to put these shared group
    in transitioned_saved_tab_groups_. And later when GetAllGroups() is
    called, these originating groups will not be returned in the result.
    
    This issue causes session restore to guess that those tab groups are
    local only and needs to be send to sync. Thus creating a duplicate copy
    of the same group later.
    
    This CL fixes the issue by adding a new field in SavedTabGroups to
    indicate whether it has finished transitioning. The field will be
    populated to db. With this field, there is no need to maintain the
    transitioned_saved_tab_groups_ list.
    
    Not sure if this is impacting desktop though.
    
    Bug: 393422161
    Change-Id: I31f950af5bdb1486b6bba07cdac64fb3d12a7c15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6242221
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419412}

--- components/saved_tab_groups/internal/tab_group_sync_service_impl.h
+++ components/saved_tab_groups/internal/tab_group_sync_service_impl.h
@@ -228,9 +228,8 @@ class TabGroupSyncServiceImpl : public TabGroupSyncService,
   void NotifyTabGroupMigrated(const base::Uuid& new_group_guid,
                               TriggerSource source);
 
-  void HandleTabGroupRemoved(
-      std::pair<base::Uuid, std::optional<LocalTabGroupID>> id_pair,
-      TriggerSource source);
+  void HandleTabGroupRemoved(const SavedTabGroup& removed_group,
+                             TriggerSource source);
   void HandleTabGroupsReordered(TriggerSource source);
 
   void NotifyOnSyncBridgeUpdateTypeChanged(
@@ -312,10 +311,6 @@ class TabGroupSyncServiceImpl : public TabGroupSyncService,
       const GURL& previous_tab_url,
       const std::optional<proto::UrlRestriction>& url_restriction);
 
-  // Updates the list of saved tab groups which were transitioned to shared
-  // groups.
-  void UpdateTransitionedSavedTabGroupsList();
-
   void NotifyTabSelected();
 
   void OnTabGroupSharingTimeout(const base::Uuid& group_guid);
@@ -350,9 +345,6 @@ class TabGroupSyncServiceImpl : public TabGroupSyncService,
   // from sync. UI can't handle these groups, hence the service needs to wait
   // before notifying the observers.
   std::set<base::Uuid> empty_groups_;
-
-  // Groups which were transitioned to shared.
-  std::set<base::Uuid> transitioned_saved_tab_groups_;
 
   // Keeps track of shared tab groups that are waiting for their respective
   // people groups to be available in DataSharingService backend. UI can't
