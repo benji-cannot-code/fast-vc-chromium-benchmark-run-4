91974e20222eff4d8eb8cfa35b0544bcf216b6ef
Yoichi Osato <yoichio@chromium.org>
2025-02-11 16:17:01 -0800

    Clean up fake_blob_registry.h
    This CL does some clean up:
    - removes unused variables
    - mark private such members
    - initialize a flag in constructor
    
    Change-Id: I7f101f5bb9d0bbaeb929991d7b131fac2cffd397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6238937
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418989}

--- third_party/blink/renderer/platform/blob/blob_data_test.cc
+++ third_party/blink/renderer/platform/blob/blob_data_test.cc
@@ -143,7 +143,6 @@ class BlobDataHandleTest : public testing::Test {
     EXPECT_EQ(is_single_unknown_size_file, handle->IsSingleUnknownSizeFile());
 
     blob_registry_remote_.FlushForTesting();
-    EXPECT_EQ(0u, mock_blob_registry_.owned_receivers.size());
     ASSERT_EQ(1u, mock_blob_registry_.registrations.size());
     auto& reg = mock_blob_registry_.registrations[0];
     EXPECT_EQ(handle->Uuid(), reg.uuid);
@@ -231,7 +230,6 @@ TEST_F(BlobDataHandleTest, CreateEmpty) {
   EXPECT_FALSE(handle->IsSingleUnknownSizeFile());
 
   blob_registry_remote_.FlushForTesting();
-  EXPECT_EQ(0u, mock_blob_registry_.owned_receivers.size());
   ASSERT_EQ(1u, mock_blob_registry_.registrations.size());
   const auto& reg = mock_blob_registry_.registrations[0];
   EXPECT_EQ(handle->Uuid(), reg.uuid);
