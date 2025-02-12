e7fc4df685c9c79c1e276bf2bc54f32ee9b33228
Christian Xu <christianxu@google.com>
2025-02-12 03:09:07 -0800

    [iOS][OR] Use histogram function instead of macro
    Bug: None
    Change-Id: Ia4f608fc478a69f4d0afef5bb1e3bacc6c50f6a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6252628
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419198}

--- ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
+++ ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <string>
 
 #import "base/memory/raw_ptr.h"
-#import "base/metrics/histogram_macros.h"
+#import "base/metrics/histogram_functions.h"
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
 #import "components/bookmarks/browser/bookmark_model.h"
@@ -148,7 +148,7 @@ using base::UserMetricsAction;
 
   if (matchCopy.type == AutocompleteMatchType::CLIPBOARD_URL) {
     base::RecordAction(UserMetricsAction("MobileOmniboxClipboardToURL"));
-    UMA_HISTOGRAM_LONG_TIMES_100(
+    base::UmaHistogramLongTimes100(
         "MobileOmnibox.PressedClipboardSuggestionAge",
         ClipboardRecentContent::GetInstance()->GetClipboardContentAge());
   }
