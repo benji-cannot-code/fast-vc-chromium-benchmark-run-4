fd862cb21eac63dc757be77536cde300ed433204
Mickey Burks <mickeyburks@chromium.org>
2025-02-11 17:05:29 -0800

    [TabGroupSync] Use learn more URL from feature param
    Remove constant and use feature parameter similar to data sharing UI.
    
    Bug: 376347328
    Change-Id: Icb2662a4df2d78a79631586ac784bb116b4a637b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6253640
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419010}

--- chrome/browser/ui/views/user_education/browser_user_education_service.cc
+++ chrome/browser/ui/views/user_education/browser_user_education_service.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/compose/buildflags.h"
 #include "components/compose/core/browser/compose_features.h"
+#include "components/data_sharing/public/features.h"
 #include "components/feature_engagement/public/event_constants.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/lens/lens_features.h"
@@ -898,7 +899,8 @@ void MaybeRegisterChromeFeaturePromos(
             feature_engagement::kIPHTabGroupsSharedTabChangedFeature,
             kTopContainerElementId, IDS_DATA_SHARING_USER_ED_FIRST_TAB_CHANGE,
             IDS_LEARN_MORE,
-            CreateNavigationAction(GURL(chrome::kCollaborationLearnMoreURL)))
+            CreateNavigationAction(GURL(
+                data_sharing::features::kLearnMoreSharedTabGroupPageURL.Get())))
             .SetBubbleArrow(HelpBubbleArrow::kTopLeft)
             .SetAnchorElementFilter(base::BindRepeating(
                 [](const ui::ElementTracker::ElementList& elements)
