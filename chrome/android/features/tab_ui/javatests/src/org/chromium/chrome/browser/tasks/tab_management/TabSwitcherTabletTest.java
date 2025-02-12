238d9dcec6394e72ef594aa5a81578feda15bd20
Calder Kitagawa <ckitagawa@chromium.org>
2025-02-12 10:43:40 -0800

    Revert "Reland "[Tab Groups] Updated Shrink Animation for Final Incognito Tab""
    This reverts commit 8b7cde1e4ff01bb20d05d52b36fe2f3e8c31c989.
    
    Reason for revert: crbug.com/396046588
    
    Original change's description:
    > Reland "[Tab Groups] Updated Shrink Animation for Final Incognito Tab"
    >
    > This reverts commit 7284c0e088f223c4e4069f7e632e26be783916b0.
    >
    > - null checks added
    >
    > Reason for revert: bug fixed.
    >
    > Original change's description:
    > > Revert "[Tab Groups] Updated Shrink Animation for Final Incognito Tab"
    > >
    > > This reverts commit 052ba202b4571dc037e47dd4230a46a7dde4afd4.
    > >
    > > Reason for revert: bug blocks major release.
    > >
    > > Original change's description:
    > > > [Tab Groups] Updated Shrink Animation for Final Incognito Tab
    > > >
    > > > - Ensures the final shrink animation for the incognito tab switcher
    > > >         runs when closing the tab switcher
    > > > - Track animation completion using a Supplier
    > > > - Clean up method is aware of whether the final tab is closed via
    > > >         swipe
    > > > - Clean up method is aware of whether the final tab is closed via
    > > >         tab grid dialog
    > > > - Animation Recording: https://drive.google.com/file/d/1G-bdbIcQI9wY6HQ_Kx3KuUrfsN9GIz4S/view?usp=sharing&resourcekey=0-RLXdKuJIVe57phjl-binLQ
    > > >
    > > > Bug: 362329064
    > > > Change-Id: Iac81cbc04cc585debdade24c9b090b13e08bf123
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6184553
    > > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > > Reviewed-by: Sky Malice <skym@chromium.org>
    > > > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1414236}
    > >
    > > Bug: 393786206
    > > Change-Id: I2dea373a751f70e3ede432ef480a48a658b5796b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6225860
    > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1414919}
    >
    > Bug: 393786206
    > Change-Id: Id72be59b0eae0e503b20c7a7d34f96b5ea77dc83
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6242837
    > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1418176}
    
    Bug: 393786206, 396046588
    Change-Id: I89e0e848aadc03852e4dc72e08f96d047de1c1d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6258675
    Reviewed-by: Sky Malice <skym@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419399}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherTabletTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherTabletTest.java
@@ -22,8 +22,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import static org.chromium.ui.test.util.ViewUtils.onViewWaiting;
-
 import android.os.Build;
 import android.view.ViewGroup;
 import android.view.ViewStub;
@@ -391,7 +389,7 @@ public class TabSwitcherTabletTest {
 
         // Incognito tab switcher should exit to go to normal tab switcher and we should see empty
         // view.
-        onViewWaiting(
+        onView(
                         allOf(
                                 withId(R.id.empty_state_container),
                                 isDescendantOfA(withId(tabSwitcherAncestorId))))
