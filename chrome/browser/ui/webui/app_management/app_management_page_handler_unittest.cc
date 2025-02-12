363c431c791fb97aa567870bff7ea01d792b57be
msiem <msiem@chromium.org>
2025-02-11 16:02:17 -0800

    [PWA/NavCapture]: Move link_capturing_features to /web_applications
    This CL moves link_capturing_features to chrome/browser/web_applications
    so that it is accessible to files in the the web_applications directory.
    
    This is the refactor for files in the
    ['chrome/browser/ui/webui/app_management']
    
    Once all related CLs have been merged, CL to delete the component with
    chrome/browser/apps/link_capturing/link_capturing_features.h from its
    BUILD file will be merged.
    
    This CL was uploaded by git cl split.
    
    R=dibyapal@chromium.org
    
    Change-Id: I78714b716a6241c1559da4907a0b53a0d7d6293c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6254845
    Auto-Submit: May Siem <msiem@chromium.org>
    Commit-Queue: May Siem <msiem@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418972}

--- chrome/browser/ui/webui/app_management/app_management_page_handler_unittest.cc
+++ chrome/browser/ui/webui/app_management/app_management_page_handler_unittest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/apps/link_capturing/link_capturing_feature_test_support.h"
-#include "chrome/browser/apps/link_capturing/link_capturing_features.h"
+#include "chrome/browser/web_applications/link_capturing_features.h"
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom-shared.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/test/web_app_test.h"
