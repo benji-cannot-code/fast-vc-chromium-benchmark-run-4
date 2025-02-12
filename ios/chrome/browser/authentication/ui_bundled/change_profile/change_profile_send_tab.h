3907ac494079858200993dc88367f987bcf53254
Arthur Milchior <arthurmilchior@chromium.org>
2025-02-12 04:39:21 -0800

    [iOS][MIM] Introduce continuation for Sending tab to self
    It should be noted that the caller should close the tab in the
    original profile before the sign-in occurs.
    
    Fixed: 374283052
    Change-Id: Ibc437d48cefdb0799d264e7495cc24db23479004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6252978
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419217}

---
+++ ios/chrome/browser/authentication/ui_bundled/change_profile/change_profile_send_tab.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_CHANGE_PROFILE_CHANGE_PROFILE_SEND_TAB_H_
+#define IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_CHANGE_PROFILE_CHANGE_PROFILE_SEND_TAB_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/app/change_profile_continuation.h"
+
+class GURL;
+
+// Returns a ChangeProfileContinuation that opens the provided URL and the
+// option to Send to Your Device. This URL usually comes from a tab with this
+// URL in the previous profile. In this case, it’s the caller responsibility to
+// close this tab before the profile switching.
+
+ChangeProfileContinuation CreateChangeProfileSendTabToOtherDevice(
+    GURL url,
+    NSString* title);
+
+#endif  // IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_CHANGE_PROFILE_CHANGE_PROFILE_SEND_TAB_H_
