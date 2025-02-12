6b0277752ffa15b506cb2cc34b689569629d6d68
Eric Leese <leese@chromium.org>
2025-02-12 02:48:51 -0800

    Display styles for carousel pseudo elements
    Previously, GetMatchedCSSRulesForNode translated pseudo elements to
    their parent+pseudo id. This resulted in incorrect results for
    nested pseudo elements or subselectors on a pseudo element, which are
    necessary to support styling carousels. With this change, we now
    usually use the pseudo element node directly; however, we fall back
    to the old logic for view transition styles.
    
    Bug: 395033546
    Change-Id: Icc90c481d888f577bf3591e56fb979bd9e3f983c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244943
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Commit-Queue: Eric Leese <leese@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419192}

--- third_party/blink/web_tests/inspector-protocol/dom/dom-get-dom-for-carousel.js
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-get-dom-for-carousel.js
@@ -9,25 +9,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           anchor-name: --carousel;
 
           &::scroll-button(right),
-          &::scroll-button(left) {
+          &::scroll-button(left),
+          &::scroll-marker-group {
             position: fixed;
             position-anchor: --carousel;
           }
 
           &::scroll-button(right) {
-            --_inner: center span-inline-start;
-            --_outer: inline-end center;
-
-            position-area: var(--_outer);
+            position-area: inline-end center;
             content: '->';
           }
 
           &::scroll-button(left) {
-            --_inner: center span-inline-end;
-            --_outer: inline-start center;
-
-            position-area: var(--_outer);
+            position-area: inline-start center;
             content: '<-';
+          }
+
+          &::scroll-marker-group {
+            position-area: block-end;
+            display: grid;
           }
 
           scroll-marker-group: after;
@@ -43,10 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
             &::column::scroll-marker {
               content: ' ';
+            }
 
-              &:target-current {
-                background: blue;
-              }
+            &::column::scroll-marker:target-current {
+              background: blue;
             }
           }
 
@@ -112,13 +112,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   testRunner.log('Carousel DOM tree:');
   dumpDom(carousel, nodeMap);
 
-  const scrollMarker = carousel.pseudoElements.at(-1).pseudoElements.at(-1);
-  assert(
-      'Nested scroll marker is present', scrollMarker.pseudoType,
-      'scroll-marker');
+  testRunner.log('Verify that styles can be queried for pseudo elements');
 
-  testRunner.log('Scroll marker styles:');
-  await dumpStyles(scrollMarker);
+  for (const pseudoElement of carousel.pseudoElements) {
+    testRunner.log(`Style for ${pseudoElement.nodeName}:`);
+    await dumpStyles(pseudoElement);
+    if (pseudoElement.pseudoElements && pseudoElement.pseudoElements.length > 0) {
+      testRunner.log(`Style for nested ${pseudoElement.pseudoElements[0].nodeName}:`);
+      await dumpStyles(pseudoElement.pseudoElements[0]);
+    }
+  }
+
+  testRunner.log('Verify that pseudo element styles are returned for the root element');
+  await dumpStyles(carousel);
 
   testRunner.log('Changing carousel class');
 
@@ -177,17 +183,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   async function dumpStyles(node) {
     const styles = await dp.CSS.getMatchedStylesForNode({nodeId: node.nodeId});
-    for (const style of styles.result.matchedCSSRules) {
-      for (const matcher of style.rule.selectorList.selectors) {
-        testRunner.log(matcher.text);
+    for (const pseudoElementMatch of [null, ...(styles.result.pseudoElements ?? [])]) {
+      if (pseudoElementMatch) {
+        testRunner.log(`Included style for nested ::${pseudoElementMatch.pseudoType}:`);
       }
-      testRunner.log('{');
-      for (const property of style.rule.style.cssProperties) {
-        if (property.text) {
-          testRunner.log('  ' + property.text);
+      for (const style of (pseudoElementMatch ? pseudoElementMatch.matches : styles.result.matchedCSSRules)) {
+        if (style.rule.origin !== 'regular') {
+          continue;
         }
+        for (const matcher of style.rule.selectorList.selectors) {
+          testRunner.log(matcher.text);
+        }
+        testRunner.log('{');
+        for (const property of style.rule.style.cssProperties) {
+          if (property.text) {
+            testRunner.log('  ' + property.text);
+          }
+        }
+        testRunner.log('}');
       }
-      testRunner.log('}');
     }
   }
 
