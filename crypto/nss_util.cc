68fe0d445989c87ac14b0721194e626a054a493a
Georg Neis <neis@chromium.org>
2025-02-11 16:31:33 -0800

    Remove Lacros leftovers from crypto/
    Lacros is gone.
    
    - Replace IS_CHROMEOS_ASH uses with IS_CHROMEOS.
    - Replace is_chromeos uses with is_chromeos.
    - Remove IS_CHROMEOS_LACROS uses.
    - (There were no is_chromeos_lacros uses.)
    - Where appropriate, remove includes of build/chromeos_buildflags.h or
      replace with build/build_config.h.
    - Remove imports of build/config/chromeos/ui_mode.gni.
    
    Bug: b:354842935
    Change-Id: I61e0e51b06c7f89739754f1ad8f087ce781eecb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6242733
    Auto-Submit: Georg Neis <neis@chromium.org>
    Commit-Queue: Bob Beck <bbe@chromium.org>
    Reviewed-by: Bob Beck <bbe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418996}

--- crypto/nss_util.cc
+++ crypto/nss_util.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "crypto/nss_crypto_module_delegate.h"
 #include "crypto/nss_util_internal.h"
 
@@ -37,7 +36,7 @@ namespace crypto {
 
 namespace {
 
-#if !(BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS))
+#if !BUILDFLAG(IS_CHROMEOS)
 base::FilePath GetDefaultConfigDirectory() {
   base::FilePath dir;
   base::PathService::Get(base::DIR_HOME, &dir);
@@ -53,17 +52,17 @@ base::FilePath GetDefaultConfigDirectory() {
   DVLOG(2) << "DefaultConfigDirectory: " << dir.value();
   return dir;
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#endif  // BUILDFLAG!(IS_CHROMEOS)
 
-// On non-Chrome OS platforms, return the default config directory. On Chrome
-// OS return a empty path which will result in NSS being initialized without a
+// On non-ChromeOS platforms, return the default config directory. On ChromeOS
+// return a empty path which will result in NSS being initialized without a
 // persistent database.
 base::FilePath GetInitialConfigDirectory() {
-#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if BUILDFLAG(IS_CHROMEOS)
   return base::FilePath();
 #else
   return GetDefaultConfigDirectory();
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
 // This callback for NSS forwards all requests to a caller-specified
@@ -158,9 +157,9 @@ class NSSInitSingleton {
     } else {
       LOG(ERROR) << "Error opening persistent database (" << modspec
                  << "): " << GetNSSErrorMessage();
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
       DiagnosePublicSlotAndCrash(path);
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
     }
 
     return ScopedPK11Slot(db_slot_info);
@@ -219,7 +218,7 @@ class NSSInitSingleton {
       // Use "sql:" which can be shared by multiple processes safely.
       std::string nss_config_dir =
           base::StringPrintf("sql:%s", database_dir.value().c_str());
-#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if BUILDFLAG(IS_CHROMEOS)
       status = NSS_Init(nss_config_dir.c_str());
 #else
       status = NSS_InitReadWrite(nss_config_dir.c_str());
--- crypto/nss_util_internal.h
+++ crypto/nss_util_internal.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "build/chromeos_buildflags.h"
+#include "build/build_config.h"
 #include "components/nacl/common/buildflags.h"
 #include "crypto/crypto_export.h"
 #include "crypto/scoped_nss_types.h"
@@ -57,7 +57,7 @@ class CRYPTO_EXPORT AutoSECMODListReadLock {
   raw_ptr<SECMODListLock> lock_;
 };
 
-#if BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
+#if BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
 // Returns path to the NSS database file in the provided profile
 // directory.
 CRYPTO_EXPORT base::FilePath GetSoftwareNSSDBPath(
@@ -149,7 +149,7 @@ CRYPTO_EXPORT void CloseChromeOSUserForTesting(
 CRYPTO_EXPORT void SetPrivateSoftwareSlotForChromeOSUserForTesting(
     ScopedPK11Slot slot);
 
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
+#endif  // BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
 
 // Loads the given module for this NSS session.
 SECMODModule* LoadNSSModule(const char* name,
