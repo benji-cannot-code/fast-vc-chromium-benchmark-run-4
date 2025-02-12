61f3da48ea7982f2ffd626a59b758fa2f7529b98
rbpotter <rbpotter@chromium.org>
2025-02-11 17:17:42 -0800

    chrome://chrome-urls: Run mocha tests on Android
    Bug: 40108641
    Change-Id: I8cdcf52c6c76d1b306b6a010409a0026b1c878a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6250442
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419015}

--- chrome/test/base/web_ui_mocha_browser_test.h
+++ chrome/test/base/web_ui_mocha_browser_test.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/test/scoped_feature_list.h"
+#include "content/public/common/content_features.h"
 #else
 #include "chrome/test/base/devtools_agent_coverage_observer.h"
 #endif
@@ -75,9 +76,6 @@ class WebUIMochaBrowserTest : public PlatformBrowserTest {
   virtual Profile* GetProfileForSetup();
 
   // PlatformBrowserTest overrides.
-#if BUILDFLAG(IS_ANDROID)
-  void SetUp() override;
-#endif
   void SetUpOnMainThread() override;
 
   void set_test_loader_host(const std::string& host);
@@ -105,7 +103,13 @@ class WebUIMochaBrowserTest : public PlatformBrowserTest {
   std::string test_loader_scheme_;
 
 #if BUILDFLAG(IS_ANDROID)
-  base::test::ScopedFeatureList scoped_feature_list_;
+  // On Android, JavaScript console messages are only added to test logs if
+  // kLogJsConsoleMessages is enabled (on other platforms, such messages are
+  // included in test logs by default). Console messages are necessary for
+  // WebUI tests since they include logs indicating which tests in a suite
+  // passed/failed and the console errors related to any failures.
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kLogJsConsoleMessages};
 #else
   // Handles collection of code coverage.
   std::unique_ptr<DevToolsAgentCoverageObserver> coverage_handler_;
