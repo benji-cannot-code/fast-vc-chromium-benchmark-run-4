7c448c9ed71f32cc49cf076fe429941d22c92a76
Orr Bernstein <orrb@google.com>
2025-02-13 07:15:41 -0800

    Add a new feature to control whether Chrome queries k-anon server.
    This new feature, `FledgeQueryKAnonymity`, defaults to enabled,
    mimicking the current behavior. When enabled, this feature causes the
    browser to query k-anonymity status at interest group join and update
    time.
    
    This feature operates independently of the
    existing `FledgeConsiderKAnonymity` and `FledgeEnforceKAnonymity`
    features, which together decide the KAnonymityBidMode.
    
    It may be reasonable to disable kFledgeQueryKAnonymity on clients on
    which k-anonymity is not enforced, as k-anonymity status isn't used in
    those auctions.
    
    Though it's possible to enforce k-anonymity using
    kFledgeConsiderKAnonymity and kFledgeEnforceKAnonymity without also
    enabling kFledgeQueryKAnonymity, this would be unusual, since it means
    that no keys will be queried, so that new ads added to interest groups
    via subsequent joins and updates would be considered non-k-anonymous,
    and as each interest group's cache of k-anonymous keys expire beyond the
    kKAnonymityExpiration of seven days, all of its ads would be considered
    non-k-anonymous.
    
    Change-Id: I7ed340c4a2d5640b134de574a9c1e4ea7880dc9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6259656
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Orr Bernstein <orrb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419886}

--- content/browser/interest_group/interest_group_k_anonymity_manager.cc
+++ content/browser/interest_group/interest_group_k_anonymity_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/time/time.h"
+#include "content/browser/interest_group/interest_group_features.h"
 #include "content/browser/interest_group/interest_group_manager_impl.h"
 #include "third_party/blink/public/common/interest_group/interest_group.h"
 
@@ -56,6 +57,10 @@ InterestGroupKAnonymityManager::InProgressQueryState::~InProgressQueryState() =
 void InterestGroupKAnonymityManager::QueryKAnonymityData(
     const blink::InterestGroupKey& interest_group_key,
     const InterestGroupKanonUpdateParameter& k_anon_data) {
+  if (!base::FeatureList::IsEnabled(features::kFledgeQueryKAnonymity)) {
+    return;
+  }
+
   KAnonymityServiceDelegate* k_anonymity_service =
       k_anonymity_service_callback_.Run();
   if (!k_anonymity_service) {
