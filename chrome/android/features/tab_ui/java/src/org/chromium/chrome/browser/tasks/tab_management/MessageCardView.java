2b2dda188abe11d413192b5905015619e0fdf1c8
Sky Malice <skym@chromium.org>
2025-02-13 12:48:07 -0800

    Stop overwriting message description margin.
    Bug: 395859849
    Change-Id: I8963d61a89668c517e19dab6451b87f56d46a541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265214
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420082}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardView.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tasks.tab_management;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
@@ -137,23 +138,24 @@ class MessageCardView extends LinearLayout {
     /**
      * Modify the view based on the visibility of the icon. For messages that doesn't have an icon,
      * remove the icon and update the margin of the description text field.
-     * @param visible  Whether icon is visible.
+     *
+     * @param visible Whether icon is visible.
      */
     void setIconVisibility(boolean visible) {
+        Resources resources = getResources();
+        int verticalMargin =
+                resources.getDimensionPixelOffset(R.dimen.message_card_description_vertical_margin);
         MarginLayoutParams params = (MarginLayoutParams) mDescription.getLayoutParams();
         if (visible) {
             if (indexOfChild(mIcon) == -1) {
                 addView(mIcon, 0);
-                params.setMargins(0, 0, 0, 0);
+                params.setMargins(0, verticalMargin, 0, verticalMargin);
             }
         } else {
-            int margin =
-                    (int)
-                            getContext()
-                                    .getResources()
-                                    .getDimension(R.dimen.tab_grid_iph_item_description_margin);
             removeView(mIcon);
-            params.setMargins(margin, 0, 0, 0);
+            int leftMargin =
+                    resources.getDimensionPixelSize(R.dimen.tab_grid_iph_item_description_margin);
+            params.setMargins(leftMargin, verticalMargin, 0, verticalMargin);
         }
     }
 
