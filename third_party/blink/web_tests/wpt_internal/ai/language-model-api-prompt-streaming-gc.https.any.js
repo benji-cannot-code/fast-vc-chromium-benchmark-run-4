c92cc661bade3ce409fb53da5e7a0e0504296fb7
Mingyu Lei <leimy@chromium.org>
2025-02-12 23:03:08 -0800

    Use assert_equals/assert_greater_than instead of assert_true in AI WPT
    Using the more specific assertion method gives us more descriptive
    messages when the test fails.
    
    Bug: 395509560
    Change-Id: I8cd0e94bd5297a5e7d690e623f98a9304a10f616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6262885
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419716}

--- third_party/blink/web_tests/wpt_internal/ai/language-model-api-prompt-streaming-gc.https.any.js
+++ third_party/blink/web_tests/wpt_internal/ai/language-model-api-prompt-streaming-gc.https.any.js
@@ -13,7 +13,10 @@ promise_test(async t => {
     session.promptStreaming(kTestPrompt);
   // Run GC.
   gc();
-  assert_true(Object.prototype.toString.call(streamingResponse) === "[object ReadableStream]");
+  assert_equals(
+    Object.prototype.toString.call(streamingResponse),
+    "[object ReadableStream]"
+  );
   const reader = streamingResponse.getReader();
   let result = "";
   while (true) {
@@ -25,5 +28,5 @@ promise_test(async t => {
       result += value;
     }
   }
-  assert_true(result.length > 0, "The result should not be empty.");
+  assert_greater_than(result.length, 0, "The result should not be empty.");
 }, 'Prompt Streaming API must continue even after GC has been performed.');
