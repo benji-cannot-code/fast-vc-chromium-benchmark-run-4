7548064651f4d12c41738a96b4defadb9691f489
Madhav Pruthi <madhavpruthi@google.com>
2025-02-13 10:19:03 -0800

    Correct the padding for the message cards on the tab group pane
    This includes removing margins from tab group rows and adding universal 16dp padding to the group list to make all the children consistent
    
    Impl screenshot: https://screenshot.googleplex.com/AALe4vjDw2J9CWv
    
    Bug: 348733921
    Change-Id: Ie9f0f7f1f936dc9a8d75c15008bc7bfdeaaaf2a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6255572
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419980}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListMediator.java
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
+import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.BOTTOM_MARGIN_OVERRIDE;
 import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.DESCRIPTION_TEXT;
 import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.DISMISS_BUTTON_CONTENT_DESCRIPTION;
 import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.IS_ICON_VISIBLE;
+import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.LEFT_MARGIN_OVERRIDE;
 import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.MESSAGE_IDENTIFIER;
+import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.RIGHT_MARGIN_OVERRIDE;
 import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.UI_DISMISS_ACTION_PROVIDER;
 import static org.chromium.chrome.browser.tasks.tab_management.MessageService.DEFAULT_MESSAGE_IDENTIFIER;
 import static org.chromium.chrome.browser.tasks.tab_management.TabGroupMessageCardViewProperties.ALL_KEYS;
@@ -329,6 +332,9 @@ public class TabGroupListMediator {
                             .with(IS_ICON_VISIBLE, false)
                             .with(CARD_TYPE, MESSAGE)
                             .with(MESSAGING_BACKEND_SERVICE_ID, messageId)
+                            .with(LEFT_MARGIN_OVERRIDE, 0)
+                            .with(RIGHT_MARGIN_OVERRIDE, 0)
+                            .with(BOTTOM_MARGIN_OVERRIDE, 0)
                             .build();
 
             tabGroupRemovedMessages.add(propertyModel);
