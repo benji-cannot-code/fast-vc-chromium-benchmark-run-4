60bcc8c5f9cb3b7ecfad524d1340506263c4718b
rajendrant <rajendrant@chromium.org>
2025-02-13 12:35:00 -0800

    Implement word count client signal for cueing
    Change-Id: I16a8aec94bd9e85e55971c13dbc4679f34006fd8
    Bug: 389751174, 395721741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237754
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420070}

--- chrome/browser/contextual_cueing/contextual_cueing_helper.h
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.h
@@ -54,7 +54,7 @@ class ContextualCueingHelper
 
   void OnCueingDecision(
       std::unique_ptr<ScopedNudgeDecisionRecorder> decision_recorder,
-      const std::string& cue_label);
+      std::string cue_label);
 
   bool IsBrowserBlockingNudges(ScopedNudgeDecisionRecorder* recorder);
 
