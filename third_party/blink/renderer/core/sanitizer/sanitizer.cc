12c52752b659520a70aec264738aba57f0b421e7
Daniel Vogelheim <vogelheim@chromium.org>
2025-02-13 08:52:01 -0800

    [Sanitizer] Properly recurse into <template> contents.
    Fix Sanitizer to properly recurse into template contents. Also update
    tests (& test support files) to take template contents into account.
    
    Bug: 356601280
    Change-Id: Idb81d0f01afe7dcc1ce5d13d719b0515d1ef4752
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6234275
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419925}

--- third_party/blink/renderer/core/sanitizer/sanitizer.cc
+++ third_party/blink/renderer/core/sanitizer/sanitizer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/dom/node_traversal.h"
+#include "third_party/blink/renderer/core/html/html_template_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/sanitizer/sanitizer_builtins.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -308,6 +309,17 @@ void Sanitizer::SanitizeElement(Element* element) const {
       element->removeAttribute(name);
     }
   }
+
+  // Recurse into template and (later) shadow root content.
+  // TODO(vogelheim): Also implement shadow root support, once that's settled
+  // down.
+  // TODO(vogelheim): Merge this code with the case in SanitizeUnsafe.
+  if (IsA<HTMLTemplateElement>(element)) {
+    Node* content = To<HTMLTemplateElement>(element)->content();
+    if (content) {
+      SanitizeUnsafe(content);
+    }
+  }
 }
 
 void Sanitizer::SanitizeSafe(Node* root) const {
@@ -321,8 +333,17 @@ void Sanitizer::SanitizeSafe(Node* root) const {
 }
 
 void Sanitizer::SanitizeUnsafe(Node* root) const {
-  enum { kKeep, kKeepElement, kDrop, kReplaceWithChildren } action = kKeep;
+  // Recurse into template and (later) shadow root content.
+  // TODO(vogelheim): Also implement shadow root support, once that's settled
+  // down.
+  if (IsA<HTMLTemplateElement>(root)) {
+    Node* content = To<HTMLTemplateElement>(root)->content();
+    if (content) {
+      SanitizeUnsafe(content);
+    }
+  }
 
+  enum { kKeep, kKeepElement, kDrop, kReplaceWithChildren } action = kKeep;
   Node* node = NodeTraversal::Next(*root);
   while (node) {
     switch (node->getNodeType()) {
