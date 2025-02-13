a7a41af5424ec2b43ed27cc22585cdf101e69ee0
Feras Aldahlawi <feras@chromium.org>
2025-02-12 16:57:36 -0800

    Use implicitly convert decrypt buffer in cdm unittest
    DecoderBuffer can be implicitly be converted to a byte span. We can remove
    the calls to AsSpan().
    
    Bug: 373790934
    Change-Id: Ia02399925cb8b70b360f2558051efd216f3a0c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6260997
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Feras Aldahlawi <feras@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419631}

--- media/cdm/cenc_decryptor_unittest.cc
+++ media/cdm/cenc_decryptor_unittest.cc
@@ -84,7 +84,7 @@ TEST(CencDecryptorTest, OneBlock) {
       CreateEncryptedBuffer(encrypted_block, kIv, subsamples);
   auto decrypted_buffer = DecryptCencBuffer(*encrypted_buffer, kKey);
   ASSERT_TRUE(decrypted_buffer);
-  EXPECT_EQ(kOneBlock, decrypted_buffer->AsSpan());
+  EXPECT_EQ(kOneBlock, base::as_byte_span(*decrypted_buffer));
 }
 
 TEST(CencDecryptorTest, ExtraData) {
@@ -124,7 +124,7 @@ TEST(CencDecryptorTest, NoSubsamples) {
       CreateEncryptedBuffer(encrypted_block, kIv, subsamples);
   auto decrypted_buffer = DecryptCencBuffer(*encrypted_buffer, kKey);
   ASSERT_TRUE(decrypted_buffer);
-  EXPECT_EQ(kOneBlock, decrypted_buffer->AsSpan());
+  EXPECT_EQ(kOneBlock, base::as_byte_span(*decrypted_buffer));
 }
 
 TEST(CencDecryptorTest, BadSubsamples) {
@@ -154,7 +154,7 @@ TEST(CencDecryptorTest, InvalidIv) {
       CreateEncryptedBuffer(encrypted_block, kBadIv, subsamples);
   auto decrypted_buffer = DecryptCencBuffer(*encrypted_buffer, kKey);
   ASSERT_TRUE(decrypted_buffer);
-  EXPECT_NE(kOneBlock, decrypted_buffer->AsSpan());
+  EXPECT_NE(kOneBlock, base::as_byte_span(*decrypted_buffer));
 }
 
 TEST(CencDecryptorTest, InvalidKey) {
@@ -171,7 +171,7 @@ TEST(CencDecryptorTest, InvalidKey) {
       CreateEncryptedBuffer(encrypted_block, kIv, subsamples);
   auto decrypted_buffer = DecryptCencBuffer(*encrypted_buffer, kBadKey);
   ASSERT_TRUE(decrypted_buffer);
-  EXPECT_NE(kOneBlock, decrypted_buffer->AsSpan());
+  EXPECT_NE(kOneBlock, base::as_byte_span(*decrypted_buffer));
 }
 
 TEST(CencDecryptorTest, PartialBlock) {
@@ -185,7 +185,7 @@ TEST(CencDecryptorTest, PartialBlock) {
       CreateEncryptedBuffer(encrypted_block, kIv, subsamples);
   auto decrypted_buffer = DecryptCencBuffer(*encrypted_buffer, kKey);
   ASSERT_TRUE(decrypted_buffer);
-  EXPECT_EQ(kPartialBlock, decrypted_buffer->AsSpan());
+  EXPECT_EQ(kPartialBlock, base::as_byte_span(*decrypted_buffer));
 }
 
 TEST(CencDecryptorTest, MultipleSubsamples) {
@@ -203,7 +203,7 @@ TEST(CencDecryptorTest, MultipleSubsamples) {
       CreateEncryptedBuffer(encrypted_block, kIv, subsamples);
   auto decrypted_buffer = DecryptCencBuffer(*encrypted_buffer, kKey);
   ASSERT_TRUE(decrypted_buffer);
-  EXPECT_EQ(plaintext, decrypted_buffer->AsSpan());
+  EXPECT_EQ(plaintext, base::as_byte_span(*decrypted_buffer));
 }
 
 TEST(CencDecryptorTest, MultipleSubsamplesWithClearBytes) {
@@ -231,7 +231,7 @@ TEST(CencDecryptorTest, MultipleSubsamplesWithClearBytes) {
   auto encrypted_buffer = CreateEncryptedBuffer(input_data, kIv, subsamples);
   auto decrypted_buffer = DecryptCencBuffer(*encrypted_buffer, kKey);
   ASSERT_TRUE(decrypted_buffer);
-  EXPECT_EQ(expected_result, decrypted_buffer->AsSpan());
+  EXPECT_EQ(expected_result, base::as_byte_span(*decrypted_buffer));
 }
 
 }  // namespace media
