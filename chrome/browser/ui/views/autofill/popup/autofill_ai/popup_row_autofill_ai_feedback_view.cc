0ff6663852b46f8fee0a2b85ed0e46c2a690572d
Jan Keitel <jkeitel@google.com>
2025-02-13 06:34:18 -0800

    Remove autofill_ai::kShowDetailsText
    We can probably remove the entire feedback view in a follow up.
    
    Bug: 395555410
    Change-Id: If54f291392c8a81022849deaae2d8977594a2cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6263089
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419861}

--- chrome/browser/ui/views/autofill/popup/autofill_ai/popup_row_autofill_ai_feedback_view.cc
+++ chrome/browser/ui/views/autofill/popup/autofill_ai/popup_row_autofill_ai_feedback_view.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/autofill/popup/popup_view_utils.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "components/autofill/core/browser/ui/suggestion_button_action.h"
-#include "components/autofill_ai/core/browser/autofill_ai_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -116,7 +115,6 @@ std::unique_ptr<PopupRowContentView> CreateFeedbackContentView(
       views::BoxLayout::Orientation::kVertical);
   feedback_outer_container->SetMainAxisAlignment(
       views::LayoutAlignment::kStart);
-  if (autofill_ai::kShowDetailsText.Get()) {
     feedback_outer_container->AddChildView(
         views::Builder<views::Label>()
             .SetText(l10n_util::GetStringUTF16(
@@ -127,7 +125,6 @@ std::unique_ptr<PopupRowContentView> CreateFeedbackContentView(
             .SetHorizontalAlignment(gfx::ALIGN_LEFT)
             .SetMultiLine(true)
             .Build());
-  }
 
   // The feedback section, containing general text about it. Later on, thumbs up
   // and down buttons are added.
