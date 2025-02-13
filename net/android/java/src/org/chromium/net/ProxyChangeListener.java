8230e30c667815f56e86902bf9b88c78629f2d93
Bo Liu <boliu@chromium.org>
2025-02-12 17:30:52 -0800

    Remove enable/disablePlatformNotifications
    It's deadcode except for tests.
    
    Fixed: 396034010
    Change-Id: I3089639f7eef014de0b621f3dbb158b48fdc0ac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6259077
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419648}

--- net/android/java/src/org/chromium/net/ProxyChangeListener.java
+++ net/android/java/src/org/chromium/net/ProxyChangeListener.java
@@ -52,7 +52,6 @@ import java.util.Locale;
 @NullMarked
 public class ProxyChangeListener {
     private static final String TAG = "ProxyChangeListener";
-    private static boolean sEnabled = true;
 
     private final @Nullable Looper mLooper;
     private final Handler mHandler;
@@ -125,10 +124,6 @@ public class ProxyChangeListener {
         assert myLooper != null;
         mLooper = myLooper;
         mHandler = new Handler(mLooper);
-    }
-
-    public static void setEnabled(boolean enabled) {
-        sEnabled = enabled;
     }
 
     public void setDelegateForTesting(Delegate delegate) {
@@ -236,9 +231,6 @@ public class ProxyChangeListener {
     private void proxySettingsChanged(@Nullable ProxyConfig cfg) {
         assertOnThread();
 
-        if (!sEnabled) {
-            return;
-        }
         if (mDelegate != null) {
             // proxySettingsChanged is called even if mNativePtr == 0, for testing purposes.
             mDelegate.proxySettingsChanged();
