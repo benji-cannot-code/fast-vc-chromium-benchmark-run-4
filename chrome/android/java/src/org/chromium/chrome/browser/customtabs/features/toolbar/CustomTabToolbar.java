d1420d172f5d7f4ee58325cc11bd70251faf9125
Tomasz Wiszkowski <ender@google.com>
2025-02-12 16:49:13 -0800

    Bypass CCT logic repositioning URL bar when SiCCT is Active
    CCT utilizes its own logic to compute URL and position it within the
    URL bar, overriding the default behavior to `SCROLL_TO_TLD`.
    
    This change disables this mechanism if the SiCCT is enabled, ensuring
    that CCT does not reposition the URL.
    
    Fixed: b:395544225 b:396203317
    Change-Id: I18a77819e31dea94b52c2fc15c8e99bed81abb17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6257039
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419625}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
@@ -1668,7 +1668,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
             final CharSequence displayText;
             final int originStart;
             final int originEnd;
-            if (publisherUrl != null) {
+            if (!mOmniboxEnabled && publisherUrl != null) {
                 String plainDisplayText =
                         getContext()
                                 .getString(
@@ -1692,7 +1692,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
                 originStart = 0;
                 if (mOmniboxEnabled) {
                     displayText = urlBarData.displayText;
-                    originEnd = 0;
+                    originEnd = urlBarData.originEndIndex;
                 } else if (urlBarData.displayText != null) {
                     displayText =
                             urlBarData.displayText.subSequence(
