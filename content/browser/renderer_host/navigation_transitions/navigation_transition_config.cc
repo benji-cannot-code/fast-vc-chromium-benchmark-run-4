7c32171894b689810c1616840dad2dd58e5974de
Aldo Culquicondor <acondor@chromium.org>
2025-02-13 11:16:11 -0800

    CopyOutputRequest for navigation screenshots in background
    Screenshots for navigations are usually not immediately needed. Set the
    priority for transferring the screenshots to background. This change
    only affects cross-document navigations, as this case tends to have more
    contention than same-document navigations, like seen in this trace:
    https://screenshot.googleplex.com/9y2XmWjnf93qSbN
    
    This change builds on https://crrev.com/c/6236753
    
    Bug: 393558258
    Change-Id: I293f01ae606c02407835041d11b680622930b31b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6259020
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420019}

--- content/browser/renderer_host/navigation_transitions/navigation_transition_config.cc
+++ content/browser/renderer_host/navigation_transitions/navigation_transition_config.cc
@@ -26,6 +26,10 @@ const base::FeatureParam<base::TimeDelta> kInvisibleCacheCleanupDelay{
     &blink::features::kBackForwardTransitions, "invisible-cache-cleanup-delay",
     base::Minutes(7)};
 
+const base::FeatureParam<bool> kTransferScreenshotInBackgroundPriority{
+    &blink::features::kBackForwardTransitions,
+    "transfer-screenshot-in-background-priority", false};
+
 size_t GetMaxCacheSizeInBytes() {
   constexpr int kLowEndMax = 32 * 1024 * 1024;  // 32MB
   constexpr int kOtherMax = 128 * 1024 * 1024;  // 128MB
@@ -73,6 +77,12 @@ size_t NavigationTransitionConfig::ComputeCacheSizeInBytes() {
 base::TimeDelta
 NavigationTransitionConfig::GetCleanupDelayForInvisibleCaches() {
   return kInvisibleCacheCleanupDelay.Get();
+}
+
+// static
+bool NavigationTransitionConfig::
+    ShouldTransferScreenshotInBackgroundPriority() {
+  return kTransferScreenshotInBackgroundPriority.Get();
 }
 
 }  // namespace content
--- content/browser/renderer_host/render_widget_host_view_base.h
+++ content/browser/renderer_host/render_widget_host_view_base.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/input/input_router_impl.h"
 #include "components/input/render_input_router.h"
 #include "components/input/render_widget_host_view_input.h"
+#include "components/viz/common/frame_sinks/copy_output_request.h"
 #include "components/viz/common/hit_test/hit_test_query.h"
 #include "components/viz/common/surfaces/scoped_surface_id_allocator.h"
 #include "components/viz/common/surfaces/surface_id.h"
@@ -78,6 +79,8 @@ class TouchSelectionControllerInputObserver;
 class WebContentsAccessibility;
 class DelegatedFrameHost;
 class SyntheticGestureTarget;
+
+using CopyOutputIpcPriority = viz::CopyOutputRequest::IpcPriority;
 
 // Basic implementation shared by concrete RenderWidgetHostView subclasses.
 class CONTENT_EXPORT RenderWidgetHostViewBase
@@ -167,6 +170,14 @@ class CONTENT_EXPORT RenderWidgetHostViewBase
       const gfx::Rect& src_rect,
       const gfx::Size& output_size,
       base::OnceCallback<void(const SkBitmap&)> callback);
+
+#if BUILDFLAG(IS_ANDROID)
+  virtual void CopyFromExactSurfaceWithIpcPriority(
+      const gfx::Rect& src_rect,
+      const gfx::Size& output_size,
+      base::OnceCallback<void(const SkBitmap&)> callback,
+      CopyOutputIpcPriority ipc_priority);
+#endif
 
   // For HiDPI capture mode, allow applying a render scale multiplier
   // which modifies the effective device scale factor. Use a scale
