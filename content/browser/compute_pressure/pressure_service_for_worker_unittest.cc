38bcd72a85a674bcba806354a565257a8ec24668
Alvin Ji <alvinji@chromium.org>
2025-02-13 09:33:58 -0800

    Clean up unused scoped feature list header in content/
    This CL removes an unused scoped feature list header. These headers are
    frequently overlooked during feature flag removal, leading to
    unnecessary compilation overhead.
    
    Change-Id: I47486538a21d446be026dd23146f6956a394e0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6263125
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419951}

--- content/browser/compute_pressure/pressure_service_for_worker_unittest.cc
+++ content/browser/compute_pressure/pressure_service_for_worker_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ref.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "content/browser/compute_pressure/pressure_client_impl.h"
