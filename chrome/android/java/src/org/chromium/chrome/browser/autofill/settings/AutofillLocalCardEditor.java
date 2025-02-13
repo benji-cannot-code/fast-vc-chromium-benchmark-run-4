dbf1286a9378485035c5489fd201e036f5de0314
Nick Navarro <npnavarro@chromium.org>
2025-02-13 08:16:36 -0800

    [Clank][a11y] Fix initial focus on card editor page
    This fixes the initial focus logic for the autofill credit card editor
    page to only set initial focus to the card number field if TalkBack is
    off. This matches the logic for the address editor, see bug for details.
    
    Confirmed the fix manually.
    
    Bug: 382297841
    Change-Id: I368b1723a20b86564a6b0fd7cd4f9264e8f203ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6253188
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Nick Navarro <npnavarro@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419914}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalCardEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalCardEditor.java
@@ -43,6 +43,7 @@ import org.chromium.chrome.browser.autofill.settings.CreditCardScannerManager.Fi
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.settings.SettingsActivity;
 import org.chromium.components.autofill.AutofillProfile;
+import org.chromium.ui.accessibility.AccessibilityState;
 import org.chromium.ui.text.EmptyTextWatcher;
 
 import java.text.SimpleDateFormat;
@@ -264,6 +265,13 @@ public class AutofillLocalCardEditor extends AutofillCreditCardEditor
 
     private void addCardDataToEditFields() {
         if (mCard == null) {
+            // If TalkBack is enabled, we want to keep the focus at the top
+            // because the user would not learn about the elements that are
+            // above the focused field.
+            if (AccessibilityState.isTouchExplorationEnabled()
+                    || AccessibilityState.isPerformGesturesEnabled()) {
+                return;
+            }
             mNumberLabel.requestFocus();
             return;
         }
