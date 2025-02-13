543a3f067b0a556e180f2f3ee551b865b525d37b
S. Ganesh <ganesh@chromium.org>
2025-02-13 06:41:43 -0800

    updater: more changes and tests to use correct `lang` in the client UI
    Fixed: 385004395
    Change-Id: Id372af68317c5488c7a55d639cb9f38d64a301b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6262527
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419864}

--- chrome/updater/win/installer/splash_wnd.cc
+++ chrome/updater/win/installer/splash_wnd.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/updater/win/installer/splash_wnd.h"
 
+#include "base/strings/utf_string_conversions.h"
 #include "base/win/scoped_gdi_object.h"
+#include "chrome/updater/util/util.h"
 #include "chrome/updater/win/installer/installer_resource.h"
 #include "chrome/updater/win/ui/l10n_util.h"
 #include "chrome/updater/win/ui/ui_util.h"
@@ -19,7 +21,8 @@ LRESULT SplashWnd::OnInitDialog(UINT /*msg*/,
                                 WPARAM /*wparam*/,
                                 LPARAM /*lparam*/,
                                 BOOL& handled) {
-  SetWindowText(GetInstallerDisplayName({}).c_str());
+  SetWindowText(
+      GetInstallerDisplayName({}, base::UTF8ToWide(GetTagLanguage())).c_str());
 
   CenterWindow();
   SetWindowIcon(m_hWnd, IDI_MINI_INSTALLER,
--- chrome/updater/win/ui/owner_draw_controls.cc
+++ chrome/updater/win/ui/owner_draw_controls.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_op.h"
+#include "base/strings/utf_string_conversions.h"
+#include "chrome/updater/util/util.h"
 #include "chrome/updater/win/ui/l10n_util.h"
 #include "chrome/updater/win/ui/resources/updater_installer_strings.h"
 #include "chrome/updater/win/ui/ui_util.h"
@@ -163,7 +165,9 @@ void CaptionButton::set_tool_tip_text(const CString& tool_tip_text) {
 }
 
 CloseButton::CloseButton() {
-  set_tool_tip_text(GetLocalizedString(IDS_CLOSE_BUTTON_BASE).c_str());
+  set_tool_tip_text(GetLocalizedString(IDS_CLOSE_BUTTON_BASE,
+                                       base::UTF8ToWide(GetTagLanguage()))
+                        .c_str());
 }
 
 HRGN CloseButton::GetButtonRgn(int rgn_width, int rgn_height) {
@@ -190,7 +194,9 @@ HRGN CloseButton::GetButtonRgn(int rgn_width, int rgn_height) {
 }
 
 MinimizeButton::MinimizeButton() {
-  set_tool_tip_text(GetLocalizedString(IDS_MINIMIZE_BUTTON_BASE).c_str());
+  set_tool_tip_text(GetLocalizedString(IDS_MINIMIZE_BUTTON_BASE,
+                                       base::UTF8ToWide(GetTagLanguage()))
+                        .c_str());
 }
 
 HRGN MinimizeButton::GetButtonRgn(int rgn_width, int rgn_height) {
