38d9ff535a4b77dade9a1f373163aa693eca6f97
Jun Ishiguro <junis@google.com>
2025-02-12 19:22:45 -0800

    Move chrome/browser/ash/arc/keymaster
    ... to chromeos/ash/experiences/arc/keymaster
    
    Bug: b:391754943
    Test: Manually try launching Play Store and ARC app on ARC-T and ARC-R devices
    Change-Id: Idf7b226923c4ac8369c1a752b14ba9f64de92873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6192387
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Vaibhav Raheja <vraheja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419682}

--- chromeos/ash/experiences/arc/keymaster/cert_store_bridge.h
+++ chromeos/ash/experiences/arc/keymaster/cert_store_bridge.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_ARC_KEYMASTER_CERT_STORE_BRIDGE_H_
-#define CHROME_BROWSER_ASH_ARC_KEYMASTER_CERT_STORE_BRIDGE_H_
+#ifndef CHROMEOS_ASH_EXPERIENCES_ARC_KEYMASTER_CERT_STORE_BRIDGE_H_
+#define CHROMEOS_ASH_EXPERIENCES_ARC_KEYMASTER_CERT_STORE_BRIDGE_H_
 
 #include <memory>
 
@@ -48,4 +48,4 @@ class CertStoreBridge {
 
 }  // namespace arc::keymaster
 
-#endif  // CHROME_BROWSER_ASH_ARC_KEYMASTER_CERT_STORE_BRIDGE_H_
+#endif  // CHROMEOS_ASH_EXPERIENCES_ARC_KEYMASTER_CERT_STORE_BRIDGE_H_
