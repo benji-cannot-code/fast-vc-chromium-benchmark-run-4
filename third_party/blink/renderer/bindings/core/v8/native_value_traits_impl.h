7a3e14ad648ad118354c84803164680e0750e685
Nate Chapin <japhet@chromium.org>
2025-02-13 13:02:26 -0800

    Delete dead code in v8_binding_for_core.{h,cc}
    Change-Id: If6cbbe87bafad3f0bcec70ac938253fa8b769b29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6203046
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420095}

--- third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h
+++ third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/trustedtypes/trusted_script.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_script_url.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_types_util.h"
+#include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_data_view.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_typed_array.h"
 #include "third_party/blink/renderer/platform/bindings/bigint.h"
--- third_party/blink/renderer/bindings/core/v8/to_v8_traits.h
+++ third_party/blink/renderer/bindings/core/v8/to_v8_traits.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "third_party/blink/renderer/bindings/core/v8/idl_types.h"
 #include "third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h"
+#include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_deque.h"
