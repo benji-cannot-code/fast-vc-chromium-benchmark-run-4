3ca1277c381b0b4f357cf639bac97dec272afc1b
Stepan Khapugin <stkhapugin@chromium.org>
2025-02-13 15:21:25 -0800

    [iOS][LO] Enable Lens Overlay by default.
    Enables lens overlay and Fast QoS for logging. Does not enable the
    location bar entrypoint since this is not the configuration that's
    being launched at the moment.
    
    Bug: none
    Change-Id: I0ad5a94985fd0c45abc22906a13d012146c5b826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6257468
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420188}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -209,7 +209,7 @@ BASE_FEATURE(kEnableLensInOmniboxCopiedImage,
 
 BASE_FEATURE(kEnableLensOverlay,
              "EnableLensOverlay",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableLensViewFinderUnifiedExperience,
              "EnableLensViewFinderUnifiedExperience",
@@ -245,11 +245,11 @@ BASE_FEATURE(kLensOverlayEnableLVFEscapeHatch,
 
 BASE_FEATURE(kLensOverlayEnableLocationBarEntrypoint,
              "LensOverlayEnableLocationBarEntrypoint",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensOverlayEnableLocationBarEntrypointOnSRP,
              "LensOverlayEnableLocationBarEntrypointOnSRP",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensOverlayEnableSameTabNavigation,
              "EnableLensOverlaySameTabNavigation",
