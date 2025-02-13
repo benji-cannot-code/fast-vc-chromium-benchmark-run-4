d2d0c5abc54d0e254e034eeeff3d887f455c6d9f
Jordan Bayles <jophba@chromium.org>
2025-02-13 13:26:21 -0800

    [Paint] Apply remaining ObjectPaintProperties feedback
    This patch contains some minor changes that were suggested as part
    of a post hoc review of the SparseObjectPaintProperties experiment.
    
    Bug: 40280669
    Change-Id: I7b4452f42a827ab00c739560d0cdcdccffe0ded7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6243188
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420118}

--- third_party/blink/renderer/core/paint/object_paint_properties.h
+++ third_party/blink/renderer/core/paint/object_paint_properties.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <memory>
 #include <utility>
-#include <variant>
 
 #include "base/dcheck_is_on.h"
 #include "base/functional/function_ref.h"
