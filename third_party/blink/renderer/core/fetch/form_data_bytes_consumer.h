daef3b8f36747934dd2f155941c77def49f1f536
Yoichi Osato <yoichio@chromium.org>
2025-02-12 19:55:50 -0800

    Enable complex FormData upload over Service Worker
    This CL fixes the bug uploading FormData is failed when
    - the Formdata is uploaded over service worker
    - that contains both File and Blob
    That's because when we pass the formdata to service worker,
    PopulateResourceRequestBody converts Blob to DataPipe to avoid
    data copy that combination FormDataBytesConsumer doesn't assume.
    
    Then this CL has DataAndDataPipeBytesConsumer reuse
    DataAndEncodedFileOrBlobBytesConsumer to adapt any combination.
    
    Fixed: 40790626, 40921917
    Change-Id: I8fa42e2fb7b7e4c0bfd9341ed55f9c708e3276b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6182832
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419688}

--- third_party/blink/renderer/core/fetch/form_data_bytes_consumer.h
+++ third_party/blink/renderer/core/fetch/form_data_bytes_consumer.h
@@ -64,8 +64,6 @@ class FormDataBytesConsumer final : public BytesConsumer {
     BytesConsumer::Trace(visitor);
   }
 
-  CORE_EXPORT static bool IsValidForTesting(const EncodedFormData* form_data);
-
  private:
   static BytesConsumer* GetImpl(ExecutionContext*,
                                 scoped_refptr<EncodedFormData>,
