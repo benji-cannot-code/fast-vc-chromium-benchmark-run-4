a5f5804276fa8c52009eb3edaa5764de10124b98
dljames <dljames@chromium.org>
2025-02-12 16:13:46 -0800

    [SharedTabGroups] Add SharedTabGroup feedback action item
    Adds the kActionSendSharedTabGroupFeedback action item which opens the
    listnr feedback form when clicked.
    
    A follow-up CL will display the action item within the pinned toolbar.
    
    NO_IFTTT=Added action not gated on fenced frame
    
    Change-Id: If6c6e46d8575d23a0f05426da97e525d5aaef508
    Bug: 396028056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6256440
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419603}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -536,6 +536,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionHome, IDC_HOME) \
   E(kActionForward, IDC_FORWARD) \
   E(kActionNewIncognitoWindow, IDC_NEW_INCOGNITO_WINDOW) \
+  E(kActionSendSharedTabGroupFeedback, IDC_SEND_SHARED_TAB_GROUP_FEEDBACK) \
   E(kActionShowPasswordManager, IDC_SHOW_PASSWORD_MANAGER) \
   E(kActionShowPaymentMethods, IDC_SHOW_PAYMENT_METHODS) \
   E(kActionShowAddresses, IDC_SHOW_ADDRESSES) \
