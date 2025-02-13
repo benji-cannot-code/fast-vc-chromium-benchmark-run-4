652f78101cf2b1a5be8b866e9e35b8982edbaf8b
Anders Hartvoll Ruud <andruud@chromium.org>
2025-02-13 12:45:42 -0800

    [functions] Support @container within @function
    This CL completes the conditional rule support for custom functions,
    along with CL:6227574 (@supports) and CL:6234272 (@media).
    
    @container is evaluated more dynamically than the other conditional
    at-rules, and therefore the existing invalidation path is sufficient
    for functional container queries as well.
    
    Note that ShadowDOM is ignored in this CL; we pass the wrong tree scope
    to CQ lookup currently (Issue 394500599). It makes sense to address
    this along with tree-scoped lookups of @function rules
    (Issue 394111301).
    
    I have also filed two follow-up tasks to avoid mutating MatchResult from
    StyleCascade (Issue 394500600), and for renaming HasContainerRelative-
    Units to something more accommodating (Issue 394353319).
    
    Bug: 325504770, 394500599, 394111301, 394500600, 394353319
    Change-Id: I5cc337562b85f1d5a1561765cf9250fe67f0d6cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6233917
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420078}

--- third_party/blink/renderer/core/css/container_selector.h
+++ third_party/blink/renderer/core/css/container_selector.h
@@ -83,6 +83,11 @@ class CORE_EXPORT ContainerSelector {
            SelectsScrollableContainers();
   }
   bool HasUnknownFeature() const { return has_unknown_feature_; }
+  bool SelectsAnyContainer() const {
+    return !HasUnknownFeature() &&
+           (SelectsSizeContainers() || SelectsStyleContainers() ||
+            SelectsScrollStateContainers());
+  }
 
   PhysicalAxes GetPhysicalAxes() const { return physical_axes_; }
   LogicalAxes GetLogicalAxes() const { return logical_axes_; }
