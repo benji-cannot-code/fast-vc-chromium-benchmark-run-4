894cd01ccdfebfd6c55e3bc2eb3d9604fe381e21
Radu Nitescu <radunitescu@google.com>
2025-02-13 02:27:11 -0800

    [iOS] Refrain from presenting consent screen for LVF initiated sessions
    Fixed: 393221474
    Change-Id: I22bff024ce561c13cd8bd233f8e1c4bf39e4e368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6257767
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1419777}

--- ios/chrome/browser/lens_overlay/model/lens_overlay_entrypoint.mm
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_entrypoint.mm
@@ -7,6 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace lens {
 
+bool IsLVFEntrypoint(LensOverlayEntrypoint entrypoint) {
+  return entrypoint == LensOverlayEntrypoint::kLVFCameraCapture ||
+         entrypoint == LensOverlayEntrypoint::kLVFImagePicker;
+}
+
+bool IsImageContextMenuEntrypoint(LensOverlayEntrypoint entrypoint) {
+  return entrypoint == LensOverlayEntrypoint::kSearchImageContextMenu;
+}
+
 LensOverlayInvocationSource InvocationSourceFromEntrypoint(
     LensOverlayEntrypoint entrypoint) {
   switch (entrypoint) {
