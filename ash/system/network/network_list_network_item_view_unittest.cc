d797b9e3c3caa8511419ef52519d17815b472407
Alvin Ji <alvinji@chromium.org>
2025-02-13 17:47:53 -0800

    Clean up unused scoped feature list header in ash/system/
    This CL removes an unused scoped feature list header. These headers are
    frequently overlooked during feature flag removal, leading to
    unnecessary compilation overhead.
    
    Change-Id: I1449da8e5e6b739ea2445daf702be5044fdf09d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265603
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420247}

--- ash/system/network/network_list_network_item_view_unittest.cc
+++ ash/system/network/network_list_network_item_view_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/i18n/number_formatting.h"
 #include "base/memory/raw_ptr.h"
-#include "base/test/scoped_feature_list.h"
 #include "chromeos/ash/services/network_config/public/cpp/cros_network_config_test_helper.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
--- ash/system/scheduled_feature/scheduled_feature_unittest.cc
+++ ash/system/scheduled_feature/scheduled_feature_unittest.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
