4964805af6b706ad927fab31576ef3f4555c9b00
Michael Cui <mlcui@google.com>
2025-02-13 21:31:34 -0800

    scanner: Add enterprise policy logic to settings page
    Per go/cros-scanner-policy, the best option for the policy - a separate
    integer pref to the current boolean toggle - will unfortunately require
    some non-trivial logic in the settings page.
    
    If the feature is disallowed by enterprise policy, we should show a
    disabled toggle in the off position which appears as managed, instead of
    the usual toggle.
    
    As the enterprise policy is under a different pref to the usual toggle,
    we instead replace the toggle with a toggle attached to the enterprise
    policy pref to make the enterprise management indicator correctly
    appear. This is significantly simpler and more reliable than manually
    styling the enterprise management indicator, as is done in some Browser
    settings pages.
    
    Extensive tests have been written to ensure that this workaround works
    as expected, especially in tricky cases such as the enterprise policy
    changing while the settings page is open affecting deep links.
    
    Bug: b:388450143
    Change-Id: I6a6a2100f1ef27f0bad3850e6536b93c34f71686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6247659
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420312}

--- chrome/browser/resources/ash/settings/os_search_page/search_and_assistant_settings_card.ts
+++ chrome/browser/resources/ash/settings/os_search_page/search_and_assistant_settings_card.ts
@@ -33,6 +33,8 @@ import {Router, routes} from '../router.js';
 
 import {getTemplate} from './search_and_assistant_settings_card.html.js';
 
+const ENTERPRISE_POLICY_DISALLOWED = 2;
+
 const SearchAndAssistantSettingsCardElementBase =
     DeepLinkingMixin(RouteOriginMixin(I18nMixin(PolymerElement)));
 
@@ -95,6 +97,18 @@ export class SearchAndAssistantSettingsCardElement extends
         value: () => {
           return isScannerSettingsToggleVisible();
         },
+      },
+
+      isScannerAllowedByEnterprisePolicy_: {
+        type: Boolean,
+        computed: 'isEnterprisePolicyAllowed_(' +
+            'prefs.ash.scanner.enterprise_policy_allowed.value)',
+      },
+
+      enterprisePolicyToggleUncheckedValues_: {
+        type: Array,
+        readOnly: true,
+        value: () => [ENTERPRISE_POLICY_DISALLOWED],
       },
 
       /** Can be disallowed due to flag, policy, locale, etc. */
@@ -170,6 +184,10 @@ export class SearchAndAssistantSettingsCardElement extends
     return this.i18n(
         isAssistantEnabled ? 'searchGoogleAssistantEnabled' :
                              'searchGoogleAssistantDisabled');
+  }
+
+  private isEnterprisePolicyAllowed_(value: number): boolean {
+    return value !== ENTERPRISE_POLICY_DISALLOWED;
   }
 }
 
