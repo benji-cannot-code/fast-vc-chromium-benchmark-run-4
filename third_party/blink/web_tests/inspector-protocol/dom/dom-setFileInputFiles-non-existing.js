e8709bc87690057e0075c6f56b71979f2df1fd31
Maksim Sadym <sadym@google.com>
2025-02-14 04:14:07 -0800

    Extend `DOM.setFileInputFiles` tests
    Test a directory and non existing file set to a file input.
    
    Change-Id: I5697325797a4040ddf41cf433b4de7f00c59d426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6268641
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Maksim Sadym <sadym@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420426}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-setFileInputFiles-non-existing.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {dp} = await testRunner.startHTML(`
+    <input type="file" id="file" name="avatar" accept="image/png, image/jpeg">`, 'Tests DOM.setFileInputFiles method with a non existing file.');
+
+  let response = await dp.Runtime.evaluate({ expression: `document.querySelector('#file')` });
+  const test_path = "/SOME_NONE_EXISTING_FILE";
+  await dp.DOM.setFileInputFiles({objectId: response.result.result.objectId, files: [test_path]});
+
+  response = await dp.Runtime.evaluate({ expression: `document.querySelector('#file').files[0]` });
+  const info = await dp.DOM.getFileInfo({objectId: response.result.result.objectId});
+  const file_info_path = info.result.path;
+  testRunner.log(`File info path: ${file_info_path}`);
+  testRunner.completeTest();
+})
