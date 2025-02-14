8eaed04946bbcd56642a8c04a5d8a64bb7433476
Patrick Noland <pnoland@google.com>
2025-02-14 11:56:47 -0800

    [Anchor] Recover from bad pref write
    This adds a recovery path for clients who have an int written to the key
    ADDRESS_BAR_SETTINGS_CLICKED, overwriting the value with a boolean. It
    also fixes the <new> being included in user-visible text.
    
    Bug: 396134319, 396457570
    Change-Id: I285c29a58f90475901d52a3ff0b0e32810f2185a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6270477
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420653}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/settings/AddressBarSettingsFragment.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/settings/AddressBarSettingsFragment.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.toolbar.settings;
 
 import static org.chromium.build.NullUtil.assumeNonNull;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
@@ -22,6 +23,8 @@ import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
 import org.chromium.chrome.browser.toolbar.R;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 import org.chromium.ui.base.DeviceFormFactor;
+import org.chromium.ui.text.SpanApplier;
+import org.chromium.ui.text.SpanApplier.SpanInfo;
 
 /** Fragment for address bar settings. */
 @NullMarked
@@ -35,7 +38,9 @@ public class AddressBarSettingsFragment extends ChromeBaseSettingsFragment {
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         SettingsUtils.addPreferencesFromResource(this, R.xml.address_bar_settings);
-        mPageTitle.set(getString(R.string.address_bar_settings));
+        CharSequence summary = getTitleWithoutSpans(getContext());
+        mPageTitle.set(summary.toString());
+        assumeNonNull((Preference) findPreference(PREF_ADDRESS_BAR_TITLE)).setTitle(summary);
         overrideDescriptionIfFoldable();
     }
 
@@ -58,5 +63,12 @@ public class AddressBarSettingsFragment extends ChromeBaseSettingsFragment {
             assumeNonNull((Preference) findPreference(PREF_ADDRESS_BAR_PREFERENCE))
                     .setEnabled(!DeviceFormFactor.isNonMultiDisplayContextOnTablet(getContext()));
         }
+    }
+
+    public static CharSequence getTitleWithoutSpans(Context context) {
+        return SpanApplier.removeSpanText(
+                        context.getString(R.string.address_bar_settings),
+                        new SpanInfo("<new>", "</new>"))
+                .trim();
     }
 }
