8f70c158b39343f1b30578bb8bb153247c10fa53
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-02-14 10:58:11 -0800

    Migrated views::Label::SetBackgroundColor() to use ui::ColorVariant
    Bug: b:394420459
    Change-Id: Ia2e09fc27cb1a075d31c06cc435a9cf71181d4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6266947
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420610}

--- ash/wm/desks/templates/saved_desk_icon_view.cc
+++ ash/wm/desks/templates/saved_desk_icon_view.cc
@@ -120,7 +120,7 @@ void SavedDeskIconView::CreateCountLabelChildView(bool show_plus,
                            kCountLabelInsetSize, kCountLabelInsetSize,
                            kCountLabelInsetSize, inset_size)))
                        .SetEnabledColorId(cros_tokens::kCrosSysSecondary)
-                       .SetBackgroundColorId(cros_tokens::kCrosSysSystemOnBase)
+                       .SetBackgroundColor(cros_tokens::kCrosSysSystemOnBase)
                        .SetAutoColorReadabilityEnabled(false)
                        .Build());
 }
