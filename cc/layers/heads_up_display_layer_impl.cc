30521bf48788603832c3734522d0a7ca01904e04
Colin Blundell <blundell@chromium.org>
2025-02-14 06:31:43 -0800

    [//cc] Make ResourcePool::Backing::shared_image private
    This ensures that we know exactly where/how the SI is getting set and
    serves as a precursor to folding in as much of the creation as possible
    to happen within Backing itself based directly on Backing's state. That
    in turn will allow removing overrides currently passed when creating a
    TransferableResource from this SharedImage.
    
    Bug: 40064122
    Change-Id: I987d258d3cb8150a65fc58d2ca773d3c8aab3a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6263445
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420464}

--- cc/layers/heads_up_display_layer_impl.cc
+++ cc/layers/heads_up_display_layer_impl.cc
@@ -261,7 +261,7 @@ void HeadsUpDisplayLayerImpl::UpdateHudTexture(
           {pool_resource.format(), pool_resource.size(),
            pool_resource.color_space(), flags, "HeadsUpDisplayLayer"},
           gpu::kNullSurfaceHandle));
-      CHECK(backing->shared_image);
+      CHECK(backing->shared_image());
       auto* ri = raster_context_provider->RasterInterface();
       ri->WaitSyncTokenCHROMIUM(sii->GenUnverifiedSyncToken().GetConstData());
       pool_resource.set_backing(std::move(backing));
@@ -288,7 +288,7 @@ void HeadsUpDisplayLayerImpl::UpdateHudTexture(
           {pool_resource.format(), pool_resource.size(),
            pool_resource.color_space(), gpu::SHARED_IMAGE_USAGE_CPU_WRITE_ONLY,
            "HeadsUpDisplayLayer"}));
-      CHECK(backing->shared_image);
+      CHECK(backing->shared_image());
       pool_resource.set_backing(std::move(backing));
     }
   }
@@ -317,7 +317,7 @@ void HeadsUpDisplayLayerImpl::UpdateHudTexture(
                               gpu::raster::kNoMSAA, can_use_lcd_text,
                               /*visible=*/true, gfx::ColorSpace::CreateSRGB(),
                               /*hdr_headroom=*/1.f,
-                              backing->shared_image->mailbox().name);
+                              backing->shared_image()->mailbox().name);
       constexpr gfx::Vector2dF post_translate(0.f, 0.f);
       constexpr gfx::Vector2dF post_scale(1.f, 1.f);
       DummyImageProvider image_provider;
@@ -349,8 +349,8 @@ void HeadsUpDisplayLayerImpl::UpdateHudTexture(
       SkPixmap pixmap;
       staging_surface_->peekPixels(&pixmap);
 
-      uint32_t texture_target = backing->shared_image->GetTextureTarget();
-      ri->WritePixels(backing->shared_image->mailbox(), /*dst_x_offset=*/0,
+      uint32_t texture_target = backing->shared_image()->GetTextureTarget();
+      ri->WritePixels(backing->shared_image()->mailbox(), /*dst_x_offset=*/0,
                       /*dst_y_offset=*/0, texture_target, pixmap);
     }
 
@@ -368,7 +368,7 @@ void HeadsUpDisplayLayerImpl::UpdateHudTexture(
     SkSurfaceProps props = skia::LegacyDisplayGlobals::GetSkSurfaceProps();
     const size_t row_bytes = info.minRowBytes();
     auto* backing = pool_resource.backing();
-    auto mapping = backing->shared_image->Map();
+    auto mapping = backing->shared_image()->Map();
     base::span<uint8_t> mem = mapping->GetMemoryForPlane(0);
     CHECK_GE(mem.size(), info.computeByteSize(row_bytes));
     sk_sp<SkSurface> surface =
