e8990ab5eb2fe2c8f167602d9f7779041676b1f6
Christian Biesinger <cbiesinger@chromium.org>
2025-02-14 12:07:33 -0800

    Support non-BMP characters in RoundedIconGenerator
    This makes certain characters like emojis work correctly.
    
    Change-Id: I0cbc217271168d1a2a63de4179fc561f95159fc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6264873
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420664}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RoundedIconGeneratorTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RoundedIconGeneratorTest.java
@@ -37,6 +37,10 @@ public class RoundedIconGeneratorTest {
         return RoundedIconGenerator.getIconTextForUrl(url, includePrivateRegistries);
     }
 
+    private String getInitialLetter(String text) {
+        return RoundedIconGenerator.getInitialLetter(text);
+    }
+
     /**
      * Verifies that RoundedIconGenerator's ability to generate icons based on URLs considers the
      * appropriate parts of the URL for the icon to generate.
@@ -91,5 +95,19 @@ public class RoundedIconGeneratorTest {
 
         Assert.assertTrue(generator.generateIconForText("") != null);
         Assert.assertTrue(generator.generateIconForText("A") != null);
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"Browser", "RoundedIconGenerator"})
+    public void testGetInitialLetter() {
+        Assert.assertEquals("E", getInitialLetter("English Text"));
+        Assert.assertEquals("😊", getInitialLetter("😊 Emoji Text"));
+        Assert.assertEquals("ا", getInitialLetter("النص العربي"));
+        Assert.assertEquals("ט", getInitialLetter("טקסט בעברית"));
+        Assert.assertEquals("中", getInitialLetter("中文文本"));
+        Assert.assertEquals("H́", getInitialLetter("h́ Text with combining character"));
+        Assert.assertEquals(
+                "👩🏾‍⚕️", getInitialLetter("👩🏾‍⚕️ Emoji with skin tone (combining character)"));
     }
 }
