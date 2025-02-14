e691223b3bf064089862904864e553d224f5e05a
Brandon Wylie <wylieb@chromium.org>
2025-02-14 10:59:20 -0800

    [declutter] Handle CTA being destroyed while settings change
    When CTA is destroyed in the background while the declutter settings
    change, the archived orchestrator will try to look for the most recent
    tabbed orchestrator to rescue archived tabs to. This will NPE when
    there's no tabbed orchestrator to rescue tabs to. Fixing this by
    checking if there are any tabbed orchestrators available. If there
    isn't, the rescue will resolve when CTA starts and the tabbed
    orchestrator is registered.
    
    Bug: 395999879
    Change-Id: I927149813c47f63074c5543b9865fdda94a42459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6261686
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420612}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -2000,7 +2000,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements MismatchedIn
                                     tabModel.getTabCreator(),
                                     Arrays.asList(archivedTab),
                                     /* updateTimestamp= */ true,
-                                    /* areTabsBeingOpened= */ false);
+                                    /* areTabsBeingOpened= */ true);
                 } else {
                     mTabModelOrchestrator.tryToRestoreTabStateForId(tabIdToBringToFront);
                 }
