6ce1459a099bed60f67a4fa25579b7ed623c369b
Mickey Burks <mickeyburks@chromium.org>
2025-02-14 13:58:45 -0800

    [TabGroupSync] Add color for hovercard fallback icon
    Define a separate color for the avatar fallback icon used in hovercard
    section for collaboration messaging.
    
    Bug: 381719301
    Change-Id: I9e61bad0277b52782f1c28cc6323cb4c3171f082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6270579
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420731}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -680,6 +680,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorHoverCardTabAlertMediaRecordingIcon) \
   E_CPONLY(kColorHoverCardTabAlertPipPlayingIcon) \
   E_CPONLY(kColorHoverCardTabAlertAudioPlayingIcon) \
+  E_CPONLY(kColorHoverCardTabAlertUserAvatarFallbackIcon) \
   /* Tab colors. */ \
   E_CPONLY(kColorTabBackgroundActiveFrameActive) \
   E_CPONLY(kColorTabBackgroundActiveFrameInactive) \
