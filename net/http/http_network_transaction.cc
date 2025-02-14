39ea14014335b77d950c3e9b89b714839ed74f2b
Debin Zhang <debinzhang3@gmail.com>
2025-02-13 16:15:41 -0800

    [HTTP] Process Alt-svc header in 103 Early Hints response
    Enable Chromium to detect HTTP/3 support earlier by processing the Alt-svc
    header in 103 Early Hints responses. This allows faster QUIC session setup
    by detecting HTTP/3 support from Early Hints instead of waiting for the
    final response.
    
    Previously, HttpNetworkTransaction::DoReadHeadersComplete() would process
    Early Hint responses but return before reaching HTTP/3 Alt-svc header
    processing code. This change ensures Alt-svc headers in 103 responses
    are processed.
    
    Change details:
    - Modified HttpNetworkTransaction::DoReadHeadersComplete() to process Alt-svc
      headers in 103 Early Hints responses
    - Added unit tests to verify the behavior
    - No changes to the external API
    
    This improvement was discussed in chromium-dev forum:
    https://groups.google.com/a/chromium.org/g/chromium-dev/c/pbMYYso2Pb8/m/WucSAJkDAwAJ
    
    R=rch@chromium.org
    
    Bug: 388064484
    Change-Id: I8d06c3b67053ae9a1eeebb1dc39467b1a7f93e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6149141
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420206}

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -1391,6 +1391,9 @@ int HttpNetworkTransaction::DoReadHeadersComplete(int result) {
 
     if (EarlyHintsAreAllowedOn(response_.connection_info) &&
         early_response_headers_callback_) {
+      // Process Alt-Svc headers so that QUIC session can be set up sooner
+      ProcessAltSvcHeader();
+
       early_response_headers_callback_.Run(std::move(response_.headers));
     }
 
@@ -1475,15 +1478,7 @@ int HttpNetworkTransaction::DoReadHeadersComplete(int result) {
     return OK;
   }
 
-  if (IsSecureRequest()) {
-    stream_->GetSSLInfo(&response_.ssl_info);
-    if (response_.ssl_info.is_valid() &&
-        !IsCertStatusError(response_.ssl_info.cert_status)) {
-      session_->http_stream_factory()->ProcessAlternativeServices(
-          session_, network_anonymization_key_, response_.headers.get(),
-          url::SchemeHostPort(request_->url));
-    }
-  }
+  ProcessAltSvcHeader();
 
   int rv = HandleAuthChallenge();
   if (rv != OK)
@@ -2239,6 +2234,18 @@ void HttpNetworkTransaction::RecordStreamRequestResult(int result) {
   } else {
     base::UmaHistogramSparse("Net.NetworkTransaction.StreamRequestErrorCode",
                              -result);
+  }
+}
+
+void HttpNetworkTransaction::ProcessAltSvcHeader() {
+  if (IsSecureRequest()) {
+    stream_->GetSSLInfo(&response_.ssl_info);
+    if (response_.ssl_info.is_valid() &&
+        !IsCertStatusError(response_.ssl_info.cert_status)) {
+      session_->http_stream_factory()->ProcessAlternativeServices(
+          session_, network_anonymization_key_, response_.headers.get(),
+          url::SchemeHostPort(request_->url));
+    }
   }
 }
 
--- net/http/http_network_transaction.h
+++ net/http/http_network_transaction.h
@@ -354,6 +354,8 @@ class NET_EXPORT_PRIVATE HttpNetworkTransaction
 
   void RecordStreamRequestResult(int result);
 
+  void ProcessAltSvcHeader();
+
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
   enum class QuicProtocolErrorRetryStatus {
