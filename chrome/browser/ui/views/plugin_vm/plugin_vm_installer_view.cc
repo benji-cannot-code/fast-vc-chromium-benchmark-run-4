021be09d72ee57511d5a47a3bd167205f475f6d2
Eriko Kurimoto <elkurin@google.com>
2025-02-14 01:14:29 -0800

    plugin_vm: Disallow //c/b/ui/views dependencies from plugin_vm
    This CL removes //c/b/ui/views dependences from //c/b/ui/views/plugin_vm
    and add DEPS rule to disallow including //c/b/ui/views.
    
    DISTANCE_STANDALONE_BUBBLE_PREFERRED_WIDTH is the same as
    views::DISTANCE_DIALOG_CONTENT_MARGIN_BOTTOM_CONTROL.
    
    This is a part of Ash refactoring effort.
    
    Bug: b:394731607, b:396297906
    Change-Id: I4b5411334209b7564766aae6cec1acaa2ddd5506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6268000
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420370}

--- chrome/browser/ui/views/plugin_vm/plugin_vm_installer_view.cc
+++ chrome/browser/ui/views/plugin_vm/plugin_vm_installer_view.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/plugin_vm/plugin_vm_util.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
-#include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
