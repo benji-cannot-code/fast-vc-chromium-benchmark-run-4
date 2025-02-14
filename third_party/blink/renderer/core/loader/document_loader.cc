1a6ecd02f7795d5e951ba5e5e858087a57e895b3
Charlie Reis <creis@chromium.org>
2025-02-14 14:04:46 -0800

    Do not reuse document sequence number on cross-origin navigations.
    Cross-document navigations only reuse document sequence numbers for
    "logically related" navigations. This CL ensures that they are not
    reused for cross-origin navigations as well (e.g., about:blank with
    different owners), by computing this in the browser process and sending
    it in the CommitNavigationParams.
    
    Note that an exception is needed for error pages, which should preserve
    the document sequence number and other history item state, though they
    transition to and from an opaque origin with the same precursor.
    
    Bug: 40051596
    Change-Id: I502251ce12ec8b3e613596b914fbe8a63330b4fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185347
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420736}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -452,6 +452,7 @@ struct SameSizeAsDocumentLoader
   const std::optional<
       HashMap<mojom::blink::PermissionName, mojom::blink::PermissionStatus>>
       initial_permission_statuses;
+  bool force_new_document_sequence_number;
 };
 
 // Asserts size of DocumentLoader, so that whenever a new attribute is added to
@@ -615,7 +616,9 @@ DocumentLoader::DocumentLoader(
       cookie_deprecation_label_(params_->cookie_deprecation_label),
       content_settings_(std::move(params_->content_settings)),
       initial_permission_statuses_(ConvertPermissionStatusFlatMapToHashMap(
-          params_->initial_permission_statuses)) {
+          params_->initial_permission_statuses)),
+      force_new_document_sequence_number_(
+          params_->force_new_document_sequence_number) {
   TRACE_EVENT_WITH_FLOW0("loading", "DocumentLoader::DocumentLoader",
                          TRACE_ID_LOCAL(this), TRACE_EVENT_FLAG_FLOW_OUT);
   DCHECK(frame_);
@@ -781,6 +784,9 @@ DocumentLoader::CreateWebNavigationParamsToCloneDocument() {
             CachedPermissionStatus::From(frame_->DomWindow())
                 ->GetPermissionStatusMap());
   }
+  // Do not copy over force_new_document_sequence_number_, since all
+  // JavaScript and XSLT navigations are same-origin and thus are allowed to
+  // reuse the document sequence number.
   return params;
 }
 
@@ -1262,14 +1268,17 @@ void DocumentLoader::SetHistoryItemStateForCommit(
 
   // Don't propagate state from the old item if this is a different-document
   // navigation, unless the before and after pages are logically related. This
-  // means they have the same url (ignoring fragment) and the new item was
-  // loaded via reload or client redirect.
-  // TODO(crbug.com/40051596): Also return early if the origin changes as a
-  // result of this commit.
+  // means they have the same origin or a compatible origin for error page
+  // cases (as computed by the browser process in
+  // `force_new_document_sequence_number_ `), the same url (ignoring
+  // fragment), and the new item was loaded via reload or client redirect.
   if (navigation_type == HistoryNavigationType::kDifferentDocument &&
-      (history_commit_type != kWebHistoryInertCommit ||
-       !EqualIgnoringFragmentIdentifier(old_item->Url(), history_item_->Url())))
+      (force_new_document_sequence_number_ ||
+       history_commit_type != kWebHistoryInertCommit ||
+       !EqualIgnoringFragmentIdentifier(old_item->Url(),
+                                        history_item_->Url()))) {
     return;
+  }
   history_item_->SetDocumentSequenceNumber(old_item->DocumentSequenceNumber());
 
   history_item_->CopyViewStateFrom(old_item);
