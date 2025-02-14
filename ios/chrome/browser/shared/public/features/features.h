a7dee2ccbc353d15cc1acc24482a6d16a7e389f6
Alexis Hetu <sugoi@chromium.org>
2025-02-14 12:28:42 -0800

    [iOS] Cleanup new KA flags for phones
    This CL removes:
    - kIOSKeyboardAccessoryUpgrade
    which is no longer used.
    
    Bug: 326398845
    Change-Id: I0bc678161e342a11297c7b67d93f4ba78e7b3974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265864
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420681}

--- ios/chrome/browser/shared/public/features/features.h
+++ ios/chrome/browser/shared/public/features/features.h
@@ -45,9 +45,6 @@ bool IsSegmentedDefaultBrowserPromoEnabled();
 SegmentedDefaultBrowserExperimentType
 SegmentedDefaultBrowserExperimentTypeEnabled();
 
-// Feature flag to enable the Keyboard Accessory Upgrade.
-BASE_DECLARE_FEATURE(kIOSKeyboardAccessoryUpgrade);
-
 // Feature flag to enable the Keyboard Accessory Upgrade for iPads.
 BASE_DECLARE_FEATURE(kIOSKeyboardAccessoryUpgradeForIPad);
 
@@ -675,7 +672,7 @@ bool IsContentPushNotificationsProvisionalRegistrationOnly();
 // change.
 bool IsContentPushNotificationsSetUpListRegistrationOnly();
 
-// Whether or not the kIOSKeyboardAccessoryUpgrade feature is enabled.
+// Whether or not the Keyboard Accessory Upgrade feature is enabled.
 bool IsKeyboardAccessoryUpgradeEnabled();
 
 // Whether or not the kIOSKeyboardAccessoryUpgradeShortManualFillMenu feature is
