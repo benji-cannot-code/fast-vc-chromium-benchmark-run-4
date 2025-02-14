8c0e4fecd162a5915b74aa8f945cc67b6d7e5b8f
pjlee <pjlee@google.com>
2025-02-14 15:39:47 -0800

    arc/gio: Remove alpha v2 code 3/n
    This CL removes code related action label focus, which also entails
    blur, editing functionalities in focus, and various constants.
    
    Bug: b:286152619
    Test: ./out/Default_unit_tests
    Change-Id: I74f28dd5bd19f9d8e68d88cddd5cef939eee7c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5988114
    Commit-Queue: Joseph Pang <pjlee@google.com>
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420780}

--- chrome/browser/ash/arc/input_overlay/ui/input_mapping_view.h
+++ chrome/browser/ash/arc/input_overlay/ui/input_mapping_view.h
@@ -30,8 +30,6 @@ class InputMappingView : public views::View, public TouchInjectorObserver {
   void SetDisplayMode(const DisplayMode mode);
 
  private:
-  void ProcessPressedEvent(const ui::LocatedEvent& event);
-
   // Reorder the child views to have focus order as:
   // If the window aspect-ratio > 1
   // - First, focus the views on the left half of the window from top to bottom.
@@ -42,10 +40,6 @@ class InputMappingView : public views::View, public TouchInjectorObserver {
 
   // Adds the action without opening the ButtonOptionsMenu.
   void OnActionAddedInternal(Action& action);
-
-  // ui::EventHandler:
-  void OnMouseEvent(ui::MouseEvent* event) override;
-  void OnGestureEvent(ui::GestureEvent* event) override;
 
   // TouchInjectorObserver:
   void OnActionAdded(Action& action) override;
