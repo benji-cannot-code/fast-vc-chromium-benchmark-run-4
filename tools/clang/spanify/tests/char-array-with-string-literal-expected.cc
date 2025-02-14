0e84fe97e7e5a74fdf6dde3a8f2c0da8b5505e12
Yuki Shiino <yukishiino@chromium.org>
2025-02-14 01:32:28 -0800

    spanify: Supports non-const array initialization with string literal
    The current implementation supports rewriting of const char arrays
    initialized with string literals to std::string_view, but it doesn't
    work if the char array is not const.
    
    This patch supports rewriting of non-const char arrays initialized
    with string literals to std::array<CharT, size>, which is mutable.
    
    Change-Id: I94454752d7a6de8cdf3a1bf9bcf180a0381938c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6241674
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420381}

--- tools/clang/spanify/tests/char-array-with-string-literal-expected.cc
+++ tools/clang/spanify/tests/char-array-with-string-literal-expected.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <uchar.h>
 #include <wchar.h>
 
+#include <array>
 #include <string_view>
 #include <tuple>
 
@@ -31,4 +32,19 @@ void func() {
   // const std::u32string_view buf = U"123456789";
   const std::u32string_view buf5 = U"123456789";
   std::ignore = buf5[UnsafeIndex()];
+}
+
+void non_const_case() {
+  // The explicit specification of the element type and size are necessary,
+  // otherwise the deduced element type will be const.
+  //
+  // Expected rewrite:
+  // std::array<char, 4> buf{"abc"};
+  std::array<char, 4> buf{"abc"};
+  std::ignore = buf[UnsafeIndex()];
+
+  // Expected rewrite:
+  // std::array<char, 1 + 3> buf2{"abc"};
+  std::array<char, 1 + 3> buf2{"abc"};
+  std::ignore = buf2[UnsafeIndex()];
 }
