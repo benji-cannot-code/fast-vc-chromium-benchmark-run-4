9f5651cd1d80419035703f5259148edf45170f2c
Janice Liu <janiceliu@chromium.org>
2025-02-14 06:59:49 -0800

    [Blob URL] Add DevTools Issues for Partitioned Blob URL w/ Navigation
    This DevTools Issues indicates when `noopener` was enforced for a cross
    partitioned Blob URL navigation due to storage partitioning. A separate
    CL will be needed to add these issues to the DevTools issue panel in the
    frontend.
    
    Bug: 372883697
    
    Change-Id: Idf6940ce09e4dcefdba454eab3abe230b9503082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6250859
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Janice Liu <janiceliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420478}

--- third_party/blink/renderer/core/html/html_anchor_element.cc
+++ third_party/blink/renderer/core/html/html_anchor_element.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/input/event_handler.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
+#include "third_party/blink/renderer/core/inspector/inspector_audits_issue.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/loader/anchor_element_interaction_tracker.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
@@ -558,6 +559,9 @@ void HTMLAnchorElementBase::NavigateToHyperlink(
       }
       UseCounter::Count(GetDocument(),
                         WebFeature::kCrossTopLevelSiteBlobURLNavigation);
+      AuditsIssue::ReportPartitioningBlobURLIssue(
+          window, completed_url.GetString(),
+          mojom::blink::PartitioningBlobURLInfo::kEnforceNoopenerForNavigation);
     }
   }
 
