d44208e279e94632febae13720f6d0307511d02e
Ryan Kalla <ryankalla@google.com>
2025-02-14 09:09:32 -0800

    Reland "Add persistent storage for Issuer Tokens"
    Replace EXPECT_DEATH_IF_SUPPORTED with EXPECT_DCHECK_DEATH_WITH
    
    This is a reland of commit 000c4274d848d431f2b3c6169bfc24c002c0c885
    
    Original change's description:
    > Add persistent storage for Issuer Tokens
    >
    > Issuer Tokens will be written to a file in the given data directory.
    > Calls to store tokens will be gated by a feature check (to be
    > implemented later), and will be triggered by the Issuer Token Manager.
    >
    > Bug: 394344558
    > Change-Id: I6ca92d1eeffbcc16212b9d0932ebaacd8645cfc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227288
    > Reviewed-by: Aykut Bulut <aykutb@google.com>
    > Commit-Queue: Ryan Kalla <ryankalla@google.com>
    > Reviewed-by: Olivier Li <olivierli@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1418808}
    
    Bug: 394344558
    Change-Id: I0a3ea6f623c0e967aeab5269add565c364eee9bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6254050
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Ryan Kalla <ryankalla@google.com>
    Reviewed-by: Aykut Bulut <aykutb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420541}

---
+++ components/test/data/ip_protection/issuer_tokens_v0.too_old.sql
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+PRAGMA foreign_keys=OFF;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE meta(key LONGVARCHAR NOT NULL UNIQUE PRIMARY KEY, value LONGVARCHAR);
+INSERT INTO meta VALUES('version','0');
+INSERT INTO meta VALUES('last_compatible_version','1');
+
+CREATE TABLE tokens (
+version INTEGER NOT NULL,
+u TEXT NOT NULL,
+e TEXT NOT NULL,
+expiration INTEGER NOT NULL,
+num_tokens_with_signal INTEGER NOT NULL,
+public_key TEXT NOT NULL
+);
+
+INSERT INTO tokens VALUES (1, 'u', 'e', 123, 100, 'public_key');
+
+COMMIT;
