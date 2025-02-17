ee1293fe6cfc8b3008b1239f3cc8ff267783363d
Vishwas Uppoor <vishwasuppoor@google.com>
2025-02-16 20:31:22 -0800

    [PIX] Code cleanup - 5
    The onUiEvent handles the bottom sheet dismissal event. Earlier, the
    dismissal event was handled by OnPix(Ewallet)PaymentPromptResult.
    
    Rename OnPix(Ewallet)PaymentPromptResult to
    OnPix(Ewallet)AccountSelected, remove unused params, and update the
    flows.
    
    Low-Coverage-Reason: FacilitatedPaymentsClient is an abstract class. Its implementation class ChromeFacilitatedPaymentsClient has tests.
    Bug: 395148411, 395148605
    Change-Id: I5d0214f557500a466492cb315208dfdce0b1bbe5
    Fixed: 375089558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6259174
    Reviewed-by: Kavita Soni <kavitasoni@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420901}

--- components/facilitated_payments/core/browser/ewallet_manager_unittest.cc
+++ components/facilitated_payments/core/browser/ewallet_manager_unittest.cc
@@ -399,19 +399,6 @@ TEST_F(EwalletManagerTest,
       /*expected_bucket_count=*/1);
 }
 
-// If the user does not select an eWallet account in the payment prompt, request
-// for risk data is not made, and progress screen is not shown.
-TEST_F(
-    EwalletManagerTest,
-    EwalletPaymentPromptNotAccepted_LoadRiskDataNotTriggered_ProgressScreenNotShown) {
-  EXPECT_CALL(client_, LoadRiskData(testing::_)).Times(0);
-  EXPECT_CALL(client_, ShowProgressScreen()).Times(0);
-
-  test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/false,
-                                    /*selected_instrument_id=*/0);
-}
-
 // If the user selects an eWallet account in the payment prompt, request for
 // risk data is made, and progress screen is shown.
 TEST_F(EwalletManagerTest,
@@ -435,8 +422,7 @@ TEST_F(EwalletManagerTest,
   EXPECT_CALL(client_, ShowProgressScreen());
 
   test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/true,
-                                    /*selected_instrument_id=*/100L);
+      .OnEwalletAccountSelected(/*selected_instrument_id=*/100L);
 }
 
 TEST_F(EwalletManagerTest, DeviceIsBound) {
@@ -456,8 +442,7 @@ TEST_F(EwalletManagerTest, DeviceIsBound) {
       supported_payment_link, GURL("https://www.example.com"),
       ukm::UkmRecorder::GetNewSourceID());
   test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/true,
-                                    /*selected_instrument_id=*/100L);
+      .OnEwalletAccountSelected(/*selected_instrument_id=*/100L);
 
   EXPECT_TRUE(test_api(*ewallet_manager_).is_device_bound());
 }
@@ -479,8 +464,7 @@ TEST_F(EwalletManagerTest, DeviceIsNotBound) {
       supported_payment_link, GURL("https://www.example.com"),
       ukm::UkmRecorder::GetNewSourceID());
   test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/true,
-                                    /*selected_instrument_id=*/100L);
+      .OnEwalletAccountSelected(/*selected_instrument_id=*/100L);
 
   EXPECT_FALSE(test_api(*ewallet_manager_).is_device_bound());
 }
@@ -1199,7 +1183,7 @@ TEST_P(EwalletManagerOnTransactionResultLoggingTest,
 }
 
 TEST_F(EwalletManagerTest,
-       OnEwalletPaymentPromptResult_HistogramLogged_SingleBound) {
+       OnEwalletAccountSelected_HistogramLogged_SingleBound) {
   base::HistogramTester histogram_tester;
   payments_data_manager_.AddEwalletForTest(
       autofill::Ewallet(/*instrument_id=*/100, u"nickname",
@@ -1219,8 +1203,7 @@ TEST_F(EwalletManagerTest,
       supportedPaymentLink, GURL("https://www.example.com"),
       ukm::UkmRecorder::GetNewSourceID());
   test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/true,
-                                    /*selected_instrument_id=*/100L);
+      .OnEwalletAccountSelected(/*selected_instrument_id=*/100L);
 
   histogram_tester.ExpectUniqueSample(
       "FacilitatedPayments.Ewallet.FopSelector.UserAction.SingleBoundEwallet",
@@ -1229,7 +1212,7 @@ TEST_F(EwalletManagerTest,
 }
 
 TEST_F(EwalletManagerTest,
-       OnEwalletPaymentPromptResult_HistogramLogged_SingleUnboundEwallet) {
+       OnEwalletAccountSelected_HistogramLogged_SingleUnboundEwallet) {
   base::HistogramTester histogram_tester;
   payments_data_manager_.AddEwalletForTest(
       autofill::Ewallet(/*instrument_id=*/100, u"nickname",
@@ -1249,8 +1232,7 @@ TEST_F(EwalletManagerTest,
       supportedPaymentLink, GURL("https://www.example.com"),
       ukm::UkmRecorder::GetNewSourceID());
   test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/true,
-                                    /*selected_instrument_id=*/100L);
+      .OnEwalletAccountSelected(/*selected_instrument_id=*/100L);
 
   histogram_tester.ExpectUniqueSample(
       "FacilitatedPayments.Ewallet.FopSelector.UserAction.SingleUnboundEwallet",
@@ -1259,7 +1241,7 @@ TEST_F(EwalletManagerTest,
 }
 
 TEST_F(EwalletManagerTest,
-       OnEwalletPaymentPromptResult_HistogramLogged_MultipleEwallets) {
+       OnEwalletAccountSelected_HistogramLogged_MultipleEwallets) {
   base::HistogramTester histogram_tester;
   payments_data_manager_.AddEwalletForTest(
       autofill::Ewallet(/*instrument_id=*/100, u"nickname1",
@@ -1287,8 +1269,7 @@ TEST_F(EwalletManagerTest,
       supportedPaymentLink, GURL("https://www.example.com"),
       ukm::UkmRecorder::GetNewSourceID());
   test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/true,
-                                    /*selected_instrument_id=*/100L);
+      .OnEwalletAccountSelected(/*selected_instrument_id=*/100L);
 
   histogram_tester.ExpectUniqueSample(
       "FacilitatedPayments.Ewallet.FopSelector.UserAction.MultipleEwallets",
@@ -1317,8 +1298,7 @@ TEST_F(EwalletManagerTest, OnPaymentPromptResult_FopSelectorAccepted) {
   EXPECT_CALL(client_, ShowProgressScreen());
 
   test_api(*ewallet_manager_)
-      .OnEwalletPaymentPromptResult(/*is_prompt_accepted=*/true,
-                                    /*selected_instrument_id=*/100L);
+      .OnEwalletAccountSelected(/*selected_instrument_id=*/100L);
 
   auto ukm_entries = ukm_recorder_.GetEntries(
       ukm::builders::FacilitatedPayments_Ewallet_FopSelectorResult::kEntryName,
