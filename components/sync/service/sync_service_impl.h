84301d8297554d93846e3d4d01957f16b4049d04
Mikel Astiz <mastiz@chromium.org>
2025-02-17 03:13:56 -0800

    [sync] Introduce SyncAuthManager::Delegate
    Pure refactoring without behavioral changes.
    
    This delegate replaces the less readable solution using callbacks,
    following the same approach used by analogous classes such as
    SyncServiceCrypto.
    
    Change-Id: Ia55d9309b66a13f071ef0060315f59a604b29dfe
    Bug: 387132027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276128
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421006}

--- components/sync/service/sync_service_impl.h
+++ components/sync/service/sync_service_impl.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/service/data_type_manager.h"
 #include "components/sync/service/data_type_manager_observer.h"
 #include "components/sync/service/local_data_migration_item_queue.h"
+#include "components/sync/service/sync_auth_manager.h"
 #include "components/sync/service/sync_client.h"
 #include "components/sync/service/sync_prefs.h"
 #include "components/sync/service/sync_service.h"
@@ -55,7 +56,6 @@ class SharedURLLoaderFactory;
 namespace syncer {
 
 class BackendMigrator;
-class SyncAuthManager;
 class SyncFeatureStatusForMigrationsRecorder;
 class SyncPrefsPolicyHandler;
 
@@ -69,6 +69,7 @@ class SyncServiceImpl : public SyncService,
                         public SyncEngineHost,
                         public SyncPrefObserver,
                         public DataTypeManagerObserver,
+                        public SyncAuthManager::Delegate,
                         public SyncServiceCrypto::Delegate,
                         public SyncUserSettingsImpl::Delegate,
                         public signin::IdentityManager::Observer {
@@ -185,6 +186,10 @@ class SyncServiceImpl : public SyncService,
   void OnConfigureDone(const DataTypeManager::ConfigureResult& result) override;
   void OnConfigureStart() override;
 
+  // SyncAuthManager::Delegate implementation.
+  void SyncAuthAccountStateChanged() override;
+  void SyncAuthCredentialsChanged() override;
+
   // SyncServiceCrypto::Delegate implementation.
   void CryptoStateChanged() override;
   void CryptoRequiredUserActionChanged() override;
@@ -299,10 +304,6 @@ class SyncServiceImpl : public SyncService,
       ResetEngineReason reset_reason);
 
   void StopAndClear(ResetEngineReason reset_engine_reason);
-
-  // Callbacks for SyncAuthManager.
-  void AccountStateChanged();
-  void CredentialsChanged();
 
   bool IsEngineAllowedToRun() const;
 
