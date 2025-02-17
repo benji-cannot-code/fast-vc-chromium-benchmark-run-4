5aba7f0ed5359a657b9f951d18e39e44f2c56bc6
Nicolas Dossou-Gbete <dgn@chromium.org>
2025-02-17 07:42:25 -0800

    waffle: Remove the keywords_use_extended_list pref
    The default_search_provider.keywords_use_extended_list pref was
    used to make sure we correctly recompute search engines lists when
    the state of the feature extending the number of items in the list
    would change. As this is fully launched and the feature removed, this
    pref is now unnecessary, and is in the way of other refactorings.
    
    Fixed: b:304947278
    Change-Id: Ieddc1fbe84995f6e330611e2077842825f4f25a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6269214
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421079}

--- components/search_engines/template_url_prepopulate_data.cc
+++ components/search_engines/template_url_prepopulate_data.cc
@@ -187,8 +187,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
       prefs::kDefaultSearchProviderChoiceScreenRandomShuffleSeed, 0);
   registry->RegisterIntegerPref(
       prefs::kDefaultSearchProviderChoiceScreenShuffleMilestone, 0);
-  registry->RegisterBooleanPref(
-      prefs::kDefaultSearchProviderKeywordsUseExtendedList, false);
 }
 
 int GetDataVersion(PrefService* prefs) {
