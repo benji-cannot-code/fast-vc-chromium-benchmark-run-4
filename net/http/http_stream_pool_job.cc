cf5093c6e404f9072208b6b68571908245803354
Kenichi Ishibashi <bashi@chromium.org>
2025-02-16 20:34:34 -0800

    HttpStreamPool: Move port check from Job to JobController
    We can perform the check before creating a Job. Also, performing the
    port check in JobController is consistent with preconnects [1].
    
    No behavior changes, existing tests should cover this change.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:net/http/http_stream_pool_job_controller.cc;l=224;drc=4b517e777e02e5e32d7f4cdfcc0d49e8a7c8db62
    
    Bug: 396998469
    Change-Id: Ie5deacb68b2627fe9c0078305a0186c5d14d6e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6272802
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420904}

--- net/http/http_stream_pool_job.cc
+++ net/http/http_stream_pool_job.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_error_details.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_export.h"
-#include "net/base/port_util.h"
 #include "net/dns/public/resolve_error_info.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_stream_pool.h"
@@ -242,15 +241,6 @@ HttpStreamPool::AttemptManager* HttpStreamPool::Job::attempt_manager() const {
 void HttpStreamPool::Job::StartInternal() {
   CHECK(attempt_manager());
   CHECK(!attempt_manager()->is_failing());
-
-  const url::SchemeHostPort& destination = group_->stream_key().destination();
-  if (!IsPortAllowedForScheme(destination.port(), destination.scheme())) {
-    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
-        FROM_HERE,
-        base::BindOnce(&Job::OnStreamFailed, weak_ptr_factory_.GetWeakPtr(),
-                       ERR_UNSAFE_PORT, NetErrorDetails(), ResolveErrorInfo()));
-    return;
-  }
 
   attempt_manager()->StartJob(this, priority(), delegate_->allowed_bad_certs(),
                               quic_version_, request_net_log_,
