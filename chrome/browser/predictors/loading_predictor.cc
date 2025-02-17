77fc7d0ba8a15071ed38504c891a546d7b00e78d
Alex Ilin <alexilin@chromium.org>
2025-02-16 21:19:18 -0800

    Remove obsolete LoadingPredictor histograms
    Fixed: 40678700, 40695859, 40776527, 40775310, 40939112, 40925910, 40923459, 40915812, 40912527, 40877113, 40776526
    Change-Id: I548a9d02313b4d2e3cba726f4e64a48fbc7ffc25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6268680
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420908}

--- chrome/browser/predictors/loading_predictor.cc
+++ chrome/browser/predictors/loading_predictor.cc
@@ -135,8 +135,7 @@ LoadingPredictor::LoadingPredictor(const LoadingPredictorConfig& config,
       resource_prefetch_predictor_(
           std::make_unique<ResourcePrefetchPredictor>(config, profile)),
       stats_collector_(std::make_unique<LoadingStatsCollector>(
-          resource_prefetch_predictor_.get(),
-          config)),
+          resource_prefetch_predictor_.get())),
       loading_data_collector_(std::make_unique<LoadingDataCollector>(
           resource_prefetch_predictor_.get(),
           stats_collector_.get(),
@@ -467,7 +466,6 @@ void LoadingPredictor::PreconnectFinished(
 
   DCHECK(stats);
   active_hints_.erase(stats->url);
-  stats_collector_->RecordPreconnectStats(std::move(stats));
 }
 
 void LoadingPredictor::PrefetchInitiated(const GURL& url,
