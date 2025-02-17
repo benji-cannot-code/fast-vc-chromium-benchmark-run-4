3375582e40cddbb37800ae003bd7b659dcc7d9f9
Jan Keitel <jkeitel@google.com>
2025-02-16 22:12:02 -0800

    Remove unnecessary member from SavePromptAcceptanceResult
    Bug: 389629676
    Change-Id: I81a59556da9e35bee1a7d770d97bf193e7672d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6274221
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420923}

--- components/autofill_ai/core/browser/autofill_ai_client.cc
+++ components/autofill_ai/core/browser/autofill_ai_client.cc
@@ -6,22 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_ai/core/browser/autofill_ai_client.h"
 
 #include <optional>
+#include <utility>
 
 #include "components/autofill/core/browser/data_model/entity_instance.h"
 
 namespace autofill_ai {
 
 AutofillAiClient::SavePromptAcceptanceResult::SavePromptAcceptanceResult(
-    bool prompt_was_accepted,
     bool did_user_interact,
     std::optional<autofill::EntityInstance> entity)
-    : prompt_was_accepted(prompt_was_accepted),
-      did_user_interact(did_user_interact),
-      entity(std::move(entity)) {}
-
-AutofillAiClient::SavePromptAcceptanceResult::SavePromptAcceptanceResult(
-    bool prompt_was_accepted)
-    : prompt_was_accepted(prompt_was_accepted) {}
+    : did_user_interact(did_user_interact), entity(std::move(entity)) {}
 
 AutofillAiClient::SavePromptAcceptanceResult::SavePromptAcceptanceResult() =
     default;
