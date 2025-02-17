d44774648f5dcd659206321f0d618fbadbd8876c
Ewann Pellé <ewannpv@chromium.org>
2025-02-17 08:12:36 -0800

    Revert "[iOS] Add "Chrome settings" button to the account menu"
    This reverts commit a8e47052be9490fc47013886fa882b566225be30.
    
    Reason for revert:
    AccountMenuTestCase/testOpenSettings is failing on bots:
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-fieldtrial-rel/38811/overview
    
    Original change's description:
    > [iOS] Add "Chrome settings" button to the account menu
    >
    > Screen-recording in
    > https://g-issues.chromium.org/issues/392534699#comment2
    >
    > The settings icon, and re-ordering the sections will be done in
    > follow-up CLs.
    >
    > Bug: 392534699
    > Change-Id: I271117912eaa312ba96ba60dbdf7530ea2426f7a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6269397
    > Commit-Queue: Jood Hajeer <jood@google.com>
    > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1421044}
    
    Bug: 392534699
    Change-Id: I96d4417ac01d532bc1f7ba963a533cbee731bec6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276513
    Owners-Override: Ewann Pellé <ewannpv@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421085}

--- ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_mediator.mm
+++ ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_mediator.mm
@@ -432,13 +432,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }];
 }
 
-- (void)didTapSettingsButton {
-  if (self.userInteractionsBlocked) {
-    return;
-  }
-  [self.delegate didTapSettingsButton];
-}
-
 #pragma mark - Callbacks
 
 // Callback for didTapAddAccount
--- ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_mutator.h
+++ ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_mutator.h
@@ -38,9 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The user tapped on "Add account…".
 - (void)didTapAddAccount;
 
-// The user tapped to open Settings page.
-- (void)didTapSettingsButton;
-
 @end
 
 #endif  // IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_ACCOUNT_MENU_ACCOUNT_MENU_MUTATOR_H_
