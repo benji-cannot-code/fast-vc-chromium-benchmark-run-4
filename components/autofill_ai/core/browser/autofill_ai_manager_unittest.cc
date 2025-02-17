7d7e702c253b303dfe508cb415937aa5b4b11813
Jan Keitel <jkeitel@google.com>
2025-02-16 22:16:00 -0800

    Remove unused methods from AutofillAiClient
    Bug: 395038288
    Change-Id: I561f8297fae7b976dc41a58da53ec74b91541879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6273542
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420924}

--- components/autofill_ai/core/browser/autofill_ai_manager_unittest.cc
+++ components/autofill_ai/core/browser/autofill_ai_manager_unittest.cc
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_ai/core/browser/suggestion/autofill_ai_suggestions.h"
 #include "components/optimization_guide/core/model_execution/model_execution_features.h"
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
-#include "components/user_annotations/test_user_annotations_service.h"
+#include "components/optimization_guide/proto/features/forms_predictions.pb.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -109,16 +109,6 @@ class MockAutofillAiModelExecutor : public AutofillAiModelExecutor {
       (const override));
 };
 
-const GURL& url() {
-  static GURL url = GURL("https://example.com");
-  return url;
-}
-
-const url::Origin& origin() {
-  static url::Origin origin = url::Origin::Create(url());
-  return origin;
-}
-
 class BaseAutofillAiManagerTest : public testing::Test {
  public:
   BaseAutofillAiManagerTest() {
@@ -152,11 +142,6 @@ class AutofillAiManagerTest : public BaseAutofillAiManagerTest {
                       {"send_title_url", "false"}});
     ON_CALL(client(), GetModelExecutor)
         .WillByDefault(Return(&model_executor()));
-    ON_CALL(client(), GetLastCommittedOrigin)
-        .WillByDefault(ReturnRef(origin()));
-    ON_CALL(client(), GetTitle).WillByDefault(Return("title"));
-    ON_CALL(client(), GetUserAnnotationsService)
-        .WillByDefault(Return(&user_annotations_service_));
     ON_CALL(client(), GetEntityDataManager)
         .WillByDefault(Return(&entity_data_manager_));
     ON_CALL(client(), IsUserEligible).WillByDefault(Return(true));
@@ -195,7 +180,6 @@ class AutofillAiManagerTest : public BaseAutofillAiManagerTest {
   }
 
  private:
-  user_annotations::TestUserAnnotationsService user_annotations_service_;
   autofill::AutofillWebDataServiceTestHelper webdata_helper_{
       std::make_unique<autofill::EntityTable>()};
   autofill::EntityDataManager entity_data_manager_{
@@ -604,8 +588,6 @@ TEST_F(AutofillAiManagerTest,
 class IsFormAndFieldEligibleAutofillAiTest : public BaseAutofillAiManagerTest {
  public:
   IsFormAndFieldEligibleAutofillAiTest() {
-    ON_CALL(client(), GetLastCommittedOrigin)
-        .WillByDefault(ReturnRef(origin()));
     autofill::test::FormDescription form_description = {
         .fields = {{.role = autofill::NAME_FIRST,
                     .heuristic_type = autofill::NAME_FIRST}}};
