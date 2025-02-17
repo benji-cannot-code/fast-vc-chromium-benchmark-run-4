04ae4f655e5b50feefc1d731a5da53d723364a15
Ting-Hao Lin <tinghaolin@google.com>
2025-02-17 12:21:52 -0800

    Add flag to disable VPN apps on Flex
    Introduced a Chrome flag to prevent VPN apps from running on Flex
    devices during the TT phase, even if the DeviceFlexArcPreloadEnabled
    policy would typically allow them. This flag will be removed prior to
    the GA release.
    
    Bug=b:395723805
    TEST=deploy Chrome and verify that the flag was created
    
    Change-Id: I0fb56d138a2738bc23b938a2954c74ce757d0e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6248723
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: TingHao Lin <tinghaolin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421136}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1020,6 +1020,12 @@ BASE_FEATURE(kFlexFirmwareUpdate,
              "FlexFirmwareUpdate",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Controls whether to ignore policy setting for enabling VPN Apps
+// for ChromeOS Flex.
+BASE_FEATURE(kIgnoreDeviceFlexArcEnabledPolicy,
+             "IgnoreDeviceFlexArcEnabledPolicy",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables Vpn Apps for ChromeOS Flex.
 BASE_FEATURE(kVpnAppsOnFlex,
              "VpnAppsOnFlex",
@@ -3619,6 +3625,10 @@ bool IsFlexAutoEnrollmentEnabled() {
 bool IsFlexFirmwareUpdateEnabled() {
   return switches::IsRevenBranding() &&
          base::FeatureList::IsEnabled(kFlexFirmwareUpdate);
+}
+
+bool ShouldIgnoreDeviceFlexArcEnabledPolicy() {
+  return base::FeatureList::IsEnabled(kIgnoreDeviceFlexArcEnabledPolicy);
 }
 
 bool IsVpnAppsOnFlexEnabled() {
