9de5981fd70418ff5728afd3d4a092cfe1ec4576
Viktoria Zlatinova <vizlatin@microsoft.com>
2025-02-17 12:41:55 -0800

    Update HTTP request headers, body, and referrer policy on CORS redirect
    This change updates the request on CORS redirect by removing the
    "request-body-headers" and body if the method has changed, and also
    updates the referrer policy, following 4.4 HTTP Redirect Fetch of the
    spec (https://fetch.spec.whatwg.org/#http-redirect-fetch). Chrome status
    entry: https://chromestatus.com/feature/5129859522887680.
    
    Bug: 40686262
    Change-Id: I7d54cb78fca8aedab527a80f90e8e63534d7d170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226486
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Viktoria Zlatinova <vizlatin@microsoft.com>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421142}

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -358,4 +358,8 @@ BASE_FEATURE(kAdAuctionEventRegistration,
              "AdAuctionEventRegistration",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kUpdateRequestForCorsRedirect,
+             "UpdateRequestForCorsRedirect",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace network::features
