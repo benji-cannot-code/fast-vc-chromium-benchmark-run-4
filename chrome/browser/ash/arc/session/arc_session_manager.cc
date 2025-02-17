ccf8b884aade717c87a9ed9b4f1fcb01712089fd
Ting-Hao Lin <tinghaolin@google.com>
2025-02-17 12:22:05 -0800

    Use device policy to control arcvm preload
    After the rollout of the DeviceFlexArcPreloadEnabled device policy,
    replace the enable-vpn-apps-on-flex flag with the new policy. This
    allows administrators to control the enabling of VPN apps on Flex
    devices using the DeviceFlexArcPreloadEnabled policy.
    
    Bug=b:395723805
    TEST=run arc_session_manager_unittest
    TEST=deploy Chrome and verify that the policy works as expected
    
    Change-Id: Iaf3c787a37ff1feaf2ca913522c1bbc94981d62b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6250941
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: TingHao Lin <tinghaolin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421137}

--- chrome/browser/ash/arc/session/arc_session_manager.cc
+++ chrome/browser/ash/arc/session/arc_session_manager.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/session_manager/session_manager_client.h"
 #include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/memory/swap_configuration.h"
+#include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/experiences/arc/app/arc_app_constants.h"
 #include "chromeos/ash/experiences/arc/arc_features.h"
 #include "chromeos/ash/experiences/arc/arc_prefs.h"
@@ -456,6 +457,17 @@ void UmaHistogramDeferActivationTimes(const std::string& name,
                                 base::Seconds(25), 125);
 }
 
+bool IsDeviceFlexArcPreloadEnabledByPolicy() {
+  bool device_flex_arc_preload_enabled_allowed;
+  if (ash::CrosSettings::Get()->GetBoolean(
+          ash::kDeviceFlexArcPreloadEnabled,
+          &device_flex_arc_preload_enabled_allowed)) {
+    return device_flex_arc_preload_enabled_allowed;
+  }
+  // If device policy is not set, doesn't allow arcvm dlc.
+  return false;
+}
+
 bool NeedRevenDLC() {
   if (!ash::switches::IsRevenBranding()) {
     return false;
@@ -466,9 +478,9 @@ bool NeedRevenDLC() {
     return false;
   }
 
-  if (!ash::features::IsVpnAppsOnFlexEnabled()) {
-    VLOG(1) << "enable-vpn-apps-on-flex flag is off and cannot "
-               "install arcvm images.";
+  if (!IsDeviceFlexArcPreloadEnabledByPolicy()) {
+    VLOG(1) << "Reven device cannot install arcvm images because the "
+               "DeviceFlexArcPreloadEnabled policy prevents it.";
     return false;
   }
 
