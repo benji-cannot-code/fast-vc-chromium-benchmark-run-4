87fe8dd7d78da34c57aafe7bfe854c9fd66306c4
Christoph Schwering <schwering@google.com>
2025-02-16 20:11:52 -0800

    [Autofill] Remove some unused aliases in tests
    Bug: 40100455
    Change-Id: I3ad6730c272a48be25130fab08bf5ab9dd2d1c10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6271545
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420870}

--- components/autofill/core/browser/ui/autofill_external_delegate_unittest.cc
+++ components/autofill/core/browser/ui/autofill_external_delegate_unittest.cc
@@ -81,9 +81,6 @@ namespace {
 
 using base::test::RunOnceCallback;
 using test::CreateTestAddressFormData;
-using test::CreateTestCreditCardFormData;
-using test::CreateTestPersonalInformationFormData;
-using test::CreateTestUnclassifiedFormData;
 using ::testing::_;
 using ::testing::AllOf;
 using ::testing::AnyOf;
