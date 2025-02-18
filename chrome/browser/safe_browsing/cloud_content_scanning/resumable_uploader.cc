bcb3021e7ea6bda5f6dec4d87b5224d70c6f730f
Nasser Al-shawwa <alshawwa@chromium.org>
2025-02-18 12:53:20 -0800

    Finish scanning request early when receiving async upload header.
    This is an intermediate change that unblocks the user when the async
    upload / intermediate header is received in the metadata scan response.
    We will submit a subsequent change to perform the async content scan in
    this case.
    
    Bug: 329293309
    Change-Id: I274201a37fa190de0a4a4fb71f0e1cf8d924ceb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276346
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421597}

--- chrome/browser/safe_browsing/cloud_content_scanning/resumable_uploader.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/resumable_uploader.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/base64.h"
 #include "base/files/file_path.h"
 #include "base/files/memory_mapped_file.h"
 #include "base/functional/bind.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
+#include "chrome/browser/enterprise/connectors/analysis/content_analysis_features.h"
 #include "components/file_access/scoped_file_access_delegate.h"
 #include "components/safe_browsing/core/common/utils.h"
 #include "content/public/browser/browser_thread.h"
@@ -39,6 +41,8 @@ constexpr char kUploadHeaderContentTypeHeader[] =
 constexpr char kUploadStatusHeader[] = "X-Goog-Upload-Status";
 constexpr char kUploadUrlHeader[] = "X-Goog-Upload-Url";
 constexpr char kUploadOffsetHeader[] = "X-Goog-Upload-Offset";
+constexpr char kUploadIntermediateHeader[] =
+    "X-Goog-Upload-Header-Cep-Response";
 // Content type of the upload contents.
 constexpr char kUploadContentType[] = "application/octet-stream";
 // Content type of metadata.
@@ -225,7 +229,6 @@ void ResumableUploadRequest::OnMetadataUploadCompleted(
                     GetRequestType(), ".Duration"}),
       base::TimeTicks::Now() - start_time, base::Milliseconds(1),
       base::Minutes(6), 50);
-
   int response_code = 0;
   if (!url_loader_->ResponseInfo() || !url_loader_->ResponseInfo()->headers) {
     // TODO(b/322005992): Add retry logics.
@@ -233,9 +236,26 @@ void ResumableUploadRequest::OnMetadataUploadCompleted(
     return;
   }
 
-  // If there is an error or if the metadata check has already determined a
-  // verdict, CanUploadContent() returns false.
   response_code = url_loader_->ResponseInfo()->headers->response_code();
+  if (base::FeatureList::IsEnabled(
+          enterprise_connectors::kEnableAsyncUploadAfterVerdict)) {
+    if (ContainsIntermediateHeader(url_loader_->ResponseInfo()->headers)) {
+      // Intermediate change, simply unblock the user and cleanup the request.
+      response_body = url_loader_->ResponseInfo()->headers->GetNormalizedHeader(
+          kUploadIntermediateHeader);
+      std::string output;
+      bool decode_result = base::Base64Decode(response_body.value(), &output);
+      // TODO(329293309): Add logic to perform async upload before calling
+      // Finish(). Depending on whether or not this takes place in a callback,
+      // delay the Finish() call until the content upload is complete.
+      Finish(decode_result ? url_loader_->NetError() : net::ERR_FAILED,
+             response_code, std::move(output));
+      return;
+    }
+  }
+
+  // If there is an error or if the metadata check has already
+  // determined a verdict, CanUploadContent() returns false.
   if (!CanUploadContent(url_loader_->ResponseInfo()->headers)) {
     Finish(url_loader_->NetError(), response_code, std::move(response_body));
     return;
@@ -359,6 +379,22 @@ bool ResumableUploadRequest::CanUploadContent(
   upload_url_ = std::move(upload_url).value();
   return base::EqualsCaseInsensitiveASCII(upload_status.value_or(std::string()),
                                           "active");
+}
+
+bool ResumableUploadRequest::ContainsIntermediateHeader(
+    const scoped_refptr<net::HttpResponseHeaders>& headers) {
+  if (headers->response_code() != net::HTTP_OK) {
+    return false;
+  }
+  // the upload URL is also required because we need a destination for our
+  // content
+  std::optional<std::string> upload_url =
+      headers->GetNormalizedHeader(kUploadUrlHeader);
+  if (upload_url && headers->HasHeader(kUploadIntermediateHeader)) {
+    upload_url_ = std::move(upload_url).value();
+    return true;
+  }
+  return false;
 }
 
 void ResumableUploadRequest::Finish(int net_error,
