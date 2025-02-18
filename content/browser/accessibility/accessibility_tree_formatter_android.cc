456253fb99131d0720428e3a288e391fbdde17bd
Wenyu Zhang <zhangwenyu@google.com>
2025-02-18 14:01:47 -0800

    Pass isRequired boolean to AccessibilityNodeInfoBuilder
    setFieldRequired API was introduced in AndroidX. This CL passes this
    boolean info from Clank to Android a11y node.
    
    Remove the "required" state from state description since the new API
    will be used to indicate this status. Also remove the string for this
    state since it's no longer needed.
    
    Consumed the same API in TalkBack, verified working locally. See
    screen recording: comment2 of ticket below.
    
    AX-Relnotes: N/A
    Bug: 395975812
    Change-Id: I0f90817b922c3c294d86d006dd8eac49fe44ac64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6263055
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421641}

--- content/browser/accessibility/accessibility_tree_formatter_android.cc
+++ content/browser/accessibility/accessibility_tree_formatter_android.cc
@@ -50,6 +50,7 @@ const char* const BOOL_ATTRIBUTES[] = {
     "multiselectable",
     "password",
     "range",
+    "required",
     "selected",
     "interesting",
     "table_header"
@@ -184,6 +185,7 @@ void AccessibilityTreeFormatterAndroid::AddProperties(
   dict->Set("multiline", android_node->IsMultiLine());
   dict->Set("multiselectable", android_node->IsMultiselectable());
   dict->Set("range", android_node->GetData().IsRangeValueSupported());
+  dict->Set("required", android_node->IsRequired());
   dict->Set("password", android_node->IsPasswordField());
   dict->Set("selected", android_node->IsSelected());
   dict->Set("interesting", android_node->IsInterestingOnAndroid());
