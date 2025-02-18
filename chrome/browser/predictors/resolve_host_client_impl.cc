acca9b90ad944976abf36c0a48c6134b04fc186b
Minoru Chikamune <chikamune@chromium.org>
2025-02-17 20:21:00 -0800

    Deflake ExtensionInstalledBubbleViewsExplicitSignInBrowserTest.BubbleExplicitSignin
    The above test sometimes fails due to "Check failed: task". This is
    because the above test uses base::ScopedMockTimeMessageLoopTaskRunner
    which doesn't support TaskAnnotator. ProxyLookupClientImpl and
    ResolveHostClientImpl uses TaskAnnotator expecting that
    CurrentTaskForThread() always returns a non-null task. This expectation
    fails when ScopedMockTimeMessageLoopTaskRunner is used.
    
    Sample failure:
    
    proxy_lookup_client_impl.cc(47): Check failed: task.
    
    https://chromium-swarm.appspot.com/task?id=6f1fa7800fde5811&o=true&w=true
    
    Run history:
    
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FExtensionInstalledBubbleViewsExplicitSignInBrowserTest.BubbleExplicitSignin
    
    This CL is a preparation CL for an upcoming
    feature (https://crbug.com/396998476).
    
    Bug: 396998476
    Change-Id: Ib8e0d8bdc8e4f1080ed05af9de0b8525f275c72b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6275692
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421213}

--- chrome/browser/predictors/resolve_host_client_impl.cc
+++ chrome/browser/predictors/resolve_host_client_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/check_is_test.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/task/common/task_annotator.h"
@@ -62,16 +63,21 @@ void ResolveHostClientImpl::OnComplete(
                       base::TimeTicks::Now() - resolve_host_start_time_);
 
   auto* task = base::TaskAnnotator::CurrentTaskForThread();
+  if (!task) {
+    // The `task` can be null in base::ScopedMockTimeMessageLoopTaskRunner scope
+    // in test.
+    CHECK_IS_TEST();
+  }
   // As this method is executed as a callback from a Mojo call, it should be
   // executed via RunTask() and thus have a non-delayed PendingTask associated
   // with it.
-  DCHECK(task);
-  DCHECK(task->delayed_run_time.is_null());
+  DCHECK(!task || task->delayed_run_time.is_null());
 
   // The task will have a null |queue_time| if run synchronously (this happens
   // in unit tests, for example).
-  base::TimeTicks queue_time =
-      !task->queue_time.is_null() ? task->queue_time : base::TimeTicks::Now();
+  base::TimeTicks queue_time = (task && !task->queue_time.is_null())
+                                   ? task->queue_time
+                                   : base::TimeTicks::Now();
   UMA_HISTOGRAM_TIMES("Navigation.Preconnect.ResolveHostCallbackQueueingTime",
                       base::TimeTicks::Now() - queue_time);
 
