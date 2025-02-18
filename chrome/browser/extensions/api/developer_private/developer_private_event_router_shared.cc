58e9ee8398af2d060820d276df0715d3e2aefc55
Keigo Oka <oka@google.com>
2025-02-17 18:10:37 -0800

    extensions: Shared event router implements PermissionManager observer
    Bug: 392777363
    Change-Id: Ied0cbdac486c820a6d1f558f0abdae787b1f1ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265028
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421193}

--- chrome/browser/extensions/api/developer_private/developer_private_event_router_shared.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_event_router_shared.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/developer_private/developer_private_event_router_shared.h"
 
+#include <memory>
 #include <set>
+#include <utility>
 
 #include "base/check.h"
+#include "base/values.h"
 #include "chrome/browser/extensions/error_console/error_console.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/developer_private.h"
@@ -15,8 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/disable_reason.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/extension_error.h"
+#include "extensions/browser/extension_event_histogram_value.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
+#include "extensions/browser/permissions_manager.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/service_worker/worker_id.h"
 #include "extensions/browser/uninstall_reason.h"
@@ -27,6 +32,24 @@ namespace extensions {
 
 namespace developer = api::developer_private;
 
+// static
+developer::UserSiteSettings
+DeveloperPrivateEventRouterShared::ConvertToUserSiteSettings(
+    const PermissionsManager::UserPermissionsSettings& settings) {
+  api::developer_private::UserSiteSettings user_site_settings;
+  user_site_settings.permitted_sites.reserve(settings.permitted_sites.size());
+  for (const auto& origin : settings.permitted_sites) {
+    user_site_settings.permitted_sites.push_back(origin.Serialize());
+  }
+
+  user_site_settings.restricted_sites.reserve(settings.restricted_sites.size());
+  for (const auto& origin : settings.restricted_sites) {
+    user_site_settings.restricted_sites.push_back(origin.Serialize());
+  }
+
+  return user_site_settings;
+}
+
 DeveloperPrivateEventRouterShared::DeveloperPrivateEventRouterShared(
     Profile* profile)
     : profile_(profile), event_router_(EventRouter::Get(profile_)) {
@@ -35,6 +58,7 @@ DeveloperPrivateEventRouterShared::DeveloperPrivateEventRouterShared(
   process_manager_observation_.Observe(ProcessManager::Get(profile));
   extension_prefs_observation_.Observe(ExtensionPrefs::Get(profile));
   warning_service_observation_.Observe(WarningService::Get(profile));
+  permissions_manager_observation_.Observe(PermissionsManager::Get(profile));
 }
 
 DeveloperPrivateEventRouterShared::~DeveloperPrivateEventRouterShared() =
@@ -157,6 +181,27 @@ void DeveloperPrivateEventRouterShared::ExtensionWarningsChanged(
   for (const ExtensionId& id : affected_extensions) {
     BroadcastItemStateChanged(developer::EventType::kWarningsChanged, id);
   }
+}
+
+void DeveloperPrivateEventRouterShared::OnUserPermissionsSettingsChanged(
+    const PermissionsManager::UserPermissionsSettings& settings) {
+  developer::UserSiteSettings user_site_settings =
+      ConvertToUserSiteSettings(settings);
+  base::Value::List args;
+  args.Append(user_site_settings.ToValue());
+
+  auto event = std::make_unique<Event>(
+      events::DEVELOPER_PRIVATE_ON_USER_SITE_SETTINGS_CHANGED,
+      developer::OnUserSiteSettingsChanged::kEventName, std::move(args));
+  event_router_->BroadcastEvent(std::move(event));
+}
+
+void DeveloperPrivateEventRouterShared::OnExtensionPermissionsUpdated(
+    const Extension& extension,
+    const PermissionSet& permissions,
+    PermissionsManager::UpdateReason reason) {
+  BroadcastItemStateChanged(developer::EventType::kPermissionsChanged,
+                            extension.id());
 }
 
 void DeveloperPrivateEventRouterShared::BroadcastItemStateChanged(
