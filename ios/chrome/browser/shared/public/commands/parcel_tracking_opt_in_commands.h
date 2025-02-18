d17fa47eaf49e06ce5410e01c03336b2e388ab4e
Stephen McGruer <smcgruer@google.com>
2025-02-18 10:41:54 -0800

    [Parcel Tracking] Remove most commands for showing tracking UI
    Parcel tracking was un-launched in https://crrev.com/b6ecf1cb, and
    we are now removing the related code from the codebase. This CL
    removes most of the commands relating to showing the parcel tracking
    UI. One command, `showParcelTrackingInfobarWithParcels`, is not
    removed yet as it is called from other code locations.
    
    Bug: 391002352
    Change-Id: Ic4f1c9940900356855c8b33b7f38da3ecd6451b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6258740
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421491}

--- ios/chrome/browser/shared/public/commands/parcel_tracking_opt_in_commands.h
+++ ios/chrome/browser/shared/public/commands/parcel_tracking_opt_in_commands.h
@@ -12,24 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Commands related to the parcel tracking opt-in prompt.
 @protocol ParcelTrackingOptInCommands <NSObject>
 
-// Shows the parcel tracking opt-in UI if the user is eligible for parcel list
-// `parcels`.
-- (void)showTrackingForParcels:(NSArray<CustomTextCheckingResult*>*)parcels;
-
-// Shows the parcel tracking opt-in UI if the user is eligible for filtered
-// parcel list `parcels`. Should only be called on a list of parcels that are
-// not already being tracked by the ShoppingService. Otherwise, use command
-// `showTrackingForParcels`.
-- (void)showTrackingForFilteredParcels:
-    (NSArray<CustomTextCheckingResult*>*)parcels;
-
 // Shows the parcel tracking infobar.
 - (void)showParcelTrackingInfobarWithParcels:
             (NSArray<CustomTextCheckingResult*>*)parcels
                                      forStep:(ParcelTrackingStep)step;
-
-// Shows the Parcel Tracking IPH on the Magic Stack.
-- (void)showParcelTrackingIPH;
 
 @end
 
