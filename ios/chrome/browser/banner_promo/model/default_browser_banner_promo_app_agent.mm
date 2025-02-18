f1e38fde353cc56f1294ec2d72c4bd4802da6351
Robbie Gibson <rkgibson@google.com>
2025-02-17 16:55:07 -0800

    [iOS] Fix name of Banner Promo user dismiss metric
    This was a typo. The name of the metric in histograms.xml is
    "ManuallyDismissed".
    
    Bug: 396722780
    Change-Id: I5a96f4350626fcb7662e97bbe491f7317460496b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6271663
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421174}

--- ios/chrome/browser/banner_promo/model/default_browser_banner_promo_app_agent.mm
+++ ios/chrome/browser/banner_promo/model/default_browser_banner_promo_app_agent.mm
@@ -114,7 +114,7 @@ struct SceneStateData {
 }
 
 - (void)promoCloseButtonTapped {
-  base::UmaHistogramCounts100("IOS.DefaultBrowserBannerPromo.Dismissed",
+  base::UmaHistogramCounts100("IOS.DefaultBrowserBannerPromo.ManuallyDismissed",
                               _sessionDisplayCount);
   base::UmaHistogramEnumeration(
       "IOS.DefaultBrowserBannerPromo.PromoSessionEnded",
