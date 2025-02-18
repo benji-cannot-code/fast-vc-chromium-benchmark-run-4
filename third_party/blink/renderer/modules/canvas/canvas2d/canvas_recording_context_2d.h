7294cb6a43d795ff3d1d0265ddddec581795bc63
Yi Xu <yiyix@chromium.org>
2025-02-18 06:48:04 -0800

    Revert "Moving Canvas Transform API to the new class"
    This reverts commit 562991255b64f6f8a61e6b551ef0290163768169.
    
    Reason for revert: 
    Changed the refactor plan, so this cl is not needed.
    
    Original change's description:
    > Moving Canvas Transform API to the new class
    >
    > Following cl 6072844, this cl continues moving common API, the Canvas
    > Transform API of CanvasRenderingContext2D and PaintCanvas to
    > CanvasRecordingContext2D.
    >
    > Bug: 383575391
    >
    > Change-Id: Ibff25f9b486be8a7310b13f50c97ded052b484be
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169183
    > Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    > Commit-Queue: Yi Xu <yiyix@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1417290}
    
    Bug: 383575391
    Change-Id: I79a8297d40e5c8fb1a856b7a418830f0af6be835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6250319
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421363}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_recording_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_recording_context_2d.h
@@ -13,12 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
-#include "third_party/blink/renderer/platform/transforms/affine_transform.h"
 
 namespace blink {
-
-class DOMMatrix;
-class DOMMatrixInit;
 
 // CanvasRecordingContext2D implements the canvas_recording_context_2d.idl, a 2D
 // context API that records PaintOps. This class holds the canvas recording API
@@ -28,26 +24,6 @@ class MODULES_EXPORT CanvasRecordingContext2D : public CanvasPath {
  public:
   CanvasRecordingContext2D(const CanvasRecordingContext2D&) = delete;
   CanvasRecordingContext2D& operator=(const CanvasRecordingContext2D&) = delete;
-
-  // Functions implementing the CanvasTransform Interface.
-  void scale(double sx, double sy);
-  void rotate(double angle_in_radians);
-  void translate(double tx, double ty);
-  void transform(double m11,
-                 double m12,
-                 double m21,
-                 double m22,
-                 double dx,
-                 double dy);
-  void setTransform(double m11,
-                    double m12,
-                    double m21,
-                    double m22,
-                    double dx,
-                    double dy);
-  void setTransform(DOMMatrixInit*, ExceptionState&);
-  virtual DOMMatrix* getTransform();
-  virtual void resetTransform();
 
   // Functions implementing the CanvasShadowStyles interface.
   virtual double shadowOffsetX() const;
@@ -59,7 +35,6 @@ class MODULES_EXPORT CanvasRecordingContext2D : public CanvasPath {
   virtual double shadowBlur() const;
   virtual void setShadowBlur(double);
 
-  virtual cc::PaintCanvas* GetOrCreatePaintCanvas() = 0;
   void Trace(Visitor*) const override;
 
  protected:
@@ -69,16 +44,7 @@ class MODULES_EXPORT CanvasRecordingContext2D : public CanvasPath {
   }
 
   HeapVector<Member<CanvasRenderingContext2DState>> state_stack_;
-
- private:
-  void SetTransform(const AffineTransform&);
 };
-
-ALWAYS_INLINE void CanvasRecordingContext2D::SetTransform(
-    const AffineTransform& matrix) {
-  GetState().SetTransform(matrix);
-  SetIsTransformInvertible(matrix.IsInvertible());
-}
 
 }  // namespace blink
 
