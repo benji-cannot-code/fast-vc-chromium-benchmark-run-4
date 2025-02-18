80d94618b140e800a0f1ca3e30181fea8bd2ff91
Peter Kasting <pkasting@chromium.org>
2025-02-18 06:05:57 -0800

    [cleanup] Clean up chrome_...extra_parts_profiles.cc #includes, part 1
    Remove trailing comments on #endifs.
    
    These are not required by the style guide or auto-added by the
    formatter; they are a stylistic choice. They aren't used consistently in
    this file, and like in crrev.com/c/6277306 I'll be removing them because
    they cause extra churn as I change the conditions and blocks and are
    noisy anyway.
    
    Bug: 364987728
    Change-Id: I8754d4732924351cfeb14c99b529b934e75a8848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6277346
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421352}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -114,7 +114,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/profile_network_context_service_factory.h"
 #if BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
 #include "chrome/browser/net/server_certificate_database_service_factory.h"
-#endif  //  BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
+#endif
 #include "chrome/browser/enterprise/connectors/connectors_service.h"
 #include "chrome/browser/notifications/metrics/notification_metrics_logger_factory.h"
 #include "chrome/browser/notifications/notification_display_service_factory.h"
@@ -378,7 +378,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/keyed_services/browser_context_keyed_service_factories.h"
 #include "extensions/browser/browser_context_keyed_service_factories.h"
 #include "extensions/browser/extensions_browser_client.h"
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+#endif
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "apps/browser_context_keyed_service_factories.h"
@@ -395,8 +395,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/chromeos/extensions/telemetry/api/telemetry_extension_api_browser_context_keyed_service_factories.h"
 #include "chrome/browser/extensions/api/chromeos_api_browser_context_keyed_service_factories.h"
-#endif  // BUILDFLAG(IS_CHROMEOS)
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif
+#endif
 
 #if BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS)
 #include "chrome/browser/extensions/desktop_android/desktop_android_extension_system.h"
@@ -405,7 +405,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_GLIC)
 #include "chrome/browser/glic/glic_enabling.h"
 #include "chrome/browser/glic/glic_keyed_service_factory.h"
-#endif  // BUILDFLAG(ENABLE_GLIC)
+#endif
 
 #if BUILDFLAG(ENABLE_SESSION_SERVICE)
 #include "chrome/browser/sessions/app_session_service_factory.h"
@@ -526,7 +526,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_ANDROID) && BUILDFLAG(ENABLE_PDF)
 #include "chrome/browser/accessibility/pdf_ocr_controller_factory.h"
-#endif  // !BUILDFLAG(IS_ANDROID) && BUILDFLAG(ENABLE_PDF)
+#endif
 
 #if BUILDFLAG(USE_NSS_CERTS)
 #include "chrome/browser/net/nss_service_factory.h"
@@ -547,32 +547,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 #include "chrome/browser/signin/bound_session_credentials/dice_bound_session_cookie_service_factory.h"
-#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
-
-#endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
+#endif
+#endif
 
 #if BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
 #include "chrome/browser/on_device_translation/service_controller_manager_factory.h"
-#endif  // BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
+#endif
 
 #if BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
 #include "chrome/browser/enterprise/data_controls/chrome_rules_service.h"
 
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 #include "chrome/browser/enterprise/data_controls/reporting_service.h"
-#endif  // BUILDFLAG(SAFE_BROWSING_AVAILABLE)
-#endif  // BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
+#endif
+#endif
 
 #if BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
 #include "chrome/browser/net/server_certificate_database_service_factory.h"
-#endif  // BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
+#endif
 
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || \
     BUILDFLAG(IS_WIN)
 #include "chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_observer_factory.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
-#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) ||
-        // BUILDFLAG(IS_WIN)
+#endif
 
 void AddProfilesExtraParts(ChromeBrowserMainParts* main_parts) {
   main_parts->AddParts(std::make_unique<ChromeBrowserMainExtraPartsProfiles>());
@@ -619,7 +617,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if BUILDFLAG(IS_CHROMEOS)
   chromeos::EnsureBrowserContextKeyedServiceFactoriesBuilt();
   chromeos_extensions::EnsureBrowserContextKeyedServiceFactoriesBuilt();
-#endif  // BUILDFLAG(IS_CHROMEOS)
+#endif
   extensions::EnsureBrowserContextKeyedServiceFactoriesBuilt();
 #elif BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS)
   chrome_extensions::EnsureBrowserContextKeyedServiceFactoriesBuilt();
@@ -733,8 +731,8 @@ void ChromeBrowserMainExtraPartsProfiles::
   BoundSessionCookieRefreshServiceFactory::GetInstance();
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   DiceBoundSessionCookieServiceFactory::GetInstance();
-#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
-#endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
+#endif
+#endif
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   browser_switcher::BrowserSwitcherServiceFactory::GetInstance();
 #endif
@@ -779,7 +777,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if BUILDFLAG(ENTERPRISE_CLIENT_CERTIFICATES)
   client_certificates::CertificateProvisioningServiceFactory::GetInstance();
   client_certificates::CertificateStoreFactory::GetInstance();
-#endif  // BUILDFLAG(ENTERPRISE_CLIENT_CERTIFICATES)
+#endif
   ClientHintsFactory::GetInstance();
   ClipboardRestrictionServiceFactory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS)
@@ -788,7 +786,7 @@ void ChromeBrowserMainExtraPartsProfiles::
           IsMicrosoftOneDriveIntegrationForEnterpriseEnabled()) {
     chromeos::cloud_storage::OneDrivePrefObserverFactory::GetInstance();
   }
-#endif  // BUILDFLAG(IS_CHROMEOS)
+#endif
   collaboration::CollaborationServiceFactory::GetInstance();
   collaboration::messaging::MessagingBackendServiceFactory::GetInstance();
   commerce::ShoppingServiceFactory::GetInstance();
@@ -910,7 +908,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   if (ash::features::IsFloatingSsoAllowed()) {
     ash::floating_sso::FloatingSsoServiceFactory::GetInstance();
   }
-#endif  // BUILDFLAG(IS_CHROMEOS)
+#endif
   FontPrefChangeNotifierFactory::GetInstance();
 #if !BUILDFLAG(IS_CHROMEOS)
   GAIAInfoUpdateServiceFactory::GetInstance();
@@ -922,7 +920,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   if (GlicEnabling::IsEnabledByFlags()) {
     glic::GlicKeyedServiceFactory::GetInstance();
   }
-#endif  // BUILDFLAG(ENABLE_GLIC)
+#endif
 #if !BUILDFLAG(IS_ANDROID)
   GlobalErrorServiceFactory::GetInstance();
 #endif
@@ -1011,7 +1009,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   NavigationPredictorKeyedServiceFactory::GetInstance();
 #if BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
   net::ServerCertificateDatabaseServiceFactory::GetInstance();
-#endif  // BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
+#endif
   PreloadingModelKeyedServiceFactory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS)
   NearbySharingServiceFactory::GetInstance();
@@ -1029,9 +1027,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   if (base::FeatureList::IsEnabled(features::kSafetyHub)) {
     NotificationPermissionsReviewServiceFactory::GetInstance();
   }
-#else   // BUILDFLAG(IS_ANDROID)
+#else
   NotificationPermissionsReviewServiceFactory::GetInstance();
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif
   NotificationsEngagementServiceFactory::GetInstance();
   NotifierStateTrackerFactory::GetInstance();
 #if BUILDFLAG(USE_NSS_CERTS)
@@ -1055,7 +1053,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #endif
 #if BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
   on_device_translation::ServiceControllerManagerFactory::GetInstance();
-#endif  // BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
+#endif
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
   OnDeviceTailModelServiceFactory::GetInstance();
 #endif
@@ -1227,7 +1225,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   }
 #if BUILDFLAG(ENABLE_PDF)
   screen_ai::PdfOcrControllerFactory::GetInstance();
-#endif  // BUILDFLAG(ENABLE_PDF)
+#endif
   screen_ai::ScreenAIServiceRouterFactory::EnsureFactoryBuilt();
 #endif
 #if BUILDFLAG(IS_CHROMEOS)
@@ -1348,9 +1346,9 @@ void ChromeBrowserMainExtraPartsProfiles::
           safe_browsing::kSafetyHubAbusiveNotificationRevocation)) {
     UnusedSitePermissionsServiceFactory::GetInstance();
   }
-#else   // BUILDFLAG(IS_ANDROID)
+#else
   UnusedSitePermissionsServiceFactory::GetInstance();
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif
   UrlLanguageHistogramFactory::GetInstance();
   UsbChooserContextFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
@@ -1380,8 +1378,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   if (tab_groups::SavedTabGroupUtils::SupportsSharedTabGroups()) {
     tab_groups::CollaborationMessagingObserverFactory::GetInstance();
   }
-#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) ||
-        // BUILDFLAG(IS_WIN)
+#endif
 }
 
 void ChromeBrowserMainExtraPartsProfiles::PreProfileInit() {
