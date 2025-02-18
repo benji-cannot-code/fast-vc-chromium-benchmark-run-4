6e20de42ffc93023f750e609f4b2da43670f2bac
Christoph Schwering <schwering@google.com>
2025-02-18 05:35:53 -0800

    [Autofill] Fix AutofillEntityDataManager in ProfileKeyedServiceGuestBrowserTest
    Branded builds don't have the feature AutofillAiWithDataSchema from the
    fieldtrial_testing_config.json enabled, so we need to condition the test
    on AutofillAiWithDataSchema.
    
    Bug: 388590912
    Change-Id: I915b41ab4ec53cc164eb67eb256cacd0fb6fce51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6275935
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421340}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/profile_waiter.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/enterprise/buildflags/buildflags.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -194,6 +195,7 @@ class ProfileKeyedServiceBrowserTest : public InProcessBrowserTest {
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
           features::kEnableCertManagementUIV2Write,
 #endif
+          autofill::features::kAutofillAiWithDataSchema,
           blink::features::kBrowsingTopics,
           blink::features::kBuiltInAIAPI,
           extensions_features::kForceWebRequestProxyForTest,
