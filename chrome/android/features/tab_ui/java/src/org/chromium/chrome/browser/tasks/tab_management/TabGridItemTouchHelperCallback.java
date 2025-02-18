d2663d571d0f40e474fc9be27a45942bf8c78f98
Calder Kitagawa <ckitagawa@chromium.org>
2025-02-18 05:29:58 -0800

    [Tab Switcher] Allow reorder for collaborations
    canDropOver returning false completely blocks dropping. That isn't
    desirable. Instead we should prevent the merge and hover. Hover is
    already prevented. This CL allows dropping to reorder and blocks
    merging if a collaboration somehow manages to get past the other
    check.
    
    Fixed: 396707841
    Change-Id: I993c6159285b59066fbf70c4d298814ff2ddf806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6271183
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421337}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridItemTouchHelperCallback.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridItemTouchHelperCallback.java
@@ -195,10 +195,6 @@ public class TabGridItemTouchHelperCallback extends ItemTouchHelper.SimpleCallba
                 || target.getItemViewType() == TabProperties.UiType.CUSTOM_MESSAGE) {
             return false;
         }
-        // Collaborations cannot be dropped as it would destroy the collaboration.
-        if (hasCollaboration(current)) {
-            return false;
-        }
         return super.canDropOver(recyclerView, current, target);
     }
 
@@ -283,7 +279,9 @@ public class TabGridItemTouchHelperCallback extends ItemTouchHelper.SimpleCallba
                     !(hoveredViewHolder instanceof SimpleRecyclerViewAdapter.ViewHolder)
                             || hasTabPropertiesModel(hoveredViewHolder);
 
-            if (mHoveredTabIndex != TabModel.INVALID_TAB_INDEX && mActionsOnAllRelatedTabs) {
+            if (mHoveredTabIndex != TabModel.INVALID_TAB_INDEX
+                    && mActionsOnAllRelatedTabs
+                    && !hasCollaboration(viewHolder)) {
                 RecyclerView.ViewHolder selectedViewHolder =
                         mRecyclerView.findViewHolderForAdapterPosition(mSelectedTabIndex);
                 if (selectedViewHolder != null
