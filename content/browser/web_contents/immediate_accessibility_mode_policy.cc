31da36f15498b8ec959d9d99ea4756e5ed42a6b2
David Sanders <dsanders11@ucsbalum.com>
2025-02-18 15:43:15 -0800

    Remove unused include in immediate_accessibility_mode_policy.cc
    Bug: 40318405
    Change-Id: Ibc006e6a14a517a18d532f0001c66a3578892d08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279532
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1421680}

--- content/browser/web_contents/immediate_accessibility_mode_policy.cc
+++ content/browser/web_contents/immediate_accessibility_mode_policy.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_contents/immediate_accessibility_mode_policy.h"
 
 #include "base/functional/callback.h"
-#include "content/browser/web_contents/web_contents_impl.h"
 
 namespace content {
 
