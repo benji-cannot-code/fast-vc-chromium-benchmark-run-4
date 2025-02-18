781510baacbd394aaa1807ecfe62ebe77d13c630
Reilly Grant <reillyg@chromium.org>
2025-02-18 10:48:01 -0800

    webnn: Don't call ChromeMLApi when CFI is enabled
    CFI's indirect call checks do not behave well when calling function
    pointers from libraries opened with dlopen(). This change provides a
    quick fix by simply disabling WebNN's integration with the GPU delegate
    provided by liboptimization_guide_internal.so while we figure out a
    better solution.
    
    Fixed: 396748132
    Change-Id: I86b8518003cf48b1a2e5cd8e8eea8370aa66186a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279085
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421498}

--- services/webnn/tflite/op_resolver.cc
+++ services/webnn/tflite/op_resolver.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/tflite/src/tensorflow/lite/tflite_with_xnnpack_optional.h"
 #endif
 
-#if defined(ENABLE_ML_INTERNAL)
+#if defined(WEBNN_USE_CHROME_ML_API)
 #include "services/on_device_model/ml/chrome_ml.h"  // nogncheck
 #include "services/on_device_model/ml/chrome_ml_api.h"  // nogncheck
 #endif
@@ -290,7 +290,7 @@ OpResolver::OpResolver(const mojom::CreateContextOptions& options) {
   }
 #endif
 
-#if defined(ENABLE_ML_INTERNAL)
+#if defined(WEBNN_USE_CHROME_ML_API)
   if (options.device == mojom::CreateContextOptions::Device::kGpu) {
     // TODO(crbug.com/394119734): Simplify this check once these functions are
     // always available.
