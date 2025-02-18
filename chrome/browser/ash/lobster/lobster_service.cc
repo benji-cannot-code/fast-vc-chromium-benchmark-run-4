5d807ad6ee73242af5e1bd3b264c68816b7dab0a
Grey Wang <greywang@google.com>
2025-02-17 21:11:20 -0800

    [Lobster] Make announcement after insertion
    Bug: b:391645622
    Change-Id: Ibc78f862991321088e345774dcc9191ce3deb6cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249741
    Reviewed-by: Chuong Ho <hdchuong@chromium.org>
    Commit-Queue: Grey Wang <greywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421224}

--- chrome/browser/ash/lobster/lobster_service.cc
+++ chrome/browser/ash/lobster/lobster_service.cc
@@ -29,6 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/manta/snapper_provider.h"
 #include "ui/display/screen.h"
 
+namespace {
+
+constexpr std::u16string_view kAnnouncementViewName = u"Lobster";
+
+}  // namespace
+
 LobsterService::LobsterService(
     std::unique_ptr<manta::SnapperProvider> snapper_provider,
     Profile* profile)
@@ -43,7 +49,8 @@ LobsterService::LobsterService(
               image_provider_.get(),
               &candidate_id_generator_))),
       resizer_(std::make_unique<LobsterCandidateResizer>(image_fetcher_.get())),
-      system_state_provider_(profile) {
+      system_state_provider_(profile),
+      announcer_(kAnnouncementViewName) {
   if (profile != nullptr) {
     PrefService* pref_service = profile->GetPrefs();
     pref_change_registrar_.Init(pref_service);
@@ -137,6 +144,10 @@ void LobsterService::OnFocus(int context_id) {
 
   queued_insertion_->Commit();
   queued_insertion_ = nullptr;
+}
+
+void LobsterService::Announce(const std::u16string& message) {
+  announcer_.Announce(message);
 }
 
 bool LobsterService::OverrideLobsterImageProviderForTesting() {
