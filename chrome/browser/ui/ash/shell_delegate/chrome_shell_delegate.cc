a057b2ff585686a45f1ed6d512034cddf2509546
Eriko Kurimoto <elkurin@google.com>
2025-02-18 08:22:53 -0800

    shell_delegate: Rename SetTabScrubberEnabled
    This is a clanup cl to rename SetTabScrubberChromeOSEnabled to
    SetTabScrubberEnabled since the method is now implemented only in
    chromeos so that we no longer need to specify the platform to the name.
    
    This CL does not affect the behavior.
    
    Bug: None
    Change-Id: I56cd2c29d2cd9ba1abcfc30e8fa18e6303844fa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6275367
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421410}

--- chrome/browser/ui/ash/shell_delegate/chrome_shell_delegate.cc
+++ chrome/browser/ui/ash/shell_delegate/chrome_shell_delegate.cc
@@ -258,7 +258,7 @@ bool ChromeShellDelegate::CanGoBack(gfx::NativeWindow window) const {
   return contents ? contents->GetController().CanGoBack() : false;
 }
 
-void ChromeShellDelegate::SetTabScrubberChromeOSEnabled(bool enabled) {
+void ChromeShellDelegate::SetTabScrubberEnabled(bool enabled) {
   ash::TabScrubber::GetInstance()->SetEnabled(enabled);
 }
 
