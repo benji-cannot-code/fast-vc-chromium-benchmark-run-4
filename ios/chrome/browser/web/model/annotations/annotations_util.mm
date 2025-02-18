6ddf97200ac3473e9db3358dd0831617f2e0bb6b
Stephen McGruer <smcgruer@google.com>
2025-02-18 09:54:00 -0800

    [Parcel Tracking] Remove parcel tracking code from annotations
    Parcel tracking was un-launched in https://crrev.com/b6ecf1cb, and
    we are now removing the related code from the codebase.
    
    Bug: 391002352
    Change-Id: Ia11a1e4925a16ed3ae8d34091c35ad6341343756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6259075
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421456}

--- ios/chrome/browser/web/model/annotations/annotations_util.mm
+++ ios/chrome/browser/web/model/annotations/annotations_util.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/feature_list.h"
 #import "base/metrics/field_trial_params.h"
 #import "components/prefs/pref_service.h"
-#import "ios/chrome/browser/parcel_tracking/features.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/web/model/annotations/annotations_util.h"
 #import "ios/web/common/features.h"
@@ -146,7 +145,8 @@ bool IsLongPressAnnotationEnabledForType(PrefService* prefs,
     case WebAnnotationType::kEMailAddresses:
       return true;
     case WebAnnotationType::kPackage:
-      return IsIOSParcelTrackingEnabled();
+      // Package tracking was turned down in https://crbug.com/377724731
+      return false;
     case WebAnnotationType::kPhoneNumbers:
       return true;
     case WebAnnotationType::kUnits:
@@ -171,7 +171,8 @@ bool IsOneTapAnnotationEnabledForType(PrefService* prefs,
     case WebAnnotationType::kEMailAddresses:
       return true;
     case WebAnnotationType::kPackage:
-      return IsIOSParcelTrackingEnabled();
+      // Package tracking was turned down in https://crbug.com/377724731
+      return false;
     case WebAnnotationType::kPhoneNumbers:
       return true;
     case WebAnnotationType::kUnits:
