ad272847b98e0b3b91b34278cd9424c1af2c0a14
Hira Mahmood <hiramahmood@google.com>
2025-02-19 09:28:57 -0800

    [iOS] Create feature flag for Bling's Best Features screen
    This Cl creates the feature flag for the 6-arm experiment.
    Design doc: go/blings-best-features-dd
    
    Fixed: 396479573
    Change-Id: I2900e861f4a6ddafebdb146ce976ed1bb415f21d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6271603
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422018}

--- ios/chrome/browser/first_run/ui_bundled/features.mm
+++ ios/chrome/browser/first_run/ui_bundled/features.mm
@@ -12,11 +12,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace first_run {
 
+BASE_FEATURE(kAnimatedDefaultBrowserPromoInFRE,
+             "AnimatedDefaultBrowserPromoInFRE",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kBestFeaturesScreenInFirstRun,
+             "BestFeaturesScreenInFirstRunExperience",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kUpdatedFirstRunSequence,
              "UpdatedFirstRunSequence",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+const char kAnimatedDefaultBrowserPromoInFREExperimentType[] =
+    "AnimatedDefaultBrowserPromoInFREExperimentType";
+
+const char kBestFeaturesScreenInFirstRunParam[] =
+    "BestFeaturesScreenInFirstRunParam";
+
 const char kUpdatedFirstRunSequenceParam[] = "updated-first-run-sequence-param";
+
+BestFeaturesScreenVariationType GetBestFeaturesScreenVariationType() {
+  if (!base::FeatureList::IsEnabled(kBestFeaturesScreenInFirstRun)) {
+    return BestFeaturesScreenVariationType::kDisabled;
+  }
+  return static_cast<BestFeaturesScreenVariationType>(
+      base::GetFieldTrialParamByFeatureAsInt(kBestFeaturesScreenInFirstRun,
+                                             kBestFeaturesScreenInFirstRunParam,
+                                             1));
+}
 
 UpdatedFRESequenceVariationType GetUpdatedFRESequenceVariation(
     ProfileIOS* profile) {
@@ -31,17 +55,10 @@ UpdatedFRESequenceVariationType GetUpdatedFRESequenceVariation(
                                              kUpdatedFirstRunSequenceParam, 1));
 }
 
-BASE_FEATURE(kAnimatedDefaultBrowserPromoInFRE,
-             "AnimatedDefaultBrowserPromoInFRE",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 bool IsAnimatedDefaultBrowserPromoInFREEnabled() {
   return base::FeatureList::IsEnabled(kAnimatedDefaultBrowserPromoInFRE) &&
          !base::FeatureList::IsEnabled(first_run::kUpdatedFirstRunSequence);
 }
-
-const char kAnimatedDefaultBrowserPromoInFREExperimentType[] =
-    "AnimatedDefaultBrowserPromoInFREExperimentType";
 
 AnimatedDefaultBrowserPromoInFREExperimentType
 AnimatedDefaultBrowserPromoInFREExperimentTypeEnabled() {
