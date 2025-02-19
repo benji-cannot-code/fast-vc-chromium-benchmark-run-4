50f5e8c9e490a823af40f0c76ab5de08a0dc048a
Fernando Ramirez <ferny@google.com>
2025-02-19 12:08:49 -0800

    Add kill-switch flag for displaying "Expired" text on expired cards
    For cards that have already expired, we currently display the text
    "Expires **/**" - when it should ideally display "Expired **/**". This
    CL adds a kill-switch flag that we will use to quickly enable/disable
    this feature.
    
    Bug: 397498693
    Change-Id: I598b8e159625459405fa848cdfc897b61bceeca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279588
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422131}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -2485,6 +2485,11 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      FEATURE_WITH_PARAMS_VALUE_TYPE(first_run::kBestFeaturesScreenInFirstRun,
                                     kBestFeaturesScreenInFirstRunVariations,
                                     "BestFeaturesScreenInFirstRun")},
+    {"autofill-enable-card-expired-text",
+     flag_descriptions::kAutofillEnableCardExpiredTextName,
+     flag_descriptions::kAutofillEnableCardExpiredTextDescription,
+     flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(autofill::features::kAutofillEnableCardExpiredText)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
