4b34bc3e6b13a024024f6b4385f8ac90d4e7c1cb
Rushan Suleymanov <rushans@google.com>
2025-02-19 02:54:06 -0800

    Move GCM network to a dedicated sequence
    This CL adds a dedicated single-threaded sequence for GCM driver for
    network operations. This is needed to speed up GCM driver initialization
    on browser startup because IO thread can be blocked for too much time,
    and GCM driver is an important part e.g. for Sync invalidations to
    postpone its initialization for too long. It could happen that there are
    several sync cycles (also using network) while the GCM driver is not
    initialized yet.
    
    The change is behind a feature toggle disabled by default.
    
    Bug: 40281771
    Change-Id: I9b53f42c4d21c629a66aedaf2c0ae4d01b5e05bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276588
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421856}

--- chrome/browser/gcm/gcm_profile_service_factory.cc
+++ chrome/browser/gcm/gcm_profile_service_factory.cc
@@ -4,11 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/gcm/gcm_profile_service_factory.h"
+
 #include <memory>
 
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/no_destructor.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/task/single_thread_task_runner_thread_mode.h"
 #include "base/task/thread_pool.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
@@ -40,6 +43,12 @@ namespace gcm {
 namespace {
 
 #if !BUILDFLAG(IS_ANDROID)
+// When enabled, GCM will use a dedicated thread for network operations instead
+// of the IO thread.
+BASE_FEATURE(kGCMUseDedicatedNetworkThread,
+             "GCMUseDedicatedNetworkThread",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Requests a ProxyResolvingSocketFactory on the UI thread. Note that a WeakPtr
 // of GCMProfileService is needed to detect when the KeyedService shuts down,
 // and avoid calling into |profile| which might have also been destroyed.
@@ -48,8 +57,9 @@ void RequestProxyResolvingSocketFactoryOnUIThread(
     base::WeakPtr<GCMProfileService> service,
     mojo::PendingReceiver<network::mojom::ProxyResolvingSocketFactory>
         receiver) {
-  if (!service || !profile)
+  if (!service || !profile) {
     return;
+  }
   network::mojom::NetworkContext* network_context =
       profile->GetDefaultStoragePartition()->GetNetworkContext();
   network_context->CreateProxyResolvingSocketFactory(std::move(receiver));
@@ -66,7 +76,18 @@ void RequestProxyResolvingSocketFactory(
                                 std::move(profile), std::move(service),
                                 std::move(receiver)));
 }
-#endif
+
+scoped_refptr<base::SequencedTaskRunner> GetNetworkThreadTaskRunner() {
+  if (base::FeatureList::IsEnabled(kGCMUseDedicatedNetworkThread)) {
+    return base::ThreadPool::CreateSingleThreadTaskRunner(
+        {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+         base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
+        base::SingleThreadTaskRunnerThreadMode::SHARED);
+  }
+  return content::GetIOThreadTaskRunner({});
+}
+
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 GCMProfileServiceFactory::GlobalTestingFactory& GetTestingFactory() {
   static base::NoDestructor<GCMProfileServiceFactory::GlobalTestingFactory>
@@ -162,7 +183,7 @@ GCMProfileServiceFactory::BuildServiceInstanceForBrowserContext(
       gcm::GetProductCategoryForSubtypes(profile->GetPrefs()),
       IdentityManagerFactory::GetForProfile(profile),
       std::make_unique<GCMClientFactory>(), content::GetUIThreadTaskRunner({}),
-      content::GetIOThreadTaskRunner({}), blocking_task_runner);
+      GetNetworkThreadTaskRunner(), blocking_task_runner);
 #endif
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
   // TODO(crbug.com/40260641): Removing image fetcher references here breaks
