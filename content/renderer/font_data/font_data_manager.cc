3836b2e41795b6d79dc1fd8739f4274d2350fce3
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2025-02-19 12:21:30 -0800

    Allow choosing freetype or fontations in FontDataManager
    This change splits out Freetype and Fontations codepaths, previously
    combined under the Internal experimental param, into their own params.
    
    This will allow varying between the 3 typeface types in an A/B
    performance experiment.
    
    Bug: 395924057
    Change-Id: I5acfcca5691b0aaf93d26895da2fa1da702b0b85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279725
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422139}

--- content/renderer/font_data/font_data_manager.cc
+++ content/renderer/font_data/font_data_manager.cc
@@ -23,9 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/src/ports/SkTypeface_win_dw.h"  // nogncheck
 #if BUILDFLAG(ENABLE_FREETYPE)
 #include "third_party/skia/include/ports/SkFontMgr_empty.h"
-#else
-#include "third_party/skia/include/ports/SkTypeface_fontations.h"
 #endif
+#include "third_party/skia/include/ports/SkTypeface_fontations.h"
 
 namespace font_data_service {
 
@@ -224,15 +223,20 @@ sk_sp<SkTypeface> FontDataManager::onMakeFromStreamArgs(
   // Experiment will test the performance of different SkTypefaces.
   // 'custom_fnt_mgr_' is a wrapper to create an SkFreeType typeface.
 
-  return features::kFontDataServiceTypefaceType.Get() ==
-                 features::FontDataServiceTypefaceType::kInternal
-             ?
+  if (features::kFontDataServiceTypefaceType.Get() ==
+      features::FontDataServiceTypefaceType::kDwrite) {
+    return DWriteFontTypeface::MakeFromStream(std::move(stream), args);
+  } else if (features::kFontDataServiceTypefaceType.Get() ==
+             features::FontDataServiceTypefaceType::kFreetype) {
+    // Chromium currently always sets ENABLE_FREETYPE, but nonetheless allow
+    // falling back to fontations if the param is set to freetype but freetype
+    // isn't enabled.
 #if BUILDFLAG(ENABLE_FREETYPE)
-             custom_fnt_mgr_->makeFromStream(std::move(stream), args)
-#else
-             SkTypeface_Make_Fontations(std::move(stream), args)
+    return custom_fnt_mgr_->makeFromStream(std::move(stream), args);
 #endif
-             : DWriteFontTypeface::MakeFromStream(std::move(stream), args);
+  }
+
+  return SkTypeface_Make_Fontations(std::move(stream), args);
 }
 
 sk_sp<SkTypeface> FontDataManager::onMakeFromFile(const char path[],
