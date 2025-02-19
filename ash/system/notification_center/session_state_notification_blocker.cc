68a6e014b38bc9af8f96678b9456d84ec845425c
Ashutosh Singhal <macinashutosh@google.com>
2025-02-19 02:28:33 -0800

    Always Allow remote screen sharing notification
    The session state notification blocker prevents the remote screen
    sharing notification from appearing on the login screen. This
    notification serves as a privacy indicator during active sessions. To
    ensure transparency and prevent unauthorized monitoring, this
    notification should always be enabled (excluding app mode), as it
    confirms active remote screen sharing
    
    Bug: b:390165054
    Change-Id: I81eae381a912387948082eb04d44d11904faa1a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276091
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421847}

--- ash/system/notification_center/session_state_notification_blocker.cc
+++ ash/system/notification_center/session_state_notification_blocker.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/do_not_disturb_notification_controller.h"
 #include "ash/system/lock_screen_notification_controller.h"
 #include "ash/system/power/battery_notification.h"
+#include "ash/system/privacy/screen_security_controller.h"
 #include "base/containers/contains.h"
 #include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller_delegate_impl.h"
 #include "ui/message_center/message_center.h"
@@ -130,6 +131,13 @@ bool SessionStateNotificationBlocker::ShouldShowNotification(
     return false;
   }
 
+  // Always show remote activity notification (which also acts as a privacy
+  // indicator) irrespective of the session state (except when running in
+  // app mode).
+  if (notification.id() == ash::kRemotingScreenShareNotificationId) {
+    return true;
+  }
+
   const SessionState session_state =
       Shell::Get()->session_controller()->GetSessionState();
   // Do not show the "Do not disturb" notification if there is no active
@@ -178,6 +186,13 @@ bool SessionStateNotificationBlocker::ShouldShowNotificationAsPopup(
           message_center::NotifierType::SYSTEM_COMPONENT &&
       login_delay_timer_.IsRunning()) {
     return false;
+  }
+
+  // Always show remote activity notification (which also acts as a privacy
+  // indicator) irrespective of the session state (except when running in
+  // app mode).
+  if (notification.id() == ash::kRemotingScreenShareNotificationId) {
+    return true;
   }
 
   if (IsAllowedDuringOOBE(notification.id())) {
