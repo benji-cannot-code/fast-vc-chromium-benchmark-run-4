ced403afab03649a5314670cc1ee14bce7eeddd2
Brandon Wylie <wylieb@chromium.org>
2025-02-19 13:56:26 -0800

    [bookmarks] Modularize bookmarks browser code
    Moved production bookmarks browser code to chrome/browser/bookmarks
    directory. Made some small changes along the way to simplify the
    process.
    * Moved all production bookmarks browser code to module.
    * Added a separate target for files that can be modularized now.
    * Added a separate gni list for files that can't be modularized now
      which is included in chrome_java directly. Over time I will refactor
      those files so that they are moved into the main modularized target.
    * Merged ReadingListUtils and BookmarkUtils. In the long run, browser
      reading list code shouldn't really exist since the concepts are
      merged at the native level.
    * Moved one method from BookmarkUtils to BookmarkModel to allow for
      more files to be modularized.
    
    Eventually some files, particularly implementations of interfaces
    defined in chrome/browser/bookmarks, will live in the unmodularized
    directory.
    
    Bug: 395908100
    Change-Id: Ic1258d23dd530e468c7d1ae616acbc9af98ab89e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265507
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422188}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/SelectableTabListEditorTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/SelectableTabListEditorTest.java
@@ -77,7 +77,6 @@ import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.app.bookmarks.BookmarkEditActivity;
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
-import org.chromium.chrome.browser.bookmarks.BookmarkUtils;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.layouts.LayoutType;
@@ -261,7 +260,7 @@ public class SelectableTabListEditorTest {
                     if (mSnackbarManager == null) return;
                     mSnackbarManager.dismissAllSnackbars();
                 });
-        BookmarkUtils.clearLastUsedPrefs();
+        BookmarkModel.clearLastUsedParent();
     }
 
     private @TabListCoordinator.TabListMode int getMode() {
--- chrome/browser/bookmarks/android/bookmark_bridge.cc
+++ chrome/browser/bookmarks/android/bookmark_bridge.cc
@@ -68,7 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "chrome/android/chrome_jni_headers/BookmarkBridge_jni.h"
+#include "chrome/browser/bookmarks/android/jni_headers/BookmarkBridge_jni.h"
 
 using base::android::AttachCurrentThread;
 using base::android::JavaParamRef;
