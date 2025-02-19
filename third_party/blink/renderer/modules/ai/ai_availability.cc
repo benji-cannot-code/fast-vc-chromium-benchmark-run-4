69f11f5ed81e6b6ec1e3418cd895721f2785d173
Christine Hollingsworth <christinesm@chromium.org>
2025-02-19 15:54:14 -0800

    [Translator] Implement availability() method
    Implementation of the `availability()` method for Translator
    API.
    
    This method will be modified with updated requirements per privacy
    review in followup CLs (which will be tested and flag-guarded).
    
    Bug: 390459310
    Change-Id: Id31b940978a54ac5eecf2490d689a57fe2345220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227470
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422259}

--- third_party/blink/renderer/modules/ai/ai_availability.cc
+++ third_party/blink/renderer/modules/ai/ai_availability.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "third_party/blink/public/mojom/ai/ai_manager.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/ai/ai_manager.mojom-blink.h"
+#include "third_party/blink/public/mojom/on_device_translation/translation_manager.mojom-blink.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/modules/ai/ai_metrics.h"
 #include "third_party/blink/renderer/modules/ai/exception_helpers.h"
@@ -47,6 +48,41 @@ AIAvailability HandleModelAvailabilityCheckResult(
   base::UmaHistogramEnumeration(
       AIMetrics::GetAIAvailabilityMetricName(session_type), availability);
   return availability;
+}
+
+AIAvailability HandleTranslatorAvailabilityCheckResult(
+    ExecutionContext* execution_context,
+    mojom::blink::CanCreateTranslatorResult result) {
+  switch (result) {
+    case mojom::blink::CanCreateTranslatorResult::kReadily:
+      return HandleModelAvailabilityCheckResult(
+          execution_context, AIMetrics::AISessionType::kTranslator,
+          mojom::blink::ModelAvailabilityCheckResult::kAvailable);
+    case mojom::blink::CanCreateTranslatorResult::kAfterDownloadLibraryNotReady:
+    case mojom::blink::CanCreateTranslatorResult::
+        kAfterDownloadLanguagePackNotReady:
+    case mojom::blink::CanCreateTranslatorResult::
+        kAfterDownloadLibraryAndLanguagePackNotReady:
+      return HandleModelAvailabilityCheckResult(
+          execution_context, AIMetrics::AISessionType::kTranslator,
+          mojom::blink::ModelAvailabilityCheckResult::kDownloadable);
+    case mojom::blink::CanCreateTranslatorResult::kNoNotSupportedLanguage:
+      return HandleModelAvailabilityCheckResult(
+          execution_context, AIMetrics::AISessionType::kTranslator,
+          mojom::blink::ModelAvailabilityCheckResult::
+              kUnavailableUnsupportedLanguage);
+    case mojom::blink::CanCreateTranslatorResult::kNoAcceptLanguagesCheckFailed:
+    case mojom::blink::CanCreateTranslatorResult::
+        kNoExceedsLanguagePackCountLimitation:
+    case mojom::blink::CanCreateTranslatorResult::kNoServiceCrashed:
+    case mojom::blink::CanCreateTranslatorResult::kNoDisallowedByPolicy:
+    case mojom::blink::CanCreateTranslatorResult::
+        kNoExceedsServiceCountLimitation:
+      return HandleModelAvailabilityCheckResult(
+          execution_context, AIMetrics::AISessionType::kTranslator,
+          mojom::blink::ModelAvailabilityCheckResult::
+              kUnavailableTranslationNotEligible);
+  }
 }
 
 V8AIAvailability AIAvailabilityToV8(AIAvailability availability) {
--- third_party/blink/renderer/modules/ai/ai_availability.h
+++ third_party/blink/renderer/modules/ai/ai_availability.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_AI_AI_AVAILABILITY_H_
 
 #include "third_party/blink/public/mojom/ai/ai_manager.mojom-blink-forward.h"
+#include "third_party/blink/public/mojom/on_device_translation/translation_manager.mojom-blink-forward.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ai_availability.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/modules/ai/ai_metrics.h"
@@ -32,6 +33,10 @@ AIAvailability HandleModelAvailabilityCheckResult(
     ExecutionContext* execution_context,
     AIMetrics::AISessionType session_type,
     mojom::blink::ModelAvailabilityCheckResult result);
+
+AIAvailability HandleTranslatorAvailabilityCheckResult(
+    ExecutionContext* execution_context,
+    mojom::blink::CanCreateTranslatorResult result);
 
 }  // namespace blink
 
