a4d0c65b9e02aeec7edcef0942a8c5085de5d6a0
Zaina Al-Mashni <zalmashni@google.com>
2025-02-19 05:29:07 -0800

    [DBD] Add DBD revamp feature flag
    Bug: 397187800
    Change-Id: I1a92c0f6913a063d129e627752fd861e389d760a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276088
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421892}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -215,6 +215,9 @@ extern const char kDataSharingDescription[];
 extern const char kDataSharingJoinOnlyName[];
 extern const char kDataSharingJoinOnlyDescription[];
 
+extern const char kDbdRevampDesktopName[];
+extern const char kDbdRevampDesktopDescription[];
+
 extern const char kDisableFacilitatedPaymentsMerchantAllowlistName[];
 extern const char kDisableFacilitatedPaymentsMerchantAllowlistDescription[];
 
