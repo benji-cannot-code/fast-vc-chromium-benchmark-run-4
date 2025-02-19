26a0cbc5aeae9cdbfa1245ed90e726f5f349fea0
Lukasz Anforowicz <lukasza@chromium.org>
2025-02-18 21:38:02 -0800

    [rust png] Make `ClipboardUtilitiesTest` `libpng`-agnostic.
    Before this CL, a test in the `ClipboardUtilitiesTest` suite would
    hardcode the exact PNG encoding.  This CL tweaks the test so that the
    test assertions will succeed regardless of which PNG encoder is used.
    In other words, this CL focuses `ClipboardUtilitiesTest` on testing the
    clipboard-layer functionality (e.g. `PNGToImageMarkup`) rather than on
    (re)testing implementation details/choices of a PNG encoder.
    
    Fixed: 395908713
    Change-Id: Iefeb4d08e697b998bbea6761d42d29b26aebded4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279179
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Sanket Joshi <sajos@microsoft.com>
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1421785}

--- third_party/blink/renderer/core/clipboard/clipboard_utilities_test.cc
+++ third_party/blink/renderer/core/clipboard/clipboard_utilities_test.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/clipboard/clipboard_utilities.h"
 
+#include <string>
+
 #include "base/containers/span.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/image-encoders/image_encoder.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
@@ -65,9 +68,16 @@ TEST(ClipboardUtilitiesTest, PNGToImageMarkup) {
   Vector<uint8_t> png_data;
   EXPECT_TRUE(ImageEncoder::Encode(&png_data, pixmap, options));
 
-  EXPECT_EQ(
-      R"HTML(<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAFCAYAAAB8ZH1oAAAADElEQVQYGWNgGEYAAADNAAGVVebMAAAAAElFTkSuQmCC" alt=""/>)HTML",
-      PNGToImageMarkup(png_data));
+  std::string markup = PNGToImageMarkup(png_data).Utf8();
+
+  // The first 16 of a PNG file are always the same, so the
+  // `StartsWith`/`EndsWith`-based assertions below are expected to succeed
+  // regardless of the exact encoding settings or the encoding library used.
+  EXPECT_THAT(
+      markup,
+      testing::StartsWith(
+          R"HTML(<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhE)HTML"));
+  EXPECT_THAT(markup, testing::EndsWith(R"HTML(" alt=""/>)HTML"));
 }
 
 }  // namespace blink
