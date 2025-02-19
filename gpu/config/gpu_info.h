07ef7782ee3e376b956167c4706b8166342546bb
Saifuddin Hitawala <hitawala@chromium.org>
2025-02-19 13:32:02 -0800

    [gpu] Add SkiaBackendType to GpuInfo
    Add SkiaBackendType info to GpuInfo. This moves the SkiaBackendType
    from SharedContextState to GpuPreferences so that it can be shared
    more easily. Also update the relevant mojom files.
    
    This will be later displayed in chrome://gpu information.
    
    Bug: 364913797
    Change-Id: I588652fd8babe17d28061ac39c4df909e6e38ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6282205
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422176}

--- gpu/config/gpu_info.h
+++ gpu/config/gpu_info.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/version.h"
 #include "build/build_config.h"
+#include "gpu/config/gpu_preferences.h"
 #include "gpu/gpu_export.h"
 #include "gpu/vulkan/buildflags.h"
 #include "ui/gfx/geometry/size.h"
@@ -392,6 +393,9 @@ struct GPU_EXPORT GPUInfo {
 
   // The DisplayType requested from ANGLE.
   std::string display_type;
+
+  // Skia Backend used for rendering and compositing.
+  SkiaBackendType skia_backend_type = SkiaBackendType::kNone;
 
   // The GL_VERSION string.
   std::string gl_version;
--- gpu/ipc/common/gpu_info_mojom_traits.h
+++ gpu/ipc/common/gpu_info_mojom_traits.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "gpu/config/gpu_info.h"
+#include "gpu/config/gpu_preferences.h"
 #include "gpu/gpu_export.h"
 #include "gpu/ipc/common/gpu_info.mojom.h"
 #include "ui/gfx/buffer_types.h"
@@ -79,6 +80,14 @@ struct GPU_EXPORT
       const gpu::GPUInfo::GPUDevice& input) {
     return input.gpu_preference;
   }
+};
+
+template <>
+struct GPU_EXPORT EnumTraits<gpu::mojom::SkiaBackendType,
+                             gpu::SkiaBackendType> {
+  static gpu::mojom::SkiaBackendType ToMojom(gpu::SkiaBackendType type);
+  static bool FromMojom(gpu::mojom::SkiaBackendType input,
+                        gpu::SkiaBackendType* out);
 };
 
 template <>
@@ -316,6 +325,10 @@ struct GPU_EXPORT StructTraits<gpu::mojom::GpuInfoDataView, gpu::GPUInfo> {
 
   static const std::string& display_type(const gpu::GPUInfo& input) {
     return input.display_type;
+  }
+
+  static gpu::SkiaBackendType skia_backend_type(const gpu::GPUInfo& input) {
+    return input.skia_backend_type;
   }
 
   static const std::string& gl_version(const gpu::GPUInfo& input) {
