dd0d786ce0c1a4b33b843ec355e5aae209d2ed4b
andreydav <andreydav@google.com>
2025-02-19 09:05:04 -0800

    Remove extra AllowJavascript calls in LockScreenReauthHandler
    This is a follow-up to https://crrev.com/c/6264167. AllowJavascript is
    called in HandleStartOnlineAuth which is called at the start of lock
    screen online reauth flow. All other methods handling incoming messages
    are expected to be called after HandleStartOnlineAuth, so they shouldn't
    need to call AllowJavascript.
    
    Bug: 395396188
    Change-Id: I58ebe5e7fd9840c389960322090c7e95f8425cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276525
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422000}

--- chrome/browser/ui/webui/ash/lock_screen_reauth/lock_screen_reauth_handler.cc
+++ chrome/browser/ui/webui/ash/lock_screen_reauth/lock_screen_reauth_handler.cc
@@ -127,7 +127,6 @@ void LockScreenReauthHandler::HandleStartOnlineAuth(
 
 void LockScreenReauthHandler::HandleAuthenticatorLoaded(
     const base::Value::List& value) {
-  AllowJavascript();
   VLOG(1) << "Authenticator finished loading";
   authenticator_state_ = AuthenticatorState::LOADED;
 
@@ -306,7 +305,6 @@ void LockScreenReauthHandler::CallJavascript(const std::string& function,
 
 void LockScreenReauthHandler::HandleCompleteAuthentication(
     const base::Value::List& params) {
-  AllowJavascript();
   absl::Cleanup run_callback_on_return = [this] {
     auth_flow_auto_reload_manager_.Terminate();
   };
@@ -417,7 +415,6 @@ void LockScreenReauthHandler::CheckCredentials(
 
 void LockScreenReauthHandler::HandleUpdateUserPassword(
     const base::Value::List& value) {
-  AllowJavascript();
   DCHECK(!value.empty());
   std::string old_password = value[0].GetString();
   lock_screen_reauth_manager_->UpdateUserPassword(old_password);
@@ -434,7 +431,6 @@ void LockScreenReauthHandler::ShowSamlConfirmPasswordScreen() {
 
 void LockScreenReauthHandler::HandleOnPasswordTyped(
     const base::Value::List& value) {
-  AllowJavascript();
   OnPasswordTyped(value[0].GetString());
 }
 
@@ -482,7 +478,6 @@ void LockScreenReauthHandler::SamlConfirmPassword(
 }
 
 void LockScreenReauthHandler::HandleWebviewLoadAborted(int error_code) {
-  AllowJavascript();
   if (error_code == net::ERR_BLOCKED_BY_ADMINISTRATOR) {
     // Ignore this error to let the user see the error screen for blocked sites.
     return;
@@ -508,7 +503,6 @@ void LockScreenReauthHandler::HandleWebviewLoadAborted(int error_code) {
 
 void LockScreenReauthHandler::HandleGetDeviceId(
     const std::string& callback_id) {
-  AllowJavascript();
   user_manager::KnownUser known_user{g_browser_process->local_state()};
   ResolveJavascriptCallback(callback_id, GetDeviceId(known_user));
 }
