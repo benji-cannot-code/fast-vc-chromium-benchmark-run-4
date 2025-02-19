efdc37ac84098f4fb034a0d93ad087526dd07740
Christoph Schwering <schwering@google.com>
2025-02-19 09:44:30 -0800

    [Autofill] Add EntityDataManager::GetEntityInstance()
    Bug: 397623202
    Change-Id: I6140741609565364a2c309e9e33a907645199c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281030
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422036}

--- components/autofill/core/browser/data_model/entity_instance.h
+++ components/autofill/core/browser/data_model/entity_instance.h
@@ -60,7 +60,7 @@ class AttributeInstance final : public FormGroup {
     std::string format;
   };
 
-  // Less-than relation based on the AttributeType.
+  // Transparent less-than relation based on the AttributeType.
   struct CompareByType;
 
   // Comparator that ranks types by their priority for disambiguating different
@@ -154,6 +154,9 @@ class EntityInstance final {
   EntityInstance& operator=(EntityInstance&&);
   ~EntityInstance();
 
+  // Transparent less-than relation based on the the GUID.
+  struct CompareByGuid;
+
   // Comparator that ranks instances by their priority for import on form
   // submission.
   // `ImportOrder(x, y) == true` means `x` has higher priority than `y`.
@@ -232,6 +235,22 @@ class EntityInstance final {
 
 std::ostream& operator<<(std::ostream& os, const AttributeInstance& a);
 std::ostream& operator<<(std::ostream& os, const EntityInstance& e);
+
+struct EntityInstance::CompareByGuid {
+  using is_transparent = void;
+
+  bool operator()(const EntityInstance& lhs, const base::Uuid& rhs) const {
+    return lhs.guid() < rhs;
+  }
+
+  bool operator()(const base::Uuid& lhs, const EntityInstance& rhs) const {
+    return lhs < rhs.guid();
+  }
+
+  bool operator()(const EntityInstance& lhs, const EntityInstance& rhs) const {
+    return lhs.guid() < rhs.guid();
+  }
+};
 
 }  // namespace autofill
 
