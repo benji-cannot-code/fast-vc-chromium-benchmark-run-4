d05f2afe65a6fec6dd62851529f1a77b4efc9858
James Cook <jamescook@chromium.org>
2025-02-19 14:57:44 -0800

    extensions: Extract ManifestCheckLevel enum into its own header
    This helps break the dependency between CrxInstaller and
    WebstoreInstaller. It's a step towards supporting CrxInstaller on
    desktop Android.
    
    Bug: 396722906
    Change-Id: I4f67f2935acafa4c89d93b219719135db6e492c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279587
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422221}

---
+++ chrome/browser/extensions/manifest_check_level.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_MANIFEST_CHECK_LEVEL_H_
+#define CHROME_BROWSER_EXTENSIONS_MANIFEST_CHECK_LEVEL_H_
+
+namespace extensions {
+
+// The amount of manifest checking to perform.
+enum class ManifestCheckLevel {
+  // Do not check for any manifest equality.
+  kNone,
+
+  // Only check that the expected and actual permissions have the same
+  // effective permissions.
+  kLoose,
+
+  // All data in the expected and actual manifests must match.
+  kStrict,
+};
+
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_MANIFEST_CHECK_LEVEL_H_
--- chrome/browser/extensions/webstore_installer.h
+++ chrome/browser/extensions/webstore_installer.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "base/version.h"
 #include "chrome/browser/extensions/extension_install_prompt.h"
+#include "chrome/browser/extensions/manifest_check_level.h"
 #include "components/download/public/common/download_interrupt_reasons.h"
 #include "components/download/public/common/download_item.h"
 #include "content/public/browser/browser_thread.h"
@@ -68,18 +69,6 @@ class WebstoreInstaller : public ExtensionRegistryObserver,
     FAILURE_REASON_DEPENDENCY_NOT_FOUND,
     FAILURE_REASON_DEPENDENCY_NOT_SHARED_MODULE,
     FAILURE_REASON_OTHER
-  };
-
-  enum ManifestCheckLevel {
-    // Do not check for any manifest equality.
-    MANIFEST_CHECK_LEVEL_NONE,
-
-    // Only check that the expected and actual permissions have the same
-    // effective permissions.
-    MANIFEST_CHECK_LEVEL_LOOSE,
-
-    // All data in the expected and actual manifests must match.
-    MANIFEST_CHECK_LEVEL_STRICT,
   };
 
   using SuccessCallback = base::OnceCallback<void(const std::string&)>;
@@ -133,7 +122,7 @@ class WebstoreInstaller : public ExtensionRegistryObserver,
 
     // Manifest check level for checking actual manifest against expected
     // manifest.
-    ManifestCheckLevel manifest_check_level = MANIFEST_CHECK_LEVEL_STRICT;
+    ManifestCheckLevel manifest_check_level = ManifestCheckLevel::kStrict;
 
     // Used to show the install dialog.
     ExtensionInstallPrompt::ShowDialogCallback show_dialog_callback;
