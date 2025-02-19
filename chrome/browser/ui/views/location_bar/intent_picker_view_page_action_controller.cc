a1b43051c2ae2573a0f02eec86f8822e8ada2232
Muhammad Salmaan <musalmaan@chromium.org>
2025-02-19 07:26:34 -0800

    Add check for tab_interface and Remove Const
    Added check for tab Interface to ensure its not a nullptr.
    Simplified ptr const to non ptr const for clarity.
    
    Bug: 396720194
    Change-Id: Id1159711a0c9de80ef8070cb01f1d7a95b6ccb3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6278925
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421941}

--- chrome/browser/ui/views/location_bar/intent_picker_view_page_action_controller.cc
+++ chrome/browser/ui/views/location_bar/intent_picker_view_page_action_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/location_bar/intent_picker_view_page_action_controller.h"
 
+#include "base/check_op.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser.h"
@@ -22,8 +23,10 @@ class WebContents;
 
 IntentPickerViewPageActionController::IntentPickerViewPageActionController(
     tabs::TabInterface* tab_interface)
-    : tab_interface_(*tab_interface) {}
-// TODO(396720194): Add a check to ensure the tab_interface ptr is not nullptr.
+    : tab_interface_(*tab_interface) {
+  CHECK_NE(tab_interface, nullptr);
+}
+
 void IntentPickerViewPageActionController::UpdatePageActionVisibility(
     bool should_show_icon) {
   if (!base::FeatureList::IsEnabled(features::kPageActionsMigration)) {
