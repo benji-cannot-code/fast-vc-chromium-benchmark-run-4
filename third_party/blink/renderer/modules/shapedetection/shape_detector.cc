d58a662ef19a0de20d63a0ab040245363542598b
Mike Wasserman <msw@chromium.org>
2025-02-19 13:01:21 -0800

    Expose static ShapeDetector::GetBitmapFromSource utility function
    Supports other Blink modules requiring equivalent functionality.
    
    Bug: 385173789
    Change-Id: I6af6e9a449244eed666183d04c51b025c5261a41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6282809
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422159}

--- third_party/blink/renderer/modules/shapedetection/shape_detector.cc
+++ third_party/blink/renderer/modules/shapedetection/shape_detector.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// static
 std::optional<SkBitmap> ShapeDetector::GetBitmapFromSource(
     ScriptState* script_state,
     const V8ImageBitmapSource* image_source,
@@ -124,6 +125,7 @@ std::optional<SkBitmap> ShapeDetector::GetBitmapFromSource(
   return std::move(n32_bitmap);
 }
 
+// static
 std::optional<SkBitmap> ShapeDetector::GetBitmapFromImageData(
     ScriptState* script_state,
     ImageData* image_data,
@@ -159,6 +161,7 @@ std::optional<SkBitmap> ShapeDetector::GetBitmapFromImageData(
   return std::move(sk_bitmap);
 }
 
+// static
 std::optional<SkBitmap> ShapeDetector::GetBitmapFromImageElement(
     ScriptState* script_state,
     const HTMLImageElement* img,
