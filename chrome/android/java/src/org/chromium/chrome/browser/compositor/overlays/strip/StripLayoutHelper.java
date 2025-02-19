1eedef4829e4226fc02edce06b2682e2b234ba8c
Neil Coronado <nemco@google.com>
2025-02-19 11:23:46 -0800

    [TabGroupDragDrop] Hide/show the group when dragged off/on strip
    Hide/show the group when dragged off/on strip.
    -Currently, this does not animate the views entering/leaving the source
     tab strip.
    
    Bug: 384969886
    Change-Id: I4a933c953341e0d5c6284f8241dfbed6b9f785e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6266508
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422102}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -3675,7 +3675,7 @@ public class StripLayoutHelper
                 }
 
                 view.setIdealX(startX + drawXOffset);
-                delta = view.getWidth() - mGroupTitleOverlapWidth;
+                delta = (view.getWidth() - mGroupTitleOverlapWidth) * view.getWidthWeight();
             }
 
             delta = MathUtils.flipSignIf(delta, LocalizationUtils.isLayoutRtl());
@@ -3683,10 +3683,14 @@ public class StripLayoutHelper
         }
     }
 
+    private boolean shouldRenderView(StripLayoutView view) {
+        return view.isVisible() && !view.isDraggedOffStrip();
+    }
+
     private int getVisibleViewCount(StripLayoutView[] views) {
         int renderCount = 0;
         for (int i = 0; i < views.length; ++i) {
-            if (views[i].isVisible()) renderCount++;
+            if (shouldRenderView(views[i])) renderCount++;
         }
         return renderCount;
     }
@@ -3695,7 +3699,7 @@ public class StripLayoutHelper
         int renderIndex = 0;
         for (int i = 0; i < allViews.length; ++i) {
             final StripLayoutView view = allViews[i];
-            if (view.isVisible()) viewsToRender[renderIndex++] = view;
+            if (shouldRenderView(view)) viewsToRender[renderIndex++] = view;
         }
     }
 
