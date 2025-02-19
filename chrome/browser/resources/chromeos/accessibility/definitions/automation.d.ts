1a60ab5ccfb742dac161e56a5e7dca6b994ccbec
Alexander Surkov <asurkov@igalia.com>
2025-02-18 17:34:10 -0800

    [ChromeVox] convert desktop automation handler to TypeScript
    AX-Relnotes: n/a.
    Bug: 267329383
    Change-Id: I2fa8737b74cdf18b8324d5ff25fe6528364f1d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6246894
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1421736}

--- chrome/browser/resources/chromeos/accessibility/definitions/automation.d.ts
+++ chrome/browser/resources/chromeos/accessibility/definitions/automation.d.ts
@@ -633,7 +633,7 @@ declare namespace chrome {
     export interface AutomationIntent {
       command: IntentCommandType;
       textBoundary: IntentTextBoundaryType;
-      moveDirection: IntentMoveDirectionType;
+      moveDirection?: IntentMoveDirectionType;
     }
 
     export interface AutomationEvent {
