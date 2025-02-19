2af5ade59b3b6941cfce6b0cd564a3ff06f5e2ba
Sneha Agarwal <snehagarwal@microsoft.com>
2025-02-19 11:22:01 -0800

    IDB WPTs: Extend three IDB WPTs to run on workers.
    This set of IndexedDB WPTs currently only run in a window environment.
    This change extends them to also run in dedicated, shared, and service
    worker environments.
    
    Bug: 41455766
    Change-Id: I57a1781edc007b0134489725d713e6ebabddd60f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6251264
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Sneha Agarwal <snehagarwal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1422100}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbindex_indexNames.any.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: title=IDBObjectStore.indexNames
+// META: script=resources/support.js
+
+'use_strict';
+
+async_test(t => {
+  let db;
+  const open_rq = createdb(t);
+  open_rq.onupgradeneeded = t.step_func(e => {
+    db = e.target.result;
+    const objStore = db.createObjectStore('test', {keyPath: 'key'});
+    objStore.createIndex('index', 'data');
+
+    assert_equals(objStore.indexNames[0], 'index', 'indexNames');
+    assert_equals(objStore.indexNames.length, 1, 'indexNames.length');
+  });
+
+  open_rq.onsuccess = t.step_func(e => {
+    const objStore = db.transaction('test', 'readonly').objectStore('test');
+
+    assert_equals(objStore.indexNames[0], 'index', 'indexNames (second)');
+    assert_equals(objStore.indexNames.length, 1, 'indexNames.length (second)');
+
+    t.done();
+  });
+}, 'Verify IDBObjectStore.indexNames property');
