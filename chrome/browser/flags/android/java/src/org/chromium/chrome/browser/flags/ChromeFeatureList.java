46ce79cc52a738bf34de2db2801d7982c1184e72
Xi Han <hanxi@google.com>
2025-02-19 08:48:41 -0800

    [AppIntegration] Add a new flag for sharing multiple data sources.
    Screenshot in chrome://flags:
    https://drive.google.com/file/d/1cN3EMhjM35z5aOTcXgfPxVDAx3LoZst7/view?usp=drive_link
    
    Bug: 397457989
    Change-Id: I51b0b699d1de1e0bbf8b287178f8f192cd61c3d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6278045
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421988}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -169,6 +169,8 @@ public abstract class ChromeFeatureList {
     public static final String NEW_TAB_PAGE_CUSTOMIZATION = "NewTabPageCustomization";
     public static final String ANDROID_APP_INTEGRATION_WITH_FAVICON =
             "AndroidAppIntegrationWithFavicon";
+    public static final String ANDROID_APP_INTEGRATION_MULTI_DATA_SOURCE =
+            "AndroidAppIntegrationMultiDataSource";
     public static final String ANDROID_BOOKMARK_BAR = "AndroidBookmarkBar";
     public static final String ANDROID_BOTTOM_TOOLBAR = "AndroidBottomToolbar";
     public static final String ANDROID_DUMP_ON_SCROLL_WITHOUT_RESOURCE =
@@ -615,6 +617,9 @@ public abstract class ChromeFeatureList {
             newCachedFlag(NEW_TAB_PAGE_CUSTOMIZATION, false);
     public static final CachedFlag sAndroidAppIntegrationWithFavicon =
             newCachedFlag(ANDROID_APP_INTEGRATION_WITH_FAVICON, false, true);
+
+    public static final CachedFlag sAndroidAppIntegrationMultiDataSource =
+            newCachedFlag(ANDROID_APP_INTEGRATION_MULTI_DATA_SOURCE, false);
     public static final CachedFlag sAndroidBottomToolbar =
             newCachedFlag(ANDROID_BOTTOM_TOOLBAR, false, true);
     public static final CachedFlag sAndroidElegantTextHeight =
@@ -855,6 +860,7 @@ public abstract class ChromeFeatureList {
                     sAndroidAppIntegrationModule,
                     sAndroidAppIntegrationV2,
                     sAndroidAppIntegrationWithFavicon,
+                    sAndroidAppIntegrationMultiDataSource,
                     sAndroidTabSkipSaveTabsKillswitch,
                     sAndroidBottomToolbar,
                     sAndroidElegantTextHeight,
