ecd18af6455379894ddd16e2125f3d942f241d23
Thiabaud Engelbrecht <thiabaud@google.com>
2025-02-19 08:07:35 -0800

    [self_freeze] Fix start time
    This CL changes the start time to be from directly after the SelfCompact is triggered, rather than from when we actually begin the compaction. This fixes a bug where cancellation was not triggered correctly if it happened during that time.
    
    Bug: 379290553
    Change-Id: I044104b7a2bd84c379c51eeceeeb1710ae14659e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279726
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421965}

--- base/android/pre_freeze_background_memory_trimmer.h
+++ base/android/pre_freeze_background_memory_trimmer.h
@@ -117,7 +117,8 @@ class BASE_EXPORT PreFreezeBackgroundMemoryTrimmer {
   static bool SelfCompactionIsSupported();
 
   // Compacts the memory for the process.
-  void CompactSelf();
+  void CompactSelf(scoped_refptr<SequencedTaskRunner> task_runner,
+                   base::TimeTicks started_at);
 
   // If we are currently running self compaction, cancel it.
   static void MaybeCancelSelfCompaction();
@@ -156,6 +157,7 @@ class BASE_EXPORT PreFreezeBackgroundMemoryTrimmer {
   friend class PreFreezeSelfCompactionTest;
   FRIEND_TEST_ALL_PREFIXES(PreFreezeSelfCompactionTest, Cancel);
   FRIEND_TEST_ALL_PREFIXES(PreFreezeSelfCompactionTest, NotCanceled);
+  FRIEND_TEST_ALL_PREFIXES(PreFreezeSelfCompactionTest, OnSelfFreezeCancel);
 
   // We use our own implementation here, based on |PostCancelableDelayedTask|,
   // rather than relying on something like |base::OneShotTimer|, since
@@ -285,7 +287,7 @@ class BASE_EXPORT PreFreezeBackgroundMemoryTrimmer {
   void OnPreFreezeInternal() LOCKS_EXCLUDED(lock());
   void RunPreFreezeTasks() EXCLUSIVE_LOCKS_REQUIRED(lock());
 
-  void OnSelfFreezeInternal();
+  void OnSelfFreezeInternal(scoped_refptr<SequencedTaskRunner> task_runner);
 
   void MaybeCancelSelfCompactionInternal() LOCKS_EXCLUDED(lock());
 
