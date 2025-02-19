26fb823c4311be349a980c6652194006f5debe0d
Ho Cheung <hocheung@chromium.org>
2025-02-19 01:09:59 -0800

    [Cast MRP] Remove the kCastSilentlyRemoveVcOnNavigation feature flag
    kCastSilentlyRemoveVcOnNavigation has been enabled
    by default many versions ago and no regressions
    were found, so now we can remove it.
    
    Fixed: 315057815
    Change-Id: Id99464b62fe9a922af74573cb38e6a0a9ae2a75a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279947
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421820}

--- chrome/browser/media/router/providers/cast/cast_activity.cc
+++ chrome/browser/media/router/providers/cast/cast_activity.cc
@@ -6,9 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/router/providers/cast/cast_activity.h"
 
 #include "base/containers/contains.h"
-#include "base/feature_list.h"
 #include "base/logging.h"
-#include "chrome/browser/media/router/media_router_feature.h"
 #include "chrome/browser/media/router/providers/cast/cast_internal_message_util.h"
 #include "chrome/browser/media/router/providers/cast/cast_session_client_impl.h"
 #include "components/media_router/common/discovery/media_sink_internal.h"
@@ -178,8 +176,7 @@ void CastActivity::CloseConnectionOnReceiver(
   if (!session) {
     return;
   }
-  if (reason == blink::mojom::PresentationConnectionCloseReason::CLOSED ||
-      !base::FeatureList::IsEnabled(kCastSilentlyRemoveVcOnNavigation)) {
+  if (reason == blink::mojom::PresentationConnectionCloseReason::CLOSED) {
     message_handler_->CloseConnection(cast_channel_id(), client_id,
                                       session->destination_id());
 
