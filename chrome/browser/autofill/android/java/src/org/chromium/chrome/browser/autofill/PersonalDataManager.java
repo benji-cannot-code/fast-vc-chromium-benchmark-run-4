10bc7bc130ab3b83e745c3f8831ca7760da6ca68
Piotr Kotynia <piotrkotynia@google.com>
2025-02-19 06:19:21 -0800

    Add Robolectric tests for AutofillLocalCardEditor
    Migrate JUnit tests for AutofillLocalCardEditor to Robolectric, enabling
    faster execution and device-independent testing by mocking JNI calls.
    This is a staged migration to simplify review.
    
    Bug: 391100396
    Change-Id: I059008c4fdcc32bdfbcd46bed261241b8acceabb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249782
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1421916}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.text.TextUtils;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
@@ -1208,7 +1209,8 @@ public class PersonalDataManager implements Destroyable {
     }
 
     @NativeMethods
-    interface Natives {
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public interface Natives {
         long init(PersonalDataManager caller, @JniType("Profile*") Profile profile);
 
         void destroy(long nativePersonalDataManagerAndroid);
