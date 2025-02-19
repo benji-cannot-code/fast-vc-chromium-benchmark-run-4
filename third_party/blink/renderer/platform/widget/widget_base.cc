bbb2f6eea8d6a669d57a47aa8c2cc9dd7dab35c9
Richard Chen <richche@chromium.org>
2025-02-19 09:50:12 -0800

    [Code Health] Clean up stale RunTextInputUpdatePostLifecycle
    Bug: 356625208
    Change-Id: I6a4dc67df286a20beb98f5059c686521df8fe2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216845
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422041}

--- third_party/blink/renderer/platform/widget/widget_base.cc
+++ third_party/blink/renderer/platform/widget/widget_base.cc
@@ -671,8 +671,7 @@ void WidgetBase::OnCommitRequested() {
 }
 
 void WidgetBase::DidBeginMainFrame() {
-  if (base::FeatureList::IsEnabled(features::kRunTextInputUpdatePostLifecycle))
-    UpdateTextInputState();
+  UpdateTextInputState();
   client_->DidBeginMainFrame();
 }
 
@@ -981,10 +980,6 @@ void WidgetBase::WillBeginMainFrame() {
   client_->SetSuppressFrameRequestsWorkaroundFor704763Only(true);
   client_->WillBeginMainFrame();
   UpdateSelectionBounds();
-  // UpdateTextInputState() will cause a forced style and layout update, which
-  // we would like to eliminate.
-  if (!base::FeatureList::IsEnabled(features::kRunTextInputUpdatePostLifecycle))
-    UpdateTextInputState();
 }
 
 void WidgetBase::RunPaintBenchmark(int repeat_count,
