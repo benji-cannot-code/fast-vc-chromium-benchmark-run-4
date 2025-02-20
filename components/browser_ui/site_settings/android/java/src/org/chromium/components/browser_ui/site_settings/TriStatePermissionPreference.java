9243e2a14ed09e8d1a3c1b721b520010fe001e88
Yifan Luo <lyf@google.com>
2025-02-20 01:03:38 -0800

    [Site Setting] When CPSS is disabled, ensure title doesn't take up any space.
    A previous cl: https://chromium-review.googlesource.com/c/chromium/src/+/6054697 introduced title
    "How to request" to CPSS settings behind a flag. However, the UI was
    not correctly settled when the feature is disabled. Extra space is
    added because of the new TextView added for the title mentioned above.
    
    Bug: 41494473
    Change-Id: I621b5a3eeb34bef4aa44022f67c89f348af19381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276543
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422403}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/TriStatePermissionPreference.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/TriStatePermissionPreference.java
@@ -91,7 +91,7 @@ public class TriStatePermissionPreference extends Preference
         if (mShowTitle) {
             mTitleView.setVisibility(View.VISIBLE);
         } else {
-            mTitleView.setVisibility(View.INVISIBLE);
+            mTitleView.setVisibility(View.GONE);
         }
         mRadioGroup = (RadioGroup) holder.findViewById(R.id.radio_button_layout);
         assumeNonNull(mRadioGroup);
