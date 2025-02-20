7ca2f84d90e5aa0033e5e11c3c11ab74ec5a4dae
Ian Wells <iwells@chromium.org>
2025-02-19 17:37:09 -0800

    Add base::FeatureParams for glic widget initial size
    Also update the default initial size to 350x48.
    
    This change only updates Chrome's initial size. The client needs to request the same width when resizing.
    
    Bug: 397704384, 397515077
    Change-Id: Ic80b8127b41865266f23e67e9aeafa6999176e90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6283065
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Sana Akbani <sanaakbani@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422302}

--- chrome/browser/glic/glic_window_controller.cc
+++ chrome/browser/glic/glic_window_controller.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/glic_button.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_action_container.h"
 #include "chrome/browser/ui/views/tabs/window_finder.h"
+#include "chrome/common/chrome_features.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/display/screen.h"
 #include "ui/events/event_observer.h"
@@ -61,8 +62,6 @@ namespace {
 constexpr static int kAttachmentBuffer = 20;
 constexpr static int kDetachYDistance = 36;
 
-constexpr static int kWidgetDefaultWidth = 300;
-constexpr static int kWidgetTopBarHeight = 48;
 constexpr static int kAnimationDurationMs = 300;
 
 constexpr char kHistogramGlicPanelPresentationTime[] =
@@ -87,6 +86,11 @@ GlicButton* GetGlicButton(const Browser& browser) {
       ->AsBrowserView()
       ->tab_strip_region_view()
       ->GetGlicButton();
+}
+
+gfx::Size GetWidgetInitialSize() {
+  return {features::kGlicInitialWidth.Get(),
+          features::kGlicInitialHeight.Get()};
 }
 
 }  // namespace
@@ -489,7 +493,7 @@ void GlicWindowController::AuthCheckDoneBeforeShow(
 }
 
 gfx::Rect GlicWindowController::GetInitialDetachedBounds() {
-  gfx::Size widget_size(kWidgetDefaultWidth, kWidgetTopBarHeight);
+  gfx::Size widget_size = GetWidgetInitialSize();
   if (glic_size_) {
     widget_size = *glic_size_;
   }
@@ -522,7 +526,7 @@ void GlicWindowController::OpenAttached(Browser& browser) {
   AttachToBrowser(browser);
 
   // Set target size for animation and run the open attached animation.
-  gfx::Size widget_size(kWidgetDefaultWidth, kWidgetTopBarHeight);
+  gfx::Size widget_size = GetWidgetInitialSize();
   if (glic_size_) {
     widget_size = *glic_size_;
   }
@@ -624,7 +628,7 @@ void GlicWindowController::ShowFinish() {
 
   // Set the draggable area to the top bar of the window, by default.
   GetGlicView()->SetDraggableAreas(
-      {{0, 0, GetGlicView()->width(), kWidgetTopBarHeight}});
+      {{0, 0, GetGlicView()->width(), GetWidgetInitialSize().height()}});
   NotifyIfPanelStateChanged();
 }
 
@@ -1002,7 +1006,8 @@ Browser* GlicWindowController::FindBrowserForAttachment() {
     // Define attachment zone as the right of the tab strip. It either is the
     // width of the widget or 1/3 of the tab strip, whichever is smaller.
     gfx::Rect attachment_zone = tab_strip_region_view->GetBoundsInScreen();
-    int width = std::min(attachment_zone.width() / 3, kWidgetDefaultWidth);
+    int width =
+        std::min(attachment_zone.width() / 3, GetWidgetInitialSize().width());
     attachment_zone.SetByBounds(attachment_zone.right() - width,
                                 attachment_zone.y() - kAttachmentBuffer,
                                 attachment_zone.right() + kAttachmentBuffer,
