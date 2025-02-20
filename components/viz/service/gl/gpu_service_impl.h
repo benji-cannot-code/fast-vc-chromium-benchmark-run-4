00f35fd7a5ff4c2b233414002405295848c18e7b
Saifuddin Hitawala <hitawala@chromium.org>
2025-02-20 09:23:32 -0800

    [gpu] Remove unnecessary media_buildflags include
    gpu_preferences.h includes media_buildflags.h which is not really
    needed. This change removes the include and includes it in
    GpuServiceImpl where it is really needed.
    
    This helps fix a build failure due to transitive dependency on
    media_buildflags.h from gpu_info.h via gpu_preferences.h
    
    Bug: 397776075
    Change-Id: I86abb6f779b416e89fa79b84c319e40cf8bc18c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6286526
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422628}

--- components/viz/service/gl/gpu_service_impl.h
+++ components/viz/service/gl/gpu_service_impl.h
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/service/gpu_config.h"
 #include "gpu/ipc/service/x_util.h"
 #include "gpu/vulkan/buildflags.h"
+#include "media/media_buildflags.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
--- gpu/config/gpu_preferences.h
+++ gpu/config/gpu_preferences.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "gpu/gpu_export.h"
-#include "media/media_buildflags.h"
 #include "ui/gfx/buffer_types.h"
 
 #if BUILDFLAG(IS_OZONE)
