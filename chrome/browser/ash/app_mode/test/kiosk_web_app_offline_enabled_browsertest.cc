c6d06e73232d0d54631ce3c980a8ae5740ff41da
Sergii Bykov <sbykov@google.com>
2025-02-20 08:49:10 -0800

    [kiosk] Implement KioskWebAppOfflineEnabled policy for IWA
    Adds logic to KioskIwaLauncher to check kiosk_web_app_offline_enabled.
    Setting this policy to false will force a network config dialog before
    the app can launch. Consistent with web/pwa kiosk.
    
    Bug: 372848695
    Change-Id: I81c13cc85f47451abb872fa561c131fdc8caf033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276332
    Reviewed-by: Ashutosh Singhal <macinashutosh@google.com>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422603}

--- chrome/browser/ash/app_mode/test/kiosk_web_app_offline_enabled_browsertest.cc
+++ chrome/browser/ash/app_mode/test/kiosk_web_app_offline_enabled_browsertest.cc
@@ -15,12 +15,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/js_checker.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_server_mixin.h"
+#include "chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_builder.h"
+#include "chrome/browser/web_applications/isolated_web_apps/test/test_signed_web_bundle_builder.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_types.h"
 #include "components/policy/policy_constants.h"
+#include "components/web_package/signed_web_bundles/signed_web_bundle_id.h"
+#include "components/web_package/test_support/signed_web_bundles/key_pair.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/app_window/app_window.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -29,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 using kiosk::test::BlockKioskLaunch;
-using kiosk::test::TheKioskWebApp;
+using kiosk::test::TheKioskApp;
 using kiosk::test::WaitNetworkScreen;
 
 namespace {
@@ -37,6 +42,23 @@ namespace {
 // Workflow: COM_KIOSK_CUJ3_TASK8_WF1.
 constexpr char kLaunchKioskOfflineTag[] =
     "screenplay-35e430a3-04b3-46a7-aa0a-207a368b8cba";
+
+const web_package::SignedWebBundleId kTestWebBundleId =
+    web_app::test::GetDefaultEd25519WebBundleId();
+const web_package::test::KeyPair kTestKeyPair =
+    web_app::test::GetDefaultEd25519KeyPair();
+
+// Possible values for `KioskWebAppOfflineEnabledTest`.
+enum class TestAppType { kKioskWeb, kKioskIwa };
+
+std::string TestAppName(const testing::TestParamInfo<TestAppType>& info) {
+  switch (info.param) {
+    case TestAppType::kKioskWeb:
+      return "KioskWeb";
+    case TestAppType::kKioskIwa:
+      return "KioskIwa";
+  }
+}
 
 void ExpectNetworkScreenContinueButtonNotShown() {
   const test::UIPath kNetworkConfigureScreenContinueButton = {"error-message",
@@ -48,11 +70,18 @@ void ExpectNetworkScreenContinueButtonNotShown() {
 
 }  // namespace
 
-// Verifies the `KioskWebAppOfflineEnabled` policy works correctly.
+// Verifies that `KioskWebAppOfflineEnabled` policy works correctly for
+// supported app types.
 template <bool policy_value>
-class KioskWebAppOfflineEnabledTest : public MixinBasedInProcessBrowserTest {
+class KioskWebAppOfflineEnabledTest
+    : public MixinBasedInProcessBrowserTest,
+      public testing::WithParamInterface<TestAppType> {
  public:
-  KioskWebAppOfflineEnabledTest() = default;
+  KioskWebAppOfflineEnabledTest() {
+    iwa_server_mixin_.AddBundle(
+        web_app::IsolatedWebAppBuilder(web_app::ManifestBuilder())
+            .BuildBundle(kTestKeyPair));
+  }
 
   KioskWebAppOfflineEnabledTest(const KioskWebAppOfflineEnabledTest&) = delete;
   KioskWebAppOfflineEnabledTest& operator=(
@@ -75,36 +104,47 @@ class KioskWebAppOfflineEnabledTest : public MixinBasedInProcessBrowserTest {
     provider_.UpdateChromePolicy(values);
   }
 
+ protected:
+  KioskMixin::Config GetConfig() {
+    switch (GetParam()) {
+      case TestAppType::kKioskWeb:
+        return {/*name=*/"WebApp",
+                /*auto_launch_account_id=*/{},
+                {KioskMixin::SimpleWebAppOption()}};
+
+      case TestAppType::kKioskIwa:
+        return {/*name=*/"IsolatedWebApp",
+                /*auto_launch_account_id=*/{},
+                {KioskMixin::IsolatedWebAppOption(
+                    /*account_id=*/"simple-iwa@localhost",
+                    /*web_bundle_id=*/kTestWebBundleId,
+                    /*update_manifest_url=*/
+                    iwa_server_mixin_.GetUpdateManifestUrl(kTestWebBundleId))}};
+    }
+  }
+
   testing::NiceMock<policy::MockConfigurationPolicyProvider> provider_;
-
+  web_app::IsolatedWebAppUpdateServerMixin iwa_server_mixin_{&mixin_host_};
   NetworkStateMixin network_state_{&mixin_host_};
-
-  KioskMixin kiosk_{&mixin_host_,
-                    /*cached_configuration=*/KioskMixin::Config{
-                        /*name=*/{},
-                        /*auto_launch_account_id=*/{},
-                        {KioskMixin::SimpleWebAppOption()}}};
+  KioskMixin kiosk_{&mixin_host_, /*cached_configuration=*/GetConfig()};
 };
 
 using KioskWebAppOfflineDisabledByPolicyTest =
     KioskWebAppOfflineEnabledTest<false>;
 
-using KioskWebAppOfflineEnabledByPolicyTest =
-    KioskWebAppOfflineEnabledTest<true>;
-
-IN_PROC_BROWSER_TEST_F(KioskWebAppOfflineDisabledByPolicyTest,
+IN_PROC_BROWSER_TEST_P(KioskWebAppOfflineDisabledByPolicyTest,
                        PRE_CannotLaunchOffline) {
   network_state_.SimulateOnline();
-  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskWebApp()));
+  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskApp()));
   ASSERT_TRUE(kiosk_.WaitSessionLaunched());
 }
 
-IN_PROC_BROWSER_TEST_F(KioskWebAppOfflineDisabledByPolicyTest,
+IN_PROC_BROWSER_TEST_P(KioskWebAppOfflineDisabledByPolicyTest,
                        CannotLaunchOffline) {
   base::AddFeatureIdTagToTestResult(kLaunchKioskOfflineTag);
 
   network_state_.SimulateOffline();
-  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskWebApp()));
+  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskApp()));
 
   auto scoped_launch_blocker = BlockKioskLaunch();
   WaitNetworkScreen();
@@ -115,19 +155,34 @@ IN_PROC_BROWSER_TEST_F(KioskWebAppOfflineDisabledByPolicyTest,
   ASSERT_TRUE(kiosk_.WaitSessionLaunched());
 }
 
-IN_PROC_BROWSER_TEST_F(KioskWebAppOfflineEnabledByPolicyTest,
+INSTANTIATE_TEST_SUITE_P(All,
+                         KioskWebAppOfflineDisabledByPolicyTest,
+                         testing::Values(TestAppType::kKioskWeb,
+                                         TestAppType::kKioskIwa),
+                         TestAppName);
+
+using KioskWebAppOfflineEnabledByPolicyTest =
+    KioskWebAppOfflineEnabledTest<true>;
+
+IN_PROC_BROWSER_TEST_P(KioskWebAppOfflineEnabledByPolicyTest,
                        PRE_LaunchesOffline) {
   network_state_.SimulateOnline();
-  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskWebApp()));
+  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskApp()));
   ASSERT_TRUE(kiosk_.WaitSessionLaunched());
 }
 
-IN_PROC_BROWSER_TEST_F(KioskWebAppOfflineEnabledByPolicyTest, LaunchesOffline) {
+IN_PROC_BROWSER_TEST_P(KioskWebAppOfflineEnabledByPolicyTest, LaunchesOffline) {
   base::AddFeatureIdTagToTestResult(kLaunchKioskOfflineTag);
 
   network_state_.SimulateOffline();
-  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskWebApp()));
+  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskApp()));
   ASSERT_TRUE(kiosk_.WaitSessionLaunched());
 }
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         KioskWebAppOfflineEnabledByPolicyTest,
+                         testing::Values(TestAppType::kKioskWeb,
+                                         TestAppType::kKioskIwa),
+                         TestAppName);
 
 }  // namespace ash
