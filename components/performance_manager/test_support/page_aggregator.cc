335b65b7520d4018f49f7ce13ae92c6a51fa98ca
Patrick Monette <pmonette@chromium.org>
2025-02-20 08:07:02 -0800

    [PM] Migrate uses of CallOnGraph/RunInGraph
    The performance manager now lives on the UI thread, making
    these functions obsolete.
    
    The graph can be directly accessed through the
    PerformanceManager::GetGraph() function.
    
    This CL was uploaded by git cl split.
    
    R=joenotcharles@google.com
    
    Bug: 40755583
    Change-Id: I1f6a798881b78f42e9b9bfe9420132dcba6cc59e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6284306
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422584}

--- components/performance_manager/test_support/page_aggregator.cc
+++ components/performance_manager/test_support/page_aggregator.cc
@@ -5,27 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/performance_manager/test_support/page_aggregator.h"
 
-#include "base/run_loop.h"
-#include "base/test/bind.h"
 #include "components/performance_manager/decorators/page_aggregator.h"
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/performance_manager.h"
-#include "testing/gtest/include/gtest/gtest.h"
 
 namespace performance_manager {
 namespace testing {
 
 void CreatePageAggregatorAndPassItToGraph() {
-  base::RunLoop run_loop;
-  auto quit_closure = run_loop.QuitClosure();
-
-  PerformanceManager::CallOnGraph(
-      FROM_HERE, base::BindLambdaForTesting([&](Graph* graph) {
-        graph->PassToGraph(
-            std::make_unique<performance_manager::PageAggregator>());
-        std::move(quit_closure).Run();
-      }));
-  run_loop.Run();
+  Graph* graph = PerformanceManager::GetGraph();
+  graph->PassToGraph(std::make_unique<PageAggregator>());
 }
 
 }  // namespace testing
--- components/performance_manager/test_support/page_aggregator.h
+++ components/performance_manager/test_support/page_aggregator.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PERFORMANCE_MANAGER_TEST_SUPPORT_PAGE_AGGREGATOR_H_
 #define COMPONENTS_PERFORMANCE_MANAGER_TEST_SUPPORT_PAGE_AGGREGATOR_H_
 
-#include "components/performance_manager/public/decorators/page_live_state_decorator.h"
-
 namespace performance_manager {
 namespace testing {
 
