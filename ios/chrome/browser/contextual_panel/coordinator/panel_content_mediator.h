147d93c84e2787db0542ec582c0e54be2ca75c3d
“Prasanaa” <prasanaa@google.com>
2025-02-20 08:16:20 -0800

    [iOS] Refactor ToolbarUI from Broadcasting to Observer Pattern
    This CL refactors the ToolbarUI from a broadcasting to an observer
    pattern. `FullscreenModel` and `PanelContentMediator` register
    themselves as observer of `ToolbarUIState` through
    `ToolbarUIObserverInterface`.
    
    Moreover, this CL fixes the bug `396052192`, by ensuring that
    `_toolbarUIState` exists before calling `[self.consumer
    updateBottomToolbarHeight ... ]`.
    
    Video for bug contextual panel :
    https://drive.google.com/file/d/1JA1W5-oIS0V2QxIebKxCmEwzOXHY5SvE/view?usp=sharing&resourcekey=0-uDV2DQzQjBMVWR3vNQplhw
    
    Fixed: 396052192
    Change-Id: I7289b9131ffa9863d8ccb845d64f344c352faba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6225725
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422585}

--- ios/chrome/browser/contextual_panel/coordinator/panel_content_mediator.h
+++ ios/chrome/browser/contextual_panel/coordinator/panel_content_mediator.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+@class ToolbarUIState;
 @class ChromeBroadcaster;
 @protocol PanelContentConsumer;
 
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface PanelContentMediator : NSObject
 
 - (instancetype)initWithBroadcaster:(ChromeBroadcaster*)broadcaster
+                     toolbarUIState:(ToolbarUIState*)toolbarUIState
     NS_DESIGNATED_INITIALIZER;
 - (instancetype)init NS_UNAVAILABLE;
 
