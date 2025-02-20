2f7eaecd3826895bcf1417acdb781ec4061c5ae2
Rouslan Solomakhin <rouslan@chromium.org>
2025-02-20 14:17:02 -0800

    Move the MockPackageManagerDelegate to //components/payments.
    Before this patch, the MockPackageManagerDelegate.java was located in
    //chrome, although it does not have dependencies on //chrome. It is used
    by PaymentRequest integration tests to simulate different Package
    Manager responses. This functionality is also useful for testing
    PaymentRequest in //android_webview, which cannot depend on //chrome,
    but does depend on //components/payments.
    
    This package moves MockPackageManagerDelegate.java should be moved to
    //components/payments.
    
    After this patch, tests in both //chrome and //android_webview can use
    the MockPackageManagerDelegate.java for testing how PaymentRequest API
    queries and invokes payment apps over Android intents.
    
    Bug: 397996885
    Change-Id: I4d1201cd464e53d7e2750bbf1784efb7a7e36fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287688
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422803}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentDetailsUpdateServiceHelperTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentDetailsUpdateServiceHelperTest.java
@@ -35,6 +35,7 @@ import org.chromium.components.payments.Address;
 import org.chromium.components.payments.ErrorStrings;
 import org.chromium.components.payments.IPaymentDetailsUpdateService;
 import org.chromium.components.payments.IPaymentDetailsUpdateServiceCallback;
+import org.chromium.components.payments.MockPackageManagerDelegate;
 import org.chromium.components.payments.PaymentDetailsUpdateService;
 import org.chromium.components.payments.PaymentDetailsUpdateServiceHelper;
 import org.chromium.components.payments.PaymentRequestUpdateEventListener;
