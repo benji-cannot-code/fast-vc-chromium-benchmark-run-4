f4bdfc1b5e7f52256a91ad8a3a5e4a6a720df45c
Will Yeager <wyeager@google.com>
2025-02-20 09:11:22 -0800

    [ios] Log the same memory metric to UMA as is recorded by perf tests
    Bug: 396671210
    Change-Id: I625248aeaf9a1d4e26572df326c8911902437800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6269639
    Commit-Queue: Will Yeager <wyeager@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422616}

--- ios/chrome/app/app_metrics_app_state_agent_unittest.mm
+++ ios/chrome/app/app_metrics_app_state_agent_unittest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/app/app_metrics_app_state_agent.h"
 
+#import "base/test/task_environment.h"
 #import "ios/chrome/app/application_delegate/app_init_stage_test_utils.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/metrics_mediator.h"
@@ -65,6 +66,7 @@ class AppMetricsAppStateAgentTest : public PlatformTest {
 // Tests that -logStartupDuration: and -createStartupTrackingTask are called
 // once and in the right order for a regular startup (no safe mode).
 TEST_F(AppMetricsAppStateAgentTest, LogStartupDuration) {
+  base::test::SingleThreadTaskEnvironment task_environment;
   id metricsMediator = [OCMockObject mockForClass:[MetricsMediator class]];
 
   [[metricsMediator expect] createStartupTrackingTask];
