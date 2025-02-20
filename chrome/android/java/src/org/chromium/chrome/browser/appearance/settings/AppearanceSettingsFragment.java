51786b99aff625d2e26ac6cb4ca735f2211803bd
dmblack <dmblack@google.com>
2025-02-20 15:44:07 -0800

    Add empty preference screen for "Appearance" settings.
    This CL adds a new empty preference screen for "Appearance" settings.
    The screen will be populated in a later CL and its presence is gated by
    the AndroidAppearanceScreen feature flag. When the "Appearance" menu
    option is visible in main settings, "Theme" and "Toolbar shortcut" are
    removed as they will be moved to the new screen in later CLs.
    
    Design: http://shortn/_oxzSbwNuTe
    
    Bug: 397775252
    Change-Id: I90df378241173d06489322fd901e2237f1c040ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6288038
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422853}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/appearance/settings/AppearanceSettingsFragment.java
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.appearance.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.base.supplier.ObservableSupplier;
+import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
+import org.chromium.components.browser_ui.settings.CustomDividerFragment;
+import org.chromium.components.browser_ui.settings.SettingsUtils;
+
+/** Fragment to manage appearance settings. */
+public class AppearanceSettingsFragment extends ChromeBaseSettingsFragment
+        implements CustomDividerFragment {
+
+    private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        mPageTitle.set(getString(R.string.appearance_settings));
+        SettingsUtils.addPreferencesFromResource(this, R.xml.appearance_preferences);
+    }
+
+    @Override
+    public ObservableSupplier<String> getPageTitle() {
+        return mPageTitle;
+    }
+
+    // CustomDividerFragment implementation.
+
+    @Override
+    public boolean hasDivider() {
+        return false;
+    }
+}
