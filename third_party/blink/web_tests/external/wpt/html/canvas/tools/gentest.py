2a89c4e92c05bd23797ecc0ede54c0b3abf08210
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-02-20 09:32:30 -0800

    Remove the old canvas test generator
    All tests have been migrated to the new generator script.
    
    Bug: 40207206
    Change-Id: Ib50c7970d08b61bd580c7baf10b9b80e0789cf74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6204427
    Reviewed-by: Andres Ricardo Perez <andresrperez@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422633}

--- third_party/blink/web_tests/external/wpt/html/canvas/tools/gentest.py
+++ third_party/blink/web_tests/external/wpt/html/canvas/tools/gentest.py
@@ -10,11 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # -change to the directory with this script and run it
 #   python3 gentest.py
 
-from gentestutils import genTestUtils
 import gentestutilsunion
 
-genTestUtils('../element', '../element', 'templates.yaml',
-             'name2dir-canvas.yaml', False)
-genTestUtils('../offscreen', '../offscreen', 'templates.yaml',
-             'name2dir-offscreen.yaml', True)
 gentestutilsunion.generate_test_files('name2dir.yaml')
