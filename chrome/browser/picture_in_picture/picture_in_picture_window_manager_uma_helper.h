305246454392e5d4f69c123236565866d35345e7
Ivan Murashov <ivan.murashov@lge.com>
2025-02-20 10:37:19 -0800

    IWYU: Add missing include for std::optional usage in picture_in_picture_window_manager_uma_helper.h
    In the CL https://crrev.com/c/6191230 used std::optional in the
    chrome/browser/picture_in_picture/picture_in_picture_window_manager_uma_helper.h,
    but corresponding include was missed.
    Added missing include for std::optional usage.
    
    Bug: 41455655
    Change-Id: I70f67c4a06481875d4602b16a97532cdad2a63b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6275068
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422669}

--- chrome/browser/picture_in_picture/picture_in_picture_window_manager_uma_helper.h
+++ chrome/browser/picture_in_picture/picture_in_picture_window_manager_uma_helper.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PICTURE_IN_PICTURE_PICTURE_IN_PICTURE_WINDOW_MANAGER_UMA_HELPER_H_
 #define CHROME_BROWSER_PICTURE_IN_PICTURE_PICTURE_IN_PICTURE_WINDOW_MANAGER_UMA_HELPER_H_
 
+#include <optional>
+
 #include "base/memory/raw_ptr.h"
 #include "base/time/clock.h"
 #include "base/time/time.h"
