08d876db7aa6fd9062a0688e493d8baf7a6d9b6d
Charles Meng <charlesmeng@google.com>
2025-02-20 12:46:58 -0800

    Make glic settings open in a new tab when help bubble is throttled
    Use glic::OpenGlicSettingsPage instead of chrome::ShowSettingsSubPage
    when the help bubble is throttled so that settings will open in a new
    tab even if the current tab is already on the glic settings page.
    
    Also changes the tests to check that the functions in
    glic_settings_util.cc open a new settings tab in all scenarios.
    
    Fixed: 397465710
    Change-Id: Ie86293439ffa762d95c33e195f2d333e80670e42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6283785
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422744}

--- chrome/browser/glic/glic_settings_util.cc
+++ chrome/browser/glic/glic_settings_util.cc
@@ -18,6 +18,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_education/common/help_bubble/help_bubble_params.h"
 #include "ui/base/l10n/l10n_util.h"
 
+namespace glic {
+
+void OpenGlicSettingsPage(Profile* profile) {
+  NavigateParams params(profile,
+                        chrome::GetSettingsUrl(chrome::kChromeUIGlicHost),
+                        ui::PAGE_TRANSITION_AUTO_TOPLEVEL);
+  params.disposition = WindowOpenDisposition::NEW_FOREGROUND_TAB;
+  Navigate(&params);
+}
+
+}  // namespace glic
+
 namespace {
 
 void OpenGlicSettingsPageWithPromo(Profile* profile,
@@ -37,21 +49,13 @@ void OpenGlicSettingsPageWithPromo(Profile* profile,
     promo_params.target_url = chrome::GetSettingsUrl(chrome::kChromeUIGlicHost);
     ShowPromoInPage::Start(browser, std::move(promo_params));
   } else {
-    chrome::ShowSettingsSubPage(browser, chrome::kChromeUIGlicHost);
+    glic::OpenGlicSettingsPage(profile);
   }
 }
 
 }  // namespace
 
 namespace glic {
-
-void OpenGlicSettingsPage(Profile* profile) {
-  NavigateParams params(profile,
-                        chrome::GetSettingsUrl(chrome::kChromeUIGlicHost),
-                        ui::PAGE_TRANSITION_AUTO_TOPLEVEL);
-  params.disposition = WindowOpenDisposition::NEW_FOREGROUND_TAB;
-  Navigate(&params);
-}
 
 void OpenGlicOsToggleSetting(Profile* profile) {
   ShowPromoInPage::Params params;
