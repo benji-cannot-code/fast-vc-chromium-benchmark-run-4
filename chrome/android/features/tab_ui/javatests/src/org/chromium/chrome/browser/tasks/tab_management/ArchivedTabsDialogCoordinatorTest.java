85dfa1dd966f5bb53ad1a8387216a44f9e773f5c
Brandon Wylie <wylieb@chromium.org>
2025-02-19 16:59:04 -0800

    [declutter] Enable IPH feature by default and cleanup fieldtrial config
    Bug: 396735576
    Change-Id: Iae9e58fbb9f132423447b5191c80664a3e4be426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281988
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422287}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinatorTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinatorTest.java
@@ -28,7 +28,6 @@ import static org.junit.Assert.assertTrue;
 import static org.chromium.ui.test.util.ViewUtils.onViewWaiting;
 
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.Espresso;
@@ -48,6 +47,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -83,6 +83,7 @@ import org.chromium.url.GURL;
 @DoNotBatch(reason = "TODO(crbug.com/348068134): Batch this test suite.")
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @EnableFeatures({ChromeFeatureList.ANDROID_TAB_DECLUTTER})
+@DisabledTest(message = "crbug.com/397759336")
 public class ArchivedTabsDialogCoordinatorTest {
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
@@ -99,7 +100,6 @@ public class ArchivedTabsDialogCoordinatorTest {
     private Profile mProfile;
     private ArchivedTabModelOrchestrator mArchivedTabModelOrchestrator;
     private TabModel mArchivedTabModel;
-    private ViewGroup mParentView;
     private TabCreator mRegularTabCreator;
     private TabModel mRegularTabModel;
     private UserActionTester mUserActionTester;
@@ -756,16 +756,15 @@ public class ArchivedTabsDialogCoordinatorTest {
     }
 
     private void showDialog(int numOfArchivedTabs) {
-        onViewWaiting(
-                        withText(
-                                mActivityTestRule
-                                        .getActivity()
-                                        .getResources()
-                                        .getQuantityString(
-                                                R.plurals.archived_tab_card_title,
-                                                numOfArchivedTabs,
-                                                numOfArchivedTabs)))
-                .perform(click());
+        String tabsText =
+                mActivityTestRule
+                        .getActivity()
+                        .getResources()
+                        .getQuantityString(
+                                R.plurals.archived_tab_card_title,
+                                numOfArchivedTabs,
+                                numOfArchivedTabs);
+        onViewWaiting(withText(tabsText)).perform(click());
         mRobot.resultRobot.verifyTabListEditorIsVisible();
         mTimesShown++;
         assertEquals(mTimesShown, mUserActionTester.getActionCount("Tabs.ArchivedTabsDialogShown"));
