ed033dc952327a04e34ac8daac4e7f574cdd37a1
Etienne Pierre-doray <etiennep@chromium.org>
2025-02-20 07:36:12 -0800

    [tracing] Use unique id for histogram rules
    This prevent triggering the wrong rule if multiple rules
    have the same names and the histogram is emitted outside the
    browser process.
    
    Bug: 40257548
    Change-Id: Id352dea12fa77389da893a550e1d6d4a5d359d7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6282886
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422556}

--- content/browser/tracing/background_tracing_rule.cc
+++ content/browser/tracing/background_tracing_rule.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/histogram_scope.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/trace_id_helper.h"
+#include "base/unguessable_token.h"
 #include "components/variations/hashing.h"
 #include "content/browser/tracing/background_tracing_manager_impl.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -183,6 +184,7 @@ class HistogramRule : public BackgroundTracingRule,
                 int histogram_lower_value,
                 int histogram_upper_value)
       : histogram_name_(histogram_name),
+        rule_id_(base::UnguessableToken::Create().ToString()),
         histogram_lower_value_(histogram_lower_value),
         histogram_upper_value_(histogram_upper_value) {}
 
@@ -219,7 +221,7 @@ class HistogramRule : public BackgroundTracingRule,
                             base::Unretained(this), histogram_lower_value_,
                             histogram_upper_value_));
     BackgroundTracingManagerImpl::GetInstance().AddNamedTriggerObserver(
-        rule_name(), this);
+        rule_id_, this);
     BackgroundTracingManagerImpl::GetInstance().AddAgentObserver(this);
   }
 
@@ -227,7 +229,7 @@ class HistogramRule : public BackgroundTracingRule,
     histogram_sample_callback_.reset();
     BackgroundTracingManagerImpl::GetInstance().RemoveAgentObserver(this);
     BackgroundTracingManagerImpl::GetInstance().RemoveNamedTriggerObserver(
-        rule_name(), this);
+        rule_id_, this);
   }
 
   perfetto::protos::gen::TriggerRule ToProtoForTesting() const override {
@@ -254,14 +256,14 @@ class HistogramRule : public BackgroundTracingRule,
 
   // BackgroundTracingManagerImpl::AgentObserver implementation
   void OnAgentAdded(tracing::mojom::BackgroundTracingAgent* agent) override {
-    agent->SetUMACallback(
-        tracing::mojom::BackgroundTracingRule::New(rule_name()),
-        histogram_name_, histogram_lower_value_, histogram_upper_value_);
+    agent->SetUMACallback(tracing::mojom::BackgroundTracingRule::New(rule_id_),
+                          histogram_name_, histogram_lower_value_,
+                          histogram_upper_value_);
   }
 
   void OnAgentRemoved(tracing::mojom::BackgroundTracingAgent* agent) override {
     agent->ClearUMACallback(
-        tracing::mojom::BackgroundTracingRule::New(rule_name()));
+        tracing::mojom::BackgroundTracingRule::New(rule_id_));
   }
 
   void OnHistogramChangedCallback(
@@ -298,6 +300,7 @@ class HistogramRule : public BackgroundTracingRule,
 
  private:
   std::string histogram_name_;
+  std::string rule_id_;
   int histogram_lower_value_;
   int histogram_upper_value_;
   std::optional<base::StatisticsRecorder::ScopedHistogramSampleObserver>
