e95cd010d39e20ddfb87a6fefdfda6017cfd9ec6
Tanmoy Mollik <triploblastic@google.com>
2025-02-20 03:13:27 -0800

    [SCRIP] Add admin_policy_enforced error in OAuth2AccessTokenFetcher
    This cl adds logic to parse a new error string - admin_policy_enforced. This error is converted to
    GoogleServiceAuthError::SCOPE_LIMITED_UNRECOVERABLE_ERROR.
    
    Also this cl adds a subreason similar to InvalidGaiaCredentialsReason
    for scope limited errors. This subreason will be used to disable sync
    for enterprise policy for some reasons, but not for all.
    
    Bug: b:352246509
    Change-Id: Iabbfde58870a9a469eb540c207da2dbe391e42ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179438
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422446}

--- components/signin/internal/identity_manager/profile_oauth2_token_service_delegate_unittest.cc
+++ components/signin/internal/identity_manager/profile_oauth2_token_service_delegate_unittest.cc
@@ -105,7 +105,14 @@ TEST_F(ProfileOAuth2TokenServiceDelegateTest, UpdateAuthErrorPersistenErrors) {
       "table size should match number of auth error types");
 
   for (GoogleServiceAuthError::State state : table) {
-    GoogleServiceAuthError error(state);
+    GoogleServiceAuthError error;
+    if (state == GoogleServiceAuthError::SCOPE_LIMITED_UNRECOVERABLE_ERROR) {
+      error = GoogleServiceAuthError::FromScopeLimitedUnrecoverableErrorReason(
+          GoogleServiceAuthError::ScopeLimitedUnrecoverableErrorReason::
+              kInvalidScope);
+    } else {
+      error = GoogleServiceAuthError(state);
+    }
     if (!error.IsPersistentError() || error.IsScopePersistentError()) {
       continue;
     }
@@ -137,7 +144,14 @@ TEST_F(ProfileOAuth2TokenServiceDelegateTest, UpdateAuthErrorTransientErrors) {
   EXPECT_TRUE(delegate.BackoffEntry());
   int failure_count = 0;
   for (GoogleServiceAuthError::State state : table) {
-    GoogleServiceAuthError error(state);
+    GoogleServiceAuthError error;
+    if (state == GoogleServiceAuthError::SCOPE_LIMITED_UNRECOVERABLE_ERROR) {
+      error = GoogleServiceAuthError::FromScopeLimitedUnrecoverableErrorReason(
+          GoogleServiceAuthError::ScopeLimitedUnrecoverableErrorReason::
+              kInvalidScope);
+    } else {
+      error = GoogleServiceAuthError(state);
+    }
     if (!error.IsTransientError()) {
       continue;
     }
@@ -165,8 +179,10 @@ TEST_F(ProfileOAuth2TokenServiceDelegateTest,
   const CoreAccountId account_id =
       CoreAccountId::FromGaiaId(GaiaId("account_id"));
   delegate.UpdateCredentials(account_id, "refresh_token");
-  GoogleServiceAuthError error(
-      GoogleServiceAuthError::SCOPE_LIMITED_UNRECOVERABLE_ERROR);
+  GoogleServiceAuthError error =
+      GoogleServiceAuthError::FromScopeLimitedUnrecoverableErrorReason(
+          GoogleServiceAuthError::ScopeLimitedUnrecoverableErrorReason::
+              kInvalidScope);
 
   // Scope persistent errors are not persisted or notified as it does not imply
   // that the account is in an error state but the error is only relevant to
