f18f1fce4afce534955047fb38d9d486f335ce80
Adem Derinel <derinel@google.com>
2025-02-20 02:49:18 -0800

    WebAuthn: Disable FCM registrations in Clank
    Google Play Services has been handling registrations (i.e. GetToken) for
    CaBLE linking for quite a while. The notifications are also handled by
    Google Play Services. However, Chrome was also registering itself, even
    though the token was not populated anywhere (i.e. in deviceinfo for
    sync) nor the notifications were handled.
    
    Note that Chrome still needs to query the Google Play Services so it can
    populate the DeviceInfo for Google Play Service's registration token.
    
    Disable this and put it behind a kill switch to make the transition
    easier. Also tested manually that the deviceinfo is still populated
    after this change.
    
    Bug: 397348336
    Change-Id: I43f69790bc41e7e3f30dc860d58bf11af4cdb029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281025
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422438}

--- chrome/browser/webauthn/android/cable_registration_state.cc
+++ chrome/browser/webauthn/android/cable_registration_state.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 
 #include "base/base64.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "crypto/random.h"
@@ -40,6 +41,10 @@ void RegistrationState::Register() {
       base::BindOnce(&RegistrationState::OnSyncRegistrationReady,
                      base::Unretained(this)),
       base::BindRepeating(&RegistrationState::OnEvent, base::Unretained(this)));
+  StartPrelinkEligibilityChecks();
+}
+
+void RegistrationState::StartPrelinkEligibilityChecks() {
   interface_->CanDeviceSupportCable(base::BindOnce(
       &RegistrationState::OnDeviceSupportResult, base::Unretained(this)));
   interface_->AmInWorkProfile(base::BindOnce(
@@ -49,13 +54,18 @@ void RegistrationState::Register() {
 // have_data_for_sync returns true if this object has loaded enough state to
 // put information into sync's DeviceInfo.
 bool RegistrationState::have_data_for_sync() const {
+  if (base::FeatureList::IsEnabled(device::kWebAuthnRegisterForFcm)) {
+    return device_supports_cable_.has_value() &&
+           am_in_work_profile_.has_value() && sync_registration_ != nullptr &&
+           sync_registration_->contact_id() && have_play_services_data();
+  }
   return device_supports_cable_.has_value() &&
-         am_in_work_profile_.has_value() && sync_registration_ != nullptr &&
-         sync_registration_->contact_id() && have_play_services_data();
+         am_in_work_profile_.has_value() && have_play_services_data();
 }
 
 void RegistrationState::SignalSyncWhenReady() {
-  if (sync_registration_ && !sync_registration_->contact_id()) {
+  if (base::FeatureList::IsEnabled(device::kWebAuthnRegisterForFcm) &&
+      sync_registration_ && !sync_registration_->contact_id()) {
     sync_registration_->PrepareContactID();
   }
   if (!have_play_services_data() && !play_services_query_pending_) {
