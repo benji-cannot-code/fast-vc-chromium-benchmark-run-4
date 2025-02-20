03e3600af499e86381bde13f077335c67ae51d1f
ginnnnnnny <ginnyhuang@chromium.org>
2025-02-20 14:16:30 -0800

    [iOS][evolve-disco-header] Add field trial testing config
    Bug: 359325090
    Change-Id: I9380272f28dac7fa9a1bad4b83857edb18ecf81b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287686
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422802}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -1491,6 +1491,9 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
   if (![ChromeEarlGrey isWebChannelsEnabled]) {
     EARL_GREY_TEST_SKIPPED(@"Only applicable with Web Channels enabled.");
   }
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.features_disabled.push_back(kDeprecateFeedHeader);
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
 
   // Sign in to enable Following.
   [SigninEarlGreyUI signinWithFakeIdentity:[FakeSystemIdentity fakeIdentity1]];
@@ -1519,6 +1522,9 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
   if (![ChromeEarlGrey isWebChannelsEnabled]) {
     EARL_GREY_TEST_SKIPPED(@"Only applicable with Web Channels enabled.");
   }
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.features_disabled.push_back(kDeprecateFeedHeader);
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
 
   // Check that regular feed header is visible when signed out, and not
   // Following header.
