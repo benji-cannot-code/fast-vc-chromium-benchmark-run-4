b98e9bfd2a016fddf862f113f0896fa7d970507f
Emily Shack <emshack@chromium.org>
2025-02-20 15:52:51 -0800

    [SxS] Add (non-functional) resize handle
    Creates a MultiContentsResizeArea class which is visually similar to the
    side panel's resize area. This can respond to left/right keyboard events
    to resize as well as mouse click+drag, and has a keyboard focusable
    icon. Currently emits events to a stubbed out OnResize method, will
    implement this in a follow-up to actually resize the WebContents as
    relative to each other.
    
    Bug: 393450761
    Change-Id: Ic8945f0e25995cfb5a9c19bd9d4e097050f5b95c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281806
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422859}

--- chrome/browser/ui/views/frame/multi_contents_view.cc
+++ chrome/browser/ui/views/frame/multi_contents_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
+#include "chrome/browser/ui/views/frame/multi_contents_resize_area.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/events/types/event_type.h"
@@ -23,6 +24,14 @@ MultiContentsView::MultiContentsView(
     : inactive_view_pressed_callback_(inactive_view_pressed_callback) {
   start_contents_view_ =
       AddChildView(std::make_unique<ContentsWebView>(browser_context));
+
+  resize_area_ = AddChildView(std::make_unique<MultiContentsResizeArea>(this));
+  resize_area_->SetProperty(
+      views::kFlexBehaviorKey,
+      views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
+                               views::MaximumFlexSizeRule::kPreferred));
+  resize_area_->SetVisible(false);
+
   end_contents_view_ =
       AddChildView(std::make_unique<ContentsWebView>(browser_context));
   end_contents_view_->SetVisible(false);
@@ -52,6 +61,12 @@ void MultiContentsView::SetWebContents(content::WebContents* web_contents,
       active ? GetActiveContentsView() : GetInactiveContentsView();
   contents_view->SetWebContents(web_contents);
   contents_view->SetVisible(web_contents != nullptr);
+
+  if (start_contents_view_->GetVisible() && end_contents_view_->GetVisible()) {
+    resize_area_->SetVisible(true);
+  } else {
+    resize_area_->SetVisible(false);
+  }
 }
 
 ContentsWebView* MultiContentsView::SetActivePosition(int position) {
@@ -78,6 +93,10 @@ bool MultiContentsView::PreHandleMouseEvent(const blink::WebMouseEvent& event) {
   // Always allow the event to propagate to the WebContents, regardless of
   // whether it was also handled above.
   return false;
+}
+
+void MultiContentsView::OnResize(int resize_amount, bool done_resizing) {
+  // TODO(crbug.com/393450761): Implement this.
 }
 
 BEGIN_METADATA(MultiContentsView)
