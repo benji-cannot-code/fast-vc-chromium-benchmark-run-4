a83c9c4a2dc587ea5c57cd8e7c50e5e917e7c5f7
Jeffrey Cohen <jeffreycohen@google.com>
2025-02-20 15:43:03 -0800

    [CC] nudge not shown when window is open
    Bug: b:397455565
    Change-Id: Ie2148bb665d55b0a11b7a0ad5cd845fc803ed238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6288987
    Commit-Queue: Jeffrey Cohen <jeffreycohen@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422852}

--- chrome/browser/contextual_cueing/contextual_cueing_helper.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_GLIC)
 #include "chrome/browser/glic/glic_enabling.h"
+#include "chrome/browser/glic/glic_keyed_service_factory.h"
 #endif
 
 namespace contextual_cueing {
@@ -182,6 +183,18 @@ bool ContextualCueingHelper::IsBrowserBlockingNudges(
     recorder->set_nudge_decision(NudgeDecision::kNudgeNotShownIPH);
     return true;
   }
+
+#if BUILDFLAG(ENABLE_GLIC)
+  Profile* profile =
+      Profile::FromBrowserContext(web_contents()->GetBrowserContext());
+  auto* glic_service =
+      glic::GlicKeyedServiceFactory::GetGlicKeyedService(profile);
+
+  if (glic_service->IsWindowShowing()) {
+    recorder->set_nudge_decision(NudgeDecision::kNudgeNotShownWindowShowing);
+    return true;
+  }
+#endif  // BUILDFLAG(ENABLE_GLIC)
 
   return false;
 }
