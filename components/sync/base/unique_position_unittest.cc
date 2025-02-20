09182580ab09c346283f0b8f306c3bc233aa70b6
Rushan Suleymanov <rushans@google.com>
2025-02-20 03:01:30 -0800

    [Sync] Limit the memory allocated by UniquePosition
    Uncompressing of the UniquePosition may allocate too much memory
    resulting in exceeding memory. This CL limits the UniquePosition size
    when uncompressed to 10 MB.
    
    Bug: 395117713
    Change-Id: I525363cc1eb3175f4ef5e5f3604c3edfb6acf970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281687
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422443}

--- components/sync/base/unique_position_unittest.cc
+++ components/sync/base/unique_position_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_span.h"
+#include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "components/sync/protocol/unique_position.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -175,6 +176,20 @@ TEST_F(UniquePositionTest, DeserializeObsoleteGzippedPosition) {
 
   UniquePosition pos = UniquePosition::FromProto(proto);
   EXPECT_PRED_FORMAT2(Equals, kHugePosition, pos);
+}
+
+TEST_F(UniquePositionTest, UncompressTooLongRepeatingDigit) {
+  // First 4 bytes represent the digit to expand, and the next 4 bytes is the
+  // number of bytes.
+  constexpr char kSerializedCstr[] = {'\x12', '\x12', '\x12', '\x12',
+                                      '\x88', '\x88', '\x88', '\x88'};
+
+  sync_pb::UniquePosition proto;
+  proto.set_custom_compressed_v1(kSerializedCstr);
+  proto.mutable_custom_compressed_v1()->append(
+      base::RandBytesAsString(UniquePosition::kSuffixLength));
+  UniquePosition unique_position = UniquePosition::FromProto(proto);
+  EXPECT_FALSE(unique_position.IsValid());
 }
 
 class RelativePositioningTest : public UniquePositionTest {};
