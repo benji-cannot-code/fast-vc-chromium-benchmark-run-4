eaf16730c14332c6c466dd0a84b8a04730f53667
Jian Li <jianli@chromium.org>
2025-02-20 15:30:32 -0800

    Revert "Remove feed memory improvement feature"
    This reverts commit de7bb55cabf394875da8b650916cedd0a13bf8b6.
    
    Reason for revert: can break the build without doing upstream change first
    
    Original change's description:
    > Remove feed memory improvement feature
    >
    > Bug: none
    > Change-Id: I6ea8a7ef569438b9ceced954c448564132cd0e4b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6283546
    > Reviewed-by: Xi Han <hanxi@chromium.org>
    > Commit-Queue: Jian Li <jianli@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1422648}
    
    Bug: none
    Change-Id: I50cf380c2a6769a5ac29bb39e894129b1cca492b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6288250
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422844}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -158,6 +158,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &feed::kFeedFollowUiUpdate,
     &feed::kFeedImageMemoryCacheSizePercentage,
     &feed::kFeedLoadingPlaceholder,
+    &feed::kFeedLowMemoryImprovement,
     &feed::kFeedNoViewCache,
     &feed::kFeedPerformanceStudy,
     &feed::kFeedShowSignInCommand,
