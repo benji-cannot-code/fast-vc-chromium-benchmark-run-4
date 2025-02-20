90e6d5020bf3730ed539fff006b0fe761e7bec92
Adem Derinel <derinel@google.com>
2025-02-20 00:18:00 -0800

    WebAuthn: Deprecate authenticator_root_secret pref in the CaBLE module
    The pref and the secret are not used. Deprecate the pref and remove the
    methods regarding the secret.
    
    Bug: 397348336
    Change-Id: I2e96981236e670c9aaa1174ca6a7ba0271fe7d64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281091
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422391}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1085,6 +1085,12 @@ inline constexpr char kUserMicrophoneCaptionLanguageCode[] =
 constexpr char kScannerFeedbackEnabled[] = "ash.scanner.feedback_enabled";
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
+#if BUILDFLAG(IS_ANDROID)
+// Deprecated 02/2025.
+inline constexpr char kRootSecretPrefName[] =
+    "webauthn.authenticator_root_secret";
+#endif  // BUILDFLAG(IS_ANDROID)
+
 // Register local state used only for migration (clearing or moving to a new
 // key).
 void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
@@ -1105,7 +1111,7 @@ void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
   registry->RegisterTimePref(kDeviceRegisteredTime, base::Time());
   registry->RegisterDictionaryPref(kArcKioskDictionaryName);
 
-// Deprecated 04/2024.
+  // Deprecated 04/2024.
   registry->RegisterDictionaryPref(kLastUploadedEuiccStatusPrefLegacy);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
@@ -1177,6 +1183,11 @@ void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
 
   // Deprecated 02/2025.
   registry->RegisterBooleanPref(kUserAgentClientHintsGREASEUpdateEnabled, true);
+
+#if BUILDFLAG(IS_ANDROID)
+  // Deprecated 02/2025.
+  registry->RegisterStringPref(kRootSecretPrefName, std::string());
+#endif  // BUILDFLAG(IS_ANDROID)
 }
 
 // Register prefs used only for migration (clearing or moving to a new key).
@@ -1317,7 +1328,7 @@ void RegisterProfilePrefsForMigration(
                              base::Time());
   registry->RegisterBooleanPref(kShowTunaScreenEnabled, true);
 
-// Deprecated 08/2024
+  // Deprecated 08/2024
   registry->RegisterBooleanPref(kStandaloneWindowMigrationNudgeShown, false);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
@@ -2407,6 +2418,11 @@ void MigrateObsoleteLocalStatePrefs(PrefService* local_state) {
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS)
   local_state->ClearPref(prefs::kDefaultBrowserPromptRefreshStudyGroup);
 #endif
+
+  // Added 02/2025
+#if BUILDFLAG(IS_ANDROID)
+  local_state->ClearPref(kRootSecretPrefName);
+#endif  // BUILDFLAG(IS_ANDROID)
 
   // Please don't delete the following line. It is used by PRESUBMIT.py.
   // END_MIGRATE_OBSOLETE_LOCAL_STATE_PREFS
