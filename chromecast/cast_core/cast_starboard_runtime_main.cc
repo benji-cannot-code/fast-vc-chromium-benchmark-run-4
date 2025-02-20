78c44c4800ceccfa414f7bb38fd871b64e3b7a1f
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-02-20 11:00:36 -0800

    spanification: Add `#pragma allow_unsafe_buffers` to chromecast/
    `//chromecast` and `//ios` are
    the last two directories where `-WUnsafe-buffer-usage` isn't yet
    enforced.
    
    This patch adds opt outs in files failing `-WUnsafe-buffer-usage`.
    The next patch will enable the check:
    https://chromium-review.googlesource.com/c/chromium/src/+/6278826
    
    This patch was made by the script:
    https://paste.googleplex.com/5397514570956800
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    chromium-dev announcement:
    https://groups.google.com/a/chromium.org/g/chromium-dev/c/iEy69ygz-rs/m/yuzoKynQAgAJ
    
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm-dbg
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm-rel
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm64-dbg
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm64-rel
    Cq-Include-Trybots: luci.chromium.try:fuchsia-arm64-cast-receiver-rel
    Cq-Include-Trybots: luci.chromium.try:fuchsia-x64-cast-receiver-dbg
    Cq-Include-Trybots: luci.chromium.try:fuchsia-x64-cast-receiver-rel
    Cq-Include-Trybots: luci.chromium.try:linux-cast-arm-rel
    Cq-Include-Trybots: luci.chromium.try:linux-cast-arm64-rel
    Cq-Include-Trybots: luci.chromium.try:linux-cast-x64-dbg
    Cq-Include-Trybots: luci.chromium.try:linux-cast-x64-rel
    Bug: 40285824
    Change-Id: I6347fe96b020058c5285d40948543aa9e3a2a1a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6273884
    Reviewed-by: Antonio Rivera <antoniori@google.com>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422686}

--- chromecast/cast_core/cast_starboard_runtime_main.cc
+++ chromecast/cast_core/cast_starboard_runtime_main.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 // The entrypoint for the starboard cast runtime. If loggy is enabled (typically
 // used for partner builds), this code forks a separate process to run loggy for
 // logging.
