e64df678e7b72a2e13475d924d83b65cd8a9ed93
Josh Karlin <jkarlin@chromium.org>
2025-02-20 06:46:39 -0800

    [AdTracker] Monitor how often geolocation succeeds with ads in stack
    Monitor not just how often geolocation is called when ad script is
    in the stack, but also how often it would succeed when ad script
    is in the stack.
    
    Bug: 384511645
    Change-Id: Ibafc0dc1da0e9b2a2bedd700227955447182617f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276309
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422514}

--- third_party/blink/renderer/modules/geolocation/geolocation.cc
+++ third_party/blink/renderer/modules/geolocation/geolocation.cc
@@ -219,16 +219,18 @@ void Geolocation::getCurrentPosition(V8PositionCallback* success_callback,
   if (!GetFrame())
     return;
 
-  if (GetFrame()->IsAdScriptInStack()) {
-    UseCounter::Count(GetExecutionContext(),
-                      WebFeature::kAdScriptInStackOnGeoLocation);
-  }
 
   probe::BreakableLocation(GetExecutionContext(),
                            "Geolocation.getCurrentPosition");
 
   auto* notifier = MakeGarbageCollected<GeoNotifier>(this, success_callback,
                                                      error_callback, options);
+
+  if (GetFrame()->IsAdScriptInStack()) {
+    notifier->SetCalledWithAdScriptInStack();
+    UseCounter::Count(GetExecutionContext(),
+                      WebFeature::kAdScriptInStackOnGeoLocation);
+  }
 
   one_shots_->insert(notifier);
 
@@ -250,6 +252,12 @@ int Geolocation::watchPosition(V8PositionCallback* success_callback,
 
   auto* notifier = MakeGarbageCollected<GeoNotifier>(this, success_callback,
                                                      error_callback, options);
+
+  if (GetFrame()->IsAdScriptInStack()) {
+    notifier->SetCalledWithAdScriptInStack();
+    UseCounter::Count(GetExecutionContext(),
+                      WebFeature::kAdScriptInStackOnWatchGeoLocation);
+  }
 
   int watch_id;
   // Keep asking for the next id until we're given one that we don't already
