a80f50d4fc14691a686c81c85751a3a9b41545b0
Keigo Oka <oka@google.com>
2025-02-21 00:37:59 -0800

    extensions: Compile more c/b/extensions files on Android
    A sequel of https://crrev.com/c/6276365. There are files mutually
    including each other but have no external dependencies, and these
    can already compile on Android.
    
    Bug: b/392777363
    Change-Id: I81d6c731914ded0cda4c834ae5b71a36c5e53ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6284647
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422977}

--- chrome/browser/sync/chrome_sync_controller_builder.cc
+++ chrome/browser/sync/chrome_sync_controller_builder.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/extensions/api/storage/settings_sync_util.h"
+#include "chrome/browser/extensions/api/storage/settings_sync_util.h"  // nogncheck
 #include "chrome/browser/extensions/extension_sync_service.h"
 #include "chrome/browser/sync/glue/extension_data_type_controller.h"
 #include "chrome/browser/sync/glue/extension_setting_data_type_controller.h"
