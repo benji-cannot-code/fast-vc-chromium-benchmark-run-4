4fb4c46d998c82915279410dd8670d3e3939fd70
Ted Meyer <tmathmeyer@chromium.org>
2025-02-21 10:06:33 -0800

    Add track selection menu items to media controls
    The audio and video track menus are each only visible when there are
    more than 1 of the respective types of tracks to switch between. As a
    result, this UX is gated behind the
    --enable-experimental-web-platform-features flag, which enabls the
    exposure of multiple tracks.
    
    DISABLE_SPELLCHECKER
    BYPASS_LARGE_CHANGE_WARNING
    
    Binary-Size: The binary size is mostly due to added AX-strings, rather than code. There doesn't seem to be a way to get under the 17kb added without removing the accessibility strings.
    Bug: 381024736
    Change-Id: I17a17c46a74afdf9f348f3b5cf893f05c2488f8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176702
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423248}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
@@ -59,9 +59,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/media/html_media_element_controls_list.h"
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
 #include "third_party/blink/renderer/core/html/time_ranges.h"
+#include "third_party/blink/renderer/core/html/track/audio_track_list.h"
 #include "third_party/blink/renderer/core/html/track/text_track.h"
 #include "third_party/blink/renderer/core/html/track/text_track_container.h"
 #include "third_party/blink/renderer/core/html/track/text_track_list.h"
+#include "third_party/blink/renderer/core/html/track/video_track_list.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/page/spatial_navigation.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer.h"
@@ -92,6 +94,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_text_track_list_element.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_timeline_element.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_toggle_closed_captions_button_element.h"
+#include "third_party/blink/renderer/modules/media_controls/elements/media_control_track_selector_list_element.h"
+#include "third_party/blink/renderer/modules/media_controls/elements/media_control_track_selector_menu_button_element.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_volume_control_container_element.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_volume_slider_element.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_display_cutout_delegate.h"
@@ -177,6 +181,22 @@ bool ShouldShowPlaybackSpeedButton(HTMLMediaElement& media_element) {
   }
 
   return true;
+}
+
+bool ShouldShowTrackSelectionButton(WebMediaPlayer::TrackType track_type,
+                                    HTMLMediaElement& element) {
+  if (!RuntimeEnabledFeatures::AudioVideoTracksEnabled()) {
+    return false;
+  }
+  switch (track_type) {
+    case WebMediaPlayer::TrackType::kVideoTrack:
+      return element.videoTracks().length() >= 2;
+    case WebMediaPlayer::TrackType::kAudioTrack:
+      return element.audioTracks().length() >= 2;
+    case WebMediaPlayer::TrackType::kTextTrack:
+      NOTREACHED();
+  }
+  return false;
 }
 
 bool ShouldShowPictureInPictureButton(HTMLMediaElement& media_element) {
@@ -369,6 +389,10 @@ MediaControlsImpl::MediaControlsImpl(HTMLMediaElement& media_element)
       volume_control_container_(nullptr),
       toggle_closed_captions_button_(nullptr),
       text_track_list_(nullptr),
+      audio_track_selector_button_(nullptr),
+      video_track_selector_button_(nullptr),
+      video_track_selector_list_(nullptr),
+      audio_track_selector_list_(nullptr),
       playback_speed_button_(nullptr),
       playback_speed_list_(nullptr),
       overflow_list_(nullptr),
@@ -599,8 +623,20 @@ void MediaControlsImpl::InitializeControls() {
           *this);
   playback_speed_button_ =
       MakeGarbageCollected<MediaControlPlaybackSpeedButtonElement>(*this);
+
   playback_speed_button_->SetIsWanted(
       ShouldShowPlaybackSpeedButton(MediaElement()));
+
+  video_track_selector_button_ =
+      MakeGarbageCollected<MediaControlTrackSelectorMenuButtonElement>(
+          *this, WebMediaPlayer::TrackType::kVideoTrack);
+  audio_track_selector_button_ =
+      MakeGarbageCollected<MediaControlTrackSelectorMenuButtonElement>(
+          *this, WebMediaPlayer::TrackType::kAudioTrack);
+  video_track_selector_button_->SetIsWanted(ShouldShowTrackSelectionButton(
+      WebMediaPlayer::TrackType::kVideoTrack, MediaElement()));
+  audio_track_selector_button_->SetIsWanted(ShouldShowTrackSelectionButton(
+      WebMediaPlayer::TrackType::kAudioTrack, MediaElement()));
   overflow_menu_ =
       MakeGarbageCollected<MediaControlOverflowMenuButtonElement>(*this);
 
@@ -616,6 +652,13 @@ void MediaControlsImpl::InitializeControls() {
   playback_speed_list_ =
       MakeGarbageCollected<MediaControlPlaybackSpeedListElement>(*this);
   ParserAppendChild(playback_speed_list_);
+
+  video_track_selector_list_ =
+      MakeGarbageCollected<MediaControlTrackSelectorListElement>(*this, true);
+  audio_track_selector_list_ =
+      MakeGarbageCollected<MediaControlTrackSelectorListElement>(*this, false);
+  ParserAppendChild(video_track_selector_list_);
+  ParserAppendChild(audio_track_selector_list_);
 
   overflow_list_ =
       MakeGarbageCollected<MediaControlOverflowMenuListElement>(*this);
@@ -642,6 +685,15 @@ void MediaControlsImpl::InitializeControls() {
   overflow_list_->ParserAppendChild(
       playback_speed_button_->CreateOverflowElement(
           MakeGarbageCollected<MediaControlPlaybackSpeedButtonElement>(*this)));
+  overflow_list_->ParserAppendChild(
+      video_track_selector_button_->CreateOverflowElement(
+          MakeGarbageCollected<MediaControlTrackSelectorMenuButtonElement>(
+              *this, WebMediaPlayer::TrackType::kVideoTrack)));
+  overflow_list_->ParserAppendChild(
+      audio_track_selector_button_->CreateOverflowElement(
+          MakeGarbageCollected<MediaControlTrackSelectorMenuButtonElement>(
+              *this, WebMediaPlayer::TrackType::kAudioTrack)));
+
   if (picture_in_picture_button_) {
     overflow_list_->ParserAppendChild(
         picture_in_picture_button_->CreateOverflowElement(
@@ -955,6 +1007,11 @@ void MediaControlsImpl::OnControlsListUpdated() {
 
   playback_speed_button_->SetIsWanted(
       ShouldShowPlaybackSpeedButton(MediaElement()));
+
+  video_track_selector_button_->SetIsWanted(ShouldShowTrackSelectionButton(
+      WebMediaPlayer::TrackType::kVideoTrack, MediaElement()));
+  audio_track_selector_button_->SetIsWanted(ShouldShowTrackSelectionButton(
+      WebMediaPlayer::TrackType::kAudioTrack, MediaElement()));
 }
 
 LayoutObject* MediaControlsImpl::PanelLayoutObject() {
@@ -1203,6 +1260,22 @@ void MediaControlsImpl::TogglePlaybackSpeedList() {
   playback_speed_list_->SetIsWanted(!playback_speed_list_->IsWanted());
 }
 
+void MediaControlsImpl::ToggleTrackSelectionList(
+    WebMediaPlayer::TrackType type) {
+  switch (type) {
+    case WebMediaPlayer::TrackType::kAudioTrack:
+      audio_track_selector_list_->SetIsWanted(
+          !audio_track_selector_list_->IsWanted());
+      break;
+    case WebMediaPlayer::TrackType::kVideoTrack:
+      video_track_selector_list_->SetIsWanted(
+          !video_track_selector_list_->IsWanted());
+      break;
+    case WebMediaPlayer::TrackType::kTextTrack:
+      NOTREACHED();
+  }
+}
+
 bool MediaControlsImpl::PlaybackSpeedListIsWanted() {
   return playback_speed_list_->IsWanted();
 }
@@ -1288,6 +1361,8 @@ void MediaControlsImpl::UpdateOverflowMenuWanted() const {
       std::make_pair(download_button_.Get(), false),
       std::make_pair(toggle_closed_captions_button_.Get(), false),
       std::make_pair(playback_speed_button_.Get(), false),
+      std::make_pair(video_track_selector_button_.Get(), false),
+      std::make_pair(audio_track_selector_button_.Get(), false),
   };
 
   // These are the elements in order of priority that take up vertical room.
@@ -2017,6 +2092,8 @@ void MediaControlsImpl::MaybeRecordElementsDisplayed() const {
       volume_slider_.Get(),
       toggle_closed_captions_button_.Get(),
       playback_speed_button_.Get(),
+      video_track_selector_button_.Get(),
+      audio_track_selector_button_.Get(),
       picture_in_picture_button_.Get(),
       cast_button_.Get(),
       current_time_display_.Get(),
@@ -2139,6 +2216,14 @@ void MediaControlsImpl::HidePopupMenu() {
 
   if (PlaybackSpeedListIsWanted())
     TogglePlaybackSpeedList();
+
+  if (video_track_selector_list_->IsWanted()) {
+    ToggleTrackSelectionList(WebMediaPlayer::TrackType::kVideoTrack);
+  }
+
+  if (audio_track_selector_list_->IsWanted()) {
+    ToggleTrackSelectionList(WebMediaPlayer::TrackType::kAudioTrack);
+  }
 }
 
 void MediaControlsImpl::VolumeSliderWantedTimerFired(TimerBase*) {
@@ -2232,6 +2317,13 @@ void MediaControlsImpl::Trace(Visitor* visitor) const {
   visitor->Trace(animated_arrow_container_element_);
   visitor->Trace(toggle_closed_captions_button_);
   visitor->Trace(playback_speed_button_);
+
+  visitor->Trace(video_track_selector_button_);
+  visitor->Trace(audio_track_selector_button_);
+
+  visitor->Trace(video_track_selector_list_);
+  visitor->Trace(audio_track_selector_list_);
+
   visitor->Trace(fullscreen_button_);
   visitor->Trace(download_button_);
   visitor->Trace(duration_display_);
--- third_party/blink/renderer/modules/media_controls/media_controls_impl.h
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.h
@@ -28,9 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIA_CONTROLS_MEDIA_CONTROLS_IMPL_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIA_CONTROLS_MEDIA_CONTROLS_IMPL_H_
 
+#include "third_party/blink/public/platform/web_media_player.h"
 #include "third_party/blink/renderer/core/geometry/dom_rect_read_only.h"
 #include "third_party/blink/renderer/core/html/html_div_element.h"
 #include "third_party/blink/renderer/core/html/media/media_controls.h"
+#include "third_party/blink/renderer/modules/media_controls/elements/media_control_track_selector_list_element.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/timer.h"
 
@@ -67,6 +69,7 @@ class MediaControlScrubbingMessageElement;
 class MediaControlTextTrackListElement;
 class MediaControlsTextTrackManager;
 class MediaControlTimelineElement;
+class MediaControlTrackSelectorMenuButtonElement;
 class MediaControlToggleClosedCaptionsButtonElement;
 class MediaControlVolumeControlContainerElement;
 class MediaControlVolumeSliderElement;
@@ -137,6 +140,9 @@ class MODULES_EXPORT MediaControlsImpl final : public HTMLDivElement,
   // Methods related to the playback speed menu.
   void TogglePlaybackSpeedList();
   bool PlaybackSpeedListIsWanted();
+
+  // Methods related to the track selection menu.
+  void ToggleTrackSelectionList(WebMediaPlayer::TrackType);
 
   // Methods related to the overflow menu.
   void OpenOverflowMenu();
@@ -382,6 +388,13 @@ class MODULES_EXPORT MediaControlsImpl final : public HTMLDivElement,
   Member<MediaControlToggleClosedCaptionsButtonElement>
       toggle_closed_captions_button_;
   Member<MediaControlTextTrackListElement> text_track_list_;
+  Member<MediaControlTrackSelectorMenuButtonElement>
+      audio_track_selector_button_;
+  Member<MediaControlTrackSelectorMenuButtonElement>
+      video_track_selector_button_;
+  Member<MediaControlTrackSelectorListElement> video_track_selector_list_;
+  Member<MediaControlTrackSelectorListElement> audio_track_selector_list_;
+
   Member<MediaControlPlaybackSpeedButtonElement> playback_speed_button_;
   Member<MediaControlPlaybackSpeedListElement> playback_speed_list_;
   Member<MediaControlOverflowMenuButtonElement> overflow_menu_;
