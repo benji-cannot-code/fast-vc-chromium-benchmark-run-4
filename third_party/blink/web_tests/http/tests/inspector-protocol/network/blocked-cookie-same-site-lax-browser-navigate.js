f83ec71550c949fcb3f7dd93fee63c8028571a97
Muyao Xu <muyaoxu@google.com>
2025-02-21 15:49:09 -0800

    Revert "Add webkit-httpd.pem's SPKI hash to base.py"
    This reverts commit d786eda6b9e17c0b487d263b185ee3f5c7bca32d.
    
    Reason for revert: causing test failures crbug.com/398286786
    
    Original change's description:
    > Add webkit-httpd.pem's SPKI hash to base.py
    >
    > Tests under third_party/blink/web_tests/http/ run with a different SSL
    > cert than WPT tests. This CL adds the spki hash for that cert to
    > base.py's --ignore-certificate-errors-spki-list flag.
    >
    > This CL also fixes tests that inadvertently depended on an invalid cert.
    >
    > sxg-disable-cache: The 3rd request should be cached now.
    >
    > activate-and-redirect*: Security state is now secure.
    >
    > blocked-cookie-same-site-lax*: Disable cache so that extra info is sent
    > on every request.
    >
    > security-info-on-response: Security state is now secure.
    >
    > request-will-be-sent-redirect-cross-site: Security state is now secure.
    >
    > Change-Id: I56542682c70116f87e265da89a589fc21b2e6848
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6285248
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Steven Bingler <bingler@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1423191}
    
    Bug: 398286786
    Change-Id: I8e8fc1c4e5f71f343fa32088da58d436edaab689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6293284
    Auto-Submit: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Owners-Override: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423462}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/blocked-cookie-same-site-lax-browser-navigate.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/blocked-cookie-same-site-lax-browser-navigate.js
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const {page, session, dp} = await testRunner.startBlank(
       `Verifies that making cross origin requests with SameSite=Lax cookies sends us Network.RequestWillBeSentExtraInfo events with corresponding blocked cookies.\n`);
   await dp.Network.enable();
-  await dp.Network.setCacheDisabled({cacheDisabled: true});
 
   const setCookieUrl = 'https://cookie.test:8443/inspector-protocol/network/resources/set-cookie.php?cookie='
       + encodeURIComponent('name=value; SameSite=Lax');
