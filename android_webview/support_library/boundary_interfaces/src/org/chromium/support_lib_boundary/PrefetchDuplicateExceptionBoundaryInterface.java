905c0c3476c903435138fca29f5e574faae68612
elabadysayed <elabadysayed@chromium.org>
2025-02-21 09:33:54 -0800

    [AW] Add support_lib prefetch duplicate exception
    Add the support library version of the PrefetchDuplicateException to
    prepare for adding it to AndroidX API.
    
    Bug: 391840170
    Change-Id: I29a5d68b7bacab6ea3d81a79dad5063ae5a53bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287929
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423218}

---
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/PrefetchDuplicateExceptionBoundaryInterface.java
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.support_lib_boundary;
+
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+public interface PrefetchDuplicateExceptionBoundaryInterface
+        extends PrefetchExceptionBoundaryInterface {}
