45bfc7e893f885582aa510f467a59a0e46e82e07
Yann Dago <ydago@chromium.org>
2025-02-21 12:02:27 -0800

    Reland "Record metrics for Extensible enterprise SSO flows"
    This is a reland of commit 1c0dd85b4acaf612d03af641901321df94498186
    
    Disables failing tests, they work locally and on the CI but fail
    once they get published. This needs to land because for testing in
    canary. It is also behind a feature flag.
    
    Original change's description:
    > Record metrics for Extensible enterprise SSO flows
    >
    > Bug: 340868357
    > Change-Id: I8a1f6b72838bb374a1066f9c8486aae0501f5af1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265645
    > Commit-Queue: Yann Dago <ydago@chromium.org>
    > Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1423189}
    
    Bug: 340868357
    Change-Id: I47918a99846ee12d6b24ab483eb2dd2f8dc925e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287452
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423303}

--- chrome/browser/enterprise/platform_auth/extensible_enterprise_sso_entra.h
+++ chrome/browser/enterprise/platform_auth/extensible_enterprise_sso_entra.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <AuthenticationServices/AuthenticationServices.h>
 #import <Foundation/Foundation.h>
 
+#import "chrome/browser/enterprise/platform_auth/extensible_enterprise_sso_provider_mac.h"
 #import "chrome/browser/enterprise/platform_auth/platform_auth_provider_manager.h"
 
 namespace net {
@@ -23,6 +24,8 @@ class HttpRequestHeaders;
 
 - (ASAuthorizationSingleSignOnRequest*)createRequest;
 
+- (void)performRequest;
+
 - (ASAuthorizationController*)createAuthorizationControllerWithRequest:
     (ASAuthorizationSingleSignOnRequest*)request;
 
@@ -33,8 +36,10 @@ class HttpRequestHeaders;
 // with empty headers.
 - (void)getAuthHeaders:(NSURL*)url
           withCallback:
-              (enterprise_auth::PlatformAuthProviderManager::GetDataCallback)
-                  callback;
+              (base::OnceCallback<
+                  void(std::unique_ptr<
+                       enterprise_auth::ExtensibleEnterpriseSSOProvider::
+                           DelegateResult>)>)callback;
 
 - (net::HttpRequestHeaders)getHeadersFromHttpResponse:
     (NSHTTPURLResponse*)response;
