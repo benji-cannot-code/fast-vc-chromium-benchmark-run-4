419e5112b45d86cfe29f95119226ff06728fcc0c
Ramin Halavati <rhalavati@google.com>
2025-02-20 23:46:59 -0800

    Clean up PDFiumOnDemandSearchifier's metrics collection.
    Searchfier is supposed to process each page only once. Hence do not keep
    track of pages for which PDF.SearchifyAddedText metric is recorded.
    
    Bug: 360803943
    Change-Id: I330f228435af6e571399a6cd9ddac55ac45ddd8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287327
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422965}

--- pdf/pdfium/pdfium_on_demand_searchifier.h
+++ pdf/pdfium/pdfium_on_demand_searchifier.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PDF_PDFIUM_PDFIUM_ON_DEMAND_SEARCHIFIER_H_
 
 #include <optional>
-#include <set>
 #include <vector>
 
 #include "base/containers/circular_deque.h"
@@ -95,10 +94,6 @@ class PDFiumOnDemandSearchifier {
 
   // Scheduled pages to be searchified.
   base::circular_deque<int> pages_queue_;
-
-  // Keeps track of the indices of pages for which "PDF.SearchifyAddedText"
-  // metric is reported.
-  std::set<int> searchify_added_text_metric_reported_;
 
   State state_ = State::kIdle;
 
