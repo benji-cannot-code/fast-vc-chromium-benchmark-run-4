21d3adcbbb63d7cc0ae7461c527067990c1655de
Matt Menke <mmenke@chromium.org>
2025-02-21 05:05:37 -0800

    Make IOBufferWithCronetBuffer used the span-based IOBuffer API.
    This CL makes IOBufferWithCronetBuffer used the span-based internal
    IOBuffer API to update the value exposed by the buffer. This is part
    of the effort to fully spanify IOBuffer's internal API.
    
    Bug: 396621713
    Change-Id: I4164ce5e41f8d412e9baba8b6f9f6770613565b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287485
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423064}

--- components/cronet/native/io_buffer_with_cronet_buffer.cc
+++ components/cronet/native/io_buffer_with_cronet_buffer.cc
@@ -47,7 +47,7 @@ IOBufferWithCronet_Buffer::~IOBufferWithCronet_Buffer() {
 }
 
 Cronet_BufferPtr IOBufferWithCronet_Buffer::Release() {
-  data_ = nullptr;  // Avoid dangling pointer.
+  ClearSpan();  // Avoid dangling pointer.
   return cronet_buffer_.release();
 }
 
