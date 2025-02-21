42aadc8e4d972fd7e4471df27eed66edd09c4336
Christian Xu <christianxu@google.com>
2025-02-21 02:09:50 -0800

    [iOS][OR] Move open clipboard image to controller
    Move open clipboard image match from OmniboxViewIOS to
    OmniboxAutocompleteController.
    
    Bug: 390409252
    Change-Id: Idff89843f97d5c5632f0638cf4a2cf1df804b561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6277981
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1423005}

--- ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
+++ ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/prefs/pref_backed_boolean.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "third_party/omnibox_proto/groups.pb.h"
+#import "ui/gfx/image/image.h"
 #import "url/gurl.h"
 
 using base::UserMetricsAction;
@@ -205,6 +206,17 @@ using base::UserMetricsAction;
           },
           weakSelf, disposition, matchSelectionTimestamp));
       return;
+    } else if (match.type == AutocompleteMatchType::CLIPBOARD_IMAGE) {
+      clipboardRecentContent->GetRecentImageFromClipboard(base::BindOnce(
+          [](OmniboxAutocompleteController* controller,
+             WindowOpenDisposition disposition, base::TimeTicks timestamp,
+             std::optional<gfx::Image> optionalImage) {
+            [controller openClipboardImage:optionalImage
+                               disposition:disposition
+                                 timestamp:timestamp];
+          },
+          weakSelf, disposition, matchSelectionTimestamp));
+      return;
     }
   }
 
@@ -288,4 +300,25 @@ using base::UserMetricsAction;
       selectionTimestamp:timestamp];
 }
 
+/// Creates a match with the clipboard image and open it.
+- (void)openClipboardImage:(std::optional<gfx::Image>)optionalImage
+               disposition:(WindowOpenDisposition)disposition
+                 timestamp:(base::TimeTicks)timestamp {
+  if (!optionalImage || !_autocompleteController) {
+    return;
+  }
+
+  __weak __typeof(self) weakSelf = self;
+  _autocompleteController->clipboard_provider()->NewClipboardImageMatch(
+      optionalImage,
+      base::BindOnce(
+          [](OmniboxAutocompleteController* controller,
+             WindowOpenDisposition disposition, base::TimeTicks timestamp,
+             std::optional<AutocompleteMatch> optionalMatch) {
+            [controller openCustomMatch:optionalMatch
+                            disposition:disposition
+                     selectionTimestamp:timestamp];
+          },
+          weakSelf, disposition, timestamp));
+}
 @end
