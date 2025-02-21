1eedd3fc74c5b41fca45ba61b037b418844cfaf3
Paul Semel <paulsemel@chromium.org>
2025-02-21 08:42:00 -0800

    fuzzing: experiment with a tint wgsl fuzzer
    This fuzzer isn't much different from the existing one in dawn. The main
    difference is that this one leverages FuzzTest, which might give great
    results somehow. This is an experiment, and generating a string might
    not be the smartest thing. At some point, when we support it, the domain
    will be switched to a grammar based domain generating legitimate WebGPU
    shader language.
    
    Change-Id: Ia2e12127e96c98f219e03a6dd84a296e383146f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6291543
    Auto-Submit: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423178}

---
+++ testing/libfuzzer/fuzzers/tint_wgsl_fuzzer.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/dawn/src/tint/lang/wgsl/reader/reader.h"
+#include "third_party/fuzztest/src/fuzztest/fuzztest.h"
+
+void CanParseWithoutCrashing(std::string_view wgsl) {
+  tint::Source::File file("test.wgsl", wgsl);
+  tint::wgsl::reader::Options parse_options;
+  parse_options.allowed_features = tint::wgsl::AllowedFeatures::Everything();
+  auto program = tint::wgsl::reader::Parse(&file, parse_options);
+}
+
+FUZZ_TEST(ChromiumTintWgslTest, CanParseWithoutCrashing);
