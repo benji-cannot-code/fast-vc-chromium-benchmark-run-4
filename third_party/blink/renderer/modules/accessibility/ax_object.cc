0a0274ffb0499b292b32d2353140321b87d1ef36
Alison Maher <almaher@microsoft.com>
2025-02-21 13:06:40 -0800

    Add map of AXIDs requiring cache update
    The change adds a map from AXIDs that require cache update to the
    update reason. This map, called `nodes_requiring_cache_update_`, lives
    on the AXObjectCacheImpl. AXIDs are added when their corresponding
    object's cached attributes need updating, and they are removed when
    the object has been removed from the tree, or if their cached
    attributes have been updated.
    
    The change adds a setter for `cached_values_need_update_` to AXObject
    to allow us to track when a Node needs to be added/removed from the
    new map.
    
    This change also moves TreeUpdateReason to a common enums file
    so that it can be easily referenced outside of AXObjectCacheImpl.
    
    There were some call sites that added an AXID to
    `nodes_requiring_cache_update_` but did not have a corresponding
    TreeUpdateReason. To handle these, three new reasons have been added:
    kChildInserted, kMarkDocumentDirty, and kNewRelationTargetDirty.
    
    This map is used to confirm there are no objects requiring cached
    attribute update after the tree is finalized. We know there are still
    some cases where this is happening in the wild, and this change is
    meant to help in investigating the root cause of currently possible
    and future cases where this occurs. If this map is not empty after
    the tree is finalized, DUMP_WILL_BE_CHECK is fired with info on all
    remaining entries in the map.
    
    To avoid perf impact, this logic was handled behind AX_FAIL_FAST_BUILD.
    
    Bug: 382104886
    Change-Id: I4e9ec8eda7592cadecc522d81f97cb579aa6b806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6285955
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1423370}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -806,10 +806,10 @@ void AXObject::Detach() {
   }
 
   parent_ = nullptr;
+  SetCachedValuesNeedUpdate(false);
   ax_object_cache_ = nullptr;
   children_dirty_ = false;
   child_cached_values_need_update_ = false;
-  cached_values_need_update_ = false;
   has_dirty_descendants_ = false;
   id_ = 0;
 }
@@ -1333,6 +1333,21 @@ const AtomicString& AXObject::GetInternalsAttribute(
     return g_null_atom;
   }
   return internals->FastGetAttribute(attribute);
+}
+
+void AXObject::SetCachedValuesNeedUpdate(
+    bool cached_values_need_update,
+    std::optional<TreeUpdateReason> reason) {
+  cached_values_need_update_ = cached_values_need_update;
+#if AX_FAIL_FAST_BUILD()
+  CHECK(ax_object_cache_);
+  if (cached_values_need_update) {
+    CHECK(reason);
+    AXObjectCache().AddNodeRequiringCacheUpdate(AXObjectID(), reason.value());
+  } else {
+    AXObjectCache().RemoveNodeRequiringCacheUpdate(AXObjectID());
+  }
+#endif
 }
 
 namespace {
@@ -3634,7 +3649,7 @@ void AXObject::CheckCanAccessCachedValues() const {
   }
 }
 
-void AXObject::InvalidateCachedValues() {
+void AXObject::InvalidateCachedValues(TreeUpdateReason reason) {
   CHECK(AXObjectCache().lifecycle().StateAllowsAXObjectsToBeDirtied())
       << AXObjectCache();
 #if DCHECK_IS_ON()
@@ -3642,7 +3657,7 @@ void AXObject::InvalidateCachedValues() {
       << "Should not invalidate cached values while updating them.";
 #endif
 
-  cached_values_need_update_ = true;
+  SetCachedValuesNeedUpdate(true, reason);
 }
 
 void AXObject::UpdateCachedAttributeValuesIfNeeded(
@@ -3657,7 +3672,7 @@ void AXObject::UpdateCachedAttributeValuesIfNeeded(
     return;
   }
 
-  cached_values_need_update_ = false;
+  SetCachedValuesNeedUpdate(false);
 
   // TODO(almaher): This should never happen and should be updated back to a
   // CHECK once the root cause of the related crashes is better understood.
