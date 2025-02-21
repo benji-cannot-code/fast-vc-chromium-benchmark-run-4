42193158283d1ee4dc9aa7bd479f54e9192211c6
Christopher Cameron <ccameron@chromium.org>
2025-02-21 04:41:23 -0800

    GetSourceImageForCanvas: Use kDoNotChangeAlpha in BaseRenderingContext2D
    The calls to GetSourceImageForCanvas are in the functions drawImage,
    createPattern, and drawMesh.
    
    The conversion to premultiplied format for these functions is unnecessary work, because the downstream Skia calls (e.g,
    SkCanvas::drawImage) will take into account the alpha format via the
    SkAlphaType (assuming that the SkAlphaType is not dropped on the
    ground).
    
    Add a new WPT test to ensure that ImageBitmaps created as premultiplied
    and unpremultiplied both work correctly. This test failed when using
    OOP-raster when the cc::ImageTransferCache dropped the SkiaAlphaType
    on the ground. This tests passes on FireFox, but fails one sub-case
    on WebKit (likely due to unpremultiplying already unpremultiplied
    ImageData when creating an ImageBitmap).
    
    Bug: 40277213
    Change-Id: Iede7051d18b96a0f502e2c24745119018313cfec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6280927
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423050}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -2332,7 +2332,7 @@ void BaseRenderingContext2D::drawImage(CanvasImageSource* image_source,
   } else {
     image = image_source->GetSourceImageForCanvas(
         FlushReason::kDrawImage, &source_image_status, default_object_size,
-        kPremultiplyAlpha);
+        kDontChangeAlpha);
     if (source_image_status == kUndecodableSourceImageStatus) {
       exception_state.ThrowDOMException(
           DOMExceptionCode::kInvalidStateError,
@@ -2534,7 +2534,7 @@ CanvasPattern* BaseRenderingContext2D::createPattern(
   scoped_refptr<Image> image_for_rendering =
       image_source->GetSourceImageForCanvas(FlushReason::kCreatePattern,
                                             &status, default_object_size,
-                                            kPremultiplyAlpha);
+                                            kDontChangeAlpha);
 
   switch (status) {
     case kNormalSourceImageStatus:
@@ -2661,7 +2661,7 @@ void BaseRenderingContext2D::drawMesh(
   SourceImageStatus source_image_status = kInvalidSourceImageStatus;
   scoped_refptr<Image> image = image_source->GetSourceImageForCanvas(
       FlushReason::kDrawMesh, &source_image_status,
-      gfx::SizeF(Width(), Height()), kPremultiplyAlpha);
+      gfx::SizeF(Width(), Height()), kDontChangeAlpha);
   switch (source_image_status) {
     case kUndecodableSourceImageStatus:
       exception_state.ThrowDOMException(
