5a708be8cca4fbe0684b8b49bf0c147467ae98d9
James Price <jrprice@google.com>
2025-02-20 18:42:41 -0800

    Remove expired Support.ShaderF16 histogram
    We no longer need to collect this data.
    
    Change-Id: Ia0521cd837d1ef0a3e8833b38d9da2b4f5573bb0
    Fixed: 388023734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6278330
    Auto-Submit: James Price <jrprice@google.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422912}

--- gpu/config/gpu_info_collector.cc
+++ gpu/config/gpu_info_collector.cc
@@ -310,7 +310,6 @@ void ReportWebGPUAdapterMetrics(dawn::native::Instance* instance) {
   adapter_options.backendType = wgpu::BackendType::Vulkan;
 #endif
 
-  bool supports_shader_f16 = false;
   for (dawn::native::Adapter& nativeAdapter :
        instance->EnumerateAdapters(&adapter_options)) {
     nativeAdapter.SetUseTieredLimits(false);
@@ -350,9 +349,6 @@ void ReportWebGPUAdapterMetrics(dawn::native::Instance* instance) {
       adapter_type = info.adapterType;
     }
 #endif  // WGPU_BREAKING_CHANGE_FLATTEN_LIMITS
-
-    supports_shader_f16 |=
-        wgpu::Adapter(adapter.Get()).HasFeature(wgpu::FeatureName::ShaderF16);
   }
 
   bool has_gpu_adapter = adapter_type != wgpu::AdapterType::Unknown;
@@ -367,9 +363,6 @@ void ReportWebGPUAdapterMetrics(dawn::native::Instance* instance) {
     base::UmaHistogramCounts100000(
         "GPU.WebGPU.MaxTextureDimension2D." + adapter_string,
         max_limits.maxTextureDimension2D);
-
-    base::UmaHistogramBoolean("GPU.WebGPU.Support.ShaderF16",
-                              supports_shader_f16);
   }
 }
 
