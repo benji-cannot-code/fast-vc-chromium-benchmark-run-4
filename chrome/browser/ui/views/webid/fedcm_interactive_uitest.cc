1b8676913710581c8c9fc96061ad78d64b62e673
Arthur Eubanks <aeubanks@google.com>
2025-02-21 08:19:04 -0800

    Remove trailing commas
    These were mistakenly allowed by clang and are now again disallowed.
    
    Bug: 398040357
    Change-Id: Ie19a6507c7277a0e029f8d071a5552f0cb8d1251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6289425
    Auto-Submit: Arthur Eubanks <aeubanks@google.com>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423158}

--- chrome/browser/ui/views/webid/fedcm_interactive_uitest.cc
+++ chrome/browser/ui/views/webid/fedcm_interactive_uitest.cc
@@ -87,7 +87,7 @@ class FedCmCUJTest : public InteractiveBrowserTest {
 IN_PROC_BROWSER_TEST_F(FedCmCUJTest, SelectAccount) {
   RunTestSequence(OpenAccountsModal(),
                   WaitForShow(kFedCmAccountChooserDialogAccountElementId),
-                  PressButton(kFedCmAccountChooserDialogAccountElementId), );
+                  PressButton(kFedCmAccountChooserDialogAccountElementId));
 }
 
 // Shows the bubble account picker. It should hide when a modal UI is shown. It
@@ -97,7 +97,7 @@ IN_PROC_BROWSER_TEST_F(FedCmCUJTest, BubbleHidesWhenModalUIShown) {
       OpenAccountsBubble(),
       WaitForShow(kFedCmAccountChooserDialogAccountElementId), ShowTabModalUI(),
       WaitForHide(kFedCmAccountChooserDialogAccountElementId), HideTabModalUI(),
-      WaitForShow(kFedCmAccountChooserDialogAccountElementId), );
+      WaitForShow(kFedCmAccountChooserDialogAccountElementId));
 }
 
 // TODO(https://crbug.com/387473078): Fix this on windows.
@@ -127,7 +127,7 @@ IN_PROC_BROWSER_TEST_F(FedCmCUJTest, MAYBE_OneClickOutsideBubble) {
       ClickMouse(),
       CheckJsResult(
           kActiveTab,
-          "() => document.getElementById('text').style.display == 'block'"), );
+          "() => document.getElementById('text').style.display == 'block'"));
 }
 
 }  // namespace
