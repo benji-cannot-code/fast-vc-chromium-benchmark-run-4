ead36691b201926d792324edeeb2b46f41cbf632
Marke Hallowell <wry@chromium.org>
2025-02-21 13:12:10 -0800

    Implement focus behavior for attached state for Glic.
    Bug: 394334806
    Change-Id: I6b8592c6b78a1c33e04b16cf0ed873994cb10469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6289227
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423372}

--- chrome/browser/glic/glic_focused_tab_manager.h
+++ chrome/browser/glic/glic_focused_tab_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/glic/glic.mojom.h"
 #include "chrome/browser/glic/glic_tab_data.h"
+#include "chrome/browser/glic/glic_window_controller.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -30,14 +31,13 @@ class Widget;
 class BrowserWindowInterface;
 
 namespace glic {
-class GlicWindowController;
-
 // Responsible for managing which tab is considered "focused" and for accessing
 // its WebContents. This is an implementation detail of GlicKeyedService and
 // others should rely on the interface that GlicKeyedService exposes for
 // observing state changes.
 class GlicFocusedTabManager : public BrowserListObserver,
                               public content::WebContentsObserver,
+                              public GlicWindowController::StateObserver,
                               public views::WidgetObserver {
  public:
   explicit GlicFocusedTabManager(Profile* profile,
@@ -58,6 +58,9 @@ class GlicFocusedTabManager : public BrowserListObserver,
 
   // content::WebContentsObserver
   void PrimaryPageChanged(content::Page& page) override;
+
+  // GlicWindowController::StateObserver
+  void PanelStateChanged(const glic::mojom::PanelState& panel_state) override;
 
   // Callback for changes to focused tab. If no tab is in focus an error reason
   // is returned indicating why and maybe a tab candidate with details as to
