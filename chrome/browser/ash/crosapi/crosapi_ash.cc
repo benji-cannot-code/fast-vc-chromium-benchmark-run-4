9b1df0f71de8bb7365a1c550e9947922c4e8fdb2
Georg Neis <neis@chromium.org>
2025-02-20 23:11:13 -0800

    ash: Remove TimeZoneService crosapi
    Lacros is gone. Remove the unused TimeZoneService crosapi and its
    implementation (TimeZoneServiceAsh).
    
    Bug: b:365741912, b:365902693
    Change-Id: I58ee17ad7bf6b957fd09ecc41f62f710e47c027a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6290187
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422959}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -70,7 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/screen_ai_downloader_ash.h"
 #include "chrome/browser/ash/crosapi/structured_metrics_service_ash.h"
 #include "chrome/browser/ash/crosapi/suggestion_service_ash.h"
-#include "chrome/browser/ash/crosapi/time_zone_service_ash.h"
 #include "chrome/browser/ash/crosapi/virtual_keyboard_ash.h"
 #include "chrome/browser/ash/crosapi/volume_manager_ash.h"
 #include "chrome/browser/ash/crosapi/vpn_service_ash.h"
@@ -237,7 +236,6 @@ CrosapiAsh::CrosapiAsh()
       structured_metrics_service_ash_(
           std::make_unique<StructuredMetricsServiceAsh>()),
       suggestion_service_ash_(std::make_unique<SuggestionServiceAsh>()),
-      time_zone_service_ash_(std::make_unique<TimeZoneServiceAsh>()),
       video_conference_manager_ash_(
           std::make_unique<ash::VideoConferenceManagerAsh>()),
       virtual_keyboard_ash_(std::make_unique<VirtualKeyboardAsh>()),
@@ -688,11 +686,6 @@ void CrosapiAsh::BindTelemetryManagementService(
 void CrosapiAsh::BindTelemetryProbeService(
     mojo::PendingReceiver<mojom::TelemetryProbeService> receiver) {
   probe_service_ash_->BindReceiver(std::move(receiver));
-}
-
-void CrosapiAsh::BindTimeZoneService(
-    mojo::PendingReceiver<mojom::TimeZoneService> receiver) {
-  time_zone_service_ash_->BindReceiver(std::move(receiver));
 }
 
 void CrosapiAsh::BindVideoCaptureDeviceFactory(
