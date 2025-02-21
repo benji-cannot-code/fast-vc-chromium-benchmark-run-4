89ac31ee53a2dd1c29256358e896de606e0b4398
Jeremy Roman <jbroman@chromium.org>
2025-02-21 09:27:40 -0800

    Forward-declare mojo types in form_submission.h.
    This contributes 481 MB (0.12%) of Chrome's expanded source size.
    
    Bug: 40318405
    Change-Id: Id66ee5d8f2c8c1e6b5058661a66bb790376a3020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287692
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423214}

--- third_party/blink/renderer/core/loader/form_submission.h
+++ third_party/blink/renderer/core/loader/form_submission.h
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_FORM_SUBMISSION_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_FORM_SUBMISSION_H_
 
+#include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
-#include "third_party/blink/public/mojom/frame/policy_container.mojom-blink.h"
-#include "third_party/blink/public/mojom/frame/remote_frame.mojom-blink.h"
+#include "third_party/blink/public/mojom/frame/remote_frame.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/frame/triggering_event_info.mojom-blink-forward.h"
 #include "third_party/blink/public/web/web_frame_load_type.h"
 #include "third_party/blink/renderer/core/loader/frame_loader_types.h"
