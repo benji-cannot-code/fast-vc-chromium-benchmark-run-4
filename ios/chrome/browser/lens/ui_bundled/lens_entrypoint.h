3c2375452accb00b4e6adf42ad09ac14a92ee7f9
Radu Nitescu <radunitescu@google.com>
2025-02-21 03:25:27 -0800

    [iOS] Add entry points for LVF Shutter button and gallery
    Also delete the stale lint IfChange. The mapping between the C++ enum
    and the XML enum has been out of sync for a while and it is not in use
    for any histogram in iOS.
    
    Bug: None
    Change-Id: I71602a35b2689eaf9619e0c638dda5c8b949bbb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276452
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1423033}

--- ios/chrome/browser/lens/ui_bundled/lens_entrypoint.h
+++ ios/chrome/browser/lens/ui_bundled/lens_entrypoint.h
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_LENS_UI_BUNDLED_LENS_ENTRYPOINT_H_
 
 // Enum representing the possible Lens entrypoints on iOS.
-// Current values should not be renumbered.
 enum class LensEntrypoint {
-  // LINT.IfChange(IOSLensEntrypoint)
   ContextMenu = 0,
   HomeScreenWidget = 1,
   NewTabPage = 2,
@@ -27,8 +25,9 @@ enum class LensEntrypoint {
   LensOverlayLocationBar = 14,
   LensOverlayOverflowMenu = 15,
   LensOverlayLvfEscapeHatch = 16,
-  kMaxValue = LensOverlayLvfEscapeHatch,
-  //  LINT.ThenChange(//tools/metrics/histograms/metadata/search/enums.xml:AmbientSearchEntryPoint)
+  LensOverlayLvfShutterButton = 17,
+  LensOverlayLvfGallery = 18,
+  kMaxValue = LensOverlayLvfGallery,
 };
 
 extern const char kIOSLensEntrypoint[];
