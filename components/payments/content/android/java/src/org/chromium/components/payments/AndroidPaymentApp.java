86af047a34d292c2fdf5cd76489b6f4fafa65984
Rouslan Solomakhin <rouslan@chromium.org>
2025-02-22 14:33:54 -0800

    Handle a null DialogController in AndroidPaymentApp
    Fixed: 398251572
    Change-Id: Icb15f864f3ecd26ee156bdc01134158b5fdb087d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6292040
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423627}

--- components/payments/content/android/java/src/org/chromium/components/payments/AndroidPaymentApp.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/AndroidPaymentApp.java
@@ -45,7 +45,7 @@ public class AndroidPaymentApp extends PaymentApp
         implements IsReadyToPayServiceHelper.ResultHandler, WindowAndroid.IntentCallback {
     private final Handler mHandler;
     private final AndroidIntentLauncher mLauncher;
-    private final DialogController mDialogController;
+    @Nullable private final DialogController mDialogController;
     private final Set<String> mMethodNames;
     private final boolean mIsIncognito;
     private final String mPackageName;
@@ -91,7 +91,7 @@ public class AndroidPaymentApp extends PaymentApp
      */
     public AndroidPaymentApp(
             AndroidIntentLauncher launcher,
-            DialogController dialogController,
+            @Nullable DialogController dialogController,
             String packageName,
             String activity,
             @Nullable String isReadyToPayService,
@@ -196,7 +196,7 @@ public class AndroidPaymentApp extends PaymentApp
 
         assert !mIsIncognito;
 
-        if (mShowReadyToPayDebugInfo) {
+        if (mShowReadyToPayDebugInfo && mDialogController != null) {
             mDialogController.showReadyToPayDebugInfo(
                     buildReadyToPayDebugInfoString(
                             mIsReadyToPayServiceName,
@@ -290,6 +290,10 @@ public class AndroidPaymentApp extends PaymentApp
             launchRunnable.run();
             return;
         }
+
+        // The dialog controller can be null only in WebView, which does not have a concept of
+        // incognito mode, i.e., `mIsIncognito` is always false in WebView.
+        assert mDialogController != null;
 
         mDialogController.showLeavingIncognitoWarning(
                 this::notifyErrorInvokingPaymentApp, launchRunnable);
