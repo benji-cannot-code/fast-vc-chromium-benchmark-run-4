a43823fee5ea8b50363957998aed2eeee8a1881b
Erik Chen <erikchen@chromium.org>
2025-02-21 16:10:02 -0800

    glic: add --glic-dev flag.
    This flag circumvents some logic that can make development difficult in
    a development environment.
    
    Change-Id: Ia9068e242a26c00e91029419d122c86321ee34e9
    Bug: 398170639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6292964
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423472}

--- chrome/test/data/webui/glic/unit_tests/webview_test.ts
+++ chrome/test/data/webui/glic/unit_tests/webview_test.ts
@@ -12,6 +12,7 @@ suite('WebviewTest', () => {
     loadTimeData.resetForTesting({
       glicAllowedOrigins: '',
       glicGuestURL: 'https://cat.fun/',
+      glicSkipOriginCheck: false,
     });
   });
 
