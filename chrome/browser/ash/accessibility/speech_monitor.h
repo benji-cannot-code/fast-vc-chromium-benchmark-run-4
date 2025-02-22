e18e2736c56869929dbe1695961ce9ae36dc8c30
Jeremy Roman <jbroman@chromium.org>
2025-02-21 17:34:01 -0800

    ash: Rewrite SpeechMonitor time usage to base/ types.
    <chrono> is banned in Chromium except for interop with third-party
    libraries. Using base::TimeTicks here should be functionally equivalent.
    
    Change-Id: Iff2dc7f783ea06da3b6eee566e096f76f54eab36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281315
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423507}

--- chrome/browser/ash/accessibility/speech_monitor.h
+++ chrome/browser/ash/accessibility/speech_monitor.h
@@ -6,13 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_ACCESSIBILITY_SPEECH_MONITOR_H_
 #define CHROME_BROWSER_ASH_ACCESSIBILITY_SPEECH_MONITOR_H_
 
-#include <chrono>
 #include <map>
 #include <optional>
 
 #include "base/containers/circular_deque.h"
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
+#include "base/time/time.h"
 #include "content/public/browser/tts_platform.h"
 
 // TODO(katie): This may need to move into Content as part of the TTS refactor.
@@ -128,7 +128,9 @@ class SpeechMonitor : public content::TtsPlatform {
   void FinishSpeech();
 
   // Delayed utterances.
-  double GetDelayForLastUtteranceMS();
+  base::TimeDelta GetDelayForLastUtterance() const {
+    return delay_for_last_utterance_;
+  }
 
   // When set to `true`, SpeechMonitor will send `START` and `WORD` events for
   // each utterance and will wait to send the `END` event until `FinishSpeech()`
@@ -177,14 +179,11 @@ class SpeechMonitor : public content::TtsPlatform {
 
   std::string error_;
 
-  // Calculates the milliseconds elapsed since the last call to Speak().
-  double CalculateUtteranceDelayMS();
-
-  // Stores the milliseconds elapsed since the last call to Speak().
-  double delay_for_last_utterance_ms_;
+  // Stores the time elapsed since the last call to Speak().
+  base::TimeDelta delay_for_last_utterance_;
 
   // Stores the last time Speak() was called.
-  std::chrono::steady_clock::time_point time_of_last_utterance_;
+  base::TimeTicks time_of_last_utterance_;
 
   // Queue of expectations to be replayed.
   std::vector<ReplayArgs> replay_queue_;
