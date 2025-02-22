17bd3b5c95f9611e24ab4445aca22abd29b12ff4
Elias Klim <elklm@chromium.org>
2025-02-22 03:07:33 -0800

    [PermissionsAI] Upload CPSS UKM only for supported permissions
    Bug: 382447738
    Change-Id: I89c1074ffbdbe4ccb7b8133fea7cf47982ffa6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216525
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423568}

--- components/permissions/prediction_service/prediction_common.cc
+++ components/permissions/prediction_service/prediction_common.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/permissions/prediction_service/prediction_common.h"
 
 #include <cmath>
+
 #include "base/notreached.h"
 #include "base/values.h"
 #include "build/build_config.h"
+#include "components/permissions/features.h"
 
 namespace permissions {
 
@@ -133,8 +135,10 @@ std::unique_ptr<GeneratePredictionsRequest> GetPredictionRequestProto(
       proto_request->mutable_permission_features()->Add();
   FillInStatsFeatures(entity.requested_permission_counts,
                       permission_features->mutable_permission_stats());
-  permission_features->set_permission_relevance(
-      ConvertToProtoRelevance(entity.permission_relevance));
+  if (base::FeatureList::IsEnabled(permissions::features::kPermissionsAIv1)) {
+    permission_features->set_permission_relevance(
+        ConvertToProtoRelevance(entity.permission_relevance));
+  }
   switch (entity.type) {
     case RequestType::kNotifications:
       permission_features->mutable_notification_permission()->Clear();
