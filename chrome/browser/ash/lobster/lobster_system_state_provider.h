da440631a6625e581a6f3f5379fb84538b16866f
Chuong Ho <hdchuong@google.com>
2025-02-23 15:44:12 -0800

    lobster: Add basic specialized feature checks.
    Bug: b:383212706
    
    Change-Id: Ie4a80c77c4c25b69fb3ea6fcf303ada5078f4197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6257607
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423735}

--- chrome/browser/ash/lobster/lobster_system_state_provider.h
+++ chrome/browser/ash/lobster/lobster_system_state_provider.h
@@ -8,25 +8,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/lobster/lobster_text_input_context.h"
 #include "base/memory/raw_ptr.h"
+#include "chromeos/ash/components/specialized_features/feature_access_checker.h"
 #include "ui/base/ime/text_input_type.h"
 
-class Profile;
+class PrefService;
 
 namespace ash {
 struct LobsterSystemState;
 }  // namespace ash
 
-// TODO(b/348280621): Complete enable/disable logic.
+namespace signin {
+class IdentityManager;
+}
+
 class LobsterSystemStateProvider {
  public:
-  explicit LobsterSystemStateProvider(Profile* profile);
+  explicit LobsterSystemStateProvider(
+      PrefService* pref,
+      signin::IdentityManager* identity_manager);
   ~LobsterSystemStateProvider();
 
   ash::LobsterSystemState GetSystemState(
       const ash::LobsterTextInputContext& text_input_context);
 
  private:
-  raw_ptr<Profile> profile_;
+  raw_ptr<PrefService> pref_;
+  specialized_features::FeatureAccessChecker access_checker_;
 };
 
 #endif  // CHROME_BROWSER_ASH_LOBSTER_LOBSTER_SYSTEM_STATE_PROVIDER_H_
