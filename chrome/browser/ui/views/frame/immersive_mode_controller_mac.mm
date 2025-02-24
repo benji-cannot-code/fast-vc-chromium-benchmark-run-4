2cb464c6ed0322ca4a4cb1a8eecebb4b146a328e
Ian Wells <iwells@chromium.org>
2025-02-24 13:57:15 -0800

    Glic user resize 1/~4: create GlicWidget
    No behavior change yet, just extract the glic widget into a
    views::Widget subclass. This will be needed later for enforcing size constraints by
    overriding GetMinimumSize() and GetMaximumSize().
    
    Bug: 397590009
    Change-Id: I31fa76475107a073e3b4df1671bedbe8ca947cd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300072
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424150}

--- chrome/browser/ui/views/frame/immersive_mode_controller_mac.mm
+++ chrome/browser/ui/views/frame/immersive_mode_controller_mac.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/foundation_util.h"
 #include "base/check.h"
 #include "base/feature_list.h"
-#include "chrome/browser/glic/glic_window_controller.h"
+#include "chrome/browser/glic/glic_widget.h"
 #include "chrome/browser/ui/find_bar/find_bar.h"
 #include "chrome/browser/ui/find_bar/find_bar_controller.h"
 #include "chrome/browser/ui/fullscreen_util_mac.h"
