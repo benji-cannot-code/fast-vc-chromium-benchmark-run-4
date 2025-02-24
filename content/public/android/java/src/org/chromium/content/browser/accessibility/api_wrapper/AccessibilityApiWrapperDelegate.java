2923b91506406edd9345dbb212024752aff1c23e
Wenyu Zhang <zhangwenyu@google.com>
2025-02-24 14:47:04 -0800

    a11y: Initiate a11y API wrapper library
    Accessibility API wrapper library aims to verify proposed android
    frameworks accessibility API changes. This library is only available in the default and canary build.
    
    The internal repo CL is at #comment3 in the ticket below.
    
    Bug: 393448318
    Test: Verified locally working in default/canary, not available in dev
    Change-Id: I749375bef6d64907a42a4bb6112eced392a65673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6274405
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424174}

---
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/api_wrapper/AccessibilityApiWrapperDelegate.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content.browser.accessibility.api_wrapper;
+
+import androidx.annotation.Nullable;
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
+
+/**
+ * A delegation interface for accessibility API wrapper library with accessibility API signatures
+ * exposed to internal clank to implement. Methods should exactly match the API.
+ */
+public interface AccessibilityApiWrapperDelegate {
+    /** An example test API demonstrating the usage of the accessibility API wrapper library. */
+    @Nullable
+    public CharSequence getMyTestStringApi(AccessibilityNodeInfoCompat node);
+
+    /** An example test API demonstrating the usage of the accessibility API wrapper library. */
+    public void setMyTestStringApi(
+            AccessibilityNodeInfoCompat node, @Nullable CharSequence myTestStringApi);
+}
