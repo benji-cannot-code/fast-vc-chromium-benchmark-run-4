b3fedbb9e5c267d6cdf8f4c7def2667beb16f04d
rbpotter <rbpotter@chromium.org>
2025-02-24 14:09:22 -0800

    chrome://traces-internals: Place behind InternalOnlyUisEnabled pref
    This UI was identified as an internal debugging tool in the WebUI
    survey. Place it behind the InternalOnlyUisEnabled pref, so that its
    availability is controlled from chrome://chrome-urls and it appears in
    the "internal debugging pages" list on that page.
    
    Bug: 379889249
    Change-Id: I599f1ca5cb8183119c3184bd3e4cd342c5f2465b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6283647
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424158}

--- content/browser/tracing/trace_report/trace_report_internals_ui.h
+++ content/browser/tracing/trace_report/trace_report_internals_ui.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_TRACING_TRACE_REPORT_TRACE_REPORT_INTERNALS_UI_H_
 
 #include "content/browser/tracing/trace_report/trace_report.mojom.h"
+#include "content/public/browser/internal_webui_config.h"
 #include "content/public/browser/web_ui_controller.h"
-#include "content/public/browser/webui_config.h"
 #include "content/public/common/url_constants.h"
 
 namespace content {
@@ -19,10 +19,10 @@ class TraceReportInternalsUI;
 
 // WebUIConfig for the chrome://traces page.
 class TraceReportInternalsUIConfig
-    : public DefaultWebUIConfig<TraceReportInternalsUI> {
+    : public DefaultInternalWebUIConfig<TraceReportInternalsUI> {
  public:
   TraceReportInternalsUIConfig()
-      : DefaultWebUIConfig(kChromeUIScheme, kChromeUITracesInternalsHost) {}
+      : DefaultInternalWebUIConfig(kChromeUITracesInternalsHost) {}
 };
 
 // WebUIController for the chrome://traces page.
