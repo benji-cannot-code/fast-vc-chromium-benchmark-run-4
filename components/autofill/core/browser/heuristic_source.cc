3d7ec82f942523bd2ff26655caf0c945bd7e93ec
Christoph Schwering <schwering@google.com>
2025-02-24 02:03:11 -0800

    [Autofill] Cache kAutofillModelPredictionsAreActive
    Before crrev.com/c/6278509, kAutofillModelPredictionsAreActive.Get() had
    been cached with a `static` variable. The `static` variable broke tests,
    but the cache was intentional: crrev.com/c/5907380.
    
    This CL caches the parameter with the new `BASE_FEATURE_PARAM()` macro.
    
    Bug: 308433396
    Change-Id: Ib5cf76ff4eaeece2c3b7b48839aa4737124327d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6294903
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1423804}

--- components/autofill/core/browser/heuristic_source.cc
+++ components/autofill/core/browser/heuristic_source.cc
@@ -16,9 +16,7 @@ namespace autofill {
 
 HeuristicSource GetActiveHeuristicSource() {
   if (base::FeatureList::IsEnabled(features::kAutofillModelPredictions)) {
-    bool model_predictions_active =
-        features::kAutofillModelPredictionsAreActive.Get();
-    if (model_predictions_active) {
+    if (features::kAutofillModelPredictionsAreActive.Get()) {
       return HeuristicSource::kAutofillMachineLearning;
     }
   }
