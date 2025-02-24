87ce1370956615f638e7ae0cdc6b80b20f1f09e3
“Prasanaa” <prasanaa@google.com>
2025-02-24 02:34:52 -0800

    [iOS] Add empty method SetToolbarUIState in FullscreenController.
    Add method SetToolbarUIState in FullscreenController, so that test
    `FullscreenBrowserAgentTest` in ios_internal can pass.
    
    Change-Id: Ib217d8c94788be32166e9314f399840cf36429cd
    
    Bug: 397960215
    Change-Id: Ib217d8c94788be32166e9314f399840cf36429cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6291129
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423809}

--- ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller.h
+++ ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 @class ChromeBroadcaster;
 class FullscreenControllerObserver;
+@class ToolbarUIState;
 
 // An object that observes scrolling events in the main content area and
 // calculates how much of the toolbar should be visible as a result.  When the
@@ -36,6 +37,8 @@ class FullscreenController : public base::SupportsUserData::Data {
   // Adds and removes FullscreenControllerObservers.
   virtual void AddObserver(FullscreenControllerObserver* observer) = 0;
   virtual void RemoveObserver(FullscreenControllerObserver* observer) = 0;
+
+  virtual void SetToolbarUIState(ToolbarUIState* toolbar_ui_state) = 0;
 
   // FullscreenController can be disabled when a feature requires that the
   // toolbar be fully visible.  Since there are multiple reasons fullscreen
--- ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller_impl.mm
+++ ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller_impl.mm
@@ -195,3 +195,6 @@ void FullscreenControllerImpl::ResizeHorizontalViewport() {
   // two relayouts.
   mediator_.ResizeHorizontalInsets();
 }
+
+void FullscreenControllerImpl::SetToolbarUIState(
+    ToolbarUIState* toolbar_ui_state) {}
