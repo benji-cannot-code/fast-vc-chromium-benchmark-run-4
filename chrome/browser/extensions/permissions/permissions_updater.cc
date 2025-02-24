0d98b75ce5c71f2c9b8f6396e35b048e03cb53f9
James Cook <jamescook@chromium.org>
2025-02-24 14:41:04 -0800

    extensions: Remove a //c/b/e dependency from PermissionsUpdater
    Specifically:
    //chrome/browser/extensions/chrome_extension_system_factory.h
    
    Replace it with a call to ExtensionsBrowserClient. This is a step
    towards making PermissionsUpdater compile on desktop Android.
    
    Bug: 397766259
    Change-Id: I3589c4c03bfcfc24c1042db65c01efc7750793e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6293690
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424163}

--- chrome/browser/extensions/permissions/permissions_updater.cc
+++ chrome/browser/extensions/permissions/permissions_updater.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/no_destructor.h"
 #include "base/values.h"
-#include "chrome/browser/extensions/chrome_extension_system_factory.h"
 #include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/permissions/scripting_permissions_modifier.h"
 #include "chrome/browser/profiles/profile.h"
@@ -29,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/event_router_factory.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_system_provider.h"
 #include "extensions/browser/extension_util.h"
+#include "extensions/browser/extensions_browser_client.h"
 #include "extensions/browser/network_permissions_updater.h"
 #include "extensions/browser/permissions_manager.h"
 #include "extensions/browser/renderer_startup_helper.h"
@@ -74,7 +75,7 @@ class PermissionsUpdaterShutdownNotifierFactory
       : BrowserContextKeyedServiceShutdownNotifierFactory(
             "PermissionsUpdaterShutdownFactory") {
     DependsOn(EventRouterFactory::GetInstance());
-    DependsOn(ChromeExtensionSystemFactory::GetInstance());
+    DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
   }
   ~PermissionsUpdaterShutdownNotifierFactory() override = default;
 };
