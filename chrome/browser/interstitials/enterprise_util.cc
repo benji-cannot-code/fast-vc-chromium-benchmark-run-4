4e7653214667e88f1795808c412e639dd038bdf5
Byungwoo Lee <blee@igalia.com>
2025-02-23 20:50:15 -0800

    Reland "Fix build with safe_browsing_mode=0 (4/5)"
    This is a reland of commit 8f68ae0dc09483d11056235dd23538a9c1716adb
    
    Original change's description:
    > Fix build with safe_browsing_mode=0 (4/5)
    >
    > Fix the `safe_browsing_mode=0` build by guarding header includes
    > and related codes using `BUILDFLAG(SAFE_BROWSING_AVAILABLE)`.
    >
    > This CL is a part of the build fix that removes following source
    > files or build dependency from build target:
    > - //chrome/browser:browser
    >   - component_updater/file_type_policies_component_installer.cc
    >   - component_updater/file_type_policies_component_installer.h
    >   - //components/safe_browsing/content/browser:client_side_detection
    > - //chrome/browser/enterprise/data_controls:enterprise_data_controls
    >   - reporting_service.cc
    >   - reporting_service.h
    > - //chrome/browser/extensions:extensions
    >   - api/safe_browsing_private/
    >     - safe_browsing_private_event_router.cc
    >     - safe_browsing_private_event_router.h
    >     - safe_browsing_private_event_router_factory.cc
    >     - safe_browsing_private_event_router_factory.h
    > - //chrome/browser/safe_browsing:safe_browsing
    >   - chrome_safe_browsing_tab_observer_delegate.cc
    >   - chrome_safe_browsing_tab_observer_delegate.h
    >   - //components/safe_browsing/content/browser:client_side_detection
    > - //chrome/browser/permissions:permissions
    >   - contextual_notification_permission_ui_selector.cc
    >   - contextual_notification_permission_ui_selector.h
    > - //chrome/browser/ui:ui
    >   - //components/safe_browsing/content/browser:client_side_detection
    > - //chrome/browser/ui/webui:configs
    >   - //chrome/browser/ui/webui/safe_browsing
    > - //chrome/test:browser_test
    >   - //components/safe_browsing/content/browser:client_side_detection
    > - //chrome/test:unit_tests
    >   - //components/safe_browsing/content/browser:client_side_detection
    > - //components/safe_browsing/core/browser/db:v4_store
    >   - v4_store.cc
    >   - v4_store.h
    >
    > and guards following header inclusions under the flag:
    > - chrome/browser/enterprise/data_controls/reporting_service.h
    > - chrome/browser/extensions/api/safe_browsing_private/
    >   - safe_browsing_private_event_router_factory.h
    > - chrome/browser/safe_browsing
    >   - chrome_safe_browsing_tab_observer_delegate.h
    > - chrome/browser/permissions/
    >   - contextual_notification_permission_ui_selector.h
    > - chrome/browser/ui/webui/safe_browsing/chrome_safe_browsing_ui.h
    > - components/safe_browsing/
    >   - content/browser/safe_browsing_tab_observer.h
    >
    > This CL also removes unnecessary guard in safe_browsing_tab_observer.cc
    >
    > Bug: 390205854
    > Change-Id: Ic9047c55fd0e29d8e09242dd0135a3f8dccc1666
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174774
    > Reviewed-by: Elias Klim <elklm@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Matt Mueller <mattm@chromium.org>
    > Commit-Queue: Byungwoo Lee <blee@igalia.com>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Reviewed-by: Lily Chen <chlily@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1420836}
    
    Bug: 390205854
    Change-Id: I0cf54eb6535ac58761ad7e45976d054eb1101d7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6275265
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Byungwoo Lee <blee@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1423763}

--- chrome/browser/interstitials/enterprise_util.cc
+++ chrome/browser/interstitials/enterprise_util.cc
@@ -6,18 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/interstitials/enterprise_util.h"
 
 #include "chrome/browser/profiles/profile.h"
-#include "components/safe_browsing/core/common/proto/realtimeapi.pb.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/buildflags/buildflags.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 #include "chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h"
 #include "chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_factory.h"
+#include "components/safe_browsing/core/common/proto/realtimeapi.pb.h"
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 namespace {
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 extensions::SafeBrowsingPrivateEventRouter* GetEventRouter(
     content::WebContents* web_contents) {
   // |web_contents| can be null in tests.
@@ -45,7 +45,7 @@ void MaybeTriggerSecurityInterstitialShownEvent(
     const GURL& page_url,
     const std::string& reason,
     int net_error_code) {
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(SAFE_BROWSING_AVAILABLE)
   extensions::SafeBrowsingPrivateEventRouter* event_router =
       GetEventRouter(web_contents);
   if (!event_router)
@@ -59,7 +59,7 @@ void MaybeTriggerSecurityInterstitialProceededEvent(
     const GURL& page_url,
     const std::string& reason,
     int net_error_code) {
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(SAFE_BROWSING_AVAILABLE)
   extensions::SafeBrowsingPrivateEventRouter* event_router =
       GetEventRouter(web_contents);
   if (!event_router)
@@ -69,6 +69,7 @@ void MaybeTriggerSecurityInterstitialProceededEvent(
 #endif
 }
 
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 void MaybeTriggerUrlFilteringInterstitialEvent(
     content::WebContents* web_contents,
     const GURL& page_url,
@@ -82,5 +83,6 @@ void MaybeTriggerUrlFilteringInterstitialEvent(
   }
   event_router->OnUrlFilteringInterstitial(page_url, threat_type,
                                            rt_lookup_response);
-#endif
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 }
+#endif  // BUILDFLAG(SAFE_BROWSING_AVAILABLE)
--- chrome/browser/ui/webui/chrome_web_ui_configs.cc
+++ chrome/browser/ui/webui/chrome_web_ui_configs.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/policy/policy_ui.h"
 #include "chrome/browser/ui/webui/predictors/predictors_ui.h"
 #include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.h"
-#include "chrome/browser/ui/webui/safe_browsing/chrome_safe_browsing_ui.h"
 #include "chrome/browser/ui/webui/saved_tab_groups_unsupported/saved_tab_groups_unsupported_ui.h"
 #include "chrome/browser/ui/webui/segmentation_internals/segmentation_internals_ui.h"
 #include "chrome/browser/ui/webui/signin_internals_ui.h"
@@ -194,6 +193,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/glic_ui.h"
 #endif
 
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
+#include "chrome/browser/ui/webui/safe_browsing/chrome_safe_browsing_ui.h"
+#endif
+
 void RegisterChromeWebUIConfigs() {
   // Don't add calls to `AddWebUIConfig()` for Ash-specific WebUIs here. Add
   // them in chrome_web_ui_configs_chromeos.cc.
@@ -240,8 +243,6 @@ void RegisterChromeWebUIConfigs() {
   map.AddWebUIConfig(
       std::make_unique<
           security_interstitials::KnownInterceptionDisclosureUIConfig>());
-  map.AddWebUIConfig(
-      std::make_unique<safe_browsing::ChromeSafeBrowsingUIConfig>());
   map.AddWebUIConfig(std::make_unique<SavedTabGroupsUnsupportedUIConfig>());
   map.AddWebUIConfig(std::make_unique<SegmentationInternalsUIConfig>());
   map.AddWebUIConfig(std::make_unique<SignInInternalsUIConfig>());
@@ -391,5 +392,10 @@ void RegisterChromeWebUIConfigs() {
 #if BUILDFLAG(ENABLE_GLIC)
   map.AddWebUIConfig(std::make_unique<glic::GlicUIConfig>());
   map.AddWebUIConfig(std::make_unique<glic::GlicFreUIConfig>());
+#endif
+
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
+  map.AddWebUIConfig(
+      std::make_unique<safe_browsing::ChromeSafeBrowsingUIConfig>());
 #endif
 }
