53aae27c6157029c177f491da77d9629f8ddac56
Gauthier Ambard <gambard@chromium.org>
2025-02-24 10:04:25 -0800

    Return when group data is failing
    I think this is a mistake of the original implementation.
    
    Bug: 398797371
    Change-Id: I11425661577b997e883e8ba517bf46dea8d0a1e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297929
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1423978}

--- components/collaboration/internal/collaboration_controller.cc
+++ components/collaboration/internal/collaboration_controller.cc
@@ -426,6 +426,7 @@ class CheckingFlowRequirementsState : public ControllerState {
     if (!group_outcome.has_value()) {
       RecordJoinEvent(CollaborationServiceJoinEvent::kReadNewGroupFailed);
       HandleErrorWithType(ErrorInfo::Type::kInvalidUrl);
+      return;
     }
 
     RecordJoinEvent(CollaborationServiceJoinEvent::kReadNewGroupSuccess);
