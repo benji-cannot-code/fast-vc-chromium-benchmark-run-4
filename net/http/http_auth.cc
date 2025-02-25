985d2577f5a7d79bb0be03c7e5e4675e79270b2b
Kalvin Lee <kdlee@chromium.org>
2025-02-25 05:28:29 -0800

    arrayify: Prefer `constexpr`
    This CL is intended to be trivial. It marks the return from `std::to_array` as
    `constexpr`. This CL is a split of a naive textual search-and-replace under:
    
    ['components/webcrypto', 'net/http']
    
    This CL was uploaded by git cl split.
    
    Change-Id: I927974d810653819533fd45ae8f1a4960f6cd93d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298273
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424468}

--- net/http/http_auth.cc
+++ net/http/http_auth.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 namespace {
-const auto kSchemeNames = std::to_array<const char*>({
+constexpr auto kSchemeNames = std::to_array<const char*>({
     kBasicAuthScheme,
     kDigestAuthScheme,
     kNtlmAuthScheme,
--- net/http/transport_security_state_unittest.cc
+++ net/http/transport_security_state_unittest.cc
@@ -82,7 +82,7 @@ namespace test3 {
 
 const char kHost[] = "example.test";
 
-const auto kGoodPath = std::to_array<const char*>({
+constexpr auto kGoodPath = std::to_array<const char*>({
     "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
     "sha256/fzP+pVAbH0hRoUphJKenIP8+2tD/d2QH9J+kQNieM6Q=",
     "sha256/9vRUVdjloCa4wXUKfDWotV5eUXYD7vu0v0z9SRzQdzg=",
@@ -90,7 +90,7 @@ const auto kGoodPath = std::to_array<const char*>({
     nullptr,
 });
 
-const auto kBadPath = std::to_array<const char*>({
+constexpr auto kBadPath = std::to_array<const char*>({
     "sha256/1111111111111111111111111111111111111111111=",
     "sha256/2222222222222222222222222222222222222222222=",
     "sha256/3333333333333333333333333333333333333333333=",
