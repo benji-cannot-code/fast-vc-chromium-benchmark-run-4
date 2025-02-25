f372ebe6796cfeea3fadac6dd419fe0e3b5a0040
Peter Kvitek <kvitekp@chromium.org>
2025-02-25 12:40:34 -0800

    [headless] Fix unusual window origin coordinate order
    Change-Id: Ic6c994209a2f3d6cd95433d929dbfe7537ccc571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299976
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424726}

--- headless/test/data/protocol/sanity/move-window-between-screens.js
+++ headless/test/data/protocol/sanity/move-window-between-screens.js
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     testRunner.log(
         `Window` +
-        ` ${bounds.top},${bounds.left} ${bounds.width}x${bounds.height}` +
+        ` ${bounds.left},${bounds.top} ${bounds.width}x${bounds.height}` +
         `, screen ${screen}`);
   }
 
