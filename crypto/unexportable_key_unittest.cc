76a9abdad23645211c84a830303eed69a2f1c0f1
Nina Satragno <nsatragno@chromium.org>
2025-02-25 12:38:16 -0800

    [crypto] Explicitly label win unexportable keys
    Chrome used to rely on an undocumented Windows feature to export wrapped
    NCrypt keys to avoid storing metadata about them. This is risky: the
    Windows API could stop working underneath us, and it doesn't work for
    the software backend, so it's blocking crbug.com/398125798.
    
    Instead, do the same thing we do on MacOS and generate a random
    identifier for the key. When loading a TPM key, try to do it both by
    name and from the wrapped key to support previously created keys.
    
    Fixed: 398125799
    Change-Id: I4747ba1b8777a11783d8bfe8937e0f1ed7a9bfb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297627
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424722}

--- crypto/unexportable_key_unittest.cc
+++ crypto/unexportable_key_unittest.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/logging.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
+#include "crypto/features.h"
 #include "crypto/scoped_mock_unexportable_key_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -30,8 +32,24 @@ constexpr char kTestKeychainAccessGroup[] = "test-keychain-access-group";
 
 class UnexportableKeySigningTest
     : public testing::TestWithParam<
-          std::tuple<crypto::SignatureVerifier::SignatureAlgorithm, bool>> {
+          std::tuple<crypto::SignatureVerifier::SignatureAlgorithm,
+                     bool,
+                     bool>> {
+ public:
+  void SetUp() override {
+    bool label_win_keys = std::get<2>(GetParam());
+#if BUILDFLAG(IS_WIN)
+    scoped_feature_list.InitWithFeatureState(
+        crypto::features::kLabelWindowsUnexportableKeys, label_win_keys);
+#else
+    if (label_win_keys) {
+      GTEST_SKIP() << "Skipping Windows-specific flag on non Windows";
+    }
+#endif
+  }
+
  private:
+  base::test::ScopedFeatureList scoped_feature_list;
 #if BUILDFLAG(IS_MAC)
   crypto::ScopedFakeAppleKeychainV2 scoped_fake_apple_keychain_{
       kTestKeychainAccessGroup};
@@ -41,6 +59,7 @@ class UnexportableKeySigningTest
 INSTANTIATE_TEST_SUITE_P(All,
                          UnexportableKeySigningTest,
                          testing::Combine(testing::ValuesIn(kAllAlgorithms),
+                                          testing::Bool(),
                                           testing::Bool()));
 
 TEST_P(UnexportableKeySigningTest, RoundTrip) {
