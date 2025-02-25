b2d1198a2a3c65e178496849ebdebe7a9d8a9cf1
Anthony Cui <cuianthony@google.com>
2025-02-25 13:07:34 -0800

    Send the current chrome theme to the glic fre page via query param
    Bug: 398251634
    Change-Id: Ie8e25f3d45f651a58d1fe769dcda3f272c15aef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6293287
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424744}

--- chrome/browser/glic/fre_util.h
+++ chrome/browser/glic/fre_util.h
@@ -9,11 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 class GURL;
+class Profile;
+class ThemeService;
 
 namespace glic {
 
-GURL GetFreURL();
+GURL GetFreURL(Profile* profile);
 std::string GetHotkeyString();
+bool UseDarkMode(ThemeService* theme_service);
 
 }  // namespace glic
 
