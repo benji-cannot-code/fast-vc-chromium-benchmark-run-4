b9a7fa2a9d22b38b1471d8be32d0090b87f0377b
anthie@google.com <anthie@google.com>
2025-02-25 02:17:48 -0800

    Overrize zoom in parent approval dialog
    The font size of the dialog follow the size in
    Chrome > Settings > Appearance > fornt size rather than
    the content zoom level.
    
    Bug: 391308258
    Change-Id: I8842084286df13f92c566740c29179cde5c9ca89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287415
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424405}

--- chrome/browser/supervised_user/linux_mac_windows/parent_access_view.cc
+++ chrome/browser/supervised_user/linux_mac_windows/parent_access_view.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/supervised_user/core/browser/supervised_user_utils.h"
 #include "components/supervised_user/core/common/features.h"
+#include "content/public/browser/host_zoom_map.h"
+#include "third_party/blink/public/common/page/page_zoom.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/controls/webview/webview.h"
@@ -40,6 +42,19 @@ const GURL GetPacpUrl(
   return supervised_user::GetParentAccessURLForDesktop(
       g_browser_process->GetApplicationLocale(), blocked_url, filtering_reason);
 }
+
+// Override the default zoom level for the parent approval dialog.
+// Its size should align with native UI elements, rather than web content.
+void OverrideZoomFactor(content::WebContents* web_contents,
+                        const GURL& pacp_url) {
+  CHECK(web_contents);
+  double zoom_factor = 1;
+  content::HostZoomMap* zoom_map =
+      content::HostZoomMap::GetForWebContents(web_contents);
+  zoom_map->SetZoomLevelForHost(pacp_url.host(),
+                                blink::ZoomFactorToZoomLevel(zoom_factor));
+}
+
 }  // namespace
 
 DialogContentLoadWithTimeoutObserver::DialogContentLoadWithTimeoutObserver(
@@ -186,6 +201,7 @@ void ParentAccessView::Initialize(const GURL& pacp_url, int corner_radius) {
   // Loads the PACP widget's url. This creates the new web_contents of the
   // dialog.
   web_view_->LoadInitialURL(pacp_url);
+
   // Allows dismissing the dialog via the `Escape` button.
   web_view_->set_allow_accelerators(true);
   web_view_->SetProperty(views::kElementIdentifierKey,
@@ -200,6 +216,7 @@ void ParentAccessView::Initialize(const GURL& pacp_url, int corner_radius) {
 
   corner_radius_ = corner_radius;
   is_initialized_ = true;
+  OverrideZoomFactor(GetWebViewContents(), pacp_url);
 }
 
 void ParentAccessView::ShowNativeView() {
