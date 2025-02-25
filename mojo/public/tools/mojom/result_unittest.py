2b1bff3e3ea17564f070a29cda729f2438429d4e
Fred Shih <ffred@chromium.org>
2025-02-25 13:23:10 -0800

    Add translation logic from AST to decorated tree for result<T,E>
    I chose to introduce a new field in the Method node because it will be
    quite difficult to refactor all the code to expect either a param list
    or a result return type.
    
    By using a new field, we should be able to branch off of the
    "if param_response_list == None" path to generate code to specifically
    handle result returns.
    
    Bug: 40841428
    Change-Id: I8cfedba118e1745bc208e5ea5a42df1e4d5b3564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298560
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424760}

---
+++ mojo/public/tools/mojom/result_unittest.py
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from mojom_parser_test_case import MojomParserTestCase
+from mojom.generate import module as mojom
+
+
+class ResultTest(MojomParserTestCase):
+  """Tests result<T,E> parsing behavior."""
+
+  def testResultResponse(self):
+    a_mojom = 'a.mojom'
+    self.WriteFile(
+        a_mojom, """
+        interface Test {
+            Method() => result<bool, string>;
+        };
+    """)
+    self.ParseMojoms([a_mojom])
+    a = self.LoadModule(a_mojom)
+    self.assertEqual(1, len(a.interfaces))
+    self.assertEqual(1, len(a.interfaces[0].methods))
+    self.assertEqual('b', a.interfaces[0].methods[0].result.success_type)
+    self.assertEqual('s', a.interfaces[0].methods[0].result.failure_type)
+
+  def testResultResponse(self):
+    a_mojom = 'a.mojom'
+    self.WriteFile(
+        a_mojom, """
+        struct Success {};
+        struct Failure {};
+
+        interface Test {
+            Method() => result<Success, Failure>;
+        };
+    """)
+    self.ParseMojoms([a_mojom])
+    a = self.LoadModule(a_mojom)
+    self.assertEqual(1, len(a.interfaces))
+    self.assertEqual(1, len(a.interfaces[0].methods))
+    self.assertEqual('x:Success',
+                     a.interfaces[0].methods[0].result.success_type)
+    self.assertEqual('x:Failure',
+                     a.interfaces[0].methods[0].result.failure_type)
