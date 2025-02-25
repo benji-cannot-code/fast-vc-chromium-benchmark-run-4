a3d6e86aadd1965ae1b40f89af3de4eeca8acd70
James Cook <jamescook@chromium.org>
2025-02-24 18:07:55 -0800

    extensions: Add ExtensionInstallPrompt tests to desktop Android
    ExtensionInstallPrompt was recently added to the desktop Android build.
    Add some of its unit and browser tests as well.
    
    This requires adding a couple of utility methods to
    ExtensionPlatformBrowserTest dealing with tabs / web contents.
    A small test for these methods is added as well.
    
    Bug: 396722906, 397754565
    Change-Id: Idf3a6b197cad13bf44caadfdf1c1a1eeb9f9183b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287966
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424266}

--- chrome/browser/extensions/extension_install_prompt_unittest.cc
+++ chrome/browser/extensions/extension_install_prompt_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_install_prompt_show_params.h"
-#include "chrome/browser/extensions/extension_service_test_with_install.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_task_environment.h"
@@ -40,16 +39,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/skia_util.h"
 
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/extensions/extension_service_test_with_install.h"
+#endif
+
 namespace extensions {
 
 namespace {
 
+#if !BUILDFLAG(IS_ANDROID)
 void SetImage(gfx::Image* image_out,
               base::OnceClosure quit_closure,
               const gfx::Image& image_in) {
   *image_out = image_in;
   std::move(quit_closure).Run();
 }
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 class ExtensionInstallPromptUnitTest : public testing::Test {
  public:
@@ -113,6 +118,10 @@ TEST_F(ExtensionInstallPromptUnitTest, PromptShowsPermissionWarnings) {
   ASSERT_TRUE(install_prompt.get());
   EXPECT_EQ(1u, install_prompt->GetPermissionCount());
 }
+
+// TODO(crbug.com/397973212): Enable these tests when
+// ExtensionServiceTestWithInstall is supported on Android.
+#if !BUILDFLAG(IS_ANDROID)
 
 using ExtensionInstallPromptTestWithService = ExtensionServiceTestWithInstall;
 
@@ -233,5 +242,5 @@ TEST_F(ExtensionInstallPromptTestWithholdingAllowed,
   auto [params, done_callback, install_prompt] = show_dialog_future.Take();
   EXPECT_EQ(install_prompt->ShouldWithheldPermissionsOnDialogAccept(), false);
 }
-
+#endif  // !BUILDFLAG(IS_ANDROID)
 }  // namespace extensions
