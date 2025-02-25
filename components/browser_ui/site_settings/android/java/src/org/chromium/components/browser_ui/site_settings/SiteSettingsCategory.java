41bb04550120b9cb29c9aa8cd2e19b6cfbda81a0
Alexander Cooper <alcooper@chromium.org>
2025-02-25 08:34:11 -0800

    Update XR_IMMERSIVE feature checks
    The android.software.xr.immersive feature is deprecated. This updates
    checks of that feature string to temporarily check for both itself and
    it's replacement (android.software.xr.api.openxr).
    
    Bug: 398275913
    Change-Id: I050c3e40b8cecfabd342dbada840453627a9f2c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6292209
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424568}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
@@ -25,7 +25,6 @@ import androidx.annotation.IntDef;
 import androidx.preference.Preference;
 
 import org.chromium.base.ApiCompatibilityUtils;
-import org.chromium.base.PackageManagerUtils;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
@@ -164,8 +163,7 @@ public class SiteSettingsCategory {
         } else if (type == Type.AUGMENTED_REALITY) {
             permission = android.Manifest.permission.CAMERA;
         } else if (type == Type.HAND_TRACKING
-                && PackageManagerUtils.hasSystemFeature(
-                        PackageManagerUtils.XR_IMMERSIVE_FEATURE_NAME)) {
+                && PermissionUtil.handTrackingNeedsAdditionalPermissions()) {
             permission = PermissionUtil.ANDROID_PERMISSION_HAND_TRACKING;
         } else {
             permission = "";
