e641049c77f130d37c4740b05a6f662cdf05ab1f
Takuto Ikuta <tikuta@chromium.org>
2025-02-25 05:03:30 -0800

    components: fix include for modules build
    This is to fix build error when we set use_libcxx_modules=true.
    
    Bug: 40263312
    Change-Id: Iecddbb51b371bb4e93f4fb9710e30d0e2b6772ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6301372
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424456}

--- components/autofill/core/browser/webdata/passes/passes_table.h
+++ components/autofill/core/browser/webdata/passes/passes_table.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_PASSES_PASSES_TABLE_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_PASSES_PASSES_TABLE_H_
 
+#include <optional>
 #include <string_view>
 #include <vector>
 
