2a1b553e5bd2827aab2a753ac7ba7d37f9cb1c93
Xianzhu Wang <wangxianzhu@chromium.org>
2025-02-25 15:51:28 -0800

    [ExpandCompositedCullRect] changed_enough parameter
    Recent UMA metrics data seem to indicate that a larger
    ChangedEnoughMinimumDistance for cull rects can lead to better
    scroll performance. This CL prepares to experiment it.
    
    Bug: 361344221
    Change-Id: I65c9ab529d8bea14eff872cfe31c33fb2a943b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6302448
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424851}

--- third_party/blink/renderer/platform/testing/paint_test_configurations.h
+++ third_party/blink/renderer/platform/testing/paint_test_configurations.h
@@ -23,7 +23,8 @@ class CullRectTestConfig {
  public:
   CullRectTestConfig() {
     feature_.InitAndEnableFeatureWithParameters(
-        features::kExpandCompositedCullRect, {{"pixels", "4000"}});
+        features::kExpandCompositedCullRect,
+        {{"pixels", "4000"}, {"changed_enough", "512"}});
   }
 
  private:
