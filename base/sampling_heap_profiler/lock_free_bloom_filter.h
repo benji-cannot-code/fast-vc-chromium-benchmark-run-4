0770284123b64ed9c4a3756e53211216a671eaa1
Takuto Ikuta <tikuta@chromium.org>
2025-02-25 15:45:32 -0800

    base: fix include for modules build
    This is to fix build error when we set use_libcxx_modules=true.
    
    Bug: 40263312
    Change-Id: I8d2b0236c42b0c7dc2a30112056c04804ccc79e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298910
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424846}

--- base/sampling_heap_profiler/lock_free_bloom_filter.h
+++ base/sampling_heap_profiler/lock_free_bloom_filter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_SAMPLING_HEAP_PROFILER_LOCK_FREE_BLOOM_FILTER_H_
 #define BASE_SAMPLING_HEAP_PROFILER_LOCK_FREE_BLOOM_FILTER_H_
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include <atomic>
