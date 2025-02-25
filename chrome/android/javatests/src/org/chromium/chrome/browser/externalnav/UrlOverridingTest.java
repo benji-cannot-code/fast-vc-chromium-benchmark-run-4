eb9ab5d197a09655040875e16302b9fa414e8b25
Nafis Abedin <nafisabedin@google.com>
2025-02-25 08:56:05 -0800

    [External Navigation] Add histogram for redirects
    This CL adds a histogram recording whether Chrome was launched for the
    purpose of acting as an intermediate redirector between two external
    apps.
    
    Bug: 382748196
    Change-Id: I429974a884214c757d77a18d43ffcd97e9d6fe84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300230
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424581}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -66,6 +66,7 @@ import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.PackageManagerWrapper;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.blink_public.common.BlinkFeatures;
@@ -215,6 +216,8 @@ public class UrlOverridingTest {
     private static final String TRUSTED_CCT_PACKAGE = "com.trusted.cct";
 
     private static final String EXTERNAL_APP_SCHEME = "externalappscheme";
+
+    private static final String TAB_OPENED_FOR_REDIRECT_HISTOGRAM = "Android.TabOpenedForRedirect";
 
     @IntDef({NavigationType.SELF, NavigationType.BLANK, NavigationType.TOP})
     @Retention(RetentionPolicy.SOURCE)
@@ -1034,6 +1037,8 @@ public class UrlOverridingTest {
     @Test
     @SmallTest
     public void testRedirectionFromIntentWarm() throws Exception {
+        HistogramWatcher redirectWatcher =
+                HistogramWatcher.newSingleRecordWatcher(TAB_OPENED_FOR_REDIRECT_HISTOGRAM, true);
         Context context = ContextUtils.getApplicationContext();
         mActivityTestRule.startMainActivityOnBlankPage();
 
@@ -1053,6 +1058,7 @@ public class UrlOverridingTest {
                 },
                 10000L,
                 CriteriaHelper.DEFAULT_POLLING_INTERVAL);
+        redirectWatcher.assertExpected();
         CriteriaHelper.pollUiThread(
                 () -> AsyncInitializationActivity.wasMoveTaskToBackInterceptedForTesting());
     }
