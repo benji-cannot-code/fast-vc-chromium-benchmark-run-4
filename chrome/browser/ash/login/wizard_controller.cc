d13cdd1be064689c0c8e512bcca408f66edcb26b
Danila Kuzmin <dkuzmin@google.com>
2025-02-25 07:21:41 -0800

    [oobe] Fix GAIA page native navigation for enrollment
     - Make secondary button appearance fully controlled by GAIA.
     - Refactor primary action button update on the client side.
    
    Old behavior: http://shortn/_SXbJ41IxSw
    New behavior: http://shortn/_WPEi1DL0jY
    
    Bug: 392653569
    Change-Id: Iefbdafc1932b0b93834c9ac6a2f8050209f36eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299049
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424524}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -1448,6 +1448,8 @@ void WizardController::OnUserCreationScreenExit(
       break;
     case UserCreationScreen::Result::ENTERPRISE_ENROLL_TRIAGE:
     case UserCreationScreen::Result::ENTERPRISE_ENROLL_SHORTCUT:
+      wizard_context_->enrollment_preference_ =
+          WizardContext::EnrollmentPreference::kEnterprise;
       ShowEnrollmentScreenIfEligible();
       break;
     case UserCreationScreen::Result::KIOSK_ENTERPRISE_ENROLL:
