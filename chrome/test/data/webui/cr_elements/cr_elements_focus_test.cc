ae0aa383f41a748faeb84467feb327415695e686
rbpotter <rbpotter@chromium.org>
2025-02-25 10:18:57 -0800

    WebUI: Add cr_elements_focus_tests on Android builds
    Specifically running the CrTabs test, since cr-tabs is included on
    Android builds.
    
    Bug: 40108641
    Change-Id: Ia157cd35e8a0b0536415319588f7b097acadd27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6293805
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424625}

--- chrome/test/data/webui/cr_elements/cr_elements_focus_test.cc
+++ chrome/test/data/webui/cr_elements/cr_elements_focus_test.cc
@@ -8,6 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef WebUIMochaFocusTest CrElementsFocusTest;
 
+IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, CrTabs) {
+  RunTest("cr_elements/cr_tabs_test.js", "mocha.run()");
+}
+
+#if !BUILDFLAG(IS_ANDROID)
 IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, CrActionMenu) {
   RunTest("cr_elements/cr_action_menu_test.js", "mocha.run()");
 }
@@ -33,10 +38,6 @@ IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, MAYBE_CrInput) {
 
 IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, CrProfileAvatarSelector) {
   RunTest("cr_elements/cr_profile_avatar_selector_test.js", "mocha.run()");
-}
-
-IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, CrTabs) {
-  RunTest("cr_elements/cr_tabs_test.js", "mocha.run()");
 }
 
 IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, CrToggle) {
@@ -74,3 +75,4 @@ IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, CrFocusRowMixin) {
 IN_PROC_BROWSER_TEST_F(CrElementsFocusTest, FocusRowMixinLit) {
   RunTest("cr_elements/focus_row_mixin_lit_test.js", "mocha.run()");
 }
+#endif
