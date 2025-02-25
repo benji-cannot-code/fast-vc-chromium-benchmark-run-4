402ab288f1466864543c19739b849de7727783db
Will Yeager <wyeager@google.com>
2025-02-25 10:26:21 -0800

    Revert "Replace show setting button with disable button in Mini Map"
    This reverts commit 2148cdb1907192b048842d67fb8d899deec02cbb.
    
    Reason for revert: This is missing chrome branded strings and is causing official builds to fail to compile.
    
    Original change's description:
    > Replace show setting button with disable button in Mini Map
    >
    > To make it easy for user to disable the feature if a site is broken add
    > a disable button directly in the minimap screen (replacing the settings
    > button).
    >
    > Bug: 398881839
    > Change-Id: I395d2897c91ba41790f8c303891e2ad30a166ca7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299508
    > Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    > Reviewed-by: Quentin Pubert <qpubert@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1424442}
    
    Bug: 398881839
    Change-Id: I96fa4f774473e3d731999507264a16eb5a81eb9d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297639
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424638}

--- ios/chrome/browser/mini_map/ui_bundled/mini_map_coordinator.mm
+++ ios/chrome/browser/mini_map/ui_bundled/mini_map_coordinator.mm
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/mini_map_commands.h"
 #import "ios/chrome/browser/shared/public/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/shared/public/commands/settings_commands.h"
-#import "ios/chrome/browser/shared/public/commands/snackbar_commands.h"
 #import "ios/chrome/browser/web/model/annotations/annotations_util.h"
 #import "ios/chrome/common/string_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -179,11 +178,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.miniMapController
       configureFooterWithTitle:l10n_util::GetNSString(
                                    IDS_IOS_MINI_MAP_FOOTER_STRING)
-      leadingButtonTitle:l10n_util::GetNSString(IDS_IOS_MINI_MAP_DISABLE_STRING)
+      leadingButtonTitle:l10n_util::GetNSString(IDS_IOS_CONTENT_SETTINGS_TITLE)
       trailingButtonTitle:l10n_util::GetNSString(
                               IDS_IOS_OPTIONS_REPORT_AN_ISSUE)
       leadingButtonAction:^(UIViewController* viewController) {
-        [weakSelf disableOneTapMinimapFromViewController:viewController];
+        [weakSelf
+            showContentSettingsFromMiniMapInViewController:viewController];
       }
       trailingButtonAction:^(UIViewController* viewController) {
         [weakSelf reportAnIssueFromMiniMapInViewController:viewController];
@@ -237,36 +237,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       self.browser->GetCommandDispatcher(), SettingsCommands);
   [settingsCommandHandler
       showContentsSettingsFromViewController:viewController];
-}
-
-- (void)disableOneTapMinimapFromViewController:
-    (UIViewController*)viewController {
-  [self.mediator userDisabledSettingFromMiniMap];
-
-  [viewController.presentingViewController dismissViewControllerAnimated:YES
-                                                              completion:nil];
-  id<SnackbarCommands> snackbarCommandHandler = HandlerForProtocol(
-      self.browser->GetCommandDispatcher(), SnackbarCommands);
-  __weak __typeof(self) weakSelf = self;
-  [snackbarCommandHandler
-      showSnackbarWithMessage:l10n_util::GetNSString(
-                                  IDS_IOS_MINI_MAP_DISABLE_CONFIRMATION_STRING)
-      buttonText:l10n_util::GetNSString(
-                     IDS_IOS_MINI_MAP_DISABLE_CONFIRMATION_BUTTON_STRING)
-      messageAction:^{
-        [weakSelf userOpenedSettingsFromConfirmation];
-      }
-      completionAction:^(BOOL) {
-        [weakSelf workflowEnded];
-      }];
-}
-
-- (void)userOpenedSettingsFromConfirmation {
-  [self.mediator userOpenedSettingsFromDisableConfirmation];
-  id<SettingsCommands> settingsCommandHandler = HandlerForProtocol(
-      self.browser->GetCommandDispatcher(), SettingsCommands);
-  [settingsCommandHandler
-      showContentsSettingsFromViewController:self.baseViewController];
 }
 
 - (void)reportAnIssueFromMiniMapInViewController:
