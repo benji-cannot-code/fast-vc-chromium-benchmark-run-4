cb4dd84709369a71682f1f45fbbdaa65a5af4304
Salvador Guerrero <salg@google.com>
2025-02-24 16:10:52 -0800

    Added menu item for summarization feature
    This CL adds a menu item for the experimental summarization feature.
    This feature was already available as a toolbar shortcut, its icon and
    strings are placeholders.
    
    Additionally it modifies AiAssistantService to fall back to sending an
    intent whenever a feature isn't available in the system provider.
    
    Bug: 397527693
    Change-Id: I5b5cf8e51557dda559b48b5b32b17d74e716a9f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279010
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424217}

--- chrome/android/junit/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegateUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegateUnitTest.java
@@ -36,6 +36,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.build.BuildConfig;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ActivityTabProvider;
@@ -48,6 +49,7 @@ import org.chromium.chrome.browser.feed.FeedFeatures;
 import org.chromium.chrome.browser.feed.webfeed.WebFeedBridge;
 import org.chromium.chrome.browser.feed.webfeed.WebFeedBridgeJni;
 import org.chromium.chrome.browser.feed.webfeed.WebFeedSnackbarController;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.incognito.reauth.IncognitoReauthController;
 import org.chromium.chrome.browser.layouts.LayoutStateProvider;
 import org.chromium.chrome.browser.layouts.LayoutType;
@@ -94,6 +96,7 @@ import java.util.List;
 
 /** Unit tests for {@link TabbedAppMenuPropertiesDelegate}. */
 @RunWith(BaseRobolectricTestRunner.class)
+@DisableFeatures(ChromeFeatureList.ADAPTIVE_BUTTON_IN_TOP_TOOLBAR_PAGE_SUMMARY)
 public class TabbedAppMenuPropertiesDelegateUnitTest {
     // Constants defining flags that determines multi-window menu items visibility.
     private static final boolean TAB_M = true; // multiple tabs
