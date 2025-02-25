0ce4e186e339d4c4c51bbf903201b78bc94f63ce
Mark Mentovai <mark@chromium.org>
2025-02-24 17:52:54 -0800

    Update Crashpad to 0945d596298764b15f02c217e38add179d5afc5c
    312919d97f61 Remove traces of third_party/gyp
    4a227b2d7c8d Add a newline to scoped_registry_key.h
    85ecbd2a772c Fix SyntaxWarning in build/run_tests.py
    db25cdc03050 Remove a NOTREACHED() after a __builtin_trap()
    d7aeda9fca8b Prepare for building with clang
    73b3e4f25871 Enable clang by default on Windows
    e2acb9f3324c Fix iOS test when run on arm64 under x86_64 translation
    0945d5962987 Update cpp-httplib to 71ba7e7b1b32 (0.19.0 + 22 commits)
    
    Change-Id: I97f8c7c98f7d4ebf4cdd5117009bced6124c3cb0
    Bug: 374516744, 384682775, 393924694, 384682775, 384682775, 398836269, 398273506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300017
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Auto-Submit: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424259}

--- third_party/crashpad/crashpad/client/crashpad_client_linux_test.cc
+++ third_party/crashpad/crashpad/client/crashpad_client_linux_test.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 
 #include "base/check_op.h"
-#include "base/notreached.h"
 #include "build/build_config.h"
 #include "client/annotation.h"
 #include "client/annotation_list.h"
@@ -765,8 +764,6 @@ class StartHandlerForChildTest : public Multiprocess {
     CHECK(test_state_.InstallHandler());
 
     __builtin_trap();
-
-    NOTREACHED();
   }
 
   StartHandlerForClientTest test_state_;
