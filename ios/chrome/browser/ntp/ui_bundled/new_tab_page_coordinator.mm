fa6ae0ca6bacdea5b2b4ea1fcee304efd0eb599a
Jérôme Lebel <jlebel@chromium.org>
2025-02-25 10:42:47 -0800

    [iOS] Remove unused method in NewTabPageCoordinator
    -[NewTabPageCoordinator accountMenuCoordinatorIsStopped] is not
    used. This method can be removed.
    
    Change-Id: Icf43866be21bbdde55782b80fcd0f65c053f5739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298392
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424648}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -1611,12 +1611,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
-// Update the state, to take into account that the menu coordinator is stopped.
-- (void)accountMenuCoordinatorIsStopped {
-  CHECK(_showAccountMenuInProgress);
-  _showAccountMenuInProgress = NO;
-}
-
 // Update the state, to take into account that the account menu coordinator is
 // stopped.
 - (void)showAccountMenuDidFinish {
