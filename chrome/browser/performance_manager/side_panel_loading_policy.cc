db7b6ac55f16f4fb215fdbb04de8eba1acbb668c
Patrick Monette <pmonette@chromium.org>
2025-02-25 09:49:24 -0800

    [PM] Migrate uses of CallOnGraph/RunInGraph
    The performance manager now lives on the UI thread, making
    these functions obsolete.
    
    The graph can be directly accessed through the
    PerformanceManager::GetGraph() function.
    
    This CL was uploaded by git cl split.
    
    R=joenotcharles@google.com
    
    Bug: 40755583
    Change-Id: I9a76d4924a81fc795f7d5bb1a3411d2a278a96b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299208
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424609}

--- chrome/browser/performance_manager/side_panel_loading_policy.cc
+++ chrome/browser/performance_manager/side_panel_loading_policy.cc
@@ -14,18 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace performance_manager::execution_context_priority {
 
 void MarkAsSidePanel(content::WebContents* web_contents) {
-  PerformanceManager::CallOnGraph(
-      FROM_HERE,
-      base::BindOnce(
-          [](base::WeakPtr<PageNode> page_node, Graph* graph) {
-            CHECK(page_node);
-            auto* voter = graph->GetRegisteredObjectAs<
-                execution_context_priority::SidePanelLoadingVoter>();
-            CHECK(voter);
+  base::WeakPtr<PageNode> page_node =
+      PerformanceManager::GetPrimaryPageNodeForWebContents(web_contents);
+  CHECK(page_node);
 
-            voter->MarkAsSidePanel(page_node.get());
-          },
-          PerformanceManager::GetPrimaryPageNodeForWebContents(web_contents)));
+  Graph* graph = PerformanceManager::GetGraph();
+  auto* voter = graph->GetRegisteredObjectAs<
+      execution_context_priority::SidePanelLoadingVoter>();
+  CHECK(voter);
+
+  voter->MarkAsSidePanel(page_node.get());
 }
 
 }  // namespace performance_manager::execution_context_priority
