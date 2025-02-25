756a0286a0ddf5a96b4464f90a1c0f66b244f8f7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-02-24 21:22:37 -0800

    Import wpt@941cd9eea676ed3cef42682f681e1c7b178a1d06
    https://github.com/web-platform-tests/wpt/compare/240b35734...941cd9eea
    
    Using wpt-import in Chromium 19f527ad0db1e979f898d4c26d17f9ede3fcb5a6.
    With Chromium commits locally applied on WPT:
      c4eb35d458 "bluetooth: Migrate some WPT tests to use bidi commands"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    meade@chromium.org:
      external/wpt/web-animations
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Iaccef47f3c1070f1291d9f239393320973022336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299499
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1424310}

--- third_party/blink/web_tests/external/wpt/web-animations/animation-model/animation-types/property-list.js
+++ third_party/blink/web_tests/external/wpt/web-animations/animation-model/animation-types/property-list.js
@@ -26,6 +26,12 @@ const gCSSProperties1 = {
       { type: 'discrete', options: [ [ 'flex-start', 'flex-end' ] ] }
     ]
   },
+  'anchor-scope': {
+    // https://drafts.csswg.org/css-anchor-position-1/#anchor-scope
+    types: [
+      { type: 'discrete', options: [ [ 'none', 'all' ] ] }
+    ]
+  },
   'appearance': {
     // https://drafts.csswg.org/css-ui/#appearance-switching
     types: [
@@ -570,6 +576,10 @@ const gCSSProperties1 = {
     types: [
       { type: 'discrete', options: [ [ 'italic', 'oblique' ] ] }
     ]
+  },
+  'font-width': {
+    // https://drafts.csswg.org/css-fonts-4/#propdef-font-width
+    types: [ 'percentage' ]
   },
   'float': {
     // https://drafts.csswg.org/css-page-floats/#propdef-float
