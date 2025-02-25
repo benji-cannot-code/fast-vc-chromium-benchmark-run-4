ba5593ee527e70b7461c845556fd92dc5391783d
Nina Satragno <nsatragno@chromium.org>
2025-02-25 14:27:20 -0800

    [webauthn] Increase priority of TPM check
    Chrome tests for the existence of a TPM on a dedicated thread since the
    operation may result in disk access. Since Chrome blocks showing any UI
    on this check finishing, increase the thread priority to USER_BLOCKING.
    This results in a speedup from ~1700ms to ~1ms on my development
    machine. Add a FIDO log entry to measure how long the check takes.
    
    This might help some users who are seeing very long times for any UI to
    pop up, but that stop experiencing the bug when GPM is disabled.
    
    Bug: 396607267
    Change-Id: I326812b8e086398453fe794306fa8a04341eefbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300761
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424806}

--- chrome/browser/webauthn/chrome_web_authentication_delegate.cc
+++ chrome/browser/webauthn/chrome_web_authentication_delegate.cc
@@ -582,8 +582,9 @@ void ChromeWebAuthenticationDelegate::BrowserProvidedPasskeysAvailable(
     std::move(callback).Run(*tpm_available_);
     return;
   }
+  base::Time tpm_check_start_time = base::Time::Now();
   base::ThreadPool::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::TaskPriority::BEST_EFFORT, base::MayBlock()},
+      FROM_HERE, {base::TaskPriority::USER_BLOCKING, base::MayBlock()},
       base::BindOnce([]() -> bool {
         std::unique_ptr<crypto::UnexportableKeyProvider> provider =
             GetWebAuthnUnexportableKeyProvider();
@@ -597,8 +598,11 @@ void ChromeWebAuthenticationDelegate::BrowserProvidedPasskeysAvailable(
       }),
       base::BindOnce(
           [](base::OnceCallback<void(bool)> callback,
+             base::Time tpm_check_start_time,
              base::WeakPtr<ChromeWebAuthenticationDelegate> webauthn_delegate,
              bool available) {
+            FIDO_LOG(DEBUG) << "Checking for TPM availability took "
+                            << (base::Time::Now() - tpm_check_start_time);
             if (webauthn_delegate) {
               webauthn_delegate->tpm_available_ = available;
             }
@@ -608,5 +612,6 @@ void ChromeWebAuthenticationDelegate::BrowserProvidedPasskeysAvailable(
             }
             std::move(callback).Run(available);
           },
-          std::move(callback), weak_ptr_factory_.GetWeakPtr()));
+          std::move(callback), tpm_check_start_time,
+          weak_ptr_factory_.GetWeakPtr()));
 }
