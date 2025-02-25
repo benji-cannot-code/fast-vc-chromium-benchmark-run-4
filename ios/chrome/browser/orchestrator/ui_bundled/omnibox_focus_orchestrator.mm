d79c45b74a6a8212f9ba1fd8f147f2f880b65166
ginnnnnnny <ginnyhuang@chromium.org>
2025-02-25 11:20:29 -0800

    [iOS][evolve-disco-header] Large fakebox animation fix
    When the large fakebox is focused to handle user input, it should be
    shrunken to the normal size. Currently it's done without an animation;
    this CL fixes that so the transition between omnibox sizes is smooth.
    
    Bug: 359325090
    Change-Id: Ib8de4dab34687d2519ccce1e6fa984fbe5af95f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299699
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424676}

--- ios/chrome/browser/orchestrator/ui_bundled/omnibox_focus_orchestrator.mm
+++ ios/chrome/browser/orchestrator/ui_bundled/omnibox_focus_orchestrator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/ios/ios_util.h"
+#import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.h"
 #import "ios/chrome/browser/orchestrator/ui_bundled/edit_view_animatee.h"
 #import "ios/chrome/browser/orchestrator/ui_bundled/location_bar_animatee.h"
 #import "ios/chrome/browser/orchestrator/ui_bundled/toolbar_animatee.h"
@@ -302,8 +303,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Use UIView animateWithDuration instead of UIViewPropertyAnimator to
     // avoid UIKit bug. See https://crbug.com/856155.
     self.inProgressAnimationCount += 1;
+    if (ShouldEnlargeLogoAndFakebox()) {
+      // Set the location bar height to the default.
+      [self.toolbarAnimatee setLocationBarHeightExpanded];
+    }
     [self.toolbarAnimatee setToolbarFaded:NO];
     switch (_trigger) {
+      case OmniboxFocusTrigger::kPinnedFakebox:
+        if (ShouldEnlargeLogoAndFakebox()) {
+          [self.toolbarAnimatee setLocationBarHeightToMatchFakeOmnibox];
+        }
+        break;
       case OmniboxFocusTrigger::kUnpinnedFakebox:
         [self.toolbarAnimatee setToolbarFaded:YES];
         break;
@@ -418,6 +428,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else if (_completion) {
     _completion();
     _completion = nil;
+    if (ShouldEnlargeLogoAndFakebox()) {
+      // Reset the location bar height back to the default.
+      [self.toolbarAnimatee setLocationBarHeightExpanded];
+    }
   }
   [self.locationBarAnimatee clearFakeboxButtonsSnapshot];
   self.stateChangedDuringAnimation = NO;
@@ -430,6 +444,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.toolbarAnimatee expandLocationBar];
   [self.toolbarAnimatee showCancelButton];
   switch (_trigger) {
+    case OmniboxFocusTrigger::kPinnedFakebox:
+      if (ShouldEnlargeLogoAndFakebox()) {
+        [self.toolbarAnimatee setLocationBarHeightExpanded];
+      }
+      break;
     case OmniboxFocusTrigger::kUnpinnedFakebox:
       [self.toolbarAnimatee setToolbarFaded:NO];
       break;
@@ -441,6 +460,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Visually contracts the location bar for defocus.
 - (void)contraction {
   [self.toolbarAnimatee contractLocationBar];
+  if (_trigger == OmniboxFocusTrigger::kPinnedFakebox &&
+      ShouldEnlargeLogoAndFakebox()) {
+    [self.toolbarAnimatee setLocationBarHeightToMatchFakeOmnibox];
+  }
 }
 
 // Returns YES if the focus event was triggered by the NTP Fakebox in its
