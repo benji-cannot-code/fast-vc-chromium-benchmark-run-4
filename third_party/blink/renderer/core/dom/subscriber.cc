fb398dde2cfa57fae32be30966bb1f855b0ceaa2
David Sanders <dsanders11@ucsbalum.com>
2025-02-25 11:50:34 -0800

    Remove unused include in to_v8_traits.h
    Also adds missing includes to other files as needed
    to fix the build.
    
    Bug: 40318405
    Change-Id: Ifd37c3f10d813cd236aa5ac54ccb102a31db865c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6295188
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424702}

--- third_party/blink/renderer/core/dom/subscriber.cc
+++ third_party/blink/renderer/core/dom/subscriber.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/subscriber.h"
 
 #include "base/containers/adapters.h"
+#include "base/containers/contains.h"
 #include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_observer.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_observer_callback.h"
--- third_party/blink/renderer/modules/xr/xr_view.cc
+++ third_party/blink/renderer/modules/xr/xr_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cmath>
 
+#include "base/containers/contains.h"
 #include "base/trace_event/trace_event.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_xr_eye.h"
 #include "third_party/blink/renderer/modules/xr/xr_camera.h"
