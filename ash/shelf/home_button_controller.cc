7993d3592ce0296e66d98b95b14224ab3c1b191a
Michael Cui <mlcui@google.com>
2025-02-25 16:16:58 -0800

    shelf: Remove unused `show_icon_` in `AssistantOverlay`
    Bug: b:391909810
    Change-Id: I6a6a2100c9ed3420dc0587c1695415584bf4d9dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298337
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424870}

--- ash/shelf/home_button_controller.cc
+++ ash/shelf/home_button_controller.cc
@@ -152,7 +152,7 @@ void HomeButtonController::OnUiVisibilityChanged(
 }
 
 void HomeButtonController::StartAssistantAnimation() {
-  assistant_overlay_->StartAnimation(false);
+  assistant_overlay_->StartAnimation();
 }
 
 void HomeButtonController::OnAppListShown() {
