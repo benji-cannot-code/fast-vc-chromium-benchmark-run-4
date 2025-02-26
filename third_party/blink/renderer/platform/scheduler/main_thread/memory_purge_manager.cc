5631a7568aebfeb9571ba1b57c6ae9dcbed39ecc
Thiabaud Engelbrecht <thiabaud@google.com>
2025-02-26 10:13:26 -0800

    [self_freeze] Add metrics for cancellation reason
    This CL adds a metric to keep track of the reason why a self compaction
    was cancelled.
    
    Bug: 344550156
    Change-Id: Ia59ad8bbf4b80c426c8c676c7b30bd27e37ef92a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6232928
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425242}

--- third_party/blink/renderer/platform/scheduler/main_thread/memory_purge_manager.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/memory_purge_manager.cc
@@ -110,7 +110,9 @@ void MemoryPurgeManager::OnPageResumed() {
 #if BUILDFLAG(IS_ANDROID)
   // Cancel a pending self compaction, since we are resuming now, and will
   // presumably touch most of that memory soon.
-  base::android::PreFreezeBackgroundMemoryTrimmer::MaybeCancelSelfCompaction();
+  base::android::PreFreezeBackgroundMemoryTrimmer::MaybeCancelSelfCompaction(
+      base::android::PreFreezeBackgroundMemoryTrimmer::
+          SelfCompactCancellationReason::kPageResumed);
 #endif
 }
 
