e4f1bb075d81ead196da42e3db9fdd8eb82f02aa
Steve Kobes <skobes@chromium.org>
2025-02-26 06:17:44 -0800

    Construct ViewTransition object for element.
    Bug: 394052227
    Change-Id: If460a9ee062ebc5418cab9677adf0cc4f7d361e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287532
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425103}

--- third_party/blink/renderer/core/view_transition/scoped_view_transition.cc
+++ third_party/blink/renderer/core/view_transition/scoped_view_transition.cc
@@ -5,31 +5,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/view_transition/scoped_view_transition.h"
 
+#include "third_party/blink/renderer/core/view_transition/view_transition_supplement.h"
+
 namespace blink {
 
 DOMViewTransition* ScopedViewTransition::startViewTransition(
-    ScriptState*,
-    Element&,
+    ScriptState* script_state,
+    Element& element,
     V8ViewTransitionCallback* callback,
-    ExceptionState&) {
-  // TODO(crbug.com/391146469): implement.
-  return nullptr;
+    ExceptionState& exception_state) {
+  return ViewTransitionSupplement::StartViewTransitionForElement(
+      script_state, &element, callback, std::nullopt, exception_state);
 }
 
 DOMViewTransition* ScopedViewTransition::startViewTransition(
-    ScriptState*,
-    Element&,
+    ScriptState* script_state,
+    Element& element,
     ViewTransitionOptions* options,
-    ExceptionState&) {
-  // TODO(crbug.com/391146469): implement.
-  return nullptr;
+    ExceptionState& exception_state) {
+  // The generated bindings class for view_transition_options.idl ensures that
+  // hasUpdate() and hasTypes() both return true.
+  CHECK(!options || (options->hasUpdate() && options->hasTypes()));
+  return ViewTransitionSupplement::StartViewTransitionForElement(
+      script_state, &element, options ? options->update() : nullptr,
+      options ? options->types() : std::nullopt, exception_state);
 }
 
-DOMViewTransition* ScopedViewTransition::startViewTransition(ScriptState*,
-                                                             Element&,
-                                                             ExceptionState&) {
-  // TODO(crbug.com/391146469): implement.
-  return nullptr;
+DOMViewTransition* ScopedViewTransition::startViewTransition(
+    ScriptState* script_state,
+    Element& element,
+    ExceptionState& exception_state) {
+  return ViewTransitionSupplement::StartViewTransitionForElement(
+      script_state, &element, nullptr, std::nullopt, exception_state);
 }
 
 }  // namespace blink
