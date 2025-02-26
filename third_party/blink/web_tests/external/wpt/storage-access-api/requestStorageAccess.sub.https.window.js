0449766ded987be9db4334913ebfc28b174108c5
Chris Fredrickson <cfredric@chromium.org>
2025-02-26 10:42:39 -0800

    [SAA] Remove permission state manipulation from autogrant WPTs
    The tests that rely on autogrants don't need to override permission
    states, and the "denied" test is nonsense since that state is impossible
    (due to the autogrants).
    
    Change-Id: Ifa9da7bb9a938ec218348d60426daf9c4c35a8ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297532
    Commit-Queue: Sandor «Alex» Major <sandormajor@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Sandor «Alex» Major <sandormajor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425270}

--- third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess.sub.https.window.js
+++ third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess.sub.https.window.js
@@ -14,6 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // don't want to recurse forever.
 const {testPrefix, topLevelDocument} = processQueryParams();
 
+const canUseAutogrant = topLevelDocument ||
+          testPrefix.includes('same-site') ||
+          testPrefix.includes('same-origin') ||
+          testPrefix.includes('cross-origin') ||
+          testPrefix.includes('ABA');
+
 if (!topLevelDocument) {
   // WPT synthesizes a top-level HTML test for this JS file, and in that case we
   // don't want to, or need to, call set_test_context.
@@ -27,14 +33,15 @@ promise_test(async () => {
 
 // Most tests need to start with the feature in "prompt" state.
 async function CommonSetup() {
-  await test_driver.set_permission({ name: 'storage-access' }, 'prompt');
+  if (!canUseAutogrant) {
+    await test_driver.set_permission({ name: 'storage-access' }, 'prompt');
+  }
 }
 
 promise_test(
     async t => {
       await CommonSetup();
-      if (topLevelDocument || !testPrefix.includes('cross-site') ||
-          testPrefix.includes('ABA')) {
+      if (canUseAutogrant) {
         await document.requestStorageAccess().catch(t.unreached_func(
             'document.requestStorageAccess() call should resolve in top-level frame or same-site iframe.'));
 
@@ -67,7 +74,7 @@ promise_test(
         '] document.requestStorageAccess() should be resolved with no user gesture when a permission grant exists, and ' +
         'should allow cookie access');
 
-if (testPrefix.includes('cross-site')) {
+if (!canUseAutogrant) {
   promise_test(
       async t => {
         await test_driver.set_permission(
@@ -83,23 +90,10 @@ if (testPrefix.includes('cross-site')) {
 } else {
   promise_test(
       async () => {
-        await CommonSetup();
         await document.requestStorageAccess();
 
         assert_true(await CanAccessCookiesViaHTTP(), 'After obtaining storage access, subresource requests from the frame should send and set cookies.');
         assert_true(CanAccessCookiesViaJS(), 'After obtaining storage access, scripts in the frame should be able to access cookies.');
       },
       `[${testPrefix}] document.requestStorageAccess() should resolve without permission grant or user gesture`);
-
-  promise_test(
-      async () => {
-        await test_driver.set_permission(
-            {name: 'storage-access'}, 'denied');
-
-        await document.requestStorageAccess();
-
-        assert_true(await CanAccessCookiesViaHTTP(), 'After obtaining storage access, subresource requests from the frame should send and set cookies.');
-        assert_true(CanAccessCookiesViaJS(), 'After obtaining storage access, scripts in the frame should be able to access cookies.');
-      },
-      `[${testPrefix}] document.requestStorageAccess() should resolve with denied permission`);
 }
