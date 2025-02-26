2ddcf28855a352d41d1cf13852679dbbd89efb51
Aldo Culquicondor <acondor@chromium.org>
2025-02-26 09:44:36 -0800

    Histograms for SetCookieString and GetCookiesString
    Bug: 380864710
    Change-Id: I75c87ba27e9624b65efb91c07dea4457b758a232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304880
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425222}

--- services/network/restricted_cookie_manager.h
+++ services/network/restricted_cookie_manager.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
+#include "base/rand_util.h"
 #include "base/sequence_checker.h"
 #include "base/timer/timer.h"
 #include "mojo/public/cpp/base/shared_memory_version.h"
@@ -352,6 +353,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) RestrictedCookieManager
 
   mojo::SharedMemoryVersionController shared_memory_version_controller_;
   base::OneShotTimer shared_memory_invalidation_timer_;
+
+  base::MetricsSubSampler metrics_subsampler_;
 
   base::WeakPtrFactory<RestrictedCookieManager> weak_ptr_factory_{this};
 };
