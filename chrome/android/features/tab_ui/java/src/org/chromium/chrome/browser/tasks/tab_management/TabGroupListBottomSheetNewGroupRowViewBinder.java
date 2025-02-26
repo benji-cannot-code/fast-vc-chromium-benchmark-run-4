507462b87542059a60a64bfd773785a8e5b629c5
Fiaz Muhammad <mfiaz@google.com>
2025-02-26 12:19:30 -0800

    [Tab Group Parity] Add Views, Layouts & New Tab Group Row
    - New ViewBinder class for the New Tab Group Row of the Bottom Sheet
    - New Strings
    - New Dimensions
    - Add Layout for Tab Group Parity Bottom Sheet and the 'New Tab Group'
            Row
    
    Bug: 391387522
    Change-Id: I3e335212c0ed1b58e6616191695448097f5378fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304824
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425336}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetNewGroupRowViewBinder.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import static org.chromium.chrome.browser.tasks.tab_management.TabGroupRowProperties.ROW_CLICK_RUNNABLE;
+
+import android.view.ViewGroup;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Forwards changed property values to the view. */
+@NullMarked
+public class TabGroupListBottomSheetNewGroupRowViewBinder {
+    /** Propagates a key from the model to the view. */
+    public static void bind(PropertyModel model, ViewGroup view, PropertyKey propertyKey) {
+        if (propertyKey == ROW_CLICK_RUNNABLE) {
+            @Nullable Runnable runnable = model.get(ROW_CLICK_RUNNABLE);
+            view.setOnClickListener(runnable == null ? null : ignored -> runnable.run());
+        }
+    }
+}
