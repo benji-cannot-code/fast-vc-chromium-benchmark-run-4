aecdf918149771fb67770191908f7c3a466c77f3
Qi Bao <qib@google.com>
2025-02-26 14:26:36 -0800

    [Discount][Clank] Add new height spec to commerce bottom sheet.
    1. If content height < 50% screen height, wrap content.
    Demo:https://drive.google.com/file/d/1HW8GLEMq6B_MzSub0ZzQsaFNmNJhEsya/view?usp=sharing
    2. If content height >= 50% && < 100% screen height, open at 50%, then
    can be dragged to wrap content. Demo:
    https://drive.google.com/file/d/1J-PSXbiKOlgw4IM9VQKx3Khs7QoX1TEf/view?usp=sharing
    3. If content height >= 100% screen height, open at 50%, then can be
    dragged to full height.
    https://drive.google.com/file/d/1i08ylXI_qD3OLAByCdGzNYsQJ3QLIYYE/view
    4. Scrim is added for condition 3 if there are top offset. (offset is not added in this CL) Demo:
    https://drive.google.com/file/d/1rrQdOPnMQpuQVkQWVhVeYQ0BMwf-Ix9y/view?usp=sharing
    
    Bug: 362361187
    Change-Id: Ia44dae9f4d8a14484e7f9beb0a7dabd1d264dd96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6296924
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Qi Bao <qib@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425407}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/AdaptiveToolbarUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/AdaptiveToolbarUiCoordinator.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.ui;
 
 import android.content.Context;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
 
 import org.chromium.base.supplier.ObservableSupplier;
@@ -19,6 +20,7 @@ import org.chromium.chrome.browser.bookmarks.AddToBookmarksToolbarButtonControll
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
 import org.chromium.chrome.browser.bookmarks.TabBookmarker;
 import org.chromium.chrome.browser.commerce.CommerceBottomSheetContentController;
+import org.chromium.chrome.browser.commerce.CommerceBottomSheetContentCoordinator;
 import org.chromium.chrome.browser.commerce.ShoppingServiceFactory;
 import org.chromium.chrome.browser.commerce.coupons.DiscountsButtonController;
 import org.chromium.chrome.browser.dom_distiller.ReaderModeToolbarButtonController;
@@ -29,6 +31,7 @@ import org.chromium.chrome.browser.omnibox.voice.VoiceRecognitionHandler;
 import org.chromium.chrome.browser.omnibox.voice.VoiceRecognitionHandler.VoiceInteractionSource;
 import org.chromium.chrome.browser.price_insights.PriceInsightsButtonController;
 import org.chromium.chrome.browser.price_tracking.CurrentTabPriceTrackingStateSupplier;
+import org.chromium.chrome.browser.price_tracking.PriceTrackingBottomSheetContentCoordinator;
 import org.chromium.chrome.browser.price_tracking.PriceTrackingButtonController;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.readaloud.ReadAloudController;
@@ -46,6 +49,8 @@ import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarButtonVariant
 import org.chromium.chrome.browser.toolbar.adaptive.TranslateToolbarButtonController;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+import org.chromium.components.browser_ui.widget.scrim.ScrimManager;
+import org.chromium.components.commerce.core.CommerceFeatureUtils;
 import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -65,6 +70,10 @@ public class AdaptiveToolbarUiCoordinator {
     private ContextualPageActionController mContextualPageActionController;
     private AdaptiveToolbarButtonController mAdaptiveToolbarButtonController;
     private VoiceToolbarButtonController mVoiceToolbarButtonController;
+    private BottomSheetController mBottomSheetController;
+    private ObservableSupplier<Profile> mProfileSupplier;
+    private Supplier<ScrimManager> mScrimSupplier;
+    private CommerceBottomSheetContentCoordinator mCommerceBottomSheetContentCoordinator;
 
     /**
      * Constructor.
@@ -94,12 +103,14 @@ public class AdaptiveToolbarUiCoordinator {
             ObservableSupplier<BookmarkModel> bookmarkModelSupplier,
             Supplier<ReadAloudController> readAloudControllerSupplier,
             ObservableSupplier<ShareDelegate> shareDelegateSupplier,
-            Supplier<CommerceBottomSheetContentController> commerceBottomSheetControllerSupplier,
             Runnable onShareRunnable,
             WindowAndroid windowAndroid,
-            Supplier<Tracker> trackerSupplier) {
+            Supplier<Tracker> trackerSupplier,
+            Supplier<ScrimManager> scrimSupplier) {
         if (!toolbarBehavior.shouldInitialize()) return;
-
+        mBottomSheetController = bottomSheetController;
+        mProfileSupplier = profileSupplier;
+        mScrimSupplier = scrimSupplier;
         IdentityDiscController identityDiscController =
                 new IdentityDiscController(mContext, activityLifecycleDispatcher, profileSupplier);
         mCurrentTabPriceTrackingStateSupplier =
@@ -117,7 +128,7 @@ public class AdaptiveToolbarUiCoordinator {
                         new PriceInsightsDelegateImpl(
                                 mContext, mCurrentTabPriceTrackingStateSupplier),
                         AppCompatResources.getDrawable(mContext, R.drawable.ic_trending_down_24dp),
-                        commerceBottomSheetControllerSupplier);
+                        this::getCommerceBottomSheetContentController);
         PriceTrackingButtonController priceTrackingButtonController =
                 new PriceTrackingButtonController(
                         mContext,
@@ -188,7 +199,7 @@ public class AdaptiveToolbarUiCoordinator {
                             mContext,
                             mActivityTabProvider,
                             mModalDialogManagerSupplier.get(),
-                            commerceBottomSheetControllerSupplier);
+                            this::getCommerceBottomSheetContentController);
             adaptiveToolbarButtonController.addButtonVariant(
                     AdaptiveToolbarButtonVariant.DISCOUNTS, discountsButtonController);
         }
@@ -290,5 +301,29 @@ public class AdaptiveToolbarUiCoordinator {
             for (ButtonDataProvider provider : mButtonDataProviders) provider.destroy();
             mButtonDataProviders = null;
         }
+    }
+
+    private PriceTrackingBottomSheetContentCoordinator createPriceTrackingContentProvider() {
+        return new PriceTrackingBottomSheetContentCoordinator(
+                mContext,
+                mActivityTabProvider,
+                new PriceInsightsDelegateImpl(mContext, mCurrentTabPriceTrackingStateSupplier));
+    }
+
+    @Nullable
+    private CommerceBottomSheetContentController getCommerceBottomSheetContentController() {
+        if (mCommerceBottomSheetContentCoordinator == null
+                // This flag is for discounts and commerce bottom sheet as a feature together.
+                && CommerceFeatureUtils.isDiscountInfoApiEnabled(
+                        ShoppingServiceFactory.getForProfile(mProfileSupplier.get()))) {
+            mCommerceBottomSheetContentCoordinator =
+                    new CommerceBottomSheetContentCoordinator(
+                            mContext,
+                            mBottomSheetController,
+                            mScrimSupplier,
+                            this::createPriceTrackingContentProvider);
+        }
+
+        return mCommerceBottomSheetContentCoordinator;
     }
 }
