c57bb5482ebb57d1be7e83159ed9688abba9b806
Dominik Bylica <bylica@google.com>
2025-02-25 23:49:46 -0800

    [iwa] Check if policies disable controlled frame.
    This is a followup CL for https://chromium-review.googlesource.com/c/chromium/src/+/6157803.
    The previous one added new policies for controlled frame.
    This one actually checks if those policies mapped into the content
    settings allow the controlled frame to be used or not.
    
    Bug: 388832862
    Change-Id: Ie5876705a25100ab78c16017c9f990cc4f763995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172277
    Reviewed-by: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Dominik Bylica <bylica@google.com>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Dominik Bylica <bylica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424989}

--- extensions/browser/extensions_browser_client.cc
+++ extensions/browser/extensions_browser_client.cc
@@ -264,4 +264,10 @@ ExtensionsBrowserClient::GetMediaDeviceSaltService(
   return nullptr;
 }
 
+bool ExtensionsBrowserClient::HasControlledFrameCapability(
+    content::BrowserContext* context,
+    const GURL& url) {
+  return false;
+}
+
 }  // namespace extensions
