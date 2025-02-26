d4c5982f066494331752b79ebfe26f5de85342df
Tom Sepez <tsepez@chromium.org>
2025-02-26 11:38:42 -0800

    Remove file-wide unsafe_buffers pragma from blink/ [5 of N]
    Bug: 351564777
    Change-Id: I5eda3cdb12699eb5f50c876c6c7fd0340cef71ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298476
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425298}

--- third_party/blink/renderer/core/page/spatial_navigation.cc
+++ third_party/blink/renderer/core/page/spatial_navigation.cc
@@ -27,13 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/page/spatial_navigation.h"
 
+#include "base/compiler_specific.h"
 #include "base/containers/adapters.h"
 #include "third_party/blink/public/mojom/scroll/scrollbar_mode.mojom-blink.h"
 #include "third_party/blink/renderer/core/dom/node_traversal.h"
@@ -776,7 +772,7 @@ PhysicalRect FirstVisibleFragment(const PhysicalRect& visibility,
     physical_fragment.Intersect(visibility);
     if (!physical_fragment.IsEmpty())
       return physical_fragment;
-    ++fragment;
+    UNSAFE_TODO(++fragment);
   }
   return visibility;
 }
