19ee3aa0b290769b0762c411e0e692b6d1e520e0
Jeffrey Cohen <jeffreycohen@google.com>
2025-02-25 16:31:48 -0800

    [Glic_button] Make the glic button square in its collapsed state
    This adds a centered set_cross_axis_alignment to avoid stretching out the button in the display, and also changes the collapsed width to be equal to the height to force the button to be square.
    
    Bug: b:398867442
    Change-Id: I67de2f8a519724268950ee6c0714c7576ceb8391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298719
    Commit-Queue: Jeffrey Cohen <jeffreycohen@google.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424878}

--- chrome/browser/ui/views/tabs/glic_button.cc
+++ chrome/browser/ui/views/tabs/glic_button.cc
@@ -22,10 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/view_class_properties.h"
 
-namespace {
-constexpr int kIconSizePadding = 24;
-}  // namespace
-
 namespace glic {
 
 GlicButton::GlicButton(TabStripController* tab_strip_controller,
@@ -91,13 +87,10 @@ gfx::Size GlicButton::CalculatePreferredSize(
   const int full_width =
       GetLayoutManager()->GetPreferredSize(this, available_size).width();
 
-  int icon_width =
-      GetImage(views::Button::STATE_NORMAL).size().width() + kIconSizePadding;
-  const int width = (full_width - icon_width) * GetWidthFactor() + icon_width;
-
   const int height = TabStripControlButton::CalculatePreferredSize(
-                         views::SizeBounds(width, available_size.height()))
+                         views::SizeBounds(full_width, available_size.height()))
                          .height();
+  const int width = (full_width - height) * GetWidthFactor() + height;
 
   return gfx::Size(width, height);
 }
