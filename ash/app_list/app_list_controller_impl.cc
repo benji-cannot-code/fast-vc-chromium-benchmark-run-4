d7cf21fa014de2921f9a118adb51737f76f1ed2f
Michael Cui <mlcui@google.com>
2025-02-25 16:16:03 -0800

    sunfish: Refactor launcher's `GetSunfishButtonVisibility`
    Because Sunfish and Scanner are now decoupled,
    `GetSunfishButtonVisibility` can now be refactored to avoid checking
    Sunfish's feature state twice.
    
    Bug: b:397521940
    Change-Id: I6a6a210097dd521b22c0e48613221ccab1c9eea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6290671
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424869}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/wallpaper/wallpaper_controller.h"
 #include "ash/root_window_controller.h"
+#include "ash/scanner/scanner_controller.h"
 #include "ash/scanner/scanner_metrics.h"
 #include "ash/screen_util.h"
 #include "ash/session/session_controller_impl.h"
@@ -290,15 +291,15 @@ bool IsAssistantExitPointInsideLauncher(
 }
 
 SearchBoxModel::SunfishButtonVisibility GetSunfishButtonVisibility() {
-  if (!CanShowSunfishOrScannerUi()) {
-    return SearchBoxModel::SunfishButtonVisibility::kHidden;
+  if (CanShowSunfishUi()) {
+    return SearchBoxModel::SunfishButtonVisibility::kShownWithSunfishIcon;
   }
 
-  if (!CanShowSunfishUi()) {
+  if (ScannerController::CanShowUiForShell()) {
     return SearchBoxModel::SunfishButtonVisibility::kShownWithScannerIcon;
   }
 
-  return SearchBoxModel::SunfishButtonVisibility::kShownWithSunfishIcon;
+  return SearchBoxModel::SunfishButtonVisibility::kHidden;
 }
 
 }  // namespace
