004ef035470293b8df9c0ed3db5f1abe7e6d1779
Michael Cui <mlcui@google.com>
2025-02-25 16:13:25 -0800

    sunfish: Decouple Scanner and Sunfish in `IsSunfishSessionAllowed`
    `IsSunfishSessionAllowed()` will now be equivalent to
    `CanShowSunfishUi() || ScannerController::CanShowUiForShell()`, which is
    significantly simpler to reason about. Future CLs will clean up code
    left by this change.
    
    Bug: b:397521940
    Change-Id: I6a6a2100d2dc2cd92ae75a7e25321c67ca1d6ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6290668
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424864}

--- ash/capture_mode/capture_mode_api.cc
+++ ash/capture_mode/capture_mode_api.cc
@@ -21,8 +21,8 @@ namespace ash {
 
 namespace {
 
-// Extra checks for Sunfish prefs and policy, used in `CanShowSunfishUi` and
-// `IsSunfishSessionAllowed`.
+// Extra checks for Sunfish prefs and policy, used in `CanShowSunfishUi`.
+// TODO: crbug.com/397521940 - Inline this into `CanShowSunfishUi`.
 bool ExtraSunfishChecks() {
   Shell* shell = Shell::HasInstance() ? Shell::Get() : nullptr;
   if (!shell) {
@@ -58,9 +58,7 @@ bool CanShowSunfishUi() {
 }
 
 bool IsSunfishSessionAllowed() {
-  return (features::IsSunfishFeatureEnabled() ||
-          ScannerController::CanShowUiForShell()) &&
-         ExtraSunfishChecks();
+  return CanShowSunfishUi() || ScannerController::CanShowUiForShell();
 }
 
 }  // namespace ash
