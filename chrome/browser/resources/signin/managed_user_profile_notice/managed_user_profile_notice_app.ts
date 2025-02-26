c301663ed8fc7c0e9c30b1f1763f2b9d6dd87ead
Yann Dago <ydago@chromium.org>
2025-02-26 09:19:04 -0800

    Add back button on the management disclosure screen
    When on the management disclosure screen, the user should be able to go
    to the value proposition screen if that screen was the initial screen of
    the dialog
    
    Bug: 385174637
    Change-Id: Id3c02a73b4e5bcd308ccdc95365402982fa7f01c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300760
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425202}

--- chrome/browser/resources/signin/managed_user_profile_notice/managed_user_profile_notice_app.ts
+++ chrome/browser/resources/signin/managed_user_profile_notice/managed_user_profile_notice_app.ts
@@ -186,7 +186,16 @@ export class ManagedUserProfileNoticeAppElement extends
 
   /** Called when the cancel button is clicked. */
   protected onCancel_() {
+    if (this.allowValuePropStateBackFromDisclosure_()) {
+      this.updateCurrentState_(State.VALUE_PROPOSITION);
+      return;
+    }
     this.managedUserProfileNoticeBrowserProxy_.cancel();
+  }
+
+  protected allowValuePropStateBackFromDisclosure_() {
+    return this.currentState_ === State.DISCLOSURE &&
+        loadTimeData.getInteger('initialState') !== State.DISCLOSURE;
   }
 
   private setProfileInfo_(info: ManagedUserProfileInfo) {
@@ -225,10 +234,14 @@ export class ManagedUserProfileNoticeAppElement extends
   }
 
   private computeCancelLabel_() {
-    return this.currentState_ === State.VALUE_PROPOSITION &&
-            !loadTimeData.getBoolean('enforcedByPolicy') ?
-        this.i18n('cancelValueProp') :
-        this.i18n('cancelLabel');
+    if (this.currentState_ === State.VALUE_PROPOSITION &&
+        !loadTimeData.getBoolean('enforcedByPolicy')) {
+      return this.i18n('cancelValueProp');
+    }
+    if (this.allowValuePropStateBackFromDisclosure_()) {
+      return this.i18n('backLabel');
+    }
+    return this.i18n('cancelLabel');
   }
 
   protected allowProceedButton_() {
