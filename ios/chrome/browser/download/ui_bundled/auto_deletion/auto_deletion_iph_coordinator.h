86e93d139ef33eb29c7b776f35a2f1fef2eb6bf1
Daniel White <danieltwhite@google.com>
2025-02-26 08:36:17 -0800

    [iOS] Auto-deletion IPH UI Functionality
    In this CL, the functionality that the Auto-deletion IPH UI affords the
    user is implemented. The user will be able to enable the auto-deletion
    feature, decline to enabled the feature, and dismiss the IPH. Below, a
    screen recording of this CL in action has been attached.
    
    https://drive.google.com/file/d/1_QI8rZLwAQDFNQeiEIjie21uZnMj9ze_/view?usp=sharing&resourcekey=0-0xNvJtoTeQmp7671qTLX0Q
    
    Bug: 390199773
    Change-Id: I63f45748dce3ec0a9d5a546994d09b2152c9c516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299296
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425174}

--- ios/chrome/browser/download/ui_bundled/auto_deletion/auto_deletion_iph_coordinator.h
+++ ios/chrome/browser/download/ui_bundled/auto_deletion/auto_deletion_iph_coordinator.h
@@ -8,8 +8,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.h"
 
+class Browser;
+namespace web {
+class DownloadTask;
+}  // namespace web
+
 // Coordinator for the Auto-deletion IPH.
 @interface AutoDeletionIPHCoordinator : ChromeCoordinator
+
+- (instancetype)initWithBaseViewController:(UIViewController*)baseViewController
+                                   browser:(Browser*)browser
+                              downloadTask:(web::DownloadTask*)task
+    NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithBaseViewController:(UIViewController*)baseViewController
+                                   browser:(Browser)browser NS_UNAVAILABLE;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_DOWNLOAD_UI_BUNDLED_AUTO_DELETION_AUTO_DELETION_IPH_COORDINATOR_H_
--- ios/chrome/browser/download/ui_bundled/auto_deletion/auto_deletion_iph_view_controller.h
+++ ios/chrome/browser/download/ui_bundled/auto_deletion/auto_deletion_iph_view_controller.h
@@ -10,8 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
 
+@protocol AutoDeletionMutator;
+class Browser;
+
 // The UIViewController that manages the Auto-deletion IPH's view.
 @interface AutoDeletionIPHViewController : ConfirmationAlertViewController
+
+- (instancetype)initWithBrowser:(Browser*)browser NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithNibName:(NSString*)name
+                         bundle:(NSBundle*)bundle NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+
+@property(nonatomic, weak) id<AutoDeletionMutator> mutator;
 
 @end
 
