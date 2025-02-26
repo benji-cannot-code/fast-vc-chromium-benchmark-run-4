fa60bb3fe0c07653fe999763234da59051cab490
Traian Captan <tcaptan@chromium.org>
2025-02-26 11:47:01 -0800

    Null-Check mask_position_x before use
    This fixes the ClusterFuzz reported:
    "Null-dereference READ in blink::CSSValue::CssText".
    
    R=pdr
    
    Bug: 398401905
    Change-Id: I92c56146036a57cae97a66f73e39ca1eadcb8698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299815
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425311}

--- third_party/blink/renderer/core/css/style_property_serializer.cc
+++ third_party/blink/renderer/core/css/style_property_serializer.cc
@@ -1821,7 +1821,8 @@ String StylePropertySerializer::GetLayeredShorthandValue(
         } else if (property->IDEquals(CSSPropertyID::kWebkitMaskPositionY)) {
           omit_value = true;
 
-          if (!IsZeroPercent(mask_position_x) || !IsZeroPercent(value)) {
+          if (mask_position_x &&
+              (!IsZeroPercent(mask_position_x) || !IsZeroPercent(value))) {
             is_position_x_serialized = true;
             is_position_y_serialized = true;
 
