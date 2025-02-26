bb832040da906acf24715c73c8de45292e1879f4
Aldo Culquicondor <acondor@chromium.org>
2025-02-26 12:37:08 -0800

    Reduce cookies copies in GetCookiesString
    Using std::move when the original object(s) is no longer needed.
    
    When measuring GetCookiesString on verge.com on a Pixel 8, the median
    latency goes from ~325 to ~265us
    
    Bug: 380864710
    Change-Id: Ibac1d2932a7ca26bff2ee06c7937818f98f9af9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6306082
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425344}

--- services/network/restricted_cookie_manager.cc
+++ services/network/restricted_cookie_manager.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/timer/elapsed_timer.h"
+#include "base/trace_event/trace_event.h"
 #include "base/types/optional_util.h"
 #include "mojo/public/cpp/bindings/message.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -561,8 +562,7 @@ void RestrictedCookieManager::CookieListToGetAllForUrlCallback(
     result.reserve(maybe_included_cookies.size());
   mojom::CookieMatchType match_type = options->match_type;
   const std::string& match_name = options->name;
-  for (const net::CookieWithAccessResult& cookie_item :
-       maybe_included_cookies) {
+  for (net::CookieWithAccessResult& cookie_item : maybe_included_cookies) {
     const net::CanonicalCookie& cookie = cookie_item.cookie;
     net::CookieAccessResult access_result = cookie_item.access_result;
     const std::string& cookie_name = cookie.Name();
@@ -580,21 +580,11 @@ void RestrictedCookieManager::CookieListToGetAllForUrlCallback(
     }
 
     if (access_result.status.IsInclude()) {
-      result.push_back(cookie_item);
+      result.push_back(std::move(cookie_item));
     }
   }
 
-  auto notify_observer = [&]() {
-    if (cookie_observer_ && !on_cookies_accessed_result.empty()) {
-      OnCookiesAccessed(mojom::CookieAccessDetails::New(
-          mojom::CookieAccessDetails::Type::kRead, url,
-          isolated_top_frame_origin, site_for_cookies,
-          std::move(on_cookies_accessed_result), std::nullopt, is_ad_tagged,
-          cookie_setting_overrides));
-    }
-  };
-
-  if (!maybe_included_cookies.empty() && IsPartitionedCookiesEnabled()) {
+  if (!result.empty() && IsPartitionedCookiesEnabled()) {
     UMA_HISTOGRAM_COUNTS_100(
         "Net.RestrictedCookieManager.PartitionedCookiesInScript",
         std::ranges::count_if(result, [](const net::CookieWithAccessResult& c) {
@@ -638,7 +628,12 @@ void RestrictedCookieManager::CookieListToGetAllForUrlCallback(
             cookie.access_result));
   }
 
-  notify_observer();
+  if (cookie_observer_ && !on_cookies_accessed_result.empty()) {
+    OnCookiesAccessed(mojom::CookieAccessDetails::New(
+        mojom::CookieAccessDetails::Type::kRead, url, isolated_top_frame_origin,
+        site_for_cookies, std::move(on_cookies_accessed_result), std::nullopt,
+        is_ad_tagged, cookie_setting_overrides));
+  }
 }
 
 void RestrictedCookieManager::UpdateSharedMemoryVersionInvalidationTimer(
@@ -907,6 +902,7 @@ void RestrictedCookieManager::SetCookieFromString(
     bool apply_devtools_overrides,
     const std::string& cookie,
     SetCookieFromStringCallback callback) {
+  TRACE_EVENT("net", "RestrictedCookieManager::SetCookieFromString");
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   base::ElapsedTimer timer;
 
@@ -968,6 +964,7 @@ void RestrictedCookieManager::GetCookiesString(
     bool apply_devtools_overrides,
     bool force_disable_third_party_cookies,
     GetCookiesStringCallback callback) {
+  TRACE_EVENT("net", "RestrictedCookieManager::GetCookiesString");
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   base::ElapsedTimer timer;
   // Checks done by GetAllForUrl
