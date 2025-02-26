dac9866151e4716974d6d90b0fa1b6f1c2af0540
Xu Li <mxlg@google.com>
2025-02-26 11:57:24 -0800

    [iOS] Add ios_realtime_reporting_client skeleton.
    This is part of the overall effort of implementing event reporting
    feature on iOS. This CL only adds a basic framework for the ios_realtime_reporting_client. The detailed implementation for the functions will be added in the followup CLs.
    
    Bug: 394097677
    
    Change-Id: I5cca813754d6a816da611c76a55f9f9bdbcbfabd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298834
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Xu Li <mxlg@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425327}

---
+++ ios/chrome/browser/enterprise/connectors/reporting/ios_realtime_reporting_client_unittest.mm
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/connectors/reporting/ios_realtime_reporting_client.h"
+
+#import "base/memory/raw_ptr.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios.h"
+#import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
+#import "ios/web/public/test/web_task_environment.h"
+#import "testing/platform_test.h"
+#import "third_party/ocmock/gtest_support.h"
+
+namespace enterprise_connectors {
+
+class IOSRealtimeReportingClientTest : public PlatformTest {
+ public:
+  IOSRealtimeReportingClientTest() {
+    profile_ =
+        profile_manager_.AddProfileWithBuilder(TestProfileIOS::Builder());
+  }
+
+ protected:
+  web::WebTaskEnvironment task_environment_;
+  // Add local state to test ApplicationContext. Required by
+  // TestProfileManagerIOS.
+  IOSChromeScopedTestingLocalState scoped_testing_local_state_;
+  TestProfileManagerIOS profile_manager_;
+  raw_ptr<ProfileIOS> profile_;
+};
+
+// Tests that the GetProfileUserName() returns the expected value.
+TEST_F(IOSRealtimeReportingClientTest, ReturnsProfileUserName) {
+  IOSRealtimeReportingClient client(profile_);
+  ASSERT_EQ(client.GetProfileUserName(), std::string());
+}
+
+}  // namespace enterprise_connectors
