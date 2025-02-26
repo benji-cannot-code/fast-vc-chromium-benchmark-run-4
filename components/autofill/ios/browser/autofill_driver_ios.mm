13e6c5676ba69830c4c46e1a93cea851525845c7
Nohemi Fernandez <fernandex@google.com>
2025-02-26 05:04:42 -0800

    [iOS] Remove unused ui/ imports in AutofillDriverIOS.
    Bug: N/A
    Change-Id: Ib8b2b2f989bc980425807d44ee13ab32faefe353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297911
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425081}

--- components/autofill/ios/browser/autofill_driver_ios.mm
+++ components/autofill/ios/browser/autofill_driver_ios.mm
@@ -39,8 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/js_messaging/web_frames_manager.h"
 #import "ios/web/public/web_state.h"
 #import "services/network/public/cpp/shared_url_loader_factory.h"
-#import "ui/accessibility/ax_tree_id.h"
-#import "ui/gfx/geometry/rect_f.h"
 #import "url/origin.h"
 
 namespace autofill {
