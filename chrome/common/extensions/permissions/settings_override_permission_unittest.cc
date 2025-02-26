7878448e7a7ddbde5fc246774931ec53fecf18e5
Jonathan Lee <jonathanjlee@google.com>
2025-02-26 09:00:28 -0800

    [gtest] Consume `AssertionResult`s in `//c/c/extensions/permissions/`
    Wrap each `testing::AssertionResult` in an `EXPECT_TRUE(...)` [0] to
    actually check if the assertion passed. The dropped assertion causes a
    build failure [1] with a recent Googletest revision that marks
    `AssertionResult` as `[[nodiscard]]` [2].
    
    [0]: https://google.github.io/googletest/advanced.html#predicate-assertions-for-better-error-messages
    [1]: https://ci.chromium.org/ui/p/chromium/builders/try/linux-rel/2130794/overview
    [2]: https://chromium.googlesource.com/external/github.com/google/googletest.git/+/3fbe4db9a39291ae8d7a9c5f1d75896bb4c5a18f%5E%21/
    
    Bug: 398298306
    Change-Id: If6b96b50b426cf7ff76e41d4ca2ac28e53d03f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6303081
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425193}

--- chrome/common/extensions/permissions/settings_override_permission_unittest.cc
+++ chrome/common/extensions/permissions/settings_override_permission_unittest.cc
@@ -88,8 +88,8 @@ TEST_F(SettingsOverridePermissionTest, HomePage) {
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   EXPECT_TRUE(permission_set.HasAPIPermission(APIPermissionID::kHomepage));
-  VerifyOnePermissionMessage(extension->permissions_data(),
-                             "Change your home page to: google.com");
+  EXPECT_TRUE(VerifyOnePermissionMessage(
+      extension->permissions_data(), "Change your home page to: google.com"));
 #else
   EXPECT_FALSE(permission_set.HasAPIPermission(APIPermissionID::kHomepage));
 #endif
@@ -106,8 +106,8 @@ TEST_F(SettingsOverridePermissionTest, StartupPages) {
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   EXPECT_TRUE(permission_set.HasAPIPermission(APIPermissionID::kStartupPages));
-  VerifyOnePermissionMessage(extension->permissions_data(),
-                             "Change your start page to: startup.com");
+  EXPECT_TRUE(VerifyOnePermissionMessage(
+      extension->permissions_data(), "Change your start page to: startup.com"));
 #else
   EXPECT_FALSE(permission_set.HasAPIPermission(APIPermissionID::kStartupPages));
 #endif
@@ -125,8 +125,9 @@ TEST_F(SettingsOverridePermissionTest, SearchSettings) {
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   EXPECT_TRUE(
       permission_set.HasAPIPermission(APIPermissionID::kSearchProvider));
-  VerifyOnePermissionMessage(extension->permissions_data(),
-                             "Change your search settings to: google.com");
+  EXPECT_TRUE(
+      VerifyOnePermissionMessage(extension->permissions_data(),
+                                 "Change your search settings to: google.com"));
 #else
   EXPECT_FALSE(
       permission_set.HasAPIPermission(APIPermissionID::kSearchProvider));
