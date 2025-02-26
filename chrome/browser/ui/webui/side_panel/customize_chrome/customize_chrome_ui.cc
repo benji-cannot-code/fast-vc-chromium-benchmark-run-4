56d1fb4e6550c220bf90dd13a9ab8dae337a20ef
Taylor Bergquist <tbergquist@chromium.org>
2025-02-26 11:45:51 -0800

    Fetch the browser only as needed in CustomizeToolbarHandler.
    This should fix some crashes (see linked bug). It's also more correct in
    cases where the tab is moved out of the original browser while this
    webui is open, especially if said original browser has since been
    closed (i.e. this might prevent UAFs).
    
    Bug: 396071119
    Change-Id: I3f8cb49cf251478d9a517333cc085b1780e323dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6271205
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425309}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search/background/ntp_custom_background_service_factory.h"
 #include "chrome/browser/search/background/wallpaper_search/wallpaper_search_background_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/side_panel/customize_chrome/customize_chrome_utils.h"
 #include "chrome/browser/ui/webui/cr_components/customize_color_scheme_mode/customize_color_scheme_mode_handler.h"
@@ -469,10 +468,6 @@ void CustomizeChromeUI::CreateCustomizeToolbarHandler(
     return;
   }
 
-  const raw_ptr<Browser> browser =
-      chrome::FindBrowserWithWindow(web_contents_->GetTopLevelNativeWindow());
-  CHECK(browser);
-
   customize_toolbar_handler_ = std::make_unique<CustomizeToolbarHandler>(
-      std::move(handler), std::move(client), browser);
+      std::move(handler), std::move(client), web_contents_);
 }
--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser_actions.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar.mojom.h"
 #include "chrome/browser/ui/webui/util/image_util.h"
+#include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
@@ -143,11 +145,12 @@ CustomizeToolbarHandler::CustomizeToolbarHandler(
         side_panel::customize_chrome::mojom::CustomizeToolbarHandler> handler,
     mojo::PendingRemote<
         side_panel::customize_chrome::mojom::CustomizeToolbarClient> client,
-    raw_ptr<Browser> browser)
+    content::WebContents* web_contents)
     : client_(std::move(client)),
       receiver_(this, std::move(handler)),
-      browser_(browser),
-      model_(PinnedToolbarActionsModel::Get(browser_->profile())) {
+      web_contents_(web_contents),
+      model_(PinnedToolbarActionsModel::Get(
+          Profile::FromBrowserContext(web_contents_->GetBrowserContext()))) {
   model_observation_.Observe(model_);
   pref_change_registrar_.Init(prefs());
   pref_change_registrar_.Add(
@@ -164,13 +167,11 @@ CustomizeToolbarHandler::~CustomizeToolbarHandler() = default;
 
 void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
   std::vector<side_panel::customize_chrome::mojom::ActionPtr> actions;
-
-  const ui::ColorProvider* const provider =
-      browser_->window()->GetColorProvider();
+  const ui::ColorProvider& provider = web_contents_->GetColorProvider();
   const int icon_color_id = ui::kColorSysOnSurface;
   const float scale_factor =
       display::Screen::GetScreen()
-          ->GetDisplayNearestWindow(browser_->window()->GetNativeWindow())
+          ->GetDisplayNearestWindow(web_contents_->GetTopLevelNativeWindow())
           .device_scale_factor();
 
   auto home_action = side_panel::customize_chrome::mojom::Action::New(
@@ -181,7 +182,7 @@ void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
       GURL(webui::EncodePNGAndMakeDataURI(
           ui::ImageModel::FromVectorIcon(kNavigateHomeChromeRefreshIcon,
                                          icon_color_id)
-              .Rasterize(provider),
+              .Rasterize(&provider),
           scale_factor)));
 
   auto forward_action = side_panel::customize_chrome::mojom::Action::New(
@@ -192,15 +193,18 @@ void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
       GURL(webui::EncodePNGAndMakeDataURI(
           ui::ImageModel::FromVectorIcon(
               vector_icons::kForwardArrowChromeRefreshIcon, icon_color_id)
-              .Rasterize(provider),
+              .Rasterize(&provider),
           scale_factor)));
 
+  const raw_ptr<BrowserWindowInterface> bwi =
+      webui::GetBrowserWindowInterface(web_contents_);
+  CHECK(bwi);
   const auto add_action =
-      [&actions, this, provider, scale_factor](
+      [&actions, this, &provider, scale_factor, bwi](
           actions::ActionId id,
           side_panel::customize_chrome::mojom::CategoryId category) {
         actions::ActionItem* const scope_action =
-            browser_->browser_actions()->root_action_item();
+            bwi->GetActions()->root_action_item();
         actions::ActionItem* const action_item =
             actions::ActionManager::Get().FindAction(id, scope_action);
         if (!action_item || !action_item->GetVisible()) {
@@ -235,7 +239,7 @@ void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
             base::UTF16ToUTF8(action_item->GetText()), model_->Contains(id),
             has_enterprise_controlled_pinned_state, category,
             GURL(webui::EncodePNGAndMakeDataURI(
-                recolored_icon.Rasterize(provider), scale_factor)));
+                recolored_icon.Rasterize(&provider), scale_factor)));
         actions.push_back(std::move(mojo_action));
       };
 
@@ -379,5 +383,6 @@ void CustomizeToolbarHandler::OnActionItemChanged() {
 }
 
 PrefService* CustomizeToolbarHandler::prefs() const {
-  return browser_->profile()->GetPrefs();
+  return Profile::FromBrowserContext(web_contents_->GetBrowserContext())
+      ->GetPrefs();
 }
