696ba1bc9effc534f6978124b9eca45b5d7f1ee8
Fredrik Söderquist <fs@opera.com>
2025-02-26 07:00:25 -0800

    Move StrokeStyle enum to styled_stroke_data.h
    StrokeStyle is strongly connected to StyledStrokeData which is defined
    in this file.
    
    Bug: 399058536
    Change-Id: I060c8390f9bfdbc41ab633e2f6d833c65710be2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304679
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1425124}

--- third_party/blink/renderer/platform/graphics/styled_stroke_data.h
+++ third_party/blink/renderer/platform/graphics/styled_stroke_data.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_STYLED_STROKE_DATA_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_STYLED_STROKE_DATA_H_
 
-#include "third_party/blink/renderer/platform/graphics/graphics_types.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
@@ -41,6 +40,15 @@ class PaintFlags;
 namespace blink {
 
 class StrokeData;
+
+enum StrokeStyle {
+  kNoStroke,
+  kSolidStroke,
+  kDottedStroke,
+  kDashedStroke,
+  kDoubleStroke,
+  kWavyStroke,
+};
 
 // Stroke geometry information based on a specified style (StrokeStyle).
 //
