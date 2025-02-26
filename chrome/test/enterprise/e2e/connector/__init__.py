6fa55bd93400a43d6873c8b7caa550284978996d
Jonathan Lee <jonathanjlee@google.com>
2025-02-26 13:58:32 -0800

    [enterprise][e2e] Add test and infra for per-profile client certificates
    Bug: 398278180
    Test: https://screencast.googleplex.com/cast/NDk5MDY1MzE0MTM1MjQ0OHwwNWQ4MjRlNS1lYg
    Change-Id: I0474ac50eaee3ccbb2c94a2a9f1c230b3be7f2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6293144
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Xiang Ji <jxiang@google.com>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425395}

--- chrome/test/enterprise/e2e/connector/__init__.py
+++ chrome/test/enterprise/e2e/connector/__init__.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from .verifyContent import VerifyContent
 from .verifyable import Verifyable
 from .chrome_reporting_connector_test_case import ChromeReportingConnectorTestCase
+from .client_certs.client_certs_test import *
 from .device_trust_connector.device_trust_connector_windows_enrollment_test import *
 from .identity_connector.managed_profile_test import *
 from .local_content_analysis_connector.local_content_analysis_connector_test import *
