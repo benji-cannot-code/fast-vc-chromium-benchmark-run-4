b8d31ad949ff1edbb122f09e3d6bb042c1e1e3d9
Anastasia Helfinstein <anastasi@google.com>
2025-02-26 11:35:30 -0800

    Revert "Add UKM for nudges ignored with nav change."
    This reverts commit 843a415224c81e0bb8e11fc98d97e8f01667bab3.
    
    Reason for revert: Cause compile failure, closed tree
    
    Original change's description:
    > Add UKM for nudges ignored with nav change.
    >
    > This logs an additional possible nudge interaction under ContextualCueing.NudgeInteraction in this collection doc: https://docs.google.com/document/d/1LhnQ-2KDDdJMZ6eLieNr2EVlW1SP0ArhBfDFjUcCT0A/
    >
    > Bug: 395169951
    > Change-Id: Ic875fa0670e4bc2a84081a562989c4c3cec6f635
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299194
    > Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    > Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1425283}
    
    Bug: 395169951
    Change-Id: I65f4eef1cba394dc9d4ebe9ddf5c913963a4ebbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6306882
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Hao Liu <haoliuk@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Owners-Override: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1425292}

--- chrome/browser/ui/tabs/glic_nudge_controller.cc
+++ chrome/browser/ui/tabs/glic_nudge_controller.cc
@@ -22,11 +22,9 @@ GlicNudgeController::GlicNudgeController(
 
 GlicNudgeController::~GlicNudgeController() = default;
 
-void GlicNudgeController::UpdateNudgeLabel(
-    content::WebContents* web_contents,
-    const std::string& nudge_label,
-    std::optional<GlicNudgeActivity> activity,
-    GlicNudgeActivityCallback callback) {
+void GlicNudgeController::UpdateNudgeLabel(content::WebContents* web_contents,
+                                           const std::string& nudge_label,
+                                           GlicNudgeActivityCallback callback) {
   auto* const tab_interface =
       browser_window_interface_->GetActiveTabInterface();
   if (tab_interface->GetContents() != web_contents) {
@@ -37,11 +35,6 @@ void GlicNudgeController::UpdateNudgeLabel(
   nudge_activity_callback_ = callback;
   for (auto& observer : observers_) {
     observer.OnTriggerGlicNudgeUI(nudge_label);
-  }
-
-  if (nudge_label.empty()) {
-    CHECK(activity);
-    OnNudgeActivity(*activity);
   }
 }
 
@@ -56,7 +49,6 @@ void GlicNudgeController::OnNudgeActivity(GlicNudgeActivity activity) {
     case GlicNudgeActivity::kNudgeClicked:
     case GlicNudgeActivity::kNudgeDismissed:
     case GlicNudgeActivity::kNudgeIgnoredActiveTabChanged:
-    case GlicNudgeActivity::kNudgeIgnoredNavigation:
       nudge_activity_callback_.Run(activity);
       nudge_activity_callback_.Reset();
       break;
