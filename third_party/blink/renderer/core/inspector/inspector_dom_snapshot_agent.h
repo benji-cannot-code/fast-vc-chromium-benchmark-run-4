443838b98392f1f411c9da2c947332471a6d6d37
Philip Pfaffe <pfaffe@chromium.org>
2025-02-26 09:16:42 -0800

    Fix crash in DOM snapshot agent
    When taking as snapshot, the DOM snapshot agent holds a pointer to a
    hashmap on the stack. The hashmap is destroyed when the devtools session
    detaches, so if that happens while taking a snapshot we run into a
    crash.
    
    Fixed: 395032416
    Change-Id: I4b784cb3105f6a760ca5b1d3dcbe1ea254b3ad3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297444
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425198}

--- third_party/blink/renderer/core/inspector/inspector_dom_snapshot_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_dom_snapshot_agent.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_DOM_SNAPSHOT_AGENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_DOM_SNAPSHOT_AGENT_H_
 
+#include "base/memory/weak_ptr.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/inspector/inspector_base_agent.h"
@@ -26,6 +27,7 @@ class Element;
 class InspectedFrames;
 class Node;
 class PaintLayer;
+struct OriginUrlMap;
 
 class CORE_EXPORT InspectorDOMSnapshotAgent final
     : public InspectorBaseAgent<protocol::DOMSnapshot::Metainfo> {
@@ -106,7 +108,6 @@ class CORE_EXPORT InspectorDOMSnapshotAgent final
   static void VisitPaintLayer(PaintLayer*, PaintOrderMap* paint_order_map);
 
   using CSSPropertyFilter = Vector<const CSSProperty*>;
-  using OriginUrlMap = WTF::HashMap<DOMNodeId, String>;
 
   // State of current snapshot.
   std::unique_ptr<protocol::Array<protocol::DOMSnapshot::DOMNode>> dom_nodes_;
