70d844ae9c2d4a07fb36870de8925f9144bf5d4f
Lukasz Anforowicz <lukasza@chromium.org>
2025-02-26 10:03:27 -0800

    [gnrt] Teach `platforms.rs` about `target_family = ...` conditions.
    Overview of the problem fixed by this CL
    ----------------------------------------
    
    `cargo tree` under `tools/crates/gnrt/sample_package` shows that
    `time` crate depends on `libc` and `num_threads`:
    
        ```
        sample_package v0.1.0 (...)
        ├── bar v0.1.0 (...)
        ├── foo v0.1.0 (...)
        │   ├── ...
        │   └── time v0.3.14
        │       ├── libc v0.2.133
        │       └── num_threads v0.1.6
        ...
        ```
    
    Despite the above, before this CL, `libc` and `num_threads` would be
    incorrectly missing when running the
    `collect_dependencies_on_sample_output` test.  AFAICT this is because
    the dependency is conditional, and before this CL `platforms.rs` didn't
    understand `target_family = ...` kind of conditions.
    
    This CL is kind of temporary
    ----------------------------
    
    Changes in this CL are somewhat temporary, because `platforms.rs` will
    disappear soon in https://crrev.com/c/6300716.  This CL is done
    separately to make it easier to review and justify unit test changes in
    this smaller, separate CL.
    
    Testing
    -------
    
    This CL has been tested by:
    
    * Running `tools/crates/run_gnrt.py gen` and verifying it doesn't make
      any changes under `//third_party/rust`.  (Only `gen` depends on
      `fn collect_dependencies` from `deps.rs`.)
    * Same for `tools/rust/gnrt_stdlib.py`
    * Running `cargo test` under `tools/crates/gnrt`
    
    Bug: 395924069
    Change-Id: I11d80cb85197b3cf0f6f6bc39921939d1d666a34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300655
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425233}

--- tools/crates/gnrt/lib/deps.rs
+++ tools/crates/gnrt/lib/deps.rs
@@ -630,6 +630,15 @@ mod tests {
 
         i += 1;
 
+        assert_eq!(dependencies[i].package_name, "libc");
+        assert_eq!(dependencies[i].version, Version::new(0, 2, 133));
+        assert_eq!(
+            dependencies[i].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
+            &["std"],
+        );
+
+        i += 1;
+
         assert_eq!(dependencies[i].package_name, "more-asserts");
         assert_eq!(dependencies[i].version, Version::new(0, 3, 0));
         assert!(dependencies[i].is_toplevel_dep);
@@ -662,6 +671,15 @@ mod tests {
             assert!(path.ends_with("num-traits-0.2.15/build.rs"));
             true
         }));
+
+        i += 1;
+
+        assert_eq!(dependencies[i].package_name, "num_threads");
+        assert_eq!(dependencies[i].version, Version::new(0, 1, 6));
+        assert_eq!(
+            dependencies[i].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
+            empty_str_slice
+        );
 
         i += 1;
 
@@ -831,6 +849,25 @@ mod tests {
             dependencies[i].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
             &["alloc", "std"]
         );
+        assert_eq!(dependencies[i].dependencies.len(), 2);
+        assert_eq!(
+            dependencies[i].dependencies[0],
+            DepOfDep {
+                package_name: "libc".to_string(),
+                use_name: "libc".to_string(),
+                version: Version::new(0, 2, 133),
+                platform: Some(Platform::from_str("cfg(target_family = \"unix\")").unwrap()),
+            }
+        );
+        assert_eq!(
+            dependencies[i].dependencies[1],
+            DepOfDep {
+                package_name: "num_threads".to_string(),
+                use_name: "num_threads".to_string(),
+                version: Version::new(0, 1, 6),
+                platform: Some(Platform::from_str("cfg(target_family = \"unix\")").unwrap()),
+            }
+        );
 
         i += 1;
 
@@ -912,6 +949,20 @@ mod tests {
 
         assert_eq!(dependencies[i].package_name, "foo");
         assert_eq!(dependencies[i].version, Version::new(0, 1, 0));
+
+        i += 1;
+
+        assert_eq!(dependencies[i].package_name, "libc");
+        assert_eq!(dependencies[i].version, Version::new(0, 2, 133));
+        assert_eq!(
+            dependencies[i].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
+            &["std"]
+        );
+
+        i += 1;
+
+        assert_eq!(dependencies[i].package_name, "num_threads");
+        assert_eq!(dependencies[i].version, Version::new(0, 1, 6));
 
         i += 1;
 
