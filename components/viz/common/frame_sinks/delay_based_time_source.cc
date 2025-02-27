29f90543990ed8f2c444c41d0480f7ec254b9445
Peter McNeeley <petermcneeley@google.com>
2025-02-26 19:58:43 -0800

    Allow for judder in vblank reporting (disabled by default)
    Some devices report vblank timings with enough variance as to confuse
    'SnappedToNextTick' on the time remaining for next begin frame. This
    code allows for up to 'kMaxJudderAllowed' time for these values to
    simply be corrected to the next interval and thereby avoid an
    erroneous double tick
    
    Bug: 398090404, 390188048
    Change-Id: Ie69b5225088db4967b55de820f5cf5939b5e2c5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305401
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425550}

--- components/viz/common/frame_sinks/delay_based_time_source.cc
+++ components/viz/common/frame_sinks/delay_based_time_source.cc
@@ -7,14 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
+#include <cstdint>
 #include <string>
 
 #include "base/check_op.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/traced_value.h"
+#include "components/viz/common/features.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 
 namespace viz {
@@ -155,8 +158,24 @@ void DelayBasedTimeSource::PostNextTickTask(base::TimeTicks now) {
                  base::subtle::DelayPolicy::kPrecise);
   } else {
     next_tick_time_ = now.SnappedToNextTick(timebase_, interval_);
-    if (next_tick_time_ == now)
+    // TODO( crbug.com/398090404 ): Remove the code in this condition as it is
+    // being superseded by the next block.
+    if (next_tick_time_ == now) {
       next_tick_time_ += interval_;
+    }
+
+    if (base::FeatureList::IsEnabled(
+            features::kAvoidDuplicateDelayBeginFrame)) {
+      // Some devices report vblank timings with enough variance as to confuse
+      // 'SnappedToNextTick' on the time remaining for next begin frame. This
+      // code allows for up to 'kMaxJudderAllowed' time for these values to
+      // simply be corrected to the next interval and thereby avoid an erroneous
+      // double tick. See crbug.com/398090404 for details.
+      const auto kMaxJudderAllowed = base::Microseconds(500);
+      if (now + kMaxJudderAllowed >= next_tick_time_) {
+        next_tick_time_ += interval_;
+      }
+    }
     DCHECK_GT(next_tick_time_, now);
     timer_.Start(FROM_HERE, next_tick_time_, tick_closure_,
                  base::subtle::DelayPolicy::kPrecise);
