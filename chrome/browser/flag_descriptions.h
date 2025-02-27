a807756203286d8aebb73eb2148b12bcda884867
Noam Rosenthal <nrosenthal@chromium.org>
2025-02-27 10:44:33 -0800

    Merge ViewTransitionOverflowRectFromSurface
    It's been stable for a while, this cleans up a lot of dead code.
    
    R=vmpstr@chromium.org
    
    Bug: 373759981
    Change-Id: I763b4caedc03d2d3bd5aded1535ebed2800e2a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6308563
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425843}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -680,9 +680,6 @@ extern const char kViewTransitionOnNavigationDescription[];
 extern const char kViewTransitionOnNavigationIframeName[];
 extern const char kViewTransitionOnNavigationIframeDescription[];
 
-extern const char kViewTransitionOverflowRectFromSurfaceName[];
-extern const char kViewTransitionOverflowRectFromSurfaceDescription[];
-
 extern const char kEnableAutofillAddressSavePromptName[];
 extern const char kEnableAutofillAddressSavePromptDescription[];
 
