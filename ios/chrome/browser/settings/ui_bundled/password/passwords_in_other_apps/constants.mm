990c98af20a501f544cdc3dbdb1aed5fb2dd5496
Noémie St-Onge <noemies@google.com>
2025-02-27 13:33:32 -0800

    [iOSPasskeysM2] Update passwords in other apps strings
    This CL updates strings related to the Passwords in Other Apps feature
    when the Passkeys M2 feature is on, and depending on the iOS version.
    
    Mocks:
    https://docs.google.com/presentation/d/1tferRiPZYcBw0CY_oXFqsYX_DTfUrYnV1x9zAAKjtW8/edit#slide=id.g3372317018e_1_47
    
    Bug: 394580626, 389690891
    Change-Id: I8b7ddb351af7142609f1ddfa5cb657b004711847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305583
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425957}

--- ios/chrome/browser/settings/ui_bundled/password/passwords_in_other_apps/constants.mm
+++ ios/chrome/browser/settings/ui_bundled/password/passwords_in_other_apps/constants.mm
@@ -19,3 +19,5 @@ NSString* const kPasswordsInOtherAppsActionAccessibilityIdentifier =
     @"kPasswordsInOtherAppsActionAccessibilityIdentifier";
 NSString* const kPasswordsInOtherAppsScrollViewAccessibilityIdentifier =
     @"kPasswordsInOtherAppsScrollViewAccessibilityIdentifier";
+NSString* const kPasswordsInOtherAppsTurnOffCaptionAccessibilityIdentifier =
+    @"PasswordsInOtherAppsTurnOffCaptionAccessibilityIdentifier";
