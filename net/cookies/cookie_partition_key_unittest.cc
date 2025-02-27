c453cf0749d33d16f8407d35031a82ae67b2d862
Abigail Katcoff <abigailkatcoff@chromium.org>
2025-02-27 08:21:28 -0800

    Handle NetworkIsolationPartition in FromNetworkIsolationKey
    Previously we added a CHECK in this method to prevent creating
    CookiePartitionKeys when we have non-general NetworkIsolationPartition.
    However, we discovered a new callsite where we'd need to avoid calling
    FromNetworkIsolationKey, and it's cleaner to just return nullopt.
    
    Bug: 396463430
    Change-Id: I2a172c3ff06ae1972c8ca955c63707fd05f9a8c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305404
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Abigail Katcoff <abigailkatcoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425766}

--- net/cookies/cookie_partition_key_unittest.cc
+++ net/cookies/cookie_partition_key_unittest.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cookies/cookie_partition_key.h"
 
+#include <optional>
 #include <string>
 #include <tuple>
 
 #include "base/test/scoped_feature_list.h"
 #include "net/base/features.h"
+#include "net/base/network_isolation_partition.h"
 #include "net/cookies/cookie_constants.h"
 #include "net/cookies/cookie_switches.h"
 #include "net/cookies/site_for_cookies.h"
@@ -259,6 +261,13 @@ TEST_P(CookiePartitionKeyTest, FromNetworkIsolationKey) {
        CookiePartitionKey::FromURLForTesting(kCookieSite.GetURL(), kCrossSite,
                                              kNonce),
        SiteForCookies::FromUrl(GURL::EmptyGURL()), SchemefulSite(kTopLevelSite),
+       /*main_frame_navigation=*/false},
+      {"WithNetworkIsolationPartition",
+       NetworkIsolationKey(
+           kTopLevelSite, kCookieSite, /*nonce=*/std::nullopt,
+           NetworkIsolationPartition::kProtectedAudienceSellerWorklet),
+       std::nullopt, SiteForCookies::FromUrl(GURL::EmptyGURL()),
+       SchemefulSite(kTopLevelSite),
        /*main_frame_navigation=*/false},
       {"WithCrossSiteAncestorSameSite",
        NetworkIsolationKey(kTopLevelSite, kTopLevelSite),
