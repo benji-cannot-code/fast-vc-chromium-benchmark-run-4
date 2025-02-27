83e54b8ac03bf541b1f9a4fbcb49db36223459f4
Taiyo Mizuhashi <taiyo@chromium.org>
2025-02-26 19:59:27 -0800

    [CCT Prefetch] Add use counter for CustomTabsConnection.prefetch
    This CL introduces `CustomTabs.Prefetch.PrefetchCalled` UMA, which
    records true if PCustomTabsConnection.prefetch` is called.
    
    Bug: 335331245
    Change-Id: Ia27a3d31dad66766c3a24346802a6c41ff851d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305982
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425551}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -698,6 +698,7 @@ public class CustomTabsConnection {
                                 + " CCTNavigationalPrefetch is not enabled.");
                 return;
             }
+            RecordHistogram.recordBooleanHistogram("CustomTabs.Prefetch.PrefetchCalled", true);
             prefetchInternal(new SessionHolder<>(session), urls, options);
         }
     }
