a99290cd502c6d62578d3069ae052e114087ab8c
Peter Kotwicz <pkotwicz@chromium.org>
2025-02-26 18:53:53 -0800

    Make HostContentSettingsMapTest.Observer work on Android
    The CL changes HostContentSettingsMapTest.Observer to not use
    HostContentSettingsMapFactory to build the HostContentSettingsMap.
    This enables the test to ignore UnusedSitePermissionsService and ContentSettingsType::REVOKED_UNUSED_SITE_PERMISSIONS.
    This is only an issue on Android due to the implementation of
    UnusedSitePermissionsServiceFactory::ServiceIsCreatedWithBrowserContext()
    
    BUG=398891214
    
    Change-Id: I38ae134c8ad332e01bcb75c23e7209ebeb88d2f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6302853
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425535}

--- chrome/browser/content_settings/host_content_settings_map_unittest.cc
+++ chrome/browser/content_settings/host_content_settings_map_unittest.cc
@@ -47,7 +47,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/features.h"
 #include "components/content_settings/core/common/host_indexed_content_settings.h"
 #include "components/content_settings/core/common/pref_names.h"
+#include "components/content_settings/core/test/content_settings_mock_provider.h"
 #include "components/content_settings/core/test/content_settings_test_utils.h"
+#include "components/keyed_service/core/refcounted_keyed_service.h"
 #include "components/permissions/features.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -216,6 +218,23 @@ class TesterForType {
   raw_ptr<HostContentSettingsMap> host_content_settings_map_;
   ContentSettingsType content_type_;
   const char* policy_default_setting_;
+};
+
+// For building HostContentSettingsMap without HostContentSettingsMapFactory.
+// Shuts down HostContentSettingsMap properly on destruction.
+class ScopedMapNoFactoryTester {
+ public:
+  scoped_refptr<HostContentSettingsMap> map;
+
+  explicit ScopedMapNoFactoryTester(TestingProfile& profile) {
+    map = new HostContentSettingsMap(profile.GetPrefs(),
+                                     /*is_off_the_record=*/false,
+                                     /*store_last_modified=*/false,
+                                     /*restore_session=*/false,
+                                     /*should_record_metrics=*/false);
+  }
+
+  ~ScopedMapNoFactoryTester() { map->ShutdownOnUIThread(); }
 };
 
 TEST_F(HostContentSettingsMapTest, DefaultValues) {
@@ -464,16 +483,13 @@ TEST_F(HostContentSettingsMapTest, Origins) {
                 host4, host4, ContentSettingsType::COOKIES));
 }
 
-// TODO(crbug.com/398891214): Make test pass on Android.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_Observer DISABLED_Observer
-#else
-#define MAYBE_Observer Observer
-#endif
-TEST_F(HostContentSettingsMapTest, MAYBE_Observer) {
+TEST_F(HostContentSettingsMapTest, Observer) {
   TestingProfile profile;
-  HostContentSettingsMap* host_content_settings_map =
-      HostContentSettingsMapFactory::GetForProfile(&profile);
+  // Use ScopedMapNoFactoryTester in order to enable the test to ignore
+  // UnusedSitePermissionsService and
+  // ContentSettingsType::REVOKED_UNUSED_SITE_PERMISSIONS.
+  ScopedMapNoFactoryTester map_tester(profile);
+  HostContentSettingsMap* host_content_settings_map = map_tester.map.get();
   MockSettingsObserver observer(host_content_settings_map);
 
   GURL host("http://example.com/");
