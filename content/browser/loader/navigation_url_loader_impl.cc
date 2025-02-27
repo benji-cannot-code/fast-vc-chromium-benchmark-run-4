9b49a5ea379978c1a519311b743058087dbec378
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-02-26 21:25:03 -0800

    Measure elapsed time to parse headers
    To understand the benefit of doing ParseHeaders() in the browser
    process, let me add metrics for that.
    
    Bug: 394974691
    Change-Id: I33d3ada15ebd83bc02d545db7d96a60301ec0d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6301690
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425565}

--- content/browser/loader/navigation_url_loader_impl.cc
+++ content/browser/loader/navigation_url_loader_impl.cc
@@ -1503,6 +1503,10 @@ void NavigationURLLoaderImpl::ParseHeaders(
 
   // If the network service is running in process, skip unnecessary thread hops.
   if (IsInProcessNetworkService() && !head->parsed_headers) {
+    base::ScopedUmaHistogramTimer in_process(
+        "Navigation.URLLoader.ParseHeaders.InProcessTime",
+        base::ScopedUmaHistogramTimer::ScopedHistogramTiming::
+            kMicrosecondTimes);
     head->parsed_headers =
         network::PopulateParsedHeaders(head->headers.get(), url);
   }
@@ -1520,13 +1524,18 @@ void NavigationURLLoaderImpl::ParseHeaders(
     // In debug mode, force reparsing the headers and check that they match.
     auto check = [](base::OnceClosure continuation,
                     network::mojom::URLResponseHead* head, GURL url,
+                    base::TimeTicks call_time,
                     network::mojom::ParsedHeadersPtr parsed_headers) {
+      base::UmaHistogramMicrosecondsTimes(
+          "Navigation.URLLoader.ParseHeaders.RoundTripTimeForVerify",
+          base::TimeTicks::Now() - call_time);
       CheckParsedHeadersEquals(parsed_headers, head->parsed_headers, url);
       std::move(continuation).Run();
     };
     GetNetworkService()->ParseHeaders(
         url, head->headers,
-        base::BindOnce(check, std::move(continuation), head, url));
+        base::BindOnce(check, std::move(continuation), head, url,
+                       base::TimeTicks::Now()));
 #else   // NDEBUG
     std::move(continuation).Run();
 #endif  // NDEBUG
@@ -1535,14 +1544,19 @@ void NavigationURLLoaderImpl::ParseHeaders(
 
   auto assign = [](base::OnceClosure continuation,
                    network::mojom::URLResponseHead* head,
+                   base::TimeTicks call_time,
                    network::mojom::ParsedHeadersPtr parsed_headers) {
+    base::UmaHistogramMicrosecondsTimes(
+        "Navigation.URLLoader.ParseHeaders.RoundTripTimeForNonNetworkResponse",
+        base::TimeTicks::Now() - call_time);
     head->parsed_headers = std::move(parsed_headers);
     std::move(continuation).Run();
   };
 
   GetNetworkService()->ParseHeaders(
       url, head->headers,
-      base::BindOnce(assign, std::move(continuation), head));
+      base::BindOnce(assign, std::move(continuation), head,
+                     base::TimeTicks::Now()));
 }
 
 // TODO(crbug.com/40552600): pass `navigation_ui_data` along with the
