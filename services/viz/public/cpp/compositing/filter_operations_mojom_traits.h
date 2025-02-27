5fbb2acb965a74c193eed7aad51e58271b3a575f
Colin Blundell <blundell@chromium.org>
2025-02-27 07:21:27 -0800

    [//cc] Support backdrop filters in TreesInViz
    TileDisplayLayerImpls need to know the following:
    
    * Whether they're a backdrop filter mask
    * What their ContentsResourceId is if they are a backdrop filter mask
    
    EffectNodes need to know the following:
    
    * What their backdrop mask element ID is (if any)
    * Their backdrop filters (if any)
    
    Change-Id: Ida85b83a62e401498830bbcffbd3e6addd44e307
    Bug: 397377184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298871
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425734}

--- services/viz/public/cpp/compositing/filter_operations_mojom_traits.h
+++ services/viz/public/cpp/compositing/filter_operations_mojom_traits.h
@@ -6,12 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_FILTER_OPERATIONS_MOJOM_TRAITS_H_
 #define SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_FILTER_OPERATIONS_MOJOM_TRAITS_H_
 
-#include "services/viz/public/mojom/compositing/filter_operations.mojom-shared.h"
+#include <vector>
 
-namespace cc {
-class FilterOperation;
-class FilterOperations;
-}  // namespace cc
+#include "cc/paint/filter_operations.h"
+#include "services/viz/public/mojom/compositing/filter_operations.mojom-shared.h"
 
 namespace mojo {
 
