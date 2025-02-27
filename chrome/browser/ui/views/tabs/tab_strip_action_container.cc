5c1b67a7eb969b1e340a50fa5f92dce12d7b100c
Jeffrey Cohen <jeffreycohen@google.com>
2025-02-26 16:47:58 -0800

    Reland "[CC] cleanup uma and ukm interaction calls"
    This is a reland of commit 1661b93ac825e59ee04813e39f9055f15bb9085b
    
    Original change's description:
    > [CC] cleanup uma and ukm interaction calls
    >
    > Bug: b:394350483
    > Change-Id: I6ad54190df4c1f57a6b72efe07f7250c34aa263f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6291999
    > Commit-Queue: Jeffrey Cohen <jeffreycohen@google.com>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1424707}
    
    Bug: b:394350483
    Change-Id: I7d4fa6bb50a1f3a3a27d200c0577c55c5d8696a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6306845
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425471}

--- chrome/browser/ui/views/tabs/tab_strip_action_container.cc
+++ chrome/browser/ui/views/tabs/tab_strip_action_container.cc
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/types/pass_key.h"
 #include "chrome/app/vector_icons/vector_icons.h"
-#include "chrome/browser/contextual_cueing/contextual_cueing_service.h"
-#include "chrome/browser/contextual_cueing/contextual_cueing_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
@@ -420,24 +418,16 @@ void TabStripActionContainer::OnGlicButtonClicked() {
                      : glic::InvocationSource::kTopChromeButton);
 
   if (glic_button_->GetIsShowingNudge()) {
-    auto* contextual_cueing_service =
-        contextual_cueing::ContextualCueingServiceFactory::GetForProfile(
-            tab_strip_controller_->GetProfile());
-    if (contextual_cueing_service) {
-      contextual_cueing_service->CueingNudgeClicked();
-    }
+    glic_nudge_controller_->OnNudgeActivity(
+        tabs::GlicNudgeActivity::kNudgeClicked);
   }
 
   ExecuteHideTabStripNudge(glic_button_);
 }
 
 void TabStripActionContainer::OnGlicButtonDismissed() {
-  auto* contextual_cueing_service =
-      contextual_cueing::ContextualCueingServiceFactory::GetForProfile(
-          tab_strip_controller_->GetProfile());
-  if (contextual_cueing_service) {
-    contextual_cueing_service->CueingNudgeDismissed();
-  }
+  glic_nudge_controller_->OnNudgeActivity(
+      tabs::GlicNudgeActivity::kNudgeDismissed);
 
   // Force hide the button when pressed, bypassing locked expansion mode.
   ExecuteHideTabStripNudge(glic_button_);
