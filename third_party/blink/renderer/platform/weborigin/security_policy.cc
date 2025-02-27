c7e2a2693ec1d386b538f7e7cffb6e9d1aec1a7e
Tom Sepez <tsepez@chromium.org>
2025-02-27 13:27:35 -0800

    Avoid unsafe_buffers in SecurityPolicy.
    Convert SecurityPolicy::ReferrerPolicyFromHeaderValue() to use the
    span form of SkipWhile<>().
    
    Bug: 351564777
    Change-Id: I2732b829689eff79e9130832dfdf2223f7b3dbd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309044
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425954}

--- third_party/blink/renderer/platform/weborigin/security_policy.cc
+++ third_party/blink/renderer/platform/weborigin/security_policy.cc
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/weborigin/security_policy.h"
 
 #include <memory>
@@ -327,11 +322,10 @@ bool SecurityPolicy::ReferrerPolicyFromHeaderValue(
     } else {
       Vector<UChar> characters;
       stripped_token.AppendTo(characters);
-      const UChar* position = characters.data();
-      UChar* end = characters.data() + characters.size();
-      SkipWhile<UChar, IsASCIIAlphaOrHyphen>(position, end);
-      if (position != end)
+      if (SkipWhile<UChar, IsASCIIAlphaOrHyphen>(characters, 0) !=
+          characters.size()) {
         return false;
+      }
     }
   }
 
