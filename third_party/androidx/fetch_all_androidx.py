b747b23e562a1ade1bf103d48e0ac83c81b2c531
Peter Wen <wnwen@chromium.org>
2025-02-27 09:40:22 -0800

    android: Skip cipd.yaml for androidx committed
    These files are not necessary and unused except when packaging (see
    fetch_all.py), so they do not need to be committed.
    
    OWNERS files are no longer generated and so no longer need to be omitted
    when generating the overall cipd.yaml file.
    
    Bug: 396437534
    Change-Id: Ie159ebe542111bbfdcada3bd2c5c5cd57182634d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310175
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425801}

--- third_party/androidx/fetch_all_androidx.py
+++ third_party/androidx/fetch_all_androidx.py
@@ -232,7 +232,7 @@ def _write_cipd_yaml(libs_dir,
             continue
 
         for lib_file in lib_files:
-            if lib_file == 'cipd.yaml' or lib_file == 'OWNERS':
+            if lib_file == 'cipd.yaml':
                 continue
             data_files.append(os.path.join(androidx_rel_lib_dir, lib_file))
 
@@ -352,7 +352,7 @@ def main():
     for root, _, files in os.walk(libs_dir):
         for file in files:
             # Avoid committing actual artifacts.
-            if file.endswith(('.aar', '.jar')):
+            if file.endswith(('.aar', '.jar', 'cipd.yaml')):
                 continue
             file_path = os.path.join(root, file)
             file_path_in_committed = os.path.relpath(file_path, _CIPD_PATH)
