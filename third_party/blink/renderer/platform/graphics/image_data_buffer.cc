d5413eb70b69a9f0ffa6e11a30b2ba88a212a370
Fredrik Söderquist <fs@opera.com>
2025-02-27 14:22:12 -0800

    Use gfx::SkPixmapToSpan() in ImageDataBuffer::PixelData()
    Eliminates one UNSAFE_BUFFERS().
    
    Bug: 351564777
    Change-Id: I085043a9cf60d486b34ce4d059db563f42a08ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309144
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425984}

--- third_party/blink/renderer/platform/graphics/image_data_buffer.cc
+++ third_party/blink/renderer/platform/graphics/image_data_buffer.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/encode/SkJpegEncoder.h"
 #include "third_party/skia/include/encode/SkPngEncoder.h"
+#include "ui/gfx/skia_span_util.h"
 
 namespace blink {
 
@@ -125,10 +126,7 @@ std::unique_ptr<ImageDataBuffer> ImageDataBuffer::Create(
 
 base::span<const uint8_t> ImageDataBuffer::PixelData() const {
   DCHECK(is_valid_);
-  // SAFETY: The creator of the SkPixmap ensures that pixmap_.addr() points to
-  // at least pixmap_.computeByteSize() bytes.
-  return UNSAFE_BUFFERS({static_cast<const unsigned char*>(pixmap_.addr()),
-                         pixmap_.computeByteSize()});
+  return gfx::SkPixmapToSpan(pixmap_);
 }
 
 bool ImageDataBuffer::EncodeImage(const ImageEncodingMimeType mime_type,
