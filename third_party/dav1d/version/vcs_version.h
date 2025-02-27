a65aeba60b094334f6f02524775062ad0666f97c
Wan-Teh Chang <wtc@google.com>
2025-02-26 21:56:13 -0800

    Roll src/third_party/dav1d/libdav1d/ 42b2b24fb..7d4b789f5 (1 commit)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/42b2b24fb881..7d4b789f5538
    
    $ git log 42b2b24fb..7d4b789f5 --date=short --no-merges --format='%ad %ae %s'
    2025-02-21 prka Mark C globals with small code model
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: I7e6217d4ad75743604c86a35d3f29f1980b9c788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305536
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425577}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.1-0-g42b2b24"
+#define DAV1D_VERSION "1.5.1-1-g7d4b789"
