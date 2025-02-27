393ea858a276dee9db3c6cb801994c9f75475667
Mohamed Heikal <mheikal@chromium.org>
2025-02-27 09:16:16 -0800

    Create a logfile when registering a new build
    Change-Id: I894703f6c6e2c1fbedc32b0e9924a4c5c4f75001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310037
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425789}

--- build/android/fast_local_dev_server.py
+++ build/android/fast_local_dev_server.py
@@ -382,10 +382,10 @@ class BuildManager:
       with cls._lock:
         if build.cwd is None:
           build.cwd = cwd
-          build.ensure_logfile()
         else:
           assert pathlib.Path(cwd).samefile(
               build.cwd), f'{repr(cwd)} != {repr(build.cwd)}'
+        build.ensure_logfile()
 
   @classmethod
   def get_build(cls, build_id):
