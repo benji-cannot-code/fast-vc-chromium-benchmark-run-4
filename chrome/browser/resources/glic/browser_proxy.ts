f9ad8e08d91a334bdb0088d08a08bb8efc50fd73
Justin DeWitt <dewittj@chromium.org>
2025-02-27 12:03:25 -0800

    [Glic] Add page handler logging in debug mode.
    This injects a proxy which logs to console each time a PageHandler
    method is called, including the arguments list. This is useful for
    understanding interaction of webui and Chrome. Enabled only if
    GlicDebugWebview is enabled.
    
    Change-Id: Iaffb9ba81ac548b6922002f1b15a568b4fbf2356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300818
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Auto-Submit: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425882}

--- chrome/browser/resources/glic/browser_proxy.ts
+++ chrome/browser/resources/glic/browser_proxy.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {loadTimeData} from '//resources/js/load_time_data.js';
+
 import {PageHandlerFactory, PageHandlerRemote, PageReceiver} from './glic.mojom-webui.js';
 import type {PageHandlerInterface, PageInterface} from './glic.mojom-webui.js';
 
@@ -10,13 +12,36 @@ export interface BrowserProxy {
   handler: PageHandlerInterface;
 }
 
+// Whether to enable PageHandler debug logging. Can be enabled with the
+// --enable-features=GlicDebugWebview command-line flag.
+const kEnableDebug = loadTimeData.getBoolean('enableDebug');
+
 export class BrowserProxyImpl implements BrowserProxy {
   handler: PageHandlerInterface;
   constructor(pageInterface: PageInterface) {
     const pageReceiver = new PageReceiver(pageInterface);
-    this.handler = new PageHandlerRemote();
+    const pageHandlerRemote = new PageHandlerRemote();
+    this.handler = pageHandlerRemote;
+    if (kEnableDebug) {
+      this.handler = new Proxy(pageHandlerRemote, {
+        get(target: PageHandlerRemote, name: keyof PageHandlerRemote,
+            receiver) {
+          const prop = Reflect.get(target, name, receiver);
+          if (!target.hasOwnProperty(name)) {
+            if (typeof prop === 'function') {
+              return function(this: PageHandlerRemote, ...args: any) {
+                /* eslint no-console: ["error", { allow: ["log"] }] */
+                console.log('PageHandler#', name, args);
+                return (prop as any).apply(this, args);
+              };
+            }
+          }
+          return prop;
+        },
+      });
+    }
     PageHandlerFactory.getRemote().createPageHandler(
-        (this.handler as PageHandlerRemote).$.bindNewPipeAndPassReceiver(),
+        pageHandlerRemote.$.bindNewPipeAndPassReceiver(),
         pageReceiver.$.bindNewPipeAndPassRemote());
   }
 }
