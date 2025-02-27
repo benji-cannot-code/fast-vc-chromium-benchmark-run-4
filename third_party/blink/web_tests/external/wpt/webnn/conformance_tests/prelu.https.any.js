c52256c7c4c10b028f4e0c29fb21f4dbd168ed12
Phillis Tang <phillis@chromium.org>
2025-02-26 16:50:27 -0800

    webnn: implement prelu on coreml
    Mostly done by emulation, only when input is 4D and slope match the
    input second dimension, uses coreml's prelu.
    
    Bug: 385173305
    Change-Id: I224071444341c0993f42eaed67482eda38ff6d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6307004
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425475}

--- third_party/blink/web_tests/external/wpt/webnn/conformance_tests/prelu.https.any.js
+++ third_party/blink/web_tests/external/wpt/webnn/conformance_tests/prelu.https.any.js
@@ -581,8 +581,8 @@ const preluTests = [
           'constant': true
         },
         'preluSlope': {
-          'data': [5.0114545822143555],
-          'descriptor': {shape: [1, 1, 1, 1], dataType: 'float32'},
+          'data': [5.0114545822143555, 5.0114545822143555],
+          'descriptor': {shape: [1, 2, 1, 1], dataType: 'float32'},
           'constant': true
         }
       },
