6a12f209c44fdc103456153e5dc7025ab02fe2ed
Saifuddin Hitawala <hitawala@chromium.org>
2025-02-27 14:51:51 -0800

    [gpu] Post delayed flush task through ThreadPool for DestroySharedImage
    When clients currently call SIInterface::DestroySharedImage they
    need to explicitly call SIInterface::Flush as that will flush the
    deferred destroy request.
    
    This change adds capability to PostDelayedTask using ThreadPool so
    that the DestroySharedImage deferred task(s) can be batched and
    flushed together after a certain delay.
    
    After this, we can avoid calling SIInterface::Flush that is needed
    for such DestroySharedImage cases.
    
    Bug: 399387909
    Change-Id: I079d6a8c30e9a1303ba94916177ed2f269278d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6302849
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425993}

--- gpu/ipc/client/gpu_channel_host.cc
+++ gpu/ipc/client/gpu_channel_host.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/task/thread_pool.h"
 #include "build/build_config.h"
 #include "gpu/config/gpu_finch_features.h"
 #include "gpu/ipc/client/client_shared_image_interface.h"
@@ -165,6 +166,26 @@ bool GpuChannelHost::IsConnected() {
   return static_cast<bool>(gpu_channel_);
 }
 #endif
+
+void GpuChannelHost::DelayedEnsureFlush(uint32_t deferred_message_id) {
+  AutoLock lock(deferred_message_lock_);
+  if (delayed_flush_deferred_message_id_) {
+    delayed_flush_deferred_message_id_ = deferred_message_id;
+  } else {
+    delayed_flush_deferred_message_id_ = deferred_message_id;
+    base::ThreadPool::PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(
+            [](GpuChannelHost* host) {
+              AutoLock lock(host->deferred_message_lock_);
+              host->InternalFlush(
+                  host->delayed_flush_deferred_message_id_.value());
+              host->delayed_flush_deferred_message_id_ = std::nullopt;
+            },
+            base::RetainedRef(this)),
+        kDelayForEnsuringFlush);
+  }
+}
 
 void GpuChannelHost::EnsureFlush(uint32_t deferred_message_id) {
   AutoLock lock(deferred_message_lock_);
