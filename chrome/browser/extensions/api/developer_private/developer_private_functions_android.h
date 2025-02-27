87573dc6938c3c47d2a51c0e16cde0aed281d295
Keigo Oka <oka@google.com>
2025-02-26 18:24:01 -0800

    extensions: Enable removeUserSpecifiedSites on Desktop Android
    Bug: b/392777363
    Change-Id: Ic9fce6b5f29da81bd070ff72300643281cd58057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6301333
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425520}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_android.h
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_android.h
@@ -90,9 +90,6 @@ DECLARE_EMPTY_FUNC(DeveloperPrivateAddHostPermissionFunction,
 DECLARE_EMPTY_FUNC(DeveloperPrivateRemoveHostPermissionFunction,
                    "developerPrivate.removeHostPermission",
                    DEVELOPERPRIVATE_REMOVEHOSTPERMISSION);
-DECLARE_EMPTY_FUNC(DeveloperPrivateRemoveUserSpecifiedSitesFunction,
-                   "developerPrivate.removeUserSpecifiedSites",
-                   DEVELOPERPRIVATE_REMOVEUSERSPECIFIEDSITES);
 DECLARE_EMPTY_FUNC(DeveloperPrivateGetUserAndExtensionSitesByEtldFunction,
                    "developerPrivate.getUserAndExtensionSitesByEtld",
                    DEVELOPERPRIVATE_GETUSERANDEXTENSIONSITESBYETLD);
