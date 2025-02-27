dbcddf6f806895ac957c5501913073206d6fb13c
Xi Han <hanxi@google.com>
2025-02-27 13:39:42 -0800

    [AppIntegration] Refactor buildDocument() based on type.
    In this CL, we change buildDocument() method to be a template method,
    and handle the class type check.
    
    Bug: 97457989
    Change-Id: I44eabea433f9500e48a0598bc98bffbdb65ef8a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6308947
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425960}

--- chrome/browser/auxiliary_search/javatests/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchDonorTest.java
+++ chrome/browser/auxiliary_search/javatests/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchDonorTest.java
@@ -134,7 +134,9 @@ public final class AuxiliarySearchDonorTest {
                             GenericDocument genericDocument = result.getGenericDocument();
                             WebPage webPage = genericDocument.toDocumentClass(WebPage.class);
 
-                            String documentId = AuxiliarySearchDonor.getDocumentId(mIds[i]);
+                            String documentId =
+                                    AuxiliarySearchDonor.getDocumentId(
+                                            AuxiliarySearchEntryType.TAB, mIds[i]);
                             assertEquals(documentId, genericDocument.getId());
                             assertEquals(
                                     mLastAccessTimestamps[i],
