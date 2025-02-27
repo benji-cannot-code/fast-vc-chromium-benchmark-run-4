68d8487a5bc883308de0ffe44911c166319a4ed8
Carlos IL <carlosil@chromium.org>
2025-02-27 09:51:35 -0800

    Enable network time querying on Android
    This is required for the secure time launch.
    
    Change-Id: Id3e16af84864706241606e08794a59e1722dc3ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309128
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425810}

--- components/network_time/network_time_tracker.cc
+++ components/network_time/network_time_tracker.cc
@@ -57,9 +57,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace network_time {
 
-// Network time queries are enabled on all desktop platforms except ChromeOS,
-// which uses tlsdated to set the system time.
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_IOS)
+// Network time queries are enabled on Android and all desktop platforms except
+// Chrome OS, which uses tlsdated to set the system time.
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_IOS)
 BASE_FEATURE(kNetworkTimeServiceQuerying,
              "NetworkTimeServiceQuerying",
              base::FEATURE_DISABLED_BY_DEFAULT);
