ab9fa7bc5dbe3c280b1f42eafaf9470d8c08cc34
Jenna Himawan <jhimawan@google.com>
2025-02-27 14:57:08 -0800

    [TabContextMenu] Add "close tab" option
    Bug: 398042939, 397249431
    Test: TabContextMenuCoordinatorUnitTest
    Change-Id: Ifeccea1461a628dc065e75416a23a78c95f710aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6306822
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425997}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabContextMenuCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabContextMenuCoordinator.java
@@ -24,6 +24,7 @@ import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.share.ShareUtils;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab_group_sync.TabGroupSyncServiceFactory;
+import org.chromium.chrome.browser.tabmodel.TabClosureParams;
 import org.chromium.chrome.browser.tabmodel.TabGroupModelFilter;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tasks.tab_management.ActionConfirmationManager;
@@ -141,6 +142,10 @@ public class TabContextMenuCoordinator extends TabOverflowMenuCoordinator<Intege
                 shareDelegate.share(tab, /* shareDirectly= */ false, TAB_STRIP_CONTEXT_MENU);
                 recordUserAction("ShareTab");
             } else if (menuId == R.id.close_tab) {
+                tabModelSupplier
+                        .get()
+                        .getTabRemover()
+                        .closeTabs(TabClosureParams.closeTab(tab).build(), /* allowDialog= */ true);
                 recordUserAction("CloseTab");
             }
         };
