7a37b9f1999f9292db8d60362c5fec71f63b4dcd
Etienne Dechamps <edechamps@google.com>
2025-02-27 12:09:11 -0800

    Restore protoc Java support
    Java support was dropped from //third_party/protobuf in
    https://crrev.com/c/6243314, but is is required by Cronet in AOSP, which
    uses //third_party/protobuf to compile protos to Java (as opposed to
    Chromium, which uses //third_party/android_build_tools/protoc).
    
    Bug: 397933339, 328417294
    Change-Id: Ica828c32e9f30854f4bf6c2362222e83fa5460e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6291330
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425891}

--- third_party/protobuf/src/google/protobuf/compiler/main.cc
+++ third_party/protobuf/src/google/protobuf/compiler/main.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "absl/log/initialize.h"
 #include "google/protobuf/compiler/command_line_interface.h"
 #include "google/protobuf/compiler/cpp/generator.h"
+#include "google/protobuf/compiler/java/generator.h"
 #include "google/protobuf/compiler/python/generator.h"
 #include "google/protobuf/compiler/python/pyi_generator.h"
 
@@ -46,6 +47,16 @@ int ProtobufMain(int argc, char* argv[]) {
   cpp_generator.set_opensource_runtime(true);
   cpp_generator.set_runtime_include_base(GOOGLE_PROTOBUF_RUNTIME_INCLUDE_BASE);
 #endif
+
+  // Proto2 Java
+  java::JavaGenerator java_generator;
+  cli.RegisterGenerator("--java_out", "--java_opt", &java_generator,
+                        "Generate Java source file.");
+
+#ifdef GOOGLE_PROTOBUF_RUNTIME_INCLUDE_BASE
+  java_generator.set_opensource_runtime(true);
+#endif
+
 
   // Proto2 Python
   python::Generator py_generator;
