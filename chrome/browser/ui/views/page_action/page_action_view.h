5403fec88698369de1b4d06ad2f89d48e0888761
Christopher Grant <cjgrant@chromium.org>
2025-02-27 09:34:12 -0800

    Reland "Support bubble toggling when clicking on page actions"
    This reverts commit 45b32737c62e350974c4646fe446f55008cc9ddc.
    
    Reason for revert: Relanding original CL with a fix (since the test
    view is now owned by a Widget, need to delete the Widget, not just
    close it).  Linux MSAN tests failed locally before, and pass with
    this fix. Also removed a spurious LOG() statement left over from
    debugging the original CL.
    
    Original change's description:
    > Revert "Support bubble toggling when clicking on page actions"
    >
    > This reverts commit 2b1a50c2ff21e0b7d6e16cfe295e0adb1dde0740.
    >
    > Reason for revert: Caused several unit_tests to fail, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/54200/overview
    >
    > Original change's description:
    > > Support bubble toggling when clicking on page actions
    > >
    > > The original page action system toggles action bubbles when the page
    > > action icon is repeatedly clicked. This change brings the new framework
    > > to parity with that behavior.
    > >
    > > See http://shortn/_bZEDYKdpwg for more details.
    > >
    > > Bug: 395901614
    > > Change-Id: Ib8112fbf850e2e1f6315a647b1b2c142930dc0aa
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6269811
    > > Reviewed-by: Caroline Rising <corising@chromium.org>
    > > Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    > > Reviewed-by: Dana Fried <dfried@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1424717}
    >
    > Bug: 395901614
    > Change-Id: I28afbd9d96d17487c4ad61e719a1109161f94c40
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6302807
    > Auto-Submit: Chan Li <chanli@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Chan Li <chanli@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1424884}
    
    Bug: 395901614
    Change-Id: Id01a3d566014f84da4301c53c48541e39281436d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305641
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425799}

--- chrome/browser/ui/views/page_action/page_action_view.h
+++ chrome/browser/ui/views/page_action/page_action_view.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/views/view.h"
 
+namespace ui {
+class MouseEvent;
+}  // namespace ui
+
 namespace page_actions {
 
 class PageActionController;
@@ -58,6 +62,8 @@ class PageActionView : public IconLabelBubbleView,
   bool ShouldUpdateInkDropOnClickCanceled() const override;
   void NotifyClick(const ui::Event& event) override;
   gfx::Size GetMinimumSize() const override;
+  bool OnMousePressed(const ui::MouseEvent& event) override;
+  void OnClickCanceled(const ui::Event& event) override;
 
   actions::ActionId GetActionId() const;
 
@@ -92,6 +98,12 @@ class PageActionView : public IconLabelBubbleView,
 
   // Indicates that the current page action is showing as a suggestion chip.
   bool showing_suggestion_chip_ = false;
+
+  // Used to track whether the mouse was pressed when associated ephemeral UI
+  // (eg. a bubble that closes on focus loss) was showing, to avoid
+  // re-triggering the action if so. This is necessary because the bubble will
+  // have closed by the time the view invokes the action on button click.
+  bool skip_action_invocation_ = false;
 };
 
 }  // namespace page_actions
