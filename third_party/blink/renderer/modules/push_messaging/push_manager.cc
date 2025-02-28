905beb3ab2508f0a6ffe6a504a32f389dcc679e1
Andrey Kosyakov <caseq@chromium.org>
2025-02-28 08:58:37 -0800

    Remove PushSubscriptionCallbacks
    These don't have to be WebCallbacks or CallbackPromiseAdapters now, as we're not passing them outside of blink, so just pass ScriptPromiseResolver<> directly.
    
    Change-Id: I6fff923ed67ad04d34db6dcfc566ccb7bf9846ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311211
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426373}

--- third_party/blink/renderer/modules/push_messaging/push_manager.cc
+++ third_party/blink/renderer/modules/push_messaging/push_manager.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/push_messaging/push_messaging_client.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_provider.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_subscription.h"
-#include "third_party/blink/renderer/modules/push_messaging/push_subscription_callbacks.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_subscription_options.h"
 #include "third_party/blink/renderer/modules/service_worker/service_worker_registration.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -112,14 +111,11 @@ ScriptPromise<PushSubscription> PushManager::subscribe(
 
     messaging_client->Subscribe(
         registration_, options,
-        LocalFrame::HasTransientUserActivation(window->GetFrame()),
-        std::make_unique<PushSubscriptionCallbacks>(resolver,
-                                                    /*null_allowed=*/false));
+        LocalFrame::HasTransientUserActivation(window->GetFrame()), resolver);
   } else {
     GetPushProvider(registration_)
         ->Subscribe(options, LocalFrame::HasTransientUserActivation(nullptr),
-                    std::make_unique<PushSubscriptionCallbacks>(
-                        resolver, /*null_allowed=*/false));
+                    resolver);
   }
 
   return promise;
@@ -129,12 +125,8 @@ ScriptPromise<IDLNullable<PushSubscription>> PushManager::getSubscription(
     ScriptState* script_state) {
   auto* resolver = MakeGarbageCollected<
       ScriptPromiseResolver<IDLNullable<PushSubscription>>>(script_state);
-  auto promise = resolver->Promise();
-
-  GetPushProvider(registration_)
-      ->GetSubscription(std::make_unique<PushSubscriptionCallbacks>(
-          resolver, /*null_allowed=*/true));
-  return promise;
+  GetPushProvider(registration_)->GetSubscription(resolver);
+  return resolver->Promise();
 }
 
 ScriptPromise<V8PermissionState> PushManager::permissionState(
--- third_party/blink/renderer/modules/push_messaging/push_provider.h
+++ third_party/blink/renderer/modules/push_messaging/push_provider.h
@@ -6,19 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PUSH_MESSAGING_PUSH_PROVIDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_PUSH_MESSAGING_PUSH_PROVIDER_H_
 
-#include <stdint.h>
-#include <memory>
-
-#include "base/memory/scoped_refptr.h"
-#include "base/task/single_thread_task_runner.h"
 #include "third_party/blink/public/mojom/push_messaging/push_messaging.mojom-blink.h"
 #include "third_party/blink/public/mojom/push_messaging/push_messaging_status.mojom-blink-forward.h"
-#include "third_party/blink/renderer/modules/push_messaging/push_subscription_callbacks.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/modules/service_worker/service_worker_registration.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
-#include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
@@ -27,6 +21,7 @@ enum class PushGetRegistrationStatus;
 enum class PushRegistrationStatus;
 }  // namespace mojom
 
+class PushSubscription;
 class PushSubscriptionOptions;
 
 class PushProvider final : public GarbageCollected<PushProvider>,
@@ -45,9 +40,10 @@ class PushProvider final : public GarbageCollected<PushProvider>,
 
   void Subscribe(PushSubscriptionOptions* options,
                  bool user_gesture,
-                 std::unique_ptr<PushSubscriptionCallbacks> callbacks);
-  void Unsubscribe(std::unique_ptr<PushUnsubscribeCallbacks> callbacks);
-  void GetSubscription(std::unique_ptr<PushSubscriptionCallbacks> callbacks);
+                 ScriptPromiseResolver<PushSubscription>* resolver);
+  void Unsubscribe(ScriptPromiseResolver<IDLBoolean>* resolver);
+  void GetSubscription(
+      ScriptPromiseResolver<IDLNullable<PushSubscription>>* resolver);
 
   void Trace(Visitor*) const override;
 
@@ -56,18 +52,19 @@ class PushProvider final : public GarbageCollected<PushProvider>,
   // established after the first call to this method.
   mojom::blink::PushMessaging* GetPushMessagingRemote();
 
-  void DidSubscribe(std::unique_ptr<PushSubscriptionCallbacks> callbacks,
+  void DidSubscribe(ScriptPromiseResolver<PushSubscription>* resolver,
                     mojom::blink::PushRegistrationStatus status,
                     mojom::blink::PushSubscriptionPtr subscription);
 
-  void DidUnsubscribe(std::unique_ptr<PushUnsubscribeCallbacks> callbacks,
+  void DidUnsubscribe(ScriptPromiseResolver<IDLBoolean>* resolver,
                       mojom::blink::PushErrorType error_type,
                       bool did_unsubscribe,
                       const WTF::String& error_message);
 
-  void DidGetSubscription(std::unique_ptr<PushSubscriptionCallbacks> callbacks,
-                          mojom::blink::PushGetRegistrationStatus status,
-                          mojom::blink::PushSubscriptionPtr subscription);
+  void DidGetSubscription(
+      ScriptPromiseResolver<IDLNullable<PushSubscription>>* resolver,
+      mojom::blink::PushGetRegistrationStatus status,
+      mojom::blink::PushSubscriptionPtr subscription);
 
   HeapMojoRemote<mojom::blink::PushMessaging> push_messaging_manager_;
 };
