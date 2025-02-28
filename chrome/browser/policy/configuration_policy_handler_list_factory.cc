7d51d70f8a3eeae60f02085b213617c5c7d3fa84
Michael Cui <mlcui@google.com>
2025-02-28 03:20:00 -0800

    scanner: Add enterprise policy
    See go/cros-scanner-policy for more details. After multiple revisions,
    this enterprise policy now does not require any additional custom policy
    handlers.
    
    Bug: b:388450143
    Change-Id: I6a6a210027d735332beb86c641ad678f2e223f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6211197
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426212}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1821,6 +1821,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kGenAISmartGroupingSettings,
     ash::prefs::kGenAISmartGroupingSettings,
     base::Value::Type::INTEGER},
+  { key::kGenAiChromeOsSmartActionsSettings,
+    ash::prefs::kScannerEnterprisePolicyAllowed,
+    base::Value::Type::INTEGER},
 #endif // BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(IS_LINUX)
@@ -3335,6 +3338,9 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
                                        ash::prefs::kGenAIPhotoEditingSettings);
   gen_ai_default_policies.emplace_back(key::kGenAISmartGroupingSettings,
                                        ash::prefs::kGenAISmartGroupingSettings);
+  gen_ai_default_policies.emplace_back(
+      key::kGenAiChromeOsSmartActionsSettings,
+      ash::prefs::kScannerEnterprisePolicyAllowed);
 #endif  // BUILDFLAG(IS_CHROMEOS)
   handlers->AddHandler(std::make_unique<GenAiDefaultSettingsPolicyHandler>(
       std::move(gen_ai_default_policies)));
