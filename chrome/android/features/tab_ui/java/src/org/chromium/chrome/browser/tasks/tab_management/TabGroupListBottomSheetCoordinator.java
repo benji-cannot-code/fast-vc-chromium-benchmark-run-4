54f554fb32c2e92323d3c693779fe9c96ecfd52f
Jenna Himawan <jhimawan@google.com>
2025-02-28 10:53:06 -0800

    [Tab Group Parity] Generalize tab group creation callback
    This will allow different usages to perform a different action when the tab group is newly created. For example, if we open the bottom sheet from the tab context menu on LFF's tab strip, we want to open the new tab group's context menu in the tab strip rather than using the tab group customization dialog.
    
    were never added)
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL (see crrev.com/6306533 - tests
    Bug: 391387522
    Change-Id: I9edcad8e69188fd14b17b7caf859cb84d845ca63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309683
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426456}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinator.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.Token;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
@@ -59,6 +60,16 @@ public class TabGroupListBottomSheetCoordinator {
         void onSheetClosed();
     }
 
+    /** A callback to run after a tab group is created. */
+    public interface TabGroupCreationCallback {
+        /**
+         * Responds to tab group creation.
+         *
+         * @param tabGroupId The tab group ID of the newly-created tab group.
+         */
+        void onTabGroupCreated(Token tabGroupId);
+    }
+
     private final TabGroupListBottomSheetView mView;
     private final BottomSheetController mBottomSheetController;
     private final SimpleRecyclerViewAdapter mSimpleRecyclerViewAdapter;
@@ -68,14 +79,14 @@ public class TabGroupListBottomSheetCoordinator {
     /**
      * @param context The {@link Context} to attach the bottom sheet to.
      * @param profile The current user profile.
-     * @param tabGroupCreationDialogManager Used to show the tab group creation dialog.
+     * @param tabGroupCreationCallback Used to follow up on tab group creation.
      * @param filter Used to read current tab groups.
      * @param bottomSheetController Used to interact with the bottom sheet.
      */
     public TabGroupListBottomSheetCoordinator(
             Context context,
             Profile profile,
-            TabGroupCreationDialogManager tabGroupCreationDialogManager,
+            TabGroupCreationCallback tabGroupCreationCallback,
             TabGroupModelFilter filter,
             BottomSheetController bottomSheetController) {
         mView = new TabGroupListBottomSheetView(context);
@@ -123,7 +134,7 @@ public class TabGroupListBottomSheetCoordinator {
                 new TabGroupListBottomSheetMediator(
                         modelList,
                         filter,
-                        tabGroupCreationDialogManager,
+                        tabGroupCreationCallback,
                         faviconResolver,
                         tabGroupSyncService,
                         dataSharingService,
