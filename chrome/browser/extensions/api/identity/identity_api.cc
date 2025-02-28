4035773931b70f6d0d21570e821ce67fa987be55
James Lee <ljjlee@google.com>
2025-02-28 05:12:49 -0800

    Remove unused includes from extensions identity API code
    Bug: 399628344
    Change-Id: I7cdb18db3d99217ba4ba8de684102850392a7b47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309082
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426242}

--- chrome/browser/extensions/api/identity/identity_api.cc
+++ chrome/browser/extensions/api/identity/identity_api.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/app_mode/app_mode_utils.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
--- chrome/browser/extensions/api/identity/identity_clear_all_cached_auth_tokens_function.cc
+++ chrome/browser/extensions/api/identity/identity_clear_all_cached_auth_tokens_function.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/identity/identity_api.h"
 #include "chrome/browser/extensions/api/identity/identity_constants.h"
-#include "chrome/browser/extensions/api/identity/web_auth_flow.h"
 #include "chrome/browser/profiles/profile.h"
 
 namespace extensions {
