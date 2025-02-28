2080387f239cee5a0cfbf7fad70fe159d642b712
Ioana Pandele <ioanap@chromium.org>
2025-02-28 03:47:12 -0800

    [UPMLoginDbSunset] Introduce CSV download dialog
    This CL introduces all the scaffolding for the CSV download component
    and wires first part of the flow.
    
    The flow is as follows:
    1. A modal dialog is shown with explanations on why GPM is unavailable
    and a way to download unmigrated passwords.
    2. If the user clicks "Export passwords", reauthentication is
    triggered.
       2a. If reauth isn't available a toast is shown telling the user to set up screen lock.
       2b. If reauth fails, the flow ends.
       2c. If reauth succeeds, the flow proceeds with step 3.
    3. An activity is launched that allows the user to choose the location
    and name for the file. It returns a URI to the document to write in.
    
    The flow will continue with file write in the next CL.
    
    
    
    Change-Id: Ie23177df69d6c7e41e72b98112301c027a509276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304709
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426221}

---
+++ chrome/browser/password_manager/android/pwm_disabled/java/src/org/chromium/chrome/browser/pwm_disabled/PasswordCsvDownloadFlowControllerFactory.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.pwm_disabled;
+
+/**
+ * Creates and holds a unique instance of a {@link PasswordCsvDownloadFlowController} for as long as
+ * the download flow is ongoing.
+ */
+public class PasswordCsvDownloadFlowControllerFactory {
+    private static PasswordCsvDownloadFlowController sController;
+
+    private PasswordCsvDownloadFlowControllerFactory() {}
+
+    public static PasswordCsvDownloadFlowController getOrCreateController() {
+        if (sController == null) {
+            sController =
+                    new PasswordCsvDownloadFlowController(
+                            PasswordCsvDownloadFlowControllerFactory::releaseController);
+        }
+        return sController;
+    }
+
+    private static void releaseController() {
+        sController = null;
+    }
+}
