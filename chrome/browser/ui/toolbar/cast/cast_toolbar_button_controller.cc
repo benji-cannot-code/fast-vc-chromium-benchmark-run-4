e9d5f265763b5f81065be1dc1017c16cf779f63e
David Sanders <dsanders11@ucsbalum.com>
2025-02-27 18:21:44 -0800

    Fix incorrect header included in cast_toolbar_button_controller.cc
    The incorrect header was inadvertently added in
    https://chromium-review.googlesource.com/c/chromium/src/+/5738787
    
    Change-Id: I2c9d6ced2905e716836293da72063c9dff3429d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6306833
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1426092}

--- chrome/browser/ui/toolbar/cast/cast_toolbar_button_controller.cc
+++ chrome/browser/ui/toolbar/cast/cast_toolbar_button_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/observer_list.h"
 #include "chrome/browser/media/router/media_router_feature.h"
+#include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_actions.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -28,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
-#include "ui/actions/action_id.h"
 
 CastToolbarButtonController::CastToolbarButtonController(Profile* profile)
     : CastToolbarButtonController(
