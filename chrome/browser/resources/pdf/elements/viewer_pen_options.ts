4cdd1c7a2d58ca2e724df506bcb528615e127e61
dpapad <dpapad@chromium.org>
2025-02-28 15:50:31 -0800

    PDF Viewer: Force TS compiler 'noUncheckedIndexedAccess=true' on CrOS.
    This allows turning on the ESLint
    @typescript-eslint/no-unnecessary-type-assertion check across both CrOS
    and Desktop builds which otherwise use different TS configurations due
    to CrOS code still using Polymer behind the 'enable_ink' GN flag, that
    forces CrOS builds to use tools/typsecript/tsconfig_base_polymer.json.
    
    Bug: 393525665
    Change-Id: Ia651de6c14cf2754af48c5fd527ac545a1fb9372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313222
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426609}

--- chrome/browser/resources/pdf/elements/viewer_pen_options.ts
+++ chrome/browser/resources/pdf/elements/viewer_pen_options.ts
@@ -221,7 +221,7 @@ export class ViewerPenOptionsElement extends PolymerElement {
 
   /** Used to lookup a string in a computed binding. */
   private lookup_(strings: {[key: string]: string}, name: string): string {
-    return strings ? strings[name] : '';
+    return strings ? strings[name]! : '';
   }
 
   /**
