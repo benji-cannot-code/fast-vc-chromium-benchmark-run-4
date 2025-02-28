93a76da0402776efebbe9aa38f96bcce8c3aa2e5
Connor Clark <cjamcl@chromium.org>
2025-02-28 14:42:46 -0800

    Add RemoteFontLoaded to trace font PostScriptName
    This is to provide a user-friendly string to display for the Performance
    panel's trace analysis in devtools.
    
    Bug: 369422196
    Change-Id: I56964535430a81631dc906918c973716ff01f43b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299979
    Commit-Queue: Connor Clark <cjamcl@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Auto-Submit: Connor Clark <cjamcl@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426578}

--- third_party/blink/renderer/core/css/css_font_face.cc
+++ third_party/blink/renderer/core/css/css_font_face.cc
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_font_face.h"
 
 #include <algorithm>
+
+#include "base/trace_event/typed_macros.h"
 #include "third_party/blink/renderer/core/css/css_font_face_source.h"
 #include "third_party/blink/renderer/core/css/css_font_selector.h"
 #include "third_party/blink/renderer/core/css/css_segmented_font_face.h"
@@ -36,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/remote_font_face_source.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
+#include "third_party/blink/renderer/platform/fonts/font_custom_platform_data.h"
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
 #include "third_party/blink/renderer/platform/fonts/simple_font_data.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
@@ -84,6 +87,14 @@ bool CSSFontFace::FontLoaded(CSSFontFaceSource* source) {
   for (CSSSegmentedFontFace* segmented_font_face : segmented_font_faces_) {
     segmented_font_face->FontFaceInvalidated();
   }
+
+  const FontCustomPlatformData* platform_data = source->GetCustomPlaftormData();
+  if (LoadStatus() == FontFace::kLoaded && platform_data) {
+    TRACE_EVENT("devtools.timeline", "RemoteFontLoaded", "url",
+                source->GetURL(), "name",
+                platform_data->GetPostScriptNameOrFamilyNameForInspector());
+  }
+
   return true;
 }
 
