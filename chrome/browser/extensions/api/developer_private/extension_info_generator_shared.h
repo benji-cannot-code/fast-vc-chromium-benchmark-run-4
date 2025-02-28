9622400a4cd54571851371ecd24f8ec536bc1519
Shuhei Takahashi <nya@chromium.org>
2025-02-27 18:33:14 -0800

    extensions: Pass around ExtensionInfo without unique_ptr
    This is a follow-up of crrev.com/c/6307985.
    
    Bug: b/392777363
    Cq-Include-Trybots: luci.chromium.try:android-desktop-15-x64-rel
    Change-Id: I3d1d2690eaf0b7bff5f3ce7b6a5a68c50a3906e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311690
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426098}

--- chrome/browser/extensions/api/developer_private/extension_info_generator_shared.h
+++ chrome/browser/extensions/api/developer_private/extension_info_generator_shared.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <memory>
-
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -93,10 +91,9 @@ class ExtensionInfoGeneratorShared : public ProfileObserver {
   //
   // This function fills only a portion of ExtensionInfo that is common to all
   // platforms. Subclasses may override this function to fill more members.
-  virtual void FillExtensionInfo(
-      const Extension& extension,
-      api::developer_private::ExtensionState state,
-      std::unique_ptr<api::developer_private::ExtensionInfo> info);
+  virtual void FillExtensionInfo(const Extension& extension,
+                                 api::developer_private::ExtensionState state,
+                                 api::developer_private::ExtensionInfo info);
 
   // Various systems, cached for convenience.
   raw_ptr<content::BrowserContext> browser_context_;
@@ -108,9 +105,8 @@ class ExtensionInfoGeneratorShared : public ProfileObserver {
 
  private:
   // Callback for the asynchronous image loading.
-  void OnImageLoaded(
-      std::unique_ptr<api::developer_private::ExtensionInfo> info,
-      const gfx::Image& image);
+  void OnImageLoaded(api::developer_private::ExtensionInfo info,
+                     const gfx::Image& image);
 
   // Returns the icon url for the default icon to use.
   std::string GetDefaultIconUrl(const std::string& name);
