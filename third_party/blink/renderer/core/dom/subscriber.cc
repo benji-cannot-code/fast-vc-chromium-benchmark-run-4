feb7a52a3a6587ce05d8860611ca8a8f8a00165d
Dominic Farolino <dom@chromium.org>
2025-02-28 07:19:11 -0800

    DOM: Fix 'ref-counted producer' Subscriber crash
    This CL fixes a crash in Subscriber, which was introduced in
    https://crrev.com/c/6221901, when we implemented ref-counted producers.
    With ref-counted producers, Subscriber::next()/error()/complete() have
    to iterate over the list of internal observers and call the respective
    methods on them. However, because these methods can terminate the
    subscription for a given observer, the list of internal observers can
    mutate while iterating over it, which is unsafe and causes a crash.
    
    This is essentially the implementation version of
    https://github.com/whatwg/infra/issues/396.
    
    This CL fixes this bug by taking a copy of the list of internal
    observers before iterating over it in each of these methods, so we can
    call the methods on each registered observer, while iterating over a
    stable vector that cannot be mutated (since it is a copy).
    
    R=masonf
    
    Bug: 40282760
    Change-Id: I9ade96a95370120b4c9f7309a78d3222398aed6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311209
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426289}

--- third_party/blink/renderer/core/dom/subscriber.cc
+++ third_party/blink/renderer/core/dom/subscriber.cc
@@ -104,7 +104,15 @@ void Subscriber::next(ScriptValue value) {
     return;
   }
 
-  for (auto& observer : internal_observers_) {
+  // Call `Next()` on all observers. Do this by iterating over a *copy* of the
+  // list of observers, because `Next()` can actually complete one of the
+  // observers' subscriptions, thus removing `observer` from
+  // `internal_observers_`. That means `internal_observers_` can be mutated
+  // throughout this process, and we cannot iterate over it while it is
+  // mutating.
+  HeapVector<Member<ObservableInternalObserver>> internal_observers =
+      internal_observers_;
+  for (auto& observer : internal_observers) {
     observer->Next(value);
   }
 }
@@ -119,7 +127,10 @@ void Subscriber::complete(ScriptState* script_state) {
   // `internal_observer` out before calling this.
   CloseSubscription(script_state, /*abort_reason=*/std::nullopt);
 
-  for (auto& observer : internal_observers_) {
+  // See the documentation in `Subscriber::next()`.
+  HeapVector<Member<ObservableInternalObserver>> internal_observers =
+      internal_observers_;
+  for (auto& observer : internal_observers) {
     observer->Complete();
   }
 }
@@ -149,7 +160,10 @@ void Subscriber::error(ScriptState* script_state, ScriptValue error_value) {
   // `internal_observer` out before calling this.
   CloseSubscription(script_state, error_value);
 
-  for (auto& observer : internal_observers_) {
+  // See the documentation in `Subscriber::next()`.
+  HeapVector<Member<ObservableInternalObserver>> internal_observers =
+      internal_observers_;
+  for (auto& observer : internal_observers) {
     observer->Error(script_state, error_value);
   }
 }
