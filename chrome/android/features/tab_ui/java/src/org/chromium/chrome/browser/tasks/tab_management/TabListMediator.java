5d448672c9b3df14f3f43f2b62183951fbb75ea9
Gurmeet Kalra <gurmeetk@google.com>
2025-02-28 11:19:10 -0800

    [XR] The tab overview is displayed in three columns span when full space mode is enabled.
    Overview image from -
      Moohan: https://drive.google.com/file/d/1HfTONtGxlIpIVqZpHhGlizEBasWNM8k0/view?usp=drive_link&resourcekey=0-53pK1_UKuPthuBxUKa9ecA
      Tangor: https://drive.google.com/file/d/1aGbRKUjRoVemz5UqNNZC68CvvZH6CyT0/view?usp=drive_link&resourcekey=0-_qHBEX5g5SvVJMYKepOCcQ
    
    Test: The tabs overview column counts on both Moohan and Tangor devices were manually verified. Attached images are available for reference.
    
    Bug: 399500045
    Change-Id: Idbc857387fe772de99051c0d292dc7881608cd6a
    Fixed: 399500045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310430
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Gurmeet Kalra <gurmeetk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426472}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -109,6 +109,7 @@ import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
+import org.chromium.ui.util.XrUtils;
 import org.chromium.url.GURL;
 
 import java.lang.annotation.Retention;
@@ -1631,6 +1632,11 @@ class TabListMediator implements TabListNotificationHandler {
      * multi-window mode on phone, the span count is fixed to 2 to keep tab card size reasonable.
      */
     private int getSpanCount(int screenWidthDp) {
+        if (XrUtils.isXrDevice()) {
+            // The layout span count is restricted to medium on XR immersive devices to display
+            // larger tab thumbnails, despite the large screen width.
+            return TabListCoordinator.GRID_LAYOUT_SPAN_COUNT_MEDIUM;
+        }
         if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(mActivity)) {
             return screenWidthDp < TabListCoordinator.MAX_SCREEN_WIDTH_COMPACT_DP
                     ? TabListCoordinator.GRID_LAYOUT_SPAN_COUNT_COMPACT
@@ -2849,5 +2855,9 @@ class TabListMediator implements TabListNotificationHandler {
         var oldValue = mActionsOnAllRelatedTabs;
         mActionsOnAllRelatedTabs = actionOnAllRelatedTabs;
         ResettersForTesting.register(() -> mActionsOnAllRelatedTabs = oldValue);
+    }
+
+    int getSpanCountForTesting(int screenWidthDp) {
+        return getSpanCount(screenWidthDp);
     }
 }
