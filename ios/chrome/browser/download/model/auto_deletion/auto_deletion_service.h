bfab8c0c0fda7b593310962e07daa0c5be995447
Daniel White <danieltwhite@google.com>
2025-02-28 09:36:54 -0800

    [iOS] Implement AutoDeletionService::ScheduleFileForDeletion
    In this CL, the AutoDeletionService received additional functionality
    that allows it to schedule the user's downloaded content with the
    Scheduler for automatic deletion one month from the downloaded date.
    
    In a follow up CL, the AutoDeletionService will add the ability to
    remove the downloaded content from user's device once the data is
    expired.
    
    This is multiple CL change:
      -  crrev.com/c/6219361
      => crrev.com/c/6304078
      -  crrev.com/c/6305579
    
    Bug: 390200553
    Change-Id: I9473cc4962960399bec4fe91152d47b8d7307b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304078
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426399}

--- ios/chrome/browser/download/model/auto_deletion/auto_deletion_service.h
+++ ios/chrome/browser/download/model/auto_deletion/auto_deletion_service.h
@@ -6,9 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_DOWNLOAD_MODEL_AUTO_DELETION_AUTO_DELETION_SERVICE_H_
 #define IOS_CHROME_BROWSER_DOWNLOAD_MODEL_AUTO_DELETION_AUTO_DELETION_SERVICE_H_
 
+#import <Foundation/Foundation.h>
+
+#import "base/memory/weak_ptr.h"
+#import "ios/chrome/browser/download/model/auto_deletion/scheduler.h"
+
 class PrefRegistrySimple;
+namespace web {
+class DownloadTask;
+}  // namespace web
 
 namespace auto_deletion {
+class Scheduler;
 
 // Service responsible for the orchestration of the various functionality within
 // the auto-deletion system.
@@ -19,6 +28,21 @@ class AutoDeletionService {
 
   // Registers the auto deletion Chrome settings status.
   static void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
+
+  // Schedules a file for auto-deletion.
+  void ScheduleFileForDeletion(web::DownloadTask* task);
+
+ private:
+  // Invoked after the download task data is read from data. It finishes
+  // scheduling the file for deletion.
+  void ScheduleFileForDeletionHelper(web::DownloadTask* task, NSData* data);
+
+  // The Scheduler object which tracks and manages the downloaded files
+  // scheduled for automatic deletion.
+  Scheduler scheduler_;
+
+  // Weak factory.
+  base::WeakPtrFactory<AutoDeletionService> weak_ptr_factory_{this};
 };
 
 }  // namespace auto_deletion
