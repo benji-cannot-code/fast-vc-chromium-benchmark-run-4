4f95ed90b833b9a4f720233a7e323f4e97b15bc1
Keigo Oka <oka@google.com>
2025-02-27 17:31:58 -0800

    extensions: Enable updateExtensionConfiguration on desktop android
    We defer supporting parameters needing the toolbar support until the UI
    is ready.
    
    Bug: b/392777363
    Change-Id: I6c350945b4ee9f8379b1a9867e9117e814dbc23f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6306753
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426074}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
@@ -19,8 +19,6 @@ DEFINE_EMPTY_FUNC(DeveloperPrivateGetExtensionSizeFunction,
                   "developerPrivate.getExtensionSize")
 DEFINE_EMPTY_FUNC(DeveloperPrivateGetProfileConfigurationFunction,
                   "developerPrivate.getProfileConfiguration")
-DEFINE_EMPTY_FUNC(DeveloperPrivateUpdateExtensionConfigurationFunction,
-                  "developerPrivate.updateExtensionConfiguration")
 DEFINE_EMPTY_FUNC(DeveloperPrivateReloadFunction, "developerPrivate.reload")
 DEFINE_EMPTY_FUNC(DeveloperPrivateLoadUnpackedFunction,
                   "developerPrivate.loadUnpacked")
--- chrome/browser/extensions/api/developer_private/developer_private_functions_shared.h
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_shared.h
@@ -138,6 +138,17 @@ class DeveloperPrivateUpdateProfileConfigurationFunction
   ResponseAction Run() override;
 };
 
+class DeveloperPrivateUpdateExtensionConfigurationFunction
+    : public DeveloperPrivateAPIFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("developerPrivate.updateExtensionConfiguration",
+                             DEVELOPERPRIVATE_UPDATEEXTENSIONCONFIGURATION)
+
+ protected:
+  ~DeveloperPrivateUpdateExtensionConfigurationFunction() override;
+  ResponseAction Run() override;
+};
+
 class DeveloperPrivateIsProfileManagedFunction : public ExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("developerPrivate.isProfileManaged",
