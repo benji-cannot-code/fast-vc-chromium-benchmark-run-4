7de556d7cb57d11a689b2bd73cce3cda3124cce4
Ari Chivukula <arichiv@chromium.org>
2025-02-28 10:07:16 -0800

    [Partitioned Popins] Add RSA Support (1) test main-frame
    We should ensure RSA calls to enable access to first-party cookies from
    within a popin work. We will need other tests to verify the UX flow.
    
    This CL is part of a series:
    (1) test main-frame
    (2) test sub-frame
    (3) add WPTs
    
    Explainer: https://explainers-by-googlers.github.io/partitioned-popins/
    I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/ApU_zUmpQ2g/
    
    Bug: 340606651
    Change-Id: Iff5b209c09207b7d172f45654ea70aeb14f2f54a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954128
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Sandor «Alex» Major <sandormajor@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426421}

--- content/public/browser/web_contents.h
+++ content/public/browser/web_contents.h
@@ -68,6 +68,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/jni_zero/jni_zero.h"
 #endif
 
+class StorageAccessGrantPermissionContext;
+
 namespace base {
 class FilePath;
 }  // namespace base
@@ -1640,6 +1642,13 @@ class WebContents : public PageNavigator, public base::SupportsUserData {
   // `kInvalidNetworkHandle` indicates that the current default network will
   // be bound.
   virtual net::handles::NetworkHandle GetTargetNetwork() = 0;
+
+  // Returns the origin of the popin's opener if this is a partitioned popin.
+  // CHECKS if this is not a partitioned popin, as it should never be called
+  // in that case. This is used in permissions checks.
+  // See https://explainers-by-googlers.github.io/partitioned-popins/
+  virtual GURL GetPartitionedPopinEmbedderOrigin(
+      base::PassKey<StorageAccessGrantPermissionContext>) const = 0;
 
  private:
   // This interface should only be implemented inside content.
