35521d7095eaf202e1b1182c0f898836192b5ebe
Keigo Oka <oka@google.com>
2025-02-28 10:36:45 -0800

    extensions: Enable getUserAndExtensionSitesByEtld on desktop android
    Bug: b/392777363
    Cq-Include-Trybots: luci.chromium.try:android-desktop-15-x64-rel
    Change-Id: I3ea8afaf65d2ba8e50deef60451757be2ef8549d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6312049
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426442}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_desktop.h
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_desktop.h
@@ -432,24 +432,6 @@ class DeveloperPrivateUpdateExtensionCommandFunction
   ResponseAction Run() override;
 };
 
-class DeveloperPrivateGetUserAndExtensionSitesByEtldFunction
-    : public DeveloperPrivateAPIFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("developerPrivate.getUserAndExtensionSitesByEtld",
-                             DEVELOPERPRIVATE_GETUSERANDEXTENSIONSITESBYETLD)
-  DeveloperPrivateGetUserAndExtensionSitesByEtldFunction();
-
-  DeveloperPrivateGetUserAndExtensionSitesByEtldFunction(
-      const DeveloperPrivateGetUserAndExtensionSitesByEtldFunction&) = delete;
-  DeveloperPrivateGetUserAndExtensionSitesByEtldFunction& operator=(
-      const DeveloperPrivateGetUserAndExtensionSitesByEtldFunction&) = delete;
-
- private:
-  ~DeveloperPrivateGetUserAndExtensionSitesByEtldFunction() override;
-
-  ResponseAction Run() override;
-};
-
 class DeveloperPrivateUpdateSiteAccessFunction
     : public DeveloperPrivateAPIFunction {
  public:
