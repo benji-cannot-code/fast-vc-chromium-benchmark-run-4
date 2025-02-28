bc3285c15e43d5da859c3fd854d25def624cdf2d
Jun Ishiguro <junis@google.com>
2025-02-27 19:14:12 -0800

    Remove unused browser_process include from c/b/ui/ash/app_access
    Bug: 393260137
    Change-Id: I1a27d5e0c7638143615177e2954b78934c39c3e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6277920
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426109}

--- chrome/browser/ui/ash/app_access/app_access_notifier.cc
+++ chrome/browser/ui/ash/app_access/app_access_notifier.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "components/account_id/account_id.h"
