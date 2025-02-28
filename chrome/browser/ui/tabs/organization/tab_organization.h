87a9f228b72f1bc5fde757e1bb34247a3fbc9f40
David Sanders <dsanders11@ucsbalum.com>
2025-02-28 11:59:02 -0800

    Do IWYU for tab_organization.h
    Bug: 40318405
    Change-Id: I1052cf3888d1582d87cd307cb4c5a5faf16d5b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309901
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1426492}

--- chrome/browser/ui/tabs/organization/tab_organization.h
+++ chrome/browser/ui/tabs/organization/tab_organization.h
@@ -6,13 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_TABS_ORGANIZATION_TAB_ORGANIZATION_H_
 #define CHROME_BROWSER_UI_TABS_ORGANIZATION_TAB_ORGANIZATION_H_
 
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
 #include "chrome/browser/ui/tabs/organization/tab_data.h"
-#include "components/optimization_guide/core/optimization_guide_model_executor.h"
+#include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 
