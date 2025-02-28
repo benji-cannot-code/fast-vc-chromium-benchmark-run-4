b81b14356e2ec1ae766ec2b4b9c14c5d71d0e5f0
Rakina Zata Amni <rakina@chromium.org>
2025-02-27 19:19:04 -0800

    Add isHistory/Back/Forward/Restore impl to AwNavigation
    This makes the Navigation information on par with what what was exposed
    through NavigationWebMessageSender.
    
    Bug: 359826084
    Change-Id: If586c9fd633dca45561ff6c64d51c6cf2bc1880e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298207
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426112}

--- content/public/android/java/src/org/chromium/content_public/browser/NavigationHandle.java
+++ content/public/android/java/src/org/chromium/content_public/browser/NavigationHandle.java
@@ -47,6 +47,10 @@ public class NavigationHandle {
     private long mNavigationId;
     private boolean mIsPageActivation;
     private boolean mIsReload;
+    private boolean mIsHistory;
+    private boolean mIsBack;
+    private boolean mIsForward;
+    private boolean mIsRestore;
     private @Nullable UserDataHost mUserDataHost;
     private boolean mIsPdf;
     private @Nullable String mMimeType;
@@ -116,6 +120,10 @@ public class NavigationHandle {
                 /* navigationId= */ 0,
                 /* isPageActivation= */ false,
                 isReload,
+                /* isHistory= */ false,
+                /* isBack= */ false,
+                /* isForward= */ false,
+                /* isRestore= */ false,
                 /* isPdf= */ false,
                 /* mimeType= */ "",
                 isSaveableNavigation,
@@ -145,6 +153,10 @@ public class NavigationHandle {
             long navigationId,
             boolean isPageActivation,
             boolean isReload,
+            boolean isHistory,
+            boolean isBack,
+            boolean isForward,
+            boolean isRestore,
             boolean isPdf,
             String mimeType,
             boolean isSaveableNavigation,
@@ -164,6 +176,10 @@ public class NavigationHandle {
         mNavigationId = navigationId;
         mIsPageActivation = isPageActivation;
         mIsReload = isReload;
+        mIsHistory = isHistory;
+        mIsBack = isBack;
+        mIsForward = isForward;
+        mIsRestore = isRestore;
         mIsPdf = isPdf;
         mMimeType = mimeType;
         mIsSaveableNavigation = isSaveableNavigation;
@@ -406,6 +422,30 @@ public class NavigationHandle {
     public boolean isReload() {
         assert mStarted;
         return mIsReload;
+    }
+
+    /** Whether this navigation is a history navigation or not. */
+    public boolean isHistory() {
+        assert mStarted;
+        return mIsHistory;
+    }
+
+    /** Whether this navigation is a back history navigation or not (offset < 0). */
+    public boolean isBack() {
+        assert mStarted;
+        return mIsBack;
+    }
+
+    /** Whether this navigation is a back history navigation or not (offset > 0). */
+    public boolean isForward() {
+        assert mStarted;
+        return mIsForward;
+    }
+
+    /** Whether this navigation was initiated by a session restore. */
+    public boolean isRestore() {
+        assert mStarted;
+        return mIsRestore;
     }
 
     /** Return any user data which has been set on the NavigationHandle. */
