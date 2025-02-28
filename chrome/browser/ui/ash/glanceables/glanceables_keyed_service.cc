385ca2d88fbfff40e775514db5da75bd9aa77098
Jun Ishiguro <junis@google.com>
2025-02-27 19:12:54 -0800

    Remove unused browser_process.h include from c/b/ui/ash/glanceables
    Bug: 393260137
    Change-Id: I3365517bc693c2beff30c339c574fc57e8f55beb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6286066
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426107}

--- chrome/browser/ui/ash/glanceables/glanceables_keyed_service.cc
+++ chrome/browser/ui/ash/glanceables/glanceables_keyed_service.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/time/default_clock.h"
 #include "chrome/browser/ash/api/tasks/tasks_client_impl.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/ash/glanceables/glanceables_classroom_client_impl.h"
