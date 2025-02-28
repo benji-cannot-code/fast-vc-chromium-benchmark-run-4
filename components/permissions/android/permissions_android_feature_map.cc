7182ca1aacc7e44b89f7e5e86c62c9bb087a5b19
Peter Kotwicz <pkotwicz@chromium.org>
2025-02-28 12:13:56 -0800

    Add kill switch for OsAdditionalSecurityPermissionUtil
    This CL adds
    permissions::features::kOsAdditionalSecurityPermissionKillSwitch for
    disabling the new
    OsAdditionalSecurityPermissionUtil#hasJavascriptOptimizerPermission()
    behavior.
    
    BUG=393153126
    
    Change-Id: Iad218c4db44c7a09a2b75d937637a9db9ffd42bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6271764
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426503}

--- components/permissions/android/permissions_android_feature_map.cc
+++ components/permissions/android/permissions_android_feature_map.cc
@@ -23,6 +23,7 @@ namespace {
 const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidCancelPermissionPromptOnTouchOutside,
     &features::kOneTimePermission,
+    &features::kOsAdditionalSecurityPermissionKillSwitch,
 };
 
 // static
