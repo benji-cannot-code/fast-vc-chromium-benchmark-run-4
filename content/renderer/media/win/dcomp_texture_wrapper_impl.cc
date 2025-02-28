9a9395440184ac47659e963da65e4ce3ed3cf0d8
Saifuddin Hitawala <hitawala@chromium.org>
2025-02-28 12:30:04 -0800

    [media] Avoid SIInterface::Flush in texture wrappers
    Now that the ClientSharedImage destructor calls DestroySharedImage
    which in turn can ensure that the deferred destroy request is flushed,
    clients don't need to call SharedImageInterface::Flush explicitly.
    
    This change removes Flush call from StreamTextureWrapperImpl's
    video frame release callback on Android and DCOMPTextureWrapperImpl's
    video frame release callback on Windows.
    
    Bug: 399387909
    Change-Id: I81a5f59c7c0205c7b62510c4789e36866c46d4ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309670
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426514}

--- content/renderer/media/win/dcomp_texture_wrapper_impl.cc
+++ content/renderer/media/win/dcomp_texture_wrapper_impl.cc
@@ -44,8 +44,6 @@ class DCOMPTextureMailboxResources
     return shared_image_;
   }
 
-  DCOMPTextureFactory* Factory() { return factory_.get(); }
-
  private:
   friend class base::RefCounted<DCOMPTextureMailboxResources>;
 
@@ -76,9 +74,9 @@ void OnReleaseVideoFrame(
   DVLOG(1) << __func__;
 
   dcomp_texture_resources->SetSyncToken(sync_token);
-  gpu::SharedImageInterface* sii =
-      dcomp_texture_resources->Factory()->SharedImageInterface();
-  sii->Flush();
+  // ClientSharedImage destructor calls DestroySharedImage which in turn ensures
+  // that the deferred destroy request is flushed. Thus, clients don't need to
+  // call SharedImageInterface::Flush explicitly.
 }
 
 }  // namespace
