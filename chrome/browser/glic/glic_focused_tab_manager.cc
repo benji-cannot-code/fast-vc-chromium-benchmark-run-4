6b776e119211b71803778ae63f8d18c13e0f36ea
Marke Hallowell <wry@chromium.org>
2025-02-27 17:49:18 -0800

    Address bug in focused tab behavior for Glic when non-active browser windows with focus are closed directly.
    Bug: 399478101
    Change-Id: I5e1298953272cd40096b248eb7b26c137a485d36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311152
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426079}

--- chrome/browser/glic/glic_focused_tab_manager.cc
+++ chrome/browser/glic/glic_focused_tab_manager.cc
@@ -77,6 +77,7 @@ void GlicFocusedTabManager::OnBrowserAdded(Browser* browser) {
 void GlicFocusedTabManager::OnBrowserRemoved(Browser* browser) {
   // Remove the browser if it exists in the map.
   browser_subscriptions_.erase(browser);
+  MaybeUpdateFocusedTab();
 }
 
 void GlicFocusedTabManager::OnBrowserBecameActive(
