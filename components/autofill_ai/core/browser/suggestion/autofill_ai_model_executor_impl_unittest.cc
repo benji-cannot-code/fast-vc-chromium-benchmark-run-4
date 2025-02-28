6a3c91050ae1013b6fa2a19969dc3e05c5c2bb05
Jan Keitel <jkeitel@google.com>
2025-02-28 09:15:20 -0800

    Remove field eligibility/sensitivity maps
    This CL stops uploading field's values. With that, eligibility and
    sensitivity are no longer used in code:
    - Eligibility was used to determine which fields are eligible for form
      import, but form import is no longer handled by a model.
    - Sensitivity was used to determine whether to upload a field's value,
      but field values are no longer uploaded at all.
    
    Bug: 389631477
    Change-Id: I9d7655b1c2219d391bb5600374994cf42c5c15a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310096
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426385}

--- components/autofill_ai/core/browser/suggestion/autofill_ai_model_executor_impl_unittest.cc
+++ components/autofill_ai/core/browser/suggestion/autofill_ai_model_executor_impl_unittest.cc
@@ -70,15 +70,13 @@ TEST_F(AutofillAiModelExecutorImplTest, ValidResponse) {
           _, An<OptimizationGuideModelExecutionResultCallback>()))
       .WillOnce(base::test::RunOnceCallback<3>(
           OptimizationGuideModelExecutionResult(
-
               optimization_guide::AnyWrapProto(AutofillAiTypeResponse()),
               /*execution_info=*/nullptr),
           /*log_entry=*/nullptr));
 
   base::test::TestFuture<PredictionsOrError, std::optional<std::string>>
       test_future;
-  engine()->GetPredictions(autofill::FormData(), /*field_eligibility_map=*/{},
-                           /*field_sensitivity_map=*/{},
+  engine()->GetPredictions(autofill::FormData(),
                            optimization_guide::proto::AXTreeUpdate(),
                            test_future.GetCallback());
   EXPECT_TRUE(test_future.Get<0>().has_value());
@@ -101,8 +99,7 @@ TEST_F(AutofillAiModelExecutorImplTest, ModelError) {
 
   base::test::TestFuture<PredictionsOrError, std::optional<std::string>>
       test_future;
-  engine()->GetPredictions(autofill::FormData(), /*field_eligibility_map=*/{},
-                           /*field_sensitivity_map=*/{},
+  engine()->GetPredictions(autofill::FormData(),
                            optimization_guide::proto::AXTreeUpdate(),
                            test_future.GetCallback());
   EXPECT_FALSE(test_future.Get<0>().has_value());
@@ -121,7 +118,7 @@ TEST_F(AutofillAiModelExecutorImplTest, WrongTypeReturned) {
 
   base::test::TestFuture<PredictionsOrError, std::optional<std::string>>
       test_future;
-  engine()->GetPredictions(autofill::FormData(), {}, {},
+  engine()->GetPredictions(autofill::FormData(),
                            optimization_guide::proto::AXTreeUpdate(),
                            test_future.GetCallback());
   EXPECT_FALSE(test_future.Get<0>().has_value());
