7a89de58bb3972b66c8a625f5228772251a34ac4
John Lee <johntlee@chromium.org>
2025-02-28 12:01:50 -0800

    History: Migrate history-synced-device-manager to Lit
    Bug: 40943652
    Change-Id: I04e4ab2a913702e0bbdc2f83ebed8eca79a5bca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300368
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426494}

--- chrome/browser/resources/history/synced_device_card.ts
+++ chrome/browser/resources/history/synced_device_card.ts
@@ -6,10 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_collapse/cr_collapse.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/icons.html.js';
-import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import './searched_label.js';
-import './shared_style.css.js';
-import './shared_vars.css.js';
 import '/strings.m.js';
 
 import type {CrCollapseElement} from 'chrome://resources/cr_elements/cr_collapse/cr_collapse.js';
