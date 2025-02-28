d6e1e06d7e62f18cb2ae928bdf8bc3af76cb00a3
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-02-28 12:37:38 -0800

    [iOS][Mojo] Enhanced calendar mojo interface and service, menu hook up
    Mojo interface for enhanced calendar, its service implementation, and
    the service being used in the AI prototyping menu. This interface takes
    in a proto wrapper, and also outputs a proto wrapper or an MES
    error string if there is one. Users of this service presumably wouldn't
    simply want a string representation of the response proto, so they
    receive the proto itself, or the error string if there isn't a response
    proto.
    
    Bug: 390621435
    Change-Id: I5fcd651359802ad893fa214673affa086acaa242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297885
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426515}

--- ios/chrome/browser/ai_prototyping/ui/ai_prototyping_calendar_view_controller.mm
+++ ios/chrome/browser/ai_prototyping/ui/ai_prototyping_calendar_view_controller.mm
@@ -14,9 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/l10n/l10n_util.h"
 
 @implementation AIPrototypingCalendarViewController {
-  UITextView* _responseContainer;
-  UIButton* _submitButton;
+  UITextField* _selectedTextField;
   UITextField* _promptField;
+  UIButton* _submitButton;
+  UITextView* _responseContainer;
 }
 
 // Synthesized from `AIPrototypingViewControllerProtocol`.
@@ -47,19 +48,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleTitle2];
   label.text = l10n_util::GetNSString(IDS_IOS_AI_PROTOTYPING_CALENDAR_HEADER);
 
+  // Selected date/time text field.
+  _selectedTextField = [[UITextField alloc] init];
+  _selectedTextField.translatesAutoresizingMaskIntoConstraints = NO;
+  _selectedTextField.placeholder = l10n_util::GetNSString(
+      IDS_IOS_AI_PROTOTYPING_CALENDAR_SELECTED_TEXT_PLACEHOLDER);
+  UIView* selectedTextFieldContainer = [self textFieldContainer];
+  [selectedTextFieldContainer addSubview:_selectedTextField];
+
   // Optional user query.
   _promptField = [[UITextField alloc] init];
   _promptField.translatesAutoresizingMaskIntoConstraints = NO;
   _promptField.placeholder = l10n_util::GetNSString(
       IDS_IOS_AI_PROTOTYPING_CALENDAR_PROMPT_PLACEHOLDER);
-
-  UIView* promptFieldContainer = [[UIView alloc] init];
-  promptFieldContainer.translatesAutoresizingMaskIntoConstraints = NO;
-  promptFieldContainer.layer.cornerRadius = kCornerRadius;
-  promptFieldContainer.layer.masksToBounds = YES;
-  promptFieldContainer.layer.borderColor =
-      [[UIColor colorNamed:kTextPrimaryColor] CGColor];
-  promptFieldContainer.layer.borderWidth = kBorderWidth;
+  UIView* promptFieldContainer = [self textFieldContainer];
   [promptFieldContainer addSubview:_promptField];
 
   // Submit button.
@@ -85,7 +87,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _responseContainer.textContainer.lineBreakMode = NSLineBreakByWordWrapping;
 
   UIStackView* stackView = [[UIStackView alloc] initWithArrangedSubviews:@[
-    label, promptFieldContainer, _submitButton, _responseContainer
+    label, selectedTextFieldContainer, promptFieldContainer, _submitButton,
+    _responseContainer
   ]];
   stackView.translatesAutoresizingMaskIntoConstraints = NO;
   stackView.axis = UILayoutConstraintAxisVertical;
@@ -104,6 +107,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         constraintGreaterThanOrEqualToAnchor:self.view.heightAnchor
                                   multiplier:
                                       kResponseContainerHeightMultiplier],
+
+    [selectedTextFieldContainer.heightAnchor
+        constraintEqualToAnchor:_selectedTextField.heightAnchor
+                       constant:kVerticalInset],
+    [selectedTextFieldContainer.widthAnchor
+        constraintEqualToAnchor:_selectedTextField.widthAnchor
+                       constant:kHorizontalInset],
+    [selectedTextFieldContainer.centerXAnchor
+        constraintEqualToAnchor:_selectedTextField.centerXAnchor],
+    [selectedTextFieldContainer.centerYAnchor
+        constraintEqualToAnchor:_selectedTextField.centerYAnchor],
 
     [promptFieldContainer.heightAnchor
         constraintEqualToAnchor:_promptField.heightAnchor
@@ -142,6 +156,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)disableSubmitButton {
   _submitButton.enabled = NO;
   _submitButton.backgroundColor = [UIColor colorNamed:kDisabledTintColor];
+}
+
+// Creates a text field container.
+- (UIView*)textFieldContainer {
+  UIView* container = [[UIView alloc] init];
+  container.layer.masksToBounds = YES;
+  container.layer.cornerRadius = kCornerRadius;
+  container.layer.borderColor =
+      [[UIColor colorNamed:kTextPrimaryColor] CGColor];
+  container.layer.borderWidth = kBorderWidth;
+  container.translatesAutoresizingMaskIntoConstraints = NO;
+  return container;
 }
 
 @end
