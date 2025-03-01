8b286efe5fca3ac4baa76dda73a6a86321bfc0f3
David Trainor <dtrainor@chromium.org>
2025-02-28 19:49:20 -0800

    Add log support to CollaborationService
    Add logs for state transitions, record events, and errors to the
    CollaborationService to help track down bugs in the wild.
    
    Bug: 398039103
    Change-Id: Ief2fef4d3dc2ab6034a1c1adec717eedcae9d451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6302298
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426680}

--- components/collaboration/internal/metrics.h
+++ components/collaboration/internal/metrics.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_COLLABORATION_INTERNAL_METRICS_H_
 #define COMPONENTS_COLLABORATION_INTERNAL_METRICS_H_
 
+namespace data_sharing {
+class Logger;
+}  // namespace data_sharing
+
 namespace collaboration::metrics {
 
 // Types of join events that occur in the collaboration service.
@@ -45,7 +49,7 @@ enum class CollaborationServiceJoinEvent {
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/collaboration_service/enums.xml:CollaborationServiceJoinEvent)
 
-// Types of join events that occur in the collaboration service.
+// Types of share or manage events that occur in the collaboration service.
 // These values are persisted to logs. Entries should not be renumbered and
 // number values should never be reused.
 // LINT.IfChange(CollaborationServiceShareOrManageEvent)
@@ -77,8 +81,10 @@ enum class CollaborationServiceShareOrManageEvent {
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/collaboration_service/enums.xml:CollaborationServiceShareOrManageEvent)
 
-void RecordJoinEvent(CollaborationServiceJoinEvent event);
-void RecordShareOrManageEvent(CollaborationServiceShareOrManageEvent event);
+void RecordJoinEvent(data_sharing::Logger* logger,
+                     CollaborationServiceJoinEvent event);
+void RecordShareOrManageEvent(data_sharing::Logger* logger,
+                              CollaborationServiceShareOrManageEvent event);
 }  // namespace collaboration::metrics
 
 #endif  // COMPONENTS_COLLABORATION_INTERNAL_METRICS_H_
