36e244a6fee239f04fc4cc7f5fd48873a0d89dd8
Dan Harrington <harringtond@chromium.org>
2025-02-28 16:34:47 -0800

    Add variant of TruncateUTF8ToByteSize that returns value
    This can be more ergonomic, and can avoid copying.
    
    Change-Id: I10b6e4a79573152ecbd53d4bced3b071d75f71f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309944
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426633}

--- base/strings/string_util.cc
+++ base/strings/string_util.cc
@@ -154,9 +154,13 @@ void TruncateUTF8ToByteSize(const std::string& input,
                             const size_t byte_size,
                             std::string* output) {
   DCHECK(output);
+  *output = TruncateUTF8ToByteSize(input, byte_size);
+}
+
+std::string_view TruncateUTF8ToByteSize(std::string_view input,
+                                        size_t byte_size) {
   if (byte_size > input.length()) {
-    *output = input;
-    return;
+    return input;
   }
   DCHECK_LE(byte_size,
             static_cast<uint32_t>(std::numeric_limits<int32_t>::max()));
@@ -182,9 +186,9 @@ void TruncateUTF8ToByteSize(const std::string& input,
   }
 
   if (char_index >= 0) {
-    *output = input.substr(0, static_cast<size_t>(char_index));
+    return input.substr(0, static_cast<size_t>(char_index));
   } else {
-    output->clear();
+    return std::string_view();
   }
 }
 
