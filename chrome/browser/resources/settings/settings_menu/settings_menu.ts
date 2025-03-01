44daa0cab9304bfbc5319efd62dd725cdbf230b1
David Bokan <bokan@chromium.org>
2025-02-28 16:28:40 -0800

    Move Glic settings page
    This moves the Glic chrome://settings page from a top-level menu item
    into a subpage under the existing AI experiences page. This requires
    merging glic_data_page into glic_page since these are now in a nested
    section.
    
    Fixed: 393445109
    Change-Id: I58575b049635969977cd00ba2f19cd243d91d225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305388
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426630}

--- chrome/browser/resources/settings/settings_menu/settings_menu.ts
+++ chrome/browser/resources/settings/settings_menu/settings_menu.ts
@@ -76,13 +76,6 @@ export class SettingsMenuElement extends SettingsMenuElementBase {
         type: String,
         computed: 'computeAiPageTitle_(enableAiSettingsPageRefresh_)',
       },
-
-      // <if expr="enable_glic">
-      glicEnabled_: {
-        type: Boolean,
-        value: () => loadTimeData.getBoolean('showGlicSettings'),
-      },
-      // </if>
     };
   }
 
@@ -94,9 +87,6 @@ export class SettingsMenuElement extends SettingsMenuElementBase {
   private aiPageTitle_: string;
   private metricsBrowserProxy_: MetricsBrowserProxy =
       MetricsBrowserProxyImpl.getInstance();
-  // <if expr="enable_glic">
-  private glicEnabled_: boolean;
-  // </if>
 
   override ready() {
     super.ready();
@@ -107,13 +97,6 @@ export class SettingsMenuElement extends SettingsMenuElementBase {
     return this.showAdvancedFeaturesMainControl_ &&
         (!this.pageVisibility || this.pageVisibility.ai !== false);
   }
-
-  // <if expr="enable_glic">
-  private showGlicMenuItem_(): boolean {
-    return this.glicEnabled_ &&
-        (!this.pageVisibility || this.pageVisibility.glic !== false);
-  }
-  // </if>
 
   private computeAiPageIcon_(): string {
     return this.enableAiSettingsPageRefresh_ ? 'settings20:magic' :
