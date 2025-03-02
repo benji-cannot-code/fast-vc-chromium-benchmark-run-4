afa08ec7346a2781e06f20409cced0d0e15b5e83
Keigo Oka <oka@google.com>
2025-03-01 17:30:36 -0800

    extensions: Enable updateSiteAccess on desktop android
    Bug: b/392777363
    Cq-Include-Trybots: luci.chromium.try:android-desktop-15-x64-rel
    Change-Id: If0c47c8ff98a08e2646f2bf8d5cedd628fb2edd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6312215
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426781}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
@@ -43,8 +43,6 @@ DEFINE_EMPTY_FUNC(DeveloperPrivateSetShortcutHandlingSuspendedFunction,
                   "developerPrivate.setShortcutHandlingSuspended")
 DEFINE_EMPTY_FUNC(DeveloperPrivateUpdateExtensionCommandFunction,
                   "developerPrivate.updateExtensionCommand")
-DEFINE_EMPTY_FUNC(DeveloperPrivateUpdateSiteAccessFunction,
-                  "developerPrivate.updateSiteAccess")
 DEFINE_EMPTY_FUNC(DeveloperPrivateRemoveMultipleExtensionsFunction,
                   "developerPrivate.removeMultipleExtensions")
 DEFINE_EMPTY_FUNC(
--- chrome/browser/extensions/api/developer_private/developer_private_functions_shared.h
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_shared.h
@@ -313,6 +313,26 @@ class DeveloperPrivateGetMatchingExtensionsForSiteFunction
   ResponseAction Run() override;
 };
 
+class DeveloperPrivateUpdateSiteAccessFunction
+    : public DeveloperPrivateAPIFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("developerPrivate.updateSiteAccess",
+                             DEVELOPERPRIVATE_UPDATESITEACCESS)
+  DeveloperPrivateUpdateSiteAccessFunction();
+
+  DeveloperPrivateUpdateSiteAccessFunction(
+      const DeveloperPrivateUpdateSiteAccessFunction&) = delete;
+  DeveloperPrivateUpdateSiteAccessFunction& operator=(
+      const DeveloperPrivateUpdateSiteAccessFunction&) = delete;
+
+ private:
+  ~DeveloperPrivateUpdateSiteAccessFunction() override;
+
+  ResponseAction Run() override;
+
+  void OnSiteSettingsUpdated();
+};
+
 class DeveloperPrivateDismissSafetyHubExtensionsMenuNotificationFunction
     : public DeveloperPrivateAPIFunction {
  public:
