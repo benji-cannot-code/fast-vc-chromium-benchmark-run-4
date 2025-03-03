bd0e3c8a7efce8ebafce664ef00092fddc8bdc99
Michael Cui <mlcui@google.com>
2025-03-02 19:43:39 -0800

    scanner: Add debug shortcut entry-point metrics
    This should not typically be emitted by end-user devices, but this
    shortcut can be enabled in chrome://flags.
    
    Bug: b:397106119
    Change-Id: I6a6a2100ce0eeababa92e9c2dcb962e442a8d96a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311849
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426918}

--- ash/accelerators/debug_commands.cc
+++ ash/accelerators/debug_commands.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/system/toast_data.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/root_window_controller.h"
+#include "ash/scanner/scanner_metrics.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/style/ash_color_id.h"
@@ -340,6 +341,8 @@ void HandleShowSystemNudge() {
 void HandleStartSunfishSession() {
   if (CanShowSunfishOrScannerUi() &&
       !Shell::Get()->session_controller()->IsUserSessionBlocked()) {
+    RecordScannerFeatureUserState(
+        ScannerFeatureUserState::kSunfishSessionStartedFromDebugShortcut);
     CaptureModeController::Get()->StartSunfishSession();
   }
 }
--- ash/capture_mode/sunfish_unittest.cc
+++ ash/capture_mode/sunfish_unittest.cc
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/aura_constants.h"
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
+#include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/geometry/rect.h"
@@ -229,6 +230,22 @@ TEST_F(SunfishDisabledScannerDisabledTest, AccelEntryPointIsNoop) {
 
   auto* controller = CaptureModeController::Get();
   EXPECT_FALSE(controller->IsActive());
+}
+
+// Tests that the accelerator entry point does not emit metrics when neither
+// Sunfish nor Scanner is enabled.
+TEST_F(SunfishDisabledScannerDisabledTest, AccelEntryPointDoesNotEmitMetrics) {
+  base::HistogramTester histogram_tester;
+  histogram_tester.ExpectBucketCount(
+      "Ash.ScannerFeature.UserState",
+      ScannerFeatureUserState::kSunfishSessionStartedFromDebugShortcut, 0);
+
+  PressAndReleaseKey(ui::VKEY_8,
+                     ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN | ui::EF_SHIFT_DOWN);
+
+  histogram_tester.ExpectBucketCount(
+      "Ash.ScannerFeature.UserState",
+      ScannerFeatureUserState::kSunfishSessionStartedFromDebugShortcut, 0);
 }
 
 TEST_F(SunfishDisabledScannerDisabledTest,
@@ -461,6 +478,21 @@ TEST_F(SunfishTest, AccelEntryPoint) {
       controller->capture_mode_session()->active_behavior();
   ASSERT_TRUE(active_behavior);
   EXPECT_EQ(active_behavior->behavior_type(), BehaviorType::kSunfish);
+}
+
+// Tests that the accelerator entry point emits the correct metrics.
+TEST_F(SunfishTest, AccelEntryPointMetrics) {
+  base::HistogramTester histogram_tester;
+  histogram_tester.ExpectBucketCount(
+      "Ash.ScannerFeature.UserState",
+      ScannerFeatureUserState::kSunfishSessionStartedFromDebugShortcut, 0);
+
+  PressAndReleaseKey(ui::VKEY_8,
+                     ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN | ui::EF_SHIFT_DOWN);
+
+  histogram_tester.ExpectBucketCount(
+      "Ash.ScannerFeature.UserState",
+      ScannerFeatureUserState::kSunfishSessionStartedFromDebugShortcut, 1);
 }
 
 // Tests that the accelerator entry point is a no-op when the enabled pref is
