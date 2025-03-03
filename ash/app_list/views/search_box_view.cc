766b327e933cc5c45ab1b98e4083b1cd2c0c0e69
Michael Cui <mlcui@google.com>
2025-03-02 19:44:47 -0800

    scanner: Add launcher button entry-point metrics
    Bug: b:397106119
    Change-Id: I6a6a2100824b0cb804a09e946988a6e2b43d68a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310497
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426920}

--- ash/app_list/views/search_box_view.cc
+++ ash/app_list/views/search_box_view.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/capture_mode/capture_mode_api.h"
 #include "ash/public/cpp/wallpaper/wallpaper_types.h"
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/scanner/scanner_metrics.h"
 #include "ash/search_box/search_box_constants.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -1286,6 +1287,8 @@ void SearchBoxView::SunfishButtonPressed() {
   // so set the pref to its limit.
   SetSunfishLauncherNudgeShownCount(capture_mode::kSunfishNudgeMaxShownCount);
 
+  RecordScannerFeatureUserState(
+      ScannerFeatureUserState::kSunfishSessionStartedFromLauncherButton);
   CaptureModeController::Get()->StartSunfishSession();
 }
 
--- ash/scanner/scanner_metrics.h
+++ ash/scanner/scanner_metrics.h
@@ -117,8 +117,9 @@ enum class ScannerFeatureUserState {
   kSmartActionsButtonNotShownDueToOffline = 51,
 
   kSunfishSessionStartedFromDebugShortcut = 52,
+  kSunfishSessionStartedFromLauncherButton = 53,
 
-  kMaxValue = kSunfishSessionStartedFromDebugShortcut,
+  kMaxValue = kSunfishSessionStartedFromLauncherButton,
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/ash/enums.xml:ScannerFeatureUserState)
 
