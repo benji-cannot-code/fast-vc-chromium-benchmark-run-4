02b582cf8c5264f52c60858f56551ce25075a9d1
Daniel Cheng <dcheng@chromium.org>
2025-03-03 12:55:38 -0800

    Remove multiple argument support from base::ToString()
    This can be used to support:
    - string concatenation, but `base::StrCat()` already works for this. In
      theory, this could be nice for stringifying multiple non-string types
      in a single call to `base::ToString()`, but in practice, it's never
      actually used—and if it were, it'd be a lot less efficient (in both
      performance and binary size) than `base::StrCat`.
    - iomanip operators, such as `std::hex`—but this is never actually used
      outside tests. `std::hex` would probably be the most common use of
      this, but base already has some `HexEncode()` helpers—adding overloads
      to handle arbitrary numeric types would be a reasonable extension to
      these overloads.
    
    Bug: 399888636
    Change-Id: I0e7648dc3629b3748b0e86b2268501746793e043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314545
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427329}

--- base/strings/to_string.h
+++ base/strings/to_string.h
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-template <typename... Ts>
-std::string ToString(const Ts&... values);
+template <typename T>
+std::string ToString(const T& values);
 
 namespace internal {
 
@@ -137,14 +137,12 @@ struct ToStringHelper<std::tuple<T...>> {
 }  // namespace internal
 
 // Converts any type to a string, preferring defined operator<<() or ToString()
-// methods if they exist. If multiple `values` are given, returns the
-// concatenation of the result of applying `ToString` to each value.
-template <typename... Ts>
-std::string ToString(const Ts&... values) {
+// methods if they exist.
+template <typename T>
+std::string ToString(const T& value) {
   std::ostringstream ss;
-  (...,
-   internal::ToStringHelper<std::remove_cvref_t<decltype(values)>>::Stringify(
-       values, ss));
+  internal::ToStringHelper<std::remove_cvref_t<decltype(value)>>::Stringify(
+      value, ss);
   return ss.str();
 }
 
