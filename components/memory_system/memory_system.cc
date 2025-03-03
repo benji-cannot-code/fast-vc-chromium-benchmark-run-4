4dcc1cbc159a4580401de092eb50e7427a429fc7
Tarcisio Fischer <tarcisio.fischer@arm.com>
2025-03-03 05:08:54 -0800

    Replace FORCE_ALLOCATION_TRACE_RECORDER build flag with llvm equivalent
    The goal is to decrease the number of build variables involving the
    Allocation Trace Recorder, thus replacing FORCE_ALLOCATION_TRACE_RECORDER
    with LLVM's __LLVM_INSTR_PROFILE_GENERATE build flag.
    
    Change-Id: I91b3519a45e54e440a87fdcaf2b1d12ca957993f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6158684
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427068}

--- components/memory_system/memory_system.cc
+++ components/memory_system/memory_system.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/debugging_buildflags.h"
 #include "build/build_config.h"
 #include "components/gwp_asan/buildflags/buildflags.h"
-#include "components/memory_system/buildflags.h"
 #include "components/memory_system/memory_system_features.h"
 #include "components/memory_system/parameters.h"
 #include "partition_alloc/buildflags.h"
