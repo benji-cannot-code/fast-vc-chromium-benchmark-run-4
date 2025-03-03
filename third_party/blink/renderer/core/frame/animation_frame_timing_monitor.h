afc0698c6a737834d48915b6624d6823c4cf74a9
Joone Hur <joonehur@microsoft.com>
2025-03-03 01:20:17 -0800

    Remove unnecessary #includes from animation_frame_timiming_monitor.h
    This CL has no behavior changes.
    
    Bug: none
    Change-Id: I04f3bfc6cf385bc7f59267c984f6bfb24a13e8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314891
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427003}

--- third_party/blink/renderer/core/frame/animation_frame_timing_monitor.h
+++ third_party/blink/renderer/core/frame/animation_frame_timing_monitor.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/frame/frame.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/core/timing/animation_frame_timing_info.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
