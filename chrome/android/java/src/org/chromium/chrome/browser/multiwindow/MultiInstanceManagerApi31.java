75c9ee70bae9bdb75f4464c80778b2a8ce30981d
Xinyi Ji <xinyiji@google.com>
2025-03-03 10:24:05 -0800

    [EducationalTip] Implement trigger scenarios for tab groups promo card (Part 3)
    This CL addresses an edge case where the tab count is incorrectly reported as 0 when TabModelSelector#isTabStateInitialized() is false, and tabs are still being loaded during startup. This issue causes the "number_of_tabs" signal to be inaccurate and affect the display of tab group promo card. To resolve this, the CL records the tab count before Chrome is paused and becomes invisible to the user, storing it in shared preferences. When the tab state is not yet initialized, this stored value is used for the "number_of_tabs" signal.
    
    The method for creating and removing Shared Preference keys varies depending on the Android version. Here's a breakdown of the two approaches:
    
    1. Android Versions Greater than R:
    Android versions greater than R have the Multi-Instance API enabled, allowing Chrome to have multiple windows. These windows persist even when Chrome is swiped away from Android Recents, and the activity is destroyed. They can only be closed through the Instance Manager UI.
    Therefore, we used Shared Preference Key prefix "Chrome.MultiInstance.TabCountForRelaunch" and the window ID is used as the index for the key. The key is removed in MultiInstanceManagerApi31#removeInstanceInfo when the window is permanently closed.
    
    2. Android Versions R and Below:
    The Multi-Instance API is disabled on Android R and below, limiting Chrome to a maximum of two windows: one instance of ChromeTabbedActivity and one of ChromeTabbedActivity2. We can only have two windows in split screen mode. When one window is closed, its tabs are merged into the remaining window. Upon relaunching, the remaining window always initializes as a ChromeTabbedActivity instance. Consequently, only one Shared Preference key, "Chrome.MultiInstance.TabCountForRelaunch.0" is utilized in this scenario.
    
    Bug: 355015904
    Change-Id: Ia89037807db6774890a2e2179d35e223346233a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063506
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427195}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
@@ -89,8 +89,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
     private static final String EMPTY_DATA = "";
     private static MultiInstanceState sState;
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    protected final int mMaxInstances;
+    @VisibleForTesting protected final int mMaxInstances;
 
     private ObservableSupplier<ModalDialogManager> mModalDialogManagerSupplier;
 
@@ -185,7 +184,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
                 getInstanceInfo());
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     void moveTabAction(InstanceInfo info, Tab tab, int tabAtIndex) {
         Activity targetActivity = getActivityById(info.instanceId);
         if (targetActivity != null) {
@@ -200,7 +199,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         }
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     void moveTabGroupAction(InstanceInfo info, TabGroupMetadata tabGroupMetadata, int startIndex) {
         Activity targetActivity = getActivityById(info.instanceId);
         assert targetActivity != null;
@@ -208,7 +207,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
                 (ChromeTabbedActivity) targetActivity, tabGroupMetadata, startIndex);
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     void moveAndReparentTabToNewWindow(
             Tab tab,
             int instanceId,
@@ -226,7 +225,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
                 null);
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     void reparentTabToRunningActivity(
             ChromeTabbedActivity targetActivity, Tab tab, int tabAtIndex) {
         Intent intent = createIntentForGeneralReparenting(targetActivity, tabAtIndex);
@@ -236,7 +235,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         bringTaskForeground(targetActivity.getTaskId());
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     void reparentTabGroupToRunningActivity(
             ChromeTabbedActivity targetActivity,
             TabGroupMetadata tabGroupMetadata,
@@ -336,7 +335,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         return allInstances.get(0).instanceId;
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     protected boolean isRunningInAdjacentWindow(
             SparseBooleanArray visibleTasks, Activity activity) {
         assert activity != mActivity;
@@ -463,7 +462,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         writeTabCount(mInstanceId, selector);
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     protected void installTabModelObserver() {
         TabModelSelector selector = mTabModelOrchestratorSupplier.get().getTabModelSelector();
         mTabModelObserver =
@@ -536,7 +535,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         return ChromePreferenceKeys.MULTI_INSTANCE_TASK_MAP.createKey(String.valueOf(index));
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static void updateTaskMap(int instanceId, int taskId) {
         ChromeSharedPreferences.getInstance().writeInt(taskMapKey(instanceId), taskId);
     }
@@ -630,7 +629,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         }
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static Activity getActivityById(int id) {
         TabWindowManager windowManager = TabWindowManagerSingleton.getInstance();
         for (Activity activity : getAllRunningActivities()) {
@@ -677,28 +676,30 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
                 mMaxInstances + 1);
     }
 
-    private static String incognitoSelectedKey(int index) {
+    @VisibleForTesting
+    static String incognitoSelectedKey(int index) {
         return ChromePreferenceKeys.MULTI_INSTANCE_IS_INCOGNITO_SELECTED.createKey(
                 String.valueOf(index));
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static void writeIncognitoSelected(int index, Tab tab) {
         ChromeSharedPreferences.getInstance()
                 .writeBoolean(incognitoSelectedKey(index), tab.isIncognito());
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static boolean readIncognitoSelected(int index) {
         return ChromeSharedPreferences.getInstance()
                 .readBoolean(incognitoSelectedKey(index), false);
     }
 
-    private static String urlKey(int index) {
+    @VisibleForTesting
+    static String urlKey(int index) {
         return ChromePreferenceKeys.MULTI_INSTANCE_URL.createKey(String.valueOf(index));
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static String readUrl(int index) {
         return ChromeSharedPreferences.getInstance().readString(urlKey(index), null);
     }
@@ -712,11 +713,12 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         writeUrl(index, tab.getOriginalUrl().getSpec());
     }
 
-    private static String titleKey(int index) {
+    @VisibleForTesting
+    static String titleKey(int index) {
         return ChromePreferenceKeys.MULTI_INSTANCE_TITLE.createKey(String.valueOf(index));
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static String readTitle(int index) {
         return ChromeSharedPreferences.getInstance().readString(titleKey(index), null);
     }
@@ -730,26 +732,32 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         ChromeSharedPreferences.getInstance().writeString(titleKey(index), title);
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static String tabCountKey(int index) {
         return ChromePreferenceKeys.MULTI_INSTANCE_TAB_COUNT.createKey(String.valueOf(index));
+    }
+
+    @VisibleForTesting
+    static String tabCountForRelaunchKey(int index) {
+        return MultiWindowUtils.getTabCountForRelaunchKey(index);
     }
 
     static int readTabCount(int index) {
         return ChromeSharedPreferences.getInstance().readInt(tabCountKey(index));
     }
 
-    private static String incognitoTabCountKey(int index) {
+    @VisibleForTesting
+    static String incognitoTabCountKey(int index) {
         return ChromePreferenceKeys.MULTI_INSTANCE_INCOGNITO_TAB_COUNT.createKey(
                 String.valueOf(index));
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static int readIncognitoTabCount(int index) {
         return ChromeSharedPreferences.getInstance().readInt(incognitoTabCountKey(index));
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static void writeTabCount(int index, TabModelSelector selector) {
         if (!selector.isTabStateInitialized()) return;
         SharedPreferencesManager prefs = ChromeSharedPreferences.getInstance();
@@ -766,7 +774,8 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         return readLastAccessedTime(index) != 0;
     }
 
-    private static String lastAccessedTimeKey(int index) {
+    @VisibleForTesting
+    static String lastAccessedTimeKey(int index) {
         return MultiWindowUtils.lastAccessedTimeKey(index);
     }
 
@@ -774,7 +783,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         return MultiWindowUtils.readLastAccessedTime(index);
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     static void writeLastAccessedTime(int index) {
         MultiWindowUtils.writeLastAccessedTime(index);
     }
@@ -862,10 +871,11 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
 
     /**
      * Close a given task/activity instance.
+     *
      * @param instanceId ID of the activity instance.
      * @param taskId ID of the task including the activity.
      */
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     protected void closeInstance(int instanceId, int taskId) {
         removeInstanceInfo(instanceId);
         TabModelSelector selector =
@@ -937,11 +947,13 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         super.onDestroy();
     }
 
-    private static void removeInstanceInfo(int index) {
+    @VisibleForTesting
+    static void removeInstanceInfo(int index) {
         SharedPreferencesManager prefs = ChromeSharedPreferences.getInstance();
         prefs.removeKey(urlKey(index));
         prefs.removeKey(titleKey(index));
         prefs.removeKey(tabCountKey(index));
+        prefs.removeKey(tabCountForRelaunchKey(index));
         prefs.removeKey(incognitoTabCountKey(index));
         prefs.removeKey(incognitoSelectedKey(index));
         prefs.removeKey(lastAccessedTimeKey(index));
@@ -1063,7 +1075,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         }
     }
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     InstanceInfo getInstanceInfoFor(Activity activity) {
         // Loop thru all instances to determine if the destination activity is present.
         int destinationWindowTaskId = INVALID_TASK_ID;
--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
@@ -1253,7 +1253,7 @@ public class TabPersistentStore {
         return modelInfo;
     }
 
-    private static boolean shouldSkipTab(@NonNull Tab tab) {
+    public static boolean shouldSkipTab(@NonNull Tab tab) {
         boolean isNtp = tab.isNativePage() && UrlUtilities.isNtpUrl(tab.getUrl());
         if (!isNtp) return false;
 
