813786feb60b2555ad90b0736157773d6e580de6
Chuong Ho <hdchuong@google.com>
2025-03-02 19:24:38 -0800

    lobster: add block reason metrics
    Bug: b:348279517
    Change-Id: I0767546dfe3916d0592b3adeeca7c6f5f7888b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6312214
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426915}

--- ash/lobster/lobster_metrics_recorder.h
+++ ash/lobster/lobster_metrics_recorder.h
@@ -7,11 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_LOBSTER_LOBSTER_METRICS_RECORDER_H_
 
 #include "ash/ash_export.h"
+#include "ash/public/cpp/lobster/lobster_enums.h"
 #include "ash/public/cpp/lobster/lobster_metrics_state_enums.h"
 
 namespace ash {
 
 void ASH_EXPORT RecordLobsterState(LobsterMetricState state);
+
+void ASH_EXPORT RecordLobsterBlockedReason(LobsterSystemCheck failed_reason);
 
 }  // namespace ash
 
