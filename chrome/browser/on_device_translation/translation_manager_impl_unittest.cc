ea9eeec64975cb048b95bfc1464c0824d407f669
Nathan Memmott <memmott@chromium.org>
2025-03-03 10:36:59 -0800

    Translator: Remove requirement source or target is preferred
    Removes the requirement that either the source language or the target
    language is in the user's preferred languages.
    
    Fixed: 395910479
    Change-Id: I48228974bce016cca6206ff6b2292b284df3b115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313601
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427217}

--- chrome/browser/on_device_translation/translation_manager_impl_unittest.cc
+++ chrome/browser/on_device_translation/translation_manager_impl_unittest.cc
@@ -117,8 +117,7 @@ TEST_F(TranslationManagerImplTest, PassAcceptLanguagesCheck) {
   // Target lang:
   //   - Is in accept-languages : false
   //   - Is popular lang        : true
-  // None of source and target lang is in accept-languages.
-  EXPECT_FALSE(PassAcceptLanguagesCheck("en,es", "ja", "zh"));
+  EXPECT_TRUE(PassAcceptLanguagesCheck("en,es", "ja", "zh"));
 
   // Source lang:
   //   - Is in accept-languages : false
@@ -126,7 +125,7 @@ TEST_F(TranslationManagerImplTest, PassAcceptLanguagesCheck) {
   // Target lang:
   //   - Is in accept-languages : false
   //   - Is popular lang        : false
-  // None of source and target lang is in accept-languages.
+  // Target is not in accept-languages, and not popular.
   EXPECT_FALSE(PassAcceptLanguagesCheck("en,es", "ja", "fr"));
 
   // Source lang:
@@ -153,7 +152,7 @@ TEST_F(TranslationManagerImplTest, PassAcceptLanguagesCheck) {
   // Target lang:
   //   - Is in accept-languages : false
   //   - Is popular lang        : true
-  // None of source and target lang is in accept-languages.
+  // Source is not in accept-languages, and not popular.
   EXPECT_FALSE(PassAcceptLanguagesCheck("en,es", "de", "zh"));
 
   // Source lang:
@@ -162,7 +161,7 @@ TEST_F(TranslationManagerImplTest, PassAcceptLanguagesCheck) {
   // Target lang:
   //   - Is in accept-languages : false
   //   - Is popular lang        : false
-  // None of source and target lang is in accept-languages.
+  // Target and source are not in accept-languages, and not popular.
   EXPECT_FALSE(PassAcceptLanguagesCheck("en,es", "de", "fr"));
 }
 
