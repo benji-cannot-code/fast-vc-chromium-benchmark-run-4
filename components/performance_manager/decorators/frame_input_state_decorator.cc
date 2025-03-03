aed2aa3019624eb1aa1f71c02f2da98c3d537ab8
Joe Mason <joenotcharles@google.com>
2025-03-03 07:40:09 -0800

    Update global InputScenario when frame input changes
    Also add unit tests
    
    Bug: 365586676
    Change-Id: I0994c5291842b77b8abeac66d1af8f9d3aafd568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309336
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jiahe Zhang <jiahe.zhang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1427124}

--- components/performance_manager/decorators/frame_input_state_decorator.cc
+++ components/performance_manager/decorators/frame_input_state_decorator.cc
@@ -5,14 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/performance_manager/decorators/frame_input_state_decorator.h"
 
+#include "base/check_is_test.h"
 #include "components/performance_manager/graph/frame_node_impl.h"
 #include "components/performance_manager/public/performance_manager.h"
 #include "components/performance_manager/public/render_frame_host_proxy.h"
 #include "content/public/browser/render_frame_host.h"
-
-namespace {
-constexpr base::TimeDelta kInactivityTimeoutForTyping = base::Seconds(3);
-}
 
 namespace performance_manager {
 
@@ -119,7 +116,13 @@ FrameInputStateDecorator::InputObserver::InputObserver(
     RenderFrameHostProxy proxy)
     : decorator_(decorator), frame_node_(frame_node) {
   content::RenderFrameHost* rfh = proxy.Get();
-  CHECK(rfh, base::NotFatalUntil::M136);
+  if (!rfh) {
+    // In tests a FrameNode might not be backed by a RenderFrameHost, in which
+    // case this observer does nothing. The test can simulate input by calling
+    // UpdateInputScenario() directly.
+    CHECK_IS_TEST(base::NotFatalUntil::M136);
+    return;
+  }
 
   // Observes the RenderWidgetHost and its input events.
   content::RenderWidgetHost* rwh = rfh->GetRenderWidgetHost();
