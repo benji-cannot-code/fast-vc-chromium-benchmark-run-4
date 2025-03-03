95e7574676b707b0e3071f1d1d4f2ac6a6fa7d22
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-03-02 17:52:41 -0800

    Revert "[lensoverlay] Update the viewport image with each contextual query."
    This reverts commit 603ac59a95d96c0ecdcb3f1d7f47d98ea7a7a127.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6325007903883264
    
    Sample build with failed test: https://ci.chromium.org/b/8721638240974889041
    Affected test(s):
    [ninja://chrome/test:browser_tests/LensOverlayControllerBrowserTest.CsbInputTypeSetsLensSelectionType](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FLensOverlayControllerBrowserTest.CsbInputTypeSetsLensSelectionType?q=VHash%3A15e8c90d55ca90bb)
    [ninja://chrome/test:browser_tests/LensOverlayControllerInnerHtmlEnabledTest.RecordSearchboxFocusHistograms](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FLensOverlayControllerInnerHtmlEnabledTest.RecordSearchboxFocusHistograms?q=VHash%3A15e8c90d55ca90bb)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6325007903883264&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6288046&type=BUG
    
    Original change's description:
    > [lensoverlay] Update the viewport image with each contextual query.
    >
    > After contextual search has been initiated from the Lens overlay, take
    > new screenshots of the live page when the page contextualization is
    > updated (i.e. each time the user focuses the searchbox or issues a
    > query), and send a new full image request if the screenshot has changed.
    > The updated screenshot is stored as a new member of the overlay
    > initialization data, separate from the initial screenshot taken when the
    > overlay is invoked.
    >
    > Bug: b:394147795
    > Change-Id: I90feeb4b9afca987f23180cdcc4c78311653be83
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6288046
    > Reviewed-by: Duncan Mercer <mercerd@google.com>
    > Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1426636}
    >
    
    Bug: b:394147795
    Change-Id: I1859c93a2ff32a4e13e3c5fef324f0eda3ac12e3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314644
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426899}

--- chrome/browser/ui/lens/lens_overlay_image_helper.cc
+++ chrome/browser/ui/lens/lens_overlay_image_helper.cc
@@ -2,10 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
-#pragma allow_unsafe_libc_calls
-#endif
 
 #include "chrome/browser/ui/lens/lens_overlay_image_helper.h"
 
@@ -524,18 +520,5 @@ SkColor FindBestMatchedColorOrTransparent(
     return SK_ColorTRANSPARENT;
   }
   return *closest_color;
-}
-
-bool AreBitmapsEqual(const SkBitmap& bitmap1, const SkBitmap& bitmap2) {
-  // Verify the dimensions are the same.
-  if (bitmap1.width() != bitmap2.width() ||
-      bitmap1.height() != bitmap2.height()) {
-    return false;
-  }
-
-  // Compare pixel data
-  SkPixmap pixmap1 = bitmap1.pixmap();
-  SkPixmap pixmap2 = bitmap2.pixmap();
-  return memcmp(pixmap1.addr(), pixmap2.addr(), pixmap1.computeByteSize()) == 0;
 }
 }  // namespace lens
