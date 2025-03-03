fb4e1d90220f87a4048c70cac610a029373f39f7
Rakina Zata Amni <rakina@chromium.org>
2025-03-02 18:52:10 -0800

    Implement AwNavigationClient.onPageDeleted
    This makes AwWebContentsObserver also observe and notify page deletion
    by signing up as a PageDeletionListener, registered on all pages that
    have been the primary page.
    
    Bug: 359826084
    Change-Id: Id5a360aebf33b4c00ea80c3653e210620b405c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304280
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426907}

--- content/browser/android/web_contents_observer_proxy.cc
+++ content/browser/android/web_contents_observer_proxy.cc
@@ -90,6 +90,12 @@ void WebContentsObserverProxy::RenderFrameDeleted(
       render_frame_host->GetRoutingID());
 }
 
+void WebContentsObserverProxy::PrimaryPageChanged(content::Page& page) {
+  JNIEnv* env = AttachCurrentThread();
+  Java_WebContentsObserverProxy_primaryPageChanged(env, java_observer_,
+                                                   page.GetJavaPage());
+}
+
 void WebContentsObserverProxy::PrimaryMainFrameRenderProcessGone(
     base::TerminationStatus termination_status) {
   JNIEnv* env = AttachCurrentThread();
--- content/public/android/java/src/org/chromium/content_public/browser/Page.java
+++ content/public/android/java/src/org/chromium/content_public/browser/Page.java
@@ -9,6 +9,7 @@ import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 
 /** JNI bridge with content::Page */
 @JNINamespace("content")
@@ -16,8 +17,19 @@ import org.chromium.build.annotations.NullMarked;
 public class Page {
     private boolean mIsPrerendering;
 
+    private @Nullable PageDeletionListener mListener;
+
     public static Page createForTesting() {
         return new Page(/* isPrerendering= */ false);
+    }
+
+    // Listener for when the native C++ Page object is destructed.
+    public interface PageDeletionListener {
+        void onWillDeletePage(Page page);
+    }
+
+    public void setPageDeletionListener(PageDeletionListener listener) {
+        mListener = listener;
     }
 
     @CalledByNative
@@ -29,10 +41,16 @@ public class Page {
     @CalledByNative
     private void willDeletePage(boolean isPrerendering) {
         mIsPrerendering = isPrerendering;
-        // TODO(crbug.com/359826084): Make AwContentsObserver a listener to this release.
+        if (mListener != null) {
+            mListener.onWillDeletePage(this);
+        }
     }
 
     public boolean isPrerendering() {
         return mIsPrerendering;
+    }
+
+    public void setIsPrerendering(boolean isPrerendering) {
+        mIsPrerendering = isPrerendering;
     }
 }
