e4d336f40b81bf94a185b5a9e55fd89a1e4693a0
Jan Keitel <jkeitel@google.com>
2025-03-03 09:09:39 -0800

    Trim AutofillAiModelCache
    This CL introduces a TrimEntries method to AutofillAiModelCacheImpl.
    TrimEntries does two things:
    - It removes entries (starting with the oldest) if the cache exceeds
      the target maximum size.
    - It removes entries once they exceed the maximum target age.
    
    The TrimEntries method is triggered whenever the DB is loaded (i.e.,
    on start) and when a cache entry is updated/added.
    
    Bug: 389631477
    Change-Id: If9e1fee273e6596121ba45514a9114fe97b2be04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317566
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427183}

--- components/autofill/core/browser/ml_model/autofill_ai/autofill_ai_model_cache_impl.h
+++ components/autofill/core/browser/ml_model/autofill_ai/autofill_ai_model_cache_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "components/autofill/core/browser/ml_model/autofill_ai/autofill_ai_model_cache.h"
 #include "components/autofill/core/browser/proto/autofill_ai_model_cache.pb.h"
 #include "components/autofill/core/common/signatures.h"
@@ -33,12 +34,12 @@ inline constexpr base::FilePath::StringViewType
 // `AutofillAiModelCacheImpl` implements `AutofillAiModelCache` using a
 // LevelDB as a persistence layer and a map as an in-memory cache to allow
 // synchronous access.
-// TODO(crbug.com/389631477): Implement a maximum size of the cache.
-// TODO(crbug.com/389631477): Implement a maximum lifetime for cache entries.
 class AutofillAiModelCacheImpl : public AutofillAiModelCache {
  public:
   AutofillAiModelCacheImpl(leveldb_proto::ProtoDatabaseProvider* db_provider,
-                           const base::FilePath& profile_path);
+                           const base::FilePath& profile_path,
+                           size_t max_cache_size,
+                           base::TimeDelta max_cache_age);
   AutofillAiModelCacheImpl(const AutofillAiModelCacheImpl&) = delete;
   AutofillAiModelCacheImpl& operator=(const AutofillAiModelCacheImpl&) = delete;
   AutofillAiModelCacheImpl(AutofillAiModelCacheImpl&&) = delete;
@@ -52,6 +53,9 @@ class AutofillAiModelCacheImpl : public AutofillAiModelCache {
  private:
   using CacheEntryWithMetadata = AutofillAiModelCacheEntryWithMetadata;
 
+  // Removes expired cache entries and limits the cache size to `max_cache_size`
+  // by removing the oldest entries.
+  void TrimEntries();
   void UpdateInDatabase(FormSignature form_signature,
                         const CacheEntryWithMetadata& entry);
 
@@ -63,6 +67,9 @@ class AutofillAiModelCacheImpl : public AutofillAiModelCache {
   base::WeakPtr<AutofillAiModelCacheImpl> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
+
+  const size_t max_cache_size_;
+  const base::TimeDelta max_cache_age_;
 
   // An in-memory cache that allows for synchronous access. Should contain the
   // same content as the database.
