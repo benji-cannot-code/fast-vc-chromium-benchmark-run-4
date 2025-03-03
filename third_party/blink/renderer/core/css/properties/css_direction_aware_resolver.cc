f4ebba52638df3c5395b6d352ddbf75afbd7889a
Tom Sepez <tsepez@chromium.org>
2025-03-03 14:41:46 -0800

    Remove file-wide unsafe_buffers pragma from blink/ [13 of N]
    Suppress warnings on an expression by expression basis.
    
    Bug: 351564777
    Change-Id: I99727f5dc0d87fc616be7de2637cdd26c3ea3398
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317183
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427401}

--- third_party/blink/renderer/core/css/properties/css_direction_aware_resolver.cc
+++ third_party/blink/renderer/core/css/properties/css_direction_aware_resolver.cc
@@ -3,15 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/css/properties/css_direction_aware_resolver.h"
 
 #include <array>
 
+#include "base/compiler_specific.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
 #include "third_party/blink/renderer/core/css/properties/shorthands.h"
 #include "third_party/blink/renderer/core/style_property_shorthand.h"
@@ -79,13 +75,13 @@ template <size_t size>
 const CSSProperty& CSSDirectionAwareResolver::Group<size>::GetProperty(
     size_t index) const {
   DCHECK_LT(index, size);
-  return *properties_[index];
+  return UNSAFE_TODO(*properties_[index]);
 }
 
 template <size_t size>
 bool CSSDirectionAwareResolver::Group<size>::Contains(CSSPropertyID id) const {
   for (size_t i = 0; i < size; ++i) {
-    if (properties_[i]->IDEquals(id)) {
+    if (UNSAFE_TODO(properties_[i]->IDEquals(id))) {
       return true;
     }
   }
--- third_party/blink/renderer/core/css/properties/css_property.cc
+++ third_party/blink/renderer/core/css/properties/css_property.cc
@@ -3,13 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
 
+#include "base/compiler_specific.h"
 #include "third_party/blink/renderer/core/css/cssom/cross_thread_unsupported_value.h"
 #include "third_party/blink/renderer/core/css/cssom/style_value_factory.h"
 #include "third_party/blink/renderer/core/css/properties/computed_style_utils.h"
@@ -80,7 +76,7 @@ void CSSProperty::FilterWebExposedCSSPropertiesIntoVector(
   DCHECK(out_vector.empty());
   out_vector.reserve(property_count);
   for (unsigned i = 0; i < property_count; i++) {
-    const CSSProperty& property = Get(properties[i]);
+    const CSSProperty& property = Get(UNSAFE_TODO(properties[i]));
     if (property.IsWebExposed(execution_context)) {
       out_vector.push_back(&property);
     }
