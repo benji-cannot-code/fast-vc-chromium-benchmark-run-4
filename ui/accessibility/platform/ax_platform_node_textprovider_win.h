632646b7ce664b1c9a7dbaf6daffbb66a5ff4a7b
Greg Thompson <grt@chromium.org>
2025-03-03 08:33:51 -0800

    [accessibility] Fix text provider reference leaks
    Return a ComPtr rather than a naked pointer from the
    AXPlatformNodeTextProviderWin and AXPlatformNodeTextChildProviderWin
    factory functions. This fixes a reference leak from each.
    
    AX-Relnotes: n/a.
    Bug: 40665982
    Change-Id: I824baf49c2de8a771abf4aeced226186368f5d54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317405
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1427162}

--- ui/accessibility/platform/ax_platform_node_textprovider_win.h
+++ ui/accessibility/platform/ax_platform_node_textprovider_win.h
@@ -28,7 +28,8 @@ class COMPONENT_EXPORT(AX_PLATFORM) __declspec(
   AXPlatformNodeTextProviderWin();
   ~AXPlatformNodeTextProviderWin();
 
-  static AXPlatformNodeTextProviderWin* Create(AXPlatformNodeWin* owner);
+  static Microsoft::WRL::ComPtr<AXPlatformNodeTextProviderWin> Create(
+      AXPlatformNodeWin* owner);
   static void CreateIUnknown(AXPlatformNodeWin* owner, IUnknown** unknown);
 
   //
