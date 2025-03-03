feb6bc49b2136741b9dc8d1f81f8eb89ab03899c
Jonathan Lee <jonathanjlee@google.com>
2025-03-03 07:00:04 -0800

    [ios] Reenable reliably passing EG tests for user policies
    Bug: 397928848
    Fixed: 331794057
    Change-Id: I5e45c4d4f4f1707083d8565a1505c4d489339b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313620
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427099}

--- ios/chrome/browser/policy/model/user_policy_egtest.mm
+++ ios/chrome/browser/policy/model/user_policy_egtest.mm
@@ -253,8 +253,7 @@ void WaitForVisibleChromeManagementURL() {
 
 // Tests that the user policies are fetched and activated when signing in with
 // a managed account.
-// TODO(crbug.com/331794057): Test fails.
-- (void)DISABLED_testThatPoliciesAreFetchedOnSignIn {
+- (void)testThatPoliciesAreFetchedOnSignIn {
   // Sign in with managed account to fetch user policies.
   FakeSystemIdentity* fakeManagedIdentity = [FakeSystemIdentity
       identityWithEmail:base::SysUTF8ToNSString(GetTestEmail())];
@@ -264,8 +263,7 @@ void WaitForVisibleChromeManagementURL() {
 }
 
 // Tests that the user policies are cleared after sign out.
-// TODO(crbug.com/331794057): Test fails.
-- (void)DISABLED_testThatPoliciesAreClearedOnSignOut {
+- (void)testThatPoliciesAreClearedOnSignOut {
   // Sign in with managed account to fetch user policies.
   FakeSystemIdentity* fakeManagedIdentity = [FakeSystemIdentity
       identityWithEmail:base::SysUTF8ToNSString(GetTestEmail())];
@@ -279,8 +277,7 @@ void WaitForVisibleChromeManagementURL() {
 
 // Tests that the user policies previously fetched are loaded from the store
 // when signed in at startup.
-// TODO(crbug.com/331794057): Test fails.
-- (void)DISABLED_testThatPoliciesAreLoadedFromStoreWhenSignedInAtStartup {
+- (void)testThatPoliciesAreLoadedFromStoreWhenSignedInAtStartup {
   // Sign in with managed account to fetch user policies.
   FakeSystemIdentity* fakeManagedIdentity = [FakeSystemIdentity
       identityWithEmail:base::SysUTF8ToNSString(GetTestEmail())];
@@ -406,8 +403,7 @@ void WaitForVisibleChromeManagementURL() {
 
 // Tests that the managed accout confirmation dialog is shown in the sign-in
 // flow with its contextual and specific content when user policies are enabled.
-// TODO(crbug.com/331794057): Test fails.
-- (void)DISABLED_testSigninFlowConfirmationDialogWhenUserPolicyAndSignin {
+- (void)testSigninFlowConfirmationDialogWhenUserPolicyAndSignin {
   AppLaunchConfiguration config = [self minimalAppConfigurationForTestCase];
   // Enable User Policy for sign-in consent level exclusively.
   config.features_enabled.push_back(
@@ -495,16 +491,7 @@ void WaitForVisibleChromeManagementURL() {
 
 // Tests that the managed account confirmation dialog isn't shown if the browser
 // is already managed. Only applies for the sign-in consent level.
-// TODO(crbug.com/331794057): Test fails on device.
-#if !TARGET_IPHONE_SIMULATOR
-#define MAYBE_testSigninFlowConfirmationDialogNotShownWhenAlreadyBrowserPolicies \
-  DISABLED_testSigninFlowConfirmationDialogNotShownWhenAlreadyBrowserPolicies
-#else
-#define MAYBE_testSigninFlowConfirmationDialogNotShownWhenAlreadyBrowserPolicies \
-  testSigninFlowConfirmationDialogNotShownWhenAlreadyBrowserPolicies
-#endif
-- (void)
-    MAYBE_testSigninFlowConfirmationDialogNotShownWhenAlreadyBrowserPolicies {
+- (void)testSigninFlowConfirmationDialogNotShownWhenAlreadyBrowserPolicies {
   AppLaunchConfiguration config = [self minimalAppConfigurationForTestCase];
   // Enable User Policy for sign-in consent level exclusively.
   config.features_enabled.push_back(
