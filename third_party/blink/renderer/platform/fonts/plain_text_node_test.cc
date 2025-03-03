ab60dd0f54160f795fe4c1524b2dff4617062845
Kent Tamura <tkent@chromium.org>
2025-03-02 21:57:13 -0800

    CanvasTextNG: Implement BiDi reordering
    This CL has no behavior changes yet because of a disabled runtime flag.
    
    Bug: 389726691
    Change-Id: I14f7324a43567011000c0b9882fa931867c8590a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311534
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426949}

--- third_party/blink/renderer/platform/fonts/plain_text_node_test.cc
+++ third_party/blink/renderer/platform/fonts/plain_text_node_test.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/plain_text_node.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/platform/fonts/font.h"
+#include "third_party/blink/renderer/platform/fonts/font_description.h"
 
 namespace blink {
 
@@ -16,6 +18,14 @@ class PlainTextNodeTest : public testing::Test {
       bool normalize_space = false) {
     return PlainTextNode::NormalizeSpacesAndMaybeBidi(text, normalize_space);
   }
+
+  static Font& TestFont() {
+    return *MakeGarbageCollected<Font>(FontDescription{}, nullptr);
+  }
+
+  static constexpr bool kNormalizeSpace = true;
+  static constexpr bool kBidiOverridden = true;
+  static constexpr bool kSupportsBidi = true;
 };
 
 TEST_F(PlainTextNodeTest, NormalizeSpacesAndMaybeBidiNoConversion) {
@@ -126,6 +136,65 @@ TEST_F(PlainTextNodeTest, NormalizeSpacesAndMaybeBidiCanvasSpaces) {
     EXPECT_FALSE(normalized.Is8Bit());
     EXPECT_FALSE(is_bidi);
   }
+}
+
+TEST_F(PlainTextNodeTest, SegmentTextBasic) {
+  TextRun run("hello world");
+  PlainTextNode& node = *MakeGarbageCollected<PlainTextNode>(
+      run, !kNormalizeSpace, !kBidiOverridden, TestFont(), !kSupportsBidi);
+
+  ASSERT_EQ(node.ItemList().size(), 1u);
+  const PlainTextItem& item = node.ItemList()[0];
+  EXPECT_EQ(item.StartOffset(), 0u);
+  EXPECT_EQ(item.Length(), 11u);
+  EXPECT_EQ(item.Direction(), TextDirection::kLtr);
+  EXPECT_EQ(item.Text(), "hello world");
+}
+
+TEST_F(PlainTextNodeTest, SegmentTextNormalizeSpaces) {
+  TextRun run("hello\t world\n");
+  PlainTextNode& node = *MakeGarbageCollected<PlainTextNode>(
+      run, kNormalizeSpace, !kBidiOverridden, TestFont(), !kSupportsBidi);
+
+  ASSERT_EQ(node.ItemList().size(), 1u);
+  const PlainTextItem& item = node.ItemList()[0];
+  EXPECT_EQ(item.StartOffset(), 0u);
+  EXPECT_EQ(item.Length(), 13u);
+  EXPECT_EQ(item.Direction(), TextDirection::kLtr);
+  EXPECT_EQ(item.Text(), "hello  world ");
+}
+
+TEST_F(PlainTextNodeTest, SegmentTextBidi) {
+  String text = u"123\u05E9\u05DC\u05D5\u05DD456";  // Hebrew characters
+  TextRun run(text, TextDirection::kRtl);
+  PlainTextNode& node = *MakeGarbageCollected<PlainTextNode>(
+      run, !kNormalizeSpace, !kBidiOverridden, TestFont(), kSupportsBidi);
+
+  ASSERT_EQ(node.ItemList().size(), 3u);
+
+  const PlainTextItem& item1 = node.ItemList()[0];
+  EXPECT_EQ(item1.Direction(), TextDirection::kLtr);
+  EXPECT_EQ(item1.Text(), "456");
+
+  const PlainTextItem& item2 = node.ItemList()[1];
+  EXPECT_EQ(item2.Direction(), TextDirection::kRtl);
+  EXPECT_EQ(item2.Text(), u"\u05E9\u05DC\u05D5\u05DD");
+
+  const PlainTextItem& item3 = node.ItemList()[2];
+  EXPECT_EQ(item3.Direction(), TextDirection::kLtr);
+  EXPECT_EQ(item3.Text(), "123");
+}
+
+TEST_F(PlainTextNodeTest, SegmentTextBidiNoSupport) {
+  String text = u"123\u05E9\u05DC\u05D5\u05DD456";  // Hebrew characters
+  TextRun run(text, TextDirection::kRtl);
+  PlainTextNode& node = *MakeGarbageCollected<PlainTextNode>(
+      run, !kNormalizeSpace, !kBidiOverridden, TestFont(), !kSupportsBidi);
+
+  ASSERT_EQ(node.ItemList().size(), 1u);
+  const PlainTextItem& item1 = node.ItemList()[0];
+  EXPECT_EQ(item1.Direction(), TextDirection::kRtl);
+  EXPECT_EQ(item1.Text(), u"123\u05E9\u05DC\u05D5\u05DD456");
 }
 
 }  // namespace blink
