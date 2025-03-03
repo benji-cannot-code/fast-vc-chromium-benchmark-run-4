5cb4d8bc1feb1e407b1f13c72e7faac780fb6f25
Chuong Ho <hdchuong@google.com>
2025-03-02 19:54:18 -0800

    lobster: add impression metrics
    This CL adds a logic to record metrics when the lobster entry points
    are shown to users.
    
    Bug: b:348279517
    Change-Id: If1cb9a6cf831ec2ad1f53e45258c5c4fd36ff3fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311579
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426926}

--- ash/lobster/lobster_controller.cc
+++ ash/lobster/lobster_controller.cc
@@ -33,6 +33,17 @@ bool HasOpportunityToShow(
       !system_checks.Has(ash::LobsterSystemCheck::kInvalidInputField));
 }
 
+void RecordEntryPointImpressionMetric(LobsterEntryPoint entry_point) {
+  switch (entry_point) {
+    case LobsterEntryPoint::kQuickInsert:
+      RecordLobsterState(LobsterMetricState::kQuickInsertTriggerImpression);
+      return;
+    case LobsterEntryPoint::kRightClickMenu:
+      RecordLobsterState(LobsterMetricState::kRightClickTriggerImpression);
+      return;
+  }
+}
+
 }  // namespace
 
 LobsterController::Trigger::Trigger(
@@ -118,6 +129,9 @@ std::unique_ptr<LobsterController::Trigger> LobsterController::CreateTrigger(
     return nullptr;
   }
 
+  // Creates the trigger point if the status is either enabled, or requires
+  // consent.
+  RecordEntryPointImpressionMetric(entry_point);
   return std::make_unique<Trigger>(std::move(client), entry_point,
                                    text_input_context.support_image_insertion
                                        ? LobsterMode::kInsert
