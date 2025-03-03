d9a65b629b19947a80982311ff6ac943c30cde61
Fredrik Söderquist <fs@opera.com>
2025-03-03 07:09:44 -0800

    Fix censoring of top-level NaN values for <angle> and certain <length>s
    NaNs in <angle>s were being censored to the biggest multiple of 360
    degrees rather 0 as defined by CSS Values and Units [1].
    
    Fix ClampLength(float) as well to make it match ClampLength(double).
    Reorder functions to have overloads next to each other and update
    ordering of the declarations to match the definitions.
    
    [1] https://www.w3.org/TR/css-values-4/#top-level-calculation
    
    Bug: 396043330
    Change-Id: I4c69317ac4d9de2b24377c68cc0fb8886b4627cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281093
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1427105}

--- third_party/blink/renderer/core/css/css_value_clamping_utils.cc
+++ third_party/blink/renderer/core/css/css_value_clamping_utils.cc
@@ -8,18 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-/*
- The value must be multiple of 360deg.
- Reference:  https://drafts.csswg.org/css-values/#numeric-types
-
- This constant is the biggest multiple of 360 that a double can accurately
- represent, and after converting to rads, the sin() value is close enough to 0.
-
- The details: https://bit.ly/349gXjq
-*/
-
-constexpr static double kApproxDoubleInfinityAngle = 2867080569122160;
-
 double CSSValueClampingUtils::ClampDouble(double value) {
   // https://www.w3.org/TR/css-values-4/#top-level-calculation
   if (std::isnan(value)) {
@@ -32,23 +20,34 @@ double CSSValueClampingUtils::ClampLength(double value) {
   return ClampDouble(value);
 }
 
+float CSSValueClampingUtils::ClampLength(float value) {
+  // https://www.w3.org/TR/css-values-4/#top-level-calculation
+  if (std::isnan(value)) {
+    value = 0;
+  }
+  return ClampTo<float>(value);
+}
+
 double CSSValueClampingUtils::ClampTime(double value) {
   return ClampDouble(value);
 }
 
 double CSSValueClampingUtils::ClampAngle(double value) {
+  // https://www.w3.org/TR/css-values-4/#top-level-calculation
   if (std::isnan(value)) {
-    value = kApproxDoubleInfinityAngle;
+    value = 0;
   }
+  // The value must be multiple of 360deg.
+  // Reference:  https://drafts.csswg.org/css-values/#numeric-types
+  //
+  // This constant is the biggest multiple of 360 that a double can accurately
+  // represent, and after converting to rads, the sin() value is close enough
+  // to 0.
+  //
+  // The details: https://bit.ly/349gXjq
+  static constexpr double kApproxDoubleInfinityAngle = 2867080569122160;
   return ClampTo<double>(value, -kApproxDoubleInfinityAngle,
                          kApproxDoubleInfinityAngle);
-}
-
-float CSSValueClampingUtils::ClampLength(float value) {
-  if (std::isnan(value)) {
-    value = std::numeric_limits<float>::max();
-  }
-  return ClampTo<float>(value);
 }
 
 }  // namespace blink
