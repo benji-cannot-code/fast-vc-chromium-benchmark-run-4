f237dfd90215622bc777749bae657a78a4be258e
Etienne Pierre-doray <etiennep@chromium.org>
2025-03-03 13:49:40 -0800

    [memory] Add kUserLevelMemoryPressureSignal features
    To control sending user level memory pressure signal.
    The current state is somewhat inconsistent; 4-6gb is enabled but
    3gb is disabled.
    Meanwhile, I'm working on v8 memory saver mode, which
    might replace some other v8 memory pressure handling.
    
    This is partly reverting
    https://chromium-review.googlesource.com/c/chromium/src/+/6082219
    https://chromium-review.googlesource.com/c/chromium/src/+/6082217
    
    Bug: 388844091
    Change-Id: I4567b0296ada061ff20c7953194e70e85c3a6cc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318320
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427352}

--- content/common/user_level_memory_pressure_signal_features.h
+++ content/common/user_level_memory_pressure_signal_features.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content::features {
 
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kUserLevelMemoryPressureSignalOn3GbDevices);
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kUserLevelMemoryPressureSignalOn4GbDevices);
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kUserLevelMemoryPressureSignalOn6GbDevices);
 
 // Helper functions for UserLevelMemoryPressureSignal features.
 CONTENT_EXPORT bool IsUserLevelMemoryPressureSignalEnabledOn3GbDevices();
