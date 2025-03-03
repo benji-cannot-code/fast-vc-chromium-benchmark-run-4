8b4459a7bff77e8fe4b370eca537cf32b7542cd4
Shibalik Mohapatra <shibalik@chromium.org>
2025-03-02 21:52:09 -0800

    [SxS] Migrate Tabstrip to using OnTabGroupDetached and OnTabGroupAttached.
    When a group is detached and re-inserted TabGroupedChanged observers are called with different reasons instead of sending observation that tab's group membership changed or group is removed / created.
    
    This CL migrates TabStrip view code to handle this new flow. This CL also adds client side handling for multiple tabs being inserted in browser_tab_strip_controller.cc.
    
    Bug: 399194773
    Change-Id: Ib57b01f94769caa33d547d0328c5460bf8994740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6303063
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426946}

--- chrome/browser/ui/views/tabs/tab_container.cc
+++ chrome/browser/ui/views/tabs/tab_container.cc
@@ -7,5 +7,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/metadata/metadata_impl_macros.h"
 
+TabContainer::TabInsertionParams::TabInsertionParams(std::unique_ptr<Tab> tab,
+                                                     int index,
+                                                     TabPinned pinned)
+    : tab(std::move(tab)), model_index(index), pinned(pinned) {}
+
+TabContainer::TabInsertionParams::~TabInsertionParams() = default;
+
+TabContainer::TabInsertionParams::TabInsertionParams(
+    TabInsertionParams&& other) noexcept
+    : tab(std::move(other.tab)),
+      model_index(other.model_index),
+      pinned(other.pinned) {}
+
+TabContainer::TabInsertionParams& TabContainer::TabInsertionParams::operator=(
+    TabInsertionParams&& other) noexcept {
+  if (this != &other) {
+    tab = std::move(other.tab);
+    model_index = other.model_index;
+    pinned = other.pinned;
+  }
+  return *this;
+}
+
 BEGIN_METADATA(TabContainer)
 END_METADATA
