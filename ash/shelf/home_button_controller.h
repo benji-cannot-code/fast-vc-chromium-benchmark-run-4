144be5613dc346e858093daa19518479f263faa4
Michael Cui <mlcui@google.com>
2025-03-02 17:05:11 -0800

    sunfish: Add home button entry point
    This will override the existing Assistant entry point. Future CLs will
    update the home button icon based on Sunfish / Scanner feature state,
    similar to Assistant.
    
    Bug: b:391909810
    Change-Id: I6a6a2100f0facf45ffcb83392757464e7f0639b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6297343
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426894}

--- ash/shelf/home_button_controller.h
+++ ash/shelf/home_button_controller.h
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/assistant/model/assistant_ui_model_observer.h"
+#include "ash/capture_mode/sunfish_scanner_feature_watcher.h"
 #include "ash/public/cpp/app_list/app_list_controller_observer.h"
 #include "ash/public/cpp/assistant/assistant_state.h"
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "ui/display/display_observer.h"
 
 namespace display {
@@ -33,7 +35,8 @@ class HomeButton;
 class HomeButtonController : public AppListControllerObserver,
                              public display::DisplayObserver,
                              public AssistantStateObserver,
-                             public AssistantUiModelObserver {
+                             public AssistantUiModelObserver,
+                             public SunfishScannerFeatureWatcher::Observer {
  public:
   explicit HomeButtonController(HomeButton* button);
 
@@ -54,6 +57,9 @@ class HomeButtonController : public AppListControllerObserver,
   bool IsAssistantVisible();
 
  private:
+  // Whether Sunfish or Scanner's UI can be shown.
+  bool IsSunfishOrScannerAvailable() const;
+
   // AppListControllerObserver:
   void OnAppListVisibilityWillChange(bool shown, int64_t display_id) override;
 
@@ -72,6 +78,10 @@ class HomeButtonController : public AppListControllerObserver,
       std::optional<AssistantEntryPoint> entry_point,
       std::optional<AssistantExitPoint> exit_point) override;
 
+  // SunfishScannerFeatureWatcher::Observer:
+  void OnSunfishScannerFeatureStatesChanged(
+      SunfishScannerFeatureWatcher& source) override;
+
   void OnAppListShown();
   void OnAppListDismissed();
 
@@ -86,6 +96,11 @@ class HomeButtonController : public AppListControllerObserver,
   // Owned by the button's view hierarchy.
   raw_ptr<HomeButtonTapOverlay> tap_overlay_ = nullptr;
   std::unique_ptr<base::OneShotTimer> tap_animation_delay_timer_;
+
+  // Observes changes in Sunfish and Scanner feature states.
+  base::ScopedObservation<SunfishScannerFeatureWatcher,
+                          SunfishScannerFeatureWatcher::Observer>
+      sunfish_scanner_feature_observation_{this};
 
   display::ScopedDisplayObserver display_observer_{this};
 };
