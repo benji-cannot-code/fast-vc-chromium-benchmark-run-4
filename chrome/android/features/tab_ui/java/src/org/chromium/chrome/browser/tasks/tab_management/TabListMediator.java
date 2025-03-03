a9f3d47db92f483ce3652413ab4c5d1b1cc4e6bd
Calder Kitagawa <ckitagawa@chromium.org>
2025-03-03 10:35:48 -0800

    [Tab Switcher] Use @VisibleForTesting rather than ForTesting method
    Fast-follow to
    https://chromium-review.googlesource.com/c/chromium/src/+/6310430 to
    use @VisibleForTesting instead of a dedicated `ForTesting` method.
    
    Bug: 399500045
    Change-Id: Ibd64b10bfb858fcc1e931f320cd6ec504094005b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318120
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427215}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -1627,7 +1627,8 @@ class TabListMediator implements TabListNotificationHandler {
      * Span count is computed based on screen width for tablets and orientation for phones. When in
      * multi-window mode on phone, the span count is fixed to 2 to keep tab card size reasonable.
      */
-    private int getSpanCount(int screenWidthDp) {
+    @VisibleForTesting
+    int getSpanCount(int screenWidthDp) {
         if (XrUtils.isXrDevice()) {
             // The layout span count is restricted to medium on XR immersive devices to display
             // larger tab thumbnails, despite the large screen width.
@@ -2851,9 +2852,5 @@ class TabListMediator implements TabListNotificationHandler {
         var oldValue = mActionsOnAllRelatedTabs;
         mActionsOnAllRelatedTabs = actionOnAllRelatedTabs;
         ResettersForTesting.register(() -> mActionsOnAllRelatedTabs = oldValue);
-    }
-
-    int getSpanCountForTesting(int screenWidthDp) {
-        return getSpanCount(screenWidthDp);
     }
 }
