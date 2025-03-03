2f2c21f9bf8a31e3e8d11b3e66e946ea6e76e87e
Eriko Kurimoto <elkurin@google.com>
2025-03-02 21:56:31 -0800

    security_token_restriction: Remove chrome layout provider deps
    Since it's not used.
    
    Bug: b:394731607
    Change-Id: If291400fc390ba54894f4c118d82d5d1cc0fb133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6301512
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426948}

--- chrome/browser/ui/ash/security_token_restriction/security_token_session_restriction_view.cc
+++ chrome/browser/ui/ash/security_token_restriction/security_token_session_restriction_view.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
-#include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/ui/vector_icons/vector_icons.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -26,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
-#include "ui/views/layout/layout_provider.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_delegate.h"
 
