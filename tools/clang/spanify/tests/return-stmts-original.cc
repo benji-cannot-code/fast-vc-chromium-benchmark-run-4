e82b4295e7b550827519c0bcb2cb7d62ce2e43cf
Ali Hijazi <ahijazi@chromium.org>
2025-03-03 04:35:42 -0800

    [Spanify] Handle binary operations
    This cl adapts binary operations on now-spans as follows:
    buf + offset => buf.subspan(offset);
    
    Enclosing such expressions with parenthesis when adding data changes is
    no longer necessary.
    
    Expressions previously rewritten as:
    (buf + offset).data()
    now become:
    buf.subspan(offset).data()
    
    Dereference expressions are adapted similarly.
    
    Change-Id: I6c0ee4d0719fb8bdf44acdd0e4054d17fc6ad44d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304724
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427056}

--- tools/clang/spanify/tests/return-stmts-original.cc
+++ tools/clang/spanify/tests/return-stmts-original.cc
@@ -65,7 +65,10 @@ char* fct6() {
   // base::span<int> var1 = new int[1024];
   int* var1 = new int[1024];
   int offset = 1;
-  // No rewrite here for now.
+  // Expected rewrite:
+  // return reinterpret_cast<char*>(var1.subspan(offset));
+  // As-is, this code doesn't compile because we don't yet handle
+  // adapting these reinterpret_cast expressions for spans.
   return reinterpret_cast<char*>(var1 + offset);
 }
 
@@ -77,7 +80,7 @@ int* fct7() {
   int* var1 = new int[1024];
   int offset = 1;
   // Expected rewrite:
-  // return (var1 + offset).data();
+  // return var1.subspan(offset).data();
   return var1 + offset;
 }
 
@@ -89,7 +92,9 @@ char* fct8() {
   int* var1 = new int[1024];
   int offset = 1;
   // Expected rewrite:
-  // return (reinterpret_cast<char*>(var1) + offset).data();
+  // return reinterpret_cast<char*>(var1).subspan(offset).data();
+  // As-is, this code doesn't compile because we don't yet handle
+  // adapting these reinterpret_cast expressions for spans.
   return reinterpret_cast<char*>(var1) + offset;
 }
 
