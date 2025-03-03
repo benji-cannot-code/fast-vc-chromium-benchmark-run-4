f85740e852d42be95419a82d0ece52c8bf935aab
Georg Neis <neis@chromium.org>
2025-03-03 00:41:12 -0800

    Remove some redundant platform checks
    The code in question is only used for ChromeOS builds, so there's no
    point in #if BUILDFLAG(IS_CHROMEOS).
    
    Change-Id: I28c6f3dd49ebdedb46fdd8f0648659de56dd0c8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6315784
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426987}

--- chrome/browser/ash/system_web_apps/test_support/test_system_web_app_installation.h
+++ chrome/browser/ash/system_web_apps/test_support/test_system_web_app_installation.h
@@ -70,10 +70,7 @@ class UnittestingSystemAppDelegate : public SystemWebAppDelegate {
   bool IsAppEnabled() const override;
   bool IsUrlInSystemAppScope(const GURL& url) const override;
   bool UseSystemThemeColor() const override;
-#if BUILDFLAG(IS_CHROMEOS)
   bool ShouldAnimateThemeChanges() const override;
-#endif  // BUILDFLAG(IS_CHROMEOS)
-
   void SetAppIdsToUninstallAndReplace(const std::vector<webapps::AppId>&);
   void SetMinimumWindowSize(const gfx::Size&);
   void SetShouldReuseExistingWindow(bool);
@@ -97,13 +94,10 @@ class UnittestingSystemAppDelegate : public SystemWebAppDelegate {
   void SetIsAppEnabled(bool);
   void SetUrlInSystemAppScope(const GURL& url);
   void SetUseSystemThemeColor(bool);
-#if BUILDFLAG(IS_CHROMEOS)
   void SetShouldAnimateThemeChanges(bool);
-#endif  // BUILDFLAG(IS_CHROMEOS)
 
  private:
   web_app::WebAppInstallInfoFactory info_factory_;
-
   std::vector<webapps::AppId> uninstall_and_replace_;
   gfx::Size minimum_window_size_;
   bool single_window_ = true;
@@ -124,16 +118,11 @@ class UnittestingSystemAppDelegate : public SystemWebAppDelegate {
   bool is_app_enabled = true;
   GURL url_in_system_app_scope_;
   bool use_system_theme_color_ = true;
-#if BUILDFLAG(IS_CHROMEOS)
   bool should_animate_theme_changes_ = false;
-#endif  // BUILDFLAG(IS_CHROMEOS)
-
   base::RepeatingCallback<gfx::Rect(Browser*)> get_default_bounds_ =
       base::NullCallback();
-
   LaunchAndNavigateSystemWebAppCallback launch_and_navigate_system_web_apps_ =
       base::NullCallback();
-
   std::optional<SystemWebAppBackgroundTaskInfo> timer_info_;
 };
 
