229f9b9888344ac45db6dbb6cce76648f0a34ce3
caott <caott@google.com>
2025-03-04 13:27:48 -0800

    [Boca] Add kManagedDefaultMediaStreamSetting to BocaValidPref
    This is used to check whether the camera and microphone are enforced by
    admin. See:
    https://chromeenterprise.google/policies/#DefaultMediaStreamSetting
    
    Bug: b:396508344
    Change-Id: I78627098271080f25d26d3a11a2825985e022819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311415
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Luke Cao <caott@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427956}

--- ash/webui/boca_ui/boca_app_page_handler.cc
+++ ash/webui/boca_ui/boca_app_page_handler.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ui/frame/multitask_menu/float_controller_base.h"
 #include "chromeos/ui/wm/constants.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/sessions/core/session_id.h"
 #include "content/public/browser/web_contents.h"
@@ -183,6 +184,8 @@ std::string GetPrefName(mojom::BocaValidPref pref) {
       return ash::prefs::kClassManagementToolsNavRuleSetting;
     case mojom::BocaValidPref::kCaptionEnablementSetting:
       return ash::prefs::kClassManagementToolsCaptionEnablementSetting;
+    case mojom::BocaValidPref::kDefaultMediaStreamSetting:
+      return ::prefs::kManagedDefaultMediaStreamSetting;
   }
   NOTREACHED();
 }
@@ -603,6 +606,13 @@ void BocaAppHandler::GetUserPref(mojom::BocaValidPref pref,
 void BocaAppHandler::SetUserPref(mojom::BocaValidPref pref,
                                  base::Value value,
                                  SetUserPrefCallback callback) {
+  // Boca should only get but not set kDefaultMediaStreamSetting.
+  if (pref == mojom::BocaValidPref::kDefaultMediaStreamSetting) {
+    mojo::ReportBadMessage(
+        "Attempted to set kDefaultMediaStreamSetting user pref.");
+    return;
+  }
+
   pref_service_->Set(GetPrefName(pref), std::move(value));
   std::move(callback).Run();
 }
--- ash/webui/boca_ui/boca_app_page_handler_unittest.cc
+++ ash/webui/boca_ui/boca_app_page_handler_unittest.cc
@@ -43,9 +43,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/browser_context_helper/fake_browser_context_helper_delegate.h"
 #include "chromeos/ash/components/test/ash_test_suite.h"
 #include "components/account_id/account_id.h"
+#include "components/content_settings/core/browser/content_settings_policy_provider.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "components/user_manager/fake_user_manager.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "components/user_manager/user_manager.h"
@@ -332,6 +334,8 @@ class BocaAppPageHandlerTest : public testing::Test {
     // Set up UserManager related modules.
     user_manager::UserManagerImpl::RegisterPrefs(local_state_.registry());
     ash::boca_util::RegisterPrefs(local_state_.registry());
+    content_settings::PolicyProvider::RegisterProfilePrefs(
+        pref_service_.registry());
     fake_user_manager_.Reset(
         std::make_unique<user_manager::FakeUserManager>(&local_state_));
 
@@ -438,10 +442,14 @@ class BocaAppPageHandlerTest : public testing::Test {
         boca_app_handler_.get()->GetWebviewAuthHandlerForTesting());
   }
   FakePage* fake_page() { return fake_page_.get(); }
+  sync_preferences::TestingPrefServiceSyncable* pref_service() {
+    return &pref_service_;
+  }
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
   content::BrowserTaskEnvironment task_environment_;
+  sync_preferences::TestingPrefServiceSyncable pref_service_;
   TestingPrefServiceSimple local_state_;
 
   user_manager::TypedScopedUserManager<user_manager::FakeUserManager>
@@ -1864,6 +1872,14 @@ TEST_F(BocaAppPageHandlerTest, AuthenticateWebviewFailure) {
         run_loop.Quit();
       }));
   run_loop.Run();
+}
+
+TEST_F(BocaAppPageHandlerTest, TestPrefGetter) {
+  boca_app_handler()->SetPrefForTesting(pref_service());
+  base::test::TestFuture<base::Value> future;
+  boca_app_handler()->GetUserPref(
+      mojom::BocaValidPref::kDefaultMediaStreamSetting, future.GetCallback());
+  EXPECT_TRUE(future.Get().is_int());
 }
 
 TEST_F(BocaAppPageHandlerTest, TestPrefGetterAndSetter) {
