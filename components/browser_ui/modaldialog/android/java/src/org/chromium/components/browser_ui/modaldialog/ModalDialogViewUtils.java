0767c14b703f61eae9d12361a08f494e9a586c5a
Matthew Jones <mdjones@chromium.org>
2025-03-04 09:17:09 -0800

    Revert "[Data Sharing] Integrate spinners into ActionConfirmationDialog"
    This reverts commit 3678ad0b609affb4029301870c195fe7b96cad1f.
    
    Reason for revert: Suspected cause of compile failure on android-10-x86 related to R8 removing some required symbol:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-10-x86-rel/566/overview
    
    Missing field int Wt1.collaboration_loading_button (referenced from: org.chromium.components.browser_ui.widget.SpinnerButtonWrapper...
    
    Original change's description:
    > [Data Sharing] Integrate spinners into ActionConfirmationDialog
    >
    > Integrate spinners onto the ActionConfirmationDialog buttons.
    >
    > Bug: 399725798
    > Change-Id: I0ff0d9be8f3187dbbb2beea1d4197c70d64b36a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309679
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Commit-Queue: Brandon Fong <bjfong@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1427739}
    
    Bug: 399725798
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I7037b40dfbf793776c80a9c53ae84430135e6d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322777
    Owners-Override: Matt Jones <mdjones@google.com>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427781}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogViewUtils.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogViewUtils.java
@@ -3,13 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.components.browser_ui.widget;
+package org.chromium.components.browser_ui.modaldialog;
 
 import android.content.Context;
 import android.content.res.Resources;
 import android.view.View;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.components.browser_ui.widget.DualControlLayout;
 
 /** Set of shared helper functions for UI related to the {@link ModalDialogView}. */
 @NullMarked
