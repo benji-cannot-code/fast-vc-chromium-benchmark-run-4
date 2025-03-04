0aeb8d1ebc41389832855bf3e8067e851802b9a8
Jonathan Lee <jonathanjlee@google.com>
2025-03-03 17:47:24 -0800

    [chromedriver] Allow clients to stop browser process gracefully
    Add a Chrome-specific capability `quitGracefully` that forces
    chromedriver to attempt to stop the browser with `Browser.close` or
    SIGTERM. wptrunner will enable this capability when used with the `rr`
    debugger (`rr record chrome ...`), which needs time to dump the
    recording. The runner doesn't want to manage `--user-data-dir` or
    `--log-net-log` itself, so the existing code path for graceful quit
    is insufficient.
    
    SIGKILL remains the default behavior so that quitting remains fast for
    clients that don't opt-in.
    
    Bug: 339644807
    Change-Id: I8b4286bb188f36d0c786e78bf6257c516c91f865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6269180
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427507}

--- chrome/test/chromedriver/log_replay/chrome_replay_impl.cc
+++ chrome/test/chromedriver/log_replay/chrome_replay_impl.cc
@@ -36,7 +36,8 @@ ChromeReplayImpl::ChromeReplayImpl(
                         extension_dir,
                         network_emulation_enabled,
                         autoaccept_beforeunload,
-                        enable_extension_targets) {}
+                        enable_extension_targets,
+                        /*quit_gracefully=*/false) {}
 
 ChromeReplayImpl::~ChromeReplayImpl() = default;
 
