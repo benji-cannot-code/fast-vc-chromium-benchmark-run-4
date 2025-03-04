94994f0825b12946b41f4d148847aa1d4acd315f
Lukasz Anforowicz <lukasza@chromium.org>
2025-03-04 12:40:06 -0800

    [gnrt] Remove mostly unused `Package.dev_dependencies` field.
    This field is not used today (today `gnrt gen` does *not* generate
    targets for building native Rust unit tests for crates.io libraries from
    under `//third_party/rust`).
    
    Additionally removing this field helps to smooth out the transition
    to `guppy` in a follow-up CL at https://crrev.com/c/6259145, because:
    
    * It front-loads related unit test tweaks
    * Skipping dev dependencies matches the default behavior of `guppy`.
    
    Bug: 395924069
    Change-Id: I598ea6f0150baf1ef0e76a50dfe86c90b63178c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314175
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427913}

--- tools/crates/gnrt/lib/deps.rs
+++ tools/crates/gnrt/lib/deps.rs
@@ -40,8 +40,6 @@ pub struct Package {
     pub dependencies: Vec<DepOfDep>,
     /// Same as the above, but for build script deps.
     pub build_dependencies: Vec<DepOfDep>,
-    /// Same as the above, but for test deps.
-    pub dev_dependencies: Vec<DepOfDep>,
     /// A package can be depended upon in different ways: as a normal
     /// dependency, just for build scripts, or just for tests. `kinds` contains
     /// an entry for each way this package is depended on.
@@ -328,8 +326,7 @@ pub fn collect_dependencies(
             match node_dep.kind {
                 DependencyKind::Normal => dep.dependencies.push(dep_of_dep),
                 DependencyKind::Build => dep.build_dependencies.push(dep_of_dep),
-                DependencyKind::Development => dep.dev_dependencies.push(dep_of_dep),
-                DependencyKind::Unknown => unreachable!(),
+                DependencyKind::Development | DependencyKind::Unknown => unreachable!(),
             }
         }
 
@@ -386,7 +383,6 @@ fn explore_node<'a>(state: &mut TraversalState<'a>, node: &'a cargo_metadata::No
         edition: String::new(),
         dependencies: Vec::new(),
         build_dependencies: Vec::new(),
-        dev_dependencies: Vec::new(),
         dependency_kinds: HashMap::new(),
         lib_target: None,
         bin_targets: Vec::new(),
@@ -455,6 +451,12 @@ fn iter_node_deps(node: &cargo_metadata::Node) -> impl Iterator<Item = Dependenc
             if condition == Condition::AlwaysFalse {
                 return None;
             };
+
+            // At this point we don't support building dev dependencies
+            // (e.g. native Rust unit tests) for packages from crates.io.
+            if dep_kind_info.kind == DependencyKind::Development {
+                return None;
+            }
 
             if seen.contains(&(&dep_kind_info.kind, &dep_kind_info.target)) {
                 return None;
@@ -632,16 +634,6 @@ mod tests {
 
         i += 1;
 
-        assert_eq!(dependencies[i].package_name, "more-asserts");
-        assert_eq!(dependencies[i].version, Version::new(0, 3, 0));
-        assert!(dependencies[i].is_toplevel_dep);
-        assert_eq!(
-            dependencies[i].dependency_kinds.get(&DependencyKind::Development).unwrap().features,
-            empty_str_slice
-        );
-
-        i += 1;
-
         assert_eq!(dependencies[i].package_name, "num-traits");
         assert_eq!(dependencies[i].version, Version::new(0, 2, 15));
         assert!(dependencies[i].is_toplevel_dep);
@@ -718,7 +710,6 @@ mod tests {
         );
         assert_eq!(dependencies[i].dependencies.len(), 1);
         assert_eq!(dependencies[i].build_dependencies.len(), 0);
-        assert_eq!(dependencies[i].dev_dependencies.len(), 0);
         assert_eq!(
             dependencies[i].dependencies[0],
             DepOfDep {
@@ -741,7 +732,6 @@ mod tests {
         assert_eq!(dependencies[i].group, Group::Safe);
         assert_eq!(dependencies[i].dependencies.len(), 3);
         assert_eq!(dependencies[i].build_dependencies.len(), 0);
-        assert_eq!(dependencies[i].dev_dependencies.len(), 0);
         assert_eq!(
             dependencies[i].dependencies[0],
             DepOfDep {
@@ -781,7 +771,6 @@ mod tests {
         );
         assert_eq!(dependencies[i].dependencies.len(), 3);
         assert_eq!(dependencies[i].build_dependencies.len(), 0);
-        assert_eq!(dependencies[i].dev_dependencies.len(), 0);
         assert_eq!(
             dependencies[i].dependencies[0],
             DepOfDep {
@@ -821,7 +810,6 @@ mod tests {
         );
         assert_eq!(dependencies[i].dependencies.len(), 1);
         assert_eq!(dependencies[i].build_dependencies.len(), 0);
-        assert_eq!(dependencies[i].dev_dependencies.len(), 0);
         assert_eq!(
             dependencies[i].dependencies[0],
             DepOfDep {
@@ -892,7 +880,6 @@ mod tests {
         );
         assert_eq!(dependencies[i].dependencies.len(), 0);
         assert_eq!(dependencies[i].build_dependencies.len(), 0);
-        assert_eq!(dependencies[i].dev_dependencies.len(), 0);
 
         i += 1;
 
@@ -905,7 +892,6 @@ mod tests {
         }));
         assert_eq!(dependencies[i].dependencies.len(), 1);
         assert_eq!(dependencies[i].build_dependencies.len(), 0);
-        assert_eq!(dependencies[i].dev_dependencies.len(), 0);
         assert_eq!(
             dependencies[i].dependencies[0],
             DepOfDep {
