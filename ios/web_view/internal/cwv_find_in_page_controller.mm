af01a29eac8671ca3502e8781305d688182b847d
Kyle Hickinson <khickinson@brave.com>
2025-03-04 06:30:15 -0800

    Add missing WebState observation to CWVFindInPageController
    Fixed: 400340588
    Change-Id: Ic7fb3496a3eccdcc42e8e514a068d3cc0c5f6913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6316988
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427693}

--- ios/web_view/internal/cwv_find_in_page_controller.mm
+++ ios/web_view/internal/cwv_find_in_page_controller.mm
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _findInPageManager = web::FindInPageManager::FromWebState(webState);
 
     _webState = webState->GetWeakPtr();
+    _webStateObserverBridge =
+        std::make_unique<web::WebStateObserverBridge>(self);
+    _webState->AddObserver(_webStateObserverBridge.get());
   }
   return self;
 }
