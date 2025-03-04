e6d1131478d7f3074d7be6df5c540e4fb9bc1706
Christine Ying <chrstne@google.com>
2025-03-04 10:01:09 -0800

    [N&C] Remove LearnMore from notice storage service.
    Remove this action since it doesn't complete the notice flow, metrics
    should still be tracked but separately from the N&C storage model
    
    Steps: go/koilos:notice-consent-storage-schema-2-update-onepager
    
    NO_IFTTT=added comment to deprecate
    
    Change-Id: Ia188ac7db769931ab6b22f57cd391741ed80ea8e
    Bug: 392088228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6201245
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427817}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
@@ -935,9 +935,6 @@ void UpdateNoticeStorage(
       break;
     }
     case PromptAction::kConsentMoreInfoOpened: {
-      notice_storage->SetNoticeActionTaken(
-          pref_service, notice_name,
-          privacy_sandbox::NoticeActionTaken::kLearnMore, base::Time::Now());
       break;
     }
     // EEA and ROW notices
@@ -959,15 +956,11 @@ void UpdateNoticeStorage(
       break;
     }
     case PromptAction::kNoticeMoreInfoOpened:
-    // Ads API UX Enhancements
+      // Ads API UX Enhancements
     case PromptAction::kNoticeSiteSuggestedAdsMoreInfoOpened:
     case PromptAction::kNoticeAdsMeasurementMoreInfoOpened: {
-      notice_storage->SetNoticeActionTaken(
-          pref_service, notice_name,
-          privacy_sandbox::NoticeActionTaken::kLearnMore, base::Time::Now());
       break;
     }
-
     // Restricted notices
     case PromptAction::kRestrictedNoticeShown: {
       DCHECK(privacy_sandbox::IsRestrictedNoticeRequired());
