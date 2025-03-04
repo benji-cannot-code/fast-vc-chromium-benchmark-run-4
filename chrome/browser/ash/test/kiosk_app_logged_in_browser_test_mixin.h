31df86073147a7b8197e0e3169e973ab49358332
Hidehiko Abe <hidehiko@chromium.org>
2025-03-03 18:08:45 -0800

    Refactor *LoggedInBrowserTestMixin for DeviceLocalAccounts.
    This is preparation to use these mixins for more test cases.
    - Renamed KioskLoggedIn to KioskAppLoggedIn for naming consistency.
    - Let ctor take account_id instead of user_id directly.
      Then, generate user_id from the account_id.
    
    BUG=396238168
    TEST=Tryjob
    
    Change-Id: Ic05d44eb29b702d4f18d042417cabf5770c84e5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318899
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427512}

--- chrome/browser/ash/test/kiosk_app_logged_in_browser_test_mixin.h
+++ chrome/browser/ash/test/kiosk_app_logged_in_browser_test_mixin.h
@@ -3,25 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_TEST_PUBLIC_ACCOUNT_LOGGED_IN_BROWSER_TEST_MIXIN_H_
-#define CHROME_BROWSER_ASH_TEST_PUBLIC_ACCOUNT_LOGGED_IN_BROWSER_TEST_MIXIN_H_
+#ifndef CHROME_BROWSER_ASH_TEST_KIOSK_APP_LOGGED_IN_BROWSER_TEST_MIXIN_H_
+#define CHROME_BROWSER_ASH_TEST_KIOSK_APP_LOGGED_IN_BROWSER_TEST_MIXIN_H_
 
 #include <string>
+#include <string_view>
 
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 
 namespace ash {
 
-// Registers a new PublicAccount User, and then use the user to enter
-// into a session.
-class PublicAccountLoggedInBrowserTestMixin : public InProcessBrowserTestMixin {
+// Registers a new KioskApp User, and then use the user to enter
+// into a kiosk app session.
+class KioskAppLoggedInBrowserTestMixin : public InProcessBrowserTestMixin {
  public:
-  // `account_id` is the ID for the PublicSession account.
+  // `account_id` is the ID for the KioskApp account.
   // AccountId (to be used by, e.g., UserManager) is generated from the
   // `account_id`.
-  PublicAccountLoggedInBrowserTestMixin(InProcessBrowserTestMixinHost* host,
-                                        std::string_view account_id);
-  ~PublicAccountLoggedInBrowserTestMixin() override;
+  KioskAppLoggedInBrowserTestMixin(InProcessBrowserTestMixinHost* host,
+                                   std::string_view account_id);
+  ~KioskAppLoggedInBrowserTestMixin() override;
 
   // InProcessBrowserTestMixin:
   void SetUpCommandLine(base::CommandLine* command_line) override;
@@ -35,4 +36,4 @@ class PublicAccountLoggedInBrowserTestMixin : public InProcessBrowserTestMixin {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_TEST_PUBLIC_ACCOUNT_LOGGED_IN_BROWSER_TEST_MIXIN_H_
+#endif  // CHROME_BROWSER_ASH_TEST_KIOSK_APP_LOGGED_IN_BROWSER_TEST_MIXIN_H_
