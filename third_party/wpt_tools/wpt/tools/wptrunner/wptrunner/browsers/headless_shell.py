46c67b4948b119eb917f171d3938303d6b81cc49
Jonathan Lee <jonathanjlee@google.com>
2025-03-04 07:48:08 -0800

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 294ba7aef895bab061d12ebb9b04f2c56ae4e80e
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I75df1f9ff96e3fcf44352bd726233b5dadf146f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6319882
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427729}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/headless_shell.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/headless_shell.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from .base import require_arg
 from .base import get_timeout_multiplier   # noqa: F401
-from .chrome import ChromeBrowser, debug_args  # noqa: F401
+from .chrome import ChromeBrowser  # noqa: F401
 from .chrome import executor_kwargs as chrome_executor_kwargs
 from ..executors.base import WdspecExecutor  # noqa: F401
 from ..executors.executorchrome import (  # noqa: F401
