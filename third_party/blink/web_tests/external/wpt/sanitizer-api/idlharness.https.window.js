5b5762c5899cd6c4e4254d45c4b686b51cb3dd64
Daniel Vogelheim <vogelheim@chromium.org>
2025-03-04 04:03:17 -0800

    [Sanitizer] Remove outdated IDL expectation.
    Remove the long-outdated interface expectation file
    interfaces/sanitizer-api.tentative.idl.
    
    Note that the current interface remains, at interfaces/sanitizer-api.idl.
    
    Bug: 356601280
    Change-Id: I78df1eeff74350851237a4f414ed66a6a7bb299d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6291304
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427651}

--- third_party/blink/web_tests/external/wpt/sanitizer-api/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/sanitizer-api/idlharness.https.window.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/idlharness.js
 
 idl_test(
-  ['sanitizer-api.tentative'],
+  ['sanitizer-api'],
   ['html'],
   idl_array => {
     idl_array.add_objects({
