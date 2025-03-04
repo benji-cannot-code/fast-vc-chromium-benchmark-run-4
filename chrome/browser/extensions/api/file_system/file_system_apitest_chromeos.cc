98be1bc9c4e5a785cf19e1782d3e4e4b95f1bfba
Hidehiko Abe <hidehiko@chromium.org>
2025-03-04 08:40:47 -0800

    Reland "Refactor *LoggedInBrowserTestMixin for DeviceLocalAccounts."
    This reverts commit 898c9e9ef22fb98e2aa29cd4384dd5f24d00c920.
    
    Reason for revert: Reland with fix tests.
    The original CL was based on a bit older commit, so new usage was not captured.
    
    Original change's description:
    > Revert "Refactor *LoggedInBrowserTestMixin for DeviceLocalAccounts."
    >
    > This reverts commit bcbf75ff053614f4d46bb3da856fdcd82ccaaf57.
    >
    > Reason for revert:
    > Breaks MSAN compile:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Builder/86450/overview
    >
    > Original change's description:
    > > Refactor *LoggedInBrowserTestMixin for DeviceLocalAccounts.
    > >
    > > This is preparation to use these mixins for more test cases.
    > > - Renamed KioskLoggedIn to KioskAppLoggedIn for naming consistency.
    > > - Let ctor take account_id instead of user_id directly.
    > >   Then, generate user_id from the account_id.
    > >
    > > BUG=396238168
    > > TEST=Tryjob
    > >
    > > Change-Id: Ic05d44eb29b702d4f18d042417cabf5770c84e5c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318899
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1427512}
    >
    > Bug: 396238168
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: If304f545c26aa5a06108bfa31f3380e797283890
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6320855
    > Commit-Queue: Alan Cutter <alancutter@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Alan Cutter <alancutter@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1427520}
    
    Bug: 396238168
    Change-Id: Ia0b04641a7f9246f32147b463351701b90ddc490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6321066
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427759}

--- chrome/browser/extensions/api/file_system/file_system_apitest_chromeos.cc
+++ chrome/browser/extensions/api/file_system/file_system_apitest_chromeos.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/drive/drive_integration_service.h"
 #include "chrome/browser/ash/drive/drivefs_test_support.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
-#include "chrome/browser/ash/test/kiosk_logged_in_browser_test_mixin.h"
+#include "chrome/browser/ash/test/kiosk_app_logged_in_browser_test_mixin.h"
 #include "chrome/browser/extensions/api/file_system/consent_provider_impl.h"
 #include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/ui/browser.h"
@@ -290,8 +290,8 @@ class FileSystemApiKioskTestForRequestFileSystem
   FileSystemApiKioskTestForRequestFileSystem() { set_chromeos_user_ = false; }
 
  private:
-  ash::KioskLoggedInBrowserTestMixin kiosk_mixin_{
-      &mixin_host_, "example@kiosk-apps.device-local.localhost"};
+  ash::KioskAppLoggedInBrowserTestMixin kiosk_mixin_{&mixin_host_,
+                                                     "kiosk-app-account"};
 };
 
 IN_PROC_BROWSER_TEST_F(FileSystemApiTestForDrive,
