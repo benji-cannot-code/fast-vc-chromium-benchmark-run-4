17afc99103424dcb6e973ceeb2b4930c1c6056c7
Ameur Hosni <ameurhosni@google.com>
2025-03-04 02:42:23 -0800

    [IOS][OR] Introduce autocomplete match wrapper
    This CL introduces a new class that will be responsible for wrapping
    autocomplete matches.
    
    Bug: 399042940
    Change-Id: I514fb532d644890346eda22e5d9976e871b91a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6316733
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427636}

--- ios/chrome/browser/omnibox/ui_bundled/popup/omnibox_popup_mediator.h
+++ ios/chrome/browser/omnibox/ui_bundled/popup/omnibox_popup_mediator.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/window_open_disposition.h"
 
 @protocol ApplicationCommands;
+@class AutocompleteMatchWrapper;
 @class BrowserActionFactory;
 @class CarouselItem;
 @protocol CarouselItemConsumer;
@@ -109,6 +110,9 @@ class Tracker;
     protocolProvider;
 @property(nonatomic, strong) BrowserActionFactory* mostVisitedActionFactory;
 @property(nonatomic, weak) id<CarouselItemConsumer> carouselItemConsumer;
+
+/// Autcomplete match wrapper.
+@property(nonatomic, strong) AutocompleteMatchWrapper* autocompleteMatchWrapper;
 
 /// Designated initializer. Takes ownership of `imageFetcher`.
 - (instancetype)
