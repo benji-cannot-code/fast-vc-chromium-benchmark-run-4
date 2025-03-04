3bf9183dd59d0a0ff2325d4e4bf42611534bee1b
Kent Tamura <tkent@chromium.org>
2025-03-03 22:42:30 -0800

    CanvasTextNG: Implement shaping
    This CL has no behavior changes yet because of a disabled runtime flag.
    
    Bug: 389726691
    Change-Id: I373186bcedb5a31365aaf1d152cbf13a7578c439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6320373
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427574}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_spacing.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_spacing.h
@@ -55,6 +55,8 @@ class PLATFORM_EXPORT ShapeResultSpacing final {
   // Set letter-spacing, word-spacing, and
   // justification. Available only for TextRun.
   void SetSpacingAndExpansion(const FontDescription&);
+  // This one is not only for TextRun.
+  void SetSpacingAndExpansion(const FontDescription&, bool normalize_space);
 
   // Compute the sum of all spacings for the specified |index|.
   // The |index| is for the |TextContainerType| given in the constructor.
