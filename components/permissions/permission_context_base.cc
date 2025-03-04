b26a176c3475582d76b06affce37f13f1cc3b525
Ari Chivukula <arichiv@chromium.org>
2025-03-04 12:01:34 -0800

    Reland "[Partitioned Popins] Add RSA Support (1) test main&sub-frame"
    This reverts commit e1d2b89e75308fad04b1a691adfc2be426b91d95.
    
    Reason for revert: Fixing issues with `linux-chromeos-chrome` related to default feature values.
    
    The only changes in this CL are in tests, there were no discovered
    issues in the production logic of the reverted CL.
    
    This also merges in the following pure test CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6309932
    
    Original change's description:
    > Revert "[Partitioned Popins] Add RSA Support (1) test main-frame"
    >
    > This reverts commit 49d8b69b6d93540fc7e3345d3249154878570424.
    >
    > Reason for revert:
    > New tests are failing in `linux-chromeos-chrome`.
    >
    > First failed build:
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/49277/overview
    >
    > Original change's description:
    > > [Partitioned Popins] Add RSA Support (1) test main-frame
    > >
    > > We should ensure RSA calls to enable access to first-party cookies from
    > > within a popin work. We will need other tests to verify the UX flow.
    > >
    > > This CL is part of a series:
    > > (1) test main-frame
    > > (2) test sub-frame
    > > (3) add WPTs
    > >
    > > Explainer: https://explainers-by-googlers.github.io/partitioned-popins/
    > > I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/ApU_zUmpQ2g/
    > >
    > > Bug: 340606651
    > > Change-Id: Iff5b209c09207b7d172f45654ea70aeb14f2f54a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954128
    > > Commit-Queue: Balazs Engedy <engedy@chromium.org>
    > > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > > Reviewed-by: Balazs Engedy <engedy@chromium.org>
    > > Reviewed-by: Sandor «Alex» Major <sandormajor@chromium.org>
    > > Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1426421}
    >
    > Bug: 340606651
    > Change-Id: I9fa318d11d0203ebd787bc2d545a3866352641d7
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313909
    > Auto-Submit: Xiyuan Xia <xiyuan@chromium.org>
    > Owners-Override: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1426522}
    
    Bug: 340606651
    Change-Id: Iaaa37af8476cd14ba2553cd094136d7fc48a219d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313462
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Sandor «Alex» Major <sandormajor@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427896}

--- components/permissions/permission_context_base.cc
+++ components/permissions/permission_context_base.cc
@@ -111,8 +111,7 @@ void PermissionContextBase::RequestPermission(
   request_data
       .WithRequestingOrigin(
           request_data.requesting_origin.DeprecatedGetOriginAsURL())
-      .WithEmbeddingOrigin(
-          PermissionUtil::GetLastCommittedOriginAsURL(rfh->GetMainFrame()));
+      .WithEmbeddingOrigin(GetEffectiveEmbedderOrigin(rfh));
 
   if (!request_data.requesting_origin.is_valid() ||
       !request_data.embedding_origin.is_valid()) {
@@ -266,6 +265,11 @@ const PermissionRequest* PermissionContextBase::FindPermissionRequest(
   }
 
   return request->second.first.get();
+}
+
+GURL PermissionContextBase::GetEffectiveEmbedderOrigin(
+    content::RenderFrameHost* rfh) const {
+  return PermissionUtil::GetLastCommittedOriginAsURL(rfh->GetMainFrame());
 }
 
 content::PermissionResult PermissionContextBase::GetPermissionStatus(
