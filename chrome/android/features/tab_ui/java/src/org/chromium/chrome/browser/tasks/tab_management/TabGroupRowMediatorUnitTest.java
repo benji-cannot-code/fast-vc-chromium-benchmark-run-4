4a013b0eeb313ea0264c11a56909b830d837efa3
Sky Malice <skym@chromium.org>
2025-03-04 13:38:06 -0800

    Update SharedGroupTestHelper to reflect backend changes.
    Change-Id: Id0c3e642d3abe7f5daaab94d7a72ed9e46d8adea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6320001
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427965}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupRowMediatorUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupRowMediatorUnitTest.java
@@ -107,8 +107,7 @@ public class TabGroupRowMediatorUnitTest {
         mUrl4 = JUnitTestGURLs.BLUE_1;
         mUrl5 = JUnitTestGURLs.BLUE_2;
         mSyncedGroupTestHelper = new SyncedGroupTestHelper(mTabGroupSyncService);
-        mSharedGroupTestHelper =
-                new SharedGroupTestHelper(mDataSharingService, mCollaborationService);
+        mSharedGroupTestHelper = new SharedGroupTestHelper(mCollaborationService);
         mContext =
                 new ContextThemeWrapper(
                         ContextUtils.getApplicationContext(), R.style.Theme_BrowserUI_DayNight);
@@ -220,7 +219,7 @@ public class TabGroupRowMediatorUnitTest {
 
     @Test
     public void testCollaborationButOnlyOneUser() {
-        mSharedGroupTestHelper.respondToReadGroup(COLLABORATION_ID1, GROUP_MEMBER1);
+        mSharedGroupTestHelper.mockGetGroupData(COLLABORATION_ID1, GROUP_MEMBER1);
         PropertyModel propertyModel = buildTestModel(/* isShared= */ true, mUrl1);
 
         assertFalse(propertyModel.get(DISPLAY_AS_SHARED));
@@ -229,7 +228,7 @@ public class TabGroupRowMediatorUnitTest {
 
     @Test
     public void testShared() {
-        mSharedGroupTestHelper.respondToReadGroup(COLLABORATION_ID1, GROUP_MEMBER1, GROUP_MEMBER2);
+        mSharedGroupTestHelper.mockGetGroupData(COLLABORATION_ID1, GROUP_MEMBER1, GROUP_MEMBER2);
         PropertyModel propertyModel = buildTestModel(/* isShared= */ true, mUrl1);
 
         assertTrue(propertyModel.get(DISPLAY_AS_SHARED));
@@ -238,7 +237,7 @@ public class TabGroupRowMediatorUnitTest {
 
     @Test
     public void testDestroyable() {
-        mSharedGroupTestHelper.respondToReadGroup(COLLABORATION_ID1, GROUP_MEMBER1, GROUP_MEMBER2);
+        mSharedGroupTestHelper.mockGetGroupData(COLLABORATION_ID1, GROUP_MEMBER1, GROUP_MEMBER2);
         PropertyModel propertyModel = buildTestModel(/* isShared= */ true, mUrl1);
 
         assertTrue(propertyModel.get(DISPLAY_AS_SHARED));
