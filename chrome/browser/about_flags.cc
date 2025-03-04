437d8f0bb93c44700ca74ba2fb885a560a988f71
Xi Han <hanxi@google.com>
2025-03-04 09:28:45 -0800

    [AppIntegration] Add a testing option for CCT to use the existing schema.
    Before the new subclasses of the WebPage schema are ready, setting new
    schema request will fail, and thus the donation will be disabled. In
    this CL, we add a flag to reuse the existing WebPage schema and only
    checks the whether the existing v1 schema is ready. This param is for
    testing only.
    
    Bug: 397457989
    Change-Id: I56b93423846e13de4e83f124d0688b3c4bd7155d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6316886
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427801}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2630,6 +2630,17 @@ const FeatureEntry::FeatureVariation kAndroidAppIntegrationModuleVariations[] =
       std::size(kAndroidAppIntegrationModule_ForceCardShown_NonPixel),
       nullptr}};
 
+const FeatureEntry::FeatureParam
+    kAndroidAppIntegrationMultiDataSource_UseSchemaV1[] = {
+        {"use_schema_v1", "true"}};
+
+const FeatureEntry::FeatureVariation
+    kAndroidAppIntegrationMultiDataSourceVariations[] = {
+        {"Force to use schema v1",
+         kAndroidAppIntegrationMultiDataSource_UseSchemaV1,
+         std::size(kAndroidAppIntegrationMultiDataSource_UseSchemaV1),
+         nullptr}};
+
 const FeatureEntry::FeatureParam kAuxiliarySearchDonation_MaxDonation_20[] = {
     {chrome::android::kAuxiliarySearchMaxBookmarksCountParam.name, "20"},
     {chrome::android::kAuxiliarySearchMaxTabsCountParam.name, "20"}};
@@ -6290,8 +6301,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAndroidAppIntegrationMultiDataSourceName,
      flag_descriptions::kAndroidAppIntegrationMultiDataSourceDescription,
      kOsAndroid,
-     FEATURE_VALUE_TYPE(
-         chrome::android::kAndroidAppIntegrationMultiDataSource)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(
+         chrome::android::kAndroidAppIntegrationMultiDataSource,
+         kAndroidAppIntegrationMultiDataSourceVariations,
+         "AndroidAppIntegrationMultiDataSource")},
 
     {"android-app-integration-v2",
      flag_descriptions::kAndroidAppIntegrationV2Name,
