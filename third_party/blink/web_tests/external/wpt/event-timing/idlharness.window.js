cdf8716cbf2abef0737a40d4b14479ae6d573945
Annie Sullivan <sullivan@chromium.org>
2025-03-04 12:07:24 -0800

    Clean up event-timing idlharness tests.
    * `performance` should be available for `window` and workers
    * `performance.eventCounts` is only available on `window`
    * `performance.interactionCounts` has been renamed and is no longer an interface.
    
    Bug: 40231541
    Change-Id: I50b834dbd408a61512faf91f59bbc71f8a945c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322853
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427902}

--- third_party/blink/web_tests/external/wpt/event-timing/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/event-timing/idlharness.window.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: global=window,worker
+// META: global=window
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
 
@@ -13,6 +13,8 @@ idl_test(
   idl_array => {
     idl_array.add_objects({
       Performance: ['performance'],
+      EventCounts: ['performance.eventCounts'],
+      // PerformanceEventTiming: [ TODO ]
     });
   }
 );
