11b1284c835f928ba95fa6d6c3c1ef44508716b3
Timofey Chudakov <tchudakov@google.com>
2025-03-04 07:58:33 -0800

    [Wallet] Use GURL to represent loyalty card program logo URL.
    Before this CL, the loyalty card's program logo URL was represented
    as an std::string. This CL:
    * Changes to type to GURL in the LoyaltyCard class;
    * Makes sure loyalty cards with an invalid program logo URLs are not
    added to the database;
    * Adds URL validation logic during the loyalty card retrieval from the
    database.
    
    Bug: 393123618
    Change-Id: I49fc3edc285edcc4db9512331c46aa6246788e4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299397
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427735}

--- components/autofill/core/browser/data_model/passes/loyalty_card.h
+++ components/autofill/core/browser/data_model/passes/loyalty_card.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "url/gurl.h"
+
 namespace autofill {
 
 // Represents a loaylty card coming from the Google Wallet.
@@ -16,7 +18,7 @@ class LoyaltyCard final {
   LoyaltyCard(std::string loyalty_card_id,
               std::string merchant_name,
               std::string program_name,
-              std::string program_logo,
+              GURL program_logo,
               std::string unmasked_loyalty_card_suffix);
 
   LoyaltyCard(const LoyaltyCard&);
@@ -25,6 +27,11 @@ class LoyaltyCard final {
   LoyaltyCard& operator=(LoyaltyCard&&) = default;
 
   ~LoyaltyCard();
+
+  // Checks if this loyalty card is valid. A valid loyalty card contains a
+  // non-empty loyalty card id and a logo URL which should be either empty or
+  // valid.
+  bool IsValid() const;
 
   friend bool operator==(const LoyaltyCard&, const LoyaltyCard&) = default;
 
@@ -36,7 +43,7 @@ class LoyaltyCard final {
   // The loyalty card program name e.g. "BahnBonus".
   std::string program_name;
   // The logo icon URL.
-  std::string program_logo;
+  GURL program_logo;
   // The unmasked part of the  loyalty card issuer text code. The full number is
   // not available on the client.
   std::string unmasked_loyalty_card_suffix;
--- components/autofill/core/browser/test_utils/passes_data_test_utils.cc
+++ components/autofill/core/browser/test_utils/passes_data_test_utils.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/test_utils/passes_data_test_utils.h"
 
 #include "components/autofill/core/browser/data_model/passes/loyalty_card.h"
+#include "url/gurl.h"
 
 namespace autofill::test {
 
@@ -13,14 +14,14 @@ LoyaltyCard CreateLoyaltyCard() {
   return LoyaltyCard(
       /*loyalty_card_id=*/"loyalty_card_id_1",
       /*merchant_name=*/"Deutsche Bahn", /*program_name=*/"BahnBonus",
-      /*program_logo=*/"https://empty.url.com",
+      /*program_logo=*/GURL("https://empty.url.com"),
       /*unmasked_loyalty_card_suffix=*/"1234");
 }
 
 LoyaltyCard CreateLoyaltyCard2() {
   return LoyaltyCard(/*loyalty_card_id=*/"loyalty_card_id_2",
                      /*merchant_name=*/"Lidl", /*program_name=*/"CustomerCard",
-                     /*program_logo=*/"https://empty.url.com",
+                     /*program_logo=*/GURL("https://empty.url.com"),
                      /*unmasked_loyalty_card_suffix=*/"4321");
 }
 
