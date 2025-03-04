c65e7af22b79ff7059f417f299a0562434df943f
Peter Wen <wnwen@chromium.org>
2025-03-04 14:42:52 -0800

    android: Create generate_orderfile targets
    Add new arm32 and arm64 targets:
    - generate_orderfile_android_trichrome_chrome_bundle
    - generate_orderfile_android_trichrome_chrome_64_32_bundle
    
    Add --chromium-output-directory as an arg passed to run_benchmark for
    the two scenarios that generate_orderfile.py calls.
    
    For now, use --streamline-for-debugging until the bots are set up and
    passing.
    
    R=mheikal@chromium.org
    
    Bug: 380082658
    Change-Id: I7ae8d46d3b1d31202162474bd9b8d598da70afdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6321261
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428003}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -2370,6 +2370,7 @@ _GENERIC_PYDEPS_FILES = [
     'third_party/blink/tools/merge_web_test_results.pydeps',
     'tools/binary_size/sizes.pydeps',
     'tools/binary_size/supersize.pydeps',
+    'tools/cygprofile/generate_orderfile.pydeps',
     'tools/perf/process_perf_results.pydeps',
     'tools/pgo/generate_profile.pydeps',
 ]
