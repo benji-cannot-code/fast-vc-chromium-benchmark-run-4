021f825940e60110d43e63702f30c49124a37c39
Fredrik Söderquist <fs@opera.com>
2025-03-04 09:48:10 -0800

    Move AlphaDisposition from graphics_types.h to canvas_image_source.h
    This is a parameter type (essentially a boolean) for CanvasImageSource.
    Make it local to said class. Change a use in StaticBitmapImageTransform
    by hoisting the trivial case up into callers.
    
    Bug: 399058536
    Change-Id: Iaf787183d23a44247521d91c756412add76e9c47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322191
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1427808}

--- third_party/blink/renderer/core/html/canvas/canvas_image_source.h
+++ third_party/blink/renderer/core/html/canvas/canvas_image_source.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/graphics/flush_reason.h"
-#include "third_party/blink/renderer/platform/graphics/graphics_types.h"
 #include "third_party/blink/renderer/platform/graphics/image_orientation.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "ui/gfx/geometry/size_f.h"
@@ -53,6 +52,10 @@ enum SourceImageStatus {
 
 class CORE_EXPORT CanvasImageSource {
  public:
+  enum AlphaDisposition {
+    kPremultiplyAlpha,
+    kDontChangeAlpha,
+  };
   virtual scoped_refptr<Image> GetSourceImageForCanvas(
       FlushReason,
       SourceImageStatus*,
