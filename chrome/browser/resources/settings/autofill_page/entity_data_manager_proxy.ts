e5e7fc84aeb4363e1cb6822e5a0d716a83963405
Theo Cristea <theocristea@google.com>
2025-03-04 13:44:13 -0800

    Implement editing entities in Autofill UI settings UI
    Upon clicking the "edit" button, the user will see a modal where they
    can edit all attributes of that entity. The user can confirm or cancel
    their updates.
    
    Demo:
    https://screenshot.googleplex.com/7TezjpSozmEnyqM
    
    The user is not able to submit the form if all the fields are empty or
    full of whitespaces.
    
    Demo:
    https://screenshot.googleplex.com/7Kpzhy6nQuxJ8uQ
    
    Bug: 393319296
    Change-Id: I0bb92e1761bed95a066b7f4d48e945a55bc9285d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6280788
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427969}

--- chrome/browser/resources/settings/autofill_page/entity_data_manager_proxy.ts
+++ chrome/browser/resources/settings/autofill_page/entity_data_manager_proxy.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+type AttributeType = chrome.autofillPrivate.AttributeType;
 type EntityInstance = chrome.autofillPrivate.EntityInstance;
 type EntityInstanceWithLabels = chrome.autofillPrivate.EntityInstanceWithLabels;
 
@@ -26,6 +27,16 @@ export interface EntityDataManagerProxy {
    * Returns the user's entity instances with labels.
    */
   loadEntityInstances(): Promise<EntityInstanceWithLabels[]>;
+
+  /**
+   * Returns the entity instance by its guid.
+   */
+  getEntityInstanceByGuid(guid: string): Promise<EntityInstance>;
+
+  /**
+   * Returns a list of all possible attributes that can be set on an entity.
+   */
+  getAllAttributeTypesForEntity(entityType: number): Promise<AttributeType[]>;
 }
 
 export class EntityDataManagerProxyImpl implements EntityDataManagerProxy {
@@ -39,6 +50,14 @@ export class EntityDataManagerProxyImpl implements EntityDataManagerProxy {
 
   loadEntityInstances() {
     return chrome.autofillPrivate.loadEntityInstances();
+  }
+
+  getEntityInstanceByGuid(guid: string) {
+    return chrome.autofillPrivate.getEntityInstanceByGuid(guid);
+  }
+
+  getAllAttributeTypesForEntity(entityType: number) {
+    return chrome.autofillPrivate.getAllAttributeTypesForEntity(entityType);
   }
 
   static getInstance() {
