bb308f7108669f74e43b7a27d3ae296fb5381c14
manukh <manukh@chromium.org>
2025-03-03 19:14:27 -0800

    [omnibox][spark] Finch param to toggle doc provider
    Motive is because enterprise provider has poor precision so we must
    score it conservatively. Not running drive provider will result in users
    who rely on it no longer reliably seeing their docs.
    
    For unscoped inputs:
    - BEFORE, doc provider doesn't run; enterprise does.
    - AFTER, with the param `disable_drive` false, both providers run.
    - AFTER, with the param `disable_drive` true, then it behaves like
      before: doc doesn't run; enterprise does. (deafult)
    
    For scoped inputs, there's no change: if the input is in enterprise /
    doc scope then the enterprise / doc provider runs and the other doesn't.
    
    Bug: 395881808
    Change-Id: Ifdb8bac8ed934861ace6f602708c3244bcc2ed75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6315829
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427526}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -1156,7 +1156,9 @@ bool AutocompleteController::ShouldRunProvider(
     // aggregator provider suggestions, which can be configured to provide
     // Google Drive suggestions.
     case AutocompleteProvider::TYPE_DOCUMENT:
-      return !should_run_search_aggregator_provider;
+      return !omnibox_feature_configs::SearchAggregatorProvider::Get()
+                  .disable_drive ||
+             !should_run_search_aggregator_provider;
 
     case AutocompleteProvider::TYPE_OPEN_TAB:
       return is_cros_launcher_;
