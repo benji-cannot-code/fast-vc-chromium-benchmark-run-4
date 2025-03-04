29fd794f1c1e8f297c0ee1e2fdaea4b1fbd2708e
Noam Rosenthal <nrosenthal@chromium.org>
2025-03-04 13:36:46 -0800

    Reapply style after a moveBefore
    The style's render-blocking status should be preserved when it is moved,
    but the style must be un-applied and re-applied, as the location of the style in the DOM has significance.
    
    Bug: 394380744
    
    R=dom@chromium.org
    
    Change-Id: I26404c40bd58f36c8b7e205533c8d3328e21fe24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304605
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427964}

--- third_party/blink/renderer/core/html/link_style.cc
+++ third_party/blink/renderer/core/html/link_style.cc
@@ -256,16 +256,12 @@ void LinkStyle::SetDisabledState(bool disabled) {
 LinkStyle::LoadReturnValue LinkStyle::LoadStylesheetIfNeeded(
     const LinkLoadParameters& params,
     const WTF::TextEncoding& charset) {
-  if (GetDocument().StatePreservingAtomicMoveInProgress()) {
-    return kNotNeeded;
-  }
-
   if (disabled_state_ == kDisabled || !owner_->RelAttribute().IsStyleSheet() ||
       !StyleSheetTypeIsSupported(params.type) || !ShouldLoadResource() ||
       !params.href.IsValid())
     return kNotNeeded;
 
-  if (GetResource()) {
+  if (GetResource() && !GetDocument().StatePreservingAtomicMoveInProgress()) {
     RemovePendingSheet();
     ClearResource();
   }
@@ -298,7 +294,9 @@ LinkStyle::LoadReturnValue LinkStyle::LoadStylesheetIfNeeded(
       *owner_, critical_style, owner_->IsCreatedByParser());
   PendingSheetType type = type_and_behavior.first;
 
-  AddPendingSheet(type);
+  if (!GetDocument().StatePreservingAtomicMoveInProgress()) {
+    AddPendingSheet(type);
+  }
 
   // Load stylesheets that are not needed for the layout immediately with low
   // priority.  When the link element is created by scripts, load the
@@ -387,8 +385,10 @@ void LinkStyle::SetSheetTitle(const String& title) {
 }
 
 void LinkStyle::OwnerRemoved() {
-  if (StyleSheetIsLoading())
+  if (StyleSheetIsLoading() &&
+      !GetDocument().StatePreservingAtomicMoveInProgress()) {
     RemovePendingSheet();
+  }
 
   if (sheet_)
     ClearSheet();
