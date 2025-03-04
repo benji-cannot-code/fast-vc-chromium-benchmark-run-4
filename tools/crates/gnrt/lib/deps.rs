abb15ad83c533496019dc98563e67123cb67d8dd
Lukasz Anforowicz <lukasza@chromium.org>
2025-03-04 14:34:41 -0800

    [gnrt] Deduplicate where packages are sorted.
    Bug: 395924069
    Change-Id: I3194128a09e989ae563cfd7c22510332e659d302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318488
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427994}

--- tools/crates/gnrt/lib/deps.rs
+++ tools/crates/gnrt/lib/deps.rs
@@ -350,7 +350,11 @@ pub fn collect_dependencies(
     }
 
     // Return a flat list of dependencies.
-    Ok(dependencies.into_values().collect())
+    let mut dependencies = dependencies.into_values().collect::<Vec<_>>();
+    dependencies.sort_unstable_by(|a, b| {
+        a.package_name.cmp(&b.package_name).then(a.version.cmp(&b.version))
+    });
+    Ok(dependencies)
 }
 
 /// Graph traversal state shared by recursive calls of `explore_node`.
@@ -544,11 +548,8 @@ mod tests {
 
         let metadata: cargo_metadata::Metadata =
             serde_json::from_str(SAMPLE_CARGO_METADATA).unwrap();
-        let mut dependencies =
+        let dependencies =
             collect_dependencies(&metadata, "sample_package", &build_config).unwrap();
-        dependencies.sort_by(|left, right| {
-            left.package_name.cmp(&right.package_name).then(left.version.cmp(&right.version))
-        });
 
         let empty_str_slice: &'static [&'static str] = &[];
 
@@ -913,10 +914,7 @@ mod tests {
             serde_json::from_str(SAMPLE_CARGO_METADATA).unwrap();
 
         // Start from "foo" workspace member.
-        let mut dependencies = collect_dependencies(&metadata, "foo", &config).unwrap();
-        dependencies.sort_by(|left, right| {
-            left.package_name.cmp(&right.package_name).then(left.version.cmp(&right.version))
-        });
+        let dependencies = collect_dependencies(&metadata, "foo", &config).unwrap();
 
         let mut i = 0;
 
