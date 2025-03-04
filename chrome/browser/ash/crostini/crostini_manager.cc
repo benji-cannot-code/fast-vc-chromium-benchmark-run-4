846033923a647fd7c34e960f5ac4d7a87dc119b7
maciek swiech <drmasquatch@google.com>
2025-03-04 14:54:06 -0800

    Use container name for baguette
    Instead of fighting existing code, we choose to pretend baguette vm's
    also have a container, which seems to solve many problems, not limited
    to solving terminal access working correctly.
    
    This patch also clears container prefs on delete, as the previous code
    that would have been trigerred during container deletion is not being
    run. There may still be other cleanup code that needs to be added, such
    as handling MIME type association, etc.
    
    Bug: b:377377749
    Change-Id: I28ba0eb72130b319b68e3daf75b1f2a840ce6fdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313396
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: maciek swiech <drmasquatch@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428008}

--- chrome/browser/ash/crostini/crostini_manager.cc
+++ chrome/browser/ash/crostini/crostini_manager.cc
@@ -885,11 +885,8 @@ void CrostiniManager::CrostiniRestarter::SharePathsFinished(
   }
   if (base::FeatureList::IsEnabled(ash::features::kCrostiniContainerless)) {
     StartStage(mojom::InstallerState::kConfigureContainer);
-    // TODO(crbug.com/377377749): eventually username should come from
-    // requests_[0].options.container_username once vsh knows how to connect to
-    // the right username
     crostini_manager_->SetUpBaguetteUser(
-        container_id_.vm_name, "chronos",
+        container_id_.vm_name, requests_[0].options.container_username,
         base::BindOnce(&CrostiniRestarter::SetUpBaguetteUserFinished,
                        weak_ptr_factory_.GetWeakPtr()));
   } else {
@@ -3822,6 +3819,12 @@ void CrostiniManager::OnRemoveTermina(bool success) {
     return;
   }
 
+  if (base::FeatureList::IsEnabled(ash::features::kCrostiniContainerless)) {
+    // container prefs seem to be wiped as some part of lxd container removal
+    // callbacks in the regular flow, so we must remove them manually here for
+    // baguette.
+    profile_->GetPrefs()->ClearPref(guest_os::prefs::kGuestOsContainers);
+  }
   profile_->GetPrefs()->SetBoolean(prefs::kCrostiniEnabled, false);
   profile_->GetPrefs()->ClearPref(prefs::kCrostiniLastDiskSize);
   guest_os::RemoveVmFromPrefs(profile_, kCrostiniDefaultVmType);
