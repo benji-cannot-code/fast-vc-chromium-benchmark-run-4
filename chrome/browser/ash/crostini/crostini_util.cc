54aa4515eeb9027f37249c1da46433485072aaeb
maciek swiech <drmasquatch@google.com>
2025-03-04 10:57:43 -0800

    Create baguette user as part of installation
    In crostini installs, users were created as part of container setup. Now
    that we have no more setup, we need to call new maitred-based RPC for
    account creation. This fixes the issue where a manual `vmc start` was
    required to get a baguette install functional.
    
    Bug: b:377377749
    Change-Id: Ia113acb7309c816e577cd66a460e549dad143696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265104
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: maciek swiech <drmasquatch@google.com>
    Cr-Commit-Position: refs/heads/main@{#1427847}

--- chrome/browser/ash/crostini/crostini_util.cc
+++ chrome/browser/ash/crostini/crostini_util.cc
@@ -54,6 +54,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crostini {
 namespace {
 
+// Keep 'penguin' terminal label for backwards-consistent appearance.
+constexpr char kBaguetteTerminalLabel[] = "penguin";
 constexpr char kCrostiniAppLaunchHistogram[] = "Crostini.AppLaunch";
 constexpr char kCrostiniAppLaunchResultHistogram[] = "Crostini.AppLaunchResult";
 constexpr char kCrostiniAppLaunchResultHistogramTerminal[] =
@@ -510,6 +512,10 @@ bool ShouldStopVm(Profile* profile, const guest_os::GuestId& container_id) {
 
 std::string FormatForUi(guest_os::GuestId guest_id) {
   if (guest_id.vm_name == kCrostiniDefaultVmName) {
+    if (guest_id.container_name.empty()) {
+      // containerless crostini, aka baguette
+      return kBaguetteTerminalLabel;
+    }
     return guest_id.container_name;
   }
   return base::StrCat({guest_id.vm_name, ":", guest_id.container_name});
