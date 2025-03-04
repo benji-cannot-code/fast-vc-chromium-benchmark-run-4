e092389bb40457dd3476ae528f8af9a217eaa298
Joel Hockey <joelhockey@chromium.org>
2025-03-03 20:44:14 -0800

    Remove flag kDragDropEmpty
    Flag was enabled since M131 and can now be removed in M136.
    
    Bug: 363930156
    Change-Id: I73d0b82f355ef9da1f25cfeb07cbf93534c94349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169860
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1427546}

--- ui/android/java/src/org/chromium/ui/dragdrop/DragAndDropDelegateImpl.java
+++ ui/android/java/src/org/chromium/ui/dragdrop/DragAndDropDelegateImpl.java
@@ -40,8 +40,6 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.ui.R;
 import org.chromium.ui.accessibility.AccessibilityState;
 import org.chromium.ui.base.MimeTypeUtils;
-import org.chromium.ui.base.UiAndroidFeatureList;
-import org.chromium.ui.base.UiAndroidFeatureMap;
 import org.chromium.ui.dragdrop.AnimatedImageDragShadowBuilder.CursorOffset;
 import org.chromium.ui.dragdrop.AnimatedImageDragShadowBuilder.DragShadowSpec;
 import org.chromium.ui.dragdrop.DragDropMetricUtils.UrlIntentSource;
@@ -159,11 +157,6 @@ public class DragAndDropDelegateImpl implements DragAndDropDelegate, DragStateTr
     private boolean startDragAndDropInternal(
             View containerView, DragShadowBuilder dragShadowBuilder, DropDataAndroid dropData) {
         ClipData clipdata = buildClipData(dropData);
-        if (clipdata == null
-                && !UiAndroidFeatureMap.isEnabled(UiAndroidFeatureList.DRAG_DROP_EMPTY)) {
-            return false;
-        }
-
         mIsDragStarted = true;
         mDragStartSystemElapsedTime = SystemClock.elapsedRealtime();
         mDragTargetType = getDragTargetType(dropData);
@@ -460,8 +453,6 @@ public class DragAndDropDelegateImpl implements DragAndDropDelegate, DragStateTr
         // Only record metrics when drop does not happen for ContentView.
         if (!mIsDropOnView) {
             assert mDragStartSystemElapsedTime > 0;
-            assert mDragTargetType != DragTargetType.INVALID
-                    || UiAndroidFeatureMap.isEnabled(UiAndroidFeatureList.DRAG_DROP_EMPTY);
             long dragDuration = SystemClock.elapsedRealtime() - mDragStartSystemElapsedTime;
             recordDragDurationAndResult(dragDuration, dragResult);
             recordDragTargetType(mDragTargetType);
