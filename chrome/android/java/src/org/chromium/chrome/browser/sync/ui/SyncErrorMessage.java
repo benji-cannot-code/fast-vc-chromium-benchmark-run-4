a20b0d9c5602e15448996800a1108d48ff71deed
Yurii Lahodiuk <ylahodiuk@google.com>
2025-03-05 03:11:31 -0800

    [sync] Update strings for the "key required for passwords" error notification
    Based on the recommendations of UX and UXW we will perform A/B/C
    testing with the following error message variants:
    
    Title: Get all your passwords
    Message: Verify it’s you to get all your passwords on this device
    Button:
    - Option 1: Okay
    - Option 2: Verify
    
    Bug: 379880581
    Change-Id: I58ca1b43c378ca2dd8b0d9ee653212f79c79653a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322249
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428227}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ui/SyncErrorMessage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ui/SyncErrorMessage.java
@@ -272,7 +272,18 @@ public class SyncErrorMessage implements SyncService.SyncStateChangedListener, U
     private @StringRes int getButtonTextForTrustedVaultErrorInfobarStudy() {
         if (ChromeFeatureList.isEnabled(
                 ChromeFeatureList.SYNC_ENABLE_PASSWORDS_SYNC_ERROR_MESSAGE_ALTERNATIVE)) {
-            return R.string.identity_error_card_button_verify;
+            switch (getTrustedVaultErrorMessageVersion()) {
+                case 1:
+                    return R.string.identity_error_message_button_verify;
+                case 2:
+                    return R.string.identity_error_card_button_okay;
+                default:
+                    // This should never happen, as there are only two versions.
+                    assert false
+                            : "Invalid version for SyncEnablePasswordsSyncErrorMessageAlternative: "
+                                    + getTrustedVaultErrorMessageVersion();
+                    break;
+            }
         }
         return R.string.identity_error_message_button_verify;
     }
@@ -310,18 +321,7 @@ public class SyncErrorMessage implements SyncService.SyncStateChangedListener, U
     private @StringRes int getTitleForTrustedVaultErrorMessageStudy() {
         if (ChromeFeatureList.isEnabled(
                 ChromeFeatureList.SYNC_ENABLE_PASSWORDS_SYNC_ERROR_MESSAGE_ALTERNATIVE)) {
-            switch (getTrustedVaultErrorMessageVersion()) {
-                case 1:
-                    return R.string.password_sync_trusted_vault_error_title_1;
-                case 2:
-                    return R.string.password_sync_trusted_vault_error_title_2;
-                default:
-                    // This should never happen, as there are only two versions.
-                    assert false
-                            : "Invalid version for SyncEnablePasswordsSyncErrorMessageAlternative: "
-                                    + getTrustedVaultErrorMessageVersion();
-                    break;
-            }
+            return R.string.password_sync_trusted_vault_error_title;
         }
         return R.string.identity_error_card_button_verify;
     }
@@ -363,18 +363,7 @@ public class SyncErrorMessage implements SyncService.SyncStateChangedListener, U
     private @StringRes int getContentForTrustedVaultErrorMessageStudy() {
         if (ChromeFeatureList.isEnabled(
                 ChromeFeatureList.SYNC_ENABLE_PASSWORDS_SYNC_ERROR_MESSAGE_ALTERNATIVE)) {
-            switch (getTrustedVaultErrorMessageVersion()) {
-                case 1:
-                    return R.string.password_sync_trusted_vault_error_hint_1;
-                case 2:
-                    return R.string.password_sync_trusted_vault_error_hint_2;
-                default:
-                    // This should never happen, as there are only two versions.
-                    assert false
-                            : "Invalid version for SyncEnablePasswordsSyncErrorMessageAlternative: "
-                                    + getTrustedVaultErrorMessageVersion();
-                    break;
-            }
+            return R.string.password_sync_trusted_vault_error_hint;
         }
         return R.string.identity_error_message_body_sync_retrieve_keys_for_passwords;
     }
