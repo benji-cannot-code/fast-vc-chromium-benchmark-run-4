e9dc2f4a2f3b30dc345c3152070b7f78deb85f99
Alexander Cooper <alcooper@chromium.org>
2025-03-05 15:49:07 -0800

    Remove UI Flag for WebXr Hand Input
    The feature has been enabled by default for a while now. We feel that a
    UI flag is no longer needed; however, we'd like to keep the actual
    feature around a bit longer to ensure no issues arise.
    
    Bug: 400544139
    Change-Id: Idbd8920a00ee137fbbc39c1360554479e8c73d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329759
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428623}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2360,9 +2360,6 @@ extern const char kWebXrHandAnonymizationChoiceNone[];
 extern const char kWebXrHandAnonymizationChoiceRuntime[];
 extern const char kWebXrHandAnonymizationChoiceFallback[];
 
-extern const char kWebXrHandInputName[];
-extern const char kWebXrHandInputDescription[];
-
 extern const char kWebXrProjectionLayersName[];
 extern const char kWebXrProjectionLayersDescription[];
 
