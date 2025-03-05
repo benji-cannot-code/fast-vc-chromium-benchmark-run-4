3675720918159e6813ae184ddef2985499ac73f4
Alex Chen <alexwchen@chromium.org>
2025-03-05 12:00:52 -0800

    Reland "[omnibox][search aggregator] Show icon in chrome://settings/searchEngines"
    This is a reland of commit 987a66e8bcff50d1a326ef408e2544d04c25683b
    
    Instead of directly calling `chrome://image/?` for fetching icons via
    URL directly, use `cr-auto-img` which is the recommended way to fetch
    external images for WebUI. The search_engine_entry.html is updated to
    have two icon options:
     - `cr-auto-img` which is used by default for engines with `iconURL` defined. This downloads the image using `chrome://image/?`.
     - `site-favicon` which is used for if 1) `iconPath` is defined or 2) downloading image using `cr-auto-img` fails. A common failure is due to cross-origin resource sharing errors as described in b:393574078#comment10.
    
    Unit tests are added as well to verify favicon behavior.
    
    Per discussions with WebUI team, backend changes were also considered
    including updating `SanitizedImageSource` (which backs
    `chrome://image/?` to support fallback URLs and changing
    `SearchEnginesHandler()` to predetermine the appropriate icon type to
    use.
    
    This change, which utilizes front-end logic, mimics existing favicon
    behavior used by password manager
    (https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/resources/password_manager/site_favicon.html).
    
    Before: https://screenshot.googleplex.com/6AkQWLFWXiWH7hg
    
    After: https://screenshot.googleplex.com/9pbeUmGuhu8FKY5
    
    Original change's description:
    > [omnibox][search aggregator] Show icon in chrome://settings/searchEngines
    >
    > This change updates the search engines page to show the image for the
    > favicon URL set in a search engine. Before, the page relied on the
    > favicon and history service to fetch the favicon for a URL. As a result,
    > no icon would show until a user had navigated to the URL itself.
    >
    > This is achieved by updating `site_favicon.ts` to accept an `iconUrl`
    > which should point directly to an external image/icon. `icon.ts` is
    > updated to use chrome://image to fetch an external image.
    >
    > Before: https://screenshot.googleplex.com/6AkQWLFWXiWH7hg
    > After: https://screenshot.googleplex.com/7tibrchCyi2diJy
    >
    > Bug: b:393574078
    > Change-Id: I085a72389fd8c85f807fc91cc5af6709c87c1abb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6277482
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Commit-Queue: Alex Chen <alexwchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1424094}
    
    Bug: b:393574078
    Change-Id: I9b8722dda15e34b40bedcf7609c0d454ab13aab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313766
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428488}

--- chrome/browser/resources/settings/search_engines_page/search_engine_entry.ts
+++ chrome/browser/resources/settings/search_engines_page/search_engine_entry.ts
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview 'settings-search-engine-entry' is a component for showing a
  * search engine with its name, domain and query URL.
  */
+import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/icons.html.js';
 import 'chrome://resources/cr_elements/policy/cr_policy_indicator.js';
@@ -29,6 +30,7 @@ export interface SettingsSearchEngineEntryElement {
     delete: HTMLButtonElement,
     makeDefault: HTMLButtonElement,
     edit: HTMLButtonElement,
+    downloadedIcon: HTMLImageElement,
   };
 }
 
@@ -46,7 +48,10 @@ export class SettingsSearchEngineEntryElement extends
 
   static get properties() {
     return {
-      engine: Object,
+      engine: {
+        type: Object,
+        observer: 'onEngineChanged_',
+      },
 
       showShortcut: {type: Boolean, value: false, reflectToAttribute: true},
 
@@ -62,6 +67,11 @@ export class SettingsSearchEngineEntryElement extends
         type: Boolean,
         computed: 'computeShowEditIcon_(engine)',
       },
+
+      showDownloadedIcon_: {
+        type: Boolean,
+        value: false,
+      },
     };
   }
 
@@ -72,6 +82,28 @@ export class SettingsSearchEngineEntryElement extends
   private browserProxy_: SearchEnginesBrowserProxy =
       SearchEnginesBrowserProxyImpl.getInstance();
   private showEditIcon_: boolean;
+  private showDownloadedIcon_: boolean;
+  private timeoutId_: number|null = null;
+
+  private onEngineChanged_(
+      newEngine: SearchEngine, oldEngine: SearchEngine|undefined) {
+    if (oldEngine && newEngine.iconURL === oldEngine.iconURL) {
+      return;
+    }
+    this.showDownloadedIcon_ = false;
+    if (this.timeoutId_) {
+      clearTimeout(this.timeoutId_);
+      this.timeoutId_ = null;
+    }
+    this.timeoutId_ = setTimeout(() => {
+      if (!this.$.downloadedIcon.complete) {
+        // Reset src to cancel ongoing request.
+        this.$.downloadedIcon.src = '';
+        this.showDownloadedIcon_ = false;
+      }
+      this.timeoutId_ = null;
+    }, 1000);
+  }
 
   private closePopupMenu_() {
     this.shadowRoot!.querySelector('cr-action-menu')!.close();
@@ -150,6 +182,23 @@ export class SettingsSearchEngineEntryElement extends
     this.closePopupMenu_();
     this.browserProxy_.setIsActiveSearchEngine(
         this.engine.modelIndex, /*is_active=*/ false);
+  }
+
+  private onDownloadedIconLoadError_() {
+    this.showDownloadedIcon_ = false;
+  }
+
+  private onDownloadedIconLoadSuccess_() {
+    this.showDownloadedIcon_ = true;
+    if (this.timeoutId_) {
+      clearTimeout(this.timeoutId_);
+    }
+    this.timeoutId_ = null;
+  }
+
+  private shouldShowDownloadedIcon_(): boolean {
+    return this.showDownloadedIcon_ && !this.engine.iconPath &&
+        !!this.engine.iconURL;
   }
 
   private getMoreActionsAriaLabel_(): string {
