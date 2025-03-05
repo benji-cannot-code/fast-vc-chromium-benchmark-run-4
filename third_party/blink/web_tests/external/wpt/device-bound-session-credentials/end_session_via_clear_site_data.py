5e987d61b879be4d9e2cc65831d35ea0882be9d1
Daniel Rubery <drubery@chromium.org>
2025-03-05 09:57:42 -0800

    Fix origin matching for clearing device bound sessions
    If a site serves a Clear-Site-Data: storage, we should clear the device
    bound sessions on that origin. This was broken due to the origin we
    compare against being passed through net::SchemefulSite, which drops the
    port. Now we compare against the proper origin and add a WPT test for
    this behavior.
    
    Bug: 353767385
    Change-Id: I9402d3d41b029a03dffde41e95b523102285f13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317357
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428403}

--- third_party/blink/web_tests/external/wpt/device-bound-session-credentials/end_session_via_clear_site_data.py
+++ third_party/blink/web_tests/external/wpt/device-bound-session-credentials/end_session_via_clear_site_data.py
@@ -1,3 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import json
+
 def main(request, response):
-    return (200, [("Clear-Site-Data", '"cookies"')], "")
+    types = request.body.decode('utf-8')
+    if types == "":
+        types = '"cookies"'
+    return (200, [("Clear-Site-Data", types)], "")
