e98e1923d35d001afc6404489aed10488a8827b5
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2025-03-05 12:27:18 -0800

    Revert "Bookmarks: Migrate bookmarks-app to Lit"
    This reverts commit d067ef625d45a2e9f9122a6580d344fc39ef7c05.
    
    Reason for revert: tree-closing build failure, e.g.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac%20Builder/240826/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-archive-rel/140090/overview
    
    Original change's description:
    > Bookmarks: Migrate bookmarks-app to Lit
    >
    > This is the last custom element to migrate. As part of the migration:
    > - Merging MouseFocusMixin into BookmarksApp, since this is the only
    >   client.
    > - Removing now unused StoreClientMixin and shared_style.css (Polymer
    >   mixin and shared style).
    > - Updating store test to test the Lit version of them mixin, which is
    >   now used by all bookmarks elements.
    >
    > Bug: 40943652
    > Change-Id: I10b59eebe7612bd7a23187f3c26f1669730f0dac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6320601
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1428489}
    
    Bug: 40943652
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I922a73235651a1bd2fed6f3cbca37f27246fac34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330538
    Owners-Override: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1428498}

--- chrome/test/data/webui/bookmarks/dnd_manager_test.ts
+++ chrome/test/data/webui/bookmarks/dnd_manager_test.ts
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import type {BookmarkElement, BookmarksAppElement, BookmarksFolderNodeElement, BookmarksItemElement, BookmarksListElement, DndManager} from 'chrome://bookmarks/bookmarks.js';
 import {BookmarkManagerApiProxyImpl, BrowserProxyImpl, DragInfo, overrideFolderOpenerTimeoutDelay, setDebouncerForTesting} from 'chrome://bookmarks/bookmarks.js';
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertDeepEquals, assertEquals, assertFalse, assertNotReached, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {middleOfNode, topLeftOfNode} from 'chrome://webui-test/mouse_mock_interactions.js';
-import {eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
 import {TestBookmarkManagerApiProxy} from './test_bookmark_manager_api_proxy.js';
 import {TestBookmarksBrowserProxy} from './test_browser_proxy.js';
@@ -156,7 +158,7 @@ suite('drag and drop', function() {
           eventToPromise('viewport-filled', list.$.list),
         ])
         .then(() => {
-          return microtasksFinished();
+          flush();
         });
   });
 
@@ -454,7 +456,7 @@ suite('drag and drop', function() {
     store.data.folderOpenState.set('14', false);
     store.data.folderOpenState.set('15', false);
     store.notifyObservers();
-    await microtasksFinished();
+    flush();
 
     const dragElement = getFolderNode('15');
     await simulateDragStart(dragElement);
@@ -462,19 +464,19 @@ suite('drag and drop', function() {
     // Dragging onto folders without children doesn't open the folder.
     let dragTarget = getFolderNode('14');
     move(dragTarget);
-    await microtasksFinished();
+    await flushTasks();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto itself doesn't open the folder.
     move(dragElement);
-    await microtasksFinished();
+    await flushTasks();
     assertFalse(dragElement.isOpen);
 
     // Dragging onto an open folder doesn't affect the folder.
     dragTarget = getFolderNode('1');
     assertTrue(dragTarget.isOpen);
     move(dragTarget);
-    await microtasksFinished();
+    await flushTasks();
     assertTrue(dragTarget.isOpen);
 
     dragTarget = getFolderNode('11');
@@ -482,25 +484,25 @@ suite('drag and drop', function() {
     // Dragging off of a closed folder doesn't open it.
     move(dragTarget);
     move(list);
-    await microtasksFinished();
+    await flushTasks();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto a folder with DragStyle.BELOW doesn't open it.
     move(dragTarget, bottomRightOfNode(dragTarget));
     assertDragStyle(dragTarget, DragStyle.BELOW);
-    await microtasksFinished();
+    await flushTasks();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto a folder with DragStyle.ABOVE doesn't open it.
     move(dragTarget, topLeftOfNode(dragTarget));
     assertDragStyle(dragTarget, DragStyle.ABOVE);
-    await microtasksFinished();
+    await flushTasks();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto a closed folder with children opens it.
     move(dragTarget);
     assertDragStyle(dragTarget, DragStyle.ON);
-    await microtasksFinished();
+    await flushTasks();
     assertTrue(dragTarget.isOpen);
   });
 
--- chrome/test/data/webui/bookmarks/store_test.ts
+++ chrome/test/data/webui/bookmarks/store_test.ts
@@ -4,37 +4,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {BookmarksPageState, NodeMap} from 'chrome://bookmarks/bookmarks.js';
-import {createEmptyState, removeBookmark, Store, StoreClientMixinLit} from 'chrome://bookmarks/bookmarks.js';
-import {CrLitElement, html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
-import type {PropertyValues} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+import {createEmptyState, removeBookmark, Store, StoreClientMixin} from 'chrome://bookmarks/bookmarks.js';
+import {flush, html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestStore} from './test_store.js';
 import {createFolder, createItem, getAllFoldersOpenState, testTree} from './test_util.js';
 
-const TestStoreClientBase = StoreClientMixinLit(CrLitElement);
+const TestStoreClientBase = StoreClientMixin(PolymerElement);
 
 class TestStoreClient extends TestStoreClientBase {
-  static get is() {
-    return 'test-store-client';
-  }
+  hasChanged: boolean = false;
 
-  override render() {
+  static get template() {
     return html`
-      ${this.toArray(this.items).map(item => html`
-        <div class="item">${item}</div>`)}
+      <template is="dom-repeat" items="[[toArray(items)]]">
+        <div class="item">[[item]]</div>
+      </template>
     `;
   }
 
-  static override get properties() {
+  static get properties() {
     return {
-      items: {type: Object},
+      items: {
+        type: Object,
+        observer: 'itemsChanged',
+      },
     };
   }
-
-  items: NodeMap = {};
-  hasChanged: boolean = false;
 
   toArray(items: NodeMap) {
     return Object.values(items).map(value => value.id);
@@ -42,23 +39,14 @@ class TestStoreClient extends TestStoreClientBase {
 
   override connectedCallback() {
     super.connectedCallback();
+    this.watch('items', function(state) {
+      return state.nodes;
+    });
     this.updateFromStore();
   }
 
-  override willUpdate(changedProperties: PropertyValues<this>) {
-    super.willUpdate(changedProperties);
-    if (changedProperties.has('items') &&
-        Object.values(this.items).length > 0) {
-      this.itemsChanged(this.items, changedProperties.get('items'));
-    }
-  }
-
-  override onStateChanged(state: BookmarksPageState) {
-    this.items = state.nodes || {};
-  }
-
   itemsChanged(_newItems: NodeMap, oldItems?: NodeMap) {
-    if (oldItems && Object.values(oldItems).length > 0) {
+    if (oldItems) {
       this.hasChanged = true;
     }
   }
@@ -108,10 +96,10 @@ suite('bookmarks.StoreClientMixin', function() {
   let store: Store;
   let client: TestStoreClient;
 
-  function update(newState: BookmarksPageState): Promise<void> {
+  function update(newState: BookmarksPageState) {
     store.data = newState;
     store.endBatchUpdate();
-    return microtasksFinished();
+    flush();
   }
 
   function getRenderedItems() {
@@ -135,14 +123,14 @@ suite('bookmarks.StoreClientMixin', function() {
 
     client = document.createElement('test-store-client') as TestStoreClient;
     document.body.appendChild(client);
-    return microtasksFinished();
+    flush();
   });
 
   test('renders initial data', function() {
     assertDeepEquals(['0', '1', '11', '12', '13'], getRenderedItems());
   });
 
-  test('renders changes to watched state', async () => {
+  test('renders changes to watched state', function() {
     assertFalse(client.hasChanged);
     const newItems = testTree(createFolder('1', [
       createItem('11'),
@@ -151,15 +139,15 @@ suite('bookmarks.StoreClientMixin', function() {
     const newState = Object.assign({}, store.data, {
       nodes: newItems,
     });
-    await update(newState);
+    update(newState);
 
     assertTrue(client.hasChanged);
     assertDeepEquals(['0', '1', '11', '12'], getRenderedItems());
   });
 
-  test('ignores changes to other subtrees', async () => {
+  test('ignores changes to other subtrees', function() {
     const newState = Object.assign({}, store.data, {selectedFolder: 'foo'});
-    await update(newState);
+    update(newState);
 
     assertFalse(client.hasChanged);
   });
