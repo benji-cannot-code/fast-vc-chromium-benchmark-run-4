896cb1999a44e8542656404d833e2007a1d30dd7
anthie@google.com <anthie@google.com>
2025-03-05 10:57:31 -0800

    Enforce single dialog can be opened during an in-progress local approval
    This change detects if an approval is already in progress and
    terminates further requests early.
    The decision of whether a local approval runs is based on the
    existence of the PACP content observer, whose creation is now
    moved earlier to prevent the user from opening multiple dialogs
    with fast, consequent "Ask in person" button clicks (before the
    action is blocked by the modal dialog).
    
    Bug: 397169132
    Change-Id: Ie135b814948b7f551bd4dab40c12afe439dedfda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322028
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428452}

--- chrome/browser/supervised_user/linux_mac_windows/supervised_user_web_content_handler_impl.h
+++ chrome/browser/supervised_user/linux_mac_windows/supervised_user_web_content_handler_impl.h
@@ -57,9 +57,7 @@ class SupervisedUserWebContentHandlerImpl
   }
 
  private:
-  void CreateObserverFromContents(base::TimeTicks start_time,
-                                  const GURL& target_url,
-                                  content::WebContents* contents);
+  void StartObervingPacpContents(content::WebContents* contents);
 
   void CompleteUrlApprovalAndCloseOrUpdateDialog(
       const GURL& target_url,
@@ -77,6 +75,8 @@ class SupervisedUserWebContentHandlerImpl
   // Stops WebContents observation by the `dialog_web_contents_observer_`
   // and resets the unique pointer.
   void ResetDialogResultContentObserver();
+
+  bool IsLocalApprovalInProgress();
 
   std::unique_ptr<ParentAccessDialogResultObserver>
       dialog_web_contents_observer_;
