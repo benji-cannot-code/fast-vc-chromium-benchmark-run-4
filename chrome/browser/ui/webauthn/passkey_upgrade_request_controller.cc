3300b3b265cf47c1dad7ca84fadf228303fb9202
Martin Kreichgauer <martinkr@google.com>
2025-03-05 07:16:29 -0800

    passkeys: fail conditional create requests with setting disabled
    CL:6300089 added a settings toggle for disabling automatic passkey
    upgrades in GPM settings. But the underlying pref isn't currently wired
    into the PasskeyUpgradeRequestController, so it has no effect.
    
    Fail the request with a NotAllowedError if the feature is disabled.
    
    Include-Ci-Only-Tests: true
    
    Fixed: 400800958
    Change-Id: I25f3af63b7daea7bda6955dafd96a2966127ab25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6324953
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428314}

--- chrome/browser/ui/webauthn/passkey_upgrade_request_controller.cc
+++ chrome/browser/ui/webauthn/passkey_upgrade_request_controller.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_store/password_store_interface.h"
 #include "components/password_manager/core/browser/password_store/password_store_util.h"
 #include "components/password_manager/core/browser/password_sync_util.h"
+#include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/sync/service/sync_service.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_frame_host.h"
@@ -53,6 +54,7 @@ enum class PasskeyUpgradeRequestController::RequestError {
   kPasswordStoreError,
   kNotEligible,
   kEnclaveError,
+  kOptOut,
 };
 
 enum class PasskeyUpgradeRequestController::EnclaveState {
@@ -94,6 +96,12 @@ void PasskeyUpgradeRequestController::TryUpgradePasswordToPasskey(
   delegate_ = delegate;
   rp_id_ = std::move(rp_id);
   username_ = base::UTF8ToUTF16(username);
+
+  if (!profile()->GetPrefs()->GetBoolean(
+          password_manager::prefs::kAutomaticPasskeyUpgrades)) {
+    SignalRequestFailure(RequestError::kOptOut);
+    return;
+  }
 
   switch (enclave_state_) {
     case EnclaveState::kUnknown:
