95b7bba2cbfdce462e82eadfc6991ffcbc64492d
Mohamed Heikal <mheikal@chromium.org>
2025-03-05 12:30:45 -0800

    Add @NullMarked to //chrome/browser/policy
    Bug: 389129271
    Change-Id: If724abe419db3ef4912368c08dc68b7c498975b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329637
    Commit-Queue: Martin Kong <martinkong@google.com>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Owners-Override: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428502}

--- chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/PolicyServiceFactory.java
+++ chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/PolicyServiceFactory.java
@@ -10,6 +10,8 @@ import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.ResettersForTesting;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.policy.PolicyService;
 
@@ -19,8 +21,9 @@ import org.chromium.components.policy.PolicyService;
  * is still valid.
  */
 @JNINamespace("policy::android")
+@NullMarked
 public class PolicyServiceFactory {
-    private static PolicyService sPolicyServiceForTest;
+    private static @Nullable PolicyService sPolicyServiceForTest;
 
     /**
      * Returns the PolicyService instance that contains browser policies.
