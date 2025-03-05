cf5356beedb6b712ab4384867ef6dad5d89c0f0d
Christine Hollingsworth <christinesm@chromium.org>
2025-03-05 07:27:25 -0800

    Reland "[Language Detector] Implement overhauled availability() method"
    This is a reland of commit 1a53226877f80465213a47d5730cd255612b742b
    
    Instead of disabling failing tests per-platform (crrev.com/c/6299237),
    the browsertest setup has been updated for all platforms, in order to
    directly solve the initial failing tests from both previous reland
    attempts.
    
    Manually enabling the "LanguageDetectionAPI" feature in the updated
    browsertest resolves previous failures that occurred post-merge.
    (linux-chromeos-rel and linux-win-cross-rel)
    
    Please refer to the diff from Patchset 2 --> Patchset 7 for the update
    in this reland attempt. Note that Gerrit is unable to create a reland CL
    for previous attempts due to merge conflicts, and this reland CL was
    created manually.
    
    Original change's description:
    > [Language Detector] Implement overhauled availability() method
    >
    > Implementation of the availability() method for the
    > Language Detector API. Determines whether the language model is available for Language Detection.
    >
    > Bug: 390459311
    > Change-Id: Ie8c168b509b397fec08b3229c1a51930bec413c6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6272235
    > Reviewed-by: Nathan Memmott <memmott@chromium.org>
    > Reviewed-by: Jiacheng Guo <gjc@google.com>
    > Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    > Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1423915}
    
    Bug: 390459311
    Change-Id: Ic61f34eacb68a90eefef87660a27fc636abbf981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304828
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Auto-Submit: Christine Hollingsworth <christinesm@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428320}

--- chrome/browser/language_detection/language_detection_model_service_browsertest.cc
+++ chrome/browser/language_detection/language_detection_model_service_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/strings/stringprintf.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/models.pb.h"
 #include "components/translate/core/common/translate_util.h"
 #include "components/translate/core/language_detection/language_detection_model.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
@@ -101,6 +103,28 @@ class LanguageDetectionModelServiceDisabledBrowserTest
     InProcessBrowserTest::SetUp();
   }
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    InProcessBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
+                                    "LanguageDetectionAPI");
+  }
+
+  void TestLanguageDetectionAvailable(Browser* browser,
+                                      const std::string_view result) {
+    ASSERT_EQ(EvalJs(browser->tab_strip_model()->GetActiveWebContents(),
+                     base::StringPrintf(R"(
+        (async () => {
+            try {
+            return await ai.languageDetector.availability();
+            } catch (e) {
+            return e.toString();
+            }
+            })();
+        )", ))
+                  .ExtractString(),
+              result);
+  }
+
   ~LanguageDetectionModelServiceDisabledBrowserTest() override = default;
 
   const GURL& english_url() const { return english_url_; }
@@ -115,6 +139,14 @@ IN_PROC_BROWSER_TEST_F(LanguageDetectionModelServiceDisabledBrowserTest,
                        LanguageDetectionModelServiceDisabled) {
   EXPECT_FALSE(LanguageDetectionModelServiceFactory::GetForProfile(
       browser()->profile()));
+}
+
+IN_PROC_BROWSER_TEST_F(LanguageDetectionModelServiceDisabledBrowserTest,
+                       Availability_ModelUnavailable) {
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), english_url()));
+
+  // Language detection is not available when the model service is disabled.
+  TestLanguageDetectionAvailable(browser(), "unavailable");
 }
 
 class LanguageDetectionModelServiceWithoutOptimizationGuideBrowserTest
@@ -231,6 +263,12 @@ class LanguageDetectionModelServiceBrowserTest
     ASSERT_TRUE(origin_server_->Start());
     english_url_ = origin_server_->GetURL("/hello_world.html");
     InProcessBrowserTest::SetUp();
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    InProcessBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
+                                    "LanguageDetectionAPI");
   }
 
   // Waits for the model file to be resolved. `nullopt` will be returned if the
@@ -573,6 +611,29 @@ IN_PROC_BROWSER_TEST_F(LanguageDetectionModelServiceBrowserTest,
   // Requesting one more now should give a valid file because the queue has been
   // emptied.
   ASSERT_TRUE(RequestAndWaitForModelFile()->IsValid());
+}
+
+// Tests the behavior of availability().
+IN_PROC_BROWSER_TEST_F(LanguageDetectionModelServiceBrowserTest, Availability) {
+  base::ScopedAllowBlockingForTesting allow_io_for_test_setup;
+  ASSERT_TRUE(language_detection_model_service());
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), english_url()));
+
+  // Language detection is not readily available until the model is downloaded.
+  TestLanguageDetectionAvailable(browser(), "downloadable");
+
+  ModelFileGetter getter(*language_detection_model_service());
+  OptimizationGuideKeyedServiceFactory::GetForProfile(browser()->profile())
+      ->OverrideTargetModelForTesting(
+          optimization_guide::proto::OPTIMIZATION_TARGET_LANGUAGE_DETECTION,
+          optimization_guide::TestModelInfoBuilder()
+              .SetModelFilePath(model_file_path())
+              .Build());
+
+  getter.RequestModelFile();
+  auto model_file = getter.WaitForModelFile();
+
+  TestLanguageDetectionAvailable(browser(), "available");
 }
 
 }  // namespace
