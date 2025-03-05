436d168e4b6ff06c15b0188b98fce1236a276288
Peter Kvitek <kvitekp@chromium.org>
2025-03-05 12:59:53 -0800

    [headless] Remove shared display util dependency on //ui/aura
    This dependency may create a cyclic in the places of intended use,
    so it's safer not to have it.
    
    Bug: 396072563
    Change-Id: Ie4194e7aaec7c51e1f9a4eda2f5fa33123ea197f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6324790
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428531}

--- components/headless/display_util/headless_display_util.h
+++ components/headless/display_util/headless_display_util.h
@@ -12,14 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/native_widget_types.h"
 
 namespace headless {
-
-// Return display that the window belongs or is closest to.
-std::optional<display::Display> GetDisplayFromWindow(
-    const std::vector<display::Display>& displays,
-    gfx::NativeWindow window);
 
 // Return display that the screen rectangle belongs or is closest to.
 std::optional<display::Display> GetDisplayFromScreenRect(
