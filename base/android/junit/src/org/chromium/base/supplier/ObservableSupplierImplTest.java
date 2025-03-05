69c2b657bbe236b02a139a53f9ab9548234de671
Andrew Grieve <agrieve@chromium.org>
2025-03-05 14:19:21 -0800

    ObservableSupplier: Remove addObserverAndCall()
    When trying to roll NullAway, ObservableSupplier started to be properly
    null-checked. This lead to the realization that:
    
    For an ObservableSupplier<@NonNull Foo>:
    1) addObserver*() takes a Callback<@NonNull Foo>
    2) except "addObserverAndCall()", which takes Callback<@Nullable Foo>
    
    This means that within the class, all callbacks should probably be
    Callback<@Nullable>, but it's not actually valid to cast a
    Callback<@NonNull> to Callback<@Nullable> since a callback expected a
    non-null should not receive a null.
    
    Since there are not yet any uses of addObserverAndCall(), I'm removing
    it for now. If we find compelling use-cases for it, we can add it back,
    and likely just mark the entire class as @NullUnmarked.
    
    I also realized that "addObserverAndCallIfSet()" is poor wording for
    @Nullable ObservableSuppliers, because they could be explicitly set
    to null. Rather than support "null" meaning "hasValue()=true", this
    renames the method to "addObserverAndCallIfNonNull()"
    
    Finally, this fixes an innocuous bug where UnwrapObservableSupplier
    observers were being called before a value was set.
    
    Bug: 389129271, 394598049
    Change-Id: Idf82bb88255ead37f9f20ee2c13b215c1c497be4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6323286
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428580}

--- base/android/junit/src/org/chromium/base/supplier/ObservableSupplierImplTest.java
+++ base/android/junit/src/org/chromium/base/supplier/ObservableSupplierImplTest.java
@@ -86,21 +86,11 @@ public class ObservableSupplierImplTest {
     }
 
     @Test
-    public void testAddObserver_ShouldNotOmitNullOnAdd() {
-        AtomicBoolean called = new AtomicBoolean(false);
-        Callback<String> supplierObserver = ignored -> called.set(true);
-
-        mSupplier.addSyncObserverAndCall(supplierObserver); // !NotifyBehavior.OMIT_NULL_ON_ADD;
-        ShadowLooper.runUiThreadTasks();
-        assertTrue(called.get());
-    }
-
-    @Test
     public void testAddObserver_ShouldOmitNullOnAdd() {
         AtomicBoolean called = new AtomicBoolean(false);
         Callback<String> supplierObserver = ignored -> called.set(true);
 
-        mSupplier.addSyncObserverAndCallIfSet(supplierObserver);
+        mSupplier.addSyncObserverAndCallIfNonNull(supplierObserver);
         ShadowLooper.runUiThreadTasks();
         assertFalse(called.get());
 
@@ -115,7 +105,7 @@ public class ObservableSupplierImplTest {
         AtomicBoolean called = new AtomicBoolean(false);
         Callback<String> supplierObserver = ignored -> called.set(true);
 
-        mSupplier.addSyncObserverAndPostIfSet(supplierObserver);
+        mSupplier.addSyncObserverAndPostIfNonNull(supplierObserver);
         ShadowLooper.runUiThreadTasks();
         assertTrue(called.get());
     }
@@ -126,7 +116,7 @@ public class ObservableSupplierImplTest {
         AtomicBoolean called = new AtomicBoolean(false);
         Callback<String> supplierObserver = ignored -> called.set(true);
 
-        mSupplier.addSyncObserverAndCallIfSet(supplierObserver); // !NotifyBehavior.POST_ON_ADD
+        mSupplier.addSyncObserverAndCallIfNonNull(supplierObserver); // !NotifyBehavior.POST_ON_ADD
         boolean idle = ShadowLooper.shadowMainLooper().isIdle();
         assertTrue(idle);
     }
