076612cc8bf092e906b3dc8abc309b3cf9d6e800
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-03-04 16:47:00 -0800

    Rename base::win::EvaluateWin11UpgradeEligibility()
    This CL rename `EvaluateWin11UpgradeEligibility()` to
    `EvaluateWin11HardwareRequirement()` to make it align with the current implementation.
    
    Change-Id: I696a7e74db7e3e8a6d752dbad5a53ef39f8f8554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6324549
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428057}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -778,7 +778,7 @@ void RecordAppCompatMetrics() {
   base::UmaHistogramBoolean("Windows.AcLayersLoaded", !!mod);
 }
 
-void RecordWin11UpgradeEligibilityMetrics(
+void RecordWin11HardwareRequirementsMetrics(
     const base::win::HardwareEvaluationResult& result) {
   base::UmaHistogramBoolean("Windows.Win11UpgradeEligible",
                             result.IsEligible());
@@ -839,8 +839,8 @@ void RecordStartupMetrics() {
 
   if (base::win::OSInfo::Kernel32Version() < base::win::Version::WIN11) {
     base::win::HardwareEvaluationResult result =
-        base::win::EvaluateWin11UpgradeEligibility();
-    RecordWin11UpgradeEligibilityMetrics(result);
+        base::win::EvaluateWin11HardwareRequirements();
+    RecordWin11HardwareRequirementsMetrics(result);
   }
   key_credential_manager_support::ReportKeyCredentialManagerSupport();
 #endif  // BUILDFLAG(IS_WIN)
