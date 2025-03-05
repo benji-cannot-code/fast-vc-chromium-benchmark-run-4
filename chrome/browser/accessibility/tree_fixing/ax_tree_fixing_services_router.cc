7032137dd71915ec7098dd11c0daca864658cab6
Mark Schillaci <mschillaci@google.com>
2025-03-05 08:05:38 -0800

    Add stubbed AXTreeFixingWrapper and associated Factory
    AX-Relnotes: N/A
    Bug: 395134535
    Change-Id: I2407df515b21f93004f4d3e78e8c6f9131175ca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6265503
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428338}

---
+++ chrome/browser/accessibility/tree_fixing/ax_tree_fixing_services_router.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/accessibility/tree_fixing/ax_tree_fixing_services_router.h"
+
+namespace tree_fixing {
+
+AXTreeFixingServicesRouter::AXTreeFixingServicesRouter(
+    content::BrowserContext* context) {}
+AXTreeFixingServicesRouter::~AXTreeFixingServicesRouter() = default;
+
+}  // namespace tree_fixing
