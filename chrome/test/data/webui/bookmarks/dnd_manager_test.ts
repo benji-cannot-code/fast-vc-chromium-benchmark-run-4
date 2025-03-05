50f8649b9b09c2241278f9f23bb0a489e4203a91
rbpotter <rbpotter@chromium.org>
2025-03-05 13:35:10 -0800

    Reland "Bookmarks: Migrate bookmarks-app to Lit"
    Adding the Lit dep broke the build due to new eslint checks.
    
    See PS3 -> PS4 diff for fix.
    
    Original change's description:
    > Bookmarks: Migrate bookmarks-app to Lit
    >
    > This is the last custom element to migrate. As part of the migration:
    > - Merging MouseFocusMixin into BookmarksApp, since this is the only
    >   client.
    > - Removing now unused StoreClientMixin and shared_style.css (Polymer
    >   mixin and shared style).
    > - Updating store test to test the Lit version of them mixin, which is
    >   now used by all bookmarks elements.
    >
    > Bug: 40943652
    > Change-Id: I10b59eebe7612bd7a23187f3c26f1669730f0dac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6320601
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1428489}
    
    Bug: 40943652
    Change-Id: I5937ba8ee0df323c6651d6450a48b5a38a646ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329846
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428555}

--- chrome/test/data/webui/bookmarks/dnd_manager_test.ts
+++ chrome/test/data/webui/bookmarks/dnd_manager_test.ts
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import type {BookmarkElement, BookmarksAppElement, BookmarksFolderNodeElement, BookmarksItemElement, BookmarksListElement, DndManager} from 'chrome://bookmarks/bookmarks.js';
 import {BookmarkManagerApiProxyImpl, BrowserProxyImpl, DragInfo, overrideFolderOpenerTimeoutDelay, setDebouncerForTesting} from 'chrome://bookmarks/bookmarks.js';
-import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertDeepEquals, assertEquals, assertFalse, assertNotReached, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {middleOfNode, topLeftOfNode} from 'chrome://webui-test/mouse_mock_interactions.js';
-import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
-import {eventToPromise} from 'chrome://webui-test/test_util.js';
+import {eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestBookmarkManagerApiProxy} from './test_bookmark_manager_api_proxy.js';
 import {TestBookmarksBrowserProxy} from './test_browser_proxy.js';
@@ -144,8 +142,8 @@ suite('drag and drop', function() {
     app = document.createElement('bookmarks-app');
     replaceBody(app);
     list =
-        app.shadowRoot!.querySelector<BookmarksListElement>('bookmarks-list')!;
-    rootFolderNode = app.shadowRoot!.querySelector<BookmarksFolderNodeElement>(
+        app.shadowRoot.querySelector<BookmarksListElement>('bookmarks-list')!;
+    rootFolderNode = app.shadowRoot.querySelector<BookmarksFolderNodeElement>(
         'bookmarks-folder-node')!;
     dndManager = app.getDndManagerForTesting() as DndManager;
     dndManager!.setTimerProxyForTesting(new TestTimerProxy());
@@ -158,7 +156,7 @@ suite('drag and drop', function() {
           eventToPromise('viewport-filled', list.$.list),
         ])
         .then(() => {
-          flush();
+          return microtasksFinished();
         });
   });
 
@@ -456,7 +454,7 @@ suite('drag and drop', function() {
     store.data.folderOpenState.set('14', false);
     store.data.folderOpenState.set('15', false);
     store.notifyObservers();
-    flush();
+    await microtasksFinished();
 
     const dragElement = getFolderNode('15');
     await simulateDragStart(dragElement);
@@ -464,19 +462,19 @@ suite('drag and drop', function() {
     // Dragging onto folders without children doesn't open the folder.
     let dragTarget = getFolderNode('14');
     move(dragTarget);
-    await flushTasks();
+    await microtasksFinished();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto itself doesn't open the folder.
     move(dragElement);
-    await flushTasks();
+    await microtasksFinished();
     assertFalse(dragElement.isOpen);
 
     // Dragging onto an open folder doesn't affect the folder.
     dragTarget = getFolderNode('1');
     assertTrue(dragTarget.isOpen);
     move(dragTarget);
-    await flushTasks();
+    await microtasksFinished();
     assertTrue(dragTarget.isOpen);
 
     dragTarget = getFolderNode('11');
@@ -484,25 +482,25 @@ suite('drag and drop', function() {
     // Dragging off of a closed folder doesn't open it.
     move(dragTarget);
     move(list);
-    await flushTasks();
+    await microtasksFinished();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto a folder with DragStyle.BELOW doesn't open it.
     move(dragTarget, bottomRightOfNode(dragTarget));
     assertDragStyle(dragTarget, DragStyle.BELOW);
-    await flushTasks();
+    await microtasksFinished();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto a folder with DragStyle.ABOVE doesn't open it.
     move(dragTarget, topLeftOfNode(dragTarget));
     assertDragStyle(dragTarget, DragStyle.ABOVE);
-    await flushTasks();
+    await microtasksFinished();
     assertFalse(dragTarget.isOpen);
 
     // Dragging onto a closed folder with children opens it.
     move(dragTarget);
     assertDragStyle(dragTarget, DragStyle.ON);
-    await flushTasks();
+    await microtasksFinished();
     assertTrue(dragTarget.isOpen);
   });
 
