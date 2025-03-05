292b234f2945d5abac102270f81dac0091736745
Stepan Khapugin <stkhapugin@chromium.org>
2025-03-05 11:38:17 -0800

    [iOS][LO] Disable the SRP entrypoint by default
    Bug: 400998875
    Change-Id: I3ae6c1476ae769aee61fe208e848b29cdd61698f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329358
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428477}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -246,7 +246,7 @@ BASE_FEATURE(kLensOverlayEnableLocationBarEntrypoint,
 
 BASE_FEATURE(kLensOverlayEnableLocationBarEntrypointOnSRP,
              "LensOverlayEnableLocationBarEntrypointOnSRP",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensOverlayEnableSameTabNavigation,
              "EnableLensOverlaySameTabNavigation",
