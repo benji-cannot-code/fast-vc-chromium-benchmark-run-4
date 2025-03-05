a88749e214b50edcc8221703a8500c5b7cdd51de
Lauren Winston <lwinston@google.com>
2025-03-05 12:07:38 -0800

    [Read aloud] Add exceptions for comma / hyphen splicing.
    e.g. 10,000
    
    Bug: 395909372
    Change-Id: If2c046e5d061d1d01fa6487073186e0459ae1f00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6320594
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428494}

--- chrome/browser/resources/side_panel/read_anything/common.ts
+++ chrome/browser/resources/side_panel/read_anything/common.ts
@@ -92,3 +92,8 @@ export function openMenu(
     });
   });
 }
+
+// Returns true is the given string can be considered whitespace.
+export function isWhitespace(s: string): boolean {
+  return /\s+/g.test(s);
+}
