17dc8764db948d31b9836a09d9cbcd4b9c3b048b
Vladimir Levin <vmpstr@chromium.org>
2025-03-06 08:57:11 -0800

    vt: Allow kIdle state when destroying VT style tracker
    Since the style tracker can be destroyed before kCapturing phase
    (but after kInitial), we need to also allow the kIdle state for the
    style tracker.
    
    R=khushalsagar@chromium.org
    
    Bug: 399698557
    Change-Id: I3e738fe172dd04735b4fbcf5ebe97f32dd883c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330377
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428964}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
@@ -607,7 +607,8 @@ ViewTransitionStyleTracker::ViewTransitionStyleTracker(
 
 ViewTransitionStyleTracker::~ViewTransitionStyleTracker() {
   if (!RuntimeEnabledFeatures::SerializeViewTransitionStateInSPAEnabled()) {
-    CHECK_EQ(state_, State::kFinished);
+    CHECK(state_ == State::kIdle || state_ == State::kFinished)
+        << static_cast<int>(state_);
   }
 }
 
