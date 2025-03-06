5f337f1041f6f0717d38b0466ff59667e92864d3
Radu Nitescu <radunitescu@google.com>
2025-03-06 09:27:00 -0800

    [Objective-C Style][LO] Use const reference for URL in url utils
    Bug: 400916185
    Change-Id: Ie21ddef520ad11e5179af0557949cf3c56a5f120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330299
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428986}

--- ios/chrome/browser/lens_overlay/model/lens_overlay_url_utils.h
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_url_utils.h
@@ -11,20 +11,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace lens {
 
-/// Whether the `url` is from a google domain.
-bool IsGoogleHostURL(GURL url);
+/// Whether the `url` is from a Google domain.
+bool IsGoogleHostURL(const GURL& url);
 
 /// Whether the `url` is a lens overlay SRP.
-bool IsLensOverlaySRP(GURL url);
+bool IsLensOverlaySRP(const GURL& url);
 
 /// Whether the `url` is a lens multimodal SRP.
-bool IsLensMultimodalSRP(GURL url);
+bool IsLensMultimodalSRP(const GURL& url);
 
 /// Returns the search tearm of the lens overlay SRP.
-std::string ExtractQueryFromLensOverlaySRP(GURL url);
+std::string ExtractQueryFromLensOverlaySRP(const GURL& url);
 
-/// Whether the `url` is a redirection initiated from a google URL.
-bool IsGoogleRedirection(GURL url,
+/// Whether the `url` is a redirection initiated from Google URL.
+bool IsGoogleRedirection(const GURL& url,
                          web::WebStatePolicyDecider::RequestInfo request_info);
 
 }  // namespace lens
--- ios/chrome/browser/lens_overlay/model/lens_overlay_url_utils.mm
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_url_utils.mm
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace lens {
 
-bool IsGoogleHostURL(GURL url) {
+bool IsGoogleHostURL(const GURL& url) {
   return google_util::IsGoogleDomainUrl(
       url, google_util::DISALLOW_SUBDOMAIN,
       google_util::DISALLOW_NON_STANDARD_PORTS);
 }
 
-bool IsLensOverlaySRP(GURL url) {
+bool IsLensOverlaySRP(const GURL& url) {
   std::string search_term;
   bool hasSearchTerms = net::GetValueForKeyInQuery(url, "q", &search_term);
   std::string lens_surface;
@@ -31,7 +31,7 @@ bool IsLensOverlaySRP(GURL url) {
          lens_surface == "4";
 }
 
-bool IsLensMultimodalSRP(GURL url) {
+bool IsLensMultimodalSRP(const GURL& url) {
   std::string search_term;
   bool has_search_terms = net::GetValueForKeyInQuery(url, "q", &search_term);
   std::string lens_surface;
@@ -48,13 +48,13 @@ bool IsLensMultimodalSRP(GURL url) {
          has_unified_drilldown_param && lens_surface == "4" && udm == "24";
 }
 
-std::string ExtractQueryFromLensOverlaySRP(GURL url) {
+std::string ExtractQueryFromLensOverlaySRP(const GURL& url) {
   std::string search_term = "";
   net::GetValueForKeyInQuery(url, "q", &search_term);
   return search_term;
 }
 
-bool IsGoogleRedirection(GURL url,
+bool IsGoogleRedirection(const GURL& url,
                          web::WebStatePolicyDecider::RequestInfo request_info) {
   return IsGoogleHostURL(url) &&
          (request_info.transition_type & ui::PAGE_TRANSITION_CLIENT_REDIRECT);
