b763fcc2468499030097c58729025da407ad8f40
Oksana Zhuravlova <oksamyt@chromium.org>
2025-03-06 10:36:55 -0800

    [CodeHealth] Add a reason for RAW_PTR_EXCLUSION in sandbox_nt_util.h
    The memory might be managed by the OS and not PartitionAlloc.
    
    Bug: 346693833
    Change-Id: I122f502e9a60cc6ad0f8be13bc94e67d22a92437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298505
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429030}

--- sandbox/win/src/sandbox_nt_util.h
+++ sandbox/win/src/sandbox_nt_util.h
@@ -14,9 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <intrin.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <memory>
 
+#include <memory>
 #include <optional>
+
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr_exclusion.h"
 #include "sandbox/win/src/nt_internals.h"
@@ -209,8 +210,7 @@ class AutoProtectMemory {
 
  private:
   bool changed_;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
+  // RAW_PTR_EXCLUSION: not managed by PartitionAlloc.
   RAW_PTR_EXCLUSION void* address_;
   size_t bytes_;
   ULONG old_protect_;
