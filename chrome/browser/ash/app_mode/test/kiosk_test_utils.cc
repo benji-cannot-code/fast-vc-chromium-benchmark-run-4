278768a0a29b5a6210ee28f23bcdb84a6674e528
Edman Anjos <edmanp@google.com>
2025-03-06 09:18:41 -0800

    kiosk: Extract KioskAutoLoginBailoutTest from KioskEnterpriseTest
    Also migrates the test to KioskMixin, parameterize over policies, and
    expand it to other Kiosk app types.
    
    Bug: 379246208
    Test: testing/xvfb.py tools/autotest.py -C out/Release chrome/browser/ash/app_mode/test/splash_screen_browsertest.cc chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
    Change-Id: I0928f5ac7b6a06bc10ea1924b982ea701b40f9af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6328644
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428977}

--- chrome/browser/ash/app_mode/test/kiosk_test_utils.cc
+++ chrome/browser/ash/app_mode/test/kiosk_test_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "apps/test/app_window_waiter.h"
+#include "ash/public/cpp/login_accelerators.h"
 #include "ash/public/cpp/login_screen_test_api.h"
 #include "base/auto_reset.h"
 #include "base/check.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_web_app_install_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/webui/ash/login/app_launch_splash_screen_handler.h"
@@ -151,6 +153,11 @@ void WaitNetworkScreen() {
 bool PressNetworkAccelerator() {
   return LoginScreenTestApi::PressAccelerator(
       ui::Accelerator(ui::VKEY_N, ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN));
+}
+
+bool PressBailoutAccelerator() {
+  return LoginDisplayHost::default_host()->HandleAccelerator(
+      LoginAcceleratorAction::kAppLaunchBailout);
 }
 
 void CloseAppWindow(const KioskApp& app) {
--- chrome/browser/ash/app_mode/test/kiosk_test_utils.h
+++ chrome/browser/ash/app_mode/test/kiosk_test_utils.h
@@ -78,6 +78,10 @@ void WaitNetworkScreen();
 // Returns true if the accelerator was processed.
 [[nodiscard]] bool PressNetworkAccelerator();
 
+// Presses the accelerator to cancel (bailout) Kiosk launch. Returns true if the
+// accelerator was processed.
+[[nodiscard]] bool PressBailoutAccelerator();
+
 // Closes the window of the given `app`.
 void CloseAppWindow(const KioskApp& app);
 
