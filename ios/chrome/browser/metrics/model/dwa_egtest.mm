0b6894a1bd809f2c55c044e65004748aaf589a22
Ruth Itua <itua@google.com>
2025-03-06 08:25:34 -0800

    [Fix] Flaky DWATestCase/testUkmMsbbConsentChangeCheck
    NO_IFTTT=for dwa_egtest, as only fixing flaky test.
    
    Bug: 400413009
    Change-Id: I0bf3566ffd01f4c8f6e520d5f7be148ec2bd5e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331138
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ruth Itua <itua@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428951}

--- ios/chrome/browser/metrics/model/dwa_egtest.mm
+++ ios/chrome/browser/metrics/model/dwa_egtest.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/app_launch_configuration.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
+
 using chrome_test_util::GoogleServicesSettingsButton;
 using chrome_test_util::SettingsDoneButton;
 
@@ -166,11 +167,9 @@ using chrome_test_util::SettingsDoneButton;
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
                       chrome_test_util::TableViewSwitchCell(
                           @"betterSearchAndBrowsingItem_switch", YES)];
-  [[[EarlGrey
+  [[EarlGrey
       selectElementWithMatcher:chrome_test_util::TableViewSwitchCell(
                                    @"betterSearchAndBrowsingItem_switch", YES)]
-         usingSearchAction:grey_scrollToContentEdge(kGREYContentEdgeBottom)
-      onElementWithMatcher:chrome_test_util::GoogleServicesSettingsView()]
       performAction:chrome_test_util::TurnTableViewSwitchOn(NO)];
 }
 
@@ -373,16 +372,19 @@ using chrome_test_util::SettingsDoneButton;
 // Tests that disabling MSBB UKM consent disables and purges DWA.
 // Additionally tests that DWA is disabled until all UKM consents are enabled.
 // LINT.IfChange(UkmMsbbConsentChangeCheck)
-- (void)FLAKY_testUkmMsbbConsentChangeCheck {
+- (void)testUkmMsbbConsentChangeCheck {
   // Note: Tests begin with an open regular tab. This tab is opened in setUp.
   [self recordTestDWAEntryMetricAndPageLoadEvent];
   [self assertDwaIsEnabledAndAllowed];
   [self assertDwaRecorderHasMetrics];
 
   [ChromeEarlGreyUI openSettingsMenu];
+  [ChromeEarlGrey waitForSufficientlyVisibleElementWithMatcher:
+                      GoogleServicesSettingsButton()];
 
   // Open Google services settings.
   [ChromeEarlGreyUI tapSettingsMenuButton:GoogleServicesSettingsButton()];
+  [ChromeEarlGreyUI waitForAppToIdle];
 
   // Toggle "Make searches and browsing better" switch off.
   [self turnOffMsbbSwitch];
