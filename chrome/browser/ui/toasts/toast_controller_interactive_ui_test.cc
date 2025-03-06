3d1aa86a485131d31e201e74b97d1db78edebe0f
Srinivas Sista <srinivassista@chromium.org>
2025-03-06 10:54:48 -0800

    Revert "Fix context menu not dismissing when window moves with Win+Shift+Arrow"
    This reverts commit 14045a2f04d9836ea9bd39477ffcdd96f374747b.
    
    Reason for revert: Causing crashes and RBD crbug/401116319
    Original change's description:
    > Fix context menu not dismissing when window moves with Win+Shift+Arrow
    >
    > The existing Destroying and ShowStateChanged callbacks, that
    > MenuController uses to cancel currently showing menus, don't handle the
    > case where the user moves the browser window using the keyboard shortcut
    > Win+Shift+Arrow. Adding a callback for BoundsChanged to cancel current
    > menus will handle the scenario where the user moves the browser window
    > with the keyboard.
    >
    > Bug: 394634481
    > Change-Id: I8fcceb209dbd376eda8a683525dcfb891d364a40
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244522
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Brad Peters <brpeters@microsoft.com>
    > Reviewed-by: Alison Gale <agale@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1427428}
    
    Bug: 394634481
    Change-Id: I61579a8c3758de60125d2b84b571445f9a015713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334006
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429046}

--- chrome/browser/ui/toasts/toast_controller_interactive_ui_test.cc
+++ chrome/browser/ui/toasts/toast_controller_interactive_ui_test.cc
@@ -62,8 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kFirstTab);
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSecondTab);
-DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ui::test::PollingStateObserver<bool>,
-                                    kToastAnimation);
 
 class OmniboxInputWaiter : public OmniboxTabHelper::Observer {
  public:
@@ -150,20 +148,11 @@ class ToastControllerInteractiveTest : public InteractiveBrowserTest {
   }
 
   auto ShowToast(ToastParams params) {
-    return Steps(
-        Do(base::BindOnce(
-            [](ToastController* toast_controller, ToastParams toast_params) {
-              toast_controller->MaybeShowToast(std::move(toast_params));
-            },
-            GetToastController(), std::move(params))),
-        PollState(kToastAnimation,
-                  [this]() {
-                    toasts::ToastView* toast_view =
-                        GetToastController()->GetToastViewForTesting();
-                    return toast_view && toast_view->is_animating_for_testing();
-                  }),
-        WaitForState(kToastAnimation, false),
-        StopObservingState(kToastAnimation));
+    return Do(base::BindOnce(
+        [](ToastController* toast_controller, ToastParams toast_params) {
+          toast_controller->MaybeShowToast(std::move(toast_params));
+        },
+        GetToastController(), std::move(params)));
   }
 
   auto FireToastCloseTimer() {
