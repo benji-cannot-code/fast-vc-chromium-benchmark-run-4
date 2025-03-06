066525084f860eb6355e6aac75c0d5b4678bbe53
Weizhong Xia <weizhong@google.com>
2025-03-05 17:09:10 -0800

    [Importer] Really upload local changes when parsing expectations failed
    This is a follow up change of crrev.com/c/6270396. We need commit local
    change first before uploading.
    
    Bug: None
    Change-Id: Icc1dcd205cd57cbf59049c46732f6505b068841a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330199
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428661}

--- third_party/blink/tools/blinkpy/w3c/test_importer.py
+++ third_party/blink/tools/blinkpy/w3c/test_importer.py
@@ -276,6 +276,9 @@ class TestImporter:
                     self.project_git.add_list([path])
 
                 self._generate_manifest()
+            except ParseError as e:
+                raise
+            finally:
                 message = 'Update test expectations and baselines.'
                 if self.project_git.has_working_directory_changes():
                     self._commit_changes(message)
@@ -283,11 +286,6 @@ class TestImporter:
                 # `TestExpectations`, which are committed earlier (before
                 # rebaselining).
                 self._upload_patchset(message)
-            except ParseError as e:
-                # When there is an error when parse TestExpectations, upload
-                # the updated TestExpectations for easier debugging later.
-                self._upload_patchset('Dump invalid expectations')
-                raise
 
         return True
 
