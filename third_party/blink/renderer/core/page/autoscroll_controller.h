3015844dd97d605ca6fdb81cef3cdbeac7e92304
Xiaobin Cui <xiaobincui@microsoft.com>
2025-03-05 18:46:10 -0800

    [ios blink] Implement autoscroll for selection
    Should start autoscroll when users drag selection handle to edge of
    viewport/scrollable container.
    1. Add new method StartAutoscrollForSelectionToPoint to
       AutoscrollController to scroll to point for selection.
    2. Expose StartAutoscrollForSelectionToPoint and StopAutoscroll
       to Browser process
    3. Call corresponding methods in autoscrollToPoint and cancelAutoscroll
    
    Note: when users drag the right handle of selection, the point passed to
    autoscrollToPoint is not the point that users touch. The Y value of the
    point passed is smaller than the Y value that users touch. So, we will
    use a greater gap for the bottom of belt box.
    
    Bug: 40254930
    Change-Id: I91af9cd319535a2a9683d2afd89597389b40e979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6267941
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Xiaobin Cui <xiaobincui@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1428693}

--- third_party/blink/renderer/core/page/autoscroll_controller.h
+++ third_party/blink/renderer/core/page/autoscroll_controller.h
@@ -49,6 +49,9 @@ enum AutoscrollType {
   kAutoscrollForDragAndDrop,
   kAutoscrollForSelection,
   kAutoscrollForMiddleClick,
+#if BUILDFLAG(IS_IOS)
+  kAutoscrollForSelectionToPoint,
+#endif  // BUILDFLAG(IS_IOS)
 };
 
 enum MiddleClickMode {
@@ -84,6 +87,11 @@ class CORE_EXPORT AutoscrollController final
                          const gfx::PointF& event_position,
                          base::TimeTicks event_time);
 
+#if BUILDFLAG(IS_IOS)
+  void StartAutoscrollForSelectionToPoint(LayoutObject* layout_object,
+                                          const gfx::PointF& point_in_viewport);
+#endif  // BUILDFLAG(IS_IOS)
+
   // Middle-click autoscroll.
   void StartMiddleClickAutoscroll(LocalFrame*,
                                   LayoutBox* scrollable,
@@ -117,6 +125,10 @@ class CORE_EXPORT AutoscrollController final
 
   PhysicalOffset drag_and_drop_autoscroll_reference_position_;
   base::TimeTicks drag_and_drop_autoscroll_start_time_;
+
+#if BUILDFLAG(IS_IOS)
+  PhysicalOffset autoscroll_to_point_reference_position_;
+#endif  // BUILDFLAG(IS_IOS)
 
   // Middle-click autoscroll.
   Member<LayoutBox> horizontal_autoscroll_layout_box_ = nullptr;
