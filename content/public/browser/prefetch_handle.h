a31946427fcb8f673bdb40ee9faec2a560d419ef
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-03-06 14:00:41 -0800

    Make PrefetchHandle move-only
    Bug: 390329781
    Change-Id: I644f45f25c12c96a55ba17fe5bda49e78634d333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6325473
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429161}

--- content/public/browser/prefetch_handle.h
+++ content/public/browser/prefetch_handle.h
@@ -16,6 +16,11 @@ class PrefetchHandle {
  public:
   PrefetchHandle() = default;
   virtual ~PrefetchHandle() = default;
+
+  PrefetchHandle(const PrefetchHandle& other) = delete;
+  PrefetchHandle& operator=(const PrefetchHandle& other) = delete;
+  PrefetchHandle(PrefetchHandle&& other) = default;
+  PrefetchHandle& operator=(PrefetchHandle&& other) = default;
 };
 
 }  // namespace content
