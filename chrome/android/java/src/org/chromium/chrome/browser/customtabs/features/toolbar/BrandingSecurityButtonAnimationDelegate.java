ab4b42fe8496d7b17bdb80f74dd6e5d1241c9f6a
Sinan Sahin <sinansahin@google.com>
2025-03-06 15:13:23 -0800

    [eCCT] Prevent Resources$NotFoundException after branding
    This is a speculative fix.
    
    Bug: 399058738
    Change-Id: Ie03483cb4a07d63256953388b52cf751ebf2d259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6319673
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429206}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/BrandingSecurityButtonAnimationDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/BrandingSecurityButtonAnimationDelegate.java
@@ -67,7 +67,9 @@ class BrandingSecurityButtonAnimationDelegate {
         if (mCurrentDrawableResource == newResourceId) return;
         mCurrentDrawableResource = newResourceId;
 
-        if (mImageView.getVisibility() == View.VISIBLE && mImageView.getDrawable() != null) {
+        if (newResourceId != 0
+                && mImageView.getVisibility() == View.VISIBLE
+                && mImageView.getDrawable() != null) {
             updateWithTransitionalDrawable(newResourceId);
         } else {
             mImageView.setImageResource(newResourceId);
