8aceabba21cc3bbcdd39ec0ab06bcaa9f06dc21a
Riley Tatum <rtatum@google.com>
2025-03-06 09:22:31 -0800

    [NTP] Add default module order
    Adds a default module order to the method for retreiving what order
    modules should be in after any changes via Finch or Drag&Drop.
    
    Bug: 398016633
    Change-Id: I4c2b53d4c3bfa9226e153df632b215341af33905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6324471
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428983}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search/ntp_features.h"
 
 #include <string>
+#include <vector>
 
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
