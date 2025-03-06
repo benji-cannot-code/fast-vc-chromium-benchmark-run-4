bb16ade56c065d9f208610385a4f566a4dc5d0c2
Greg Thompson <grt@chromium.org>
2025-03-06 11:13:27 -0800

    Fix bugs in update_wpr
    - Support $EDITOR with more than one arg.
    - Specify encoding when working with files in _FilterLogForDiff.
    - Return from _FilterLogForDiff after the last line is handled.
    
    Change-Id: Ieb8581f1a7bef041d9f37aefcfe08eca40279aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311001
    Reviewed-by: John Chen <johnchen@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429059}

--- tools/perf/cli_tools/update_wpr/update_wpr.py
+++ tools/perf/cli_tools/update_wpr/update_wpr.py
@@ -19,6 +19,7 @@ import shutil
 import subprocess
 import tempfile
 import time
+import shlex
 import sys
 import webbrowser
 
@@ -94,7 +95,7 @@ def _EnsureEditor():
 
 
 def _OpenEditor(filepath):
-  subprocess.check_call([os.environ['EDITOR'], filepath])
+  subprocess.check_call(shlex.split(os.environ['EDITOR']) + [filepath])
 
 
 def _PrepareEnv():
@@ -378,8 +379,13 @@ class WprUpdater(object):
     Returns:
       Path to the filtered log.
     """
-    with open(log_filename) as src, tempfile.NamedTemporaryFile(
-        suffix='diff', dir=self.output_dir, delete=False) as dest:
+    with open(log_filename,
+              encoding='utf-8') as src, tempfile.NamedTemporaryFile(
+                  encoding='utf-8',
+                  mode='w+',
+                  suffix='diff',
+                  dir=self.output_dir,
+                  delete=False) as dest:
       for line in src:
         # Remove timestamps.
         line = re.sub(
@@ -394,7 +400,7 @@ class WprUpdater(object):
         # Remove random durations in ms.
         line = re.sub(r'\d+ ms', r'<duration>', line)
         dest.write(line)
-        return dest.name
+      return dest.name
 
   def _GetTargetFromConfiguration(self, configuration):
     """Returns the target that should be used for a Pinpoint job."""
