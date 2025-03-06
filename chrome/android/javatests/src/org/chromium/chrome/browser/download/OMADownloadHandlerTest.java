1a88130fc2bda2f18028d2c2cdce69394d940828
Henrique Nakashima <hnakashima@chromium.org>
2025-03-06 08:37:32 -0800

    [Android] Convert android.emulator_10.chrome_public_test_apk.filter
    Mostly replace with @DisableIfs or adjust existing ones.
    
    Cq-Include-Trybots: luci.chromium.try:android-10-x86-rel
    Bug: 400465162
    Change-Id: Ifa1706feb68103e14549aebf12bc5a48a23b8dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331216
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428955}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/OMADownloadHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/OMADownloadHandlerTest.java
@@ -313,7 +313,7 @@ public class OMADownloadHandlerTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.Q) // https://crbug.com/338971643
+    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.P) // https://crbug.com/338971643
     @Feature({"Download"})
     public void testQueryDownloadResult() {
         Context context = getTestContext();
@@ -347,7 +347,7 @@ public class OMADownloadHandlerTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.Q) // https://crbug.com/338971643
+    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.P) // https://crbug.com/338971643
     @Feature({"Download"})
     public void testClearPendingOMADownloads() {
         Context context = getTestContext();
