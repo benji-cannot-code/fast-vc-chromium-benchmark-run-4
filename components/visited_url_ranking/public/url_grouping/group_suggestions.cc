1da3840482d27833733e842bbef3c6d89e648842
ssid <ssid@chromium.org>
2025-03-06 11:48:52 -0800

    [tab suggestions] API for suggestions service
    Bug: 397221723
    
    Change-Id: I7e7e30ad5c2210c0cc09aadfb57a5b6c2c857263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329756
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429080}

---
+++ components/visited_url_ranking/public/url_grouping/group_suggestions.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/visited_url_ranking/public/url_grouping/group_suggestions.h"
+
+namespace visited_url_ranking {
+
+GroupSuggestion::GroupSuggestion() = default;
+GroupSuggestion::~GroupSuggestion() = default;
+
+GroupSuggestions::GroupSuggestions() = default;
+GroupSuggestions::~GroupSuggestions() = default;
+
+}  // namespace visited_url_ranking
