bf2eb122798e69a4818ca5359e9012a4af998d09
Osama Fathy <osamafathy@google.com>
2025-03-06 03:08:56 -0800

    [Gardening] Disable GlicWindowControllerMultipleDisplaysUiTest tests
    GlicWindowControllerMultipleDisplaysUiTest tests are flaky on Mac.
    
    Since the tests are targeted for Mac, there is no need to use `#ifdef`
    to disable it on Mac only.
    
    Bug: 399703468
    Change-Id: I075b9240b8518b9ced68f89728d549c284908000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329364
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Bohdan Tyshchenko <bohdanty@google.com>
    Owners-Override: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428812}

--- chrome/browser/glic/glic_window_controller_interactive_uitest.cc
+++ chrome/browser/glic/glic_window_controller_interactive_uitest.cc
@@ -476,8 +476,9 @@ class GlicWindowControllerMultipleDisplaysUiTest
   display::Display secondary_display_;
 };
 
+// TODO(crbug.com/399703468): Flaky on Mac. Test is targeted for Mac only.
 IN_PROC_BROWSER_TEST_F(GlicWindowControllerMultipleDisplaysUiTest,
-                       MoveDetachedGlicWindowToSecondDisplay) {
+                       DISABLED_MoveDetachedGlicWindowToSecondDisplay) {
   if (!SetUpVirtualDisplays()) {
     return;
   }
@@ -491,8 +492,10 @@ IN_PROC_BROWSER_TEST_F(GlicWindowControllerMultipleDisplaysUiTest,
                   CloseGlicWindow(), CheckControllerHasWidget(false));
 }
 
-IN_PROC_BROWSER_TEST_F(GlicWindowControllerMultipleDisplaysUiTest,
-                       DetachAttachedGlicWindowAndMoveToSecondDisplay) {
+// TODO(crbug.com/399703468): Flaky on Mac. Test is targeted for Mac only.
+IN_PROC_BROWSER_TEST_F(
+    GlicWindowControllerMultipleDisplaysUiTest,
+    DISABLED_DetachAttachedGlicWindowAndMoveToSecondDisplay) {
   if (!SetUpVirtualDisplays()) {
     return;
   }
