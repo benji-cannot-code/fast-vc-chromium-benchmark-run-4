4e94fe8575d4046d3c585d6c0d2beb010e2f92f5
Georg Neis <neis@chromium.org>
2025-03-06 01:05:17 -0800

    ash: Remove ScreenAIDownloader crosapi
    Lacros is gone, so this interface and its implementation is obsolete.
    Remove it.
    
    Bug: b:365741912, b:365902693
    Change-Id: I3f96fce3259fc77a64abc410689651fc1749a134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331853
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428785}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/payment_app_instance_ash.h"
 #include "chrome/browser/ash/crosapi/policy_service_ash.h"
 #include "chrome/browser/ash/crosapi/remoting_ash.h"
-#include "chrome/browser/ash/crosapi/screen_ai_downloader_ash.h"
 #include "chrome/browser/ash/crosapi/structured_metrics_service_ash.h"
 #include "chrome/browser/ash/crosapi/vpn_service_ash.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_factory.h"
@@ -202,7 +201,6 @@ CrosapiAsh::CrosapiAsh()
       remoting_ash_(std::make_unique<RemotingAsh>()),
       print_preview_webcontents_adapter_ash_(
           std::make_unique<ash::printing::PrintPreviewWebcontentsAdapterAsh>()),
-      screen_ai_downloader_ash_(std::make_unique<ScreenAIDownloaderAsh>()),
       structured_metrics_service_ash_(
           std::make_unique<StructuredMetricsServiceAsh>()),
       video_conference_manager_ash_(
@@ -552,11 +550,6 @@ void CrosapiAsh::BindRemoteAppsLacrosBridge(
 
 void CrosapiAsh::BindRemoting(mojo::PendingReceiver<mojom::Remoting> receiver) {
   remoting_ash_->BindReceiver(std::move(receiver));
-}
-
-void CrosapiAsh::BindScreenAIDownloader(
-    mojo::PendingReceiver<mojom::ScreenAIDownloader> receiver) {
-  screen_ai_downloader_ash_->Bind(std::move(receiver));
 }
 
 void CrosapiAsh::BindSensorHalClient(
--- chrome/browser/ash/crosapi/crosapi_ash.h
+++ chrome/browser/ash/crosapi/crosapi_ash.h
@@ -88,7 +88,6 @@ class NonclosableAppToastServiceAsh;
 class PrintingMetricsAsh;
 #endif  // BUILDFLAG(USE_CUPS)
 class RemotingAsh;
-class ScreenAIDownloaderAsh;
 class StructuredMetricsServiceAsh;
 class VpnServiceAsh;
 
@@ -231,8 +230,6 @@ class CrosapiAsh : public mojom::Crosapi {
           chromeos::remote_apps::mojom::RemoteAppsLacrosBridge> receiver)
       override;
   void BindRemoting(mojo::PendingReceiver<mojom::Remoting> receiver) override;
-  void BindScreenAIDownloader(
-      mojo::PendingReceiver<mojom::ScreenAIDownloader> receiver) override;
   void BindSensorHalClient(
       mojo::PendingRemote<chromeos::sensors::mojom::SensorHalClient> remote)
       override;
@@ -362,10 +359,6 @@ class CrosapiAsh : public mojom::Crosapi {
 
   ash::ProbeServiceAsh* probe_service_ash() { return probe_service_ash_.get(); }
 
-  ScreenAIDownloaderAsh* screen_ai_downloader_ash() {
-    return screen_ai_downloader_ash_.get();
-  }
-
   StructuredMetricsServiceAsh* structured_metrics_service_ash() {
     return structured_metrics_service_ash_.get();
   }
@@ -438,7 +431,6 @@ class CrosapiAsh : public mojom::Crosapi {
   std::unique_ptr<RemotingAsh> remoting_ash_;
   std::unique_ptr<ash::printing::PrintPreviewWebcontentsAdapterAsh>
       print_preview_webcontents_adapter_ash_;
-  std::unique_ptr<ScreenAIDownloaderAsh> screen_ai_downloader_ash_;
   std::unique_ptr<StructuredMetricsServiceAsh> structured_metrics_service_ash_;
   std::unique_ptr<ash::VideoConferenceManagerAsh> video_conference_manager_ash_;
   std::unique_ptr<VpnServiceAsh> vpn_service_ash_;
