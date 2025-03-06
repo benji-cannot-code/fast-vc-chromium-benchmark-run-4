6c1732bab17f05cf85f548a8296dbfa773d2c768
Osama Fathy <osamafathy@google.com>
2025-03-06 06:00:47 -0800

    [Gardening] Disable ImageWriterPrivateApiTest.WriteFromUrl* - ASAN LSAN
    Bug: 401198566
    Change-Id: I851c37dae2f880fea4d7a427643f4a0e6dca1b79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330725
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Owners-Override: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428867}

--- chrome/browser/extensions/api/image_writer_private/image_writer_private_api_chromeos_unittest.cc
+++ chrome/browser/extensions/api/image_writer_private/image_writer_private_api_chromeos_unittest.cc
@@ -176,7 +176,13 @@ TEST_F(ImageWriterPrivateApiTest, WriteFromFile_Allowlist) {
   EXPECT_EQ(app_file_handler_util::kInvalidParameters, result);
 }
 
-TEST_F(ImageWriterPrivateApiTest, WriteFromUrl) {
+// TODO(crbug.com/401198566): Flaky test on ASan + LSan bots.
+#if defined(ADDRESS_SANITIZER) && defined(LEAK_SANITIZER)
+#define MAYBE_WriteFromUrl DISABLED_WriteFromUrl
+#else
+#define MAYBE_WriteFromUrl WriteFromUrl
+#endif
+TEST_F(ImageWriterPrivateApiTest, MAYBE_WriteFromUrl) {
   auto result = RunWriteFromUrl(kDevicePath1, kImageUrl);
   EXPECT_TRUE(result.has_value());
 }
@@ -195,7 +201,13 @@ TEST_F(ImageWriterPrivateApiTest, WriteFromUrl_ReadOnly) {
   EXPECT_EQ(image_writer::error::kDeviceWriteError, result.error());
 }
 
-TEST_F(ImageWriterPrivateApiTest, WriteFromUrl_Allowlist) {
+// TODO(crbug.com/401198566): Flaky test on ASan + LSan bots.
+#if defined(ADDRESS_SANITIZER) && defined(LEAK_SANITIZER)
+#define MAYBE_WriteFromUrl_Allowlist DISABLED_WriteFromUrl_Allowlist
+#else
+#define MAYBE_WriteFromUrl_Allowlist WriteFromUrl_Allowlist
+#endif
+TEST_F(ImageWriterPrivateApiTest, MAYBE_WriteFromUrl_Allowlist) {
   SetDisabled(true);
   SetAllowlist({kVendorId1, kProductId1});
   auto result = RunWriteFromUrl(kDevicePath1, kImageUrl);
