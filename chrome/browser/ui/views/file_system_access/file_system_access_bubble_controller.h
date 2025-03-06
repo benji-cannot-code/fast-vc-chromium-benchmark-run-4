db037971c73744ffbf8fdc3e4a8968036165ea25
Omar Flores <omarflores@google.com>
2025-03-06 07:50:23 -0800

    Add file system access bubble controller
    Bug: 376282751
    Change-Id: I64f7ebf15002df3924b291e25ae36736882cc0a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239848
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Omar Flores <omarflores@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428922}

---
+++ chrome/browser/ui/views/file_system_access/file_system_access_bubble_controller.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_FILE_SYSTEM_ACCESS_FILE_SYSTEM_ACCESS_BUBBLE_CONTROLLER_H_
+#define CHROME_BROWSER_UI_VIEWS_FILE_SYSTEM_ACCESS_FILE_SYSTEM_ACCESS_BUBBLE_CONTROLLER_H_
+
+class Browser;
+
+/** FileSystemAccessBubbleController class hosting helper methods to assist with
+ * the page action such as the logic for showing the action bubble. */
+
+// TODO(crbug.com/376282751) Implement the rest of the page action controller
+// class
+class FileSystemAccessBubbleController {
+ public:
+  // Triggers the file system access bubble to be shown for the provided
+  // browser's active web contents.
+  static void Show(Browser* browser);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_FILE_SYSTEM_ACCESS_FILE_SYSTEM_ACCESS_BUBBLE_CONTROLLER_H_
