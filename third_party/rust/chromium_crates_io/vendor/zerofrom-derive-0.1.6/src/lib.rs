8da3cb763c6671ebb1327a8ba53f392bfbfb3547
Daniel Cheng <dcheng@chromium.org>
2025-03-06 14:38:18 -0800

    Roll zerofrom-derive: 0.1.5 => 0.1.6 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * zerofrom-derive: 0.1.5 => 0.1.6
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * zerofrom-derive@0.1.6: crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: I187d427229f970187eb563768c7fafc42de4751d
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322855
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429179}

--- third_party/rust/chromium_crates_io/vendor/zerofrom-derive-0.1.6/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/zerofrom-derive-0.1.6/src/lib.rs
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         clippy::panic,
         clippy::exhaustive_structs,
         clippy::exhaustive_enums,
+        clippy::trivially_copy_pass_by_ref,
         missing_debug_implementations,
     )
 )]
