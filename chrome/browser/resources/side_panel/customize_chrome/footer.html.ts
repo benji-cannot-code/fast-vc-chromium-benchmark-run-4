82667e4447cf75e98f726b842ca2ae7546ca8f92
Teresa Mao <temao@chromium.org>
2025-03-06 10:19:39 -0800

    [ntp footer] Add Footer UI.
    screenshots:
    light - screen/A4LE4XPxmvSSyz8
    dark - screen/3K5Y2msVkT5AD3c
    
    Bug: 399179081
    Change-Id: I88a7e091c58cef61baf772d0989a35e1fdace4a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6321412
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429018}

---
+++ chrome/browser/resources/side_panel/customize_chrome/footer.html.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {FooterElement} from './footer.js';
+
+export function getHtml(this: FooterElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div id="showToggleContainer" class="sp-card-content"
+    @click="${this.onShowToggleClick_}">
+  <div id="showTitle">$i18n{showFooterToggleTitle}</div>
+  <cr-toggle id="showToggle" title="$i18n{showFooterToggleTitle}"
+      ?checked="${this.show_}" @change="${this.onShowToggleChange_}">
+  </cr-toggle>
+</div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
