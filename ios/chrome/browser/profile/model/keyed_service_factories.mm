c71e55ae623e1f8d87a04c5aee721a60f814a3f2
Xu Li <mxlg@google.com>
2025-03-06 09:43:52 -0800

    [iOS] Register IOSRealtimeReportingClientFactory with keyedServiceFactories.
    Bug: 394097677
    
    Change-Id: I6283300d1dc39948da631688d27c04281542164d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329682
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428996}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/download/model/browser_download_service_factory.h"
 #import "ios/chrome/browser/drive/model/drive_service_factory.h"
 #import "ios/chrome/browser/enterprise/connectors/connectors_service_factory.h"
+#import "ios/chrome/browser/enterprise/connectors/reporting/ios_realtime_reporting_client_factory.h"
 #import "ios/chrome/browser/enterprise/model/idle/idle_service_factory.h"
 #import "ios/chrome/browser/external_files/model/external_file_remover_factory.h"
 #import "ios/chrome/browser/favicon/model/favicon_service_factory.h"
@@ -198,6 +199,7 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   dom_distiller::DomDistillerServiceFactory::GetInstance();
   drive::DriveServiceFactory::GetInstance();
   enterprise_connectors::ConnectorsServiceFactory::GetInstance();
+  enterprise_connectors::IOSRealtimeReportingClientFactory::GetInstance();
   enterprise_idle::IdleServiceFactory::GetInstance();
   feature_engagement::TrackerFactory::GetInstance();
   ios::AboutSigninInternalsFactory::GetInstance();
