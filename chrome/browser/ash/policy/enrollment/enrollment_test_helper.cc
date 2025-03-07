881a513c95ea9cef4ca15f646156f18e7047e61f
Roland Bock <rbock@google.com>
2025-03-07 02:36:45 -0800

    [enrollment] Remove legacy code from AutoEnrollmentTypeChecker
    Unified State Determination (USD) fully replaces legacy state
    determination since M131.
    
    As a result, enrollment state determination is to be expected during
    OOBE irrespective of FWMP or VPD. It will run unless disabled by command
    line switch.
    
    This CL therefore
    
     - removes legacy code for determining the enrollment type
     - migrates the TPM firmware update code from using the obsolete
       AutoEnrollmentTypeChecker::GetFRERequirementAccordingToVPD() to
       policy::AutoEnrollmentTypeChecker::IsEnabled().
    
    Cq-Include-Trybots: luci.chromium.try:linux-chromeos-rel
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: 383047722
    Change-Id: Id821ed90f32e5d75ce5b48bcea73b716470f737a
    Tests: Remaining, new, and refactored unit tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330676
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429412}

--- chrome/browser/ash/policy/enrollment/enrollment_test_helper.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_test_helper.cc
@@ -54,7 +54,7 @@ void EnrollmentTestHelper::SetUpEnrollmentTokenConfig(
 void EnrollmentTestHelper::DisableFREOnFlex() {
   command_line_->GetProcessCommandLine()->AppendSwitchASCII(
       ash::switches::kEnterpriseEnableForcedReEnrollmentOnFlex,
-      AutoEnrollmentTypeChecker::kForcedReEnrollmentNever);
+      AutoEnrollmentTypeChecker::kFlexForcedReEnrollmentNever);
 }
 
 void EnrollmentTestHelper::EnableFREOnFlex() {
