f1d8a8fef15a23c945fc4d245bc4834c61659cdc
Weizhong Xia <weizhong@google.com>
2025-03-07 11:18:01 -0800

    Import wpt@a6db779a18dc4c8c7877928d01a5150ae9ec5501
    https://github.com/web-platform-tests/wpt/compare/a33cc7c5e...a6db779a1
    
    Using wpt-import in Chromium 574e1b216da7732553e45d014f731e883b977e3f.
    With Chromium commits locally applied on WPT:
      bc43ccf673 "WebNN: Enable in SharedWorker and ServiceWorker contexts"
      bf7905fb34 "Invalidate 'fill'/'stroke' color expressions on [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    foolip@chromium.org:
      external/wpt/fullscreen
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    kojii@chromium.org:
      external/wpt/css/css-writing-modes
    mkwst@chromium.org:
      external/wpt/subresource-integrity
    msisov@igalia.com:
      external/wpt/webvtt
    vogelheim@chromium.org:
      external/wpt/trusted-types
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Id4c8e2717219a922ed31f19e32958df4f7925363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332337
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429640}

---
+++ third_party/blink/web_tests/external/wpt/trusted-types/support/trusted-types-reporting-for-setTimeout-setInterval-worker.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const testSetupPolicy = trustedTypes.createPolicy("p", { createScriptURL: s => s });
+
+importScripts(testSetupPolicy.createScriptURL("/resources/testharness.js"));
+importScripts(testSetupPolicy.createScriptURL("helper.sub.js"));
+importScripts(testSetupPolicy.createScriptURL("csp-violations.js"));
+
+importScripts(testSetupPolicy.createScriptURL(
+    "trusted-types-reporting-for-DOMWindowTimers-setTimeout-setInterval.js"
+));
+
+done();
