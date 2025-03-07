78d16e7242aaa00cd75469f0220a176de8e947f9
Radu Nitescu <radunitescu@google.com>
2025-03-07 12:15:58 -0800

    [objc style][LO] Fix result presenter delegate pattern conformance
    Methods in a delegate should begin with the class name of the object
    doing the delegating and methods must pass the delegating object as the
    first argument.
    
    Bug: 400916185
    Change-Id: Ia1eb5badafb3162e1f0276ecadfb523735f99974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333359
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429667}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_results_page_presenter.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_results_page_presenter.mm
@@ -124,8 +124,8 @@ const CGFloat kVisibleAreaMediumDetentThreshold = 100.0f;
         constraintEqualToAnchor:_baseViewController.view.rightAnchor],
   ]];
 
-  [_delegate
-      onResultsPageVisibleAreaLayoutGuideAdjusted:_visibleAreaLayoutGuide];
+  [_delegate lensOverlayResultsPagePresenter:self
+             didAdjustVisibleAreaLayoutGuide:_visibleAreaLayoutGuide];
 }
 
 - (void)presentResultsPageAnimated:(BOOL)animated
@@ -314,7 +314,8 @@ const CGFloat kVisibleAreaMediumDetentThreshold = 100.0f;
     [_displayLink invalidate];
     [self sheetPresentationHeightChanged:0];
     [_windowPanTracker stopTracking];
-    [self.delegate onResultsPageWillInitiateGestureDrivenDismiss];
+    [self.delegate
+        lensOverlayResultsPagePresenterWillInitiateGestureDrivenDismiss:self];
   }
 }
 
@@ -334,8 +335,8 @@ const CGFloat kVisibleAreaMediumDetentThreshold = 100.0f;
   CGFloat estimatedMediumDetentHeight =
       _detentsManager.estimatedMediumDetentHeight;
   CGFloat offsetNeeded = estimatedMediumDetentHeight + kSelectionOffsetPadding;
-
-  [self.delegate onResultsPageVerticalOcclusionInsetsSettled:offsetNeeded];
+  [_delegate lensOverlayResultsPagePresenter:self
+               updateVerticalOcclusionOffset:offsetNeeded];
 }
 
 #pragma mark - UIPanGestureRecognizer handlers
@@ -395,8 +396,8 @@ const CGFloat kVisibleAreaMediumDetentThreshold = 100.0f;
 
 - (void)lensOverlayDetentsManagerDidChangeDimensionState:
     (LensOverlayDetentsManager*)detentsManager {
-  [self.delegate
-      onResultsPageDimensionStateChanged:detentsManager.sheetDimension];
+  [_delegate lensOverlayResultsPagePresenter:self
+                     didUpdateDimensionState:detentsManager.sheetDimension];
 }
 
 - (BOOL)lensOverlayDetentsManagerShouldDismissBottomSheet:
