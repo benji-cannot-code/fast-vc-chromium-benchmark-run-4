f6e708b79f156ff61691ae9083d9ec352fe81540
Jenna Himawan <jhimawan@google.com>
2025-03-07 11:21:47 -0800

    SelectableListToolbar: announce w/ a11y pane title
    Note that this CL adds an "exiting selection mode" announcement due to limitations of AccessibilityPaneTitle.
    
    Also note that this CL makes some changes which ensure that selection mode for 0 items is correctly suppoted (and does some simplification/cleanup here as well). This is used for the tab selection screen.
    
    Additionally, this CL removes a specificiation of importantForAccessibility="no" on bookmark_toolbar.xml. It is not clear to me why this was set and the bookmark page seems to work fine when importantForAccessibility is allowed to default to true (see manual tests below).
    
    Manual tests:
    - History: [before](https://screencast.googleplex.com/cast/NTM5ODAxNTc1Njk5MjUxMnwyZWViYjM4ZC0yZA), [after](https://screencast.googleplex.com/cast/NjIzMzIzNTY5OTc5MzkyMHw4NTVlZmU5Yy0zNw)
    - Tab selection: [before](https://screencast.googleplex.com/cast/NjI3NjEyMzA2MjE3MzY5Nnw3ZGVlN2JjNy01NA), [after](https://screencast.googleplex.com/cast/NDc5NzcyMzIxMDQxNjEyOHxhZDJjNWRjYS00Mg)
    - Bookmarks: [before](https://screencast.googleplex.com/cast/NjQ3MzUyMzgxOTE4NDEyOHw3M2MzOTkyMi1kNw), [after](https://screencast.googleplex.com/cast/NDUyMTM1Nzc2ODM5MjcwNHwyNDE1ZjJiYS03Nw)
    
    Fixed: 376746283
    Test: BookmarkToolbarTest, TabListEditorMenuTest. Also manual, see above
    Change-Id: Iea4d1672776c036e46fa466b173cbf1375406b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6236707
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429642}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorMediator.java
@@ -231,7 +231,6 @@ class TabListEditorMediator
         mTabListCoordinator.prepareTabGridView();
         mVisibleTabs.clear();
         mVisibleTabs.addAll(tabs);
-        mSelectionDelegate.setSelectionModeEnabledForZeroItems(true);
 
         mResetHandler.resetWithListOfTabs(tabs, recyclerViewPosition, /* quickMode= */ false);
 
