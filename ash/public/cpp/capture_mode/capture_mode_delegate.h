304c0ad9a90ff522754ff65ef06d93ff0ab63287
Elijah Hewer <hewer@chromium.org>
2025-03-07 13:37:49 -0800

    Sunfish: Create resource request for Lens Web API
    Bug: 395939382
    Change-Id: Ic9e4ef4d57d9f702e002a42ef97bb3c89d67f1f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311048
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429722}

--- ash/public/cpp/capture_mode/capture_mode_delegate.h
+++ ash/public/cpp/capture_mode/capture_mode_delegate.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "chromeos/crosapi/mojom/video_conference.mojom-shared.h"
 #include "chromeos/crosapi/mojom/video_conference.mojom.h"
+#include "components/search_engines/template_url.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
@@ -258,6 +259,14 @@ class ASH_PUBLIC_EXPORT CaptureModeDelegate {
   virtual void GetPrimaryAccountAccessToken(
       base::RepeatingCallback<void(const std::string& access_token)>
           callback) = 0;
+
+  // Gets the POST request URL for a Lens Web API image search, and encodes the
+  // `image` data into `post_content` to be used as the body of the request.
+  // Other URL parameters may need to be appended to the returned GURL.
+  virtual GURL GetBaseSearchURLAndPostContent(
+      const gfx::Image& image,
+      gfx::Size image_original_size,
+      TemplateURLRef::PostContent* post_content) = 0;
 
   // Sends the captured `region` and `image` to the backend. Invokes `callback`
   // when the response is fetched.
