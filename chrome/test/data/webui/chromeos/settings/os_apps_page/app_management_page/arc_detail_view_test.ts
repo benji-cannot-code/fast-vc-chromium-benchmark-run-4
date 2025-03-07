e809bdb0b2b7981be5baeae1b0a73620542fe8e2
Md Shahadat Hossain Shahin <shahinmd@google.com>
2025-03-07 05:03:25 -0800

    Cleanup cros-privacy-hub-app-permissions-v2 flag
    The feature has been enabled by default for more than 3 months.
    
    Bug: 401234542
    Change-Id: Ibd98a3677c2f913f984801fd2472fe9310610484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330263
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429441}

--- chrome/test/data/webui/chromeos/settings/os_apps_page/app_management_page/arc_detail_view_test.ts
+++ chrome/test/data/webui/chromeos/settings/os_apps_page/app_management_page/arc_detail_view_test.ts
@@ -347,7 +347,6 @@ suite('<app-management-arc-detail-view>', () => {
 
           loadTimeData.overrideValues({
             'appManagementArcReadOnlyPermissions': true,
-            'privacyHubAppPermissionsV2Enabled': true,
             'privacyHubLocationAccessControlEnabled': true,
           });
 
@@ -377,7 +376,6 @@ suite('<app-management-arc-detail-view>', () => {
           arcPermissionView.remove();
           loadTimeData.overrideValues({
             'appManagementArcReadOnlyPermissions': false,
-            'privacyHubAppPermissionsV2Enabled': false,
             'privacyHubLocationAccessControlEnabled': false,
           });
         });
