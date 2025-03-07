8f1204bd651f666faf8ea52d346cc4c88b922aba
Haihan Chen <haihan@google.com>
2025-03-06 17:25:33 -0800

    [DLP Support in FSA] Use deep scanning metadata interface in DeepScanningRequest
    Functionality should not change with this CL.
    As a first step, use the deep scanning interface wrapper instead of item_ directly in deep_scanning_request.cc. deep_scanning_request will also now own the new metadata object.
    
    Moved PromptForPassword and GetEventResult methods into DownloadItemMetadata instead as they are download_item specific operations.
    
    Will have follow-up CL for `check_file_system_access_write_request.cc` to initiate deep scans with fsa item (finch-gated).
    
    Bug: 399845136
    Change-Id: Ifc97b2d391a80a5a986ae54048cf537bb5e802f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318556
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429267}

--- chrome/browser/safe_browsing/download_protection/download_protection_service.cc
+++ chrome/browser/safe_browsing/download_protection/download_protection_service.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager_factory.h"
 #include "chrome/browser/safe_browsing/download_protection/check_client_download_request.h"
 #include "chrome/browser/safe_browsing/download_protection/check_file_system_access_write_request.h"
+#include "chrome/browser/safe_browsing/download_protection/download_item_metadata.h"
 #include "chrome/browser/safe_browsing/download_protection/download_protection_util.h"
 #include "chrome/browser/safe_browsing/download_protection/download_request_maker.h"
 #include "chrome/browser/safe_browsing/download_protection/download_url_sb_client.h"
@@ -658,8 +659,9 @@ void DownloadProtectionService::UploadForDeepScanning(
     base::optional_ref<const std::string> password) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   auto request = std::make_unique<DeepScanningRequest>(
-      item, trigger, download_check_result, callback, this,
-      std::move(analysis_settings), password);
+      std::make_unique<DownloadItemMetadata>(item), trigger,
+      download_check_result, callback, this, std::move(analysis_settings),
+      password);
   DeepScanningRequest* request_raw = request.get();
   auto insertion_result = deep_scanning_requests_.insert(
       std::make_pair(request_raw, std::move(request)));
@@ -765,7 +767,8 @@ void DownloadProtectionService::UploadSavePackageForDeepScanning(
     enterprise_connectors::AnalysisSettings analysis_settings) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   auto request = std::make_unique<DeepScanningRequest>(
-      item, DownloadCheckResult::UNKNOWN, callback, this,
+      std::make_unique<DownloadItemMetadata>(item),
+      DownloadCheckResult::UNKNOWN, callback, this,
       std::move(analysis_settings), std::move(save_package_files));
   DeepScanningRequest* request_raw = request.get();
   auto insertion_result = deep_scanning_requests_.insert(
