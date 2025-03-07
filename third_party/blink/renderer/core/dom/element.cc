12be04a2973041a3cfbedf99d1f0acb014b32eb1
Alex Keng <shihken@microsoft.com>
2025-03-06 18:06:56 -0800

    [ReferenceTarget] Add a feature flag for aria-owns
    This CL adds a feature flag to disable aria-owns for now per the
    discussion in https://github.com/WICG/webcomponents/issues/1091 (and
    https://github.com/w3c/aria/issues/2266)
    
    Note in aria-owns.html, we verify that the ownership specified by
    aria-owns is not forwarded to the referenced element. This test would
    still pass even if the flag, ShadowRootReferenceTargetAriaOwns, is
    enabled. (There is currently a gap in our implementation).
    
    If we decide to support aria-owns in the future, we'll need to first
    address the gap then we can set the flag status to experimental and
    write more tests.
    
    Bug: 353750122
    Change-Id: I4ae0cac79f0276f75b00ebfb032e5fe1db994fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310056
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Alex Keng <shihken@microsoft.com>
    Reviewed-by: Alice Boxhall <alice@igalia.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429278}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -931,9 +931,11 @@ Element* Element::GetShadowReferenceTarget(const QualifiedName& name) const {
           GetExecutionContext())) {
     return nullptr;
   }
-
-  // TODO (crbug.com/353750122): Disallow aria-owns from participating in
-  // ReferenceTarget.
+  if (!RuntimeEnabledFeatures::ShadowRootReferenceTargetAriaOwnsEnabled(
+          GetExecutionContext()) &&
+      name == html_names::kAriaOwnsAttr) {
+    return nullptr;
+  }
 
   if (ShadowRoot* shadow_root = GetShadowRoot()) {
     if (Element* target = shadow_root->referenceTargetElement()) {
@@ -949,6 +951,11 @@ Element* Element::GetShadowReferenceTarget(const QualifiedName& name) const {
 Element* Element::GetShadowReferenceTargetOrSelf(const QualifiedName& name) {
   if (!RuntimeEnabledFeatures::ShadowRootReferenceTargetEnabled(
           GetExecutionContext())) {
+    return this;
+  }
+  if (!RuntimeEnabledFeatures::ShadowRootReferenceTargetAriaOwnsEnabled(
+          GetExecutionContext()) &&
+      name == html_names::kAriaOwnsAttr) {
     return this;
   }
   ShadowRoot* shadow_root = GetShadowRoot();
