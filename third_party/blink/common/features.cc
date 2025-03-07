3d7c2727994c643d1eb0c25c9d8770d18b0aef42
Hiroki Nakagawa <nhiroki@chromium.org>
2025-03-07 06:27:04 -0800

    Prerender: Move kPrerender2NoVarySearch flag from blink/ to content/
    This flag is now used only in content/. It's not necessary to keep it in
    blink/public.
    
    Bug: n/a
    Change-Id: Ib99e2746ab7cdae9ca74d87163d111c8929a911f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332277
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429464}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1985,10 +1985,6 @@ BASE_FEATURE(kPrerender2EarlyDocumentLifecycleUpdate,
              "Prerender2EarlyDocumentLifecycleUpdate",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kPrerender2NoVarySearch,
-             "Prerender2NoVarySearch",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kPrerender2WarmUpCompositor,
              "Prerender2WarmUpCompositor",
              base::FEATURE_ENABLED_BY_DEFAULT);
