b8cb2aa5afc98a318d678e7580cab8d2040c9585
Peilin Wang <peilinwang@google.com>
2025-03-07 07:48:36 -0800

    [BCIV] Fold BCIVZeroBrowserFrames into AndroidBrowserControlsInViz
    BCIVZeroBrowserFrames isn't needed anymore. This flag was added to see
    if submitting a frame when controls get scrolled offscreen has an impact
    on jank metrics. There is a noticeable improvement to jank when there
    are zero browser frames submitted.
    
    Now when users enable AndroidBrowserControlsInViz in chrome://flags,
    they will get the behavior with zero browser frames.
    
    Bug: 401006051
    Change-Id: I82a0b498f0cd6200bf3b875d48359058395df787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330757
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429502}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -126,7 +126,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &base::features::kCollectAndroidFrameTimelineMetrics,
     &download::features::kDownloadNotificationServiceUnifiedAPI,
     &features::kAndroidBcivBottomControls,
-    &features::kAndroidBcivZeroBrowserFrames,
     &features::kAndroidBrowserControlsInViz,
     &features::kGenericSensorExtraClasses,
     &features::kBackForwardCache,
--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -46,10 +46,6 @@ BASE_FEATURE(kAndroidBcivBottomControls,
              "AndroidBcivBottomControls",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAndroidBcivZeroBrowserFrames,
-             "AndroidBcivZeroBrowserFrames",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 #endif  // BUILDFLAG(IS_ANDROID)
 
 BASE_FEATURE(kBackdropFilterMirrorEdgeMode,
