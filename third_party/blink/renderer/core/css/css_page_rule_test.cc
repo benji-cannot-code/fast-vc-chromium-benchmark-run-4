e96c23db4d1305009dbff62207ac436f6239cf39
Morten Stenshorne <mstensho@chromium.org>
2025-03-07 00:50:28 -0800

    Enable PageMarginBoxes runtime feature forever.
    Remove the feature flag.
    
    Bug: 40341678
    Change-Id: I883d06941fe0c6e9a2dabdea66aed5615d55ce47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333416
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429376}

--- third_party/blink/renderer/core/css/css_page_rule_test.cc
+++ third_party/blink/renderer/core/css/css_page_rule_test.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/testing/null_execution_context.h"
 #include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
-#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 
 namespace blink {
@@ -68,7 +67,6 @@ TEST(CSSPageRule, selectorText) {
 }
 
 TEST(CSSPageRule, MarginRules) {
-  ScopedPageMarginBoxesForTest enabled(true);
   test::TaskEnvironment task_environment;
   css_test_helpers::TestStyleSheet sheet;
 
@@ -84,7 +82,6 @@ TEST(CSSPageRule, MarginRules) {
 }
 
 TEST(CSSPageRule, MarginRulesInvalidPrelude) {
-  ScopedPageMarginBoxesForTest enabled(true);
   test::TaskEnvironment task_environment;
   css_test_helpers::TestStyleSheet sheet;
 
@@ -95,24 +92,6 @@ TEST(CSSPageRule, MarginRulesInvalidPrelude) {
   EXPECT_EQ(1u, sheet.CssRules()->length());
   EXPECT_EQ("@page { size: auto; }", sheet.CssRules()->item(0)->cssText());
   EXPECT_EQ(CSSRule::kPageRule, sheet.CssRules()->item(0)->GetType());
-}
-
-TEST(CSSPageRule, MarginRulesIgnoredWhenDisabled) {
-  ScopedPageMarginBoxesForTest enabled(false);
-  test::TaskEnvironment task_environment;
-  css_test_helpers::TestStyleSheet sheet;
-
-  const char* css_rule =
-      "@page { size: auto; @top-right { content: \"fisk\"; margin-bottom: 1cm; "
-      "} margin-top: 2cm; }";
-  sheet.AddCSSRules(css_rule);
-  ASSERT_TRUE(sheet.CssRules());
-  EXPECT_EQ(1u, sheet.CssRules()->length());
-  EXPECT_EQ("@page { size: auto; margin-top: 2cm; }",
-            sheet.CssRules()->item(0)->cssText());
-  EXPECT_EQ(CSSRule::kPageRule, sheet.CssRules()->item(0)->GetType());
-  auto* page_rule = To<CSSPageRule>(sheet.CssRules()->item(0));
-  EXPECT_EQ("", page_rule->selectorText());
 }
 
 class CSSPageRuleTest : public PageTestBase {};
