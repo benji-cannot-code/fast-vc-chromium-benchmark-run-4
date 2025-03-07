5c1b3f26cd7e758d75dc82ef8abd00cc7d40373e
Ryan Tarpine <rtarpine@chromium.org>
2025-03-07 07:44:30 -0800

    btm: Emit BTM.ShortVisitNeighbor to help distinguish multiple visits.
    When the user hits the back button, the old UKM source id is used for
    the new visit. This makes it difficult to evaluate multiple visits to
    the same page. By adding a ShortVisitId metric and emitting events on
    the pages before and after with that same visit id, we should gain
    visibility.
    
    Remove singular="True" for BTM.ShortVisit in ukm.xml, because we
    sometimes want to emit another event when the user navigates back to the
    same page.
    
    Add some more details of special values to the TimeSinceLastInteraction
    metric summary.
    
    UKM collection review: go/btm-shortvisit-ukm
    
    Bug: 400503246
    Change-Id: I6c68a815ccdaa5197c24583684b13a307edc9c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329425
    Reviewed-by: Andrew Liu <liu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429499}

--- content/browser/btm/btm_short_visit_observer.cc
+++ content/browser/btm/btm_short_visit_observer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/overloaded.h"
 #include "base/memory/weak_ptr.h"
+#include "base/rand_util.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
 #include "content/browser/btm/btm_service_impl.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/metrics_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "url/origin.h"
 
@@ -261,6 +263,7 @@ void BtmShortVisitObserver::DidFinishNavigation(
         const bool next_site_same = visit_site == next_site;
 
         if (!prev_site_same || !next_site_same) {
+          const int32_t visit_id = static_cast<int32_t>(base::RandUint64());
           ukm::builders::BTM_ShortVisit event(page_source_id_);
           event.SetVisitDuration(visit_seconds)
               .SetExitWasRendererInitiated(
@@ -270,7 +273,8 @@ void BtmShortVisitObserver::DidFinishNavigation(
               // TODO: .SetSiteEngagement()
               .SetPreviousSiteSame(prev_site_same)
               .SetNextSiteSame(next_site_same)
-              .SetPreviousAndNextSiteSame(prev_site_ == next_site);
+              .SetPreviousAndNextSiteSame(prev_site_ == next_site)
+              .SetShortVisitId(visit_id);
           current_page_metrics_->SetUkmBuilder(std::move(event));
           if (current_page_metrics_->is_ready()) {
             current_page_metrics_->Emit();
@@ -280,11 +284,25 @@ void BtmShortVisitObserver::DidFinishNavigation(
             // interaction time. (StoreLastInteraction() will delete the state.)
             pending_metrics_.insert(std::move(current_page_metrics_));
           }
+
+          if (prev_source_id_ != ukm::kInvalidSourceId) {
+            ukm::builders::BTM_ShortVisitNeighbor(prev_source_id_)
+                .SetShortVisitId(visit_id)
+                .SetIsPreceding(true)
+                .Record(ukm::UkmRecorder::Get());
+          }
+
+          ukm::builders::BTM_ShortVisitNeighbor(
+              navigation_handle->GetNextPageUkmSourceId())
+              .SetShortVisitId(visit_id)
+              .SetIsPreceding(false)
+              .Record(ukm::UkmRecorder::Get());
         }
       }
     }
   }
 
+  prev_source_id_ = page_source_id_;
   page_source_id_ = navigation_handle->GetNextPageUkmSourceId();
   last_committed_at_ = clock_->Now();
   prev_site_ = visit_site;
