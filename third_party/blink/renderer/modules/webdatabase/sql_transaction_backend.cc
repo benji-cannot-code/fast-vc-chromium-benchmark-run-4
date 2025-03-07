9e52a889581c95f134f6175f766dedf1df6fd1f2
Tom Sepez <tsepez@chromium.org>
2025-03-06 16:21:13 -0800

    Remove file-wide unsafe_buffers pragma from blink/ [26 of N]
    Suppress warnings on an expression by expression basis.
    
    -- Two files appear already clean.
    
    Bug: 351564777
    Change-Id: I9c8a5a4911b9741282f4cef7166c630bcfe3ddcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332485
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429240}

--- third_party/blink/renderer/modules/webdatabase/sql_transaction_backend.cc
+++ third_party/blink/renderer/modules/webdatabase/sql_transaction_backend.cc
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/modules/webdatabase/sql_transaction_backend.h"
 
 #include <array>
