396314ddc6ab3b16d27ee9173ab9771f6b1f753f
Allen Bauer <kylixrd@chromium.org>
2025-03-07 14:26:04 -0800

    Use ChromeLayoutProvider for the label insets metric instead of literals.
    Change-Id: I0b684a3bc3bbd2e2300d822f0dd2b61503365e3c
    Bug: 363332740, 356443046, 356443046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334758
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429759}

--- chrome/browser/ui/views/autofill/payments/bnpl_dialog_footnote.cc
+++ chrome/browser/ui/views/autofill/payments/bnpl_dialog_footnote.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/autofill/payments/bnpl_dialog_footnote.h"
 
+#include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/views/controls/label.h"
@@ -15,12 +16,12 @@ namespace autofill::payments {
 
 BnplDialogFootnote::BnplDialogFootnote() {
   SetOrientation(views::BoxLayout::Orientation::kHorizontal);
-  SetInsideBorderInsets(gfx::Insets::TLBR(10, 10, 10, 10));
+  SetInsideBorderInsets(ChromeLayoutProvider::Get()->GetInsetsMetric(
+      views::INSETS_DIALOG_FOOTNOTE));
   AddChildView(
       views::Builder<views::Label>()
           // TODO(crbug.com/356443046): Move to resources and translate string.
           .SetText(u"To hide pay over time options, go to payment settings")
-          .SetProperty(views::kMarginsKey, gfx::Insets::TLBR(5, 10, 5, 0))
           .Build());
 }
 
