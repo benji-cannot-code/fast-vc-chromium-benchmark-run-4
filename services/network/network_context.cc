bd6619a2fd8fad34f05d80903f4038d0113416eb
Vlad Krot <vkrot@google.com>
2025-03-07 02:31:30 -0800

    [extensions] less strict CT in verifyTLSServerCertificate
    This fix is related to
    https://chromium-review.googlesource.com/c/chromium/src/+/6196859
    
    In the cl above I have renamed VerifyCertForSignedExchange -> VerifyCert
    thinking that the behavior has nothing specific to SignedExchange (SGX).
    
    As mattm@chromium.org pointed out SGX have stricter CT verification.
    Compare the following almost identical code:
    
    https://crsrc.org/c/net/socket/ssl_client_socket_impl.cc;drc=20799f4c32d950ce93d495f44eec648400f38a19;l=1175
    
    https://source.chromium.org/chromium/chromium/src/+/main:services/network/network_context.cc;drc=58fb75d86a0ad2642beec2d6c16b1e6c008e33cd;bpv=1;bpt=1;l=1732?gsn=CheckCTRequirements&gs=KYTHE%3A%2F%2Fkythe%3A%2F%2Fchromium.googlesource.com%2Fcodesearch%2Fchromium%2Fsrc%2F%2Fmain%3Flang%3Dc%252B%252B%3Fpath%3Dservices%2Fnetwork%2Fnetwork_context.cc%231BS5z2OmuEs-qsnFGg_csnxBM8OxwZDyDv74cEaGHm4
    
    They have difference that in SGX mode if CT verifier returns
    net::TransportSecurityState::CT_NOT_REQUIRED then failure might happen
    in certain conditions.
    
    For platformKeys.verifyTLSServerCertificate we don't need such strict
    CT, we would like to have it as usual WebSite https verification.
    
    Design doc is updated accordingly for history
    https://docs.google.com/document/d/1jiX_rQf352WWmtkteZFqNxjoNgVDXntVWSHsZlpp5Ts/edit?tab=t.0
    
    Low-Coverage-Reason: OTHER
    Bug: 399859278
    Change-Id: Ia5ae90fd4927d3f2a9f19c6aa879aa5a384e41c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6308831
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Vlad Krot <vkrot@google.com>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429410}

--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -1728,7 +1728,8 @@ void NetworkContext::SetCTPolicy(mojom::CTPolicyPtr ct_policy) {
 
 int NetworkContext::CheckCTRequirements(
     net::CertVerifyResult& cert_verify_result,
-    const net::HostPortPair& host_port_pair) {
+    const net::HostPortPair& host_port_pair,
+    CTVerificationMode ct_verification_mode) {
   net::X509Certificate* verified_cert = cert_verify_result.verified_cert.get();
 
   net::TransportSecurityState::CTRequirementsStatus ct_requirement_status =
@@ -1748,6 +1749,13 @@ int NetworkContext::CheckCTRequirements(
     case net::TransportSecurityState::CT_REQUIREMENTS_MET:
       return net::OK;
     case net::TransportSecurityState::CT_NOT_REQUIRED:
+      switch (ct_verification_mode) {
+        case CTVerificationMode::kTlsCertificate:
+          return net::OK;
+        case CTVerificationMode::kSignedExchange:
+          // Proceed.
+          break;
+      }
       // CT is not required if the certificate does not chain to a publicly
       // trusted root certificate.
       if (!cert_verify_result.is_issued_by_known_root) {
@@ -2032,11 +2040,12 @@ void NetworkContext::CreateHostResolver(
       url_request_context_->net_log()));
 }
 
-void NetworkContext::VerifyCert(
+void NetworkContext::VerifyCertInternal(
     const scoped_refptr<net::X509Certificate>& certificate,
     const net::HostPortPair& host_port,
     const std::string& ocsp_result,
     const std::string& sct_list,
+    CTVerificationMode ct_verification_mode,
     VerifyCertCallback callback) {
   uint64_t cert_verify_id = ++next_cert_verify_id_;
   CHECK_NE(0u, next_cert_verify_id_);  // The request ID should not wrap around.
@@ -2047,6 +2056,7 @@ void NetworkContext::VerifyCert(
   pending_cert_verify->host_port = host_port;
   pending_cert_verify->ocsp_result = ocsp_result;
   pending_cert_verify->sct_list = sct_list;
+  pending_cert_verify->ct_verification_mode = ct_verification_mode;
   net::CertVerifier* cert_verifier =
       g_cert_verifier_for_testing ? g_cert_verifier_for_testing
                                   : url_request_context_->cert_verifier();
@@ -2064,6 +2074,26 @@ void NetworkContext::VerifyCert(
   if (result != net::ERR_IO_PENDING) {
     OnVerifyCertComplete(cert_verify_id, result);
   }
+}
+
+void NetworkContext::VerifyCert(
+    const scoped_refptr<net::X509Certificate>& certificate,
+    const net::HostPortPair& host_port,
+    const std::string& ocsp_result,
+    const std::string& sct_list,
+    VerifyCertCallback callback) {
+  VerifyCertInternal(certificate, host_port, ocsp_result, sct_list,
+                     CTVerificationMode::kTlsCertificate, std::move(callback));
+}
+
+void NetworkContext::VerifyCertForSignedExchange(
+    const scoped_refptr<net::X509Certificate>& certificate,
+    const net::HostPortPair& host_port,
+    const std::string& ocsp_result,
+    const std::string& sct_list,
+    VerifyCertCallback callback) {
+  VerifyCertInternal(certificate, host_port, ocsp_result, sct_list,
+                     CTVerificationMode::kSignedExchange, std::move(callback));
 }
 
 void NetworkContext::NotifyExternalCacheHit(const GURL& url,
@@ -3146,8 +3176,9 @@ void NetworkContext::OnVerifyCertComplete(uint64_t cert_verify_id, int result) {
   bool pkp_bypassed = false;
   if (result == net::OK) {
 #if BUILDFLAG(IS_CT_SUPPORTED)
-    int ct_result = CheckCTRequirements(*pending_cert_verify->result,
-                                        pending_cert_verify->host_port);
+    int ct_result = CheckCTRequirements(
+        *pending_cert_verify->result, pending_cert_verify->host_port,
+        pending_cert_verify->ct_verification_mode);
 #endif  // BUILDFLAG(IS_CT_SUPPORTED)
     net::TransportSecurityState::PKPStatus pin_validity =
         url_request_context_->transport_security_state()->CheckPublicKeyPins(
