5c2ab6aa7c3f124f0104a3313b2198bbd3c2c560
Xiyuan Xia <xiyuan@chromium.org>
2025-03-07 12:09:18 -0800

    dbus: CHECK if a response is not sent after handling calls
    This CL wraps ExportedObject::SendResponse in a helper to ensure
    that it is invoked before destruction.
    
    Bug: 400758194
    Change-Id: I6515f164a0e2594e7313d6e7e0a5cdfe9fa2af94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6325811
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Michael Sun <michaelfsun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429663}

--- dbus/test_service.cc
+++ dbus/test_service.cc
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace dbus {
 
 // Echo, SlowEcho, AsyncEcho, BrokenMethod, GetAll, Get, Set, PerformAction,
-// GetManagedObjects
-const int TestService::kNumMethodsToExport = 9;
+// GetManagedObjects, NotSendingResponseCrash.
+constexpr int TestService::kNumMethodsToExport = 10;
 
 TestService::Options::Options()
     : request_ownership_options(Bus::REQUIRE_PRIMARY) {
@@ -256,6 +256,13 @@ void TestService::Run(base::RunLoop* run_loop) {
   exported_object_manager_->ExportMethod(
       kObjectManagerInterface, kObjectManagerGetManagedObjects,
       base::BindRepeating(&TestService::GetManagedObjects,
+                          base::Unretained(this)),
+      base::BindOnce(&TestService::OnExported, base::Unretained(this)));
+  ++num_methods;
+
+  exported_object_->ExportMethod(
+      "org.chromium.TestInterface", "NotSendingResponseCrash",
+      base::BindRepeating(&TestService::NotSendingResponseCrash,
                           base::Unretained(this)),
       base::BindOnce(&TestService::OnExported, base::Unretained(this)));
   ++num_methods;
@@ -491,7 +498,6 @@ void TestService::OwnershipReleased(
                                   std::move(response_sender)));
 }
 
-
 void TestService::OwnershipRegained(
     MethodCall* method_call,
     ExportedObject::ResponseSender response_sender,
@@ -499,6 +505,11 @@ void TestService::OwnershipRegained(
   PerformActionResponse(method_call, std::move(response_sender));
 }
 
+void TestService::NotSendingResponseCrash(
+    MethodCall* method_call,
+    dbus::ExportedObject::ResponseSender response_sender) {
+  // Not invoking `response_sender` and CHECK crash.
+}
 
 void TestService::GetManagedObjects(
     MethodCall* method_call,
