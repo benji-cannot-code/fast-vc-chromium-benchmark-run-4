eb6cb2595ecf71565128f1a48c53f6b050f19505
Jan Keitel <jkeitel@google.com>
2025-03-07 07:30:47 -0800

    Call AutofillAI model from BAM
    In addition, the CL adds some logging.
    
    Bug: 389631477
    Change-Id: I859e901004d6ed66d8c9932c269546dc9a8bce90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333543
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429494}

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -109,6 +109,10 @@ std::string ServerTypesToString(const AutofillField& field) {
   return base::StrCat({"[", base::JoinString(server_types, ", "), "]"});
 }
 
+std::string_view ToYesOrNo(bool value) {
+  return value ? "Yes" : "No";
+}
+
 }  // namespace
 
 FormStructure::FormStructure(const FormData& form)
@@ -929,6 +933,10 @@ std::ostream& operator<<(std::ostream& buffer, const FormStructure& form) {
                  " (", url::Origin::Create(form.source_url()).Serialize(),
                  ")"});
   buffer << "\n Target URL:" << form.target_url();
+  if (base::FeatureList::IsEnabled(features::kAutofillAiServerModel)) {
+    buffer << "\n May run AutofillAI model: "
+           << ToYesOrNo(form.may_run_autofill_ai_model());
+  }
   for (size_t i = 0; i < form.field_count(); ++i) {
     buffer << "\n Field " << i << ": ";
     const AutofillField* field = form.field(i);
@@ -992,7 +1000,7 @@ std::ostream& operator<<(std::ostream& buffer, const FormStructure& form) {
     buffer << "\n  Label: " << truncated_label;
 
     buffer << "\n  Is empty: "
-           << (field->value(ValueSemantics::kCurrent).empty() ? "Yes" : "No");
+           << ToYesOrNo(field->value(ValueSemantics::kCurrent).empty());
   }
   return buffer;
 }
@@ -1020,6 +1028,10 @@ LogBuffer& operator<<(LogBuffer& buffer, const FormStructure& form) {
                  " (", url::Origin::Create(form.source_url()).Serialize(),
                  ")"});
   buffer << Tr{} << "Target URL:" << form.target_url();
+  if (base::FeatureList::IsEnabled(features::kAutofillAiServerModel)) {
+    buffer << Tr{} << "May run AutofillAI model: "
+           << ToYesOrNo(form.may_run_autofill_ai_model());
+  }
   for (size_t i = 0; i < form.field_count(); ++i) {
     buffer << Tag{"tr"};
     buffer << Tag{"td"} << "Field " << i << ": " << CTag{};
@@ -1094,7 +1106,7 @@ LogBuffer& operator<<(LogBuffer& buffer, const FormStructure& form) {
     buffer << Tr{} << "Label:" << truncated_label;
 
     buffer << Tr{} << "Is empty:"
-           << (field->value(ValueSemantics::kCurrent).empty() ? "Yes" : "No");
+           << ToYesOrNo(field->value(ValueSemantics::kCurrent).empty());
     buffer << Tr{} << "Is focusable:"
            << (field->IsFocusable() ? "Yes (focusable)" : "No (unfocusable)");
     buffer << Tr{} << "Is visible:"
