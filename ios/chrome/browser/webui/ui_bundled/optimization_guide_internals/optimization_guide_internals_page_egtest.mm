d926bd4d5bb1d56369427a933e5db7dfa116c856
rbpotter <rbpotter@chromium.org>
2025-03-07 15:18:24 -0800

    Reland "InternalOnlyUisPref: Enable on iOS"
    This is a reland of commit 7dc7dfdd463f0662a0f351d56dd55ea76c0715ef
    
    Fixing non-CQ tests that broke due to:
    - chrome://chrome-urls now using shadow DOM
    - chrome://optimization-guide-internals moved behind the pref
    
    Original change's description:
    > InternalOnlyUisPref: Enable on iOS
    >
    > Enabling on M136 for iOS, 1 milestone later than for other platforms.
    > This is because iOS code landed ~1 milestone later.
    >
    > Bug: 379889249
    > Change-Id: I7b3f924a25d7f43b12a6bd1982c6534eb5da25b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330200
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1429061}
    
    Bug: 379889249
    Change-Id: Ib90bcd4f1f94a5419d9b840999cb2c45e62b7530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334092
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429782}

--- ios/chrome/browser/webui/ui_bundled/optimization_guide_internals/optimization_guide_internals_page_egtest.mm
+++ ios/chrome/browser/webui/ui_bundled/optimization_guide_internals/optimization_guide_internals_page_egtest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/wait_util.h"
 #import "components/optimization_guide/core/optimization_guide_switches.h"
 #import "components/optimization_guide/optimization_guide_internals/webui/url_constants.h"
+#import "components/webui/chrome_urls/pref_names.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_test_app_interface.h"
 #import "ios/chrome/browser/webui/ui_bundled/web_ui_test_utils.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
@@ -26,6 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   config.additional_args.push_back(
       std::string("--") + optimization_guide::switches::kDebugLoggingEnabled);
   return config;
+}
+
+- (void)setUp {
+  [super setUp];
+  // Enables internal only UIs.
+  [ChromeEarlGrey setBoolValue:YES
+             forLocalStatePref:chrome_urls::kInternalOnlyUisEnabled];
 }
 
 // Tests that chrome://optimization-guide-internals loads when debug logs flag
