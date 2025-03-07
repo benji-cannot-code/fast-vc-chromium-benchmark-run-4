c42abe27c4293c476749f0a7346c0ae4ec56ecb9
Kaan Alsan <alsan@google.com>
2025-03-07 08:39:43 -0800

    (2/N) Remove unused "expand icon" option from OfferNotificationOptions
    The option is unused as of crrev.com/c/5694222, where a new UI handles
    the "expanded icon" logic.
    
    The previous CL removed the option from the struct. This CL updates the
    bubble controller to remove relevant logic. The follow-up CL will
    simplify `offer_notification_icon_view.cc`.
    
    Bug: 376283803
    Change-Id: I577e86f9ca0672a3b10334ebd69960c4a09b5dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317646
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429539}

--- chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
@@ -109,14 +109,6 @@ bool OfferNotificationBubbleControllerImpl::IsIconVisible() const {
   return bubble_state_ != BubbleState::kHidden;
 }
 
-bool OfferNotificationBubbleControllerImpl::ShouldIconExpand() const {
-  return icon_should_expand_;
-}
-
-void OfferNotificationBubbleControllerImpl::OnIconExpanded() {
-  icon_should_expand_ = false;
-}
-
 void OfferNotificationBubbleControllerImpl::OnBubbleClosed(
     PaymentsUiClosedReason closed_reason) {
   set_bubble_view(nullptr);
@@ -153,7 +145,6 @@ void OfferNotificationBubbleControllerImpl::ShowOfferNotificationIfApplicable(
     const AutofillOfferData& offer,
     const CreditCard* card,
     const OfferNotificationOptions& options) {
-  icon_should_expand_ = false;
 
   // If this is not the bubble's first show, and offer to be shown has not
   // changed, and it has not been shown for more than
