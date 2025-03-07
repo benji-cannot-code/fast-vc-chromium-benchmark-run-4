ffedd4207653755a57c9653a21b296a5efa8d446
Jérôme Lebel <jlebel@chromium.org>
2025-03-07 00:25:47 -0800

    [iOS] Fix for SharedTabGroupsTestCase/testForegroundIPH
    After landing this patch crrev.com/c/6330105,
    SharedTabGroupsTestCase/testForegroundIPH started to fail only on the
    bots.
    This patch partially reverts the guilty patch. Now the
    ConsistencyPromoSigninCoordinator declares being done after the view
    controller being dismissed, using the dismissed completion block.
    
    Fixed: 401241035
    Change-Id: Ia9aef88d813fbb5a161daf8637c539e767df0bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330686
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429365}

--- ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -362,11 +362,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     userPrefService->SetInteger(prefs::kSigninWebSignDismissalCount,
                                 skipCounter);
   }
+  __weak __typeof(self) weakSelf = self;
   [self.navigationController.presentingViewController
       dismissViewControllerAnimated:YES
-                         completion:nil];
-  [self runCompletionWithSigninResult:SigninCoordinatorResultCanceledByUser
-                   completionIdentity:nil];
+                         completion:^() {
+                           [weakSelf runCompletionWithSigninResult:
+                                         SigninCoordinatorResultCanceledByUser
+                                                completionIdentity:nil];
+                         }];
 }
 
 - (void)consistencyDefaultAccountCoordinatorOpenIdentityChooser:
@@ -473,11 +476,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                     withIdentity:(id<SystemIdentity>)identity {
   DCHECK([identity isEqual:self.selectedIdentity]);
   id<SystemIdentity> completionIdentity = identity;
+  __weak __typeof(self) weakSelf = self;
   [self.navigationController.presentingViewController
       dismissViewControllerAnimated:YES
-                         completion:nil];
-  [self runCompletionWithSigninResult:SigninCoordinatorResultSuccess
-                   completionIdentity:completionIdentity];
+                         completion:^() {
+                           [weakSelf runCompletionWithSigninResult:
+                                         SigninCoordinatorResultSuccess
+                                                completionIdentity:
+                                                    completionIdentity];
+                         }];
 }
 
 - (void)consistencyPromoSigninMediatorSignInCancelled:
