1889697c1b2c1153f8fb6c8d6972d4613fa489a5
dpapad <dpapad@chromium.org>
2025-03-06 16:11:04 -0800

    WebUI: Move deprecated mocha_adapter.js to a CrOS-only folder.
    This file is not used anywhere other than deprecated js2gtest tests,
    which only exist on CrOS. Moving it to a CrOS-only folder to make this
    clearer.
    
    Bug: 40273581
    Change-Id: I6fc47c5020b6efa003d6d178df8627896f071b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331715
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429233}

--- chrome/test/data/webui/chromeos/sys_internals/sys_internals_browsertest.js
+++ chrome/test/data/webui/chromeos/sys_internals/sys_internals_browsertest.js
@@ -25,7 +25,7 @@ SysInternalsBrowserTest.prototype = {
 
   extraLibraries: [
     '//third_party/node/node_modules/mocha/mocha.js',
-    '//chrome/test/data/webui/mocha_adapter.js',
+    '//chrome/test/data/webui/chromeos/mocha_adapter.js',
   ],
 };
 
