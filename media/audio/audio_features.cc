7f5bb46b154bb6df40b96daaeaddee2f36e48922
Johannes Kron <kron@chromium.org>
2025-03-07 12:17:16 -0800

    Add feature MacSystemAudioLoopbackOverride
    The feature enables experimentation with system audio loopback on
    macOS 15.0, where it is otherwise disabled.
    
    In addition,
    - The flag mac-loopback-audio-for-screen-share is extended to M145.
    - Swizzling of a private API that no longer exists is disabled on
      macOS 15+.
    
    Bug: 394329567
    Change-Id: If2d9d26ff2619b135602f33f03d0056ebe5c3d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329985
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429669}

--- media/audio/audio_features.cc
+++ media/audio/audio_features.cc
@@ -13,8 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/mac_util.h"
 #endif
 
-namespace features {
+namespace {
+#if BUILDFLAG(IS_MAC)
+BASE_FEATURE(kMacSystemAudioLoopbackOverride,
+             "MacSystemAudioLoopbackOverride",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
+}  // namespace
 
+namespace features {
 // If enabled, base::DumpWithoutCrashing is called whenever an audio service
 // hang is detected.
 BASE_FEATURE(kDumpOnAudioServiceHang,
@@ -50,10 +57,12 @@ bool IsSystemLoopbackCaptureSupported() {
 #elif BUILDFLAG(IS_MAC)
   // Only supported on macOS 13.0+.
   // Not supported on macOS 15.0+ yet.
+  // The override feature is useful for testing on unsupported versions.
   // TODO(crbug.com/365602111): Implement SCContentPicker compatible capture
   // for MacOS 15.
-  return base::mac::MacOSVersion() >= 13'00'00 &&
-         base::mac::MacOSVersion() < 15'00'00;
+  return (base::mac::MacOSVersion() >= 13'00'00 &&
+          base::mac::MacOSVersion() < 15'00'00) ||
+         base::FeatureList::IsEnabled(kMacSystemAudioLoopbackOverride);
 #elif BUILDFLAG(IS_LINUX) && defined(USE_PULSEAUDIO)
   return true;
 #else
