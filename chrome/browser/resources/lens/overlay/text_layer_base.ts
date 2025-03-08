e4a0278537008a2e718d875d81cfc4b232fc19b6
Juan Mojica <juanmojica@google.com>
2025-03-07 17:07:11 -0800

    [lensoverlay] Use full image response to detect text for context menu.
    Currently, the simplified text layer only uses the text response from
    interaction requests. However, we want to use the text from the full
    image response to prevent delay in showing the the detected text context
    menu items. This CL passes the full image response text back to the
    overlay WebUI so the simplified text layer can use it for detected words
    in a region. It will only be used as a fallback if the user clicks on
    the context menu item and region text has still not been returned.
    
    Change-Id: Ia9e59a71d3e47d2c7969cb80e1a12943d240fdf3
    Bug: 400512280, 401613784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331061
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1429822}

--- chrome/browser/resources/lens/overlay/text_layer_base.ts
+++ chrome/browser/resources/lens/overlay/text_layer_base.ts
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import type {GestureEvent} from './selection_utils.js';
 
+export type TextCopyCallback =
+    (textStartIndex: number, textEndIndex: number, text: string) => void;
+
 // Shared interface for the text layer on the overlay.
 export interface TextLayerBase {
   // Mouse events that the text selection layer handles.
@@ -21,6 +24,10 @@ export interface TextLayerBase {
   // Called when a selection was finished regardless of which layer handled it.
   // This includes region and text selections.
   onSelectionFinish(): void;
+
+  // Called when the selection overlay attempts to copy detected text.
+  onCopyDetectedText(
+      startIndex: number, endIndex: number, callbackFn: TextCopyCallback): void;
 
   selectAndSendWords(selectionStartIndex: number, selectionEndIndex: number):
       void;
