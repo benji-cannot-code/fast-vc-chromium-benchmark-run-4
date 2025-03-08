c77ceaf856b1848e916e5bd43b83ce3c06fb931f
Phillis Tang <phillis@chromium.org>
2025-03-07 22:30:22 -0800

    webnn: allow setting precision for gpu delegate
    Allow passing a flag to gpu delegate about floating precision to be
    used.
    
    Bug: 400455000
    Change-Id: Ic29f2ae216d005e4d5795b115b071e29d6e9f76f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6337359
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429861}

--- services/on_device_model/ml/chrome_ml_api.h
+++ services/on_device_model/ml/chrome_ml_api.h
@@ -242,6 +242,9 @@ struct ChromeMLMetricsFns {
                                       size_t buckets);
 };
 
+// Precision used by the gpu delegate during inference.
+enum class GpuDelegatePrecision { kFp16, kFp32 };
+
 struct ChromeMLTSAPI {
   // Construct a text safety model.
   // Destroy the returned object by passing it to DestroyModel.
@@ -396,6 +399,9 @@ struct ChromeMLAPI {
 
   // Creates a new TFLite delegate using the GPU inference engine.
   TfLiteDelegate* (*CreateGpuDelegate)();
+
+  TfLiteDelegate* (*CreateGpuDelegateWithPrecision)(
+      GpuDelegatePrecision precision);
 
   // Destroys the TFLite delegate created by `CreateDelegate()` call.
   void (*DestroyGpuDelegate)(TfLiteDelegate* delegate);
