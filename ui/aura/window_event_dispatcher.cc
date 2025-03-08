c25006da10a703ab49c9b607a7331e86bf32941b
Yichen <yichenz@chromium.org>
2025-03-07 23:52:53 -0800

    display: Mouse exit from the old display when switching displays
    When switching from a window tree host to another, mouse should
    exit the old window tree host otherwise some effects on the old
    display will never exit such as hovering.
    
    BUG=298548104
    
    Change-Id: I2045d2b21a79f5fa3826337cfdb0876c3e303e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6336772
    Commit-Queue: Yichen Zhou <yichenz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429869}

--- ui/aura/window_event_dispatcher.cc
+++ ui/aura/window_event_dispatcher.cc
@@ -268,6 +268,16 @@ void WindowEventDispatcher::OnCursorMovedToRootLocation(
   PostSynthesizeMouseMove(window());
 }
 
+void WindowEventDispatcher::OnHostCursorExit() {
+  auto details =
+      DispatchMouseExitAtPoint(window(), GetLastMouseLocationInRoot());
+  if (details.dispatcher_destroyed) {
+    return;
+  }
+  mouse_pressed_handler_ = nullptr;
+  mouse_moved_handler_ = nullptr;
+}
+
 void WindowEventDispatcher::OnPostNotifiedWindowDestroying(Window* window) {
   OnWindowHidden(window, WINDOW_DESTROYED);
 }
--- ui/aura/window_event_dispatcher.h
+++ ui/aura/window_event_dispatcher.h
@@ -131,6 +131,9 @@ class AURA_EXPORT WindowEventDispatcher : public ui::EventProcessor,
   void OnHostLostMouseGrab();
   void OnCursorMovedToRootLocation(const gfx::Point& root_location);
 
+  // Invoked when mouse exit the underlying window tree host.
+  void OnHostCursorExit();
+
   // TODO(beng): This is only needed because this cleanup needs to happen after
   //             all other observers are notified of OnWindowDestroying() but
   //             before OnWindowDestroyed() is sent (i.e. while the window
