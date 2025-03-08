a05b01085ee06c5b2263e79abcec3dc38efd5d33
Camillia Smith Barnes <cammie@chromium.org>
2025-03-07 20:43:51 -0800

    Shared Storage: Remove unused NotifySharedStorageAccessed in Observer
    longer used. We remove it.
    
    SharedStorageHeaderObserver: :NotifySharedStorageAccessed() is no
    Change-Id: Ia86d2a0e115e84a774f618b5f67b53e809218cee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332343
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429851}

--- content/browser/shared_storage/shared_storage_header_observer.h
+++ content/browser/shared_storage/shared_storage_header_observer.h
@@ -24,9 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-using AccessType =
-    SharedStorageRuntimeManager::SharedStorageObserverInterface::AccessType;
-
 // Receives notifications from `StoragePartitionImpl` when a parsed
 // "Shared-Storage-Write" header is received from the network service. The
 // parsed header takes the form of a vector of StructPtrs bundling operation
@@ -121,11 +118,6 @@ class CONTENT_EXPORT SharedStorageHeaderObserver {
       NavigationOrDocumentHandle* navigation_or_document_handle,
       const url::Origin& request_origin,
       std::string* out_debug_message = nullptr);
-
-  void NotifySharedStorageAccessed(AccessType type,
-                                   FrameTreeNodeId main_frame_id,
-                                   const url::Origin& request_origin,
-                                   const SharedStorageEventParams& params);
 
   // `storage_partition_` owns `this`, so it will outlive `this`.
   raw_ptr<StoragePartitionImpl> storage_partition_;
