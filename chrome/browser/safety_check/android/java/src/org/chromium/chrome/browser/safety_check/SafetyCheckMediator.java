78560ae6ad9c17bf67491111a354a12132af767c
Ioana Pandele <ioanap@chromium.org>
2025-03-10 08:29:34 -0700

    Clean up duplicate code in SafetyCheckMediator constructor
    There is a common constructor that both the public and test constructor
    use, so members which are initialized the same way in both public and
    test constructors should just use the common constructor.
    
    In addition, this CL makes the common constructor private to make it
    clear that it's only used internally in the class.
    
    Change-Id: I3d2231eceedcfdb2e00bae4fa5ec966f54234c7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6339729
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430258}

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
@@ -229,8 +229,8 @@ class SafetyCheckMediator {
                 new Handler(),
                 passwordStoreBridge,
                 new PasswordCheckControllerFactory(),
-                passwordManagerHelper);
-        mModalDialogManagerSupplier = modalDialogManagerSupplier;
+                passwordManagerHelper,
+                modalDialogManagerSupplier);
         mCustomTabIntentHelper = customTabIntentHelper;
     }
 
@@ -263,11 +263,11 @@ class SafetyCheckMediator {
                 handler,
                 passwordStoreBridge,
                 passwordCheckControllerFactory,
-                passwordManagerHelper);
-        mModalDialogManagerSupplier = modalDialogManagerSupplier;
+                passwordManagerHelper,
+                modalDialogManagerSupplier);
     }
 
-    SafetyCheckMediator(
+    private SafetyCheckMediator(
             Profile profile,
             PropertyModel safetyCheckModel,
             PropertyModel passwordsCheckAccountModel,
@@ -280,7 +280,8 @@ class SafetyCheckMediator {
             Handler handler,
             PasswordStoreBridge passwordStoreBridge,
             PasswordCheckControllerFactory passwordCheckControllerFactory,
-            PasswordManagerHelper passwordManagerHelper) {
+            PasswordManagerHelper passwordManagerHelper,
+            ObservableSupplier<ModalDialogManager> modalDialogManagerSupplier) {
         mProfile = profile;
         mSafetyCheckModel = safetyCheckModel;
         mPasswordsCheckAccountStorageModel = passwordsCheckAccountModel;
@@ -295,6 +296,7 @@ class SafetyCheckMediator {
                 passwordCheckControllerFactory.create(
                         syncService, prefService, passwordStoreBridge, passwordManagerHelper);
         mPasswordManagerHelper = passwordManagerHelper;
+        mModalDialogManagerSupplier = modalDialogManagerSupplier;
         // Set the listener for clicking the updates element.
         mSafetyCheckModel.set(
                 SafetyCheckProperties.UPDATES_CLICK_LISTENER,
