e89de961233aa5fe4253ed75fdfff90a67265b0c
yiwenqian <yiwenqian@google.com>
2025-03-10 12:20:50 -0700

    [BNPL] Add ShowIPHFeaturePromos() to trigger BNPL IPH bubble.
    When we update the suggestions list to add a BNPL suggestion, it ends up
    going through OnSuggestionsChanged(). In order for
    MaybeShowFeaturePromo() to be triggered for IPH bubble, we add
    ShowIPHFeaturePromos() to OnSuggestionsChanged().
    
    Bug: 399124584
    Change-Id: I619552a05240da944bf0e9eba8f72bfb6458817e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6312258
    Commit-Queue: Yiwen Qian <yiwenqian@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430401}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.cc
@@ -690,6 +690,7 @@ void PopupViewViews::OnSuggestionsChanged(bool prefer_prev_arrow_side) {
   }
 
   MaybeA11yFocusInformationalSuggestion();
+  ShowIPHFeaturePromos();
 }
 
 bool PopupViewViews::OverlapsWithPictureInPictureWindow() const {
@@ -766,12 +767,7 @@ bool PopupViewViews::HasFocus() const {
 
 void PopupViewViews::OnWidgetVisibilityChanged(views::Widget* widget,
                                                bool visible) {
-  if (!visible || !controller_) {
-    return;
-  }
-
-  Browser* browser = GetBrowser();
-  if (!browser) {
+  if (!visible) {
     return;
   }
 
@@ -780,21 +776,7 @@ void PopupViewViews::OnWidgetVisibilityChanged(views::Widget* widget,
   // educational messages. The promo bubble should only be shown once in one
   // session and has a limit for how many times it can be shown at most in a
   // period of time.
-  for (const auto& iph_metadata : base::MakeFlatSet<Suggestion::IPHMetadata>(
-           controller_->GetSuggestions(), /*comp=*/{},
-           &Suggestion::iph_metadata)) {
-    if (iph_metadata.feature) {
-      user_education::FeaturePromoParams params(*iph_metadata.feature);
-      // Setting the params to a `std::vector` (even if it is empty), indicates
-      // to the framework that a substitution should be made. Therefore only
-      // set it if `iph_params` is non-empty.
-      if (!iph_metadata.iph_params.empty()) {
-        params.body_params = iph_metadata.iph_params;
-        params.screen_reader_params = iph_metadata.iph_params;
-      }
-      browser->window()->MaybeShowFeaturePromo(std::move(params));
-    }
-  }
+  ShowIPHFeaturePromos();
 }
 
 void PopupViewViews::SearchBarOnInputChanged(std::u16string_view query) {
@@ -893,6 +875,33 @@ void PopupViewViews::SetSelectedCell(
                        autoselect_first_suggestion));
   } else {
     row_with_selected_cell_ = std::nullopt;
+  }
+}
+
+void PopupViewViews::ShowIPHFeaturePromos() {
+  Browser* browser = GetBrowser();
+  if (!browser) {
+    return;
+  }
+
+  if (!GetWidget() || !GetWidget()->IsVisible() || !controller_) {
+    return;
+  }
+
+  for (const auto& iph_metadata : base::MakeFlatSet<Suggestion::IPHMetadata>(
+           controller_->GetSuggestions(), /*comp=*/{},
+           &Suggestion::iph_metadata)) {
+    if (iph_metadata.feature) {
+      user_education::FeaturePromoParams params(*iph_metadata.feature);
+      // Setting the params to a `std::vector` (even if it is empty), indicates
+      // to the framework that a substitution should be made. Therefore only
+      // set it if `iph_params` is non-empty.
+      if (!iph_metadata.iph_params.empty()) {
+        params.body_params = iph_metadata.iph_params;
+        params.screen_reader_params = iph_metadata.iph_params;
+      }
+      browser->window()->MaybeShowFeaturePromo(std::move(params));
+    }
   }
 }
 
