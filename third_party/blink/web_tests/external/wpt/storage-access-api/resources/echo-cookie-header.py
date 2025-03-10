e05a17b0ebd0cd7c6d782d28daf4a617bcaa6760
Chris Fredrickson <cfredric@chromium.org>
2025-03-09 19:15:56 -0700

    [SAA] Add cross-site redirect test case
    This test verifies that a cross-site HTTP redirect clears the SAA-opt-in
    signal, even if the request is redirected back to the origin that
    originally opted in.
    
    Bug: 379030052
    Change-Id: I55cd123d2340cae6b88fed585170377912cf667b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330426
    Commit-Queue: Sandor «Alex» Major <sandormajor@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Sandor «Alex» Major <sandormajor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430072}

--- third_party/blink/web_tests/external/wpt/storage-access-api/resources/echo-cookie-header.py
+++ third_party/blink/web_tests/external/wpt/storage-access-api/resources/echo-cookie-header.py
@@ -2,12 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
   # Set the cors enabled headers.
   origin = request.headers.get(b"Origin")
-  headers = []
-  if origin is not None and origin != b"null":
-      headers.append((b"Content-Type", b"text/plain"))
-      headers.append((b"Access-Control-Allow-Origin", origin))
-      headers.append((b"Access-Control-Allow-Credentials", 'true'))
+  if origin:
+      response.headers.set(b"Content-Type", b"text/plain")
+      response.headers.set(b"Access-Control-Allow-Origin", origin)
+      response.headers.set(b"Access-Control-Allow-Credentials", 'true')
 
-  cookie_header = request.headers.get(b"Cookie", b"")
-
-  return (200, headers, cookie_header)
+  return request.headers.get(b"Cookie", b"")
