9e69cb1c6685a1f5e929103c378e9f83079471c0
Kristi Saney <kristislee@google.com>
2025-03-10 11:53:09 -0700

    [Read aloud] Use a browser proxy for SpeechSynthesis
    This is the preferred pattern for wrapping logic that's sent to the
    browser (in this case window.speechSynthesis). This is also better for
    tests where we can use a TestBrowserProxy to better streamline our tests
    that use speech synthesis.
    
    This should also reduce the number of microtasksFinished() calls needed
    and reduce test flakiness. This is also a prerequisite for pulling out
    speech logic from app.ts in the future if we want to.
    
    Bug: 397973923
    Change-Id: Idf852a7814db6692ea64d937974901184923f7da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6336717
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430384}

--- chrome/test/data/webui/side_panel/read_anything/speech_uses_max_text_length_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/speech_uses_max_text_length_test.ts
@@ -5,16 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 
 import type {AppElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
-import {MAX_SPEECH_LENGTH_FOR_REMOTE_VOICES, MAX_SPEECH_LENGTH_FOR_WORD_BOUNDARIES} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {MAX_SPEECH_LENGTH_FOR_REMOTE_VOICES, MAX_SPEECH_LENGTH_FOR_WORD_BOUNDARIES, SpeechBrowserProxyImpl} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertEquals, assertFalse, assertGT, assertLT, assertNotEquals, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
 
 import {createAndSetVoices, createApp} from './common.js';
-import {FakeSpeechSynthesis} from './fake_speech_synthesis.js';
+import {TestSpeechBrowserProxy} from './test_speech_browser_proxy.js';
 
 suite('SpeechUsesMaxTextLength', () => {
   let app: AppElement;
   let maxSpeechLength: number;
-  let speechSynthesis: FakeSpeechSynthesis;
+  let speech: TestSpeechBrowserProxy;
 
   const shortSentence =
       'The snow glows white on the mountain tonight, not a footprint to be ' +
@@ -124,11 +124,11 @@ suite('SpeechUsesMaxTextLength', () => {
     // ReadAnythingAppController, onConnected creates mojo pipes to connect to
     // the rest of the Read Anything feature, which we are not testing here.
     chrome.readingMode.onConnected = () => {};
+    speech = new TestSpeechBrowserProxy();
+    SpeechBrowserProxyImpl.setInstance(speech);
 
     app = await createApp();
     maxSpeechLength = MAX_SPEECH_LENGTH_FOR_REMOTE_VOICES;
-    speechSynthesis = new FakeSpeechSynthesis();
-    app.synth = speechSynthesis;
   });
   // These checks ensure the text used in this test stays up to date
   // in case the maximum speech length changes.
@@ -201,7 +201,7 @@ suite('SpeechUsesMaxTextLength', () => {
         MAX_SPEECH_LENGTH_FOR_REMOTE_VOICES);
 
 
-    createAndSetVoices(app, speechSynthesis, [
+    createAndSetVoices(app, speech, [
       {lang: 'en-us', name: 'Google Elsa (Natural)', localService: true},
     ]);
     // On ChromeOS we don't care about the length of local voices because
@@ -219,7 +219,7 @@ suite('SpeechUsesMaxTextLength', () => {
   });
 
   test('correct max length used with ChromeOS', () => {
-    createAndSetVoices(app, speechSynthesis, [
+    createAndSetVoices(app, speech, [
       {lang: 'en-us', name: 'Google Kristoff (Natural)', localService: true},
     ]);
     assertGT(longSentence.length, MAX_SPEECH_LENGTH_FOR_WORD_BOUNDARIES);
