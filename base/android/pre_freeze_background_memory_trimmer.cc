f37369279dea6f70748e8695d19d5522183b8905
Thiabaud Engelbrecht <thiabaud@google.com>
2025-03-10 14:06:28 -0700

    [pre_freeze] Fix crash if unable to record metrics
    This CL fixes the crash shown here:
    https://crash.corp.google.com/browse?q=product_name%3D%22Chrome_Android%22+AND+expanded_custom_data.ChromeCrashProto.magic_signature_1.name+LIKE+%27%25PreFreeze%25%27+AND+expanded_custom_data.ChromeCrashProto.magic_signature_1.name%3D%27base%3A%3Aandroid%3A%3APreFreezeBackgroundMemoryTrimmer%3A%3ARecordMetrics%27,
    which happens if unable to get the value for a particular metric during
    metric recording. The code previously did not check that the optional
    had a value before using it.
    
    Change-Id: I3dad7507b8b4bf51e64d9f4a89741ac89177f4fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6337589
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430480}

--- base/android/pre_freeze_background_memory_trimmer.cc
+++ base/android/pre_freeze_background_memory_trimmer.cc
@@ -40,7 +40,8 @@ namespace {
 enum class MetricsFailure {
   kAlreadyRunning,
   kSizeMismatch,
-  kMaxValue = kSizeMismatch
+  kMeasureFailure,
+  kMaxValue = kMeasureFailure
 };
 
 // This constant is chosen arbitrarily, to allow time for the background tasks
@@ -235,6 +236,12 @@ void PreFreezeBackgroundMemoryTrimmer::RecordMetrics() {
     const std::optional<uint64_t> value_before = values_before_[i];
 
     std::optional<uint64_t> value_after = metric->Measure();
+
+    if (!value_after) {
+      UmaHistogramEnumeration("Memory.PreFreeze2.RecordMetricsFailureType",
+                              MetricsFailure::kMeasureFailure);
+      continue;
+    }
 
     MaybeRecordPreFreezeMetric(value_before, metric->name(), "Before");
     MaybeRecordPreFreezeMetric(value_after, metric->name(), "After");
