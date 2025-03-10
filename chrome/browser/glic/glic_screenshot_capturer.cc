12ec1ce48adbbc8ec8db59e59be828460068badc
Lei Zhang <thestig@chromium.org>
2025-03-10 13:03:54 -0700

    glic: Add screenshot encode debug logging
    Add logging so --vmodule=glic_screenshot_capturer=1 will display:
    
    1) The input frame size.
    2) Time spent encoding.
    3) The output file size.
    
    This information is useful for fine tuning the encode quality value.
    
    Bug: 381874264
    Change-Id: I0c3acbdd3564aa3565560da69626e2a944795dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313300
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430433}

--- chrome/browser/glic/glic_screenshot_capturer.cc
+++ chrome/browser/glic/glic_screenshot_capturer.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/logging.h"
 #include "base/task/thread_pool.h"
+#include "base/time/time.h"
 #include "chrome/browser/glic/resources/grit/glic_browser_resources.h"
 #include "chrome/browser/media/webrtc/desktop_media_picker_controller.h"
 #include "chrome/browser/ui/browser.h"
@@ -44,11 +46,17 @@ std::vector<uint8_t> ConvertFrameToJpeg(
     return {};
   }
 
+  auto start_time = base::Time::Now();
   auto jpeg_data = gfx::JPEGCodec::Encode(
       sk_bitmap, features::kGlicScreenshotEncodeQuality.Get());
   if (!jpeg_data.has_value()) {
     return {};
   }
+  auto encode_time = base::Time::Now() - start_time;
+  VLOG(1) << "JPEGCodec::Encode() frame size: " << frame->size().width() << "x"
+          << frame->size().height();
+  VLOG(1) << "JPEGCodec::Encode() time: " << encode_time;
+  VLOG(1) << "JPEGCodec::Encode() result file size: " << jpeg_data->size();
   return std::move(*jpeg_data);
 }
 
