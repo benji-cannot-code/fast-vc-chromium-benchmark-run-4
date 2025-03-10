abd1dbd99fb49125e67ae088471c4eec7026b906
Mingyu Lei <leimy@chromium.org>
2025-03-09 23:36:50 -0700

    Reland "Enable BFCache for CCNS by default"
    This is a reland of commit f9061125ef2bc3aa3c30ea6cc0343f97f7b4bbc1
    
    The changes are in the webview tests, previously it failed because the
    CCNS page is now in BFCache and the onPageFinishedHelper won't work. I
    followed the [BFCache
    test](https://source.chromium.org/chromium/chromium/src/+/main:android_webview/javatests/src/org/chromium/android_webview/test/AwBackForwardCacheTest.java;l=117-130;drc=4c284a737e2b8b03e3872e6a77fda7da8efebc7e)
    to use onPageStartedHelper instead to resolve it.
    
    Original change's description:
    > Enable BFCache for CCNS by default
    >
    > According to the ramp up plan, we will launch this feature on Mar 11.
    >
    > This CL also updates the back_forward_cache_no_store_browsertest.cc:
    > - for the previous main frame/sub frame test
    > (BackForwardCacheBrowserTestNoTestingConfig after
    > http://crrev.com/c/4943313) will explicitly disable BFCache for CCNS
    > since it's testing that behaviour.
    >
    > - for
    > BackForwardCacheWithJsNetworkRequestReceivingCCNSResourceBrowserTest,
    > update the logic and comment to make sure that the CCNS page will be
    > evicted only when the same-origin JS network happens, and they will be
    > reported together.
    >
    > This CL also rename the feature name for
    > `kJsNetworkRequestReceivedCacheControlNoStoreResource` to
    > `js-network-request-received-cache-control-no-store` so it's different
    > from the name with CCNS itself. This makes the reason field in the not
    > restore reason API more meaningful.
    >
    > Bug: 40189625
    > Change-Id: I85692053a46a56b998b65164d7e4df3dfc118ec0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309859
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Fergal Daly <fergal@chromium.org>
    > Commit-Queue: Mingyu Lei <leimy@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1427608}
    
    Bug: 40189625
    Change-Id: I2a77ed9cd73038fd5fff936ef89a57768c016b20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332393
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430108}

--- android_webview/javatests/src/org/chromium/android_webview/test/LoadUrlTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/LoadUrlTest.java
@@ -678,7 +678,7 @@ public class LoadUrlTest extends AwParameterizedTest {
         HistoryUtils.goBackSync(
                 InstrumentationRegistry.getInstrumentation(),
                 awContents.getWebContents(),
-                contentsClient.getOnPageFinishedHelper());
+                contentsClient.getOnPageStartedHelper());
         validateHeadersValue(awContents, contentsClient, extraHeaders, true);
     }
 
