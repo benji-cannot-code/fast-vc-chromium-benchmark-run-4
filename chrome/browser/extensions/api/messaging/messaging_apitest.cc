b1b7f62ac4f1f0bb27ef535ec7192deea824e360
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-03-10 11:39:41 -0700

    [Desktop Android] Enable (some) extensions messaging API tests
    Enable a smattering of messaging API tests on the experimental
    desktop android build.
    
    Bug: 400455005
    Change-Id: Ib122bace68caceb8db89f286163eb9df052cd2d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333638
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430375}

--- chrome/browser/extensions/api/messaging/messaging_apitest.cc
+++ chrome/browser/extensions/api/messaging/messaging_apitest.cc
@@ -25,12 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/browsertest_util.h"
-#include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_commands.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "components/embedder_support/switches.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -59,6 +54,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/features.h"
 #include "url/gurl.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/extensions/extension_platform_apitest.h"
+#else
+#include "chrome/browser/extensions/extension_apitest.h"
+#endif
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/ui_test_utils.h"
+#endif
+
+#if BUILDFLAG(IS_ANDROID)
+using ExtensionApiTestBase = extensions::ExtensionPlatformApiTest;
+#else
+using ExtensionApiTestBase = extensions::ExtensionApiTest;
+#endif
 
 namespace extensions {
 namespace {
@@ -113,7 +127,7 @@ class MessageSender : public ExtensionHostRegistry::Observer {
       host_registry_observation_{this};
 };
 
-class MessagingApiTest : public ExtensionApiTest {
+class MessagingApiTest : public ExtensionApiTestBase {
  public:
   explicit MessagingApiTest(bool enable_back_forward_cache = true) {
     if (!enable_back_forward_cache) {
@@ -133,7 +147,7 @@ class MessagingApiTest : public ExtensionApiTest {
   ~MessagingApiTest() override = default;
 
   void SetUpOnMainThread() override {
-    ExtensionApiTest::SetUpOnMainThread();
+    ExtensionApiTestBase::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
     ASSERT_TRUE(StartEmbeddedTestServer());
   }
@@ -154,6 +168,8 @@ class MessagingApiWithoutBackForwardCacheTest : public MessagingApiTest {
   MessagingApiWithoutBackForwardCacheTest()
       : MessagingApiTest(/*enable_back_forward_cache=*/false) {}
 };
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 
 IN_PROC_BROWSER_TEST_F(MessagingApiTest, Messaging) {
   ASSERT_TRUE(RunExtensionTest("messaging/connect", {.custom_arg = "bfcache"}))
@@ -248,6 +264,8 @@ IN_PROC_BROWSER_TEST_F(MessagingApiTest, SendMessageDisconnect) {
   ASSERT_TRUE(RunExtensionTest(dir.UnpackedPath(), {}, {}));
 }
 
+#endif
+
 // Tests that message passing from one extension to another works.
 IN_PROC_BROWSER_TEST_F(MessagingApiTest, MessagingExternal) {
   ASSERT_TRUE(LoadExtension(
@@ -258,6 +276,8 @@ IN_PROC_BROWSER_TEST_F(MessagingApiTest, MessagingExternal) {
       << message_;
 }
 
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+
 // Tests that a content script can exchange messages with a tab even if there is
 // no background page.
 IN_PROC_BROWSER_TEST_F(MessagingApiTest, MessagingNoBackground) {
@@ -265,6 +285,8 @@ IN_PROC_BROWSER_TEST_F(MessagingApiTest, MessagingNoBackground) {
                                {.extension_url = "page_in_main_frame.html"}))
       << message_;
 }
+
+#endif
 
 // Tests that messages with event_urls are only passed to extensions with
 // appropriate permissions.
@@ -281,7 +303,6 @@ IN_PROC_BROWSER_TEST_F(MessagingApiTest, MessagingBackgroundOnly) {
 // TODO(kalman): Most web messaging tests disabled on windows due to extreme
 // flakiness. See http://crbug.com/350517.
 #if !BUILDFLAG(IS_WIN)
-
 
 IN_PROC_BROWSER_TEST_F(MessagingApiTest, MessagingUserGesture) {
   const char kManifest[] = "{"
@@ -507,6 +528,8 @@ IN_PROC_BROWSER_TEST_F(MessagingApiTest,
 
 #endif  // !BUILDFLAG(IS_WIN)
 
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+
 // Tests that messages sent in the pagehide handler of a window arrive.
 IN_PROC_BROWSER_TEST_F(MessagingApiTest, MessagingOnPagehide) {
   const Extension* extension =
@@ -722,6 +745,8 @@ IN_PROC_BROWSER_TEST_F(MessagingApiFencedFrameTest, Load) {
   ASSERT_TRUE(RunExtensionTest("messaging/connect_fenced_frames", {}))
       << message_;
 }
+
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 }  // namespace
 
