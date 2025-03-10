a47e41e9c03679410d8041b0f766bb131d2fd678
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-03-10 13:23:06 -0700

    [tracing-stdlib] Fix formatting in some SQL files
    Perfetto now runs third_party/perfetto/tools/format_sql.py on presubmit.
    That's causing the import of the chrome stdlib to fail in
    https://r.android.com/3510819. This CL fixes the issues signaled by
    the script. More concretely, it:
    - Adds omitted JOINs, parenthesis and SELECTs.
    - Disables formatting entirely in scroll_offsets.sql, which was done
      upstream in https://r.android.com/3500388.
    
    PERFETTO_TESTS=`autoninja -C out/linux perfetto_diff_tests && out/linux/bin/run_perfetto_diff_tests`
    
    Bug: None
    Change-Id: I7f750117a74b1f16f684485291aae7176a6f4e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6335842
    Commit-Queue: Primiano Tucci <primiano@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430447}

--- base/tracing/stdlib/chrome/scroll_jank/scroll_offsets.sql
+++ base/tracing/stdlib/chrome/scroll_jank/scroll_offsets.sql
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -- Use of this source code is governed by a BSD-style license that can be
 -- found in the LICENSE file.
 
+--- sqlformat file off
+
 -- This file creates two public views:
 --     - chrome_scroll_input_offsets and
 --     - chrome_presented_scroll_offsets
