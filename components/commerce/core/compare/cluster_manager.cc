e5abf9e8cc865a09847384461da7aef168f8978a
Matt Jones <mdjones@chromium.org>
2025-03-10 11:21:26 -0700

    [compare] Use const ref for iteration and check value before access
    Speculative means of addressing a crash in the cluster manager. Rather
    than copying pair objects, we'll just use references. We also check
    that a product info has a value before trying to access it.
    
    Bug: 401914721
    Change-Id: Ic4b94be26365e427f20bbd6902d6cfde5e1887dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6339967
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430365}

--- components/commerce/core/compare/cluster_manager.cc
+++ components/commerce/core/compare/cluster_manager.cc
@@ -68,7 +68,11 @@ std::string FindTitleForSimilarProducts(
   base::flat_set<std::string> bottom_labels;
   std::map<std::string, int> label_count;
 
-  for (auto entry : product_infos) {
+  for (const auto& entry : product_infos) {
+    if (!entry.second.has_value()) {
+      continue;
+    }
+
     base::flat_set<std::string> counted_labels;
     for (const auto& product_category :
          entry.second->category_data.product_categories()) {
@@ -118,7 +122,7 @@ std::string FindTitleForSimilarProducts(
   // a good second-to-bottom label. For a second-to-bottom label to be good
   // enough to become title, it has to be shared by all products.
   std::string alternate_title;
-  for (auto pair : label_count) {
+  for (const auto& pair : label_count) {
     if ((size_t)pair.second != product_infos.size()) {
       continue;
     }
