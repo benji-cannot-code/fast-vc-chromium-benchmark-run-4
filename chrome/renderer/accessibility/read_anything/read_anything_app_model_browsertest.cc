18b5f70d0e8f184e0890a5a72e3dff3b3cfb344b
Peter Kasting <pkasting@chromium.org>
2025-03-09 17:07:32 -0700

    Hold app model by value instead of by pointer.
    The only reason this needed to be by pointer is to reset it for one
    test, which can be simpler (albeit with more boilerplate test case
    framework) if it's just pulled out separately. Holding it by value makes
    the lifetime clearer.
    
    Bug: 364987728
    Change-Id: Ied189782c7d759c4f141c28ad83a75ddd4640435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6338096
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430057}

--- chrome/renderer/accessibility/read_anything/read_anything_app_model_browsertest.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_model_browsertest.cc
@@ -26,16 +26,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using ::testing::ElementsAre;
 
+class ReadAnythingAppModelNoInitTest : public ChromeRenderViewTest {
+ public:
+  ReadAnythingAppModelNoInitTest() = default;
+  ReadAnythingAppModelNoInitTest(const ReadAnythingAppModelNoInitTest&) =
+      delete;
+  ReadAnythingAppModelNoInitTest& operator=(
+      const ReadAnythingAppModelNoInitTest&) = delete;
+  ~ReadAnythingAppModelNoInitTest() override = default;
+
+  const ReadAnythingAppModel& model() const { return model_; }
+
+ private:
+  ReadAnythingAppModel model_;
+};
+
+TEST_F(ReadAnythingAppModelNoInitTest, IsDocs_FalseBeforeTreeInitialization) {
+  EXPECT_FALSE(model().IsDocs());
+}
+
 class ReadAnythingAppModelTest : public ChromeRenderViewTest {
  public:
   ReadAnythingAppModelTest() = default;
-  ~ReadAnythingAppModelTest() override = default;
   ReadAnythingAppModelTest(const ReadAnythingAppModelTest&) = delete;
   ReadAnythingAppModelTest& operator=(const ReadAnythingAppModelTest&) = delete;
+  ~ReadAnythingAppModelTest() override = default;
 
   void SetUp() override {
     ChromeRenderViewTest::SetUp();
-    model_ = std::make_unique<ReadAnythingAppModel>();
 
     // Create a tree id.
     tree_id_ = ui::AXTreeID::CreateNewAXTreeID();
@@ -49,17 +67,13 @@ class ReadAnythingAppModelTest : public ChromeRenderViewTest {
     model().Reset({});
   }
 
-  void SetUpWithoutInitialization() {
-    model_ = std::make_unique<ReadAnythingAppModel>();
-  }
-
-  ReadAnythingAppModel& model() { return *model_; }
-  const ReadAnythingAppModel& model() const { return *model_; }
+  ReadAnythingAppModel& model() { return model_; }
+  const ReadAnythingAppModel& model() const { return model_; }
 
   bool AreAllPendingUpdatesEmpty() {
     size_t count = 0;
     for (auto const& [tree_id, updates] :
-         model_->GetPendingUpdatesForTesting()) {
+         model().GetPendingUpdatesForTesting()) {
       count += updates.size();
     }
     return count == 0;
@@ -75,14 +89,14 @@ class ReadAnythingAppModelTest : public ChromeRenderViewTest {
                                   const std::vector<ui::AXTreeUpdate>& updates,
                                   bool speech_playing = false) {
     std::vector<ui::AXEvent> events;
-    model_->AccessibilityEventReceived(
+    model().AccessibilityEventReceived(
         tree_id, const_cast<std::vector<ui::AXTreeUpdate>&>(updates), events,
         speech_playing);
   }
 
   void ProcessDisplayNodes(const std::vector<ui::AXNodeID>& content_node_ids) {
     model().Reset(content_node_ids);
-    model_->ComputeDisplayNodeIdsForDistilledTree();
+    model().ComputeDisplayNodeIdsForDistilledTree();
   }
 
   std::vector<int> SendSimpleUpdateAndGetChildIds() {
@@ -104,14 +118,8 @@ class ReadAnythingAppModelTest : public ChromeRenderViewTest {
   ui::AXTreeID tree_id_;
 
  private:
-  std::unique_ptr<ReadAnythingAppModel> model_ = nullptr;
+  ReadAnythingAppModel model_;
 };
-
-TEST_F(ReadAnythingAppModelTest, IsDocs_FalseBeforeTreeInitialization) {
-  EXPECT_FALSE(model().IsDocs());
-  SetUpWithoutInitialization();
-  EXPECT_FALSE(model().IsDocs());
-}
 
 TEST_F(ReadAnythingAppModelTest, FontName) {
   EXPECT_NE(model().font_name(), std::string());
