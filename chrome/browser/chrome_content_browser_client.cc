25aad0cd3f943920d729fcf553b9a4bf3dfce479
Wen Zhao <wenz@google.com>
2025-03-10 11:54:53 -0700

    Remove restriction on client certs in background request on android desktop.
    Bug: 397967650
    Change-Id: I6f78d407c21a812a1d5148643fce2ba2aaaf6d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331568
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Wen Zhao <wenz@google.com>
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430385}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
 #include "base/values.h"
+#include "build/android_buildflags.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "build/config/chromebox_for_meetings/buildflags.h"  // PLATFORM_CFM
@@ -4179,7 +4180,10 @@ base::OnceClosure ChromeContentBrowserClient::SelectClientCertificate(
     // result in always proceeding with no certificate for any request from an
     // extension service worker. That decision would be remembered across the
     // entire profile, potentially locking the user out of the origin.
-#if BUILDFLAG(ENABLE_EXTENSIONS) && !BUILDFLAG(IS_ANDROID)
+    // Allow background requests on desktop android even if there are no
+    // matching certificates.
+#if BUILDFLAG(ENABLE_EXTENSIONS) && \
+    !(BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_DESKTOP_ANDROID))
     if (matching_certificates.empty() && nonmatching_certificates.empty()) {
       extensions::ProcessMap* process_map =
           extensions::ProcessMap::Get(profile);
