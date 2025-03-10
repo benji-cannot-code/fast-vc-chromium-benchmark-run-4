6bf0aaf805e5ccbbb5cff2193c425929135d6461
Devon Loehr <dloehr@google.com>
2025-03-10 07:16:16 -0700

    Simple AST rewriter example
    This is an implementation of an AST rewriter based on clang's AST
    matchers, designed to be usable as a template for learning how to
    make such rewriters/beginning new ones. The rewriter itself
    replaces instances of `b ? "true" : "false"` with `base::ToString(b)`
    in C++ code, and was used to make several contributions to
    crbug.com/335797528.
    
    Change-Id: I0602694ed1acb424cc3e5eb17a8e89837ded4350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322829
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430219}

---
+++ tools/clang/ast_rewriter/tests/cond-test.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma clang diagnostic ignored "-Wunused-variable"
+
+#include <string>
+
+void foo(bool b, int n) {
+  auto x = b ? 1 : 2;
+  auto y = b ? "true" : "fls";
+  auto w = b ? "0" : "1";
+  auto a = b ? "tluo" : "dalse";
+  auto z = b ? "true" : "false";
+  const char* z1 = b ? "true" : "false";
+  const char* z2 = (b ? "true" : "false");
+  std::string z3 = b ? "true" : "false";
+  std::string z4 = b ? "false" : "true";
+  std::string z5 = n == 5 ? "false" : "true";
+}
