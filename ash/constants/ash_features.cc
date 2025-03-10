4f5a2ad0e92099842682444212ba0b078bd81474
Elijah Hewer <hewer@chromium.org>
2025-03-10 16:30:46 -0700

    Sunfish: Add feature flag for Lens Web copy text
    Add a new feature flag to hide the Lens Web API-based copy text
    implementation that uses the /qfmetadata endpoint.
    
    Bug: 401586625
    Change-Id: I96928249a5c0609c234f3a6b3adec6bb950b1915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334769
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430555}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2728,6 +2728,12 @@ BASE_FEATURE(kSunfishLensWeb,
              "SunfishLensWeb",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Changes the Sunfish copy text functionality to use the Lens Web API instead
+// of the Chromnient-like query.
+BASE_FEATURE(kSunfishLensWebCopyText,
+             "SunfishLensWebCopyText",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enable the suspend state machine to better handle suspend accelerators.
 BASE_FEATURE(kSuspendStateMachine,
              "SuspendStateMachine",
@@ -4426,6 +4432,11 @@ bool IsSunfishFeatureEnabled() {
 bool IsSunfishLensWebEnabled() {
   return IsSunfishFeatureEnabled() &&
          base::FeatureList::IsEnabled(kSunfishLensWeb);
+}
+
+bool IsSunfishLensWebCopyTextEnabled() {
+  return IsSunfishLensWebEnabled() &&
+         base::FeatureList::IsEnabled(kSunfishLensWebCopyText);
 }
 
 bool IsSuspendStateMachineEnabled() {
