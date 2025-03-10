383853dfb2f0fb4841f3f9668a924099a6eab798
Elmehdi Rahmaoui <erahmaoui@google.com>
2025-03-10 03:39:39 -0700

    [iOS] New share extension vc placeholder
    Bug: 397379313, 398803565
    Change-Id: Ib472eb3a6f62c14d4a960f69cb7bd5b9c6fded07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329977
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430157}

---
+++ ios/chrome/share_extension/extended_share_view_controller.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_SHARE_EXTENSION_EXTENDED_SHARE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_SHARE_EXTENSION_EXTENDED_SHARE_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+// A view controller that implements Chrome's primary view, it will act as the
+// `NSExtensionPrincipalClass`.
+@interface ExtendedShareViewController : UIViewController
+
+@end
+
+#endif  // IOS_CHROME_SHARE_EXTENSION_EXTENDED_SHARE_VIEW_CONTROLLER_H_
