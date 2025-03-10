08ad53e07a7c76201f5e9ead4745755ee525875f
Akihiro Ota <akihiroota@chromium.org>
2025-03-10 14:45:37 -0700

    [Accessibility] Ensure TTS speaker names are omitted from UI
    The bug details an issue where internal TTS speaker names were being
    exposed through the settings UI. This change ensures that speaker names
    are removed from user-visible strings.
    
    Bug: 349537799
    Change-Id: Ied908143fa97adeb3aedf9ba80bdec4c24a552a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309118
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430499}

--- chrome/browser/ui/webui/ash/settings/pages/a11y/tts_handler.cc
+++ chrome/browser/ui/webui/ash/settings/pages/a11y/tts_handler.cc
@@ -123,6 +123,7 @@ void TtsHandler::OnVoicesChanged() {
     response.Set("fullLanguageCode", voice.lang);
     response.Set("languageScore", language_score);
     response.Set("extensionId", voice.engine_id);
+    response.Set("displayName", voice.name);
     responses.Append(std::move(response));
   }
   AllowJavascript();
