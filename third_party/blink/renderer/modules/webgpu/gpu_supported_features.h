181a2e1676f844a04f82204dc286065e9edf369b
Shrek Shao <shrekshao@google.com>
2025-03-10 13:15:02 -0700

    WebGPU: Add Feature: core-features-and-limits
    Reflecting https://github.com/gpuweb/gpuweb/pull/5036
    
    Bug: 395855517
    Change-Id: I7d9164a6a0825c2636195dce32e41da61b839136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6307124
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Shrek Shao <shrekshao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430440}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_features.h
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_features.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_feature_name.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_sync_iterator_gpu_supported_features.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/graphics/gpu/webgpu_cpp.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 
 namespace blink {
@@ -21,8 +22,12 @@ class GPUSupportedFeatures : public ScriptWrappable,
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  static std::optional<V8GPUFeatureName::Enum> ToV8FeatureNameEnum(
+      wgpu::FeatureName f);
+
   GPUSupportedFeatures();
-  explicit GPUSupportedFeatures(const Vector<V8GPUFeatureName>& feature_names);
+  explicit GPUSupportedFeatures(
+      const wgpu::SupportedFeatures& supported_features);
 
   void AddFeatureName(const V8GPUFeatureName feature_name);
 
