358548651fe729335c9e1bc352b466516a86b98d
Rupert Wiser <bewise@chromium.org>
2025-03-10 06:30:16 -0700

    🌱 Disable flaking testViewportFitDefault test
    Bug: 402051996
    Change-Id: I4cb9736ecbffd6fdb5571cb7c4084ba09bcf8f07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333422
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Auto-Submit: Rupert Wiser <bewise@chromium.org>
    Owners-Override: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430196}

--- chrome/android/javatests/src/org/chromium/chrome/browser/display_cutout/DisplayCutoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/display_cutout/DisplayCutoutTest.java
@@ -134,6 +134,8 @@ public class DisplayCutoutTest {
     /** Test that no safe area is applied when we have no viewport fit. */
     @Test
     @LargeTest
+    @DisableIf.Build(supported_abis_includes = "armeabi-v7a", message = "https://crbug.com/402051996")
+    @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "https://crbug.com/402051996")
     public void testViewportFitDefault() throws TimeoutException {
         mTestRule.enterFullscreen();
         mTestRule.setViewportFit(DisplayCutoutTestRule.VIEWPORT_FIT_COVER);
