5ee0cd27e4781961e08f58af95e5501ddb336874
Martin Kong <martinkong@google.com>
2025-03-11 06:26:30 -0700

    Add @NullMarked to //c/b/android/metrics:java
    Bug: 389129271
    Change-Id: Ib06cf94f9616918c11e083b3acb15ff210d54c5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6340867
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Martin Kong <martinkong@google.com>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430804}

--- chrome/browser/android/metrics/java/src/org/chromium/chrome/browser/metrics/AppUpdateInfoUtils.java
+++ chrome/browser/android/metrics/java/src/org/chromium/chrome/browser/metrics/AppUpdateInfoUtils.java
@@ -4,13 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.metrics;
 
-import androidx.annotation.Nullable;
-
 import org.jni_zero.CalledByNative;
 
 import org.chromium.base.ServiceLoaderUtil;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 
 /** Utility class for native to request AppUpdateInfo */
+@NullMarked
 public class AppUpdateInfoUtils {
     private static @Nullable AppUpdateInfoDelegate sDelegate =
             ServiceLoaderUtil.maybeCreate(AppUpdateInfoDelegate.class);
