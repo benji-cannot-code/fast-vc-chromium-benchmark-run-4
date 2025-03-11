b65b8cb894cc4c4e12cc945380a2324c704490c7
Lei Zhang <thestig@chromium.org>
2025-03-11 11:08:23 -0700

    Change base::VlogInfo ctor parameter to a reference
    Since Google C++ style guide allows non-const reference parameters, use
    it in the base::VlogInfo ctor to implicitly require the caller the pass
    in a non-null argument.
    
    Change-Id: Ib2586f2d6f9c154d6c000e5fc8d270ea8ea3ae9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6341692
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431021}

--- base/vlog.cc
+++ base/vlog.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <utility>
 
-#include "base/check_op.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
@@ -59,11 +58,9 @@ std::vector<VlogInfo::VmodulePattern> VlogInfo::ParseVmoduleLevels(
 
 VlogInfo::VlogInfo(const std::string& v_switch,
                    const std::string& vmodule_switch,
-                   int* min_log_level)
+                   int& min_log_level)
     : vmodule_levels_(ParseVmoduleLevels(vmodule_switch)),
       min_log_level_(min_log_level) {
-  DCHECK_NE(min_log_level, nullptr);
-
   int vlog_level = 0;
   if (!v_switch.empty()) {
     if (base::StringToInt(v_switch, &vlog_level)) {
@@ -123,7 +120,7 @@ int VlogInfo::GetMaxVlogLevel() const {
 }
 
 VlogInfo::VlogInfo(std::vector<VmodulePattern> vmodule_levels,
-                   int* min_log_level)
+                   int& min_log_level)
     : vmodule_levels_(std::move(vmodule_levels)),
       min_log_level_(min_log_level) {}
 
@@ -133,7 +130,7 @@ VlogInfo* VlogInfo::WithSwitches(const std::string& vmodule_switch) const {
       ParseVmoduleLevels(vmodule_switch);
   vmodule_levels.insert(vmodule_levels.end(), additional_vmodule_levels.begin(),
                         additional_vmodule_levels.end());
-  return new VlogInfo(std::move(vmodule_levels), min_log_level_);
+  return new VlogInfo(std::move(vmodule_levels), *min_log_level_);
 }
 
 bool MatchVlogPattern(std::string_view string, std::string_view vlog_pattern) {
