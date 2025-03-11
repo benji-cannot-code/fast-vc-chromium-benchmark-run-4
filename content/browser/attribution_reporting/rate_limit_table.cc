043b9670f578b303339005e29d6aa8eff5807489
Nan Lin <linnan@chromium.org>
2025-03-11 13:03:09 -0700

    Check with source time for rate-limits on trigger registrations
    https://github.com/WICG/attribution-reporting-api/pull/1502
    
    Change-Id: If857a5c4ce86e9aa40e8c7faf3b1b7086ad66ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317251
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431087}

--- content/browser/attribution_reporting/rate_limit_table.cc
+++ content/browser/attribution_reporting/rate_limit_table.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/containers/flat_tree.h"
 #include "base/containers/span.h"
+#include "base/feature_list.h"
 #include "base/memory/raw_ref.h"
 #include "base/notreached.h"
 #include "base/time/time.h"
@@ -52,6 +53,11 @@ namespace content {
 
 namespace {
 
+// Kill switch.
+BASE_FEATURE(kAttributionReportingRateLimitCheckSourceTime,
+             "AttributionReportingRateLimitCheckSourceTime",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 bool IsAttribution(RateLimitTable::Scope scope) {
   switch (scope) {
     case RateLimitTable::Scope::kSource:
@@ -67,7 +73,9 @@ bool IsAttribution(RateLimitTable::Scope scope) {
 }  // namespace
 
 RateLimitTable::RateLimitTable(const AttributionResolverDelegate* delegate)
-    : delegate_(
+    : rate_limit_check_source_time_enabled_(base::FeatureList::IsEnabled(
+          kAttributionReportingRateLimitCheckSourceTime)),
+      delegate_(
           raw_ref<const AttributionResolverDelegate>::from_ptr(delegate)) {}
 
 RateLimitTable::~RateLimitTable() {
@@ -302,7 +310,12 @@ RateLimitResult RateLimitTable::AttributionAllowedForAttributionLimit(
   DCHECK_GT(rate_limits.time_window, base::TimeDelta());
   DCHECK_GT(rate_limits.max_attributions, 0);
 
-  base::Time min_timestamp = attribution_info.time - rate_limits.time_window;
+  // Note that we intentionally use source time to bound the limit for any
+  // source, which is consistent with the time stored in `AddRateLimit()`.
+  base::Time min_timestamp =
+      (rate_limit_check_source_time_enabled_ ? source.source_time()
+                                             : attribution_info.time) -
+      rate_limits.time_window;
 
   sql::Statement statement(db->GetCachedStatement(
       SQL_FROM_HERE, attribution_queries::kRateLimitAttributionAllowedSql));
@@ -682,8 +695,12 @@ RateLimitResult RateLimitTable::AttributionAllowedForReportingOriginLimit(
     const AttributionInfo& attribution_info,
     const StoredSource& source) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  // Note that we intentionally use source time to bound the limit for any
+  // source, which is consistent with the time stored in `AddRateLimit()`.
   return AllowedForReportingOriginLimit(
-      db, /*is_source=*/false, source.common_info(), attribution_info.time,
+      db, /*is_source=*/false, source.common_info(),
+      rate_limit_check_source_time_enabled_ ? source.source_time()
+                                            : attribution_info.time,
       base::span_from_ref(net::SchemefulSite(attribution_info.context_origin)));
 }
 
