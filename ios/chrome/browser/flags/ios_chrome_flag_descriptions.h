708507ff031f6a542e9e206f775f979fb8df81cf
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-03-11 14:19:48 -0700

    [iOS][Enhanced Calendar] Enhanced calendar feature flag
    Fixed: 402479763
    Change-Id: I31e47d9976d640a7cda8dbf1696fd7303e66c951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6343493
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431145}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -268,6 +268,9 @@ extern const char kEnableVariationsGoogleGroupFilteringDescription[];
 extern const char kEnableWebChannelsName[];
 extern const char kEnableWebChannelsDescription[];
 
+extern const char kEnhancedCalendarName[];
+extern const char kEnhancedCalendarDescription[];
+
 extern const char kEnhancedSafeBrowsingPromoName[];
 extern const char kEnhancedSafeBrowsingPromoDescription[];
 
---
+++ ios/chrome/browser/intelligence/features/features.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INTELLIGENCE_FEATURES_FEATURES_H_
+#define IOS_CHROME_BROWSER_INTELLIGENCE_FEATURES_FEATURES_H_
+
+#import "base/feature_list.h"
+
+// Feature flag controlling whether enhanced calendar is enabled.
+BASE_DECLARE_FEATURE(kEnhancedCalendar);
+
+// Returns true if enhanced calendar is enabled.
+bool IsEnhancedCalendarEnabled();
+
+#endif  // IOS_CHROME_BROWSER_INTELLIGENCE_FEATURES_FEATURES_H_
