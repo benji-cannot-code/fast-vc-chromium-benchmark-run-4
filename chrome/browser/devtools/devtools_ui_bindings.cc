759e7ab3a38b6e7fa082e256b03fd7482f3800ef
Benedikt Meurer <bmeurer@chromium.org>
2025-03-11 02:51:38 -0700

    [DevTools] Pass release channel information via host config.
    Extend the `GetHostConfig()` API to also pass the release channel if
    it's a branded build.
    
    Bug: 402171599
    Change-Id: I36a72afd13e72cca8c4de88c5af2bd0e94136def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6343130
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430721}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/uuid.h"
 #include "base/values.h"
+#include "base/version_info/channel.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/devtools/devtools_file_watcher.h"
@@ -52,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/hats/hats_service.h"
 #include "chrome/browser/ui/hats/hats_service_factory.h"
+#include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/chrome_manifest_url_handlers.h"
 #include "chrome/common/pref_names.h"
@@ -1655,6 +1657,11 @@ void DevToolsUIBindings::GetHostConfig(DispatchCallback callback) {
   aida_availability.Set("enterprisePolicyValue",
                         static_cast<int>(availability.enterprise_policy_value));
   response_dict.Set("aidaAvailability", std::move(aida_availability));
+
+  version_info::Channel channel = chrome::GetChannel();
+  if (channel != version_info::Channel::UNKNOWN) {
+    response_dict.Set("channel", version_info::GetChannelString(channel));
+  }
 
   base::Value::Dict console_insights_dict;
   console_insights_dict.Set(
