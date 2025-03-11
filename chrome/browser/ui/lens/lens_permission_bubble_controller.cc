b640ee4eeafcf0770c1231f6766fc841424a1be2
Kaan Alsan <alsan@chromium.org>
2025-03-11 08:40:09 -0700

    Make LensPermissionBubbleController track TabInterface instead of BWI
    LensPermissionBubbleController was tracking a BWI, which led to a
    possible dangling ptr (see crash linked in bug) when the tab owning the
    controller is moved to a different window. Also, the controller wasn't
    cleaning up its widget when the tab is detached.
    
    This CL updates it to track a TabInterface instead of a BWI, and cleanup
    its widget respectively when the tab is being deleted.
    
    Change-Id: I1f3bba7548b74f56a95e3aa6f3929addc6627e67
    Bug: 402252107
    Fixed: 402252107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334416
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430891}

--- chrome/browser/ui/lens/lens_permission_bubble_controller.cc
+++ chrome/browser/ui/lens/lens_permission_bubble_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/lens/lens_permission_bubble_controller.h"
 
+#include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/lens/lens_overlay_theme_utils.h"
+#include "chrome/browser/ui/tabs/public/tab_interface.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
@@ -41,12 +43,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace lens {
 
 LensPermissionBubbleController::LensPermissionBubbleController(
-    BrowserWindowInterface* browser_window_interface,
+    tabs::TabInterface& tab_interface,
     PrefService* pref_service,
     LensOverlayInvocationSource invocation_source)
     : invocation_source_(invocation_source),
-      browser_window_interface_(browser_window_interface),
-      pref_service_(pref_service) {}
+      tab_interface_(tab_interface),
+      pref_service_(pref_service) {
+  tab_will_detach_subscription_ = tab_interface_->RegisterWillDetach(
+      base::BindRepeating(&LensPermissionBubbleController::TabWillDetach,
+                          base::Unretained(this)));
+}
 
 LensPermissionBubbleController::~LensPermissionBubbleController() {
   if (HasOpenDialogWidget()) {
@@ -156,7 +162,7 @@ void LensPermissionBubbleController::OnHelpCenterLinkClicked(
     const ui::Event& event) {
   RecordPermissionUserAction(LensPermissionUserAction::kLinkOpened,
                              invocation_source_);
-  browser_window_interface_->OpenGURL(
+  tab_interface_->GetBrowserWindowInterface()->OpenGURL(
       GURL(lens::features::GetLensOverlayHelpCenterURL()),
       ui::DispositionFromEventFlags(event.flags(),
                                     WindowOpenDisposition::NEW_BACKGROUND_TAB));
@@ -208,6 +214,16 @@ void LensPermissionBubbleController::OnPermissionPreferenceUpdated(
     }
     pref_observer_.Reset();
     callback.Run();
+  }
+}
+
+void LensPermissionBubbleController::TabWillDetach(
+    tabs::TabInterface* tab,
+    tabs::TabInterface::DetachReason reason) {
+  if (reason == tabs::TabInterface::DetachReason::kDelete &&
+      HasOpenDialogWidget()) {
+    dialog_widget_->Close();
+    dialog_widget_ = nullptr;
   }
 }
 
