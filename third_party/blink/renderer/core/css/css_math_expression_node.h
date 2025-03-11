58323d38b44f7356d48e2f57049f14c2e6393c3a
Rune Lillesveen <futhark@chromium.org>
2025-03-11 07:15:53 -0700

    Support sibling-index()/sibling-count() in container queries
    The tree-counting functions should evaluate against the container they
    query, like font-relative units do.
    
    Return the container element as the Element for the CSSLengthResolver
    and allow ComputeValueInCanonicalUnit() for sibling functions.
    
    Bug: 40282719
    Change-Id: I2521b69692b2451751f8ab3a544d675362f13130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6336114
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430833}

--- third_party/blink/renderer/core/css/css_math_expression_node.h
+++ third_party/blink/renderer/core/css/css_math_expression_node.h
@@ -961,9 +961,7 @@ class CORE_EXPORT CSSMathExpressionSiblingFunction final
     return std::nullopt;
   }
   std::optional<double> ComputeValueInCanonicalUnit(
-      const CSSLengthResolver& length_resolver) const final {
-    NOTREACHED();
-  }
+      const CSSLengthResolver& length_resolver) const final;
   std::optional<PixelsAndPercent> ToPixelsAndPercent(
       const CSSLengthResolver&) const final {
     return std::nullopt;
