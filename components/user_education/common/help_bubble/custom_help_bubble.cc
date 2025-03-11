3c6df529a18c63b0d80889d442798a684991ecfe
Dana Fried <dfried@chromium.org>
2025-03-11 10:21:40 -0700

    [User Education] Add custom help bubble logic for Views
    This allows easy wrapping of a help bubble that is not actually a
    HelpBubble, as long as it's a bubble dialog view and supports a
    particular interface.
    
    Bug: 399398690
    Change-Id: I014fb79495c4f3b6594f959f99704c7abf9ed8d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311301
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430975}

---
+++ components/user_education/common/help_bubble/custom_help_bubble.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/user_education/common/help_bubble/custom_help_bubble.h"
+
+#include "base/logging.h"
+
+namespace user_education {
+
+CustomHelpBubbleUi::CustomHelpBubbleUi() = default;
+CustomHelpBubbleUi::~CustomHelpBubbleUi() = default;
+
+base::CallbackListSubscription CustomHelpBubbleUi::AddUserActionCallback(
+    UserActionCallback callback) {
+  return user_action_callbacks_.Add(std::move(callback));
+}
+
+void CustomHelpBubbleUi::NotifyUserAction(UserAction user_action) {
+  user_action_callbacks_.Notify(user_action);
+}
+
+base::WeakPtr<CustomHelpBubbleUi> CustomHelpBubbleUi::AsWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
+}
+
+CustomHelpBubble::CustomHelpBubble(CustomHelpBubbleUi& bubble)
+    : bubble_(bubble.AsWeakPtr()) {}
+CustomHelpBubble::~CustomHelpBubble() = default;
+
+}  // namespace user_education
--- components/user_education/views/help_bubble_views.h
+++ components/user_education/views/help_bubble_views.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_USER_EDUCATION_VIEWS_HELP_BUBBLE_VIEWS_H_
 #define COMPONENTS_USER_EDUCATION_VIEWS_HELP_BUBBLE_VIEWS_H_
 
+#include <concepts>
+
 #include "base/callback_list.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
+#include "components/user_education/common/help_bubble/custom_help_bubble.h"
 #include "components/user_education/common/help_bubble/help_bubble.h"
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/base/interaction/element_tracker.h"
@@ -91,6 +94,17 @@ class HelpBubbleViews : public HelpBubble,
   base::CallbackListSubscription anchor_bounds_changed_subscription_;
 
   base::WeakPtrFactory<HelpBubbleViews> weak_ptr_factory_{this};
+};
+
+// Help bubble that wraps a custom help bubble view.
+class CustomHelpBubbleViews : public HelpBubbleViews, public CustomHelpBubble {
+ public:
+  template <typename T>
+    requires(std::derived_from<T, views::BubbleDialogDelegateView> &&
+             std::derived_from<T, CustomHelpBubbleUi>)
+  CustomHelpBubbleViews(T* bubble, ui::TrackedElement* anchor_element)
+      : HelpBubbleViews(bubble, anchor_element), CustomHelpBubble(*bubble) {}
+  ~CustomHelpBubbleViews() override = default;
 };
 
 }  // namespace user_education
