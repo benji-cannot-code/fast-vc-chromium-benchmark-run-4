a1c53cc77aa85a0e35faf2dea50535fb7e1c9933
Vlad Korotkevich <vkorotkevich@google.com>
2025-03-11 10:02:11 -0700

    Lay out foundation for reload button component
    Adding MVC components for reload button that are empty to make future changes more granular and easier to review.
    
    New component for reload button is created for reuse purposes,
    because tablet toolbar and PWA web app header coordinator are
    going to share back and reload logic.
    
    Bug: https://b.corp.google.com/issues/401525560
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Change-Id: Iec2b611067bbae1cd637a759a8446677994761a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6335526
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Vlad Korotkevich <vkorotkevich@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430951}

---
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/reload_button/ReloadButtonCoordinator.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.toolbar.reload_button;
+
+import android.widget.ImageButton;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
+
+/**
+ * Root component for the reload button. Exposes public API to change button's state and allows
+ * consumers to react to button state changes.
+ */
+@NullMarked
+public class ReloadButtonCoordinator {
+    private final ReloadButtonMediator mMediator;
+
+    public ReloadButtonCoordinator(ImageButton view) {
+        final var model = new PropertyModel.Builder().build();
+        mMediator = new ReloadButtonMediator(model);
+        PropertyModelChangeProcessor.create(model, view, ReloadButtonViewBinder::bind);
+    }
+
+    public void destroy() {
+        mMediator.destroy();
+    }
+}
