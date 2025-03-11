b2e65441de9481fc893def33cd8cc92c3c16297b
kenoss <kenoss@chromium.org>
2025-03-10 18:13:46 -0700

    PrefetchNewWaitLoop: Rename PrefetchMatchResolver2 to PrefetchMatchResolver
    Bug: 353490734
    Change-Id: Id0bf2480a2b817f547be3910540106f163444bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332157
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430593}

--- content/browser/preloading/prefetch/prefetch_container.h
+++ content/browser/preloading/prefetch/prefetch_container.h
@@ -226,7 +226,7 @@ class CONTENT_EXPORT PrefetchContainer {
   // Each callback is called at most once in the lifecycle of a container.
   //
   // Be careful about using this. This is designed only for
-  // `PrefetchMatchResolver2`.
+  // `PrefetchMatchResolver`.
   class Observer : public base::CheckedObserver {
    public:
     // Called at the head of dtor.
@@ -481,7 +481,7 @@ class CONTENT_EXPORT PrefetchContainer {
     // This received non redirect header and is not expired.
     //
     // Note that it needs more checks to serve, e.g. cookie check. See also e.g.
-    // `PrefetchMatchResolver2::OnDeterminedHead()`.
+    // `PrefetchMatchResolver::OnDeterminedHead()`.
     kServable,
 
     // Not other states.
@@ -573,8 +573,8 @@ class CONTENT_EXPORT PrefetchContainer {
   //
   // - Roughly speaking, when non-redirect header received and
   //   `PrefetchService`/`PrefetchContainer` detected cookies change of the head
-  //   of redirect chain. `PrefetchMatchResolver`/`PrefetchMatchResolver2`
-  //   propagates it to other waiting prefetches as they share domain.
+  //   of redirect chain. `PrefetchMatchResolver` propagates it to other waiting
+  //   prefetches as they share domain.
   // - When `PrefetchURLLoaderInterceptor::MaybeCreateLoader()` handles
   //   redirects in the serving prefetch.
   void OnDetectedCookiesChange();
@@ -710,12 +710,12 @@ class CONTENT_EXPORT PrefetchContainer {
   // Records metrics when serving result is determined.
   //
   // This is eventually called once for every `PrefetchContainer` put in
-  // `PrefetchMatchResolver2::candidates_`, i.e. those potentially matching
+  // `PrefetchMatchResolver::candidates_`, i.e. those potentially matching
   // and expected to become servable at the head of
-  // `PrefetchMatchResolver2::FindPrefetch()`.
+  // `PrefetchMatchResolver::FindPrefetch()`.
   //
   // This can be called multiple times, because this can be called for multiple
-  // `PrefetchMatchResolver2`s.
+  // `PrefetchMatchResolver`s.
   void OnUnregisterCandidate(const GURL& navigated_url,
                              bool is_served,
                              std::optional<base::TimeDelta> blocked_duration);
