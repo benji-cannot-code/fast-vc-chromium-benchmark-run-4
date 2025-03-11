1f82bc4d6713ae027b0fd77be49a9c7d113be72f
Stepan Khapugin <stkhapugin@chromium.org>
2025-03-11 05:20:21 -0700

    [iOS] CHECK for Profile in [ChromeCoordinator isOffTheRecord]
    Coordinators normally have an associated profile, except the root tab
    grid coordinator. The profile typically remains valid until -stop is
    called. Without a profile, a check for "isOffTheRecord" does not make
    sense and probably happens as a result of a programming error (likely
    code executed after -stop).
    
    Add a CHECK and update documentation to this effect.
    
    Bug: 401523912
    Change-Id: Ie3ad41ac91b2af521f70b5532f32ce681b31867e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6336204
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430772}

--- ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.mm
+++ ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.mm
@@ -47,9 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (BOOL)isOffTheRecord {
-  if (!self.profile) {
-    return NO;
-  }
+  CHECK(self.profile);
   return self.profile->IsOffTheRecord();
 }
 
