95c6101900b03435b9b01addc18ac18a1c5b8824
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-03-11 06:27:38 -0700

    Remove unused include in paint_vector_icon_fuzztest.cc
    Bug: None
    Change-Id: Ib847e7741514df025d3d04775ee310d26e593f9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6343174
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430806}

--- testing/libfuzzer/fuzzers/paint_vector_icon_fuzztest.cc
+++ testing/libfuzzer/fuzzers/paint_vector_icon_fuzztest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "skia/ext/switches.h"
-#include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/canvas.h"
