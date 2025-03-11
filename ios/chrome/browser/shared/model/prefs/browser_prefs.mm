c566e83e6060f95f64cc4054792bddc35a40e565
Marc Treib <treib@chromium.org>
2025-03-11 06:56:36 -0700

    [iOS] Migrate pref ios.magic_stack_segmentation.shop_card_freshness
    ...from LocalState to Profile.
    All the other MagicStack Segmentation ranking prefs were already
    migrated, at just about the same time as this one was introduced.
    
    This pref has never been written to yet (so far there's only a single
    read access), so there's no need to migrate any persisted value, nor to
    keep registering the no-longer-used LocalState version.
    
    Bug: 398173021
    Change-Id: Ie5585ca79e7768822c4639ad90f232c5dd7236f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6343093
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430824}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -503,12 +503,6 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   // Preference related to the tab pickup feature.
   registry->RegisterBooleanPref(prefs::kTabPickupEnabled, true);
 
-  // Pref used to store the number of impressions of the shop card
-  // module in the Home Surface since a shop card freshness signal.
-  // TODO(crbug.com/398173021): Migrate from LocalState to Profile.
-  registry->RegisterIntegerPref(
-      prefs::kIosMagicStackSegmentationShopCardImpressionsSinceFreshness, -1);
-
   // Preferences related to the new Safety Check Manager.
   registry->RegisterStringPref(
       prefs::kIosSafetyCheckManagerUpdateCheckResult,
@@ -933,6 +927,10 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterIntegerPref(
       prefs::kIosMagicStackSegmentationTabResumptionImpressionsSinceFreshness,
       -1);
+  // Pref used to store the number of impressions of the shop card
+  // module in the Home Surface since a shop card freshness signal.
+  registry->RegisterIntegerPref(
+      prefs::kIosMagicStackSegmentationShopCardImpressionsSinceFreshness, -1);
 
   registry->RegisterIntegerPref(
       lens::prefs::kLensOverlaySettings,
