3cab057f6293a1395f55ce09d617082704056f4e
Amelie Schneider <amelies@google.com>
2025-03-11 10:14:48 -0700

    Remove browser test dependency in signin UI tests
    https://chromium-review.googlesource.com/c/chromium/src/+/6291300
    falsely introduced a dependency on `//chrome/test:browser_tests` in
    signin UI tests. Add the necessary `test_support`s instead.
    
    `chrome/browser/ui/passwords/manage_passwords_test.cc` is moved to
    `//chrome/test:test_support`, which would cause a dependency cycle
    between `//chrome/browser/optimization_guide:test_support` and
    `//chrome/test:test_support`. In order to avoid this merge
    `optimization_guide:test_support` into `test:test_support`.
    
    Change-Id: Id20751a704609b23c4e914165d0b3611fc45aaa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333282
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430966}

--- chrome/browser/ui/passwords/manage_passwords_test.cc
+++ chrome/browser/ui/passwords/manage_passwords_test.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
-#include "chrome/browser/optimization_guide/mock_optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/password_manager/chrome_password_change_service.h"
