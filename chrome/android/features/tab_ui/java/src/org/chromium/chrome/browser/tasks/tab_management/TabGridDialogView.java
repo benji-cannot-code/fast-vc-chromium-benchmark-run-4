15409a90d7efced988f6c5d8fa40ec451ea05f6d
Madhav Pruthi <madhavpruthi@google.com>
2025-03-11 13:48:18 -0700

    [Clank BC25] Modify the corner radius for grid tab switcher
    Impl SS[Light]: https://screenshot.googleplex.com/9gcR3tFMVgZG3y4
    Animation Videos: https://photos.app.goo.gl/cxfoENDAdaA2efK6A
    
    Bug: 401271155
    Change-Id: Ic0223f2ea4301dd42140a2061cdf0e60fd6ad426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332416
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431120}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
@@ -9,6 +9,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
@@ -43,8 +44,10 @@ import org.chromium.base.Callback;
 import org.chromium.base.MathUtils;
 import org.chromium.base.ResettersForTesting;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.hub.RoundedCornerAnimatorUtil;
 import org.chromium.chrome.browser.tab_ui.TabThumbnailView;
 import org.chromium.chrome.tab_ui.R;
+import org.chromium.components.browser_ui.widget.RoundedCornerImageView;
 import org.chromium.components.browser_ui.widget.scrim.ScrimManager;
 import org.chromium.components.browser_ui.widget.scrim.ScrimProperties;
 import org.chromium.components.browser_ui.widget.scrim.ScrimView;
@@ -57,6 +60,7 @@ import org.chromium.ui.widget.ButtonCompat;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -92,7 +96,7 @@ public class TabGridDialogView extends FrameLayout {
     private FrameLayout mAnimationClip;
     private FrameLayout mToolbarContainer;
     private FrameLayout mRecyclerViewContainer;
-    private View mBackgroundFrame;
+    private RoundedCornerImageView mBackgroundFrame;
     private View mAnimationCardView;
     private View mItemView;
     private View mUngroupBar;
@@ -615,6 +619,21 @@ public class TabGridDialogView extends FrameLayout {
                 .with(frameZoomOutScaleYAnimator)
                 .with(frameZoomOutScaleXAnimator);
 
+        int endRadius =
+                mContext.getResources().getDimensionPixelSize(R.dimen.tab_grid_dialog_bg_radius);
+        int[] endRadii = new int[4];
+        int[] startRadii = new int[4];
+
+        Arrays.fill(endRadii, endRadius);
+        Arrays.fill(startRadii, (int) (endRadius * cardScale));
+
+        // While background frame is zooming out, the corners scale down along with the animation.
+        final ValueAnimator frameZoomOutCornerAnimator =
+                RoundedCornerAnimatorUtil.createRoundedCornerAnimator(
+                        mBackgroundFrame, startRadii, endRadii);
+        frameZoomOutCornerAnimator.setDuration(DIALOG_ANIMATION_DURATION);
+        frameZoomOutCornerAnimator.setInterpolator(Interpolators.EMPHASIZED);
+
         // After the dialog showing animation starts, the original card in grid tab switcher fades
         // out.
         final ObjectAnimator tabFadeOutAnimator =
@@ -661,6 +680,7 @@ public class TabGridDialogView extends FrameLayout {
                 .play(cardZoomOutAnimatorSet)
                 .with(cardZoomOutAlphaAnimator)
                 .with(frameZoomOutAnimatorSet)
+                .with(frameZoomOutCornerAnimator)
                 .with(dialogZoomOutAnimatorSet)
                 .with(dialogZoomOutAlphaAnimator)
                 .with(tabFadeOutAnimator);
@@ -794,6 +814,13 @@ public class TabGridDialogView extends FrameLayout {
                     }
                 });
 
+        // While background frame is zooming in, the corners scale up along with the animation.
+        final ValueAnimator frameZoomInCornerAnimator =
+                RoundedCornerAnimatorUtil.createRoundedCornerAnimator(
+                        mBackgroundFrame, endRadii, startRadii);
+        frameZoomOutCornerAnimator.setDuration(DIALOG_ANIMATION_DURATION);
+        frameZoomOutCornerAnimator.setInterpolator(Interpolators.EMPHASIZED);
+
         // At the end of the dialog hiding animation, the original tab grid card fades in.
         final ObjectAnimator tabFadeInAnimator =
                 ObjectAnimator.ofFloat(mItemView, View.ALPHA, 0f, 1f);
@@ -806,6 +833,7 @@ public class TabGridDialogView extends FrameLayout {
                 .play(dialogZoomInAnimatorSet)
                 .with(dialogZoomInAlphaAnimator)
                 .with(frameZoomInAnimatorSet)
+                .with(frameZoomInCornerAnimator)
                 .with(cardZoomInAnimatorSet)
                 .with(cardZoomInAlphaAnimator)
                 .with(tabFadeInAnimator);
@@ -1061,7 +1089,7 @@ public class TabGridDialogView extends FrameLayout {
     void updateDialogContainerBackgroundColor(int backgroundColor) {
         mBackgroundDrawableColor = backgroundColor;
         DrawableCompat.setTint(mDialogContainerView.getBackground(), backgroundColor);
-        DrawableCompat.setTint(mBackgroundFrame.getBackground(), backgroundColor);
+        mBackgroundFrame.setRoundedFillColor(backgroundColor);
     }
 
     void updateHairlineColor(@ColorInt int hairlineColor) {
--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogViewBinderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogViewBinderTest.java
@@ -406,7 +406,7 @@ public class TabGridDialogViewBinderTest {
         // animations.
         mModel.set(TabGridDialogProperties.ANIMATION_SOURCE_VIEW, sourceView);
         assertEquals(
-                6,
+                7,
                 mTabGridDialogView.getShowDialogAnimationForTesting().getChildAnimations().size());
     }
 
