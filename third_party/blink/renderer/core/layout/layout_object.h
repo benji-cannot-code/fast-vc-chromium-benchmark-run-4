26f69cc9e52663f386ff23b12885e0d7e44ad84d
Morten Stenshorne <mstensho@chromium.org>
2025-03-11 07:31:01 -0700

    Move scroll marker group getters to LayoutBox.
    No need for it to be in LayoutObject, since scroll marker groups and
    scrollers are always LayoutBox or better.
    
    Also change the return type of GetScrollMarkerGroup() to LayoutBlock,
    since scroll marker groups are blockified.
    
    No behavior changes intended.
    
    Change-Id: Id4de828f00c1d716e665a8403782d3815508632c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330409
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430839}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -1654,8 +1654,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
   bool IsScrollMarker() const;
   bool IsScrollMarkerGroup() const;
   bool IsScrollMarkerGroupBefore() const;
-  LayoutObject* GetScrollMarkerGroup() const;
-  LayoutBlock* ScrollerFromScrollMarkerGroup() const;
 
   // Returns true if this object represents ::marker for the first SUMMARY
   // child of a DETAILS, and list-style-type is disclosure-*.
