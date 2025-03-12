5fc26c8415ba9b00193bb74ae20d3611d1849e9b
Muyao Xu <muyaoxu@google.com>
2025-03-12 16:22:31 -0700

    Revert "Extend MediaRouter.Cast* metrics expiry"
    This reverts commit 3bb1927402a81a463a123221770348fe3fa8d2a1.
    
    Reason for revert: Caused test failure: https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/49576/test-results?sortby=&groupby=
    
    Original change's description:
    > Extend MediaRouter.Cast* metrics expiry
    >
    > Extend metrics expiry and add myself as an owner for metrics with only
    > one owner, mfoltz@chromium.org, who no longer works on Cast.
    >
    > OBSOLETE_HISTOGRAM[MediaRouter.Cast.Feedback.Event]=This histogram is no longer needed.
    >
    > Bug: 398847549, 398847136
    > Change-Id: Ica67c913d92874ce4ac34d377be600e98a2e0948
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6324954
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Commit-Queue: Muyao Xu <muyaoxu@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1431648}
    
    Bug: 398847549, 398847136
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ice4c687fc8674dd22dbcc0d4dc74abb1039a1d7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6349404
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431817}

--- chrome/browser/resources/media_router/cast_feedback/cast_feedback_ui.ts
+++ chrome/browser/resources/media_router/cast_feedback/cast_feedback_ui.ts
@@ -28,10 +28,27 @@ export enum FeedbackType {
 }
 
 /**
+ * Keep in sync with MediaRouterCastFeedbackEvent in enums.xml.
+ */
+export enum FeedbackEvent {
+  OPENED = 0,
+  SENDING = 1,
+  RESENDING = 2,
+  SUCCEEDED = 3,
+  FAILED = 4,
+  MAX_VALUE = 4,
+}
+
+/**
  * See
  * https://docs.google.com/document/d/1c20VYdwpUPyBRQeAS0CMr6ahwWnb0s26gByomOwqDjk
  */
 export interface FeedbackUiBrowserProxy {
+  /**
+   * Records an event using Chrome Metrics.
+   */
+  recordEvent(event: FeedbackEvent): void;
+
   /**
    * Proxy for chrome.feedbackPrivate.sendFeedback().
    */
@@ -40,6 +57,12 @@ export interface FeedbackUiBrowserProxy {
 }
 
 export class FeedbackUiBrowserProxyImpl implements FeedbackUiBrowserProxy {
+  recordEvent(event: FeedbackEvent) {
+    chrome.send(
+        'metricsHandler:recordInHistogram',
+        ['MediaRouter.Cast.Feedback.Event', event, FeedbackEvent.MAX_VALUE]);
+  }
+
   sendFeedback(info: chrome.feedbackPrivate.FeedbackInfo) {
     return chrome.feedbackPrivate.sendFeedback(
         info, /*loadSystemInfo=*/ undefined, /*formOpenTime=*/ undefined);
@@ -136,6 +159,8 @@ export class CastFeedbackUiElement extends CrLitElement {
     chrome.feedbackPrivate.getUserEmail(email => {
       this.userEmail_ = email;
     });
+
+    this.browserProxy_.recordEvent(FeedbackEvent.OPENED);
   }
 
   override willUpdate(changedProperties: PropertyValues<this>) {
@@ -226,7 +251,7 @@ export class CastFeedbackUiElement extends CrLitElement {
       };
     }
 
-    this.updateSendDialog_('sending', false);
+    this.updateSendDialog_(FeedbackEvent.SENDING, 'sending', false);
     this.$.sendDialog.showModal();
     this.trySendFeedback_(feedback, 0, 0);
   }
@@ -242,15 +267,15 @@ export class CastFeedbackUiElement extends CrLitElement {
       this.browserProxy_.sendFeedback(feedback).then(result => {
         if (result.status === chrome.feedbackPrivate.Status.SUCCESS) {
           this.feedbackSent = true;
-          this.updateSendDialog_('sendSuccess', true);
+          this.updateSendDialog_(FeedbackEvent.SUCCEEDED, 'sendSuccess', true);
         } else if (failureCount < this.maxResendAttempts) {
-          this.updateSendDialog_('resending', false);
+          this.updateSendDialog_(FeedbackEvent.RESENDING, 'resending', false);
           const sendDuration = Date.now() - sendStartTime;
           this.trySendFeedback_(
               feedback, failureCount + 1,
               Math.max(0, this.resendDelayMs - sendDuration));
         } else {
-          this.updateSendDialog_('sendFail', true);
+          this.updateSendDialog_(FeedbackEvent.FAILED, 'sendFail', true);
         }
       });
     }, delayMs);
@@ -259,7 +284,9 @@ export class CastFeedbackUiElement extends CrLitElement {
   /**
    * Updates the status of the "send" dialog and records the event.
    */
-  private updateSendDialog_(stringKey: string, isInteractive: boolean) {
+  private updateSendDialog_(
+      event: FeedbackEvent, stringKey: string, isInteractive: boolean) {
+    this.browserProxy_.recordEvent(event);
     this.sendDialogText_ = loadTimeData.getString(stringKey);
     this.sendDialogIsInteractive_ = isInteractive;
   }
