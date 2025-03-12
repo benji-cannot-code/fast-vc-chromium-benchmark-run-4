812644bbc880fad5f618eee35a2ac8fb9b93113f
Brandon Wylie <wylieb@chromium.org>
2025-03-11 18:24:08 -0700

    [declutter] Adjust IPH triggering to be less aggressive
    Pinpoint has identified a regression caused by enabling declutter iph
    by default. When digging into it, I noticed that the iph feature
    was misconfigured to trigger any number of times during a session.
    While in the code, also adding more guardrails to ensure the
    regression is fixed/landed smoothly, although it may be overkill.
    
    Bug: 397992504
    Change-Id: Ic70825d58f56f40f96b49422b4858cec968c5597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344334
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431279}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToggleTabStackButtonCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToggleTabStackButtonCoordinator.java
@@ -64,6 +64,7 @@ public class ToggleTabStackButtonCoordinator {
     private @Nullable Runnable mArchivedTabsIphShownCallback;
     private @Nullable Runnable mArchivedTabsIphDismissedCallback;
     private @Nullable Callback<Integer> mArchivedTabCountObserver = this::maybeShowDeclutterIph;
+    private boolean mAlreadyRequestedDeclutterIph;
 
     /**
      * @param context The Android context used for various view operations.
@@ -318,8 +319,9 @@ public class ToggleTabStackButtonCoordinator {
     private void maybeShowDeclutterIph(int tabCount) {
         if (!ChromeFeatureList.sAndroidTabDeclutter.isEnabled()) return;
         if (mIsIncognitoSupplier.get()) return;
+        if (mAlreadyRequestedDeclutterIph) return;
         if (tabCount == 0) return;
-
+        mAlreadyRequestedDeclutterIph = true;
         HighlightParams params = new HighlightParams(HighlightShape.CIRCLE);
         params.setBoundsRespectPadding(true);
         mUserEducationHelper.requestShowIph(
