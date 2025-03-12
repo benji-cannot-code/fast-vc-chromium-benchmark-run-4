c0ab1432864e0b055cda5bdd887a20f7dea03bdc
Christine Ying <chrstne@google.com>
2025-03-12 08:28:40 -0700

    [N&C] Add migration code to browser prefs
    Write path code added in https://chromium-review.googlesource.com/c/chromium/src/+/6330956
    
    Change-Id: I01f358fe27d9a1e0cdf4a8bab9e8d4271f0efd50
    Bug: 392088228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6335980
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431523}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -153,6 +153,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
+#include "components/privacy_sandbox/privacy_sandbox_notice_storage.h"
 #include "components/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "components/privacy_sandbox/tpcd_pref_names.h"
 #include "components/proxy_config/pref_proxy_config_tracker_impl.h"
@@ -2469,6 +2470,9 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
 
   // BEGIN_MIGRATE_OBSOLETE_PROFILE_PREFS
   // Please don't delete the preceding line. It is used by PRESUBMIT.py.
+
+  privacy_sandbox::PrivacySandboxNoticeStorage::UpdateNoticeSchemaV2(
+      profile_prefs);
 
   // Check MigrateDeprecatedAutofillPrefs() to see if this is safe to remove.
   autofill::prefs::MigrateDeprecatedAutofillPrefs(profile_prefs);
