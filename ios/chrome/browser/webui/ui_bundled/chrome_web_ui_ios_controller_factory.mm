0d0fa7148eae5e4dce744f5e9aaf9ec7153d7fe0
Gauthier Ambard <gambard@chromium.org>
2025-03-12 06:38:40 -0700

    [iOS] Support chrome://data-sharing-internals
    Add the iOS implementation of the debug page.
    
    Fixed: 401099811
    Change-Id: Ibec3686d34a17d0ffdf4743bccbf59bc50906d22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330149
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431473}

--- ios/chrome/browser/webui/ui_bundled/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/webui/ui_bundled/chrome_web_ui_ios_controller_factory.mm
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/webui/ui_bundled/autofill_and_password_manager_internals/password_manager_internals_ui_ios.h"
 #import "ios/chrome/browser/webui/ui_bundled/chrome_urls/chrome_urls_ui.h"
 #import "ios/chrome/browser/webui/ui_bundled/crashes_ui.h"
+#import "ios/chrome/browser/webui/ui_bundled/data_sharing_internals/data_sharing_internals_ui.h"
 #import "ios/chrome/browser/webui/ui_bundled/download_internals_ui.h"
 #import "ios/chrome/browser/webui/ui_bundled/flags_ui.h"
 #import "ios/chrome/browser/webui/ui_bundled/gcm/gcm_internals_ui.h"
@@ -123,6 +124,9 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(const GURL& url) {
   }
   if (url_host == kChromeUICrashesHost) {
     return &NewWebUIIOS<CrashesUI>;
+  }
+  if (url_host == kChromeUIDataSharingInternalsHost) {
+    return &NewWebUIIOS<DataSharingInternalsUI>;
   }
   if (url_host == kChromeUIDownloadInternalsHost) {
     return InternalDebugPagesEnabled()
