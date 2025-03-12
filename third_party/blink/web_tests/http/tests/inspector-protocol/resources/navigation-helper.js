0940e86f9bb4b0a076f49635f61cad005800c058
Maksim Sadym <sadym@chromium.org>
2025-03-12 07:13:51 -0700

    Emit `Page.frameStartedNavigating` for pending navigation
    If navigation starts while page handler is disabled, cache the data and
    emit event when/if the page handler is enabled.
    
    Bug: 402432771
    Change-Id: I46e0eaa80aab1df952aafbdd4490c473ed537ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6253719
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Maksim Sadym <sadym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431485}

--- third_party/blink/web_tests/http/tests/inspector-protocol/resources/navigation-helper.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/navigation-helper.js
@@ -41,11 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           });
     });
 
-    await initDelegate(dp);
+    void initDelegate(dp);
 
-    await dp.Page.enable();
-    await dp.Runtime.runIfWaitingForDebugger();
-  };
+    void dp.Page.enable();
+    void dp.Runtime.runIfWaitingForDebugger();
+    };
 
   function onceFrameNavigated(urlSuffix) {
     return new Promise(resolve => {
