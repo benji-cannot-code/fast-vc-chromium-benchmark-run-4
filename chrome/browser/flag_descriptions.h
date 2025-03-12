76a53c88df962779fcad931ebd9dc1876ff35d76
Maria Kazinova <kazinova@google.com>
2025-03-12 03:00:12 -0700

    Clean up UsernameFirstFlowWithIntermediateValuesVoting feature
    The feature was launched in December 2024, time to clean it up.
    
    Fixed: 394329877, 40925827
    Change-Id: Ie685158401ba2ec71617b92e435961d74cd97f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6343456
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431394}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1452,10 +1452,6 @@ extern const char kMostVisitedTilesNewScoringDescription[];
 extern const char kMostVisitedTilesReselectName[];
 extern const char kMostVisitedTilesReselectDescription[];
 
-extern const char kUsernameFirstFlowWithIntermediateValuesPredictionsName[];
-extern const char
-    kUsernameFirstFlowWithIntermediateValuesPredictionsDescription[];
-
 extern const char kCanvas2DLayersName[];
 extern const char kCanvas2DLayersDescription[];
 
