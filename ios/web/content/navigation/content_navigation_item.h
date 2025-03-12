6de52326734313c2f35860f1098061bece11c0e0
Dave Tapuska <dtapuska@chromium.org>
2025-03-12 08:03:34 -0700

    [ios blink] Fix build failure
    Fix fallout from https://chromium-review.googlesource.com/c/chromium/src/+/6281689
    Add minimal code to get the build to pass.
    
    Bug: 396091796
    Change-Id: Ife8da35ac79bf95e9b5c8391ed7500ae5c0df0b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348217
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431513}

--- ios/web/content/navigation/content_navigation_item.h
+++ ios/web/content/navigation/content_navigation_item.h
@@ -66,6 +66,9 @@ class ContentNavigationItem : public NavigationItem {
   void SetUserAgentType(UserAgentType type) override;
   UserAgentType GetUserAgentType() const override;
 
+  void SetSecurityScopedFileResource(NSData* data) override;
+  NSData* GetSecurityScopedFileResource() override;
+
   bool HasPostData() const override;
 
   HttpRequestHeaders* GetHttpRequestHeaders() const override;
@@ -88,6 +91,7 @@ class ContentNavigationItem : public NavigationItem {
   mutable HttpRequestHeaders* headers_ = nil;
   mutable FaviconStatus favicon_status_;
   mutable SSLStatus ssl_status_;
+  NSData* security_scoped_file_resource_ = nil;
 
   UserAgentType user_agent_type_ = UserAgentType::MOBILE;
 };
