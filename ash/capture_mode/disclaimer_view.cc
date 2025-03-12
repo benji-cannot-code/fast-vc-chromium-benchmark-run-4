76a303da842e0e7cc3d30e5c2d1f9a09445f3a3b
Michael Cui <mlcui@google.com>
2025-03-12 16:41:28 -0700

    scanner: Use inside border insets in disclaimer view
    …instead of an invisible border. An invisible border is still painted,
    causing unnecessary damage.
    
    Bug: b:401110577
    Change-Id: I6a6a210089a259692458184eef7222a692ccaec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332275
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431826}

--- ash/capture_mode/disclaimer_view.cc
+++ ash/capture_mode/disclaimer_view.cc
@@ -186,7 +186,7 @@ DisclaimerView::DisclaimerView(
               views::Builder<views::BoxLayoutView>()
                   .SetOrientation(views::LayoutOrientation::kVertical)
                   .SetBetweenChildSpacing(kTextContainerBetweenChildSpacing)
-                  .SetBorder(views::CreateEmptyBorder(kTextContainerInsets))
+                  .SetInsideBorderInsets(kTextContainerInsets)
                   .AddChildren(
                       views::Builder<views::Label>()
                           .SetFontList(
@@ -206,7 +206,7 @@ DisclaimerView::DisclaimerView(
       views::Builder<views::BoxLayoutView>()
           .SetMainAxisAlignment(views::LayoutAlignment::kEnd)
           .SetBetweenChildSpacing(kBetweenButtonsSpacing)
-          .SetBorder(views::CreateEmptyBorder(kButtonContainerInsets))
+          .SetInsideBorderInsets(kButtonContainerInsets)
           .AddChildren(
               views::Builder<views::MdTextButton>()
                   .SetText(GetTextDeclineButton())
