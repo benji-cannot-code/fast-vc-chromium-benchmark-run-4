d084856d9bae59394358f201e87c49bf806ce60b
Muyao Xu <muyaoxu@google.com>
2025-03-12 11:34:45 -0700

    Extend MediaRouter.Cast* metrics expiry
    Extend metrics expiry and add myself as an owner for metrics with only
    one owner, mfoltz@chromium.org, who no longer works on Cast.
    
    OBSOLETE_HISTOGRAM[MediaRouter.Cast.Feedback.Event]=This histogram is no longer needed.
    
    Bug: 398847549, 398847136
    Change-Id: Ica67c913d92874ce4ac34d377be600e98a2e0948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6324954
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431648}

--- chrome/test/data/webui/media_router/cast_feedback_ui_test.ts
+++ chrome/test/data/webui/media_router/cast_feedback_ui_test.ts
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {CastFeedbackUiElement, FeedbackUiBrowserProxy} from 'chrome://cast-feedback/cast_feedback_ui.js';
-import {FeedbackEvent, FeedbackUiBrowserProxyImpl} from 'chrome://cast-feedback/cast_feedback_ui.js';
+import {FeedbackUiBrowserProxyImpl} from 'chrome://cast-feedback/cast_feedback_ui.js';
 import type {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import {PromiseResolver} from 'chrome://resources/js/promise_resolver.js';
-import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
@@ -17,14 +17,7 @@ class TestFeedbackUiBrowserProxy extends TestBrowserProxy implements
   resolver: PromiseResolver<void> = new PromiseResolver();
 
   constructor() {
-    super(['recordEvent', 'sendFeedback']);
-  }
-
-  recordEvent(event: FeedbackEvent) {
-    this.methodCalled('recordEvent', event);
-    if (event === FeedbackEvent.SUCCEEDED || event === FeedbackEvent.FAILED) {
-      this.resolver.resolve();
-    }
+    super(['sendFeedback']);
   }
 
   sendFeedback(info: chrome.feedbackPrivate.FeedbackInfo) {
@@ -72,14 +65,6 @@ suite('Suite', function() {
     await submit();
     await browserProxy.resolver.promise;
     assertEquals(2, browserProxy.getCallCount('sendFeedback'));
-    assertDeepEquals(
-        [
-          FeedbackEvent.OPENED,
-          FeedbackEvent.SENDING,
-          FeedbackEvent.RESENDING,
-          FeedbackEvent.SUCCEEDED,
-        ],
-        browserProxy.getArgs('recordEvent'));
     assertTrue(ui.feedbackSent);
     assertTrue(
         browserProxy.getArgs('sendFeedback')[0].description.indexOf(
@@ -94,15 +79,6 @@ suite('Suite', function() {
     await submit();
     await browserProxy.resolver.promise;
     assertEquals(3, browserProxy.getCallCount('sendFeedback'));
-    assertDeepEquals(
-        [
-          FeedbackEvent.OPENED,
-          FeedbackEvent.SENDING,
-          FeedbackEvent.RESENDING,
-          FeedbackEvent.RESENDING,
-          FeedbackEvent.FAILED,
-        ],
-        browserProxy.getArgs('recordEvent'));
     assertFalse(ui.feedbackSent);
   });
 });
