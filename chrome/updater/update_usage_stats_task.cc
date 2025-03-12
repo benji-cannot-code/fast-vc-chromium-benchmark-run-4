5bb8a7729c00a747cac1ba579b62bf1ebc0c83c2
Felipe Pazos <fpazos@google.com>
2025-03-11 18:22:58 -0700

    Reland "Create updater::UsageStatsProvider"
    Change 6334513 was reverted because the test helper
    AnyAppUsageUsageEnabled was not called from branded mac builds.
    This removes the function.
    
    original commit: commit 736662131ce5fb91864b72b4c600302466069f7a
    
    Bug: 371595849
    Change-Id: I669bcf5abc7214c8ddcf0be7b3626575b9e202fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344256
    Commit-Queue: Felipe Pazos <fpazos@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431277}

--- chrome/updater/update_usage_stats_task.cc
+++ chrome/updater/update_usage_stats_task.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/updater/update_usage_stats_task.h"
 
+#include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
+#include "base/task/thread_pool.h"
 #include "chrome/updater/crash_client.h"
 #include "chrome/updater/persisted_data.h"
 #include "chrome/updater/updater_scope.h"
@@ -28,6 +30,21 @@ void UpdateUsageStatsTask::SetUsageStatsEnabled(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   persisted_data->SetUsageStatsEnabled(enabled);
   CrashClient::GetInstance()->SetUploadsEnabled(enabled);
+}
+
+void UpdateUsageStatsTask::Run(base::OnceClosure callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, {base::MayBlock()},
+      base::BindOnce(
+          [](UpdaterScope scope) {
+            return UsageStatsProvider::Create()->AnyAppEnablesUsageStats(scope);
+          },
+          scope_),
+      base::BindOnce(&UpdateUsageStatsTask::SetUsageStatsEnabled, this,
+                     persisted_data_)
+          .Then(std::move(callback)));
 }
 
 }  // namespace updater
--- chrome/updater/update_usage_stats_task_linux.cc
+++ chrome/updater/update_usage_stats_task_linux.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/updater/update_usage_stats_task.h"
 
+#include <memory>
 #include <utility>
 
 #include "base/functional/bind.h"
@@ -16,20 +17,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace updater {
 
-bool AnyAppUsageStatsAllowed(UpdaterScope scope) {
+class UsageStatsProviderImpl : public UsageStatsProvider {
+ public:
+  UsageStatsProviderImpl() = default;
+
   // TODO(crbug.com/40821596): Implement.
-  return false;
-}
+  bool AnyAppEnablesUsageStats(UpdaterScope scope) override { return false; }
+};
 
-void UpdateUsageStatsTask::Run(base::OnceClosure callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  base::ThreadPool::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::MayBlock()},
-      base::BindOnce(&AnyAppUsageStatsAllowed, scope_),
-      base::BindOnce(&UpdateUsageStatsTask::SetUsageStatsEnabled, this,
-                     persisted_data_)
-          .Then(std::move(callback)));
+std::unique_ptr<UsageStatsProvider> UsageStatsProvider::Create() {
+  return std::make_unique<UsageStatsProviderImpl>();
 }
 
 }  // namespace updater
