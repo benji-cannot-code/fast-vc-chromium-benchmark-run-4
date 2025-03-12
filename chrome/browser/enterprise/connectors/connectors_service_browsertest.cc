84fdf317978983dd57f92f6100abd55d57e3d719
Lina Ismail <ismaill@google.com>
2025-03-12 13:07:18 -0700

    [Enhanced Events] Populate device_fqdn and network_name for URLF and deep scan requests.
    Bug: 397738537
    Change-Id: Idee1e8425de25a4ef6229f5f92082f8ab9b0038d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310517
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Lina Ismail <ismaill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431715}

--- chrome/browser/enterprise/connectors/connectors_service_browsertest.cc
+++ chrome/browser/enterprise/connectors/connectors_service_browsertest.cc
@@ -33,7 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/mock_cloud_policy_client.h"
 #include "components/policy/core/common/cloud/reporting_job_configuration_base.h"
 #include "components/policy/core/common/cloud/user_cloud_policy_manager.h"
+#include "components/policy/core/common/features.h"
 #include "components/policy/core/common/policy_switches.h"
+#include "components/safe_browsing/core/common/features.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/version_info/version_info.h"
 #include "content/public/test/browser_test.h"
@@ -293,12 +295,27 @@ IN_PROC_BROWSER_TEST_P(ConnectorsServiceReportingProfileBrowserTest, Test) {
 class ConnectorsServiceAnalysisProfileBrowserTest
     : public ConnectorsServiceProfileBrowserTest,
       public testing::WithParamInterface<
-          std::tuple<ManagementStatus, const char*>> {
+          std::tuple<ManagementStatus, const char*, bool>> {
  public:
   ConnectorsServiceAnalysisProfileBrowserTest()
       : ConnectorsServiceProfileBrowserTest(std::get<0>(GetParam())) {
+    if (enhanced_fields_enabled()) {
+      scoped_feature_list_.InitWithFeatures(
+          /*enabled_features=*/{safe_browsing::kEnhancedFieldsForSecOps,
+                                policy::features::kEnhancedSecurityEventFields},
+          /*disabled_features=*/{});
+    } else {
+      scoped_feature_list_.InitWithFeatures(
+          /*enabled_features=*/{},
+          /*disabled_features=*/{
+              safe_browsing::kEnhancedFieldsForSecOps,
+              policy::features::kEnhancedSecurityEventFields});
+    }
   }
+
   const char* settings_value() { return std::get<1>(GetParam()); }
+
+  bool enhanced_fields_enabled() { return std::get<2>(GetParam()); }
 
   bool is_cloud() {
     return strcmp(settings_value(), kNormalCloudAnalysisSettingsPref) == 0;
@@ -404,6 +421,18 @@ class ConnectorsServiceAnalysisProfileBrowserTest
       ASSERT_TRUE(metadata.device().has_name());
       ASSERT_EQ(metadata.device().name(),
                 *reporting_metadata.FindStringByDottedPath("device.name"));
+
+      if (enhanced_fields_enabled()) {
+        ASSERT_TRUE(metadata.device().has_device_fqdn());
+        ASSERT_EQ(
+            metadata.device().device_fqdn(),
+            *reporting_metadata.FindStringByDottedPath("device.deviceFqdn"));
+
+        ASSERT_TRUE(metadata.device().has_network_name());
+        ASSERT_EQ(
+            metadata.device().network_name(),
+            *reporting_metadata.FindStringByDottedPath("device.networkName"));
+      }
     }
 
     ASSERT_TRUE(metadata.has_profile());
@@ -443,7 +472,8 @@ INSTANTIATE_TEST_SUITE_P(
                                      ManagementStatus::UNAFFILIATED,
                                      ManagementStatus::UNMANAGED),
                      testing::Values(kNormalCloudAnalysisSettingsPref,
-                                     kNormalLocalAnalysisSettingsPref)));
+                                     kNormalLocalAnalysisSettingsPref),
+                     testing::Bool()));
 
 IN_PROC_BROWSER_TEST_P(ConnectorsServiceAnalysisProfileBrowserTest,
                        DeviceReporting) {
