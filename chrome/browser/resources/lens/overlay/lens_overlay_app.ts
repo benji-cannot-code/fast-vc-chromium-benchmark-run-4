b6b0a11b8163e86997a763cc07d59e246c148c7f
Bryan Nguyen <nguyenbryan@google.com>
2025-03-12 11:00:20 -0700

    [lensoverlay] Trigger autocomplete manually when auto-focusing searchbox.
    Bug: 400993856
    Change-Id: Ie29d2443119cd40a79074c225560550f43bb91a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6341370
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431625}

--- chrome/browser/resources/lens/overlay/lens_overlay_app.ts
+++ chrome/browser/resources/lens/overlay/lens_overlay_app.ts
@@ -559,6 +559,7 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
     if (this.autoFocusSearchbox &&
         this.isLensOverlayContextualSearchboxVisible) {
       this.focusSearchbox();
+      this.$.searchbox.queryAutocomplete();
     }
   }
 
