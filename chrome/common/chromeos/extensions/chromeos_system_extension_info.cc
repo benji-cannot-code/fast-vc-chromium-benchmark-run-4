ea4846fa6a0eccefe6efd373ae1896269f64aaef
Byron Lee <byronlee@chromium.org>
2025-03-12 03:08:37 -0700

    [Telemetry] Update PWA origin for Google extension
    Reduced PWA domain coverage for testing purposes only.
    
    Bug: 391534038
    Test: unit_tests \
          --gtest_filter="*ExtensionManifestChromeOSSystemExtensionTest*"
    Test: unit_tests --gtest_filter="*ChromeOSSystemExtensionInfo*"
    Test: unit_tests --gtest_filter="*ApiGuardDelegateTest*"
    Test: browser_tests -ozone-platform=headless \
          --gtest_filter="*TelemetryExtension*"
    Test: tast run ${DUT_IP} telemetryextension.PlatformAPI*
    
    Change-Id: Ife1faabc6176d11249c2b7cbbeedb2cb3912e29e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6189535
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>
    Commit-Queue: Byron Lee <byronlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431396}

--- chrome/common/chromeos/extensions/chromeos_system_extension_info.cc
+++ chrome/common/chromeos/extensions/chromeos_system_extension_info.cc
@@ -53,7 +53,9 @@ ChromeOSSystemExtensionInfoMap ConstructMap() {
       {/*extension_id=*/"gogonhoemckpdpadfnjnpgbjpbjnodgc",
        {
            /*manufacturers=*/{"HP", "ASUS", "Acer", "Lenovo"},
-           /*pwa_origin=*/"*://googlechromelabs.github.io/*",
+           /*pwa_origin=*/
+           "*://googlechromelabs.github.io/cros-sample-telemetry-extension/"
+           "test-page/*",
            /*iwa_id=*/std::nullopt,
        }},
       {/*extension_id=*/"alnedpmllcfpgldkagbfbjkloonjlfjb",
@@ -87,7 +89,9 @@ ChromeOSSystemExtensionInfoMap ConstructMap() {
         kChromeOSSystemExtensionDevExtensionId,
         ChromeOSSystemExtensionInfo{
             /*manufacturers=*/{"Google", "HP", "ASUS", "Acer", "Lenovo"},
-            /*pwa_origin=*/"*://googlechromelabs.github.io/*",
+            /*pwa_origin=*/
+            "*://googlechromelabs.github.io/cros-sample-telemetry-extension/"
+            "test-page/*",
             /*iwa_id=*/
             web_package::SignedWebBundleId::Create(
                 "pt2jysa7yu326m2cbu5mce4rrajvguagronrsqwn5dhbaris6eaaaaic")
