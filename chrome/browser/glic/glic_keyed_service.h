6b20385d89b349af702935fd6b39de460e9d1c43
Dan Harrington <harringtond@chromium.org>
2025-03-12 15:17:53 -0700

    Add glic_api_uitest
    The goal of this test is to systematically test each glic
    API function. It's structured in a way that allows us to
    write assertions in typescript, which is better for checking
    types as the API changes, and makes checking several aspects
    of an API easier (than other tests we have today).
    More tests are needed.
    
    Bug: 401291368
    Change-Id: Ieab62804a18cc33b55bdb2e0b59126ac28df1cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317364
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431781}

--- chrome/browser/glic/glic_keyed_service.h
+++ chrome/browser/glic/glic_keyed_service.h
@@ -162,6 +162,11 @@ class GlicKeyedService : public KeyedService {
 
   base::WeakPtr<GlicKeyedService> GetWeakPtr();
 
+  const base::flat_set<raw_ptr<GlicPageHandler>>& GetPageHandlersForTesting()
+      const {
+    return page_handlers_;
+  }
+
  private:
   GlicPageHandler* GetPageHandler(const content::WebContents* webui_contents);
 
