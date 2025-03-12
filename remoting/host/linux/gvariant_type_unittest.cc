a3e139cff79e0cdc2576408a00f3761d1b019c88
Erik Jensen <rkjnsn@google.com>
2025-03-12 14:30:22 -0700

    GVariantRef improvements
    A few cleanups and quality-of-life improvements for GVariantRef:
    
     * Remove iteration functionality from gvariant::Type. It was not
       entirely obvious what iterating should mean for a type string.
       Explicitly getting a string_view to iterate makes it clear that one
       is iterating characters.
     * Update various usages of pointers to instead use std::string_views or
       base::spans in gvariant::Type.
     * Add GVariantFrom() helper function to infer the GVariant type string
       from the passed C++ types. E.g., GVariantFrom("foo") returns a
       GVariantRef<"s">.
     * Add BoxedRef() and FilledMaybeRef() helper functions to more easily
       wrap a reference to an existing value.
     * Introduce ObjectPathCStr and TypeSignatureCStr, which reference an
       unowned string that is known to be a valid object path or type
       signature, respectively. They can reference a constant string (which
       will be verified at compile time to be valid) or an owned ObjectPath
       or TypeSignature object, and are thus useful as function parameters.
    
    Bug: 378932951
    Change-Id: Idf625de0caf5b711c477da12d34a0d2a0a2306b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334519
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431759}

--- remoting/host/linux/gvariant_type_unittest.cc
+++ remoting/host/linux/gvariant_type_unittest.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/linux/gvariant_type.h"
 
-#include <iterator>
-#include <vector>
-
-#include "remoting/host/linux/gvariant_ref.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace remoting::gvariant {
