e887223ee01ebddc3a30824af24ecc8a18728e70
Aoyuan Zuo <zuoaoyuan@chromium.org>
2025-03-12 16:05:37 -0700

    Support 2 phases of Glic client unresponsive handling
    * phase 1: show place holder UI for unresponsive state.
    * phase 2: turn to error state after 5s unresponsive.
    client can automatically resume responsive from phase 1, but not phase
    2.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    
    Bug: 402435079
    Change-Id: I8d8d9d76eab762b7e6656777b33a2b24f564c8da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6347269
    Commit-Queue: Aoyuan Zuo <zuoaoyuan@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431805}

--- chrome/browser/glic/glic_window_controller_interactive_uitest.cc
+++ chrome/browser/glic/glic_window_controller_interactive_uitest.cc
@@ -48,8 +48,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace glic {
 
 namespace {
-const InteractiveBrowserTestApi::DeepQuery kMockGlicClientUnresponsiveButton = {
-    "#busyWork"};
+
+const InteractiveBrowserTestApi::DeepQuery
+    kMockGlicClientStart3sUnresponsiveButton = {"#busyWork3s"};
+const InteractiveBrowserTestApi::DeepQuery
+    kMockGlicClientStart8sUnresponsiveButton = {"#busyWork8s"};
+
 }  // anonymous namespace
 
 class GlicWindowControllerUiTest : public test::InteractiveGlicTest {
@@ -356,13 +360,28 @@ IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest,
       CheckControllerHasWidget(false));
 }
 
-IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, ClientBecomeUnresponsive) {
+IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest,
+                       ClientUnresponsiveThenResumeResponsive) {
   RunTestSequence(
       OpenGlicWindow(GlicWindowMode::kAttached),
-      ClickMockGlicElement(kMockGlicClientUnresponsiveButton, true),
+      ClickMockGlicElement(kMockGlicClientStart3sUnresponsiveButton, true),
       ObserveState(test::internal::kGlicAppState, &window_controller()),
       WaitForState(test::internal::kGlicAppState,
-                   mojom::WebUiState::kUnresponsive));
+                   mojom::WebUiState::kUnresponsive),
+      // Client should resume responsive if unresponsive less than 5s.
+      WaitForState(test::internal::kGlicAppState, mojom::WebUiState::kReady));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest,
+                       ClientUnresponsiveThenError) {
+  RunTestSequence(
+      OpenGlicWindow(GlicWindowMode::kAttached),
+      ClickMockGlicElement(kMockGlicClientStart8sUnresponsiveButton, true),
+      ObserveState(test::internal::kGlicAppState, &window_controller()),
+      WaitForState(test::internal::kGlicAppState,
+                   mojom::WebUiState::kUnresponsive),
+      // Client should show error after showing the unresponsive UI for 5s.
+      WaitForState(test::internal::kGlicAppState, mojom::WebUiState::kError));
 }
 
 class GlicWindowControllerWithMemoryPressureUiTest
