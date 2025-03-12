a551524de05986a0ec1e3c9652384dc519b1cf0d
Saifuddin Hitawala <hitawala@chromium.org>
2025-03-12 13:06:49 -0700

    [gpu] Remove SharedImageInterface::Flush
    All callsites of SharedImageInterface::Flush have been removed since
    the Flush now happens as a delayed post task when the client shared
    image is destroyed.
    
    This change removes SharedImageInterface::Flush and its associated
    implementations.
    
    Bug: 399387909
    Change-Id: Iffc41457a39cfef13de6e92efe7e7e327bea63d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348176
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431714}

--- gpu/command_buffer/client/test_shared_image_interface.h
+++ gpu/command_buffer/client/test_shared_image_interface.h
@@ -51,8 +51,6 @@ class TestSharedImageInterface : public SharedImageInterface {
                     gpu::SurfaceHandle surface_handle,
                     gfx::BufferUsage buffer_usage));
 
-  MOCK_METHOD0(DoFlush, void());
-
   scoped_refptr<ClientSharedImage> CreateSharedImage(
       const SharedImageInfo& si_info,
       SurfaceHandle surface_handle,
@@ -102,8 +100,6 @@ class TestSharedImageInterface : public SharedImageInterface {
   SyncToken GenUnverifiedSyncToken() override;
   void VerifySyncToken(SyncToken& sync_token) override;
   void WaitSyncToken(const SyncToken& sync_token) override;
-
-  void Flush() override;
 
   void CreateSharedImagePool(
       const SharedImagePoolId& pool_id,
