0fdce2e95a44f0d810426ab0e278aed2c8307273
Duncan Mercer <mercerd@google.com>
2025-03-12 16:16:32 -0700

    Don't show preselection bubble over if omnibox already focused
    This prevents the preselection bubble showing over @page suggestions.
    
    Fixed: 402499996
    Change-Id: I4520a01152d9021b6ac687d1a212cf4b817d9e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344952
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431813}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/lens/lens_overlay_url_builder.h"
 #include "chrome/browser/ui/lens/lens_permission_bubble_controller.h"
 #include "chrome/browser/ui/lens/lens_preselection_bubble.h"
+#include "chrome/browser/ui/search/omnibox_utils.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/views/side_panel/side_panel.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
@@ -2442,7 +2443,8 @@ void LensOverlayController::InitializeOverlay(
 
   // Show the preselection overlay now that the overlay is initialized and ready
   // to be shown.
-  if (!pending_region_) {
+  if (!pending_region_ &&
+      !search::IsOmniboxInputInProgress(tab_->GetContents())) {
     ShowPreselectionBubble();
   }
 
