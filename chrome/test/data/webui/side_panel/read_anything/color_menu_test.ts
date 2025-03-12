3459e47d21b2b7cef3d092ccea720bacbec17461
Eitan Goldberger <eitang@google.com>
2025-03-12 11:44:24 -0700

    [Reading Mode] Lit migration- color menu
    Bug: 352583619
    Change-Id: I79b37d9b35b9fd076714cc10232e7bb5446a94e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6343737
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431653}

--- chrome/test/data/webui/side_panel/read_anything/color_menu_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/color_menu_test.ts
@@ -5,17 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 
-import {flush} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {MetricsBrowserProxyImpl, ReadAnythingLogger, ReadAnythingSettingsChange, ToolbarEvent} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
-import type {ColorMenu} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import type {ColorMenuElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertEquals} from 'chrome-untrusted://webui-test/chai_assert.js';
 
-import {emitEventForPolymer} from './common.js';
 import {FakeReadingMode} from './fake_reading_mode.js';
 import {TestMetricsBrowserProxy} from './test_metrics_browser_proxy.js';
 
-suite('ColorMenu', () => {
-  let colorMenu: ColorMenu;
+suite('ColorMenuElement', () => {
+  let colorMenu: ColorMenuElement;
   let metrics: TestMetricsBrowserProxy;
 
   setup(() => {
@@ -30,23 +28,22 @@ suite('ColorMenu', () => {
 
     colorMenu = document.createElement('color-menu');
     document.body.appendChild(colorMenu);
-    flush();
   });
 
   test('theme change', async () => {
     const theme1 = chrome.readingMode.blueTheme;
-    emitEventForPolymer(
-        colorMenu.$.menu, ToolbarEvent.THEME, {detail: {data: theme1}});
+    colorMenu.$.menu.dispatchEvent(
+        new CustomEvent(ToolbarEvent.THEME, {detail: {data: theme1}}));
     assertEquals(theme1, chrome.readingMode.colorTheme);
 
     const theme2 = chrome.readingMode.defaultTheme;
-    emitEventForPolymer(
-        colorMenu.$.menu, ToolbarEvent.THEME, {detail: {data: theme2}});
+    colorMenu.$.menu.dispatchEvent(
+        new CustomEvent(ToolbarEvent.THEME, {detail: {data: theme2}}));
     assertEquals(theme2, chrome.readingMode.colorTheme);
 
     const theme3 = chrome.readingMode.darkTheme;
-    emitEventForPolymer(
-        colorMenu.$.menu, ToolbarEvent.THEME, {detail: {data: theme3}});
+    colorMenu.$.menu.dispatchEvent(
+        new CustomEvent(ToolbarEvent.THEME, {detail: {data: theme3}}));
     assertEquals(theme3, chrome.readingMode.colorTheme);
 
     assertEquals(
