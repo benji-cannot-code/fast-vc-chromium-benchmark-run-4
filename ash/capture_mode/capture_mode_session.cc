4b21c577d51d7f64be316fb8128e2db65014f150
Michelle Chen <michellegc@google.com>
2025-03-12 01:49:02 -0700

    scanner: Set disclaimer as a11y override window when needed.
    If the user is using keyboard navigation, set the Scanner disclaimer
    dialogue as the a11y override window when it is shown, and then pass
    focus back to the focus cycler when the disclaimer is closed.
    
    This is to ensure that the disclaimer contents can be properly conveyed
    by ChromeVox when needed.
    
    Bug: b:401579287
    Change-Id: I008b7b7cda7b4b09cb147fc9a2f296f2a71bfa44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6345256
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431368}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -1575,6 +1575,7 @@ void CaptureModeSession::MaybeShowScannerDisclaimer(
                           weak_ptr_factory_.GetWeakPtr(),
                           std::move(decline_callback)));
   disclaimer_->Show();
+  focus_cycler_->OnDisclaimerWidgetOpened(disclaimer_.get());
 }
 
 void CaptureModeSession::OnScannerActionsFetched(
@@ -1640,6 +1641,7 @@ void CaptureModeSession::OnDisclaimerDeclined(base::RepeatingClosure callback) {
       ScannerFeatureUserState::kConsentDisclaimerRejected);
 
   disclaimer_.reset();
+  focus_cycler_->OnDisclaimerWidgetClosed();
   if (callback) {
     std::move(callback).Run();
   }
@@ -1652,6 +1654,7 @@ void CaptureModeSession::OnDisclaimerAccepted(base::RepeatingClosure callback) {
       prefs::kSunfishConsentDisclaimerAccepted, true);
 
   disclaimer_.reset();
+  focus_cycler_->OnDisclaimerWidgetClosed();
   if (callback) {
     std::move(callback).Run();
   }
@@ -1736,11 +1739,6 @@ void CaptureModeSession::OnKeyEvent(ui::KeyEvent* event) {
 
   // If the consent disclaimer is visible, let it handle key events.
   if (disclaimer_) {
-    // The action button may still have a focus ring when we switch focus to the
-    // disclaimer, so clear it first.
-    if (focus_cycler_->HasFocus()) {
-      focus_cycler_->ClearFocus();
-    }
     return;
   }
 
