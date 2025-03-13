138239f5de5bb8f2460283567f73634fcf08a210
Theo Cristea <theocristea@google.com>
2025-03-13 05:45:53 -0700

    Separate `GenerateCountryList` into 2 functions, for profile and account
    The aim of this CL is to not query `AddressDataManager` when the country
    list is used only for profile storage.
    
    The motivation behind this is that Autofill Ai will need to use the same
    function to generate the country list for profile storage, and Autofill
    Ai logic should be as decoupled as possible from regular Autofill logic.
    
    Bug: 393318914
    Change-Id: Ibc333b4a86a674f2c1917fc3447a14bfdc979a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6347597
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432044}

--- chrome/test/data/extensions/api_test/autofill_private/test.js
+++ chrome/test/data/extensions/api_test/autofill_private/test.js
@@ -228,7 +228,7 @@ var availableTests = [
     };
 
     chrome.autofillPrivate.getCountryList(
-        /*forAccountAddressProfile=*/ false, handler);
+        /*forAccountStorage=*/ false, handler);
   },
 
   function getAddressComponents() {
