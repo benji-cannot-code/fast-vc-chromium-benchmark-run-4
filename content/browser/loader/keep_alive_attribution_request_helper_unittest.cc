9c01b501c135f71f8c692aa8a040d1669967e80d
Ming-Ying Chung <mych@chromium.org>
2025-03-12 21:12:37 -0700

    Add initial skeleton of [`KeepAliveTrackerThrottle`][1].
    This is for observing the lifetime of a subset of fetch keepalive
    requests that match all of the following conditions:
    
    - A network request with keepalive set to true.
    - A network request with URL param "category=<certain value>"[2].
    
    [1]: https://docs.google.com/document/d/1byKFqqKTsVFnj6rSb7ZjHi4LuRi1LNb-vf4maQbmSSQ/edit?resourcekey=0-7GAe1ae8j_55DMcvp3U3IQ&tab=t.0
    [2]: https://docs.google.com/document/d/1FY3AINZW_h2GU81U-XPQz8bKT6wgt0u7Z9GF4o7_Llk/edit?resourcekey=0-lQnRY9BK0iclRRV62LgVDQ&tab=t.0
    
    Bug: 382527001
    Change-Id: I5e488bf3f0c2c35aeb0222550c34f5b0f9c66a8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6325517
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431911}

--- content/browser/loader/keep_alive_attribution_request_helper_unittest.cc
+++ content/browser/loader/keep_alive_attribution_request_helper_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/test/test_web_contents.h"
 #include "net/http/http_response_headers.h"
 #include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
+#include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/mojom/attribution.mojom-shared.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -74,9 +75,10 @@ using attribution_reporting::kAttributionReportingRegisterOsTriggerHeader;
 using attribution_reporting::kAttributionReportingRegisterSourceHeader;
 using attribution_reporting::kAttributionReportingRegisterTriggerHeader;
 
-class KeepAliveAttributionRequestHelperTest : public RenderViewHostTestHarness {
+class KeepAliveAttributionRequestHelperTestBase
+    : public RenderViewHostTestHarness {
  public:
-  KeepAliveAttributionRequestHelperTest()
+  KeepAliveAttributionRequestHelperTestBase()
       : RenderViewHostTestHarness(
             base::test::TaskEnvironment::MainThreadType::UI,
             base::test::TaskEnvironment::TimeSource::MOCK_TIME),
@@ -156,6 +158,9 @@ class KeepAliveAttributionRequestHelperTest : public RenderViewHostTestHarness {
   raw_ptr<MockAttributionManager> mock_attribution_manager_;
   data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
 };
+
+using KeepAliveAttributionRequestHelperTest =
+    KeepAliveAttributionRequestHelperTestBase;
 
 TEST_F(KeepAliveAttributionRequestHelperTest, SingleResponse) {
   const GURL source_url = GURL("https://secure_source.com");
@@ -683,6 +688,75 @@ TEST_F(KeepAliveAttributionRequestHelperTest, CreateIfNeeded_MetricRecorded) {
                                   0);
     }
   }
+}
+
+// A type to support parameterized testing for testing attribution request.
+struct IsAttributionRequestTestCase {
+  std::string test_case;
+  AttributionReportingEligibility eligibility;
+  bool expected;
+};
+
+class IsNonKeepAliveRequestAttributionRequestTest
+    : public KeepAliveAttributionRequestHelperTestBase,
+      public ::testing::WithParamInterface<IsAttributionRequestTestCase> {};
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    IsNonKeepAliveRequestAttributionRequestTest,
+    testing::ValuesIn<IsAttributionRequestTestCase>({
+        {"kEmpty", AttributionReportingEligibility::kEmpty, false},
+        {"kEventSource", AttributionReportingEligibility::kEventSource, false},
+        {"kEventSourceOrTrigger",
+         AttributionReportingEligibility::kEventSourceOrTrigger, false},
+        {"kNavigationSource",
+         AttributionReportingEligibility::kNavigationSource, false},
+        {"kTrigger", AttributionReportingEligibility::kTrigger, false},
+    }),
+    [](const testing::TestParamInfo<IsAttributionRequestTestCase>& info) {
+      return info.param.test_case;
+    });
+
+TEST_P(IsNonKeepAliveRequestAttributionRequestTest, WithEligibility) {
+  network::ResourceRequest request;
+  request.url = GURL("https://report.test");
+  request.keepalive = false;
+
+  request.attribution_reporting_eligibility = GetParam().eligibility;
+
+  EXPECT_THAT(KeepAliveAttributionRequestHelper::IsAttributionRequest(request),
+              testing::Eq(GetParam().expected));
+}
+
+class IsKeepAliveRequestAttributionRequestTest
+    : public KeepAliveAttributionRequestHelperTestBase,
+      public ::testing::WithParamInterface<IsAttributionRequestTestCase> {};
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    IsKeepAliveRequestAttributionRequestTest,
+    testing::ValuesIn<IsAttributionRequestTestCase>({
+        {"kEmpty", AttributionReportingEligibility::kEmpty, false},
+        {"kEventSource", AttributionReportingEligibility::kEventSource, true},
+        {"kEventSourceOrTrigger",
+         AttributionReportingEligibility::kEventSourceOrTrigger, true},
+        {"kNavigationSource",
+         AttributionReportingEligibility::kNavigationSource, true},
+        {"kTrigger", AttributionReportingEligibility::kTrigger, true},
+    }),
+    [](const testing::TestParamInfo<IsAttributionRequestTestCase>& info) {
+      return info.param.test_case;
+    });
+
+TEST_P(IsKeepAliveRequestAttributionRequestTest, WithEligibility) {
+  network::ResourceRequest request;
+  request.url = GURL("https://report.test");
+  request.keepalive = true;
+
+  request.attribution_reporting_eligibility = GetParam().eligibility;
+
+  EXPECT_THAT(KeepAliveAttributionRequestHelper::IsAttributionRequest(request),
+              testing::Eq(GetParam().expected));
 }
 
 }  // namespace
