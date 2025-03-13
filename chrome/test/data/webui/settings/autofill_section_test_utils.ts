6a9251704a689ee28facd789bc830923b2c33419
Theo Cristea <theocristea@google.com>
2025-03-13 13:51:44 -0700

    Move CountryDetailManager(Proxy) to its own file
    This proxy is used by the `settings-address-edit-dialog` component, but
    it is going to be used in a follow-up CL also by the
    `settings-autofill-ai-add-or-edit-dialog` component.
    
    Therefore, the CountryDetailManager(Proxy) was moved to its own file.
    
    The test proxy was also moved to its own file. The test proxy used an
    anti-pattern, and instead of simulating prod behavior, it actually
    called production methods from the autofill private API.
    This CL also eliminates this anti-pattern, and starts simulating prod
    behavior.
    
    Bug: 393318914
    Change-Id: Ie7891359a829e46a7a0c48e99fd2da697a284be3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348920
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432343}

--- chrome/test/data/webui/settings/autofill_section_test_utils.ts
+++ chrome/test/data/webui/settings/autofill_section_test_utils.ts
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://settings/settings.js';
 
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import type {CountryDetailManager, SettingsAddressEditDialogElement, SettingsAddressRemoveConfirmationDialogElement, SettingsAutofillSectionElement} from 'chrome://settings/lazy_load.js';
+import type {SettingsAddressEditDialogElement, SettingsAddressRemoveConfirmationDialogElement, SettingsAutofillSectionElement} from 'chrome://settings/lazy_load.js';
 import {AutofillManagerImpl} from 'chrome://settings/lazy_load.js';
 import {assertFalse, assertGT, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
@@ -15,27 +15,6 @@ import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
 import {createAddressEntry, TestAutofillManager} from './autofill_fake_data.js';
 // clang-format on
-
-/**
- * Test implementation.
- */
-export class CountryDetailManagerTestImpl implements CountryDetailManager {
-  getCountryList() {
-    return new Promise<chrome.autofillPrivate.CountryEntry[]>(function(
-        resolve) {
-      resolve([
-        {name: 'United States', countryCode: 'US'},  // Default test country.
-        {name: 'Israel', countryCode: 'IL'},
-        {name: 'United Kingdom', countryCode: 'GB'},
-      ]);
-    });
-  }
-
-  getAddressFormat(countryCode: string) {
-    return chrome.autofillPrivate.getAddressComponents(countryCode);
-  }
-}
-
 
 /**
  * Resolves the promise after the element fires the expected event. |causeEvent|
