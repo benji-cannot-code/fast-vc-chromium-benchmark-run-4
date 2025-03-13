6e2c2d02078de625af843e12f7b1ab792be3c7a3
Bryant Chandler <bryantchandler@chromium.org>
2025-03-13 11:15:16 -0700

    Add UI test for ESC press on glic
    The added tests verify that that ESC press on active glic closes it.
    
    Bug: b:402506950
    Change-Id: I9e2ae3b3856987fb805a7ed5ae37a363137a9c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348019
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432247}

--- chrome/browser/glic/interactive_glic_test.h
+++ chrome/browser/glic/interactive_glic_test.h
@@ -35,9 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feature_engagement/public/feature_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/interaction/interactive_test.h"
+#include "ui/events/test/event_generator.h"
 #include "url/gurl.h"
 #include "url/url_util.h"
-
 
 namespace glic::test {
 
@@ -283,6 +283,22 @@ class InteractiveGlicTestT : public T {
         Api::WaitForHide(kGlicViewElementId)));
     Api::AddDescriptionPrefix(steps, "CloseGlicWindow");
     return steps;
+  }
+
+  auto SimulateAcceleratorPress(const ui::Accelerator& accelerator) {
+    return Api::Do([this, accelerator] {
+      gfx::NativeWindow target_window =
+          window_controller().GetGlicWidget()->GetNativeWindow();
+#if (USE_AURA)
+      ui::test::EventGenerator event_generator(target_window->GetRootWindow(),
+                                               target_window);
+#else
+      ui::test::EventGenerator event_generator(target_window);
+#endif
+      event_generator.set_target(ui::test::EventGenerator::Target::WINDOW);
+      event_generator.PressAndReleaseKeyAndModifierKeys(
+          accelerator.key_code(), accelerator.modifiers());
+    });
   }
 
   auto CheckControllerHasWidget(bool expect_widget) {
