2b80d4d5e6f7bc4b9842601d0d06811bc33b4a4a
Vlad Batalov <inverse12@yandex-team.ru>
2025-03-13 04:20:35 -0700

    Ensure valid surface when renderer is about to be shown.
    Specific combination of fullscreen, eviction and
    visibility states can leads to crash.
    
    Bug: 402791079
    Change-Id: Iccab648282878030b2655ad199d37ff90343add6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330310
    Commit-Queue: Vladislav Batalov <inverse12@yandex-team.ru>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432014}

--- content/browser/renderer_host/render_widget_host_view_android.h
+++ content/browser/renderer_host/render_widget_host_view_android.h
@@ -527,6 +527,9 @@ class CONTENT_EXPORT RenderWidgetHostViewAndroid
     void WasEvicted();
     void WasShownAfterEviction();
 
+    // Clears flags used to throttle SurfaceSync.
+    void Unthrottle();
+
    private:
     friend class RenderWidgetHostViewAndroidRotationTest;
 
@@ -538,9 +541,6 @@ class CONTENT_EXPORT RenderWidgetHostViewAndroid
     // fullscreen, and Picture-in-Picture mode.
     bool HandleScreenStateChanges(const cc::DeadlinePolicy& deadline_policy,
                                   bool force_fullscreen_sync = false);
-
-    // Clears flags used to throttle SurfaceSync.
-    void Unthrottle();
 
     // The ScreenState of the current world, the pending visual properties, or
     // the properties from before we entered Picture-in-Picture mode.
