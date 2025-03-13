f87c207296a60faadc9f2fac95aa2d26086d15b3
Jan Keitel <jkeitel@google.com>
2025-03-13 04:22:31 -0700

    Loyalty fields are no longer AutofillAI fields
    AutofillAI code ran into a CHECK failure because it could not retrieve
    a corresponding AttributeType.
    
    Bug: 403184080
    Change-Id: Iffd2bc7ebb063510b456b8c722600967c651f3e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348519
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1432016}

--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -656,9 +656,6 @@ FieldTypeGroup GroupTypeOfFieldType(FieldType field_type) {
     case PASSPORT_ISSUING_COUNTRY:
     case PASSPORT_EXPIRATION_DATE:
     case PASSPORT_ISSUE_DATE:
-    case LOYALTY_MEMBERSHIP_PROGRAM:
-    case LOYALTY_MEMBERSHIP_PROVIDER:
-    case LOYALTY_MEMBERSHIP_ID:
     case VEHICLE_OWNER_TAG:
     case VEHICLE_LICENSE_PLATE:
     case VEHICLE_VIN:
@@ -691,6 +688,9 @@ FieldTypeGroup GroupTypeOfFieldType(FieldType field_type) {
     case EMPTY_TYPE:
     case AMBIGUOUS_TYPE:
     case FIELD_WITH_DEFAULT_VALUE:
+    case LOYALTY_MEMBERSHIP_PROGRAM:
+    case LOYALTY_MEMBERSHIP_PROVIDER:
+    case LOYALTY_MEMBERSHIP_ID:
     case MERCHANT_EMAIL_SIGNUP:
     case MERCHANT_PROMO_CODE:
     case ONE_TIME_CODE:
