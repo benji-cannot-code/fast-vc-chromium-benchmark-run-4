f766e9fc9e147fdf48b8afffea00a60efa678210
Ayu Ishii <ayui@chromium.org>
2025-03-13 14:02:01 -0700

    WebSQL: Remove WebSQL everywhere
    WebSQL has been deprecated and removed from WebView in M133,
    and M119 on all other platforms. This change updates
    RuntimeEnabledFeatures to remove the feature from everywhere.
    
    Chromestatus: https://chromestatus.com/feature/5134293578285056
    I2D&R: https://groups.google.com/a/chromium.org/g/blink-dev/c/fWYb6evVA-w/m/pziWcvboAgAJ
    
    Bug: 395838064
    Change-Id: Ic5840a5372cd26559b6ec385eef4f2f610343487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318423
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432355}

--- third_party/blink/web_tests/http/tests/security/resources/worker-for-websql.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.onmessage = function() {
-	try {
-		var db = self.openDatabase('testdb', '1.0', 'Testing database', 512 * 1024);
-		self.postMessage(null);
-	} catch (exception) {
-		self.postMessage(exception.name);
-	}
-}
