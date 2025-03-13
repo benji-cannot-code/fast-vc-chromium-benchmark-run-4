9cea7c97a3d2593f19ea7ab3934784a8b975d9ad
Sana Akbani <sanaakbani@google.com>
2025-03-13 16:55:22 -0700

    Add SetMinimumWidgetSize to Glic API
    This will be used to enforce the minimum size on the glic widget for manual resizing.
    
    Bug: 400522982
    Change-Id: Ibc90a13f7cb55f479491837047d286b64632e105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6352391
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432455}

--- chrome/browser/resources/glic/glic_api_impl/glic_api_client.ts
+++ chrome/browser/resources/glic/glic_api_impl/glic_api_client.ts
@@ -314,6 +314,11 @@ class GlicBrowserHostImpl implements GlicBrowserHost {
         'glicBrowserSetWindowDraggableAreas', {areas});
   }
 
+  setMinimumWidgetSize(width: number, height: number): Promise<void> {
+    return this.sender.requestWithResponse(
+        'glicBrowserSetMinimumWidgetSize', {size: {width, height}});
+  }
+
   getPanelState(): ObservableValueImpl<PanelState> {
     return this.panelState;
   }
