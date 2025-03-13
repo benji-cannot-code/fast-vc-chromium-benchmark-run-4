29f81c904ab9d1964ceb37c6f852a94584db3e74
Kristi Saney <kristislee@google.com>
2025-03-12 18:25:44 -0700

    Reland "[Read aloud] Use a browser proxy for SpeechSynthesis"
    This is a reland of commit f618b1bcf3232cb1cb86800f3c870625f53ac0f0
    
    I fixed the issue that it was causing before. Note the .bind(this) added
    in app.ts when setting onVoicesChanged().
    
    Original change's description:
    > [Read aloud] Use a browser proxy for SpeechSynthesis
    >
    > This is the preferred pattern for wrapping logic that's sent to the
    > browser (in this case window.speechSynthesis). This is also better for
    > tests where we can use a TestBrowserProxy to better streamline our tests
    > that use speech synthesis.
    >
    > This should also reduce the number of microtasksFinished() calls needed
    > and reduce test flakiness. This is also a prerequisite for pulling out
    > speech logic from app.ts in the future if we want to.
    >
    > Bug: 397973923
    > Change-Id: Idf852a7814db6692ea64d937974901184923f7da
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6336717
    > Reviewed-by: Eitan Goldberger <eitang@google.com>
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Commit-Queue: Kristi Saney <kristislee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1430384}
    
    Bug: 397973923
    Change-Id: I6893e58dd787860c47dd67f967b2e8e961953828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6341576
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431873}

--- chrome/test/data/webui/side_panel/read_anything/update_content_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/update_content_test.ts
@@ -4,15 +4,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 
-import {BrowserProxy} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {BrowserProxy, SpeechBrowserProxyImpl} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import type {AppElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
 import {microtasksFinished} from 'chrome-untrusted://webui-test/test_util.js';
 
-import {setDefaultSpeechSynthesis} from './common.js';
+import {setupBasicSpeech} from './common.js';
 import {FakeReadingMode} from './fake_reading_mode.js';
 import {FakeTreeBuilder} from './fake_tree_builder.js';
 import {TestColorUpdaterBrowserProxy} from './test_color_updater_browser_proxy.js';
+import {TestSpeechBrowserProxy} from './test_speech_browser_proxy.js';
 
 suite('UpdateContent', () => {
   let app: AppElement;
@@ -32,6 +33,8 @@ suite('UpdateContent', () => {
     BrowserProxy.setInstance(new TestColorUpdaterBrowserProxy());
     readingMode = new FakeReadingMode();
     chrome.readingMode = readingMode as unknown as typeof chrome.readingMode;
+    const speech = new TestSpeechBrowserProxy();
+    SpeechBrowserProxyImpl.setInstance(speech);
 
     // Don't use await createApp() when using a FakeTree, as it seems to cause
     // flakiness.
@@ -49,8 +52,7 @@ suite('UpdateContent', () => {
         .addText(textNodeIds[3]!, /* parentId= */ 8, texts[3]!)
         .build(readingMode);
 
-    setDefaultSpeechSynthesis(app);
-    app.getSpeechSynthesisVoice();
+    setupBasicSpeech(app, speech);
   });
 
   test('playable if done with distillation', async () => {
