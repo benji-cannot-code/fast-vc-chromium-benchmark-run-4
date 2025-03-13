7a01eed3b6f41bde180baf68ab91e426c7211c01
Jonathan Lee <jonathanjlee@google.com>
2025-03-13 11:49:39 -0700

    [enterprise][e2e] Improve readability of CBCM reporting test messages
    No-op refactor:
    1. Pretty-print the raw takeout API response.
    2. Replace `assertTrue()` with `assertGreater()` so that failure will
       log the operands and not just `expected true got false`.
    
    Bug: 327797500
    Change-Id: If12b4f9809e7d1a1ed9ca42db0d7dd3b62f17010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344348
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1432270}

--- chrome/test/enterprise/e2e/policy/cloud_reporting_enabled/cloud_reporting_enabled.py
+++ chrome/test/enterprise/e2e/policy/cloud_reporting_enabled/cloud_reporting_enabled.py
@@ -76,7 +76,7 @@ class CloudReportingEnabledTest(ChromeEnterpriseTestCase):
 
     # Get the device id of the browser
     index = output.find('DEVICE_ID=')
-    self.assertTrue(index > 0)
+    self.assertGreater(index, 0)
     device_id = output[index + len('DEVICE_ID='):].split()[0]
 
     # Get OAuth2 access token
@@ -92,7 +92,7 @@ class CloudReportingEnabledTest(ChromeEnterpriseTestCase):
     logging.info('Querying CBCM API for browser data...')
     logging.info('server response = %s' % response)
     json_object = response.json()
-    logging.info(json_object)
+    logging.info(json.dumps(json_object, indent=2))
 
     last_registration_time = datetime.fromisoformat(
         json_object['browsers'][0]['lastRegistrationTime'])
@@ -101,6 +101,6 @@ class CloudReportingEnabledTest(ChromeEnterpriseTestCase):
     last_status_report_time = datetime.fromisoformat(
         json_object['browsers'][0]['lastStatusReportTime'])
 
-    self.assertTrue(int(json_object['browsers'][0]['policyCount']) > 0)
-    self.assertTrue(last_policy_fetch_time > last_registration_time)
-    self.assertTrue(last_status_report_time > last_registration_time)
+    self.assertGreater(int(json_object['browsers'][0]['policyCount']), 0)
+    self.assertGreater(last_policy_fetch_time, last_registration_time)
+    self.assertGreater(last_status_report_time, last_registration_time)
