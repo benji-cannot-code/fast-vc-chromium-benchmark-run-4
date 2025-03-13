e13fe23944d01654f2f0e781ba273ec2935b7a66
Lukasz Anforowicz <lukasza@chromium.org>
2025-03-13 15:04:57 -0700

    Roll either: 1.14.0 => 1.15.0 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * either: 1.14.0 => 1.15.0
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * either@1.15.0: crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: If3b474a6e790a99d11a730541db420011cf67086
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6352896
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432401}

--- third_party/rust/chromium_crates_io/vendor/either-1.15.0/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/either-1.15.0/src/lib.rs
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! **Crate features:**
 //!
-//! * `"use_std"`
+//! * `"std"`
 //!   Enabled by default. Disable to make the library `#![no_std]`.
 //!
 //! * `"serde"`
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #![doc(html_root_url = "https://docs.rs/either/1/")]
 #![no_std]
 
-#[cfg(any(test, feature = "use_std"))]
+#[cfg(any(test, feature = "std"))]
 extern crate std;
 
 #[cfg(feature = "serde")]
@@ -32,9 +32,9 @@ use core::ops::Deref;
 use core::ops::DerefMut;
 use core::pin::Pin;
 
-#[cfg(any(test, feature = "use_std"))]
+#[cfg(any(test, feature = "std"))]
 use std::error::Error;
-#[cfg(any(test, feature = "use_std"))]
+#[cfg(any(test, feature = "std"))]
 use std::io::{self, BufRead, Read, Seek, SeekFrom, Write};
 
 pub use crate::Either::{Left, Right};
@@ -1155,10 +1155,10 @@ where
     }
 }
 
-#[cfg(any(test, feature = "use_std"))]
+#[cfg(any(test, feature = "std"))]
 /// `Either<L, R>` implements `Read` if both `L` and `R` do.
 ///
-/// Requires crate feature `"use_std"`
+/// Requires crate feature `"std"`
 impl<L, R> Read for Either<L, R>
 where
     L: Read,
@@ -1181,10 +1181,10 @@ where
     }
 }
 
-#[cfg(any(test, feature = "use_std"))]
+#[cfg(any(test, feature = "std"))]
 /// `Either<L, R>` implements `Seek` if both `L` and `R` do.
 ///
-/// Requires crate feature `"use_std"`
+/// Requires crate feature `"std"`
 impl<L, R> Seek for Either<L, R>
 where
     L: Seek,
@@ -1195,8 +1195,8 @@ where
     }
 }
 
-#[cfg(any(test, feature = "use_std"))]
-/// Requires crate feature `"use_std"`
+#[cfg(any(test, feature = "std"))]
+/// Requires crate feature `"std"`
 impl<L, R> BufRead for Either<L, R>
 where
     L: BufRead,
@@ -1219,10 +1219,10 @@ where
     }
 }
 
-#[cfg(any(test, feature = "use_std"))]
+#[cfg(any(test, feature = "std"))]
 /// `Either<L, R>` implements `Write` if both `L` and `R` do.
 ///
-/// Requires crate feature `"use_std"`
+/// Requires crate feature `"std"`
 impl<L, R> Write for Either<L, R>
 where
     L: Write,
@@ -1280,18 +1280,18 @@ macro_rules! impl_specific_ref_and_mut {
 impl_specific_ref_and_mut!(str,);
 impl_specific_ref_and_mut!(
     ::std::path::Path,
-    cfg(feature = "use_std"),
-    doc = "Requires crate feature `use_std`."
+    cfg(feature = "std"),
+    doc = "Requires crate feature `std`."
 );
 impl_specific_ref_and_mut!(
     ::std::ffi::OsStr,
-    cfg(feature = "use_std"),
-    doc = "Requires crate feature `use_std`."
+    cfg(feature = "std"),
+    doc = "Requires crate feature `std`."
 );
 impl_specific_ref_and_mut!(
     ::std::ffi::CStr,
-    cfg(feature = "use_std"),
-    doc = "Requires crate feature `use_std`."
+    cfg(feature = "std"),
+    doc = "Requires crate feature `std`."
 );
 
 impl<L, R, Target> AsRef<[Target]> for Either<L, R>
@@ -1346,10 +1346,10 @@ where
     }
 }
 
-#[cfg(any(test, feature = "use_std"))]
+#[cfg(any(test, feature = "std"))]
 /// `Either` implements `Error` if *both* `L` and `R` implement it.
 ///
-/// Requires crate feature `"use_std"`
+/// Requires crate feature `"std"`
 impl<L, R> Error for Either<L, R>
 where
     L: Error,
@@ -1554,7 +1554,7 @@ fn _unsized_ref_propagation() {
 }
 
 // This "unused" method is here to ensure that compilation doesn't fail on given types.
-#[cfg(feature = "use_std")]
+#[cfg(feature = "std")]
 fn _unsized_std_propagation() {
     check_t!(::std::path::Path);
     check_t!(::std::ffi::OsStr);
