befbcf69dbcec02bd058293310d828d41a63bcf5
Nathan Memmott <memmott@chromium.org>
2025-03-13 13:06:15 -0700

    Translator: Ensure that all progress updates are normalized correctly
    This update ensures that a progress updates are normalized to zero
    and 1 rounded to the nearest 0x10000th will.
    
    Instead of updating OnDownloadProgressUpdate to a single double and
    assuming that it is normalized properly, this ensures that it is
    normalized properly by forcing the caller to pass it's total as
    0x10000. This forces the caller to be aware that it should be normalized
    to 0x10000.
    
    Bug: 391715395
    Change-Id: I3c25b7a737b9077d597c9f1d003471299858ced7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300721
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432316}

--- content/browser/ai/echo_ai_manager_impl.cc
+++ content/browser/ai/echo_ai_manager_impl.cc
@@ -27,7 +27,7 @@ namespace content {
 namespace {
 
 const int kMockDownloadPreparationTimeMillisecond = 300;
-const int kMockModelSizeBytes = 3000;
+const int kMockModelSizeBytes = 0x10000;
 
 using blink::mojom::AILanguageCodePtr;
 
