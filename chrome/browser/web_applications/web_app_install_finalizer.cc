fd9570d4236e7108ec4aab23a1dbb45a641c41af
Magdalena Skarbinska <mskarbinska@google.com>
2025-03-13 00:24:26 -0700

    iwa: Grant pop ups content setting by default to IWAs
    This change grants all IWAs the ability to open pop-up windows and
    perform redirects without requiring explicit user permission/gesture.
    The permission is added after app installation.
    
    Bug: b:340203910
    Change-Id: Ic5727dffbaf19bb2d3057f606c389a443a03ea54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299394
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Reviewed-by: Dominik Bylica <bylica@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431947}

--- chrome/browser/web_applications/web_app_install_finalizer.cc
+++ chrome/browser/web_applications/web_app_install_finalizer.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/optional_ref.h"
 #include "base/values.h"
 #include "build/build_config.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/commands/web_app_uninstall_command.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_integrity_block_data.h"
@@ -60,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_ui_manager.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/chrome_features.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/sync/protocol/web_app_specifics.pb.h"
@@ -393,6 +395,15 @@ void WebAppInstallFinalizer::OnOriginAssociationValidated(
         web_app.get(), options.iwa_options->location,
         web_app_info.isolated_web_app_version,
         options.iwa_options->integrity_block_data);
+
+    if (options.source == WebAppManagement::kIwaPolicy) {
+      HostContentSettingsMap* const host_content_settings_map =
+          HostContentSettingsMapFactory::GetForProfile(profile_);
+
+      host_content_settings_map->SetContentSettingDefaultScope(
+          web_app_info.scope, web_app_info.scope, ContentSettingsType::POPUPS,
+          CONTENT_SETTING_ALLOW);
+    }
   }
 
   web_app->SetParentAppId(web_app_info.parent_app_id);
