5c647f5afb00bff3a45495ebcc7b08cb136f95a6
Yue Zhang <yuezhanggg@chromium.org>
2025-03-13 12:59:20 -0700

    [tab suggestions] Adds JNI service skeleton
    This CL implements the framework for being able to access the
    GroupSuggestionsService KeyedService from Java in the //chrome layer
    seamlessly.
    
    The JNI aspect of the API is hidden away from client code and
    automatically setup through use of the profile keyed service factory.
    
    Bug: 397221723
    Change-Id: I1303533ea6bda89221676ed5a5ee4a9fff74c861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6346442
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432310}

---
+++ chrome/browser/tab_group_suggestion/java/src/org/chromium/chrome/browser/tab_group_suggestion/GroupSuggestionsServiceFactory.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab_group_suggestion;
+
+import org.jni_zero.JniType;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.visited_url_ranking.url_grouping.GroupSuggestionsService;
+
+/** This factory creates GroupSuggestionsService for the given {@link Profile}. */
+public final class GroupSuggestionsServiceFactory {
+
+    // Don't instantiate me.
+    private GroupSuggestionsServiceFactory() {}
+
+    /**
+     * A factory method to create or retrieve a {@link GroupSuggestionsService} object for a given
+     * profile.
+     *
+     * @return The {@link GroupSuggestionsService} for the given profile.
+     */
+    public static GroupSuggestionsService getForProfile(Profile profile) {
+        return GroupSuggestionsServiceFactoryJni.get().getForProfile(profile);
+    }
+
+    @NativeMethods
+    interface Natives {
+        GroupSuggestionsService getForProfile(@JniType("Profile*") Profile profile);
+    }
+}
