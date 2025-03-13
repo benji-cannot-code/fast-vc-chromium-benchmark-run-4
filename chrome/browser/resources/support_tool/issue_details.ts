4fcdc5adcc6315d8939f4b70b95f6913b3a733c6
Irem Uguz <iremuguz@google.com>
2025-03-13 07:01:08 -0700

    support-tool: Fix issue details UI on dark mode
    Fix the styling of email select menu by fixing the color token for
    md-select. Update the textarea element for issue description to
    cr-textarea element to inherit the correct styling. Remove obsolete
    TODO item.
    
    The screenshots of how the page looks after this change:
    https://screenshot.googleplex.com/4k935tHajARaUzH.png,
    https://screenshot.googleplex.com/9oR5VULexyBRfCA.png.
    
    Fixed: b:397653417
    Change-Id: I897b392f82237787c2259b087ad4a48060eb9f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348961
    Reviewed-by: Andrei Nedoluzhko <nedol@google.com>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1432078}

--- chrome/browser/resources/support_tool/issue_details.ts
+++ chrome/browser/resources/support_tool/issue_details.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import '/strings.m.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import 'chrome://resources/cr_elements/cr_textarea/cr_textarea.js';
 import 'chrome://resources/cr_elements/md_select.css.js';
 import './support_tool_shared.css.js';
 
