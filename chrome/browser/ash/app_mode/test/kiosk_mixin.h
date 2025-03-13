8ffd20e6054f36b1dcb87bfeaca84eb659fa6f75
Edman Anjos <edmanp@google.com>
2025-03-13 05:39:57 -0700

    kiosk: Set device local account policies in KioskMixin
    KioskMixin used to set only device policies in fake session manager,
    which is enough to launch Kiosk, but it didn't set any device local
    account policies.
    
    As a side effect, the extension system would sometimes not install or
    uninstall extensions or Chrome apps, since the policy provider was not
    ready.
    
    To more closely mirror the production behavior from DM server, this
    change makes KioskMixin set an empty device local account policy for
    Chrome apps, and a default of ExtensionInstallBlocklist set to "*" for
    Web apps and IWAs.
    
    Fixed: 401504522
    Change-Id: I655bf3155d4cd30604034d175b3368aa71135e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6339728
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432040}

--- chrome/browser/ash/app_mode/test/kiosk_mixin.h
+++ chrome/browser/ash/app_mode/test/kiosk_mixin.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_APP_MODE_TEST_KIOSK_MIXIN_H_
 #define CHROME_BROWSER_ASH_APP_MODE_TEST_KIOSK_MIXIN_H_
 
+#include <memory>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/command_line.h"
+#include "base/functional/callback_forward.h"
 #include "base/time/time.h"
 #include "base/types/strong_alias.h"
 #include "chrome/browser/app_mode/test/fake_origin_test_server_mixin.h"
@@ -162,6 +164,11 @@ class KioskMixin : public InProcessBrowserTestMixin {
     std::vector<Option> options;
   };
 
+  // Helper alias to create `ScopedUserPolicyUpdate` instances for `Configure`.
+  using UserPolicyUpdateCallback =
+      base::RepeatingCallback<std::unique_ptr<ScopedUserPolicyUpdate>(
+          std::string_view)>;
+
   // Returns the gtest parameter name for this `Config`.
   static std::string ConfigName(const testing::TestParamInfo<Config>& info);
 
@@ -203,12 +210,20 @@ class KioskMixin : public InProcessBrowserTestMixin {
 
   ~KioskMixin() override;
 
-  // Sets up Kiosk policies corresponding to the `config` in the given
-  // `scoped_update`.
+  // Sets up Kiosk device policies corresponding to `config` in the given
+  // `device_policy_update`, and sets device local account policies
+  // for each account in `config` via the updates returned by
+  // `user_policy_update_callback`.
   //
   // This can be used to simulate a policy change mid test, for example when
   // combined with a `policy::DevicePolicyCrosTestHelper`.
-  void Configure(ScopedDevicePolicyUpdate& scoped_update, const Config& config);
+  void Configure(ScopedDevicePolicyUpdate& device_policy_update,
+                 UserPolicyUpdateCallback user_policy_update_callback,
+                 const Config& config);
+  // Similar to above but defaults to a `user_policy_update_callback` that sets
+  // policies using `DeviceStateMixin`.
+  void Configure(ScopedDevicePolicyUpdate& device_policy_update,
+                 const Config& config);
 
   // Launches the given `app`, simulating a manual launch from the login screen.
   // Returns true if the launch started.
