e42d74adf64075b71e6bf8107ad5679aa4da7329
Peter Wen <wnwen@chromium.org>
2025-03-13 12:09:11 -0700

    Add @NullMarked to //com/collaboration/internal
    R=martinkong@google.com
    
    Bug: 389129271
    Change-Id: If0847b2001f7d28064e58121e884f619f9b81228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6349159
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432280}

--- components/collaboration/internal/android/java/src/org/chromium/components/collaboration/CollaborationServiceImpl.java
+++ components/collaboration/internal/android/java/src/org/chromium/components/collaboration/CollaborationServiceImpl.java
@@ -11,6 +11,7 @@ import org.jni_zero.NativeMethods;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ObserverList;
+import org.chromium.build.annotations.NullMarked;
 import org.chromium.components.data_sharing.GroupData;
 import org.chromium.components.data_sharing.member_role.MemberRole;
 import org.chromium.url.GURL;
@@ -20,6 +21,7 @@ import org.chromium.url.GURL;
  * are delegated to the native C++ class.
  */
 @JNINamespace("collaboration")
+@NullMarked
 public class CollaborationServiceImpl implements CollaborationService {
     private final ObserverList<CollaborationService.Observer> mObservers = new ObserverList<>();
     private long mNativePtr;
--- components/collaboration/internal/android/java/src/org/chromium/components/collaboration/messaging/MessagingBackendServiceBridge.java
+++ components/collaboration/internal/android/java/src/org/chromium/components/collaboration/messaging/MessagingBackendServiceBridge.java
@@ -10,6 +10,8 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.ObserverList;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.components.tab_group_sync.EitherId;
 import org.chromium.components.tab_group_sync.EitherId.EitherGroupId;
 import org.chromium.components.tab_group_sync.EitherId.EitherTabId;
@@ -21,8 +23,9 @@ import java.util.Optional;
 
 /** Implementation of {@link MessagingBackendService} that connects to the native counterpart. */
 @JNINamespace("collaboration::messaging::android")
+@NullMarked
 /*package*/ class MessagingBackendServiceBridge implements MessagingBackendService {
-    private static String getSyncId(EitherId id) {
+    private static @Nullable String getSyncId(@Nullable EitherId id) {
         if (id == null || !id.isSyncId()) {
             return null;
         }
@@ -33,7 +36,7 @@ import java.util.Optional;
             new ObserverList<>();
 
     private long mNativeMessagingBackendServiceBridge;
-    private InstantMessageDelegate mInstantMessageDelegate;
+    private @Nullable InstantMessageDelegate mInstantMessageDelegate;
 
     private MessagingBackendServiceBridge(long nativeMessagingBackendServiceBridge) {
         mNativeMessagingBackendServiceBridge = nativeMessagingBackendServiceBridge;
@@ -62,8 +65,10 @@ import java.util.Optional;
     }
 
     @Override
+    @SuppressWarnings("NullableOptional")
     public List<PersistentMessage> getMessagesForTab(
-            EitherTabId tabId, Optional</* @PersistentNotificationType */ Integer> type) {
+            @Nullable EitherTabId tabId,
+            @Nullable Optional</* @PersistentNotificationType */ Integer> type) {
         if (mNativeMessagingBackendServiceBridge == 0) {
             return new ArrayList<PersistentMessage>();
         }
@@ -93,8 +98,10 @@ import java.util.Optional;
     }
 
     @Override
+    @SuppressWarnings("NullableOptional")
     public List<PersistentMessage> getMessagesForGroup(
-            EitherGroupId groupId, Optional</* @PersistentNotificationType */ Integer> type) {
+            @Nullable EitherGroupId groupId,
+            @Nullable Optional</* @PersistentNotificationType */ Integer> type) {
         if (mNativeMessagingBackendServiceBridge == 0) {
             return new ArrayList<PersistentMessage>();
         }
@@ -124,8 +131,9 @@ import java.util.Optional;
     }
 
     @Override
+    @SuppressWarnings("NullableOptional")
     public List<PersistentMessage> getMessages(
-            Optional</* @PersistentNotificationType */ Integer> type) {
+            @Nullable Optional</* @PersistentNotificationType */ Integer> type) {
         if (mNativeMessagingBackendServiceBridge == 0) {
             return new ArrayList<PersistentMessage>();
         }
@@ -163,8 +171,9 @@ import java.util.Optional;
     }
 
     @Override
+    @SuppressWarnings("NullableOptional")
     public void clearPersistentMessage(
-            String messageId, Optional</* @PersistentNotificationType */ Integer> type) {
+            String messageId, @Nullable Optional</* @PersistentNotificationType */ Integer> type) {
         Integer type_int;
         if (type == null || !type.isPresent()) {
             type_int = PersistentNotificationType.UNDEFINED;
@@ -239,14 +248,14 @@ import java.util.Optional;
                 long nativeMessagingBackendServiceBridge,
                 MessagingBackendServiceBridge caller,
                 int localTabId,
-                String syncTabId,
+                @Nullable String syncTabId,
                 @PersistentNotificationType int type);
 
         List<PersistentMessage> getMessagesForGroup(
                 long nativeMessagingBackendServiceBridge,
                 MessagingBackendServiceBridge caller,
-                LocalTabGroupId localGroupId,
-                String syncGroupId,
+                @Nullable LocalTabGroupId localGroupId,
+                @Nullable String syncGroupId,
                 @PersistentNotificationType int type);
 
         List<PersistentMessage> getMessages(
