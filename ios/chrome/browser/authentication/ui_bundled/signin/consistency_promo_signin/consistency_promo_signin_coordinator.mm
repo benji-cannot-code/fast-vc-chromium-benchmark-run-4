f823673cbc7022746d37dbf2424a50bf929536c3
Jérôme Lebel <jlebel@chromium.org>
2025-03-13 03:49:27 -0700

    [iOS] Dismiss account choice screen after adding an identity
    In the bottom sheet, once an identity is added, the account chooser
    screen needs to be closed. The bottom sheet needs to come back to its
    main screen.
    
    This was the original implementation until the behaviour was changed
    in the patch [1], in the implementation of
    `self.addAccountCoordinator.signinCompletion`, line 150.
    Before it was calling:
    [strongSelf
        finishedWithResult:signinResult
                  identity:signinCompletionInfo.identity];
    
    But not after the patch.
    
    This patch was not supposed to change this behaviour according to the
    description and the bug description.
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/2911413/11/ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
    
    Fixed: 41481985
    Change-Id: I9c6e487c2191ea6f5fe8217bbeb9a0305c10c034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6350000
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432005}

--- ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -264,6 +264,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.defaultAccountCoordinator.selectedIdentity = completionIdentity;
 
   if (hasAccounts) {
+    [self.navigationController popViewControllerAnimated:YES];
+    [self stopAccountChooserCoordinator];
     return;
   }
   [self startSignIn];
