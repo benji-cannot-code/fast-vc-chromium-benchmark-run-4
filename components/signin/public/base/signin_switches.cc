8ae931158f9356a9dc5b08884f74e47d10f44ba9
David Roger <droger@chromium.org>
2025-03-13 06:43:21 -0700

    [Uno-D] Enable Phase 0 fast-follow flags by default
    Enables by default:
    * kOutlineSilhouetteIcon
    * kImprovedSigninUIOnDesktop
    * kImprovedSettingsUIOnDesktop
    * kAutofillRemovePaymentsButterDropdown
    
    Bug: 356606258
    Change-Id: I6451cd1120eadad0f9e003591e04a6e9a20efcf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6335520
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1432071}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -140,7 +140,7 @@ BASE_FEATURE(kInterceptBubblesDismissibleByAvatarButton,
 
 BASE_FEATURE(kImprovedSigninUIOnDesktop,
              "ImprovedSigninUIOnDesktop",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsImprovedSigninUIOnDesktopEnabled() {
   return base::FeatureList::IsEnabled(kImprovedSigninUIOnDesktop);
@@ -148,7 +148,7 @@ bool IsImprovedSigninUIOnDesktopEnabled() {
 
 BASE_FEATURE(kImprovedSettingsUIOnDesktop,
              "ImprovedSettingsUIOnDesktop",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsImprovedSettingsUIOnDesktopEnabled() {
   return base::FeatureList::IsEnabled(kImprovedSettingsUIOnDesktop);
@@ -241,7 +241,7 @@ BASE_FEATURE(kProfilesReordering,
 
 BASE_FEATURE(kOutlineSilhouetteIcon,
              "OutlineSilhouetteIcon",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kIgnoreMirrorHeadersInBackgoundTabs,
