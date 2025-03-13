b10d6417d78775d72cf55221d592053e8d2afa24
Kalvin Lee <kdlee@chromium.org>
2025-03-13 01:14:21 -0700

    spanify: Don't `subspan()` arrays
    When a rewrite is emitted to rewrite a C-style array to a `std::array`,
    we must explicitly wrap the declref in `base::span` to use offsets.
    
    Fixed: 400676553
    Change-Id: Ib45cebccf113f5f0aefa0434a10aef43a239fe02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344676
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431958}

---
+++ tools/clang/spanify/tests/span-from-array-issue-400861276-expected.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <array>
+
+#include "base/containers/span.h"
+
+int UnsafeIndex();
+
+// Expected rewrite:
+// void UseBufferUnsafely(base::span<int> int_buffer) {
+void UseBufferUnsafely(base::span<int> int_buffer) {
+  int_buffer[UnsafeIndex()] = 13;
+}
+
+void function() {
+  // Expected rewrite:
+  // std::array<int, 10> int_array;
+  std::array<int, 10> int_array;
+
+  // No rewrite expected. `base::span` is directly constructible
+  // from this expression as written.
+  UseBufferUnsafely(int_array);
+
+  // Expected rewrite:
+  // UseBufferUnsafely(base::span<int>(int_array).subspan(1));
+  UseBufferUnsafely(base::span<int>(int_array).subspan(1));
+}
