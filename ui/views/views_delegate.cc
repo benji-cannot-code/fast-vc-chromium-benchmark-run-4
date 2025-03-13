7fa69487f4fbcf8114ba6bac3215f1b9bb3955ee
Kenichi Ishibashi <bashi@chromium.org>
2025-03-12 22:14:34 -0700

    Revert "[views-ax] Create BrowserViewsAXManager instance in the correct place"
    This reverts commit db6d48dfddf25da84d62ea301e918881aa9d6555.
    
    Reason for revert: Caused Linux UBSan test failure, e.g. https://chromium-swarm.appspot.com/task?id=6fa5b90aa914e711&o=true&w=true
    
    Original change's description:
    > [views-ax] Create BrowserViewsAXManager instance in the correct place
    >
    > Previously, I introduced BrowserViewsAXManager (behind a flag) and
    > instantiated it from the ViewsDelegate constructor. This was incorrect,
    > as we rely on the AXPlatform::GetInstance that only gets created a few
    > moments later in the BrowserMainLoop. The design was invalid.
    >
    > The challenge here is that we must initialize the BrowserViewsAXManager
    > class, which lives in the ui/views layer, from the browser_main_loop.
    > Because content/browser cannot depend directly on ui/views, we must
    > instead leverage the ChromeBrowserMainExtraPartViews to initialize the
    > shared instance and store it.
    >
    > Bug: 40672441
    > Change-Id: I62432f9e547172bbe357040db29428e41db7d82c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6293824
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1431815}
    
    Bug: 40672441
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I178c4f10b9e31c62cef3ec606ceca6f8d15a8dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6349100
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1431927}

--- ui/views/views_delegate.cc
+++ ui/views/views_delegate.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "build/build_config.h"
 #include "ui/accessibility/accessibility_features.h"
+#include "ui/accessibility/platform/ax_platform.h"
 #include "ui/base/mojom/window_show_state.mojom.h"
 #include "ui/views/widget/native_widget_private.h"
 
@@ -37,6 +38,15 @@ ViewsDelegate::ViewsDelegate() {
   // will not get the replacement.
   touch_selection_menu_runner_ =
       std::make_unique<TouchSelectionMenuRunnerViews>();
+#endif
+
+#if BUILDFLAG(ENABLE_DESKTOP_AURA)
+  if (::features::IsAccessibilityTreeForViewsEnabled() &&
+      (BrowserViewsAXManager::GetInstance()->is_enabled() ||
+       !ui::AXPlatform::GetInstance().GetMode().has_mode(
+           ui::AXMode::kNativeAPIs))) {
+    BrowserViewsAXManager::GetInstance()->Enable();
+  }
 #endif
 }
 
@@ -117,15 +127,6 @@ void ViewsDelegate::OnBeforeWidgetInit(
 
 bool ViewsDelegate::WindowManagerProvidesTitleBar(bool maximized) {
   return false;
-}
-
-void ViewsDelegate::InitializeViewsAXManager() {
-#if BUILDFLAG(ENABLE_DESKTOP_AURA)
-  if (::features::IsAccessibilityTreeForViewsEnabled() &&
-      !browser_views_ax_manager_handle_) {
-    browser_views_ax_manager_handle_ = views::BrowserViewsAXManager::Create();
-  }
-#endif
 }
 
 #if BUILDFLAG(IS_MAC)
