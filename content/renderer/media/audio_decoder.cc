b371341ed87261ab244c132ca86fd3068b92684b
Tom Sepez <tsepez@chromium.org>
2025-03-13 11:16:28 -0700

    Remove file-wide unsafe buffer suppression from content/ [7 of N]
    Bug: 342213636
    Change-Id: Ie87253a13b7a9412782c86f79ad5b3318c8b876e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344995
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432249}

--- content/renderer/media/audio_decoder.cc
+++ content/renderer/media/audio_decoder.cc
@@ -3,17 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/342213636): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "content/renderer/media/audio_decoder.h"
 
 #include <stdint.h>
 
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/containers/span_writer.h"
 #include "base/logging.h"
 #include "base/strings/string_util.h"
@@ -75,8 +71,8 @@ bool DecodeAudioFileData(
   std::vector<base::SpanWriter<float>> dest_channels;
   dest_channels.reserve(number_of_channels);
   for (size_t ch = 0; ch < number_of_channels; ++ch) {
-    dest_channels.emplace_back(base::span(destination_bus->ChannelData(ch),
-                                          destination_bus->length()));
+    dest_channels.emplace_back(UNSAFE_TODO(base::span(
+        destination_bus->ChannelData(ch), destination_bus->length())));
   }
 
   // Append all `decoded_audio_packets`, channel per channel.
--- content/renderer/v8_value_converter_impl.cc
+++ content/renderer/v8_value_converter_impl.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/342213636): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "content/renderer/v8_value_converter_impl.h"
 
 #include <stddef.h>
@@ -21,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/containers/span.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
@@ -519,7 +515,7 @@ std::unique_ptr<base::Value> V8ValueConverterImpl::FromV8ArrayBuffer(
     const auto* data = static_cast<const uint8_t*>(array_buffer->Data());
     const size_t byte_length = array_buffer->ByteLength();
     return base::Value::ToUniquePtrValue(
-        base::Value(base::span(data, byte_length)));
+        base::Value(UNSAFE_TODO(base::span(data, byte_length))));
   }
   if (val->IsArrayBufferView()) {
     v8::Local<v8::ArrayBufferView> view = val.As<v8::ArrayBufferView>();
