ba714d0b07f38234ec9f0cf1b34b959a4e9a86db
Dana Fried <dfried@chromium.org>
2025-03-14 08:13:32 -0700

    [User Education] Custom WebUI Help Bubbles
    This CL allows a Top Chrome WebUI Bubble Dialog to be used as a custom
    UI help bubble.
    
    Certain requirements are put on the WebUI controller associated with the
    custom dialog.
    
    Tests are provided, but full README documentation will come in a
    follow-up CL.
    
    Bug: 399398690
    Change-Id: Ic7e6651108c26d8fed6bc98c242f13c7ac767962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333856
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432738}

---
+++ chrome/test/data/webui/cr_components/help_bubble/test_custom_help_bubble.html.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {TestCustomHelpBubbleElement} from './test_custom_help_bubble.js';
+
+export function getHtml(this: TestCustomHelpBubbleElement) {
+  // clang-format off
+  return html`
+<button id="cancel" @click="${this.onCancelButton_}">Cancel</button>
+<button id="dismiss" @click="${this.onDismissButton_}">Dismiss</button>
+<button id="snooze" @click="${this.onSnoozeButton_}">Snooze</button>
+<button id="action" @click="${this.onActionButton_}">Action</button>
+`;
+  // clang-format on
+}
---
+++ ui/webui/resources/cr_components/help_bubble/custom_help_bubble_proxy.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {CustomHelpBubbleHandlerInterface} from './custom_help_bubble.mojom-webui.js';
+import {CustomHelpBubbleHandlerFactory, CustomHelpBubbleHandlerRemote} from './custom_help_bubble.mojom-webui.js';
+
+export interface CustomHelpBubbleProxy {
+  getHandler(): CustomHelpBubbleHandlerInterface;
+}
+
+export class CustomHelpBubbleProxyImpl implements CustomHelpBubbleProxy {
+  private handler_ = new CustomHelpBubbleHandlerRemote();
+
+  constructor() {
+    const factory = CustomHelpBubbleHandlerFactory.getRemote();
+    factory.createCustomHelpBubbleHandler(
+        this.handler_.$.bindNewPipeAndPassReceiver());
+  }
+
+  static getInstance(): CustomHelpBubbleProxy {
+    return instance || (instance = new CustomHelpBubbleProxyImpl());
+  }
+
+  static setInstance(obj: CustomHelpBubbleProxy) {
+    instance = obj;
+  }
+
+  getHandler(): CustomHelpBubbleHandlerRemote {
+    return this.handler_;
+  }
+}
+
+let instance: CustomHelpBubbleProxy|null = null;
