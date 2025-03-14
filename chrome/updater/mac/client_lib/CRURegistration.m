e1dfaf93cb6219376ca18c09a433cddc8a30ac3d
Adam Norberg <norberg@google.com>
2025-03-14 10:06:18 -0700

    Explicitly require ARC in CRURegistration.
    I spent a day trying to debug an access violation reported by a team
    trying to migrate from Keystone to Omaha 4 for first installs, which
    turned out to be due to disabling ARC in their build, and their first
    attempt to enable it didn't work, which we didn't discover until later.
    Let's let the compiler spot this problem if it pops up again...
    
    Change-Id: I666872547370db1cb267ca34bc9dd2f44ca8bb86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6353865
    Commit-Queue: Adam Norberg <norberg@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432818}

--- chrome/updater/mac/client_lib/CRURegistration.m
+++ chrome/updater/mac/client_lib/CRURegistration.m
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "CRURegistration requires ARC support. Compile with `-fobjc-arc.`"
+#endif
+
 #import "CRURegistration.h"
 
 #import <Foundation/Foundation.h>
