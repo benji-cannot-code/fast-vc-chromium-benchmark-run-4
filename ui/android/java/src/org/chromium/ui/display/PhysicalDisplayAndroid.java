b732bada2635fbce25b83f24991548c50ba06063
Daniil Hrybchuk <renkens@google.com>
2025-03-14 11:01:04 -0700

    ui-display: Change deprecated `BuildInfo` to `DeviceInfo`
    Change-Id: I88246dcb9272416e44ecd651f57092ce4aab83db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333563
    Commit-Queue: Daniil Hrybchuk <renkens@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432875}

--- ui/android/java/src/org/chromium/ui/display/PhysicalDisplayAndroid.java
+++ ui/android/java/src/org/chromium/ui/display/PhysicalDisplayAndroid.java
@@ -25,9 +25,9 @@ import android.view.WindowManager;
 import androidx.annotation.RequiresApi;
 import androidx.core.os.BuildCompat;
 
-import org.chromium.base.BuildInfo;
 import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.DeviceInfo;
 import org.chromium.base.Log;
 import org.chromium.base.StrictModeContext;
 import org.chromium.base.ThreadUtils;
@@ -221,7 +221,9 @@ import java.util.function.Consumer;
         if (USE_CONFIGURATION) {
             Context appContext = ContextUtils.getApplicationContext();
             // `createWindowContext` on some devices writes to disk. See crbug.com/1408587.
-            try (StrictModeContext ignored = StrictModeContext.allowAllThreadPolicies()) {
+            try (@SuppressWarnings("unused")
+                    StrictModeContext strictModeContext =
+                            StrictModeContext.allowAllThreadPolicies()) {
                 mWindowContext =
                         appContext.createWindowContext(
                                 display, WindowManager.LayoutParams.TYPE_APPLICATION, null);
@@ -279,7 +281,7 @@ import java.util.function.Consumer;
 
         DisplayMetrics displayMetrics = mWindowContext.getResources().getDisplayMetrics();
 
-        if (BuildInfo.getInstance().isAutomotive
+        if (DeviceInfo.isAutomotive()
                 && CommandLine.getInstance()
                         .hasSwitch(DisplaySwitches.AUTOMOTIVE_WEB_UI_SCALE_UP_ENABLED)) {
             mDisplay.getRealMetrics(displayMetrics);
@@ -300,8 +302,7 @@ import java.util.function.Consumer;
                 mWindowContext.getDisplay());
     }
 
-    /* package */
-    void onDisplayRemoved() {
+    /* package */ void onDisplayRemoved() {
         if (USE_CONFIGURATION) {
             assumeNonNull(mWindowContext);
             assumeNonNull(mComponentCallbacks);
@@ -327,7 +328,7 @@ import java.util.function.Consumer;
         display.getRealSize(size);
         display.getRealMetrics(displayMetrics);
 
-        if (BuildInfo.getInstance().isAutomotive
+        if (DeviceInfo.isAutomotive()
                 && CommandLine.getInstance()
                         .hasSwitch(DisplaySwitches.AUTOMOTIVE_WEB_UI_SCALE_UP_ENABLED)) {
             DisplayUtil.scaleUpDisplayMetricsForAutomotive(
