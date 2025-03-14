2f5b067c4b5c6f22db02a7dae112b479f99de7c0
Sky Malice <skym@chromium.org>
2025-03-14 11:36:55 -0700

    Remove theme observers from tab toolstrip.
    Change-Id: I5d6d1847d12cf6e04838237a32845f8e2a3fac33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357994
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432896}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediatorUnitTest.java
@@ -916,6 +916,8 @@ public class TabGroupUiMediatorUnitTest {
         verify(mTabModelSupplier).removeObserver(mTabModelSupplierObserverCaptor.capture());
         verify(mTabGroupModelFilter, times(2))
                 .removeTabGroupObserver(mTabGroupModelFilterObserverArgumentCaptor.capture());
+        verify(mThemeColorProvider).removeThemeColorObserver(any());
+        verify(mThemeColorProvider).removeTintObserver(any());
     }
 
     @Test
