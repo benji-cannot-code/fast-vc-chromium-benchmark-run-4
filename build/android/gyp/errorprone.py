ccd87127890a8ea9b742ca648949e278fbf5003c
Andrew Grieve <agrieve@chromium.org>
2025-03-14 13:50:16 -0700

    NullAway: Consider PreferenceFragmentCompat.onCreatePreferences an @Initializer
    Bug: 389129271
    Change-Id: Ifb5a87b53f814227e49b71b6b42e10e142ffb1cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356673
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432977}

--- build/android/gyp/errorprone.py
+++ build/android/gyp/errorprone.py
@@ -205,6 +205,7 @@ def main():
         'android.content.ContentProvider.attachInfo',
         'android.content.ContentProvider.onCreate',
         'android.content.ContextWrapper.attachBaseContext',
+        'androidx.preference.PreferenceFragmentCompat.onCreatePreferences',
     ]
     errorprone_flags += [
         '-XepOpt:NullAway:KnownInitializers=' + ','.join(init_methods)
