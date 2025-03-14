f43a04f7963c9396cc1e1cdc48a85ae9f596d178
Oleg Nitz <ovn@google.com>
2025-03-14 01:20:37 -0700

    [Web Serial API] Add feature flag for Web Serial API on Android.
    Bug: https://crbug.com/359783414
    Change-Id: Ic96b558f75e5e7725eb4952e3dd7cdbf09cd0a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6352776
    Commit-Queue: Oleg Nitz <ovn@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432576}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4755,6 +4755,9 @@ extern const char kSupervisedUserLocalWebApprovalsDescription[];
 extern const char kHistoryOptInEntryPointsName[];
 extern const char kHistoryOptInEntryPointsDescription[];
 
+extern const char kWebSerialAPIName[];
+extern const char kWebSerialAPIDescription[];
+
 extern const char kSupervisedUserForceSigninWithCapabilitiesName[];
 extern const char kSupervisedUserForceSigninWithCapabilitiesDescription[];
 #endif  // BUILDFLAG(IS_ANDROID)
