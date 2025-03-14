4bd8746a4ac61de44f5a552a53276dd9e2c192b1
Sky Malice <skym@chromium.org>
2025-03-14 13:23:45 -0700

    Wire theme color to image tiles background.
    The border (stroke) and background (used during loading) of the shared
    image tiles should match the color of the tab toolstip. Typically when
    not in incognito, this is surface-0 during day and surface-3 during
    night. However when the toolbar is put on the bottom of the screen, this
    because surface-0 during night and potentially website themed during
    day.
    
    The tab toolstrip already has a theme color provider wired to update its
    color at runtime. We simply need to wire this up to the shared image
    tiles.
    
    Bug: 403344917
    Change-Id: I028fc07a98931a02c0b95f846c6c5399c83258a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6355583
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432965}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
@@ -33,6 +33,7 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.collaboration.CollaborationServiceFactory;
 import org.chromium.chrome.browser.data_sharing.DataSharingServiceFactory;
+import org.chromium.chrome.browser.data_sharing.ui.shared_image_tiles.SharedImageTilesConfig;
 import org.chromium.chrome.browser.data_sharing.ui.shared_image_tiles.SharedImageTilesCoordinator;
 import org.chromium.chrome.browser.layouts.LayoutStateProvider;
 import org.chromium.chrome.browser.layouts.LayoutStateProvider.LayoutStateObserver;
@@ -148,6 +149,7 @@ public class TabGroupUiMediator implements BackPressHandler {
 
     // These should only be used when regular (non-incognito) tabs are set in the model.
     private final @Nullable SharedImageTilesCoordinator mSharedImageTilesCoordinator;
+    private final @Nullable SharedImageTilesConfig.Builder mSharedImageTilesConfigBuilder;
     private final @Nullable TransitiveSharedGroupObserver mTransitiveSharedGroupObserver;
 
     private final LayoutStateObserver mLayoutStateObserver;
@@ -173,7 +175,8 @@ public class TabGroupUiMediator implements BackPressHandler {
                     LazyOneshotSupplier<TabGridDialogMediator.DialogController>
                             dialogControllerSupplier,
             ObservableSupplier<Boolean> omniboxFocusStateSupplier,
-            SharedImageTilesCoordinator sharedImageTilesCoordinator,
+            @Nullable SharedImageTilesCoordinator sharedImageTilesCoordinator,
+            @Nullable SharedImageTilesConfig.Builder sharedImageTilesConfigBuilder,
             ThemeColorProvider themeColorProvider,
             Callback<Object> onSnapshotTokenChange,
             ObservableSupplierImpl<Object> childTokenSupplier) {
@@ -186,6 +189,7 @@ public class TabGroupUiMediator implements BackPressHandler {
         mTabGridDialogControllerSupplier = dialogControllerSupplier;
         mOmniboxFocusStateSupplier = omniboxFocusStateSupplier;
         mSharedImageTilesCoordinator = sharedImageTilesCoordinator;
+        mSharedImageTilesConfigBuilder = sharedImageTilesConfigBuilder;
         mThemeColorProvider = themeColorProvider;
 
         mThemeColorProvider.addThemeColorObserver(mThemeColorObserver);
@@ -364,11 +368,17 @@ public class TabGroupUiMediator implements BackPressHandler {
         layoutStateProvider.addObserver(mLayoutStateObserver);
     }
 
+    // TODO(skym): Should be private.
     public void onThemeColorChanged(@ColorInt int color, boolean shouldAnimate) {
         mModel.set(BACKGROUND_COLOR, color);
+        if (mSharedImageTilesCoordinator != null && mSharedImageTilesConfigBuilder != null) {
+            mSharedImageTilesConfigBuilder.setBorderColor(color).setBackgroundColor(color);
+            mSharedImageTilesCoordinator.updateConfig(mSharedImageTilesConfigBuilder.build());
+        }
         publishSnapshotToken();
     }
 
+    // TODO(skym): Should be private.
     public void onTintChanged(
             ColorStateList tint, ColorStateList activityFocusTint, int brandedColorScheme) {
         mModel.set(TINT, mThemeColorProvider.getTint());
