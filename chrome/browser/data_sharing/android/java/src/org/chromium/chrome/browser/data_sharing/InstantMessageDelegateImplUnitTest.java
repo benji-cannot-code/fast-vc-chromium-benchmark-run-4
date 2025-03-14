7ee24b9f0cd20f47d45478a66f83c0144dac6486
Calder Kitagawa <ckitagawa@chromium.org>
2025-03-14 12:03:00 -0700

    [Tab Group] Remove getRelatedTabCountForRootId from sync/share
    Remove root id from a few places in tab group sync/share code.
    
    Bug: 399354986
    Change-Id: I8530b6cba488090e0cd0023a8c292d7b2a37b688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6358014
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432914}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/InstantMessageDelegateImplUnitTest.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/InstantMessageDelegateImplUnitTest.java
@@ -142,6 +142,7 @@ public class InstantMessageDelegateImplUnitTest {
 
         when(mWindowAndroid.getActivity()).thenReturn(new WeakReference<>(activity));
         when(mTabGroupModelFilter.getRootIdFromTabGroupId(TAB_GROUP_ID)).thenReturn(TAB_ID);
+        when(mTabGroupModelFilter.tabGroupExists(TAB_GROUP_ID)).thenReturn(true);
         when(mTabGroupModelFilter.getTabModel()).thenReturn(mTabModel);
         when(mTabModel.getTabCreator()).thenReturn(mTabCreator);
 
@@ -188,8 +189,7 @@ public class InstantMessageDelegateImplUnitTest {
 
     @Test
     public void testDisplayInstantaneousMessage_NotInTabModel() {
-        when(mTabGroupModelFilter.getRootIdFromTabGroupId(TAB_GROUP_ID))
-                .thenReturn(Tab.INVALID_TAB_ID);
+        when(mTabGroupModelFilter.tabGroupExists(any())).thenReturn(false);
         mDelegate.displayInstantaneousMessage(
                 Arrays.asList(newInstantMessage(CollaborationEvent.TAB_REMOVED)), mSuccessCallback);
         verify(mManagedMessageDispatcher, never()).enqueueWindowScopedMessage(any(), anyBoolean());
@@ -335,9 +335,7 @@ public class InstantMessageDelegateImplUnitTest {
 
     @Test
     public void testCollaborationMemberAdded_FallbackTitle() {
-        when(mTabGroupModelFilter.getRootIdFromTabGroupId(any())).thenReturn(TAB_ID);
-        when(mTabGroupModelFilter.getRelatedTabCountForRootId(anyInt()))
-                .thenReturn(TAB_COUNT_IN_GROUP);
+        when(mTabGroupModelFilter.getTabCountForGroup(any())).thenReturn(TAB_COUNT_IN_GROUP);
         InstantMessage message = newInstantMessage(CollaborationEvent.COLLABORATION_MEMBER_ADDED);
         message.attribution.tabGroupMetadata.lastKnownTitle = "";
         mDelegate.displayInstantaneousMessage(Arrays.asList(message), mSuccessCallback);
@@ -378,7 +376,7 @@ public class InstantMessageDelegateImplUnitTest {
         // Remove the group from the sync service, since it's being deleted. This will make fetching
         // the title difficult.
         reset(mTabGroupSyncService);
-        when(mTabGroupModelFilter.getRelatedTabCountForRootId(anyInt())).thenReturn(1);
+        when(mTabGroupModelFilter.getTabCountForGroup(any())).thenReturn(1);
 
         InstantMessage message = newInstantMessage(CollaborationEvent.TAB_GROUP_REMOVED);
         message.attribution.tabGroupMetadata.lastKnownTitle = null;
@@ -399,8 +397,7 @@ public class InstantMessageDelegateImplUnitTest {
     @Test
     public void testCollaborationRemoved_FallbackTitle() {
         when(mTabGroupModelFilter.getRootIdFromTabGroupId(any())).thenReturn(TAB_ID);
-        when(mTabGroupModelFilter.getRelatedTabCountForRootId(anyInt()))
-                .thenReturn(TAB_COUNT_IN_GROUP);
+        when(mTabGroupModelFilter.getTabCountForGroup(TAB_GROUP_ID)).thenReturn(TAB_COUNT_IN_GROUP);
         InstantMessage message = newInstantMessage(CollaborationEvent.TAB_GROUP_REMOVED);
         message.attribution.tabGroupMetadata.lastKnownTitle = "";
         mDelegate.displayInstantaneousMessage(Arrays.asList(message), mSuccessCallback);
