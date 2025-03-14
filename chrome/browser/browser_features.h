7184546b8df5ccfce39fb8da9d16cd46ed07f4b6
Ivana Žužić <izuzic@google.com>
2025-03-14 08:58:30 -0700

    Clear user data to prevent a crash in ~StoreMetricsReporter
    StoreMetricsReporter holds a raw pointer to PrefService since this CL:
    crrev.com/c/5587720.
    Preferences are owned by ProfileImpl and they are destroyed before
    Profile's user data that owns the metrics reporter since the user data
    is stored in a base class of ProfileImpl.
    
    As it was proposed in the bug, ProfileImpl calls ClearAllUserData() in
    its destructor to avoid user data accessing the destroyed state.
    
    Bug: 402028628
    Change-Id: Ia8c95e476ef9a9e0ee9af936a3a6c1845788527a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357189
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1432761}

--- chrome/browser/browser_features.h
+++ chrome/browser/browser_features.h
@@ -37,6 +37,7 @@ BASE_DECLARE_FEATURE(kBookmarksTreeView);
 BASE_DECLARE_FEATURE(kBookmarkTriggerForPrerender2);
 BASE_DECLARE_FEATURE(kCertificateTransparencyAskBeforeEnabling);
 BASE_DECLARE_FEATURE(kCertVerificationNetworkTime);
+BASE_DECLARE_FEATURE(kClearUserDataUponProfileDestruction);
 
 BASE_DECLARE_FEATURE(kContentUsesBrowserThemeColorMode);
 
