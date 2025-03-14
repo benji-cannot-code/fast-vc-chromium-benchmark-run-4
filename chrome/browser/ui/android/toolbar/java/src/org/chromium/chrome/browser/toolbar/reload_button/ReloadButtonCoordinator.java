c8123b017bf9789896bb6783a10abbd13a29615c
Vlad Korotkevich <vkorotkevich@google.com>
2025-03-14 12:20:33 -0700

    [Reload Button] Extract long click logic
    This CL moves long click handling from ToolbarTablet to
    ReloadButtonMediator.
    
    Bug: 402347979
    Change-Id: If12530a37fa190759e330e9e2f1adce34aef6654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6349038
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Vlad Korotkevich <vkorotkevich@google.com>
    Auto-Submit: Vlad Korotkevich <vkorotkevich@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432927}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/reload_button/ReloadButtonCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/reload_button/ReloadButtonCoordinator.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
+import org.chromium.ui.widget.Toast;
 
 /**
  * Root component for the reload button. Exposes public API to change button's state and allows
@@ -37,7 +38,12 @@ public class ReloadButtonCoordinator {
      */
     public ReloadButtonCoordinator(ImageButton view, ReloadButtonCoordinator.Delegate delegate) {
         final var model = new PropertyModel.Builder(ReloadButtonProperties.ALL_KEYS).build();
-        mMediator = new ReloadButtonMediator(model, delegate, view.getResources());
+        mMediator =
+                new ReloadButtonMediator(
+                        model,
+                        delegate,
+                        (text) -> Toast.showAnchoredToast(view.getContext(), view, text),
+                        view.getResources());
         PropertyModelChangeProcessor.create(model, view, ReloadButtonViewBinder::bind);
     }
 
