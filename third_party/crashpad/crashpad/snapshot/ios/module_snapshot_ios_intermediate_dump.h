74de5a44005521a0dca2ca76053a760418f228cf
Justin Cohen <justincohen@google.com>
2025-03-14 10:22:20 -0700

    Update Crashpad to 3eba16f1ca616412593e93c54b401cf1161b859d
    c405b3e9a060 ios: Support intermediate dump extra_memory_ranges
    3eba16f1ca61 ios: Correct bad dep
    
    Change-Id: Ib130a8071e3199b1bc10656a2963407a9f769bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357532
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432833}

--- third_party/crashpad/crashpad/snapshot/ios/module_snapshot_ios_intermediate_dump.h
+++ third_party/crashpad/crashpad/snapshot/ios/module_snapshot_ios_intermediate_dump.h
@@ -79,6 +79,7 @@ class ModuleSnapshotIOSIntermediateDump final : public ModuleSnapshot {
 
   // Used by ProcessSnapshot
   std::vector<const MemorySnapshot*> ExtraMemory() const;
+  std::vector<const MemorySnapshot*> IntermediateDumpExtraMemory() const;
 
  private:
   std::string name_;
@@ -94,6 +95,8 @@ class ModuleSnapshotIOSIntermediateDump final : public ModuleSnapshot {
   std::vector<AnnotationSnapshot> annotation_objects_;
   std::vector<std::unique_ptr<internal::MemorySnapshotIOSIntermediateDump>>
       extra_memory_;
+  std::vector<std::unique_ptr<internal::MemorySnapshotIOSIntermediateDump>>
+      intermediate_dump_extra_memory_;
   InitializationStateDcheck initialized_;
 };
 
