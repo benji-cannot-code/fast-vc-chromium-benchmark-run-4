13d9de97f80adddad41ea1d12881af3dcc6f56e4
Camillia Smith Barnes <cammie@chromium.org>
2025-03-14 08:59:38 -0700

    Shared Storage: Add operator<< for new Access struct in test observer
    When we changed from using a tuple to using a struct in
    https://crrev.com/c/6335479, we forgot to implement an ostream
    operator for the struct. Since the struct is used in many test
    expectations, it's essential that we have one.
    
    Bug: 403342572
    Change-Id: I0c9a3bf8808fdafc3fec7770e07fba238613a662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6353960
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432762}

--- content/browser/shared_storage/test_shared_storage_observer.cc
+++ content/browser/shared_storage/test_shared_storage_observer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstddef>
 #include <optional>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -20,6 +21,63 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace content {
+
+namespace {
+
+using AccessScope = blink::SharedStorageAccessScope;
+using AccessMethod = TestSharedStorageObserver::AccessMethod;
+
+std::string SerializeScope(AccessScope scope) {
+  switch (scope) {
+    case AccessScope::kWindow:
+      return "Window";
+    case AccessScope::kSharedStorageWorklet:
+      return "SharedStorageWorklet";
+    case AccessScope::kProtectedAudienceWorklet:
+      return "ProtectedAudienceWorklet";
+    case AccessScope::kHeader:
+      return "Header";
+  }
+  NOTREACHED();
+}
+
+std::string SerializeMethod(AccessMethod method) {
+  switch (method) {
+    case AccessMethod::kAddModule:
+      return "AddModule";
+    case AccessMethod::kCreateWorklet:
+      return "CreateWorklet";
+    case AccessMethod::kSelectURL:
+      return "SelectURL";
+    case AccessMethod::kRun:
+      return "Run";
+    case AccessMethod::kBatchUpdate:
+      return "BatchUpdate";
+    case AccessMethod::kSet:
+      return "Set";
+    case AccessMethod::kAppend:
+      return "Append";
+    case AccessMethod::kDelete:
+      return "Delete";
+    case AccessMethod::kClear:
+      return "Clear";
+    case AccessMethod::kGet:
+      return "Get";
+    case AccessMethod::kKeys:
+      return "Keys";
+    case AccessMethod::kValues:
+      return "Values";
+    case AccessMethod::kEntries:
+      return "Entries";
+    case AccessMethod::kLength:
+      return "Length";
+    case AccessMethod::kRemainingBudget:
+      return "RemainingBudget";
+  }
+  NOTREACHED();
+}
+
+}  // namespace
 
 TestSharedStorageObserver::TestSharedStorageObserver() = default;
 TestSharedStorageObserver::~TestSharedStorageObserver() = default;
@@ -53,5 +111,15 @@ void TestSharedStorageObserver::ExpectAccessObserved(
 
 bool operator==(const TestSharedStorageObserver::Access& lhs,
                 const TestSharedStorageObserver::Access& rhs) = default;
+
+std::ostream& operator<<(std::ostream& os,
+                         const TestSharedStorageObserver::Access& access) {
+  os << "{ Access Scope: " << SerializeScope(access.scope)
+     << "; Access Method: " << SerializeMethod(access.method)
+     << "; Main Frame Id: " << access.main_frame_id.GetUnsafeValue()
+     << "; Owner Origin: " << access.owner_origin
+     << "; Params: " << access.params << " }";
+  return os;
+}
 
 }  // namespace content
--- content/browser/shared_storage/test_shared_storage_observer.h
+++ content/browser/shared_storage/test_shared_storage_observer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_SHARED_STORAGE_TEST_SHARED_STORAGE_OBSERVER_H_
 
 #include <optional>
+#include <ostream>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -26,13 +27,13 @@ class TestSharedStorageObserver
   using AccessScope = blink::SharedStorageAccessScope;
 
   struct Access {
-    blink::SharedStorageAccessScope scope;
+    AccessScope scope;
     AccessMethod method;
     FrameTreeNodeId main_frame_id;
     std::string owner_origin;
     SharedStorageEventParams params;
-    friend bool operator==(const TestSharedStorageObserver::Access& lhs,
-                           const TestSharedStorageObserver::Access& rhs);
+    friend bool operator==(const Access& lhs, const Access& rhs);
+    friend std::ostream& operator<<(std::ostream& os, const Access& access);
   };
 
   TestSharedStorageObserver();
