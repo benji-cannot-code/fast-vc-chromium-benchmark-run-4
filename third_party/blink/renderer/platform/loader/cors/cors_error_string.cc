2a62a3c6fba7a4d64fad8cd57f5076d435a1db63
Benedikt Meurer <bmeurer@chromium.org>
2025-03-14 08:38:23 -0700

    [DevTools] Remove confusing hint to use 'no-cors'.
    Some CORS error messages generated by Blink contain a hint that if an
    opaque response serves the developers needs, they should set the
    request's mode to 'no-cors'. Apparently this does more harm than
    good[^1], and so we'll remove that hint.
    
    [^1]: https://evertpot.com/no-cors/
    
    Bug: 371503691
    Fixed: 398193018
    Change-Id: Ib26f28bb20c3d55612f6303e0c533c427e0e4301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357590
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432746}

--- third_party/blink/renderer/platform/loader/cors/cors_error_string.cc
+++ third_party/blink/renderer/platform/loader/cors/cors_error_string.cc
@@ -86,9 +86,7 @@ String GetErrorStringForIssueSummary(const network::CorsErrorStatus& status,
                                      const AtomicString& initiator_name) {
   StringBuilder builder;
   static constexpr char kNoCorsInformation[] =
-      " Have the server send the header with a valid value, or, if an opaque "
-      "response serves your needs, set the request's mode to 'no-cors' to "
-      "fetch the resource with CORS disabled.";
+      " Have the server send the header with a valid value.";
 
   using CorsError = network::mojom::CorsError;
   const StringView hint(base::as_byte_span(status.failed_parameter));
@@ -128,11 +126,6 @@ String GetErrorStringForIssueSummary(const network::CorsErrorStatus& status,
       builder.Append(
           "No 'Access-Control-Allow-Origin' header is present on the "
           "requested resource.");
-      if (initiator_name == fetch_initiator_type_names::kFetch) {
-        builder.Append(
-            " If an opaque response serves your needs, set the request's "
-            "mode to 'no-cors' to fetch the resource with CORS disabled.");
-      }
       break;
     case CorsError::kMultipleAllowOriginValues:
     case CorsError::kPreflightMultipleAllowOriginValues:
@@ -278,9 +271,7 @@ String GetErrorStringForConsoleMessage(const network::CorsErrorStatus& status,
                                        const AtomicString& initiator_name) {
   StringBuilder builder;
   static constexpr char kNoCorsInformation[] =
-      " Have the server send the header with a valid value, or, if an opaque "
-      "response serves your needs, set the request's mode to 'no-cors' to "
-      "fetch the resource with CORS disabled.";
+      " Have the server send the header with a valid value.";
 
   using CorsError = network::mojom::CorsError;
   const StringView hint(base::as_byte_span(status.failed_parameter));
@@ -335,11 +326,6 @@ String GetErrorStringForConsoleMessage(const network::CorsErrorStatus& status,
       builder.Append(
           "No 'Access-Control-Allow-Origin' header is present on the "
           "requested resource.");
-      if (initiator_name == fetch_initiator_type_names::kFetch) {
-        builder.Append(
-            " If an opaque response serves your needs, set the request's "
-            "mode to 'no-cors' to fetch the resource with CORS disabled.");
-      }
       break;
     case CorsError::kMultipleAllowOriginValues:
     case CorsError::kPreflightMultipleAllowOriginValues:
