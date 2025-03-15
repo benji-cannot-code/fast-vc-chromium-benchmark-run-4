befb4fe4d3b4c22be383689cf3baf1b63574bb8d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-03-15 07:25:12 -0700

    Import wpt@6cb542d1e95932eb552a788dd6c4b957906d1839
    https://github.com/web-platform-tests/wpt/compare/49f733c76...6cb542d1e
    
    Using wpt-import in Chromium b4b8ab9794e879bcde74c84f99dbebc681b50aa3.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Iaaba603a3f98c2e6a6f389b6ff5e7cd281216e66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6355612
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1433131}

--- third_party/blink/web_tests/external/wpt/url/url-statics-canparse.any.js
+++ third_party/blink/web_tests/external/wpt/url/url-statics-canparse.any.js
@@ -17,6 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "expected": false
   },
   {
+    "url": undefined,
+    "base": "https://test:test/",
+    "expected": false
+  },
+  {
     "url": "aaa:/b",
     "base": undefined,
     "expected": true
