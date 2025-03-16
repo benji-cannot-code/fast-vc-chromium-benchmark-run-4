c0ea8dca53b49fc7ba5ea93e1e8ef1f4eaa5db20
Thomas Lukaszewicz <tluk@chromium.org>
2025-03-16 14:12:04 -0700

    [blink] Ensure queued device change events do not crash renderers
    This CL adds additional handling to MediaDevices to ensure that
    scheduled device change events do not crash renderers after its
    associated ExecutionContext has been destroyed.
    
    This is handled in the following ways:
    
      - Ensuring the receiver_ is reset when the execution context is
        destroyed, ensuring in-flight OnDevicesChanged mojo
        messages are not delivered after-the-fact
    
      - Ensuring any queued ScheduleDispatchEvent() tasks appropriately
        no-op after the execution context is destroyed.
    
    A common situation in which the ExecutionContext can be destroyed
    is during blink::Document navigations - where the LocalFrame's
    document and dom window are replaced.
    
    Bug: 403348706
    Change-Id: Ied2cfdc8546f9c857cafbb8f4cc19867c3eb8899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356063
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433287}

--- third_party/blink/renderer/modules/mediastream/media_devices.cc
+++ third_party/blink/renderer/modules/mediastream/media_devices.cc
@@ -1124,6 +1124,7 @@ void MediaDevices::ContextDestroyed() {
 
   stopped_ = true;
   enumerate_device_requests_.clear();
+  StopObserving();
 }
 
 void MediaDevices::OnDevicesChanged(
@@ -1131,6 +1132,7 @@ void MediaDevices::OnDevicesChanged(
     const Vector<WebMediaDeviceInfo>& device_infos) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(GetExecutionContext());
+  DCHECK(!stopped_);
   if (std::ranges::equal(current_device_infos_[static_cast<wtf_size_t>(type)],
                          device_infos, EqualDeviceForDeviceChange)) {
     return;
@@ -1158,6 +1160,10 @@ void MediaDevices::MaybeFireDeviceChangeEvent(bool has_permission) {
 
 void MediaDevices::ScheduleDispatchEvent(Event* event) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (stopped_) {
+    return;
+  }
+
   scheduled_events_.push_back(event);
   if (dispatch_scheduled_events_task_handle_.IsActive()) {
     return;
