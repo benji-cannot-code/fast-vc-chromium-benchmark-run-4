01eb3d7b1b6a23de8058c54819df82db549b1127
Kartar Singh <kartarsingh@google.com>
2025-03-17 07:39:05 -0700

    [InputVizard] Handle in process gpu case.
    In case of InputVizard enabled, the InputTransferToken is forwarded from
    Viz to Browser over Binder which is used by Browser for transfer touch
    sequence. The Binder connection doesn't exist when gpu is in process. In
    this change we have set up "forwarding" of token for in process gpu
    case.
    
    Test: `--single-process` mode with `InputOnViz` flag enabled, doesn't
    crash with this change.
    
    Bug: 394852489
    Change-Id: I2178c0b87348ce128350ec01360d09ec2ef0b27c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6340771
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433516}

--- content/public/android/java/src/org/chromium/content/browser/GpuProcessCallback.java
+++ content/public/android/java/src/org/chromium/content/browser/GpuProcessCallback.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
-import android.os.Build;
 import android.view.Surface;
 
 import org.jni_zero.JNINamespace;
@@ -14,11 +13,10 @@ import org.jni_zero.NativeMethods;
 
 import org.chromium.base.UnguessableToken;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.content.browser.input.InputTokenForwarderManager;
 import org.chromium.content.common.IGpuProcessCallback;
 import org.chromium.content.common.InputTransferTokenWrapper;
 import org.chromium.content.common.SurfaceWrapper;
-import org.chromium.content_public.browser.InputTransferHandler;
-import org.chromium.content_public.browser.SurfaceInputTransferHandlerMap;
 
 @JNINamespace("content")
 @NullMarked
@@ -37,13 +35,7 @@ class GpuProcessCallback extends IGpuProcessCallback.Stub {
 
     @Override
     public void forwardInputTransferToken(int surfaceId, InputTransferTokenWrapper wrapper) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-            return;
-        }
-        InputTransferHandler handler = SurfaceInputTransferHandlerMap.getMap().get(surfaceId);
-        if (handler != null) {
-            handler.setVizToken(wrapper.getInputTransferToken());
-        }
+        InputTokenForwarderManager.onTokenReceived(surfaceId, wrapper.getInputTransferToken());
     }
 
     @NativeMethods
