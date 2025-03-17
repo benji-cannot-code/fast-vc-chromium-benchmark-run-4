f66707e997f5079d24b7cdb96ebc27d56e13e3ea
Thomas Thrainer <thomasth@google.com>
2025-03-17 07:17:24 -0700

    Refactor StandaloneTrustedVaultBackendTest.
    This change introduces StandaloneTrustedVaultStorage::FileAccess, and
    introduces a fake version of that in StandaloneTrustedVaultBackendTest.
    
    The file system / migration specific tests in
    StandaloneTrustedVaultBackendTest are moved to
    StandaloneTrustedVaultStorageImplTest.
    
    Bug: b:398160323
    Change-Id: I379eece66b3836d61a0065931185cfcadeb0287c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299047
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Thomas Thrainer <thomasth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1433505}

--- components/trusted_vault/standalone_trusted_vault_client.cc
+++ components/trusted_vault/standalone_trusted_vault_client.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/trusted_vault/proto/local_trusted_vault.pb.h"
 #include "components/trusted_vault/standalone_trusted_vault_backend.h"
 #include "components/trusted_vault/standalone_trusted_vault_storage.h"
-#include "components/trusted_vault/standalone_trusted_vault_storage_impl.h"
 #include "components/trusted_vault/trusted_vault_access_token_fetcher_impl.h"
 #include "components/trusted_vault/trusted_vault_connection_impl.h"
 #include "components/trusted_vault/trusted_vault_server_constants.h"
@@ -268,8 +267,8 @@ StandaloneTrustedVaultClient::StandaloneTrustedVaultClient(
 
   backend_ = base::MakeRefCounted<StandaloneTrustedVaultBackend>(
       security_domain,
-      std::make_unique<StandaloneTrustedVaultStorageImpl>(base_dir,
-                                                          security_domain),
+      std::make_unique<StandaloneTrustedVaultStorage>(base_dir,
+                                                      security_domain),
       std::make_unique<BackendDelegate>(
           base::BindPostTaskToCurrentDefault(
               base::BindRepeating(&StandaloneTrustedVaultClient::
