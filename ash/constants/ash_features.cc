4063f0e6579f0bdd5648e795c707152ce314d7b7
Yunke Zhou <yunkez@google.com>
2025-03-17 06:44:13 -0700

    Clean up kSeparatePasswordAndPinOnLogin feature
    Bug: b:286522648
    Change-Id: I76d55bb57ea2a7f561d08730b321abca5d2eb9ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356489
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433488}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2597,12 +2597,6 @@ BASE_FEATURE(kSeparateNetworkIcons,
              "SeparateNetworkIcons",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables separate password and PIN fields on the login screen when PIN
-// autosubmit is off, instead of a combined "Password or PIN" field.
-BASE_FEATURE(kSeparatePasswordAndPinOnLogin,
-             "SeparatePasswordAndPinOnLogin",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // With this feature enabled, the shortcut app badge is painted in the UI
 // instead of being part of the shortcut app icon.
 BASE_FEATURE(kSeparateWebAppShortcutBadgeIcon,
@@ -4364,10 +4358,6 @@ bool IsSeaPenUseExptTemplateEnabled() {
 
 bool IsSeparateNetworkIconsEnabled() {
   return base::FeatureList::IsEnabled(kSeparateNetworkIcons);
-}
-
-bool IsSeparatePasswordAndPinOnLoginEnabled() {
-  return base::FeatureList::IsEnabled(kSeparatePasswordAndPinOnLogin);
 }
 
 bool IsSeparateWebAppShortcutBadgeIconEnabled() {
