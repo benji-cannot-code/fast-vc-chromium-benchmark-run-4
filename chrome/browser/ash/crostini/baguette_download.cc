d66d3c1f242c85129df16327da72d01b53d63e67
maciek swiech <drmasquatch@google.com>
2025-03-17 12:23:39 -0700

    Obtain baguette image URL and hash from concierge.
    Previous patchset hard-coded image URL and hash. We now call a concierge
    RPC to get the version which should be pinned for our correct
    architecture and version.
    
    Bug: b:393151776
    Change-Id: Iba34f5c9014c044e7862c958c8d472bce5849b4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6353760
    Commit-Queue: maciek swiech <drmasquatch@google.com>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1433697}

--- chrome/browser/ash/crostini/baguette_download.cc
+++ chrome/browser/ash/crostini/baguette_download.cc
@@ -49,8 +49,7 @@ const net::NetworkTrafficAnnotationTag kBaguetteTrafficAnnotation =
         last_reviewed: "2023-01-09"
       }
       policy {
-        cookies_allowed: YES
-        cookies_store: "user"
+        cookies_allowed: NO
       }
     )");
 
