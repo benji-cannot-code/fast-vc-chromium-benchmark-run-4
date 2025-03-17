296916e9a114cea3779bc4cf5b5359e7ea74a07c
Frank Liberato <liberato@chromium.org>
2025-03-17 09:13:31 -0700

    [pip] Don't resize doc pip for non-modal dialogs on Mac
    Mac doesn't clip child dialogs to their parents, so there's no
    need to resize for them.  This causes problems for PWAs especially
    because they do a bunch of intermediate resizes due to "immersive
    fullscreen" mode.
    
    It's not clear what that is or why it causes intermediate resizes,
    but simply not worrying about clipping on mac fixes these and
    generally improves the experience since the doc pip window doesn't
    need to resize at all.
    
    Thanks to beaufort.francois@gmail.com for much of the investigation
    leading to this CL.
    
    Bug: 403166045
    Change-Id: If1806b1846413e7417486701e4de1563d0388eee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6358327
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433566}

--- chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.cc
+++ chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.cc
@@ -75,9 +75,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
-// Windows and CrOS do not clip child widgets to their parents, so we don't have
-// to worry about resizing quite as much.
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+// Windows, Mac and CrOS do not clip child widgets to their parents, so we
+// don't have to worry about resizing quite as much.
+#if BUILDFLAG(IS_LINUX)
 #define PLATFORM_CLIPS_CHILD_WINDOWS
 #endif
 
