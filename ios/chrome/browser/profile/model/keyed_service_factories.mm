d00a2a7f6ee8120e9a815b9a7ac5742293fd20e7
Nohemi Fernandez <fernandex@google.com>
2025-03-17 09:39:40 -0700

    [iOS] Remove unused DistillerPageFactory implementations.
    The main implementation of DOM distiller is currently found within
    the Reading List feature. This patch removes unused code in preparation
    for refactoring the DOM distiller libraries.
    
    Bug: 399379541
    Change-Id: I80dca4bf81384f51b5c565975d4ea0b0ff4b9da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361017
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433583}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/device_reauth/ios_device_authenticator_factory.h"
 #import "ios/chrome/browser/device_sharing/model/device_sharing_manager_factory.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service_factory.h"
-#import "ios/chrome/browser/dom_distiller/model/dom_distiller_service_factory.h"
 #import "ios/chrome/browser/download/model/background_service/background_download_service_factory.h"
 #import "ios/chrome/browser/download/model/browser_download_service_factory.h"
 #import "ios/chrome/browser/drive/model/drive_service_factory.h"
@@ -198,7 +197,6 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   collaboration::messaging::MessagingBackendServiceFactory::GetInstance();
   commerce::ShoppingServiceFactory::GetInstance();
   data_sharing::DataSharingServiceFactory::GetInstance();
-  dom_distiller::DomDistillerServiceFactory::GetInstance();
   drive::DriveServiceFactory::GetInstance();
   enterprise::ProfileIdServiceFactoryIOS::GetInstance();
   enterprise_connectors::ConnectorsServiceFactory::GetInstance();
