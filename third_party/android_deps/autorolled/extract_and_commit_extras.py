f066f8235b7264300cd70fc57fd0c35e7ab5f3ba
Mohamed Heikal <mheikal@chromium.org>
2025-03-17 14:47:43 -0700

    Update groovy code to understand the current reality
    - Removed useless groovy project vars and renamed the rest to be more
      clear of what they are.
    - Refactor path handling in BuildConfigGenerator.groovy to be more
      understandable.
    - Groovy code now understands that output files can live in
      subdirectories like committed/ and cipd/
    - fetch_all.py script now understands that some projects also come with
      cipd/ subdirectory.
    - gradle projects other than the main project can now also use
      subprojects.txt
    - fetch_all_androidx.py no longer deletes the cipd directory each run.
    - This cl regenerates the files of //third_party/androidx without
      rolling since the roller is currently broken.
    - This also regenerates //third_party/android_deps/autorolled but the
      committed/libs subdir was removed from this cl for brevity.
    
    Bug: 40774645
    Change-Id: I3cbc1d534083f0be910f59956ac88c63528c602b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361501
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433780}

---
+++ third_party/android_deps/autorolled/extract_and_commit_extras.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2025 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import pathlib
+import sys
+
+_SRC_ROOT = pathlib.Path(__file__).resolve().parents[3]
+sys.path.insert(0, str(_SRC_ROOT / 'build/autoroll'))
+import extract_and_commit_util as ecu
+
+_AUTOROLLED_PATH = _SRC_ROOT / 'third_party/android_deps/autorolled'
+_COMMITED_DIR_NAME = 'committed'
+
+ecu.main(committed_dir_path=_AUTOROLLED_PATH / _COMMITED_DIR_NAME)
