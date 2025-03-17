b8bd280ee58dcd3054da1a1c7d39f0b3262dad43
Justin Cohen <justincohen@google.com>
2025-03-17 05:18:36 -0700

    ios: Wire up gwp_asan memory regions and crash analyzer.
    Include InternalMemoryRegions in Crashpad's iOS extra_memory_ranges for
    gwp_asan, and modify the gwp_asan crash analyzer to read iOS memory from
    IntermediateDumpMemory instead of directly from the ProcessSnapshot
    memory.
    
    Change-Id: I21bb331bbc32c41941f96e17e770ae030ac94702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357450
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433459}

--- components/gwp_asan/client/guarded_page_allocator.cc
+++ components/gwp_asan/client/guarded_page_allocator.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "partition_alloc/gwp_asan_support.h"
 #include "third_party/boringssl/src/include/openssl/rand.h"
 
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
 #include "components/crash/core/app/crashpad.h"  // nogncheck
 #endif
 
@@ -222,6 +222,19 @@ void GuardedPageAllocator::Init(const AllocatorSettings& settings,
   // on what it reads from the crashing process.
   for (auto& memory_region : GetInternalMemoryRegions())
     crash_reporter::AllowMemoryRange(memory_region.first, memory_region.second);
+#elif BUILDFLAG(IS_IOS)
+  // Explicitly add internal memory regions to Crashpad's iOS intermediate dump
+  // handler.
+  crashpad::SimpleAddressRangeBag* ios_extra_ranges =
+      crash_reporter::IntermediateDumpExtraMemoryRanges();
+  if (ios_extra_ranges) {
+    for (auto& memory_region : GetInternalMemoryRegions()) {
+      if (!ios_extra_ranges->Insert(memory_region.first,
+                                    memory_region.second)) {
+        PLOG(INFO) << "Failed to add InternalMemoryRegions to Crashpad.";
+      }
+    }
+  }
 #endif
 }
 
