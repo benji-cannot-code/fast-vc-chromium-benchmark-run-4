82cac6bd28fb63faffa23bdb25c47735710cd8e3
Fiona Macintosh <fmacintosh@google.com>
2025-03-17 11:45:12 -0700

    [RWS] Clean up RWS sample sets feature param
    AFAICT this isn't used for anything other than testing, which we have
    coverage for via stubbing out the handler. This is pre-work to enable
    cleaning up the UI feature, which has been launched for well over a year
    and has additional UX being built on top of it.
    
    Bug: b:399857405
    Change-Id: I3cd98c2de1e8e85b25a0ca3f6ea7e6e9ea860b74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361978
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433667}

--- chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.h
+++ chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.h
@@ -67,10 +67,6 @@ class MockPrivacySandboxService : public PrivacySandboxService {
               IsRelatedWebsiteSetsDataAccessManaged,
               (),
               (const, override));
-  MOCK_METHOD((base::flat_map<net::SchemefulSite, net::SchemefulSite>),
-              GetSampleRelatedWebsiteSets,
-              (),
-              (const, override));
   MOCK_METHOD(std::optional<net::SchemefulSite>,
               GetRelatedWebsiteSetOwner,
               (const GURL& site_url),
