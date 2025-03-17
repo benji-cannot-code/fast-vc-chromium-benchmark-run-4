6c44a0bd18bcb497951b7691e89b24bba934828f
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-03-17 16:52:12 -0700

    Attach the HTMLCanvasElement to the DOM in canvasPromiseTest
    canvasPromiseTest was testing against HTMLCanvasElement detached from
    the DOM. It turns out that this can impact the test coverage and isn't
    exactly representative of what usually happens in production because
    detached HTML canvas aren't automatically presented to the compositor.
    This can actually impact code execution (resource management, flushes,
    etc.)
    
    Testing with un-detached canvas can still be useful though, for
    instance to validate that a detached canvas correctly recovers from a
    GPU context loss for instance. For this reason, a new test type
    DETACHED_HTML is added to canvasPromiseTest. It's unclear whether it's
    worth always testing with both attached and detached canvases tough, so
    for now, we default to only testing with attached canvas. Constants
    were added to facilitate the selection of which tests we want to test
    against. This will allow adding new test types to one of these
    categories later, if we need to.
    
    Bug: 403282523
    Change-Id: Ibcadcac5d2376f417da16daa1bf87e148c35601f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6355065
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433875}

--- third_party/blink/web_tests/wpt_internal/webgpu/canvas_webgpu_transfer/canvas-test.js
+++ third_party/blink/web_tests/wpt_internal/webgpu/canvas_webgpu_transfer/canvas-test.js
@@ -21,9 +21,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 const CanvasTestType = Object.freeze({
   HTML:   Symbol("html"),
+  DETACHED_HTML:   Symbol("detached_html"),
   OFFSCREEN:  Symbol("offscreen"),
   WORKER: Symbol("worker")
 });
+
+ALL_CANVAS_TEST_TYPES = Object.values(CanvasTestType);
+DEFAULT_CANVAS_TEST_TYPES = [
+    CanvasTestType.HTML,
+    CanvasTestType.OFFSCREEN,
+    CanvasTestType.WORKER,
+];
+HTML_CANVAS_ELEMENT_TEST_TYPES = [
+    CanvasTestType.HTML,
+    CanvasTestType.DETACHED_HTML,
+];
+OFFSCREEN_CANVAS_TEST_TYPES = [
+    CanvasTestType.OFFSCREEN,
+    CanvasTestType.WORKER,
+];
+MAIN_THREAD_CANVAS_TEST_TYPES = [
+    CanvasTestType.HTML,
+    CanvasTestType.DETACHED_HTML,
+    CanvasTestType.OFFSCREEN,
+];
+WORKER_CANVAS_TEST_TYPES = [
+    CanvasTestType.WORKER,
+];
 
 /**
  * Run `testBody` in a `promise_test` against multiple types of canvases. By
@@ -39,7 +63,7 @@ const CanvasTestType = Object.freeze({
  */
 function canvasPromiseTest(
     testBody, description,
-    {testTypes = Object.values(CanvasTestType)} = {}) {
+    {testTypes = DEFAULT_CANVAS_TEST_TYPES} = {}) {
   if (testTypes.includes(CanvasTestType.WORKER)) {
     setup(() => {
       const currentScript = document.currentScript;
@@ -54,8 +78,20 @@ function canvasPromiseTest(
   }
 
   if (testTypes.includes(CanvasTestType.HTML)) {
+    promise_test(async () => {
+      if (!document.body) {
+        document.documentElement.appendChild(document.createElement("body"));
+      }
+      const canvas = document.createElement('canvas');
+      document.body.appendChild(canvas);
+      await testBody(canvas);
+      document.body.removeChild(canvas);
+    }, 'HTMLCanvasElement: ' + description);
+  }
+
+  if (testTypes.includes(CanvasTestType.DETACHED_HTML)) {
     promise_test(() => testBody(document.createElement('canvas')),
-                 'HTMLCanvasElement: ' + description);
+                 'Detached HTMLCanvasElement: ' + description);
   }
 
   if (testTypes.includes(CanvasTestType.OFFSCREEN)) {
