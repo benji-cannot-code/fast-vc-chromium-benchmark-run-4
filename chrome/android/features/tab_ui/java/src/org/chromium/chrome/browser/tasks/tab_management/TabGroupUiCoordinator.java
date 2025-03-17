363ae77581fb90e70a150f995dbfe86254fb65b0
Sky Malice <skym@chromium.org>
2025-03-17 12:48:57 -0700

    Minor tab toolstrip cleanup.
    Change-Id: I1f1ca9fa985fa007a21c1cc95e1ed591fd769e68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354898
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433716}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiCoordinator.java
@@ -250,7 +250,7 @@ public class TabGroupUiCoordinator implements TabGroupUiMediator.ResetHandler, T
                     new TabGroupUiMediator(
                             visibilityController,
                             mHandleBackPressChangedSupplier,
-                            this,
+                            /* resetHandler= */ this,
                             mModel,
                             mTabModelSelector,
                             mTabContentManager,
