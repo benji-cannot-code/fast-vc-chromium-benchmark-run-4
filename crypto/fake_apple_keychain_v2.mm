d2c406168b0ba6eae5ee86cd132b0858e14d5e90
Gyuyoung Kim <gyuyoung@igalia.com>
2025-03-16 17:56:50 -0700

    [tvOS] Disable LAContextCanEvaluatePolicy Method
    The LAPolicy used by LAContextCanEvaluatePolicy is not
    available on tvOS. This change disables the method using
    the tvOS build flag.
    
    Bug: 391914246
    Change-Id: I2c1f8f53ddeeaf823bf9f1b4f0fb362925c1e082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6346508
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433309}

--- crypto/fake_apple_keychain_v2.mm
+++ crypto/fake_apple_keychain_v2.mm
@@ -317,6 +317,7 @@ FakeAppleKeychainV2::TaskCopyValueForEntitlement(SecTaskRef task,
 }
 #endif  // !BUILDFLAG(IS_IOS)
 
+#if !BUILDFLAG(IS_IOS_TVOS)
 BOOL FakeAppleKeychainV2::LAContextCanEvaluatePolicy(
     LAPolicy policy,
     NSError* __autoreleasing* error) {
@@ -334,5 +335,6 @@ BOOL FakeAppleKeychainV2::LAContextCanEvaluatePolicy(
       return false;
   }
 }
+#endif  // !BUILDFLAG(IS_IOS_TVOS)
 
 }  // namespace crypto
