1cccf0c1acf005820776de15343bf906ce4f3b83
David Bertoni <dbertoni@chromium.org>
2025-03-17 15:34:45 -0700

    [Extensions] Convert uses of absl::variant to std::variant.
    Bug: 40242126
    Change-Id: I7851226009d3e5cc1dcd87af8ee6453f70a48e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6363017
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433817}

--- chrome/browser/extensions/api/identity/identity_token_cache.h
+++ chrome/browser/extensions/api/identity/identity_token_cache.h
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <variant>
 
 #include "base/time/time.h"
 #include "chrome/browser/extensions/api/identity/extension_token_key.h"
 #include "google_apis/gaia/oauth2_mint_token_flow.h"
-#include "third_party/abseil-cpp/absl/types/variant.h"
 
 namespace extensions {
 
@@ -73,10 +73,10 @@ class IdentityTokenCacheValue {
 
   base::Time expiration_time_;
 
-  absl::variant<absl::monostate,
-                RemoteConsentResolutionData,
-                std::string,
-                TokenValue>
+  std::variant<std::monostate,
+               RemoteConsentResolutionData,
+               std::string,
+               TokenValue>
       value_;
 };
 
--- chrome/browser/extensions/api/web_authentication_proxy/web_authentication_proxy_service.h
+++ chrome/browser/extensions/api/web_authentication_proxy/web_authentication_proxy_service.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_EXTENSIONS_API_WEB_AUTHENTICATION_PROXY_WEB_AUTHENTICATION_PROXY_SERVICE_H_
 
 #include <optional>
+#include <variant>
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -233,7 +234,7 @@ class WebAuthenticationProxyService
   std::optional<ExtensionId> active_proxy_;
 
   using CallbackType =
-      absl::variant<IsUvpaaCallback, CreateCallback, GetCallback>;
+      std::variant<IsUvpaaCallback, CreateCallback, GetCallback>;
   std::map<RequestId, CallbackType> pending_callbacks_;
 
   SEQUENCE_CHECKER(sequence_checker_);
