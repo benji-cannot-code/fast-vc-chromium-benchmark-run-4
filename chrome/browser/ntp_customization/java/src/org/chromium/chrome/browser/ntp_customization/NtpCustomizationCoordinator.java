bd44755252aaf41bbb88f60152bf450b2a100f43
Yanling <yanlinghu@google.com>
2025-03-17 08:04:14 -0700

    [NTP Customization] Add system back press handler
    This CL adds the logics to handle system back press and the logics to
    destroy the coordinator.
    
    The user interaction with three-button navigation bar can be found in
    the video below: https://drive.google.com/file/d/1KVI
    mJLCgXWzt81mMAYcUQ4JQqLNzFUkp/view?usp=sharing&resourcekey=0-EdFAQY1KDhGkHT0hPnxWDg
    
    Bug: 397439004
    Change-Id: I54e38f27733b8257890f009d8f884f458e7cc451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334419
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433528}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationCoordinator.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationCoordinator.java
@@ -68,7 +68,10 @@ public class NtpCustomizationCoordinator {
                 mPropertyModel, mViewFlipperView, NtpCustomizationViewBinder::bind);
 
         NtpCustomizationBottomSheetContent bottomSheetContent =
-                new NtpCustomizationBottomSheetContent(mContentView);
+                new NtpCustomizationBottomSheetContent(
+                        mContentView,
+                        /* backPressRunnable= */ () -> mMediator.backPressOnCurrentBottomSheet(),
+                        this::destroy);
 
         mMediator =
                 new NtpCustomizationMediator(
@@ -111,7 +114,9 @@ public class NtpCustomizationCoordinator {
     /** Removes all click listeners, all views inside the view flipper and destroy the mediator. */
     public void destroy() {
         mPropertyModel.set(NTP_CARDS_OPTION_CLICK_LISTENER, null);
-        mPropertyModel.set(NTP_CARDS_BACK_PRESS_HANDLER, null);
+        if (mNtpCardsCoordinator != null) {
+            mPropertyModel.set(NTP_CARDS_BACK_PRESS_HANDLER, null);
+        }
         mViewFlipperView.removeAllViews();
         mMediator.destroy();
     }
