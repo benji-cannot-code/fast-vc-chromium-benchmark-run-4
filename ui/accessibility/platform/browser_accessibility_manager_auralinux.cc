8e1cab706bc543c547154d0b79b12c40160b6eae
Evelynn Kaplan <evelynn.kaplan@microsoft.com>
2025-03-17 11:38:56 -0700

    [AriaNotify] Fire ARIA notifications for newer versions of ATK/Linux
    This CL adds the ARIA Notify implementation for Linux.
    
    In versions of the ATK library >= 2.50.0, there is a new "notification"
    signal type: https://docs.gtk.org/atk/signal.Object.notification.html
    
    There is more information about this signal type and how it will work in
    Orca here:
    https://gitlab.gnome.org/GNOME/orca/-/blob/main/README-APPLICATION-DEVELOPERS.md#speaking-your-applications-custom-messageannouncement
    
    In ATK versions that support this signal type, we will use it; in older
    versions, we "fall back" to the type of signal that is sent for live
    regions. A follow-up CL will have this implementation, CL:6349538.
    
    Bug: 326277796
    Change-Id: Ia03971d35c7c16e34bf911ccca9cc5d4174405b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797671
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Evelynn Kaplan <evelynn.kaplan@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1433666}

--- ui/accessibility/platform/browser_accessibility_manager_auralinux.cc
+++ ui/accessibility/platform/browser_accessibility_manager_auralinux.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
+#include "base/version.h"
 #include "ui/accessibility/ax_selection.h"
 #include "ui/accessibility/platform/ax_platform_node_auralinux.h"
 #include "ui/accessibility/platform/ax_platform_tree_manager_delegate.h"
@@ -399,6 +400,21 @@ void BrowserAccessibilityManagerAuraLinux::FireGeneratedEvent(
     case AXEventGenerator::Event::TEXT_SELECTION_CHANGED:
     case AXEventGenerator::Event::WIN_IACCESSIBLE_STATE_CHANGED:
       break;
+  }
+}
+
+void BrowserAccessibilityManagerAuraLinux::FireAriaNotificationEvent(
+    BrowserAccessibility* node,
+    const std::string& announcement,
+    ax::mojom::AriaNotificationPriority priority_property,
+    const std::string& notification_id,
+    ax::mojom::AriaNotificationInterrupt interrupt_property) {
+  DCHECK(node);
+
+  // Only newer Atk versions support the notification signal type.
+  if (base::Version(atk_get_version()).CompareTo(base::Version("2.50.0")) >= 0) {
+    ToBrowserAccessibilityAuraLinux(node)->GetNode()->OnAriaNotificationPosted(
+        announcement, priority_property);
   }
 }
 
