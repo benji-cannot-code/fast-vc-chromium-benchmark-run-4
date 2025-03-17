c6f4aab1b4e87ce94d7d2708ec7d68b0e657d218
Petr Cermak <petrcermak@chromium.org>
2025-03-17 03:22:25 -0700

    Pass first coalesced begin frame time to SimpleBeginFrameObservers
    If ui::HostBeginFrameObserver coalesces multiple begin frames, it passes
    the begin frame time of the first frame to
    ui::HostBeginFrameObserver::SimpleBeginFrameObservers.
    
    Bug: b:349975994
    Change-Id: I35787520053fbbe823209ba04b99e40bcd417822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357074
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Petr Cermak <petrcermak@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Auto-Submit: Petr Cermak <petrcermak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433418}

--- content/browser/renderer_host/input/synthetic_gesture_target_aura.h
+++ content/browser/renderer_host/input/synthetic_gesture_target_aura.h
@@ -59,8 +59,10 @@ class SyntheticGestureTargetAura
 
  private:
   // ui::HostBeginFrameObserver::SimpleBeginFrameObserver:
-  void OnBeginFrame(base::TimeTicks frame_begin_time,
-                    base::TimeDelta frame_interval) override;
+  void OnBeginFrame(
+      base::TimeTicks frame_begin_time,
+      base::TimeDelta frame_interval,
+      std::optional<base::TimeTicks> first_coalesced_frame_begin_time) override;
   void OnBeginFrameSourceShuttingDown() override;
 
   RenderWidgetHostViewAura* GetView() const;
--- ui/compositor/host_begin_frame_observer.cc
+++ ui/compositor/host_begin_frame_observer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/compositor/host_begin_frame_observer.h"
 
+#include <optional>
+
 #include "base/logging.h"
 #include "base/task/common/task_annotator.h"
 #include "base/time/time.h"
@@ -46,6 +48,9 @@ void HostBeginFrameObserver::OnStandaloneBeginFrame(
                         [&](perfetto::EventContext ctx) {
                           WriteBeginFrameIdToTrace(ctx, args.frame_id);
                         });
+    if (!first_coalesced_begin_frame_time_) {
+      first_coalesced_begin_frame_time_ = begin_frame_args_.frame_time;
+    }
     begin_frame_args_ = args;
     return;
   }
@@ -54,6 +59,7 @@ void HostBeginFrameObserver::OnStandaloneBeginFrame(
     begin_frame_args_ = args;
     pending_coalesce_callback_ = true;
     coalesce_flow_id_ = base::trace_event::GetNextGlobalTraceId();
+    CHECK(!first_coalesced_begin_frame_time_);
     TRACE_EVENT_INSTANT("ui", "HostBeginFrameObserver::start coalescing",
                         perfetto::Flow::Global(coalesce_flow_id_),
                         [&](perfetto::EventContext ctx) {
@@ -67,7 +73,7 @@ void HostBeginFrameObserver::OnStandaloneBeginFrame(
     return;
   }
 
-  CallObservers(args);
+  CallObservers(args, std::nullopt);
 }
 
 mojo::PendingRemote<viz::mojom::BeginFrameObserver>
@@ -86,14 +92,20 @@ void HostBeginFrameObserver::CoalescedBeginFrame() {
   pending_coalesce_callback_ = false;
   viz::BeginFrameArgs args = begin_frame_args_;
   begin_frame_args_ = viz::BeginFrameArgs();
+  std::optional<base::TimeTicks> first_coalesced_begin_frame_time =
+      first_coalesced_begin_frame_time_;
+  first_coalesced_begin_frame_time_ = std::nullopt;
   coalesce_flow_id_ = ~0ull;
-  CallObservers(args);
+  CallObservers(args, first_coalesced_begin_frame_time);
 }
 
 // This may be deleted as part of `CallObservers`.
-void HostBeginFrameObserver::CallObservers(const viz::BeginFrameArgs& args) {
+void HostBeginFrameObserver::CallObservers(
+    const viz::BeginFrameArgs& args,
+    std::optional<const base::TimeTicks> first_coalesced_begin_frame_time) {
   simple_begin_frame_observers_->Notify(&SimpleBeginFrameObserver::OnBeginFrame,
-                                        args.frame_time, args.interval);
+                                        args.frame_time, args.interval,
+                                        first_coalesced_begin_frame_time);
 }
 
 }  // namespace ui
