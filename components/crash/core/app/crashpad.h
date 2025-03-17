fbef636f40eec2ad9daf2358c4ec62163c4901e1
Justin Cohen <justincohen@google.com>
2025-03-17 04:46:23 -0700

    ios: Wire up new Crashpad APIs in crash_reporter.
    Add helpers for Crashpad's iOS extra_memory_ranges and
    user_stream_sources.
    
    Change-Id: I2a5a2d76008ccbbd627657e037daf30d1455d9df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354668
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433452}

--- components/crash/core/app/crashpad.h
+++ components/crash/core/app/crashpad.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_IOS)
 #include "base/containers/span.h"
+#include "third_party/crashpad/crashpad/client/simple_address_range_bag.h"
+#include "third_party/crashpad/crashpad/handler/user_stream_data_source.h"  // nogncheck
 #endif
 
 namespace base {
@@ -176,6 +178,21 @@ bool ProcessExternalDump(
 
 // "platform", used to determine device_model, can be overridden.
 void OverridePlatformValue(const std::string& platform_value);
+
+// The simple extra memory ranges SimpleAddressRangeBag object.
+crashpad::SimpleAddressRangeBag* ExtraMemoryRanges();
+
+// Sets the bag of extra memory ranges to be included in the snapshot.
+void SetExtraMemoryRanges(crashpad::SimpleAddressRangeBag* address_range_bag);
+
+// The extra memory ranges SimpleAddressRangeBag object stored in the snapshot
+// but not the minidump.
+crashpad::SimpleAddressRangeBag* IntermediateDumpExtraMemoryRanges();
+
+// Sets the bag of extra memory ranges to be included in the snapshot but not
+// the minidump.
+void SetIntermediateDumpExtraMemoryRanges(
+    crashpad::SimpleAddressRangeBag* address_range_bag);
 #endif  // BUILDFLAG(IS_IOS)
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
@@ -233,7 +250,8 @@ void DumpProcessWithoutCrashing(task_t task_port);
 // merge with any process annotations. These are useful for adding annotations
 // detected on the next run after a crash but before upload.
 void ProcessIntermediateDumps(
-    const std::map<std::string, std::string>& annotations = {});
+    const std::map<std::string, std::string>& annotations = {},
+    const crashpad::UserStreamDataSources* user_stream_sources = nullptr);
 
 // Convert a single intermediate dump at |file| into a minidump and
 // trigger an upload if StartProcessingPendingReports() has been called.
