d3a01c878179d3ac3211d25b7d32c91cf3c7e790
Andrew Grieve <agrieve@chromium.org>
2025-03-17 10:29:29 -0700

    Add a component() for android_webview native code
    Should speed up monochrome component builds by not having all the code
    live in the top-level .so file.
    
    Bug: None
    Change-Id: Iec414a99ae3cbe79e67cdb1a30fc882139056f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354219
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433613}

--- android_webview/lib/webview_jni_onload.h
+++ android_webview/lib/webview_jni_onload.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ANDROID_WEBVIEW_LIB_WEBVIEW_JNI_ONLOAD_H_
 #define ANDROID_WEBVIEW_LIB_WEBVIEW_JNI_ONLOAD_H_
 
+#include "base/component_export.h"
+
 namespace android_webview {
 
-bool OnJNIOnLoadInit();
+COMPONENT_EXPORT(ANDROID_WEBVIEW) bool OnJNIOnLoadInit();
 
 }  // android_webview
 
