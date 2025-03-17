00d2f2d342db67198a9846ca0cce1604ea1a0559
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-03-17 09:12:19 -0700

    Revert "[NTP Customization] Add system back press handler"
    This reverts commit dc66b1630c9e99eb7cb9de42942952223962555f.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8720138924831787089
    
    Sample failed build: https://ci.chromium.org/b/8720138924831787089
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6334419&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8720138924831787089&type=BUG
    
    Original change's description:
    > [NTP Customization] Add system back press handler
    >
    > This CL adds the logics to handle system back press and the logics to
    > destroy the coordinator.
    >
    > The user interaction with three-button navigation bar can be found in
    > the video below: https://drive.google.com/file/d/1KVI
    > mJLCgXWzt81mMAYcUQ4JQqLNzFUkp/view?usp=sharing&resourcekey=0-EdFAQY1KDhGkHT0hPnxWDg
    >
    > Bug: 397439004
    > Change-Id: I54e38f27733b8257890f009d8f884f458e7cc451
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6334419
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Commit-Queue: Yanling Hu <yanlinghu@google.com>
    > Reviewed-by: Xi Han <hanxi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1433528}
    >
    
    Bug: 397439004
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia1ffe93544a55e263811065dedef552389d45385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6360621
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1433564}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationBottomSheetContent.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationBottomSheetContent.java
@@ -12,22 +12,14 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
 
 /** Bottom sheet content of the NTP customization. */
 public class NtpCustomizationBottomSheetContent implements BottomSheetContent {
     private final View mContentView;
-    private final Runnable mBackPressRunnable;
-    private final Runnable mOnDestroyRunnable;
-    private ObservableSupplierImpl<Boolean> mBackPressStateChangedSupplier;
 
-    NtpCustomizationBottomSheetContent(
-            View contentView, Runnable backPressRunnable, Runnable onDestroy) {
+    public NtpCustomizationBottomSheetContent(View contentView) {
         mContentView = contentView;
-        mBackPressRunnable = backPressRunnable;
-        mBackPressStateChangedSupplier = new ObservableSupplierImpl<>();
-        mOnDestroyRunnable = onDestroy;
     }
 
     @Override
@@ -46,9 +38,7 @@ public class NtpCustomizationBottomSheetContent implements BottomSheetContent {
     }
 
     @Override
-    public void destroy() {
-        mOnDestroyRunnable.run();
-    }
+    public void destroy() {}
 
     @Override
     public int getPriority() {
@@ -71,22 +61,6 @@ public class NtpCustomizationBottomSheetContent implements BottomSheetContent {
     }
 
     @Override
-    public void onBackPressed() {
-        mBackPressRunnable.run();
-    }
-
-    @Override
-    public boolean handleBackPress() {
-        mBackPressRunnable.run();
-        return true;
-    }
-
-    @Override
-    public ObservableSupplierImpl<Boolean> getBackPressStateChangedSupplier() {
-        return mBackPressStateChangedSupplier;
-    }
-
-    @Override
     public String getSheetContentDescription(@NonNull Context context) {
         return context.getString(R.string.ntp_customization_main_bottom_sheet_content_description);
     }
@@ -106,23 +80,5 @@ public class NtpCustomizationBottomSheetContent implements BottomSheetContent {
     @Override
     public int getSheetClosedAccessibilityStringId() {
         return R.string.ntp_customization_main_bottom_sheet_closed;
-    }
-
-    /** Sets up ObservableSupplierImpl<Boolean> when opening the bottom sheet. */
-    void onSheetOpened() {
-        // Sets the value in the supplier to true to indicate that back press should be handled by
-        // the bottom sheet.
-        mBackPressStateChangedSupplier.set(true);
-    }
-
-    /** Sets up ObservableSupplierImpl<Boolean> when closing the bottom sheet. */
-    void onSheetClosed() {
-        // Sets the value in the supplier to false to indicate that back press should not be handled
-        // by the bottom sheet.
-        mBackPressStateChangedSupplier.set(false);
-    }
-
-    void setBackPressStateChangedSupplierForTesting(ObservableSupplierImpl<Boolean> supplier) {
-        mBackPressStateChangedSupplier = supplier;
     }
 }
--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationMediator.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationMediator.java
@@ -8,13 +8,10 @@ package org.chromium.chrome.browser.ntp_customization;
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator.BottomSheetType.MAIN;
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationViewProperties.LAYOUT_TO_DISPLAY;
 
-import android.support.annotation.VisibleForTesting;
 import android.widget.ViewFlipper;
 
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
-import org.chromium.components.browser_ui.bottomsheet.BottomSheetObserver;
-import org.chromium.components.browser_ui.bottomsheet.EmptyBottomSheetObserver;
 import org.chromium.ui.modelutil.PropertyModel;
 
 import java.util.HashMap;
@@ -32,33 +29,18 @@ public class NtpCustomizationMediator {
     private final Map<Integer, Integer> mViewFlipperMap;
 
     private final BottomSheetController mBottomSheetController;
-    private final NtpCustomizationBottomSheetContent mBottomSheetContent;
-    private final BottomSheetObserver mBottomSheetObserver;
+    private final BottomSheetContent mBottomSheetContent;
     private final PropertyModel mPropertyModel;
     private Integer mCurrentBottomSheet;
 
     public NtpCustomizationMediator(
             BottomSheetController bottomSheetController,
-            NtpCustomizationBottomSheetContent bottomSheetContent,
+            BottomSheetContent bottomSheetContent,
             PropertyModel propertyModel) {
         mBottomSheetController = bottomSheetController;
         mBottomSheetContent = bottomSheetContent;
         mPropertyModel = propertyModel;
         mViewFlipperMap = new HashMap<>();
-        mBottomSheetObserver =
-                new EmptyBottomSheetObserver() {
-                    @Override
-                    public void onSheetOpened(@BottomSheetController.StateChangeReason int reason) {
-                        mBottomSheetContent.onSheetOpened();
-                    }
-
-                    @Override
-                    public void onSheetClosed(@BottomSheetController.StateChangeReason int reason) {
-                        mBottomSheetContent.onSheetClosed();
-                        mBottomSheetController.removeObserver(mBottomSheetObserver);
-                    }
-                };
-        mBottomSheetController.addObserver(mBottomSheetObserver);
     }
 
     /**
@@ -116,10 +98,5 @@ public class NtpCustomizationMediator {
     void setCurrentBottomSheetForTesting(
             @NtpCustomizationCoordinator.BottomSheetType int bottomSheetType) {
         mCurrentBottomSheet = bottomSheetType;
-    }
-
-    @VisibleForTesting
-    BottomSheetObserver getBottomSheetObserver() {
-        return mBottomSheetObserver;
     }
 }
