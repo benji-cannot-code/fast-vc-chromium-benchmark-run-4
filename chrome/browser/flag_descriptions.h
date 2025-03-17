4a70bf137b7c7d1423e12835c3d7b5754b4b1cf9
Colin Blundell <blundell@chromium.org>
2025-03-17 07:07:48 -0700

    [Blink] Add flags for Canvas2D killswitches
    Will enable toggling these on/off to evaluate impact on any issues for
    which *they* might be implicated.
    
    Bug: 40280152, 399731315
    Change-Id: I5be0ade083dff384750d79d531587eb546a880cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356978
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433500}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -56,6 +56,12 @@ extern const char kAcceleratedVideoDecodeDescription[];
 extern const char kAcceleratedVideoEncodeName[];
 extern const char kAcceleratedVideoEncodeDescription[];
 
+extern const char kAdjustCanCreateCanvas2DResourceProviderName[];
+extern const char kAdjustCanCreateCanvas2DResourceProviderDescription[];
+
+extern const char kAdjustGetOrCreate2DCanvasProviderName[];
+extern const char kAdjustGetOrCreate2DCanvasProviderDescription[];
+
 extern const char kAiSettingsPageRefreshName[];
 extern const char kAiSettingsPageRefreshDescription[];
 
@@ -651,6 +657,10 @@ extern const char kUseDnsHttpsSvcbAlpnDescription[];
 
 extern const char kIsolatedSandboxedIframesName[];
 extern const char kIsolatedSandboxedIframesDescription[];
+
+extern const char kIsPaintableChecksResourceProviderInsteadOfBridgeName[];
+extern const char
+    kIsPaintableChecksResourceProviderInsteadOfBridgeDescription[];
 
 extern const char kExperimentalAccessibilityLanguageDetectionName[];
 extern const char kExperimentalAccessibilityLanguageDetectionDescription[];
