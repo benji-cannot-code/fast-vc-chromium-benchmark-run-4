9db5114e2a5877412fe2d7586798002053920dd8
Kramer Ge <fangzhoug@chromium.org>
2025-03-17 11:51:21 -0700

    Refactor SharedImageInterface in MailboxVideoFrameConverter
    - Remove gpu_task_runner threading interface, rely on sii.
    - Remove directly scheduling wait sync_token task, use a callback
    - Refactor mock shared_image_interface to media/mojo/common:test_support
    - Modify unittest to verify against mock shared_image_interface
    
    Bug: 403183890
    Change-Id: I54cf1171fd97c761242142470b89904ed878afd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6336624
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433669}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -333,6 +333,7 @@ template <class WorkerInterface,
 class CodecWorkerImpl;
 class FileVideoCaptureDeviceFactory;
 class GpuMojoMediaClientWin;
+class MailboxVideoFrameConverter;
 class MojoVideoEncodeAccelerator;
 class PaintCanvasVideoRenderer;
 class V4L2DevicePoller;  // TODO(crbug.com/41486289): remove this.
@@ -860,6 +861,7 @@ class BASE_EXPORT
   friend class gpu::GpuMemoryBufferImplDXGI;
   friend class media::AudioInputDevice;
   friend class media::AudioOutputDevice;
+  friend class media::MailboxVideoFrameConverter;
   friend class media::PaintCanvasVideoRenderer;
   friend class media::V4L2DevicePoller;  // TODO(crbug.com/41486289): remove
                                          // this.
