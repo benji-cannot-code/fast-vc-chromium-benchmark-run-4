c261f8f0fd91437279636888bc044691aecfadc7
rbpotter <rbpotter@chromium.org>
2025-03-18 13:35:06 -0700

    [PDF Ink Signatures]: Use cr-grid in ink color selector
    Allows users to navigate the grid using only left/right arrow keys,
    and removes a bunch of bespoke keyboard handling code that is already
    implemented in cr-grid.
    
    Originally the PDF code was using custom logic where arrow keys would
    only navigate within a single row or column, but this is not obvious
    to screen readers because this is labelled as a radiogroup with radio
    options, which should be fully navigable using up/down or left/right
    arrow keys.
    
    Since the PDF ink color selector wants to actually select items when
    the arrow keys navigate, not just focus them, have cr-grid fire a
    cr-grid-focus-changed CustomEvent when a new item is focused with the
    arrow keys, so that the color selector can use this event to select
    the newly focused element. Add a test for the new event.
    
    Bug: 402545967
    Change-Id: If1e7ced08a7ec0bd0526b23bae2d99122f782888
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6362682
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434394}

--- ui/webui/resources/cr_elements/cr_grid/cr_grid.ts
+++ ui/webui/resources/cr_elements/cr_grid/cr_grid.ts
@@ -120,6 +120,7 @@ export class CrGridElement extends CrLitElement {
           item;
       assert(toFocus);
       toFocus.focus();
+      this.fire('cr-grid-focus-changed', toFocus);
     }
 
     if (['Enter', ' '].includes(e.key)) {
