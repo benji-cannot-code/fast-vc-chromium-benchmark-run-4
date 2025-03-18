783bfac5f6c7ea73be2c734290fa35e8dd1875aa
Rushan Suleymanov <rushans@google.com>
2025-03-18 03:51:35 -0700

    [Sync] Do not use VAPID sharing messages by default
    Some tests were removed as the feature toggle will be removed soon.
    
    Bug: 388468664
    Change-Id: I2b1932ac6037c1cb100efa8b1bb2f51d2f767771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361195
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434048}

--- chrome/browser/sharing/sharing_device_registration_impl.cc
+++ chrome/browser/sharing/sharing_device_registration_impl.cc
@@ -44,7 +44,7 @@ namespace {
 // not register with VAPID.
 BASE_FEATURE(kSharingDisableVapid,
              "SharingDisableVapid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace
 
--- chrome/browser/sharing/sharing_device_registration_impl_unittest.cc
+++ chrome/browser/sharing/sharing_device_registration_impl_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
 #include "components/gcm_driver/instance_id/instance_id_driver.h"
@@ -285,9 +286,9 @@ TEST_F(SharingDeviceRegistrationImplTest, RegisterDeviceTest_Success) {
   RegisterDeviceSync();
 
   std::set<sync_pb::SharingSpecificFields::EnabledFeatures> enabled_features =
-      GetExpectedEnabledFeatures(/*supports_vapid=*/true);
+      GetExpectedEnabledFeatures(/*supports_vapid=*/false);
   syncer::DeviceInfo::SharingInfo expected_sharing_info(
-      {kVapidFCMToken, kDevicep256dh, kDeviceAuthSecret},
+      /*vapid_target_info=*/{},
       {kSenderIdFCMToken, kSenderIdP256dh, kSenderIdAuthSecret},
       /*chime_representative_target_id=*/std::string(), enabled_features);
 
@@ -301,35 +302,12 @@ TEST_F(SharingDeviceRegistrationImplTest, RegisterDeviceTest_Success) {
 
   // Device should be re-registered with the new FCM token.
   syncer::DeviceInfo::SharingInfo expected_synced_sharing_info_2(
-      {kVapidFCMToken2, kDevicep256dh2, kDeviceAuthSecret2},
+      /*vapid_target_info=*/{},
       {kSenderIdFCMToken, kSenderIdP256dh, kSenderIdAuthSecret},
       /*chime_representative_target_id=*/std::string(), enabled_features);
 
   EXPECT_EQ(SharingDeviceRegistrationResult::kSuccess, result_);
   EXPECT_EQ(expected_synced_sharing_info_2, local_sharing_info_);
-  EXPECT_TRUE(fcm_registration_);
-}
-
-TEST_F(SharingDeviceRegistrationImplTest, RegisterDeviceTest_Vapid_Only) {
-  // Make sync unavailable to force using vapid.
-  test_sync_service_.SetFailedDataTypes({syncer::SHARING_MESSAGE});
-  SetInstanceIDFCMResult(instance_id::InstanceID::Result::SUCCESS);
-  SetInstanceIDFCMToken(kVapidFCMToken);
-  fake_device_info_sync_service_.GetDeviceInfoTracker()->Add(
-      fake_device_info_sync_service_.GetLocalDeviceInfoProvider()
-          ->GetLocalDeviceInfo());
-
-  RegisterDeviceSync();
-
-  std::set<sync_pb::SharingSpecificFields::EnabledFeatures> enabled_features =
-      GetExpectedEnabledFeatures(/*supports_vapid=*/true);
-  syncer::DeviceInfo::SharingInfo expected_sharing_info(
-      {kVapidFCMToken, kDevicep256dh, kDeviceAuthSecret},
-      syncer::DeviceInfo::SharingTargetInfo(),
-      /*chime_representative_target_id=*/std::string(), enabled_features);
-
-  EXPECT_EQ(SharingDeviceRegistrationResult::kSuccess, result_);
-  EXPECT_EQ(expected_sharing_info, local_sharing_info_);
   EXPECT_TRUE(fcm_registration_);
 }
 
@@ -425,9 +403,9 @@ TEST_F(SharingDeviceRegistrationImplTest, UnregisterDeviceTest_Success) {
 
   // Device should be registered with the new FCM token.
   std::set<sync_pb::SharingSpecificFields::EnabledFeatures> enabled_features =
-      GetExpectedEnabledFeatures(/*supports_vapid=*/true);
+      GetExpectedEnabledFeatures(/*supports_vapid=*/false);
   syncer::DeviceInfo::SharingInfo expected_sharing_info(
-      {kVapidFCMToken2, kDevicep256dh, kDeviceAuthSecret},
+      /*vapid_target_info=*/{},
       {kSenderIdFCMToken, kSenderIdP256dh, kSenderIdAuthSecret},
       /*chime_representative_target_id=*/std::string(), enabled_features);
 
