c8377953cffa7c1d520b063ab18bd65e0bb98350
Evan Shrubsole <eshr@google.com>
2025-03-18 06:29:59 -0700

    Replace forward declaration of PacketTimeUpdateParams
    Bug: webrtc:42232595
    Change-Id: I5e52567e78642d4b5794f02e7f993af167683645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367076
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Evan Shrubsole <eshr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434094}

--- remoting/protocol/stream_packet_processor.h
+++ remoting/protocol/stream_packet_processor.h
@@ -9,14 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/memory/scoped_refptr.h"
+#include "third_party/webrtc/rtc_base/async_packet_socket.h"
 
 namespace net {
 class IOBufferWithSize;
 }  // namespace net
-
-namespace rtc {
-struct PacketTimeUpdateParams;
-}  // namespace rtc
 
 namespace remoting::protocol {
 
