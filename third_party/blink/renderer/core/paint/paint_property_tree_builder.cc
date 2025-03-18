53531df7bb6a501e2fe8682a0d3e5aa0387d1a0f
Steve Kobes <skobes@chromium.org>
2025-03-18 10:49:51 -0700

    Reland: ScopedVT: Composite tagged elements.
    This relands crrev.com/1430494 which was reverted at crrev.com/1430582
    for breaking the leak bot. The leak was fixed by crrev.com/1433054.
    
    This reland is identical to the original change.
    
    Bug: 401465381
    Change-Id: Iea0b5b71a2bda3a4948a2c8809c2f4507344e393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357032
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434242}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -1941,7 +1941,7 @@ void FragmentPaintPropertyTreeBuilder::UpdateViewTransitionEffect() {
 
     if (needs_view_transition_effect) {
       auto* transition =
-          ViewTransitionUtils::GetTransition(object_.GetDocument());
+          ViewTransitionUtils::TransitionForTaggedElement(object_);
       DCHECK(transition);
 
       EffectPaintPropertyNode::State state;
@@ -1999,7 +1999,7 @@ void FragmentPaintPropertyTreeBuilder::UpdateViewTransitionClip() {
     if (full_context_.direct_compositing_reasons &
         CompositingReason::kViewTransitionElement) {
       auto* transition =
-          ViewTransitionUtils::GetTransition(object_.GetDocument());
+          ViewTransitionUtils::TransitionForTaggedElement(object_);
       DCHECK(transition);
 
       if (!transition->NeedsViewTransitionClipNode(object_)) {
