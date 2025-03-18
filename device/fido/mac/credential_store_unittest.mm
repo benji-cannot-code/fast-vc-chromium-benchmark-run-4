c93abbc20e6fe8764ee511fc35773262199e8040
Jayson Adams <shrike@chromium.org>
2025-03-18 14:39:50 -0700

    [Mac Device Unittests] Disable legacy CredentialStoreTest on macOS ARM
    This test remains broken and is being disabled to green the bots (in
    preparation for macOS 16).
    
    Test: out/default/device_unittests \
      --gtest_filter=CredentialStoreTest.FindCredentialsFromCredentialDescriptorList_LegacyCredentials
    
    Bug: 354937434,354935959
    Change-Id: Ia8be638a8419950061daeef6242cbc4e7966108a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354825
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434436}

--- device/fido/mac/credential_store_unittest.mm
+++ device/fido/mac/credential_store_unittest.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/apple/foundation_util.h"
+#include "base/mac/mac_util.h"
 #include "crypto/scoped_fake_apple_keychain_v2.h"
 #include "device/fido/mac/authenticator_config.h"
 #include "device/fido/mac/credential_store.h"
@@ -130,6 +131,13 @@ TEST_F(CredentialStoreTest,
 // credentials with IDs that have an old metadata version.
 TEST_F(CredentialStoreTest,
        FindCredentialsFromCredentialDescriptorList_LegacyCredentials) {
+#if BUILDFLAG(IS_MAC)
+  // See https://crbug.com/354937434 .
+  if (base::mac::MacOSMajorVersion() == 15) {
+    GTEST_SKIP() << "Disabled on macOS Sequoia.";
+  }
+#endif
+
   std::vector<Credential> credentials;
   for (const auto version :
        {CredentialMetadata::Version::kV0, CredentialMetadata::Version::kV1,
