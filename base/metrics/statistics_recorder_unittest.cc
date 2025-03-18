e401f95c218f9d31968e9a289f8e5104751b5baf
Sayed Elabady <elabadysayed@chromium.org>
2025-03-18 06:35:53 -0700

    Revert "Remove runtime vlogging from android-official"
    This reverts commit fdcb27d8acbbfade7eaee3a0c0e4a140aec8e80f.
    
    Reason for revert: This is causing a failure for `LoggingTest.ScopedVmoduleSwitche` test on android-arm-tests
    builder, an example: https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-tests/30923/overview
    
    Original change's description:
    > Remove runtime vlogging from android-official
    >
    > This saves ~135k on the android-size bots. Some parts of the code do
    > compile-time enable ENABLED_VLOG_LEVEL in BUILD.gn to collect data in
    > the wild but paying for runtime vlogging universally for the few times
    > it's potentially used seems expensive.
    >
    > Linux seems to save ~460k, so it may be worth pursuing all platforms
    > as a follow-up, but Android as a platform seems least controversial to
    > remove runtime-vlogging on. It is also where size improvements are most
    > important.
    >
    > Bug: 404291877
    > Cq-Include-Trybots: luci.chromium.try:android-official
    > Change-Id: I01b77e67847ebb32f0ea2a2d7abd5b30843be06e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344673
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1433971}
    
    Bug: 404291877
    Cq-Include-Trybots: luci.chromium.try:android-official
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5ab2a45317475d1a6e36e07ab199d774912fd5c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367098
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434097}

--- base/metrics/statistics_recorder_unittest.cc
+++ base/metrics/statistics_recorder_unittest.cc
@@ -810,10 +810,6 @@ TEST_P(StatisticsRecorderTest, GlobalCallbackCalled) {
 }
 
 TEST_P(StatisticsRecorderTest, LogOnShutdownNotInitialized) {
-  // Some builds don't have runtime vlogging. See base/logging.h.
-  if (!VLOG_IS_ON(0)) {
-    GTEST_SKIP();
-  }
   ResetVLogInitialized();
   logging::SetMinLogLevel(logging::LOGGING_WARNING);
   InitializeStatisticsRecorder();
@@ -824,10 +820,6 @@ TEST_P(StatisticsRecorderTest, LogOnShutdownNotInitialized) {
 }
 
 TEST_P(StatisticsRecorderTest, LogOnShutdownInitializedExplicitly) {
-  // Some builds don't have runtime vlogging. See base/logging.h.
-  if (!VLOG_IS_ON(0)) {
-    GTEST_SKIP();
-  }
   ResetVLogInitialized();
   logging::SetMinLogLevel(logging::LOGGING_WARNING);
   InitializeStatisticsRecorder();
@@ -840,10 +832,6 @@ TEST_P(StatisticsRecorderTest, LogOnShutdownInitializedExplicitly) {
 }
 
 TEST_P(StatisticsRecorderTest, LogOnShutdownInitialized) {
-  // Some builds don't have runtime vlogging. See base/logging.h.
-  if (!VLOG_IS_ON(0)) {
-    GTEST_SKIP();
-  }
   ResetVLogInitialized();
   logging::SetMinLogLevel(logging::LOGGING_VERBOSE);
   InitializeStatisticsRecorder();
