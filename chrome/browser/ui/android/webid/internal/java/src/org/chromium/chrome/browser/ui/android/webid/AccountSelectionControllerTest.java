3312302c8091baa16f6c90a74beb1dda4876d99b
Nicolás Peña <npm@chromium.org>
2025-03-18 13:52:51 -0700

    [FedCM] Fix showing 3-line accounts in multi IDP continue button dialog
    Currently, if we initially show multi IDP UI and select an account, the
    next dialog shows 3 lines instead of 2 for the account row being
    confirmed because we do not unset the visibility of the secondary
    description. This CL fixes that.
    
    Bug: 392142580
    Change-Id: I4db1c1563a0a10af3971e75ebefa5c95fa43bfdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368427
    Reviewed-by: Zachary Tan <tanzachary@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434404}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionControllerTest.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionControllerTest.java
@@ -777,6 +777,13 @@ public class AccountSelectionControllerTest extends AccountSelectionJUnitTestBas
         assertNotNull(headerModel.get(IDP_BRAND_ICON));
         assertFalse(headerModel.get(IS_MULTIPLE_ACCOUNT_CHOOSER));
         assertFalse(headerModel.get(IS_MULTIPLE_IDPS));
+        // Should show continue button with the single account.
+        assertEquals("Incorrect item sheet count", 1, mSheetAccountItems.size());
+        testAccount(
+                mSheetAccountItems.get(0).model,
+                mNewUserAccount,
+                /* expectClickListener= */ false,
+                /* expectShowIdp= */ false);
 
         // Go back should show multiple IDP UI again.
         pressBack();
@@ -788,6 +795,17 @@ public class AccountSelectionControllerTest extends AccountSelectionJUnitTestBas
         assertNull(headerModel.get(IDP_BRAND_ICON));
         assertTrue(headerModel.get(IS_MULTIPLE_ACCOUNT_CHOOSER));
         assertTrue(headerModel.get(IS_MULTIPLE_IDPS));
+        assertEquals("Incorrect item sheet count", 3, mSheetAccountItems.size());
+        testAccount(
+                mSheetAccountItems.get(0).model,
+                mNewUserAccount,
+                /* expectClickListener= */ true,
+                /* expectShowIdp= */ true);
+        testAccount(
+                mSheetAccountItems.get(1).model,
+                mAnaAccountWithUseDifferentAccount,
+                /* expectClickListener= */ true,
+                /* expectShowIdp= */ true);
     }
 
     private void pressBack() {
--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionJUnitTestBase.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionJUnitTestBase.java
@@ -259,7 +259,7 @@ public class AccountSelectionJUnitTestBase {
                         "ana@email.example",
                         "Ana Doe",
                         "Ana",
-                        /* secondaryDescription= */ null,
+                        /* secondaryDescription= */ "email.example",
                         /* pictureBitmap= */ null,
                         /* circledBadgedPictureBitmap= */ Bitmap.createBitmap(
                                 100, 100, Bitmap.Config.ARGB_4444),
@@ -284,10 +284,10 @@ public class AccountSelectionJUnitTestBase {
         mAnaAccountWithoutBrandIcons =
                 new Account(
                         "Ana",
-                        "ana@email.example",
+                        "ana@email2.example",
                         "Ana Doe",
                         "Ana",
-                        /* secondaryDescription= */ null,
+                        /* secondaryDescription= */ "email2.example",
                         /* pictureBitmap= */ null,
                         /* circledBadgedPictureBitmap= */ Bitmap.createBitmap(
                                 100, 100, Bitmap.Config.ARGB_4444),
@@ -430,12 +430,13 @@ public class AccountSelectionJUnitTestBase {
                         mMockModalDialogManager);
     }
 
-    MVCListAdapter.ListItem buildAccountItem(Account account) {
+    MVCListAdapter.ListItem buildAccountItem(Account account, boolean showIdp) {
         return new MVCListAdapter.ListItem(
                 AccountSelectionProperties.ITEM_TYPE_ACCOUNT,
                 new PropertyModel.Builder(AccountProperties.ALL_KEYS)
                         .with(AccountProperties.ACCOUNT, account)
                         .with(AccountProperties.ON_CLICK_LISTENER, mAccountCallback)
+                        .with(AccountProperties.SHOW_IDP, showIdp)
                         .build());
     }
 
