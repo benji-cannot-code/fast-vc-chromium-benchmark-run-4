d96119fd1aceb63910149f597a1f14dac5c4c4e7
elabadysayed <elabadysayed@chromium.org>
2025-03-18 03:58:42 -0700

    [gardener] Disable failing tests in EmbeddedPermissionPromptTest
    Bug: 404464768
    Change-Id: Ib9dfa7f9dc903e22864e2c2a93a37d5f550fe25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6366956
    Auto-Submit: Sayed Elabady <elabadysayed@chromium.org>
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Owners-Override: Sayed Elabady <elabadysayed@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434050}

--- chrome/android/javatests/src/org/chromium/chrome/browser/permissions/EmbeddedPermissionPromptTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/permissions/EmbeddedPermissionPromptTest.java
@@ -519,6 +519,7 @@ public class EmbeddedPermissionPromptTest {
     @Test
     @MediumTest
     @Features.EnableFeatures({PermissionsAndroidFeatureList.PERMISSION_ELEMENT})
+    @DisabledTest(message = "crbug.com/404464768")
     public void testDisableLocationSettingsPromptText() throws Exception {
         RuntimePermissionTestUtils.setupGeolocationSystemMock(false);
         String[] requestablePermission =
@@ -544,6 +545,7 @@ public class EmbeddedPermissionPromptTest {
     @Test
     @MediumTest
     @Features.EnableFeatures({PermissionsAndroidFeatureList.PERMISSION_ELEMENT})
+    @DisabledTest(message = "crbug.com/404464768")
     public void testOsSettingsPromptText() throws Exception {
         mTestAndroidPermissionDelegate =
                 new TestAndroidPermissionDelegate(new String[] {}, RuntimePromptResponse.DENY);
