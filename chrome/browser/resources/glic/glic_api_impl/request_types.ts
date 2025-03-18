a058bf2bf4f1530b87127151e2f614c474d77fde
Gary Klassen <gklassen@chromium.org>
2025-03-17 19:56:50 -0700

    Add AIPageContentMetadata to the glic mojom.
    Change-Id: If50e46a732be4a182d3228b9f8769d3c7c4d4f88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356510
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1433948}

--- chrome/browser/resources/glic/glic_api_impl/request_types.ts
+++ chrome/browser/resources/glic/glic_api_impl/request_types.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {type WebClientInitialState} from '../glic.mojom-webui.js';
-import type {AnnotatedPageData, ActInFocusedTabParams, ActInFocusedTabResult, ChromeVersion, DraggableArea, ErrorReasonTypes, ErrorWithReason, FocusedTabCandidate, FocusedTabData, InvalidCandidateError, NoCandidateTabError, OpenPanelInfo, PanelOpeningData, PanelState, PdfDocumentData, Screenshot, ScrollToParams, TabContextOptions, TabContextResult, TabData, UserProfileInfo} from '../glic_api/glic_api.js';
+import type {AnnotatedPageData, ActInFocusedTabParams, ActInFocusedTabResult, ChromeVersion, DraggableArea, ErrorReasonTypes, ErrorWithReason, FocusedTabCandidate, FocusedTabData, InvalidCandidateError, NoCandidateTabError, OpenPanelInfo, PageMetadata, PanelOpeningData, PanelState, PdfDocumentData, Screenshot, ScrollToParams, TabContextOptions, TabContextResult, TabData, UserProfileInfo} from '../glic_api/glic_api.js';
 
 /*
 This file defines messages sent over postMessage in-between the Glic WebUI
@@ -430,8 +430,8 @@ export declare interface PdfDocumentDataPrivate extends
 export declare interface AnnotatedPageDataPrivate extends
     Omit<AnnotatedPageData, 'annotatedPageContent'> {
   annotatedPageContent?: ArrayBuffer;
+  metadata?: PageMetadata;
 }
-
 
 export class ErrorWithReasonImpl<T extends keyof ErrorReasonTypes> extends Error
     implements ErrorWithReason<T> {
