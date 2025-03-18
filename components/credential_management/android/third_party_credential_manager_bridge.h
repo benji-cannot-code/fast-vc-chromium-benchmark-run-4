4c6329296c310c8e3b857773176ec7636d011a45
Ivana Žužić <izuzic@google.com>
2025-03-18 09:36:40 -0700

    [3PWebAPIRequestFwd] Implement the Store method of the bridge
    The bridge calls the CredentialManagement API to store a credential.
    
    Bug: 404198714
    Change-Id: I8c725c289c29b388a4100dc8c800456c142c0c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6362356
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434191}

--- components/credential_management/android/third_party_credential_manager_bridge.h
+++ components/credential_management/android/third_party_credential_manager_bridge.h
@@ -12,8 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/types/pass_key.h"
 #include "components/password_manager/core/common/credential_manager_types.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 
 namespace credential_management {
+using StoreCallback = base::OnceCallback<void()>;
 using GetCallback = base::OnceCallback<void(
     password_manager::CredentialManagerError,
     const std::optional<password_manager::CredentialInfo>&)>;
@@ -31,8 +33,14 @@ class ThirdPartyCredentialManagerBridge {
 
     // Creates the JNI bridge.
     virtual void CreateBridge(ThirdPartyCredentialManagerBridge* bridge) = 0;
+
     // Gets a credential from the Android Credential Manager.
     virtual void Get(const std::string& origin) = 0;
+
+    // Stores a credential to the Android Credential Manager.
+    virtual void Store(const std::string& username,
+                       const std::string& password,
+                       const std::string& origin) = 0;
   };
 
   ThirdPartyCredentialManagerBridge();
@@ -47,7 +55,7 @@ class ThirdPartyCredentialManagerBridge {
 
   ~ThirdPartyCredentialManagerBridge();
 
-  void Create(GetCallback callback);
+  void Create(absl::variant<GetCallback, StoreCallback> callback);
 
   void Get(const std::string& origin);
   void OnPasswordCredentialReceived(
@@ -57,8 +65,15 @@ class ThirdPartyCredentialManagerBridge {
       const base::android::JavaParamRef<jstring>& j_origin);
   void OnGetPasswordCredentialError(JNIEnv* env);
 
+  void Store(const std::string& username,
+             const std::string& password,
+             const std::string& origin);
+  void OnCreateCredentialResponse(JNIEnv* env, jboolean success);
+
  private:
-  GetCallback callback_;
+  // TODO(crbug.com/404505860): Pass the callback to Java instead of having it
+  // as a member.
+  absl::variant<GetCallback, StoreCallback> callback_;
   // Forwards all requests to JNI. Can be replaced in tests.
   std::unique_ptr<JniDelegate> jni_delegate_;
 };
