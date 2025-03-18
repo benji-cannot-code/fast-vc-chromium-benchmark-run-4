95a6976f294ed3e10e07e3cb01c4c69f8e69d5ab
Xiaochen Zhou <xiaochenzh@chromium.org>
2025-03-18 08:01:03 -0700

    [Privacy Sandbox] Add pre-install attestation component to Android APK assets
    This CL adds the pre-install attestation component to Android APK
    assets. It does not have behavior change. Privacy Sandbox attestation
    component will read the pre-install component to populate the enrollment
    list, which will be done in the next few CLs.
    
    The .expected files are edited according to the android-binary-size
    try bot instructions.
    
    Bug: 40942169
    Change-Id: Id7e7186320ae611625304aab999ee63a61a22d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6351128
    Reviewed-by: Andrew Verge <averge@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434130}

---
+++ components/privacy_sandbox/privacy_sandbox_attestations/preload/android_apk_assets.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PRIVACY_SANDBOX_PRIVACY_SANDBOX_ATTESTATIONS_PRELOAD_ANDROID_APK_ASSETS_H_
+#define COMPONENTS_PRIVACY_SANDBOX_PRIVACY_SANDBOX_ATTESTATIONS_PRELOAD_ANDROID_APK_ASSETS_H_
+
+#include <string_view>
+
+namespace privacy_sandbox {
+
+inline constexpr std::string_view kManifestAssetPath =
+    "assets/privacy_sandbox_attestations/manifest.json";
+
+inline constexpr std::string_view kAttestationsListAssetPath =
+    "assets/privacy_sandbox_attestations/privacy-sandbox-attestations.dat";
+
+}  // namespace privacy_sandbox
+
+#endif  // COMPONENTS_PRIVACY_SANDBOX_PRIVACY_SANDBOX_ATTESTATIONS_PRELOAD_ANDROID_APK_ASSETS_H_
