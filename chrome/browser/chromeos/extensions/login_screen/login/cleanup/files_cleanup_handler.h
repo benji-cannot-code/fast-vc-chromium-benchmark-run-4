40900241ef3dcaa2bca9911157c93213de70b569
Fergal Daly <fergal@chromium.org>
2025-03-17 22:49:51 -0700

    Revert "demomode: Delete user created files between shopper sessions"
    This reverts commit 7166891c627dc309ab9fee80a4e56af9416d022c.
    
    Reason for revert:
    
    DemoSessionLoginIdleHandlerTest.CleanUpLocalFiles (newly added in this CL) fails on MSAN and ASAN
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/63689/blamelist
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/50746/blamelist
    
    
    
    Original change's description:
    > demomode: Delete user created files between shopper sessions
    >
    > Delete all files under 'MyFiles' and reset demo media data when device
    > is idle, which is the end of one shopper session.
    >
    > Bug: 396731490
    > Change-Id: Ic9a2f69f33261661dc771a980f036f14580cf1b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6292333
    > Reviewed-by: Li Lin <llin@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1433918}
    
    Bug: 396731490
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I42555a18b20148c77092a16c9dc61bb3537c456c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6365596
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Fergal Daly <fergal@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433975}

--- chrome/browser/chromeos/extensions/login_screen/login/cleanup/files_cleanup_handler.h
+++ chrome/browser/chromeos/extensions/login_screen/login/cleanup/files_cleanup_handler.h
@@ -20,7 +20,6 @@ namespace chromeos {
 class FilesCleanupHandler : public CleanupHandler {
  public:
   FilesCleanupHandler();
-  explicit FilesCleanupHandler(scoped_refptr<base::TaskRunner> task_runner);
   ~FilesCleanupHandler() override;
 
   // CleanupHandler:
@@ -36,4 +35,4 @@ class FilesCleanupHandler : public CleanupHandler {
 
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_CHROMEOS_EXTENSIONS_LOGIN_SCREEN_LOGIN_CLEANUP_FILES_CLEANUP_HANDLER_H_
+#endif  //  CHROME_BROWSER_CHROMEOS_EXTENSIONS_LOGIN_SCREEN_LOGIN_CLEANUP_FILES_CLEANUP_HANDLER_H_
