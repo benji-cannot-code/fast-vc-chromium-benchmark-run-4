49649033b7a0051b3da8c9dca2477db22677f12c
David Risney <david.risney@microsoft.com>
2025-03-18 11:34:49 -0700

    Defer registerProtocolHandler registration work while prerendering.
    This change modifies navigator.registerProtocolHandler and
    navigator.unregisterProtocolHandler while prerendering. They will
    continue to synchronously perform parameter validation, but they will
    defer the work to register or unregister protocol handlers until after
    the prerendered document is activated.
    
    I've added WPT to test the synchronous parameter validation, the
    deferred registration, and the deferred unregistration. In order for the
    unregistration test to work in content shell I've added a Shell::UnregisterProtocolHandler implementation. It previously only had
    a Shell::RegisterProtocolHandler.
    
    Bug: 40288240
    Change-Id: Ibff03d4acd50434f5753c99a5314fc10e2f3f375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322749
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Dave Risney <david.risney@microsoft.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434291}

--- third_party/blink/renderer/modules/navigatorcontentutils/navigator_content_utils.cc
+++ third_party/blink/renderer/modules/navigatorcontentutils/navigator_content_utils.cc
@@ -208,9 +208,17 @@ void NavigatorContentUtils::registerProtocolHandler(
                         ? WebFeature::kRegisterProtocolHandlerSecureOrigin
                         : WebFeature::kRegisterProtocolHandlerInsecureOrigin);
 
-  NavigatorContentUtils::From(navigator, *window->GetFrame())
-      .Client()
-      ->RegisterProtocolHandler(scheme, window->CompleteURL(url));
+  Document* document = window->document();
+  auto* client =
+      NavigatorContentUtils::From(navigator, *window->GetFrame()).Client();
+
+  if (!document->IsPrerendering()) {
+    client->RegisterProtocolHandler(scheme, window->CompleteURL(url));
+  } else {
+    document->AddPostPrerenderingActivationStep(WTF::BindOnce(
+        &NavigatorContentUtilsClient::RegisterProtocolHandler,
+        WrapWeakPersistent(client), scheme, window->CompleteURL(url)));
+  }
 }
 
 void NavigatorContentUtils::unregisterProtocolHandler(
@@ -235,9 +243,17 @@ void NavigatorContentUtils::unregisterProtocolHandler(
   if (!VerifyCustomHandlerURL(*window, url, exception_state, security_level))
     return;
 
-  NavigatorContentUtils::From(navigator, *window->GetFrame())
-      .Client()
-      ->UnregisterProtocolHandler(scheme, window->CompleteURL(url));
+  Document* document = window->document();
+  auto* client =
+      NavigatorContentUtils::From(navigator, *window->GetFrame()).Client();
+
+  if (!document->IsPrerendering()) {
+    client->UnregisterProtocolHandler(scheme, window->CompleteURL(url));
+  } else {
+    document->AddPostPrerenderingActivationStep(WTF::BindOnce(
+        &NavigatorContentUtilsClient::UnregisterProtocolHandler,
+        WrapWeakPersistent(client), scheme, window->CompleteURL(url)));
+  }
 }
 
 void NavigatorContentUtils::Trace(Visitor* visitor) const {
