4a839893bd3d48adaf7c1738b57ed4cdcc553dd7
Mingyu Lei <leimy@chromium.org>
2025-03-18 02:20:41 -0700

    Add ShouldPrioritizeForBackForwardCache method in ContentBrowserClient
    For the ChromeContentBrowserClient implementation, this returns true if
    the URL is a DSE result page.
    
    The next CL will use this method and apply special handling for the
    prioritized page.
    
    Change-Id: I4f17acca49013974cff75eb920a208df6fa77d3d
    Bug: 402313221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333191
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434021}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -1950,4 +1950,10 @@ bool ContentBrowserClient::ShouldEnableSubframeZoom() {
   return false;
 }
 
+bool ContentBrowserClient::ShouldPrioritizeForBackForwardCache(
+    BrowserContext* browser_context,
+    const GURL& url) {
+  return false;
+}
+
 }  // namespace content
