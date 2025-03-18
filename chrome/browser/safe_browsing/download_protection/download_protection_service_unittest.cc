3200a6a86ec673a7247e5e2c8dc32b15e9215b07
Lily Chen <chlily@chromium.org>
2025-03-18 09:37:50 -0700

    [MaliciousApkDownloadCheck] Implement random sampling
    This is part 5 of 6 of relanding code that was originally landed in
    crrev.com/c/6309053 but was reverted.
    
    This CL implements random sampling of eligible downloads, controlled by
    a FeatureParam that is configurable via fieldtrial. This is separate
    from the sampling of allowlisted downloads and the sampling of
    unsupported filetypes, which are both supported on desktop platforms
    but not Android.
    
    Bug: 397407934
    Change-Id: Ib0f07da7b6e4688c66a0b645e160cb0663a66dab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6341010
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434193}

--- chrome/browser/safe_browsing/download_protection/download_protection_service_unittest.cc
+++ chrome/browser/safe_browsing/download_protection/download_protection_service_unittest.cc
@@ -5587,6 +5587,12 @@ class AndroidDownloadProtectionTest
                                     outcome, 1);
   }
 
+  void OverrideNextShouldSample(bool should_sample) {
+    static_cast<DownloadProtectionDelegateAndroid*>(
+        download_service_->delegate())
+        ->SetNextShouldSampleForTesting(should_sample);
+  }
+
   void ExpectNoCheckClientDownload(download::DownloadItem* item) {
     EXPECT_CALL(*binary_feature_extractor_.get(), CheckSignature(_, _))
         .Times(0);
@@ -5640,6 +5646,8 @@ class AndroidDownloadProtectionTest
           base::DoNothing()) {
     // Response to any requests will be DANGEROUS.
     PrepareResponse(ClientDownloadResponse::DANGEROUS, net::HTTP_OK, net::OK);
+    // Override the random sampling.
+    OverrideNextShouldSample(true);
 
     ResetHistogramTester();
     {
@@ -5865,6 +5873,24 @@ TEST_P(AndroidDownloadProtectionTest, IsSupportedDownloadFalse) {
         item_display_name_not_apk, final_path_));
   }
   ExpectHistogramUniqueSample(Outcome::kDownloadNotSupportedType);
+
+  ResetHistogramTester();
+  {
+    // An otherwise eligible item that is just not sampled.
+    NiceMockDownloadItem item_eligible;
+    OverrideNextShouldSample(false);
+    content::DownloadItemUtils::AttachInfoForTesting(&item_eligible, profile(),
+                                                     nullptr);
+    PrepareBasicDownloadItemWithContentUri(
+        &item_eligible,
+        /*url_chain_items=*/{"https://evil.com/bla.apk"},
+        /*referrer_url=*/"",
+        /*display_name=*/base::FilePath(FILE_PATH_LITERAL("a.apk")));
+
+    EXPECT_FALSE(
+        download_service_->IsSupportedDownload(item_eligible, final_path_));
+  }
+  ExpectHistogramUniqueSample(Outcome::kNotSampled);
 }
 
 // Tests that CheckClientDownload is called even if the download is not a
