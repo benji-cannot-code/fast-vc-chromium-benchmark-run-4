d43071bc591809525270d876f8551631a8ac4066
Fergal Daly <fergal@chromium.org>
2025-03-17 23:03:57 -0700

    Revert "[ChromeOS] Mute ARC VM audio when entering locked fullscreen mode"
    This reverts commit 5153e29257d6b4e391bbfe0623b0792d177646ea.
    
    Reason for revert: 
    
    Several ArcLockedFullscreenManagerTest tests (newly added in this CL) are failing on MSAN and ASAN bots
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/50745/blamelist
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/63689/blamelist
    
    
    Original change's description:
    > [ChromeOS] Mute ARC VM audio when entering locked fullscreen mode
    >
    > This change attempts to mute ARC VM audio when entering locked
    > fullscreen mode and unmute when exiting the mode. This attempts to
    > deprecate and phase out the pre-existing flow of disabling critical ARC
    > apps for locked fullscreen mode.
    >
    > Bug: b:399460520
    > Change-Id: Icad207df8d00c758d6cdf18fd95c9c1f5b4ff9fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313925
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    > Reviewed-by: Luzanne Batoon <batoon@google.com>
    > Reviewed-by: Luke Cao <caott@google.com>
    > Reviewed-by: April Zhou <aprilzhou@google.com>
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1433886}
    
    Bug: b:399460520
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If8551a81aebd0b599ef632f2ddacf80f077cfe2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6365276
    Owners-Override: Fergal Daly <fergal@google.com>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433978}

--- chrome/browser/ui/ash/shell_delegate/chrome_shell_delegate.cc
+++ chrome/browser/ui/ash/shell_delegate/chrome_shell_delegate.cc
@@ -32,8 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ash/api/tasks/chrome_tasks_delegate.h"
 #include "chrome/browser/ash/arc/arc_util.h"
-#include "chrome/browser/ash/arc/locked_fullscreen/arc_locked_fullscreen_manager.h"
-#include "chrome/browser/ash/arc/session/arc_service_launcher.h"
+#include "chrome/browser/ash/arc/session/arc_session_manager.h"
 #include "chrome/browser/ash/assistant/assistant_util.h"
 #include "chrome/browser/ash/crosapi/crosapi_ash.h"
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
@@ -358,20 +357,26 @@ void ChromeShellDelegate::SetUpEnvironmentForLockedFullscreen(
   ChromeCaptureModeDelegate::Get()->SetIsScreenCaptureLocked(locked);
 
   // Get the primary profile as that's what ARC and Assistant are attached to.
-  const Profile* const profile = ProfileManager::GetPrimaryUserProfile();
+  const Profile* profile = ProfileManager::GetPrimaryUserProfile();
   // Commands below require profile.
   if (!profile) {
     return;
   }
 
-  arc::ArcServiceLauncher* const arc_service_launcher =
-      arc::ArcServiceLauncher::Get();
-  if (arc_service_launcher &&
-      arc_service_launcher->arc_locked_fullscreen_manager() &&
-      !ash::IsArcWindow(window_state.window()) &&
+  // Disable ARC while in the locked fullscreen mode.
+  arc::ArcSessionManager* const arc_session_manager =
+      arc::ArcSessionManager::Get();
+  if (!ash::IsArcWindow(window_state.window()) && arc_session_manager &&
       arc::IsArcAllowedForProfile(profile)) {
-    arc_service_launcher->arc_locked_fullscreen_manager()
-        ->UpdateForLockedFullscreenMode(locked);
+    if (locked) {
+      // Disable ARC, preserve data.
+      arc_session_manager->RequestDisable();
+    } else {
+      // Re-enable ARC if needed.
+      if (arc::IsArcPlayStoreEnabledForProfile(profile)) {
+        arc_session_manager->RequestEnable();
+      }
+    }
   }
 
   if (assistant::IsAssistantAllowedForProfile(profile) ==
