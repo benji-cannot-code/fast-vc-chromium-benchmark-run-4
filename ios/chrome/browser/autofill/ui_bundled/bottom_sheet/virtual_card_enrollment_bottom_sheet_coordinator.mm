79df26dde9daaba393cb8be0990e1b88d43683bc
Benjamin Williams <bwwilliams@google.com>
2025-03-18 11:40:29 -0700

    [iOS][Code Health] Fix UISheetPresentationControllerDetent syntax
    This CL corrects improper instances of property-style access for detents
    to proper method call syntax.
    
    Impact:
    
    - No functional changes to behavior
    - Improves code health by using the correct Objective-C method
      invocation syntax
    
    This CL was uploaded by git cl split.
    
    R=slobodan@chromium.org
    
    Bug: 402705893
    Change-Id: I8d264d645f8b1328b8867537bbc75e483ee7f6b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368914
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434298}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_coordinator.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_coordinator.mm
@@ -77,8 +77,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       self.viewController.sheetPresentationController;
   presentationController.prefersEdgeAttachedInCompactHeight = YES;
   presentationController.detents = @[
-    UISheetPresentationControllerDetent.mediumDetent,
-    UISheetPresentationControllerDetent.largeDetent,
+    [UISheetPresentationControllerDetent mediumDetent],
+    [UISheetPresentationControllerDetent largeDetent],
   ];
 
   [self.baseViewController presentViewController:self.viewController
