2b4d76bf8667b260663f500894de53090e9edbe1
Michael Thiessen <mthiesse@chromium.org>
2025-03-18 13:57:35 -0700

    Clean up MojoMessageAlwaysUseLatestVersion
    Flag launched to M131 and shipped by default in M135.
    
    Bug: 359312468
    Change-Id: Ibb3c81daf6c67b55bc8ba99148327d29332956bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356439
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434408}

--- mojo/public/cpp/bindings/lib/message_internal.cc
+++ mojo/public/cpp/bindings/lib/message_internal.cc
@@ -11,36 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace internal {
 
-namespace {
-
-size_t ComputeHeaderSize(uint32_t flags,
-                         size_t payload_interface_id_count,
-                         int64_t creation_timeticks_us) {
-  if (creation_timeticks_us > 0 ||
-      base::FeatureList::IsEnabled(kMojoMessageAlwaysUseLatestVersion)) {
-    // Version 3
-    return sizeof(MessageHeaderV3);
-  } else if (payload_interface_id_count > 0) {
-    // Version 2
-    return sizeof(MessageHeaderV2);
-  } else if (flags &
-             (Message::kFlagExpectsResponse | Message::kFlagIsResponse)) {
-    // Version 1
-    return sizeof(MessageHeaderV1);
-  } else {
-    // Version 0
-    return sizeof(MessageHeader);
-  }
-}
-
-}  // namespace
-
-size_t ComputeSerializedMessageSize(uint32_t flags,
-                                    size_t payload_size,
-                                    size_t payload_interface_id_count,
-                                    int64_t creation_timeticks_us) {
-  const size_t header_size = ComputeHeaderSize(
-      flags, payload_interface_id_count, creation_timeticks_us);
+size_t ComputeSerializedMessageSize(size_t payload_size,
+                                    size_t payload_interface_id_count) {
+  const size_t header_size = sizeof(MessageHeaderV3);
   if (payload_interface_id_count > 0) {
     return Align(header_size + Align(payload_size) +
                  ArrayDataTraits<uint32_t>::GetStorageSize(
