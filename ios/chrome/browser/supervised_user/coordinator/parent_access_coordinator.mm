c777e830795c3ce6f4ef5d3238dbd8f59332d060
Stepan Khapugin <stkhapugin@chromium.org>
2025-03-18 10:55:44 -0700

    [iOS] Use ChromeCoordinator's `profile` property.
    Simplify code by using `self.profile` where appropriate.
    Some uses with a local `profile = self.profile` are not
    updated to optimize for better readability.
    
    Created with git cl split.
    
    This CL was uploaded by git cl split.
    
    R=ljjlee@google.com
    
    Bug: 401523912
    Change-Id: I9600c1c13e5722a21e5702f750c00c64ff6699db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368515
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434252}

--- ios/chrome/browser/supervised_user/coordinator/parent_access_coordinator.mm
+++ ios/chrome/browser/supervised_user/coordinator/parent_access_coordinator.mm
@@ -68,7 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)start {
-  ProfileIOS* profile = self.browser->GetProfile()->GetOriginalProfile();
+  ProfileIOS* profile = self.profile->GetOriginalProfile();
 
   // Set up the WebState and its TabHelper.
   web::WebState::CreateParams params = web::WebState::CreateParams(profile);
