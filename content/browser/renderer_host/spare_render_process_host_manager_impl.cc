705dfef81cd05c5252b3e561103868e9691bd366
Patrick Monette <pmonette@chromium.org>
2025-03-18 10:43:58 -0700

    Add new metrics for the multiple spare RPHs experiment
    1. An histogram that records the time between creating a spare
    and when it is taken.
    2. An histogram that records whether the spare was fully initialized
    when the spare was taken.
    
    Also fixes an existing metric that relied on a timer that was never
    started.
    
    Bug: 364635886
    Change-Id: I25808d4f0248ac138795402ef9a89a911622d97d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354704
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434232}

--- content/browser/renderer_host/spare_render_process_host_manager_impl.cc
+++ content/browser/renderer_host/spare_render_process_host_manager_impl.cc
@@ -38,6 +38,10 @@ BASE_FEATURE(kKillSpareRenderOnMemoryPressure,
              "KillSpareRenderOnMemoryPressure",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+constexpr char kSpareRendererTakenTimeSinceCreation[] =
+    "BrowserRenderProcessHost.SpareRendererTaken.TimeSinceCreation";
+constexpr char kSpareRendererTakenIsReady[] =
+    "BrowserRenderProcessHost.SpareRendererTaken.IsReady";
 constexpr char kSpareRendererDispatchResultUmaName[] =
     "BrowserRenderProcessHost.SpareRendererDispatchResult";
 constexpr char kPreviouslyTakenSourceUmaName[] =
@@ -240,6 +244,7 @@ void LogNoSparePresentUmas(
 }
 
 void LogSpareProcessTakeActionUMAs(
+    RenderProcessHost* host,
     SpareProcessMaybeTakeAction action,
     NoSpareRendererReason no_spare_renderer_reason,
     const ProcessAllocationContext& allocation_context,
@@ -249,6 +254,12 @@ void LogSpareProcessTakeActionUMAs(
   if (action == SpareProcessMaybeTakeAction::kNoSparePresent) {
     LogNoSparePresentUmas(no_spare_renderer_reason, allocation_context,
                           previous_taken_context);
+  } else if (action == SpareProcessMaybeTakeAction::kSpareTaken) {
+    CHECK(host);
+    base::UmaHistogramBoolean(kSpareRendererTakenIsReady, host->IsReady());
+    base::UmaHistogramLongTimes(
+        kSpareRendererTakenTimeSinceCreation,
+        base::TimeTicks::Now() - host->GetLastInitTime());
   }
 }
 
@@ -272,6 +283,8 @@ SpareRenderProcessHostManagerImpl::SpareRenderProcessHostManagerImpl()
           base::BindRepeating(
               &SpareRenderProcessHostManagerImpl::OnMetricsHeartbeatTimerFired,
               base::Unretained(this))) {
+  metrics_heartbeat_timer_.Reset();
+
   // Immediately start the timer if the system is already under memory pressure.
   if (IsCurrentlyUnderMemoryPressure()) {
     check_memory_pressure_timer_.Reset();
@@ -580,8 +593,9 @@ RenderProcessHost* SpareRenderProcessHostManagerImpl::MaybeTakeSpare(
   } else {
     action = SpareProcessMaybeTakeAction::kSpareTaken;
   }
-  LogSpareProcessTakeActionUMAs(action, no_spare_renderer_reason_,
-                                allocation_context, previous_taken_context_);
+  LogSpareProcessTakeActionUMAs(next_spare_rph, action,
+                                no_spare_renderer_reason_, allocation_context,
+                                previous_taken_context_);
   if (spare_renderer_maybe_take_timer_) {
     auto maybe_take_time = spare_renderer_maybe_take_timer_->Elapsed();
     base::UmaHistogramLongTimes(
