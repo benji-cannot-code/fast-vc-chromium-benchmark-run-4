4f917ae7f81f753856e11548c079b2db754b5021
Guido Urdaneta <guidou@chromium.org>
2025-03-18 02:10:53 -0700

    [VideoCapture] Add chrome://flags entry for camera usage monitoring
    This CL adds an entry in chrome://flags for easier user manipulation
    of this feature.
    
    
    Bug: 377126111
    Change-Id: Ied923b0133d75fa2fd9fc9fd8e3e8e517d1adac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361854
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434020}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3181,6 +3181,9 @@ extern const char kMediaFoundationClearDescription[];
 extern const char kMediaFoundationClearStrategyName[];
 extern const char kMediaFoundationClearStrategyDescription[];
 
+extern const char kMediaFoundationCameraUsageMonitoringName[];
+extern const char kMediaFoundationCameraUsageMonitoringDescription[];
+
 extern const char kRawAudioCaptureName[];
 extern const char kRawAudioCaptureDescription[];
 
--- media/capture/capture_switches.cc
+++ media/capture/capture_switches.cc
@@ -32,12 +32,9 @@ bool IsVideoCaptureUseGpuMemoryBufferEnabled() {
 }
 
 #if BUILDFLAG(IS_WIN)
-BASE_FEATURE(kMediaFoundationCameraUsageMonitoring,
-             "MediaFoundationCameraUsageMonitoring",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 bool IsMediaFoundationCameraUsageMonitoringEnabled() {
-  return base::FeatureList::IsEnabled(kMediaFoundationCameraUsageMonitoring);
+  return base::FeatureList::IsEnabled(
+      features::kMediaFoundationCameraUsageMonitoring);
 }
 #endif
 
@@ -52,5 +49,12 @@ BASE_FEATURE(kWebRtcPipeWireCamera,
              "WebRtcPipeWireCamera",
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif  // defined(WEBRTC_USE_PIPEWIRE)
+
+#if BUILDFLAG(IS_WIN)
+// Controls monitoring for camera usage by other applications.
+BASE_FEATURE(kMediaFoundationCameraUsageMonitoring,
+             "MediaFoundationCameraUsageMonitoring",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(IS_WIN)
 
 }  // namespace features
