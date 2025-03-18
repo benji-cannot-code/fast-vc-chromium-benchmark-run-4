4653ce9e69b73d7317908169d2bc566d27c20d47
Duncan Mercer <mercerd@google.com>
2025-03-18 11:01:55 -0700

    [lensoverlay] Fix the bucket size of DocumentSize metric
    Before, the bucket was using the KB function which defined a bug as:
    MIN: 1000 - MAX: 500,000 - # of Buckets - 1000
    
    This had two main issues:
      1) The MIN is too high, causing lots of pages to be bucketed in the
         smallest bucket: https://screenshot.googleplex.com/5rAJpMiCLYyCh4d
      2) Each bucket is 1000 wide, so not a ton of granularity at smaller
         numbers
    
    To fix this, we are now using a custom bucket count with a MIN at 1, MAX
    at 100,000, and 100 buckets. This should give the correct level of
    granularity.
    
    This also means renaming the histogram as per,
    http://shortn/_7zCsHRzqOp.
    
    Bug: 404253089
    Change-Id: I9ed4cf9e2a91dd6b2737b8da8c189b8325f62065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6363654
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434263}

--- chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
+++ chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
@@ -5434,7 +5434,7 @@ IN_PROC_BROWSER_TEST_P(LensOverlayControllerBrowserPDFContextualizationTest,
 
   // Verify the histogram recorded the new byte size.
   histogram_tester.ExpectTotalCount(
-      "Lens.Overlay.ByPageContentType.Pdf.DocumentSize",
+      "Lens.Overlay.ByPageContentType.Pdf.DocumentSize2",
       /*expected_count=*/2);
   // Verify the histogram two documents of one page.
   histogram_tester.ExpectBucketCount(
@@ -5624,7 +5624,7 @@ IN_PROC_BROWSER_TEST_P(LensOverlayControllerBrowserPDFContextualizationTest,
       /*sample*/ true,
       /*expected_bucket_count=*/1);
   histogram_tester.ExpectTotalCount(
-      "Lens.Overlay.ByPageContentType.Pdf.DocumentSize",
+      "Lens.Overlay.ByPageContentType.Pdf.DocumentSize2",
       /*expected_count=*/1);
   histogram_tester.ExpectUniqueSample(
       "Lens.Overlay.ByPageContentType.Pdf.PageCount", /*sample*/ 1,
@@ -6277,15 +6277,15 @@ IN_PROC_BROWSER_TEST_F(LensOverlayControllerBrowserTest,
   // testing on is so small it rounds to 0 KB.
   ASSERT_TRUE(base::test::RunUntil([&]() {
     return histogram_tester.GetBucketCount(
-               "Lens.Overlay.ByPageContentType.Html.DocumentSize", 0) == 2;
+               "Lens.Overlay.ByPageContentType.Html.DocumentSize2", 0) == 2;
   }));
 
   // Verify the size histograms recorded the new bytes.
   histogram_tester.ExpectTotalCount(
-      "Lens.Overlay.ByPageContentType.PlainText.DocumentSize",
+      "Lens.Overlay.ByPageContentType.PlainText.DocumentSize2",
       /*expected_count=*/2);
   histogram_tester.ExpectTotalCount(
-      "Lens.Overlay.ByPageContentType.Html.DocumentSize",
+      "Lens.Overlay.ByPageContentType.Html.DocumentSize2",
       /*expected_count=*/2);
   histogram_tester.ExpectTotalCount(
       "Lens.Overlay.ContextualSearchBox.ByPageContentType.PlainText."
@@ -6406,10 +6406,10 @@ IN_PROC_BROWSER_TEST_F(LensOverlayControllerBrowserTest,
       /*sample*/ true,
       /*expected_bucket_count=*/1);
   histogram_tester.ExpectTotalCount(
-      "Lens.Overlay.ByPageContentType.PlainText.DocumentSize",
+      "Lens.Overlay.ByPageContentType.PlainText.DocumentSize2",
       /*expected_count=*/1);
   histogram_tester.ExpectTotalCount(
-      "Lens.Overlay.ByPageContentType.Html.DocumentSize",
+      "Lens.Overlay.ByPageContentType.Html.DocumentSize2",
       /*expected_count=*/1);
 
   // Verify UKM metrics were recorded.
