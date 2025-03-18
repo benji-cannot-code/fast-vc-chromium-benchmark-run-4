c9b79e6debdd5d4f9b78cdab5a9e4036afa1b68d
Vikram <vpasupathy@chromium.org>
2025-03-17 18:00:04 -0700

    media: Report Unknown MediaCodec CodecExceptions
    A lot of the undefined MediaCodec CodecExceptions are negative. This CL
    introduces a histogram to start reporting the error codes, so we can
    then later cross reference and figure out what they are, ultimately
    adding them to our MediaCodecStatus enum.
    
    Bug: 283544711
    Change-Id: I72209c2282ec99c5f7b6496b1758a1f8222483cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6353625
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433910}

--- media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
@@ -22,6 +22,7 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.Log;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 
@@ -173,6 +174,11 @@ class MediaCodecBridge {
                 return MediaCodecStatus.RECLAIMED;
             default:
                 Log.e(TAG, "Unknown CodecException error: " + e.getErrorCode());
+                if (e.getErrorCode() < 0) {
+                    RecordHistogram.recordSparseHistogram(
+                            "Media.MediaCodecError.NegativeCodecExceptionErrorCode",
+                            e.getErrorCode());
+                }
                 return MediaCodecStatus.UNKNOWN_CODEC_EXCEPTION;
         }
     }
