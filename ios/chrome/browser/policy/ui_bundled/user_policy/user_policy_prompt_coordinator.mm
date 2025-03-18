ab5376905b882e63fb68d3c1ea58b7f5ecab77c3
Benjamin Williams <bwwilliams@google.com>
2025-03-18 11:59:04 -0700

    [iOS][Code Health] Fix UISheetPresentationControllerDetent syntax
    This CL corrects improper instances of property-style access for detents
    to proper method call syntax.
    
    Impact:
    
    - No functional changes to behavior
    - Improves code health by using the correct Objective-C method
      invocation syntax
    
    This CL was uploaded by git cl split.
    
    R=vincb@google.com
    
    Bug: 402705893
    Change-Id: I582eb859bb2c78573d8b8113e8d77deb4c5d8550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368796
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434318}

--- ios/chrome/browser/policy/ui_bundled/user_policy/user_policy_prompt_coordinator.mm
+++ ios/chrome/browser/policy/ui_bundled/user_policy/user_policy_prompt_coordinator.mm
@@ -77,8 +77,8 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
       self.presentedViewController.sheetPresentationController;
   presentationController.prefersEdgeAttachedInCompactHeight = YES;
   presentationController.detents = @[
-    UISheetPresentationControllerDetent.mediumDetent,
-    UISheetPresentationControllerDetent.largeDetent
+    [UISheetPresentationControllerDetent mediumDetent],
+    [UISheetPresentationControllerDetent largeDetent]
   ];
   presentationController.preferredCornerRadius = kHalfSheetCornerRadius;
 
