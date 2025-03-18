67ba4387aa766a1f1d66a0bd6181725440766bb4
Kenichi Ishibashi <bashi@chromium.org>
2025-03-17 18:12:01 -0700

    HttpStreamPool: Don't start QuicTask when failing
    AttemptManager::MaybeAttemptQuic() should not start QuicTask when
    `is_failing_` is true. Otherwise, we end up hitting CHECK(!is_failing_)
    in AttemptManager::OnQuicTaskComplete() when a QUIC attempt completes
    successfully.
    
    Bug: 403341337
    Change-Id: I06b50d92e16eff6c37061ad3af77331c8734b001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6355632
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433916}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -1278,7 +1278,7 @@ void HttpStreamPool::AttemptManager::MaybeNotifySSLConfigReady() {
 
 void HttpStreamPool::AttemptManager::MaybeAttemptQuic() {
   CHECK(service_endpoint_request_);
-  if (!CanUseQuic() || quic_task_result_.has_value() ||
+  if (is_failing_ || !CanUseQuic() || quic_task_result_.has_value() ||
       !service_endpoint_request_->EndpointsCryptoReady()) {
     return;
   }
