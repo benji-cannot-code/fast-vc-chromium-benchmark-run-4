27aa6a31925ceb9dccb8465be529105e54eed8ad
Stepan Khapugin <stkhapugin@chromium.org>
2025-03-18 11:57:49 -0700

    [iOS] Use ChromeCoordinator's `profile` property.
    Simplify code by using `self.profile` where appropriate.
    Some uses with a local `profile = self.profile` are not
    updated to optimize for better readability.
    
    Created with git cl split.
    
    This CL was uploaded by git cl split.
    
    R=vincb@google.com
    
    Bug: 401523912
    Change-Id: I016dfb662d870af5e18bcd00674d9a765260118c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6366781
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434316}

--- ios/chrome/browser/passwords/ui_bundled/password_suggestion_coordinator.mm
+++ ios/chrome/browser/passwords/ui_bundled/password_suggestion_coordinator.mm
@@ -174,9 +174,8 @@ constexpr CGFloat preferredCornerRadius = 20;
 
 // Returns the user email.
 - (NSString*)userEmail {
-  ProfileIOS* profile = self.browser->GetProfile();
   AuthenticationService* authService =
-      AuthenticationServiceFactory::GetForProfile(profile);
+      AuthenticationServiceFactory::GetForProfile(self.profile);
   id<SystemIdentity> authenticatedIdentity =
       authService->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
 
@@ -249,11 +248,10 @@ constexpr CGFloat preferredCornerRadius = 20;
               kIOSProactivePasswordGenerationBottomSheet)) {
     return;
   }
-  ProfileIOS* profile = self.browser->GetProfile();
-  if (!profile) {
+  if (!self.profile) {
     return;
   }
-  PrefService* prefService = profile->GetPrefs();
+  PrefService* prefService = self.profile->GetPrefs();
   if (prefService) {
     const int newDismissCount =
         prefService->GetInteger(
