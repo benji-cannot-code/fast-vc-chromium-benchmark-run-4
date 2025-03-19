b918694bc9df7b67f27f9045a7a0d481ee2ced50
Anders Hartvoll Ruud <andruud@chromium.org>
2025-03-19 07:24:48 -0700

    Map WebFeature::kCSSFunctions to WebDXFeature::kDRAFT_CssFunction
    "CssFunction" was chosen as the "likely name" after discussing
    with foolip@.
    
    Also, the counter was missing a test, so this CL adds one.
    
    Fixed: 325504770
    Change-Id: I1f53c7912e38acc0f057a373dcf94acf17d3af13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372400
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434781}

---
+++ third_party/blink/renderer/core/css/style_use_counter_test.cc
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/testing/dummy_page_holder.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
+
+namespace blink {
+
+namespace {
+
+bool IsCountedOnParsing(WebFeature feature, String css) {
+  auto holder = std::make_unique<DummyPageHolder>(gfx::Size(800, 600));
+  Document& document = holder->GetDocument();
+  document.documentElement()->setInnerHTML("<style id=style></style>");
+  Element* style = document.getElementById(AtomicString("style"));
+  CHECK(style);
+  style->setInnerHTML(css);
+  document.View()->UpdateAllLifecyclePhasesForTest();
+  return document.IsUseCounted(feature);
+}
+
+}  // namespace
+
+class StyleUseCounterTest : public testing::Test {
+ private:
+  test::TaskEnvironment task_environment_;
+};
+
+TEST_F(StyleUseCounterTest, CSSFunctions) {
+  WebFeature feature = WebFeature::kCSSFunctions;
+  EXPECT_FALSE(IsCountedOnParsing(feature, "div {}"));
+  EXPECT_FALSE(IsCountedOnParsing(feature, "@invalid {}"));
+  EXPECT_FALSE(IsCountedOnParsing(feature, "@layer {}"));
+  EXPECT_TRUE(IsCountedOnParsing(feature, "@function --f() {}"));
+}
+
+}  // namespace blink
