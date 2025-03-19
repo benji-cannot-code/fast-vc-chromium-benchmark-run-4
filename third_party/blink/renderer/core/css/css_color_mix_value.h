2374bbdedc7f40e8fea6b00e07ae6c2fe95d6dfa
Fredrik Söderquist <fs@opera.com>
2025-03-19 03:47:57 -0700

    Fix some cases of percentage serialization for color-mix() functions
    In some cases, the second percentage parameter could get lost, and some
    cases the correct result wasn't produced when the second percentage
    parameter involved calc().
    
    Factor out a function that implements the algorithm from [1] - hopefully
    in a more readable way. Expose it so that it can be reused for
    palette-mix() which follows the same serialization algorithm.
    
    [1] https://drafts.csswg.org/css-color-5/#serial-color-mix
    
    Bug: 404464596
    Change-Id: I7f628baa3423be920f9be0b416948392331358d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368441
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434690}

--- third_party/blink/renderer/core/css/css_color_mix_value.h
+++ third_party/blink/renderer/core/css/css_color_mix_value.h
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_COLOR_MIX_VALUE_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/css/css_primitive_value.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
 #include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
+
+class CSSLengthResolver;
+class CSSPrimitiveValue;
+
 namespace cssvalue {
 
 // This is a class for storing the result of parsing the color-mix function
@@ -34,7 +37,7 @@ class CORE_EXPORT CSSColorMixValue : public CSSValue {
         color_interpolation_space_(color_interpolation_space),
         hue_interpolation_method_(hue_interpolation_method) {}
 
-  String CustomCSSText() const;
+  WTF::String CustomCSSText() const;
 
   void TraceAfterDispatch(blink::Visitor* visitor) const;
 
@@ -69,6 +72,11 @@ class CORE_EXPORT CSSColorMixValue : public CSSValue {
     return NormalizePercentages(Percentage1(), Percentage2(), mix_amount,
                                 alpha_multiplier, length_resolver);
   }
+
+  // https://drafts.csswg.org/css-color-5/#serial-color-mix
+  static std::pair<const CSSPrimitiveValue*, const CSSPrimitiveValue*>
+  PercentageValuesForSerialization(const CSSPrimitiveValue* p1,
+                                   const CSSPrimitiveValue* p2);
 
  private:
   Member<const CSSValue> color1_;
