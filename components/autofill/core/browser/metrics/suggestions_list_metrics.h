cd446001ec48caf55dcb65c61ded6cf2965ef6c2
Bruno Braga <brunobraga@google.com>
2025-03-19 06:31:00 -0700

    [AutofillOnTyping] Log of characters typed before acceptance
    This metric was requested in the PRD and depending on the outcome will
    lead to suggestions being displayed for shorter period.
    
    Right now, if the user types 10 characters and does not accept the
    suggestions, we remove the popup. However if most users accept it
    after 5 characters (for example), we can update the limit accordingly.
    
    Bug: 381994105
    Change-Id: I8859f5184667f2cf1a1d17b1898a2993513cf6c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368504
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434751}

--- components/autofill/core/browser/metrics/suggestions_list_metrics.h
+++ components/autofill/core/browser/metrics/suggestions_list_metrics.h
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 
 #include "base/containers/flat_map.h"
+#include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/suggestions/suggestion.h"
 
 namespace autofill {
+class AutofillField;
 enum class FillingProduct;
 
 namespace autofill_metrics {
@@ -84,6 +86,11 @@ void LogAutofillShowCardsFromGoogleAccountButtonEventMetric(
 // ranking algorithm compared to the legacy ranking algorithm.
 void LogAutofillRankingSuggestionDifference(
     SuggestionRankingContext::RelativePosition ranking_difference);
+
+// Logs metrics related to an autofill on typing suggestion being accepted.
+void LogAddressAutofillOnTypingSuggestionAccepted(
+    FieldType field_type_used,
+    const AutofillField* autofill_trigger_field);
 
 }  // namespace autofill_metrics
 }  // namespace autofill
