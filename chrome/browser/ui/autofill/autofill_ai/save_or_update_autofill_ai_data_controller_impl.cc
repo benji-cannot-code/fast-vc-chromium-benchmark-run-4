6912255aa82fb4bc4a5273696f7482334b9d98b5
Bruno Braga <brunobraga@google.com>
2025-03-19 03:39:35 -0700

    [AutofillAi] Add save prompt header image placeholder
    1. Deletes the older icon and its implementation.
    2. Brings a placeholder image that is hard coded to a random address
       dialog one. Once we have the proper assets updating will be trivial.
    
    screenshot: https://screenshot.googleplex.com/98czMqvisTkps4B
    
    Bug: 389629676
    Change-Id: I831ea7d7c202e0f9a3a6807a633f88def30dfa9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354788
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434686}

--- chrome/browser/ui/autofill/autofill_ai/save_or_update_autofill_ai_data_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_ai/save_or_update_autofill_ai_data_controller_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/grit/theme_resources.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_type.h"
 #include "components/autofill/core/browser/integrators/autofill_ai_delegate.h"
@@ -223,6 +224,20 @@ void SaveOrUpdateAutofillAiDataControllerImpl::DoShowBubble() {
 base::WeakPtr<SaveOrUpdateAutofillAiDataController>
 SaveOrUpdateAutofillAiDataControllerImpl::GetWeakPtr() {
   return weak_ptr_factory_.GetWeakPtr();
+}
+
+// TODO(389629676): Return proper resources once we have them.
+std::pair<int, int>
+SaveOrUpdateAutofillAiDataControllerImpl::GetTitleImagesResourceId() const {
+  switch (new_entity_->type().name()) {
+    case autofill::EntityTypeName::kVehicle:
+      return {IDR_SAVE_ADDRESS, IDR_SAVE_ADDRESS_DARK};
+    case autofill::EntityTypeName::kPassport:
+      return {IDR_SAVE_ADDRESS, IDR_SAVE_ADDRESS_DARK};
+    case autofill::EntityTypeName::kDriversLicense:
+      return {IDR_SAVE_ADDRESS, IDR_SAVE_ADDRESS_DARK};
+  }
+  NOTREACHED();
 }
 
 base::optional_ref<const autofill::EntityInstance>
--- chrome/browser/ui/autofill/autofill_ai/save_or_update_autofill_ai_data_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_ai/save_or_update_autofill_ai_data_controller_impl.h
@@ -57,6 +57,7 @@ class SaveOrUpdateAutofillAiDataControllerImpl
   std::vector<EntityAttributeUpdateDetails> GetUpdatedAttributesDetails()
       const override;
   bool IsSavePrompt() const override;
+  std::pair<int, int> GetTitleImagesResourceId() const override;
 
  protected:
   explicit SaveOrUpdateAutofillAiDataControllerImpl(
