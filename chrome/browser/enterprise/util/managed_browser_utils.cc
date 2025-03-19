13484730ac625bc97a5e19f5f6201acb8ab9c250
Nancy Xiao <nancylanxiao@google.com>
2025-03-19 07:21:06 -0700

    Create url filtering event reporting flag on Android
    Bug: 344591500
    Change-Id: I96c2fc1ff15f39d37066b97e5cf5e5f04d919454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368298
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434780}

--- chrome/browser/enterprise/util/managed_browser_utils.cc
+++ chrome/browser/enterprise/util/managed_browser_utils.cc
@@ -363,7 +363,10 @@ jboolean JNI_ManagedBrowserUtils_IsOnSecurityEventEnterpriseConnectorEnabled(
   DCHECK(profile);
 
   if (!base::FeatureList::IsEnabled(
-          enterprise_connectors::kEnterpriseSecurityEventReportingOnAndroid)) {
+          enterprise_connectors::kEnterpriseSecurityEventReportingOnAndroid) &&
+      !base::FeatureList::IsEnabled(
+          enterprise_connectors::
+              kEnterpriseUrlFilteringEventReportingOnAndroid)) {
     return false;
   }
 
