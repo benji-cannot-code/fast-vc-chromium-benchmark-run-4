2737cfa7b24ea091ed76ef222429b632690dbaf3
Timofey Chudakov <tchudakov@google.com>
2025-03-19 03:00:52 -0700

    [Android] Move DropdownKeyValue class out of EditorProperties.
    This is done to write Jni converters for this class in the
    next CL.
    
    Bug: 402020076
    Change-Id: Ica58604a4899215c0f667cbcc862555d0c5efa55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6357060
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434672}

---
+++ components/autofill/android/java/src/org/chromium/components/autofill/DropdownKeyValue.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.autofill;
+
+import android.util.Pair;
+
+/** A convenience class for displaying keyed values in a dropdown. */
+public class DropdownKeyValue extends Pair<String, String> {
+    public DropdownKeyValue(String key, String value) {
+        super(key, value);
+    }
+
+    /**
+     * @return The key identifier.
+     */
+    public String getKey() {
+        return super.first;
+    }
+
+    /**
+     * @return The human-readable localized display value.
+     */
+    public String getValue() {
+        return super.second;
+    }
+
+    @Override
+    public String toString() {
+        return super.second.toString();
+    }
+}
