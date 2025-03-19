3b3c15cf735f6d3a7cf1006975458b6808180f37
Nathan Memmott <memmott@chromium.org>
2025-03-19 15:06:45 -0700

    Translator: Implement progress updates
    This fully implements reporting the actual progress of both the
    TranslateKit and the Language packs to the translator's monitor.
    
    Fixed: 391715395
    Change-Id: I6d5f6292781159cc9b22d5f11b8a4bc267cb8145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6350513
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435116}

--- chrome/browser/on_device_translation/translation_manager_impl.h
+++ chrome/browser/on_device_translation/translation_manager_impl.h
@@ -12,11 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/supports_user_data.h"
 #include "base/types/pass_key.h"
+#include "chrome/browser/ai/ai_model_download_progress_manager.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_frame_host.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/unique_receiver_set.h"
+#include "third_party/blink/public/mojom/ai/model_download_progress_observer.mojom-forward.h"
 #include "third_party/blink/public/mojom/on_device_translation/translation_manager.mojom.h"
 #include "third_party/blink/public/mojom/on_device_translation/translator.mojom.h"
 #include "url/origin.h"
@@ -63,6 +65,8 @@ class TranslationManagerImpl : public base::SupportsUserData::Data,
 
   // Overridden for testing.
   virtual base::TimeDelta GetTranslatorDownloadDelay();
+  virtual component_updater::ComponentUpdateService*
+  GetComponentUpdateService();
 
   void CreateTranslatorImpl(
       mojo::PendingRemote<
@@ -96,9 +100,12 @@ class TranslationManagerImpl : public base::SupportsUserData::Data,
 
   const base::WeakPtr<content::BrowserContext> browser_context_;
   const url::Origin origin_;
+
   scoped_refptr<OnDeviceTranslationServiceController> service_controller_;
   mojo::UniqueReceiverSet<blink::mojom::Translator> translators_;
   mojo::ReceiverSet<blink::mojom::TranslationManager> receiver_set_;
+  on_device_ai::AIModelDownloadProgressManager model_download_progress_manager_;
+
   base::WeakPtrFactory<TranslationManagerImpl> weak_ptr_factory_{this};
 };
 
