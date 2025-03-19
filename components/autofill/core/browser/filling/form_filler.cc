f2b797e7818477f8f060a26445f4c084049cdb39
Jihad Hanna <jihadghanna@google.com>
2025-03-19 09:50:22 -0700

    Do not update the cache on Undo preview
    Fixed: 404793138
    Change-Id: Ib7b6372a59d27fc498ed80f0e1b70a9e65b34af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372624
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434889}

--- components/autofill/core/browser/filling/form_filler.cc
+++ components/autofill/core/browser/filling/form_filler.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/dense_set.h"
 #include "components/autofill/core/common/logging/log_buffer.h"
 #include "components/autofill/core/common/logging/log_macros.h"
+#include "components/autofill/core/common/mojom/autofill_types.mojom-shared.h"
 #include "components/autofill/core/common/unique_ids.h"
 
 namespace autofill {
@@ -473,13 +474,15 @@ FillingProduct FormFiller::UndoAutofill(
     field.set_value(previous_state.value);
     field.set_is_autofilled(previous_state.is_autofilled);
 
-    // Update the cached AutofillField in the browser.
-    // TODO(crbug.com/40232021): Consider updating the value too.
-    autofill_field.set_is_autofilled(previous_state.is_autofilled);
-    autofill_field.set_autofill_source_profile_guid(
-        previous_state.autofill_source_profile_guid);
-    autofill_field.set_autofilled_type(previous_state.autofilled_type);
-    autofill_field.set_filling_product(previous_state.filling_product);
+    // Update the cached AutofillField in the browser if the operation isn't a
+    // preview.
+    if (action_persistence == mojom::ActionPersistence::kFill) {
+      autofill_field.set_is_autofilled(previous_state.is_autofilled);
+      autofill_field.set_autofill_source_profile_guid(
+          previous_state.autofill_source_profile_guid);
+      autofill_field.set_autofilled_type(previous_state.autofilled_type);
+      autofill_field.set_filling_product(previous_state.filling_product);
+    }
   }
   form.set_fields(std::move(fields));
 
