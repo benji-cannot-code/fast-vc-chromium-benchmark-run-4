d0f42bdd43e4504608aa3ee305848e73fcca5e90
Olivier ROBIN <olivierrobin@google.com>
2025-03-19 04:48:33 -0700

    Add a remove section break flag for minimap
    This flag will remove the section break when detecting and presenting
    addresses
    
    Bug: 400942152
    Change-Id: If3e4a7219fca6141b27ce3754fad82fb16e0f282
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6371631
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434709}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -349,6 +349,9 @@ extern const char kIOSKeyboardAccessoryUpgradeForIPadDescription[];
 extern const char kIOSKeyboardAccessoryUpgradeShortManualFillMenuName[];
 extern const char kIOSKeyboardAccessoryUpgradeShortManualFillMenuDescription[];
 
+extern const char kIOSOneTapMiniMapRemoveSectionBreaksName[];
+extern const char kIOSOneTapMiniMapRemoveSectionBreaksDescription[];
+
 extern const char kIOSOneTapMiniMapRestrictionsName[];
 extern const char kIOSOneTapMiniMapRestrictionsDescription[];
 
