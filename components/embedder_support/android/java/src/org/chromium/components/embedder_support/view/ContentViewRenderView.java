7da5beae93a1cea25c915bf051d221c8b58a482c
Henrique Nakashima <hnakashima@chromium.org>
2025-03-19 06:40:26 -0700

    Android: Add @NullMarked to com/embedder_support/android:view_java
    Bug: 389129271
    Change-Id: Ifebc35cfe1d4e4b1ce3ccaadd0f6bc2fb0bf7050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361758
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434759}

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/view/ContentViewRenderView.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/view/ContentViewRenderView.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.embedder_support.view;
 
+import static org.chromium.build.NullUtil.assumeNonNull;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
@@ -22,6 +24,9 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.build.annotations.Initializer;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.components.input.InputUtils;
 import org.chromium.content_public.browser.InputTransferHandler;
 import org.chromium.content_public.browser.SurfaceInputTransferHandlerMap;
@@ -35,18 +40,19 @@ import org.chromium.ui.base.WindowAndroid;
  * Note that only one WebContents can be shown at a time.
  */
 @JNINamespace("embedder_support")
+@NullMarked
 public class ContentViewRenderView extends FrameLayout {
     // The native side of this object.
     private long mNativeContentViewRenderView;
-    private WindowAndroid mWindowAndroid;
+    private @Nullable WindowAndroid mWindowAndroid;
 
     protected SurfaceBridge mSurfaceBridge;
-    protected WebContents mWebContents;
+    protected @Nullable WebContents mWebContents;
 
     private int mWidth;
     private int mHeight;
 
-    private Integer mSurfaceId;
+    private @Nullable Integer mSurfaceId;
 
     /**
      * Constructs a new ContentViewRenderView. This should be called and the {@link
@@ -85,12 +91,14 @@ public class ContentViewRenderView extends FrameLayout {
                     public void surfaceChanged(
                             SurfaceHolder holder, int format, int width, int height) {
                         assert mNativeContentViewRenderView != 0;
+                        assumeNonNull(mWindowAndroid);
 
                         InputTransferToken browserInputToken = null;
                         Window window = mWindowAndroid.getWindow();
                         if (InputUtils.isTransferInputToVizSupported() && window != null) {
                             AttachedSurfaceControl rootSurfaceControl =
                                     window.getRootSurfaceControl();
+                            assumeNonNull(rootSurfaceControl);
                             browserInputToken = rootSurfaceControl.getInputTransferToken();
                         }
                         Integer surfaceId =
@@ -275,12 +283,13 @@ public class ContentViewRenderView extends FrameLayout {
 
     protected static class SurfaceBridge {
         private SurfaceView mSurfaceView;
-        private SurfaceHolder.Callback mSurfaceCallback;
+        private SurfaceHolder.@Nullable Callback mSurfaceCallback;
 
         protected SurfaceView getSurfaceView() {
             return mSurfaceView;
         }
 
+        @Initializer
         protected void initialize(ContentViewRenderView renderView) {
             mSurfaceView = renderView.createSurfaceView(renderView.getContext());
             mSurfaceView.setZOrderMediaOverlay(true);
@@ -335,7 +344,7 @@ public class ContentViewRenderView extends FrameLayout {
                 int width,
                 int height,
                 Surface surface,
-                InputTransferToken browserInputToken);
+                @Nullable InputTransferToken browserInputToken);
 
         void setOverlayVideoMode(
                 long nativeContentViewRenderView, ContentViewRenderView caller, boolean enabled);
