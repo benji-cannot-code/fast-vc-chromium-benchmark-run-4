9eedd34e26246edae7e054af2aec96c92e7e4add
Zhe Li <zheliooo@google.com>
2025-03-19 10:22:36 -0700

    Revert "[metrics] Add PSS metrics on Linux/Android/ChromeOS"
    This reverts commit a7165d8e50c1940af508e7d956a74443d27d5f26.
    
    Reason for revert: This caused consistent failure in CQ see bug: crbug.com/404852444
    
    Original change's description:
    > [metrics] Add PSS metrics on Linux/Android/ChromeOS
    >
    > This CL adds "Memory.<process>.{SwapPss,Pss}" metrics.
    >
    > Bug: 403588202
    > Change-Id: Iaff82a1e594891eab1d4f891b5488e9d3211921d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6347824
    > Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1434569}
    
    Bug: 403588202
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ic7be120d57d09bfddc554d664362bd6ada925ce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372101
    Auto-Submit: Zhe Li <zheliooo@google.com>
    Owners-Override: Zhe Li <zheliooo@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434913}

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_linux.cc
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_linux.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/library_loader/anchor_functions_buildflags.h"
 #include "base/containers/heap_array.h"
 #include "base/debug/elf_reader.h"
-#include "base/debug/proc_maps_linux.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
@@ -307,18 +306,6 @@ uint32_t CountMappings(base::ProcessId pid) {
   return newline_characters;
 }
 
-// Get values from smaps_rollup for the current process.
-void GetSmapsRollup(uint32_t* pss, uint32_t* swap_pss) {
-  auto value = base::debug::ReadAndParseSmapsRollup();
-  if (!value) {
-    *pss = 0;
-    *swap_pss = 0;
-    return;
-  }
-  *pss = value->pss;
-  *swap_pss = value->swap_pss;
-}
-
 }  // namespace
 
 FILE* g_proc_smaps_for_testing = nullptr;
@@ -342,8 +329,8 @@ bool OSMetrics::FillOSMemoryDump(base::ProcessHandle handle,
       base::saturated_cast<uint32_t>(info->resident_set_bytes / 1024);
   dump->peak_resident_set_kb = GetPeakResidentSetSize(handle);
   dump->is_peak_rss_resettable = ResetPeakRSSIfPossible(handle);
+
   dump->mappings_count = CountMappings(handle);
-  GetSmapsRollup(&dump->pss_kb, &dump->swap_pss_kb);
 
 #if BUILDFLAG(IS_ANDROID)
 #if BUILDFLAG(SUPPORTS_CODE_ORDERING)
