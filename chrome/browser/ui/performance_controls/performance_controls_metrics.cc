4e3d4b2451fddc9d37ae9e3a332417ccfb0201d0
Steven Luong <stluong@chromium.org>
2025-03-19 10:12:05 -0700

    Record performance detection notification acceptance percentage
    Add metrics to record the performance detection notification acceptance
    percentage on a daily basis.
    
    Fixed: 400774977
    Change-Id: I6ade2175dcf57be27ebf26dcbe40f4191a6a8a20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368734
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434906}

--- chrome/browser/ui/performance_controls/performance_controls_metrics.cc
+++ chrome/browser/ui/performance_controls/performance_controls_metrics.cc
@@ -8,14 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/strcat.h"
 #include "base/time/time.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/performance_manager/public/user_tuning/performance_detection_manager.h"
+#include "chrome/browser/ui/performance_controls/performance_intervention_button_controller.h"
 #include "chrome/common/pref_names.h"
 #include "components/metrics/daily_event.h"
+#include "components/performance_manager/public/features.h"
+#include "components/performance_manager/public/user_tuning/prefs.h"
 #include "components/prefs/pref_service.h"
 
 namespace {
@@ -61,6 +66,19 @@ class DailyEventObserver : public metrics::DailyEvent::Observer {
                       ".RateLimitedCount"}),
         GetAndResetPref(
             prefs::kPerformanceInterventionBackgroundCpuRateLimitedCount));
+
+    PrefService* const pref_service = g_browser_process->local_state();
+    if (base::FeatureList::IsEnabled(
+            performance_manager::features::
+                kPerformanceInterventionNotificationImprovements) &&
+        !pref_service
+             ->GetList(performance_manager::user_tuning::prefs::
+                           kPerformanceInterventionNotificationAcceptHistory)
+             .empty()) {
+      base::UmaHistogramPercentage(
+          "PerformanceControls.Intervention.DailyAcceptancePercentage",
+          PerformanceInterventionButtonController::GetAcceptancePercentage());
+    }
   }
 
   int GetAndResetPref(std::string pref_name) {
