04d7253f30fd4c1ce4ba8547fb3e8ce14f64b0bd
Kent Tamura <tkent@chromium.org>
2025-03-19 06:43:10 -0700

    CanvasTextNG: Fix a DCHECK failure by a vertical writing-mode
    Canvas doesn't support vertical writing-modes, but writing-mode property
    affects font rendering by accident for now.  CanvasTextNG should
    not crash in such cases.
    
    * Add HasVerticalOffsets() flag to PlainTextNode, like
      ShapingResultBuffer has.
    
    Fixed: 404448251
    Change-Id: I99b949660719ec443b2dc2ccf37fbc29c3d7edf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367876
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434763}

--- third_party/blink/renderer/platform/fonts/plain_text_node.cc
+++ third_party/blink/renderer/platform/fonts/plain_text_node.cc
@@ -271,6 +271,7 @@ void PlainTextNode::Shape(const Font& font, FrameShapeCache* cache) {
       if (entry && entry->shape_result) {
         item.shape_result_ = entry->shape_result;
         item.ink_bounds_ = entry->ink_bounds;
+        has_vertical_offsets_ |= item.shape_result_->HasVerticalOffsets();
         continue;
       }
     }
@@ -306,6 +307,7 @@ void PlainTextNode::Shape(const Font& font, FrameShapeCache* cache) {
     }
     item.shape_result_ = shape_result;
     item.ink_bounds_ = ink_bounds;
+    has_vertical_offsets_ |= item.shape_result_->HasVerticalOffsets();
     if (cache) {
       cache->RegisterShapeEntry(item, entry);
     }
