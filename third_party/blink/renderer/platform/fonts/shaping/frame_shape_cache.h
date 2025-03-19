5c47ef70bae7faf3fd4eadd0acc10b10bde2aa64
Kent Tamura <tkent@chromium.org>
2025-03-19 01:24:02 -0700

    CanvasTextNG: Add unit tests for FrameShapeCache
    This CL has no behavior changes.
    
    Bug: 389726691
    Change-Id: Id09f1e58c9aea961ad0613ae6833d8534208adf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367796
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434654}

--- third_party/blink/renderer/platform/fonts/shaping/frame_shape_cache.h
+++ third_party/blink/renderer/platform/fonts/shaping/frame_shape_cache.h
@@ -27,7 +27,8 @@ class ShapeResult;
 //
 // The caches are aware of frames distinguished by DidSwitchFrame() calls. This
 // allows the caches to purge old entries when the frame is switched.
-class FrameShapeCache : public GarbageCollected<FrameShapeCache> {
+class PLATFORM_EXPORT FrameShapeCache
+    : public GarbageCollected<FrameShapeCache> {
  public:
   FrameShapeCache();
   void Trace(Visitor* visitor) const;
