48ad041d37f81b7680953b66c896a2fb033b9ecc
Federica Germinario <fedegermi@google.com>
2025-03-19 03:13:40 -0700

    [ios] Use Umbrella Header in price_card_item.h
    Style guide suggests to import umbrella headers rather than
    individual files.
    
    Bug: 404539984
    Change-Id: I6b378a69da1a3cf56528725d23768abc26e5a6d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368556
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434675}

--- ios/chrome/browser/commerce/ui_bundled/price_card/price_card_item.h
+++ ios/chrome/browser/commerce/ui_bundled/price_card/price_card_item.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_COMMERCE_UI_BUNDLED_PRICE_CARD_PRICE_CARD_ITEM_H_
 #define IOS_CHROME_BROWSER_COMMERCE_UI_BUNDLED_PRICE_CARD_PRICE_CARD_ITEM_H_
 
-#import <Foundation/NSString.h>
+#import <Foundation/Foundation.h>
 
 // Model object representing price information for a shopping website.
 @interface PriceCardItem : NSObject
