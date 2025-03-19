880a2bc541de2bc28e882936d0b80281ae2173e5
Elmehdi Rahmaoui <erahmaoui@google.com>
2025-03-19 08:35:32 -0700

    [iOS] Share extension - Incognito handling
    This CL adds the incognito actions for share extension, url opening,
    image searching and text searching.
    
    Bug: 402278503
    Change-Id: I64aed16c8a1141b90de5b1d2a859ab1df8047f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368260
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434828}

--- ios/chrome/share_extension/share_extension_delegate.h
+++ ios/chrome/share_extension/share_extension_delegate.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (ShareExtensionSheet*)shareExtensionSheet;
 - (void)didTapSearchInChromeShareExtensionSheet:
     (ShareExtensionSheet*)shareExtensionSheet;
+- (void)didTapSearchInIncognitoShareExtensionSheet:
+    (ShareExtensionSheet*)shareExtensionSheet;
 
 @end
 
