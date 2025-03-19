47a80d57c640ccfe8b87d271ed798bf728d4b08b
Vincent Chiang <vincentchiang@chromium.org>
2025-03-19 10:23:57 -0700

    Mark shelf as shutting down on root window destruction
    ShelfLayoutManager is only marked as shutting down after
    ShelfWidget::Shutdown(). This allows ShelfLayoutManager to make changes
    to a partially destroyed RootWindow, resulting in crashes. This change
    blocks all such changes after the root window begins to shutdown.
    
    Bug: 373925165
    Change-Id: Ib002cdd3c6b19d1db1300bb04bca795accf2efac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6362635
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434915}

--- ash/shelf/shelf_layout_manager.h
+++ ash/shelf/shelf_layout_manager.h
@@ -274,6 +274,7 @@ class ASH_EXPORT ShelfLayoutManager : public AppListControllerObserver,
   // ShellObserver:
   void OnUserWorkAreaInsetsChanged(aura::Window* root_window) override;
   void OnPinnedStateChanged(aura::Window* pinned_window) override;
+  void OnRootWindowWillShutdown(aura::Window* root_window) override;
   void OnShellDestroying() override;
 
   // SplitViewObserver:
