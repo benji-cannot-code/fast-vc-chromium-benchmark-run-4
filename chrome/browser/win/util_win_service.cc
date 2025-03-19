27ac7dbc691a21b2b109ab5d5f4b3f287cc87f85
Francois Pierre Doray <fdoray@chromium.org>
2025-03-19 15:16:56 -0700

    Cleanup features for COM initialization in utility processes.
    The features have been enabled by default for a while and are
    ready to be cleaned up.
    
    Fixed: 348014083
    Change-Id: I53c4b42541aadd525ffb547bc8cd023689f87402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6369016
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435124}

--- chrome/browser/win/util_win_service.cc
+++ chrome/browser/win/util_win_service.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/win/util_win_service.h"
 
-#include "chrome/common/chrome_features.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/services/util_win/public/mojom/util_win.mojom.h"
 #include "content/public/browser/service_process_host.h"
@@ -14,9 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 mojo::Remote<chrome::mojom::UtilWin> LaunchUtilWinServiceInstance() {
   content::ServiceProcessHost::Options options;
   options.WithDisplayName(IDS_UTILITY_PROCESS_UTILITY_WIN_NAME);
-  if (base::FeatureList::IsEnabled(features::kUtilWinProcessUsesUiPump)) {
-    options.WithExtraCommandLineSwitches({switches::kMessageLoopTypeUi});
-  }
+  options.WithExtraCommandLineSwitches({switches::kMessageLoopTypeUi});
 
   // Runs with kNoSandbox from sandbox.mojom.Sandbox.
   return content::ServiceProcessHost::Launch<chrome::mojom::UtilWin>(
--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -1580,11 +1580,6 @@ BASE_FEATURE(kUmaStorageDimensions,
 BASE_FEATURE(kWin10AcceleratedDefaultBrowserFlow,
              "Win10AcceleratedDefaultBrowserFlow",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-// When enabled, a UI pump is requested for the UtilWin utility process.
-BASE_FEATURE(kUtilWinProcessUsesUiPump,
-             "UtilWinProcessUsesUiPump",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_WIN)
 
 // Enables writing basic system profile to the persistent histograms files
