f5aa7db7dc51c38751b3b866af8f503ece69d2f1
Theo Cristea <theocristea@google.com>
2025-03-19 14:41:47 -0700

    Add string for empty option in Autofill <select>
    The option is not left blank so that screen readers can also pick it up.
    
    Demo:
    https://screenshot.googleplex.com/6LPJfdN7PnVd22b
    https://screenshot.googleplex.com/7g7F64FwMtgGc9h
    
    Bug: 393318914
    Change-Id: Ia30f7c971eacc118f73acea858b4a51568b13df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6363694
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435107}

--- chrome/test/data/webui/settings/autofill_ai_add_or_edit_dialog_test.ts
+++ chrome/test/data/webui/settings/autofill_ai_add_or_edit_dialog_test.ts
@@ -384,8 +384,10 @@ suite('AutofillAiAddOrEditDialogSelectElementUiTest', function() {
         assertTrue(!!countrySelect);
         if (params.add) {
           assertEquals('', countrySelect.value);
+          assertTrue(countrySelect.textContent!.includes('No option selected'));
         } else {
           assertEquals(oldCountryCode, countrySelect.value);
+          assertTrue(countrySelect.textContent!.includes('Germany'));
         }
 
         if (params.changeCountry) {
