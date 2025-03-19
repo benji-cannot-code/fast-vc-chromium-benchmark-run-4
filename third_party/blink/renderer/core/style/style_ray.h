32f7f610906eeeaae8db0ac7023b88c5ce9dfb66
Florin Malita <fmalita@chromium.org>
2025-03-19 06:46:58 -0700

    Return path by value from BasicShape::GetPath()
    Somewhat unrelated cleanup split off from
    https://chromium-review.googlesource.com/c/chromium/src/+/6330705
    
    Bug: 378688986
    Change-Id: Ia03aac5e8a54e67d2ccc9365be2175bb6316fae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6362399
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1434767}

--- third_party/blink/renderer/core/style/style_ray.h
+++ third_party/blink/renderer/core/style/style_ray.h
@@ -48,7 +48,7 @@ class StyleRay : public BasicShape {
   const BasicShapeCenterCoordinate& CenterX() const { return center_x_; }
   const BasicShapeCenterCoordinate& CenterY() const { return center_y_; }
 
-  void GetPath(Path&, const gfx::RectF&, float) const override;
+  Path GetPath(const gfx::RectF&, float) const override;
 
   ShapeType GetType() const override { return kStyleRayType; }
 
