c58ff78ca807a344b621bdc479801671a6f5c6fa
Justin Lulejian <jlulejian@chromium.org>
2025-03-19 11:20:23 -0700

    [Extensions] Create a `browser` namespace as an alternative to `chrome`
    Before this change extensions had to use the `chrome` namespace to
    access extension APIs.
    
    After this change we introduce a new `browser` namespace (behind a
    feature flag) that is available to call extension APIs wherever they are
    available. The exceptions to this is WebUI.
    
    The API objects pointed to by each namespace are the same so they can be
    used interchangeably if desired.
    
    The deprecated `app` API is never available on `browser` though.
    
    There is no change to the `chrome` namespace.
    
    Bug: 401226626
    Change-Id: I797ff4c56022981fe75ddb9f54cf70e5a5b5c454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6306847
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434960}

---
+++ chrome/test/data/extensions/api_test/browser_object/background_context/background.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {getBrowserNamespaceTestCases} from '/_test_resources/api_test/browser_object/browser_object_common_tests.js';
+
+chrome.test.runTests(getBrowserNamespaceTestCases());
---
+++ chrome/test/data/extensions/api_test/browser_object/foreground_context/content_script.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This works-around that content scripts can't import because they aren't
+// considered modules.
+(async () => {
+  const src = chrome.runtime.getURL(
+      '/_test_resources/api_test/browser_object/' +
+      'browser_object_common_tests.js');
+  const testUtil = await import(src);
+  chrome.test.runTests(testUtil.getBrowserNamespaceTestCases());
+})();
