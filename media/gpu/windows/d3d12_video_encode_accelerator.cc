6adf412f1bc94e1f1020be267e41de5d0f3159e5
Qiu Jianlin <jianlin.qiu@intel.com>
2025-03-19 16:10:46 -0700

    [Win][DX12 Enc] Support shared memory GMB for DX12 encoding
    Allow encoding from canvas that is backed by shared memory GMB. Follow
    up CL will place check on input canvas size before request for encoding.
    
    Bug: 40275246
    Change-Id: Ie5426c36f4c5adce34cf4b3aaddaabd1ef10a629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6369764
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435142}

--- media/gpu/windows/d3d12_video_encode_accelerator.cc
+++ media/gpu/windows/d3d12_video_encode_accelerator.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/task/thread_pool.h"
+#include "media/base/video_util.h"
 #include "media/gpu/macros.h"
 #include "media/gpu/windows/d3d12_video_encode_av1_delegate.h"
 #include "media/gpu/windows/d3d12_video_encode_delegate.h"
@@ -327,7 +328,11 @@ Microsoft::WRL::ComPtr<ID3D12Resource>
 D3D12VideoEncodeAccelerator::CreateResourceForSharedMemoryVideoFrame(
     const VideoFrame& frame) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_checker_);
-  DCHECK_EQ(frame.storage_type(), VideoFrame::STORAGE_SHMEM);
+  if (frame.storage_type() != VideoFrame::STORAGE_SHMEM &&
+      frame.storage_type() != VideoFrame::STORAGE_UNOWNED_MEMORY) {
+    LOG(ERROR) << "Unsupported frame storage type for mapping";
+    return nullptr;
+  }
   CHECK(frame.IsMappable());
 
   D3D12_RESOURCE_DESC input_texture_desc = CD3DX12_RESOURCE_DESC::Tex2D(
@@ -411,7 +416,17 @@ void D3D12VideoEncodeAccelerator::DoEncodeTask(
   DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_checker_);
   Microsoft::WRL::ComPtr<ID3D12Resource> input_texture;
   if (frame->storage_type() == VideoFrame::STORAGE_GPU_MEMORY_BUFFER) {
-    input_texture = CreateResourceForGpuMemoryBufferVideoFrame(*frame);
+    if (frame->HasNativeGpuMemoryBuffer()) {
+      input_texture = CreateResourceForGpuMemoryBufferVideoFrame(*frame);
+    } else {
+      frame = ConvertToMemoryMappedFrame(std::move(frame));
+      if (!frame) {
+        return NotifyError(
+            {EncoderStatus::Codes::kInvalidInputFrame,
+             "Failed to convert shared memory GMB for encoding"});
+      }
+      input_texture = CreateResourceForSharedMemoryVideoFrame(*frame);
+    }
   } else if (frame->storage_type() == VideoFrame::STORAGE_SHMEM) {
     input_texture = CreateResourceForSharedMemoryVideoFrame(*frame);
   } else {
