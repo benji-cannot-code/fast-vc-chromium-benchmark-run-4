b25bf943de6ac4006c19d0ab5069a5e8d5bcd87a
Mei Liang <meiliang@chromium.org>
2025-03-20 14:24:41 -0700

    [Gardener] Revert "Instantiate Android system fonts with Fontations"
    This reverts commit 1f4d0f8635d4fdf5672aa69d249971fbfc705039.
    
    Reason for revert: crbug.com/405186881
    
    Original change's description:
    > Instantiate Android system fonts with Fontations
    >
    > After Julia landed [1] (thank you!), it's now possible to create
    > Fontations Android system fonts.
    >
    > Create a base feature "FontationsAndroidSystemFonts" to set the Android
    > default font manager to one that instantiates through Fontations.
    >
    > In the first patchset/upload, enable the feature by default to do one
    > round of testing on bots.
    >
    > [1] https://chromium.googlesource.com/skia.git/+/eb9fc76e302f873de982cefa7690443e7a2cede5
    >
    > Bug: chromium:370816214
    > Change-Id: Ieee9828fd5569289e726a45211c489c25e99a2aa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6079955
    > Commit-Queue: Dominik Röttsches <drott@chromium.org>
    > Auto-Submit: Dominik Röttsches <drott@chromium.org>
    > Reviewed-by: Ben Wagner <bungeman@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1435391}
    
    Bug: chromium:370816214
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I3b3bced7a997874249d9edab7db12d892d90a18b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378864
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Owners-Override: Mei Liang <meiliang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1435691}

--- skia/ext/font_utils.cc
+++ skia/ext/font_utils.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "third_party/skia/include/ports/SkFontMgr_android.h"
-#include "third_party/skia/include/ports/SkFontScanner_Fontations.h"
 #endif
 
 #if BUILDFLAG(IS_APPLE)
@@ -61,11 +60,7 @@ static sk_sp<SkFontMgr> fontmgr_factory() {
     return sk_ref_sp(g_fontmgr_override);
   }
 #if BUILDFLAG(IS_ANDROID)
-  if (base::FeatureList::IsEnabled(skia::kFontationsAndroidSystemFonts)) {
-    return SkFontMgr_New_Android(nullptr, SkFontScanner_Make_Fontations());
-  } else {
-    return SkFontMgr_New_Android(nullptr);
-  }
+  return SkFontMgr_New_Android(nullptr);
 #elif BUILDFLAG(IS_APPLE)
   return SkFontMgr_New_CoreText(nullptr);
 #elif BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
--- skia/fontations_feature.h
+++ skia/fontations_feature.h
@@ -13,7 +13,6 @@ namespace skia {
 
 SK_API BASE_DECLARE_FEATURE(kFontationsLinuxSystemFonts);
 
-SK_API BASE_DECLARE_FEATURE(kFontationsAndroidSystemFonts);
 }
 
 #endif  // SKIA_FONTATIONS_FEATURE_H_
