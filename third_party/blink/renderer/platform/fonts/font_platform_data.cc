c04bad791026de4fff513ca09df822462f7b5ebf
Traian Captan <tcaptan@chromium.org>
2025-03-19 22:04:59 -0700

    Remove DisableAhemAntialias
    Now that font antialiasing is disabled on all test platforms [1],
    DisableAhemAntialias is no longer needed and can be removed.
    
    [1]
    http://crrev.com/c/chromium/src/+/6355383
    
    Change-Id: I46cbfb11e0cf8ec09b94d5e95fc9ee6a59d36d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6370105
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435247}

--- third_party/blink/renderer/platform/fonts/font_platform_data.cc
+++ third_party/blink/renderer/platform/fonts/font_platform_data.cc
@@ -179,11 +179,6 @@ String FontPlatformData::FontFamilyName() const {
   return String::FromUTF8(base::as_byte_span(localized_string.fString));
 }
 
-bool FontPlatformData::IsAhem() const {
-  return EqualIgnoringASCIICase(FontFamilyName(), "ahem") ||
-         EqualIgnoringASCIICase(FontFamilyName(), "ahem (fontations)");
-}
-
 SkTypeface* FontPlatformData::Typeface() const {
   return typeface_.get();
 }
@@ -280,9 +275,8 @@ SkFont FontPlatformData::CreateSkFont(const FontDescription*) const {
 
   font.setEmbeddedBitmaps(!avoid_embedded_bitmaps_);
 
-  if ((RuntimeEnabledFeatures::NoFontAntialiasingEnabled() &&
-       !WebTestSupport::IsFontAntialiasingEnabledForTest()) ||
-      (RuntimeEnabledFeatures::DisableAhemAntialiasEnabled() && IsAhem())) {
+  if (RuntimeEnabledFeatures::NoFontAntialiasingEnabled() &&
+      !WebTestSupport::IsFontAntialiasingEnabledForTest()) {
     font.setEdging(SkFont::Edging::kAlias);
   }
 
