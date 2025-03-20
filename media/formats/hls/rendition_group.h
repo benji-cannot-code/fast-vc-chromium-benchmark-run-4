7bac24001885c76c822917133ff5fd9e8968055a
Ted Meyer <tmathmeyer@chromium.org>
2025-03-19 19:42:16 -0700

    Assign MediaTracks to AudioRenditions at creation
    Rather than let RenditionManager do double-duty creating the
    MediaTracks, we can just create them at the beginning.
    
    These tracks will be imported into RenditionManager in a followup CL.
    
    Bug: 381024736
    Bug: 371024058
    Change-Id: I94489a22c0220da9c3fc3989172dfb5d2bb0acc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6364014
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Auto-Submit: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435222}

--- media/formats/hls/rendition_group.h
+++ media/formats/hls/rendition_group.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/types/pass_key.h"
 #include "media/base/media_export.h"
+#include "media/base/media_track.h"
 #include "media/formats/hls/parse_status.h"
 #include "media/formats/hls/tags.h"
 #include "media/formats/hls/types.h"
@@ -36,6 +37,8 @@ class MEDIA_EXPORT RenditionGroup : public base::RefCounted<RenditionGroup> {
   RenditionGroup& operator=(const RenditionGroup&) = delete;
   RenditionGroup& operator=(RenditionGroup&&) = delete;
 
+  using RenditionTrack = std::tuple<MediaTrack, raw_ptr<const Rendition>>;
+
   // Adds a rendition specified by the given `XMediaTag` to this group. The
   // caller is responsible for ensuring that the rendition passed in is
   // individually valid, has a type matching Rendition::Type, and belongs to
@@ -44,7 +47,15 @@ class MEDIA_EXPORT RenditionGroup : public base::RefCounted<RenditionGroup> {
   ParseStatus::Or<std::monostate> AddRendition(
       base::PassKey<MultivariantPlaylist>,
       XMediaTag tag,
-      const GURL& playlist_uri);
+      const GURL& playlist_uri,
+      uint64_t rendition_unique_id);
+
+  // Returns the set of renditions that belong to this group with the media
+  // track object that can be exposed to the web.
+  const base::flat_map<MediaTrack::Id, RenditionTrack>& GetRenditionMapping()
+      const {
+    return renditions_map_;
+  }
 
   // Returns the id of this rendition group.
   const std::string& GetId() const { return id_; }
@@ -68,9 +79,7 @@ class MEDIA_EXPORT RenditionGroup : public base::RefCounted<RenditionGroup> {
   // pointer stability.
   std::list<Rendition> renditions_;
 
-  // Set of renditions within this group, keyed by their NAME attribute.
-  base::flat_map<std::string, raw_ptr<const Rendition, CtnExperimental>>
-      renditions_map_;
+  base::flat_map<MediaTrack::Id, RenditionTrack> renditions_map_;
 
   // Default rendition, `nullptr` if none.
   raw_ptr<const Rendition> default_rendition_ = nullptr;
