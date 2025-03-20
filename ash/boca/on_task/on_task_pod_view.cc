a3525d1e75d568fdfa4a9851f9a679fd4bd11dc0
caott <caott@google.com>
2025-03-20 11:49:42 -0700

    [ChromeOS] Add separate copies of top_left and top_right icons
    This is to address the suggestion in
    https://chromium-review.googlesource.com/c/chromium/src/+/6363243
    
    Bug: b:400842133
    Change-Id: Ic73dd9cb5e315c2d38db02ab916e167b1025d979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372795
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Luke Cao <caott@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435580}

--- ash/boca/on_task/on_task_pod_view.cc
+++ ash/boca/on_task/on_task_pod_view.cc
@@ -70,7 +70,7 @@ class PinTabStripButton : public views::LabelButton {
       : views::LabelButton(std::move(callback), text) {
     SetImageModel(views::Button::STATE_NORMAL,
                   ui::ImageModel::FromVectorIcon(
-                      kTabsIcon, cros_tokens::kCrosSysOnSurface));
+                      kOnTaskPodTabsIcon, cros_tokens::kCrosSysOnSurface));
     // Accessing protected member label() to set the font list.
     label()->SetFontList(TypographyProvider::Get()->ResolveTypographyToken(
         TypographyToken::kCrosButton2));
@@ -123,13 +123,13 @@ void OnTaskPodView::AddShortcutButtons() {
       base::BindRepeating(&OnTaskPodController::SetSnapLocation,
                           base::Unretained(pod_controller_),
                           OnTaskPodSnapLocation::kTopLeft),
-      &kAutoclickPositionTopLeftIcon,
+      &kOnTaskPodPositionTopLeftIcon,
       l10n_util::GetStringUTF16(IDS_ON_TASK_MOVE_POD_TOP_LEFT_ACCESSIBLE_NAME));
   dock_right_button_ = pod_position_slider_->AddButton<IconSliderButton>(
       base::BindRepeating(&OnTaskPodController::SetSnapLocation,
                           base::Unretained(pod_controller_),
                           OnTaskPodSnapLocation::kTopRight),
-      &kAutoclickPositionTopRightIcon,
+      &kOnTaskPodPositionTopRightIcon,
       l10n_util::GetStringUTF16(
           IDS_ON_TASK_MOVE_POD_TOP_RIGHT_ACCESSIBLE_NAME));
   dock_left_button_->SetSelected(true);
@@ -186,7 +186,7 @@ void OnTaskPodView::UpdatePinTabStripButton() {
     pin_tab_strip_button_->SetImageModel(
         views::Button::STATE_NORMAL,
         ui::ImageModel::FromVectorIcon(
-            kTabsIcon, cros_tokens::kCrosSysSystemOnPrimaryContainer));
+            kOnTaskPodTabsIcon, cros_tokens::kCrosSysSystemOnPrimaryContainer));
   } else {
     // Otherwise, button is "Show tabs" when the tab strip is already hidden.
     pin_tab_strip_button_->SetText(l10n_util::GetStringUTF16(
@@ -196,7 +196,7 @@ void OnTaskPodView::UpdatePinTabStripButton() {
     pin_tab_strip_button_->SetEnabledTextColors(cros_tokens::kCrosSysOnSurface);
     pin_tab_strip_button_->SetImageModel(
         views::Button::STATE_NORMAL,
-        ui::ImageModel::FromVectorIcon(kTabsIcon,
+        ui::ImageModel::FromVectorIcon(kOnTaskPodTabsIcon,
                                        cros_tokens::kCrosSysOnSurface));
   }
 }
