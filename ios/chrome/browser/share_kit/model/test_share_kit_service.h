1016de21ef3c85ad417fb4cb0cac043c5a0af5f2
Gauthier Ambard <gambard@chromium.org>
2025-03-20 03:46:32 -0700

    [iOS][SK] Remove deprecated ReadGroup API
    Now that it is used downstream, it can be cleaned up.
    
    Bug: 400700174
    Change-Id: I3b9435811a861077369191fce5614acf5b7bdcfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368663
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435318}

--- ios/chrome/browser/share_kit/model/test_share_kit_service.h
+++ ios/chrome/browser/share_kit/model/test_share_kit_service.h
@@ -43,7 +43,6 @@ class TestShareKitService : public ShareKitService {
   NSString* ManageTabGroup(ShareKitManageConfiguration* config) override;
   NSString* JoinTabGroup(ShareKitJoinConfiguration* config) override;
   UIViewController* FacePile(ShareKitFacePileConfiguration* config) override;
-  void ReadGroups(ShareKitReadConfiguration* config) override;
   void ReadGroups(ShareKitReadGroupsConfiguration* config) override;
   void ReadGroupWithToken(
       ShareKitReadGroupWithTokenConfiguration* config) override;
