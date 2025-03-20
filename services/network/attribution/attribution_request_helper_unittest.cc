c52baa89029e7af821f023c1b4b1c62acef6998e
Caleb Raitto <caraitto@chromium.org>
2025-03-20 13:35:16 -0700

    Rename clickiness request header
    Rename Ad-Auction-Registration-Eligible to
    Sec-Ad-Auction-Event-Recording-Eligible
    
    Bug: 394108643
    Change-Id: Ic52639fd845379328705f80b2a3cbeb395ac55c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6377205
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Caleb Raitto <caraitto@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435652}

--- services/network/attribution/attribution_request_helper_unittest.cc
+++ services/network/attribution/attribution_request_helper_unittest.cc
@@ -40,7 +40,7 @@ constexpr char kAttributionReportingEligible[] =
     "Attribution-Reporting-Eligible";
 
 constexpr char kAdAuctionRegistrationEligible[] =
-    "Ad-Auction-Registration-Eligible";
+    "Sec-Ad-Auction-Event-Recording-Eligible";
 
 class AttributionRequestHelperTest : public testing::Test {
  protected:
