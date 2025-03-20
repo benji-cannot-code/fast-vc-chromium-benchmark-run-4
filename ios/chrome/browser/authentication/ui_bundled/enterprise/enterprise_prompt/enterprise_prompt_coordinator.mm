81adac79cb89e196323568155eae498d549d85b8
Benjamin Williams <bwwilliams@google.com>
2025-03-19 21:01:49 -0700

    [iOS][Code Health] Fix UISheetPresentationControllerDetent syntax
    This CL corrects improper instances of property-style access for detents
    to proper method call syntax.
    
    Impact:
    
    - No functional changes to behavior
    - Improves code health by using the correct Objective-C method
      invocation syntax
    
    This CL was uploaded by git cl split.
    
    R=arthurmilchior@chromium.org
    
    Bug: 402705893
    Change-Id: I83feaf82f3b2cf02592e61c59f08d10ef33a5c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368476
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435239}

--- ios/chrome/browser/authentication/ui_bundled/enterprise/enterprise_prompt/enterprise_prompt_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/enterprise/enterprise_prompt/enterprise_prompt_coordinator.mm
@@ -55,8 +55,8 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
       self.viewController.sheetPresentationController;
   presentationController.prefersEdgeAttachedInCompactHeight = YES;
   presentationController.detents = @[
-    UISheetPresentationControllerDetent.mediumDetent,
-    UISheetPresentationControllerDetent.largeDetent
+    [UISheetPresentationControllerDetent mediumDetent],
+    [UISheetPresentationControllerDetent largeDetent]
   ];
   presentationController.preferredCornerRadius = kHalfSheetCornerRadius;
 
--- ios/chrome/browser/authentication/ui_bundled/enterprise/managed_profile_creation/learn_more_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/enterprise/managed_profile_creation/learn_more_coordinator.mm
@@ -50,8 +50,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       _navigationController.sheetPresentationController;
   presentationController.prefersEdgeAttachedInCompactHeight = YES;
   presentationController.detents = @[
-    UISheetPresentationControllerDetent.mediumDetent,
-    UISheetPresentationControllerDetent.largeDetent
+    [UISheetPresentationControllerDetent mediumDetent],
+    [UISheetPresentationControllerDetent largeDetent]
   ];
   [self.baseViewController presentViewController:_navigationController
                                         animated:YES
