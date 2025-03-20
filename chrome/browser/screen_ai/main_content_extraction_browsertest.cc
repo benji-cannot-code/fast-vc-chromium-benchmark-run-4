fde8b0e533bf5509cbeb5cadb5745ab945f4afa3
Ramin Halavati <rhalavati@google.com>
2025-03-20 09:53:03 -0700

    Add more main content extraction metrics and crash key.
    Add more metrics and a crash key to follow up cases where main content
    extraction fails.
    Failure metrics are recorded separately per caller for easier follow up.
    
    OBSOLETE_HISTOGRAM[Accessibility.ScreenAI.MainContentExtraction.Successful]=Replaced by "Accessibility.ScreenAI.MainContentExtraction.Successful2" which has slightly different scope.
    NO_IFTTT=The suggestion looks irrelevant to me.
    
    AX-Relnotes: n/a
    Bug: 359853518
    Change-Id: Ifaa61afa234f748514d1d75038cead6699515932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6375385
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435502}

--- chrome/browser/screen_ai/main_content_extraction_browsertest.cc
+++ chrome/browser/screen_ai/main_content_extraction_browsertest.cc
@@ -153,6 +153,8 @@ class MainContentExtractionTest : public InProcessBrowserTest {
 
 // Tests that calling main content extraction without content gets replied.
 IN_PROC_BROWSER_TEST_F(MainContentExtractionTest, EmptyInput) {
+  base::HistogramTester histograms;
+
   Connect();
 
   ui::AXNodeData root;
@@ -170,6 +172,17 @@ IN_PROC_BROWSER_TEST_F(MainContentExtractionTest, EmptyInput) {
   ExtractMainContent(empty_tree, main_content_ids);
 
   ASSERT_EQ(0u, main_content_ids.size());
+
+  metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+
+  histograms.ExpectTotalCount(
+      "Accessibility.ScreenAI.MainContentExtraction.Error.ResultNull", 1);
+  histograms.ExpectUniqueSample(
+      "Accessibility.ScreenAI.MainContentExtraction.Successful2", false, 1);
+  histograms.ExpectTotalCount(
+      "Accessibility.ScreenAI.MainContentExtraction.Latency.Success", 0);
+  histograms.ExpectTotalCount(
+      "Accessibility.ScreenAI.MainContentExtraction.Latency.Failure", 1);
 }
 
 // Fake library always returns empty.
@@ -192,10 +205,12 @@ IN_PROC_BROWSER_TEST_F(MainContentExtractionTest, RequestWithContent) {
 
   metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
 
+  histograms.ExpectUniqueSample(
+      "Accessibility.ScreenAI.MainContentExtraction.Successful2", true, 1);
   histograms.ExpectTotalCount(
-      "Accessibility.ScreenAI.MainContentExtraction.Successful", 1);
-  histograms.ExpectBucketCount(
-      "Accessibility.ScreenAI.MainContentExtraction.Successful", true, 1);
+      "Accessibility.ScreenAI.MainContentExtraction.Latency.Success", 1);
+  histograms.ExpectTotalCount(
+      "Accessibility.ScreenAI.MainContentExtraction.Latency.Failure", 0);
 }
 
 // Test requesting several extractions without waiting for the previous ones to
