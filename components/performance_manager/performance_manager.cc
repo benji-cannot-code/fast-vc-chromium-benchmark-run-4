1a85db33856971977b2e2768a046c11d6a1ccd90
Patrick Monette <pmonette@chromium.org>
2025-03-20 13:46:15 -0700

    [PM] Remove CallOnGraph and RunInGraph
    The performance manager now lives on the UI thread. This means the
    graph can be directly accessed using PerformanceManager::GetGraph().
    
    This removes the need for the CallOnGraph family of functions, and
    its associated RunInGraph test helper.
    
    Bug: 40755583
    Change-Id: Id15d252340d53123d0d5aaa5c122fc236744d23b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6374137
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435659}

--- components/performance_manager/performance_manager.cc
+++ components/performance_manager/performance_manager.cc
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/performance_manager/public/performance_manager.h"
 
-#include <utility>
-
-#include "base/functional/bind.h"
-#include "base/functional/callback.h"
-#include "base/task/sequenced_task_runner.h"
 #include "components/performance_manager/graph/frame_node_impl.h"
 #include "components/performance_manager/graph/page_node_impl.h"
 #include "components/performance_manager/graph/process_node_impl.h"
@@ -30,43 +25,6 @@ PerformanceManager::~PerformanceManager() = default;
 // static
 Graph* PerformanceManager::GetGraph() {
   return PerformanceManagerImpl::GetGraphImpl();
-}
-
-// static
-void PerformanceManager::CallOnGraph(const base::Location& from_here,
-                                     base::OnceClosure callback) {
-  DCHECK(callback);
-
-  PerformanceManagerImpl::GetTaskRunner()->PostTask(from_here,
-                                                    std::move(callback));
-}
-// static
-void PerformanceManager::CallOnGraph(const base::Location& from_here,
-                                     GraphCallback callback) {
-  DCHECK(callback);
-
-  // TODO(siggi): Unwrap this by binding the loose param.
-  PerformanceManagerImpl::GetTaskRunner()->PostTask(
-      from_here, base::BindOnce(&PerformanceManagerImpl::RunCallbackWithGraph,
-                                std::move(callback)));
-}
-
-// static
-void PerformanceManager::PassToGraph(const base::Location& from_here,
-                                     std::unique_ptr<GraphOwned> graph_owned) {
-  DCHECK(graph_owned);
-
-  // PassToGraph() should only be called when a graph is available to take
-  // ownership of |graph_owned|.
-  DCHECK(IsAvailable());
-
-  PerformanceManagerImpl::CallOnGraphImpl(
-      from_here,
-      base::BindOnce(
-          [](std::unique_ptr<GraphOwned> graph_owned, GraphImpl* graph) {
-            graph->PassToGraph(std::move(graph_owned));
-          },
-          std::move(graph_owned)));
 }
 
 // static
@@ -177,11 +135,6 @@ void PerformanceManager::AddObserver(PerformanceManagerObserver* observer) {
 // static
 void PerformanceManager::RemoveObserver(PerformanceManagerObserver* observer) {
   PerformanceManagerRegistryImpl::GetInstance()->RemoveObserver(observer);
-}
-
-// static
-scoped_refptr<base::SequencedTaskRunner> PerformanceManager::GetTaskRunner() {
-  return PerformanceManagerImpl::GetTaskRunner();
 }
 
 // static
--- components/performance_manager/resource_attribution/queries_unittest.cc
+++ components/performance_manager/resource_attribution/queries_unittest.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/test_support/performance_manager_test_harness.h"
 #include "components/performance_manager/test_support/resource_attribution/gtest_util.h"
 #include "components/performance_manager/test_support/resource_attribution/measurement_delegates.h"
-#include "components/performance_manager/test_support/run_in_graph.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
