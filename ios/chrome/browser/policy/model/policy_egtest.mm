897048d4da205ccd18d0932152ae0d0ebbaee702
Yann Dago <ydago@chromium.org>
2025-03-20 11:18:54 -0700

    [MIM2] Fix PolicyTests with multi profile
    * Modify SigninEarlGreyAppInterface signinWithFakeIdentity to convert
    the current profile in a managed one when signing in with a managed
    account while multi profile is enabled.
    
    Bug: 400648043, 379077722
    Change-Id: I2d29bb134bd34a8b95d149d6f81a6ec6a9835957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6358035
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435566}

--- ios/chrome/browser/policy/model/policy_egtest.mm
+++ ios/chrome/browser/policy/model/policy_egtest.mm
@@ -485,7 +485,13 @@ NSString* const kDomain2 = @"domain2.com";
       base::StrCat({"enterprise@", policy::SignatureProvider::kTestDomain1}));
   FakeSystemIdentity* fakeManagedIdentity =
       [FakeSystemIdentity identityWithEmail:managedAccountEmail];
-  [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+    [SigninEarlGrey
+        signinWithFakeManagedIdentityInPersonalProfile:fakeManagedIdentity];
+  } else {
+    [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+  }
 
   // Open the menu and click on the item.
   [ChromeEarlGreyUI openToolsMenu];
@@ -513,7 +519,13 @@ NSString* const kDomain2 = @"domain2.com";
       base::StrCat({"enterprise@", policy::SignatureProvider::kTestDomain1}));
   FakeSystemIdentity* fakeManagedIdentity =
       [FakeSystemIdentity identityWithEmail:managedAccountEmail];
-  [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+    [SigninEarlGrey
+        signinWithFakeManagedIdentityInPersonalProfile:fakeManagedIdentity];
+  } else {
+    [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+  }
 
   // Open the menu and click on the item.
   [ChromeEarlGreyUI openToolsMenu];
@@ -593,7 +605,13 @@ NSString* const kDomain2 = @"domain2.com";
       [@"enterprise@" stringByAppendingString:kDomain1];
   FakeSystemIdentity* fakeManagedIdentity =
       [FakeSystemIdentity identityWithEmail:managedAccountEmail];
-  [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+    [SigninEarlGrey
+        signinWithFakeManagedIdentityInPersonalProfile:fakeManagedIdentity];
+  } else {
+    [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+  }
 
   // Open the management page and check if the content is expected.
   [ChromeEarlGrey loadURL:GURL(kChromeUIManagementURL)];
@@ -635,7 +653,13 @@ NSString* const kDomain2 = @"domain2.com";
       [@"enterprise@" stringByAppendingString:kDomain2];
   FakeSystemIdentity* fakeManagedIdentity =
       [FakeSystemIdentity identityWithEmail:managedAccountEmail];
-  [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+    [SigninEarlGrey
+        signinWithFakeManagedIdentityInPersonalProfile:fakeManagedIdentity];
+  } else {
+    [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+  }
 
   // Open the management page and check if the content is expected.
   [ChromeEarlGrey loadURL:GURL(kChromeUIManagementURL)];
@@ -679,7 +703,13 @@ NSString* const kDomain2 = @"domain2.com";
       [@"enterprise@" stringByAppendingString:kDomain1];
   FakeSystemIdentity* fakeManagedIdentity =
       [FakeSystemIdentity identityWithEmail:managedAccountEmail];
-  [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+    [SigninEarlGrey
+        signinWithFakeManagedIdentityInPersonalProfile:fakeManagedIdentity];
+  } else {
+    [SigninEarlGrey signinWithFakeIdentity:fakeManagedIdentity];
+  }
 
   // Open the management page and check if the content is expected.
   [ChromeEarlGrey loadURL:GURL(kChromeUIManagementURL)];
