3510f665a20df4478b17dc1e08111d6d930b21cd
Fiona Macintosh <fmacintosh@google.com>
2025-03-20 05:52:07 -0700

    [RWS] Add managed state to RWS V2 entrypoint but preserve settings link
    We temporarily removed the `setManagedPreferenceDelegate` in the V2
    codepath to prevent the entrypoint link to site settings from being
    blocked. This CL re-adds it while still preserving the site settings
    link by overriding the `isPreferenceClickDisabled` function
    
    Bug: b:405069085
    Change-Id: I6b30eb8fd2f4a5b6354e5950d30acceff071ba1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6371829
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435351}

--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/FakePrivacySandboxBridge.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/FakePrivacySandboxBridge.java
@@ -19,6 +19,7 @@ import java.util.Set;
 public class FakePrivacySandboxBridge implements PrivacySandboxBridge.Natives {
     private boolean mIsPrivacySandboxRestricted /* = false*/;
     private boolean mIsRestrictedNoticeEnabled /* = false*/;
+    private boolean mIsRwsManaged /* = false*/;
 
     private final HashMap<String, Topic> mTopics = new HashMap<>();
     private final Set<Topic> mCurrentTopTopics = new LinkedHashSet<>();
@@ -93,7 +94,7 @@ public class FakePrivacySandboxBridge implements PrivacySandboxBridge.Natives {
 
     @Override
     public boolean isRelatedWebsiteSetsDataAccessEnabled(Profile profile) {
-        return false;
+        return true;
     }
 
     @Override
@@ -103,7 +104,7 @@ public class FakePrivacySandboxBridge implements PrivacySandboxBridge.Natives {
 
     @Override
     public boolean isPartOfManagedRelatedWebsiteSet(Profile profile, String origin) {
-        return false;
+        return mIsRwsManaged;
     }
 
     @Override
@@ -112,6 +113,10 @@ public class FakePrivacySandboxBridge implements PrivacySandboxBridge.Natives {
     @Override
     public String getRelatedWebsiteSetOwner(Profile profile, String memberOrigin) {
         return null;
+    }
+
+    public void setIsRwsManaged(boolean managed) {
+        mIsRwsManaged = managed;
     }
 
     public void setPrivacySandboxRestricted(boolean restricted) {
