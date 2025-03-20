195824110d66c202973b297264876a093e24ea09
Philipp Hancke <philipp.hancke@googlemail.com>
2025-03-20 05:29:31 -0700

    webrtc: fix missing transitive include of WEBRTC_VIDEO_CODEC_*
    which was found during downstream IWYU
      https://webrtc-review.googlesource.com/c/src/+/381860
    
    BUG=webrtc:42226242
    
    Change-Id: I1e8a62c0b92ce30afd3617fc45e315d9bfdf19c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6374348
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1435346}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/video/video_frame.h"
 #include "third_party/webrtc/api/video_codecs/vp9_profile.h"
 #include "third_party/webrtc/modules/video_coding/codecs/h264/include/h264.h"
+#include "third_party/webrtc/modules/video_coding/include/video_error_codes.h"
 #include "third_party/webrtc/rtc_base/ref_count.h"
 #include "third_party/webrtc/rtc_base/ref_counted_object.h"
 #include "ui/gfx/color_space.h"
