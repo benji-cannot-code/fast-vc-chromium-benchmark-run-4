5a1e861532a891934a5126c57c5d151d127ae7f6
Mitsuru Oshima <oshima@chromium.org>
2025-03-19 23:32:51 -0700

    Ensure that the background covers entire TopContainerView.
    This is a stop-gap solution to fix the gap between TopContainerView
    and contents.
    
    Due to the rounding happening in cc side, the paint operation may
    not cover the entire texture layer, because the origin  and size of
    the clip may be rounded down.
    
    Because the component below TopContainerView is a web content which
    embeds the content from the renderer, nothing will be pained
    behind it, which leaves the texture transparent (or blue in debug
    build).
    
    This CL expand the height of the clip bounds of the TopContainerView,
    and its child components which paints the background so that they
    always fills the entire texture.
    
    In theory, ContentsSeparator should also need this, but it didn't
    cause the issue for 1/1.25/1.5/1.75/2.0, so it is excluded.
    
    The correct fix is to enable pixel canvas (go/pixel-canvas-slide) on
    Windows (and Linux). I'll work on it as follow up task.
    
    Bug: 390669712, 41344902
    Test: I tested 1/1.25/1.5/1.75/2 scales with/without theme, and with/without BookmarkBar on Windows.
    Change-Id: I1eb49d21bb56910027e0955fd28abd188c60dea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6370016
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435260}

--- chrome/browser/ui/views/frame/browser_view_layout.cc
+++ chrome/browser/ui/views/frame/browser_view_layout.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/web_modal/web_contents_modal_dialog_host.h"
 #include "ui/base/hit_test.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/compositor/compositor_switches.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
@@ -62,6 +63,24 @@ namespace {
 // The number of pixels the constrained window should overlap the bottom
 // of the omnibox.
 const int kConstrainedWindowOverlap = 3;
+
+// The normal clipping created by `View::Paint()` may not cover the bottom of
+// the TopContainerView at certain scale factor because both of the position and
+// the height might be roudned down. This function sets the clip path that
+// enlarges the height at 2 DPs to compensate this error (both origin and size)
+// that the canvas can cover the entire TopContainerView.  See
+// crbug.com/390669712 for more details.  TODO(crbug.com/41344902): Remove this
+// hack once the pixel canvas is enabled on all aura platforms.  Note that macOS
+// supports integer scale only, so this isn't necessary on macOS.
+void SetClipPathWithBottomAllowance(views::View* view) {
+  if (!ui::IsPixelCanvasRecordingEnabled()) {
+    constexpr int kBottomPaintAllowance = 2;
+    const gfx::Rect local_bounds = view->GetLocalBounds();
+    const int extended_height = local_bounds.height() + kBottomPaintAllowance;
+    view->SetClipPath(
+        SkPath::Rect(SkRect::MakeWH(local_bounds.width(), extended_height)));
+  }
+}
 
 }  // namespace
 
@@ -499,6 +518,12 @@ int BrowserViewLayout::LayoutToolbar(int top) {
   SetViewVisibility(toolbar_, toolbar_visible);
   toolbar_->SetBounds(vertical_layout_rect_.x(), top, browser_view_width,
                       height);
+  if (bookmark_bar_ && bookmark_bar_->GetVisible()) {
+    // Remove the clip if the toolbar is not at the bottom.
+    toolbar_->SetClipPath({});
+  } else {
+    SetClipPathWithBottomAllowance(toolbar_);
+  }
   return toolbar_->bounds().bottom();
 }
 
@@ -550,6 +575,16 @@ int BrowserViewLayout::LayoutBookmarkBar(int top) {
   int bookmark_bar_height = bookmark_bar_->GetPreferredSize().height();
   bookmark_bar_->SetBounds(vertical_layout_rect_.x(), top,
                            vertical_layout_rect_.width(), bookmark_bar_height);
+  SetClipPathWithBottomAllowance(bookmark_bar_);
+  // Make sure the contents separator is painted last as the background for
+  // BookmarkVieBar/ToolbarView may paint over it otherwise.
+  // TODO(crbug.com/41344902): Remove once the pixel canvas is enabled on
+  // all aura platforms.
+  if (top_container_ == bookmark_bar_->parent()) {
+    top_container_->ReorderChildView(contents_separator_,
+                                     top_container_->children().size());
+  }
+
   // Set visibility after setting bounds, as the visibility update uses the
   // bounds to determine if the mouse is hovering over a button.
   SetViewVisibility(bookmark_bar_, true);
@@ -763,6 +798,7 @@ void BrowserViewLayout::UpdateTopContainerBounds() {
             top_container_bounds.size()));
   }
   top_container_->SetBoundsRect(top_container_bounds);
+  SetClipPathWithBottomAllowance(top_container_);
 }
 
 int BrowserViewLayout::LayoutDownloadShelf(int bottom) {
