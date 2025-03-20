f5b7f7f22bdbaf9035b04c61225411e8d771d180
Chuong Ho <hdchuong@google.com>
2025-03-19 19:43:54 -0700

    magic-boost: keep settings toggle value intact when approving consent
    In case any magic boost feature is switched off, approving the magic boost consent should not update
    
    Fixed: b:397306988
    Change-Id: I379fb380b2fbc899d2efc199532a7fa43509f3b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6370884
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435223}

--- chrome/browser/ash/magic_boost/magic_boost_controller_ash_unittest.cc
+++ chrome/browser/ash/magic_boost/magic_boost_controller_ash_unittest.cc
@@ -130,8 +130,6 @@ TEST_F(MagicBoostControllerAshTest, OnDisclaimerAcceptButtonPressed) {
 
   EXPECT_EQ(chromeos::HMRConsentStatus::kApproved,
             mock_magic_boost_state_->hmr_consent_status());
-  EXPECT_TRUE(mock_magic_boost_state_->hmr_enabled().value());
-
   EXPECT_FALSE(controller.disclaimer_widget_for_test());
 
   // Records the `kAcceptButtonPressed` metrics.
@@ -169,8 +167,6 @@ TEST_F(MagicBoostControllerAshTest,
 
   EXPECT_EQ(chromeos::HMRConsentStatus::kApproved,
             mock_magic_boost_state_->hmr_consent_status());
-  EXPECT_TRUE(mock_magic_boost_state_->hmr_enabled().value());
-
   EXPECT_FALSE(controller.disclaimer_widget_for_test());
 
   // Records the `kAcceptButtonPressed` metrics.
