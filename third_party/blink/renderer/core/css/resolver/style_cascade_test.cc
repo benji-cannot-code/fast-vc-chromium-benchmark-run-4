ecc6f458eb435c3297686256d277a8fd7839982d
Anders Hartvoll Ruud <andruud@chromium.org>
2025-03-20 05:21:23 -0700

    [css-properties-values-api] Ship CSSTypeAgnosticVarFallback
    A var() fallback no longer needs to match the type of the custom
    property being referenced, per #10455 [1].
    
    [1] https://github.com/w3c/csswg-drafts/issues/10455
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/yc4XmGZi1Mk/m/XP8MBN9XAgAJ
    Fixed: 372475301
    Change-Id: I71cddc7ab8ca405132b47923cc780ba8f6ea8233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6373485
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435344}

--- third_party/blink/renderer/core/css/resolver/style_cascade_test.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade_test.cc
@@ -569,6 +569,8 @@ TEST_F(StyleCascadeTest, RegisteredPropertyFallback) {
 }
 
 TEST_F(StyleCascadeTest, RegisteredPropertyFallbackValidation) {
+  ScopedCSSTypeAgnosticVarFallbackForTest scoped_feature(false);
+
   RegisterProperty(GetDocument(), "--x", "<length>", "0px", false);
 
   TestCascade cascade(GetDocument());
@@ -578,6 +580,19 @@ TEST_F(StyleCascadeTest, RegisteredPropertyFallbackValidation) {
   cascade.Apply();
 
   EXPECT_EQ("pass", cascade.ComputedValue("--z"));
+}
+
+TEST_F(StyleCascadeTest, TypeAgnosticFallback) {
+  ScopedCSSTypeAgnosticVarFallbackForTest scoped_feature(true);
+
+  RegisterProperty(GetDocument(), "--x", "<length>", "0px", false);
+
+  TestCascade cascade(GetDocument());
+  cascade.Add("--x", "10px");
+  cascade.Add("--y", "var(--x,foo)");  // Fallback need not be a <length>.
+  cascade.Apply();
+
+  EXPECT_EQ("10px", cascade.ComputedValue("--y"));
 }
 
 TEST_F(StyleCascadeTest, VarInFallback) {
@@ -4671,6 +4686,8 @@ TEST_F(StyleCascadeTest, CSSFunctionDoesNotExistInShorthand) {
 }
 
 TEST_F(StyleCascadeTest, VarFallbackValidationCounter) {
+  ScopedCSSTypeAgnosticVarFallbackForTest scoped_feature(false);
+
   RegisterProperty(GetDocument(), "--registered", "<length>", "0px",
                    /*inherited=*/false);
 
