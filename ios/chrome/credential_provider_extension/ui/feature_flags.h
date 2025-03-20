bdb40cc2f596baf21cbdec8bc8cf06f3be1a2b0f
Alexis Hetu <sugoi@chromium.org>
2025-03-20 08:50:15 -0700

    [iOS] Return a different error message when the enterprise policy isn't set
    When the enterprise policy isn't set at all, the CPE shouldn't
    behave as if the enterprise policy was set to false, as this can
    be very confusing to non enterprise users. Instead, it should
    behave the same way as when the gaia id isn't set.
    
    Bug: 383555009
    Change-Id: I0e71a975814373abfb3c418b3cf82dff5b4a1735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367507
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435447}

--- ios/chrome/credential_provider_extension/ui/feature_flags.h
+++ ios/chrome/credential_provider_extension/ui/feature_flags.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import <optional>
+
 // Whether automatic passkey upgrade is enabled for the user.
 BOOL IsAutomaticPasskeyUpgradeEnabled();
 
@@ -24,12 +26,12 @@ BOOL IsPasswordCreationManaged();
 // Whether password sync is enabled for this user.
 BOOL IsPasswordSyncEnabled();
 
-// Whether passkey saving is allowed by policy. Always returns `YES` for
+// Policy about whether passkey saving is allowed. The return value isn't set if
+// the policy isn't set in user defaults. The policy itself returns `true` for
 // unmanaged users, or for users whose enterprise has not configured this
-// policy.
-// IMPORTANT: If `IsPasswordCreationUserEnabled()` is `NO`, that supercedes this
-// policy.
-BOOL IsPasskeyCreationAllowedByPolicy();
+// policy. IMPORTANT: If `IsPasswordCreationUserEnabled()` is `NO`, that
+// supercedes this policy.
+std::optional<bool> GetPasskeyCreationPolicy();
 
 // Whether the passkeys M2 feature is currently enabled.
 BOOL IsPasskeysM2Enabled();
--- ios/chrome/credential_provider_extension/ui/feature_flags.mm
+++ ios/chrome/credential_provider_extension/ui/feature_flags.mm
@@ -42,11 +42,14 @@ BOOL IsPasswordSyncEnabled() {
       boolValue];
 }
 
-BOOL IsPasskeyCreationAllowedByPolicy() {
-  return [[app_group::GetGroupUserDefaults()
+std::optional<bool> GetPasskeyCreationPolicy() {
+  id passkeyCreationPolicy = [app_group::GetGroupUserDefaults()
       objectForKey:
-          AppGroupUserDefaultsCredentialProviderSavingPasskeysEnabled()]
-      boolValue];
+          AppGroupUserDefaultsCredentialProviderSavingPasskeysEnabled()];
+  if (!passkeyCreationPolicy) {
+    return std::nullopt;
+  }
+  return [passkeyCreationPolicy boolValue];
 }
 
 BOOL IsPasskeysM2Enabled() {
