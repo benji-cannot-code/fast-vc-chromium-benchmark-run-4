692195b5f50c787e8ceaed454f16472bbbb95a3a
Michael Cui <mlcui@google.com>
2025-03-20 16:56:43 -0700

    Rename `HomeButton::OnAssistantAvailabilityChanged` to `OnIconUpdated`
    This more accurately reflects what the method is doing, and makes it
    clearer that it is agnostic to Assistant (for an upcoming change which
    calls it when Sunfish / Scanner's state is updated).
    
    Bug: b:402237553
    Change-Id: I6a6a2100c6dbb5a7c823ec444063667845ef765e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6365735
    Commit-Queue: Michael Cui <mlcui@google.com>
    Auto-Submit: Michael Cui <mlcui@google.com>
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435775}

--- ash/shelf/home_button_controller.cc
+++ ash/shelf/home_button_controller.cc
@@ -164,11 +164,11 @@ void HomeButtonController::OnDisplayTabletStateChanged(
 
 void HomeButtonController::OnAssistantFeatureAllowedChanged(
     assistant::AssistantAllowedState state) {
-  button_->OnAssistantAvailabilityChanged();
+  button_->OnIconUpdated();
 }
 
 void HomeButtonController::OnAssistantSettingsEnabled(bool enabled) {
-  button_->OnAssistantAvailabilityChanged();
+  button_->OnIconUpdated();
 }
 
 void HomeButtonController::OnUiVisibilityChanged(
@@ -176,12 +176,12 @@ void HomeButtonController::OnUiVisibilityChanged(
     AssistantVisibility old_visibility,
     std::optional<AssistantEntryPoint> entry_point,
     std::optional<AssistantExitPoint> exit_point) {
-  button_->OnAssistantAvailabilityChanged();
+  button_->OnIconUpdated();
 }
 
 void HomeButtonController::OnSunfishScannerFeatureStatesChanged(
     SunfishScannerFeatureWatcher& source) {
-  // TODO: crbug.com/391909810 - Update the button's visibility here.
+  // TODO: crbug.com/391909810 - Update the button's icon here.
 }
 
 void HomeButtonController::StartAssistantAnimation() {
