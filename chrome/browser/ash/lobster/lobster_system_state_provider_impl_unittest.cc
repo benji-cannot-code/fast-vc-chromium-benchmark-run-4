e51275f49bbbac6dfb7fbab2b744a5e069cf69cb
Chuong Ho <hdchuong@google.com>
2025-03-19 20:51:12 -0700

    lobster: fix tablet mode
    Previously, this CL crrev/c/6347140 enables the feature in tablet mode,
    however the expectation is completely opposite.
    
    This CL corrects this behavior and only enables the feature in clamshell
    mode.
    
    Bug: b:399513104
    Change-Id: I3acbe57716ac5d05a74cbef04e8f501fcfef7245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6369841
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Grey Wang <greywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435235}

--- chrome/browser/ash/lobster/lobster_system_state_provider_impl_unittest.cc
+++ chrome/browser/ash/lobster/lobster_system_state_provider_impl_unittest.cc
@@ -222,7 +222,7 @@ class LobsterSystemStateProviderImplGeolocationTest
     SetActiveIme("xkb:us::eng");
     SetAccountCapabilityValue(true);
     SetCountryCode(std::get<0>(GetParam()));
-    SetTabletModeState(true);
+    SetTabletModeState(false);
   }
 };
 
@@ -254,7 +254,7 @@ class LobsterSystemStateProviderImplAccountCapabilityTest
     SetActiveIme("xkb:us::eng");
     SetCountryCode("au");
     SetAccountCapabilityValue(/*satisfied=*/std::get<0>(GetParam()));
-    SetTabletModeState(true);
+    SetTabletModeState(false);
   }
 };
 
@@ -285,7 +285,7 @@ class LobsterSystemStateProviderImplTextInputFieldTest
     SetActiveIme("xkb:us::eng");
     SetCountryCode("au");
     SetAccountCapabilityValue(true);
-    SetTabletModeState(true);
+    SetTabletModeState(false);
   }
 };
 
@@ -348,7 +348,7 @@ class LobsterSystemStateProviderImplNetworkStatusTest
     SetActiveIme("xkb:us::eng");
     SetCountryCode("au");
     SetAccountCapabilityValue(true);
-    SetTabletModeState(true);
+    SetTabletModeState(false);
   }
 };
 
@@ -379,7 +379,7 @@ class LobsterSystemStateProviderImplImeTest
     SetActiveIme(std::get<0>(GetParam()));
     SetCountryCode("au");
     SetAccountCapabilityValue(true);
-    SetTabletModeState(true);
+    SetTabletModeState(false);
   }
 };
 
@@ -435,7 +435,7 @@ class LobsterSystemStateProviderImplTabletModeTest
     SetActiveIme("xkb:us::eng");
     SetCountryCode("au");
     SetAccountCapabilityValue(true);
-    SetTabletModeState(/*is_online=*/std::get<0>(GetParam()));
+    SetTabletModeState(/*is_in_tablet_mode=*/std::get<0>(GetParam()));
   }
 };
 
@@ -443,9 +443,9 @@ INSTANTIATE_TEST_SUITE_P(
     ,
     LobsterSystemStateProviderImplTabletModeTest,
     testing::Values(std::make_tuple(/*is_in_tablet_mode=*/true,
-                                    ash::LobsterStatus::kEnabled),
+                                    ash::LobsterStatus::kBlocked),
                     std::make_tuple(/*is_in_tablet_mode=*/false,
-                                    ash::LobsterStatus::kBlocked)));
+                                    ash::LobsterStatus::kEnabled)));
 
 TEST_P(LobsterSystemStateProviderImplTabletModeTest,
        ChecksTheSystemStateStatus) {
