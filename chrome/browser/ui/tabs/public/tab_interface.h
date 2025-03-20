55435da1c51e713f39ac83d2a76bf7559189cc33
Calder Kitagawa <ckitagawa@chromium.org>
2025-03-20 09:52:52 -0700

    [Tab Collections] Remove split_tab_id header from tab_interface
    Per-TabInterface's class-level comment it should take no direct
    dependencies on chrome/. Enforce this by forward declaring SplitTabId.
    
    Bug: 404296389
    Change-Id: I7fa1ca90055b35b65033cbff76492d630add39d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6375923
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435501}

--- chrome/browser/ui/tabs/public/tab_interface.h
+++ chrome/browser/ui/tabs/public/tab_interface.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/functional/callback.h"
-#include "chrome/browser/ui/tabs/split_tab_id.h"
 #include "components/tab_collections/public/supports_handles.h"
 #include "components/tab_groups/tab_group_id.h"
 
@@ -19,6 +18,10 @@ class WebContents;
 }  // namespace content
 
 class BrowserWindowInterface;
+
+namespace split_tabs {
+class SplitTabId;
+}  // namespace split_tabs
 
 namespace tabs {
 
