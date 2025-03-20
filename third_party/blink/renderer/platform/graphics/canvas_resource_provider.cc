3b5ff6245dda1373e9f3c299d74bdc4818808b76
Colin Blundell <blundell@chromium.org>
2025-03-20 08:53:30 -0700

    [Blink] Enforce CanvasResourceSwapChain validity
    CanvasResourceSwapChain::IsValid() returns true iff the context provider
    wrapper is non-null. On construction this will always be case, as is
    shown by the CHECKs introduced in this CL. Note that:
    
    * The only way to instantiate a CanvasResourceSwapChain is via
    CanvasResourceSwapChain::Create()
    * The only caller of the latter is the CanvasResourceProviderSwapChain
      constructor
    * The only way to instantiate a CanvasResourceProviderSwapChain is via
      CanvasResourceProvider::CreateSwapChainProvider(), which
      short-circuits out if the context provider wrapper is null [1]
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1270-1271;drc=4c6d5b3c3ed16fa75258c6b4763188f840cb433d;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: I3696db41ac75ca5cb962403acaac4d3d79ce5181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6373031
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435449}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -892,9 +892,11 @@ class CanvasResourceProviderSwapChain final : public CanvasResourceProvider {
                                    ->ContextProvider()
                                    .GetCapabilities()
                                    .gpu_rasterization) {
+    CHECK(ContextProviderWrapper());
     resource_ = CanvasResourceSwapChain::Create(
         size, format, alpha_type, color_space, ContextProviderWrapper(),
         CreateWeakPtr());
+    CHECK(resource_);
   }
   ~CanvasResourceProviderSwapChain() override = default;
 
