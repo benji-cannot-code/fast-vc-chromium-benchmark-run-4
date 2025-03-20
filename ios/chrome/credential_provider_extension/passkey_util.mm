1aaed177473c80383edd2736e60d5e6690db457b
Noémie St-Onge <noemies@google.com>
2025-03-20 08:23:45 -0700

    [iOS] Remove user verification preference crash key
    We've received reports proving that passkey requests can come with a
    user verification preference that doesn't match one of the values
    defined in the WebAuthn API (i.e., "required", "preferred" or
    "discouraged").
    
    Knowing that this is possible, there's no need to track such occurrences
    anymore. We can simply handle them by mapping the unexpected values to
    one of the known preference values (like we already do). Hence, the
    crash key and DumpWithoutCrashing reporting can be removed.
    
    Note: We used to map unexpected values to the "required" preference.
    However, as the WebAuthn spec identifies the "preferred" preference as
    the default one, this CL updates the mapping of such values to follow
    this guideline.
    
    Fixed: 392239320
    Change-Id: I6ac25f9f47a08afd66caba6b4b8508f3e6c4aa27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6353619
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435425}

--- ios/chrome/credential_provider_extension/passkey_util.mm
+++ ios/chrome/credential_provider_extension/passkey_util.mm
@@ -9,11 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/apple/foundation_util.h"
 #import "base/containers/span.h"
-#import "base/debug/dump_without_crashing.h"
 #import "base/strings/string_number_conversions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/time/time.h"
-#import "components/crash/core/common/crash_key.h"
 #import "components/sync/protocol/webauthn_credential_specifics.pb.h"
 #import "components/webauthn/core/browser/passkey_model_utils.h"
 #import "ios/chrome/common/app_group/app_group_constants.h"
@@ -26,11 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::SysNSStringToUTF8;
 
 namespace {
-
-// Crash key to investigate the content of the user verification preference
-// string received through a passkey request.
-crash_reporter::CrashKeyString<64> user_verification_preference_crash_key(
-    "iOS CPE user verification preference");
 
 // Appends "data" at the end of "container".
 void Append(std::vector<uint8_t>& container, NSData* data) {
@@ -195,7 +188,8 @@ UserVerificationPreference UserVerificationPreferenceFromString(
               ASAuthorizationPublicKeyCredentialUserVerificationPreferenceDiscouraged]) {
     return UserVerificationPreference::kDiscouraged;
   } else {
-    // Probably indicates that the WebAuthn API changed.
+    // Either indicates that the WebAuthn API changed, or that the website
+    // provided an unexpected/empty string.
     return UserVerificationPreference::kOther;
   }
 }
@@ -353,22 +347,13 @@ BOOL ShouldPerformUserVerificationForPreference(
   UserVerificationPreference user_verification_preference =
       UserVerificationPreferenceFromString(user_verification_preference_string);
 
-  // If the UserVerificationPreference value is `kOther`, the WebAuthn API
-  // probably changed. This should be investigated, but shouldn't cause a crash.
-  if (user_verification_preference == UserVerificationPreference::kOther) {
-    // TODO(crbug.com/392239320): Clean up crash key and when done with the
-    // investigation.
-    user_verification_preference_crash_key.Set(
-        base::SysNSStringToUTF8(user_verification_preference_string));
-    base::debug::DumpWithoutCrashing();
-  }
-
   switch (user_verification_preference) {
     case UserVerificationPreference::kRequired:
-    case UserVerificationPreference::kOther:  // Fall back to highest degree of
-                                              // security.
       return YES;
     case UserVerificationPreference::kPreferred:
+    case UserVerificationPreference::kOther:  // Fall back to the default
+                                              // preference as per the WebAuthn
+                                              // spec.
       return is_biometric_authentication_enabled;
     case UserVerificationPreference::kDiscouraged:
       return NO;
