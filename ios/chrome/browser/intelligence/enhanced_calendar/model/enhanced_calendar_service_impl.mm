8decb43ed2563e4b93898903f00fbd6b2dce12bd
“Prasanaa” <prasanaa@google.com>
2025-03-20 13:19:50 -0700

    [iOS] Adding helper methods in PageContextWrapper
    Adding helper methods in PageContextWrapper to improve readability.
    
    Change-Id: Idb8bab08fa0a72aabc6375030d4551ddf675522f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6376283
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435642}

--- ios/chrome/browser/intelligence/enhanced_calendar/model/enhanced_calendar_service_impl.mm
+++ ios/chrome/browser/intelligence/enhanced_calendar/model/enhanced_calendar_service_impl.mm
@@ -106,14 +106,7 @@ void EnhancedCalendarServiceImpl::OnEnhancedCalendarResponse(
     std::unique_ptr<optimization_guide::ModelQualityLogEntry> entry) {
   mojom::EnhancedCalendarResponseResultPtr result_union;
 
-  if (!result.response.has_value()) {
-    std::string error_string =
-        base::StrCat({"Server model execution error: ",
-                      service_->ResponseForErrorCode(
-                          static_cast<int>(result.response.error().error()))});
-    result_union =
-        mojom::EnhancedCalendarResponseResult::NewError(error_string);
-  } else {
+  if (result.response.has_value()) {
     std::optional<optimization_guide::proto::EnhancedCalendarResponse>
         response_proto = optimization_guide::ParsedAnyMetadata<
             optimization_guide::proto::EnhancedCalendarResponse>(
@@ -126,6 +119,13 @@ void EnhancedCalendarServiceImpl::OnEnhancedCalendarResponse(
       result_union = mojom::EnhancedCalendarResponseResult::NewError(
           "Proto unmarshalling error.");
     }
+  } else {
+    std::string error_string =
+        base::StrCat({"Server model execution error: ",
+                      service_->ResponseForErrorCode(
+                          static_cast<int>(result.response.error().error()))});
+    result_union =
+        mojom::EnhancedCalendarResponseResult::NewError(error_string);
   }
 
   std::move(request_callback).Run(std::move(result_union));
