4258c9d3b7af68acef6ca1b8e964ed2c734afe0e
Aliona DANGLA <alionadangla@google.com>
2025-03-21 09:45:02 -0700

    [iOS] Add TabGroupService
    Create a tab group service to store some helper functions needed for the
    web state list.
    
    Fixed: 378881981
    Change-Id: Iaf49b35e36c1fee7f6a14e2bbedc0babf46d4ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304464
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436105}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -113,6 +113,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/safe_browsing/model/tailored_security/tailored_security_service_factory.h"
 #import "ios/chrome/browser/safe_browsing/model/verdict_cache_manager_factory.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_factory.h"
+#import "ios/chrome/browser/saved_tab_groups/model/tab_group_service_factory.h"
 #import "ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory.h"
 #import "ios/chrome/browser/screen_time/model/screen_time_buildflags.h"
 #import "ios/chrome/browser/search_engines/model/search_engine_choice_service_factory.h"
@@ -330,6 +331,7 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   SupervisedUserSettingsServiceFactory::GetInstance();
   SyncInvalidationsServiceFactory::GetInstance();
   SyncServiceFactory::GetInstance();
+  TabGroupServiceFactory::GetInstance();
   TabsSearchServiceFactory::GetInstance();
   TailoredSecurityServiceFactory::GetInstance();
   TextClassifierModelServiceFactory::GetInstance();
