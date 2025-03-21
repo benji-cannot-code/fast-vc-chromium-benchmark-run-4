dba7cf0435eb4edebdd4a510cf69ccc9f3a81b0d
Morten Stenshorne <mstensho@chromium.org>
2025-03-21 11:27:44 -0700

    Create ::column pseudo elements correctly with multiple rows.
    This was failing because we started enumerating columns at 0 for every
    row. Fix this by using the sequence numbers from break tokens, which
    already counts columns nicely for us.
    
    This fixes issues with both column spanners and column wrapping.
    
    Enumeration of columns should now also work if the multicol container is
    nested inside another fragmentation context, but other parts of the code
    aren't ready for this yet.
    
    Bug: 403183884
    Change-Id: I49c6a7e927c69c1d011f129072adddac2570c32e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382015
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436176}

--- third_party/blink/renderer/core/layout/column_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/column_layout_algorithm.cc
@@ -1113,6 +1113,9 @@ const LayoutResult* ColumnLayoutAlgorithm::LayoutRow(
       Node().GetDocument().GetStyleEngine());
 
   wtf_size_t num_columns = 0u;
+  if (IsBreakInside(next_column_token)) {
+    num_columns = next_column_token->SequenceNumber() + 1;
+  }
   // Commit all column fragments to the fragment builder.
   for (auto result_with_offset : new_columns) {
     const PhysicalBoxFragment& column = result_with_offset.Fragment();
@@ -1140,7 +1143,9 @@ const LayoutResult* ColumnLayoutAlgorithm::LayoutRow(
   // If there were superfluous ::column pseudo-elements from the previous pass,
   // remove the superfluous ones. This happens when the number of columns
   // decreases.
-  element->ClearColumnPseudoElements(num_columns);
+  if (!result->GetPhysicalFragment().GetBreakToken()) {
+    element->ClearColumnPseudoElements(num_columns);
+  }
 
   if (min_break_appeal)
     container_builder_.ClampBreakAppeal(*min_break_appeal);
