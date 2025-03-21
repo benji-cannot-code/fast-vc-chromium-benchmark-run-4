13bfe093ddaf132e9bf9a9cae08ec8f2420fc63b
Lukasz Anforowicz <lukasza@chromium.org>
2025-03-20 20:01:09 -0700

    [rust] Remove unused `rstest_reuse` crate.
    The only dependency on the crate has been removed some time ago in
    https://crrev.com/c/6188123/2/third_party/beto-core/BUILD.gn
    
    The removed version of `rstest_reuse` depends on an old version of the
    `getrandom` crate and therefore this CL helps a little bit with
    https://crbug.com/404598090.
    
    Bug: 404598090
    Change-Id: I3f54d95cca7b3ebdbb9573ae028425fae53cf21c
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378867
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435837}

--- third_party/rust/chromium_crates_io/vendor/rstest_reuse-0.7.0/tests/resources/export_template_root.rs
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-pub use rstest_reuse;
-use rstest_reuse::template;
-
-#[template]
-#[export]
-#[rstest]
-#[case("bar")]
-fn root_level(#[case] s: &str) {}
--- third_party/rust/chromium_crates_io/vendor/rstest_reuse-0.7.0/tests/resources/not_used.rs
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-mod foo {
-    use rstest_reuse::template;
-
-    #[template]
-    #[rstest]
-    #[case("bar")]
-    fn not_used(#[case] s: &str) {}
-}
