e28da7481979ef2c81bafb4c6fd70d5d1b886938
Andrew Grieve <agrieve@chromium.org>
2025-03-21 10:48:33 -0700

    Fix Nullity of ObserverList
    Bug: 389129271
    Change-Id: I9d22dbc1795dabe19101d7deda4658a2e8e605a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382596
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436150}

--- components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetControllerImpl.java
+++ components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetControllerImpl.java
@@ -80,6 +80,14 @@ class BottomSheetControllerImpl implements ManagedBottomSheetController, ScrimCo
     private final KeyboardVisibilityDelegate mKeyboardVisibilityDelegate;
     private final @Nullable DesktopWindowStateManager mDesktopWindowStateManager;
 
+    /**
+     * An observer that observes changes to the bottom sheet content {@code
+     * BottomSheetContent#mBackPressStateChangedSupplier} and updates the {@code
+     * BottomSheetControllerImpl#mBackPressStateChangedSupplier}.
+     */
+    private final Callback<Boolean> mContentBackPressStateChangedObserver =
+            contentWillHandleBackPress -> updateBackPressStateChangedSupplier();
+
     /** A handle to the {@link BottomSheet} that this class controls. */
     private @MonotonicNonNull BottomSheet mBottomSheet;
 
@@ -106,13 +114,6 @@ class BottomSheetControllerImpl implements ManagedBottomSheetController, ScrimCo
 
     /** The content being shown prior to the sheet being suppressed. */
     private @Nullable BottomSheetContent mContentWhenSuppressed;
-
-    /**
-     * An observer that observes changes to the bottom sheet content {@code
-     * BottomSheetContent#mBackPressStateChangedSupplier} and updates the {@code
-     * BottomSheetControllerImpl#mBackPressStateChangedSupplier}.
-     */
-    private @Nullable Callback<Boolean> mContentBackPressStateChangedObserver;
 
     private int mAppHeaderHeight;
     private int mBottomControlsHeight;
@@ -655,8 +656,6 @@ class BottomSheetControllerImpl implements ManagedBottomSheetController, ScrimCo
         }
         if (nextContent != null) {
             mKeyboardVisibilityDelegate.hideKeyboard(mBottomSheetContainer);
-            mContentBackPressStateChangedObserver =
-                    (contentWillHandleBackPress) -> updateBackPressStateChangedSupplier();
             nextContent
                     .getBackPressStateChangedSupplier()
                     .addObserver(mContentBackPressStateChangedObserver);
