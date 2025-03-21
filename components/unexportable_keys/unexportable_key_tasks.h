48adc773a3b55b9035dc9a2373ccc07582c23391
Alex Ilin <alexilin@chromium.org>
2025-03-21 11:47:56 -0700

    [DBSC] Refactor TPM operation result post-processing
    This CL moves the code that transforms the results of TPM operations
    from the UI to the background thread, for the code simplification.
    
    Bug: 400903525
    Change-Id: Iad5a892e90dcdf331d6f48d1fd231287f1b821ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6375248
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436195}

--- components/unexportable_keys/unexportable_key_tasks.h
+++ components/unexportable_keys/unexportable_key_tasks.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "components/unexportable_keys/background_task_impl.h"
 #include "components/unexportable_keys/background_task_priority.h"
+#include "components/unexportable_keys/service_error.h"
 #include "crypto/signature_verifier.h"
 
 namespace crypto {
@@ -27,8 +28,9 @@ namespace unexportable_keys {
 class RefCountedUnexportableSigningKey;
 
 // A `BackgroundTask` to generate a new `crypto::UnexportableSigningKey`.
-class GenerateKeyTask : public internal::BackgroundTaskImpl<
-                            std::unique_ptr<crypto::UnexportableSigningKey>> {
+class GenerateKeyTask
+    : public internal::BackgroundTaskImpl<
+          ServiceErrorOr<scoped_refptr<RefCountedUnexportableSigningKey>>> {
  public:
   GenerateKeyTask(
       std::unique_ptr<crypto::UnexportableKeyProvider> key_provider,
@@ -42,7 +44,7 @@ class GenerateKeyTask : public internal::BackgroundTaskImpl<
 // wrapped key.
 class FromWrappedKeyTask
     : public internal::BackgroundTaskImpl<
-          std::unique_ptr<crypto::UnexportableSigningKey>> {
+          ServiceErrorOr<scoped_refptr<RefCountedUnexportableSigningKey>>> {
  public:
   FromWrappedKeyTask(
       std::unique_ptr<crypto::UnexportableKeyProvider> key_provider,
@@ -52,8 +54,8 @@ class FromWrappedKeyTask
 };
 
 // A `BackgroundTask` to sign data with `crypto::UnexportableSigningKey`.
-class SignTask
-    : public internal::BackgroundTaskImpl<std::optional<std::vector<uint8_t>>> {
+class SignTask : public internal::BackgroundTaskImpl<
+                     ServiceErrorOr<std::vector<uint8_t>>> {
  public:
   SignTask(scoped_refptr<RefCountedUnexportableSigningKey> signing_key,
            base::span<const uint8_t> data,
@@ -63,7 +65,7 @@ class SignTask
 
  protected:
   bool ShouldRetryBasedOnResult(
-      const std::optional<std::vector<uint8_t>>& result) const override;
+      const ServiceErrorOr<std::vector<uint8_t>>& result) const override;
 };
 
 }  // namespace unexportable_keys
