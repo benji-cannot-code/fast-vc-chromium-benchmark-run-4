774b939a8b545f862cdac407cfe54ea3964475f5
James Cook <jamescook@chromium.org>
2025-03-21 12:38:08 -0700

    extensions: More support for CRX install on desktop Android
    This CL plus another one that adds drag-and-drop support to
    chrome://extensions make it possible to install .crx files
    on desktop Android.
    
    Bug: 396722906, 398299722
    Change-Id: I66791400801259a009cdd8ab0938792347085acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6371981
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436232}

--- chrome/browser/extensions/keyed_services/chrome_browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/keyed_services/chrome_browser_context_keyed_service_factories.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/error_console/error_console_factory.h"
 #include "chrome/browser/extensions/extension_allowlist_factory.h"
 #include "chrome/browser/extensions/extension_web_ui_override_registrar.h"
+#include "chrome/browser/extensions/forced_extensions/install_stage_tracker_factory.h"
+#include "chrome/browser/extensions/install_tracker_factory.h"
 #include "chrome/browser/extensions/pending_extension_manager_factory.h"
 #include "chrome/browser/extensions/permissions/permissions_updater.h"
 
@@ -29,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_sync_service_factory.h"
 #include "chrome/browser/extensions/extension_web_ui_override_registrar.h"
 #include "chrome/browser/extensions/external_provider_manager_factory.h"
-#include "chrome/browser/extensions/install_tracker_factory.h"
 #include "chrome/browser/extensions/install_verifier_factory.h"
 #include "chrome/browser/extensions/manifest_v2_experiment_manager.h"
 #include "chrome/browser/extensions/menu_manager_factory.h"
@@ -50,6 +51,8 @@ void EnsureChromeBrowserContextKeyedServiceFactoriesBuilt() {
   extensions::ErrorConsoleFactory::GetInstance();
   extensions::ExtensionAllowlistFactory::GetInstance();
   extensions::ExtensionWebUIOverrideRegistrar::GetFactoryInstance();
+  extensions::InstallStageTrackerFactory::GetInstance();
+  extensions::InstallTrackerFactory::GetInstance();
   extensions::PendingExtensionManagerFactory::GetInstance();
   extensions::PermissionsUpdater::EnsureAssociatedFactoryBuilt();
 
@@ -68,7 +71,6 @@ void EnsureChromeBrowserContextKeyedServiceFactoriesBuilt() {
   extensions::ExtensionManagementFactory::GetInstance();
   extensions::ExternalProviderManagerFactory::GetInstance();
   extensions::ChromeExtensionSystemFactory::GetInstance();
-  extensions::InstallTrackerFactory::GetInstance();
   extensions::InstallVerifierFactory::GetInstance();
   extensions::ManifestV2ExperimentManager::GetFactory();
   extensions::MenuManagerFactory::GetInstance();
