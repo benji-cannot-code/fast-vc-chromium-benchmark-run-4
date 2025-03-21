0b0b2b1f0e07dcf7ec95afd7ee20da05d3d31cec
Marc Treib <treib@chromium.org>
2025-03-21 05:53:28 -0700

    [iOS] AuthenticationFlowInProfile: Error handling, part 1
    If signing in fails because the to-be-signed-in account wasn't valid,
    AuthenticationFlowInProfile now switches back to the personal profile.
    
    Still to be done, left for a followup CL: Also show an error message to
    the user in this case.
    
    Bug: 375605482, 403183877
    Change-Id: I1b2f3d003cd3b0644bdc8139e246d04c9095faee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6375288
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435985}

--- ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_in_profile.mm
+++ ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_in_profile.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/service/sync_service.h"
 #import "components/sync/service/sync_user_settings.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/app/change_profile_commands.h"
 #import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_performer.h"
 #import "ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_performer_delegate.h"
@@ -20,7 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/profile/profile_attributes_storage_ios.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
+#import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
@@ -37,6 +41,7 @@ enum class AuthenticationFlowInProfileState {
   kFetchUserPolicyIfNeeded,
   kFetchCapabilitiesIfNeeded,
   kCompletionWithSuccess,
+  kSwitchBackToPersonalProfileIfNeeded,
   kCompletionWithFailure,
   kCleanupBeforeDone,
   kDone,
@@ -50,6 +55,7 @@ enum class AuthenticationFlowInProfileState {
 @implementation AuthenticationFlowInProfile {
   // State machine tracking for sign-in flow.
   AuthenticationFlowInProfileState _state;
+  BOOL _didSignIn;
   // This AuthenticationFlowInProfile keeps a reference to `self` while a
   // sign-in flow is is in progress to ensure it outlives until the last step.
   AuthenticationFlowInProfile* _selfRetainer;
@@ -103,8 +109,12 @@ enum class AuthenticationFlowInProfileState {
   CHECK(completion);
   _selfRetainer = self;
   _signInCompletion = completion;
-  _performer = [[AuthenticationFlowPerformer alloc] initWithDelegate:self
-                                                changeProfileHandler:nil];
+  id<ChangeProfileCommands> changeProfileHandler = HandlerForProtocol(
+      _browser->GetSceneState().profileState.appState.appCommandDispatcher,
+      ChangeProfileCommands);
+  _performer = [[AuthenticationFlowPerformer alloc]
+          initWithDelegate:self
+      changeProfileHandler:changeProfileHandler];
   // Make sure -[AuthenticationFlow startSignInWithCompletion:] doesn't call
   // the completion block synchronously.
   // Related to http://crbug.com/1246480.
@@ -152,9 +162,14 @@ enum class AuthenticationFlowInProfileState {
     case AuthenticationFlowInProfileState::kRegisterForUserPolicyIfNeeded:
     case AuthenticationFlowInProfileState::kFetchUserPolicyIfNeeded:
     case AuthenticationFlowInProfileState::kFetchCapabilitiesIfNeeded:
+      return AuthenticationFlowInProfileState::
+          kSwitchBackToPersonalProfileIfNeeded;
+    case AuthenticationFlowInProfileState::kCompletionWithSuccess:
+      // This state should not be reached in error cases.
+      NOTREACHED();
+    case AuthenticationFlowInProfileState::kSwitchBackToPersonalProfileIfNeeded:
       return AuthenticationFlowInProfileState::kCompletionWithFailure;
     case AuthenticationFlowInProfileState::kCompletionWithFailure:
-    case AuthenticationFlowInProfileState::kCompletionWithSuccess:
       return AuthenticationFlowInProfileState::kCleanupBeforeDone;
     case AuthenticationFlowInProfileState::kCleanupBeforeDone:
     case AuthenticationFlowInProfileState::kDone:
@@ -180,8 +195,12 @@ enum class AuthenticationFlowInProfileState {
     case AuthenticationFlowInProfileState::kFetchCapabilitiesIfNeeded:
       return AuthenticationFlowInProfileState::kCompletionWithSuccess;
     case AuthenticationFlowInProfileState::kCompletionWithSuccess:
-    case AuthenticationFlowInProfileState::kCompletionWithFailure:
       return AuthenticationFlowInProfileState::kCleanupBeforeDone;
+    case AuthenticationFlowInProfileState::kSwitchBackToPersonalProfileIfNeeded:
+    case AuthenticationFlowInProfileState::kCompletionWithFailure:
+      // These states should not be reached because `error_` should be set in
+      // those cases.
+      NOTREACHED();
     case AuthenticationFlowInProfileState::kCleanupBeforeDone:
     case AuthenticationFlowInProfileState::kDone:
       return AuthenticationFlowInProfileState::kDone;
@@ -210,6 +229,9 @@ enum class AuthenticationFlowInProfileState {
       return;
     case AuthenticationFlowInProfileState::kCompletionWithSuccess:
       [self successCompleteFlowStep];
+      return;
+    case AuthenticationFlowInProfileState::kSwitchBackToPersonalProfileIfNeeded:
+      [self switchBackToPersonalProfileIfNeededStep];
       return;
     case AuthenticationFlowInProfileState::kCompletionWithFailure:
       [self failureCompleteFlowStep];
@@ -251,6 +273,8 @@ enum class AuthenticationFlowInProfileState {
                      &CoreAccountInfo::gaia);
   if (!isValidIdentityInProfile) {
     _error = YES;
+    // TODO(crbug.com/375605482): Show an error to the user via
+    // `[_performer showAuthenticationError:...]`.
     [self continueFlow];
     return;
   }
@@ -262,6 +286,7 @@ enum class AuthenticationFlowInProfileState {
     [_performer signInIdentity:_identityToSignIn
                  atAccessPoint:_accessPoint
                 currentProfile:profile];
+    _didSignIn = YES;
   } else {
     CHECK([currentIdentity isEqual:_identityToSignIn],
           base::NotFatalUntil::M138);
@@ -335,10 +360,34 @@ enum class AuthenticationFlowInProfileState {
   [self continueFlow];
 }
 
+- (void)switchBackToPersonalProfileIfNeededStep {
+  // Note: It's theoretically possible that the originating profile was not the
+  // personal one, but rather another managed profile. In that case, switching
+  // back to that managed profile would be "more correct". However, that would
+  // be significantly more complicated (e.g. what if that profile doesn't exist
+  // anymore), and this is a supposedly-impossible error case anyway.
+  std::string personalProfileName = GetApplicationContext()
+                                        ->GetProfileManager()
+                                        ->GetProfileAttributesStorage()
+                                        ->GetPersonalProfileName();
+  bool inPersonalProfile =
+      personalProfileName == [self originalProfile]->GetProfileName();
+  if (inPersonalProfile) {
+    // Already in the personal profile, no switching necessary.
+    [self continueFlow];
+    return;
+  }
+  SceneState* sceneState = _browser->GetSceneState();
+  [_performer switchToProfileWithName:personalProfileName
+                           sceneState:sceneState];
+}
+
 - (void)failureCompleteFlowStep {
-  // TODO(crbug.com/375605482): Need to switch back to the personal profile
-  // (if the current profile is a managed profile), sign out (if the personal
-  // profile is signed in), and display an error.
+  // None of the steps after signin can fail. If any failable steps after the
+  // signin step get added in the future, then a call to
+  // `[_performer signOutImmediatelyFromProfile:...]` should be added here.
+  CHECK(!_didSignIn);
+
   signin_ui::SigninCompletionCallback signInCompletion = _signInCompletion;
   _signInCompletion = nil;
   // If the sign-in failed, the result is `SigninCoordinatorResultInterrupted`.
@@ -404,7 +453,13 @@ enum class AuthenticationFlowInProfileState {
 
 - (void)didSwitchToProfileWithSuccess:(BOOL)success
                     newProfileBrowser:(Browser*)newProfileBrowser {
-  NOTREACHED();
+  // This class only ever switches (back) to the personal profile, which should
+  // never fail.
+  CHECK(success);
+  CHECK(newProfileBrowser);
+
+  // After the profile switch, `_browser` is not valid anymore.
+  _browser = nullptr;
 }
 
 - (void)didRegisterForUserPolicyWithDMToken:(NSString*)dmToken
--- ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_in_profile_unittest.mm
+++ ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_in_profile_unittest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/fake_authentication_service_delegate.h"
@@ -39,7 +40,7 @@ class AuthenticationFlowInProfileTest : public PlatformTest {
         AuthenticationServiceFactory::GetInstance(),
         AuthenticationServiceFactory::GetFactoryWithDelegate(
             std::make_unique<FakeAuthenticationServiceDelegate>()));
-    profile_ = std::move(builder).Build();
+    profile_ = profile_manager_.AddProfileWithBuilder(std::move(builder));
     browser_ = std::make_unique<TestBrowser>(profile_.get());
 
     FakeSystemIdentityManager* fake_system_identity_manager =
@@ -76,7 +77,7 @@ class AuthenticationFlowInProfileTest : public PlatformTest {
     id<AuthenticationFlowPerformerDelegate> performer_delegate =
         GetAuthenticationFlowPerformerDelegate();
     OCMExpect([performer_mock_ initWithDelegate:performer_delegate
-                           changeProfileHandler:nil])
+                           changeProfileHandler:[OCMArg any]])
         .andReturn(performer_mock_);
   }
 
@@ -88,9 +89,10 @@ class AuthenticationFlowInProfileTest : public PlatformTest {
 
   web::WebTaskEnvironment task_environment_;
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
-  AuthenticationFlowInProfile* authentication_flow_in_profile_ = nil;
-  std::unique_ptr<TestProfileIOS> profile_;
+  TestProfileManagerIOS profile_manager_;
+  raw_ptr<TestProfileIOS> profile_;
   std::unique_ptr<Browser> browser_;
+  AuthenticationFlowInProfile* authentication_flow_in_profile_ = nil;
   id<SystemIdentity> identity1_ = nil;
   id<SystemIdentity> identity2_ = nil;
   id<SystemIdentity> managed_identity_ = nil;
