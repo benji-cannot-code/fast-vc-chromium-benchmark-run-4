8841a9373febaaca46b54c8f139fc99602630bf9
Michael Cui <mlcui@google.com>
2025-03-20 17:36:29 -0700

    sunfish/scanner: Adjust home button icon when enabled
    See attached bug for more details. This uses the same logic and icon as
    Assistant being available.
    
    Bug: b:402237553
    Change-Id: I6a6a2100eaae61971b52c4e09190689a7c56aff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6365737
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435790}

--- ash/shelf/home_button_controller.cc
+++ ash/shelf/home_button_controller.cc
@@ -130,8 +130,7 @@ bool HomeButtonController::MaybeHandleGestureEvent(ui::GestureEvent* event) {
 }
 
 bool HomeButtonController::IsLongPressActionAvailable() {
-  // TODO: crbug.com/402237553 - Call `IsSunfishOrScannerAvailable` here.
-  return IsAssistantAvailable();
+  return IsAssistantAvailable() || IsSunfishOrScannerAvailable();
 }
 
 bool HomeButtonController::IsAssistantVisible() {
@@ -186,7 +185,7 @@ void HomeButtonController::OnUiVisibilityChanged(
 
 void HomeButtonController::OnSunfishScannerFeatureStatesChanged(
     SunfishScannerFeatureWatcher& source) {
-  // TODO: crbug.com/402237553 - Update the button's icon here.
+  button_->OnIconUpdated();
 }
 
 void HomeButtonController::StartAssistantAnimation() {
