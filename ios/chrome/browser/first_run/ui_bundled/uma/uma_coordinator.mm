a7379b16988c2618c8c38115f5da6e56e8e94d0e
Benjamin Williams <bwwilliams@google.com>
2025-03-21 11:32:58 -0700

    [iOS][Code Health] Fix UISheetPresentationControllerDetent syntax
    This CL corrects improper instances of property-style access for detents
    to proper method call syntax.
    
    Impact:
    
    - No functional changes to behavior
    - Improves code health by using the correct Objective-C method
      invocation syntax
    
    This CL was uploaded by git cl split.
    
    R=jlebel@chromium.org
    
    Bug: 402705893
    Change-Id: Ib500f004a27b829c4b991472923d487feffdb6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368915
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436180}

--- ios/chrome/browser/first_run/ui_bundled/uma/uma_coordinator.mm
+++ ios/chrome/browser/first_run/ui_bundled/uma/uma_coordinator.mm
@@ -47,8 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       navigationController.sheetPresentationController;
   presentationController.prefersEdgeAttachedInCompactHeight = YES;
   presentationController.detents = @[
-    UISheetPresentationControllerDetent.mediumDetent,
-    UISheetPresentationControllerDetent.largeDetent
+    [UISheetPresentationControllerDetent mediumDetent],
+    [UISheetPresentationControllerDetent largeDetent]
   ];
   [self.baseViewController presentViewController:navigationController
                                         animated:YES
