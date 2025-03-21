428996a721ae614038017308a12b44a80987c40a
Tomasz Wiszkowski <ender@google.com>
2025-03-21 10:01:29 -0700

    Allow json-to-struct generate fixed_flat_maps
    This functionality will be used to generate regional configuration
    from a JSON file.
    
    The change replaces struct initialization with `{0}` with a simple `{}`
    because `{0}` assumes the first field is an integer - if it is not,
    the resulting file fails to compile.
    
    The change modernizes (deprecated) `assertEquals()` unittest calls,
    replacing them with their updated `assertEqual()` equivalent.
    
    This component has no owners - I would gladly volunteer to become one
    as I will need to do more changes around here anyway.
    
    Bug: b:404850650
    Change-Id: I0261a649110efe43b0d803f8092bc4e267a94579
    See: https://crrev.com/c/6373574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6373573
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436116}

---
+++ tools/json_to_struct/aggregation_test.py
@@ -0,0 +1,82 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from aggregation import AggregationKind, AggregationDetails, GetAggregationDetails
+import unittest
+
+
+class AggregationTest(unittest.TestCase):
+
+  def testDefaults(self):
+    self.assertEqual(GetAggregationDetails({}),
+                     AggregationDetails(AggregationKind.NONE, None, True))
+
+  def testGenerateOldStyleArray(self):
+    self.assertEqual(
+        GetAggregationDetails({"generate_array": {
+            "array_name": "TestArray"
+        }}), AggregationDetails(AggregationKind.ARRAY, "TestArray", True))
+
+  def testGenerateArray(self):
+    self.assertEqual(
+        GetAggregationDetails(
+            {"aggregation": {
+                "type": "array",
+                "name": "TestArray"
+            }}), AggregationDetails(AggregationKind.ARRAY, "TestArray", True))
+
+  def testGenerateArrayAndHideElements(self):
+    self.assertEqual(
+        GetAggregationDetails({
+            "aggregation": {
+                "type": "array",
+                "name": "TestArray",
+                "export_items": False
+            }
+        }), AggregationDetails(AggregationKind.ARRAY, "TestArray", False))
+
+  def testGenerateMap(self):
+    self.assertEqual(
+        GetAggregationDetails(
+            {"aggregation": {
+                "type": "map",
+                "name": "TestMap"
+            }}), AggregationDetails(AggregationKind.MAP, "TestMap", True))
+
+  def testGenerateMapAndHideElements(self):
+    self.assertEqual(
+        GetAggregationDetails({
+            "aggregation": {
+                "type": "map",
+                "name": "TestMap",
+                "export_items": False
+            }
+        }), AggregationDetails(AggregationKind.MAP, "TestMap", False))
+
+  def testUnrecognizedAggregationType(self):
+    self.assertRaisesRegex(
+        Exception, "'collection' is not a valid AggregationKind",
+        lambda: GetAggregationDetails({"aggregation": {
+            "type": "collection",
+        }}))
+
+  def testArrayMustHaveAName(self):
+    self.assertRaisesRegex(
+        Exception, "Aggregation container needs a `name`.",
+        lambda: GetAggregationDetails({"aggregation": {
+            "type": "array",
+        }}))
+
+  def testMapMustHaveAName(self):
+    self.assertRaisesRegex(
+        Exception, "Aggregation container needs a `name`.",
+        lambda: GetAggregationDetails({"aggregation": {
+            "type": "map",
+        }}))
+
+
+if __name__ == '__main__':
+  unittest.main()
