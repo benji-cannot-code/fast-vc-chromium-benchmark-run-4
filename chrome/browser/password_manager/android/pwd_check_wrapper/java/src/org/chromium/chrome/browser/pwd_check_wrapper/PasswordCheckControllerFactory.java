a89bc80671024a4f91fef9d3125a1d91dadf3acc
Ioana Pandele <ioanap@chromium.org>
2025-03-21 05:25:31 -0700

    [UPMLoginDbSunset] Update SafetyCheck UPM checks
    This surface is used only from the PhishGuard dialog, in case the
    phished credential is saved in both the local and account store.
    
    Concomitant usage of the 2 stores is only possible if UPM is fully
    available, so the checks for UPM availability are no longer needed.
    
    The changes are made under the flag for the login DB deprecation, also
    because this feature is deprecating old UPM versions, making the
    previous UPM checks inaccurate.
    
    Bug: 397186266
    Change-Id: I82efe77763fe61a28c84bcbe504754e0b0a14a94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368816
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435980}

--- chrome/browser/password_manager/android/pwd_check_wrapper/java/src/org/chromium/chrome/browser/pwd_check_wrapper/PasswordCheckControllerFactory.java
+++ chrome/browser/password_manager/android/pwd_check_wrapper/java/src/org/chromium/chrome/browser/pwd_check_wrapper/PasswordCheckControllerFactory.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.pwd_check_wrapper;
 
+import static org.chromium.chrome.browser.flags.ChromeFeatureList.LOGIN_DB_DEPRECATION_ANDROID;
+
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.password_manager.PasswordManagerHelper;
 import org.chromium.chrome.browser.password_manager.PasswordManagerUtilBridge;
 import org.chromium.chrome.browser.password_manager.PasswordStoreBridge;
@@ -17,6 +20,14 @@ public class PasswordCheckControllerFactory {
             PrefService prefService,
             PasswordStoreBridge passwordStoreBridge,
             PasswordManagerHelper passwordManagerHelper) {
+        if (ChromeFeatureList.isEnabled(LOGIN_DB_DEPRECATION_ANDROID)) {
+            // This is only used by the old Safety Check, which is only opened from the PhishGuard
+            // dialog and only if the phished credential is saved in both local and account stores.
+            // This means that UPM is completely available.
+            assert PasswordManagerUtilBridge.isPasswordManagerAvailable(prefService);
+            return new GmsCorePasswordCheckController(
+                    syncService, prefService, passwordStoreBridge, passwordManagerHelper);
+        }
         if (passwordManagerHelper.canUseUpm()
                 || PasswordManagerUtilBridge.isGmsCoreUpdateRequired(prefService, syncService)) {
             return new GmsCorePasswordCheckController(
