f30c89b59044a3d02ab9e672a1d6fcb95c2bfd0c
Fiaz Muhammad <mfiaz@google.com>
2025-03-21 11:53:05 -0700

    [Tab Group Parity] Generalize AppMenuDismissTimer for Reuse
    - Rename class to RunnableTimer
    - Update javadocs, imports, etc.
    
    Bug: 405132264
    Change-Id: Ic539940be340aaf0c759dae0926529caacdeee50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6377823
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436199}

--- ui/android/junit/src/org/chromium/ui/util/RunnableTimerTest.java
+++ ui/android/junit/src/org/chromium/ui/util/RunnableTimerTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.components.messages;
+package org.chromium.ui.util;
 
 import static org.mockito.Mockito.verify;
 
@@ -20,16 +20,16 @@ import org.robolectric.Robolectric;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.CallbackHelper;
 
-/** Unit tests for {@link MessageAutoDismissTimer}. */
+/** Unit tests for {@link RunnableTimer}. */
 @RunWith(BaseRobolectricTestRunner.class)
-public class MessageAutoDismissTimerTest {
+public class RunnableTimerTest {
     /** Ensure Runnable has been called by an active timer. */
     @Test
     @SmallTest
     public void testStartTimer() throws Exception {
         CallbackHelper callbackHelper = new CallbackHelper();
         long duration = 1;
-        MessageAutoDismissTimer timer = new MessageAutoDismissTimer();
+        RunnableTimer timer = new RunnableTimer();
         timer.startTimer(duration, callbackHelper::notifyCalled);
         // Not flushing will make the looper blocked.
         Robolectric.flushForegroundThreadScheduler();
@@ -42,7 +42,7 @@ public class MessageAutoDismissTimerTest {
     public void testCancelTimer() {
         Handler h = Mockito.mock(Handler.class);
         long duration = 1;
-        MessageAutoDismissTimer timer = new MessageAutoDismissTimer();
+        RunnableTimer timer = new RunnableTimer();
         timer.setHandlerForTesting(h);
         Runnable r = () -> {};
         timer.startTimer(duration, r);
@@ -62,7 +62,7 @@ public class MessageAutoDismissTimerTest {
     public void testResetTimer() {
         Handler h = Mockito.mock(Handler.class);
         long duration = 1;
-        MessageAutoDismissTimer timer = new MessageAutoDismissTimer();
+        RunnableTimer timer = new RunnableTimer();
         timer.setHandlerForTesting(h);
         Runnable r = () -> {};
         timer.startTimer(duration, r);
