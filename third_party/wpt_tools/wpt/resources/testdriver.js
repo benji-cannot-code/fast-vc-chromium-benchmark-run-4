8e9110882fdce721d8419938636e66ad3563e485
Russ Hamilton <behamilton@google.com>
2025-03-21 15:21:49 -0700

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 24b0a3ae223a95ad7c3f11e42cb74018ad9c3ffe
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ia3c03a14160774f56626bf791025e8fdd1cf4dff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383397
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436325}

--- third_party/wpt_tools/wpt/resources/testdriver.js
+++ third_party/wpt_tools/wpt/resources/testdriver.js
@@ -1429,7 +1429,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          * Causes a virtual pressure source to report a new reading.
          *
          * Matches the `Update virtual pressure source
-         * <https://w3c.github.io/compute-pressure/#update-virtual-pressure-source>`_
+         * <https://w3c.github.io/compute-pressure/?experimental=1#update-virtual-pressure-source>`_
          * WebDriver command.
          *
          * @param {String} source_type - A `virtual pressure source type
@@ -1438,6 +1438,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          * @param {String} sample - A `virtual pressure state
          *                          <https://w3c.github.io/compute-pressure/#dom-pressurestate>`_
          *                          such as "critical".
+         * @param {number} estimate - Optional, A `virtual own contribution estimate`
+         *                          <https://w3c.github.io/compute-pressure/?experimental=1#the-owncontributionestimate-attribute>`_
+
          * @param {WindowProxy} [context=null] - Browsing context in which to
          *                                       run the call, or null for the
          *                                       current browsing context.
@@ -1448,8 +1451,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          *                    virtual pressure source of the given type does not
          *                    exist).
          */
-        update_virtual_pressure_source: function(source_type, sample, context=null) {
-            return window.test_driver_internal.update_virtual_pressure_source(source_type, sample, context);
+        update_virtual_pressure_source: function(source_type, sample, estimate, context=null) {
+            return window.test_driver_internal.update_virtual_pressure_source(source_type, sample, estimate, context);
         },
 
         /**
@@ -1473,6 +1476,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          */
         remove_virtual_pressure_source: function(source_type, context=null) {
             return window.test_driver_internal.remove_virtual_pressure_source(source_type, context);
+        },
+
+        /**
+         * Sets which hashes are considered k-anonymous for the Protected
+         * Audience interest group with specified `owner` and `name`.
+         *
+         * Matches the `Set Protected Audience K-Anonymity
+         * <https://wicg.github.io/turtledove/#sctn-automation-set-protected-audience-k-anonymity>
+         * WebDriver command.
+         *
+         *  @param {String} owner - Origin of the owner of the interest group
+         *                          to modify
+         *  @param {String} name -  Name of the interest group to modify
+         *  @param {Array} hashes - An array of strings, each of which is a
+         *                          base64 ecoded hash to consider k-anonymous.
+         *
+         *  @returns {Promise} Fulfilled after the k-anonymity status for the
+         *                     specified Protected Audience interest group has
+         *                     been updated.
+         *
+         */
+        set_protected_audience_k_anonymity: function(owner, name, hashes, context = null) {
+            return window.test_driver_internal.set_protected_audience_k_anonymity(owner, name, hashes, context);
         }
     };
 
@@ -1724,12 +1750,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             throw new Error("create_virtual_pressure_source() is not implemented by testdriver-vendor.js");
         },
 
-        async update_virtual_pressure_source(source_type, sample, context=null) {
+        async update_virtual_pressure_source(source_type, sample, estimate, context=null) {
             throw new Error("update_virtual_pressure_source() is not implemented by testdriver-vendor.js");
         },
 
         async remove_virtual_pressure_source(source_type, context=null) {
             throw new Error("remove_virtual_pressure_source() is not implemented by testdriver-vendor.js");
+        },
+
+        async set_protected_audience_k_anonymity(owner, name, hashes, context=null) {
+            throw new Error("set_protected_audience_k_anonymity() is not implemented by testdriver-vendor.js");
         }
     };
 })();
