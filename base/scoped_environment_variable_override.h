81c08ce4f33660e81cf44f2f4927023d1f46b1fa
Helmut Januschka <helmut@januschka.com>
2025-03-21 15:44:57 -0700

    [base] Modernize Environment::GetVar to use std::optional
    Updates GetVar usage in //base to use the modern std::optional API
    instead of out-parameters.
    
    Bug: 400758498
    Change-Id: I525c188cf4a74cc3f960484b7da9035ac4e53c72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354402
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Cr-Commit-Position: refs/heads/main@{#1436336}

--- base/scoped_environment_variable_override.h
+++ base/scoped_environment_variable_override.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_SCOPED_ENVIRONMENT_VARIABLE_OVERRIDE_H_
 
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/base_export.h"
@@ -38,8 +39,7 @@ class BASE_EXPORT ScopedEnvironmentVariableOverride final {
   std::unique_ptr<Environment> environment_;
   std::string variable_name_;
   bool overridden_;
-  bool was_set_;
-  std::string old_value_;
+  std::optional<std::string> old_value_;
 };
 
 }  // namespace base
