3ec1df12b2e7a773e2ddaab468c52ed74496c9a6
Wenyu Fu <wenyufu@chromium.org>
2025-03-21 15:35:57 -0700

    [Theme] Add ThemeModuleUtils
    Add the interface ThemeOverlayProvider to supply an external theme
    overlay that's not defined in Chrome.
    
    Bug: 399504033
    Change-Id: I1f3a22e35c96d5ac3889ff664176654fe45d0b53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378883
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436332}

---
+++ chrome/browser/ui/android/theme/java/src/org/chromium/chrome/browser/theme/ThemeOverlayProvider.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.theme;
+
+import androidx.annotation.StyleRes;
+
+/** Interface that provides a theme overlay resource ID. */
+public interface ThemeOverlayProvider {
+    /** Get the theme overlay resource ID provided by the instance. */
+    @StyleRes
+    int getThemeOverlay();
+}
