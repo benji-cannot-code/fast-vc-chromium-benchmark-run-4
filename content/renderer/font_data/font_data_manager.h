f1191827b618671ffb41cc13a2610c37b198e2a9
Olivier Li <olivierli@chromium.org>
2025-03-21 07:38:19 -0700

    Create base::HashCombine.
    This helper function uses the existing HashInts[32,64] functions to
    provide the actual combination.
    
    This helper can be used to replace existing uses of hash combining
    scattered across the code base to reduce complexity.
    
    Change-Id: I971a6f717f11056fa64fdd833b7806b2fe8d76d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6369775
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436028}

--- content/renderer/font_data/font_data_manager.h
+++ content/renderer/font_data/font_data_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/lru_cache.h"
+#include "base/hash/hash.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/synchronization/lock.h"
 #include "base/task/single_thread_task_runner.h"
@@ -99,8 +100,8 @@ class CONTENT_EXPORT FontDataManager : public SkFontMgr {
   };
   struct MatchFamilyRequestHash {
     size_t operator()(const MatchFamilyRequest& key) const {
-      return std::hash<std::string>{}(key.name) ^ key.weight ^
-             (key.width << 8) ^ (key.slant << 16);
+      return base::HashCombine(0ull, key.name, key.weight, key.width,
+                               key.slant);
     }
   };
   struct MatchFamilyRequestEqual {
