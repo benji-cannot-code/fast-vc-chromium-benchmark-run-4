3d5a8f1bd58c3779e1c180dd34e7332e6522c3f4
Thiabaud Engelbrecht <thiabaud@google.com>
2025-03-20 21:06:17 -0700

    Reland "[metrics] Add PSS metrics on Linux/Android/ChromeOS"
    This reverts commit 3ba259a85d88ab34370f7ffb0caac5cd8f120d61.
    
    Reason for revert: Fixed broken unit tests
    
    Original change's description:
    > Revert "[metrics] Add PSS metrics on Linux/Android/ChromeOS"
    >
    > This reverts commit a7165d8e50c1940af508e7d956a74443d27d5f26.
    >
    > Reason for revert: This caused consistent failure in CQ see bug: crbug.com/404852444
    >
    > Original change's description:
    > > [metrics] Add PSS metrics on Linux/Android/ChromeOS
    > >
    > > This CL adds "Memory.<process>.{SwapPss,Pss}" metrics.
    > >
    > > Bug: 403588202
    > > Change-Id: Iaff82a1e594891eab1d4f891b5488e9d3211921d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6347824
    > > Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    > > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1434569}
    >
    > Bug: 403588202
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ic7be120d57d09bfddc554d664362bd6ada925ce9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372101
    > Auto-Submit: Zhe Li <zheliooo@google.com>
    > Owners-Override: Zhe Li <zheliooo@google.com>
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1434913}
    
    Bug: 403588202
    Change-Id: Ic6cb0e93fa0855c8830c0b8689cc2de3e6f98736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372971
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435847}

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_unittest.cc
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
 #include <sys/mman.h>
+#include <sys/utsname.h>
 #endif
 
 namespace memory_instrumentation {
@@ -131,6 +132,29 @@ void CreateTempFileWithContents(const char* contents, base::ScopedFILE* file) {
   ASSERT_TRUE(*file);
 
   ASSERT_TRUE(base::WriteFileDescriptor(fileno(file->get()), contents));
+}
+
+// SmapsRollup was added in Linux 4.14.
+bool IsSmapsRollupSupported() {
+  struct utsname info;
+  if (uname(&info) < 0) {
+    NOTREACHED();
+  }
+
+  int major, minor, patch;
+  if (sscanf(info.release, "%d.%d.%d", &major, &minor, &patch) < 3) {
+    NOTREACHED();
+  }
+
+  if (major > 4) {
+    return true;
+  }
+
+  if (major < 4 || minor < 14) {
+    return false;
+  }
+
+  return true;
 }
 
 }  // namespace
@@ -287,6 +311,22 @@ TEST(OSMetricsTest, CountMappings) {
   EXPECT_GT(dump.mappings_count, mappings_count);
 
   munmap(addr, kPageCount * page_size);
+}
+
+TEST(OSMetricsTest, Pss) {
+  // Some older Android devices may not support this, so skip the test in those
+  // cases.
+  if (!IsSmapsRollupSupported()) {
+    GTEST_SKIP() << "smaps_rollup not supported";
+  }
+
+  mojom::RawOSMemDump dump;
+  dump.platform_private_footprint = mojom::PlatformPrivateFootprint::New();
+  ASSERT_TRUE(OSMetrics::FillOSMemoryDump(base::kNullProcessHandle, &dump));
+  uint32_t pss = dump.pss_kb;
+
+  // We don't know the exact value here, but it should be greater than 0.
+  EXPECT_GT(pss, 0u);
 }
 
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) ||
