ffebc22fa7704a9079ad53ddc21cb9b930efe23b
Dylan Cutler <dylancutler@google.com>
2025-03-21 14:33:10 -0700

    Add WPTs for not dispatching cookiechange event when setting already-expired cookie
    Already expired cookies should not cause cookiechange events to be
    dispatched. This CL adds tests to verify this for HTTP cookies,
    document.cookie, and the CookieStore API.
    
    This CL only covers cookiechange events being not dispatched to
    listeners in the DOM, I will add tests for service workers in a
    separate CL.
    
    Bug: 405165851
    Change-Id: I023ca8cceb527228951382a1400a18d3dd7218e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378643
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436296}

---
+++ third_party/blink/web_tests/external/wpt/cookie-store/change_eventhandler_for_already_expired.https.window.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=Cookie Store API: Test that setting an already-expired cookie does not trigger an event.
+// META: script=resources/cookie-test-helpers.js
+
+'use strict';
+
+cookie_test(async t => {
+  const eventPromise = observeNextCookieChangeEvent();
+  await cookieStore.set({
+    name: 'cookie',
+    value: 'ALREADY-EXPIRED',
+    expires: new Date(new Date() - 10_000),
+  });
+  await cookieStore.set('alt-cookie', 'IGNORE');
+  assert_equals(
+    await getCookieString(),
+    'alt-cookie=IGNORE',
+    'Already-expired cookie not included in CookieStore');
+  await verifyCookieChangeEvent(
+    eventPromise,
+    {deleted: [], changed: [{name: 'alt-cookie', value: 'IGNORE'}]},
+    'Deletion not observed after document.cookie sets already-expired cookie');
+}, 'CookieStore setting already-expired cookie should not be observed');
