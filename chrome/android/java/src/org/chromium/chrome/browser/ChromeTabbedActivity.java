30b9f7597c27c4e829096b802b56ddec6fb8c8df
Gurmeet Kalra <gurmeetk@google.com>
2025-03-21 14:27:32 -0700

    [XR] Separate JXR session is maintained for each Chrome instance.
    * In multi instance cases each activity can switch to full space mode for the tabs overview in a spatial environment.
    * XrUtils is split to keep the JXR session with each Activity lifecycle.
    * The JXR library maintains a session per activity and manages to free its resources when the activity is destroyed.
    
    Recording on Moohan -
    https://drive.google.com/file/d/1AcXPn3cgfMba1bTgJHrqkPEPWmibM51G/view?usp=drive_link&resourcekey=0-xtCaU5lhsHwb914xkFembQ
    
    Test: Manually verified the tab tearing with pull down and initiate
    switching to full space mode in separate Chrome instances. Dragged the
    last tab to another instance.
    
    Low-Coverage-Reason: OTHER All the code changes are exercised on XR devices and do not impact any other Android devices. TESTS_IN_SEPARATE_CL A task 398916203 exists and is assigned.
    Bug: 402535108, 399711974
    Change-Id: Ida3206e8a0105e3abed73dc7e7b1eb9170b2fca9
    Fixed: 402535108, 399711974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6366714
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Gurmeet Kalra <gurmeetk@google.com>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436290}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -836,7 +836,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements MismatchedIn
                         adaptOnToolbarAlphaChange());
 
         // Set up layout state osberver for transitions between HSM and FSM on an XR device.
-        if (XrUtils.isXrDevice()) {
+        if (XrUtils.isXrDevice() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             mXrLayoutStateObserver =
                     new XrLayoutStateObserver(
                             this,
@@ -2393,10 +2393,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements MismatchedIn
         mDseNewTabUrlManager = new DseNewTabUrlManager(mTabModelProfileSupplier);
 
         initHub();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            XrUtils.getInstance().init(this);
-        }
     }
 
     @Override
@@ -3712,7 +3708,8 @@ public class ChromeTabbedActivity extends ChromeActivity implements MismatchedIn
             mDseNewTabUrlManager.destroy();
         }
 
-        if (mXrLayoutStateObserver != null) {
+        if (mXrLayoutStateObserver != null
+                && Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             mXrLayoutStateObserver.destroy();
             mXrLayoutStateObserver = null;
         }
