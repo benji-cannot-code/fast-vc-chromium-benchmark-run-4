551a70b7afe74382d6f2bf4edbaf57d403cc471d
Shakti Sahu <shaktisahu@chromium.org>
2025-03-21 17:11:31 -0700

    [Instant message] Add API for aggregated message
    Bug: 400794347
    Change-Id: I4f895e432ee6ccca96d3741e5081e6cd00a3f293
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6374663
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436373}

--- chrome/browser/ui/tabs/saved_tab_groups/instant_message_queue_processor_unittest.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/instant_message_queue_processor_unittest.cc
@@ -133,7 +133,7 @@ TEST_F(InstantMessageQueueProcessorTest,
   auto message = CreateMessage(CollaborationEvent::TAB_REMOVED);
 
   // Remove user so that toast cannot be created from message.
-  message.attribution.triggering_user = std::nullopt;
+  message.attribution->triggering_user = std::nullopt;
 
   base::MockCallback<SuccessCallback> callback;
 
---
+++ components/collaboration/public/android/java/src/org/chromium/components/collaboration/messaging/AggregatedMessageData.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.collaboration.messaging;
+
+import org.chromium.build.annotations.NullMarked;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a message metadata for an aggregated instant message. Contains the list of
+ * attributions for the individual messages that it represents. See
+ * //components/collaboration/public/messaging/message.h for specific details.
+ */
+@NullMarked
+public class AggregatedMessageData {
+    // List of message attributions that is being aggregated in the instant message.
+    public List<MessageAttribution> attributions = new ArrayList<>();
+}
