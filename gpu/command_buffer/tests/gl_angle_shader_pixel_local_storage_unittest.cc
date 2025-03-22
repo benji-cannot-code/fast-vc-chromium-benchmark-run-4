7fff8f5869afd97992fab206a28b79b9ff574541
Kenneth Russell <kbr@chromium.org>
2025-03-22 00:16:41 -0700

    Update Chromium code for Pixel Local Storage extension updates.
    Apply enum renumbering and regenerate GL bindings. Update WebGL2
    extension IDL. Reenable PLS unit test in gl_tests.
    
    Bug: angleproject:40096838
    Change-Id: I27de7441952fafce823d30b97ce790be3a4fa21c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6380793
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436433}

--- gpu/command_buffer/tests/gl_angle_shader_pixel_local_storage_unittest.cc
+++ gpu/command_buffer/tests/gl_angle_shader_pixel_local_storage_unittest.cc
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gl/gl_implementation.h"
 
-// Temporarily disable this test while enums are being renumbered.
-// anglebug.com/40096838
-#if 0
-
 namespace gpu {
 class ANGLEShaderPixelLocalStorageTest : public testing::Test {
  public:
@@ -66,9 +62,6 @@ TEST_F(ANGLEShaderPixelLocalStorageTest, GetIntegerv) {
   }
 
   EXPECT_GT(gl_get_integer(GL_MAX_PIXEL_LOCAL_STORAGE_PLANES_ANGLE), 4);
-  EXPECT_GT(gl_get_integer(
-                GL_MAX_COLOR_ATTACHMENTS_WITH_ACTIVE_PIXEL_LOCAL_STORAGE_ANGLE),
-            0);
   EXPECT_GT(
       gl_get_integer(
           GL_MAX_COMBINED_DRAW_BUFFERS_AND_PIXEL_LOCAL_STORAGE_PLANES_ANGLE),
@@ -118,8 +111,10 @@ TEST_F(ANGLEShaderPixelLocalStorageTest, GetIntegerv) {
 // Verifies that glGetFramebufferPixelLocalStorageParameter{f,i}vANGLE is
 // marshalled properly over the command buffer. Thorough testing of these
 // commands is done in angle_end2end_tests.
+
+// TODO(anglebug.com/40096838): debug and re-enable this test.
 TEST_F(ANGLEShaderPixelLocalStorageTest,
-       GetFramebufferPixelLocalStorageParameter) {
+       DISABLED_GetFramebufferPixelLocalStorageParameter) {
   if (!gl_.IsInitialized() ||
       !GLTestHelper::HasExtension("GL_ANGLE_shader_pixel_local_storage")) {
     return;
@@ -364,5 +359,3 @@ TEST_F(ANGLEShaderPixelLocalStorageTest, BlockEmulatedDefaultFramebuffer) {
   EXPECT_GL_ERROR(GL_NO_ERROR);
 }
 }  // namespace gpu
-
-#endif  // #if 0
