759d48f8e7cb06acbd2baf6dd01fe64335a146b0
Christoph Schwering <schwering@google.com>
2025-03-22 08:05:51 -0700

    [Autofill] Inline QueryAutofillSuggestions()
    The function is only called by ShowAutofillSuggestions(). The naming
    arguably doesn't help to understand who calls whom. There also no
    abstraction layer between the two. So inlining it just avoids a source
    of confusion.
    
    Bug: 40100455
    Change-Id: I210011a6c261780edfecd43e2e26d0aef6f50cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383096
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436472}

--- components/autofill/content/renderer/autofill_agent_browsertest.cc
+++ components/autofill/content/renderer/autofill_agent_browsertest.cc
@@ -1505,9 +1505,10 @@ class AutofillAgentTestCaret
       case FormControlType::kInputText:
       case FormControlType::kTextArea:
         test_api(autofill_agent())
-            .QueryAutofillSuggestions(
+            .ShowSuggestions(
                 GetElement().DynamicTo<blink::WebFormControlElement>(),
-                /*trigger_source=*/{}, /*form_cache=*/{});
+                AutofillSuggestionTriggerSource::kFormControlElementClicked,
+                /*form_cache=*/{});
         break;
       default:
         NOTREACHED();
