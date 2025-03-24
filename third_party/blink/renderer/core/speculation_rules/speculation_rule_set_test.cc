e1484e1604dfcf4d8e221514ed39ef0b1b075dfe
HuanPo Lin <robertlin@chromium.org>
2025-03-24 07:26:21 -0700

    preload: rename ruleset level speculation rules tags
    This CL renames the naming of ruleset level speculation rules tags
    to avoid confusion as rule level will be introduced in
    https://chromium-review.googlesource.com/c/chromium/src/+/6192578
    
    Bug: 381687257
    Change-Id: Ic85b836cd4d761ab7d3b963abad8c34d7e4232ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382394
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436814}

--- third_party/blink/renderer/core/speculation_rules/speculation_rule_set_test.cc
+++ third_party/blink/renderer/core/speculation_rules/speculation_rule_set_test.cc
@@ -164,8 +164,10 @@ MATCHER_P(HasTags, tag, std::string(tag) + " is in the tag list") {
   return false;
 }
 
-MATCHER_P(HasTag, tag, "has tag " + std::string(tag)) {
-  return arg->tag().value() == tag;
+MATCHER_P(HasRulesetLevelTag,
+          tag,
+          "has ruleset level tag " + std::string(tag)) {
+  return arg->ruleset_tag().value() == tag;
 }
 
 class SpeculationRuleSetTest : public ::testing::Test {
@@ -4443,7 +4445,8 @@ TEST_F(SpeculationRuleSetTest, ParseValidTag) {
       })",
       KURL("https://example.com/"), execution_context());
   EXPECT_THAT(rule_set->prefetch_rules(),
-              ElementsAre(HasTag("example/foo"), HasTag("example/foo")));
+              ElementsAre(HasRulesetLevelTag("example/foo"),
+                          HasRulesetLevelTag("example/foo")));
 
   // \"null\" string is valid.
   auto* rule_set2 = CreateRuleSet(
@@ -4454,7 +4457,8 @@ TEST_F(SpeculationRuleSetTest, ParseValidTag) {
         }]
       })",
       KURL("https://example.com/"), execution_context());
-  EXPECT_THAT(rule_set2->prefetch_rules(), ElementsAre(HasTag("null")));
+  EXPECT_THAT(rule_set2->prefetch_rules(),
+              ElementsAre(HasRulesetLevelTag("null")));
 }
 
 TEST_F(SpeculationRuleSetTest, InvalidTag) {
