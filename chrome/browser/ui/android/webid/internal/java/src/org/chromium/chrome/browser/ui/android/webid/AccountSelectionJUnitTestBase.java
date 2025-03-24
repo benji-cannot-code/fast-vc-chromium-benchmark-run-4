a578b43f1d79d2f25616d228d45b81079f8384d8
Nicolás Peña <npm@chromium.org>
2025-03-24 06:45:49 -0700

    [FedCM] Add "You used on this site" on Android multi IDP
    If an account is returning (according to the IDP knowledge if present,
    otherwise according to browser knowledge), we add that string to the
    secondary description in the multi IDP case.
    
    Bug: 392142580
    Change-Id: I2f455de50245705a3a06acf43ed77d22b1c917de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383183
    Reviewed-by: Zachary Tan <tanzachary@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436798}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionJUnitTestBase.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionJUnitTestBase.java
@@ -327,7 +327,7 @@ public class AccountSelectionJUnitTestBase {
                         "goto@email.example",
                         "Sam E. Goto",
                         "Sam",
-                        /* secondaryDescription= */ null,
+                        /* secondaryDescription= */ "email.example",
                         /* pictureBitmap= */ null,
                         /* circledBadgedPictureBitmap= */ Bitmap.createBitmap(
                                 100, 100, Bitmap.Config.ARGB_4444),
