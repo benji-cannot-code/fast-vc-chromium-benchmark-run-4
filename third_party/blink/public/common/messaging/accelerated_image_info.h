3a6585b9a0a341fa0784d75098ccae7d22310f37
Colin Blundell <blundell@chromium.org>
2025-03-24 05:44:30 -0700

    [Blink] Explode AcceleratedImageInfo fields
    Will enable moving this struct away from Skia types in followup work.
    
    Bug: 371227617
    Change-Id: I4d6a53a7a82a16c17cdcde2392613897f5601e15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367354
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436773}

--- third_party/blink/public/common/messaging/accelerated_image_info.h
+++ third_party/blink/public/common/messaging/accelerated_image_info.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/common/common_export.h"
+#include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/include/core/SkImage.h"
 
 namespace blink {
@@ -23,7 +24,10 @@ namespace blink {
 struct BLINK_COMMON_EXPORT AcceleratedImageInfo {
   gpu::ExportedSharedImage shared_image;
   gpu::SyncToken sync_token;
-  SkImageInfo image_info;
+  SkISize size;
+  SkColorType sk_color_type;
+  SkAlphaType alpha_type;
+  sk_sp<SkColorSpace> sk_color_space;
   base::OnceCallback<void(const gpu::SyncToken& sync_token)> release_callback;
 };
 
--- third_party/blink/public/common/messaging/accelerated_static_bitmap_image_mojom_traits.h
+++ third_party/blink/public/common/messaging/accelerated_static_bitmap_image_mojom_traits.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/common/messaging/accelerated_image_info.h"
 #include "third_party/blink/public/mojom/messaging/static_bitmap_image.mojom.h"
+#include "third_party/skia/include/core/SkColorSpace.h"
 
 namespace mojo {
 
@@ -38,7 +39,8 @@ struct BLINK_COMMON_EXPORT
   }
 
   static SkImageInfo image_info(const blink::AcceleratedImageInfo& input) {
-    return input.image_info;
+    return SkImageInfo::Make(input.size, input.sk_color_type, input.alpha_type,
+                             input.sk_color_space);
   }
 
   static mojo::PendingRemote<blink::mojom::ImageReleaseCallback>
