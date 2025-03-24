dc53bd3b28687e5dd086b28c9771bf77567c6369
Jan Keitel <jkeitel@google.com>
2025-03-24 05:49:23 -0700

    Remove migration logic for prefs that have been deprecated for 1+ yr
    That is how long we usually keep pref migration logic around for in
    the codebase.
    
    Bug: 40100455
    Change-Id: Ib143715bf47493a18edd9911e806c321a2f86416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382160
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436777}

--- components/autofill/core/common/autofill_prefs.cc
+++ components/autofill/core/common/autofill_prefs.cc
@@ -18,6 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill::prefs {
 
+namespace {
+// To simplify the rollout of AutofillSilentlyRemoveQuasiDuplicates,
+// deduplication can be run a second time per milestone for users enrolled in
+// the experiment. This pref tracks whether deduplication was run a second time.
+// TODO(crbug.com/325450676): Remove after the rollout finished.
+constexpr char kAutofillRanQuasiDuplicateExtraDeduplication[] =
+    "autofill.ran_quasi_duplicate_extra_deduplication";
+}  // namespace
+
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   // Synced prefs. Used for cross-device choices, e.g., credit card Autofill.
   registry->RegisterBooleanPref(
@@ -73,11 +82,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
   // Deprecated prefs registered for migration.
   registry->RegisterBooleanPref(kAutofillEnabledDeprecated, true);
-  registry->RegisterBooleanPref(kAutofillOrphanRowsRemoved, false);
-  registry->RegisterBooleanPref(kAutofillIbanEnabled, true);
-  registry->RegisterIntegerPref(kAutofillLastVersionDisusedAddressesDeleted, 0);
-  registry->RegisterIntegerPref(kAutofillLastVersionDisusedCreditCardsDeleted,
-                                0);
   registry->RegisterStringPref(kAutofillAblationSeedPref, "");
   registry->RegisterBooleanPref(kAutofillRanQuasiDuplicateExtraDeduplication,
                                 false);
@@ -107,19 +111,12 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 }
 
 void MigrateDeprecatedAutofillPrefs(PrefService* pref_service) {
-  // Added 09/2022.
-  pref_service->ClearPref(kAutofillEnabledDeprecated);
-  // Added 05/2023.
-  pref_service->ClearPref(kAutofillOrphanRowsRemoved);
-  // Added 09/2023.
-  pref_service->ClearPref(kAutofillIbanEnabled);
-  // Added 10/2023
-  pref_service->ClearPref(kAutofillLastVersionDisusedAddressesDeleted);
-  pref_service->ClearPref(kAutofillLastVersionDisusedCreditCardsDeleted);
   // Added 07/2024 (moved from profile pref to local state)
   pref_service->ClearPref(kAutofillAblationSeedPref);
   // Added 10/2024
   pref_service->ClearPref(kAutofillRanQuasiDuplicateExtraDeduplication);
+  // Added 03/2025
+  pref_service->ClearPref(kAutofillEnabledDeprecated);
 }
 
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
