72ca90181a6726044e6ead05f0a20702dfae48ed
Darren Shen <shend@chromium.org>
2025-03-24 16:16:52 -0700

    jp settings: Remove Mobile entry from keymap style
    This doesn't exist in the legacy JP settings page.
    
    Bug: b/297291512
    Change-Id: I1ce55e169c812c85f92ef365575cc52e58ff79bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6371611
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437179}

--- chrome/browser/resources/ash/settings/os_languages_page/input_method_util.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/input_method_util.ts
@@ -962,10 +962,6 @@ export function getOptionMenuItems(option: OptionType):
           name: 'inputMethodOptionsJapaneseKeymapStyleKotoeri',
         },
         {
-          value: JapaneseKeymapStyle.MOBILE,
-          name: 'inputMethodOptionsJapaneseKeymapStyleMobile',
-        },
-        {
           value: JapaneseKeymapStyle.CHROME_OS,
           name: 'inputMethodOptionsJapaneseKeymapStyleChromeOs',
         },
