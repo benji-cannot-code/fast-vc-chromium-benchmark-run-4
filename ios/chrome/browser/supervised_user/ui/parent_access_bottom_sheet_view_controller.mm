e4d9a737f1a736346e121baabc2d21b87cbf21c3
Duong Dac <ddac@google.com>
2025-03-24 08:30:45 -0700

    [iOS] Use fixed height detent for PACP bottom sheet
    Use a fixed height detent for all platforms (phone & tablet). The fixed
    height is calculated from the finalized PACP widget content height. The
    mediumDetent, which is not available for landscape mode, is no longer
    used. See demo in the bug entry.
    
    Bug: 384514294
    Change-Id: I6eaa4d88507b7e4fd886625bf22cf3c619242637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382101
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436866}

--- ios/chrome/browser/supervised_user/ui/parent_access_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/supervised_user/ui/parent_access_bottom_sheet_view_controller.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 constexpr CGFloat kCloseButtonPadding = 16;
+constexpr CGFloat kWidgetContentHeight = 410;
 
 // Custom detent identifier for when the bottom sheet is expanded.
 NSString* const kCustomBottomSheetDetentIdentifier = @"customBottomSheetDetent";
@@ -125,11 +126,8 @@ UIImage* CloseButtonImage(BOOL highlighted) {
     CGFloat largeDetentHeight =
         [[UISheetPresentationControllerDetent largeDetent]
             resolvedValueInContext:context];
-    CGFloat mediumDetentHeight =
-        [[UISheetPresentationControllerDetent mediumDetent]
-            resolvedValueInContext:context];
-    // Make sure detent is at least 67.5% of the maximum detent.
-    return MAX(mediumDetentHeight, largeDetentHeight * 0.675);
+    // Limit custom detent height at the maximum height.
+    return MIN(kWidgetContentHeight, largeDetentHeight);
   };
 
   return
@@ -140,36 +138,11 @@ UIImage* CloseButtonImage(BOOL highlighted) {
 - (void)updateBottomSheetDetents {
   UISheetPresentationController* presentationController =
       self.sheetPresentationController;
-  switch (ui::GetDeviceFormFactor()) {
-    case ui::DeviceFormFactor::DEVICE_FORM_FACTOR_PHONE:
-      if (IsPortrait(self.view.window)) {
-        // In portrait mode, the bottom sheet occupies the bottom half of the
-        // screen.
-        presentationController.detents =
-            @[ [UISheetPresentationControllerDetent mediumDetent] ];
-        presentationController.selectedDetentIdentifier =
-            UISheetPresentationControllerDetentIdentifierMedium;
-      } else {
-        // In landscape mode, the bottom sheet is centered horizontally and
-        // occupies half of the screen width.
-        presentationController.detents =
-            @[ [UISheetPresentationControllerDetent largeDetent] ];
-        presentationController.selectedDetentIdentifier =
-            UISheetPresentationControllerDetentIdentifierLarge;
-      }
-      break;
-    default:
-      // On devices where the screen width difference between portrait and
-      // landscape is less pronounced, display the bottom sheet with a custom
-      // detent.
-      presentationController.detents = @[
-        [self
-            customHeightDetentWithIdentifier:kCustomBottomSheetDetentIdentifier]
-      ];
-      presentationController.selectedDetentIdentifier =
-          kCustomBottomSheetDetentIdentifier;
-      break;
-  }
+
+  presentationController.detents = @[ [self
+      customHeightDetentWithIdentifier:kCustomBottomSheetDetentIdentifier] ];
+  presentationController.selectedDetentIdentifier =
+      kCustomBottomSheetDetentIdentifier;
 }
 
 - (void)closeButtonTapped {
