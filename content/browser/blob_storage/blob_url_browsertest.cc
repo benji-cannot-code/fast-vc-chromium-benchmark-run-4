865acf5f421f882dc0661d5be3bc2a97f6666725
Janice Liu <janiceliu@chromium.org>
2025-03-24 10:29:10 -0700

    [Blob URL] Allow contexts with a StorageAccessHandle to bypass Blob URL partitioning
    Given that the RequestStorageAccess API specifically exposes the ability
    to create unpartitioned Blob URLs by getting a StorageAccessHandle,
    create an unpartitioned blob URL using the handle's CreateObjectURL
    method, and then fetch it, it makes sense to ensure that contexts that
    have been granted storage access can bypass the blob URL partitioning
    check.
    
    Bug: 399308041, 399769596
    Change-Id: Ia47e815c819c39e6a6ce00aa0c8c6617173ea742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313600
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Janice Liu <janiceliu@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Janice Liu <janiceliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436948}

--- content/browser/blob_storage/blob_url_browsertest.cc
+++ content/browser/blob_storage/blob_url_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/with_feature_override.h"
 #include "build/build_config.h"
 #include "content/browser/devtools/render_frame_devtools_agent_host.h"
+#include "content/browser/permissions/permission_controller_impl.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/common/content_switches.h"
@@ -44,6 +45,14 @@ class MockContentBrowserClient : public ContentBrowserTestContentBrowserClient {
               LogWebFeatureForCurrentPage,
               (content::RenderFrameHost*, blink::mojom::WebFeature),
               (override));
+
+  bool IsFullCookieAccessAllowed(
+      content::BrowserContext* browser_context,
+      content::WebContents* web_contents,
+      const GURL& url,
+      const blink::StorageKey& storage_key) override {
+    return false;
+  }
 };
 }  // namespace
 
@@ -252,8 +261,10 @@ IN_PROC_BROWSER_TEST_F(BlobUrlBrowserTest,
 class BlobUrlDevToolsIssueTest : public ContentBrowserTest {
  protected:
   BlobUrlDevToolsIssueTest() {
-    feature_list_.InitAndEnableFeature(
-        features::kBlockCrossPartitionBlobUrlFetching);
+    feature_list_.InitWithFeatures(
+        {features::kBlockCrossPartitionBlobUrlFetching,
+         blink::features::kEnforceNoopenerOnBlobURLNavigation},
+        {});
   }
 
   void SetUpOnMainThread() override {
@@ -261,7 +272,10 @@ class BlobUrlDevToolsIssueTest : public ContentBrowserTest {
     host_resolver()->AddRule("*", "127.0.0.1");
     SetupCrossSiteRedirector(embedded_test_server());
     ASSERT_TRUE(embedded_test_server()->Start());
+    client_ = std::make_unique<MockContentBrowserClient>();
   }
+
+  void TearDownOnMainThread() override { client_.reset(); }
 
   void WaitForIssueAndCheckUrl(const std::string& url,
                                TestDevToolsProtocolClient* client,
@@ -301,7 +315,11 @@ class BlobUrlDevToolsIssueTest : public ContentBrowserTest {
     client->ClearNotifications();
   }
 
+ private:
   base::test::ScopedFeatureList feature_list_;
+
+ private:
+  std::unique_ptr<MockContentBrowserClient> client_;
 };
 
 IN_PROC_BROWSER_TEST_F(BlobUrlDevToolsIssueTest, PartitioningBlobUrlIssue) {
@@ -324,6 +342,12 @@ IN_PROC_BROWSER_TEST_F(BlobUrlDevToolsIssueTest, PartitioningBlobUrlIssue) {
 
   RenderFrameHost* rfh_b = ChildFrameAt(rfh_c, 0);
   RenderFrameHost* rfh_c_2 = ChildFrameAt(rfh_b, 0);
+
+  static_cast<PermissionControllerImpl*>(
+      rfh_c_2->GetBrowserContext()->GetPermissionController())
+      ->SetPermissionOverride(/*origin=*/std::nullopt,
+                              blink::PermissionType::STORAGE_ACCESS_GRANT,
+                              blink::mojom::PermissionStatus::DENIED);
 
   std::unique_ptr<content::TestDevToolsProtocolClient> client =
       std::make_unique<content::TestDevToolsProtocolClient>();
--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -157,6 +157,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/site_info.h"
 #include "content/browser/sms/webotp_service.h"
 #include "content/browser/speech/speech_synthesis_impl.h"
+#include "content/browser/storage_access/storage_access_handle.h"
 #include "content/browser/storage_partition_impl.h"
 #include "content/browser/url_loader_factory_params_helper.h"
 #include "content/browser/usb/web_usb_service_impl.h"
@@ -12667,6 +12668,12 @@ void RenderFrameHostImpl::ReportBlockingCrossPartitionBlobURL(
       std::move(details)));
 }
 
+void RenderFrameHostImpl::DoesDocumentHaveStorageAccess(
+    base::OnceCallback<void(bool)> callback) {
+  std::move(callback).Run(
+      StorageAccessHandle::DoesFrameHaveStorageAccess(this));
+}
+
 void RenderFrameHostImpl::BindBlobUrlStoreAssociatedReceiver(
     mojo::PendingAssociatedReceiver<blink::mojom::BlobURLStore> receiver) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -12678,6 +12685,8 @@ void RenderFrameHostImpl::BindBlobUrlStoreAssociatedReceiver(
       base::BindRepeating(
           &RenderFrameHostImpl::ReportBlockingCrossPartitionBlobURL,
           weak_ptr_factory_.GetWeakPtr()),
+      base::BindRepeating(&RenderFrameHostImpl::DoesDocumentHaveStorageAccess,
+                          weak_ptr_factory_.GetWeakPtr()),
       !(GetContentClient()->browser()->IsBlobUrlPartitioningEnabled(
           GetBrowserContext())));
 }
