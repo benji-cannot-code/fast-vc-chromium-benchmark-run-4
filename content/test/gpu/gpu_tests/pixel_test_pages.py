ae00124b8ce4781256470ed45a1edb598e044d89
Brian Sheedy <bsheedy@chromium.org>
2025-03-24 13:56:37 -0700

    Add sobel matching to Pixel_SVGHuge
    Adds sobel matching to the Pixel_SVGHuge test since new images are
    occasionally generated with minor differences along the edges.
    
    Also drive-by removes a long-expired grace period for a nearby test.
    
    Bug: 41495981
    Change-Id: Ie043ffdb44b9999c07b8bc69d7f071acb9e2f4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6388438
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437083}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # pylint: disable=too-many-lines
 
 from collections.abc import Callable
-from datetime import date
 import json
 import logging
 import os
@@ -569,7 +568,11 @@ class PixelTestPages():
             ]),
         PixelTestPage('pixel_svg_huge.html',
                       base_name + '_SVGHuge',
-                      crop_action=ca.FixedRectCropAction(0, 0, 400, 400)),
+                      crop_action=ca.FixedRectCropAction(0, 0, 400, 400),
+                      matching_algorithm=algo.SobelMatchingAlgorithm(
+                          max_different_pixels=0,
+                          pixel_delta_threshold=0,
+                          edge_threshold=90)),
         PixelTestPage('pixel_webgl_display_p3.html',
                       base_name + '_WebGLDisplayP3',
                       crop_action=standard_crop),
@@ -602,7 +605,6 @@ class PixelTestPages():
             base_name + '_PerspectiveTest',
             crop_action=ca.NonWhiteContentCropAction(
                 initial_crop=ca.FixedRectCropAction(0, 0, 500, 300)),
-            grace_period_end=date(2024, 8, 1),
         ),
     ]
 
