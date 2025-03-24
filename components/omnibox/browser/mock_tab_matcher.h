2761eff6b81f8c182c128063e3ed4c4884df88ad
Nihar Majmudar <niharm@google.com>
2025-03-24 16:11:58 -0700

    [omnibox] Don't show currently open tabs from MV provider on desktop.
    Adds functionality to TabMatcher on Desktop to also consider the
    active tabs when matching URLs.
    
    Change-Id: I611b163d7832df31475e9edbdf93f2fb1701a534
    Bug: 405113997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6377163
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437172}

--- components/omnibox/browser/mock_tab_matcher.h
+++ components/omnibox/browser/mock_tab_matcher.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // with tests.
 class MockTabMatcher : public TabMatcher {
  public:
-  MOCK_CONST_METHOD2(IsTabOpenWithURL,
-                     bool(const GURL&, const AutocompleteInput*));
+  MOCK_CONST_METHOD3(IsTabOpenWithURL,
+                     bool(const GURL&, const AutocompleteInput*, bool));
   MockTabMatcher();
   ~MockTabMatcher() override;
 };
