9ca572a3c962fd501006f3774136cc57adcc7acd
Sandor Major <sandormajor@chromium.org>
2025-03-24 14:55:02 -0700

    `ShouldSetLoadWithStorageAccess` to calculate storage status
    `ShouldSetLoadWithStorageAccess` relied on
    `URLRequest::storage_access_status()` calculated earlier. This CL
    introduces a new `URLRequest::StorageAccessStatus` state `Unset` to
    determine if the state was not calculated earlier and let
    `ShouldSetLoadWithStorageAccess` calculate it in this case. This will
    Allow us to not calculate storage access status before it's necessary.
    
    Bug: 382291442
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Change-Id: I98ba67048646cb4abd92eb0a77c92bf1a0585727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361478
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Sandor «Alex» Major <sandormajor@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437118}

--- net/url_request/storage_access_status_cache_unittest.cc
+++ net/url_request/storage_access_status_cache_unittest.cc
@@ -14,18 +14,20 @@ namespace {
 
 TEST(StorageAccessStatusTest, DefaultConstructor) {
   StorageAccessStatusCache status;
-  EXPECT_EQ(status.GetStatusForThirdPartyContext(), std::nullopt);
+  EXPECT_FALSE(status.IsSet());
 }
 
 TEST(StorageAccessStatusTest, ConstructorWithValue) {
   StorageAccessStatusCache status(
       net::cookie_util::StorageAccessStatus::kActive);
+  ASSERT_TRUE(status.IsSet());
   EXPECT_EQ(status.GetStatusForThirdPartyContext(),
             std::make_optional(net::cookie_util::StorageAccessStatus::kActive));
 }
 
 TEST(StorageAccessStatusTest, ConstructorWithNullopt) {
   StorageAccessStatusCache status(std::nullopt);
+  ASSERT_TRUE(status.IsSet());
   EXPECT_EQ(status.GetStatusForThirdPartyContext(), std::nullopt);
 }
 
@@ -37,7 +39,15 @@ TEST(StorageAccessStatusTest, EqualityOperatorWithValue) {
 
 TEST(StorageAccessStatusTest, EqualityOperatorWithoutValue) {
   StorageAccessStatusCache status;
-  EXPECT_NE(status, cookie_util::StorageAccessStatus::kNone);
+  EXPECT_DEATH_IF_SUPPORTED(
+      EXPECT_NE(status, cookie_util::StorageAccessStatus::kNone),
+      "Check failed");
+}
+
+TEST(StorageAccessStatusTest, GetStatusForThirdPartyContext_Unset) {
+  StorageAccessStatusCache status;
+  EXPECT_DEATH_IF_SUPPORTED(status.GetStatusForThirdPartyContext(),
+                            "Check failed");
 }
 
 }  // namespace
