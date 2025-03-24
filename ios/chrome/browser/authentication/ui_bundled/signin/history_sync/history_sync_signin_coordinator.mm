de94c2f9462f0e9ff20050f5764e7ab4e5d61b85
Arthur Milchior <arthurmilchior@chromium.org>
2025-03-24 14:47:18 -0700

    [iOS]Set a coordinator to nil
    When the history sync signin coordinator interrupted its
    syncPopupCoordinator, it nows remove its delegate and set the
    SyncPopupCoordinator to nil, following Chrome’s best practice.
    
    Bug: None
    Change-Id: Ic21c35b3b52887c6ad3fa7f18c9e1d6b04b6fe14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382148
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437113}

--- ios/chrome/browser/authentication/ui_bundled/signin/history_sync/history_sync_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/history_sync/history_sync_signin_coordinator.mm
@@ -54,6 +54,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)interruptAnimated:(BOOL)animated {
   [_syncPopupCoordinator interruptAnimated:animated];
+  _syncPopupCoordinator.delegate = nil;
+  _syncPopupCoordinator = nil;
 }
 
 #pragma mark - HistorySyncPopupCoordinatorDelegate
