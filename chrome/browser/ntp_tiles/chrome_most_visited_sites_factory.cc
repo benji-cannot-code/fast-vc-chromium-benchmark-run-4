c372fc7f7b2d73c3e4b1f242547d0bffc575c662
Samuel Huang <huangs@chromium.org>
2025-03-24 16:03:43 -0700

    [MVT Customization] Add custom links mixability option to MostVisitedSites.
    On Desktop, MVT Customization is "all-or-nothing": When off, only Top
    Sites Tiles (TST, but includes Popular) are shown; when on, only Custom
    Tiles (CT) are shown.
    
    By contrast, on Clank we plan to show CTs together with TSTs.
    
    This CL adds |is_custom_links_mixable_| to ntp_tiles::MostVisitedSites
    to distinguish 3 cases: {TST mode, CT mode, Modeless}:
    * Non-clank: |is_custom_links_mixable_| = false always.
       * "TST mode": IsCustomLinksInitialized() = false.
       * "CT mode": IsCustomLinksInitialized() = true (assuming custom links
         is used).
    * (Planned) Clank: |is_custom_links_mixable_| = true always.
       * "Modeless": IsCustomLinksInitialized() determines whether or not to
         loading CT (for Clank currently this is not enabled).
    
    More refactoring is applied to enact the above:
    1. Add IsExclusivelyCustomLinks(), which returns
         !is_custom_links_mixable_ && IsCustomLinksInitialized()
       * True => CT mode.
       * False => TST mode / Modeless.
       * On Desktop: Evaluates to IsCustomLinksInitialized(), which then
         choose between {CT mode, TST mode}.
       * On Clank: False always (CTs never excludes TSTs), so Modeless.
    
    2. Replace all 2 IsCustomLinksInitialized() usages with
       IsExclusivelyCustomLinks():
       a. OnMostVisitedURLsAvailable(): True = CT mode; skip TST
          computation. False = TST mode / Modeless: Compute TST.
       b. BuildCurrentTiles(): True = CT mode: Call BuildCustomLinks().
          False = TST mode / Modeless: Call InitiateTopSitesQuery instead.
    
    3. Replace the only |mv_source_| usage with IsExclusivelyCustomLinks(),
       allowing for its removal.
       * Where does |mv_source_| get written?
         * TOP_SITES in (a) and (b) when IsCustomLinksInitialized() is
           false.
         * CUSTOM_LINKS initially and in BuildCustomLinks() (which
           is called in (a).
       => |mv_source_| is CUSTOM_LINKS iff IsExclusivelyCustomLinks(). Also,
          the value is used only in TopSitesChanged(). Can replace.
       c. TopSitesChanged(): False = TST mode / Modeless: Call
          InitiateTopSitesQuery(). True = CT mode: Skip.
    
    Other changes:
    * ChromeMostVisitedSitesFactory::NewForProfile(): Pass
      |is_custom_links_mixable| = true for Clank only; else false.
    * Update tests / iOS calls to MostVisitedSites CTORs, passing
      |is_custom_links_mixable| = false for old behavior.
    * Add TODO in InitializeCustomLinks() as reminder that for Modeless,
      we shouldn't bulk-convert TST to CT (this change is better left to
      follow-up CL).
    
    Bug: 397422358
    Change-Id: If28c6d54e1725bac79f08f7769c6b1c1d1f9c6a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387640
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437161}

--- chrome/browser/ntp_tiles/chrome_most_visited_sites_factory.cc
+++ chrome/browser/ntp_tiles/chrome_most_visited_sites_factory.cc
@@ -54,10 +54,13 @@ ChromeMostVisitedSitesFactory::NewForProfile(Profile* profile) {
 #endif
 
   bool is_default_chrome_app_migrated;
+  bool is_custom_links_mixable;
 #if BUILDFLAG(IS_ANDROID)
   is_default_chrome_app_migrated = false;
+  is_custom_links_mixable = true;
 #else
   is_default_chrome_app_migrated = true;
+  is_custom_links_mixable = false;
 #endif
 
   auto most_visited_sites = std::make_unique<ntp_tiles::MostVisitedSites>(
@@ -83,6 +86,6 @@ ChromeMostVisitedSitesFactory::NewForProfile(Profile* profile) {
               profile->GetDefaultStoragePartition()
                   ->GetURLLoaderFactoryForBrowserProcess()),
           std::move(data_decoder)),
-      is_default_chrome_app_migrated);
+      is_default_chrome_app_migrated, is_custom_links_mixable);
   return most_visited_sites;
 }
