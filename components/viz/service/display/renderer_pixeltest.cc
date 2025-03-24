0eb43d541bb683670b7bfaae959e0dba020b3e00
Michael Tang <tangm@microsoft.com>
2025-03-24 11:20:07 -0700

    viz: test: Allow F16 in SkiaOutputDeviceOffscreen and use in tests
    This change adds support for RGBA F16 pixel formats for the offscreen
    output device and adds it to the DisplayColorSpaces for
    RendererPixelTests that want wide color spaces.
    
    Change-Id: I792d36f2b8196e8308231cc4b2d62f6a56f7c553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384303
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1436986}

--- components/viz/service/display/renderer_pixeltest.cc
+++ components/viz/service/display/renderer_pixeltest.cc
@@ -5959,6 +5959,8 @@ class RendererPixelTestColorConversion
     this->display_color_spaces_ =
         gfx::DisplayColorSpaces(gfx::ColorSpace::CreateSCRGBLinear80Nits());
     this->display_color_spaces_.SetSDRMaxLuminanceNits(80.f);
+    this->display_color_spaces_.SetOutputBufferFormats(
+        gfx::BufferFormat::RGBA_F16, gfx::BufferFormat::RGBA_F16);
 
     if (std::get<1>(GetParam())) {
       features_.InitAndEnableFeature(features::kColorConversionInRenderer);
@@ -6029,6 +6031,8 @@ class VideoPixelRendererPixelTestColorConversion
     this->display_color_spaces_ =
         gfx::DisplayColorSpaces(gfx::ColorSpace::CreateSCRGBLinear80Nits());
     this->display_color_spaces_.SetSDRMaxLuminanceNits(80.f);
+    this->display_color_spaces_.SetOutputBufferFormats(
+        gfx::BufferFormat::RGBA_F16, gfx::BufferFormat::RGBA_F16);
 
     // Allow non-root render passes to have the above non-suitable-for-blending
     // color space by being scanout.
@@ -6175,6 +6179,10 @@ class ColorTransformPixelTest
     }
     this->display_color_spaces_ =
         gfx::DisplayColorSpaces(this->dst_color_space_);
+    if (this->dst_color_space_.IsWide()) {
+      this->display_color_spaces_.SetOutputBufferFormats(
+          gfx::BufferFormat::RGBA_F16, gfx::BufferFormat::RGBA_F16);
+    }
     this->premultiplied_alpha_ = std::get<3>(GetParam());
     if (std::get<4>(GetParam())) {
       features_.InitAndEnableFeature(features::kColorConversionInRenderer);
