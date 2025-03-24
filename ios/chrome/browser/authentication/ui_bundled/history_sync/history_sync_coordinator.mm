6985b526e1ba52024d2fdfa08d18f5dd2d49c7ef
Ewann Pelle <ewannpv@chromium.org>
2025-03-24 09:12:25 -0700

    [iOS][TG] Updates sync strings when interacting with a shared tab group
    Updates the sync strings according to the current access point.
    
    Bug: 391087906
    Change-Id: I9833bb624c80280c0f2af9c1c5b14d0e6ca2e136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6381862
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436891}

--- ios/chrome/browser/authentication/ui_bundled/history_sync/history_sync_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/history_sync/history_sync_coordinator.mm
@@ -121,7 +121,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
-  _viewController = [[HistorySyncViewController alloc] init];
+  _viewController =
+      [[HistorySyncViewController alloc] initWithAccessPoint:_accessPoint];
   _viewController.delegate = self;
 
   ChromeAccountManagerService* chromeAccountManagerService =
--- ios/chrome/browser/authentication/ui_bundled/history_sync/history_sync_view_controller.mm
+++ ios/chrome/browser/authentication/ui_bundled/history_sync/history_sync_view_controller.mm
@@ -19,7 +19,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 
-@implementation HistorySyncViewController
+using signin_metrics::AccessPoint;
+
+@implementation HistorySyncViewController {
+  // Access point associated with the history opt-in screen.
+  AccessPoint _accessPoint;
+}
+
+- (instancetype)initWithAccessPoint:(AccessPoint)accessPoint {
+  self = [super init];
+  if (self) {
+    _accessPoint = accessPoint;
+  }
+  return self;
+}
 
 @dynamic delegate;
 
@@ -36,8 +49,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.headerBackgroundImage =
         [UIImage imageNamed:@"history_sync_opt_in_background"];
   }
-  self.titleText = l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_TITLE);
-  self.subtitleText = l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_SUBTITLE);
+
+  switch (_accessPoint) {
+    case AccessPoint::kCollaborationTabGroup: {
+      self.titleText = l10n_util::GetNSString(
+          IDS_IOS_HISTORY_SYNC_GROUP_COLLABORATION_TITLE);
+      self.subtitleText = l10n_util::GetNSString(
+          IDS_IOS_HISTORY_SYNC_GROUP_COLLABORATION_SUBTITLE);
+      break;
+    }
+    default: {
+      self.titleText = l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_TITLE);
+      self.subtitleText = l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_SUBTITLE);
+      break;
+    }
+  }
+
   self.primaryActionString =
       l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_PRIMARY_ACTION);
   self.secondaryActionString =
