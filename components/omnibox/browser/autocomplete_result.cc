02388c6e542f55af044659f8a8187892e0984825
Orin Jaworski <orinj@google.com>
2025-03-24 16:32:17 -0700

    Surface contextual search ad actions; enter and keep @page keyword mode
    This CL does several things to make the omnibox work better with the
    contextual search provider (CSP):
    - The CSP produces pedal-like action matches to advertise contextual
      search when *not* in '@page' mode, only for empty input/zero suggest.
      For now these are high relevance to make the list, but eventually
      need to be sorted to the bottom of the list instead of top.
    - The ContextualSearchAction is renamed to
      ContextualSearchFulfillmentAction to avoid ambiguity with the ads.
    - The "Ask about this page" action puts the omnibox into '@page'
      keyword mode for direct query entry with page-contextual suggestions.
    - The "Search with Google Lens" action invokes the lens overlay
      so the user can select a region for lens search.
    - The CSP uses placeholder matches to keep the omnibox in keyword
      mode. This is currently required due to how the omnibox deals
      with keywords and default matches.
    - The OmniboxEditModel is slightly refactored to support more general
      keyword mode entry, as it only supported default search engine
      entry before.
    - Invocation of lens overlay on '@page' keyword mode entry is
      removed because it's too soon, not intentional enough. Using
      an action match is now the only way to invoke lens, though we may
      want to add more indirect support via '@page' starter pack later.
    - Matches from suggest are modified by the provider to preserve
      keyword mode (and there's a TODO with details about how to clean
      this up in next steps).
    - Eliminates surfacing the '@page' starter pack keyword match from
      the FeaturedSearchProvider; it wasn't sufficient or a right fit.
    - Simplifies contextual search fulfillment action attachment.
    
    Note that the behavior with this CL may not work as expected, and
    this is intentional to show the gaps and what remains to be done.
    In particular, clicking "Ask about this page" will enter the scope
    but receive no suggest results because there's no background lens
    invocation yet. If you then invoke the "Search with Google Lens"
    action, lens appears for the current page, and suggestions from
    omnibox can work. Then go back and invoke the "Ask..." action again,
    and suggestions will appear because lens inputs are populated.
    
    Bug: 402771688, 400812940, 400952597
    Change-Id: Idf144fc7dff51c57853385e81383ebbbe8548dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384376
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437189}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <functional>
 #include <iterator>
 #include <optional>
+#include <ostream>
 #include <sstream>
 #include <string>
 #include <unordered_set>
@@ -886,23 +887,16 @@ void AutocompleteResult::AttachPedalsToMatches(
   }
 }
 
-void AutocompleteResult::AttachContextualSearchActionToMatches() {
+void AutocompleteResult::AttachContextualSearchFulfillmentActionToMatches() {
   for (AutocompleteMatch& match : matches_) {
-    if (match.subtypes.contains(omnibox::SUBTYPE_ZERO_PREFIX)) {
-      if (match.subtypes.contains(omnibox::SUBTYPE_CONTEXTUAL_SEARCH)) {
-        match.takeover_action = base::MakeRefCounted<ContextualSearchAction>(
-            match.destination_url, match.type,
-            match.subtypes.contains(omnibox::SUBTYPE_ZERO_PREFIX));
-      }
-      continue;
+    if ((match.provider && match.provider->type() ==
+                               AutocompleteProvider::TYPE_CONTEXTUAL_SEARCH) ||
+        match.subtypes.contains(omnibox::SUBTYPE_CONTEXTUAL_SEARCH)) {
+      match.takeover_action =
+          base::MakeRefCounted<ContextualSearchFulfillmentAction>(
+              match.destination_url, match.type,
+              match.subtypes.contains(omnibox::SUBTYPE_ZERO_PREFIX));
     }
-    // TODO(crbug.com/400952597): Add a check on the provider type once one
-    // provider is created for @page suggestions. That also means we can cleanup
-    // the above logic to only check if its from the contextual search provider
-    // or has the subtype omnibox::SUBTYPE_CONTEXTUAL_SEARCH.
-    match.takeover_action = base::MakeRefCounted<ContextualSearchAction>(
-        match.destination_url, match.type,
-        match.subtypes.contains(omnibox::SUBTYPE_ZERO_PREFIX));
   }
 }
 
@@ -1659,4 +1653,15 @@ void AutocompleteResult::GroupSuggestionsBySearchVsURL(iterator begin,
 
   std::ranges::stable_sort(begin, end, {},
                            [](const auto& m) { return m.GetSortingOrder(); });
+}
+
+std::ostream& operator<<(std::ostream& os, const AutocompleteResult& result) {
+  os << "AutocompleteResult {" << std::endl;
+  for (size_t i = 0; i < result.matches_.size(); i++) {
+    const AutocompleteMatch& match = result.matches_[i];
+    os << "  - " << i << ": `" << match.contents << "`"
+       << (match.allowed_to_be_default_match ? '*' : ' ') << std::endl;
+  }
+  os << "}" << std::endl;
+  return os;
 }
