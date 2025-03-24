e86365df65a9758e19d77e6a43ae4bdac8fd841d
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-03-24 08:15:38 -0700

    Fix typo in page_action_view.cc and page-action_container_view.cc
    Change-Id: I4aa19171f4cdd77d7a543ebfba15b64847b9b92b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383309
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436850}

--- chrome/browser/ui/views/page_action/page_action_view.h
+++ chrome/browser/ui/views/page_action/page_action_view.h
@@ -49,7 +49,7 @@ class PageActionView : public IconLabelBubbleView,
   bool IsChipVisible() const;
 
   using ChipVisibilityChanged = base::RepeatingCallback<void(PageActionView*)>;
-  base::CallbackListSubscription AddChipVisibiltyChangedCallback(
+  base::CallbackListSubscription AddChipVisibilityChangedCallback(
       ChipVisibilityChanged callback);
 
   // PageActionModelObserver:
