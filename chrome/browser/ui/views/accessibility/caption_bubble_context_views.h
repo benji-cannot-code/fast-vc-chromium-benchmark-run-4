1aeaa7698a6620c40e773b0c2d93d4195069568a
Evan Liu <evliu@google.com>
2025-03-24 16:15:41 -0700

    Hide the back-to-tab button in the Live Caption bubble if the audio is from the active tab
    This CL hides the back-to-tab button if the active tab the same tab as the one that is currently being transcribed.
    
    Fixed: 397157079
    Change-Id: I55844e8dc5cda1c8599dbb4e7e4f29d4c1f53054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313877
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437177}

--- chrome/browser/ui/views/accessibility/caption_bubble_context_views.h
+++ chrome/browser/ui/views/accessibility/caption_bubble_context_views.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/accessibility/caption_bubble_context_browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 
 namespace content {
 class WebContents;
@@ -18,13 +19,15 @@ class WebContents;
 namespace captions {
 
 class CaptionBubbleSessionObserverViews;
+class CaptionBubble;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Caption Bubble Context for Views
 //
 //  The implementation of the Caption Bubble Context for Views.
 //
-class CaptionBubbleContextViews : public CaptionBubbleContextBrowser {
+class CaptionBubbleContextViews : public CaptionBubbleContextBrowser,
+                                  public TabStripModelObserver {
  public:
   explicit CaptionBubbleContextViews(content::WebContents* web_contents);
   ~CaptionBubbleContextViews() override;
@@ -40,11 +43,21 @@ class CaptionBubbleContextViews : public CaptionBubbleContextBrowser {
   std::unique_ptr<CaptionBubbleSessionObserver>
   GetCaptionBubbleSessionObserver() override;
   OpenCaptionSettingsCallback GetOpenCaptionSettingsCallback() override;
+  void SetContextActivatabilityObserver(CaptionBubble* caption_bubble) override;
+  void RemoveContextActivatabilityObserver() override;
+
+  // TabStripModelObserver overrides:
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
 
  private:
   void OpenCaptionSettings();
 
   raw_ptr<content::WebContents, AcrossTasksDanglingUntriaged> web_contents_;
+
+  raw_ptr<CaptionBubble> caption_bubble_;
 
   std::unique_ptr<CaptionBubbleSessionObserver> web_contents_observer_;
 };
