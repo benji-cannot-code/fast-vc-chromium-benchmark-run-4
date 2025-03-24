71d325162dfe8a8a32cb0f6ecc870eaecb4c2b2d
Ernesto Izquierdo Clua <eic@google.com>
2025-03-24 08:04:30 -0700

    Inject ManagementService into enterprise url lookup service
    Remove a dependency on Profile by injecting the Profile-bound
    ManagementService on initialization.
    
    This is part of the effort to move the enterprise service to components
    so it can be used for iOS Url filtering.
    
    Bug: 399376916
    Change-Id: Ib42190482e37284a641dc56ba151dbe0966625f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383140
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436841}

--- chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
+++ chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/enterprise/connectors/connectors_service.h"
 #include "chrome/browser/enterprise/util/affiliation.h"
@@ -80,6 +79,7 @@ ChromeEnterpriseRealTimeUrlLookupService::
         ReferrerChainProvider* referrer_chain_provider,
         PrefService* pref_service,
         signin::IdentityManager* identity_manager,
+        policy::ManagementService* management_service,
         bool is_off_the_record,
         bool is_guest_session)
     : RealTimeUrlLookupServiceBase(
@@ -94,6 +94,7 @@ ChromeEnterpriseRealTimeUrlLookupService::
       token_fetcher_(std::move(token_fetcher)),
       pref_service_(pref_service),
       identity_manager_(identity_manager),
+      management_service_(management_service),
       is_off_the_record_(is_off_the_record),
       is_guest_session_(is_guest_session) {}
 
@@ -113,9 +114,8 @@ bool ChromeEnterpriseRealTimeUrlLookupService::
 
   // Don't allow using the access token if the managed profile doesn't match the
   // managed device.
-  if (policy::ManagementServiceFactory::GetForProfile(profile_)
-          ->HasManagementAuthority(
-              policy::EnterpriseManagementAuthority::CLOUD_DOMAIN) &&
+  if (management_service_->HasManagementAuthority(
+          policy::EnterpriseManagementAuthority::CLOUD_DOMAIN) &&
       !enterprise_util::IsProfileAffiliated(profile_)) {
     return false;
   }
--- chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service_factory.cc
+++ chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service_factory.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service_factory.h"
 
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/enterprise/browser_management/management_service_factory.h"
+#include "chrome/browser/enterprise/connectors/connectors_service.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
@@ -24,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/utils.h"
 #include "content/public/browser/browser_context.h"
 #include "services/network/public/cpp/cross_thread_pending_shared_url_loader_factory.h"
-#include "chrome/browser/enterprise/connectors/connectors_service.h"
 
 namespace safe_browsing {
 
@@ -60,6 +61,7 @@ ChromeEnterpriseRealTimeUrlLookupServiceFactory::
   DependsOn(enterprise_connectors::ConnectorsServiceFactory::GetInstance());
   DependsOn(SafeBrowsingNavigationObserverManagerFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
+  DependsOn(policy::ManagementServiceFactory::GetInstance());
 }
 
 std::unique_ptr<KeyedService> ChromeEnterpriseRealTimeUrlLookupServiceFactory::
@@ -83,6 +85,7 @@ std::unique_ptr<KeyedService> ChromeEnterpriseRealTimeUrlLookupServiceFactory::
       SafeBrowsingNavigationObserverManagerFactory::GetForBrowserContext(
           profile),
       profile->GetPrefs(), IdentityManagerFactory::GetForProfile(profile),
+      policy::ManagementServiceFactory::GetForProfile(profile),
       profile->IsOffTheRecord(), profile->IsGuestSession());
 }
 
