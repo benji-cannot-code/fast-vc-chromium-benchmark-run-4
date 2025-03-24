c8081ea7655c53f8a396dcd0035b8fe9a30c1421
Shu Yang <shuyng@google.com>
2025-03-24 14:42:16 -0700

    Record pdf transient download time
    Used to assess the prioritization of a refined user experience for
    downloading.
    
    Bug: 405822870
    Change-Id: I6977bd36d299b7dddbf2152cc5d5674e79bb18de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6385437
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437109}

--- chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfPage.java
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfPage.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.pdf;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.os.SystemClock;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -25,6 +26,7 @@ public class PdfPage extends BasicNativePage {
     private final String mUrl;
     private boolean mIsIncognito;
     private boolean mIsDownloadSafe;
+    private long mTransientDownloadStartTimestamp;
 
     /**
      * Create a new instance of the pdf page.
@@ -61,6 +63,10 @@ public class PdfPage extends BasicNativePage {
         mPdfCoordinator = new PdfCoordinator(profile, activity, filepath, tabId);
         mIsIncognito = profile.isOffTheRecord();
         initWithView(mPdfCoordinator.getView());
+        // PDF is downloading when the filepath is null.
+        if (filepath == null) {
+            mTransientDownloadStartTimestamp = SystemClock.elapsedRealtime();
+        }
     }
 
     @Override
@@ -114,6 +120,8 @@ public class PdfPage extends BasicNativePage {
     public void onDownloadComplete(String pdfFileName, String pdfFilePath, boolean isDownloadSafe) {
         mTitle = pdfFileName;
         mIsDownloadSafe = isDownloadSafe;
+        PdfUtils.recordPdfTransientDownloadTime(
+                SystemClock.elapsedRealtime() - mTransientDownloadStartTimestamp);
         // TODO(b/348701300): check if pdf should be opened inline.
         if (mIsIncognito) {
             Uri uri = PdfContentProvider.createContentUri(pdfFilePath, pdfFileName);
