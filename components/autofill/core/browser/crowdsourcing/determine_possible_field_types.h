02fea5c29291e8c550b31f995067ab3bcc7444d2
Christoph Schwering <schwering@google.com>
2025-03-23 23:50:02 -0700

    [Autofill] Vote for format strings of complete dates
    This CL adds voting for format strings that match dates in single
    fields. For example,
      <input type=text value=31/12/2025>
    leads to votes for DD/MM/YYYY and D/M/YYYY.
    
    Voting is still limited to cases where there is a relevant field type
    on file and form contains at least one relevant field type.
    
    Bug: 396325496
    Change-Id: Idf3f5861a61f119e40420bab6f106e55273fc471
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382261
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436696}

--- components/autofill/core/browser/crowdsourcing/determine_possible_field_types.h
+++ components/autofill/core/browser/crowdsourcing/determine_possible_field_types.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_CROWDSOURCING_DETERMINE_POSSIBLE_FIELD_TYPES_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_CROWDSOURCING_DETERMINE_POSSIBLE_FIELD_TYPES_H_
 
+#include <map>
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_set.h"
+#include "components/autofill/core/browser/autofill_field.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/common/unique_ids.h"
 
 namespace autofill {
 
@@ -37,6 +41,17 @@ void DeterminePossibleFieldTypesForUpload(
     const std::u16string& last_unlocked_credit_card_cvc,
     const std::string& app_locale,
     FormStructure& form);
+
+// Matches the current field values against regular expressions.
+//
+// For example, a field value "09/03/2025" matches "DD/MM/YYYY" and
+// "MM/DD/YYYY".
+//
+// TODO(crbug.com/396325496): Add matching of dates split across sequences of
+// fields.
+std::map<FieldGlobalId, base::flat_set<std::u16string>>
+DeterminePossibleFormatStringsForUpload(
+    base::span<const std::unique_ptr<AutofillField>> fields);
 
 }  // namespace autofill
 
