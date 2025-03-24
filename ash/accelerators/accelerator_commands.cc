3022e8ad4840484d1c31507e463c013f43a82128
Michael Cui <mlcui@google.com>
2025-03-23 20:21:25 -0700

    scanner: Add metrics for non-debug keyboard shortcut
    Bug: b:393140991, b:381766720
    Change-Id: I6a6a210062881c62bd6b5bdc9f06da3985adcda4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367566
    Reviewed-by: Min Chen <minch@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436674}

--- ash/accelerators/accelerator_commands.cc
+++ ash/accelerators/accelerator_commands.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_insert/quick_insert_controller.h"
 #include "ash/root_window_controller.h"
 #include "ash/rotator/window_rotation.h"
+#include "ash/scanner/scanner_metrics.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_focus_cycler.h"
@@ -1268,7 +1269,8 @@ void ShowTaskManager() {
 }
 
 void StartSunfishSession() {
-  // TODO: crbug.com/393140991 - Record a metric here.
+  RecordScannerFeatureUserState(
+      ScannerFeatureUserState::kSunfishSessionStartedFromKeyboardShortcut);
   CaptureModeController::Get()->StartSunfishSession();
 }
 
