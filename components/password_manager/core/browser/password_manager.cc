c2e255b2cbe010dd6a7ef10001a6f523c822701c
Viktor Semeniuk <vsemeniuk@google.com>
2025-03-24 09:05:23 -0700

    Inline usage of visible_forms_data in PasswordManager
    There is no justification for visible_forms_data_ being a member of
    PasswordManager. This CL removes it and uses visible_forms_data inline.
    
    Change-Id: I58a88be6c7b27ab36227c1d4cc23b7e5cdefc9c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387396
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436887}

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -696,7 +696,6 @@ void PasswordManager::UpdateFormManagers() {
 void PasswordManager::DropFormManagers() {
   ResetFormsAndPredictionsCache();
   owned_submitted_form_manager_.reset();
-  visible_forms_data_.clear();
 }
 
 base::span<const PasswordForm> PasswordManager::GetBestMatches(
@@ -1343,10 +1342,6 @@ void PasswordManager::OnPasswordFormsRendered(
                       visible_forms_data.size());
   }
 
-  // Record all visible forms from the frame.
-  visible_forms_data_.insert(visible_forms_data_.end(),
-                             visible_forms_data.begin(),
-                             visible_forms_data.end());
   if (
 #if BUILDFLAG(IS_IOS)
       // On iOS, drivers are bound to WebFrames, but some pages (e.g. files)
@@ -1365,7 +1360,7 @@ void PasswordManager::OnPasswordFormsRendered(
   // If we see the login form again, then the login failed.
   if (submitted_manager->GetPendingCredentials().scheme ==
       PasswordForm::Scheme::kHtml) {
-    for (const FormData& form_data : visible_forms_data_) {
+    for (const FormData& form_data : visible_forms_data) {
       if (submitted_manager->IsEqualToSubmittedForm(form_data)) {
         if (submitted_manager->HasLikelyChangeOrResetFormSubmitted() &&
             AreChangePasswordFieldsEmpty(
@@ -1377,8 +1372,6 @@ void PasswordManager::OnPasswordFormsRendered(
                               form_data);
         }
         OnLoginFailed(logger.get());
-        // Clear visible_forms_data_ once we found the match.
-        visible_forms_data_.clear();
         return;
       }
     }
@@ -1387,8 +1380,6 @@ void PasswordManager::OnPasswordFormsRendered(
       logger->LogMessage(Logger::STRING_PROVISIONALLY_SAVED_FORM_IS_NOT_HTML);
     }
   }
-  // Clear visible_forms_data_ after checking all the visible forms.
-  visible_forms_data_.clear();
 
   // Looks like a successful login attempt. Either show an infobar or
   // automatically save the login data. We prompt when the user hasn't
