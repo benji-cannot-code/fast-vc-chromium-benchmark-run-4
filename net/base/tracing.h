e9a3567fbfa39d0605381849dcf7ba2b63495395
Etienne Dechamps <edechamps@google.com>
2025-03-24 05:42:09 -0700

    Use enable_base_tracing to gate the trace stubs
    This more accurately reflects the requirements for this header to work.
    
    Bug: b:346770535, 40287068
    Change-Id: I529114590bb41e5dccbded11e05848f11356f7f9
    Cq-Include-Trybots: luci.chromium.try:android-cronet-x64-dbg-15-tests,android-cronet-x86-dbg-marshmallow-tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372667
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436769}

--- net/base/tracing.h
+++ net/base/tracing.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "net/base/cronet_buildflags.h"
 
-#if BUILDFLAG(CRONET_BUILD)
+#if !BUILDFLAG(ENABLE_BASE_TRACING)
 #include "net/base/trace_event_stub.h"  // IWYU pragma: export
-#endif                                  // BUILDFLAG(CRONET_BUILD)
+#endif                                  // BUILDFLAG(ENABLE_BASE_TRACING)
 
 #include "base/trace_event/base_tracing.h"  // IWYU pragma: export
 
