85d12794280227a8c0d90699ca86d5ad39569992
Norge Vizcay <vizcay@google.com>
2025-03-24 03:59:01 -0700

    Fix loyalty card class description typo.
    Change-Id: Ie5eec5b385d97cd323b9cf45957637ca58859ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383095
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436752}

--- components/autofill/core/browser/data_model/valuables/loyalty_card.h
+++ components/autofill/core/browser/data_model/valuables/loyalty_card.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-// Represents a loaylty card coming from the Google Wallet.
+// Represents a loyalty card coming from the Google Wallet.
 class LoyaltyCard final {
  public:
   LoyaltyCard(std::string loyalty_card_id,
