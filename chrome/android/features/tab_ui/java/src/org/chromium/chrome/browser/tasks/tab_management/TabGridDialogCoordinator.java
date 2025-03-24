4f52c970ed652b6c819d827937e6313f47564cdb
Madhav Pruthi <madhavpruthi@google.com>
2025-03-24 11:13:15 -0700

    Modify the background color of shared image tiles on the tab group dialog
    Fixed dialog SS - https://photos.app.goo.gl/j38riSrssymdaEuH9
    
    Bug: 383319095
    Change-Id: I5d33b9c779324945ef31f1536ba35fb460790042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384475
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436983}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogCoordinator.java
@@ -16,6 +16,7 @@ import android.view.ViewStub;
 import android.widget.FrameLayout;
 import android.widget.PopupWindow;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
@@ -50,6 +51,7 @@ import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.desktop_windowing.DesktopWindowStateManager;
+import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.scrim.ScrimManager;
 import org.chromium.components.collaboration.CollaborationService;
 import org.chromium.components.collaboration.ServiceStatus;
@@ -171,8 +173,13 @@ public class TabGridDialogCoordinator implements TabGridDialogMediator.DialogCon
                 DataSharingService dataSharingService =
                         DataSharingServiceFactory.getForProfile(originalProfile);
 
+                @ColorInt
+                int backgroundColor = SemanticColorUtils.getDialogBgColor(mDialogView.getContext());
                 SharedImageTilesConfig config =
-                        new SharedImageTilesConfig.Builder(activity).build();
+                        new SharedImageTilesConfig.Builder(activity)
+                                .setBorderColor(backgroundColor)
+                                .setBackgroundColor(backgroundColor)
+                                .build();
                 mSharedImageTilesCoordinator =
                         new SharedImageTilesCoordinator(
                                 activity, config, dataSharingService, collaborationService);
