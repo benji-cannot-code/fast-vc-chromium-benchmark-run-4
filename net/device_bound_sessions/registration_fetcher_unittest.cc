c1101462a900931aa3894899c89297747f084300
Alex Ilin <alexilin@chromium.org>
2025-03-24 10:10:11 -0700

    [DBSC] Add TPM operations retries to LST binding
    Enable the recently added support for TPM operation retries for the LST
    binding feature. No changes for other features using
    UnexportableKeyService.
    
    The maximum number of retries is passed as a
    `unexportable_keys::UnexportableKeyService::SignSlowlyAsync()` parameter
    instead of being hardcoded. This allows interested
    UnexportableKeyService clients to opt-in individually. Some of the
    clients have already implemented a retry policy outside of
    UnexportableKeyService, so they can keep using max_retries=0 for now.
    
    Fixed: 400903525
    Change-Id: I1bf8a3208b7349395444cd55262649fa75a24d77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368442
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436934}

--- net/device_bound_sessions/registration_fetcher_unittest.cc
+++ net/device_bound_sessions/registration_fetcher_unittest.cc
@@ -1187,8 +1187,8 @@ TEST_F(RegistrationTest, RetriesOnKeyFailure) {
       .WillRepeatedly(Invoke(
           &unexportable_key_service(),
           &unexportable_keys::UnexportableKeyService::GetSubjectPublicKeyInfo));
-  EXPECT_CALL(mock_service, SignSlowlyAsync(_, _, _, _))
-      .WillOnce(base::test::RunOnceCallback<3>(
+  EXPECT_CALL(mock_service, SignSlowlyAsync(_, _, _, _, _))
+      .WillOnce(base::test::RunOnceCallback<4>(
           base::unexpected(unexportable_keys::ServiceError::kCryptoApiFailed)))
       .WillOnce(
           Invoke(&unexportable_key_service(),
@@ -1225,8 +1225,8 @@ TEST_F(RegistrationTest, TerminateSessionOnRepeatedFailure_Refresh) {
       .WillRepeatedly(Invoke(
           &unexportable_key_service(),
           &unexportable_keys::UnexportableKeyService::GetSubjectPublicKeyInfo));
-  EXPECT_CALL(mock_service, SignSlowlyAsync(_, _, _, _))
-      .WillRepeatedly(base::test::RunOnceCallbackRepeatedly<3>(
+  EXPECT_CALL(mock_service, SignSlowlyAsync(_, _, _, _, _))
+      .WillRepeatedly(base::test::RunOnceCallbackRepeatedly<4>(
           base::unexpected(unexportable_keys::ServiceError::kCryptoApiFailed)));
 
   TestRegistrationCallback callback;
@@ -1262,8 +1262,8 @@ TEST_F(RegistrationTest, TerminateSessionOnRepeatedFailure_Registration) {
       .WillRepeatedly(Invoke(
           &unexportable_key_service(),
           &unexportable_keys::UnexportableKeyService::GetSubjectPublicKeyInfo));
-  EXPECT_CALL(mock_service, SignSlowlyAsync(_, _, _, _))
-      .WillRepeatedly(base::test::RunOnceCallbackRepeatedly<3>(
+  EXPECT_CALL(mock_service, SignSlowlyAsync(_, _, _, _, _))
+      .WillRepeatedly(base::test::RunOnceCallbackRepeatedly<4>(
           base::unexpected(unexportable_keys::ServiceError::kCryptoApiFailed)));
 
   TestRegistrationCallback callback;
