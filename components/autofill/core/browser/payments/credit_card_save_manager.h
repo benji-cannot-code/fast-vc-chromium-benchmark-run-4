82260b788f7f16924ba63989a1b2077d7a66ec5e
Olivia Saul <jsaul@google.com>
2025-03-24 18:07:47 -0700

    [AF] Remove local card migration strike database
    This is CL 5/6 in removing Bulk Local Card Migration flows.
    
    OBSOLETE_HISTOGRAMS=Feature deprecated in Q1 2025.
    
    Bug: 377383176
    Change-Id: Idfe070b919a2aed3ab24b8197312317c8faad5b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382697
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437244}

--- components/autofill/core/browser/payments/credit_card_save_manager.h
+++ components/autofill/core/browser/payments/credit_card_save_manager.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_request_details.h"
 #include "components/autofill/core/browser/strike_databases/payments/credit_card_save_strike_database.h"
 #include "components/autofill/core/browser/strike_databases/payments/cvc_storage_strike_database.h"
-#include "components/autofill/core/browser/strike_databases/payments/local_card_migration_strike_database.h"
 #include "url/origin.h"
 
 class SaveCardOfferObserver;
@@ -184,7 +183,6 @@ class CreditCardSaveManager {
  private:
   friend class CreditCardSaveManagerTest;
   friend class CreditCardSaveManagerTestObserverBridge;
-  friend class LocalCardMigrationBrowserTest;
   friend class TestCreditCardSaveManager;
   friend class SaveCardBubbleViewsFullFormBrowserTest;
   friend class FakeCreditCardServer;
@@ -213,11 +211,6 @@ class CreditCardSaveManager {
   // Query the CvcStorageStrikeDatabase to check if the offer-to-save prompt for
   // this CVC should be blocked.
   bool DetermineAndLogCvcSaveStrikeDatabaseBlockDecision();
-
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
-  // Returns the GetLocalCardMigrationStrikeDatabase for |client_|.
-  LocalCardMigrationStrikeDatabase* GetLocalCardMigrationStrikeDatabase();
-#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
   // Returns the legal message retrieved from Payments. On failure or not
   // meeting Payments's conditions for upload, |legal_message| will contain
@@ -421,11 +414,6 @@ class CreditCardSaveManager {
   // during a card import to determine the name and country for storing a new
   // card.
   std::vector<AutofillProfile> preliminarily_imported_address_profiles_;
-
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
-  std::unique_ptr<LocalCardMigrationStrikeDatabase>
-      local_card_migration_strike_database_;
-#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
   // May be null.
   raw_ptr<ObserverForTest> observer_for_testing_ = nullptr;
