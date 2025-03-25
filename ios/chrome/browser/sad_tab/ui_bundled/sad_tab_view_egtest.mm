57b9d8df8484ca5284195a5f9925bc345b4ef723
Ameur Hosni <ameurhosni@google.com>
2025-03-25 09:33:26 -0700

    [IOS][Code Health] Move popup_menu to ui_bundled
    Fixed: 324232765
    Change-Id: Iec4fb3806880e15327a49ad519a1a1e531795626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6388346
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437540}

--- ios/chrome/browser/sad_tab/ui_bundled/sad_tab_view_egtest.mm
+++ ios/chrome/browser/sad_tab/ui_bundled/sad_tab_view_egtest.mm
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "components/strings/grit/components_strings.h"
-#import "ios/chrome/browser/ui/popup_menu/popup_menu_constants.h"
+#import "ios/chrome/browser/popup_menu/ui_bundled/popup_menu_constants.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
--- ios/chrome/browser/toolbar/ui_bundled/adaptive_toolbar_view_controller.h
+++ ios/chrome/browser/toolbar/ui_bundled/adaptive_toolbar_view_controller.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_ui_element.h"
+#import "ios/chrome/browser/popup_menu/ui_bundled/public/popup_menu_ui_updating.h"
 #import "ios/chrome/browser/toolbar/ui_bundled/public/toolbar_type.h"
 #import "ios/chrome/browser/toolbar/ui_bundled/toolbar_consumer.h"
-#import "ios/chrome/browser/ui/popup_menu/public/popup_menu_ui_updating.h"
 
 @protocol AdaptiveToolbarMenusProvider;
 @class AdaptiveToolbarViewController;
