21ee63761d710f1a0c25a166a9732964b3ea241d
Marc Treib <treib@chromium.org>
2025-03-25 04:41:16 -0700

    Revert "unzip_service: Add support for decompressing xz files."
    This reverts commit 4f54359a8860a911ffd3e8238bb283ceed4e7e36.
    
    Reason for revert: Seems to have broken the build on Fuchsia, see https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-cast-receiver-rel/22032/overview
    
    Original change's description:
    > unzip_service: Add support for decompressing xz files.
    >
    > This will be used by components/update_client in a future CL.
    >
    > Bug: 399619105
    > Change-Id: I940a857bcecf8dc722eb68d1467adfceb6c7b7bd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378645
    > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1437403}
    
    Bug: 399619105
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If2b822091f6d1f1cd7266180def6bdf3eedd2c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6388347
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437410}

--- components/services/unzip/unzipper_impl.h
+++ components/services/unzip/unzipper_impl.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SERVICES_UNZIP_UNZIPPER_IMPL_H_
 
 #include "base/files/file.h"
-#include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
@@ -51,10 +50,6 @@ class UnzipperImpl : public mojom::Unzipper {
 
   void GetExtractedInfo(base::File zip_file,
                         GetExtractedInfoCallback callback) override;
-
-  void DecodeXz(base::File in_file,
-                base::File out_file,
-                base::OnceCallback<void(bool)> callback) override;
 
   // Disconnect handler for the receiver.
   void OnReceiverDisconnect();
