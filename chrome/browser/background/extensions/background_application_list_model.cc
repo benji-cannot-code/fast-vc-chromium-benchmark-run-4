4a5478172b6f213336c8a841b57f571ee72f852e
Devon Loehr <dloehr@google.com>
2025-03-25 00:11:50 -0700

    Remove unnecessary virtual specifiers
    Clang has added a new warning for virtual specifiers on methods of
    `final` classes. This cleans them up in preparation for enabling that
    warning.
    
    This CL was uploaded by git cl split.
    
    R=hidehiko@chromium.org
    
    Bug: chromium:403236787
    Change-Id: I99c0831115678c0eebdb0c44d4fd063a9e15640f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6388187
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Devon Loehr <dloehr@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437335}

--- chrome/browser/background/extensions/background_application_list_model.cc
+++ chrome/browser/background/extensions/background_application_list_model.cc
@@ -67,7 +67,7 @@ class BackgroundApplicationListModel::Application final {
   Application(BackgroundApplicationListModel* model,
               const Extension* an_extension);
 
-  virtual ~Application();
+  ~Application();
 
   // Invoked when a request icon is available.
   void OnImageLoaded(const gfx::Image& image);
