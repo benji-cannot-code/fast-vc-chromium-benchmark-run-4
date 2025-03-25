3daae7a404a07c6682937126d70a05f58ff458bd
Lei Zhang <thestig@chromium.org>
2025-03-25 16:54:02 -0700

    Remove debugging code from SetRegValueWorkItem::DoImpl()
    The crash stopped happening 4 years ago.
    
    Bug: 40706274
    Change-Id: I26601f5fedeb5d07af93120c0dcf18f69f9ff17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394621
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437840}

--- chrome/installer/util/set_reg_value_work_item.cc
+++ chrome/installer/util/set_reg_value_work_item.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/installer/util/set_reg_value_work_item.h"
 
-#include "base/debug/alias.h"
 #include "base/logging.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -158,13 +157,6 @@ bool SetRegValueWorkItem::DoImpl() {
     if (!size) {
       previous_type_ = type;
     } else {
-      // TODO(crbug.com/40706274): Remove after bug is resolved.
-      DEBUG_ALIAS_FOR_CSTR(key_path_copy, base::WideToUTF8(key_path_).c_str(),
-                           255);
-      DEBUG_ALIAS_FOR_CSTR(value_name_copy,
-                           base::WideToUTF8(value_name_).c_str(), 200);
-      base::debug::Alias(&size);
-      base::debug::Alias(&type);
       previous_value_.resize(size);
       result = key.ReadValue(value_name_.c_str(), &previous_value_[0], &size,
                              &previous_type_);
