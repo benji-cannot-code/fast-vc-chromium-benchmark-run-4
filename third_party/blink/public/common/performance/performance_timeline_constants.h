da0cd63bde961442e167705b15727ffed9eb6b87
Johannes Henkel <johannes@chromium.org>
2025-03-25 16:14:49 -0700

    inline blink::kSoftNavigationCountDefaultValue just in case
    Marking as inline ensures there's only one copy of the variable.
    I'm not completely sure it's needed since it's "just" an int,
    but according to our c++ best practices constexpr variables
    without inline in the header could cause ODR violations (and subtle bugs).
    
    Change-Id: I6dc25b1f7edfb0b2152ea6aaaf8b5ebafcd42a82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394620
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437811}

--- third_party/blink/public/common/performance/performance_timeline_constants.h
+++ third_party/blink/public/common/performance/performance_timeline_constants.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-constexpr uint32_t kSoftNavigationCountDefaultValue = 0;
+inline constexpr uint32_t kSoftNavigationCountDefaultValue = 0;
 
 struct SoftNavigationMetrics {
   uint32_t count = kSoftNavigationCountDefaultValue;
