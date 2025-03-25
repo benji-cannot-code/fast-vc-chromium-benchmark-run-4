71fa411a4016dc4310ccd7cbbe3b1b0632c853f6
Darren Wu <darrenwu@google.com>
2025-03-24 18:00:29 -0700

    pgo: Add connect-over-swarming-bot for remote ADB profile generation
    This adds a flag to `generate_profile.py` for testing PGO profile generation
    with ADB from ChromeOS swarming bots (where the ADB host is SWARMING_BOT_ID).
    
    BUG=386710478
    
    Change-Id: I282214be56b73cbef1373cb7a1b3e47395081779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6350783
    Auto-Submit: Darren Wu <darrenwu@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Darren Wu <darrenwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437236}

--- tools/pgo/generate_profile.py
+++ tools/pgo/generate_profile.py
@@ -86,6 +86,7 @@ class OptionsNamespace(argparse.Namespace):
     # The following are bot-specific args.
     isolated_script_test_output: Optional[str]
     isolated_script_test_perf_output: Optional[str]
+    android_hostname: str
 
 
 def parse_args():
@@ -154,6 +155,8 @@ def parse_args():
                         help='Output.json file that the script can write to.')
     parser.add_argument('--isolated-script-test-perf-output',
                         help='Deprecated and ignored, but bots pass it.')
+    parser.add_argument("--android-hostname",
+                        help="Run benchmarks with adb hostname.")
     # ▲▲▲▲▲ Please update OptionsNamespace when adding or modifying args. ▲▲▲▲▲
 
     args = parser.parse_args(namespace=OptionsNamespace())
@@ -304,6 +307,10 @@ def run_benchmark(benchmark_args: List[str], args: OptionsNamespace):
             cmd += ['--assume-browser-already-installed']
         else:
             _android_browser_installed = True
+
+        if args.android_hostname:
+            cmd += [f"--android={args.android_hostname}"]
+
         _LOGGER.debug(
             f"Running benchmark on Android with command: {' '.join(cmd)}")
     else:
