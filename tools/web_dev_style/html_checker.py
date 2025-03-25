4c369137f697292fd8e467fbf4460c0f0028d24c
Gary Tong <gatong@chromium.org>
2025-03-25 12:34:29 -0700

    Fix SyntaxWarnings in presubmit.py
    Python 3.11 generates SyntaxWarnings for invalid escape sequences.
    
    Bug: 397963615
    Change-Id: I6919559dae57f08e1861e5113434106a98e5fb5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6392355
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Gary Tong <gatong@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437675}

--- tools/web_dev_style/html_checker.py
+++ tools/web_dev_style/html_checker.py
@@ -19,7 +19,7 @@ class HtmlChecker(object):
   def ClassesUseDashFormCheck(self, line_number, line):
     msg = "Classes should use dash-form."
     re = self.input_api.re
-    class_regex = re.compile("""
+    class_regex = re.compile(r"""
         (?:^|\s)                    # start of line or whitespace
         (class="[^"]*[A-Z_][^"]*")  # class contains caps or '_'
         """,
@@ -27,7 +27,7 @@ class HtmlChecker(object):
 
     # $i18n{...} messes with highlighting. Special path for this.
     if "$i18n{" in line:
-      match = re.search(class_regex, re.sub("\$i18n{[^}]+}", "", line))
+      match = re.search(class_regex, re.sub(r"\$i18n{[^}]+}", "", line))
       return "  line %d: %s" % (line_number, msg) if match else ""
 
     return regex_check.RegexCheck(re, line_number, line, class_regex, msg)
@@ -43,7 +43,7 @@ class HtmlChecker(object):
         "Do not use <br>; place blocking elements (<div>) as appropriate.")
 
   def DoNotUseInputTypeButtonCheck(self, line_number, line):
-    regex = self.input_api.re.compile("""
+    regex = self.input_api.re.compile(r"""
         (<input [^>]*  # "<input " followed by anything but ">"
         type="button"  # type="button"
         [^>]*>)        # anything but ">" then ">"
@@ -53,7 +53,7 @@ class HtmlChecker(object):
         'Use the button element instead of <input type="button">')
 
   def DoNotUseSingleQuotesCheck(self, line_number, line):
-    regex = self.input_api.re.compile("""
+    regex = self.input_api.re.compile(r"""
         <\S+                           # The tag name.
         (?:\s+\S+\$?="[^"]*"|\s+\S+)*  # Correctly quoted or non-value props.
         \s+(\S+\$?='[^']*')            # Find incorrectly quoted (foo='bar').
@@ -64,7 +64,7 @@ class HtmlChecker(object):
         'Use double quotes rather than single quotes in HTML properties')
 
   def I18nContentJavaScriptCaseCheck(self, line_number, line):
-    regex = self.input_api.re.compile("""
+    regex = self.input_api.re.compile(r"""
         (?:^|\s)                      # start of line or whitespace
         i18n-content="                # i18n-content="
         ([A-Z][^"]*|[^"]*[-_][^"]*)"  # starts with caps or contains '-' or '_'
@@ -74,7 +74,7 @@ class HtmlChecker(object):
         "For i18n-content use javaScriptCase.")
 
   def LabelCheck(self, line_number, line):
-    regex = self.input_api.re.compile("""
+    regex = self.input_api.re.compile(r"""
         (?:^|\s)     # start of line or whitespace
         <label[^>]+? # <label tag
         (for=)       # for=
