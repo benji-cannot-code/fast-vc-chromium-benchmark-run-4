81212e18b5fd4407c2bf06b98d321f42059ed245
Colin Blundell <blundell@chromium.org>
2025-03-25 00:49:55 -0700

    [//cc] Have BitmapRasterBufferImpl take in bool
    Rather than having it take in the current and previous resource IDs and
    compute whether there is previous content, have the caller do this. This
    will streamline folding this functionality into
    ZeroCopyRasterBufferImpl.
    
    Bug: 403372453
    Change-Id: Iedd26309a388990085bb581b5ac9147ce0dea3e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382087
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437344}

--- cc/raster/bitmap_raster_buffer_provider.cc
+++ cc/raster/bitmap_raster_buffer_provider.cc
@@ -30,10 +30,8 @@ namespace {
 class BitmapRasterBufferImpl : public RasterBuffer {
  public:
   BitmapRasterBufferImpl(ResourcePool::Backing* backing,
-                         uint64_t resource_content_id,
-                         uint64_t previous_content_id)
-      : resource_has_previous_content_(
-            resource_content_id && resource_content_id == previous_content_id),
+                         bool resource_has_previous_content)
+      : resource_has_previous_content_(resource_has_previous_content),
         backing_(backing) {}
   BitmapRasterBufferImpl(const BitmapRasterBufferImpl&) = delete;
   BitmapRasterBufferImpl& operator=(const BitmapRasterBufferImpl&) = delete;
@@ -102,8 +100,10 @@ BitmapRasterBufferProvider::AcquireBufferForRaster(
   }
   ResourcePool::Backing* backing = resource.backing();
 
-  return std::make_unique<BitmapRasterBufferImpl>(backing, resource_content_id,
-                                                  previous_content_id);
+  bool resource_has_previous_content =
+      resource_content_id && resource_content_id == previous_content_id;
+  return std::make_unique<BitmapRasterBufferImpl>(
+      backing, resource_has_previous_content);
 }
 
 void BitmapRasterBufferProvider::Flush() {}
