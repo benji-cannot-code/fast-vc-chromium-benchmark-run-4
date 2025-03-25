d14a09e6e8b5a0987e35d801f751eb4070a714af
Juan Mojica <juanmojica@google.com>
2025-03-25 15:26:56 -0700

    [lensoverlay] Ignore text received while selecting a new region.
    When simplified selection is enabled, text highlights will render if the
    user makes a new selection quickly after having already made one. This
    prevents that by ignoring text if it was received while selecting a new
    region.
    
    Change-Id: Iec22c81aaaf3fb4faf8bcc6331524d680b60de62
    Bug: 405413550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6388958
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437779}

--- chrome/browser/resources/lens/overlay/simplified_text_layer.ts
+++ chrome/browser/resources/lens/overlay/simplified_text_layer.ts
@@ -76,6 +76,8 @@ export class SimplifiedTextLayerElement extends CrLitElement implements
   // this class needing to call getBoundingClientRect()
   private selectionOverlayRect: DOMRect;
   private listenerIds: number[] = [];
+  // Whether the user is in the middle of selecting a new region.
+  private isSelectingRegion: boolean = false;
   // Timeout for onTextReceived for the full image response text. The selected
   // region context menu should not be shown until either the text is received
   // or the timeout elapses.      ;
@@ -160,6 +162,7 @@ export class SimplifiedTextLayerElement extends CrLitElement implements
   }
 
   onSelectionStart(): void {
+    this.isSelectingRegion = true;
     this.hasActionedText = false;
     // Hide highlighted lines but do not clear them in order to allow them to
     // fade out.
@@ -168,6 +171,7 @@ export class SimplifiedTextLayerElement extends CrLitElement implements
   }
 
   onSelectionFinish(): void {
+    this.isSelectingRegion = false;
     // Clear the previous region selection text response as a new selection has
     // been made. Also clear any timeouts that also pertained to the last region
     // response.
@@ -277,6 +281,12 @@ export class SimplifiedTextLayerElement extends CrLitElement implements
   }
 
   private onRegionTextReceived(text: Text) {
+    // If the user is currently selecting a new region, ignore any text received
+    // for the old region.
+    if (this.isSelectingRegion) {
+      return;
+    }
+
     // If there was rendered text, log a text gleam render end event.
     if (this.regionTextResponse &&
         this.regionTextResponse.receivedWords.length > 0) {
