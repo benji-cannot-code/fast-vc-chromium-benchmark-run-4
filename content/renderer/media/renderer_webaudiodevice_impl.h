1f68a9addaf034f0a6c72d588fb173976f6f50dd
Alvin Ji <alvinji@chromium.org>
2025-03-24 18:28:43 -0700

    WebAudio: Remove AudioDestination resampler when feature enabled
    This CL introduces the feature flag
    WebAudioRemoveAudioDestinationResampler. When enabled, this flag
    bypasses the resampling step within the AudioDestination node. This
    allows the AudioService to handle resampling, potentially reducing
    latency and overhead. Additionally, GetOutputBufferSize has been
    modified to scale hardware buffer sizes and enforce minimum and maximum
    buffer size limits.
    
    Design Doc: go/webaudio-sample-rate-reconciliation
    
    Bug: 374797496
    Change-Id: I91c2d3064ded355b09309da11589be4d8ae2f04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6279011
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437249}

--- content/renderer/media/renderer_webaudiodevice_impl.h
+++ content/renderer/media/renderer_webaudiodevice_impl.h
@@ -50,7 +50,11 @@ class CONTENT_EXPORT RendererWebAudioDeviceImpl
       const blink::WebAudioSinkDescriptor& sink_descriptor,
       int number_of_output_channels,
       const blink::WebAudioLatencyHint& latency_hint,
+      std::optional<float> context_sample_rate,
       media::AudioRendererSink::RenderCallback* webaudio_callback);
+  static int GetOutputBufferSize(const blink::WebAudioLatencyHint& latency_hint,
+                                 int resolved_context_sample_rate,
+                                 const media::AudioParameters& hardware_params);
 
   // blink::WebAudioDevice implementation.
   void Start() override;
@@ -105,6 +109,7 @@ class CONTENT_EXPORT RendererWebAudioDeviceImpl
       const blink::WebAudioSinkDescriptor& sink_descriptor,
       media::ChannelLayoutConfig layout_config,
       const blink::WebAudioLatencyHint& latency_hint,
+      std::optional<float> context_sample_rate,
       media::AudioRendererSink::RenderCallback* webaudio_callback,
       OutputDeviceParamsCallback device_params_cb,
       CreateSilentSinkCallback create_silent_sink_cb);
