5804dd2d719861e85ff1aadc2a22bc02ca139afa
Bruno Braga <brunobraga@google.com>
2025-03-25 02:44:10 -0700

    [AutofillAi] Remove separate updated/added fields section
    Bug: 389630809
    Change-Id: I6a2fae14144418687aaf29aa67fc429ca314f503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384076
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437375}

--- chrome/browser/ui/views/autofill/autofill_ai/save_or_update_autofill_ai_data_bubble_view.h
+++ chrome/browser/ui/views/autofill/autofill_ai/save_or_update_autofill_ai_data_bubble_view.h
@@ -22,9 +22,6 @@ class SaveOrUpdateAutofillAiDataBubbleView
   METADATA_HEADER(SaveOrUpdateAutofillAiDataBubbleView, views::View)
 
  public:
-  static constexpr int kNewEntityAddedOrUpdatedAttributesContainer = 437;
-  static constexpr int kNewEntityUnchagedAttributesContainer = 438;
-
   SaveOrUpdateAutofillAiDataBubbleView(
       views::View* anchor_view,
       content::WebContents* web_contents,
