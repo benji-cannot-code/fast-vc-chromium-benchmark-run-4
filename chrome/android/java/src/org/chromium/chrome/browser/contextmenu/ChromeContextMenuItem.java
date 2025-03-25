b80bc3fec522d4f4e4e15dbc6745b4e32009d748
Grace Cham <hscham@google.com>
2025-03-24 21:01:05 -0700

    [context menu] add page save option to empty space CM
    Screen record:
    https://screencast.googleplex.com/cast/NTc4MTc5OTAwMjE3NzUzNnwwNDE5MmJkMC00ZA
    
    Note: Only IDS_CONTEXTMENU_SAVE_PAGE is an UI string and the page group
    title is not used anywhere, hence skipping the translation screenshots
    check.
    
    Bug: 391719844
    Skip-Translation-Screenshots-Check: True
    Test: tools/autotest.py -C out/Default ChromeContextMenuPopulatorTest
    Change-Id: If047f6250f90092fe5bae8e25850adf020c92ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333204
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437290}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuItem.java
@@ -64,7 +64,8 @@ class ChromeContextMenuItem {
         Item.OPEN_IN_NEW_TAB_IN_GROUP,
         Item.SHARE_HIGHLIGHT,
         Item.REMOVE_HIGHLIGHT,
-        Item.LEARN_MORE
+        Item.LEARN_MORE,
+        Item.SAVE_PAGE,
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface Item {
@@ -112,8 +113,10 @@ class ChromeContextMenuItem {
         int SHARE_HIGHLIGHT = 32;
         int REMOVE_HIGHLIGHT = 33;
         int LEARN_MORE = 34;
+        // Page Group
+        int SAVE_PAGE = 35;
         // ALWAYS UPDATE!
-        int NUM_ENTRIES = 35;
+        int NUM_ENTRIES = 36;
     }
 
     /** Mapping from {@link Item} to the ID found in the ids.xml. */
@@ -153,6 +156,7 @@ class ChromeContextMenuItem {
         R.id.contextmenu_share_highlight, // Item.SHARE_HIGHLIGHT
         R.id.contextmenu_remove_highlight, // Item.REMOVE_HIGHLIGHT
         R.id.contextmenu_learn_more, // Item.LEARN_MORE
+        R.id.contextmenu_save_page, // Item.SAVE_PAGE
     };
 
     /** Mapping from {@link Item} to the ID of the string that describes the action of the item. */
@@ -192,6 +196,7 @@ class ChromeContextMenuItem {
         R.string.contextmenu_share_highlight, // Item.SHARE_HIGHLIGHT
         R.string.contextmenu_remove_highlight, // Item.REMOVE_HIGHLIGHT
         R.string.contextmenu_learn_more, // Item.LEARN_MORE
+        R.string.contextmenu_save_page, // Item.SAVE_PAGE
     };
 
     /**
