2b039851c5c40fae880937fa4d098f5c50d1f323
Marc Treib <treib@chromium.org>
2025-03-25 06:12:15 -0700

    [iOS] Cleanup: Remove AuthenticationService::GrantSyncConsent
    This method is never used in production code anymore. This CL removes
    any (test) references to it, and removes the implementation.
    
    Note: This should only land after the internal part,
    https://crrev.com/i/8128143, has been landed and rolled.
    
    Bug: 40067025
    Change-Id: I84d8f4bb2cbe2dc149241865dd3138713d7614f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382895
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437444}

--- ios/chrome/browser/recent_tabs/ui_bundled/recent_tabs_coordinator_unittest.mm
+++ ios/chrome/browser/recent_tabs/ui_bundled/recent_tabs_coordinator_unittest.mm
@@ -233,8 +233,6 @@ class RecentTabsTableCoordinatorTest : public BlockCleanupTest {
 
       authentication_service->SignIn(
           identity, signin_metrics::AccessPoint::kResigninInfobar);
-      authentication_service->GrantSyncConsent(
-          identity, signin_metrics::AccessPoint::kResigninInfobar);
     }
 
     ON_CALL(*session_sync_service, GetOpenTabsUIDelegate())
