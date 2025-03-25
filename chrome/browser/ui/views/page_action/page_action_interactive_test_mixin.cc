cff8ac50a3b5efceee58abf55da6bf08fcb310a9
Christopher Grant <cjgrant@chromium.org>
2025-03-25 15:51:48 -0700

    PageActions: Add interactive UI test mixin
    This doesn't do much at this point, but it installs the mixin mechanism
    so that adding more helpers across many page actions is easy.
    
    Change-Id: Ie8368c45e9fbc83aaa5a5bac71f800496ebba8a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6388697
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437796}

---
+++ chrome/browser/ui/views/page_action/page_action_interactive_test_mixin.cc
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/page_action/page_action_interactive_test_mixin.h"
+
+#include "ui/base/interaction/state_observer.h"
+
+DEFINE_STATE_IDENTIFIER_VALUE(ui::test::PollingStateObserver<bool>,
+                              kPageActionButtonVisible);
