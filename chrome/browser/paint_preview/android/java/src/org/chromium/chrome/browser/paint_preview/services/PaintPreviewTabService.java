fb9aa1df04012271f6d9bca2dd8f92c482e43110
Sky Malice <skym@chromium.org>
2025-03-25 14:27:41 -0700

    Remove PaintPreviewTabService.CaptureTriggerListener in CTA's clean up.
    Bug: 404705391
    Change-Id: I7e689591c66bcaef806dcb59343748cd5712b8d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6392615
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437744}

--- chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabService.java
+++ chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabService.java
@@ -16,6 +16,7 @@ import org.chromium.base.ApplicationState;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Callback;
 import org.chromium.base.ResettersForTesting;
+import org.chromium.base.lifetime.Destroyable;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.chrome.browser.tab.Tab;
@@ -58,7 +59,7 @@ public class PaintPreviewTabService implements NativePaintPreviewServiceProvider
     }
 
     private class CaptureTriggerListener extends TabModelSelectorTabObserver
-            implements ApplicationStatus.ApplicationStateListener {
+            implements ApplicationStatus.ApplicationStateListener, Destroyable {
         private @ApplicationState int mCurrentApplicationState;
 
         private CaptureTriggerListener(TabModelSelector tabModelSelector) {
@@ -69,9 +70,6 @@ public class PaintPreviewTabService implements NativePaintPreviewServiceProvider
         @Override
         public void onApplicationStateChange(int newState) {
             mCurrentApplicationState = newState;
-            if (newState == ApplicationState.HAS_DESTROYED_ACTIVITIES) {
-                ApplicationStatus.unregisterApplicationStateListener(this);
-            }
         }
 
         @Override
@@ -99,6 +97,12 @@ public class PaintPreviewTabService implements NativePaintPreviewServiceProvider
         @Override
         public void onTabUnregistered(Tab tab) {
             tabClosed(tab);
+        }
+
+        @Override
+        public void destroy() {
+            super.destroy();
+            ApplicationStatus.unregisterApplicationStateListener(this);
         }
 
         private boolean qualifiesForCapture(Tab tab) {
@@ -151,13 +155,15 @@ public class PaintPreviewTabService implements NativePaintPreviewServiceProvider
      * Should be called when all tabs are restored. Registers a {@link TabModelSelectorTabObserver}
      * for the regular to capture and delete paint previews as needed. Audits restored tabs to
      * remove any failed deletions.
+     *
      * @param tabModelSelector the TabModelSelector for the activity.
      * @param runAudit whether to delete tabs not in the tabModelSelector.
+     * @return A reference to an observer that should be cleaned up when the activity is destroyed.
      */
-    public void onRestoreCompleted(TabModelSelector tabModelSelector, boolean runAudit) {
-        new CaptureTriggerListener(tabModelSelector);
+    public Destroyable onRestoreCompleted(TabModelSelector tabModelSelector, boolean runAudit) {
+        Destroyable listener = new CaptureTriggerListener(tabModelSelector);
 
-        if (!runAudit || mAuditRunnable != null) return;
+        if (!runAudit || mAuditRunnable != null) return listener;
 
         // Delay actually performing the audit by a bit to avoid contention with the native task
         // runner that handles IO when showing at startup.
@@ -178,6 +184,7 @@ public class PaintPreviewTabService implements NativePaintPreviewServiceProvider
                     mAuditRunnable = null;
                 },
                 AUDIT_START_DELAY_MS);
+        return listener;
     }
 
     @VisibleForTesting
