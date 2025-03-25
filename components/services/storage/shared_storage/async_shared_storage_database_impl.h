a5408944e3ad225262c50e56197c881f9557bfaf
Yao Xiao <yaoxia@chromium.org>
2025-03-25 06:28:40 -0700

    [shared storage] Add SharedStorageManager::BatchUpdate() method
    Introduce SharedStorageManager::BatchUpdate() method that forwards
    the request to the underlying SharedStorageDatabase.
    
    Explainer PR: https://github.com/WICG/shared-storage/pull/228
    
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Bug: 404568020
    Change-Id: I39feb84af04f69d6b0d2c4a7432ad5e46b9d3ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384755
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437454}

--- components/services/storage/shared_storage/async_shared_storage_database_impl.h
+++ components/services/storage/shared_storage/async_shared_storage_database_impl.h
@@ -39,6 +39,7 @@ class AsyncSharedStorageDatabaseImpl : public AsyncSharedStorageDatabase {
   using InitStatus = SharedStorageDatabase::InitStatus;
   using SetBehavior = SharedStorageDatabase::SetBehavior;
   using OperationResult = SharedStorageDatabase::OperationResult;
+  using BatchUpdateResult = SharedStorageDatabase::BatchUpdateResult;
   using GetResult = SharedStorageDatabase::GetResult;
   using BudgetResult = SharedStorageDatabase::BudgetResult;
   using TimeResult = SharedStorageDatabase::TimeResult;
@@ -99,6 +100,11 @@ class AsyncSharedStorageDatabaseImpl : public AsyncSharedStorageDatabase {
   void Clear(url::Origin context_origin,
              base::OnceCallback<void(OperationResult)> callback,
              DataClearSource source) override;
+  void BatchUpdate(
+      url::Origin context_origin,
+      std::vector<network::mojom::SharedStorageModifierMethodWithOptionsPtr>
+          methods_with_options,
+      base::OnceCallback<void(BatchUpdateResult)> callback) override;
   void Length(url::Origin context_origin,
               base::OnceCallback<void(int)> callback) override;
   void Keys(url::Origin context_origin,
