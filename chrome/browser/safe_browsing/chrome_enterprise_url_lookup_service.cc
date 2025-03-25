12b48a9b5d5c953fc55321881ac3e36f82ad8a51
Ernesto Izquierdo Clua <eic@google.com>
2025-03-25 09:56:20 -0700

    Inject affiliation status into enterprise url lookup service
    Remove a dependency on Profile by injecting a callback returning if the
    profile and browser are managed by the same customer.
    
    This is part of the effort to move the enterprise service to components
    so it can be used for iOS Url filtering.
    
    Bug: 399376916
    Change-Id: Ic5f2109058559c5d0dacb170b1a24710a9d6a7fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6388098
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437559}

--- chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
+++ chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
@@ -10,13 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/enterprise/connectors/connectors_service.h"
-#include "chrome/browser/enterprise/util/affiliation.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
-#include "chrome/browser/policy/dm_token_utils.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
 #include "components/enterprise/common/proto/connectors.pb.h"
 #include "components/enterprise/connectors/core/common.h"
 #include "components/policy/core/common/cloud/dm_token.h"
@@ -82,7 +78,8 @@ ChromeEnterpriseRealTimeUrlLookupService::
         policy::ManagementService* management_service,
         bool is_off_the_record,
         bool is_guest_session,
-        base::RepeatingCallback<std::string()> get_profile_email_callback)
+        base::RepeatingCallback<std::string()> get_profile_email_callback,
+        base::RepeatingCallback<bool()> is_profile_affiliated_callback)
     : RealTimeUrlLookupServiceBase(
           url_loader_factory,
           cache_manager,
@@ -98,7 +95,8 @@ ChromeEnterpriseRealTimeUrlLookupService::
       management_service_(management_service),
       is_off_the_record_(is_off_the_record),
       is_guest_session_(is_guest_session),
-      get_profile_email_callback_(get_profile_email_callback) {}
+      get_profile_email_callback_(get_profile_email_callback),
+      is_profile_affiliated_callback_(is_profile_affiliated_callback) {}
 
 ChromeEnterpriseRealTimeUrlLookupService::
     ~ChromeEnterpriseRealTimeUrlLookupService() = default;
@@ -118,7 +116,7 @@ bool ChromeEnterpriseRealTimeUrlLookupService::
   // managed device.
   if (management_service_->HasManagementAuthority(
           policy::EnterpriseManagementAuthority::CLOUD_DOMAIN) &&
-      !enterprise_util::IsProfileAffiliated(profile_)) {
+      !is_profile_affiliated_callback_.Run()) {
     return false;
   }
 
@@ -285,7 +283,7 @@ std::string ChromeEnterpriseRealTimeUrlLookupService::GetProfileDMTokenString()
     const {
 #if !BUILDFLAG(IS_CHROMEOS)
   if (!connectors_service_->GetBrowserDmToken().has_value() ||
-      enterprise_util::IsProfileAffiliated(profile_)) {
+      is_profile_affiliated_callback_.Run()) {
     return connectors_service_->GetProfileDmToken().value_or("");
   }
 #endif
