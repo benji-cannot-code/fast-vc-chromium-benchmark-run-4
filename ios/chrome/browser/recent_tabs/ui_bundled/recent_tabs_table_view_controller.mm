de0f8b3decd448a4b0b2ac505b83a3878adb275e
Gauthier Ambard <gambard@chromium.org>
2025-03-25 10:12:21 -0700

    [iOS] Remove modal provider
    No longer useful.
    
    Bug: None
    Change-Id: I0a5bec24880a3634abbbc3ff16e8128a51b32ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382133
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437574}

--- ios/chrome/browser/recent_tabs/ui_bundled/recent_tabs_table_view_controller.mm
+++ ios/chrome/browser/recent_tabs/ui_bundled/recent_tabs_table_view_controller.mm
@@ -100,7 +100,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
-#import "ios/public/provider/chrome/browser/modals/modals_api.h"
 #import "ios/web/public/web_state.h"
 #import "ui/base/l10n/l10n_util.h"
 #import "ui/base/l10n/time_format.h"
--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_panel_view_controller.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_panel_view_controller.mm
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_panel_item_data.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_panel_mutator.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_panel_notification_cell.h"
-#import "ios/public/provider/chrome/browser/modals/modals_api.h"
 
 using tab_groups::SharingState;
 
