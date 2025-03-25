8a6bd23882e02abfa398cb551e1cadf322f5198e
Jenna Himawan <jhimawan@google.com>
2025-03-25 11:12:51 -0700

    Improve ObserverList's thread assertion message
    Change-Id: Icd5c9da58dbd9f55673a1a9552317ab48f14b32e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6373365
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437615}

--- base/android/junit/src/org/chromium/base/ThreadUtilsTest.java
+++ base/android/junit/src/org/chromium/base/ThreadUtilsTest.java
@@ -40,7 +40,9 @@ public class ThreadUtilsTest {
                                         TaskTraits.USER_BLOCKING, checker::assertOnValidThread));
         Assert.assertThat(
                 e.getCause().getMessage(),
-                startsWith("UI-only class called from background thread"));
+                startsWith(
+                        "Class was initialized on the UI thread, but current operation was"
+                            + " performed on a background thread:"));
     }
 
     @Test
@@ -57,6 +59,9 @@ public class ThreadUtilsTest {
         AssertionError e =
                 Assert.assertThrows(AssertionError.class, checkerHolder[0]::assertOnValidThread);
         Assert.assertThat(
-                e.getMessage(), startsWith("Background-only class called from UI thread"));
+                e.getMessage(),
+                startsWith(
+                        "Class was initialized on a background thread, but current operation was"
+                            + " performed on the UI thread (expected:"));
     }
 }
