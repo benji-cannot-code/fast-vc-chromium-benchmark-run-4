7c9973ec7e6c0a71c78201969c5acf03698711d8
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-03-25 07:35:50 -0700

    Drop leftover ManageSyncSettingsCoordinator::_syncSetupInProgressHandle
    No behavior change, this isn't set since crrev.com/c/6214770.
    
    Bug: 40066949
    Change-Id: Id02b6a0360cf4516e37ed4114196452b1b520d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387744
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437488}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_coordinator.mm
@@ -124,12 +124,6 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
   SettingsNavigationController* _navigationControllerInModalView;
   // The coordinator for the Personalize Google Services view.
   PersonalizeGoogleServicesCoordinator* _personalizeGoogleServicesCoordinator;
-  // Prevents any data from syncing while the UI is open.
-  // TODO(crbug.com/40066949): This is currently needed for syncing users,
-  // otherwise accidentally touching a toggle immediately uploads existing data.
-  // For non-syncing users that's not true. So remove this after the syncing
-  // state is gone on iOS.
-  std::unique_ptr<syncer::SyncSetupInProgressHandle> _syncSetupInProgressHandle;
 }
 
 @synthesize baseNavigationController = _baseNavigationController;
@@ -201,8 +195,6 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
   [self interruptAccountMenuCoordinator];
   self.mediator = nil;
   self.viewController = nil;
-  // Unblock any sync data type changes.
-  _syncSetupInProgressHandle.reset();
   [_syncEncryptionPassphraseTableViewController settingsWillBeDismissed];
   _syncEncryptionPassphraseTableViewController = nil;
   [_syncEncryptionTableViewController settingsWillBeDismissed];
