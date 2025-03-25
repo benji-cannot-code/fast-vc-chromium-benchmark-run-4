d0f0c17f3e5a4aca044cea73534af54fce21424d
Florin Malita <fmalita@chromium.org>
2025-03-25 08:09:21 -0700

    Path cleanup pass
    Some minor things I noticed while working on PathBuilder:
    
      - Return an optional point from CurrentPoint() and remove
        HasCurrentPoint().
      - Remove SkScalar conversions, as Skia no longer pretends to support
        anything other than floats.
      - Some general boilerplate reduction.
    
    Change-Id: Idaa06653fe0059c436c333f13d4cbb23d6ac00ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384538
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1437508}

--- third_party/blink/renderer/platform/geometry/path.h
+++ third_party/blink/renderer/platform/geometry/path.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GEOMETRY_PATH_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GEOMETRY_PATH_H_
 
+#include <optional>
+
 #include "base/memory/raw_span.h"
 #include "third_party/blink/renderer/platform/geometry/float_rounded_rect.h"
 #include "third_party/blink/renderer/platform/geometry/path_types.h"
@@ -151,8 +153,7 @@ class PLATFORM_EXPORT Path {
   // Gets the current point of the current path, which is conceptually the final
   // point reached by the path so far. Note the Path can be empty
   // (isEmpty() == true) and still have a current point.
-  bool HasCurrentPoint() const;
-  gfx::PointF CurrentPoint() const;
+  std::optional<gfx::PointF> CurrentPoint() const;
 
   // TODO(crbug.com/378688986): convert clients to PathBuilder and remove all
   // editing (non-const) methods.
