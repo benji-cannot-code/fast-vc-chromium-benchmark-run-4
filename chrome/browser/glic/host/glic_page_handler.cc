29bf9f4d09950d79ffa22d3d6c58191b7cc0aed3
Nick Birnie <birnie@google.com>
2025-03-24 18:58:11 -0700

    Open system settings menu API Mac only
    Mac has more restrictive system settings controlling app access to mic and location. When they are disabled we provide a UI that prompts the user to open the settings and manually flip the switch to enable the permission Chrome-wide. This menu does not exist on Linux and Windows and will crash Chrome if used by the embedee. Thus we disable the API.
    
    Bug: 405182433
    Change-Id: I46f1aea990b1fcde6c1ef6dd3277547f721bb82a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378869
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437259}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -318,6 +318,12 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
 
     state->browser_is_open = browser_is_open_calculator_.IsOpen();
 
+#if BUILDFLAG(IS_MAC)
+    state->open_os_settings_api_is_allowed = true;
+#else
+    state->open_os_settings_api_is_allowed = false;
+#endif
+
     local_state_pref_change_registrar_.Init(g_browser_process->local_state());
     local_state_pref_change_registrar_.Add(
         prefs::kGlicLauncherHotkey,
@@ -558,6 +564,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
   }
 
   void OpenOsPermissionSettingsMenu(ContentSettingsType type) override {
+#if BUILDFLAG(IS_MAC)
     if (type != ContentSettingsType::MEDIASTREAM_MIC &&
         type != ContentSettingsType::GEOLOCATION) {
       // This will terminate the render process.
@@ -568,6 +575,10 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
     }
     system_permission_settings::OpenSystemSettings(
         page_handler_->webui_contents(), type);
+#else
+    mojo::ReportBadMessage(
+        "OpenOsPermissionSettingsMenu not supported on this platform.");
+#endif
   }
 
   void GetOsMicrophonePermissionStatus(
--- chrome/browser/resources/glic/glic_api_impl/glic_api_client.ts
+++ chrome/browser/resources/glic/glic_api_impl/glic_api_client.ts
@@ -247,6 +247,10 @@ class GlicBrowserHostImpl implements GlicBrowserHost {
     if (!state.dragResizeEnabled) {
       (this as GlicBrowserHost).enableDragResize = undefined;
     }
+
+    if (!state.openOsSettingsApiIsAllowed) {
+      (this as GlicBrowserHost).openOsPermissionSettingsMenu = undefined;
+    }
   }
 
   webClientInitialized(
