ad074d2ed56da914ade31ae7869ec5757f43e381
Ryan Kalla <ryankalla@google.com>
2025-03-25 13:33:57 -0700

    Add the API key to Probabilistic Reveal Token issuer requests
    The PRT issuer service expects the API key to be set in the request headers.
    
    
    Bug: 405170843
    Change-Id: If45abb952bce5754a05d8364901ca0d36b5b0f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6377707
    Commit-Queue: Ryan Kalla <ryankalla@google.com>
    Reviewed-by: Aykut Bulut <aykutb@google.com>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437703}

--- components/ip_protection/common/ip_protection_probabilistic_reveal_token_direct_fetcher.h
+++ components/ip_protection/common/ip_protection_probabilistic_reveal_token_direct_fetcher.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
+#include "base/version_info/channel.h"
 #include "components/ip_protection/common/ip_protection_probabilistic_reveal_token_fetcher.h"
 #include "components/ip_protection/get_probabilistic_reveal_token.pb.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -23,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/simple_url_loader.h"
 
 namespace ip_protection {
-
 
 // Implements IpProtectionProbabilisticRevealTokenFetcher abstract base class.
 // Main functionality is implemented in TryGetProbabilisticRevealTokens method.
@@ -33,8 +33,9 @@ class IpProtectionProbabilisticRevealTokenDirectFetcher
   // Retriever class fetches probabilistic reveal tokens from the issuer server.
   class Retriever {
    public:
-    explicit Retriever(std::unique_ptr<network::PendingSharedURLLoaderFactory>
-                           pending_url_loader_factory);
+    Retriever(std::unique_ptr<network::PendingSharedURLLoaderFactory>
+                  pending_url_loader_factory,
+              version_info::Channel channel);
     ~Retriever();
     // Returns HTTP body string from issuer or url_loader->NetError() code on
     // error.
@@ -55,9 +56,10 @@ class IpProtectionProbabilisticRevealTokenDirectFetcher
     base::WeakPtrFactory<Retriever> weak_ptr_factory_{this};
   };
 
-  explicit IpProtectionProbabilisticRevealTokenDirectFetcher(
+  IpProtectionProbabilisticRevealTokenDirectFetcher(
       std::unique_ptr<network::PendingSharedURLLoaderFactory>
-          url_loader_factory);
+          url_loader_factory,
+      version_info::Channel channel);
 
   ~IpProtectionProbabilisticRevealTokenDirectFetcher() override;
 
--- components/ip_protection/common/ip_protection_probabilistic_reveal_token_direct_fetcher_unittest.cc
+++ components/ip_protection/common/ip_protection_probabilistic_reveal_token_direct_fetcher_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
+#include "base/version_info/channel.h"
 #include "components/ip_protection/common/ip_protection_probabilistic_reveal_token_fetcher.h"
 #include "components/ip_protection/get_probabilistic_reveal_token.pb.h"
 #include "net/base/features.h"
@@ -117,6 +118,7 @@ class FetcherTestBase : public testing::Test {
           EXPECT_THAT(
               request.headers.GetHeader("Ip-Protection-Debug-Experiment-Arm"),
               expected_experiment_arm_);
+          EXPECT_TRUE(request.headers.HasHeader("X-Goog-Api-Key"));
           ASSERT_TRUE(request.request_body);
           GetProbabilisticRevealTokenRequest request_proto;
           ASSERT_TRUE(request_proto.ParseFromString(GetUploadData(request)));
@@ -146,7 +148,8 @@ class IpProtectionProbabilisticRevealTokenDirectFetcherRetrieverTest
     FetcherTestBase::SetFeatureParameters(parameters);
     retriever_ = std::make_unique<
         IpProtectionProbabilisticRevealTokenDirectFetcher::Retriever>(
-        test_url_loader_factory_.GetSafeWeakWrapper()->Clone());
+        test_url_loader_factory_.GetSafeWeakWrapper()->Clone(),
+        version_info::Channel::DEFAULT);
   }
 
   std::unique_ptr<IpProtectionProbabilisticRevealTokenDirectFetcher::Retriever>
@@ -247,7 +250,8 @@ class IpProtectionProbabilisticRevealTokenDirectFetcherTest
     FetcherTestBase::SetFeatureParameters(parameters);
     fetcher_ =
         std::make_unique<IpProtectionProbabilisticRevealTokenDirectFetcher>(
-            test_url_loader_factory_.GetSafeWeakWrapper()->Clone());
+            test_url_loader_factory_.GetSafeWeakWrapper()->Clone(),
+            version_info::Channel::DEFAULT);
 
     absl::StatusOr<std::string> maybe_public_key_bytes =
         GetTestPublicKeyBytes(/*private_key=*/1);
