aff934c0c870a36c9293c09425893ba35db4f3d6
Lei Zhang <thestig@chromium.org>
2025-03-26 13:51:23 -0700

    glic: Close Glic UI if host renderer crashes
    The glic UI will be in a bad state if its host renderer crashes. Augment
    the code that detects the crash to close the UI via GlicKeyedService.
    This resets the UI to remove visual artifacts, and allows the UI to get
    recreated the next time the user triggers it.
    
    Bug: 399741243
    Change-Id: I0a363e71be5215d4d233a85183b0be0718e886f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309339
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438383}

--- chrome/browser/glic/host/webui_contents_container.cc
+++ chrome/browser/glic/host/webui_contents_container.cc
@@ -40,6 +40,7 @@ class WebUIContentsContainer::WCObserver : public content::WebContentsObserver {
   void PrimaryMainFrameRenderProcessGone(
       base::TerminationStatus status) override {
     container_->RendererCrashed(this);
+    // WARNING: Do not do any more work, as `this` may have been destroyed.
   }
 
   // The container that owns this.
@@ -95,6 +96,17 @@ void WebUIContentsContainer::InnerWebContentsAttached(
 }
 
 void WebUIContentsContainer::RendererCrashed(WCObserver* observer) {
+  RecordRendererCrashedMetrics(observer);
+  if (outer_wc_observer_.get() == observer) {
+    auto* keyed_service = GlicKeyedServiceFactory::GetGlicKeyedService(
+        web_contents_->GetBrowserContext());
+    keyed_service->CloseUI();
+  }
+  // WARNING: Do not do any more work, as `this` may have been destroyed.
+}
+
+void WebUIContentsContainer::RecordRendererCrashedMetrics(
+    WCObserver* observer) {
   if (inner_wc_observer_.get() == observer) {
     base::RecordAction(base::UserMetricsAction("GlicSessionWebClientCrash"));
   }
