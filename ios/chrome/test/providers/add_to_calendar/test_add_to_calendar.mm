a394f7402528f39385aea12a639068eea4f9bc7c
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-03-26 12:13:21 -0700

    [iOS] Enhanced calendar "add to calendar" public provider API
    Internal impl: http://crrev.com/i/8134768
    
    Bug: 405195689
    Change-Id: I579c76e31a1d4b9e8ad102acd0d4752f24663667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382620
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438314}

---
+++ ios/chrome/test/providers/add_to_calendar/test_add_to_calendar.mm
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/add_to_calendar/add_to_calendar_api.h"
+
+namespace ios::provider {
+
+void PresentAddToCalendar(
+    UIViewController* presenting_view_controller,
+    AddToCalendarIntegrationProvider provider,
+    base::WeakPtr<web::WebState> web_state,
+    std::unique_ptr<optimization_guide::proto::EnhancedCalendarResponse>
+        enhanced_calendar_response) {}
+
+}  // namespace ios::provider
