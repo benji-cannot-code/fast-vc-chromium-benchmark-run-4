b70dde14234d65f05985cb125bf977adb74cab6f
Bailey Myers-Morgan <bmyersmorgan@google.com>
2025-03-26 16:09:16 -0700

    Dummy test / build rule for OpenScreen perf tests.
    Bug: 403338499
    
    Following structure from https://crrev.com/c/5727141.
    
    Change-Id: I5c7290f3f4c43f553d368cb3c559c1d52fb3c4ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6354725
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Commit-Queue: Bailey Myers-Morgan <bmyersmorgan@google.com>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438465}

---
+++ chrome/test/media_router/performance/performance_test.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+""" Execute Media Router performance tests against miscellaneous test devices. """
+
+# TODO(crbug.com/403338499): Implement tests.
+def main() -> int:
+  print('Running Media Router performance tests.')
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main())
