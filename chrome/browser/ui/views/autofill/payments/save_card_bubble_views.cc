9f89757476626b81c1a76946e174524bd507a9aa
Qihui Zhao <qihuizhao@google.com>
2025-03-26 10:02:52 -0700

    Update save card bubble view to render new FOP display
    This CL updates save card bubble view to show new FOP display
    
    Before: https://screenshot.googleplex.com/6mQKtbXbzmtajUs
    After: https://screenshot.googleplex.com/6RmB4E2FFrfn8zU
    Mock: https://screenshot.googleplex.com/7Nm9EkE6DcXbX5Q
    
    Change-Id: I5305ca71c337f88621f3d2c0000bb47f1fc48a11
    Bug: b:405973373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6386777
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438213}

--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views.cc
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views.cc
@@ -42,6 +42,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+constexpr char16_t kEllipsisDotSeparator[] = u"\u2022";
+
+int GetObfuscationLength() {
+  return base::FeatureList::IsEnabled(
+             features::kAutofillEnableNewFopDisplayDesktop)
+             ? 2
+             : 4;
+}
+
 SaveCardBubbleViews::SaveCardBubbleViews(views::View* anchor_view,
                                          content::WebContents* web_contents,
                                          SaveCardBubbleController* controller)
@@ -121,7 +130,8 @@ views::View* SaveCardBubbleViews::GetFootnoteViewForTesting() {
 }
 
 const std::u16string SaveCardBubbleViews::GetCardIdentifierString() const {
-  return controller_->GetCard().CardNameAndLastFourDigits();
+  return controller_->GetCard().CardNameAndLastFourDigits(
+      /*customized_nickname=*/u"", GetObfuscationLength());
 }
 
 SaveCardBubbleViews::~SaveCardBubbleViews() = default;
@@ -188,7 +198,8 @@ std::unique_ptr<views::View> SaveCardBubbleViews::GetCardIdentifierView() {
   auto card_identifier_view = std::make_unique<views::View>();
   auto* layout = card_identifier_view->SetLayoutManager(
       std::make_unique<views::FlexLayout>());
-  if (is_cvc_only_save) {
+  if (is_cvc_only_save || base::FeatureList::IsEnabled(
+                              features::kAutofillEnableNewFopDisplayDesktop)) {
     layout->SetCollapseMargins(true);
     layout->SetDefault(
         views::kMarginsKey,
@@ -240,6 +251,12 @@ std::unique_ptr<views::View> SaveCardBubbleViews::GetCardIdentifierView() {
                                  views::MaximumFlexSizeRule::kUnbounded)
             .WithOrder(2));
   } else if (!card.IsExpired(base::Time::Now())) {
+    if (base::FeatureList::IsEnabled(
+            features::kAutofillEnableNewFopDisplayDesktop)) {
+      card_identifier_view->AddChildView(std::make_unique<views::Label>(
+          kEllipsisDotSeparator, views::style::CONTEXT_DIALOG_BODY_TEXT,
+          views::style::STYLE_SECONDARY));
+    }
     // Add card expiration date for card saves.
     auto* expiration_date_label =
         card_identifier_view->AddChildView(std::make_unique<views::Label>(
