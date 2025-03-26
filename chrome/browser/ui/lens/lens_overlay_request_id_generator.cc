b2bea7de41ab9f5f806ceeec287e09ad8ea20c1a
Duncan Mercer <mercerd@google.com>
2025-03-26 13:01:22 -0700

    [lensoverlay] Add long context id
    Adds a new id to the request id to be used to notify the server that the page content has changed. Currently unused unless the request id fix param is enabled.
    
    Fixed: 405978424
    Change-Id: Id1893a3eb74a2a487d3ff767a0136e0642fe4e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394337
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438350}

--- chrome/browser/ui/lens/lens_overlay_request_id_generator.cc
+++ chrome/browser/ui/lens/lens_overlay_request_id_generator.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/lens/lens_overlay_request_id_generator.h"
 
+#include "base/check.h"
 #include "base/containers/span.h"
 #include "base/rand_util.h"
 #include "components/base32/base32.h"
@@ -27,6 +28,7 @@ void LensOverlayRequestIdGenerator::ResetRequestId() {
   uuid_ = base::RandUint64();
   sequence_id_ = 0;
   image_sequence_id_ = 0;
+  long_context_id_ = 0;
   analytics_id_ = base::RandBytesAsString(kAnalyticsIdBytesSize);
   routing_info_.reset();
 }
@@ -34,9 +36,18 @@ void LensOverlayRequestIdGenerator::ResetRequestId() {
 std::unique_ptr<lens::LensOverlayRequestId>
 LensOverlayRequestIdGenerator::GetNextRequestId(
     RequestIdUpdateMode update_mode) {
+  // Verify that the initial request id is only generated once.
+  CHECK(update_mode != RequestIdUpdateMode::kInitialRequest ||
+        sequence_id_ == 0);
+
   bool increment_image_sequence =
-      update_mode == RequestIdUpdateMode::kFullImageRequest;
+      update_mode == RequestIdUpdateMode::kFullImageRequest ||
+      update_mode == RequestIdUpdateMode::kInitialRequest;
   bool increment_sequence = update_mode != RequestIdUpdateMode::kOpenInNewTab;
+  bool increment_long_context =
+      lens::features::PageContentUploadRequestIdFixEnabled() &&
+      (update_mode == RequestIdUpdateMode::kPageContentRequest ||
+       update_mode == RequestIdUpdateMode::kInitialRequest);
   bool create_analytics_id =
       update_mode != RequestIdUpdateMode::kSearchUrl &&
       update_mode != RequestIdUpdateMode::kPartialPageContentRequest;
@@ -56,6 +67,9 @@ LensOverlayRequestIdGenerator::GetNextRequestId(
   if (increment_sequence) {
     sequence_id_++;
   }
+  if (increment_long_context) {
+    long_context_id_++;
+  }
   std::string analytics_id_to_set = analytics_id_;
   if (create_analytics_id) {
     analytics_id_to_set = base::RandBytesAsString(kAnalyticsIdBytesSize);
@@ -68,6 +82,7 @@ LensOverlayRequestIdGenerator::GetNextRequestId(
   request_id->set_uuid(uuid_);
   request_id->set_sequence_id(sequence_id_);
   request_id->set_analytics_id(analytics_id_to_set);
+  request_id->set_long_context_id(long_context_id_);
   request_id->set_image_sequence_id(image_sequence_id_);
   if (routing_info_.has_value()) {
     request_id->mutable_routing_info()->CopyFrom(routing_info_.value());
