04c506841588df93e2af7b9c920dc832f51f69fb
Wenyu Fu <wenyufu@chromium.org>
2025-03-25 17:20:27 -0700

    Properly release PartnerCustomizationsUma
    This CL releases the PartnerCustomizationsUma when #destroy is called
    for the static instance; this does not fully mitigate the memory leak
    mentioned in the bug.
    
    Bug: 406321517
    Change-Id: I378e88ef0e0b3b9b4c10d27598bea102ba2cafcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6395172
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437849}

--- chrome/browser/partnercustomizations/java/src/org/chromium/chrome/browser/partnercustomizations/PartnerBrowserCustomizations.java
+++ chrome/browser/partnercustomizations/java/src/org/chromium/chrome/browser/partnercustomizations/PartnerBrowserCustomizations.java
@@ -405,6 +405,7 @@ public class PartnerBrowserCustomizations {
 
     private void destroyInternal() {
         mInitializeAsyncCallbacks.clear();
+        mPartnerCustomizationsUma = null;
         mListener = null;
     }
 
