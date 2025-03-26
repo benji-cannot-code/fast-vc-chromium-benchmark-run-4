a3e27ca8d1dbe482686be50bdba8ca124f6f796b
Sky Malice <skym@chromium.org>
2025-03-26 05:18:58 -0700

    Remove delayed clean up from tab switcher on destroy.
    Otherwise the posted tasks in the Handler end up leaking things
    temporarily.
    
    Bug: 406326705
    Change-Id: Iadfb24604b3691874fe36e0cd190a00048427e99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394812
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438045}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneBase.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneBase.java
@@ -186,6 +186,7 @@ public abstract class TabSwitcherPaneBase implements Pane, TabSwitcher, TabSwitc
 
     @Override
     public void destroy() {
+        removeDelayedCallbacks();
         mIsVisibleSupplier.removeObserver(mVisibilityObserver);
         destroyTabSwitcherPaneCoordinator();
     }
