1983b8b676185089158277e2331408b0cf885abd
Wen-Chien Wang <wcwang@chromium.org>
2025-03-26 10:53:22 -0700

    Revert "[MV2 Deprecation] Add another fieldtrial_testing_config.json entry"
    This reverts commit a7e280a1ba1c657b7f121e21b0718c04004e5c33.
    
    Reason for revert: b/406523920
    
    Original change's description:
    > [MV2 Deprecation] Add another fieldtrial_testing_config.json entry
    >
    > Add a fieldtrial_testing_config.json entry for the
    > ExtensionManifestV2Unsupported experiment phase.
    >
    > Bug: 367395349
    > Change-Id: I432d2e90cb1984dd290ebca6f8ec0e959426c65c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6358257
    > Reviewed-by: Georg Neis <neis@chromium.org>
    > Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1438037}
    
    Bug: 367395349
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I59679d608a746ac765a006ac5347e67e9547d8bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397497
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Wen-Chien Wang <wcwang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Wen-Chien Wang <wcwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438252}

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -109,7 +109,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/extensions/chrome_test_extension_loader.h"
-#include "chrome/browser/extensions/scoped_test_mv2_enabler.h"
 #include "extensions/browser/browsertest_util.h"
 #include "extensions/test/test_extension_dir.h"
 #endif
@@ -802,9 +801,6 @@ std::unique_ptr<net::test_server::HttpResponse> EchoCookieHeader(
 
 IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationBrowserTest,
                        ThirdPartyCookiesAllowedForExtensions) {
-  // TODO(https://crbug.com/40804030): Remove this when updated to use MV3.
-  extensions::ScopedTestMV2Enabler mv2_enabler;
-
   if (IsRestartStateWithInProcessNetworkService())
     return;
 
--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -728,7 +728,6 @@ class SitePerProcessPrintBrowserTest : public PrintBrowserTest {
 
   // content::BrowserTestBase
   void SetUpCommandLine(base::CommandLine* command_line) override {
-    PrintBrowserTest::SetUpCommandLine(command_line);
     content::IsolateAllSitesForTesting(command_line);
   }
 };
@@ -863,7 +862,6 @@ class SitePerProcessPrintExtensionBrowserTest
  public:
   // content::BrowserTestBase
   void SetUpCommandLine(base::CommandLine* command_line) override {
-    PrintExtensionBrowserTest::SetUpCommandLine(command_line);
     content::IsolateAllSitesForTesting(command_line);
   }
 };
