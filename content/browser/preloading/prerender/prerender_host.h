10353d3135d6493ddd45764353e27bfca33277ef
Hiroki Nakagawa <nhiroki@chromium.org>
2025-03-26 01:25:47 -0700

    Prerender: Pass SpeculationRulesTags up to PrerenderHost
    This is a preparation for supporting Speculation Rules tags for
    prerendering. This CL passes SpeculationRulesTags from PrerenererImpl to
    PrerenderHost, so that follow-up CLs can add the Sec-Speculation-Tags
    HTTP header based on that.
    
    PrerendererImpl passes other Speculation Rules specific parameters like
    the target hint and the eagerness. For code simplification, this CL
    bundle them into SpeculationRulesParams struct and also adds
    SpeculationRulesTags into the struct.
    
    This CL changes SpeculationRulesTags from movable-only to
    copyable-and-movable, as its owners (PrerenderAttributes and
    SpeculationRulesParams) can be copied.
    
    Bug: 381687257
    Change-Id: Id330fbc2ca71ecc314f33375e69ade84a8ff1ebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383243
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437985}

--- content/browser/preloading/prerender/prerender_host.h
+++ content/browser/preloading/prerender/prerender_host.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/pass_key.h"
 #include "content/browser/preloading/prerender/prerender_attributes.h"
 #include "content/browser/preloading/prerender/prerender_final_status.h"
+#include "content/browser/preloading/speculation_rules/speculation_rules_tags.h"
 #include "content/browser/renderer_host/frame_tree.h"
 #include "content/browser/renderer_host/navigation_controller_delegate.h"
 #include "content/common/content_export.h"
@@ -387,7 +388,7 @@ class CONTENT_EXPORT PrerenderHost : public FrameTree::Delegate,
   }
 
   std::optional<blink::mojom::SpeculationEagerness> eagerness() const {
-    return attributes_.eagerness;
+    return attributes_.GetEagerness();
   }
 
   base::WeakPtr<PreloadingAttempt> preloading_attempt() { return attempt_; }
---
+++ content/browser/preloading/speculation_rules/speculation_rules_params.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_PRELOADING_SPECULATION_RULES_SPECULATION_RULES_PARAMS_H_
+#define CONTENT_BROWSER_PRELOADING_SPECULATION_RULES_SPECULATION_RULES_PARAMS_H_
+
+#include "content/browser/preloading/speculation_rules/speculation_rules_tags.h"
+#include "third_party/blink/public/mojom/speculation_rules/speculation_rules.mojom-shared.h"
+
+namespace content {
+
+// This struct holds parameters specific to the Speculation Rules trigger.
+struct CONTENT_EXPORT SpeculationRulesParams {
+  SpeculationRulesParams();
+  SpeculationRulesParams(blink::mojom::SpeculationTargetHint target_hint,
+                         blink::mojom::SpeculationEagerness eagerness,
+                         SpeculationRulesTags tags);
+  ~SpeculationRulesParams() = default;
+
+  // Copyable and movable.
+  SpeculationRulesParams(const SpeculationRulesParams&);
+  SpeculationRulesParams& operator=(const SpeculationRulesParams&);
+  SpeculationRulesParams(SpeculationRulesParams&&) noexcept;
+  SpeculationRulesParams& operator=(SpeculationRulesParams&&) noexcept;
+
+  // https://wicg.github.io/nav-speculation/speculation-rules.html#speculation-rule-target-navigable-name-hint
+  blink::mojom::SpeculationTargetHint target_hint;
+
+  // https://wicg.github.io/nav-speculation/speculation-rules.html#speculation-rule-eagerness
+  blink::mojom::SpeculationEagerness eagerness;
+
+  // https://wicg.github.io/nav-speculation/speculation-rules.html#speculation-rule-tags
+  SpeculationRulesTags tags;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_PRELOADING_SPECULATION_RULES_SPECULATION_RULES_PARAMS_H_
