d7af4f30b64858e50e3cccff6ae208e9733c197a
Yair Schiff <yair.schiff@island.io>
2025-03-26 15:04:50 -0700

    fix memory leak in QueryParentsForProperty [macOS]
    A call to CFStringCreateWithCString was not being released.
    
    This was found with the built-in 'leaks' tool with PartitionAlloc
    disabled (use_partition_alloc_as_malloc = false gn arg).
    Launching Chromium using env MallocStackLogging=1 and the
    --metrics-recording-only switch will trigger this code path
    (after 30 seconds) and reveal the leak in the leaks utility.
    
    Bug: 405430588
    Change-Id: I0eb4150b705715f82edc4c9998950fdcf9efa004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6381846
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438428}

--- base/files/drive_info_apple.mm
+++ base/files/drive_info_apple.mm
@@ -35,12 +35,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 template <typename T>
 T QueryParentsForProperty(io_object_t io_object, const char* key) {
-  return static_cast<T>(IORegistryEntrySearchCFProperty(
-      io_object, kIOServicePlane,
-      CFStringCreateWithCString(kCFAllocatorDefault, key,
-                                kCFStringEncodingUTF8),
-      kCFAllocatorDefault,
+  CFStringRef search_key = CFStringCreateWithCString(kCFAllocatorDefault, key,
+                                                     kCFStringEncodingUTF8);
+  if (search_key == nullptr) {
+    return nullptr;
+  }
+  T result = static_cast<T>(IORegistryEntrySearchCFProperty(
+      io_object, kIOServicePlane, search_key, kCFAllocatorDefault,
       kIORegistryIterateRecursively | kIORegistryIterateParents));
+  CFRelease(search_key);
+  return result;
 }
 }  // namespace
 #endif
