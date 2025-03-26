5440ee574025f6cf579aacc9ecb15c079fadaeda
Paul Adedeji <pauladedeji@google.com>
2025-03-26 07:55:40 -0700

    [glic metrics] Attempt to deflake shortcut input tests.
    Resolve flaky shortcut input tests by ensuring sequential metric
    logging. The tests currently fail due to missing a boolean metric (1 is
    logged instead of 2) [1]. This change implements a wait for the initial
    shortcut setting metric before subsequent updates, including calls to
    setGlicShortcutResponse, to guarantee correct logging of metrics
    dependent on its return value.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/main:chrome/test/data/webui/settings/glic_page_focus_test.ts?q=file:glic_page_focus&ss=chromium%2Fchromium%2Fsrc#:~:text=184-,185,-186
    
    Change-Id: Ia5dd6291b9d9c299dc6bb51e8d5154cb8c69b212
    Bug: 402262311, 400246327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394338
    Reviewed-by: David Bokan <bokan@chromium.org>
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438119}

--- chrome/test/data/webui/settings/glic_page_focus_test.ts
+++ chrome/test/data/webui/settings/glic_page_focus_test.ts
@@ -139,9 +139,10 @@ suite('GlicPageFocusTest', function() {
       metricsBrowserProxy.reset();
 
       // Act.
-      glicBrowserProxy.setGlicShortcutResponse('');
       shortcutInput.$.edit.click();
+      await metricsBrowserProxy.whenCalled('recordBooleanHistogram');
       await flushTasks();
+      glicBrowserProxy.setGlicShortcutResponse('');
       keyDownOn(field, 27);  // Escape key.
       await flushTasks();
       assertEquals('', field.value);
@@ -174,9 +175,10 @@ suite('GlicPageFocusTest', function() {
       metricsBrowserProxy.reset();
 
       // Act.
-      glicBrowserProxy.setGlicShortcutResponse('⌃A');
       shortcutInput.$.edit.click();
+      await metricsBrowserProxy.whenCalled('recordBooleanHistogram');
       await flushTasks();
+      glicBrowserProxy.setGlicShortcutResponse('Ctrl + A');
       keyDownOn(field, 65, ['ctrl']);
       await flushTasks();
 
