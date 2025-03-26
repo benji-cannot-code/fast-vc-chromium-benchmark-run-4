67fc32258380df6a00655e2a2fc5f3d33e973ba0
Devon Loehr <dloehr@google.com>
2025-03-26 15:28:01 -0700

    Export things to prevent accidental duplication
    Clang has added a new warning for objects which might get accidentally
    duplicated when built into a shared library with hidden visibility.
    This CL addresses the warning by moving the definition out-of-line,
    since it only applies to things defined in header files.
    
    This CL applies to the following directories: extensions
    
    This CL was uploaded by git cl split.
    
    Bug: chromium:368047728
    Change-Id: I4dec68b59d6b360c98aa411230b6b8418e55cedd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394767
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Auto-Submit: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438443}

--- extensions/browser/event_router.cc
+++ extensions/browser/event_router.cc
@@ -65,6 +65,8 @@ using content::RenderProcessHost;
 
 namespace extensions {
 
+base::TimeDelta kEventAckMetricTimeLimit = base::Minutes(5);
+
 namespace {
 
 // A dictionary of event names to lists of filters that this extension has
