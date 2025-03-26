fb7ef70b91fd485d03dceb1e21f0bd5acad7b7be
Aldo Culquicondor <acondor@chromium.org>
2025-03-26 13:36:16 -0700

    Resize screenshot cache when display changes
    Fixed: 406039355
    Change-Id: Ia0814694210c82b424e296f3e49e55ce77b7ca23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397395
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438368}

--- content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_manager.h
+++ content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_manager.h
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "content/common/content_export.h"
+#include "ui/display/display_observer.h"
+
+namespace display {
+class Display;
+}
 
 namespace content {
 
@@ -25,14 +30,20 @@ class NavigationEntryScreenshotCacheEvictor;
 // `BrowserContext`. All primary `FrameTree`s sharing the same `BrowserContext`
 // share the same manager. The manager should only be accessed by the
 // `NavigationEntryScreenshotCache` and tests.
-class CONTENT_EXPORT NavigationEntryScreenshotManager {
+class CONTENT_EXPORT NavigationEntryScreenshotManager
+    : public display::DisplayObserver {
  public:
   NavigationEntryScreenshotManager();
   NavigationEntryScreenshotManager(const NavigationEntryScreenshotManager&) =
       delete;
   NavigationEntryScreenshotManager& operator=(
       const NavigationEntryScreenshotManager&) = delete;
-  ~NavigationEntryScreenshotManager();
+  ~NavigationEntryScreenshotManager() override;
+
+  void OnDisplayAdded(const display::Display&) override;
+  void OnDisplaysRemoved(const display::Displays&) override;
+  void OnDisplayMetricsChanged(const display::Display&,
+                               uint32_t metrics_changed) override;
 
   // Called when a screenshot is stashed into a `NavigationEntry`, or when a
   // screenshot is removed from the entry (for preview, or during the
@@ -71,6 +82,8 @@ class CONTENT_EXPORT NavigationEntryScreenshotManager {
   }
 
  private:
+  void RecalculateCacheSize();
+
   // Called when the first screenshot is cached into `cache`, and when the last
   // screenshot is removed from `cache`.
   void Register(NavigationEntryScreenshotCacheEvictor* cache);
