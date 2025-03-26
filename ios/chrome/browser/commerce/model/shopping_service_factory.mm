979900e613d4024c31e094adfe73d983473ef18e
Stephen McGruer <smcgruer@google.com>
2025-03-26 06:52:02 -0700

    [Parcel Tracking] No longer record opt-in status
    Parcel tracking was un-launched in https://crrev.com/b6ecf1cb, and we
    are now removing the related code from the codebase. This CL removes
    a call in shopping_service_factory.mm that recorded the opt-in
    status. The underlying RecordParcelTrackingOptInStatus method will
    be cleaned up when //ios/chrome/browser/parcel_tracking/ is removed
    in a future CL.
    
    Bug: 391002352
    Change-Id: If8a5c96799f5923ea38f914babbc96d15aaf1958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6386749
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438078}

--- ios/chrome/browser/commerce/model/shopping_service_factory.mm
+++ ios/chrome/browser/commerce/model/shopping_service_factory.mm
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/history/model/history_service_factory.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory.h"
-#import "ios/chrome/browser/parcel_tracking/features.h"
-#import "ios/chrome/browser/parcel_tracking/parcel_tracking_opt_in_status.h"
 #import "ios/chrome/browser/power_bookmarks/model/power_bookmark_service_factory.h"
 #import "ios/chrome/browser/sessions/model/ios_chrome_tab_restore_service_factory.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
@@ -70,10 +68,6 @@ std::unique_ptr<KeyedService> ShoppingServiceFactory::BuildServiceInstanceFor(
     web::BrowserState* state) const {
   ProfileIOS* profile = ProfileIOS::FromBrowserState(state);
   PrefService* pref_service = profile->GetPrefs();
-
-  if (IsIOSParcelTrackingEnabled()) {
-    RecordParcelTrackingOptInStatus(pref_service);
-  }
 
   return std::make_unique<ShoppingService>(
       GetCurrentCountryCode(GetApplicationContext()->GetVariationsService()),
