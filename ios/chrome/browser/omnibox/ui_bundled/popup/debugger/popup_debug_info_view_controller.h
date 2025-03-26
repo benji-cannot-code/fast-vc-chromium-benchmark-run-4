fa43cb643c48a8cff4be4f7fe6d695b75ebb8b0b
Ameur Hosni <ameurhosni@google.com>
2025-03-26 05:11:32 -0700

    [IOS][OR] Move omnibox debugger files to the right folder
    This CL moves the omnibox debugger related files to the debugger files.
    
    Bug: 402391944
    Change-Id: I7a94d17b8cbedaf6497e4c931f104e02da379bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396696
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438042}

--- ios/chrome/browser/omnibox/ui_bundled/popup/debugger/popup_debug_info_view_controller.h
+++ ios/chrome/browser/omnibox/ui_bundled/popup/debugger/popup_debug_info_view_controller.h
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_POPUP_POPUP_DEBUG_INFO_VIEW_CONTROLLER_H_
-#define IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_POPUP_POPUP_DEBUG_INFO_VIEW_CONTROLLER_H_
+#ifndef IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_POPUP_DEBUGGER_POPUP_DEBUG_INFO_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_POPUP_DEBUGGER_POPUP_DEBUG_INFO_VIEW_CONTROLLER_H_
 
 #import <UIKit/UIKit.h>
 
-#import "ios/chrome/browser/omnibox/ui_bundled/popup/autocomplete_controller_observer_bridge.h"
-#import "ios/chrome/browser/omnibox/ui_bundled/popup/popup_debug_info_consumer.h"
-#import "ios/chrome/browser/omnibox/ui_bundled/popup/remote_suggestions_service_observer_bridge.h"
+#import "ios/chrome/browser/omnibox/ui_bundled/popup/debugger/autocomplete_controller_observer_bridge.h"
+#import "ios/chrome/browser/omnibox/ui_bundled/popup/debugger/popup_debug_info_consumer.h"
+#import "ios/chrome/browser/omnibox/ui_bundled/popup/debugger/remote_suggestions_service_observer_bridge.h"
 
 /// View controller used to display omnibox and popup related debug info.
 @interface PopupDebugInfoViewController
@@ -26,4 +26,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_POPUP_POPUP_DEBUG_INFO_VIEW_CONTROLLER_H_
+#endif  // IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_POPUP_DEBUGGER_POPUP_DEBUG_INFO_VIEW_CONTROLLER_H_
--- ios/chrome/browser/omnibox/ui_bundled/popup/debugger/popup_debug_info_view_controller.mm
+++ ios/chrome/browser/omnibox/ui_bundled/popup/debugger/popup_debug_info_view_controller.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/omnibox/ui_bundled/popup/popup_debug_info_view_controller.h"
+#import "ios/chrome/browser/omnibox/ui_bundled/popup/debugger/popup_debug_info_view_controller.h"
 
 #import "base/apple/foundation_util.h"
 #import "base/strings/sys_string_conversions.h"
