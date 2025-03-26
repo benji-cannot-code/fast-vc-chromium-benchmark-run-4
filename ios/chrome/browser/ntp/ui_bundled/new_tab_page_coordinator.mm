5142245cdb65ef5f4f29e7e43cfaa9948f41c3b5
ginnnnnnny <ginnyhuang@chromium.org>
2025-03-26 08:19:27 -0700

    [iOS] Ask voiceover to refocus to top left on feed visibility change
    This workaround mitigates the issue that the content offset jumps when
    user turns on and off the feed.
    (https://drive.google.com/file/d/10oimWkhwJ7Plazr7Ih0wTUCDftsv0lJq/view?usp=drive_link)
    The root cause lies in the collection view in UIKit.
    
    Screen recording after fix:
    https://drive.google.com/file/d/1_N4oRIdQb_qTGEiaJnc40PLPPOkh-w9B/view?usp=sharing&resourcekey=0-s4Vg2awBIuXnyWn0r3OPtQ
    
    Bug: 399123658
    Change-Id: I5ef6a3381e6916dacdb253c35494890531aecd7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6393959
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438136}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -1269,6 +1269,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self cancelOmniboxEdit];
   [self setContentOffsetToTop];
   [self.feedHeaderViewController updateForFeedVisibilityChanged];
+  UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification,
+                                  nil);
 }
 
 - (void)feedDidScroll {
