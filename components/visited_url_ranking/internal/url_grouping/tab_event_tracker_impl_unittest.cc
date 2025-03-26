55b8f7fbbaf4af1d2440db3eb1cae000d60fa119
Yue Zhang <yuezhanggg@chromium.org>
2025-03-26 15:57:08 -0700

    [tab suggestions] Fix issues with tab switch counter
    * Adds selection timestamp recording so that we only return selection
      count within last 10 minutes.
    * Fix an over count issue for tab addition. With the current code,
      a newly added tab will be recorded with selection for 3 times, one
      from DidAddTab and two from DidSelectTab (one with selection type
      kFromUser and the other with kFromNewTab). This CL fixes this issue
      by only keeping the kFromUser signal.
    
    Bug: 397221723
    Change-Id: Ic5d939a3b581f9daa0d3afa3c0127b7d71e3819c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6393759
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438458}

--- components/visited_url_ranking/internal/url_grouping/tab_event_tracker_impl_unittest.cc
+++ components/visited_url_ranking/internal/url_grouping/tab_event_tracker_impl_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/visited_url_ranking/internal/url_grouping/tab_event_tracker_impl.h"
 
 #include "base/test/mock_callback.h"
+#include "base/test/task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace visited_url_ranking {
@@ -27,6 +28,8 @@ class TabEventTrackerImplTest : public testing::Test {
   }
 
  protected:
+  base::test::TaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   base::MockCallback<TabEventTrackerImpl::OnNewEventCallback> mock_callback_;
   std::unique_ptr<TabEventTrackerImpl> tab_event_tracker_;
 };
@@ -37,7 +40,7 @@ TEST_F(TabEventTrackerImplTest, CallbackCalled) {
 
   EXPECT_CALL(mock_callback_, Run());
   tab_event_tracker_->DidSelectTab(
-      1, TabEventTracker::TabSelectionType::kFromAppExit, 2);
+      1, TabEventTracker::TabSelectionType::kFromUser, 2);
 }
 
 TEST_F(TabEventTrackerImplTest, SwitchedCount) {
@@ -47,28 +50,87 @@ TEST_F(TabEventTrackerImplTest, SwitchedCount) {
   EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
   EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
 
+  // Add does not change counts.
   tab_event_tracker_->DidAddTab(1, 0);
-  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
   EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
 
   // Move does not change counts.
   tab_event_tracker_->DidMoveTab(1, 2, 3);
-  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
   EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
 
   tab_event_tracker_->DidSelectTab(
-      2, TabEventTracker::TabSelectionType::kFromAppExit, 1);
-  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+      2, TabEventTracker::TabSelectionType::kFromUser, 1);
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
   EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId2));
 
   tab_event_tracker_->DidEnterTabSwitcher();
-  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
   EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId2));
 
   tab_event_tracker_->DidSelectTab(
-      1, TabEventTracker::TabSelectionType::kFromAppExit, 2);
-  EXPECT_EQ(2, tab_event_tracker_->GetSelectedCount(kTabId1));
+      1, TabEventTracker::TabSelectionType::kFromUser, 2);
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
   EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId2));
+}
+
+TEST_F(TabEventTrackerImplTest, SwitchedCount_IgnoreOldSwitch) {
+  const int kTabId1 = 1;
+  const int kTabId2 = 2;
+
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
+
+  tab_event_tracker_->DidSelectTab(
+      1, TabEventTracker::TabSelectionType::kFromUser, 2);
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
+
+  task_environment_.FastForwardBy(base::Minutes(16));
+
+  tab_event_tracker_->DidSelectTab(
+      2, TabEventTracker::TabSelectionType::kFromUser, 1);
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId2));
+
+  task_environment_.FastForwardBy(base::Minutes(16));
+
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId2));
+
+  task_environment_.FastForwardBy(base::Minutes(16));
+
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
+}
+
+TEST_F(TabEventTrackerImplTest, SwitchedCount_CloseTab) {
+  const int kTabId1 = 1;
+  const int kTabId2 = 2;
+
+  tab_event_tracker_->DidSelectTab(
+      2, TabEventTracker::TabSelectionType::kFromUser, 1);
+  tab_event_tracker_->DidSelectTab(
+      1, TabEventTracker::TabSelectionType::kFromUser, 2);
+
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId2));
+
+  tab_event_tracker_->WillCloseTab(1);
+
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId2));
+
+  tab_event_tracker_->TabClosureCommitted(2);
+
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
+
+  tab_event_tracker_->TabClosureUndone(1);
+
+  EXPECT_EQ(1, tab_event_tracker_->GetSelectedCount(kTabId1));
+  EXPECT_EQ(0, tab_event_tracker_->GetSelectedCount(kTabId2));
 }
 
 }  // namespace visited_url_ranking
