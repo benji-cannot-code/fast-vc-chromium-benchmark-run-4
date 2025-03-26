5881f3d92e3df5b9f84c199a409de0d341048b4b
Wenyu Fu <wenyufu@chromium.org>
2025-03-25 17:39:44 -0700

    [E2E] Add E2E state provider as user data
    Make the state available for java classes that are created based on
    dependencies from WindowAndroid, where getting wiring done right is
    difficult.
    
    Usage see crrev.com/c/6394004
    
    Bug: 401075913
    Change-Id: I040ae974d4749216214786985e798b76a1f8ce63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6393937
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437859}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -444,6 +444,11 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     protected void onPostCreate() {
         incrementCounter(ChromePreferenceKeys.UMA_ON_POSTCREATE_COUNTER);
         super.onPostCreate();
+
+        // WindowAndroid is created in #onCreateInternal, happened before onPostCreate.
+        if (getWindowAndroid() != null) {
+            getEdgeToEdgeStateProvider().attach(getWindowAndroid());
+        }
     }
 
     @Override
@@ -1650,6 +1655,10 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         if (mBottomContainer != null) {
             mBottomContainer.destroy();
             mBottomContainer = null;
+        }
+
+        if (getEdgeToEdgeStateProvider() != null) {
+            getEdgeToEdgeStateProvider().detach();
         }
 
         WindowAndroid windowAndroid = getWindowAndroid();
