d3281bc3748cf17d66b95533c36573c5821bf466
Dominik Röttsches <drott@chromium.org>
2025-03-26 03:29:01 -0700

    [Fontations] Re-enable on Android
    Friendly gardeners: if you see pixel failures in
    content/test/data/forms/*, please consider assigning those to me instead
    of a revert. I'll provide a rebaseline quickly.
    
    Reland and reactivate Fontations after https://crrev.com/c/6378864 got
    reverted, initially for pixel differences, but also, a performance
    regression was measured.
    
    The performance regression is thought to be addressed by Skia change
    https://crrev.com/665a9d27ba558d9306dec8fb5bccd8f0c2cdd7a9, rolled in
    https://crrev.com/6e1e89f42a39dfff07a3e4998d42ec3a2c74a3e1 - autohinting
    GlyphStyles are now pre-computed at the font level, instead of redundant
    computation at the Fontations ScalerContext level.
    
    https://crrev.com/c/6382313 re-introduced the flag but kept it
    deactivated, activate it here.
    
    Bug: chromium:405247133, chromium:370816214
    Change-Id: I97cfa36d345232c688b0a6c8694692bdaafe2912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6381604
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438020}

--- skia/fontations_feature.cc
+++ skia/fontations_feature.cc
@@ -16,6 +16,6 @@ BASE_FEATURE(kFontationsLinuxSystemFonts,
 // SkFontMgr instantiation in skia/ext/font_utils.cc
 BASE_FEATURE(kFontationsAndroidSystemFonts,
              "FontationsAndroidSystemFonts",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace skia
