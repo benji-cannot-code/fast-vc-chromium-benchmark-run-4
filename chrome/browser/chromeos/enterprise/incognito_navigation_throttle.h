63992229f2f27739d4b31ce439c6029c2cca0253
Florian Jacky <fjacky@chromium.org>
2025-03-26 06:15:18 -0700

    Revert "Extend MandatoryExtensionsForIncognitoNavigation policy support to Windows, Mac, and Linux"
    This reverts commit 73fbdaf34db3ba52749f883a0d102d792058b744.
    
    Reason for revert: Likely culprit for ASAN builder failures, see issue.
    
    Original change's description:
    > Extend MandatoryExtensionsForIncognitoNavigation policy support to Windows, Mac, and Linux
    >
    > This change moves code that is currently ChromeOS only to Chrome
    > browser to support other platforms.
    >
    > Bug: 40269962
    > Change-Id: I5dfc1314d92f1e139822c3b519590bc71b583866
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6360938
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Commit-Queue: Dean Albury <deanalbury@google.com>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1437805}
    
    Bug: 40269962, 406464640
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I76b399bb39067ff647f8426a42d59d6967e67c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396698
    Owners-Override: Florian Jacky <fjacky@chromium.org>
    Auto-Submit: Florian Jacky <fjacky@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438059}

--- chrome/browser/chromeos/enterprise/incognito_navigation_throttle.h
+++ chrome/browser/chromeos/enterprise/incognito_navigation_throttle.h
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ENTERPRISE_INCOGNITO_INCOGNITO_NAVIGATION_THROTTLE_H_
-#define CHROME_BROWSER_ENTERPRISE_INCOGNITO_INCOGNITO_NAVIGATION_THROTTLE_H_
+#ifndef CHROME_BROWSER_CHROMEOS_ENTERPRISE_INCOGNITO_NAVIGATION_THROTTLE_H_
+#define CHROME_BROWSER_CHROMEOS_ENTERPRISE_INCOGNITO_NAVIGATION_THROTTLE_H_
 
 #include <string>
-
 #include "base/memory/raw_ptr.h"
 #include "base/values.h"
 #include "content/public/browser/navigation_throttle.h"
@@ -18,7 +17,7 @@ namespace content {
 class NavigationHandle;
 }  // namespace content
 
-namespace enterprise_incognito {
+namespace chromeos {
 
 // A `content::NavigationThrottle` implementation that blocks navigation in
 // Incognito mode unless the user enables a list of mandatory extensions to run
@@ -63,6 +62,6 @@ class IncognitoNavigationThrottle : public content::NavigationThrottle {
 
   base::WeakPtrFactory<IncognitoNavigationThrottle> weak_ptr_factory_{this};
 };
-}  // namespace enterprise_incognito
+}  // namespace chromeos
 
-#endif  // CHROME_BROWSER_ENTERPRISE_INCOGNITO_INCOGNITO_NAVIGATION_THROTTLE_H_
+#endif  // CHROME_BROWSER_CHROMEOS_ENTERPRISE_INCOGNITO_NAVIGATION_THROTTLE_H_
