8c24a329202f0f432d0eb786a4eaf3afdb2a462b
Istvan Nagy <iscsi@google.com>
2025-03-27 07:50:14 -0700

    Add early user check to the PinFactorEditor
    Bug: b:399148556
    Change-Id: If79df7b75990c3b4ff244c0119fc56e471709132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372040
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438737}

--- chromeos/ash/services/auth_factor_config/pin_factor_editor.cc
+++ chromeos/ash/services/auth_factor_config/pin_factor_editor.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/osauth/public/auth_session_storage.h"
 #include "chromeos/ash/services/auth_factor_config/auth_factor_config.h"
 #include "components/user_manager/known_user.h"
+#include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 
 namespace ash::auth {
@@ -134,6 +135,15 @@ void PinFactorEditor::SetPinWithContext(
     return;
   }
   AccountId account_id = context->GetAccountId();
+
+  const user_manager::User* user =
+      user_manager::UserManager::Get()->FindUser(account_id);
+  if (!user) {
+    LOG(ERROR) << "Invalid auth token: user does not exist";
+    std::move(callback).Run(mojom::ConfigureResult::kInvalidTokenError);
+    return;
+  }
+
   ash::AuthSessionStorage::Get()->Return(auth_token, std::move(context));
   pin_backend_->Set(
       account_id, auth_token, pin,
