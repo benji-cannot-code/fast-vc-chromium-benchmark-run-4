8efef6ab084e594c1c7ab3d67732e1ec5bb744cd
Adam Norberg <norberg@google.com>
2025-03-27 06:22:55 -0700

    Add missing `#include <string>` in mac_util.
    Just a minor "include what you use" thing.
    
    Change-Id: Ifc4ad435e6d8e3da438043ba0de3a002931efdce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6399255
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438697}

--- chrome/updater/util/mac_util.mm
+++ chrome/updater/util/mac_util.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <CoreFoundation/CoreFoundation.h>
 
 #include <optional>
+#include <string>
 
 #include "base/apple/bridging.h"
 #include "base/apple/foundation_util.h"
