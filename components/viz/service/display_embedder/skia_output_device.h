65b709e1a5a14256fe722861cd39a4be7d8b9ada
Saifuddin Hitawala <hitawala@chromium.org>
2025-03-27 08:21:17 -0700

    [viz] Remove SkiaOutputDevice graphite_context_
    Now that SkiaOutputDevice gr_context_ member is removed, we can also
    remove graphite_context_ by plumbing it on Draw method as all other
    usages of graphite_context_ have also been removed.
    
    Bug: 40266876
    Change-Id: I839c121f38129ad451980acee6e4ca0f7beb89ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397076
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438759}

--- components/viz/service/display_embedder/skia_output_device.h
+++ components/viz/service/display_embedder/skia_output_device.h
@@ -77,7 +77,8 @@ class VIZ_SERVICE_EXPORT SkiaOutputDevice {
     bool Draw(sk_sp<const GrDeferredDisplayList> ddl);
 
     // Graphite
-    bool Draw(std::unique_ptr<skgpu::graphite::Recording> graphite_recording,
+    bool Draw(skgpu::graphite::Context* graphite_context,
+              std::unique_ptr<skgpu::graphite::Recording> graphite_recording,
               base::OnceClosure on_finished);
 
     std::vector<GrBackendSemaphore> TakeEndPaintSemaphores() {
@@ -232,6 +233,7 @@ class VIZ_SERVICE_EXPORT SkiaOutputDevice {
   virtual bool Draw(SkSurface* sk_surface,
                     sk_sp<const GrDeferredDisplayList> ddl);
   virtual bool Draw(
+      skgpu::graphite::Context* graphite_context,
       SkSurface* sk_surface,
       std::unique_ptr<skgpu::graphite::Recording> graphite_recording,
       base::OnceClosure on_finished);
@@ -248,8 +250,6 @@ class VIZ_SERVICE_EXPORT SkiaOutputDevice {
       OutputSurfaceFrame frame,
       const std::optional<gfx::Rect>& damage_area = std::nullopt,
       std::vector<gpu::Mailbox> released_overlays = {});
-
-  const raw_ptr<skgpu::graphite::Context> graphite_context_;
 
   OutputSurface::Capabilities capabilities_;
 
