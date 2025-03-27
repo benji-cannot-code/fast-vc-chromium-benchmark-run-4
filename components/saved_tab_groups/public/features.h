fbfd11975efd8b35fe88bde8bea8f2be6ef9a66e
David Pennington <dpenning@chromium.org>
2025-03-26 17:01:40 -0700

    Remove kDeferMediaLoadInBackgroundTab
    We are consolidating flags behind data-sharing feature flag. This CL
    removes kDeferMediaLoadInBackgroundTab.
    
    Bug: 402231711
    Change-Id: Iea710b1a05d798ef46a7a3ce9ad5dd8bc5b4b748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6386783
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438495}

--- components/saved_tab_groups/public/features.h
+++ components/saved_tab_groups/public/features.h
@@ -27,8 +27,6 @@ BASE_DECLARE_FEATURE(kTabGroupSyncAutoOpenKillSwitch);
 
 BASE_DECLARE_FEATURE(kRestrictDownloadOnSyncedTabs);
 
-BASE_DECLARE_FEATURE(kDeferMediaLoadInBackgroundTab);
-
 BASE_DECLARE_FEATURE(kUseAlternateHistorySyncIllustration);
 
 BASE_DECLARE_FEATURE(kForceRemoveClosedTabGroupsOnStartup);
