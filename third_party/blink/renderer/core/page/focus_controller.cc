2e67ec184fad4fd9b2626d934c2e97f48c1a5c38
Lei Zhang <thestig@chromium.org>
2025-03-27 09:12:37 -0700

    Remove obsolete FocusController/KeyboardEventManager trace events
    These were used for diagnosing a bug that has been marked as obsolete
    for several years.
    
    Bug: 40680754
    Change-Id: I6144358143a7f1dc1e696c99bf1c6ea50729e9b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6400573
    Reviewed-by: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438788}

--- third_party/blink/renderer/core/page/focus_controller.cc
+++ third_party/blink/renderer/core/page/focus_controller.cc
@@ -1582,8 +1582,6 @@ bool FocusController::AdvanceFocus(
     mojom::blink::FocusType type,
     bool initial_focus,
     InputDeviceCapabilities* source_capabilities) {
-  // TODO (liviutinta) remove TRACE after fixing crbug.com/1063548
-  TRACE_EVENT0("input", "FocusController::AdvanceFocus");
   switch (type) {
     case mojom::blink::FocusType::kForward:
     case mojom::blink::FocusType::kBackward: {
@@ -1638,8 +1636,6 @@ bool FocusController::AdvanceFocusInDocumentOrder(
     mojom::blink::FocusType type,
     bool initial_focus,
     InputDeviceCapabilities* source_capabilities) {
-  // TODO (liviutinta) remove TRACE after fixing crbug.com/1063548
-  TRACE_EVENT0("input", "FocusController::AdvanceFocusInDocumentOrder");
   DCHECK(frame);
   Document* document = frame->GetDocument();
   OwnerMap owner_map;
@@ -1708,11 +1704,6 @@ bool FocusController::AdvanceFocusInDocumentOrder(
                                                                   owner_map);
 
     if (!element) {
-      // TODO (liviutinta) remove TRACE after fixing crbug.com/1063548
-      TRACE_EVENT_INSTANT1(
-          "input", "FocusController::AdvanceFocusInDocumentOrder",
-          TRACE_EVENT_SCOPE_THREAD, "reason_for_no_focus_element",
-          "no_recursive_focusable_element");
       return false;
     }
   }
