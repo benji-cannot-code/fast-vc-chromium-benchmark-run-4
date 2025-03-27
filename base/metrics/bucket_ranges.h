f0e260825c935230bf4d48b5043ab18ac3bf5cd6
Roger McFarlane <rogerm@chromium.org>
2025-03-27 13:06:38 -0700

    Improve validation of histogram range data when created from shared mem.
    This CL moves the logic to create histogram range data from a memory
    span into the BucketRanges class. It also improves the validation
    performed to ensure that:
    
    * the data is non-empty
    * the data values are non-negative
    * the data values are sorted in increasing order
    * the data values are unique
    
    This change further ensures that invalid histogram range data is
    gracefully ignored when read from shared memory, which is a fuzzed
    interface boundary.
    
    Bug: 403816126
    Change-Id: I17c1ffca08e85fafc0753623d9cda05814168122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6393876
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438950}

--- base/metrics/bucket_ranges.h
+++ base/metrics/bucket_ranges.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/check_op.h"
+#include "base/containers/span.h"
 #include "base/metrics/histogram_base.h"
 
 namespace base {
@@ -36,6 +37,7 @@ class BASE_EXPORT BucketRanges {
   typedef std::vector<HistogramBase::Sample32> Ranges;
 
   explicit BucketRanges(size_t num_ranges);
+  explicit BucketRanges(base::span<const HistogramBase::Sample32> data);
 
   BucketRanges(const BucketRanges&) = delete;
   BucketRanges& operator=(const BucketRanges&) = delete;
