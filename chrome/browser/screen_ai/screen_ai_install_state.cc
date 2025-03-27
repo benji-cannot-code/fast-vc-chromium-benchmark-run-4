0635e4562010f04252c21607130a0e76774ff586
Ramin Halavati <rhalavati@google.com>
2025-03-27 02:40:27 -0700

    Update ChromeScreenIA's min version.
    To force update the library release that fixes the empty lines OCR bug.
    
    AX-Relnotes: n/a
    Bug: 406145318
    Change-Id: Id6c2af5d31776a4483142cb6d12b2de3be3c0aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6390615
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438624}

--- chrome/browser/screen_ai/screen_ai_install_state.cc
+++ chrome/browser/screen_ai/screen_ai_install_state.cc
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/accessibility_features.h"
 
 namespace {
-// Proto updated.
-const char kMinExpectedVersion[] = "127.19";
+// crbug.com/406145318 and crbug.com/381000306
+const char kMinExpectedVersion[] = "136.00";
 const int kScreenAICleanUpDelayInDays = 30;
 
 bool IsDeviceCompatible() {
