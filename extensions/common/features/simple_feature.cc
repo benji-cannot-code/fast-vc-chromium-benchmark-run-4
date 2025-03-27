ddda57dc3a6b49edc1a3aae16a4189f412d3d2c1
Zengfeng Ye <zenye@microsoft.com>
2025-03-26 18:58:27 -0700

    Remove unused //content header from simple_feature.cc
    Bug: 406530496
    Change-Id: Icf33cb91d0100d72bc0d33bfc74cf28b5879c2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396177
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Zengfeng Ye <zenye@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1438538}

--- extensions/common/features/simple_feature.cc
+++ extensions/common/features/simple_feature.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "components/crx_file/id_util.h"
-#include "content/public/common/content_features.h"
 #include "extensions/common/extension_api.h"
 #include "extensions/common/extension_features.h"
 #include "extensions/common/extension_id.h"
