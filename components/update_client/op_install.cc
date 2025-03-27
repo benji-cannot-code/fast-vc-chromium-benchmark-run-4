6307a91b6bc5ce0b6a6472afc0f5217b92ae81fc
James Decker <deckerjames@google.com>
2025-03-27 15:04:07 -0700

    Begin emitting crx3 (install) operation event results
    Bug: 405226943
    Change-Id: Ie5bd5bed29829b36768e95acfa44092bb5214c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378631
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: James Decker <deckerjames@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439039}

--- components/update_client/op_install.cc
+++ components/update_client/op_install.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crx_file/crx_verifier.h"
 #include "components/update_client/configurator.h"
 #include "components/update_client/crx_cache.h"
+#include "components/update_client/protocol_definition.h"
 #include "components/update_client/task_traits.h"
 #include "components/update_client/unpacker.h"
 #include "components/update_client/unzipper.h"
@@ -80,6 +81,25 @@ class CallbackChecker : public base::RefCountedThreadSafe<CallbackChecker> {
   CrxInstaller::ProgressCallback progress_callback_;
 };
 
+base::Value::Dict MakeEvent(const CategorizedError& error) {
+  base::Value::Dict event;
+  event.Set("eventtype", protocol_request::kEventCrx3);
+  event.Set("eventresult",
+            static_cast<int>(error.category_ == ErrorCategory::kNone
+                                 ? protocol_request::kEventResultSuccess
+                                 : protocol_request::kEventResultError));
+  if (error.category_ != ErrorCategory::kNone) {
+    event.Set("errorcat", static_cast<int>(error.category_));
+  }
+  if (error.code_ != 0) {
+    event.Set("errorcode", error.code_);
+  }
+  if (error.extra_ != 0) {
+    event.Set("extracode1", error.extra_);
+  }
+  return event;
+}
+
 // Runs on the original sequence.
 void InstallComplete(
     base::OnceCallback<void(const CrxInstaller::Result&)>
@@ -89,7 +109,7 @@ void InstallComplete(
     base::RepeatingCallback<void(base::Value::Dict)> event_adder,
     base::FilePath crx_file,
     const CrxInstaller::Result& result) {
-  // TODO(crbug.com/353249967): Add an event describing the install's outcome.
+  event_adder.Run(MakeEvent(result.result));
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE, base::BindOnce(std::move(installer_result_callback), result));
   if (result.result.category_ != ErrorCategory::kNone) {
