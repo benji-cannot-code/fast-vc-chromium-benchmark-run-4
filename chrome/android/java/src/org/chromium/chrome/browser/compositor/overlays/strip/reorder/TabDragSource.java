95196eb87807aa304f8fe3311412b72937ddce2d
Zhe Li <zheliooo@google.com>
2025-03-27 12:32:46 -0700

    [TabGroupDragDrop] Handle merge collapsed group
    demo: https://drive.google.com/file/d/1tDtMBtQeHCshNWwT17MMzYj5Cqz6-7GZ/view?usp=sharing
    
    Bug: 405166521
    Change-Id: Ibd429ceb57322e1c884c0eace2d4cb84d8e22882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378206
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438919}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/TabDragSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/TabDragSource.java
@@ -520,7 +520,9 @@ public class TabDragSource implements View.OnDragListener {
             int tabIndex = helper.getTabIndexForTabDrop(dropEvent.getX() * mPxToDp);
             mMultiInstanceManager.moveTabToWindow(getActivity(), tabBeingDragged, tabIndex);
             helper.maybeMergeToGroupOnDrop(
-                    Collections.singletonList(tabBeingDragged.getId()), tabIndex);
+                    Collections.singletonList(tabBeingDragged.getId()),
+                    tabIndex,
+                    /* isCollapsed= */ false);
         }
         DragDropMetricUtils.recordTabDragDropType(
                 DragDropType.TAB_STRIP_TO_TAB_STRIP,
@@ -559,7 +561,8 @@ public class TabDragSource implements View.OnDragListener {
             if (!tabGroupMetadata.isGroupShared) {
                 maybeMergeToGroupOnDrop =
                         () -> {
-                            helper.maybeMergeToGroupOnDrop(tabIds, tabIndex);
+                            helper.maybeMergeToGroupOnDrop(
+                                    tabIds, tabIndex, tabGroupMetadata.tabGroupCollapsed);
                         };
             }
             mMultiInstanceManager.moveTabGroupToWindow(
