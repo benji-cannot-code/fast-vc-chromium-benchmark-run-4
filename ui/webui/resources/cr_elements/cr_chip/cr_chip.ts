06010bb0b20130e91f4a881968937e6818219609
Zaina Al-Mashni <zalmashni@google.com>
2025-03-27 08:21:52 -0700

    [Settings] Add time picker to the revamped DBD dialog
    This change create a new Polymer element to encapsulate the time
    picker UI in the new Delete browsing data dialog.
    It also handles the selection logic across the chips and dropdown and
    updates the position of the time period dynamically.
    
    The TimePeriods are ordered by duration, the first three chips are
    static, and the last chip can have different TimePeriods depending on
    the selection.
    The remaining TimePeriods are included in the dropdown menu.
    Selecting elements from the default chip, will only update the UI but
    will not change the position of the TimePeriods.
    Selecting elements from the dropdown will swap their position with
    the fourth 'dynamic' chip slot.
    
    Currently the selection does not update the pref or counters, this will
    added in a follow up.
    
    Demo: https://drive.google.com/file/d/1I04LxwojZgnQtPcGQw6OGAB2gGydRSrp/
    Bug: 397187800
    Change-Id: I25d0159c431812f3bc4fceec89bba444b8996313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6376403
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438760}

--- ui/webui/resources/cr_elements/cr_chip/cr_chip.ts
+++ ui/webui/resources/cr_elements/cr_chip/cr_chip.ts
@@ -36,7 +36,7 @@ export class CrChipElement extends CrChipElementBase {
       disabled: {type: Boolean},
       chipAriaLabel: {type: String},
       chipRole: {type: String},
-      selected: {type: Boolean},
+      selected: {type: Boolean, reflect: true},
     };
   }
 
