7479577eb970ef423b37681824553d4eb3f29186
Lei Zhang <thestig@chromium.org>
2025-03-27 09:00:40 -0700

    Remove unused base/trace_event/traced_value.h include
    No longer needed in cc/metrics/frame_sequence_metrics.h as of
    https://crrev.com/1252174.
    
    Bug: 40318405
    Change-Id: I36c1748cddfd46b2e1428567d3ce5e97d1c309c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6400473
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438784}

--- cc/metrics/frame_sequence_metrics.h
+++ cc/metrics/frame_sequence_metrics.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
-#include "base/trace_event/traced_value.h"
 #include "cc/cc_export.h"
 #include "cc/metrics/frame_info.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
