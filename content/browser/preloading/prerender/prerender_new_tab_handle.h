1d61aa6ec4c7fa7c4eabf0bfc0f2c6e5433a002f
Hiroki Nakagawa <nhiroki@chromium.org>
2025-03-26 19:57:30 -0700

    Reland "Prerender: Pass SpeculationRulesTags up to PrerenderHost"
    This reverts commit 7d99adf649ccdfa6f15f7da4c9b42e68e15eb3fb.
    
    Reason for revert:
    The original CL forgot to initialize `target_hint` and `eagerness` on
    SpeculationRulesParams for the default constructor. This reland CL
    fixes them.
    
    Original change's description:
    > Revert "Prerender: Pass SpeculationRulesTags up to PrerenderHost"
    >
    > This reverts commit 6d4fb315c6ecc3f20c5291fd027f3332a66fa26f.
    >
    > Reason for revert: Likely culprit for builder failures, see linked issue.
    >
    > Original change's description:
    > > Prerender: Pass SpeculationRulesTags up to PrerenderHost
    > >
    > > This is a preparation for supporting Speculation Rules tags for
    > > prerendering. This CL passes SpeculationRulesTags from PrerenererImpl to
    > > PrerenderHost, so that follow-up CLs can add the Sec-Speculation-Tags
    > > HTTP header based on that.
    > >
    > > PrerendererImpl passes other Speculation Rules specific parameters like
    > > the target hint and the eagerness. For code simplification, this CL
    > > bundle them into SpeculationRulesParams struct and also adds
    > > SpeculationRulesTags into the struct.
    > >
    > > This CL changes SpeculationRulesTags from movable-only to
    > > copyable-and-movable, as its owners (PrerenderAttributes and
    > > SpeculationRulesParams) can be copied.
    > >
    > > Bug: 381687257
    > > Change-Id: Id330fbc2ca71ecc314f33375e69ade84a8ff1ebd
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383243
    > > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > > Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    > > Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1437985}
    >
    > Bug: 381687257, 406437899
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I94452ade4c730aff782f2e75f1979e6d4757ddec
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396777
    > Auto-Submit: Florian Jacky <fjacky@chromium.org>
    > Owners-Override: Florian Jacky <fjacky@chromium.org>
    > Reviewed-by: Florian Jacky <fjacky@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1438017}
    
    Bug: 381687257, 406437899
    Change-Id: I167347f4952b319a1d76629c5d1bf26308448f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396437
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438547}

--- content/browser/preloading/prerender/prerender_new_tab_handle.h
+++ content/browser/preloading/prerender/prerender_new_tab_handle.h
@@ -66,7 +66,7 @@ class PrerenderNewTabHandle {
 
   // Returns SpeculationEagerness.
   std::optional<blink::mojom::SpeculationEagerness> eagerness() const {
-    return attributes_.eagerness;
+    return attributes_.GetEagerness();
   }
 
   PrerenderHostRegistry& GetPrerenderHostRegistry();
