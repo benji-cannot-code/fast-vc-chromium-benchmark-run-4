34e259d86c28f44c76382d39b248088a347732dc
Koji Ishii <kojii@chromium.org>
2025-03-27 10:56:27 -0700

    [performance] Mark `DISALLOW_NEW()` to two `ShapeResult` classes
    This patch marks `GlyphDataCollection` and `GlyphOffsetArray`
    as `DISALLOW_NEW()`.
    
    This patch has no behavior changes.
    
    Bug: 406127861
    Change-Id: I267fcab3a1ffa53b2a4834898c3a5aaed18534ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6400153
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438868}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_inline_headers.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_inline_headers.h
@@ -243,6 +243,8 @@ struct ShapeResult::RunInfo final
 
   // Collection of |HarfBuzzRunGlyphData| with optional glyph offset
   class GlyphDataCollection final {
+    DISALLOW_NEW();
+
    public:
     explicit GlyphDataCollection(unsigned num_glyphs)
         : data_(new HarfBuzzRunGlyphData[num_glyphs]), offsets_(num_glyphs) {}
