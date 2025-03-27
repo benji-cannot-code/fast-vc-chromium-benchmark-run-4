d544b89a1dd6e2d3268de52369f0b5fbb8e66a15
rbpotter <rbpotter@chromium.org>
2025-03-27 14:04:18 -0700

    [PDF Ink Signatures] Add metrics for text annotations UI
    - Add metrics for entering and exiting text annotation mode
    - Update tests to validate the correct metrics are recorded.
    
    Bug: 402547554
    Change-Id: Ie80ac2b997d1eac32ff8d12334427857b6275445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394103
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438994}

--- chrome/browser/resources/pdf/pdf_viewer.ts
+++ chrome/browser/resources/pdf/pdf_viewer.ts
@@ -540,7 +540,7 @@ export class PdfViewerElement extends PdfViewerBaseElement {
         record(UserAction.EXIT_INK2_ANNOTATION_MODE);
         break;
       case AnnotationMode.TEXT:
-        // TODO (crbug.com/402546554): Add text annotation metrics.
+        record(UserAction.EXIT_INK2_TEXT_ANNOTATION_MODE);
         break;
       case AnnotationMode.NONE:
         break;
@@ -554,7 +554,7 @@ export class PdfViewerElement extends PdfViewerBaseElement {
         record(UserAction.ENTER_INK2_ANNOTATION_MODE);
         break;
       case AnnotationMode.TEXT:
-        // TODO (crbug.com/402546554): Add text annotation metrics.
+        record(UserAction.ENTER_INK2_TEXT_ANNOTATION_MODE);
         break;
       case AnnotationMode.NONE:
         break;
