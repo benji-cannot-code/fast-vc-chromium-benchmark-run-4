bd4046e132176ab039fee6aaa0102ecd0b804c2e
Henrique Nakashima <hnakashima@chromium.org>
2025-03-27 09:30:56 -0700

    [Android] Migrate o.c.chrome.browser to Public Transit rules (part 1/2)
    Migrate the tests shallowly to use Public Transit entry points.
    
    Bug: 399877997
    Change-Id: I362d1041740be72cdc810c79128b4734f3346554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387897
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438803}

--- chrome/android/javatests/src/org/chromium/chrome/browser/FeaturesAnnotationsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/FeaturesAnnotationsTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 import androidx.test.filters.SmallTest;
 
-import org.junit.ClassRule;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,8 +24,9 @@ import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
-import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
+import org.chromium.chrome.test.transit.ChromeTransitTestRules;
+import org.chromium.chrome.test.transit.ReusedCtaTransitTestRule;
+import org.chromium.chrome.test.transit.page.WebPageStation;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,13 +37,14 @@ import java.util.List;
 @CommandLineFlags.Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
 @Batch(Batch.PER_CLASS)
 public class FeaturesAnnotationsTest {
-    @ClassRule
-    public static ChromeTabbedActivityTestRule sActivityTestRule =
-            new ChromeTabbedActivityTestRule();
-
     @Rule
-    public BlankCTATabInitialStateRule mInitialStateRule =
-            new BlankCTATabInitialStateRule(sActivityTestRule, false);
+    public ReusedCtaTransitTestRule<WebPageStation> mActivityTestRule =
+            ChromeTransitTestRules.blankPageStartReusedActivityRule();
+
+    @Before
+    public void setUp() {
+        mActivityTestRule.start();
+    }
 
     /**
      * Tests that {@link EnableFeatures} and {@link DisableFeatures} can alter the flags registered
