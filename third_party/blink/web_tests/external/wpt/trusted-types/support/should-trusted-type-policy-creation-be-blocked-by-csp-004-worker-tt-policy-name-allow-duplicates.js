8a330b742d21add761792c69fd6a8455a979a4a3
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-03-27 14:53:28 -0700

    Import wpt@07e7c98c000986641e01646a77756f6960c2b65f
    https://github.com/web-platform-tests/wpt/compare/085fb6bed...07e7c98c0
    
    Using wpt-import in Chromium c8dbea527b48d39f80415d89591be3acb65ac99f.
    With Chromium commits locally applied on WPT:
      dd9fe191f9 "[CompositeClipPathAnimation] Fix crash with non- [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    vogelheim@chromium.org:
      external/wpt/trusted-types
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I728c7e20c48159b4aa291cf8574ec5c1d48e7b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6404836
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1439034}

---
+++ third_party/blink/web_tests/external/wpt/trusted-types/support/should-trusted-type-policy-creation-be-blocked-by-csp-004-worker-tt-policy-name-allow-duplicates.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts("/resources/testharness.js");
+importScripts("csp-violations.js");
+
+// This test is similar to allow-duplicates-tt-policy-name.js but with a
+// different ordering of tt-expressions.
+
+// For CSP applying to this file, please refer to
+// should-trusted-type-policy-creation-be-blocked-by-csp-004-worker-tt-policy-name-allow-duplicates.js.headers
+const tt_directive = `tt-policy-name 'allow-duplicates'`;
+
+promise_test(async () => {
+  await no_trusted_type_violation_for(_ => trustedTypes.createPolicy("tt-policy-name"));
+} , `No violation/exception for allowed policy name (${tt_directive}).`);
+
+promise_test(async () => {
+  await no_trusted_type_violation_for(_ => trustedTypes.createPolicy("tt-policy-name"));
+} , `No violation/exception for duplicate policy name (${tt_directive}).`);
+
+promise_test(async () => {
+  await trusted_type_violation_for(TypeError, _ => trustedTypes.createPolicy("duplicate"));
+}, `Violation and exception for forbidden policy name 'duplicate' ${tt_directive}.`);
+
+done();
