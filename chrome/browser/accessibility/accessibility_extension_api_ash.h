9673a802418480b1a793869739c14633b0ed80c2
Aaron Leventhal <aleventhal@google.com>
2025-03-27 05:36:36 -0700

    [A11yPerformance] Use ScopedAccessibilityMode for Ash a11y extensions
    Bug: none
    Change-Id: I00ca819543380791eb566eecdacc406b85122c13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6395186
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438678}

--- chrome/browser/accessibility/accessibility_extension_api_ash.h
+++ chrome/browser/accessibility/accessibility_extension_api_ash.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "chrome/common/extensions/api/accessibility_private.h"
+#include "content/public/browser/scoped_accessibility_mode.h"
 #include "extensions/browser/extension_function.h"
 
 // API function that darkens or undarkens the screen.
@@ -292,12 +293,16 @@ class AccessibilityPrivateSetKeyboardListenerFunction
 // API function that enables or disables web content accessibility support.
 class AccessibilityPrivateSetNativeAccessibilityEnabledFunction
     : public ExtensionFunction {
-  ~AccessibilityPrivateSetNativeAccessibilityEnabledFunction() override =
-      default;
+ public:
+  AccessibilityPrivateSetNativeAccessibilityEnabledFunction();
+
+ private:
+  ~AccessibilityPrivateSetNativeAccessibilityEnabledFunction() override;
   ResponseAction Run() override;
   DECLARE_EXTENSION_FUNCTION(
       "accessibilityPrivate.setNativeAccessibilityEnabled",
       ACCESSIBILITY_PRIVATE_SETNATIVEACCESSIBILITYENABLED)
+  std::unique_ptr<content::ScopedAccessibilityMode> scoped_accessibility_mode_;
 };
 
 // API function that sets native ChromeVox ARC support.
