b4d38ba92e26bf7d78410380b4d23350d5b72ddc
Erica Lee <ericamlee@google.com>
2025-03-27 14:50:51 -0700

    p13n: Create a freeform translation experiment
    Create the ChromeOS experiment and send a new tag to the backend.
    
    BUG=b:406505817
    
    Change-Id: I483ae5c5271efd7dd1db97bf50e984903277bd80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397762
    Reviewed-by: Thuong Phan <thuongphan@chromium.org>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439030}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2626,6 +2626,11 @@ BASE_FEATURE(kSeaPenTextInput,
              "SeaPenTextInput",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables sea pen text input translation feature.
+BASE_FEATURE(kSeaPenTextInputTranslation,
+             "SeaPenTextInputTranslation",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables sea pen feature for ChromeOS demo mode.
 BASE_FEATURE(kSeaPenDemoMode,
              "SeaPenDemoMode",
@@ -4436,6 +4441,11 @@ bool IsSeaPenEnabled() {
 
 bool IsSeaPenTextInputEnabled() {
   return IsSeaPenEnabled() && base::FeatureList::IsEnabled(kSeaPenTextInput);
+}
+
+bool IsSeaPenTextInputTranslationEnabled() {
+  return IsSeaPenTextInputEnabled() &&
+         base::FeatureList::IsEnabled(kSeaPenTextInputTranslation);
 }
 
 bool IsSeaPenUseExptTemplateEnabled() {
