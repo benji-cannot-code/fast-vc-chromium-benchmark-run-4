f3c9ece8b3a7d0f6ace246c461095b563ccf9da0
Greg Thompson <grt@chromium.org>
2025-03-27 06:01:25 -0700

    [etw tracing] Prepare for enabling WindowsSystemTracing by default
    Do not record a failure to launch when an attempt to connect to the
    service fails with REGDB_E_CLASSNOTREG and the service is not
    registered. This is expected for per-machine beta and stable installs
    for which the user has not manually registered the service via
    chrome://traces-internals/scenarios.
    
    Bug: 41497361
    Change-Id: Ie08340e2eee5d36f59d2e17812787b0ce5d6de77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387163
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438683}

--- chrome/browser/tracing/windows_system_tracing_client_impl_win.cc
+++ chrome/browser/tracing/windows_system_tracing_client_impl_win.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/win/com_init_util.h"
 #include "base/win/win_util.h"
+#include "chrome/installer/util/install_service_work_item.h"
 #include "mojo/public/cpp/platform/named_platform_channel.h"
 #include "mojo/public/cpp/system/invitation.h"
 
@@ -76,7 +77,14 @@ WindowsSystemTracingClientImpl::Host::CreateSession() {
   Microsoft::WRL::ComPtr<ISystemTraceSession> trace_session;
   HRESULT hresult = ::CoCreateInstance(
       clsid_, /*pUnkOuter=*/nullptr, CLSCTX_LOCAL_SERVER, iid_, &trace_session);
-  RecordLaunchResult(LaunchStage::kCoCreateInstance, hresult);
+  // Do not record failure to create the instance if the service isn't
+  // registered. This is expected for per-machine beta and stable installs for
+  // which the user has not manually registered the service via
+  // chrome://traces-internals/scenarios.
+  if (hresult != REGDB_E_CLASSNOTREG ||
+      installer::InstallServiceWorkItem::IsComServiceInstalled(clsid_)) {
+    RecordLaunchResult(LaunchStage::kCoCreateInstance, hresult);
+  }
   if (FAILED(hresult)) {
     return base::unexpected(hresult);
   }
