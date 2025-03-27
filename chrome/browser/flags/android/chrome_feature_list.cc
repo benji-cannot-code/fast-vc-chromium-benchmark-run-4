7ce24f1d22e6044e01ef65ed15b9f2f0d14bc849
Nafis Abedin <nafisabedin@google.com>
2025-03-27 14:21:31 -0700

    Disable Battery Status Manager & Power Saving Mode Monitor receivers in BG.
    Battery Status Manager is causing assert failures in Webview in Canary
    so disabling for now. This change also disables Power Saving Mode
    Monitor receivers in BG as both features were being ramped together.
    
    Bug: 405978297
    Change-Id: Ie4b2445a4dc73ad53e251d663c0bd217da1b3b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397396
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439004}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -980,7 +980,7 @@ BASE_FEATURE(kPageContentProvider,
 
 BASE_FEATURE(kPowerSavingModeBroadcastReceiverInBackground,
              "PowerSavingModeBroadcastReceiverInBackground",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPreconnectOnTabCreation,
              "PreconnectOnTabCreation",
