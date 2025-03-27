7db77bcf5efae596e2763c2bc3a6f221ad83d140
Joel Hockey <joelhockey@chromium.org>
2025-03-26 19:45:09 -0700

    Revert "cros-geo-apikey: Update ChromeOS surfaces to use new API keys"
    This reverts commit f1341b6f555f480251c1343061d02b44ac7f642e.
    
    Reason for revert: many crashing tests when is_chrome_branded is set
    b/406579271
    
    Original change's description:
    > cros-geo-apikey: Update ChromeOS surfaces to use new API keys
    >
    > Introduce new methods for ChromeOS to access the ChromeOS-specific GCP
    > API keys. Update the `SimpleGeolocationRequest` and
    > `ChromeContentBrowserClient` to use these new keys instead.
    >
    > Bug: 394051078
    > Change-Id: I36b6995b3bfb7edd81ac63d785c0a8946101137b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361503
    > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Commit-Queue: Zauri Meshveliani <zauri@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1438300}
    
    Bug: 394051078, 406579271
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ie920746087812a1fd118b66031e7bd42cf8c210f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6399493
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438546}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3777,11 +3777,6 @@ ChromeContentBrowserClient::GetSystemNetworkContext() {
 }
 
 std::string ChromeContentBrowserClient::GetGeolocationApiKey() {
-#if BUILDFLAG(IS_CHROMEOS)
-  if (ash::features::IsCrosSeparateGeoApiKeyEnabled()) {
-    return google_apis::GetCrosChromeGeoAPIKey();
-  }
-#endif
   return google_apis::GetAPIKey();
 }
 
--- chromeos/ash/components/geolocation/simple_geolocation_request.h
+++ chromeos/ash/components/geolocation/simple_geolocation_request.h
@@ -86,7 +86,6 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_GEOLOCATION)
   static void SetTestMonitor(SimpleGeolocationRequestTestMonitor* monitor);
 
   std::string FormatRequestBodyForTesting() const;
-  GURL GetServiceURLForTesting() const;
 
  private:
   void OnSimpleURLLoaderComplete(std::unique_ptr<std::string> response_body);
