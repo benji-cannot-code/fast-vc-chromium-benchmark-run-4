cc9078971176b0185c764c54733c4ae0ac3a3e1c
Camillia Smith Barnes <cammie@chromium.org>
2025-03-27 16:03:35 -0700

    Shared Storage: Tidy up SharedStorageEventParams before adding more
    We do a little cleanup for the SharedStorageEventParams struct, since
    we will shortly be adding several more members to the struct in
    followup CLs.
    
    In particular, we
    (1) remove a TODO that we already completed and forgot to delete,
    (2) insert parameter labels for the numerous places that std::nullopt
    is passed in the static creator methods,
    (3) implement an operator<< overload for SharedStorageEventParams::
    SharedStorageUrlSpecWithMetadata, and
    (4) revise the anonymous namespace function
    SerializeOptionalUrlsWithMetadata() to take advantage of the new
    operator<< for the child struct, as well as to use style consistent
    with the existing operator<< for the parent struct.
    
    Bug: 401011862
    Change-Id: If5c883f7e3df6d02890608a9ac1bfbfac38f2e8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6399793
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439068}

--- content/browser/shared_storage/shared_storage_event_params.h
+++ content/browser/shared_storage/shared_storage_event_params.h
@@ -34,6 +34,9 @@ class CONTENT_EXPORT SharedStorageEventParams {
     SharedStorageUrlSpecWithMetadata& operator=(
         const SharedStorageUrlSpecWithMetadata&);
     bool operator==(const SharedStorageUrlSpecWithMetadata&) const;
+    friend std::ostream& operator<<(
+        std::ostream& os,
+        const SharedStorageUrlSpecWithMetadata& url_with_metadata);
   };
 
   static SharedStorageEventParams CreateForAddModule(
@@ -66,8 +69,6 @@ class CONTENT_EXPORT SharedStorageEventParams {
       const std::string& key,
       std::optional<int> worklet_id = std::nullopt);
 
-  // TODO(crbug.com/401011862): Use `CreateWithWorkletId()` for the worklet
-  // events that previously used `CreateDefault()`.
   static SharedStorageEventParams CreateWithWorkletId(int worklet_id);
   static SharedStorageEventParams CreateDefault();
 
