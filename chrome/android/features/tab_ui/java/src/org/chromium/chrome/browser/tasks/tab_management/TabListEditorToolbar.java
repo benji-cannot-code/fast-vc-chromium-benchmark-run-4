3cec311c519c6f32f8b8ab4c21c66f57ad83d3fa
Fiaz Muhammad <mfiaz@google.com>
2025-03-27 11:51:28 -0700

    [Hub Colors] Set TabListEditorToolbar Background Color
    - Previously, this was only set on #showSelectionView
    - Adds missing #setBackgroundColor to this method
    
    Bug: 406470698
    Change-Id: I902ab271390dce302213593757c1422d5d4e15dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6403275
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438906}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorToolbar.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorToolbar.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.tasks.tab_management;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.ViewGroup;
@@ -107,7 +108,7 @@ class TabListEditorToolbar extends SelectableListToolbar<Integer> {
     @Override
     protected void showSelectionView(List<Integer> selectedItems, boolean wasSelectionEnabled) {
         super.showSelectionView(selectedItems, wasSelectionEnabled);
-        if (mBackgroundColor != 0) {
+        if (mBackgroundColor != Color.TRANSPARENT) {
             setBackgroundColor(mBackgroundColor);
         }
     }
@@ -138,10 +139,12 @@ class TabListEditorToolbar extends SelectableListToolbar<Integer> {
 
     /**
      * Update the toolbar background color.
+     *
      * @param backgroundColor The new color to use.
      */
     public void setToolbarBackgroundColor(@ColorInt int backgroundColor) {
         mBackgroundColor = backgroundColor;
+        setBackgroundColor(mBackgroundColor);
     }
 
     /**
