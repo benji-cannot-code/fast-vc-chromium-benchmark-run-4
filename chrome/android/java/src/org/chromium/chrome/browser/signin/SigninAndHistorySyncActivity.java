1a76b1966ce15c17711d63d4457c3dd1b134bbdd
Joel Tan-Aristy <jtanaristy@google.com>
2025-03-27 08:58:13 -0700

    [E2E] Adds nullchecks for EdgeToEdgeManager and EdgeToEdgeStateProvider
    Bug: 402825578
    Change-Id: If9f25af0eb653f4e849247d416d52b77ffa1a766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6398043
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438781}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninAndHistorySyncActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninAndHistorySyncActivity.java
@@ -266,7 +266,9 @@ public class SigninAndHistorySyncActivity extends FullscreenSigninAndHistorySync
     @Override
     public void setStatusBarColor(int statusBarColor) {
         StatusBarColorController.setStatusBarColor(
-                getEdgeToEdgeManager().getEdgeToEdgeSystemBarColorHelper(),
+                (getEdgeToEdgeManager() != null)
+                        ? getEdgeToEdgeManager().getEdgeToEdgeSystemBarColorHelper()
+                        : null,
                 getWindow(),
                 statusBarColor);
     }
