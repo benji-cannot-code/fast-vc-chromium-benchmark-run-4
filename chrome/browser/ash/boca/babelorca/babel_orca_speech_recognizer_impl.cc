c33c17045a2109ab7483ffcd8a3d19e1f9fd0c31
Avynn Donaghe <avynn@google.com>
2025-03-27 13:23:39 -0700

    [BabelOrca] Add Multiple States to SodaInstaller callback
    Adds multiple states to the SodaInstaller callback in order to
    differentiate error types.
    
    Bug: 378702821
    Change-Id: I7a65889c8363b2306c848aa9bf385f351fa25ee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387291
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438963}

--- chrome/browser/ash/boca/babelorca/babel_orca_speech_recognizer_impl.cc
+++ chrome/browser/ash/boca/babelorca/babel_orca_speech_recognizer_impl.cc
@@ -10,12 +10,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "chrome/browser/ash/accessibility/live_caption/system_live_caption_service.h"
 #include "chromeos/ash/components/boca/babelorca/babel_orca_speech_recognizer.h"
+#include "chromeos/ash/components/boca/babelorca/soda_installer.h"
 #include "chromeos/ash/components/boca/babelorca/speech_recognition_event_handler.h"
 #include "components/live_caption/pref_names.h"
 #include "media/mojo/mojom/speech_recognition.mojom.h"
 #include "media/mojo/mojom/speech_recognition_result.h"
 
 namespace ash::babelorca {
+
+namespace {
+void UnwrapSodaInstallationStatus(
+    base::OnceCallback<void(bool)> availabililty_callback,
+    SodaInstaller::InstallationStatus status) {
+  std::move(availabililty_callback)
+      .Run(status == SodaInstaller::InstallationStatus::kReady);
+}
+}  // namespace
 
 BabelOrcaSpeechRecognizerImpl::BabelOrcaSpeechRecognizerImpl(
     Profile* profile,
@@ -27,10 +37,6 @@ BabelOrcaSpeechRecognizerImpl::BabelOrcaSpeechRecognizerImpl(
       soda_installer_(global_prefs, profile->GetPrefs(), application_locale),
       speech_recognition_event_handler_(application_locale),
       primary_profile_(profile) {
-  // TODO(384026579): Notify Producer of error, then retry or alert user.
-  // We pass DoNothing here as there is no error reporting mechanism and
-  // we don't want to start speech recognition until Start is called.
-  soda_installer_.GetAvailabilityOrInstall(base::DoNothing());
 }
 BabelOrcaSpeechRecognizerImpl::~BabelOrcaSpeechRecognizerImpl() = default;
 
@@ -48,9 +54,11 @@ void BabelOrcaSpeechRecognizerImpl::OnLanguageIdentificationEvent(
 
 void BabelOrcaSpeechRecognizerImpl::Start() {
   // TODO(384026579): Notify Producer of error, then retry or alert user.
-  soda_installer_.GetAvailabilityOrInstall(base::BindOnce(
-      &SystemLiveCaptionService::SpeechRecognitionAvailabilityChanged,
-      service_ptr_factory_.GetWeakPtr()));
+  soda_installer_.InstallSoda(base::BindOnce(
+      &UnwrapSodaInstallationStatus,
+      base::BindOnce(
+          &SystemLiveCaptionService::SpeechRecognitionAvailabilityChanged,
+          service_ptr_factory_.GetWeakPtr())));
 }
 
 void BabelOrcaSpeechRecognizerImpl::Stop() {
