1bd214ca623e7318de739af55fd891f13b9d96ba
Morten Stenshorne <mstensho@chromium.org>
2025-03-27 02:46:22 -0700

    [carousel] Focus a column with inlines from ::column::scroll-marker.
    Look for culled inlines, and handle fragmented inlines correctly (both
    culled and non-culled ones). Need to know if they are "first for node"
    or not.
    
    Bug: 378698659
    Change-Id: I4bb4ff1b45f7af3cc7e50dc4f22d5fc1e758000a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394975
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438626}

--- third_party/blink/renderer/core/layout/tree_traversal_utils.h
+++ third_party/blink/renderer/core/layout/tree_traversal_utils.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class LayoutInline;
 class PhysicalBoxFragment;
 
 // How to proceed after having processed a fragment (via the callback).
@@ -20,16 +21,23 @@ enum class FragmentTraversalNextStep {
   kSkipChildren,
 };
 
+// TODO(bug/406288653): Get rid of the LayoutInline parameter.
+using BoxFragmentDescendantsCallback =
+    base::FunctionRef<FragmentTraversalNextStep(
+        const PhysicalBoxFragment*,
+        const LayoutInline* culled_inline,
+        bool is_first_for_node)>;
+
 // Visit every box fragment descendant in the subtree, depth-first, from left to
 // right, including those inside inline formatting contexts (FragmentItem), and
 // invoke the callback for each. Fragments that both have PhysicalBoxFragment
 // children and an inline formatting context (rare) will walk the
 // PhysicalBoxFragment children first. For each descendant visited, the
 // specified callback will be called, and its return value determines how to
-// proceed with the traversal afterwards.
-void ForAllBoxFragmentDescendants(
-    const PhysicalBoxFragment&,
-    base::FunctionRef<FragmentTraversalNextStep(const PhysicalBoxFragment&)>);
+// proceed with the traversal afterwards. As an added bonus, mostly thanks to
+// crbug.com/406288653 , culled inlines also have to be visited.
+void ForAllBoxFragmentDescendants(const PhysicalBoxFragment&,
+                                  BoxFragmentDescendantsCallback);
 
 }  // namespace blink
 
