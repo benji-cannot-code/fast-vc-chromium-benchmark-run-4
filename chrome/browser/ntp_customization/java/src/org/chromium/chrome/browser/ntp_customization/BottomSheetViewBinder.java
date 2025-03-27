e609868e452cc7bd8029df59bfb251b69566f9a4
Yanling <yanlinghu@google.com>
2025-03-27 13:39:04 -0700

    [NTP Customization] Rewrite a list in the main bottom sheet
    This CL rewrites the list in the main bottom sheet using a general
    container view sharing among all NTP Customization bottom sheets. The
    ntp cards bottom sheet also used the general container view and the UI
    stays the same before and after this CL.
    
    The specific UI can be found in the documentation:
    https://docs.google.com/document/d/1bMRVyyqOd0-cAmyTbL9IhKNU9uSvDdpdCngSzSXIHnk/edit?tab=t.0#bookmark=id.apbu6upgx5x8
    
    Bug: 376238770
    Change-Id: I9302beb5fe4e789f8abf74b301dd472386af2dd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6319103
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438979}

---
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/BottomSheetViewBinder.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ntp_customization;
+
+import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationViewProperties.BACK_PRESS_HANDLER;
+
+import android.view.View;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Class responsible for binding a back press handler to the back button in a bottom sheet. */
+public class BottomSheetViewBinder {
+    public static void bind(PropertyModel model, View view, PropertyKey propertyKey) {
+        if (propertyKey == BACK_PRESS_HANDLER) {
+            View backButton = view.findViewById(R.id.back_button);
+            backButton.setOnClickListener(model.get(BACK_PRESS_HANDLER));
+        }
+    }
+}
--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationCoordinatorUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationCoordinatorUnitTest.java
@@ -6,13 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp_customization;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator.BottomSheetType.MAIN;
-import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationViewProperties.NTP_CARDS_BACK_PRESS_HANDLER;
-import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationViewProperties.NTP_CARDS_OPTION_CLICK_LISTENER;
+import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator.BottomSheetType.NTP_CARDS;
 
 import android.content.Context;
 import android.view.View;
@@ -31,7 +30,6 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
-import org.chromium.ui.modelutil.PropertyModel;
 
 /** Unit tests for {@link NtpCustomizationCoordinator} */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -40,7 +38,6 @@ public class NtpCustomizationCoordinatorUnitTest {
 
     @Mock private BottomSheetController mBottomSheetController;
     @Mock private NtpCustomizationMediator mMediator;
-    @Mock private PropertyModel mPropertyModel;
     @Mock private View mView;
     @Mock private ViewFlipper mViewFlipper;
 
@@ -51,27 +48,9 @@ public class NtpCustomizationCoordinatorUnitTest {
     public void setUp() {
         mContext = ApplicationProvider.getApplicationContext();
         mNtpCustomizationCoordinator =
-                new NtpCustomizationCoordinator(mContext, mBottomSheetController, mPropertyModel);
-        mNtpCustomizationCoordinator.setMediatorForTesting(mMediator);
+                new NtpCustomizationCoordinator(mContext, mBottomSheetController);
         mNtpCustomizationCoordinator.setViewFlipperForTesting(mViewFlipper);
-    }
-
-    @Test
-    @SmallTest
-    public void testConstructor() {
-        // Verifies that the mNtpCardsCoordinator is initialized.
-        NtpCustomizationCoordinator coordinator =
-                new NtpCustomizationCoordinator(
-                        mContext,
-                        mBottomSheetController,
-                        new PropertyModel(NtpCustomizationViewProperties.ALL_KEYS));
-
-        View ntpCards =
-                coordinator
-                        .getContentViewForTesting()
-                        .findViewById(R.id.new_tab_page_cards_list_item_container);
-        ntpCards.performClick();
-        assertNotNull(coordinator.getNtpCardsCoordinatorForTesting());
+        mNtpCustomizationCoordinator.setMediatorForTesting(mMediator);
     }
 
     @Test
@@ -83,8 +62,9 @@ public class NtpCustomizationCoordinatorUnitTest {
 
     @Test
     @SmallTest
-    public void testImplementBottomSheetDelegate() {
-        BottomSheetDelegate delegate = mNtpCustomizationCoordinator.getDelegateForTesting();
+    public void testBottomSheetDelegateImplementation() {
+        BottomSheetDelegate delegate =
+                mNtpCustomizationCoordinator.getBottomSheetDelegateForTesting();
 
         // Verifies each implementation calls the corresponding method of the mediator.
         delegate.registerBottomSheetLayout(11, mView);
@@ -97,12 +77,25 @@ public class NtpCustomizationCoordinatorUnitTest {
 
     @Test
     @SmallTest
+    public void testGetOptionClickListener() {
+        View.OnClickListener listener =
+                mNtpCustomizationCoordinator.getOptionClickListener(NTP_CARDS);
+
+        listener.onClick(new View(mContext));
+        assertNotNull(mNtpCustomizationCoordinator.getNtpCardsCoordinatorForTesting());
+        verify(mMediator).showBottomSheet(eq(NTP_CARDS));
+    }
+
+    @Test
+    @SmallTest
     public void testDestroy() {
+        NtpCardsCoordinator ntpCardsCoordinator = mock(NtpCardsCoordinator.class);
+        mNtpCustomizationCoordinator.setNtpCardsCoordinatorForTesting(ntpCardsCoordinator);
+
         mNtpCustomizationCoordinator.destroy();
 
-        assertNull(mPropertyModel.get(NTP_CARDS_OPTION_CLICK_LISTENER));
-        assertNull(mPropertyModel.get(NTP_CARDS_BACK_PRESS_HANDLER));
         verify(mViewFlipper).removeAllViews();
         verify(mMediator).destroy();
+        verify(ntpCardsCoordinator).destroy();
     }
 }
