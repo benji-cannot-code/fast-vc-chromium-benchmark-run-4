101284e34100407599c93f8b203ac4df16079b25
Michael Wilson <mjwilson@chromium.org>
2025-03-27 12:53:43 -0700

    Revert "demo_mode: Clean up user created DriveFs files between shoppers visit"
    This reverts commit 823cd78f0372a9145c3254a07398a93122126b9b.
    
    Reason for revert: Likely culprit for build failure: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/87210/overview
    
    See also bug 406823191
    
    Original change's description:
    > demo_mode: Clean up user created DriveFs files between shoppers visit
    >
    > Delete all files under DriveFS root without logout user after device is
    > idle for 90s.
    >
    > Bug: 396731490
    > Change-Id: Ifcec9fa02b4d2041c559749b8bda9d6f7efa5baf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318925
    > Reviewed-by: François Degros <fdegros@chromium.org>
    > Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1438893}
    
    Bug: 396731490
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iaab410bf46245a5babb8ad4886b196c42dc50c86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405234
    Owners-Override: Michael Wilson <mjwilson@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438935}

--- chrome/browser/ash/login/demo_mode/demo_mode_idle_handler.h
+++ chrome/browser/ash/login/demo_mode/demo_mode_idle_handler.h
@@ -74,8 +74,6 @@ class DemoModeIdleHandler : public ui::UserActivityObserver {
   // Not owned:
   raw_ptr<DemoModeWindowCloser> window_closer_;
 
-  scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
-
   // Cleaner for `MyFiles` directory:
   chromeos::FilesCleanupHandler file_cleaner_;
 
