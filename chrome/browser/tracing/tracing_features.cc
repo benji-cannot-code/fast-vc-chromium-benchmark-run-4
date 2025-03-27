69c4eff040c058c5e49145f738fd83fb1c48c2b7
Etienne Pierre-Doray <etiennep@chromium.org>
2025-03-27 06:26:54 -0700

    [tracing] Enable WindowsSystemTracing by default
    WindowsSystemTracing is only relevant for users who run tracing.
    The feature is mostly a no-op if etw service isn't installed,
    hence enabling the feature for everyone should be safe.
    
    Change-Id: I6ce63beed08af75096ba397a1854f5745374e7ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6386751
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438700}

--- chrome/browser/tracing/tracing_features.cc
+++ chrome/browser/tracing/tracing_features.cc
@@ -8,5 +8,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 BASE_FEATURE(kWindowsSystemTracing,
              "WindowsSystemTracing",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_WIN)
