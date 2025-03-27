f6fd4ff7423c3decbb887b25e999bdd465354b9a
Chuong Ho <hdchuong@google.com>
2025-03-26 20:31:10 -0700

    lobster: add flag to enable/disable IME restriction
    Bug: b:406392667
    Change-Id: I9910c9b21d403b9976a78810981167a439d1461c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396019
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438554}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1773,6 +1773,11 @@ BASE_FEATURE(kLobsterFileNamingImprovement,
              "LobsterFileNamingImprovement",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enables lobster restriction based on the current active IME.
+BASE_FEATURE(kLobsterDisabledByInvalidIME,
+             "LobsterDisabledByInvalidIME",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Enables lobster entry point in quick insert zero state.
 BASE_FEATURE(kLobsterQuickInsertZeroState,
              "LobsterQuickInsertZeroState",
@@ -3999,6 +4004,10 @@ bool IsLobsterEnabled() {
   return base::FeatureList::IsEnabled(kLobsterDogfood) ||
          (base::FeatureList::IsEnabled(kLobster) &&
           base::FeatureList::IsEnabled(kFeatureManagementLobster));
+}
+
+bool IsLobsterDisabledByInvalidIME() {
+  return base::FeatureList::IsEnabled(kLobsterDisabledByInvalidIME);
 }
 
 bool IsLobsterUseRewrittenQuery() {
