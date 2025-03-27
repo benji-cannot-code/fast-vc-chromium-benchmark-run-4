39ff3126a832c9d0910c19be73ae459ff699c008
Fiaz Muhammad <mfiaz@google.com>
2025-03-27 08:26:51 -0700

    [Tab Group Entry Points] New Feature Flag
    - New feature flag
    
    Bug: 406315806
    Change-Id: Iacdd3eb1650c3648c74dcc88885bdd4da1aa5f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397156
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1438767}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -9855,6 +9855,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kTabGroupPaneAndroidDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(tab_groups::kTabGroupPaneAndroid)},
 
+    {"tab-group-entry-points-android",
+     flag_descriptions::kTabGroupEntryPointsAndroidName,
+     flag_descriptions::kTabGroupEntryPointsAndroidDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kTabGroupEntryPointsAndroid)},
+
     {"tab-group-parity-bottom-sheet-android",
      flag_descriptions::kTabGroupParityBottomSheetAndroidName,
      flag_descriptions::kTabGroupParityBottomSheetAndroidDescription,
