497f7bd97d5b40db0bc258c8f901d04033443868
Peter E Conn <peconn@chromium.org>
2025-03-27 05:32:52 -0700

    Add logging to loadUrl to capture success.
    WebView's loadUrl relies on NavigationController.loadUrl, which can
    return null. WebView ignores the return value so the developer doesn't
    know whether this happens or not.
    
    This change adds logging to determine how often this happens in practice.
    
    Change-Id: Ibac4d107864d46bb0932886dfbc6e8c1571eb550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382088
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438676}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -2509,7 +2509,8 @@ public class AwContents implements SmartClipProvider {
         // "fixing".  See also https://crbug.com/1145717.
         params.setUrl(UrlFormatter.fixupUrl(params.getUrl()).getPossiblyInvalidSpec());
 
-        mNavigationController.loadUrl(params);
+        NavigationHandle result = mNavigationController.loadUrl(params);
+        RecordHistogram.recordBooleanHistogram("Android.WebView.LoadUrl.Success", result != null);
 
         // The behavior of WebViewClassic uses the populateVisitedLinks callback in WebKit.
         // Chromium does not use this use code path and the best emulation of this behavior to call
