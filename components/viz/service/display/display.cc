db01e9cca115807bd7125e733f6d666e2a8bd543
harthuang <harthuang@google.com>
2025-03-27 15:00:41 -0700

    Add UMA histogram to track Viz display compositor code path usages
    Change-Id: I42f515eb934c15cd37f3110421dabd5cf56c8a26
    Bug: 399710254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6264790
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Commit-Queue: Hart Huang <harthuang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439037}

--- components/viz/service/display/display.cc
+++ components/viz/service/display/display.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/containers/contains.h"
+#include "base/containers/flat_set.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/observer_list.h"
@@ -816,6 +817,7 @@ OverdrawTracker::OverdrawTimeSeries Display::StopTrackingOverdraw() {
 
 bool Display::DrawAndSwap(const DrawAndSwapParams& params) {
   TRACE_EVENT0("viz", "Display::DrawAndSwap");
+  VIZ_HIT_PATH("DrawAndSwap");
   if (debug_settings_->show_aggregated_damage !=
       aggregator_->HasFrameAnnotator()) {
     if (debug_settings_->show_aggregated_damage) {
@@ -912,6 +914,7 @@ bool Display::DrawAndSwap(const DrawAndSwapParams& params) {
     // aggregated again so that the trail exists for a single frame.
     target_damage_bounding_rect.Union(
         renderer_->GetDelegatedInkTrailDamageRect());
+    VIZ_HIT_PATH("Aggregate");
     frame = aggregator_->Aggregate(
         current_surface_id_, params.expected_display_time,
         current_display_transform, target_damage_bounding_rect,
