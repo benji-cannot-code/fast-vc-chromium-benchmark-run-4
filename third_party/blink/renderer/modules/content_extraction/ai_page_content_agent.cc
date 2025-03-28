d02fe8e748757baf44a052c3c55f26ba5b6af9d3
Gary Klassen <gklassen@chromium.org>
2025-03-28 16:54:08 -0700

    Add Node Depth UKM metric for AIPageContent.
    UKM Collection Review Doc is https://docs.google.com/document/d/1wf99leudueP4WJOnuYFd5RDuPSeg64tKDpvGvGXCiPI/edit?usp=sharing&resourcekey=0-buhzwIk_KKrANyLCXvxcPg
    
    Bug: 404239036
    Change-Id: Iedb3d8e2eb923dc2e246843d8170d00654aa4573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6371458
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439787}

--- third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
+++ third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.h"
 
 #include "base/time/time.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "third_party/blink/public/mojom/content_extraction/ai_page_content.mojom-blink.h"
 #include "third_party/blink/renderer/core/accessibility/ax_object_cache.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
@@ -684,6 +685,13 @@ mojom::blink::AIPageContentPtr AIPageContentAgent::ContentBuilder::Build(
   WalkChildren(*layout_view, *root_node, *document_style);
   page_content->root_node = std::move(root_node);
 
+  if (stack_depth_exceeded_) {
+    ukm::builders::OptimizationGuide_AIPageContentAgent(
+        document.UkmSourceID())
+        .SetNodeDepthLimitExceeded(true)
+        .Record(document.UkmRecorder());
+  }
+
   return page_content;
 }
 
@@ -741,7 +749,7 @@ bool AIPageContentAgent::ContentBuilder::WalkChildren(
   // used in message creation.
   static const int kMaxTreeDepth = kMaxRecursionDepth - 8;
   if (stack_depth_ > kMaxTreeDepth) {
-    // TODO(gklassen): Add a metric for this.
+    stack_depth_exceeded_ = true;
     return false;
   }
 
