a81a3fa75f06631c536754362c35f369d9682ef6
Richard Chui <richui@chromium.org>
2025-03-28 13:47:13 -0700

    focus-mode: Update soundscape config size limit
    In the process of trying to update the soundscape playlists to have
    localized translations, we realized that the new config size will exceed
    our previous expectations. This CL updates the max config size allowed,
    updates the config endpoint for backwards compatibility, and also adds
    some tests for other translation cases.
    
    Bug: 360422919
    Test: updated, added new RTL test
    Change-Id: I4a293f8235e3bf54fdb511df327700d9dde0b756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6400293
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439679}

--- ash/system/focus_mode/sounds/soundscape/soundscapes_downloader.cc
+++ ash/system/focus_mode/sounds/soundscape/soundscapes_downloader.cc
@@ -25,7 +25,7 @@ namespace ash {
 
 namespace {
 
-constexpr size_t kMaxDownloadBytes = 20 * 1024;
+constexpr size_t kMaxDownloadBytes = 50 * 1024;
 
 // Max number of retries for fetching the configuration.
 constexpr int kMaxRetries = 3;
@@ -34,7 +34,7 @@ SoundscapesDownloader::Urls ProductionConfiguration(const std::string& locale) {
   SoundscapesDownloader::Urls configuration;
   configuration.locale = locale;
   configuration.host = GURL("https://gstatic.com/chromeos-focusmode/");
-  configuration.config_path = "config.json";
+  configuration.config_path = "config_v2.json";
   return configuration;
 }
 
