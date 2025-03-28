b78e85dff83ebf6aa8670433c1a2db78b0cf473e
Yue Zhang <yuezhanggg@chromium.org>
2025-03-28 16:39:48 -0700

    [tab suggestions] Enable testing single heuristics
    Bug: 397221723
    Change-Id: I511059d689ed661398d86f16dff80565a01583c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6410395
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439775}

--- components/visited_url_ranking/internal/url_grouping/grouping_heuristics.cc
+++ components/visited_url_ranking/internal/url_grouping/grouping_heuristics.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <variant>
 
+#include "base/containers/contains.h"
 #include "base/containers/fixed_flat_map.h"
 #include "base/containers/flat_map.h"
 #include "base/hash/hash.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/segmentation_platform/public/input_context.h"
 #include "components/segmentation_platform/public/types/processed_value.h"
+#include "components/visited_url_ranking/public/features.h"
 #include "components/visited_url_ranking/public/url_grouping/group_suggestions.h"
 #include "components/visited_url_ranking/public/url_visit.h"
 #include "components/visited_url_ranking/public/url_visit_schema.h"
@@ -274,12 +276,18 @@ std::optional<GroupSuggestions> GetAllGroupSuggestions(
 }  // namespace
 
 GroupingHeuristics::GroupingHeuristics() {
-  heuristics_.emplace(GroupSuggestion::SuggestionReason::kSwitchedBetween,
-                      std::make_unique<SwitchedBetweenHeuristic>());
-  heuristics_.emplace(GroupSuggestion::SuggestionReason::kSimilarSource,
-                      std::make_unique<SimilarSourceHeuristic>());
-  heuristics_.emplace(GroupSuggestion::SuggestionReason::kRecentlyOpened,
-                      std::make_unique<RecentlyOpenedHeuristic>());
+  if (features::kGroupSuggestionEnableRecentlyOpened.Get()) {
+    heuristics_.emplace(GroupSuggestion::SuggestionReason::kRecentlyOpened,
+                        std::make_unique<RecentlyOpenedHeuristic>());
+  }
+  if (features::kGroupSuggestionEnableSwitchBetween.Get()) {
+    heuristics_.emplace(GroupSuggestion::SuggestionReason::kSwitchedBetween,
+                        std::make_unique<SwitchedBetweenHeuristic>());
+  }
+  if (features::kGroupSuggestionEnableSimilarSource.Get()) {
+    heuristics_.emplace(GroupSuggestion::SuggestionReason::kSimilarSource,
+                        std::make_unique<SimilarSourceHeuristic>());
+  }
 }
 
 GroupingHeuristics::~GroupingHeuristics() = default;
@@ -310,6 +318,9 @@ void GroupingHeuristics::GetSuggestions(
     signals.push_back(AsInputContext(kSuggestionsPredictionSchema, candidate));
   }
   for (const auto type : heuristics_priority) {
+    if (!base::Contains(heuristics_, type)) {
+      continue;
+    }
     auto& heuristic = heuristics_[type];
     heuristic_results.emplace(heuristic->reason(), heuristic->Run(signals));
   }
