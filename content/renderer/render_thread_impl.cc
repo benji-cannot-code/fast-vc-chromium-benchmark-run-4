1e9697bf3a7e8bb8333c53be2b55d176ae9715fd
vikas soni <vikassoni@chromium.org>
2025-03-28 10:09:58 -0700

    [MapableSI] Remove RenderThreadImpl::GetGpuMemoryBufferManager().
    Remove RenderThreadImpl::GetGpuMemoryBufferManager() since it is no
    longer used.
    
    Bug: 394997675
    Change-Id: I19e236b693fed756eb01e9f72377bfb848e9267d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405184
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439503}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1328,10 +1328,6 @@ bool RenderThreadImpl::IsElasticOverscrollEnabled() {
   return is_elastic_overscroll_enabled_;
 }
 
-gpu::GpuMemoryBufferManager* RenderThreadImpl::GetGpuMemoryBufferManager() {
-  return gpu_->gpu_memory_buffer_manager();
-}
-
 blink::scheduler::WebThreadScheduler*
 RenderThreadImpl::GetWebMainThreadScheduler() {
   return main_thread_scheduler_.get();
