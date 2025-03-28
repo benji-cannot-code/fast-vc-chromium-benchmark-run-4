f56a371c05c59cd92d87bb78cef8d0c2b3a59d60
Timofey Chudakov <tchudakov@google.com>
2025-03-28 07:26:03 -0700

    [HaTS] Report UMA metric if the last survey trigger check was to recent.
    Before this CL, the
    Feedback.HappinessTrackingSurvey.ShouldShowSurveyReason was not reported
    if the survey triggering condition was less than a day ago. This CL
    added this metric for this case.
    
    Bug: 406712359
    Change-Id: Ie4a51b4f5033a7d3d56e40360404dd6450902314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6400798
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439391}

--- chrome/browser/ui/hats/hats_service_desktop.h
+++ chrome/browser/ui/hats/hats_service_desktop.h
@@ -106,7 +106,8 @@ class HatsServiceDesktop : public HatsService {
     kNoSurveyAlreadyInProgress = 14,
     kNoAnyLastSurveyTooRecent = 15,
     kNoRejectedByHatsService = 16,
-    kMaxValue = kNoRejectedByHatsService,
+    kNoLastSurveyCheckTooRecent = 17,
+    kMaxValue = kNoLastSurveyCheckTooRecent,
   };
 
   explicit HatsServiceDesktop(Profile* profile);
