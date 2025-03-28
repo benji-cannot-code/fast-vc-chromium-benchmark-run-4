22242dbf07139b0b58a4ca1fab7b7dfc62dd4e5b
Justin Lulejian <jlulejian@chromium.org>
2025-03-28 08:22:09 -0700

    [Extensions] Partially validate event_router.mojom methods for ext Id.
    Before this change extension Ids were sent as mojom strings and manually
    validated upon receipt.
    
    After this change all mojom methods now utilize
    extensions::mojom::ExtensionId and are automatically validated. Invalid
    extension IDs sent from the renderer will sever the mojom connection.
    
    I've also removed the non-redundant security tests that test for invalid
    extension IDs. They no longer reflect production behavior because the
    mojom testing message interceptor it uses bypasses the StructTraits
    specialization validation.
    
    Bug: 404568023
    Change-Id: I5d82674ab7d07c2c50587cfea96b4529570bcea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368779
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439430}

--- extensions/browser/event_router.cc
+++ extensions/browser/event_router.cc
@@ -470,8 +470,7 @@ void EventRouter::AddListenerForMainThread(
 
   const mojom::EventListenerOwner& listener_owner =
       *event_listener->listener_owner;
-  if (listener_owner.is_extension_id() &&
-      crx_file::id_util::IdIsValid(listener_owner.get_extension_id())) {
+  if (listener_owner.is_extension_id()) {
     AddEventListener(event_listener->event_name, process,
                      listener_owner.get_extension_id());
   } else if (listener_owner.is_listener_url() &&
@@ -492,8 +491,7 @@ void EventRouter::AddListenerForServiceWorker(
 
   const mojom::EventListenerOwner& listener_owner =
       *event_listener->listener_owner;
-  if (!listener_owner.is_extension_id() ||
-      !crx_file::id_util::IdIsValid(listener_owner.get_extension_id())) {
+  if (!listener_owner.is_extension_id()) {
     mojo::ReportBadMessage(kAddEventListenerWithInvalidExtensionID);
     return;
   }
@@ -510,14 +508,6 @@ void EventRouter::AddLazyListenerForMainThread(const ExtensionId& extension_id,
                                                const std::string& event_name) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  // TODO(https://crbug.com/394042459): Perform more IPC validation here.
-  if (!crx_file::id_util::IdIsValid(extension_id)) {
-    bad_message::ReceivedBadMessage(
-        GetRenderProcessHostForCurrentReceiver(),
-        bad_message::ER_INVALID_EXTENSION_ID_FOR_PROCESS);
-    return;
-  }
-
   std::unique_ptr<EventListener> listener = EventListener::CreateLazyListener(
       event_name, extension_id, browser_context_, false, GURL(), std::nullopt);
   AddLazyEventListenerImpl(std::move(listener), RegisteredEventType::kLazy);
@@ -527,13 +517,6 @@ void EventRouter::AddLazyListenerForServiceWorker(
     const ExtensionId& extension_id,
     const GURL& worker_scope_url,
     const std::string& event_name) {
-  if (!crx_file::id_util::IdIsValid(extension_id)) {
-    bad_message::ReceivedBadMessage(
-        GetRenderProcessHostForCurrentReceiver(),
-        bad_message::ER_INVALID_EXTENSION_ID_FOR_PROCESS);
-    return;
-  }
-
   // TODO(richardzh): Passing in browser context from the process.
   // Browser context is added to listener object in order to separate lazy
   // listeners for regular and incognito(split) context. The first step adds
@@ -588,8 +571,7 @@ void EventRouter::RemoveListenerForMainThread(
 
   const mojom::EventListenerOwner& listener_owner =
       *event_listener->listener_owner;
-  if (listener_owner.is_extension_id() &&
-      crx_file::id_util::IdIsValid(listener_owner.get_extension_id())) {
+  if (listener_owner.is_extension_id()) {
     RemoveEventListener(event_listener->event_name, process,
                         listener_owner.get_extension_id());
   } else if (listener_owner.is_listener_url() &&
@@ -610,8 +592,7 @@ void EventRouter::RemoveListenerForServiceWorker(
 
   const mojom::EventListenerOwner& listener_owner =
       *event_listener->listener_owner;
-  if (!listener_owner.is_extension_id() ||
-      !crx_file::id_util::IdIsValid(listener_owner.get_extension_id())) {
+  if (!listener_owner.is_extension_id()) {
     mojo::ReportBadMessage(kRemoveEventListenerWithInvalidExtensionID);
     return;
   }
@@ -629,14 +610,6 @@ void EventRouter::RemoveLazyListenerForMainThread(
     const std::string& event_name) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  // TODO(https://crbug.com/394042459): Perform more IPC validation here.
-  if (!crx_file::id_util::IdIsValid(extension_id)) {
-    bad_message::ReceivedBadMessage(
-        GetRenderProcessHostForCurrentReceiver(),
-        bad_message::ER_INVALID_EXTENSION_ID_FOR_PROCESS);
-    return;
-  }
-
   std::unique_ptr<EventListener> listener = EventListener::CreateLazyListener(
       event_name, extension_id, browser_context_, false, GURL(), std::nullopt);
   RemoveLazyEventListenerImpl(std::move(listener), RegisteredEventType::kLazy);
@@ -646,13 +619,6 @@ void EventRouter::RemoveLazyListenerForServiceWorker(
     const ExtensionId& extension_id,
     const GURL& worker_scope_url,
     const std::string& event_name) {
-  if (!crx_file::id_util::IdIsValid(extension_id)) {
-    bad_message::ReceivedBadMessage(
-        GetRenderProcessHostForCurrentReceiver(),
-        bad_message::ER_INVALID_EXTENSION_ID_FOR_PROCESS);
-    return;
-  }
-
   // TODO(richardzh): Passing in browser context from the process.
   // Browser context is added to listener object in order to separate lazy
   // listeners for regular and incognito(split) context. The first step adds
@@ -1057,7 +1023,9 @@ void EventRouter::DispatchEventToURL(const GURL& url,
 
 void EventRouter::DispatchEventWithLazyListener(const ExtensionId& extension_id,
                                                 std::unique_ptr<Event> event) {
-  DCHECK(!extension_id.empty());
+  // This method calls multiple mojom::EventRouter implementations. Ensure the
+  // id is valid before we proceed.
+  CHECK(crx_file::id_util::IdIsValid(extension_id));
   const Extension* extension = ExtensionRegistry::Get(browser_context_)
                                    ->enabled_extensions()
                                    .GetByID(extension_id);
