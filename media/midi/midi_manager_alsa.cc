cf3bc9c9b043bb8e36822cbbe9dbd7f9b058a794
Andrew Paseltiner <apaseltiner@chromium.org>
2025-03-28 05:18:44 -0700

    Remove unnecessary use of unique ptr with base::Value::Dict
    As well as some unnecessary string allocations.
    
    Bug: 40258809
    Change-Id: I91c966c236b85cfc0a94ef3ab73a97801c828b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6403273
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439325}

--- media/midi/midi_manager_alsa.cc
+++ media/midi/midi_manager_alsa.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "base/functional/bind.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
+#include "base/values.h"
 #include "build/build_config.h"
 #include "crypto/sha2.h"
 #include "media/midi/midi_service.h"
@@ -156,11 +158,11 @@ std::string GetVendor(udev_device* dev) {
   return vendor;
 }
 
-void SetStringIfNonEmpty(base::Value::Dict* value,
-                         const std::string& path,
-                         const std::string& in_value) {
+void SetStringIfNonEmpty(base::Value::Dict& value,
+                         std::string_view path,
+                         std::string in_value) {
   if (!in_value.empty())
-    value->Set(path, in_value);
+    value.Set(path, std::move(in_value));
 }
 
 }  // namespace
@@ -357,8 +359,8 @@ MidiManagerAlsa::MidiPort::MidiPort(const std::string& path,
 MidiManagerAlsa::MidiPort::~MidiPort() = default;
 
 // Note: keep synchronized with the MidiPort::Match* methods.
-std::unique_ptr<base::Value::Dict> MidiManagerAlsa::MidiPort::Value() const {
-  std::unique_ptr<base::Value::Dict> value(new base::Value::Dict);
+base::Value::Dict MidiManagerAlsa::MidiPort::Value() const {
+  base::Value::Dict value;
 
   std::string type;
   switch (type_) {
@@ -369,26 +371,26 @@ std::unique_ptr<base::Value::Dict> MidiManagerAlsa::MidiPort::Value() const {
       type = "output";
       break;
   }
-  value->Set("type", type);
-  SetStringIfNonEmpty(value.get(), "path", path_);
-  SetStringIfNonEmpty(value.get(), "clientName", client_name_);
-  SetStringIfNonEmpty(value.get(), "portName", port_name_);
-  value->Set("clientId", client_id_);
-  value->Set("portId", port_id_);
-  value->Set("midiDevice", midi_device_);
+  value.Set("type", std::move(type));
+  SetStringIfNonEmpty(value, "path", path_);
+  SetStringIfNonEmpty(value, "clientName", client_name_);
+  SetStringIfNonEmpty(value, "portName", port_name_);
+  value.Set("clientId", client_id_);
+  value.Set("portId", port_id_);
+  value.Set("midiDevice", midi_device_);
 
   // Flatten id fields.
-  SetStringIfNonEmpty(value.get(), "bus", id_.bus());
-  SetStringIfNonEmpty(value.get(), "vendorId", id_.vendor_id());
-  SetStringIfNonEmpty(value.get(), "modelId", id_.model_id());
-  SetStringIfNonEmpty(value.get(), "usbInterfaceNum", id_.usb_interface_num());
-  SetStringIfNonEmpty(value.get(), "serial", id_.serial());
+  SetStringIfNonEmpty(value, "bus", id_.bus());
+  SetStringIfNonEmpty(value, "vendorId", id_.vendor_id());
+  SetStringIfNonEmpty(value, "modelId", id_.model_id());
+  SetStringIfNonEmpty(value, "usbInterfaceNum", id_.usb_interface_num());
+  SetStringIfNonEmpty(value, "serial", id_.serial());
 
   return value;
 }
 
 std::string MidiManagerAlsa::MidiPort::JSONValue() const {
-  return base::WriteJson(*Value()).value_or(std::string());
+  return base::WriteJson(Value()).value_or(std::string());
 }
 
 // TODO(agoode): Do not use SHA256 here. Instead store a persistent
