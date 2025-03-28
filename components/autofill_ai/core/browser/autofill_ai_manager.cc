2631c38fba266bcfb1ba83fbb0f3131e008580b0
Jihad Hanna <jihadghanna@google.com>
2025-03-28 07:32:44 -0700

    Consistent naming in AutofillAiLogger
    This CL makes names in AutofillAiLogger follow the conventions used in
    AutofillAiManager (and the rest of Autofill)
    
    Bug: 406013367
    Change-Id: I82ba0d64680652a85a47efbb5bee9b8426a04444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396803
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439394}

--- components/autofill_ai/core/browser/autofill_ai_manager.cc
+++ components/autofill_ai/core/browser/autofill_ai_manager.cc
@@ -289,7 +289,7 @@ AutofillAiManager::~AutofillAiManager() = default;
 void AutofillAiManager::OnSuggestionsShown(const autofill::FormStructure& form,
                                            const autofill::AutofillField& field,
                                            ukm::SourceId ukm_source_id) {
-  logger_.OnFillingSuggestionsShown(form, field, ukm_source_id);
+  logger_.OnSuggestionsShown(form, field, ukm_source_id);
 }
 
 void AutofillAiManager::OnFormSeen(const FormStructure& form) {
@@ -323,7 +323,7 @@ void AutofillAiManager::OnEditedAutofilledField(
     const autofill::FormStructure& form,
     const autofill::AutofillField& field,
     ukm::SourceId ukm_source_id) {
-  logger_.OnDidCorrectFillingSuggestion(form, field, ukm_source_id);
+  logger_.OnEditedAutofilledField(form, field, ukm_source_id);
 }
 
 bool AutofillAiManager::OnFormSubmitted(const FormStructure& form,
