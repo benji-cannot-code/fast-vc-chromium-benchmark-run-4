47b91de1ad88b0cc8e7d30f668be96abb91c59e6
Vishwas Uppoor <vishwasuppoor@google.com>
2025-03-28 08:45:53 -0700

    [Pix][a11y] Fix account info truncated in FOP selector - 2
    In large font/display sizes, the bank account number gets truncated. The
    bank account number is important identifier info and should never be
    truncated.
    
    Split account summary into 3 separate text views, and truncate account
    type(middle view) when the account info doesn't fit in a single line.
    
    Before: https://screenshot.googleplex.com/8rQqKnr7JM6HHie.png
    After: https://screenshot.googleplex.com/ALriPg5xxYapXfY.png
    
    Fixed: 400998174, 405149949
    Change-Id: I6e26d95cdc9b1616413ea8d84cd0712e8bc31cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384257
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439451}

--- chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/BankAccountViewBinder.java
+++ chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/BankAccountViewBinder.java
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.facilitated_payments;
 
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.BANK_ACCOUNT_ICON;
-import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.BANK_ACCOUNT_SUMMARY;
+import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.BANK_ACCOUNT_NUMBER;
+import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.BANK_ACCOUNT_PAYMENT_RAIL;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.BANK_ACCOUNT_TRANSACTION_LIMIT;
+import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.BANK_ACCOUNT_TYPE;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.BANK_NAME;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.BankAccountProperties.ON_BANK_ACCOUNT_CLICK_ACTION;
 
@@ -35,9 +37,15 @@ class BankAccountViewBinder {
         if (propertyKey == BANK_NAME) {
             TextView bankName = view.findViewById(R.id.bank_name);
             bankName.setText(model.get(BANK_NAME));
-        } else if (propertyKey == BANK_ACCOUNT_SUMMARY) {
-            TextView bankAccountSummary = view.findViewById(R.id.bank_account_summary);
-            bankAccountSummary.setText(model.get(BANK_ACCOUNT_SUMMARY));
+        } else if (propertyKey == BANK_ACCOUNT_PAYMENT_RAIL) {
+            TextView bankAccountPaymentRail = view.findViewById(R.id.bank_account_payment_rail);
+            bankAccountPaymentRail.setText(model.get(BANK_ACCOUNT_PAYMENT_RAIL));
+        } else if (propertyKey == BANK_ACCOUNT_TYPE) {
+            TextView bankAccountType = view.findViewById(R.id.bank_account_type);
+            bankAccountType.setText(model.get(BANK_ACCOUNT_TYPE));
+        } else if (propertyKey == BANK_ACCOUNT_NUMBER) {
+            TextView bankAccountNumber = view.findViewById(R.id.bank_account_number);
+            bankAccountNumber.setText(model.get(BANK_ACCOUNT_NUMBER));
         } else if (propertyKey == BANK_ACCOUNT_TRANSACTION_LIMIT) {
             TextView transactionLimit = view.findViewById(R.id.bank_account_additional_info);
             transactionLimit.setText(model.get(BANK_ACCOUNT_TRANSACTION_LIMIT));
