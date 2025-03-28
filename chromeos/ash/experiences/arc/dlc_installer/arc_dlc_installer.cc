c705a7c28704bfdf34e4467da8615c857f10b45f
Ting-Hao Lin <tinghaolin@google.com>
2025-03-28 09:30:23 -0700

    Record DLC installation time via UMA
    Adds a histogram UMA metric to track the elapsed time of arcvm
    image installation from DLC. Installation time can vary from
    milliseconds to minutes, depending on internet speed. This
    metric will be used for future analysis.
    
    BUG=b:404279627
    TEST=Deploy Chrome and verify the histograms
    
    Change-Id: Iace6a7ecb19d0dddb6aca6f395511f7e8c7e6a6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6398126
    Reviewed-by: Lingyu Feng <lingyufeng@chromium.org>
    Commit-Queue: TingHao Lin <tinghaolin@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439470}

--- chromeos/ash/experiences/arc/dlc_installer/arc_dlc_installer.cc
+++ chromeos/ash/experiences/arc/dlc_installer/arc_dlc_installer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/time/time.h"
 #include "chromeos/ash/components/dbus/dlcservice/dlcservice_client.h"
 #include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
@@ -64,10 +65,12 @@ void ArcDlcInstaller::OnHardwareCheckComplete(
   dlcservice::InstallRequest install_request;
   install_request.set_id(kArcvmDlcId);
   MaybeShowDlcInstallNotification(NotificationType::kArcVmPreloadStarted);
+  base::TimeTicks start_installation_time = base::TimeTicks::Now();
   ash::DlcserviceClient::Get()->Install(
       install_request,
       base::BindOnce(&ArcDlcInstaller::OnDlcInstalled,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)),
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback),
+                     start_installation_time),
       base::DoNothing());
 }
 
@@ -119,6 +122,7 @@ void ArcDlcInstaller::OnPrimaryUserSessionStarted(const AccountId& account_id) {
 
 void ArcDlcInstaller::OnDlcInstalled(
     base::OnceCallback<void(bool)> callback,
+    base::TimeTicks start_installation_time,
     const ash::DlcserviceClient::InstallResult& install_result) {
   if (install_result.error != dlcservice::kErrorNone) {
     VLOG(1) << "Failed to install ARCVM DLC: " << install_result.error;
@@ -128,6 +132,9 @@ void ArcDlcInstaller::OnDlcInstalled(
     return;
   }
 
+  base::TimeDelta install_duration =
+      base::TimeTicks::Now() - start_installation_time;
+  base::UmaHistogramLongTimes("Arc.DlcInstaller.InstallTime", install_duration);
   base::UmaHistogramBoolean("Arc.DlcInstaller.Install", true);
   MaybeShowDlcInstallNotification(NotificationType::kArcVmPreloadSucceeded);
   OnPrepareArcDlc(std::move(callback), true);
