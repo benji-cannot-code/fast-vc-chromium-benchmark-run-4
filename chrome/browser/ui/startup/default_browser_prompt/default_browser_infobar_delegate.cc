dbf63acfbf9ea7f207aeca54e48745d22cd0b5f7
David Bienvenu <davidbienvenu@chromium.org>
2025-03-28 08:10:02 -0700

    win: add pin to taskbar to set as default info bar
    If Chrome is not pinned to the taskbar, and pinning is possible,
    when showing the set as default info bar, change info bar text to
    say "Set Chrome as default and pin to the taskbar", and if the
    user accepts, pin Chrome to the taskbar.
    
    This is guarded by the OfferPinToTaskbarWhenSettingDefault feature.
    
    Bug: 343734031
    Change-Id: I98ee9dfb067861671ae558c2d88e16c32523ada3
    Low-Coverage-Reason: HARD_TO_TEST
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882881
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439421}

--- chrome/browser/ui/startup/default_browser_prompt/default_browser_infobar_delegate.cc
+++ chrome/browser/ui/startup/default_browser_prompt/default_browser_infobar_delegate.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/types/pass_key.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/infobars/confirm_infobar_creator.h"
 #include "chrome/browser/profiles/profile.h"
@@ -32,19 +33,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "chrome/browser/win/taskbar_manager.h"
+#include "chrome/installer/util/install_util.h"
+#include "chrome/installer/util/shell_util.h"
+#endif
+
+namespace {
+
+#if BUILDFLAG(IS_WIN)
+void PinToTaskbarResult(bool pinned) {
+  // TODO(crbug.com/343734031): Emit a metric with the pin result. Initially,
+  // taskbar_manager.cc metrics will suffice, but taskbar_manager will most
+  // likely get used by other code.
+}
+#endif  // BUILDFLAG(IS_WIN)
+
+}  // namespace
+
 // static
 infobars::InfoBar* DefaultBrowserInfoBarDelegate::Create(
     infobars::ContentInfoBarManager* infobar_manager,
-    Profile* profile) {
+    Profile* profile,
+    bool can_pin_to_taskbar) {
   return infobar_manager->AddInfoBar(
       CreateConfirmInfoBar(std::make_unique<DefaultBrowserInfoBarDelegate>(
-          base::PassKey<DefaultBrowserInfoBarDelegate>(), profile)));
+          base::PassKey<DefaultBrowserInfoBarDelegate>(), profile,
+          can_pin_to_taskbar)));
 }
 
 DefaultBrowserInfoBarDelegate::DefaultBrowserInfoBarDelegate(
     base::PassKey<DefaultBrowserInfoBarDelegate>,
-    Profile* profile)
-    : profile_(profile) {}
+    Profile* profile,
+    bool can_pin_to_taskbar)
+    : profile_(profile), can_pin_to_taskbar_(can_pin_to_taskbar) {}
 
 DefaultBrowserInfoBarDelegate::~DefaultBrowserInfoBarDelegate() {
   if (!action_taken_) {
@@ -85,7 +107,9 @@ void DefaultBrowserInfoBarDelegate::InfoBarDismissed() {
 }
 
 std::u16string DefaultBrowserInfoBarDelegate::GetMessageText() const {
-  return l10n_util::GetStringUTF16(IDS_DEFAULT_BROWSER_INFOBAR_TEXT);
+  return l10n_util::GetStringUTF16(can_pin_to_taskbar_
+                                       ? IDS_DEFAULT_BROWSER_PIN_INFOBAR_TEXT
+                                       : IDS_DEFAULT_BROWSER_INFOBAR_TEXT);
 }
 
 int DefaultBrowserInfoBarDelegate::GetButtons() const {
@@ -115,6 +139,19 @@ bool DefaultBrowserInfoBarDelegate::Accept() {
   // finished.
   base::MakeRefCounted<shell_integration::DefaultBrowserWorker>()
       ->StartSetAsDefault(base::DoNothing());
+  if (can_pin_to_taskbar_) {
+#if BUILDFLAG(IS_WIN)
+    // Attempt the pin to taskbar in parallel with bringing up the Windows
+    // settings UI. Serializing the operations is an option, but since the user
+    // might not complete the first operation, serializing would probably make
+    // the second operation less likely to happen.
+    browser_util::PinAppToTaskbar(
+        ShellUtil::GetBrowserModelId(InstallUtil::IsPerUserInstall()),
+        base::BindOnce(&PinToTaskbarResult));
+#else
+    NOTREACHED();
+#endif  // BUILDFLAG(IS_WIN)
+  }
 
   return ConfirmInfoBarDelegate::Accept();
 }
