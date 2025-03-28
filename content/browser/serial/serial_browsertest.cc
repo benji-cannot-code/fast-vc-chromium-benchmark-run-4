16029a43119eaafeb9a3a1fbaf9d577f56df101f
Garfield Tan <xutan@google.com>
2025-03-28 08:26:19 -0700

    serial: Enable serial_browsertest on Android
    Bug: 396145433
    Test: content_browsertest -f SerialTest*
    Change-Id: I14414a3448baa4ce53d23afa0a1348d6652dfe72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405740
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Garfield Tan <xutan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439433}

--- content/browser/serial/serial_browsertest.cc
+++ content/browser/serial/serial_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/memory/raw_ptr.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
 #include "content/browser/serial/serial_test_utils.h"
 #include "content/public/browser/content_browser_client.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test_content_browser_client.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
+#include "device/base/features.h"
 #include "services/device/public/cpp/test/fake_serial_port_manager.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -48,6 +50,13 @@ class SerialTestShellContentBrowserClient
 
 class SerialTest : public ContentBrowserTest {
  public:
+#if BUILDFLAG(IS_ANDROID)
+  SerialTest() {
+    scoped_feature_list_.InitAndEnableFeature(
+        device::features::kBluetoothRfcommAndroid);
+  }
+#endif  // BUILDFLAG(IS_ANDROID)
+
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
@@ -66,6 +75,10 @@ class SerialTest : public ContentBrowserTest {
  private:
   std::unique_ptr<SerialTestShellContentBrowserClient> test_client_;
   device::FakeSerialPortManager port_manager_;
+
+#if BUILDFLAG(IS_ANDROID)
+  base::test::ScopedFeatureList scoped_feature_list_;
+#endif  // BUILDFLAG(IS_ANDROID)
 };
 
 }  // namespace
