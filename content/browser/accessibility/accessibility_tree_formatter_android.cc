dff10a12d6959c4e3c69c2f9d0ecf2d0b46e2d43
Wenyu Zhang <zhangwenyu@google.com>
2025-03-27 20:54:07 -0700

    a11y: Provide selectionMode info for CollectionInfo
    Currently when the user unselects an item in a listbox, there is no
    spoken feedback. This CL provides the selectionMode for CollectionInfo.
    This allows TalkBack to understand if this item is "selectable" and
    announces "not selected" state when the user unselects an item.
    
    Unlike "isCheckable", there is no Android API for "isSelectable".
    Currently, TalkBack only announces "selected" when a node is selected,
    and do not announce "not selected" when a node is unselected.
    Otherwise, this state will be announced on nearly every node.
    
    Before fix: http://go/scrcast/NjYxMDU2Mzg3NzU2ODUxMnxjNDIxNDNmZi0wOQ
    After fix: http://go/scrcast/NDgzOTYwNjY1Nzc0NDg5NnwwY2Q0MzE4My04Zg
    
    Bug: 391469359
    Change-Id: I4ccd89e8ed0651e38cbdbd2beadc7d6335bb8a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6395116
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439203}

--- content/browser/accessibility/accessibility_tree_formatter_android.cc
+++ content/browser/accessibility/accessibility_tree_formatter_android.cc
@@ -79,6 +79,7 @@ const char* const INT_ATTRIBUTES[] = {
     "range_current_value",
     "text_change_added_count",
     "text_change_removed_count",
+    "selection_mode",
 };
 
 const char* const ACTION_ATTRIBUTES[] = {
@@ -217,6 +218,7 @@ void AccessibilityTreeFormatterAndroid::AddProperties(
   dict->Set("text_change_added_count", android_node->GetTextChangeAddedCount());
   dict->Set("text_change_removed_count",
             android_node->GetTextChangeRemovedCount());
+  dict->Set("selection_mode", android_node->GetSelectionMode());
 
   // Actions.
   dict->Set("action_expand", android_node->IsCollapsed());
--- content/browser/accessibility/browser_accessibility_android.h
+++ content/browser/accessibility/browser_accessibility_android.h
@@ -173,6 +173,7 @@ class CONTENT_EXPORT BrowserAccessibilityAndroid
   int GetItemIndex() const;
   int GetItemCount() const;
   int GetSelectedItemCount() const;
+  int GetSelectionMode() const;
 
   bool CanScrollForward() const;
   bool CanScrollBackward() const;
