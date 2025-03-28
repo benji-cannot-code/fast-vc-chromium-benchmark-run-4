65344f38250eeed054c50ad94c5f5e7287642ac7
Jenna Himawan <jhimawan@google.com>
2025-03-28 09:07:40 -0700

    [Keyboard A11y] Add bookmark bar methods for set/get focus
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Bug: 360423850, 399498687
    Change-Id: Idb81b4c1be26282c90ed6ed614bcff0a4c8a53a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6393416
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439461}

--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.bookmarks.bar;
 
+import static org.chromium.ui.accessibility.KeyboardFocusUtil.setFocus;
+import static org.chromium.ui.accessibility.KeyboardFocusUtil.setFocusOnFirstFocusableDescendant;
+
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -120,5 +123,23 @@ public class BookmarkBarCoordinator {
         return (BookmarkBarButton)
                 LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.bookmark_bar_button, parent, false);
+    }
+
+    /** Requests focus within the bookmark bar. */
+    public void requestFocus() {
+        if (setFocusOnFirstFocusableDescendant(
+                mView.findViewById(R.id.bookmark_bar_items_container))) {
+            // If we set focus on a bookmark in the RecyclerView of user bookmarks, we are done.
+            return;
+        }
+        // Otherwise (there were no user bookmarks), focus on the all bookmarks button at the end.
+        setFocus(mView.findViewById(R.id.bookmark_bar_all_bookmarks_button));
+    }
+
+    /**
+     * @return Whether keyboard focus is within this view.
+     */
+    public boolean hasKeyboardFocus() {
+        return mView.getFocusedChild() != null;
     }
 }
