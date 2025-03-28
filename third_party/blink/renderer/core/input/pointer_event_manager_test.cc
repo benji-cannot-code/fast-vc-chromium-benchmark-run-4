eebde878d34c213cdd5e1aefab145c861b0f0799
Nate Chapin <japhet@chromium.org>
2025-03-27 18:25:04 -0700

    Replace IGNORE_EXCEPTION macro with a helper class wrapper
    Change-Id: Iaccf39112999e0e823fc46873c6818a91ad46ff3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001310
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439150}

--- third_party/blink/renderer/core/input/pointer_event_manager_test.cc
+++ third_party/blink/renderer/core/input/pointer_event_manager_test.cc
@@ -165,7 +165,7 @@ TEST_F(PointerEventManagerTest, HasPointerCapture) {
       GetDocument().body()->hasPointerCapture(PointerEventFactory::kMouseId));
 
   GetDocument().body()->setPointerCapture(PointerEventFactory::kMouseId,
-                                          IGNORE_EXCEPTION);
+                                          IGNORE_EXCEPTION_FOR_TESTING);
   ASSERT_TRUE(
       GetDocument().body()->hasPointerCapture(PointerEventFactory::kMouseId));
 
@@ -178,7 +178,7 @@ TEST_F(PointerEventManagerTest, HasPointerCapture) {
       GetDocument().body()->hasPointerCapture(PointerEventFactory::kMouseId));
 
   GetDocument().body()->releasePointerCapture(PointerEventFactory::kMouseId,
-                                              IGNORE_EXCEPTION);
+                                              IGNORE_EXCEPTION_FOR_TESTING);
   ASSERT_FALSE(
       GetDocument().body()->hasPointerCapture(PointerEventFactory::kMouseId));
 }
