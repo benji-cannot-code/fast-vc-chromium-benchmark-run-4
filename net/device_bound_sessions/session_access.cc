b6ece909430394fdfebdad89bf17d9b8d67a4330
Daniel Rubery <drubery@chromium.org>
2025-03-28 08:05:06 -0700

    Provide cookie names on termination
    Account reconciliation (see dependent CL) wants to know which cookies
    the DBSC session has bound. These should be cleared when the session is
    terminated so that the sign-in code can keep the browser login state and
    cookie jar login state in sync.
    
    Bug: 405437918
    Change-Id: If50a681ce3a094994b779412160a684271b3f6e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384676
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439417}

--- net/device_bound_sessions/session_access.cc
+++ net/device_bound_sessions/session_access.cc
@@ -5,7 +5,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/device_bound_sessions/session_access.h"
 
+#include "base/check_op.h"
+
 namespace net::device_bound_sessions {
+
+SessionAccess::SessionAccess() = default;
+SessionAccess::SessionAccess(AccessType access, SessionKey key)
+    : access_type(access), session_key(key) {}
+SessionAccess::SessionAccess(AccessType access,
+                             SessionKey key,
+                             const std::vector<std::string>& cookies)
+    : access_type(access), session_key(key), cookies(cookies) {
+  CHECK_EQ(access, AccessType::kTermination);
+}
+SessionAccess::~SessionAccess() = default;
+SessionAccess::SessionAccess(const SessionAccess&) = default;
+SessionAccess& SessionAccess::operator=(const SessionAccess&) = default;
+SessionAccess::SessionAccess(SessionAccess&&) noexcept = default;
+SessionAccess& SessionAccess::operator=(SessionAccess&&) noexcept = default;
 
 bool SessionAccess::operator==(const SessionAccess& other) const = default;
 
--- services/network/public/cpp/device_bound_sessions_mojom_traits.cc
+++ services/network/public/cpp/device_bound_sessions_mojom_traits.cc
@@ -38,6 +38,10 @@ bool StructTraits<network::mojom::DeviceBoundSessionAccessDataView,
     return false;
   }
 
+  if (!data.ReadCookies(&out->cookies)) {
+    return false;
+  }
+
   return true;
 }
 
