6225cb7bfa4e6165bc86e352440a9813024faffd
Zonghan Xu <xzonghan@google.com>
2025-03-28 14:39:22 -0700

    [Signals Report] Solve potential EV build issue on Chrome OS
    The migrated "Linux" functions seem to actually support both ChromeOS and Linux previously, and the migration for OsSignalsCollector only created the new util functions for Linux.
    
    Making platform gating more accurate to prevent any issue for EV on ChromeOS
    
    Change-Id: I9b878846413d817b1d687454ad46aaea08ffcb7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405454
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Auto-Submit: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439718}

---
+++ components/device_signals/core/browser/chromeos/browser_utils_chromeos.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/device_signals/core/browser/browser_utils.h"
+
+#include <string>
+
+#include "net/base/network_interfaces.h"
+
+namespace device_signals {
+
+std::string GetHostName() {
+  return net::GetHostName();
+}
+
+}  // namespace device_signals
