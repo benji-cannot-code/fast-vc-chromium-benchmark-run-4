12e8ea7b0e076c0e7606da39695527157a4e599e
Ananya <ananyaseelam@google.com>
2025-03-28 13:55:26 -0700

    [glic metrics] Replace settings toggle histogram with user action
    This CL adds user action metrics for when BB is disabled or enabled by
    toggling from glic settings. This is done by replacing  the existing
    `Glic.OsEntrypoint.Settings.Toggle` histogram with two user actions,
    `Glic.OsEntrypoint.Settings.ToggleEnabled` and
    `Glic.OsEntrypoint.Settings.ToggleDisabled`.
    
    Bug: 391417447,406788386
    Change-Id: Ic1dd1d8dfb148fd2c54e49c9e8de4473a71e479b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6403533
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439687}

--- chrome/test/data/webui/settings/glic_page_focus_test.ts
+++ chrome/test/data/webui/settings/glic_page_focus_test.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://settings/settings.js';
 
 import type {CrShortcutInputElement} from 'chrome://settings/lazy_load.js';
-import type {SettingsGlicPageElement, SettingsPrefsElement} from 'chrome://settings/settings.js';
+import type {SettingsGlicPageElement, SettingsPrefsElement, SettingsToggleButtonElement} from 'chrome://settings/settings.js';
 import {CrSettingsPrefs, GlicBrowserProxyImpl, loadTimeData, MetricsBrowserProxyImpl, resetRouterForTesting, Router, routes, SettingsGlicPageFeaturePrefName as PrefName} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {keyDownOn} from 'chrome://webui-test/keyboard_mock_interactions.js';
@@ -225,6 +225,36 @@ suite('GlicPageFocusTest', function() {
       userActions = await metricsBrowserProxy.getArgs('recordAction');
       assertEquals(1, userActions.length);
       verifyUserAction('GlicOsEntrypoint.Settings.ShortcutEdited');
+    });
+
+    test('toggle OS entrypoint', async () => {
+      // Assert no actions are logged upon load.
+      userActions = await metricsBrowserProxy.getArgs('recordAction');
+      assertEquals(0, userActions.length);
+
+      // Arrange.
+      const launcherToggle = $<SettingsToggleButtonElement>('launcherToggle');
+      assertTrue(!!launcherToggle);
+      assertTrue(launcherToggle.checked);
+
+      // Act.
+      launcherToggle.click();
+      await flushTasks();
+
+      // Assert.
+      assertTrue(!launcherToggle.checked);
+      userActions = await metricsBrowserProxy.getArgs('recordAction');
+      assertEquals(1, userActions.length);
+      verifyUserAction('Glic.OsEntrypoint.Settings.Toggle.Disabled');
+
+      // Act.
+      launcherToggle.click();
+      await flushTasks();
+
+      // Assert.
+      userActions = await metricsBrowserProxy.getArgs('recordAction');
+      assertEquals(2, userActions.length);
+      verifyUserAction('Glic.OsEntrypoint.Settings.Toggle.Enabled');
     });
   });
 });
