10b5e39789c30ce5850f020aaa7235dbada78c5e
Steven Holte <holte@google.com>
2025-03-28 12:47:30 -0700

    Simplify ModelValidation flow.
    Since the service now supports multiple sessions, a session disconnect
    should only happen on a service crash, not because another session
    started.
    
    Change-Id: Idcc7be39e3a1ee982cc44045f986253b3999adcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405709
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439631}

--- components/optimization_guide/core/model_execution/on_device_model_validator.cc
+++ components/optimization_guide/core/model_execution/on_device_model_validator.cc
@@ -19,7 +19,7 @@ OnDeviceModelValidator::OnDeviceModelValidator(
   // base::Unretained is safe since `this` owns the session.
   session_.set_disconnect_handler(base::BindOnce(
       &OnDeviceModelValidator::FinishValidation, base::Unretained(this),
-      OnDeviceModelValidationResult::kInterrupted));
+      OnDeviceModelValidationResult::kServiceCrash));
   ValidateNextPrompt();
 }
 
@@ -37,7 +37,7 @@ void OnDeviceModelValidator::ValidateNextPrompt() {
   // base::Unretained is safe since `this` owns the session.
   active_session_.set_disconnect_handler(base::BindOnce(
       &OnDeviceModelValidator::FinishValidation, base::Unretained(this),
-      OnDeviceModelValidationResult::kInterrupted));
+      OnDeviceModelValidationResult::kServiceCrash));
 
   current_response_ = "";
   auto append_options = on_device_model::mojom::AppendOptions::New();
@@ -74,6 +74,9 @@ void OnDeviceModelValidator::OnComplete(
 
 void OnDeviceModelValidator::FinishValidation(
     OnDeviceModelValidationResult result) {
+  // Reset sessions to avoid further callbacks.
+  active_session_.reset();
+  session_.reset();
   std::move(finish_callback_).Run(result);
 }
 
