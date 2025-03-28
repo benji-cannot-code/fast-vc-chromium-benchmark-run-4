b541ce4df61cb86ba712279892c7f629ebf732bc
Evelynn Kaplan <evelynn.kaplan@microsoft.com>
2025-03-28 13:26:09 -0700

    [AriaNotify] Prepare for Origin Trial
    This CL adds code that will be needed when the time comes to enable an
    Origin Trial for the new AriaNotify API. This code does not enable the
    OT, as it is missing some config pieces from
    `runtime_enabled_features.json5`.
    
    More on AriaNotify in the explainer:
    https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/Accessibility/AriaNotify/explainer.md#screen-reader-customizations-for-user-preference
    
    Bug: 326277796
    Change-Id: I6cbab61d8af6993b938773b8047443a22a6a81da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6407353
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Evelynn Kaplan <evelynn.kaplan@microsoft.com>
    Reviewed-by: Mike Jackson <mjackson@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1439666}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -6917,7 +6917,7 @@ void Document::TrustTokenQueryAnswererConnectionError() {
 
 void Document::ariaNotify(const String& announcement,
                           const AriaNotificationOptions* options) {
-  DCHECK(RuntimeEnabledFeatures::AriaNotifyEnabled());
+  DCHECK(RuntimeEnabledFeatures::AriaNotifyEnabled(GetExecutionContext()));
 
   if (auto* cache = ExistingAXObjectCache()) {
     cache->HandleAriaNotification(this, announcement, options);
--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2940,7 +2940,7 @@ String Element::ComputedNameNoLifecycleUpdate() {
 
 void Element::ariaNotify(const String& announcement,
                          const AriaNotificationOptions* options) {
-  DCHECK(RuntimeEnabledFeatures::AriaNotifyEnabled());
+  DCHECK(RuntimeEnabledFeatures::AriaNotifyEnabled(GetExecutionContext()));
 
   if (auto* cache = GetDocument().ExistingAXObjectCache()) {
     cache->HandleAriaNotification(this, announcement, options);
