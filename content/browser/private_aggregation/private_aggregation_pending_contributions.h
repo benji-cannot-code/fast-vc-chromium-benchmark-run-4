9fd8d0164599add0efca24222f29315b78741f87
Alex Turner <alexmt@chromium.org>
2025-03-28 15:27:25 -0700

    Support aggregate error reporting in PrivateAggregationBudgeter
    Adds support for the kPrivateAggregationApiErrorReporting feature*,
    including the new flow involving two calls to the interface: first,
    `InspectBudgetAndLock()`; and then, `ConsumeBudget()`. The old flow
    (when the feature is disabled) is also maintained for now.
    
    When the feature is disabled (which it is by default), this cl should
    have no effect. These new calls are also not yet integrated with the
    manager; this will be done in a follow-up cl.
    
    *This feature is described further here:
    https://github.com/patcg-individual-drafts/private-aggregation-api/blob/main/error_reporting.md
    It was specified here:
    https://github.com/patcg-individual-drafts/private-aggregation-api/pull/172
    
    Bug: 381788013
    Change-Id: Iccd452826b887e1de39a33ab5344de2de6774978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304832
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439738}

--- content/browser/private_aggregation/private_aggregation_pending_contributions.h
+++ content/browser/private_aggregation/private_aggregation_pending_contributions.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/numerics/safe_conversions.h"
+#include "content/browser/private_aggregation/private_aggregation_budgeter.h"
 #include "content/common/content_export.h"
 #include "third_party/abseil-cpp/absl/numeric/int128.h"
 #include "third_party/blink/public/mojom/aggregation_service/aggregatable_report.mojom.h"
@@ -59,23 +60,9 @@ class CONTENT_EXPORT PrivateAggregationPendingContributions {
 
   class Wrapper;
 
-  // For a single contribution, whether the budgeter approved its budget usage
-  // (including provisionally) or denied it.
-  enum BudgeterResult {
-    kApproved,
-    kDenied,
-  };
-
-  // Note that if the limit has been reached and there is no space for this
-  // report, the entire report will be dropped with a fatal error.
-  enum PendingReportLimitResult {
-    // Indicates the limit has not been reached.
-    kNotAtLimit,
-
-    // Indicates the limit has been reached with this report, i.e. the report
-    // can still be processed.
-    kAtLimit
-  };
+  using BudgeterResult = PrivateAggregationBudgeter::ResultForContribution;
+  using PendingReportLimitResult =
+      PrivateAggregationBudgeter::PendingReportLimitResult;
 
   // Indicates the reason for the PrivateAggregationHost mojo pipe closing.
   // TODO(crbug.com/381788013): Consider moving this enum to
