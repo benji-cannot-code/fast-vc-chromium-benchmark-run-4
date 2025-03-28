7a4d4c1c2c15181895af40c92e5e3dac4ac4d1cf
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2025-03-28 16:16:19 -0700

    [gap-decorations] Paint gap decorations for flex
    This CL implements basic painting for Gap Decorations in flex.
    
    In order to implement the rules, we had to add some data to the
    `GapIntersections` as well as the `GapGeometry`. For flex, we use
    `is_blocked_before_/after` to indicate whether the intersection borders
    the edge of the container (is blocked by the edge).
    
    This CL adds the `ContainerType` enum, and adds a member of this type to
    `GapGeometry` which dictates for what type of container that
    `GapGeometry` object is (i.e. flex).
    
    We also add `inline_gap_size_`. In flex it refers to the gap between
    flex items, and in grid it refers to the column gutter size.
    
    For `block_gap_size_` in flex, it refers to the gap between flex lines
    and in grid it refers to the row gutter size.
    
    Bug: 357648037
    Change-Id: Ifc65ba53049bff52388f57afda2471e3dce6adb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367772
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1439763}

--- third_party/blink/renderer/core/layout/grid/grid_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/grid/grid_layout_algorithm.cc
@@ -3311,7 +3311,8 @@ void GridLayoutAlgorithm::PlaceGridItems(
   GapGeometry* gap_geometry = nullptr;
 
   if (RuntimeEnabledFeatures::CSSGapDecorationEnabled()) {
-    gap_geometry = MakeGarbageCollected<GapGeometry>();
+    gap_geometry =
+        MakeGarbageCollected<GapGeometry>(GapGeometry::ContainerType::kGrid);
     BuildGapIntersectionPoints(layout_data, gap_geometry);
   }
 
@@ -3965,6 +3966,8 @@ void GridLayoutAlgorithm::BuildGapIntersectionPoints(
 
   gap_geometry->SetGapIntersections(kForColumns, std::move(columns));
   gap_geometry->SetGapIntersections(kForRows, std::move(rows));
+  gap_geometry->SetBlockGapSize(layout_data.Rows().GutterSize());
+  gap_geometry->SetInlineGapSize(layout_data.Columns().GutterSize());
 }
 
 void GridLayoutAlgorithm::MarkBlockedStatusForGapIntersections(
--- third_party/blink/renderer/core/layout/grid/grid_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/grid/grid_layout_algorithm_test.cc
@@ -263,7 +263,8 @@ TEST_F(GridLayoutAlgorithmTest, GridLayoutAlgorithmGapGeometry) {
       CalculateInitialFragmentGeometry(space, node, /* break_token */ nullptr);
   GridLayoutAlgorithm algorithm({node, fragment_geometry, space});
 
-  auto* gap_geometry = MakeGarbageCollected<GapGeometry>();
+  auto* gap_geometry =
+      MakeGarbageCollected<GapGeometry>(GapGeometry::ContainerType::kGrid);
   BuildGridGeometry(algorithm, gap_geometry);
 
   Vector<GapIntersectionList> expected_column_intersections = {
@@ -340,7 +341,8 @@ TEST_F(GridLayoutAlgorithmTest, GapIntersectionsForGridWithSpanners) {
       CalculateInitialFragmentGeometry(space, node, /* break_token */ nullptr);
   GridLayoutAlgorithm algorithm({node, fragment_geometry, space});
 
-  auto* gap_geometry = MakeGarbageCollected<GapGeometry>();
+  auto* gap_geometry =
+      MakeGarbageCollected<GapGeometry>(GapGeometry::ContainerType::kGrid);
   BuildGridGeometry(algorithm, gap_geometry);
 
   Vector<GapIntersectionList> expected_column_intersections = {
