35278e6cb8a9fc50061b9be5a886d61d096b36d5
Taiyo Mizuhashi <taiyo@chromium.org>
2025-03-28 11:29:03 -0700

    Prefetch: Add an emergency kill switch for the browsing data removal
    Bug: 40262310, 404703517
    Change-Id: I4b64ca940a3438eeabd8e54b047815b062a1fd80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6406438
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439568}

--- content/browser/browsing_data/browsing_data_remover_impl.cc
+++ content/browser/browsing_data/browsing_data_remover_impl.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/btm/btm_service_impl.h"
 #include "content/browser/btm/btm_utils.h"
 #include "content/browser/fingerprinting_protection/canvas_noise_token_data.h"
+#include "content/browser/preloading/prefetch/prefetch_features.h"
 #include "content/browser/preloading/prefetch/prefetch_service.h"
 #include "content/browser/preloading/prefetch/prefetch_status.h"
 #include "content/browser/preloading/prerender/prerender_host_registry.h"
@@ -615,11 +616,14 @@ void BrowsingDataRemoverImpl::RemoveImpl(
         // cache.
         web_contents->GetPrerenderHostRegistry()->CancelAllHosts(
             PrerenderFinalStatus::kBrowsingDataRemoved);
-        PrefetchService* prefetch_service =
-            BrowserContextImpl::From(browser_context_)->GetPrefetchService();
-        prefetch_service->EvictPrefetchesForBrowsingDataRemoval(
-            storage_key_filter,
-            PrefetchStatus::kPrefetchEvictedAfterBrowsingDataRemoved);
+        if (base::FeatureList::IsEnabled(
+                features::kPrefetchBrowsingDataRemoval)) {
+          PrefetchService* prefetch_service =
+              BrowserContextImpl::From(browser_context_)->GetPrefetchService();
+          prefetch_service->EvictPrefetchesForBrowsingDataRemoval(
+              storage_key_filter,
+              PrefetchStatus::kPrefetchEvictedAfterBrowsingDataRemoved);
+        }
       }
     }
   }
--- content/browser/browsing_data/browsing_data_remover_impl_browsertest.cc
+++ content/browser/browsing_data/browsing_data_remover_impl_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browsing_data/shared_storage_clear_site_data_tester.h"
 #include "content/browser/fingerprinting_protection/canvas_noise_token_data.h"
 #include "content/browser/preloading/prefetch/prefetch_document_manager.h"
+#include "content/browser/preloading/prefetch/prefetch_features.h"
 #include "content/browser/preloading/prefetch/prefetch_status.h"
 #include "content/browser/preloading/prerender/prerender_final_status.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
@@ -1052,7 +1053,9 @@ IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverImplPrerenderingBrowserTest,
 class BrowsingDataRemoverImplPrefetchBrowserTest
     : public BrowsingDataRemoverImplBrowserTest {
  public:
-  BrowsingDataRemoverImplPrefetchBrowserTest() = default;
+  BrowsingDataRemoverImplPrefetchBrowserTest() {
+    feature_list_.InitAndEnableFeature(features::kPrefetchBrowsingDataRemoval);
+  }
 
   void StartPrefetch(const GURL& url, Shell* shell) {
     auto* prefetch_document_manager =
@@ -1072,6 +1075,9 @@ class BrowsingDataRemoverImplPrefetchBrowserTest
   }
 
   ~BrowsingDataRemoverImplPrefetchBrowserTest() override = default;
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverImplPrefetchBrowserTest,
