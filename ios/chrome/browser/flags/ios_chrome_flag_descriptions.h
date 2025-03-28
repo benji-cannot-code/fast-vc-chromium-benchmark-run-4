99c91ead990a5253623f3efe50014efa7a4ae5db
“Prasanaa” <prasanaa@google.com>
2025-03-28 09:32:14 -0700

    [iOS] Add flag for adding Explain Gemini in the EditMenu
    This CL adds flag for adding Explain Gemini in the EditMenu.
    
    Fixed: 406560900
    Change-Id: Ie80729b976696cd193cc68bc46e8b9aa56cbfc3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397162
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439471}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -796,6 +796,9 @@ extern const char kEnableLensViewFinderUnifiedExperienceDescription[];
 extern const char kEnableLensContextMenuUnifiedExperienceName[];
 extern const char kEnableLensContextMenuUnifiedExperienceDescription[];
 
+extern const char kExplainGeminiEditMenuName[];
+extern const char kExplainGeminiEditMenuDescription[];
+
 extern const char kTabGridNewTransitionsName[];
 extern const char kTabGridNewTransitionsDescription[];
 
--- ios/chrome/browser/intelligence/features/features.mm
+++ ios/chrome/browser/intelligence/features/features.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/intelligence/features/features.h"
 
+#import "base/metrics/field_trial_params.h"
+
 BASE_FEATURE(kEnhancedCalendar,
              "EnhancedCalendar",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -20,3 +22,21 @@ BASE_FEATURE(kPageActionMenu,
 bool IsPageActionMenuEnabled() {
   return base::FeatureList::IsEnabled(kPageActionMenu);
 }
+
+const char kExplainGeminiEditMenuParams[] = "ExplainGeminiEditMenuPosition";
+
+ExplainGeminiEditMenuPosition ExplainGeminiEditMenuPositionParam() {
+  int param = base::GetFieldTrialParamByFeatureAsInt(
+      kExplainGeminiEditMenu, kExplainGeminiEditMenuParams, 0);
+  if (param == 1) {
+    return ExplainGeminiEditMenuPosition::kBeforeSearch;
+  }
+  if (param == 2) {
+    return ExplainGeminiEditMenuPosition::kAfterSearch;
+  }
+  return ExplainGeminiEditMenuPosition::kDisabled;
+}
+
+BASE_FEATURE(kExplainGeminiEditMenu,
+             "ExplainGeminiEditMenu",
+             base::FEATURE_DISABLED_BY_DEFAULT);
