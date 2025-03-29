33f24acd2e45c74ce13732225928762580b4147e
Carlos Knippschild <carlosk@chromium.org>
2025-03-28 22:15:44 -0700

    Deprecate and "unimplement" panel attach/detach API functionality
    Update the Glic API to the panel becoming detached-only. This change
    updates the API definition to comply with that and "unimplements" the
    attachment related functions when the detached-only feature flag is
    enabled.
    
    The test client was updated to better represent unavailable functionality, and a breaking test parameterized to only be executed
    if attachment is available.
    
    BYPASS_GLIC_API_COMPATIBILITY_CHECK=PanelOpeningData is not yet being used by the web client
    
    Bug: 406281026, 406528268
    Change-Id: Ifff997a77c2022471cf246c72447f3372631f4c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6406113
    Reviewed-by: Nick Birnie <birnie@google.com>
    Auto-Submit: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439847}

--- chrome/test/data/webui/glic/test_client/test_client.ts
+++ chrome/test/data/webui/glic/test_client/test_client.ts
@@ -113,6 +113,8 @@ interface PageElementTypes {
   actionStatus: HTMLSpanElement;
   actionUpdatedContextResult: HTMLSpanElement;
   actionUpdatedScreenshotImg: HTMLImageElement;
+  macOsPermissionsFieldset: HTMLFieldSetElement;
+  attachmentControlsFieldset: HTMLFieldSetElement;
 }
 
 const $: PageElementTypes = new Proxy({}, {
@@ -153,7 +155,15 @@ class WebClient implements GlicWebClient {
     logMessage('initialize called');
     $.pageHeader!.classList.add('connected');
 
-
+    // Disable sections with unavailable functionality.
+    if (this.browser.openOsPermissionSettingsMenu === undefined) {
+      logMessage('OS permissions are disabled');
+      $.macOsPermissionsFieldset.disabled = true;
+    }
+    if (this.browser.attachPanel === undefined) {
+      logMessage('Attachment controls are disabled (detached-only mode)');
+      $.attachmentControlsFieldset.disabled = true;
+    }
 
     const ver = await browser.getChromeVersion();
     logMessage(`Chrome version: ${JSON.stringify(ver)}`);
