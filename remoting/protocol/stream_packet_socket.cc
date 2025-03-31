cb9e72ce6694f601a6369a353f0a7f8a6239aead
Evan Shrubsole <eshr@google.com>
2025-03-31 08:06:00 -0700

    Include webrtc time_utils in stream_packet_socket.cc
    Bug: webrtc:42232595
    Change-Id: I9a4053338ba2c8364cd2285e53688e0a39a9b0e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415654
    Auto-Submit: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440285}

--- remoting/protocol/stream_packet_socket.cc
+++ remoting/protocol/stream_packet_socket.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/tcp_client_socket.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "remoting/protocol/stun_tcp_packet_processor.h"
+#include "third_party/webrtc/rtc_base/time_utils.h"
 
 namespace remoting::protocol {
 
