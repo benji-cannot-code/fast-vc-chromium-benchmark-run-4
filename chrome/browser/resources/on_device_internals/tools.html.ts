bf3907b946497594c7589f03ae8bc76f0a899842
dpapad <dpapad@chromium.org>
2025-03-31 16:42:08 -0700

    WebUI: c/b/r/on_device_internals post Lit migration cleanup.
    Removing Polymer dependency, fixing new type errors and checking in the
    remaining html.ts files.
    
    Bug: 40943652
    Change-Id: I100e3181fbc60943f410d1b0062cd88862fc859c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418684
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440630}

--- chrome/browser/resources/on_device_internals/tools.html.ts
+++ chrome/browser/resources/on_device_internals/tools.html.ts
@@ -1,4 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ToolsElement} from './tools.js';
+
+export function getHtml(this: ToolsElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
 <div class="performance-class">
   Device performance class: <strong>${this.performanceClassText_}</strong>
 </div>
@@ -119,3 +130,6 @@ ${this.responses_.map(item => html`
     <div class="${item.responseClass}">${item.response}</div>
   </div>
 `)}
+<!--_html_template_end_-->`;
+  // clang-format on
+}
