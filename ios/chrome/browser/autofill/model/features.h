5a5c3262545106a6cee5560397388af6f590d4cb
Vincent Boisselle <vincb@google.com>
2025-03-31 12:45:07 -0700

    [ios] Option to dedupe concurrent autofill suggestions requests when stateless
    The bottom sheets still get their provider from the
    FormSuggestionController directly so we should keep the logic to
    set the `_provider` in the FormSuggestionController the same. At
    least form now until we manage to not have anyone needing to get a
    provider via the FormSuggestionController interface.
    
    R=noemies@google.com
    
    Bug: 404982498, 407553910
    Change-Id: I5e54d32e63bf3b04044a07d4781577d7965a78aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6417223
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440490}

--- ios/chrome/browser/autofill/model/features.h
+++ ios/chrome/browser/autofill/model/features.h
@@ -11,6 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Enables the stateless form suggestion controller.
 BASE_DECLARE_FEATURE(kStatelessFormSuggestionController);
 
+// Enables deduping concurrent requests even when using the stateless
+// FormSuggestionController. This feature is only effective if
+// `kStatelessFormSuggestionController` is enabled.
+BASE_DECLARE_FEATURE(kStatelessFormSuggestionControllerWithRequestDeduping);
+
 // Enables the throttling of form suggestion refresh in the form input keyboard
 // accessory.
 BASE_DECLARE_FEATURE(kThrottleFormInputAccessorySuggestionRefresh);
