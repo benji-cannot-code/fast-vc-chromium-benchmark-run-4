540c3a466fd1dcd1042efdd69aa88ec9fd4807bc
Petr Cermak <petrcermak@chromium.org>
2025-03-31 10:51:30 -0700

    Do not send the same BeginFrameId to a CompositorFrameSinkClient twice
    This CL fixes a crash in blink::VideoFrameSubmitter::OnBeginFrame()
    which was caused by VideoFrameSubmitter receiving two frames with the
    same viz::BeginFrameId. This was in turn caused by the
    VideoFrameSubmitter's associated viz::BeginFrameObserver being removed
    and then immediately re-added to the same
    viz::DelayBasedBeginFrameSource, which resulted in the
    BeginFrameObserver (and subsequently its VideoFrameSubmitter) receiving
    a viz::BeginFrameArgs::MISSED begin frame with the same BeginFrameId as
    it had already received before.
    
    Fixed: 40286473
    Change-Id: I80aa08bdcb8c76ca666a8598945711a7e3c3160f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415950
    Commit-Queue: Petr Cermak <petrcermak@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440399}

--- components/viz/service/display/display_unittest.cc
+++ components/viz/service/display/display_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/math_util.h"
 #include "cc/test/scheduler_test_common.h"
 #include "components/viz/common/features.h"
+#include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/frame_sinks/begin_frame_source.h"
 #include "components/viz/common/frame_sinks/copy_output_request.h"
 #include "components/viz/common/frame_sinks/copy_output_result.h"
@@ -240,7 +241,8 @@ class DisplayTest : public testing::Test {
 
   bool ShouldSendBeginFrame(CompositorFrameSinkSupport* support,
                             base::TimeTicks frame_time) {
-    return support->ShouldSendBeginFrame(frame_time, base::Seconds(0));
+    return support->ShouldSendBeginFrame(BeginFrameId(999, 999), frame_time,
+                                         base::Seconds(0));
   }
 
   void UpdateBeginFrameTime(CompositorFrameSinkSupport* support,
