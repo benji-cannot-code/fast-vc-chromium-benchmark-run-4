bd5c4fbb185e3e8a5195dfe501116fab29f02de8
Colin Blundell <blundell@chromium.org>
2025-03-31 04:18:41 -0700

    [Blink] Have AcceleratedStaticBitmapImage take in/hold gfx::ColorSpace
    Finalizes the long chain of work to have this class take in and hold
    native SharedImage types rather than Skia types.
    
    Bug: 371227617
    Change-Id: I13921ca815d7c56a77279db0db500f5f1528aa54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6408672
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440195}

--- third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h
+++ third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h
@@ -137,11 +137,9 @@ class PLATFORM_EXPORT AcceleratedStaticBitmapImage final
     return viz::ToClosestSkColorType(format_);
   }
   sk_sp<SkColorSpace> GetSkColorSpace() const override {
-    return sk_color_space_;
+    return color_space_.ToSkColorSpace();
   }
-  gfx::ColorSpace GetColorSpace() const override {
-    return SkColorSpaceToGfxColorSpace(GetSkColorSpace());
-  }
+  gfx::ColorSpace GetColorSpace() const override { return color_space_; }
   viz::SharedImageFormat GetSharedImageFormat() const override {
     return format_;
   }
@@ -162,7 +160,7 @@ class PLATFORM_EXPORT AcceleratedStaticBitmapImage final
       const gfx::Size& size,
       viz::SharedImageFormat format,
       SkAlphaType alpha_type,
-      sk_sp<SkColorSpace> sk_color_space,
+      const gfx::ColorSpace& color_space,
       const ImageOrientation& orientation,
       base::WeakPtr<WebGraphicsContext3DProviderWrapper>,
       base::PlatformThreadRef context_thread_ref,
@@ -176,7 +174,7 @@ class PLATFORM_EXPORT AcceleratedStaticBitmapImage final
   gfx::Size size_;
   viz::SharedImageFormat format_;
   SkAlphaType alpha_type_;
-  sk_sp<SkColorSpace> sk_color_space_;
+  gfx::ColorSpace color_space_;
 
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper_;
   scoped_refptr<MailboxRef> mailbox_ref_;
