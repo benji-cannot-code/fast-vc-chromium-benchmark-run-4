501411d48a5861c7abef5cc08df8e5d8ad2aef9f
Dale Curtis <dalecurtis@chromium.org>
2025-03-31 14:23:05 -0700

    Reland "Enable resolution based priority for src= playbacks."
    This is a reland of commit a808cad79e6993eb45ba6985d40f44366fc3d5f8
    
    Crash fixed in https://crbug.com/407493654
    
    Original change's description:
    > Enable resolution based priority for src= playbacks.
    >
    > Data shows this significantly improves rebuffering events and time
    > to first frame for src= playbacks, but is neutral to negative for
    > MSE based playbacks.
    >
    > As a side effect of this change Android will return to allowing
    > platform decoders for low resolution MSE playbacks. I'll monitor
    > gpu crash rates to see if this regresses the improvements gained
    > when we launched the software decoder there.
    >
    > Bug: 368085608
    > Change-Id: Ib6a5a093fe8c05b88b671e7d53b789ae18350cc2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6410634
    > Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1439915}
    
    Bug: 368085608
    Change-Id: I35088b393f29a3dba5868334b08fcc518984ecb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6417975
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440556}

--- media/renderers/audio_renderer_impl_unittest.cc
+++ media/renderers/audio_renderer_impl_unittest.cc
@@ -674,7 +674,8 @@ TEST_F(AudioRendererImplTest, ReinitializeForDifferentStream) {
 
   // Prepare a new demuxer stream.
   MockDemuxerStream new_stream(DemuxerStream::AUDIO);
-  EXPECT_CALL(new_stream, SupportsConfigChanges()).WillOnce(Return(false));
+  EXPECT_CALL(new_stream, SupportsConfigChanges())
+      .WillRepeatedly(Return(false));
   AudioDecoderConfig audio_config(kCodec, kSampleFormat, kChannelLayout,
                                   kInputSamplesPerSecond, EmptyExtraData(),
                                   EncryptionScheme::kUnencrypted);
