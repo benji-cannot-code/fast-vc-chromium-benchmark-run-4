8010e03b1388c891eda01d37cf75996159cec7e1
Liza Burakova <liza@chromium.org>
2025-03-31 16:07:08 -0700

    Roll cxx: 1.0.146 => 1.0.150 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * cxx: 1.0.146 => 1.0.150
    * cxxbridge-cmd: 1.0.146 => 1.0.150
    * cxxbridge-flags: 1.0.146 => 1.0.150
    * cxxbridge-macro: 1.0.146 => 1.0.150
    
    New crates:
    
    * codespan-reporting@0.12.0
    
    Removed crates:
    
    * codespan-reporting@0.11.1
    
    Bug: None
    Change-Id: I9f1a1dddc0ff112cecb4d05eca76ead91cd37b85
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394143
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440613}

--- third_party/rust/chromium_crates_io/vendor/codespan-reporting-0.11.1/src/lib.rs
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! Diagnostic reporting support for the codespan crate.
-
-#![forbid(unsafe_code)]
-
-pub mod diagnostic;
-pub mod files;
-pub mod term;
