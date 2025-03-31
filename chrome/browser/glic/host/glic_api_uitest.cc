b017348bcf1fa533f5fdbc152c0e5be367061baa
Nicola Tommasi <tommasin@chromium.org>
2025-03-31 04:32:22 -0700

    [glic] Add tabs assertions after creating a tab and opening setting
    Adds tab count assertion after glic creates a tab and and asserts
    a settings tab to be correctly opened.
    
    Change-Id: I08491149fd36ad5a2c3514647522bccecc2019a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405501
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440200}

--- chrome/browser/glic/host/glic_api_uitest.cc
+++ chrome/browser/glic/host/glic_api_uitest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/test_support/interactive_glic_test.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
+#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/profiles/profile_picker.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace glic {
 namespace {
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kFirstTab);
+DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSettingsTab);
 std::vector<std::string> GetTestSuiteNames() {
   return {
       "GlicApiTest",
@@ -188,14 +190,19 @@ IN_PROC_BROWSER_TEST_F(GlicApiTestWithOneTab, testAllTestsAreRegistered) {
 
 IN_PROC_BROWSER_TEST_F(GlicApiTest, testCreateTab) {
   RunTestSequence(OpenGlicWindow(GlicWindowMode::kDetached,
-                                 GlicInstrumentMode::kHostAndContents));
+                                 GlicInstrumentMode::kHostAndContents),
+                  CheckTabCount(1));
   ExecuteJsTest();
-  // TODO(harringtond): Add assertions to verify a tab was created.
+  RunTestSequence(CheckTabCount(2));
 }
 
 IN_PROC_BROWSER_TEST_F(GlicApiTestWithOneTab, testOpenGlicSettingsPage) {
   ExecuteJsTest();
-  // TODO(harringtond): Add assertions to verify the settings page opened.
+
+  RunTestSequence(
+      InstrumentTab(kSettingsTab),
+      WaitForWebContentsReady(
+          kSettingsTab, chrome::GetSettingsUrl(chrome::kGlicSettingsSubpage)));
 }
 
 IN_PROC_BROWSER_TEST_F(GlicApiTestWithOneTab, testClosePanel) {
