a81df32185663b67d5d667189fe0bdb30fd85798
François Doray <fdoray@chromium.org>
2025-03-31 10:29:10 -0700

    [freezing] Fix crash in DiscardFrozenPagesWithGrowingMemoryOnMemoryMeasurement.
    A CHECK failed when
    DiscardFrozenPagesWithGrowingMemoryOnMemoryMeasurement
    was invoked, and a browsing instance in which not all pages are frozen
    had a non-empty `per_origin_pmf_after_freezing_kb`. This can occur when
    a (non-frozen) page is added to a browsing instance in which all pages
    were previously frozen. This CL fixes the issue by clearing
    `per_origin_pmf_after_freezing_kb` when a (non-frozen) page is added
    to a browsing instance.
    
    Fixed: 407522185
    Change-Id: I5aa8194a40d4e0f34d2de7fa102e5ce9c60c5e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416442
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440381}

--- components/performance_manager/freezing/freezing_policy.cc
+++ components/performance_manager/freezing/freezing_policy.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
+#include "base/not_fatal_until.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/timer/timer.h"
@@ -613,6 +614,12 @@ void FreezingPolicy::OnFrameNodeAdded(const FrameNode* frame_node) {
     return;
   }
 
+  // Clear `per_origin_pmf_after_freezing_kb` since not all pages in the
+  // browsing instance are frozen when a new page is added.
+  CHECK_EQ(frame_node->GetLifecycleState(), FrameNode::LifecycleState::kRunning,
+           base::NotFatalUntil::M140);
+  browsing_instance_state.per_origin_pmf_after_freezing_kb.clear();
+
   // Update frozen state for browsing instances associated with the frame's
   // page.
   UpdateFrozenState(frame_node->GetPageNode());
@@ -799,8 +806,10 @@ void FreezingPolicy::DiscardFrozenPagesWithGrowingMemoryOnMemoryMeasurement(
         browsing_instance_states_without_initial_measurement.insert(id);
       }
     } else {
-      // Should have been cleared by `OnPageLifecycleStateChanged`.
-      CHECK(state.per_origin_pmf_after_freezing_kb.empty());
+      // Should have been cleared by OnPageLifecycleStateChanged() or
+      // OnFrameNodeAdded().
+      CHECK(state.per_origin_pmf_after_freezing_kb.empty(),
+            base::NotFatalUntil::M140);
     }
   }
 
