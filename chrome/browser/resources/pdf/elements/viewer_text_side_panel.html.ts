4a55ea3b661fe62382f0dc78dc366b2dd411449a
rbpotter <rbpotter@chromium.org>
2025-03-31 13:27:56 -0700

    [PDF Ink Signatures] Add text side panel custom element
    - Add a text side panel custom element with 2 dropdowns for font and
      text size.
    - Add a unit test for the new element.
    - Refactor some CSS in the existing side panel, so that the two panels
      can share a new side panel shared custom style.
    
    Additional controls to be added later.
    
    Bug: 402547554
    Change-Id: I613ddfa2445e75cbb1b5560e62ecb39da2d7c161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6404698
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440522}

---
+++ chrome/browser/resources/pdf/elements/viewer_text_side_panel.html.ts
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import type {ViewerTextSidePanelElement} from './viewer_text_side_panel.js';
+
+export function getHtml(this: ViewerTextSidePanelElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+    <div class="side-panel-content">
+      <h2>Font</h2>
+      <select class="md-select" style="font-family: '${this.currentFont_}';"
+          @change="${this.onFontChange_}"=>
+        ${this.fonts_.map(font => html`
+          <option value="${font}" ?selected="${this.isSelectedFont_(font)}">
+            ${font}
+          </option>`)}
+      </select>
+      <select class="md-select" @change="${this.onSizeChange_}">
+        ${this.sizes_.map(size => html`
+          <option value="${size}" ?selected="${this.isSelectedSize_(size)}">
+            ${size}
+          </option>`)}
+      </select>
+    </div>
+  <!--_html_template_end_-->`;
+  // clang-format on
+}
