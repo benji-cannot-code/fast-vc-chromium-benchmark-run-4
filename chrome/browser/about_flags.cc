c8d7eab4cec4a0e734933f584a124b897e2dc8f1
Brandon Fong <bjfong@google.com>
2025-03-31 11:57:27 -0700

    [declutter] Expose the tab group declutter flag on about flags
    Expose the already implemented tab group declutter flag.
    
    Bug: 404566251
    Change-Id: Ib1b0e5053e76ba0d68cf3332b1396791364fec8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6410793
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440454}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -10481,6 +10481,12 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          chrome::android::kAndroidTabDeclutterArchiveDuplicateTabs)},
 
+    {"android-tab-declutter-archive-tab-groups",
+     flag_descriptions::kAndroidTabDeclutterArchiveTabGroupsName,
+     flag_descriptions::kAndroidTabDeclutterArchiveTabGroupsDescription,
+     kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kAndroidTabDeclutterArchiveTabGroups)},
+
     {"force-list-tab-switcher", flag_descriptions::kForceListTabSwitcherName,
      flag_descriptions::kForceListTabSwitcherDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(chrome::android::kForceListTabSwitcher)},
