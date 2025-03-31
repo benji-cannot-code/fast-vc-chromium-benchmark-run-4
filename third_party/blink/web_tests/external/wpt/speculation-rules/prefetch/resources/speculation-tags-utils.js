f02fe067b8614f2f8d995960264cbcc1ecfc18c5
Hiroki Nakagawa <nhiroki@chromium.org>
2025-03-30 19:12:12 -0700

    Prefetch: Add WPTs for ruleset-based speculation rules tags
    Bug: 404294138
    Change-Id: Ice577c0efefd4f87bb0fe134ada380d410550cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6365774
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440068}

---
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/speculation-tags-utils.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Utilities for speculation tags tests.
+
+function testRulesetTag(tag, expectedTag, description) {
+  promise_test(async t => {
+      const agent = await spawnWindow(t);
+      const nextUrl = agent.getExecutorURL({ page: 2 });
+      await agent.forceSpeculationRules({
+          tag,
+          prefetch: [{source: "list", urls: [nextUrl]}]
+      });
+      await agent.navigate(nextUrl);
+
+      const headers = await agent.getRequestHeaders();
+      assert_prefetched(headers, "must be prefetched");
+      assert_equals(headers.sec_speculation_tags, expectedTag, "Sec-Speculation-Tags");
+  }, "Sec-Speculation-Tags: " + description);
+}
