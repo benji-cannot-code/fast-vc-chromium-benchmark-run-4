000e50026d449715ed30e04af93464dd5340fa81
Palak Agarwal <agpalak@chromium.org>
2025-03-31 02:11:04 -0700

    Rm RuntimeEnabled flag check inside fn IsCapturedSurfaceResolutionActive
    Bug: 383946052
    Change-Id: I785e32b641a8ffa2bf40eb44bfe8b02402a51c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415729
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440151}

--- third_party/blink/renderer/modules/mediastream/media_stream_track_impl.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_track_impl.cc
@@ -1169,9 +1169,7 @@ void MediaStreamTrackImpl::SendLogMessage(const WTF::String& message) {
 bool MediaStreamTrackImpl::IsCapturedSurfaceResolutionActive(
     const MediaStreamTrackPlatform::Settings& platform_settings) const {
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
-  if (RuntimeEnabledFeatures::CapturedSurfaceResolutionEnabled(
-          execution_context_) &&
-      platform_settings.physical_frame_size) {
+  if (platform_settings.physical_frame_size) {
     return true;
   }
 #endif
