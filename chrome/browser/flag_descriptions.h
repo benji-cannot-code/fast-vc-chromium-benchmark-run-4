b39412d2c1d11c8d8cd2a58018c64ef87bf3429e
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-03-31 14:56:34 -0700

    Remove stale flag kSendTabToSelfV2
    This is never launching at this point.
    
    OBSOLETE_HISTOGRAMS=The code to integrate send-tab-to-self with Android messages was deleted along with its histograms.
    
    Fixed: 40283828
    Change-Id: I61966ff8f5b38374cf17d04eb01fb61f23887241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415697
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440579}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2866,9 +2866,6 @@ extern const char kShowNewTabAnimationsDescription[];
 extern const char kShowReadyToPayDebugInfoName[];
 extern const char kShowReadyToPayDebugInfoDescription[];
 
-extern const char kSendTabToSelfV2Name[];
-extern const char kSendTabToSelfV2Description[];
-
 extern const char kSetMarketUrlForTestingName[];
 extern const char kSetMarketUrlForTestingDescription[];
 
