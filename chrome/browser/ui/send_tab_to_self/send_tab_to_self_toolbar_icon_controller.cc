efb23ea247fef8d60f608126326a2c00d4eaec4e
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-03-31 14:56:50 -0700

    [send-tab-to-self] Inline LogNotification{Opened,Dismissed}
    No behavior change.
    
    Bug: None
    Change-Id: Ic39a1de59dc574191bf3894e1bd5698438635cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415835
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440580}

--- chrome/browser/ui/send_tab_to_self/send_tab_to_self_toolbar_icon_controller.cc
+++ chrome/browser/ui/send_tab_to_self/send_tab_to_self_toolbar_icon_controller.cc
@@ -136,14 +136,6 @@ const Profile* SendTabToSelfToolbarIconController::profile() const {
   return profile_;
 }
 
-void SendTabToSelfToolbarIconController::LogNotificationOpened() {
-  send_tab_to_self::RecordNotificationOpened();
-}
-
-void SendTabToSelfToolbarIconController::LogNotificationDismissed() {
-  send_tab_to_self::RecordNotificationDismissed();
-}
-
 SendTabToSelfToolbarIconController::~SendTabToSelfToolbarIconController() =
     default;
 
--- chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_toolbar_bubble_view.cc
+++ chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_toolbar_bubble_view.cc
@@ -139,7 +139,7 @@ void SendTabToSelfToolbarBubbleView::OpenInNewTab() {
   std::move(navigate_callback_).Run(&params);
 
   GetWidget()->Close();
-  LogNotificationOpened();
+  send_tab_to_self::RecordNotificationOpened();
 }
 
 void SendTabToSelfToolbarBubbleView::Timeout() {
@@ -149,7 +149,7 @@ void SendTabToSelfToolbarBubbleView::Timeout() {
 
 void SendTabToSelfToolbarBubbleView::Hide() {
   if (!opened_) {
-    LogNotificationDismissed();
+    send_tab_to_self::RecordNotificationDismissed();
   }
   send_tab_to_self::ReceivingUiHandlerRegistry::GetInstance()
       ->GetToolbarButtonControllerForProfile(browser_->profile())
@@ -174,18 +174,6 @@ void SendTabToSelfToolbarBubbleView::ReplaceEntry(
       IDS_TOOLBAR_BUTTON_SEND_TAB_TO_SELF_FROM_DEVICE,
       base::UTF8ToUTF16(new_entry.GetDeviceName())));
   guid_ = new_entry.GetGUID();
-}
-
-void SendTabToSelfToolbarBubbleView::LogNotificationOpened() {
-  send_tab_to_self::ReceivingUiHandlerRegistry::GetInstance()
-      ->GetToolbarButtonControllerForProfile(browser_->profile())
-      ->LogNotificationOpened();
-}
-
-void SendTabToSelfToolbarBubbleView::LogNotificationDismissed() {
-  send_tab_to_self::ReceivingUiHandlerRegistry::GetInstance()
-      ->GetToolbarButtonControllerForProfile(browser_->profile())
-      ->LogNotificationDismissed();
 }
 
 BEGIN_METADATA(SendTabToSelfToolbarBubbleView)
