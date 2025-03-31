e3a4d18280b7e2efac2da8a300b8dd031d04c78d
Lingqi Chi <lingqi@chromium.org>
2025-03-30 21:48:03 -0700

    [NVS-httpcache] update filed trial config
    We're going to run the study by combining the two flags.
    
    This CL also has to disable the two flags for
    HTTPCacheSearchPreloadUnifiedBrowserTest.BackwardHitHttpCache
    because we need to update some conditions to make it pass.
    There is an ongoing CL to update the conditions, but it gets stuck
    due to the test infra flakiness.
    https://crrev.com/c/6383241
    
    Bug: 388557183
    Change-Id: I6d6a4f8d8dfb4855472a74059016a22b58491346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397504
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440097}

--- chrome/browser/preloading/prefetch/search_prefetch/search_preload_unified_browsertest.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_preload_unified_browsertest.cc
@@ -1257,7 +1257,9 @@ class HTTPCacheSearchPreloadUnifiedBrowserTest
         },
         // Disable BackForwardCache to ensure that the page is not restored from
         // the cache.
-        /*disabled_features=*/{features::kBackForwardCache});
+        /*disabled_features=*/{features::kBackForwardCache,
+                               net::features::kHttpCacheNoVarySearch,
+                               kSearchPrefetchWithNoVarySearchDiskCache});
   }
 
  private:
