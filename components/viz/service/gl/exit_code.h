30b7937a8fbb9d1520f064bf6af80f438d20e868
Sahir Vellani <sahir.vellani@microsoft.com>
2025-03-31 15:08:19 -0700

    Update viewport_pixel_size_ after Resize is confirmed
    There are reports of invalid present parameters to
    IDXGISwapChain1::Present1 causing DXGI device removal. It is found that
    the damage rect can be larger than the swap chain buffer size. This
    could happen due to SoftwareOutputDeviceWinSwapchain::ResizeDelegated
    exiting early causing the swapchain size and viewport_pixel_size_ to
    become desynchronized. Therefore ensure that viewport_pixel_size_
    synchronized with the swapchain by setting it during ResizeDelegated.
    
    Bug: 40176068
    Change-Id: Ie5c449ce60dc6b5a3a1aaf9cda82855b7448ab99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310338
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440585}

---
+++ components/viz/service/gl/exit_code.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_SERVICE_GL_EXIT_CODE_H_
+#define COMPONENTS_VIZ_SERVICE_GL_EXIT_CODE_H_
+
+namespace viz {
+enum class ExitCode {
+  // Matches service_manager::ResultCode::RESULT_CODE_NORMAL_EXIT
+  RESULT_CODE_NORMAL_EXIT = 0,
+  // Matches chrome::ResultCode::RESULT_CODE_GPU_EXIT_ON_CONTEXT_LOST
+  RESULT_CODE_GPU_EXIT_ON_CONTEXT_LOST = 34,
+};
+}  // namespace viz
+
+#endif  // COMPONENTS_VIZ_SERVICE_GL_EXIT_CODE_H_
