8bccbce767f50638afc9f65fba1489cbdc35a8b4
Ramon Cano Aparicio <rcanoaparicio@google.com>
2025-03-31 06:59:01 -0700

    Change SeedWriteWriter to store Seed data in StoredSeed struct.
    We're planning to move the Seed Signature and other seed-related
    information to the Seed Files. We'll store all this information in
    StoreSeed.
    
    This change stores the seed in the StoredInfo struct instead of storing
    it as a individual string. In future CLs the moved info will also be
    stored in this struct.
    
    Bug: 380465790
    
    Change-Id: I8f1c02a9793ad8a3d67412a1eab0abbee00ce8e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6347299
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440252}

--- components/variations/service/safe_seed_manager_unittest.cc
+++ components/variations/service/safe_seed_manager_unittest.cc
@@ -54,9 +54,7 @@ class FakeSeedStore : public VariationsSeedStore {
                                 /*entropy_providers=*/nullptr),
                             version_info::Channel::UNKNOWN,
                             /*seed_file_dir=*/base::FilePath(),
-                            /*entropy_providers=*/nullptr) {
-    VariationsSeedStore::RegisterPrefs(local_state->registry());
-  }
+                            /*entropy_providers=*/nullptr) {}
 
   FakeSeedStore(const FakeSeedStore&) = delete;
   FakeSeedStore& operator=(const FakeSeedStore&) = delete;
@@ -144,6 +142,7 @@ class SafeSeedManagerTest : public ::testing::Test {
   SafeSeedManagerTest() {
     metrics::CleanExitBeacon::RegisterPrefs(prefs_.registry());
     SafeSeedManager::RegisterPrefs(prefs_.registry());
+    VariationsSeedStore::RegisterPrefs(prefs_.registry());
   }
   ~SafeSeedManagerTest() override = default;
 
