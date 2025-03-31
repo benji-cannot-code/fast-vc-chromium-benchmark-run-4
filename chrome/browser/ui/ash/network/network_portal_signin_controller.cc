2566dc7bf1fcab266df861c0c75d2ab9857872c1
Di Wu <diwux@google.com>
2025-03-31 15:49:47 -0700

    Update obsolete documentation
    Bug: b:354842935
    Test: tryjob
    Change-Id: I69dda8551e9ce8d6793feb134183cb1460ca47ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415028
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440601}

--- chrome/browser/ui/ash/network/network_portal_signin_controller.cc
+++ chrome/browser/ui/ash/network/network_portal_signin_controller.cc
@@ -292,8 +292,7 @@ void NetworkPortalSigninController::ShowSigninDialog(const GURL& url) {
 }
 
 void NetworkPortalSigninController::ShowSigninWindow(const GURL& url) {
-  // Calls NetworkPortalSigninWindow::Show in the appropriate browser (Ash or
-  // Lacros).
+  // Calls NetworkPortalSigninWindow::Show in the appropriate browser.
   ash::NewWindowDelegate::GetPrimary()->OpenCaptivePortalSignin(url);
 }
 
@@ -314,7 +313,7 @@ void NetworkPortalSigninController::ShowTab(Profile* profile, const GURL& url) {
 }
 
 void NetworkPortalSigninController::ShowActiveProfileTab(const GURL& url) {
-  // Opens a new tab the appropriate browser (Ash or Lacros).
+  // Opens a new tab the appropriate browser.
   ash::NewWindowDelegate::GetPrimary()->OpenUrl(
       url, NewWindowDelegate::OpenUrlFrom::kUserInteraction,
       NewWindowDelegate::Disposition::kNewForegroundTab);
