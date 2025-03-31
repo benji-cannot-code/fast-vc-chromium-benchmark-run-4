20c9210256e62c056ab50e9de1ff4c401731c177
Nasser Al-shawwa <alshawwa@chromium.org>
2025-03-31 12:49:01 -0700

    Add additional logging for async uploads.
    This is to enable trusted testers to test that their rules trigger
    the correct code path using specific builds.
    
    Bug: 329293309
    Change-Id: Ic0d0456bf09b1893ceef764651ec911891beff34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416805
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440492}

--- chrome/browser/safe_browsing/cloud_content_scanning/resumable_uploader.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/resumable_uploader.cc
@@ -262,7 +262,10 @@ void ResumableUploadRequest::OnMetadataUploadCompleted(
 
   if (base::FeatureList::IsEnabled(
           enterprise_connectors::kEnableAsyncUploadAfterVerdict)) {
+    // TODO(329293309): Remove logging when rolled out to 100% Stable
+    VLOG(1) << "enterprise.asyncupload: feature enabled";
     if (headers->HasHeader(kUploadIntermediateHeader)) {
+      VLOG(1) << "enterprise.asyncupload: async upload";
       response_body = headers->GetNormalizedHeader(kUploadIntermediateHeader);
 
       std::string output;
@@ -276,6 +279,8 @@ void ResumableUploadRequest::OnMetadataUploadCompleted(
       std::move(verdict_received_callback_)
           .Run(IsSuccess(url_loader_->NetError(), response_code), response_code,
                output);
+    } else {
+      VLOG(1) << "enterprise.asyncupload: sync upload";
     }
   }
 
