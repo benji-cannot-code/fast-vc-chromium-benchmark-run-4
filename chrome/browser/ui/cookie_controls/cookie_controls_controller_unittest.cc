5a1798c371ed0b168be545727184b0e86cac7959
Abhi Patel <abhipatel@chromium.org>
2025-03-31 08:41:54 -0700

    Revert "[ACT UB] Split out IPP/FPP subresource checks into class variables."
    This reverts commit 50f624a01f237ede439ab721f02faec87f691e51.
    
    Reason for revert: These changes caused a bug where the user bypass icon visibility was incorrect when switching between tabs.
    
    Original change's description:
    > [ACT UB] Split out IPP/FPP subresource checks into class variables.
    >
    > This is a precursor to crrev.com/c/6331218, as discussed in the comment
    > here:
    > https://chromium-review.googlesource.com/c/chromium/src/+/6331218/comment/a65bbe78_4b36229b
    >
    > Bug: b:388294499
    > Change-Id: Ifc477d40f09c8c0942e1847185ab054119ea0534
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361956
    > Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    > Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1435455}
    
    Bug: b:388294499
    Change-Id: I9fbbd9ddbf2761452472f22cf34919654b4170cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6409395
    Reviewed-by: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Abhi Patel <abhipatel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440309}

--- chrome/browser/ui/cookie_controls/cookie_controls_controller_unittest.cc
+++ chrome/browser/ui/cookie_controls/cookie_controls_controller_unittest.cc
@@ -717,7 +717,7 @@ TEST_F(CookieControlsUserBypassTest, ThirdPartyCookiesException) {
 }
 
 TEST_F(CookieControlsUserBypassTest, FrequentPageReloads) {
-  // Update on the initial web contents to ensure the tab observer is set up.
+  // Update on the initial web contents to ensure the tab observer is setup.
   cookie_controls()->Update(web_contents());
   auto* hcsm = HostContentSettingsMapFactory::GetForProfile(profile());
 
@@ -1596,9 +1596,6 @@ TEST_F(CookieControlsUserBypassTest, SubresourceBlocked) {
       web_contents(), /*pref_service=*/nullptr, /*content_settings=*/nullptr,
       /*tracking_protection_settings=*/nullptr, /*is_incognito=*/false);
 
-  // Update on the initial web contents to ensure the tab observer is set up.
-  cookie_controls()->Update(web_contents());
-
   NavigateAndCommit(GURL(kUrl));
   fingerprinting_protection_filter::FingerprintingProtectionWebContentsHelper::
       FromWebContents(web_contents())
@@ -1630,9 +1627,6 @@ TEST_F(CookieControlsUserBypassTest, SubresourceBlockedInIncognito) {
       web_contents(), /*pref_service=*/nullptr, /*content_settings=*/nullptr,
       /*tracking_protection_settings=*/nullptr, /*is_incognito=*/true);
 
-  // Update on the initial web contents to ensure the tab observer is set up.
-  cookie_controls()->Update(web_contents());
-
   NavigateAndCommit(GURL(kUrl));
   fingerprinting_protection_filter::FingerprintingProtectionWebContentsHelper::
       FromWebContents(web_contents())
@@ -1661,9 +1655,6 @@ TEST_F(CookieControlsUserBypassTest, SubresourceProxied) {
       net::features::kEnableIpProtectionProxy);
 
   ip_protection::IpProtectionStatus::CreateForWebContents(web_contents());
-
-  // Update on the initial web contents to ensure the tab observer is set up.
-  cookie_controls()->Update(web_contents());
 
   NavigateAndCommit(GURL(kUrl));
 
@@ -1845,9 +1836,6 @@ TEST_F(CookieControlsUserBypassIncognitoTest, SubresourceProxied) {
       net::features::kEnableIpProtectionProxy);
 
   ip_protection::IpProtectionStatus::CreateForWebContents(web_contents());
-
-  // Update on the initial web contents to ensure the tab observer is set up.
-  incognito_cookie_controls()->Update(web_contents());
 
   NavigateAndCommit(GURL(kUrl));
 
