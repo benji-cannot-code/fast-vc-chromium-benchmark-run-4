17c0d615213c74b4e4377d6457f1edf2d7c554a0
Norge Vizcay <vizcay@google.com>
2025-03-31 02:49:22 -0700

    Rename loyalty cards sync type to valuable.
    Bug: 393119606
    Change-Id: I4774da23273cfe5380a6e8299e768b62e1abb1f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6403214
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440162}

--- components/autofill/core/browser/webdata/valuables/valuable_sync_bridge.cc
+++ components/autofill/core/browser/webdata/valuables/valuable_sync_bridge.cc
@@ -53,7 +53,7 @@ void ValuableSyncBridge::CreateForWebDataServiceAndBackend(
       &kAutofillValuableSyncBridgeUserDataKey,
       std::make_unique<ValuableSyncBridge>(
           std::make_unique<syncer::ClientTagBasedDataTypeProcessor>(
-              syncer::AUTOFILL_LOYALTY_CARD,
+              syncer::AUTOFILL_VALUABLE,
               /*dump_stack=*/base::DoNothing()),
           web_data_backend));
 }
@@ -80,7 +80,7 @@ std::unique_ptr<syncer::MetadataChangeList>
 ValuableSyncBridge::CreateMetadataChangeList() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return std::make_unique<syncer::SyncMetadataStoreChangeList>(
-      GetSyncMetadataStore(), syncer::AUTOFILL_LOYALTY_CARD,
+      GetSyncMetadataStore(), syncer::AUTOFILL_VALUABLE,
       base::BindRepeating(&syncer::DataTypeLocalChangeProcessor::ReportError,
                           change_processor()->GetWeakPtr()));
 }
@@ -128,8 +128,7 @@ std::optional<syncer::ModelError> ValuableSyncBridge::MergeFullSyncData(
   if (transaction) {
     transaction->Commit();
   }
-  web_data_backend_->NotifyOnAutofillChangedBySync(
-      syncer::AUTOFILL_LOYALTY_CARD);
+  web_data_backend_->NotifyOnAutofillChangedBySync(syncer::AUTOFILL_VALUABLE);
   return std::nullopt;
 }
 
@@ -203,8 +202,7 @@ void ValuableSyncBridge::ApplyDisableSyncChanges(
 
   // False positives can occur here if there were no loyalty cards to begin
   // with.
-  web_data_backend_->NotifyOnAutofillChangedBySync(
-      syncer::AUTOFILL_LOYALTY_CARD);
+  web_data_backend_->NotifyOnAutofillChangedBySync(syncer::AUTOFILL_VALUABLE);
 }
 
 sync_pb::EntitySpecifics
@@ -254,10 +252,11 @@ bool ValuableSyncBridge::SyncMetadataCacheContainsSupportedFields(
 
 void ValuableSyncBridge::LoadMetadata() {
   auto batch = std::make_unique<syncer::MetadataBatch>();
-  if (!GetSyncMetadataStore()->GetAllSyncMetadata(syncer::AUTOFILL_LOYALTY_CARD,
+  if (!GetSyncMetadataStore()->GetAllSyncMetadata(syncer::AUTOFILL_VALUABLE,
                                                   batch.get())) {
     change_processor()->ReportError(
-        {FROM_HERE, "Failed reading LOYALTY_CARD metadata from WebDatabase."});
+        {FROM_HERE,
+         "Failed reading AUTOFILL_VALUABLE metadata from WebDatabase."});
     return;
   } else if (SyncMetadataCacheContainsSupportedFields(
                  batch->GetAllMetadata())) {
@@ -267,7 +266,7 @@ void ValuableSyncBridge::LoadMetadata() {
     // we should force the initial sync flow to propagate the cached data into
     // the local model.
     GetSyncMetadataStore()->DeleteAllSyncMetadata(
-        syncer::DataType::AUTOFILL_LOYALTY_CARD);
+        syncer::DataType::AUTOFILL_VALUABLE);
 
     batch = std::make_unique<syncer::MetadataBatch>();
   }
--- components/sync/engine/cycle/data_type_tracker.cc
+++ components/sync/engine/cycle/data_type_tracker.cc
@@ -108,7 +108,7 @@ base::TimeDelta GetDefaultLocalChangeNudgeDelay(DataType data_type) {
     case WEBAUTHN_CREDENTIAL:
     case PLUS_ADDRESS:
     case PLUS_ADDRESS_SETTING:
-    case AUTOFILL_LOYALTY_CARD:
+    case AUTOFILL_VALUABLE:
     case SHARED_TAB_GROUP_ACCOUNT_DATA:
       return kMediumLocalChangeNudgeDelay;
     case UNSPECIFIED:
@@ -178,7 +178,7 @@ bool CanGetCommitsFromExtensions(DataType data_type) {
     case PLUS_ADDRESS_SETTING:
     case PRODUCT_COMPARISON:
     case COOKIES:
-    case AUTOFILL_LOYALTY_CARD:
+    case AUTOFILL_VALUABLE:
     case SHARED_TAB_GROUP_ACCOUNT_DATA:
       return false;
     case UNSPECIFIED:
