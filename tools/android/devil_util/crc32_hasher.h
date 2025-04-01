3e73fd5da6de4bd552c80422f04247bcd56e8ecd
Martin Kong <martinkong@google.com>
2025-04-01 07:43:31 -0700

    Create and extract a custom archive format compressed via zst
    This CL adds two new functionalities to the devil_util binary:
    (1) Create a zst-compressed custom archive file
    (2) Extract a zst-compressed custom archive file
    
    A followup change will be made to the catapult repository,
    to push a zst-compressed custom archive file instead of a zip
    to the device. Current benchmark shows that this gives us a
    ~30% reduction in the time spent in the PushChangedFiles() step.
    
    Bug: 397457369
    Change-Id: Iaab42f222435c22e2fb2ec2ae4d59db7fedfc43a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382436
    Commit-Queue: Martin Kong <martinkong@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440912}

--- tools/android/devil_util/crc32_hasher.h
+++ tools/android/devil_util/crc32_hasher.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <vector>
 
-const std::string_view kFilePathDelimiter = ":";
+inline constexpr std::string_view kFilePathDelimiter = ":";
 
 class Crc32Hasher {
  public:
