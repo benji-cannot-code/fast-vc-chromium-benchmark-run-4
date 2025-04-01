15a4966f7f34cd677a328543a68953bcdd1f39c2
Ramon Cano Aparicio <rcanoaparicio@google.com>
2025-04-01 01:08:19 -0700

    Pass signature pref to SeedReaderWriter constructor.
    We're planning to move the seed signature and other seed-related info to
    the seed files. We'll store the seed and seed-related info in a
    StoreInfo struct in SeedWriteWriter.
    
    We want to store-retrieve the signature through SeedReaderWriter. This
    CL changes the SeedReaderWriter to accept the signature pref so the
    value can be read from Local State. For now this signature is not being
    used, we only check that the pref passed corresponds with the seed pref.
    
    Bug: 380465790
    
    Change-Id: I054af08ac1e67ab8e93896503650c7cdb25ed2ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348879
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440764}

--- components/variations/variations_seed_store.cc
+++ components/variations/variations_seed_store.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/client_filterable_state.h"
 #include "components/variations/pref_names.h"
 #include "components/variations/proto/variations_seed.pb.h"
+#include "components/variations/seed_reader_writer.h"
 #include "components/variations/variations_safe_seed_store_local_state.h"
 #include "components/variations/variations_switches.h"
 #include "components/version_info/version_info.h"
@@ -193,7 +194,7 @@ VariationsSeedStore::VariationsSeedStore(
           std::make_unique<SeedReaderWriter>(local_state,
                                              seed_file_dir,
                                              kSeedFilename,
-                                             prefs::kVariationsCompressedSeed,
+                                             kRegularSeedFieldsPrefs,
                                              channel,
                                              entropy_providers)) {
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
