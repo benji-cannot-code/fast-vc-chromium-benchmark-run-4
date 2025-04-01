d6473297ae98e796ceeb5b3468ab7db67bb6e9e7
Yuta Hijikata <ythjkt@google.com>
2025-04-01 01:32:52 -0700

    Remove EchoPrivate crosapi [3/3].
    Since Lacros is gone, crosapi is set to be removed including
    EchoPrivate. However some methods on crosapi::EchoPrivateAsh
    are still in use after lacros sunset. Therefore the removal is done in
    separate steps:
    
    1. Duplicate methods still in use out of crosapi.
    2. Remove unused crosapi::EchoPrivateAsh.
    
    This patch is step 2.
    
    [1/N] crrev.com/c/6356747.
    [2/N] crrev.com/c/6370564.
    
    Bug: b:365741912, b:365902693
    Change-Id: I7cc389ae95e432222c88d51655f57c685406bea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367560
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440773}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/device_attributes_ash.h"
 #include "chrome/browser/ash/crosapi/device_oauth2_token_service_ash.h"
 #include "chrome/browser/ash/crosapi/document_scan_ash.h"
-#include "chrome/browser/ash/crosapi/echo_private_ash.h"
 #include "chrome/browser/ash/crosapi/file_change_service_bridge_ash.h"
 #include "chrome/browser/ash/crosapi/file_system_access_cloud_identifier_provider_ash.h"
 #include "chrome/browser/ash/crosapi/file_system_provider_service_ash.h"
@@ -123,7 +122,6 @@ CrosapiAsh::CrosapiAsh()
           std::make_unique<DeviceOAuth2TokenServiceAsh>()),
       diagnostics_service_ash_(std::make_unique<ash::DiagnosticsServiceAsh>()),
       document_scan_ash_(std::make_unique<DocumentScanAsh>()),
-      echo_private_ash_(std::make_unique<EchoPrivateAsh>()),
       file_system_access_cloud_identifier_provider_ash_(
           std::make_unique<FileSystemAccessCloudIdentifierProviderAsh>()),
       file_system_provider_service_ash_(
@@ -243,11 +241,6 @@ void CrosapiAsh::BindDiagnosticsService(
 void CrosapiAsh::BindDocumentScan(
     mojo::PendingReceiver<mojom::DocumentScan> receiver) {
   document_scan_ash_->BindReceiver(std::move(receiver));
-}
-
-void CrosapiAsh::BindEchoPrivate(
-    mojo::PendingReceiver<mojom::EchoPrivate> receiver) {
-  echo_private_ash_->BindReceiver(std::move(receiver));
 }
 
 void CrosapiAsh::BindFileChangeServiceBridge(
--- chrome/browser/ash/crosapi/crosapi_util_unittest.cc
+++ chrome/browser/ash/crosapi/crosapi_util_unittest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile_manager.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_types.h"
-#include "chromeos/ash/components/settings/cros_settings_names.h"
 #include "chromeos/ash/components/system/fake_statistics_provider.h"
 #include "chromeos/ash/components/system/statistics_provider.h"
 #include "chromeos/ash/experiences/arc/test/arc_util_test_support.h"
