2f930891b3622dfe40b66a11fb107616e96fdbb1
Henrique Nakashima <hnakashima@chromium.org>
2025-04-01 08:51:37 -0700

    [Android] Add more entry points and utils to BaseCtaTransitTestRule (4)
    Adding these separately from the CL that needs these methods to manage
    revert risk:
    
    Bug: 399877997
    Change-Id: I3a10a1cc5e0d172c2c66b63c44c0c993a25c60e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419102
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440950}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/BaseCtaTransitTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/BaseCtaTransitTestRule.java
@@ -14,6 +14,7 @@ import org.chromium.chrome.browser.ui.appmenu.AppMenuCoordinator;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.net.test.EmbeddedTestServer;
+import org.chromium.net.test.EmbeddedTestServerRule;
 import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.url.GURL;
 
@@ -134,9 +135,14 @@ class BaseCtaTransitTestRule {
         mActivityTestRule.resumeMainActivityFromLauncher();
     }
 
-    // TODO(crbug.com/406324209): Most of these checks are done in Public Transit as part of
-    // PageStation, TabSwitcherStation and subclasses. Audit each case, add any checks missed in the
-    // Stations, and remove these calls.
+    // TODO(crbug.com/406324209): ChromeTabbedActivityTestRule#startActivityCompletely() already
+    // calls this. Double check that callers are using those entry points and remove this.
+    public void waitForActivityNativeInitializationComplete() {
+        mActivityTestRule.waitForActivityNativeInitializationComplete();
+    }
+
+    // TODO(crbug.com/406324209): ChromeTabbedActivityTestRule#startActivityCompletely() already
+    // calls this. Double check that callers are using those entry points and remove this.
     public void waitForActivityCompletelyLoaded() {
         mActivityTestRule.waitForActivityCompletelyLoaded();
     }
@@ -144,5 +150,9 @@ class BaseCtaTransitTestRule {
     // TODO(crbug.com/406324209): Support recreate() in Public Transit.
     public void recreateActivity() {
         mActivityTestRule.recreateActivity();
+    }
+
+    public EmbeddedTestServerRule getEmbeddedTestServerRule() {
+        return mActivityTestRule.getEmbeddedTestServerRule();
     }
 }
