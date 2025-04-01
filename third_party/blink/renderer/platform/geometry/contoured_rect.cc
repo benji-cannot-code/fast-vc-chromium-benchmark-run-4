23f14a9396d455e425ff8a6d215fdba56a4f7699
Noam Rosenthal <nrosenthal@chromium.org>
2025-04-01 05:32:30 -0700

    corner-shape: use proportional radii for shadows
    Instead of using offsets and curvature adjustments like in borders,
    we treat the shadow spread as if it was a scale, and maintain the
    original rect->radii proportions.
    
    This ensures that the shadow shape looks like a scaled version
    of the original rect, and its edges (e.g. scoop) are crisp.
    
    Bug: 397459628
    Change-Id: Ie155c131be3478cb6d838970cef0afb73d6ed32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420932
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1440842}

--- third_party/blink/renderer/platform/geometry/contoured_rect.cc
+++ third_party/blink/renderer/platform/geometry/contoured_rect.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/geometry/path.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
+#include "ui/gfx/geometry/outsets_f.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/quad_f.h"
 
@@ -59,6 +60,30 @@ String ContouredRect::ToString() const {
 bool ContouredRect::IntersectsQuad(const gfx::QuadF& quad) const {
   return HasRoundCurvature() ? rect_.IntersectsQuad(quad)
                              : GetPath().Intersects(quad);
+}
+
+void ContouredRect::OutsetForMarginOrShadow(const gfx::OutsetsF& outsets) {
+  // For ordinary rounded rects, we use the existing formula.
+  if (HasRoundCurvature()) {
+    rect_.OutsetForMarginOrShadow(outsets);
+    return;
+  }
+
+  // For anything else, keep the same proportions between the original radii and
+  // the original rect.
+  gfx::RectF new_rect = rect_.Rect();
+  FloatRoundedRect::Radii radii = rect_.GetRadii();
+  new_rect.Outset(outsets);
+
+  CHECK(!rect_.IsEmpty());
+  float scale_x = new_rect.width() / rect_.Rect().width();
+  float scale_y = new_rect.height() / rect_.Rect().height();
+  radii.SetTopLeft(gfx::ScaleSize(radii.TopLeft(), scale_x, scale_y));
+  radii.SetTopRight(gfx::ScaleSize(radii.TopRight(), scale_x, scale_y));
+  radii.SetBottomRight(gfx::ScaleSize(radii.BottomRight(), scale_x, scale_y));
+  radii.SetBottomLeft(gfx::ScaleSize(radii.BottomLeft(), scale_x, scale_y));
+  rect_.SetRadii(radii);
+  rect_.SetRect(new_rect);
 }
 
 Path ContouredRect::GetPath() const {
