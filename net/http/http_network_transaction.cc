e36a9475367b3bff75ad75038a3aec779b7333ea
Kenichi Ishibashi <bashi@chromium.org>
2025-04-01 14:44:32 -0700

    Make SpdySession unavailable when HTTP/1.1 is required by auth
    This is a preparation for fixing the linked bug.
    
    Before this CL, HttpNetworkTransaction::PrepareForAuthRestart() called
    HttpServerProperties::SetHTTP11Required() but didn't do anything for the
    underlying SpdySession. The SpdySession was kept available. This caused
    a need to check HttpServerProperties::HTTP11Required() after getting an
    existing SpdySession in HttpStreamPool [1]. If the destination required
    HTTP/1.1, HttpStreamPool made the session unavailable. This logic made
    the lifetime of Jobs and AttemptManager complicated. Due to the
    complexity, fixing the linked bug was difficult.
    
    This CL adds HttpStream::SetHTTP11Required() to make the underlying
    SpdySession unavailable immediately to avoid such complexities. With
    this CL, SpdySessionPool::FindAvailableSession() should always return
    nullptr when HTTP/1.1 is required.
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:net/http/http_stream_pool.cc;l=515;drc=6fc15a0ca8e80da597abb373986b33844d920d04
    
    Bug: 406932139
    Change-Id: If87c19dbe514a0f18f1b03be2c7f71ba43475688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6414331
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441201}

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -388,6 +388,7 @@ void HttpNetworkTransaction::PrepareForAuthRestart(HttpAuth::Target target) {
 
     session_->http_server_properties()->SetHTTP11Required(
         url::SchemeHostPort(rewritten_url), network_anonymization_key_);
+    stream_->SetHTTP11Required();
   }
 
   bool keep_alive = false;
