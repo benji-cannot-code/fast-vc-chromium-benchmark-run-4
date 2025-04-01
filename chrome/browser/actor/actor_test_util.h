576eebeed6fc3483d337a279e54a74e0c568c3ca
David Bokan <bokan@chromium.org>
2025-03-31 19:51:41 -0700

    Implement actor framework history tools
    This tool handles both the back and forward cases.
    
    Bug: 402730958, 402730309
    Change-Id: If2ee09925a4eb0879f09ad8d1eda5f588098f204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405913
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440692}

--- chrome/browser/actor/actor_test_util.h
+++ chrome/browser/actor/actor_test_util.h
@@ -10,7 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace actor {
 
+inline constexpr char kTestDataPath[] = "chrome/test/data/actor";
+
 optimization_guide::proto::BrowserAction MakeClick(int content_node_id);
+optimization_guide::proto::BrowserAction MakeHistoryBack();
+optimization_guide::proto::BrowserAction MakeHistoryForward();
 
 }  // namespace actor
 
