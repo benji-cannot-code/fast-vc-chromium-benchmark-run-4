fe1e8c9102b4e4e8a66108fbcd9623e1cc7b6e53
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-04-01 16:49:41 -0700

    Extend invalid size context loss and restore to OffscreenCanvas
    HTMLCanvasElement has a mechanism for triggering a context loss if the
    canvas size is invalid. OffscreenCanvas doesn't have this.
    OffscreenCanvas however are subject to the same canvas limit size.
    The OffscreenCanvas is undrawable if is_valid_size_ is false [1], which
    is a shorthand for IsValidImageSize [2] [3], same as what
    HTMLCanvasElement uses [4].
    
    By enabling the context loss and restore logic for OffscreenCanvas, we
    can move the logic to BaseRenderingContext2D, simplifying the overall
    implementation.
    
    The test OffscreenCanvas-convertToBlob-oversized.html was failing with
    this change. The test validates that that generating large images with
    max sizes do not crash. There is a clear size limit for JPEG and WEBP,
    but for PNG, a limit of 65535 was used. PNG actually has a much higher
    limit. 65535 is actually the limit we put on the canvas size. The CL
    adjusts the test to remain under this limit.
    
    [1]: https://crsrc.org/c/third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=288;drc=7334424b6359af19a92dee7da024b6cffc486bb7
    [2]: https://crsrc.org/c/third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=106;drc=7334424b6359af19a92dee7da024b6cffc486bb7
    [3]: https://crsrc.org/c/third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=192;drc=7334424b6359af19a92dee7da024b6cffc486bb7
    [4]: https://crsrc.org/c/third_party/blink/renderer/core/html/canvas/html_canvas_element.cc;l=2145;drc=fcb352f82e1d879fa5b1765254c1de92f570c714
    
    Bug: 404257881
    Change-Id: I2bd1632480af674ea628bcc5f8156b8425b55e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6414174
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441270}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_font_cache.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_performance_monitor.h"
+#include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h"
 #include "third_party/blink/renderer/core/html/canvas/html_canvas_element.h"
 #include "third_party/blink/renderer/core/html/canvas/image_data.h"
@@ -84,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/image.h"
 #include "third_party/blink/renderer/platform/graphics/image_data_buffer.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_image.h"
+#include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/video_frame_image_util.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
@@ -316,6 +318,20 @@ void BaseRenderingContext2D::TryRestoreContextEvent(TimerBase* timer) {
   // Retry up to `kMaxTryRestoreContextAttempts` times before giving up.
   if (++try_restore_context_attempt_count_ > kMaxTryRestoreContextAttempts) {
     try_restore_context_event_timer_.Stop();
+  }
+}
+
+void BaseRenderingContext2D::RestoreFromInvalidSizeIfNeeded() {
+  CanvasRenderingContextHost* host = GetCanvasRenderingContextHost();
+  if (!context_restorable_ || context_lost_mode_ != kInvalidCanvasSize ||
+      !host) {
+    return;
+  }
+  DCHECK(!host->ResourceProvider());
+
+  if (IsValidImageSize(host->Size())) {
+    dispatch_context_restored_event_timer_.StartOneShot(base::TimeDelta(),
+                                                        FROM_HERE);
   }
 }
 
--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.h
@@ -136,7 +136,6 @@ class MODULES_EXPORT CanvasRenderingContext2D final
   void drawFocusIfNeeded(Path2D*, Element*);
 
   void LoseContext(LostContextMode) override;
-  void RestoreFromInvalidSizeIfNeeded() override;
 
   // TaskObserver implementation
   void DidProcessTask(const base::PendingTask&) final;
