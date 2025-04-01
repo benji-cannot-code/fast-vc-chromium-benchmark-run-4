36ce7714d8b3d474330a9300b48229243291e0eb
Avi Drissman <avi@chromium.org>
2025-04-01 12:07:30 -0700

    Modernize base/files/drive_info_apple.mm
    - Split it into Mac/iOS files, and turn them into .cc files
    - Use memory scopers
    - Use conversion helpers
    - Do CFStringRef conversion at build time
    - Fix an io_object leak
    - Add a smoke unittest
    
    and more.
    
    Bug: 405430588
    Change-Id: I00dc560d6215ce5f955aefb8df5014170fbde6bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6399073
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Feels: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Feels: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441099}

--- base/files/drive_info_ios.cc
+++ base/files/drive_info_ios.cc
@@ -7,9 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-DriveInfo::DriveInfo() = default;
-DriveInfo::DriveInfo(DriveInfo&&) = default;
-DriveInfo& DriveInfo::operator=(DriveInfo&&) = default;
-DriveInfo::~DriveInfo() = default;
+std::optional<DriveInfo> GetFileDriveInfo(const FilePath& file_path) {
+  DriveInfo info;
+  info.has_seek_penalty = false;
+  return info;
+}
 
 }  // namespace base
