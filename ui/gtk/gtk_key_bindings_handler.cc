aba7a4f6da830c9da288265c5ae4faa3b42f7285
Tom Anderson <thomasanderson@chromium.org>
2025-03-31 23:14:22 -0700

    Add ui/base/glib/gsettings.*
    This is intended to extract similar GSettings creation code to a common
    location.
    
    R=thestig
    
    Bug: None
    Change-Id: Ifa442a656dd47c3745f7662d4dcafd940a8fb620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6414677
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440727}

--- ui/gtk/gtk_key_bindings_handler.cc
+++ ui/gtk/gtk_key_bindings_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/logging.h"
+#include "ui/base/glib/gsettings.h"
 #include "ui/base/ime/text_edit_commands.h"
 #include "ui/events/event_constants.h"
 #include "ui/gtk/gtk_compat.h"
@@ -85,17 +86,10 @@ constexpr auto kEmacsBindings =
 }  // namespace
 
 GtkKeyBindingsHandler::GtkKeyBindingsHandler() {
-  // g_settings_new() will fatally fail if the schema does not exist, so
-  // use g_settings_schema_source_lookup() to check for it first.
-  auto* schema_source = g_settings_schema_source_get_default();
-  auto* schema =
-      g_settings_schema_source_lookup(schema_source, kDesktopInterface, true);
-  if (!schema) {
+  settings_ = ui::GSettingsNew(kDesktopInterface);
+  if (!settings_) {
     return;
   }
-  settings_ = TakeGObject(g_settings_new_full(schema, nullptr, nullptr));
-  g_settings_schema_unref(schema);
-
   signal_ = ScopedGSignal(
       settings_, "changed",
       base::BindRepeating(&GtkKeyBindingsHandler::OnSettingsChanged,
