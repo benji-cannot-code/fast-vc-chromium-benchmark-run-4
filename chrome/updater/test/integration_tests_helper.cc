ecbee77e2842eb143d1a27ad25ddf6f2c1e2a60f
S. Ganesh <ganesh@chromium.org>
2025-04-01 03:08:40 -0700

    updater: enable CECA in cross arch integration tests for PolicyStatus
    This is a follow-up CL to r1440700, enabling CECA when running these
    tests. Since these are policy tests, it makes sense to enable CECA.
    
    Fixed: 406866043
    Change-Id: Ib537e0ebf3f4a922ebf22f5b78e8d076cdf9488c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420025
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440808}

--- chrome/updater/test/integration_tests_helper.cc
+++ chrome/updater/test/integration_tests_helper.cc
@@ -59,7 +59,7 @@ constexpr int kBadCommand = 102;
 
 base::Value ValueFromString(const std::string& values) {
   std::optional<base::Value> results_value = base::JSONReader::Read(values);
-  EXPECT_TRUE(results_value);
+  EXPECT_TRUE(results_value) << values;
   return results_value->Clone();
 }
 
@@ -452,8 +452,9 @@ void AppTestHelper::FirstTaskRun() {
           {"setup_fake_updater_lower_version",
            WithSystemScope(Wrap(&SetupFakeUpdaterLowerVersion))},
           {"setup_real_updater",
-           WithSwitch("updater_path",
-                      WithSystemScope(Wrap(&SetupRealUpdater)))},
+           WithSwitch("switches",
+                      WithSwitch("updater_path",
+                                 WithSystemScope(Wrap(&SetupRealUpdater))))},
           {"set_first_registration_counter",
            WithSwitch("value", WithSystemScope(Wrap(&SetServerStarts)))},
           {"stress_update_service",
