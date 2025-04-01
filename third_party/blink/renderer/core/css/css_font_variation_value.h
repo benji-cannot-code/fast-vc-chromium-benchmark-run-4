57ea9a8f58f74544fc7a34be9aa1f570deffd3af
Rune Lillesveen <futhark@chromium.org>
2025-04-01 03:17:33 -0700

    Support sibling-index() and sign() for font-variation-settings
    Bug: 405311304
    Change-Id: Ic54686684ab02e64767e5578b02b90b63870f652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405545
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440810}

--- third_party/blink/renderer/core/css/css_font_variation_value.h
+++ third_party/blink/renderer/core/css/css_font_variation_value.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_FONT_VARIATION_VALUE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_FONT_VARIATION_VALUE_H_
 
-#include "third_party/blink/renderer/core/css/css_value.h"
+#include "third_party/blink/renderer/core/css/css_primitive_value.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -15,21 +15,23 @@ namespace cssvalue {
 
 class CSSFontVariationValue : public CSSValue {
  public:
-  CSSFontVariationValue(const AtomicString& tag, float value);
+  CSSFontVariationValue(const AtomicString& tag,
+                        const CSSPrimitiveValue* value);
 
   const AtomicString& Tag() const { return tag_; }
-  float Value() const { return value_; }
+  const CSSPrimitiveValue* Value() const { return value_.Get(); }
   String CustomCSSText() const;
 
   bool Equals(const CSSFontVariationValue&) const;
 
   void TraceAfterDispatch(blink::Visitor* visitor) const {
+    visitor->Trace(value_);
     CSSValue::TraceAfterDispatch(visitor);
   }
 
  private:
   AtomicString tag_;
-  const float value_;
+  Member<const CSSPrimitiveValue> value_;
 };
 
 }  // namespace cssvalue
