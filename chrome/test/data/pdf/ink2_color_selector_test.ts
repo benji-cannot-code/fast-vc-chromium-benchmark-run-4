b7df84bf0bb18eefe7f3e2748c445fb5381449fb
rbpotter <rbpotter@chromium.org>
2025-04-01 12:19:17 -0700

    [PDF Ink Signatures] Refactor color selector to remove brush logic
    Remove brush specific logic from the color selector and move it into
    the AnnotationBrushMixin that's used by both the ink annotations bottom
    toolbar and the ink annotations side panel. Instead, have the color
    selector accept an array of ColorOptions optionally containing a
    different visibleColor for CSS styling. This makes the selector
    reusable for text color selection.
    
    In order to continue fully testing the logic that is now moved to the
    mixin, add a new dedicated test suite for AnnotationBrushMixin.
    
    NO_IFTTT=CL moves constants to a new file and adds boolean to the data
    structure that is only needed in TS, it does not change values
    shared with the backend.
    
    Bug: 402547554
    Change-Id: I2ddf164e256a3e8f77a1cfe3bb33b8e6f460fe0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418005
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441107}

--- chrome/test/data/pdf/ink2_color_selector_test.ts
+++ chrome/test/data/pdf/ink2_color_selector_test.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {AnnotationBrushType, hexToColor, PEN_COLORS} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
+import {hexToColor, HIGHLIGHTER_COLORS, PEN_COLORS} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
 import type {Color, InkColorSelectorElement} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
 import {keyDownOn} from 'chrome://webui-test/keyboard_mock_interactions.js';
 import {eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
@@ -13,6 +13,7 @@ import {assertLabels, assertSelectedColor, getColorButtons} from './test_util.js
 
 function createSelector(initialValue?: Color): InkColorSelectorElement {
   const selector = document.createElement('ink-color-selector');
+  selector.colors = PEN_COLORS;
   // Emulate the parent initializing this value via a data binding, e.g. before
   // the sidepanel is shown, or before the bottom toolbar color button is
   // clicked to add this element to the DOM.
@@ -57,18 +58,16 @@ chrome.test.runTests([
     chrome.test.succeed();
   },
 
-  // Test that certain brush types have different color options.
+  // Test that changing the color options updates the DOM correctly.
   async function testColorLength() {
     const selector = createSelector();
 
     // Pens should have 20 color options.
-    selector.currentType = AnnotationBrushType.PEN;
     await microtasksFinished();
-
     chrome.test.assertEq(20, getColorButtons(selector).length);
 
     // Highlighters should have 10 color options.
-    selector.currentType = AnnotationBrushType.HIGHLIGHTER;
+    selector.colors = HIGHLIGHTER_COLORS;
     await microtasksFinished();
 
     chrome.test.assertEq(10, getColorButtons(selector).length);
@@ -173,10 +172,8 @@ chrome.test.runTests([
   // pressing 'ArrowDown' will select the color button in the first row in the
   // same column.
   async function testArrowKeysChangeColorFirstLastRow() {
-    // Switch to pen, which has multiple rows of colors.
+    // Defaults to the 20 PEN_COLORS in createSelector().
     const selector = createSelector();
-    chrome.test.assertEq(AnnotationBrushType.PEN, selector.currentType);
-
     const colorButtons = getColorButtons(selector);
     chrome.test.assertEq(20, colorButtons.length);
 
@@ -197,10 +194,10 @@ chrome.test.runTests([
     chrome.test.succeed();
   },
 
-  // Test the labels for each highlighter color button.
+  // Test the labels when the colors are set to the HIGHLIGHTER_COLORS.
   async function testHighlighterLabels() {
     const selector = createSelector();
-    selector.currentType = AnnotationBrushType.HIGHLIGHTER;
+    selector.colors = HIGHLIGHTER_COLORS;
     await microtasksFinished();
 
     const colorButtons = getColorButtons(selector);
@@ -226,11 +223,10 @@ chrome.test.runTests([
     chrome.test.succeed();
   },
 
-  // Test the labels for each pen color button.
+  // Test the labels when the colors are set to the PEN_COLORS (default in
+  // createSelector()).
   function testPenLabels() {
     const selector = createSelector();
-    chrome.test.assertEq(AnnotationBrushType.PEN, selector.currentType);
-
     const colorButtons = getColorButtons(selector);
 
     const expectedLabels: string[] = [
