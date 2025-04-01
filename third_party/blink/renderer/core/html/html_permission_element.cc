fcd45154735ffb66c5af4afe4d7836bb709f1e9a
Thomas Nguyen <tungnh@chromium.org>
2025-04-01 00:28:20 -0700

    [PEPC] Add new API `isTypeSupported` to permission element
    Bug: 403209084,403209102
    Change-Id: If8a5c1cfef4ca9411c4909df4a59db59976e4f56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6414771
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440747}

--- third_party/blink/renderer/core/html/html_permission_element.cc
+++ third_party/blink/renderer/core/html/html_permission_element.cc
@@ -397,6 +397,11 @@ void RecordUserInteractionAccepted(bool accepted) {
 
 }  // namespace
 
+// static
+bool HTMLPermissionElement::isTypeSupported(const AtomicString& type) {
+  return !ParsePermissionDescriptorsFromString(type).empty();
+}
+
 HTMLPermissionElement::HTMLPermissionElement(Document& document)
     : HTMLElement(html_names::kPermissionTag, document),
       ScrollSnapshotClient(GetDocument().GetFrame()),
