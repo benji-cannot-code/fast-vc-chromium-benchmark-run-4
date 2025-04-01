6d40cf04f3f360454449eb0f87c50eac64684057
Mike Wasserman <msw@chromium.org>
2025-04-01 11:34:35 -0700

    Built-In AI APIs: Fix downloadprogress monitoring
    Enforce start 0 and end 1 events in AICreateMonitor to match spec
    github.com/webmachinelearning/writing-assistance-apis#download-progress
    github.com/webmachinelearning/prompt-api#download-progress
    
    Ensure end event is dispatched in AIMojoCreateClient::OnResult impls.
    Add explicit mock start event in EchoAIManagerImpl; fix error case.
    Simplify AIOnDeviceModelComponentObserver logic.
    
    Add per-API WPTs, remove createSummarizerMaybeDownload.
    
    Bug: 381969448, 407592116, 391712889
    Fixed: 393553575
    Change-Id: If553fd91f0fd79ffb42296367b494ca96c88c0c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6411291
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441074}

--- third_party/blink/renderer/modules/ai/ai_create_monitor.cc
+++ third_party/blink/renderer/modules/ai/ai_create_monitor.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/ai/ai_create_monitor.h"
 
+#include <algorithm>
+
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/mojom/ai/model_download_progress_observer.mojom-blink.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
@@ -41,10 +43,18 @@ ExecutionContext* AICreateMonitor::GetExecutionContext() const {
 void AICreateMonitor::OnDownloadProgressUpdate(uint64_t downloaded_bytes,
                                                uint64_t total_bytes) {
   CHECK_EQ(total_bytes, kNormalizedDownloadProgressMax);
-  double normalized_downloaded_bytes =
-      downloaded_bytes / static_cast<double>(total_bytes);
-  DispatchEvent(*ProgressEvent::Create(event_type_names::kDownloadprogress,
-                                       true, normalized_downloaded_bytes, 1));
+  // Dispatch a synthetic start event, as needed, for spec compliance.
+  if (!(dispatched_start_ |= (downloaded_bytes == 0))) {
+    OnDownloadProgressUpdate(0, total_bytes);
+  }
+  // Refrain from dispatching events after the end event.
+  if (!dispatched_end_) {
+    const double normalized_downloaded_bytes =
+        std::min(downloaded_bytes / static_cast<double>(total_bytes), 1.0);
+    DispatchEvent(*ProgressEvent::Create(event_type_names::kDownloadprogress,
+                                         true, normalized_downloaded_bytes, 1));
+  }
+  dispatched_end_ |= (downloaded_bytes >= total_bytes);
 }
 
 mojo::PendingRemote<mojom::blink::ModelDownloadProgressObserver>
