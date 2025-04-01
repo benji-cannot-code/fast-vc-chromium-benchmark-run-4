d20e90b6d7af27cca3626566b2573b378604af0c
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-04-01 16:51:22 -0700

    Test cancelling invalid size restore before context lost
    Page can prevent context restoration from happening by doing
    event.preventDefault() in the oncontextloss event handler. Nothing
    prevents the page from addressing the size issue in the same task.
    In this case, restoration should still be cancelled.
    
    Bug: 404257881
    Change-Id: I83cadd1ebfbef0efa9369d3c78ac33dfc7cfb3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6414177
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441273}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -281,6 +281,10 @@ void BaseRenderingContext2D::DispatchContextRestoredEvent(TimerBase*) {
     return;
   }
 
+  if (!context_restorable_) {
+    return;
+  }
+
   CanvasRenderingContextHost* host = GetCanvasRenderingContextHost();
   if (host == nullptr) {
     // This function can be called in a new task, via
