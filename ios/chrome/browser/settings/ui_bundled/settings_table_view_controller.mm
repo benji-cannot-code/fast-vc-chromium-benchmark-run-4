6112628f8b15dded8ebabf0fdee459de779ad1ed
Ye Tian <tianye@microsoft.com>
2025-04-01 07:03:24 -0700

    [iOS]Load table view model when viewDidLoad instead of init stage
    -loadModel should not be called from initializer. It should be
    called in -viewDidLoad.
    
    Bug: 41344225
    Change-Id: If38209babd28506d5755e876e17f38c086947ef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6414072
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Tian Ye <tianye@microsoft.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440883}

--- ios/chrome/browser/settings/ui_bundled/settings_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/settings_table_view_controller.mm
@@ -429,10 +429,6 @@ struct EnhancedSafeBrowsingActivePromoData
         [[NotificationsSettingsObserver alloc] initWithPrefService:prefService
                                                         localState:localState];
     _notificationsObserver.delegate = self;
-
-    // TODO(crbug.com/41344225): -loadModel should not be called from
-    // initializer. A possible fix is to move this call to -viewDidLoad.
-    [self loadModel];
   }
   return self;
 }
@@ -456,6 +452,8 @@ struct EnhancedSafeBrowsingActivePromoData
 
   self.navigationItem.largeTitleDisplayMode =
       UINavigationItemLargeTitleDisplayModeAlways;
+
+  [self loadModel];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
