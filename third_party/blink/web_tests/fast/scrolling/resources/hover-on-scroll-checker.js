582123e855e647994018548676882d6b95c6d073
Rahul Arakeri <arakeri@microsoft.com>
2025-04-01 10:57:14 -0700

    Speculative fix for no-hover-during-smooth-keyboard-scroll.html.
    This CL is a speculative fix for a timeout. The root cause is still
    unknown but updating waitForCompositorCommit to waitForCompositorReady
    makes the test stable.
    
    Bug: 40286812
    Change-Id: I0014b5b0d61e7f3bf8414bc089c6b4a3c967eb0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6410031
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1441040}

--- third_party/blink/web_tests/fast/scrolling/resources/hover-on-scroll-checker.js
+++ third_party/blink/web_tests/fast/scrolling/resources/hover-on-scroll-checker.js
@@ -24,7 +24,7 @@ function elementHeight() {
 function runHoverStateOnScrollTest(scrollCallback, targetIndex) {
   verifyTestDriverLoaded();
   const runTest = async (resolve, reject) => {
-    await waitForCompositorCommit();
+    await waitForCompositorReady();
 
     const array = document.getElementsByClassName('hoverme');
     const center = elementCenter(array[0]);
@@ -63,7 +63,7 @@ function runHoverStateOnScrollTest(scrollCallback, targetIndex) {
 
     document.removeEventListener('scroll', scrollListener);
 
-    await waitForCompositorCommit();
+    await waitForCompositorReady();
 
     // Once scrolling is complete, the hover state must update.
     validateHoverState(array, targetIndex, () => {
