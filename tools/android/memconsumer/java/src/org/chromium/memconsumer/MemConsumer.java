83542378b96930107382ad3ff8db204c186f9617
Peter Wen <wnwen@google.com>
2025-04-01 14:16:02 -0700

    Add @NullMarked to //tools/android/memconsumer
    R=mheikal@chromium.org
    
    Bug: 389129271
    Change-Id: I6cdc1dd7f9083281077b86e60cba13cf407f2700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423052
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441182}

--- tools/android/memconsumer/java/src/org/chromium/memconsumer/MemConsumer.java
+++ tools/android/memconsumer/java/src/org/chromium/memconsumer/MemConsumer.java
@@ -20,10 +20,14 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+
+@NullMarked
 public class MemConsumer extends Activity {
     public static final String NOTIFICATION_ACTION = MemConsumer.class.toString() + ".NOTIFICATION";
 
-    private ResidentService mResidentService;
+    private @Nullable ResidentService mResidentService;
     private int mMemory;
     private NumberPicker mMemoryPicker;
 
@@ -42,7 +46,7 @@ public class MemConsumer extends Activity {
             };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mMemoryPicker = new NumberPicker(this);
         mMemoryPicker.setGravity(Gravity.CENTER);
