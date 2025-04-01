1c24abad80240631522dd9d29527947d69f094d7
dmblack <dmblack@google.com>
2025-04-01 15:45:41 -0700

    Add fieldtrial testing config for "AndroidBookmarkBar".
    This CL adds a fieldtrial testing config for "AndroidBookmarkBar" in
    anticipation of an experiment that will turn on the Android bookmark bar
    feature for the desktop form factor on Android.
    
    Note that "Appearance" settings are also turned on as that is the new
    preference screen which hosts the bookmark bar visibility toggle.
    
    Bug: 347632437
    Change-Id: I05cfff6f99767070f17e6aa7e2e3032da74741b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6422712
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Feels: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441234}

--- chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
@@ -241,11 +241,13 @@ public class MainSettingsFragmentTest {
                 mSettingsActivityTestRule
                         .getActivity()
                         .findViewById(R.id.account_management_account_row);
+        ChromeRenderTestRule.sanitize(accountRow);
         mRenderTestRule.render(accountRow, "main_settings_signed_out_account");
         View googleServicesRow =
                 mSettingsActivityTestRule
                         .getActivity()
                         .findViewById(R.id.account_management_google_services_row);
+        ChromeRenderTestRule.sanitize(googleServicesRow);
         mRenderTestRule.render(googleServicesRow, "main_settings_signed_out_google_services");
     }
 
@@ -261,6 +263,7 @@ public class MainSettingsFragmentTest {
                 mSettingsActivityTestRule
                         .getActivity()
                         .findViewById(R.id.account_management_account_row);
+        ChromeRenderTestRule.sanitize(accountRow);
         mRenderTestRule.render(accountRow, "main_settings_signin_disabled_by_policy_account");
     }
 
@@ -547,6 +550,7 @@ public class MainSettingsFragmentTest {
                         .getActivity()
                         .findViewById(android.R.id.content)
                         .getRootView();
+        ChromeRenderTestRule.sanitize(view);
         mRenderTestRule.render(view, "main_settings_signed_in_identity_error");
     }
 
