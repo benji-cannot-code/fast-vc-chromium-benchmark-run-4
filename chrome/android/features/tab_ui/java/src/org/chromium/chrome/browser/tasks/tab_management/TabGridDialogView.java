59de3f2d08f5bf8220a4f5a1ae5026ff88c0edf9
Ritika Gupta <ritikagup@google.com>
2025-04-01 00:09:34 -0700

    [Data Sharing] Join only flow, checking data sharing functionality correctly.
    In this CL, instead of checking data sharing flag, also checking
    join only flag to see if data sharing feature is enabled or not.
    Fixed the tests as well.
    
    Bug: 377366460
    Change-Id: I20167c52e800bf7cae67eb80beca809b4c80cc74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416182
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440742}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
@@ -43,7 +43,6 @@ import androidx.core.widget.ImageViewCompat;
 import org.chromium.base.Callback;
 import org.chromium.base.MathUtils;
 import org.chromium.base.ResettersForTesting;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.hub.RoundedCornerAnimatorUtil;
 import org.chromium.chrome.browser.tab_ui.TabThumbnailView;
 import org.chromium.chrome.tab_ui.R;
@@ -380,8 +379,7 @@ public class TabGridDialogView extends FrameLayout {
             if (view == TabGridDialogView.this) {
                 continue;
             }
-            // TODO(b/395153186) : Investigate and see if any changes needed for join only flow.
-            if (!ChromeFeatureList.isEnabled(ChromeFeatureList.DATA_SHARING)) {
+            if (!TabUiUtils.isDataSharingFunctionalityEnabled()) {
                 assert mAccessibilityImportanceMap.containsKey(view);
             }
             Integer importance = mAccessibilityImportanceMap.get(view);
