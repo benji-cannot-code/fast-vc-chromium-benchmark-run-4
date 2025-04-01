b5659facedba7066391a9eac0fe2220b6aa679e9
Kaan Alsan <alsan@chromium.org>
2025-04-01 14:11:34 -0700

    Revert "Implement IconLabelBubbleView::CalculateProposedLayout instead of overriding Layout"
    This reverts commit 89e03191575d757c4505d61c584a118926420653.
    
    Reason for revert: Omnibox keywords are misformatted: https://screenshot.googleplex.com/46BRGbAkSPZWdnE
    
    Original change's description:
    > Implement IconLabelBubbleView::CalculateProposedLayout instead of overriding Layout
    >
    > Change-Id: I84f9f1645bb1ab6be063ab19a834a0ba77103e3b
    > Bug: 405440671
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382460
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Kaan Alsan <alsan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1440841}
    
    Bug: 405440671
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibb13c56f4759e8f1ece557dda53b1248c5bb21c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420152
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441179}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
@@ -104,8 +104,7 @@ class IconLabelBubbleView : public views::InkDropObserver,
   void InkDropRippleAnimationEnded(views::InkDropState state) override;
 
   // views::LabelButton:
-  views::ProposedLayout CalculateProposedLayout(
-      const views::SizeBounds& size_bounds) const override;
+  void Layout(PassKey) override;
 
   // Returns true when the label should be visible.
   virtual bool ShouldShowLabel() const;
@@ -182,7 +181,6 @@ class IconLabelBubbleView : public views::InkDropObserver,
   // views::LabelButton:
   gfx::Size CalculatePreferredSize(
       const views::SizeBounds& available_size) const override;
-  gfx::Size GetMinimumSize() const override;
   bool OnMousePressed(const ui::MouseEvent& event) override;
   void OnThemeChanged() override;
   bool IsTriggerableEvent(const ui::Event& event) override;
--- chrome/browser/ui/views/location_bar/icon_label_bubble_view_unittest.cc
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view_unittest.cc
@@ -206,7 +206,6 @@ class TestIconLabelBubbleFakeAnimationView : public TestIconLabelBubbleView {
 
   void SetCurrentAnimationValue(int value) {
     value_ = value;
-    InvalidateLayout();
     SizeToPreferredSize();
   }
 
