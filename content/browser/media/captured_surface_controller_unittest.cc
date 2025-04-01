56afc217122cbab0ce6ced538b71238c6a53be0a
Elad Alon <eladalon@chromium.org>
2025-04-01 05:36:07 -0700

    Remove outdated CSC TODOs
    Bug: 40276312
    Change-Id: I1faf91e18b207a64fe33de318d138d4f4a55d44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421976
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440845}

--- content/browser/media/captured_surface_controller_unittest.cc
+++ content/browser/media/captured_surface_controller_unittest.cc
@@ -1384,7 +1384,6 @@ TEST_P(CapturedSurfaceControllerSendWheelClampTest, ClampMaxWheelDeltaY) {
   run_loop.Run();
 }
 
-// TODO(crbug.com/40276312): Migrate test suite to validate forwardWheel().
 class WebContentsObserverCscNotifiedBySendWheelTest
     : public CapturedSurfaceControllerTestBase {
  public:
