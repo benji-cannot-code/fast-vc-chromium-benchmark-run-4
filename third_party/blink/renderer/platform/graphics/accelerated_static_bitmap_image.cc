3497126172b3e233159654b52ebef669d607cac1
Colin Blundell <blundell@chromium.org>
2025-03-31 23:58:52 -0700

    [Blink] Remove include of SkImage.h from AcceleratedStaticBitmapImage
    It no longer needs it :).
    
    Bug: 371227617
    Change-Id: Id20350b47f4d04f6a84614cbf07bc71d4d407264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416764
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440734}

--- third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.cc
+++ third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/include/core/SkColorType.h"
 #include "third_party/skia/include/core/SkImage.h"
-#include "third_party/skia/include/core/SkImageInfo.h"
 #include "third_party/skia/include/core/SkSamplingOptions.h"
 #include "third_party/skia/include/gpu/ganesh/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/ganesh/GrDirectContext.h"
