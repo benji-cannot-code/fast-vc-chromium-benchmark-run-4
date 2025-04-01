de2f76373ac385f9130c186f21ae62c8f613fb2f
Abe Boujane <boujane@google.com>
2025-04-01 08:53:41 -0700

    Remove Feature from Notice Constructor
    Change-Id: Ia421f877bee89617deeaccb71ed97931f0c24193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419104
    Reviewed-by: Aashna Sheth <aashnas@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440955}

--- chrome/browser/privacy_sandbox/notice/notice_model.h
+++ chrome/browser/privacy_sandbox/notice/notice_model.h
@@ -39,7 +39,7 @@ using NoticeId = std::pair<notice::mojom::PrivacySandboxNotice, SurfaceType>;
 class Notice {
   // TODO(crbug.com/392612108): Include view group information.
  public:
-  explicit Notice(NoticeId notice_id, const base::Feature*);
+  explicit Notice(NoticeId notice_id);
   Notice(const Notice& other);
   virtual ~Notice();
 
@@ -47,6 +47,7 @@ class Notice {
   // notice.
   Notice* SetTargetApis(const std::vector<NoticeApi*>& apis);
   Notice* SetPreReqApis(const std::vector<NoticeApi*>& apis);
+  Notice* SetFeature(const base::Feature* feature);
 
   bool WasFulfilled();
 
@@ -85,7 +86,7 @@ class Notice {
 
 class Consent : public Notice {
  public:
-  explicit Consent(NoticeId notice_id, const base::Feature* feature);
+  explicit Consent(NoticeId notice_id);
   NoticeType GetNoticeType() override;
 
  private:
@@ -141,9 +142,8 @@ class NoticeCatalog {
   // classes need access to the template implementation source.
   // Registers a new notice.
   template <typename T>
-  Notice* RegisterAndRetrieveNewNotice(NoticeId notice_id,
-                                       const base::Feature* feature) {
-    notices_.emplace(notice_id, std::make_unique<T>(T(notice_id, feature)));
+  Notice* RegisterAndRetrieveNewNotice(NoticeId notice_id) {
+    notices_.emplace(notice_id, std::make_unique<T>(T(notice_id)));
     return notices_[notice_id].get();
   }
 
@@ -154,8 +154,9 @@ class NoticeCatalog {
       std::vector<std::pair<NoticeId, const base::Feature*>>&& notice_ids,
       std::vector<NoticeApi*>&& target_apis,
       std::vector<NoticeApi*>&& pre_req_apis = {}) {
-    for (auto notice_id : notice_ids) {
-      RegisterAndRetrieveNewNotice<T>(notice_id.first, notice_id.second)
+    for (auto [notice_id, feature] : notice_ids) {
+      RegisterAndRetrieveNewNotice<T>(notice_id)
+          ->SetFeature(feature)
           ->SetTargetApis(target_apis)
           ->SetPreReqApis(pre_req_apis);
     }
