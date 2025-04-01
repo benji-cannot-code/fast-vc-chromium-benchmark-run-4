c61bc8e6637e63191fd7f5f6cb4ff0536b04f8cc
Colin Blundell <blundell@chromium.org>
2025-04-01 00:03:05 -0700

    [Blink] Have ZeroCopyRBP take in SII in SW constructor
    It doesn't need the LayerTreeSinkImpl.
    
    Bug: 403372453
    Change-Id: I1cafdfe9c9bd4e5eaec8ccd6da7e686061d31c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416964
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440741}

--- cc/raster/raster_buffer_provider_unittest.cc
+++ cc/raster/raster_buffer_provider_unittest.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/fake_layer_tree_frame_sink.h"
 #include "cc/test/fake_raster_source.h"
 #include "cc/tiles/tile_task_manager.h"
+#include "cc/trees/layer_tree_frame_sink.h"
 #include "cc/trees/raster_capabilities.h"
 #include "components/viz/client/client_resource_provider.h"
 #include "components/viz/common/resources/platform_color.h"
@@ -52,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "gpu/command_buffer/client/raster_implementation_gles.h"
 #include "gpu/command_buffer/client/raster_interface.h"
+#include "gpu/ipc/client/client_shared_image_interface.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/axis_transform2d.h"
 #include "url/gurl.h"
@@ -202,7 +204,8 @@ class RasterBufferProviderTest
         CreateSoftwareResourceProvider();
         raster_buffer_provider_ =
             std::make_unique<ZeroCopyRasterBufferProvider>(
-                layer_tree_frame_sink_.get(), raster_caps);
+                layer_tree_frame_sink_.get()->shared_image_interface(),
+                raster_caps);
         break;
     }
 
