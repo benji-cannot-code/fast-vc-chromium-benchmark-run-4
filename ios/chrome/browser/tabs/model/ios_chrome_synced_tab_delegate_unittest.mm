307530cf9c519ae926dfad3c60678fe35c258426
Marc Treib <treib@chromium.org>
2025-04-02 01:57:20 -0700

    [iOS] Apply sync-tabs-since-signin restriction only when necessary
    https://crrev.com/c/5797408 added a restriction that for managed
    accounts, only those tabs that have been updated since signin should be
    synced. This CL limits the restriction to only apply within the personal
    profile. In managed profiles, there's no need, since signin happens
    right at profile creation, so no earlier tabs can exist.
    
    The main point of this is to make clearer when this code can be cleaned
    up again: Once all managed users have been migrated into their own
    profiles.
    
    Bug: 359385827
    Change-Id: Ic80a322cbbcc0d11466491ba2f957aa8c2e84d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6417082
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Feels: Marc Treib <treib@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441405}

--- ios/chrome/browser/tabs/model/ios_chrome_synced_tab_delegate_unittest.mm
+++ ios/chrome/browser/tabs/model/ios_chrome_synced_tab_delegate_unittest.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sessions/model/ios_chrome_session_tab_helper.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/fake_authentication_service_delegate.h"
@@ -148,12 +149,14 @@ TEST_F(IOSChromeSyncedTabDelegateTest,
   IOSChromeScopedTestingLocalState scoped_testing_local_state;
 
   // Create a BrowserState with the necessary services.
+  TestProfileManagerIOS profile_manager;
   TestProfileIOS::Builder builder;
   builder.AddTestingFactory(
       AuthenticationServiceFactory::GetInstance(),
       AuthenticationServiceFactory::GetFactoryWithDelegate(
           std::make_unique<FakeAuthenticationServiceDelegate>()));
-  std::unique_ptr<TestProfileIOS> profile = std::move(builder).Build();
+  TestProfileIOS* profile =
+      profile_manager.AddProfileWithBuilder(std::move(builder));
 
   const base::Time pre_signin_time = base::Time::Now();
 
@@ -164,7 +167,7 @@ TEST_F(IOSChromeSyncedTabDelegateTest,
           GetApplicationContext()->GetSystemIdentityManager());
   system_identity_manager->AddIdentity(identity);
   AuthenticationService* authentication_service =
-      AuthenticationServiceFactory::GetForProfile(profile.get());
+      AuthenticationServiceFactory::GetForProfile(profile);
   authentication_service->SignIn(identity,
                                  signin_metrics::AccessPoint::kUnknown);
 
@@ -178,7 +181,7 @@ TEST_F(IOSChromeSyncedTabDelegateTest,
 
   // Create a WebState aka "a tab" plus the necessary helpers.
   web::FakeWebState web_state;
-  web_state.SetBrowserState(profile.get());
+  web_state.SetBrowserState(profile);
   web_state.SetNavigationManager(std::move(navigation_manager));
   web_state.SetNavigationItemCount(1);
   IOSChromeSessionTabHelper::CreateForWebState(&web_state);
