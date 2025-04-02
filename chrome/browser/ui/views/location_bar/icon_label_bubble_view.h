2754524f6ab577f3c2556919f0ccc1245d0aa6f1
Kaan Alsan <alsan@chromium.org>
2025-04-02 12:54:37 -0700

    Reland "Implement IconLabelBubbleView::CalculateProposedLayout instead of overriding Layout"
    This is a reland of commit 89e03191575d757c4505d61c584a118926420653
    
    The issue with original CL is that the new `CalculateProposedLayout`
    method was using `ShouldShowLabel` to determine whether the label should
    be visible. This would inadvertently hide the label when the text is
    empty. The `SelectedKeywordView` child class sometimes makes the text
    empty, but expects the label's visibility to remain.
    
    Original change's description:
    > Implement IconLabelBubbleView::CalculateProposedLayout instead of overriding Layout
    >
    > Change-Id: I84f9f1645bb1ab6be063ab19a834a0ba77103e3b
    > Bug: 405440671
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382460
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Kaan Alsan <alsan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1440841}
    
    Bug: 405440671
    Change-Id: I8c8780065f1666047b380d2b05b63b6c10bd6a51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423155
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441748}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
@@ -104,7 +104,8 @@ class IconLabelBubbleView : public views::InkDropObserver,
   void InkDropRippleAnimationEnded(views::InkDropState state) override;
 
   // views::LabelButton:
-  void Layout(PassKey) override;
+  views::ProposedLayout CalculateProposedLayout(
+      const views::SizeBounds& size_bounds) const override;
 
   // Returns true when the label should be visible.
   virtual bool ShouldShowLabel() const;
@@ -181,6 +182,7 @@ class IconLabelBubbleView : public views::InkDropObserver,
   // views::LabelButton:
   gfx::Size CalculatePreferredSize(
       const views::SizeBounds& available_size) const override;
+  gfx::Size GetMinimumSize() const override;
   bool OnMousePressed(const ui::MouseEvent& event) override;
   void OnThemeChanged() override;
   bool IsTriggerableEvent(const ui::Event& event) override;
