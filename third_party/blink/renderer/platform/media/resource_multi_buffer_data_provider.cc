3c7b1d207c9cf7a1a7c8e6d3732e51d356009797
Dale Curtis <dalecurtis@chromium.org>
2025-04-02 13:02:21 -0700

    Add Media.Network.TotalBytesReceived.SRC histogram.
    This adds a new histogram to help us track if the
    MultiBufferNeverDefer experiment leads to increased network usage.
    
    A couple of generic network histograms exist, but we should have
    a media specific one to reduce noise.
    
    R=rkaplow
    
    Bug: 41161335
    Change-Id: Ibe8d995cfaf1d431ac12afcd77b411ced551ef0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426622
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441751}

--- third_party/blink/renderer/platform/media/resource_multi_buffer_data_provider.cc
+++ third_party/blink/renderer/platform/media/resource_multi_buffer_data_provider.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/location.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -71,7 +72,10 @@ ResourceMultiBufferDataProvider::ResourceMultiBufferDataProvider(
   DCHECK_GE(pos, 0);
 }
 
-ResourceMultiBufferDataProvider::~ResourceMultiBufferDataProvider() = default;
+ResourceMultiBufferDataProvider::~ResourceMultiBufferDataProvider() {
+  base::UmaHistogramCustomCounts("Media.Network.TotalBytesReceived.SRC",
+                                 total_bytes_received_, 1024, 1073741824, 100);
+}
 
 void ResourceMultiBufferDataProvider::Start() {
   DVLOG(1) << __func__ << " @ " << byte_pos();
@@ -388,6 +392,8 @@ void ResourceMultiBufferDataProvider::DidReceiveData(
   DCHECK(!Available());
   DCHECK(active_loader_);
   DCHECK_GT(data.size(), 0u);
+
+  total_bytes_received_ += data.size();
 
   auto bytes_data = base::as_bytes(data);
   if (bytes_to_discard_) {
