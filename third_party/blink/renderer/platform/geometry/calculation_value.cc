b3cc3bc5db16dc4c6419a5d2200b1e3f9abb4840
David Baron <dbaron@chromium.org>
2025-04-02 11:23:05 -0700

    Make ValueForLength (in svg_length_functions) skip interesting calc-size() values.
    This makes the IsSpecified check in ValueForLength skip interesting
    calc-size() values the same way that it checks for
    non-pixel/percent/calc values.
    
    Prior to this change, the added test fails a CHECK().
    
    Fixed: 406530491
    Change-Id: I14dc498076c729c6c34e56909ec8a70f8e938ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6422172
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441654}

--- third_party/blink/renderer/platform/geometry/calculation_value.cc
+++ third_party/blink/renderer/platform/geometry/calculation_value.cc
@@ -202,4 +202,12 @@ bool CalculationValue::HasFitContent() const {
          data_.expression->HasFitContent();
 }
 
+bool CalculationValue::HasOnlyFixedAndPercent() const {
+  if (!IsExpression()) {
+    return true;
+  }
+  return !data_.expression->HasAutoOrContentOrIntrinsicSize() &&
+         !data_.expression->HasStretch();
+}
+
 }  // namespace blink
