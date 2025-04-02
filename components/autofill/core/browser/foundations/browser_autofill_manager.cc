f1aea948004488234f52e25b63494201acf21361
Christoph Schwering <schwering@google.com>
2025-04-02 05:52:33 -0700

    [Autofill] Block non-AI suggestions on "fillable" fields
    Autofill AI considers a field "fillale" if it has an entity that
    can serve field's Autofill AI type.
    
    That does not imply that there's actually a value to write into the
    field.
    
    For example, suppose there's 1 passport on file with a NAME_FULL
    but without NAME_MIDDLE, and 1 address with NAME_MIDDLE. If a field
    has the Autofill AI type PASSPORT_NAME_TAG and the classical type
    NAME_MIDDLE, the field is "fillable" yet there's no value to fill.
    
    This CL fixes that in this case, Autofill shows an address suggestion
    but doesn't fill it. After this CL, no suggestion is shown.
    
    Bug: 402397312
    Change-Id: If098d8e6a9caca5294460fd5b37eb8bcffcf19c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421637
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441460}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_type_utils.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/filling/addresses/field_filling_address_util.h"
+#include "components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util.h"
 #include "components/autofill/core/browser/filling/field_filling_skip_reason.h"
 #include "components/autofill/core/browser/filling/filling_product.h"
 #include "components/autofill/core/browser/filling/form_autofill_history.h"
@@ -1184,7 +1185,10 @@ void BrowserAutofillManager::GenerateSuggestionsAndMaybeShowUIPhase2(
     return;
   }
   AutofillAiDelegate* delegate = client().GetAutofillAiDelegate();
-  if (!autofill_ai_suggestions.empty()) {
+  if (form_structure && autofill_field &&
+      MayPerformAutofillAiAction(client(), AutofillAiAction::kFilling) &&
+      GetFieldsFillableByAutofillAi(*form_structure, client())
+          .contains(autofill_field->global_id())) {
     std::move(callback).Run(/*show_suggestions=*/true,
                             std::move(autofill_ai_suggestions),
                             /*ranking_context=*/std::nullopt);
