ef143777eb5970fc308422e801196997c6ed5778
Christian Xu <christianxu@google.com>
2025-04-02 11:47:20 -0700

    [iOS][OR][2/N] Refactor return key handling
    Context: OmniboxReturnDelegate is being refactored into
    OmniboxKeyboardDelegate.
    
    - Adds `contentConfigurationAtIndexPath` to simplify content configuration access.
    - Creates `canPerformReturnKeyAction` and `performReturnKeyAction` that will be used with OmniboxKeyboardDelegate in a later CL.
    
    Bug: 402392448
    Change-Id: Iea148d3100dde56685a8e0751c56c91d2198501c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418908
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441689}

--- ios/chrome/browser/omnibox/public/omnibox_ui_features.h
+++ ios/chrome/browser/omnibox/public/omnibox_ui_features.h
@@ -8,4 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/feature_list.h"
 
+/// TODO(crbug.com/388820891): Update milestone after refactoring is complete.
+const base::NotFatalUntil kOmniboxRefactoringNotFatalUntil =
+    base::NotFatalUntil::M200;
+
 #endif  // IOS_CHROME_BROWSER_OMNIBOX_PUBLIC_OMNIBOX_UI_FEATURES_H_
