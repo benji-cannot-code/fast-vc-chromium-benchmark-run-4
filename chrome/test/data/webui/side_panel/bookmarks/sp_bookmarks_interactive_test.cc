431ca668ccbd3881772096c7e799390246214b8b
Alison Gale <agale@chromium.org>
2025-04-02 12:53:26 -0700

    Shrink bookmarks list test to reduce debug timeouts
    Bug: 407730007
    Change-Id: I7bb6f9064a2671e27adadde799fca5ff222527b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426350
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441746}

--- chrome/test/data/webui/side_panel/bookmarks/sp_bookmarks_interactive_test.cc
+++ chrome/test/data/webui/side_panel/bookmarks/sp_bookmarks_interactive_test.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/strings/stringprintf.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "components/power_bookmarks/core/power_bookmark_features.h"
@@ -12,6 +13,15 @@ class SidePanelBookmarksTest : public WebUIMochaFocusTest {
  protected:
   SidePanelBookmarksTest() {
     set_test_loader_host(chrome::kChromeUIBookmarksSidePanelHost);
+  }
+};
+
+class SidePanelBookmarksListTest : public SidePanelBookmarksTest {
+ protected:
+  void RunTestSuite(const std::string& suiteName) {
+    SidePanelBookmarksTest::RunTest(
+        "side_panel/bookmarks/power_bookmarks_list_test.js",
+        base::StringPrintf("runMochaSuite('%s');", suiteName.c_str()));
   }
 };
 
@@ -36,8 +46,16 @@ IN_PROC_BROWSER_TEST_F(SidePanelPowerBookmarksTest, EditDialog) {
           "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(SidePanelPowerBookmarksTest, List) {
-  RunTest("side_panel/bookmarks/power_bookmarks_list_test.js", "mocha.run()");
+IN_PROC_BROWSER_TEST_F(SidePanelBookmarksListTest, General) {
+  RunTestSuite("General");
+}
+
+IN_PROC_BROWSER_TEST_F(SidePanelBookmarksListTest, TransportMode) {
+  RunTestSuite("TransportMode");
+}
+
+IN_PROC_BROWSER_TEST_F(SidePanelBookmarksListTest, TreeView) {
+  RunTestSuite("TreeView");
 }
 
 IN_PROC_BROWSER_TEST_F(SidePanelPowerBookmarksTest, Service) {
