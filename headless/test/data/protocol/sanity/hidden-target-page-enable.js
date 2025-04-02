2ecf89ecda815c6a81054cde39e42d6fd7ed920c
Maksim Sadym <sadym@google.com>
2025-04-02 04:51:47 -0700

    Create hidden targets via CDP `Target.CreateTarget`
    Add a "hidden" boolean flag to `Target.CreateTarget`. It is intended to
    be used for creating hidden WebContents to run WebDriver BiDi wrapper.
    The hidden target is not added to the tab strip, but is observable via
    CDP.
    
    Based on https://crrev.com/c/6012560.
    Design doc: http://go/webdriver:hide-mapper-tab-dd
    
    Bug: 378635448
    Change-Id: I77f7949101b0a1545d52998789d7e60e90f74511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330754
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Maksim Sadym <sadym@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441443}

---
+++ headless/test/data/protocol/sanity/hidden-target-page-enable.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  await testRunner.startBlank(`Test page enable in hidden target`);
+
+  function testRunnerLog() {
+    testRunner.log(
+        arguments[0] ?? '', arguments[1] ?? '', testRunner.stabilizeNames,
+        ['sessionId', 'targetId']);
+  }
+
+
+  testRunnerLog('Create hidden target');
+  const {result: hiddenTarget} =
+      await testRunner.browserP().Target.createTarget({
+        url: 'about:blank?HIDDEN=TARGET',
+        hidden: true,
+      });
+
+  testRunner.browserP().Target.attachToTarget(
+      {targetId: hiddenTarget.targetId, flatten: true});
+  const attachedToHiddenTargetEvent =
+      await testRunner.browserP().Target.onceAttachedToTarget();
+  testRunnerLog('Attached to the hidden target');
+
+  const hiddenSession = new TestRunner.Session(
+      testRunner, attachedToHiddenTargetEvent.params.sessionId);
+
+  testRunnerLog('Enable page');
+  testRunnerLog(await hiddenSession.protocol.Page.enable());
+
+  testRunner.completeTest();
+})
