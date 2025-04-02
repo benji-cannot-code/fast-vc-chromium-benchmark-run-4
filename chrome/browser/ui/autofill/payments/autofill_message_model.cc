e864e38054b82b12e1ba6fb2c82268d64bf01078
Qihui Zhao <qihuizhao@google.com>
2025-04-02 06:56:19 -0700

    Update card art to render new FOP display
    This CL updates all static card art from 32x20 to 48x30
    
    Dropdown:
    Before: https://screenshot.googleplex.com/BuAfNiC3e8MjHfq
    After: https://screenshot.googleplex.com/8d654rsVKb4TtAn
    
    Settings page:
    Before: https://screenshot.googleplex.com/9K2sKqyEpSmHxPG
    After: https://screenshot.googleplex.com/6ckhfNs6Yj8Wc2X
    
    Note: The newly added images are a reasonable reason to expect the APK size went up, and it should go back down when the launch is complete.
    
    Bug: b:401602593
    Change-Id: Ie46ebe235ece9e1d44732a2672c0f568742ce7ae
    
    Binary-Size: Added a new library.
    Binary-Size: Enables a large feature that was previously flagged.
    Change-Id: Ie46ebe235ece9e1d44732a2672c0f568742ce7ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6408014
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441472}

--- chrome/browser/ui/autofill/payments/autofill_message_model.cc
+++ chrome/browser/ui/autofill/payments/autofill_message_model.cc
@@ -39,7 +39,7 @@ AutofillMessageModel::CreateForSaveCardFailure() {
   message->SetDescription(ui_params.description_text);
   message->SetPrimaryButtonText(ui_params.failure_ok_button_text);
   message->SetIconResourceId(
-      ResourceMapper::MapToJavaDrawableId(IDR_AUTOFILL_CC_GENERIC));
+      ResourceMapper::MapToJavaDrawableId(IDR_AUTOFILL_CC_GENERIC_OLD));
 
   return base::WrapUnique(
       new AutofillMessageModel(std::move(message), Type::kSaveCardFailure));
@@ -59,7 +59,7 @@ AutofillMessageModel::CreateForVirtualCardEnrollFailure(
   message->SetDescription(ui_params.description_text);
   message->SetPrimaryButtonText(ui_params.failure_ok_button_text);
   message->SetIconResourceId(
-      ResourceMapper::MapToJavaDrawableId(IDR_AUTOFILL_CC_GENERIC));
+      ResourceMapper::MapToJavaDrawableId(IDR_AUTOFILL_CC_GENERIC_OLD));
 
   return base::WrapUnique(new AutofillMessageModel(
       std::move(message), Type::kVirtualCardEnrollFailure));
