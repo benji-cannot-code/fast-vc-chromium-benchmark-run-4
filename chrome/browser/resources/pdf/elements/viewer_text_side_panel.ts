7f30c1f4862f90633c1c3c10c5f3832081bbae88
rbpotter <rbpotter@chromium.org>
2025-04-02 13:42:22 -0700

    [PDF Ink Signatures] Refactor style selector into dedicated element
    This is in preparation for adding a bottom toolbar, which will also
    need to have this set of buttons.
    
    Bug: 402546154
    Change-Id: Icf05a1fe45e851a8cc1124070fd7c3e99a0dd399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420499
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441780}

--- chrome/browser/resources/pdf/elements/viewer_text_side_panel.ts
+++ chrome/browser/resources/pdf/elements/viewer_text_side_panel.ts
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {EventTracker} from 'chrome://resources/js/event_tracker.js';
 import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
-import type {AnnotationText, Color, TextStyles} from '../constants.js';
-import {TextAlignment, TextStyle} from '../constants.js';
+import type {AnnotationText, Color} from '../constants.js';
+import {TextAlignment} from '../constants.js';
 import {Ink2Manager} from '../ink2_manager.js';
 import {hexToColor} from '../pdf_viewer_utils.js';
 
@@ -68,7 +68,6 @@ export class ViewerTextSidePanelElement extends CrLitElement {
       currentColor_: {type: Object},
       currentFont_: {type: String},
       currentSize_: {type: Number},
-      currentStyles_: {type: Object},
       colors_: {type: Array},
       fonts_: {type: Array},
       sizes_: {type: Array},
@@ -79,12 +78,6 @@ export class ViewerTextSidePanelElement extends CrLitElement {
   protected currentColor_: Color = hexToColor(TEXT_COLORS[0]!.color);
   protected currentFont_: string = '';
   protected currentSize_: number = TEXT_SIZES[0]!;
-  protected currentStyles_: TextStyles = {
-    [TextStyle.BOLD]: false,
-    [TextStyle.ITALIC]: false,
-    [TextStyle.UNDERLINE]: false,
-    [TextStyle.STRIKETHROUGH]: false,
-  };
 
   protected colors_ = TEXT_COLORS;
   protected fonts_ = [
@@ -115,10 +108,6 @@ export class ViewerTextSidePanelElement extends CrLitElement {
     this.tracker_.removeAll();
   }
 
-  protected getTextStyles_(): TextStyle[] {
-    return Object.values(TextStyle);
-  }
-
   protected isSelectedFont_(font: string) {
     return font === this.currentFont_;
   }
@@ -135,21 +124,6 @@ export class ViewerTextSidePanelElement extends CrLitElement {
   protected onSizeChange_(e: Event) {
     const newValue = Number((e.target as HTMLSelectElement).value);
     Ink2Manager.getInstance().setTextSize(newValue);
-  }
-
-  protected onStyleButtonClick_(e: Event) {
-    const style = (e.target as HTMLElement).dataset['style'] as TextStyle;
-    const newStyles = Object.assign({}, this.currentStyles_);
-    newStyles[style] = !newStyles[style];
-    Ink2Manager.getInstance().setTextStyles(newStyles);
-  }
-
-  protected getActiveClass_(style: TextStyle) {
-    return this.currentStyles_[style] ? 'active' : '';
-  }
-
-  protected getAriaPressed_(style: TextStyle) {
-    return this.currentStyles_[style] ? 'true' : 'false';
   }
 
   protected onSelectedAlignmentChanged_(e: CustomEvent<{value: string}>) {
@@ -171,7 +145,6 @@ export class ViewerTextSidePanelElement extends CrLitElement {
     this.currentColor_ = text.color;
     this.currentFont_ = text.font;
     this.currentSize_ = text.size;
-    this.currentStyles_ = text.styles;
     this.currentAlignment_ = text.alignment;
   }
 }
