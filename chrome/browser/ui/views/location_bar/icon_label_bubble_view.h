bd5858d93c3646371d2efbf86b98a0c0f5bab809
Kaan Alsan <alsan@chromium.org>
2025-04-02 15:37:47 -0700

    Add option for IconLabelBubbleView to set additional padding on expanded labels
    This CL adds the option of applying custom padding to
    IconLabelBubbleView's icon and label when the label is expanded,
    allowing clients to apply their own insets without having to worry about
    updating insets when the label is hidden or collapsed due to space
    restrictions.
    
    Change-Id: Id1f01c463777f5adc434dacf8636f792b9aea26e
    Bug: 403333986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6410431
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441843}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/ink_drop_observer.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/layout/flex_layout_types.h"
 #include "ui/views/widget/widget_observer.h"
 
 namespace gfx {
@@ -119,6 +120,10 @@ class IconLabelBubbleView : public views::InkDropObserver,
   void SetLabel(std::u16string_view label);
   void SetLabel(std::u16string_view label, std::u16string_view accessible_name);
   void SetFontList(const gfx::FontList& font_list);
+
+  // Applies additional padding around the icon and label whenever the label
+  // is visible and not collapsed.
+  void SetExpandedLabelAdditionalInsets(const views::Inset1D& insets);
 
   gfx::RoundedCornersF GetCornerRadii() const;
   void SetCornerRadii(const gfx::RoundedCornersF& radii);
@@ -336,6 +341,9 @@ class IconLabelBubbleView : public views::InkDropObserver,
   std::optional<ui::ColorId> foreground_color_id_;
 
   std::optional<gfx::RoundedCornersF> radii_;
+
+  // Padding that should be applied when the label is expanded.
+  views::Inset1D expanded_label_additional_insets_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_ICON_LABEL_BUBBLE_VIEW_H_
