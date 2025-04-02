11015e0f2442d64145aa0bfa3352b444247d20c5
Jérôme Lebel <jlebel@chromium.org>
2025-04-01 19:38:22 -0700

    [iOS] The bottom should not be opened when the user is signed in
    In crbug.com/407430698, AuthenticationFlow tries to sign-in the user
    with the same identity than the primary identity. According to the
    breadcrumbs, the bottom sheet was opened just before.
    The bottom sheet should not be opened if the user has a primary
    identity. To understand which UI opens the bottom sheet while there is
    a primary identity, CHECK is added in -[ConsistencyPromoSigninCoordinator
    start], to make sure there is no primary identity.
    
    Once this CHECK will fail, it should be easy to fix
    crbug.com/407430698
    
    Bug: 407430698
    Change-Id: I17b750f0df92b2634f81f667e84a6405e643909e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419492
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441322}

--- ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -126,6 +126,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ProfileIOS* profile = self.profile;
   signin::IdentityManager* identityManager =
       IdentityManagerFactory::GetForProfile(profile);
+  // The sign-in bottom sheet should not be opened if the user is already signed
+  // in. This is related to crbug.com/407430698.
+  CHECK(!identityManager->HasPrimaryAccount(signin::ConsentLevel::kSignin),
+        base::NotFatalUntil::M142);
   AccountReconcilor* accountReconcilor =
       ios::AccountReconcilorFactory::GetForProfile(profile);
   ChromeAccountManagerService* accountManagerService =
