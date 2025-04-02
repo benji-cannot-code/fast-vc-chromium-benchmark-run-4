514323d7ac68c084637acdca82757d1810152805
Gloria Fang <gloriafang@google.com>
2025-04-02 16:11:19 -0700

    Show all 4 cases for product/favicon image presence/absence.
    screenshots:
    - favicon only: screen/BedLB7p2gCceSwF
    - favicon + product: screen/iirEa2d5tAQiq9X
    - product only: screen/TjgSFUGZhvjNKQm
    - none: screen/96AfFox9jmpr62B
    - dark mode: screen/5WQvepiJVG8MEiw
    
    Bug: 392970898
    Change-Id: I076a3fc74181b888be2f2cf4f60c11ef093262c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423775
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Auto-Submit: Gloria Fang <gloriafang@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441862}

--- ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_mediator.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_mediator.mm
@@ -232,17 +232,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                 length:imageData.size()];
   if (data) {
     self->_shopCardItem.shopCardData.productImage = data;
-
-    // Fetch favicon if product image available.
-    // TODO(crbug.com/392970898): add support for no product image case
-    __weak ShopCardMediator* weakSelf = self;
-    _faviconLoader->FaviconForPageUrl(
-        productUrl, kDesiredSmallFaviconSizePt, kMinFaviconSizePt,
-        /*fallback_to_google_server=*/false, ^(FaviconAttributes* attributes) {
-          [weakSelf onFaviconReceived:attributes];
-        });
   }
   [self.delegate insertShopCard];
+
+  // Fetch favicon, regardless of whether product image available.
+  __weak ShopCardMediator* weakSelf = self;
+  _faviconLoader->FaviconForPageUrl(
+      productUrl, kDesiredSmallFaviconSizePt, kMinFaviconSizePt,
+      /*fallback_to_google_server=*/false, ^(FaviconAttributes* attributes) {
+        [weakSelf onFaviconReceived:attributes];
+      });
 }
 
 - (void)onFaviconReceived:(FaviconAttributes*)attributes {
