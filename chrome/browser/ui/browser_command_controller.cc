2d584c050bef8b09d1477e06fd4f860987f6ab3a
Dana Fried <dfried@chromium.org>
2025-04-02 12:03:42 -0700

    [Extensions] Collapse app menu submenu when no manageable extensions
    This changes the "extensions" submenu in the main menu to be a single
    "find extensions" entry when no extensions would be shown on the
    chrome://extensions page.
    
    In this case, the "extensions" submenu would otherwise only contain
    two items:
     - Manage extensions (leads to a blank page)
     - Visit Chrome Web Store (confusing to new users)
    
    A separate browser command and UMA metrics are added for the new menu
    item in order to be able to see the difference in click-through rates
    between the two options.
    
    This change is behind a flag that is disabled by default.
    
    NO_IFTTT=Commands are not gated on fenced frame, etc.
    
    Bug: 407733524
    Change-Id: Ifb5556d20d2f8735fb13fe78c0afb1fb4dc4499e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418443
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Feels: Dana Fried <dfried@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441714}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -921,6 +921,7 @@ bool BrowserCommandController::ExecuteCommandWithDisposition(
       ShowExtensions(browser_->GetBrowserForOpeningWebUi());
       break;
     case IDC_EXTENSIONS_SUBMENU_VISIT_CHROME_WEB_STORE:
+    case IDC_EXPLORE_EXTENSIONS:
       ShowWebStore(browser_, extension_urls::kAppMenuUtmSource);
       break;
     case IDC_PERFORMANCE:
@@ -1618,6 +1619,8 @@ void BrowserCommandController::UpdateCommandsForExtensionsMenu() {
       /*state=*/true);
   command_updater_.UpdateCommandEnabled(
       IDC_EXTENSIONS_SUBMENU_VISIT_CHROME_WEB_STORE, /*state=*/true);
+  command_updater_.UpdateCommandEnabled(IDC_EXPLORE_EXTENSIONS,
+                                        /*state=*/true);
 }
 
 void BrowserCommandController::UpdateCommandsForTabState() {
