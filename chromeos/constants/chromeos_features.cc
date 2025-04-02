4ae944d733d6ada7d175cb9d955ff3201a39b823
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-04-02 15:43:56 -0700

    Allow to control feature kDisableSystemBlur via chrome://flags
    Bug: b:407308128
    Change-Id: I9d7f1f3f56c9a2469cb575dc3cc0d1f08657933f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423510
    Reviewed-by: Vincent Chiang <vincentchiang@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Feels: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441846}

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -541,7 +541,8 @@ bool IsRoundedWindowsEnabled() {
 }
 
 bool IsSystemBlurEnabled() {
-  return !base::FeatureList::IsEnabled(kDisableSystemBlur);
+  static bool disable_blur = base::FeatureList::IsEnabled(kDisableSystemBlur);
+  return !disable_blur;
 }
 
 bool IsPkcs12ToChapsDualWriteEnabled() {
