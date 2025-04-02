c2e884403e17b5428f93dbc38cd4426220ee065e
Calder Kitagawa <ckitagawa@chromium.org>
2025-04-02 10:40:52 -0700

    [Tab Switcher] Default enable foreign favicon fetching
    Bug: 407981306
    Change-Id: I864dcceee979ee8da6b08602674827128b18e2a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426685
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441600}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1164,7 +1164,7 @@ BASE_FEATURE(kTabSwitcherColorBlendAnimate,
 
 BASE_FEATURE(kTabSwitcherForeignFaviconSupport,
              "TabSwitcherForeignFaviconSupport",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTabWindowManagerReportIndicesMismatch,
              "TabWindowManagerReportIndicesMismatch",
