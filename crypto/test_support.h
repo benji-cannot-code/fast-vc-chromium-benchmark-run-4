0bc64a76900246acc956df082cf8674a6a956467
Elly <ellyjones@chromium.org>
2025-04-02 13:12:55 -0700

    crypto: add new sign/verify interface
    Goals for these interfaces are:
    * Unify RSAPrivateKey with ECPrivateKey
    * Unify ECSignatureCreator with SignatureCreator
    * Support the same methods for signing and verification
    * Do not require clients to ensure passed-in parameters outlive signer /
      verifier instances (ie: simplify a lot of lifetimes)
    * Spanify everything
    * Encourage tests not to expensively generate fresh RSA keys
    * To the extent possible, push clients towards safe paths / configs
    
    This change adds four new classes and a few new free functions:
    
    * crypto::keypair::PrivateKey / crypto::keypair::PublicKey:
    These are both wrappers around EVP_PKEY that can only be constructed
    using special factory functions. They don't have any behavior other than
    wrapping an EVP_PKEY and allowing import/export in various formats. The
    main reason for having two separate types is that EVP_PKEY may contain
    either or both parts of a keypair, so wrapping it in separate
    private/public types moves one kind of mistake from run time to
    compile time.
    
    * crypto::sign::Signer / crypto::sign::Verifier:
    These are streaming interfaces that accept crypto::keypair::PrivateKey /
    crypto::keypair::PublicKey respectively.
    
    * crypto::sign::Sign() / crypto::sign::Verify():
    These are one-shot sign/verify functions. From a quick code skim, it
    seems like almost all uses of the existing SignatureCreator /
    SignatureVerifier classes will actually migrate to these.
    
    * crypto::test::FixedRsaNKeyForTesting():
    This family of functions provides tests with pre-generated RSA keys to
    use for testing, rather than expensively generating fresh ones.
    
    Bug: 406190025
    Change-Id: I82fc6b8627b68ed81577dcbce8b5c6a3f53bb24e
    Thanks: jbroman
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394201
    Feels: David Benjamin <davidben@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Bob Beck <bbe@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441760}

---
+++ crypto/test_support.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CRYPTO_TEST_SUPPORT_H_
+#define CRYPTO_TEST_SUPPORT_H_
+
+#include "crypto/keypair.h"
+
+namespace crypto::test {
+
+using crypto::keypair::PrivateKey;
+using crypto::keypair::PublicKey;
+
+// These create and return PrivateKey instances that wrap fixed, pre-generated
+// private keys for use in tests. Tests should prefer these keys over freshly
+// generating keys whenever practical, since they are much cheaper.
+PrivateKey FixedRsa2048PrivateKeyForTesting();
+PrivateKey FixedRsa4096PrivateKeyForTesting();
+
+PublicKey FixedRsa2048PublicKeyForTesting();
+PublicKey FixedRsa4096PublicKeyForTesting();
+
+}  // namespace crypto::test
+
+#endif  // CRYPTO_TEST_SUPPORT_H_
