dbffe79796fb5752b82b12d1c54c07648c262863
Nick Navarro <npnavarro@chromium.org>
2025-04-02 08:50:20 -0700

    Fix flaky SaveCardBubbleControllerImplTest
    This test suite calls a uitest utility function
    BringBrowserWindowToFront, which is flaky when called in a non-uitest.
    This change should fix the flake by making this test a uitest.
    
    Bug: 402536429
    Change-Id: I1a356b542c224bfe25c1567ce2f492e6dcf2967f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423289
    Commit-Queue: Nick Navarro <npnavarro@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441517}

--- chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl_interactive_uitest.cc
+++ chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl_interactive_uitest.cc
@@ -3,14 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h"
-
 #include <memory>
 
 #include "base/functional/callback_helpers.h"
 #include "base/json/json_reader.h"
 #include "base/memory/raw_ptr.h"
 #include "base/values.h"
+#include "chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/save_card_ui.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
