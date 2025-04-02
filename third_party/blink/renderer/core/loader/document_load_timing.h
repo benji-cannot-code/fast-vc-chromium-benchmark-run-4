92bcb9f54d494c79253af6bbb37930347ed164e1
David Sanders <dsanders11@ucsbalum.com>
2025-04-02 15:46:45 -0700

    Remove unused include in document_load_timing.h
    Also replaces incorrect inclusion of traced_value_forward.h
    
    Bug: 40318405
    Change-Id: Iada44d5221067a5db8e8271a17c248171cf57b7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421696
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441848}

--- third_party/blink/renderer/core/loader/document_load_timing.h
+++ third_party/blink/renderer/core/loader/document_load_timing.h
@@ -35,8 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
-#include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
-#include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
+#include "third_party/perfetto/include/perfetto/tracing/traced_value.h"
 
 namespace base {
 class Clock;
