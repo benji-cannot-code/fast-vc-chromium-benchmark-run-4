0fe02407445a283617829643bffb937cd55db812
CJ Huang <chenjih@google.com>
2025-04-02 04:03:25 -0700

    [launcher search] remove SQL check.
    Remove the SQL check per
    https://chromium.googlesource.com/chromium/src/+/main/sql/README.md#no-checks.
    The function ConvertIndexingSourceToInt() should be sufficient to
    restrict the value in the right range.
    
    Bug: 407930214
    
    Change-Id: If3ba07808e274a6e88fb167bddd3bc58a22ebce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6424681
    Reviewed-by: Xinglong (Alan) Luan <alanlxl@chromium.org>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441434}

--- chrome/browser/ash/app_list/search/local_image_search/inverted_index_table.cc
+++ chrome/browser/ash/app_list/search/local_image_search/inverted_index_table.cc
@@ -16,8 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_list {
 
 namespace {
-// When extra indexing source is added, also update the `CHECK` within the
-// `source` of InvertedIndex table.
+// Only uses this function to convert source into the right value.
 int ConvertIndexingSourceToInt(IndexingSource indexing_source) {
   switch (indexing_source) {
     case IndexingSource::kOcr:
@@ -35,7 +34,7 @@ bool InvertedIndexTable::Create(SqlDatabase* db) {
       "CREATE TABLE inverted_index("
           "term_id INTEGER NOT NULL,"
           "document_id INTEGER NOT NULL,"
-          "source INTEGER NOT NULL CHECK (source IN (0, 1)),"
+          "source INTEGER NOT NULL,"
           "score REAL,"
           "x REAL,"
           "y REAL,"
