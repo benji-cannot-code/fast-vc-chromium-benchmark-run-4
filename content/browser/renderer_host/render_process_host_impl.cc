06903d6786582f486afcb9aee8f060c39180c6cd
Viktoriya Bryhider <vbryhider@microsoft.com>
2025-04-01 17:15:50 -0700

    Prevent reusing hung processes for the same-site frames
    When RendererProcessHost is reused for hosting a new frame, it can be in
    a hung state and new navigation will be hung as well. Check that the
    renderer is not unresponsive when picking a suitable process.
    
    Bug: 40618220
    Include-Ci-Only-Tests: chromium.android:android-15-x64-rel|content_browsertests
    Change-Id: If1f70e9e65d156f3184e5564a9debee130c42fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314112
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Viktoriya Bryhider <vbryhider@microsoft.com>
    Reviewed-by: Liang Zhao <lzhao@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1441282}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1302,6 +1302,18 @@ static RenderProcessHost* FindEmptyBackgroundHostForReuse(
   return nullptr;
 }
 
+bool IsRendererUnresponsive(RenderProcessHost* render_process_host) {
+  bool is_unresponsive = false;
+  render_process_host->ForEachRenderFrameHost(
+      [&is_unresponsive](RenderFrameHost* render_frame_host) {
+        if (render_frame_host->GetRenderWidgetHost()
+                ->IsCurrentlyUnresponsive()) {
+          is_unresponsive = true;
+        }
+      });
+  return is_unresponsive;
+}
+
 }  // namespace
 
 RenderProcessHostImpl::IOThreadHostImpl::IOThreadHostImpl(
@@ -4631,7 +4643,8 @@ bool RenderProcessHostImpl::MayReuseAndIsSuitable(
     const IsolationContext& isolation_context,
     const SiteInfo& site_info) {
   return host->MayReuseHost() &&
-         IsSuitableHost(host, isolation_context, site_info);
+         IsSuitableHost(host, isolation_context, site_info) &&
+         !IsRendererUnresponsive(host);
 }
 
 // static
