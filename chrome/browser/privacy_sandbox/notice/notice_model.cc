9b29c415fcf6fd368fdfe049b4c1ce8da1b1ba3b
Abe Boujane <boujane@google.com>
2025-04-02 08:07:29 -0700

    Reland "Remove Feature from Notice Constructor"
    This reverts commit 987f6205c30f1796bc1fdaa4fba36b76bca668a0.
    
    Reason for revert: Auto Merge Conflict was done incorrectly.
    
    Original change's description:
    > Revert "Remove Feature from Notice Constructor"
    >
    > This reverts commit 0fd8e81aa0f9a9d2af14344aac94c4773d6d7714.
    >
    > Reason for revert: Suspected to cause compile failure: https://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-compile-rel/23575/overview and tree closure
    >
    > Original change's description:
    > > Remove Feature from Notice Constructor
    > >
    > > Change-Id: Ia421f877bee89617deeaccb71ed97931f0c24193
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419104
    > > Reviewed-by: Aashna Sheth <aashnas@google.com>
    > > Commit-Queue: Abe Boujane <boujane@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1440955}
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ib25df3f40aac046d771866737f947a1f86633742
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421745
    > Auto-Submit: Gabrielle Lau <gabriellelau@google.com>
    > Owners-Override: Gabrielle Lau <gabriellelau@google.com>
    > Commit-Queue: Abe Boujane <boujane@google.com>
    > Reviewed-by: Abe Boujane <boujane@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1440969}
    
    Change-Id: If770d5a2d33fcddd69747673188c9f189621e1cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419148
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441494}

--- chrome/browser/privacy_sandbox/notice/notice_model.cc
+++ chrome/browser/privacy_sandbox/notice/notice_model.cc
@@ -59,8 +59,7 @@ bool NoticeApi::IsFulfilled() {
 }
 
 // Notice class definitions.
-Notice::Notice(NoticeId notice_id, const base::Feature* feature)
-    : notice_id_(notice_id), feature_(feature) {}
+Notice::Notice(NoticeId notice_id) : notice_id_(notice_id) {}
 Notice::Notice(const Notice& other) = default;
 Notice::~Notice() = default;
 
@@ -70,6 +69,11 @@ const std::vector<raw_ptr<NoticeApi>>& Notice::GetTargetApis() {
 
 const std::vector<raw_ptr<NoticeApi>>& Notice::GetPreReqApis() {
   return pre_req_apis_;
+}
+
+Notice* Notice::SetFeature(const base::Feature* feature) {
+  feature_ = feature;
+  return this;
 }
 
 Notice* Notice::SetPreReqApis(const std::vector<NoticeApi*>& apis) {
@@ -152,8 +156,7 @@ const std::set<PrivacySandboxNoticeEvent>& Notice::DisablementFulfillEvents() {
 }
 
 // Consent class definitions.
-Consent::Consent(NoticeId notice_id, const base::Feature* feature)
-    : Notice(notice_id, feature) {}
+Consent::Consent(NoticeId notice_id) : Notice(notice_id) {}
 
 NoticeType Consent::GetNoticeType() {
   return NoticeType::kConsent;
