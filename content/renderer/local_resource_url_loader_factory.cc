d16a5f8b1495268a434f8a928ee4ca7b01a32237
Ho Cheung <hocheung@chromium.org>
2025-04-02 07:24:17 -0700

    [content] Cleaned up unnecessary base/notimplemented.h header files from some files
    Just as the title says.
    
    Bug: 41493641
    Change-Id: I9350b19f5ecd8e81539a183f62978eef498598ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420996
    Feels: Ho Cheung <hocheung@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441484}

--- content/renderer/local_resource_url_loader_factory.cc
+++ content/renderer/local_resource_url_loader_factory.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/notimplemented.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
