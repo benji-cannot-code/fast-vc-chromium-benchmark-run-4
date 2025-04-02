45349afec3a53f1e485069a046fe8d74c83bff2e
Kalvin Lee <kdlee@chromium.org>
2025-04-02 08:35:40 -0700

    remoting: Remove `const` qualifier
    `MakeAbsoluteSD()` does not seem to accept a `const` buffer
    (as unearthed in a downstream CL):
    
    ```
    no known conversion from 'const _SECURITY_DESCRIPTOR *' to
    'PSECURITY_DESCRIPTOR' (aka 'void *') for 1st argument
    ```
    
    Use a `const_cast` to solve this. As a drive-by, make several other args
    references, as their usage strongly implies non-nullptr.
    
    Bug: 406029216
    Change-Id: Ic2179240bcb42f19d29e8f730837d31e9fe163c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6399734
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441512}

--- remoting/host/win/com_security.cc
+++ remoting/host/win/com_security.cc
@@ -29,8 +29,8 @@ bool InitializeComSecurity(const std::string& security_descriptor,
   ScopedSid group;
   ScopedSid owner;
   ScopedAcl sacl;
-  if (!MakeScopedAbsoluteSd(relative_sd, &absolute_sd, &dacl, &group, &owner,
-                            &sacl)) {
+  if (!MakeScopedAbsoluteSd(relative_sd, absolute_sd, dacl, group, owner,
+                            sacl)) {
     PLOG(ERROR) << "MakeScopedAbsoluteSd() failed";
     return false;
   }
