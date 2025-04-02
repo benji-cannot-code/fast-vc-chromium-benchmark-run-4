4e86c33c313cef43373f36d1331b59d1914882f0
Sandor Major <sandormajor@chromium.org>
2025-04-01 22:40:12 -0700

    Set ChromeJsErrorReportProcessor-initiated requests to omit credentials.
    The traffic annotation of these requests says "cookies_allowed: NO" so I
    assume they have to be omitted.
    
    Bug: 382291442
    Change-Id: I5df691e888667ff161302e5c6fdf69f42723321d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6417208
    Auto-Submit: Sandor «Alex» Major <sandormajor@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Feels: Troy Wang <troywang@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441356}

--- chrome/browser/error_reporting/chrome_js_error_report_processor_nonchromeos.cc
+++ chrome/browser/error_reporting/chrome_js_error_report_processor_nonchromeos.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/mojom/fetch_api.mojom-shared.h"
 #include "url/gurl.h"
 
 namespace {
@@ -115,6 +116,7 @@ void ChromeJsErrorReportProcessor::SendReport(
   auto resource_request = std::make_unique<network::ResourceRequest>();
   resource_request->method = "POST";
   resource_request->url = url;
+  resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
 
   const auto traffic_annotation =
       net::DefineNetworkTrafficAnnotation("javascript_report_error", R"(
