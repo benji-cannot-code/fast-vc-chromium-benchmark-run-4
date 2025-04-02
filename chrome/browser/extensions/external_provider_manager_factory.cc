23d7ba6227ddf99a0ea64bb337af59a3f0acbee1
James Cook <jamescook@chromium.org>
2025-04-02 10:37:21 -0700

    extensions: Port ExternalProviderManager to desktop Android
    This is a dependency of several core extension classes. We need it now
    to unblock CorruptedExtensionReinstaller, which itself is a dependency
    of several install-related classes.
    
    For now we #ifdef out its usage of ExternalProviderImpl, which should
    disabled the use of external extensions on desktop Android. This should
    be resolved soon, as other dependencies land.
    
    Bug: 407824028
    Change-Id: I23815ecb87691b5bf0fa49dcb4c4208077744f26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423409
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441594}

--- chrome/browser/extensions/external_provider_manager_factory.cc
+++ chrome/browser/extensions/external_provider_manager_factory.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/external_provider_manager_factory.h"
 
 #include "base/check.h"
-#include "chrome/browser/extensions/chrome_extension_system_factory.h"
 #include "chrome/browser/extensions/extension_error_controller_factory.h"
 #include "chrome/browser/extensions/external_install_manager_factory.h"
 #include "chrome/browser/extensions/external_provider_manager.h"
@@ -16,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs_factory.h"
 #include "extensions/browser/extension_registrar_factory.h"
 #include "extensions/browser/extension_registry_factory.h"
+#include "extensions/browser/extension_system_provider.h"
+#include "extensions/browser/extensions_browser_client.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/ash/extensions/install_limiter_factory.h"
@@ -56,7 +57,7 @@ ExternalProviderManagerFactory::ExternalProviderManagerFactory()
   DependsOn(ExtensionRegistrarFactory::GetInstance());
   DependsOn(ExternalInstallManagerFactory::GetInstance());
   DependsOn(PendingExtensionManagerFactory::GetInstance());
-  DependsOn(ChromeExtensionSystemFactory::GetInstance());
+  DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
   DependsOn(InstallStageTrackerFactory::GetInstance());
 #if BUILDFLAG(IS_CHROMEOS)
   DependsOn(InstallLimiterFactory::GetInstance());
