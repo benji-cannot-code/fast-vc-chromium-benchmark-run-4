c3a3fad4cecdcac6cea659005e42b7dc2a2558c2
Tommy Steimel <steimel@chromium.org>
2025-04-02 08:36:49 -0700

    [video pip] Show VC controls when available in 2024 UI
    This CL adds VC controls to the new video picture-in-picture UI. This
    places them at the right size and position. Updated icons will come in
    a folloup CL.
    
    Bug: 360357715
    Change-Id: I2039ef520105b7fe32f163dabff2775218580cd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418495
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Feels: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441514}

--- chrome/browser/ui/views/overlay/playback_image_button.cc
+++ chrome/browser/ui/views/overlay/playback_image_button.cc
@@ -32,7 +32,7 @@ PlaybackImageButton::PlaybackImageButton(PressedCallback callback)
   // never change.
   if (base::FeatureList::IsEnabled(
           media::kVideoPictureInPictureControlsUpdate2024)) {
-    SetSize(gfx::Size(kPlaybackButtonSize, kPlaybackButtonSize));
+    SetSize(gfx::Size(kCenterButtonSize, kCenterButtonSize));
 
     play_image_ = ui::ImageModel::FromVectorIcon(
         vector_icons::kPlayArrowIcon, ui::kColorSysOnSecondaryContainer,
@@ -121,7 +121,7 @@ void PlaybackImageButton::SetPlayButtonBackground() {
     return;
   }
   SetBackground(views::CreateRoundedRectBackground(
-      ui::kColorSysSecondaryContainer, kPlaybackButtonSize / 2));
+      ui::kColorSysSecondaryContainer, kCenterButtonSize / 2));
 }
 
 void PlaybackImageButton::SetPauseButtonBackground() {
@@ -131,7 +131,7 @@ void PlaybackImageButton::SetPauseButtonBackground() {
   }
 
   SetBackground(views::CreateRoundedRectBackground(
-      SkColorSetARGB(0x33, 0xFF, 0xFF, 0xFF), kPlaybackButtonSize / 2));
+      SkColorSetARGB(0x33, 0xFF, 0xFF, 0xFF), kCenterButtonSize / 2));
 }
 
 BEGIN_METADATA(PlaybackImageButton)
