82c79b396c133aec3a38a43dc21aa595427668cf
Brandon Wylie <wylieb@chromium.org>
2025-04-02 13:33:38 -0700

    [reader mode] Record word count when distillation is complete
    Bug: 406299154
    Change-Id: I1011037150718b0ade3a23f26d4c502924f065f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396773
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441772}

--- components/dom_distiller/core/distiller.cc
+++ components/dom_distiller/core/distiller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
@@ -240,6 +241,10 @@ void DistillerImpl::OnPageDistillationFinished(
     MaybeFetchImage(page_num, image_id,
                     distiller_result->content_images(img_num).url());
   }
+
+  base::UmaHistogramCounts100000(
+      "DomDistiller.WordCount",
+      distiller_result->statistics_info().word_count());
 
   AddPageIfDone(page_num);
   DistillNextPage();
