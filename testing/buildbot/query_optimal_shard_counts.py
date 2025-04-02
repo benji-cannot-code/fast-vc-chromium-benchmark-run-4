df824ca862085c1228f5e46204d7f794c2108fc8
Struan Shrimpton <sshrimp@google.com>
2025-04-02 11:35:19 -0700

    autosharder: parameterize tables
    The first step to reworking autosharder is to introduce tests to verify
    the SQL is doing what we expect when it is refactored/reworked. This
    will require the scripts to be able to be run against mock tables in a
    controlled dataset which we can do by parameterizing the table names.
    
    Bug: 407846444
    Change-Id: I64db6077f12dfbc48aa024cde936a43096178d5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423231
    Feels: Struan Shrimpton <sshrimp@google.com>
    Reviewed-by: Garrett Beaty <gbeaty@google.com>
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441669}

--- testing/buildbot/query_optimal_shard_counts.py
+++ testing/buildbot/query_optimal_shard_counts.py
@@ -74,10 +74,14 @@ If this is the first time you run the script, do the following steps:
 
 def _query_overheads(lookback_start_date, lookback_end_date):
   query_file = os.path.join(os.path.dirname(__file__), 'autosharder_sql',
-                            'query_test_overheads.sql')
+                            'query_test_overheads.sql.tmpl')
   with open(query_file, 'r') as f:
     query_str = f.read()
   query = query_str.format(
+      builds_project='cr-buildbucket',
+      builds_dataset='chromium',
+      tasks_project='chromium-swarm',
+      tasks_dataset='swarming',
       lookback_start_date=lookback_start_date,
       lookback_end_date=lookback_end_date,
   )
@@ -90,10 +94,14 @@ def _query_overheads(lookback_start_date, lookback_end_date):
 def _query_suite_durations(lookback_start_date, lookback_end_date, percentile,
                            ignore_cl_owner):
   query_file = os.path.join(os.path.dirname(__file__), 'autosharder_sql',
-                            'query_suite_durations.sql')
+                            'query_suite_durations.sql.tmpl')
   with open(query_file, 'r') as f:
     query_str = f.read()
   query = query_str.format(
+      builds_project='cr-buildbucket',
+      builds_dataset='chromium',
+      tasks_project='chromium-swarm',
+      tasks_dataset='swarming',
       lookback_start_date=lookback_start_date,
       lookback_end_date=lookback_end_date,
       percentile=percentile,
@@ -108,10 +116,12 @@ def _query_suite_durations(lookback_start_date, lookback_end_date, percentile,
 
 def _query_avg_num_builds_per_hour(lookback_start_date, lookback_end_date):
   query_file = os.path.join(os.path.dirname(__file__), 'autosharder_sql',
-                            'query_average_number_builds_per_hour.sql')
+                            'query_average_number_builds_per_hour.sql.tmpl')
   with open(query_file, 'r') as f:
     query_str = f.read()
   query = query_str.format(
+      builds_project='cr-buildbucket',
+      builds_dataset='chromium',
       lookback_start_date=lookback_start_date,
       lookback_end_date=lookback_end_date,
   )
@@ -278,9 +288,12 @@ def _meets_optimal_shard_count_and_simulated_duration_requirements(
 
 def _prune_builders(data):
   query_file = os.path.join(os.path.dirname(__file__), 'autosharder_sql',
-                            'query_cq_builders.sql')
+                            'query_cq_builders.sql.tmpl')
   with open(query_file, 'r') as f:
-    query = f.read()
+    query = f.read().format(
+        builders_project='chrome-trooper-analytics',
+        builders_dataset='metrics',
+    )
 
   rows = _run_query([
       'bq', 'query', '--project_id=' + _CLOUD_PROJECT_ID, '--format=json',
