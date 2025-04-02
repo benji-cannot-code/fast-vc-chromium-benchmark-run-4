5f212bab292081dd594971afa70b568368da8a4c
Alan Screen <awscreen@chromium.org>
2025-04-02 10:39:56 -0700

    Fix FetchCapabilities() time metric for in-browser printing
    The metric times for fetching printer capabilities from the browser
    process have been erroneously small, because it has been measuring only
    the time for GetPrinterBasicInfo() but not the time for
    GetPrinterSemanticCapsAndDefaults().  It also had discrepancies
    compared to out-of-process metric timing for including creation of the
    PrintBackend object, and for macOS also for GetMacCustomPaperSizes().
    
    Modify the collection for capture this metric on return from
    FetchCapabilitiesOnBlockingTaskRunner() to ensure a better match for
    comparing against out-of-process calls.  Update the metric name to
    PrintPreview.FetchCapabilitiesTime2 to avoid mixing of the new vs. old
    metrics data.
    
    Also put the writer for the metric in a helper function, so that it
    complies with guidance to only write a metric out from one place.
    
    OBSOLETE_HISTOGRAM[PrintPreview.FetchCapabilitiesTime]=Replaced by PrintPreview.FetchCapabilitiesTime2
    
    Change-Id: I0351e52c5189f144005d3b0e802e4fa870369de6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6036265
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441599}

--- chrome/browser/ui/webui/print_preview/local_printer_handler_default.cc
+++ chrome/browser/ui/webui/print_preview/local_printer_handler_default.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "printing/buildflags/buildflags.h"
 #include "printing/mojom/print.mojom.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 
 #if BUILDFLAG(IS_MAC)
 #include "chrome/common/printing/printer_capabilities_mac.h"
@@ -65,6 +66,11 @@ scoped_refptr<base::TaskRunner> CreatePrinterHandlerTaskRunner() {
 #endif
 }
 
+void ReportFetchCapabilitiesTime(base::TimeTicks start_time) {
+  base::UmaHistogramTimes("PrintPreview.FetchCapabilitiesTime2",
+                          base::TimeTicks::Now() - start_time);
+}
+
 }  // namespace
 
 // static
@@ -99,6 +105,16 @@ base::Value::Dict
 LocalPrinterHandlerDefault::FetchCapabilitiesOnBlockingTaskRunner(
     const std::string& device_name,
     const std::string& locale) {
+  scoped_refptr<PrintBackend> print_backend(
+      PrintBackend::CreateInstance(locale));
+
+  // Start timing after CreateInstance() to match what gets timed for the
+  // out-of-process query.
+  auto query_start_time = base::TimeTicks::Now();
+  absl::Cleanup metric_reporter = [query_start_time] {
+    ReportFetchCapabilitiesTime(query_start_time);
+  };
+
   PrinterSemanticCapsAndDefaults::Papers user_defined_papers;
 #if BUILDFLAG(IS_MAC)
   user_defined_papers = GetMacCustomPaperSizes();
@@ -110,21 +126,15 @@ LocalPrinterHandlerDefault::FetchCapabilitiesOnBlockingTaskRunner(
   base::ScopedAllowBlocking allow_blocking;
 #endif
 
-  auto query_start_time = base::TimeTicks::Now();
-
-  scoped_refptr<PrintBackend> print_backend(
-      PrintBackend::CreateInstance(locale));
-
   PrinterBasicInfo basic_info;
   mojom::ResultCode result =
       print_backend->GetPrinterBasicInfo(device_name, &basic_info);
-  base::UmaHistogramTimes("PrintPreview.FetchCapabilitiesTime",
-                          base::TimeTicks::Now() - query_start_time);
   if (result != mojom::ResultCode::kSuccess) {
     PRINTER_LOG(ERROR) << "Invalid printer when getting basic info for "
                        << device_name << ", result: " << result;
     return base::Value::Dict();
   }
+
   PRINTER_LOG(EVENT) << "Got basic info for " << device_name;
   return GetSettingsOnBlockingTaskRunner(
       device_name, basic_info, std::move(user_defined_papers), print_backend);
@@ -314,8 +324,7 @@ void LocalPrinterHandlerDefault::OnDidFetchCapabilitiesFromPrintBackendService(
     base::TimeTicks query_start_time,
     GetCapabilityCallback callback,
     mojom::PrinterCapsAndInfoResultPtr result) {
-  base::UmaHistogramTimes("PrintPreview.FetchCapabilitiesTime",
-                          base::TimeTicks::Now() - query_start_time);
+  ReportFetchCapabilitiesTime(query_start_time);
 
   if (result->is_result_code()) {
     PRINTER_LOG(ERROR)
