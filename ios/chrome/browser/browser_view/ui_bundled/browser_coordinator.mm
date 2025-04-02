2ed1ea8ba501e901f017eaa12088e59652abb03b
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-04-02 14:06:50 -0700

    [iOS] Enhanced Calendar UI and commands
    - Commands and Browser coordinator protocol handling
    - MVC for UI
    - Actual UI (bottom sheet, cancel button, throbber
    (https://screenshot.googleplex.com/9iaNevXJ6xWShdE.png)
    
    Bug: 405196085
    Change-Id: Id96e20bd2039295dcd5aca2874ea2cf1d25dabc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418864
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Feels: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441796}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -118,6 +118,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_scene_agent.h"
 #import "ios/chrome/browser/infobars/model/infobar_ios.h"
 #import "ios/chrome/browser/infobars/model/infobar_manager_impl.h"
+#import "ios/chrome/browser/intelligence/enhanced_calendar/coordinator/enhanced_calendar_coordinator.h"
 #import "ios/chrome/browser/intents/model/intents_donation_helper.h"
 #import "ios/chrome/browser/lens/ui_bundled/lens_coordinator.h"
 #import "ios/chrome/browser/lens_overlay/coordinator/lens_overlay_availability.h"
@@ -198,6 +199,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/contextual_sheet_commands.h"
 #import "ios/chrome/browser/shared/public/commands/country_code_picker_commands.h"
 #import "ios/chrome/browser/shared/public/commands/drive_file_picker_commands.h"
+#import "ios/chrome/browser/shared/public/commands/enhanced_calendar_commands.h"
 #import "ios/chrome/browser/shared/public/commands/feed_commands.h"
 #import "ios/chrome/browser/shared/public/commands/find_in_page_commands.h"
 #import "ios/chrome/browser/shared/public/commands/google_one_commands.h"
@@ -329,6 +331,7 @@ enum class ToolbarKind {
     DefaultBrowserGenericPromoCommands,
     DefaultPromoNonModalPresentationDelegate,
     DriveFilePickerCommands,
+    EnhancedCalendarCommands,
     EditMenuBuilder,
     EnterprisePromptCoordinatorDelegate,
     FormInputAccessoryCoordinatorNavigator,
@@ -650,6 +653,9 @@ enum class ToolbarKind {
   LensPromoCoordinator* _lensPromoCoordinator;
   EnhancedSafeBrowsingPromoCoordinator* _enhancedSafeBrowsingPromoCoordinator;
   AutoDeletionCoordinator* _autoDeletionCoordinator;
+
+  // The coordinator for the Enhanced Calendar feature UI (bottom sheet).
+  EnhancedCalendarCoordinator* _enhancedCalendarCoordinator;
 }
 
 #pragma mark - ChromeCoordinator
@@ -1048,6 +1054,7 @@ enum class ToolbarKind {
     @protocol(ContextualSheetCommands),
     @protocol(DefaultBrowserPromoNonModalCommands),
     @protocol(DriveFilePickerCommands),
+    @protocol(EnhancedCalendarCommands),
     @protocol(FeedCommands),
     @protocol(PromosManagerCommands),
     @protocol(FindInPageCommands),
@@ -1638,6 +1645,9 @@ enum class ToolbarKind {
 
   [_quickDeleteCoordinator stop];
   _quickDeleteCoordinator = nil;
+
+  [_enhancedCalendarCoordinator stop];
+  _enhancedCalendarCoordinator = nil;
 
   [self hideDriveFilePicker];
   [self hideContextualSheet];
@@ -2525,6 +2535,22 @@ enum class ToolbarKind {
 - (void)setDriveFilePickerSelectedIdentity:
     (id<SystemIdentity>)selectedIdentity {
   [_driveFilePickerCoordinator setSelectedIdentity:selectedIdentity];
+}
+
+#pragma mark - EnhancedCalendarCommands
+
+- (void)showEnhancedCalendarBottomSheetWithIntegrationProvider:
+    (ios::provider::AddToCalendarIntegrationProvider)integrationProvider {
+  _enhancedCalendarCoordinator = [[EnhancedCalendarCoordinator alloc]
+      initWithBaseViewController:self.viewController
+                         browser:self.browser
+             integrationProvider:integrationProvider];
+  [_enhancedCalendarCoordinator start];
+}
+
+- (void)hideEnhancedCalendarBottomSheet {
+  [_enhancedCalendarCoordinator stop];
+  _enhancedCalendarCoordinator = nil;
 }
 
 #pragma mark - FeedCommands
