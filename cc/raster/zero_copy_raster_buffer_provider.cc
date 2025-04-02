f0ad49bba32d479b8c89c41bb3e8d9a22d0aaa3c
Colin Blundell <blundell@chromium.org>
2025-04-01 23:58:22 -0700

    [//cc] Unify ZeroCopyRasterBufferProvider constructors
    The RasterContextProvider has initialized its SharedImageInterface at
    the point at which LayerTreeHostImpl accesses it [1][2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:cc/trees/layer_tree_host_impl.cc;drc=87fedd04f34e479b4d5a45f957f09b79d45e3cee;l=4438
    [2] https://source.chromium.org/chromium/chromium/src/+/main:cc/trees/layer_tree_host_impl.cc;drc=87fedd04f34e479b4d5a45f957f09b79d45e3cee;l=4467
    
    Bug: 403372453
    Change-Id: I07d4cb616cf0bec940503248f8110d6caeb8a3e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421886
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441372}

--- cc/raster/zero_copy_raster_buffer_provider.cc
+++ cc/raster/zero_copy_raster_buffer_provider.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/resources/resource_pool.h"
 #include "cc/trees/layer_tree_frame_sink.h"
 #include "components/viz/client/client_resource_provider.h"
-#include "components/viz/common/gpu/raster_context_provider.h"
 #include "components/viz/common/resources/platform_color.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
 #include "gpu/command_buffer/client/client_shared_image.h"
@@ -165,15 +164,10 @@ bool ZeroCopyRasterBufferImpl::SupportsBackgroundThreadPriority() const {
 }
 
 ZeroCopyRasterBufferProvider::ZeroCopyRasterBufferProvider(
-    viz::RasterContextProvider* compositor_context_provider,
-    const RasterCapabilities& raster_caps)
-    : compositor_context_provider_(compositor_context_provider),
-      tile_format_(raster_caps.tile_format) {}
-
-ZeroCopyRasterBufferProvider::ZeroCopyRasterBufferProvider(
     const scoped_refptr<gpu::SharedImageInterface>& shared_image_interface,
-    const RasterCapabilities& raster_caps)
-    : is_software_(true),
+    const RasterCapabilities& raster_caps,
+    bool is_software)
+    : is_software_(is_software),
       shared_image_interface_(shared_image_interface),
       tile_format_(raster_caps.tile_format) {
   CHECK(shared_image_interface_)
@@ -199,9 +193,7 @@ ZeroCopyRasterBufferProvider::AcquireBufferForRaster(
   }
 
   return std::make_unique<ZeroCopyRasterBufferImpl>(
-      resource,
-      base::WrapRefCounted(
-          compositor_context_provider_->SharedImageInterface()),
+      resource, shared_image_interface_,
       /*resource_has_previous_content=*/false, /*is_software=*/false);
 }
 
--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -4193,7 +4193,8 @@ LayerTreeHostImpl::CreateRasterBufferProvider() {
 
   if (!compositor_context_provider) {
     return std::make_unique<ZeroCopyRasterBufferProvider>(
-        layer_tree_frame_sink_->shared_image_interface(), raster_caps_);
+        layer_tree_frame_sink_->shared_image_interface(), raster_caps_,
+        /*is_software=*/true);
   }
 
   const gpu::Capabilities& caps =
@@ -4222,7 +4223,8 @@ LayerTreeHostImpl::CreateRasterBufferProvider() {
 
   if (use_zero_copy) {
     return std::make_unique<ZeroCopyRasterBufferProvider>(
-        compositor_context_provider, raster_caps_);
+        compositor_context_provider->SharedImageInterface(), raster_caps_,
+        /*is_software=*/false);
   }
 
   const int max_copy_texture_chromium_size =
