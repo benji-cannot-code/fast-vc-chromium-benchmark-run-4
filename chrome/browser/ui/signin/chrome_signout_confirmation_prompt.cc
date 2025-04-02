c2e9639dc826eeea26f284b7a01e20a98bf3d875
Kelvin Jiang <kelvinjiang@chromium.org>
2025-04-02 11:30:00 -0700

    [Extensions UNO] Record if users keep or uninstall account extensions on signout
    Records the user's interaction choice when a signout dialog shows
    account extensions:
    - user signs out and keeps account extensions
    - user signs out and uninstalls account extensions
    - user cancels signout
    
    Bug: 406819291
    Change-Id: I6e6854a31f397b167e62bb2626cc4ba7702b29dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6406409
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441660}

--- chrome/browser/ui/signin/chrome_signout_confirmation_prompt.cc
+++ chrome/browser/ui/signin/chrome_signout_confirmation_prompt.cc
@@ -19,6 +19,8 @@ constexpr char kChromeSignoutPromptHistogramNoUnsyncedVariant[] = "NoUnsynced";
 constexpr char kChromeSignoutPromptHistogramSupervisedProfileVariant[] =
     "SupervisedProfile";
 
+constexpr char kAccountExtensionsSignoutChoiceHistogramName[] =
+    "Signin.Extensions.AccountExtensionsSignoutChoice";
 }  // namespace
 
 void RecordChromeSignoutConfirmationPromptMetrics(
@@ -46,4 +48,26 @@ void RecordChromeSignoutConfirmationPromptMetrics(
       base::StrCat(
           {kChromeSignoutPromptHistogramBaseName, histogram_variant_name}),
       choice);
+}
+
+void RecordAccountExtensionsSignoutChoice(
+    ChromeSignoutConfirmationChoice choice,
+    bool account_extensions_kept) {
+  AccountExtensionsSignoutChoice extension_choice =
+      AccountExtensionsSignoutChoice::kCancelSignout;
+  switch (choice) {
+    case ChromeSignoutConfirmationChoice::kCancelSignout:
+    case ChromeSignoutConfirmationChoice::kCancelSignoutAndReauth:
+      break;
+    case ChromeSignoutConfirmationChoice::kSignout:
+      extension_choice =
+          account_extensions_kept
+              ? AccountExtensionsSignoutChoice::kSignoutAccountExtensionsKept
+              : AccountExtensionsSignoutChoice::
+                    kSignoutAccountExtensionsUninstalled;
+      break;
+  }
+
+  base::UmaHistogramEnumeration(kAccountExtensionsSignoutChoiceHistogramName,
+                                extension_choice);
 }
