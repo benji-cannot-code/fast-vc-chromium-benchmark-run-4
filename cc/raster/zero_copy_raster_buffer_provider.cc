30ef883e75acb08a8ad1ffc9af52c4459dbc9618
Colin Blundell <blundell@chromium.org>
2025-04-03 00:00:14 -0700

    [//cc] Support partial raster in ZeroCopyRBP with GPU compositing
    The support is now already in place as it's used with SW compositing;
    we just need to turn it on. As this is a behavioral change, we roll it
    out with a killswitch.
    
    Bug: 403372453
    Change-Id: I92ff110dd7e64b113cb2f76c560fbef7b616db84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6400615
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441993}

--- cc/raster/zero_copy_raster_buffer_provider.cc
+++ cc/raster/zero_copy_raster_buffer_provider.cc
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/traced_value.h"
+#include "cc/base/features.h"
 #include "cc/resources/resource_pool.h"
 #include "cc/trees/layer_tree_frame_sink.h"
 #include "components/viz/client/client_resource_provider.h"
@@ -184,17 +186,17 @@ ZeroCopyRasterBufferProvider::AcquireBufferForRaster(
     bool depends_on_at_raster_decodes,
     bool depends_on_hardware_accelerated_jpeg_candidates,
     bool depends_on_hardware_accelerated_webp_candidates) {
-  if (is_software_) {
-    bool resource_has_previous_content =
+  bool resource_has_previous_content = false;
+  if (is_software_ ||
+      base::FeatureList::IsEnabled(
+          features::kZeroCopyRBPPartialRasterWithGpuCompositor)) {
+    resource_has_previous_content =
         resource_content_id && resource_content_id == previous_content_id;
-    return std::make_unique<ZeroCopyRasterBufferImpl>(
-        resource, shared_image_interface_, resource_has_previous_content,
-        /*is_software=*/true);
   }
 
   return std::make_unique<ZeroCopyRasterBufferImpl>(
-      resource, shared_image_interface_,
-      /*resource_has_previous_content=*/false, /*is_software=*/false);
+      resource, shared_image_interface_, resource_has_previous_content,
+      is_software_);
 }
 
 void ZeroCopyRasterBufferProvider::Flush() {}
@@ -209,7 +211,9 @@ bool ZeroCopyRasterBufferProvider::IsResourcePremultiplied() const {
 
 bool ZeroCopyRasterBufferProvider::CanPartialRasterIntoProvidedResource()
     const {
-  return is_software_;
+  return is_software_ ||
+         base::FeatureList::IsEnabled(
+             features::kZeroCopyRBPPartialRasterWithGpuCompositor);
 }
 
 bool ZeroCopyRasterBufferProvider::IsResourceReadyToDraw(
