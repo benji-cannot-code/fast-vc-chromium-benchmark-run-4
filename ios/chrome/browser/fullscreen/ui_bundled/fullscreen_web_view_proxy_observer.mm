367d5e3494b3615c7ad26746f301accf7cc466fb
Scott Yoder <scottyoder@google.com>
2025-04-03 03:34:33 -0700

    [ios] Fix fullscreen left in partial state when scroll ends
    This change fixes an issue where the fullscreen progress could be left
    in a partial state at the end of a scroll event. When the drag ends if
    there will be no deceleration then the scroll should be marked as ended
    immediately. If there is deceleration, then the scroll event will be
    ended when the deceleration ends (as is already implemented).
    
    Fixed: 407868629
    Change-Id: Id4e6773cb3ce191058ee9ba6453f4379dd7d27ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426419
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1442053}

--- ios/chrome/browser/fullscreen/ui_bundled/fullscreen_web_view_proxy_observer.mm
+++ ios/chrome/browser/fullscreen/ui_bundled/fullscreen_web_view_proxy_observer.mm
@@ -93,6 +93,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                          willDecelerate:(BOOL)decelerate {
   if (!base::FeatureList::IsEnabled(web::features::kSmoothScrollingDefault)) {
     self.model->SetScrollViewIsDragging(false);
+    if (!decelerate) {
+      self.model->SetScrollViewIsScrolling(false);
+    }
   }
 }
 
