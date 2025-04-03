2af73388d5602a344d677beac87dd6a3dc75e1eb
Zonghan Xu <xzonghan@google.com>
2025-04-03 13:58:12 -0700

    [Signals Reporting] Create UserSecuritySignalsService
    This class will provide an object for the existing status reporting
    classes to interpret the UserSecuritySignalsReporting policy values. It
    will also handle all of the new triggers for that policy, and expose
    them via an aggregated hook.
    
    The plan for this class is to be instantiated by
    ReportScheduler::Delegate implementations when they are created for
    Profile reporting flows. The ReportScheduler will then interact with
    this class in a decoupled fashion through its delegate.
    
    Bug: 399164647,402486791
    Change-Id: Id7da1bb5d1a3739c088f797d7970f64fd10ce8e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6355071
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442412}

--- components/enterprise/browser/reporting/report_util.cc
+++ components/enterprise/browser/reporting/report_util.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/enterprise/browser/reporting/report_util.h"
+
 #include "base/files/file_path.h"
 #include "crypto/sha2.h"
 
