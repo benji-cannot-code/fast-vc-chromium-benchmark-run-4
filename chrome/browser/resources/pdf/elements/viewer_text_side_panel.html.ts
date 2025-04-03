c82ed6fbf70002bb0530c42a9cdf84a091708813
rbpotter <rbpotter@chromium.org>
2025-04-02 17:40:58 -0700

    [PDF Ink Signatures] Refactor alignment to a custom element
    - Refactor the text alignment controls to a new custom element.
    - Add a mixin for observing text changes, and use it from the new
      control as well as the text side panel and the style selector.
    - Refactor test for alignment controls.
    - Update styles to match 16px spacing in mock and to enable the
      alignment to be also shown from a viewer-bottom-toolbar-dropdown.
    
    This is in preparation for adding a bottom toolbar, which will also
    require an alignment selector.
    
    Bug: 402546154
    Change-Id: I37caac8e2bd11a263b7edf102cb257c4dbc8106c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421503
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441907}

--- chrome/browser/resources/pdf/elements/viewer_text_side_panel.html.ts
+++ chrome/browser/resources/pdf/elements/viewer_text_side_panel.html.ts
@@ -3,15 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
-import './icons.html.js';
 import './ink_color_selector.js';
-import './selectable_icon_button.js';
+import './text_alignment_selector.js';
 import './text_styles_selector.js';
 
 import {html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
-
-import {TextAlignment} from '../constants.js';
 
 import type {ViewerTextSidePanelElement} from './viewer_text_side_panel.js';
 
@@ -37,22 +33,7 @@ export function getHtml(this: ViewerTextSidePanelElement) {
     <div class="side-panel-content">
       <h2>Styles</h2>
       <text-styles-selector></text-styles-selector>
-      <cr-radio-group selectable-elements="selectable-icon-button"
-          .selected="${this.currentAlignment_}"
-          @selected-changed="${this.onSelectedAlignmentChanged_}">
-        <selectable-icon-button icon="pdf:text-align-left"
-            name="${TextAlignment.LEFT}" label="Left">
-        </selectable-icon-button>
-        <selectable-icon-button icon="pdf:text-align-center"
-            name="${TextAlignment.CENTER}" label="Center">
-        </selectable-icon-button>
-        <selectable-icon-button icon="pdf:text-align-justify"
-            name="${TextAlignment.JUSTIFY}" label="Justify">
-        </selectable-icon-button>
-        <selectable-icon-button icon="pdf:text-align-right"
-            name="${TextAlignment.RIGHT}" label="Right">
-        </selectable-icon-button>
-      </cr-radio-group>
+      <text-alignment-selector></text-alignment-selector>
     </div>
     <div class="side-panel-content">
       <h2>Text color</h2>
