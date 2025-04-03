fa4595dff0e201c188c729f4365fbbe1df64797a
Christoph Schwering <schwering@google.com>
2025-04-03 14:45:51 -0700

    [Autofill] Remove AutofillField::is_most_recent_single_username_candidate_
    This CL moves AutofillField::is_most_recent_single_username_candidate_
    to EncodeUploadRequestOptions::Field.
    
    Bug: 405558390
    Change-Id: Ic62e03b39afb9a7370e5d9e3a114d5fa39dd4c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6430113
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1442441}

--- components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding_unittest.cc
+++ components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding_unittest.cc
@@ -1679,8 +1679,6 @@ TEST_F(AutofillCrowdsourcingEncoding,
   form.set_fields({field});
 
   FormStructure form_structure(form);
-  form_structure.field(0)->set_is_most_recent_single_username_candidate(
-      IsMostRecentSingleUsernameCandidate::kMostRecentCandidate);
   for (auto& fs_field : form_structure) {
     fs_field->set_host_form_signature(form_structure.form_signature());
   }
@@ -1693,6 +1691,9 @@ TEST_F(AutofillCrowdsourcingEncoding,
   options.observed_submission = true;
   options.fields[form_structure.field(0)->global_id()]
       .single_username_vote_type = AutofillUploadContents::Field::STRONG;
+  options.fields[form_structure.field(0)->global_id()]
+      .is_most_recent_single_username_candidate =
+      IsMostRecentSingleUsernameCandidate::kMostRecentCandidate;
 
   std::vector<AutofillUploadContents> uploads =
       EncodeUploadRequest(form_structure, options);
--- components/password_manager/core/browser/votes_uploader.h
+++ components/password_manager/core/browser/votes_uploader.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
 #include "components/autofill/core/browser/autofill_field.h"
+#include "components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/proto/server.pb.h"
@@ -24,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 class AutofillField;
-struct EncodeUploadRequestOptions;
 class FormData;
 class FormStructure;
 }  // namespace autofill
