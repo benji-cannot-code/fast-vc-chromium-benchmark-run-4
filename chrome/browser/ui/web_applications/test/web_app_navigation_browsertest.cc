e3d21be981fab75f174311ac18ae667c71750ecb
Mustaq Ahmed <mustaq@google.com>
2025-04-03 09:07:22 -0700

    Fix browser_tests that don't need to wait for paint-holding for input
    Tests that are paint-holding agnostic need to end paint-holding before
    sending input events to the page.  We are expecting to suppress input
    events to a page during the paint-holding period (this is currently
    behind the flag `kDropInputEventsWhilePaintHolding`).
    
    Bug: 40057499
    Change-Id: Ia8eb410afb9707f308e3ea3e0f04ce26448f769c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427062
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442198}

--- chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.cc
+++ chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.cc
@@ -154,6 +154,7 @@ void WebAppNavigationBrowserTest::ClickLink(
       rel.c_str());
   ASSERT_TRUE(content::ExecJs(web_contents, script));
 
+  content::SimulateEndOfPaintHoldingOnPrimaryMainFrame(web_contents);
   content::SimulateMouseClick(web_contents, modifiers, button);
 }
 
