e52b50fb18b81c97248fc04329043a8cc42f8a24
Andrea Gomez <andreaxg@google.com>
2025-04-03 16:28:03 -0700

    Resize to last requested size on drag end
    Demo of current behavior and new behavior:
    http://go/scrcast/NDkzOTg5MDA1MDQ2NTc5Mnw5Yjc1MjhlMy0xZQ
    
    - Stops animations from continuing if glic is being dragged.
    - Saves the last target size requested to update glic's bounds when
      dragging ends.
    
    Bug: 398042493
    Change-Id: I435c07ae75a3e55d5cf6670ac8292d2245141983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6431186
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1442491}

--- chrome/browser/glic/widget/glic_window_controller.h
+++ chrome/browser/glic/widget/glic_window_controller.h
@@ -272,6 +272,8 @@ class GlicWindowController : public views::WidgetObserver,
   // Helper function to get the always detached flag.
   static bool AlwaysDetached();
 
+  bool IsDragging() { return in_move_loop_; }
+
  private:
   // ui::AcceleratorTarget
   bool AcceleratorPressed(const ui::Accelerator& accelerator) override;
