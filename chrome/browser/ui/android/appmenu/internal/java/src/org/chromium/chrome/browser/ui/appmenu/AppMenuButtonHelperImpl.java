204bdf80c2f09349552f2a51f0445b09e4b63eb9
Fiaz Muhammad <mfiaz@google.com>
2025-04-03 11:05:58 -0700

    [Tab Group Entry Points] Highlight Menu Items from the Button Coord.
    - New method to highlight a menu item on next 3-dot menu open
    	from MenuButtonCoordinator
    - This makes it easier to highlight menu items from entry points for
    	the 3-dot menu (one use case would be creating an IPH which
            highlights the menu from within HubToolbarCoordinator)
    
    Bug: 408190509
    Change-Id: Icc958db7870c4f0ee6e7cfbd737df240037a8e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426169
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442291}

--- chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuButtonHelperImpl.java
+++ chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuButtonHelperImpl.java
@@ -14,6 +14,8 @@ import android.view.View.AccessibilityDelegate;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
+import androidx.annotation.IdRes;
+
 import org.chromium.base.metrics.RecordUserAction;
 
 /**
@@ -51,6 +53,11 @@ class AppMenuButtonHelperImpl extends AccessibilityDelegate implements AppMenuBu
     @Override
     public boolean onEnterKeyPress(View view) {
         return showAppMenu(view, false);
+    }
+
+    @Override
+    public void highlightMenuItemOnShow(@IdRes int menuItemId) {
+        mMenuHandler.setMenuHighlight(menuItemId);
     }
 
     @Override
