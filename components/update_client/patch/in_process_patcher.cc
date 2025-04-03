1d4e0bd025a535cad41adc4bc9fd05f6fe1cb751
Joshua Pawlicki <waffles@chromium.org>
2025-04-03 07:36:29 -0700

    update_client: Implement support for zucchini patch operations.
    Bug: 399619105
    Change-Id: Id0d9220f46a6cba6eec94a708e357341bb073677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416522
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442146}

--- components/update_client/patch/in_process_patcher.cc
+++ components/update_client/patch/in_process_patcher.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
+#include "components/zucchini/zucchini.h"
+#include "components/zucchini/zucchini_integration.h"
 #include "third_party/puffin/src/include/puffin/puffpatch.h"
 
 namespace update_client {
@@ -26,6 +28,15 @@ class InProcessPatcher : public Patcher {
                       PatchCompleteCallback callback) const override {
     std::move(callback).Run(puffin::ApplyPuffPatch(
         std::move(input_file), std::move(patch_file), std::move(output_file)));
+  }
+
+  void PatchZucchini(base::File old_file,
+                     base::File patch_file,
+                     base::File destination_file,
+                     PatchCompleteCallback callback) const override {
+    std::move(callback).Run(static_cast<int>(
+        zucchini::Apply(std::move(old_file), std::move(patch_file),
+                        std::move(destination_file))));
   }
 
  protected:
--- components/update_client/pipeline.cc
+++ components/update_client/pipeline.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/update_client/op_download.h"
 #include "components/update_client/op_install.h"
 #include "components/update_client/op_puffin.h"
+#include "components/update_client/op_zucchini.h"
 #include "components/update_client/protocol_parser.h"
 #include "components/update_client/unzipper.h"
 #include "components/update_client/update_client_errors.h"
@@ -254,6 +255,11 @@ std::queue<Operation> MakeOperations(
     } else if (operation.type == "puff") {
       ops.push(SkipIfCached(
           cache_check, base::BindOnce(&PuffOperation, crx_cache,
+                                      config->GetPatcherFactory()->Create(),
+                                      event_adder, id, operation.sha256_from)));
+    } else if (operation.type == "zucchini") {
+      ops.push(SkipIfCached(
+          cache_check, base::BindOnce(&ZucchiniOperation, crx_cache,
                                       config->GetPatcherFactory()->Create(),
                                       event_adder, id, operation.sha256_from)));
     } else if (operation.type == "crx3") {
