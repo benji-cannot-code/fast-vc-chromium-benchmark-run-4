3650577ee46fe1f6f787724b748d66221f12222f
Juan Mojica <juanmojica@google.com>
2025-04-02 18:21:19 -0700

    [lensoverlay] Add feature for new feedback entrypoint.
    Also adds it to chrome:// flags.
    
    Change-Id: Ib6f0bf7b3eb6ec31e7e559c5383b883a1ede2c28
    Bug: 408057948
    Fixed: 408057948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6428347
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441921}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1441,6 +1441,9 @@ extern const char kLensOverlayLatencyOptimizationsDescription[];
 extern const char kLensImageFormatOptimizationsName[];
 extern const char kLensImageFormatOptimizationsDescription[];
 
+extern const char kLensSearchSidePanelNewFeedbackName[];
+extern const char kLensSearchSidePanelNewFeedbackDescription[];
+
 extern const char kLinkedServicesSettingName[];
 extern const char kLinkedServicesSettingDescription[];
 
--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -72,6 +72,10 @@ BASE_FEATURE(kLensOverlayMGTInSidePanel,
              "LensOverlayMGTInSidePanel",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kLensSearchSidePanelNewFeedback,
+             "LensSearchSidePanelNewFeedback",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables the Lens Overlay omnibox entry point. This is a separate feature from
 // kLensOverlay so that the omnibox entry point can be disabled without a
 // dependency on the rest of the Lens Overlay features. This means if can be
@@ -998,6 +1002,10 @@ bool IsLensOverlayUploadChunkingUseDebugOptionsEnabled() {
 
 int GetLensOverlayUploadChunkRequestTimeoutMs() {
   return kLensOverlayUploadChunkRequestTimeoutMs.Get();
+}
+
+bool IsLensSearchSidePanelNewFeedbackEnabled() {
+  return base::FeatureList::IsEnabled(kLensSearchSidePanelNewFeedback);
 }
 
 }  // namespace lens::features
