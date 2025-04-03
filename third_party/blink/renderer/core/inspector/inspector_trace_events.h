d65a4f89751dfab9c50831dfa383c48436838111
Alison Maher <almaher@microsoft.com>
2025-04-03 06:50:47 -0700

    Invalidate when OOF alignment changes
    OOF alignment impacts an OOF when statically positioned. As such, when
    alignment changes, we need to invalidate. This works in Grid as a result
    of CL:3159264. This change generalizes this code to work beyond Grid.
    
    Bug: 407949606, 3764981, 40827518
    Change-Id: Icbd9395c02b6c0789b092a1b33448a85ea1889a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426901
    Auto-Submit: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442114}

--- third_party/blink/renderer/core/inspector/inspector_trace_events.h
+++ third_party/blink/renderer/core/inspector/inspector_trace_events.h
@@ -311,6 +311,7 @@ extern const char kDisplayLock[];
 extern const char kDevtools[];
 extern const char kAnchorPositioning[];
 extern const char kScrollMarkersChanged[];
+extern const char kOutOfFlowAlignmentChanged[];
 }  // namespace layout_invalidation_reason
 
 // LayoutInvalidationReasonForTracing is strictly for tracing. Blink logic must
