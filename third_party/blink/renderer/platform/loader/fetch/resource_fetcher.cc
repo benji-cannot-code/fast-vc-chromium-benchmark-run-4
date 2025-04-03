22d96d8d41269672b76980a9dcd70558499eb6c1
Lingqi Chi <lingqi@chromium.org>
2025-04-03 02:19:54 -0700

    Remove existing_top_frame_sites_in_cache_
    The value was used to record an expired histogram of
    Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite,
    which was deleted by https://crbug.com/40879015.
    
    Bug: 40879015
    Change-Id: I8d13adefb1bf77426a160498d4da76ad09bd9482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6429184
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442028}

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -1191,8 +1191,7 @@ void ResourceFetcher::UpdateMemoryCacheStats(
     RevalidationPolicyForMetrics policy,
     const FetchParameters& params,
     const ResourceFactory& factory,
-    bool is_static_data,
-    bool same_top_frame_site_resource_cached) const {
+    bool is_static_data) const {
   // Do not count static data or data not associated with the MemoryCache.
   if (is_static_data || !IsMainThread()) {
     return;
@@ -1447,7 +1446,6 @@ Resource* ResourceFetcher::RequestResource(FetchParameters& params,
     }
   }
 
-  bool same_top_frame_site_resource_cached = false;
   bool in_cached_resources_map = cached_resources_map_.Contains(
       MemoryCache::RemoveFragmentIdentifierIfNeeded(params.Url()));
 
@@ -1475,12 +1473,6 @@ Resource* ResourceFetcher::RequestResource(FetchParameters& params,
       if (resource) {
         policy = DetermineRevalidationPolicy(resource_type, params, *resource,
                                              is_static_data);
-        scoped_refptr<const SecurityOrigin> top_frame_origin =
-            resource_request.TopFrameOrigin();
-        if (top_frame_origin) {
-          same_top_frame_site_resource_cached =
-              resource->AppendTopFrameSiteForMetrics(*top_frame_origin);
-        }
       }
     }
   }
@@ -1491,7 +1483,7 @@ Resource* ResourceFetcher::RequestResource(FetchParameters& params,
 
   UpdateMemoryCacheStats(
       resource, MapToPolicyForMetrics(policy, resource, defer_policy), params,
-      factory, is_static_data, same_top_frame_site_resource_cached);
+      factory, is_static_data);
 
   switch (policy) {
     case RevalidationPolicy::kReload:
