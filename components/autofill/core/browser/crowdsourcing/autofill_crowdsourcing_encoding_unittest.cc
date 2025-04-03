9d5a19358d1e25f6e89e5bc076d1867d5467d402
Christoph Schwering <schwering@google.com>
2025-04-03 11:49:11 -0700

    [Autofill] Remove FormStructure::submission_event_
    This CL moves FormStructure::submission_event_ to
    EncodeUploadRequestOptions.
    
    Bug: 405558390
    Change-Id: Ie538eb8da1591fb4bac78162a14069e5d44d86ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6429693
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442324}

--- components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding_unittest.cc
+++ components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding_unittest.cc
@@ -249,8 +249,6 @@ TEST_F(AutofillCrowdsourcingEncoding, EncodeUploadRequest) {
 
   std::unique_ptr<FormStructure> form_structure =
       std::make_unique<FormStructure>(form);
-  form_structure->set_submission_event(
-      SubmissionIndicatorEvent::HTML_FORM_SUBMISSION);
   for (const std::unique_ptr<AutofillField>& fs_field : *form_structure) {
     fs_field->set_host_form_signature(form_structure->form_signature());
   }
@@ -289,6 +287,7 @@ TEST_F(AutofillCrowdsourcingEncoding, EncodeUploadRequest) {
                                    EMAIL_ADDRESS,
                                    PHONE_HOME_WHOLE_NUMBER};
   options.observed_submission = true;
+  options.submission_event = SubmissionIndicatorEvent::HTML_FORM_SUBMISSION;
 
   EXPECT_THAT(EncodeUploadRequest(*form_structure, options),
               ElementsAre(WithoutMetadataSerializesSameAs(upload)));
@@ -302,8 +301,6 @@ TEST_F(AutofillCrowdsourcingEncoding, EncodeUploadRequest) {
   }
 
   form_structure = std::make_unique<FormStructure>(form);
-  form_structure->set_submission_event(
-      SubmissionIndicatorEvent::HTML_FORM_SUBMISSION);
   ASSERT_EQ(form_structure->field_count(), possible_field_types.size());
   for (size_t i = 0; i < form_structure->field_count(); ++i) {
     form_structure->field(i)->set_host_form_signature(
