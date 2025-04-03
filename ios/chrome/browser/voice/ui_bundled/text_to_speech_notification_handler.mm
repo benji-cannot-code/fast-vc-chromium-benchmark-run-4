b35d21f950af94a58d970733a625d2f4fb9b0aa6
Sylvain Defresne <sdefresne@chromium.org>
2025-04-03 06:57:40 -0700

    [ios] Avoid unregistering observer when unnecessary
    For users that have thousands of tabs, accessing the default
    notification center to remove observer that are not installed
    takes too long.
    
    Omit the removal if the observers are not registered.
    
    Fixed: 406004525, 405250164
    Change-Id: I93fcc9ce6a5d17a0aaf6ad6df217f641543a4960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421826
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442118}

--- ios/chrome/browser/voice/ui_bundled/text_to_speech_notification_handler.mm
+++ ios/chrome/browser/voice/ui_bundled/text_to_speech_notification_handler.mm
@@ -19,8 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize TTSPlayer = _TTSPlayer;
 
 - (void)dealloc {
-  [self cancelPlayback];
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  self.enabled = NO;
 }
 
 #pragma mark Accessors
