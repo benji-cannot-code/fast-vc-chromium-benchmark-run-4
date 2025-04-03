5cef690d9745f393cbf958d3b75bbdc1b69d34af
Weizhong Xia <weizhong@google.com>
2025-04-03 16:19:58 -0700

    Log the start of run_wpt_tests.py
    This way, we can clearly see when the script started,
    and then collect data to compare performance.
    
    Bug: 380517608
    Change-Id: I158716f99e90266d5e118ac6846fc331cc49194b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6431628
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1442488}

--- third_party/blink/tools/run_wpt_tests.py
+++ third_party/blink/tools/run_wpt_tests.py
@@ -4,12 +4,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import multiprocessing
 import sys
 
-from blinkpy.wpt_tests import wpt_adapter
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger('run_wpt_tests')
 
 
 if __name__ == '__main__':
+    logger.info('Running web platform tests')
     multiprocessing.set_start_method('spawn')
+    from blinkpy.wpt_tests import wpt_adapter
     sys.exit(wpt_adapter.main(sys.argv[1:]))
