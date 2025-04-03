1231004fc3cb3fedafdb39ce9d05560945b266e7
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2025-04-03 04:54:58 -0700

    Roll inspector_protocol to 6d1ae0f13aae6ad381ca31b17b88a0f5af29ca94
    This brings in a fast path in JSONEncoder::HandleString8() that results
    in a 17% reduction in heap snapshot transfer time on YouTube.com.
    Transfer time before: 3225ms
    Transfer time after:  2664ms
    Snapshot size: 95.9 MB
    Nodes:  1,873,392
    Edges: 10,434,103
    
    Change-Id: I728c24f99ce214b77609d156c2728b4eacc77d0b
    Bug: 387077340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426290
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1442078}

--- third_party/inspector_protocol/crdtp/json.cc
+++ third_party/inspector_protocol/crdtp/json.cc
@@ -186,6 +186,14 @@ class JSONEncoder : public ParserHandler {
       return;
     state_.top().StartElement(out_);
     Emit('"');
+    // Fast path for input strings that can be emitted as-is.
+    if (std::all_of(chars.begin(), chars.end(), [](uint8_t c) {
+          return c != '"' && c != '\\' && c >= 32 && c <= 127;
+        })) {
+      Emit(chars);
+      Emit('"');
+      return;
+    }
     for (size_t ii = 0; ii < chars.size(); ++ii) {
       uint8_t c = chars[ii];
       if (c == '"') {
@@ -375,6 +383,9 @@ class JSONEncoder : public ParserHandler {
   }
   inline void Emit(const std::string& str) {
     out_->insert(out_->end(), str.begin(), str.end());
+  }
+  inline void Emit(const span<uint8_t>& bytes) {
+    out_->insert(out_->end(), bytes.begin(), bytes.end());
   }
 
   C* out_;
