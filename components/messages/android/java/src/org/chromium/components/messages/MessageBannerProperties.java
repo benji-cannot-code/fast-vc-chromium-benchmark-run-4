a77e2186e60a844a9e885d07550fd7db533835c3
Lijin Shen <lazzzis@google.com>
2025-04-03 15:02:33 -0700

    Add tap protection to message ui
    Disallow any touch event as soon as the animation starts. The message should become clickable again once the tap protection period ends.
    
    Bug: 356658477
    Change-Id: I3522cb07e0b9a4c6fbe9f88cbe12777ffc41b5d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6411166
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442451}

--- components/messages/android/java/src/org/chromium/components/messages/MessageBannerProperties.java
+++ components/messages/android/java/src/org/chromium/components/messages/MessageBannerProperties.java
@@ -151,6 +151,10 @@ public class MessageBannerProperties {
     static final WritableObjectPropertyKey<Runnable> ON_SECONDARY_BUTTON_CLICK =
             new WritableObjectPropertyKey<>();
 
+    // Supplier of whether the view is under tap protection.
+    static final WritableObjectPropertyKey<Supplier<Boolean>>
+            IS_WITHIN_TAP_PROTECTION_PERIOD_SUPPLIER = new WritableObjectPropertyKey();
+
     public static final PropertyKey[] ALL_KEYS =
             new PropertyKey[] {
                 MESSAGE_IDENTIFIER,
@@ -188,6 +192,7 @@ public class MessageBannerProperties {
                 PRIMARY_WIDGET_APPEARANCE,
                 ELEVATION,
                 MARGIN_TOP,
-                VISUAL_HEIGHT
+                VISUAL_HEIGHT,
+                IS_WITHIN_TAP_PROTECTION_PERIOD_SUPPLIER
             };
 }
