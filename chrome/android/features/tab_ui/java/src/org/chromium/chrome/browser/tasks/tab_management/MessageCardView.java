d5dc6f809cc7d169d944fabf6e1529fdb36bb6f9
Wenyu Fu <wenyufu@chromium.org>
2025-04-02 20:38:15 -0700

    [SurfaceColor] Migration MaterialCardViewNoShadow
    Direct mapping update: surface 2 -> surface container high
    
    Changes many surfaces, example:
    https://screenshot.googleplex.com/3UUSUHkaiVcBzJu
    
    Bug: 348667900
    Change-Id: Icd865e67b4ea4a48dd8dcf0076b63048e462c264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421010
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441950}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardView.java
@@ -17,7 +17,7 @@ import android.widget.LinearLayout;
 
 import org.chromium.base.Callback;
 import org.chromium.chrome.tab_ui.R;
-import org.chromium.components.browser_ui.styles.ChromeColors;
+import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.ui.widget.ButtonCompat;
 import org.chromium.ui.widget.ChromeImageView;
 import org.chromium.ui.widget.TextViewWithLeading;
@@ -172,8 +172,7 @@ class MessageCardView extends LinearLayout {
         }
         // Set dynamic color.
         GradientDrawable gradientDrawable = (GradientDrawable) getBackground();
-        gradientDrawable.setColor(
-                ChromeColors.getSurfaceColor(getContext(), R.dimen.default_elevation_2));
+        gradientDrawable.setColor(SemanticColorUtils.getCardBackgroundColor(getContext()));
     }
 
     /**
