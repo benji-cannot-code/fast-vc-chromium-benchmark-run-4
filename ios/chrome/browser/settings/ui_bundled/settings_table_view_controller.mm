8b70a8d16e8d6c7d192ff59ad2e0ba6c49973a75
Scott Yoder <scottyoder@google.com>
2025-04-03 03:32:44 -0700

    [ios] Fix crash displaying managed search engine in settings
    This change fixes a crash when a default search engine is configured
    by policy, but no name is provided.
    
    Fixed: 407516255
    Change-Id: I6d1db992f71235b3fd90b439f6e48b2ab1436e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427621
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442052}

--- ios/chrome/browser/settings/ui_bundled/settings_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/settings_table_view_controller.mm
@@ -1782,6 +1782,9 @@ struct EnhancedSafeBrowsingActivePromoData
   // Default search engine is enabled and set by policy.
   const std::string* status =
       dict.FindStringByDottedPath(DefaultSearchManager::kShortName);
+  if (!status) {
+    return @"";
+  }
   return base::SysUTF8ToNSString(*status);
 }
 
