d3f0aa3d00fe2544d18f982875af005ec36bd1ce
Mitsuru Oshima <oshima@chromium.org>
2025-04-03 14:06:16 -0700

    Do not hide transient windows in Overview/SplitView
    * Do not adjust the bounds for anchored bubble view. The z-order issue
      should be mostly working after divider is moved to desk container.
    * and don't adjust the bounds via offscreen logic during
      overview.
    * Above changes will fix the issue crbug.com/342459246.
    
    This dones't solve all z-order issue and incorrect bubble bounds
    issues. I'll address them separately.
    
    Bug: 388678800
    Bug: 355378538
    Bug: 398736027
    Bug: 41410374
    Bug: 342459246
    Test: Updated existing tests, and added new tests for swipe up scenario.
    
    Change-Id: Iba2d192b50024aad425b05cf66a9cd942a1249f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421175
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442416}

--- ash/wm/splitview/split_view_divider.cc
+++ ash/wm/splitview/split_view_divider.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/screen.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/view_targeter_delegate.h"
 #include "ui/views/view_utils.h"
 #include "ui/views/widget/widget.h"
@@ -799,11 +800,24 @@ void SplitViewDivider::RefreshStackingOrder() {
 }
 
 void SplitViewDivider::StartObservingTransientChild(aura::Window* transient) {
-  // Confine the bounds of a transient window if the given `transient` is a
+  // Confine the bounds of a transient window iif the given `transient` is a
   // bubble dialog or dialog window.
   if (!window_util::AsBubbleDialogDelegate(transient) &&
       !window_util::AsDialogDelegate(transient)) {
     return;
+  }
+
+  // Do not adjust if the bubble dialog is attached to an anchor.
+  auto* widget = views::Widget::GetTopLevelWidgetForNativeView(transient);
+  if (widget->widget_delegate() &&
+      widget->widget_delegate()->AsBubbleDialogDelegate()) {
+    auto* bubble_dialog_delegate =
+        widget->widget_delegate()->AsBubbleDialogDelegate();
+    bool has_anchor = bubble_dialog_delegate->GetAnchorView() &&
+                      !bubble_dialog_delegate->GetAnchorRect().IsEmpty();
+    if (has_anchor) {
+      return;
+    }
   }
 
   // Explicitly check and early return if the `transient` is the divider native
