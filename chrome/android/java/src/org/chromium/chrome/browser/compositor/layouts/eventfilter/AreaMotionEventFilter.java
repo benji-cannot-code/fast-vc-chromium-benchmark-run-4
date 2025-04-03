7d4a5fe02fb66488970bc3bba921dfda9beef792
Ted Choc <tedchoc@chromium.org>
2025-04-03 11:16:56 -0700

    Prevent generic motion event bleed through on the tab strip.
    Right clicking on the tab strip would result in the page context
    menu showing up from the underlying webpage. This is because
    generic motion events are dispatched by ContentView before the
    CompositorViewHolder could normally intercept.
    
    This exposes generic motion events to scene layers to consume,
    and for the AreaMotionEventFilter (used by the tab strip), this will
    consume motion event types that would generate an action on
    underlying layers.
    
    Bug: 391719844
    Change-Id: I22d94b161a1dc5308053be1a5ca9f6eb9254e29b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427483
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442295}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/eventfilter/AreaMotionEventFilter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/eventfilter/AreaMotionEventFilter.java
@@ -11,6 +11,8 @@ import android.view.MotionEvent;
 
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.ui.util.MotionEventUtils;
+
 /**
  * A {@link AreaMotionEventFilter} intercepts all events that start in a specific Rect on the
  * screen.
@@ -124,6 +126,21 @@ public class AreaMotionEventFilter extends MotionEventFilter {
                 mHasHoverEnterOrMoveEventInArea = false;
                 mHoverExitedArea = true;
                 return super.onInterceptHoverEventInternal(e);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean onGenericMotionEventInternal(MotionEvent e) {
+        // Prevent action generating motion events from leaking to underlying layers.
+        if ((MotionEventUtils.isMouseEvent(e) || MotionEventUtils.isTrackpadEvent(e))
+                && isMotionEventInArea(e)) {
+            int action = e.getActionMasked();
+            if (action == MotionEvent.ACTION_BUTTON_PRESS
+                    || action == MotionEvent.ACTION_BUTTON_RELEASE
+                    || action == MotionEvent.ACTION_SCROLL) {
+                return true;
             }
         }
         return false;
