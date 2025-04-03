01d6b8351bd7ae331679309d1cbef3856b848445
Maksim Sadym <sadym@google.com>
2025-04-03 07:19:33 -0700

    Close hidden targets in headless
    In headless shell, when session is closing, rely on agent_host to close
    the hidden targets.
    
    Follow-up after https://crrev.com/c/6330754, addressing comment
    https://crrev.com/c/6330754/comment/f288a6ea_da70e9d4/
    
    Bug: 378635448
    Change-Id: Icebe1633e472da493c38921d56e2951c79dc573d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6425423
    Commit-Queue: Maksim Sadym <sadym@google.com>
    Auto-Submit: Maksim Sadym <sadym@google.com>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442133}

--- third_party/blink/web_tests/http/tests/inspector-protocol/target/hidden-target-create.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/target/hidden-target-create.js
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   testRunner.browserP().Target.onDetachedFromTarget((event) => {
     testRunnerLog(event, 'DetachedFromTarget');
   });
+  testRunner.browserP().Target.onTargetDestroyed((event) => {
+    testRunnerLog(event, 'TargetDestroyed');
+  });
   /**
    * Filter out the targets that are already present in the existing targets set
    * keeping only the new targets. If the result is an error, return the error.
@@ -69,7 +72,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       `Filtered targets via browser session: `);
 
   testRunnerLog('Disconnect the parent session');
-  await newBrowserSession.disconnect();
+  newBrowserSession.disconnect();
+
+  await testRunner.browserP().Target.onceTargetDestroyed();
 
   // Verify the hidden target's session is closed.
   testRunnerLog(
