e845dc6b100aad445325d56e2c51f850e3a6a737
Michael Maltsev <michaelm@island.io>
2025-04-03 12:34:05 -0700

    Content verifier: Handle paths with . or .. components
    Relative paths which are specified in an extension's manifest (e.g.
    content scripts, service worker) are used to build extension URLs, such
    as: `chrome-extension://<extension_id>/src/service_worker.js`.
    
    When the URL is built, the relative path goes through some normalization
    steps, so the following are equivalent:
    
    * `src/service_worker.js`
    * `../src//./a/../service_worker.js`
    
    Improve the content verification path normalization logic to treat such
    paths as equivalent.
    
    Bug: 400340590
    Change-Id: I611e11aefe0205a28cd8f723a103319c4d8b6d7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6318499
    Commit-Queue: Michael Maltsev (R&D) <michaelm@island.io>
    Feels: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442354}

--- extensions/browser/content_verifier/content_verifier_utils.cc
+++ extensions/browser/content_verifier/content_verifier_utils.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "extensions/common/extension_features.h"
 
-namespace extensions {
-namespace content_verifier_utils {
+namespace extensions::content_verifier_utils {
 
 namespace {
 #if BUILDFLAG(IS_WIN)
@@ -66,5 +65,38 @@ CanonicalRelativePath CanonicalizeRelativePath(
   return CanonicalRelativePath(std::move(canonical_path));
 }
 
-}  // namespace content_verifier_utils
-}  // namespace extensions
+base::FilePath NormalizePathComponents(const base::FilePath& relative_path) {
+  CHECK(!relative_path.IsAbsolute());
+
+  base::FilePath normalized_path;
+  for (const auto& component : relative_path.GetComponents()) {
+    if (component == base::FilePath::kCurrentDirectory) {
+      // Do nothing.
+    } else if (component == base::FilePath::kParentDirectory) {
+      const auto base_name = normalized_path.BaseName();
+      if (base_name.empty() ||
+          base_name.value() == base::FilePath::kCurrentDirectory ||
+          base_name.value() == base::FilePath::kParentDirectory) {
+        normalized_path =
+            normalized_path.Append(base::FilePath::kParentDirectory);
+      } else {
+        normalized_path = normalized_path.DirName();
+      }
+    } else {
+      // This is just a regular component. Append it.
+      normalized_path = normalized_path.Append(component);
+    }
+  }
+
+  if (normalized_path.empty()) {
+    normalized_path = base::FilePath(base::FilePath::kCurrentDirectory);
+  }
+
+  if (relative_path.EndsWithSeparator()) {
+    normalized_path = normalized_path.AsEndingWithSeparator();
+  }
+
+  return normalized_path.NormalizePathSeparators();
+}
+
+}  // namespace extensions::content_verifier_utils
