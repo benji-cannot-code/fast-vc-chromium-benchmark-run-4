153fc02d7d7e702bd9d5de3993ee7a8ebf9a858b
Antonio Rivera <antoniori@google.com>
2025-04-03 10:46:52 -0700

    Cherrypick some starboard-related CLs from eureka.
    go/eurcl/1031631
    go/eurcl/1031413
    go/eurcl/1031216
    go/eurcl/1037091
    
    Note that this causes a shutdown crash on 3P linux TVs. The child CL
    (crrev.com/c/6427608) fixes this issue. I'm keeping the CLs separate to
    make the logic changes a bit clearer.
    
    Bug: b:357270172
    Change-Id: I84e87190dc9d4b8a3e01deb948ac4ec3c62bdee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421803
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Commit-Queue: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1442275}

--- chromecast/starboard/chromecast/starboard_adapter/src/cast_starboard_api_adapter_impl.h
+++ chromecast/starboard/chromecast/starboard_adapter/src/cast_starboard_api_adapter_impl.h
@@ -6,13 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_STARBOARD_CHROMECAST_STARBOARD_ADAPTER_SRC_CAST_STARBOARD_API_ADAPTER_IMPL_H_
 #define CHROMECAST_STARBOARD_CHROMECAST_STARBOARD_ADAPTER_SRC_CAST_STARBOARD_API_ADAPTER_IMPL_H_
 
-#include <mutex>
-#include <unordered_map>
-
-#if SB_API_VERSION >= 15
 #include <future>
+#include <mutex>
 #include <thread>
-#endif  // SB_API_VERSION >= 15
+#include <unordered_map>
 
 #include "chromecast/starboard/chromecast/starboard_adapter/public/cast_starboard_api_adapter.h"
 
@@ -35,9 +32,10 @@ class CastStarboardApiAdapterImpl : public CastStarboardApiAdapter {
 
  private:
   void SbEventHandleInternal(const SbEvent* event);
+  void Initialize();
+  void Release();
 
   // CastStarboardApiAdapter implementation:
-  bool EnsureInitialized() override;
   void Subscribe(void* context,
                  CastStarboardApiAdapterImplCB callback) override;
   void Unsubscribe(void* context) override;
@@ -46,9 +44,9 @@ class CastStarboardApiAdapterImpl : public CastStarboardApiAdapter {
 
 #if SB_API_VERSION >= 15
   std::unique_ptr<std::thread> sb_main_;
+#endif  // SB_API_VERSION >= 15
   std::promise<bool> init_p_;
   std::future<bool> init_f_;
-#endif  // SB_API_VERSION >= 15
   SbWindow window_ = kSbWindowInvalid;
   std::mutex lock_;
   bool initialized_;
