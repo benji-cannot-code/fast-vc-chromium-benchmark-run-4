b3ec67b296f1b7e10c1b3e267224fc57a011303a
Caroline Rising <corising@chromium.org>
2025-04-03 07:13:29 -0700

    Store ContentsWebViews in a vector in MultiContentsView and update SetWebContents
    This should visually be a no-op. Storing ContentsWebViews in a vector
    and making the first in the vector always visible will make
    MultiContentsView more scalable without really increasing complexity.
    Changing SetWebContents from being based on whether it is the active
    ContentsWebView to an index is more reliable and allows us to set the
    webcontents before setting the active position.
    
    Bug: 408052275
    Change-Id: I4514acac7273d8f4ec91f32ba1029ed4a4a4929f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427883
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442131}

--- chrome/browser/ui/views/frame/multi_contents_view_interactive_uitest.cc
+++ chrome/browser/ui/views/frame/multi_contents_view_interactive_uitest.cc
@@ -54,7 +54,7 @@ class MultiContentsViewUiTest : public InteractiveBrowserTest {
         Do([&]() {
           content::WebContents* inactive_contents =
               tab_strip_model()->GetWebContentsAt(1);
-          multi_contents_view()->SetWebContents(inactive_contents, false);
+          multi_contents_view()->SetWebContentsAtIndex(inactive_contents, 1);
         }),
         PollView(kResizeLoadObserver,
                  MultiContentsResizeArea::kMultiContentsResizeAreaElementId,
