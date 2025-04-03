aa40857451886dad62508d0f55a5134ee9755567
dpapad <dpapad@chromium.org>
2025-04-03 10:18:49 -0700

    WebUI: Remove unnecessary "not ios" assertion in focus_without_ink.ts.
    The assertion is no longer needed, as the minimum supported iOS version
    has advanced to iOS 16 since this check was added. Moreover, this check
    could lead to a Javascript runtime error like the following (revealed by
    error reporting):
    
    Uncaught Error: Assertion failed: pointerdown doesn't work on iOS
    (chrome://new-tab-page)
    
    When modifying the user agent via DevTools "Network predictions" panel
    to return the Chrome iOS user agent, even if running on Desktop.
    
    Fixed: 399203749
    Change-Id: Id90b7170ede5da427e0d8125f4ad29eace5dd760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427174
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442257}

--- ui/webui/resources/js/focus_without_ink.ts
+++ ui/webui/resources/js/focus_without_ink.ts
@@ -3,15 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// clang-format off
 import {assert} from './assert.js';
-import {isIOS} from './platform.js';
-// clang-format on
 
 
-let hideInk = false;
-
-assert(!isIOS, 'pointerdown doesn\'t work on iOS');
+let hideInk: boolean = false;
 
 document.addEventListener('pointerdown', function() {
   hideInk = true;
