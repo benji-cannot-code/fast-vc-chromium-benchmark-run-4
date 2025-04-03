948948670a142462539d5381f7b670c9deda518b
Kaan Alsan <alsan@chromium.org>
2025-04-03 09:01:35 -0700

    Fix PageActionView padding when label is collapsed due to space limit
    Prior to this CL, PageActionView would apply custom insets to the border
    whenever it is in a chip state. However, this created problems when the
    chip's label was collapsed due to space restrictions, since the icon
    would be off-centered, but the label is still technically showing.
    
    Updating the border in response to bounds changes is also not the ideal
    approach since we'd be creating a layout loop.
    
    Instead, this CL updates PageActionView to use its parent class'
    "expanded label insets" option (added in parent CLs) to handle this.
    
    Change-Id: Ia0fcf729edb576d659302f90f3870e48ce6b45ea
    Bug: 403333986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416163
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442193}

--- chrome/browser/ui/views/page_action/page_action_view.cc
+++ chrome/browser/ui/views/page_action/page_action_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/color/color_id.h"
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/font_list.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/view_class_properties.h"
@@ -48,6 +49,7 @@ PageActionView::PageActionView(actions::ActionItem* action_item,
   SetUseTonalColorsWhenExpanded(true);
   SetBackgroundVisibility(BackgroundVisibility::kWithLabel);
   UpdateBorder();
+  SetExpandedLabelAdditionalInsets(views::Inset1D(4, 8));
 
   label_visibility_changed_subscription_ =
       label()->AddVisibleChangedCallback(base::BindRepeating(
@@ -125,26 +127,11 @@ void PageActionView::ViewHierarchyChanged(
   View::ViewHierarchyChanged(details);
   if (details.is_add && details.child == this) {
     UpdateIconImage();
-    UpdateBorder();
   }
 }
 
 void PageActionView::UpdateBorder() {
-  gfx::Insets insets = icon_insets_;
-  if (IsChipVisible()) {
-    constexpr int kInsetsLeftPaddingMax = 4;
-    constexpr int kInsetsRightPaddingMax = 8;
-
-    // Set the padding according to the progress of the expand/collapse
-    // animation.
-    const int left_padding = GetWidthBetween(0, kInsetsLeftPaddingMax);
-    const int right_padding = GetWidthBetween(0, kInsetsRightPaddingMax);
-    insets += gfx::Insets().set_left_right(left_padding, right_padding);
-  }
-
-  if (GetInsets() != insets) {
-    SetBorder(views::CreateEmptyBorder(insets));
-  }
+  SetBorder(views::CreateEmptyBorder(icon_insets_));
 }
 
 bool PageActionView::ShouldShowSeparator() const {
@@ -218,7 +205,6 @@ bool PageActionView::IsBubbleShowing() const {
 
 void PageActionView::OnLabelVisibilityChanged() {
   UpdateBackground();
-  UpdateBorder();
   UpdateLabelColors();
   UpdateIconImage();
   chip_visibility_changed_callbacks_.Notify(this);
