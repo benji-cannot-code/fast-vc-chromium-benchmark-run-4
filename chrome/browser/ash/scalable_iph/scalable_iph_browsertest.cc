ad3ca1d7081e7fb3da1d4c2d617763a52f43e1ad
alancutter <alancutter@google.com>
2025-04-02 18:06:57 -0700

    Use SetPreinstallUrlAllowListForTesting() instead of removing kDisableDefaultApps in tests
    This CL adds an allow list for preinstalled web apps that need to be
    covered by tests. Instead of enabling all preinstalled web apps this
    allows tests to be specific about which.
    
    When all preinstalled web apps are enabled in a test some will require
    internet access to install and wait 30s, with multiple of these queued
    up in a global install queue this can cause the tests to timeout.
    
    The addition of NotebookLM pushed these tests over the line such that
    they started breaching the test timeout threshold.
    https://chromium-review.googlesource.com/c/chromium/src/+/6389898
    
    This CL updates the tests allow list the apps they actually care about
    and avoid being affected by unrelated preinstalls.
    
    On my machine this CL takes
    All/GeminiAppInteractiveUiIneligibilityTest.AbsentFromAppList/kFeatureFlagDisabled
    down from 26 seconds to 4 seconds.
    
    Bug: b:402986247
    Change-Id: I268bb6490037e64b7a61477e8fae9fa058db37d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6395835
    Reviewed-by: Sophia Lin <sophialin@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441919}

--- chrome/browser/ash/scalable_iph/scalable_iph_browsertest.cc
+++ chrome/browser/ash/scalable_iph/scalable_iph_browsertest.cc
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/multi_user/multi_user_window_manager_helper.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.h"
+#include "chrome/browser/web_applications/preinstalled_web_apps/youtube.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -101,14 +103,6 @@ BASE_FEATURE(kScalableIphTestTwo,
 void OverrideStoredPermanentCountry(std::string_view country_code) {
   CHECK(g_browser_process->variations_service()->OverrideStoredPermanentCountry(
       std::string(country_code)));
-}
-
-bool IsGoogleChrome() {
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  return true;
-#else
-  return false;
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 }
 
 void LockAndUnlockSession() {
@@ -296,15 +290,23 @@ class ScalableIphBrowserTestFeatureOffDebugOn : public ScalableIphBrowserTest {
   }
 };
 
+// Preinstalled apps only deploy on Google Chrome branded builds of Chromium.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 class ScalableIphBrowserTestPreinstallApps : public ScalableIphBrowserTest {
  public:
+  ScalableIphBrowserTestPreinstallApps()
+      : scoped_preinstall_url_allow_list_(
+            web_app::SetPreinstallUrlAllowListForTesting(
+                {{web_app::GetConfigForYouTube().install_url}})) {}
+
   void SetUpDefaultCommandLine(base::CommandLine* command_line) override {
     ScalableIphBrowserTest::SetUpDefaultCommandLine(command_line);
 
-    command_line->RemoveSwitch(switches::kDisableDefaultApps);
     command_line->AppendSwitch(
         ash::switches::kAllowDefaultShelfPinLayoutIgnoringSync);
   }
+
+  web_app::ScopedPreinstallUrlAllowList scoped_preinstall_url_allow_list_;
 };
 
 class ScalableIphBrowserTestHelpApp
@@ -344,6 +346,7 @@ class ScalableIphBrowserTestHelpAppParameterized
     ScalableIphBrowserTestHelpApp::SetUp();
   }
 };
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 class ScalableIphBrowserTestPerksMinecraftRealms
     : public ScalableIphBrowserTest {
@@ -1246,12 +1249,10 @@ IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestMultipleIphs, OneIphAtATime) {
   testing::Mock::VerifyAndClearExpectations(mock_tracker());
 }
 
+// Preinstalled apps only deploy on Google Chrome branded builds of Chromium.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestPreinstallApps,
                        AppListItemActivationWebApp) {
-  if (!IsGoogleChrome()) {
-    GTEST_SKIP()
-        << "Google Chrome is required for preinstall apps used by this test";
-  }
 
   // Those constants in `scalable_iph` must be synced with ones in `web_app`.
   // Test them in this test case.
@@ -1284,10 +1285,6 @@ IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestPreinstallApps,
 // TODO(crbug.com/328713274): Test is flaky.
 IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestPreinstallApps,
                        DISABLED_ShelfItemActivationWebApp) {
-  if (!IsGoogleChrome()) {
-    GTEST_SKIP()
-        << "Google Chrome is required for preinstall apps used by this test";
-  }
 
   apps::AppReadinessWaiter(browser()->profile(),
                            scalable_iph::kWebAppYouTubeAppId)
@@ -1300,13 +1297,9 @@ IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestPreinstallApps,
 }
 
 IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestHelpApp, HelpAppPinnedToShelf) {
-  if (!IsGoogleChrome()) {
-    GTEST_SKIP()
-        << "Google Chrome is required for preinstall apps used by this test";
-  }
-
   EXPECT_TRUE(ash::ShelfModel::Get()->IsAppPinned(ash::kHelpAppId));
 }
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 INSTANTIATE_TEST_SUITE_P(
     Perks,
@@ -2060,6 +2053,7 @@ IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestMinor, ScalableIphNotAvailable) {
             ScalableIphFactory::GetForBrowserContext(browser()->profile()));
 }
 
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 INSTANTIATE_TEST_SUITE_P(
     NoHelpAppPin,
     ScalableIphBrowserTestHelpAppParameterized,
@@ -2079,3 +2073,4 @@ IN_PROC_BROWSER_TEST_P(ScalableIphBrowserTestHelpAppParameterized,
                        HelpAppNotPinnedToShelf) {
   EXPECT_FALSE(ash::ShelfModel::Get()->IsAppPinned(ash::kHelpAppId));
 }
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
