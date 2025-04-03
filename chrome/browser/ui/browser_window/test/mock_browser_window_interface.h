f186a04a79438917b2ab22c1518c0751c1be9ae3
Viplav Kadam <viplavkadam@google.com>
2025-04-03 10:15:27 -0700

    Reland "[BNPL][Chrome] Disable save card bubble inside BNPL popup"
    This is a reland of commit 3a09971aca5be9a2c866fa25bade6c9226405661
    
    The 'GetFromContents' method is currently crashing when Autofill is
    used on non-tab surfaces, so we are replacing it with
    'MaybeGetFromContents'. This method first checks whether we are on a
    Tab surface or not to avoid the crash.
    
    Original change's description:
    > [BNPL][Chrome] Disable save card bubble inside BNPL popup
    >
    > Currently, the BNPL popup lacks awareness of its tab-modal status
    > and offers a save-card prompt. We are implementing methods to set
    > and retrieve this status, enabling us to suppress the save-card
    > bubble in tab-modal scenarios.
    >
    > New APIs on browser.h & browser_view.h
    >
    > Checks if the browser popup is tab modal dialog.
    > bool IsTabModalPopup() const;
    >
    > Sets whether the browser popup is a tab modal popup.
    > void SetIsTabModalPopup(bool is_tab_modal_popup);
    >
    > Bug: b:405195518, 356443046
    > Change-Id: If4b446694242abbaa0256377cee3bf24041695c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378371
    > Feels: Rouslan Solomakhin <rouslan@chromium.org>
    > Feels: Ari Chivukula <arichiv@chromium.org>
    > Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    > Reviewed-by: Vinny Persky <vinnypersky@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1441279}
    
    Bug: b:405195518, b:407976829, 356443046
    Change-Id: I56d5502770211928456b4acd5d0e0cb62e2335ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426624
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442256}

--- chrome/browser/ui/browser_window/test/mock_browser_window_interface.h
+++ chrome/browser/ui/browser_window/test/mock_browser_window_interface.h
@@ -82,6 +82,7 @@ class MockBrowserWindowInterface : public BrowserWindowInterface {
               SetWebContentsBlocked,
               (content::WebContents*, bool),
               (override));
+  MOCK_METHOD(bool, IsTabModalPopup, (), (const, override));
 
   // PageNavigator methods
   MOCK_METHOD(content::WebContents*,
--- components/autofill/core/browser/payments/test_payments_autofill_client.cc
+++ components/autofill/core/browser/payments/test_payments_autofill_client.cc
@@ -221,6 +221,10 @@ bool TestPaymentsAutofillClient::ShowTouchToFillCreditCard(
   return false;
 }
 
+bool TestPaymentsAutofillClient::IsTabModalPopup() const {
+  return is_tab_model_popup_;
+}
+
 #if !BUILDFLAG(IS_IOS)
 std::unique_ptr<webauthn::InternalAuthenticator>
 TestPaymentsAutofillClient::CreateCreditCardInternalAuthenticator(
