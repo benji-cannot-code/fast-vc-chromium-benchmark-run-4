97c4fbb1520f1de9cbd1e8a754b600d7ab64d962
Eleanor Lee <eleanorlee@google.com>
2025-04-04 14:14:42 -0700

    [Surface Color] Omnibox & toolbar
    Light:
    Toolbar: surface 0 -> surface
    Omnibox bg: surface 4 -> surface container high
    
    Dark:
    Toolbar: Surface 0 -> Surface
    Omnibox bg: surface 4 -> surface container highest
    
    Before:
    https://screenshot.googleplex.com/4dX62JRmShEBsBw
    https://screenshot.googleplex.com/4B2SZSYpaF5pPBh
    
    After:
    https://screenshot.googleplex.com/9kFF4eMkD4FscZV
    https://screenshot.googleplex.com/6jpDFSwPNBaWHgk
    
    
    ChromeColors#getDefaultThemeColor, which is used for the toolbar bg, is already set to colorSurface.
    
    Bug: 406890625
    Change-Id: I777a357f5be687fe2da922e66ffb7ad316fc0d01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405708
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442928}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
@@ -55,6 +55,7 @@ import androidx.annotation.Px;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.browser.customtabs.CustomTabsIntent.CloseButtonPosition;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.MarginLayoutParamsCompat;
 import androidx.core.widget.ImageViewCompat;
 
@@ -2408,7 +2409,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
                             getContext(), R.drawable.custom_tabs_url_bar_omnibox_bg);
             mOmniboxBackground.mutate();
             mOmniboxBackground.setTint(
-                    ChromeColors.getSurfaceColor(getContext(), R.dimen.toolbar_text_box_elevation));
+                    ContextCompat.getColor(getContext(), R.color.toolbar_text_box_bg_color));
             mLocationBarFrameLayout.setBackground(mOmniboxBackground);
             var lp = mLocationBarFrameLayout.getLayoutParams();
             lp.height =
