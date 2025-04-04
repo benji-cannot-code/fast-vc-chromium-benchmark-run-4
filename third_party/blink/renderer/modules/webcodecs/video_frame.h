c8d4dd5e8fb8f606bbcfc16043fbb8b5c69d69ef
Eugene Zemtsov <eugene@chromium.org>
2025-04-04 13:48:27 -0700

    webcodecs: Stop reporting VideoFrame's external memory usage to v8
    Video frames (especially 8k frames) take a lot of memory and when
    a frame is created and reported to v8 it immediately triggers a major GC.
    This causes scores of GCs per second and interferes with playback smoothness.
    
    Since we have VideoFrames.close() and don't rely on GC to free VideoFrames
    there is no practical benefit in reporting external memory usage.
    
    If VideoFrames.close() hasn't been called we have a console error message when this happens so that developers know when they've made a mistake.
    
    Bug: 407812151
    Change-Id: I228a039a5a29642cdb1a8fc5e2c37618813e5f56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432486
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442903}

--- third_party/blink/renderer/modules/webcodecs/video_frame.h
+++ third_party/blink/renderer/modules/webcodecs/video_frame.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webcodecs/array_buffer_util.h"
 #include "third_party/blink/renderer/modules/webcodecs/video_frame_handle.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
-#include "third_party/blink/renderer/platform/bindings/v8_external_memory_accounter.h"
 #include "third_party/blink/renderer/platform/graphics/predefined_color_space.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -142,7 +141,6 @@ class MODULES_EXPORT VideoFrame final : public ScriptWrappable,
   bool IsOpaque() const override;
   bool IsAccelerated() const override;
 
-  void ResetExternalMemory();
   void ConvertAndCopyToRGB(scoped_refptr<media::VideoFrame> frame,
                            const gfx::Rect& src_rect,
                            const VideoFrameLayout& dest_layout,
@@ -172,7 +170,6 @@ class MODULES_EXPORT VideoFrame final : public ScriptWrappable,
   Member<DOMRectReadOnly> visible_rect_;
   Member<VideoColorSpace> color_space_;
   Member<VideoColorSpace> empty_color_space_;
-  V8ExternalMemoryAccounter external_memory_accounter_;
 };
 
 }  // namespace blink
