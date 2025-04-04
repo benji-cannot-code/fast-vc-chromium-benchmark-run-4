94fe9f3bf5937b451e901cbbb2d28f97cfb3a7ad
Florian Jacky <fjacky@google.com>
2025-04-04 11:08:25 -0700

    Remove default arguments from HatsServiceAndroid
    Default arguments on virtual or override methods are prohibited by clang-tidy (https://clang.llvm.org/extra/clang-tidy/checks/google/default-arguments.html)
    
    Change-Id: I0707c88a1a50e18ce9432b5d5a9486d5d95d9586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432950
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442780}

--- chrome/browser/ui/android/hats/survey_client_android_browsertest.cc
+++ chrome/browser/ui/android/hats/survey_client_android_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/android/hats/hats_service_android.h"
 #include "chrome/browser/ui/android/hats/test/test_survey_utils_bridge.h"
+#include "chrome/browser/ui/hats/hats_service.h"
 #include "chrome/browser/ui/hats/hats_service_factory.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "components/messages/android/message_dispatcher_bridge.h"
@@ -124,7 +125,8 @@ IN_PROC_BROWSER_TEST_F(SurveyClientAndroidBrowserTest, LaunchSurvey) {
         kTestSurveyTrigger, web_contents(), kTestSurveyProductSpecificBitsData,
         kTestSurveyProductSpecificStringData,
         base::BindOnce(&SurveyObserver::Accept, observer.GetWeakPtr()),
-        base::BindOnce(&SurveyObserver::Dismiss, observer.GetWeakPtr()));
+        base::BindOnce(&SurveyObserver::Dismiss, observer.GetWeakPtr()),
+        std::nullopt, HatsService::SurveyOptions());
     EXPECT_TRUE(waiter.Wait());
   }
 
