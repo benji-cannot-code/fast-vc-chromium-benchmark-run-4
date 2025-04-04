a562ee92ed91d802e4314c2f618bfa3b0d774bd3
dpapad <dpapad@chromium.org>
2025-04-04 13:10:37 -0700

    Remove useDefineForClassFields=false in a couple more c/b/r folders.
    Specifically omnibox_popup/ and side_panel/shared/.
    
    Bug: 389737066
    Change-Id: I71f6e1f16e53561b2df9353a935165a265c937cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433805
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442872}

--- chrome/browser/resources/side_panel/shared/sp_footer.ts
+++ chrome/browser/resources/side_panel/shared/sp_footer.ts
@@ -37,7 +37,7 @@ export class SpFooterElement extends CrLitElement {
     };
   }
 
-  pinned: boolean = false;
+  accessor pinned: boolean = false;
 
   override firstUpdated() {
     this.setAttribute('role', 'toolbar');
