ec067b77efda7eb46e6e3ae0169890f6cf3e3966
James Cook <jamescook@chromium.org>
2025-04-04 13:39:52 -0700

    extensions: Add stub extension disable UI function for desktop Android
    AddExtensionDisabledError() is a dependency of
    ChromeExtensionRegistrarDelegate, which we are trying to port to
    desktop Android soon.
    
    Take a similar approach to the extension error UI in:
    https://chromium-review.googlesource.com/c/chromium/src/+/6334536
    
    Rename the existing UI file to _desktop.cc and add an _android.cc file
    with a NOTIMPLEMENTED() and TODO.
    
    Bug: 394968360
    Change-Id: I3495c06e65a86f5de841fbb7717689540fbc528e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6431410
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442901}

---
+++ chrome/browser/extensions/extension_disabled_ui_android.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/extension_disabled_ui.h"
+
+#include "base/logging.h"
+#include "base/notimplemented.h"
+
+namespace extensions {
+
+void AddExtensionDisabledError(Profile* profile,
+                               const Extension* extension,
+                               bool is_remote_install) {
+  // TODO(crbug.com/399680111): Android will need a special error UI because it
+  // cannot use the views implementation of the error bubble.
+  NOTIMPLEMENTED() << "Extension disabled error";
+}
+
+}  // namespace extensions
