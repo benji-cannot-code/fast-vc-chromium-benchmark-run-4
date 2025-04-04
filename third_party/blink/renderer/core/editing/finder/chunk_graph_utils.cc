7f769d23e0f8feff09a62159603e4a13b79634fc
Kent Tamura <tkent@chromium.org>
2025-04-04 13:02:04 -0700

    FindRuby: Fix an issue of nested rubies
    If a paragraph contains multiple <ruby>s, and the former one has
    only a single annotation, and the latter one has two nested
    annotations, FindBuffer::SerializeLevelInGraph() failed to
    generate text after the former <ruby> for the second-level
    annotation.
    
    Bug: 408309951
    Change-Id: I143898ad5e590b397e492960ce83b1c2f0d8bb19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433767
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442861}

--- third_party/blink/renderer/core/editing/finder/chunk_graph_utils.cc
+++ third_party/blink/renderer/core/editing/finder/chunk_graph_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/finder/find_buffer.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
 
 namespace blink {
@@ -407,8 +408,10 @@ const CorpusChunk* CorpusChunk::FindNext(const String& level) const {
   }
   wtf_size_t delimiter_index = level.ReverseFind(kLevelDelimiter);
   if (delimiter_index == kNotFound) {
-    // No link for `level`. It means the graph is incorrect.
-    return nullptr;
+    // No link for `level`. We should apply the base level link.
+    return RuntimeEnabledFeatures::FindNestedAnnotationFixEnabled()
+               ? FindNext(String(kBaseLevel))
+               : nullptr;
   }
   return FindNext(level.Substring(0, delimiter_index));
 }
