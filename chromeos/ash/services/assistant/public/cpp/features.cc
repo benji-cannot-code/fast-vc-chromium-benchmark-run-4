6bf16e95abdc90c0115e34267ada19d2dc73bbcd
Sven Zheng <svenzheng@google.com>
2025-04-04 10:57:27 -0700

    [Code Health] Remove AssistantWaitScheduling feature
    The feature was already enabled.
    
    Bug: 356236002
    Change-Id: I58a0a3f5af6fdfedcdabaf7d6c21381512b5f5dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427484
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442771}

--- chromeos/ash/services/assistant/public/cpp/features.cc
+++ chromeos/ash/services/assistant/public/cpp/features.cc
@@ -24,10 +24,6 @@ BASE_FEATURE(kAssistantDebugging,
              "AssistantDebugging",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAssistantWaitScheduling,
-             "AssistantWaitScheduling",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kEnableDspHotword,
              "EnableDspHotword",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -94,10 +90,6 @@ bool IsStereoAudioInputEnabled() {
 
 bool IsVoiceMatchDisabled() {
   return base::FeatureList::IsEnabled(kDisableVoiceMatch);
-}
-
-bool IsWaitSchedulingEnabled() {
-  return base::FeatureList::IsEnabled(kAssistantWaitScheduling);
 }
 
 bool IsLibAssistantSandboxEnabled() {
