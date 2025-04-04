d9e2751ad6b6c00ff14f578f5a8812b0d66a7b5c
Filipa Senra <fsenra@google.com>
2025-04-04 02:11:50 -0700

    [iOS][DBD] Update browsing history strings
    This CL follows https://crrev.com/c/6387439. It updates the iOS
    counterpart strings to use unique domains.
    
    Screenshot: http://screen/BzxiwKxMSuhurhe.png
    
    Fixed: 396648088
    Change-Id: I0bf38d8f113d9c9aa7f7139914937a08a989ab55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6429426
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1442632}

--- components/browsing_data/core/browsing_data_utils_unittest.cc
+++ components/browsing_data/core/browsing_data_utils_unittest.cc
@@ -171,7 +171,7 @@ TEST_F(BrowsingDataUtilsTest, PasswordsCounterResult) {
   store->ShutdownOnUIThread();
 }
 
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
 // Tests the output of the History counter.
 TEST_F(BrowsingDataUtilsTest, HistoryCounterResult) {
   history::HistoryService history_service;
@@ -223,7 +223,7 @@ TEST_F(BrowsingDataUtilsTest, HistoryCounterResult) {
   }
 }
 
-#else  // !BUILDFLAG(IS_ANDROID)
+#else  // !(BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS))
 
 // Tests the output of the History counter.
 TEST_F(BrowsingDataUtilsTest, Deprecated_HistoryCounterResult) {
@@ -265,6 +265,6 @@ TEST_F(BrowsingDataUtilsTest, Deprecated_HistoryCounterResult) {
   }
 }
 
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
 
 }  // namespace browsing_data
