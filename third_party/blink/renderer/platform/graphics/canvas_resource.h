04413a3bc124834ecd5cc2d3c942e90eb8abfd35
Colin Blundell <blundell@chromium.org>
2025-04-04 01:22:52 -0700

    [BLink] Eliminate CanvasResource::IsRecycleable()
    This is now called only on CanvasResourceSharedImage instances, so we
    can inline that class' implementation at the callsite (note: in the
    current organization CanvasResourceSI::IsLost() will always return false
    there because the code structure in CanvasResource means that
    OnResourceReturnedByCompositor() won't be called in that case, but I
    chose to leave the check here for future-proofing).
    
    Bug: 352263194
    Change-Id: I332e2e24cf0abef05f07917a7b6a6826a61d14e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419488
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442617}

--- third_party/blink/renderer/platform/graphics/canvas_resource.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource.h
@@ -86,14 +86,6 @@ class PLATFORM_EXPORT CanvasResource
 
   bool HasLastUnrefCallback() { return !!last_unref_callback_; }
 
-  // We perform a lazy copy on write if the canvas content needs to be updated
-  // while its current resource is in use. In order to avoid re-allocating
-  // resources, its preferable to reuse a resource if its no longer in use.
-  // This API indicates whether a resource can be recycled.  This method does
-  // not however check whether the resource is still in use (e.g. has
-  // outstanding references).
-  virtual bool IsRecycleable() const = 0;
-
   // Returns true if this instance creates TransferableResources for usage with
   // GPU compositing.
   virtual bool CreatesAcceleratedTransferableResources() const = 0;
@@ -252,7 +244,6 @@ class PLATFORM_EXPORT CanvasResourceSharedImage final : public CanvasResource {
       gpu::SharedImageUsageSet shared_image_usage_flags);
   ~CanvasResourceSharedImage() override;
 
-  bool IsRecycleable() const final { return !IsLost(); }
   bool CreatesAcceleratedTransferableResources() const override {
     return !GetClientSharedImage()->is_software();
   }
@@ -384,7 +375,6 @@ class PLATFORM_EXPORT ExternalCanvasResource final : public CanvasResource {
       base::WeakPtr<CanvasResourceProvider>);
 
   ~ExternalCanvasResource() override;
-  bool IsRecycleable() const final { return IsValid(); }
   bool IsValid() const override;
   bool CreatesAcceleratedTransferableResources() const override { return true; }
   void NotifyResourceLost() override { resource_is_lost_ = true; }
@@ -442,7 +432,6 @@ class PLATFORM_EXPORT CanvasResourceSwapChain final : public CanvasResource {
       base::WeakPtr<WebGraphicsContext3DProviderWrapper>,
       base::WeakPtr<CanvasResourceProvider>);
   ~CanvasResourceSwapChain() override;
-  bool IsRecycleable() const final { return IsValid(); }
   bool IsValid() const override;
   bool CreatesAcceleratedTransferableResources() const override { return true; }
   void NotifyResourceLost() override {
