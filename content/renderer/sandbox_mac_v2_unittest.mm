a24c4798533209263fac90c88cccb6b8830a3001
Sven Zheng <svenzheng@google.com>
2025-04-04 10:57:53 -0700

    [Code Health] Remove MacSyscallSandbox feature
    The feature will not be used in the near future.
    
    Bug: 356623853, 40637835
    Change-Id: I0b09fefb027a7e4c6b304b5a0ff4d70d2e6d723a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419522
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442772}

--- content/renderer/sandbox_mac_v2_unittest.mm
+++ content/renderer/sandbox_mac_v2_unittest.mm
@@ -41,8 +41,7 @@ namespace {
 
 void SetParametersForTest(SandboxSerializer* serializer,
                           const base::FilePath& logging_path,
-                          const base::FilePath& executable_path,
-                          bool use_syscall_filter) {
+                          const base::FilePath& executable_path) {
   bool enable_logging = true;
   CHECK(serializer->SetBooleanParameter(sandbox::policy::kParamEnableLogging,
                                         enable_logging));
@@ -76,9 +75,6 @@ void SetParametersForTest(SandboxSerializer* serializer,
 
   CHECK(serializer->SetParameter(sandbox::policy::kParamExecutablePath,
                                  executable_path.value()));
-
-  CHECK(serializer->SetBooleanParameter(sandbox::policy::kParamFilterSyscalls,
-                                        use_syscall_filter));
 }
 
 }  // namespace
@@ -108,11 +104,7 @@ MULTIPROCESS_TEST_MAIN(SandboxProfileProcess) {
   const base::FilePath log_file = temp_path.Append("log-file");
   const base::FilePath exec_file("/bin/ls");
 
-  // TODO(crbug.com/40273168): re-enable syscall filter for this test.
-  // SandboxV2Test.SandboxProfileTest uses system() which uses a denied syscall,
-  // which should cause the test to fail.
-  SetParametersForTest(&serializer, log_file, exec_file,
-                       /*use_syscall_filter=*/false);
+  SetParametersForTest(&serializer, log_file, exec_file);
 
   std::string error, serialized;
   CHECK(serializer.SerializePolicy(serialized, error)) << error;
