9e2e27d30107390e4c59285d05eaa2271506489c
Andrea Gomez <andreaxg@google.com>
2025-04-04 15:57:07 -0700

    Settings: Logic fix for checking if there are supported languages.
    canEnableSomeSupportedLanguage_ was returning true when languages were
    undefined when it should've been returning false.
    
    Bug: 401376276
    Change-Id: I7564f94126c92c93f5bfc88ebfddb021862fbac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6434720
    Auto-Submit: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1442974}

--- chrome/browser/resources/settings/languages_page/languages_page.ts
+++ chrome/browser/resources/settings/languages_page/languages_page.ts
@@ -127,9 +127,7 @@ export class SettingsLanguagesPageElement extends
    */
   private onAddLanguagesClick_(e: Event) {
     e.preventDefault();
-    if (this.languages === undefined) {
-      return;
-    }
+    assert(this.languages);
     this.languageSettingsMetricsProxy_.recordPageImpressionMetric(
         LanguageSettingsPageImpressionType.ADD_LANGUAGE);
     this.addLanguagesDialogLanguages_ = this.languages.supported.filter(
@@ -168,7 +166,7 @@ export class SettingsLanguagesPageElement extends
    * @return True if there is at least one available language.
    */
   private canEnableSomeSupportedLanguage_(languages?: LanguagesModel): boolean {
-    return languages === undefined || languages.supported.some(language => {
+    return languages !== undefined && languages.supported.some(language => {
       return this.languageHelper.canEnableLanguage(language);
     });
   }
