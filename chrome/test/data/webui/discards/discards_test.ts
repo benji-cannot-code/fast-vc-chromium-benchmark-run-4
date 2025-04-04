b89c530d108a802f4b1cdbeed8524b7673aabd1c
Kuo-Hsin Yang <vovoy@google.com>
2025-04-04 12:31:52 -0700

    [discards webui] Migrate to Lit, part 2
    - Migrate discards_tab.ts
    
    Bug: b:388544211
    Change-Id: I68377b0c9e1f48a597a1eb3d4e779853814201fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6424641
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442846}

--- chrome/test/data/webui/discards/discards_test.ts
+++ chrome/test/data/webui/discards/discards_test.ts
@@ -4,12 +4,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {durationToString, maybeMakePlural} from 'chrome://discards/discards.js';
-import {compareTabDiscardsInfos} from 'chrome://discards/discards_tab.js';
+import type {TabDiscardsInfo} from 'chrome://discards/discards.mojom-webui.js';
+import {CanFreeze} from 'chrome://discards/discards.mojom-webui.js';
+import {getSortFunctionForKey} from 'chrome://discards/discards_tab.js';
+import {LifecycleUnitDiscardReason, LifecycleUnitLoadingState} from 'chrome://discards/lifecycle_unit_state.mojom-webui.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 suite('discards', function() {
-  test('CompareTabDiscardsInfo', function() {
-    const dummy1 = {
+  test('GetSortFunctionForKey', function() {
+    const dummy1: TabDiscardsInfo = {
       title: 'title 1',
       tabUrl: 'http://urlone.com',
       visibility: 0,  // hidden
@@ -18,8 +21,19 @@ suite('discards', function() {
       discardCount: 0,
       utilityRank: 0,
       lastActiveSeconds: 0,
+      // Dummy default values.
+      loadingState: LifecycleUnitLoadingState.UNLOADED,
+      canDiscard: false,
+      hasFocus: false,
+      cannotDiscardReasons: [],
+      canFreeze: CanFreeze.YES,
+      cannotFreezeReasons: [],
+      discardReason: LifecycleUnitDiscardReason.EXTERNAL,
+      id: 0,
+      siteEngagementScore: 0,
+      stateChangeTime: {microseconds: 0n},
     };
-    const dummy2 = {
+    const dummy2: TabDiscardsInfo = {
       title: 'title 2',
       tabUrl: 'http://urltwo.com',
       visibility: 1,  // occluded
@@ -28,15 +42,26 @@ suite('discards', function() {
       discardCount: 1,
       utilityRank: 1,
       lastActiveSeconds: 1,
+      // Dummy default values.
+      loadingState: LifecycleUnitLoadingState.UNLOADED,
+      canDiscard: false,
+      hasFocus: false,
+      cannotDiscardReasons: [],
+      canFreeze: CanFreeze.YES,
+      cannotFreezeReasons: [],
+      discardReason: LifecycleUnitDiscardReason.EXTERNAL,
+      id: 0,
+      siteEngagementScore: 0,
+      stateChangeTime: {microseconds: 0n},
     };
 
     ['title', 'tabUrl', 'visibility', 'state', 'isAutoDiscardable',
      'discardCount', 'utilityRank', 'lastActiveSeconds']
         .forEach((sortKey) => {
-          assertTrue(compareTabDiscardsInfos(sortKey, dummy1, dummy2) < 0);
-          assertTrue(compareTabDiscardsInfos(sortKey, dummy2, dummy1) > 0);
-          assertTrue(compareTabDiscardsInfos(sortKey, dummy1, dummy1) === 0);
-          assertTrue(compareTabDiscardsInfos(sortKey, dummy2, dummy2) === 0);
+          assertTrue(getSortFunctionForKey(sortKey)(dummy1, dummy2) < 0);
+          assertTrue(getSortFunctionForKey(sortKey)(dummy2, dummy1) > 0);
+          assertTrue(getSortFunctionForKey(sortKey)(dummy1, dummy1) === 0);
+          assertTrue(getSortFunctionForKey(sortKey)(dummy2, dummy2) === 0);
         });
   });
 
