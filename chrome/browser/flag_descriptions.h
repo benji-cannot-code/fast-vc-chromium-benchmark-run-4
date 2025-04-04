2248117616d207598649c431b0399e49275fe4d5
Garfield Tan <xutan@chromium.org>
2025-04-04 14:13:28 -0700

    serial: Change WebSerialAPI chrome flag to WebSerialOverBluetooth
    The existing WebSerialAPI enables the API itself in Blink without
    enabling backend code that provides the functionality. Change it to a
    flag that also enables backend code so that the Web Serial API is able
    to serve its functions over Bluetooth.
    
    Wired serial ports aren't yet supported on Android devices due to system
    restrictions.
    
    Bug: 407820149
    Test: Web serial over Bluetooth is enabled when the flag is on.
    Change-Id: I78e1fd005fd4be36a12076a5d7f42ac9c7e6d375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427133
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Garfield Tan <xutan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442926}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4873,8 +4873,8 @@ extern const char kSupervisedUserLocalWebApprovalsDescription[];
 extern const char kHistoryOptInEntryPointsName[];
 extern const char kHistoryOptInEntryPointsDescription[];
 
-extern const char kWebSerialAPIName[];
-extern const char kWebSerialAPIDescription[];
+extern const char kWebSerialOverBluetoothName[];
+extern const char kWebSerialOverBluetoothDescription[];
 
 extern const char kSupervisedUserForceSigninWithCapabilitiesName[];
 extern const char kSupervisedUserForceSigninWithCapabilitiesDescription[];
