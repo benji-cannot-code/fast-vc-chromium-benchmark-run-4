ad3ba551ba8d2c0b3afa4638ca1b6fc6e143c05b
Oleksandr Tara <otara@google.com>
2025-04-04 02:29:02 -0700

    Remove StrategyOnCacheMiss
    The fields are no longer used for any meaningful logic, so this will
    simplify the code significantly.
    
    Bug: 371938601
    Change-Id: I35e48e96d8c94521877734aa06daa03e12249511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6347179
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442636}

--- components/affiliations/core/browser/affiliation_service_impl.cc
+++ components/affiliations/core/browser/affiliation_service_impl.cc
@@ -254,7 +254,7 @@ void AffiliationServiceImpl::GetAffiliationsAndBranding(
     const FacetURI& facet_uri,
     ResultCallback result_callback) {
   PostToBackend(&AffiliationBackend::GetAffiliationsAndBranding, facet_uri,
-                StrategyOnCacheMiss::FAIL, std::move(result_callback),
+                std::move(result_callback),
                 base::SequencedTaskRunner::GetCurrentDefault());
 }
 
