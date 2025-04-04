1137c46fba41f4f82f2657b6b187466962cca6ec
Steinar H. Gunderson <sesse@chromium.org>
2025-04-04 13:11:47 -0700

    Speed up UpdateReverse*Relations().
    When inserting a new node and the a11y tree is on, we update the cache
    of various relations, mostly by looking at the set of aria-*
    attributes. These can come from the normal element attributes, or from
    the element's ElementInternals.
    
    A very common case is that none of these are set and we need to do
    nothing. However, we still go through the motions of creating vectors
    and sending them around to various functions, and these are not inlined,
    so we get a lot of overhead for nothing.
    
    You would think that PGO would help for this by inlining the critical
    paths, but it seems that it makes it _worse_; presumably there is no
    PGO data with a11y enabled, so the code is counted as really cold and
    mostly deoptimized (this is, however, a speculation).
    
    As a band-aid, we do a very early test for most of these attributes,
    in the parts that _are_ inlined; if we don't have either the attribute
    nor ElementInternals, we can just skip the entire process for that
    attribute.
    
    In order to show that this is not doing optimization that is
    overlapping with what PGO would do (which can sometimes happen when
    doing e.g. manual inlining), I've run Speedometer3 with PGO _on_.
    Usually, this unfair against the new code, since PGO is better-adjusted
    to older code than newer code. However, here it is still a strong win;
    Speedometer3 (M1 Pinpoint, a11y on, significant results at 99% CI only):
    
      TodoMVC-Preact-Complex-DOM                  [ +2.6%, +16.6%]
      React-Stockcharts-SVG                       [ -2.7%,  -0.6%]
      Perf-Dashboard                              [ -2.6%,  -1.3%]
      NewsSite-Next                               [ -4.3%,  -1.6%]
      Editor-CodeMirror                           [ -4.1%,  -1.0%]
      Charts-observable-plot                      [ -4.8%,  -2.5%]
      TodoMVC-WebComponents                       [ -4.9%,  -4.0%]
      TodoMVC-JavaScript-ES6-Webpack-Complex-DOM  [ -9.2%,  -6.8%]
      TodoMVC-jQuery                              [-10.2%,  -8.6%]
      TodoMVC-JavaScript-ES5                      [-12.1%, -11.0%]
    
      Score                                       [ +1.8%,  +2.5%]
    
    I've also verified that LTO on but PGO off is a significant win,
    but less so (about 1% overall win).
    
    Change-Id: I8b179fae79f59d211ad264d5caa838a2675a40b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421744
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442874}

--- third_party/blink/renderer/modules/accessibility/ax_relation_cache.h
+++ third_party/blink/renderer/modules/accessibility/ax_relation_cache.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ACCESSIBILITY_AX_RELATION_CACHE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ACCESSIBILITY_AX_RELATION_CACHE_H_
 
+#include <stdint.h>
+
+#include <utility>
+
 #include "third_party/blink/renderer/core/html/forms/html_label_element.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h"
 #include "third_party/blink/renderer/platform/graphics/dom_node_id.h"
@@ -280,8 +284,10 @@ class AXRelationCache {
   // aria-describedby or aria-labeledby, update the text for the related object.
   void UpdateRelatedText(Node*);
 
-  static base::span<QualifiedName> GetTextRelationAttributes();
-  static base::span<QualifiedName> GetOtherRelationAttributes();
+  static base::span<std::pair<QualifiedName, uint32_t>>
+  GetTextRelationAttributes();
+  static base::span<std::pair<QualifiedName, uint32_t>>
+  GetOtherRelationAttributes();
 
   bool IsValidOwnsRelation(AXObject* owner, Node& child_node) const;
   void UnmapOwnedChildrenWithCleanLayout(const AXObject* owner,
