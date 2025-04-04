5e2261a522c7b91345ce89b09c082d2eecd96430
Janice Liu <janiceliu@chromium.org>
2025-04-04 08:08:04 -0700

    [Blob URL] Update Return Value of RenderFrameHostImpl::DoesDocumentHaveStorageAccess()
    Crash Bug: https://crbug.com/406845738
    
    Bug: 406845738
    Change-Id: I46366aad06ab3318dcfaf6956c5ebc817ed8d4db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6422993
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Feels: Mingyu Lei <leimy@chromium.org>
    Auto-Submit: Janice Liu <janiceliu@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Janice Liu <janiceliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442714}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -12751,10 +12751,8 @@ void RenderFrameHostImpl::ReportBlockingCrossPartitionBlobURL(
       std::move(details)));
 }
 
-void RenderFrameHostImpl::DoesDocumentHaveStorageAccess(
-    base::OnceCallback<void(bool)> callback) {
-  std::move(callback).Run(
-      StorageAccessHandle::DoesDocumentHaveStorageAccess(this));
+bool RenderFrameHostImpl::DoesDocumentHaveStorageAccess() {
+  return StorageAccessHandle::DoesDocumentHaveStorageAccess(this);
 }
 
 void RenderFrameHostImpl::BindBlobUrlStoreAssociatedReceiver(
@@ -12768,8 +12766,14 @@ void RenderFrameHostImpl::BindBlobUrlStoreAssociatedReceiver(
       base::BindRepeating(
           &RenderFrameHostImpl::ReportBlockingCrossPartitionBlobURL,
           weak_ptr_factory_.GetWeakPtr()),
-      base::BindRepeating(&RenderFrameHostImpl::DoesDocumentHaveStorageAccess,
-                          weak_ptr_factory_.GetWeakPtr()),
+      base::BindRepeating(
+          [](base::WeakPtr<RenderFrameHostImpl> frame) -> bool {
+            if (!frame) {
+              return false;
+            }
+            return frame->DoesDocumentHaveStorageAccess();
+          },
+          weak_ptr_factory_.GetWeakPtr()),
       !(GetContentClient()->browser()->IsBlobUrlPartitioningEnabled(
           GetBrowserContext())));
 }
--- storage/browser/blob/blob_url_store_impl.cc
+++ storage/browser/blob/blob_url_store_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/strings/strcat.h"
 #include "components/crash/core/common/crash_key.h"
-#include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "net/base/features.h"
 #include "storage/browser/blob/blob_impl.h"
@@ -77,8 +76,7 @@ BlobURLStoreImpl::BlobURLStoreImpl(
     base::RepeatingCallback<
         void(const GURL&, std::optional<blink::mojom::PartitioningBlobURLInfo>)>
         partitioning_blob_url_closure,
-    base::RepeatingCallback<void(base::OnceCallback<void(bool)>)>
-        storage_access_check_callback,
+    base::RepeatingCallback<bool()> storage_access_check_callback,
     bool partitioning_disabled_by_policy)
     : storage_key_(storage_key),
       renderer_origin_(renderer_origin),
@@ -136,10 +134,8 @@ void BlobURLStoreImpl::ResolveAsURLLoaderFactory(
     std::move(callback).Run(std::nullopt, std::nullopt);
     return;
   }
-  storage_access_check_callback_.Run(
-      base::BindOnce(&BlobURLStoreImpl::FinishResolveAsURLLoaderFactory,
-                     weak_ptr_factory_.GetWeakPtr(), url, std::move(receiver),
-                     std::move(callback)));
+  FinishResolveAsURLLoaderFactory(url, std::move(receiver), std::move(callback),
+                                  storage_access_check_callback_.Run());
 }
 
 void BlobURLStoreImpl::FinishResolveAsURLLoaderFactory(
@@ -186,12 +182,9 @@ void BlobURLStoreImpl::ResolveAsBlobURLToken(
     std::move(callback).Run(std::nullopt);
     return;
   }
-  storage_access_check_callback_.Run(
-      mojo::WrapCallbackWithDefaultInvokeIfNotRun(
-          base::BindOnce(&BlobURLStoreImpl::FinishResolveAsBlobURLToken,
-                         weak_ptr_factory_.GetWeakPtr(), url, std::move(token),
-                         is_top_level_navigation, std::move(callback)),
-          false));
+  FinishResolveAsBlobURLToken(url, std::move(token), is_top_level_navigation,
+                              std::move(callback),
+                              storage_access_check_callback_.Run());
 }
 
 void BlobURLStoreImpl::FinishResolveAsBlobURLToken(
