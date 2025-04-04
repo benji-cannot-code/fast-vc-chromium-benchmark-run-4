bd520742982e4101412e33251a489490592975dd
Alex Yang <aycyang@chromium.org>
2025-04-04 16:00:43 -0700

    Refactor ProcessMessagesFromJsTest to return list of SubTestResults
    Make it easier to unit test this helper function.
    
    Remove test coverage of calls to SubTestReporter. The previous
    architecture needed to mock it in order to verify correct behavior of
    ProcessMessagesFromJsTest, but with this new architecture it's no longer
    necessary.
    
    Bug: 393139276
    Change-Id: I115658148d244dd40fccc99a70932a1f04c84deb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6435195
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442976}

--- chrome/test/base/web_ui_mocha_browser_test_browsertest.cc
+++ chrome/test/base/web_ui_mocha_browser_test_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 
 #include <string>
+#include <tuple>
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
@@ -21,19 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest-spi.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-class MockSubTestReporter : public SubTestReporter {
- public:
-  MOCK_METHOD(void,
-              Report,
-              (std::string_view name,
-               testing::TimeInMillis elapsed_time,
-               std::optional<std::string_view> failure_message),
-              (const, override));
-};
 
 // For unit testing private methods of WebUIMochaBrowserTest.
 using WebUIMochaUnitTest = WebUIMochaBrowserTest;
@@ -45,15 +35,17 @@ IN_PROC_BROWSER_TEST_F(WebUIMochaUnitTest, CanonicalizeTestName) {
 }
 
 IN_PROC_BROWSER_TEST_F(WebUIMochaUnitTest, ProcessMessagesFromJsTest) {
-  MockSubTestReporter mock_sub_test_reporter;
-  EXPECT_CALL(mock_sub_test_reporter, Report).Times(2);
-
   auto* web_contents = chrome_test_utils::GetActiveWebContents(this);
 
+  bool success;
+  std::vector<SubTestResult> results;
   base::RunLoop run_loop;
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE, base::BindLambdaForTesting([&]() {
-        webui::ProcessMessagesFromJsTest(web_contents, &mock_sub_test_reporter);
+        std::tuple<bool, std::vector<SubTestResult>> t =
+            webui::ProcessMessagesFromJsTest(web_contents);
+        success = std::get<0>(t);
+        results = std::move(std::get<1>(t));
         run_loop.Quit();
       }));
 
@@ -71,6 +63,10 @@ IN_PROC_BROWSER_TEST_F(WebUIMochaUnitTest, ProcessMessagesFromJsTest) {
   )"));
 
   run_loop.Run();
+  EXPECT_EQ(success, true);
+  EXPECT_EQ(results.size(), 2ul);
+  EXPECT_EQ(results[0].name, "test1");
+  EXPECT_EQ(results[1].name, "test2");
 }
 
 // Test that code coverage metrics are reported from WebUIMochaBrowserTest
