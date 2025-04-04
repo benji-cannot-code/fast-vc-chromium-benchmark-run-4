dfe9ded886d1fee1f73019193a29d1f66bf75be7
Glenn Hartmann <hartmanng@chromium.org>
2025-04-04 07:27:35 -0700

    Pin nix deps using npins.
    This enables `nix develop`, and `nix-shell` should continue to work, but
    now it will use the pinned deps in sources.json instead of using
    the user's local nixpkgs channel definition.
    
    Advantages to pinning deps:
    
    * Reproducible over time and between users
    * Can roll back sources.json if something in nixpkgs breaks
    
    Disadvantages:
    
    * We probably will want to periodically update sources.json
      in the Chromium repo
    * Could use extra space on the user's device if their nixpkgs channel is
      different from the sources.json version
    
    Change-Id: I5e3b87d71ea2e611d65a3ee91cfcef9d8fda9af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6408154
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Delan Azabani <dazabani@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1442703}

---
+++ tools/nix/flake.nix
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+{
+  inputs = { };
+  outputs =
+    { self }:
+    let
+      devShell = system: {
+        "${system}".default = import ./make-shell-for-system.nix {
+          inherit system;
+        } { };
+      };
+    in
+    {
+      devShells = devShell "aarch64-linux" // devShell "x86_64-linux";
+    };
+}
