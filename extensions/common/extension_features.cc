5d8219c4ba3bd45ebd30103ced407def248242c4
Richard Chen <richche@chromium.org>
2025-04-04 17:58:56 -0700

    Remove `--load-extension` switch on Chrome builds
    Part of an on-going effort to reduce harm from the malicious
    command-line extensions, this CL removes the exploited switch
    only on Chrome builds.
    
    Bug: 401529219
    Change-Id: I989dc9b3d50bbeb349b9e0a534945dd64d8c55cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6335815
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443013}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_features.h"
 
 #include "base/feature_list.h"
+#include "build/branding_buildflags.h"
+#include "build/build_config.h"
 
 namespace extensions_features {
 
@@ -197,6 +199,17 @@ BASE_FEATURE(kSilentDebuggerExtensionAPI,
 BASE_FEATURE(kRemoveCoreSiteInstance,
              "RemoveCoreSiteInstance",
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kDisableLoadExtensionCommandLineSwitch,
+             "DisableLoadExtensionCommandLineSwitch",
+// TODO(crbug.com/401529219): Enable feature for Google Chrome Branding only
+// once ready.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+             base::FEATURE_DISABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+);
 
 BASE_FEATURE(kUserScriptUserExtensionToggle,
              "UserScriptUserExtensionToggle",
