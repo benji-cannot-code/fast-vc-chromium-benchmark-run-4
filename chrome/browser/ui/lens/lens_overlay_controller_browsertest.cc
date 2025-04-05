337d52e54481bc9389e9898e4ec53d48ebd74878
Bryan Nguyen <nguyenbryan@google.com>
2025-04-04 17:57:34 -0700

    [lensoverlay] Limit page contextualization updates.
    Removes updates that occur when the live page is not showing. Gates
    recontextualizing on each query behind flag.
    
    Bug: 408223189
    Change-Id: Ifc72b64bddc1665c19eadcc569f248cadd4f5ad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432286
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1443010}

--- chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
+++ chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
@@ -5432,6 +5432,9 @@ IN_PROC_BROWSER_TEST_P(LensOverlayControllerBrowserPDFContextualizationTest,
   const GURL new_url = embedded_test_server()->GetURL(kPdfDocumentWithForm);
   LoadPdfGetExtensionHost(new_url);
 
+  // Focus the searchbox.
+  controller->OnFocusChangedForTesting(true);
+
   // Issue a new searchbox query.
   controller->IssueSearchBoxRequestForTesting(
       "oranges", AutocompleteMatchType::SEARCH_SUGGEST,
@@ -5552,14 +5555,6 @@ IN_PROC_BROWSER_TEST_P(LensOverlayControllerBrowserPDFContextualizationTest,
   // Focus the searchbox.
   controller->OnFocusChangedForTesting(true);
 
-  // Verify a new full image and page content request was sent. Both requests
-  // happen async, so need to wait for both to be sent before continuing to
-  // prevent flakiness.
-  ASSERT_TRUE(base::test::RunUntil([&]() {
-    return fake_query_controller->num_full_image_requests_sent() == 2 &&
-           fake_query_controller->num_page_content_update_requests_sent() == 2;
-  }));
-
   // Issue a query.
   controller->IssueSearchBoxRequestForTesting(
       "red", AutocompleteMatchType::Type::SEARCH_SUGGEST,
@@ -5583,8 +5578,8 @@ IN_PROC_BROWSER_TEST_P(LensOverlayControllerBrowserPDFContextualizationTest,
   // state. Both requests happen async, so need to wait for both to be sent
   // before continuing to prevent flakiness.
   ASSERT_TRUE(base::test::RunUntil([&]() {
-    return fake_query_controller->num_full_image_requests_sent() == 3 &&
-           fake_query_controller->num_page_content_update_requests_sent() == 3;
+    return fake_query_controller->num_full_image_requests_sent() == 2 &&
+           fake_query_controller->num_page_content_update_requests_sent() == 2;
   }));
 }
 
@@ -5765,18 +5760,24 @@ IN_PROC_BROWSER_TEST_P(LensOverlayControllerBrowserPDFContextualizationTest,
   const GURL pdf_url = embedded_test_server()->GetURL(kPdfDocumentWithForm);
   LoadPdfGetExtensionHost(pdf_url);
 
+  // Focus the searchbox.
+  controller->OnFocusChangedForTesting(true);
+
   // Issue a new searchbox query.
   controller->IssueSearchBoxRequestForTesting(
       "oranges", AutocompleteMatchType::SEARCH_SUGGEST,
       /*is_zero_prefix_suggestion=*/false,
       /*additional_query_params=*/{});
 
+  // Verify transitions to live page.
+  ASSERT_TRUE(base::test::RunUntil(
+      [&]() { return controller->state() == State::kLivePageAndResults; }));
+
   ASSERT_TRUE(base::test::RunUntil([&]() {
     return !fake_query_controller->last_sent_underlying_content_bytes().empty();
   }));
   ASSERT_EQ(lens::MimeType::kPdf,
             fake_query_controller->last_sent_underlying_content_type());
-  controller->OnFocusChangedForTesting(true);
 
   CloseOverlayAndWaitForOff(controller,
                             LensOverlayDismissalSource::kOverlayCloseButton);
@@ -6326,6 +6327,9 @@ IN_PROC_BROWSER_TEST_F(LensOverlayControllerBrowserTest,
 
   // Change page.
   WaitForPaint(kDocumentWithNamedElement);
+
+  // Focus the searchbox.
+  controller->OnFocusChangedForTesting(true);
 
   // Issue a new searchbox query.
   controller->IssueSearchBoxRequestForTesting(
@@ -7239,6 +7243,9 @@ IN_PROC_BROWSER_TEST_F(LensOverlayControllerBrowserTest,
 
   // Change page.
   WaitForPaint(kDocumentWithNamedElement);
+
+  // Focus the searchbox.
+  controller->OnFocusChangedForTesting(true);
 
   // Issue a new searchbox query.
   controller->IssueSearchBoxRequestForTesting(
