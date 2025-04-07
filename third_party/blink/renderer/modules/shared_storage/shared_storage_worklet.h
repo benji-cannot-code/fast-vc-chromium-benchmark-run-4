5ce7920454707659c3e5a32fd13d6550fae49900
Lingqi Chi <lingqi@chromium.org>
2025-04-06 20:03:51 -0700

    [prerender] defer run and selectUrl operation of shared storage
    This is a follow-up of I898a63403728eb8fd716ddc6ab029cabeee48127.
    SharedStorage is using an Associated Mojo interface, so the remote
    side cannot be used until its receiver is bound.
    Before this CL, the `worklet_host_` will be accessed when calling
    run or selectUrl API, and this CL fixes this issue.
    
    Bug: 390136190
    Change-Id: Ife0638a7efcfe9b0096f4429b4179a84e8b3c7d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420347
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443262}

--- third_party/blink/renderer/modules/shared_storage/shared_storage_worklet.h
+++ third_party/blink/renderer/modules/shared_storage/shared_storage_worklet.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/mojom/shared_storage/shared_storage.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_fencedframeconfig_usvstring.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -71,6 +72,9 @@ class MODULES_EXPORT SharedStorageWorklet final : public ScriptWrappable {
                        scoped_refptr<SecurityOrigin> custom_data_origin);
 
  private:
+  // These internal methods wrap logic that requires IPCs. Since the
+  // corresponding IPCs are not valid during prerendering, they have to be
+  // deferred until prerendering pages being activated.
   void AddModuleOnLocalDomWindow(
       LocalDOMWindow* dom_window,
       KURL script_source_url,
@@ -80,8 +84,24 @@ class MODULES_EXPORT SharedStorageWorklet final : public ScriptWrappable {
       bool resolve_to_worklet,
       base::TimeTicks start_time,
       ScriptPromiseResolverBase* resolver);
+  void SelectUrlInternal(
+      ScriptState* script_state,
+      const String& name,
+      Vector<mojom::blink::SharedStorageUrlWithMetadataPtr> converted_urls,
+      BlinkCloneableMessage serialized_data,
+      const SharedStorageRunOperationMethodOptions* options,
+      base::TimeTicks start_time,
+      ScriptPromiseResolver<V8SharedStorageResponse>* resolver);
+  void RunInternal(ScriptState* script_state,
+                   const String& name,
+                   BlinkCloneableMessage serialized_data,
+                   const SharedStorageRunOperationMethodOptions* options,
+                   base::TimeTicks start_time,
+                   ScriptPromiseResolver<IDLAny>* resolver);
 
-  // Set when addModule() was called and passed early renderer checks.
+  // On non-prerendering pages, set when addModule() was called and passed
+  // early renderer checks. When it comes to prerendering pages, it also needs
+  // to wait for the page to be activated before setting the Mojo connection.
   HeapMojoAssociatedRemote<mojom::blink::SharedStorageWorkletHost>
       worklet_host_{nullptr};
 
--- third_party/blink/web_tests/external/wpt/speculation-rules/prerender/resources/shared-storage-worklet.js
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prerender/resources/shared-storage-worklet.js
@@ -1,6 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-class ReadOperation {
+class WriteOperation {
   async run() {
-    return await this.sharedStorage.get("prerender");
+    return this.sharedStorage.set("prerender", true);
   }
 }
+
+class SelectURLOperation {
+  async run() {
+    return 0;
+  }
+}
+
+register('test-prerender', WriteOperation);
+register('test-prerender-selection', SelectURLOperation);
