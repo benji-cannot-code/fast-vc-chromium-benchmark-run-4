de35bd28fefc40212f3d750292fb3db37dc74c9e
Will Harris <wfh@chromium.org>
2025-04-07 15:54:24 -0700

    Harden a DCHECK to a CHECK in mojo ipcz driver
    As per
    https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/checks.md#invariant_verification-mechanisms
    CHECKs are preferred over DCHECK except for the case of highly
    performance sensitive code.
    
    This CL therefore upgrades a DCHECK to a CHECK in the mojo handle
    passing code.
    
    BUG=None
    
    Change-Id: Ia4263253b35f66ca1d96ff77ac7da497da9b8194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438894
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443819}

--- mojo/core/ipcz_driver/transport.cc
+++ mojo/core/ipcz_driver/transport.cc
@@ -521,7 +521,7 @@ IpczResult Transport::DeserializeObject(
   }
 
 #if BUILDFLAG(IS_WIN)
-  DCHECK(handles.empty());
+  CHECK(handles.empty());
   size_t num_handles = header.num_handles;
   const HandleOwner handle_owner = header.handle_owner;
 
