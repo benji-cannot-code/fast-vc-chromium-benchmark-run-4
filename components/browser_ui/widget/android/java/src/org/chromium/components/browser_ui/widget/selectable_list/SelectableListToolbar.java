718f492e08399471c970c6c465e0e48e201fc1b1
Aishwarya Rajesh <aishwaryarj@google.com>
2025-04-07 14:34:29 -0700

    Move KeyboardNavigationListener to //ui
    Intend to use this in TextViewWithClickableSpans that resides in //ui,
    per the updates in https://crrev.com/c/6383822.
    
    Bug: 405441323
    Change-Id: Ic731d6a29bdb0e3a976dca06a9a987e5a3e8ade4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438501
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443773}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/selectable_list/SelectableListToolbar.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/selectable_list/SelectableListToolbar.java
@@ -45,7 +45,6 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
-import org.chromium.components.browser_ui.util.KeyboardNavigationListener;
 import org.chromium.components.browser_ui.widget.NumberRollView;
 import org.chromium.components.browser_ui.widget.R;
 import org.chromium.components.browser_ui.widget.TintedDrawable;
@@ -57,6 +56,7 @@ import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.UiUtils;
 import org.chromium.ui.text.EmptyTextWatcher;
 import org.chromium.ui.util.ColorUtils;
+import org.chromium.ui.util.KeyboardNavigationListener;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
