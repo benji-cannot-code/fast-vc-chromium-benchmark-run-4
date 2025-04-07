f139569485328b8cac9361d96dfa406042703d75
April Zhou <aprilzhou@google.com>
2025-04-07 11:05:21 -0700

    boca: Do not allow create session or join class in non-managed network.
    A follow up CL will be needed to expose detailed error message to UI.
    
    Bug: b:406552629
    Test: Unit tested
    Change-Id: I29e3f7828a5bd34256467c8f07d253feb0c5aa7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6434867
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1443625}

--- chromeos/ash/components/boca/boca_session_manager.cc
+++ chromeos/ash/components/boca/boca_session_manager.cc
@@ -306,6 +306,10 @@ void BocaSessionManager::NotifyAppReload() {
   }
 }
 
+bool BocaSessionManager::disabled_on_non_managed_network() {
+  return disabled_on_non_managed_network_;
+}
+
 void BocaSessionManager::SetSessionCaptionInitializer(
     SessionCaptionInitializer session_caption_initializer) {
   session_caption_initializer_ = session_caption_initializer;
