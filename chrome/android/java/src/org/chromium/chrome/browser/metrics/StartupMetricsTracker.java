cfe5f1cd3efdc39d1bb029edd66b3d1a4d467ffe
Nafis Abedin <nafisabedin@google.com>
2025-04-07 13:07:29 -0700

    [Binder] Add a metric that records time spent in Binder calls
    As part of an ongoing effort to reduce the number of/time spent in
    Binder calls on the UI thread, this CL adds a cold start metric
    indicating how much of that cold start time was spent handling Binder
    transactions.
    
    Bug: 405129431
    Change-Id: Idbc66f0c0d5c654d12358272bb42b1dd72a7799d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6431906
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1443714}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/StartupMetricsTracker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/StartupMetricsTracker.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
+import org.chromium.base.BinderCallsListener;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.task.PostTask;
@@ -39,6 +40,8 @@ import org.chromium.url.GURL;
 public class StartupMetricsTracker {
 
     private static final long TIME_TO_DRAW_METRIC_RECORDING_DELAY_MS = 2500;
+    private static final String NTP_COLD_START_HISTOGRAM =
+            "Startup.Android.Cold.NewTabPage.TimeToFirstDraw";
     private boolean mFirstNavigationCommitted;
 
     private class TabObserver extends TabModelSelectorTabObserver {
@@ -199,7 +202,7 @@ public class StartupMetricsTracker {
      */
     public void registerNtpViewObserver(@NonNull View ntpRootView) {
         if (!mShouldTrackTimeToFirstDraw) return;
-        trackTimeToFirstDraw(ntpRootView, "Startup.Android.Cold.NewTabPage.TimeToFirstDraw");
+        trackTimeToFirstDraw(ntpRootView, NTP_COLD_START_HISTOGRAM);
     }
 
     /**
@@ -223,6 +226,9 @@ public class StartupMetricsTracker {
                 view,
                 () -> {
                     long timeToFirstDrawMs = SystemClock.uptimeMillis() - mActivityStartTimeMs;
+                    if (NTP_COLD_START_HISTOGRAM.equals(histogram)) {
+                        recordTimeSpentInBinderCold("NewTabPage");
+                    }
                     // During a cold start, first draw can be triggered while Chrome is in
                     // the background, leading to ablated draw times. This early in the startup
                     // process, events that indicate Chrome has been backgrounded do not run until
@@ -259,6 +265,14 @@ public class StartupMetricsTracker {
     private void recordExperimentalHistogram(String name, long ms) {
         RecordHistogram.deprecatedRecordMediumTimesHistogram(
                 "Startup.Android.Experimental." + name + ".Tabbed.ColdStartTracker", ms);
+    }
+
+    private void recordTimeSpentInBinderCold(String variant) {
+        Long binderTimeMs = BinderCallsListener.getInstance().getTimeSpentInBinderCalls();
+        if (binderTimeMs != null) {
+            RecordHistogram.recordMediumTimesHistogram(
+                    "Startup.Android.Cold." + variant + ".TimeSpentInBinder", binderTimeMs);
+        }
     }
 
     private void recordNavigationCommitMetrics(long firstCommitMs) {
