f6f780011f37c4fdfec130b2d4a54b2d363e1e41
Haiyang Pan <hypan@google.com>
2025-04-07 12:38:36 -0700

    Reland "Manual roll Android Lint from cpi5krkjlhoCfjDOZ... to vBqKHQdjLFM5l79fp..."
    This is a reland of commit c12d388b70687b801d11dae3440ac2bbfc19220c
    
    Added the missing file that need `@SuppressWarnings("GestureBackNavigation")`
    
    Original change's description:
    > Manual roll Android Lint from cpi5krkjlhoCfjDOZ... to vBqKHQdjLFM5l79fp...
    >
    > Maual: Suppress the lint error,
    >
    > If this roll has caused a breakage, revert this CL and stop the roller
    > using the controls here:
    > https://autoroll.skia.org/r/lint-chromium
    > Please CC clank-build-core@google.com,clank-library-failures@google.com,wnwen@google.com on the revert to ensure that a human
    > is aware of the problem.
    >
    > To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    >
    > To report a problem with the AutoRoller itself, please file a bug:
    > https://issues.skia.org/issues/new?component=1389291&template=1850622
    >
    > Documentation for the AutoRoller is here:
    > https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    >
    > Tbr: clank-library-failures@google.com
    > Change-Id: Iccc0be25ec30e92baa18a5a4398bd1acbb85049b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432318
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > Commit-Queue: Haiyang Pan <hypan@google.com>
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1442987}
    
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm-dbg
    Change-Id: I934a750cf8bfb92e44d84ee3d60968c254078fa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6435045
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443698}

--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -100,6 +100,7 @@ public class WebViewBrowserActivity extends AppCompatActivity {
     }
 
     @Override
+    @SuppressWarnings("GestureBackNavigation")
     public void onBackPressed() {
         if (mWebView != null && mWebView.canGoBack()) {
             mWebView.goBack();
