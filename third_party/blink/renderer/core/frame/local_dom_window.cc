3ea5c81ac80ac7bb418a60f2d3c231632bbf2eee
Mike West <mkwst@chromium.org>
2025-04-07 11:01:44 -0700

    Experiment with reducing screen size exposure.
    This CL adds a runtime flag which maps various `Screen` interface
    methods either to constants or to their `Window` interface equivalents.
    
    Bug: 408932088
    Change-Id: If803d44a53ba4eed1ddfce9dddf987e9b7f1d835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437711
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443622}

--- third_party/blink/renderer/core/frame/local_dom_window.cc
+++ third_party/blink/renderer/core/frame/local_dom_window.cc
@@ -1654,6 +1654,10 @@ int LocalDOMWindow::screenX() const {
   if (!page)
     return 0;
 
+  if (RuntimeEnabledFeatures::ReduceScreenSizeEnabled()) {
+    return 0;
+  }
+
   ChromeClient& chrome_client = page->GetChromeClient();
   if (page->GetSettings().GetReportScreenSizeInPhysicalPixelsQuirk()) {
     return static_cast<int>(
@@ -1671,6 +1675,10 @@ int LocalDOMWindow::screenY() const {
   Page* page = frame->GetPage();
   if (!page)
     return 0;
+
+  if (RuntimeEnabledFeatures::ReduceScreenSizeEnabled()) {
+    return 0;
+  }
 
   ChromeClient& chrome_client = page->GetChromeClient();
   if (page->GetSettings().GetReportScreenSizeInPhysicalPixelsQuirk()) {
--- third_party/blink/renderer/core/frame/screen.cc
+++ third_party/blink/renderer/core/frame/screen.cc
@@ -43,6 +43,14 @@ namespace blink {
 
 namespace {
 
+bool ShouldReduceScreenSize(const LocalDOMWindow* window) {
+  CHECK(window);
+
+  // TODO(408932088): Take the current state of the window management permission
+  // (`mojom::blink::PermissionName::WINDOW_MANAGEMENT`) into account here.
+  return RuntimeEnabledFeatures::ReduceScreenSizeEnabled();
+}
+
 }  // namespace
 
 Screen::Screen(LocalDOMWindow* window, int64_t display_id)
@@ -101,12 +109,22 @@ bool Screen::AreWebExposedScreenPropertiesEqual(
 int Screen::height() const {
   if (!DomWindow())
     return 0;
+
+  if (ShouldReduceScreenSize(DomWindow())) {
+    return DomWindow()->innerHeight();
+  }
+
   return GetRect(/*available=*/false).height();
 }
 
 int Screen::width() const {
   if (!DomWindow())
     return 0;
+
+  if (ShouldReduceScreenSize(DomWindow())) {
+    return DomWindow()->innerWidth();
+  }
+
   return GetRect(/*available=*/false).width();
 }
 
@@ -123,24 +141,44 @@ unsigned Screen::pixelDepth() const {
 int Screen::availLeft() const {
   if (!DomWindow())
     return 0;
+
+  if (ShouldReduceScreenSize(DomWindow())) {
+    return 0;
+  }
+
   return GetRect(/*available=*/true).x();
 }
 
 int Screen::availTop() const {
   if (!DomWindow())
     return 0;
+
+  if (ShouldReduceScreenSize(DomWindow())) {
+    return 0;
+  }
+
   return GetRect(/*available=*/true).y();
 }
 
 int Screen::availHeight() const {
   if (!DomWindow())
     return 0;
+
+  if (ShouldReduceScreenSize(DomWindow())) {
+    return DomWindow()->innerHeight();
+  }
+
   return GetRect(/*available=*/true).height();
 }
 
 int Screen::availWidth() const {
   if (!DomWindow())
     return 0;
+
+  if (ShouldReduceScreenSize(DomWindow())) {
+    return DomWindow()->innerWidth();
+  }
+
   return GetRect(/*available=*/true).width();
 }
 
