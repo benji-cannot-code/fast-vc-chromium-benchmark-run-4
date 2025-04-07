6e981a7d5c81e911ad28f169130a86e3300195c6
Kent Tamura <tkent@chromium.org>
2025-04-07 13:26:01 -0700

    [RemoveLegacy] Remove Logical{Left, Right, Top, Bottom}() of LayoutBox
    Use new LayoutBox::LogicalRectInContainer() instead.
    
    Bug: 40056555
    Change-Id: I47660159a68f13c4fd8c8a4d34ff4b059a8f1e33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432816
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443731}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_slider_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_slider_element.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_div_element.h"
 #include "third_party/blink/renderer/core/html/shadow/shadow_element_names.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
+#include "third_party/blink/renderer/core/layout/geometry/logical_rect.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer.h"
@@ -34,8 +35,9 @@ void SetSegmentDivPosition(blink::HTMLDivElement* segment,
   // then it will be a nullptr so we should assume zero.
   blink::LayoutBox* box = segment->GetLayoutBox();
   if (box) {
-    current_width = box->LogicalWidth().ToInt();
-    current_left = box->LogicalLeft().ToInt();
+    blink::LogicalRect rect = box->LogicalRectInContainer();
+    current_width = rect.size.inline_size.ToInt();
+    current_left = rect.offset.inline_offset.ToInt();
   }
 
   // If the width and left has not changed then do not update the segment.
