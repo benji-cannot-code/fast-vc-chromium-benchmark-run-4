9099054025d645217162d4ca6a2af42dabd6f8c5
Sana Akbani <sanaakbani@google.com>
2025-04-07 15:45:05 -0700

    Resize FRE dynamically based on content size.
    This CL adds the autosize functionality to the FRE's webview tag,
    which updates the widget bounds in SetContentsBounds in the newly
    created WebContentsContainer delegate when the web contents size
    changes.
    
    See https://crbug.com/399940102#comment15 for demo
    
    Bug: 399940102
    Change-Id: Ic24adfa5ef468d057f887ca6fc150858ac40a663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6397987
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443817}

--- chrome/browser/glic/fre/glic_fre_dialog_view.h
+++ chrome/browser/glic/fre/glic_fre_dialog_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_GLIC_FRE_GLIC_FRE_DIALOG_VIEW_H_
 #define CHROME_BROWSER_GLIC_FRE_GLIC_FRE_DIALOG_VIEW_H_
 
+#include "chrome/browser/glic/fre/fre_webui_contents_container.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/views/window/dialog_delegate.h"
@@ -18,17 +19,21 @@ namespace glic {
 // View, which is the preferred structure.
 class GlicFreDialogView : public views::DialogDelegateView {
  public:
-  GlicFreDialogView(Profile* profile, const gfx::Size& initial_size);
+  GlicFreDialogView(Profile* profile, GlicFreController* fre_controller);
   GlicFreDialogView(const GlicFreDialogView&) = delete;
   GlicFreDialogView& operator=(const GlicFreDialogView&) = delete;
   ~GlicFreDialogView() override;
 
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kWebViewElementIdForTesting);
 
-  content::WebContents* web_contents() { return web_contents_.get(); }
+  content::WebContents* web_contents() { return contents_->web_contents(); }
+
+  views::WebView* web_view() { return web_view_; }
 
  private:
-  std::unique_ptr<content::WebContents> web_contents_;
+  std::unique_ptr<FreWebUIContentsContainer> contents_;
+
+  raw_ptr<views::WebView> web_view_;
 };
 
 }  // namespace glic
