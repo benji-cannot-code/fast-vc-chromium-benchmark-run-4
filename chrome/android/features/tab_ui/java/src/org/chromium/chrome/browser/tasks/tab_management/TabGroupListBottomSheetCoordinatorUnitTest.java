25d63d4e8745466f88bd0ab02108b18d1541d791
Jenna Himawan <jhimawan@google.com>
2025-04-07 08:48:23 -0700

    [Tab Group Parity] Fix tab group list bottom sheet in incognito
    Fixed: 408880986
    Test: TabGroupListBottomSheetCoordinatorUnitTest
    Change-Id: I73bf9bc622e78213201ce5f612565f337cc60222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6436536
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443510}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinatorUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinatorUnitTest.java
@@ -61,6 +61,7 @@ public class TabGroupListBottomSheetCoordinatorUnitTest {
     @Mock private Tab mTab;
     private final SavedTabGroup mSavedTabGroup = new SavedTabGroup();
     private final SavedTabGroupTab mSavedTabGroupTab = new SavedTabGroupTab();
+    private Context mContext;
     private TabGroupListBottomSheetCoordinator mCoordinator;
 
     @Before
@@ -73,13 +74,13 @@ public class TabGroupListBottomSheetCoordinatorUnitTest {
         when(mTabGroupSyncService.getAllGroupIds()).thenReturn(new String[] {TAB_GROUP_ID_STRING});
         when(mTabGroupSyncService.getGroup(TAB_GROUP_ID_STRING)).thenReturn(mSavedTabGroup);
 
-        Context context =
+        mContext =
                 new ContextThemeWrapper(
                         ApplicationProvider.getApplicationContext(),
                         R.style.Theme_BrowserUI_DayNight);
         mCoordinator =
                 new TabGroupListBottomSheetCoordinator(
-                        context,
+                        mContext,
                         mProfile,
                         ignored -> {},
                         mFilter,
@@ -126,5 +127,13 @@ public class TabGroupListBottomSheetCoordinatorUnitTest {
                         eq(true),
                         eq(StateChangeReason.INTERACTION_COMPLETE));
         verify(mCoordinator).destroy();
+    }
+
+    @Test
+    public void testIncognito_dontFetchTabGroupSyncService() {
+        when(mProfile.isOffTheRecord()).thenReturn(true);
+        // Test that the Coordinator can be constructed without crashing.
+        new TabGroupListBottomSheetCoordinator(
+                mContext, mProfile, ignored -> {}, mFilter, mBottomSheetController, true, false);
     }
 }
