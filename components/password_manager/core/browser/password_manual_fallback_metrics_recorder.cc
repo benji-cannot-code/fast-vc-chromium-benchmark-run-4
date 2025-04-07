e1e07588be995e9b197ccc35613fc90a7fe7b1a4
Bruno Braga <brunobraga@google.com>
2025-04-07 01:37:50 -0700

    [mulhet] Remove checks from logging class
    The current checks are not taking into account form reparsing. The check
    is likely happening because a field is going from classified to
    unclassified in the period between the context menu is shown and a
    suggestion is accepted (no the opposite case is also a problem).
    
    While the check seems reasonable, if the field type changes in between,
    the check will be triggered. Since form reparsing is normal in autofill
    the check should not exist.
    
    While I was not able to reproduce this bug myself, this is the only
    reason I can think of at this moment. Interestingly this only started
    on M136
    
    Bug: 408292617
    Change-Id: I5c60cb3e8f99e2465e37a1628ed8ea97bb3ab92f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432729
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1443315}

--- components/password_manager/core/browser/password_manual_fallback_metrics_recorder.cc
+++ components/password_manager/core/browser/password_manual_fallback_metrics_recorder.cc
@@ -60,7 +60,6 @@ void PasswordManualFallbackMetricsRecorder::OnDidFillSuggestion(
       classified_as_target_filling_password
           ? classified_as_target_filling_suggestion_state_
           : not_classified_as_target_filling_suggestion_state_;
-  CHECK_NE(state, SuggestionState::kNotShown);
   state = SuggestionState::kFilled;
 }
 
@@ -81,7 +80,6 @@ void PasswordManualFallbackMetricsRecorder::ContextMenuEntryAccepted(
       classified_as_target_filling_password
           ? classified_as_target_filling_context_menu_state_
           : not_classified_as_target_filling_context_menu_state_;
-  CHECK_NE(state, ContextMenuEntryState::kNotShown);
   state = ContextMenuEntryState::kAccepted;
 }
 
