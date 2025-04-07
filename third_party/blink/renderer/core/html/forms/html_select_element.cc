7200840b65b1873520ac255703a5d9798e8e57a1
Ana Sollano Kim <ansollan@microsoft.com>
2025-04-07 14:04:34 -0700

    Allow comments, <selectedcontent> as descendants of <select>, <div>
    This CL adds a check to return early if a descendant of a <select> is a
    comment node. Additionally, we now also check for the button's content
    model in `TraverseAncestorsAndCheckDescendant`, which is called when a
    parent node is either a <div> or a <span>. This fixes the case where a
    <select> with a <button> that contains a <div> and the <div> contains
    <selectedcontent> would log a console message about the content model
    being broken. Tests are added to validate behavior.
    
    Bug: 408118042
    Change-Id: I1e1dd9982c5d56a281ea84eb37044fc67a367500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433007
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Ana Sollano Kim <ansollan@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1443751}

--- third_party/blink/renderer/core/html/forms/html_select_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element.cc
@@ -306,6 +306,9 @@ class SelectDescendantsObserver : public MutationObserver::Delegate {
   }
 
   SelectElementAccessibilityIssueReason CheckForIssue(const Node& descendant) {
+    if (descendant.getNodeType() == Node::kCommentNode) {
+      return SelectElementAccessibilityIssueReason::kValidChild;
+    }
     // Get the parent of the descendant.
     const Node* parent = descendant.parentNode();
     // If the node has no parent, assume it is being appended to a
@@ -345,10 +348,10 @@ class SelectDescendantsObserver : public MutationObserver::Delegate {
       return TraverseAncestorsAndCheckDescendant(descendant);
     }
     if (IsA<HTMLButtonElement>(*parent)) {
-      if (IsA<HTMLSelectedContentElement>(descendant)) {
+      if (IsAllowedDescendantOfButton(descendant)) {
         return SelectElementAccessibilityIssueReason::kValidChild;
       }
-      return CheckDescedantOfOption(descendant);
+      return SelectElementAccessibilityIssueReason::kDisallowedSelectChild;
     }
     if (IsA<HTMLLegendElement>(*parent)) {
       if (IsAllowedPhrasingContent(descendant) &&
@@ -387,6 +390,12 @@ class SelectDescendantsObserver : public MutationObserver::Delegate {
            IsA<HTMLNoScriptElement>(descendant) ||
            IsA<HTMLScriptElement>(descendant) ||
            IsA<HTMLTemplateElement>(descendant);
+  }
+
+  bool IsAllowedDescendantOfButton(const Node& descendant) {
+    return IsA<HTMLSelectedContentElement>(descendant) ||
+           CheckDescedantOfOption(descendant) ==
+               SelectElementAccessibilityIssueReason::kValidChild;
   }
 
   SelectElementAccessibilityIssueReason CheckDescedantOfOption(
@@ -440,6 +449,10 @@ class SelectDescendantsObserver : public MutationObserver::Delegate {
       }
       if (IsA<HTMLSelectElement>(*ancestor) &&
           IsAllowedDescendantOfSelect(descendant, *parent)) {
+        return SelectElementAccessibilityIssueReason::kValidChild;
+      }
+      if (IsA<HTMLButtonElement>(*ancestor) &&
+          IsAllowedDescendantOfButton(descendant)) {
         return SelectElementAccessibilityIssueReason::kValidChild;
       }
     }
