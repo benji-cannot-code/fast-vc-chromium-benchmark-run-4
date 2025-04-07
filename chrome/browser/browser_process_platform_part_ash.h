222ad2765f28803878057c5f16ae9d84a035ecd1
Di Wu <diwux@google.com>
2025-04-07 01:51:57 -0700

    Make CanRestoreUrlsForProfile() non virtual
    CanRestoreUrlsForProfile() in BrowserProcessPlatformPartChromeOS was
    made virtual because sub-classes needed to have specific and different
    implementations. This is no longer the case.
    
    Bug: b:354842935
    Test: tryjob
    Change-Id: Ic0cc7ed434a7f61627e04bdc6b3d81fb152039a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433495
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443325}

--- chrome/browser/browser_process_platform_part_ash.h
+++ chrome/browser/browser_process_platform_part_ash.h
@@ -172,10 +172,6 @@ class BrowserProcessPlatformPart : public BrowserProcessPlatformPartChromeOS {
 
   static void EnsureFactoryBuilt();
 
- protected:
-  // BrowserProcessPlatformPartChromeOS:
-  bool CanRestoreUrlsForProfile(const Profile* profile) const override;
-
  private:
   friend class BrowserProcessPlatformPartTestApi;
 
--- chrome/browser/browser_process_platform_part_chromeos.cc
+++ chrome/browser/browser_process_platform_part_chromeos.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process_platform_part_chromeos.h"
 
+#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/profiles/profile.h"
@@ -31,7 +32,9 @@ BrowserProcessPlatformPartChromeOS::~BrowserProcessPlatformPartChromeOS() =
 
 bool BrowserProcessPlatformPartChromeOS::CanRestoreUrlsForProfile(
     const Profile* profile) const {
-  return profile->IsRegularProfile();
+  return profile->IsRegularProfile() && !profile->IsSystemProfile() &&
+         ash::ProfileHelper::IsUserProfile(profile) &&
+         !ash::ProfileHelper::IsEphemeralUserProfile(profile);
 }
 
 BrowserProcessPlatformPartChromeOS::BrowserRestoreObserver::
