70c03ebb38c3ee8ab6dee46022f038390295f382
Kalvin Lee <kdlee@chromium.org>
2025-04-06 22:57:36 -0700

    spanify busywork: Adjust `span_rust_unittest`
    Tweak this unit test to use the safe constructor of `base::span` in
    conjunction with its `first()` method (in lieu of the unsafe
    data-plus-extent constructor). Remove the unsafe buffers pragma.
    
    Change-Id: I15082ce23bcd98df6d60cafdd686954777ca7fcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433493
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443280}

--- base/containers/span_rust_unittest.cc
+++ base/containers/span_rust_unittest.cc
@@ -3,12 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/341324165): Fix and remove.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "base/containers/span_rust.h"
+
+#include <cstdint>
 
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -16,8 +13,8 @@ namespace base {
 namespace {
 
 TEST(BaseSpanRustTest, SliceConstruct) {
-  uint8_t data[] = {0, 1, 2, 3, 4};
-  span<const uint8_t> data_span(data, 2u);
+  constexpr uint8_t data[] = {0, 1, 2, 3, 4};
+  span<const uint8_t> data_span = base::span(data).first(2u);
   rust::Slice<const uint8_t> rust_slice = SpanToRustSlice(data_span);
   EXPECT_EQ(2ul, rust_slice.length());
   EXPECT_EQ(1, rust_slice[1]);
