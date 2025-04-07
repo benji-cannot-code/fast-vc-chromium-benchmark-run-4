4e7d708835f81a0910011fbf5c39c25675bf14f7
Sylvain Defresne <sdefresne@chromium.org>
2025-04-07 05:33:13 -0700

    [ios] Fix block capturing pointer to C++ objects
    The implementation of CredentialProviderMigratorAppAgent was using
    a block that captured a pointer to multiple C++ objects (profile &
    passkeyModel).
    
    Simplify the implementation by using an helper function (easier to
    use with base::BindOnce(...) without risking accidentally capturing
    variable by a block) and an helper method.
    
    While doing that, split the code performing the migration for all
    profiles and for a specific passkey model into two methods and move
    the shared logic into an helper method.
    
    Fixed: 408430684
    Change-Id: Id2537ed2af9e64eb88ad8d4fea80dd11838655be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432433
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443402}

--- ios/chrome/common/credential_provider/passkey_model_observer_bridge.h
+++ ios/chrome/common/credential_provider/passkey_model_observer_bridge.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/webauthn/core/browser/passkey_model.h"
 
 @protocol PasskeyModelObserverDelegate
+- (void)passKeyModelShuttingDown:(webauthn::PasskeyModel*)passkeyModel;
 - (void)passkeyModelIsReady:(webauthn::PasskeyModel*)passkeyModel;
 @end
 
