6e1f327b66504ee2a6d41b93311e16ee1bcfa348
Peter Kasting <pkasting@chromium.org>
2025-04-08 11:08:30 -0700

    Inline CreateLockScreenWidget() into lone caller.
    This code needs to be friended to access deprecated WidgetDelegate
    functionality, and it's easier to do that with a class than a standalone
    function.
    
    Bug: 407812144
    Change-Id: I761ca16e38b1bcb8f6a52fcdc97adb8ea8c5abc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441155
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444240}

--- ash/login/ui/lock_screen.cc
+++ ash/login/ui/lock_screen.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/lock_debug_view.h"
 #include "ash/login/ui/login_data_dispatcher.h"
 #include "ash/login/ui/login_detachable_base_model.h"
-#include "ash/public/cpp/lock_screen_widget_factory.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shelf/login_shelf_view.h"
@@ -139,8 +138,24 @@ void LockScreen::Show(ScreenType type) {
     parent = Shell::GetContainer(Shell::GetPrimaryRootWindow(),
                                  kShellWindowId_LockScreenContainer);
   }
-  instance_->widget_ =
-      CreateLockScreenWidget(parent, instance_->MakeContentsView());
+
+  instance_->widget_ = std::make_unique<views::Widget>();
+  views::Widget::InitParams params(
+      views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET,
+      views::Widget::InitParams::TYPE_WINDOW_FRAMELESS);
+  params.delegate = new views::WidgetDelegate();
+  params.delegate->SetOwnedByWidget(
+      views::WidgetDelegate::OwnedByWidgetPassKey());
+  params.delegate->SetContentsView(instance_->MakeContentsView());
+  params.delegate->SetInitiallyFocusedView(params.delegate->GetContentsView());
+
+  params.show_state = ui::mojom::WindowShowState::kFullscreen;
+  params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
+  params.parent = parent;
+  params.name = "LockScreenWidget";
+  instance_->widget_->Init(std::move(params));
+  instance_->widget_->SetVisibilityAnimationTransition(
+      views::Widget::ANIMATE_NONE);
   instance_->widget_->SetBounds(
       display::Screen::GetScreen()->GetPrimaryDisplay().bounds());
 
