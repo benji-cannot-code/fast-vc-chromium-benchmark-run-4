d2379cae30d828afb023be719b9530f25d08fbf4
Wenyu Fu <wenyufu@chromium.org>
2025-04-07 19:36:58 -0700

    [SurfaceColor] GTS: Update mini thumbnail placeholder color
    * Light / Dark: Surface 1 -> Surface Container Low
    * Before / After: https://crbug.com/406529540#comment4
    
    This change also rewrite getMiniThumbnailPlaceholderColor to improve the
    readability, and remove the unused TabThumbnailPlaceholderStyle.
    
    NO_IFTTT=No real semantic color changes.
    
    Bug: 406529540
    Change-Id: Ia11a670d98d788efc527ca9a4f01f557679356a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432713
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443898}

--- components/browser_ui/styles/android/java/src/org/chromium/components/browser_ui/styles/SemanticColorUtils.java
+++ components/browser_ui/styles/android/java/src/org/chromium/components/browser_ui/styles/SemanticColorUtils.java
@@ -52,7 +52,7 @@ public class SemanticColorUtils {
 
     /** Returns the semantic color value that corresponds to default_icon_color_on_accent1. */
     public static @ColorInt int getDefaultIconColorOnAccent1(Context context) {
-        return resolve(R.attr.colorOnPrimary, context);
+        return getColorOnPrimary(context);
     }
 
     /**
@@ -64,7 +64,7 @@ public class SemanticColorUtils {
 
     /** Returns the semantic color value that corresponds to default_text_color_on_accent1. */
     public static @ColorInt int getDefaultTextColorOnAccent1(Context context) {
-        return resolve(R.attr.colorOnPrimary, context);
+        return getColorOnPrimary(context);
     }
 
     /** Returns the semantic color value that corresponds to default_text_color_secondary. */
@@ -188,6 +188,11 @@ public class SemanticColorUtils {
     /** Returns the semantic color values that corresponds to colorPrimaryContainer. */
     public static @ColorInt int getColorPrimaryContainer(Context context) {
         return resolve(R.attr.colorPrimaryContainer, context);
+    }
+
+    /** Returns the semantic color values that correspond to colorOnPrimary. */
+    public static @ColorInt int getColorOnPrimary(Context context) {
+        return resolve(R.attr.colorOnPrimary, context);
     }
 
     /** Returns the semantic color values that correspond to colorOnSurfaceInverse. */
