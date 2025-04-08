b12e21986b7121ff03f039fd29dca5859a0ddc88
Ella Ge <eirage@chromium.org>
2025-04-08 13:37:40 -0700

    Track New Tab Page creation state with UserData
    This CL tracks the NTP creation state with a UserData attached to a Tab.
    When a new tab with NTP URL is created, and the launch type is from
    Chrome UI, and the NTP UI loading complete, we can focus to the Omnibox
    for targeted users.
    
    Bug: 408364837
    Change-Id: I6fca803dbd8415921145a2eac4c7638ab3b0c462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6431567
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444347}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -410,6 +410,10 @@ public class NewTabPage
             if (mIsDestroyed) return;
             mIsLoaded = true;
             NewTabPageUma.recordNtpImpression(NewTabPageUma.NTP_IMPRESSION_REGULAR);
+
+            var state = NewTabPageCreationState.from(mTab);
+            if (state != null) state.onNtpLoaded(this);
+
             // If not visible when loading completes, wait until onShown is received.
             if (!mTab.isHidden()) recordNtpShown();
         }
@@ -458,8 +462,7 @@ public class NewTabPage
      * @param lifecycleDispatcher Activity lifecycle dispatcher.
      * @param tabModelSelector {@link TabModelSelector} object.
      * @param isTablet {@code true} if running on a Tablet device.
-     * @param newTabPageCreationTracker {@link NewTabPageCreationTracker} object recording user
-     *     metrics.
+     * @param tabCreationTracker {@link NewTabPageCreationTracker} object recording user metrics.
      * @param isInNightMode {@code true} if the night mode setting is on.
      * @param nativePageHost The host that is showing this new tab page.
      * @param tab The {@link Tab} that contains this new tab page.
@@ -484,7 +487,7 @@ public class NewTabPage
             ActivityLifecycleDispatcher lifecycleDispatcher,
             TabModelSelector tabModelSelector,
             boolean isTablet,
-            NewTabPageCreationTracker mNewTabPageCreationTracker,
+            NewTabPageCreationTracker tabCreationTracker,
             boolean isInNightMode,
             NativePageHost nativePageHost,
             Tab tab,
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -189,6 +189,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidElegantTextHeight,
     &kAndroidKeyboardA11y,
     &kAndroidNoVisibleHintForDifferentTLD,
+    &kAndroidOmniboxFocusedNewTabPage,
     &kAndroidOpenPdfInlineBackport,
     &kAndroidPdfAssistContent,
     &kAndroidTabDeclutter,
@@ -525,6 +526,10 @@ BASE_FEATURE(kAndroidKeyboardA11y,
 BASE_FEATURE(kAndroidNoVisibleHintForDifferentTLD,
              "AndroidNoVisibleHintForDifferentTLD",
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAndroidOmniboxFocusedNewTabPage,
+             "AndroidOmniboxFocusedNewTabPage",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidOpenPdfInlineBackport,
              "AndroidOpenPdfInlineBackport",
