b7607f59156d21e668ab90638cd0dbee75fa37ad
Henrique Nakashima <hnakashima@chromium.org>
2025-04-08 06:42:29 -0700

    [Android] Migrate .compositor to Public Transit rules
    Migrate the tests shallowly to use Public Transit entry points.
    
    Bug: 399877997
    Change-Id: I937fe5dddb0b269e5227f7bfd582f49e73462381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437096
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444090}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/CompositorVisibilityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/CompositorVisibilityTest.java
@@ -11,7 +11,6 @@ import android.view.View;
 import androidx.test.filters.SmallTest;
 
 import org.junit.Assert;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,8 +21,8 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.chrome.browser.compositor.layouts.LayoutRenderHost;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
-import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
+import org.chromium.chrome.test.transit.AutoResetCtaTransitTestRule;
+import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.ui.resources.ResourceManager;
 
 /** Integration tests for {@link org.chromium.chrome.browser.compositor.CompositorView}. */
@@ -31,13 +30,9 @@ import org.chromium.ui.resources.ResourceManager;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @Batch(Batch.PER_CLASS)
 public class CompositorVisibilityTest {
-    @ClassRule
-    public static ChromeTabbedActivityTestRule sActivityTestRule =
-            new ChromeTabbedActivityTestRule();
-
     @Rule
-    public BlankCTATabInitialStateRule mBlankCTATabInitialStateRule =
-            new BlankCTATabInitialStateRule(sActivityTestRule, false);
+    public AutoResetCtaTransitTestRule mActivityTestRule =
+            ChromeTransitTestRules.fastAutoResetCtaActivityRule();
 
     private CompositorView mCompositorView;
 
@@ -76,7 +71,7 @@ public class CompositorVisibilityTest {
                     @Override
                     public void run() {
                         mCompositorView =
-                                new CompositorView(sActivityTestRule.getActivity(), mRenderHost);
+                                new CompositorView(mActivityTestRule.getActivity(), mRenderHost);
                         mCompositorView.setVisibility(View.VISIBLE);
                         Assert.assertEquals(
                                 View.VISIBLE, mCompositorView.getChildAt(0).getVisibility());
@@ -99,7 +94,7 @@ public class CompositorVisibilityTest {
                     @Override
                     public void run() {
                         mCompositorView =
-                                new CompositorView(sActivityTestRule.getActivity(), mRenderHost);
+                                new CompositorView(mActivityTestRule.getActivity(), mRenderHost);
                         Assert.assertEquals(1, mCompositorView.getChildCount());
                         Assert.assertTrue(mCompositorView.getChildAt(0) instanceof SurfaceView);
                     }
@@ -117,7 +112,7 @@ public class CompositorVisibilityTest {
                     @Override
                     public void run() {
                         mCompositorView =
-                                new CompositorView(sActivityTestRule.getActivity(), mRenderHost);
+                                new CompositorView(mActivityTestRule.getActivity(), mRenderHost);
                         Assert.assertEquals(View.VISIBLE, mCompositorView.getVisibility());
                         Assert.assertEquals(
                                 View.INVISIBLE, mCompositorView.getChildAt(0).getVisibility());
