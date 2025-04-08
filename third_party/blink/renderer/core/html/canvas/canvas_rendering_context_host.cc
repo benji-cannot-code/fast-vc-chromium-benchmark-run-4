d2b19bf08ca20761f1c04f92dc1082be70032666
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-04-08 08:33:32 -0700

    Remove GetOrCreateCanvasResourceProviderImpl hint parameter
    The hint parameter was only used for 2D HTMLCanvasElement contexts.
    HTMLCanvasElement::RecreateCanvasResourceProviderFor2DContext was the
    only caller specifying a hint and that hint was just forwarded to
    CanvasRenderingContextHost::CreateCanvasResourceProvider2D. The hint
    parameter can be removed if we inline the call to
    ShouldTryToUseGpuRaster in CreateCanvasResourceProvider2D.
    
    Bug: 346766781
    Change-Id: Iead83227d8acbbd58a691d0564a578f5f4372109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437253
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444142}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
@@ -121,12 +121,11 @@ bool CanvasRenderingContextHost::IsImageBitmapRenderingContext() const {
 
 CanvasResourceProvider*
 CanvasRenderingContextHost::GetOrCreateCanvasResourceProvider() {
-  return GetOrCreateCanvasResourceProviderImpl(RasterModeHint::kPreferGPU);
+  return GetOrCreateCanvasResourceProviderImpl();
 }
 
 CanvasResourceProvider*
-CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl(
-    RasterModeHint hint) {
+CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl() {
   if (!ResourceProvider() && !did_fail_to_create_resource_provider_) {
     if (IsValidImageSize(Size())) {
       if (IsWebGPU()) {
@@ -134,7 +133,7 @@ CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl(
       } else if (IsWebGL()) {
         CreateCanvasResourceProviderWebGL();
       } else {
-        CreateCanvasResourceProvider2D(hint);
+        CreateCanvasResourceProvider2D();
       }
     }
     if (!ResourceProvider())
@@ -249,8 +248,7 @@ void CanvasRenderingContextHost::CreateCanvasResourceProviderWebGL() {
   }
 }
 
-void CanvasRenderingContextHost::CreateCanvasResourceProvider2D(
-    RasterModeHint hint) {
+void CanvasRenderingContextHost::CreateCanvasResourceProvider2D() {
   DCHECK(IsRenderingContext2D() || IsImageBitmapRenderingContext());
   base::WeakPtr<CanvasResourceDispatcher> dispatcher =
       GetOrCreateResourceDispatcher()
@@ -261,8 +259,7 @@ void CanvasRenderingContextHost::CreateCanvasResourceProvider2D(
   const SkAlphaType alpha_type = GetRenderingContextAlphaType();
   const viz::SharedImageFormat format = GetRenderingContextFormat();
   const gfx::ColorSpace color_space = GetRenderingContextColorSpace();
-  const bool use_gpu =
-      hint == RasterModeHint::kPreferGPU && ShouldAccelerate2dContext();
+  const bool use_gpu = ShouldTryToUseGpuRaster() && ShouldAccelerate2dContext();
   constexpr auto kShouldInitialize =
       CanvasResourceProvider::ShouldInitialize::kCallClear;
   if (use_gpu && LowLatencyEnabled()) {
