486900cc0790dbe479b0a4c423701290e8872b12
Jagadesh P <jagadeshjai1999@gmail.com>
2025-04-08 11:15:35 -0700

    Fix dangling pointer in |MetricsCollectorTest|.
    Bug: 40212619
    Change-Id: Iab11fb77df066af4d5aea596662175e1f6e15971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6027652
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444245}

--- components/performance_manager/metrics/metrics_collector_unittest.cc
+++ components/performance_manager/metrics/metrics_collector_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/performance_manager/public/metrics/metrics_collector.h"
 
+#include <utility>
+
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -47,8 +49,10 @@ class MetricsCollectorTest : public GraphTestHarness {
   }
 
   void TearDown() override {
-    graph()->TakeFromGraph(metrics_collector_);  // Destroy the observer.
-    metrics_collector_ = nullptr;
+    // Clear |metrics_collector_| to avoid leaving a dangling pointer.
+    MetricsCollector* collector = std::exchange(metrics_collector_, nullptr);
+    // Destroy the observer.
+    graph()->TakeFromGraph(collector);
     Super::TearDown();
   }
 
@@ -58,7 +62,7 @@ class MetricsCollectorTest : public GraphTestHarness {
   base::HistogramTester histogram_tester_;
 
  private:
-  raw_ptr<MetricsCollector, DanglingUntriaged> metrics_collector_ = nullptr;
+  raw_ptr<MetricsCollector> metrics_collector_ = nullptr;
 };
 
 TEST_F(MetricsCollectorTest, ProcessLifetime_LaunchAndExit) {
