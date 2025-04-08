edfdf2e07df9dc306c2ae10f662722d4bf21d137
Colin Blundell <blundell@chromium.org>
2025-04-07 23:53:57 -0700

    [Blink] Remove CanvasRCHost::GetCanvasRenderingContextSkColorSpace()
    The two remaining internal callsites are passing this value to Skia and
    hence won't be converted to use gfx::ColorSpace. Inline the conversion
    at these callsites.
    
    Bug: 371227617
    Change-Id: I40e5973015fce80c666100de84be980479510138
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432732
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443958}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -1216,7 +1216,7 @@ scoped_refptr<StaticBitmapImage> HTMLCanvasElement::Snapshot(
               SkImageInfo::Make(
                   SkISize::Make(adjusted_size.width(), adjusted_size.height()),
                   color_type, kUnpremul_SkAlphaType,
-                  GetRenderingContextSkColorSpace()));
+                  GetRenderingContextColorSpace().ToSkColorSpace()));
         }
       }
     }
