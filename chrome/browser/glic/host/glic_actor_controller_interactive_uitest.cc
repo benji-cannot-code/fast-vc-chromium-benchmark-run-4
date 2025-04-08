96da5cc3d50331df70068c55a6a49799eb0c295f
Lan Wei <lanwei@chromium.org>
2025-04-08 14:59:11 -0700

    Implement scroll actor tool
    Bug: 402083666
    Change-Id: I94c5add3d2d0f1415f403186d03a73ac531112c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427366
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444395}

--- chrome/browser/glic/host/glic_actor_controller_interactive_uitest.cc
+++ chrome/browser/glic/host/glic_actor_controller_interactive_uitest.cc
@@ -41,7 +41,8 @@ class GlicActorControllerUiTest : public test::InteractiveGlicTest {
   ~GlicActorControllerUiTest() override = default;
 
   void SetUpOnMainThread() override {
-    embedded_test_server()->ServeFilesFromSourceDirectory(actor::kTestDataPath);
+    embedded_test_server()->ServeFilesFromSourceDirectory(
+        actor::kActorTestDataPath);
     test::InteractiveGlicTest::SetUpOnMainThread();
   }
 
--- chrome/renderer/actor/tool_executor.cc
+++ chrome/renderer/actor/tool_executor.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/actor.mojom.h"
 #include "chrome/renderer/actor/click_tool.h"
 #include "chrome/renderer/actor/mouse_move_tool.h"
+#include "chrome/renderer/actor/scroll_tool.h"
 #include "chrome/renderer/actor/tool_utils.h"
 #include "chrome/renderer/actor/type_tool.h"
 #include "content/public/renderer/render_frame.h"
@@ -52,6 +53,12 @@ void ToolExecutor::InvokeTool(mojom::ToolInvocationPtr request,
       CHECK(request->action->get_type());
       tool_ = std::make_unique<TypeTool>(std::move(request->action->get_type()),
                                          frame_);
+      break;
+    }
+    case actor::mojom::ToolAction::Tag::kScroll: {
+      CHECK(request->action->get_scroll());
+      tool_ = std::make_unique<ScrollTool>(
+          std::move(request->action->get_scroll()), frame_);
       break;
     }
   }
