c9b945d7fecbe6a790e37af5a1d143152a9446d8
Jun Ishiguro <junis@google.com>
2025-04-08 01:46:52 -0700

    Refactor ash/wm/desks
    This CL refactors ash/wm/desks to make it a bit more readable.
    
    This applies the following changes:
    - Use GetDeskContainerForRoot to get the desk container window.
    - Remove unused callback.
    - Use const boolean for common check in DesksController::ActivateDesk.
    
    Bug: b:398287318
    Change-Id: Id125ff99ef459c77a1ef1332cf075b45ee3a4cbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432996
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1443997}

--- ash/wm/desks/desk.cc
+++ ash/wm/desks/desk.cc
@@ -297,7 +297,7 @@ void Desk::OnRootWindowAdded(aura::Window* root) {
 
   // No windows should be added to the desk container on |root| prior to
   // tracking it by the desk.
-  aura::Window* desk_container = root->GetChildById(container_id_);
+  aura::Window* desk_container = GetDeskContainerForRoot(root);
   DCHECK(desk_container->children().empty());
   auto container_observer =
       std::make_unique<DeskContainerObserver>(this, desk_container);
@@ -496,7 +496,7 @@ void Desk::PrepareForActivationAnimation() {
   }
 
   for (aura::Window* root : Shell::GetAllRootWindows()) {
-    auto* container = root->GetChildById(container_id_);
+    auto* container = GetDeskContainerForRoot(root);
     container->layer()->SetOpacity(0);
     container->Show();
   }
@@ -511,8 +511,9 @@ void Desk::Activate(bool update_window_activation) {
   };
 
   if (!MaybeResetContainersOpacities()) {
-    for (aura::Window* root : Shell::GetAllRootWindows())
-      root->GetChildById(container_id_)->Show();
+    for (aura::Window* root : Shell::GetAllRootWindows()) {
+      GetDeskContainerForRoot(root)->Show();
+    }
   }
 
   is_active_ = true;
@@ -590,8 +591,9 @@ void Desk::Deactivate(bool update_window_activation) {
   auto* active_window = window_util::GetActiveWindow();
 
   // Hide the associated containers on all roots.
-  for (aura::Window* root : Shell::GetAllRootWindows())
-    root->GetChildById(container_id_)->Hide();
+  for (aura::Window* root : Shell::GetAllRootWindows()) {
+    GetDeskContainerForRoot(root)->Hide();
+  }
 
   is_active_ = false;
   last_day_visited_ = desks_restore_util::GetDaysFromLocalEpoch();
@@ -740,7 +742,6 @@ void Desk::MoveWindowToDesk(aura::Window* window,
 
 aura::Window* Desk::GetDeskContainerForRoot(aura::Window* root) const {
   DCHECK(root);
-
   return root->GetChildById(container_id_);
 }
 
@@ -1044,7 +1045,7 @@ bool Desk::MaybeResetContainersOpacities() {
     return false;
 
   for (aura::Window* root : Shell::GetAllRootWindows()) {
-    auto* container = root->GetChildById(container_id_);
+    auto* container = GetDeskContainerForRoot(root);
     container->layer()->SetOpacity(1);
   }
   started_activation_animation_ = false;
