419fff99f642880a26d9ac5342e7053f3c4acc90
Alvin Ji <alvinji@chromium.org>
2025-04-08 10:46:18 -0700

    usb: Use GlobalRenderFrameHostId in UsbChooserController
    The UsbChooserController currently holds a raw pointer to the requesting
    RenderFrameHost. This can lead to use-after-free issues if the
    RenderFrameHost is destroyed before the chooser controller. This CL
    replaces the raw pointer with a `GlobalRenderFrameHostId`. This ID can
    be used to retrieve the RenderFrameHost when needed, and checks are
    added to ensure the RenderFrameHost is still valid before accessing it.
    
    Bug: 405292639
    Change-Id: Ifedaf80f6700d57ea28691abfaf4d2ff9cdbb448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6440254
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444224}

--- chrome/browser/usb/usb_chooser_controller.h
+++ chrome/browser/usb/usb_chooser_controller.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "chrome/browser/usb/usb_chooser_context.h"
 #include "components/permissions/chooser_controller.h"
+#include "content/public/browser/global_routing_id.h"
 #include "services/device/public/mojom/usb_device.mojom.h"
 #include "third_party/blink/public/mojom/usb/web_usb_service.mojom.h"
 #include "url/origin.h"
@@ -65,8 +66,9 @@ class UsbChooserController : public permissions::ChooserController,
   blink::mojom::WebUsbService::GetPermissionCallback callback_;
   url::Origin origin_;
 
-  const raw_ptr<content::RenderFrameHost, AcrossTasksDanglingUntriaged>
-      requesting_frame_;
+  // Hold the GlobalRenderFrameHostId for requesting frame so we can always
+  // check whether the frame host is still valid before we access it.
+  const content::GlobalRenderFrameHostId render_frame_host_id_;
   base::WeakPtr<UsbChooserContext> chooser_context_;
   base::ScopedObservation<UsbChooserContext, UsbChooserContext::DeviceObserver>
       observation_{this};
