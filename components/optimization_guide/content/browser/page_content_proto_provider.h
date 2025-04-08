1acaf952a0deb73d51fc6dd5f0cfeffe27897161
Gary Klassen <gklassen@chromium.org>
2025-04-08 16:53:51 -0700

    Use mojom for AIPageContentMetadata.
    Eliminates the need to copy to/from C++ structs.
    
    Change-Id: Ief0662701cad0b48ff68c2b04fed8ae1b858b6cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437809
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444460}

--- components/optimization_guide/content/browser/page_content_proto_provider.h
+++ components/optimization_guide/content/browser/page_content_proto_provider.h
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/functional/callback.h"
-#include "components/optimization_guide/content/browser/ai_page_content_metadata.h"
 #include "components/optimization_guide/proto/features/model_prototyping.pb.h"
 #include "content/public/browser/document_user_data.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/weak_document_ptr.h"
 #include "third_party/blink/public/mojom/content_extraction/ai_page_content.mojom.h"
+#include "components/optimization_guide/content/mojom/ai_page_content_metadata.mojom.h"
 
 namespace content {
 class WebContents;
@@ -58,7 +58,7 @@ struct AIPageContentResult {
   ~AIPageContentResult();
 
   optimization_guide::proto::AnnotatedPageContent proto;
-  AIPageContentMetadata metadata;
+  optimization_guide::mojom::PageMetadataPtr metadata;
   // A map from a serialized unguessable token to the document pointer.
   // Callers should use this to map the frame identifiers in the proto to the
   // right frame host.
