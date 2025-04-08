7e1af041ae2c3be0951c4e61ab0830dac0844204
Maria Kazinova <kazinova@google.com>
2025-04-08 01:56:39 -0700

    [Passwords] Extend or clean up expiring UMAs.
    This CL
    1) Extends a few histograms that can still be of use
    2) Removes @khamutov from owners (he left Chrome in 2024)
    3) Updates autofill team maillist in owners to PWM  maillist
    4) Removes PrefilledUsernameFillOutcome, which was meant to be used
    for the rollout of `EnableOverwritingPlaceholderUsernames`, but the feature was abandoned with time after observing low impact during
    the experimentation.
    
    OBSOLETE_HISTOGRAM[PasswordManager.PrefilledUsernameFillOutcome]=Unused.This UMA was created for an experiment that was later abandoned
    
    Fixed: 407543778, 407543742
    Change-Id: Ib97a5336bf34cf35266b17126935e3dd361430eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6434214
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444003}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -2009,9 +2009,6 @@ bool PasswordAutofillAgent::FillCredentialsAutomatically(
   if (password.empty() && !is_single_username_fill) {
     if (username_element && !username_element.Value().IsEmpty() &&
         !prefilled_placeholder_username) {
-      LogPrefilledUsernameFillOutcome(
-          PrefilledUsernameFillOutcome::kPrefilledUsernameNotOverridden);
-
       LogMessage(logger, Logger::STRING_FAILED_TO_FILL_PREFILLED_USERNAME);
       LogFirstFillingResult(
           fill_data, FillingResult::kUsernamePrefilledWithIncompatibleValue);
@@ -2036,11 +2033,6 @@ bool PasswordAutofillAgent::FillCredentialsAutomatically(
          username_element.GetAutofillState() != WebAutofillState::kNotFilled ||
          prefilled_placeholder_username)) {
       FillFieldAutomatically(username, username_element);
-      if (prefilled_placeholder_username) {
-        LogPrefilledUsernameFillOutcome(
-            PrefilledUsernameFillOutcome::
-                kPrefilledPlaceholderUsernameOverridden);
-      }
     }
     if (logger)
       logger->LogElementName(Logger::STRING_USERNAME_FILLED, username_element);
@@ -2061,15 +2053,6 @@ bool PasswordAutofillAgent::FillCredentialsAutomatically(
 
   LogFirstFillingResult(fill_data, FillingResult::kSuccess);
   return true;
-}
-
-void PasswordAutofillAgent::LogPrefilledUsernameFillOutcome(
-    PrefilledUsernameFillOutcome outcome) {
-  if (prefilled_username_metrics_logged_)
-    return;
-  prefilled_username_metrics_logged_ = true;
-  UMA_HISTOGRAM_ENUMERATION("PasswordManager.PrefilledUsernameFillOutcome",
-                            outcome);
 }
 
 void PasswordAutofillAgent::FireHostSubmitEvent(
