d5043403c207b1a33d8ab3fd766fbee569c95a78
Colin Blundell <blundell@chromium.org>
2025-04-07 23:50:15 -0700

    [Blink] Remove redundant gfx::Size arg in CopyTextureToResourceProvider
    The size is always that of the texture. For the callsites that pass
    `swap_buffers_->Size()`:
    
    * That value originates here [1]
    * That method is called by GpuCanvasContext passing
      `swap_texture_descriptor_` [2]
    * `swap_texture_descriptor_` and `texture_descriptor_` share the same
      value for the size field, and those descriptors are what are used
      to configure `swap_texture_` and `texture_` respectively
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/webgpu_swap_buffer_provider.cc;l=146;drc=78883e6c824fd669688b9943096a7b01f87c1cb5;bpv=1;bpt=1
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc;l=654;drc=78883e6c824fd669688b9943096a7b01f87c1cb5;bpv=1;bpt=1
    
    Change-Id: I22f206e85574a8711186089395f450c82ab9978e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433918
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443951}

--- third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
@@ -174,9 +174,9 @@ scoped_refptr<StaticBitmapImage> GPUCanvasContext::GetImage(FlushReason) {
 
   // If there is a current texture, create a snapshot from it.
   if (texture_ && !texture_->IsDestroyed()) {
-    return SnapshotInternal(texture_->GetHandle(), swap_buffers_->Size());
+    return SnapshotInternal(texture_->GetHandle());
   } else if (swap_texture_) {
-    return SnapshotInternal(swap_texture_->GetHandle(), swap_buffers_->Size());
+    return SnapshotInternal(swap_texture_->GetHandle());
   }
 
   // If there is no current texture, return a snapshot of the front buffer if
@@ -186,10 +186,7 @@ scoped_refptr<StaticBitmapImage> GPUCanvasContext::GetImage(FlushReason) {
     return nullptr;
   }
 
-  return SnapshotInternal(
-      front_buffer_texture->GetTexture(),
-      gfx::Size(front_buffer_texture->GetTexture().GetWidth(),
-                front_buffer_texture->GetTexture().GetHeight()));
+  return SnapshotInternal(front_buffer_texture->GetTexture());
 }
 
 bool GPUCanvasContext::PaintRenderingResultsToCanvas(
@@ -245,9 +242,7 @@ bool GPUCanvasContext::PaintRenderingResultsToCanvas(
     return false;
   }
 
-  return CopyTextureToResourceProvider(
-      texture, gfx::Size(texture.GetWidth(), texture.GetHeight()),
-      resource_provider);
+  return CopyTextureToResourceProvider(texture, resource_provider);
 }
 
 bool GPUCanvasContext::CopyRenderingResultsToVideoFrame(
@@ -826,9 +821,10 @@ void GPUCanvasContext::CopyToSwapTexture() {
 
 bool GPUCanvasContext::CopyTextureToResourceProvider(
     const wgpu::Texture& texture,
-    const gfx::Size& size,
     CanvasResourceProvider* resource_provider) const {
   DCHECK(resource_provider);
+
+  gfx::Size size(texture.GetWidth(), texture.GetHeight());
   DCHECK_EQ(resource_provider->Size(), size);
 
   // This method will copy the contents of `texture` to `resource_provider`'s
@@ -953,8 +949,9 @@ bool GPUCanvasContext::CopyTextureToResourceProvider(
 }
 
 scoped_refptr<StaticBitmapImage> GPUCanvasContext::SnapshotInternal(
-    const wgpu::Texture& texture,
-    const gfx::Size& size) const {
+    const wgpu::Texture& texture) const {
+  gfx::Size size(texture.GetWidth(), texture.GetHeight());
+
   // We tag the SharedImage inside the WebGPUImageProvider with display usages
   // since there are uncommon paths which may use this snapshot for compositing.
   // These paths are usually related to either printing or either video and
@@ -966,8 +963,9 @@ scoped_refptr<StaticBitmapImage> GPUCanvasContext::SnapshotInternal(
   if (!resource_provider)
     return nullptr;
 
-  if (!CopyTextureToResourceProvider(texture, size, resource_provider.get()))
+  if (!CopyTextureToResourceProvider(texture, resource_provider.get())) {
     return nullptr;
+  }
 
   return resource_provider->Snapshot(FlushReason::kNone);
 }
