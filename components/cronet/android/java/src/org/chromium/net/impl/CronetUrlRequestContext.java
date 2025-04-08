c6a2124124bc4f431f3711c34181450c6d53e64a
Etienne Dechamps <edechamps@google.com>
2025-04-08 10:03:39 -0700

    Clarify Cronet thread names
    Currently, the way the Cronet network thread is named is a bit of mess:
    
    Native CronetContextAdapter sets the name to "network", only for Java
    CronetUrlRequestContext to immediately change it to "ChromiumNet", which
    seems pointless.
    
    Perfetto will actually latch on to the first name ("network") for
    display in traces, whereas other tools (such as crash dumps) will use
    the latest name ("ChromiumNet"). The inconsistency can cause confusion.
    
    Besides, neither "network" nor "ChromiumNet" seem like good names, as
    they are too generic. They are also inconsistent with the convention
    used for the init thread, which is called "CronetInit".
    
    This CL changes the name to "CronetNet", and gets rid of the pointless
    name switcharoo on the Java side.
    
    Also, do the same for the file thread while we're at it.
    
    Change-Id: I007c548738674e37e803937fd5fe8ab14eb3c3d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6440205
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444199}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
@@ -946,7 +946,6 @@ public class CronetUrlRequestContext extends CronetEngineBase {
     private void initNetworkThread() {
         mNetworkThread = Thread.currentThread();
         mInitCompleted.open();
-        Thread.currentThread().setName("ChromiumNet");
     }
 
     @SuppressWarnings("unused")
