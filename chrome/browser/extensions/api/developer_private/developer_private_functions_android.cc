5e6b55af1881f872edd1faabb541497b9a524462
Keigo Oka <oka@google.com>
2025-04-07 22:35:47 -0700

    extensions: Enable requestFileSource on desktop android
    The change was made with a simple cut and paste.
    
    Bug: 392777363
    Cq-Include-Trybots: luci.chromium.try:android-desktop-15-x64-rel
    Change-Id: I5fdcaf0d826be914c65c83920a1445c444075b5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6434531
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443932}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
@@ -23,9 +23,6 @@ DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivatePackDirectoryFunction,
                                         "developerPrivate.packDirectory")
 DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivateLoadDirectoryFunction,
                                         "developerPrivate.loadDirectory")
-DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(
-    DeveloperPrivateRequestFileSourceFunction,
-    "developerPrivate.requestFileSource")
 DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivateRepairExtensionFunction,
                                         "developerPrivate.repairExtension")
 DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivateShowOptionsFunction,
