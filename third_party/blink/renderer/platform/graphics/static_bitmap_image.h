e2883c608db0b1312d24d22ba18413ee20d212e6
Vasiliy Telezhnikov <vasilyt@chromium.org>
2025-04-08 08:45:13 -0700

    Remove StaticBitmapImage::IsOriginTopLeft
    Inline remaining call-sites and remove the function. All call-sites
    except one have AcceleratedStaticBitmapImage and get origin from shared
    image. One case is software path in ImageLayerBridge uses unaccelerated
    version and so always top-left.
    
    Bug: 378688985
    Change-Id: Ia54128437ecdbd8d4421de9a9ae91530516778e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6428312
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444151}

--- third_party/blink/renderer/platform/graphics/static_bitmap_image.h
+++ third_party/blink/renderer/platform/graphics/static_bitmap_image.h
@@ -54,7 +54,6 @@ class PLATFORM_EXPORT StaticBitmapImage : public Image {
   // sub-class
   virtual bool IsValid() const { return true; }
   virtual void Transfer() {}
-  virtual bool IsOriginTopLeft() const { return true; }
 
   // Creates a non-gpu copy of the image, or returns this if image is already
   // non-gpu.
