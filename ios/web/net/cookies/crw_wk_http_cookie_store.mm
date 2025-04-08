fb10e32da9d4052151322ca8467178d2ac1995c1
Justin Cohen <justincohen@google.com>
2025-04-08 13:05:30 -0700

    ios: Experiment with skipping cookie cache.
    There appear to be lots of BackupRefPtr corruption crashes related to
    this cache optimization.  There are also crashes deep within Apple's
    network code that could be related. Since this optimization may no
    longer be necessary, experiment with its removal.
    
    Bug: 40620220
    
    Change-Id: Ib5ab6920cbdb96f4852e35220b011b67740b69dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438934
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444320}

--- ios/web/net/cookies/crw_wk_http_cookie_store.mm
+++ ios/web/net/cookies/crw_wk_http_cookie_store.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/net/cookies/crw_wk_http_cookie_store.h"
 
 #import "base/check.h"
+#import "base/feature_list.h"
 #import "base/sequence_checker.h"
 #import "base/task/sequenced_task_runner.h"
 
@@ -24,6 +25,15 @@ void PrioritizeWKHTTPCookieStoreCallbacks(WKWebsiteDataStore* data_store) {
                     completionHandler:^(NSArray<WKWebsiteDataRecord*>* records){
                     }];
 }
+
+// There appear to be lots of BackupRefPtr corruption crashes related to
+// this cache optimization.  There are also crashes deep within Apple's
+// network code that could be related. Since this optimization may no
+// longer be necessary, experiment with its removal (crbug.com/40620220)
+BASE_FEATURE(kIOSSkipCookieCaching,
+             "SkipCookieCaching",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace
 
 @interface CRWWKHTTPCookieStore () <WKHTTPCookieStoreObserver>
@@ -58,7 +68,9 @@ void PrioritizeWKHTTPCookieStoreCallbacks(WKWebsiteDataStore* data_store) {
   __weak __typeof(self) weakSelf = self;
   [_websiteDataStore.httpCookieStore
       getAllCookies:^(NSArray<NSHTTPCookie*>* cookies) {
-        weakSelf.cachedCookies = cookies;
+        if (!base::FeatureList::IsEnabled(kIOSSkipCookieCaching)) {
+          weakSelf.cachedCookies = cookies;
+        }
         completionHandler(cookies);
       }];
   PrioritizeWKHTTPCookieStoreCallbacks(_websiteDataStore);
