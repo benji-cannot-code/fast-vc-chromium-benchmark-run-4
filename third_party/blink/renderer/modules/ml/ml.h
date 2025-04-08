ff482c9f438b5e95b7b38dab22d9b1ecd9a541db
Patrick To <patrto@microsoft.com>
2025-04-08 13:14:41 -0700

    WebNN: Add IDL for ML.createContext(GPUDevice)
    This change adds the IDL for the `ML.createContext(GPUDevice)` overload
    defined at https://www.w3.org/TR/webnn/#dom-ml-createcontext-gpudevice.
    
    This method initializes WebNN by accepting a WebGPU device. The WebNN
    context is then initialized on the GPU used by the WebGPU device. This
    is an alternative to `ML.createContext(MLContextOptions)`, which lets
    WebNN select a GPU based on the options specified.
    
    This change was originally reviewed in a larger CL that contains the
    full implementation of this function. However, due to the size of that
    CL, it will be split into smaller ones with this being the 1st.
    Original CL: http://crrev.com/c/5833743
    
    Change-Id: I436247d51361aeab21acee6e7d5e550c021f6cd0
    Bug: 409110243
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427159
    Commit-Queue: Patrick To <patrto@microsoft.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444330}

--- third_party/blink/renderer/modules/ml/ml.h
+++ third_party/blink/renderer/modules/ml/ml.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class GPUDevice;
 class MLContextOptions;
 class ScriptState;
 
@@ -40,6 +41,9 @@ class MODULES_EXPORT ML final : public ScriptWrappable,
   // IDL interface:
   ScriptPromise<MLContext> createContext(ScriptState* state,
                                          MLContextOptions* option,
+                                         ExceptionState& exception_state);
+  ScriptPromise<MLContext> createContext(ScriptState* state,
+                                         GPUDevice* gpu_device,
                                          ExceptionState& exception_state);
 
  private:
