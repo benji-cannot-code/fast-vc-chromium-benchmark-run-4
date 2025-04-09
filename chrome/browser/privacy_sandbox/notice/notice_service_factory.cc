1b8df29989e26533effb478a1de0dffe9b1a6ac1
Abe Boujane <boujane@google.com>
2025-04-09 07:40:20 -0700

    Change NoticeService to take dependencies in the constructor
    Bug: 409386887
    Change-Id: Ia64592721f307a86665bf597f89519e211961b62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437460
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444714}

--- chrome/browser/privacy_sandbox/notice/notice_service_factory.cc
+++ chrome/browser/privacy_sandbox/notice/notice_service_factory.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/no_destructor.h"
+#include "chrome/browser/privacy_sandbox/notice/notice_catalog.h"
 #include "chrome/browser/privacy_sandbox/notice/notice_service.h"
+#include "chrome/browser/privacy_sandbox/notice/notice_storage.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 
@@ -34,5 +36,6 @@ PrivacySandboxNoticeServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   Profile* profile = Profile::FromBrowserContext(context);
   return std::make_unique<privacy_sandbox::PrivacySandboxNoticeService>(
-      profile);
+      profile, std::make_unique<privacy_sandbox::NoticeCatalog>(),
+      std::make_unique<privacy_sandbox::PrivacySandboxNoticeStorage>());
 }
--- chrome/browser/privacy_sandbox/notice/notice_service_unittest.cc
+++ chrome/browser/privacy_sandbox/notice/notice_service_unittest.cc
@@ -36,8 +36,9 @@ class PrivacySandboxNoticeServiceTest
   PrivacySandboxNoticeServiceTest() {
     profile_ = IdentityTestEnvironmentProfileAdaptor::
         CreateProfileForIdentityTestEnvironment();
-    notice_service_ =
-        std::make_unique<PrivacySandboxNoticeService>(profile_.get());
+    notice_service_ = std::make_unique<PrivacySandboxNoticeService>(
+        profile_.get(), std::make_unique<NoticeCatalog>(),
+        std::make_unique<PrivacySandboxNoticeStorage>());
   }
 
  protected:
