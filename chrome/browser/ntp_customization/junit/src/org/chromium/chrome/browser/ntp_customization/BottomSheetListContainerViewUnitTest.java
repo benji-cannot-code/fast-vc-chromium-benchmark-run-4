a37c39b1bb5dc3e0fda726ef7a587cba26bfae70
Xinyi Ji <xinyiji@google.com>
2025-04-09 11:00:27 -0700

    [NTP Customization] Add NTP Customization Feed bottom sheet.
    This CL introduces the feed bottom sheet and its core functionalities: enabling/disabling the feed and navigating to different URLs upon clicking its sections.
    1. Screenshots illustrating the feed bottom sheet in various states:
    https://docs.google.com/document/d/1BpmwMpGqpaMbBskWj2BG32ULY9akbGwBeKCN5vf1frM/edit?usp=sharing&resourcekey=0-5yFHPoQYngYD3wI37AxnwA
    2. The video demonstrates the main functionalities of the feed bottom sheet:
    https://drive.google.com/file/d/1gS58R2CRKaJ8f1B_ZzNkXd3Q8qbNsMmp/view?usp=sharing&resourcekey=0-B87-AWabFT4SW6hMw8wTfg
    
    Bug: 400474804
    Change-Id: Ia311c61caa5fae505a2a4d141a408e274cf0d50e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6350933
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444840}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/BottomSheetListContainerViewUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/BottomSheetListContainerViewUnitTest.java
@@ -14,7 +14,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator.BottomSheetType.DISCOVER_FEED;
+import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator.BottomSheetType.FEED;
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator.BottomSheetType.NTP_CARDS;
 
 import android.content.Context;
@@ -59,7 +59,7 @@ public class BottomSheetListContainerViewUnitTest {
         // requires complicated setting of themes.
         doReturn(mListItemView).when(mContainerView).createListItemView();
 
-        mListContent = List.of(NTP_CARDS, DISCOVER_FEED);
+        mListContent = List.of(NTP_CARDS, FEED);
         when(mDelegate.getListItems()).thenReturn(mListContent);
     }
 
