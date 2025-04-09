ebc5f6761720f090534970565b2159f63a6f99e7
Yann Dago <ydago@chromium.org>
2025-04-09 06:45:21 -0700

    Fix disable signin not working with multi profiles
    Create a new pref kSigninAllowedOnBrowser for iOS in the local state to
    handle the "Allow Chrome signin" setting.
    
    The value of kSigninAllowedOnBrowser is the same as kSigninAllowed so
    that it can be used for migration of the existing profile pref to the
    local state.
    
    Both prefs are kept in sync inside of AuthenticationService so that low
    level components that depended on it do not get broken.
    
    With multi profiles, the pref needs to be in the local state to ensure
    all profiles know about it.
    
    This allows disabling Chrome sign-in from a managed profiles also
    disable it all other profiles. This is good because of the way profiles
    are implemented on iOS; top the user it seems like there is only a
    single profile, so it makes sense to disable sign-in altogether. This is
    different from desktop where each profile appears very distinct to the
    user.
    
    Bug: 407088982
    Change-Id: I58d236592e57bbe268df43dc18c0cf8ed0fd706d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6410371
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444690}

--- ios/chrome/browser/settings/ui_bundled/google_services/google_services_settings_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/google_services_settings_egtest.mm
@@ -99,7 +99,11 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
   [super setUp];
 
   // Some tests change the value of this pref, so reset.
-  [ChromeEarlGrey clearUserPrefWithName:prefs::kSigninAllowed];
+  [ChromeEarlGrey resetDataForLocalStatePref:prefs::kSigninAllowedOnDevice];
+  GREYAssertEqual(
+      [ChromeEarlGrey userBooleanPref:prefs::kSigninAllowed],
+      [ChromeEarlGrey localStateBooleanPref:prefs::kSigninAllowedOnDevice],
+      @"Signin allowed policy not synchronized");
   [BookmarkEarlGrey waitForBookmarkModelLoaded];
   [BookmarkEarlGrey clearBookmarks];
 }
@@ -107,7 +111,11 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
 - (void)tearDownHelper {
   [super tearDownHelper];
   // Some tests change the value of this pref, so reset.
-  [ChromeEarlGrey clearUserPrefWithName:prefs::kSigninAllowed];
+  [ChromeEarlGrey resetDataForLocalStatePref:prefs::kSigninAllowedOnDevice];
+  GREYAssertEqual(
+      [ChromeEarlGrey userBooleanPref:prefs::kSigninAllowed],
+      [ChromeEarlGrey localStateBooleanPref:prefs::kSigninAllowedOnDevice],
+      @"Signin allowed policy not synchronized");
   [BookmarkEarlGrey clearBookmarks];
   [BookmarkEarlGrey clearBookmarksPositionCache];
 }
