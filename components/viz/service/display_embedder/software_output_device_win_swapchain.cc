b0a8720c12969f9838cf02d9a9ad002c78259dc1
kylechar <kylechar@chromium.org>
2025-04-09 10:41:56 -0700

    Add RestartGpuProcessForContextLoss()
    Add a helper function that logs a message and terminates the GPU process
    with the context loss exit code. We're using this same logic in more
    than one place so make sure it's consistent.
    
    Bug: 40176068
    Change-Id: I1d37f7d3ab5fa88b5e8efd72ceef5714492070c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418447
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444821}

--- components/viz/service/display_embedder/software_output_device_win_swapchain.cc
+++ components/viz/service/display_embedder/software_output_device_win_swapchain.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/alias.h"
 #include "base/logging.h"
-#include "base/process/process.h"
 #include "base/strings/strcat.h"
 #include "components/viz/service/gl/exit_code.h"
 #include "mojo/public/cpp/system/platform_handle.h"
@@ -89,14 +88,12 @@ bool SoftwareOutputDeviceWinSwapChain::ResizeDelegated(
                                                 viewport_pixel_size.height(),
                                                 kDXGISwapChainFormat, 0);
     if (FAILED(hr)) {
-      LOG(ERROR) << "Exiting GPU process due to unrecoverable error. "
-                    "IDXGISwapChain::ResizeBuffers failed: "
-                 << logging::SystemErrorCodeToString(hr);
       // If ResizeBuffers fails, the swapchain and window sizes will be out of
       // sync, causing unexpected behavior such as permanent gutters or
       // clipping. Therefore, terminate the GPU process to refresh state.
-      base::Process::TerminateCurrentProcessImmediately(
-          static_cast<int>(ExitCode::RESULT_CODE_GPU_EXIT_ON_CONTEXT_LOST));
+      RestartGpuProcessForContextLoss(
+          base::StringPrintf("IDXGISwapChain::ResizeBuffers failed: %s",
+                             logging::SystemErrorCodeToString(hr)));
     }
   } else {
     // Defer the creation of DirectX related objects to when they're needed
@@ -159,15 +156,13 @@ bool SoftwareOutputDeviceWinSwapChain::ResizeDelegated(
     hr = dxgi_factory->CreateSwapChainForComposition(
         d3d11_device.Get(), &dxgi_swapchain_desc, nullptr, &dxgi_swapchain);
     if (FAILED(hr)) {
-      LOG(ERROR) << "Exiting GPU process due to unrecoverable error. "
-                    "IDXGIFactory2::CreateSwapChainForComposition failed: "
-                 << logging::SystemErrorCodeToString(hr);
       // An error in CreateSwapChainForComposition is observed to happen mostly
       // due to OOM issues. This causes the DXGI device to be removed resulting
       // in an error somewhere else. If this occurs, terminate the GPU process
       // here to refresh state.
-      base::Process::TerminateCurrentProcessImmediately(
-          static_cast<int>(ExitCode::RESULT_CODE_GPU_EXIT_ON_CONTEXT_LOST));
+      RestartGpuProcessForContextLoss(base::StringPrintf(
+          "IDXGIFactory2::CreateSwapChainForComposition failed: %s",
+          logging::SystemErrorCodeToString(hr)));
     }
 
     // Set swapchain as root visual content.
--- components/viz/service/gl/exit_code.h
+++ components/viz/service/gl/exit_code.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_SERVICE_GL_EXIT_CODE_H_
 #define COMPONENTS_VIZ_SERVICE_GL_EXIT_CODE_H_
 
+#include <string_view>
+
 namespace viz {
 enum class ExitCode {
   // Matches service_manager::ResultCode::RESULT_CODE_NORMAL_EXIT
@@ -13,6 +15,12 @@ enum class ExitCode {
   // Matches chrome::ResultCode::RESULT_CODE_GPU_EXIT_ON_CONTEXT_LOST
   RESULT_CODE_GPU_EXIT_ON_CONTEXT_LOST = 34,
 };
+
+// Called to restart the GPU process on context loss. First logs a message to
+// the user including `reason` and then terminates the GPU process. This does
+// not produce a crash report.
+void RestartGpuProcessForContextLoss(std::string_view reason);
+
 }  // namespace viz
 
 #endif  // COMPONENTS_VIZ_SERVICE_GL_EXIT_CODE_H_
