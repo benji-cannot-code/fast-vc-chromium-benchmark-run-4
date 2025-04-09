f0427f9902caf5fd006150400637312a17e38523
Thomas Guilbert <tguilbert@chromium.org>
2025-04-09 14:28:20 -0700

    Enforce minimum audio sample rate
    The media/base/limits.h defines a `kMinSampleRate` of 3000 samples/s,
    which is used in various locations throughout the code base.
    
    However, `AudioDecoderConfig::IsValidConfig()` accepts sample rates as
    low as 0. This can cause some confusion, if one code path manually
    checks the sample rate against `kMinSampleRate`, and another relies on
    `IsValidConfig()`.
    
    This CL updates `IsValidConfig()` to use `kMinSamplRate`. This might
    help prevent some SincResampler crashes.
    
    Bug: 406745979
    Change-Id: I3bb173b435f47c92d4c10d6765663b5e3dae2c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432670
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444948}

--- media/filters/source_buffer_stream_unittest.cc
+++ media/filters/source_buffer_stream_unittest.cc
@@ -41,6 +41,8 @@ namespace {
 
 enum class TimeGranularity { kMicrosecond, kMillisecond };
 
+static constexpr int kSampleRate = 3000;
+
 }  // namespace
 
 namespace media {
@@ -96,9 +98,9 @@ class SourceBufferStreamTest : public testing::Test {
   void SetAudioStream() {
     video_config_ = TestVideoConfig::Invalid();
     audio_config_.Initialize(AudioCodec::kVorbis, kSampleFormatPlanarF32,
-                             CHANNEL_LAYOUT_STEREO, 1000, EmptyExtraData(),
-                             EncryptionScheme::kUnencrypted, base::TimeDelta(),
-                             0);
+                             CHANNEL_LAYOUT_STEREO, kSampleRate,
+                             EmptyExtraData(), EncryptionScheme::kUnencrypted,
+                             base::TimeDelta(), 0);
     ResetStream<>(audio_config_);
 
     // Equivalent to 2ms per frame.
@@ -4518,9 +4520,9 @@ TEST_F(SourceBufferStreamTest, Audio_PrerollFrame) {
 }
 
 TEST_F(SourceBufferStreamTest, Audio_ConfigChangeWithPreroll) {
-  AudioDecoderConfig new_config(AudioCodec::kVorbis, kSampleFormatPlanarF32,
-                                CHANNEL_LAYOUT_MONO, 2000, EmptyExtraData(),
-                                EncryptionScheme::kUnencrypted);
+  AudioDecoderConfig new_config(
+      AudioCodec::kVorbis, kSampleFormatPlanarF32, CHANNEL_LAYOUT_MONO,
+      kSampleRate, EmptyExtraData(), EncryptionScheme::kUnencrypted);
   SetAudioStream();
   Seek(0);
 
@@ -4565,7 +4567,7 @@ TEST_F(SourceBufferStreamTest, Audio_Opus_SeekToJustBeforeRangeStart) {
   // interval requires a nonzero seek_preroll value.
   video_config_ = TestVideoConfig::Invalid();
   audio_config_.Initialize(AudioCodec::kOpus, kSampleFormatPlanarF32,
-                           CHANNEL_LAYOUT_STEREO, 1000, EmptyExtraData(),
+                           CHANNEL_LAYOUT_STEREO, kSampleRate, EmptyExtraData(),
                            EncryptionScheme::kUnencrypted,
                            base::Milliseconds(10), 0);
   ResetStream<>(audio_config_);
