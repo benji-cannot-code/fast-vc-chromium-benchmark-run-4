67c50a508c5da5ca681d32f440530530a88b4d44
Christine Ying <chrstne@google.com>
2025-04-09 08:40:06 -0700

    [NoticeFramework] Create a Mock Notice Service for tests
    Change-Id: I18d122bb7ed6c79390b5eeaa82f1085c75ad7824
    Bug: 322163208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438668
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444743}

--- chrome/browser/privacy_sandbox/notice/mock_notice_service.cc
+++ chrome/browser/privacy_sandbox/notice/mock_notice_service.cc
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/privacy_sandbox/notice/desktop_view_manager.h"
+#include "chrome/browser/privacy_sandbox/notice/mock_notice_service.h"
 
 namespace privacy_sandbox {
 
-DesktopViewManager::DesktopViewManager(
-    PrivacySandboxNoticeServiceInterface* notice_service) {}
-DesktopViewManager::~DesktopViewManager() = default;
+MockPrivacySandboxNoticeService::MockPrivacySandboxNoticeService() = default;
+MockPrivacySandboxNoticeService::~MockPrivacySandboxNoticeService() = default;
 
 }  // namespace privacy_sandbox
--- chrome/browser/privacy_sandbox/notice/notice_service_interface.h
+++ chrome/browser/privacy_sandbox/notice/notice_service_interface.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/privacy_sandbox/notice/notice.mojom-forward.h"
 
-
 namespace privacy_sandbox {
 
 enum class SurfaceType;
