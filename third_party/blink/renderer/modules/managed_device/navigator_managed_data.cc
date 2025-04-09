43dd58fc64a8826377dac468de5b84921096fb89
Patryk Chodur <pchodur@google.com>
2025-04-09 03:37:30 -0700

    Allow Device Attributes only in top level frame
    This commit unifies how the `IsTrustedContext` is called. Previously, it
    checked either the current frame or top level frame. Now, it does the
    check only on current frame, but additionally we allow only top level
    frames.
    
    Bug: 407230975
    Change-Id: Iaf789bfb1500effad6376e22d91e2bd903f24861
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6412113
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Robert Ferens <rferens@google.com>
    Commit-Queue: Patryk Chodur <pchodur@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444622}

--- third_party/blink/renderer/modules/managed_device/navigator_managed_data.cc
+++ third_party/blink/renderer/modules/managed_device/navigator_managed_data.cc
@@ -24,19 +24,22 @@ namespace blink {
 
 namespace {
 
-const char kNotHighTrustedAppExceptionMessage[] =
+constexpr char kNotHighTrustedAppExceptionMessage[] =
     "Managed configuration is empty. This API is available only for "
     "managed apps.";
-const char kServiceConnectionExceptionMessage[] =
+constexpr char kServiceConnectionExceptionMessage[] =
     "Service connection error. This API is available only for managed apps.";
 
 #if BUILDFLAG(IS_ANDROID)
-const char kManagedConfigNotSupported[] =
+constexpr char kManagedConfigNotSupported[] =
     "Managed Configuration API is not supported on this platform.";
 #endif  // BUILDFLAG(IS_ANDROID)
 
-const char kDeviceAttributesNotAllowedByPermissionsPolicy[] =
+constexpr char kDeviceAttributesNotAllowedByPermissionsPolicy[] =
     "Permissions-Policy: device-attributes are disabled.";
+
+constexpr char kDeviceAttributesNotAllowedInChildFrames[] =
+    "This API is allowed only in top level frames.";
 
 bool IsDeviceAttributesPermissionsPolicyFeatureEnabled() {
   return RuntimeEnabledFeatures::DeviceAttributesPermissionPolicyEnabled();
@@ -273,6 +276,14 @@ bool NavigatorManagedData::CheckDeviceAttributesAllowed(
     ExceptionState& exception_state) {
   ExecutionContext* const context = GetExecutionContext();
   if (!context) {
+    return false;
+  }
+  LocalDOMWindow* window = DynamicTo<LocalDOMWindow>(context);
+  CHECK(window);
+  if (!window->GetFrame()->IsMainFrame() ||
+      window->GetFrame()->IsInFencedFrameTree()) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kNotAllowedError,
+                                      kDeviceAttributesNotAllowedInChildFrames);
     return false;
   }
   if (!AreDeviceAttributesAllowedByPermissionsPolicy(context)) {
