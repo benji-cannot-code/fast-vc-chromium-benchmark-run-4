1cd6df172588f9aa743bdaea2e4212ba7756e0b2
Fredrik Söderquist <fs@opera.com>
2025-04-09 15:41:23 -0700

    Use base::VSpanPrintf() in StringBuilder::AppendFormat()
    Bug: 351564777
    Change-Id: I093c478d0cc734eacb92b37bd05a1610e2a82326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6439004
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1444987}

--- third_party/blink/renderer/platform/wtf/text/string_builder.cc
+++ third_party/blink/renderer/platform/wtf/text/string_builder.cc
@@ -25,17 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 #include <algorithm>
 #include <optional>
 
-#include "base/strings/string_util.h"
+#include "base/strings/span_printf.h"
 #include "third_party/blink/renderer/platform/wtf/dtoa.h"
 #include "third_party/blink/renderer/platform/wtf/text/integer_to_string_conversion.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -307,14 +302,14 @@ void StringBuilder::AppendFormat(const char* format, ...) {
   Vector<char, kDefaultSize> buffer(kDefaultSize);
 
   va_start(args, format);
-  int length = base::vsnprintf(buffer.data(), kDefaultSize, format, args);
+  int length = base::VSpanPrintf(buffer, format, args);
   va_end(args);
   DCHECK_GE(length, 0);
 
   if (length >= static_cast<int>(kDefaultSize)) {
     buffer.Grow(length + 1);
     va_start(args, format);
-    length = base::vsnprintf(buffer.data(), buffer.size(), format, args);
+    length = base::VSpanPrintf(buffer, format, args);
     va_end(args);
   }
 
