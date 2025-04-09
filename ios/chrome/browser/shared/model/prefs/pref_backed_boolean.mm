f5203af5a734b37d3d49563422347858dfd7acf3
Sylvain Defresne <sdefresne@chromium.org>
2025-04-09 15:00:23 -0700

    [ios] Check that -stop is called on PrefBackedBoolean
    If -stop is not called, then the unregistration of BooleanPrefMember
    from the PrefService will happen in -dealloc. This is problematic as
    the lifetime of the PrefBackedBoolean may be arbitrarily extended if
    it the last reference is "owned" by the autorelease pool.
    
    To avoid this risk, force all clients of PrefBackedBoolean to call
    -stop explicitly by adding a CHECK(...) in -dealloc that -stop has
    been called.
    
    Bug: 407023920
    Change-Id: Ica8b6a3ecad334ed297bc6024b2293c73d43f5d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433123
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444972}

--- ios/chrome/browser/shared/model/prefs/pref_backed_boolean.mm
+++ ios/chrome/browser/shared/model/prefs/pref_backed_boolean.mm
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation PrefBackedBoolean {
   BooleanPrefMember _pref;
+
+  // Record whether -stop was called.
+  BOOL _stopped;
 }
 
 @synthesize observer = _observer;
@@ -21,13 +24,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Use weak pointer to prevent circular dependency.
     __weak PrefBackedBoolean* weakSelf = self;
     _pref.Init(prefName, prefs, base::BindRepeating(^() {
-                 PrefBackedBoolean* strongSelf = weakSelf;
-                 if (strongSelf) {
-                   [strongSelf.observer booleanDidChange:strongSelf];
-                 }
+                 [weakSelf notifyObserver];
                }));
   }
   return self;
+}
+
+- (void)dealloc {
+  CHECK(_stopped) << "-stop should be called on PrefBackedBoolean";
 }
 
 - (BOOL)value {
@@ -39,7 +43,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
+  _stopped = YES;
   _pref.Destroy();
+}
+
+- (void)notifyObserver {
+  [_observer booleanDidChange:self];
 }
 
 @end
