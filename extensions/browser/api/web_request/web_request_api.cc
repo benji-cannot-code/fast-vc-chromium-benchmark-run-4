d46d6221dbdc08a341f01d17fed1c1a16d04e209
Andrea Orru <andreaorru@chromium.org>
2025-04-08 20:20:19 -0700

    Reland "[Extensions] Regression test: WebRequestAPI ResetURLLoaderFactories bug"
    Test needs to be skipped when `kForceWebRequestProxyForTest` is enabled
    (for example, in `network_service_linux` trybot).
    
    That's because factories are never reset when this feature is enabled.
    
    See
    https://crsrc.org/c/extensions/browser/api/web_request/web_request_api.cc;drc=e1dda381f52e3a553444a434e89139e016675701;l=680
    
    This is a reland of commit e1dda381f52e3a553444a434e89139e016675701
    
    Original change's description:
    > [Extensions] Regression test: WebRequestAPI ResetURLLoaderFactories bug
    >
    > This change adds a regression test for the referenced bug.
    >
    > It tests both that enabling the `DeferResetURLLoaderFactories` feature
    > flag resolves the issue, and that disabling it causes the issue.
    >
    > Design doc: http://go/defer-reset-url-loader-factories
    >
    > Bug: 394523691
    > Change-Id: I46671553e4c0631416b76654c476cb6e29dd65ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6424484
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1443868}
    
    Bug: 394523691
    Change-Id: I21b26c9a60a470be52148a6c11de474b38a4b496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438674
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444520}

--- extensions/browser/api/web_request/web_request_api.cc
+++ extensions/browser/api/web_request/web_request_api.cc
@@ -94,9 +94,7 @@ namespace web_request = api::web_request;
 
 namespace {
 
-BASE_FEATURE(kDeferResetURLLoaderFactories,
-             "DeferResetURLLoaderFactories",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+WebRequestAPI::TestObserver* g_test_observer = nullptr;
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
@@ -345,7 +343,8 @@ WebRequestAPI::WebRequestAPI(content::BrowserContext* context)
   // We only have to observe ServiceWorkerTaskQueue and react to
   // `OnAllRegistrationsStored` if we need to defer calls to
   // `ResetURLLoaderFactories` to after registration storage.
-  if (base::FeatureList::IsEnabled(kDeferResetURLLoaderFactories)) {
+  if (base::FeatureList::IsEnabled(
+          extensions_features::kDeferResetURLLoaderFactories)) {
     service_worker_task_queue_observation_.Observe(
         ServiceWorkerTaskQueue::Get(browser_context_));
   }
@@ -373,6 +372,15 @@ BrowserContextKeyedAPIFactory<WebRequestAPI>*
 WebRequestAPI::GetFactoryInstance() {
   return g_factory.Pointer();
 }
+
+// static
+void WebRequestAPI::SetObserverForTest(TestObserver* observer) {
+  g_test_observer = observer;
+}
+
+WebRequestAPI::TestObserver::TestObserver() = default;
+
+WebRequestAPI::TestObserver::~TestObserver() = default;
 
 void WebRequestAPI::OnListenerRemoved(const EventListenerInfo& details) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -716,10 +724,18 @@ bool WebRequestAPI::HasExtraHeadersListenerForTesting() {
       ->HasAnyExtraHeadersListener(browser_context_);
 }
 
+void WebRequestAPI::ResetURLLoaderFactories() {
+  browser_context_->GetDefaultStoragePartition()->ResetURLLoaderFactories();
+  if (g_test_observer) {
+    g_test_observer->OnDidResetURLLoaderFactories();
+  }
+}
+
 void WebRequestAPI::UpdateMayHaveProxies() {
   bool may_have_proxies = MayHaveProxies();
   if (!may_have_proxies_ && may_have_proxies) {
-    if (base::FeatureList::IsEnabled(kDeferResetURLLoaderFactories) &&
+    if (base::FeatureList::IsEnabled(
+            extensions_features::kDeferResetURLLoaderFactories) &&
         has_pending_worker_registrations_) {
       // If any service worker registration is still in flight (started, but not
       // stored), it's not safe to call `ResetURLLoaderFactories`, since it
@@ -732,7 +748,7 @@ void WebRequestAPI::UpdateMayHaveProxies() {
       // can be reset safely.
     } else {
       // Otherwise, we can safely call it now.
-      browser_context_->GetDefaultStoragePartition()->ResetURLLoaderFactories();
+      ResetURLLoaderFactories();
     }
   }
   may_have_proxies_ = may_have_proxies;
@@ -762,7 +778,7 @@ void WebRequestAPI::OnAllRegistrationsStored() {
   service_worker_context_observation_.RemoveAllObservations();
   // ...and reset the URLLoaderFactories if we haven't done it already.
   if (deferred_reset_url_loader_factories_) {
-    browser_context_->GetDefaultStoragePartition()->ResetURLLoaderFactories();
+    ResetURLLoaderFactories();
     deferred_reset_url_loader_factories_ = false;
   }
 }
