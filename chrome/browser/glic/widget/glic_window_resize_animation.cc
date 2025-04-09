5492fed14ef74732521aa26981bb5209e20e290d
Andrea Gomez <andreaxg@google.com>
2025-04-09 10:22:45 -0700

    Remove call to End animation if glic is being dragged.
    Removes the End animation call and instead just returns.
    
    Bug: 409334265
    Change-Id: Iacb09e283edeab0f274cb8395ad94f8a563b96e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6442542
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Sana Akbani <sanaakbani@google.com>
    Auto-Submit: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444812}

--- chrome/browser/glic/widget/glic_window_resize_animation.cc
+++ chrome/browser/glic/widget/glic_window_resize_animation.cc
@@ -64,7 +64,6 @@ GlicWindowResizeAnimation::~GlicWindowResizeAnimation() {
 
 void GlicWindowResizeAnimation::AnimateToState(double state) {
   if (window_controller_->IsDragging()) {
-    End();
     return;
   }
   gfx::Rect bounds_to_animate = gfx::Tween::RectValueBetween(
