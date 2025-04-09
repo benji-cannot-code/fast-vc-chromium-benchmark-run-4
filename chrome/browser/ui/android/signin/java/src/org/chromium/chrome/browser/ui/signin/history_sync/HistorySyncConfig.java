158d4d6e0f78d32932309dd059b3b9201a97c6f1
Menghan Yang <myuu@google.com>
2025-04-09 01:26:27 -0700

    [Android][Signin] Avoid using Parcelable in sign-in activity extras
    This CL adds back the workaround in SigninAndHistorySyncActivity to
    add config objects in the intent extras without using Parcelables, to
    fix related crash. It also removes the Parcelable implementation from
    the impacted objects to avoid accidental use in the meanwhile.
    
    Bug: 394559360
    Change-Id: I8d5f3c45bc68cc8e1153e94f7a3a8430dfd977fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333509
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1444584}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncConfig.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncConfig.java
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ui.signin.history_sync;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
@@ -19,7 +16,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.util.Objects;
 
 /* Class containing IDs of resources for the history sync opt-in view. */
-public final class HistorySyncConfig implements Parcelable {
+public final class HistorySyncConfig {
 
     /** The visibility rule to apply to the history opt-in step in the sign-in flow. */
     @IntDef({OptInMode.NONE, OptInMode.OPTIONAL, OptInMode.REQUIRED})
@@ -38,19 +35,6 @@ public final class HistorySyncConfig implements Parcelable {
     public final @StringRes int titleId;
     public final @StringRes int subtitleId;
 
-    public static final Parcelable.Creator<HistorySyncConfig> CREATOR =
-            new Parcelable.Creator<HistorySyncConfig>() {
-                @Override
-                public HistorySyncConfig createFromParcel(Parcel in) {
-                    return new HistorySyncConfig(in);
-                }
-
-                @Override
-                public HistorySyncConfig[] newArray(int size) {
-                    return new HistorySyncConfig[size];
-                }
-            };
-
     public HistorySyncConfig() {
         this(/* titleId= */ 0, /* subtitleId= */ 0);
     }
@@ -58,10 +42,6 @@ public final class HistorySyncConfig implements Parcelable {
     public HistorySyncConfig(@StringRes int titleId, @StringRes int subtitleId) {
         this.titleId = titleId == 0 ? R.string.history_sync_title : titleId;
         this.subtitleId = subtitleId == 0 ? R.string.history_sync_subtitle : subtitleId;
-    }
-
-    private HistorySyncConfig(Parcel in) {
-        this(/* titleId= */ in.readInt(), /* subtitleId= */ in.readInt());
     }
 
     @Override
@@ -76,18 +56,5 @@ public final class HistorySyncConfig implements Parcelable {
     @Override
     public int hashCode() {
         return Objects.hash(titleId, subtitleId);
-    }
-
-    /** Implements {@link Parcelable} */
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    /** Implements {@link Parcelable} */
-    @Override
-    public void writeToParcel(Parcel out, int flags) {
-        out.writeInt(titleId);
-        out.writeInt(subtitleId);
     }
 }
