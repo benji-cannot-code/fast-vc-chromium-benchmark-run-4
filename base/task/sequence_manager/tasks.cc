153a24e0ee9c1cb58013202fd42803614f3a9c4e
Gabriel Charette <gab@chromium.org>
2025-04-10 12:40:10 -0700

    Cleanup ExplicitHighResolutionTimerWin
    Bug: 40158967
    Fixed: 362247076
    Change-Id: I91aa67db7ccf7ec87db2682593be90142d77a86c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6133860
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445437}

--- base/task/sequence_manager/tasks.cc
+++ base/task/sequence_manager/tasks.cc
@@ -15,7 +15,6 @@ Task::Task(internal::PostedTask posted_task,
            EnqueueOrder sequence_order,
            EnqueueOrder enqueue_order,
            TimeTicks queue_time,
-           WakeUpResolution resolution,
            TimeDelta leeway)
     : PendingTask(
           posted_task.location,
@@ -43,7 +42,6 @@ Task::Task(internal::PostedTask posted_task,
   // change of |PendingTask::sequence_num|'s type.
   static_assert(std::is_same_v<decltype(sequence_num), int>, "");
   sequence_num = static_cast<int>(sequence_order);
-  this->is_high_res = resolution == WakeUpResolution::kHigh;
 }
 
 Task::Task(Task&& move_from) = default;
