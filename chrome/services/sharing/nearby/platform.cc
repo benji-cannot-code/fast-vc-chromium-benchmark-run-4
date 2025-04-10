7b6e65aa4c650502b8200be2964cdf1e00394524
Crisrael Lucero <crisrael@google.com>
2025-04-10 15:11:14 -0700

    Roll nearby-connections from 8acf9249344e to 531dd284007e (18 revisions)
    This CL supports cl/742808756 and cl/746121206 to remove dependency on
    third_party/beto-core. This change also supports the handful of AWDL CLs
    (e.g., cl/742925127 cl/744206141).
    
    https://chromium.googlesource.com/external/github.com/google/nearby-connections.git/+log/8acf9249344e..531dd284007e
    
    2025-04-10 crisrael@google.com Updated preprocessors to include correct LDT stub impl
    > PiperOrigin-RevId: 746121206
    >
    
    2025-04-10 edwinwu@google.com Implement openL2CAPServerWithCompletionHandler in GNCBLEMedium class.
    > PiperOrigin-RevId: 745877478
    >
    
    2025-04-10 edwinwu@google.com Refactor the GNCBLEL2CAPServer.
    > PiperOrigin-RevId: 745855701
    >
    
    2025-04-08 guogang@google.com Handle discovered endpoint when using DCT format
    > PiperOrigin-RevId: 745276198
    >
    
    2025-04-08 hais@google.com Add AWDL in HelloConnection
    > PiperOrigin-RevId: 745025636
    >
    
    2025-04-07 hais@google.com adds disconnection error code
    > PiperOrigin-RevId: 744896066
    >
    
    2025-04-07 guogang@google.com Fix issue to build endpoint info
    > PiperOrigin-RevId: 744895491
    >
    
    2025-04-07 ftsui@google.com Improve logging of skipped share targets due to vendorID.
    > PiperOrigin-RevId: 744814331
    >
    
    2025-04-07 ftsui@google.com Add logging of uploaded and downloaded cert ids.
    > PiperOrigin-RevId: 744807951
    >
    
    2025-04-07 ftsui@google.com More frequent contact sync.
    > PiperOrigin-RevId: 744801485
    >
    
    2025-04-07 guogang@google.com Update payload type values
    > PiperOrigin-RevId: 744763516
    >
    
    2025-04-05 hais@google.com Implement AWDL in connection layer (part 2)
    > PiperOrigin-RevId: 744206141
    >
    
    2025-04-02 hais@google.com Implement AWDL in connection layer (part 1)
    > PiperOrigin-RevId: 742925127
    >
    
    2025-04-02 guogang@google.com Refactor thread pool
    > PiperOrigin-RevId: 742919232
    >
    
    2025-04-02 ftsui@google.com Stop generating selected contacts certs.
    > PiperOrigin-RevId: 742919089
    >
    
    2025-04-01 hais@google.com Add back in Ldt stub impl, so it can be used in chromium
    > PiperOrigin-RevId: 742808756
    >
    
    2025-04-01 johnsonlu@google.com Add fields for dynamic role switching
    > PiperOrigin-RevId: 742474968
    >
    
    2025-03-31 ftsui@google.com Move device ID generation to login success.
    > PiperOrigin-RevId: 742428028
    >
    
    Bug: 362502228
    Test: Manually verified on DUT
    Change-Id: Iaaef938aff0c554b0da1304bd2223e43ea99fc19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448105
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445542}

--- chrome/services/sharing/nearby/platform.cc
+++ chrome/services/sharing/nearby/platform.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/shared_remote.h"
 #include "third_party/nearby/src/internal/platform/implementation/atomic_boolean.h"
 #include "third_party/nearby/src/internal/platform/implementation/atomic_reference.h"
+#include "third_party/nearby/src/internal/platform/implementation/awdl.h"
 #include "third_party/nearby/src/internal/platform/implementation/ble.h"
 #include "third_party/nearby/src/internal/platform/implementation/ble_v2.h"
 #include "third_party/nearby/src/internal/platform/implementation/bluetooth_adapter.h"
@@ -175,6 +176,12 @@ ImplementationPlatform::CreateScheduledExecutor() {
 std::unique_ptr<AtomicUint32> ImplementationPlatform::CreateAtomicUint32(
     std::uint32_t initial_value) {
   return std::make_unique<chrome::AtomicUint32>(initial_value);
+}
+
+std::unique_ptr<AwdlMedium> ImplementationPlatform::CreateAwdlMedium() {
+  // This constructor is not supported by Chrome.
+  NOTIMPLEMENTED();
+  return nullptr;
 }
 
 std::unique_ptr<BluetoothAdapter>
