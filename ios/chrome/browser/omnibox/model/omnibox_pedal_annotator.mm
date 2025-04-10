ae6eed972334a07d7fd6e3fe38acc14b61c5bbaf
Ameur Hosni <ameurhosni@google.com>
2025-04-10 01:34:42 -0700

    [IOS][OR] Move omnibox pedal swift out of ui bundled
    Bug: 409007712
    Change-Id: If26107c39d1f9f226d180c5af935a9d66835f832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441925
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445157}

--- ios/chrome/browser/omnibox/model/omnibox_pedal_annotator.mm
+++ ios/chrome/browser/omnibox/model/omnibox_pedal_annotator.mm
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/omnibox/browser/autocomplete_match.h"
 #import "components/password_manager/core/browser/ui/password_check_referrer.h"
 #import "ios/chrome/browser/default_browser/model/promo_source.h"
-#import "ios/chrome/browser/omnibox/ui_bundled/popup/popup_swift.h"
+#import "ios/chrome/browser/omnibox/model/omnibox_pedal_swift.h"
 #import "ios/chrome/browser/settings/ui_bundled/clear_browsing_data/features.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
