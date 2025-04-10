ea9c521b35920950fb3979c0e9344f8370427d45
Maggie Jennings <mjenn@google.com>
2025-04-10 15:29:52 -0700

    Clean-up Tracking Protection route and test case
    The row was cleaned up in crrev.com/c/6021307 and users trying to access
    the route will still be redirected to the /cookies page.
    
    Bug: b:408036586
    Change-Id: Iaad725ee27fb811553e907cf0ff927eb793f4215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441955
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445552}

--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -1097,11 +1097,6 @@ IN_PROC_BROWSER_TEST_F(SettingsPrivacyPageTest, CookiesSubpage) {
   RunTest("settings/privacy_page_test.js", "runMochaSuite('CookiesSubpage')");
 }
 
-IN_PROC_BROWSER_TEST_F(SettingsPrivacyPageTest, TrackingProtectionSubpage) {
-  RunTest("settings/privacy_page_test.js",
-          "runMochaSuite('TrackingProtectionSubpage')");
-}
-
 IN_PROC_BROWSER_TEST_F(SettingsPrivacyPageTest, AllSitesSubpage) {
   RunTest("settings/privacy_page_test.js", "runMochaSuite('AllSitesSubpage')");
 }
