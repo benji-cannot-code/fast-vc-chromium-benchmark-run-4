3b5e4895177234a700e4610081773f1d3997cb16
Vidhan Jain <vidhanj@google.com>
2025-04-10 05:59:34 -0700

    Add enum AddressUIComponentIconType
    AddressUIComponentIconType will be used to determine the icons that need
    to be displayed in the field based on the field type across platforms.
    
    Change-Id: Idc37b58048c4d7077abe18d10cd589ab46aacc18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441926
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445237}

--- ios/chrome/browser/infobars/ui_bundled/modals/autofill_address_profile/infobar_save_address_profile_table_view_controller.mm
+++ ios/chrome/browser/infobars/ui_bundled/modals/autofill_address_profile/infobar_save_address_profile_table_view_controller.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/user_metrics_action.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/autofill/core/browser/field_types.h"
+#import "components/autofill/core/browser/ui/addresses/autofill_address_util.h"
 #import "components/autofill/core/common/autofill_features.h"
 #import "components/autofill/ios/common/features.h"
 #import "components/strings/grit/components_strings.h"
@@ -453,22 +454,18 @@ const CGFloat kInfobarSaveAddressProfileSeparatorInset = 54;
 
 // Returns the symbol corresponding to the `type` for the update modal view.
 - (UIImage*)symbolForUpdateModelFromAutofillType:(autofill::FieldType)type {
-  switch (type) {
-    case autofill::NAME_FULL:
-    case autofill::ALTERNATIVE_FULL_NAME:
+  switch (GetAddressUIComponentIconTypeForFieldType(type)) {
+    case autofill::AddressUIComponentIconType::kNoIcon:
+      return nil;
+    case autofill::AddressUIComponentIconType::kName:
       return DefaultSymbolTemplateWithPointSize(kPersonFillSymbol, kSymbolSize);
-    case autofill::ADDRESS_HOME_STREET_ADDRESS:
-    case autofill::ADDRESS_HOME_ADDRESS:
+    case autofill::AddressUIComponentIconType::kAddress:
       return CustomSymbolTemplateWithPointSize(kLocationSymbol, kSymbolSize);
-    case autofill::EMAIL_ADDRESS:
+    case autofill::AddressUIComponentIconType::kEmail:
       return DefaultSymbolTemplateWithPointSize(kMailFillSymbol, kSymbolSize);
-    case autofill::PHONE_HOME_WHOLE_NUMBER:
+    case autofill::AddressUIComponentIconType::kPhone:
       return DefaultSymbolTemplateWithPointSize(kPhoneFillSymbol, kSymbolSize);
-    default:
-      break;
   }
-
-  NOTREACHED();
 }
 
 // Returns YES if the old section is shown in the update modal.
