da657e2b3f6346b405f22b387223fa161ce53bbb
Mark Rowe <markrowe@chromium.org>
2025-04-10 12:20:34 -0700

    Remove ItemDelete from the Mac version of AppleKeychain
    It no longer has callers and was never implemented for the iOS /
    `SecItem` implementation of `AppleKeychain`. If there's a need for it,
    it can be brought back after the migration away from `SecKeychain` is
    complete.
    
    Bug: 395659797
    Change-Id: I6a6a21009ff519cb3b40937e06a3c61cdb15aaee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6443473
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445409}

--- crypto/mock_apple_keychain_mac.cc
+++ crypto/mock_apple_keychain_mac.cc
@@ -17,8 +17,4 @@ MockAppleKeychain::MockAppleKeychain()
 
 MockAppleKeychain::~MockAppleKeychain() {}
 
-OSStatus MockAppleKeychain::ItemDelete(AppleSecKeychainItemRef itemRef) const {
-  return noErr;
-}
-
 }  // namespace crypto
