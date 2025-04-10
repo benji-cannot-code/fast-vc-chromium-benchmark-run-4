94ea27b21a3ff043c0c21eb478e8cf94f59bb41b
Sourav <sinhasourav@google.com>
2025-04-10 07:38:45 -0700

    [iOSAddAddressManually] Add field trial config
    This CL adds a field trial config for the AddAddressManually feature.
    
    Change-Id: I41e480776b33dc295efef795cb02af72aa31ebac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6442102
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445279}

--- components/autofill/ios/common/features.mm
+++ components/autofill/ios/common/features.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Features that are exlusive to iOS go here in alphabetical order.
 
 BASE_FEATURE(kAddAddressManually,
-             "AddAdressManually",
+             "AddAddressManually",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsAddAddressManuallyEnabled() {
