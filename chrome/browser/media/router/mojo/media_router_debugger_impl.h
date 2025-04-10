efa17ca84b3b6e77a9bf90df55cafbd9f40ef2f7
Andrew Paseltiner <apaseltiner@chromium.org>
2025-04-10 05:24:56 -0700

    Remove unnecessary base::Value clones
    These methods accepted a base::Value by value, not by reference, but had
    a misleading/pessimizing const qualifier.
    
    Change-Id: I9ec0899af7f34afd807319fa34c9091db5276011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6442317
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445224}

--- chrome/browser/media/router/mojo/media_router_debugger_impl.h
+++ chrome/browser/media/router/mojo/media_router_debugger_impl.h
@@ -47,7 +47,7 @@ class MediaRouterDebuggerImpl : public MediaRouterDebugger,
   // mojom::Debugger overrides:
   void ShouldFetchMirroringStats(
       ShouldFetchMirroringStatsCallback callback) override;
-  void OnMirroringStats(const base::Value json_stats) override;
+  void OnMirroringStats(base::Value json_stats) override;
   void BindReceiver(mojo::PendingReceiver<mojom::Debugger> receiver) override;
 
  protected:
@@ -55,7 +55,7 @@ class MediaRouterDebuggerImpl : public MediaRouterDebugger,
   FRIEND_TEST_ALL_PREFIXES(MediaRouterDebuggerImplTest,
                            ShouldFetchMirroringStatsFeatureDisabled);
 
-  void NotifyGetMirroringStats(const base::Value::Dict& json_logs);
+  void NotifyGetMirroringStats(base::Value::Dict json_logs);
   void LogMirroringStats();
 
   base::ObserverList<MirroringStatsObserver> observers_;
