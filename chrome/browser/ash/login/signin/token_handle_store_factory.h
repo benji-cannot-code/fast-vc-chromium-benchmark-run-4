8067399a082145f2811acc27c00d796f305569af
Elie Maamari <emaamari@google.com>
2025-04-10 06:11:59 -0700

    Implement TokenHandleStoreFactory::DoesUserHaveGaiaPassword functor
    This functor takes an `AccountId` and tries to determine if the
    corresponding user has a gaia password by calling `AuthFactorEditor`.
    
    A single request can be in flight for a given `AccountId`, but
    concurrent requests can be in flight for distinct `AccountId`s.
    
    We pass this functor as a RepeatingCallback to TokenHandleStoreImpl.
    
    Bug: b:383733245
    Change-Id: Ibbf1d3ca8e75a8e90b97622ed4db2cb1f5dc261b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6404716
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445244}

--- chrome/browser/ash/login/signin/token_handle_store_factory.h
+++ chrome/browser/ash/login/signin/token_handle_store_factory.h
@@ -6,8 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_LOGIN_SIGNIN_TOKEN_HANDLE_STORE_FACTORY_H_
 #define CHROME_BROWSER_ASH_LOGIN_SIGNIN_TOKEN_HANDLE_STORE_FACTORY_H_
 
+#include <memory>
+
 #include "ash/public/cpp/token_handle_store.h"
+#include "base/containers/flat_map.h"
+#include "base/memory/weak_ptr.h"
 #include "base/no_destructor.h"
+#include "chromeos/ash/components/login/auth/auth_factor_editor.h"
 
 namespace ash {
 
@@ -18,7 +23,6 @@ namespace ash {
 // TokenHandleStoreImpl.
 // This class is temporary, and will be removed once we completely migrate to
 // TokenHandleStoreImpl.
-// TODO(b/387248794): Remove as part of cleanup.
 class TokenHandleStoreFactory {
  public:
   TokenHandleStoreFactory(const TokenHandleStoreFactory&) = delete;
@@ -29,11 +33,51 @@ class TokenHandleStoreFactory {
   TokenHandleStore* GetTokenHandleStore();
 
  private:
+  // Functor that determines if a given `account_id` has a gaia password.
+  // The class maintains the invariant that at any given time, there is at most
+  // one request in flight for a given `account_id`.
+  class DoesUserHaveGaiaPassword {
+   public:
+    using OnUserHasGaiaPasswordDetermined =
+        base::OnceCallback<void(std::optional<bool>)>;
+    using DoesUserHaveGaiaPasswordCallback = base::RepeatingCallback<void(
+        const AccountId&,
+        TokenHandleStoreFactory::DoesUserHaveGaiaPassword ::
+            OnUserHasGaiaPasswordDetermined)>;
+
+    explicit DoesUserHaveGaiaPassword(
+        std::unique_ptr<AuthFactorEditor> factor_editor);
+    ~DoesUserHaveGaiaPassword();
+    DoesUserHaveGaiaPassword(const DoesUserHaveGaiaPassword&) = delete;
+    DoesUserHaveGaiaPassword& operator=(const DoesUserHaveGaiaPassword&) =
+        delete;
+
+    void Run(const AccountId& account_id,
+             OnUserHasGaiaPasswordDetermined callback);
+
+    DoesUserHaveGaiaPasswordCallback CreateRepeatingCallback();
+
+   private:
+    // Callback passed to `AuthFactorEditor::GetAuthFactorConfiguration`.
+    void OnGetAuthFactorConfiguration(std::unique_ptr<UserContext> user_context,
+                                      std::optional<AuthenticationError> error);
+
+    // Runs cleanup logic after replying to the request for `account_id`.
+    void OnRepliedToRequest(const AccountId account_id);
+
+    std::unique_ptr<AuthFactorEditor> factor_editor_;
+    base::flat_map<AccountId, OnUserHasGaiaPasswordDetermined> callbacks_;
+    base::WeakPtrFactory<DoesUserHaveGaiaPassword> weak_factory_{this};
+  };
+
   friend class base::NoDestructor<TokenHandleStoreFactory>;
+
+  std::unique_ptr<TokenHandleStore> CreateTokenHandleStoreImpl();
 
   TokenHandleStoreFactory();
   ~TokenHandleStoreFactory();
 
+  DoesUserHaveGaiaPassword does_user_have_gaia_password_;
   std::unique_ptr<TokenHandleStore> token_handle_store_;
 };
 
