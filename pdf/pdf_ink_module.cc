70864bf27cdfe42480c9f6fa5c0cc908c0d48d78
rbpotter <rbpotter@chromium.org>
2025-04-10 08:30:21 -0700

    [PDF Ink Signatures] Send messages to set text annotation mode and font
    Adding messages to the plugin when turning on or off text annotation
    mode and when updating font parameters.
    
    Sends an initial message to the plugin to get the fonts list the first
    time fonts are needed.
    
    Bug: 402546154
    Change-Id: I3f538e75aef835aee3fcabbe1540bdd3933b37a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6434865
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445322}

--- pdf/pdf_ink_module.cc
+++ pdf/pdf_ink_module.cc
@@ -315,6 +315,10 @@ bool PdfInkModule::OnMessage(const base::Value::Dict& message) {
           {"setAnnotationBrush",
            &PdfInkModule::HandleSetAnnotationBrushMessage},
           {"setAnnotationMode", &PdfInkModule::HandleSetAnnotationModeMessage},
+          {"getTextAnnotFontNames",
+           &PdfInkModule::HandleGetTextAnnotFontNamesMessage},
+          {"setTextAnnotationFont",
+           &PdfInkModule::HandleSetTextAnnotationFontMessage},
       });
 
   auto it = kMessageHandlers.find(*message.FindString("type"));
@@ -1027,7 +1031,9 @@ void PdfInkModule::HandleSetAnnotationBrushMessage(
 
 void PdfInkModule::HandleSetAnnotationModeMessage(
     const base::Value::Dict& message) {
-  enabled_ = message.FindBool("enable").value();
+  const std::string* mode = message.FindString("mode");
+  CHECK(mode);
+  enabled_ = *mode == "draw";
   client_->OnAnnotationModeToggled(enabled_);
   if (enabled_ && !loaded_data_from_pdf_) {
     loaded_data_from_pdf_ = true;
@@ -1042,6 +1048,18 @@ void PdfInkModule::HandleSetAnnotationModeMessage(
     }
   }
   MaybeSetCursor();
+}
+
+void PdfInkModule::HandleGetTextAnnotFontNamesMessage(
+    const base::Value::Dict& message) {
+  // TODO(crbug.com/409439509): Fill in this method. For now, just create it
+  // so the backend doesn't CHECK when it's sent from the frontend.
+}
+
+void PdfInkModule::HandleSetTextAnnotationFontMessage(
+    const base::Value::Dict& message) {
+  // TODO(crbug.com/409439509): Fill in this method. For now, just create it
+  // so the backend doesn't CHECK when it's sent from the frontend.
 }
 
 PdfInkBrush& PdfInkModule::GetDrawingBrush() {
