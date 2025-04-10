691c190b832f3f6b4de1db54d3d916c6ea3836da
Peter Birk Pakkenberg <pbirk@chromium.org>
2025-04-10 06:57:32 -0700

    Add flag for sequenced shouldInterceptRequest
    This flag re-enables the intended behaviour of shouldInterceptRequest in
    WebView by posting all calls to a single sequenced TaskRunner, instead
    of posting each call on a new TaskRunner.
    
    Also adds a new metric to measure the total duration for
    shouldInterceptRequest in WebView.
    
    Bug: 407009844
    Change-Id: If66212386eb114eda0be637bff80606c50260f75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437733
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445261}

--- android_webview/browser/aw_contents_io_thread_client.cc
+++ android_webview/browser/aw_contents_io_thread_client.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
@@ -358,6 +359,12 @@ void ClientMapEntryUpdater::WebContentsDestroyed() {
   delete this;
 }
 
+base::SequencedTaskRunner* GetGlobalSequencedTaskRunner() {
+  static base::NoDestructor<scoped_refptr<base::SequencedTaskRunner>> instance(
+      base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()}));
+  return instance->get();
+}
+
 }  // namespace
 
 WebContentsKey GetWebContentsKey(content::WebContents& web_contents) {
@@ -454,6 +461,7 @@ AwContentsIoThreadClient::InterceptResponseData NoInterceptRequest() {
 }
 
 void OnShouldInterceptCallback(
+    const base::TimeTicks request_started,
     AwContentsIoThreadClient::ShouldInterceptRequestResponseCallback callback,
     AwWebResourceInterceptResponse java_response) {
   JNIEnv* env = AttachCurrentThread();
@@ -476,12 +484,17 @@ void OnShouldInterceptCallback(
   }
   response_data.response =
       std::make_unique<AwWebResourceInterceptResponse>(java_response);
+
+  base::UmaHistogramTimes(
+      "Android.WebView.ShouldInterceptRequest.InterceptDuration",
+      base::TimeTicks::Now() - request_started);
   content::GetIOThreadTaskRunner({})->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), std::move(response_data)));
 }
 
 void StartShouldInterceptRequest(
     AwWebResourceRequest request,
+    const base::TimeTicks request_started,
     AwContentsIoThreadClient::ShouldInterceptRequestResponseCallback callback,
     JavaObjectWeakGlobalRef ref) {
   // Historically this method was called `RunShouldInterceptRequest` and so we
@@ -506,8 +519,8 @@ void StartShouldInterceptRequest(
   Java_ShouldInterceptRequestMediator_shouldInterceptRequestFromNative(
       env, obj, request,
       base::android::ToJniCallback(
-          env,
-          base::BindOnce(&OnShouldInterceptCallback, std::move(callback))));
+          env, base::BindOnce(&OnShouldInterceptCallback, request_started,
+                              std::move(callback))));
 }
 
 }  // namespace
@@ -535,11 +548,22 @@ void AwContentsIoThreadClient::ShouldInterceptRequestAsync(
       "Android.WebView.ShouldInterceptRequest.IsRequestSkipped", !mediator);
 
   if (mediator) {
+    const base::TimeTicks request_started = base::TimeTicks::Now();
     // The mediator is kept alive on the Java side.
-    sequenced_task_runner_->PostTask(
-        FROM_HERE, base::BindOnce(&StartShouldInterceptRequest,
-                                  std::move(request), std::move(callback),
-                                  JavaObjectWeakGlobalRef(env, mediator)));
+    if (base::FeatureList::IsEnabled(
+            features::kWebViewSequencedShouldInterceptRequest)) {
+      GetGlobalSequencedTaskRunner()->PostTask(
+          FROM_HERE,
+          base::BindOnce(&StartShouldInterceptRequest, std::move(request),
+                         request_started, std::move(callback),
+                         JavaObjectWeakGlobalRef(env, mediator)));
+    } else {
+      sequenced_task_runner_->PostTask(
+          FROM_HERE,
+          base::BindOnce(&StartShouldInterceptRequest, std::move(request),
+                         request_started, std::move(callback),
+                         JavaObjectWeakGlobalRef(env, mediator)));
+    }
   } else {
     Java_AwContentsIoThreadClient_onLoadResource(env, java_object_,
                                                  request.url);
