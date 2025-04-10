3272efc39d2701e613de7c889fbfeb2acaf26a6b
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-04-10 16:58:09 -0700

    [Extensions] Clean up  ReloadExtension() in extension_util.cc
    ReloadExtension() curently uses a combination of direct calls into
    the DesktopAndroidExtensionSystem and ExtensionService. These can be
    replaced with a call to ExtensionRegistrar (which is shared across
    platforms). Do so.
    
    This allows us to remove DesktopAndroidExtensionSystem::ReloadExtension.
    
    This CL has no behavior change.
    
    Bug: 409821655
    Change-Id: Iabe008a090ae11d34fa9e10aa015c0b51eba7cfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448887
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445595}

--- chrome/browser/extensions/extension_util.cc
+++ chrome/browser/extensions/extension_util.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/site_instance.h"
 #include "extensions/browser/disable_reason.h"
 #include "extensions/browser/extension_prefs.h"
+#include "extensions/browser/extension_registrar.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extension_util.h"
@@ -47,9 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/file_manager/app_id.h"
 #endif
 
-#if BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/extensions/desktop_android/desktop_android_extension_system.h"
-#else
+#if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_sync_service.h"
 #include "chrome/browser/ui/browser.h"
@@ -67,18 +66,8 @@ std::string ReloadExtension(const std::string& extension_id,
   // When we reload the extension the ID may be invalidated if we've passed it
   // by const ref everywhere. Make a copy to be safe. http://crbug.com/103762
   std::string id = extension_id;
-#if BUILDFLAG(IS_ANDROID)
-  DesktopAndroidExtensionSystem* extension_system =
-      static_cast<DesktopAndroidExtensionSystem*>(
-          ExtensionSystem::Get(context));
-  CHECK(extension_system);
-  extension_system->ReloadExtension(id);
-#else
-  ExtensionService* service =
-      ExtensionSystem::Get(context)->extension_service();
-  CHECK(service);
-  service->ReloadExtension(id);
-#endif
+  ExtensionRegistrar::Get(context)->ReloadExtension(
+      extension_id, ExtensionRegistrar::LoadErrorBehavior::kNoisy);
   return id;
 }
 
