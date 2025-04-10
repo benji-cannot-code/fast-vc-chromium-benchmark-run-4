a1b9f7c5d5f9c8397b48ad0300f1f24ad227c6d2
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-04-10 06:45:19 -0700

    [gardener] Disable ExceptionSnapshotLinux.Raise
    Bug: 409657856
    Change-Id: I860d990472674596d5e317c5118e1c6e67ee8537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447093
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: proberge <proberge@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445253}

--- third_party/crashpad/crashpad/snapshot/linux/exception_snapshot_linux_test.cc
+++ third_party/crashpad/crashpad/snapshot/linux/exception_snapshot_linux_test.cc
@@ -446,7 +446,8 @@ class RaiseTest {
 };
 bool RaiseTest::test_complete_ = false;
 
-TEST(ExceptionSnapshotLinux, Raise) {
+// TODO(crbug.com/409657856): Flaky
+TEST(ExceptionSnapshotLinux, DISABLED_Raise) {
   RaiseTest::Run();
 }
 
