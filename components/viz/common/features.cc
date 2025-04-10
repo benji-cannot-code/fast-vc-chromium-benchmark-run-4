85354a282f0b788cd20295776256a7d7b76a2771
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-04-10 02:31:30 -0700

    Remove feature `OcclusionCullingForTextureQuads`
    The feature has been enabled since M134, with no issue reported. So we
    can safely remove the feature.
    
    Bug: b:390503465
    Change-Id: I9218a0ea0d3665e8e41bc41cd0d1b0f7046a5e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441625
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445174}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -264,11 +264,6 @@ BASE_FEATURE(kAllowUndamagedNonrootRenderPassToSkip,
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
-// Enables occlusion culling for TextureDrawQuads when possible.
-BASE_FEATURE(kOcclusionCullingForTextureQuads,
-             "OcclusionCullingForTextureQuads",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Allow SurfaceAggregator to merge render passes when they contain quads that
 // require overlay (e.g. protected video). See usage in |EmitSurfaceContent|.
 BASE_FEATURE(kAllowForceMergeRenderPassWithRequireOverlayQuads,
@@ -539,12 +534,6 @@ int MaxOverlaysConsidered() {
 
 bool ShouldOnBeginFrameThrottleVideo() {
   return base::FeatureList::IsEnabled(features::kOnBeginFrameThrottleVideo);
-}
-
-bool IsOcclusionCullingForTextureQuadsEnabled() {
-  static bool enabled =
-      base::FeatureList::IsEnabled(features::kOcclusionCullingForTextureQuads);
-  return enabled;
 }
 
 bool ShouldDrawImmediatelyWhenInteractive() {
--- components/viz/service/display/occlusion_culler.cc
+++ components/viz/service/display/occlusion_culler.cc
@@ -419,10 +419,6 @@ bool OcclusionCuller::CanSplitDrawQuad(
   }
 
   if (quad->material == DrawQuad::Material::kTextureContent) {
-    if (!features::IsOcclusionCullingForTextureQuadsEnabled()) {
-      return false;
-    }
-
     // Exclude possible overlay candidates from quad splitting. See
     // `OverlayCandidateFactory::FromDrawQuad()`.
     if (resource_provider_->IsOverlayCandidate(quad->resource_id)) {
