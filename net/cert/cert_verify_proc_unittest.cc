7e7bfad4f7b5998bc30597a127ff5053c7aba9f2
Matt Mueller <mattm@chromium.org>
2025-04-10 11:57:38 -0700

    cleanup some CHROME_ROOT_STORE related ifdefs
    The linux and chromeos conditions are a remnant from when those
    platforms used the builtin verifier without CRS. They exclusively use
    CRS now, so they don't need to be listed explicitly in the ifdefs.
    
    Bug: 40890963
    Change-Id: Ia5065b1f3c2180339308595e726aa66242c57e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6444257
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445395}

--- net/cert/cert_verify_proc_unittest.cc
+++ net/cert/cert_verify_proc_unittest.cc
@@ -79,8 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif BUILDFLAG(IS_IOS)
 #include "base/ios/ios_util.h"
 #include "net/cert/cert_verify_proc_ios.h"
-#elif BUILDFLAG(IS_MAC)
-#include "base/mac/mac_util.h"
 #endif
 
 #if BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
