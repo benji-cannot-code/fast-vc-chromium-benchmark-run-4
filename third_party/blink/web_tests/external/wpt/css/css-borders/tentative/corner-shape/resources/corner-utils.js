244e83cc5fd6609e47ab0ce7fdef24d5c1078a89
Noam Rosenthal <nrosenthal@chromium.org>
2025-04-10 04:48:21 -0700

    corner-shape: remove curvature adjustment
    Curvature adjustment created the illusion that the border had
    consistent thickness.
    
    However, the thicker the border, the closer the inner curvature came
    to scoop/straight, and stopped resembling the original shape.
    
    Undoing the curvature adjustment - as having a bit of a belly in the
    very high/low curvatures is better than not having an inner shape.
    
    Bug: 399449172
    Change-Id: I84da4f36b10edbf1fa3f3f8a3b6270e5dc9c440f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6443779
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1445212}

--- third_party/blink/web_tests/external/wpt/css/css-borders/tentative/corner-shape/resources/corner-utils.js
+++ third_party/blink/web_tests/external/wpt/css/css-borders/tentative/corner-shape/resources/corner-utils.js
@@ -19,16 +19,6 @@ function offset_for_curvature(curvature) {
   return [norm_a, -norm_b];
 }
 
-function compute_inner_curvature(curvature, outer_length, inner_length) {
-  if (curvature === 0)
-    return 0;
-  if (curvature < 1)
-    return 1 / compute_inner_curvature(1 / curvature, outer_length, inner_length);
-  const target_length = (inner_length - outer_length) / Math.SQRT2;
-  return Math.log(0.5) /
-      Math.log((superellipse(curvature).x * outer_length + target_length) / inner_length);
-}
-
 /**
  *
  * @param {number} curvature
@@ -138,8 +128,6 @@ function resolve_corner_params(style, width, height, outset = null) {
             outer_rect[2] - outer_rect[0], outer_rect[3] - outer_rect[1]);
           const inner_length = Math.hypot(
             inner_rect[2] - inner_rect[0], inner_rect[3] - inner_rect[1])
-          inner_shape =
-            compute_inner_curvature(shape, outer_length, inner_length);
         }
 
         return [
@@ -151,7 +139,6 @@ function resolve_corner_params(style, width, height, outset = null) {
             inset,
             inner_rect,
             inner_offset,
-            inner_shape
           },
         ];
       }));
