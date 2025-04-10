cc07b715429ad57ef42504b630d3bca0e5924aad
Arthur Milchior <arthurmilchior@google.com>
2025-04-10 07:03:53 -0700

    [iOS][Mim2]Add continuation provider to ShowSigninCommand
    This is done to prepare future CL using those new commands.
    
    Bug: 375605572
    Change-Id: I803b9d4bef105c3e78898c4cd0f399d86bf1a3bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433120
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445267}

--- ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_performer.mm
+++ ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_performer.mm
@@ -81,6 +81,18 @@ const int64_t kAuthenticationFlowTimeoutSeconds = 10;
 NSString* const kAuthenticationSnackbarCategory =
     @"AuthenticationSnackbarCategory";
 
+// The change profile continuation for the authentication flow.
+void AuthenticationFlowContinuationImpl(
+    id<AuthenticationFlowPerformerDelegate> delegate,
+    SceneState* scene_state,
+    base::OnceClosure closure) {
+  CHECK(delegate);
+  [delegate
+      didSwitchToProfileWithNewProfileBrowser:
+          scene_state.browserProviderInterface.currentBrowserProvider.browser
+                                   completion:std::move(closure)];
+}
+
 // Handler for the signout action from a snackbar. Will `clear_selected_type`
 // if it is not std::nullopt.
 void HandleSignoutForSnackbar(
@@ -110,18 +122,6 @@ void HandleSignoutForSnackbar(
   signin::ProfileSignoutRequest(
       signin_metrics::ProfileSignout::kUserTappedUndoRightAfterSignIn)
       .Run(browser);
-}
-
-// The change profile continuation for the authentication flow.
-void AuthenticationFlowContinuationImpl(
-    id<AuthenticationFlowPerformerDelegate> delegate,
-    SceneState* scene_state,
-    base::OnceClosure closure) {
-  CHECK(delegate);
-  [delegate
-      didSwitchToProfileWithNewProfileBrowser:
-          scene_state.browserProviderInterface.currentBrowserProvider.browser
-                                   completion:std::move(closure)];
 }
 
 }  // namespace
@@ -277,7 +277,6 @@ void AuthenticationFlowContinuationImpl(
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE, base::BindOnce(
                        [](__typeof(_delegate) delegate) {
-                         CHECK(delegate);
                          [delegate didFailToSwitchToProfile];
                        },
                        weakDelegate));
