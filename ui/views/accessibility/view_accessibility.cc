43b1906832d6dc9b66b1dda5cebc92a0f095ae33
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2025-04-09 17:52:21 -0700

    [a11y] Fire kTextChanged events from SetValue for TextField and Combobox
    This CL fires missing events for when the value changes on textfields
    and selectable elements.
    
    See the bug linked below for details on user impact.
    
    Bug: 407791466
    Change-Id: Ie77ffccc987ae15111aaa829c1868656c256096e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6444533
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445044}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "ui/accessibility/accessibility_features.h"
 #include "ui/accessibility/ax_enums.mojom.h"
+#include "ui/accessibility/ax_role_properties.h"
 #include "ui/accessibility/platform/ax_platform.h"
 #include "ui/accessibility/platform/ax_platform_node.h"
 #include "ui/accessibility/platform/ax_platform_node_delegate.h"
@@ -1188,6 +1189,12 @@ void ViewAccessibility::SetValue(const std::string& value) {
   if (ready_to_notify_events_) {
     OnStringAttributeChanged(ax::mojom::StringAttribute::kValue, value);
     NotifyEvent(ax::mojom::Event::kValueChanged, true);
+
+    // Only fire a text changed event on text fields and select elements to
+    // mimic what is done in the web content.
+    if (data_.IsTextField() || ui::IsSelectElement(data_.role)) {
+      NotifyEvent(ax::mojom::Event::kTextChanged, true);
+    }
   }
 
   NotifyDataChanged();
