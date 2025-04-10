d6ce8e2a76d2d1cf593b51808259db6106c73f59
Lei Zhang <thestig@chromium.org>
2025-04-09 23:38:36 -0700

    Minimize includes in ui/gfx/ipc/color/gfx_param_traits*.h
    Remove unused / unnecessary header and then do IWYU to fix the build.
    
    Bug: 40318405
    Change-Id: I83f5ce5a2680ac61a0e26301d36d4bddc665e6da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6409173
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Mark Yacoub <markyacoub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445118}

--- ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
+++ ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "base/not_fatal_until.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
