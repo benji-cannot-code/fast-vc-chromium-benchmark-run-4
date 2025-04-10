d25223dd757143cb76f5083047ef977fdf9f671c
Zhe Li <zheliooo@google.com>
2025-04-10 10:31:42 -0700

    Add NullMarked to chrome/browser/touch_to_fill/password_manager/android:public_java
    Bug: 389129271
    Change-Id: I7a3288489e339978b4465c8b84ea881eb73bcbc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6445211
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445371}

--- chrome/browser/touch_to_fill/password_manager/android/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillComponent.java
+++ chrome/browser/touch_to_fill/password_manager/android/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillComponent.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.touch_to_fill;
 
 import android.content.Context;
 
+import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.touch_to_fill.common.BottomSheetFocusHelper;
 import org.chromium.chrome.browser.touch_to_fill.data.Credential;
@@ -20,6 +21,7 @@ import java.util.List;
  * This component allows to fill credentials into a form. It suppresses the keyboard until dismissed
  * and acts as a safe surface to fill credentials from.
  */
+@NullMarked
 public interface TouchToFillComponent {
     /**
      * This delegate is called when the TouchToFill component is interacted with (e.g. dismissed or
