fd649aa79c935108e9f7edb08d46c634607dcdf5
Mike West <mkwst@chromium.org>
2025-04-10 13:46:04 -0700

    [ReduceScreenSize] Reduce `Screen.isExtended`.
    If we're reducing access to the size of the screen, we should also
    reduce access to the fact that additional screens might exist.
    
    Bug: 408932088
    Change-Id: I796ee968361fe0f4264c790f2309eecee72851c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441446
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445488}

--- third_party/blink/renderer/core/frame/screen.cc
+++ third_party/blink/renderer/core/frame/screen.cc
@@ -197,8 +197,9 @@ ExecutionContext* Screen::GetExecutionContext() const {
 }
 
 bool Screen::isExtended() const {
-  if (!DomWindow())
+  if (!DomWindow() || ShouldReduceScreenSize(DomWindow())) {
     return false;
+  }
   auto* context = GetExecutionContext();
   if (!context->IsFeatureEnabled(
           network::mojom::PermissionsPolicyFeature::kWindowManagement)) {
