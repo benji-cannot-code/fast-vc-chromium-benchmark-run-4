23e29fe26f3829214269a14f852b9c4bd30a8efe
Jinsuk Kim <jinsukkim@chromium.org>
2025-04-11 15:21:32 -0700

    [mtb-cct] Remove 'Open in Browser' from toolbar shortcut
    This reverts crrev.com/c/6307283 partially to not show this CCT-only
    option in settings UI.
    
    Bug: 397498287
    Change-Id: Ia6dd4652fceb695eea7c56ce2afbbd679ed481cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448195
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446106}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/settings/RadioButtonGroupAdaptiveToolbarPreference.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/settings/RadioButtonGroupAdaptiveToolbarPreference.java
@@ -18,7 +18,6 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
 
 import org.chromium.base.metrics.RecordUserAction;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.toolbar.R;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarButtonVariant;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarStatePredictor;
@@ -39,7 +38,6 @@ public class RadioButtonGroupAdaptiveToolbarPreference extends Preference
     private @NonNull RadioButtonWithDescription mAddToBookmarksButton;
     private @NonNull RadioButtonWithDescription mReadAloudButton;
     private @NonNull RadioButtonWithDescription mPageSummaryButton;
-    private @NonNull RadioButtonWithDescription mOpenInBrowserButton;
     private @AdaptiveToolbarButtonVariant int mSelected;
     private @AdaptiveToolbarButtonVariant int mAutoButtonCaption;
     private @Nullable AdaptiveToolbarStatePredictor mStatePredictor;
@@ -78,9 +76,6 @@ public class RadioButtonGroupAdaptiveToolbarPreference extends Preference
                 (RadioButtonWithDescription) holder.findViewById(R.id.adaptive_option_read_aloud);
         mPageSummaryButton =
                 (RadioButtonWithDescription) holder.findViewById(R.id.adaptive_option_page_summary);
-        mOpenInBrowserButton =
-                (RadioButtonWithDescription)
-                        holder.findViewById(R.id.adaptive_option_open_in_browser);
 
         mInitRadioButtonRunnable.run();
         RecordUserAction.record("Mobile.AdaptiveToolbarButton.SettingsPage.Opened");
@@ -152,7 +147,6 @@ public class RadioButtonGroupAdaptiveToolbarPreference extends Preference
         updateVoiceButtonVisibility();
         updateReadAloudButtonVisibility();
         updatePageSummaryButtonVisibility();
-        updateOpenInBrowserButtonVisibility();
         mButtonsInitialized = true;
     }
 
@@ -175,8 +169,6 @@ public class RadioButtonGroupAdaptiveToolbarPreference extends Preference
             mSelected = AdaptiveToolbarButtonVariant.READ_ALOUD;
         } else if (mPageSummaryButton.isChecked()) {
             mSelected = AdaptiveToolbarButtonVariant.PAGE_SUMMARY;
-        } else if (mOpenInBrowserButton.isChecked()) {
-            mSelected = AdaptiveToolbarButtonVariant.OPEN_IN_BROWSER;
         } else {
             assert false : "No matching setting found.";
         }
@@ -217,8 +209,6 @@ public class RadioButtonGroupAdaptiveToolbarPreference extends Preference
                 return mReadAloudButton;
             case AdaptiveToolbarButtonVariant.PAGE_SUMMARY:
                 return mPageSummaryButton;
-            case AdaptiveToolbarButtonVariant.OPEN_IN_BROWSER:
-                return mOpenInBrowserButton;
         }
         return null;
     }
@@ -281,11 +271,6 @@ public class RadioButtonGroupAdaptiveToolbarPreference extends Preference
 
     private void updatePageSummaryButtonVisibility() {
         updateButtonVisibility(mPageSummaryButton, mCanUsePageSummary);
-    }
-
-    private void updateOpenInBrowserButtonVisibility() {
-        boolean show = ChromeFeatureList.sCctAdaptiveButtonEnableOpenInBrowser.getValue();
-        updateButtonVisibility(mOpenInBrowserButton, show);
     }
 
     /**
