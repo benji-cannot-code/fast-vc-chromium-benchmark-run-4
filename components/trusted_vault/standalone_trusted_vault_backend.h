53942992f0e8b8fd61639c03d0ce5fdf90797f59
Thomas Thrainer <thomasth@google.com>
2025-04-11 02:11:34 -0700

    Remove StandaloneTrustedVaultClient::DebugObserver.
    It is unused and `OnBackendStateChanged()` isn't called consistently
    currently because of the introduction of
    StandaloneTrustedVaultStorage.
    
    Bug: b:398160323
    Change-Id: If06f593b66a6dbe54d538faf0f6b43f70341662c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415689
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Thomas Thrainer <thomasth@google.com>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445746}

--- components/trusted_vault/standalone_trusted_vault_backend.h
+++ components/trusted_vault/standalone_trusted_vault_backend.h
@@ -53,8 +53,6 @@ class StandaloneTrustedVaultBackend
     Delegate& operator=(const Delegate&) = delete;
 
     virtual void NotifyRecoverabilityDegradedChanged() = 0;
-    // Called whenever persisted state changes.
-    virtual void NotifyStateChanged() = 0;
   };
 
   class LocalRecoveryFactorsFactory {
@@ -234,8 +232,6 @@ class StandaloneTrustedVaultBackend
   // Removes all data for non-primary accounts if they were previously marked
   // for deletion due to accounts in cookie jar changes.
   void RemoveNonPrimaryAccountKeysIfMarkedForDeletion();
-
-  void WriteDataToDiskAndNotify();
 
   const SecurityDomainId security_domain_id_;
 
--- components/trusted_vault/standalone_trusted_vault_backend_unittest.cc
+++ components/trusted_vault/standalone_trusted_vault_backend_unittest.cc
@@ -107,7 +107,6 @@ class MockDelegate : public StandaloneTrustedVaultBackend::Delegate {
   MockDelegate() = default;
   ~MockDelegate() override = default;
   MOCK_METHOD(void, NotifyRecoverabilityDegradedChanged, (), (override));
-  MOCK_METHOD(void, NotifyStateChanged, (), (override));
 };
 
 class FakeLocalRecoveryFactor : public LocalRecoveryFactor {
