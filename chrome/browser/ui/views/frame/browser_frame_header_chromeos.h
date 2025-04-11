48dd6bf9018d7457400ad900f16837c8edbeef4c
Mitsuru Oshima <oshima@chromium.org>
2025-04-11 14:04:49 -0700

    Remove unused method
    Bug: None
    
    Change-Id: Ib675dee9367eccc4b3e2fb2b77d12b39da185ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433842
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446077}

--- chrome/browser/ui/views/frame/browser_frame_header_chromeos.h
+++ chrome/browser/ui/views/frame/browser_frame_header_chromeos.h
@@ -42,7 +42,6 @@ class BrowserFrameHeaderChromeOS : public chromeos::FrameHeader {
 
   // FrameHeader:
   void UpdateFrameColors() override;
-  SkPath GetWindowMaskForFrameHeader(const gfx::Size& size) override;
 
  protected:
   // FrameHeader:
