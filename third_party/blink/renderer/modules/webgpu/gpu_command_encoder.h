d8504762ab6b46058509baa514eaff935b17122c
François Beaufort <beaufort.francois@gmail.com>
2025-04-10 23:25:32 -0700

    WebGPU: Enable copyBufferToBuffer overload by default
    Intent to ship: https://groups.google.com/a/chromium.org/g/blink-dev/c/rjtrmhywpf0
    
    Bug: 405883444
    Change-Id: If8ef8789b4155c211b4fb05db104265f35ba8731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6419165
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1445688}

--- third_party/blink/renderer/modules/webgpu/gpu_command_encoder.h
+++ third_party/blink/renderer/modules/webgpu/gpu_command_encoder.h
@@ -48,7 +48,7 @@ class GPUCommandEncoder : public DawnObject<wgpu::CommandEncoder> {
   void copyBufferToBuffer(DawnObject<wgpu::Buffer>* source,
                           DawnObject<wgpu::Buffer>* destination,
                           ExceptionState& exception_state) {
-    if (!RuntimeEnabledFeatures::WebGPUExperimentalFeaturesEnabled()) {
+    if (!RuntimeEnabledFeatures::WebGPUCopyBufferToBufferOverloadEnabled()) {
       exception_state.ThrowTypeError("Offsets and size are required.");
       return;
     }
@@ -58,7 +58,7 @@ class GPUCommandEncoder : public DawnObject<wgpu::CommandEncoder> {
                           DawnObject<wgpu::Buffer>* destination,
                           uint64_t size,
                           ExceptionState& exception_state) {
-    if (!RuntimeEnabledFeatures::WebGPUExperimentalFeaturesEnabled()) {
+    if (!RuntimeEnabledFeatures::WebGPUCopyBufferToBufferOverloadEnabled()) {
       exception_state.ThrowTypeError("Offsets are required.");
       return;
     }
@@ -69,7 +69,7 @@ class GPUCommandEncoder : public DawnObject<wgpu::CommandEncoder> {
                           DawnObject<wgpu::Buffer>* destination,
                           uint64_t destination_offset,
                           ExceptionState& exception_state) {
-    if (!RuntimeEnabledFeatures::WebGPUExperimentalFeaturesEnabled()) {
+    if (!RuntimeEnabledFeatures::WebGPUCopyBufferToBufferOverloadEnabled()) {
       exception_state.ThrowTypeError("Size is required.");
       return;
     }
