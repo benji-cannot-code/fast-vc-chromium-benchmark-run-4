1ce14c565c8e541343abac6391a8349c42ee78ed
Clark DuVall <cduvall@chromium.org>
2025-04-10 17:09:37 -0700

    Remove SupportsUserData from AIContextBoundObjectSet
    This is not used, cleanup.
    
    Change-Id: I40ae102c8e172cd2ad3f9a31f144b3a1aa66209e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448076
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445597}

--- chrome/browser/ai/ai_context_bound_object_set.h
+++ chrome/browser/ai/ai_context_bound_object_set.h
@@ -9,24 +9,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <variant>
 
 #include "base/containers/unique_ptr_adapters.h"
-#include "base/supports_user_data.h"
 #include "chrome/browser/ai/ai_context_bound_object.h"
 
 // The data structure that supports adding and removing `AIContextBoundObject`.
-class AIContextBoundObjectSet : public base::SupportsUserData::Data {
+class AIContextBoundObjectSet {
  public:
   AIContextBoundObjectSet();
   AIContextBoundObjectSet(const AIContextBoundObjectSet&) = delete;
   AIContextBoundObjectSet& operator=(const AIContextBoundObjectSet&) = delete;
-  ~AIContextBoundObjectSet() override;
+  ~AIContextBoundObjectSet();
 
   // Add an `AIContextBoundObject` into the set.
   void AddContextBoundObject(std::unique_ptr<AIContextBoundObject> object);
   // Returns the size of user data set for testing purpose.
   size_t GetSizeForTesting();
-
-  static AIContextBoundObjectSet* GetFromContext(
-      base::SupportsUserData& context_user_data);
 
   // Remove the `AIContextBoundObject` from the set.
   void RemoveContextBoundObject(AIContextBoundObject* object);
