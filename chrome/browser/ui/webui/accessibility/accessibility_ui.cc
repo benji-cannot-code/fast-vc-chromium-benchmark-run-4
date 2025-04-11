40fc49526a91a5fe1fec17d4c1ce33285840bd01
Aaron Leventhal <aleventhal@google.com>
2025-04-11 08:07:08 -0700

    Reland "[A11yPerformance] Additional cleanups from AXMode refactorings"
    This is a reland of commit c8adeceeff6ac0cbdee2d961e11a59c9e8989743
    
    Reason for revert:
    Bug:409833861
    
    We can reland the test change rename at a later time once we isolate
    and fix the Android Auto failures.
    
    Original change's description:
    > [A11yPerformance] Additional cleanups from AXMode refactorings
    >
    > R=grt@chromium.org
    >
    > Bug: none
    > Change-Id: Ie116bf5cf296a4c4d0059f64889a94b0e3f9cd56
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6444371
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1445316}
    
    Bug: none
    Change-Id: I037a8e60ee7ca970b0998aebffdc625e1b121a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447501
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445885}

--- chrome/browser/ui/webui/accessibility/accessibility_ui.cc
+++ chrome/browser/ui/webui/accessibility/accessibility_ui.cc
@@ -110,10 +110,6 @@ static const char kShowOrRefreshTree[] = "showOrRefreshTree";
 static const char kText[] = "text";
 static const char kWeb[] = "web";
 
-// Possible global flag values
-static const char kOff[] = "off";
-static const char kOn[] = "on";
-
 using ui::AXPropertyFilter;
 
 namespace {
@@ -204,17 +200,17 @@ void HandleAccessibilityRequestCallback(
 
   // The "native" and "web" flags are disabled if
   // --disable-renderer-accessibility is set.
-  data.Set(kNative, native ? kOn : kOff);
-  data.Set(kWeb, web ? kOn : kOff);
+  data.Set(kNative, native);
+  data.Set(kWeb, web);
 
   // The "text", "extendedProperties" and "html" flags are only
   // meaningful if "web" is enabled.
-  data.Set(kText, text ? kOn : kOff);
-  data.Set(kExtendedProperties, extendedProperties ? kOn : kOff);
-  data.Set(kHTML, html ? kOn : kOff);
+  data.Set(kText, text);
+  data.Set(kExtendedProperties, extendedProperties);
+  data.Set(kHTML, html);
 
   // The "pdfPrinting" flag is independent of the others.
-  data.Set(kPDFPrinting, pdf_printing ? kOn : kOff);
+  data.Set(kPDFPrinting, pdf_printing);
 
   std::string pref_api_type =
       pref->GetString(prefs::kShownAccessibilityApiType);
@@ -242,7 +238,7 @@ void HandleAccessibilityRequestCallback(
 
   bool is_mode_locked = !content::BrowserAccessibilityState::GetInstance()
                              ->IsAXModeChangeAllowed();
-  data.Set(kLocked, is_mode_locked ? kOn : kOff);
+  data.Set(kLocked, is_mode_locked);
 
   base::Value::List page_list;
   std::unique_ptr<content::RenderWidgetHostIterator> widget_iter(
