814464f7e92cd66369f7e9f6acc8e62ef16939c3
Glenn Hartmann <hartmanng@chromium.org>
2025-04-11 07:30:02 -0700

    Grit: support gendered translations in xtb_reader.py.
    Note that this does not actually add full gender support
    to grit, this is just the first piece.
    
    BUG=409770185
    
    Change-Id: Ic2ae068189ba389988dfffdd1c2223671ee9913c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6443419
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445852}

--- tools/grit/grit/clique.py
+++ tools/grit/grit/clique.py
@@ -16,6 +16,7 @@ from grit import lazy_re
 from grit import pseudo
 from grit import pseudolocales
 from grit import tclib
+from grit import xtb_reader
 
 
 class UberClique:
@@ -220,7 +221,8 @@ class UberClique:
       original_msg = self.BestClique(id).GetMessage()
 
       translation = tclib.Translation(id=id)
-      for is_ph,text in structure:
+      # TODO(crbug.com/409770185): Support gendered translations.
+      for is_ph, text in structure[xtb_reader.DEFAULT_GENDER]:
         if not is_ph:
           translation.AppendText(text)
         else:
