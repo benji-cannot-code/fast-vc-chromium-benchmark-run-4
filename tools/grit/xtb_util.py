939b510057cd6e02bb4484fc4255d86d2d3f078d
Glenn Hartmann <hartmanng@chromium.org>
2025-04-11 07:31:13 -0700

    Add a tool to parse and dump .xtb files.
    Mainly useful for debugging xtb parsing.
    
    This is quite a simple tool - on the one hand, it seems a bit silly to
    add a new tool just for this. But on the other hand, this functionality
    doesn't really seem to fit in well with any of the other existing tools.
    
    Bug: 409770185
    Change-Id: If924eecf1a74c6c6023931961517ffe2ea5e72b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447457
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445854}

---
+++ tools/grit/xtb_util.py
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Tool for interacting with .xtb files.
+
+Currently the only functionality in this tool is to parse and dump the given
+.xtb file to the console (useful for debugging .xtb parsing).
+"""
+
+import sys
+
+from grit import xtb_reader
+
+
+def main():
+  if len(sys.argv) != 2:
+    print(f'Usage: {sys.argv[0]} <xtb_file>')
+    sys.exit(1)
+
+  with open(sys.argv[1], 'rb') as f:
+    xtb_reader.Parse(f, print)
+
+
+if __name__ == "__main__":
+  main()
