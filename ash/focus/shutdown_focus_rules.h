3614d7a60c318e080887877ef7bc454d236bc4f1
Xiyuan Xia <xiyuan@chromium.org>
2025-04-10 18:29:25 -0700

    ash: Fix crash by activating a window during shutdown
    An activated/focused window during shutdown could crash in
    `Shell::CloseAllRootWindowChildWindows` if the window is in
    a container destroyed after desks containers. When it is
    activate and being destroyed, `FocusController` needs to
    find the next activatable window. This would access the
    already destroyed desk containers and causes UaF crashes.
    
    There is existing code to reset activated/focused window in
    `Shell` destructor. However, that is not enough because some
    code between the focus reset and `CloseAllRootWindowChildWindows`
    could activate or focus a new window. This CL adds a special
    `ShutdownFocusRules` to `FocusController` so that no window is
    activated or focused during shutdown.
    
    Bug: 369135212
    Change-Id: I3ac07b8231f2aba290ac000c55632f0ad73da39b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447534
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445624}

---
+++ ash/focus/shutdown_focus_rules.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_FOCUS_SHUTDOWN_FOCUS_RULES_H_
+#define ASH_FOCUS_SHUTDOWN_FOCUS_RULES_H_
+
+#include "ash/ash_export.h"
+#include "ash/focus/ash_focus_rules.h"
+
+namespace ash {
+
+// Focus rules used during shutdown. Nothing is activatable or focusable.
+class ASH_EXPORT ShutdownFocusRules : public AshFocusRules {
+ public:
+  ShutdownFocusRules();
+
+  ShutdownFocusRules(const ShutdownFocusRules&) = delete;
+  ShutdownFocusRules& operator=(const ShutdownFocusRules&) = delete;
+
+  ~ShutdownFocusRules() override;
+
+  // AshFocusRules:
+  bool SupportsChildActivation(const aura::Window* window) const override;
+  bool CanActivateWindow(const aura::Window* window) const override;
+  bool CanFocusWindow(const aura::Window* window,
+                      const ui::Event* event) const override;
+  aura::Window* GetNextActivatableWindow(aura::Window* ignore) const override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_FOCUS_SHUTDOWN_FOCUS_RULES_H_
