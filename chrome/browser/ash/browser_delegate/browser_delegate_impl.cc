581e1e1fbbfce79d7cb15ae26a8793e0a4202916
Georg Neis <neis@chromium.org>
2025-04-10 19:15:59 -0700

    ash: Extend BrowserController and BrowserDelegate
    - Add two methods for inspection:
      GetActiveWebContents, GetNativeWindow
    - Add one action:
      Show
    - Add utility functions for incremental development.
    
    Bug: b:369689187, b:375070223
    Change-Id: I88dac4c580a8be49c8d94cab85f55a1b52787897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6442957
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445643}

--- chrome/browser/ash/browser_delegate/browser_delegate_impl.cc
+++ chrome/browser/ash/browser_delegate/browser_delegate_impl.cc
@@ -5,17 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/browser_delegate/browser_delegate_impl.h"
 
+#include "base/check_deref.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 
 namespace ash {
 
 BrowserDelegateImpl::BrowserDelegateImpl(Browser* browser)
-    : browser_(browser) {}
+    : browser_(CHECK_DEREF(browser)) {}
 
 BrowserDelegateImpl::~BrowserDelegateImpl() = default;
 
+Browser& BrowserDelegateImpl::GetBrowser() const {
+  return browser_.get();
+}
+
 SessionID BrowserDelegateImpl::GetSessionID() const {
   return browser_->session_id();
+}
+
+content::WebContents* BrowserDelegateImpl::GetActiveWebContents() const {
+  return browser_->tab_strip_model()->GetActiveWebContents();
+}
+
+aura::Window* BrowserDelegateImpl::GetNativeWindow() const {
+  return browser_->window()->GetNativeWindow();
+}
+
+void BrowserDelegateImpl::Show() {
+  return browser_->window()->Show();
 }
 
 }  // namespace ash
