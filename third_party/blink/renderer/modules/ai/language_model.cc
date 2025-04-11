78ea8165a472828c02983cf9584cf942ff3e2785
Brad Triebwasser <btriebw@chromium.org>
2025-04-11 11:43:20 -0700

    Add WebFeatures and UseCounter for tracking image & audio multi-modaliy usage.
    Bug: 402828390
    Change-Id: I89d5a8402377346b645af708dd0a6c35d0b605ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6443721
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446008}

--- third_party/blink/renderer/modules/ai/language_model.cc
+++ third_party/blink/renderer/modules/ai/language_model.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/fileapi/blob.h"
 #include "third_party/blink/renderer/core/fileapi/file_reader_client.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/modules/ai/ai.h"
 #include "third_party/blink/renderer/modules/ai/ai_context_observer.h"
 #include "third_party/blink/renderer/modules/ai/ai_metrics.h"
@@ -278,6 +279,8 @@ ConvertPromptToMojoContent(V8LanguageModelPromptType content_type,
     case V8LanguageModelPromptType::Enum::kText:
       return ToMojo(content->GetAsString());
     case V8LanguageModelPromptType::Enum::kImage:
+      UseCounter::Count(execution_context,
+                        WebFeature::kLanguageModel_Prompt_Input_Image);
       if (content->IsV8ImageBitmapSource()) {
         return ToMojo(content->GetAsV8ImageBitmapSource(), script_state,
                       exception_state);
@@ -285,6 +288,8 @@ ConvertPromptToMojoContent(V8LanguageModelPromptType content_type,
       return base::unexpected(MakeGarbageCollected<DOMException>(
           DOMExceptionCode::kSyntaxError, "Unsupported image content type"));
     case V8LanguageModelPromptType::Enum::kAudio:
+      UseCounter::Count(execution_context,
+                        WebFeature::kLanguageModel_Prompt_Input_Audio);
       switch (content->GetContentType()) {
         case V8LanguageModelPromptContent::ContentType::kAudioBuffer:
           return ToMojo(content->GetAsAudioBuffer());
