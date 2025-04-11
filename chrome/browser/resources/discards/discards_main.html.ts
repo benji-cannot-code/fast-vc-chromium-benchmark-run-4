61b16c20ef94a75975f4c8fca7813aab9444ed14
Kuo-Hsin Yang <vovoy@google.com>
2025-04-10 18:41:28 -0700

    [discards webui] Migrate to Lit, part 6 (last)
    - Convert .html files to .html.ts files
    - Remove discards visibility entry in third_party/polymer/v3_0/BUILD.gn
    
    Bug: 388544211
    Change-Id: Ie99e05887005642be5d65330b186115d03cff9a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6444811
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445629}

---
+++ chrome/browser/resources/discards/discards_main.html.ts
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {DiscardsMainElement} from './discards_main.js';
+
+export function getHtml(this: DiscardsMainElement) {
+  //clang-format off
+  return html`<!--_html_template_start_-->
+<cr-tabs .tabNames="${this.tabs}" .selected="${this.selected}"
+    @selected-changed="${this.onSelectedChanged_}">
+</cr-tabs>
+
+<cr-page-selector selected="${this.selected}">
+  <discards-tab></discards-tab>
+  <database-tab></database-tab>
+  <graph-tab></graph-tab>
+</cr-page-selector>
+<!--_html_template_end_-->`;
+  //clang-format on
+}
--- chrome/browser/resources/discards/discards_tab.html.ts
+++ chrome/browser/resources/discards/discards_tab.html.ts
@@ -1,5 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {DiscardsTabElement} from './discards_tab.js';
+
+export function getHtml(this: DiscardsTabElement) {
+  //clang-format off
+  return html`<!--_html_template_start_-->
 <div id="discards">
   <div is="action-link" @click="${this.discardUrgentNow_}">
     [Urgent discard a tab now]
@@ -178,3 +188,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </tbody>
   </table>
 </div>
+<!--_html_template_end_-->`;
+  //clang-format on
+}
