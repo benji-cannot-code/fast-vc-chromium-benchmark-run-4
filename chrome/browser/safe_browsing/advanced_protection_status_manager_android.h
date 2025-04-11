415dc43450a39f6c1b791e31fb185b9819631ccc
Peter Kotwicz <pkotwicz@google.com>
2025-04-11 12:13:08 -0700

    Enable strict-site-isolation when the OS requests advanced-protection
    BUG=395862563
    TEST=SiteIsolationPolicyBrowserTest.*
    
    Change-Id: If6e8038b4d5ac04faca52bf69aba0e2a89d3c68e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426144
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446021}

--- chrome/browser/safe_browsing/advanced_protection_status_manager_android.h
+++ chrome/browser/safe_browsing/advanced_protection_status_manager_android.h
@@ -23,6 +23,8 @@ class AdvancedProtectionStatusManagerAndroid
   AdvancedProtectionStatusManagerAndroid& operator=(
       const AdvancedProtectionStatusManagerAndroid&) = delete;
 
+  static bool QueryIsUnderAdvancedProtection();
+
   // AdvancedProtectionStatusManager:
   Type GetAdvancedProtectionType() const override;
   void SetAdvancedProtectionStatusForTesting(bool enrolled) override;
