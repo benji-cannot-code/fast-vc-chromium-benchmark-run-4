10e5d8f9462baf0219eab770abbce5ef45265d9f
Samuel Huang <huangs@chromium.org>
2025-04-11 10:59:48 -0700

    [MVT Customization] Support adding non-Tile Views to MostVisitedTilesLayout.
    In crrev.com/c/6356790 we prepared code (mostly tests) for the existence
    of non-Tile Views to MostVisitedTilesLayout, and replaced addView()
    usage with new function addTile() to allow tracking.
    
    This CL continues the work by providing means to add non-Tile Views.
    A consideration is that MostVisitedTilesLayout needs the total width of
    its contents to dynamically compute margins for centering. Therefore we
    require non-Tile Views to have a fix width, and that the caller provides
    the width (in dp) when adding a non-Tile View. These widths are then
    summed, and used in total width calculation. Details:
    * TilesLinearLayout:
      * Add `mNonTileViewsTotalWidthDp`, in dp instead of pixels to avoid
        accumulating rounding errors from pixel conversion.
      * Add addNonTileViewWithWidth().
      * Add getNonTileViewsTotalWidthPx().
      * Change other method's params to explicitly state unit (Px).
    * MostVisitedTilesLayout:
      * Rename `*Num` fields to `*Count`.
      * Add `mInitialChildCount` (in addition to `mInitialTileCount`).
      * Change edge margin formula to use getNonTileViewsTotalWidthPx().
        * The formula is duplicated. We're avoiding moving this to a helper
          function to reduce delta; will refactor in a separate follow-up.
    
    Bug: 400481269, 388782412
    Change-Id: I1de49ee818de6c5b4a29832a0843241e126ac857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447868
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445983}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/MostVisitedTilesLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/MostVisitedTilesLayout.java
@@ -19,9 +19,10 @@ import org.chromium.ui.base.DeviceFormFactor;
 /** The most visited tiles layout. */
 public class MostVisitedTilesLayout extends TilesLinearLayout {
 
-    private final int mTileViewWidth;
-    private Integer mInitialTileNum;
     private final boolean mIsTablet;
+    private final int mTileViewWidth;
+    private Integer mInitialTileCount;
+    private Integer mInitialChildCount;
     private final int mIntervalPaddingsTablet;
     private final int mEdgePaddingsTablet;
 
@@ -66,8 +67,9 @@ public class MostVisitedTilesLayout extends TilesLinearLayout {
     void updateEdgeMarginTablet(int totalWidth) {
         boolean isFullFilled =
                 totalWidth
-                                - mTileViewWidth * mInitialTileNum
-                                - mIntervalPaddingsTablet * (mInitialTileNum - 1)
+                                - mTileViewWidth * mInitialTileCount
+                                - getNonTileViewsTotalWidthPx()
+                                - mIntervalPaddingsTablet * (mInitialChildCount - 1)
                                 - 2 * mEdgePaddingsTablet
                         >= 0;
         if (!isFullFilled) {
@@ -78,19 +80,24 @@ public class MostVisitedTilesLayout extends TilesLinearLayout {
             return;
         }
 
-        int currentNum = getTileCount();
+        int tileCount = getTileCount();
+        int childCount = getChildCount();
         int edgeMargin =
                 (totalWidth
-                                - mTileViewWidth * currentNum
-                                - mIntervalPaddingsTablet * (currentNum - 1))
+                                - mTileViewWidth * tileCount
+                                - getNonTileViewsTotalWidthPx()
+                                - mIntervalPaddingsTablet * (childCount - 1))
                         / 2;
         setEdgeMargins(edgeMargin);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        if (mInitialTileNum == null) {
-            mInitialTileNum = getTileCount();
+        if (mInitialTileCount == null) {
+            mInitialTileCount = getTileCount();
+        }
+        if (mInitialChildCount == null) {
+            mInitialChildCount = getChildCount();
         }
         if (mIsTablet) {
             updateEdgeMarginTablet(widthMeasureSpec);
