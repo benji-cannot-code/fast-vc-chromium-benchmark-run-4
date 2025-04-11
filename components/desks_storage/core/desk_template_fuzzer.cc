57d568d4e9e46ac9e2510695546d8e5f1bb2d8ea
Irem Uguz <iremuguz@google.com>
2025-04-11 08:57:59 -0700

    Revert "gardener: Fix compile failure"
    This reverts commit 1371c4d22d33d208dae92b17994f03ecc66c3a75.
    
    Reason for revert: The failure didn't get fixed. https://ci.chromium.org/ui/p/chromium/builders/ci/Libfuzzer%20Upload%20Chrome%20OS%20ASan/121276/overview
    
    Original change's description:
    > gardener: Fix compile failure
    >
    > Fix the compilation failure in this builder:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Libfuzzer%20Upload%20Chrome%20OS%20ASan/121267/overview
    >
    > Bug: 410002275
    > Change-Id: Id8920ea8b21369a28dd957165f68bfe607095449
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449564
    > Owners-Override: Irem Uguz <iremuguz@google.com>
    > Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    > Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    > Auto-Submit: Irem Uguz <iremuguz@google.com>
    > Commit-Queue: Irem Uguz <iremuguz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1445903}
    
    Bug: 410002275
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I8daadb7f4d35203b18e6c445174105a0eae7b671
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450759
    Auto-Submit: Irem Uguz <iremuguz@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Irem Uguz <iremuguz@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445911}

--- components/desks_storage/core/desk_template_fuzzer.cc
+++ components/desks_storage/core/desk_template_fuzzer.cc
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string_view>
 
-#include "base/containers/span.h"
 #include "base/json/json_reader.h"
+#include "base/types/span.h"
 #include "components/account_id/account_id.h"
 #include "components/desks_storage/core/desk_template_conversion.h"
 #include "components/desks_storage/core/desk_template_util.h"
