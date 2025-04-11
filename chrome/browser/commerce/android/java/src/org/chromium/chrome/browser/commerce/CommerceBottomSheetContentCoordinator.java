17adf68bad1488a43b82cbc237f12ba72b53224c
Qi Bao <qib@google.com>
2025-04-11 12:02:48 -0700

    [Discount][Clank] Fix the recycler view view item overlapping issue.
    This CL also fix price tracking layout issue when sheet is half opened
    
    https://drive.google.com/file/d/13kNLJ5hQMjs2j16F7umJErnGvuVOetUy/view?usp=sharing&resourcekey=0-j_5qQsCETsEyz5ZC0bGXpA
    
    Change-Id: I303e3910720cf93876529667b5e31604cb22b3c0
    Bug: 408247117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6443324
    Commit-Queue: Qi Bao <qib@google.com>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446014}

--- chrome/browser/commerce/android/java/src/org/chromium/chrome/browser/commerce/CommerceBottomSheetContentCoordinator.java
+++ chrome/browser/commerce/android/java/src/org/chromium/chrome/browser/commerce/CommerceBottomSheetContentCoordinator.java
@@ -12,6 +12,7 @@ import android.os.Looper;
 import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
@@ -42,7 +43,7 @@ public class CommerceBottomSheetContentCoordinator implements CommerceBottomShee
 
     private List<CommerceBottomSheetContentProvider> mContentProviders = new ArrayList<>();
     private final CommerceBottomSheetContentMediator mMediator;
-    private RecyclerView mContenRecyclerView;
+    private RecyclerView mContentRecyclerView;
     private View mCommerceBottomSheetContentContainer;
     private ModelList mModelList;
     private @Nullable Long mSheetOpenTimeMs;
@@ -59,7 +60,15 @@ public class CommerceBottomSheetContentCoordinator implements CommerceBottomShee
         mModelList = new ModelList();
 
         mScrimManagerSupplier = scrimSupplier;
-        SimpleRecyclerViewAdapter adapter = new SimpleRecyclerViewAdapter(mModelList);
+        SimpleRecyclerViewAdapter adapter =
+                new SimpleRecyclerViewAdapter(mModelList) {
+                    @Override
+                    public void onViewRecycled(ViewHolder holder) {
+                        super.onViewRecycled(holder);
+                        ((ViewGroup) holder.itemView.findViewById(R.id.content_view_container))
+                                .removeAllViews();
+                    }
+                };
         adapter.registerType(
                 0,
                 new LayoutViewBuilder(R.layout.commerce_bottom_sheet_content_item_container),
@@ -69,11 +78,11 @@ public class CommerceBottomSheetContentCoordinator implements CommerceBottomShee
                 LayoutInflater.from(context)
                         .inflate(
                                 R.layout.commerce_bottom_sheet_content_container, /* root= */ null);
-        mContenRecyclerView =
+        mContentRecyclerView =
                 mCommerceBottomSheetContentContainer.findViewById(
                         R.id.commerce_content_recycler_view);
-        mContenRecyclerView.setAdapter(adapter);
-        mContenRecyclerView.addItemDecoration(
+        mContentRecyclerView.setAdapter(adapter);
+        mContentRecyclerView.addItemDecoration(
                 new ItemDecoration() {
                     @Override
                     public void getItemOffsets(
@@ -97,13 +106,13 @@ public class CommerceBottomSheetContentCoordinator implements CommerceBottomShee
                             mSheetOpenTimeMs = SystemClock.elapsedRealtime();
                         }
                         if (newState == SheetState.FULL) {
-                            mContenRecyclerView.suppressLayout(false);
+                            mContentRecyclerView.setNestedScrollingEnabled(true);
                             if (mScrimModel != null && !mMediator.isContentWrappingContent()) {
                                 mScrimModel = bottomSheetController.createScrimParams();
                                 mScrimManagerSupplier.get().showScrim(mScrimModel);
                             }
                         } else if (newState == SheetState.HALF) {
-                            mContenRecyclerView.suppressLayout(true);
+                            mContentRecyclerView.setNestedScrollingEnabled(false);
                         } else if (newState == SheetState.HIDDEN) {
                             if (mSheetOpenTimeMs != null) {
                                 Long durationMs = SystemClock.elapsedRealtime() - mSheetOpenTimeMs;
@@ -162,7 +171,7 @@ public class CommerceBottomSheetContentCoordinator implements CommerceBottomShee
     }
 
     public RecyclerView getRecyclerViewForTesting() {
-        return mContenRecyclerView;
+        return mContentRecyclerView;
     }
 
     public ModelList getModelListForTesting() {
