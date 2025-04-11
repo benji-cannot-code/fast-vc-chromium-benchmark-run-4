ff34ba21f5b28a7d33988867e3a68b83b93127bd
“Prasanaa” <prasanaa@google.com>
2025-04-11 07:58:05 -0700

    [iOS] Remove check for running search_wih egtests for devices on iOS15-
    Remove check for running egtest for search_with_mediator egtests for
    devices on iOS15-.
    
    Chromium does not support iOS15-, so the check will always result in
    YES.
    
    Fixed: 409816404
    Change-Id: Ib8951530f415e04d23440123a6627ef80dbca71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447101
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445875}

--- ios/chrome/browser/search_with/ui_bundled/search_with_mediator_egtest.mm
+++ ios/chrome/browser/search_with/ui_bundled/search_with_mediator_egtest.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-#import "base/ios/ios_util.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "ios/chrome/browser/browser_container/ui_bundled/edit_menu_app_interface.h"
@@ -180,10 +179,6 @@ void TriggerEditMenu() {
 }
 
 - (void)testSearchWith {
-  if (!base::ios::IsRunningOnIOS16OrLater()) {
-    // Feature disabled on iOS15-.
-    EARL_GREY_TEST_SKIPPED(@"Test disabled on iOS15-");
-  }
   [self loadPage];
   TriggerEditMenu();
   bool found = FindEditMenuAction(@"Search with test");
@@ -199,10 +194,6 @@ void TriggerEditMenu() {
 }
 
 - (void)testSearchWithIncognito {
-  if (!base::ios::IsRunningOnIOS16OrLater()) {
-    // Feature disabled on iOS15-.
-    EARL_GREY_TEST_SKIPPED(@"Test disabled on iOS15-");
-  }
   [ChromeEarlGrey openNewIncognitoTab];
   [self loadPage];
   TriggerEditMenu();
