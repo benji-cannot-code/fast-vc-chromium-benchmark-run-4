eab46f0d56291ee213bfb05f11d1d94bf04df19b
Eleanor Lee <eleanorlee@google.com>
2025-04-11 09:48:41 -0700

    [Surface Color] Match page info container color with sheet bg color
    Before: https://screencast.googleplex.com/cast/NjI2MTI2Nzk0NDcwMTk1MnxlZWRlNmY1Mi0wOQ
    
    After:
    https://screenshot.googleplex.com/4btqhJyzdbae2t2
    
    
    Bug: 409549506
    Change-Id: I3d62edcdb3b60f64400ca3f6960482c855336943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447355
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445936}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoDialog.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoDialog.java
@@ -23,10 +23,10 @@ import android.view.Window;
 import android.widget.LinearLayout;
 
 import androidx.annotation.GravityInt;
-import androidx.appcompat.content.res.AppCompatResources;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.FadingEdgeScrollView;
 import org.chromium.ui.interpolators.Interpolators;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
@@ -223,10 +223,7 @@ public class PageInfoDialog {
                             (int)
                                     context.getResources()
                                             .getDimension(R.dimen.page_info_popup_corners_radius);
-                    GradientDrawable background =
-                            (GradientDrawable)
-                                    AppCompatResources.getDrawable(
-                                            getContext(), R.drawable.page_info_bg);
+                    GradientDrawable background = new GradientDrawable();
                     float[] radii;
                     if (mDialogPosition == Gravity.TOP) {
                         radii =
@@ -257,6 +254,7 @@ public class PageInfoDialog {
                     }
 
                     background.setCornerRadii(radii);
+                    background.setColor(SemanticColorUtils.getColorSurface(getContext()));
                     setBackground(background);
                 }
             }
