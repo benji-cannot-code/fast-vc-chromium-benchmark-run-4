9d5261b95b3ae4433118f7c08d821a849d30d979
Kouhei Ueno <kouhei@chromium.org>
2025-04-11 16:06:54 -0700

    Implement WebDX UseCounter for Http3
    The UseCounter is recorded when the main resource was loaded over HTTP3.
    
    Change-Id: Ie20e06ce01e99cd82b7d3e9f308ac8cb82bd60e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6446854
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446127}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -381,7 +381,8 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
            {WebFeature::kSelectionGetComposedRanges,
             WebDXFeature::kComposedRanges},
            {WebFeature::kSharedDictionaryUsed,
-            WebDXFeature::kCompressionDictionaryTransport}}};
+            WebDXFeature::kCompressionDictionaryTransport},
+           {WebFeature::kHttp3, WebDXFeature::kHttp3}}};
 
   return *kMap;
 }
