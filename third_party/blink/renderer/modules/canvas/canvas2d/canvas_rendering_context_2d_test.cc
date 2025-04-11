73f2e7e22a64a9fe34ef57f4c766b92512644bc4
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-04-11 10:34:01 -0700

    Reuse code from CanvasRenderingContext2DTestAccelerated test fixture
    The test fixture CanvasRenderingContext2DTestAcceleratedMultipleDisables
    was inheriting from CanvasRenderingContext2DTest and was re-defining
    everything in CanvasRenderingContext2DTestAccelerated, needed to make
    the canvas accelerated. This code can be deduplicated by having one
    fixture inherit from the other. A follow-up CL will add code needed by
    accelerated fixtures, so the inheritance will help avoiding duplicated
    code there as well.
    
    Bug: 409576869
    Change-Id: I5f8b9b568aac7231bf9800d7404973dbdc6c6ee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448622
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445966}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
@@ -2902,10 +2902,8 @@ TEST_P(CanvasRenderingContext2DTestAccelerated,
 }
 
 class CanvasRenderingContext2DTestAcceleratedMultipleDisables
-    : public CanvasRenderingContext2DTest {
+    : public CanvasRenderingContext2DTestAccelerated {
  protected:
-  bool AllowsAcceleration() override { return true; }
-
   void CreateAlotOfCanvasesWithAccelerationExplicitlyDisabled() {
     for (int i = 0; i < 100; ++i) {
       auto* canvas = MakeGarbageCollected<HTMLCanvasElement>(GetDocument());
@@ -2918,9 +2916,6 @@ class CanvasRenderingContext2DTestAcceleratedMultipleDisables
       canvas->DisableAcceleration();
     }
   }
-
- private:
-  ScopedTestingPlatformSupport<GpuMemoryBufferTestPlatform> platform_;
 };
 
 INSTANTIATE_PAINT_TEST_SUITE_P(
