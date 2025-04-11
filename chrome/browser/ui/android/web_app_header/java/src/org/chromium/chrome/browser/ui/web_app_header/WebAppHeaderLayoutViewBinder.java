79a3b3b42e053da6320e296bcef9dd6c495324c2
Vlad Korotkevich <vkorotkevich@google.com>
2025-04-11 01:50:53 -0700

    [Minimal UI] Add web app header ui implementation
    Introducing a new UI WebAppHeaderLayout component that will
    contain browser related controls in the caption bar area. This CL
    contains root layout related logic such as setting paddings
    and listening for component's height.
    
    Bug: https://b.corp.google.com/issues/396100732
    Change-Id: I8fe90c95827be71e18b20bff1115fc56c62a5dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6322651
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Vlad Korotkevich <vkorotkevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445736}

--- chrome/browser/ui/android/web_app_header/java/src/org/chromium/chrome/browser/ui/web_app_header/WebAppHeaderLayoutViewBinder.java
+++ chrome/browser/ui/android/web_app_header/java/src/org/chromium/chrome/browser/ui/web_app_header/WebAppHeaderLayoutViewBinder.java
@@ -5,12 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ui.web_app_header;
 
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewGroup;
+
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
+/** Binder that reflects model updates on the {@link WebAppHeaderLayout}. */
 class WebAppHeaderLayoutViewBinder {
 
     private WebAppHeaderLayoutViewBinder() {}
 
-    public static void bind(PropertyModel model, WebAppHeaderLayout view, PropertyKey key) {}
+    /**
+     * Updates webapp header to reflect the state of a single model parameter.
+     *
+     * @param model the model that contains properties to reflect on the view.
+     * @param view a view to be updated with a model state.
+     * @param key a key that indicates what model property has changed.
+     */
+    public static void bind(PropertyModel model, ViewGroup view, PropertyKey key) {
+        if (key == WebAppHeaderLayoutProperties.PADDINGS) {
+            final Rect paddings = model.get(WebAppHeaderLayoutProperties.PADDINGS);
+            view.setPadding(paddings.left, paddings.top, paddings.right, paddings.bottom);
+        } else if (key == WebAppHeaderLayoutProperties.IS_VISIBLE) {
+            view.setVisibility(
+                    model.get(WebAppHeaderLayoutProperties.IS_VISIBLE) ? View.VISIBLE : View.GONE);
+        } else if (key == WebAppHeaderLayoutProperties.MIN_HEIGHT) {
+            view.setMinimumHeight(model.get(WebAppHeaderLayoutProperties.MIN_HEIGHT));
+        } else {
+            assert false : String.format("Unsupported property key %s", key.toString());
+        }
+    }
 }
