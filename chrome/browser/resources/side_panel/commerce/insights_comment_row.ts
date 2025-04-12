24d9ef07facd8c6f9b1dfb71489a9ccef06db96b
rbpotter <rbpotter@chromium.org>
2025-04-11 17:09:09 -0700

    Commerce side panel: Remove use of useDefineForClassFields: false
    Bug: 389737066
    Change-Id: I59e3e9ffa3013bb8c132ce8b1bc8519b50016d70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448109
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446146}

--- chrome/browser/resources/side_panel/commerce/insights_comment_row.ts
+++ chrome/browser/resources/side_panel/commerce/insights_comment_row.ts
@@ -30,7 +30,7 @@ export class InsightsCommentRow extends PolymerElement {
     };
   }
 
-  private shouldShowFeedback_: boolean;
+  declare private shouldShowFeedback_: boolean;
   private priceInsightsProxy_: PriceInsightsBrowserProxy =
       PriceInsightsBrowserProxyImpl.getInstance();
 
