0616fac118dbcda326a0c1610d5046d9dd161aa9
Noam Rosenthal <nrosenthal@chromium.org>
2025-04-14 06:06:40 -0700

    Support offset-path: shape()
    - Allow parsing shape() when consuming offset-path
    - Interpolate offset-path: shape()
    - Use the existing methods to get the path position
    - Changed the order of interpolation classes so that
      path is before shape() for offset-path.
    - Fixed a few WPTs that used an old version of the shape() syntax.
    
    This is behind a flag with a separate I2S because the original I2S
    only mentioned clip-path.
    
    Bug: 389713717
    Change-Id: I0d1aaf5ab0ac6b1eb09d3fbdc20187b92fce1e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441130
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446474}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -1686,12 +1686,6 @@ void ComputedStyle::ApplyMotionPathTransform(float origin_x,
         path_position = CalculatePointAndTangentOnPath(path.GetPath());
         break;
       }
-      case BasicShape::kStyleShapeType: {
-        const StyleShape& shape = To<StyleShape>(basic_shape);
-        path_position = CalculatePointAndTangentOnPath(
-            shape.GetPath(bounding_box, EffectiveZoom(), /*path_scale=*/1.f));
-        break;
-      }
       case BasicShape::kStyleRayType: {
         const gfx::RectF reference_box = GetReferenceBox(box, coord_box);
         const gfx::PointF offset_from_reference_box =
@@ -1725,7 +1719,8 @@ void ComputedStyle::ApplyMotionPathTransform(float origin_x,
       case BasicShape::kBasicShapeCircleType:
       case BasicShape::kBasicShapeEllipseType:
       case BasicShape::kBasicShapeInsetType:
-      case BasicShape::kBasicShapePolygonType: {
+      case BasicShape::kBasicShapePolygonType:
+      case BasicShape::kStyleShapeType: {
         const gfx::RectF reference_box = GetReferenceBox(box, coord_box);
         const gfx::PointF offset_from_reference_box =
             GetOffsetFromContainingBlock(box) -
