eb4dbe22ce3a627f8fde1f93d582444773133c83
Jagadesh P <jagadeshjai1999@gmail.com>
2025-04-14 11:57:06 -0700

    Remove stored SessionSyncService pointer to avoid dangling ptr issues
    Instead of storing the SessionSyncService pointer, get it directly from
    the factory when needed. This ensures we always have a valid service
    pointer during the test execution since the service is owned by the
    profile.
    
    Bug: 40212619
    Change-Id: Iee7c856da489dee21de27098297fc0a94d9174e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6452772
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446682}

--- chrome/browser/ui/tabs/recent_tabs_sub_menu_model_browsertest.cc
+++ chrome/browser/ui/tabs/recent_tabs_sub_menu_model_browsertest.cc
@@ -125,7 +125,7 @@ class RecentTabsSubMenuModelTest : public InProcessBrowserTest {
   void WaitForLoadFromLastSession() { content::RunAllTasksUntilIdle(); }
 
   virtual void Init() {
-    session_sync_service_ =
+    auto* session_sync_service =
         SessionSyncServiceFactory::GetForProfile(browser()->profile());
 
     syncer::DataTypeActivationRequest activation_request;
@@ -134,7 +134,7 @@ class RecentTabsSubMenuModelTest : public InProcessBrowserTest {
 
     std::unique_ptr<syncer::DataTypeActivationResponse> activation_response;
     base::RunLoop loop;
-    session_sync_service_->GetControllerDelegate()->OnSyncStarting(
+    session_sync_service->GetControllerDelegate()->OnSyncStarting(
         activation_request,
         base::BindLambdaForTesting(
             [&](std::unique_ptr<syncer::DataTypeActivationResponse> response) {
@@ -168,7 +168,9 @@ class RecentTabsSubMenuModelTest : public InProcessBrowserTest {
 
   void RegisterRecentTabs(RecentTabsBuilderTestHelper* helper) {
     helper->ExportToSessionSync(sync_processor_.get());
-    helper->VerifyExport(session_sync_service_->GetOpenTabsUIDelegate());
+    helper->VerifyExport(
+        SessionSyncServiceFactory::GetForProfile(browser()->profile())
+            ->GetOpenTabsUIDelegate());
   }
 
   void AddTabToBrowser(const GURL& tab_url) {
@@ -189,8 +191,6 @@ class RecentTabsSubMenuModelTest : public InProcessBrowserTest {
   }
 
  private:
-  raw_ptr<sync_sessions::SessionSyncService, DanglingUntriaged>
-      session_sync_service_;
   std::unique_ptr<syncer::DataTypeProcessor> sync_processor_;
 };
 
