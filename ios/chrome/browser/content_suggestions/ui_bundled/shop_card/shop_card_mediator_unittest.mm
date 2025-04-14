0596bd99115dce91dd29c5313e52e2997941a83e
David Maunder <davidjm@google.com>
2025-04-14 08:39:19 -0700

    Add 5th arm to ShopCard and impressions limits to arm 4
    A 5th arm is added to ShopCard which is regular Tab Resumption with
    the same impression limits applied to arm #3 and #4 (variations
    of Tab Resumption). This will ensure we can adequately benchmark
    the variations to Tab Resumption against Tab Resumption. Also
    adds impression limits to arm 4.
    
    Bug: 410050321
    Change-Id: I5f5ce16a4006f04d88e06f56a1c01f8822a45b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448647
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446551}

--- ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_mediator_unittest.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_mediator_unittest.mm
@@ -39,6 +39,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
 
+namespace {
+
+const char* kPrefsToRegister[] = {
+    shop_card_prefs::kShopCardPriceDropUrlImpressions,
+    tab_resumption_prefs::kTabResumptionRegularUrlImpressions,
+    tab_resumption_prefs::kTabResumptionWithPriceDropUrlImpressions,
+    tab_resumption_prefs::kTabResumptionWithPriceTrackableUrlImpressions,
+};
+
+}  // namespace
+
 class ShopCardMediatorTest : public PlatformTest {
  public:
   ShopCardMediatorTest() {
@@ -56,10 +67,9 @@ class ShopCardMediatorTest : public PlatformTest {
     builder.AddTestingFactory(ios::HistoryServiceFactory::GetInstance(),
                               ios::HistoryServiceFactory::GetDefaultFactory());
     bookmark_model_ = bookmarks::TestBookmarkClient::CreateModel();
-    pref_service_.registry()->RegisterDictionaryPref(
-        shop_card_prefs::kShopCardPriceDropUrlImpressions);
-    pref_service_.registry()->RegisterDictionaryPref(
-        tab_resumption_prefs::kTabResumptionWithPriceDropUrlImpressions);
+    for (auto* const pref : kPrefsToRegister) {
+      pref_service_.registry()->RegisterDictionaryPref(pref);
+    }
     builder.AddTestingFactory(
         ImpressionLimitServiceFactory::GetInstance(),
         base::BindRepeating(
--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -870,6 +870,9 @@ const FeatureEntry::FeatureParam kPriceDropOnTabArm[] = {
 const FeatureEntry::FeatureParam kPriceTrackableProductOnTabArm[] = {
     {"ShopCardVariant", "arm_4"},
 };
+const FeatureEntry::FeatureParam kTabResumptionWithImpressionLimitsArm[] = {
+    {"ShopCardVariant", "arm_5"},
+};
 const FeatureEntry::FeatureParam kPriceDropForTrackedProductsFront[] = {
     {"ShopCardVariant", "arm_1"},
     {"ShopCardPosition", "shop_card_front"},
@@ -884,6 +887,10 @@ const FeatureEntry::FeatureParam kPriceDropOnTabFront[] = {
 };
 const FeatureEntry::FeatureParam kPriceTrackableProductOnTabFront[] = {
     {"ShopCardVariant", "arm_4"},
+    {"ShopCardPosition", "shop_card_front"},
+};
+const FeatureEntry::FeatureParam kTabResumptionWithImpressionLimitsFront[] = {
+    {"ShopCardVariant", "arm_5"},
     {"ShopCardPosition", "shop_card_front"},
 };
 
@@ -975,6 +982,9 @@ const FeatureEntry::FeatureVariation kShopCardOverrideOptions[] = {
      std::size(kPriceDropOnTabArm), nullptr},
     {"Card 4 Price Trackable on Tab Resumption", kPriceTrackableProductOnTabArm,
      std::size(kPriceTrackableProductOnTabArm), nullptr},
+    {"Card 5 Tab Resumption with Impression Limits",
+     kTabResumptionWithImpressionLimitsArm,
+     std::size(kTabResumptionWithImpressionLimitsArm), nullptr},
     {"Card 1 Price Drop at front of magic stack",
      kPriceDropForTrackedProductsFront,
      std::size(kPriceDropForTrackedProductsFront), nullptr},
@@ -985,6 +995,9 @@ const FeatureEntry::FeatureVariation kShopCardOverrideOptions[] = {
     {"Card 4 Price Trackable on Tab Resumption at front of magic stack",
      kPriceTrackableProductOnTabFront,
      std::size(kPriceTrackableProductOnTabFront), nullptr},
+    {"Card 5 Tab Resumption with Impression Limits at front of magic stack",
+     kTabResumptionWithImpressionLimitsFront,
+     std::size(kTabResumptionWithImpressionLimitsFront), nullptr},
 };
 
 const FeatureEntry::FeatureVariation kEphemeralCardRankerCardOverrideOptions[] =
