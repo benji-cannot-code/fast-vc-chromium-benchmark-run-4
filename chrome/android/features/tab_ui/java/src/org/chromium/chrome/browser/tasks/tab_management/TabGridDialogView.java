03a845d62665cb8f86eec49b37a93d8b910de233
Calder Kitagawa <ckitagawa@chromium.org>
2025-04-14 11:08:43 -0700

    [Tab Switcher] Speculative fix for missing tab group dialog
    Speculative fix for a case where the tab group dialog thinks it is
    visible indefinitely and won't try to show again resulting in blocked
    touch inputs on the tab switcher among other problems.
    
    Theory:
    1) BINDING_TOKEN is unchanged.
    2) IS_DIALOG_VISIBLE was already true (somehow) yet the dialog is GONE.
    
    If this combination happened prior to this CL the dialog would be
    treated as "visible" for most purposes, but would not actually be
    VISIBLE and interactable. The fix proposed in this CL is to just ensure
    that setting IS_DIALOG_VISIBLE if unchanged does at least attempt to
    run the showing logic with the caveat that it will noop if already
    visible.
    
    In theory, this should fix the bug the next time a tab group attempts
    to show.
    
    Unfortunately, it is unclear to me how we can end up in a GONE
    visibility while being visible in the first place.
    
    Bug: 409985953
    Change-Id: Ib4caf65f69220b86aa27f4655638d41773d3a7a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6451603
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446655}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
@@ -153,6 +153,7 @@ public class TabGridDialogView extends FrameLayout {
         mUngroupBarHoveredBackgroundColor =
                 TabUiThemeProvider.getTabGridDialogUngroupBarHoveredBackgroundColor(
                         mContext, false);
+        setVisibility(GONE);
     }
 
     void forceAnimationToFinish() {
@@ -194,8 +195,6 @@ public class TabGridDialogView extends FrameLayout {
                 };
         mParent.getViewTreeObserver().addOnGlobalLayoutListener(mParentGlobalLayoutListener);
         updateDialogWithOrientation(mOrientation);
-        setVisibility(GONE);
-        notifyVisibilityListenerOnHide();
     }
 
     @Override
@@ -1009,26 +1008,30 @@ public class TabGridDialogView extends FrameLayout {
 
     /** Show {@link PopupWindow} for dialog with animation. */
     void showDialog() {
+        if (getVisibility() == VISIBLE) return;
+
         if (mCurrentDialogAnimator != null && mCurrentDialogAnimator != mShowDialogAnimation) {
             mCurrentDialogAnimator.end();
         }
         mCurrentDialogAnimator = mShowDialogAnimation;
+
         assert mScrimManager != null && mScrimPropertyModel != null;
         mScrimManager.showScrim(mScrimPropertyModel);
+
         setVisibility(View.VISIBLE);
         mShowDialogAnimation.start();
     }
 
     /** Hide {@link PopupWindow} for dialog with animation. */
     void hideDialog() {
-        // Skip the hideDialog call caused by initializing the dialog visibility as false.
         if (getVisibility() != VISIBLE) return;
 
-        assert mScrimManager != null && mScrimPropertyModel != null;
         if (mCurrentDialogAnimator != null && mCurrentDialogAnimator != mHideDialogAnimation) {
             mCurrentDialogAnimator.end();
         }
         mCurrentDialogAnimator = mHideDialogAnimation;
+
+        assert mScrimManager != null && mScrimPropertyModel != null;
         if (mScrimManager.isShowingScrim()) {
             if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(mContext)) {
                 mScrimManager.hideScrim(
