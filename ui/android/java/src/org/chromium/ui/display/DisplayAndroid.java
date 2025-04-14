c84863248abff37586cf02f0467b94ab7dbd1699
Bartosz Chominski <chominskib@google.com>
2025-04-14 05:05:23 -0700

    Add comments about display ID in DisplayAndroid
    Stemming from a comment in crrev.com/c/6441543.
    
    This ID divergence was introduced in
    https://codereview.chromium.org/2523273002 but then VirtualDisplays
    for which this divergence occurred were removed in
    crrev.com/c/4114165.
    
    Change-Id: If2e20a5dcc0a83b606905d235bc0ad5dea337fac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448437
    Reviewed-by: Dominik Bylica <bylica@google.com>
    Commit-Queue: Bartosz Chomiński <chominskib@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446457}

--- ui/android/java/src/org/chromium/ui/display/DisplayAndroid.java
+++ ui/android/java/src/org/chromium/ui/display/DisplayAndroid.java
@@ -153,9 +153,7 @@ public class DisplayAndroid {
         return getManager().getDisplayAndroid(display);
     }
 
-    /**
-     * Returns the display id that does not necessarily match the one defined in Android's Display.
-     */
+    /** Returns the display ID that matches the one defined in Android's Display. */
     public int getDisplayId() {
         return mDisplayId;
     }
@@ -318,6 +316,11 @@ public class DisplayAndroid {
         mObservers.remove(observer);
     }
 
+    /**
+     * Constructs an instance.
+     *
+     * @param displayId The display ID of Android's Display represented by this object.
+     */
     protected DisplayAndroid(int displayId) {
         mDisplayId = displayId;
         mObservers = new WeakHashMap<>();
