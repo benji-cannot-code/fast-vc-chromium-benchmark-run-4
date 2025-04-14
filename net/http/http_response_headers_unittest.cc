ae4ade51d23773d34c3e5f1c1fd03978ef54ddd9
Etienne Dechamps <edechamps@google.com>
2025-04-14 08:56:10 -0700

    Enable base tracing in Cronet
    This is not expected to do anything in and of itself, as Cronet does not
    initialize Perfetto (yet). This is just one step on the path to make
    Perfetto track events show up in Cronet.
    
    Note this CL makes Cronet ~304 KiB bigger, as measured on ARM64 Release
    builds. This size increase is expected and has been pre-approved.
    
    This also involves an extra ApkInfo call during Cronet initialization, as
    we need to fetch the package name in PerfettoPlatform::GetCurrentProcessName().
    
    Bug: b:346770535
    Change-Id: I40c4706b118f0b8e4007b3991fa1e791a739d12f
    Cq-Include-Trybots: luci.chrome.try:cronet-arm64-gn2bp-debug
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6424666
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Feels: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446559}

--- net/http/http_response_headers_unittest.cc
+++ net/http/http_response_headers_unittest.cc
@@ -25,10 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_util.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-#if !BUILDFLAG(CRONET_BUILD)
 #include "third_party/perfetto/include/perfetto/test/traced_value_test_support.h"
-#endif
 
 namespace net {
 
@@ -2034,8 +2031,6 @@ TEST(HttpResponseHeadersTest, TryToCreateWithNul) {
   EXPECT_EQ(headers, nullptr);
 }
 
-#if !BUILDFLAG(CRONET_BUILD)
-// Cronet disables tracing so this test would fail.
 TEST(HttpResponseHeadersTest, TracingSupport) {
   scoped_refptr<HttpResponseHeaders> headers = HttpResponseHeaders::TryToCreate(
       "HTTP/1.1 200 OK\n"
@@ -2045,7 +2040,6 @@ TEST(HttpResponseHeadersTest, TracingSupport) {
   EXPECT_EQ(perfetto::TracedValueToString(headers),
             "{response_code:200,headers:[{name:connection,value:keep-alive}]}");
 }
-#endif
 
 struct RemoveHeaderTestData {
   const char* orig_headers;
