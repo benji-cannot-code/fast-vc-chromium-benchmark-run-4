a0edc99b7265eb1a90da1fb3ca5ac4ec679508e3
Nohemi Fernandez <fernandex@google.com>
2025-04-14 06:13:31 -0700

    Move MockTabRestoreService to the core sessions component.
    Moves the general mock for sessions::TabRestoreService to the sessions
    component for easier reuse by clients outside of components/commerce.
    
    Bug: 406904337
    Change-Id: Ifa33239283ed679769c35fa9c41e988383f35151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450341
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446480}

--- components/sessions/core/mock_tab_restore_service.cc
+++ components/sessions/core/mock_tab_restore_service.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/commerce/core/mock_tab_restore_service.h"
+#include "components/sessions/core/mock_tab_restore_service.h"
 
 MockTabRestoreService::MockTabRestoreService() = default;
 
