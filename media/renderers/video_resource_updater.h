c05aafd3744740eca47cbac7d5ca35deef113a34
vikas soni <vikassoni@chromium.org>
2025-04-14 12:56:21 -0700

    Do not allow VideoResourceUpdater to import empty resources.
    Currently VideoResourceUpdater can import an empty resource into the
    ClientResourceProvider and obtain a valid ResourceId corresponding to
    it. The quad and frame containing that ResourceId will be sent to viz
    but viz will reject the whole frame. This is an overhead where clients
    sends empty resources to viz only to get the whole frame dropped later
    in this pipeline.
    
    This CL fixes the client VideoResourceUpdater behavior to not import
    any empty resource into the ClientResourceProvider.
    
    The impact of the change is a behavior change. Without this CL, whole
    frame with any empty TransferableResource would be rejected whereas now
    only the visual content corresponding to the video with empty resource
    will be blank since it was never inserted in Quad.
    
    Future CLs will tighten the CHECKs in ClientResourceProvider to disallow
    clients importing empty resources.
    
    Bug: 408529854
    Change-Id: I39f6b88ec4a354a9c0609cdbe7007df61e357bff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6445013
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446725}

--- media/renderers/video_resource_updater.h
+++ media/renderers/video_resource_updater.h
@@ -123,6 +123,8 @@ class MEDIA_EXPORT VideoResourceUpdater
   viz::SharedImageFormat YuvSharedImageFormat(int bits_per_channel);
   gpu::SharedImageInterface* shared_image_interface() const;
 
+  viz::ResourceId GetFrameResourceIdForTesting() const;
+
  private:
   class FrameResource;
 
