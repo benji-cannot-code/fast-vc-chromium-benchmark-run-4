3e436d8e53ee4d245bcae609123e7572e03706cb
Fiaz Muhammad <mfiaz@google.com>
2025-04-14 11:24:38 -0700

    [Tab Group Parity] Make ContextMenu theme Incognito status agnostic
    - Adds overload to method to allow for supplying whether we want to
    	theme this with incognito colors
    - Before CL: http://screen/3KLuYb4dE7PgBrB.png
    - After CL: http://screen/75tE5yCAYfAPVvx.png
    
    Bug: 409764403
    Change-Id: I5dc9bd3d3b21ba9885c3f226d904e637b3d66b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6446795
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446667}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherContextMenuCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherContextMenuCoordinator.java
@@ -132,7 +132,8 @@ public class TabSwitcherContextMenuCoordinator extends TabOverflowMenuCoordinato
                 /* verticalOverlapAnchor= */ false,
                 /* animStyle= */ R.style.TabSwitcherContextMenuAnimation,
                 HorizontalOrientation.LAYOUT_DIRECTION,
-                mActivity);
+                mActivity,
+                /* isIncognito= */ false);
         recordUserActionWithPrefix("Shown");
     }
 
