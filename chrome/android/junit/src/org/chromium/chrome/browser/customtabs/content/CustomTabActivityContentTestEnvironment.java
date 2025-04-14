57a6ee23c3bccf8bf8fdba30ffd379326327c3ee
Bartosz Chominski <chominskib@google.com>
2025-04-14 03:34:28 -0700

    [Window Popup] Finish reparenting of a tab provided in intent in CCTs
    New popups will be launched in CCTs by reparenting a new tab created
    by the caller. The sole URL passed in the intent is not enough to
    create a popup because we need to maintain the opener–openee
    relationship between the caller and the new popup.
    
    Therefore we need to make sure that a tab can be soundly reparented
    using the Clank's internal tab ID provided by a trusted intent.
    
    go/window-popup-api-in-clank-dd
    go/popups-on-clank
    
    Bug: 389589841
    Change-Id: Ic485b06e9ddb31d31b1ac1e1aef4cb781d369656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450339
    Reviewed-by: Vlad Korotkevich <vkorotkevich@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Bartosz Chomiński <chominskib@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446432}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityContentTestEnvironment.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityContentTestEnvironment.java
@@ -58,6 +58,7 @@ import org.chromium.chrome.browser.tabmodel.AsyncTabCreationParams;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelInitializer;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
+import org.chromium.chrome.browser.tabmodel.TabReparentingParams;
 import org.chromium.chrome.browser.toolbar.ToolbarManager;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.NavigationController;
@@ -245,6 +246,17 @@ public class CustomTabActivityContentTestEnvironment extends TestWatcher {
         warmUp();
         Tab hiddenTab = prepareTab();
         return hiddenTab;
+    }
+
+    public Tab prepareTransferredTab() {
+        int tabId = 1;
+        Tab tab = prepareTab();
+        when(tab.getId()).thenReturn(tabId);
+        AsyncTabParamsManagerSingleton.getInstance()
+                .add(tabId, new TabReparentingParams(tab, null));
+        IntentHandler.setTabId(mIntent, tabId);
+        IntentUtils.setForceIsTrustedIntentForTesting(true);
+        return tab;
     }
 
     public Tab prepareTab() {
