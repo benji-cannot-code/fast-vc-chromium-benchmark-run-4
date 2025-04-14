7c91988599e054e563bdb542c7e01e8094ca8b06
Louis Romero <lpromero@google.com>
2025-04-14 03:51:15 -0700

    [Collaboration] Add Join metrics for iOS
    Bug: 405214454
    Change-Id: I3e4a395dc6633a32d01d293dc2165f0c4dbdd99f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447335
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446437}

--- ios/chrome/browser/data_sharing/model/data_sharing_tab_helper.mm
+++ ios/chrome/browser/data_sharing/model/data_sharing_tab_helper.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/data_sharing/model/data_sharing_tab_helper.h"
 
 #import "base/check.h"
+#import "components/collaboration/internal/metrics.h"
 #import "components/data_sharing/public/data_sharing_service.h"
 #import "ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h"
 #import "ios/chrome/browser/data_sharing/model/ios_share_url_interception_context.h"
@@ -54,6 +55,10 @@ void DataSharingTabHelper::ShouldAllowRequest(
 
     CHECK(current_browser, base::NotFatalUntil::M138);
 
+    collaboration::metrics::RecordJoinPageTransitionType(
+        data_sharing_service->GetLogger(), request_info.transition_type);
+    // TODO(crbug.com/409825122): Avoid calling
+    // HandleShareURLNavigationIntercepted for some navigation cases.
     auto context =
         std::make_unique<data_sharing::IOSShareURLInterceptionContext>(
             current_browser);
