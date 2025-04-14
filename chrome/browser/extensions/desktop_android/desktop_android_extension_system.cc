04fcd8b71a993a3a01b94839e7b196f5f71515e7
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-04-14 14:57:28 -0700

    [Extensions] Implement GetDependentExtensions() on desktop android
    Add a (real) implementation for
    ExtensionSystem::GetDependentExtensions() in the experimental desktop
    android build, via DesktopAndroidExtensionSystem.
    
    Bug: 409120449
    Cq-Include-Trybots: luci.chromium.try:android-desktop-x64-rel
    Change-Id: I203591b690e140e20cd15dddc7f5f81db2c5eed0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6454956
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446798}

--- chrome/browser/extensions/desktop_android/desktop_android_extension_system.cc
+++ chrome/browser/extensions/desktop_android/desktop_android_extension_system.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/chrome_extension_registrar_delegate.h"
 #include "chrome/browser/extensions/load_error_reporter.h"
 #include "chrome/browser/extensions/permissions/permissions_updater.h"
+#include "chrome/browser/extensions/shared_module_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/value_store/value_store_factory_impl.h"
@@ -283,7 +284,8 @@ ContentVerifier* DesktopAndroidExtensionSystem::content_verifier() {
 std::unique_ptr<ExtensionSet>
 DesktopAndroidExtensionSystem::GetDependentExtensions(
     const Extension* extension) {
-  return std::make_unique<ExtensionSet>();
+  return SharedModuleService::Get(browser_context_)
+      ->GetDependentExtensions(extension);
 }
 
 void DesktopAndroidExtensionSystem::InstallUpdate(
