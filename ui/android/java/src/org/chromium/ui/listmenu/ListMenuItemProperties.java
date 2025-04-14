1a592187fe0e65ba29c8004433d0268ea4121f80
Jenna Himawan <jhimawan@google.com>
2025-04-14 15:44:13 -0700

    [ExtensionContextMenu] Add Java MenuModelBridge
    Bug: 410564902
    Change-Id: I037d83f522f49e5fb6a858e9d97b9de3072a8e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6454593
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446813}

--- ui/android/java/src/org/chromium/ui/listmenu/ListMenuItemProperties.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListMenuItemProperties.java
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.listmenu;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.view.View;
+
+import androidx.annotation.DrawableRes;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -31,9 +34,27 @@ public class ListMenuItemProperties {
     // A11y content description of menu item
     public static final WritableObjectPropertyKey<@Nullable String> CONTENT_DESCRIPTION =
             new WritableObjectPropertyKey<>();
+
+    /**
+     * The {@link DrawableRes} ID of the start icon. Mutually exclusive from {@code
+     * START_ICON_DRAWABLE} and {@code START_ICON_BITMAP}.
+     */
     public static final WritableIntPropertyKey START_ICON_ID = new WritableIntPropertyKey();
+
+    /**
+     * The {@link Drawable} for the start icon. Mutually exclusive from {@code START_ICON_ID} and
+     * {@code START_ICON_BITMAP}.
+     */
     public static final WritableObjectPropertyKey<@Nullable Drawable> START_ICON_DRAWABLE =
             new WritableObjectPropertyKey<>();
+
+    /**
+     * The {@link Bitmap} for the start icon. Mutually exclusive from {@code START_ICON_ID} and
+     * {@code START_ICON_DRAWABLE}.
+     */
+    public static final WritableObjectPropertyKey<@Nullable Bitmap> START_ICON_BITMAP =
+            new WritableObjectPropertyKey<>();
+
     public static final WritableIntPropertyKey END_ICON_ID = new WritableIntPropertyKey();
     public static final WritableIntPropertyKey GROUP_ID = new WritableIntPropertyKey();
     public static final WritableIntPropertyKey MENU_ITEM_ID = new WritableIntPropertyKey();
@@ -56,6 +77,7 @@ public class ListMenuItemProperties {
         CONTENT_DESCRIPTION,
         START_ICON_ID,
         START_ICON_DRAWABLE,
+        START_ICON_BITMAP,
         END_ICON_ID,
         GROUP_ID,
         MENU_ITEM_ID,
