f91cd119d8279f4efcdae7444c347f6edf95fb00
Brahim Chikhaoui <bchikhaoui@google.com>
2025-04-15 00:33:59 -0700

    [OOBE] Allow ZTE retry via reboot from user creation screen
    If a user lands on the user creation screen (potentially due to failed
    ZTE), allow them to retry Zero-Touch Enrollment by rebooting.
    
    This is achieved by preventing the 'post-update' screen state from
    being saved in the resumable flow when proceeding to user creation.
    When the device reboots from the user creation screen, it will now
    resume from the update screen, re-triggering the auto-enrollment check.
    
    Bug: b:370945613
    Change-Id: I30e4807e24c7e216cc2c7e3fad172981002c13e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448334
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446950}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -297,10 +297,6 @@ const StaticOobeScreenId kResumableOobeScreens[] = {
     WelcomeView::kScreenId,
     NetworkScreenView::kScreenId,
     UpdateView::kScreenId,
-    EnrollmentScreenView::kScreenId,
-    AutoEnrollmentCheckScreenView::kScreenId,
-    UserCreationView::kScreenId,
-    AddChildScreenView::kScreenId,
     ConsumerUpdateScreenView::kScreenId,
 };
 
