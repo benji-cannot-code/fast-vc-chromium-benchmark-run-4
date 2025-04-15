a8541f17d2f3870b0655a5055c56e2f0479f5ae1
Lei Zhang <thestig@chromium.org>
2025-04-15 12:44:54 -0700

    Only include base/check.h in base/containers/checked_iterators.h
    Since https://crrev.com/1431853 changed checked_iterators.h to only use
    CHECK(), it should stop including check_op.h and switch to check.h. Then
    do IWYU to fix the build.
    
    Change-Id: Iedf39471f5fb7693d6f8ad0a5cf8341222cfdcb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6456528
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447311}

--- base/containers/checked_iterators.h
+++ base/containers/checked_iterators.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <type_traits>
 
-#include "base/check_op.h"
+#include "base/check.h"
 #include "base/compiler_specific.h"
 #include "base/containers/span_forward_internal.h"
 #include "base/memory/raw_ptr_exclusion.h"
--- base/strings/cstring_view.h
+++ base/strings/cstring_view.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/check.h"
+#include "base/check_op.h"
 #include "base/compiler_specific.h"
 #include "base/containers/checked_iterators.h"
 #include "base/memory/raw_ptr_exclusion.h"
