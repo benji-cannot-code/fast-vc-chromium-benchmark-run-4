07b06650d0dbfe3e3dcf764168a491ef5d773600
Luke Klimek <zgroza@chromium.org>
2025-04-15 08:58:08 -0700

    SmartCard: Add policy-allowlisted origins to content settings page
    Policy-allowlisted origins are now visible on
    chrome://settings/content/smartCardReaders, utilizing the mechanism used
    for other chooser-based types.
    
    Screenshot in the bug's comment.
    
    Bug: 410750216
    Change-Id: If427f259e81f452e18f802ba725585da6a4dea78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458937
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Luke Klimek <zgroza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447153}

--- chrome/browser/smart_card/smart_card_permission_context_unittest.cc
+++ chrome/browser/smart_card/smart_card_permission_context_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/smart_card/smart_card_permission_context.h"
 
 #include "base/check_deref.h"
+#include "base/test/values_test_util.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
@@ -27,7 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "url/origin.h"
 
+using testing::ElementsAre;
+using testing::Field;
 using testing::InSequence;
+using testing::Pointee;
+using testing::ResultOf;
 using testing::StrictMock;
 
 namespace {
@@ -384,12 +389,17 @@ TEST_F(SmartCardPermissionContextTest, AllowedByPolicy) {
 
   EXPECT_TRUE(HasReaderPermission(permission_context, origin_1, kDummyReader));
 
-  auto grants = permission_context.GetGrantedObjects(origin_1);
-  ASSERT_EQ(1u, grants.size());
-  EXPECT_EQ(content_settings::SettingSource::kPolicy, grants[0]->source);
-  EXPECT_EQ(l10n_util::GetStringUTF8(
-                IDS_SMART_CARD_POLICY_DESCRIPTION_FOR_ANY_DEVICE),
-            CHECK_DEREF(grants[0]->value.FindString("reader-name")));
+  auto matcher = ElementsAre(Pointee(AllOf(
+      Field(&SmartCardPermissionContext::Object::source,
+            content_settings::SettingSource::kPolicy),
+      Field(&SmartCardPermissionContext::Object::value,
+            AllOf(base::test::DictionaryHasValue(
+                "reader-name",
+                base::Value(l10n_util::GetStringUTF8(
+                    IDS_SMART_CARD_POLICY_DESCRIPTION_FOR_ANY_DEVICE))))))));
+
+  EXPECT_THAT(permission_context.GetGrantedObjects(origin_1), matcher);
+  EXPECT_THAT(permission_context.GetAllGrantedObjects(), matcher);
 }
 
 TEST_F(SmartCardPermissionContextTest, BlockedByPolicy) {
