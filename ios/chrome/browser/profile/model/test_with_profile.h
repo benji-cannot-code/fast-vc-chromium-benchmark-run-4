152df2fe2474cc2803c206ba2b6633e876016dc5
Sylvain Defresne <sdefresne@chromium.org>
2025-04-15 09:48:23 -0700

    [ios] Add way to use ScopedFeatureList with TestWithProfile
    The registration (and destruction) of ScopedFeatureList is not
    thread-safe so expose an API to allow TestWithProfile sub-classes
    to enable/disable features with a ScopedFeatureList that outlives
    the threads created internally by TestWithProfile.
    
    Bug: 410532590
    Change-Id: If1adc14fecf4b3ee230ccb10f9fbdafc58ce5973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448320
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447184}

--- ios/chrome/browser/profile/model/test_with_profile.h
+++ ios/chrome/browser/profile/model/test_with_profile.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/files/file_path.h"
+#include "base/test/scoped_feature_list.h"
 #include "components/variations/scoped_variations_ids_provider.h"
 #include "ios/chrome/browser/profile/model/profile_manager_ios_impl.h"
 #include "ios/chrome/browser/signin/model/account_profile_mapper.h"
@@ -22,6 +23,8 @@ class IOSChromeIOThread;
 class TestWithProfile : public PlatformTest {
  public:
   TestWithProfile();
+  TestWithProfile(const std::vector<base::test::FeatureRef>& enabled_features,
+                  const std::vector<base::test::FeatureRef>& disabled_features);
 
   ~TestWithProfile() override;
 
@@ -37,6 +40,23 @@ class TestWithProfile : public PlatformTest {
   }
 
  private:
+  // Helper around a ScopedFeatureList that initialize it in its constructor.
+  class InitializedFeatureList {
+   public:
+    InitializedFeatureList(
+        const std::vector<base::test::FeatureRef>& enabled_features,
+        const std::vector<base::test::FeatureRef>& disabled_features);
+
+    InitializedFeatureList(const InitializedFeatureList&) = delete;
+    InitializedFeatureList& operator=(const InitializedFeatureList&) = delete;
+
+    ~InitializedFeatureList();
+
+   private:
+    base::test::ScopedFeatureList scoped_feature_list_;
+  };
+
+  InitializedFeatureList initialized_scoped_feature_list_;
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   std::unique_ptr<IOSChromeIOThread> chrome_io_;
   web::WebTaskEnvironment web_task_environment_{
