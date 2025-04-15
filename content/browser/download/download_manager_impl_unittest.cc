e1a451259a22efc2da3d38ea4d3789312a13a8f8
Min Qin <qinmin@chromium.org>
2025-04-15 11:12:33 -0700

    Enable Save page for incognito mode
    This CL will enable save page functionality for incognito mode.
    
    Bug: 405842034
    Change-Id: I55690656142b29a582fdae631c1665e50a4c4912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6451516
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447243}

--- content/browser/download/download_manager_impl_unittest.cc
+++ content/browser/download/download_manager_impl_unittest.cc
@@ -177,6 +177,7 @@ class MockDownloadItemFactory final : public download::DownloadItemFactory {
       download::DownloadItemImplDelegate* delegate,
       uint32_t download_id,
       const base::FilePath& path,
+      const base::FilePath& display_name,
       const GURL& url,
       const std::string& mime_type,
       download::DownloadJob::CancelRequestCallback cancel_request_callback)
@@ -333,6 +334,7 @@ download::DownloadItemImpl* MockDownloadItemFactory::CreateSavePageItem(
     download::DownloadItemImplDelegate* delegate,
     uint32_t download_id,
     const base::FilePath& path,
+    const base::FilePath& display_name,
     const GURL& url,
     const std::string& mime_type,
     download::DownloadJob::CancelRequestCallback cancel_request_callback) {
