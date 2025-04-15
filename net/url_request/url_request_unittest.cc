91de6b6ff6392e05f1b4c88a3fe320b515e686aa
Mohannad Farrag <aymanm@google.com>
2025-04-15 02:53:36 -0700

    Disable OCSPVerifyResult for Cronet on Android 16
    Starting from Android 16 and https://crrev.com/c/6416586, both WebView
    and Cronet has started using the new Conscrypt API for certificate
    validation which does OCSP stapling verification. This meant that the
    test starts failing as it became an integration test more than a
    unit-test. This failing test is now blocking our imports in AOSP, and
    there's no definite deadline on when this will get fixed, so unblock
    our imports while we fix this later.
    
    Note: This disables the test only for Cronet. There's no way to know
    if the test is built for WebView or not (No such build flag exists).
    
    Bug: 408137065
    Change-Id: Ia169b7c2c5409fbcb960468f8678c57c68909f7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448906
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446999}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/android/android_info.h"
 #ifdef UNSAFE_BUFFERS_BUILD
 // TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
 #pragma allow_unsafe_buffers
 #endif
-
-#include "net/url_request/url_request.h"
 
 #include <stdint.h>
 
@@ -56,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "crypto/sha2.h"
 #include "net/base/chunked_upload_data_stream.h"
+#include "net/base/cronet_buildflags.h"
 #include "net/base/directory_listing.h"
 #include "net/base/elements_upload_data_stream.h"
 #include "net/base/features.h"
@@ -149,6 +149,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/referrer_policy.h"
 #include "net/url_request/static_http_user_agent_settings.h"
 #include "net/url_request/storage_access_status_cache.h"
+#include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_filter.h"
@@ -11162,6 +11163,17 @@ class HTTPSOCSPVerifyTest
       public testing::WithParamInterface<OCSPVerifyTestData> {};
 
 TEST_P(HTTPSOCSPVerifyTest, VerifyResult) {
+#if BUILDFLAG(CRONET_BUILD)
+  // Previously, OCSP stapling would never be passed to the platform verifier
+  // which meant that the only layer performing the verification was BoringSSL.
+  // However, this has changed with Android 16 where we started passing the OCSP
+  // stapling data to Conscrypt. This meant certificates with bad OCSP stapling
+  // that were previously passing the Conscrypt check are now failing the check.
+  // See crbug.com/408137065 for more information.
+  if (base::android::android_info::sdk_int() >= 36) {
+    GTEST_SKIP();
+  }
+#endif
   OCSPVerifyTestData test = GetParam();
 
   scoped_refptr<X509Certificate> root_cert =
