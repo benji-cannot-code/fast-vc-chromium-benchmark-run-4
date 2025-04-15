4a71142b05e39df4af2ae156be08f61a5710ac6a
caott <caott@google.com>
2025-04-15 13:58:59 -0700

    [OnTask] Block microphone and camera access in pause mode
    Bug: b:409603095
    Change-Id: Ifa791867fe8606c6929cc87e85c45c5ef0eccaef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6455371
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Luke Cao <caott@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447374}

--- chrome/browser/ash/boca/on_task/on_task_system_web_app_manager_impl.h
+++ chrome/browser/ash/boca/on_task/on_task_system_web_app_manager_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_BOCA_ON_TASK_ON_TASK_SYSTEM_WEB_APP_MANAGER_IMPL_H_
 #define CHROME_BROWSER_ASH_BOCA_ON_TASK_ON_TASK_SYSTEM_WEB_APP_MANAGER_IMPL_H_
 
+#include <optional>
+
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -64,7 +66,15 @@ class OnTaskSystemWebAppManagerImpl : public OnTaskSystemWebAppManager {
 
   void EnableOrDisableCommandsForTabSwitch(SessionID window_id, bool enabled);
 
+  void PauseCameraInput(bool paused);
+
+  void PauseMicrophoneInput(bool paused);
+
   raw_ptr<Profile> profile_;
+
+  std::optional<bool> was_camera_disabled_;
+
+  std::optional<bool> was_microphone_disabled_;
 
   raw_ptr<LockedSessionWindowTracker> window_tracker_for_testing_;
 
--- chrome/browser/ash/boca/on_task/on_task_system_web_app_manager_impl_browsertest.cc
+++ chrome/browser/ash/boca/on_task/on_task_system_web_app_manager_impl_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/boca/on_task/on_task_system_web_app_manager_impl.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/system/privacy_hub/camera_privacy_switch_controller.h"
 #include "ash/webui/system_apps/public/system_web_app_type.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chromeos/ash/components/audio/cras_audio_handler.h"
 #include "chromeos/ash/components/boca/on_task/on_task_session_manager.h"
 #include "chromeos/ash/components/boca/proto/roster.pb.h"
 #include "chromeos/ui/wm/window_util.h"
@@ -249,10 +251,20 @@ IN_PROC_BROWSER_TEST_F(OnTaskSystemWebAppManagerImplBrowserTest,
   EXPECT_FALSE(command_controller->IsCommandEnabled(IDC_SELECT_TAB_5));
   EXPECT_FALSE(command_controller->IsCommandEnabled(IDC_SELECT_TAB_6));
   EXPECT_FALSE(command_controller->IsCommandEnabled(IDC_SELECT_TAB_7));
+
+  // Verify that camera and microphone access are disabled.
+  EXPECT_TRUE(
+      ash::CameraPrivacySwitchController::Get()->IsCameraAccessForceDisabled());
+  EXPECT_TRUE(ash::CrasAudioHandler::Get()->IsInputMuted());
 }
 
 IN_PROC_BROWSER_TEST_F(OnTaskSystemWebAppManagerImplBrowserTest,
                        UnpauseSystemWebAppWindow) {
+  // Enable camera and disable microphone.
+  ash::CameraPrivacySwitchController::Get()->SetForceDisableCameraAccess(false);
+  ash::CrasAudioHandler::Get()->SetInputMute(
+      true, ash::CrasAudioHandler::InputMuteChangeMethod::kOther);
+
   // Launch Boca app for testing purposes.
   OnTaskSystemWebAppManagerImpl system_web_app_manager(profile());
   base::test::TestFuture<bool> launch_future;
@@ -260,6 +272,11 @@ IN_PROC_BROWSER_TEST_F(OnTaskSystemWebAppManagerImplBrowserTest,
   ASSERT_TRUE(launch_future.Get());
   Browser* const boca_app_browser = FindBocaSystemWebAppBrowser();
   ASSERT_THAT(boca_app_browser, NotNull());
+
+  // Disable camera and enable microphone.
+  ash::CameraPrivacySwitchController::Get()->SetForceDisableCameraAccess(true);
+  ash::CrasAudioHandler::Get()->SetInputMute(
+      false, ash::CrasAudioHandler::InputMuteChangeMethod::kOther);
 
   // Pin and pause the Boca app.
   system_web_app_manager.SetPinStateForSystemWebAppWindow(
@@ -272,6 +289,11 @@ IN_PROC_BROWSER_TEST_F(OnTaskSystemWebAppManagerImplBrowserTest,
   auto* const browser_view =
       BrowserView::GetBrowserViewForBrowser(boca_app_browser);
   EXPECT_FALSE(browser_view->immersive_mode_controller()->IsEnabled());
+
+  // Verify that camera and microphone access are disabled.
+  ASSERT_TRUE(
+      ash::CameraPrivacySwitchController::Get()->IsCameraAccessForceDisabled());
+  ASSERT_TRUE(ash::CrasAudioHandler::Get()->IsInputMuted());
 
   // Unpause the Boca app and verify result.
   system_web_app_manager.SetPauseStateForSystemWebAppWindow(
@@ -294,6 +316,12 @@ IN_PROC_BROWSER_TEST_F(OnTaskSystemWebAppManagerImplBrowserTest,
   EXPECT_TRUE(command_controller->IsCommandEnabled(IDC_SELECT_TAB_5));
   EXPECT_TRUE(command_controller->IsCommandEnabled(IDC_SELECT_TAB_6));
   EXPECT_TRUE(command_controller->IsCommandEnabled(IDC_SELECT_TAB_7));
+
+  // Verify that camera and microphone access are restored to the latest state
+  // before pause mode.
+  EXPECT_TRUE(
+      ash::CameraPrivacySwitchController::Get()->IsCameraAccessForceDisabled());
+  EXPECT_FALSE(ash::CrasAudioHandler::Get()->IsInputMuted());
 }
 
 IN_PROC_BROWSER_TEST_F(OnTaskSystemWebAppManagerImplBrowserTest,
