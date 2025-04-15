46a85f997deac97f3b12ca62bbb07db4c0c67360
Aida Zolic <aidazolic@chromium.org>
2025-04-15 05:18:15 -0700

    [AppCreep] Add a feature flag for hidden by default
    Adds a feature flag to hide the changes for disabling icons by default.
    
    R=anqing@chromium.org
    
    Bug: 409027905
    Change-Id: I185f44c791052cd41c098060a4301cf4481be2f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448281
    Commit-Queue: Aida Zolić <aidazolic@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447038}

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -354,6 +354,12 @@ BASE_FEATURE(kFileSystemProviderContentCache,
              "FileSystemProviderContentCache",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables hiding apps disabled by SystemFeaturesDisableList policy by default
+// in user sessions.
+BASE_FEATURE(kSystemFeaturesDisableListHidden,
+             "SystemFeaturesDisableListHidden",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 const char kRoundedWindowsRadius[] = "window_radius";
 
 bool IsApnPoliciesEnabled() {
@@ -411,6 +417,10 @@ bool IsFileSystemProviderContentCacheEnabled() {
     return false;
   }
   return base::FeatureList::IsEnabled(kFileSystemProviderContentCache);
+}
+
+bool IsSystemFeaturesDisableListHiddenEnabled() {
+  return base::FeatureList::IsEnabled(kSystemFeaturesDisableListHidden);
 }
 
 bool IsGeminiAppPreinstallFeatureManagementEnabled() {
