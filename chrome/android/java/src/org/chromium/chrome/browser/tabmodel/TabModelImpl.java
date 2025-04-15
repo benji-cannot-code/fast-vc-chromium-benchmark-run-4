742b279472901cff6866638ac053954e25cbc590
Calder Kitagawa <ckitagawa@chromium.org>
2025-04-15 11:34:22 -0700

    [Tabs] Implement TabAndroid::GetGroup and use it
    - Implement GetGroup using the prior impl of GetTabGroupId with slight
      tweaks.
    - Delete IsInTabGroup and IsInTabGroupLegacy in favor of just using
      GetGroup.
    
    Bug: 409366905
    Change-Id: Ic5b388f4b9c173be7cdd88c613c5445be464ef72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6454984
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447258}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
@@ -1038,12 +1038,6 @@ public class TabModelImpl extends TabModelJniBridge {
     }
 
     @Override
-    public boolean isTabInTabGroup(@NonNull Tab tab) {
-        TabGroupModelFilter filter = mModelDelegate.getFilter(isIncognito());
-        return filter == null ? false : filter.isTabInTabGroup(tab);
-    }
-
-    @Override
     public int getTabCountNavigatedInTimeWindow(long beginTimeMs, long endTimeMs) {
         return getTabsNavigatedInTimeWindow(beginTimeMs, endTimeMs).size();
     }
