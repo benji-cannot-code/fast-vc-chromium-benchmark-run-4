74c377806457d83902c81424898dee728c4c6840
Dan Harrington <harringtond@google.com>
2025-04-15 09:38:00 -0700

    allow glic client to navigate
    Before this change, the glic client, once connected, could not
    navigate without triggering a reload back to the original
    glic url.
    After this change, we allow navigation, but if a client does
    not connect on the new URL, we won't use it again.
    
    Bug: b:409798264
    Change-Id: I87e2d367b013cb4d22e47b136da82a656861f3f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449914
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447178}

--- chrome/browser/resources/glic/glic_app_controller.ts
+++ chrome/browser/resources/glic/glic_app_controller.ts
@@ -12,7 +12,7 @@ import type {PageInterface} from './glic.mojom-webui.js';
 import type {ApiHostEmbedder} from './glic_api_impl/glic_api_host.js';
 import {WebClientState} from './glic_api_impl/glic_api_host.js';
 import type {PageType, WebviewDelegate} from './webview.js';
-import {WebviewController} from './webview.js';
+import {WebviewController, WebviewPersistentState} from './webview.js';
 
 const transitionDuration = {
   microseconds: BigInt(100000),
@@ -73,6 +73,7 @@ export class GlicAppController implements PageInterface, WebviewDelegate,
 
   // Present only when loading or after loading is finished. Removed on error.
   private webview?: WebviewController;
+  private webviewPersistentState = new WebviewPersistentState();
 
   private profileReadyState: ProfileReadyState|undefined = undefined;
   private profileReadyInitialState = Promise.withResolvers<void>();
@@ -283,7 +284,8 @@ export class GlicAppController implements PageInterface, WebviewDelegate,
     // Load the web client only after cookie sync is complete.
     this.destroyWebview();
     this.webview = new WebviewController(
-        $.webviewContainer, this.browserProxy, this, this);
+        $.webviewContainer, this.browserProxy, this, this,
+        this.webviewPersistentState);
     this.webview.getWebClientState().subscribe(
         this.webClientStateChanged.bind(this));
 
