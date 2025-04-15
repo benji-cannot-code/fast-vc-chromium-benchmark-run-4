6b4f0eb6e1b0c92b321c28de98e2b55542bc8b1a
Matt Mueller <mattm@chromium.org>
2025-04-15 16:23:58 -0700

    restore SSLClientSocket test of ERR_SSL_SERVER_CERT_BAD_FORMAT
    This used to be tested by the CERT_BAD_VALIDITY certificate, however
    that stopped being useful once X509Certificate stopped using the OS
    certificate types internally, and the Chrome implementation was able
    to correctly parse the bad_validity cert (which didn't really have a
    bad validity, just a date that windows was unable to represent.)
    
    Change the test to generate an actually invalid certificate that is
    allowed by the SSL layer but fails the current X509Certificate parsing.
    
    Change-Id: I4daa79200c14c81373d5496b177800b8846b6f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6459830
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447452}

--- net/socket/ssl_server_socket_impl.h
+++ net/socket/ssl_server_socket_impl.h
@@ -19,6 +19,9 @@ namespace net {
 
 class SSLServerContextImpl : public SSLServerContext {
  public:
+  SSLServerContextImpl(std::vector<bssl::UniquePtr<CRYPTO_BUFFER>> cert_chain,
+                       EVP_PKEY* pkey,
+                       const SSLServerConfig& ssl_server_config);
   SSLServerContextImpl(X509Certificate* certificate,
                        EVP_PKEY* pkey,
                        const SSLServerConfig& ssl_server_config);
@@ -40,8 +43,8 @@ class SSLServerContextImpl : public SSLServerContext {
   // Options for the SSL socket.
   SSLServerConfig ssl_server_config_;
 
-  // Certificate for the server.
-  scoped_refptr<X509Certificate> cert_;
+  // Certificate chain for the server.
+  std::vector<bssl::UniquePtr<CRYPTO_BUFFER>> cert_chain_;
 
   // Private key used by the server.
   // Only one representation should be set at any time.
