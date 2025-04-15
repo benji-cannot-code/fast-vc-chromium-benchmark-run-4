54ff93962a3ccae727156991be03346e709d96ec
April Zhou <aprilzhou@google.com>
2025-04-15 15:12:46 -0700

    boca: Trigger tab update events when user switch in and out workbook.
    Bug: b:409363970
    Test: browser tested. Manually tested
    Change-Id: I7314372704ca56e831693d11da408c368fe88ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6459884
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447408}

--- chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker.h
+++ chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker.h
@@ -35,6 +35,8 @@ namespace ash {
 class OnTaskPodController;
 }
 
+inline static constexpr char16_t kOutsideOfWorkbookTitle[] = u"Not in workbook";
+
 // This class is used to track the windows and tabs that are opened in the
 // user's OnTask locked session. Only one browser window is allowed at a time to
 // be tracked. Attempting to track another browser while there is one already
@@ -122,6 +124,8 @@ class LockedSessionWindowTracker : public KeyedService,
   // BrowserListObserver Implementation
   void OnBrowserClosing(Browser* browser) override;
   void OnBrowserAdded(Browser* browser) override;
+  void OnBrowserSetLastActive(Browser* browser) override;
+  void OnBrowserNoLongerActive(Browser* browser) override;
 
   // content::WebContentsObserver Impl
   void DidFinishNavigation(
