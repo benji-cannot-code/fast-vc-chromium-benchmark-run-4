76b1febfdfe29b491d46bc8881e4aa575b6846fe
jalonthomas <jalonthomas@google.com>
2025-04-15 08:56:51 -0700

    Remove redundant validator update on aliases in first party sets parser
    Since aliases are already in their respective additions/replacements
    sets, there's no need to run validator.Update on the alias list. In
    fact, calling update on the aliases causes a CHECK error if the list
    has an invalid key.
    
    Bug: 406036301
    Change-Id: I5fbd28d2b192f4d466caaf9cf672eb70e72fd54d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6427118
    Commit-Queue: Jalon Thomas <jalonthomas@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447151}

--- content/browser/first_party_sets/first_party_set_parser_unittest.cc
+++ content/browser/first_party_sets/first_party_set_parser_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/test/fuzztest_support.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/values_test_util.h"
 #include "base/version.h"
 #include "content/public/browser/first_party_sets_handler.h"
 #include "content/public/common/content_features.h"
@@ -1739,6 +1740,83 @@ TEST(FirstPartySetParser, EnterprisePolicies_ExemptFromAssociatedSiteLimit) {
                                        std::nullopt)},
           }},
           {}, {})));
+}
+
+// Regression test for https://crbug.com/406036301.
+TEST(FirstPartySetParser,
+     EnterprisePolicies_AcceptsValidAndInvalidCctld_JustAdditions) {
+  net::SchemefulSite primary(GURL("https://primary.test"));
+  net::SchemefulSite alias(GURL("https://primary.foo"));
+
+  base::Value::Dict policy_dict = base::test::ParseJsonDict(R"(
+             {
+                "additions": [
+                  {
+                    "primary": "https://primary.test",
+                    "associatedSites": [
+                      "https://associated.test"
+                    ],
+                    "ccTLDs": {
+                      "https://primary.test": [
+                        "https://sub.associated.test",
+                        "https://primary.foo"
+                      ]
+                    }
+                  }
+                ]
+              }
+            )");
+
+  EXPECT_EQ(
+      FirstPartySetParser::ParseSetsFromEnterprisePolicy(policy_dict)
+          .first.value(),
+      FirstPartySetsOverridesPolicy(net::SetsMutation(
+          {},
+          {{
+              {primary, net::FirstPartySetEntry(
+                            primary, net::SiteType::kPrimary, std::nullopt)},
+              {alias, net::FirstPartySetEntry(primary, net::SiteType::kPrimary,
+                                              std::nullopt)},
+          }},
+          {{alias, primary}})));
+}
+
+// Regression test for https://crbug.com/406036301.
+TEST(FirstPartySetParser,
+     EnterprisePolicies_AcceptsValidAndInvalidCctld_JustReplacements) {
+  net::SchemefulSite primary(GURL("https://primary.test"));
+  net::SchemefulSite alias(GURL("https://primary.foo"));
+
+  base::Value::Dict policy_dict = base::test::ParseJsonDict(R"(
+             {
+                "replacements": [
+                  {
+                    "primary": "https://primary.test",
+                    "associatedSites": [
+                      "https://associated.test"
+                    ],
+                    "ccTLDs": {
+                      "https://primary.test": [
+                        "https://sub.associated.test",
+                        "https://primary.foo"
+                      ]
+                    }
+                  }
+                ]
+              }
+            )");
+
+  EXPECT_EQ(
+      FirstPartySetParser::ParseSetsFromEnterprisePolicy(policy_dict)
+          .first.value(),
+      FirstPartySetsOverridesPolicy(net::SetsMutation(
+          {{
+              {primary, net::FirstPartySetEntry(
+                            primary, net::SiteType::kPrimary, std::nullopt)},
+              {alias, net::FirstPartySetEntry(primary, net::SiteType::kPrimary,
+                                              std::nullopt)},
+          }},
+          {}, {{alias, primary}})));
 }
 
 TEST(FirstPartySetParser, ParseFromCommandLine_Invalid_MultipleSets) {
