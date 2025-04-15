7f8f16039621acabe4cbe0c263f82a5eb9b6e281
Kenichi Ishibashi <bashi@chromium.org>
2025-04-14 20:06:39 -0700

    Reland "HttpStreamPool: Add experimental trace events"
    This is a reland of commit 08a30fbdaaba56e5ef04e5782c064db1152ae1c9
    
    The original CL was reverted [1] due to build failures caused by
    using perfetto::Track::ThreadScoped() in net_unittests. The method
    doesn't seem to exist on some platforms. Use
    perfetto::Track() instead. Diff is [2].
    
    [1] https://crrev.com/c/6453988
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/6449930/2..5
    
    Original change's description:
    > HttpStreamPool: Add experimental trace events
    >
    > This CL add experimental trace events to HttpStreamPool subclasses.
    > Currently we use tracks for AttemptManager, TcpBasedAttempt and QuicTask
    > and these are connected with flows. We may change how to record these
    > trace events if we figure out better way to record events.
    >
    > Introduce a new trace category since it might be noisy if we use the
    > existing "net" category.
    >
    > Bug: 383824591
    > Change-Id: I85ef2b6ab2a2ab92e1ce9b654fdc59848057a62a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437996
    > Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    > Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1446399}
    
    Bug: 383824591
    Change-Id: I340264c3e8bef3e82c595f88bd135004922bb0a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449930
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446908}

--- net/http/http_stream_pool_attempt_manager_quic_task.cc
+++ net/http/http_stream_pool_attempt_manager_quic_task.cc
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_id_helper.h"
 #include "base/values.h"
 #include "net/base/connection_endpoint_metadata.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_error_details.h"
 #include "net/base/net_errors.h"
+#include "net/base/tracing.h"
 #include "net/dns/host_resolver.h"
 #include "net/dns/public/host_resolver_results.h"
 #include "net/http/http_network_session.h"
@@ -35,13 +37,18 @@ HttpStreamPool::AttemptManager::QuicTask::QuicTask(
     quic::ParsedQuicVersion quic_version)
     : manager_(manager),
       quic_version_(quic_version),
-      net_log_(NetLogWithSource::Make(
-          manager->net_log().net_log(),
-          NetLogSourceType::HTTP_STREAM_POOL_QUIC_TASK)) {
+      net_log_(
+          NetLogWithSource::Make(manager->net_log().net_log(),
+                                 NetLogSourceType::HTTP_STREAM_POOL_QUIC_TASK)),
+      track_(base::trace_event::GetNextGlobalTraceId()),
+      flow_(perfetto::Flow::ProcessScoped(
+          base::trace_event::GetNextGlobalTraceId())) {
   CHECK(manager_);
   CHECK(service_endpoint_request());
   CHECK(service_endpoint_request()->EndpointsCryptoReady());
 
+  TRACE_EVENT_INSTANT("net.stream", "QuicTaskStart", manager_->track_, flow_);
+  TRACE_EVENT_BEGIN("net.stream", "QuicTask::QuicTask", track_, flow_);
   net_log_.BeginEvent(NetLogEventType::HTTP_STREAM_POOL_QUIC_TASK_ALIVE, [&] {
     base::Value::Dict dict;
     dict.Set("quic_version", quic::ParsedQuicVersionToString(quic_version_));
@@ -54,6 +61,8 @@ HttpStreamPool::AttemptManager::QuicTask::QuicTask(
 }
 
 HttpStreamPool::AttemptManager::QuicTask::~QuicTask() {
+  TRACE_EVENT_END("net.stream", track_);
+  TRACE_EVENT_INSTANT("net.stream", "QuicTaskEnd", manager_->track_, flow_);
   net_log_.EndEvent(NetLogEventType::HTTP_STREAM_POOL_QUIC_TASK_ALIVE);
 }
 
@@ -108,6 +117,8 @@ void HttpStreamPool::AttemptManager::QuicTask::MaybeAttempt() {
   std::set<std::string> dns_aliases =
       service_endpoint_request()->GetDnsAliasResults();
 
+  TRACE_EVENT_INSTANT("net.stream", "QuicTask::QuicSessionAttemptStart", track_,
+                      "endpoint", quic_endpoint->ToValue());
   net_log_.AddEvent(NetLogEventType::HTTP_STREAM_POOL_QUIC_ATTEMPT_START,
                     [&] { return quic_endpoint->ToValue(); });
 
@@ -218,6 +229,9 @@ HttpStreamPool::AttemptManager::QuicTask::GetPreferredIPEndPoint(
 
 void HttpStreamPool::AttemptManager::QuicTask::OnSessionAttemptComplete(
     int rv) {
+  TRACE_EVENT_INSTANT("net.stream", "QuicTask::OnSessionAttemptComplete",
+                      track_, "result", rv);
+
   if (rv == OK) {
     QuicChromiumClientSession* session =
         quic_session_pool()->FindExistingSession(GetKey().session_key(),
