7eefc30f93ad862c815dccc294d797f4dfb4b998
Michael Tang <tangm@microsoft.com>
2025-04-15 16:39:03 -0700

    win: Add DVLOGs to SwapChainPresenter lifecycle events
    This helps us quickly understand what state a SwapChainPresenter is in
    and how often it and its resources are being allocated.
    
    Change-Id: I52270ca2aef24ff3b5c22685745af57b4b5f6cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6454046
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1447457}

--- ui/gl/swap_chain_presenter.cc
+++ ui/gl/swap_chain_presenter.cc
@@ -492,9 +492,12 @@ SwapChainPresenter::SwapChainPresenter(
       is_on_battery_power_(
           base::PowerMonitor::GetInstance()
               ->AddPowerStateObserverAndReturnBatteryPowerStatus(this) ==
-          base::PowerStateObserver::BatteryPowerStatus::kBatteryPower) {}
+          base::PowerStateObserver::BatteryPowerStatus::kBatteryPower) {
+  DVLOG(1) << __func__ << "(" << this << ")";
+}
 
 SwapChainPresenter::~SwapChainPresenter() {
+  DVLOG(1) << __func__ << "(" << this << ")";
   base::PowerMonitor::GetInstance()->RemovePowerStateObserver(this);
 }
 
@@ -1415,6 +1418,7 @@ bool SwapChainPresenter::PresentToDecodeSwapChain(
       return false;
     }
     DCHECK(decode_swap_chain_);
+    DVLOG(2) << "Update visual's content. " << __func__ << "(" << this << ")";
     SetSwapChainPresentDuration();
 
     Microsoft::WRL::ComPtr<IDCompositionDesktopDevice> desktop_device;
@@ -1998,6 +2002,7 @@ bool SwapChainPresenter::PresentDCOMPSurface(DCLayerOverlayParams& params,
 
 void SwapChainPresenter::ReleaseDCOMPSurfaceResourcesIfNeeded() {
   if (dcomp_surface_handle_ != INVALID_HANDLE_VALUE) {
+    DVLOG(2) << __func__ << "(" << this << ")";
     dcomp_surface_handle_ = INVALID_HANDLE_VALUE;
     last_overlay_image_.reset();
     content_.Reset();
@@ -2257,6 +2262,7 @@ bool SwapChainPresenter::VideoProcessorBlt(
 
 void SwapChainPresenter::ReleaseSwapChainResources() {
   if (swap_chain_ || decode_swap_chain_) {
+    DVLOG(2) << __func__ << "(" << this << ")";
     output_view_.Reset();
     swap_chain_.Reset();
     swap_chain_handle_.Close();
@@ -2361,6 +2367,9 @@ bool SwapChainPresenter::ReallocateSwapChain(
                   << "\nFalling back to BGRA";
       use_yuv_swap_chain = false;
       swap_chain_format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    } else {
+      DVLOG(2) << "Update visual's content (yuv). " << __func__ << "(" << this
+               << ")";
     }
   }
   if (!use_yuv_swap_chain) {
@@ -2401,6 +2410,8 @@ bool SwapChainPresenter::ReallocateSwapChain(
                   << ". Disable overlay swap chains";
       return false;
     }
+
+    DVLOG(2) << "Update visual's content. " << __func__ << "(" << this << ")";
   }
 
   if (DXGIWaitableSwapChainEnabled()) {
