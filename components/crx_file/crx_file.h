d5b16797b67ecad8be5c160dc0c2a0ea9dceee4b
Elly <ellyjones@chromium.org>
2025-04-14 17:26:50 -0700

    components/crx_file: fix unsafe buffers
    This change removes unsafe buffer warnings in components/crx_file,
    except for those in crx_build_action_main.cc. To do that, this change:
    
    * Replaces most uses of plain arrays with std::array
    * Replaces uses of file and other APIs that accept (pointer, length)
      pairs with variants that accept spans
    * Simplifies a bit of related code using newer APIs (especially
      base::File::*AndCheck and std::ranges::search)
    
    This is a preparatory refactor for migrating components/crx_file to new
    crypto APIs.
    
    Change-Id: I81773202a5aaa00933fb0db6d181d28f7cc215dc
    Bug: 372283556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441646
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446861}

--- components/crx_file/crx_file.h
+++ components/crx_file/crx_file.h
@@ -9,9 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crx_file {
 
 // The magic string embedded in the header.
-constexpr char kCrxFileHeaderMagic[] = "Cr24";
-constexpr char kCrxDiffFileHeaderMagic[] = "CrOD";
-constexpr int kCrxFileHeaderMagicSize = 4;
+constexpr auto kCrxFileHeaderMagic =
+    std::to_array<uint8_t>({'C', 'r', '2', '4'});
+constexpr auto kCrxDiffFileHeaderMagic =
+    std::to_array<uint8_t>({'C', 'r', 'O', 'D'});
 constexpr char kSignatureContext[] = "CRX3 SignedData";
 
 }  // namespace crx_file
