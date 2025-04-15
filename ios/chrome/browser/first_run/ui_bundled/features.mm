e37c375be3700c05c3f85600d73e9cfce617dd1d
Briana McClure <bmcclure@google.com>
2025-04-14 17:05:33 -0700

    [iOS] Create the feature flag for Welcome Back screen
    This CL creates the feature flag for the 4-arm Welcome Back Screen
    experiment.
    
    Fixed: 407969608
    Change-Id: Ied9188dd578b98907a5094f98358150a844155bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437373
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Briana McClure <bmcclure@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446850}

--- ios/chrome/browser/first_run/ui_bundled/features.mm
+++ ios/chrome/browser/first_run/ui_bundled/features.mm
@@ -28,6 +28,10 @@ BASE_FEATURE(kUpdatedFirstRunSequence,
              "UpdatedFirstRunSequence",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kWelcomeBackInFirstRun,
+             "WelcomeBackInFirstRun",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 const char kAnimatedDefaultBrowserPromoInFREExperimentType[] =
     "AnimatedDefaultBrowserPromoInFREExperimentType";
 
@@ -35,6 +39,8 @@ const char kBestFeaturesScreenInFirstRunParam[] =
     "BestFeaturesScreenInFirstRunParam";
 
 const char kUpdatedFirstRunSequenceParam[] = "updated-first-run-sequence-param";
+
+const char kWelcomeBackInFirstRunParam[] = "WelcomeBackInFirstRunParam";
 
 BestFeaturesScreenVariationType GetBestFeaturesScreenVariationType() {
   if (!base::FeatureList::IsEnabled(kBestFeaturesScreenInFirstRun)) {
@@ -57,6 +63,15 @@ UpdatedFRESequenceVariationType GetUpdatedFRESequenceVariation(
   return static_cast<UpdatedFRESequenceVariationType>(
       base::GetFieldTrialParamByFeatureAsInt(kUpdatedFirstRunSequence,
                                              kUpdatedFirstRunSequenceParam, 1));
+}
+
+WelcomeBackScreenVariationType GetWelcomeBackScreenVariationType() {
+  if (!base::FeatureList::IsEnabled(kWelcomeBackInFirstRun)) {
+    return WelcomeBackScreenVariationType::kDisabled;
+  }
+  return static_cast<WelcomeBackScreenVariationType>(
+      base::GetFieldTrialParamByFeatureAsInt(kWelcomeBackInFirstRun,
+                                             kWelcomeBackInFirstRunParam, 1));
 }
 
 bool IsAnimatedDefaultBrowserPromoInFREEnabled() {
