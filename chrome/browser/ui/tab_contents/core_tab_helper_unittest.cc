0dc4282fb40350e51615a4c688a4602f7d6c7588
Jagadesh P <jagadeshjai1999@gmail.com>
2025-04-15 09:43:51 -0700

    Fix dangling pointer in |CoreTabHelperImageProcessingTest|
    Bug: 40212619
    Change-Id: I438b7a7e51d697f0c4633991dc58bddee36d9515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450497
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447181}

--- chrome/browser/ui/tab_contents/core_tab_helper_unittest.cc
+++ chrome/browser/ui/tab_contents/core_tab_helper_unittest.cc
@@ -19,8 +19,7 @@ namespace {
 class CoreTabHelperImageProcessingTest
     : public ChromeRenderViewHostTestHarness {
  public:
-  CoreTabHelperImageProcessingTest() : helper_(nullptr) {}
-
+  CoreTabHelperImageProcessingTest() = default;
   CoreTabHelperImageProcessingTest(const CoreTabHelperImageProcessingTest&) =
       delete;
   CoreTabHelperImageProcessingTest& operator=(
@@ -32,7 +31,6 @@ class CoreTabHelperImageProcessingTest
     ChromeRenderViewHostTestHarness::SetUp();
 
     CoreTabHelper::CreateForWebContents(web_contents());
-    helper_ = CoreTabHelper::FromWebContents(web_contents());
   }
 
   void DownscaleAndEncodeBitmapAndVerifyResponse(
@@ -71,11 +69,13 @@ class CoreTabHelperImageProcessingTest
     gfx::Size downscaled_size;
     int log_data_size;
     base::RunLoop run_loop;
-    helper_->DownscaleAndEncodeBitmap(
-        bitmap, thumbnail_min_size, thumbnail_max_width, thumbnail_max_height,
-        base::BindOnce(callback, &thumbnail_data, &content_type, &original_size,
-                       &downscaled_size, &log_data_size,
-                       run_loop.QuitClosure()));
+    CoreTabHelper::FromWebContents(web_contents())
+        ->DownscaleAndEncodeBitmap(
+            bitmap, thumbnail_min_size, thumbnail_max_width,
+            thumbnail_max_height,
+            base::BindOnce(callback, &thumbnail_data, &content_type,
+                           &original_size, &downscaled_size, &log_data_size,
+                           run_loop.QuitClosure()));
     run_loop.Run();
 
     EXPECT_EQ(downscaled_size, expected_downscaled_size);
@@ -103,9 +103,6 @@ class CoreTabHelperImageProcessingTest
       ASSERT_EQ(expected_downscaled_height, height);
     }
   }
-
- private:
-  raw_ptr<CoreTabHelper, DanglingUntriaged> helper_;
 };
 
 }  // namespace
