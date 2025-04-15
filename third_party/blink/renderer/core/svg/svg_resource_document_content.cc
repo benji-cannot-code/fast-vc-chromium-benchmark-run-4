8eea6907fdb2501b6e1c2760541c8f156fc0d730
Divyansh Mangal <dmangal@microsoft.com>
2025-04-15 03:11:24 -0700

    [SVG] Allow 'use' to reference an external document's root element.
    In this CL, we are implementing the spec [1] to streamline the
    SVG use element by loosening referencing requirements.
    With this change omitting fragments will automatically reference the
    root element, eliminating the need for authors to alter
    the referenced document just to assign an ID to the root.
    This enhancement simplifies the process and improves efficiency.
    
    To implement this, we first allow to fetch external resource even
    if fragment is not specified when href target is being parsed
    and then when the use element tries to resolve this target without
    the fragment specifier for the shadow tree creation we return
    back the SVG root element of the external document.
    
    [1] https://svgwg.org/svg2-draft/struct.html#UseElement
    
    This feature is being tracked in:
    https://chromestatus.com/feature/5128141573718016
    
    Bug: 40362369
    Change-Id: Id9e78a5a532c61c1d9f3c3feee47f81fb748788a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6370320
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1447006}

--- third_party/blink/renderer/core/svg/svg_resource_document_content.cc
+++ third_party/blink/renderer/core/svg/svg_resource_document_content.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/core/svg/svg_resource_document_cache.h"
 #include "third_party/blink/renderer/core/svg/svg_resource_document_observer.h"
+#include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 
@@ -233,6 +234,21 @@ SVGResourceTarget* SVGResourceDocumentContent::GetResourceTarget(
   }
   auto* svg_target =
       DynamicTo<SVGElement>(document->getElementById(element_id));
+  if (!svg_target) {
+    return nullptr;
+  }
+  return &svg_target->EnsureResourceTarget();
+}
+
+// TODO(dmangal): incorporate the below function into `GetResourceTarget`.
+SVGResourceTarget* SVGResourceDocumentContent::GetResourceTargetForRoot()
+    const {
+  Document* document = GetDocument();
+  if (!document) {
+    return nullptr;
+  }
+  auto* svg_target = DynamicTo<SVGSVGElement>(document->documentElement());
+
   if (!svg_target) {
     return nullptr;
   }
