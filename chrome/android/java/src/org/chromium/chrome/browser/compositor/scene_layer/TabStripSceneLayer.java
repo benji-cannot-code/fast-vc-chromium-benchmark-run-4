b9a0781846ecefddda34dcaf6d233fa8cd47e832
Zhe Li <zheliooo@google.com>
2025-04-15 13:56:39 -0700

    [TabGroupDragDrop] Add group title highlight for dragging collapsed group
    demo: https://drive.google.com/file/d/1-lxEjjGJqW00Tfz_vbkYdIM7AOhlG-CO/view?usp=sharing
    
    Bug: 410044835
    Change-Id: I5ac26a1380efe0e0bd751de7a51129c6cec03b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458279
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447370}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
@@ -297,7 +297,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                             TabStripSceneLayer.this,
                             gt.isIncognito(),
                             gt.isForegrounded(),
-                            gt.shouldShowReorderBackground(),
+                            gt.isCollapsed(),
                             gt.getNotificationBubbleShown(),
                             gt.getTabGroupId(),
                             gt.getTint(),
@@ -332,8 +332,8 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                 long nativeTabStripSceneLayer,
                 int reorderBackgroundTopMargin,
                 int reorderBackgroundBottomMargin,
-                int reorderBackgroundPaddingStart,
-                int reorderBackgroundPaddingEnd,
+                int reorderBackgroundPaddingShort,
+                int reorderBackgroundPaddingLong,
                 int reorderBackgroundCornerRadius);
 
         void beginBuildingFrame(
@@ -445,7 +445,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                 TabStripSceneLayer caller,
                 boolean incognito,
                 boolean foreground,
-                boolean showReorderBackground,
+                boolean collapsed,
                 boolean showBubble,
                 Token groupToken,
                 int tint,
--- chrome/browser/android/compositor/scene_layer/tab_strip_scene_layer.h
+++ chrome/browser/android/compositor/scene_layer/tab_strip_scene_layer.h
@@ -43,8 +43,8 @@ class TabStripSceneLayer : public SceneLayer {
   void SetConstants(JNIEnv* env,
                     jint reorder_background_top_margin,
                     jint reorder_background_bottom_margin,
-                    jint reorder_background_padding_start,
-                    jint reorder_background_padding_end,
+                    jint reorder_background_padding_short,
+                    jint reorder_background_padding_long,
                     jint reorder_background_corner_radius);
 
   void SetContentTree(
@@ -163,7 +163,7 @@ class TabStripSceneLayer : public SceneLayer {
       const base::android::JavaParamRef<jobject>& jobj,
       jboolean incognito,
       jboolean foreground,
-      jboolean show_reorder_background,
+      jboolean collapsed,
       jboolean show_bubble,
       const base::android::JavaParamRef<jobject>& jgroup_token,
       jint tint,
