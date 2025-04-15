b08c250df581dbb400f7f1594d60d0cff5354e26
Tsuyoshi Horo <horo@chromium.org>
2025-04-14 17:58:37 -0700

    Revert "Support All element type mode independently for timing predictor."
    This reverts commit 961525f3a6382e8086b47c6f7ec912228b1adbd8.
    
    Reason for revert: LCPPTimingPredictorBrowserFlagTest.WithKCriticalPathFlag is flaky.
    
    Bug: 40285771, 410658819
    Original change's description:
    > Support All element type mode independently for timing predictor.
    >
    > This CL has the timing predictor use `lcp_element_locator_stat_all`
    > instead of `lcp_element_locator_stat`, which was introduced in
    > "Add lcp_element_locator_stat_all in LcppStat":
    > https://chromium-review.googlesource.com/c/chromium/src/+/6396035
    > whereby the timing predictor can see elements of all type independently
    > from the kLCPCriticalPathPredictor flag.
    >
    > Bug: 40285771
    > Change-Id: I7a4c642b9d724fff367f3de2cee71a02b36be667
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420508
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    > Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1446387}
    
    Bug: 40285771
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ie468304b7c7d7bccda129497d7c6c4ce7dfeeb3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6456964
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1446879}

--- third_party/blink/renderer/core/frame/web_local_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_local_frame_impl.cc
@@ -3366,8 +3366,7 @@ void WebLocalFrameImpl::SetLCPPHint(
     return;
   }
 
-  lcpp->set_lcp_element_locators(hint->lcp_element_locators,
-                                 hint->lcp_element_locators_all);
+  lcpp->set_lcp_element_locators(hint->lcp_element_locators);
 
   HashSet<KURL> lcp_influencer_scripts;
   for (auto& url : hint->lcp_influencer_scripts) {
