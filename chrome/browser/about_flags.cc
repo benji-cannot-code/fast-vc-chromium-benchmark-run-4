e0062487d2c309151bbf0ab85599e0a5304fb188
Judith Hemp <hempjudith@google.com>
2025-04-15 10:40:30 -0700

    [Permissions AI] Add new feature flag for aiv3.
    Bug: 382447738
    Change-Id: I379da3cf574e9b21f8f18557247faa193ed166f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448982
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447224}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11135,6 +11135,9 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kPermissionsAIv1Description, kOsDesktop,
      FEATURE_VALUE_TYPE(permissions::features::kPermissionsAIv1)},
 
+    {"permissions-ai-v3", flag_descriptions::kPermissionsAIv3Name,
+     flag_descriptions::kPermissionsAIv3Description, kOsDesktop,
+     FEATURE_VALUE_TYPE(permissions::features::kPermissionsAIv3)},
 #if BUILDFLAG(IS_CHROMEOS)
     {"exclude-display-in-mirror-mode",
      flag_descriptions::kExcludeDisplayInMirrorModeName,
