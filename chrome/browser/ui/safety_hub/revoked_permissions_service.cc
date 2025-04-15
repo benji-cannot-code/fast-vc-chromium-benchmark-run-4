49dbdb74ad68852b357085208217caca9cfd4328
Tsuyoshi Horo <horo@chromium.org>
2025-04-14 21:45:10 -0700

    Revert "[DNS] Revoke disruptive notifications"
    This reverts commit 58c7782d46df5c1e06172bba951b28bbe668fc6e.
    
    Reason for revert: Made TestRevokeDisruptiveNotificationPermissions test flaky.
    
    Bug: 406475122, 410697652
    Original change's description:
    > [DNS] Revoke disruptive notifications
    >
    > Revoke disruptive notifications when the shadow run is false. Revocation
    > happens in two steps: 1. the url is marked as proposed; 2. the
    > notification is revoked. Two step revocation allows us to record UKM for
    > proposed revocations when the notification from the site is shown.
    >
    > Only revoked notifications are returned to RevokedPermissionsService.
    > Revoked notifications are merged with other revoked permissions into one
    > list. If there are both revoked unused permissions and revoked
    > disruptive notifications for one URL, they are merged into one entry.
    > Abusive and disruptive notification revocations cannot happen at the
    > same time, since as soon as the notification permission is revoked, the
    > other check won't be run for the URL.
    >
    > UI changes to support the new revocation type will be implemented in a
    > separate CL.
    >
    > Bug: 406475122
    > Change-Id: I922dd6c73a0ccefe2a7d5758d46941206a12916b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6428568
    > Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    > Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    > Auto-Submit: Olesia Marukhno <olesiamarukhno@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1446441}
    
    Bug: 406475122
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia2841d44535edc04648a3cfa985bae5c11ba9815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6457290
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446929}

--- chrome/browser/ui/safety_hub/revoked_permissions_service.cc
+++ chrome/browser/ui/safety_hub/revoked_permissions_service.cc
@@ -437,10 +437,9 @@ void RevokedPermissionsService::OnContentSettingChanged(
   // When permissions change for a pattern it is either (1) through resetting
   // permissions, e.g. in page info or site settings, (2) user modifying
   // permissions manually, (3) through auto-revocation of unused site
-  // permissions that this module performs, (4) through auto-revocation of
-  // abusive notifications that this module performs, or (5) through
-  // auto-revocation of disruptive notifications that this module performs. In
-  // (1) and (2) the pattern should no longer be shown to the user.
+  // permissions that this module performs, or (4) through auto-revocation of
+  // abusive notifications that this module performs. In (1) and (2) the
+  // pattern should no longer be shown to the user.
   bool should_clean_revoked_permission_data = true;
 
   // If `content_type_set` contains all types, all permissions are changed at
@@ -451,7 +450,6 @@ void RevokedPermissionsService::OnContentSettingChanged(
     switch (content_type_set.GetType()) {
       case ContentSettingsType::REVOKED_UNUSED_SITE_PERMISSIONS:
       case ContentSettingsType::REVOKED_ABUSIVE_NOTIFICATION_PERMISSIONS:
-      case ContentSettingsType::REVOKED_DISRUPTIVE_NOTIFICATION_PERMISSIONS:
         // If content setting is changed because of auto revocation, the revoked
         // permission data should not be deleted.
         should_clean_revoked_permission_data = false;
@@ -461,17 +459,13 @@ void RevokedPermissionsService::OnContentSettingChanged(
         // permissions data. However if the permission is changed because of
         // Safety Hub revocation, then the revoked permission data should not be
         // revoked.
-        const bool is_abusive_revocation_running =
+        bool is_abusive_revocation_running =
             IsAbusiveNotificationAutoRevocationEnabled()
                 ? abusive_notification_manager_->IsRevocationRunning()
                 : false;
-        const bool is_disruptive_revocation_running =
-            disruptive_notification_manager_
-                ? disruptive_notification_manager_->IsRevocationRunning()
-                : false;
         should_clean_revoked_permission_data =
             !is_unused_site_revocation_running &&
-            !is_abusive_revocation_running && !is_disruptive_revocation_running;
+            !is_abusive_revocation_running;
         break;
     }
   }
@@ -787,7 +781,7 @@ RevokedPermissionsService::GetRevokedPermissions() {
           chooser_permissions_data_dict->Clone();
     }
 
-    // If the origin has a revoked notification, add `NOTIFICATIONS` to
+    // If the origin has a revoked abusive notification, add `NOTIFICATIONS` to
     // the list of revoked permissions.
     const GURL& url = GURL(revoked_permissions.primary_pattern.ToString());
     if (safety_hub_util::IsUrlRevokedAbusiveNotification(hcsm(), url)) {
@@ -806,15 +800,6 @@ RevokedPermissionsService::GetRevokedPermissions() {
           info.metadata.expiration()) {
         permissions_data.constraints = GetConstraintFromInfo(info);
       }
-    } else if (safety_hub_util::IsUrlRevokedDisruptiveNotification(hcsm(),
-                                                                   url)) {
-      // If the origin has a revoked disruptive notification, add
-      // `NOTIFICATIONS` to the list of revoked permissions.
-      CHECK(disruptive_notification_manager_);
-      permissions_data.permission_types.insert(
-          static_cast<ContentSettingsType>(ContentSettingsType::NOTIFICATIONS));
-      // TODO(crbug.com/406473591): Add revocation type field.
-      // TODO(crbug.com/406473591): Update constraints to the latest one.
     }
 
     result->AddRevokedPermission(permissions_data);
@@ -844,33 +829,6 @@ RevokedPermissionsService::GetRevokedPermissions() {
         revoked_abusive_notification_permission.metadata.lifetime());
 
     result->AddRevokedPermission(permissions_data);
-  }
-
-  if (disruptive_notification_manager_) {
-    ContentSettingsForOneType revoked_disruptive_notifications =
-        disruptive_notification_manager_->GetRevokedNotifications();
-    for (const auto& permission : revoked_disruptive_notifications) {
-      // Skip origins with revoked unused site permissions, since these were
-      // handled above.
-      if (safety_hub_util::IsUrlRevokedUnusedSite(
-              hcsm(), GURL(permission.primary_pattern.ToString()))) {
-        continue;
-      }
-      CHECK(!safety_hub_util::IsUrlRevokedAbusiveNotification(
-          hcsm(), GURL(permission.primary_pattern.ToString())));
-      PermissionsData permissions_data;
-      permissions_data.primary_pattern = permission.primary_pattern;
-      permissions_data.permission_types.insert(
-          static_cast<ContentSettingsType>(ContentSettingsType::NOTIFICATIONS));
-
-      permissions_data.constraints =
-          content_settings::ContentSettingConstraints(
-              permission.metadata.expiration() -
-              permission.metadata.lifetime());
-      permissions_data.constraints.set_lifetime(permission.metadata.lifetime());
-
-      result->AddRevokedPermission(permissions_data);
-    }
   }
 
   return result;
@@ -1084,9 +1042,6 @@ RevokedPermissionsService::GetTrackedUnusedPermissionsForTesting() {
 
 void RevokedPermissionsService::SetClockForTesting(base::Clock* clock) {
   clock_ = clock;
-  if (disruptive_notification_manager_) {
-    disruptive_notification_manager_->SetClockForTesting(clock);  // IN-TEST
-  }
   if (IsAbusiveNotificationAutoRevocationEnabled()) {
     abusive_notification_manager_->SetClockForTesting(clock);  // IN-TEST
   }
