4e05209c0dcaafa3631ef7e40757ae789fcab3c0
Fernando Ramirez <ferny@google.com>
2025-04-15 14:02:48 -0700

    [Card Benefits][Curinos] Add flag for Curinos flat rate benefits
    This CL introduces a flag we will use to launch flat rate benefits
    sourced from Curinos.
    
    go/pay-autofill-chrome-scaling-benefits-design
    
    Bug: 404865672
    Change-Id: I54e266e62a0c66ea12e4cfc5fca2801988f2ff21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6435197
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447378}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -419,6 +419,9 @@ extern const char kAutofillEnableCardBenefitsSyncDescription[];
 extern const char kAutofillEnableCardInfoRuntimeRetrievalName[];
 extern const char kAutofillEnableCardInfoRuntimeRetrievalDescription[];
 
+extern const char kAutofillEnableFlatRateCardBenefitsFromCurinosName[];
+extern const char kAutofillEnableFlatRateCardBenefitsFromCurinosDescription[];
+
 extern const char kAutofillEnableLogFormEventsToAllParsedFormTypesName[];
 extern const char kAutofillEnableLogFormEventsToAllParsedFormTypesDescription[];
 
--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -64,6 +64,9 @@ extern const char kAutofillEnableDynamicallyLoadingFieldsForAddressInputName[];
 extern const char
     kAutofillEnableDynamicallyLoadingFieldsForAddressInputDescription[];
 
+extern const char kAutofillEnableFlatRateCardBenefitsFromCurinosName[];
+extern const char kAutofillEnableFlatRateCardBenefitsFromCurinosDescription[];
+
 extern const char kAutofillEnableLogFormEventsToAllParsedFormTypesName[];
 extern const char kAutofillEnableLogFormEventsToAllParsedFormTypesDescription[];
 
