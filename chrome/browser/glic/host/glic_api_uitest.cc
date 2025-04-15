6af5c42656f0e85cdedb9d65325b7ba99eafaa88
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2025-04-15 13:53:12 -0700

    gardener: Disable GlicApiTestWithFastTimeout.testNavigateToBadPage
    Test is timing out on macOS:
    
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Ainteractive_ui_tests%2FGlicApiTestWithFastTimeout.testNavigateToBadPage
    
    Bug: 410881522
    Change-Id: Ic3e25df18a2dad9a812f37cd2e3f9d07ae649d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6457895
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447364}

--- chrome/browser/glic/host/glic_api_uitest.cc
+++ chrome/browser/glic/host/glic_api_uitest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_features.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_service.h"
@@ -1021,7 +1022,14 @@ IN_PROC_BROWSER_TEST_F(GlicApiTest, testNavigateToDifferentClientPage) {
   ExecuteJsTest({.params = base::Value(1)});  // test run count: 1.
 }
 
-IN_PROC_BROWSER_TEST_F(GlicApiTestWithFastTimeout, testNavigateToBadPage) {
+// TODO(crbug.com/410881522): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_testNavigateToBadPage DISABLED_testNavigateToBadPage
+#else
+#define MAYBE_testNavigateToBadPage testNavigateToBadPage
+#endif
+IN_PROC_BROWSER_TEST_F(GlicApiTestWithFastTimeout,
+                       MAYBE_testNavigateToBadPage) {
 #if defined(SLOW_BINARY)
   GTEST_SKIP() << "skip timeout test for slow binary";
 #else
