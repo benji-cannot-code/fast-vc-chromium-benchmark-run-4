b5893dfb1ec0ba3c631c7124fd4d8a17110e7e21
Ian Kilpatrick <ikilpatrick@chromium.org>
2025-04-15 10:46:21 -0700

    [perf] Compress LayoutObject::style_
    As above. This doesn't appear to be necessary anymore, and might be
    marginally performance positive.
    
    Fuchsia-Binary-Size: Unavoidable - uncompressing style extra ops.
    Change-Id: I9e1ff2ce16ef7117e0cd9c6affe5ca6be386e29a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449540
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447230}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -4150,8 +4150,7 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
   friend class LineLayoutItem;
   friend class LocalFrameView;
 
-  subtle::UncompressedMember<const ComputedStyle> style_;
-
+  Member<const ComputedStyle> style_;
   Member<Node> node_;
 
   Member<LayoutObject> parent_;
