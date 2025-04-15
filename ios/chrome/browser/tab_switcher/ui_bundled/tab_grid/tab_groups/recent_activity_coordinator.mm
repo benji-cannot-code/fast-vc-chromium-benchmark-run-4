26ed6207b0bddb26949315ee3be6998b3794d8a5
Louis Romero <lpromero@google.com>
2025-04-15 06:18:32 -0700

    Fix typo in method case
    Bug: none
    Change-Id: Ib3feb5073929e72201643f98f3caf843bf531383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458877
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447056}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/recent_activity_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/recent_activity_coordinator.mm
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   ProfileIOS* profile = self.profile;
   _mediator = [[RecentActivityMediator alloc]
-            initWithtabGroup:_tabGroup
+            initWithTabGroup:_tabGroup
             messagingService:collaboration::messaging::
                                  MessagingBackendServiceFactory::GetForProfile(
                                      profile)
