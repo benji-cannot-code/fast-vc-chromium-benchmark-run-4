a73937bbdf07b94cc01e1f4a5175d13f422b1911
Marc Treib <treib@chromium.org>
2025-04-16 09:29:25 -0700

    Sync: Handle invalid metadata at ProcessorEntity creation
    Before this CL, ProcessorEntity::CreateFromMetadata() always returned
    an object, even if the passed-in metadata was inconsistent or
    corrupted.
    This CL introduces a MetadataIsValid() helper, and makes creation return
    null if the passed-in metadata is invalid.
    It also updates the two call sites to handle null being returned.
    
    Bug: 408182457
    Change-Id: I8828e7cc77a8eb43d77ab1dce6a0e7318f44b2ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450354
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447797}

--- components/sync/model/processor_entity.cc
+++ components/sync/model/processor_entity.cc
@@ -28,6 +28,11 @@ namespace syncer {
 
 namespace {
 
+bool MetadataIsValid(const sync_pb::EntityMetadata& metadata) {
+  return metadata.has_client_tag_hash() && metadata.has_creation_time() &&
+         metadata.sequence_number() >= metadata.acked_sequence_number();
+}
+
 std::string HashSpecifics(const sync_pb::EntitySpecifics& specifics) {
   DCHECK_GT(specifics.ByteSizeLong(), 0u);
   return base::Base64Encode(
@@ -51,6 +56,7 @@ std::unique_ptr<ProcessorEntity> ProcessorEntity::CreateNew(
   metadata.set_acked_sequence_number(0);
   metadata.set_server_version(kUncommittedVersion);
   metadata.set_creation_time(TimeToProtoTime(creation_time));
+  CHECK(MetadataIsValid(metadata));
 
   return base::WrapUnique(
       new ProcessorEntity(storage_key, std::move(metadata)));
@@ -60,6 +66,9 @@ std::unique_ptr<ProcessorEntity> ProcessorEntity::CreateFromMetadata(
     const std::string& storage_key,
     sync_pb::EntityMetadata metadata) {
   DCHECK(!storage_key.empty());
+  if (!MetadataIsValid(metadata)) {
+    return nullptr;
+  }
   return base::WrapUnique(
       new ProcessorEntity(storage_key, std::move(metadata)));
 }
@@ -69,8 +78,7 @@ ProcessorEntity::ProcessorEntity(const std::string& storage_key,
     : storage_key_(storage_key),
       metadata_(std::move(metadata)),
       commit_requested_sequence_number_(metadata_.acked_sequence_number()) {
-  DCHECK(metadata_.has_client_tag_hash());
-  DCHECK(metadata_.has_creation_time());
+  CHECK(MetadataIsValid(metadata_));
 }
 
 ProcessorEntity::~ProcessorEntity() = default;
