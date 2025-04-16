2cde5d13ce0efa947671e2267f14543f80f9f609
Benjamin Keen <bkeen@google.com>
2025-04-15 17:15:06 -0700

    [CHAP] Add BrowserInitiatedAutomaticPictureInPicture to chrome://flags
    Bug: 408502322, b/318568892
    Change-Id: I38ffd826498584c195f6a1118cd65513efc985de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437622
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447469}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3050,6 +3050,9 @@ extern const char kAllowAllSitesToInitiateMirroringDescription[];
 extern const char kAXTreeFixingName[];
 extern const char kAXTreeFixingDescription[];
 
+extern const char kBrowserInitiatedAutomaticPictureInPictureName[];
+extern const char kBrowserInitiatedAutomaticPictureInPictureDescription[];
+
 extern const char kDialMediaRouteProviderName[];
 extern const char kDialMediaRouteProviderDescription[];
 extern const char kDelayMediaSinkDiscoveryName[];
