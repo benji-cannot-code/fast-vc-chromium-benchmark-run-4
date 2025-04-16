7ebe9aba9974dfd3827b337c19f0e1a846d6e0fd
Etienne Bergeron <etienneb@chromium.org>
2025-04-16 11:36:54 -0700

    Fix UaF while scanning browser list
    This CL is fixing a UaF caused by a vector resize while
    scanning the content of the vector.
    
    The broken stack is:
    
      BrowserList::AddBrowser   <<<--- Modifying the list
      Browser::Browser
      Browser::Create
      DevToolsWindow::CreateDevToolsBrowser
      DevToolsWindow::Show
      ScheduleShow
      DevToolsWindow::ToggleDevToolsWindow
      DevToolsWindow::OpenDevToolsWindow
      DevToolsAutoOpener::OnTabStripModelChanged
      BrowserTabStripTracker::MaybeTrackBrowser
      BrowserTabStripTracker::Init   <<<-- List scanning
    
    Bug: 405910169
    Change-Id: Id1273e6c1ebaa6adac0149edab0032552b971373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6431316
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447899}

--- chrome/browser/ui/browser_list.h
+++ chrome/browser/ui/browser_list.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_set.h"
 #include "base/functional/callback_forward.h"
+#include "base/functional/function_ref.h"
 #include "base/lazy_instance.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
@@ -74,6 +75,13 @@ class BrowserList {
   size_t size() const { return browsers_.size(); }
 
   Browser* get(size_t index) const { return browsers_[index]; }
+
+  // Enumerate the current browser and the new browser in-order.
+  void ForEachCurrentAndNewBrowser(
+      base::FunctionRef<void(Browser*)> on_browser);
+
+  // Enumerate the current browser in-order.
+  void ForEachCurrentBrowser(base::FunctionRef<void(Browser*)> on_browser);
 
   // Returns iterated access to list of open browsers ordered by activation. The
   // underlying data structure is a vector and we push_back on recent access so
