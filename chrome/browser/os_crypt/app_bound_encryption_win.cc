3e3e15466ecc3ea211b2236252a720d627c76463
Will Harris <wfh@chromium.org>
2025-04-16 14:21:28 -0700

    Add Mocks for App-Bound Encryption
    These mocks allow some future functionality to be more easily unit
    tested without needing to have a fake system-level install and use
    browser tests.
    
    This also adds a small unit test to verify that the mocks are working.
    
    BUG=403517819
    
    Change-Id: Ib5c8bcddc12d631dbf5c077187a742cb4535a94a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6370664
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448013}

--- chrome/browser/os_crypt/app_bound_encryption_win.cc
+++ chrome/browser/os_crypt/app_bound_encryption_win.cc
@@ -35,6 +35,8 @@ namespace os_crypt {
 
 namespace {
 
+AppBoundEncryptionOverridesForTesting* g_overrides_for_testing = nullptr;
+
 ProtectionLevel AddFlags(ProtectionLevel protection_level,
                          elevation_service::EncryptFlags flags) {
   // Check protection_level fits into 8-bits.
@@ -62,6 +64,11 @@ BASE_FEATURE(kAppBoundDataReencrypt,
 }  // namespace features
 
 SupportLevel GetAppBoundEncryptionSupportLevel(PrefService* local_state) {
+  if (g_overrides_for_testing) {
+    return g_overrides_for_testing->GetAppBoundEncryptionSupportLevel(
+        local_state);
+  }
+
   // Must be a system install.
   if (!install_static::IsSystemInstall()) {
     return SupportLevel::kNotSystemLevel;
@@ -150,6 +157,11 @@ HRESULT EncryptAppBoundString(ProtectionLevel protection_level,
                               std::string& ciphertext,
                               DWORD& last_error,
                               elevation_service::EncryptFlags* flags) {
+  if (g_overrides_for_testing) {
+    return g_overrides_for_testing->EncryptAppBoundString(
+        protection_level, plaintext, ciphertext, last_error, flags);
+  }
+
   base::win::AssertComInitialized();
   Microsoft::WRL::ComPtr<IElevator> elevator;
   last_error = ERROR_GEN_FAILURE;
@@ -194,6 +206,12 @@ HRESULT DecryptAppBoundString(const std::string& ciphertext,
                               std::optional<std::string>& new_ciphertext,
                               DWORD& last_error,
                               elevation_service::EncryptFlags* flags) {
+  if (g_overrides_for_testing) {
+    return g_overrides_for_testing->DecryptAppBoundString(
+        ciphertext, plaintext, protection_level, new_ciphertext, last_error,
+        flags);
+  }
+
   DCHECK(!ciphertext.empty());
   base::win::AssertComInitialized();
   Microsoft::WRL::ComPtr<IElevator> elevator;
@@ -250,6 +268,11 @@ HRESULT DecryptAppBoundString(const std::string& ciphertext,
 
   last_error = ERROR_SUCCESS;
   return S_OK;
+}
+
+void SetOverridesForTesting(AppBoundEncryptionOverridesForTesting* overrides) {
+  CHECK(!g_overrides_for_testing || !overrides);
+  g_overrides_for_testing = overrides;
 }
 
 }  // namespace os_crypt
