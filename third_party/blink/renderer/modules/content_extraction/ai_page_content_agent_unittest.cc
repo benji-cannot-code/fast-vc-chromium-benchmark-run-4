1422d5f03cb2550e91fb3d2dff06f6e49fd29721
Khushal Sagar <khushalsagar@chromium.org>
2025-04-16 15:01:30 -0700

    Reapply "blink: Add SVG text to AIPageContent"
    This reverts commit 05bd33bb04b998f864eb94d3b137e6c1ccf091c4.
    
    It's possible for inner text to be null. Make it optional and add a test
    case. Also ensure all tests go through mojom serialization to catch such
    errors.
    
    Bug: 383538166
    Change-Id: I8aeea47cb2ea3e44dd1a92ae6b9c1bc3655ea9a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6464192
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448027}

--- third_party/blink/renderer/modules/content_extraction/ai_page_content_agent_unittest.cc
+++ third_party/blink/renderer/modules/content_extraction/ai_page_content_agent_unittest.cc
@@ -262,8 +262,16 @@ class AIPageContentAgentTest : public testing::Test {
         *helper_.LocalMainFrame()->GetFrame()->GetDocument());
     EXPECT_TRUE(agent);
 
-    return agent->GetAIPageContentInternal(options ? *options
-                                                   : default_options_);
+    auto content =
+        agent->GetAIPageContentInternal(options ? *options : default_options_);
+
+    // Always validate serialization.
+    mojom::blink::AIPageContentPtr output;
+    EXPECT_TRUE(
+        mojo::test::SerializeAndDeserialize<mojom::blink::AIPageContent>(
+            content, output));
+
+    return content;
   }
 
   void FireMouseMoveEvent(const gfx::PointF& point) {
@@ -396,10 +404,7 @@ TEST_F(AIPageContentAgentTest, ImageNoAltText) {
       url_test_helpers::ToKURL("http://foobar.com"));
 
   auto content = GetAIPageContent();
-
-  mojom::blink::AIPageContentPtr output;
-  ASSERT_TRUE(mojo::test::SerializeAndDeserialize<mojom::blink::AIPageContent>(
-      content, output));
+  ASSERT_TRUE(content);
 }
 
 TEST_F(AIPageContentAgentTest, Headings) {
@@ -3431,6 +3436,52 @@ TEST_F(AIPageContentAgentTest, LabelWithForDescendant) {
             input.content_attributes->dom_node_id);
 
   CheckTextNode(*label.children_nodes[1], "Check me!");
+}
+
+TEST_F(AIPageContentAgentTest, SVG) {
+  frame_test_helpers::LoadHTMLString(
+      helper_.LocalMainFrame(),
+      "<body>"
+      "  <svg width='400' height='200'>"
+      "    <text x='50%' y='50/%' font-size='24'>"
+      "      Hello SVG Text!"
+      "    </text>"
+      "  </svg>"
+      "</body>",
+      url_test_helpers::ToKURL("http://foobar.com"));
+
+  auto content = GetAIPageContent();
+  ASSERT_TRUE(content);
+  ASSERT_TRUE(content->root_node);
+
+  const auto& svg = *content->root_node->children_nodes[0];
+  EXPECT_EQ(svg.content_attributes->attribute_type,
+            mojom::blink::AIPageContentAttributeType::kSVG);
+  ASSERT_TRUE(svg.content_attributes->svg_data);
+  EXPECT_EQ(svg.content_attributes->svg_data->inner_text, "Hello SVG Text!");
+}
+
+TEST_F(AIPageContentAgentTest, SVGWithNoText) {
+  frame_test_helpers::LoadHTMLString(
+      helper_.LocalMainFrame(),
+      "<body>"
+      "  <svg width='400' height='200' style='content-visibility: hidden'>"
+      "    <text x='50%' y='50/%' font-size='24'>"
+      "      Hello SVG Text!"
+      "    </text>"
+      "  </svg>"
+      "</body>",
+      url_test_helpers::ToKURL("http://foobar.com"));
+
+  auto content = GetAIPageContent();
+  ASSERT_TRUE(content);
+  ASSERT_TRUE(content->root_node);
+
+  const auto& svg = *content->root_node->children_nodes[0];
+  EXPECT_EQ(svg.content_attributes->attribute_type,
+            mojom::blink::AIPageContentAttributeType::kSVG);
+  ASSERT_TRUE(svg.content_attributes->svg_data);
+  EXPECT_FALSE(svg.content_attributes->svg_data->inner_text);
 }
 
 }  // namespace
