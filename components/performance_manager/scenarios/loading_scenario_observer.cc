6f53f394f713b8261896e2962c8cb86f2138df9c
Joe Mason <joenotcharles@google.com>
2025-04-16 14:45:03 -0700

    Add a "PageNode" tracing track to PageNodeImpl
    The track is named "PageNode" so that other events can be added without
    changing the name, and nested under a collapsible "PageNodes" track.
    
    Moves "PageNode Loading" trace events to this track and puts them in the
    "loading" category since they're no longer emitted as part of scenario
    detection.
    
    R=etiennep
    
    Change-Id: I8fa301705120428c34794c3cbc133b86d1d6eb28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6446796
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448024}

--- components/performance_manager/scenarios/loading_scenario_observer.cc
+++ components/performance_manager/scenarios/loading_scenario_observer.cc
@@ -13,16 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/numerics/checked_math.h"
 #include "base/sequence_checker.h"
-#include "base/trace_event/typed_macros.h"
-#include "base/unguessable_token.h"
 #include "components/performance_manager/graph/graph_impl.h"
 #include "components/performance_manager/graph/page_node_impl.h"
 #include "components/performance_manager/graph/process_node_impl.h"
 #include "components/performance_manager/scenario_api/performance_scenarios.h"
 #include "components/performance_manager/scenarios/browser_performance_scenarios.h"
 #include "components/performance_manager/scenarios/loading_scenario_data.h"
-#include "third_party/perfetto/include/perfetto/tracing/tracing.h"
-#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace performance_manager {
 
@@ -52,32 +48,6 @@ LoadingScenario CalculateLoadingScenario(const LoadingScenarioCounts& counts) {
     return LoadingScenario::kBackgroundPageLoading;
   }
   return LoadingScenario::kNoPageLoading;
-}
-
-void RecordLoadingTraceEvent(const PageNode* page_node,
-                             bool was_loading,
-                             bool is_loading) {
-  static perfetto::NamedTrack loading_state_track = [] {
-    // Each PageNode gets its own track, which is grouped under this track.
-    perfetto::NamedTrack track{"PageNodeLoadingState", 0,
-                               perfetto::Track::Global(0)};
-    if (perfetto::Tracing::IsInitialized()) {
-      // Because the track doesn't get any events of its own it must manually
-      // emit the track descriptor. SetTrackDescriptor may crash in unit tests
-      // where tracing isn't initialized.
-      base::TrackEvent::SetTrackDescriptor(track, track.Serialize());
-    }
-    return track;
-  }();
-  const base::UnguessableToken page_token =
-      PageNodeImpl::FromNode(page_node)->page_token().value();
-  perfetto::Track page_track(base::UnguessableTokenHash()(page_token),
-                             loading_state_track);
-  if (is_loading && !was_loading) {
-    TRACE_EVENT_BEGIN("performance_scenarios", "PageNode Loading", page_track);
-  } else if (was_loading && !is_loading) {
-    TRACE_EVENT_END("performance_scenarios", page_track);
-  }
 }
 
 }  // namespace
@@ -147,8 +117,6 @@ void LoadingScenarioObserver::OnBeforePageNodeRemoved(
     DecrementLoadingCounts(process_nodes, page_node->IsVisible(),
                            page_node->IsFocused());
     UpdateLoadingScenarios(process_nodes);
-    RecordLoadingTraceEvent(page_node, /*was_loading=*/true,
-                            /*is_loading=*/false);
   }
 }
 
@@ -198,7 +166,6 @@ void LoadingScenarioObserver::OnLoadingStateChanged(
                              page_node->IsFocused());
     }
     UpdateLoadingScenarios(process_nodes);
-    RecordLoadingTraceEvent(page_node, was_loading, is_loading);
   }
 }
 
