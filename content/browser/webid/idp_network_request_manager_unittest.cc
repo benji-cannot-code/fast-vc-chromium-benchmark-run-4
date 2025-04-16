da561c74946a78f89e9ba266e8eb3e0cf5320d5b
Christian Biesinger <cbiesinger@chromium.org>
2025-04-16 11:28:47 -0700

    [FedCM] Enable new syntax
    This is for "use other account" and account labels.
    
    Bug: 404568028
    Change-Id: Ied5bac088c8eb9a144de48e5c2d808e0e196d6da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6460742
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447892}

--- content/browser/webid/idp_network_request_manager_unittest.cc
+++ content/browser/webid/idp_network_request_manager_unittest.cc
@@ -682,6 +682,9 @@ TEST_F(IdpNetworkRequestManagerTest, ParseAccountMalformed) {
 }
 
 TEST_F(IdpNetworkRequestManagerTest, ParseAccountLabelsOldSyntax) {
+  base::test::ScopedFeatureList list;
+  list.InitAndDisableFeature(features::kFedCmUseOtherAccountAndLabelsNewSyntax);
+
   // New syntax should be ignored with the flag disabled.
   const auto* test_accounts_json = R"({
   "accounts" : [
@@ -1294,7 +1297,7 @@ TEST_F(IdpNetworkRequestManagerTest, ParseConfigBrandingIconReltivePath) {
 TEST_F(IdpNetworkRequestManagerTest,
        ParseConfigSupportsOtherAccountActiveMode) {
   base::test::ScopedFeatureList list;
-  list.InitAndEnableFeature(features::kFedCmUseOtherAccount);
+  list.InitAndDisableFeature(features::kFedCmUseOtherAccountAndLabelsNewSyntax);
 
   const char test_json[] = R"({
   "modes": {
@@ -1318,7 +1321,7 @@ TEST_F(IdpNetworkRequestManagerTest,
 TEST_F(IdpNetworkRequestManagerTest,
        ParseConfigSupportsOtherAccountPassiveMode) {
   base::test::ScopedFeatureList list;
-  list.InitAndEnableFeature(features::kFedCmUseOtherAccount);
+  list.InitAndDisableFeature(features::kFedCmUseOtherAccountAndLabelsNewSyntax);
 
   // The toplevel field should be ignored with the flag disabled.
   const char test_json[] = R"({
@@ -1503,6 +1506,9 @@ TEST_F(IdpNetworkRequestManagerTest,
 }
 
 TEST_F(IdpNetworkRequestManagerTest, ParseConfigRequestedLabelOldSyntax) {
+  base::test::ScopedFeatureList list;
+  list.InitAndDisableFeature(features::kFedCmUseOtherAccountAndLabelsNewSyntax);
+
   // New syntax should be ignored with flag disabled.
   const char test_json[] = R"({
     "account_label": "l1",
