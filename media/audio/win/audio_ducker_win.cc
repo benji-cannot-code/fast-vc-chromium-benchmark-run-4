a946035e1a4443fd03371337e70e541e6fed5bb3
Sunggook Chue <sunggch@microsoft.com>
2025-04-15 17:35:17 -0700

    Uses the singleton object of IMMDeviceEnumerator.
    On Windows, the MMDeviceEnumerator is implemented as an internal singleton,
    so clients like Chromium can safely cache the instance without worrying
    about it becoming stale.
    
    Bug: 378507951
    Change-Id: Ie4fc4770a7422bbba9c73c42cfd88ddc61ae2da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6451872
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1447480}

--- media/audio/win/audio_ducker_win.cc
+++ media/audio/win/audio_ducker_win.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/win/scoped_co_mem.h"
+#include "media/audio/win/core_audio_util_win.h"
 #include "media/base/media_switches.h"
 
 using Microsoft::WRL::ComPtr;
@@ -26,14 +27,15 @@ namespace {
 bool ForEachAudioSession(
     base::RepeatingCallback<void(ComPtr<IAudioSessionControl2>& session)>
         callback) {
-  ComPtr<IMMDeviceEnumerator> device_enumerator;
-  HRESULT hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr,
-                                CLSCTX_ALL, IID_PPV_ARGS(&device_enumerator));
-  if (!SUCCEEDED(hr)) {
+  ComPtr<IMMDeviceEnumerator> device_enumerator =
+      media::CoreAudioUtil::CreateDeviceEnumerator();
+
+  if (!device_enumerator) {
     return false;
   }
   ComPtr<IMMDevice> device;
-  hr = device_enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &device);
+  HRESULT hr =
+      device_enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &device);
   if (!SUCCEEDED(hr)) {
     return false;
   }
