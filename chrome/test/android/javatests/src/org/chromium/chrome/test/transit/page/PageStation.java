55f7f4a319cfac4ca2e82d79bd0bb45c2830bf44
Henrique Nakashima <hnakashima@chromium.org>
2025-04-16 10:57:52 -0700

    [Android] Create trigger methods in ViewElement
    Instead of triggering transitions through ViewSpecs, use ViewElements.
    
    The ViewElements will check if they are in the active state and this
    allows removing the static declarations of ViewSpecs in almost all cases.
    
    Bug: 406325287
    Change-Id: Ieedfc0256b32d020fb0c5039856e2d95606cebbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6459120
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447858}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/page/PageStation.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/page/PageStation.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.test.transit.page;
 
-import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import static org.chromium.base.test.transit.Condition.whether;
@@ -62,6 +61,7 @@ import java.util.function.Function;
  * the expected title, etc.
  */
 public class PageStation extends Station<ChromeTabbedActivity> {
+
     /**
      * Builder for all PageStation subclasses.
      *
@@ -164,15 +164,13 @@ public class PageStation extends Station<ChromeTabbedActivity> {
     protected final String mExpectedUrlSubstring;
     protected final String mExpectedTitle;
 
-    public static final ViewSpec TOOLBAR = viewSpec(withId(R.id.control_container));
-    public static final ViewSpec HOME_BUTTON = viewSpec(withId(R.id.home_button));
-    public static final ViewSpec URL_BAR = viewSpec(withId(R.id.url_bar));
-    public static final ViewSpec TAB_SWITCHER_BUTTON = viewSpec(withId(R.id.tab_switcher_button));
-    public static final ViewSpec MENU_BUTTON = viewSpec(withId(R.id.menu_button));
+    protected static final ViewSpec URL_BAR = viewSpec(withId(R.id.url_bar));
     protected Supplier<Tab> mActivityTabSupplier;
     protected Supplier<Tab> mSelectedTabSupplier;
     protected Element<Tab> mPageLoadedSupplier;
-    private ViewElement mToolbar;
+    protected ViewElement mToolbar;
+    protected ViewElement mTabSwitcherButton;
+    protected ViewElement mMenuButton;
 
     /** Prefer the PageStation's subclass |newBuilder()|. */
     public static Builder<PageStation> newGenericBuilder() {
@@ -230,10 +228,16 @@ public class PageStation extends Station<ChromeTabbedActivity> {
         // since they unintentionally still exist in the non-Hub tab switcher. They are mostly
         // occluded by the tab switcher toolbar, but at least the tab_switcher_button is still
         // visible.
-        mToolbar = elements.declareView(TOOLBAR, ViewElement.unscopedOption());
-        elements.declareView(HOME_BUTTON, ViewElement.unscopedOption());
-        elements.declareView(TAB_SWITCHER_BUTTON, ViewElement.unscopedOption());
-        elements.declareView(MENU_BUTTON, ViewElement.unscopedOption());
+        mToolbar =
+                elements.declareView(
+                        viewSpec(withId(R.id.control_container)), ViewElement.unscopedOption());
+        elements.declareView(viewSpec(withId(R.id.home_button)), ViewElement.unscopedOption());
+        mTabSwitcherButton =
+                elements.declareView(
+                        viewSpec(withId(R.id.tab_switcher_button)), ViewElement.unscopedOption());
+        mMenuButton =
+                elements.declareView(
+                        viewSpec(withId(R.id.menu_button)), ViewElement.unscopedOption());
 
         if (mNumTabsBeingOpened > 0) {
             elements.declareEnterCondition(
@@ -334,15 +338,15 @@ public class PageStation extends Station<ChromeTabbedActivity> {
     public TabSwitcherActionMenuFacility openTabSwitcherActionMenu() {
         recheckActiveConditions();
         return enterFacilitySync(
-                new TabSwitcherActionMenuFacility(),
-                () -> TAB_SWITCHER_BUTTON.perform(longClick()));
+                new TabSwitcherActionMenuFacility(), mTabSwitcherButton.longClickTrigger());
     }
 
     /** Opens the app menu by pressing the toolbar "..." button */
     public PageAppMenuFacility<PageStation> openGenericAppMenu() {
         recheckActiveConditions();
 
-        return enterFacilitySync(new PageAppMenuFacility<PageStation>(), MENU_BUTTON::click);
+        return enterFacilitySync(
+                new PageAppMenuFacility<PageStation>(), mMenuButton.clickTrigger());
     }
 
     /** Shortcut to open a new tab programmatically as if selecting "New Tab" from the app menu. */
@@ -399,7 +403,7 @@ public class PageStation extends Station<ChromeTabbedActivity> {
         assert !mIncognito;
         return travelToSync(
                 RegularTabSwitcherStation.from(getActivity().getTabModelSelector()),
-                TAB_SWITCHER_BUTTON::click);
+                mTabSwitcherButton.clickTrigger());
     }
 
     /** Opens the incognito tab switcher by pressing the toolbar tab switcher button. */
@@ -407,7 +411,7 @@ public class PageStation extends Station<ChromeTabbedActivity> {
         assert mIncognito;
         return travelToSync(
                 IncognitoTabSwitcherStation.from(getActivity().getTabModelSelector()),
-                TAB_SWITCHER_BUTTON::click);
+                mTabSwitcherButton.clickTrigger());
     }
 
     /** Loads a |url| in the same tab and waits to transition. */
