dff55e000f032c82e06b296388ea151a27069c8e
Ivan Murashov <ivan.murashov@lge.com>
2025-04-16 11:11:53 -0700

    IWYU: Add missing include for base::StringPrintf usage in interest_group_auction_reporter.cc
    In the file interest_group_auction_reporter.cc the method
    base::StringPrintf is used without corresponding include.
    Added missing include.
    
    Bug: none
    Change-Id: I7044d246016dcdf2df1be4243a63473867f1d911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458700
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447872}

--- content/browser/interest_group/interest_group_auction_reporter.cc
+++ content/browser/interest_group/interest_group_auction_reporter.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/rand_util.h"
 #include "base/strings/strcat.h"
+#include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "base/trace_event/trace_event.h"
