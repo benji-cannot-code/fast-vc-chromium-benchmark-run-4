2c3a1e9244f9971dc75f0701c18b864e276aca6c
Gyuyoung Kim <gyuyoung@igalia.com>
2025-04-15 18:56:45 -0700

    [tvOS] Exclude ChildProcessTaskPortProviderMac from the build
    ChildProcessTaskPortProvider is used to maintain an association
    between a handle and a process's task port. However, tvOS only
    supports a single-process mode, making this functionality
    unnecessary. This CL removes related files and usage from the
    tvOS build.
    
    Bug: 391914246
    Change-Id: I777f2c89d1541ea17627f043125668718a193824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450051
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447509}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -240,7 +240,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/oop_video_decoder_factory.h"
 #endif
 
-#if BUILDFLAG(IS_APPLE)
+#if BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_IOS_TVOS)
 #include "content/browser/child_process_task_port_provider_mac.h"
 #endif
 
@@ -5302,7 +5302,7 @@ uint64_t RenderProcessHostImpl::GetPrivateMemoryFootprint() {
   auto dump = memory_instrumentation::mojom::RawOSMemDump::New();
   dump->platform_private_footprint =
       memory_instrumentation::mojom::PlatformPrivateFootprint::New();
-#if BUILDFLAG(IS_APPLE)
+#if BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_IOS_TVOS)
   bool success = memory_instrumentation::OSMetrics::FillOSMemoryDump(
       GetProcess().Handle(), ChildProcessTaskPortProvider::GetInstance(),
       dump.get());
