23097baf266df8837080ecd127698972f1750db2
Tom Sepez <tsepez@chromium.org>
2025-04-16 11:18:30 -0700

    Use newly introdued base::RemovePrefix().
    Code now avoids calls strlen(), which may someday be considered
    an unsafe libc function.
    
    This CL was uploaded by git cl split.
    
    R=julietlevesque@google.com
    
    Change-Id: I3ae037182a49c8bcd786aba9b01a229e1439bccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6463235
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447880}

--- ash/quick_pair/companion_app/companion_app_parser.cc
+++ ash/quick_pair/companion_app/companion_app_parser.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
 #include "components/cross_device/logging/logging.h"
 
 namespace {
@@ -53,14 +54,13 @@ void CompanionAppParser::OnDeviceMetadataRetrieved(
 
   const std::string intent_uri_from_metadata =
       device_metadata->GetDetails().intent_uri();
-  if (intent_uri_from_metadata.find(kIntentKeyPrefix) != 0) {
+  std::optional<std::string_view> remainder =
+      base::RemovePrefix(intent_uri_from_metadata, kIntentKeyPrefix);
+  if (!remainder) {
     std::move(callback).Run(std::nullopt);
     return;
   }
-
-  std::optional<std::string> result = GetCompanionAppExtra(
-      intent_uri_from_metadata.substr(strlen(kIntentKeyPrefix)));
-  std::move(callback).Run(result);
+  std::move(callback).Run(GetCompanionAppExtra(std::string(*remainder)));
 }
 
 std::optional<std::string> CompanionAppParser::GetCompanionAppExtra(
