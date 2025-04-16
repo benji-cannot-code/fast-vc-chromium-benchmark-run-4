dc2ea12ffd12eb0ed44f6459b622b7d94d22f455
Mohannad Farrag <aymanm@google.com>
2025-04-16 07:52:36 -0700

    Separate JniAndroid from base_java
    Cronet has to explicitly list all of its dependencies in order to
    package them in a JAR. Usually we depend on the fact that Proguard will
    complain if there's any missing java class. However, this is not always
    the case especially when the java class is used only by native code (JNI).
    
    So in order to fix this without depending on base again, I separated the JniAndroid into its separate target.
    
    Bug: 410846554
    Change-Id: Ic335c4142fb5d9fbf0ecf4989ef22b94fa3ee0bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458680
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447727}

--- base/android/jni_android.cc
+++ base/android/jni_android.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/jni_zero/jni_zero.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "base/base_minimal_jni/JniAndroid_jni.h"
+#include "base/jni_android_jni/JniAndroid_jni.h"
 
 namespace base {
 namespace android {
