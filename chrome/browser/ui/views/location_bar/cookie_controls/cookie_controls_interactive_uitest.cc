0541d88c0eca6f1cc193b43858c619ef3b20c3f4
Dana Fried <dfried@chromium.org>
2025-04-16 14:34:35 -0700

    Reland "Fix bug in CookieControlsIconView that was causing test failures"
    This reverts commit a14833742ab792e5b440057a78aa9fb3ba29c4e1.
    
    Reason for revert: Leave test disabled. The logic still needs to be fixed.
    
    Bug: b:399817291
    Original change's description:
    > Revert "Fix bug in CookieControlsIconView that was causing test failures"
    >
    > This reverts commit 148070f72fd2fe7f72a2868919355c8b73c0b828.
    >
    > Reason for revert: Causing flaky tests in b/399817291
    >
    > Original change's description:
    > > Fix bug in CookieControlsIconView that was causing test failures
    > >
    > > Previous code had delayed starting animation until the system was sure
    > > an IPH would not be displayed, but there were other code paths that
    > > could start the animation while it was queued.
    > >
    > > This has been fixed with a check for a queued IPH and the affected tests
    > > re-enabled.
    > >
    > > Bug: 398716530
    > > Change-Id: I68a11edcfbbe8632a9094a2bf6717aba361b11be
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304101
    > > Commit-Queue: Emily Shack <emshack@chromium.org>
    > > Auto-Submit: Dana Fried <dfried@chromium.org>
    > > Reviewed-by: Emily Shack <emshack@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1425261}
    >
    > Bug: b:399817291
    > Change-Id: Ie0bfd9f982844fabf037796e0c832fef7e438657
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6439310
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1444762}
    
    Bug: b:399817291
    Change-Id: I0b0abf7e653c58798a1a9f72f837fc35326faa24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6462468
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Michelle Abreo <michelleabreo@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448017}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_interactive_uitest.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_interactive_uitest.cc
@@ -417,9 +417,8 @@ class CookieControlsWithIphUiTest : public CookieControlsInteractiveTestBase {
   ~CookieControlsWithIphUiTest() override = default;
 };
 
-// TODO(crbug.com/398716530): Re-enable this test.
 IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest,
-                       DISABLED_ShowAndDismissIphOnHighSiteEngagement) {
+                       ShowAndDismissIphOnHighSiteEngagement) {
   BlockThirdPartyCookies();
   SetHighSiteEngagement();
   RunTestSequence(
