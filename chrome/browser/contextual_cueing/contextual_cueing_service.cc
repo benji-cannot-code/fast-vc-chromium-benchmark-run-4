3ec6ba7bc13dc1a7b1ddbc3d0c3ed6f4e2207ec8
Zekun Jiang <zekunjiang@google.com>
2025-04-16 10:45:19 -0700

    Add hints path to zero state suggestions.
    Bug: 409329159, 405988283
    Change-Id: I8e2b07844aacea0ce063366f777adf1135f1cc39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448909
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447851}

--- chrome/browser/contextual_cueing/contextual_cueing_service.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_service.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/glic_nudge_controller.h"
 #include "chrome/common/buildflags.h"
 #include "components/optimization_guide/core/optimization_guide_switches.h"
+#include "components/optimization_guide/proto/hints.pb.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/network_anonymization_key.h"
@@ -76,6 +77,12 @@ ContextualCueingService::ContextualCueingService(
   CHECK(base::FeatureList::IsEnabled(contextual_cueing::kContextualCueing) ||
         base::FeatureList::IsEnabled(
             contextual_cueing::kGlicZeroStateSuggestions));
+  if (optimization_guide_keyed_service_ &&
+      base::FeatureList::IsEnabled(
+          contextual_cueing::kGlicZeroStateSuggestions)) {
+    optimization_guide_keyed_service_->RegisterOptimizationTypes(
+        {optimization_guide::proto::GLIC_ZERO_STATE_SUGGESTIONS});
+  }
 
   if (kEnablePageContentExtraction.Get() && page_content_extraction_service_) {
     page_content_extraction_service_->AddObserver(this);
@@ -231,8 +238,6 @@ void ContextualCueingService::GetContextualGlicZeroStateSuggestions(
     std::move(callback).Run(std::nullopt);
     return;
   }
-
-  // TODO(crbug.com/405988283): Add branch for hints suggestions.
 
   // Remote suggestions generation.
   ZeroStateSuggestionsPageData* page_data =
