3a1d09a6e39a6bbe331d28f5de4e37e99d294a6d
Elly Fong-Jones <ellyjones@google.com>
2025-04-16 08:22:48 -0700

    Update AppleKeychain API to be friendlier
    Use `std::string_view` and `base::span` in place of pointer / length
    pairs. `FindGenericPassword` now returns a `base::expected` containing
    either the password data as a `std::vector<uint8_t>` or an `OSStatus`
    error.
    
    The previous API was a close match to `SecKeychain`, but with the
    migration away from `SecKeychain` to `SecItem`, there's no reason to
    preserve those idiosyncrasies.
    
    Bug: 395659797
    Change-Id: I6a6a210067eb7eb930f0fd98c93f17ae586a07da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6443472
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447756}

--- components/os_crypt/sync/keychain_password_mac_unittest.mm
+++ components/os_crypt/sync/keychain_password_mac_unittest.mm
@@ -49,7 +49,6 @@ TEST(KeychainPasswordTest, FindPasswordSuccess) {
   KeychainPasswordEnvironment environment(noErr);
   EXPECT_FALSE(environment.GetPassword().empty());
   EXPECT_FALSE(environment.keychain().called_add_generic());
-  EXPECT_EQ(0, environment.keychain().password_data_count());
 }
 
 // Test that if we do not have an existing password in the Keychain then it
@@ -58,7 +57,6 @@ TEST(KeychainPasswordTest, FindPasswordNotFound) {
   KeychainPasswordEnvironment environment(errSecItemNotFound);
   EXPECT_EQ(24U, environment.GetPassword().length());
   EXPECT_TRUE(environment.keychain().called_add_generic());
-  EXPECT_EQ(0, environment.keychain().password_data_count());
 }
 
 // Test that if get denied access by the user then we return an empty password.
@@ -67,7 +65,6 @@ TEST(KeychainPasswordTest, FindPasswordNotAuthorized) {
   KeychainPasswordEnvironment environment(errSecAuthFailed);
   EXPECT_TRUE(environment.GetPassword().empty());
   EXPECT_FALSE(environment.keychain().called_add_generic());
-  EXPECT_EQ(0, environment.keychain().password_data_count());
 }
 
 // Test that if some random other error happens then we return an empty
@@ -76,7 +73,6 @@ TEST(KeychainPasswordTest, FindPasswordOtherError) {
   KeychainPasswordEnvironment environment(errSecNotAvailable);
   EXPECT_TRUE(environment.GetPassword().empty());
   EXPECT_FALSE(environment.keychain().called_add_generic());
-  EXPECT_EQ(0, environment.keychain().password_data_count());
 }
 
 // Test that subsequent additions to the keychain give different passwords.
@@ -85,13 +81,11 @@ TEST(KeychainPasswordTest, PasswordsDiffer) {
   std::string password1 = environment1.GetPassword();
   EXPECT_FALSE(password1.empty());
   EXPECT_TRUE(environment1.keychain().called_add_generic());
-  EXPECT_EQ(0, environment1.keychain().password_data_count());
 
   KeychainPasswordEnvironment environment2(errSecItemNotFound);
   std::string password2 = environment2.GetPassword();
   EXPECT_FALSE(password2.empty());
   EXPECT_TRUE(environment2.keychain().called_add_generic());
-  EXPECT_EQ(0, environment2.keychain().password_data_count());
 
   // And finally check that the passwords are different.
   EXPECT_NE(password1, password2);
