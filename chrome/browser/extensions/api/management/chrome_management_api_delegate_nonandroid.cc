f08d9fc33265fe47f5ff5db4110aa7a0a1784f29
Achuith Bhandarkar <achuith@chromium.org>
2025-04-15 18:23:58 -0700

    [extensions] Remove ExtensionService::DisableExtensionWithSource.
    Use ExtensionRegistrar version of DisableExtensionWithSource, delete
    ExtensionService version.
    
    This is one of a series of CLs to migrate the extension disabling logic
    from ExtensionService to ExtensionRegistrar.
    
    Bug: 405411622
    Change-Id: I03319637771298371c51ca0e6c0f14f244044786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6456750
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447500}

--- chrome/browser/extensions/api/management/chrome_management_api_delegate_nonandroid.cc
+++ chrome/browser/extensions/api/management/chrome_management_api_delegate_nonandroid.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/browser_app_launcher.h"
 #include "chrome/browser/extensions/chrome_extension_function_details.h"
 #include "chrome/browser/extensions/extension_management.h"
-#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/extensions/manifest_v2_experiment_manager.h"
 #include "chrome/browser/extensions/mv2_experiment_stage.h"
@@ -570,10 +569,8 @@ void ChromeManagementAPIDelegate::DisableExtension(
   SupervisedUserExtensionsDelegate* extensions_delegate =
       GetSupervisedUserExtensionsDelegateFromContext(context);
   extensions_delegate->RecordExtensionEnablementUmaMetrics(/*enabled=*/false);
-  ExtensionSystem::Get(context)
-      ->extension_service()
-      ->DisableExtensionWithSource(source_extension, extension_id,
-                                   disable_reason);
+  ExtensionRegistrar::Get(context)->DisableExtensionWithSource(
+      source_extension, extension_id, disable_reason);
 }
 
 bool ChromeManagementAPIDelegate::UninstallExtension(
