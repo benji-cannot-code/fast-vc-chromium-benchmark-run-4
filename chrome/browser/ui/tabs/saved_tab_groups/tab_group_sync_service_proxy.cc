ef7103b52345c1067d805c90e5ea336d4a20d098
David Pennington <dpenning@chromium.org>
2025-04-15 21:51:48 -0700

    [Shared Tab Groups] add hourly metrics to tgss
    adding hourly metrics so that the number of groups and other metrics
    can be tracked on a more granular timescale, and match
    SavedTabGroupKeyedService. This CL also allows for getting the model
    directly in production code so that the metrics logger can be reused.
    
    Bug: 380119908
    Change-Id: I9fe0020286cc526cc26c5a0791111a4f5a8443f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458924
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447530}

--- chrome/browser/ui/tabs/saved_tab_groups/tab_group_sync_service_proxy.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/tab_group_sync_service_proxy.cc
@@ -406,7 +406,7 @@ std::u16string TabGroupSyncServiceProxy::GetTabTitle(
   return std::u16string();
 }
 
-SavedTabGroupModel* TabGroupSyncServiceProxy::GetModelForTesting() {
+SavedTabGroupModel* TabGroupSyncServiceProxy::GetModel() {
   return service_->model();
 }
 
