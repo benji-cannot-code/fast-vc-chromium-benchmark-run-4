82fda3474dcddd63c7c289422c383cf6fdd3a1f6
Igor Kraskevich <kraskevich@google.com>
2025-04-16 02:58:06 -0700

    Add Event.Scroll histogram to track scroll durations
    Bug: 409972133
    Change-Id: I7e560cff1e146179656b84439a767834a5c458d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449706
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447607}

--- cc/input/input_handler.cc
+++ cc/input/input_handler.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/types/optional_ref.h"
 #include "build/build_config.h"
@@ -64,6 +65,20 @@ InputHandlerClient::ScrollEventDispatchMode GetScrollEventDispatchMode() {
   }
 
   return InputHandlerClient::ScrollEventDispatchMode::kEnqueueScrollEvents;
+}
+
+std::string GetScrollInputTypeSuffix(ui::ScrollInputType input_type) {
+  switch (input_type) {
+    case ui::ScrollInputType::kTouchscreen:
+      return "Touchscreen";
+    case ui::ScrollInputType::kWheel:
+      return "Wheel";
+    case ui::ScrollInputType::kAutoscroll:
+      return "Autoscroll";
+    case ui::ScrollInputType::kScrollbar:
+      return "Scrollbar";
+  }
+  NOTREACHED();
 }
 
 }  // namespace
@@ -558,6 +573,8 @@ void InputHandler::ScrollEnd(ScrollNode* scroll_node, bool should_snap) {
 void InputHandler::RecordScrollBegin(
     ui::ScrollInputType input_type,
     ScrollBeginThreadState scroll_start_state) {
+  last_scroll_begin_time_ = base::TimeTicks::Now();
+
   auto tracker_type = GetTrackerTypeForScroll(input_type);
   DCHECK_NE(tracker_type, FrameSequenceTrackerType::kMaxType);
 
@@ -587,6 +604,9 @@ void InputHandler::RecordScrollBegin(
 
 void InputHandler::RecordScrollEnd(ui::ScrollInputType input_type) {
   compositor_delegate_->StopSequence(GetTrackerTypeForScroll(input_type));
+  base::UmaHistogramTimes(
+      "Event.Scroll." + GetScrollInputTypeSuffix(input_type),
+      base::TimeTicks::Now() - last_scroll_begin_time_);
 }
 
 InputHandlerPointerResult InputHandler::MouseMoveAt(
