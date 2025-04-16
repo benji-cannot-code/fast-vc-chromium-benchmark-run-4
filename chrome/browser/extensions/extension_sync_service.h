3278a33c3cb0020e7129f8f657f23f476830dc24
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-04-16 10:11:59 -0700

    [Extensions] Move Web App logic out of PendingExtensionManager
    The PendingExtensionManager has logic to not add an item from sync if
    it corresponds to a pre-installed web app. This exists to avoid adding
    "zombie" extensions from sync after they've migrated to the web apps.
    
    Instead of having this in PendingExtensionManager, move the logic to
    directly above the call to add the item to the PendingExtensionManager
    in ExtensionSyncService. This keeps the logic for "should we add this
    from sync" more contained within sync classes and also paves the way to
    move PendingExtensionManager to //extensions.
    
    This CL should have no effective behavior change.
    
    Bug: 358567092
    Change-Id: I1581361ebbae38dd0e46fe2b6191a95208dc1b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6460669
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447833}

--- chrome/browser/extensions/extension_sync_service.h
+++ chrome/browser/extensions/extension_sync_service.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <optional>
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -140,6 +142,11 @@ class ExtensionSyncService : public syncer::SyncableService,
   // can be uploaded to the sync server).
   bool ShouldSync(const extensions::Extension& extension) const;
 
+  // Returns true if the given `extension_id` corresponds to an item that has
+  // migrated to a pre-installed web app.
+  bool IsMigratingPreinstalledWebApp(
+      const extensions::ExtensionId& extension_id);
+
   // The normal profile associated with this ExtensionSyncService.
   raw_ptr<Profile> profile_;
 
@@ -172,6 +179,11 @@ class ExtensionSyncService : public syncer::SyncableService,
   // have started happening. It will cause sync to call us back
   // asynchronously via MergeDataAndStartSyncing as soon as possible.
   SyncableService::StartSyncFlare flare_;
+
+  // Caches the set of Chrome app IDs undergoing migration to web apps because
+  // it is expensive to generate every time (multiple SkBitmap copies).
+  std::optional<base::flat_set<std::string>>
+      migrating_default_chrome_app_ids_cache_;
 
   base::WeakPtrFactory<ExtensionSyncService> weak_ptr_factory_{this};
 };
