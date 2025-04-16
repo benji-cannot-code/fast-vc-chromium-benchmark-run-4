7d049c70651ee10b59e703fe1f9737cb876a121b
William Liu <liuwilliam@chromium.org>
2025-04-16 12:46:48 -0700

    Implement the scroll-to UX
    Bug: 395858827
    
    Change-Id: I0c5bbac9d5bf8d80506f204d1ce188f35480bd41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6420190
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447951}

--- third_party/blink/renderer/core/editing/markers/document_marker.h
+++ third_party/blink/renderer/core/editing/markers/document_marker.h
@@ -50,6 +50,7 @@ class CORE_EXPORT DocumentMarker : public GarbageCollected<DocumentMarker> {
     kSuggestionMarkerIndex,
     kTextFragmentMarkerIndex,
     kCustomHighlightMarkerIndex,
+    kGlicMarkerIndex,
     kMarkerTypeIndexesCount
   };
 
@@ -61,7 +62,8 @@ class CORE_EXPORT DocumentMarker : public GarbageCollected<DocumentMarker> {
     kActiveSuggestion = 1 << kActiveSuggestionMarkerIndex,
     kSuggestion = 1 << kSuggestionMarkerIndex,
     kTextFragment = 1 << kTextFragmentMarkerIndex,
-    kCustomHighlight = 1 << kCustomHighlightMarkerIndex
+    kCustomHighlight = 1 << kCustomHighlightMarkerIndex,
+    kGlic = 1 << kGlicMarkerIndex,
   };
 
   class MarkerTypesIterator {
@@ -143,6 +145,7 @@ class CORE_EXPORT DocumentMarker : public GarbageCollected<DocumentMarker> {
     static MarkerTypes CustomHighlight() {
       return MarkerTypes(kCustomHighlight);
     }
+    static MarkerTypes Glic() { return MarkerTypes(kGlic); }
 
     bool Contains(MarkerType type) const { return mask_ & type; }
     bool Intersects(const MarkerTypes& types) const {
