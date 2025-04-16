f455f45ec362adf29d68eaa8648f7e1661995abc
Tom Van Goethem <tov@chromium.org>
2025-04-16 02:20:05 -0700

    [FPF-CI] Add UseCounter for canvas noise
    This adds a use counter whenever noise was added to a canvas readback.
    
    Bug: 377325952
    Change-Id: Iaba12d07fb23f800d7065535c24a36b55bb17ec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458478
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: John Kim <johnykim@google.com>
    Auto-Submit: Tom Van Goethem <tov@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447592}

--- third_party/blink/renderer/core/canvas_interventions/canvas_interventions_helper.cc
+++ third_party/blink/renderer/core/canvas_interventions/canvas_interventions_helper.cc
@@ -20,11 +20,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/frame.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_host.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/runtime_feature_state/runtime_feature_state_override_context.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
@@ -63,6 +65,12 @@ bool ShouldApplyNoise(CanvasRenderingContext* rendering_context,
   }
   if (!execution_context) {
     noise_reason |= CanvasNoiseReason::kNoExecutionContext;
+  }
+  // Check if all heuristics have matched so far (excluding whether the feature
+  // is enabled).
+  if (noise_reason == CanvasNoiseReason::kAllConditionsMet) {
+    UseCounter::Count(execution_context,
+                      WebFeature::kCanvasReadbackNoiseMatchesHeuristics);
   }
   if (execution_context &&
       !execution_context->GetRuntimeFeatureStateOverrideContext()
@@ -176,6 +184,7 @@ bool CanvasInterventionsHelper::MaybeNoiseSnapshot(
   UMA_HISTOGRAM_COUNTS_1M(
       "FingerprintingProtection.CanvasNoise.NoisedCanvasSize",
       pixmap_to_noise.width() * pixmap_to_noise.height());
+  UseCounter::Count(execution_context, WebFeature::kCanvasReadbackNoise);
 
   return true;
 }
