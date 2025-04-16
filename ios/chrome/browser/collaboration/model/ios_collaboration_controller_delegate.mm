e93e9cf6a6e1d38cc2e5226c7f7fcae5b7dd31ce
Federica Germinario <fedegermi@google.com>
2025-04-16 04:43:15 -0700

    Revert "[ios] Add switch profile continuation entry point for share tab group"
    This reverts commit b33487ac932f60acf45cbdb423a52c276ba78abb.
    
    Reason for revert: Implementation not needed anymore
    
    Bug: 374282360
    Original change's description:
    > [ios] Add switch profile continuation entry point for share tab group
    >
    > This CL adds and entry point for the switch profile continuation for
    > share tab group.
    > Implementation will be handled in a follow-up CL.
    >
    > Bug: 374282360
    > Change-Id: I3ce91d513181da29ba190e93cb6e1bc38e51ea35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6197425
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Mark Cogan <marq@chromium.org>
    > Auto-Submit: Federica Germinario <fedegermi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1447169}
    
    Bug: 374282360
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Id6de5a8a234f4bd32a9b69456ecc78a7ed0161fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6459066
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447641}

--- ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate.mm
+++ ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate.mm
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/base/user_selectable_type.h"
 #import "components/sync/service/sync_service.h"
 #import "components/sync/service/sync_user_settings.h"
-#import "ios/chrome/browser/authentication/ui_bundled/change_profile/change_profile_share_tab_group_continuation.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin/signin_constants.h"
 #import "ios/chrome/browser/collaboration/model/collaboration_service_factory.h"
 #import "ios/chrome/browser/favicon/model/favicon_loader.h"
@@ -215,17 +214,13 @@ void IOSCollaborationControllerDelegate::ShowAuthenticationUi(
       break;
   }
 
-  ChangeProfileContinuationProvider provider =
-      base::BindRepeating(&CreateChangeProfileShareTabGroupContinuation);
-
   ShowSigninCommand* command = [[ShowSigninCommand alloc]
-                      initWithOperation:operation
-                               identity:nil
-                            accessPoint:access_point
-                            promoAction:signin_metrics::PromoAction::
-                                            PROMO_ACTION_NO_SIGNIN_PROMO
-                             completion:completion_block
-      changeProfileContinuationProvider:provider];
+      initWithOperation:operation
+               identity:nil
+            accessPoint:access_point
+            promoAction:signin_metrics::PromoAction::
+                            PROMO_ACTION_NO_SIGNIN_PROMO
+             completion:completion_block];
 
   command.optionalHistorySync = NO;
   command.fullScreenPromo = fullScreenPromo;
