a934df1fe9e647abee7b381d52ef0d87f5cc6304
François Doray <fdoray@chromium.org>
2025-04-16 14:14:53 -0700

    [base]] Validate crash key name even if no CrashKeyImplementation is registered.
    Today, we only validate a crash key name (forbidden characters,
    maximum length) if a CrashKeyImplementation is registered. This means
    that a unit test which runs the code emitting a crash key might not
    detect issues with the crash key name. This CL prevents this problem
    by moving the validation before we check if an implementation is
    registered. This only affects DCHECK-enabled builds.
    
    Bug: 411108300
    Change-Id: I57155ba0fd9e7f09d0c9f4a6d496aa9cfedaa351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6462464
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448009}

--- base/debug/crash_logging.cc
+++ base/debug/crash_logging.cc
@@ -20,10 +20,6 @@ CrashKeyImplementation* g_crash_key_impl = nullptr;
 
 CrashKeyString* AllocateCrashKeyString(const char name[],
                                        CrashKeySize value_length) {
-  if (!g_crash_key_impl) {
-    return nullptr;
-  }
-
   // TODO(crbug.com/40850825): It would be great if the DCHECKs below
   // could also be enabled on Android, but debugging tryjob failures was a bit
   // difficult... :-/
@@ -41,6 +37,10 @@ CrashKeyString* AllocateCrashKeyString(const char name[],
   // `CrashKeyStringImpl`s.
   DCHECK_LT(name_piece.size(), 40u);
 #endif
+
+  if (!g_crash_key_impl) {
+    return nullptr;
+  }
 
   return g_crash_key_impl->Allocate(name, value_length);
 }
