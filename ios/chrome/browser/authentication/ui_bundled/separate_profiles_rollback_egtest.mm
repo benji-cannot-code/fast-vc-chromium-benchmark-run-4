dfea46fa53b0ec8aa241c6fafa7703eeb1be505b
Asami Doi <asamidoi@chromium.org>
2025-04-16 08:02:47 -0700

    [ios] Mark SeparateProfilesTestCase/SeparateProfilesRollbackTestCase tests as flaky
    Mark the following tests as flaky.
    
    - SeparateProfilesTestCase/testProfileDeletedOnManagedAccountGone
    - SeparateProfilesTestCase/testProfileDeletedOnRemoveManagedAccount
    - SeparateProfilesTestCase/testSigninWithManagedAccountFromUnsignedStateWithDataMigrationDisabled
    - SeparateProfilesTestCase/testSigninWithManagedAccountFromUnsignedStateSeparateData
    - SeparateProfilesTestCase/testSigninWithManagedAccountFromUnsignedStateWithDataMergingSuggested
    - SeparateProfilesTestCase/testProfileNotDeletedOnRemovePersonalAccount
    - SeparateProfilesTestCase/testSigninWithManagedAccountFromUnsignedStateConvertsProfile
    - SeparateProfilesRollbackTestCase/testRollbackWithManagedProfile
    - SeparateProfilesRollbackTestCase/testRollbackWithManagedProfile_ManagedAccountRemoved
    - SeparateProfilesRollbackTestCase/testRollbackWithManagedProfile_KillSwitch
    
    Bug: 411035267
    Change-Id: I575371419aa3db91f4bf90d6749c3dd5608c1450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458486
    Owners-Override: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447737}

--- ios/chrome/browser/authentication/ui_bundled/separate_profiles_rollback_egtest.mm
+++ ios/chrome/browser/authentication/ui_bundled/separate_profiles_rollback_egtest.mm
@@ -109,7 +109,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
              @"Separate profiles should be disabled now");
 }
 
-- (void)testRollbackWithManagedProfile {
+// TODO(crbug.com/411035267): Fix this flaky test on simulator.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_testRollbackWithManagedProfile \
+  FLAKY_testRollbackWithManagedProfile
+#else
+#define MAYBE_testRollbackWithManagedProfile testRollbackWithManagedProfile
+#endif
+- (void)MAYBE_testRollbackWithManagedProfile {
   // Separate profiles are only available in iOS 17+.
   if (!@available(iOS 17, *)) {
     return;
@@ -203,7 +210,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       @"Should be in the managed profile again");
 }
 
-- (void)testRollbackWithManagedProfile_ManagedAccountRemoved {
+// TODO(crbug.com/411035267): Fix this flaky test on simulator.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_testRollbackWithManagedProfile_ManagedAccountRemoved \
+  FLAKY_testRollbackWithManagedProfile_ManagedAccountRemoved
+#else
+#define MAYBE_testRollbackWithManagedProfile_ManagedAccountRemoved \
+  testRollbackWithManagedProfile_ManagedAccountRemoved
+#endif
+- (void)MAYBE_testRollbackWithManagedProfile_ManagedAccountRemoved {
   // Separate profiles are only available in iOS 17+.
   if (!@available(iOS 17, *)) {
     return;
@@ -294,7 +309,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
              @"Separate profiles should still be enabled");
 }
 
-- (void)testRollbackWithManagedProfile_KillSwitch {
+// TODO(crbug.com/411035267): Fix this flaky test on simulator.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_testRollbackWithManagedProfile_KillSwitch \
+  FLAKY_testRollbackWithManagedProfile_KillSwitch
+#else
+#define MAYBE_testRollbackWithManagedProfile_KillSwitch \
+  testRollbackWithManagedProfile_KillSwitch
+#endif
+- (void)MAYBE_testRollbackWithManagedProfile_KillSwitch {
   // Separate profiles are only available in iOS 17+.
   if (!@available(iOS 17, *)) {
     return;
