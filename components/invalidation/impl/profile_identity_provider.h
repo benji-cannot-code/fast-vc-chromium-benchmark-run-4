63d4761b7cfd1d42656b21c0020bc8d4f3695ce0
Roland Bock <rbock@google.com>
2025-04-17 08:27:15 -0700

    Implement signin::IdentityManager::Observer::OnIdentityManagerShutdown
    Bug: 411114786
    Bug: 410122787
    Test: Existing tests
    Change-Id: Ia809d4cf3b526b8cea0ad58d1f9025d1e885fa48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6463831
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448325}

--- components/invalidation/impl/profile_identity_provider.h
+++ components/invalidation/impl/profile_identity_provider.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_INVALIDATION_IMPL_PROFILE_IDENTITY_PROVIDER_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "build/build_config.h"
 #include "components/invalidation/public/identity_provider.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -38,9 +39,15 @@ class ProfileIdentityProvider : public IdentityProvider,
       const signin::PrimaryAccountChangeEvent& event_details) override;
   void OnRefreshTokenUpdatedForAccount(
       const CoreAccountInfo& account_info) override;
+  void OnIdentityManagerShutdown(
+      signin::IdentityManager* identity_manager) override;
 
  private:
-  const raw_ptr<signin::IdentityManager> identity_manager_;
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observation_{this};
+
+  raw_ptr<signin::IdentityManager> identity_manager_;
 };
 
 }  // namespace invalidation
