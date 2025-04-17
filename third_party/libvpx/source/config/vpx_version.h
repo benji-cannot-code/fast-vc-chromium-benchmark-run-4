d7da9e1b15f897eec3570e3e224966f71cc18f8e
Marco Paniconi <marpan@google.com>
2025-04-17 14:59:34 -0700

    Roll src/third_party/libvpx/source/libvpx/ 0ab8891af..1c9202e7d (3 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/0ab8891af8fb..1c9202e7da20
    
    $ git log 0ab8891af..1c9202e7d --date=short --no-merges --format='%ad %ae %s'
    2025-04-15 jianj Fix comment for avx512 detection
    2025-04-15 hnt tools.mk: Expose tiny_ssim.SRCS as tiny_ssim_srcs.txt
    2025-04-09 chengchen Remove CONFIG_RATE_CTRL
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=jzern@google.com
    
    Bug: b:308446709
    
    Change-Id: I50194c7d11d80181057fe2f503c459eaafe5bd6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469184
    Reviewed-by: James Zern <jzern@google.com>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448603}

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -2,12 +2,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file is generated. Do not edit.
 #ifndef VPX_VERSION_H_
 #define VPX_VERSION_H_
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 15
-#define VERSION_PATCH 1
-#define VERSION_EXTRA "72-g0ab8891af"
-#define VERSION_PACKED \
-  ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.15.1-72-g0ab8891af"
-#define VERSION_STRING " v1.15.1-72-g0ab8891af"
+#define VERSION_MAJOR  1
+#define VERSION_MINOR  15
+#define VERSION_PATCH  1
+#define VERSION_EXTRA  "75-g1c9202e7d"
+#define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
+#define VERSION_STRING_NOSP "v1.15.1-75-g1c9202e7d"
+#define VERSION_STRING      " v1.15.1-75-g1c9202e7d"
 #endif  // VPX_VERSION_H_
