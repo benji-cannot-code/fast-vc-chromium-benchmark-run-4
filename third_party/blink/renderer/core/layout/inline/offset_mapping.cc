aba06fcefb85d7a3640a0afee8e4843d85e03bf1
Kent Tamura <tkent@chromium.org>
2025-04-16 21:29:51 -0700

    Add OffsetMappingUnit::IsCollapsed()
    It simplifies the code.
    This CL has no behavior changes.
    
    Change-Id: I6d31bd17c893a336f1e3868f616cf5fdfee7c368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6466684
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448144}

--- third_party/blink/renderer/core/layout/inline/offset_mapping.cc
+++ third_party/blink/renderer/core/layout/inline/offset_mapping.cc
@@ -434,8 +434,7 @@ Position OffsetMapping::StartOfNextNonCollapsedContent(
     if (u.AssociatedNode() != node) {
       break;
     }
-    if (u.DOMEnd() > offset &&
-        u.GetType() != OffsetMappingUnitType::kCollapsed) {
+    if (u.DOMEnd() > offset && !u.IsCollapsed()) {
       const unsigned result = std::max(offset, u.DOMStart());
       return CreatePositionForOffsetMapping(node, result);
     }
@@ -459,8 +458,7 @@ Position OffsetMapping::EndOfLastNonCollapsedContent(
     if (u.AssociatedNode() != node) {
       break;
     }
-    if (u.DOMStart() < offset &&
-        u.GetType() != OffsetMappingUnitType::kCollapsed) {
+    if (u.DOMStart() < offset && !u.IsCollapsed()) {
       const unsigned result = std::min(offset, u.DOMEnd());
       return CreatePositionForOffsetMapping(node, result);
     }
@@ -473,8 +471,7 @@ bool OffsetMapping::IsBeforeNonCollapsedContent(
   DCHECK(OffsetMapping::AcceptsPosition(position));
   const OffsetMappingUnit* unit = GetMappingUnitForPosition(position);
   const unsigned offset = ToNodeOffsetPair(position).second;
-  return unit && offset < unit->DOMEnd() &&
-         unit->GetType() != OffsetMappingUnitType::kCollapsed;
+  return unit && offset < unit->DOMEnd() && !unit->IsCollapsed();
 }
 
 bool OffsetMapping::IsAfterNonCollapsedContent(const Position& position) const {
@@ -488,8 +485,7 @@ bool OffsetMapping::IsAfterNonCollapsedContent(const Position& position) const {
   // |offset|, we need to find the former. Hence, search with |offset - 1|.
   const OffsetMappingUnit* unit = GetMappingUnitForPosition(
       CreatePositionForOffsetMapping(node, offset - 1));
-  return unit && offset > unit->DOMStart() &&
-         unit->GetType() != OffsetMappingUnitType::kCollapsed;
+  return unit && offset > unit->DOMStart() && !unit->IsCollapsed();
 }
 
 std::optional<UChar> OffsetMapping::GetCharacterBefore(
