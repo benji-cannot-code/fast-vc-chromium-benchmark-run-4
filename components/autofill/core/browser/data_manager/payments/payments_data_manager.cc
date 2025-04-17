4b9aa63ef1103ed189a5af0c1e88c9fd8ae2ccc7
Florian Leimgruber <fleimgruber@google.com>
2025-04-17 15:10:49 -0700

    Unregister PaymentDataManager's IdentityObserver during Shutdown()
    See crbug.com/411079887#comment4.
    
    Fixed: 411079887
    Change-Id: Iad6bef142eb91a96134920534070b9aca03b2365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469092
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448612}

--- components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
+++ components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
@@ -284,6 +284,7 @@ PaymentsDataManager::~PaymentsDataManager() {
 
 void PaymentsDataManager::Shutdown() {
   sync_observer_.Reset();
+  identity_observer_.Reset();
 }
 
 void PaymentsDataManager::OnAutofillChangedBySync(syncer::DataType data_type) {
