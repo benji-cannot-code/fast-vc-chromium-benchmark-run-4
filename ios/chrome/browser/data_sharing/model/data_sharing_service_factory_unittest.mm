6181d4315695fe7d6bcc20e3094e7f5b7b69e39e
Louis Romero <lpromero@google.com>
2025-04-17 02:04:31 -0700

    Fix feature overrides for Shared Tab Groups in unit tests
    The conditions were incorrect. It worked only because DataSharing
    features are not on by default.
    
    Bug: 409499069
    Change-Id: Ie164c0ae0ef59212552d656cdf4e5bcd402a2f63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6465495
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448206}

--- ios/chrome/browser/data_sharing/model/data_sharing_service_factory_unittest.mm
+++ ios/chrome/browser/data_sharing/model/data_sharing_service_factory_unittest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/data_sharing/public/data_sharing_service.h"
 #import "components/data_sharing/public/features.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
@@ -25,11 +26,21 @@ class DataSharingServiceFactoryTest : public PlatformTest {
 
   void InitService(bool enable_feature) {
     if (enable_feature) {
-      scoped_feature_list_.InitWithFeaturesAndParameters(
-          {{data_sharing::features::kDataSharingFeature, {}}}, {});
+      scoped_feature_list_.InitWithFeatures(
+          /*enabled_features=*/
+          {
+              kTabGroupSync,
+              kTabGroupsIPad,
+              data_sharing::features::kDataSharingJoinOnly,
+          },
+          /*disable_features=*/{});
     } else {
-      scoped_feature_list_.InitWithFeaturesAndParameters(
-          {}, {{data_sharing::features::kDataSharingFeature}});
+      scoped_feature_list_.InitWithFeatures(
+          /*enabled_features=*/{},
+          /*disable_features=*/{
+              data_sharing::features::kDataSharingJoinOnly,
+              data_sharing::features::kDataSharingFeature,
+          });
     }
     TestProfileIOS::Builder builder;
     builder.AddTestingFactory(DataSharingServiceFactory::GetInstance(),
@@ -39,8 +50,9 @@ class DataSharingServiceFactoryTest : public PlatformTest {
 
   void TearDown() override { web_task_env_.RunUntilIdle(); }
 
-  web::WebTaskEnvironment web_task_env_;
+ protected:
   base::test::ScopedFeatureList scoped_feature_list_;
+  web::WebTaskEnvironment web_task_env_;
   std::unique_ptr<TestProfileIOS> profile_;
 };
 
