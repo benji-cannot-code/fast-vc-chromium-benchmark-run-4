d08697d65be8b39347f8bbd2bc7493d8d385ae25
Ameur Hosni <ameurhosni@google.com>
2025-04-17 09:49:55 -0700

    [IOS][OR] Move test related files to test folder
    This CL relocates public files from ui_bundled into the test folder,
    aligning with the bling folder structure.
    
    Bug: 406467969,394567614
    Change-Id: I9e960b7b0f83c4536b8b1d17a5ab37e168ddef9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6459233
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448378}

--- ios/chrome/browser/omnibox/eg_tests/fake_suggestions_database.h
+++ ios/chrome/browser/omnibox/eg_tests/fake_suggestions_database.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_FAKE_SUGGESTIONS_DATABASE_H_
-#define IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_FAKE_SUGGESTIONS_DATABASE_H_
+#ifndef IOS_CHROME_BROWSER_OMNIBOX_EG_TESTS_FAKE_SUGGESTIONS_DATABASE_H_
+#define IOS_CHROME_BROWSER_OMNIBOX_EG_TESTS_FAKE_SUGGESTIONS_DATABASE_H_
 
 #include <map>
 #include <string>
@@ -54,4 +54,4 @@ class FakeSuggestionsDatabase {
   std::map<std::u16string, std::string> data_;
 };
 
-#endif  // IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_FAKE_SUGGESTIONS_DATABASE_H_
+#endif  // IOS_CHROME_BROWSER_OMNIBOX_EG_TESTS_FAKE_SUGGESTIONS_DATABASE_H_
