a6a37918bf766b989c76d34fcc8d4025afb6b9bb
Christine Ying <chrstne@google.com>
2025-04-17 13:49:27 -0700

    [N&C] Adding a startup histogram version 2 to track schema v2
    See: https://chromium.googlesource.com/chromium/src/tools/+/HEAD/metrics/histograms/README.md#revising
    
    Bug: 408016037
    Change-Id: I3f82ab62a0077378a3149bc6ad3493b8fb8bebeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470703
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448562}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service_impl_unittest.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_impl_unittest.cc
@@ -766,6 +766,10 @@ TEST_F(PrivacySandboxServiceTest, HistogramsAreEmptyOnStartup) {
         histograms,
         testing::Not(testing::AnyOf(base::StrCat(
             {"PrivacySandbox.Notice.NoticeStartupState.", notice_name}))));
+    EXPECT_THAT(
+        histograms,
+        testing::Not(testing::AnyOf(base::StrCat(
+            {"PrivacySandbox.Notice.NoticeStartupState2.", notice_name}))));
   }
 }
 
