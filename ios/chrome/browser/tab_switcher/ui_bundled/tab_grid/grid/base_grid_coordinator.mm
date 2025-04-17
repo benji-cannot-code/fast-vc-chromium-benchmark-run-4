9cdc44e6bed1ae1fac8b66d5b1c8de29815acc4b
Ewann Pelle <ewannpv@chromium.org>
2025-04-17 09:54:50 -0700

    [iOS] Updates leave/delete shared group on base grid flow
    The group will be automatically leaved / deleted on after a `kSuccess`.
    
    Bug: 409472956
    Change-Id: I67d504aeb9b865dd0795ac7c3cd3c44f51091f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469775
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448380}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/base_grid_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/base_grid_coordinator.mm
@@ -534,16 +534,11 @@ using collaboration::CollaborationControllerDelegate;
       }
       break;
     case TabGroupActionType::kLeaveSharedTabGroup:
-      if (weakGroup) {
-        [self.mediator leaveSharedTabGroup:weakGroup.get()];
-      }
+      [self runLeaveOrDeleteCompletion];
       break;
     case TabGroupActionType::kDeleteSharedTabGroup:
-      if (weakGroup) {
-        [self.mediator deleteSharedTabGroup:weakGroup.get()];
-      }
+      [self runLeaveOrDeleteCompletion];
       break;
-
     case TabGroupActionType::kLeaveOrKeepSharedTabGroup:
     case TabGroupActionType::kDeleteOrKeepSharedTabGroup:
       NOTREACHED();
@@ -561,6 +556,15 @@ using collaboration::CollaborationControllerDelegate;
   if (self.leaveOrDeleteCompletion) {
     self.leaveOrDeleteCompletion(
         CollaborationControllerDelegate::Outcome::kCancel);
+  }
+  self.leaveOrDeleteCompletion = nil;
+}
+
+// Runs `leaveOrDeleteCompletion`. If not nil, calls it with `kSuccess`.
+- (void)runLeaveOrDeleteCompletion {
+  if (self.leaveOrDeleteCompletion) {
+    self.leaveOrDeleteCompletion(
+        CollaborationControllerDelegate::Outcome::kSuccess);
   }
   self.leaveOrDeleteCompletion = nil;
 }
