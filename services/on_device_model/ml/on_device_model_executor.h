e730cc61b61d836e6bb7f8c2fc38e5e87e14fffa
Clark DuVall <cduvall@chromium.org>
2025-04-17 16:45:03 -0700

    Integrate llguidance for structured outputs in on-device model
    This integrates the llguidance library added in crrev.com/c/6272323 to
    support structured outputs for the on-device model.
    
    This is based on crrev.com/c/6232561 by frankli@microsoft.com, with some
    of the simplifications suggested there.
    
    Bug: 391919456
    Change-Id: I26608bc6bb84eae3405fdcf68180c5c83527ac4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469181
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448670}

--- services/on_device_model/ml/on_device_model_executor.h
+++ services/on_device_model/ml/on_device_model_executor.h
@@ -28,15 +28,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/on_device_model/public/mojom/on_device_model_service.mojom.h"
 #include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
+struct LlgTokenizer;
+
 namespace ml {
 
 class ContextHolder;
+class OnDeviceModelExecutor;
 class Responder;
 
 class COMPONENT_EXPORT(ON_DEVICE_MODEL_ML) SessionImpl final {
  public:
   SessionImpl(const ChromeML& chrome_ml,
-              ChromeMLModel model,
+              OnDeviceModelExecutor& executor,
               SessionAccessor::Ptr session,
               uint32_t max_tokens,
               std::optional<uint32_t> adaptation_id);
@@ -64,7 +67,7 @@ class COMPONENT_EXPORT(ON_DEVICE_MODEL_ML) SessionImpl final {
   void RemoveContext(ContextHolder* context);
 
   const raw_ref<const ChromeML> chrome_ml_;
-  ChromeMLModel model_;
+  const raw_ref<OnDeviceModelExecutor> executor_;
   SessionAccessor::Ptr session_;
   const uint32_t max_tokens_;
   std::unique_ptr<Responder> responder_;
@@ -111,6 +114,8 @@ class COMPONENT_EXPORT(ON_DEVICE_MODEL_ML) OnDeviceModelExecutor final {
   std::unique_ptr<ScopedAdaptation> LoadAdaptation(
       on_device_model::mojom::LoadAdaptationParamsPtr params);
 
+  ChromeMLConstraint CreateConstraint(const std::string& json_schema);
+
  private:
   on_device_model::mojom::LoadModelResult Init(
       on_device_model::mojom::LoadModelParamsPtr params,
@@ -128,6 +133,9 @@ class COMPONENT_EXPORT(ON_DEVICE_MODEL_ML) OnDeviceModelExecutor final {
   scoped_refptr<base::SequencedTaskRunner> model_task_runner_;
   uint32_t max_tokens_ = 0;
   uint32_t next_adaptation_id_ = 0;
+  // Disabling dangling pointer detection because this uses C functions to
+  // allocate/free from rust.
+  raw_ptr<LlgTokenizer, DisableDanglingPtrDetection> tokenizer_ = nullptr;
   base::WeakPtrFactory<OnDeviceModelExecutor> weak_ptr_factory_{this};
 };
 
