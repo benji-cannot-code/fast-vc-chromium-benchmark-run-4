9cb02a9bd58514214cf8c1d6a344fc1e65915272
Andrew Grieve <agrieve@chromium.org>
2025-04-17 12:44:05 -0700

    Android: Check for missing classpath .jar files in compile_java.py
    Missing files can be caused by GN bugs, but javac does not complain
    about them. They generally result in "Symbol not found" errors, which is
    a bit confusing.
    
    Bug: None
    Change-Id: I21b7d1883fd6d4aa154437bae335ba9b76dc48d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469394
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448523}

--- build/android/gyp/turbine.py
+++ build/android/gyp/turbine.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import functools
 import logging
+import os
 import sys
 import time
 import zipfile
@@ -36,7 +37,6 @@ def main(argv):
   parser.add_argument(
       '--java-srcjars',
       action='append',
-      default=[],
       help='List of srcjars to include in compilation.')
   parser.add_argument('--classpath', action='append', help='Classpath to use.')
   parser.add_argument(
@@ -78,6 +78,15 @@ def main(argv):
       parser.error('Unknown flag: ' + arg)
     else:
       files.append(arg)
+
+  # Turbine does not complain about missing classpath files. Missing files can
+  # result in misleading compile errors though, so do an upfront check.
+  classpath_jars = options.classpath + options.processorpath
+  missing_jars = [p for p in classpath_jars if not os.path.exists(p)]
+  if missing_jars:
+    sys.stderr.write('One or more classpath .jar files does not exist:\n')
+    sys.stderr.write('\n'.join(missing_jars) + '\n')
+    sys.exit(1)
 
   # The target's .sources file contains both Java and Kotlin files. We use
   # compile_kt.py to compile the Kotlin files to .class and header jars.
@@ -159,10 +168,8 @@ def main(argv):
   if options.depfile:
     # GN already knows of the java files, so avoid listing individual java files
     # in the depfile.
-    depfile_deps = (options.classpath + options.processorpath +
-                    options.java_srcjars)
     action_helpers.write_depfile(options.depfile, options.jar_path,
-                                 depfile_deps)
+                                 classpath_jars)
 
 
 if __name__ == '__main__':
