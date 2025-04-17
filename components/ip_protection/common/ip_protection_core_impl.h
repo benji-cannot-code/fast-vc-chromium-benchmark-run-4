cecc49742ec3862b7fdc3fe9509af2381b9cf328
Aykut Bulut <aykutb@google.com>
2025-04-17 15:39:31 -0700

    Add PRT header to eligible requests
    Implement adding a Probabilistic Reveal Token (PRT) to
    `Sec-Probabilistic-Reveal-Token` header for appropriate
    requests when the corresponding feature flag is on.
    
    Fixed: 402425762
    Change-Id: Ibf5f18b043302e73f81411af7c488f3fe754a61d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6351478
    Commit-Queue: Aykut Bulut <aykutb@google.com>
    Reviewed-by: Ryan Kalla <ryankalla@google.com>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448638}

--- components/ip_protection/common/ip_protection_core_impl.h
+++ components/ip_protection/common/ip_protection_core_impl.h
@@ -81,6 +81,7 @@ class IpProtectionCoreImpl
       ProxyLayer proxy_layer);
   IpProtectionProxyConfigManager* GetIpProtectionProxyConfigManagerForTesting();
 
+  bool IsProbabilisticRevealTokenAvailable() override;
   std::optional<std::string> GetProbabilisticRevealToken(
       const std::string& top_level,
       const std::string& third_party) override;
