8dcacc7592df3db300100c3bd9c39b31b7a0e67e
Solomon Kinard <solomonkinard@chromium.org>
2025-04-17 10:38:33 -0700

    Extensions: CodeHealth: Prefer /*=*/ instead of trailing comment
    Bug: chromium:346952289
    Change-Id: I53a4ae9269166805e766bd531e0103272375d9fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6465915
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448426}

--- chrome/browser/extensions/extension_util.cc
+++ chrome/browser/extensions/extension_util.cc
@@ -271,7 +271,7 @@ base::Value::Dict GetExtensionInfo(const Extension* extension) {
   GURL icon = extensions::ExtensionIconSource::GetIconURL(
       extension, extension_misc::EXTENSION_ICON_SMALLISH,
       ExtensionIconSet::Match::kBigger,
-      false);  // Not grayscale.
+      /*grayscale=*/false);
   dict.Set("icon", icon.spec());
 
   return dict;
