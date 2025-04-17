142dddcc65cb50710b260cd56e8986782c9b5e43
Qingxin Wu <qingxinwu@google.com>
2025-04-17 10:15:04 -0700

    Update KeepDebugReport() to consider cookie setting.
    It does not actually consider cookie setting yet, which is a todo in
    ShouldSampleDebugReport(). This CL only decides when to call
    ShouldSampleDebugReport().
    
    Bug: 391877228
    
    Change-Id: I2a0166beb120350cdece3491521c2b987b6b3814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348197
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448403}

--- content/browser/interest_group/for_debugging_only_report_util.h
+++ content/browser/interest_group/for_debugging_only_report_util.h
@@ -36,7 +36,7 @@ struct CONTENT_EXPORT DebugReportCooldown {
   bool operator==(const DebugReportCooldown& other) const = default;
 };
 
-// Should forDebuggingOnly reports be sampled or not.
+// True if only send sampled debug reports, false if send all debug reports.
 CONTENT_EXPORT bool ShouldSampleDebugReport();
 
 // Ceil kFledgeEnableFilteringDebugReportStartingFrom to its nearest
