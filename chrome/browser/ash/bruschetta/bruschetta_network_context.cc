8aaa5ac4a8d504d211d879e1a05cc44c854d6c14
Maks Orlovich <morlovich@chromium.org>
2025-04-17 13:57:07 -0700

    clickiness: Check attestation and user settings on origins
    The eligible and provider origins must all be attested and allowed by
    user settings.
    
    Since the state of frames might change by the time the notification is
    received in the browser, the top frame origin for the permission check
    is forwarded from the network process along the notification; this is
    the same value that's used for the cookie permission checks.
    
    Bug: 394108643
    Change-Id: I91b74edacebdab84bb180044e0700a6c6cb64e79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356796
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448570}

--- chrome/browser/ash/bruschetta/bruschetta_network_context.cc
+++ chrome/browser/ash/bruschetta/bruschetta_network_context.cc
@@ -275,7 +275,8 @@ void BruschettaNetworkContext::OnSharedStorageHeaderReceived(
 }
 
 void BruschettaNetworkContext::OnAdAuctionEventRecordHeaderReceived(
-    network::AdAuctionEventRecord event_record) {}
+    network::AdAuctionEventRecord event_record,
+    const std::optional<url::Origin>& top_frame_origin) {}
 
 void BruschettaNetworkContext::Clone(
     mojo::PendingReceiver<network::mojom::URLLoaderNetworkServiceObserver>
