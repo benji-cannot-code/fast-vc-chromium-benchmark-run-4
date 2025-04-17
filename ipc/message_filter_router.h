51efd059bd405e0cd85ab0e292d6bdabc0e5ad3d
Kalvin Lee <kdlee@chromium.org>
2025-04-17 12:34:49 -0700

    spanify: Renewed arrayification (X/43)
    This CL is part of the wider spanification / arrayification effort [1]
    and does not (intentionally) introduce behavioral change. It applies the
    `std::array` rewrite to
    
    ipc
    
    as close as possible to the output directly written by `spanify`, with
    no hand-rolled fixes (except where rebasing would require).
    
    This change (and its siblings taken together) is thought not to cause
    any measurable perf regressions [2].
    
    [1] https://issues.chromium.org/356643982
    [2] https://docs.google.com/document/d/1jughaR6JKn7T-dDjMou10awyNkhE5T-vLY_K2YMqHE4/edit?tab=t.0#heading=h.r2eguxl5lhu7
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 406029216
    Change-Id: I98eedaafb8c53e58ebc3d06577f5866316fed5d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6436854
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448511}

--- ipc/message_filter_router.h
+++ ipc/message_filter_router.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IPC_MESSAGE_FILTER_ROUTER_H_
 #define IPC_MESSAGE_FILTER_ROUTER_H_
 
+#include <array>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
@@ -37,7 +38,7 @@ class MessageFilterRouter {
   // responsibility to ensure that a filter is either global or selective to
   // ensure proper message filtering order.
   MessageFilters global_filters_;
-  MessageFilters message_class_filters_[LastIPCMsgStart];
+  std::array<MessageFilters, LastIPCMsgStart> message_class_filters_;
 };
 
 }  // namespace IPC
