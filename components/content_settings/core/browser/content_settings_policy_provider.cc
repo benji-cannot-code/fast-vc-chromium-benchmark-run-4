e2038140734388f1681ec273e82516ec131c4537
Luke Klimek <zgroza@chromium.org>
2025-04-17 03:55:53 -0700

    SmartCard: Add policy for setting the default content setting value
    Context: go/web-smart-card-api-permissions
    
    This adds a policy for controlling the default content setting value,
    in addition to *AllowedForUrls and *BlockedForUrls to align with the
    other new policies.
    
    Now blocking by default except for the allowlisted origins will require
    setting the DefaultSmartCardConnectSetting policy rather than putting a
    wildcard in SmartCardConnectBlockedForUrls.
    
    Bug: 410742241
    Change-Id: Id320ee6d60d7665d6f5caf58bc69723cb96b9ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6458639
    Commit-Queue: Luke Klimek <zgroza@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448239}

--- components/content_settings/core/browser/content_settings_policy_provider.cc
+++ components/content_settings/core/browser/content_settings_policy_provider.cc
@@ -250,6 +250,9 @@ constexpr const char* kManagedDefaultPrefs[] = {
     prefs::kManagedDefaultDirectSocketsSetting,
     prefs::kManagedDefaultDirectSocketsPrivateNetworkAccessSetting,
     prefs::kManagedDefaultControlledFrameSetting,
+#if BUILDFLAG(IS_CHROMEOS)
+    prefs::kManagedDefaultSmartCardConnectSetting
+#endif  // BUILDFLAG(IS_CHROMEOS)
 };
 
 void ReportCookiesAllowedForUrlsUsage(
@@ -365,6 +368,10 @@ const PolicyProvider::PrefsForManagedDefaultMapEntry
          prefs::kManagedDefaultDirectSocketsPrivateNetworkAccessSetting},
         {ContentSettingsType::CONTROLLED_FRAME,
          prefs::kManagedDefaultControlledFrameSetting},
+#if BUILDFLAG(IS_CHROMEOS)
+        {ContentSettingsType::SMART_CARD_GUARD,
+         prefs::kManagedDefaultSmartCardConnectSetting},
+#endif  // BUILDFLAG(IS_CHROMEOS)
 };
 
 // static
@@ -463,10 +470,9 @@ void PolicyProvider::GetContentSettingsFromPreferences() {
 
 #if BUILDFLAG(IS_CHROMEOS)
       if (entry.content_type == ContentSettingsType::SMART_CARD_GUARD &&
-          entry.setting == CONTENT_SETTING_ALLOW &&
           !pattern_pair.first.MatchesSingleOrigin()) {
-        VLOG(1) << "Smart card reader access cannot be allowed by wildcard, "
-                   "skipping pattern "
+        VLOG(1) << "Smart card reader access cannot be allowed or blocked by "
+                   "wildcard, skipping pattern."
                 << original_pattern_str;
         continue;
       }
