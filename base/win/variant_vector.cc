3a00a77d25ebc14a73bab6c29e1866cd76a5ec6e
Jan Keitel <jkeitel@google.com>
2025-04-17 08:20:30 -0700

    variant_vector: Use C++20 concepts and remove unneeded != operator
    No functionality changes.
    
    Bug: 40942274
    Change-Id: I7b56d6d057b9712251a89b65569a644e786d8dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6468712
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448323}

--- base/win/variant_vector.cc
+++ base/win/variant_vector.cc
@@ -87,14 +87,6 @@ VariantVector::~VariantVector() {
   Reset();
 }
 
-bool VariantVector::operator==(const VariantVector& other) const {
-  return !Compare(other);
-}
-
-bool VariantVector::operator!=(const VariantVector& other) const {
-  return !VariantVector::operator==(other);
-}
-
 void VariantVector::Reset() {
   vector_.clear();
   vartype_ = VT_EMPTY;
