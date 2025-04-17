c96a9fa34e5a6c0f96ac69197c635a0fd17dd72a
Elly <ellyjones@chromium.org>
2025-04-16 21:30:29 -0700

    media/midi: migrate to crypto/hash for sha256
    Bug: 41161661, 372283556
    Change-Id: I8b3895e143b7ddc5456bd49efe0a8859ca51c94b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6465935
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448145}

--- media/midi/midi_manager_alsa.cc
+++ media/midi/midi_manager_alsa.cc
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "build/build_config.h"
-#include "crypto/sha2.h"
+#include "crypto/hash.h"
 #include "media/midi/midi_service.h"
 #include "media/midi/midi_service.mojom.h"
 #include "media/midi/task_service.h"
@@ -397,9 +397,7 @@ std::string MidiManagerAlsa::MidiPort::JSONValue() const {
 //               mapping and just use a UUID or other random string.
 //               http://crbug.com/465320
 std::string MidiManagerAlsa::MidiPort::OpaqueKey() const {
-  uint8_t hash[crypto::kSHA256Length];
-  crypto::SHA256HashString(JSONValue(), hash, sizeof(hash));
-  return base::HexEncode(hash);
+  return base::HexEncode(crypto::hash::Sha256(JSONValue()));
 }
 
 bool MidiManagerAlsa::MidiPort::MatchConnected(const MidiPort& query) const {
