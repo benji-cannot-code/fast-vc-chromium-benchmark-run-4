a4ac623892a481e11b5fadc798d64e2b23baac3c
Liza Bipin <mlbipin@google.com>
2025-04-17 09:20:08 -0700

    [Signin][Android] Add HistoryOptInEducationalTip to chrome://flags
    Bug: 409242912
    Change-Id: If52099d87331966270294214332185250445cab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6468872
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448359}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4915,6 +4915,9 @@ extern const char kSupervisedUserLocalWebApprovalsDescription[];
 extern const char kHistoryPageHistorySyncPromoName[];
 extern const char kHistoryPageHistorySyncPromoDescription[];
 
+extern const char kHistoryOptInEducationalTipName[];
+extern const char kHistoryOptInEducationalTipDescription[];
+
 extern const char kWebSerialOverBluetoothName[];
 extern const char kWebSerialOverBluetoothDescription[];
 
