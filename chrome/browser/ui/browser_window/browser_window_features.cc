0c864e4b13a1aedd3ab2e466fd876c3545fa0a7f
David Yeung <dayeung@chromium.org>
2025-04-16 21:32:30 -0700

    Add a skeleton frame for TabStripController api
    This is necessary to start parallel work for
    go/chrome-browser-tabstrip-api-dd
    
    The CL introduces the new TabStripController which inherits from
    mojom::TabStripController. This CL does not do anything nor does it
    replace any controllers yet, it is simply a skeleton frame to kick off
    experimentation.
    
    Bug: 409086859
    Change-Id: I5a854d66a2318dcefbfd2788a7e33b9332915481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6439404
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448146}

--- chrome/browser/ui/browser_window/browser_window_features.cc
+++ chrome/browser/ui/browser_window/browser_window_features.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/session_service_tab_group_sync_observer.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/shared_tab_group_feedback_controller.h"
+#include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_controller_impl.h"
 #include "chrome/browser/ui/toasts/toast_controller.h"
 #include "chrome/browser/ui/toasts/toast_features.h"
 #include "chrome/browser/ui/toasts/toast_service.h"
@@ -161,6 +162,11 @@ void BrowserWindowFeatures::Init(BrowserWindowInterface* browser) {
       std::make_unique<lens::LensRegionSearchController>();
 
   tab_strip_model_ = browser->GetTabStripModel();
+
+  if (base::FeatureList::IsEnabled(features::kTabStripBrowserApi)) {
+    tab_strip_controller_ =
+        std::make_unique<TabStripControllerImpl>(browser, tab_strip_model_);
+  }
 
   memory_saver_bubble_controller_ =
       std::make_unique<memory_saver::MemorySaverBubbleController>(browser);
