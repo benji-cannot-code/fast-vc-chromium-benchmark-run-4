fbc576f4dee69a84530f19f437f5700332e69c54
Ming-Ying Chung <mych@chromium.org>
2025-04-17 02:00:48 -0700

    Split fetch keepalive's content browser tests into their own files.
    - Add a base helper class in `keep_alive_request_browsertest_util.*`
    - Move `sendBeacon()` related tests into standalone file
    - Move `fetchLater()` related tests into standalone file
    
    Subsequent CL will rename the file `keep_alive_url_browsertest.cc` to
    `fetch_keep_alive_browsertest.cc`. (Not doing in this CL because git
    cannot preserve commit history`).
    
    Bug: 382527001
    Change-Id: Icd733f03f683722085488ecfb4bffb2f0a3feed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6461165
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Auto-Submit: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448203}

--- content/browser/loader/keep_alive_url_loader.h
+++ content/browser/loader/keep_alive_url_loader.h
@@ -49,7 +49,7 @@ namespace content {
 class BrowserContext;
 class KeepAliveAttributionRequestHelper;
 class KeepAliveRequestTracker;
-class KeepAliveURLBrowserTestBase;
+class KeepAliveRequestBrowserTestBase;
 class KeepAliveURLLoaderService;
 class PolicyContainerHost;
 class RenderFrameHostImpl;
@@ -263,7 +263,7 @@ class CONTENT_EXPORT KeepAliveURLLoader
 
   void DeleteSelf();
 
-  friend class KeepAliveURLBrowserTestBase;
+  friend class KeepAliveRequestBrowserTestBase;
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
   //
