56380dff9d2f63b06db99be91d16fb951ef785f8
Abe Boujane <boujane@google.com>
2025-04-18 08:17:29 -0700

    remove ps service dependency on notice storage
    this cl removes the notice storage histogram emission from
    the ps service, into the notice service where it belongs.
    
    Bug: 409376841
    Change-Id: If736c71faed3f64e4a2ea3c4975eb686aa46738b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6455943
    Reviewed-by: Aashna Sheth <aashnas@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448870}

--- chrome/browser/privacy_sandbox/notice/notice_service.cc
+++ chrome/browser/privacy_sandbox/notice/notice_service.cc
@@ -30,6 +30,8 @@ PrivacySandboxNoticeService::PrivacySandboxNoticeService(
   desktop_view_manager_ = std::make_unique<DesktopViewManager>(this);
   CHECK(desktop_view_manager_);
 #endif  // !BUILDFLAG(IS_ANDROID)
+
+  EmitStartupHistograms();
 }
 
 PrivacySandboxNoticeService::~PrivacySandboxNoticeService() = default;
@@ -75,6 +77,13 @@ PrefService* PrivacySandboxNoticeService::GetPrefService() {
 
 NoticeCatalog* PrivacySandboxNoticeService::GetCatalog() {
   return catalog_.get();
+}
+
+void PrivacySandboxNoticeService::EmitStartupHistograms() {
+  for (const auto& [notice_id, notice] : catalog_->GetNoticeMap()) {
+    GetNoticeStorage()->RecordHistogramsOnStartup(GetPrefService(),
+                                                  notice->GetStorageName());
+  }
 }
 
 #if !BUILDFLAG(IS_ANDROID)
