3fe2f772ffd93d4bf5319bfce39170654edc9dce
Kent Tamura <tkent@chromium.org>
2025-04-18 00:49:00 -0700

    Spanification: Resolve UNSAFE_TODOs in inline_item_segment.*
    Iterator holds a span and an index instead of a pointer.
    This CL should have no behavior changes.
    
    Bug: 351564777
    Change-Id: I679cd55a86f1cee8888e71305b1d6225519cb767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6472157
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448771}

--- third_party/blink/renderer/core/layout/inline/inline_item_segment.h
+++ third_party/blink/renderer/core/layout/inline/inline_item_segment.h
@@ -134,7 +134,8 @@ class CORE_EXPORT InlineItemSegments {
    public:
     Iterator(unsigned start_offset,
              unsigned end_offset,
-             const InlineItemSegment* segment);
+             base::span<const InlineItemSegment> span,
+             unsigned segment_index);
 
     bool IsDone() const { return range_.start == end_offset_; }
 
@@ -148,7 +149,8 @@ class CORE_EXPORT InlineItemSegments {
 
    private:
     RunSegmenter::RunSegmenterRange range_;
-    const InlineItemSegment* segment_;
+    base::raw_span<const InlineItemSegment> span_;
+    unsigned segment_index_;
     unsigned start_offset_;
     unsigned end_offset_;
   };
@@ -188,13 +190,18 @@ class CORE_EXPORT InlineItemSegments {
   Vector<unsigned> items_to_segments_;
 };
 
-inline InlineItemSegments::Iterator::Iterator(unsigned start_offset,
-                                              unsigned end_offset,
-                                              const InlineItemSegment* segment)
-    : segment_(segment), start_offset_(start_offset), end_offset_(end_offset) {
+inline InlineItemSegments::Iterator::Iterator(
+    unsigned start_offset,
+    unsigned end_offset,
+    base::span<const InlineItemSegment> span,
+    unsigned segment_index)
+    : span_(span),
+      segment_index_(segment_index),
+      start_offset_(start_offset),
+      end_offset_(end_offset) {
   DCHECK_LT(start_offset, end_offset);
-  DCHECK_LT(start_offset, segment->EndOffset());
-  range_ = segment->ToRunSegmenterRange(start_offset_, end_offset_);
+  DCHECK_LT(start_offset, span[segment_index].EndOffset());
+  range_ = span[segment_index].ToRunSegmenterRange(start_offset_, end_offset_);
 }
 
 inline void InlineItemSegments::Iterator::operator++() {
@@ -204,9 +211,9 @@ inline void InlineItemSegments::Iterator::operator++() {
     return;
   }
   start_offset_ = range_.end;
-  // TODO(crbug.com/351564777): Resolve a buffer safety issue.
-  UNSAFE_TODO(++segment_);
-  range_ = segment_->ToRunSegmenterRange(start_offset_, end_offset_);
+  ++segment_index_;
+  range_ =
+      span_[segment_index_].ToRunSegmenterRange(start_offset_, end_offset_);
 }
 
 }  // namespace blink
