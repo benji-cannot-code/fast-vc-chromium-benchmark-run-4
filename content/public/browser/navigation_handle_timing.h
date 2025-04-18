9550e4c8131a3840d77cee6d1a2de57bfaf7c0cc
Hayato Ito <hayato@chromium.org>
2025-04-17 22:55:31 -0700

    Add UMA to measure FetchStartToRequestStart for Google Search
    This metric will help us understand the P90 latency between FetchStart
    and RequestStart, as described in go/p90-breakdown (internal document).
    
    Note that `FetchStart` is not guaranteed to always be set.
    Therefore, the total count of this metric is usually lower than other
    navigation metrics.
    
    Bug: 393980912
    Change-Id: I5aa47eff1b3169659c8dac17afedaaa2f0499ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6459483
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448750}

--- content/public/browser/navigation_handle_timing.h
+++ content/public/browser/navigation_handle_timing.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_NAVIGATION_HANDLE_TIMING_H_
 #define CONTENT_PUBLIC_BROWSER_NAVIGATION_HANDLE_TIMING_H_
 
+#include <optional>
+
 #include "base/time/time.h"
 #include "content/common/content_export.h"
 
@@ -23,6 +25,10 @@ struct CONTENT_EXPORT NavigationHandleTiming {
 
   // The time the URLLoader for the navigation started.
   base::TimeTicks loader_start_time;
+
+  // The time the browser is ready to fetch the first HTTP request.
+  // This is filled with URLResponseHead::request_start during navigation.
+  std::optional<base::TimeTicks> first_fetch_start_time;
 
   // The time the first HTTP request was sent. This is filled with
   // net::LoadTimingInfo::send_start during navigation.
