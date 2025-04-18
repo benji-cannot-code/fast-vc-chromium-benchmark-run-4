cacfb24027d658e54fab8269ab04d5ba5fef01fc
Vinny Persky <vinnypersky@google.com>
2025-04-18 16:30:40 -0700

    Fix Select BNPL issuer dialog UI
    This CL accomplishes a couple of UI bug fixes:
    1) Fixes the corner shading issue for linked BNPL issuer pills
    2) Updates the text to be ui::views::STYLE_PRIMARY to more closely match
    the mocks
    3) Sets the distance between each hover button to be
    views::DISTANCE_RELATED_CONTROL_VERTICAL to match the mock.
    
    Mock: https://screenshot.googleplex.com/4q4v5gNPZ6wKnkW
    
    Before Impl: https://screenshot.googleplex.com/9NURzy8QLE8AJRR
    
    After Impl: https://screenshot.googleplex.com/8GLNr4rpmy9rUys
    
    After Impl showing distance:
    https://screenshot.googleplex.com/Bxvanap4GM9Tvqy
    
    Bug: 356443046
    Change-Id: I328f1e30db1cac7c7e3c67ac858fcfc48d3b384d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6471388
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449056}

--- chrome/browser/ui/views/autofill/payments/bnpl_issuer_linked_pill.cc
+++ chrome/browser/ui/views/autofill/payments/bnpl_issuer_linked_pill.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_variant.h"
+#include "ui/compositor/layer.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/background.h"
@@ -24,7 +25,7 @@ BnplLinkedIssuerPill::BnplLinkedIssuerPill()
     : views::Label(l10n_util::GetStringUTF16(
                        IDS_AUTOFILL_CARD_BNPL_LINKED_ISSUER_PILL_LABEL),
                    views::style::CONTEXT_DIALOG_BODY_TEXT,
-                   views::style::STYLE_BODY_5) {
+                   views::style::STYLE_PRIMARY) {
   SetEnabledColor(kColorBnplIssuerLinkedPillForeground);
 }
 
@@ -35,13 +36,14 @@ void BnplLinkedIssuerPill::AddedToWidget() {
   // TODO(kylixrd): Find appropriate metrics on ChromeLayoutProvider.
   gfx::Size size = GetPreferredSize(views::SizeBounds());
   size.Enlarge(0, 4);
-  SetBackground(views::CreateRoundedRectBackground(
-      kColorBnplIssuerLinkedPillBackground,
-      gfx::RoundedCornersF(size.height() / 2)));
+  SetBackground(
+      views::CreateSolidBackground(kColorBnplIssuerLinkedPillBackground));
   SetBorder(views::CreateRoundedRectBorder(
       0, size.height() / 2,
       gfx::Insets::TLBR(2, size.height() / 2, 2, size.height() / 2),
       kColorBnplIssuerLinkedPillBackground));
+  SetPaintToLayer();
+  layer()->SetRoundedCornerRadius(gfx::RoundedCornersF(size.height() / 2));
 }
 
 BEGIN_METADATA(BnplLinkedIssuerPill)
--- chrome/browser/ui/views/autofill/payments/bnpl_issuer_view.cc
+++ chrome/browser/ui/views/autofill/payments/bnpl_issuer_view.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
+#include "ui/views/layout/layout_provider.h"
 #include "ui/views/style/typography.h"
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
@@ -54,6 +55,8 @@ BnplIssuerView::BnplIssuerView(
     : issuer_dialog_(issuer_dialog), controller_(controller) {
   SetOrientation(views::BoxLayout::Orientation::kVertical);
   auto* layout_provider = ChromeLayoutProvider::Get();
+  SetBetweenChildSpacing(layout_provider->GetDistanceMetric(
+      views::DISTANCE_RELATED_CONTROL_VERTICAL));
   int corner_radius =
       layout_provider->GetCornerRadiusMetric(views::Emphasis::kHigh);
   auto issuer_contexts = controller_->GetIssuerContexts();
