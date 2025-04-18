ac9a7bae41e5223d5863daba22c704da3522b8b2
Gauthier Ambard <gambard@chromium.org>
2025-04-18 07:44:06 -0700

    [iOS] Remove FacePile API
    No longer used.
    
    Bug: 407545906
    Change-Id: I9ef041144c33159879aec2e208ac0559f5cde194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469878
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448857}

--- ios/chrome/browser/share_kit/model/share_kit_service.mm
+++ ios/chrome/browser/share_kit/model/share_kit_service.mm
@@ -8,8 +8,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 ShareKitService::ShareKitService() = default;
 
 ShareKitService::~ShareKitService() = default;
-
-UIViewController* ShareKitService::FacePile(
-    ShareKitFacePileConfiguration* config) {
-  return nil;
-}
