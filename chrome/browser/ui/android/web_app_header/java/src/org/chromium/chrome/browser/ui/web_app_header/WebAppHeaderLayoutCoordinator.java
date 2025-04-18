f6924b3852021242a006a19cb9df14578aae8622
Vlad Korotkevich <vkorotkevich@google.com>
2025-04-18 00:52:30 -0700

    [Minimal UI] Fix missing updates of non-draggable area
    In the initial CL non-draggable area is not updated when view
    is in the process of layout. This is incorrect logic that migrated
    from prototyping, because width changes of the layout indicate
    that we're in the active layout phase, thus area was never updated.
    
    This CL removes isInLayout check, because we always receive signals
    in the active layout phase and after view has changed its bounds.
    
    Bug: 410749095
    Change-Id: I9f06c621a0e385b2e856c9e587ee6f06249a195b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6468655
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Vlad Korotkevich <vkorotkevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448776}

--- chrome/browser/ui/android/web_app_header/java/src/org/chromium/chrome/browser/ui/web_app_header/WebAppHeaderLayoutCoordinator.java
+++ chrome/browser/ui/android/web_app_header/java/src/org/chromium/chrome/browser/ui/web_app_header/WebAppHeaderLayoutCoordinator.java
@@ -103,7 +103,6 @@ public class WebAppHeaderLayoutCoordinator implements DesktopWindowStateManager.
                         mDesktopWindowStateManager,
                         mTabSupplier,
                         this::collectNonDraggableAreas,
-                        this::isPendingLayout,
                         headerMinHeight);
         PropertyModelChangeProcessor.create(model, mView, WebAppHeaderLayoutViewBinder::bind);
 
@@ -150,10 +149,6 @@ public class WebAppHeaderLayoutCoordinator implements DesktopWindowStateManager.
         return areas;
     }
 
-    private boolean isPendingLayout() {
-        return mView != null && (mView.isInLayout() || mView.isLayoutRequested());
-    }
-
     // TODO(vkorotkevich): Move to the Mediator
     @VisibleForTesting
     void refreshTab(boolean ignoreCache) {
@@ -175,6 +170,10 @@ public class WebAppHeaderLayoutCoordinator implements DesktopWindowStateManager.
      */
     public void destroy() {
         mDesktopWindowStateManager.removeObserver(this);
+
+        if (mView != null) {
+            mView.destroy();
+        }
 
         if (mMediator != null) {
             mMediator.destroy();
