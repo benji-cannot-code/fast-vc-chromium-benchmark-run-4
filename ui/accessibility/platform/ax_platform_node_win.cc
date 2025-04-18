1ac40e0b805a1bd673974a1cccd20003e856f82a
Greg Thompson <grt@chromium.org>
2025-04-18 11:47:56 -0700

    [accessibility] Detect destroyed nodes in InternalQueryInterface
    AXPlatformNodeWin::InternalQueryInterface must check that the object
    being queried isn't a platform node that has already been destroyed by
    its delegate.
    
    Bug: 411041133
    Change-Id: I1e939ad2405464421e16ebdfcb924291cfde72fa
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470917
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448954}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -5982,7 +5982,9 @@ STDMETHODIMP AXPlatformNodeWin::InternalQueryInterface(
   *object = nullptr;
   AXPlatformNodeWin* accessible =
       reinterpret_cast<AXPlatformNodeWin*>(this_ptr);
-  DCHECK(accessible);
+  if (!accessible || accessible->IsDestroyed()) {
+    return E_NOINTERFACE;
+  }
 
   if (riid == IID_IAccessibleTable || riid == IID_IAccessibleTable2) {
     if (!IsTableLike(accessible->GetRole()))
