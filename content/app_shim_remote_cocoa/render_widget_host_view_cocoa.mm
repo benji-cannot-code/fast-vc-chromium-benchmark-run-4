f7f88046208b49e1f6fc63a1d3c0f7ec35131079
Keren Zhu <kerenzhu@chromium.org>
2025-04-18 11:02:46 -0700

    mac: fix CHECK on IME reconversion due to invalid replacement range
    It appears that Google Japanese IME can call -setMarkedText: with an
    invalid replacement range when doing text reconversion (変換, i.e.,
    conversion between different text with same pronunciation). This range
    is a NSRange and NSRange.location is supposed to be NSNotFound (2^31-1)
    for invalid range, but the IME can pass in 2^32. Subsequently causing
    CHECK error.
    
    This CL fixes the issue by converting such invalid NSRange to
    gfx::InvalidRange using FromPossiblyInvalidNSRange(range).
    
    Fixed: 409864204
    Change-Id: I08ff426a933ef76aa81e33af59aa32e2ac0b674d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470915
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448935}

--- content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
+++ content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
@@ -2389,9 +2389,10 @@ extern NSString* NSTextInputReplacementRangeAttributeName;
   if ([self isHandlingKeyDown] && !_isReconversionTriggered) {
     _setMarkedTextReplacementRange = gfx::Range(replacementRange);
   } else {
-    _host->ImeSetComposition(_markedText, _imeTextSpans,
-                             gfx::Range(replacementRange), newSelRange.location,
-                             NSMaxRange(newSelRange));
+    _host->ImeSetComposition(
+        _markedText, _imeTextSpans,
+        gfx::Range::FromPossiblyInvalidNSRange(replacementRange),
+        newSelRange.location, NSMaxRange(newSelRange));
   }
 
   [[self inputContext] invalidateCharacterCoordinates];
