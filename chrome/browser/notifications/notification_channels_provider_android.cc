3578cca8fd0bcff4ffff9418a365616001400cfc
Min Qin <qinmin@chromium.org>
2025-04-18 11:10:42 -0700

    Remove notification content setting from PrefProvider during migration
    These lines are removed in https://chromium-review.googlesource.com/c/chromium/src/+/6438633. But this CL
    reintroduces them back as there is no need to keep the same
    settings in PrefProvider after migration.
    
    Change-Id: Ic6d885ca1fd0d7fafc65676b5a4c25720abc173e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469080
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448938}

--- chrome/browser/notifications/notification_channels_provider_android.cc
+++ chrome/browser/notifications/notification_channels_provider_android.cc
@@ -278,6 +278,12 @@ void NotificationChannelsProviderAndroid::MigrateToChannelsIfNecessaryImpl(
     }
   }
 
+  for (const auto& pattern : patterns) {
+    pref_provider->SetWebsiteSetting(
+        pattern.first, pattern.second, ContentSettingsType::NOTIFICATIONS,
+        base::Value(), {}, content_settings::PartitionKey::WipGetDefault());
+  }
+
   pref_service_->SetBoolean(prefs::kMigratedToSiteNotificationChannels, true);
 }
 
