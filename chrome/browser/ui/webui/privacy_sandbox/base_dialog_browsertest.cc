f12015f3ccb0f089ab6448044b738663efd88995
Jacob Stanley <jacobstanley@google.com>
2025-04-18 15:02:30 -0700

    [Privacy Narrative] Remove privacy_sandbox prefix to files
    All of the renamed files are already within the privacy_sandbox
    directory and within a privacy_sandbox namespace. Removing the prefix
    from the file names and class names makes it a bit cleaner.
    
    Bug: 398005782
    Change-Id: Id2c2affbd0db795f3ba4f7ac25e449334ec700fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6471272
    Auto-Submit: Jacob Stanley <jacobstanley@google.com>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449030}

--- chrome/browser/ui/webui/privacy_sandbox/base_dialog_browsertest.cc
+++ chrome/browser/ui/webui/privacy_sandbox/base_dialog_browsertest.cc
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "ui/base/l10n/l10n_util.h"
 
-class PrivacySandboxBaseDialogTest : public InProcessBrowserTest {};
+class BaseDialogTest : public InProcessBrowserTest {};
 
-IN_PROC_BROWSER_TEST_F(PrivacySandboxBaseDialogTest, PageLoadsWhenFeatureOn) {
+IN_PROC_BROWSER_TEST_F(BaseDialogTest, PageLoads) {
   GURL kUrl(chrome::kChromeUIPrivacySandboxBaseDialogURL);
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), kUrl));
   content::WebContents* web_contents =
