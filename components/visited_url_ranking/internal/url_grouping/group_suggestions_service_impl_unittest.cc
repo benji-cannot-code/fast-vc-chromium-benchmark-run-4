13fd3ae6ad540336775f7e7cd8b7d9b5dcdcb776
Yue Zhang <yuezhanggg@chromium.org>
2025-04-18 14:55:14 -0700

    [tab suggestions] Disable RecentlyOpen and SameOrigin by default
    Bug: 397221723
    Change-Id: I9fb4f16137b5fc5680fc8af79365db65bbf1f567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6471273
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449025}

--- components/visited_url_ranking/internal/url_grouping/group_suggestions_service_impl_unittest.cc
+++ components/visited_url_ranking/internal/url_grouping/group_suggestions_service_impl_unittest.cc
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/test/gmock_move_support.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/visited_url_ranking/internal/url_grouping/mock_suggestions_delegate.h"
 #include "components/visited_url_ranking/internal/url_grouping/tab_events_visit_transformer.h"
+#include "components/visited_url_ranking/public/features.h"
 #include "components/visited_url_ranking/public/tab_metadata.h"
 #include "components/visited_url_ranking/public/test_support.h"
 #include "components/visited_url_ranking/public/testing/mock_visited_url_ranking_service.h"
@@ -75,6 +77,9 @@ class GroupSuggestionsServiceImplTest : public testing::Test {
 
   void SetUp() override {
     Test::SetUp();
+    features_.InitAndEnableFeatureWithParameters(
+        features::kGroupSuggestionService,
+        {{"group_suggestion_enable_recently_opened", "true"}});
     auto* registry = pref_service_.registry();
     GroupSuggestionsServiceImpl::RegisterProfilePrefs(registry);
     mock_ranking_service_ = std::make_unique<MockVisitedURLRankingService>();
@@ -169,6 +174,7 @@ class GroupSuggestionsServiceImplTest : public testing::Test {
 
  protected:
   base::test::TaskEnvironment task_environment_;
+  base::test::ScopedFeatureList features_;
   TestingPrefServiceSimple pref_service_;
   std::unique_ptr<MockVisitedURLRankingService> mock_ranking_service_;
   std::unique_ptr<MockTabEventsVisitTransformer> mock_transformer_;
--- components/visited_url_ranking/public/features.cc
+++ components/visited_url_ranking/public/features.cc
@@ -155,7 +155,7 @@ BASE_FEATURE(kGroupSuggestionService,
 constexpr base::FeatureParam<bool> kGroupSuggestionEnableRecentlyOpened{
     &kGroupSuggestionService,
     /*name=*/"group_suggestion_enable_recently_opened",
-    /*default_value=*/true};
+    /*default_value=*/false};
 
 constexpr base::FeatureParam<bool> kGroupSuggestionEnableSwitchBetween{
     &kGroupSuggestionService,
@@ -170,6 +170,6 @@ constexpr base::FeatureParam<bool> kGroupSuggestionEnableSimilarSource{
 constexpr base::FeatureParam<bool> kGroupSuggestionEnableSameOrigin{
     &kGroupSuggestionService,
     /*name=*/"group_suggestion_enable_same_origin",
-    /*default_value=*/true};
+    /*default_value=*/false};
 
 }  // namespace visited_url_ranking::features
