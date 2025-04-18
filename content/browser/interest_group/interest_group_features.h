88b6ac253af6adbf26d43a0092eee81294dd8f79
Russ Hamilton <behamilton@google.com>
2025-04-18 11:15:40 -0700

    Always check for k-anonymity cache expiration after auctions.
    Metrics suggest that the k-anonymity cache isn't always being kept up to
    date. This change restores the previous behavior where we always check
    to see if we need a k-anonymity update whenever we check for an interest
    group update.
    
    Also increase the k_anonymity_service_client max queue size to 400,
    since this change will likely result in more requests being sent at
    once.
    
    Fixed: 411494023
    Change-Id: I6fb96f21411687c21d2dcfbd8ade2ad1acc39f38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470211
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448943}

--- content/browser/interest_group/interest_group_features.h
+++ content/browser/interest_group/interest_group_features.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 // Please keep features in alphabetical order.
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kAlwaysUpdateKAnon);
+
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kDetectInconsistentPageImpl);
 
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kEnableBandAClickiness);
