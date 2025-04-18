9bb39af074ec2598fa486113adcbb055bfe31b09
Rouslan Solomakhin <rouslan@google.com>
2025-04-18 14:03:52 -0700

    Pass the "modifiers" field to Android payment app.
    Before this patch, the PaymentRequestUpdateEvent.updateWith() JavaScript
    call [1] passed the "modifiers" field from the merchant website to the
    currently invoked (showing UI) Web Payment Handler [2] and internal
    Google Pay integration, but not to Android payment apps invoked through
    a PAY intent [3]. This resulted in an inconsistency for merchant
    websites that provide dynamic price updates. Such price updates
    typically happen when a user selects a different shipping address or
    shipping option.
    
    The "modifiers" field was originally passed to Android payment apps
    before 2020, but then was removed in commit
    60b04777084451359cd717fd7e6934e2aca1f89d, reviewed in
    https://crrev.com/c/2251089. Although that patch states that the "data"
    field can be used instead of "modifiers", no such field can be found in
    the code base. This patch is a revert of that commit.
    
    This patch makes PaymentRequestUpdateEvent.updateWith() pass the
    "modifiers" field from the merchant website to the Android payment app
    that it is currently invoking.
    
    After this patch, the interoperability between different types of
    payment apps is improved, so merchant websites can have fewer special
    cases.
    
    [1]https://w3c.github.io/payment-request/#updatewith-method
    [2]https://w3c.github.io/payment-handler/
    [3]https://web.dev/articles/android-payment-apps-developers-guide
    
    Bug: 411154309, 40108383
    Change-Id: If4db64033b6e8cc95fdd097bf40b5f3093d36829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6472587
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449003}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentDetailsUpdateServiceHelperTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentDetailsUpdateServiceHelperTest.java
@@ -43,6 +43,7 @@ import org.chromium.components.payments.PaymentDetailsUpdateServiceHelper;
 import org.chromium.components.payments.PaymentRequestUpdateEventListener;
 import org.chromium.components.payments.intent.WebPaymentIntentHelperType.PaymentCurrencyAmount;
 import org.chromium.components.payments.intent.WebPaymentIntentHelperType.PaymentHandlerMethodData;
+import org.chromium.components.payments.intent.WebPaymentIntentHelperType.PaymentHandlerModifier;
 import org.chromium.components.payments.intent.WebPaymentIntentHelperType.PaymentRequestDetailsUpdate;
 import org.chromium.components.payments.intent.WebPaymentIntentHelperType.PaymentShippingOption;
 import org.chromium.payments.mojom.PaymentAddress;
@@ -147,6 +148,13 @@ public class PaymentDetailsUpdateServiceHelperTest {
                         new PaymentCurrencyAmount("CAD", "0.00"),
                         /* selected= */ true));
 
+        // Populate modifiers.
+        List<PaymentHandlerModifier> modifiers = new ArrayList<PaymentHandlerModifier>();
+        modifiers.add(
+                new PaymentHandlerModifier(
+                        new PaymentCurrencyAmount(/* currency= */ "CAD", /* value= */ "2.00"),
+                        new PaymentHandlerMethodData("method name", "stringified method data")));
+
         // Populate address errors.
         Bundle bundledShippingAddressErrors = new Bundle();
         bundledShippingAddressErrors.putString("addressLine", "invalid address line");
@@ -164,6 +172,7 @@ public class PaymentDetailsUpdateServiceHelperTest {
                 new PaymentRequestDetailsUpdate(
                         total,
                         shippingOptions,
+                        modifiers,
                         /* error= */ "error message",
                         /* stringifiedPaymentMethodErrors= */ "stringified payment method",
                         bundledShippingAddressErrors);
@@ -203,6 +212,23 @@ public class PaymentDetailsUpdateServiceHelperTest {
         Assert.assertEquals("0.00", amount.getString(PaymentCurrencyAmount.EXTRA_VALUE));
         Assert.assertTrue(
                 shippingOption.getBoolean(PaymentShippingOption.EXTRA_SHIPPING_OPTION_SELECTED));
+
+        // Validate modifiers
+        Parcelable[] modifiers =
+                mUpdatedPaymentDetails.getParcelableArray(
+                        PaymentRequestDetailsUpdate.EXTRA_MODIFIERS);
+        Assert.assertEquals(1, modifiers.length);
+        Bundle modifier = (Bundle) modifiers[0];
+        Bundle modifierTotal = modifier.getBundle(PaymentHandlerModifier.EXTRA_TOTAL);
+        Assert.assertEquals("CAD", modifierTotal.getString(PaymentCurrencyAmount.EXTRA_CURRENCY));
+        Assert.assertEquals("2.00", modifierTotal.getString(PaymentCurrencyAmount.EXTRA_VALUE));
+        Bundle modifierMethodData = modifier.getBundle(PaymentHandlerModifier.EXTRA_METHOD_DATA);
+        Assert.assertEquals(
+                "method name",
+                modifierMethodData.getString(PaymentHandlerMethodData.EXTRA_METHOD_NAME));
+        Assert.assertEquals(
+                "stringified method data",
+                modifierMethodData.getString(PaymentHandlerMethodData.EXTRA_STRINGIFIED_DETAILS));
 
         Assert.assertEquals(
                 "error message",
