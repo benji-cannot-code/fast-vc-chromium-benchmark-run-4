eff30e289d249a24b8c75b9ceef227e6d2f82b89
Ewann Pelle <ewannpv@chromium.org>
2025-04-18 05:43:40 -0700

    [iOS] Updates leave/delete shared group on tab strip flow
    The group will be automatically leaved / deleted on after a `kSuccess`.
    
    Bug: 409472956
    Change-Id: I39d98d54ab94d178a2da4a7bedd56dcca506cc9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469893
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448831}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/coordinator/tab_strip_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/coordinator/tab_strip_coordinator.mm
@@ -476,19 +476,17 @@ using collaboration::CollaborationControllerDelegate;
       [_mediator deleteGroup:tabGroupItem];
       break;
     case TabGroupActionType::kLeaveSharedTabGroup:
+      [self runLeaveOrDeleteCompletion];
+      break;
     case TabGroupActionType::kLeaveOrKeepSharedTabGroup:
       [_mediator leaveSharedGroup:tabGroupItem];
       break;
     case TabGroupActionType::kDeleteSharedTabGroup:
+      [self runLeaveOrDeleteCompletion];
+      break;
     case TabGroupActionType::kDeleteOrKeepSharedTabGroup:
       [_mediator deleteSharedGroup:tabGroupItem];
       break;
-  }
-
-  if (self.leaveOrDeleteCompletion) {
-    self.leaveOrDeleteCompletion(
-        CollaborationControllerDelegate::Outcome::kSuccess);
-    self.leaveOrDeleteCompletion = nil;
   }
 
   [_tabGroupConfirmationCoordinator stop];
@@ -534,6 +532,15 @@ using collaboration::CollaborationControllerDelegate;
   if (self.leaveOrDeleteCompletion) {
     self.leaveOrDeleteCompletion(
         CollaborationControllerDelegate::Outcome::kCancel);
+  }
+  self.leaveOrDeleteCompletion = nil;
+}
+
+// Runs `leaveOrDeleteCompletion`. If not nil, calls it with `kSuccess`.
+- (void)runLeaveOrDeleteCompletion {
+  if (self.leaveOrDeleteCompletion) {
+    self.leaveOrDeleteCompletion(
+        CollaborationControllerDelegate::Outcome::kSuccess);
   }
   self.leaveOrDeleteCompletion = nil;
 }
