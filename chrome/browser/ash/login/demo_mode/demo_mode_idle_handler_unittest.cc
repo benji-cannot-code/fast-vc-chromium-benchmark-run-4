756d941823b044940b8cdad1b2ed7fb899741528
Haifan Wang <wanghaifan@google.com>
2025-04-17 18:25:08 -0700

    Clear and close the clipboard when the demo mode shopper session resets
    Upon the demo mode shopper session resets, the clipboard needs to be
    cleared. Clearing the clipboard will automatically close its window as
    there's no more contents in it.
    
    TEST=unit_test DemoModeIdleHandlerTest.*
    
    Bug: 392556702
    Change-Id: If1aed6eb9f6b4e7185abaf31f6fb362fc0bdafac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6451585
    Commit-Queue: Haifan Wang <wanghaifan@google.com>
    Reviewed-by: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448707}

--- chrome/browser/ash/login/demo_mode/demo_mode_idle_handler_unittest.cc
+++ chrome/browser/ash/login/demo_mode/demo_mode_idle_handler_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/demo_mode/demo_mode_idle_handler.h"
 
+#include "ash/clipboard/clipboard_history_controller_impl.h"
+#include "ash/clipboard/clipboard_history_item.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/metrics/demo_session_metrics_recorder.h"
 #include "ash/public/cpp/wallpaper/wallpaper_info.h"
@@ -17,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/login/demo_mode/demo_mode_window_closer.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
@@ -31,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/base/clipboard/scoped_clipboard_writer.h"
 #include "ui/base/user_activity/user_activity_detector.h"
 
 namespace ash {
@@ -159,6 +163,43 @@ TEST_F(DemoModeIdleHandlerTest, CloseAllBrowsers) {
 
   EXPECT_EQ(get_launch_demo_app_count(), 1);
   EXPECT_TRUE(BrowserList::GetInstance()->empty());
+}
+
+TEST_F(DemoModeIdleHandlerTest, ClearAndCloseClipboard) {
+  ash::ClipboardHistoryControllerImpl* clipboard_history_controller =
+      ash::Shell::Get()->clipboard_history_controller();
+  base::test::TestFuture<bool> operation_confirmed_future;
+  clipboard_history_controller->set_confirmed_operation_callback_for_test(
+      operation_confirmed_future.GetRepeatingCallback());
+
+  // Write text to the clipboard
+  {
+    ui::ScopedClipboardWriter scw(ui::ClipboardBuffer::kCopyPaste);
+    scw.WriteText(u"test");
+  }
+  // Wait for the operation to be confirmed.
+  EXPECT_TRUE(operation_confirmed_future.Take());
+
+  // Make sure the clipboard is not empty.
+  const std::list<ClipboardHistoryItem>& items =
+      clipboard_history_controller->history()->GetItems();
+  EXPECT_TRUE(!items.empty());
+
+  // Show the clipboard menu.
+  PressAndReleaseKey(ui::VKEY_V, ui::EF_COMMAND_DOWN);
+  EXPECT_TRUE(clipboard_history_controller->IsMenuShowing());
+
+  // Trigger clearing clipboard by being idle for `kReLuanchDemoAppIdleDuration`
+  // period of time.
+  SimulateUserActivity();
+  FastForwardBy(kReLuanchDemoAppIdleDuration);
+
+  // Expect the clipboard to be not showing.
+  EXPECT_FALSE(clipboard_history_controller->IsMenuShowing());
+  // And nothing in the clipboard.
+  const std::list<ClipboardHistoryItem>& no_items =
+      clipboard_history_controller->history()->GetItems();
+  EXPECT_TRUE(no_items.empty());
 }
 
 TEST_F(DemoModeIdleHandlerTest, ResetWallpaper) {
