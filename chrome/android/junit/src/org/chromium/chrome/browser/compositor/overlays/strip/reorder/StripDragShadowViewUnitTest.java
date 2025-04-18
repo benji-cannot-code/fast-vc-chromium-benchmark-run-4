1e51d372926c97333bd1b555107e6b340d48265e
Neil Coronado <nemco@google.com>
2025-04-17 17:36:52 -0700

    [TabGroupDragDrop] Limit drag shadow size
    Limit drag shadow size.
    -Make as big as possible with maxWidth=265dp and maxHeight=265dp.
    
    Bug: 410046285
    Change-Id: I12ebf31dd99fe5e3f7013759bfbffe4eff3238fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6463538
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1448689}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/StripDragShadowViewUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/StripDragShadowViewUnitTest.java
@@ -41,12 +41,14 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
 import org.chromium.base.Token;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.compositor.LayerTitleCache;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabUtils;
@@ -75,6 +77,7 @@ public class StripDragShadowViewUnitTest {
     @Captor private ArgumentCaptor<FaviconImageCallback> mGetFaviconCallbackCaptor;
     @Captor private ArgumentCaptor<Callback<Bitmap>> mGetThumbnailCallbackCaptor;
 
+    @Mock private BrowserControlsStateProvider mMockBrowserControlsStateProvider;
     @Mock private MultiThumbnailCardProvider mMockMultiThumbnailCardProvider;
     @Mock private Supplier<TabContentManager> mMockTabContentManagerSupplier;
     @Mock private Supplier<LayerTitleCache> mMockLayerTitleCacheSupplier;
@@ -127,7 +130,7 @@ public class StripDragShadowViewUnitTest {
                                 .getLayoutInflater()
                                 .inflate(R.layout.strip_drag_shadow_view, null);
         mStripDragShadowView.initialize(
-                /* browserControlsStateProvider= */ null,
+                mMockBrowserControlsStateProvider,
                 mMockMultiThumbnailCardProvider,
                 mMockTabContentManagerSupplier,
                 mMockLayerTitleCacheSupplier,
@@ -166,13 +169,28 @@ public class StripDragShadowViewUnitTest {
     }
 
     @Test
-    public void testUpdate_LayoutSize() {
+    @Config(qualifiers = "w640dp-h360dp")
+    public void testUpdate_LayoutSize_Landscape() {
         mStripDragShadowView.prepareForTabDrag(mMockTab, 0);
 
         int expectedWidth = StripDragShadowView.WIDTH_DP;
         int expectedHeight =
                 TabUtils.deriveGridCardHeight(
-                        expectedWidth, mActivity, /* browserControlsStateProvider= */ null);
+                        expectedWidth, mActivity, mMockBrowserControlsStateProvider);
+        LayoutParams layoutParams = mStripDragShadowView.getLayoutParams();
+        assertEquals("Unexpected view width.", expectedWidth, layoutParams.width);
+        assertEquals("Unexpected view height.", expectedHeight, layoutParams.height);
+    }
+
+    @Test
+    @Config(qualifiers = "w360dp-h640dp")
+    public void testUpdate_LayoutSize_Portrait() {
+        mStripDragShadowView.prepareForTabDrag(mMockTab, 0);
+
+        int expectedHeight = StripDragShadowView.HEIGHT_DP;
+        int expectedWidth =
+                TabUtils.deriveGridCardWidth(
+                        expectedHeight, mActivity, mMockBrowserControlsStateProvider);
         LayoutParams layoutParams = mStripDragShadowView.getLayoutParams();
         assertEquals("Unexpected view width.", expectedWidth, layoutParams.width);
         assertEquals("Unexpected view height.", expectedHeight, layoutParams.height);
