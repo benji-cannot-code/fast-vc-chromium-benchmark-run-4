80451ff7baf8dc8858383828d38df5042260de2c
Lukasz Anforowicz <lukasza@chromium.org>
2025-04-21 14:49:44 -0700

    [rust] Remove `//third_party/beto-core` deps from `//third_party/rust`.
    https://crrev.com/c/6472852 has removed `//third_party/beto-core` from
    Chromium.  This is a follow-up CL that removes `//third_party/rust`
    crates that were dependencies of `beto-core`.
    
    This CL has been created semi-automatically.  The following files were
    edited manually to remove crates that were only needed for `beto-core`:
    
    * `//third_party/rust/chromium_crates_io/Cargo.toml`
    * `//third_party/rust/chromium_crates_io/gnrt_config.toml`
    
    The remaining changes were generated by running the following commands:
    
    * `tools/crates/run_gnrt.py vendor`
    * `tools/crates/run_gnrt.py gen`
    
    Bug: 6472852
    Change-Id: I620194c7fe456005eaae112beae2893dc0be22e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476826
    Reviewed-by: Nabil Wadih <nwadih@google.com>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449626}

--- third_party/rust/chromium_crates_io/vendor/getrandom-v0_2/src/linux_android.rs
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! Implementation for Linux / Android without `/dev/urandom` fallback
-use crate::{util_libc, Error};
-use core::mem::MaybeUninit;
-
-pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
-    util_libc::sys_fill_exact(dest, util_libc::getrandom_syscall)
-}
