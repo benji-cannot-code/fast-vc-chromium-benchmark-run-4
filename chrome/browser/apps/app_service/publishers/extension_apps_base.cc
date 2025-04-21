d1dcfb8a6861a94db100e4c068d96b026e242318
James Cook <jamescook@chromium.org>
2025-04-21 11:29:13 -0700

    extensions: Port ComponentLoaderTest to desktop Android
    Change the base class for most ComponentLoaderTests to testing::Test,
    since ExtensionServiceUserTestBase is not supported on Android.
    
    Wrap a couple of tests that depend on user setup functionality
    in ENABLE_EXTENSIONS so they can still use
    ExtensionServiceUserTestBase.
    
    Bug: 408454704
    Change-Id: I11aca1e3eb248a3e962852ee9899f0e0f645dbe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6465162
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449529}

--- chrome/browser/apps/app_service/publishers/extension_apps_base.cc
+++ chrome/browser/apps/app_service/publishers/extension_apps_base.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/check_is_test.h"
 #include "base/functional/callback.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/one_shot_event.h"
@@ -164,9 +165,14 @@ AppPtr ExtensionAppsBase::CreateAppImpl(const extensions::Extension* extension,
 
   const extensions::ManagementPolicy* policy =
       extensions::ExtensionSystem::Get(profile())->management_policy();
-  DCHECK(policy);
-  app->allow_uninstall = policy->UserMayModifySettings(extension, nullptr) &&
-                         !policy->MustRemainInstalled(extension, nullptr);
+  // `policy` may be null in unit tests.
+  if (policy) {
+    app->allow_uninstall = policy->UserMayModifySettings(extension, nullptr) &&
+                           !policy->MustRemainInstalled(extension, nullptr);
+  } else {
+    CHECK_IS_TEST();
+    app->allow_uninstall = true;
+  }
   app->allow_close = true;
   return app;
 }
