10515dfe4594bbc6dad2a1b1d8611fe0cac6f207
Kenichi Ishibashi <bashi@chromium.org>
2025-04-21 00:15:06 -0700

    Use Flow for ResourceScheduler trace events
    Having a separate track for each URLLoader/URLRequest could be
    very noisy on the UI. Use a flow that connects other
    URLLoader/URLRequest events.
    
    Bug: 385170278
    Change-Id: Ie6b19ecd6c418cd0ed64f398641a7e0a19048350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6472608
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449374}

--- services/network/resource_scheduler/resource_scheduler.cc
+++ services/network/resource_scheduler/resource_scheduler.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/tracing.h"
 #include "net/http/http_server_properties.h"
 #include "net/log/net_log.h"
+#include "net/log/net_log_util.h"
 #include "net/nqe/effective_connection_type_observer.h"
 #include "net/nqe/network_quality_estimator.h"
 #include "net/nqe/peer_to_peer_connections_count_observer.h"
@@ -235,7 +236,7 @@ class ResourceScheduler::ScheduledResourceRequestImpl
                                bool visible,
                                bool is_async)
       : client_id_(client_id),
-        trace_track_(base::trace_event::GetNextGlobalTraceId()),
+        flow_(NetLogWithSourceToFlow(request->net_log())),
         request_(request),
         ready_(false),
         deferred_(false),
@@ -255,9 +256,8 @@ class ResourceScheduler::ScheduledResourceRequestImpl
       priority_.priority = net::RequestPriority::IDLE;
       request_->SetPriority(priority_.priority);
     }
-    TRACE_EVENT_BEGIN("network.scheduler", "ScheduledResourceRequest",
-                      trace_track_, "url", request->url(), "priority",
-                      priority_.priority);
+    TRACE_EVENT("network.scheduler", "ScheduledResourceRequest", flow_,
+                "priority", priority_.priority);
 
     DCHECK(!request_->GetUserData(kUserDataKey));
     request_->SetUserData(kUserDataKey, std::make_unique<UnownedPointer>(this));
@@ -268,7 +268,6 @@ class ResourceScheduler::ScheduledResourceRequestImpl
       delete;
 
   ~ScheduledResourceRequestImpl() override {
-    TRACE_EVENT_END("network.scheduler", trace_track_);
     request_->RemoveUserData(kUserDataKey);
     scheduler_->RemoveRequest(this);
   }
@@ -282,8 +281,8 @@ class ResourceScheduler::ScheduledResourceRequestImpl
   // Starts the request. If |start_mode| is START_ASYNC, the request will not
   // be started immediately.
   void Start(StartMode start_mode) {
-    TRACE_EVENT_INSTANT("network.scheduler", "RequestStart", trace_track_,
-                        "mode", start_mode == START_ASYNC ? "async" : "sync");
+    TRACE_EVENT("network.scheduler", "ScheduledResourceRequest::Start", flow_,
+                "mode", start_mode == START_ASYNC ? "async" : "sync");
     DCHECK(!ready_);
 
     // If the request was deferred, need to start it.  Otherwise, will just not
@@ -307,9 +306,9 @@ class ResourceScheduler::ScheduledResourceRequestImpl
   }
 
   void Reprioritize(const RequestPriorityParams& priority) {
-    TRACE_EVENT_INSTANT("network.scheduler", "RequestReprioritize",
-                        trace_track_, "old_priority", priority_.priority,
-                        "new_priority", priority.priority);
+    TRACE_EVENT("network.scheduler", "ScheduledResourceRequest::Reprioritize",
+                flow_, "old_priority", priority_.priority, "new_priority",
+                priority.priority);
     priority_ = priority;
   }
 
@@ -324,7 +323,7 @@ class ResourceScheduler::ScheduledResourceRequestImpl
     return preserved_priority_;
   }
   ClientId client_id() const { return client_id_; }
-  perfetto::Track trace_track() const { return trace_track_; }
+  perfetto::Flow flow() const { return flow_; }
   net::URLRequest* url_request() { return request_; }
   const net::URLRequest* url_request() const { return request_; }
   bool is_async() const { return is_async_; }
@@ -364,12 +363,12 @@ class ResourceScheduler::ScheduledResourceRequestImpl
   // ScheduledResourceRequest implemnetation
   void WillStartRequest(bool* defer) override {
     deferred_ = *defer = !ready_;
-    TRACE_EVENT_INSTANT("network.scheduler", "RequestWillStart", trace_track_,
-                        "defered", deferred_);
+    TRACE_EVENT("network.scheduler", "ScheduledResourceRequest::WillStart",
+                flow_, "defered", deferred_);
   }
 
   const ClientId client_id_;
-  perfetto::Track trace_track_;
+  const perfetto::Flow flow_;
   raw_ptr<net::URLRequest> request_;
   bool ready_;
   bool deferred_;
@@ -411,16 +410,16 @@ bool ResourceScheduler::ScheduledResourceSorter::operator()(
 void ResourceScheduler::RequestQueue::Insert(
     ScheduledResourceRequestImpl* request) {
   DCHECK(!base::Contains(pointers_, request));
-  TRACE_EVENT_INSTANT("network.scheduler", "RequestEnqueue",
-                      request->trace_track());
+  TRACE_EVENT("network.scheduler", "ResourceScheduler::RequestQueue::Insert",
+              request->flow());
   request->set_fifo_ordering(MakeFifoOrderingId());
   pointers_[request] = queue_.insert(request);
 }
 
 void ResourceScheduler::RequestQueue::Erase(
     ScheduledResourceRequestImpl* request) {
-  TRACE_EVENT_INSTANT("network.scheduler", "RequestDequeue",
-                      request->trace_track());
+  TRACE_EVENT("network.scheduler", "ResourceScheduler::RequestQueue::Erase",
+              request->flow());
   PointerMap::iterator it = pointers_.find(request);
   CHECK(it != pointers_.end());
   queue_.erase(it->second);
