7eb11da36fcee78eeb1d7302ffa061939b67eb42
Robert Liao <robliao@chromium.org>
2025-04-21 15:02:27 -0700

    Remove BrowserViewLayoutDelegate::BrowserIs*
    All BrowserIs* methods return unchanging values, which means this value
    can be computed once and set as a policy on the BrowserViewLayout.
    
    Bug: 40756914
    Change-Id: I02109d14a443f2b9e2ba0f2bdf86918d981ee910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476708
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449631}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -1152,6 +1152,9 @@ class BrowserView : public BrowserWindow,
   // when it should not be able to.
   void UpdateFullscreenAllowedFromPolicy(bool allowed_without_policy);
 
+  bool ShouldUseBrowserContentMinimumSize() const;
+  bool IsBrowserAWebApp() const;
+
   // The BrowserFrame that hosts this view.
   raw_ptr<BrowserFrame> frame_ = nullptr;
 
