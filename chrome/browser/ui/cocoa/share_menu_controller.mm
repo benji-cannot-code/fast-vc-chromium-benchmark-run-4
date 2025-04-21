a60cdead5f7b73468aa0ec111d5200cf82ffeb30
Leonard Grey <lgrey@chromium.org>
2025-04-21 14:16:27 -0700

    Mac: clean up DirectEmailShare
    We've given it a little while, and Canary crash rate is now at zero
    and other channels have nothing from 7075 on.
    
    Bug: 356643975
    Change-Id: Ie06e1069cc754f3af866f3831e7f8ebfa2f9be1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6475490
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449607}

--- chrome/browser/ui/cocoa/share_menu_controller.mm
+++ chrome/browser/ui/cocoa/share_menu_controller.mm
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #import "chrome/browser/ui/cocoa/accelerators_cocoa.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/omnibox/browser/location_bar_model.h"
 #include "net/base/apple/url_conversions.h"
@@ -98,19 +97,16 @@ bool CanShare() {
   // to fetch sharing services that can handle the NSURL type.
   NSArray* services = [NSSharingService
       sharingServicesForItems:@[ [NSURL URLWithString:@"https://google.com"] ]];
-  bool directMail =
-      base::FeatureList::IsEnabled(features::kMacDirectEmailShare);
-  if (directMail) {
-    NSMenuItem* email = [[NSMenuItem alloc]
-        initWithTitle:l10n_util::GetNSString(IDS_EMAIL_LINK_MAC)
-               action:@selector(emailLink:)
-        keyEquivalent:[self keyEquivalentForMail]];
-    email.target = self;
-    [menu addItem:email];
-  }
+  NSMenuItem* email = [[NSMenuItem alloc]
+      initWithTitle:l10n_util::GetNSString(IDS_EMAIL_LINK_MAC)
+             action:@selector(emailLink:)
+      keyEquivalent:[self keyEquivalentForMail]];
+  email.target = self;
+  [menu addItem:email];
   for (NSSharingService* service in services) {
-    if (directMail &&
-        [service.name isEqualToString:NSSharingServiceNameComposeEmail]) {
+    // Email share service causes mysterious crashes, so share directly.
+    // See https://crbug.com/356643975
+    if ([service.name isEqualToString:NSSharingServiceNameComposeEmail]) {
       continue;
     }
     // Don't include "Add to Reading List".
@@ -291,13 +287,9 @@ bool CanShare() {
 
 // Creates a menu item that calls |service| when invoked.
 - (NSMenuItem*)menuItemForService:(NSSharingService*)service {
-  BOOL isMail = [service.name isEqual:NSSharingServiceNameComposeEmail];
-  NSString* keyEquivalent = isMail ? [self keyEquivalentForMail] : @"";
-  NSString* title = isMail ? l10n_util::GetNSString(IDS_EMAIL_LINK_MAC)
-                           : service.menuItemTitle;
-  NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:title
+  NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:service.menuItemTitle
                                                 action:@selector(performShare:)
-                                         keyEquivalent:keyEquivalent];
+                                         keyEquivalent:@""];
   item.target = self;
   item.image = service.image;
   item.representedObject = service;
