3df00c38935e9521cc6885e5160cea4197f82a29
Moe Ahmadi <mahmadi@chromium.org>
2025-04-21 09:30:13 -0700

    [lens] Adds c/b/ui/lens:test_support BULD rule
    Bug: 408513470
    Change-Id: I6331782077059e8cc57bf006e9f79d016751b9ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6475445
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449469}

--- chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
+++ chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
@@ -70,7 +70,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/lens/lens_permission_bubble_controller.h"
 #include "chrome/browser/ui/lens/lens_search_controller.h"
 #include "chrome/browser/ui/lens/lens_side_panel_untrusted_ui.h"
+#include "chrome/browser/ui/lens/test_lens_overlay_controller.h"
 #include "chrome/browser/ui/lens/test_lens_overlay_query_controller.h"
+#include "chrome/browser/ui/lens/test_lens_search_controller.h"
 #include "chrome/browser/ui/location_bar/location_bar.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -507,7 +509,7 @@ class LensOverlayPageFake : public lens::mojom::LensPage {
 };
 
 // Stubs out network requests and mojo calls.
-class LensOverlayControllerFake : public LensOverlayController {
+class LensOverlayControllerFake : public lens::TestLensOverlayController {
  public:
   LensOverlayControllerFake(tabs::TabInterface* tab,
                             variations::VariationsClient* variations_client,
@@ -515,12 +517,12 @@ class LensOverlayControllerFake : public LensOverlayController {
                             PrefService* pref_service,
                             syncer::SyncService* sync_service,
                             ThemeService* theme_service)
-      : LensOverlayController(tab,
-                              variations_client,
-                              identity_manager,
-                              pref_service,
-                              sync_service,
-                              theme_service) {}
+      : lens::TestLensOverlayController(tab,
+                                        variations_client,
+                                        identity_manager,
+                                        pref_service,
+                                        sync_service,
+                                        theme_service) {}
 
   std::unique_ptr<lens::LensOverlayQueryController> CreateLensQueryController(
       lens::LensOverlayFullImageResponseCallback full_image_callback,
@@ -624,10 +626,10 @@ class LensOverlayControllerFake : public LensOverlayController {
       &fake_overlay_page_};
 };
 
-class LensSearchControllerFake : public LensSearchController {
+class LensSearchControllerFake : public lens::TestLensSearchController {
  public:
   explicit LensSearchControllerFake(tabs::TabInterface* tab)
-      : LensSearchController(tab) {}
+      : lens::TestLensSearchController(tab) {}
 
  protected:
   std::unique_ptr<LensOverlayController> CreateLensOverlayController(
---
+++ chrome/browser/ui/lens/test_lens_search_controller.h
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_LENS_TEST_LENS_SEARCH_CONTROLLER_H_
+#define CHROME_BROWSER_UI_LENS_TEST_LENS_SEARCH_CONTROLLER_H_
+
+#include "chrome/browser/ui/lens/lens_search_controller.h"
+
+namespace variations {
+class VariationsClient;
+}  // namespace variations
+
+namespace lens {
+
+class TestLensSearchController : public LensSearchController {
+ public:
+  explicit TestLensSearchController(tabs::TabInterface* tab)
+      : LensSearchController(tab) {}
+
+  std::unique_ptr<LensOverlayController> CreateLensOverlayController(
+      tabs::TabInterface* tab,
+      variations::VariationsClient* variations_client,
+      signin::IdentityManager* identity_manager,
+      PrefService* pref_service,
+      syncer::SyncService* sync_service,
+      ThemeService* theme_service) override;
+};
+
+}  // namespace lens
+
+#endif  // CHROME_BROWSER_UI_LENS_TEST_LENS_SEARCH_CONTROLLER_H_
