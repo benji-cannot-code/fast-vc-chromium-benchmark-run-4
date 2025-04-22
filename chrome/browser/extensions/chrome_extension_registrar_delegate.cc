5ffc0e8221e72484cef0ddfb094889a968f9bfb6
James Cook <jamescook@chromium.org>
2025-04-21 18:16:43 -0700

    extensions: Add ExtensionManagement to ChromeExtensionRegistrarDelegate
    Specifically on desktop Android. That code was ifdef'd out while we
    were porting ExtensionManagement to desktop Android. It's ported now,
    so we can put the code back.
    
    Bug: 394876083
    Change-Id: I8c55042a9d99c9c66b76e6b16dd401c5cf8e1326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476829
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449700}

--- chrome/browser/extensions/chrome_extension_registrar_delegate.cc
+++ chrome/browser/extensions/chrome_extension_registrar_delegate.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_allowlist.h"
 #include "chrome/browser/extensions/extension_assets_manager.h"
 #include "chrome/browser/extensions/extension_disabled_ui.h"
+#include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/extension_special_storage_policy.h"
 #include "chrome/browser/extensions/external_install_manager.h"
 #include "chrome/browser/extensions/install_verifier.h"
@@ -57,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/extension_sync_service.h"
 #include "chrome/browser/ui/webui/theme_source.h"
 #endif
@@ -375,16 +375,12 @@ void ChromeExtensionRegistrarDelegate::OnExtensionInstalled(
 
       pending_extension_manager->Remove(id);
 
-// TODO(crbug.com/394876083): Enable the following code on desktop Android
-// when ExtensionManagement is ported to desktop Android.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
       ExtensionManagement* management =
           ExtensionManagementFactory::GetForBrowserContext(profile_);
       LOG(WARNING) << "ShouldAllowInstall() returned false for " << id
                    << " of type " << extension->GetType() << " and update URL "
                    << management->GetEffectiveUpdateURL(*extension).spec()
                    << "; not installing";
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
       // Delete the extension directory since we're not going to
       // load it.
@@ -420,9 +416,6 @@ void ChromeExtensionRegistrarDelegate::OnExtensionInstalled(
     disable_reasons.erase(disable_reason::DISABLE_UNSUPPORTED_REQUIREMENT);
   }
 
-// TODO(crbug.com/394876083): Enable the following code on desktop Android
-// when ExtensionManagement is ported to desktop Android.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
   // Check if the extension was disabled because of the minimum version
   // requirements from enterprise policy, and satisfies it now.
   if (ExtensionManagementFactory::GetForBrowserContext(profile_)
@@ -430,7 +423,6 @@ void ChromeExtensionRegistrarDelegate::OnExtensionInstalled(
     // And remove the corresponding disable reason.
     disable_reasons.erase(disable_reason::DISABLE_UPDATE_REQUIRED_BY_POLICY);
   }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
   if (install_flags & kInstallFlagIsBlocklistedForMalware) {
     // Installation of a blocklisted extension can happen from sync, policy,
