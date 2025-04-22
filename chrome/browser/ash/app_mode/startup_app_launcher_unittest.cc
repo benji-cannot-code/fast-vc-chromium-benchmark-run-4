75a9621f2288c30cd0c75f71144206244070b951
Di Wu <diwux@google.com>
2025-04-22 01:16:10 -0700

    Delete excessive includes of chrome_app_kiosk_service.mojom.h
    chrome_app_kiosk_service.mojom was initially introduced in this CL
    crrev.com/c/3440127 to ease the transition into Lacros. Later on it was
    used for both Lacros and Ash scenarios.
    
    Even though Lacros was sunsetted, due to its current exposure to Ash
    code, chrome_app_kiosk_service.mojo cannot be simply removed. However,
    there do exist excessive includes here and there that were for Lacros
    code and can be now safely removed.
    
    Bug: b:354842935
    Test: tryjob
    
    Delete unused chrome_app_kiosk_service_ash{h,cc} and friends
    
    chrome_app_kiosk_service_ash.{h,cc} was solely made for Lacros. So
    remove them.
    
    Bug: b:354842935
    Test: tryjob
    Change-Id: I6326695b6caa195cb2c044c28229d07503d0f7cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6474985
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449785}

--- chrome/browser/ash/app_mode/startup_app_launcher_unittest.cc
+++ chrome/browser/ash/app_mode/startup_app_launcher_unittest.cc
@@ -52,8 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile_manager.h"
 #include "chromeos/ash/components/login/login_state/login_state.h"
 #include "chromeos/ash/components/settings/cros_settings_names.h"
-#include "chromeos/crosapi/mojom/chrome_app_kiosk_service.mojom-forward.h"
-#include "chromeos/crosapi/mojom/chrome_app_kiosk_service.mojom-shared.h"
 #include "components/account_id/account_id.h"
 #include "components/policy/core/common/device_local_account_type.h"
 #include "components/sync/model/string_ordinal.h"
