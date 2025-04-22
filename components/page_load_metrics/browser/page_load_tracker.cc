3c2896ee221e4c7f5204f1c99b9f693214761fb4
Lukasz Anforowicz <lukasza@chromium.org>
2025-04-22 07:18:18 -0700

    [code health] Replace a `raw_ptr<RFH>` with `GlobalRenderFrameHostId`.
    This CL deletes an `AcrossTasksDanglingUntriaged` annotation from the
    `PageLoadTracker::page_main_frame_` field by changing the field type.
    The field was only used for pointer-equality/object-identity comparison,
    so we can use `GlobalRenderFrameHostId` instead of a pointer.
    
    Fixed: 41485244
    Change-Id: Ic5593c4f67b1a5aed84a2990c9d5e8a950130430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476827
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449904}

--- components/page_load_metrics/browser/page_load_tracker.cc
+++ components/page_load_metrics/browser/page_load_tracker.cc
@@ -978,8 +978,8 @@ void PageLoadTracker::MediaStartedPlaying(
 }
 
 bool PageLoadTracker::IsPageMainFrame(content::RenderFrameHost* rfh) const {
-  DCHECK(page_main_frame_);
-  return rfh == page_main_frame_;
+  DCHECK(page_main_frame_id_);
+  return rfh->GetGlobalId() == page_main_frame_id_;
 }
 
 void PageLoadTracker::OnTimingChanged() {
@@ -1490,7 +1490,7 @@ void PageLoadTracker::AddCustomUserTimings(
 }
 
 void PageLoadTracker::SetPageMainFrame(content::RenderFrameHost* rfh) {
-  page_main_frame_ = rfh;
+  page_main_frame_id_ = rfh->GetGlobalId();
 }
 
 base::WeakPtr<PageLoadTracker> PageLoadTracker::GetWeakPtr() {
