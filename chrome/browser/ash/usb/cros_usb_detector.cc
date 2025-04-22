15c0891eaab6dafb2abd6645ce37b1c29d0057a6
maciek swiech <drmasquatch@google.com>
2025-04-22 08:56:50 -0700

    Additional logging around persisted USB sharing.
    Adding one additional log that could help determing if codepath which
    causes entries to be added into `persistent_passthrough_devices` is
    being triggered via an unknown mechanism.
    
    Bug: b:407526401
    Change-Id: If240b327738ba3dc8ce39e5a77bda33e378d792e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6460449
    Commit-Queue: maciek swiech <drmasquatch@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449973}

--- chrome/browser/ash/usb/cros_usb_detector.cc
+++ chrome/browser/ash/usb/cros_usb_detector.cc
@@ -1118,6 +1118,10 @@ void CrosUsbDetector::OnUsbDeviceAttachFinished(
   }
 
   PrefService* prefs = profile()->GetPrefs();
+  if (success) {
+    LOG(WARNING) << "Successful connection of "
+                 << UsbDeviceIdentifier(device_info);
+  }
   if (success &&
       prefs->GetBoolean(
           guest_os::prefs::kGuestOsUSBPersistentPassthroughEnabled)) {
