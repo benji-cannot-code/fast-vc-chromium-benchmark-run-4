8f09f8060b1f973889e515f4aa11e0d7cc6a3e9d
James Cook <jamescook@chromium.org>
2025-04-22 16:44:59 -0700

    extensions: Tidy ChromeContentBrowserClientExtensionPart in BUILD.gn
    The files live in the chrome/browser/extensions directory, so they
    should live in chrome/browser/extensions/BUILD.gn. Right now they
    are listed in two places, which is confusing.
    
    Bug: none
    Test: compiles
    Change-Id: Id652e4a338acfa0b7c025856bd934a56cce50fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479471
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450310}

--- chrome/browser/task_manager/providers/child_process_task.cc
+++ chrome/browser/task_manager/providers/child_process_task.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/extensions/chrome_content_browser_client_extensions_part.h"
 #include "chrome/browser/process_resource_usage.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/task_manager/task_manager_observer.h"
@@ -35,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 #if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/extensions/chrome_content_browser_client_extensions_part.h"  // nogncheck
 #include "extensions/browser/extension_registry.h"  // nogncheck
 #include "extensions/common/extension_set.h"        // nogncheck
 #endif
