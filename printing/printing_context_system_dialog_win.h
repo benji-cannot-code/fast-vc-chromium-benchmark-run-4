4b8205f12f61d104c06475927851b2a5c7ab4c8f
Dominik Bylica <bylica@google.com>
2025-04-22 16:27:16 -0700

    [Printing] Improve code readability of out-of-process printing flow.
    Make the code more consistent with the naming of out-of-process data
    structures.
    
    Bug: 410749690
    Change-Id: I81c665e93d0aeeaf9648327d93d50efd3a655e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6456326
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Auto-Submit: Dominik Bylica <bylica@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450300}

--- printing/printing_context_system_dialog_win.h
+++ printing/printing_context_system_dialog_win.h
@@ -22,7 +22,7 @@ class COMPONENT_EXPORT(PRINTING) PrintingContextSystemDialogWin
     : public PrintingContextWin {
  public:
   PrintingContextSystemDialogWin(Delegate* delegate,
-                                 ProcessBehavior process_behavior);
+                                 OutOfProcessBehavior out_of_process_behavior);
   PrintingContextSystemDialogWin(const PrintingContextSystemDialogWin&) =
       delete;
   PrintingContextSystemDialogWin& operator=(
