11a413c8ff52f56099a5c0786ece62d0d9a2b3b5
Achuith Bhandarkar <achuith@chromium.org>
2025-04-21 17:22:50 -0700

    [extensions]: Replace ExtensionService::AddExtension
    Bug: 405411622
    Change-Id: I98e7d278f32ce5228fc3ebf505fc288e3d4f9e48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477110
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449677}

--- chrome/browser/themes/theme_local_data_batch_uploader_unittest.cc
+++ chrome/browser/themes/theme_local_data_batch_uploader_unittest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/test/fake_sync_change_processor.h"
 #include "components/sync/test/sync_change_processor_wrapper_for_test.h"
 #include "extensions/browser/extension_prefs.h"
+#include "extensions/browser/extension_registrar.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/permissions/permission_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -97,7 +98,7 @@ class ThemeLocalDataBatchUploaderTestBase
 
     extensions::ExtensionServiceTestBase::SetUp();
     InitializeExtensionService(ExtensionServiceInitParams());
-    service_->Init();
+    service()->Init();
 
     theme_service_ = ThemeServiceFactory::GetForProfile(profile());
     theme_sync_service_ = theme_service_->GetThemeSyncableService();
@@ -111,7 +112,7 @@ class ThemeLocalDataBatchUploaderTestBase
         extensions::mojom::ManifestLocation::kInternal, kCustomThemeUrl);
     extensions::ExtensionPrefs::Get(profile())->AddGrantedPermissions(
         theme_extension_->id(), extensions::PermissionSet());
-    service_->AddExtension(theme_extension_.get());
+    registrar()->AddExtension(theme_extension_.get());
     ASSERT_EQ(1u, extensions::ExtensionRegistry::Get(profile())
                       ->enabled_extensions()
                       .size());
