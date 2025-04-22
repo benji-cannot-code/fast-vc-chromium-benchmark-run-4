f9af20d03bf8cbd44fb3d6acd7ad4d1c29756fbf
Jan Keitel <jkeitel@google.com>
2025-04-22 08:13:48 -0700

    Use base::test::EqualsProto
    This is a pure code health clean-up CL for tests that roll their own
    custom proto matcher. It removes those custom matchers and use the
    equivalent one in base instead.
    
    This CL was uploaded by git cl split.
    
    R=miersh@google.com
    
    Change-Id: I4e2676587aac393849441e1cd9318cf7abd15648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469633
    Commit-Queue: Michael Ershov <miersh@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449947}

--- chrome/browser/ash/cert_provisioning/cert_provisioning_client_unittest.cc
+++ chrome/browser/ash/cert_provisioning/cert_provisioning_client_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/test/protobuf_matchers.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "base/types/expected.h"
@@ -18,22 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace em = enterprise_management;
 
-using testing::_;
-using testing::Invoke;
-using testing::SizeIs;
-
 namespace ash::cert_provisioning {
 
 namespace {
 
-MATCHER_P(EqualsProto,
-          message,
-          "Match a proto Message equal to the matcher's argument.") {
-  std::string expected_serialized, actual_serialized;
-  message.SerializeToString(&expected_serialized);
-  arg.SerializeToString(&actual_serialized);
-  return expected_serialized == actual_serialized;
-}
+using base::test::EqualsProto;
+using testing::_;
+using testing::Invoke;
+using testing::SizeIs;
 
 // A fake CloudPolicyClient that can record cert provisioning actions and
 // provides the test a way to supply a response by saving the callbacks passed
