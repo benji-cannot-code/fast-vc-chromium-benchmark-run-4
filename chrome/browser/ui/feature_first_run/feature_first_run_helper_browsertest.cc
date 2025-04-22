b2859f1b28b861a78077ba39f610cc069acae7cc
Zaina Al-Mashni <zalmashni@google.com>
2025-04-22 12:29:23 -0700

    [AutofillAi] Add button callbacks to FFR dialog
    This change does the following:
    - Include accept & cancel button callbacks as params for building the
    generic FFR dialog
    - Pass the callbacks from the Autofill AI dialog.
    - Opt-in to AutofillAI if the user accepts the dialog.
    
    Bug: 409520456
    Change-Id: I17baa9082b4af609c8100db0c14fb25fec9d1ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469532
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450127}

--- chrome/browser/ui/feature_first_run/feature_first_run_helper_browsertest.cc
+++ chrome/browser/ui/feature_first_run/feature_first_run_helper_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/test/mock_callback.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_factory.h"
@@ -31,8 +32,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/widget_test.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
+#include "ui/views/window/dialog_delegate.h"
 
 namespace feature_first_run {
+
+namespace {
+
+views::Widget* ShowDialog(content::WebContents* web_contents,
+                          base::OnceClosure accept_callback = base::DoNothing(),
+                          base::OnceClosure cancel_callback = base::DoNothing(),
+                          std::u16string title = std::u16string(),
+                          ui::ImageModel banner = ui::ImageModel(),
+                          ui::ImageModel dark_mode_banner = ui::ImageModel(),
+                          std::unique_ptr<views::View> content_view =
+                              std::make_unique<views::View>()) {
+  return ShowFeatureFirstRunDialog(
+      std::move(title), std::move(banner), std::move(dark_mode_banner),
+      std::move(content_view), std::move(accept_callback),
+      std::move(cancel_callback), web_contents);
+}
+
+}  // namespace
 
 using FeatureFirstRunDialogHelperBrowserTest = InProcessBrowserTest;
 
@@ -46,8 +66,9 @@ IN_PROC_BROWSER_TEST_F(FeatureFirstRunDialogHelperBrowserTest,
       ui::ImageModel::FromResourceId(IDR_SAVE_PASSPORT_DARK);
 
   auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
-  auto* dialog_widget = ShowFeatureFirstRunDialog(
-      title, banner, dark_mode_banner, std::move(content_view), web_contents);
+  auto* dialog_widget =
+      ShowDialog(web_contents, base::DoNothing(), base::DoNothing(), title,
+                 banner, dark_mode_banner, std::move(content_view));
   auto* dialog_widget_delegate = dialog_widget->widget_delegate();
 
   EXPECT_TRUE(dialog_widget->IsVisible());
@@ -75,6 +96,29 @@ IN_PROC_BROWSER_TEST_F(FeatureFirstRunDialogHelperBrowserTest,
           kFeatureFirstRunDialogContentViewElementId,
           browser()->window()->GetElementContext());
   EXPECT_EQ(expected_content_view, actual_content_view);
+}
+
+IN_PROC_BROWSER_TEST_F(FeatureFirstRunDialogHelperBrowserTest,
+                       OnDialogAccepted) {
+  base::MockOnceClosure accept_callback;
+
+  auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
+  auto* dialog_widget = ShowDialog(web_contents, accept_callback.Get());
+
+  EXPECT_CALL(accept_callback, Run);
+  dialog_widget->widget_delegate()->AsDialogDelegate()->Accept();
+}
+
+IN_PROC_BROWSER_TEST_F(FeatureFirstRunDialogHelperBrowserTest,
+                       OnDialogCancelled) {
+  base::MockOnceClosure cancel_callback;
+
+  auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
+  auto* dialog_widget =
+      ShowDialog(web_contents, base::DoNothing(), cancel_callback.Get());
+
+  EXPECT_CALL(cancel_callback, Run);
+  dialog_widget->widget_delegate()->AsDialogDelegate()->Cancel();
 }
 
 IN_PROC_BROWSER_TEST_F(FeatureFirstRunDialogHelperBrowserTest,
