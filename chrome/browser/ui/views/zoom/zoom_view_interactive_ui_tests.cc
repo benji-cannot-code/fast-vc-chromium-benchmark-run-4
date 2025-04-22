7740f0db463257749007a2ea82bbf92b8b7a1faa
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-04-22 11:09:51 -0700

    [Zoom Page Action] Fix Bubble Visibility and Action Item Update
    This CL addresses an issue where the PageActionView button controller
    suppressed the click event when a bubble was already visible. This led
    to a scenario where, if the associated feature did not correctly update
    the action item, the PageAction would end up in a state where clicking
    the action item would have no effect.
    
    This CL ensures that the Zoom action item is properly updated every
    time, regardless of the bubble’s state, by fixing the condition that was
    preventing the update.
    
    Additionally, the approach to determining the bubble’s visibility has
    been improved. Previously, the visibility check relied on the action
    item, but this was problematic because the bubble could already be
    closed when `UpdateBubbleVisibility` was called. As a result, the action
    item’s state was not always an accurate source of truth regarding the
    bubble’s visibility.
    
    Bug: 376284060
    Change-Id: Iee648305b56309def6910e29e5608e3582ecd601
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6460644
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450074}

--- chrome/browser/ui/views/zoom/zoom_view_interactive_ui_tests.cc
+++ chrome/browser/ui/views/zoom/zoom_view_interactive_ui_tests.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/interaction/element_identifier.h"
+#include "ui/base/interaction/state_observer.h"
 #include "ui/base/models/image_model.h"
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
@@ -78,6 +79,20 @@ class ZoomViewInteractiveUiTest : public InteractiveBrowserTest {
                        testing::Eq(nullptr));
   }
 
+  MultiStep WaitForZoomBubble(bool visible) {
+    DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ui::test::PollingStateObserver<bool>,
+                                        kZoomBubbleVisible);
+
+    return Steps(PollState(kZoomBubbleVisible,
+                           [&, visible]() {
+                             bool is_visible =
+                                 ZoomBubbleView::GetZoomBubble() != nullptr;
+                             return is_visible == visible;
+                           }),
+                 WaitForState(kZoomBubbleVisible, true),
+                 StopObservingState(kZoomBubbleVisible));
+  }
+
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
@@ -116,6 +131,30 @@ IN_PROC_BROWSER_TEST_F(ZoomViewInteractiveUiTest, ZoomStateUpdates) {
                              .value() != zoom_in_image;
                 }),
       EnsureZoomBubblePresent());
+}
+
+IN_PROC_BROWSER_TEST_F(ZoomViewInteractiveUiTest,
+                       ShowAndHideZoomBubbleByClickWithMouse) {
+  RunTestSequence(WaitForZoomBubble(/*visible=*/false), DoZoomIn(),
+                  EnsurePresent(kActionItemZoomElementId),
+                  MoveMouseTo(kActionItemZoomElementId), ClickMouse(),
+                  WaitForZoomBubble(/*visible=*/true),
+                  MoveMouseTo(kActionItemZoomElementId), ClickMouse(),
+                  WaitForZoomBubble(/*visible=*/false),
+                  MoveMouseTo(kActionItemZoomElementId), ClickMouse(),
+                  WaitForZoomBubble(/*visible=*/true));
+}
+
+IN_PROC_BROWSER_TEST_F(ZoomViewInteractiveUiTest,
+                       ShowAndHideZoomBubbleByClickWithKeyboardPress) {
+  RunTestSequence(WaitForZoomBubble(/*visible=*/false), DoZoomIn(),
+                  EnsurePresent(kActionItemZoomElementId),
+                  PressButton(kActionItemZoomElementId),
+                  WaitForZoomBubble(/*visible=*/true),
+                  PressButton(kActionItemZoomElementId),
+                  WaitForZoomBubble(/*visible=*/false),
+                  PressButton(kActionItemZoomElementId),
+                  WaitForZoomBubble(/*visible=*/true));
 }
 
 }  // namespace
