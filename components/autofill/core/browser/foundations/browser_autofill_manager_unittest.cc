c343772d8bd761156cae812654273e478129e426
Jan Keitel <jkeitel@google.com>
2025-04-22 04:00:10 -0700

    Remove UserAnnotations references from BAM test
    This is a prerequisite for removing the UserAnnotations proto in
    google3.
    
    Bug: 395038288
    Change-Id: I9e34b79206b1a6e5c2393ac90f96aa217aa1dfa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6475924
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449841}

--- components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
@@ -6995,8 +6995,7 @@ TEST_F(BrowserAutofillManagerTest_AutofillAi,
 // Tests that an Autofill profile is not imported into the address data manager
 // when the submitted form was imported by AutofillAI.
 TEST_F(BrowserAutofillManagerTest_AutofillAi,
-       ProfileNotImportedOnSuccessfulUserAnnotationsImport) {
-  using optimization_guide::proto::UserAnnotationsEntry;
+       ProfileNotImportedOnSuccessfulAutofillAiImport) {
   TestAddressDataManager& adm = personal_data().test_address_data_manager();
   FormData form = CreateTestAddressFormData();
   FormsSeen({form});
@@ -7022,7 +7021,7 @@ TEST_F(BrowserAutofillManagerTest_AutofillAi,
 // Tests that an Autofill profile is imported into the address data manager when
 // the submitted form was not imported by AutofillAI.
 TEST_F(BrowserAutofillManagerTest_AutofillAi,
-       ProfileImportedOnFailedUserAnnotationsImport) {
+       ProfileImportedOnFailedAutofillAiImport) {
   TestAddressDataManager& adm = personal_data().test_address_data_manager();
   FormData form = CreateTestAddressFormData();
   FormsSeen({form});
