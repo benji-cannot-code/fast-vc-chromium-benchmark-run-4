1bb18be9ec0ab6c07800ac4ac1b3c170d0964ab5
Sam Goto <goto@chromium.org>
2025-04-22 09:57:50 -0700

    [fedcm] Add unit tests verified profiles in BrowserAutofillManager
    Add unittests for verified profiles
    
    Bug: 380367784
    Change-Id: I047161b804499bbeca779d838dd5754902afd5e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6474356
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450012}

--- components/autofill/core/browser/foundations/test_autofill_client.h
+++ components/autofill/core/browser/foundations/test_autofill_client.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/foundations/autofill_driver_factory.h"
 #include "components/autofill/core/browser/integrators/autofill_ai/mock_autofill_ai_delegate.h"
 #include "components/autofill/core/browser/integrators/fast_checkout/mock_fast_checkout_client.h"
+#include "components/autofill/core/browser/integrators/identity_credential_delegate.h"
 #include "components/autofill/core/browser/integrators/optimization_guide/mock_autofill_optimization_guide.h"
 #include "components/autofill/core/browser/integrators/plus_addresses/autofill_plus_address_delegate.h"
 #include "components/autofill/core/browser/logging/log_manager.h"
@@ -187,6 +188,10 @@ class TestAutofillClientTemplate : public T {
 
   AutofillPlusAddressDelegate* GetPlusAddressDelegate() override {
     return plus_address_delegate_.get();
+  }
+
+  IdentityCredentialDelegate* GetIdentityCredentialDelegate() override {
+    return identity_credential_delegate_.get();
   }
 
   test::AutofillTestingPrefService* GetPrefs() override {
@@ -582,6 +587,12 @@ class TestAutofillClientTemplate : public T {
     plus_address_delegate_ = std::move(plus_address_delegate);
   }
 
+  void set_identity_credential_delegate(
+      std::unique_ptr<IdentityCredentialDelegate>
+          identity_credential_delegate) {
+    identity_credential_delegate_ = std::move(identity_credential_delegate);
+  }
+
   void set_suggestion_ui_session_id(
       std::optional<AutofillClient::SuggestionUiSessionId> session_id) {
     suggestion_ui_session_id_ = session_id;
@@ -601,6 +612,7 @@ class TestAutofillClientTemplate : public T {
   signin::IdentityTestEnvironment identity_test_env_;
   raw_ptr<syncer::SyncService> test_sync_service_ = nullptr;
   std::unique_ptr<AutofillPlusAddressDelegate> plus_address_delegate_;
+  std::unique_ptr<IdentityCredentialDelegate> identity_credential_delegate_;
   TestAddressNormalizer test_address_normalizer_;
   std::unique_ptr<::testing::NiceMock<MockAutofillOptimizationGuide>>
       mock_autofill_optimization_guide_ =
