153e6029c43c3807f6e84cb37cfdd041ef8ad17e
Ryan Sultanem <rsult@google.com>
2025-04-22 08:07:49 -0700

    [B4B] Enable fieldtrial testing
    Fixed: 412567144
    Change-Id: I021bccc0cdd9b40bcf10dac30446857e0aefe295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477028
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449938}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -208,6 +208,7 @@ class ProfileKeyedServiceBrowserTest : public InProcessBrowserTest {
           omnibox::kOnDeviceTailModel,
           omnibox::kOnDeviceHeadProviderNonIncognito,
 #endif  // BUILDFLAG(BUILD_WITH_TFLITE_LIB)
+          switches::kSyncEnableBookmarksInTransportMode,
         },
         {});
     // clang-format on
@@ -539,6 +540,7 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
                        GuestProfileParent_NeededServices) {
   // clang-format off
   std::set<std::string> guest_active_services {
+    "AccountBookmarkSyncServiceFactory",
     "AccountExtensionTracker",
     "ActivityLog",
     "ActivityLogPrivateAPI",
@@ -680,7 +682,6 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
     "InstallTracker",
     "InstallVerifier",
     "InstanceIDProfileService",
-
 #if !BUILDFLAG(IS_CHROMEOS)
     // TODO(crbug.com/374351946): Investigate if this is necessary on CrOS.
     "InvalidationService",
@@ -691,7 +692,6 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
     "ListFamilyMembersService",
     "LocalOrSyncableBookmarkSyncServiceFactory",
     "LoginUIServiceFactory",
-
     "MDnsAPI",
     "ManagedBookmarkService",
     "ManagedConfigurationAPI",
@@ -790,14 +790,12 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
     "SiteDataCacheFacadeFactory",
     "SiteEngagementService",
     "SocketManager",
-
 #if !BUILDFLAG(IS_CHROMEOS)
     // TODO(crbug.com/374351946): Desktop chrome create this via
     // ShoppingService->SyncService->Spellchecker. Investigate if this is
     // expected on desktop chrome.
     "SpellcheckService",
 #endif
-
     "StorageFrontend",
     "StorageNotificationService",
     "SupervisedUserService",
