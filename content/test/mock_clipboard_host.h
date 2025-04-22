374b6aa62bece38d64a6fc51a70d5c6b31742eb3
Rohan Raja <roraja@microsoft.com>
2025-04-22 14:41:09 -0700

    [ClipboardChange event] - MOJOM methods to monitor clipboard changes
    Diagram: https://imgur.com/a/Vwd209d
    
    Adds new MOJOM methods for renderer to request listening to clipboard
    changes. Renderer can also request browser to stop listening to the
    clipboard changes. (when it no more needs the event from browser). Added
    unit tests for ClipboardHostImpl changes to ensure it receives the
    clipboardchange event from ui::ClipboardMonitor class
    
    Design doc:
    https://docs.google.com/document/d/1bY2pzV6PSX56fiFcrXEgOjpFen07xaxmnsM5dqXFE1U/edit#heading=h.lem8rnjx4cw8
    
    Full implementation reference CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6328739
    
    Bug: 41442253
    Change-Id: I2f6e83782164c7c5b3ec011652caf8755a367ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6342493
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Rohan Raja <roraja@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1450230}

--- content/test/mock_clipboard_host.h
+++ content/test/mock_clipboard_host.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/clipboard/clipboard.mojom.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/clipboard/clipboard.h"
@@ -70,13 +71,17 @@ class MockClipboardHost : public blink::mojom::ClipboardHost {
       ReadUnsanitizedCustomFormatCallback callback) override;
   void WriteUnsanitizedCustomFormat(const std::u16string& format,
                                     mojo_base::BigBuffer data) override;
+  void RegisterClipboardListener(
+      mojo::PendingRemote<blink::mojom::ClipboardListener> listener) override;
 #if BUILDFLAG(IS_MAC)
   void WriteStringToFindPboard(const std::u16string& text) override;
 #endif
  private:
   std::vector<std::u16string> ReadStandardFormatNames();
+  void OnClipboardDataChanged();
 
   mojo::ReceiverSet<blink::mojom::ClipboardHost> receivers_;
+  mojo::Remote<blink::mojom::ClipboardListener> clipboard_listener_;
   ui::ClipboardSequenceNumberToken sequence_number_;
   std::u16string plain_text_;
   std::u16string html_text_;
