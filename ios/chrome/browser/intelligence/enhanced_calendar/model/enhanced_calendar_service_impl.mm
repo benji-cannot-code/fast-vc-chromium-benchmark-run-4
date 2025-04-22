213a0adbabdf9b16f82315d5488aff9cc4d3124f
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-04-22 13:45:41 -0700

    Revert "[iOS] Add enhanced calendar response status metrics"
    This reverts commit 25a556af7960cbe8110561a2d938e6a393f1c21d.
    
    Reason for revert: Fails check_static_initializers
    (https://ci.chromium.org/ui/p/chromium/builders/ci/ios-device/420809/overview)
    
    Original change's description:
    > [iOS] Add enhanced calendar response status metrics
    >
    > This CL adds enhanced calendar response status histogram.
    >
    > Fixed: 411171228
    > Change-Id: I79a1bc1c4ef86d1c900cc51cb32a609a8df0ad32
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6468324
    > Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    > Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1450163}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6b7dd1040da297b7ecb8b725ea48c87e2406ef2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6480051
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Owners-Override: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450179}

--- ios/chrome/browser/intelligence/enhanced_calendar/model/enhanced_calendar_service_impl.mm
+++ ios/chrome/browser/intelligence/enhanced_calendar/model/enhanced_calendar_service_impl.mm
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/enhanced_calendar/model/enhanced_calendar_service_impl.h"
 
 #import <optional>
-#import <string>
 #import <utility>
 
 #import "base/functional/bind.h"
-#import "base/metrics/histogram_functions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/time/time.h"
 #import "components/optimization_guide/core/model_execution/feature_keys.h"
@@ -18,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/optimization_guide/core/optimization_guide_model_executor.h"
 #import "components/optimization_guide/core/optimization_guide_util.h"
 #import "components/optimization_guide/proto/features/enhanced_calendar.pb.h"
-#import "ios/chrome/browser/intelligence/enhanced_calendar/metrics/enhanced_calendar_metrics.h"
 #import "ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory.h"
@@ -28,10 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "mojo/public/cpp/base/proto_wrapper.h"
 
 namespace ai {
-
-// Error message when the service is shutting down.
-const std::string kServiceShutDownErrorString =
-    "Enhanced Calendar service is shutting down.";
 
 // TODO(crbug.com/398903376): Evaluate a good timeout when using prod-served
 // models.
@@ -65,7 +58,7 @@ EnhancedCalendarServiceImpl::~EnhancedCalendarServiceImpl() {
 
   mojom::EnhancedCalendarResponseResultPtr result_union =
       mojom::EnhancedCalendarResponseResult::NewError(
-          kServiceShutDownErrorString);
+          "Enhanced Calendar service is shutting down.");
 
   InvokePendingCallback(std::move(result_union));
 }
@@ -141,7 +134,6 @@ void EnhancedCalendarServiceImpl::OnEnhancedCalendarResponse(
   if (!pending_request_callback_) {
     return;
   }
-
   mojom::EnhancedCalendarResponseResultPtr result_union;
 
   if (result.response.has_value()) {
@@ -172,31 +164,12 @@ void EnhancedCalendarServiceImpl::OnEnhancedCalendarResponse(
 void EnhancedCalendarServiceImpl::InvokePendingCallback(
     mojom::EnhancedCalendarResponseResultPtr result_union) {
   CHECK(pending_request_callback_);
-  RecordMetrics(result_union->is_error()
-                    ? std::make_optional(result_union->get_error())
-                    : std::nullopt);
+
+  if (result_union->is_error()) {
+    // TODO(crbug.com/411171228) : Add metrics for success rate.
+  }
 
   std::move(pending_request_callback_).Run(std::move(result_union));
-}
-
-void EnhancedCalendarServiceImpl::RecordMetrics(
-    std::optional<std::string> error_message) {
-  if (!error_message.has_value()) {
-    base::UmaHistogramEnumeration(kEnhancedCalendarResponseStatusHistogram,
-                                  EnhancedCalendarResponseStatus::kSuccess);
-    return;
-  }
-
-  if (error_message.value() == kServiceShutDownErrorString) {
-    base::UmaHistogramEnumeration(
-        kEnhancedCalendarResponseStatusHistogram,
-        EnhancedCalendarResponseStatus::kCancelRequest);
-    return;
-  }
-
-  base::UmaHistogramEnumeration(
-      kEnhancedCalendarResponseStatusHistogram,
-      EnhancedCalendarResponseStatus::kGenericFailure);
 }
 
 #pragma mark - IdentityManagerObserverBridgeDelegate
