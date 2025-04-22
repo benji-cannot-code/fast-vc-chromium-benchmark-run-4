73ada405eca1a1037943d521b9486ca7a17b2fe1
Fiaz Muhammad <mfiaz@google.com>
2025-04-22 07:29:22 -0700

    [Tab Group Parity] Use standard animation for context menu
    - Delete custom animations
    - Use default animations
    
    Bug: 411434664
    Change-Id: I542db06696fc8189e25a1d44dfa3e4f90b4a9d4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469333
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449911}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherContextMenuCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherContextMenuCoordinator.java
@@ -11,6 +11,7 @@ import static org.chromium.chrome.browser.share.ShareDelegate.ShareOrigin.TAB_ST
 import android.app.Activity;
 
 import androidx.annotation.VisibleForTesting;
+import androidx.core.content.res.ResourcesCompat;
 
 import org.chromium.base.Token;
 import org.chromium.base.metrics.RecordUserAction;
@@ -132,7 +133,7 @@ public class TabSwitcherContextMenuCoordinator extends TabOverflowMenuCoordinato
                 tabId,
                 /* horizontalOverlapAnchor= */ true,
                 /* verticalOverlapAnchor= */ false,
-                /* animStyle= */ R.style.TabSwitcherContextMenuAnimation,
+                /* animStyle= */ ResourcesCompat.ID_NULL,
                 HorizontalOrientation.LAYOUT_DIRECTION,
                 mActivity,
                 /* isIncognito= */ false);
