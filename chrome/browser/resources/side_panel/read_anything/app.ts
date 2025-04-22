b6a3992339278656f34580b582c28fb31f1acd90
Kristi Saney <kristislee@google.com>
2025-04-22 13:56:08 -0700

    [Read aloud] Use engine-provided word length if it exists
    Bug: 351873366
    Change-Id: Ic0a0ef722056a42b9b3fbb21f05126aaf3a0fd2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470523
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450191}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -141,6 +141,9 @@ export interface WordBoundaryState {
   // just the correct index within the current string.
   // Default is 0.
   speechUtteranceStartIndex: number;
+  // The length of the current word if it was provided by the speech engine. If
+  // not, this is 0.
+  speechUtteranceLength: number;
   // If we have to break a string because the text is too long, we need to
   // offset future word boundaries within this utterance by this offset so
   // that they appear in the correct locations.
@@ -337,6 +340,7 @@ export class AppElement extends AppElementBase {
     mode: WordBoundaryMode.BOUNDARIES_NOT_SUPPORTED,
     speechUtteranceStartIndex: 0,
     previouslySpokenIndex: 0,
+    speechUtteranceLength: 0,
     tooLongTextOffset: 0,
   };
 
@@ -2007,7 +2011,7 @@ export class AppElement extends AppElementBase {
       // the sentence granularity level, so we'll retrieve these boundaries in
       // message.onEnd instead.
       if (event.name === 'word') {
-        this.updateBoundary(event.charIndex);
+        this.updateBoundary(event.charIndex, event.charLength);
 
         // No need to update the highlight on word boundary events if
         // highlighting is off or if sentence highlighting is used.
@@ -2188,10 +2192,11 @@ export class AppElement extends AppElementBase {
     this.stopSpeech(PauseActionSource.DEFAULT);
   }
 
-  updateBoundary(charIndex: number) {
+  updateBoundary(charIndex: number, charLength: number = 0) {
     this.wordBoundaryState.previouslySpokenIndex =
         charIndex + this.wordBoundaryState.tooLongTextOffset;
     this.wordBoundaryState.mode = WordBoundaryMode.BOUNDARY_DETECTED;
+    this.wordBoundaryState.speechUtteranceLength = charLength;
   }
 
   resetToDefaultWordBoundaryState(
@@ -2213,6 +2218,7 @@ export class AppElement extends AppElementBase {
           WordBoundaryMode.NO_BOUNDARIES :
           WordBoundaryMode.BOUNDARIES_NOT_SUPPORTED,
       speechUtteranceStartIndex: 0,
+      speechUtteranceLength: 0,
       tooLongTextOffset: 0,
     };
   }
@@ -2268,13 +2274,15 @@ export class AppElement extends AppElementBase {
     this.resetPreviousHighlight_();
     const index = this.wordBoundaryState.speechUtteranceStartIndex +
         this.wordBoundaryState.previouslySpokenIndex;
+    const length = this.wordBoundaryState.speechUtteranceLength;
+
     const highlightNodes =
         chrome.readingMode.getHighlightForCurrentSegmentIndex(
             index, highlightPhrases);
     let anyHighlighted: boolean = false;
     for (const highlightNode of highlightNodes) {
       const nodeId = highlightNode.nodeId;
-      const highlightLength: number = highlightNode.length;
+      const highlightLength: number = length ? length : highlightNode.length;
       const highlightStartIndex = highlightNode.start;
       const endIndex = highlightStartIndex + highlightLength;
       const element = this.domNodeToAxNodeIdMap_.keyFrom(nodeId);
