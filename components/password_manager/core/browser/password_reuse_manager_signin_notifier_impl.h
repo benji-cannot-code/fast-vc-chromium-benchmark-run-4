1845ea7eebdc4e1699ee23fb21de97d71f4c00c1
Jérôme Lebel <jlebel@chromium.org>
2025-04-22 04:19:15 -0700

    Add PasswordReuseManagerSigninNotifierImpl::OnIdentityManagerShutdown
    This patch tries to make sure all IdentityManager observers are
    removed before IdentityManager shuts down.
    
    Add:
    void PasswordReuseManagerSigninNotifierImpl::OnIdentityManagerShutdown(
        signin::IdentityManager* identity_manager) {
      identity_manager_observation_.Reset();
      identity_manager_ = nullptr;
    }
    
    Bug: 410122787
    Change-Id: Iec91140ab048b469b10072bbe2ecd097e162066a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469998
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449849}

--- components/password_manager/core/browser/password_reuse_manager_signin_notifier_impl.h
+++ components/password_manager/core/browser/password_reuse_manager_signin_notifier_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "components/password_manager/core/browser/password_reuse_manager_signin_notifier.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -37,12 +38,17 @@ class PasswordReuseManagerSigninNotifierImpl
   void OnPrimaryAccountChanged(
       const signin::PrimaryAccountChangeEvent& event) override;
   void OnExtendedAccountInfoRemoved(const AccountInfo& info) override;
+  void OnIdentityManagerShutdown(
+      signin::IdentityManager* identity_manager) override;
 
  private:
   // Passes the sign-out notification to `reuse_manager_`.
   void NotifySignedOut(const std::string& username, bool syncing_account);
 
   raw_ptr<signin::IdentityManager> identity_manager_ = nullptr;
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observation_{this};
 
   raw_ptr<PasswordReuseManager> reuse_manager_ = nullptr;  // weak
 };
