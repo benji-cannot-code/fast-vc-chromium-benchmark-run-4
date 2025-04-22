57d2a65ed6e8499a2a2eb81dcb1a6bc662707224
Alison Gale <agale@chromium.org>
2025-04-22 12:43:50 -0700

    [SxS] Allocate extra space to split tabs
    When allocating the extra pixels we don't want an early split tab to end
    up 2 pixels wider than a later tab (particularly with tab closing mode)
    so only the first tab in a split will get an extra pixel.
    
    Bug: 411471023
    Change-Id: Ibe197a51c71b69064a5ee11a74eb0589e4fa0a0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476490
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450140}

--- chrome/browser/ui/views/tabs/tab_strip_layout_helper.cc
+++ chrome/browser/ui/views/tabs/tab_strip_layout_helper.cc
@@ -33,7 +33,8 @@ struct TabStripLayoutHelper::TabSlot {
     TabStripLayoutHelper::TabSlot slot;
     slot.type = view->GetTabSlotViewType();
     slot.view = view;
-    slot.state = TabLayoutState(TabOpen::kOpen, pinned, TabActive::kInactive);
+    slot.state = TabLayoutState(TabOpen::kOpen, pinned, TabActive::kInactive,
+                                view->split());
     return slot;
   }
 
@@ -266,7 +267,8 @@ std::vector<gfx::Rect> TabStripLayoutHelper::CalculateIdealBounds(
     auto open = (slots_[i].state.IsClosed() || SlotIsCollapsedTab(i))
                     ? TabOpen::kClosed
                     : TabOpen::kOpen;
-    TabLayoutState state = TabLayoutState(open, pinned, active);
+    TabLayoutState state =
+        TabLayoutState(open, pinned, active, slots_[i].view->split());
     TabSizeInfo size_info = slots_[i].view->GetTabSizeInfo();
 
     tab_widths.emplace_back(state, size_info);
