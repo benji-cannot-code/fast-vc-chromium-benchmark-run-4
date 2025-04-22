4f21e013fd90645ca341d901b3a8664a90dd0686
Jérôme Lebel <jlebel@chromium.org>
2025-04-22 02:33:23 -0700

    Make sure PasswordDataTypeController removes observer
    This patch tries to make sure all IdentityManager observers are
    removed before IdentityManager shuts down.
    
    Add:
    void PasswordDataTypeController::OnIdentityManagerShutdown(
        signin::IdentityManager* identity_manager) {
      identity_manager_observation_.Reset();
    }
    
    Bug: 410122787
    Change-Id: Ia878e899e6a0026feb04214f07acd960882d72f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469694
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449816}

--- components/password_manager/core/browser/sync/password_data_type_controller.cc
+++ components/password_manager/core/browser/sync/password_data_type_controller.cc
@@ -105,4 +105,9 @@ void PasswordDataTypeController::OnPrimaryAccountChanged(
 #endif
 }
 
+void PasswordDataTypeController::OnIdentityManagerShutdown(
+    signin::IdentityManager* identity_manager) {
+  identity_manager_observation_.Reset();
+}
+
 }  // namespace password_manager
