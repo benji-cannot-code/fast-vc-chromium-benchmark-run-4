d62322ad34cb3c8d16f4cdbc574e938398b14378
Ian Kilpatrick <ikilpatrick@chromium.org>
2025-04-21 19:16:41 -0700

    [memory] Shrink ShapeResultView to 64 bytes on 64bit.
    Improves wikipedia:2018 gc effective size by 1.6%.
    Might have marginal benefit on SP3.
    
    Bug: 412125816, 406127861
    Change-Id: Iacbdade99e4ae2981bf00014d48e19c9cec9c7e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476844
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449718}

--- third_party/blink/renderer/platform/fonts/shaping/glyph_data_range.cc
+++ third_party/blink/renderer/platform/fonts/shaping/glyph_data_range.cc
@@ -15,14 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 GlyphDataRange::GlyphDataRange(const ShapeResultRun& run)
-    : run_(&run),
-      size_(run.glyph_data_.size()),
-      has_offsets_(run.glyph_data_.HasNonZeroOffsets()) {}
+    : run_(&run), size_(run.glyph_data_.size()) {}
 
 GlyphDataRange::GlyphDataRange(const GlyphDataRange& range,
                                const_iterator begin_glyph,
                                const_iterator end_glyph)
-    : run_(range.run_), has_offsets_(range.HasOffsets()) {
+    : run_(range.run_) {
   DCHECK(run_);
   CHECK_GE(begin_glyph, run_->glyph_data_.begin());
   index_ = begin_glyph - run_->glyph_data_.begin();
@@ -46,8 +44,12 @@ GlyphDataRange::const_iterator GlyphDataRange::end() const {
   return run_ ? run_->glyph_data_.begin() + index_ + size_ : nullptr;
 }
 
+bool GlyphDataRange::HasOffsets() const {
+  return run_ && run_->glyph_data_.HasNonZeroOffsets();
+}
+
 base::span<const GlyphOffset> GlyphDataRange::Offsets() const {
-  if (HasOffsets() && run_) [[unlikely]] {
+  if (HasOffsets()) [[unlikely]] {
     return run_->glyph_data_.Offsets().subspan(index_, size_);
   }
   return base::span<const GlyphOffset>{};
--- third_party/blink/renderer/platform/fonts/shaping/shape_result_view.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_view.cc
@@ -177,7 +177,6 @@ struct ShapeResultView::InitData {
 
 ShapeResultView::ShapeResultView(const InitData& data)
     : start_index_(data.start_index),
-      num_glyphs_(0),
       direction_(static_cast<unsigned>(data.direction)),
       has_vertical_offsets_(data.has_vertical_offsets),
       char_index_offset_(data.char_index_offset) {}
@@ -186,6 +185,7 @@ ShapeResult* ShapeResultView::CreateShapeResult() const {
   ShapeResult* new_result = MakeGarbageCollected<ShapeResult>(
       start_index_ + char_index_offset_, num_characters_, Direction());
   new_result->runs_.ReserveInitialCapacity(parts_.size());
+  unsigned num_glyphs = 0u;
   for (const auto& part : RunsOrParts()) {
     auto* new_run = MakeGarbageCollected<ShapeResultRun>(
         part.run_->font_data_.Get(), part.run_->HbDirection(),
@@ -203,9 +203,10 @@ ShapeResult* ShapeResultView::CreateShapeResult() const {
     new_run->num_characters_ = part.num_characters_;
     new_run->CheckConsistency();
     new_result->runs_.push_back(new_run);
+    num_glyphs += part.NumGlyphs();
   }
 
-  new_result->num_glyphs_ = num_glyphs_;
+  new_result->num_glyphs_ = num_glyphs;
   new_result->has_vertical_offsets_ = has_vertical_offsets_;
   new_result->width_ = width_;
 
@@ -263,14 +264,13 @@ void ShapeResultView::PopulateRunInfoParts(const ShapeResultType& other,
           });
     }
 
+    width_ += part_width;
+
     // Adjust start_index for runs to be continuous.
     const unsigned part_start_index = run_start + range_start + index_diff;
     const unsigned part_offset = range_start;
     parts_.emplace_back(run->GetRunInfo(), range, part_start_index, part_offset,
                         part_characters, part_width);
-
-    num_glyphs_ += range.size();
-    width_ += part_width;
   }
 }
 
@@ -293,7 +293,6 @@ ShapeResultView* ShapeResultView::Create(base::span<const Segment> segments) {
 
   ShapeResultView* out = MakeGarbageCollected<ShapeResultView>(data);
   DCHECK_EQ(out->num_characters_, 0u);
-  DCHECK_EQ(out->num_glyphs_, 0u);
   DCHECK_EQ(out->width_, 0);
   out->parts_.ReserveInitialCapacity(data.num_parts);
 
@@ -332,7 +331,6 @@ ShapeResultView* ShapeResultView::Create(const ShapeResult* result) {
 
   ShapeResultView* out = MakeGarbageCollected<ShapeResultView>(data);
   DCHECK_EQ(out->num_characters_, 0u);
-  DCHECK_EQ(out->num_glyphs_, 0u);
   DCHECK_EQ(out->width_, 0);
   out->parts_.ReserveInitialCapacity(data.num_parts);
 
@@ -372,6 +370,14 @@ void ShapeResultView::GetRunFontData(
         {part.run_->font_data_.Get(),
          static_cast<wtf_size_t>(part.end() - part.begin())}));
   }
+}
+
+unsigned ShapeResultView::NumGlyphs() const {
+  unsigned num_glyphs = 0u;
+  for (const auto& part : RunsOrParts()) {
+    num_glyphs += part.NumGlyphs();
+  }
+  return num_glyphs;
 }
 
 HeapHashSet<Member<const SimpleFontData>> ShapeResultView::UsedFonts() const {
