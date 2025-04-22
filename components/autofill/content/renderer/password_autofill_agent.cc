c659a183e405ccdd7dd62419cbfb539965c71f4b
Jan Keitel <jkeitel@google.com>
2025-04-22 01:04:41 -0700

    Use base::zip in autofill code
    Bug: 40100455
    Change-Id: Ic4c8c0cd7841c21ea983e9739fe0c1a3bbe7a0f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6472497
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449781}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/to_string.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/types/zip.h"
 #include "build/build_config.h"
 #include "components/autofill/content/common/mojom/autofill_driver.mojom.h"
 #include "components/autofill/content/renderer/form_autofill_util.h"
@@ -2252,10 +2253,8 @@ PasswordAutofillAgent::ExtractFormStructureInfo(const FormData& form_data) {
   result.renderer_id = form_data.renderer_id();
   result.fields.resize(form_data.fields().size());
 
-  for (size_t i = 0; i < form_data.fields().size(); ++i) {
-    const FormFieldData& form_field = form_data.fields()[i];
-
-    FormFieldInfo& field_info = result.fields[i];
+  for (auto [form_field, field_info] :
+       base::zip(form_data.fields(), result.fields)) {
     field_info.renderer_id = form_field.renderer_id();
     field_info.form_control_type = form_field.form_control_type();
     field_info.autocomplete_attribute = form_field.autocomplete_attribute();
@@ -2280,10 +2279,8 @@ bool PasswordAutofillAgent::WasFormStructureChanged(
   if (form_info.fields.size() != cached_form_info.fields.size())
     return true;
 
-  for (size_t i = 0; i < form_info.fields.size(); ++i) {
-    const FormFieldInfo& form_field = form_info.fields[i];
-    const FormFieldInfo& cached_form_field = cached_form_info.fields[i];
-
+  for (auto [form_field, cached_form_field] :
+       base::zip(form_info.fields, cached_form_info.fields)) {
     if (form_field.renderer_id != cached_form_field.renderer_id) {
       return true;
     }
--- components/autofill/core/common/form_field_data.cc
+++ components/autofill/core/common/form_field_data.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/types/cxx23_to_underlying.h"
+#include "base/types/zip.h"
 #include "build/build_config.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_util.h"
@@ -174,9 +175,10 @@ bool DeserializeSection3(base::PickleIterator* iter,
   }
   field_data->set_text_direction(text_direction);
   std::vector<SelectOption> options;
-  for (size_t i = 0; i < option_values.size(); ++i) {
-    options.push_back({.value = std::move(option_values[i]),
-                       .text = std::move(option_texts[i])});
+  for (auto [option_value, option_text] :
+       base::zip(option_values, option_texts)) {
+    options.push_back(
+        {.value = std::move(option_value), .text = std::move(option_text)});
   }
   field_data->set_options(std::move(options));
   return true;
