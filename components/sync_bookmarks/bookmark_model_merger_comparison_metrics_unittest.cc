cea4149de66a3357918a3c3dd96cc105e3840115
Mikel Astiz <mastiz@chromium.org>
2025-04-22 09:45:42 -0700

    [bookmarks] Implement set comparison for metrics purposes
    The metric themselves aren't introduced in this patch, just the logic
    (currently unused outside tests) that, given two sets, returns an enum
    that provides insights into how the two sets compare to each other.
    
    Change-Id: I72b385936bbcb85070f799f7e2e664c8de9cd5d2
    Bug: 407729650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479792
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450005}

--- components/sync_bookmarks/bookmark_model_merger_comparison_metrics_unittest.cc
+++ components/sync_bookmarks/bookmark_model_merger_comparison_metrics_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/uuid.h"
@@ -46,6 +47,7 @@ namespace {
 
 using RemoteTreeNode = BookmarkModelMerger::RemoteTreeNode;
 
+using testing::Eq;
 using testing::UnorderedElementsAre;
 
 // Constants forked from bookmark_model_merger.cc.
@@ -57,6 +59,14 @@ const char kOtherBookmarksTag[] = "other_bookmarks";
 constexpr char16_t kBookmarkBarFolderName[] = u"__Bookmarks bar__";
 constexpr char16_t kOtherBookmarksFolderName[] = u"__Other bookmarks__";
 constexpr char16_t kMobileBookmarksFolderName[] = u"__Mobile bookmarks__";
+
+base::flat_set<int> ContiguousSetBetween(int start, int end) {
+  base::flat_set<int> result;
+  for (int i = start; i <= end; ++i) {
+    result.insert(i);
+  }
+  return result;
+}
 
 // Test class to build bookmark URLs conveniently and compactly in tests.
 class UrlBookmarkBuilder {
@@ -419,6 +429,71 @@ TEST_F(BookmarkModelMergerComparisonMetricsTest, ShouldExtractAccountNodes) {
           UrlAndTitleAndPath{kUrl2, kUrl2Title,
                              base::StrCat({u"/", kBookmarkBarFolderName, u"/",
                                            kFolder1Title})}));
+}
+
+TEST_F(BookmarkModelMergerComparisonMetricsTest, ShouldCompareSets) {
+  // kBothEmpty.
+  EXPECT_THAT(CompareSetsForTesting(/*account_data=*/{}, /*local_data=*/{}),
+              Eq(SetComparisonOutcome::kBothEmpty));
+
+  // kLocalDataEmpty.
+  EXPECT_THAT(CompareSetsForTesting(/*account_data=*/{1}, /*local_data=*/{}),
+              Eq(SetComparisonOutcome::kLocalDataEmpty));
+
+  // kAccountDataEmpty.
+  EXPECT_THAT(CompareSetsForTesting(/*account_data=*/{}, /*local_data=*/{1}),
+              Eq(SetComparisonOutcome::kAccountDataEmpty));
+
+  // kExactMatchNonEmpty.
+  EXPECT_THAT(CompareSetsForTesting(/*account_data=*/{1}, /*local_data=*/{1}),
+              Eq(SetComparisonOutcome::kExactMatchNonEmpty));
+  EXPECT_THAT(
+      CompareSetsForTesting(/*account_data=*/{1, 2}, /*local_data=*/{1, 2}),
+      Eq(SetComparisonOutcome::kExactMatchNonEmpty));
+
+  // kLocalDataIsStrictSubsetOfAccountData.
+  EXPECT_THAT(
+      CompareSetsForTesting(/*account_data=*/{1, 2}, /*local_data=*/{1}),
+      Eq(SetComparisonOutcome::kLocalDataIsStrictSubsetOfAccountData));
+  EXPECT_THAT(
+      CompareSetsForTesting(/*account_data=*/{1, 2}, /*local_data=*/{2}),
+      Eq(SetComparisonOutcome::kLocalDataIsStrictSubsetOfAccountData));
+
+  // kIntersectionBetween99And100Percent.
+  EXPECT_THAT(
+      CompareSetsForTesting(/*account_data=*/ContiguousSetBetween(1, 200),
+                            /*local_data=*/ContiguousSetBetween(2, 201)),
+      Eq(SetComparisonOutcome::kIntersectionBetween99And100Percent));
+
+  // kIntersectionBetween95And99Percent.
+  EXPECT_THAT(
+      CompareSetsForTesting(/*account_data=*/ContiguousSetBetween(1, 50),
+                            /*local_data=*/ContiguousSetBetween(2, 51)),
+      Eq(SetComparisonOutcome::kIntersectionBetween95And99Percent));
+
+  // kIntersectionBetween90And95Percent.
+  EXPECT_THAT(
+      CompareSetsForTesting(/*account_data=*/ContiguousSetBetween(1, 30),
+                            /*local_data=*/ContiguousSetBetween(2, 31)),
+      Eq(SetComparisonOutcome::kIntersectionBetween90And95Percent));
+
+  // kIntersectionBetween50And90Percent.
+  EXPECT_THAT(CompareSetsForTesting(/*account_data=*/{1, 2, 3, 4},
+                                    /*local_data=*/{2, 3, 4, 5}),
+              Eq(SetComparisonOutcome::kIntersectionBetween50And90Percent));
+
+  // kIntersectionBetween10And50Percent.
+  EXPECT_THAT(CompareSetsForTesting(/*account_data=*/{1, 2},
+                                    /*local_data=*/{2, 3}),
+              Eq(SetComparisonOutcome::kIntersectionBetween10And50Percent));
+
+  // kIntersectionBelow10Percent.
+  EXPECT_THAT(
+      CompareSetsForTesting(/*account_data=*/{1, 2},
+                            /*local_data=*/ContiguousSetBetween(2, 11)),
+      Eq(SetComparisonOutcome::kIntersectionBelow10PercentExcludingZero));
+
+  // IntersectionEmpty.
 }
 
 }  // namespace
