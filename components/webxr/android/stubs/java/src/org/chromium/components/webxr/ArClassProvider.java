15e8f3edfe9f0bd4168cd5690565fbaa6a77c3d7
wuguobin <wuguobin.1229@bytedance.com>
2025-04-22 07:00:18 -0700

    webxr: Replace AndroidX Nullable with Chromium Nullable.
    This two files won't be compiled if |enable_cardboard| and |enable_arcore|
    are true. If they are set to false, compile errors raise as follows.
    
    ```
    CardboardClassProvider.java:21: error:
      annotation @Nullable not applicable in this type context
        /*package*/ static XrImmersiveOverlay.@Nullable Delegate getOverlayDelegate(
    ```
    
    Use the Chromium Nullable instead to fix them.
    
    Bug: 389129271
    Change-Id: I509169a8a55e7a6c9ddb537bb4c83e02baf05016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6472159
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Guobin Wu <wuguobin.1229@bytedance.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449892}

--- components/webxr/android/stubs/java/src/org/chromium/components/webxr/ArClassProvider.java
+++ components/webxr/android/stubs/java/src/org/chromium/components/webxr/ArClassProvider.java
@@ -5,9 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.webxr;
 
-import androidx.annotation.Nullable;
-
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.content_public.browser.WebContents;
 
 /**
