e38dfa28cca5f666ff2d79eb94dc64daad998e0b
Reilly Grant <reillyg@chromium.org>
2025-04-22 15:45:10 -0700

    Revert "Extensions: CodeHealth: Prefer CHECK_IS_TEST() for test-only methods"
    This reverts commit 6d3f5d32c34cbaa88320f6d5475343d659dc829f.
    
    Reason for revert: CHECK_IS_TEST is unnecessary within test-only functions as presubmits already block calling them from production code.
    
    Bug: chromium:346952289
    Original change's description:
    > Extensions: CodeHealth: Prefer CHECK_IS_TEST() for test-only methods
    >
    > Reference:
    > https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/c++.md#test_only-code-paths-in-production-code
    >
    > This CL was uploaded by git cl split.
    >
    > R=mcnee@chromium.org
    >
    > Bug: chromium:346952289
    > Change-Id: I7a58148940cae4dcfb07cfa832c8eb0aa8bbce86
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477611
    > Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    > Commit-Queue: Kevin McNee <mcnee@chromium.org>
    > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1450131}
    
    Bug: chromium:346952289
    Change-Id: Id74f222dd72f44e6461839e17e32110ebf02cfb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479001
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1450265}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_attach_helper.h
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_attach_helper.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/check_is_test.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -92,7 +91,6 @@ class MimeHandlerViewAttachHelper : content::RenderProcessHostObserver {
   // AttachToOuterWebContents() and ResumeAttachOrDestroy().
   void set_resume_attach_callback_for_testing(
       base::OnceCallback<void(base::OnceClosure)> callback) {
-    CHECK_IS_TEST();
     resume_attach_callback_for_testing_ = std::move(callback);
   }
 
--- extensions/browser/guest_view/web_view/web_view_renderer_state.cc
+++ extensions/browser/guest_view/web_view/web_view_renderer_state.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/guest_view/web_view/web_view_renderer_state.h"
 
-#include "base/check_is_test.h"
 #include "base/containers/contains.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -162,11 +161,6 @@ void WebViewRendererState::RemoveContentScriptIDs(
       // due to OOPIFs inside <webview>).
     }
   }
-}
-
-size_t WebViewRendererState::guest_count_for_testing() {
-  CHECK_IS_TEST();
-  return web_view_info_map_.size();
 }
 
 }  // namespace extensions
