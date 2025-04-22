710cad544e4b06fb61ce2bf58191ff368fd2dd18
Matt Menke <mmenke@chromium.org>
2025-04-22 12:49:31 -0700

    Remove allow_unsafe_buffers from web_transport.cc.
    It was manually converting a string_view to a span<uint8_t> instead of
    using the macros. Unfortunately, quiche APIs all use string_views for
    binary data and can't depend on base/, so we need such conversions
    around values received from/passed to quiche.
    
    Bug: 351564777
    Change-Id: Id720a817dc6dcbfdc9601c15b8c3af69e8495399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476425
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450143}

--- services/network/web_transport.cc
+++ services/network/web_transport.cc
@@ -3,12 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "services/network/web_transport.h"
+
+#include <stdint.h>
 
 #include "base/containers/span.h"
 #include "base/functional/bind.h"
@@ -737,8 +734,7 @@ void WebTransport::OnDatagramReceived(std::string_view datagram) {
     return;
   }
 
-  client_->OnDatagramReceived(base::span(
-      reinterpret_cast<const uint8_t*>(datagram.data()), datagram.size()));
+  client_->OnDatagramReceived(base::as_byte_span(datagram));
 }
 
 void WebTransport::OnCanCreateNewOutgoingBidirectionalStream() {
