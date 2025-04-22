e2d28562934e1cdd391b63148c8a48528be917fa
Tove Petersson <tovep@chromium.org>
2025-04-22 06:42:02 -0700

    Update TabSharingInfoBar labels to use the default infobar text style
    Bug: 380903159
    Change-Id: Icdd0328016b5f12da046869cb06370e376d912e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6478434
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449882}

--- chrome/browser/ui/views/tab_sharing/tab_sharing_status_message_view.h
+++ chrome/browser/ui/views/tab_sharing/tab_sharing_status_message_view.h
@@ -67,6 +67,7 @@ class TabSharingStatusMessageView : public views::View {
 
  private:
   void SetupMessage(MessageInfo info);
+  void AddLabel(const std::u16string& text);
   void AddButton(const EndpointInfo& endpoint_info);
 };
 
