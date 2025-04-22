baebd2198c89c712b9c74dcad83c718382519d0b
Reilly Grant <reillyg@chromium.org>
2025-04-22 15:36:48 -0700

    Revert "Extensions: CodeHealth: Prefer CHECK_IS_TEST() for test-only methods"
    This reverts commit ff0e1a6949f63281df2f1ede2eb6dd9e597a113e.
    
    Reason for revert: CHECK_IS_TEST is unnecessary within test-only functions as presubmits already block calling them from production code.
    
    Bug: chromium:346952289
    Original change's description:
    > Extensions: CodeHealth: Prefer CHECK_IS_TEST() for test-only methods
    >
    > Reference:
    > https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/c++.md#test_only-code-paths-in-production-code
    >
    > This CL was uploaded by git cl split.
    >
    > R=cros-settings-reviews@google.com
    >
    > Bug: chromium:346952289
    > Change-Id: I9cb13b7ebc02fa56e942e6affa9a560ce6697596
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476953
    > Reviewed-by: Jeffrey Young <cowmoo@google.com>
    > Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    > Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    > Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1450045}
    
    Bug: chromium:346952289
    Change-Id: I8e012cc8d6d80372a9174db095f49e9ddf9192c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479018
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450257}

--- chrome/browser/extensions/api/settings_private/settings_private_event_router.cc
+++ chrome/browser/extensions/api/settings_private/settings_private_event_router.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/check_is_test.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/task/single_thread_task_runner.h"
@@ -44,11 +43,6 @@ SettingsPrivateEventRouter::~SettingsPrivateEventRouter() {
 void SettingsPrivateEventRouter::OnGeneratedPrefChanged(
     const std::string& pref_name) {
   OnPreferenceChanged(pref_name);
-}
-
-content::BrowserContext* SettingsPrivateEventRouter::context_for_test() {
-  CHECK_IS_TEST();
-  return context_;
 }
 
 void SettingsPrivateEventRouter::Shutdown() {
