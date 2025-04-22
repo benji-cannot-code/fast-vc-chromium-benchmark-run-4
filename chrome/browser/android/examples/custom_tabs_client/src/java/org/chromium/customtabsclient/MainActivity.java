eeae311c067fcde70a249b4ec476406d1769d051
Jinsuk Kim <jinsukkim@chromium.org>
2025-04-22 04:16:56 -0700

    [pcct] Example app uses the official API for background interaction
    Instead of a deprecated intent extra, now the example app adopts
    the official API:
    
    - CustomTabsIntent.Builder#setBackgroundInteractionEnabled
    
    The feature is enabled by default, and may be disabled by calling
    the API with |false|.
    
    Change-Id: Idb364369ec8e15d3d3089189ff41ff166c0363c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476984
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449847}

--- chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/MainActivity.java
+++ chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/MainActivity.java
@@ -147,7 +147,6 @@ public class MainActivity extends AppCompatActivity
     private static final int CLOSE_ICON_CHECK = 2;
     private static final int UNCHECKED = 0;
     private static final int CHECKED = 1;
-    private static final int BACKGROUND_INTERACT_OFF_VALUE = 2;
 
     /** Minimal height the bottom sheet CCT should show is half of the display height. */
     private static final float MINIMAL_HEIGHT_RATIO = 0.5f;
@@ -1115,6 +1114,8 @@ public class MainActivity extends AppCompatActivity
                 builder.setActivitySideSheetBreakpointDp(pcctBreakpointDp);
             }
 
+            builder.setBackgroundInteractionEnabled(mBackgroundInteractCheckbox.isChecked());
+
             customTabsIntent = builder.build();
             int toolbarCornerRadiusDp = mToolbarCornerRadiusSlider.getProgress();
             customTabsIntent.intent.putExtra(EXTRA_CLOSE_BUTTON_POSITION, closeButtonPosition);
@@ -1133,11 +1134,6 @@ public class MainActivity extends AppCompatActivity
             if (!mPcctHeightResizableCheckbox.isChecked()) {
                 customTabsIntent.intent.putExtra(
                         EXTRA_ACTIVITY_HEIGHT_RESIZE_BEHAVIOR, ACTIVITY_HEIGHT_FIXED);
-            }
-            if (!mBackgroundInteractCheckbox.isChecked()) {
-                customTabsIntent.intent.putExtra(
-                        "androidx.browser.customtabs.extra.ENABLE_BACKGROUND_INTERACTION",
-                        BACKGROUND_INTERACT_OFF_VALUE);
             }
             if (mSideSheetRoundedCornerCheckbox.isChecked()) {
                 customTabsIntent.intent.putExtra(
