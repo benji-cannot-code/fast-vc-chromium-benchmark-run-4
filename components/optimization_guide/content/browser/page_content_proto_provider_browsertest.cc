9332dd7c1a91e4097441983cd90a88f1097a53cf
Sophie Chang <sophiechang@chromium.org>
2025-04-23 14:56:31 -0700

    Use mojom as source of truth for defaults for AIPageContentOptions
    Sets all options to false as defaults
    
    Change-Id: I7b170ef9ccf15017e861e66d8d4c9a64903187d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479123
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450823}

--- components/optimization_guide/content/browser/page_content_proto_provider_browsertest.cc
+++ components/optimization_guide/content/browser/page_content_proto_provider_browsertest.cc
@@ -75,6 +75,15 @@ void AssertValidOrigin(
   }
 }
 
+blink::mojom::AIPageContentOptionsPtr GetAIPageContentOptions() {
+  auto request = blink::mojom::AIPageContentOptions::New();
+  request->include_geometry = true;
+  request->on_critical_path = true;
+  request->include_hidden_searchable_content = true;
+
+  return request;
+}
+
 class PageContentProtoProviderBrowserTest : public content::ContentBrowserTest {
  public:
   PageContentProtoProviderBrowserTest() = default;
@@ -127,7 +136,7 @@ class PageContentProtoProviderBrowserTest : public content::ContentBrowserTest {
   }
 
   void LoadData(blink::mojom::AIPageContentOptionsPtr request =
-                    DefaultAIPageContentOptions()) {
+                    GetAIPageContentOptions()) {
     base::RunLoop run_loop;
     GetAIPageContent(
         web_contents(), std::move(request),
@@ -320,6 +329,7 @@ IN_PROC_BROWSER_TEST_F(PageContentProtoProviderBrowserTest,
 
   auto request = blink::mojom::AIPageContentOptions::New();
   request->on_critical_path = false;
+  request->include_geometry = true;
   LoadData(std::move(request));
 
   EXPECT_EQ(page_content().root_node().children_nodes().size(), 1);
