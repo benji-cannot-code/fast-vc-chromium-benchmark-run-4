d6b5c67da9a87e380b4d9e9b20a03f73ba3a34f1
Lei Zhang <thestig@chromium.org>
2025-04-23 14:35:08 -0700

    Printing: Add debug code for PrintDialogGtk::LoadPrintSettings() failure
    A CHECK() is failing in PrintDialogGtk::LoadPrintSettings() but it is
    unclear why. Add some temporary code for debugging to capture the
    in-use printer name and the list of printers.
    
    Bug: 411336538
    Change-Id: I17b872e27ee7fc3a713d21324291752bb4825ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6481394
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450812}

--- ui/gtk/printing/print_dialog_gtk.cc
+++ ui/gtk/printing/print_dialog_gtk.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/dcheck_is_on.h"
+#include "base/debug/crash_logging.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
@@ -152,6 +153,16 @@ class GtkPrinterList {
     }
 
     return nullptr;
+  }
+
+  // TODO(crbug.com/411336538): Delete when done diagnosing.
+  std::string GetAllPrinterNames() {
+    std::string result;
+    for (ScopedGObject<GtkPrinter>& printer : printers_) {
+      result += gtk_printer_get_name(printer.get());
+      result += ',';
+    }
+    return result;
   }
 
  private:
@@ -378,7 +389,14 @@ void PrintDialogGtk::LoadPrintSettings(const PrintSettings& settings) {
 
   auto printer_list = std::make_unique<GtkPrinterList>();
   printer_ = printer_list->GetPrinterWithName(*printer_name);
-  CHECK(printer_);
+  if (!printer_) {
+    // TODO(crbug.com/411336538): Delete the crash keys when done diagnosing.
+    // Then either put CHECK(printer_); back, or properly handle the failure.
+    SCOPED_CRASH_KEY_STRING64("PrintDialogGtk", "printer_name", *printer_name);
+    SCOPED_CRASH_KEY_STRING1024("PrintDialogGtk", "all_printer_names",
+                                printer_list->GetAllPrinterNames());
+    CHECK(false);
+  }
 
   if (!gtk_settings_) {
     gtk_settings_ = gtk_print_settings_copy(GetLastUsedSettings().settings());
