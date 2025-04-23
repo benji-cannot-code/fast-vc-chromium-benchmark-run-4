2a55c885e0e3ce8d9d8bfeccfad46916070dd59c
Sam Maier <smaier@chromium.org>
2025-04-23 07:30:59 -0700

    Roll Android NDK to r28
    Release notes here https://github.com/android/ndk/releases/tag/r28.
    
    Bug: 411432018
    Change-Id: Iff853504c9a16772b2d01ab320de6f48e6418cd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479215
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450535}

--- net/base/network_interfaces_getifaddrs.h
+++ net/base/network_interfaces_getifaddrs.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "net/base/net_export.h"
 #include "net/base/network_interfaces.h"
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/requires_api.h"
+#endif  // BUILDFLAG(IS_ANDROID)
 
 struct ifaddrs;
 
@@ -50,11 +53,13 @@ NET_EXPORT_PRIVATE bool IfaddrsToNetworkInterfaceList(
     NetworkInterfaceList* networks);
 
 #if BUILDFLAG(IS_ANDROID)
+#define GETIFADDRS_MIN_API 24
 // A version of GetNetworkList() that uses getifaddrs(). Only callable on
 // Android N+ where getifaddrs() was available.
 // Also, some devices are with buggy getifaddrs(). To work around,
 // Use Chromium's own getifaddrs() implementation if
 // use_alternative_getifaddrs is true.
+REQUIRES_ANDROID_API(GETIFADDRS_MIN_API)
 bool GetNetworkListUsingGetifaddrs(NetworkInterfaceList* networks,
                                    int policy,
                                    bool use_alternative_getifaddrs);
