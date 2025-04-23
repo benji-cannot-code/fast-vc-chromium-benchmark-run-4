4fe64454171153c762e5cdf7f0dfa8f48880a8e3
Aishwarya Rajesh <aishwaryarj@google.com>
2025-04-23 13:34:32 -0700

    [Gardener] Disable test BookmarkTest#testTopLevelFolders
    Bug: 413079613
    Change-Id: I1e2e94e9466f69d7cb991ed308e30865b22d8845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6483133
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Owners-Override: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450772}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
@@ -1243,6 +1243,7 @@ public class BookmarkTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/413079613")
     public void testTopLevelFolders() throws Exception {
         openBookmarkManager();
         onViewWaiting(allOf(withText("Mobile bookmarks"), isDisplayed()));
