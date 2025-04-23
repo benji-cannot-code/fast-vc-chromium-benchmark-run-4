e32ee5bd32c6174871d04068bbaf1260b51bf63b
Dorian Brandon <dorianbrandon@google.com>
2025-04-23 11:34:36 -0700

    Start spotlight notification at end of countdown.
    Modify the persistent notification for the teacher CRD session to be initiated after the end of the countdown. This will give us more reliability than waiting on the ACTIVE state from server.
    
    Bug: 411484418
    Change-Id: I522c172890038dd66ea694c0e9931b913c030f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479774
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Dorian Brandon <dorianbrandon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450690}

--- chromeos/ash/components/boca/spotlight/spotlight_session_manager_unittest.cc
+++ chromeos/ash/components/boca/spotlight/spotlight_session_manager_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "chromeos/ash/components/boca/boca_app_client.h"
 #include "chromeos/ash/components/boca/boca_session_manager.h"
 #include "chromeos/ash/components/boca/proto/session.pb.h"
@@ -44,6 +45,10 @@ constexpr char kSpotlightConnectionCode[] = "456";
 constexpr char kUserEmail[] = "cat@gmail.com";
 constexpr char kUserFullName[] = "Best Teacher";
 constexpr char kTestBaseUrl[] = "https://test";
+// Length of the notification duration and one extra interval for the
+// notification to start.
+constexpr base::TimeDelta kTestNotificationDuration =
+    kSpotlightNotificationDuration + kSpotlightNotificationCountdownInterval;
 
 class MockBocaAppClient : public BocaAppClient {
  public:
@@ -155,7 +160,8 @@ class SpotlightSessionManagerTest : public testing::Test {
   }
 
  protected:
-  base::test::SingleThreadTaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   MockSessionManager* session_manager() { return session_manager_.get(); }
   MockSpotlightService* spotlight_service() { return spotlight_service_; }
   MockSpotlightCrdManager* spotlight_crd_manager() {
@@ -194,20 +200,28 @@ TEST_F(SpotlightSessionManagerTest, IniatesSpotlightSessionWhenRequested) {
   std::map<std::string, ::boca::StudentStatus> activities;
   activities.emplace(kGaiaId, status);
 
+  // Expect CRD to return an connection code.
   EXPECT_CALL(*spotlight_crd_manager(), InitiateSpotlightSession)
       .WillOnce(WithArg<0>(Invoke([&](auto callback) {
         std::move(callback).Run(kSpotlightConnectionCode);
       })));
+  // Expect sending the code to server.
   EXPECT_CALL(*spotlight_service(),
               RegisterScreen(kSpotlightConnectionCode, kTestBaseUrl, _))
       .WillOnce(WithArg<2>(Invoke(
           [&](auto callback) { std::move(callback).Run(base::ok(true)); })));
+  // Expect persistent notification to show after countdown.
+  EXPECT_CALL(*spotlight_crd_manager(),
+              ShowPersistentNotification(kUserFullName))
+      .Times(1);
   EXPECT_CALL(*session_manager(), LoadCurrentSession(false)).Times(1);
 
   ::boca::UserIdentity producer;
   producer.set_email(kUserEmail);
+  producer.set_full_name(kUserFullName);
   spotlight_session_manager_->OnSessionStarted(kSessionId, producer);
   spotlight_session_manager_->OnConsumerActivityUpdated(activities);
+  task_environment_.FastForwardBy(kTestNotificationDuration);
 }
 
 TEST_F(SpotlightSessionManagerTest, DoesNotStartSpotlightWithInactiveSession) {
@@ -293,47 +307,6 @@ TEST_F(SpotlightSessionManagerTest, OnlyProcessesOneRequestAtATime) {
               RegisterScreen(kSpotlightConnectionCode, kTestBaseUrl, _))
       .Times(1);
   spotlight_session_manager_->OnSessionEnded(kSessionId);
-  spotlight_session_manager_->OnSessionStarted(kSessionId, producer);
-  spotlight_session_manager_->OnConsumerActivityUpdated(activities);
-}
-
-TEST_F(SpotlightSessionManagerTest,
-       StartsPersistentNotificationWhenSpotlightActive) {
-  ::boca::StudentDevice device;
-  device.mutable_view_screen_config()->set_view_screen_state(
-      ::boca::ViewScreenConfig::ACTIVE);
-  ::boca::StudentStatus status;
-  status.mutable_devices()->emplace(kDeviceId, device);
-
-  std::map<std::string, ::boca::StudentStatus> activities;
-  activities.emplace(kGaiaId, status);
-
-  EXPECT_CALL(*spotlight_crd_manager(),
-              ShowPersistentNotification(kUserFullName))
-      .Times(1);
-
-  ::boca::UserIdentity producer;
-  producer.set_email(kUserEmail);
-  producer.set_full_name(kUserFullName);
-  spotlight_session_manager_->OnSessionStarted(kSessionId, producer);
-  spotlight_session_manager_->OnConsumerActivityUpdated(activities);
-}
-
-TEST_F(SpotlightSessionManagerTest,
-       StopsPersistentNotificationWhenSpotlightInactive) {
-  ::boca::StudentDevice device;
-  device.mutable_view_screen_config()->set_view_screen_state(
-      ::boca::ViewScreenConfig::INACTIVE);
-  ::boca::StudentStatus status;
-  status.mutable_devices()->emplace(kDeviceId, device);
-
-  std::map<std::string, ::boca::StudentStatus> activities;
-  activities.emplace(kGaiaId, status);
-
-  EXPECT_CALL(*spotlight_crd_manager(), HidePersistentNotification).Times(1);
-
-  ::boca::UserIdentity producer;
-  producer.set_email(kUserEmail);
   spotlight_session_manager_->OnSessionStarted(kSessionId, producer);
   spotlight_session_manager_->OnConsumerActivityUpdated(activities);
 }
