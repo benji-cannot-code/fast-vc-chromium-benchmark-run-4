ab83238aeea0892a51d4124565b172fb3d0c4b09
Alexander Surkov <asurkov@igalia.com>
2025-04-23 05:13:02 -0700

    [Accessibility Common] Build MV3 version; move third-party test files
    Remove copies of third-party test files from MV2 BUILD.gn. These files
    are now included only in the MV3 BUILD.gn, as they are shared but only
    need to be copied once.
    
    Bug: 267329383
    Change-Id: I90668cace0bd0fe425e403ea1b6151966748d2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438335
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1450481}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/mv3/facegaze/web_cam_face_landmarker.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/mv3/facegaze/web_cam_face_landmarker.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {TestImportManager} from '/common/testing/test_import_manager.js';
 import type {FaceLandmarkerOptions, FaceLandmarkerResult} from '/third_party/mediapipe/vision.js';
-import {FaceLandmarker} from 'chrome-extension://egfdjlfmgnehecnclamagfafdccgfndp/accessibility_common/third_party/mediapipe_task_vision/vision_bundle.mjs';
+import {FaceLandmarker} from 'chrome-extension://egfdjlfmgnehecnclamagfafdccgfndp/accessibility_common/mv3/third_party/mediapipe_task_vision/vision_bundle.mjs';
 
 import {PrefNames} from './constants.js';
 
