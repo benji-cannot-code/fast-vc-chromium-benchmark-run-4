c8da190e8f5d0461e6d19a02d0ce9172ac48a5b5
Clark DuVall <cduvall@chromium.org>
2025-04-22 19:45:43 -0700

    Enable AIPromptAPIStructuredOutput by default if AIPromptAPI is enabled
    This may also be enabled through the AIPromptAPIForExtension origin
    trial.
    
    Bug: 391919456
    Change-Id: I1f4e92585a98a83bfd4cd414fca72e7b29d94e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6481525
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450322}

--- third_party/blink/renderer/modules/ai/language_model.cc
+++ third_party/blink/renderer/modules/ai/language_model.cc
@@ -521,7 +521,8 @@ ScriptPromise<IDLString> LanguageModel::prompt(
   }
 
   String stringified_schema;
-  if (RuntimeEnabledFeatures::AIPromptAPIStructuredOutputEnabled()) {
+  if (RuntimeEnabledFeatures::AIPromptAPIStructuredOutputEnabled(
+          GetExecutionContext())) {
     if (options->hasResponseJSONSchema()) {
       stringified_schema = ValidateAndStringifyObject(
           options->responseJSONSchema(), script_state, exception_state);
@@ -596,7 +597,8 @@ ReadableStream* LanguageModel::promptStreaming(
   }
 
   String stringified_schema;
-  if (RuntimeEnabledFeatures::AIPromptAPIStructuredOutputEnabled()) {
+  if (RuntimeEnabledFeatures::AIPromptAPIStructuredOutputEnabled(
+          GetExecutionContext())) {
     if (options->hasResponseJSONSchema()) {
       stringified_schema = ValidateAndStringifyObject(
           options->responseJSONSchema(), script_state, exception_state);
