cc50e6604eb0f0216e4d6d0b6f368b4ba53b76e2
dpapad <dpapad@chromium.org>
2025-04-23 16:12:04 -0700

    PrintPreview: Migrate to Lit, part 5.
    In this part migrating
     - ui/header.ts
     - ui/media_size_settings.ts
     - ui/more_settings.ts
    
    Bug: 410607166
    Change-Id: I3ed0d27cf0c48a89323a51415a7fc5d57eab31f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6473218
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450854}

--- chrome/browser/resources/print_preview/ui/more_settings.ts
+++ chrome/browser/resources/print_preview/ui/more_settings.ts
@@ -3,18 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
-import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import '/strings.m.js';
 
 import type {CrExpandButtonElement} from 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import {assert} from 'chrome://resources/js/assert.js';
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import {MetricsContext, PrintSettingsUiBucket} from '../metrics.js';
 
-import {getTemplate} from './more_settings.html.js';
+import {getCss} from './more_settings.css.js';
+import {getHtml} from './more_settings.html.js';
 
 interface PrintPreviewMoreSettingsElement {
   $: {
@@ -23,16 +22,20 @@ interface PrintPreviewMoreSettingsElement {
 }
 
 
-class PrintPreviewMoreSettingsElement extends PolymerElement {
+class PrintPreviewMoreSettingsElement extends CrLitElement {
   static get is() {
     return 'print-preview-more-settings';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  static get properties() {
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
     return {
       settingsExpandedByUser: {
         type: Boolean,
@@ -41,19 +44,19 @@ class PrintPreviewMoreSettingsElement extends PolymerElement {
 
       disabled: {
         type: Boolean,
-        reflectToAttribute: true,
+        reflect: true,
       },
     };
   }
 
-  declare settingsExpandedByUser: boolean;
-  declare disabled: boolean;
+  accessor settingsExpandedByUser: boolean;
+  accessor disabled: boolean;
   private metrics_: MetricsContext = MetricsContext.printSettingsUi();
 
   /**
    * Toggles the expand button within the element being listened to.
    */
-  private toggleExpandButton_(e: Event) {
+  protected toggleExpandButton_(e: Event) {
     // The expand button handles toggling itself.
     const expandButtonTag = 'CR-EXPAND-BUTTON';
     if ((e.target as HTMLElement).tagName === expandButtonTag) {
@@ -73,7 +76,13 @@ class PrintPreviewMoreSettingsElement extends PolymerElement {
             PrintSettingsUiBucket.MORE_SETTINGS_CLICKED :
             PrintSettingsUiBucket.LESS_SETTINGS_CLICKED);
   }
+
+  protected onSettingsExpandedByUserChanged_(e: CustomEvent<{value: boolean}>) {
+    this.settingsExpandedByUser = e.detail.value;
+  }
 }
+
+export type MoreSettingsElement = PrintPreviewMoreSettingsElement;
 
 declare global {
   interface HTMLElementTagNameMap {
--- chrome/test/data/webui/print_preview/header_test.ts
+++ chrome/test/data/webui/print_preview/header_test.ts
@@ -3,12 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://print/print_preview.js';
+
 import type {PrintPreviewHeaderElement} from 'chrome://print/print_preview.js';
 import {Destination, DestinationOrigin, GooglePromotedDestinationId, PrintPreviewPluralStringProxyImpl, State} from 'chrome://print/print_preview.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {fakeDataBind} from 'chrome://webui-test/polymer_test_util.js';
 import {TestPluralStringProxy} from 'chrome://webui-test/test_plural_string_proxy.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 suite('HeaderTest', function() {
   let header: PrintPreviewHeaderElement;
@@ -26,7 +28,6 @@ suite('HeaderTest', function() {
     document.body.appendChild(model);
 
     header = document.createElement('print-preview-header');
-    header.settings = model.settings;
     model.set('settings.duplex.available', true);
     model.set('settings.duplex.value', false);
 
@@ -36,25 +37,23 @@ suite('HeaderTest', function() {
     header.state = State.READY;
     header.managed = false;
     header.sheetCount = 1;
-    fakeDataBind(model, header, 'settings');
     document.body.appendChild(header);
   });
 
   function setPdfDestination() {
-    header.set(
-        'destination',
-        new Destination(
-            GooglePromotedDestinationId.SAVE_AS_PDF, DestinationOrigin.LOCAL,
-            loadTimeData.getString('printToPDF')));
+    header.destination = new Destination(
+        GooglePromotedDestinationId.SAVE_AS_PDF, DestinationOrigin.LOCAL,
+        loadTimeData.getString('printToPDF'));
   }
 
   // Tests that the 4 different messages (non-virtual printer singular and
   // plural, virtual printer singular and plural) all show up as expected.
   test('HeaderPrinterTypes', async function() {
-    const summary = header.shadowRoot!.querySelector('.summary')!;
+    const summary = header.shadowRoot.querySelector('.summary')!;
     {
       const {messageName, itemCount} =
           await pluralString.whenCalled('getPluralString');
+      await microtasksFinished();
       assertEquals('1 sheet of paper', summary.textContent!.trim());
       assertEquals('printPreviewSheetSummaryLabel', messageName);
       assertEquals(1, itemCount);
@@ -64,6 +63,7 @@ suite('HeaderTest', function() {
       header.sheetCount = 3;
       const {messageName, itemCount} =
           await pluralString.whenCalled('getPluralString');
+      await microtasksFinished();
       assertEquals('printPreviewSheetSummaryLabel', messageName);
       assertEquals(3, itemCount);
     }
@@ -72,6 +72,7 @@ suite('HeaderTest', function() {
       setPdfDestination();
       const {messageName, itemCount} =
           await pluralString.whenCalled('getPluralString');
+      await microtasksFinished();
       assertEquals('printPreviewPageSummaryLabel', messageName);
       assertEquals(3, itemCount);
     }
@@ -80,39 +81,47 @@ suite('HeaderTest', function() {
       header.sheetCount = 1;
       const {messageName, itemCount} =
           await pluralString.whenCalled('getPluralString');
+      await microtasksFinished();
       assertEquals('printPreviewPageSummaryLabel', messageName);
       assertEquals(1, itemCount);
     }
     // Verify the chrome://print case of a zero length document does not show
     // the summary.
     header.sheetCount = 0;
+    await microtasksFinished();
     assertEquals('', summary.textContent);
   });
 
   // Tests that the correct message is shown for non-READY states, and that
   // the print button is disabled appropriately.
   test('HeaderChangesForState', async function() {
-    const summary = header.shadowRoot!.querySelector('.summary')!;
+    const summary = header.shadowRoot.querySelector('.summary')!;
     await pluralString.whenCalled('getPluralString');
+    await microtasksFinished();
     assertEquals('1 sheet of paper', summary.textContent!.trim());
 
     header.state = State.NOT_READY;
+    await microtasksFinished();
     assertEquals('', summary.textContent!.trim());
 
     header.state = State.PRINTING;
+    await microtasksFinished();
     assertEquals(
         loadTimeData.getString('printing'), summary.textContent!.trim());
     setPdfDestination();
+    await microtasksFinished();
     assertEquals(loadTimeData.getString('saving'), summary.textContent!.trim());
 
     header.state = State.ERROR;
+    await microtasksFinished();
     assertEquals('', summary.textContent!.trim());
   });
 
   // Tests that enterprise badge shows up if any setting is managed.
-  test('EnterprisePolicy', function() {
-    assertTrue(header.shadowRoot!.querySelector('cr-icon')!.hidden);
+  test('EnterprisePolicy', async function() {
+    assertTrue(header.shadowRoot.querySelector('cr-icon')!.hidden);
     header.managed = true;
-    assertFalse(header.shadowRoot!.querySelector('cr-icon')!.hidden);
+    await microtasksFinished();
+    assertFalse(header.shadowRoot.querySelector('cr-icon')!.hidden);
   });
 });
