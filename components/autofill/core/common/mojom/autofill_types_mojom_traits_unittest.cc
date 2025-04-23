37b9b2cdfb22e36848bd4ad3a7d63bd34589e25a
Yi Gu <yigu@chromium.org>
2025-04-23 04:43:20 -0700

    [FedCM] Fix ubsan failure due to not initializing the new field in test
    crrev.com/c/6470262 introduced a new field but didn't update the test,
    which caused undefined behavior.
    
    Bug: 410533051,412709277
    Change-Id: I1c6560c436ad3a55cf70fb3f647105719c47a69d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6480317
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Owners-Override: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450471}

--- components/autofill/core/common/mojom/autofill_types_mojom_traits_unittest.cc
+++ components/autofill/core/common/mojom/autofill_types_mojom_traits_unittest.cc
@@ -102,6 +102,7 @@ void CreateTriggeringField(TriggeringField* data) {
   data->text_direction = base::i18n::RIGHT_TO_LEFT;
   data->typed_username = u"username";
   data->show_webauthn_credentials = true;
+  data->show_identity_credentials = true;
 }
 
 void CreatePasswordSuggestionRequest(PasswordSuggestionRequest* data) {
@@ -144,6 +145,8 @@ void CheckEqualTriggeringField(const TriggeringField& expected,
   EXPECT_EQ(expected.typed_username, actual.typed_username);
   EXPECT_EQ(expected.show_webauthn_credentials,
             actual.show_webauthn_credentials);
+  EXPECT_EQ(expected.show_identity_credentials,
+            actual.show_identity_credentials);
   EXPECT_EQ(expected.bounds, actual.bounds);
 }
 
