089cafefcf3a425839580e607ad3b983ae11ad92
Lukasz Anforowicz <lukasza@chromium.org>
2025-04-23 12:21:06 -0700

    [code health] Delete `AvoidSelectedTabFocusOnLayoutDoneShowing` feature.
    The feature (enabled-by-default) has been introduced in
    https://crrev.com/c/4749380 in August 2023 so it seems safe to remove it
    now.
    
    Fixed: 40249125
    Fixed: 356235112
    Change-Id: I663725df258b12addebc74bfdc1e08ce94e2ec07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476830
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450729}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -214,7 +214,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAsyncNotificationManager,
     &kAsyncNotificationManagerForDownload,
     &kAuxiliarySearchDonation,
-    &kAvoidSelectedTabFocusOnLayoutDoneShowing,
     &kBackgroundThreadPool,
     &kBlockIntentsWhileLocked,
     &kBookmarkPaneAndroid,
@@ -468,10 +467,6 @@ BASE_FEATURE(kAdaptiveButtonInTopToolbarPageSummary,
 
 BASE_FEATURE(kAllowNewIncognitoTabIntents,
              "AllowNewIncognitoTabIntents",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kAvoidSelectedTabFocusOnLayoutDoneShowing,
-             "AvoidSelectedTabFocusOnLayoutDoneShowing",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDisableCompositedProgressBar,
