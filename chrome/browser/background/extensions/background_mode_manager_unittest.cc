d79d7711dd077242e9898ce8c837fd172cb20576
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-04-23 16:09:17 -0700

    [Extensions] Add a TestExtensionSystem::Init() method
    Add an Init() method to TestExtensionSystem to be used to initialize
    the broader extension system. On non-desktop-android builds, this
    corresponds to initializing the ExtensionService, but on the
    experimental desktop android build, this will have separate
    bootstrapping.
    
    This CL has no behavior change.
    
    Bug: 412935322
    Change-Id: I71b80643187f281e2eac8a229df2060b3ceb9e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479322
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450851}

--- chrome/browser/background/extensions/background_mode_manager_unittest.cc
+++ chrome/browser/background/extensions/background_mode_manager_unittest.cc
@@ -702,8 +702,7 @@ TEST_F(BackgroundModeManagerWithExtensionsTest, BackgroundMenuGeneration) {
 
   static_cast<extensions::TestExtensionSystem*>(
       extensions::ExtensionSystem::Get(profile_))
-      ->CreateExtensionService(base::CommandLine::ForCurrentProcess(),
-                               base::FilePath(), false);
+      ->Init();
   extensions::ExtensionService* service =
       extensions::ExtensionSystem::Get(profile_)->extension_service();
   service->Init();
