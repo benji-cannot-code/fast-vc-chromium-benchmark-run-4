0c220e398affc200ae6092013acf786ef3836bc9
Juan Mojica <juanmojica@google.com>
2025-04-23 13:56:31 -0700

    [lensoverlay] Allow the error page to be set to protected.
    Screenshot: http://screen/aESY98EMcLBuHtn
    Dark Mode: http://screen/BoqGNQdtevtTC47
    
    Change-Id: I200f1989273c75d7b443b220d80856c352a41cbe
    Bug: 409669067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6484553
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450787}

--- chrome/browser/resources/lens/overlay/side_panel/side_panel_app.ts
+++ chrome/browser/resources/lens/overlay/side_panel/side_panel_app.ts
@@ -374,6 +374,7 @@ export class LensSidePanelAppElement extends LensSidePanelAppElementBase {
   }
 
   private setShowErrorPage(shouldShowErrorPage: boolean) {
+    this.$.errorPage.setIsProtectedError(false);
     this.isErrorPageVisible =
         shouldShowErrorPage && loadTimeData.getBoolean('enableErrorPage');
   }
