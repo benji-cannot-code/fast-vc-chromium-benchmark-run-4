6bec56b496d735f73527a9847774c1f10ca91ef5
Jenny Zhang <jennyz@chromium.org>
2025-04-22 19:47:47 -0700

    [extensions] Enable more offscreen api tests.
    Enable more offscreen api test cases on desktop android since
    messaging is supported on desktop android now. The only 2 test
    cases remained disabled on desktop android uses chrome.action
    api which is not supported on desktop android yet.
    
    The other minor change is to use ENABLE_EXTENSIONS instead
    of IS_ANDROID build flag due to the recent discussion.
    
    Bug: 378916068
    Change-Id: Ifbf2c4880e317504859a8bfe8d92edee22bbbd09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477112
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450325}

--- chrome/browser/extensions/api/offscreen/offscreen_document_manager_browsertest.cc
+++ chrome/browser/extensions/api/offscreen/offscreen_document_manager_browsertest.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/switches.h"
 #include "extensions/test/test_extension_dir.h"
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/ui/browser.h"
 #endif
 
@@ -514,15 +514,15 @@ IN_PROC_BROWSER_TEST_F(OffscreenDocumentManagerBrowserTest,
                    ->GetBrowserContext()
                    ->IsOffTheRecord());
 
-#if BUILDFLAG(IS_ANDROID)
-  Profile* incognito_profile =
-      profile()->GetPrimaryOTRProfile(/*create_if_needed=*/true);
-#else
+#if BUILDFLAG(ENABLE_EXTENSIONS)
   // Create an incognito browser and an incognito offscreen document, and
   // validate that the proper context is used.
   Browser* incognito_browser = CreateIncognitoBrowser();
   ASSERT_TRUE(incognito_browser);
   Profile* incognito_profile = incognito_browser->profile();
+#else
+  Profile* incognito_profile =
+      profile()->GetPrimaryOTRProfile(/*create_if_needed=*/true);
 #endif
 
   OffscreenDocumentHost* incognito_host = CreateDocumentAndWaitForLoad(
