dba36b86a67460743c1ca35c887b8d9d868fb1f4
Kalvin Lee <kdlee@chromium.org>
2025-04-23 14:45:55 -0700

    spanify: Renewed arrayification (X/43)
    This CL is part of the wider spanification / arrayification effort [1]
    and does not (intentionally) introduce behavioral change. It applies the
    `std::array` rewrite to
    
    url
    
    as close as possible to the output directly written by `spanify`, with
    no hand-rolled fixes (except where rebasing would require).
    
    This change (and its siblings taken together) is thought not to cause
    any measurable perf regressions [2].
    
    [1] https://issues.chromium.org/356643982
    [2] https://docs.google.com/document/d/1jughaR6JKn7T-dDjMou10awyNkhE5T-vLY_K2YMqHE4/edit?tab=t.0#heading=h.r2eguxl5lhu7
    
    This CL was uploaded by git cl split.
    
    Bug: 406029216
    Change-Id: Ifb665e62567e9eeddd8f26b9f4e9e5fdaec10f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6475173
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Owners-Override: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450817}

--- url/url_canon_internal.h
+++ url/url_canon_internal.h
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/350788890): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef URL_URL_CANON_INTERNAL_H_
 #define URL_URL_CANON_INTERNAL_H_
 
@@ -16,8 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // template bloat because everything is inlined when anybody calls any of our
 // functions.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/350788890): Remove this and spanify to fix the errors.
+#pragma allow_unsafe_buffers
+#endif
+
 #include <stddef.h>
 
+#include <array>
 #include <string>
 
 #include "base/component_export.h"
@@ -65,7 +66,7 @@ enum SharedCharTypes {
 // Using an unsigned char type has a small but measurable performance benefit
 // over using a 32-bit number.
 // clang-format off
-inline constexpr uint8_t kSharedCharTypeTable[0x100] = {
+inline constexpr std::array<uint8_t, 0x100> kSharedCharTypeTable = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 0x00 - 0x0f
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 0x10 - 0x1f
     0,                           // 0x20  ' ' (escape spaces in queries)
