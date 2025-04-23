81ec6af89a9a8bc3745a132370773a55c3388932
Ted Meyer <tmathmeyer@chromium.org>
2025-04-23 16:21:37 -0700

    Enable HLS MP4 demuxing by default.
    Currently at 10% on stable, UMA looks good - some notes: data:
    go/uma-hls-mp4-finch
    
    Media.HLS.InitializationError is down by 72% (or 280k entries), while
    Media.HLS.PlaybackError is only up 10k entries, with most of them being
    network errors. This is to be expected, since those network errors are
    already the most common issue, and is more related to the CORS situation
    that a lot of HLS streams find themselves commonly facing (ie,
    less-than-sanctioned streams).
    
    Final PipelineStatus for both AudioOnly and AudioVideo remains similar,
    but there is some overall movement from PIPELINE_ERROR_COULD_NOT_PARSE
    to PIPELINE_ERROR_DECODE (which is again to be expected), while
    PIPELINE_OK remains unchanged.
    
    Finally, watch time seems to have not moved all that much, but
    potentially shows some shift from Audio-only to Audio-Video.
    
    There is no appreciable change in crashiness as a result of this
    experiment.
    
    Fixed: 314837904
    Bug: 40057824
    Change-Id: I2f4a74a1155342b64420fa6deeccd9f3a35a7c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6484972
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450860}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -1120,9 +1120,7 @@ BASE_FEATURE(kBuiltInHlsPlayer,
 #endif
 );
 
-BASE_FEATURE(kBuiltInHlsMP4,
-             "BuiltInHlsMP4",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kBuiltInHlsMP4, "BuiltInHlsMP4", base::FEATURE_ENABLED_BY_DEFAULT);
 
 #endif  // BUILDFLAG(ENABLE_HLS_DEMUXER)
 
