5c6908805c9dc96108ed0fcefb0fa15cb43be4a7
Lukasz Anforowicz <lukasza@chromium.org>
2025-04-23 06:45:11 -0700

    Roll clap: 4.5.35 => 4.5.36 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * clap: 4.5.35 => 4.5.36
    * clap_builder: 4.5.35 => 4.5.36
    
    Bug: None
    Change-Id: I69e4ae7ec93c12f0c27befe278a170b7e6951160
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6456127
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Jonathan Hao <phao@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450519}

--- third_party/rust/chromium_crates_io/vendor/clap_builder-v4/src/builder/debug_asserts.rs
+++ third_party/rust/chromium_crates_io/vendor/clap_builder-v4/src/builder/debug_asserts.rs
@@ -632,7 +632,6 @@ fn _verify_positionals(cmd: &Command) -> bool {
                     continue;
                 }
                 found = true;
-                continue;
             } else {
                 found = false;
             }
@@ -657,7 +656,6 @@ fn _verify_positionals(cmd: &Command) -> bool {
                 //      $ prog r1 -- r2
                 //      $ prog r1 o1 -- r2
                 found = true;
-                continue;
             }
         }
     }
@@ -734,7 +732,7 @@ fn assert_arg(arg: &Arg) {
         );
         assert!(
             arg.is_takes_value_set(),
-            "Argument '{}` is positional and it must take a value but action is {:?}{}",
+            "Argument '{}' is positional and it must take a value but action is {:?}{}",
             arg.get_id(),
             arg.get_action(),
             if arg.get_id() == Id::HELP {
