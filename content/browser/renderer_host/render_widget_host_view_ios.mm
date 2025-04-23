2147205dba5d1e82de72e38285d99aaae6899654
Gyuyoung Kim <gyuyoung@igalia.com>
2025-04-23 01:04:47 -0700

    [tvOS] Add dummy RenderWidgetUIView
    RenderWidgetUIView on iOS uses the BrowserEngineKit library, which is
    not supported on Apple tvOS. Therefore, this CL introduces dummy
    implementations of RenderWidgetUIView for the tvOS port.
    
    Bug: 391914246, 411452047
    Change-Id: I313cdc516c90bb3cc44b0eb57e6605a3e84aa4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6466808
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1450419}

--- content/browser/renderer_host/render_widget_host_view_ios.mm
+++ content/browser/renderer_host/render_widget_host_view_ios.mm
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/input/synthetic_gesture_target_ios.h"
 #include "content/browser/renderer_host/render_view_host_delegate_view.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
-#include "content/browser/renderer_host/render_widget_host_view_ios_uiview.h"
 #include "content/browser/renderer_host/text_input_manager.h"
 #include "content/common/content_switches_internal.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -34,6 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/gesture_detection/gesture_provider_config_helper.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/native_widget_types.h"
+
+#if BUILDFLAG(IS_IOS_TVOS)
+#include "content/browser/renderer_host/render_widget_host_view_tvos_uiview.h"
+#else
+#include "content/browser/renderer_host/render_widget_host_view_ios_uiview.h"
+#endif
 
 @interface UIApplication (Testing)
 - (BOOL)isRunningTests;
@@ -754,6 +759,7 @@ void RenderWidgetHostViewIOS::OnUpdateTextInputStateCalled(
     TextInputManager* text_input_manager,
     RenderWidgetHostViewBase* updated_view,
     bool did_update_state) {
+#if !BUILDFLAG(IS_IOS_TVOS)
   if (text_input_manager->GetActiveWidget()) {
     [ui_view_->view_
         onUpdateTextInputState:*text_input_manager->GetTextInputState()
@@ -764,11 +770,13 @@ void RenderWidgetHostViewIOS::OnUpdateTextInputStateCalled(
     [ui_view_->view_ onUpdateTextInputState:ui::mojom::TextInputState()
                                  withBounds:[ui_view_->view_ bounds]];
   }
+#endif
 }
 
 void RenderWidgetHostViewIOS::OnTextSelectionChanged(
     TextInputManager* text_input_manager,
     RenderWidgetHostViewBase* updated_view) {
+#if !BUILDFLAG(IS_IOS_TVOS)
   DCHECK_EQ(GetTextInputManager(), text_input_manager);
   const TextInputManager::TextSelection* selection =
       text_input_manager->GetTextSelection(updated_view);
@@ -799,12 +807,16 @@ void RenderWidgetHostViewIOS::OnTextSelectionChanged(
     [[ui_view_->view_ textInteraction] textSelectionDisplayInteraction]
         .activated = NO;
   }
+#endif
 }
+
 void RenderWidgetHostViewIOS::OnSelectionBoundsChanged(
     TextInputManager* text_input_manager,
     RenderWidgetHostViewBase* updated_view) {
+#if !BUILDFLAG(IS_IOS_TVOS)
   [[ui_view_->view_ textInteraction]
           .textSelectionDisplayInteraction setNeedsSelectionUpdate];
+#endif
 }
 
 ui::Compositor* RenderWidgetHostViewIOS::GetCompositor() {
