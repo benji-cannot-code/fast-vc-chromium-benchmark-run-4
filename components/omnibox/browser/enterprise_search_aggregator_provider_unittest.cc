d70ca903d51bf14456d0d0207842914837f1ed9c
Tomasz Moniuszko <tmoniuszko@opera.com>
2025-04-24 07:53:13 -0700

    Force America/Los_Angeles timezone in test
    This fixes EnterpriseSearchAggregatorProviderTest.Parse failures in
    other timezones.
    
    Fixed: 413285135
    Change-Id: Ifd6580ef7b0e4c593b727e43d8bf38eccd139cec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6487729
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tomasz Moniuszko <tmoniuszko@opera.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Tomasz Moniuszko <tmoniuszko@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1451152}

--- components/omnibox/browser/enterprise_search_aggregator_provider_unittest.cc
+++ components/omnibox/browser/enterprise_search_aggregator_provider_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/icu_test_util.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
@@ -717,6 +718,8 @@ TEST_F(EnterpriseSearchAggregatorProviderTest,
 
 // Test response is parsed accurately.
 TEST_F(EnterpriseSearchAggregatorProviderTest, Parse) {
+  base::test::ScopedRestoreDefaultTimezone la_time("America/Los_Angeles");
+
   scoped_config_.Get().relevance_scoring_mode = "server";
 
   provider_->adjusted_input_ = CreateInput(u"john d", true);
