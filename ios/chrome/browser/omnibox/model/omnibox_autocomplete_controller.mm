f14b88e4c0ebf0a4f08be1a4545ac94e6adef341
Christian Xu <christianxu@google.com>
2025-04-24 07:33:33 -0700

    [iOS][OR] Remove hideKeyboard from OmniboxViewIOS
    Move hideKeyboard logic to OmniboxTextController.
    
    Bug: 409730560
    Change-Id: I91ef51b26dd39a3bd3a9630081b830edfaa623d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6480079
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451137}

--- ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
+++ ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
@@ -230,9 +230,7 @@ using base::UserMetricsAction;
 }
 
 - (void)onCallAction {
-  if (_omniboxViewIOS) {
-    _omniboxViewIOS->OnCallActionTap();
-  }
+  [self.omniboxTextController hideKeyboard];
 }
 
 #pragma mark - OmniboxText events
--- ios/chrome/browser/omnibox/model/omnibox_text_controller.mm
+++ ios/chrome/browser/omnibox/model/omnibox_text_controller.mm
@@ -89,12 +89,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)endEditing {
-  // This check is a tentative fix for a crash that happens when calling
-  // `resignFirstResponder`. TODO(crbug.com/375429786): Verify the crash rate
-  // and remove the comment or check if needed.
-  if (self.textField.window) {
-    [self.textField resignFirstResponder];
-  }
+  [self hideKeyboard];
+
   if (!_omniboxEditModel || !_omniboxEditModel->has_focus()) {
     return;
   }
@@ -391,9 +387,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)onScroll {
-  /// Hides the keyboard.
-  [self.textField resignFirstResponder];
+  [self hideKeyboard];
   _suggestionsListScrolled = YES;
+}
+
+- (void)hideKeyboard {
+  // This check is a tentative fix for a crash that happens when calling
+  // `resignFirstResponder`. TODO(crbug.com/375429786): Verify the crash rate
+  // and remove the comment or check if needed.
+  if (self.textField.window) {
+    [self.textField resignFirstResponder];
+  }
 }
 
 #pragma mark - Private
