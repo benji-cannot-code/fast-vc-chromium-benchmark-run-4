27b6c47e5b5c7f4a021be3e4db9f8d2d71896ceb
Dana Fried <dfried@chromium.org>
2025-04-24 07:37:42 -0700

    Revert "Add fieldtrial test config for AutofillI18nNLNameModel"
    This reverts commit 95de48a39a4ea703bbbb2765efb982c38b3ec1b3.
    
    Reason for revert: [Gardener] Tests failing on multiple builders; see b/413332923
    
    Bug: 386916943, 413332923
    Original change's description:
    > Add fieldtrial test config for AutofillI18nNLNameModel
    >
    > This CL adds a fieldtrial test config for the NL last name prefix
    > feature. Additionally it adds small test modifications to ensure the
    > proper flag initialization.
    >
    > Bug: 386916943
    > Change-Id: Iaa26ff926e6cd88d317132ab83ca3ae94ef5017d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281288
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Reviewed-by: Norge Vizcay <vizcay@google.com>
    > Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    > Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1451081}
    
    Bug: 386916943
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I3b8c26bcc8973fd717d504890faa0372e6b9a228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488308
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Owners-Override: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451139}

--- chrome/browser/ui/autofill/edit_address_profile_dialog_controller_impl_interactive_uitest.cc
+++ chrome/browser/ui/autofill/edit_address_profile_dialog_controller_impl_interactive_uitest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "components/autofill/core/browser/data_model/addresses/autofill_profile.h"
 #include "components/autofill/core/browser/test_utils/autofill_test_utils.h"
-#include "components/autofill/core/common/autofill_features.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "ui/views/window/dialog_client_view.h"
@@ -25,8 +24,6 @@ class EditAddressProfileDialogControllerImplTest
     : public InteractiveBrowserTest {
  protected:
   EditAddressProfileDialogControllerImplTest() {
-    base::test::ScopedFeatureList feature_list_{
-        features::kAutofillSupportLastNamePrefix};
     local_profile_ = std::make_unique<AutofillProfile>(
         AutofillProfile::RecordType::kLocalOrSyncable,
         AddressCountryCode("US"));
