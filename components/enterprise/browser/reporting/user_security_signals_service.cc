e41d44194ea5c6983010e527b7b8331c132ea2bb
Zonghan Xu <xzonghan@google.com>
2025-04-24 08:14:26 -0700

    [Signals Reporting] Add feature params for profile signals reporting
    Add new feature params to allow more control over profile signals reporting include:
    - whether trigger new reports on cookie changes
    - customize report interval to avoid spamming
    
    Bug: 404577630
    Change-Id: I8baa9c2631522a33eb3172be0d6ebabca8311ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6483090
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451164}

--- components/enterprise/browser/reporting/user_security_signals_service.cc
+++ components/enterprise/browser/reporting/user_security_signals_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/browser/reporting/user_security_signals_service.h"
 
 #include "base/check.h"
+#include "components/device_signals/core/common/signals_features.h"
 #include "components/enterprise/browser/reporting/common_pref_names.h"
 #include "components/enterprise/browser/reporting/report_scheduler.h"
 #include "components/enterprise/browser/reporting/report_util.h"
@@ -13,12 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_constants.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "net/cookies/cookie_change_dispatcher.h"
-
-namespace {
-
-constexpr base::TimeDelta kSecurityUploadsInterval = base::Hours(4);
-
-}
 
 namespace enterprise_reporting {
 
@@ -33,7 +28,7 @@ UserSecuritySignalsService::UserSecuritySignalsService(
 UserSecuritySignalsService::~UserSecuritySignalsService() = default;
 
 base::TimeDelta UserSecuritySignalsService::GetSecurityUploadCadence() {
-  return kSecurityUploadsInterval;
+  return enterprise_signals::features::kProfileSignalsReportingInterval.Get();
 }
 
 void UserSecuritySignalsService::Start() {
@@ -77,6 +72,9 @@ void UserSecuritySignalsService::OnReportUploaded() {
 
 void UserSecuritySignalsService::OnCookieChange(
     const net::CookieChangeInfo& change) {
+  if (!enterprise_signals::features::kTriggerOnCookieChange.Get()) {
+    return;
+  }
   // Only trigger a new upload if the cookie change could result in a new or
   // updated session.
   if (change.cause == net::CookieChangeCause::INSERTED) {
