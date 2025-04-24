2d697133390566ed3d3526f3189ffc9aace10a85
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-04-24 00:09:42 -0700

    Introduce extended_lifetime to SharedWorkerHost
    Let me make SharedWorkerHost to remember if the SharedWorker instance
    need to have an extended lifetime on its destruction.  Plumbing from a
    JavaScript to this struct, and extending the lifetime upon the field set
    will be implemented in coming CLs.
    
    Bug: 400473072
    Change-Id: I8434a791ea38e897c15d94c815d17e11467d0c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6482827
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450972}

--- content/browser/worker_host/shared_worker_host_unittest.cc
+++ content/browser/worker_host/shared_worker_host_unittest.cc
@@ -92,7 +92,8 @@ class SharedWorkerHostTest : public testing::Test {
     auto host = std::make_unique<SharedWorkerHost>(
         &service_, instance, site_instance_,
         std::vector<network::mojom::ContentSecurityPolicyPtr>(),
-        base::MakeRefCounted<PolicyContainerHost>());
+        base::MakeRefCounted<PolicyContainerHost>(),
+        /*extended_lifetime=*/false);
     auto weak_host = host->AsWeakPtr();
     service_.worker_hosts_.insert(std::move(host));
     return weak_host;
@@ -384,7 +385,8 @@ TEST_F(SharedWorkerHostTest,
   auto host = std::make_unique<SharedWorkerHost>(
       &service_, instance, site_instance_,
       std::vector<network::mojom::ContentSecurityPolicyPtr>(),
-      base::MakeRefCounted<PolicyContainerHost>());
+      base::MakeRefCounted<PolicyContainerHost>(),
+      /*extended_lifetime=*/false);
 
   // Start the worker.
   mojo::PendingRemote<blink::mojom::SharedWorkerFactory> factory;
@@ -441,7 +443,8 @@ TEST_F(SharedWorkerHostTestWithPNAEnabled,
   auto host = std::make_unique<SharedWorkerHost>(
       &service_, instance, site_instance_,
       std::vector<network::mojom::ContentSecurityPolicyPtr>(),
-      base::MakeRefCounted<PolicyContainerHost>(std::move(policies)));
+      base::MakeRefCounted<PolicyContainerHost>(std::move(policies)),
+      /*extended_lifetime=*/false);
 
   // Start the worker.
   mojo::PendingRemote<blink::mojom::SharedWorkerFactory> factory;
