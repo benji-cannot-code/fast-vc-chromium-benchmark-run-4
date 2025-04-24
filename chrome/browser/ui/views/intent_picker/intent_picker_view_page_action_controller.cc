96d8077cd899c61e7a1f433ff5f7143787722927
Lei Zhang <thestig@chromium.org>
2025-04-24 09:03:57 -0700

    Do IWYU in chrome/browser/ui/views/intent_picker
    Add a missing header that are being transitively included. The
    transitive include is going away soon. Then update build dependency
    rules to match.
    
    Change-Id: Iafffc943f713a264a4a0ce98a5872512f4f25b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6485534
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451194}

--- chrome/browser/ui/views/intent_picker/intent_picker_view_page_action_controller.cc
+++ chrome/browser/ui/views/intent_picker/intent_picker_view_page_action_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/ui_features.h"
