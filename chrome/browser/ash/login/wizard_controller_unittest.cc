1cc80b51b9da8740cd21464835ee602a3c02a70f
Hidehiko Abe <hidehiko@chromium.org>
2025-04-24 09:19:55 -0700

    Fix WizardControllerTest for branded builds.
    Between branded build and non-branded build
    AutoEnrollmentTypeChecker::IsUnifiedStateDeterminationEnabled() returns
    different value, so the behavior is changed.
    Specifically, for WizardControllerTest, branded build fails to run.
    This CL forces to return the value (false) to be aligned with the
    non-branded build always to stabilize the test.
    
    BUG=411620255
    TEST=Tryjob
    
    Change-Id: I62a2d380d1d8479c78c8977d79d39da1d5023621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6486609
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451209}

--- chrome/browser/ash/login/wizard_controller_unittest.cc
+++ chrome/browser/ash/login/wizard_controller_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "ash/constants/ash_switches.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_helper.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/net/network_portal_detector_test_impl.h"
 #include "chrome/browser/ash/net/rollback_network_config/fake_rollback_network_config.h"
 #include "chrome/browser/ash/net/rollback_network_config/rollback_network_config_service.h"
+#include "chrome/browser/ash/policy/enrollment/auto_enrollment_type_checker.h"
 #include "chrome/browser/ash/profiles/signin_profile_handler.h"
 #include "chrome/browser/ash/settings/device_settings_cache.h"
 #include "chrome/browser/ash/settings/device_settings_test_helper.h"
@@ -178,7 +180,12 @@ void CreateExtensionServiceFor(Profile* profile) {
 // be done to run unit tests, but is not directly related to the tests.
 class WizardControllerTestBase : public ::testing::Test {
  public:
-  WizardControllerTestBase() = default;
+  WizardControllerTestBase() {
+    // Stabilizes the behavior on branded build.
+    command_line_.GetProcessCommandLine()->AppendSwitchASCII(
+        ash::switches::kEnterpriseEnableUnifiedStateDetermination,
+        policy::AutoEnrollmentTypeChecker::kUnifiedStateDeterminationNever);
+  }
 
   void SetUp() override {
     profile_manager_ = std::make_unique<TestingProfileManager>(
@@ -264,6 +271,8 @@ class WizardControllerTestBase : public ::testing::Test {
   testing::NiceMock<MockEnrollmentLauncher> mock_enrollment_launcher_;
 
  private:
+  base::test::ScopedCommandLine command_line_;
+
   std::unique_ptr<base::test::TaskEnvironment> task_environment_ =
       std::make_unique<content::BrowserTaskEnvironment>(
           base::test::TaskEnvironment::ThreadingMode::MULTIPLE_THREADS,
