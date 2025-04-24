f2ce0c17e26657dedaaa0f376f310850822bd000
Nick Birnie <birnie@google.com>
2025-04-24 14:58:22 -0700

    Replace CEP DLP Paste control WebModal with GlicModal
    This CL introduces a GlicModalView to show a toast-like popup with a configurable label and close button. The first use is to override the unsupported DLP web modal which breaks glic when attempting to show.
    
    Bug: 410611367
    Change-Id: I37b884684494435c105d80e6cebd2fe5208f465c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6481428
    Commit-Queue: Nick Birnie <birnie@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451483}

--- chrome/browser/glic/widget/glic_window_controller.cc
+++ chrome/browser/glic/widget/glic_window_controller.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/webui_contents_container.h"
 #include "chrome/browser/glic/resources/grit/glic_browser_resources.h"
 #include "chrome/browser/glic/widget/browser_conditions.h"
+#include "chrome/browser/glic/widget/glic_modal_manager.h"
+#include "chrome/browser/glic/widget/glic_modal_view.h"
 #include "chrome/browser/glic/widget/glic_view.h"
 #include "chrome/browser/glic/widget/glic_widget.h"
 #include "chrome/browser/glic/widget/glic_window_animator.h"
@@ -295,6 +297,7 @@ GlicWindowController::GlicWindowController(
       fre_controller_(
           std::make_unique<GlicFreController>(profile, identity_manager)),
       window_finder_(std::make_unique<WindowFinder>()),
+      glic_modal_manager_(std::make_unique<GlicModalManager>()),
       glic_service_(glic_service),
       enabling_(enabling) {}
 
@@ -728,6 +731,10 @@ void GlicWindowController::Show(Browser* browser,
 // static
 bool GlicWindowController::AlwaysDetached() {
   return base::FeatureList::IsEnabled(features::kGlicDetached);
+}
+
+void GlicWindowController::ShowGlicModal(std::u16string label) {
+  glic_modal_manager_->ShowModal(std::move(label), glic_widget_.get());
 }
 
 void GlicWindowController::SetupGlicWidget(Browser* browser) {
--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -202,6 +202,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorIntentPickerItemBackgroundSelected) \
   /* Glic colors */ \
   E_CPONLY(kColorGlicBackground) \
+  E_CPONLY(kColorGlicModalBackground) \
+  E_CPONLY(kColorGlicModalForeground) \
   /* Hover Button colors */ \
   E_CPONLY(kColorHoverButtonBackgroundHovered) \
   /* Lens overlay colors. */ \
