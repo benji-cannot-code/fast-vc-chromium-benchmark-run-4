b595e15a5a3df470f204369948777fbb85f6f880
Elly <ellyjones@chromium.org>
2025-04-24 10:14:19 -0700

    hunspell: remove old base::numerics references
    These aliases for base functions are deprecated and about to be removed.
    The new ones are shorter :)
    
    Bug: 409350835
    Change-Id: I5afe161b8f9d99caa5d851fa386d9708626f6d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489246
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451250}

--- third_party/hunspell/google/bdict_reader.cc
+++ third_party/hunspell/google/bdict_reader.cc
@@ -385,10 +385,10 @@ NodeReader::FindResult NodeReader::ReaderForLookup0th(
     NodeReader* result) const {
   size_t child_offset;
   if (is_lookup_32()) {
-    child_offset = base::numerics::U32FromLittleEndian(
+    child_offset = base::U32FromLittleEndian(
         bdict_data_.subspan(zeroth_entry_offset()).first<4>());
   } else {
-    child_offset = base::numerics::U16FromLittleEndian(
+    child_offset = base::U16FromLittleEndian(
         bdict_data_.subspan(zeroth_entry_offset()).first<2>());
     child_offset += node_offset_;
   }
@@ -417,7 +417,7 @@ NodeReader::FindResult NodeReader::ReaderForLookupAt(
   size_t child_offset = 0;
   if (is_lookup_32()) {
     // Table contains 32-bit absolute offsets.
-    child_offset = base::numerics::U32FromLittleEndian(
+    child_offset = base::U32FromLittleEndian(
         bdict_data_.subspan(table_offset + index * sizeof(uint32_t))
             .first<4>());
     if (!child_offset) {
@@ -425,7 +425,7 @@ NodeReader::FindResult NodeReader::ReaderForLookupAt(
     }
   } else {
     // Table contains 16-bit offsets relative to the current node.
-    child_offset = base::numerics::U16FromLittleEndian(
+    child_offset = base::U16FromLittleEndian(
         bdict_data_.subspan(table_offset + index * sizeof(uint16_t))
             .first<2>());
     if (!child_offset) {
@@ -508,7 +508,7 @@ NodeReader::FindResult NodeReader::ReaderForListAt(
     // The children begin right after the list.
     size_t children_begin = list_begin + list_item_count() * 3;
     offset = children_begin +
-             base::numerics::U16FromLittleEndian(list_item.subspan<1>());
+             base::U16FromLittleEndian(list_item.subspan<1>());
   } else {
     auto list_item = bdict_data_.subspan(list_begin + index * 2).first<2>();
     *found_char = list_item[0];
