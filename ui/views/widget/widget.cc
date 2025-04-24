6fc0a6e637587b37b49fae9b87137045a65f48c9
Keren Zhu <kerenzhu@chromium.org>
2025-04-24 11:49:20 -0700

    views: Add widget observer method for on-screen visibility change
    This CL adds OnWidgetVisibilityOnScreenChanged() to WidgetObserver. This
    event will fire whenever Widget::IsVisibleOnScreen() changes. Currently
    only implemented on macOS. Windows is TBD.
    
    Bug: 410938804
    Change-Id: Ie5f913e54632d78223be5903ce3b75f8e74ca80c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6486149
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451343}

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -1892,6 +1892,11 @@ void Widget::OnNativeWidgetVisibilityChanged(bool visible) {
   MaybeNotifyWindowModalVisibilityChanged(visible);
 }
 
+void Widget::OnNativeWidgetVisibilityOnScreenChanged(bool visible) {
+  observers_.Notify(&WidgetObserver::OnWidgetVisibilityOnScreenChanged, this,
+                    visible);
+}
+
 void Widget::OnNativeWidgetCreated() {
   if (is_top_level()) {
     focus_manager_ = FocusManagerFactory::Create(this);
