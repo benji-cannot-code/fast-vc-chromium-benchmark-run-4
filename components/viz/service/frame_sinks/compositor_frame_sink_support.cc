feb958416d20468e32de3ebe63d7396f07a3adf3
Li Chen <li1.chen@intel.com>
2025-04-24 00:24:33 -0700

    Fix regression in ShouldSendBeginFrame
    Regression introduced in [1].
    The issue is that sending begin frame to throttled client when
    there are pending timing details can cause regression.
    Non-throttled clients can still receive those begin frames.
    This CL updates the condition.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5969048
    
    Bug: 405454652
    Change-Id: I11b1a7358c4f4387baed88f97aae6168265383b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6385203
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Jiahe Zhang <jiahe.zhang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1450979}

--- components/viz/service/frame_sinks/compositor_frame_sink_support.cc
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.cc
@@ -1475,8 +1475,7 @@ bool CompositorFrameSinkSupport::ShouldSendBeginFrame(
     return RecordShouldSendBeginFrame("SendFrameTiming", true);
   }
 
-  if (!client_needs_begin_frame_ && !layer_context_wants_begin_frames_ &&
-      frame_timing_details_.empty()) {
+  if (!client_needs_begin_frame_ && !layer_context_wants_begin_frames_) {
     return RecordShouldSendBeginFrame("StopNotRequested", false);
   }
 
