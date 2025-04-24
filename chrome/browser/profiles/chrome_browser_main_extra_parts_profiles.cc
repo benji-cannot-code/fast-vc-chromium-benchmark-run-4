e3d01fc04a05ee2eb13f70f06cb88a2589c82ea0
David Pennington <dpenning@chromium.org>
2025-04-24 13:14:53 -0700

    Fix CollaborationMessagingObserverFactory to not require feature flag
    Some tests are failing with an inexplicable reason, features flags have
    different values when extra_parts is called vs when initialization
    of the service happens. While its not clear what causes this, in linux
    debug builds this issue causes failures. To prevent this we move the
    checks for the feature flag into the service, and create the factory
    regardless of the flag, then the observer is just not init if the flags
    are off for that particular profile.
    
    Fixed: 413320516
    Change-Id: If07d84086c1e8c6456dbda8451ba5ba2e96d91a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489346
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451374}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -796,6 +796,10 @@ void ChromeBrowserMainExtraPartsProfiles::
 #endif
   collaboration::CollaborationServiceFactory::GetInstance();
   collaboration::messaging::MessagingBackendServiceFactory::GetInstance();
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || \
+    BUILDFLAG(IS_WIN)
+  tab_groups::CollaborationMessagingObserverFactory::GetInstance();
+#endif
   commerce::ShoppingServiceFactory::GetInstance();
   ConsentAuditorFactory::GetInstance();
   ContentIndexProviderFactory::GetInstance();
@@ -1388,12 +1392,6 @@ void ChromeBrowserMainExtraPartsProfiles::
 #endif
   WebDataServiceFactory::GetInstance();
   webrtc_event_logging::WebRtcEventLogManagerKeyedServiceFactory::GetInstance();
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || \
-    BUILDFLAG(IS_WIN)
-  if (tab_groups::SavedTabGroupUtils::SupportsSharedTabGroups()) {
-    tab_groups::CollaborationMessagingObserverFactory::GetInstance();
-  }
-#endif
 }
 
 void ChromeBrowserMainExtraPartsProfiles::PreProfileInit() {
--- chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_observer_factory.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_observer_factory.cc
@@ -43,8 +43,6 @@ CollaborationMessagingObserverFactory::
 std::unique_ptr<KeyedService>
 CollaborationMessagingObserverFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  CHECK(tab_groups::SavedTabGroupUtils::SupportsSharedTabGroups());
-  DCHECK(context);
   Profile* profile = Profile::FromBrowserContext(context);
   return std::make_unique<CollaborationMessagingObserver>(profile);
 }
