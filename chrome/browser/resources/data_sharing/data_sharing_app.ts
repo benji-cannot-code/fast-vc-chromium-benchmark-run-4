9f5547f1d2423548588b67d1fe5da6af2337f96c
Yuheng Huang <yuhengh@chromium.org>
2025-04-24 11:00:55 -0700

    Data sharing: add title for share, manage and join flow
    The title is not visible but announced in some screen readers, e.g.
    JAWS on Windows, for accessibility.
    
    Bug: 410855476
    Change-Id: Ife0a202e6869b470b3f23c68b4e413e1bd1ee941
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6484973
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451303}

--- chrome/browser/resources/data_sharing/data_sharing_app.ts
+++ chrome/browser/resources/data_sharing/data_sharing_app.ts
@@ -480,6 +480,20 @@ export class DataSharingApp extends CustomElement implements Logger {
 
     switch (flow) {
       case FlowValues.SHARE:
+        document.title =
+            loadTimeData.getStringF('shareGroupTitle', getTabGroupName());
+        break;
+      case FlowValues.MANAGE:
+        document.title =
+            loadTimeData.getStringF('manageGroupTitle', getTabGroupName());
+        break;
+      case FlowValues.JOIN:
+        document.title = loadTimeData.getStringF('previewA11yName');
+        break;
+    }
+
+    switch (flow) {
+      case FlowValues.SHARE:
         this.dataSharingSdk_
             .runInviteFlow({
               parent,
