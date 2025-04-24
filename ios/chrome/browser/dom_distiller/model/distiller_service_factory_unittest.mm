c828863c97008f0a52a71fd23e9e1fa04eae95f4
Louis Romero <lpromero@google.com>
2025-04-24 09:18:46 -0700

    Fix DistillerServiceFactoryTest test
    This new test got created right before crrev.com/c/6465636 got
    submitted.
    
    Bug: 409494559
    Change-Id: Ie963fc958978b088d86a9c80cd0e0586a283d820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6487732
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451207}

--- ios/chrome/browser/dom_distiller/model/distiller_service_factory_unittest.mm
+++ ios/chrome/browser/dom_distiller/model/distiller_service_factory_unittest.mm
@@ -5,15 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/dom_distiller/model/distiller_service_factory.h"
 
-#import "base/test/task_environment.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
 
 class DistillerServiceFactoryTest : public PlatformTest {
  protected:
   DistillerServiceFactoryTest() : profile_(TestProfileIOS::Builder().Build()) {}
 
-  base::test::TaskEnvironment task_environment_;
+  web::WebTaskEnvironment task_environment_;
   std::unique_ptr<TestProfileIOS> profile_;
 };
 
