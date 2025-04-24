52c238909d3d3eb5a6f58cc5619a53f381b7162a
Pier-Alexandre Bouchard <pabouchard@google.com>
2025-04-24 07:52:00 -0700

    [iOS] Improve accessibility color contrast for Sign-in button and Home customization menu
    Updated the sign-in button and the Home customization menu to meet
    accessibility color contrast requirements as per specs:
    
    https://docs.google.com/presentation/d/1lRln5BugIlsVbhjCybGrGjUuARTRK0kdS-sOGJ_y_a8/edit?slide=id.g35228ea0d01_0_11#slide=id.g35228ea0d01_0_11
    
    Icon and text color: #1A73E8
    
    Font: dynamic type (UIFontTextStyleBody, ~17pt)
    
    Background color: #1A73E8 at 8% opacity
    
    These changes ensure better visibility and compliance with accessibility
    standards.
    
    Old experience: https://screenshot.googleplex.com/5TkxV2zNJhr2cnV New
    experience: https://screenshot.googleplex.com/54nPnmwBvrycK6G
    
    Bug: 410599738
    Change-Id: Ie4cec7bcd259090a10199f01026c7effa348c0ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470363
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451151}

--- ios/chrome/browser/content_suggestions/ui_bundled/ntp_home_constant.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/ntp_home_constant.mm
@@ -29,6 +29,7 @@ const CGFloat kHeaderIconMargin = 8;
 const CGFloat kIdentityAvatarPadding = 8;
 const CGFloat kSignedOutIdentityIconSize = 24;
 const CGFloat kCustomizationMenuIconSize = 16;
+const CGFloat kCustomizationMenuIconSizeWhenSignInButtonHasNoAvatar = 17;
 const CGFloat kCustomizationMenuButtonDimension = 37;
 const CGFloat kCustomizationMenuButtonCornerRadius = 11;
 
