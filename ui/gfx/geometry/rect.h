d90ea54d270530e90e6217aa5c9f5065c36c06a3
Mitsuru Oshima <oshima@chromium.org>
2025-04-23 17:29:44 -0700

    Do not update the window bounds in SplitViewDivider.
    * The logic to confine transient the window within a snapped window's
     bounds is no longernecessary as the dividier widget is now in the same
     container.
    * This can make UI difficult to use as the dialog become very narrow,
      or places at inconvenient place.
    * The fix the center window logic so that it tries to center to the
      transient parent when it's not bigger than parent in both direction.
      Because the dialog will be place at the center of the parent if it
      fits to its size, this is more natural placement, unless the window
      can hide the parent.
    
    Note: The sharesheet dialog is fixed in
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6477147
    
    Bug: 406855448, 409493343
    Test: Removed unnecessrary check. I'll add more test when bubble code
         is refactored.
    
    Change-Id: I4651e1434ef695fb883e35b6acf3d046440f4ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6466709
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450893}

--- ui/gfx/geometry/rect.h
+++ ui/gfx/geometry/rect.h
@@ -231,6 +231,9 @@ class COMPONENT_EXPORT(GEOMETRY) Rect {
   // Returns the center of this rectangle.
   Point CenterPoint() const;
 
+  // Becomes a rectangle that has the same center point but with a |size|.
+  void ToCenteredSize(const Size& size);
+
   // Becomes a rectangle that has the same center point but with a size capped
   // at given |size|.
   void ClampToCenteredSize(const Size& size);
