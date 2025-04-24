35c627eb72aef23144e753704b00e1f6db4c658b
Zonghan Xu <xzonghan@google.com>
2025-04-24 09:34:47 -0700

    [Signals Reporting] Better naming for profile signals report types
    Make profile signals report types naming more reflective & accurate.
    
    Change-Id: I50fa919cb4e5e74e6b44ed6e11da62cc6780579f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488788
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451220}

--- components/enterprise/browser/reporting/chrome_profile_request_generator_unittest.cc
+++ components/enterprise/browser/reporting/chrome_profile_request_generator_unittest.cc
@@ -57,7 +57,8 @@ void VerifyReportContent(
   // True if a status report-exclusive field is expected to be filled correctly,
   // status reports with signals also count.
   bool expect_status_report_only_value =
-      expected_report_type != em::ChromeProfileReportRequest::PROFILE_SIGNALS;
+      expected_report_type !=
+      em::ChromeProfileReportRequest::PROFILE_SECURITY_SIGNALS;
   bool expect_signals_override_value =
       expected_report_type != em::ChromeProfileReportRequest::PROFILE_REPORT;
 
@@ -274,7 +275,7 @@ TEST_F(ChromeProfileRequestGeneratorTest,
 
   VerifyReportContent(
       test_future.Get(),
-      em::ChromeProfileReportRequest::PROFILE_REPORT_WITH_SIGNALS);
+      em::ChromeProfileReportRequest::PROFILE_REPORT_WITH_SECURITY_SIGNALS);
 }
 
 TEST_F(ChromeProfileRequestGeneratorTest, GenerateSecuritySignalsOnlyReport) {
@@ -291,7 +292,7 @@ TEST_F(ChromeProfileRequestGeneratorTest, GenerateSecuritySignalsOnlyReport) {
                                              /*use_cookies=*/false),
                       test_future.GetCallback());
   VerifyReportContent(test_future.Get(),
-                      em::ChromeProfileReportRequest::PROFILE_SIGNALS);
+                      em::ChromeProfileReportRequest::PROFILE_SECURITY_SIGNALS);
 }
 
 TEST_F(ChromeProfileRequestGeneratorTest, IncorrectReportType) {
