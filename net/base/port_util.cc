0618b6ee319c02351dc47bb4b41b7da08c91f0d7
Adam Rice <ricea@chromium.org>
2025-04-24 01:02:47 -0700

    Improve performance of RestrictAbusePorts
    Switch the implementation of RestrictAbusePorts from a std::set to a
    base::flat_set to reduce memory usage and improve data locality.
    
    Use the base::flat_set::contains() method for lookup.
    
    Skip invalid port numbers instead of crashing to reduce the damage of a
    bad config push.
    
    BUG=b:409713509
    R=bashi@chromium.org
    
    Change-Id: Ieed1851b61caa2a678b92fa3146351dc6193c454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6486508
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450987}

--- net/base/port_util.cc
+++ net/base/port_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/containers/fixed_flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/feature_list.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
@@ -127,8 +128,10 @@ constexpr int kAllowablePorts[] = {};
 
 int g_scoped_allowable_port = 0;
 
-std::set<int>* g_restricted_abuse_ports() {
-  static base::NoDestructor<std::set<int>> restricted_abuse_ports;
+using PortSet = base::flat_set<int>;
+
+PortSet* g_restricted_abuse_ports() {
+  static base::NoDestructor<PortSet> restricted_abuse_ports;
   return restricted_abuse_ports.get();
 }
 
@@ -136,14 +139,20 @@ void InitializeRestrictedAbusePorts() {
   g_restricted_abuse_ports()->clear();
   if (base::FeatureList::IsEnabled(features::kRestrictAbusePorts)) {
     const std::string ports_string = features::kPortsToRestrictForAbuse.Get();
+    PortSet::container_type ports;
     for (const auto& port_string :
          base::SplitStringPiece(ports_string, ",", base::TRIM_WHITESPACE,
                                 base::SPLIT_WANT_NONEMPTY)) {
       int port;
-      CHECK(net::ParseInt32(port_string,
-                            net::ParseIntFormat::STRICT_NON_NEGATIVE, &port));
-      g_restricted_abuse_ports()->insert(port);
+      if (net::ParseInt32(port_string, net::ParseIntFormat::STRICT_NON_NEGATIVE,
+                          &port)) {
+        ports.push_back(port);
+      } else {
+        DLOG(ERROR) << "Ignoring invalid port for kPortsToRestrictForAbuse: "
+                    << port_string;
+      }
     }
+    *g_restricted_abuse_ports() = PortSet(std::move(ports));
   }
 }
 
@@ -178,10 +187,8 @@ bool IsPortAllowedForScheme(int port, std::string_view url_scheme) {
       InitializeRestrictedAbusePorts();
     }
 
-    for (int abused_port : *g_restricted_abuse_ports()) {
-      if (port == abused_port) {
-        return false;
-      }
+    if (g_restricted_abuse_ports()->contains(port)) {
+      return false;
     }
   }
 
