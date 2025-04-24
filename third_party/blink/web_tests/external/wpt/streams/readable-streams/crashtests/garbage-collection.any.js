0ec3e3ddb50b78ed1a090b7fa4df2d06c9d05eed
Andrey Kosyakov <caseq@chromium.org>
2025-04-24 15:36:36 -0700

    SuppressDetachCheck() of ReadableStreamGenericReason::closed promise resolver
    Bug: 335506658
    Change-Id: I704589fe0b4c7d14d8b702956d4beede868c75f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489940
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451500}

---
+++ third_party/blink/web_tests/external/wpt/streams/readable-streams/crashtests/garbage-collection.any.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/common/gc.js
+'use strict';
+
+// See https://crbug.com/335506658 for details.
+promise_test(async () => {
+    let closed = new ReadableStream({
+        pull(controller) {
+          controller.enqueue('is there anybody in there?');
+        }
+    }).getReader().closed;
+    // 3 GCs are actually required to trigger the bug at time of writing.
+    for (let i = 0; i < 5; ++i)
+      await garbageCollect();
+}, 'Garbage-collecting a stream along with its reader should not crash');
