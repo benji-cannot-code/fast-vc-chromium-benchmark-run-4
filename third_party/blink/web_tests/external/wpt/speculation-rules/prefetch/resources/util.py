c6a7a98ea7804ab1167ad667b79af75c4d6f3399
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-04-23 23:47:45 -0700

    Centralize custom executor to executor.sub.html
    This CL merges custom executors into newly-introduced
    `util.get_executor_html`, that derives the executors from
    `executor.sub.html`.
    
    To centralize `requestHeaders` creation to `executor.sub.html`,
    to dedup logic and to make the diff in
    https://chromium-review.googlesource.com/c/chromium/src/+/6363856
    smaller.
    
    Bug: 40947546, 406819293, 406819294
    Change-Id: I03b4ebd7b259830d21d8aa3aedea2da5179eb0bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6363376
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450966}

---
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/util.py
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import os
+from wptserve.pipes import template
+
+# Returns the executor HTML (as bytes).
+# `additional_script` (str) is inserted to the JavaScript before the executor.
+def get_executor_html(request, additional_script):
+  content = template(
+    request,
+    open(os.path.join(os.path.dirname(__file__), "executor.sub.html"), "rb").read())
+
+  # Insert an additional script at the head of script before Executor.
+  content = content.replace(
+      b'<script nonce="abc">',
+      b'<script nonce="abc">' + additional_script.encode('utf-8'))
+
+  return content
