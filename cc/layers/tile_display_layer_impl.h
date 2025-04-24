87e48f2cd60e174d1589ee443c051daea8d0c305
Vikas Soni <vikassoni@chromium.org>
2025-04-24 10:21:44 -0700

    [TreesInViz] Discard resource in TileDisplayLayerImpl::Tile::~Tile().
    Currently we discard tile resources in viz when we de-serialize tiles
    received from the Renderer and if the new tile update replaced an old
    tile at same location for the given tiling.
    
    Above resource discard solution works only for cases mentioned above and
    does not work when a tile is destroyed for some other reasons for eg: if
    whole tiling is being removed OR if a tile is not being replaced by an
    updated tile resource.
    
    This CL moves the tile resource discard logic into the
    TileDisplayLayerImpl::Tile::~Tile() which is more inline to cc:Tile
    behavior. In order to do that, TileDisplayLayerImpl::Tile also needs a
    reference to TileDisplayLayerImpl.
    
    Future CLs will remove tilings in viz when Renderer asks for it. This CL
    will is pre-requisite for it.
    
    Bug: 408053792, 403385455
    Change-Id: I4d2e2f937599a6259dde2b6188c5ccae14a01905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6482304
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451260}

--- cc/layers/tile_display_layer_impl.h
+++ cc/layers/tile_display_layer_impl.h
@@ -49,11 +49,9 @@ class CC_EXPORT TileDisplayLayerImpl : public LayerImpl {
 
   class CC_EXPORT Tile {
    public:
-    Tile();
-    explicit Tile(const TileContents& contents);
-    Tile(Tile&&);
-    Tile& operator=(Tile&&);
+    explicit Tile(TileDisplayLayerImpl& layer, const TileContents& contents);
     ~Tile();
+    Tile(Tile&&);
 
     const TileContents& contents() const { return contents_; }
 
@@ -75,6 +73,7 @@ class CC_EXPORT TileDisplayLayerImpl : public LayerImpl {
     bool IsReadyToDraw() const { return true; }
 
    private:
+    const raw_ref<TileDisplayLayerImpl> layer_;
     TileContents contents_;
   };
 
@@ -158,13 +157,13 @@ class CC_EXPORT TileDisplayLayerImpl : public LayerImpl {
   void DiscardResource(viz::ResourceId resource);
 
  private:
-  std::vector<std::unique_ptr<Tiling>> tilings_;
   std::optional<SkColor4f> solid_color_;
   bool is_backdrop_filter_mask_ = false;
 
   // Denotes an area that is damaged and needs redraw. This is in the layer's
   // space.
   gfx::Rect damage_rect_;
+  std::vector<std::unique_ptr<Tiling>> tilings_;
 };
 
 }  // namespace cc
