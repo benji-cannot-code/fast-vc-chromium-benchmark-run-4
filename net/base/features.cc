28cfda49d6b68f66a83066e66522d862244a95ed
Tom Van Goethem <tov@chromium.org>
2025-04-23 18:28:53 -0700

    Add Finch-controlled list of ports to block
    This adds a new Finch experiment that allows specifying a
    comma-separated list of ports. The main goal of this is to mitigate
    ongoing abuse faster, and quickly adjust to new ports if needbe.
    
    Bug: 409713509
    Change-Id: I60c3197eb0a2ba592a0de8c04fb045fb87dc0d5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6480959
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450911}

--- net/base/features.cc
+++ net/base/features.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/features.h"
 
+#include <string>
 #include <vector>
 
 #include "base/feature_list.h"
@@ -739,5 +740,14 @@ BASE_FEATURE(kIncludeDeprecatedClientCertLookup,
              "IncludeDeprecatedClientCertLookup",
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
+
+BASE_FEATURE(kRestrictAbusePorts,
+             "RestrictAbusePorts",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE_PARAM(std::string,
+                   kPortsToRestrictForAbuse,
+                   &kRestrictAbusePorts,
+                   "PortsToRestrictForAbuse",
+                   "");
 
 }  // namespace net::features
