7584719c1bdc3f3f77e0776f7f795e1ca956899b
msiem <msiem@chromium.org>
2025-04-24 13:41:39 -0700

    [PWA]: Remove FakeExternallyManagedAppManager
    This CL removes FakeExternallyManagedAppManager, and makes tests using
    the external app installation flow follow the production path instead of
    the faked out path, which makes testing more robust. This does not cause
    a regression, since the fake version would do it anyway if the
    SetHandleInstallRequestCallback() was not set.
    
    Bug: 407797639
    Change-Id: If68adfad20cd01adf1c79a237433bcd768f5f266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6483038
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: May Siem <msiem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451419}

--- chrome/browser/ash/android_sms/android_sms_app_setup_controller_impl_unittest.cc
+++ chrome/browser/ash/android_sms/android_sms_app_setup_controller_impl_unittest.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/web_applications/external_install_options.h"
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
-#include "chrome/browser/web_applications/test/fake_externally_managed_app_manager.h"
 #include "chrome/browser/web_applications/test/fake_web_app_provider.h"
 #include "chrome/browser/web_applications/test/fake_web_contents_manager.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
--- chrome/browser/web_applications/test/fake_web_app_provider.cc
+++ chrome/browser/web_applications/test/fake_web_app_provider.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/preinstalled_web_app_manager.h"
-#include "chrome/browser/web_applications/test/fake_externally_managed_app_manager.h"
 #include "chrome/browser/web_applications/test/fake_os_integration_manager.h"
 #include "chrome/browser/web_applications/test/fake_web_app_database_factory.h"
 #include "chrome/browser/web_applications/test/fake_web_app_ui_manager.h"
@@ -310,9 +309,6 @@ void FakeWebAppProvider::CreateFakeSubsystems() {
   SetFileUtils(base::MakeRefCounted<TestFileUtils>());
 
   SetWebAppUiManager(std::make_unique<FakeWebAppUiManager>());
-
-  SetExternallyManagedAppManager(
-      std::make_unique<FakeExternallyManagedAppManager>(profile_));
 
   IwaIdentityValidator::CreateSingleton();
 
