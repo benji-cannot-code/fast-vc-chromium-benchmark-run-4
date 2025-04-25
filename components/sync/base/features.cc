383bd05fdb0fba28f78dffb1909e6f0976e497bf
Ankush Singh <ankushkush@google.com>
2025-04-25 05:39:33 -0700

    [sync] Decouple priority prefs from user toggle #1
    This CL is part of the changes to decouple priority preferences from
    prefs sync user toggle. The priority prefs will still only be synced
    with the user toggle on, except for the allowlisted prefs which will
    continue to sync even with the toggle off. This feature is dependent on
    EnablePreferencesAccountStorage launch. Note that not all this behavior
    is implemented with this CL but is rather planned for follow-up CLs.
    
    Introduces a finch flag "SyncSupportAlwaysSyncingPriorityPreferences" to
    guard the changes.
    
    This CL, behind the feature flag, marks priority prefs as always
    preferred and removes it from under the kPreferences user toggle.
    
    Bug: 412602018
    Change-Id: Ifcb9ed7a33396f27205576e8440d912ca4e2b1e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491708
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451742}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -79,6 +79,10 @@ BASE_FEATURE(kReplaceSyncPromosWithSignInPromos,
 #endif
 );
 
+BASE_FEATURE(kSyncSupportAlwaysSyncingPriorityPreferences,
+             "SyncSupportAlwaysSyncingPriorityPreferences",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kEnableBookmarksSelectedTypeOnSigninForTesting,
              "EnableBookmarksSelectedTypeOnSigninForTesting",
              base::FEATURE_DISABLED_BY_DEFAULT);
--- components/sync/base/user_selectable_type.cc
+++ components/sync/base/user_selectable_type.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <ostream>
 
+#include "base/feature_list.h"
 #include "base/notreached.h"
 #include "components/sync/base/data_type.h"
+#include "components/sync/base/features.h"
 
 namespace syncer {
 
@@ -51,10 +53,14 @@ UserSelectableTypeInfo GetUserSelectableTypeInfo(UserSelectableType type) {
   switch (type) {
     case UserSelectableType::kBookmarks:
       return {kBookmarksTypeName, BOOKMARKS, {BOOKMARKS, POWER_BOOKMARK}};
-    case UserSelectableType::kPreferences:
-      return {kPreferencesTypeName,
-              PREFERENCES,
-              {PREFERENCES, DICTIONARY, PRIORITY_PREFERENCES, SEARCH_ENGINES}};
+    case UserSelectableType::kPreferences: {
+      DataTypeSet types = {PREFERENCES, DICTIONARY, SEARCH_ENGINES};
+      if (!base::FeatureList::IsEnabled(
+              kSyncSupportAlwaysSyncingPriorityPreferences)) {
+        types.Put(PRIORITY_PREFERENCES);
+      }
+      return {kPreferencesTypeName, PREFERENCES, types};
+    }
     case UserSelectableType::kPasswords:
       return {
           kPasswordsTypeName,
