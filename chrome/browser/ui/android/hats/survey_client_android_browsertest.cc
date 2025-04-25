37fbe2dd28dbfda9f0c270f7bad447de1c55f025
Kamil Jarosz <kjarosz@google.com>
2025-04-25 06:49:05 -0700

    hats: Add tests for navigation behavior on Android
    Add browser tests for navigation behavior for delayed surveys on
    Android. Tests cover all three types of navigation behavior.
    
    Bug: 397440908
    Change-Id: Ied4f5b36389800bcf3f424f0a8d7e9e8432d45e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470427
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451765}

--- chrome/browser/ui/android/hats/survey_client_android_browsertest.cc
+++ chrome/browser/ui/android/hats/survey_client_android_browsertest.cc
@@ -21,7 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 
 namespace hats {
+
 namespace {
+
 const char kTestSurveyTrigger[] = "testing";
 const SurveyBitsData kTestSurveyProductSpecificBitsData{
     {"Test Field 1", true},
--- chrome/browser/ui/android/hats/survey_client_android_unittest.cc
+++ chrome/browser/ui/android/hats/survey_client_android_unittest.cc
@@ -25,7 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::ScopedJavaGlobalRef;
 
 namespace hats {
+
 namespace {
+
 const char kTestSurveyTrigger[] = "testing";
 const SurveyBitsData kTestSurveyProductSpecificBitsData{
     {"Test Field 1", true},
