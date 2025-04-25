0271e249d35dd635b3e39e86944b870489166bb1
Mike Wittman <wittman@chromium.org>
2025-04-25 11:26:12 -0700

    Measure additional PassageEmbeddingsServiceController latencies
    Measures the latency for starting the embedder service, as well as the
    latency for processing individual tasks. In conjunction with the
    existing measurement of the actual embedding computation within the
    embedder service, this will allow us to understand all the factors that
    contribute to latency for computing embeddings.
    
    Bug: 408250012
    Change-Id: I755b2d528344223a739678149169e1a88fb04097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479315
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451940}

--- components/passage_embeddings/passage_embeddings_service_controller.h
+++ components/passage_embeddings/passage_embeddings_service_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/observer_list.h"
+#include "base/timer/elapsed_timer.h"
 #include "base/types/optional_ref.h"
 #include "components/optimization_guide/core/model_info.h"
 #include "components/optimization_guide/proto/passage_embeddings_model_metadata.pb.h"
@@ -88,14 +89,18 @@ class PassageEmbeddingsServiceController : public EmbedderMetadataProvider {
   // the service.
   void LoadModelsToService(
       mojo::PendingReceiver<mojom::PassageEmbedder> receiver,
+      base::ElapsedTimer service_launch_timer,
       mojom::PassageEmbeddingsLoadModelsParamsPtr params);
 
   // Called when an attempt to load models to service finishes.
-  void OnLoadModelsResult(bool success);
+  void OnLoadModelsResult(base::ElapsedTimer service_launch_timer,
+                          bool success);
 
   // Called when an attempt to generate embeddings finishes.
   void OnGotEmbeddings(RequestId request_id,
                        GetEmbeddingsResultCallback callback,
+                       base::ElapsedTimer generate_embeddings_timer,
+                       PassagePriority priority,
                        std::vector<mojom::PassageEmbeddingsResultPtr> results);
 
   // Version of the embeddings model.
