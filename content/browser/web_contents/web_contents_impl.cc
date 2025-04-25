8855147923a7e55bf4788045d65811b307ce5bd9
Adem Derinel <derinel@google.com>
2025-04-25 09:39:18 -0700

    [Android][WebAuthn] Disable WebAuthn in Automotive
    The topic was also brought earlier in b/329158516#comment20.
    
    Underlying platform APIs do not support WebAuthn yet on Auto. Disable it
    for now.
    
    Verified that it is disabled: photos.app.goo.gl/Z5v8YJB8tvCopFQa7
    
    Fixed: 408118757
    Change-Id: I8cc0229377a5fc512fe49d7860116f69fb597e59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490470
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451869}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -232,6 +232,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(IS_ANDROID)
+#include "base/android/build_info.h"
 #include "base/check.h"
 #include "content/browser/android/java_interfaces_impl.h"
 #include "content/browser/android/nfc_host.h"
@@ -3657,6 +3658,13 @@ const blink::web_pref::WebPreferences WebContentsImpl::ComputeWebPreferences(
 
   prefs.payment_request_enabled =
       base::FeatureList::IsEnabled(features::kWebPayments);
+
+#if BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(features::kWebauthnDisabledOnAuto) &&
+      base::android::BuildInfo::GetInstance()->is_automotive()) {
+    prefs.disable_webauthn = true;
+  }
+#endif  // BUILDFLAG(IS_ANDROID)
 
   GetContentClient()->browser()->OverrideWebPreferences(
       this, *main_frame->GetSiteInstance(), &prefs);
