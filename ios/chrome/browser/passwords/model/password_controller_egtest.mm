c54e1443de754f1a2282d78995d452d7b85cba3d
Vincent Boisselle <vincb@google.com>
2025-04-25 11:06:08 -0700

    [ios] Disable PasswordControllerEGTest/testLoginEventReported
    Bug: 413658473
    Change-Id: Ia0d141a6906ecefad584568404c8d4e0e4fdc2fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492108
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Owners-Override: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451929}

--- ios/chrome/browser/passwords/model/password_controller_egtest.mm
+++ ios/chrome/browser/passwords/model/password_controller_egtest.mm
@@ -174,7 +174,7 @@ void LoginOnUff() {
 }
 
 - (void)setUp {
-  if ([self isRunningTest:@selector(testLoginEventReported)]) {
+  if ([self isRunningTest:@selector(FLAKY_testLoginEventReported)]) {
     // Start the servers before calling the superclass's `-setUp` so that their
     // addresses can be added to the app launch config. `GREYAssertTrue` can
     // only be used after calling the superclass's `-setUp`, so use `CHECK()`
@@ -227,7 +227,7 @@ void LoginOnUff() {
   }
 
   // Set Enterprise features for testing password-related event reporting.
-  if ([self isRunningTest:@selector(testLoginEventReported)]) {
+  if ([self isRunningTest:@selector(FLAKY_testLoginEventReported)]) {
     CHECK(_policyServer);
     CHECK(_reportingServer);
     config.additional_args.push_back(
@@ -758,8 +758,9 @@ void LoginOnUff() {
                                 password:passwordValue];
 }
 
+// TODO(crbug.com/413658473): Test fails with the app crashing.
 // Tests that log in events are reported to an enterprise connector.
-- (void)testLoginEventReported {
+- (void)FLAKY_testLoginEventReported {
   [self loadLoginPage];
 
   // Simulate login.
