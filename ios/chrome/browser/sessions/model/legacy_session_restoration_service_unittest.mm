8987fb3a6f9485aee2fedd94fb442e5b16d605bf
Louis Romero <lpromero@google.com>
2025-04-25 07:19:29 -0700

    Mock DataSharingService in Session Restoration tests
    This is a follow-up to crrev.com/c/6480957 that stubbed out
    TabGroupSyncService.
    
    Bug: 409499069
    Change-Id: I4b3db874ecef79f39286c6ec3712fb34c583ed1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488096
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451779}

--- ios/chrome/browser/sessions/model/legacy_session_restoration_service_unittest.mm
+++ ios/chrome/browser/sessions/model/legacy_session_restoration_service_unittest.mm
@@ -23,7 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/metrics/histogram_tester.h"
 #import "base/test/scoped_feature_list.h"
 #import "base/time/time.h"
+#import "components/data_sharing/test_support/mock_data_sharing_service.h"
 #import "components/saved_tab_groups/test_support/fake_tab_group_sync_service.h"
+#import "ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h"
 #import "ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory.h"
 #import "ios/chrome/browser/sessions/model/session_constants.h"
 #import "ios/chrome/browser/sessions/model/session_internal_util.h"
@@ -383,6 +385,12 @@ class LegacySessionRestorationServiceTest : public PlatformTest {
               // Creates a FakeTabGroupSyncService, as the real implementation
               // affects the list of created files.
               return std::make_unique<tab_groups::FakeTabGroupSyncService>();
+            }));
+    builder.AddTestingFactory(
+        data_sharing::DataSharingServiceFactory::GetInstance(),
+        base::BindOnce(
+            [](web::BrowserState* context) -> std::unique_ptr<KeyedService> {
+              return std::make_unique<data_sharing::MockDataSharingService>();
             }));
     profile_ = std::move(builder).Build();
     file_tracker_.Start(profile_->GetStatePath());
