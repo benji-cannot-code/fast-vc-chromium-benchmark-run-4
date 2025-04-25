0dc43d93909bb7128d452a02ab072de8f0e73e02
Jihad Hanna <jihadghanna@google.com>
2025-04-24 23:03:03 -0700

    Fix permissions for MQLS logging
    This is already checked by the MQLS logic [1] and having it here blocks
    the bypass logic they introduced.
    
    The reason that this wasn't caught by the test I added is that the
    TestAutofillClient is initialized with kAllow (as it should) [2] and not
    kAllowWithoutLogging, which is the default configuration for corp
    accounts.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/optimization_guide/core/model_execution/model_execution_features_controller.cc;l=235-244;drc=87b24bc831966733aa45ad8d1a3ea00d3950b245
    
    [2] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/foundations/test_autofill_client.h;l=479-480;drc=87b24bc831966733aa45ad8d1a3ea00d3950b245
    
    Bug: 406013367
    Change-Id: Ib083dec965c935021bd40919522fb23209a216e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6485695
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451629}

--- components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils_unittest.cc
+++ components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils_unittest.cc
@@ -30,10 +30,6 @@ namespace {
 
 using ::testing::Values;
 
-constexpr auto kAutofillPredictionSettingsAllowWithoutLogging =
-    base::to_underlying(
-        optimization_guide::model_execution::prefs::
-            ModelExecutionEnterprisePolicyValue::kAllowWithoutLogging);
 constexpr auto kAutofillPredictionSettingsDisable =
     base::to_underlying(optimization_guide::model_execution::prefs::
                             ModelExecutionEnterprisePolicyValue::kDisable);
@@ -166,20 +162,6 @@ TEST_P(AutofillAiMayPerformActionTest, OptInIphFeatureOff) {
 
   SetAutofillAiOptInStatus(client(), false);
   const bool is_allowed = GetParam() == AutofillAiAction::kOptIn;
-  EXPECT_EQ(MayPerformAutofillAiAction(client(), GetParam()), is_allowed);
-}
-
-// Tests that everything but logging is enabled if the AutofillAI enterprise
-// policy allows the feature without logging.
-TEST_P(AutofillAiMayPerformActionTest,
-       ActionsWhenAutofillAiEnterprisePolicyLoggingDisabled) {
-  client().GetPrefs()->SetInteger(
-      optimization_guide::prefs::
-          kAutofillPredictionImprovementsEnterprisePolicyAllowed,
-      kAutofillPredictionSettingsAllowWithoutLogging);
-
-  const bool is_allowed = GetParam() != AutofillAiAction::kIphForOptIn &&
-                          GetParam() != AutofillAiAction::kLogToMqls;
   EXPECT_EQ(MayPerformAutofillAiAction(client(), GetParam()), is_allowed);
 }
 
