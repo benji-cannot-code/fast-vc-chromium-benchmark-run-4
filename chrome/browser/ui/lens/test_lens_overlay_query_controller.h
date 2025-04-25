9d5d673c0905928dd5dffb29e3090e0f5c109922
Duncan Mercer <mercerd@google.com>
2025-04-25 11:20:44 -0700

    [lensoverlay] Add test to catch not using GetNextRequestId
    This CL adds regression testing to ensure callers don't call
    RequestIdGenerator directly, but instead go through GetNextRequestId.
    This test also caught several other places where the suggest_inputs_
    gets updated but not passed to the SuggestInputsCallback, so this CL
    also fixes those.
    
    Bug: 411227185
    Change-Id: Ia4bf254ec854a2a7c4d8d7e11662e2cd79f289d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470851
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451936}

--- chrome/browser/ui/lens/test_lens_overlay_query_controller.h
+++ chrome/browser/ui/lens/test_lens_overlay_query_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_LENS_TEST_LENS_OVERLAY_QUERY_CONTROLLER_H_
 #define CHROME_BROWSER_UI_LENS_TEST_LENS_OVERLAY_QUERY_CONTROLLER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "components/endpoint_fetcher/endpoint_fetcher.h"
 #include "lens_overlay_query_controller.h"
 
@@ -281,6 +282,8 @@ class TestLensOverlayQueryController : public LensOverlayQueryController {
   void SendSemanticEventGen204IfEnabled(
       lens::mojom::SemanticEvent event,
       std::optional<lens::LensOverlayRequestId> request_id) override;
+
+  void RunSuggestInputsCallback() override;
 
   // The fake response to return for cluster info requests.
   lens::LensOverlayServerClusterInfoResponse fake_cluster_info_response_;
