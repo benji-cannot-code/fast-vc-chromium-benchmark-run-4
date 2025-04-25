55732f1f8a88e389e3ca3717837b374415bf17a8
Baran Erfani <baranerf@google.com>
2025-04-25 11:24:03 -0700

    Minimizing the glic window when the dump without crash happens in ResetEmphasisAndReplay
    This is to gracefully handle the crash case and close(minimize) the glic
    window when we don't have a compositor but call on
    `ResetEmphasisAndReplay`. Closing the window will reset the context
    access indicator bit automatically and the next time that the user opens
    the window we get a clear state.
    
    Testing: Manually tested the solution. Please see b/398319435#comment19
    for more information.
    
    Bug: b:398319435
    Change-Id: I7b08a652770a4d603e5f7a5feed6537c6ba12a43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490127
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451937}

--- chrome/browser/glic/browser_ui/glic_border_view.h
+++ chrome/browser/glic/browser_ui/glic_border_view.h
@@ -27,6 +27,8 @@ class GpuDataManager;
 
 namespace glic {
 
+class GlicKeyedService;
+
 class GlicBorderView : public views::View,
                        public ui::CompositorAnimationObserver,
                        public ui::CompositorObserver,
@@ -126,6 +128,8 @@ class GlicBorderView : public views::View,
   base::TimeTicks GetCreationTime() const;
 
   bool ForceSimplifiedShader() const;
+
+  GlicKeyedService* GetGlicService() const;
 
   // A utility class that subscribe to `GlicKeyedService` for various browser UI
   // status change.
