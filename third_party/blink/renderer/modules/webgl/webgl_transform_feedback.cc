b8f617f9669fe52b8331c258d16b2f16e71c2ec3
Corentin Wallez <cwallez@chromium.org>
2025-04-25 01:06:54 -0700

    [webgl] Make WebGLObject the owner of the object's ID
    Logic to get, set and check the GLuint ID of the WebGL object was
    duplicated between WebGLSharedPlatform3DObject and the non-shareable
    object, leading to some complexity and additional virtual calls. Merge
    all the logic in WebGLObject. This will make it easier to work with
    WebGL[Context/Shared/Shared3DPlatform]Object in future CLs.
    
    Also move IsRenderbuffer/Texture on WebGLObject and remove other unused
    RTTI virtual methods.
    
    Bug: 413078308
    Change-Id: Ie9fbd263b9958007caec0ba744ed300bf3407157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6485690
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451655}

--- third_party/blink/renderer/modules/webgl/webgl_transform_feedback.cc
+++ third_party/blink/renderer/modules/webgl/webgl_transform_feedback.cc
@@ -15,7 +15,6 @@ WebGLTransformFeedback::WebGLTransformFeedback(
     TFType type,
     GLint max_transform_feedback_separate_attribs)
     : WebGLContextObject(ctx),
-      object_(0),
       type_(type),
       target_(0),
       program_(nullptr),
@@ -35,7 +34,7 @@ WebGLTransformFeedback::WebGLTransformFeedback(
     case TFType::kUser: {
       GLuint tf;
       ctx->ContextGL()->GenTransformFeedbacks(1, &tf);
-      object_ = tf;
+      SetObject(tf);
       break;
     }
   }
@@ -57,8 +56,7 @@ void WebGLTransformFeedback::DeleteObjectImpl(gpu::gles2::GLES2Interface* gl) {
     case TFType::kDefault:
       break;
     case TFType::kUser:
-      gl->DeleteTransformFeedbacks(1, &object_);
-      object_ = 0;
+      gl->DeleteTransformFeedbacks(1, &Object());
       break;
   }
 
