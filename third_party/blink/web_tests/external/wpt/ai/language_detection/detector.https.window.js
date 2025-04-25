cca57a4ab2fcf013ca090f4f229fe2d99a8bbebc
Nathan Memmott <memmott@chromium.org>
2025-04-25 11:03:48 -0700

    LanguageDetector: Update und logic to match new spec logic
    The logic to calculate the results and und have been updated in the
    spec. This updates our implementation to match.
    
    Fixed: 413151736
    Change-Id: I99ac48978104cf9e599b20880382ca6ea3a54f65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490186
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451925}

--- third_party/blink/web_tests/external/wpt/ai/language_detection/detector.https.window.js
+++ third_party/blink/web_tests/external/wpt/ai/language_detection/detector.https.window.js
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=Detect english
 // META: global=window
 // META: script=../resources/util.js
+// META: script=../resources/locale-util.js
 
 'use strict';
 
@@ -16,6 +17,9 @@ promise_test(async t => {
   const detector = await LanguageDetector.create();
   const results = await detector.detect('Hello world!');
 
+  // must at least have the 'und' result.
+  assert_greater_than_equal(results.length, 1);
+
   // The last result should be 'und'.
   const undResult = results.pop();
   assert_equals(undResult.detectedLanguage, 'und');
@@ -23,24 +27,31 @@ promise_test(async t => {
 
   let total_confidence_without_und = 0;
   let last_confidence = 1;
-  for (const {confidence} of results) {
+  for (const {detectedLanguage, confidence} of results) {
+    // All results must be in canonical form.
+    assert_is_canonical(detectedLanguage);
+
     assert_greater_than(confidence, 0);
+    assert_greater_than(confidence, undResult.confidence);
 
     total_confidence_without_und += confidence;
 
-    // Except for 'und', results should be from high to low confidence.
+    // Except for 'und', results must be from high to low confidence.
     assert_greater_than_equal(last_confidence, confidence);
     last_confidence = confidence;
   }
 
-  // Confidences, excluding both 'und' and the last non-'und' result, should be
-  // less than 0.99.
-  assert_less_than(
-      total_confidence_without_und - results.at(-1).confidence, 0.99);
+  // If we have non-und results, their confidences, excluding the last non-'und'
+  // result, must be less than 0.99.
+  if (results.length > 0) {
+    assert_less_than(
+        total_confidence_without_und - results.at(-1).confidence, 0.99);
+  }
 
-  // Confidences, including 'und', should add up to 1.
-  assert_equals(total_confidence_without_und + undResult.confidence, 1);
-}, 'Simple LanguageDetector.detect() call');
+  // Confidences, including 'und', should be less than or equal to one.
+  assert_less_than_equal(
+      total_confidence_without_und + undResult.confidence, 1);
+}, 'LanguageDetector.detect() returns valid results');
 
 promise_test(async t => {
   const error = new Error('CreateMonitorCallback threw an error');
