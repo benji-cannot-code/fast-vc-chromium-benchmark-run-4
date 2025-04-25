ebccbecb6d7cac93db7ace05d4702c9a18694ec4
Qihui Zhao <qihuizhao@google.com>
2025-04-25 07:46:55 -0700

    Fix upload save IBAN metrics for kAccept case
    This CL fixes an issue where the "upload save IBAN accepted" case was
    not being logged. The root cause was that current_bubble_type_ was
    being set to IbanBubbleType::kUploadInProgress right after the
    user clicked the save button. However, the logging logic for the bubble
    closing reason did not account for kUploadInProgress in upload
    scenarios. This change ensures that the accepted upload case is properly
    logged.
    This issue is introduced at http://crrev.com/c/5983935 when
    loading effect is added to the upload bubble.
    
    Bug: b:413338121
    Change-Id: Id04bf41f27e26522245de203c233477a5c88403d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488936
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451791}

--- chrome/browser/ui/autofill/payments/iban_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/iban_bubble_controller_impl.cc
@@ -304,7 +304,8 @@ void IbanBubbleControllerImpl::OnBubbleClosed(
 
   // Log save IBAN prompt result according to the closed reason.
   if (current_bubble_type_ == IbanBubbleType::kLocalSave ||
-      current_bubble_type_ == IbanBubbleType::kUploadSave) {
+      current_bubble_type_ == IbanBubbleType::kUploadSave ||
+      current_bubble_type_ == IbanBubbleType::kUploadInProgress) {
     autofill_metrics::SaveIbanPromptResult metric;
     switch (closed_reason) {
       case PaymentsUiClosedReason::kAccepted:
@@ -327,7 +328,9 @@ void IbanBubbleControllerImpl::OnBubbleClosed(
     }
     autofill_metrics::LogSaveIbanPromptResultMetric(
         metric, is_reshow_,
-        /*is_upload_save=*/current_bubble_type_ == IbanBubbleType::kUploadSave);
+        /*is_upload_save=*/
+        (current_bubble_type_ == IbanBubbleType::kUploadSave ||
+         current_bubble_type_ == IbanBubbleType::kUploadInProgress));
   }
 
   if (current_bubble_type_ == IbanBubbleType::kUploadCompleted) {
