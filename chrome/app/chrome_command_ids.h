86db9b304aed63dc5c5e6fa5d1b39fb0fb5ba079
Bryant Chandler <bryantchandler@chromium.org>
2025-04-25 15:01:22 -0700

    Remove static focus toggle hotkey
    This is being replaced by a configurable hotkey.
    
    NO_IFTTT=This command wasn't represented in the context menu
    
    Bug: b:407829577
    Change-Id: I59e60f70c1126cdc0a1f2d6327d6d8d32f20bc43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489387
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452085}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -580,9 +580,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Glic button context menu and tabstrip context menu
 #define IDC_GLIC_TOGGLE_PIN 53320
 
-// Glic focus navigation
-#define IDC_GLIC_TOGGLE_FOCUS 53325
-
 // NOTE: The last valid command value is 57343 (0xDFFF)
 // See http://msdn.microsoft.com/en-us/library/t2zechd4(VS.71).aspx
 
