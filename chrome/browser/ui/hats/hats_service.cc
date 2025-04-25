3a109ec9f4be9eb759cb41e7539581a50f6a58e9
Kamil Jarosz <kjarosz@google.com>
2025-04-25 06:45:33 -0700

    hats: Implement navigation behavior for delayed surveys on Android
    Navigation behavior allows restricting where the user can navigate
    during the delay before showing the survey. This patch implements that
    behavior similarly to HaTS on desktop, deduplicating common logic.
    
    Bug: 397440908
    Change-Id: I01e7cf50468d55834ead9fc2a9684169864b944b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6439023
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451764}

--- chrome/browser/ui/hats/hats_service.cc
+++ chrome/browser/ui/hats/hats_service.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/hats/hats_service.h"
 
+#include "content/public/browser/navigation_handle.h"
+
 HatsService::SurveyMetadata::SurveyMetadata() = default;
 
 HatsService::SurveyMetadata::~SurveyMetadata() = default;
@@ -25,4 +27,25 @@ HatsService::~HatsService() = default;
 
 hats::SurveyConfigs& HatsService::GetSurveyConfigsByTriggersForTesting() {
   return survey_configs_by_triggers_;
+}
+
+bool HatsService::IsNavigationAllowed(
+    content::NavigationHandle* navigation_handle,
+    HatsService::NavigationBehaviour navigation_behaviour) {
+  if (navigation_behaviour == NavigationBehaviour::ALLOW_ANY ||
+      !navigation_handle || !navigation_handle->IsInPrimaryMainFrame()) {
+    return true;
+  }
+
+  if (navigation_behaviour == NavigationBehaviour::REQUIRE_SAME_DOCUMENT &&
+      navigation_handle->IsSameDocument()) {
+    return true;
+  }
+
+  if (navigation_behaviour == NavigationBehaviour::REQUIRE_SAME_ORIGIN &&
+      navigation_handle->HasCommitted() && navigation_handle->IsSameOrigin()) {
+    return true;
+  }
+
+  return false;
 }
