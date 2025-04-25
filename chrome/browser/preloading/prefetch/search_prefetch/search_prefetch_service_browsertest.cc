b66de619afdc934c7ba64072b6da2a8a4081a150
Lingqi Chi <lingqi@chromium.org>
2025-04-25 01:34:21 -0700

    [SearchPrefetch] Mark search prefetches as prefetched responses in the header.
    This allows navigationTiming API and response consumers to tell whether
    a response is served from Prefetch Memory Cache or not.
    
    Bug: 413557424, 379052300
    Change-Id: I6c1a8bea101e437e965d43237333e8f5e72f32ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490000
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451661}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
@@ -1295,6 +1295,13 @@ IN_PROC_BROWSER_TEST_F(SearchPrefetchServiceEnabledBrowserTest,
   CheckCorrectForwardingResultMetric(
       histogram_tester,
       StreamingSearchPrefetchURLLoader::ForwardingResult::kCompleted, 1);
+
+  content::RenderFrameHost* frame = GetWebContents()->GetPrimaryMainFrame();
+  EXPECT_EQ(
+      "navigational-prefetch",
+      content::EvalJs(
+          frame, "performance.getEntriesByType('navigation')[0].deliveryType"));
+
   {
     ukm::SourceId ukm_source_id =
         GetWebContents()->GetPrimaryMainFrame()->GetPageUkmSourceId();
@@ -2254,6 +2261,11 @@ IN_PROC_BROWSER_TEST_F(SearchPrefetchServiceEnabledBrowserTest,
       "Omnibox.SearchPrefetch.ReceivedServableResponse2.Fallback."
       "SuggestionPrefetch",
       /*can_be_served*/ true, 1);
+  content::RenderFrameHost* frame = GetWebContents()->GetPrimaryMainFrame();
+  EXPECT_EQ(
+      "",
+      content::EvalJs(
+          frame, "performance.getEntriesByType('navigation')[0].deliveryType"));
 }
 
 IN_PROC_BROWSER_TEST_F(SearchPrefetchServiceEnabledBrowserTest,
