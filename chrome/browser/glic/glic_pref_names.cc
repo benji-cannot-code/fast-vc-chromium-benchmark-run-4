9693968817a410085f7202ee187e24ac7515dcf9
Bryant Chandler <bryantchandler@chromium.org>
2025-04-25 14:16:59 -0700

    Create objects for managing local hotkeys
    These are specifically for application or view local rather than system
    global hotkeys. They allow hotkeys to be either static or configured by
    a pref.
    
    Followup CLs will adopt these in GlicWindowController and remove
    existing local hotkey code and add tests.
    
    Bug: b:407829577
    Change-Id: Ia9d60b1597c6c00fb191d28b098c87583beac049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6484771
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452052}

--- chrome/browser/glic/glic_pref_names.cc
+++ chrome/browser/glic/glic_pref_names.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/glic_pref_names.h"
 
 #include "chrome/browser/background/glic/glic_launcher_configuration.h"
+#include "chrome/browser/glic/widget/local_hotkey_manager.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -39,6 +40,11 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
       prefs::kGlicLauncherHotkey,
       ui::Command::AcceleratorToString(
           GlicLauncherConfiguration::GetDefaultHotkey()));
+  registry->RegisterStringPref(
+      prefs::kGlicFocusToggleHotkey,
+      ui::Command::AcceleratorToString(
+          LocalHotkeyManager::GetDefaultAccelerator(
+              LocalHotkeyManager::Hotkey::kFocusToggle)));
 }
 
 }  // namespace glic::prefs
