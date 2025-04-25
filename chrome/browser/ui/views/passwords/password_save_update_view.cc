2bfbc4b30e1119dd3ddf45e664b4cb68c9d37e22
Dana Fried <dfried@chromium.org>
2025-04-25 08:01:40 -0700

    [Level Up] Password save recovery promo
    Shows when the user logs into a site that they have selected "Never save
    passwords for this site".
    
    Reminds them that they are not saving passwords and says how they can
    change that.
    
    Follow-ups probably include UI changes to better differentiate between
    the "never" state, the "not saved" state, and the "saved" state.
    
    Bug: 411441888, 413644558
    Change-Id: I7d2334a15576b3a5723c0d21780f344e5a61f496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6484191
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451808}

--- chrome/browser/ui/views/passwords/password_save_update_view.cc
+++ chrome/browser/ui/views/passwords/password_save_update_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/debug/stack_trace.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/password_manager/password_store_utils.h"
 #include "chrome/browser/profiles/profile.h"
@@ -67,6 +68,9 @@ PasswordSaveUpdateView::PasswordSaveUpdateView(
   DCHECK(controller_.state() == password_manager::ui::PENDING_PASSWORD_STATE ||
          controller_.state() ==
              password_manager::ui::PENDING_PASSWORD_UPDATE_STATE);
+
+  LOG(WARNING) << "-------------------\n"
+               << base::debug::StackTrace(20).ToString();
 
   const password_manager::PasswordForm& password_form =
       controller_.pending_password();
