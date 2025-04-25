7628b0b80933792098e85be9e7b9dcd0b1b19a60
Di Zhang <dizhangg@chromium.org>
2025-04-25 11:02:07 -0700

    [reading-flow] Can render with carousel
    Setting CSS property reading-flow on a carousel scroller is failing
    DCHECKs for both the focus navigation and the accessibility tree. To
    fix this use case,
    1. Relax the OwnedByReadingFlow to be element instead of HTMLElement.
       This is because pseudo-element can be reading flow item scope owners.
    2. Change Element::ReadingFlowChildren to add out of flow reading flow
       items in LayoutTreeBuilderTraversal instead of FlatTreeTraversal.
       This will add scroll marker and related pseudo elements.
    
    Change-Id: Icbb523c4f93aff316a8f8bfcbf867e1ec781a1d7
    Bug: 398280418, 412401428, 413659838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479432
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451922}

--- third_party/blink/renderer/core/page/focus_controller.cc
+++ third_party/blink/renderer/core/page/focus_controller.cc
@@ -798,7 +798,7 @@ ScopedFocusNavigation ScopedFocusNavigation::OwnedByReadingFlow(
     const Element& owner,
     FocusController::OwnerMap& owner_map) {
   DCHECK(IsReadingFlowScopeOwner(&owner));
-  HTMLElement& element = const_cast<HTMLElement&>(To<HTMLElement>(owner));
+  Element& element = const_cast<Element&>(owner);
   return ScopedFocusNavigation(element, nullptr, owner_map);
 }
 
