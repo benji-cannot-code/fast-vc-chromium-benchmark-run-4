db375f21b9ea2b954e23555ff5d8f60a4cc42c50
Daniel White <danieltwhite@google.com>
2025-04-25 08:55:10 -0700

    Auto-deletion PrefBackedBoolean Crash
    In response to a reported crash involving the improper deallocation of
    the PrefBackedBoolean used in the auto_deletion_settings_mediator, this
    CL remedies the issue by invoking the boolean's stop function and sets
    its observer to null nil before setting the boolean itself to null.
    
    Fixed: 412885454
    Change-Id: I1eba85fd29afc38bbf012ee98389f278b8895e77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488551
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451843}

--- ios/chrome/browser/download/coordinator/auto_deletion/auto_deletion_settings_mediator.mm
+++ ios/chrome/browser/download/coordinator/auto_deletion/auto_deletion_settings_mediator.mm
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)disconnect {
   _localState = nullptr;
+  [_autoDeletionEnabled stop];
+  [_autoDeletionEnabled setObserver:nil];
   _autoDeletionEnabled = nullptr;
 }
 
