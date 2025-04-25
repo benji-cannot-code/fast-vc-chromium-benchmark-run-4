af02c04e756502915e7d6a9ce29d8cab45ff3d5c
Sky Malice <skym@chromium.org>
2025-04-24 17:56:50 -0700

    Remove two star closing javadoc from //base/.
    Bug: None
    Change-Id: I6fca82b08c4358a532d60e12d9c74202028b144e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490053
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451567}

--- base/android/java/src/org/chromium/base/metrics/RecordHistogram.java
+++ base/android/java/src/org/chromium/base/metrics/RecordHistogram.java
@@ -11,9 +11,7 @@ import org.chromium.build.annotations.NullMarked;
 
 import java.util.List;
 
-/**
- * Java API for recording UMA histograms.
- * */
+/** Java API for recording UMA histograms. */
 @NullMarked
 public class RecordHistogram {
     /**
