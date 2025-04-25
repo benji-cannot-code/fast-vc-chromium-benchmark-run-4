55a874b717b805a4aa09bcf5ed9e1791f917e5e9
Joe Downing <joedow@google.com>
2025-04-25 14:19:32 -0700

    Implement signaling in the native remoting client
    This CL adds signaling to the native remoting client as well as some
    interfaces which will be implemented in subsequent CLs.  While testing
    this I found a bug in the ftl_signal_strategy which causes an infinite
    loop is a user_email is not provided via the OAuthTokenGetter. On a
    completely unrelated note, I also added a user_email field to the CLI
    tool so I can provide a user_email value to the
    PassthroughOAuthTokenGetter : )
    
    Bug: 412754894
    Change-Id: I632cd54a186c3014e0f356a57f5a5e48d3785af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490010
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452054}

--- remoting/base/oauth_token_info.cc
+++ remoting/base/oauth_token_info.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-OAuthTokenInfo::OAuthTokenInfo(const std::string& token)
-    : access_token_(token), user_email_("") {}
+OAuthTokenInfo::OAuthTokenInfo(const std::string& access_token)
+    : access_token_(access_token), user_email_("") {}
 
-OAuthTokenInfo::OAuthTokenInfo(const std::string& token,
-                               const std::string& email)
-    : access_token_(token), user_email_(email) {}
+OAuthTokenInfo::OAuthTokenInfo(const std::string& access_token,
+                               const std::string& user_email)
+    : access_token_(access_token), user_email_(user_email) {}
 
 }  // namespace remoting
