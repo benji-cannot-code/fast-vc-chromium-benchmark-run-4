bc7c6e236a72273eec9741888da1dc9ccf94894c
Bruno Braga <brunobraga@google.com>
2025-04-25 23:45:24 -0700

    [AutofillAi] Update table to have use count and date used data
    This is the final CL for implementation of frencency.
    
    Bug: 408421018
    Change-Id: If87ef5a97f74ba9ef1328d8eac500b346c702177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477428
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452184}

--- components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.cc
+++ components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/uuid.h"
+#include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
 #include "components/autofill/core/browser/strike_databases/autofill_ai/autofill_ai_save_strike_database_by_host.h"
 #include "components/webdata/common/web_data_results.h"
 
