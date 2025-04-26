4a2b535fa238a46a086b0bee6514399bc5c7d95f
Charles Hager <clhager@google.com>
2025-04-25 19:19:52 -0700

    Disable flakey test UnsubscribedNotificationsNotificationTest#testAckNotification
    Bug: 413793282
    Change-Id: I25cf4d1eec2000e0b7d6cdf6222c88528be68354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492894
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Owners-Override: Charles Hager <clhager@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452159}

--- chrome/browser/safety_hub/android/javatests/src/org/chromium/chrome/browser/safety_hub/UnsubscribedNotificationsNotificationTest.java
+++ chrome/browser/safety_hub/android/javatests/src/org/chromium/chrome/browser/safety_hub/UnsubscribedNotificationsNotificationTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.Restriction;
@@ -51,6 +52,7 @@ public final class UnsubscribedNotificationsNotificationTest {
     @Test
     @SmallTest
     @Feature({"SafetyHubNotification"})
+    @DisabledTest(message = "crbug.com/413793282")
     public void testAckNotification() throws Exception {
         UnsubscribedNotificationsNotificationManager.updateNotification(1);
         List<MockNotificationManagerProxy.NotificationEntry> notifications =
