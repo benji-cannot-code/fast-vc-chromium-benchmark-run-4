d72ac0f4588ee4e662d75022b47a271575333fc2
Chris Palmer <palmer@google.com>
2025-04-28 16:50:43 -0700

    Roll syn: 2.0.100 => 2.0.101 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * syn: 2.0.100 => 2.0.101
    
    Bug: None
    Change-Id: I5538141e0b28b0a7b26d8ee7342022c9e085bcba
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495656
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Commit-Queue: Chris Palmer <palmer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453002}

--- third_party/rust/chromium_crates_io/vendor/syn-v2/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/syn-v2/src/lib.rs
@@ -250,7 +250,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!   dynamic library libproc_macro from rustc toolchain.
 
 // Syn types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/syn/2.0.100")]
+#![doc(html_root_url = "https://docs.rs/syn/2.0.101")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(non_camel_case_types)]
@@ -280,7 +280,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     clippy::manual_let_else,
     clippy::manual_map,
     clippy::match_like_matches_macro,
-    clippy::match_on_vec_items,
     clippy::match_same_arms,
     clippy::match_wildcard_for_single_variants, // clippy bug: https://github.com/rust-lang/rust-clippy/issues/6984
     clippy::missing_errors_doc,
