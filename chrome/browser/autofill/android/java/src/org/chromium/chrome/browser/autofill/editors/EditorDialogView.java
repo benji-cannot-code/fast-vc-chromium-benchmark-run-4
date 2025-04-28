7f9356013fa30b3f01e0808eb3bad2a1650d9e91
Piotr Kotynia <piotrkotynia@google.com>
2025-04-28 03:21:11 -0700

    [Android] Rename Autofill Editor XML Files and Fix Lint Errors
    This change refactors the naming convention of the XML layout files
    associated with the autofill editor. Previously named with the
    `payment_request_editor...` prefix, these files have been renamed to
    `autofill_editor_dialog...`. This renaming more accurately represents
    their function, as these layouts are now utilized by the general
    autofill editor dialog and are no longer specific to payment requests.
    
    In addition to the file renaming, this CL also addresses and resolves
    several minor lint errors that were present within these XML layout
    files.
    
    Bug: 40100455
    Change-Id: I90482b0adfee875d0035edc9bdcc478e5bdddb9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491827
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452511}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/EditorDialogView.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/EditorDialogView.java
@@ -144,7 +144,7 @@ public class EditorDialogView extends AlwaysDismissedDialog
         setOnDismissListener(this);
 
         mContainerView =
-                LayoutInflater.from(mActivity).inflate(R.layout.payment_request_editor, null);
+                LayoutInflater.from(mActivity).inflate(R.layout.autofill_editor_dialog, null);
         setContentView(mContainerView);
 
         prepareToolbar();
