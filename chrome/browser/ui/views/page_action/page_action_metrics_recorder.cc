e5c270dc7f40348122cf62b734e5c7ab552f7e7b
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-04-28 07:30:35 -0700

    Do IWYU in c/b/ui/views/page_action/page_action_metrics_recorder.cc
    Change-Id: I6fe8d3d94b4305b7838133cdb3866a3e648bb97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489869
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452602}

--- chrome/browser/ui/views/page_action/page_action_metrics_recorder.cc
+++ chrome/browser/ui/views/page_action/page_action_metrics_recorder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/page_action/page_action_metrics_recorder.h"
 
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/strcat.h"
 #include "chrome/browser/ui/views/page_action/page_action_enums.h"
 #include "chrome/browser/ui/views/page_action/page_action_model.h"
 #include "chrome/browser/ui/views/page_action/page_action_properties_provider.h"
