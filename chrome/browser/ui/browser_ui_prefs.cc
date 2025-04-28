9b0a7734fad9d2b7e5a227ab91fd25ae3af84736
Salma Elmahallawy <esalma@google.com>
2025-04-28 09:10:07 -0700

    [NTPFooter] Adding policies to control visibility of footer components
    The CL adds a policy to control the visibility of the NTP footer
    management notice and theme attribution.
    
    The management notice policy is not per profile since it is shown for
    all profiles when the browser is managed. Theme attribution is per
    profile as the theme applies to the profile the extension is installed
    in.
    
    The policies will be used in follow up CLs.
    
    Bug: 411428900, 409620845
    Change-Id: I1bd79fbc4cac6b58867ca47bf34dcac142d89e91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6479319
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452671}

--- chrome/browser/ui/browser_ui_prefs.cc
+++ chrome/browser/ui/browser_ui_prefs.cc
@@ -70,6 +70,7 @@ void RegisterBrowserPrefs(PrefRegistrySimple* registry) {
                                std::string());
   registry->RegisterStringPref(prefs::kEnterpriseLogoUrlForBrowser,
                                std::string());
+  registry->RegisterBooleanPref(prefs::kNTPFooterManagementNoticeEnabled, true);
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 }
 
@@ -192,5 +193,6 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
                                std::string());
   registry->RegisterIntegerPref(prefs::kEnterpriseProfileBadgeToolbarSettings,
                                 0);
+  registry->RegisterBooleanPref(prefs::kNTPFooterThemeAttributionEnabled, true);
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 }
