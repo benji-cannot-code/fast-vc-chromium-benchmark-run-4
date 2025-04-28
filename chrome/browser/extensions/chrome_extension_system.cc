5d1d86d33fc22471384e846a3e80f34f6358bebc
James Cook <jamescook@chromium.org>
2025-04-28 14:13:22 -0700

    extensions: Add UninstallPingSender to desktop Android
    Now that we have ExtensionService this is safe to create (it depends
    on ExtensionUpdater initialization in ExtensionService).
    
    Bug: 413460628
    Test: uninstall an extension, no crash
    Change-Id: I749f809a6e028b89c5d03d4e0708d2a88c737954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492808
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452905}

--- chrome/browser/extensions/chrome_extension_system.cc
+++ chrome/browser/extensions/chrome_extension_system.cc
@@ -88,10 +88,7 @@ namespace extensions {
 
 namespace {
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Helper to serve as an UninstallPingSender::Filter callback.
-// TODO(crbug.com/413460628): This depends on initialization of ExtensionUpdater
-// performed by ExtensionService.
 UninstallPingSender::FilterResult ShouldSendUninstallPing(
     Profile* profile,
     const Extension* extension,
@@ -104,7 +101,6 @@ UninstallPingSender::FilterResult ShouldSendUninstallPing(
   }
   return UninstallPingSender::DO_NOT_SEND_PING;
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 }  // namespace
 
@@ -232,11 +228,9 @@ void ChromeExtensionSystem::Shared::Init(bool extensions_enabled) {
       ExtensionErrorController::Get(profile_), autoupdate_enabled,
       extensions_enabled, &ready_);
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
   uninstall_ping_sender_ = std::make_unique<UninstallPingSender>(
       ExtensionRegistry::Get(profile_),
       base::BindRepeating(&ShouldSendUninstallPing, profile_));
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
   // These services must be registered before the ExtensionService tries to
   // load any extensions.
