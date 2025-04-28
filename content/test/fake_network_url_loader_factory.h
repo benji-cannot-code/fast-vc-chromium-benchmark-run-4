9dd1f43aaae37367ddbe275b4b3a60b8ab65b7a4
Shunya Shishido <sisidovski@chromium.org>
2025-04-28 00:18:58 -0700

    Fix StaticRoutingRaceFetchWin and add non-200 fetch handler wins test
    This will address the discussion in crrev.com/c/6452845 ([1] and [2]).
    
    Currently, the `StaticRoutingRaceFetchWin` test in
    `ServiceWorkerMainResourceLoaderTest` doesn't receive the fetch handler result
    first, actually the network response is faster than the fetch handler. This is
    not an expected behavior. This CL fixes this issue by adding the defer option
    to the FakeNetworkURLLoaderFactory.
    
    Additionally, it would be better to have the test that the network
    request wins the race but the response is not 2xx. In that case the
    `race-network-and-fetch-handler` should use the response from the fetch
    handler instead of non-2xx response from the network.
    
    This is a preliminary change to enable
    `ServiceWorkerStaticRouterRaceNetworkRequestPerformanceImprovement`.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6452845/comment/d9f9b998_33d5396a/
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/6452845/comment/3f09136c_1f710c02/
    
    Bug: 370844790, 374606637
    Change-Id: I226f161c3f772735fc4e6d43f51691769a23a4ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6482867
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452431}

--- content/test/fake_network_url_loader_factory.h
+++ content/test/fake_network_url_loader_factory.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_TEST_FAKE_NETWORK_URL_LOADER_FACTORY_H_
 #define CONTENT_TEST_FAKE_NETWORK_URL_LOADER_FACTORY_H_
 
+#include <optional>
+
 #include "content/test/fake_network.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -47,9 +49,26 @@ class FakeNetworkURLLoaderFactory final
   void Clone(mojo::PendingReceiver<network::mojom::URLLoaderFactory> receiver)
       override;
 
+  // If `DeferHandleRequest()` is called, `CreateLoaderAndStart()` does not
+  // start handling the request. Instead, it waits for `HandleDeferredRequest()`
+  // being called.
+  //
+  // It's caller side responsibility to resume handling the request. This is
+  // convenient to manually delay the request processing for tests.
+  void DeferHandleRequest() { defer_handle_request_ = true; }
+  void HandleDeferredRequest();
+
  private:
+  URLLoaderInterceptor::RequestParams MakeRequestParams(
+      int32_t request_id,
+      uint32_t options,
+      const network::ResourceRequest& url_request,
+      mojo::PendingRemote<network::mojom::URLLoaderClient> client,
+      const net::MutableNetworkTrafficAnnotationTag& traffic_annotation);
   FakeNetwork fake_network_;
   mojo::ReceiverSet<network::mojom::URLLoaderFactory> receivers_;
+  std::optional<URLLoaderInterceptor::RequestParams> deferred_request_params_;
+  bool defer_handle_request_ = false;
 };
 
 }  // namespace content
