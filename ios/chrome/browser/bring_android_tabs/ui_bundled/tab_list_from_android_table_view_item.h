a2a8b36203899dfb641f2b40b505f9e04e5f376e
Asami Doi <asamidoi@chromium.org>
2025-04-28 09:18:16 -0700

    [ios] Replace all strong NSString* with copy
    This CL replaces all strong NSStrings with a copy keyword. Also,
    manually checked all updates and call the copy method if necessary.
    
    for file in $(git grep -l "@property\(.*strong.*\) NSString" ./ios); do
      sed -i '' -e 's/\(@property(.*\), strong\(.*)\)\( NSString\)/\1, copy\2\3/' "$file"
      sed -i '' -e 's/\(@property(.*\)strong, \(.*)\)\(NSString\)/\1copy, \2\3/' "$file"
    done
    
    This CL was uploaded by git cl split.
    
    R=ginnyhuang@chromium.org
    
    Bug: 413648986
    Change-Id: I68fce5d3505b24430a2c9afad39747952d6429de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494809
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452678}

--- ios/chrome/browser/bring_android_tabs/ui_bundled/tab_list_from_android_table_view_item.h
+++ ios/chrome/browser/bring_android_tabs/ui_bundled/tab_list_from_android_table_view_item.h
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, readonly, strong) UILabel* URLLabel;
 
 // Unique identifier that matches with one TabListFromAndroidTableViewItem.
-@property(nonatomic, strong) NSString* cellUniqueIdentifier;
+@property(nonatomic, copy) NSString* cellUniqueIdentifier;
 
 @end
 
