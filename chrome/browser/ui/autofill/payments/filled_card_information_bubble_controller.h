8d244d029a34eb324cbe36383ffbfee6f9934321
Wilson Low <wilsonlow@google.com>
2025-04-28 13:13:24 -0700

    [BNPL] Add dark theme issuer logos to filled card bubble
    Before Light: https://screenshot.googleplex.com/5FLzq8Kz77krQsB
    Before Dark: https://screenshot.googleplex.com/AKSJJNgKpiGNqco
    
    After Light (unchanged):
      https://screenshot.googleplex.com/7NkbT3bquBgUbrA
    After Dark: https://screenshot.googleplex.com/7JCAprnXKgVjo89
    
    Bug: 356443046, b/413716448
    Change-Id: I1280e69ff9744ad67fe24e2636bf1b5f6f8c9d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491969
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452859}

--- chrome/browser/ui/autofill/payments/filled_card_information_bubble_controller.h
+++ chrome/browser/ui/autofill/payments/filled_card_information_bubble_controller.h
@@ -7,10 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_FILLED_CARD_INFORMATION_BUBBLE_CONTROLLER_H_
 
 #include <string>
+#include <utility>
 
 #include "components/autofill/core/browser/ui/payments/payments_ui_closed_reasons.h"
 #include "content/public/browser/web_contents.h"
-#include "ui/gfx/image/image.h"
+#include "ui/base/models/image_model.h"
 
 namespace autofill {
 
@@ -114,8 +115,11 @@ class FilledCardInformationBubbleController {
   // bubble.
   virtual std::u16string GetMaskedCardNameForDescriptionView() const = 0;
 
-  // Returns the image to display in the description view of the bubble.
-  virtual gfx::Image GetCardImageForDescriptionView() const = 0;
+  // Returns a pair of images to display in the description view of the bubble.
+  // The first is the image to be shown for light themes, and the second is an
+  // optional image to be shown for dark themes.
+  virtual std::pair<ui::ImageModel, std::optional<ui::ImageModel>>
+  GetCardImageForDescriptionView() const = 0;
 
   // Returns whether the educational body includes a learn more link.
   virtual bool EducationalBodyHasLearnMoreLink() const = 0;
