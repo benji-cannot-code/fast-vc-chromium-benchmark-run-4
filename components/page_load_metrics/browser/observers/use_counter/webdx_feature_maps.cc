551993dc6b14365dbde768880d9837f549d4404c
Philip Jägenstedt <foolip@chromium.org>
2025-04-28 09:58:51 -0700

    Add use counters for rAF on main thread and in workers
    There was already a classic use counter for main thread rAF, but for the
    worker variant add only a WebDX use counter, for bit.ly/webdxuc.
    
    Bug: 366475997
    Change-Id: I15e698f4b3eeabf835302756352c9f34d3614905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6487509
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452709}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -381,6 +381,8 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kSpeculationRules, WebDXFeature::kSpeculationRules},
           {WebFeature::kElementCapture, WebDXFeature::kElementCapture},
           {WebFeature::kRegionCapture, WebDXFeature::kRegionCapture},
+          {WebFeature::kUnprefixedRequestAnimationFrame,
+           WebDXFeature::kRequestAnimationFrame},
           // Add new features above this line.
       }};
 
