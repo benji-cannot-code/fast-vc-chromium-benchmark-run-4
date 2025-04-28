fb104ec12c08db2447988730a98f692093b42601
Andrew Grieve <agrieve@chromium.org>
2025-04-28 10:56:31 -0700

    Reland "Delete all @MockedInTests except for 3"
    This reverts commit 6af8c6bfa936940352a81a3f19c947ce4e85e540.
    
    Reason for reland: Put one back
    
    Bug: 389166093
    Original change's description:
    > Revert "Delete all @MockedInTests except for 2"
    >
    > This reverts commit 3623d157d1d46b96d89fa9321cb3385f500ad59f.
    >
    > Reason for revert: Broke AndroidPaymentAppFinderUnitTest (see luci bisection comment)
    >
    > Bug: 389166093
    > Original change's description:
    > > Delete all @MockedInTests except for 2
    > >
    > > It's no longer needed as of b/389166093... Except for interfaces that
    > > are extended by other interfaces, where it's needed due to a
    > > hard-to-fix quirk in R8 involving synthetic bridges and improper
    > > handling of them by Mockito.
    > >
    > > Bug: 389166093
    > > Change-Id: I9100f386f0238e185efbce77f2ebbe3c3c05621d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378168
    > > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Sam Maier <smaier@chromium.org>
    > > Commit-Queue: Sam Maier <smaier@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1451832}
    >
    > No-Try: true
    > Bug: 389166093
    > Change-Id: Ifba4902a428b5b98263f29ac499f1fb4fb83167b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495487
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1452590}
    
    Bug: 389166093
    Change-Id: I46f1da4e2a5652787542f5a36372387a3e93fac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495121
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452747}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherSearchStation.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherSearchStation.java
@@ -12,6 +12,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withParentIndex;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.allOf;
 
 import static org.chromium.base.test.transit.ViewSpec.viewSpec;
@@ -26,6 +27,7 @@ import org.chromium.base.test.transit.Station;
 import org.chromium.base.test.transit.ViewElement;
 import org.chromium.base.test.transit.ViewSpec;
 import org.chromium.chrome.browser.omnibox.UrlBar;
+import org.chromium.chrome.browser.omnibox.suggestions.base.BaseSuggestionView;
 import org.chromium.chrome.browser.searchwidget.SearchActivity;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
@@ -80,7 +82,7 @@ public class TabSwitcherSearchStation extends Station<SearchActivity> {
                 .descendant(
                         allOf(
                                 withParentIndex(index),
-                                withClassName(containsString("BaseSuggestionView")),
+                                withClassName(equalTo(BaseSuggestionView.class.getName())),
                                 hasDescendant(
                                         allOf(
                                                 withId(R.id.line_1),
