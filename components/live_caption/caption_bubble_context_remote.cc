93f6d2c46379cfa22c4037ea2d49bda6408c578b
Ian Wells <iwells@chromium.org>
2025-04-28 14:36:56 -0700

    Avoid overlapping the caption bubble with a very small context area
    When using the glic WebUI with live captions enabled, the bubble is
    close to the same size as glic and gets positioned so that it overlaps
    a significant part of glic. Depending on which UI is higher in Z order,
    one or the other will be badly obscured.
    
    This change detects this scenario when positioning the caption bubble
    and moves it to be entirely below the context rect if there's room on
    the screen, or above otherwise. 40% was chosen as
    it was the most conservative value that reliably caught the harmful
    overlap.
    
    Also, only do this for non-tab WebContents.
    
    Screenshot: shortn/_IZJANr104N
    
    AX-Relnotes: n/a.
    Bug: 396475423
    Change-Id: I519596999ab459742c60996bb0947d69b7ea88df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489906
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452919}

--- components/live_caption/caption_bubble_context_remote.cc
+++ components/live_caption/caption_bubble_context_remote.cc
@@ -95,6 +95,10 @@ bool CaptionBubbleContextRemote::IsActivatable() const {
   return true;
 }
 
+bool CaptionBubbleContextRemote::ShouldAvoidOverlap() const {
+  return false;
+}
+
 std::unique_ptr<CaptionBubbleSessionObserver>
 CaptionBubbleContextRemote::GetCaptionBubbleSessionObserver() {
   auto session_observer =
