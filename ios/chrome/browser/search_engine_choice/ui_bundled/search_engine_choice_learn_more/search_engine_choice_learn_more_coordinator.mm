94ae84feeb408aef2fd03f8062f3d5a23dabfee4
Federica Germinario <fedegermi@google.com>
2025-04-28 02:49:27 -0700

    [ios] Remove NotFatalUntil::M127 from search_engine code
    These CHECK(...) are already unconditional (we are in M137), so
    base::NotFatalUntil::M127 can be removed.
    
    Bug: none
    Change-Id: I9c0b41cf2e11ee1c2c74c31d6f40e6ad8414175b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491389
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452493}

--- ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_learn_more/search_engine_choice_learn_more_coordinator.mm
+++ ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_learn_more/search_engine_choice_learn_more_coordinator.mm
@@ -76,7 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)learnMoreDone:
     (SearchEngineChoiceLearnMoreViewController*)viewController {
-  CHECK_EQ(_viewController, viewController, base::NotFatalUntil::M127);
+  CHECK_EQ(_viewController, viewController);
   __weak __typeof(self) weakSelf = self;
   [_viewController dismissViewControllerAnimated:YES
                                       completion:^() {
