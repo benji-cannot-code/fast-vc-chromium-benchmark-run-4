8ddc27d9113a129e3438a0b97b037d80ae7bee7d
Ivan Murashov <ivan.murashov@lge.com>
2025-04-28 12:37:54 -0700

    IWYU: Add missing include for base::StringPrintf usage in installed_loader.cc
    In the file chrome/browser/extensions/installed_loader.cc the
    method base::StringPrintf is used without corresponding include.
    Added missing include.
    
    Bug: 404917682
    Change-Id: I50ada73f3bb0b8859f31d680cf6c1774b6374b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491931
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1452835}

--- chrome/browser/extensions/installed_loader.cc
+++ chrome/browser/extensions/installed_loader.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notimplemented.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
