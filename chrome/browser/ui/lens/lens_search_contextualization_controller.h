dbf281e69bca9240a269bce29df7e1e13a4facc7
Duncan Mercer <mercerd@google.com>
2025-04-28 10:04:33 -0700

    [lens-rearch][7/n] Add contextualization controller
    Also includes basic state machine to be used later.
    
    Bug: 403573362, 404941800
    Change-Id: I1bc5327d951278b123c9540251a7539b32689899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490488
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452714}

---
+++ chrome/browser/ui/lens/lens_search_contextualization_controller.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_LENS_LENS_SEARCH_CONTEXTUALIZATION_CONTROLLER_H_
+#define CHROME_BROWSER_UI_LENS_LENS_SEARCH_CONTEXTUALIZATION_CONTROLLER_H_
+
+// Controller responsible for handling contextualization logic for Lens flows.
+// This includes grabbing content related to the page and issuing Lens requests
+// so searchbox requests are contextualized.
+class LensSearchContextualizationController {
+ public:
+  LensSearchContextualizationController();
+  ~LensSearchContextualizationController();
+
+  // Internal state machine. States are mutually exclusive. Exposed for testing.
+  enum class State {
+    // This is the default state. The contextualization flow is not currently
+    // active.
+    kOff,
+
+    // TODO(crbug.com/335516480): Implement suspended state.
+    kSuspended,
+  };
+  State state() { return state_; }
+
+ private:
+  // The current state of the contextualization flow.
+  State state_ = State::kOff;
+};
+#endif  // CHROME_BROWSER_UI_LENS_LENS_SEARCH_CONTEXTUALIZATION_CONTROLLER_H_
