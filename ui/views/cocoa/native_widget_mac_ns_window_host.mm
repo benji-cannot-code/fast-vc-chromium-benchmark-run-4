bfe9c2a2eeb8d3f9b6e8f3ab675bef882ee9a2df
lloydhuang <bzkirto@gmail.com>
2025-04-28 09:48:10 -0700

    call OnNativeWidgetWindowShowStateChanged on mac WindowShowState change
    On Windows and Linux platforms, when WindowShowState changes
    (e.g., maximize/minimize), There will be corresponding logic to call
    Widget::OnNativeWidgetWindowShowStateChanged.
    However, on macOS platform, this method is only called when minimizing
    the window, but not when entering fullscreen or zoom states
    (which correspond to maximize/minimize operations on Windows).
    This commit supplements the necessary invocation logic for these
    scenarios.
    
    Bug: 414112255
    Change-Id: If83312cbdae0fba79d0aea2f5de3853dc80b32d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490932
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452701}

--- ui/views/cocoa/native_widget_mac_ns_window_host.mm
+++ ui/views/cocoa/native_widget_mac_ns_window_host.mm
@@ -1351,6 +1351,10 @@ void NativeWidgetMacNSWindowHost::OnWindowFullscreenTransitionComplete(
 
   ui::NSWindowFullscreenNotificationWaiter::NotifyFullscreenTransitionComplete(
       native_widget_mac_->GetNativeWindow(), actual_fullscreen_state);
+
+  if (Widget* widget = GetWidget()) {
+    widget->OnNativeWidgetWindowShowStateChanged();
+  }
 }
 
 void NativeWidgetMacNSWindowHost::OnWindowMiniaturizedChanged(
@@ -1363,6 +1367,9 @@ void NativeWidgetMacNSWindowHost::OnWindowMiniaturizedChanged(
 
 void NativeWidgetMacNSWindowHost::OnWindowZoomedChanged(bool zoomed) {
   is_zoomed_ = zoomed;
+  if (Widget* widget = GetWidget()) {
+    widget->OnNativeWidgetWindowShowStateChanged();
+  }
 }
 
 void NativeWidgetMacNSWindowHost::OnWindowDisplayChanged(
