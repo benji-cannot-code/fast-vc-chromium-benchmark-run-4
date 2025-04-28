62fbfd427d04913925c77b4be3f2a834cb1bb4b1
Takuto Ikuta <tikuta@chromium.org>
2025-04-28 09:42:52 -0700

    [iOS] Remove unused include from browser_coordinator.mm
    https://crrev.com/c/6422270 removed references to
    `optimization_guide::proto`.
    
    Bug: 408006823,  402356473
    Change-Id: I846d34e894b30cd288e480b85c8ecdca1a1db324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490972
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452698}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/feature_engagement/public/tracker.h"
 #import "components/infobars/core/infobar.h"
 #import "components/infobars/core/infobar_manager.h"
-#import "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #import "components/password_manager/core/browser/ui/credential_ui_entry.h"
 #import "components/password_manager/core/common/password_manager_features.h"
 #import "components/plus_addresses/features.h"
