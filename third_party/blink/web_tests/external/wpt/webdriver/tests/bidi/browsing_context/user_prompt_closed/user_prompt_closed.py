cbf283fc6632fb4f00c7253701c944a09ce92ce0
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-04-28 08:50:52 -0700

    Import wpt@213112c36627c1565e08781cfdeca44ef826be93
    https://github.com/web-platform-tests/wpt/compare/cae0369c7...213112c36
    
    Using wpt-import in Chromium f37f22d2073bed54e4c8715da2ded65bc27c7a0d.
    With Chromium commits locally applied on WPT:
      73460f7c05 "Add CookieStore WPT asserting duplicate cookies do [...]
      ea697a752f "CookieStore API WPT: Already expired partitioned [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I75775325a390a4b960865e6ca35f07103facb089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494172
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1452653}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/user_prompt_closed/user_prompt_closed.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/user_prompt_closed/user_prompt_closed.py
@@ -319,7 +319,7 @@ async def test_iframe(
     event = await wait_for_future_safe(on_prompt_closed)
 
     assert event == {
-        "context": new_tab["context"],
+        "context": frame["context"],
         "accepted": True,
         "type": "alert",
     }
--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/emulation/set_geolocation_override/invalid.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/emulation/set_geolocation_override/invalid.py
@@ -115,6 +115,18 @@ async def test_params_coordinates_latitude_invalid_type(bidi_session, top_contex
         )
 
 
+@pytest.mark.parametrize("value", [-90.1, 90.1])
+async def test_params_coordinates_latitude_invalid_value(bidi_session, top_context, value):
+    with pytest.raises(error.InvalidArgumentException):
+        await bidi_session.emulation.set_geolocation_override(
+            contexts=[top_context["context"]],
+            coordinates=CoordinatesOptions(
+                latitude=value,
+                longitude=10,
+            ),
+        )
+
+
 async def test_params_coordinates_longitude_missing(bidi_session, top_context):
     with pytest.raises(error.InvalidArgumentException):
         await bidi_session.emulation.set_geolocation_override(
@@ -137,6 +149,18 @@ async def test_params_coordinates_longitude_invalid_type(bidi_session, top_conte
         )
 
 
+@pytest.mark.parametrize("value", [-180.5, 180.5])
+async def test_params_coordinates_longitude_invalid_value(bidi_session, top_context, value):
+    with pytest.raises(error.InvalidArgumentException):
+        await bidi_session.emulation.set_geolocation_override(
+            contexts=[top_context["context"]],
+            coordinates=CoordinatesOptions(
+                latitude=10,
+                longitude=value,
+            ),
+        )
+
+
 @pytest.mark.parametrize("value", [False, "foo", [], {}])
 async def test_params_coordinates_accuracy_invalid_type(bidi_session, top_context, value):
     with pytest.raises(error.InvalidArgumentException):
@@ -146,6 +170,18 @@ async def test_params_coordinates_accuracy_invalid_type(bidi_session, top_contex
                 latitude=10,
                 longitude=10,
                 accuracy=value,
+            ),
+        )
+
+
+async def test_params_coordinates_accuracy_invalid_value(bidi_session, top_context):
+    with pytest.raises(error.InvalidArgumentException):
+        await bidi_session.emulation.set_geolocation_override(
+            contexts=[top_context["context"]],
+            coordinates=CoordinatesOptions(
+                latitude=10,
+                longitude=10,
+                accuracy=-1,
             ),
         )
 
@@ -177,6 +213,19 @@ async def test_params_coordinates_altitude_accuracy_invalid_type(bidi_session, t
         )
 
 
+async def test_params_coordinates_altitude_accuracy_invalid_value(bidi_session, top_context):
+    with pytest.raises(error.InvalidArgumentException):
+        await bidi_session.emulation.set_geolocation_override(
+            contexts=[top_context["context"]],
+            coordinates=CoordinatesOptions(
+                latitude=10,
+                longitude=10,
+                altitude=10,
+                altitude_accuracy=-1,
+            ),
+        )
+
+
 async def test_params_coordinates_altitude_accuracy_without_altitude(bidi_session, top_context):
     with pytest.raises(error.InvalidArgumentException):
         await bidi_session.emulation.set_geolocation_override(
@@ -202,6 +251,19 @@ async def test_params_coordinates_heading_invalid_type(bidi_session, top_context
         )
 
 
+@pytest.mark.parametrize("value", [-0.5, 360, 360.5])
+async def test_params_coordinates_heading_invalid_value(bidi_session, top_context, value):
+    with pytest.raises(error.InvalidArgumentException):
+        await bidi_session.emulation.set_geolocation_override(
+            contexts=[top_context["context"]],
+            coordinates=CoordinatesOptions(
+                latitude=10,
+                longitude=10,
+                heading=value,
+            ),
+        )
+
+
 @pytest.mark.parametrize("value", [False, "foo", [], {}])
 async def test_params_coordinates_speed_invalid_type(bidi_session, top_context, value):
     with pytest.raises(error.InvalidArgumentException):
@@ -211,6 +273,18 @@ async def test_params_coordinates_speed_invalid_type(bidi_session, top_context, 
                 latitude=10,
                 longitude=10,
                 speed=value,
+            ),
+        )
+
+
+async def test_params_coordinates_speed_invalid_value(bidi_session, top_context):
+    with pytest.raises(error.InvalidArgumentException):
+        await bidi_session.emulation.set_geolocation_override(
+            contexts=[top_context["context"]],
+            coordinates=CoordinatesOptions(
+                latitude=10,
+                longitude=10,
+                speed=-1.5,
             ),
         )
 
