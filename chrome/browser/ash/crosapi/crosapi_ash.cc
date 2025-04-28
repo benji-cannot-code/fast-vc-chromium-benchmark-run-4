f4eb64c84b22dc6694633391833bafd3a003275b
Di Wu <diwux@google.com>
2025-04-28 16:27:33 -0700

    Delete policy_service_ash
    policy_service_ash was solely for Lacros. Delete it.
    
    Bug: b:354842935
    Test: tryjob
    Change-Id: If2e7953d60215649665c920e09105105acf4dc16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491952
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452988}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/networking_private_ash.h"
 #include "chrome/browser/ash/crosapi/parent_access_ash.h"
 #include "chrome/browser/ash/crosapi/payment_app_instance_ash.h"
-#include "chrome/browser/ash/crosapi/policy_service_ash.h"
 #include "chrome/browser/ash/crosapi/remoting_ash.h"
 #include "chrome/browser/ash/crosapi/structured_metrics_service_ash.h"
 #include "chrome/browser/ash/crosapi/vpn_service_ash.h"
@@ -131,7 +130,6 @@ CrosapiAsh::CrosapiAsh()
       networking_private_ash_(std::make_unique<NetworkingPrivateAsh>()),
       parent_access_ash_(std::make_unique<ParentAccessAsh>()),
       payment_app_instance_ash_(std::make_unique<PaymentAppInstanceAsh>()),
-      policy_service_ash_(std::make_unique<PolicyServiceAsh>()),
       telemetry_diagnostic_routine_service_ash_(
           std::make_unique<ash::TelemetryDiagnosticsRoutineServiceAsh>()),
       telemetry_event_service_ash_(
@@ -350,11 +348,6 @@ void CrosapiAsh::BindPaymentAppInstance(
   Profile* profile = ProfileManager::GetPrimaryUserProfile();
   payment_app_instance_ash_->Initialize(profile);
   payment_app_instance_ash_->BindReceiver(std::move(receiver));
-}
-
-void CrosapiAsh::BindPolicyService(
-    mojo::PendingReceiver<mojom::PolicyService> receiver) {
-  policy_service_ash_->BindReceiver(std::move(receiver));
 }
 
 void CrosapiAsh::BindPrintPreviewCrosDelegate(
