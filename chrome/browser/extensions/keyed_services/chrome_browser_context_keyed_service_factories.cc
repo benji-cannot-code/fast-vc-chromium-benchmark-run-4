98611129cff7ea027f9f59ad38d9cad778d5bc23
James Cook <jamescook@chromium.org>
2025-04-28 16:25:49 -0700

    Register ExtensionGarbageCollectorFactory on startup on desktop Android
    This is a separate CL because registering the factory will cause the
    ExtensionGarbageCollector to be created with the profile, which will
    trigger the garbage collection process. I wanted to do it in a
    separate CL in case it causes problems.
    
    Bug: 414398434
    Change-Id: If32bc1a547a8b1e7e51b3028846886a3ee1c743e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495004
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452987}

--- chrome/browser/extensions/keyed_services/chrome_browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/keyed_services/chrome_browser_context_keyed_service_factories.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/cws_info_service_factory.h"
 #include "chrome/browser/extensions/error_console/error_console_factory.h"
 #include "chrome/browser/extensions/extension_allowlist_factory.h"
+#include "chrome/browser/extensions/extension_garbage_collector_factory.h"
 #include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/extension_web_ui_override_registrar.h"
 #include "chrome/browser/extensions/external_install_manager_factory.h"
@@ -32,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/chrome_app_icon_service_factory.h"
 #include "chrome/browser/extensions/extension_action_dispatcher.h"
 #include "chrome/browser/extensions/extension_error_controller_factory.h"
-#include "chrome/browser/extensions/extension_garbage_collector_factory.h"
 #include "chrome/browser/extensions/extension_gcm_app_handler.h"
 #include "chrome/browser/extensions/extension_sync_service_factory.h"
 #include "chrome/browser/extensions/extension_web_ui_override_registrar.h"
@@ -63,6 +63,7 @@ void EnsureChromeBrowserContextKeyedServiceFactoriesBuilt() {
   extensions::CWSInfoServiceFactory::GetInstance();
   extensions::ErrorConsoleFactory::GetInstance();
   extensions::ExtensionAllowlistFactory::GetInstance();
+  extensions::ExtensionGarbageCollectorFactory::GetInstance();
   extensions::ExtensionManagementFactory::GetInstance();
   extensions::ExtensionUpdaterFactory::GetInstance();
   extensions::ExtensionWebUIOverrideRegistrar::GetFactoryInstance();
@@ -81,7 +82,6 @@ void EnsureChromeBrowserContextKeyedServiceFactoriesBuilt() {
   extensions::ChromeAppIconServiceFactory::GetInstance();
   extensions::ExtensionActionDispatcher::GetFactoryInstance();
   extensions::ExtensionErrorControllerFactory::GetInstance();
-  extensions::ExtensionGarbageCollectorFactory::GetInstance();
   extensions::ExtensionGCMAppHandler::GetFactoryInstance();
   extensions::ManifestV2ExperimentManager::GetFactory();
   extensions::MenuManagerFactory::GetInstance();
