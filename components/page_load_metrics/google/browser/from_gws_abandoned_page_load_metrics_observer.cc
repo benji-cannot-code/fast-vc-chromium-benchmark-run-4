3412c6815db45e2601c93ae8978e9634e6231593
Keita Suzuki <suzukikeita@chromium.org>
2025-04-27 17:59:47 -0700

    Add trace trigger to FromGwsAbandonedPageLoadMetricsObserver
    This CL adds a trace trigger to
    `FromGwsAbandonedPageLoadMetricsObserver` to check for anomalies in
    navigations using from Gws navigations.
    
    Bug: 390216631
    Change-Id: I41f98a2c20048bed3860c1c0714e7d3124c9f9d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6493966
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452373}

--- components/page_load_metrics/google/browser/from_gws_abandoned_page_load_metrics_observer.cc
+++ components/page_load_metrics/google/browser/from_gws_abandoned_page_load_metrics_observer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/time/time.h"
+#include "base/trace_event/named_trigger.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
 #include "components/page_load_metrics/google/browser/google_url_util.h"
 #include "content/public/browser/navigation_handle.h"
@@ -41,6 +42,10 @@ FromGWSAbandonedPageLoadMetricsObserver::OnStart(
   if (!page_load_metrics::IsGoogleSearchResultUrl(currently_committed_url)) {
     return ObservePolicy::STOP_OBSERVING;
   }
+
+  // Emit a trigger to allow trace collection tied to from gws navigations.
+  base::trace_event::EmitNamedTrigger("from-gws-navigation-start");
+
   category_parameter_id_ =
       page_load_metrics::GetCategoryIdFromUrl(navigation_handle->GetURL());
   impression_ = navigation_handle->GetImpression();
