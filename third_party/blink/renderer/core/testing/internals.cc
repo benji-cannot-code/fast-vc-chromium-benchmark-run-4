0b9fbe40589bdfc53d418c35f1d74ebcd840a7ff
Jason <cgqaq@chromium.org>
2025-04-28 03:05:47 -0700

    Removed allow_unsafe_buffers from internals.cc
    Bug: 351564777
    Change-Id: I4ff53258df6bb23eeb02051b4c2ce4c4a49c6e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489892
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452502}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -25,11 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/testing/internals.h"
 
 #include <atomic>
@@ -3153,7 +3148,7 @@ DOMArrayBuffer* Internals::serializeObject(
   DOMArrayBuffer* buffer = DOMArrayBuffer::CreateUninitializedOrNull(
       base::checked_cast<uint32_t>(span.size()), sizeof(uint8_t));
   if (buffer)
-    memcpy(buffer->Data(), span.data(), span.size());
+    buffer->ByteSpan().copy_from(span);
   return buffer;
 }
 
