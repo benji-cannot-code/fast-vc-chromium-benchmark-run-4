7f659b3429d0e59ffd7953e339a1eda50a944d39
Asami Doi <asamidoi@chromium.org>
2025-04-28 07:55:43 -0700

    [ios] Replace all strong NSString* with copy
    This CL replaces all strong NSStrings with a copy keyword. Also,
    manually checked all updates and call the copy method if necessary.
    
    for file in $(git grep -l "@property\(.*strong.*\) NSString" ./ios); do
      sed -i '' -e 's/\(@property(.*\), strong\(.*)\)\( NSString\)/\1, copy\2\3/' "$file"
      sed -i '' -e 's/\(@property(.*\)strong, \(.*)\)\(NSString\)/\1copy, \2\3/' "$file"
    done
    
    This CL was uploaded by git cl split.
    
    R=marq@chromium.org
    
    Bug: 413648986
    Change-Id: Ic0c66603049bab7b8338e13cf39c5688ae0ac0b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494827
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452620}

--- ios/chrome/browser/shared/coordinator/scene/scene_util_test_support.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_util_test_support.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (instancetype)initWithIdentifier:(NSString*)identifier;
 
-@property(nonatomic, strong, readonly) NSString* persistentIdentifier;
+@property(nonatomic, copy, readonly) NSString* persistentIdentifier;
 
 @property(nonatomic, copy) NSDictionary<NSString*, id>* userInfo;
 
