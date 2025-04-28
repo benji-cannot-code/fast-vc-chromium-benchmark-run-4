4d13026e29c137916335a32b774a8ff7c8f6dd1e
Robbie Gibson <rkgibson@google.com>
2025-04-28 10:04:23 -0700

    [iOS] Add basic API and observation for Home Background Customization
    This CL adds basic set, get, and observation methods for the
    HomeBackgroundCustomizationService. There is still a lot more work to
    do regarding persisting the data to disk (not just in memory) and
    supporting the other custom background types like colors and user-
    uploaded images, but starting here allows setting up the end-to-end
    flow.
    
    Bug: 411453184, 411453876
    Change-Id: I536aca07ad083c9a145719c2bdd57efca5b254fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489177
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452713}

---
+++ ios/chrome/browser/home_customization/model/home_background_customization_service_observer.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_HOME_CUSTOMIZATION_MODEL_HOME_BACKGROUND_CUSTOMIZATION_SERVICE_OBSERVER_H_
+#define IOS_CHROME_BROWSER_HOME_CUSTOMIZATION_MODEL_HOME_BACKGROUND_CUSTOMIZATION_SERVICE_OBSERVER_H_
+
+#include "base/observer_list_types.h"
+
+// Observer for HomeBackgroundCustomizationService.
+class HomeBackgroundCustomizationServiceObserver
+    : public base::CheckedObserver {
+ public:
+  // Called when the background is changed.
+  virtual void OnBackgroundChanged() = 0;
+};
+
+#endif  // IOS_CHROME_BROWSER_HOME_CUSTOMIZATION_MODEL_HOME_BACKGROUND_CUSTOMIZATION_SERVICE_OBSERVER_H_
