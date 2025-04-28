06f4d7f2cc6b2f3e3b7053f935736944881e6c4b
Asami Doi <asamidoi@chromium.org>
2025-04-28 07:40:16 -0700

    [ios] Replace all strong NSString* with copy
    This CL replaces all strong NSStrings with a copy keyword. Also,
    manually checked all updates and call the copy method if necessary.
    
    for file in $(git grep -l "@property\(.*strong.*\) NSString" ./ios); do
      sed -i '' -e 's/\(@property(.*\), strong\(.*)\)\( NSString\)/\1, copy\2\3/' "$file"
      sed -i '' -e 's/\(@property(.*\)strong, \(.*)\)\(NSString\)/\1copy, \2\3/' "$file"
    done
    
    This CL was uploaded by git cl split.
    
    R=alionadangla@chromium.org
    
    Bug: 413648986
    Change-Id: Id9e2c4f9251f1d9a8f1a4a63e28dbc82718c7dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494831
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452612}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/tab_group_activity_summary_cell.h
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/tab_group_activity_summary_cell.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, weak) id<TabGroupActivitySummaryCellDelegate> delegate;
 
 // The text of summary.
-@property(nonatomic, strong) NSString* text;
+@property(nonatomic, copy) NSString* text;
 
 @end
 
