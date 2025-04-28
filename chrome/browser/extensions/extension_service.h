b1cbc613d11005836a9bd3762547c496019cb2cc
Solomon Kinard <solomonkinard@chromium.org>
2025-04-28 12:08:50 -0700

    Extensions: CodeHealth: Spell check extension_service.h
    Bug: chromium:346952289
    Change-Id: I0d9cc8eed61615833d59e5523776be10410fe249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495568
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452816}

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -327,7 +327,7 @@ class ExtensionService : public ExtensionServiceInterface,
   // Hold the set of pending extensions. Not owned.
   raw_ptr<PendingExtensionManager> pending_extension_manager_ = nullptr;
 
-  // Manages external providers. Not ownedd.
+  // Manages external providers. Not owned.
   raw_ptr<ExternalProviderManager> external_provider_manager_ = nullptr;
 
   // Signaled when all extensions are loaded.
