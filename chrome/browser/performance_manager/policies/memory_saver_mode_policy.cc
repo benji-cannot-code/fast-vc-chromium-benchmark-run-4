052943ab2e93c85cb8493d173c3cc7e7e8bf79fb
Anthony Vallee-Dubois <anthonyvd@google.com>
2025-04-28 11:53:23 -0700

    Fix memory saver mode max revisits params
    The param for the "conservative" and "aggressive" settings was reversed.
    
    Bug: None
    Change-Id: I6dcfdfab685b59e69434252e0952540f24c343d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488235
    Auto-Submit: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1452798}

--- chrome/browser/performance_manager/policies/memory_saver_mode_policy.cc
+++ chrome/browser/performance_manager/policies/memory_saver_mode_policy.cc
@@ -216,11 +216,11 @@ base::TimeDelta MemorySaverModePolicy::GetTimeBeforeDiscardForCurrentMode()
 int MemorySaverModePolicy::GetMaxNumRevisitsForCurrentMode() const {
   switch (mode_) {
     case MemorySaverModeAggressiveness::kConservative:
-      return 15;
+      return 5;
     case MemorySaverModeAggressiveness::kMedium:
       return 15;
     case MemorySaverModeAggressiveness::kAggressive:
-      return 5;
+      return 15;
   }
   NOTREACHED();
 }
