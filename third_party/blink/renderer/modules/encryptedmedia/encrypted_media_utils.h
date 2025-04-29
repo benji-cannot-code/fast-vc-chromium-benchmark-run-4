ef905f74d757203124efaccda862faed7f5ae309
Elias Klim <elklm@chromium.org>
2025-04-29 04:06:10 -0700

    Replace broken goo.gl link with a correct link.
    This CL updates the message produced by permission policy with a
    new link. The old link https://goo.gl/EuHzyv pointed to a deprecated
    page.
    
    Bug: 413625698
    Change-Id: I520178772d0792a0cd3f26348a304dd11f4fb1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491906
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453158}

--- third_party/blink/renderer/modules/encryptedmedia/encrypted_media_utils.h
+++ third_party/blink/renderer/modules/encryptedmedia/encrypted_media_utils.h
@@ -50,7 +50,7 @@ struct MediaKeysConfig {
 
 constexpr const char* kEncryptedMediaPermissionsPolicyConsoleWarning =
     "Encrypted Media access has been blocked because of a Feature Policy "
-    "applied to the current document. See https://goo.gl/EuHzyv for more "
+    "applied to the current document. See https://crbug.com/414348233 for more "
     "details.";
 
 class ExecutionContext;
