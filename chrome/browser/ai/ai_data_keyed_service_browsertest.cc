3b9eefb0d8dfef323713ad5466c7d0788f91a453
Ryan Sturm <ryansturm@chromium.org>
2025-04-29 11:01:44 -0700

    Allowlisting AI Extension.
    The extension: https://issues.chromium.org/414437025.
    
    Bug: 414437025
    Change-Id: I83aa5257be53ba5a0fc88aa802da606a0bb66a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495990
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453396}

--- chrome/browser/ai/ai_data_keyed_service_browsertest.cc
+++ chrome/browser/ai/ai_data_keyed_service_browsertest.cc
@@ -144,7 +144,7 @@ IN_PROC_BROWSER_TEST_F(AiDataKeyedServiceBrowserTest,
   std::vector<std::string> expected_allowlisted_extensions = {
       "hpkopmikdojpadgmioifjjodbmnjjjca", "bgbpcgpcobgjpnpiginpidndjpggappi",
       "eefninhhiifgcimjkmkongegpoaikmhm", "fjhpgileahdpnmfmaggobehbipojhlce",
-      "abdciamfdmknaeggbnmafmbdfdmhfgfa"};
+      "abdciamfdmknaeggbnmafmbdfdmhfgfa", "fiamdfnbelfkjlacoaeiclobkdmckaoa"};
 
   for (const auto& extension_id : expected_allowlisted_extensions) {
     EXPECT_TRUE(
@@ -164,7 +164,7 @@ IN_PROC_BROWSER_TEST_F(AiDataKeyedServiceBrowserTest,
   std::vector<std::string> expected_not_allowlisted_extensions = {
       "hpkopmikdojpadgmioifjjodbmnjjjca", "bgbpcgpcobgjpnpiginpidndjpggappi",
       "eefninhhiifgcimjkmkongegpoaikmhm", "fjhpgileahdpnmfmaggobehbipojhlce",
-      "abdciamfdmknaeggbnmafmbdfdmhfgfa"};
+      "abdciamfdmknaeggbnmafmbdfdmhfgfa", "fiamdfnbelfkjlacoaeiclobkdmckaoa"};
   for (const auto& extension_id : expected_not_allowlisted_extensions) {
     EXPECT_FALSE(
         AiDataKeyedService::IsExtensionAllowlistedForActions(extension_id));
@@ -445,7 +445,8 @@ IN_PROC_BROWSER_TEST_F(AiDataKeyedServiceBrowserTestWithBlocklistedExtensions,
                        BlockedExtensionList) {
   std::vector<std::string> expected_allowlisted_extensions = {
       "bgbpcgpcobgjpnpiginpidndjpggappi", "eefninhhiifgcimjkmkongegpoaikmhm",
-      "fjhpgileahdpnmfmaggobehbipojhlce", "abdciamfdmknaeggbnmafmbdfdmhfgfa"};
+      "fjhpgileahdpnmfmaggobehbipojhlce", "abdciamfdmknaeggbnmafmbdfdmhfgfa",
+      "fiamdfnbelfkjlacoaeiclobkdmckaoa"};
 
   EXPECT_FALSE(AiDataKeyedService::IsExtensionAllowlistedForData(
       "hpkopmikdojpadgmioifjjodbmnjjjca"));
@@ -478,7 +479,8 @@ IN_PROC_BROWSER_TEST_F(
       "bgbpcgpcobgjpnpiginpidndjpggappi",
       "eefninhhiifgcimjkmkongegpoaikmhm",
       "fjhpgileahdpnmfmaggobehbipojhlce",
-      "abdciamfdmknaeggbnmafmbdfdmhfgfa"};
+      "abdciamfdmknaeggbnmafmbdfdmhfgfa",
+      "fiamdfnbelfkjlacoaeiclobkdmckaoa"};
 
   for (const auto& extension : expected_allowlisted_extensions) {
     EXPECT_TRUE(AiDataKeyedService::IsExtensionAllowlistedForData(extension));
