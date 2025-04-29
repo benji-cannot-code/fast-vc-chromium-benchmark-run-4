15cbe2cdabb11b0fee064b8ae925362e9e0be02a
amarjotgill <amarjotgill@google.com>
2025-04-29 12:03:19 -0700

    remove SFV crate
    If decided to continue sfv work the crate will be readded.
    
    Bug: 413385487
    Change-Id: I9889582c6f38c02c1bd35519b6b69cd70bf7f3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496395
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Amarjot Gill <amarjotgill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453441}

--- third_party/rust/chromium_crates_io/vendor/rust_decimal-v1/src/postgres.rs
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Shared
-mod common;
-
-#[cfg(feature = "db-diesel-postgres")]
-mod diesel;
-
-#[cfg(any(feature = "db-postgres", feature = "db-tokio-postgres"))]
-mod driver;
