584e750acd2fa3843cc11508fb203bbb28705c4e
Ho Cheung <hocheung@chromium.org>
2025-04-28 17:20:47 -0700

    [remoting] Fix unsafe buffer usage in ChromiumSocketFactoryUnitTest
    Convert unsafe pointer arithmetic to explicit string construction with
    type-casting for UDP packet payload handling.
    
    Bug: 40285824
    Change-Id: I73b4cdf863ab59227d813e80ccda3dacef3e5dd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491231
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453017}

--- remoting/protocol/chromium_socket_factory_unittest.cc
+++ remoting/protocol/chromium_socket_factory_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "remoting/protocol/chromium_socket_factory.h"
 
 #include <stddef.h>
@@ -80,8 +75,8 @@ class ChromiumSocketFactoryTest : public testing::Test,
     EXPECT_EQ(socket, socket_.get());
 
     received_packets_.push_back(
-        {packet.payload().data(),
-         packet.payload().data() + packet.payload().size()});
+        std::string(reinterpret_cast<const char*>(packet.payload().data()),
+                    packet.payload().size()));
     last_address_ = packet.source_address();
     last_packet_time_ = packet.arrival_time()->us();
     if (received_packets_.size() >= expected_packet_count_) {
