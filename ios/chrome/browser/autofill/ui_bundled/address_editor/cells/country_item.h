3a3e2699b463bb26b36193acb98c6973d9f3f800
Sylvain Defresne <sdefresne@chromium.org>
2025-04-29 10:22:35 -0700

    [ios] Fix property incorrectly marked as `assign`
    Using `assign` for a property storing a reference counted value is
    interpreted as a non-zeroing weak pointer i.e. __unsafe_unretained.
    It would mean that the property will not retain the value and will
    be become dangling as soon as the object is deallocated.
    
    Fix property in CountryItem to instead use copy (as it appears
    this code is not expecting a weak pointer here and NSString has
    a mutable variant).
    
    Bug: 411386579
    Change-Id: Ie1450b2c821dde1ff46acd0e60126b666fa6846b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494635
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453366}

--- ios/chrome/browser/autofill/ui_bundled/address_editor/cells/country_item.h
+++ ios/chrome/browser/autofill/ui_bundled/address_editor/cells/country_item.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface CountryItem : TableViewTextItem
 
 // The Country code.
-@property(nonatomic, assign) NSString* countryCode;
+@property(nonatomic, copy) NSString* countryCode;
 
 @end
 
