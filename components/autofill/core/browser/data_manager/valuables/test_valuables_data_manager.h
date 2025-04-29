b26829ec41c496adf7902a54ae75709333b83e9f
Norge Vizcay <vizcay@google.com>
2025-04-29 07:03:54 -0700

    Add icon images to loyalty card suggestions.
    The CL adds custom icons on loyalty card suggestions. Notably, images
    are extracted from the valuables data manager on Desktop, which has
    access to the image fetcher cache. On Android, the icon URL is set on
    the suggestion and later fetched from the AutofillImageFetcher.java.
    
    The suggestion generator API has been updated to take a valuables data
    manager instead of the list of loyalty cards.
    
    Bug: 404437008
    Change-Id: Iceb57237e5f11f75749860775cceae3936a6c49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494749
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453183}

--- components/autofill/core/browser/data_manager/valuables/test_valuables_data_manager.h
+++ components/autofill/core/browser/data_manager/valuables/test_valuables_data_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_DATA_MANAGER_VALUABLES_TEST_VALUABLES_DATA_MANAGER_H_
 
 #include "components/autofill/core/browser/data_manager/valuables/valuables_data_manager.h"
+#include "components/autofill/core/browser/ui/test_autofill_image_fetcher.h"
 
 namespace autofill {
 
@@ -17,6 +18,13 @@ class TestValuablesDataManager : public ValuablesDataManager {
   TestValuablesDataManager(const TestValuablesDataManager&) = delete;
   TestValuablesDataManager& operator=(const TestValuablesDataManager&) = delete;
   ~TestValuablesDataManager() override;
+
+  // Adds a `url` to `image` mapping to the local `credit_card_art_images_`
+  // cache.
+  void CacheImage(const GURL& url, const gfx::Image& image);
+
+ private:
+  std::unique_ptr<TestAutofillImageFetcher> owned_image_fetcher_;
 };
 
 }  // namespace autofill
