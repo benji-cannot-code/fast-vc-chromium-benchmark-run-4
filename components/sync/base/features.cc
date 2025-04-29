5d4451456fac33532035ce4a284eb72fc00ff07f
Ankush Singh <ankushkush@google.com>
2025-04-29 01:19:31 -0700

    [sync] Enable SyncSupportAlwaysSyncingPriorityPreferences by default
    When enabled, priority prefs are decoupled from the sync user toggle.
    The data type is marked always preferred and thus becomes active even if
    the preferences sync toggle is off. The priority prefs will still only
    be synced with the user toggle on, except for the allowlisted prefs
    which will continue to sync even with the toggle off.
    
    There is one particular concern:
    https://source.chromium.org/chromium/chromium/src/+/main:components/sync_preferences/dual_layer_user_pref_store.cc;drc=c9fa816a02e1359077f4b0ac3714bdb636599a9d;l=482.
    Based on https://crbug.com/412602018#comment4 however, this should be
    safe and no service should really be affected by account priority prefs
    value popping up only after sync init.
    
    Note: This feature is dependent on EnablePreferencesAccountStorage
    launch.
    
    Bug: 412602018
    Change-Id: I4f9a73805cbf16165410bfdc4b5300d869e3cdb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494374
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453117}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -81,7 +81,7 @@ BASE_FEATURE(kReplaceSyncPromosWithSignInPromos,
 
 BASE_FEATURE(kSyncSupportAlwaysSyncingPriorityPreferences,
              "SyncSupportAlwaysSyncingPriorityPreferences",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableBookmarksSelectedTypeOnSigninForTesting,
              "EnableBookmarksSelectedTypeOnSigninForTesting",
