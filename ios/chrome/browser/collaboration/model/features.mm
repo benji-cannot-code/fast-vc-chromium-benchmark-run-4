930b7118f06d004ac34180bb5b2bd84b9ec6075f
Louis Romero <lpromero@google.com>
2025-04-29 07:09:47 -0700

    Don't allow a null CollaborationService
    We should always have a CollaborationService. Either a real one, or an
    empty one, but one nonetheless.
    
    Bug: 409499069
    Change-Id: I9a08caabf35880d356ffa7b375a010692a751f88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494631
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453193}

--- ios/chrome/browser/collaboration/model/features.mm
+++ ios/chrome/browser/collaboration/model/features.mm
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 bool IsSharedTabGroupsJoinEnabled(
     collaboration::CollaborationService* collaboration_service) {
-  if (!collaboration_service || !IsTabGroupSyncEnabled()) {
+  CHECK(collaboration_service);
+  if (!IsTabGroupSyncEnabled()) {
     return false;
   }
   return collaboration_service->GetServiceStatus().IsAllowedToJoin();
@@ -19,7 +20,8 @@ bool IsSharedTabGroupsJoinEnabled(
 
 bool IsSharedTabGroupsCreateEnabled(
     collaboration::CollaborationService* collaboration_service) {
-  if (!collaboration_service || !IsTabGroupSyncEnabled()) {
+  CHECK(collaboration_service);
+  if (!IsTabGroupSyncEnabled()) {
     return false;
   }
   return collaboration_service->GetServiceStatus().IsAllowedToCreate();
