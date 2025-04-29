700e63ab15b990cb2c121e00dc0a5adf6ed62d9d
Tibor Goldschwendt <tiborg@chromium.org>
2025-04-29 09:37:02 -0700

    [ntp] Remove obsolete reverting/confirming background methods
    Those became obsolete when we removed the customize dialog.
    
    Bug: 40894552
    Change-Id: I2a0c8b082a0cbfc3bef87eee921d2a987dcbc8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495661
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453324}

--- chrome/browser/search/background/ntp_custom_background_service.cc
+++ chrome/browser/search/background/ntp_custom_background_service.cc
@@ -282,16 +282,6 @@ void NtpCustomBackgroundService::SetCustomBackgroundInfo(
   if (IsCustomBackgroundDisabledByPolicy()) {
     return;
   }
-  // Store current background info before it is changed so it can be used if
-  // RevertBackgroundChanges is called.
-  if (previous_background_info_ == std::nullopt) {
-    previous_background_info_ = std::make_optional(
-        pref_service_
-            ->GetValue(GetThemePrefNameInMigration(
-                ThemePrefInMigration::kNtpCustomBackgroundDict))
-            .Clone());
-    previous_local_background_ = false;
-  }
 
   bool is_backdrop_collection =
       background_service_ &&
@@ -381,8 +371,6 @@ void NtpCustomBackgroundService::SelectLocalBackgroundImage(
   if (IsCustomBackgroundDisabledByPolicy()) {
     return;
   }
-  previous_background_info_.reset();
-  previous_local_background_ = true;
   base::ThreadPool::PostTaskAndReply(
       FROM_HERE, {base::TaskPriority::USER_VISIBLE, base::MayBlock()},
       base::BindOnce(&CopyFileToProfilePath, path, profile_->GetPath()),
@@ -415,24 +403,6 @@ void NtpCustomBackgroundService::RefreshBackgroundIfNeeded() {
         background_info.Find(kNtpCustomBackgroundResumeToken)->GetString();
     background_service_->FetchNextCollectionImage(collection_id, resume_token);
   }
-}
-
-void NtpCustomBackgroundService::RevertBackgroundChanges() {
-  if (previous_background_info_.has_value()) {
-    pref_service_->Set(GetThemePrefNameInMigration(
-                           ThemePrefInMigration::kNtpCustomBackgroundDict),
-                       *previous_background_info_);
-  }
-  if (previous_local_background_) {
-    SetBackgroundToLocalResource();
-  }
-  previous_background_info_.reset();
-  previous_local_background_ = false;
-}
-
-void NtpCustomBackgroundService::ConfirmBackgroundChanges() {
-  previous_background_info_.reset();
-  previous_local_background_ = false;
 }
 
 std::optional<CustomBackground>
