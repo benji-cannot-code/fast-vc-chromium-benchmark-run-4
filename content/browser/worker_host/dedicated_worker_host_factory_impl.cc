61251cf440c7756ccb3609825e81eb1fec871264
Chris Fredrickson <cfredric@chromium.org>
2025-04-29 08:56:32 -0700

    Revert "Fix crash in DedicatedWorkerHostFactoryImpl"
    This reverts commit 93cc2cb4dffd73e735a5740a85c97c6e8e2816e2.
    
    Reason for revert: This will break the tree; regression test fails after syncing.
    
    Original change's description:
    > Fix crash in DedicatedWorkerHostFactoryImpl
    >
    > The crash occurs because the old code assumes that *every* context that
    > obtains storage access via the Storage Access API also has a matching
    > permission grant. This is not the case for A(B(A)) embedded contexts,
    > since there is no privacy boundary between the inner and outer A
    > documents.
    >
    > Fixed: 409838513
    > Change-Id: I906411da80d91e38b5c7748d6dfcebb92c96a628
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495132
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    > Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1453291}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I2a711659cca0880ee54fcb02d1a6251a61114db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6498747
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453292}

--- content/browser/worker_host/dedicated_worker_host_factory_impl.cc
+++ content/browser/worker_host/dedicated_worker_host_factory_impl.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/devtools/devtools_throttle_handle.h"
 #include "content/browser/devtools/worker_devtools_manager.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
-#include "content/browser/storage_access/storage_access_handle.h"
 #include "content/browser/storage_partition_impl.h"
 #include "content/browser/worker_host/dedicated_worker_host.h"
 #include "content/browser/worker_host/dedicated_worker_service_impl.h"
@@ -100,8 +99,9 @@ void DedicatedWorkerHostFactoryImpl::CreateWorkerHostAndStartScriptLoad(
     RenderFrameHostImpl* ancestor_render_frame_host =
         RenderFrameHostImpl::FromID(ancestor_render_frame_host_id_);
     if (!ancestor_render_frame_host ||
-        !StorageAccessHandle::DoesDocumentHaveStorageAccess(
-            ancestor_render_frame_host)) {
+        ancestor_render_frame_host->GetPermissionStatus(
+            blink::PermissionType::STORAGE_ACCESS_GRANT) !=
+            blink::mojom::PermissionStatus::GRANTED) {
       mojo::ReportBadMessage("DWH_STORAGE_ACCESS_NOT_GRANTED");
       return;
     }
