3777eab89c4282bc66dcbee10e9ae69154eaa128
Yuheng Huang <yuhengh@chromium.org>
2025-04-29 10:24:25 -0700

    Data sharing: fix remove last tab localization strings
    Fix incorrect logic to determine if current user is owner for remove
    last tab UI.
    
    Bug: 413350729
    Change-Id: I69a51b58e1b77fdb3d6fb8afae30f7810d0ab1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496517
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453369}

--- chrome/browser/resources/data_sharing/data_sharing_app.ts
+++ chrome/browser/resources/data_sharing/data_sharing_app.ts
@@ -308,7 +308,7 @@ export function createTranslationMap(): TranslationMap {
           loadTimeData.getStringF('manageGroupTitle', getTabGroupName()),
       [DynamicMessageKey.GET_CLOSE_FLOW_DESCRIPTION_FIRST_PARAGRAPH]:
           (params: DynamicMessageParams) => {
-            if (params.displayedUser!.name! === getGroupOwnerName(params)) {
+            if (params.loggedInUser.role === DataSharingMemberRoleEnum.OWNER) {
               return loadTimeData.getStringF('ownerDeleteLastTimeBody');
             } else {
               return loadTimeData.getStringF('memberDeleteLastTimeBody');
@@ -316,7 +316,7 @@ export function createTranslationMap(): TranslationMap {
           },
       [DynamicMessageKey.GET_CLOSE_FLOW_DESCRIPTION_SECOND_PARAGRAPH]:
           (params: DynamicMessageParams) => {
-            if (params.displayedUser!.name! === getGroupOwnerName(params)) {
+            if (params.loggedInUser.role === DataSharingMemberRoleEnum.OWNER) {
               return loadTimeData.getStringF(
                   'ownerDeleteLastTimeBody2', getTabGroupName());
             } else {
