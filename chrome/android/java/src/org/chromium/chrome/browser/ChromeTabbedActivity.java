30fdaa387b903398ffc00d8a32abef1380391065
Gazal <agazal@google.com>
2025-04-29 16:36:21 -0700

    Open Recent tabs in new tab on Android tablets
    Bug: 405234523
    Test: Build and push clank. Open app Menu -> Recent tabs.
    Change-Id: Iafed2115e030dec7d252c017b1968a0e5995b61a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490736
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453617}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -3263,11 +3263,12 @@ public class ChromeTabbedActivity extends ChromeActivity {
             LoadUrlParams params =
                     new LoadUrlParams(UrlConstants.RECENT_TABS_URL, PageTransition.AUTO_BOOKMARK);
             boolean isInOverviewMode = isInOverviewMode();
-            if (currentTab != null) {
-                currentTab.loadUrl(params);
-            } else {
+            if (currentTab == null || (ChromeFeatureList.sAndroidNativePagesInNewTab.isEnabled()
+                    && ChromeFeatureList.sAndroidNativePagesInNewTabRecentTabsEnabled.getValue())) {
                 getTabCreator(getCurrentTabModel().isIncognito())
                         .createNewTab(params, TabLaunchType.FROM_CHROME_UI, null);
+            } else {
+                currentTab.loadUrl(params);
             }
             if (isInOverviewMode) {
                 mLayoutManager.showLayout(LayoutType.BROWSING, true);
