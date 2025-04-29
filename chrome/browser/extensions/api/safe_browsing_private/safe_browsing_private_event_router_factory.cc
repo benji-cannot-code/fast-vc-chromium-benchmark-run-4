935428ced87f5ec73672319604067362c9d57dd4
David Bertoni <dbertoni@chromium.org>
2025-04-28 17:43:42 -0700

    [Extensions] Clean up includes in the safeBrowserPrivate API.
    These files used to contain a lot of code that's been moved elsewhere.
    This CL removes header files that are no longer needed.
    
    It also removes a couple of data members that are no longer referenced
    and removes some namespace qualifiers that aren't needed.
    
    Bug: 40593486
    Change-Id: If532cc037855db8329a0b3969f646b7edaeba502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490369
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453030}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_factory.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_factory.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "content/public/browser/browser_context.h"
 #include "extensions/browser/extension_system_provider.h"
 #include "extensions/browser/extensions_browser_client.h"
 
