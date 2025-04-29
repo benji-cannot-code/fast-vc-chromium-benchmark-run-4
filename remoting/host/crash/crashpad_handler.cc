3c51eccd6d68bc6670386828f9e5bedb5336acce
Gary Kacmarcik <garykac@chromium.org>
2025-04-29 10:05:31 -0700

    [Chromoting] Simplify crashpad handler code.
    Change-Id: Iee37f492663762201a227cd86194a34a69f93dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496948
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453350}

--- remoting/host/crash/crashpad_handler.cc
+++ remoting/host/crash/crashpad_handler.cc
@@ -3,15 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/memory/scoped_refptr.h"
-#include "components/stability_report/user_stream_data_source_posix.h"
 #include "third_party/crashpad/crashpad/handler/handler_main.h"
-#include "third_party/crashpad/crashpad/handler/user_stream_data_source.h"
 
 int main(int argc, char* argv[]) {
-  crashpad::UserStreamDataSources user_stream_data_sources;
-  user_stream_data_sources.push_back(
-      std::make_unique<stability_report::UserStreamDataSourcePosix>());
-
-  return crashpad::HandlerMain(argc, argv, &user_stream_data_sources);
+  return crashpad::HandlerMain(argc, argv,
+                               /*user_stream_data_sources=*/nullptr);
 }
