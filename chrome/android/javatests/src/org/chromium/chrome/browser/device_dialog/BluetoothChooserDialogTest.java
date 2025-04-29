6707696d7424540d3c6454a47753a4d87c487d7c
Garfield Tan <xutan@chromium.org>
2025-04-29 11:11:10 -0700

    bluetooth: Notify Android BluetoothChooserDialog when unauthorized
    Change the Android BluetoothChooserDialog to accept unauthorized adapter
    presence to align the unauthorized state handling with both Web Serial
    on Android and Web Bluetooth on other devices that require system
    permissions.
    
    The existing code assumes both the permission and the adapter powered
    state are correct when it receives the adapter powered on events, but
    that doesn't work on Android because it's possible the adapter is off
    when the chooser dialog grants the permission state, so we need to
    re-check the adapter state before start scanning. It should be safe on
    devices that rely on the re-scan logic before this CL because those
    checks would pass on those devices.
    
    Bug: 399916650
    Test: BluetoothChooserDialog handles permission and adapter powered off.
    Change-Id: I88bf431b0692e4d6cec49e762a1e834fc9d9c174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490347
    Commit-Queue: Garfield Tan <xutan@chromium.org>
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453410}

--- chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/BluetoothChooserDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/BluetoothChooserDialogTest.java
@@ -395,10 +395,7 @@ public class BluetoothChooserDialogTest {
                 new TestAndroidPermissionDelegate(dialog);
         mWindowAndroid.setAndroidPermissionDelegate(permissionDelegate);
 
-        ThreadUtils.runOnUiThreadBlocking(
-                () ->
-                        mChooserDialog.notifyDiscoveryState(
-                                BluetoothChooserDialog.DiscoveryMode.DISCOVERY_FAILED_TO_START));
+        ThreadUtils.runOnUiThreadBlocking(() -> mChooserDialog.notifyAdapterUnauthorized());
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Assert.assertEquals(
@@ -486,11 +483,7 @@ public class BluetoothChooserDialogTest {
         permissionDelegate.mLocationGranted = true;
         mLocationUtils.mSystemLocationSettingsEnabled = false;
 
-        ThreadUtils.runOnUiThreadBlocking(
-                () ->
-                        mChooserDialog.notifyDiscoveryState(
-                                BluetoothChooserDialog.DiscoveryMode.DISCOVERY_FAILED_TO_START));
-
+        ThreadUtils.runOnUiThreadBlocking(() -> mChooserDialog.notifyAdapterUnauthorized());
         Assert.assertEquals(
                 removeLinkTags(
                         mActivityTestRule
