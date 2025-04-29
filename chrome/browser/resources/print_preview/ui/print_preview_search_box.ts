7659d39a02d5e6d108c88f062322468d31f1e68b
dpapad <dpapad@chromium.org>
2025-04-28 17:42:25 -0700

    PrintPreview: Migrate to Lit, part 8.
    In this part migrating
     - ui/number_settings_section.ts
     - ui/print_preview_search_box.ts
    
    Bug: 410607166
    Change-Id: Iac17e1f6c5ea9a38c61a7c93dc368fae842c1b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6485470
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453029}

--- chrome/browser/resources/print_preview/ui/print_preview_search_box.ts
+++ chrome/browser/resources/print_preview/ui/print_preview_search_box.ts
@@ -4,18 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_icons.css.js';
-import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
-import './print_preview_shared.css.js';
 
 import type {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
-import {CrSearchFieldMixin} from 'chrome://resources/cr_elements/cr_search_field/cr_search_field_mixin.js';
-import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
+import {CrSearchFieldMixinLit} from 'chrome://resources/cr_elements/cr_search_field/cr_search_field_mixin_lit.js';
+import {WebUiListenerMixinLit} from 'chrome://resources/cr_elements/web_ui_listener_mixin_lit.js';
 import {stripDiacritics} from 'chrome://resources/js/search_highlight_utils.js';
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
-import {getTemplate} from './print_preview_search_box.html.js';
+import {getCss} from './print_preview_search_box.css.js';
+import {getHtml} from './print_preview_search_box.html.js';
 
 declare global {
   interface HTMLElementEventMap {
@@ -32,7 +31,7 @@ export interface PrintPreviewSearchBoxElement {
 }
 
 const PrintPreviewSearchBoxElementBase =
-    CrSearchFieldMixin(WebUiListenerMixin(PolymerElement));
+    CrSearchFieldMixinLit(WebUiListenerMixinLit(CrLitElement));
 
 export class PrintPreviewSearchBoxElement extends
     PrintPreviewSearchBoxElementBase {
@@ -40,13 +39,17 @@ export class PrintPreviewSearchBoxElement extends
     return 'print-preview-search-box';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  static get properties() {
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
     return {
-      autofocus: Boolean,
+      autofocus: {type: Boolean},
 
       searchQuery: {
         type: Object,
@@ -55,13 +58,11 @@ export class PrintPreviewSearchBoxElement extends
     };
   }
 
-  declare autofocus: boolean;
-  declare searchQuery: RegExp|null;
+  override accessor autofocus: boolean;
+  accessor searchQuery: RegExp|null;
   private lastQuery_: string = '';
 
-  override ready() {
-    super.ready();
-
+  override firstUpdated() {
     this.addEventListener('search-changed', e => this.onSearchChanged_(e));
   }
 
@@ -85,7 +86,7 @@ export class PrintPreviewSearchBoxElement extends
         safeQuery.length > 0 ? new RegExp(`(${safeQuery})`, 'ig') : null;
   }
 
-  private onClearClick_() {
+  protected onClearClick_() {
     this.setValue('');
     this.$.searchInput.focus();
   }
