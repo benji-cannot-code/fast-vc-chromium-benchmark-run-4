80d156e968c1866f84f9c9ee56851e2006c91f54
David Bokan <bokan@chromium.org>
2025-04-29 11:35:38 -0700

    Fix scroll tool when targeting viewport
    The scroll tool assumed the target field of the action would be null but
    due to a quirk in the proto the field is initialized without setting a
    value so the tool receives a DOMNodeId==0 in this case.
    
    Since DOMNodeIDs start at 0 we can reinterpret this to be viewport until
    this is fixed upstream.
    
    Fixed: 414431910
    Bug: 412359416
    Change-Id: I8a833ddbc4a43420b40d24f34ec1775cb3db2b4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494254
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453428}

--- chrome/renderer/actor/tool_utils.cc
+++ chrome/renderer/actor/tool_utils.cc
@@ -68,6 +68,10 @@ bool IsPointWithinViewport(const gfx::PointF& point,
 }
 
 std::string ToDebugString(const mojom::ToolTargetPtr& target) {
+  if (target.is_null()) {
+    return "target(null)";
+  }
+
   std::stringstream ss;
   ss << "target(";
   if (target->is_coordinate()) {
