baa3900e80a0ded347ca82e9b8f583882ae19795
Mingjing Zhang <mjzhang@chromium.org>
2025-04-29 13:15:24 -0700

    [media] Use RasterScopedAccess at two places in VideoResourceUpdater
    This CL replaces the SyncToken operations in member functions
    WriteRGBPixelsToTexture() and WriteYUVPixelsForAllPlanesToTexture()
    with RasterScopedAccess.
    
    Bug: 40286368
    Change-Id: Ieff8bae36ae4902338b84cda5d28292a91c8290d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494289
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453483}

--- media/renderers/video_resource_updater.cc
+++ media/renderers/video_resource_updater.cc
@@ -1016,7 +1016,10 @@ bool VideoResourceUpdater::WriteRGBPixelsToTexture(
 
   // Copy pixels into texture.
   auto* ri = RasterInterface();
-  ri->WaitSyncTokenCHROMIUM(hardware_resource->sync_token().GetConstData());
+  std::unique_ptr<gpu::RasterScopedAccess> ri_access =
+      hardware_resource->shared_image()->BeginRasterAccess(
+          ri, hardware_resource->sync_token(),
+          /*readonly=*/false);
 
   auto color_type =
       viz::ToClosestSkColorType(resource_format, /*plane_index=*/0);
@@ -1028,8 +1031,8 @@ bool VideoResourceUpdater::WriteRGBPixelsToTexture(
       /*dst_y_offset=*/0, hardware_resource->shared_image()->GetTextureTarget(),
       pixmap);
 
-  gpu::SyncToken ri_sync_token;
-  ri->GenUnverifiedSyncTokenCHROMIUM(ri_sync_token.GetData());
+  gpu::SyncToken ri_sync_token =
+      gpu::RasterScopedAccess::EndAccess(std::move(ri_access));
   hardware_resource->UpdateSyncToken(ri_sync_token);
 
   return true;
@@ -1179,12 +1182,14 @@ bool VideoResourceUpdater::WriteYUVPixelsForAllPlanesToTexture(
   auto yuv_pixmap = SkYUVAPixmaps::FromExternalPixmaps(info, pixmaps.data());
 
   auto* ri = RasterInterface();
-  ri->WaitSyncTokenCHROMIUM(resource->sync_token().GetConstData());
+  std::unique_ptr<gpu::RasterScopedAccess> ri_access =
+      resource->shared_image()->BeginRasterAccess(ri, resource->sync_token(),
+                                                  /*readonly=*/false);
 
   ri->WritePixelsYUV(resource->shared_image()->mailbox(), yuv_pixmap);
 
-  gpu::SyncToken ri_sync_token;
-  ri->GenUnverifiedSyncTokenCHROMIUM(ri_sync_token.GetData());
+  gpu::SyncToken ri_sync_token =
+      gpu::RasterScopedAccess::EndAccess(std::move(ri_access));
   resource->UpdateSyncToken(ri_sync_token);
 
   return true;
