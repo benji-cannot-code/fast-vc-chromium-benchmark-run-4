1988927aacd68bd988c28379d5a5048fdcdeba3b
James Cook <jamescook@chromium.org>
2025-04-28 17:33:11 -0700

    extensions: Add ChromeContentVerifierTest to desktop Android
    Now that we have ExtensionServiceTestBase and friends we can add this
    test (the production code ChromeContentVerifierDelegate was added
    recently).
    
    Bug: 413122584
    Change-Id: Ia5a39ed6990a6899a17ccedfa46bc317f162e4ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496954
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453026}

--- chrome/browser/extensions/chrome_content_verifier_unittest.cc
+++ chrome/browser/extensions/chrome_content_verifier_unittest.cc
@@ -17,8 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "extensions/browser/content_verifier/test_utils.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/file_util.h"
 #include "extensions/common/switches.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -46,8 +49,6 @@ bool IsSuperset(const std::set<base::FilePath>& container,
 
 // Tests are run with //chrome layer so that manifest's //chrome specific bits
 // (e.g. browser images, default_icon in actions) are present.
-// TODO(crbug.com/408458901): Port tests to desktop Android once we have a
-// testing base class that approximates ExtensionServiceTestBase.
 class ChromeContentVerifierTest : public ExtensionServiceTestWithInstall {
  public:
   void SetUp() override {
