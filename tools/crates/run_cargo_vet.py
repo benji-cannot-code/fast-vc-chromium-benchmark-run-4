d12bf985d908a2df50a5b096cc07d8248e21e16d
Michael Cui <mlcui@google.com>
2025-04-29 20:13:49 -0700

    run_cargo: Fix `PATH`-lookup trouble by specifying `executable`
    This CL is a partial revert of https://crrev.com/c/5377439.
    
    In the aforementioned CL, `tools.clang.scripts.build.RunCommand` was
    used instead of `subprocess.run`, as on Windows, the `PATH` environment
    variable passed into `subprocess.run` was not used to look up the
    executable provided in the command line.
    
    A closer look at the `subprocess` documentation states that a fully
    qualified path to the executable should be used for maximum reliability.
    The Windows `CreateProcessW` documentation also mentions that the `PATH`
    environment variable used to look up executable comes from the **current
    process**.
    
    Provide a fully qualified path instead as `executable` instead of using
    `RunCommand`. This:
    - ensures that Cargo sees itself being run as "cargo".
    - removes the "Running [command]" and "Failed." print messages which are
      printed by `RunCommand`.
    - removes the use of `shell=True`, preventing shell injections and
      allowing passing in path-like objects into the command line.
    
    Bug: None
    Change-Id: I6a6a2100d9b13507c6257f1ca9b46a41c379bf3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6326573
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453690}

--- tools/crates/run_cargo_vet.py
+++ tools/crates/run_cargo_vet.py
@@ -64,7 +64,7 @@ def main():
         '--cargo-arg=-Zbindeps',
         '--no-registry-suggestions'
     ]
-    success = RunCargo(
+    retcode = RunCargo(
         args.rust_sysroot, None,
         _CARGO_ARGS + ['vet'] + unrecognized_args + _EXTRA_VET_ARGS)
 
@@ -87,7 +87,7 @@ def main():
                        '--frozen' in unrecognized_args
         assert not is_presubmit
 
-    return 0 if success else 1
+    return retcode
 
 
 if __name__ == '__main__':
