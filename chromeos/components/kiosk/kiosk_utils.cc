88dccbdc3fd8deb1431c944aaa397f99f67ecae9
Sergii Bykov <sbykov@google.com>
2025-04-30 05:17:41 -0700

    kiosk: Rename IsLoggedInAsWebKioskApp
    Renames UserManager::IsLoggedInAsWebKioskApp to IsLoggedInAsKioskWebApp
    for better naming consistency with other IsLoggedInAs methods. Also
    improves the naming as there is no special type of "web kiosk" apps.
    It is a kiosk account/user running a web app.
    
    Bug: 413639732
    Change-Id: Ia1fe5be0c2ceda229b75996cc41d6ddaca0ff17b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494558
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453833}

--- chromeos/components/kiosk/kiosk_utils.cc
+++ chromeos/components/kiosk/kiosk_utils.cc
@@ -16,7 +16,7 @@ bool IsKioskSession() {
 
 bool IsWebKioskSession() {
   return user_manager::UserManager::IsInitialized() &&
-         user_manager::UserManager::Get()->IsLoggedInAsWebKioskApp();
+         user_manager::UserManager::Get()->IsLoggedInAsKioskWebApp();
 }
 
 }  // namespace chromeos
