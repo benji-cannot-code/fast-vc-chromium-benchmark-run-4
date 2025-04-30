bcc7295a45c42a7292aac65e5d4132c0682c8abd
Daniel Cheng <dcheng@chromium.org>
2025-04-30 16:31:24 -0700

    Clean up kUseRustJsonParser
    Bug: 404881580
    Change-Id: I9e3533b3fdbf436ab37917efbe3cbaf9e7091f67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492579
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454226}

--- chrome/browser/enterprise/profile_management/oidc_auth_response_capture_navigation_throttle_unittest.cc
+++ chrome/browser/enterprise/profile_management/oidc_auth_response_capture_navigation_throttle_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/base64url.h"
-#include "base/features.h"
 #include "base/json/json_writer.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -720,23 +719,6 @@ TEST_P(OidcAuthResponseCaptureNavigationThrottleTest, DecodeFailure) {
   auto* oidc_interceptor = GetMockOidcInterceptor();
   RunThrottleAndExpectNoOidcInterception(oidc_interceptor, redirection_url,
                                          NavigationThrottle::CANCEL_AND_IGNORE);
-  CheckFunnelAndResultHistogram(
-      OidcInterceptionFunnelStep::kValidRedirectionCaptured,
-      OidcInterceptionResult::kInvalidUrlOrTokens);
-}
-
-TEST_P(OidcAuthResponseCaptureNavigationThrottleTest, DataDecoderFailure) {
-  // Disable the Rust JSON parser, as it is in-process and cannot crash.
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitWithFeatureState(base::features::kUseRustJsonParser, false);
-  in_process_data_decoder_.SimulateJsonParserCrash(/*drop=*/true);
-
-  std::string redirection_url =
-      BuildStandardResponseUrl(/*oidc_state=*/std::string());
-
-  auto* oidc_interceptor = GetMockOidcInterceptor();
-  RunThrottleAndExpectNoOidcInterception(oidc_interceptor, redirection_url,
-                                         NavigationThrottle::DEFER);
   CheckFunnelAndResultHistogram(
       OidcInterceptionFunnelStep::kValidRedirectionCaptured,
       OidcInterceptionResult::kInvalidUrlOrTokens);
