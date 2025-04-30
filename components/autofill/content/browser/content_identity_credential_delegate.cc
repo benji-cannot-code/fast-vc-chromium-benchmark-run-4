59492136ed03710f663c77c4ff8caa9aaee60cfe
Sam Goto <goto@chromium.org>
2025-04-30 10:46:01 -0700

    [fedcm] Only preview/fill fields that were requested and are available
    Impose two requirements for making autofill suggestions:
    
    1) First, the relying party must have requested it via the Fields API [2].
    2) Second, the specific field must be available in the user's profile
       that was returned to the browser in [3].
    
    Both of these requirements need to be made for previewing and filling,
    including when filling more than one field in a profile.
    
    In addition to that, we remove the support for filling NAME_FIRST because
    that's not currently available as a recognized field in the specification
    [4] and implementation.
    
    [1] https://w3c-fedid.github.io/FedCM/#list-of-recognized-fields
    [2] https://w3c-fedid.github.io/FedCM/#dom-identityproviderrequestoptions-fields
    [3] https://w3c-fedid.github.io/FedCM/#dictdef-identityprovideraccount
    [4] https://w3c-fedid.github.io/FedCM/#list-of-recognized-fields
    
    Bug: 380367784
    Change-Id: Ic9385eb5e6ab7dcd8dc4e3af0928163e82c56215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492099
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454048}

--- components/autofill/content/browser/content_identity_credential_delegate.cc
+++ components/autofill/content/browser/content_identity_credential_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/content/browser/content_identity_credential_delegate.h"
 
+#include "base/containers/contains.h"
 #include "base/functional/callback.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -38,7 +39,7 @@ std::vector<Suggestion>
 ContentIdentityCredentialDelegate::GetVerifiedAutofillSuggestions(
     const FieldType& field_type) const {
   if (!(field_type == PASSWORD || field_type == EMAIL_ADDRESS ||
-        field_type == NAME_FIRST || field_type == NAME_FULL)) {
+        field_type == NAME_FULL)) {
     return {};
   }
   // TODO(crbug.com/380367784): reproduce and add a test to make sure this
@@ -62,11 +63,22 @@ ContentIdentityCredentialDelegate::GetVerifiedAutofillSuggestions(
     auto payload = Suggestion::IdentityCredentialPayload(
         account->identity_provider->idp_metadata.config_url, account->id);
 
-    if (field_type == EMAIL_ADDRESS || field_type == NAME_FIRST ||
-        field_type == NAME_FULL) {
-      payload.fields[NAME_FULL] = base::UTF8ToUTF16(account->name);
-      payload.fields[NAME_FIRST] = base::UTF8ToUTF16(account->given_name);
+    if (!account->email.empty() &&
+        base::Contains(account->identity_provider->disclosure_fields,
+                       content::IdentityRequestDialogDisclosureField::kEmail)) {
       payload.fields[EMAIL_ADDRESS] = base::UTF8ToUTF16(account->email);
+    }
+
+    if (!account->name.empty() &&
+        base::Contains(account->identity_provider->disclosure_fields,
+                       content::IdentityRequestDialogDisclosureField::kName)) {
+      payload.fields[NAME_FULL] = base::UTF8ToUTF16(account->name);
+    }
+
+    if (field_type == EMAIL_ADDRESS || field_type == NAME_FULL) {
+      if (!payload.fields.contains(field_type)) {
+        continue;
+      }
 
       suggestion.main_text = Suggestion::Text(payload.fields[field_type]);
       // TODO(crbug.com/380367784): revisit the iconography of the suggestion
