933d18cfd267555972f3b78f2897314013182c14
David Benjamin <davidben@chromium.org>
2025-04-30 11:23:19 -0700

    Unwind kPostQuantumKyber and kUseMLKEM feature flags
    Both features have been launched for some time, so we can remove the
    flags now. (This does not impact the admin policy, just the Chrome-level
    feature flags.)
    
    While I'm here:
    
    - Update net/quic/quic_end_to_end_unittest.cc to test by disabling
      it via config instead of flag.
    
    - We no longer need the tri-state optional<bool> in the net stack.
    
    - Rename the field from post_quantum_enabled to
      post_quantum_key_agreement_enabled because presumably we'll
      eventually have more PQ things to worry about.
    
    Fixed: 40910498
    Change-Id: I4c1019d7933cc04f72a32fce4ee42fbae620dab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6497575
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454071}

--- net/quic/quic_end_to_end_unittest.cc
+++ net/quic/quic_end_to_end_unittest.cc
@@ -35,7 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/crypto_test_utils_chromium.h"
 #include "net/quic/quic_context.h"
 #include "net/socket/client_socket_factory.h"
-#include "net/ssl/ssl_config_service_defaults.h"
+#include "net/ssl/ssl_config_service.h"
+#include "net/ssl/test_ssl_config_service.h"
 #include "net/test/cert_test_util.h"
 #include "net/test/gtest_util.h"
 #include "net/test/test_data_directory.h"
@@ -92,7 +93,8 @@ class QuicEndToEndTest : public ::testing::Test, public WithTaskEnvironment {
  protected:
   QuicEndToEndTest()
       : host_resolver_(CreateResolverImpl()),
-        ssl_config_service_(std::make_unique<SSLConfigServiceDefaults>()),
+        ssl_config_service_(
+            std::make_unique<TestSSLConfigService>(SSLContextConfig())),
         proxy_resolution_service_(
             ConfiguredProxyResolutionService::CreateDirect()),
         auth_handler_factory_(HttpAuthHandlerFactory::CreateDefault()) {
@@ -219,7 +221,7 @@ class QuicEndToEndTest : public ::testing::Test, public WithTaskEnvironment {
   MappedHostResolver host_resolver_;
   MockCertVerifier cert_verifier_;
   TransportSecurityState transport_security_state_;
-  std::unique_ptr<SSLConfigServiceDefaults> ssl_config_service_;
+  std::unique_ptr<TestSSLConfigService> ssl_config_service_;
   std::unique_ptr<ProxyResolutionService> proxy_resolution_service_;
   std::unique_ptr<HttpAuthHandlerFactory> auth_handler_factory_;
   StaticHttpUserAgentSettings http_user_agent_settings_ = {"*", "test-ua"};
@@ -310,10 +312,6 @@ TEST_F(QuicEndToEndTest, UberTest) {
 }
 
 TEST_F(QuicEndToEndTest, EnableMLKEM) {
-  // Enable ML-KEM on the client.
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures({features::kPostQuantumKyber}, {});
-
   // Configure the server to only support ML-KEM.
   server_->crypto_config()->set_preferred_groups({SSL_GROUP_X25519_MLKEM768});
 
@@ -330,8 +328,9 @@ TEST_F(QuicEndToEndTest, EnableMLKEM) {
 
 TEST_F(QuicEndToEndTest, MLKEMDisabled) {
   // Disable ML-KEM on the client.
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures({}, {features::kPostQuantumKyber});
+  SSLContextConfig config;
+  config.post_quantum_key_agreement_enabled = false;
+  ssl_config_service_->UpdateSSLConfigAndNotify(config);
 
   // Configure the server to only support ML-KEM.
   server_->crypto_config()->set_preferred_groups({SSL_GROUP_X25519_MLKEM768});
--- services/network/ssl_config_type_converter.cc
+++ services/network/ssl_config_type_converter.cc
@@ -12,20 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
-namespace {
-std::optional<bool> OptionalBoolFromMojo(network::mojom::OptionalBool v) {
-  switch (v) {
-    case network::mojom::OptionalBool::kTrue:
-      return std::make_optional(true);
-    case network::mojom::OptionalBool::kFalse:
-      return std::make_optional(false);
-    case network::mojom::OptionalBool::kUnset:
-      return std::nullopt;
-  }
-  NOTREACHED();
-}
-}  // namespace
-
 int MojoSSLVersionToNetSSLVersion(network::mojom::SSLVersion mojo_version) {
   switch (mojo_version) {
     case network::mojom::SSLVersion::kTLS12:
@@ -47,8 +33,8 @@ net::SSLContextConfig MojoSSLConfigToSSLContextConfig(
   DCHECK_LE(net_config.version_min, net_config.version_max);
 
   net_config.disabled_cipher_suites = mojo_config->disabled_cipher_suites;
-  net_config.post_quantum_override =
-      OptionalBoolFromMojo(mojo_config->post_quantum_override);
+  net_config.post_quantum_key_agreement_enabled =
+      mojo_config->post_quantum_key_agreement_enabled;
   net_config.ech_enabled = mojo_config->ech_enabled;
   return net_config;
 }
