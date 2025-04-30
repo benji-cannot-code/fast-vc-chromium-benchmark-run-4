c6acf227afcc0b4742008c59baacb7bae5dfdf75
Timofey Chudakov <tchudakov@google.com>
2025-04-30 08:16:02 -0700

    Reland "[Wallet] Implement "Manage loyalty cards" MF action."
    This reverts commit 1c39cba58ca3ab4d9ef20d5902c2e56fcb9a24ce.
    
    Reason for revert: fixed bot failures.
    
    Bug: 404437211
    Original change's description:
    > Revert "[Wallet] Implement "Manage loyalty cards" MF action."
    >
    > This reverts commit 70fa37b8e35c78b6d3ba4bcbd7de9641679fc564.
    >
    > Reason for revert: Suspected cause of presubmit failure in
    > https://ci.chromium.org/ui/p/chromium/builders/ci/win-presubmit/13161/overview
    >
    > Bug: 404437211, 414778559
    > Original change's description:
    > > [Wallet] Implement "Manage loyalty cards" MF action.
    > >
    > > MF - manual fallback.
    > >
    > > This CL adds the "Manage loyalty cards" manual fallback action to the
    > > credit card MF sheet. The action is added if the user has a non-empty
    > > list of loyalty cards. Google Wallet loyalty card management URL is
    > > opened in a CCT when this MF action is used.
    > >
    > > This CL also generalizes the PlusAddressesHelper to
    > > AutofillFallbackSurfaceLauncher.
    > >
    > > Screencast: http://shortn/_231MW0wR7Q
    > >
    > > Bug: 404437211
    > > Change-Id: I226877489912c031378b19b741f860b7d115fb79
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494395
    > > Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    > > Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    > > Reviewed-by: Norge Vizcay <vizcay@google.com>
    > > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1453812}
    >
    > Bug: 404437211
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I2c5ce6d33f458619b85650248a78248319d7fbfe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501375
    > Auto-Submit: Nicolás Peña <npm@chromium.org>
    > Owners-Override: Nicolás Peña <npm@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1453872}
    
    Bug: 404437211, 414778559
    Bug: 404437211
    Change-Id: I328c92e00903e44ac1abbfcb8e1f5c233b741f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496098
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Owners-Override: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453934}

---
+++ chrome/browser/ui/android/autofill/autofill_fallback_surface_launcher.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ANDROID_AUTOFILL_AUTOFILL_FALLBACK_SURFACE_LAUNCHER_H_
+#define CHROME_BROWSER_UI_ANDROID_AUTOFILL_AUTOFILL_FALLBACK_SURFACE_LAUNCHER_H_
+
+namespace content {
+class WebContents;
+}
+
+namespace autofill {
+
+// Opens the plus addresses management UI.
+void ShowManagePlusAddressesPage(content::WebContents& web_contents);
+
+// Opens the loyalty card management UI web page in a Chrome Custom Tab.
+void ShowGoogleWalletLoyaltyCardsPage(content::WebContents& web_contents);
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_UI_ANDROID_AUTOFILL_AUTOFILL_FALLBACK_SURFACE_LAUNCHER_H_
