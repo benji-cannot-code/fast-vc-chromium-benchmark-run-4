1f771e7d938e61bbba55c4f6b15b9b2e5162e32d
Colin Blundell <blundell@chromium.org>
2025-04-29 23:49:41 -0700

    [Blink] Remove usage of SkColorType in AcceleratedStaticBitmapImage
    This usage site can just ask the SharedImageFormat directly whether it's
    opaque.
    
    Bug: 371227617
    Change-Id: I66a28f049b30db09aa68a649d39a3713c2280970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495341
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453738}

--- third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.cc
+++ third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.cc
@@ -446,8 +446,7 @@ void AcceleratedStaticBitmapImage::Transfer() {
 
 bool AcceleratedStaticBitmapImage::CurrentFrameKnownToBeOpaque() {
   return SkAlphaTypeIsOpaque(GetAlphaType()) ||
-         SkColorTypeIsAlwaysOpaque(
-             ToClosestSkColorType(GetSharedImageFormat()));
+         !GetSharedImageFormat().HasAlpha();
 }
 
 }  // namespace blink
