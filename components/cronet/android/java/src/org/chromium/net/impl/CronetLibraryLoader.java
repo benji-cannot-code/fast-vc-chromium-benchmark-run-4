2a5a165890915993eec14d63833625a125b7e426
Mohannad Farrag <aymanm@google.com>
2025-04-30 07:08:43 -0700

    Break dependency between cronet_common_impl and base_java
    cronet_common_impl depended on base:base_java just for the sake of
    accessing ContextUtils, this dependency provided cronet_common_impl
    access to an application-wide context which is used for HttpFlags.
    However, `:cronet_impl_common_java` depending on `//base:base_java` is problematic because `//base:base_java` is only shipped in `:native_impl`, not in `:cronet_impl_common_java`. This means someone using `:cronet_impl_common_java` has to depend on `:native_impl`, which is not acceptable, especially if one considers the user may only be interested in :cronet_fallback_impl.
    
    Bug: 299420950
    Change-Id: I26c72bb9142a32ec2855ef1ea393a99592a49156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6498197
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453878}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetLibraryLoader.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetLibraryLoader.java
@@ -148,7 +148,7 @@ public class CronetLibraryLoader {
                             .nativeInit(CronetManifest.shouldUsePerfetto(applicationContext));
                 }
                 var initializeBuildInfoOnStartup =
-                        HttpFlagsForImpl.getHttpFlags()
+                        HttpFlagsForImpl.getHttpFlags(ContextUtils.getApplicationContext())
                                 .flags()
                                 .get(INITIALIZE_BUILD_INFO_ON_STARTUP);
 
@@ -222,7 +222,7 @@ public class CronetLibraryLoader {
             // likely wouldn't make any difference.
             // Load and initialize httpflags in parallel with Cronet loading
             // as an attempt to alleviate the critical path blocking.
-            HttpFlagsForImpl.getHttpFlags();
+            HttpFlagsForImpl.getHttpFlags(ContextUtils.getApplicationContext());
             sHttpFlagsLoaded.open();
             NetworkChangeNotifier.init();
             // Registers to always receive network notifications. Note
@@ -248,7 +248,7 @@ public class CronetLibraryLoader {
             // Since we would like to query the network state only once, this experiment
             // disables 2 and 3.
             var updateNetworkStateOnceFlagValue =
-                    HttpFlagsForImpl.getHttpFlags()
+                    HttpFlagsForImpl.getHttpFlags(ContextUtils.getApplicationContext())
                             .flags()
                             .get(UPDATE_NETWORK_STATE_ONCE_ON_STARTUP_FLAG_NAME);
             var updateNetworkStateOnce =
@@ -300,7 +300,9 @@ public class CronetLibraryLoader {
      */
     @CalledByNative
     private static byte[] getBaseFeatureOverrides() {
-        return BaseFeature.getOverrides(HttpFlagsForImpl.getHttpFlags()).toByteArray();
+        return BaseFeature.getOverrides(
+                        HttpFlagsForImpl.getHttpFlags(ContextUtils.getApplicationContext()))
+                .toByteArray();
     }
 
     /**
--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
@@ -16,6 +16,7 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeClassQualifiedName;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
 import org.chromium.base.metrics.ScopedSysTraceEvent;
@@ -402,11 +403,13 @@ public class CronetUrlRequestContext extends CronetEngineBase {
     private static RequestContextConfigOptions createRequestContextConfigOptions(
             CronetEngineBuilderImpl engineBuilder) {
         var networkThreadPriorityFlagValue =
-                HttpFlagsForImpl.getHttpFlags()
+                HttpFlagsForImpl.getHttpFlags(ContextUtils.getApplicationContext())
                         .flags()
                         .get(OVERRIDE_NETWORK_THREAD_PRIORITY_FLAG_NAME);
         var alwaysEnableBrotliFlagValue =
-                HttpFlagsForImpl.getHttpFlags().flags().get(ALWAYS_ENABLE_BROTLI_FLAG_NAME);
+                HttpFlagsForImpl.getHttpFlags(ContextUtils.getApplicationContext())
+                        .flags()
+                        .get(ALWAYS_ENABLE_BROTLI_FLAG_NAME);
         boolean alwaysEnableBrotli =
                 alwaysEnableBrotliFlagValue != null
                         ? alwaysEnableBrotliFlagValue.getBoolValue()
