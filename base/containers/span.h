bb698e1c9842cdaac88aae43de934ae6e62fa5cd
Lei Zhang <thestig@chromium.org>
2025-04-30 14:48:20 -0700

    Remove <array> from base/containers/span.h
    It's not used in span.h.
    
    Change-Id: I7c1b96f5f96b0b2a9e330fa3597e7d5f28952b3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6499671
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454188}

--- base/containers/span.h
+++ base/containers/span.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <algorithm>
-#include <array>
 #include <concepts>
 #include <functional>
 #include <initializer_list>
