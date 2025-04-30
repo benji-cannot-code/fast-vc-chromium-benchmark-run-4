d1553151119e2a53e5a5f6b38b859019d4692147
Daniil Sakhapov <sakhapov@chromium.org>
2025-04-30 03:07:19 -0700

    [carousel] Change carousel pseudos focus order
    ::scroll-marker-group(after) should now appear as preceding element.
    
    Also, this CL cleans and makes easier to understand carousel focus code.
    
    Fixed: 412625043
    Change-Id: Ifb04d1816576219350037539caa7fab1677c12ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494756
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453800}

--- third_party/blink/renderer/core/page/focus_controller_test.cc
+++ third_party/blink/renderer/core/page/focus_controller_test.cc
@@ -491,6 +491,7 @@ TEST_F(FocusControllerTest, FullCarouselFocusOrder) {
                                     before_scroller,
                                     before_first_child,
                                     before_second_child,
+                                    after_first_scroll_marker,
                                     after_block_start_button,
                                     after_inline_start_button,
                                     after_inline_end_button,
@@ -498,7 +499,6 @@ TEST_F(FocusControllerTest, FullCarouselFocusOrder) {
                                     after_scroller,
                                     after_first_child,
                                     after_last_child,
-                                    after_first_scroll_marker,
                                     post_input};
 
   for (std::size_t i = 0u; i < order.size() - 1; ++i) {
@@ -687,8 +687,8 @@ TEST_F(FocusControllerTest, CarouselWithOnlyScrollMarkersFocusOrder) {
 
   std::array<Element*, 10> order = {
       pre_input,          before_first_scroll_marker, before_scroller,
-      before_first_child, before_second_child,        after_scroller,
-      after_first_child,  after_last_child,           after_first_scroll_marker,
+      before_first_child, before_second_child,        after_first_scroll_marker,
+      after_scroller,     after_first_child,          after_last_child,
       post_input};
 
   for (std::size_t i = 0u; i < order.size() - 1; ++i) {
@@ -872,6 +872,7 @@ TEST_F(FocusControllerTest, FullCarouselWithExtraPseudoElementsFocusOrder) {
                                     before_scroller,
                                     before_first_child,
                                     before_second_child,
+                                    after_first_scroll_marker,
                                     after_block_start_button,
                                     after_inline_start_button,
                                     after_inline_end_button,
@@ -879,7 +880,6 @@ TEST_F(FocusControllerTest, FullCarouselWithExtraPseudoElementsFocusOrder) {
                                     after_scroller,
                                     after_first_child,
                                     after_last_child,
-                                    after_first_scroll_marker,
                                     post_input};
 
   for (std::size_t i = 0u; i < order.size() - 1; ++i) {
