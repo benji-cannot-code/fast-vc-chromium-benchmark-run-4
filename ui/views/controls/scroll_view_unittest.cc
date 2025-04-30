658e1b5f3ed5609a636490e265e65d0e78b4e599
Leonid Baraz <lbaraz@google.com>
2025-04-30 11:32:35 -0700

    Add `post-layout` callback to the `ScrollView`
    This change adds a new `post-layout` callback to the `ScrollView` to
    allow the scroll view to invoke the callback at the end of every layout
    operation. This is a useful feature to allow the user to explicitly
    scroll the contents to a specific position after a layout and update the
    scrollbars. It would allow e.g. to restore the position of the scrolled
    view in a large content view when the contents is scrolled out of the
    view.
    
    Bug: b:413733359
    Change-Id: I9e58136dbbedb3c128b31bba669db060900a87ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6498548
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454078}

--- ui/views/controls/scroll_view_unittest.cc
+++ ui/views/controls/scroll_view_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/gtest_util.h"
 #include "base/test/icu_test_util.h"
+#include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_timeouts.h"
 #include "base/timer/timer.h"
@@ -562,6 +563,17 @@ TEST_F(ScrollViewTest, BoundedViewportSizedToFit) {
   // Make sure the width of |contents| is set properly not to overflow the
   // viewport.
   EXPECT_EQ(96, contents->width());
+}
+
+// Verifies that the scroll view calls post-layout callback on every layout.
+TEST_F(ScrollViewTest, LayoutCallbackCalledOnEveryLayout) {
+  auto mock_post_layout_cb_ =
+      base::MockCallback<base::RepeatingCallback<void(ScrollView*)>>();
+  scroll_view_->RegisterPostLayoutCallback(mock_post_layout_cb_.Get());
+  EXPECT_CALL(mock_post_layout_cb_, Run(scroll_view_.get())).Times(1);
+  InstallContents();
+  ASSERT_FALSE(scroll_view_->GetContentsBounds().IsEmpty());
+  views::test::RunScheduledLayout(scroll_view_.get());
 }
 
 // Verifies that the vertical scrollbar does not unnecessarily appear for a
