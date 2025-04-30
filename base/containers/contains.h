8053189bddaacb2c9f10caab5fe7b3d16a450288
Lei Zhang <thestig@chromium.org>
2025-04-30 08:40:04 -0700

    Remove duplicate #include in base/containers/contains.h
    No longer makes sense after base/ranges/ranges.h removal.
    
    Change-Id: Iba6d1d736f69a683ff4c75212e7daec62e1777e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496654
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453951}

--- base/containers/contains.h
+++ base/containers/contains.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ranges>
 #include <type_traits>
 #include <utility>
-// TODO(dcheng): Remove this after fixing any IWYU errors.
-#include <ranges>
 
 namespace base {
 
