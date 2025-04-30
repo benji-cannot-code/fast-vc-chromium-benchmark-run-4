38f7cc94ece74eef87ea59d4f1f99671de1371fe
Sophie Chang <sophiechang@chromium.org>
2025-04-29 23:09:50 -0700

    Do not fetch page content until sufficient content loaded for page
    This also adds a bunch of logging to better understand state.
    
    Note that this only fixes it for non-same doc. Same doc change will come later
    
    Bug: 414451208
    Change-Id: Ic8f7603aaf809ce4600f85076527e2581784f4f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495993
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453725}

--- chrome/browser/contextual_cueing/contextual_cueing_helper.h
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.h
@@ -41,6 +41,8 @@ class ContextualCueingHelper
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
   void PrimaryMainDocumentElementAvailable() override;
+  void OnFirstContentfulPaintInPrimaryMainFrame() override;
+  void DocumentOnLoadCompletedInPrimaryMainFrame() override;
 
   tabs::GlicNudgeController* GetGlicNudgeController();
 
