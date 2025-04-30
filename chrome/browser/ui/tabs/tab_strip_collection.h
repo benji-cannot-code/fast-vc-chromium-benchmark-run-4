dc096d43a6320b3a8e17e1d8d750edf11235901b
Takuto Ikuta <tikuta@chromium.org>
2025-04-30 03:45:43 -0700

    chrome: Explicitly import unordered_map in tab_strip_collection.h
    Some configurations will fail to compile otherwise because unordered_map
    is not declared in the header.
    
    Bug: 40263312
    Change-Id: I349cfc8084ae0850e6f35088f98e19beae3c1d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6500481
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453813}

--- chrome/browser/ui/tabs/tab_strip_collection.h
+++ chrome/browser/ui/tabs/tab_strip_collection.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <optional>
+#include <unordered_map>
 
 #include "chrome/browser/ui/tabs/split_tab_data.h"
 #include "chrome/browser/ui/tabs/tab_collection.h"
