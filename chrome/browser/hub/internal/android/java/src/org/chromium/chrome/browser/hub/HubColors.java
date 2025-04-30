4d42394bec520d11273e8a2884cdd39bc38cc0c3
Madhav Pruthi <madhavpruthi@google.com>
2025-04-30 07:01:38 -0700

    Update color of tab selector in pane switcher
    Impl SS:
    Day mode -
    https://screenshot.googleplex.com/8zqDNmMNk8DVWRG
    https://screenshot.googleplex.com/88hezwq5AjEyaj6
    
    Night mode -
    https://screenshot.googleplex.com/frVHC94wkoXn9v8
    https://screenshot.googleplex.com/3k2eVhjgg6q45ke
    
    Bug: 404630316
    Change-Id: I19a07e7c452627b98d02d40b552eeaa57dc8f3b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6498470
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453871}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubColors.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubColors.java
@@ -116,7 +116,7 @@ public final class HubColors {
             Context context, @HubColorScheme int colorScheme) {
         switch (colorScheme) {
             case HubColorScheme.DEFAULT:
-                return SemanticColorUtils.getColorSurface(context);
+                return SemanticColorUtils.getColorSurfaceBright(context);
             case HubColorScheme.INCOGNITO:
                 return ContextCompat.getColor(
                         context, R.color.pane_switcher_selected_tab_incognito);
