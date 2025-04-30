f185f0162f3ff07916fd65275c8f0c7953027f9a
Takuto Ikuta <tikuta@chromium.org>
2025-04-30 02:36:55 -0700

    chrome: Adds a missing #include for std::optional.
    This is to fix clang module build.
    
    Bug: 40263312
    Change-Id: I91953cc95459304a6a7b2073f9c5455d166883a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6499673
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453791}

--- chrome/browser/ui/safety_hub/disruptive_notification_permissions_manager.h
+++ chrome/browser/ui/safety_hub/disruptive_notification_permissions_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_SAFETY_HUB_DISRUPTIVE_NOTIFICATION_PERMISSIONS_MANAGER_H_
 
 #include <memory>
+#include <set>
 
 #include "base/scoped_observation.h"
 #include "base/time/clock.h"
