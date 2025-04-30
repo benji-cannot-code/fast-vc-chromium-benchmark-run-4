fbfc2efe816d20932ce48b706fc7c7c017f38da9
Ho Cheung <hocheung@chromium.org>
2025-04-29 17:44:39 -0700

    [chromecast] Replace C-array with std::array in GroupedHistogram
    Some range-base loops are also used, which eliminates some unsafe
    buffers.
    
    Bug: 40285824
    Change-Id: I4455cac1342a8f59cccfafacb8abf0d3800e4e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6486867
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Antonio Rivera <antoniori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453653}

--- chromecast/base/metrics/grouped_histogram.cc
+++ chromecast/base/metrics/grouped_histogram.cc
@@ -3,16 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chromecast/base/metrics/grouped_histogram.h"
 
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
 #include <string_view>
 
 #include "base/check_op.h"
@@ -65,37 +61,15 @@ struct HistogramArgs {
 //
 // When adding more Histograms to this list, find the source of the
 // Histogram and look for the construction arguments it uses to add it in.
-const HistogramArgs kHistogramsToGroup[] = {
-  {
-    "DNS.TotalTime",
-    1,
-    1000 * 60 * 60,
-    100,
-  },
-  {
-    "Net.DNS_Resolution_And_TCP_Connection_Latency2",
-    1,
-    1000 * 60 * 10,
-    100,
-  },
-  {
-    "Net.SSL_Connection_Latency2",
-    1,
-    1000 * 60,
-    100,
-  },
-  {
-    "Net.TCP_Connection_Latency",
-    1,
-    1000 * 60 * 10,
-    100,
-  },
-  {
-    "Net.HttpJob.TotalTime",
-    1,
-    1000 * 10,
-    50,
-  },
+const std::array kHistogramsToGroup = {
+    HistogramArgs("DNS.TotalTime", 1, 1000 * 60 * 60, 100),
+    HistogramArgs("Net.DNS_Resolution_And_TCP_Connection_Latency2",
+                  1,
+                  1000 * 60 * 10,
+                  100),
+    HistogramArgs("Net.SSL_Connection_Latency2", 1, 1000 * 60, 100),
+    HistogramArgs("Net.TCP_Connection_Latency", 1, 1000 * 60 * 10, 100),
+    HistogramArgs("Net.HttpJob.TotalTime", 1, 1000 * 10, 50),
 };
 
 // This class is used to override a Histogram to generate per-app metrics.
@@ -182,11 +156,10 @@ void PreregisterHistogram(base::DurableStringView durable_name,
 } // namespace
 
 void PreregisterAllGroupedHistograms() {
-  for (size_t i = 0; i < std::size(kHistogramsToGroup); ++i) {
-    PreregisterHistogram(
-        kHistogramsToGroup[i].durable_name, kHistogramsToGroup[i].minimum,
-        kHistogramsToGroup[i].maximum, kHistogramsToGroup[i].bucket_count,
-        base::HistogramBase::kUmaTargetedHistogramFlag);
+  for (const auto& histogram : kHistogramsToGroup) {
+    PreregisterHistogram(histogram.durable_name, histogram.minimum,
+                         histogram.maximum, histogram.bucket_count,
+                         base::HistogramBase::kUmaTargetedHistogramFlag);
   }
 }
 
