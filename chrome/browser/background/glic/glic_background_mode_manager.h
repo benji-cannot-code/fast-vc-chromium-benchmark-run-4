632a99df50c803fa88aa5438174a7d399e4ca9be
Andrea Gomez <andreaxg@google.com>
2025-04-30 01:18:16 -0700

    Close Glic when Profile is marked for permanent deletion
    Adds an override to OnProfileMarkedForPermanentDeletion to close Glic
    if it is open for the profile marked for permanent deletion.
    
    Bug: 411694207
    Change-Id: If973b51ff57f978421e0056e45ff5b00743ef288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489587
    Auto-Submit: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453755}

--- chrome/browser/background/glic/glic_background_mode_manager.h
+++ chrome/browser/background/glic/glic_background_mode_manager.h
@@ -67,6 +67,8 @@ class GlicBackgroundModeManager
   // ProfileManagerObserver:
   void OnProfileAdded(Profile* profile) override;
 
+  void OnProfileMarkedForPermanentDeletion(Profile* profile) override;
+
   // ProfileObserver:
   void OnProfileWillBeDestroyed(Profile* profile) override;
 
