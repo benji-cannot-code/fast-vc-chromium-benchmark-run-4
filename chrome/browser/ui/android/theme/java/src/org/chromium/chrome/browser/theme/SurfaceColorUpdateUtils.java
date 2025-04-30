8180705fd88b391d842fddbe1470b8b698d0791b
Joel Tan-Aristy <jtanaristy@google.com>
2025-04-30 16:00:19 -0700

    [Color Surface] Applies color surface updates for TabListEditor
    Updates TabListEditor so that is background and toolbar color
    update according to the flag values.
    
    Before:
    https://screenshot.googleplex.com/99CUpPQoatwFffk
    https://screenshot.googleplex.com/BNRkuuEmYgt4sfa
    
    After:
    https://screenshot.googleplex.com/BdbhZmXDHKP2qZL
    https://screenshot.googleplex.com/62HkkzUefoGjemX
    
    Bug: 414862613
    Change-Id: Ifb6fbcce67a168a33357c01d4a2bfddcdbd7b258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6502237
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454215}

--- chrome/browser/ui/android/theme/java/src/org/chromium/chrome/browser/theme/SurfaceColorUpdateUtils.java
+++ chrome/browser/ui/android/theme/java/src/org/chromium/chrome/browser/theme/SurfaceColorUpdateUtils.java
@@ -95,6 +95,7 @@ public class SurfaceColorUpdateUtils {
      */
     public static @ColorInt int getGridTabSwitcherBackgroundColor(
             Context context, boolean isIncognito) {
+        // TODO(crbug.com/414404094): Add semantic color for incognito.
         if (ChromeFeatureList.sGridTabSwitcherSurfaceColorUpdate.isEnabled()) {
             return isIncognito
                     ? ContextCompat.getColor(
