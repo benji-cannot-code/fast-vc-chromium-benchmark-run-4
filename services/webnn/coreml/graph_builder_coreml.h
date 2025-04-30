88182a28ad189450b2ab2da757643cf65afb631f
Reilly Grant <reillyg@chromium.org>
2025-04-29 17:31:56 -0700

    webnn: Migrate to recommended Abseil container types
    The Chromium C++ style guide now recommends using absl::flat_hash_map
    and absl::flat_hash_set instead of std::map and std::unordered_set. This
    change migrates //services/webnn to these types.
    
    Change-Id: Iaa70da316aed09a3ce04cac09d4cbe4030fe1f54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496992
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453643}

--- services/webnn/coreml/graph_builder_coreml.h
+++ services/webnn/coreml/graph_builder_coreml.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/webnn/public/mojom/webnn_context_provider.mojom.h"
 #include "services/webnn/public/mojom/webnn_error.mojom-forward.h"
 #include "services/webnn/public/mojom/webnn_graph.mojom.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 #include "third_party/coremltools/mlmodel/format/MIL.pb.h"
 #include "third_party/coremltools/mlmodel/format/Model.pb.h"
 
@@ -100,7 +101,7 @@ class GraphBuilderCoreml {
     [[nodiscard]] const OperandInfo& GetOperandInfo(uint64_t operand_id) const;
 
     const base::FilePath ml_package_dir;
-    std::map<uint64_t, OperandInfo> id_to_operand_info_map;
+    absl::flat_hash_map<uint64_t, OperandInfo> id_to_operand_info_map;
   };
 
   // Factory method that creates a GraphBuilderCoreml, builds and serializes the
@@ -573,7 +574,7 @@ class GraphBuilderCoreml {
   const base::FilePath& ml_package_dir() const {
     return result_->ml_package_dir;
   }
-  std::map<uint64_t, OperandInfo>& id_to_operand_info_map() const {
+  absl::flat_hash_map<uint64_t, OperandInfo>& id_to_operand_info_map() const {
     return result_->id_to_operand_info_map;
   }
 
