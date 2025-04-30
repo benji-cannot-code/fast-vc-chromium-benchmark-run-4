c35556e59402dc13b8df73f565fbcbb187d46af0
Leonid Baraz <lbaraz@chromium.org>
2025-04-30 14:08:34 -0700

    Remove runaway debug logging
    I remained here since the debugging time...
    
    Bug: b:375383880
    Change-Id: Ia89389e20acb64606aca6b290a9268c99a2487ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6502234
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454162}

--- chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager_unittest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager_unittest.cc
@@ -789,7 +789,6 @@ TEST_F(MetricReportingManagerTelemetryTest, OneShotCollectorBootPerformance) {
                                  _, ::ash::kReportDeviceBootMode, true,
                                  metrics::kInitialCollectionDelay))
       .WillByDefault([&]() {
-        LOG(ERROR) << " lbaraz: CALLED ";
         return std::make_unique<FakeCollector>(&collector_count);
       });
 
