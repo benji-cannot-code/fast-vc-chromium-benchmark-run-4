cfd92b2b922d8747d7d1390bd6109e79a586c159
Radu Nitescu <radunitescu@google.com>
2025-04-30 07:30:47 -0700

    [iOS][LU] Add fieldtrial config for Lens Unification experiment
    Bug: None
    Change-Id: I714cc28f0cc6aa45ed86265324734ca02d688405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495616
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453892}

--- ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
@@ -105,6 +105,10 @@ LensViewFinderTransition TransitionFromPresentationStyle(
 
   _lensViewController =
       ios::provider::NewChromeLensViewFinderController(configuration);
+  if (!_lensViewController) {
+    return;
+  }
+
   [_lensViewController setLensViewFinderDelegate:self];
 
   _lensViewController.transitioningDelegate = _transitionManager;
