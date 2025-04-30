45009dcdc76f0feec7eb6bb4870567b67b343a59
Jenna Himawan <jhimawan@google.com>
2025-04-30 09:45:00 -0700

    [Keyboard A11y] Clear tab strip focus when web content focused
    Before: https://screencast.googleplex.com/cast/NTAyNTkzMjg3NTEzNzAyNHxkMTg4ZmNkOS1jZQ
    
    After: https://screencast.googleplex.com/cast/NjU0MjAwNjY3MDA2NTY2NHw2ZWQ5Y2Q2NS1kNg
    
    Bug: 360423850, 399498687
    Test: CompositorViewHolderUnitTest
    Change-Id: I679315d194d85639ea1dd310096fd3a99d1c8631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6499170
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1454000}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/CompositorViewHolderUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/CompositorViewHolderUnitTest.java
@@ -982,6 +982,12 @@ public class CompositorViewHolderUnitTest {
         verifyBackgroundRemoved();
     }
 
+    @Test
+    public void testFocusOnWebContent_resetsKeyboardFocus() {
+        mCompositorViewHolder.setFocusOnFirstContentViewItem();
+        verify(mCompositorViewHolder).resetKeyboardFocus();
+    }
+
     private static void runCurrentTasks() {
         ShadowLooper.runUiThreadTasks();
     }
