59495bbcfca0e06d01a79b061dae3f38707b8c78
Abe Boujane <boujane@google.com>
2025-05-01 07:44:24 -0700

    Ensure WasFulfilled is refreshed from NoticeService
    Change-Id: I1242e4992b151c14614ae3a2d1b21255bca27968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6505521
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1454443}

--- chrome/browser/privacy_sandbox/notice/notice_service.cc
+++ chrome/browser/privacy_sandbox/notice/notice_service.cc
@@ -31,6 +31,12 @@ PrivacySandboxNoticeService::PrivacySandboxNoticeService(
   CHECK(desktop_view_manager_);
 #endif  // !BUILDFLAG(IS_ANDROID)
 
+  // Refresh fulfillment status for all notices at service initialization.
+  for (auto& [_, notice] : catalog_->GetNoticeMap()) {
+    CHECK(notice);
+    notice->RefreshFulfillmentStatus(*notice_storage_);
+  }
+
   EmitStartupHistograms();
 }
 
@@ -51,6 +57,8 @@ void PrivacySandboxNoticeService::EventOccurred(
   CHECK(notice_ptr != catalog_->GetNoticeMap().end());
   CHECK(notice_ptr->second != nullptr);
 
+  // Refresh fulfillment status after an event has occurred.
+  notice_ptr->second->RefreshFulfillmentStatus(*notice_storage());
   notice_ptr->second->UpdateTargetApiResults(event);
 }
 
