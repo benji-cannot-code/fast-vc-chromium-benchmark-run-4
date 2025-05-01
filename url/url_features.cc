e813d13aec5a6a386e3ea4ad9bc5b1257303768e
Ari Chivukula <arichiv@chromium.org>
2025-05-01 03:07:05 -0700

    Clean up stale base::Feature "UseIDNA2008NonTransitional"
    This seems to have launched, so we can remove the feature that toggles
    it off.
    
    Fixed: 356625420
    Change-Id: I62b5ea64efd72da44bb5cc82aa12cea340518655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501932
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454380}

--- url/url_features.cc
+++ url/url_features.cc
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace url {
 
-BASE_FEATURE(kUseIDNA2008NonTransitional,
-             "UseIDNA2008NonTransitional",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Kill switch for crbug.com/1416006.
 BASE_FEATURE(kStandardCompliantNonSpecialSchemeURLParsing,
              "StandardCompliantNonSpecialSchemeURLParsing",
@@ -20,17 +16,6 @@ BASE_FEATURE(kStandardCompliantNonSpecialSchemeURLParsing,
 BASE_FEATURE(kDisallowSpaceCharacterInURLHostParsing,
              "DisallowSpaceCharacterInURLHostParsing",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-bool IsUsingIDNA2008NonTransitional() {
-  // If the FeatureList isn't available yet, fall back to the feature's default
-  // state. This may happen during early startup, see crbug.com/1441956.
-  if (!base::FeatureList::GetInstance()) {
-    return kUseIDNA2008NonTransitional.default_state ==
-           base::FEATURE_ENABLED_BY_DEFAULT;
-  }
-
-  return base::FeatureList::IsEnabled(kUseIDNA2008NonTransitional);
-}
 
 bool IsUsingStandardCompliantNonSpecialSchemeURLParsing() {
   // If the FeatureList isn't available yet, fall back to the feature's default
