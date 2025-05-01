80010c8169498d4f5695a4c8ee01da1e269817c9
Stephen Nusko <nuskos@chromium.org>
2025-05-01 11:45:57 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This covers directories:
    ['components/page_load_metrics', 'components/history_clusters', 'components/cronet', 'components/query_parser', 'components/speech', 'components/affiliations', 'components/assist_ranker', 'components/device_event_log', 'components/filename_generation', 'components/leveldb_proto', 'components/open_from_clipboard', 'components/reduce_accept_language', 'components/segmentation_platform', 'components/webcrypto']
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: I3d8bb9c6a298cd5639cdf80f2c61fdc0aae728b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504337
    Owners-Override: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454573}

--- components/affiliations/core/browser/hash_affiliation_fetcher.cc
+++ components/affiliations/core/browser/hash_affiliation_fetcher.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/callback_forward.h"
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
--- components/history_clusters/history_clusters_internals/webui/history_clusters_internals_ui.cc
+++ components/history_clusters/history_clusters_internals/webui/history_clusters_internals_ui.cc
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/354691088): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include "components/history_clusters/history_clusters_internals/webui/history_clusters_internals_ui.h"
 
