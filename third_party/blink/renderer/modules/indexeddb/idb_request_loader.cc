83771fb7d0c097337ba672103be5233f28c22d2f
Mingyu Lei <leimy@chromium.org>
2025-05-01 01:20:39 -0700

    IDB: Also logging the OK status for IDBRequestLoader
    To understand the ratio of the error case.
    
    Bug: 342779913
    Change-Id: Ided6e149eb68093b07838ab812913fafde71c689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6493971
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454363}

--- third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
@@ -141,7 +141,6 @@ void IDBRequestLoader::DidFail(FileErrorCode error_code) {
   DCHECK(file_reader_loading_);
   file_reader_loading_ = false;
 #endif  // DCHECK_IS_ON()
-  base::UmaHistogramEnumeration("IndexedDB.LargeValueReadError", error_code);
   OnLoadComplete(error_code);
 }
 
@@ -150,6 +149,7 @@ void IDBRequestLoader::OnLoadComplete(FileErrorCode error_code) {
   DCHECK(started_);
   DCHECK(!canceled_);
 #endif  // DCHECK_IS_ON()
+  base::UmaHistogramEnumeration("IndexedDB.LargeValueReadResult", error_code);
   DOMException* exception = nullptr;
   // Translate the error code from the internal file read operation to an
   // appropriate `DOMException` for the IndexedDB operation.
