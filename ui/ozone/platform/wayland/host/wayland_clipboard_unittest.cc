6303ea3d82f7676254e369a67b0cf1913b155d0f
Stephen Nusko <nuskos@chromium.org>
2025-05-01 11:50:10 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This covers directories:
    ui/ozone/platform/wayland
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: I771439e77a311ef4781f0d257a323d87be5a170b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504536
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Owners-Override: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454576}

--- ui/ozone/platform/wayland/host/wayland_clipboard_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_clipboard_unittest.cc
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/containers/span.h"
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include <linux/input.h>
 #include <wayland-server.h>
