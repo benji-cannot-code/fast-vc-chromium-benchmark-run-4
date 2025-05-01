4bcd9bbbcc4de9b8fd2b55f5c794428cff4c1b0b
Di Zhang <dizhangg@chromium.org>
2025-05-01 13:12:10 -0700

    [menu] Basic structure for menuitem, menubar, menulist
    This patch adds initial structure for the <menuitem>, <menubar>,
    <menulist> elements as described in the OpenUI explainer [1].
    
    Changes are behind the runtime flag MenuElements.
    
    [1] https://open-ui.org/components/menu.explainer/
    
    Change-Id: I6f73a5f0d5c33133e22f2fca8a5c1cddc135d111
    Bug: 406566432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6503394
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454632}

---
+++ third_party/blink/renderer/core/html/html_menu_bar_element.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/html/html_menu_bar_element.h"
+
+#include "third_party/blink/renderer/core/html_names.h"
+
+namespace blink {
+
+HTMLMenuBarElement::HTMLMenuBarElement(Document& document)
+    : HTMLElement(html_names::kMenubarTag, document) {}
+
+}  // namespace blink
---
+++ third_party/blink/renderer/core/html/html_menu_list_element.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_MENU_LIST_ELEMENT_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_MENU_LIST_ELEMENT_H_
+
+#include "third_party/blink/renderer/core/html/html_element.h"
+
+namespace blink {
+
+class HTMLMenuListElement final : public HTMLElement {
+ public:
+  explicit HTMLMenuListElement(Document&);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_MENU_LIST_ELEMENT_H_
