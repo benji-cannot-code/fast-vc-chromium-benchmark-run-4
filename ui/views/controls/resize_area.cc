26ccd9c9c9fd768bd36feead636513d3de439bc0
Caroline Rising <corising@chromium.org>
2025-05-01 06:50:13 -0700

    [SxS] Prevent swapping on a double click that was part of a resize.
    Bug: 403350873
    Change-Id: Icd52eed342acd65ee4142d9e8f22820dd1003ea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6500999
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454419}

--- ui/views/controls/resize_area.cc
+++ ui/views/controls/resize_area.cc
@@ -34,7 +34,9 @@ void ResizeArea::OnGestureEvent(ui::GestureEvent* event) {
     ReportResizeAmount(event->x(), false);
     event->SetHandled();
   } else if (event->type() == ui::EventType::kGestureEnd) {
-    ReportResizeAmount(event->x(), true);
+    if (is_resizing_) {
+      ReportResizeAmount(event->x(), true);
+    }
     event->SetHandled();
   }
 }
@@ -58,7 +60,9 @@ bool ResizeArea::OnMouseDragged(const ui::MouseEvent& event) {
 }
 
 void ResizeArea::OnMouseReleased(const ui::MouseEvent& event) {
-  ReportResizeAmount(event.x(), true);
+  if (is_resizing_) {
+    ReportResizeAmount(event.x(), true);
+  }
 }
 
 void ResizeArea::OnMouseCaptureLost() {
@@ -69,6 +73,7 @@ void ResizeArea::ReportResizeAmount(int resize_amount, bool last_update) {
   gfx::Point point(resize_amount, 0);
   View::ConvertPointToScreen(this, &point);
   resize_amount = point.x() - initial_position_;
+  is_resizing_ = !last_update;
   delegate_->OnResize(base::i18n::IsRTL() ? -resize_amount : resize_amount,
                       last_update);
 }
