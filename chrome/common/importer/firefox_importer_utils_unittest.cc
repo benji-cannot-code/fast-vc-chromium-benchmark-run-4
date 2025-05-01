babc3575502db2ae3ce6e3927ce5e7f7591665b7
Stephen Nusko <nuskos@chromium.org>
2025-05-01 14:05:37 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This covers directories:
    ['content', 'chrome', 'sandbox', 'skia', 'dbus', 'remoting', 'testing']
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: I2fc638ec819fde0d50fdfb16a8267dc6b2115025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504299
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Owners-Override: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454668}

--- chrome/common/importer/firefox_importer_utils_unittest.cc
+++ chrome/common/importer/firefox_importer_utils_unittest.cc
@@ -4,10 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include "chrome/common/importer/firefox_importer_utils.h"
 
