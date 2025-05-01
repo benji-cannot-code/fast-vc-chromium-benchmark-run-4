bc4db34fbeda17c23869016a7d5a82cc165c1358
Monica Chintala <monicach@microsoft.com>
2025-04-30 18:55:04 -0700

    Implement race-network-and-cache
    This CL implements race network and cache for SW main and sub resource.
    
    This CL also fix a test bug in service_worker_main_resource_loader_unittest.cc to properly simulate network error with the server response.
    
    Future CL includes adding race-network-and-cache support to WebIDL.
    
    Bug: 370844790
    Change-Id: Ia31566da764a048228668d2e9c961718f62c2040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6452845
    Reviewed-by: Liang Zhao <lzhao@microsoft.com>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Monica Chintala <monicach@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1454290}

--- third_party/blink/public/common/service_worker/service_worker_subresource_load_metrics.h
+++ third_party/blink/public/common/service_worker/service_worker_subresource_load_metrics.h
@@ -137,6 +137,11 @@ struct ServiceWorkerSubresourceLoadMetrics {
   // Routing API, from navigation start till onload event.
   uint32_t matched_race_network_and_fetch_router_source_count = 0;
 
+  // Total number of sub-resources which were matched to the
+  // `RouterSourceEnum.race-network-and-cache` in ServiceWorker Static
+  // Routing API, from navigation start till onload event.
+  uint32_t matched_race_network_and_cache_router_source_count = 0;
+
   // Total router evaluation time of ServiceWorker Static Routing API
   // for sub-resources.
   base::TimeDelta total_router_evaluation_time_for_subresources;
@@ -184,6 +189,8 @@ struct ServiceWorkerSubresourceLoadMetrics {
                other.matched_cache_router_source_count &&
            matched_race_network_and_fetch_router_source_count ==
                other.matched_race_network_and_fetch_router_source_count &&
+           matched_race_network_and_cache_router_source_count ==
+               other.matched_race_network_and_cache_router_source_count &&
            total_router_evaluation_time_for_subresources ==
                other.total_router_evaluation_time_for_subresources &&
            total_cache_lookup_time_for_subresources ==
