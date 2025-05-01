d83d6d86ae6a8462f2a4d23f9b2b47c93acfa520
Stacy Gaikovaia <gaiko@google.com>
2025-05-01 11:15:31 -0700

    Fix: Reorder test class members
    Reland "Detangle cyclical references in CC metrics"
    
    This reverts commit 3d158ed80c7bfed31fbbca9fd01dfc17d75e9025.
    
    Reason for revert: Re-landing with msan fix.
    
    Original change's description:
    > Revert "Detangle cyclical references in CC metrics"
    >
    > This reverts commit 9719c449b29272a6101307c75368c9e02ac7b43c.
    >
    > Reason for revert:
    > Failing on Linux MSAN https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/55337/overview
    >
    > Bug: 404588481
    > Original change's description:
    > > Detangle cyclical references in CC metrics
    > >
    > > This refactor performs the following:
    > > * Remove Add/Rem tracker calls from FSTC to CFRC
    > >   and move all logic to FSTC
    > > * Have CFRC delegate active tracker calls to FSTC
    > > * Make DFC a FSTC member rather than delegating
    > >   through CFRC
    > > * Remove FrameSorter from DFC, move to CFRC globals
    > >   and register both DFC and FSTC as observers of
    > >   FrameSorter. This replaces Callback pattern with
    > >   the observer pattern.
    > > * Rename former callbacks to "AddSortedFrame" so
    > >   they can use the same observer interface.
    > >
    > > Details here: go/chrome-cc-metrics-cyclical-deps
    > >
    > > Change-Id: Ib834e656488b756e136d1938b3d91efd343c9152
    > > Bug: 404588481
    > > Compile-Size: Size increase is unavoidable.
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6431485
    > > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > > Commit-Queue: Stacy Gaikovaia <gaiko@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1448555}
    >
    > Bug: 404588481
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ie44c32f7203ad112d9fdea9965f5f5f8c6adbdcb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6471869
    > Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    > Owners-Override: Lingqi Chi <lingqi@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1448712}
    
    Bug: 404588481
    Compile-Size: Size increase is unavoidable.
    Change-Id: I27ebce61d7dcc65732ccc2668a265a25168cbad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6502074
    Commit-Queue: Stacy Gaikovaia <gaiko@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454554}

--- cc/metrics/frame_sorter.cc
+++ cc/metrics/frame_sorter.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/metrics/frame_sorter.h"
 
+#include <cstddef>
 #include <utility>
 
 #include "cc/metrics/frame_info.h"
@@ -30,11 +31,18 @@ bool FrameState::IsComplete() const {
   return (on_begin_counter == ack_counter);
 }
 
-FrameSorter::FrameSorter(InOrderBeginFramesCallback callback)
-    : flush_callback_(std::move(callback)) {
-  DCHECK(!flush_callback_.is_null());
+FrameSorter::FrameSorter() = default;
+FrameSorter::~FrameSorter() {
+  observers_.Clear();
 }
-FrameSorter::~FrameSorter() = default;
+
+void FrameSorter::AddObserver(FrameSorterObserver* observer) {
+  observers_.AddObserver(observer);
+}
+
+void FrameSorter::RemoveObserver(FrameSorterObserver* observer) {
+  observers_.RemoveObserver(observer);
+}
 
 void FrameSorter::AddNewFrame(const viz::BeginFrameArgs& args) {
   if (current_source_id_.has_value() &&
@@ -133,7 +141,9 @@ void FrameSorter::Reset() {
     const auto& frame_id = pending_frame.frame_id;
     auto& frame_state = frame_states_[frame_id];
     if (frame_state.IsComplete() && !frame_state.should_ignore()) {
-      flush_callback_.Run(pending_frame, frame_infos_[frame_id]);
+      for (auto& observer : observers_) {
+        observer.AddSortedFrame(pending_frame, frame_infos_[frame_id]);
+      }
       frame_states_.erase(frame_id);
       frame_infos_.erase(frame_id);
       continue;
@@ -153,7 +163,9 @@ void FrameSorter::FlushFrames() {
     if (!frame_state.IsComplete())
       break;
     ++flushed_count;
-    flush_callback_.Run(first, frame_infos_[frame_id]);
+    for (auto& observer : observers_) {
+      observer.AddSortedFrame(first, frame_infos_[frame_id]);
+    }
     frame_states_.erase(frame_id);
     frame_infos_.erase(frame_id);
     pending_frames_.pop_front();
