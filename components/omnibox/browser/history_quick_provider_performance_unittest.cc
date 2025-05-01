42d7f93704aef74f442b8ef224aea24086dd6571
Stephen Nusko <nuskos@chromium.org>
2025-04-30 22:02:26 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This covers directories:
    ['components/omnibox', 'components/policy', 'components/os_crypt', 'components/services', 'components/viz', 'components/bookmarks', 'components/breadcrumbs', 'components/gcm_driver', 'components/gwp_asan', 'components/paint_preview', 'components/power_metrics', 'components/sync_bookmarks', 'components/url_formatter']
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: I851145d2d2b6405f6a04d52e744653ed1fd5b524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504336
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454332}

--- components/omnibox/browser/history_quick_provider_performance_unittest.cc
+++ components/omnibox/browser/history_quick_provider_performance_unittest.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <array>
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include <algorithm>
 #include <memory>
