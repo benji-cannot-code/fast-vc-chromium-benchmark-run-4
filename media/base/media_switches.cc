88ae39155b5652e6fce52ddfde03de9d8e06d4fa
Jordan Bayles <jophba@chromium.org>
2025-05-01 07:39:04 -0700

    [Cast Streaming] Add *very* experiment HEVC support
    This patch begins the (probably) long road to enabling HEVC support
    in Cast Streaming, by adding a feature flag and allowing it to be manually enabled in chrome://flags.
    
    \#cast-streaming-hardware-hevc
    
    Change-Id: I89373a67984fb3b2c8240c5f02f5be3202f4f9aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6442545
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1454441}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -270,14 +270,14 @@ const char kEnablePrimaryNodeAccessForVkmsTesting[] =
 
 const char kCastStreamingForceDisableHardwareH264[] =
     "cast-streaming-force-disable-hardware-h264";
-const char kCastStreamingForceDisableHardwareVp8[] =
-    "cast-streaming-force-disable-hardware-vp8";
-const char kCastStreamingForceDisableHardwareVp9[] =
-    "cast-streaming-force-disable-hardware-vp9";
 const char kCastStreamingForceEnableHardwareH264[] =
     "cast-streaming-force-enable-hardware-h264";
+const char kCastStreamingForceDisableHardwareVp8[] =
+    "cast-streaming-force-disable-hardware-vp8";
 const char kCastStreamingForceEnableHardwareVp8[] =
     "cast-streaming-force-enable-hardware-vp8";
+const char kCastStreamingForceDisableHardwareVp9[] =
+    "cast-streaming-force-disable-hardware-vp9";
 const char kCastStreamingForceEnableHardwareVp9[] =
     "cast-streaming-force-enable-hardware-vp9";
 
@@ -1559,6 +1559,10 @@ BASE_FEATURE(kCastStreamingAv1,
 // the legacy linear algorithm.
 BASE_FEATURE(kCastStreamingExponentialVideoBitrateAlgorithm,
              "CastStreamingExponentialVideoBitrateAlgorithm",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kCastStreamingHardwareHevc,
+             "CastStreamingHardwareHevc",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // TODO(crbug.com/282984511): Remove after M151.
