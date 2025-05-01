c29b3724d874f21fe0c83d9dbe7d7d29bcd3bd77
Stephen Nusko <nuskos@chromium.org>
2025-05-01 11:52:45 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This covers directories:
    ['chrome/browser/enterprise', 'chrome/browser', 'chrome/browser/policy', 'chrome/browser/resource_coordinator', 'chrome/browser/devtools', 'chrome/browser/importer', 'chrome/browser/new_tab_page', 'chrome/browser/optimization_guide', 'chrome/browser/web_applications', 'chrome/browser/webauthn']
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: Ie141818a03918dc07ff1dc462c4ce9c3929c4ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504296
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Owners-Override: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454580}

--- chrome/browser/browser_keyevents_browsertest.cc
+++ chrome/browser/browser_keyevents_browsertest.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include <stddef.h>
 
