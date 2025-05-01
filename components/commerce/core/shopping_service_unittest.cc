d3a25a9ec8f39372f243a0c891fd594305c2bad4
Stephen Nusko <nuskos@chromium.org>
2025-05-01 14:04:29 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This covers directories:
    ['components/variations', 'components/commerce', 'components/domain_reliability', 'components/payments', 'components/system_cpu', 'components/zucchini', 'components/error_page', 'components/feedback', 'components/media_router', 'components/metrics', 'components/reporting', 'components/search_engines']
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: Ib6241b62a104471668506478b357dd79adb146f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504356
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454666}

--- components/commerce/core/shopping_service_unittest.cc
+++ components/commerce/core/shopping_service_unittest.cc
@@ -4,10 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/optimization_guide/core/optimization_guide_proto_util.h"
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include <array>
 #include <string>
