f164373f60fd725ddde1ce30c29601b231d31c69
Brian Sheedy <bsheedy@chromium.org>
2025-05-01 16:01:53 -0700

    Add memory capacity tags/serialization
    Adds typ tags to GPU tests for when a test machine has more or less than
    16 GB of system memory. Similar logic is then used to serialize two
    cases where tests are flaky due to allocating 16 GB of memory on systems
    with 8 GB of memory.
    
    Bug: 373852610, 412955603
    Change-Id: I9c834f9ed8092aaf29ad7e942f5e328ec6206c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6499149
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454728}

--- content/test/gpu/gpu_tests/gpu_integration_test_unittest.py
+++ content/test/gpu/gpu_tests/gpu_integration_test_unittest.py
@@ -321,7 +321,9 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
     self.assertFalse(test_class._is_asan)
 
   @mock.patch('gpu_tests.util.host_information.IsLinux', return_value=False)
-  def testGenerateNvidiaExampleTags(self, _) -> None:
+  @mock.patch('gpu_tests.util.host_information.GetSystemMemoryBytes',
+              return_value=32_000_000_000)
+  def testGenerateNvidiaExampleTags(self, _, __) -> None:
     platform = fakes.FakePlatform('win', 'win10')
     browser = fakes.FakeBrowser(platform, 'release')
     browser._returned_system_info = _GetSystemInfo(
@@ -341,10 +343,13 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
             'target-cpu-64',
             'no-clang-coverage',
             'graphite-disabled',
+            'memory_ge_16gb',
         ]))
 
   @mock.patch('gpu_tests.util.host_information.IsLinux', return_value=False)
-  def testGenerateVendorTagUsingVendorString(self, _) -> None:
+  @mock.patch('gpu_tests.util.host_information.GetSystemMemoryBytes',
+              return_value=32_000_000_000)
+  def testGenerateVendorTagUsingVendorString(self, _, __) -> None:
     platform = fakes.FakePlatform('mac', 'mojave')
     browser = fakes.FakeBrowser(platform, 'release')
     browser._returned_system_info = _GetSystemInfo(
@@ -367,10 +372,13 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
             'renderer-skia-gl',
             'no-clang-coverage',
             'graphite-disabled',
+            'memory_ge_16gb',
         ]))
 
   @mock.patch('gpu_tests.util.host_information.IsLinux', return_value=False)
-  def testGenerateVendorTagUsingDeviceString(self, _) -> None:
+  @mock.patch('gpu_tests.util.host_information.GetSystemMemoryBytes',
+              return_value=32_000_000_000)
+  def testGenerateVendorTagUsingDeviceString(self, _, __) -> None:
     platform = fakes.FakePlatform('mac', 'mojave')
     browser = fakes.FakeBrowser(platform, 'release')
     browser._returned_system_info = _GetSystemInfo(
@@ -391,7 +399,27 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
             'renderer-skia-gl',
             'no-clang-coverage',
             'graphite-disabled',
+            'memory_ge_16gb',
         ]))
+
+  @mock.patch('gpu_tests.util.host_information.IsLinux', return_value=False)
+  def testGenerateMemoryTags(self, _):
+    platform = fakes.FakePlatform('win', 'win10')
+    browser = fakes.FakeBrowser(platform, 'release')
+    browser._returned_system_info = _GetSystemInfo(
+        gpu=VENDOR_NVIDIA, device=0x1cb3, gl_renderer='ANGLE Direct3D9')
+
+    with mock.patch('gpu_tests.util.host_information.GetSystemMemoryBytes',
+                    return_value=8_000_000_000):
+      tags = _GetTagsToTest(browser)
+      self.assertIn('memory_lt_16gb', tags)
+      self.assertNotIn('memory_ge_16gb', tags)
+
+    with mock.patch('gpu_tests.util.host_information.GetSystemMemoryBytes',
+                    return_value=16_000_000_000):
+      tags = _GetTagsToTest(browser)
+      self.assertNotIn('memory_lt_16gb', tags)
+      self.assertIn('memory_ge_16gb', tags)
 
   @mock.patch.dict(os.environ, clear=True)
   def testGenerateDisplayServer(self) -> None:
