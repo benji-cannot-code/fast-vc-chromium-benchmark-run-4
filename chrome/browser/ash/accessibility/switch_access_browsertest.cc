1e58bc1c08f2f578275e7f4af80661b5b912bb43
Akihiro Ota <akihiroota@chromium.org>
2025-05-01 14:44:32 -0700

    Switch access: Parametrize C++ browser tests on manifest version
    Adds the ability to parametrize switch access tests based on manifest
    version. Currently we only instantiate manifest v2 tests because there
    are failures with the manifest v3 tests.
    
    Bug: 388867933
    Change-Id: I71919ee03f6ab90716288dd4c40a58ffd20cb690
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6500998
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454682}

--- chrome/browser/ash/accessibility/switch_access_browsertest.cc
+++ chrome/browser/ash/accessibility/switch_access_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "chrome/browser/ash/accessibility/accessibility_feature_browsertest.h"
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
+#include "chrome/browser/ash/accessibility/accessibility_test_utils.h"
 #include "chrome/browser/ash/accessibility/automation_test_utils.h"
 #include "chrome/browser/ash/accessibility/switch_access_test_utils.h"
 #include "chrome/browser/ui/browser.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
+#include "ui/accessibility/accessibility_features.h"
 #include "ui/aura/client/cursor_client.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/display/screen.h"
@@ -21,12 +23,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class SwitchAccessTest : public AccessibilityFeatureBrowserTest {
+class SwitchAccessTest : public AccessibilityFeatureBrowserTest,
+                         public ::testing::WithParamInterface<ManifestVersion> {
  protected:
   SwitchAccessTest() = default;
   ~SwitchAccessTest() override = default;
   SwitchAccessTest(const SwitchAccessTest&) = delete;
   SwitchAccessTest& operator=(const SwitchAccessTest&) = delete;
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    std::vector<base::test::FeatureRef> enabled_features;
+    std::vector<base::test::FeatureRef> disabled_features;
+    if (GetParam() == ManifestVersion::kTwo) {
+      disabled_features.push_back(
+          ::features::kAccessibilityManifestV3SwitchAccess);
+    } else if (GetParam() == ManifestVersion::kThree) {
+      enabled_features.push_back(
+          ::features::kAccessibilityManifestV3SwitchAccess);
+    }
+    scoped_feature_list_.InitWithFeatures(enabled_features, disabled_features);
+    InProcessBrowserTest::SetUpCommandLine(command_line);
+  }
 
   void SetUpOnMainThread() override {
     switch_access_test_utils_ = std::make_unique<SwitchAccessTestUtils>(
@@ -75,10 +92,17 @@ class SwitchAccessTest : public AccessibilityFeatureBrowserTest {
  private:
   std::unique_ptr<SwitchAccessTestUtils> switch_access_test_utils_;
   std::unique_ptr<ui::test::EventGenerator> generator_;
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
+INSTANTIATE_TEST_SUITE_P(ManifestV2,
+                         SwitchAccessTest,
+                         ::testing::Values(ManifestVersion::kTwo));
+
+// TODO(https://crbug.com/388867933): Add manifest v3 variant.
+
 // Flaky. See https://crbug.com/1224254.
-IN_PROC_BROWSER_TEST_F(SwitchAccessTest, DISABLED_ConsumesKeyEvents) {
+IN_PROC_BROWSER_TEST_P(SwitchAccessTest, DISABLED_ConsumesKeyEvents) {
   utils()->EnableSwitchAccess({'1', 'A'} /* select */, {'2', 'B'} /* next */,
                               {'3', 'C'} /* previous */);
   AutomationTestUtils test_utils(extension_misc::kSwitchAccessExtensionId);
@@ -105,7 +129,7 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest, DISABLED_ConsumesKeyEvents) {
                test_utils.GetValueForNodeWithClassName("sa_input").c_str());
 }
 
-IN_PROC_BROWSER_TEST_F(SwitchAccessTest, NavigateGroupings) {
+IN_PROC_BROWSER_TEST_P(SwitchAccessTest, NavigateGroupings) {
   utils()->EnableSwitchAccess({'1', 'A'} /* select */, {'2', 'B'} /* next */,
                               {'3', 'C'} /* previous */);
 
@@ -156,7 +180,7 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest, NavigateGroupings) {
   utils()->WaitForFocusRing("primary", "button", "Southeast");
 }
 
-IN_PROC_BROWSER_TEST_F(SwitchAccessTest, NavigateButtonsInTextFieldMenu) {
+IN_PROC_BROWSER_TEST_P(SwitchAccessTest, NavigateButtonsInTextFieldMenu) {
   utils()->EnableSwitchAccess({'1', 'A'} /* select */, {'2', 'B'} /* next */,
                               {'3', 'C'} /* previous */);
 
@@ -218,7 +242,7 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest, NavigateButtonsInTextFieldMenu) {
 }
 
 // TODO(crbug.com/40926594): Enable after fixing flakiness.
-IN_PROC_BROWSER_TEST_F(SwitchAccessTest, DISABLED_TypeIntoVirtualKeyboard) {
+IN_PROC_BROWSER_TEST_P(SwitchAccessTest, DISABLED_TypeIntoVirtualKeyboard) {
   utils()->EnableSwitchAccess({'1', 'A'} /* select */, {'2', 'B'} /* next */,
                               {'3', 'C'} /* previous */);
 
@@ -258,7 +282,7 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest, DISABLED_TypeIntoVirtualKeyboard) {
 #define MAYBE_PointScanClickWhenMouseEventsEnabled \
   PointScanClickWhenMouseEventsEnabled
 #endif
-IN_PROC_BROWSER_TEST_F(SwitchAccessTest,
+IN_PROC_BROWSER_TEST_P(SwitchAccessTest,
                        MAYBE_PointScanClickWhenMouseEventsEnabled) {
   utils()->EnableSwitchAccess({'1', 'A'} /* select */, {'2', 'B'} /* next */,
                               {'3', 'C'} /* previous */);
@@ -297,7 +321,7 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest,
 #define MAYBE_PointScanClickWhenMouseEventsDisabled \
   PointScanClickWhenMouseEventsDisabled
 #endif
-IN_PROC_BROWSER_TEST_F(SwitchAccessTest,
+IN_PROC_BROWSER_TEST_P(SwitchAccessTest,
                        MAYBE_PointScanClickWhenMouseEventsDisabled) {
   utils()->EnableSwitchAccess({'1', 'A'} /* select */, {'2', 'B'} /* next */,
                               {'3', 'C'} /* previous */);
