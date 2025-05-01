2fa986cb113a76a8acd03396a7b5288d47164bdc
Briana McClure <bmcclure@google.com>
2025-05-01 11:10:03 -0700

    [iOS] Create Welcome Back pref to track feature item eligibility
    This CL creates the kWelcomeBackEligibleItems pref that contains the
    features eligible for display on the Welcome Back promo and registers
    the pref in browser_prefs.mm. This CL also creates functions for
    updating the pref and converting a BestFeaturesItemType to a string.
    
    Bug: 410072328
    Change-Id: I68c7731cf3c5c9732f179c1826daed881716dd17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6496214
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Huiting Yu <huitingyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1454547}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -101,6 +101,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/download/model/auto_deletion/auto_deletion_service.h"
 #import "ios/chrome/browser/drive/model/drive_policy.h"
 #import "ios/chrome/browser/first_run/model/first_run.h"
+#import "ios/chrome/browser/first_run/ui_bundled/welcome_back/model/welcome_back_prefs.h"
 #import "ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_scene_agent.h"
 #import "ios/chrome/browser/memory/model/memory_debugger_manager.h"
 #import "ios/chrome/browser/metrics/model/constants.h"
@@ -419,6 +420,7 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   TipsNotificationClient::RegisterLocalStatePrefs(registry);
   auto_deletion::AutoDeletionService::RegisterLocalStatePrefs(registry);
   push_notification_prefs::RegisterLocalStatePrefs(registry);
+  RegisterWelcomeBackLocalStatePrefs(registry);
 
 #if !BUILDFLAG(IS_IOS_MACCATALYST)
   default_status::RegisterDefaultStatusPrefs(registry);
