293e68aaa5f6f61119255dad0a6d09a351d021a2
Andy Phan <andyphan@chromium.org>
2025-05-01 16:52:43 -0700

    [PDF Ink Signatures] Add mouse text selection highlighting
    Introduce mouse text selection highlighting while in annotation mode.
    While using the highlighter brush during annotating, users can select
    text, which will draw a highlighting stroke matching the text selection
    rectangle.
    
    Rendering for in-progress text selections will be added in
    https://crrev.com/c/6456394. This CL focuses on text selection
    highlighting after the user has finished text selecting.
    
    Bug: 342445982
    Change-Id: I1344170b85781ab1fcb11419c81fd891fca3eefd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313743
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454750}

--- pdf/pdf_ink_module.h
+++ pdf/pdf_ink_module.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <set>
+#include <utility>
 #include <variant>
 #include <vector>
 
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "pdf/buildflags.h"
+#include "pdf/page_orientation.h"
 #include "pdf/pdf_ink_brush.h"
 #include "pdf/pdf_ink_ids.h"
 #include "pdf/pdf_ink_undo_redo_model.h"
@@ -135,6 +137,10 @@ class PdfInkModule {
   ~PdfInkModule();
 
   bool enabled() const { return enabled_; }
+
+  // Returns whether the text selection change event should be blocked to
+  // prevent modifying the clipboard content.
+  bool ShouldBlockTextSelectionChanged();
 
   // Determines if there are any `drawing_stroke_state().inputs` to be drawn.
   bool HasInputsToDraw() const;
@@ -336,6 +342,31 @@ class PdfInkModule {
 
   // Shared code for the Erase methods above.
   void EraseHelper(const gfx::PointF& position, int page_index);
+
+  // Return values have the same semantics as On{Mouse,Touch}*() above.
+  bool StartTextHighlight(const gfx::PointF& position,
+                          int click_count,
+                          base::TimeTicks timestamp);
+  bool ContinueTextHighlight(const gfx::PointF& position);
+  bool FinishTextHighlight();
+
+  // Returns a highlighter stroke that matches the position and size of
+  // `selection_rect`. `selection_rect` must be in screen coordinates.
+  ink::Stroke GetHighlightStrokeFromSelectionRect(
+      const gfx::Rect& selection_rect);
+
+  // Returns the start and end point of a stroke that covers `selection_rect`
+  // with a size of `brush_size`. `brush_size` must be large enough to cover
+  // `selection_rect`'s smallest dimension. `selection_rect` must be in screen
+  // coordinates.
+  std::pair<gfx::PointF, gfx::PointF> GetPointsForTextSelectionHighlightStroke(
+      const gfx::Rect& selection_rect,
+      float brush_size);
+
+  // Converts PdfInkModuleClient's text selection to strokes and returns a
+  // mapping of 0-based page indices to a list of those strokes. See comments
+  // for `TextHighlightState::highlight_strokes`.
+  std::map<int, std::vector<ink::Stroke>> GetTextSelectionAsStrokes();
 
   // Sets `using_stylus_instead_of_touch_` to true if `tool_type` is
   // `ink::StrokeInput::ToolType::kStylus`. Otherwise do nothing.
