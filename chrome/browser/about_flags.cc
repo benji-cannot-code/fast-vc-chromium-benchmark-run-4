31e3e0f487983928480f63f3f7c8b2ab69d7a63f
Daseul Lee <dslee@chromium.org>
2025-05-01 13:49:16 -0700

    Create a flag to enable LoRA for Summarizer API
    It sets GWS experiment ID via FeatureVariation.variation_id, which will
    be used by LoRA model config on google3-side to enable LoRA in
    a follow-up google3 CL.
    
    Bug: 414284077
    Change-Id: Ia0dac1b2560c7a43944495884a69c8d21653bc3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501478
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454658}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4608,6 +4608,11 @@ const FeatureEntry::FeatureVariation
         {"(retroactive)", kInvalidateChoiceOnRestoreIsRetroactiveOption,
          std::size(kInvalidateChoiceOnRestoreIsRetroactiveOption), nullptr}};
 
+const FeatureEntry::FeatureVariation
+    kAISummarizationAPIWithAdaptationVaration[] = {
+        {"With Adaptation", nullptr, 0, "3389300"},
+};
+
 // RECORDING USER METRICS FOR FLAGS:
 // -----------------------------------------------------------------------------
 // The first line of the entry is the internal name.
@@ -11051,7 +11056,9 @@ const FeatureEntry kFeatureEntries[] = {
     {"summarization-api-for-gemini-nano",
      flag_descriptions::kSummarizationAPIForGeminiNanoName,
      flag_descriptions::kSummarizationAPIForGeminiNanoDescription, kOsAll,
-     FEATURE_VALUE_TYPE(blink::features::kAISummarizationAPI),
+     FEATURE_WITH_PARAMS_VALUE_TYPE(blink::features::kAISummarizationAPI,
+                                    kAISummarizationAPIWithAdaptationVaration,
+                                    "AISummarizationAPIWithAdaptation"),
      flag_descriptions::kAIAPIsForGeminiNanoLinks},
 
     {"writer-api-for-gemini-nano",
