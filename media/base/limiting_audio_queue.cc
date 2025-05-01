4e3b3e0b65a08dea85d2908928841da78229aeb7
Stephen Nusko <nuskos@chromium.org>
2025-05-01 11:44:31 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This covers directories:
    media/base
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: Ifb6dbd2329910a9bd990c9d3a7709a7f17a42928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504436
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Owners-Override: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454570}

--- media/base/limiting_audio_queue.cc
+++ media/base/limiting_audio_queue.cc
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "media/base/audio_timestamp_helper.h"
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/373960632): Replace unsafe usage once AudioBus is spanified.
-#pragma allow_unsafe_buffers
-#endif
 
 namespace media {
 
