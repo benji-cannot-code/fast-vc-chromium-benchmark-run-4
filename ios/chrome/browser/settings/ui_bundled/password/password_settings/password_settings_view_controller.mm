571092e660bb86ada527b8ccd1edf9a56b49fa91
Sourav <sinhasourav@google.com>
2025-05-01 07:55:22 -0700

    [iOSImportPasswordsSafari] Added a button to import passwords
    This CL introduces a button in the password manager settings to initiate
    the password import process.
    
    Bug: 407587751
    Change-Id: Ia27abfb07f71322b61b7a4389eeb162d6a0ecd74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495889
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454451}

--- ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_view_controller.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/app/tests_hook.h"
 #import "ios/chrome/browser/credential_provider/model/features.h"
+#import "ios/chrome/browser/passwords/model/features.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/password_manager_ui_features.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_constants.h"
 #import "ios/chrome/browser/shared/coordinator/utils/credential_provider_settings_utils.h"
@@ -61,6 +62,7 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierGooglePasswordManagerPin,
   SectionIdentifierOnDeviceEncryption,
   SectionIdentifierExportPasswordsButton,
+  SectionIdentifierImportPasswordsButton,
   SectionIdentifierDeleteCredentialsButton,
 };
 
@@ -80,6 +82,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeOnDeviceEncryptionOptedInLearnMore,
   ItemTypeOnDeviceEncryptionSetUp,
   ItemTypeExportPasswordsButton,
+  ItemTypeImportPasswordsButton,
   ItemTypeDeleteCredentialsButton,
   ItemTypeFooter,
 };
@@ -228,6 +231,9 @@ BOOL AutomaticPasskeyUpgradeFeatureEnabled() {
 
   // The item related to the button for exporting passwords.
   TableViewTextItem* _exportPasswordsItem;
+
+  // The item related to the button for importing passwords.
+  TableViewTextItem* _importPasswordsItem;
 }
 
 - (instancetype)init {
@@ -327,6 +333,14 @@ BOOL AutomaticPasskeyUpgradeFeatureEnabled() {
   [self updateExportPasswordsButton];
   [model addItem:_exportPasswordsItem
       toSectionWithIdentifier:SectionIdentifierExportPasswordsButton];
+
+  // Import passwords button.
+  if (base::FeatureList::IsEnabled(kImportPasswordsFromSafari)) {
+    [model addSectionWithIdentifier:SectionIdentifierImportPasswordsButton];
+    _importPasswordsItem = [self createImportPasswordsItem];
+    [model addItem:_importPasswordsItem
+        toSectionWithIdentifier:SectionIdentifierImportPasswordsButton];
+  }
 
   if (base::FeatureList::IsEnabled(
           password_manager::features::kIOSEnableDeleteAllSavedCredentials)) {
@@ -442,6 +456,10 @@ BOOL AutomaticPasskeyUpgradeFeatureEnabled() {
       if (_canExportPasswords) {
         [self.presentationDelegate startExportFlow];
       }
+      break;
+    }
+    case ItemTypeImportPasswordsButton: {
+      // TODO(crbug.com/407587751): Start import flow.
       break;
     }
     case ItemTypeOnDeviceEncryptionSetUp: {
@@ -687,6 +705,16 @@ BOOL AutomaticPasskeyUpgradeFeatureEnabled() {
   exportPasswordsItem.text = l10n_util::GetNSString(IDS_IOS_EXPORT_PASSWORDS);
   exportPasswordsItem.accessibilityTraits = UIAccessibilityTraitButton;
   return exportPasswordsItem;
+}
+
+// Creates the "Import Passwords..." button.
+- (TableViewTextItem*)createImportPasswordsItem {
+  TableViewTextItem* importPasswordsItem =
+      [[TableViewTextItem alloc] initWithType:ItemTypeImportPasswordsButton];
+  importPasswordsItem.text = l10n_util::GetNSString(IDS_IOS_IMPORT_PASSWORDS);
+  importPasswordsItem.accessibilityTraits = UIAccessibilityTraitButton;
+  importPasswordsItem.textColor = [UIColor colorNamed:kBlueColor];
+  return importPasswordsItem;
 }
 
 // Creates the "Delete all data" button.
