c91645282541bb612d1a9cd4341b5cec85297b70
Camillia Smith Barnes <cammie@chromium.org>
2025-05-02 11:59:36 -0700

    Shared Storage: Directly implement SharedStorageObserverInterface
    This CL updates content::protocol::StorageHandler to directly
    implement SharedStorageObserverInterface instead of creating a
    separate member observer. (The latter was previously done due to
    multiple inheritance).
    
    Bug: 401011862
    Change-Id: Ib0970e74464f0d061729d0f6eb1d488486ece2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507106
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455120}

--- content/browser/devtools/protocol/storage_handler.h
+++ content/browser/devtools/protocol/storage_handler.h
@@ -41,7 +41,9 @@ class StorageHandler
     : public DevToolsDomainHandler,
       public Storage::Backend,
       public content::InterestGroupManagerImpl::InterestGroupObserver,
-      public AttributionObserver {
+      public AttributionObserver,
+      public content::SharedStorageRuntimeManager::
+          SharedStorageObserverInterface {
  public:
   explicit StorageHandler(DevToolsAgentHostClient* client);
 
@@ -221,15 +223,21 @@ class StorageHandler
                     bool is_debug_report,
                     const SendResult&) override;
 
-  void NotifySharedStorageAccessed(
+  // SharedStorageObserverInterface
+  GlobalRenderFrameHostId AssociatedMainFrameId() const override;
+  bool ShouldReceiveAllReports() const override;
+  void OnSharedStorageAccessed(
       base::Time access_time,
       blink::SharedStorageAccessScope scope,
       SharedStorageRuntimeManager::SharedStorageObserverInterface::AccessMethod
           method,
       GlobalRenderFrameHostId main_frame_id,
       const std::string& owner_origin,
-      const SharedStorageEventParams& params);
-  void NotifySharedStorageWorkletOperationExecutionFinished(
+      const SharedStorageEventParams& params) override;
+  void OnUrnUuidGenerated(const GURL& urn_uuid) override;
+  void OnConfigPopulated(
+      const std::optional<FencedFrameConfig>& config) override;
+  void OnWorkletOperationExecutionFinished(
       base::Time finished_time,
       base::TimeDelta execution_time,
       SharedStorageRuntimeManager::SharedStorageObserverInterface::AccessMethod
@@ -237,7 +245,7 @@ class StorageHandler
       int operation_id,
       int worklet_id,
       GlobalRenderFrameHostId main_frame_id,
-      const std::string& owner_origin);
+      const std::string& owner_origin) override;
 
   void NotifyCacheStorageListChanged(
       const storage::BucketLocator& bucket_locator);
@@ -270,7 +278,6 @@ class StorageHandler
   raw_ptr<RenderFrameHostImpl> frame_host_ = nullptr;
   std::unique_ptr<CacheStorageObserver> cache_storage_observer_;
   std::unique_ptr<IndexedDBObserver> indexed_db_observer_;
-  std::unique_ptr<SharedStorageObserver> shared_storage_observer_;
   std::unique_ptr<QuotaManagerObserver> quota_manager_observer_;
 
   // Exposes the API for managing storage quota overrides.
@@ -282,6 +289,10 @@ class StorageHandler
 
   base::ScopedObservation<AttributionManager, AttributionObserver>
       attribution_observation_{this};
+  base::ScopedObservation<
+      content::SharedStorageRuntimeManager,
+      content::SharedStorageRuntimeManager::SharedStorageObserverInterface>
+      shared_storage_observation_{this};
 
   base::WeakPtrFactory<StorageHandler> weak_ptr_factory_{this};
 };
