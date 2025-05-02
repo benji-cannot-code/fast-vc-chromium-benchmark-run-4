c39c9d013f48cae5e28096a27501a36ba0da002e
Daniel Cheng <dcheng@chromium.org>
2025-05-02 14:34:10 -0700

    Reland "Clean up kUseRustJsonParser"
    This is a reland of commit fc3436425ec94c94052e1449dda2061beab23785
    
    The original CL was reverted due to a cast-specific shared library
    depending on //base and inadvertently introducing two copies of //base
    when loaded into another process that already had symbols for //base.
    That shared library no longer depends on //base.
    
    Original change's description:
    > Clean up kUseRustJsonParser
    >
    > Bug: 404881580
    > Change-Id: I9e3533b3fdbf436ab37917efbe3cbaf9e7091f67
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492579
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Owners-Override: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1454226}
    
    Bug: 404881580
    Change-Id: I0b7289ff489e6341e078250c7b047a3304fa5ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6503545
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455209}

--- components/webapps/services/web_app_origin_association/web_app_origin_association_parser_unittest.cc
+++ components/webapps/services/web_app_origin_association/web_app_origin_association_parser_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/webapps/services/web_app_origin_association/web_app_origin_association_parser.h"
 
-#include "base/json/json_reader.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 #include "url/origin.h"
@@ -66,11 +65,7 @@ TEST_F(WebAppOriginAssociationParserTest, EmptyStringNull) {
   EXPECT_TRUE(IsAssociationNull(association));
   EXPECT_EQ(1u, GetErrorCount());
 
-  if (base::JSONReader::UsingRust()) {
-    EXPECT_EQ(errors()[0], "EOF while parsing a value at line 1 column 0");
-  } else {
-    EXPECT_EQ(errors()[0], "Line: 1, column: 1, Unexpected token.");
-  }
+  EXPECT_EQ(errors()[0], "EOF while parsing a value at line 1 column 0");
 }
 
 TEST_F(WebAppOriginAssociationParserTest, NoContentParses) {
