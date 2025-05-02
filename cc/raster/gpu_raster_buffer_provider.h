74617927f07108fbb95fb5e5e25e4bc7feb3ddf3
Colin Blundell <blundell@chromium.org>
2025-05-02 00:32:19 -0700

    [//cc] Have GpuRasterBufferProvider take in SII explicitly
    Decoupling GpuRBP from internally getting the SII from the worker's
    RasterContextProvider is a step on the road toward SII eventually not
    hanging off RasterContextProvider.
    
    Bug: 403372453
    Change-Id: Id822986e791e5cff4958613bd4c963aad2856c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449963
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454855}

--- cc/raster/gpu_raster_buffer_provider.h
+++ cc/raster/gpu_raster_buffer_provider.h
@@ -38,6 +38,7 @@ class CC_EXPORT GpuRasterBufferProvider : public RasterBufferProvider {
  public:
   static constexpr float kRasterMetricProbability = 0.01;
   GpuRasterBufferProvider(
+      scoped_refptr<gpu::SharedImageInterface> sii,
       viz::RasterContextProvider* compositor_context_provider,
       viz::RasterContextProvider* worker_context_provider,
       bool is_overlay_candidate,
@@ -132,6 +133,7 @@ class CC_EXPORT GpuRasterBufferProvider : public RasterBufferProvider {
     base::TimeTicks creation_time_;
   };
 
+  const scoped_refptr<gpu::SharedImageInterface> sii_;
   const raw_ptr<viz::RasterContextProvider> compositor_context_provider_;
   const raw_ptr<viz::RasterContextProvider> worker_context_provider_;
   const bool tile_overlay_candidate_;
