3c4e88cc3f23b189055ff626b5a94975e9abc725
Andrew Grieve <agrieve@chromium.org>
2025-05-02 12:55:57 -0700

    Android: Enable new Error Prone checks: IntLiteralCast, UnnecessaryStringBuilder
    Bug: None
    Change-Id: I695a463009ee02c6c5a77b769672cfe3767b32bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6506169
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455154}

--- build/android/gyp/errorprone.py
+++ build/android/gyp/errorprone.py
@@ -14,6 +14,7 @@ from util import server_utils
 
 # Add a check here to cause the suggested fix to be applied while compiling.
 # Use this when trying to enable more checks.
+# BE SURE TO BUILD WITH --offline
 ERRORPRONE_CHECKS_TO_APPLY = []
 
 # Checks to disable in tests.
@@ -28,9 +29,6 @@ TESTONLY_ERRORPRONE_WARNINGS_TO_DISABLE = [
 ERRORPRONE_WARNINGS_TO_DISABLE = [
     'InlineMeInliner',
     'InlineMeSuggester',
-    'UnnecessaryAssignment',
-    'IntLiteralCast',
-    'UnnecessaryStringBuilder',
     # High priority to enable:
     'HidingField',
     'AlreadyChecked',
@@ -123,6 +121,9 @@ ERRORPRONE_WARNINGS_TO_DISABLE = [
     'RedundantControlFlow',
     # Low priority.
     'StatementSwitchToExpressionSwitch',
+    # Assigning to fields marked as @Mock or @Spy. Suggested fix is to delete
+    # assignments, which would break tests in many cases.
+    'UnnecessaryAssignment',
 ]
 
 # Full list of checks: https://errorprone.info/bugpatterns
--- components/media_router/browser/android/junit/src/org/chromium/components/media_router/caf/CafMessageHandlerTest.java
+++ components/media_router/browser/android/junit/src/org/chromium/components/media_router/caf/CafMessageHandlerTest.java
@@ -416,12 +416,9 @@ public class CafMessageHandlerTest {
         JSONObject innerMessage =
                 new JSONObject()
                         .put("type", "SET_VOLUME")
-                        .put(
-                                "volume",
-                                new JSONObject().put("level", (double) 1).put("muted", false));
+                        .put("volume", new JSONObject().put("level", 1.0).put("muted", false));
         JSONObject message = buildCastV2Message(CLIENT_ID1, innerMessage);
         assertTrue(mMessageHandler.handleMessageFromClient(message.toString()));
-        JSONObject volumeMessage = innerMessage.getJSONObject("volume");
         verify(mSession).setMute(false);
         verify(mSession).setVolume(1.0);
         verify(mMessageHandler)
@@ -439,12 +436,9 @@ public class CafMessageHandlerTest {
         JSONObject innerMessage =
                 new JSONObject()
                         .put("type", "SET_VOLUME")
-                        .put(
-                                "volume",
-                                new JSONObject().put("level", (double) 1).put("muted", false));
+                        .put("volume", new JSONObject().put("level", 1.0).put("muted", false));
         JSONObject message = buildCastV2Message(CLIENT_ID1, innerMessage);
         assertTrue(mMessageHandler.handleMessageFromClient(message.toString()));
-        JSONObject volumeMessage = innerMessage.getJSONObject("volume");
         verify(mSession, never()).setMute(anyBoolean());
         verify(mSession, never()).setVolume(anyDouble());
         verify(mMessageHandler)
