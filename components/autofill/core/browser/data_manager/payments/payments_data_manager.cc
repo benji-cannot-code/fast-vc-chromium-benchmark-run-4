9485f967b54c03e7b9a25616bc4f8179fc477dc9
Fernando Ramirez <ferny@google.com>
2025-05-02 11:30:49 -0700

    [Card Benefits][Curinos] Update benefits feature check
    This CL adds another flag check before showing the card benefits toggle
    in Chrome settings. Adding this flag check will also ensure that we are
    logging that benefits sourced from Curinos are enabled when a new
    profile is launched (LogIsCreditCardBenefitsEnabledAtStartup)
    
    go/pay-autofill-chrome-scaling-benefits-design
    
    Bug: 415130486
    Change-Id: I7d88c2925d2b75cb1198fbf2a51fe16a310e94cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507729
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1455107}

--- components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
+++ components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
@@ -1003,7 +1003,9 @@ bool PaymentsDataManager::IsCardBenefitsFeatureEnabled() {
   return base::FeatureList::IsEnabled(
              features::kAutofillEnableCardBenefitsForAmericanExpress) ||
          base::FeatureList::IsEnabled(
-             features::kAutofillEnableCardBenefitsForBmo);
+             features::kAutofillEnableCardBenefitsForBmo) ||
+         base::FeatureList::IsEnabled(
+             features::kAutofillEnableFlatRateCardBenefitsFromCurinos);
 }
 
 bool PaymentsDataManager::ShouldBlockCardBenefitSuggestionLabels(
