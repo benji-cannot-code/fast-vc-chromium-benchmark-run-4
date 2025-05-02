8fdaded474ccf8aacedd93114471f74159b4a281
Christine Hollingsworth <christinesm@chromium.org>
2025-05-02 06:31:52 -0700

    [On-Device APIs] Set workers as off-by-default
    Update `*ForWorkers` features for on-device AI APIs to be
    off-by-default.
    
    Bug: 413709026
    Change-Id: I9960c2f36992ab5e364c7eee5dca82e0545b6ee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488570
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454946}

--- third_party/blink/web_tests/wpt_internal/ai/language-model-api-test-shared-worker.js
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.importScripts("resources/utils.js");
-
-self.onconnect = async e => {
-  const port = e.ports[0];
-  port.postMessage(await testPromptAPI());
-};
