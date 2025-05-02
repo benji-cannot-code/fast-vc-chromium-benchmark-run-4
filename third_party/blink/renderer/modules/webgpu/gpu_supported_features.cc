d407458583d146feb100bcf5e1ee480bf7e8a4ea
James Price <jrprice@google.com>
2025-05-02 15:33:58 -0700

    WebGPU: Add ChromiumExperimentalSubgroupMatrix as an unsafe feature
    This exposes the feature when --enable-unsafe-webgpu is used.
    
    Bug: 403621812
    Change-Id: Ibe0fb15d0a315cce8d8adb1ea2709bf504de43ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6506428
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Auto-Submit: James Price <jrprice@google.com>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455236}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_features.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_features.cc
@@ -53,6 +53,8 @@ std::optional<V8GPUFeatureName::Enum> GPUSupportedFeatures::ToV8FeatureNameEnum(
       return V8GPUFeatureName::Enum::kChromiumExperimentalUnorm16TextureFormats;
     case wgpu::FeatureName::Snorm16TextureFormats:
       return V8GPUFeatureName::Enum::kChromiumExperimentalSnorm16TextureFormats;
+    case wgpu::FeatureName::ChromiumExperimentalSubgroupMatrix:
+      return V8GPUFeatureName::Enum::kChromiumExperimentalSubgroupMatrix;
     default:
       return std::nullopt;
   }
