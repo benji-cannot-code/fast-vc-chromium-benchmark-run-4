7777a74fd7a2171f4e9259fba20e2783363dbe98
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-05-01 22:46:47 -0700

    [WPT] Prefetch + ServiceWorker: Add comment for import_module
    Bug: 406819294
    Change-Id: I4d1a3676d03b2cb1f6aca4dfde6c912b18b52dcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508405
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454841}

--- third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/authenticate.py
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/authenticate.py
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# TODO(https://crbug.com/406819294): Simplify relative import for util.
 import importlib
 util = importlib.import_module("speculation-rules.prefetch.resources.util")
 
