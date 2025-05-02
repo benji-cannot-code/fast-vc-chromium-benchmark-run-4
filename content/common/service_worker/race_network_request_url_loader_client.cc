fd65544cda6aa02c261ae71d1e276fb7d2f7660f
Shunya Shishido <sisidovski@chromium.org>
2025-05-01 18:08:23 -0700

    Call OnDataTransferComplete() only when `state_` is not kCompleted
    This is a speculative fix to the crash by CHECK in
    ServiceWorkerRaceNetworkRequestURLLoaderClient::TransitionState.  Since
    we have no clear evidence to identify the root cause of this, let me add
    the fix along with some new CHECKs to narrow down the possible crash
    scenarios.
    
    From the stack trace, the crash is triggered by
    `TransitionState(State::kDataTransferFinished)` in
    `OnDataTransferComplete()`. It's called from `OnCloneCompleted()` [1].
    
    This check failure can happen when the network error happens while
    cloning the response is ongoing but not yet completed.
    
    1. `OnComplete()` is called. Then `state_` is changed to `kCompleted`.
    2. `OnCloneCompleted()` is called, then `state_` is changed to
    `kDataTransferFinished` in `OnDataTransferComplete()`.
    
    We should prevent `OnDataTransferComplete()` when it's already marked as
    completed, because `owner_->OnComplete()` is already called, which means
    the response is completed.
    
    We should revisit the whole design of the state machine after the
    feature is enabled.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:content/common/service_worker/race_network_request_url_loader_client.cc;l=697;drc=ce7760e7ed8287077e074ce55a23fc1d9281c8a0
    
    Bug: 412386838
    Change-Id: If2067dc31543a8411e0293a53709677304768a42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6478411
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454783}

--- content/common/service_worker/race_network_request_url_loader_client.cc
+++ content/common/service_worker/race_network_request_url_loader_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/service_worker/race_network_request_url_loader_client.h"
 
+#include "base/check_op.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
@@ -695,7 +696,23 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::
 }
 
 void ServiceWorkerRaceNetworkRequestURLLoaderClient::OnCloneCompleted() {
-  OnDataTransferComplete();
+  if (state_ == State::kCompleted) {
+    //  `kCompleted` indicates the network request and data processing to
+    //  `owner_` are finished. With
+    //  `ServiceWorkerStaticRouterRaceNetworkRequestPerformanceImprovement`,
+    //  `state_` might reach `kCompleted` before clone completion in case of
+    //  network errors. This prevents `OnDataTransferComplete()` propagation
+    //  since `owner_->OnComplete()` would have already been called.
+    //
+    // TODO(crbug.com/374606637): The current state machine is designed without
+    // `ServiceWorkerStaticRouterRaceNetworkRequestPerformanceImprovement`. We
+    // need to re-design the state transition after the feature is enabled.
+    CHECK(completion_status_.has_value());
+    CHECK_NE(completion_status_->error_code, net::OK);
+  } else {
+    OnDataTransferComplete();
+  }
+
   // Do clone data again to fulfil the `fetch()` in the fetch-event.
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
