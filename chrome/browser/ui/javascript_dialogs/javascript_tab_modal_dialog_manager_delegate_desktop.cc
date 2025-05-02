0e7ad44a1f434ebd048b22afcce434d385864370
dljames <dljames@chromium.org>
2025-05-02 11:15:19 -0700

    Remove chrome::FindBrowserWithTab from c/b/ui/javascript_dialogs
    Removes usages of chrome::FindBrowserWithTab (an error prone API).
    
    See comments in chrome/browser/ui/browser_finder.h for more details.
    
    Change-Id: I9cfc0de5a39b2c5894d5c18ff54b08714511c8b2
    Bug: 413550537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507043
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455092}

--- chrome/browser/ui/javascript_dialogs/javascript_tab_modal_dialog_manager_delegate_desktop.cc
+++ chrome/browser/ui/javascript_dialogs/javascript_tab_modal_dialog_manager_delegate_desktop.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tab_modal_confirm_dialog.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "components/javascript_dialogs/app_modal_dialog_manager.h"
@@ -62,14 +62,15 @@ void JavaScriptTabModalDialogManagerDelegateDesktop::DidCloseDialog() {
 
 void JavaScriptTabModalDialogManagerDelegateDesktop::SetTabNeedsAttention(
     bool attention) {
-  Browser* browser = chrome::FindBrowserWithTab(web_contents_);
+  tabs::TabInterface* tab = tabs::TabInterface::GetFromContents(web_contents_);
+  BrowserWindowInterface* browser = tab->GetBrowserWindowInterface();
   if (!browser) {
     // It's possible that the WebContents is no longer in the tab strip. If so,
     // just give up. https://crbug.com/786178
     return;
   }
 
-  TabStripModel* tab_strip_model = browser->tab_strip_model();
+  TabStripModel* tab_strip_model = browser->GetTabStripModel();
   SetTabNeedsAttentionImpl(
       attention, tab_strip_model,
       tab_strip_model->GetIndexOfWebContents(web_contents_));
@@ -88,8 +89,11 @@ bool JavaScriptTabModalDialogManagerDelegateDesktop::IsWebContentsForemost() {
 }
 
 bool JavaScriptTabModalDialogManagerDelegateDesktop::IsApp() {
-  Browser* browser = chrome::FindBrowserWithTab(web_contents_);
-  return browser && (browser->is_type_app() || browser->is_type_app_popup());
+  tabs::TabInterface* tab = tabs::TabInterface::GetFromContents(web_contents_);
+  BrowserWindowInterface* browser = tab->GetBrowserWindowInterface();
+  return browser &&
+         (browser->GetType() == BrowserWindowInterface::Type::TYPE_APP ||
+          browser->GetType() == BrowserWindowInterface::Type::TYPE_APP_POPUP);
 }
 
 bool JavaScriptTabModalDialogManagerDelegateDesktop::CanShowModalUI() {
