a776d99d2f61523a0983f5b35e604c1d7dbf75f2
Lina Ismail <ismaill@google.com>
2025-05-02 09:42:25 -0700

    Fix flaky browser tests after LocalIpAddressInEvents enablement.
    Bug: 414822762
    Change-Id: I2518a5d654c39c72a9d43574f500ee521c302291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6504984
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Lina Ismail <ismaill@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1455032}

--- chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
@@ -901,9 +901,11 @@ IN_PROC_BROWSER_TEST_P(DownloadDeepScanningBrowserTest,
             enterprise_connectors::ContentAnalysisRequest::NORMAL_DOWNLOAD);
 
   // Both the DLP and malware violations generate an event.
+  base::RunLoop run_loop;
   std::set<std::string> zip_types = {"application/zip",
                                      "application/x-zip-compressed"};
   enterprise_connectors::test::EventReportValidator validator(client());
+  validator.SetDoneClosure(run_loop.QuitClosure());
   validator.ExpectSensitiveDataEventAndDangerousDeepScanningResult(
       /*url*/ url.spec(),
       /*tab_url*/ url.spec(),
@@ -930,6 +932,8 @@ IN_PROC_BROWSER_TEST_P(DownloadDeepScanningBrowserTest,
       /*profile_identifier*/ GetProfileIdentifier(),
       /*scan_id*/ last_request().request_token());
   WaitForDownloadToFinish();
+
+  run_loop.Run();
 
   // The file should be blocked.
   ASSERT_EQ(download_items().size(), 1u);
@@ -1006,7 +1010,9 @@ IN_PROC_BROWSER_TEST_P(DownloadRestrictionsDeepScanningBrowserTest,
   base::FilePath main_file = DownloadPrefs(browser()->profile())
                                  .DownloadPath()
                                  .AppendASCII("zipfile_two_archives.zip");
+  base::RunLoop run_loop;
   enterprise_connectors::test::EventReportValidator validator(client());
+  validator.SetDoneClosure(run_loop.QuitClosure());
   std::set<std::string> zip_types = {"application/zip",
                                      "application/x-zip-compressed"};
   validator.ExpectDangerousDownloadEvent(
@@ -1030,6 +1036,8 @@ IN_PROC_BROWSER_TEST_P(DownloadRestrictionsDeepScanningBrowserTest,
       /*profile_identifier*/ GetProfileIdentifier());
 
   WaitForDownloadToFinish();
+
+  run_loop.Run();
 
   ASSERT_EQ(download_items().size(), 1u);
   download::DownloadItem* item = *download_items().begin();
