3c1b6f3ce4c371098e233056123762a210196a21
Amelie Schneider <amelies@google.com>
2025-05-02 08:35:15 -0700

    [B4B] Add metric for turning sync on with account storage enabled
    Bug: 404542075
    Change-Id: I0df835b3f1ce161334baa5d5bb8a9ee9ca7579b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508887
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455003}

--- components/signin/internal/identity_manager/primary_account_manager_unittest.cc
+++ components/signin/internal/identity_manager/primary_account_manager_unittest.cc
@@ -1463,6 +1463,9 @@ TEST_F(PrimaryAccountManagerTest,
 
   EXPECT_FALSE(
       SigninPrefs(*prefs()).GetBookmarksExplicitBrowserSignin(gaia_id));
+  histogram_tester_.ExpectUniqueSample(
+      "Signin.Bookmarks.SyncTurnedOnWithAccountStorageEnabled",
+      /*sample=*/true, /*expected_bucket_count=*/1);
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
