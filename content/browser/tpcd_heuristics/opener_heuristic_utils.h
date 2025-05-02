1da614aee29fdadf36a1bc75ccd772262b80e459
Takuto Ikuta <tikuta@chromium.org>
2025-05-02 06:29:25 -0700

    content: Fix missing headers in `opener_heuristic_utils.h`.
    Some include were missing from the header which could lead to compilation errors.
    
    Bug: 40263312
    Change-Id: I113af598e6cd5a7fa05a8605e4fa3e105e0a777b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495605
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454945}

--- content/browser/tpcd_heuristics/opener_heuristic_utils.h
+++ content/browser/tpcd_heuristics/opener_heuristic_utils.h
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_TPCD_HEURISTICS_OPENER_HEURISTIC_UTILS_H_
 
 #include <map>
+#include <set>
+#include <string>
+#include <utility>
 
 #include "base/types/optional_ref.h"
 #include "content/common/content_export.h"
