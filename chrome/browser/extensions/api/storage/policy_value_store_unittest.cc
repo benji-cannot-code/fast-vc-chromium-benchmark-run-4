2a8a9f2e4d211c9118ab8bf86dea971361f55615
Jenny Zhang <jennyz@chromium.org>
2025-05-05 11:35:26 -0700

    [extensions] Port storage api tests for desktop android
    This cl ports some unit tests for Storage API for desktop android.
    The remaining tests not ported uses the features not supported yet
    (managed storage, storage sync).
    
    Bug: 371340979
    Change-Id: If0f86d9cb3fc11bbaf8e35202caf4148fddf3f63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507329
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455845}

--- chrome/browser/extensions/api/storage/policy_value_store_unittest.cc
+++ chrome/browser/extensions/api/storage/policy_value_store_unittest.cc
@@ -23,9 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/storage/backend_task_runner.h"
 #include "extensions/browser/api/storage/settings_observer.h"
 #include "extensions/browser/api/storage/storage_area_namespace.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_id.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 using testing::_;
 using testing::Mock;
--- chrome/browser/extensions/api/storage/storage_session_unittest.cc
+++ chrome/browser/extensions/api/storage/storage_session_unittest.cc
@@ -14,8 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/event_router_factory.h"
 #include "extensions/browser/test_extension_registry_observer.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/mojom/context_type.mojom.h"
 #include "extensions/test/test_extension_dir.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
