b5cc3b30ef8f0885c97897a9515aa75908ba4d47
Etienne Bergeron <etienneb@chromium.org>
2025-05-05 09:15:32 -0700

    Add DCHECK to investigate a bug related to concurrent transactions
    This CL is adding DCHECK to confirm that the cause of crashes is related
    to the database not being opened instead of not having pending
    transaction.
    
    This is a reland of the original solution for:
      [land] https://chromium-review.googlesource.com/c/chromium/src/+/6494541
      [revert] https://chromium-review.googlesource.com/c/chromium/src/+/6504263
    
    The problem with the crash keys is that they are not thread safe. The
    code for autofill backend does run on a single sequence, but each
    profile will have its own sequence. Which makes the crash keys racy
    against profiles.
    
    Bug: 410101523
    Change-Id: I7dbbdcff744ecbe55a5a7c9149a6078fad9c6303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6505740
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455750}

--- components/autofill/core/browser/webdata/autofill_webdata_backend_impl.cc
+++ components/autofill/core/browser/webdata/autofill_webdata_backend_impl.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/check.h"
 #include "base/check_is_test.h"
 #include "base/check_op.h"
+#include "base/debug/alias.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/metrics/histogram_functions.h"
@@ -193,9 +195,13 @@ WebDatabase* AutofillWebDataBackendImpl::GetDatabase() {
 }
 
 void AutofillWebDataBackendImpl::CommitChanges() {
-  DCHECK(web_database_backend_->database()
-             ->GetSQLConnection()
-             ->HasActiveTransactions());
+  DCHECK(owning_task_runner()->RunsTasksInCurrentSequence());
+  // TODO(crbug.com/410101523): Investigate the transaction errors. There should
+  // always be a pending sql transaction.
+  sql::Database* sql = web_database_backend_->database()->GetSQLConnection();
+  base::debug::Alias(&sql);
+  DCHECK(sql->is_open());
+  DCHECK(sql->HasActiveTransactions());
   web_database_backend_->database()->CommitTransaction();
   web_database_backend_->database()->BeginTransaction();
 }
