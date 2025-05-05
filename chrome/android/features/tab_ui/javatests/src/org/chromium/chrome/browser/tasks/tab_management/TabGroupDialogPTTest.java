9e6491bf235485f24e379926c9642df9c1bccad3
Brandon Wylie <wylieb@chromium.org>
2025-05-05 11:46:05 -0700

    [sheriff] Disable failing TabGroupDialogPTTest
    Bug: 415799207
    Change-Id: Ifb0d6a5dad8639c22a8dca188ba72b45817928c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513789
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1455850}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupDialogPTTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupDialogPTTest.java
@@ -21,6 +21,7 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -130,6 +131,7 @@ public class TabGroupDialogPTTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/415799207")
     public void testTabGroupNameChange() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         WebPageStation pageStation =
