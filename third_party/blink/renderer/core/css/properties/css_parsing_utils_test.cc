a1a05dcc474591f6064b6d2b7d942bb0ffef09e9
Rune Lillesveen <futhark@chromium.org>
2025-05-05 05:28:09 -0700

    Pass a CSSLengthResolver into ComputeDegrees in unit test helper
    Part of removing ComputeDegrees() without a length converter from
    CSSPrimitiveValue.
    
    Bug: 408234455
    Change-Id: Ic35e1666bf683859c155e9ae6f57b5719922e537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508850
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455637}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils_test.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_math_function_value.h"
 #include "third_party/blink/renderer/core/css/css_progress_value.h"
 #include "third_party/blink/renderer/core/css/css_scroll_value.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 #include "third_party/blink/renderer/core/css/css_view_value.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_context.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_token_stream.h"
@@ -54,13 +55,20 @@ TEST(CSSParsingUtilsTest, Revert) {
 
 double ConsumeAngleValue(String target) {
   CSSParserTokenStream stream(target);
-  return ConsumeAngle(stream, *MakeContext(), std::nullopt)->ComputeDegrees();
+  // This function only works on calc() expressions that can be resolved at
+  // parse time.
+  CSSToLengthConversionData conversion_data(/*element=*/nullptr);
+  return ConsumeAngle(stream, *MakeContext(), std::nullopt)
+      ->ComputeDegrees(conversion_data);
 }
 
 double ConsumeAngleValue(String target, double min, double max) {
   CSSParserTokenStream stream(target);
+  // This function only works on calc() expressions that can be resolved at
+  // parse time.
+  CSSToLengthConversionData conversion_data(/*element=*/nullptr);
   return ConsumeAngle(stream, *MakeContext(), std::nullopt, min, max)
-      ->ComputeDegrees();
+      ->ComputeDegrees(conversion_data);
 }
 
 TEST(CSSParsingUtilsTest, ConsumeAngles) {
