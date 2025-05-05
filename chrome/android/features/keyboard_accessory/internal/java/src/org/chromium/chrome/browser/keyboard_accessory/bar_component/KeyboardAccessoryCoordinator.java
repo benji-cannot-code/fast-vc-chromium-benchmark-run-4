15aea98f5a444285369cb6e61ca59a3ca60b5b44
Timofey Chudakov <tchudakov@google.com>
2025-05-05 07:19:52 -0700

    [Wallet] Style keyboard accessory loyalty card suggestions.
    Loyalty card suggestions will receive special round logo icons.
    This CL creates dedicated view style for these suggestions. The view
    can receive its style only at constructions time. Since keyboard
    accessory suggestion views are displayed in a RecyclerView, this
    CL creates a decicated view type for loyalty card suggestions.
    
    This CL also filters the "Manage loyalty card" suggestion because it
    should not be shown in the keyboard accessory.
    
    Mock: screen/v6tgjeSUPXsiJN5
    Implementation: screen/3yZd3ToJJajjEAq
    
    Bug: 404437211
    Change-Id: I3cd7864f2f6a4ed6e2c2d4e1216d50c314547ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501294
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455687}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryCoordinator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryCoordinator.java
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.keyboard_accessory.bar_component;
 
 import static org.chromium.chrome.browser.autofill.AutofillUiUtils.getCardIcon;
+import static org.chromium.chrome.browser.autofill.AutofillUiUtils.getValuableIcon;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.SKIP_CLOSING_ANIMATION;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.VISIBLE;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.Px;
@@ -33,6 +35,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.autofill.AutofillDelegate;
 import org.chromium.components.autofill.AutofillSuggestion;
 import org.chromium.components.autofill.ImageSize;
+import org.chromium.components.autofill.SuggestionType;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.ui.AsyncViewProvider;
 import org.chromium.ui.AsyncViewStub;
@@ -192,15 +195,26 @@ public class KeyboardAccessoryCoordinator implements KeyboardAccessoryVisualStat
         KeyboardAccessoryViewBinder.UiConfiguration uiConfiguration =
                 new KeyboardAccessoryViewBinder.UiConfiguration();
         uiConfiguration.suggestionDrawableFunction =
-                (suggestion) ->
-                        getCardIcon(
-                                context,
-                                imageFetcher,
-                                suggestion.getCustomIconUrl(),
-                                suggestion.getIconId(),
-                                ImageSize.SMALL,
-                                /* showCustomIcon= */ true);
+                (suggestion) -> getSuggestionIcon(context, imageFetcher, suggestion);
+
         return uiConfiguration;
+    }
+
+    private static @Nullable Drawable getSuggestionIcon(
+            Context context, AutofillImageFetcher imageFetcher, AutofillSuggestion suggestion) {
+        if (suggestion.getSuggestionType() == SuggestionType.LOYALTY_CARD_ENTRY) {
+            return getValuableIcon(
+                    context, imageFetcher, suggestion.getCustomIconUrl(), ImageSize.SMALL);
+        }
+        // TODO: crbug.com/404437211 - Figure out all suggestion types that have icons on Android
+        // and return icons in a switch.
+        return getCardIcon(
+                context,
+                imageFetcher,
+                suggestion.getCustomIconUrl(),
+                suggestion.getIconId(),
+                ImageSize.SMALL,
+                /* showCustomIcon= */ true);
     }
 
     /**
--- components/autofill/android/java/src/org/chromium/components/autofill/AutofillSuggestion.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/AutofillSuggestion.java
@@ -25,7 +25,7 @@ public class AutofillSuggestion extends DropdownItemBase {
     private final @Nullable String mSecondarySublabel;
     private final @Nullable String mLabelContentDescription;
     private final int mIconId;
-    private final int mSuggestionType;
+    private final @SuggestionType int mSuggestionType;
     private final boolean mIsDeletable;
     private final boolean mApplyDeactivatedStyle;
     private final boolean mShouldDisplayTermsAvailable;
@@ -121,7 +121,7 @@ public class AutofillSuggestion extends DropdownItemBase {
         return mLabelContentDescription;
     }
 
-    public int getSuggestionType() {
+    public @SuggestionType int getSuggestionType() {
         return mSuggestionType;
     }
 
