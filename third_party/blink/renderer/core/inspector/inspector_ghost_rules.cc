a12a920c22b2f7209f7a60cbb2bf61dfa7c53589
Anders Hartvoll Ruud <andruud@chromium.org>
2025-05-05 05:41:32 -0700

    Add more CHECKs to InspectorGhostRules::PopulateSheet
    It's still not clear what's causing Issue 389011795. Most reported
    crashes come from people opening devtools on chatgpt.com, but I cannot
    reproduce this. In an attempt to narrow it down, this CL adds more
    checks, primarily to figure out if the problem is *caused* by
    InspectorGhostRules or just *observed* by InspectorGhostRules.
    
    If the problem is just observed by InspectorGhostRules (i.e. this is
    really an existing issue), then the branch containing
    the DumpWithoutCrashing should hopefully silence the problem.
    
    Issue 389011795
    
    Change-Id: I99b9d6c31af61cc248e806ee7cb391f436b50509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508912
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455645}

--- third_party/blink/renderer/core/inspector/inspector_ghost_rules.cc
+++ third_party/blink/renderer/core/inspector/inspector_ghost_rules.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/inspector/inspector_ghost_rules.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "third_party/blink/renderer/core/css/css_grouping_rule.h"
 #include "third_party/blink/renderer/core/css/css_nested_declarations_rule.h"
 #include "third_party/blink/renderer/core/css/css_style_rule.h"
@@ -145,10 +146,6 @@ wtf_size_t NumItems(CSSRule& rule) {
 
 CSSRule* ItemAt(CSSRule& rule, wtf_size_t index) {
   if (auto* style_rule = DynamicTo<CSSStyleRule>(rule)) {
-    // These checks are for investigating https://crbug.com/389011795.
-    CHECK_EQ(style_rule->length(), style_rule->WrapperCountForDebugging());
-    CHECK_LE(index, style_rule->length());
-    CHECK_LE(index, style_rule->WrapperCountForDebugging());
     return style_rule->ItemInternal(index);
   }
   return To<CSSGroupingRule>(rule).ItemInternal(index);
@@ -173,6 +170,14 @@ void InspectorGhostRules::PopulateSheet(
     // https://drafts.csswg.org/css-nesting-1/#nested-group-rules
     if (IsA<CSSGroupingRule>(rule)) {
       if (!IsA<CSSStyleRule>(rule.parentRule())) {
+        return;
+      }
+    } else {
+      // For investigating crbug.com/389011795.
+      auto& style_rule = To<CSSStyleRule>(rule);
+      if (style_rule.length() != style_rule.WrapperCountForDebugging()) {
+        base::debug::DumpWithoutCrashing();
+        DCHECK(false) << "Mismatched wrapper count";
         return;
       }
     }
@@ -202,6 +207,11 @@ void InspectorGhostRules::PopulateSheet(
       inserted_rules_.insert(inserted_rule);
       inner_rules_.insert(To<CSSStyleRule>(inserted_rule->InnerCSSStyleRule()));
     }
+
+    // For investigating crbug.com/389011795.
+    if (auto* style_rule = DynamicTo<CSSStyleRule>(rule)) {
+      CHECK_EQ(style_rule->length(), style_rule->WrapperCountForDebugging());
+    }
   });
 }
 
@@ -217,6 +227,11 @@ void InspectorGhostRules::DepopulateSheet(CSSStyleSheet& sheet) {
           inserted_rules_.Contains(nested_declarations_rule)) {
         QuietlyDeleteRule(rule, i);
       }
+    }
+
+    // For investigating crbug.com/389011795.
+    if (auto* style_rule = DynamicTo<CSSStyleRule>(rule)) {
+      CHECK_EQ(style_rule->length(), style_rule->WrapperCountForDebugging());
     }
   });
 }
