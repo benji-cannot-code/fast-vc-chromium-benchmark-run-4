c7b86e75c5a5f5fea8e5194c3c9300fc374ce9d7
Calder Kitagawa <ckitagawa@chromium.org>
2025-05-05 10:25:17 -0700

    Disable testRenderGrid_3NativeTabs
    Inconsistent due to NTP not being in a consistent state when thumbnails
    captured.
    
    Bug: 415582129
    Change-Id: I154c7ae879b59997cef8e4b77d1ee737044b77db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513310
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1455790}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutPTTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutPTTest.java
@@ -206,6 +206,7 @@ public class TabSwitcherLayoutPTTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "Flaky due to NTP thumbnails not being consistently loaded")
     public void testRenderGrid_3NativeTabs() throws IOException {
         ChromeTabbedActivity cta = mCtaTestRule.getActivity();
         RegularNewTabPageStation pageStation =
