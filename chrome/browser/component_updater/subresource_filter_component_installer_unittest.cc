3024ec16da448afcc2254e66da7c48e3f4029c67
Jagadesh P <jagadeshjai1999@gmail.com>
2025-05-04 19:08:44 -0700

    Resolve dangling pointer in |SubresourceFilterComponentInstallerTest|
    Bug: 40212619
    Change-Id: Ice4b444a830e2efd4bf79e5e9ca6767dcc002e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508210
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455511}

--- chrome/browser/component_updater/subresource_filter_component_installer_unittest.cc
+++ chrome/browser/component_updater/subresource_filter_component_installer_unittest.cc
@@ -132,6 +132,7 @@ class SubresourceFilterComponentInstallerTest : public PlatformTest {
   }
 
   void TearDown() override {
+    test_ruleset_service_ = nullptr;
     TestingBrowserProcess::GetGlobal()->SetRulesetService(nullptr);
     task_environment_.RunUntilIdle();
     PlatformTest::TearDown();
@@ -189,8 +190,7 @@ class SubresourceFilterComponentInstallerTest : public PlatformTest {
   std::unique_ptr<SubresourceFilterComponentInstallerPolicy> policy_;
   TestingPrefServiceSimple pref_service_;
 
-  raw_ptr<TestRulesetService, DanglingUntriaged> test_ruleset_service_ =
-      nullptr;
+  raw_ptr<TestRulesetService> test_ruleset_service_ = nullptr;
 };
 
 TEST_F(SubresourceFilterComponentInstallerTest,
