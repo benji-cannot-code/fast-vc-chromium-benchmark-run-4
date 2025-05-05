5208e06eb1f0824f9e182a04a70b8c76e4f24d63
Radu Nitescu <radunitescu@google.com>
2025-05-05 06:11:14 -0700

    [iOS][CoI] Add Lens Overlay presentation type utility functions
    These functions will help deduct the required presentation types for the
    lens overlay.
    
    Bug: None
    Change-Id: I0e112ad01b85d4b0c8a6da401e7dd802ce198b78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6498204
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455658}

---
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_presentation_type.mm
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/lens_overlay/model/lens_overlay_presentation_type.h"
+
+#import "ios/chrome/common/ui/util/ui_util.h"
+#import "ui/base/device_form_factor.h"
+
+namespace lens {
+
+ContainerPresentationType ContainerPresentationFor(
+    id<UITraitEnvironment> environment) {
+  return IsRegularXRegularSizeClass(environment)
+             ? ContainerPresentationType::kContentAreaCover
+             : ContainerPresentationType::kFullscreenCover;
+}
+
+ResultPagePresentationType ResultPagePresentationFor(
+    id<UITraitEnvironment> environment) {
+  return IsRegularXRegularSizeClass(environment)
+             ? ResultPagePresentationType::kSidePanel
+             : ResultPagePresentationType::kEdgeAttachedBottomSheet;
+}
+
+}  // namespace lens
--- ios/chrome/browser/lens_overlay/model/lens_overlay_snapshot_controller.mm
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_snapshot_controller.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/task/bind_post_task.h"
 #import "base/task/thread_pool.h"
 #import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller.h"
+#import "ios/chrome/browser/lens_overlay/model/lens_overlay_presentation_type.h"
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_tab_helper.h"
 #import "ios/chrome/browser/lens_overlay/model/snapshot_cover_view_controller.h"
 #import "ios/chrome/browser/snapshots/model/snapshot_tab_helper.h"
@@ -163,10 +164,10 @@ UIWindow* CreateMirrorWindowFromBaseWindow(
 // The top edge is filled with the most prominent color found at the top of the
 // original snapshot. The bottom edge is extended using the background color of
 // the UI elements.
-void PreprocessSnapshot(UIImage* snapshot,
-                        CGSize expected_snapshot_size,
-                        UIEdgeInsets viewport_insets,
-                        base::OnceCallback<void(UIImage*)> callback) {
+void ExtendSnapshot(UIImage* snapshot,
+                    CGSize expected_snapshot_size,
+                    UIEdgeInsets viewport_insets,
+                    base::OnceCallback<void(UIImage*)> callback) {
   // The color used by the omnibox and the bottom toolbar.
   UIColor* elementsBackgroundColor = [UIColor colorNamed:kBackgroundColor];
 
@@ -445,6 +446,13 @@ void LensOverlaySnapshotController::ProcessRawSnapshot(UIImage* snapshot) {
   auto callbackOnInitialSequence = base::BindPostTask(
       task_runner_.get(), std::move(snapshotCapturedCallback));
 
+  if (lens::ContainerPresentationFor(base_window_) ==
+      lens::ContainerPresentationType::kContentAreaCover) {
+    // Content area cover presentation does not require any preprocessing.
+    std::move(callbackOnInitialSequence).Run(snapshot);
+    return;
+  }
+
   scoped_refptr<base::SequencedTaskRunner> backgroundRunner =
       base::ThreadPool::CreateSequencedTaskRunner(
           {base::MayBlock(), base::TaskPriority::USER_VISIBLE});
@@ -460,10 +468,10 @@ void LensOverlaySnapshotController::ProcessRawSnapshot(UIImage* snapshot) {
     viewportInsets.top = topInsetForNTP;
   }
 
-  auto preprocessCallback =
-      base::BindOnce(&PreprocessSnapshot, snapshot, expected_window_size_,
+  auto extendSnapshotCallback =
+      base::BindOnce(&ExtendSnapshot, snapshot, expected_window_size_,
                      viewportInsets, std::move(callbackOnInitialSequence));
-  backgroundRunner->PostTask(FROM_HERE, std::move(preprocessCallback));
+  backgroundRunner->PostTask(FROM_HERE, std::move(extendSnapshotCallback));
 }
 
 void LensOverlaySnapshotController::NotifySnapshotComplete(UIImage* snapshot) {
