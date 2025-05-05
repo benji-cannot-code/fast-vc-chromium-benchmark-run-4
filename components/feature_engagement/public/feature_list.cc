7b52317eb304e777cd9709e4d51f889ea6316e10
Shlomi Ben-Shimon <shlomib@google.com>
2025-05-05 11:12:23 -0700

    Added IPH feature definition for ReadAloud playback modes.
    This is only a definition, implementation will follow in next CL.
    
    Screenshot (with full implementation): https://screenshot.googleplex.com/8vth9Amk3GqSyBe
    Spec: https://docs.google.com/presentation/d/1VXSdth49bXyJeAafVH5Q86kHywbEhdKLnN3L_VykULU/edit?slide=id.g34d4c8076ec_0_29&resourcekey=0-mSQVdyZwPKrhufxK9WdStQ#slide=id.g34d4c8076ec_0_29
    
    Bug: 401256755
    Change-Id: Ide2e952132f5dbfbcfbfd9b0f5db083da894434b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512550
    Commit-Queue: Shlomi Ben Shimon <shlomib@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455825}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -78,6 +78,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHPreviewsOmniboxUIFeature,
     &kIPHReadAloudAppMenuFeature,
     &kIPHReadAloudExpandedPlayerFeature,
+    &kIPHReadAloudPlaybackModeFeature,
     &kIPHReadLaterContextMenuFeature,
     &kIPHReadLaterAppMenuBookmarkThisPageFeature,
     &kIPHReadLaterAppMenuBookmarksFeature,
