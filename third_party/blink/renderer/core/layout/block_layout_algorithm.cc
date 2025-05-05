4dc5047614cb5b7feadb2f25602eee59c5029ecb
Ian Kilpatrick <ikilpatrick@chromium.org>
2025-05-05 14:56:40 -0700

    [align] Tentatively change our anon-block behaviour.
    See: https://github.com/w3c/csswg-drafts/issues/11461
    
    Our current behaviour is (IMO) a bug, so tentatively change it to
    "Option 1" in the issue.
    
    Adds a tentative test as well.
    
    Change-Id: I145d39d819347d3446dd08cdd98e055e40625b53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512658
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455957}

--- third_party/blink/renderer/core/layout/block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/block_layout_algorithm.cc
@@ -3148,7 +3148,9 @@ BoxStrut BlockLayoutAlgorithm::CalculateMargins(
                   &Style())
               .GetPosition();
 
-      if (justify_self == ItemPosition::kStretch && !has_auto_margins) {
+      if (child.IsAnonymousBlock()) {
+        builder.SetInlineAutoBehavior(AutoSizeBehavior::kStretchImplicit);
+      } else if (justify_self == ItemPosition::kStretch && !has_auto_margins) {
         builder.SetInlineAutoBehavior(AutoSizeBehavior::kStretchExplicit);
       } else if (justify_self != ItemPosition::kNormal && !has_auto_margins) {
         builder.SetInlineAutoBehavior(AutoSizeBehavior::kFitContent);
@@ -3230,7 +3232,9 @@ ConstraintSpace BlockLayoutAlgorithm::CreateConstraintSpaceForChild(
                 {ItemPosition::kNormal, OverflowAlignment::kDefault}, &Style())
             .GetPosition();
 
-    if (justify_self == ItemPosition::kStretch && !has_auto_margins) {
+    if (child.IsAnonymousBlock()) {
+      builder.SetInlineAutoBehavior(AutoSizeBehavior::kStretchImplicit);
+    } else if (justify_self == ItemPosition::kStretch && !has_auto_margins) {
       builder.SetInlineAutoBehavior(AutoSizeBehavior::kStretchExplicit);
     } else if (justify_self != ItemPosition::kNormal && !has_auto_margins) {
       builder.SetInlineAutoBehavior(AutoSizeBehavior::kFitContent);
