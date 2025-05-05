78ba48f611123f28e24e2eaa5c2c2fe5c08f1558
Chidera Olibie <colibie@google.com>
2025-05-05 12:46:37 -0700

    [wv][cleanup] rename startChromiumLocked to startChromium
    - also renamed ensureChromiumStartedLocked to
      triggerAndWaitForChromiumStarted.
    
    startChromiumLocked and the associated ensureChromiumStartedLocked do
    not require locks anymore. See https://crrev.com/c/6081452.
    
    Bug: 389871700
    Change-Id: I4f928dac33d66e182901731a9b26432c601cc0cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198259
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Richard (Torne) Coles <torne@chromium.org>
    Feels: Chidera Olibie <colibie@google.com>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Chidera Olibie <colibie@google.com>
    Reviewed-by: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455892}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -956,7 +956,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
     @RequiresApi(Build.VERSION_CODES.P)
     @Override
     public TracingController getTracingController() {
-        mAwInit.ensureChromiumStartedLocked(
+        mAwInit.triggerAndWaitForChromiumStarted(
                 true, WebViewChromiumAwInit.CallSite.GET_TRACING_CONTROLLER);
         synchronized (mAwInit.getLazyInitLock()) {
             if (mObjectHolderForP.mTracingController == null) {
