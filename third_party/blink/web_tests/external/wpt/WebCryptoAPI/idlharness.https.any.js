011379b38e6834b434574d78fd74fa6c0e03595d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-05-05 04:54:41 -0700

    Import wpt@591c95ce6174690b92833cd92859ce2807714591
    https://github.com/web-platform-tests/wpt/compare/d3287f101...591c95ce6
    
    Using wpt-import in Chromium 9172ed6d016599ee4f05806e6108dbebb5946866.
    With Chromium commits locally applied on WPT:
      aefeff16fa "Fix a DCHECK in incremental style updates."
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I01f49dd49f1997cd787ca9da6e71d0d1ad01d6a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512010
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1455628}

--- third_party/blink/web_tests/external/wpt/WebCryptoAPI/idlharness.https.any.js
+++ third_party/blink/web_tests/external/wpt/WebCryptoAPI/idlharness.https.any.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // https://w3c.github.io/webcrypto/Overview.html
 
 idl_test(
-  ['WebCryptoAPI'],
+  ['webcrypto'],
   ['html', 'dom'],
   idl_array => {
     idl_array.add_objects({
