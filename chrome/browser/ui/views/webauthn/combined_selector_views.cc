d67ec87af803096472cb845553fe95c1a498cd57
Adem Derinel <derinel@google.com>
2025-05-05 02:01:21 -0700

    WebAuthn: Focus the RadioButton when selected in combined selector
    Before this CL, the focus indicator was not moving to the selected row.
    This CL fixes that.
    
    Bug: 393055190
    Change-Id: I4ec92063b0bcdb5687624eaa767b3edebe433805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509311
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1455576}

--- chrome/browser/ui/views/webauthn/combined_selector_views.cc
+++ chrome/browser/ui/views/webauthn/combined_selector_views.cc
@@ -80,6 +80,7 @@ void CombinedSelectorRadioButton::SetChecked(bool checked) {
       }
     }
     delegate_->OnRadioButtonChecked(index_);
+    RequestFocus();
   }
   Checkbox::SetChecked(checked);
 }
@@ -193,7 +194,6 @@ bool CombinedSelectorRowView::OnMousePressed(const ui::MouseEvent& event) {
         ui::EventType::kMousePressed, center, center, event.time_stamp(),
         event.flags(), event.changed_button_flags());
     radio_button_->OnMousePressed(synthetic_press_event);
-    radio_button_->RequestFocus();
     return true;
   }
   return views::TableLayoutView::OnMousePressed(event);
@@ -206,6 +206,7 @@ void CombinedSelectorRowView::OnMouseReleased(const ui::MouseEvent& event) {
         ui::EventType::kMouseReleased, center, center, event.time_stamp(),
         event.flags(), event.changed_button_flags());
     radio_button_->OnMouseReleased(synthetic_release_event);
+    RequestFocus();
     return;
   }
   views::TableLayoutView::OnMouseReleased(event);  // Default handling.
