44881e4979fca819025576d938c2ccfeb1bbcec0
Calder Kitagawa <ckitagawa@chromium.org>
2025-05-05 14:08:41 -0700

    [Tabs] Create TabListInterface
    Create an x-platform interface for performing operations on tabs. This
    can be shared between desktop and Android platforms for some systems
    that need to operate on tabs without having to understand the nuances of
    platform specific code.
    
    This will be backed by TabModelJniBridge on Android and will wrap
    TabStripModel on desktop.
    
    Note: this is not the final location of tab_list_interface and we plan
    to move this to another location in the future.
    
    Design doc (by Devlin):
    https://docs.google.com/document/d/1PYs9P-9ZSw63Lg1MiPJgN_hoXxlzyRlvJQ-Em5Xarnk/edit?usp=sharing
    
    NO_IFTTT=Reformat
    
    Fixed: 415323446
    Change-Id: I83a2c40786bccea215fdfda767f616eba29090aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507449
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455932}

--- chrome/browser/ui/android/tab_model/tab_model.h
+++ chrome/browser/ui/android/tab_model/tab_model.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/flags/android/chrome_session_state.h"
 #include "chrome/browser/ui/android/tab_model/android_live_tab_context.h"
+#include "chrome/browser/ui/android/tab_model/tab_list_interface.h"
 #include "components/omnibox/browser/location_bar_model.h"
 #include "components/omnibox/browser/location_bar_model_delegate.h"
 #include "components/sessions/core/session_id.h"
@@ -38,7 +39,7 @@ class TabModelObserver;
 // Abstract representation of a Tab Model for Android.  Since Android does
 // not use Browser/BrowserList, this is required to allow Chrome to interact
 // with Android's Tabs and Tab Model.
-class TabModel {
+class TabModel : public TabListInterface {
  public:
   // LINT.IfChange(TabLaunchType)
   // Various ways tabs can be launched.
@@ -154,13 +155,10 @@ class TabModel {
   // Long term, this would ideally be refactored to a traits system such that
   // the different types of side-effects are canonically defined, and listed
   // explicitly for each TabLaunchType in a single location.
+  //
+  // Multiple lines are not supported by IFTTT.
   // clang-format off
-  // LINT.ThenChange(
-  //   //tools/metrics/histograms/metadata/new_tab_page/enums.xml:TabLaunchType,
-  //   //chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java,
-  //   //chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/flatbuffer/tab_state_common.fbs,
-  //   //chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
-  // )
+  // LINT.ThenChange(//tools/metrics/histograms/metadata/new_tab_page/enums.xml:TabLaunchType,//chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java,//chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/flatbuffer/tab_state_common.fbs,//chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java)
   // clang-format on
 
   // Various ways tabs can be selected.
@@ -264,7 +262,7 @@ class TabModel {
 
  protected:
   TabModel(Profile* profile, chrome::android::ActivityType activity_type);
-  virtual ~TabModel();
+  ~TabModel() override;
 
   // Instructs the TabModel to broadcast a notification that all tabs are now
   // loaded from storage.
