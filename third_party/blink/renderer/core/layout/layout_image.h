a6c970f9e5ab8dcfef3186b1a4087acb0d96e822
Fredrik Söderquist <fs@opera.com>
2025-05-05 10:16:59 -0700

    Split LayoutImage::InvalidatePaintAndMarkForLayoutIfNeeded()
    This function has two callers - one where the natural size may have
    changed[1], and one where it wont have[2].
    
    Split the function into a "invalidate paint without layout" and a "mark
    for layout if needed" part. Move the latter part to
    LayoutImage::ImageChanged(), and only call the former in[2]
    since what we want there is a repaint.
    
    [1] LayoutImage::ImageChanged
    [2] LayoutImage::AreaElementFocusChanged
    
    Change-Id: If90bcb6effee4c5b1a3006f4ae15e35f5a9f35e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6415963
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455782}

--- third_party/blink/renderer/core/layout/layout_image.h
+++ third_party/blink/renderer/core/layout/layout_image.h
@@ -166,9 +166,10 @@ class CORE_EXPORT LayoutImage : public LayoutReplaced {
                    const PhysicalOffset& accumulated_offset,
                    HitTestPhase) final;
 
-  void InvalidatePaintAndMarkForLayoutIfNeeded(CanDeferInvalidation);
+  void InvalidatePaintWithoutLayoutChange(CanDeferInvalidation);
   bool UpdateNaturalSizeIfNeeded();
   bool NeedsLayoutOnNaturalSizeChange() const;
+  bool InvalidateLayoutOnNaturalSizeChange();
 
   // The natural dimensions for the image.
   PhysicalNaturalSizingInfo natural_dimensions_;
