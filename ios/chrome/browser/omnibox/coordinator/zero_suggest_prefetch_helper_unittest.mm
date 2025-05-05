2de1e43bbceb7c9554674cb8d481fb538d20d53b
Christian Xu <christianxu@google.com>
2025-05-05 07:36:37 -0700

    [iOS][Omnibox] Move zero_suggest_prefetch_helper to coordinators
    Moves ZeroSuggestPrefetchHelper to the coordinator folder.
    
    Bug: 394567614
    Change-Id: Ib32b133617f67744ce8966ac29a9c9e9641ade94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507713
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455692}

--- ios/chrome/browser/omnibox/coordinator/zero_suggest_prefetch_helper_unittest.mm
+++ ios/chrome/browser/omnibox/coordinator/zero_suggest_prefetch_helper_unittest.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/omnibox/ui_bundled/zero_suggest_prefetch_helper.h"
+#import "ios/chrome/browser/omnibox/coordinator/zero_suggest_prefetch_helper.h"
 
 #import "base/test/task_environment.h"
 #import "components/omnibox/browser/autocomplete_controller.h"
