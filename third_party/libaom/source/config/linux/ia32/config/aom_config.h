6ce745c34095f0050052fe1eaaeac9bb899c2dc9
Lin Zheng <linzhen@google.com>
2025-05-05 12:05:45 -0700

    Roll src/third_party/libaom/source/libaom/ 719f60edc..378f15d1c (7 commits)
    https://aomedia.googlesource.com/aom.git/+log/719f60edc51b..378f15d1cbd5
    
    $ git log 719f60edc..378f15d1c --date=short --no-merges --format='%ad %ae %s'
    2025-04-29 maryla svc_encoder_rtc: support adding local metadata
    2025-04-15 jianj Add avx512 detection & add sad highway avx512
    2025-04-28 satheesh.kumar lc-dec: Conditionally disable temporal MV prediction
    2025-04-23 mudassir.galaganath lc-dec: Improve LC decode mode functions
    2025-04-29 jzern mem_neon.h: update GCC_32_BIT check
    2025-04-29 jingning Rework speed features for temporal consistency
    2025-04-23 jingning Further adjustment to improve temporal consistency
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=jzern@google.com
    
    Bug: b:307414544
    Change-Id: I83e6e423d4ba2dfdf895d90c1a4f046fed95def1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513309
    Commit-Queue: Lin Zheng <linzhen@google.com>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1455868}

--- third_party/libaom/source/config/linux/ia32/config/aom_config.h
+++ third_party/libaom/source/config/linux/ia32/config/aom_config.h
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_ARM_CRC32 0
 #define HAVE_AVX 1
 #define HAVE_AVX2 1
+#define HAVE_AVX512 0
 #define HAVE_FEXCEPT 1
 #define HAVE_MMX 1
 #define HAVE_NEON 0
