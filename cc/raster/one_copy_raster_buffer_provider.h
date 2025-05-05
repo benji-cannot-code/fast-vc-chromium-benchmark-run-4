e3b77bc5cf5070cfa0855d778dcadd64d3682121
Colin Blundell <blundell@chromium.org>
2025-05-05 00:34:54 -0700

    [//cc] Have OneCopyRasterBufferProvider take in SII explicitly
    Decoupling OneCopyRBP from internally getting the SII from the worker's
    RasterContextProvider is a step on the road toward SII eventually not
    hanging off RasterContextProvider.
    
    Bug: 403372453
    Change-Id: I8117c95a419983fc0b45b0663ff692c840c89ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509005
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455550}

--- cc/raster/one_copy_raster_buffer_provider.h
+++ cc/raster/one_copy_raster_buffer_provider.h
@@ -32,6 +32,7 @@ class StagingBufferPool;
 class CC_EXPORT OneCopyRasterBufferProvider : public RasterBufferProvider {
  public:
   OneCopyRasterBufferProvider(
+      scoped_refptr<gpu::SharedImageInterface> sii,
       scoped_refptr<base::SequencedTaskRunner> task_runner,
       viz::RasterContextProvider* compositor_context_provider,
       viz::RasterContextProvider* worker_context_provider,
@@ -134,6 +135,7 @@ class CC_EXPORT OneCopyRasterBufferProvider : public RasterBufferProvider {
                                     bool mailbox_texture_is_overlay_candidate,
                                     const gpu::SyncToken& sync_token);
 
+  const scoped_refptr<gpu::SharedImageInterface> sii_;
   const raw_ptr<viz::RasterContextProvider> compositor_context_provider_;
   const raw_ptr<viz::RasterContextProvider> worker_context_provider_;
   const int max_bytes_per_copy_operation_;
