004ff58ad00173715fb4693d155c3eb0909e8181
Colin Blundell <blundell@chromium.org>
2025-05-06 00:14:22 -0700

    [//cc] Streamline ways of checking for TreesInViz in client process
    There are currently three ways of doing so in the code. Streamline down
    to one. Thanks to zmo@ for suggestions in [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6508945
    
    Change-Id: Id93cbbe8d15fd98610ac4e6bc2622f5593de1fc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509310
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456119}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -445,7 +445,6 @@ LayerTreeHostImpl::LayerTreeHostImpl(
       task_runner_provider_(task_runner_provider),
       current_begin_frame_tracker_(FROM_HERE),
       settings_(settings),
-      trees_in_viz_in_client_process_(settings_.TreesInVizInClientProcess()),
       use_layer_context_for_animations_(
           settings_.UseLayerContextForAnimations()),
       is_synchronous_single_threaded_(!task_runner_provider->HasImplThread() &&
@@ -1450,7 +1449,7 @@ DrawResult LayerTreeHostImpl::CalculateRenderPasses(FrameData* frame) {
 
   // In TreesInViz mode, FrameData built in the renderer side is abandoned
   // and later rebuilt in viz side. Therefore, certain steps can be skipped.
-  bool output_frame_data = !trees_in_viz_in_client_process_;
+  bool output_frame_data = !settings_.TreesInVizInClientProcess();
 
   for (EffectTreeLayerListIterator it(active_tree());
        it.state() != EffectTreeLayerListIterator::State::kEnd; ++it) {
@@ -2907,7 +2906,7 @@ std::optional<SubmitInfo> LayerTreeHostImpl::DrawLayers(FrameData* frame) {
 
   base::TimeTicks submit_time = base::TimeTicks::Now();
 
-  if (trees_in_viz_in_client_process_) {
+  if (settings_.TreesInVizInClientProcess()) {
     UpdateDisplayTree(*frame);
 
     // For the display compositor we should have already submitted at display
@@ -3091,7 +3090,7 @@ viz::CompositorFrame LayerTreeHostImpl::GenerateCompositorFrame(
   // the requests will be sent over to viz to compute them.
   // If we call TakeViewTransitionRequests() here, it will clear the requests
   // and send none to viz.
-  if (!trees_in_viz_in_client_process_) {
+  if (!settings_.TreesInVizInClientProcess()) {
     ViewTransitionRequest::ViewTransitionElementMap view_transition_element_map;
     const auto& capture_view_transition_tokens =
         active_tree_->GetCaptureViewTransitionTokens();
@@ -3337,7 +3336,7 @@ void LayerTreeHostImpl::DidDrawAllLayers(const FrameData& frame) {
 }
 
 void LayerTreeHostImpl::UpdateDisplayTree(FrameData& frame) {
-  DCHECK(trees_in_viz_in_client_process_);
+  DCHECK(settings_.TreesInVizInClientProcess());
   DCHECK(layer_context_);
 
   layer_context_->UpdateDisplayTreeFrom(
@@ -4145,7 +4144,7 @@ void LayerTreeHostImpl::SetNeedsRedraw(bool animation_only) {
   NotifyLatencyInfoSwapPromiseMonitors();
   events_metrics_manager_.SaveActiveEventMetrics();
 
-  if (trees_in_viz_in_client_process_) {
+  if (settings_.TreesInVizInClientProcess()) {
     if (!animation_only || !use_layer_context_for_animations_) {
       client_->SetNeedsRedrawOnImplThread();
     }
@@ -4512,7 +4511,7 @@ bool LayerTreeHostImpl::InitializeFrameSink(
 
   layer_tree_frame_sink_ = layer_tree_frame_sink;
   has_valid_layer_tree_frame_sink_ = true;
-  if (trees_in_viz_in_client_process_) {
+  if (settings_.TreesInVizInClientProcess()) {
     layer_context_ = layer_tree_frame_sink_->CreateLayerContext(*this);
   }
 
