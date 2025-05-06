da398be3dafc14e20133768612eaa4b9962fcac7
Jonathan Lee <jonathanjlee@google.com>
2025-05-06 11:31:28 -0700

    [ios] Speculatively fix `testProceedingPastMalwareWarningReported`
    Follow the example of other non-incognito malware tests and disable
    HPRT, which might be breaking the test because the URL is supposedly
    marked for phishing.
    
    For extra isolation, force a relaunch of the app for reporting tests.
    
    Bug: 415736132, 412683254
    Change-Id: I2cc00b5577d7e5c44c99b8f0d3c79d215910f8c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513071
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456491}

--- ios/chrome/browser/safe_browsing/model/safe_browsing_egtest.mm
+++ ios/chrome/browser/safe_browsing/model/safe_browsing_egtest.mm
@@ -154,7 +154,9 @@ id<GREYMatcher> EnhancedSafeBrowsingInfobarButtonMatcher() {
   if ([self
           isRunningTest:@selector(testRestoreToWarningPagePreservesHistory)] ||
       [self isRunningTest:@selector(testMalwarePage)] ||
-      [self isRunningTest:@selector(testProceedingPastMalwareWarning)]) {
+      [self isRunningTest:@selector(testProceedingPastMalwareWarning)] ||
+      [self
+          isRunningTest:@selector(testProceedingPastMalwareWarningReported)]) {
     config.additional_args.push_back(std::string(
         "--disable-features=SafeBrowsingHashPrefixRealTimeLookups"));
   } else {
@@ -173,6 +175,7 @@ id<GREYMatcher> EnhancedSafeBrowsingInfobarButtonMatcher() {
     config.additional_args.push_back(
         base::StrCat({"<dict><key>", kEnrollmentTokenPolicyName,
                       "</key><string>", kEnrollmentToken, "</string></dict>"}));
+    config.relaunch_policy = ForceRelaunchByKilling;
   }
 
   config.additional_args.push_back(
@@ -301,7 +304,9 @@ id<GREYMatcher> EnhancedSafeBrowsingInfobarButtonMatcher() {
 
 - (BOOL)isRunningEnterpriseReportingTest {
   return
-      [self isRunningTest:@selector(testProceedingPastPhishingWarningReported)];
+      [self
+          isRunningTest:@selector(testProceedingPastPhishingWarningReported)] ||
+      [self isRunningTest:@selector(testProceedingPastMalwareWarningReported)];
 }
 
 - (void)waitForEnterpriseReports:(int)count {
@@ -583,9 +588,7 @@ id<GREYMatcher> EnhancedSafeBrowsingInfobarButtonMatcher() {
 
 // Tests expanding the details on a malware warning, and proceeding past the
 // warning is reported to an enterprise connector.
-// TODO(crbug.com/415736132): Re-enable test and add to Enterprise reporting
-// list `isRunningEnterpriseReportingTest`.
-- (void)DISABLED_testProceedingPastMalwareWarningReported {
+- (void)testProceedingPastMalwareWarningReported {
   [ChromeEarlGrey loadURL:_safeURL1];
   [ChromeEarlGrey waitForWebStateContainingText:_safeContent1];
 
