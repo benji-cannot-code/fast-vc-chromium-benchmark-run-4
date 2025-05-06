9cb3b76914e6b6e7e7e17ce788c45ada3231db17
valerie young <spectranaut@igalia.com>
2025-05-06 11:25:40 -0700

    [ChromeVox] Fix communication between panel and service worker
    Bug: 388867840
    Change-Id: I74d884ca3954dd74287bf55635aa5fbece3aa91b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514154
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1456481}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/panel/panel.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/panel/panel.ts
@@ -31,6 +31,9 @@ const $ = (id: string): HTMLElement | null => document.getElementById(id);
 type AsyncCallback = () => Promise<void>;
 type SessionState = chrome.loginState.SessionState;
 
+type MessageSender = chrome.runtime.MessageSender;
+type SendResponse = (value: any) => void;
+
 /** Class to manage the panel. */
 export class Panel implements PanelInterface {
   private menuManager_ = new MenuManager();
@@ -58,6 +61,11 @@ export class Panel implements PanelInterface {
   }
 
   private initListeners_(): void {
+    chrome.runtime.onMessage.addListener(
+        (message: any|undefined, _sender: MessageSender,
+         sendResponse: SendResponse) =>
+            this.handleMessageFromServiceWorker_(message, sendResponse));
+
     chrome.loginState.getSessionState(
         (state: SessionState) => this.updateSessionState_(state));
     chrome.loginState.onSessionStateChanged.addListener(
@@ -82,8 +90,7 @@ export class Panel implements PanelInterface {
     window.addEventListener(
         'storage', (event: StorageEvent) => this.onStorageChanged_(event),
         false);
-    window.addEventListener(
-        'message', (message: MessageEvent) => this.onMessage_(message), false);
+
     window.addEventListener('blur', event => this.onBlur_(event), false);
     window.addEventListener('hashchange', () => this.onHashChange_(), false);
 
@@ -161,6 +168,16 @@ export class Panel implements PanelInterface {
       this.speechContainer_!.style.visibility = 'visible';
       this.brailleContainer_!.style.visibility = 'hidden';
     }
+  }
+
+  private handleMessageFromServiceWorker_(
+      message: any|undefined, sendResponse: SendResponse): boolean {
+    if (message.type == PanelCommandType.IS_PANEL_INITIALIZED) {
+      sendResponse(true);
+    } else {
+      this.exec_({type: message.type, data: message.data} as PanelCommand)
+    }
+    return false;
   }
 
   /**
@@ -592,11 +609,6 @@ export class Panel implements PanelInterface {
     if (this.originalStickyState_) {
       BackgroundBridge.CommandHandler.onCommand(Command.TOGGLE_STICKY_MODE);
     }
-  }
-
-  private onMessage_(message: MessageEvent): void {
-    const command = JSON.parse(message.data) as PanelCommand;
-    this.exec_(command);
   }
 
   private async onPanLeft_(): Promise<void> {
