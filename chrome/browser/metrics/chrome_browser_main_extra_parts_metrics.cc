1956b14643ff718e2b1c8d712ee45d54dc369df5
Ioana Pandele <ioanap@chromium.org>
2025-05-06 06:31:22 -0700

    Revert "Enable PerformanceMonitor power metrics on Android"
    This reverts commit 0eb8b44e602a29d423a5ec00cf62b0e346633fd2.
    
    Reason for revert: This depends on https://chromium-review.googlesource.com/c/chromium/src/+/6468605 which needs to be reverted for causing consistent test failures on automotive.
    
    Bug: 412634171
    Original change's description:
    > Enable PerformanceMonitor power metrics on Android
    >
    > Also defines ChildProcessTerminationInfo.cpu_usage unconditionally since
    > it's referenced in ProcessMonitor, although it will always be set to
    > nullopt on Android.
    >
    > Bug: 412634171
    > Change-Id: I91d927fca61f9a986b47b3c2ad9750b44f4760e3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6468606
    > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1455944}
    
    Bug: 412634171
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I111c17df3356b891dd7f2ac99af7c919d93ced40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516071
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456267}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/google/google_brand.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
-#include "chrome/browser/metrics/power/power_metrics_reporter.h"
-#include "chrome/browser/metrics/power/process_monitor.h"
 #include "chrome/browser/metrics/process_memory_metrics_emitter.h"
 #include "chrome/browser/metrics/tab_stats/tab_stats_tracker.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -76,6 +74,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/power_monitor/battery_state_sampler.h"
 #include "chrome/browser/metrics/first_web_contents_profiler.h"
 #include "chrome/browser/metrics/power/battery_discharge_reporter.h"
+#include "chrome/browser/metrics/power/power_metrics_reporter.h"
+#include "chrome/browser/metrics/power/process_monitor.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_ANDROID)
@@ -986,8 +986,10 @@ void ChromeBrowserMainExtraPartsMetrics::PreCreateThreads() {
 }
 
 void ChromeBrowserMainExtraPartsMetrics::PostCreateMainMessageLoop() {
+#if !BUILDFLAG(IS_ANDROID)
   // Must be initialized before any child processes are spawned.
   process_monitor_ = std::make_unique<ProcessMonitor>();
+#endif  // !BUILDFLAG(IS_ANDROID)
 }
 
 void ChromeBrowserMainExtraPartsMetrics::PreProfileInit() {
@@ -1167,6 +1169,7 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
 
   display_observer_.emplace(this);
 
+#if !BUILDFLAG(IS_ANDROID)
 // In ChromeOS, the chrome application typically starts at the login screen and
 // waits for the user to log in before opening a browser window, so calling
 // `BeginFirstWebContentsProfiling()` is inappropriate because the
@@ -1174,21 +1177,12 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
 // crash (which has no login screen) requires the user to click a notification
 // prompt before browser windows are restored, so the `BrowserList` is also
 // empty in this case.
-#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_ANDROID)
+#if !BUILDFLAG(IS_CHROMEOS)
   metrics::BeginFirstWebContentsProfiling();
-#endif  // !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_ANDROID)
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
   // Instantiate the power-related metrics reporters.
 
-  // PowerMetricsReporter focus solely on Chrome-specific metrics that affect
-  // power (CPU time, wake ups, etc.). Only instantiate it if |process_monitor_|
-  // exists. This is always the case for Chrome but not for the unit tests.
-  if (process_monitor_) {
-    power_metrics_reporter_ =
-        std::make_unique<PowerMetricsReporter>(process_monitor_.get());
-  }
-
-#if !BUILDFLAG(IS_ANDROID)
   // BatteryDischargeRateReporter reports the system-wide battery discharge
   // rate. It depends on the TabStatsTracker to determine the usage scenario,
   // and the BatteryStateSampler to determine the battery level.
@@ -1199,6 +1193,14 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
       base::BatteryStateSampler::Get()) {
     battery_discharge_reporter_ = std::make_unique<BatteryDischargeReporter>(
         base::BatteryStateSampler::Get());
+  }
+
+  // PowerMetricsReporter focus solely on Chrome-specific metrics that affect
+  // power (CPU time, wake ups, etc.). Only instantiate it if |process_monitor_|
+  // exists. This is always the case for Chrome but not for the unit tests.
+  if (process_monitor_) {
+    power_metrics_reporter_ =
+        std::make_unique<PowerMetricsReporter>(process_monitor_.get());
   }
 
   performance_intervention_metrics_reporter_ =
--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h
@@ -20,14 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display_observer.h"
 
 class ChromeBrowserMainParts;
-class PowerMetricsReporter;
 class PrefRegistrySimple;
 class PrefService;
-class ProcessMonitor;
 
 #if !BUILDFLAG(IS_ANDROID)
 class BatteryDischargeReporter;
 class PerformanceInterventionMetricsReporter;
+class PowerMetricsReporter;
+class ProcessMonitor;
 #endif
 
 #if BUILDFLAG(IS_LINUX)
@@ -110,6 +110,7 @@ class ChromeBrowserMainExtraPartsMetrics : public ChromeBrowserMainExtraParts,
 
   std::optional<display::ScopedDisplayObserver> display_observer_;
 
+#if !BUILDFLAG(IS_ANDROID)
   // The process monitor instance. Allows collecting metrics about every child
   // process.
   std::unique_ptr<ProcessMonitor> process_monitor_;
@@ -117,7 +118,6 @@ class ChromeBrowserMainExtraPartsMetrics : public ChromeBrowserMainExtraParts,
   // Reports power metrics.
   std::unique_ptr<PowerMetricsReporter> power_metrics_reporter_;
 
-#if !BUILDFLAG(IS_ANDROID)
   std::unique_ptr<BatteryDischargeReporter> battery_discharge_reporter_;
 
   std::unique_ptr<PerformanceInterventionMetricsReporter>
