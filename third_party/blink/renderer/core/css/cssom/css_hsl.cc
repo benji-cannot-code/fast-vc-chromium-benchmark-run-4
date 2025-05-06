6ae91a1f6603cf088656a35d4ef38c788df73fba
Fredrik Söderquist <fs@opera.com>
2025-05-06 02:06:56 -0700

    Rewrite CSSOM's CSSColorValue to use Color::ConvertToColorSpace
    Rather than using Color::GetHSL/GetHWB/GetRGBA, use ConvertToColorSpace
    in the color space specific CSSRGB, CSSHSL and CSSHWB.
    
    Remove the CreateCSSRGBByNumbers() helper in favor of just using the
    constructor that takes a Color.
    
    This removes the only user of Color::GetHWB(), so remove it.
    
    TEST=external/wpt/css/css-typed-om/stylevalue-subclasses/cssColorValue.html
    
    Bug: 40264727
    Change-Id: I22445e6d9e9d946b99656096f7e7174918673635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6381913
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456158}

--- third_party/blink/renderer/core/css/cssom/css_hsl.cc
+++ third_party/blink/renderer/core/css/cssom/css_hsl.cc
@@ -14,15 +14,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 CSSHSL::CSSHSL(const Color& input_color) {
-  double h, s, l;
-  input_color.GetHSL(h, s, l);
-  h_ = CSSUnitValue::Create(h * 360, CSSPrimitiveValue::UnitType::kDegrees);
-  s_ = CSSUnitValue::Create(s * 100, CSSPrimitiveValue::UnitType::kPercentage);
-  l_ = CSSUnitValue::Create(l * 100, CSSPrimitiveValue::UnitType::kPercentage);
+  Color hsl_color = input_color;
+  hsl_color.ConvertToColorSpace(Color::ColorSpace::kHSL);
 
-  double a = input_color.Alpha();
-  alpha_ =
-      CSSUnitValue::Create(a * 100, CSSPrimitiveValue::UnitType::kPercentage);
+  h_ = CSSUnitValue::Create(hsl_color.Param0(),
+                            CSSPrimitiveValue::UnitType::kDegrees);
+  s_ = CSSUnitValue::Create(hsl_color.Param1() * 100.0,
+                            CSSPrimitiveValue::UnitType::kPercentage);
+  l_ = CSSUnitValue::Create(hsl_color.Param2() * 100.0,
+                            CSSPrimitiveValue::UnitType::kPercentage);
+  alpha_ = CSSUnitValue::Create(hsl_color.Alpha() * 100.0,
+                                CSSPrimitiveValue::UnitType::kPercentage);
 }
 
 CSSHSL::CSSHSL(CSSNumericValue* h,
--- third_party/blink/renderer/core/css/cssom/css_rgb.h
+++ third_party/blink/renderer/core/css/cssom/css_rgb.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/cssom/css_color_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_numeric_value.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/graphics/color.h"
 
 namespace blink {
 
@@ -28,7 +29,7 @@ class CORE_EXPORT CSSRGB final : public CSSColorValue {
                         ExceptionState& exception_state);
 
   // Internal constructor used by blink.
-  explicit CSSRGB(const Color&);
+  explicit CSSRGB(const Color&, Color::ColorSpace);
   CSSRGB(CSSNumericValue*,
          CSSNumericValue*,
          CSSNumericValue*,
