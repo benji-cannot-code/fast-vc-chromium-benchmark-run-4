7a95e5301b1bb3cd19667731902e1923df028a68
Ioana Pandele <ioanap@chromium.org>
2025-05-06 06:35:57 -0700

    Revert "Reland "Enable usage scenarios for tab metrics on Android""
    This reverts commit 75083e72a8841eccc3a8f1b6e047a0738685908b.
    
    Reason for revert: It seems that the CL originally being reverted had
    a compile dependency on https://chromium-review.googlesource.com/c/chromium/src/+/6468605. I've reverted that now, so hopefully there's no more compile error.
    
    Bug: 412634171
    Original change's description:
    > Reland "Enable usage scenarios for tab metrics on Android"
    >
    > This reverts commit 0080535cb522ee751b9a33b1493750361bad9776.
    >
    > Reason for revert: revert caused compile failure: https://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-compile-rel/28638/overview
    >
    > Bug: 412634171
    > Original change's description:
    > > Revert "Enable usage scenarios for tab metrics on Android"
    > >
    > > This reverts commit 880b5b4643edc4d35752a06a7db1b13ebdc19547.
    > >
    > > Reason for revert: Failing on auto builders crbug.com/415848636
    > >
    > > Bug: 412634171
    > > Original change's description:
    > > > Enable usage scenarios for tab metrics on Android
    > > >
    > > > Bug: 412634171
    > > > Change-Id: I395dbf2c27f53fe75dc3aacf52e24e7f25012765
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6468605
    > > > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > > > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1455907}
    > >
    > > Bug: 412634171
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Change-Id: I3bca0fc4dd73cd9bd594442c444984fa67c37eaa
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514120
    > > Owners-Override: Brandon Wylie <wylieb@google.com>
    > > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Auto-Submit: Brandon Wylie <wylieb@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1455977}
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 412634171
    > Change-Id: I89c1734f977460ba213950a06743df75a3957413
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514338
    > Commit-Queue: Joey Arhar <jarhar@chromium.org>
    > Owners-Override: Joey Arhar <jarhar@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Joey Arhar <jarhar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1455982}
    
    Bug: 412634171
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I3bac60d94d9542fde51a002e1dcdeda8d6fc741d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513145
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1456275}

--- chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker.cc
+++ chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker.cc
@@ -12,15 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/visibility.h"
 #include "content/public/browser/web_contents.h"
-#include "extensions/buildflags/buildflags.h"
+#include "extensions/browser/script_injection_tracker.h"
 #include "extensions/common/extension_id.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "ui/display/screen.h"
 #include "url/origin.h"
-
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "extensions/browser/script_injection_tracker.h"
-#endif
 
 namespace metrics {
 
@@ -38,9 +34,6 @@ std::pair<ukm::SourceId, url::Origin> GetNavigationInfoForContents(
 
 extensions::ExtensionIdSet GetExtensionsThatRanContentScriptsInWebContents(
     content::WebContents* contents) {
-#if !BUILDFLAG(ENABLE_EXTENSIONS)
-  return {};
-#else
   content::RenderFrameHost* main_frame = contents->GetPrimaryMainFrame();
   if (!main_frame) {
     // WebContents is being destroyed.
@@ -86,7 +79,6 @@ extensions::ExtensionIdSet GetExtensionsThatRanContentScriptsInWebContents(
                          GetExtensionsThatRanContentScriptsInProcess(*process));
   }
   return extensions;
-#endif
 }
 
 }  // namespace
@@ -352,9 +344,8 @@ void TabUsageScenarioTracker::OnWebContentsRemoved(
             web_contents->GetVisibility() == content::Visibility::VISIBLE);
   // If |web_contents| is tracked in the list of visible WebContents then a
   // synthetic visibility change event should be emitted.
-  if (iter != visible_tabs_.end()) {
+  if (iter != visible_tabs_.end())
     OnTabBecameHidden(&iter);
-  }
 
   // Remove |web_contents| from the list of contents playing video. If
   // necessary, the data store was already informed that a video stopped playing
