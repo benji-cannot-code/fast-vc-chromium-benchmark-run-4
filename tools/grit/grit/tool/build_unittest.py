d58397b3876dbe7fa0f4a0a4fd8a2d896a9e6a2c
Glenn Hartmann <hartmanng@chromium.org>
2025-05-06 11:32:36 -0700

    [Grit] Rely on '/values' rather than '/res/' for Android strings paths.
    https://chromium-review.googlesource.com/c/chromium/src/+/6511926
    assumed that every Android strings.xml path would contain '/res/' (with
    the additional implicit assumption that 'values' would follow '/res/').
    Turns out that's not always true - there are many examples of having raw
    '/values' directories that aren't contained within a '/res/' directory.
    For example,
    'gen/chrome/android/webapk/shell_apk/values-af/android_webapk_strings.xml'.
    
    Change-Id: I0bd20d8f21352982c0582294e7b953a19c1ccffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6515783
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456493}

--- tools/grit/grit/tool/build_unittest.py
+++ tools/grit/grit/tool/build_unittest.py
@@ -220,8 +220,7 @@ class BuildUnittest(unittest.TestCase):
                     'java/res/values-am/components_strings.xml')),
             '-a',
             os.path.abspath(
-                output_dir.GetPath(
-                    'java/res/values-ar/components_strings.xml')),
+                output_dir.GetPath('values-ar/components_strings.xml')),
         ]))
 
     # Incomplete output file list (while zipping XMLs) should fail.
@@ -310,8 +309,7 @@ class BuildUnittest(unittest.TestCase):
     self.assertTrue(
         os.path.exists(
             os.path.abspath(
-                output_dir.GetPath(
-                    'java/res/values-ar/components_strings.xml'))))
+                output_dir.GetPath('values-ar/components_strings.xml'))))
 
     output_dir.CleanUp()
 
@@ -342,8 +340,7 @@ class BuildUnittest(unittest.TestCase):
     self.assertFalse(
         os.path.exists(
             os.path.abspath(
-                output_dir.GetPath(
-                    'java/res/values-ar/components_strings.xml'))))
+                output_dir.GetPath('values-ar/components_strings.xml'))))
 
     with zipfile.ZipFile(zip_path, 'r') as zip_file:
       for info in zip_file.infolist():
