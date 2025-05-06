73b9230b655d1122c9c277f96fa177537756454a
Avi Drissman <avi@chromium.org>
2025-05-06 16:05:03 -0700

    Extract a range using -rangeValue
    Using -[NSValue getValue:] has been soft-deprecated for a while, so it
    should be avoided. In any case, using -rangeValue to get a range from
    an NSValue is the correct way to do it, so switch to that.
    
    NO_IFTTT=changes implementation, but no external effect, so no need to match a behavior change in the other implementation
    
    Bug: 327473318
    Change-Id: Idb2e687f5ff4243f8698f1c4e7d13799201720a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512109
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456657}

--- ui/accessibility/platform/ax_platform_node_cocoa.mm
+++ ui/accessibility/platform/ax_platform_node_cocoa.mm
@@ -2434,8 +2434,7 @@ const ui::CocoaActionList& GetCocoaActionListForTesting() {
 
 // LINT.IfChange
 - (NSString*)AXSelectedText {
-  NSRange selectedTextRange;
-  [[self AXSelectedTextRange] getValue:&selectedTextRange];
+  NSRange selectedTextRange = [[self AXSelectedTextRange] rangeValue];
   return [[self getAXValueAsString] substringWithRange:selectedTextRange];
 }
 // LINT.ThenChange(accessibilitySelectedText)
