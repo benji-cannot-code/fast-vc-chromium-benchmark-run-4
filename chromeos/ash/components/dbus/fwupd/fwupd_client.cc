ef56883d42743076d99480fc75fd93e77034206f
Rishabh Agrawal <rishabhagr@chromium.org>
2025-05-06 11:50:20 -0700

    Add fwupd dev mode logic
    Add logic to skip URI authentication and trusted reports requirement.
    Add unit tests.
    
    BUG=b:415107365
    TEST=chromeos_unittests
    
    Change-Id: Ia21fa88063502457f1823e7e65c898b332a4b2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507902
    Commit-Queue: Rishabh Agrawal <rishabhagr@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456524}

--- chromeos/ash/components/dbus/fwupd/fwupd_client.cc
+++ chromeos/ash/components/dbus/fwupd/fwupd_client.cc
@@ -387,8 +387,9 @@ class FwupdClientImpl : public FwupdClient {
       can_parse = false;
     }
 
-    const bool needs_trusted_report = !features::IsFlexFirmwareUpdateEnabled();
-    FIRMWARE_LOG(DEBUG) << "Trusted reports required: " << needs_trusted_report;
+    const bool needs_trusted_report =
+        !features::IsFlexFirmwareUpdateEnabled() &&
+        !features::IsFwupdDeveloperModeEnabled();
 
     FwupdUpdateList updates;
     while (can_parse && array_reader.HasMoreData()) {
@@ -408,7 +409,9 @@ class FwupdClientImpl : public FwupdClient {
       std::optional<bool> trusted_report = dict.FindBool(kHasTrustedReportKey);
       const bool has_trusted_report =
           trusted_report.has_value() && trusted_report.value();
-      FIRMWARE_LOG(DEBUG) << "Trusted Reports: " << has_trusted_report;
+      FIRMWARE_LOG(DEBUG) << "Trusted Reports required: "
+                          << needs_trusted_report
+                          << "; Trusted Reports found: " << has_trusted_report;
       const bool missing_trusted_report =
           needs_trusted_report && !has_trusted_report;
 
@@ -680,8 +683,17 @@ base::FilePath GetUpdatePathFromDict(const base::Value::Dict& dict) {
     return base::FilePath();
   }
 
-  // Convert to a FilePath and verify the extension.
+  // Convert to a FilePath
   base::FilePath path(url.spec());
+
+  // Force return; don't authenticate URL further.
+  if (features::IsFwupdDeveloperModeEnabled()) {
+    FIRMWARE_LOG(DEBUG)
+        << "Developer mode detected; URI authentication skipped";
+    return path;
+  }
+
+  // Verify the extension.
   if (path.Extension() != kCabFileExtension) {
     FIRMWARE_LOG(ERROR) << "Invalid location extension: " << path;
     return base::FilePath();
