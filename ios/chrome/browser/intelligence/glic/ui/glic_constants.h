ff53f2580f2f1e8037a01d4fe31b6c7b592c6a26
“Prasanaa” <prasanaa@google.com>
2025-05-06 15:08:58 -0700

    [iOS] Partially implement UI for GLIC Consent Flow
    This CL sets up the main stack for the GLIC Consent Flow.
    
    Bug: 414777915, 414791668
    Change-Id: I4b848f0f64c21e34b3fc5e2d40dc014fd4760926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512514
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456637}

---
+++ ios/chrome/browser/intelligence/glic/ui/glic_constants.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INTELLIGENCE_GLIC_UI_GLIC_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_INTELLIGENCE_GLIC_UI_GLIC_CONSTANTS_H_
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// Sheet detents.
+extern NSString* const kGLICConsentPartialDetentIdentifier;
+extern NSString* const kGLICConsentFullDetentIdentifier;
+extern const CGFloat kGLICConsentPartialDetentHeight;
+extern const CGFloat kGLICConsentFullDetentHeight;
+extern const CGFloat kGLICConsentPreferredCornerRadius;
+
+// Stack view insets and spacing.
+extern const CGFloat kGLICConsentMainStackHorizontalInset;
+extern const CGFloat kGLICConsentMainStackTopInset;
+extern const CGFloat kGLICConsentMainStackSpacing;
+
+// Promo style strings.
+extern NSString* const kGLICConsentPrimaryAction;
+extern NSString* const kGLICConsentSecondaryAction;
+
+#endif  // IOS_CHROME_BROWSER_INTELLIGENCE_GLIC_UI_GLIC_CONSTANTS_H_
---
+++ ios/chrome/browser/intelligence/glic/ui/glic_constants.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/intelligence/glic/ui/glic_constants.h"
+
+// Sheet detents.
+NSString* const kGLICConsentPartialDetentIdentifier =
+    @"GLICConsentPartialDetentIdentifier";
+NSString* const kGLICConsentFullDetentIdentifier =
+    @"GLICConsentFullDetentIdentifier";
+const CGFloat kGLICConsentPartialDetentHeight = 300.0;
+const CGFloat kGLICConsentFullDetentHeight = 500.0;
+const CGFloat kGLICConsentPreferredCornerRadius = 10.0;
+
+// Stack view insets and spacing.
+const CGFloat kGLICConsentMainStackHorizontalInset = 20.0;
+const CGFloat kGLICConsentMainStackTopInset = 24.0;
+const CGFloat kGLICConsentMainStackSpacing = 16.0;
+
+// Promo style strings.
+// TODO(crbug.com/414778685): Add strings.
+NSString* const kGLICConsentPrimaryAction = @"Yes, I'm in";
+NSString* const kGLICConsentSecondaryAction = @"No thanks";
