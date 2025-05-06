d9e52a24d6fd2e528d57d6badd52d5519baf9e5a
Sahir Vellani <sahir.vellani@microsoft.com>
2025-05-06 02:11:02 -0700

    ScopedAllowBlocking for loading DispatcherQueueController in WUCBackdrop
    A Windows.UI.Composition Compositor can only be loaded on a thread with
    a DispatcherQueueController. Creating the controller requires the
    loading of CoreMessaging.dll, which is a potentially blocking action.
    Therefore, allow blocking when loading the library.
    
    Making the operation asynchronous was considered. However, it could lead
    to negative UX. For example, an empty window with only a border may be
    visible, followed by the WUC backdrop, then followed by the first
    contentful frame. There is a scenario where the user believes they're
    clicking on the window behind the browser window, but the click actually
    goes to the (completely empty) browser window itself while the
    WUCBackdrop is loading.
    
    It is expected that CoreMessaging should already be cached by the system
    and in the average case would not need to be read from disk. Running
    `tasklist -m coremessaging.dll` shows that explorer.exe and svchost.exe
    use coremessaging.dll. Additionally, on Win11, Chromium browsers already
    load coremessaging.dll via other, existing code; the additional
    LoadLibrary added for WUC backdrop just adds a reference to the already
    loaded DLL in the process. If performance regressions are observed due
    to this change, more time can be invested in the asynchronous path.
    
    Bug: 41241478
    Change-Id: Ib970f43d8d45643eeba37e8dbb7189e7c2801e44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489643
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1456161}

--- ui/gfx/win/wuc_backdrop.cc
+++ ui/gfx/win/wuc_backdrop.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/scoped_native_library.h"
 #include "base/threading/sequence_local_storage_slot.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/win/core_winrt_util.h"
 #include "base/win/hstring_reference.h"
 
@@ -24,15 +25,11 @@ using CreateDispatcherQueueControllerProc =
     decltype(&::CreateDispatcherQueueController);
 namespace WUC = ABI::Windows::UI::Composition;
 
-FARPROC LoadCoreMessagingFunction(const char* function_name) {
-  static HMODULE const handle = base::LoadSystemLibrary(L"CoreMessaging.dll");
-  return handle ? ::GetProcAddress(handle, function_name) : nullptr;
-}
-
 CreateDispatcherQueueControllerProc GetOrCreateDispatcherQueueControllerProc() {
   static CreateDispatcherQueueControllerProc const function =
       reinterpret_cast<CreateDispatcherQueueControllerProc>(
-          LoadCoreMessagingFunction("CreateDispatcherQueueController"));
+          WUCBackdrop::LoadCoreMessagingFunction(
+              "CreateDispatcherQueueController"));
   return function;
 }
 
@@ -143,6 +140,14 @@ void WUCBackdrop::UpdateBackdropColor(SkColor color) {
                                      static_cast<BYTE>(SkColorGetG(color)),
                                      static_cast<BYTE>(SkColorGetB(color))});
   CHECK_EQ(hr, S_OK);
+}
+
+FARPROC WUCBackdrop::LoadCoreMessagingFunction(const char* function_name) {
+  static HMODULE handle = [] {
+    base::ScopedAllowBlocking scoped_allow_blocking;
+    return base::LoadSystemLibrary(L"CoreMessaging.dll");
+  }();
+  return handle ? ::GetProcAddress(handle, function_name) : nullptr;
 }
 
 WUCBackdrop::~WUCBackdrop() = default;
