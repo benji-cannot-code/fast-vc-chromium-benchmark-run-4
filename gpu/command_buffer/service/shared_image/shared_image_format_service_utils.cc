317cc7fe8db88ea381e482716d41aec9b7da2b36
Saifuddin Hitawala <hitawala@chromium.org>
2025-05-06 05:53:52 -0700

    [gpu] Remove ToDawnFormat crash key
    We added ToDawnFormat crash key to investigate crashes that happened
    due to missing equivalent dawn format supported for shared image when
    supporting graphite. We haven't seen any new crashes with this crash
    key for some time and can remove it.
    
    Bug: 364549427
    Change-Id: I68dfd6d678d7ff54dab52239e33db941253ff1ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512475
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456248}

--- gpu/command_buffer/service/shared_image/shared_image_format_service_utils.cc
+++ gpu/command_buffer/service/shared_image/shared_image_format_service_utils.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "build/buildflag.h"
-#include "components/crash/core/common/crash_key.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
 #include "gpu/command_buffer/service/feature_info.h"
 #include "gpu/ipc/common/vulkan_ycbcr_info.h"
@@ -533,11 +532,6 @@ wgpu::TextureFormat ToDawnFormat(viz::SharedImageFormat format) {
     return wgpu::TextureFormat::R10X6BG10X6Biplanar444Unorm;
   }
 
-  // Unknown format: crash, surfacing the format.
-  static crash_reporter::CrashKeyString<256> crash_key(
-      "SIFServiceUtils ToDawnFormat error");
-  crash_reporter::ScopedCrashKeyString crash_key_scope(&crash_key,
-                                                       format.ToString());
   NOTREACHED() << "Unsupported format: " << format.ToString();
 }
 
