ab40d38e0e11d5917fffe841867af06f27211bfe
Jan Keitel <jkeitel@google.com>
2025-05-06 04:39:19 -0700

    Use C++20 requires instead of SFINAE in //chrome
    No functionality changes, just clean-up/code health.
    
    Bug: 415007784
    Change-Id: Ic7f6e8637cad0ef6e8170aaa0823c66751cb22a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507928
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456220}

--- chrome/browser/privacy_budget/mesa_distribution.h
+++ chrome/browser/privacy_budget/mesa_distribution.h
@@ -68,8 +68,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Parameters:
 //   pivot_point: T as described above. Any value bigger than 0.
 //   dist_ratio : τ as described above. Must be in (0,1).
-template <typename ResultType,
-          std::enable_if_t<std::is_integral<ResultType>::value, int> = 0>
+template <typename ResultType>
+  requires(std::is_integral_v<ResultType>)
 class MesaDistribution {
  public:
   MesaDistribution(ResultType pivot_point,
