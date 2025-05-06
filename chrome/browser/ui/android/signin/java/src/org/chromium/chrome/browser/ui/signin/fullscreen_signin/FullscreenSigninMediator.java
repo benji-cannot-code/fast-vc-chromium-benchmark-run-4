a6aa96f55d3f1fb9b9f840adeb0e124a35b6bacd
Tanmoy Mollik <triploblastic@google.com>
2025-05-06 08:12:04 -0700

    [Android] Replace email with CoreAccountInfo in account picker
    Bug: 40144553, 41487829
    Change-Id: I8d787a4e565b0996a64e3b0376ec923e79aeeb1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495007
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456349}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fullscreen_signin/FullscreenSigninMediator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fullscreen_signin/FullscreenSigninMediator.java
@@ -299,8 +299,8 @@ public class FullscreenSigninMediator
     }
 
     @Override
-    public void onAccountSelected(String accountName) {
-        setSelectedAccountEmail(accountName);
+    public void onAccountSelected(CoreAccountInfo coreAccountInfo) {
+        setSelectedAccountEmail(coreAccountInfo.getEmail());
         if (mDialogCoordinator != null) mDialogCoordinator.dismissDialog();
     }
 
