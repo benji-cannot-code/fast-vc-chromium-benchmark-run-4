8f808d8b6854fd3e0ffa053b9d718077b2817dfd
Christian Biesinger <cbiesinger@chromium.org>
2025-05-06 08:03:13 -0700

    [FedCM] Endpoint changes to support iframe origins
    Sends the top frame origin to the client metadata endpoint and
    parses the received boolean whether the client matches that
    origin.
    
    There are no UI changes in this CL yet.
    
    Behind the new FedCmIframeOrigin flag.
    
    Per the proposal here:
    https://github.com/w3c-fedid/FedCM/issues/449#issuecomment-1515631336
    
    Bug: 390581529
    Change-Id: I9813f42d11c892b6bc6edb0ea0d6787dfb2039c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6502076
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456338}

--- content/browser/webid/test/mock_idp_network_request_manager.cc
+++ content/browser/webid/test/mock_idp_network_request_manager.cc
@@ -11,6 +11,7 @@ namespace content {
 
 MockIdpNetworkRequestManager::MockIdpNetworkRequestManager()
     : IdpNetworkRequestManager(url::Origin(),
+                               url::Origin(),
                                nullptr,
                                nullptr,
                                network::mojom::ClientSecurityState::New()) {}
