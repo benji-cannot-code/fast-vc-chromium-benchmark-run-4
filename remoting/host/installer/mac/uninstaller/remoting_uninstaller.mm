f78e00454f8885f25640a2c05d57a08021712ed9
Yuwei Huang <yuweih@chromium.org>
2025-05-06 07:46:35 -0700

    [crd host][mac] Fix preinstall and uninstall cleanups
    Currently both the preflight script and the uninstall code use the
    `launchctl unload` command to unload the broker service. However,
    `unload` has been deprecated and it has several issues:
    
    1. It requires the plist file to exist. Meaning if you have somehow
       deleted the file, then you won't be able to unload the service.
    2. It infers the "domain" (whether the service should be run as root or
       as a specific user) from the current user context. It seems in some
       cases the script has $USER set to the current user but still has root
       privileges, while in other cases it just has $USER set to root. If
       launchctl for some reason infers that the domain is not "system" then
       it will fail to unload the broker.
    
    I'm not sure which issue has led to the zombie broker problem, or
    whether it a combination of two issues. This CL replaces the deprecated
    load/unload commands with bootstrap/bootout. The new commands require
    specifying the domain, and unload takes the service name as an argument
    and allows the service to be unloaded after the file is deleted. The
    new commands have been introduced since macOS 10.10. We have pinned the
    host version of macOS 10.10 to 101.0.4951.26 already, so it's safe to
    not provide a fallback.
    
    I've also added logic to kill all the CRD processes, which allows the
    updated versions of native messaging hosts to be loaded, and further
    minimizes the chance of having a zombie broker.
    
    Bug: 411490453
    Change-Id: I2127290eb3e661a2e97dd2feec0da8f5435592d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6515218
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456317}

--- remoting/host/installer/mac/uninstaller/remoting_uninstaller.mm
+++ remoting/host/installer/mac/uninstaller/remoting_uninstaller.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/authorization_util.h"
 #include "base/mac/scoped_authorizationref.h"
+#include "base/strings/stringprintf.h"
 #include "remoting/host/mac/constants_mac.h"
 
 void logOutput(FILE* pipe) {
@@ -117,12 +118,18 @@ NSArray<NSString*>* convertToNSArray(const char** array) {
     [self runCommand:launchCtl withArguments:argsUnload];
   }
 
-  if ([NSFileManager.defaultManager
-          fileExistsAtPath:@(remoting::kBrokerPlistPath)]) {
-    const char* argsUnload[] = {"unload", "-w", remoting::kBrokerPlistPath,
-                                nullptr};
-    [self sudoCommand:launchCtl withArguments:argsUnload usingAuth:authRef];
-  }
+  const char* argsUnloadBroker[] = {"bootout", remoting::kBrokerServiceTarget,
+                                    nullptr};
+  [self sudoCommand:launchCtl withArguments:argsUnloadBroker usingAuth:authRef];
+}
+
+- (void)killAllRemotingProcessesUsingAuth:(AuthorizationRef)authRef {
+  const char* pkill = "/usr/bin/pkill";
+  std::string remoting_processes_regex =
+      base::StringPrintf("^%s.*$", remoting::kHostBinaryPath);
+  const char* argsPkill[] = {"-9", "-f", remoting_processes_regex.data(),
+                             nullptr};
+  [self sudoCommand:pkill withArguments:argsPkill usingAuth:authRef];
 }
 
 - (void)keystoneUnregisterUsingAuth:(AuthorizationRef)authRef {
@@ -157,6 +164,7 @@ NSArray<NSString*>* convertToNSArray(const char** array) {
   [self sudoDelete:remoting::kHostEnabledPath usingAuth:authRef];
 
   [self shutdownServiceUsingAuth:authRef];
+  [self killAllRemotingProcessesUsingAuth:authRef];
 
   [self sudoDelete:remoting::kServicePlistPath usingAuth:authRef];
   [self sudoDelete:remoting::kBrokerPlistPath usingAuth:authRef];
--- remoting/host/mac/constants_mac.cc
+++ remoting/host/mac/constants_mac.cc
@@ -64,4 +64,6 @@ const char kBrandedUninstallerPath[] =
 const char kUnbrandedUninstallerPath[] =
     APPLICATIONS_DIR "Chromoting Host Uninstaller.app";
 
+const char kBrokerServiceTarget[] = "system/" BROKER_NAME;
+
 }  // namespace remoting
