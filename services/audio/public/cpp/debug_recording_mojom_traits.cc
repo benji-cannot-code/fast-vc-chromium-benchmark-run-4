7221af952efd905a79adea590eb7b4049dde5c33
Olov Brändström <brandstrom@chromium.org>
2025-05-06 07:15:32 -0700

    Name loopback debug record streams as `loopback`, rather then `input`.
    Add a new DebugRecordingStreamType, and use it if we record a loopback signal.
    
    Bug: 414576792
    Change-Id: I2a8724598525aca677a652fecab3f362d951c1fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516064
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Olov Brändström <brandstrom@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456290}

--- services/audio/public/cpp/debug_recording_mojom_traits.cc
+++ services/audio/public/cpp/debug_recording_mojom_traits.cc
@@ -16,6 +16,8 @@ EnumTraits<audio::mojom::DebugRecordingStreamType,
       return audio::mojom::DebugRecordingStreamType::kInput;
     case media::AudioDebugRecordingStreamType::kOutput:
       return audio::mojom::DebugRecordingStreamType::kOutput;
+    case media::AudioDebugRecordingStreamType::kLoopback:
+      return audio::mojom::DebugRecordingStreamType::kLoopback;
   }
   NOTREACHED();
 }
@@ -30,6 +32,9 @@ bool EnumTraits<audio::mojom::DebugRecordingStreamType,
       return true;
     case audio::mojom::DebugRecordingStreamType::kOutput:
       *out = media::AudioDebugRecordingStreamType::kOutput;
+      return true;
+    case audio::mojom::DebugRecordingStreamType::kLoopback:
+      *out = media::AudioDebugRecordingStreamType::kLoopback;
       return true;
   }
   NOTREACHED();
