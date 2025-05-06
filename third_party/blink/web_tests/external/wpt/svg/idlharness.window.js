080ec3a3340700710b0419d3dbbcf038752e5518
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-05-06 06:20:14 -0700

    Import wpt@16a6ce620e84ed527800524cf34cb72f6af31bfe
    https://github.com/web-platform-tests/wpt/compare/4e5ff79b8...16a6ce620
    
    Using wpt-import in Chromium d2418dde5777d43949038584536efc81533a5e9a.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I02e3c3ab9520c0d14a3d2967963bc5148add6fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513362
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1456258}

--- third_party/blink/web_tests/external/wpt/svg/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/svg/idlharness.window.js
@@ -112,7 +112,6 @@ idl_test(
       SVGGElement: ['objects.g'],
       SVGDefsElement: ['objects.defs'],
       SVGDescElement: ['objects.desc'],
-      SVGDiscardElement: ['objects.discard'],
       SVGTitleElement: ['objects.title'],
       SVGSymbolElement: ['objects.symbol'],
       SVGUseElement: ['objects.use'],
