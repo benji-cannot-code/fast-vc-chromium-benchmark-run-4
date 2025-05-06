b538a9d5a3586898fa9649922ac31c0dedecb716
Tom Sepez <tsepez@chromium.org>
2025-05-06 15:26:45 -0700

    Add ability to store (28 bit) value in xmlDoc and xmlAttr.
    Then use it to avoid storing generated ID in a pointer member reserved
    for a libxml data structure, which is a layering violation.
    
    This unfortunately truncated the max ID value by a few bits. We could
    squeeze two unused flag bits if we need to make this wider.
    
    Change-Id: Ie8d6673b9cb2849c6ceb7cafdedd2c557c1929f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508403
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456645}

--- third_party/libxslt/src/libxslt/functions.c
+++ third_party/libxslt/src/libxslt/functions.c
@@ -761,7 +761,7 @@ xsltGenerateIdFunction(xmlXPathParserContextPtr ctxt, int nargs){
     }
 
     if (xsltGetSourceNodeFlags(cur) & XSLT_SOURCE_NODE_HAS_ID) {
-        id = (unsigned long) (size_t) *psviPtr;
+        id = (unsigned long) xsltGetSourceNodeValue(cur);
     } else {
         if (cur->type == XML_TEXT_NODE && cur->line == USHRT_MAX) {
             /* Text nodes store big line numbers in psvi. */
@@ -773,7 +773,7 @@ xsltGenerateIdFunction(xmlXPathParserContextPtr ctxt, int nargs){
             goto out;
         }
 
-        if (tctxt->currentId == ULONG_MAX) {
+        if (tctxt->currentId == XSLT_SOURCE_NODE_VALUE_MAX) {
             xsltTransformError(tctxt, NULL, NULL,
                     "generate-id(): id overflow\n");
             ctxt->error = XPATH_MEMORY_ERROR;
@@ -781,7 +781,7 @@ xsltGenerateIdFunction(xmlXPathParserContextPtr ctxt, int nargs){
         }
 
         id = ++tctxt->currentId;
-        *psviPtr = (void *) (size_t) id;
+        xsltSetSourceNodeValue(cur, id);
         xsltSetSourceNodeFlags(tctxt, cur, XSLT_SOURCE_NODE_HAS_ID);
     }
 
