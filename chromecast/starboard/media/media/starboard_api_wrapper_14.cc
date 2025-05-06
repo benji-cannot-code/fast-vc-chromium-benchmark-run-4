4d924cd455d14498b6a1e698a5d40ee0f4ad6159
Antonio Rivera <antoniori@google.com>
2025-05-06 12:21:12 -0700

    Remove more memcpy uses in //chromecast/starboard.
    Not sure how these didn't cause compile failures earlier, but there are
    simple fixes.
    
    Test:
    Compiled SB14 version of core_runtime_starboard.
    Compiled and ran SB15 version of core_runtime_starboard on gLinux.
    
    Bug: b:399506828
    Change-Id: I010a544c053fc234715da62d81adf9c7b2ad122a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6435041
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456545}

--- chromecast/starboard/media/media/starboard_api_wrapper_14.cc
+++ chromecast/starboard/media/media/starboard_api_wrapper_14.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check.h"
+#include "base/containers/span.h"
 #include "base/logging.h"
 #include "chromecast/starboard/media/media/starboard_api_wrapper_base.h"
 
@@ -20,6 +21,12 @@ namespace chromecast {
 namespace media {
 
 namespace {
+
+static_assert(sizeof(SbMediaColorMetadata::custom_primary_matrix) ==
+              sizeof(StarboardColorMetadata::custom_primary_matrix));
+
+constexpr size_t kCustomPrimaryMatrixSize =
+    std::size(StarboardColorMetadata{}.custom_primary_matrix);
 
 // A concrete implementation of StarboardApiWrapper for Starboard version 14.
 class StarboardApiWrapper14 : public StarboardApiWrapperBase {
@@ -123,12 +130,9 @@ class StarboardApiWrapper14 : public StarboardApiWrapperBase {
     out_color_metadata.range =
         static_cast<SbMediaRangeId>(in_color_metadata.range);
 
-    static_assert(sizeof(out_color_metadata.custom_primary_matrix) ==
-                      sizeof(in_color_metadata.custom_primary_matrix),
-                  "Struct field size mismatch (custom_primary_matrix)");
-    memcpy(out_color_metadata.custom_primary_matrix,
-           in_color_metadata.custom_primary_matrix,
-           sizeof(out_color_metadata.custom_primary_matrix));
+    base::span<float, kCustomPrimaryMatrixSize>(
+        out_color_metadata.custom_primary_matrix)
+        .copy_from_nonoverlapping(in_color_metadata.custom_primary_matrix);
 
     return out_video_info;
   }
