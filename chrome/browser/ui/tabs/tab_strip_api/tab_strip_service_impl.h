52f87c66d10412f4ee5af6b2a389d9c30a7ec807
Fred Shih <ffred@chromium.org>
2025-05-06 11:11:58 -0700

    Add receiver set to service impl
    This allows the tab service impl to start accepting clients. This also
    removes the need for scaffolding in test.
    
    Change-Id: I2cc7970295e42d07bbc39c0985bf561dfa822657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514156
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456458}

--- chrome/browser/ui/tabs/tab_strip_api/tab_strip_service_impl.h
+++ chrome/browser/ui/tabs/tab_strip_api/tab_strip_service_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_api.mojom.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
 
 class BrowserWindowInterface;
 class TabStripModel;
@@ -29,6 +30,8 @@ class TabStripServiceImpl : public tabs_api::mojom::TabStripService,
 
   void AddObserver(tabs_api::mojom::TabsObserver* observer);
   void RemoveObserver(tabs_api::mojom::TabsObserver* observer);
+
+  void Accept(mojo::PendingReceiver<tabs_api::mojom::TabStripService> client);
 
   // tabs_api::mojom::TabStripService overrides
   void CreateTabAt(tabs_api::mojom::PositionPtr pos,
@@ -56,6 +59,7 @@ class TabStripServiceImpl : public tabs_api::mojom::TabStripService,
   // If usage expands to different threads, this needs to transition to
   // ObserverListThreadSafe.
   base::ObserverList<tabs_api::mojom::TabsObserver, true>::Unchecked observers_;
+  mojo::ReceiverSet<tabs_api::mojom::TabStripService> clients_;
 };
 
 #endif  // CHROME_BROWSER_UI_TABS_TAB_STRIP_API_TAB_STRIP_SERVICE_IMPL_H_
