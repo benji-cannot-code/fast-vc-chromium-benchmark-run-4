e6d1c9f61922bbf24f9cb49e3c361ca494b1d379
Timofey Chudakov <tchudakov@google.com>
2025-05-06 05:39:10 -0700

    Revert "[grit] Optionally output Android xml strings as a single zip file."
    This reverts commit 1968fce7b6f5253f08435395907f8d3babce3097.
    
    Reason for revert: This CL breaks the script to upload UI screenshots.
    Error message: gpaste/6168739432890368
    
    Original change's description:
    > [grit] Optionally output Android xml strings as a single zip file.
    >
    > This enables us to simplify some GN logic and have fewer expected
    > outputs. We should be able to remove the java_strings_grd_prebuilt rule
    > altogether, since the only thing is does is zip Android xml files that
    > were generated by different rules. java_strings_grd can also be simpler
    > by removing its zipping logic.
    >
    > Change-Id: I321a6f303ef9b30737841a2d91aa3b1754808c51
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509131
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1455991}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I3e54776f1c228fd3e4e6045635c76659d2d07e86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512875
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456242}

--- tools/grit/grit_info.py
+++ tools/grit/grit_info.py
@@ -24,8 +24,7 @@ def Outputs(filename,
             defines,
             ids_file,
             target_platform=None,
-            translate_genders=False,
-            android_output_zip_path=None):
+            translate_genders=False):
   grd = grd_reader.Parse(filename,
                          defines=defines,
                          tags_to_ignore={'messages'},
@@ -33,13 +32,17 @@ def Outputs(filename,
                          target_platform=target_platform,
                          translate_genders=translate_genders)
 
-  target = [
-      i.GetFilename() for i in grd.GetOutputFiles()
-      if i.GetType() != 'android' or android_output_zip_path is None
-  ]
+  target = []
+  lang_folders = {}
+  # Add all explicitly-specified output files
+  for output in grd.GetOutputFiles():
+    path = output.GetFilename()
+    target.append(path)
 
-  if android_output_zip_path is not None:
-    target.append(android_output_zip_path)
+    if path.endswith('.h'):
+      path, filename = os.path.split(path)
+    if output.attrs['lang']:
+      lang_folders[output.attrs['lang']] = os.path.dirname(path)
 
   return [t.replace('\\', '/') for t in target]
 
@@ -119,9 +122,6 @@ def DoMain(argv):
   parser.add_option("--translate-genders",
                     action="store_true",
                     dest="translate_genders")
-  parser.add_option("--android-output-zip-path",
-                    dest="android_output_zip_path",
-                    default=None)
 
   options, args = parser.parse_args(argv)
 
@@ -162,9 +162,9 @@ def DoMain(argv):
 
     prefix, filename = args
     outputs = [
-        posixpath.join(prefix, f) for f in Outputs(
-            filename, defines, options.ids_file, options.target_platform,
-            options.translate_genders, options.android_output_zip_path)
+        posixpath.join(prefix, f)
+        for f in Outputs(filename, defines, options.ids_file,
+                         options.target_platform, options.translate_genders)
     ]
     return '\n'.join(outputs)
   else:
