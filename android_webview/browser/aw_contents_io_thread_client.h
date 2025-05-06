2f2188ced69fc965008c8ac6c480e7b4ffa83796
Peter Birk Pakkenberg <pbirk@chromium.org>
2025-05-06 03:04:37 -0700

    Make shouldInterceptRequest explicitly async
    This changes the calls to shouldInterceptRequest to be posted to the
    default ThreadPool with `MayBlock` traits.
    
    This should not make any differences, since we previously created a new
    SequencedTaskRunner for eacn call that was posted, so the calls were
    never sequenced as intended.
    This is due to the fact that a new native AwContentsIoThreadClient
    object is created for each call to shouldInterceptRequest.
    
    This CL formalizes the inadvertent change in behavior that was made in
    2017 and makes it intentional.
    
    Bug: 407009844
    Change-Id: If42f66ee3cd729203deae70eaf96765f1a95baea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507228
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456186}

--- android_webview/browser/aw_contents_io_thread_client.h
+++ android_webview/browser/aw_contents_io_thread_client.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/compiler_specific.h"
 #include "base/functional/callback_forward.h"
-#include "base/task/sequenced_task_runner.h"
-#include "base/task/thread_pool.h"
 #include "components/safe_browsing/content/browser/web_contents_key.h"
 #include "content/public/browser/frame_tree_node_id.h"
 #include "content/public/browser/global_routing_id.h"
@@ -153,8 +151,6 @@ class AwContentsIoThreadClient {
 
  private:
   base::android::ScopedJavaGlobalRef<jobject> java_object_;
-  scoped_refptr<base::SequencedTaskRunner> sequenced_task_runner_ =
-      base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()});
 };
 
 }  // namespace android_webview
