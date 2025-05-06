a23a9f72c61cdf2970cb65a9c7431ac1d0b90491
Maggie Dong <jiaxindong@google.com>
2025-05-06 16:30:10 -0700

    Make Crossbench speedomter3 target point to the latest version.
    Remove reference to 3.0 & 3.1 and replace them with a single target.
    
    Bug: 410324958
    Change-Id: Iccebd5aedd177f445c96404ed7327897d29b162a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514531
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Maggie Dong <jiaxindong@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456663}

--- tools/perf/core/bot_platforms.py
+++ tools/perf/core/bot_platforms.py
@@ -381,14 +381,14 @@ def _crossbench_speedometer2_1(estimated_runtime=60, arguments=None):
 
 def _crossbench_speedometer2(estimated_runtime=60, arguments=None):
   """Alias for the latest Speedometer 2.X version."""
-  return CrossbenchConfig('speedometer2.1.crossbench',
-                          'speedometer_2.1',
+  return CrossbenchConfig('speedometer2.crossbench',
+                          'speedometer_2',
                           estimated_runtime=estimated_runtime,
                           arguments=arguments)
 
 
 def _crossbench_speedometer3_0(estimated_runtime=60, arguments=None):
-  return CrossbenchConfig('speedometer3.crossbench',
+  return CrossbenchConfig('speedometer3.0.crossbench',
                           'speedometer_3.0',
                           estimated_runtime=estimated_runtime,
                           arguments=arguments)
@@ -404,7 +404,7 @@ def _crossbench_speedometer3_1(estimated_runtime=60, arguments=None):
 def _crossbench_speedometer3(estimated_runtime=60, arguments=None):
   """Alias for the latest Speedometer 3.X version."""
   return CrossbenchConfig('speedometer3.crossbench',
-                          'speedometer_3.0',
+                          'speedometer_3',
                           estimated_runtime=estimated_runtime,
                           arguments=arguments)
 
@@ -499,20 +499,17 @@ def _crossbench_loadline_tablet(estimated_runtime=3600, arguments=None):
 
 _CROSSBENCH_JETSTREAM_SPEEDOMETER = frozenset([
     _crossbench_jetstream2(),
-    _crossbench_speedometer3_0(),
-    _crossbench_speedometer3_1(),
+    _crossbench_speedometer3(),
 ])
 
 _CROSSBENCH_MOTIONMARK_SPEEDOMETER = frozenset([
     _crossbench_motionmark1_3(),
-    _crossbench_speedometer3_0(),
-    _crossbench_speedometer3_1(),
+    _crossbench_speedometer3(),
 ])
 
 _CROSSBENCH_BENCHMARKS_ALL = frozenset([
     _crossbench_speedometer2(),
-    _crossbench_speedometer3_0(),
-    _crossbench_speedometer3_1(),
+    _crossbench_speedometer3(),
     _crossbench_motionmark1_3(),
     _crossbench_jetstream2(),
 ])
@@ -521,7 +518,7 @@ _CROSSBENCH_BENCHMARKS_ALL = frozenset([
 # Android.
 _CROSSBENCH_ANDROID = frozenset([
     _crossbench_speedometer3_0(arguments=['--fileserver']),
-    _crossbench_speedometer3_1(arguments=['--fileserver']),
+    _crossbench_speedometer3(arguments=['--fileserver']),
     _crossbench_loadline_phone(arguments=[
         '--cool-down-threshold=moderate',
         '--no-splash',
@@ -533,7 +530,7 @@ _CROSSBENCH_ANDROID = frozenset([
 _CROSSBENCH_PIXEL9 = frozenset([
     # _crossbench_jetstream2(arguments=['--fileserver', '--debug']),
     _crossbench_motionmark1_3(arguments=['--fileserver', '--debug']),
-    _crossbench_speedometer3_1(arguments=['--fileserver', '--debug']),
+    _crossbench_speedometer3(arguments=['--fileserver', '--debug']),
     _crossbench_loadline_phone(arguments=[
         '--cool-down-threshold=moderate',
         '--no-splash',
@@ -542,7 +539,7 @@ _CROSSBENCH_PIXEL9 = frozenset([
 ])
 
 _CROSSBENCH_ANDROID_BYRA = frozenset([
-    _crossbench_speedometer3_1(arguments=['--fileserver', '--debug']),
+    _crossbench_speedometer3(arguments=['--fileserver', '--debug']),
 ])
 
 _CROSSBENCH_TANGOR = frozenset([
