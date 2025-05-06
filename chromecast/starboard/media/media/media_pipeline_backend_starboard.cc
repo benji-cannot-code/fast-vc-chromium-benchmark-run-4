6251d18754c6508235ce0f4a486f98c2981a9e4e
Antonio Rivera <antoniori@google.com>
2025-05-06 12:56:06 -0700

    Hook up StarboardDrmWrapper to production code.
    The previous setup created one SbDrmSystem per StarboardDecryptorCast
    (CastCdm) instance. In some cases like when casting Disney+, this
    resulted in multiple SbDrmSystems being instantiated.
    
    Test:
    Cast Disney+, Hulu, and Max to a 3P TV.
    Tested both provisioning and regular DRM scenarios. Also tested
    switching between apps, and switching between DRM and non-DRM content.
    Also added unit tests.
    
    Bug: b:401610388
    Change-Id: I3dec98b9ca4cd5ea1c07e26795181866ce7d4131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509812
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456563}

--- chromecast/starboard/media/media/media_pipeline_backend_starboard.cc
+++ chromecast/starboard/media/media/media_pipeline_backend_starboard.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/task/bind_post_task.h"
 #include "chromecast/public/graphics_types.h"
+#include "chromecast/starboard/media/cdm/starboard_drm_wrapper.h"
 #include "chromecast/starboard/media/media/starboard_api_wrapper.h"
 
 namespace chromecast {
@@ -221,12 +222,20 @@ void MediaPipelineBackendStarboard::OnGeometryChanged(
 void MediaPipelineBackendStarboard::CreatePlayer() {
   DCHECK(media_task_runner_->RunsTasksInCurrentSequence());
 
+  bool has_drm = false;
   StarboardPlayerCreationParam params = {};
   if (audio_decoder_) {
     const std::optional<StarboardAudioSampleInfo>& audio_info =
         audio_decoder_->GetAudioSampleInfo();
     CHECK(audio_info);
     params.audio_sample_info = *audio_info;
+
+    std::optional<EncryptionScheme> encryption_scheme =
+        audio_decoder_->GetEncryptionScheme();
+    if (encryption_scheme.has_value() &&
+        *encryption_scheme != EncryptionScheme::kUnencrypted) {
+      has_drm = true;
+    }
   }
   if (video_decoder_) {
     const std::optional<StarboardVideoSampleInfo>& video_info =
@@ -239,7 +248,21 @@ void MediaPipelineBackendStarboard::CreatePlayer() {
       // prioritize minimizing latency (render the frames as soon as possible).
       params.video_sample_info.max_video_capabilities = "streaming=1";
     }
+
+    std::optional<EncryptionScheme> encryption_scheme =
+        video_decoder_->GetEncryptionScheme();
+    if (encryption_scheme.has_value() &&
+        *encryption_scheme != EncryptionScheme::kUnencrypted) {
+      has_drm = true;
+    }
   }
+
+  if (has_drm) {
+    params.drm_system = StarboardDrmWrapper::GetInstance().GetDrmSystem();
+  } else {
+    params.drm_system = nullptr;
+  }
+
   params.output_mode = kStarboardPlayerOutputModePunchOut;
   player_ =
       starboard_->CreatePlayer(&params,
