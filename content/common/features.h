a9bfe2e6ea059fc994f686f1d8ef0dfb6cd32151
Vasiliy Telezhnikov <vasilyt@chromium.org>
2025-05-07 08:46:01 -0700

    Don't create RasterInterface with software compositor
    It's impossible to create RasterDecoder without skia being initialized
    on the service side and we don't initialize it when we run with software
    compositor.
    
    When this happens current code falls back to Raster-over-gles which is
    not supported to run without gpu compositing either.
    
    This CL disallows creation of RasterInterface in this case under
    kill-switch.
    
    Note, this CL is no-op, the flag is flipped in a separate CL for easier
    revert if needed.
    
    Bug: 391648152
    Change-Id: I10e21724d1533c6c4206a6c5dd9f1a64fc1ea025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512430
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457023}

--- content/common/features.h
+++ content/common/features.h
@@ -155,6 +155,8 @@ CONTENT_EXPORT BASE_DECLARE_FEATURE(kWebOTPAssertionFeaturePolicy);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kWebUIInProcessResourceLoading);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kLimitCrossOriginNonActivatedPaintHolding);
 
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kDisallowRasterInterfaceWithoutSkiaBackend);
+
 // Please keep features in alphabetical order.
 
 }  // namespace features
