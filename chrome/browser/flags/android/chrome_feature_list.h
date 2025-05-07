b1888cdcd57e4adc2618ce011c672e77b015999a
fikremen <fikremengistu@chromium.org>
2025-05-07 16:13:47 -0700

    Remove base::Feature::AllowNewIncognitoTabIntents
    [Code Health]
    
    Bug: crbug.com/356235714
    Change-Id: I8a86ae7f1f5079d8441daae84dd6f31704c7bb93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512809
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Fikre Mengistu <fikremengistu@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457319}

--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -17,7 +17,6 @@ namespace android {
 // Alphabetical:
 BASE_DECLARE_FEATURE(kAdaptiveButtonInTopToolbarCustomizationV2);
 BASE_DECLARE_FEATURE(kAdaptiveButtonInTopToolbarPageSummary);
-BASE_DECLARE_FEATURE(kAllowNewIncognitoTabIntents);
 BASE_DECLARE_FEATURE(kAllowTabClosingUponMinimization);
 BASE_DECLARE_FEATURE(kAndroidAppIntegration);
 BASE_DECLARE_FEATURE(kAndroidAppIntegrationV2);
