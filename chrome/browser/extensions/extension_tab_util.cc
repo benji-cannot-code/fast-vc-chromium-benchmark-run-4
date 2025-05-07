53d6221d3a88999b3e39b5e49659e0e775f5f07c
Emily Shack <emshack@chromium.org>
2025-05-07 11:17:27 -0700

    Revert "[SxS] Add bookmark manager context menu item"
    This reverts commit 98b514edaa31f354b5f6823f2ac9e51966f06626.
    
    Reason for revert: Root cause for https://g-issues.chromium.org/issues/415425829
    
    Bug: 403351309
    Original change's description:
    > [SxS] Add bookmark manager context menu item
    >
    > Adds an "Open in split view" menu item to the bookmarks manager when
    > split view is enabled.
    >
    > Bug: 403351309
    > Change-Id: I2f83d8ea3ac64e94a97c005b37065c3de7775b57
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477265
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Emily Shack <emshack@chromium.org>
    > Auto-Submit: Emily Shack <emshack@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1454774}
    
    Bug: 403351309
    Change-Id: I3ca787d38d1720f2a7b9bd9a69b8c03f5e977966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519054
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457137}

--- chrome/browser/extensions/extension_tab_util.cc
+++ chrome/browser/extensions/extension_tab_util.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/window_controller_list.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/tabs/split_tab_visual_data.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
@@ -298,10 +297,6 @@ base::expected<base::Value::Dict, std::string> ExtensionTabUtil::OpenTab(
   // will override this default.
   bool active = params.active.value_or(true);
 
-  // Default to unsplit for the new tab. The presence of the 'split' property
-  // will override this default.
-  bool split = params.split.value_or(false);
-
   // Default to not pinning the tab. Setting the 'pinned' property to true
   // will override this default.
   bool pinned = params.pinned.value_or(false);
@@ -379,11 +374,6 @@ base::expected<base::Value::Dict, std::string> ExtensionTabUtil::OpenTab(
 
   if (active)
     navigate_params.navigated_or_inserted_contents->SetInitialFocus();
-
-  if (split) {
-    tab_strip->AddToNewSplit({new_index},
-                             split_tabs::SplitTabLayout::kVertical);
-  }
 
   ExtensionTabUtil::ScrubTabBehavior scrub_tab_behavior =
       ExtensionTabUtil::GetScrubTabBehavior(
--- chrome/browser/resources/bookmarks/bookmark_manager_api_proxy.ts
+++ chrome/browser/resources/bookmarks/bookmark_manager_api_proxy.ts
@@ -4,11 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 import type {ChromeEvent} from '/tools/typescript/definitions/chrome_event.js';
 
-export interface OpenInNewTabParams {
-  active?: boolean;
-  split?: boolean;
-}
-
 export interface BookmarkManagerApiProxy {
   onDragEnter:
       ChromeEvent<(p1: chrome.bookmarkManagerPrivate.DragData) => void>;
@@ -20,7 +15,7 @@ export interface BookmarkManagerApiProxy {
   removeTrees(idList: string[]): Promise<void>;
   canPaste(parentId: string): Promise<boolean>;
   openInNewWindow(idList: string[], incognito: boolean): void;
-  openInNewTab(id: string, params?: OpenInNewTabParams): void;
+  openInNewTab(id: string, active: boolean): void;
   openInNewTabGroup(idList: string[]): void;
   cut(idList: string[]): Promise<void>;
   paste(parentId: string, selectedIdList?: string[]): Promise<void>;
@@ -50,12 +45,11 @@ export class BookmarkManagerApiProxyImpl implements BookmarkManagerApiProxy {
   }
 
   openInNewWindow(idList: string[], incognito: boolean) {
-    chrome.bookmarkManagerPrivate.openInNewWindow(idList, incognito);
+    return chrome.bookmarkManagerPrivate.openInNewWindow(idList, incognito);
   }
 
-  openInNewTab(id: string, params: OpenInNewTabParams) {
-    chrome.bookmarkManagerPrivate.openInNewTab(
-        id, {active: params.active, split: params.split});
+  openInNewTab(id: string, active: boolean) {
+    return chrome.bookmarkManagerPrivate.openInNewTab(id, active);
   }
 
   openInNewTabGroup(idList: string[]) {
