a47b2981743aff58962bced511a9cb91fbd7e51f
Brandon Fong <bjfong@google.com>
2025-05-07 11:42:43 -0700

    [declutter] Combine tabModel and sync service observers into one
    Combine observers on the tabModel tabCountSupplier and sync service into
    one observable supplier to monitor total tab count updates.
    
    Bug: 416017248
    Change-Id: I642f76cfee77dfc56f305a91c242403df27c0efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6515300
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457160}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestrator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestrator.java
@@ -20,7 +20,6 @@ import org.chromium.base.ObserverList;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.lifetime.Destroyable;
 import org.chromium.base.supplier.ObservableSupplier;
-import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
@@ -36,6 +35,7 @@ import org.chromium.chrome.browser.tab.TabArchiverImpl;
 import org.chromium.chrome.browser.tab.tab_restore.HistoricalTabModelObserver;
 import org.chromium.chrome.browser.tab_group_sync.TabGroupSyncServiceFactory;
 import org.chromium.chrome.browser.tab_ui.TabContentManager;
+import org.chromium.chrome.browser.tabmodel.ArchivedTabCountSupplier;
 import org.chromium.chrome.browser.tabmodel.ArchivedTabCreator;
 import org.chromium.chrome.browser.tabmodel.ArchivedTabModelSelectorHolder;
 import org.chromium.chrome.browser.tabmodel.ArchivedTabModelSelectorImpl;
@@ -133,9 +133,6 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
     private final AsyncTabParamsManager mAsyncTabParamsManager;
     private final ObserverList<Observer> mObservers = new ObserverList<>();
     private final TabWindowManager mTabWindowManager;
-    private final ObservableSupplierImpl<Integer> mTabCountSupplier =
-            new ObservableSupplierImpl<>();
-    private final Callback<Integer> mTabCountSupplierObserver = mTabCountSupplier::set;
     // The set of {@link TabModelOrchestrators}  which have registered themselves as active for
     // declutter.
     private final List<TabbedModeTabModelOrchestrator> mActivityTabModelOrchestrators =
@@ -151,9 +148,10 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
     private boolean mRestoreTabsCalled;
     private boolean mRescueTabsCalled;
     private CallbackController mCallbackController = new CallbackController();
-    private ObservableSupplier<Integer> mUnderlyingTabCountSupplier;
     private @Nullable HistoricalTabModelObserver mHistoricalTabModelObserver;
     private boolean mTriggerAutodeleteAfterDataCreated;
+    private @Nullable TabGroupSyncService mTabGroupSyncService;
+    private ArchivedTabCountSupplier mArchivedTabCountSupplier;
 
     /**
      * Returns the ArchivedTabModelOrchestrator that corresponds to the given profile. Must be
@@ -241,10 +239,6 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
             mTabWindowManager.setArchivedTabModelSelector(null);
         }
 
-        if (mUnderlyingTabCountSupplier != null) {
-            mUnderlyingTabCountSupplier.removeObserver(mTabCountSupplierObserver);
-        }
-
         if (mHistoricalTabModelObserver != null) {
             mHistoricalTabModelObserver.destroy();
             mHistoricalTabModelObserver = null;
@@ -253,6 +247,11 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
         if (mTabArchiver != null) {
             mTabArchiver.destroy();
             mTabArchiver = null;
+        }
+
+        if (mArchivedTabCountSupplier != null) {
+            mArchivedTabCountSupplier.destroy();
+            mArchivedTabCountSupplier = null;
         }
 
         super.destroy();
@@ -301,7 +300,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
 
     /** Returns a supplier for the archive tab count. */
     public ObservableSupplier<Integer> getTabCountSupplier() {
-        return mTabCountSupplier;
+        return mArchivedTabCountSupplier;
     }
 
     public TabModel getTabModel() {
@@ -389,9 +388,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
             observer.onTabModelCreated(model);
         }
 
-        mUnderlyingTabCountSupplier = model.getTabCountSupplier();
-        mTabCountSupplier.set(mUnderlyingTabCountSupplier.get());
-        mUnderlyingTabCountSupplier.addObserver(mTabCountSupplierObserver);
+        mArchivedTabCountSupplier = new ArchivedTabCountSupplier(model, mTabGroupSyncService);
 
         mHistoricalTabModelObserver =
                 new HistoricalTabModelObserver(
@@ -506,8 +503,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
 
         mTabArchiveSettings = new TabArchiveSettings(ChromeSharedPreferences.getInstance());
         mTabArchiveSettings.addObserver(mTabArchiveSettingsObserver);
-        TabGroupSyncService tabGroupSyncService =
-                TabGroupSyncServiceFactory.getForProfile(mProfile);
+        mTabGroupSyncService = TabGroupSyncServiceFactory.getForProfile(mProfile);
         mTabArchiver =
                 new TabArchiverImpl(
                         mTabModelSelector
@@ -516,7 +512,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
                         mArchivedTabCreator,
                         mTabArchiveSettings,
                         System::currentTimeMillis,
-                        tabGroupSyncService);
+                        mTabGroupSyncService);
         mTabArchiver.addObserver(mTabArchiverObserver);
     }
 
