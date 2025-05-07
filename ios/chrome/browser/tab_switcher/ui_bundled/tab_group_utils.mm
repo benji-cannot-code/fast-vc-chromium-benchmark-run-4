b105b992a036d71023cc149be29c022a51acb65c
Ewann Pelle <ewannpv@chromium.org>
2025-05-07 05:20:50 -0700

    [iOS][2/2] Fetches favicons in group grid
    Demo:
    https://drive.google.com/file/d/1422_0_OQJT6ESCR09VNeIciGA8wPWHXD/view?usp=sharing&resourcekey=0-bpR56WqmTcW-brY706BBVA
    
    Bug: 400966281
    Change-Id: Ic5e0f0fbaa6682a112a60f778b0ffb3c5f6864f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512838
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456891}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_group_utils.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_group_utils.mm
@@ -14,11 +14,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/snapshots/model/snapshot_tab_helper.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/group_tab_info.h"
+#import "ios/chrome/common/ui/favicon/favicon_attributes.h"
 #import "ios/web/public/web_state.h"
 
 namespace {
+
+// Size for the default favicon.
 const CGFloat kFaviconSize = 16;
-}
+// The minimum size of tab favicons.
+constexpr CGFloat kFaviconMinimumSize = 8.0;
+
+}  // namespace
 
 @implementation TabGroupUtils
 
@@ -83,8 +89,21 @@ const CGFloat kFaviconSize = 16;
     completion(groupTabInfo);
     return;
   }
-  // TODO(crbug.com/400966281): Fetch favicon on Google server.
-  completion(groupTabInfo);
+
+  // Asynchronously fetch the favicon.
+  faviconLoader->FaviconForPageUrl(
+      webState->GetVisibleURL(), kFaviconSize, kFaviconMinimumSize,
+      /*fallback_to_google_server=*/true, ^(FaviconAttributes* attributes) {
+        // Synchronously returned default favicon.
+        if (attributes.usesDefaultImage) {
+          return;
+        }
+        // Asynchronously returned favicon.
+        if (attributes.faviconImage) {
+          groupTabInfo.favicon = attributes.faviconImage;
+        }
+        completion(groupTabInfo);
+      });
 }
 
 @end
