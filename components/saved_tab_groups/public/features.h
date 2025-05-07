bef69d09562b0debee0de2cbc21678fea0d44807
David Pennington <dpenning@chromium.org>
2025-05-07 09:39:45 -0700

    [TabGroupInteractions] Open editor with left click
    Experiment with changing the default behavior for tab group headers
    mouse interactions.
    
    When the feature is enabled, then left clicking will open the
    TGEBV, and right click will collapse/expand.
    
    Bug: 404570385
    Change-Id: Iaf17f3e4635f94ded1f8cb13b76d83904c1e4f16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6499314
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457062}

--- components/saved_tab_groups/public/features.h
+++ components/saved_tab_groups/public/features.h
@@ -33,6 +33,8 @@ BASE_DECLARE_FEATURE(kEnableUrlRestriction);
 
 BASE_DECLARE_FEATURE(kEnableOriginatingSavedGroupCleanUp);
 
+BASE_DECLARE_FEATURE(kLeftClickOpensTabGroupBubble);
+
 extern bool IsTabGroupSyncServiceDesktopMigrationEnabled();
 
 extern bool IsTabGroupSyncDelegateAndroidEnabled();
