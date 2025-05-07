0f35a8181c5ebc3e326b1968b90d316869d18529
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 05:46:54 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/send_tab_to_self.
    
    Bug: 409299519
    Change-Id: Iee62bb0b57609f0c7aebe806834042e5c5a87918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6515316
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456904}

--- ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.mm
+++ ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.mm
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state.h"
 
 SendTabToSelfBrowserAgent::SendTabToSelfBrowserAgent(Browser* browser)
-    : browser_(browser),
+    : BrowserUserData(browser),
+      browser_(browser),
       model_(
           SendTabToSelfSyncServiceFactory::GetForProfile(browser_->GetProfile())
               ->GetSendTabToSelfModel()) {
