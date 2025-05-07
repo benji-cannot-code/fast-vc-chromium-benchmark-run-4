985b58af6290191cc677e3abb90ae57044975b10
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 05:49:30 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/policy.
    
    Bug: 409299519
    Change-Id: Ie3f6f9f5f32aaa327114ea95c461e7980ef78c54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516179
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456909}

--- ios/chrome/browser/policy/model/policy_watcher_browser_agent.mm
+++ ios/chrome/browser/policy/model/policy_watcher_browser_agent.mm
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/thread/web_task_traits.h"
 
 PolicyWatcherBrowserAgent::PolicyWatcherBrowserAgent(Browser* browser)
-    : browser_(browser) {
+    : BrowserUserData(browser), browser_(browser) {
   DCHECK(!browser->GetProfile()->IsOffTheRecord());
   prefs_change_observer_.Init(GetApplicationContext()->GetLocalState());
   profile_prefs_change_observer_.Init(browser->GetProfile()->GetPrefs());
