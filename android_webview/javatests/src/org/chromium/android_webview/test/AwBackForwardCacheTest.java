bfa608d2cd7ddbe174a41bda7179c0eae055bb94
Ayu Ishii <ayui@chromium.org>
2025-05-07 12:08:36 -0700

    Reland "WebSQL: Remove WebPreference"
    This reverts commit fbd39d07c1deaf2ec4a8ea32260503369d10bdbb.
    
    Reason for revert: Fixes compile failure in chromecast
    
    Bug: 395838064
    Original change's description:
    > Revert "WebSQL: Remove WebPreference"
    >
    > This reverts commit 2b24afca3ee194959e8b2d2676b0f42693617d92.
    >
    > Reason for revert: caused tree close due to the databases_enabled removal.
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-cast-arm-rel/25286/overview
    >
    > Bug: 395838064
    > Original change's description:
    > > WebSQL: Remove WebPreference
    > >
    > > This change removes WebPreferences used for WebSettings.
    > > WebSQL is deprecated and removed, and this setting
    > > is now a no-op.
    > >
    > > Bug: 395838064
    > > Change-Id: I3eb0dfaf55d7c4ebee663a21218e80dfe06d3def
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6473313
    > > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Commit-Queue: Ayu Ishii <ayui@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1450946}
    >
    > Bug: 395838064
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ib6a6c45b9f82960e990c76a05cba6ca46ad2fbc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6487088
    > Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@google.com>
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Owners-Override: Yoshisato Yanagisawa <yyanagisawa@google.com>
    > Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1450947}
    
    Bug: 395838064
    Change-Id: Ib2994ff5cbd9bcbc9bb20687566194c2ecdfa975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6488755
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457178}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwBackForwardCacheTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwBackForwardCacheTest.java
@@ -517,8 +517,6 @@ public class AwBackForwardCacheTest extends AwParameterizedTest {
                 () -> settings.setForceZeroLayoutHeight(!settings.getForceZeroLayoutHeight()));
         verifyPageEvictedWithSettingsChange(
                 () -> settings.setDomStorageEnabled(!settings.getDomStorageEnabled()));
-        verifyPageEvictedWithSettingsChange(
-                () -> settings.setDatabaseEnabled(!settings.getDatabaseEnabled()));
         verifyPageEvictedWithSettingsChange(() -> settings.setDefaultTextEncodingName("Latin-1"));
         verifyPageEvictedWithSettingsChange(
                 () -> {
