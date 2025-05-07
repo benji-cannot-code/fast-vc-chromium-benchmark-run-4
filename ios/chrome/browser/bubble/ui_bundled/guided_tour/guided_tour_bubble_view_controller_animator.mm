d061bda439d945ee58942e863c0821cdf690787c
Chris Lu <thegreenfrog@google.com>
2025-05-07 14:39:19 -0700

    [ios] Implement GuidedTourCoordinator
    GuidedTourCoordinator will manage the lifecycle of presenting a
    BubbleView with a dimmed background that has a cutout to highlight
    the view to which the BubbleView is anchored.
    
    GuidedTourBubbleViewControllerPresenter is a subclass implementation
    of BubbleViewControllerPresenter to present the BubbleViewController
    in front of a background view. BubbleViewControllerPresenter's
    -presentInViewController:(UIViewController*)parentViewController
    anchorPoint:(CGPoint)anchorPoint anchorViewFrame: is overridden
    and refactored to reuse as much shared logic as possible while
    separating the presentation logic.
    
    The GuidedTourStepNTP is implemented as a working example of the new
    classes.
    
    Screenshot (ignore the fact that the tabgrid button is blue): https://drive.google.com/file/d/1tT0csBu_Q_-5sdcRYW_zZ7iDxaOj8lDY/view?usp=sharing
    
    Change-Id: I806c4c960f738e97702b8c66c8583e0d681d3509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6505803
    Auto-Submit: Chris Lu <thegreenfrog@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457282}

---
+++ ios/chrome/browser/bubble/ui_bundled/guided_tour/guided_tour_bubble_view_controller_animator.mm
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/bubble/ui_bundled/guided_tour/guided_tour_bubble_view_controller_animator.h"
+
+@implementation GuidedTourBubbleViewControllerAnimator
+
+#pragma mark - UIViewControllerAnimatedTransitioning
+
+- (void)animateTransition:
+    (id<UIViewControllerContextTransitioning>)transitionContext {
+  UIViewController* presentedViewController = [transitionContext
+      viewControllerForKey:self.appearing
+                               ? UITransitionContextToViewControllerKey
+                               : UITransitionContextFromViewControllerKey];
+  UIView* presentedView = [transitionContext
+      viewForKey:self.appearing ? UITransitionContextToViewKey
+                                : UITransitionContextFromViewKey];
+
+  UIView* containerView = [transitionContext containerView];
+  if (self.appearing) {
+    [containerView addSubview:presentedView];
+    presentedView.frame =
+        [transitionContext finalFrameForViewController:presentedViewController];
+  }
+
+  if (self.appearing) {
+    presentedView.alpha = 0;
+  }
+
+  [UIView animateWithDuration:.5
+      delay:0
+      usingSpringWithDamping:.85
+      initialSpringVelocity:0
+      options:0
+      animations:^{
+        presentedView.alpha = self.appearing ? 1 : 0;
+      }
+      completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+      }];
+}
+
+- (NSTimeInterval)transitionDuration:
+    (id<UIViewControllerContextTransitioning>)transitionContext {
+  return 0.5;
+}
+
+@end
