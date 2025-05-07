43c479a61a003fd6551f39ec3a25656172eb043f
Jonathan Lee <jonathanjlee@google.com>
2025-05-07 08:39:26 -0700

    [web-tests] Add fast path for WPT in `Port.reference_files(...)`
    The manifest already identifies all references, so WPTs can skip the
    search for non-WPT references (`-expected(-mismatch).*`). The search
    algorithm is slow because it brute-forces all possible test extensions
    on the filesystem.
    
    This speeds up the task construction step of `optimize-baselines --all`
    from ~96s to ~38s. More improvements to follow.
    
    Bug: 411245983
    Change-Id: I3629319abcc3115ebb1ae940a53c15b43c99cc26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516399
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457017}

--- third_party/blink/tools/blinkpy/web_tests/port/base.py
+++ third_party/blink/tools/blinkpy/web_tests/port/base.py
@@ -74,6 +74,7 @@ from blinkpy.common.system.path import abspath_to_uri
 from blinkpy.w3c.wpt_manifest import (
     FuzzyRange,
     FuzzyParameters,
+    Relation,
     WPTManifest,
     MANIFEST_NAME,
 )
@@ -1080,8 +1081,10 @@ class Port(object):
                 return True
         return False
 
-    def reference_files(self, test_name):
+    def reference_files(self, test_name: str) -> list[tuple[Relation, str]]:
         """Returns a list of expectation (== or !=) and filename pairs"""
+        if match := self.WPT_REGEX.match(test_name):
+            return self._wpt_references_files(match.group(1), match.group(2))
 
         # Try to find -expected.* or -expected-mismatch.* in the same directory.
         reftest_list = []
@@ -1092,15 +1095,12 @@ class Port(object):
                                               match=(expectation == '=='))
                 if self._filesystem.exists(path):
                     reftest_list.append((expectation, path))
-        if reftest_list:
-            return reftest_list
+        return reftest_list
 
+    def _wpt_references_files(self, wpt_path: str,
+                              path_in_wpt: str) -> list[tuple[Relation, str]]:
         # Try to extract information from MANIFEST.json.
-        match = self.WPT_REGEX.match(test_name)
-        if not match:
-            return []
-        wpt_path = match.group(1)
-        path_in_wpt = match.group(2)
+        reftest_list = []
         for expectation, ref_path_in_wpt in self.wpt_manifest(
                 wpt_path).extract_reference_list(path_in_wpt):
             if ref_path_in_wpt.startswith('about:'):
