cb17e510a0bc071cec1656f9b5cefa6b342b9438
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 06:00:06 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/web.
    
    Bug: 409299519
    Change-Id: I746ad849e66ed23d4de71f92a00ee809186bdb08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516089
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456927}

--- ios/chrome/browser/web/model/web_navigation_browser_agent.mm
+++ ios/chrome/browser/web/model/web_navigation_browser_agent.mm
@@ -23,7 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state.h"
 
 WebNavigationBrowserAgent::WebNavigationBrowserAgent(Browser* browser)
-    : web_state_list_(browser->GetWebStateList()), browser_(browser) {}
+    : BrowserUserData(browser),
+      web_state_list_(browser->GetWebStateList()),
+      browser_(browser) {}
 
 WebNavigationBrowserAgent::~WebNavigationBrowserAgent() {}
 
