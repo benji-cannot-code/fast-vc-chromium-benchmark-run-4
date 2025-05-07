4d2100c5b2b26e4489110dc1155333b635a09ecb
Ryan Sultanem <rsult@google.com>
2025-05-07 10:07:04 -0700

    [BatchUpload] Expose a function to get local data for all available types
    This allows UIs to directly query the BatchUploadService with all the
    currently active types.
    
    For the bookmarks entry point, this will be needed to know whether
    there are "other items" or not, which will impact the string of the
    description mentioning "other items" or not.
    The string and logic for that will be introduced in the followup CL.
    
    This function will also be used when adding the generic/globacl
    batch upload entry point, as it will need to query all available data
    types to know whether we should show the entry point or not.
    
    Bug: 411439295
    Change-Id: I8af280ce18f11c9343b99c876fbe8944bcdc641f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513438
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457083}

--- chrome/browser/profiles/batch_upload/batch_upload_service.cc
+++ chrome/browser/profiles/batch_upload/batch_upload_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/containers/to_vector.h"
 #include "base/functional/bind.h"
+#include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
 #include "base/notreached.h"
 #include "base/strings/to_string.h"
@@ -170,20 +171,23 @@ void BatchUploadService::OpenBatchUpload(
   state_.dialog_state_->entry_point_ = entry_point;
   state_.dialog_state_->dialog_shown_callback_ = std::move(success_callback);
 
-  RequestLocalDataDescriptions();
+  GetLocalDataDescriptionsForAvailableTypes(
+      base::BindOnce(&BatchUploadService::OnGetLocalDataDescriptionsReady,
+                     base::Unretained(this)));
 }
 
-void BatchUploadService::RequestLocalDataDescriptions() {
+void BatchUploadService::GetLocalDataDescriptionsForAvailableTypes(
+    base::OnceCallback<
+        void(std::map<syncer::DataType, syncer::LocalDataDescription>)>
+        result_callback) {
   syncer::DataTypeSet data_types;
   // Iterate over all available enums.
   for (syncer::DataType type : kBatchUploadAvailableTypesOrder) {
     data_types.Put(type);
   }
 
-  sync_service_->GetLocalDataDescriptions(
-      data_types,
-      base::BindOnce(&BatchUploadService::OnGetLocalDataDescriptionsReady,
-                     base::Unretained(this)));
+  sync_service_->GetLocalDataDescriptions(data_types,
+                                          std::move(result_callback));
 }
 
 void BatchUploadService::OnGetLocalDataDescriptionsReady(
