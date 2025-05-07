d11e98cdc4fe46b34dd3674afd49775a2aaf98c5
Dale Curtis <dalecurtis@chromium.org>
2025-05-07 10:33:34 -0700

    Add some CHECKs to track down issues with MediaRecorder encoder
    It's unclear how a nullptr exception appears to be occuring during
    encoder initialization (almost entirely on Android). Ensure some of
    the most likely cases aren't actually nullptr.
    
    R=lowkey
    
    Bug: 411694702
    Change-Id: Ia0221c21d07271d519233b29b6eca1c57ce67bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512120
    Commit-Queue: Syed AbuTalib <lowkey@google.com>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457103}

--- third_party/blink/renderer/modules/mediarecorder/video_track_recorder.cc
+++ third_party/blink/renderer/modules/mediarecorder/video_track_recorder.cc
@@ -857,6 +857,7 @@ VideoTrackRecorderImpl::VideoTrackRecorderImpl(
                              WrapPersistent(callback_interface)))),
       frame_buffer_pool_limit_(frame_buffer_pool_limit) {
   TRACE_EVENT("media", "VideoTrackRecorderImpl::VideoTrackRecorderImpl");
+  CHECK(main_thread_task_runner_);
   DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   DCHECK(track_);
   DCHECK(track_->GetSourceType() == MediaStreamSource::kTypeVideo);
@@ -906,6 +907,7 @@ void VideoTrackRecorderImpl::OnVideoFrame(
     base::TimeTicks capture_time) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   TRACE_EVENT("media", "VideoTrackRecorderImpl::OnVideoFrame");
+  CHECK(video_frame);
 
   // Measure how common video transformation changes are, crbug.com/391786486.
   auto frame_transformation = GetFrameTransformation(video_frame);
@@ -937,6 +939,7 @@ void VideoTrackRecorderImpl::ProcessOneVideoFrame(
     scoped_refptr<media::VideoFrame> video_frame,
     base::TimeTicks capture_time) {
   TRACE_EVENT("media", "VideoTrackRecorderImpl::ProcessOneVideoFrame");
+  CHECK(video_frame);
   if (!encoder_) {
     InitializeEncoder(bits_per_second_, allow_vea_encoder,
                       video_frame->storage_type(),
@@ -1046,6 +1049,7 @@ void VideoTrackRecorderImpl::InitializeEncoder(
   const bool create_vea_encoder = allow_vea_encoder && can_use_vea;
   auto encoding_task_runner =
       base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()});
+  CHECK(encoding_task_runner);
   auto encoder = CreateMediaVideoEncoder(encoding_task_runner, codec_profile,
                                          is_screencast, create_vea_encoder);
 
@@ -1102,6 +1106,7 @@ VideoTrackRecorderPassthrough::VideoTrackRecorderPassthrough(
                          callback_interface),
       track_(track),
       key_frame_processor_(key_frame_config) {
+  CHECK(main_thread_task_runner_);
   DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   // HandleEncodedVideoFrame() will be called on Render Main thread.
   // Note: Adding an encoded sink internally generates a new key frame
