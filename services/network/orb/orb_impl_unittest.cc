8b6f99a413e88c9dd6eec67540c37184fc950a34
Maks Orlovich <morlovich@chromium.org>
2025-05-07 09:12:03 -0700

    Cleanup more unsafe_buffers in services/network tests
    Bug: 351564777
    Change-Id: Id25b8cc5c7406f16b71f26d286b2f252a51eb3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512664
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457050}

--- services/network/orb/orb_impl_unittest.cc
+++ services/network/orb/orb_impl_unittest.cc
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/to_string.h"
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
-#pragma allow_unsafe_libc_calls
-#endif
-
 // This file contains the ResponseAnalyzerTests (which test the response
 // analyzer's behavior in several parameterized test scenarios) and at the end
 // includes the CrossOriginReadBlockingTests, which are more typical unittests.
