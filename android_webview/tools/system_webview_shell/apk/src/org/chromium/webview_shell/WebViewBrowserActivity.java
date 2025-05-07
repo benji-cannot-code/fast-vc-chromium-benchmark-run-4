21c380549b948140449df65b893dd88815ebf51e
Torne (Richard Coles) <torne@google.com>
2025-05-07 12:18:51 -0700

    webview: try to prevent tracing crash in shell.
    In monkey tests it's possible for menu items in the shell to be
    activated multiple times in quick succession, which has caused crashes
    in the past due to not tracking the state of tracing correctly. This was
    still crashing even after previous fixes, so a few more fixes that may
    help:
    
    - Closing the stream happens on a background thread, and was directly
      updating the tracing state flag that's otherwise accessed on the UI
      thread. Move this flag update to happen on the UI thread to avoid
      races.
    
    - The flag was being set *after* calling TracingController.stop, so
      could have gotten out of sync if the close happened very quickly. Set
      it before making the call instead.
    
    Bug: 352405043
    Change-Id: I8a946beaa68c92af7d81d0908d6d66d5d90949ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519845
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457185}

--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -233,10 +233,10 @@ public class WebViewBrowserActivity extends AppCompatActivity {
                     try (StrictModeContext ignored = StrictModeContext.allowDiskWrites()) {
                         String outFileName = getFilesDir() + "/webview_tracing.json";
                         try {
+                            mIsStoppingTracing = true;
                             tracingController.stop(
                                     new TracingLogger(outFileName, this),
                                     Executors.newSingleThreadExecutor());
-                            mIsStoppingTracing = true;
                         } catch (FileNotFoundException e) {
                             throw new RuntimeException(e);
                         }
@@ -423,19 +423,16 @@ public class WebViewBrowserActivity extends AppCompatActivity {
         @Override
         public void close() throws IOException {
             super.close();
-            showDialog(mByteCount);
-            mIsStoppingTracing = false;
-        }
 
-        private void showDialog(long nbBytes) {
             StringBuilder info = new StringBuilder();
             info.append("Tracing data written to file\n");
-            info.append("number of bytes: " + nbBytes);
+            info.append("number of bytes: " + mByteCount);
 
             mActivity.runOnUiThread(
                     new Runnable() {
                         @Override
                         public void run() {
+                            mIsStoppingTracing = false;
                             AlertDialog dialog =
                                     new AlertDialog.Builder(mActivity)
                                             .setTitle("Tracing API")
