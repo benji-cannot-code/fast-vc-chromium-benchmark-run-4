b1afbfd4ffff76496717759d247a1e3b5683be14
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 05:47:06 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/omnibox.
    
    Bug: 409299519
    Change-Id: I9e9dfdead8464daa1d4cc7648e19b8585b0e020f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513495
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456905}

--- ios/chrome/browser/omnibox/model/omnibox_position/omnibox_position_browser_agent.mm
+++ ios/chrome/browser/omnibox/model/omnibox_position/omnibox_position_browser_agent.mm
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/omnibox/model/omnibox_position/omnibox_position_browser_agent.h"
 
-OmniboxPositionBrowserAgent::OmniboxPositionBrowserAgent(Browser* browser) {}
+OmniboxPositionBrowserAgent::OmniboxPositionBrowserAgent(Browser* browser)
+    : BrowserUserData(browser) {}
 
 OmniboxPositionBrowserAgent::~OmniboxPositionBrowserAgent() = default;
 
