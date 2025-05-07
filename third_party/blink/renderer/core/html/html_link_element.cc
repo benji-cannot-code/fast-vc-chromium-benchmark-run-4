89d6dac5bd01d3b3958fa6fc050b38221552b1f3
Nancy Li <nancyly@chromium.org>
2025-05-07 03:42:02 -0700

    Add trace event for <link rel='preconnect'> element
    Manual test screenshot:
    
    https: //screenshot.googleplex.com/AshygeCVfnBFbT8
    Bug: 412974198
    Change-Id: If8168e72a5cb1f33cab566bbcecbcb5e370035e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469773
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Nancy Li <nancyly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456854}

--- third_party/blink/renderer/core/html/html_link_element.cc
+++ third_party/blink/renderer/core/html/html_link_element.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/numerics/safe_conversions.h"
+#include "base/trace_event/typed_macros.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_icon_sizes_parser.h"
 #include "third_party/blink/public/platform/web_prescient_networking.h"
@@ -102,6 +103,20 @@ void HTMLLinkElement::ParseAttribute(
         GetDocument().IsInOutermostMainFrame()) {
       UseCounter::Count(&GetDocument(), WebFeature::kLinkRelFacilitatedPayment);
       MaybeHandlePaymentLink();
+    }
+    if (rel_attribute_.IsPreconnect()) {
+      TRACE_EVENT_INSTANT(
+          TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "LinkPreconnect",
+          "data", [&](perfetto::TracedValue context) {
+            auto dict = std::move(context).WriteDictionary();
+            if (GetDocument().GetFrame()) {
+              dict.Add("frame",
+                       GetDocument().GetFrame()->GetFrameIdForTracing());
+            }
+            dict.Add("node_id", GetDomNodeId());
+            const KURL& url = GetNonEmptyURLAttribute(html_names::kHrefAttr);
+            dict.Add("url", url.GetString());
+          });
     }
     rel_list_->DidUpdateAttributeValue(params.old_value, value);
     Process();
