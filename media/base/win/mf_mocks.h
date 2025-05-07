892931df1ba6c0603af123098792b3de0133e20e
Piet Schouten <Piet.Schouten@microsoft.com>
2025-05-07 13:38:55 -0700

    Add support for querying hardware PlayReady CDM capabilities.
    See the following change for how this will work with the full PlayReady
    implementation.
    https://chromium-review.googlesource.com/c/chromium/src/+/6092020
    
    Bug: 378869813
    Change-Id: Iae3cf88abe12749cabe49de0b9a0966649585b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6408454
    Commit-Queue: Piet Schouten <Piet.Schouten@microsoft.com>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Sangbaek Park <sangbaekpark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457240}

--- media/base/win/mf_mocks.h
+++ media/base/win/mf_mocks.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_BASE_WIN_MF_MOCKS_H_
 
 #include <mfcontentdecryptionmodule.h>
+#include <mfmediaengine.h>
 #include <wrl/client.h>
 #include <wrl/implements.h>
 
@@ -57,6 +58,9 @@ class MockMFCdm
   MockMFCdm();
   ~MockMFCdm() override;
 
+  // IUnknown method
+  MOCK_STDCALL_METHOD2(QueryInterface, HRESULT(REFIID riid, void** ppv));
+
   // IMFContentDecryptionModule methods
   MOCK_STDCALL_METHOD2(SetContentEnabler,
                        HRESULT(IMFContentEnabler* content_enabler,
@@ -102,6 +106,68 @@ class MockMFCdmSession
                        HRESULT(const BYTE* response, DWORD response_size));
   MOCK_STDCALL_METHOD0(Close, HRESULT());
   MOCK_STDCALL_METHOD0(Remove, HRESULT());
+};
+
+class MockMFExtendedDRMTypeSupport
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IMFExtendedDRMTypeSupport> {
+ public:
+  MockMFExtendedDRMTypeSupport();
+  ~MockMFExtendedDRMTypeSupport() override;
+
+  // IMFExtendedDRMTypeSupport methods
+  MOCK_STDCALL_METHOD3(IsTypeSupportedEx,
+                       HRESULT(BSTR type,
+                               BSTR keySystem,
+                               MF_MEDIA_ENGINE_CANPLAY* pAnswer));
+};
+
+class MockMFGetService
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IMFGetService> {
+ public:
+  MockMFGetService();
+  ~MockMFGetService() override;
+
+  // IMFGetService methods
+  MOCK_STDCALL_METHOD3(GetService,
+                       HRESULT(REFGUID guidService,
+                               REFIID riid,
+                               LPVOID* ppvObject));
+};
+
+class MockMFPMPHost
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IMFPMPHost> {
+ public:
+  MockMFPMPHost();
+  ~MockMFPMPHost() override;
+
+  // IMFPMPHost methods
+  MOCK_STDCALL_METHOD0(LockProcess, HRESULT());
+  MOCK_STDCALL_METHOD0(UnlockProcess, HRESULT());
+  MOCK_STDCALL_METHOD4(
+      CreateObjectByCLSID,
+      HRESULT(REFCLSID clsid, IStream* pStream, REFIID riid, void** ppv));
+};
+
+class MockMFPMPHostApp
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IMFPMPHostApp> {
+ public:
+  MockMFPMPHostApp();
+  ~MockMFPMPHostApp() override;
+
+  // IMFPMPHostApp methods
+  MOCK_STDCALL_METHOD0(LockProcess, HRESULT());
+  MOCK_STDCALL_METHOD0(UnlockProcess, HRESULT());
+  MOCK_STDCALL_METHOD4(
+      ActivateClassById,
+      HRESULT(LPCWSTR id, IStream* pStream, REFIID riid, void** ppv));
 };
 
 }  // namespace media
