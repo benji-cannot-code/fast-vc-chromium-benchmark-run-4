457ff02ceebfa4fc74c882b925124c953f6330ac
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 06:01:17 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/signin.
    
    Bug: 409299519
    Change-Id: I7b2ee24cef2cfb210afe077ba021b039b0e6dcb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516556
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456929}

--- ios/chrome/browser/signin/model/account_consistency_browser_agent.mm
+++ ios/chrome/browser/signin/model/account_consistency_browser_agent.mm
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 AccountConsistencyBrowserAgent::AccountConsistencyBrowserAgent(
     Browser* browser,
     UIViewController* base_view_controller)
-    : base_view_controller_(base_view_controller), browser_(browser) {
+    : BrowserUserData(browser),
+      base_view_controller_(base_view_controller),
+      browser_(browser) {
   installation_observer_ =
       std::make_unique<WebStateDependencyInstallationObserver>(
           browser->GetWebStateList(), this);
