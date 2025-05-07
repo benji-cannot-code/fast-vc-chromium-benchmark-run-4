d65212ba2a4349008b27fdd096a99104f02a8f2d
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-05-07 11:54:40 -0700

    Print opaqueness information for all layer types
    Change-Id: I44562d45a5b5fcfc2b684e3322fa6fea286e96ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519987
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457173}

--- ui/compositor/debug_utils.cc
+++ ui/compositor/debug_utils.cc
@@ -60,8 +60,6 @@ void PrintLayerHierarchyImp(const Layer* layer,
       break;
     case ui::LAYER_TEXTURED:
       *out << " textured";
-      if (layer->fills_bounds_opaquely())
-        *out << " opaque";
       break;
     case ui::LAYER_SOLID_COLOR:
       *out << " solid";
@@ -69,6 +67,10 @@ void PrintLayerHierarchyImp(const Layer* layer,
     case ui::LAYER_NINE_PATCH:
       *out << " nine_patch";
       break;
+  }
+
+  if (layer->fills_bounds_opaquely()) {
+    *out << " opaque";
   }
 
   if (!layer->visible())
