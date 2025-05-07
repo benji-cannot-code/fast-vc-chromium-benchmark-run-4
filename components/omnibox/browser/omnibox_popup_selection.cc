45c0f5abd8b4ef9336fc9c99399db94b308148b9
Jan Keitel <jkeitel@google.com>
2025-05-07 10:45:02 -0700

    Modernize equality operators
    This CL is pure clean-up and contains no functionality changes.
    Depending on the files covered in the CL (since the CL was generated
    using git cl split), it does a subset of the following:
    - Remove unneeded operator!= declarations/definitions since C++20 can
      automatically derive those from operator==.
    - Default operator== where this is equivalent to the current behavior.
    - Default operator<=> where this is equivalent to the current
      behavior.
    
    This CL was uploaded by git cl split.
    
    R=pnoland@chromium.org
    
    Bug: 40256175
    Change-Id: I004f34f1dc911cb06bd2e22d7e829f761dabe013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512742
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457113}

--- components/omnibox/browser/omnibox_popup_selection.cc
+++ components/omnibox/browser/omnibox_popup_selection.cc
@@ -15,25 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 constexpr bool kIsDesktop = !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS);
 
-const size_t OmniboxPopupSelection::kNoMatch = static_cast<size_t>(-1);
-
-bool OmniboxPopupSelection::operator==(const OmniboxPopupSelection& b) const {
-  return line == b.line && state == b.state && action_index == b.action_index;
-}
-
-bool OmniboxPopupSelection::operator!=(const OmniboxPopupSelection& b) const {
-  return !operator==(b);
-}
-
-bool OmniboxPopupSelection::operator<(const OmniboxPopupSelection& b) const {
-  if (line == b.line) {
-    if (state == b.state) {
-      return action_index < b.action_index;
-    }
-    return state < b.state;
-  }
-  return line < b.line;
-}
+constexpr size_t OmniboxPopupSelection::kNoMatch = static_cast<size_t>(-1);
 
 bool OmniboxPopupSelection::IsChangeToKeyword(
     OmniboxPopupSelection from) const {
