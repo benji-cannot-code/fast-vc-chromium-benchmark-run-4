a40c83fa8775085f6cd25a485ef98e101ae79cf0
Steven Luong <stluong@chromium.org>
2025-05-07 10:52:47 -0700

    [Fixit] Allow opening chrome://bookmarks in an incognito window
    chrome://bookmarks should be allowed to show in an incognito window
    since the user can modify bookmarks via the bookmarks bar or side panel.
    
    Fixed: 40926260
    Change-Id: Icab4122fb5d487a36b34a1cc72897d7324755e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492710
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457122}

--- chrome/browser/ui/browser_navigator.cc
+++ chrome/browser/ui/browser_navigator.cc
@@ -578,7 +578,6 @@ bool IsHostAllowedInIncognito(const GURL& url) {
          host != chrome::kChromeUIHelpHost &&
          host != chrome::kChromeUIHistoryHost &&
          host != chrome::kChromeUIExtensionsHost &&
-         host != chrome::kChromeUIBookmarksHost &&
          host != password_manager::kChromeUIPasswordManagerHost;
 }
 
