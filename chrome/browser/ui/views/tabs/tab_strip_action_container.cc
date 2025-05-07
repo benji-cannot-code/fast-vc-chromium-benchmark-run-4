0a0c270418408ed4b4b6a9cf523adbe8a6b2f3f6
Jeffrey Cohen <jeffreycohen@google.com>
2025-05-07 11:34:50 -0700

    [GlicButton] remove text on click
    Bug: b:416129711
    Change-Id: I938c9df09f79e75611dc22bab3a321cc21400e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518923
    Commit-Queue: Jeffrey Cohen <jeffreycohen@google.com>
    Auto-Submit: Jeffrey Cohen <jeffreycohen@google.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457153}

--- chrome/browser/ui/views/tabs/tab_strip_action_container.cc
+++ chrome/browser/ui/views/tabs/tab_strip_action_container.cc
@@ -464,6 +464,7 @@ void TabStripActionContainer::OnGlicButtonClicked() {
   }
 
   ExecuteHideTabStripNudge(glic_button_);
+  glic_button_->SetText(std::u16string());
 }
 
 void TabStripActionContainer::OnGlicButtonDismissed() {
