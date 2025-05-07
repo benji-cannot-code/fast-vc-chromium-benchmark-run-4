fb8c39399d9e852bb7b12469b9125148878a43a2
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 06:00:18 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/start_surface.
    
    Bug: 409299519
    Change-Id: I8a8cdd0399102a382e7c92bdc0834f94ad5b741a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516090
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456928}

--- ios/chrome/browser/start_surface/ui_bundled/start_surface_recent_tab_browser_agent.mm
+++ ios/chrome/browser/start_surface/ui_bundled/start_surface_recent_tab_browser_agent.mm
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 StartSurfaceRecentTabBrowserAgent::StartSurfaceRecentTabBrowserAgent(
     Browser* browser)
-    : favicon_driver_observer_(this), browser_(browser) {
+    : BrowserUserData(browser),
+      favicon_driver_observer_(this),
+      browser_(browser) {
   browser_->AddObserver(this);
   browser_->GetWebStateList()->AddObserver(this);
 }
