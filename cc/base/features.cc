a1ef96c1247cd81d781784736ad77da3c8cd558f
Jonathan Ross <jonross@chromium.org>
2025-05-07 15:46:05 -0700

    Remove AdjustFastMainThreadThreshold
    Unused feature we are not planning to experiment with.
    
    Bug: 40132248
    Change-Id: Ibdca9b906d51defbacdb432360e29ad9116e41c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518714
    Auto-Submit: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457306}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -107,10 +107,6 @@ BASE_FEATURE(kEvictionThrottlesDraw,
              "EvictionThrottlesDraw",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAdjustFastMainThreadThreshold,
-             "AdjustFastMainThreadThreshold",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kClearCanvasResourcesInBackground,
              "ClearCanvasResourcesInBackground",
              base::FEATURE_DISABLED_BY_DEFAULT);
