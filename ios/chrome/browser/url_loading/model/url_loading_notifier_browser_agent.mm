fdcb607bdfb083cf7b58825234e811cb349ca842
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 05:58:57 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/url_loading.
    
    Bug: 409299519
    Change-Id: Ibd5754462a78bdaac0a9b10dbeeb9df503a3fac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513555
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456925}

--- ios/chrome/browser/url_loading/model/url_loading_notifier_browser_agent.mm
+++ ios/chrome/browser/url_loading/model/url_loading_notifier_browser_agent.mm
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/url_loading/model/url_loading_observer.h"
 
-UrlLoadingNotifierBrowserAgent::UrlLoadingNotifierBrowserAgent(
-    Browser* browser) {}
+UrlLoadingNotifierBrowserAgent::UrlLoadingNotifierBrowserAgent(Browser* browser)
+    : BrowserUserData(browser) {}
 
 UrlLoadingNotifierBrowserAgent::~UrlLoadingNotifierBrowserAgent() {}
 
