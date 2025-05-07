7a6fdccbf2f7c59ed169005044674d659f2b6682
Ivan Murashov <ivan.murashov@lge.com>
2025-05-07 14:19:48 -0700

    IWYU: Add missing include for base::StringPrintf usage in developer_private_functions_shared.cc
    In the file chrome/browser/../developer_private_functions_shared.cc
    the method base::StringPrintf is used without corresponding include.
    Added missing include.
    
    Bug: 392777363
    Change-Id: I1557dcd6ea172cab67a0f36c8c1f3b46a548da50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518244
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1457271}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_shared.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_shared.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/barrier_closure.h"
 #include "base/files/file_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/devtools/devtools_window.h"
