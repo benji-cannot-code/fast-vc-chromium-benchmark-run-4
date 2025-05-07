44578899af5e9018faea99a286b6c10e72a7899d
Vlad Korotkevich <vkorotkevich@google.com>
2025-05-07 04:06:47 -0700

    [Minimal UI] Hide custom tab toolbar on TWA
    Replacing webappExtras check with new #getResolvedDisplayMode, because
    it only supports WebAPK case.
    
    Some tests failed, because WebAPK already support minimal ui and
    expect browser controls to be visible on SDK pre 35. This is caused
    by the wrong display mode resolution that is fixed in this CL too.
    
    Bug: 415985098
    Change-Id: Ic100f29b2d63e3d64170c93b3f10aa5b6fb71f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513636
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Vlad Korotkevich <vkorotkevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456865}

--- chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebappIntentDataProviderTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebappIntentDataProviderTest.java
@@ -99,7 +99,7 @@ public class WebappIntentDataProviderTest {
 
         assertEquals(
                 "Should resolve to standalone",
-                DisplayMode.STANDALONE,
+                DisplayMode.MINIMAL_UI,
                 intentDataProvider.getResolvedDisplayMode());
     }
 
@@ -113,19 +113,6 @@ public class WebappIntentDataProviderTest {
         assertEquals(
                 "Should resolve to minimal ui",
                 DisplayMode.MINIMAL_UI,
-                intentDataProvider.getResolvedDisplayMode());
-    }
-
-    @Test
-    @Config(sdk = Build.VERSION_CODES.VANILLA_ICE_CREAM)
-    @DisableFeatures({ChromeFeatureList.ANDROID_MINIMAL_UI_LARGE_SCREEN})
-    public void testMinUiModeDisabled_ResolveToStandalone() {
-        var intentDataProvider =
-                buildWebAppIntentDataProvider(mIntent, buildWebAppExtras(DisplayMode.MINIMAL_UI));
-
-        assertEquals(
-                "Should resolve to standalone",
-                DisplayMode.STANDALONE,
                 intentDataProvider.getResolvedDisplayMode());
     }
 
