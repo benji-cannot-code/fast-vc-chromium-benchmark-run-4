58baf9fbe66c0399159e0843a5328722ecf1405f
Karol Sygiet <sygiet@google.com>
2025-05-07 05:24:31 -0700

    [RSG] Improve documentation of IbanSuggestionGenerator
    Bug: 409962888
    Change-Id: I49f6e3fb754665aa4705ef10fc92091089e34f63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516068
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456892}

--- components/autofill/core/browser/suggestions/suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/suggestion_generator.cc
@@ -8,14 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 std::vector<SuggestionGenerator::SuggestionData>
-SuggestionGenerator::GetSuggestionDataForFillingProduct(
+SuggestionGenerator::ExtractSuggestionDataForFillingProduct(
     const std::vector<std::pair<FillingProduct, std::vector<SuggestionData>>>&
-        suggestion_data,
+        all_suggestion_data,
     FillingProduct filling_product) {
   auto it = std::ranges::find(
-      suggestion_data, filling_product,
+      all_suggestion_data, filling_product,
       &std::pair<FillingProduct, std::vector<SuggestionData>>::first);
-  if (it != suggestion_data.end()) {
+  if (it != all_suggestion_data.end()) {
     return it->second;
   }
   return std::vector<SuggestionData>();
