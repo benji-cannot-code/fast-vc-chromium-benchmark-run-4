5fc3002e698217ed8cde8e4a7c5a87b20e417062
Ankush Singh <ankushkush@google.com>
2025-05-07 02:28:19 -0700

    Update few theme tests to work with SeparateLocalAndAccountThemes
    All the updated tests check the state upon receiving a default theme
    specifics from remote upon initial sync. The original logic to test
    what's committed by the client upon initial sync is not what really
    needs testing and moreover does not work with
    SeparateLocalAndAccountThemes enabled since local theme is no longer
    committed upon sync start. The test on the committed data is instead
    replaced with testing the current state.
    
    Bug: 348173250
    Change-Id: I70092e5379da5e464264991acd01bd13da31d133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512839
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456827}

--- chrome/browser/themes/theme_syncable_service_unittest.cc
+++ chrome/browser/themes/theme_syncable_service_unittest.cc
@@ -2192,14 +2192,11 @@ TEST_F(RealThemeSyncableServiceTest,
                 prefs::kNonSyncingNtpCustomBackgroundDictDoNotUse),
             new_value);
 
-  // The local theme should be committed to the server.
-  const syncer::SyncChangeList& changes = fake_change_processor()->changes();
-  ASSERT_EQ(changes.size(), 1u);
-  const sync_pb::ThemeSpecifics& change_specifics =
-      changes.back().sync_data().GetSpecifics().theme();
-  ASSERT_TRUE(change_specifics.has_browser_color_scheme());
-  ASSERT_TRUE(change_specifics.has_ntp_background());
-  EXPECT_EQ(change_specifics.ntp_background().url(), kTestUrl);
+  sync_pb::ThemeSpecifics current_specifics =
+      theme_sync_service()->GetThemeSpecificsFromCurrentThemeForTesting();
+  ASSERT_TRUE(current_specifics.has_browser_color_scheme());
+  ASSERT_TRUE(current_specifics.has_ntp_background());
+  EXPECT_EQ(current_specifics.ntp_background().url(), kTestUrl);
 }
 
 // Regression test for crbug.com/389026436.
@@ -2286,14 +2283,11 @@ TEST_F(RealThemeSyncableServiceTest,
                 prefs::kNonSyncingNtpCustomBackgroundDictDoNotUse),
             new_value);
 
-  // The local theme should be committed to the server.
-  const syncer::SyncChangeList& changes = fake_change_processor()->changes();
-  ASSERT_EQ(changes.size(), 1u);
-  const sync_pb::ThemeSpecifics& change_specifics =
-      changes.back().sync_data().GetSpecifics().theme();
-  ASSERT_TRUE(change_specifics.has_browser_color_scheme());
-  ASSERT_TRUE(change_specifics.has_ntp_background());
-  EXPECT_EQ(change_specifics.ntp_background().url(), kTestUrl);
+  sync_pb::ThemeSpecifics current_specifics =
+      theme_sync_service()->GetThemeSpecificsFromCurrentThemeForTesting();
+  ASSERT_TRUE(current_specifics.has_browser_color_scheme());
+  ASSERT_TRUE(current_specifics.has_ntp_background());
+  EXPECT_EQ(current_specifics.ntp_background().url(), kTestUrl);
 }
 
 TEST_F(RealThemeSyncableServiceTest,
@@ -2352,15 +2346,12 @@ TEST_F(RealThemeSyncableServiceTest,
   EXPECT_EQ(theme_service()->GetThemeID(), ThemeService::kUserColorThemeID);
   EXPECT_EQ(theme_service()->GetUserColor(), SK_ColorBLUE);
 
-  // The local theme should be committed to the server.
-  const syncer::SyncChangeList& changes = fake_change_processor()->changes();
-  ASSERT_EQ(changes.size(), 1u);
-  const sync_pb::ThemeSpecifics& change_specifics =
-      changes.back().sync_data().GetSpecifics().theme();
-  ASSERT_TRUE(change_specifics.has_browser_color_scheme());
-  ASSERT_TRUE(change_specifics.has_user_color_theme());
-  EXPECT_EQ(change_specifics.user_color_theme().color(), SK_ColorBLUE);
-  EXPECT_EQ(change_specifics.user_color_theme().browser_color_variant(),
+  sync_pb::ThemeSpecifics current_specifics =
+      theme_sync_service()->GetThemeSpecificsFromCurrentThemeForTesting();
+  ASSERT_TRUE(current_specifics.has_browser_color_scheme());
+  ASSERT_TRUE(current_specifics.has_user_color_theme());
+  EXPECT_EQ(current_specifics.user_color_theme().color(), SK_ColorBLUE);
+  EXPECT_EQ(current_specifics.user_color_theme().browser_color_variant(),
             sync_pb::ThemeSpecifics_UserColorTheme_BrowserColorVariant_NEUTRAL);
 }
 
@@ -2427,15 +2418,12 @@ TEST_F(RealThemeSyncableServiceTest,
   EXPECT_EQ(theme_service()->GetThemeID(), ThemeService::kUserColorThemeID);
   EXPECT_EQ(theme_service()->GetUserColor(), SK_ColorBLUE);
 
-  // The local theme should be committed to the server.
-  const syncer::SyncChangeList& changes = fake_change_processor()->changes();
-  ASSERT_EQ(changes.size(), 1u);
-  const sync_pb::ThemeSpecifics& change_specifics =
-      changes.back().sync_data().GetSpecifics().theme();
-  ASSERT_TRUE(change_specifics.has_browser_color_scheme());
-  ASSERT_TRUE(change_specifics.has_user_color_theme());
-  EXPECT_EQ(change_specifics.user_color_theme().color(), SK_ColorBLUE);
-  EXPECT_EQ(change_specifics.user_color_theme().browser_color_variant(),
+  sync_pb::ThemeSpecifics current_specifics =
+      theme_sync_service()->GetThemeSpecificsFromCurrentThemeForTesting();
+  ASSERT_TRUE(current_specifics.has_browser_color_scheme());
+  ASSERT_TRUE(current_specifics.has_user_color_theme());
+  EXPECT_EQ(current_specifics.user_color_theme().color(), SK_ColorBLUE);
+  EXPECT_EQ(current_specifics.user_color_theme().browser_color_variant(),
             sync_pb::ThemeSpecifics_UserColorTheme_BrowserColorVariant_NEUTRAL);
 }
 
