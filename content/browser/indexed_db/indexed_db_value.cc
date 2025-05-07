bd97eabb2d5d38535fed11a294d2a0e3612e9d25
Evan Stade <evanstade@microsoft.com>
2025-05-07 10:05:14 -0700

    IDB: get rid of IndexedDBValue copy ctor.
    This copy constructor is a footgun. There's one place in production code
    where it was used mistakenly, which is updated. There's another place in
    production code where it was used intentionally, and that's replaced
    with an explicit Clone() method.
    
    Bug: none
    Change-Id: I10c08a659f0d741af3191637918674a3ecd1af0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507450
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1457080}

--- content/browser/indexed_db/indexed_db_value.cc
+++ content/browser/indexed_db/indexed_db_value.cc
@@ -23,6 +23,18 @@ blink::mojom::IDBValuePtr IndexedDBValue::ConvertAndEraseValue(
 }
 
 IndexedDBValue::IndexedDBValue() = default;
+IndexedDBValue::~IndexedDBValue() = default;
+
+IndexedDBValue::IndexedDBValue(IndexedDBValue&& other) = default;
+IndexedDBValue& IndexedDBValue::operator=(IndexedDBValue&& other) = default;
+
+IndexedDBValue IndexedDBValue::Clone() const {
+  IndexedDBValue copy;
+  copy.bits = bits;
+  copy.external_objects = external_objects;
+  return copy;
+}
+
 IndexedDBValue::IndexedDBValue(
     const std::string& input_bits,
     const std::vector<IndexedDBExternalObject>& external_objects)
@@ -30,9 +42,5 @@ IndexedDBValue::IndexedDBValue(
       external_objects(external_objects) {
   DCHECK(external_objects.empty() || input_bits.size());
 }
-IndexedDBValue::IndexedDBValue(const IndexedDBValue& other) = default;
-IndexedDBValue::~IndexedDBValue() = default;
-IndexedDBValue& IndexedDBValue::operator=(const IndexedDBValue& other) =
-    default;
 
 }  // namespace content::indexed_db
--- content/browser/indexed_db/instance/leveldb/backing_store_unittest.cc
+++ content/browser/indexed_db/instance/leveldb/backing_store_unittest.cc
@@ -714,7 +714,7 @@ class BackingStoreTestWithBlobs : public BackingStoreTestWithExternalObjects {
 TEST_F(BackingStoreTest, PutGetConsistency) {
   base::RunLoop loop;
   const IndexedDBKey key = key1_;
-  IndexedDBValue value = value1_;
+  IndexedDBValue& value = value1_;
   BackingStore::Database db(*backing_store(),
                             BackingStore::DatabaseMetadata{u"name"});
   db.metadata().id = 1;
@@ -844,7 +844,7 @@ TEST_P(BackingStoreTestWithExternalObjects, BlobWriteCleanup) {
     external_objects().push_back(CreateBlobInfo(base::UTF8ToUTF16(type), 1));
   }
 
-  std::vector<IndexedDBValue> values = {
+  std::array<IndexedDBValue, 4> values{
       IndexedDBValue("value0", {external_objects()[0]}),
       IndexedDBValue("value1", {external_objects()[1]}),
       IndexedDBValue("value2", {external_objects()[2]}),
@@ -911,7 +911,7 @@ TEST_P(BackingStoreTestWithExternalObjects, DeleteRange) {
     blob_context_->ClearWrites();
     file_system_access_context_->ClearWrites();
 
-    std::vector<IndexedDBValue> values = {
+    std::array<IndexedDBValue, 4> values{
         IndexedDBValue("value0", {external_objects[0]}),
         IndexedDBValue("value1", {external_objects[1]}),
         IndexedDBValue("value2", {external_objects[2]}),
@@ -994,7 +994,7 @@ TEST_P(BackingStoreTestWithExternalObjects, DeleteRangeEmptyRange) {
     blob_context_->ClearWrites();
     file_system_access_context_->ClearWrites();
 
-    std::vector<IndexedDBValue> values = {
+    std::array<IndexedDBValue, 4> values{
         IndexedDBValue("value0", {external_objects[0]}),
         IndexedDBValue("value1", {external_objects[1]}),
         IndexedDBValue("value2", {external_objects[2]}),
@@ -1249,7 +1249,7 @@ TEST_F(BackingStoreTest, CreateAndDeleteIndex) {
 TEST_F(BackingStoreTest, HighIds) {
   IndexedDBKey key1 = key1_;
   IndexedDBKey key2 = key2_;
-  IndexedDBValue value1 = value1_;
+  IndexedDBValue& value1 = value1_;
 
   const int64_t high_database_id = 1ULL << 35;
   const int64_t high_object_store_id = 1ULL << 39;
@@ -1325,7 +1325,7 @@ TEST_F(BackingStoreTest, HighIds) {
 // Make sure that other invalid ids do not crash.
 TEST_F(BackingStoreTest, InvalidIds) {
   const IndexedDBKey key = key1_;
-  IndexedDBValue value = value1_;
+  IndexedDBValue& value = value1_;
 
   // valid ids for use when testing invalid ids
   const int64_t database_id = 1;
@@ -2114,7 +2114,7 @@ TEST_P(BackingStoreTestWithExternalObjects, ClearObjectStoreObjects) {
       external_objects.push_back(CreateBlobInfo(base::UTF8ToUTF16(type), 1));
     }
 
-    std::vector<IndexedDBValue> values = {
+    std::array<IndexedDBValue, 4> values{
         IndexedDBValue("value0", {external_objects[0]}),
         IndexedDBValue("value1", {external_objects[1]}),
         IndexedDBValue("value2", {external_objects[2]}),
