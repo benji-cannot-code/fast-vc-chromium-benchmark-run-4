b17d2d57cc71ab7aa29823dcb2e61451942032b0
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 05:56:40 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/infobars.
    
    Bug: 409299519
    Change-Id: Id5093bee25297a204d89c2f6c6d320c4593ed9bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516378
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456921}

--- ios/chrome/browser/infobars/model/overlays/browser_agent/infobar_overlay_browser_agent.mm
+++ ios/chrome/browser/infobars/model/overlays/browser_agent/infobar_overlay_browser_agent.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 InfobarOverlayBrowserAgent::InfobarOverlayBrowserAgent(Browser* browser)
     : OverlayBrowserAgentBase(browser),
+      BrowserUserData(browser),
       overlay_visibility_observer_(browser, this) {}
 
 InfobarOverlayBrowserAgent::~InfobarOverlayBrowserAgent() = default;
