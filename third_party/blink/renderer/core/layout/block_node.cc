d6e2aba6b46e041eb171708c1637c24be56edf8d
Haiyang Pan <hypan@google.com>
2025-05-07 10:20:49 -0700

    Revert "Include layout_box_utils.h where needed"
    This reverts commit 6a1d60f7b2bc128b061eb70fdaeed4f063176635.
    
    Reason for revert: Skeptically revert due to blink_unittests crash in internal builder, i.e. https://crbug.com/415842701#comment6
    
    Original change's description:
    > Include layout_box_utils.h where needed
    >
    > Change-Id: Ief405cf96e6c65cbafe70befb6deada296533bd2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509004
    > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1455216}
    
    Bug: 415842701
    Change-Id: Iafb8df856b11632d7a8717ee492b70b51cd44613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6515812
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457094}

--- third_party/blink/renderer/core/layout/block_node.cc
+++ third_party/blink/renderer/core/layout/block_node.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/inline/inline_cursor.h"
 #include "third_party/blink/renderer/core/layout/inline/inline_node.h"
 #include "third_party/blink/renderer/core/layout/layout_block_flow.h"
-#include "third_party/blink/renderer/core/layout/layout_box_utils.h"
 #include "third_party/blink/renderer/core/layout/layout_inline.h"
 #include "third_party/blink/renderer/core/layout/layout_input_node.h"
 #include "third_party/blink/renderer/core/layout/layout_multi_column_flow_thread.h"
