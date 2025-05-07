66339c439e247e696ce288af655afa4e2eb9aba4
Sylvain Defresne <sdefresne@chromium.org>
2025-05-07 05:52:53 -0700

    [ios] Invoke new BrowserUserData constructor
    Since most of the BrowserUserData<T> wants to keep a pointer back
    to the owning Browser, prepare managing the pointer in the base
    class by using the new constructor taking the Browser* pointer.
    
    This fixes all BrowserUserData<T> subclasses in the directory
    //ios/chrome/browser/bubble.
    
    Bug: 409299519
    Change-Id: Icb1d68e3dbb6c83142135b9e21c70dca98799501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512989
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456914}

--- ios/chrome/browser/bubble/model/tab_based_iph_browser_agent.mm
+++ ios/chrome/browser/bubble/model/tab_based_iph_browser_agent.mm
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/ui/crw_web_view_scroll_view_proxy.h"
 
 TabBasedIPHBrowserAgent::TabBasedIPHBrowserAgent(Browser* browser)
-    : web_state_list_(browser->GetWebStateList()),
+    : BrowserUserData(browser),
+      web_state_list_(browser->GetWebStateList()),
       active_web_state_observer_(
           std::make_unique<ActiveWebStateObservationForwarder>(web_state_list_,
                                                                this)),
