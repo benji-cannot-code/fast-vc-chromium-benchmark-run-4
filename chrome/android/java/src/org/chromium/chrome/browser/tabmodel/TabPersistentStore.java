df0225c4a7004cd0cc130ed003f92c3f6533c971
Calder Kitagawa <ckitagawa@chromium.org>
2025-05-08 12:17:17 -0700

    [Tab Persistence] Block loadNextTabs() if in the middle of loading
    Crash stacks suggest that `onPostExecute -> completeLoad` is happening
    after `loadNextTabs()` has been called with `mTabsToRestore.isEmpty()`
    being true. However, it is unclear how this situation can arise unless
    we somehow kick off concurrent restore or merge operations.
    
    Prevent multiple concurrent restore/merge operations by early exiting
    from `loadNextTabs()` if `mTabBatchLoader` is non-null. From existing
    operations this should already be the case in `restartBatch()` and
    it should be `null` if things go correctly in `mergeTabs()` or
    `restoreTabs()`. This means the only additional place it needs to be
    reset is in `completeLoad()` which is the result of `mTabBatchLoader`
    finishing and of which only one operation should be happening at a
    time so it is also safe to reset to null there.
    
    Bug: 416426264
    Change-Id: I007bd3cec9fbd1a72df47e8a34971ad395b1755c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6523343
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457761}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
@@ -657,13 +657,14 @@ public class TabPersistentStore {
      * Merge the tabs of the other Chrome instance into this instance by reading its tab metadata
      * file and tab state files.
      *
-     * This method should be called after a change in activity state indicates that a merge is
+     * <p>This method should be called after a change in activity state indicates that a merge is
      * necessary. #loadState() will take care of merging states on application cold start if needed.
      *
-     * If there is currently a merge or load in progress then this method will return early.
+     * <p>If there is currently a merge or load in progress then this method will return early.
      */
     public void mergeState() {
         if (mLoadInProgress
+                || mTabBatchLoader != null
                 || mPersistencePolicy.isMergeInProgress()
                 || !mTabsToRestore.isEmpty()) {
             Log.d(TAG, "Tab load still in progress when merge was attempted.");
@@ -1816,6 +1817,8 @@ public class TabPersistentStore {
     private void loadNextTabs() {
         if (mDestroyed) return;
 
+        if (mTabBatchLoader != null) return;
+
         if (mTabsToRestore.isEmpty()) {
             mNormalTabsRestored = null;
             mIncognitoTabsRestored = null;
@@ -1848,7 +1851,6 @@ public class TabPersistentStore {
             recordUniqueTabUrlMetrics();
             cleanUpPersistentData();
             onStateLoaded();
-            mTabBatchLoader = null;
             Log.d(
                     TAG,
                     "Loaded tab lists; counts: "
@@ -1928,6 +1930,7 @@ public class TabPersistentStore {
     private class TabBatchLoader {
         private final LinkedList<TabRestoreDetails> mBatchedTabsToRestore;
         private LoadTabsTask mLoadTabsTask;
+        private boolean mCancelled;
 
         /**
          * @param tabsToRestore details of {@link Tab}s which will be read from storage
@@ -1943,12 +1946,15 @@ public class TabPersistentStore {
 
         /** Loads {@link TabState} for the batch. */
         public void load() {
+            if (mCancelled) return;
+
             mLoadTabsTask = new LoadTabsTask(mBatchedTabsToRestore);
             mLoadTabsTask.executeOnTaskRunner(mSequencedTaskRunner);
         }
 
         /** Cancels restoring the batch of tabs. */
         public void cancel(boolean mayInterruptIfRunning) {
+            mCancelled = true;
             if (mLoadTabsTask != null) {
                 mLoadTabsTask.cancel(mayInterruptIfRunning);
             }
@@ -2168,6 +2174,7 @@ public class TabPersistentStore {
             }
         }
 
+        mTabBatchLoader = null;
         loadNextTabs();
     }
 
