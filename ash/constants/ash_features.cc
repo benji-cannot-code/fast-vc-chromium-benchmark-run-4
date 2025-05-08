1e0a59553a9108e17e353e9165142189da591211
Zifan Zhang <zifanzhang@google.com>
2025-05-08 12:17:04 -0700

    Keeps Boca SWA open on session ended
    Bug: 414670142
    Change-Id: Ia6f17047e1c6c7af9b547d6aaca460ea51007b4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516298
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Zifan Zhang <zifanzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457760}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -289,6 +289,11 @@ BASE_FEATURE(kBocaAdjustCaptionBubbleOnExpand,
              "BocaAdjustCaptionBubbleOnExpand",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enables or disables keeping the Boca SWA open when the session is ended.
+BASE_FEATURE(kBocaKeepSWAOpenOnSessionEnded,
+             "BocaKeepSWAOpenOnSessionEnded",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Enables or disables enforcing sequential execution for Boca Session load.
 BASE_FEATURE(kBocaSequentialSessionLoad,
              "BocaSequentialSessionLoad",
@@ -3519,6 +3524,10 @@ bool IsBocaClientTypeForSpeechRecognitionEnabled() {
 
 bool IsBocaAdjustCaptionBubbleOnExpandEnabled() {
   return base::FeatureList::IsEnabled(kBocaAdjustCaptionBubbleOnExpand);
+}
+
+bool IsBocaKeepSWAOpenOnSessionEndedEnabled() {
+  return base::FeatureList::IsEnabled(kBocaKeepSWAOpenOnSessionEnded);
 }
 
 bool IsBocaSequentialSessionLoadEnabled() {
