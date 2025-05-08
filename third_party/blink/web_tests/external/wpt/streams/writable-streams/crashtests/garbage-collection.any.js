623d58ae0b507f43c4db4ec4515473a237c6129c
Andrey Kosyakov <caseq@chromium.org>
2025-05-08 09:12:58 -0700

    Move writable streams GC crashtests to appropriate directory
    Change-Id: Ib78bca6999d9a47a3783af913685440047f98f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513796
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457643}

---
+++ third_party/blink/web_tests/external/wpt/streams/writable-streams/crashtests/garbage-collection.any.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/common/gc.js
+'use strict';
+
+// See https://crbug.com/390646657 for details.
+promise_test(async () => {
+  const written = new WritableStream({
+    write(chunk) {
+      return new Promise(resolve => {});
+    }
+  }).getWriter().write('just nod if you can hear me');
+  for (let i = 0; i < 5; ++i)
+    await garbageCollect();
+}, 'Garbage-collecting a stream writer with a pending write should not crash');
+
+promise_test(async () => {
+  const closed = new WritableStream({
+    write(chunk) { }
+  }).getWriter().closed;
+  for (let i = 0; i < 5; ++i)
+    await garbageCollect();
+}, 'Garbage-collecting a stream writer should not crash with closed promise is retained');
+
+promise_test(async () => {
+  const ready = new WritableStream({
+    write(chunk) { }
+  }, {highWaterMark: 0}).getWriter().ready;
+  for (let i = 0; i < 5; ++i)
+    await garbageCollect();
+}, 'Garbage-collecting a stream writer should not crash when backpressure is being applied');
+
