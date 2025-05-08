c87c66c3f15beea836915baa6d9431ebb653be98
Kristi Saney <kristislee@google.com>
2025-05-08 15:06:37 -0700

    [Read aloud] Move getAccessibleTextLength to SpeechController
    Bug: 346612365
    Change-Id: I9d977f3304a8e65e410e57126f4b5d489f0dbda3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527329
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457849}

--- chrome/test/data/webui/side_panel/read_anything/speech_uses_max_text_length_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/speech_uses_max_text_length_test.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 
 import type {AppElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
-import {MAX_SPEECH_LENGTH, SpeechBrowserProxyImpl} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {MAX_SPEECH_LENGTH, SpeechBrowserProxyImpl, SpeechController} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertEquals, assertGT, assertLT, assertNotEquals} from 'chrome-untrusted://webui-test/chai_assert.js';
 
 import {createApp} from './common.js';
@@ -15,6 +15,7 @@ suite('SpeechUsesMaxTextLength', () => {
   let app: AppElement;
   let maxSpeechLength: number;
   let speech: TestSpeechBrowserProxy;
+  let speechController: SpeechController;
 
   const shortSentence =
       'The snow glows white on the mountain tonight, not a footprint to be ' +
@@ -80,6 +81,8 @@ suite('SpeechUsesMaxTextLength', () => {
     chrome.readingMode.onConnected = () => {};
     speech = new TestSpeechBrowserProxy();
     SpeechBrowserProxyImpl.setInstance(speech);
+    speechController = new SpeechController();
+    SpeechController.setInstance(speechController);
 
     app = await createApp();
     maxSpeechLength = MAX_SPEECH_LENGTH;
@@ -104,7 +107,8 @@ suite('SpeechUsesMaxTextLength', () => {
 
   suite('on long sentence', () => {
     test('accessible text boundary is before max speech length', () => {
-      const firstBoundary = app.getAccessibleTextLength(longSentence);
+      const firstBoundary =
+          speechController.getUtteranceEndBoundary(longSentence, true);
       assertLT(firstBoundary, maxSpeechLength);
     });
 
@@ -123,7 +127,8 @@ suite('SpeechUsesMaxTextLength', () => {
     let firstBoundary: number;
 
     setup(() => {
-      firstBoundary = app.getAccessibleTextLength(longSentenceWithFewCommas);
+      firstBoundary = speechController.getUtteranceEndBoundary(
+          longSentenceWithFewCommas, true);
     });
 
 
@@ -135,7 +140,8 @@ suite('SpeechUsesMaxTextLength', () => {
     test('next accessible text boundary is before end of string', () => {
       const afterFirstBoundary = longSentenceWithFewCommas.substring(
           firstBoundary, longSentenceWithFewCommas.length);
-      const secondBoundary = app.getAccessibleTextLength(afterFirstBoundary);
+      const secondBoundary =
+          speechController.getUtteranceEndBoundary(afterFirstBoundary, true);
       const afterSecondBoundary = longSentenceWithFewCommas.substring(
           secondBoundary, longSentenceWithFewCommas.length);
 
@@ -151,13 +157,14 @@ suite('SpeechUsesMaxTextLength', () => {
 
     // When there are no other commas in a phrase, we don't splice on the
     // commas within numbers.
-    let boundary = app.getAccessibleTextLength(invalidCommaSplices);
+    let boundary =
+        speechController.getUtteranceEndBoundary(invalidCommaSplices, true);
     assertEquals(
         invalidCommaSplices, invalidCommaSplices.substring(0, boundary));
 
     // When there is a valid comma in a string, we splice on that instead of
     // on the commas within numbers
-    boundary = app.getAccessibleTextLength(validCommaSplice);
+    boundary = speechController.getUtteranceEndBoundary(validCommaSplice, true);
     assertEquals('525,600 minutes', validCommaSplice.substring(0, boundary));
   });
 
@@ -165,17 +172,20 @@ suite('SpeechUsesMaxTextLength', () => {
     let firstBoundary: number;
 
     setup(() => {
-      firstBoundary = app.getAccessibleTextLength(longSentenceWithLateComma);
+      firstBoundary = speechController.getUtteranceEndBoundary(
+          longSentenceWithLateComma, true);
     });
 
 
     test('commas after max speech length aren\'t used', () => {
       const afterFirstBoundary = longSentenceWithFewCommas.substring(
           firstBoundary, longSentenceWithFewCommas.length);
-      const secondBoundary = app.getAccessibleTextLength(afterFirstBoundary);
+      const secondBoundary =
+          speechController.getUtteranceEndBoundary(afterFirstBoundary, true);
       const afterSecondBoundary = longSentenceWithFewCommas.substring(
           firstBoundary, longSentenceWithFewCommas.length);
-      const thirdBoundary = app.getAccessibleTextLength(afterSecondBoundary);
+      const thirdBoundary =
+          speechController.getUtteranceEndBoundary(afterSecondBoundary, true);
 
       assertLT(firstBoundary, maxSpeechLength);
       assertLT(secondBoundary, maxSpeechLength);
