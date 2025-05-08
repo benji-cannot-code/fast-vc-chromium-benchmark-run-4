ac47dc5fc8126796d8bd03d6d34dd1aea37f3fd4
Hubert Chao <hchao@chromium.org>
2025-05-08 07:22:53 -0700

    [LNA] remove PNA content setting
    Bug: 394636065
    Change-Id: I903f265911592aa01dc99806e1bd4a556424b45f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514469
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457563}

--- chrome/browser/policy/test/content_settings_policy_browsertest.cc
+++ chrome/browser/policy/test/content_settings_policy_browsertest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
-#include "components/content_settings/core/browser/private_network_settings.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/policy_constants.h"
