34c7bae4501bd378aa3044e0844714a74ffd094d
Viktor Semeniuk <vsemeniuk@google.com>
2025-05-08 03:45:26 -0700

    Allow explicitly capturing dom-node-id for all elements
    This CL adds a button to chrome://autofill-internals and
    chrome://password-manager-internals to attach dom-node-id tag to all
    elements for all opened tabs.
    
    Fixed: 415955703
    Change-Id: I61a4c313714ed2cf67b2895d9266ca0143f6a1d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516204
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457496}

--- chrome/browser/ui/webui/autofill_and_password_manager_internals/internals_ui_handler.cc
+++ chrome/browser/ui/webui/autofill_and_password_manager_internals/internals_ui_handler.cc
@@ -35,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/password_manager/android/password_manager_eviction_util.h"
+#else
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "components/autofill/content/browser/content_autofill_driver.h"
 #endif
 
 using autofill::LogRouter;
@@ -131,6 +136,10 @@ void InternalsUIHandler::RegisterMessages() {
       "resetUpmEviction",
       base::BindRepeating(&InternalsUIHandler::OnResetUpmEviction,
                           base::Unretained(this)));
+#else
+  web_ui()->RegisterMessageCallback(
+      "setDomNodeId", base::BindRepeating(&InternalsUIHandler::SetDomNodeId,
+                                          base::Unretained(this)));
 #endif
 }
 
@@ -253,6 +262,24 @@ void InternalsUIHandler::OnResetUpmEviction(const base::Value::List& args) {
   }
   FireWebUIListener("enable-reset-upm-eviction-button",
                     base::Value(!is_user_unenrolled));
+}
+#else
+void InternalsUIHandler::SetDomNodeId(const base::Value::List& args) {
+  for (auto* browser : GetAllBrowserWindowInterfaces()) {
+    if (!browser->GetTabStripModel()) {
+      continue;
+    }
+
+    for (int i = 0; i < browser->GetTabStripModel()->count(); i++) {
+      auto* web_contents = browser->GetTabStripModel()->GetWebContentsAt(i);
+      autofill::AutofillDriver* driver =
+          ContentAutofillDriver::GetForRenderFrameHost(
+              web_contents->GetPrimaryMainFrame());
+      if (driver) {
+        driver->ExposeDomNodeIDs();
+      }
+    }
+  }
 }
 #endif
 
