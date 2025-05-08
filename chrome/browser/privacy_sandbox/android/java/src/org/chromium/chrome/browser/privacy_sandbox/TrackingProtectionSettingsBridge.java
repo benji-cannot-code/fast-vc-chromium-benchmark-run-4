3d2a7cf9021eefe591acc55df53435d3d98fec5e
Maggie Jennings <mjenn@google.com>
2025-05-08 11:24:30 -0700

    [ACT - Clank] Add bridge for Tracking Protection Settings API
    Bug: b:408036586
    Change-Id: Ie2f8b6f77d8b81cacde1e670a24730ff4b9e6cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501382
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457727}

---
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionSettingsBridge.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.privacy_sandbox;
+
+import org.jni_zero.NativeMethods;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.profiles.Profile;
+
+/** Bridge, providing access to the native-side TrackingProtectionSettings API. */
+@NullMarked
+public class TrackingProtectionSettingsBridge {
+    private final Profile mProfile;
+
+    public TrackingProtectionSettingsBridge(Profile profile) {
+        mProfile = profile;
+    }
+
+    public boolean isIpProtectionDisabledForEnterprise() {
+        return TrackingProtectionSettingsBridgeJni.get()
+                .isIpProtectionDisabledForEnterprise(mProfile);
+    }
+
+    @NativeMethods
+    public interface Natives {
+        boolean isIpProtectionDisabledForEnterprise(Profile profile);
+    }
+}
