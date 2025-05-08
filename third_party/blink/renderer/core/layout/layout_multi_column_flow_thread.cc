36cc809fd48bd6b49cb0d940c02b084bed3be16e
Morten Stenshorne <mstensho@chromium.org>
2025-05-08 00:44:07 -0700

    Reland "Include layout_box_utils.h where needed"
    This reverts commit 836973fd33f0a9b8dfbb5e7fcff1bcc921d0ad08.
    
    Reason for reland: Wasn't causing any trouble.
    
    Bug: 415842701
    Original change's description:
    > Revert "Include layout_box_utils.h where needed"
    >
    > This reverts commit 6a1d60f7b2bc128b061eb70fdaeed4f063176635.
    >
    > Reason for revert: Skeptically revert due to blink_unittests crash in internal builder, i.e. https://crbug.com/415842701#comment6
    >
    > Original change's description:
    > > Include layout_box_utils.h where needed
    > >
    > > Change-Id: Ief405cf96e6c65cbafe70befb6deada296533bd2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509004
    > > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > > Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1455216}
    >
    > Bug: 415842701
    > Change-Id: Iafb8df856b11632d7a8717ee492b70b51cd44613
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6515812
    > Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    > Commit-Queue: Haiyang Pan <hypan@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Haiyang Pan <hypan@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1457094}
    
    Bug: 415842701
    Change-Id: I30c593ba60cdd27f6eefe09bb8744878c5c6b0c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520076
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1457446}

--- third_party/blink/renderer/core/layout/layout_multi_column_flow_thread.cc
+++ third_party/blink/renderer/core/layout/layout_multi_column_flow_thread.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
 #include "third_party/blink/renderer/core/layout/fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/geometry/writing_mode_converter.h"
+#include "third_party/blink/renderer/core/layout/layout_box_utils.h"
 #include "third_party/blink/renderer/core/layout/layout_multi_column_set.h"
 #include "third_party/blink/renderer/core/layout/layout_multi_column_spanner_placeholder.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
