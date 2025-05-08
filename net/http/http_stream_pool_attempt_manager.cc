7415a46eafe70397d520ee152192ce6a74e9f0be
Kenichi Ishibashi <bashi@chromium.org>
2025-05-07 21:28:59 -0700

    HttpStreamPool: Move completion callback to AttemptManager
    This is a preparation for fixing the linked bug. I plan to change
    Group to have multiple AttemptManagers. Move AttemptManager's
    completion callback from Group to AttemptManager.
    
    Bug: 416088643
    Change-Id: I1dd4fe63d6168244fc2fdad02d3ebb4908c42e7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6515100
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457407}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -849,6 +849,12 @@ HttpStreamPool::AttemptManager::CalculateMultiplexedSessionCreationInitiator() {
   return MultiplexedSessionCreationInitiator::kUnknown;
 }
 
+void HttpStreamPool::AttemptManager::SetOnCompleteCallbackForTesting(
+    base::OnceClosure callback) {
+  CHECK(on_complete_callback_for_testing_.is_null());
+  on_complete_callback_for_testing_ = std::move(callback);
+}
+
 void HttpStreamPool::AttemptManager::StartInternal(Job* job) {
   RestrictAllowedProtocols(job->allowed_alpns());
   UpdateTcpBasedAttemptState();
@@ -2060,6 +2066,11 @@ void HttpStreamPool::AttemptManager::MaybeComplete() {
   CHECK(limit_ignoring_jobs_.empty());
   CHECK(ip_based_pooling_disabling_jobs_.empty());
   CHECK(alternative_service_disabling_jobs_.empty());
+
+  if (on_complete_callback_for_testing_) {
+    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+        FROM_HERE, std::move(on_complete_callback_for_testing_));
+  }
 
   group_->OnAttemptManagerComplete();
   // `this` is deleted.
