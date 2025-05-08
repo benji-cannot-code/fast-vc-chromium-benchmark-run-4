b562f95c741e9881e9810b678cd25d987b8c4ff9
Daniel Cheng <dcheng@chromium.org>
2025-05-08 15:26:47 -0700

    UpdateManifestFetcher: remove dependency on data decoder's JSON parser
    JSON parsing is backed by Rust now and is considered memory-safe for the
    rule of 2. This moves the JSON parsing in-process and removes the need
    for callbacks to parse JSON.
    
    Unlike before, the JSON parsing is now synchronous; this is based on
    performance data gathered while launching the Rust JSON parser. In
    general, the overhead of posting a task vastly outweighs the cost of
    synchronously parsing the JSON.
    
    Bug: 415079996
    Bug: 416086602
    Change-Id: I6bf20ca4f4f80edb2759dfbd14c9537386774519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518636
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457860}

--- chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fetcher_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fetcher_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_status_code.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
-#include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
@@ -91,7 +90,6 @@ class UpdateManifestFetcherTest : public ::testing::Test {
   }
 
   base::test::TaskEnvironment task_environment_;
-  data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
   network::TestURLLoaderFactory test_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
 };
