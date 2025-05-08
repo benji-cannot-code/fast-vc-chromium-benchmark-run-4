ab1f2879c96fa9b4679eadfa5f7087d68284ffc5
Piotr Wrótniak <piotrwrotniak@google.com>
2025-05-08 02:14:28 -0700

    Adds Email+LC suggestion generation
    Bug: 404436027
    Change-Id: I342c4ef118131855c743dea724926ab4c32ac309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519375
    Auto-Submit: Piotr Wrótniak <piotrwrotniak@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457473}

--- chrome/browser/ui/views/autofill/popup/popup_view_utils.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_utils.cc
@@ -460,6 +460,7 @@ bool IsExpandableSuggestionType(SuggestionType type) {
     case SuggestionType::kCreditCardEntry:
     case SuggestionType::kDevtoolsTestAddresses:
     case SuggestionType::kFillAutofillAi:
+    case SuggestionType::kLoyaltyCardEntry:
     case SuggestionType::kPasswordEntry:
       return true;
     case SuggestionType::kAccountStoragePasswordEntry:
@@ -481,7 +482,6 @@ bool IsExpandableSuggestionType(SuggestionType type) {
     case SuggestionType::kFillPassword:
     case SuggestionType::kGeneratePasswordEntry:
     case SuggestionType::kIbanEntry:
-    case SuggestionType::kLoyaltyCardEntry:
     case SuggestionType::kInsecureContextPaymentDisabledMessage:
     case SuggestionType::kManageAddress:
     case SuggestionType::kManageAutofillAi:
