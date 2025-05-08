01d6d62ca5a117deb77480ffecceec412f49a5a1
Bartosz Chominski <chominskib@google.com>
2025-05-08 08:08:28 -0700

    [Window Popup] Add a basic PT test for CCT-based popups
    Adds CCT-specific Station to Public Transit framework.
    
    go/window-popup-api-in-clank-dd
    
    Bug: 413582217
    Bug: 389589841
    Bug: 350074837
    Bug: 414438308
    Change-Id: If9f2090066a5e7d7545da354ab6139c8e4eb1292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492527
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Bartosz Chomiński <chominskib@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457599}

---
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/page/FrameInfoUpdatedCondition.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.transit.page;
+
+import org.chromium.base.supplier.Supplier;
+import org.chromium.base.test.transit.ConditionStatus;
+import org.chromium.base.test.transit.UiThreadCondition;
+import org.chromium.content_public.browser.WebContents;
+import org.chromium.content_public.browser.test.util.Coordinates;
+
+/** Fulfilled when FrameInfo of the WebContents provided has been updated. */
+public class FrameInfoUpdatedCondition extends UiThreadCondition {
+    Supplier<WebContents> mWebContentsSupplier;
+
+    public FrameInfoUpdatedCondition(Supplier<WebContents> webContentsSupplier) {
+        mWebContentsSupplier = dependOnSupplier(webContentsSupplier, "WebContents");
+    }
+
+    @Override
+    protected ConditionStatus checkWithSuppliers() {
+        Coordinates coordinates = Coordinates.createFor(mWebContentsSupplier.get());
+        return whether(
+                coordinates.frameInfoUpdated(),
+                "frameInfoUpdated %b, pageScaleFactor: %.2f",
+                coordinates.frameInfoUpdated(),
+                coordinates.getPageScaleFactor());
+    }
+
+    @Override
+    public String buildDescription() {
+        return "WebContents frame info updated";
+    }
+}
