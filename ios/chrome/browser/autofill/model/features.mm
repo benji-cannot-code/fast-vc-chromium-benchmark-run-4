7bba72d31891138332c261d6d962b41bec414b78
Noémie St-Onge <noemies@google.com>
2025-05-08 08:33:28 -0700

    [iOS] Clean up kSkipKeyboardAccessoryResetForSameDocumentNavigation
    This CL cleans up the
    kSkipKeyboardAccessoryResetForSameDocumentNavigation kill switch. The
    code change guarded by this flag has been around for a few milestones
    now and doesn't seem to have caused undesired side effects.
    
    Fixed: 392602181
    Change-Id: I03e2653f100a374ba135eb8cbb4fdd8c7499df82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520020
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457618}

--- ios/chrome/browser/autofill/model/features.mm
+++ ios/chrome/browser/autofill/model/features.mm
@@ -22,7 +22,3 @@ BASE_FEATURE(kThrottleFormInputAccessorySuggestionRefresh,
 BASE_FEATURE(kFormInputAccessorySkipInputViewReloadInBackground,
              "FormInputAccessorySkipInputViewReloadInBackground",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSkipKeyboardAccessoryResetForSameDocumentNavigation,
-             "SkipKeyboardAccessoryResetForSameDocumentNavigation",
-             base::FEATURE_ENABLED_BY_DEFAULT);
