7ff0a862f5d4acd8a915388d9073cd04fbf0eff0
Kartar Singh <kartarsingh@google.com>
2025-05-08 07:39:16 -0700

    [InputVizard] Use down time to account for transfer time in latency
    When a touch sequence is transferred from Browser to Viz, the Viz
    process receives a new down and uses the event time of this newly
    synthesized down event for latency calculations. To account for time
    taken to transfer touch sequence we are using downtime on Viz for
    calculating latency of down event.
    
    Note: This wouldn't resolve the issue with any pointer downs (that
    happen to occur before Browser had a chance to request touch transfer)
    from having incorrect latency calculations. Since down time is not the
    correct generation time to use for them, so the plan is to handle those
    with this bug where we will be forwarding events from Browser over to
    Viz: https://crbug.com/383307455.
    
    Bug: 414788518
    Change-Id: I58264ee4dc034ebe9b46c74c6e7474ed398345ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494577
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457577}

--- ui/events/android/motion_event_android_native.h
+++ ui/events/android/motion_event_android_native.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <optional>
 
 #include "base/android/scoped_input_event.h"
 #include "base/android/scoped_java_ref.h"
@@ -58,10 +59,15 @@ class EVENTS_EXPORT MotionEventAndroidNative : public MotionEventAndroid {
   float GetYPix(size_t pointer_index) const override;
   // End MotionEventAndroid overrides
 
+  struct EventTimes {
+    base::TimeTicks oldest;
+    base::TimeTicks latest;
+  };
   static std::unique_ptr<MotionEventAndroid> Create(
       base::android::ScopedInputEvent input_event,
       float pix_to_dip,
-      float y_offset_pix);
+      float y_offset_pix,
+      std::optional<EventTimes> event_times);
 
  private:
   MotionEventAndroidNative(base::android::ScopedInputEvent input_event,
