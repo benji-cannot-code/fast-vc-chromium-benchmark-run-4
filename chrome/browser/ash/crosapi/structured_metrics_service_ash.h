a775bd00f39182a5cf85c91a6923876e245e29cb
Di Wu <diwux@google.com>
2025-05-07 19:32:10 -0700

    Update structured_metrics_service_ash documentation
    The current documentation around structured_metrics_service_ash is
    outdated. It served Lacros and Ash, and is now still serving Ash. In
    addition, the use of mojo for messaging in the same process is by design
    and intentional in the context of serving Ash.
    
    This CL clarifies this outdated-ness.
    
    Bug: b:354842935
    Test: tryjob
    Change-Id: I4731e3b8a009b46993f417b4de766a1ac8d11cf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512393
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457378}

--- chrome/browser/ash/crosapi/structured_metrics_service_ash.h
+++ chrome/browser/ash/crosapi/structured_metrics_service_ash.h
@@ -13,9 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crosapi {
 
-// Implements the StructuredMetricsService mojo interface to record events.
-// Wrapper to validate and record structured metrics received from lacros. Lives
-// on the UI thread.
+// Implements the StructuredMetricsService mojo interface to record events from
+// AshStructuredMetricsDelegate. Wrapper to validate and record structured
+// metrics. Lives on the UI thread. Although both AshStructuredMetricsDelegate
+// and StructuredMetricsServiceAsh live in the same Ash process, instantiating a
+// mojo pipe adds little overhead and provides lots of benefits out of the box
+// (ie message buffer).
 class StructuredMetricsServiceAsh final
     : public mojom::StructuredMetricsService {
  public:
