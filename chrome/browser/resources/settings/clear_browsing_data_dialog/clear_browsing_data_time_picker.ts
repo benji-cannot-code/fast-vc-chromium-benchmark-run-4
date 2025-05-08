32811224942c0b5df9617985de66ff3982267ff5
Zaina Al-Mashni <zalmashni@google.com>
2025-05-08 06:13:44 -0700

    [Settings] Expose TimePeriod and change event from CBD TimePicker
    This change exposes the currently selected TimePeriod and dispatches a
    time-period-change event when the currently selected TimePeriod changes.
    
    This will be used by the Clear Browsing Data v2 dialog to update the
    counters and for the deletion.
    
    Bug: 397187800
    Change-Id: I03a052395c4fe60ddce316dfcdae7dcfc506bc28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513449
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457531}

--- chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_time_picker.ts
+++ chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_time_picker.ts
@@ -73,6 +73,7 @@ export class SettingsClearBrowsingDataTimePicker extends
     return {
       selectedTimePeriod_: {
         type: Number,
+        observer: 'onTimePeriodSelectionChanged_',
         value: TimePeriod.LAST_HOUR,
       },
 
@@ -140,9 +141,18 @@ export class SettingsClearBrowsingDataTimePicker extends
     const timePeriodValue =
         this.getPref('browser.clear_data.time_period').value;
 
-    if (timePeriodValue in TimePeriod) {
+    if (timePeriodValue in TimePeriod &&
+        timePeriodValue !== this.selectedTimePeriod_) {
       this.selectedTimePeriod_ = timePeriodValue;
     }
+  }
+
+  private onTimePeriodSelectionChanged_() {
+    // Dispatch a |selected-time-period-change| event to notify that the
+    // currently selected time period has changed due to an explicit user
+    // selection or a pref change.
+    this.dispatchEvent(new CustomEvent(
+        'selected-time-period-change', {bubbles: true, composed: true}));
   }
 
   private computeExpandedOptionList_(): TimePeriodOption[] {
@@ -193,7 +203,11 @@ export class SettingsClearBrowsingDataTimePicker extends
   }
 
   private onTimePeriodSelected_(event: DomRepeatEvent<TimePeriodOption>) {
-    this.selectedTimePeriod_ = event.model.item.value;
+    const newTimePeriod = event.model.item.value;
+
+    if (newTimePeriod !== this.selectedTimePeriod_) {
+      this.selectedTimePeriod_ = event.model.item.value;
+    }
   }
 
   private onMoreTimePeriodsButtonClick_(e: Event) {
@@ -210,6 +224,10 @@ export class SettingsClearBrowsingDataTimePicker extends
     // Stop propagation of the 'close' event so it doesn't close the outer
     // dialog.
     e.stopPropagation();
+  }
+
+  getSelectedTimePeriod(): TimePeriod {
+    return this.selectedTimePeriod_;
   }
 }
 
