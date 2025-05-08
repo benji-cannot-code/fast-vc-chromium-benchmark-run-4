75e19e559affaed1dd589d76ecdf24e7c6b59064
Jan Keitel <jkeitel@google.com>
2025-05-08 03:45:51 -0700

    Modernize equality operators
    This CL is pure clean-up and contains no functionality changes.
    Depending on the files covered in the CL (since the CL was generated
    using git cl split), it does a subset of the following:
    - Remove unneeded operator!= declarations/definitions since C++20 can
      automatically derive those from operator==.
    - Default operator== where this is equivalent to the current behavior.
    - Default operator<=> where this is equivalent to the current
      behavior.
    
    This CL was uploaded by git cl split.
    
    R=andypaicu@chromium.org
    
    Bug: 40256175
    Change-Id: I9bbe9794e49ef28aff296560a50d091cad4191d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512948
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457497}

--- components/permissions/permission_usage_session.cc
+++ components/permissions/permission_usage_session.cc
@@ -9,20 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace permissions {
 
-bool PermissionUsageSession::operator==(
-    const PermissionUsageSession& other) const {
-  return std::tie(origin, type, usage_start, usage_end, had_user_activation,
-                  was_foreground, had_focus) ==
-         std::tie(other.origin, other.type, other.usage_start, other.usage_end,
-                  other.had_user_activation, other.was_foreground,
-                  other.had_focus);
-}
-
-bool PermissionUsageSession::operator!=(
-    const PermissionUsageSession& other) const {
-  return !(*this == other);
-}
-
 bool PermissionUsageSession::IsValid() const {
   return !(origin.opaque() || usage_start.is_null() || usage_end.is_null() ||
            usage_end < usage_start);
