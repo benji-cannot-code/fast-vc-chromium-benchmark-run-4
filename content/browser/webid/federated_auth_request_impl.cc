dd250ab5e29054314a219aa9f507120541282d5f
Sam Goto <goto@chromium.org>
2025-05-07 17:17:46 -0700

    [fedcm] Replace json_t and base64_t with JSON and Base64 StrongAliases
    Makes the std::string json_t and base64_t strongly type by using
    StrongAlias.
    
    Bug: 380367784
    Change-Id: I8fa994eace5483578e698de193982d9aed8181c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520860
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457340}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -2955,8 +2955,9 @@ void FederatedAuthRequestImpl::ProcessSdJwt(const GURL& config_url,
 
   for (const auto& json : sd_jwt->disclosures) {
     data_decoder::DataDecoder::ParseJsonIsolated(
-        json, base::BindOnce(&FederatedAuthRequestImpl::OnDisclosureParsed,
-                             weak_ptr_factory_.GetWeakPtr(), callback, json));
+        json.value(),
+        base::BindOnce(&FederatedAuthRequestImpl::OnDisclosureParsed,
+                       weak_ptr_factory_.GetWeakPtr(), callback, json.value()));
   }
 }
 
@@ -2976,7 +2977,7 @@ void FederatedAuthRequestImpl::OnDisclosureParsed(
     return;
   }
 
-  disclosures_.push_back({disclosure->name, json});
+  disclosures_.push_back({disclosure->name, sdjwt::JSONString(json)});
   cb.Run();
 }
 
--- content/browser/webid/sd_jwt.h
+++ content/browser/webid/sd_jwt.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ref.h"
 #include "base/time/time.h"
+#include "base/types/strong_alias.h"
 #include "base/values.h"
 #include "content/common/content_export.h"
 
@@ -52,8 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   original value. Reverting back to the original value is
   important because they are signed and need to be verified.
 
-  So, a Jwt gets parsed into a json_t header and json_t payload,
-  which represent a string containing JSON, and a base64_t
+  So, a Jwt gets parsed into a JSON header and JSON payload,
+  which represent a string containing JSON, and a Base64
   signature, which represents a string containing a base64url
   encoded blob.
 
@@ -73,7 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   An SdJwt gets parsed into the issued Jwt as well as into the list
   of selective disclosures. Like a Jwt, the selective disclosures
-  are represented as json_t so that they can be serialized back to
+  are represented as JSON so that they can be serialized back to
   their original values.
 
   SdJwt sd_jwt = SdJwt::From(SdJwt::Parse(encoding))
@@ -113,9 +114,9 @@ struct CONTENT_EXPORT Jwk {
 };
 
 // A string that can be parsed as application/json.
-typedef std::string json_t;
+using JSONString = base::StrongAlias<class JSONStringTag, std::string>;
 // A string that is base64url encoded.
-typedef std::string base64_t;
+using Base64String = base::StrongAlias<class Base64StringTag, std::string>;
 
 // https://datatracker.ietf.org/doc/html/rfc7519#section-5
 struct CONTENT_EXPORT Header {
@@ -129,8 +130,8 @@ struct CONTENT_EXPORT Header {
 
   static std::optional<Header> From(const base::Value::Dict& json);
 
-  std::optional<json_t> ToJson() const;
-  std::optional<base64_t> Serialize() const;
+  std::optional<JSONString> ToJson() const;
+  std::optional<Base64String> Serialize() const;
 };
 
 // This struct holds the JWK in the "cnf" [1] parameter in the
@@ -171,11 +172,11 @@ struct CONTENT_EXPORT Payload {
   std::string vct;
 
   // Used in the Issued JWT
-  std::vector<base64_t> _sd;
+  std::vector<Base64String> _sd;
   std::string _sd_alg;
 
   // Used in the Key Binding JWT
-  base64_t sd_hash;
+  Base64String sd_hash;
 
   Payload();
   ~Payload();
@@ -183,8 +184,8 @@ struct CONTENT_EXPORT Payload {
 
   static std::optional<Payload> From(const base::Value::Dict& json);
 
-  std::optional<json_t> ToJson() const;
-  std::optional<base64_t> Serialize() const;
+  std::optional<JSONString> ToJson() const;
+  std::optional<Base64String> Serialize() const;
 };
 
 /**
@@ -198,9 +199,9 @@ typedef base::OnceCallback<std::optional<std::vector<uint8_t>>(
 
 // https://datatracker.ietf.org/doc/html/rfc7519
 struct CONTENT_EXPORT Jwt {
-  json_t header;
-  json_t payload;
-  base64_t signature;
+  JSONString header;
+  JSONString payload;
+  Base64String signature;
 
   Jwt();
   ~Jwt();
@@ -210,7 +211,7 @@ struct CONTENT_EXPORT Jwt {
 
   static std::optional<Jwt> From(const base::Value::List& json);
   static std::optional<base::Value::List> Parse(const std::string_view& jwt);
-  json_t Serialize() const;
+  JSONString Serialize() const;
 };
 
 /**
@@ -234,7 +235,7 @@ typedef base::RepeatingCallback<std::vector<std::uint8_t>(std::string_view)>
  * can create disclosures as issuers.
  */
 struct CONTENT_EXPORT Disclosure {
-  base64_t salt;
+  Base64String salt;
   std::string name;
   std::string value;
 
@@ -246,17 +247,17 @@ struct CONTENT_EXPORT Disclosure {
 
   // Creates a random value with the following requirements:
   // https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-13.html#name-entropy-of-the-salt
-  static base64_t CreateSalt();
+  static Base64String CreateSalt();
 
-  base64_t Serialize() const;
-  std::optional<json_t> ToJson() const;
-  std::optional<base64_t> Digest(Hasher hasher) const;
+  Base64String Serialize() const;
+  std::optional<JSONString> ToJson() const;
+  std::optional<Base64String> Digest(Hasher hasher) const;
 };
 
 // https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-13.html
 struct CONTENT_EXPORT SdJwt {
   Jwt jwt;
-  std::vector<json_t> disclosures;
+  std::vector<JSONString> disclosures;
 
   SdJwt();
   ~SdJwt();
@@ -265,8 +266,8 @@ struct CONTENT_EXPORT SdJwt {
   static std::optional<SdJwt> From(const base::Value::List& json);
   static std::optional<base::Value::List> Parse(const std::string_view& sdjwt);
 
-  static std::optional<std::vector<json_t>> Disclose(
-      const std::vector<std::pair<std::string, json_t>>& disclosures,
+  static std::optional<std::vector<JSONString>> Disclose(
+      const std::vector<std::pair<std::string, JSONString>>& disclosures,
       const std::vector<std::string>& selector);
 
   std::string Serialize() const;
