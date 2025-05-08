f28f74f3990e52e52a0ff9571fd4fdde4444b0c8
Salma Elmahallawy <esalma@google.com>
2025-05-07 19:24:38 -0700

    [NTPFooter] Add management notice label
    This CL adds the management notice label to the NTP footer, which can be
    one of the following in case of a managed browser:
    
    1. "Managed by label>" if a custom label is set via policy
    2. "Managed by your organization" if there is no known cloud management
    domain.
    3. "Managed by <domain>" if a the browser is managed by a known cloud management domain.
    
    Bug: 390689094,390684709
    Change-Id: Ia9e5aa4db6a0d2ed717d35d00ad2dbb6bc0fe570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6503543
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457376}

--- chrome/browser/resources/new_tab_footer/app.ts
+++ chrome/browser/resources/new_tab_footer/app.ts
@@ -5,14 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '/strings.m.js';
 
+import {assert} from '//resources/js/assert.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
-import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {getCss} from './app.css.js';
 import {getHtml} from './app.html.js';
 import {NewTabFooterDocumentProxy} from './browser_proxy.js';
-import type {ExtensionAttribution} from './new_tab_footer.mojom-webui.js';
+import type {ExtensionAttribution, ManagementNotice, NewTabFooterDocumentCallbackRouter} from './new_tab_footer.mojom-webui.js';
 
 export class NewTabFooterAppElement extends CrLitElement {
   static get is() {
@@ -30,22 +30,43 @@ export class NewTabFooterAppElement extends CrLitElement {
   static override get properties() {
     return {
       extensionAttribution_: {type: Object},
-      message_: {type: String},
+      managementNotice_: {type: Object},
     };
   }
 
+
   protected accessor extensionAttribution_: ExtensionAttribution|null = null;
-  // TODO(crbug.com/409056431): Remove `message_` once relevant
-  // variables/properties are added. This is used as a placeholder.
-  protected accessor message_: string = loadTimeData.getString('message');
+  protected accessor managementNotice_: ManagementNotice|null = null;
+  private callbackRouter_: NewTabFooterDocumentCallbackRouter;
+  private setManagementNoticeListener_: number|null = null;
 
   constructor() {
     super();
+    this.callbackRouter_ =
+        NewTabFooterDocumentProxy.getInstance().callbackRouter;
     this.getNtpExtensionAttribution_();
   }
 
   override firstUpdated() {
     ColorChangeUpdater.forDocument().start();
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.setManagementNoticeListener_ =
+        this.callbackRouter_.setManagementNotice.addListener(
+            (notice: ManagementNotice) => {
+              if (notice) {
+                this.managementNotice_ = notice;
+              }
+            });
+    NewTabFooterDocumentProxy.getInstance().handler.updateManagementNotice();
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    assert(this.setManagementNoticeListener_);
+    this.callbackRouter_.removeListener(this.setManagementNoticeListener_);
   }
 
   private async getNtpExtensionAttribution_() {
