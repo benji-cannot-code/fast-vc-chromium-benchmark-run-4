544e87d2d37ee50b69d566ff23d8df795bd41833
Taiyo Mizuhashi <taiyo@chromium.org>
2025-05-07 18:39:42 -0700

    Preredner: Remove kPrerender2WarmUpCompositor
    This feature is enabled by default in crrev.com/c/5982115 on Nov 2024 so
    this CL removes this. Since we determined to choose
    `WebLocalFrameImpl::DidCommitLoad` as a warm-up trigger point, this CL
    removes other trigger points (specifically,
    (`WebLocalFrameImpl::DidDispatchDOMContentLoadedEvent` and
    `WebLocalFrameImpl::DidFinish`)
    
    Bug: 41496019, 40240492
    Change-Id: I302a29937bc31a36e857e9416006d973fc4be9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6472275
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457365}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1914,9 +1914,6 @@ extern const char kPrerender2EarlyDocumentLifecycleUpdateDescription[];
 extern const char kWarmUpCompositorName[];
 extern const char kWarmUpCompositorDescription[];
 
-extern const char kPrerender2WarmUpCompositorName[];
-extern const char kPrerender2WarmUpCompositorDescription[];
-
 extern const char kPrerender2ForNewTabPageAndroidName[];
 extern const char kPrerender2ForNewTabPageAndroidDescription[];
 
