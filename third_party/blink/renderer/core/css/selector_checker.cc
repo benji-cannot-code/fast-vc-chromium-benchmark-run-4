216fa32ba961cdf8c1b96fb02898282744cf962a
Mason Freed <masonf@chromium.org>
2025-05-08 10:39:31 -0700

    Popovers without interactive content shouldn't need partial interest
    A change was made recently to the interest invokers explainer, which
    makes the "partial interest" concept apply only to target popovers
    that contain keyboard focusable elements. If the popover doesn't contain
    something keyboard focusable, then "partial interest" is effectively
    the same as "full interest". Except that all partial interest targets
    have a UA stylesheet rule that shows how to get it to "full" interest,
    which is a nuisance when there's no difference.
    
    Implementing this was a bit more tricky than it sounds, because it
    isn't possible to check focusability on a `display:none` subtree:
    nothing in that tree can possibly be focusable. So the new logic adds
    a `kPotentialPartialInterest` state, which is only around between
    showing interest and showing the popover. Within `showPopover()`, if
    the invoker has `kPotentialPartialInterest`, then once the popover is
    showing, the focusability of the descendants is tested, and either
    partial or full interest is selected. If the popover show operation
    is cancelled for any reason, we go back to `kNoInterest`.
    
    Bug: 326681249
    Change-Id: I058f5e9b4955ab664e899e87143d709e932b911e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509692
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457703}

--- third_party/blink/renderer/core/css/selector_checker.cc
+++ third_party/blink/renderer/core/css/selector_checker.cc
@@ -1878,7 +1878,9 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       DCHECK(RuntimeEnabledFeatures::HTMLInterestTargetAttributeEnabled(
           element.GetDocument().GetExecutionContext()));
       return element.GetInterestState() ==
-             Element::InterestState::kPartialInterest;
+                 Element::InterestState::kPartialInterest ||
+             element.GetInterestState() ==
+                 Element::InterestState::kPotentialPartialInterest;
     case CSSSelector::kPseudoTargetOfInterest: {
       DCHECK(RuntimeEnabledFeatures::HTMLInterestTargetAttributeEnabled(
           element.GetDocument().GetExecutionContext()));
@@ -1893,8 +1895,10 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       Element* invoker = element.GetInterestInvoker();
       DCHECK(!invoker || invoker->GetInterestState() !=
                              Element::InterestState::kNoInterest);
-      return invoker && invoker->GetInterestState() ==
-                            Element::InterestState::kPartialInterest;
+      return invoker && (invoker->GetInterestState() ==
+                             Element::InterestState::kPartialInterest ||
+                         invoker->GetInterestState() ==
+                             Element::InterestState::kPotentialPartialInterest);
     }
     case CSSSelector::kPseudoHasSlotted:
       DCHECK(RuntimeEnabledFeatures::CSSPseudoHasSlottedEnabled());
