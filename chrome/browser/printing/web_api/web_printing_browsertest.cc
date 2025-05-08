8a4b79b581b8057ee4a21fb97c01757e8b2a1869
Dominik Bylica <bylica@google.com>
2025-05-07 22:45:04 -0700

    [Printing] Support printQuality in Web Printing API.
    Bug: 371907543
    Change-Id: I40eaa286cf8a26bb123721021d8aca6d555c38fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6500900
    Auto-Submit: Dominik Bylica <bylica@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Dominik Bylica <bylica@google.com>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457424}

--- chrome/browser/printing/web_api/web_printing_browsertest.cc
+++ chrome/browser/printing/web_api/web_printing_browsertest.cc
@@ -141,6 +141,8 @@ auto ValidatePrintSettings() {
                                       Pointee(Eq("tray-1")))))),
       // printColorMode:
       Property(&PrintSettings::color, Eq(mojom::ColorModel::kColorModeColor)),
+      // printQuality:
+      Property(&PrintSettings::quality, Eq(mojom::Quality::kUnknownQuality)),
       Property(&PrintSettings::title, Eq(u"Title")),
       // multipleDocumentHandling:
       Property(&PrintSettings::collate, Eq(true)),
@@ -336,6 +338,8 @@ IN_PROC_BROWSER_TEST_F(WebPrintingBrowserTest, FetchAttributes) {
     }],
     "printColorModeDefault": "monochrome",
     "printColorModeSupported": [ "monochrome", "color" ],
+    "printQualityDefault": "normal",
+    "printQualitySupported": [ "draft", "normal", "high" ],
     "printerName": "name",
     "printerState": "idle",
     "printerStateMessage": "Ready to Print!",
