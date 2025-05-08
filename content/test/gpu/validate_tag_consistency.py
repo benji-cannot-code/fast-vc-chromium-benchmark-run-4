4f8137585bf27f61b5ec6c0c244c96483a8fc59c
Brian Sheedy <bsheedy@chromium.org>
2025-05-08 04:58:46 -0700

    Add Android Desktop tags
    Adds tags related to Android Desktop to the GPU expectation files and
    suppresses one existing Android failure on Android Desktop.
    
    Bug: 380269801, 413427416
    Change-Id: I51fed4b0b32808510e0b34ebd686f23315efbb88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519716
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457512}

--- content/test/gpu/validate_tag_consistency.py
+++ content/test/gpu/validate_tag_consistency.py
@@ -34,6 +34,8 @@ TAG_SPECIALIZATIONS = {
             'android-s',
             'android-t',
             'android-14',
+            'android-15',
+            'android-16',
         ],
         'chromeos': [],
         'fuchsia': [],
@@ -107,6 +109,7 @@ TAG_SPECIALIZATIONS = {
             'intel-0x3e92',
             'intel-0x3e9b',
             'intel-0x4680',
+            'intel-0x46a8',
             'intel-0x5912',
             'intel-0x9bc5',
         ],
@@ -189,6 +192,7 @@ TAG_HEADER = """\
 # tags: [ android-nexus-5x android-pixel-2 android-pixel-4
 #             android-pixel-6 android-shield-android-tv android-sm-a137f
 #             android-sm-a236b android-sm-s911u1
+#         android-brya
 #         chromeos-board-amd64-generic chromeos-board-eve chromeos-board-jacuzzi
 #             chromeos-board-octopus chromeos-board-volteer
 #         fuchsia-board-astro fuchsia-board-nelson fuchsia-board-sherlock
