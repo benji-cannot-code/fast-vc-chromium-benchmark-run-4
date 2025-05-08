3b85d62fa38d37c8482ddcac1c12cf4c5c5a2601
elabadysayed <elabadysayed@chromium.org>
2025-05-08 05:35:09 -0700

    Fix NPE for lazy framework wrapper experiment
    The experiment code missed two cases;
    - When we start AutofillSession without sending prefill request, and the
      UMA recorder will still be null in this case
    - When WebView navigates to a screen with a form but no session gets
      started, the UMA recorder will be null in this case as well.
    
    This CL fixes these cases by adding a null check.
    
    Bug: 416410410, 416354055
    Change-Id: I12f95edfd1dc7f6c1da662bd620b7e85197eccc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519942
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457522}

--- components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillProvider.java
+++ components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillProvider.java
@@ -145,7 +145,7 @@ public class AutofillProvider {
     }
 
     public void destroy() {
-        mAutofillUMA.recordSession();
+        if (mAutofillUMA != null) mAutofillUMA.recordSession();
         detachFromJavaAutofillProvider();
         getAutofillManagerWrapper().destroy();
     }
@@ -312,6 +312,7 @@ public class AutofillProvider {
         }
 
         transformFormFieldToContainViewCoordinates(formData);
+        maybeInitializeUmaRecorder(mContext);
         mAutofillUMA.onSessionStarted(getAutofillManagerWrapper().isDisabled());
         mRequest =
                 new AutofillRequest(
@@ -729,7 +730,7 @@ public class AutofillProvider {
 
     public void setWebContents(WebContents webContents) {
         if (webContents == mWebContents) return;
-        mAutofillUMA.recordSession();
+        if (mAutofillUMA != null) mAutofillUMA.recordSession();
         if (mWebContents != null) mRequest = null;
         mWebContents = webContents;
         detachFromJavaAutofillProvider();
