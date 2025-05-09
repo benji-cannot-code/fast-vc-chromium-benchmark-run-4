fd420dcd0a577874ade2e3d990366c7be5bde8f1
Andreea Costinas <acostinas@google.com>
2025-05-09 08:37:24 -0700

    Remove explicitly blocked wifi network configs
    Temporarily blocked networks (due to the
    'AllowOnlyPolicyWiFiToConnectIfAvailable' policy and the presence of a
     managed network) should be preserved to allow reconnection when the
     managed network is no longer available.
    
    However, explicitly blocked Wi-Fi network configurations should be
    removed to prevent accidental connections.
    
     Bug: 410976918
    
    Change-Id: I3cafebc1e7a2c359ea30fe3a298973b411126286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6511759
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458128}

--- chromeos/ash/components/network/auto_connect_handler.cc
+++ chromeos/ash/components/network/auto_connect_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/network/auto_connect_handler.h"
 
 #include "ash/constants/ash_features.h"
+#include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
@@ -377,9 +378,26 @@ void AutoConnectHandler::DisconnectAndRemoveConfigOrDisableAutoConnect(
       if (is_cellular_type) {
         DisableAutoconnectForNetwork(network->path(),
                                      ::onc::network_config::kCellular);
-      } else if (network->IsInProfile() && !available_only) {
+      }
+
+      // Determines if the network configuration should be persisted.
+      // Persistence is allowed if:
+      // 1. The SSID is not explicitly blocked by policy.
+      // 2. The network is not temporarily blocked due to a managed network
+      // being in range. (Users should retain settings to reconnect once the
+      // managed network is out of range.)
+      bool network_config_allowed =
+          available_only &&
+          !base::Contains(managed_configuration_handler_->GetBlockedHexSSIDs(),
+                          network->GetHexSsid());
+      bool shouldRemoveWifiConfig =
+          (network->type() == shill::kTypeWifi && network->IsInProfile()) &&
+          !network_config_allowed;
+
+      if (shouldRemoveWifiConfig) {
         RemoveNetworkConfigurationForNetwork(network->path());
       }
+
     } else if (only_managed_autoconnect) {
       // Disconnect & disable auto-connect.
       if (network->IsConnectingOrConnected())
