9b75da6d64e5af1c19c2bc92c8ecbc0c360b0464
Yoav Weiss <yoavweiss@chromium.org>
2025-05-09 01:16:48 -0700

    Add a usecounter for Integrity-Policy in Service Workers
    Given that we're skipping support for workers [1] for now, this CL adds
    a use-counter for workers that are sending Integrity-Policy headers.
    
    Those headers are currently a noop and we wouldn't expect them to be
    added by developers. But if we see that they are present, we'd know we
    need to add actual support for Integrity-Policy in service workers.
    
    
    [1] https://groups.google.com/a/chromium.org/g/blink-dev/c/Q304_OkDAZA/m/Oe75QpfUDwAJ
    
    Bug: 412588111
    Change-Id: Ie38870fce2e4d6395099883e5d4931e376fa69f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518160
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458014}

---
+++ third_party/blink/web_tests/http/tests/serviceworker/resources/integrity-policy-worker.php
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+require_once '../../resources/portabilityLayer.php';
+header('Integrity-Policy: blocked-destinations=(script)');
+header('Content-Type: application/javascript');
+?>
