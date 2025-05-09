460c8a70d4818e3864c74dcb6072ff5970270b50
John Chen <johnchen@chromium.org>
2025-05-08 17:19:51 -0700

    Run unit tests for run_performance_tests.py in CQ
    Change-Id: Id7d0d848d8324f6598af1d11529269b0f990cb75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6525966
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457908}

--- testing/scripts/run_performance_tests_unittest.py
+++ testing/scripts/run_performance_tests_unittest.py
@@ -441,3 +441,7 @@ def _create_crossbench_args(browser='./chrome'):
       '--benchmark-display-name=speedometer3.crossbench',
       f'--browser={browser}',
   ]
+
+
+if __name__ == '__main__':
+  unittest.main()
