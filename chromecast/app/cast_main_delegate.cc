d6bb91da1165617a90cddac61ce6bc7e993f2368
Anirudh <antadaka@google.com>
2025-05-09 14:34:39 -0700

    Enable CrashReporter in Cast Browser.
    This enables dump generation for crashes inside Cast browser.
    Does not initialize Crash Uploader since the uploads will be
    handled by mediashell's CrashUploader.
    
    Bug: 411471568
    Change-Id: Idc83104de2e43575027965b70715ea968eff775c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527510
    Auto-Submit: Anirudh Tadakamala <antadaka@google.com>
    Commit-Queue: Anirudh Tadakamala <antadaka@google.com>
    Reviewed-by: Antonio Rivera <antoniori@google.com>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458356}

--- chromecast/app/cast_main_delegate.cc
+++ chromecast/app/cast_main_delegate.cc
@@ -42,7 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/apk_assets.h"
+#include "base/android/java_exception_reporter.h"
 #include "chromecast/app/android/cast_crash_reporter_client_android.h"
+#include "components/crash/core/app/crashpad.h"
 #include "ui/base/resource/resource_bundle_android.h"
 #elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "chromecast/app/linux/cast_crash_reporter_client.h"
@@ -50,14 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 
 namespace {
-
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
-chromecast::CastCrashReporterClient* GetCastCrashReporter() {
-  static base::NoDestructor<chromecast::CastCrashReporterClient>
-      crash_reporter_client;
-  return crash_reporter_client.get();
-}
-#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(IS_ANDROID)
 const int kMaxCrashFiles = 10;
@@ -180,18 +174,27 @@ void CastMainDelegate::PreSandboxStartup() {
   bool enable_crash_reporter =
       !command_line->HasSwitch(switches::kDisableCrashReporter);
   if (enable_crash_reporter) {
+#if BUILDFLAG(IS_ANDROID)
+    static base::NoDestructor<chromecast::CastCrashReporterClientAndroid>
+        crash_reporter_client(process_type);
+#else
+    static base::NoDestructor<CastCrashReporterClient> crash_reporter_client;
+#endif
+    crash_reporter::SetCrashReporterClient(crash_reporter_client.get());
+
     // TODO(crbug.com/40188745): Complete crash reporting integration on
     // Fuchsia.
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
-    crash_reporter::SetCrashReporterClient(GetCastCrashReporter());
-
     if (process_type != switches::kZygoteProcess) {
       CastCrashReporterClient::InitCrashReporter(process_type);
     }
-    crash_reporter::InitializeCrashKeys();
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID)
+    crash_reporter::InitializeCrashpad(process_type.empty(), process_type);
+    base::android::InitJavaExceptionReporter();
+#endif
+    crash_reporter::InitializeCrashKeys();
   }
-
   InitializeResourceBundle();
 }
 
