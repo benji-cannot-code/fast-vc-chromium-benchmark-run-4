365a628d159354ccfb06eaeb49d94176753cfa70
Andrey Kosyakov <caseq@chromium.org>
2025-05-09 09:34:58 -0700

    Do not emit Page.frameDetached upon local frame swap
    - split FrameDetachType::kSwap into kSwapForLocal and kSwapForRemote
        based on the type of frame being swapped in;
    - do not emit `Page.frameDetached` for kSwapForLocal, as the frame
        remains in the local tree;
    - Adjust rest of references to FrameDetachType as appropriate;
    
    Bug: b/415064889
    Change-Id: Ie83b2636844c97c25cf6900fffd67d96fec834c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527830
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458165}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/subframe-navigation.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {session, dp} = await testRunner.startHTML(`
+      <iframe id="frame"
+          src="http://127.0.0.1:8000/inspector-protocol/resources/empty.html?1">
+      </iframe>`,
+  'Tests that a local subframe is not detached upon navigation');
+
+  await dp.Page.enable();
+
+  function logEvent(e) {
+    const url = e.params.url || e.params.frame?.url || "";
+    testRunner.log(`${e.method} ${url}`);
+  }
+
+  dp.Page.onFrameRequestedNavigation(logEvent);
+  dp.Page.onFrameStartedNavigating(logEvent);
+  dp.Page.onFrameDetached(e => {
+    testRunner.log(`${FAIL} ${e.method}`);
+  });
+  dp.Page.onFrameNavigated(logEvent);
+
+  session.evaluate(`
+      document.getElementById('frame').src =
+          'http://127.0.0.1:8000/inspector-protocol/resources/empty.html?2'
+  `);
+
+  await dp.Page.onceFrameNavigated();
+  testRunner.completeTest();
+})
