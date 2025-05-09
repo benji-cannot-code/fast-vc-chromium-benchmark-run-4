03495213be2b4f7ad09502b5c76d335f9eb73abe
Lingqi Chi <lingqi@chromium.org>
2025-05-08 23:12:49 -0700

    [searchprefetch] guard the prefetch param
    See the issue's comment#9 for more details
    
    Bug: 413557424
    Change-Id: I3953c16db2ccfdb3caeab7a82dee08ab00b34fe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6522288
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457984}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.h
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.h
@@ -83,6 +83,8 @@ enum class SearchPrefetchStatus {
 //   more easily.
 class SearchPrefetchRequest {
  public:
+  static void SetIsTest();
+
   SearchPrefetchRequest(const GURL& canonical_search_url,
                         const GURL& prefetch_url,
                         bool navigation_prefetch,
