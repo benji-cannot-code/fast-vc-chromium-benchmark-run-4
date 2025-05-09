d7f3dd89683e055d22bdd30a6c0437e6258cf536
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2025-05-09 08:22:34 -0700

    Revert CL for the high confidence crash culprit for http://crash/7610b882aec5924b
    The Suspect CL https://chromium-review.googlesource.com/q/I1358cfb8b06b9bb6b680cf5bd027a383d8a23f1b is highly likely the cause of the crash. Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    More details can be found in the go/crash-revert-cls-creation.
    
    Change-Id: If6b87ee58b2e960b175ca3dbf9ea2b328829fc80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530522
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458121}

--- third_party/blink/renderer/core/css/css_rule_list.h
+++ third_party/blink/renderer/core/css/css_rule_list.h
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_RULE_LIST_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_RULE_LIST_H_
 
-#include <tuple>
-
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
@@ -35,10 +33,8 @@ namespace blink {
 
 class CSSRule;
 class CSSStyleSheet;
-class TreeScope;
 
-using RuleIndexList =
-    GCedHeapVector<std::tuple<Member<CSSRule>, Member<const TreeScope>, int>>;
+using RuleIndexList = GCedHeapVector<std::pair<Member<CSSRule>, int>>;
 
 class CSSRuleList : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
