c8e4fc067bc1697f7f6f22675feaf95adf8b4e78
Alex Moshchuk <alexmos@chromium.org>
2025-05-09 11:18:35 -0700

    Renderer navigation timeline part 1: introduce Navigation Metrics Token
    As part of improving navigation metrics and Perfetto traces, we want
    to introduce a renderer-centric timeline view of events pertaining to
    a navigation, so that it is easier to evaluate performance improvement
    opportunities on the renderer side. This will complement the browser
    timeline introduced in https://crrev.com/c/6092101 and allow seeing
    how much processing of IPCs to create the provisional frame, proxies,
    RenderView, etc. contribute to each navigation. See the
    "Renderer-centric track" proposal in https://docs.google.com/document/d/1WUTAA9mbsr__prHyYPFPWKqNQpX0x7HVjoYvqt_n_q0/edit?tab=t.0#heading=h.p50kjlrjeqlr for more information.
    
    As the first step, it is necessary to tie various IPCs to a particular
    navigation.  Currently, IPCs like CreateFrame, CreateView, or
    CreateRemoteChildren have no information about whether they are being
    used for a particular navigation, and most of them could be used
    outside of navigations (e.g., when creating proxies for a dynamically
    created subframe). This CL introduces a "navigation metrics token", a
    base::UnguessableToken which lives in NavigationRequest (in its
    CommitNavigationParams) and gets passed in core IPCs that are required
    to prepare and commit a navigation in the final renderer process,
    including CreateFrame, CreateView, CreateRemoteMainFrame, and
    CreateRemoteChildren. This token is looked up at
    GetFrameHostForNavigation time and plumbed to all the places where
    these IPCs are sent in the browser process. In the linked followup CL,
    this token will be used by the renderer process to measure how long it
    takes to process these IPCs and record new trace events for these IPCs
    when a navigation commits, similarly to how this was done in the
    browser process in https://crrev.com/c/6092101.
    
    There are no behavior changes and no new trace events or metrics
    introduced in this CL - these will happen in the linked followup CL.
    
    Bug: 415821826
    Change-Id: I859c80480dd346277b0ae994e7a23ff0a9b5ad91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514188
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458231}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -4142,6 +4142,7 @@ void WebViewImpl::CreateRemoteMainFrame(
     mojom::blink::FrameReplicationStatePtr replicated_state,
     bool is_loading,
     const base::UnguessableToken& devtools_frame_token,
+    const std::optional<base::UnguessableToken>& navigation_metrics_token,
     mojom::blink::RemoteFrameInterfacesFromBrowserPtr remote_frame_interfaces,
     mojom::blink::RemoteMainFrameInterfacesPtr remote_main_frame_interfaces) {
   blink::WebFrame* opener = nullptr;
--- third_party/blink/renderer/core/frame/remote_frame.cc
+++ third_party/blink/renderer/core/frame/remote_frame.cc
@@ -1166,7 +1166,8 @@ void RemoteFrame::CreateRemoteChild(
 }
 
 void RemoteFrame::CreateRemoteChildren(
-    Vector<mojom::blink::CreateRemoteChildParamsPtr> params) {
+    Vector<mojom::blink::CreateRemoteChildParamsPtr> params,
+    const std::optional<base::UnguessableToken>& navigation_metrics_token) {
   Client()->CreateRemoteChildren(params);
 }
 
