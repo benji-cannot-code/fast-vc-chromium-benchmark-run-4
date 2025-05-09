36dd049a8b552793e1e03d92378cbf3577d1f6c2
Mei Liang <meiliang@chromium.org>
2025-05-09 14:53:19 -0700

    [Gardener] Disable CollaborationIntegrationTest#testDataSharingShowShare
    Bug: 415943165, 395648661
    Change-Id: I8c5f112e549a08aecbec9b347f7f2b4424f13327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6532868
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Owners-Override: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458364}

--- chrome/browser/collaboration/android/java/src/org/chromium/chrome/browser/collaboration/CollaborationIntegrationTest.java
+++ chrome/browser/collaboration/android/java/src/org/chromium/chrome/browser/collaboration/CollaborationIntegrationTest.java
@@ -45,6 +45,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -298,6 +299,7 @@ public class CollaborationIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/415943165")
     public void testDataSharingShowShare() {
         setUpSyncAndSignIn();
 
