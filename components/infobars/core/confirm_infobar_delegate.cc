e11d13f74bc6d85e41c666d36480746dd80b569c
Muhammad Salmaan <musalmaan@chromium.org>
2025-05-09 13:00:41 -0700

    Add custom layout for showing link text before button in infobar
    Added a custom layout to the infobar to show the link text before the
    button specifically for the installer downloader infobar to be used.
    Added interactive ui test for it as well. The next cl will add it to the
    installer downloader delegate file along with its associated tests. Link
    to screenshot: https://screenshot.googleplex.com/9Lhfpcx69KitYgS.
    
    Bug: 412697757
    Change-Id: I33e3555b425f71e5eea89a26996e829e82a90cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520114
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458294}

--- components/infobars/core/confirm_infobar_delegate.cc
+++ components/infobars/core/confirm_infobar_delegate.cc
@@ -63,6 +63,14 @@ std::u16string ConfirmInfoBarDelegate::GetButtonTooltip(
   return std::u16string();
 }
 
+bool ConfirmInfoBarDelegate::ShouldShowLinkBeforeButton() const {
+  return false;
+}
+
+int ConfirmInfoBarDelegate::GetLinkSpacingWhenPositionedBeforeButton() const {
+  return 0;
+}
+
 #if BUILDFLAG(IS_IOS)
 bool ConfirmInfoBarDelegate::UseIconBackgroundTint() const {
   return true;
