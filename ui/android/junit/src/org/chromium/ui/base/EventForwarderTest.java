0d554334315592eab5384026c97ffaad3a031cdd
Kartar Singh <kartarsingh@google.com>
2025-05-09 08:40:07 -0700

    Revert "Add trace event arg to tell if event came from System"
    This reverts commit 5dc3ee8cb3e9bc5401d55fc7b0819d10fd8795ff.
    
    Reason for revert: We have gathered the required data from field traces.
    The events with down time greater than event time are coming from
    system.
    
    Bug: 406485568, b/410789413 Original change's description:
    > Add trace event arg to tell if event came from System
    >
    > We see that some of the MotionEvents have their down time later than the
    > event time i.e. MotionEvent.GetDownTime() > MotionEvent.GetEventTime().
    > To confirm if the timestamps are coming faulty from the system or are
    > being synthesized wrongly on app/client side frame work code, we are
    > adding the arg to tell if the event came from system or not. The arg is
    > not always set to avoid having to make binder
    > call(InputManager.verifyInputEvent).
    >
    > Bug: 406485568, b/410789413
    > Change-Id: I4722711ec54d8d509be2fa9ef6358b1fbd9e6dd8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491373
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    > Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    > Commit-Queue: Kartar Singh <kartarsingh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1453460}
    
    Bug: 406485568, b/410789413
    Change-Id: Ic993edfacb31b625f99812859ee2645790a0c01c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530142
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458131}

--- ui/android/junit/src/org/chromium/ui/base/EventForwarderTest.java
+++ ui/android/junit/src/org/chromium/ui/base/EventForwarderTest.java
@@ -14,7 +14,6 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -163,8 +162,7 @@ public class EventForwarderTest {
                         0,
                         dragEvent.getButtonState(),
                         dragEvent.getMetaState(),
-                        false,
-                        null);
+                        false);
     }
 
     @Test
@@ -205,8 +203,7 @@ public class EventForwarderTest {
                         anyInt(),
                         anyInt(),
                         anyInt(),
-                        anyBoolean(),
-                        nullable(Boolean.class));
+                        anyBoolean());
         verify(mNativeMock, never())
                 .onMouseEvent(
                         anyLong(),
