e2d43b9dcc98d620bdb295a94fe395eb8c12750d
Ted Meyer <tmathmeyer@chromium.org>
2025-05-09 12:09:24 -0700

    Merge track change methods for Renderer
    Same flavor as merging the demuxer methods.
    
    Bug: 40663787
    Change-Id: Id4f6fdf3f74d6f6fbd65b5f823ba061753420fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516201
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458267}

--- media/renderers/renderer_impl_unittest.cc
+++ media/renderers/renderer_impl_unittest.cc
@@ -313,7 +313,7 @@ class RendererImplTest : public ::testing::Test {
   void SetAudioTrackSwitchExpectations() {
     InSequence track_switch_seq;
 
-    // Called from within OnEnabledAudioTracksChanged
+    // Called from within OnTracksChanged
     EXPECT_CALL(time_source_, CurrentMediaTime());
     EXPECT_CALL(time_source_, CurrentMediaTime());
     EXPECT_CALL(time_source_, StopTicking());
@@ -856,13 +856,14 @@ TEST_F(RendererImplTest, AudioTrackDisableThenEnable) {
 
   base::RunLoop disable_wait;
   SetAudioTrackSwitchExpectations();
-  renderer_impl_->OnEnabledAudioTracksChanged({}, disable_wait.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::AUDIO, {},
+                                  disable_wait.QuitClosure());
   disable_wait.Run();
 
   base::RunLoop enable_wait;
   SetAudioTrackSwitchExpectations();
-  renderer_impl_->OnEnabledAudioTracksChanged({streams_[0]},
-                                              enable_wait.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::AUDIO, {streams_[0]},
+                                  enable_wait.QuitClosure());
   enable_wait.Run();
 }
 
@@ -873,13 +874,14 @@ TEST_F(RendererImplTest, VideoTrackDisableThenEnable) {
 
   base::RunLoop disable_wait;
   SetVideoTrackSwitchExpectations();
-  renderer_impl_->OnSelectedVideoTracksChanged({}, disable_wait.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::VIDEO, {},
+                                  disable_wait.QuitClosure());
   disable_wait.Run();
 
   base::RunLoop enable_wait;
   SetVideoTrackSwitchExpectations();
-  renderer_impl_->OnSelectedVideoTracksChanged({streams_[1]},
-                                               enable_wait.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::VIDEO, {streams_[1]},
+                                  enable_wait.QuitClosure());
   enable_wait.Run();
 
   base::RunLoop().RunUntilIdle();
@@ -901,7 +903,7 @@ TEST_F(RendererImplTest, AudioUnderflowDuringAudioTrackChange) {
 
   EXPECT_CALL(time_source_, CurrentMediaTime()).Times(2);
   std::vector<DemuxerStream*> tracks;
-  renderer_impl_->OnEnabledAudioTracksChanged({}, loop.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::AUDIO, {}, loop.QuitClosure());
 
   EXPECT_CALL(callbacks_,
               OnBufferingStateChange(BUFFERING_HAVE_ENOUGH,
@@ -934,7 +936,7 @@ TEST_F(RendererImplTest, VideoUnderflowDuringVideoTrackChange) {
                                        BUFFERING_CHANGE_REASON_UNKNOWN));
   }
 
-  renderer_impl_->OnSelectedVideoTracksChanged({}, loop.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::VIDEO, {}, loop.QuitClosure());
 
   video_renderer_client_->OnBufferingStateChange(
       BUFFERING_HAVE_NOTHING, BUFFERING_CHANGE_REASON_UNKNOWN);
@@ -955,7 +957,7 @@ TEST_F(RendererImplTest, VideoUnderflowDuringAudioTrackChange) {
 
   EXPECT_CALL(time_source_, CurrentMediaTime()).Times(2);
   EXPECT_CALL(time_source_, StopTicking());
-  renderer_impl_->OnEnabledAudioTracksChanged({}, loop.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::AUDIO, {}, loop.QuitClosure());
 
   EXPECT_CALL(*audio_renderer_, StartPlaying());
   video_renderer_client_->OnBufferingStateChange(
@@ -979,7 +981,7 @@ TEST_F(RendererImplTest, AudioUnderflowDuringVideoTrackChange) {
   EXPECT_CALL(*video_renderer_, Flush(_))
       .WillOnce(MoveArg(&video_renderer_flush_cb));
 
-  renderer_impl_->OnSelectedVideoTracksChanged({}, loop.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::VIDEO, {}, loop.QuitClosure());
 
   EXPECT_CALL(time_source_, StopTicking());
   EXPECT_CALL(*video_renderer_, StartPlayingFrom(_));
@@ -1015,7 +1017,8 @@ TEST_F(RendererImplTest, VideoResumedFromUnderflowDuringAudioTrackChange) {
     EXPECT_CALL(*audio_renderer_, Flush(_))
         .WillOnce(MoveArg(&audio_renderer_flush_cb));
   }
-  renderer_impl_->OnEnabledAudioTracksChanged({}, track_change.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::AUDIO, {},
+                                  track_change.QuitClosure());
 
   // Signal that the renderer has enough data to resume from underflow.
   // Nothing should bubble up, since we are pending audio track change.
@@ -1054,7 +1057,8 @@ TEST_F(RendererImplTest, AudioResumedFromUnderflowDuringVideoTrackChange) {
     EXPECT_CALL(*video_renderer_, Flush(_))
         .WillOnce(MoveArg(&video_renderer_flush_cb));
   }
-  renderer_impl_->OnSelectedVideoTracksChanged({}, track_change.QuitClosure());
+  renderer_impl_->OnTracksChanged(DemuxerStream::VIDEO, {},
+                                  track_change.QuitClosure());
 
   // Signal that the renderer has enough data to resume from underflow.
   // Nothing should bubble up, since we are pending audio track change.
