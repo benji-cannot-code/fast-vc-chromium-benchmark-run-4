113cc36e64c1e3a453e3e1e205f45ed5cc738768
Joel Tan-Aristy <jtanaristy@google.com>
2025-05-09 12:28:53 -0700

    [Surface Color] Applies Surface Color update to GTS Search Box
    Changes GTS Search Box to Color Surface when flag is enabled.
    
    Before:
    https://screenshot.googleplex.com/BAm2rp7d2SBrpXL
    https://screenshot.googleplex.com/7BEG5TQwnzthAdy
    
    After w/ Flag Disabled:
    https://screenshot.googleplex.com/5WE93ovYG6wzNfH
    https://screenshot.googleplex.com/3idh96iqnXJQf3R
    
    After w/ Flag Enabled:
    https://screenshot.googleplex.com/ABkwcR46e92EcuT
    https://screenshot.googleplex.com/AHPm7946Ropjats
    https://screenshot.googleplex.com/78aJE8BE2kzjzVd
    
    Bug: 415328371
    Change-Id: Ic72b0dec8f7364af4cf35e4cf21fa0f2160fcf0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6516474
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458275}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewUnitTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewUnitTest.java
@@ -66,6 +66,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.hub.HubToolbarProperties.PaneButtonLookup;
+import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.omnibox.OmniboxFeatureList;
 import org.chromium.ui.base.TestActivity;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -327,7 +328,8 @@ public class HubToolbarViewUnitTest {
         GradientDrawable backgroundDrawable = (GradientDrawable) mSearchBox.getBackground();
         assertEquals(
                 ColorStateList.valueOf(
-                        ContextCompat.getColor(mActivity, R.color.baseline_neutral_20)),
+                        ContextCompat.getColor(
+                                mActivity, R.color.gm3_baseline_surface_container_highest_dark)),
                 backgroundDrawable.getColor());
 
         forceSetColorScheme(HubColorScheme.DEFAULT);
@@ -335,8 +337,7 @@ public class HubToolbarViewUnitTest {
                 MaterialColors.getColor(mActivity, R.attr.colorOnSurfaceVariant, "Test"),
                 mSearchBoxText.getCurrentHintTextColor());
         assertEquals(
-                ColorStateList.valueOf(
-                        ContextCompat.getColor(mActivity, R.color.color_primary_with_alpha_10)),
+                ColorStateList.valueOf(SemanticColorUtils.getColorSurfaceContainerHigh(mActivity)),
                 backgroundDrawable.getColor());
     }
 
