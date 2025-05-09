f7b817a50071146ebc6857a9dc093070c7bbf8ef
Maks Orlovich <morlovich@chromium.org>
2025-05-09 09:36:35 -0700

    Fix unsafe_buffers in main disk_cache tests.
    Bug: 40285824
    Change-Id: I7351087b915daac45180338b55ef6b8883a19e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520800
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458166}

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <stdint.h>
 
 #include <algorithm>
@@ -504,19 +499,15 @@ void DiskCacheBackendTest::BackendKeying() {
   EXPECT_TRUE(entry1 != entry2) << "Case sensitive";
   entry2->Close();
 
-  char buffer[30];
-  base::strlcpy(buffer, kName1, std::size(buffer));
-  ASSERT_THAT(OpenEntry(buffer, &entry2), IsOk());
+  ASSERT_THAT(OpenEntry(kName1, &entry2), IsOk());
   EXPECT_TRUE(entry1 == entry2);
   entry2->Close();
 
-  base::strlcpy(buffer + 1, kName1, std::size(buffer) - 1);
-  ASSERT_THAT(OpenEntry(buffer + 1, &entry2), IsOk());
+  ASSERT_THAT(OpenEntry(kName1, &entry2), IsOk());
   EXPECT_TRUE(entry1 == entry2);
   entry2->Close();
 
-  base::strlcpy(buffer + 3, kName1, std::size(buffer) - 3);
-  ASSERT_THAT(OpenEntry(buffer + 3, &entry2), IsOk());
+  ASSERT_THAT(OpenEntry(kName1, &entry2), IsOk());
   EXPECT_TRUE(entry1 == entry2);
   entry2->Close();
 
@@ -1195,7 +1186,7 @@ void DiskCacheBackendTest::BackendLoad() {
   int seed = static_cast<int>(Time::Now().ToInternalValue());
   srand(seed);
 
-  disk_cache::Entry* entries[kLargeNumEntries];
+  std::array<disk_cache::Entry*, kLargeNumEntries> entries;
   for (auto*& entry : entries) {
     std::string key = GenerateKey(true);
     ASSERT_THAT(CreateEntry(key, &entry), IsOk());
@@ -1332,7 +1323,8 @@ void DiskCacheBackendTest::BackendValidEntry() {
   const int kSize = 50;
   auto buffer1 = base::MakeRefCounted<net::IOBufferWithSize>(kSize);
   std::ranges::fill(buffer1->span(), 0);
-  base::strlcpy(buffer1->data(), "And the data to save", kSize);
+  buffer1->span().copy_prefix_from(
+      base::byte_span_with_nul_from_cstring("And the data to save"));
   EXPECT_EQ(kSize, WriteData(entry, 0, 0, buffer1.get(), kSize, false));
   entry->Close();
   SimulateCrash();
@@ -1368,7 +1360,8 @@ void DiskCacheBackendTest::BackendInvalidEntry() {
   const int kSize = 50;
   auto buffer = base::MakeRefCounted<net::IOBufferWithSize>(kSize);
   std::ranges::fill(buffer->span(), 0);
-  base::strlcpy(buffer->data(), "And the data to save", kSize);
+  buffer->span().copy_prefix_from(
+      base::byte_span_with_nul_from_cstring("And the data to save"));
   EXPECT_EQ(kSize, WriteData(entry, 0, 0, buffer.get(), kSize, false));
   SimulateCrash();
 
@@ -1412,7 +1405,8 @@ void DiskCacheBackendTest::BackendInvalidEntryRead() {
   const int kSize = 50;
   auto buffer = base::MakeRefCounted<net::IOBufferWithSize>(kSize);
   std::ranges::fill(buffer->span(), 0);
-  base::strlcpy(buffer->data(), "And the data to save", kSize);
+  buffer->span().copy_prefix_from(
+      base::byte_span_with_nul_from_cstring("And the data to save"));
   EXPECT_EQ(kSize, WriteData(entry, 0, 0, buffer.get(), kSize, false));
   entry->Close();
   ASSERT_THAT(OpenEntry(key, &entry), IsOk());
@@ -1465,7 +1459,7 @@ void DiskCacheBackendTest::BackendInvalidEntryWithLoad() {
   srand(seed);
 
   const int kNumEntries = 100;
-  disk_cache::Entry* entries[kNumEntries];
+  std::array<disk_cache::Entry*, kNumEntries> entries;
   for (auto*& entry : entries) {
     std::string key = GenerateKey(true);
     ASSERT_THAT(CreateEntry(key, &entry), IsOk());
@@ -1480,7 +1474,7 @@ void DiskCacheBackendTest::BackendInvalidEntryWithLoad() {
     entries[source2] = temp;
   }
 
-  std::string keys[kNumEntries];
+  std::array<std::string, kNumEntries> keys;
   for (int i = 0; i < kNumEntries; i++) {
     keys[i] = entries[i]->GetKey();
     if (i < kNumEntries / 2)
@@ -1662,7 +1656,7 @@ void DiskCacheBackendTest::BackendEnumerations() {
   disk_cache::Entry* entry;
   std::unique_ptr<TestIterator> iter = CreateIterator();
   int count = 0;
-  Time last_used[kNumEntries];
+  std::array<Time, kNumEntries> last_used;
   while (iter->OpenNextEntry(&entry) == net::OK) {
     ASSERT_TRUE(nullptr != entry);
     if (count < kNumEntries) {
@@ -1852,7 +1846,8 @@ TEST_F(DiskCacheBackendTest, ShaderCacheEnumerationReadData) {
 
   ASSERT_THAT(CreateEntry(first, &entry1), IsOk());
   std::ranges::fill(buffer1->span(), 0);
-  base::strlcpy(buffer1->data(), "And the data to save", kSize);
+  buffer1->span().copy_prefix_from(
+      base::byte_span_with_nul_from_cstring("And the data to save"));
   EXPECT_EQ(kSize, WriteData(entry1, 0, 0, buffer1.get(), kSize, false));
 
   ASSERT_THAT(CreateEntry(second, &entry2), IsOk());
@@ -1886,7 +1881,8 @@ void DiskCacheBackendTest::BackendInvalidEntryEnumeration() {
   const int kSize = 50;
   auto buffer1 = base::MakeRefCounted<net::IOBufferWithSize>(kSize);
   std::ranges::fill(buffer1->span(), 0);
-  base::strlcpy(buffer1->data(), "And the data to save", kSize);
+  buffer1->span().copy_prefix_from(
+      base::byte_span_with_nul_from_cstring("And the data to save"));
   EXPECT_EQ(kSize, WriteData(entry1, 0, 0, buffer1.get(), kSize, false));
   entry1->Close();
   ASSERT_THAT(OpenEntry(key, &entry1), IsOk());
@@ -4444,8 +4440,8 @@ TEST_F(DiskCacheBackendTest, SimpleFdLimit) {
   SetCacheType(net::APP_CACHE);
   InitCache();
 
-  disk_cache::Entry* entries[kLargeNumEntries];
-  std::string keys[kLargeNumEntries];
+  std::array<disk_cache::Entry*, kLargeNumEntries> entries;
+  std::array<std::string, kLargeNumEntries> keys;
   for (int i = 0; i < kLargeNumEntries; ++i) {
     keys[i] = GenerateKey(true);
     ASSERT_THAT(CreateEntry(keys[i], &entries[i]), IsOk());
