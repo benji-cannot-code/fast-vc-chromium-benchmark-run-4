8110333f794b86a5be2e499fd93c6fe19d45f8ed
Michael Tang <tangm@microsoft.com>
2025-05-09 07:25:55 -0700

    viz: Ensure DelegatedCompositing feature check only happens if supported
    To avoid noise in experimentation data, we need to ensure that we are
    only checking DelegatedCompositing on Windows if DComp texture are
    actually supported.
    
    Bug: 40150597
    Change-Id: Ic8830521f5f12c6736e68e927f12f5d5b434a7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6525051
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1458096}

--- components/viz/service/display/output_surface.cc
+++ components/viz/service/display/output_surface.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/trace_event/trace_event.h"
+#include "components/viz/common/features.h"
 #include "components/viz/service/display/output_surface_client.h"
 #include "components/viz/service/display/output_surface_frame.h"
 #include "ui/gfx/geometry/rect.h"
@@ -82,5 +83,17 @@ void OutputSurface::ReadbackForTesting(
     CopyOutputRequest::CopyOutputRequestCallback result_callback) {
   NOTIMPLEMENTED();
 }
+
+#if BUILDFLAG(IS_WIN)
+bool IsDelegatedCompositingSupportedAndEnabled(
+    OutputSurface::DCSupportLevel support_level) {
+  if (support_level < OutputSurface::DCSupportLevel::kDCompTexture) {
+    return false;
+  }
+
+  // Ensure we check the feature flag iff the feature is supported.
+  return features::IsDelegatedCompositingEnabled();
+}
+#endif
 
 }  // namespace viz
