eb729b10f43545557640b3f247560c85ac31e32e
Minseop Choi <minsubb13@gmail.com>
2025-05-08 21:00:17 -0700

    crypto: Migrate unexportable_key to new hash APIs
    Migrate unexportable_key from crypto/sha2 API to the newer crypto/hash
    interface.
    
    Bug: 372283556
    Change-Id: Ie35cff8dfab0d325b0fd1b0e7939ebaa15bb69a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518080
    Commit-Queue: Euisang Lim <eui-sang.lim@samsung.com>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457960}

--- crypto/unexportable_key_software_unsecure.cc
+++ crypto/unexportable_key_software_unsecure.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "build/build_config.h"
-#include "crypto/sha2.h"
+#include "crypto/hash.h"
 #include "crypto/signature_verifier.h"
 #include "crypto/unexportable_key.h"
 #include "third_party/boringssl/src/include/openssl/bn.h"
@@ -65,7 +65,7 @@ class SoftwareECDSA : public UnexportableSigningKey {
   std::optional<std::vector<uint8_t>> SignSlowly(
       base::span<const uint8_t> data) override {
     std::vector<uint8_t> ret(ECDSA_size(key_.get()));
-    std::array<uint8_t, kSHA256Length> digest = SHA256Hash(data);
+    std::array<uint8_t, hash::kSha256Size> digest = hash::Sha256(data);
     unsigned int ret_size;
     CHECK(ECDSA_sign(0, digest.data(), digest.size(), ret.data(), &ret_size,
                      key_.get()));
@@ -110,7 +110,7 @@ class SoftwareRSA : public UnexportableSigningKey {
   std::optional<std::vector<uint8_t>> SignSlowly(
       base::span<const uint8_t> data) override {
     std::vector<uint8_t> ret(RSA_size(key_.get()));
-    std::array<uint8_t, kSHA256Length> digest = SHA256Hash(data);
+    std::array<uint8_t, hash::kSha256Size> digest = hash::Sha256(data);
     unsigned int ret_size;
     CHECK(RSA_sign(NID_sha256, digest.data(), digest.size(), ret.data(),
                    &ret_size, key_.get()));
