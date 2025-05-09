c634d402778c41c81859bb6f5e40bc6156c75b04
Martin Kong <martinkong@google.com>
2025-05-09 13:46:11 -0700

    Reland "Capture Finch experiments in ANR reports"
    This is a reland of commit ff661caad7d1393ce5e8f2a627e2a5f2b4595427
    
    Reason for reland: Fixed the crash that the previous CL was causing.
    
    Root cause of the crash: During the construction of
    background_thread_task_runner_, we would check the value of a flag
    called PriorityInheritanceMutex, which cause UpdateCrashKeys() to be
    executed because it is an observer for the field trials, which would
    call functions on background_thread_task_runner_, which would cause
    a null pointer exception because the constructor hasn't returned yet.
    
    The flag check is guarded behind a build flag, and that build flag
    is only turned on for some combinations of gn args. That's why we
    didn't catch this issue when testing it manually and through CQ.
    
    Patchset 1 contains the changes in the original CL which has already
    been reviewed. Patchset 2 to 7 contains commit message changes.
    Patchset 8 contains the new, unreviewed changes.
    
    Original change's description:
    > Capture Finch experiments in ANR reports
    >
    > Ordinary crash reports contain information about which Finch experiments
    > were active on the client when the process crashed. ANR reports do not
    > contain this information, which is to the detriment of their usability.
    > This CL captures the list of Finch experiments and adds them to the ANR
    > reports for Chrome Android. This is done as follows:
    >
    > First, whenever there is a change in field trials (including synthetic
    > trials), UpdateCrashKeys() will be called. Inside UpdateCrashKeys(),
    > we send the list of field trials to the Java side via a JNI call.
    > In AnrCollector.java, we receive the string containing the list of
    > field trials, and we save the raw string to a file on disk, and we
    > keep one such file for each Chrome run. Additionally, we also save a
    > short hash of this string to Process State Summary (we only save a
    > short hash and not the raw string because the Process State Summary
    > is very small in size).
    >
    > Then, when an ANR happens, the app will be forcibly closed. When the
    > app is launched again, AnrCollector.java will query the historical
    > exit reasons. If the reason is ANR, it will query the associated
    > Process State Summary and retrieve the short hash. It will then read
    > each file on disk and try to find a file that matches the short hash.
    > It will then read the raw string containing the list of field trials
    > from that file, and then rewrite_minidumps_as_mimes.cc will add this
    > raw string to the ANR reports.
    >
    > An example ANR report that contains the list of Finch experiments
    > (click on Fields and then Experiments):
    > https://crash-staging.corp.google.com/fd9736f0f9b2d707
    >
    > Bug: 402461197
    > Change-Id: I3ec94a6126b506709093c0dcaa3924e2a8c7de09
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6463854
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Martin Kong <martinkong@google.com>
    > Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    > Reviewed-by: Sam Maier <smaier@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1455170}
    
    Bug: 402461197, 415489395
    Change-Id: I3bc6a8f9f6eab546367561a225d6c94b30e60967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512473
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458334}

--- components/variations/variations_crash_keys.cc
+++ components/variations/variations_crash_keys.cc
@@ -23,8 +23,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/synthetic_trials.h"
 #include "components/variations/variations_switches.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
 #include "base/task/thread_pool.h"
+#endif
+
+#if BUILDFLAG(IS_ANDROID)
+#include "base/task/cancelable_task_tracker.h"
+#include "components/variations/variations_crash_keys_android.h"
+#endif
+
+#if BUILDFLAG(IS_CHROMEOS)
 #include "components/variations/variations_crash_keys_chromeos.h"
 #endif
 
@@ -32,9 +40,9 @@ namespace variations {
 
 namespace {
 
-// Size of the "num-experiments" crash key in bytes. 1024*6 bytes should be able
-// to hold about 341 entries, given each entry is 18 bytes long (due to being
-// of the form "8e7abfb0-c16397b7,").
+// Size of the "variations" crash key (kExperimentListKey) in bytes.
+// 1024*6 bytes should be able to hold about 341 entries, given each entry is
+// 18 bytes long (due to being of the form "8e7abfb0-c16397b7,").
 #if BUILDFLAG(LARGE_VARIATION_KEY_SIZE)
 constexpr size_t kVariationsKeySize = 1024 * 8;
 constexpr char kVariationKeySizeHistogram[] =
@@ -105,11 +113,17 @@ class VariationsCrashKeys final : public base::FieldTrialList::Observer {
   // observer calls that happen on a different thread.
   scoped_refptr<base::SequencedTaskRunner> ui_thread_task_runner_;
 
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
   // Task runner corresponding to a background thread, used for tasks that may
   // block.
   scoped_refptr<base::SequencedTaskRunner> background_thread_task_runner_;
-#endif  // IS_CHROMEOS
+#endif  // IS_CHROMEOS || IS_ANDROID
+
+#if BUILDFLAG(IS_ANDROID)
+  // A task tracker that allows us to cancel any tasks that have been posted
+  // but have not started to run.
+  base::CancelableTaskTracker cancelable_task_tracker_;
+#endif  // IS_ANDROID
 
   // A serialized string containing the variations state.
   std::string variations_string_;
@@ -129,6 +143,14 @@ VariationsCrashKeys::VariationsCrashKeys() {
   // thread, calling OnFieldTrialGroupFinalized(), and accessing
   // |ui_thread_task_runner_| before it is set.
   ui_thread_task_runner_ = base::SequencedTaskRunner::GetCurrentDefault();
+
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
+  // Set |background_thread_task_runner_| before observering field trials for
+  // the same reason mentioned above.
+  background_thread_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
+      {base::TaskPriority::BEST_EFFORT, base::MayBlock()});
+#endif  // IS_CHROMEOS || IS_ANDROID
+
   // Observe field trials before filling the crash key with the currently
   // active field trials. Otherwise, there could be a race condition where a
   // trial is activated on a different thread before we started observing.
@@ -149,10 +171,6 @@ VariationsCrashKeys::VariationsCrashKeys() {
   for (const auto& entry : active_groups) {
     AppendFieldTrial(entry.trial_name, entry.group_name, entry.is_overridden);
   }
-#if BUILDFLAG(IS_CHROMEOS)
-  background_thread_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
-      {base::TaskPriority::BEST_EFFORT, base::MayBlock()});
-#endif  // IS_CHROMEOS
 
   UpdateCrashKeys();
 }
@@ -251,6 +269,11 @@ void VariationsCrashKeys::UpdateCrashKeys() {
     SetVariationsSeedVersionCrashKey(command_line->GetSwitchValueASCII(
         variations::switches::kVariationsSeedVersion));
   }
+
+#if BUILDFLAG(IS_ANDROID)
+  SaveVariationsForAnrReporting(&cancelable_task_tracker_,
+                                background_thread_task_runner_, info);
+#endif  // IS_ANDROID
 
 #if BUILDFLAG(IS_CHROMEOS)
   ReportVariationsToChromeOs(background_thread_task_runner_, info);
