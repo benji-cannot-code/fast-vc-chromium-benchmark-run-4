307898ed66feef834693dd4311f1a9d963785212
Adem Derinel <derinel@google.com>
2025-05-09 00:18:58 -0700

    WebAuthn: Update GPM touch ID UI for immediate mediation
    Cancel button, step title are renamed while the description is removed.
    
    Bug: 393055190
    Change-Id: Ib3a4af1b2913b5df150c5cfd41e3a4be3bbb828a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519526
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457998}

--- chrome/browser/ui/webauthn/sheet_models.h
+++ chrome/browser/ui/webauthn/sheet_models.h
@@ -274,6 +274,7 @@ class AuthenticatorTouchIdSheetModel : public AuthenticatorSheetModelBase {
   AcceptButtonState GetAcceptButtonState() const override;
   bool IsCancelButtonVisible() const override;
   std::u16string GetAcceptButtonLabel() const override;
+  std::u16string GetCancelButtonLabel() const override;
   void OnAccept() override;
 
   bool touch_id_completed_ = false;
