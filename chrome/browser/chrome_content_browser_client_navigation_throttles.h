bc1857bb6aa2ee11913a3c630ef8c0bf5179b6e5
Takashi Toyoshima <toyoshim@chromium.org>
2025-05-08 17:40:04 -0700

    Separate NavigationThrottle code from chrome_content_browser_client.cc
    This CL extracts NavigationThrottle related codes in
    chrome_content_browser_client.cc into a new file
    chrome_content_browser_client_navigation_throttles.cc.
    
    Currently, chrome_content_browser_client.cc is too large, and takes very
    long time to compile.
    
    This is the first step change, and other throttles will be moved into
    a new file step by step.
    
    BUG: 412524375
    Change-Id: If5d510e1454497fbde2a4c6dcad4b95cf64f5cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520913
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457917}

---
+++ chrome/browser/chrome_content_browser_client_navigation_throttles.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROME_CONTENT_BROWSER_CLIENT_NAVIGATION_THROTTLE_H_
+#define CHROME_BROWSER_CHROME_CONTENT_BROWSER_CLIENT_NAVIGATION_THROTTLE_H_
+
+namespace content {
+class NavigationThrottleRegistry;
+}  // namespace content
+
+void CreateAndAddChromeThrottlesForNavigation(
+    content::NavigationThrottleRegistry& registry);
+
+#endif  // CHROME_BROWSER_CHROME_CONTENT_BROWSER_CLIENT_NAVIGATION_THROTTLE_H_
