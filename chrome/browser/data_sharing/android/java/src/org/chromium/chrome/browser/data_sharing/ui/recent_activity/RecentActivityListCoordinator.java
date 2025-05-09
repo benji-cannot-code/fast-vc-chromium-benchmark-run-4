8ecf241754b65d2acbdb0ac20b1a9def9b148099
Hailey Wang <haileywang@google.com>
2025-05-08 17:04:21 -0700

    [STG] Modify recent activity menu item
    Change menu width to be similar to other menus found in Chrome.
    
    Change string to reflect latest UX requirements.
    
    Bug: 399709362
    Change-Id: Ie4e2211d6306fe4f4e272a7889e26755869a8521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527131
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1457897}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/ui/recent_activity/RecentActivityListCoordinator.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/ui/recent_activity/RecentActivityListCoordinator.java
@@ -160,14 +160,14 @@ public class RecentActivityListCoordinator {
             ModelList modelList = new ModelList();
             modelList.add(
                     BrowserUiListMenuUtils.buildMenuListItem(
-                            R.string.data_sharing_shared_tab_group_activity,
+                            R.string.data_sharing_shared_tab_groups_activity,
                             R.id.see_full_activity,
                             0,
                             /* enabled= */ true));
             ListMenu.Delegate delegate =
                     (model) -> {
                         int textId = model.get(ListMenuItemProperties.TITLE_ID);
-                        if (textId == R.string.data_sharing_shared_tab_group_activity) {
+                        if (textId == R.string.data_sharing_shared_tab_groups_activity) {
                             mShowFullActivityRunnable.run();
                         }
                     };
@@ -195,6 +195,8 @@ public class RecentActivityListCoordinator {
                         }
                     };
 
+            menuView.setMenuMaxWidth(
+                    view.getResources().getDimensionPixelSize(R.dimen.recent_activity_menu_width));
             menuView.setDelegate(listMenuDelegate);
             menuView.tryToFitLargestItem(true);
             menuView.showMenu();
