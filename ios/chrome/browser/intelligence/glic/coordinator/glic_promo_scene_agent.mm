4b778c9e76aa8ea0d3185d28bb8a1a420757c92e
Joemer Ramos <joemerramos@chromium.org>
2025-05-09 12:59:48 -0700

    [iOS] Create GLIC Promo Unit Test Part 1
    This CL:
    - Creates a unit test for GLIC Promo Scene Agent
    - Adds feature flag guard in scene agent to
    properly test feature flag with promo manager since unit tests don't go
    through the scene controller.
    
    This CL was meant to have incomplete code in order to make the CL size
    smaller.
    
    This is a part of a multi-part CL:
    --> crrev.com/c/6518345 (Unit tests)
        crrev.com/c/6520014
        crrev.com/c/6508351
        crrev.com/c/6505700
    
    Bug: 414808442
    Change-Id: I02c822f54dc1f7c91f71152d5bae4aae235fb7eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518345
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458293}

--- ios/chrome/browser/intelligence/glic/coordinator/glic_promo_scene_agent.mm
+++ ios/chrome/browser/intelligence/glic/coordinator/glic_promo_scene_agent.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/glic/coordinator/glic_promo_scene_agent.h"
 
 #import "base/memory/raw_ptr.h"
+#import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/promos_manager/model/constants.h"
 #import "ios/chrome/browser/promos_manager/model/promos_manager.h"
 
@@ -27,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     transitionedToActivationLevel:(SceneActivationLevel)level {
   switch (level) {
     case SceneActivationLevelForegroundActive: {
-      [self registerPromoForSingleDisplay];
+      if (IsPageActionMenuEnabled()) {
+        [self registerPromoForSingleDisplay];
+      }
       break;
     }
     case SceneActivationLevelUnattached:
