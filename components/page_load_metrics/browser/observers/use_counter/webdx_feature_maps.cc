e68a2e60294745fbff82bfaf02f11aff40be36b8
Philip Jägenstedt <foolip@chromium.org>
2025-05-09 08:52:42 -0700

    Add WebDX use counter for requestIdleCallback()
    Bug: 366475997
    Change-Id: I982d4c287154360ec440df388ecc83a7db778aae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530800
    Commit-Queue: Johnny Stenback <jstenback@google.com>
    Auto-Submit: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Johnny Stenback <jstenback@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458142}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -396,6 +396,8 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kV8Window_GetScreenDetails_Method,
            WebDXFeature::kWindowManagement},
           {WebFeature::kCSSLightDark, WebDXFeature::kLightDark},
+          {WebFeature::kV8Window_RequestIdleCallback_Method,
+           WebDXFeature::kRequestidlecallback},
           // Add new features above this line.
       }};
 
