54c0b6bee5edb44f127a05630f01ae92487ea7a5
Elly <ellyjones@chromium.org>
2025-05-09 18:44:31 -0700

    storage: add debugging code for nested widget close
    The linked bug seems like it might be caused by a nested widget close,
    which would ultimately cause StoragePressureBubbleView::OnDialogAccepted
    to have its receiver destroyed partway through executing. This change
    adds some extra CHECKs to this method to try to trap this problem more
    cleanly, since efforts to reproduce it locally (either manually or with
    automated testing) have not been successful.
    
    In the cases where these CHECKs would fire, the Navigate() call at the
    end of OnDialogAccepted() is about to crash the browser regardless, so
    these shouldn't cause any additional stability problems.
    
    Bug: 372479681
    Change-Id: I777a2c145aaed9bcf9a48b5faad0ec5aeedc2cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527688
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458438}

--- chrome/browser/ui/views/storage/storage_pressure_bubble_view.cc
+++ chrome/browser/ui/views/storage/storage_pressure_bubble_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/storage/storage_pressure_bubble_view.h"
 
+#include "base/auto_reset.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -81,18 +82,27 @@ StoragePressureBubbleView::StoragePressureBubbleView(views::View* anchor_view,
 }
 
 StoragePressureBubbleView::~StoragePressureBubbleView() {
+  CHECK(!in_accept_);
   if (ignored_) {
     RecordBubbleHistogramValue(StoragePressureBubbleHistogramValue::kIgnored);
   }
 }
 
 void StoragePressureBubbleView::OnDialogAccepted() {
+  base::AutoReset reset_in_accept(&in_accept_, true);
+  auto weak_this = weak_ptr_factory_.GetWeakPtr();
+
   ignored_ = false;
   RecordBubbleHistogramValue(
       StoragePressureBubbleHistogramValue::kOpenedAllSites);
   // TODO(ellyjones): What is this doing here? The widget's about to close
   // anyway?
   GetWidget()->Close();
+
+  CHECK(weak_this);
+  CHECK(browser_);
+  CHECK(browser_->profile());
+
   const GURL all_sites_gurl(kAllSitesContentSettingsUrl);
   NavigateParams params(browser_, all_sites_gurl,
                         ui::PAGE_TRANSITION_AUTO_TOPLEVEL);
