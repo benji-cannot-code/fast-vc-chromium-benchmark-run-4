929821549f556eb1b415155a9abfa07f9f072193
Aishwarya Rajesh <aishwaryarj@google.com>
2025-05-12 10:11:28 -0700

    [TSLO] Disable TabStripLayoutOptimization
    Step #1 of go/finch-killswitch#process, disabling because the feature
    breaks tab strip functionality on Lenovo tablets running Android 15.
    
    Bug: 416742365
    Change-Id: I9dd1d055e608a99819502ca82e6c1fb19c8ff673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533538
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458923}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1209,7 +1209,7 @@ BASE_FEATURE(kTabStripGroupReorderAndroid,
 
 BASE_FEATURE(kTabStripLayoutOptimization,
              "TabStripLayoutOptimization",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTabStripTransitionInDesktopWindow,
              "TabStripTransitionInDesktopWindow",
