696994641df2ef99c0eee2bc488f817f2936a08c
Zhe Li <zheliooo@google.com>
2025-05-12 11:29:34 -0700

    [TabGroupDragDrop] Ensure the rootTab is always included in the regrouped tab group
    - Ensure the tabs in the list are correctly ordered before regrouping to preserve their positions in #mergeListOfTabsToGroup.
    
    Bug: 415916442
    Change-Id: I0c542a41f5c5a8d6ed9befd749f3e78f434dbf76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533499
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458971}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -314,8 +314,8 @@ import org.chromium.url.GURL;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -1597,7 +1597,7 @@ public class ChromeTabbedActivity extends ChromeActivity {
     private boolean maybeHandleGroupUrlsIntent(Intent intent, TabGroupMetadata tabGroupMetadata) {
         @TabOpenType int tabOpenType = IntentHandler.getTabOpenType(intent);
 
-        ArrayList<Tab> tabs = new ArrayList<>();
+        LinkedList<Tab> tabs = new LinkedList<>();
         for (Map.Entry<Integer, String> entry : tabGroupMetadata.tabIdsToUrls) {
             int tabId = entry.getKey();
             String url = entry.getValue();
@@ -1623,7 +1623,10 @@ public class ChromeTabbedActivity extends ChromeActivity {
                             IntentUtils.safeGetStringExtra(intent, Browser.EXTRA_APPLICATION_ID),
                             Tab.INVALID_TAB_ID,
                             intent);
-            tabs.add(tab);
+            // Restores the correct tab order by adding the tab to the front. `tabIdsToUrls` were
+            // stored in reverse to preserve open positions (see {@link
+            // TabGroupMetadataExtractor#extractTabGroupMetadata}).
+            tabs.add(0, tab);
         }
 
         // 4. Regroup tabs and restore the original group properties(e.g. color, title, collapsed
