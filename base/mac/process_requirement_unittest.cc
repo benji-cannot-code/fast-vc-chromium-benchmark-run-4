b07660b05c06c6c7fe78d9f16a1599a5e07e0503
Leonard Grey <lgrey@chromium.org>
2025-05-12 10:48:58 -0700

    Use CHECK_DEATH tests in ProcessRequirementTest
    These are failing on official bots because they're trying to match a
    check's failure message, but those are stripped on official.
    `...CHECK_DEATH...` macros handle this correctly.
    
    Bug: 409716098
    Change-Id: I8c0beffbd693a4ef7f0fabfb95a28ce12c8f3dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6532915
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458948}

--- base/mac/process_requirement_unittest.cc
+++ base/mac/process_requirement_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/sys_byteorder.h"
+#include "base/test/gtest_util.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -298,7 +299,7 @@ TEST_F(ProcessRequirementTest, KernelFailuresAreNotFatal) {
 }
 
 TEST_F(ProcessRequirementTest, InvalidCombinations) {
-  EXPECT_DEATH(
+  EXPECT_CHECK_DEATH_WITH(
       {
         // A requirement that includes a team identifier but no certificate type
         // will assert because it could be matched by a self-signed certificate
@@ -308,7 +309,7 @@ TEST_F(ProcessRequirementTest, InvalidCombinations) {
       },
       "without specifying a certificate type is unsafe");
 
-  EXPECT_DEATH(
+  EXPECT_CHECK_DEATH_WITH(
       {
         // A requirement that includes a certificate type but no team identifier
         // will assert because it will match any signing identity of that type
