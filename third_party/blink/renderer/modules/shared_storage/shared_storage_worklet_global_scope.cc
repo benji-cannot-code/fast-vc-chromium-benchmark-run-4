922d689b16a240ad9ea96cfb05bd593d961ab682
Scott Haseley <shaseley@chromium.org>
2025-05-12 11:48:30 -0700

    Update blink to use [Get|Set]ContinuationPreservedEmbedderDataV2()
    [Get|Set]ContinuationPreservedEmbedderData() is being replaced by a V2
    API which uses v8::Data instead of v8::Value so that the API can be
    used with v8::CppHeapExternal. This CL updates the two places that use
    the API.
    
    Note: this doesn't update the presubmit checks, which use
    ContinuationPreservedEmbedderData.*
    
    Bug: 376599402
    Change-Id: I5596cde282d6dce09ae6f89c08746981a1750bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6528590
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458989}

--- third_party/blink/renderer/modules/shared_storage/shared_storage_worklet_global_scope.cc
+++ third_party/blink/renderer/modules/shared_storage/shared_storage_worklet_global_scope.cc
@@ -1139,8 +1139,8 @@ int64_t SharedStorageWorkletGlobalScope::GetCurrentOperationId() {
   ScriptState* script_state = ScriptController()->GetScriptState();
   DCHECK(script_state);
 
-  v8::Local<v8::Value> data =
-      script_state->GetIsolate()->GetContinuationPreservedEmbedderData();
+  v8::Local<v8::Data> data =
+      script_state->GetIsolate()->GetContinuationPreservedEmbedderDataV2();
   return data.As<v8::BigInt>()->Int64Value();
 }
 
@@ -1318,7 +1318,7 @@ SharedStorageWorkletGlobalScope::StartOperation(
   v8::Isolate* isolate = script_state->GetIsolate();
   v8::HandleScope handle_scope(isolate);
 
-  isolate->SetContinuationPreservedEmbedderData(
+  isolate->SetContinuationPreservedEmbedderDataV2(
       v8::BigInt::New(isolate, operation_id));
 
   if (ShouldDefinePrivateAggregationInSharedStorage()) {
