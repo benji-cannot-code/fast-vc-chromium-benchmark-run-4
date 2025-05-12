50b6c0292607fe6a49f18cc1b360e5c26ef50cbd
Kristi Saney <kristislee@google.com>
2025-05-12 13:34:44 -0700

    [Read aloud] Begin cleanup after pulling read aloud out of app.ts
    This includes:
    - Making private the methods that aren't used publicly
    - Moving tests into VoicePackController test and out of more UI & app
      specific tests
    
    Bug: 346612365
    Change-Id: I77fb462bb1a8dab5c082d57b808d4b501a42bb41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534634
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459064}

--- chrome/browser/resources/side_panel/read_anything/read_aloud/voice_pack_controller.ts
+++ chrome/browser/resources/side_panel/read_anything/read_aloud/voice_pack_controller.ts
@@ -106,7 +106,7 @@ export class VoicePackController {
     const hadAvailableVoices = this.hasAvailableVoices();
     // Get a new list of voices. This should be done before we call
     // updateUnavailableVoiceToDefaultVoice_();
-    this.refreshAvailableVoices(/*forceRefresh=*/ true);
+    this.refreshAvailableVoices_(/*forceRefresh=*/ true);
 
     // TODO: crbug.com/390435037 - Simplify logic around loading voices and
     // language availability, especially around the new TTS engine.
@@ -154,7 +154,6 @@ export class VoicePackController {
 
     const langCodeForVoicePackManager = convertLangOrLocaleForVoicePackManager(
         langOrLocale, this.getEnabledLangs(), this.getAvailableLangs());
-
     if (!langCodeForVoicePackManager) {
       this.autoSwitchVoice_(langOrLocale);
       return;
@@ -181,7 +180,7 @@ export class VoicePackController {
 
     // Enable the preferred locale for this lang if one exists. Otherwise,
     // enable a Google TTS supported locale for this language if one exists.
-    this.refreshAvailableVoices();
+    this.refreshAvailableVoices_();
     const preferredVoice = chrome.readingMode.getStoredVoice();
     const preferredVoiceLang = this.getAvailableVoices()
                                    .find(voice => voice.name === preferredVoice)
@@ -248,7 +247,7 @@ export class VoicePackController {
       return;
     }
 
-    this.refreshAvailableVoices();
+    this.refreshAvailableVoices_();
     const selectedVoice = this.getAvailableVoices().filter(
         voice => voice.name === storedVoiceName);
     const newVoice = (selectedVoice.length && selectedVoice[0]) ?
@@ -323,7 +322,7 @@ export class VoicePackController {
 
     // If the default voice won't work, try another voice in that language.
     const baseLang = this.getCurrentLanguage();
-    this.refreshAvailableVoices();
+    this.refreshAvailableVoices_();
     const voicesForLanguage = this.getAvailableVoices().filter(
         voice => voice.lang.startsWith(baseLang));
 
@@ -354,7 +353,7 @@ export class VoicePackController {
 
   private disableLangIfNoVoices_(lang: string): void {
     const lowerLang = lang.toLowerCase();
-    this.refreshAvailableVoices();
+    this.refreshAvailableVoices_();
     const availableVoicesForLang = this.getAvailableVoicesForLang_(lowerLang);
 
     let disableLang = false;
@@ -425,7 +424,7 @@ export class VoicePackController {
   restoreFromPrefs(): void {
     // We need to make sure the languages we choose correspond to voices, so
     // refresh the list of voices and available langs
-    this.refreshAvailableVoices();
+    this.refreshAvailableVoices_();
     this.setCurrentLanguage(chrome.readingMode.baseLanguageForSpeech);
     const storedLanguagesPref = chrome.readingMode.getLanguagesEnabledInPref();
     const langOfDefaultVoice = this.getDefaultVoice_()?.lang;
@@ -442,7 +441,7 @@ export class VoicePackController {
     this.setUserPreferredVoiceFromPrefs_();
   }
 
-  refreshAvailableVoices(forceRefresh: boolean = false): void {
+  private refreshAvailableVoices_(forceRefresh: boolean = false): void {
     if (!this.hasAvailableVoices() || forceRefresh) {
       const availableVoices = getFilteredVoiceList(this.speech_.getVoices());
       this.setAvailableVoices(availableVoices);
@@ -531,7 +530,7 @@ export class VoicePackController {
         case VoicePackServerStatusSuccessCode.INSTALLED:
           // Force a refresh of the voices list since we might not get an update
           // the voices have changed.
-          this.refreshAvailableVoices(/*forceRefresh=*/ true);
+          this.refreshAvailableVoices_(/*forceRefresh=*/ true);
           this.autoSwitchVoice_(lang);
 
           // Some languages may require a download from the voice pack
@@ -747,7 +746,7 @@ export class VoicePackController {
   }
 
   private getDefaultVoice_(): SpeechSynthesisVoice|null {
-    this.refreshAvailableVoices();
+    this.refreshAvailableVoices_();
     const allPossibleVoices = this.getAvailableVoices();
     const voicesForLanguage = allPossibleVoices.filter(
         voice => voice.lang.startsWith(this.getCurrentLanguage()));
