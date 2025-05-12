be14459557e7cebb142102b57dded9acca3f4d18
David Roger <droger@chromium.org>
2025-05-12 09:36:40 -0700

    [signin] Delete kImprovedSettingsUIOnDesktop feature flag
    Note: the flag was technically not enabled on ChromeOS.
    This feature should not have impact on ChromeOS, because it only touches
    non-syncing UIs, which are never shown on ChromeOS.
    
    I verified that the settings pages are unaffected on ChromeOS,
    including for passphrase errors.
    
    Fixed: 370473765
    Change-Id: I8062bca27f7daeeacef7bc8d695d0c7f3d5c44c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534925
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458900}

--- chrome/browser/ui/webui/settings/people_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/people_handler_unittest.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/base/signin_prefs.h"
-#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
@@ -1930,23 +1929,14 @@ TEST_F(PeopleHandlerSignoutTest, Signout) {
   EXPECT_EQ(2U, identity_manager()->GetAccountsWithRefreshTokens().size());
 
   CreatePeopleHandler();
-
-  if (switches::IsImprovedSettingsUIOnDesktopEnabled()) {
-    EXPECT_FALSE(browser()->signin_view_controller()->ShowsModalDialog());
-  }
+  EXPECT_FALSE(browser()->signin_view_controller()->ShowsModalDialog());
 
   base::Value::List args;
   args.Append(/*value=*/false);
   SimulateSignout(args);
-  if (switches::IsImprovedSettingsUIOnDesktopEnabled()) {
-    // The signout confirmation dialog is shown.
-    EXPECT_TRUE(identity_manager()->HasPrimaryAccount(ConsentLevel::kSignin));
-    EXPECT_TRUE(browser()->signin_view_controller()->ShowsModalDialog());
-  } else {
-    EXPECT_EQ(web_contents()->GetVisibleURL(),
-              GaiaUrls::GetInstance()->LogOutURLWithContinueURL(GURL()));
-    EXPECT_FALSE(identity_manager()->HasPrimaryAccount(ConsentLevel::kSignin));
-  }
+  // The signout confirmation dialog is shown.
+  EXPECT_TRUE(identity_manager()->HasPrimaryAccount(ConsentLevel::kSignin));
+  EXPECT_TRUE(browser()->signin_view_controller()->ShowsModalDialog());
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
--- chrome/browser/ui/webui/settings/sync_settings_interactive_uitest.cc
+++ chrome/browser/ui/webui/settings/sync_settings_interactive_uitest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "chrome/test/interaction/webcontents_interaction_test_util.h"
-#include "components/signin/public/base/signin_switches.h"
 #include "components/sync/test/test_sync_service.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/browser_test.h"
@@ -28,14 +27,7 @@ class SyncSettingsInteractiveTest
     : public SigninBrowserTestBaseT<
           WebUiInteractiveTestMixin<InteractiveBrowserTest>> {
  public:
-  SyncSettingsInteractiveTest() {
-    scoped_feature_list_.InitWithFeatures(
-        /*enabled_features=*/{switches::kImprovedSettingsUIOnDesktop},
-        /*disabled_features=*/{});
-  }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
+  SyncSettingsInteractiveTest() = default;
 };
 
 // TODO(crbug.com/407795729): Fix and re-enable.
