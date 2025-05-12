b5fca19921d7698c194cee551bbb308f1a99aee0
Aishwarya Rajesh <aishwaryarj@google.com>
2025-05-12 10:04:59 -0700

    [TSLO] Fix histogram name for DW heuristics
    When we revisited the heuristics and associated metrics in
    https://crrev.com/c/6021311, we updated the histogram name in
    histograms.xml, but failed to update the histogram name during emission.
    This has resulted in failure of data being logged for the same.
    
    Bug: 416786090
    Change-Id: I1dd3a8546a4682705fc16425688862a4a6fd0ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534319
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458916}

--- chrome/browser/ui/android/desktop_windowing/java/src/org/chromium/chrome/browser/ui/desktop_windowing/AppHeaderUtils.java
+++ chrome/browser/ui/android/desktop_windowing/java/src/org/chromium/chrome/browser/ui/desktop_windowing/AppHeaderUtils.java
@@ -120,7 +120,7 @@ public class AppHeaderUtils {
             @DesktopWindowHeuristicResult int result) {
         assert result != DesktopWindowHeuristicResult.UNKNOWN;
         RecordHistogram.recordEnumeratedHistogram(
-                "Android.DesktopWindowHeuristicResult",
+                "Android.DesktopWindowHeuristicResult2",
                 result,
                 DesktopWindowHeuristicResult.NUM_ENTRIES);
     }
