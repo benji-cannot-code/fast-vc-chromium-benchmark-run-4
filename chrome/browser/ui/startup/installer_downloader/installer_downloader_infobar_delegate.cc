d843d3467c6db1cb2d2f2e48d438559ed1ba78b8
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-05-12 03:24:23 -0700

    [Installer Downloader] Remove delegate dependency on c/b/infobar
    Currently the Installer Downloader Infobar delegate depend on c/b by
    using the ConfirmInfoBarCreator and c/b/ui. Since this code should move
    under c/b, that cause a cycle dependency.
    
    This CL remove the dependency on the helper in the c/b which is not
    needed for win only infobar instantiation.
    
    Change-Id: I3dfd352322d85b37742ac3dbd57164d53ca4506b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530826
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458746}

--- chrome/browser/ui/startup/installer_downloader/installer_downloader_infobar_delegate.cc
+++ chrome/browser/ui/startup/installer_downloader/installer_downloader_infobar_delegate.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/ptr_util.h"
-#include "chrome/browser/infobars/confirm_infobar_creator.h"
+#include "chrome/browser/ui/views/infobars/confirm_infobar.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/infobars/content/content_infobar_manager.h"
@@ -39,7 +39,8 @@ void InstallerDownloaderInfoBarDelegate::Show(
   std::unique_ptr<InstallerDownloaderInfoBarDelegate> delegate =
       std::make_unique<InstallerDownloaderInfoBarDelegate>(
           std::move(accept_cb));
-  infobar_manager->AddInfoBar(CreateConfirmInfoBar(std::move(delegate)));
+  infobar_manager->AddInfoBar(
+      std::make_unique<ConfirmInfoBar>(std::move(delegate)));
 }
 
 InstallerDownloaderInfoBarDelegate::InstallerDownloaderInfoBarDelegate(
