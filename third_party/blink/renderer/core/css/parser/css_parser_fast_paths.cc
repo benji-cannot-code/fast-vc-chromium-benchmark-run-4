ef0ba57fa5ffefbf050e93b997f29f47aa0c905a
Rune Lillesveen <futhark@chromium.org>
2025-05-12 02:29:51 -0700

    Remove CSSLogicalOverflow runtime flag
    The feature has been stable for more than two milestones.
    
    Bug: 41489999
    Change-Id: I7d777cbb11f9b196756cfc6992c043888bef7196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6532245
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458725}

--- third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
@@ -1304,8 +1304,6 @@ bool CSSParserFastPaths::IsValidKeywordPropertyAndValue(
       return value_id == CSSValueID::kNormal ||
              value_id == CSSValueID::kBreakWord ||
              value_id == CSSValueID::kAnywhere;
-    case CSSPropertyID::kInternalOverflowBlock:
-    case CSSPropertyID::kInternalOverflowInline:
     case CSSPropertyID::kOverflowBlock:
     case CSSPropertyID::kOverflowInline:
     case CSSPropertyID::kOverflowX:
@@ -1753,8 +1751,6 @@ CSSBitset CSSParserFastPaths::handled_by_keyword_fast_paths_properties_{{
     CSSPropertyID::kGapRulePaintOrder,
     CSSPropertyID::kHyphens,
     CSSPropertyID::kImageRendering,
-    CSSPropertyID::kInternalOverflowBlock,
-    CSSPropertyID::kInternalOverflowInline,
     CSSPropertyID::kInterpolateSize,
     CSSPropertyID::kListStylePosition,
     CSSPropertyID::kMaskType,
