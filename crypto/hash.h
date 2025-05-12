271cba08f513c1875dacd8addc1a05ed939aaa84
Takuto Ikuta <tikuta@chromium.org>
2025-05-12 07:02:28 -0700

    crypto: Fix missing includes in hash.h
    This is to fix build with Clang modules.
    
    Bug: 40263312
    Change-Id: I479938504d36c7e69c395cac5eacd2786fbb6a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6536486
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458811}

--- crypto/hash.h
+++ crypto/hash.h
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CRYPTO_HASH_H_
 #define CRYPTO_HASH_H_
 
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <string_view>
+
 #include "base/containers/span.h"
 #include "base/notreached.h"
 #include "crypto/crypto_export.h"
