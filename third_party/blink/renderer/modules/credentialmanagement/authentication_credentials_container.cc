ac8987d072f228040d9f7da573f6162426e8137d
Adem Derinel <derinel@google.com>
2025-05-12 07:44:54 -0700

    Add usage counter for immediate credential get method calls.
    The CL adds use counters to track success and failure calls when the
    credential get method is invoked with immediate mediation option.
    
    Bug: 393055190
    Change-Id: Idc44a97817ba1569f02843748a2177e353729199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6532349
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458834}

--- third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
@@ -538,6 +538,8 @@ void OnPreventSilentAccessComplete(
 
 void OnGetComplete(std::unique_ptr<ScopedPromiseResolver> scoped_resolver,
                    RequiredOriginType required_origin_type,
+                   Mediation mediation,
+
                    CredentialManagerError error,
                    CredentialInfoPtr credential_info) {
   auto* resolver =
@@ -546,12 +548,20 @@ void OnGetComplete(std::unique_ptr<ScopedPromiseResolver> scoped_resolver,
   AssertSecurityRequirementsBeforeResponse(resolver, required_origin_type);
   if (error != CredentialManagerError::SUCCESS) {
     DCHECK(!credential_info);
+    if (mediation == Mediation::IMMEDIATE) {
+      UseCounter::Count(resolver->GetExecutionContext(),
+                        WebFeature::kCredentialsGetImmediateMediationFailure);
+    }
     resolver->Reject(CredentialManagerErrorToDOMException(error));
     return;
   }
   DCHECK(credential_info);
   UseCounter::Count(resolver->GetExecutionContext(),
                     WebFeature::kCredentialManagerGetReturnedCredential);
+  if (mediation == Mediation::IMMEDIATE) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialsGetImmediateMediationPasswordSuccess);
+  }
   resolver->Resolve(mojo::ConvertTo<Credential*>(std::move(credential_info)));
 }
 
@@ -767,6 +777,9 @@ void OnGetAssertionComplete(
     if (mediation == Mediation::CONDITIONAL) {
       UseCounter::Count(resolver->GetExecutionContext(),
                         WebFeature::kWebAuthnConditionalUiGetSuccess);
+    } else if (mediation == Mediation::IMMEDIATE) {
+      UseCounter::Count(resolver->GetExecutionContext(),
+                        WebFeature::kCredentialsGetImmediateMediationPublicKeySuccess);
     }
 
     auto* authenticator_response =
@@ -790,6 +803,10 @@ void OnGetAssertionComplete(
         authenticator_response, credential->authenticator_attachment,
         extension_outputs));
     return;
+  }
+  if (mediation == Mediation::IMMEDIATE) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialsGetImmediateMediationFailure);
   }
   DCHECK(!credential);
   AbortSignal* signal =
@@ -827,7 +844,7 @@ void OnAuthenticatorGetCredentialComplete(
   auto password_response =
       std::move(get_credential_response->get_password_response());
   OnGetComplete(std::move(scoped_resolver), RequiredOriginType::kSecure,
-                CredentialManagerError::SUCCESS, std::move(password_response));
+                mediation, CredentialManagerError::SUCCESS, std::move(password_response));
 }
 
 void OnSmsReceive(ScriptPromiseResolver<IDLNullable<Credential>>* resolver,
@@ -1656,7 +1673,7 @@ ScriptPromise<IDLNullable<Credential>> AuthenticationCredentialsContainer::get(
       requirement, options->password(), std::move(providers),
       WTF::BindOnce(&OnGetComplete,
                     std::make_unique<ScopedPromiseResolver>(resolver),
-                    required_origin_type));
+                    required_origin_type, Mediation::MODAL));
 
   return promise;
 }
