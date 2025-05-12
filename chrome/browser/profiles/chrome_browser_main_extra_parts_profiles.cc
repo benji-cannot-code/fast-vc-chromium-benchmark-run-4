54945c3502b36e5fed5885812e1ba6a1ae7b3a8d
Slobodan Pejic <slobodan@chromium.org>
2025-05-12 07:13:01 -0700

    Trigger removal of invalid browser bound keys.
    This change adds an unimplemented service for deleting invalid browser
    bound keys when a profile is initialized on Android.
    
    Browser bound keys are behind the flag
    chrome://flags/#enable-secure-payment-confirmation-browser-bound-key.
    
    Bug: 377278827, 406299749
    Change-Id: I402f7326732676bd2d67399947cd45732242b124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509170
    Auto-Submit: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458814}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -254,6 +254,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/machine_learning_tflite_buildflags.h"
 #include "components/password_manager/content/browser/password_manager_log_router_factory.h"
 #include "components/password_manager/content/browser/password_requirements_service_factory.h"
+#include "components/payments/content/browser_binding/browser_bound_keys_deleter_factory.h"
 #include "components/payments/content/has_enrolled_instrument_query_factory.h"
 #include "components/permissions/features.h"
 #include "components/policy/content/policy_blocklist_service.h"
@@ -1109,6 +1110,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   PasswordReuseManagerFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
   PasswordStatusCheckServiceFactory::GetInstance();
+#endif
+#if BUILDFLAG(IS_ANDROID)
+  payments::BrowserBoundKeyDeleterFactory::GetInstance();
 #endif
   payments::HasEnrolledInstrumentQueryFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
---
+++ components/payments/content/browser_binding/browser_bound_keys_deleter_factory.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PAYMENTS_CONTENT_BROWSER_BINDING_BROWSER_BOUND_KEYS_DELETER_FACTORY_H_
+#define COMPONENTS_PAYMENTS_CONTENT_BROWSER_BINDING_BROWSER_BOUND_KEYS_DELETER_FACTORY_H_
+
+#include "base/no_destructor.h"
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+
+class KeyedService;
+
+namespace content {
+
+class BrowserContext;
+
+}  // namespace content
+
+namespace payments {
+
+// Responsible for creating a service to start the process of finding and
+// deleting invalid browser bound key metadata metadata.
+class BrowserBoundKeyDeleterFactory : public BrowserContextKeyedServiceFactory {
+ public:
+  static BrowserBoundKeyDeleterFactory* GetInstance();
+
+ protected:
+  // BrowserContextKeyedServiceFactory:
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
+      content::BrowserContext* context) const override;
+  bool ServiceIsCreatedWithBrowserContext() const override;
+
+ private:
+  friend base::NoDestructor<BrowserBoundKeyDeleterFactory>;
+  BrowserBoundKeyDeleterFactory();
+  ~BrowserBoundKeyDeleterFactory() override;
+};
+
+}  // namespace payments
+
+#endif  // COMPONENTS_PAYMENTS_CONTENT_BROWSER_BINDING_BROWSER_BOUND_KEYS_DELETER_FACTORY_H_
