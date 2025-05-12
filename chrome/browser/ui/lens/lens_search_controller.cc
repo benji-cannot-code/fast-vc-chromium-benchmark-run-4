d07274dc1505548b2a06ffb85e3c7af9cfec1885
Juan Mojica <juanmojica@google.com>
2025-05-12 16:47:22 -0700

    [lens-rearch][17/n] Add contextualization controller to search controller.
    This also moves `GetPageContextualization` from the overlay controller
    to the contextualization controller. `StartContextualization` and other
    contextual functions will be moved in a follow-up to make them easier to
    review. This change should be a no-op.
    
    Change-Id: I386adaf289774765db622ed3fe048239ca19b4f7
    Bug: 403573362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6526666
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459157}

--- chrome/browser/ui/lens/lens_search_controller.cc
+++ chrome/browser/ui/lens/lens_search_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h"
 #include "chrome/browser/ui/lens/lens_overlay_theme_utils.h"
 #include "chrome/browser/ui/lens/lens_permission_bubble_controller.h"
+#include "chrome/browser/ui/lens/lens_search_contextualization_controller.h"
 #include "chrome/browser/ui/lens/lens_searchbox_controller.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
@@ -83,6 +84,9 @@ void LensSearchController::Initialize(
       CreateLensOverlaySidePanelCoordinator();
 
   lens_searchbox_controller_ = CreateLensSearchboxController();
+
+  lens_contextualization_controller_ =
+      CreateLensSearchContextualizationController();
 
   CreatePageContextEligibilityAPI();
 }
@@ -294,6 +298,12 @@ LensSearchController::page_context_eligibility() {
   return nullptr;
 }
 
+lens::LensSearchContextualizationController*
+LensSearchController::lens_search_contextualization_controller() {
+  CheckInitialized(initialized_);
+  return lens_contextualization_controller_.get();
+}
+
 std::unique_ptr<LensOverlayController>
 LensSearchController::CreateLensOverlayController(
     tabs::TabInterface* tab,
@@ -338,6 +348,11 @@ LensSearchController::CreateLensOverlaySidePanelCoordinator() {
 std::unique_ptr<lens::LensSearchboxController>
 LensSearchController::CreateLensSearchboxController() {
   return std::make_unique<lens::LensSearchboxController>(this);
+}
+
+std::unique_ptr<lens::LensSearchContextualizationController>
+LensSearchController::CreateLensSearchContextualizationController() {
+  return std::make_unique<lens::LensSearchContextualizationController>(this);
 }
 
 void LensSearchController::CreatePageContextEligibilityAPI() {
--- chrome/browser/ui/lens/lens_search_controller.h
+++ chrome/browser/ui/lens/lens_search_controller.h
@@ -29,6 +29,7 @@ class LensOverlayGen204Controller;
 class LensOverlaySidePanelCoordinator;
 class LensPermissionBubbleController;
 class LensSearchboxController;
+class LensSearchContextualizationController;
 }  // namespace lens
 
 namespace variations {
@@ -151,6 +152,10 @@ class LensSearchController {
 
   optimization_guide::PageContextEligibility* page_context_eligibility();
 
+  // Returns the LensSearchContextualizationController.
+  lens::LensSearchContextualizationController*
+  lens_search_contextualization_controller();
+
   // Testing function for setting the page context eligibility API for this
   // controller.
   void set_page_context_eligibility_for_testing(
@@ -198,10 +203,15 @@ class LensSearchController {
   virtual std::unique_ptr<lens::LensOverlaySidePanelCoordinator>
   CreateLensOverlaySidePanelCoordinator();
 
-  // Override these methods to be able to track calls made to the side panel
-  // coordinator.
+  // Override these methods to be able to track calls made to the searchbox
+  // controller.
   virtual std::unique_ptr<lens::LensSearchboxController>
   CreateLensSearchboxController();
+
+  // Override these methods to be able to track calls made to the
+  // contextualization controller.
+  virtual std::unique_ptr<lens::LensSearchContextualizationController>
+  CreateLensSearchContextualizationController();
 
   // Called by the Lens overlay when it has finished opening and has moved to
   // the kOverlay state. This is how this class knows it can move into kActive
@@ -339,6 +349,10 @@ class LensSearchController {
   // TODO(crbug.com/413138792): Hook up this controller to handle searchbox
   // interactions, without a dependency on the overlay controller.
   std::unique_ptr<lens::LensSearchboxController> lens_searchbox_controller_;
+
+  // The contextualization controller for the Lens Search feature on this tab.
+  std::unique_ptr<lens::LensSearchContextualizationController>
+      lens_contextualization_controller_;
 
   // Holds subscriptions for TabInterface callbacks.
   std::vector<base::CallbackListSubscription> tab_subscriptions_;
