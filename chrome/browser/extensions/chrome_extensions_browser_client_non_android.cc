a065c649e1ba6d875302c157cb63efc9cc5391b6
James Cook <jamescook@chromium.org>
2025-05-12 12:30:37 -0700

    extensions: Use ChromeExtensionsBrowserClient on desktop Android, part 1
    Set up the file structure we want in the future:
    * chrome_extensions_browser_client.cc - will contain shared code,
      for now contains the existing Win/Mac/Linux code
    
    * desktop_android_extensions_browser_client.cc, contains our current
      desktop Android stub implementation
    
    * chrome_extensions_browser_client_non_android.cc - the eventual home
      of code we can't share with Android
    
    I did this small initial step to minimize CL size.
    
    Bug: 415033270
    Change-Id: Id5f6322d4c06a37a86cfcfea794245fa67c31415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508172
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459025}

---
+++ chrome/browser/extensions/chrome_extensions_browser_client_non_android.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/chrome_extensions_browser_client.h"
+
+namespace extensions {
+
+// TODO(crbug.com/415033270): Move non-Android code here.
+
+}  // namespace extensions
