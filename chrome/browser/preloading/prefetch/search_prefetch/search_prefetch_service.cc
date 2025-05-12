1d0bc3a4bdca569736151889d23b41e6c8888e14
Lingqi Chi <lingqi@chromium.org>
2025-05-12 07:27:42 -0700

    [searchprefetech]: track duplicate requests within 30ms
    Bug: 413557424
    Change-Id: I73833ab6db3267fdb54193add66aa62c7e22e5b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6535517
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458824}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
+#include "base/trace_event/named_trigger.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
@@ -1288,12 +1289,16 @@ void SearchPrefetchService::RecordInterceptionMetrics(
   }
   auto iter = search_terms_cache_.Get(search_terms);
   if (iter != search_terms_cache_.end()) {
+    base::TimeDelta age = base::Time::Now() - iter->second;
     base::UmaHistogramCustomTimes(
-        "Omnibox.SearchPrefetch.DuplicateSearchTermsAge",
-        base::Time::Now() - iter->second, base::Milliseconds(1),
-        base::Hours(10), 100);
+        "Omnibox.SearchPrefetch.DuplicateSearchTermsAge", age,
+        base::Milliseconds(1), base::Hours(10), 100);
+    if (age < base::Milliseconds(30)) {
+      base::trace_event::EmitNamedTrigger("second-search-request-within30");
+    }
   }
   search_terms_cache_.Put(search_terms, base::Time::Now());
+  base::trace_event::EmitNamedTrigger("first-search-request");
 }
 
 void SearchPrefetchService::
