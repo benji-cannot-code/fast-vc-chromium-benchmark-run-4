e92f358a2a1e3ce84e34c6386c1c02b7d52ccd41
Ari Chivukula <arichiv@chromium.org>
2025-05-12 09:22:59 -0700

    [Clear-Site-Data] Add UKM Counter
    It looks like we never added counters for the header, but we should
    have had some. Additionally, this adds the WebDX mapping from:
    https://github.com/web-platform-dx/web-features/blob/main/features/clear-site-data.yml
    
    No-Try: true
    
    Fixed: 415748354
    Change-Id: Ia70dbc865f4ea2f27246faf54bba03fa0dfea13a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512360
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458890}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -165,6 +165,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/devtools/inspector_issue.mojom-shared.h"
 #include "third_party/blink/public/mojom/private_network_device/private_network_device.mojom.h"
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom.h"
 #include "url/scheme_host_port.h"
 
 #if BUILDFLAG(IS_ANDROID)
@@ -2625,6 +2626,9 @@ void StoragePartitionImpl::OnClearSiteData(
       url_loader_network_observers_.current_context().GetWebContents();
   if (web_contents) {
     weak_web_contents = web_contents->GetWeakPtr();
+    GetContentClient()->browser()->LogWebFeatureForCurrentPage(
+        web_contents->GetPrimaryMainFrame(),
+        blink::mojom::WebFeature::kClearSiteData);
   }
 
   std::optional<blink::StorageKey> storage_key = CalculateStorageKey(
