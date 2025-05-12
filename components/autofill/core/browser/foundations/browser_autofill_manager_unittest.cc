6206add44f0714c172dfbd8f2c704c48b81580df
Norge Vizcay <vizcay@google.com>
2025-05-12 04:01:31 -0700

    Remove unnecessary sync flag check.
    Loyalty cards filling logic in browser autofill manager only needs to be
    guarded by the kAutofillEnableLoyaltyCardsFilling flag. The sync flag is
    unnecessary in this context.
    
    Bug: 404437008
    Change-Id: I99271ea81fcdd395d7294407095b270f857e1e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6536509
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458754}

--- components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
@@ -2050,11 +2050,8 @@ class BrowserAutofillManagerTestValuables : public BrowserAutofillManagerTest {
 };
 
 TEST_F(BrowserAutofillManagerTestValuables, GetSuggestions_LoyaltyCards) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures(
-      {syncer::kSyncAutofillLoyaltyCard,
-       features::kAutofillEnableLoyaltyCardsFilling},
-      {});
+  base::test::ScopedFeatureList scoped_feature_list{
+      features::kAutofillEnableLoyaltyCardsFilling};
 
   SetLoyaltyCards({test::CreateLoyaltyCard()});
 
