a85355b026486b742d2340d0d3ddadbf95c7c31d
Sahir Vellani <sahir.vellani@microsoft.com>
2025-05-13 15:22:52 -0700

    Use WUC Backdrop instead of SystemBackdrop
    DWMWA_SYSTEMBACKDROP_TYPE or DWMWA_USE_IMMERSIVE_DARK_MODE require only
    a top level window. Additionally, DWMWA_SYSTEMBACKDROP_TYPE is only
    available on WIN11_22H2+. To make the experience consistent between all
    platform versions where the redirection bitmap will be removed, as well
    as across top level and child hwnds, only use the WUC Backdrop as the
    window backdrop.
    
    Additionally, this change ensures that the WUC Backdrop only gets added
    when the Mica titlebar is not being used.
    
    Bug: 415385215
    Change-Id: I3760e6563deb6c16c19f3005c246ae0d810bdf4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512200
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1459754}

--- ui/views/views_switches.h
+++ ui/views/views_switches.h
@@ -15,7 +15,6 @@ namespace views::switches {
 VIEWS_EXPORT extern const char
     kDisableInputEventActivationProtectionForTesting[];
 VIEWS_EXPORT extern const char kDrawViewBoundsRects[];
-VIEWS_EXPORT extern const char kUseWUCForWindowBackdrop[];
 VIEWS_EXPORT extern const char kViewStackTraces[];
 
 }  // namespace views::switches
