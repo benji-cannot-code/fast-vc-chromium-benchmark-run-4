a7b219a7ea924311b2adabdec8ca588d1374198d
Ioana Pandele <ioanap@chromium.org>
2025-05-13 02:06:51 -0700

    Fix display name for backup note
    Also set `hide_by_default` to true. This is probably unused at the
    moment, but it would be consistent with the desired behavior.
    
    Bug: 407501242
    Change-Id: Ic551bdb7da722a51467106608064c484a644d366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533529
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459311}

---
+++ components/password_manager/core/browser/password_form_unittest.cc
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/password_form.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace password_manager {
+namespace {
+
+// Unittests for small pieces of logic in `PasswordForm`.
+TEST(PasswordFormTest, PasswordBackupNote) {
+  PasswordForm form;
+  form.SetPasswordBackupNote(u"backuppassword");
+  EXPECT_EQ(form.notes[0].unique_display_name,
+            PasswordNote::kPasswordChangeBackupNoteName);
+  EXPECT_EQ(form.GetPasswordBackupNote(), u"backuppassword");
+}
+
+TEST(PasswordFormTest, RegularNote) {
+  PasswordForm form;
+  form.SetNoteWithEmptyUniqueDisplayName(u"test note");
+  EXPECT_EQ(form.notes[0].unique_display_name, u"");
+  EXPECT_EQ(form.GetNoteWithEmptyUniqueDisplayName(), u"test note");
+}
+
+TEST(PasswordFormTest, MixedNotes) {
+  PasswordForm form;
+  form.SetNoteWithEmptyUniqueDisplayName(u"test note");
+  form.SetPasswordBackupNote(u"backuppassword");
+
+  EXPECT_EQ(form.GetNoteWithEmptyUniqueDisplayName(), u"test note");
+  EXPECT_EQ(form.GetPasswordBackupNote(), u"backuppassword");
+}
+
+TEST(PasswordFormTest, UpdatesExistingNote) {
+  PasswordForm form;
+  form.SetNoteWithEmptyUniqueDisplayName(u"test note");
+  ASSERT_EQ(form.GetNoteWithEmptyUniqueDisplayName(), u"test note");
+
+  form.SetNoteWithEmptyUniqueDisplayName(u"updated note");
+  EXPECT_EQ(form.GetNoteWithEmptyUniqueDisplayName(), u"updated note");
+}
+
+}  // namespace
+
+}  // namespace password_manager
