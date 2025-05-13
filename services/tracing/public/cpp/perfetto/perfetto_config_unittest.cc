432829cd97045eacf93efe9551d13bdb3682c4cd
Etienne Pierre-doray <etiennep@chromium.org>
2025-05-13 09:17:36 -0700

    [tracing] Migrate away from chrome_config_string in field tracing
    This CL stops filling chrome_config_string in field tracing config,
    which is no longer used by any of the data source.
    This uses `event_package_name_filter_enabled` proto field instead
    of IsEventPackageNameFilterEnabled() from trace_config string.
    
    chrome_config_string is still used locally by a few data sources
    - java heap profiler
    - arc/system data source on ChromeOs
    - memory instrumentation
    - kMetaDataSourceName
    
    Change-Id: I26bcd376e854ca06248b455a6126e9bd44cbbee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530108
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459492}

--- services/tracing/public/cpp/perfetto/perfetto_config_unittest.cc
+++ services/tracing/public/cpp/perfetto/perfetto_config_unittest.cc
@@ -36,7 +36,19 @@ class AdaptPerfettoConfigForChromeTest : public ::testing::Test {
     return destination;
   }
 
+  void RemoveChromeConfigString(perfetto::DataSourceConfig* message) {
+    // .gen.h proto doesn't expose a clear method.
+    message->mutable_chrome_config()->set_trace_config("");
+  }
+
+  void RemoveChromeConfigString(perfetto::TraceConfig* mesaage) {
+    for (auto& data_source_config : *mesaage->mutable_data_sources()) {
+      RemoveChromeConfigString(data_source_config.mutable_config());
+    }
+  }
+
   std::string PrintConfigToText(perfetto::TraceConfig message) {
+    RemoveChromeConfigString(&message);
     std::string serialized_message = message.SerializeAsString();
     std::string proto_text = config_loader_.PrintToText(
         "perfetto.protos.TraceConfig", serialized_message);
@@ -48,6 +60,7 @@ class AdaptPerfettoConfigForChromeTest : public ::testing::Test {
     if (!message) {
       return "";
     }
+    RemoveChromeConfigString(&message.value());
     std::string serialized_message = message->SerializeAsString();
     std::string proto_text = config_loader_.PrintToText(
         "perfetto.protos.DataSourceConfig", serialized_message);
@@ -121,21 +134,6 @@ TEST_F(AdaptPerfettoConfigForChromeTest, LegacyTraceEvent) {
   EXPECT_TRUE(AdaptPerfettoConfigForChrome(&perfetto_config));
   EXPECT_EQ(PrintConfigToText(trace_config),
             PrintConfigToText(perfetto_config));
-}
-
-TEST_F(AdaptPerfettoConfigForChromeTest, UnsupportedTrackEvent) {
-  auto perfetto_config = ParsePerfettoConfigFromText(R"pb(
-    data_sources: {
-      config: {
-        name: "org.chromium.trace_event"
-        track_event_config: {
-          enabled_tags: [ "foo" ]
-          disabled_tags: [ "*" ]
-        }
-      }
-    }
-  )pb");
-  EXPECT_FALSE(AdaptPerfettoConfigForChrome(&perfetto_config));
 }
 
 TEST_F(AdaptPerfettoConfigForChromeTest, DisabledCategories) {
