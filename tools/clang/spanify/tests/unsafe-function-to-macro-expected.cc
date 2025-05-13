7a8ceb08148066298926c9da8e61d581a4ff2bda
Yuki Shiino <yukishiino@chromium.org>
2025-05-12 20:01:44 -0700

    spanify: Rewrite unsafe third-party free func calls to helper macro calls
    Rewrites calls to unsafe third-party free functions with calls to helper
    macros.
    
    Example)
      hb_glyph_position_t* pos =
          hb_buffer_get_glyph_positions(&buffer, &length);
    ==>
      base::span<hb_glyph_position_t> pos =
          UNSAFE_HB_BUFFER_GET_GLYPH_POSITIONS(&buffer, &length);
    
    Change-Id: I8a4651f87fa7e9036b0295bc7bfdc68e94d692de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512078
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459207}

--- tools/clang/spanify/tests/unsafe-function-to-macro-expected.cc
+++ tools/clang/spanify/tests/unsafe-function-to-macro-expected.cc
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <tuple>
 
+#include "base/containers/auto_spanification_helper.h"
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "third_party/do_not_rewrite/third_party_api.h"
+
+// ---- Test cases of C++ member function calls ----------------------------
 
 SkBitmap* GetSkBitmap();
 
@@ -16,7 +20,8 @@ void test_no_arg() {
   // Expected rewrite:
   // base::span<uint32_t> image_row =
   //     UNSAFE_SKBITMAP_NOARGFORTESTING(sk_bitmap);
-  uint32_t* image_row = sk_bitmap.NoArgForTesting();  // IN-TEST
+  base::span<uint32_t> image_row =
+      UNSAFE_SKBITMAP_NOARGFORTESTING(sk_bitmap);  // IN-TEST
   std::ignore = image_row[0];
 }
 
@@ -25,7 +30,7 @@ void test_with_args() {
   // Expected rewrite:
   // base::span<uint32_t> image_row =
   //     UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
-  uint32_t* image_row = sk_bitmap.getAddr32(1, 2);
+  base::span<uint32_t> image_row = UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
   std::ignore = image_row[0];
 }
 
@@ -33,7 +38,8 @@ void test_receiver_is_expression() {
   // Expected rewrite:
   // base::span<uint32_t> image_row =
   //     UNSAFE_SKBITMAP_GETADDR32(GetSkBitmap(), 1, 2);
-  uint32_t* image_row = GetSkBitmap()->getAddr32(1, 2);
+  base::span<uint32_t> image_row =
+      UNSAFE_SKBITMAP_GETADDR32(GetSkBitmap(), 1, 2);
   std::ignore = image_row[0];
 }
 
@@ -42,7 +48,7 @@ void test_receiver_is_pointer() {
   // Expected rewrite:
   // base::span<uint32_t> image_row =
   //     UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
-  uint32_t* image_row = sk_bitmap->getAddr32(1, 2);
+  base::span<uint32_t> image_row = UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
   std::ignore = image_row[0];
 }
 
@@ -51,7 +57,7 @@ void test_receiver_is_reference() {
   // Expected rewrite:
   // base::span<uint32_t> image_row =
   //     UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
-  uint32_t* image_row = sk_bitmap.getAddr32(1, 2);
+  base::span<uint32_t> image_row = UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
   std::ignore = image_row[0];
 }
 
@@ -60,7 +66,7 @@ void test_receiver_is_std_unique_ptr() {
   // Expected rewrite:
   // base::span<uint32_t> image_row =
   //     UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
-  uint32_t* image_row = sk_bitmap->getAddr32(1, 2);
+  base::span<uint32_t> image_row = UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
   std::ignore = image_row[0];
 }
 
@@ -69,6 +75,19 @@ void test_receiver_is_base_raw_ptr() {
   // Expected rewrite:
   // base::span<uint32_t> image_row =
   //     UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
-  uint32_t* image_row = sk_bitmap->getAddr32(1, 2);
+  base::span<uint32_t> image_row = UNSAFE_SKBITMAP_GETADDR32(sk_bitmap, 1, 2);
   std::ignore = image_row[0];
+}
+
+// ---- Test cases of C/C++ free function calls ----------------------------
+
+void test_hb_buffer_get_glyph_positions() {
+  struct hb_buffer_t buffer;
+  unsigned int length;
+  // Expected rewrite:
+  // base::span<struct hb_glyph_position_t> positions =
+  //     UNSAFE_HB_BUFFER_GET_GLYPH_POSITIONS(&buffer, &length);
+  base::span<struct hb_glyph_position_t> positions =
+      UNSAFE_HB_BUFFER_GET_GLYPH_POSITIONS(&buffer, &length);
+  std::ignore = positions[0];
 }
