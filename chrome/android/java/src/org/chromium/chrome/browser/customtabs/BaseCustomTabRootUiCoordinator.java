0b01e901470d31a02d4ad6ba281a04208ed3d26c
Mohamed Adel <adelm@google.com>
2025-05-13 09:46:13 -0700

    Gatekeep Open in Browser button in the toolbar behind intent
    This changes uses CustomTabsIntent.EXTRA_ENABLE_OPEN_IN_BROWSER_BUTTON
    to determine whether the button should be added to the toolbar or not.
    
    Bug: 414824099
    Change-Id: Ida77895343bea115ab0183d2adde3ffc6e9f22b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533557
    Commit-Queue: Moe Adel <adelm@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459517}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
@@ -21,6 +21,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.browser.customtabs.ExperimentalOpenInBrowser;
 
 import org.chromium.base.Callback;
 import org.chromium.base.FeatureList;
@@ -482,13 +483,14 @@ public class BaseCustomTabRootUiCoordinator extends RootUiCoordinator {
         }
     }
 
+    @ExperimentalOpenInBrowser
     @Override
     protected AdaptiveToolbarBehavior createAdaptiveToolbarBehavior(
             Supplier<Tracker> trackerSupplier) {
         return new CustomTabAdaptiveToolbarBehavior(
                 mActivity,
                 mActivityTabProvider,
-                mIntentDataProvider.get().getCustomButtonsOnToolbar(),
+                mIntentDataProvider.get(),
                 AppCompatResources.getDrawable(mActivity, R.drawable.ic_open_in_new_white_24dp),
                 mOpenInBrowserRunnable,
                 () -> addVoiceSearchAdaptiveButton(trackerSupplier));
