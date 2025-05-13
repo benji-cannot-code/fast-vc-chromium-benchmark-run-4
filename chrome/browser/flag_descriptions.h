ac1077b95a3a0f669a34343cba7422ec9d27f26f
Anton Bershanskyi <bershanskyi@gmail.com>
2025-05-12 23:27:30 -0700

    [Client Hints] Flag and config for reduced Linux platform version
    Per https://github.com/WICG/ua-client-hints/commit/56df868c6c3564461b4c0a42ea603b80fa94c8d2
    
    Add a flag and testing config for enabling reduction (setting to empty string)
    platform version on Linux (both the HTTP header Sec-CH-UA-Platform-Version and
    the UserAgentData JS API). This flag is limited to Linux.
    
    Bug: 40245146
    Change-Id: Ib51a2d3dfe14bb11dfd8ef87a208b9d554cfb32b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508794
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1459252}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2579,6 +2579,11 @@ extern const char kReduceAcceptLanguageDescription[];
 extern const char kReduceTransferSizeUpdatedIPCName[];
 extern const char kReduceTransferSizeUpdatedIPCDescription[];
 
+#if BUILDFLAG(IS_LINUX)
+extern const char kReduceUserAgentDataLinuxPlatformVersionName[];
+extern const char kReduceUserAgentDataLinuxPlatformVersionDescription[];
+#endif  // #if BUILDFLAG(IS_LINUX)
+
 extern const char kResetAudioSelectionImprovementPrefName[];
 extern const char kResetAudioSelectionImprovementPrefDescription[];
 
