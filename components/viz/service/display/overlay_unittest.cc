9ce1e1a7d8d5b867b17896fd808b944452a0e33f
Colin Blundell <blundell@chromium.org>
2025-05-12 23:55:29 -0700

    [Viz] Remove setting of premultiplied_alpha in overlay_unittest.cc
    It's always set to true.
    
    Bug: 410591523
    Change-Id: Ife1119c6cb2d65b67afebda17a618187c6cbdc81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533132
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459255}

--- components/viz/service/display/overlay_unittest.cc
+++ components/viz/service/display/overlay_unittest.cc
@@ -737,7 +737,6 @@ class OverlayTest : public testing::Test {
       AggregatedRenderPass* render_pass,
       const gfx::Rect& rect,
       bool needs_blending,
-      bool premultiplied_alpha,
       bool low_latency_rendering,
       gfx::ProtectedVideoType protected_video_type,
       SharedImageFormat format,
@@ -759,7 +758,6 @@ class OverlayTest : public testing::Test {
                          resource_id, kUVTopLeft, kUVBottomRight,
                          SkColors::kTransparent, nearest_neighbor,
                          /*secure_output=*/false, protected_video_type);
-    overlay_quad->premultiplied_alpha = premultiplied_alpha;
 
     return overlay_quad;
   }
@@ -773,12 +771,11 @@ class OverlayTest : public testing::Test {
       const gfx::Size& resource_size_in_pixels,
       SurfaceId test_surface_id = SurfaceId()) {
     bool needs_blending = false;
-    bool premultiplied_alpha = true;
     bool low_latency_rendering = false;
-    return CreateCandidateQuadAt(
-        shared_quad_state, render_pass, rect, needs_blending,
-        premultiplied_alpha, low_latency_rendering, protected_video_type,
-        format, resource_size_in_pixels, test_surface_id);
+    return CreateCandidateQuadAt(shared_quad_state, render_pass, rect,
+                                 needs_blending, low_latency_rendering,
+                                 protected_video_type, format,
+                                 resource_size_in_pixels, test_surface_id);
   }
 
   TextureDrawQuad* CreateCandidateQuadAt(
@@ -789,13 +786,12 @@ class OverlayTest : public testing::Test {
       SharedImageFormat format,
       SurfaceId test_surface_id = SurfaceId()) {
     bool needs_blending = false;
-    bool premultiplied_alpha = true;
     bool low_latency_rendering = false;
     gfx::Size resource_size_in_pixels = rect.size();
-    return CreateCandidateQuadAt(
-        shared_quad_state, render_pass, rect, needs_blending,
-        premultiplied_alpha, low_latency_rendering, protected_video_type,
-        format, resource_size_in_pixels, test_surface_id);
+    return CreateCandidateQuadAt(shared_quad_state, render_pass, rect,
+                                 needs_blending, low_latency_rendering,
+                                 protected_video_type, format,
+                                 resource_size_in_pixels, test_surface_id);
   }
 
   TextureDrawQuad* CreateCandidateQuadAt(
@@ -814,17 +810,16 @@ class OverlayTest : public testing::Test {
       const gfx::Rect& rect,
       SurfaceId test_surface_id = SurfaceId()) {
     bool needs_blending = false;
-    bool premultiplied_alpha = true;
     bool low_latency_rendering = true;
     gfx::Size resource_size_in_pixels = rect.size();
     gfx::ProtectedVideoType protected_video_type =
         gfx::ProtectedVideoType::kClear;
     SharedImageFormat format = SinglePlaneFormat::kRGBA_8888;
 
-    return CreateCandidateQuadAt(
-        shared_quad_state, render_pass, rect, needs_blending,
-        premultiplied_alpha, low_latency_rendering, protected_video_type,
-        format, resource_size_in_pixels, test_surface_id);
+    return CreateCandidateQuadAt(shared_quad_state, render_pass, rect,
+                                 needs_blending, low_latency_rendering,
+                                 protected_video_type, format,
+                                 resource_size_in_pixels, test_surface_id);
   }
 
   TextureDrawQuad* CreateTransparentCandidateQuadAt(
@@ -832,15 +827,13 @@ class OverlayTest : public testing::Test {
       AggregatedRenderPass* render_pass,
       const gfx::Rect& rect) {
     bool needs_blending = true;
-    bool premultiplied_alpha = true;
     bool low_latency_rendering = false;
     gfx::Size resource_size_in_pixels = rect.size();
 
     return CreateCandidateQuadAt(
         shared_quad_state, render_pass, rect, needs_blending,
-        premultiplied_alpha, low_latency_rendering,
-        gfx::ProtectedVideoType::kClear, SinglePlaneFormat::kRGBA_8888,
-        resource_size_in_pixels);
+        low_latency_rendering, gfx::ProtectedVideoType::kClear,
+        SinglePlaneFormat::kRGBA_8888, resource_size_in_pixels);
   }
 
   TextureDrawQuad* CreateFullscreenCandidateQuad(
@@ -856,15 +849,13 @@ class OverlayTest : public testing::Test {
       const gfx::Rect& rect,
       const RoundedDisplayMasksInfo& rounded_display_masks_info) {
     bool needs_blending = true;
-    bool premultiplied_alpha = true;
     bool low_latency_rendering = false;
     gfx::Size resource_size_in_pixels;
 
     auto* overlay_quad = CreateCandidateQuadAt(
         shared_quad_state, render_pass, rect, needs_blending,
-        premultiplied_alpha, low_latency_rendering,
-        gfx::ProtectedVideoType::kClear, SinglePlaneFormat::kRGBA_8888,
-        resource_size_in_pixels);
+        low_latency_rendering, gfx::ProtectedVideoType::kClear,
+        SinglePlaneFormat::kRGBA_8888, resource_size_in_pixels);
 
     overlay_quad->rounded_display_masks_info = rounded_display_masks_info;
 
