a387e0350a6d594aee923a0b9dbfce8728759810
Andrea Gomez <andreaxg@google.com>
2025-05-13 10:22:36 -0700

    Delete tests disabled because of always detached.
    When kGlicAlwaysDetached was made default enabled, tests related to
    attachment were disabled. This CL deletes them.
    
    Bug: 406528268
    Change-Id: I4e725fef76a1114a253aa6f6f3de215aa17afd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534652
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459548}

--- chrome/test/data/webui/glic/api_test.ts
+++ chrome/test/data/webui/glic/api_test.ts
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // ash/webui/personalization_app/tools/gen_tsconfig.py --root_out_dir out/pc \
 //   --gn_target chrome/test/data/webui/glic:build_ts
 
-import {PanelStateKind, ScrollToErrorReason, WebClientMode} from '/glic/glic_api/glic_api.js';
+import {ScrollToErrorReason, WebClientMode} from '/glic/glic_api/glic_api.js';
 import type {GlicBrowserHost, GlicHostRegistry, GlicWebClient, Observable, OpenPanelInfo, PanelOpeningData, ScrollToError, Subscriber} from '/glic/glic_api/glic_api.js';
 import {ObservableValue} from '/glic/observable.js';
 
@@ -249,41 +249,6 @@ class ApiTests extends ApiTestFixtureBase {
     await waitFor(closedPromise.promise);
   }
 
-  async testAttachPanel() {
-    // Test starts in attached mode.
-    await this.waitForPanelState(PanelStateKind.DETACHED);
-    assertTrue(!!this.host.attachPanel);
-    this.host.attachPanel();
-    await this.waitForPanelState(PanelStateKind.ATTACHED);
-  }
-
-  async testDetachPanel() {
-    // Test starts in detached mode.
-    await this.waitForPanelState(PanelStateKind.ATTACHED);
-    assertTrue(!!this.host.detachPanel);
-    this.host.detachPanel();
-    await this.waitForPanelState(PanelStateKind.DETACHED);
-  }
-
-  // Verify that unsubscribing from an observable prevents future updates.
-  async testUnsubscribeFromObservable() {
-    await this.waitForPanelState(PanelStateKind.DETACHED);
-    const panelStateSequence1 = observeSequence(this.host.getPanelState!());
-    const panelStateSequence2 = observeSequence(this.host.getPanelState!());
-    assertEquals(
-        PanelStateKind.DETACHED, (await panelStateSequence1.next()).kind);
-    assertEquals(
-        PanelStateKind.DETACHED, (await panelStateSequence2.next()).kind);
-    panelStateSequence2.unsubscribe();
-    this.host.attachPanel!();
-    assertEquals(
-        PanelStateKind.ATTACHED, (await panelStateSequence1.next()).kind);
-    assertEquals('no-change', await Promise.race([
-      panelStateSequence2.next().then(state => state.kind),
-      sleep(100).then(() => 'no-change'),
-    ]));
-  }
-
   async testShowProfilePicker() {
     assertTrue(!!this.host.showProfilePicker);
     this.host.showProfilePicker();
@@ -301,14 +266,6 @@ class ApiTests extends ApiTestFixtureBase {
     assertTrue(await activeSequence.next());
     await this.advanceToNextStep();
     assertFalse(await activeSequence.next());
-  }
-
-  async testCanAttachPanel() {
-    assertTrue(!!this.host.canAttachPanel);
-    const canAttach = observeSequence(this.host.canAttachPanel());
-    // When subscribing to this value, an initial update is guaranteed to be
-    // emited.
-    assertTrue(await canAttach.next());
   }
 
   async testIsBrowserOpen() {
@@ -865,13 +822,6 @@ class ApiTests extends ApiTestFixtureBase {
     // Second time:
     assertEquals(runCount, 1);
     assertEquals(url.pathname, '/glic/test.html');
-  }
-
-
-  private async waitForPanelState(kind: PanelStateKind): Promise<void> {
-    assertTrue(!!this.host.getPanelState);
-    await observeSequence(this.host.getPanelState())
-        .waitFor(s => s.kind === kind);
   }
 
   private async assertCreateTabWithUnsupportedSchemeFails(url: string) {
