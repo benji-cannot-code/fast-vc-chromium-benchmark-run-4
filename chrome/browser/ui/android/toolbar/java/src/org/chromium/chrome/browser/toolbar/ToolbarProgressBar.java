bb4bb021fde26c289cba3350b2936b5cf00bac53
Gazal Agarwal <agazal@google.com>
2025-05-13 14:13:43 -0700

    Progress bar update background track color
    NO_IFTTT=unrelated change
    
    Bug: 409407482 Test: Change theme and open a variety of web pages.
    Ensure the progress bar background is updated with the Feature flag
    enabled.
    
    Change-Id: I44740b44036b5656edba7559b061de06d5e7ae9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527207
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459714}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarProgressBar.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarProgressBar.java
@@ -25,6 +25,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.theme.ThemeUtils;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.ClipDrawableProgressBar;
@@ -175,12 +176,12 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar
         // This tells accessibility services that progress bar changes are important enough to
         // announce to the user even when not focused.
         ViewCompat.setAccessibilityLiveRegion(this, ViewCompat.ACCESSIBILITY_LIVE_REGION_POLITE);
-        setForegroundOrThemeColor();
+        setProgressBarColors();
     }
 
     public void setAnimatingView(ToolbarProgressBarAnimatingView animatingView) {
         mAnimatingView = animatingView;
-        setForegroundOrThemeColor();
+        setProgressBarColors();
     }
 
     /**
@@ -362,7 +363,7 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar
         if (MathUtils.areFloatsEqual(progress, 1.0f) || progress > 1.0f) finish(true);
     }
 
-    private void setForegroundOrThemeColor() {
+    private void setProgressBarColors() {
         if (mThemeColor != 0) {
             setThemeColor(mThemeColor, false);
         } else {
@@ -389,7 +390,11 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar
         // The default toolbar has specific colors to use.
         if ((isDefaultTheme || ColorUtils.isThemeColorTooBright(color)) && !isIncognito) {
             setForegroundColor(SemanticColorUtils.getProgressBarForeground(getContext()));
-            setBackgroundColor(getContext().getColor(R.color.progress_bar_bg_color_list));
+            if (ChromeFeatureList.sAndroidProgressBarVisualUpdate.isEnabled()) {
+                setBackgroundColor(SemanticColorUtils.getProgressBarTrackColor(getContext()));
+            } else {
+                setBackgroundColor(getContext().getColor(R.color.progress_bar_bg_color_list));
+            }
             return;
         }
 
