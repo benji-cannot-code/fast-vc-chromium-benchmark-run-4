007b173f86688a171ffacf251ce1d9c640cfe677
Tomasz Jurkiewicz <tju@google.com>
2025-05-13 05:38:46 -0700

    Supervised User Metrics Service is observer of Supervised User Service
    The metrics service is merely an observer to changes in the main
    service. This change simplifies the architecture of the supervised user
    stack by condensing the metrics logic in a dedicated service.
    
    Minor: removes test-only interfaces in favour of direct values, which
    are recommended (tott/746, tott/741).
    
    Change-Id: I4368af428df661efad4a10eef8d13a4d93887d52
    Bug: 416249869, 413654322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534165
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459376}

--- chrome/browser/supervised_user/linux_mac_windows/supervised_user_extensions_metrics_delegate_impl_unittest.cc
+++ chrome/browser/supervised_user/linux_mac_windows/supervised_user_extensions_metrics_delegate_impl_unittest.cc
@@ -51,7 +51,8 @@ class SupervisedUserExtensionsMetricsDelegateImplTest
 
     supervised_user_metrics_service_ =
         std::make_unique<supervised_user::SupervisedUserMetricsService>(
-            profile()->GetPrefs(), GetURLFilter(),
+            profile()->GetPrefs(),
+            *SupervisedUserServiceFactory::GetForProfile(profile_.get()),
             std::make_unique<SupervisedUserExtensionsMetricsDelegateImpl>(
                 extensions::ExtensionRegistry::Get(profile()), profile()));
     CHECK(supervised_user_metrics_service_);
@@ -67,12 +68,6 @@ class SupervisedUserExtensionsMetricsDelegateImplTest
   int GetDayIdPref() {
     return profile()->GetPrefs()->GetInteger(
         prefs::kSupervisedUserMetricsDayId);
-  }
-
-  supervised_user::SupervisedUserURLFilter* GetURLFilter() {
-    supervised_user::SupervisedUserService* service =
-        SupervisedUserServiceFactory::GetForProfile(profile_.get());
-    return service->GetURLFilter();
   }
 
   base::HistogramTester histogram_tester_;
--- components/supervised_user/core/browser/supervised_user_metrics_service_unittest.cc
+++ components/supervised_user/core/browser/supervised_user_metrics_service_unittest.cc
@@ -29,6 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace supervised_user {
 
+namespace {
+constexpr char kWebFilterTypeHistogramName[] = "FamilyUser.WebFilterType";
+constexpr char kManagedSiteListHistogramName[] = "FamilyUser.ManagedSiteList";
+constexpr char kApprovedSitesCountHistogramName[] =
+    "FamilyUser.ManagedSiteListCount.Approved";
+constexpr char kBlockedSitesCountHistogramName[] =
+    "FamilyUser.ManagedSiteListCount.Blocked";
+
 class MockSupervisedUserMetricsServiceExtensionDelegateImpl
     : public SupervisedUserMetricsService::
           SupervisedUserMetricsServiceExtensionDelegate {
@@ -70,7 +78,7 @@ class SupervisedUserMetricsServiceTest : public testing::Test {
   void CreateMetricsService() {
     supervised_user_metrics_service_ =
         std::make_unique<SupervisedUserMetricsService>(
-            &pref_service_, GetURLFilter(),
+            &pref_service_, *supervised_user_service_,
             std::make_unique<
                 MockSupervisedUserMetricsServiceExtensionDelegateImpl>());
   }
@@ -141,12 +149,10 @@ TEST_F(SupervisedUserMetricsServiceTest,
        MetricsNotRecordedForSignedOutSupervisedUser) {
   DisableParentalControls(pref_service_);
   CreateMetricsService();
-  histogram_tester_.ExpectTotalCount(
-      SupervisedUserURLFilter::GetWebFilterTypeHistogramNameForTest(),
-      /*expected_count=*/0);
-  histogram_tester_.ExpectTotalCount(
-      SupervisedUserURLFilter::GetManagedSiteListHistogramNameForTest(),
-      /*expected_count=*/0);
+  histogram_tester_.ExpectTotalCount(kWebFilterTypeHistogramName,
+                                     /*expected_count=*/0);
+  histogram_tester_.ExpectTotalCount(kManagedSiteListHistogramName,
+                                     /*expected_count=*/0);
 }
 
 // Tests that default metrics are recorded for supervised users whose parent has
@@ -156,22 +162,22 @@ TEST_F(SupervisedUserMetricsServiceTest, RecordDefaultMetrics) {
   // should be subject to default mature sites blocking.
   EnableParentalControls(pref_service_);
   CreateMetricsService();
+  histogram_tester_.ExpectUniqueSample(kWebFilterTypeHistogramName,
+                                       /*sample=*/
+                                       WebFilterType::kTryToBlockMatureSites,
+                                       /*expected_bucket_count=*/1);
   histogram_tester_.ExpectUniqueSample(
-      SupervisedUserURLFilter::GetWebFilterTypeHistogramNameForTest(),
-      /*sample=*/
-      WebFilterType::kTryToBlockMatureSites,
-      /*expected_bucket_count=*/1);
-  histogram_tester_.ExpectUniqueSample(
-      SupervisedUserURLFilter::GetManagedSiteListHistogramNameForTest(),
+      kManagedSiteListHistogramName,
       /*sample=*/
       SupervisedUserURLFilter::ManagedSiteList::kEmpty,
       /*expected_bucket_count=*/1);
-  histogram_tester_.ExpectUniqueSample(
-      SupervisedUserURLFilter::GetApprovedSitesCountHistogramNameForTest(),
-      /*sample=*/0, /*expected_bucket_count=*/1);
-  histogram_tester_.ExpectUniqueSample(
-      SupervisedUserURLFilter::GetBlockedSitesCountHistogramNameForTest(),
-      /*sample=*/0, /*expected_bucket_count=*/1);
+  histogram_tester_.ExpectUniqueSample(kApprovedSitesCountHistogramName,
+                                       /*sample=*/0,
+                                       /*expected_bucket_count=*/1);
+  histogram_tester_.ExpectUniqueSample(kBlockedSitesCountHistogramName,
+                                       /*sample=*/0,
+                                       /*expected_bucket_count=*/1);
 }
 
+}  // namespace
 }  // namespace supervised_user
