4634fcce175c08c3e1f039f3c427e1157111bde7
Dana Fried <dfried@chromium.org>
2025-05-13 08:30:44 -0700

    [Tab Features] Create base class for webcontents-aware features
    As we move WebContents helpers to Tab Features for a more consistent
    tab API, many of them will still want to track the current WebContents
    in the tab. This provides a common base class to listen to the current
    WebContents which handles the ability of a tab to discard and replace
    those WebContents.
    
    It will be used to implement the migration described in the bug below.
    
    Bug: 401033983
    Change-Id: I3e7b2d4c7f75e1d076e84e53dd2efd8fc3713905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6541066
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459466}

---
+++ chrome/browser/ui/tabs/contents_observing_tab_feature.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TABS_CONTENTS_OBSERVING_TAB_FEATURE_H_
+#define CHROME_BROWSER_UI_TABS_CONTENTS_OBSERVING_TAB_FEATURE_H_
+
+#include "base/callback_list.h"
+#include "chrome/browser/ui/tabs/public/tab_features.h"
+#include "components/tabs/public/tab_interface.h"
+#include "content/public/browser/web_contents_observer.h"
+
+namespace tabs {
+
+// Base class for tab features that need to track the state of the WebContents
+// in the tab, possibly across discards. This is likely to be a common pattern
+// among tab helpers that care about the tab's WebContents, so is provided as a
+// convenience.
+class ContentsObservingTabFeature : public content::WebContentsObserver {
+ public:
+  explicit ContentsObservingTabFeature(TabInterface& tab);
+  ~ContentsObservingTabFeature() override;
+
+  TabInterface& tab() { return *tab_; }
+  const TabInterface& tab() const { return *tab_; }
+
+ protected:
+  // This is called when the tab contents are discarded; the `new_contents` will
+  // be observed after this point.
+  virtual void OnDiscardContents(TabInterface* tab,
+                                 content::WebContents* old_contents,
+                                 content::WebContents* new_contents);
+
+ private:
+  const raw_ref<TabInterface> tab_;
+  base::CallbackListSubscription tab_subscription_;
+};
+
+}  // namespace tabs
+
+#endif  // CHROME_BROWSER_UI_TABS_CONTENTS_OBSERVING_TAB_FEATURE_H_
