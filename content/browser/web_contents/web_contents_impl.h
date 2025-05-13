4d0177931002b9611e99371923191f89e40adb9e
Greg Thompson <grt@chromium.org>
2025-05-13 09:46:03 -0700

    [A11yPerformance] ProgressiveAccessibility phase two
    Implement the "disable_on_hide" variant of ProgressiveAccessibility.
    
    - Move the ProgressiveAccessibility feature into
      BrowserAccessibilityStateImpl, removing the AccessibilityModePolicy
      interface and its implementations.
    - WebContentsImpl now calls into BrowserAccessibilityStateImpl when a
      WebContents is revealed (un-hidden) or hidden. The former is done
      before the visibility change is sent to renderers so that they have
      the correct mode flags before they start painting. The latter is done
      after the visibility change.
    - When the ProgressiveAccessibility feature is enabled, mode flag
      changes are not sent to WebContentses that are hidden unless
      accessibility is being turned off. A WebContents is given its latest
      mode flags when revealed.
    - When the "progressive_accessibility_mode" param is "disable_on_hide"
      and no active screen reader has been detected, accessibility is
      disabled for a WebContents when it is not one of the last five to be
      hidden and it has been hidden for at least five minutes.
    
    AX-Relnotes: n/a.
    Bug: 40925658
    Change-Id: I5c422ba0a003c8b8d25fb63e4bfd9b8a3085c75e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519917
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459516}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -138,7 +138,6 @@ struct AXUpdatesAndEvents;
 }
 
 namespace content {
-class AccessibilityModePolicy;
 class JavaScriptDialogDismissNotifier;
 enum class PictureInPictureResult;
 class BeforeUnloadBlockingDelegate;  // content_browser_test_utils_internal.h
@@ -371,8 +370,7 @@ class CONTENT_EXPORT WebContentsImpl
 
   // Sets the accessibility mode if this WebContents will potentially be
   // user-visible, and broadcasts it to all of its frames if it differs from the
-  // previous mode. Application of the new mode may be deferred to a later time
-  // by the AccessibilityModePolicy.
+  // previous mode.
   void SetAccessibilityMode(ui::AXMode mode);
 
   // Inform the WebContentsImpl object that a write-access Captured Surface
@@ -1874,11 +1872,6 @@ class CONTENT_EXPORT WebContentsImpl
   void AddObserver(WebContentsObserver* observer);
   void RemoveObserver(WebContentsObserver* observer);
 
-  // Sets the accessibility mode if this WebContents will potentially be
-  // user-visible, and broadcasts it to all of its frames if it differs from the
-  // previous mode.
-  void SetAccessibilityModeImpl(ui::AXMode accessibility_mode);
-
   // Indicates whether this tab should be considered crashed. The setter will
   // also notify the delegate when the flag is changed.
   void SetPrimaryMainFrameProcessStatus(base::TerminationStatus status,
@@ -2503,8 +2496,6 @@ class CONTENT_EXPORT WebContentsImpl
       color_chooser_factory_receivers_;
 
   std::unique_ptr<ScreenOrientationProvider> screen_orientation_provider_;
-
-  std::unique_ptr<AccessibilityModePolicy> accessibility_mode_policy_;
 
   // The accessibility mode for all frames. This is queried when each frame
   // is created, and broadcast to all frames when it changes.
