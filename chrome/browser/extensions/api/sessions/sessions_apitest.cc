210c7e67f64b3aac07537f195cf607e98272f34e
Thomas Lukaszewicz <tluk@chromium.org>
2025-05-13 13:11:44 -0700

    [browser] Update RestoreNonEditableTabstrip to use real Browser
    Currently ExtensionSessionsTest.RestoreNonEditableTabstrip creates a
    semi-initialized Browser by instantiating a TestBrowserWindow (which
    skips initialization of the BrowserView and BrowserWidget). This
    creates a Browser instance that is not fully representative of a real
    browser and improperly initializes the test instance.
    
    As part of the effort to deprecate TestBrowserInstances in such cases
    this CL updates this test to use a fully-initialized Browser
    instance.
    
    Bug: 413168662
    Change-Id: I333d7906ab0de44cc30d49129f57aa84f234dcb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6535002
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459673}

--- chrome/browser/extensions/api/sessions/sessions_apitest.cc
+++ chrome/browser/extensions/api/sessions/sessions_apitest.cc
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/session_sync_service_factory.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -340,19 +342,16 @@ IN_PROC_BROWSER_TEST_F(ExtensionSessionsTest, RestoreNonEditableTabstrip) {
 
   // Set up a browser with a non-editable tabstrip, simulating one in the midst
   // of a tab dragging session.
-  std::unique_ptr<TestBrowserWindow> browser_window =
-      std::make_unique<TestBrowserWindow>();
-  Browser::CreateParams params(browser()->profile(), true);
-  params.type = Browser::TYPE_NORMAL;
-  params.window = browser_window.get();
-  std::unique_ptr<Browser> browser =
-      std::unique_ptr<Browser>(Browser::Create(params));
-  browser_window->SetIsTabStripEditable(false);
+  Browser* non_editable_browser =
+      Browser::Create(Browser::CreateParams(browser()->profile(), true));
+  non_editable_browser->GetBrowserView()
+      .tabstrip()
+      ->SetTabStripNotEditableForTesting();
 
   EXPECT_TRUE(base::MatchPattern(
       utils::RunFunctionAndReturnError(
           CreateFunction<SessionsRestoreFunction>(true).get(), "[\"1\"]",
-          browser->profile()),
+          non_editable_browser->profile()),
       ExtensionTabUtil::kTabStripNotEditableError));
 }
 
