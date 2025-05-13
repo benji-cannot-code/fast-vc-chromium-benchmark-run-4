ac448e6e3d0858150edaeea86e6598be512a520a
Eshwar Stalin <estalin@chromium.org>
2025-05-13 15:43:01 -0700

    Fixing duplicate menu items in TabGroups sub-menus
    This CL fixes a bug where existing tab group submenus would be appended
    to when viewed multiple times while the app menu is open. This happens
    because the view hierarchy is not cleared when a specific groups submenu
    is hidden. Doing so causes a UAF (See crbug.com/391709147).
    
    To remedy this, we now cache and reuse each groups submenu when the
    mouse hovers over a group. We also keep track of the most recently
    visited group id to keep track of which TabModel is visible.
    
    
    Change-Id: Ibd9bf7e59b156d4f2cafadb33dac28571b95836d
    Bug: 415793647, 391709147, 416815123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6539847
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459764}

--- chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_everything_menu.h
+++ chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_everything_menu.h
@@ -68,11 +68,11 @@ class STGEverythingMenu : public views::MenuDelegate,
 
  private:
   class AppMenuSubMenuModelDelegate;
-  friend class STGEverythingMenuUnitTest;
 
   int GenerateTabGroupCommandID(int idx_in_sorted_tab_groups);
   base::Uuid GetTabGroupIdFromCommandId(int command_id);
-  std::unique_ptr<ui::SimpleMenuModel> CreateMenuModel();
+  std::unique_ptr<ui::SimpleMenuModel> CreateMenuModel(
+      TabGroupSyncService* tab_group_service);
 
   // Returns sorted saved tab groups with the most recently created as the
   // first, filtering out empty groups.
@@ -126,8 +126,9 @@ class STGEverythingMenu : public views::MenuDelegate,
   std::unique_ptr<ui::SimpleMenuModel> groups_model_;
 
   std::unique_ptr<views::MenuRunner> context_menu_runner_;
-  std::unique_ptr<STGTabsMenuModel> tabs_model_;
+  std::map<base::Uuid, std::unique_ptr<STGTabsMenuModel>> tabs_models_;
   std::unique_ptr<AppMenuSubMenuModelDelegate> submenu_delegate_;
+  std::optional<base::Uuid> latest_group_id_;
 
   raw_ptr<Browser> browser_;
   raw_ptr<views::Widget> widget_;
