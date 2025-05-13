611affc6644b764532890a4ccaef2abb466151d7
Trevor Perrier <perrier@chromium.org>
2025-05-13 16:16:43 -0700

    [Glic] Use client area bounds to restore window position
    Bug: b:416426733
    Change-Id: I0f4e50f3eca84b7f62c74361005872338f7f05be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6544586
    Reviewed-by: Sana Akbani <sanaakbani@google.com>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459779}

--- chrome/browser/glic/widget/glic_window_controller_impl.cc
+++ chrome/browser/glic/widget/glic_window_controller_impl.cc
@@ -1077,7 +1077,8 @@ void GlicWindowControllerImpl::CloseAndReopenDetached(
 
 void GlicWindowControllerImpl::SaveWidgetPosition() {
   if (GetGlicWidget() && GetGlicWidget()->IsVisible()) {
-    previous_position_ = GetGlicWidget()->GetWindowBoundsInScreen().origin();
+    previous_position_ =
+        GetGlicWidget()->GetClientAreaBoundsInScreen().origin();
     profile_->GetPrefs()->SetInteger(prefs::kGlicPreviousPositionX,
                                      previous_position_->x());
     profile_->GetPrefs()->SetInteger(prefs::kGlicPreviousPositionY,
