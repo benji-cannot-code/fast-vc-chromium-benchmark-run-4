e505f264d057f0a341dd84762fa970afa970c59b
Anton Bikineev <bikineev@chromium.org>
2025-05-13 14:47:20 -0700

    Don't clear the inlined storage for HeapVector
    All heap allocations are guaranteed to be zeroed anyway. Stack
    allocations are not, but they shouldn't be reachable by the marker
    and are scanned conservatively.
    
    Speedometer3.0:
       Charts-chartjs                              [ -0.8%,  +0.4%]
    👍 Charts-observable-plot                      [ -1.6%,  -0.7%]
       Editor-CodeMirror                           [ -1.3%,  +0.3%]
       Editor-TipTap                               [ -0.3%,  +0.4%]
    👍 NewsSite-Next                               [ -0.7%,  -0.3%]
       NewsSite-Nuxt                               [ -0.3%,  +0.1%]
       Perf-Dashboard                              [ -0.9%,  +0.1%]
    👍 React-Stockcharts-SVG                       [ -0.6%,  -0.1%]
    👍 Score                                       [ +0.2%,  +0.5%]
       TodoMVC-Angular-Complex-DOM                 [ -0.9%,  +0.2%]
    👍 TodoMVC-Backbone                            [ -0.8%,  -0.2%]
       TodoMVC-JavaScript-ES5                      [ -0.5%,  +0.1%]
       TodoMVC-JavaScript-ES6-Webpack-Complex-DOM  [ -0.2%,  +0.3%]
    👍 TodoMVC-Lit-Complex-DOM                     [ -0.8%,  -0.2%]
       TodoMVC-Preact-Complex-DOM                  [ -0.7%,  +1.1%]
    👍 TodoMVC-React-Complex-DOM                   [ -0.9%,  -0.2%]
       TodoMVC-React-Redux                         [ -0.5%,  +0.2%]
       TodoMVC-Svelte-Complex-DOM                  [ -2.5%,  +0.8%]
    👍 TodoMVC-Vue                                 [ -0.9%,  -0.2%]
    👍 TodoMVC-WebComponents                       [ -0.7%,  -0.1%]
       TodoMVC-jQuery                              [ -0.6%,  +1.0%]
    
    Bug: 417455400
    Change-Id: Iaa931dd7cf1923221377665b4711449c24a1a455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530600
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459736}

--- third_party/blink/renderer/platform/wtf/vector.h
+++ third_party/blink/renderer/platform/wtf/vector.h
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/container_annotations.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"  // For default Vector template parameters.
 #include "third_party/blink/renderer/platform/wtf/hash_table_deleted_value_type.h"
+#include "third_party/blink/renderer/platform/wtf/stack_util.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/type_traits.h"
 #include "third_party/blink/renderer/platform/wtf/vector_traits.h"
@@ -673,10 +674,16 @@ class VectorBuffer : protected VectorBufferBase<T, Allocator> {
  public:
   using OffsetRange = typename Base::OffsetRange;
 
-  VectorBuffer() : Base(InlineBuffer(), InlineCapacity) { InitInlinedBuffer(); }
+  VectorBuffer() : Base(InlineBuffer(), InlineCapacity) {
+#if DCHECK_IS_ON()
+    VerifyInlinedBuffer();
+#endif
+  }
 
   explicit VectorBuffer(HashTableDeletedValueType value) : Base(value) {
-    InitInlinedBuffer();
+#if DCHECK_IS_ON()
+    VerifyInlinedBuffer();
+#endif
   }
   bool IsHashTableDeletedValue() const {
     return Base::IsHashTableDeletedValue();
@@ -684,7 +691,9 @@ class VectorBuffer : protected VectorBufferBase<T, Allocator> {
 
   explicit VectorBuffer(wtf_size_t capacity)
       : Base(InlineBuffer(), InlineCapacity) {
-    InitInlinedBuffer();
+#if DCHECK_IS_ON()
+    VerifyInlinedBuffer();
+#endif
     if (capacity > InlineCapacity) {
       Base::AllocateBuffer(capacity, VectorOperationOrigin::kConstruction);
     }
@@ -976,9 +985,14 @@ class VectorBuffer : protected VectorBufferBase<T, Allocator> {
     return unsafe_reinterpret_cast_ptr<const T*>(inline_buffer_);
   }
 
-  void InitInlinedBuffer() {
-    if (Allocator::kIsGarbageCollected) {
-      memset(&inline_buffer_, 0, kInlineBufferSize);
+  void VerifyInlinedBuffer() {
+    // On heap allocations are always zero-initialized. Stack is anyway scanned
+    // conservatively, stack-to-stack pointers are filtered out, so no need to
+    // clear out the inlined buffer.
+    if constexpr (Allocator::kIsGarbageCollected) {
+      const bool is_zeroed =
+          std::ranges::all_of(inline_buffer_, [](char c) { return c == 0; });
+      DCHECK(is_zeroed || WTF::IsOnStack(inline_buffer_));
     }
   }
 
