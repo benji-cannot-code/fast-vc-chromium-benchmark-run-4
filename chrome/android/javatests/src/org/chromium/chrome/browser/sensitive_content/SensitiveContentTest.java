3bf9fa3d2b81569503a3f3b0f1d4be8a53ad2ff3
Dan Polanco <polardz@google.com>
2025-05-13 10:10:11 -0700

    [New Tab Animation] Add fieldtrial testing config
    Flag added on: https://crrev.com/c/6032249
    
    Bug: 417254952
    Change-Id: If45ad7e5994ec8efb2e3698d927969c1077cf127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534535
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459534}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
@@ -47,6 +47,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
@@ -56,6 +57,7 @@ import org.chromium.chrome.browser.compositor.layouts.LayoutManagerChrome;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManagerChromePhone;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManagerHost;
 import org.chromium.chrome.browser.compositor.layouts.MockLayoutHost;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.hub.HubLayoutDependencyHolder;
 import org.chromium.chrome.browser.layouts.LayoutType;
@@ -496,6 +498,7 @@ public class SensitiveContentTest {
     @Test
     @MediumTest
     @EnableFeatures(SensitiveContentFeatures.SENSITIVE_CONTENT_WHILE_SWITCHING_TABS)
+    @DisableFeatures({ChromeFeatureList.SHOW_NEW_TAB_ANIMATIONS})
     @Restriction(DeviceFormFactor.PHONE)
     public void testSimpleAnimationLayoutHasSensitiveContent() throws TimeoutException {
         final String histogram = "SensitiveContent.SensitiveTabSwitchingAnimations";
