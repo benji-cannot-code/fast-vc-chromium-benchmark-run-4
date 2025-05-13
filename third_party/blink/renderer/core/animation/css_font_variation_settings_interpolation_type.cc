f41716137b55e90fa3a63899fb6ee6afa7c0003d
Rune Lillesveen <futhark@chromium.org>
2025-05-13 14:47:56 -0700

    Use TreeCountingChecker when animating font-variation-settings
    Bug: 415626999
    
    Change-Id: If434f09c6eff9cd5ebf1467a7995430f4978cfdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6538151
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459737}

--- third_party/blink/renderer/core/animation/css_font_variation_settings_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_font_variation_settings_interpolation_type.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/ptr_util.h"
+#include "third_party/blink/renderer/core/animation/tree_counting_checker.h"
 #include "third_party/blink/renderer/core/animation/underlying_value_owner.h"
 #include "third_party/blink/renderer/core/css/css_font_variation_value.h"
 #include "third_party/blink/renderer/core/css/css_value_list.h"
@@ -145,9 +146,18 @@ CSSFontVariationSettingsInterpolationType::MaybeConvertInherit(
 InterpolationValue CSSFontVariationSettingsInterpolationType::MaybeConvertValue(
     const CSSValue& value,
     const StyleResolverState& state,
-    ConversionCheckers&) const {
-  // TODO(crbug.com/415626999): Create a TreeCountingChecker for sibling-index()
-  // and sibling-count() if necessary.
+    ConversionCheckers& conversion_checkers) const {
+  if (const auto* value_list = DynamicTo<CSSValueList>(value)) {
+    for (const CSSValue* feature : *value_list) {
+      if (To<cssvalue::CSSFontVariationValue>(feature)
+              ->Value()
+              ->IsElementDependent()) {
+        conversion_checkers.push_back(
+            TreeCountingChecker::Create(state.CssToLengthConversionData()));
+        break;
+      }
+    }
+  }
   // TODO(crbug.com/415572412): Create a LengthUnitsChecker for relative units
   // if necessary.
   scoped_refptr<FontVariationSettings> settings =
