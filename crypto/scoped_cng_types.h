2a417c1e9bd210314230728e1d33165efe1f17c9
Sebastien Lalancette <seblalancette@chromium.org>
2025-05-13 10:47:10 -0700

    Support converting Windows software keys to SSLPrivateKey
    Make sure that SSLPrivateKeys can be created from
    UnexportableSigningKeys created via
    GetMicrosoftSoftwareUnexportableKeyProvider (software keys provider).
    
    Bug: 416539549
    Change-Id: Ic824cf65aae2dcf11bac7ab3e24d089dbbe25130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533520
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459573}

--- crypto/scoped_cng_types.h
+++ crypto/scoped_cng_types.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CRYPTO_SCOPED_CNG_TYPES_H_
 #define CRYPTO_SCOPED_CNG_TYPES_H_
 
-#include <windows.h>
-
 #include "base/scoped_generic.h"
 #include "base/win/wincrypt_shim.h"
 
--- net/ssl/ssl_platform_key_win_unittest.cc
+++ net/ssl/ssl_platform_key_win_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "crypto/features.h"
 #include "crypto/scoped_capi_types.h"
 #include "crypto/scoped_cng_types.h"
 #include "crypto/unexportable_key.h"
@@ -352,10 +353,20 @@ INSTANTIATE_TEST_SUITE_P(All,
                          testing::ValuesIn(kTestKeys),
                          TestParamsToString);
 
-TEST(UnexportableSSLPlatformKeyWinTest, WrapUnexportableKeySlowly) {
-  auto provider = crypto::GetUnexportableKeyProvider({});
+class UnexportableSSLPlatformKeyWinTest : public testing::TestWithParam<bool> {
+ protected:
+  bool UseHardwareBackedKeys() { return GetParam(); }
+};
+
+TEST_P(UnexportableSSLPlatformKeyWinTest, WrapUnexportableKeySlowly) {
+  base::test::ScopedFeatureList scoped_feature_list(
+      crypto::features::kIsHardwareBackedFixEnabled);
+
+  auto provider = UseHardwareBackedKeys()
+                      ? crypto::GetUnexportableKeyProvider({})
+                      : crypto::GetMicrosoftSoftwareUnexportableKeyProvider();
   if (!provider) {
-    GTEST_SKIP() << "Hardware-backed keys are not supported.";
+    GTEST_SKIP() << "Platform keys are not supported.";
   }
 
   const crypto::SignatureVerifier::SignatureAlgorithm algorithms[] = {
@@ -372,5 +383,9 @@ TEST(UnexportableSSLPlatformKeyWinTest, WrapUnexportableKeySlowly) {
   auto ssl_private_key = WrapUnexportableKeySlowly(*key);
   ASSERT_TRUE(ssl_private_key);
 }
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         UnexportableSSLPlatformKeyWinTest,
+                         testing::Bool());
 
 }  // namespace net
