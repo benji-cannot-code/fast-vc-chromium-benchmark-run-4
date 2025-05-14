37f4d6ce0daccd19e06cb73a31493263c114f471
Saifuddin Hitawala <hitawala@chromium.org>
2025-05-14 07:27:01 -0700

    [exo] Mark RG88 format as unreachable in exo buffer
    RG88 single-plane format should no longer be used over exo. Based on
    UMA data (linked in bug), we can confirm it is no longer the case.
    
    This change marks it as NOTREACHED under killswitch for safer rollout.
    
    Bug: 416285370
    Change-Id: I4f7d9a1dfad75c4d3b5617e8aeaddf79d1fe7ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543366
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460067}

--- components/exo/buffer.cc
+++ components/exo/buffer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
@@ -75,6 +76,11 @@ const gfx::BufferUsage kDefaultBufferUsage = gfx::BufferUsage::GPU_READ;
 const gpu::SharedImageUsageSet kDefaultMappableSIUsage =
     gpu::SHARED_IMAGE_USAGE_DISPLAY_READ;
 
+// Killswitch for disabling RG88 format support over exo.
+BASE_FEATURE(kExoDisableRG88Format,
+             "kExoDisableRG88Format",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Gets the color type of |format| for creating bitmap. If it returns
 // SkColorType::kUnknown_SkColorType, it means with this format, this buffer
 // contents should not be used to create bitmap.
@@ -106,6 +112,9 @@ viz::SharedImageFormat GetSharedImageFormat(gfx::BufferFormat buffer_format) {
     case gfx::BufferFormat::BGR_565:
       return viz::SinglePlaneFormat::kBGR_565;
     case gfx::BufferFormat::RG_88:
+      if (base::FeatureList::IsEnabled(kExoDisableRG88Format)) {
+        NOTREACHED();
+      }
       return viz::SinglePlaneFormat::kRG_88;
     case gfx::BufferFormat::RGBX_8888:
       return viz::SinglePlaneFormat::kRGBX_8888;
