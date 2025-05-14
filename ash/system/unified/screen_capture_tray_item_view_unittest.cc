0b950a2489aaa56d34d106ed7a9c2f3fe4dd1f49
HuanPo Lin <robertlin@chromium.org>
2025-05-13 17:45:54 -0700

    gardener: Disable flaky ScreenCaptureTrayItemViewTest.MultiOriginCaptureStartedNotificationSkipAllowlisted*
    Bug: 417560454
    Change-Id: I2a1662bc65025f2adb362b89c2f425b3ed9bb0e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6544278
    Owners-Override: Robert Lin <robertlin@google.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459818}

--- ash/system/unified/screen_capture_tray_item_view_unittest.cc
+++ ash/system/unified/screen_capture_tray_item_view_unittest.cc
@@ -134,8 +134,16 @@ TEST_F(ScreenCaptureTrayItemViewTest,
   EXPECT_EQ(0u, screen_capture_tray_item_view_->requests_.size());
 }
 
+// TODO(crbug.com/417560454): Re-enable this test
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_MultiOriginCaptureStartedNotificationSkipAllowlistedOneOrigin \
+  DISABLED_MultiOriginCaptureStartedNotificationSkipAllowlistedOneOrigin
+#else
+#define MAYBE_MultiOriginCaptureStartedNotificationSkipAllowlistedOneOrigin \
+  MultiOriginCaptureStartedNotificationSkipAllowlistedOneOrigin
+#endif
 TEST_F(ScreenCaptureTrayItemViewTest,
-       MultiOriginCaptureStartedNotificationSkipAllowlistedOneOrigin) {
+       MAYBE_MultiOriginCaptureStartedNotificationSkipAllowlistedOneOrigin) {
   const url::Origin origin_with_allowlisted_exception =
       url::Origin::CreateFromNormalizedTuple(
           /*scheme=*/"isolated-app",
@@ -175,8 +183,16 @@ TEST_F(ScreenCaptureTrayItemViewTest,
   EXPECT_EQ(0u, screen_capture_tray_item_view_->requests_.size());
 }
 
+// TODO(crbug.com/417560454): Re-enable this test
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_MultiOriginCaptureStartedNotificationSkipAllowlistedMixedOrigins \
+  DISABLED_MultiOriginCaptureStartedNotificationSkipAllowlistedMixedOrigins
+#else
+#define MAYBE_MultiOriginCaptureStartedNotificationSkipAllowlistedMixedOrigins \
+  MultiOriginCaptureStartedNotificationSkipAllowlistedMixedOrigins
+#endif
 TEST_F(ScreenCaptureTrayItemViewTest,
-       MultiOriginCaptureStartedNotificationSkipAllowlistedMixedOrigins) {
+       MAYBE_MultiOriginCaptureStartedNotificationSkipAllowlistedMixedOrigins) {
   const url::Origin origin_with_allowlisted_exception =
       url::Origin::CreateFromNormalizedTuple(
           /*scheme=*/"isolated-app",
