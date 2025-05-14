7005ee8c7670ae72f4c5cdffd27c80591ee6600a
Vidhan Jain <vidhanj@google.com>
2025-05-14 07:50:14 -0700

    [iOS] Remove unused method AutofillUITypeFromAutofillTypeForCard
    Change-Id: I562e031a378ffe54a8a2fdd8ddff316d17af90cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543990
    Auto-Submit: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460087}

--- ios/chrome/browser/autofill/ui_bundled/autofill_ui_type_util.mm
+++ ios/chrome/browser/autofill/ui_bundled/autofill_ui_type_util.mm
@@ -8,24 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/notreached.h"
 #import "components/autofill/core/common/autofill_features.h"
 
-AutofillCreditCardUIType AutofillUITypeFromAutofillTypeForCard(
-    autofill::FieldType type) {
-  switch (type) {
-    case autofill::UNKNOWN_TYPE:
-      return AutofillCreditCardUIType::kUnknown;
-    case autofill::CREDIT_CARD_NUMBER:
-      return AutofillCreditCardUIType::kNumber;
-    case autofill::CREDIT_CARD_NAME_FULL:
-      return AutofillCreditCardUIType::kFullName;
-    case autofill::CREDIT_CARD_EXP_MONTH:
-      return AutofillCreditCardUIType::kExpMonth;
-    case autofill::CREDIT_CARD_EXP_4_DIGIT_YEAR:
-      return AutofillCreditCardUIType::kExpYear;
-    default:
-      NOTREACHED();
-  }
-}
-
 autofill::FieldType AutofillTypeFromAutofillUITypeForCard(
     AutofillCreditCardUIType type) {
   switch (type) {
