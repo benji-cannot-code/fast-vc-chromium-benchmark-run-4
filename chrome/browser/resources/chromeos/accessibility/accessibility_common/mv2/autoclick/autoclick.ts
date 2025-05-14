fcfa5c0f215df9fd1c1edc097e404ab3c2cd13fc
Alexander Surkov <asurkov@igalia.com>
2025-05-14 07:41:35 -0700

    [Accessibility Common] AccessibilityExtensionArrayUtilTest fails in MV3 environment.
    Dynamic imports are disallowed in MV3. Expose ArrayUtil via
    TestImportManager to avoid import() usage.
    
    accessibility/common tests run in an environment with the autoclick
    extension enabled, which is part of the accessibility_common extension.
    Providing required utility imports into autoclick script via
    TestImportManager resolves the test failures.
    
    Bug: 388867838
    Change-Id: I2b7d6f62830f27c4eadf729b21783b42ab062674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543367
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1460079}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/mv2/autoclick/autoclick.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/mv2/autoclick/autoclick.ts
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Required for running accessibility/common tests like
+// AccessibilityExtensionArrayUtilTest. Without this, necessary exports
+// are not available in the test enviropment.
+import '/common/array_util.js';
+
 import {EventHandler} from '/common/event_handler.js';
 
 /**
--- chrome/browser/resources/chromeos/accessibility/common/array_util.ts
+++ chrome/browser/resources/chromeos/accessibility/common/array_util.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestImportManager} from './testing/test_import_manager.js';
+
 /** A collection of helper functions when dealing with arrays. */
 export const ArrayUtil = {
   contentsAreEqual: <T>(array1?: T[], array2?: T[]): boolean => {
@@ -17,3 +19,5 @@ export const ArrayUtil = {
     return true;
   },
 };
+
+TestImportManager.exportForTesting(['ArrayUtil', ArrayUtil]);
