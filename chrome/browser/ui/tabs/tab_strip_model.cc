9f67456861d12b8e2bd36ed744b54aa18f053221
David Pennington <dpenning@chromium.org>
2025-05-13 17:11:15 -0700

    [TabGroupController updates] Remove unused function MoveTabGroup
    MoveTabGroup is only defined and used within the TabStripModel, we can
    remove this method from the TabGroupController, and just define the
    notification within TabStripModel header as non virtual.
    
    Bug: 417235218
    Change-Id: Icd6a36d8879f92d98cf0daa0f76431523628c1e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534636
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459800}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -911,7 +911,7 @@ void TabStripModel::MoveGroupToImpl(const tab_groups::TabGroupId& group,
       base::BindOnce(&tabs::TabStripCollection::MoveTabGroupTo,
                      base::Unretained(contents_data_.get()), group, to_index));
 
-  MoveTabGroup(group);
+  NotifyTabGroupMoved(group);
 }
 
 WebContents* TabStripModel::GetActiveWebContents() const {
@@ -1849,7 +1849,7 @@ void TabStripModel::OnTabGroupVisualsChanged(
   }
 }
 
-void TabStripModel::MoveTabGroup(const tab_groups::TabGroupId& group) {
+void TabStripModel::NotifyTabGroupMoved(const tab_groups::TabGroupId& group) {
   TabGroupChange change(this, group, TabGroupChange::kMoved);
   for (auto& observer : observers_) {
     observer.OnTabGroupChanged(change);
