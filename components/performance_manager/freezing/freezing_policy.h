1fdda6bd3d5eef70f7ee2f1ba82d9e84ca614269
François Doray <fdoray@chromium.org>
2025-05-14 07:43:43 -0700

    [freezing] Make PageFreezingState an inner class of FreezingPolicy.
    Motivation:
    This allows us to make all accesses to the PageFreezingState go
    through a method of FreezingPolicy (GetFreezingState). In an
    upcoming CL, this method will initialize a member of PageFreezingState
    based on the value of a method that can be overridden in tests.
    
    Bug: 415799027
    Change-Id: I4ebd54c05f0536d5b7325ae8477828cf35fe5a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527495
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460082}

--- components/performance_manager/freezing/freezing_policy.h
+++ components/performance_manager/freezing/freezing_policy.h
@@ -98,7 +98,10 @@ class FreezingPolicy : public PageNodeObserver,
 
   class CanFreezePerTypeTracker;
 
-  // State of a browsing instance.
+  // Freezing related state for a page.
+  struct PageFreezingState;
+
+  // Freezing related state for a browsing instance.
   struct BrowsingInstanceState {
     BrowsingInstanceState();
     ~BrowsingInstanceState();
@@ -138,6 +141,9 @@ class FreezingPolicy : public PageNodeObserver,
   base::flat_set<content::BrowsingInstanceId> GetBrowsingInstances(
       const PageNode* page) const;
 
+  // Returns the `PageFreezingState` for `page`, creating it if necessary.
+  PageFreezingState& GetFreezingState(const PageNode* page_node) const;
+
   // Update frozen state for all pages connected to `page`. Connected pages
   // (including `page_node`) are added to `connected_pages_out` if not nullptr.
   void UpdateFrozenState(
@@ -151,7 +157,7 @@ class FreezingPolicy : public PageNodeObserver,
 
   // Returns the union of `CannotFreezeReason`s applicable to pages associated
   // with `browsing_instance_state`.
-  static freezing::CannotFreezeReasonSet GetCannotFreezeReasons(
+  freezing::CannotFreezeReasonSet GetCannotFreezeReasons(
       const BrowsingInstanceState& browsing_instance_state);
 
   // GraphOwned implementation:
