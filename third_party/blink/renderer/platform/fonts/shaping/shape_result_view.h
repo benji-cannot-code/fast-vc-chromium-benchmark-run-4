6de269d4a3572e57bc1cd72d0fc40ec81f338a20
Koji Ishii <kojii@chromium.org>
2025-05-14 16:55:28 -0700

    [cleanup] Remove `GlyphOffsetArray`
    After the oilpan-ization, the class no longer provides much
    values. The logic is merged into `ShapeResultRun`.
    
    This patch should have no behavior changes.
    
    Bug: 406127861
    Change-Id: I6dcd600b145697c0d3e83c8606b8928d64e83d09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6475013
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460445}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/glyph_data.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/glyph_data_range.h"
-#include "third_party/blink/renderer/platform/fonts/shaping/glyph_offset_array.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/glyph_offset_iterator.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 #include "third_party/blink/renderer/platform/fonts/simple_font_data.h"
 #include "third_party/blink/renderer/platform/geometry/layout_unit.h"
@@ -187,9 +187,8 @@ class PLATFORM_EXPORT ShapeResultView final
       return *(UNSAFE_TODO(range_.begin() + index));
     }
     template <bool has_non_zero_glyph_offsets>
-    GlyphOffsetArray::iterator<has_non_zero_glyph_offsets> GetGlyphOffsets()
-        const {
-      return GlyphOffsetArray::iterator<has_non_zero_glyph_offsets>(range_);
+    GlyphOffsetIterator<has_non_zero_glyph_offsets> GetGlyphOffsets() const {
+      return GlyphOffsetIterator<has_non_zero_glyph_offsets>(range_);
     }
     bool HasGlyphOffsets() const { return range_.HasOffsets(); }
     // The end character index of |this| without considering offsets in
