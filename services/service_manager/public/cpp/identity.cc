7a03c8790f92041df824461fbc9d2e0a69fbc5a5
Jan Keitel <jkeitel@google.com>
2025-05-14 09:23:51 -0700

    Modernize equality operators in //services
    This CL is pure clean-up and contains no functionality changes.
    Depending on the files covered in the CL (since the CL was generated
    using git cl split), it does a subset of the following:
    the following:
    - Remove unneeded operator!= declarations/definitions since C++20 can
      automatically derive those from operator==.
    - Default operator== where this is equivalent to the current behavior.
    - Default operator<=> where this is equivalent to the current
      behavior.
    
    This CL was uploaded by git cl split.
    
    R=tsepez@chromium.org
    
    Bug: 40256175
    Change-Id: I8dd58b2c295cda2474b485c7ea14f42a9b3e94fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6540227
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460152}

--- services/service_manager/public/cpp/identity.cc
+++ services/service_manager/public/cpp/identity.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/service_manager/public/cpp/identity.h"
 
-#include <tuple>
-
 #include "base/check.h"
 #include "base/strings/stringprintf.h"
 
@@ -32,18 +30,6 @@ Identity::Identity(const Identity& other) = default;
 Identity::~Identity() = default;
 
 Identity& Identity::operator=(const Identity& other) = default;
-
-bool Identity::operator<(const Identity& other) const {
-  return std::tie(name_, instance_group_, instance_id_, globally_unique_id_) <
-         std::tie(other.name_, other.instance_group_, other.instance_id_,
-                  other.globally_unique_id_);
-}
-
-bool Identity::operator==(const Identity& other) const {
-  return name_ == other.name_ && instance_group_ == other.instance_group_ &&
-         instance_id_ == other.instance_id_ &&
-         globally_unique_id_ == other.globally_unique_id_;
-}
 
 bool Identity::IsValid() const {
   return !name_.empty() && !instance_group_.is_zero() &&
