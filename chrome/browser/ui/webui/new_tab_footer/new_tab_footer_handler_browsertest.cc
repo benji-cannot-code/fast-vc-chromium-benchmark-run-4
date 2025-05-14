976894a224891cf3ad80c8d06b79fa693afa7774
Paul Adedeji <pauladedeji@google.com>
2025-05-14 11:23:35 -0700

    [ntp footer] Listen for extension ready/unload in handler.
    Updates ntp extension info whenever an extension is ready or unloaded.
    
    Also adds test to ensure that management notice text is updated
    correctly when there's no policy.
    
    Follow up work (crbug.com/416748275), will prevent extension info from
    showing on non-extension NTP pages.
    
    screencast/cast/NjI5NTk3NjMyNjIwMTM0NHwzNDZiODU2OS00Ng
    
    Change-Id: I8c73401b991a8367babf0f2e24509369daed4c77
    Bug: 414822873
    Fixed: 414822873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6523922
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460253}

--- chrome/browser/ui/webui/new_tab_footer/new_tab_footer_handler_browsertest.cc
+++ chrome/browser/ui/webui/new_tab_footer/new_tab_footer_handler_browsertest.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/webui/new_tab_footer/mock_new_tab_footer_document.h"
 #include "chrome/browser/ui/webui/new_tab_footer/new_tab_footer.mojom.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "components/search/ntp_features.h"
@@ -36,8 +36,7 @@ class NewTabFooterHandlerBrowserTest : public extensions::ExtensionBrowserTest {
     InProcessBrowserTest::SetUpOnMainThread();
     handler_ = std::make_unique<NewTabFooterHandler>(
         mojo::PendingReceiver<new_tab_footer::mojom::NewTabFooterHandler>(),
-        mojo::PendingRemote<new_tab_footer::mojom::NewTabFooterDocument>(),
-        web_contents());
+        document_.BindAndGetRemote(), web_contents());
   }
 
   void TearDownOnMainThread() override {
@@ -51,8 +50,9 @@ class NewTabFooterHandlerBrowserTest : public extensions::ExtensionBrowserTest {
   }
 
  private:
-  std::unique_ptr<NewTabFooterHandler> handler_;
   base::test::ScopedFeatureList feature_list_;
+  std::unique_ptr<NewTabFooterHandler> handler_;
+  testing::NiceMock<MockNewTabFooterDocument> document_;
 };
 
 IN_PROC_BROWSER_TEST_F(NewTabFooterHandlerBrowserTest,
@@ -74,11 +74,9 @@ IN_PROC_BROWSER_TEST_F(NewTabFooterHandlerBrowserTest,
   scoped_refptr<const extensions::Extension> extension =
       LoadExtension(extension_dir.Pack());
   ASSERT_TRUE(extension);
-  base::MockCallback<NewTabFooterHandler::GetNtpExtensionNameCallback> callback;
-  EXPECT_CALL(callback, Run).Times(1);
-  // Invoke GetNtpExtensionName, triggering the handler to set its New Tab Page
-  // extension ID.
-  handler().GetNtpExtensionName(callback.Get());
+  // Invoke UpdateNtpExtensionName, triggering the handler to set its New Tab
+  // Page extension ID.
+  handler().UpdateNtpExtensionName();
 
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
   handler().OpenExtensionOptionsPageWithFallback();
