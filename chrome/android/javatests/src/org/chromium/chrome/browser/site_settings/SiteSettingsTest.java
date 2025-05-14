1a9d1052ddbc04b3676c43a10d19ac767184ebe5
Peter Kotwicz <pkotwicz@google.com>
2025-05-14 12:33:22 -0700

    Disable "Add Exception" button if content-setting is managed
    BUG=399933929
    
    Change-Id: I6e854d3ea379efc5275d01e20f6b2637ae26809e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520460
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460303}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
@@ -1140,6 +1140,13 @@ public class SiteSettingsTest {
                 ToggleButtonState.EnabledChecked);
         onView(getManagedViewMatcher(/* activeView= */ true)).check(matches(isDisplayed()));
         onView(getManagedViewMatcher(/* activeView= */ false)).check(matches(not(isDisplayed())));
+
+        SingleCategorySettings singleCategorySettings =
+                (SingleCategorySettings) settingsActivity.getMainFragment();
+        Preference addExceptionPreference =
+                singleCategorySettings.findPreference(SingleCategorySettings.ADD_EXCEPTION_KEY);
+        Assert.assertTrue(addExceptionPreference.isEnabled());
+
         settingsActivity.finish();
     }
 
@@ -2738,7 +2745,6 @@ public class SiteSettingsTest {
                     SingleCategorySettings singleCategorySettings =
                             (SingleCategorySettings) settingsActivity.getMainFragment();
 
-                    // TODO(crbug.com/399933929) Do not show "Add Exception" button.
                     checkPreferencesForSettingsActivity(
                             settingsActivity,
                             new String[] {
@@ -2753,6 +2759,11 @@ public class SiteSettingsTest {
                                             SingleCategorySettings.BINARY_TOGGLE_KEY);
                     Assert.assertTrue(binaryToggle.isChecked());
                     Assert.assertFalse(binaryToggle.isEnabled());
+
+                    Preference addExceptionPreference =
+                            singleCategorySettings.findPreference(
+                                    SingleCategorySettings.ADD_EXCEPTION_KEY);
+                    Assert.assertFalse(addExceptionPreference.isEnabled());
 
                     onData(withKey(SingleCategorySettings.ALLOWED_GROUP))
                             .inAdapterView(
@@ -3316,9 +3327,8 @@ public class SiteSettingsTest {
     public void deleteSingleSiteDataRemovesRowSingleWebsiteSettings() throws Exception {
         final String currentSiteUrl = "one-test.com";
         final String rwsMemberUrl = "two-test.com";
-        final SettingsActivity settingsActivity =
-                SiteSettingsTestUtils.startSingleWebsitePreferences(
-                        getRwsOwnerSiteForUrls(currentSiteUrl, rwsMemberUrl));
+        SiteSettingsTestUtils.startSingleWebsitePreferences(
+                getRwsOwnerSiteForUrls(currentSiteUrl, rwsMemberUrl));
 
         onView(
                         allOf(
@@ -3361,9 +3371,8 @@ public class SiteSettingsTest {
         final String currentSiteUrl = "one-test.com";
         final String rwsMemberUrl = "two-test.com";
 
-        final SettingsActivity settingsActivity =
-                SiteSettingsTestUtils.startGroupedWebsitesPreferences(
-                        getRwsSiteGroupForUrls(currentSiteUrl, rwsMemberUrl));
+        SiteSettingsTestUtils.startGroupedWebsitesPreferences(
+                getRwsSiteGroupForUrls(currentSiteUrl, rwsMemberUrl));
 
         onView(
                         allOf(
