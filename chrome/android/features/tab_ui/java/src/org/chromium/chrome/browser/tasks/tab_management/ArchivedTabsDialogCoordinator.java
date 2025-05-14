2b6afcc6220bbbd0a5af9de836f28f502b3c675b
Brandon Fong <bjfong@google.com>
2025-05-14 10:37:49 -0700

    [declutter] Update restore tabs button to include tab groups on archive
    Update the restore tabs button logic to include tab groups on the
    archive surface.
    
    Bug: 415846768
    Change-Id: Ia2edd31975f9454295590110bf41a2cb00a80f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6511927
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Auto-Submit: Brandon Fong <bjfong@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460218}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinator.java
@@ -113,8 +113,8 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
         /** Start tab selection process. */
         void startTabSelection();
 
-        /** Restore the given list of tabs. */
-        void restoreArchivedTabs(List<Tab> tabs);
+        /** Restore the given list of tabs and tab groups. */
+        void restoreArchivedTabs(List<Tab> tabs, List<String> tabGroupSyncIds);
 
         /** Close the given list of tabs and tab groups. */
         void closeArchivedTabs(List<Tab> tabs, List<String> tabGroupSyncIds);
@@ -126,7 +126,8 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
                 public void restoreAllArchivedTabs() {
                     List<Tab> tabs = TabModelUtils.convertTabListToListOfTabs(mArchivedTabModel);
                     int tabCount = tabs.size();
-                    ArchivedTabsDialogCoordinator.this.restoreArchivedTabs(tabs);
+                    ArchivedTabsDialogCoordinator.this.restoreArchivedTabs(
+                            tabs, getArchivedTabGroupSyncIds());
                     RecordHistogram.recordCount1000Histogram(
                             "Tabs.RestoreAllArchivedTabsMenuItem.TabCount", tabCount);
                     RecordUserAction.record("Tabs.RestoreAllArchivedTabsMenuItem");
@@ -146,9 +147,9 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
                 }
 
                 @Override
-                public void restoreArchivedTabs(List<Tab> tabs) {
+                public void restoreArchivedTabs(List<Tab> tabs, List<String> tabGroupSyncIds) {
                     int tabCount = tabs.size();
-                    ArchivedTabsDialogCoordinator.this.restoreArchivedTabs(tabs);
+                    ArchivedTabsDialogCoordinator.this.restoreArchivedTabs(tabs, tabGroupSyncIds);
                     moveToState(TabActionState.CLOSABLE);
                     RecordHistogram.recordCount1000Histogram(
                             "Tabs.RestoreArchivedTabsMenuItem.TabCount", tabCount);
@@ -289,7 +290,9 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
             new TabGroupSyncService.Observer() {
                 @Override
                 public void onTabGroupUpdated(SavedTabGroup group, @TriggerSource int source) {
-                    refreshArchivedTabList();
+                    if (group.archivalTimeMs != null) {
+                        refreshArchivedTabList();
+                    }
                 }
 
                 @Override
@@ -808,7 +811,7 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
         return mArchivedTabModelOrchestrator.getTabCountSupplier().get();
     }
 
-    private void restoreArchivedTabs(List<Tab> tabs) {
+    private void restoreArchivedTabs(List<Tab> tabs, List<String> tabGroupSyncIds) {
         mArchivedTabModelOrchestrator
                 .getTabArchiver()
                 .unarchiveAndRestoreTabs(
@@ -816,6 +819,11 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
                         tabs,
                         /* updateTimestamp= */ true,
                         /* areTabsBeingOpened= */ false);
+        for (String syncId : tabGroupSyncIds) {
+            mTabGroupUiActionHandlerSupplier.get().openTabGroup(syncId);
+            mTabListEditorCoordinator.removeListItem(
+                    UiType.TAB_GROUP, TabListEditorItemSelectionId.createTabGroupSyncId(syncId));
+        }
     }
 
     private void onIphReviewClicked() {
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorRestoreArchivedTabsAction.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorRestoreArchivedTabsAction.java
@@ -51,7 +51,7 @@ public class TabListEditorRestoreArchivedTabsAction extends TabListEditorAction 
 
     @Override
     public boolean performAction(List<Tab> tabs, List<String> tabGroupSyncIds) {
-        mArchiveDelegate.restoreArchivedTabs(tabs);
+        mArchiveDelegate.restoreArchivedTabs(tabs, tabGroupSyncIds);
         return true;
     }
 
