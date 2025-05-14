ed7140808dd6c72921a13486956de8c024fc366f
Philip Eliasson <philipel@chromium.org>
2025-05-14 05:55:24 -0700

    Add a `kAllowDataChannelRecordingInWebrtcInternals` feature flag to allow DataChannel recordings in chrome://webrtc-internals.
    Bug: chromium:407785197
    Change-Id: Iaa4a739fb84931bfac71f33f6fc61d6c7cd958a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6541932
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Philip Eliasson <philipel@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460019}

--- content/browser/webrtc/webrtc_internals_message_handler.cc
+++ content/browser/webrtc/webrtc_internals_message_handler.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/common/url_constants.h"
+#include "third_party/blink/public/common/features.h"
 
 namespace content {
 
@@ -133,6 +134,10 @@ void WebRTCInternalsMessageHandler::OnSetEventLogRecordingsEnabled(
 void WebRTCInternalsMessageHandler::OnSetDataChannelRecordingsEnabled(
     bool enable,
     const base::Value::List& /* unused_list */) {
+  if (!base::FeatureList::IsEnabled(
+          blink::features::kWebRtcAllowDataChannelRecordingInWebrtcInternals)) {
+    return;
+  }
   if (enable) {
     webrtc_internals_->EnableDataChannelRecordings(web_ui()->GetWebContents());
   } else {
