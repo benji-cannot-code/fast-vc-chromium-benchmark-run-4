a2f9bbba4ca354dc0096bec18ab8eb808d830ea9
Alex Chen <alexwchen@chromium.org>
2025-05-14 13:51:28 -0700

    [Site search policy] Show two entries for featured SEs
    https://crrev.com/c/5525438 updated chrome://settings/searchEngines to
    show only one entry with a concatenated keyword for featured SEs. To
    prepare for allowing user override for SiteSearchSettings along with
    Chrome UX input, revert the
    chrome://settings/searchEngines to show an entry for each keyword
    generated by SiteSearchSettings and EnterpriseSearchAggregatorSettings
    (for consistency). This will allow users to override a specific engine
    which will be implemented in a later CL).
    
    This change also adds an additional condition in engine ordering to
    sort by keyword after checking the short_name to ensure that the "@"
    version is always first for consistency.
    
    Before: https://screenshot.googleplex.com/94BCd3eUjTGuZgD
    After: https://screenshot.googleplex.com/4sXRTjZXWRgtXXm
    
    Bug: 417478974
    Change-Id: Ia38a3cec33a02a29905197311047331141ffa258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543908
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460346}

--- chrome/browser/ui/search_engines/keyword_editor_controller_unittest.cc
+++ chrome/browser/ui/search_engines/keyword_editor_controller_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory_test_util.h"
 #include "chrome/browser/search_engines/template_url_service_test_util.h"
 #include "chrome/browser/ui/search_engines/template_url_table_model.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/search_engines/choice_made_location.h"
@@ -536,8 +537,9 @@ void CheckKeywordsToDisplay(
     const TemplateURL* template_url = table_model->GetTemplateURL(row);
     ASSERT_TRUE(template_url);
     EXPECT_EQ(template_url->short_name(), kExpectedShortNamesOrder[i]);
-    EXPECT_EQ(table_model->GetKeywordToDisplay(row),
-              kExpectedKeywordsToDisplay[i]);
+    EXPECT_EQ(
+        table_model->GetText(row, IDS_SEARCH_ENGINES_EDITOR_KEYWORD_COLUMN),
+        kExpectedKeywordsToDisplay[i]);
   }
 }
 
@@ -589,12 +591,14 @@ TEST_F(KeywordEditorControllerTest, FeaturedEnterpriseSiteSearch) {
 
   const auto kExpectedShortNamesOrder = std::vector<std::u16string>({
       u"Featured 1",
+      u"Featured 1",
       u"Featured 2",
       u"Non-featured",
       u"User-defined engine",
   });
   const auto kExpectedKeywordsToDisplay = std::vector<std::u16string>({
-      u"@kw1, kw1",
+      u"@kw1",
+      u"kw1",
       u"@kw2",
       u"kw3",
       u"kw2",
@@ -679,11 +683,13 @@ TEST_F(KeywordEditorControllerTest,
   const auto kExpectedShortNamesOrder = std::vector<std::u16string>({
       u"Featured 1",
       u"Featured 2",
+      u"Featured 2",
       u"User-defined engine",
   });
   const auto kExpectedKeywordsToDisplay = std::vector<std::u16string>({
       u"@kw1",
-      u"@kw2, kw2",
+      u"@kw2",
+      u"kw2",
       u"kw1",
   });
 
@@ -725,10 +731,10 @@ TEST_F(KeywordEditorControllerTest, EnterpriseSearchAggregator) {
     VerifyChanged();
   }
 
-  const auto kExpectedShortNamesOrder =
-      std::vector<std::u16string>({u"Featured 1", u"Non-featured"});
+  const auto kExpectedShortNamesOrder = std::vector<std::u16string>(
+      {u"Featured 1", u"Featured 1", u"Non-featured"});
   const auto kExpectedKeywordsToDisplay =
-      std::vector<std::u16string>({u"@kw1, kw1", u"kw2"});
+      std::vector<std::u16string>({u"@kw1", u"kw1", u"kw2"});
 
   size_t numExpectedKeywords = kExpectedShortNamesOrder.size();
   CheckKeywordsToDisplay(kExpectedShortNamesOrder, kExpectedKeywordsToDisplay,
@@ -850,10 +856,11 @@ TEST_F(KeywordEditorControllerTest, EnterpriseSiteSearchAndSearchAggregator) {
     VerifyChanged();
   }
 
-  const auto kExpectedShortNamesOrder = std::vector<std::u16string>(
-      {u"Featured 1", u"Featured 3", u"Non-featured"});
+  const auto kExpectedShortNamesOrder =
+      std::vector<std::u16string>({u"Featured 1", u"Featured 1", u"Featured 3",
+                                   u"Featured 3", u"Non-featured"});
   const auto kExpectedKeywordsToDisplay =
-      std::vector<std::u16string>({u"@kw1, kw1", u"@kw3, kw3", u"kw2"});
+      std::vector<std::u16string>({u"@kw1", u"kw1", u"@kw3", u"kw3", u"kw2"});
 
   size_t numExpectedKeywords = kExpectedShortNamesOrder.size();
   CheckKeywordsToDisplay(kExpectedShortNamesOrder, kExpectedKeywordsToDisplay,
