d38f141f912688d6645cac4bdb6bce72862f92cb
Fernando Ramirez <ferny@google.com>
2025-05-14 16:20:34 -0700

    [Card Benefits][Curinos] Add suffix generation function for metrics
    We will begin logging card benefit metrics by benefit source instead of
    issuer id.
    
    This CL adds a new function to return the histogram suffix based on a
    given card benefit source (Amex, BMO, or Curinos). This CL also adds a
    new histogram variant for logging benefit metrics by the benefit source.
    
    A follow-up CL will be made to update the logic for logging card benefit
    metrics by benefit source.
    
    go/pay-autofill-chrome-scaling-benefits-design
    
    Bug: 415127265, 415126931
    Change-Id: I4e2f4e6bcb4358807eb5cfaff6a68524289e87e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6517191
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460427}

--- components/autofill/core/browser/metrics/payments/card_metadata_metrics.h
+++ components/autofill/core/browser/metrics/payments/card_metadata_metrics.h
@@ -12,15 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill::autofill_metrics {
 
-// The below issuer and network names are used for logging purposes. The issuer
-// names must be consistent with the Autofill.CreditCardIssuerId in the
-// autofill/histograms.xml file.
+// The below issuer, network, and benefit source names are used for logging
+// purposes. The issuers, networks, and benefit sources must be consistent with
+// the Autofill.CreditCardIssuerId, Autofill.CreditCardNetwork, and
+// Autofill.CreditCardBenefitSource respectively, in the
+// tools/metrics/histograms/metadata/autofill/histograms.xml file.
 constexpr std::string_view kAmericanExpress = "Amex";
 constexpr std::string_view kAnz = "Anz";
 constexpr std::string_view kBmo = "Bmo";
 constexpr std::string_view kCapitalOne = "CapitalOne";
 constexpr std::string_view kChase = "Chase";
 constexpr std::string_view kCiti = "Citi";
+constexpr std::string_view kCurinos = "Curinos";
 constexpr std::string_view kDiscover = "Discover";
 constexpr std::string_view kLloyds = "Lloyds";
 constexpr std::string_view kMarqeta = "Marqeta";
@@ -118,9 +121,13 @@ struct CardMetadataLoggingContext {
   int64_t selected_card_instrument_id;
 };
 
-// Get histogram suffix based on given card issuer id or network.
+// Get histogram suffix based on a given card issuer id or network.
 std::string_view GetCardIssuerIdOrNetworkSuffix(
     const std::string& card_issuer_id_or_network);
+
+// Get histogram suffix based on a given card benefit source.
+std::string_view GetCardBenefitSourceSuffix(
+    const std::string& card_benefit_source);
 
 // Get the CardMetadataLoggingContext for the given credit cards.
 CardMetadataLoggingContext GetMetadataLoggingContext(
