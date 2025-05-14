b0695c5a87abd887f30f7f3f75fde0edc11baed7
anthie@google.com <anthie@google.com>
2025-05-14 09:03:21 -0700

    Populate avatar in History Sync Optin dialog and add background
    This CL gets the avatar of the primary signed-in account, if the
    information is already available, or observes the identify
    manager until the avatar is updated.
    The update avatar source url is sent via a new mojom method to the
    UI.
    
    The CL also adds a background illustration on the dialog, centered
    around the user's avatar (mocks: https://screenshot.googleplex.com/6M73PubSPD6ZBjm).
    
    Screenshot: https://screenshot.googleplex.com/63fNGbHNrsmdBPa
    
    Bug: 416213087
    Change-Id: I638476001e95229f5abc81e717b868eddcb65c27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519631
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Auto-Submit: Anthi Orfanou <anthie@google.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460138}

--- chrome/browser/ui/webui/signin/history_sync_optin/history_sync_optin_handler.cc
+++ chrome/browser/ui/webui/signin/history_sync_optin/history_sync_optin_handler.cc
@@ -11,10 +11,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/webui/signin/signin_utils.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/sync/protocol/user_consent_types.pb.h"
 #include "components/sync/service/sync_service.h"
 #include "components/sync/service/sync_user_settings.h"
+
+namespace {
+history_sync_optin::mojom::AccountInfoPtr CreateAccountInfoDataMojo(
+    const AccountInfo& info) {
+  history_sync_optin::mojom::AccountInfoPtr account_info_mojo =
+      history_sync_optin::mojom::AccountInfo::New();
+  account_info_mojo->account_image_src =
+      GURL(signin::GetAccountPictureUrl(info));
+  return account_info_mojo;
+}
+}  // namespace
 
 HistorySyncOptinHandler::HistorySyncOptinHandler(
     mojo::PendingReceiver<history_sync_optin::mojom::PageHandler> receiver,
@@ -24,8 +36,10 @@ HistorySyncOptinHandler::HistorySyncOptinHandler(
     : receiver_(this, std::move(receiver)),
       page_(std::move(page)),
       browser_(browser ? browser->AsWeakPtr() : nullptr),
-      profile_(profile) {
+      profile_(profile),
+      identity_manager_(IdentityManagerFactory::GetForProfile(profile_)) {
   CHECK(profile_);
+  CHECK(identity_manager_);
 }
 
 HistorySyncOptinHandler::~HistorySyncOptinHandler() = default;
@@ -37,6 +51,23 @@ void HistorySyncOptinHandler::Accept() {
 
 void HistorySyncOptinHandler::Reject() {
   FinishAndCloseDialog();
+}
+
+void HistorySyncOptinHandler::RequestAccountInfo() {
+  MaybeGetAccountInfo();
+}
+
+void HistorySyncOptinHandler::MaybeGetAccountInfo() {
+  AccountInfo primary_account_info = identity_manager_->FindExtendedAccountInfo(
+      identity_manager_->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin));
+
+  if (!primary_account_info.IsEmpty()) {
+    DispatchAccountInfoUpdate(primary_account_info);
+  }
+
+  if (!identity_manager_observation_.IsObserving()) {
+    identity_manager_observation_.Observe(identity_manager_);
+  }
 }
 
 void HistorySyncOptinHandler::FinishAndCloseDialog() {
@@ -51,13 +82,37 @@ void HistorySyncOptinHandler::AddHistorySyncConsent() {
   syncer::SyncService* sync_service =
       SyncServiceFactory::GetForProfile(profile_);
   CHECK(sync_service);
-  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile_);
-  CHECK(identity_manager);
-  CHECK(identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin));
+  CHECK(identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSignin));
   // TODO(crbug.com/404806988): As we add the invocation points check if additional actions
   // are needed to enable sync for history.
   // The invocation below works for an already syncing user. It enables the syncing for history
   // if it's not already turned on.
   sync_service->GetUserSettings()->SetSelectedType(
       syncer::UserSelectableType::kHistory, /*is_type_on=*/true);
+}
+
+void HistorySyncOptinHandler::OnAvatarChanged(const AccountInfo& info) {
+  CHECK(info.IsValid());
+  page_->SendAccountInfo(CreateAccountInfoDataMojo(info));
+}
+
+void HistorySyncOptinHandler::DispatchAccountInfoUpdate(
+    const AccountInfo& info) {
+  if (info.IsEmpty()) {
+    // No account is signed in, so there is nothing to be displayed in the sync
+    // confirmation dialog.
+    return;
+  }
+  if (info.account_id !=
+      identity_manager_->GetPrimaryAccountId(signin::ConsentLevel::kSignin)) {
+    return;
+  }
+  if (info.IsValid()) {
+    OnAvatarChanged(info);
+  }
+}
+
+void HistorySyncOptinHandler::OnExtendedAccountInfoUpdated(
+    const AccountInfo& info) {
+  DispatchAccountInfoUpdate(info);
 }
