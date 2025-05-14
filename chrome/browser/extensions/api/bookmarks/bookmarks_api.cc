da814de2db6f777c0dc648fc02cfac342929440b
Kelvin Jiang <kelvinjiang@chromium.org>
2025-05-14 14:34:53 -0700

    [Extensions] Enable all bookmarks API unit tests on desktop android
    This CL also changes one bookmarks API behavior on desktop android:
    - if create() does not specify a parent bookmark id on desktop android,
      the new bookmark will be added to the "mobile bookmarks" folder
      instead of the "other bookmarks" folder as is the case on other
      desktop OSes.
    
    Bug: 414844449
    Change-Id: Ib83b09cb945b7cc051344c1b842f8b77e6785bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533282
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460366}

--- chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
+++ chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/extension_function_dispatcher.h"
+#include "extensions/buildflags/buildflags.h"
 
 using bookmarks::BookmarkModel;
 using bookmarks::BookmarkNode;
@@ -490,9 +491,21 @@ const BookmarkNode* BookmarksCreateFunction::CreateBookmarkNode(
   int64_t parent_id;
 
   if (!details.parent_id) {
-    // Optional, default to "other bookmarks".
+    // Optional, default to "other bookmarks" as a parent ID on desktop, "mobile
+    // bookmarks" on desktop Android.
+    // TODO(crbug.com/414844449): Currently, desktop Android still saves
+    // bookmarks to the mobile bookmarks folder and the bookmarks bar/other
+    // bookmarks folder are not visible if they are empty. This behavior is
+    // subject to change.
+#if BUILDFLAG(IS_ANDROID)
+    parent_id = model->account_mobile_node()
+                    ? model->account_mobile_node()->id()
+                    : model->mobile_node()->id();
+#else
     parent_id = model->account_other_node() ? model->account_other_node()->id()
                                             : model->other_node()->id();
+#endif  // BUILDFLAG(IS_ANDROID)
+
   } else if (!base::StringToInt64(*details.parent_id, &parent_id)) {
     *error = bookmarks_errors::kInvalidIdError;
     return nullptr;
