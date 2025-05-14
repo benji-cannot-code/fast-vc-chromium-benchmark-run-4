5851dda500afd0b5108f17585266056ebbbcaf7f
“Prasanaa” <prasanaa@google.com>
2025-05-14 09:59:06 -0700

    [iOS] Add footnote in GLIC Consent Flow
    This CL adds a footnote in GLIC Consent Flow.
    
    Bug: 414777915, 414791668
    Change-Id: I4bb4b6053ae72eeab2e7c6b9dbcedf939294e5e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6540271
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460185}

--- ios/chrome/browser/intelligence/glic/ui/glic_consent_view_controller.mm
+++ ios/chrome/browser/intelligence/glic/ui/glic_consent_view_controller.mm
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Configure all the stacks.
 - (void)setupStackView {
   [self configureMainStackView];
+  [_mainStackView addArrangedSubview:[self createSubtextLabel]];
 }
 
 // Configure the differents detents for the expanded and collapsed view.
@@ -110,6 +111,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         constraintLessThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
                                               .bottomAnchor]
   ]];
+}
+
+// Create the subtext label.
+- (UILabel*)createSubtextLabel {
+  UILabel* subText = [[UILabel alloc] init];
+  subText.text = kGLIConsentSubText;
+  subText.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCaption2];
+  subText.numberOfLines = 2;
+  subText.textAlignment = NSTextAlignmentCenter;
+  return subText;
 }
 
 #pragma mark - PromoStyleViewControllerDelegate
