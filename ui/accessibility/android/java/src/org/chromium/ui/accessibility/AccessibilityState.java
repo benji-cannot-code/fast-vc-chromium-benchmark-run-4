7304d204b709ae359831f4364846bfcae96f77de
Mark Schillaci <mschillaci@google.com>
2025-05-14 09:21:40 -0700

    Remove the Animation.java file and use state in AccessibilityState.java
    AX-Relnotes: N/A
    Bug: 401281044
    Change-Id: I4eb30ebfc40d223ad54a2656cb354dd29be6adde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542241
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460149}

--- ui/accessibility/android/java/src/org/chromium/ui/accessibility/AccessibilityState.java
+++ ui/accessibility/android/java/src/org/chromium/ui/accessibility/AccessibilityState.java
@@ -495,6 +495,7 @@ public class AccessibilityState {
     }
 
     /** Returns whether the user settings specify preferred reduced motion. */
+    @CalledByNative
     public static boolean prefersReducedMotion() {
         // We default to assuming that animations are enabled, to avoid impacting the experience for
         // users that don't have ANIMATOR_DURATION_SCALE defined.
@@ -1066,6 +1067,7 @@ public class AccessibilityState {
 
     private static void processExtraStateChange() {
         updateExtraState();
+        AccessibilityStateJni.get().onAnimatorDurationScaleChanged();
         AccessibilityStateJni.get().onDisplayInversionEnabledChanged(isDisplayInversionEnabled());
         AccessibilityStateJni.get().onContrastLevelChanged(isHighContrastEnabled());
     }
--- ui/gfx/animation/animation_android.cc
+++ ui/gfx/animation/animation_android.cc
@@ -5,12 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/animation/animation.h"
 
-#include "base/android/jni_android.h"
-
-// Must come after all headers that specialize FromJniType() / ToJniType().
-#include "ui/gfx/gfx_jni_headers/Animation_jni.h"
-
-using base::android::AttachCurrentThread;
+#include "ui/accessibility/android/accessibility_state.h"
 
 namespace gfx {
 
@@ -19,9 +14,7 @@ void Animation::UpdatePrefersReducedMotion() {
   // prefers_reduced_motion_ should only be modified on the UI thread.
   // TODO(crbug.com/40611878): DCHECK this assertion once tests are
   // well-behaved.
-
-  JNIEnv* env = AttachCurrentThread();
-  prefers_reduced_motion_ = Java_Animation_prefersReducedMotion(env);
+  prefers_reduced_motion_ = ui::AccessibilityState::PrefersReducedMotion();
 }
 
 }  // namespace gfx
