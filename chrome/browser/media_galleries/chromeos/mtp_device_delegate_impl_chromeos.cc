ef142bb61e72532c9e49df0317a9b3ec77c013c3
François Degros <fdegros@chromium.org>
2025-05-13 23:38:55 -0700

    Fix bug in MTPDeviceDelegateImplLinux::EvictCachedPathToId()
    Fix bug that was inadvertently introduced by CL 6356730, due to a typo
    in an operator.
    Also add a verbose log.
    
    Bug: 414649697, 393520050
    Change-Id: Icec84aed30564265636b9687a89a26e465629805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6544929
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459899}

--- chrome/browser/media_galleries/chromeos/mtp_device_delegate_impl_chromeos.cc
+++ chrome/browser/media_galleries/chromeos/mtp_device_delegate_impl_chromeos.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/safe_base_name.h"
 #include "base/functional/bind.h"
+#include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
 #include "base/not_fatal_until.h"
@@ -2013,7 +2014,8 @@ std::optional<uint32_t> MTPDeviceDelegateImplLinux::CachedPathToId(
 
 void MTPDeviceDelegateImplLinux::EvictCachedPathToId(uint32_t id) {
   const MTPFileNodesById::const_iterator it = nodes_by_id_.find(id);
-  if (it != nodes_by_id_.cend()) {
+  if (it == nodes_by_id_.cend()) {
+    VLOG(1) << "Cannot find MTP node with file ID " << id;
     return;
   }
 
