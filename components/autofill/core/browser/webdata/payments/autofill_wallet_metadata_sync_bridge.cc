ad780ba6d0502fbfb630b86277616232f732ff6f
Qihui Zhao <qihuizhao@google.com>
2025-05-14 09:15:05 -0700

    Merge Add/Update method for card metadata in autofill_table
    Bug: b:40944567
    Change-Id: If560ef1a7e22c5ade7f3b0f0816bc1111874df2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6531684
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460144}

--- components/autofill/core/browser/webdata/payments/autofill_wallet_metadata_sync_bridge.cc
+++ components/autofill/core/browser/webdata/payments/autofill_wallet_metadata_sync_bridge.cc
@@ -243,7 +243,7 @@ bool AddServerMetadata(PaymentsAutofillTable* table,
                        const PaymentsMetadata& metadata) {
   switch (type) {
     case WalletMetadataSpecifics::CARD:
-      return table->AddServerCardMetadata(metadata);
+      return table->AddOrUpdateServerCardMetadata(metadata);
     case WalletMetadataSpecifics::IBAN:
       return table->AddOrUpdateServerIbanMetadata(metadata);
     // ADDRESS metadata syncing is deprecated.
@@ -273,7 +273,7 @@ bool UpdateServerMetadata(PaymentsAutofillTable* table,
                           const PaymentsMetadata& metadata) {
   switch (type) {
     case WalletMetadataSpecifics::CARD:
-      return table->UpdateServerCardMetadata(metadata);
+      return table->AddOrUpdateServerCardMetadata(metadata);
     case WalletMetadataSpecifics::IBAN:
       return table->AddOrUpdateServerIbanMetadata(metadata);
     // ADDRESS metadata syncing is deprecated.
--- components/autofill/core/browser/webdata/payments/autofill_wallet_metadata_sync_bridge_unittest.cc
+++ components/autofill/core/browser/webdata/payments/autofill_wallet_metadata_sync_bridge_unittest.cc
@@ -1010,7 +1010,7 @@ TEST_F(AutofillWalletMetadataSyncBridgeTest,
           kCard1SpecificsId, /*use_count=*/30, /*use_date=*/40);
 
   // Save only metadata and not data - simulate an orphan.
-  table()->AddServerCardMetadata(
+  table()->AddOrUpdateServerCardMetadata(
       CreateServerCreditCardFromSpecifics(card).GetMetadata());
 
   // Make the orphans old by advancing time.
@@ -1097,7 +1097,7 @@ TEST_F(AutofillWalletMetadataSyncBridgeTest,
           UseDateToProtoValue(base::Time::Now() - base::Minutes(1)));
 
   // Save only metadata and not data - simulate an orphan.
-  table()->AddServerCardMetadata(
+  table()->AddOrUpdateServerCardMetadata(
       CreateServerCreditCardFromSpecifics(card).GetMetadata());
 
   // We do not advance time so the orphans are recent, should not get deleted.
