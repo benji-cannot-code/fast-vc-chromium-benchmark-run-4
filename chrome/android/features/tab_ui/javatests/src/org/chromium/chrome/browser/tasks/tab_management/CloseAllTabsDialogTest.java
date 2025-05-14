a0ec0d5a0997fe27f4abfd59465658f0af33767e
Nafis Abedin <nafisabedin@google.com>
2025-05-14 11:59:50 -0700

    [Sheriff] Disable flaky CloseAllTabsDialogTest test
    Different parameters of this test have been flaky recently with the top
    flake being the Incognito variant.
    
    Bug: 417752224
    Change-Id: I3800a04fd83f8484560f1cdbf2c83b9e3dfb2301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6547877
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Nafis Abedin <nafisabedin@google.com>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Owners-Override: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460276}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/CloseAllTabsDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/CloseAllTabsDialogTest.java
@@ -32,6 +32,7 @@ import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.TestAnimations.EnableAnimations;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -115,6 +116,7 @@ public class CloseAllTabsDialogTest {
     @LargeTest
     @EnableAnimations
     @Restriction({DeviceFormFactor.PHONE})
+    @DisabledTest(message = "Flaky - crbug.com/417752224")
     public void testCloseAllTabs_CustomAnimation() {
         TabModelSelector selector =
                 mActivityTestRule.getActivity().getTabModelSelectorSupplier().get();
