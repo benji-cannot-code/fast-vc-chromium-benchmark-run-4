71236feb077a7142c348049cf317ca2dfac22bcc
James Cook <jamescook@chromium.org>
2025-05-13 18:46:16 -0700

    extensions: Use ChromeExtensionsBrowserClient on desktop Android, part 6
    Move a task manager support method from _non_android.cc to the shared
    cross-platform .cc file. There's an experimental task manager for
    desktop Android, so most of the task manager code is already compiling
    on Android.
    
    Add a few files to the desktop Android build. Clarify ENABLE_GUEST_VIEW
    vs. ENABLE_EXTENSIONS_CORE vs. IS_ANDROID in the C++ and BUILD.gn files.
    Add the ExtensionTagsTest to android_browsertests.
    
    Bug: 415033270
    Test: android_browsertests, browser_tests
    Change-Id: I920cd9322022e93c50c3671c9996725e37f4ff7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542428
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459836}

--- chrome/browser/task_manager/providers/web_contents/extension_tag.cc
+++ chrome/browser/task_manager/providers/web_contents/extension_tag.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/buildflags/buildflags.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "extensions/browser/process_manager.h"
 #endif
 
--- chrome/browser/task_manager/web_contents_tags.h
+++ chrome/browser/task_manager/web_contents_tags.h
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_TASK_MANAGER_WEB_CONTENTS_TAGS_H_
 
 #include "build/build_config.h"
+#include "components/guest_view/buildflags/buildflags.h"
 #include "components/webapps/common/web_app_id.h"
+#include "extensions/buildflags/buildflags.h"
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "extensions/common/mojom/view_type.mojom.h"  // nogncheck
-#endif                                                // !BUILDFLAG(IS_ANDROID)
+#endif  // !BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
 #if !BUILDFLAG(IS_ANDROID)
 class BackgroundContents;
@@ -75,14 +77,16 @@ class WebContentsTags {
   // up by the caller, as it is owned by |web_contents|.
   static void CreateForPrintingContents(content::WebContents* web_contents);
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(ENABLE_GUEST_VIEW)
   // Tags a WebContents owned by a GuestViewBase so that it shows up in the
   // task manager. Calling this function creates a GuestTag, and attaches it to
   // |web_contents|. If an instance is already attached, this does nothing. The
   // resulting tag does not have to be cleaned up by the caller, as it is owned
   // by |web_contents|.
   static void CreateForGuestContents(content::WebContents* web_contents);
+#endif  // BUILDFLAG(ENABLE_GUEST_VIEW)
 
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   // Tags a WebContents that belongs to |extension| so that it shows up in the
   // task manager. Calling this function creates a ExtensionTag, and attaches
   // it to |web_contents|. If an instance is already attached, this does
@@ -92,7 +96,7 @@ class WebContentsTags {
   // non-background contents Extension.
   static void CreateForExtension(content::WebContents* web_contents,
                                  extensions::mojom::ViewType view_type);
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
   // Tags a WebContents created for a tool so that it shows up in the task
   // manager. Calling this function creates a ToolTag, and attaches it to
