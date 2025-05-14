671887571304374c60e12af604c2eeb00ef3adf4
Corentin Wallez <cwallez@chromium.org>
2025-05-13 23:40:56 -0700

    [webgl] Factor the WebGL1/2 factories together.
    Both the WebGLRenderingContext and WebGL2RenderingContext have Factory
    classes that are registered globally to handle a specific type of
    <canvas>.getContext() call. The code is extremely similar and can be
    factored, which will also be useful later to decide whether to create a
    regular WebGL context or one backed by ANGLE on WebGPU.
    
    Bug: 414506479
    Change-Id: Icbf1fe78abfe60239962eedb35bdd3989b34a947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501300
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459900}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
-#include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context_factory.h"
 #include "third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h"
 
 namespace blink {
@@ -42,26 +41,6 @@ class WebGLRenderingContext final : public WebGLRenderingContextBase {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  class Factory : public CanvasRenderingContextFactory {
-   public:
-    Factory() = default;
-
-    Factory(const Factory&) = delete;
-    Factory& operator=(const Factory&) = delete;
-
-    ~Factory() override = default;
-
-    CanvasRenderingContext* Create(
-        CanvasRenderingContextHost*,
-        const CanvasContextCreationAttributesCore&) override;
-
-    CanvasRenderingContext::CanvasRenderingAPI GetRenderingAPI()
-        const override {
-      return CanvasRenderingContext::CanvasRenderingAPI::kWebgl;
-    }
-    void OnError(HTMLCanvasElement*, const String& error) override;
-  };
-
   WebGLRenderingContext(CanvasRenderingContextHost*,
                         std::unique_ptr<WebGraphicsContext3DProvider>,
                         const Platform::GraphicsInfo&,
