d4b0972d7aa900f0776f282bee929e3e8b1391c0
David Bokan <bokan@chromium.org>
2025-05-14 12:36:13 -0700

    Settings: AI Innovations shown only when a feature is enabled
    This regressed in https://crrev.com/c/6420452 as the Glic forces the AI
    subpage to be shown (advancedFeaturesMainControl). However, if there
    are no enabled non-Glic AI features we'd still show the AI innovations
    section since its visibility is implied by advancedFeaturesMainControl.
    
    This CL adds a separate `showAiFeaturesSection` setting that is used to
    control this section independently of the AI subpage.
    
    Fixed: 417379598
    Change-Id: Icba8081a62e46f1669773e45fb879e4234955755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543508
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460304}

--- chrome/browser/resources/settings/basic_page/basic_page.ts
+++ chrome/browser/resources/settings/basic_page/basic_page.ts
@@ -175,6 +175,11 @@ export class SettingsBasicPageElement extends SettingsBasicPageElementBase {
         computed: 'computeAiPageTitle_(enableAiSettingsPageRefresh_)',
       },
 
+      showAiPageAiFeatureSection_: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('showAiPageAiFeatureSection'),
+      },
+
       // <if expr="enable_glic">
       showGlicSection_: {
         type: Boolean,
@@ -204,6 +209,7 @@ export class SettingsBasicPageElement extends SettingsBasicPageElementBase {
   declare private showAdvancedFeaturesMainControl_: boolean;
   declare private enableAiSettingsPageRefresh_: boolean;
   declare private aiPageTitle_: string;
+  declare private showAiPageAiFeatureSection_: boolean;
   // <if expr="enable_glic">
   declare private showGlicSection_: boolean;
   // </if>
