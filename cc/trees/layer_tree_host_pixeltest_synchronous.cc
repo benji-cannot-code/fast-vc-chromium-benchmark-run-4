3275855920d7977ff904c66579176d1a7c3bd819
Victor Miura <vmiura@chromium.org>
2025-05-14 11:50:23 -0700

    [TreesInViz] Clean up single thread scheduling assumptions in tests
    A few cc_unittests that run in single-threaded, no-scheduler mode would
    fail because they would submit a frame before a Commit happened, that
    would have initialized the local_surface_id for the Active tree.
    
    As a workaround for this, TestLayerTreeFrameSink faked-up a
    local_surface_id to use for frame submits.
    
    In addition, in the non-scheduler mode TestLayerTreeFrameSink took care
    of issuing Display::DrawAndSwap.
    
    Those workarounds didn't port correctly to TreesInViz mode.
    
    In place of those, we remove the faked local_surface_id, and instead
    ensure that all of these tests initialize a local_surface_id on the
    Active tree. We also move the Display::DrawAndSwap calls to
    TestCompositorFrameSinkSupport, where they will also work for TreesInViz.
    
    Bug: 406273638
    Change-Id: Ifc80abda1a1baa950e2d1532c621852ddb418189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543309
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460270}

--- cc/trees/layer_tree_host_pixeltest_synchronous.cc
+++ cc/trees/layer_tree_host_pixeltest_synchronous.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/fake_content_layer_client.h"
 #include "cc/test/layer_tree_pixel_test.h"
 #include "cc/test/pixel_comparator.h"
+#include "cc/trees/layer_tree_impl.h"
 #include "components/viz/test/test_types.h"
 
 #if !BUILDFLAG(IS_ANDROID)
