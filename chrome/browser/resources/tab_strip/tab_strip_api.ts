392fbd3af6589ddcd69a6df40745b7852afa39c0
David Yeung <dayeung@chromium.org>
2025-05-14 12:32:46 -0700

    Fork WebUI Tabstrip for testing
    Forks the tab_list.ts and tab_strip.html file into a
    tab_list_playground.ts and tab_strip_playground.html. This is a
    playground html/js file that is only used if TabStripBrowserApi is
    enabled. Primarily for development purposes while integrating with the
    new TabStripService api. This allows for more direct changes to the ts
    file without having to add many if/else statements within the current
    tab_list.ts file.
    
    This is for the WebUI tabstrip which is only used on chromeos touch.
    
    Bug: 416731772
    Change-Id: I333b47d2e3be3aeba14b32396fc63d7d814c4d77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534066
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460302}

--- chrome/browser/resources/tab_strip/tab_strip_api.ts
+++ chrome/browser/resources/tab_strip/tab_strip_api.ts
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import type {Url} from '//resources/mojo/url/mojom/url.mojom-webui.js';
 
 import {TabsObserverCallbackRouter, TabStripService} from './tab_strip_api.mojom-webui.js';
-import type {Position, TabStripServiceRemote} from './tab_strip_api.mojom-webui.js';
+import type {Position, Tab, TabId, TabsSnapshot, TabStripServiceRemote} from './tab_strip_api.mojom-webui.js';
 
 export interface TabStripApiProxy {
   createTabAt(pos: Position|null, url: Url|null): Promise<boolean>;
+  getTabs(): Promise<TabsSnapshot>;
+  getTab(id: TabId): Promise<Tab>;
+  getCallbackRouter(): TabsObserverCallbackRouter;
 }
 
 export class TabStripApiProxyImpl implements TabStripApiProxy {
@@ -18,6 +21,18 @@ export class TabStripApiProxyImpl implements TabStripApiProxy {
 
   createTabAt(pos: Position|null, url: Url|null) {
     return this.service.createTabAt(pos, url);
+  }
+
+  getTabs(): Promise<TabsSnapshot> {
+    return this.service.getTabs();
+  }
+
+  getTab(id: TabId): Promise<Tab> {
+    return this.service.getTab(id);
+  }
+
+  getCallbackRouter(): TabsObserverCallbackRouter {
+    return this.observer;
   }
 
   static getInstance(): TabStripApiProxy {
