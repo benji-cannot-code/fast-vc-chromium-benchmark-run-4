7d9f1abe1fc1d7597525f4421036c241683fe6b2
Nicolás Peña <npm@chromium.org>
2025-05-14 08:15:57 -0700

    [FedCM] Move image fetching to IdpNetworkRequestManager
    This CL moves image fetching out of FederatedAuthRequestImpl. To do so
    more easily, it also moves IdentityProviderInfo outside as well.
    
    Bug: 417197032
    Change-Id: I4569b3fc9e97055eb17da97bcbd10f026cfd16ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543511
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460107}

---
+++ content/browser/webid/identity_provider_info.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/webid/identity_provider_info.h"
+
+namespace content {
+
+IdentityProviderInfo::IdentityProviderInfo(
+    const blink::mojom::IdentityProviderRequestOptionsPtr& provider,
+    IdpNetworkRequestManager::Endpoints endpoints,
+    IdentityProviderMetadata metadata,
+    blink::mojom::RpContext rp_context,
+    blink::mojom::RpMode rp_mode,
+    std::optional<blink::mojom::Format> format)
+    : provider(provider->Clone()),
+      endpoints(std::move(endpoints)),
+      metadata(std::move(metadata)),
+      rp_context(rp_context),
+      rp_mode(rp_mode),
+      format(format) {}
+
+IdentityProviderInfo::~IdentityProviderInfo() = default;
+IdentityProviderInfo::IdentityProviderInfo(const IdentityProviderInfo& other) {
+  provider = other.provider->Clone();
+  endpoints = other.endpoints;
+  metadata = other.metadata;
+  has_failing_idp_signin_status = other.has_failing_idp_signin_status;
+  rp_context = other.rp_context;
+  rp_mode = other.rp_mode;
+  data = other.data;
+  format = other.format;
+}
+
+}  // namespace content
