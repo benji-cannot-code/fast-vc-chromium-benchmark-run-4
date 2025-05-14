7ead2fdabf8a1efccef3caf0a6faa43e3368992c
Gyuyoung Kim <gyuyoung@igalia.com>
2025-05-13 17:51:18 -0700

    [iOS Blink] Support DataDecoderBrowserTest.LaunchIsolated
    Since https://crrev.com/c/6518241 switched from using a JSON parser to
    ImageDecoder, the iOS Blink port needs to follow the same approach. This
    CL enables the use of ImageDecoder in the iOS Blink port by applying the
    USE_BLINK build flag.
    
    TEST: DataDecoderBrowserTest.LaunchIsolated Bug: None
    
    Change-Id: Ie9bc02f5a9251d257dc8bbe55f40faf40fada74d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6529504
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1459819}

--- services/data_decoder/data_decoder_service.cc
+++ services/data_decoder/data_decoder_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/time/time.h"
+#include "build/blink_buildflags.h"
 #include "build/build_config.h"
 #include "components/facilitated_payments/core/validation/pix_code_validator.h"
 #include "components/web_package/web_bundle_parser_factory.h"
@@ -24,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/data_decoder/ble_scan_parser_impl.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
-#if !BUILDFLAG(IS_IOS)
+#if BUILDFLAG(USE_BLINK)
 #include "services/data_decoder/image_decoder_impl.h"
 #endif
 
@@ -46,8 +47,8 @@ void DataDecoderService::BindReceiver(
 
 void DataDecoderService::BindImageDecoder(
     mojo::PendingReceiver<mojom::ImageDecoder> receiver) {
-#if BUILDFLAG(IS_IOS)
-  LOG(FATAL) << "ImageDecoder not supported on iOS.";
+#if !BUILDFLAG(USE_BLINK)
+  LOG(FATAL) << "ImageDecoder not supported on non-Blink platforms.";
 #else
   mojo::MakeSelfOwnedReceiver(std::make_unique<ImageDecoderImpl>(),
                               std::move(receiver));
