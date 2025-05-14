c05f17167659340986bcee346acac09c14436224
Sylvain Defresne <sdefresne@chromium.org>
2025-05-14 07:21:40 -0700

    [ios] Fix Use-after-Free in AutofillProgressDialogMediator
    The implementation of AutofillProgressDialogMediator contained a
    block that captured `this` (via an access to a member field). To
    fix this, introduce an helper method `DismissDialog()` and change
    the block by a callback (this make it harder to capture a pointer).
    
    Also change the constructor to take the pointer as raw C++ pointer
    instead of `base::WeakPtr<T>` since passing a weak pointer should
    only be used internally.
    
    Also uses base::TaskRunner::PostTask instead of dispatch_async to
    post asynchronous tasks (since it is recommended to use Chromium
    primitives, especially in C++ code).
    
    Found by locally running ios_chrome_unittests and experiencing
    random crashes with when sending method to zombie Objective-C
    objects that looked like this:
    
    ```
    [       OK ] BookmarksUtilsTest.GetDefaultBookmarkFolderWithDefaultBookmarkSet (58 ms)
    [ RUN      ] BookmarksUtilsTest.PrimaryPermanentNodes
    *** Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: '-[NSTaggedPointerString dismissDialog]: unrecognized selector sent to instance 0xbadbad00badbad00'
    *** First throw call stack:
    (
      0   CoreFoundation                      0x00007ff8004ca369 __exceptionPreprocess + 242
      1   libobjc.A.dylib                     0x00007ff80008c106 objc_exception_throw + 62
      2   CoreFoundation                      0x00007ff8004dff1c +[NSObject(NSObject) instanceMethodSignatureForSelector:] + 0
      3   CoreFoundation                      0x00007ff8004ceccc ___forwarding___ + 1459
      4   CoreFoundation                      0x00007ff8004d1088 _CF_forwarding_prep_0 + 120
      5   ios_chrome_unittests                0x0000000106dd92c1 ___ZN30AutofillProgressDialogMediator7DismissEbb_block_invoke + 49
      6   libdispatch.dylib                   0x0000000125ba9ec6 _dispatch_client_callout + 8
      7   libdispatch.dylib                   0x0000000125bad0f3 _dispatch_continuation_pop + 803
      8   libdispatch.dylib                   0x0000000125bc2d0d _dispatch_source_invoke + 2240
      9   libdispatch.dylib                   0x0000000125bb9b8b _dispatch_main_queue_drain + 955
      10  libdispatch.dylib                   0x0000000125bb97c2 _dispatch_main_queue_callback_4CF + 31
      11  CoreFoundation                      0x00007ff80042945f __CFRUNLOOP_IS_SERVICING_THE_MAIN_DISPATCH_QUEUE__ + 9
      12  CoreFoundation                      0x00007ff800423d87 __CFRunLoopRun + 2515
      13  CoreFoundation                      0x00007ff800422fb3 CFRunLoopRunSpecific + 536
      14  Foundation                          0x00007ff800fcc4a3 -[NSRunLoop(NSRunLoop) runMode:beforeDate:] + 213
      15  Foundation                          0x00007ff800fcc719 -[NSRunLoop(NSRunLoop) runUntilDate:] + 72
    ```
    
    Bug: none
    Change-Id: I48dccc4b10bfc7277563e899039609e1facad7e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6545990
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460061}

--- ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_coordinator.mm
+++ ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_coordinator.mm
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     CHECK(paymentsClient);
     _modelController = paymentsClient->GetProgressDialogModel();
     _mediator = std::make_unique<AutofillProgressDialogMediator>(
-        _modelController->GetImplWeakPtr(), self);
+        _modelController.get(), self);
   }
   return self;
 }
