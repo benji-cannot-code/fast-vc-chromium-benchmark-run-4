7a4af2be828ac309fec3808cb13f134b72d3a4b1
Nan Lin <linnan@chromium.org>
2025-05-14 09:50:09 -0700

    Clean up stale base::Feature EcheSWAProcessAndroidAccessibilityTree
    Fixed: 362247265
    Change-Id: I03c773a36d1d8dcba0052919241a938488b9f778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6541909
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Abbas Nayebi <nayebi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460178}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4023,9 +4023,6 @@ extern const char kEcheSWADisableStunServerDescription[];
 extern const char kEcheSWACheckAndroidNetworkInfoName[];
 extern const char kEcheSWACheckAndroidNetworkInfoDescription[];
 
-extern const char kEcheSWAProcessAndroidAccessibilityTreeName[];
-extern const char kEcheSWAProcessAndroidAccessibilityTreeDescription[];
-
 extern const char kEnableOAuthIppName[];
 extern const char kEnableOAuthIppDescription[];
 
