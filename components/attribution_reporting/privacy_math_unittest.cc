04bb4608d558660a12a430ea78699c0f9510986a
Andrew Paseltiner <apaseltiner@chromium.org>
2025-05-14 08:36:19 -0700

    Merge privacy_math_fuzztest target into existing unit tests
    Change-Id: Iad2d0435fd49080bb2d8aa7958e30bf3e47ae0d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6545804
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460128}

--- components/attribution_reporting/privacy_math_unittest.cc
+++ components/attribution_reporting/privacy_math_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/attribution_reporting/trigger_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 
 namespace attribution_reporting {
 namespace {
@@ -861,6 +862,16 @@ TEST(PrivacyMathTest, IsValid) {
     EXPECT_EQ(test_case.expected, IsValid(test_case.response, kSpecs));
   }
 }
+
+void GetKCombinationAtIndexDoesNotCrash(uint32_t combination_index,
+                                        uint32_t k) {
+  internal::GetKCombinationAtIndex(combination_index, k);
+}
+
+FUZZ_TEST(PrivacyMathTest, GetKCombinationAtIndexDoesNotCrash)
+    .WithDomains(
+        /*combination_index=*/fuzztest::Arbitrary<uint32_t>(),
+        /*k=*/fuzztest::InRange<uint32_t>(0, 20));
 
 }  // namespace
 }  // namespace attribution_reporting
