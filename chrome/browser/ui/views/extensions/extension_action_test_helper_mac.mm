da9c2d4ddae4712870f5704829c8778aa448e729
Justin Lulejian <jlulejian@chromium.org>
2025-05-14 11:01:04 -0700

    [Extensions] Handle missing key window when waiting for popups.
    Prior art crrev.com/c/6221803. There are known problems with windows
    becoming key in tests with the past couple macOS releases. Before this
    change multiple tests in the linked bug would flake when the
    notification wouldn't be observed.
    
    This change tries HandleMissingKeyWindow() when there isn't a key window
    to see if that helps with the flakes.
    
    Bug: 417408653
    Change-Id: I23aabdb01dbcb80570e0d76f8135d8beeb9e768a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6541927
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460239}

--- chrome/browser/ui/views/extensions/extension_action_test_helper_mac.mm
+++ chrome/browser/ui/views/extensions/extension_action_test_helper_mac.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <AppKit/AppKit.h>
 
+#include "content/public/test/browser_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "ui/base/test/windowed_nsnotification_observer.h"
 
@@ -15,6 +16,7 @@ void ExtensionActionTestHelper::WaitForPopup() {
   ASSERT_TRUE(window);
 
   if (!window.keyWindow) {
+    content::HandleMissingKeyWindow();
     WindowedNSNotificationObserver* waiter =
         [[WindowedNSNotificationObserver alloc]
             initForNotification:NSWindowDidBecomeKeyNotification
