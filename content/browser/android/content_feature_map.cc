487804ac3548d4750216cbbd34aa1301e06290c0
David Tseng <dtseng@google.com>
2025-05-14 08:22:28 -0700

    android-a11y: add AccessibilityDeprecateJavaNodeCache feature flag and use
    R=mschillaci@google.com
    
    Bug: 417302062
    Change-Id: I650ca98e9a323cbb3b7bb4727eec53ee703ec55c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533646
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460111}

--- content/browser/android/content_feature_map.cc
+++ content/browser/android/content_feature_map.cc
@@ -29,6 +29,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &blink::features::kStylusRichGestures,
     &blink::features::kViewportSegments,
     &input::features::kInputOnViz,
+    &features::kAccessibilityDeprecateJavaNodeCache,
     &features::kAccessibilityDeprecateTypeAnnounce,
     &features::kAccessibilityIncludeLongClickAction,
     &features::kAccessibilityPageZoomV2,
