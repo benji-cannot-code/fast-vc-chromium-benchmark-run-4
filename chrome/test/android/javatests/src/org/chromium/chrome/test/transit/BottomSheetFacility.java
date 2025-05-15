8b69994b7a51117b658d100979fa589cabb318de
Fiaz Muhammad <mfiaz@google.com>
2025-05-15 12:33:08 -0700

    [Tab Group Parity] Init TabGroupListBottomSheet Public Transit Classes
    - New BottomSheetFacility, TabGroupListBottomSheetFacility classes
    - New condition for checking tab groups
    
    Bug: 414381396
    Change-Id: Ib18a4f1c24c1b384e0003e908bfe29c7e49b928c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551538
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460898}

---
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/BottomSheetFacility.java
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.transit;
+
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import static org.chromium.base.test.transit.ViewSpec.viewSpec;
+
+import android.view.View;
+
+import androidx.test.espresso.Espresso;
+
+import org.hamcrest.Matcher;
+
+import org.chromium.base.test.transit.Facility;
+import org.chromium.base.test.transit.Station;
+import org.chromium.base.test.transit.ViewElement;
+import org.chromium.base.test.transit.ViewSpec;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.test.R;
+
+/**
+ * Bottom Sheet that appears to move a tab or list of tabs to a tab group.
+ *
+ * @param <HostStationT> the type of station this is scoped to.
+ */
+public abstract class BottomSheetFacility<HostStationT extends Station<ChromeTabbedActivity>>
+        extends Facility<HostStationT> {
+    private static final Matcher<View> BOTTOM_SHEET_MATCHER = withId(R.id.sheet_container);
+    protected final ViewElement<View> mBottomSheetContent;
+
+    /** Constructor. Expects a specific title and selected color. */
+    public BottomSheetFacility() {
+        mBottomSheetContent = declareView(viewSpec(BOTTOM_SHEET_MATCHER));
+    }
+
+    protected final <ViewT extends View> ViewElement<ViewT> declareDescendantView(
+            ViewSpec<ViewT> viewSpec) {
+        ViewSpec<ViewT> descendant =
+                mBottomSheetContent.descendant(viewSpec.getViewClass(), viewSpec.getViewMatcher());
+        return declareView(descendant);
+    }
+
+    /** Press the system backpress to close the bottom sheet. */
+    public void close() {
+        mHostStation.exitFacilitySync(this, Espresso::pressBack);
+    }
+}
