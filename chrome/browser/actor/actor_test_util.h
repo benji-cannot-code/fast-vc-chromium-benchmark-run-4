5ee14574e8b29e27249b937e0d01e6f7d00757d3
Rodney Ding <rodneyding@google.com>
2025-05-15 10:42:51 -0700

    Support coordinate target in click tool
    - Additional safe check will be deferred until TOCTOU design is ready.
    
    Bug: 409568324
    Change-Id: Ia915787875f80f29eaf78c4b6764286cc12e7d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6546915
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460835}

--- chrome/browser/actor/actor_test_util.h
+++ chrome/browser/actor/actor_test_util.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace actor {
 
 optimization_guide::proto::BrowserAction MakeClick(int content_node_id);
+optimization_guide::proto::BrowserAction MakeClick(
+    const gfx::Point& click_point);
 optimization_guide::proto::BrowserAction MakeHistoryBack();
 optimization_guide::proto::BrowserAction MakeHistoryForward();
 optimization_guide::proto::BrowserAction MakeMouseMove(int content_node_id);
