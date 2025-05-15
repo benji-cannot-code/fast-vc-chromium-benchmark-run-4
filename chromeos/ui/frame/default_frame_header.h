d0ad76cc084ce977436b645e6e164112cb8a6b80
Lingyu Feng <lingyufeng@chromium.org>
2025-05-14 20:48:55 -0700

    Remove Ash.Frame.ColorChangeCount.*
    These UMA metrics are no longer used.
    
    Bug: 407543460
    Test: presubmit
    Change-Id: I71fc7ca39b40d88f43590520dcc6abd909366e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543289
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460495}

--- chromeos/ui/frame/default_frame_header.h
+++ chromeos/ui/frame/default_frame_header.h
@@ -6,13 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_UI_FRAME_DEFAULT_FRAME_HEADER_H_
 #define CHROMEOS_UI_FRAME_DEFAULT_FRAME_HEADER_H_
 
-#include <memory>
-
 #include "base/compiler_specific.h"  // override
 #include "base/component_export.h"
 #include "base/gtest_prod_util.h"
 #include "chromeos/ui/base/chromeos_ui_constants.h"
-#include "chromeos/ui/frame/frame_color_metrics_helper.h"
 #include "chromeos/ui/frame/frame_header.h"
 
 namespace ash {
@@ -59,17 +56,10 @@ class COMPONENT_EXPORT(CHROMEOS_UI_FRAME) DefaultFrameHeader
   // Returns the window of the target widget.
   aura::Window* GetTargetWindow();
 
-  // This function should be only called once, i.e., one instance of
-  // default_frame_header should own only one `frame_color_metrics_helper_`
-  // throughout its lifetime.
-  void InitializeFrameColorMetricsHelper();
-
   SkColor active_frame_color_ = chromeos::kDefaultFrameColor;
   SkColor inactive_frame_color_ = chromeos::kDefaultFrameColor;
 
   int width_in_pixels_ = -1;
-
-  std::unique_ptr<FrameColorMetricsHelper> frame_color_metrics_helper_;
 };
 
 }  // namespace chromeos
