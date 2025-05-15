a10d09a5924d874f6c733a4e7d95ef7ef528678b
Takuto Ikuta <tikuta@chromium.org>
2025-05-15 08:18:42 -0700

    ui: Add missing includes
    This is to fix build error with Clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=thomasanderson@chromium.org
    
    Bug: 40263312
    Change-Id: I741e266d208c65a1f147137fe0a1f8cf59b70e34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549137
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460741}

--- ui/linux/linux_ui_factory.h
+++ ui/linux/linux_ui_factory.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_LINUX_LINUX_UI_FACTORY_H_
 #define UI_LINUX_LINUX_UI_FACTORY_H_
 
+#include <vector>
+
 #include "base/component_export.h"
 #include "base/memory/raw_ptr.h"
 
