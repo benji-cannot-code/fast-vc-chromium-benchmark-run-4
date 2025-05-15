615b211f3a44093b3daaee549568a416e5c8ce4a
Zachary Tan <tanzachary@chromium.org>
2025-05-15 11:06:24 -0700

    [FedCM] Fix dangling raw_ptr in GetScrollPosition
    Makes RecordAccountSelectionScrollPosition static and moves session ID
    code to FederatedAuthRequestImpl so that it can be passed to the static
    method.
    
    Bug: 414916235, 415953689
    Change-Id: Ic9fa363b5c047aeec782733e18aae95c43da4caf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542109
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460855}

--- content/browser/webid/fedcm_metrics.cc
+++ content/browser/webid/fedcm_metrics.cc
@@ -48,16 +48,17 @@ int GetNewSessionID() {
 }  // namespace
 
 FedCmMetrics::FedCmMetrics(ukm::SourceId page_source_id)
-    : page_source_id_(page_source_id) {}
+    : page_source_id_(page_source_id) {
+  session_id_ = GetNewSessionID();
+}
 
 FedCmMetrics::~FedCmMetrics() {
-  int session_id = GetNewSessionID();
   if (fedcm_builder_) {
-    fedcm_builder_->SetFedCmSessionID(session_id)
+    fedcm_builder_->SetFedCmSessionID(session_id_)
         .Record(ukm::UkmRecorder::Get());
   }
   for (auto& [_, builder] : provider_to_fedcm_idp_builder_) {
-    builder->SetFedCmSessionID(session_id).Record(ukm::UkmRecorder::Get());
+    builder->SetFedCmSessionID(session_id_).Record(ukm::UkmRecorder::Get());
   }
 }
 
@@ -727,16 +728,6 @@ void FedCmMetrics::RecordRpUrlHasPath(bool rp_url_has_path) {
   SetUkm(GetOrCreateFedCmBuilder());
 }
 
-void FedCmMetrics::RecordAccountSelectionScrollPosition(
-    const gfx::Point& scroll_position) {
-  auto SetUkm = [&](auto ukm_builder) {
-    ukm_builder->SetAccountSelectionScrollPosition(ukm::GetExponentialBucketMin(
-        scroll_position.y(), /*bucket_spacing=*/1.15));
-  };
-
-  SetUkm(GetOrCreateFedCmBuilder());
-}
-
 void FedCmMetrics::RecordIdentityProvidersCount(int count) {
   CHECK_GT(count, 0);
   base::UmaHistogramCounts100("Blink.FedCm.IdentityProvidersCount", count);
@@ -760,6 +751,10 @@ ukm::SourceId FedCmMetrics::GetOrCreateProviderSourceId(const GURL& provider) {
   return source_id;
 }
 
+int FedCmMetrics::GetSessionID() const {
+  return session_id_;
+}
+
 void RecordPreventSilentAccess(
     const FedCmRequesterFrameType& requester_frame_type,
     int source_id) {
@@ -769,6 +764,16 @@ void RecordPreventSilentAccess(
   ukm::builders::Blink_FedCm ukm_builder(source_id);
   ukm_builder.SetPreventSilentAccessFrameType(
       static_cast<int>(requester_frame_type));
+  ukm_builder.Record(ukm::UkmRecorder::Get());
+}
+
+void RecordAccountSelectionScrollPosition(int source_id,
+                                          int session_id,
+                                          const gfx::Point& scroll_position) {
+  ukm::builders::Blink_FedCm ukm_builder(source_id);
+  ukm_builder.SetAccountSelectionScrollPosition(ukm::GetExponentialBucketMin(
+      scroll_position.y(), /*bucket_spacing=*/1.15));
+  ukm_builder.SetFedCmSessionID(session_id);
   ukm_builder.Record(ukm::UkmRecorder::Get());
 }
 
