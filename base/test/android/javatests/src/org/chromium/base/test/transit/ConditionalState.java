8385c8ba5bfd6efdf825d723cafba8dc778819f3
Henrique Nakashima <hnakashima@chromium.org>
2025-05-15 12:28:07 -0700

    [Android] Remove declareElements() and update docs
    Bug: 416324146,414826060
    Change-Id: I0559af1dc48f92919834b1a22c26da2f100e1fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533535
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460889}

--- base/test/android/javatests/src/org/chromium/base/test/transit/ConditionalState.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/ConditionalState.java
@@ -52,7 +52,7 @@ public abstract class ConditionalState {
     @Phase private int mLifecyclePhase = Phase.NEW;
     private final Elements mConsolidatedElements = new Elements(this);
     protected final Elements.Builder mElements = mConsolidatedElements.newBuilder();
-    private boolean mDeclareElementsCalled;
+    private boolean mAreElementsConsolidated;
 
     /** Lifecycle phases of ConditionalState. */
     @IntDef({
@@ -72,18 +72,6 @@ public abstract class ConditionalState {
     }
 
     /**
-     * Declare the {@link BaseElements} that define this ConditionalState, such as Views.
-     *
-     * <p>Transit-layer {@link Station}s and {@link Facility}s can declare Elements in their
-     * constructor and/or override this method.
-     *
-     * @param elements use the #declare___() methods to describe the Elements that define the state.
-     * @deprecated Declare elements in the constructor or in{@link #declareExtraElements()}.
-     */
-    @Deprecated
-    public void declareElements(Elements.Builder elements) {}
-
-    /**
      * Declare extra {@link Element}s that define this ConditionalState, such as Views.
      *
      * <p>Transit-layer {@link Station}s and {@link Facility}s can declare Elements in their
@@ -93,11 +81,10 @@ public abstract class ConditionalState {
     public void declareExtraElements() {}
 
     Elements getElements() {
-        if (!mDeclareElementsCalled) {
-            declareElements(mElements);
+        if (!mAreElementsConsolidated) {
             declareExtraElements();
             mElements.consolidate();
-            mDeclareElementsCalled = true;
+            mAreElementsConsolidated = true;
         }
         return mConsolidatedElements;
     }
