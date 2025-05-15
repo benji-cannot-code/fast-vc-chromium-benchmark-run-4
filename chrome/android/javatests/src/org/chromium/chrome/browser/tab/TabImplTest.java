9e084a6e4dea97956507c82165a2c2cbcaab49c1
Fabio Muratori <muratori@google.com>
2025-05-15 01:09:55 -0700

    [Clank Nav Cap] Implement Tab getter to distinguish between PWA and non-PWA tab
    ChromeActivity exposes a isCustomTab getter to clarify whether a Tab is being run
    in a "custom tab" (WebAPK, TWA, embedded Chrome Tab, ...) or not.
    
    We want to initially expose new Navigation Capturing scenarios for TWAs and WebAPKs
    and eventually consider all types of chromium-based activities.
    
    This CL implements a getter at Tab level that helps us in deciding whether
    a navigation scenario should be enabled or not.
    
    Test: both manual and automated. Manual tests include checking getter return value
    for WebAPKs, TWAs, browser and android embedded tabs (using reddit android app).
    Automated tests are implemented in TabImplTest. We attempt to open browser and TWA
    in 2 separate tests and check for expected return value.
    
    Bug: 417702844
    Change-Id: I7486c6d35628f1aa0d56584f2d52e027c92625be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6546840
    Reviewed-by: Dominik Bylica <bylica@google.com>
    Commit-Queue: Fabio Muratori <muratori@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460551}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabImplTest.java
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import androidx.test.filters.SmallTest;
 
+import org.hamcrest.Matchers;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,6 +19,8 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.Criteria;
+import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tabmodel.TabClosureParams;
@@ -33,6 +37,7 @@ import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
 @Batch(Batch.PER_CLASS)
 public class TabImplTest {
     private static final String TEST_PATH = "/chrome/test/data/android/about.html";
+    private static final long DEFAULT_MAX_TIME_TO_WAIT_IN_MS = 3000;
 
     @ClassRule
     public static ChromeTabbedActivityTestRule sActivityTestRule =
@@ -75,5 +80,21 @@ public class TabImplTest {
                 () -> tab.loadIfNeeded(TabLoadIfNeededCaller.MEDIA_CAPTURE_PICKER));
 
         ThreadUtils.runOnUiThreadBlocking(() -> assertTrue(tab.hasBacking()));
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"Tab"})
+    public void testTabIsNotInPWA() throws Exception {
+        CriteriaHelper.pollUiThread(
+                () -> {
+                    Criteria.checkThat(
+                            sActivityTestRule.getActivity().getActivityTab(),
+                            Matchers.notNullValue());
+                },
+                DEFAULT_MAX_TIME_TO_WAIT_IN_MS,
+                CriteriaHelper.DEFAULT_POLLING_INTERVAL);
+
+        assertFalse(sActivityTestRule.getActivity().getActivityTab().isTabInPWA());
     }
 }
