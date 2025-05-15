61a019871010229fa7ed483e0e4f5e02d032169e
Lily Chen <chlily@chromium.org>
2025-05-15 09:07:10 -0700

    Check eligibility for CheckFileSystemAccessWriteRequest
    This CL extends the eligibility checks enforced by
    DownloadProtectionDelegate(Android) to
    CheckFileSystemAccessWriteRequest. Previously, they were only checked on
    the CheckClientDownload code path, causing Android download protection
    to incorrectly send pings for non-APK files, non-enabled users, etc.
    
    To achieve that, this CL refactors some of the checks that apply to
    CheckClientDownload to reuse them for CheckFileSystemAccessWriteRequest.
    
    Fixed: 416080485
    Change-Id: I5471bce33e27a03855658406a982ae079d131ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6537688
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460767}

--- chrome/browser/safe_browsing/download_protection/download_protection_delegate_desktop.h
+++ chrome/browser/safe_browsing/download_protection/download_protection_delegate_desktop.h
@@ -15,6 +15,10 @@ namespace base {
 class FilePath;
 }
 
+namespace content {
+struct FileSystemAccessWriteItem;
+}
+
 namespace download {
 class DownloadItem;
 }
@@ -33,6 +37,8 @@ class DownloadProtectionDelegateDesktop : public DownloadProtectionDelegate {
   // DownloadProtectionDelegate:
   bool ShouldCheckDownloadUrl(download::DownloadItem* item) const override;
   bool MayCheckClientDownload(download::DownloadItem* item) const override;
+  bool MayCheckFileSystemAccessWrite(
+      content::FileSystemAccessWriteItem* item) const override;
   MayCheckDownloadResult IsSupportedDownload(
       download::DownloadItem& item,
       const base::FilePath& target_path) const override;
