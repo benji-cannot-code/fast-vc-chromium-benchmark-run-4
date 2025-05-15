7e0996db1d46c43f78513a10c076f5a67727bc2b
Yann Dago <ydago@chromium.org>
2025-05-15 07:31:34 -0700

    Show full name and email on managed accounts on device screen
    Manage accounts on device: http://screen/7d9ZdBqcF2VvdeX
    
    Fixed: 417210006
    Change-Id: Ib9e910c27fbaf4f5f6c604b4e9cd70cc2e6e7d05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6546478
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460704}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_table_view_controller.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_model_identity_data_source.h"
 #import "ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_mutator.h"
 #import "ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_table_view_controller_constants.h"
+#import "ios/chrome/browser/shared/model/profile/features.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ios/chrome/browser/signin/model/constants.h"
@@ -138,7 +139,12 @@ typedef NS_ENUM(NSInteger, EditAccountListItemType) {
 - (void)updateAccountItem:(TableViewAccountItem*)item
      withIdentityViewItem:(IdentityViewItem*)identityViewItem {
   item.image = identityViewItem.avatar;
-  item.text = identityViewItem.userEmail;
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
+    item.detailText = identityViewItem.userEmail;
+    item.text = identityViewItem.userFullName;
+  } else {
+    item.text = identityViewItem.userEmail;
+  }
   item.accessibilityIdentifier = identityViewItem.accessibilityIdentifier;
   item.mode = TableViewAccountModeNonTappable;
   item.accessoryType = UITableViewCellAccessoryNone;
