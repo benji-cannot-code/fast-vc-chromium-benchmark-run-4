5c64e39037845dd76d853c3ecf6896d19a62230a
dpapad <dpapad@chromium.org>
2025-05-15 11:35:10 -0700

    PrintPreview: Fix erroneously disabled pages select dropdown.
    The root cause was an incorrect conversion of a Polymer observer to Lit
    in crrev.com/c/6455369 . Also, added a test that would have caught the
    regression.
    
    Fixed: 417923484
    Change-Id: I4d561a3fe43b3f9be61a1a7e2a4fb51578c316b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553243
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460873}

--- chrome/test/data/webui/print_preview/print_preview_browsertest.cc
+++ chrome/test/data/webui/print_preview/print_preview_browsertest.cc
@@ -138,6 +138,10 @@ IN_PROC_BROWSER_TEST_F(PrintPreviewPagesSettingsTest, PagesDropdown) {
   RunTestCase("PagesDropdown");
 }
 
+IN_PROC_BROWSER_TEST_F(PrintPreviewPagesSettingsTest, PagesDropdownDisabled) {
+  RunTestCase("PagesDropdownDisabled");
+}
+
 IN_PROC_BROWSER_TEST_F(PrintPreviewPagesSettingsTest, NoParityOptions) {
   RunTestCase("NoParityOptions");
 }
