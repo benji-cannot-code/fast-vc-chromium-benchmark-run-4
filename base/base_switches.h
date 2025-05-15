a542b7ca2120960eab7a9b89b77fbd0a435f7cb9
Sirisha Kavuluru <skavuluru@google.com>
2025-05-15 14:58:35 -0700

    [Desktop] Add --force-desktop-android command line arg to simulate desktop
    Change-Id: I5c2f55739908fdc415c082dcfc059e724b9ea7c8
    Bug: 417994927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549115
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461010}

--- base/base_switches.h
+++ base/base_switches.h
@@ -54,6 +54,7 @@ extern const char kEnableCrashReporterForTesting[];
 extern const char kAndroidSkipChildServiceInitForTesting[];
 extern const char kDefaultCountryCodeAtInstall[];
 extern const char kEnableIdleTracing[];
+extern const char kForceDesktopAndroid[];
 extern const char kHostPackageName[];
 extern const char kHostPackageLabel[];
 extern const char kHostVersionCode[];
