d1f63900b60ecb04f8640907f7c44f85b88acb5b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-05-14 19:19:22 -0700

    Import wpt@d99035ccbd9d0f043ea89a496ea58982d45f7c45
    https://github.com/web-platform-tests/wpt/compare/253dee94a...d99035ccb
    
    Using wpt-import in Chromium f445ccd365845d2f88f8a474cc96813164e10492.
    With Chromium commits locally applied on WPT:
      0c755ee272 "WebNN: Eschew assert_true() in WPTs"
      3009dd8e45 "QuotaExceededError: Update to a DOMException derived [...]
      4353e11ae6 "Initialize speech recognition phrases"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    pastithas@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I428e8f6d5e065187d9bb0a40037178a3a62ef954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549295
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1460479}

--- third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/idlharness.https.window.js
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: variant=?exclude=SFrameTransform.*
+// META: variant=?include=SFrameTransform.*
+// META: script=/common/subset-tests-by-key.js
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
 // META: script=./RTCPeerConnection-helper.js
