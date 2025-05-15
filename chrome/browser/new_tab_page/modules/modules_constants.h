30491982e858552dc4c7f9bf43b24961318b17a1
Takuto Ikuta <tikuta@chromium.org>
2025-05-15 02:58:48 -0700

    chrome: Add missing includes
    This is to fix build error with Clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=mfacey@chromium.org
    
    Bug: 40263312
    Change-Id: I1ea995551d0fa18254a428274976afea515051df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549800
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460586}

--- chrome/browser/new_tab_page/modules/modules_constants.h
+++ chrome/browser/new_tab_page/modules/modules_constants.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NEW_TAB_PAGE_MODULES_MODULES_CONSTANTS_H_
 #define CHROME_BROWSER_NEW_TAB_PAGE_MODULES_MODULES_CONSTANTS_H_
 
+#include <array>
+
 namespace ntp_modules {
 
 inline constexpr char kDriveModuleId[] = "drive";
