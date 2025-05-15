56a087ec50161c28a5b1d1ceacc0fb845a44cead
James Cook <jamescook@chromium.org>
2025-05-14 20:31:19 -0700

    extensions: Don't load component extensions on desktop Android
    This is a temporary band-aid for a startup crash (see bug for details).
    We need to wire up resource loading for component extensions, but this
    may take a little while.
    
    Also fix the ifdefs for some extensions JS resources. Otherwise we
    crash on startup trying to load guestViewDeny (guest view is not
    supported on desktop Android).
    
    Bug: 417617863
    Bug: 415701970
    Change-Id: Ib3f53371e83451a9694d99677a4d44ee5114e008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6547856
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Owners-Override: Zoraiz Naeem <zoraiznaeem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460492}

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -4104,6 +4104,9 @@ TEST_F(ExtensionServiceTest, BlockAndUnblockBlocklistedExtension) {
 }
 #endif  // defined(ENABLE_BLOCKLIST_TESTS)
 
+// TODO(crbug.com/417617863): Skip loading component extensions until the
+// startup crash is fixed on desktop Android (see bug).
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Tests blocking then unblocking enabled component extensions after the service
 // has been initialized.
 TEST_F(ExtensionServiceTest, BlockAndUnblockEnabledComponentExtension) {
@@ -4124,6 +4127,7 @@ TEST_F(ExtensionServiceTest, BlockAndUnblockEnabledComponentExtension) {
   // Component extension should never block.
   AssertExtensionBlocksAndUnblocks(false, good0);
 }
+#endif
 
 // Tests blocking then unblocking a theme after the service has been
 // initialized.
@@ -4206,6 +4210,9 @@ TEST_F(ExtensionServiceTest, BlocklistedByPolicyRemovedIfRunning) {
   EXPECT_EQ(0u, registry()->enabled_extensions().size());
 }
 
+// TODO(crbug.com/417617863): Skip loading component extensions until the
+// startup crash is fixed on desktop Android (see bug).
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Tests that component extensions are not blocklisted by policy.
 TEST_F(ExtensionServiceTest, ComponentExtensionAllowlisted) {
   InitializeEmptyExtensionServiceWithTestingPrefs();
@@ -4287,6 +4294,7 @@ TEST_F(ExtensionServiceTest, ComponentExtensionAllowlistedPermission) {
                   ->active_permissions()
                   .HasAPIPermission(APIPermissionID::kTab));
 }
+#endif
 
 // Tests that policy-installed extensions are not blocklisted by policy.
 TEST_F(ExtensionServiceTest, PolicyInstalledExtensionsAllowlisted) {
@@ -6977,6 +6985,9 @@ TEST_F(ExtensionServiceTest, StorageQuota) {
       loaded_extensions()[2]->url()));
 }
 
+// TODO(crbug.com/417617863): Skip loading component extensions until the
+// startup crash is fixed on desktop Android (see bug).
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Tests ComponentLoader::Add().
 TEST_F(ExtensionServiceTest, ComponentExtensions) {
   // Component extensions should work even when extensions are disabled.
@@ -7013,6 +7024,7 @@ TEST_F(ExtensionServiceTest, ComponentExtensions) {
   ASSERT_EQ(1u, registry()->enabled_extensions().size());
   EXPECT_EQ(extension_id, (*registry()->enabled_extensions().begin())->id());
 }
+#endif
 
 TEST_F(ExtensionServiceTest, InstallPriorityExternalUpdateUrl) {
   InitializeEmptyExtensionService();
