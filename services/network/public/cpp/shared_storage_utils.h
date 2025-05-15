fafe150ae14fe788376166467e1d0cb757e9375e
Takuto Ikuta <tikuta@chromium.org>
2025-05-15 02:54:21 -0700

    services: Add missing includes
    This is to fix build error with Clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=bashi@chromium.org
    
    Bug: 40263312
    Change-Id: Ie645f50cc97532e9c136a40f7d36322b2d7497ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550759
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460584}

--- services/network/public/cpp/shared_storage_utils.h
+++ services/network/public/cpp/shared_storage_utils.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdlib>
 #include <string>
+#include <vector>
 
 #include "base/component_export.h"
 #include "base/types/optional_ref.h"
