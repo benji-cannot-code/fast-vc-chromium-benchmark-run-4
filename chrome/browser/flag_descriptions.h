0e32fc2843ed0b404cdbe4c90209589983ff5d8a
Vincent Chiang <vincentchiang@chromium.org>
2025-05-15 09:51:26 -0700

    Remove Hardware Mirroring flag
    Remove the flag to enable hardware mirroring
    
    Bug: b:207072219
    Change-Id: I4a4919ddc2d805f5da3d9462dafb082a7f01e851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550559
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460800}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4242,9 +4242,6 @@ extern const char kLimitShelfItemsToActiveDeskDescription[];
 extern const char kListAllDisplayModesName[];
 extern const char kListAllDisplayModesDescription[];
 
-extern const char kEnableHardwareMirrorModeName[];
-extern const char kEnableHardwareMirrorModeDescription[];
-
 extern const char kLockScreenNotificationName[];
 extern const char kLockScreenNotificationDescription[];
 
