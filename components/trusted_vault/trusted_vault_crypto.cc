a5d6721d447ea28f92b0eecbd927df5bd39e2e0d
Takuto Ikuta <tikuta@chromium.org>
2025-05-15 03:04:16 -0700

    components: Add missing includes
    This is to fix build error with Clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=treib@chromium.org
    
    Bug: 40263312
    Change-Id: I9b447097d36bddcce315f5631b9929d8a47216c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549801
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460588}

--- components/trusted_vault/trusted_vault_crypto.cc
+++ components/trusted_vault/trusted_vault_crypto.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/trusted_vault/trusted_vault_crypto.h"
 
+#include <array>
+
 #include "base/check_op.h"
 #include "components/trusted_vault/securebox.h"
 #include "crypto/hash.h"
