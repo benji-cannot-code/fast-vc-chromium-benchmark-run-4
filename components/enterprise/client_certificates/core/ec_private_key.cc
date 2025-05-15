02c6855dc23de99eb07db2dacc53ce10b6d8395d
Elly <ellyjones@chromium.org>
2025-05-15 09:30:04 -0700

    clientcerts: simplify SSLKeyConverter
    Previously SSLKeyConverter was a class with two implementations:
    SSLKeyConverterImpl and StubSSLKeyConverter. It was used as a test shim
    so that unit tests could avoid dealing with actual hardware-backed keys
    (which would not work or be very slow). It had two methods:
    
      ConvertUnexportableKeySlowly(UnexportableSigningKey&)
      ConvertECKey(ECPrivateKey&)
    
    The second method doesn't make sense though - ECPrivateKey is never
    hardware-backed, and converting it doesn't need to be mocked for tests
    because the "conversion" process is a no-op that just wraps the key into
    another object. In fact, both implementations of ConvertECKey() had
    identical code.
    
    This change therefore removes SSLKeyConverter, replacing it with a
    single free function:
    
      SSLPrivateKeyFromUnexportableSigningKeySlowly(UnexportableSigningKey&)
    
    and the test faking behavior is replaced with a single callback which
    tests can set to a conversion function to replace the default behavior.
    This simplifies the code a bit and avoids the need for allocating a
    fresh SSLKeyConverter object every time a key was to be converted.
    
    This refactor is in preparation for migrating the client cert code from
    crypto/ec_private_key (which is deprecated) to crypto/keypair (which is
    not).
    
    Bug: 372283556
    Change-Id: I16a62c214c53fc0a301f4d40b14520bcd05497f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542255
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460783}

--- components/enterprise/client_certificates/core/ec_private_key.cc
+++ components/enterprise/client_certificates/core/ec_private_key.cc
@@ -14,13 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/client_certificates/core/ssl_key_converter.h"
 #include "crypto/ec_private_key.h"
 #include "crypto/ec_signature_creator.h"
+#include "net/ssl/openssl_private_key.h"
 #include "net/ssl/ssl_private_key.h"
+#include "third_party/boringssl/src/include/openssl/evp.h"
 
 namespace client_certificates {
 
 ECPrivateKey::ECPrivateKey(std::unique_ptr<crypto::ECPrivateKey> key)
     : PrivateKey(PrivateKeySource::kSoftwareKey,
-                 SSLKeyConverter::Get()->ConvertECKey(*key)),
+                 net::WrapOpenSSLPrivateKey(bssl::UpRef(key->key()))),
       key_(std::move(key)) {
   CHECK(key_);
 }
--- components/enterprise/client_certificates/core/ssl_key_converter.cc
+++ components/enterprise/client_certificates/core/ssl_key_converter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/client_certificates/core/ssl_key_converter.h"
 
 #include "base/check.h"
+#include "base/no_destructor.h"
 #include "build/build_config.h"
 #include "net/ssl/openssl_private_key.h"
 #include "net/ssl/ssl_private_key.h"
@@ -20,36 +21,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace client_certificates {
 
 namespace {
-std::unique_ptr<SSLKeyConverter> (*g_mock_converter)() = nullptr;
-}  // namespace
 
-class SSLKeyConverterImpl : public SSLKeyConverter {
- public:
-  SSLKeyConverterImpl();
-  ~SSLKeyConverterImpl() override;
-
-  // SSLKeyConverter:
-  scoped_refptr<net::SSLPrivateKey> ConvertUnexportableKeySlowly(
-      const crypto::UnexportableSigningKey& key) override;
-  scoped_refptr<net::SSLPrivateKey> ConvertECKey(
-      const crypto::ECPrivateKey& key) override;
-};
-
-SSLKeyConverter::~SSLKeyConverter() = default;
-
-std::unique_ptr<SSLKeyConverter> SSLKeyConverter::Get() {
-  if (g_mock_converter) {
-    return g_mock_converter();
-  }
-  return std::make_unique<SSLKeyConverterImpl>();
+// base::Callback requires a static initializer, so wrap it in this function and
+// use a static base::NoDestructor to avoid creating a global static
+// initializer.
+internal::ConvertKeyCallback* GetConvertKeyCallback() {
+  static base::NoDestructor<internal::ConvertKeyCallback> s_convert_key;
+  return s_convert_key.get();
 }
 
-SSLKeyConverterImpl::SSLKeyConverterImpl() = default;
-SSLKeyConverterImpl::~SSLKeyConverterImpl() = default;
+}  // namespace
 
-scoped_refptr<net::SSLPrivateKey>
-SSLKeyConverterImpl::ConvertUnexportableKeySlowly(
+scoped_refptr<net::SSLPrivateKey> SSLPrivateKeyFromUnexportableSigningKeySlowly(
     const crypto::UnexportableSigningKey& key) {
+  if (!GetConvertKeyCallback()->is_null()) {
+    return GetConvertKeyCallback()->Run(key);
+  }
 #if BUILDFLAG(IS_WIN)
   return net::WrapUnexportableKeySlowly(key);
 #elif BUILDFLAG(IS_MAC)
@@ -59,18 +46,13 @@ SSLKeyConverterImpl::ConvertUnexportableKeySlowly(
 #endif  // BUILDFLAG(IS_WIN)
 }
 
-scoped_refptr<net::SSLPrivateKey> SSLKeyConverterImpl::ConvertECKey(
-    const crypto::ECPrivateKey& key) {
-  return net::WrapOpenSSLPrivateKey(bssl::UpRef(key.key()));
-}
-
 namespace internal {
 
-void SetConverterForTesting(std::unique_ptr<SSLKeyConverter> (*func)()) {
+void SetConverterForTesting(internal::ConvertKeyCallback callback) {
   // At least one of the two needs to be null, as nesting of scoped converters
   // is not supported.
-  CHECK(!g_mock_converter || !func);
-  g_mock_converter = func;
+  CHECK(GetConvertKeyCallback()->is_null() || !callback);
+  *GetConvertKeyCallback() = callback;
 }
 
 }  // namespace internal
