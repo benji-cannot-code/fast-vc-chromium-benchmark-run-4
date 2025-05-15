20dc564bdf92d5326e36b8b4306456c50d7eb305
Nathan Memmott <memmott@chromium.org>
2025-05-15 11:05:00 -0700

    Don't proceed after a progress event triggers an abort
    For Translator and the Writing Assistance APIs, if an ProgressEvent
    triggers an abort, then we could erroneously send another event or try
    to resolve the promise with a new instance of the API. This checks
    after each ProgressEvent if we've aborted and stops if we did.
    
    Fixed: 412444078, 415111444
    Change-Id: Ib235d061639d7d69f549c74a371031d10a082c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6547878
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460854}

--- third_party/blink/web_tests/external/wpt/ai/rewriter/rewriter.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/ai/rewriter/rewriter.tentative.https.window.js
@@ -12,7 +12,8 @@ promise_test(async () => {
 promise_test(async () => {
   // TODO(crbug.com/382615217): Test availability with various options.
   assert_equals(await Rewriter.availability(), 'available');
-  assert_equals(await Rewriter.availability({ outputLanguage: 'en' }), 'available');
+  assert_equals(
+      await Rewriter.availability({outputLanguage: 'en'}), 'available');
 }, 'Rewriter.availability');
 
 promise_test(async () => {
@@ -23,6 +24,10 @@ promise_test(async () => {
 promise_test(async () => {
   await testMonitor(Rewriter.create);
 }, 'Rewriter.create() notifies its monitor on downloadprogress');
+
+promise_test(async t => {
+  await testCreateMonitorWithAbort(t, Rewriter.create);
+}, 'Progress events are not emitted after aborted.');
 
 promise_test(async () => {
   const rewriter = await Rewriter.create();
@@ -69,23 +74,17 @@ promise_test(async () => {
 }, 'Creating a Rewriter with "longer" length');
 
 promise_test(async () => {
-  const rewriter = await Rewriter.create({
-    expectedInputLanguages: ['en']
-  });
+  const rewriter = await Rewriter.create({expectedInputLanguages: ['en']});
   assert_array_equals(rewriter.expectedInputLanguages, ['en']);
 }, 'Creating a Rewriter with expectedInputLanguages');
 
 promise_test(async () => {
-  const rewriter = await Rewriter.create({
-    expectedContextLanguages: ['en']
-  });
+  const rewriter = await Rewriter.create({expectedContextLanguages: ['en']});
   assert_array_equals(rewriter.expectedContextLanguages, ['en']);
 }, 'Creating a Rewriter with expectedContextLanguages');
 
 promise_test(async () => {
-  const rewriter = await Rewriter.create({
-    outputLanguage: 'en'
-  });
+  const rewriter = await Rewriter.create({outputLanguage: 'en'});
   assert_equals(rewriter.outputLanguage, 'en');
 }, 'Creating a Rewriter with outputLanguage');
 
@@ -96,14 +95,16 @@ promise_test(async () => {
   assert_equals(rewriter.outputLanguage, null);
 }, 'Creating a Rewriter without optional attributes');
 
-promise_test(async (t) => {
-  const rewriter = await Rewriter.create();
-  let result = await rewriter.rewrite('');
-  assert_equals(result, '');
-  result = await rewriter.rewrite(' ');
-  assert_equals(result, ' ');
-}, 'Rewriter.rewrite() with an empty input or whitespace returns the ' +
-    'original input');
+promise_test(
+    async (t) => {
+      const rewriter = await Rewriter.create();
+      let result = await rewriter.rewrite('');
+      assert_equals(result, '');
+      result = await rewriter.rewrite(' ');
+      assert_equals(result, ' ');
+    },
+    'Rewriter.rewrite() with an empty input or whitespace returns the ' +
+        'original input');
 
 promise_test(async (t) => {
   const rewriter = await Rewriter.create();
@@ -120,7 +121,8 @@ promise_test(async (t) => {
 promise_test(async (t) => {
   const rewriter = await Rewriter.create();
   rewriter.destroy();
-  assert_throws_dom('InvalidStateError', () => rewriter.rewriteStreaming('hello'));
+  assert_throws_dom(
+      'InvalidStateError', () => rewriter.rewriteStreaming('hello'));
 }, 'Rewriter.rewriteStreaming() fails after destroyed');
 
 promise_test(async () => {
@@ -131,15 +133,14 @@ promise_test(async () => {
 
 promise_test(async () => {
   const rewriter = await Rewriter.create();
-  const result =
-      await rewriter.rewrite(kTestPrompt, {context: kTestContext});
+  const result = await rewriter.rewrite(kTestPrompt, {context: kTestContext});
   assert_equals(typeof result, 'string');
 }, 'Simple Rewriter.rewrite() call');
 
 promise_test(async () => {
   const rewriter = await Rewriter.create();
-  const streamingResponse = rewriter.rewriteStreaming(
-      kTestPrompt, {context: kTestContext});
+  const streamingResponse =
+      rewriter.rewriteStreaming(kTestPrompt, {context: kTestContext});
   assert_equals(
       Object.prototype.toString.call(streamingResponse),
       '[object ReadableStream]');
@@ -152,10 +153,8 @@ promise_test(async () => {
 
 promise_test(async () => {
   const rewriter = await Rewriter.create();
-  await Promise.all([
-    rewriter.rewrite(kTestPrompt),
-    rewriter.rewrite(kTestPrompt)
-  ]);
+  await Promise.all(
+      [rewriter.rewrite(kTestPrompt), rewriter.rewrite(kTestPrompt)]);
 }, 'Multiple Rewriter.rewrite() calls are resolved successfully.');
 
 promise_test(async () => {
--- third_party/blink/web_tests/external/wpt/ai/writer/writer.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/ai/writer/writer.tentative.https.window.js
@@ -12,7 +12,7 @@ promise_test(async () => {
 promise_test(async () => {
   // TODO(crbug.com/382596381): Test availability with various options.
   assert_equals(await Writer.availability(), 'available');
-  assert_equals(await Writer.availability({ outputLanguage: 'en' }), 'available');
+  assert_equals(await Writer.availability({outputLanguage: 'en'}), 'available');
 }, 'Writer.availability');
 
 promise_test(async () => {
@@ -23,6 +23,10 @@ promise_test(async () => {
 promise_test(async () => {
   await testMonitor(Writer.create);
 }, 'Writer.create() notifies its monitor on downloadprogress');
+
+promise_test(async t => {
+  await testCreateMonitorWithAbort(t, Writer.create);
+}, 'Progress events are not emitted after aborted.');
 
 promise_test(async () => {
   const writer = await Writer.create();
@@ -71,9 +75,7 @@ promise_test(async () => {
 }, 'Creating a Writer with "long" length');
 
 promise_test(async () => {
-  const writer = await Writer.create({
-    expectedInputLanguages: ['en']
-  });
+  const writer = await Writer.create({expectedInputLanguages: ['en']});
   assert_array_equals(writer.expectedInputLanguages, ['en']);
 }, 'Creating a Writer with expectedInputLanguages');
 
@@ -86,16 +88,12 @@ promise_test(async (t) => {
 
 
 promise_test(async () => {
-  const writer = await Writer.create({
-    expectedContextLanguages: ['en']
-  });
+  const writer = await Writer.create({expectedContextLanguages: ['en']});
   assert_array_equals(writer.expectedContextLanguages, ['en']);
 }, 'Creating a Writer with expectedContextLanguages');
 
 promise_test(async () => {
-  const writer = await Writer.create({
-    outputLanguage: 'en'
-  });
+  const writer = await Writer.create({outputLanguage: 'en'});
   assert_equals(writer.outputLanguage, 'en');
 }, 'Creating a Writer with outputLanguage');
 
@@ -140,8 +138,7 @@ promise_test(async () => {
 
 promise_test(async () => {
   const writer = await Writer.create();
-  const result =
-      await writer.write(kTestPrompt, {context: kTestContext});
+  const result = await writer.write(kTestPrompt, {context: kTestContext});
   assert_equals(typeof result, 'string');
 }, 'Simple Writer.write() call');
 
@@ -161,16 +158,11 @@ promise_test(async () => {
 
 promise_test(async () => {
   const writer = await Writer.create();
-  await Promise.all([
-    writer.write(kTestPrompt),
-    writer.write(kTestPrompt)
-  ]);
+  await Promise.all([writer.write(kTestPrompt), writer.write(kTestPrompt)]);
 }, 'Multiple Writer.write() calls are resolved successfully.');
 
 promise_test(async () => {
   const writer = await Writer.create();
-  await Promise.all([
-    writer.writeStreaming(kTestPrompt),
-    writer.writeStreaming(kTestPrompt)
-  ]);
+  await Promise.all(
+      [writer.writeStreaming(kTestPrompt), writer.writeStreaming(kTestPrompt)]);
 }, 'Multiple Writer.writeStreaming() calls are resolved successfully.');
