da23cbfff0017fed6eab5d6521332cd17453c78b
Saifuddin Hitawala <hitawala@chromium.org>
2025-05-15 08:08:19 -0700

    [gpu] Remove VideoImageReaderBacking BeginReadAccess DumpWithoutCrashing
    The crashes have gone down substantially over time after adding
    dawn toggle for Android devices that avoids validation for VkImage size.
    
    This change removes DumpWithoutCrashing from
    VideoImageReaderImageBacking added for this purpose.
    
    Bug: 411835797
    Change-Id: I43b90cf84233d41a4a3e97634103af29e291fdb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551881
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460731}

--- gpu/command_buffer/service/shared_image/video_image_reader_image_backing.cc
+++ gpu/command_buffer/service/shared_image/video_image_reader_image_backing.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/android_hardware_buffer_compat.h"
 #include "base/android/scoped_hardware_buffer_fence_sync.h"
 #include "base/android/scoped_hardware_buffer_handle.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/task/bind_post_task.h"
 #include "base/task/single_thread_task_runner.h"
 #include "components/viz/common/gpu/vulkan_context_provider.h"
@@ -438,10 +437,6 @@ class VideoImageReaderImageBacking::SkiaGraphiteDawnImageRepresentation
     if (shared_texture_memory_.BeginAccess(texture_, &begin_access_desc) !=
         wgpu::Status::Success) {
       LOG(ERROR) << "Failed to begin access for texture";
-      // TODO(crbug.com/377489264): Remove after ensuring that all samsung
-      // devices which are failing AHB size vs VkImage size checks have the
-      // check disabled.
-      base::debug::DumpWithoutCrashing();
       ResetStorage();
       return {};
     }
