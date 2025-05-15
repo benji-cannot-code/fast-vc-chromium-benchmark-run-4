8523ebc4daf84acaae0956a9bbc4230bec51d7af
Joshua Pawlicki <waffles@chromium.org>
2025-05-15 07:08:14 -0700

    Updater: Fix macOS ZIP/bundle permissions modifications.
    The old code had the following undesirable characteristics:
      1. Everything in the app bundle was set to be executable.
      2. Permissions for the root_path itself were never modified.
      3. The function name was "Confirm*" but in fact the code modified
         permissions and did not test them.
      4. stat and chmod were used, not lstat and lchmod.
      5. The test never actually verified that the function did anything.
    
    Fixed: 416667122, 414383196
    Change-Id: Ie34befe32211bcff9105e47ecac24e405fa3f128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543226
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460690}

--- chrome/updater/mac/install_from_archive.mm
+++ chrome/updater/mac/install_from_archive.mm
@@ -44,11 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace updater {
 namespace {
 
-constexpr int kPermissionsMask = base::FILE_PERMISSION_USER_MASK |
-                                 base::FILE_PERMISSION_GROUP_MASK |
-                                 base::FILE_PERMISSION_READ_BY_OTHERS |
-                                 base::FILE_PERMISSION_EXECUTE_BY_OTHERS;
-
 bool RunHDIUtil(const std::vector<std::string>& args,
                 std::string* command_output) {
   base::FilePath hdiutil_path("/usr/bin/hdiutil");
@@ -364,7 +359,7 @@ int InstallFromZip(const base::FilePath& zip_file_path,
     return static_cast<int>(InstallErrors::kFailedToExpandZip);
   }
 
-  if (!ConfirmFilePermissions(dest_path, kPermissionsMask)) {
+  if (!SetFilePermissionsRecursive(dest_path)) {
     return static_cast<int>(InstallErrors::kCouldNotConfirmAppPermissions);
   }
 
@@ -390,7 +385,7 @@ int InstallFromApp(const base::FilePath& app_file_path,
 
   // Need to make sure that the app at the path being installed has the correect
   // permissions.
-  if (!ConfirmFilePermissions(app_file_path, kPermissionsMask)) {
+  if (!SetFilePermissionsRecursive(app_file_path)) {
     return static_cast<int>(InstallErrors::kCouldNotConfirmAppPermissions);
   }
 
