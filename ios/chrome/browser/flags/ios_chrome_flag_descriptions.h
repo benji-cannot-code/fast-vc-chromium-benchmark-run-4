0aea9c47a56528de6a80897b2f1ac9a732ebd7ea
“Prasanaa” <prasanaa@google.com>
2025-05-15 08:37:06 -0700

    [iOS] Change flag PageActionMenu to include GLIC Promo ARM
    This CL adds variation to the existing PageActionMenu flag for having
    a single or a double screen for the GLIC Promo Consent flow.
    
    Bug: 414791668
    Change-Id: I66ee7227f63f2d96f9947dd4d8fec3440a682bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542491
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1460751}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -647,6 +647,9 @@ extern const char kOneTapForMapsDescription[];
 extern const char kPageActionMenuName[];
 extern const char kPageActionMenuDescription[];
 
+extern const char kGLICPromoConsentName[];
+extern const char kGLICPromoConsentDescription[];
+
 extern const char kPageContentAnnotationsName[];
 extern const char kPageContentAnnotationsDescription[];
 
