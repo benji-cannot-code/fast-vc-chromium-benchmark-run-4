b58b96bcb0afc882c34789a35e1d01d9b561e2e7
Christopher Grant <cjgrant@chromium.org>
2025-05-15 10:52:53 -0700

    PageActions: Remove now needless MemorySaver controller null check
    This isn't needed after crrev.com/c/6540666. Pull out the check for
    simplicity.
    
    Tested in an earlier patch set with the corresponding field trial.
    
    Bug: 417177719
    Change-Id: I6dc0204de8642a3d345b95cb6bab3acbb09943ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6547260
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460842}

--- chrome/browser/ui/performance_controls/memory_saver_chip_tab_helper.cc
+++ chrome/browser/ui/performance_controls/memory_saver_chip_tab_helper.cc
@@ -174,12 +174,6 @@ void MemorySaverChipTabHelper::UpdatePageActionState() {
   }
   memory_saver::MemorySaverChipController* controller =
       tab_features->memory_saver_chip_controller();
-  if (!controller) {
-    // Tab features can be faked in tests.
-    CHECK_IS_TEST();
-    return;
-  }
-
   switch (chip_state_) {
     case memory_saver::ChipState::HIDDEN:
       controller->Hide();
