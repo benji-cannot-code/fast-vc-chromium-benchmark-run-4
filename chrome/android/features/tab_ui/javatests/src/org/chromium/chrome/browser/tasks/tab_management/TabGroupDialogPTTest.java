68ae09c4fabcee57b5b3e24daa6d4a13e311afab
Henrique Nakashima <hnakashima@chromium.org>
2025-05-15 11:49:28 -0700

    [Android] Re-enable TabGroupDialogPTTest#testTabGroupNameChange
    Bug: 413218346
    Change-Id: I886ed53f8bf99c52a6735325e941791941f8485a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6545872
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460884}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupDialogPTTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupDialogPTTest.java
@@ -21,7 +21,6 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -131,7 +130,6 @@ public class TabGroupDialogPTTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/415799207")
     public void testTabGroupNameChange() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         WebPageStation pageStation =
