00c91c4572f6c1100bd18ccfa35698491de228c8
Takuto Ikuta <tikuta@chromium.org>
2025-05-15 03:23:25 -0700

    components: Add missing includes
    This is to fix build error with Clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=sorin@chromium.org
    
    Bug: 40263312
    Change-Id: Ia328953a20a89cd8240d7da83ee27f1de48af806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550056
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460596}

--- components/client_update_protocol/ecdsa.h
+++ components/client_update_protocol/ecdsa.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_CLIENT_UPDATE_PROTOCOL_ECDSA_H_
 #define COMPONENTS_CLIENT_UPDATE_PROTOCOL_ECDSA_H_
 
+#include <array>
 #include <cstdint>
 #include <memory>
 #include <string>
