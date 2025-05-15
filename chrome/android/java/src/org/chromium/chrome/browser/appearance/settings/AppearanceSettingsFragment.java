6734ffbf10d72c12798e1ba70e33be78dd97acfa
Sky Malice <skym@chromium.org>
2025-05-15 10:14:27 -0700

    Add @NullMarked to AppearanceSettingsFragment
    Bug: 389129271
    Change-Id: I483f67c72ceb42eb16994763b2a684f946c644a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553242
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460817}

--- chrome/android/java/src/org/chromium/chrome/browser/appearance/settings/AppearanceSettingsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/appearance/settings/AppearanceSettingsFragment.java
@@ -8,11 +8,10 @@ package org.chromium.chrome.browser.appearance.settings;
 import android.content.Context;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.bookmarks.bar.BookmarkBarSettingProvider;
 import org.chromium.chrome.browser.bookmarks.bar.BookmarkBarUtils;
@@ -26,6 +25,7 @@ import org.chromium.components.browser_ui.settings.CustomDividerFragment;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 
 /** Fragment to manage appearance settings. */
+@NullMarked
 public class AppearanceSettingsFragment extends ChromeBaseSettingsFragment
         implements CustomDividerFragment {
 
@@ -68,7 +68,7 @@ public class AppearanceSettingsFragment extends ChromeBaseSettingsFragment
         return mPageTitle;
     }
 
-    public static @NonNull String getTitle(@NonNull Context context) {
+    public static String getTitle(Context context) {
         return context.getString(R.string.appearance_settings);
     }
 
@@ -124,7 +124,7 @@ public class AppearanceSettingsFragment extends ChromeBaseSettingsFragment
         // LINT.ThenChange(//chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java:InitPrefUiTheme)
     }
 
-    private void removePreference(@NonNull String prefKey) {
+    private void removePreference(String prefKey) {
         getPreferenceScreen().removePreference(findPreference(prefKey));
     }
 
