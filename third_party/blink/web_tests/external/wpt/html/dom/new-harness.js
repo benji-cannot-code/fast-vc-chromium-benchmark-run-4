105baf873e67ae76e0fdd7af1ab854c0015c463c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-05-15 16:57:36 -0700

    Import wpt@649319c229a6037ad25250a4f006bf605e986e81
    https://github.com/web-platform-tests/wpt/compare/13c6c3671...649319c22
    
    Using wpt-import in Chromium 3c5cae04382a45348c970244757a64c8ec3fa5b8.
    With Chromium commits locally applied on WPT:
      bf0a6e732e "Update cookieStore WPTs to not append "/" to the [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ie38ca1ddc167b43753d61f249cc06790804139b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6552339
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1461081}

--- third_party/blink/web_tests/external/wpt/html/dom/new-harness.js
+++ third_party/blink/web_tests/external/wpt/html/dom/new-harness.js
@@ -9,4 +9,6 @@ ReflectionHarness.test = function(fun, description) {
 
 ReflectionHarness.assertEquals = assert_equals;
 
+ReflectionHarness.assertInArray = assert_in_array;
+
 ReflectionHarness.assertThrows = assert_throws_dom;
