668202fe56a0cea99f163ef357d3ed3b9669a5ec
Mason Freed <masonf@chromium.org>
2025-05-16 13:07:25 -0700

    Closing interest invoker target popover loses interest
    Since it's possible that the target popover for an interest
    invoker (via `interesttarget`) can be closed through other means
    such as light dismiss or programmatic `.hidePopover()`, that
    should cause a loss of interest. Without this, the UI can get
    into an odd state where the target popover is already closed,
    yet the invoker still "has interest". To get the popover to
    show up again, the invoker needs to be de-hovered *and left
    de-hovered for the "hide" delay* so that interest can be lost.
    Since that's completely invisible, the user has no way to know
    whether it's "done" and the invoker can be re-hovered. With
    this change, the UX will be intuitive.
    
    Bug: 364669918
    Change-Id: Ifab53af643ffdccfda47524b187255c756b3a106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6536349
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461575}

--- third_party/blink/renderer/core/dom/popover_data.h
+++ third_party/blink/renderer/core/dom/popover_data.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/element_rare_data_field.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/html/closewatcher/close_watcher.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
 #include "third_party/blink/renderer/core/html_element_type_helpers.h"
@@ -82,12 +83,24 @@ class PopoverData final : public GarbageCollected<PopoverData>,
         : popover_(popover),
           was_set_(popover.GetPopoverData()->hiding_or_showing_this_popover_) {
       if (was_set_ && show_warning) {
-        popover_.GetDocument().AddConsoleMessage(MakeGarbageCollected<
-                                                 ConsoleMessage>(
-            mojom::blink::ConsoleMessageSource::kOther,
-            mojom::blink::ConsoleMessageLevel::kWarning,
-            "The `beforetoggle` event handler for a popover triggered another "
-            "popover to be shown or hidden. This is not recommended."));
+        if (RuntimeEnabledFeatures::HTMLInterestTargetAttributeEnabled(
+                popover.GetDocument().GetExecutionContext())) {
+          popover_.GetDocument().AddConsoleMessage(
+              MakeGarbageCollected<ConsoleMessage>(
+                  mojom::blink::ConsoleMessageSource::kOther,
+                  mojom::blink::ConsoleMessageLevel::kWarning,
+                  "The `beforetoggle` event handler for a popover triggered "
+                  "another popover to be shown or hidden. Or a `loseinterest` "
+                  "event handler was cancelled. This is not recommended."));
+        } else {
+          popover_.GetDocument().AddConsoleMessage(
+              MakeGarbageCollected<ConsoleMessage>(
+                  mojom::blink::ConsoleMessageSource::kOther,
+                  mojom::blink::ConsoleMessageLevel::kWarning,
+                  "The `beforetoggle` event handler for a popover triggered "
+                  "another popover to be shown or hidden. This is not "
+                  "recommended."));
+        }
       } else {
         popover_.GetPopoverData()->hiding_or_showing_this_popover_ = true;
       }
