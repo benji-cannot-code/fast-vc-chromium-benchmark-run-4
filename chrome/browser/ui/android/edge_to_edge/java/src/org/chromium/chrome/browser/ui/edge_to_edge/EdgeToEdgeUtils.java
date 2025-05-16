8739799bff9b91f202aff83ad6916b17e5791833
Sirisha Kavuluru <skavuluru@google.com>
2025-05-15 19:45:13 -0700

    [Desktop] Migrate to use DeviceInfo.isDesktop instead of BuildInfo
    Change-Id: I5622f78419e75a78ae460b75385fb57a9177b3f6
    Bug: 417994927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6552920
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461133}

--- chrome/browser/ui/android/edge_to_edge/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeUtils.java
+++ chrome/browser/ui/android/edge_to_edge/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeUtils.java
@@ -17,6 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.chromium.base.ApkInfo;
 import org.chromium.base.BuildInfo;
+import org.chromium.base.DeviceInfo;
 import org.chromium.base.Log;
 import org.chromium.base.ResettersForTesting;
 import org.chromium.base.metrics.RecordHistogram;
@@ -101,7 +102,7 @@ public class EdgeToEdgeUtils {
             return false;
         }
 
-        if (BuildInfo.getInstance().isAutomotive || BuildInfo.getInstance().isDesktop) {
+        if (DeviceInfo.isAutomotive() || DeviceInfo.isDesktop()) {
             return false;
         }
 
