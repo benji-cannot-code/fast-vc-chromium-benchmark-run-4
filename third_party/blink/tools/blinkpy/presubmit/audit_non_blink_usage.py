4841399dd2ea13146e247cf778391ea4fbfe29ff
Colin Blundell <blundell@chromium.org>
2025-05-15 23:49:49 -0700

    [Blink] Move PrepareTransferableResource() into HTMLCanvasElement
    No functional change.
    
    Bug: 399587138
    Change-Id: I4a65d2cd76fbc8857e538843b00c04ab95e22a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551777
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461190}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -2250,6 +2250,7 @@ _CONFIG = [
             'third_party/blink/renderer/modules/webgpu',
         ],
         'allowed': [
+            'base::BindOnce',
             'cc::TextureLayerClient',
             'viz::ReleaseCallback',
             'viz::SharedImageFormat',
