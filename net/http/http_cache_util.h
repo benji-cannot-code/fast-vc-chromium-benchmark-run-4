862fe0a6f683296ea7cb25f8c524a7e2d23a6d63
Takuto Ikuta <tikuta@chromium.org>
2025-05-15 22:31:34 -0700

    net: Add missing includes
    This is to fix build error with Clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=shivanisha@chromium.org
    
    Bug: 40263312
    Change-Id: I22b51e3df1b2930b6fe71083d754f12e5f2b5509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550057
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461171}

--- net/http/http_cache_util.h
+++ net/http/http_cache_util.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_HTTP_HTTP_CACHE_UTIL_H_
 #define NET_HTTP_HTTP_CACHE_UTIL_H_
 
+#include <array>
 #include <optional>
 #include <string_view>
 
