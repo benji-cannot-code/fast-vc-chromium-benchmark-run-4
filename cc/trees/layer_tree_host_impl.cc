1ec56d1878065792f3089864f57843ad66d10fab
Victor Miura <vmiura@chromium.org>
2025-05-16 06:23:22 -0700

    [TreesInViz] Set metadata::is_software correctly
    Use LayerTreeHostImpl::GetDrawMode() to determine hardware/software
    rendering mode in a TreesInViz compatible way.
    
    Bug: 415068408
    Change-Id: I23e363123204bb0ff78e657eac79660f3d077b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553985
    Auto-Submit: Victor Miura <vmiura@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461304}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -2652,7 +2652,7 @@ viz::CompositorFrameMetadata LayerTreeHostImpl::MakeCompositorFrameMetadata() {
     screenshot_destination_ = base::UnguessableToken();
   }
 
-  metadata.is_software = !layer_tree_frame_sink_->context_provider();
+  metadata.is_software = GetDrawMode() != DrawMode::DRAW_MODE_HARDWARE;
 
   return metadata;
 }
