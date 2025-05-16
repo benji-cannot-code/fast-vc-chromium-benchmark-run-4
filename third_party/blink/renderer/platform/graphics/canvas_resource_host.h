f95a4a37be44695c2bb0d605dcc868a1668fc9c6
Colin Blundell <blundell@chromium.org>
2025-05-15 23:49:38 -0700

    [Blink] Make HTMLCanvasElement TextureLayerClient
    This CL moves TextureLayerClient inheritance from CanvasResourceHost
    down to HTMLCanvasElement. A followup will move the
    PrepareTransferableResource() implementation into HTMLCanvasElement.
    
    Bug: 399587138
    Change-Id: I7a857a5e0e28bc0c13fbd813bb8e0d8c139ae3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551715
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461189}

--- third_party/blink/renderer/platform/graphics/canvas_resource_host.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_host.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "cc/layers/texture_layer.h"
-#include "cc/layers/texture_layer_client.h"
 #include "third_party/blink/renderer/platform/graphics/flush_reason.h"
 #include "third_party/blink/renderer/platform/graphics/opacity_mode.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
@@ -37,15 +36,10 @@ enum class RasterModeHint {
   kPreferCPU,
 };
 
-class PLATFORM_EXPORT CanvasResourceHost : public cc::TextureLayerClient {
+class PLATFORM_EXPORT CanvasResourceHost {
  public:
   explicit CanvasResourceHost(gfx::Size size);
-  ~CanvasResourceHost() override;
-
-  // cc::TextureLayerClient implementation.
-  bool PrepareTransferableResource(
-      viz::TransferableResource* out_resource,
-      viz::ReleaseCallback* out_release_callback) override;
+  virtual ~CanvasResourceHost();
 
   virtual void NotifyGpuContextLost() = 0;
   // TODO(crbug.com/399587138): Delete once `cc::Layer` related code is moved to
@@ -120,6 +114,10 @@ class PLATFORM_EXPORT CanvasResourceHost : public cc::TextureLayerClient {
   virtual bool TransferToGPUTextureWasInvoked() { return false; }
 
  protected:
+  bool PrepareTransferableResourceInternal(
+      viz::TransferableResource* out_resource,
+      viz::ReleaseCallback* out_release_callback);
+
   virtual CanvasResourceProvider* GetOrCreateCanvasResourceProviderImpl() = 0;
 
   bool is_opaque() { return is_opaque_; }
