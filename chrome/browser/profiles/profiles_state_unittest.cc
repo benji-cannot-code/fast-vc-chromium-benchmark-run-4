18306d9507fec6689282d85faab29afebc2d7cfb
Antonio Gomes <tonikitoo@igalia.com>
2025-05-16 06:20:19 -0700

    Build profiles_state.h as part of //c/b/profiles:profiles GN target
    This is a step forward to the componentization of c/b/profiles/.
    As a follow up, it allows a couple of TODOs to be removed - referencing
    circular dependencies against //c/b:b and //c/b/ui/search_engine_choice.
    
    R=dljames@chromium.org
    
    Bug: 417774639
    Change-Id: I1ba16ad256deb5e8195c20e970e0ba511b1ba023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553108
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461302}

--- chrome/browser/profiles/profiles_state_unittest.cc
+++ chrome/browser/profiles/profiles_state_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/sync_preferences/pref_service_syncable.h"
 #include "content/public/test/browser_task_environment.h"
-#include "profiles_state.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
