69b8fc0dd838bf86b537e2a2011f3d280b3ae6aa
Greg Thompson <grt@chromium.org>
2025-05-16 11:28:25 -0700

    [A11yPerformance] Fix some memory leaks
    ScopedCoMemArray<IA2TextSelection> must release the references held on
    its contained COM interface pointers. While at it, I deleted a bunch of
    code in favor of using a base::span.
    
    Also fix two reference leaks in tests.
    
    AX-Relnotes: n/a.
    Bug: 40665982
    Change-Id: I7e451bf54569b0bed496d9e604f633c27922efbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551728
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1461506}

---
+++ ui/accessibility/platform/iaccessible2/scoped_co_mem_array.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/accessibility/platform/iaccessible2/scoped_co_mem_array.h"
+
+#include "base/containers/span.h"
+#include "third_party/iaccessible2/ia2_api_all.h"
+
+namespace ui {
+
+template <>
+void ScopedCoMemArray<IA2TextSelection>::FreeContents(
+    base::span<const IA2TextSelection> contents) {
+  for (const auto& selection : contents) {
+    if (selection.startObj) {
+      selection.startObj->Release();
+    }
+    if (selection.endObj) {
+      selection.endObj->Release();
+    }
+  }
+}
+
+}  // namespace ui
