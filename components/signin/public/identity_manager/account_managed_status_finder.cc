06d7da49e5b51fe840fa7fa60ac856726e4041c2
Marc Treib <treib@chromium.org>
2025-05-16 10:05:28 -0700

    AccountManagedStatusFinder: Fix one edge case
    Before this CL, there was one possible sequence of events / observer
    notifications that AccountManagedStatusFinder didn't handle correctly:
    * AccountManagedStatusFinder gets created before refresh tokens are
      loaded from disk.
    * OnExtendedAccountInfoUpdated() gets called for the interesting
      account before the refresh token load finishes.
    
    In that situation, AccountManagedStatusFinder might have wrongly
    returned kConsumerNotWellKnown instead of kConsumerGmail or
    kConsumerWellKnown, or kEnterprise instead of kEnterpriseGoogleDotCom.
    (So the "overall" consumer vs enterprise determination was still
    correct, only the "subtype" was off.)
    
    This CL fixes that situation and adds a test.
    
    Bug: 417990091
    Change-Id: I125b59c5b1c20ed3758acc687f6af68d9a3db320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6554210
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461435}

--- components/signin/public/identity_manager/account_managed_status_finder.cc
+++ components/signin/public/identity_manager/account_managed_status_finder.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/time/time.h"
 #include "components/signin/public/identity_manager/account_info.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 
@@ -549,7 +550,15 @@ void AccountManagedStatusFinder::OnExtendedAccountInfoUpdated(
     return;
   }
 
-  // This is the relevant account! Determine its type.
+  if (!identity_manager_->AreRefreshTokensLoaded()) {
+    // `OnRefreshTokensLoaded()` will update the outcome.
+    return;
+  }
+
+  // This is the relevant account! Determine its type. It can't be any of the
+  // types that can be known synchronously, otherwise it would have been
+  // determined already, either in the constructor or in
+  // `OnRefreshTokensLoaded()`.
   OutcomeDeterminedAsync(info.IsManaged() ? Outcome::kEnterprise
                                           : Outcome::kConsumerNotWellKnown);
 }
@@ -574,7 +583,7 @@ void AccountManagedStatusFinder::OnErrorStateOfRefreshTokenUpdatedForAccount(
   DCHECK_EQ(outcome_, Outcome::kPending);
 
   if (!identity_manager_->AreRefreshTokensLoaded()) {
-    // `OnRefreshTokensLoaded` will update the outcome.
+    // `OnRefreshTokensLoaded()` will update the outcome.
     return;
   }
 
@@ -654,10 +663,10 @@ AccountManagedStatusFinder::DetermineOutcome() const {
                             : Outcome::kConsumerNotWellKnown;
   }
 
-  GoogleServiceAuthError authError =
+  GoogleServiceAuthError auth_error =
       identity_manager_->GetErrorStateOfRefreshTokenForAccount(
           account_.account_id);
-  if (!ignore_persistent_auth_errors_ && authError.IsPersistentError()) {
+  if (!ignore_persistent_auth_errors_ && auth_error.IsPersistentError()) {
     return Outcome::kTimeout;
   }
 
