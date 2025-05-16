1a33099a281eb16d4f7e1b2a58953201dd448f9d
Alison Gale <agale@chromium.org>
2025-05-16 13:42:15 -0700

    Use enum for comparing active and inactive tab width
    This avoids an issue where we allocate extra pixels to some tabs and tab
    closing mode handles the logic differently than if there aren't extra
    pixels.
    
    There are still other uses of the exact values which I'll audit
    separately but will need to investigate more to ensure they are safe.
    
    #top-chrome-bug-fixit
    
    Bug: 418256399
    Change-Id: I457e9a0cbeb510ad0e5d15cdeeac240e1360a1a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6557846
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461606}

--- chrome/browser/ui/views/tabs/tab_strip_layout.cc
+++ chrome/browser/ui/views/tabs/tab_strip_layout.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "chrome/browser/ui/tabs/tab_style.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_layout_types.h"
 #include "components/tabs/public/split_tab_id.h"
 #include "ui/gfx/animation/tween.h"
 #include "ui/gfx/geometry/rect.h"
@@ -138,11 +139,12 @@ void AllocateExtraSpace(std::vector<gfx::Rect>* bounds,
   }
 }
 
-std::vector<gfx::Rect> CalculateTabBounds(
+std::pair<std::vector<gfx::Rect>, LayoutDomain> CalculateTabBounds(
     const std::vector<TabWidthConstraints>& tabs,
     std::optional<int> width) {
   if (tabs.empty()) {
-    return std::vector<gfx::Rect>();
+    return {std::vector<gfx::Rect>(),
+            LayoutDomain::kInactiveWidthEqualsActiveWidth};
   }
 
   TabSizer tab_sizer = CalculateSpaceFractionAvailable(tabs, width);
@@ -163,5 +165,5 @@ std::vector<gfx::Rect> CalculateTabBounds(
   const std::optional<int> extra_space = calculated_extra_space;
   AllocateExtraSpace(&bounds, tabs, extra_space, tab_sizer);
 
-  return bounds;
+  return {bounds, tab_sizer.domain()};
 }
--- chrome/browser/ui/views/tabs/tab_strip_layout_helper.h
+++ chrome/browser/ui/views/tabs/tab_strip_layout_helper.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab_layout_state.h"
 #include "chrome/browser/ui/views/tabs/tab_slot_view.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_layout.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_layout_types.h"
 #include "chrome/browser/ui/views/tabs/tab_width_constraints.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/view_model.h"
@@ -51,6 +52,7 @@ class TabStripLayoutHelper {
 
   int active_tab_width() { return active_tab_width_; }
   int inactive_tab_width() { return inactive_tab_width_; }
+  LayoutDomain layout_domain() { return tab_strip_layout_domain_; }
 
   // Returns the number of pinned tabs in the tabstrip.
   size_t GetPinnedTabCount() const;
@@ -112,7 +114,7 @@ class TabStripLayoutHelper {
 
   // Calculates the bounds each tab should occupy, subject to the provided
   // width constraint.
-  std::vector<gfx::Rect> CalculateIdealBounds(
+  std::pair<std::vector<gfx::Rect>, LayoutDomain> CalculateIdealBounds(
       std::optional<int> available_width);
 
   // Given `model_index` for a tab already present in `slots_`, return
@@ -172,6 +174,8 @@ class TabStripLayoutHelper {
   // into these widths, the initial tabs in the strip will be 1 px larger.
   int active_tab_width_;
   int inactive_tab_width_;
+
+  LayoutDomain tab_strip_layout_domain_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_LAYOUT_HELPER_H_
