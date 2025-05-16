847aa370b02fb78ad0cbff538fb7b6556a00a7f4
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-05-16 10:07:28 -0700

    [Installer Downloader] Implement download logic
    This CL implements the installer download logic in the model. It's a
    transient download triggered using the DownloadManager.
    
    Bug: 412976021
    Change-Id: Iff3be4d548f47a8b22bdfad60646f52a32508d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6505543
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461436}

--- chrome/browser/win/installer_downloader/installer_downloader_model_impl.h
+++ chrome/browser/win/installer_downloader/installer_downloader_model_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "chrome/browser/win/installer_downloader/installer_downloader_model.h"
+#include "components/download/public/common/download_interrupt_reasons.h"
 
 class GURL;
 
@@ -18,9 +19,18 @@ namespace base {
 class FilePath;
 }
 
+namespace content {
+class DownloadManager;
+}
+
+namespace download {
+class DownloadItem;
+}
+
 namespace installer_downloader {
 
 class SystemInfoProvider;
+class InstallerDownloaderObserver;
 
 // Non-UI service that:
 //   •  Checks whether the current machine is a Win 10 device **not**
@@ -52,13 +62,27 @@ class InstallerDownloaderModelImpl : public InstallerDownloaderModel {
       override;
   void StartDownload(const GURL& url,
                      const base::FilePath& destination,
+                     content::DownloadManager& download_manager,
                      CompletionCallback completion_callback) override;
   bool IsMaxShowCountReached() const override;
 
  private:
   std::optional<base::FilePath> GetInstallerDestination() const;
 
-  std::unique_ptr<SystemInfoProvider> system_info_provider_;
+  // Invoked when the installer download started.
+  void OnInstallerDownloadCreated(CompletionCallback completion_callback,
+                                  download::DownloadItem* item,
+                                  download::DownloadInterruptReason reason);
+
+  void OnInstallerDownloadFinished(CompletionCallback completion_callback,
+                                   bool succeeded);
+
+  const std::unique_ptr<SystemInfoProvider> system_info_provider_;
+
+  // This is instantiated when a download start and is reset when the
+  // download is finished or stopped. It should be null at any point when no
+  // download is in progress.
+  std::unique_ptr<InstallerDownloaderObserver> installer_downloader_observer_;
 };
 
 }  // namespace installer_downloader
