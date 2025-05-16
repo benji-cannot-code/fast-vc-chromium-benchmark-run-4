7410bc9523e0b28d382a8f8ea84119d682284777
Ron Sailer <rsailer@google.com>
2025-05-16 12:01:48 -0700

    Invasive Script Blocking
    -   Adds method in WKContentRuleListProvider to update/clear a script
    blocking rule list.
    -   Updates tests to cover this.
    -   Restructures WKWebViewConfigurationProviderTest to allow setup /
    teardown code for rule list management tests
    -   Exposes WKContentRuleListProvider via the
    WKWebViewConfigurationProvider
    
    Change-Id: I4aaf98894479496d2f2282be1615bd67bf2cd446
    Bug: 418218469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513358
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Ron Sailer <rsailer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461533}

--- ios/web/web_state/ui/wk_web_view_configuration_provider.mm
+++ ios/web/web_state/ui/wk_web_view_configuration_provider.mm
@@ -284,6 +284,11 @@ void WKWebViewConfigurationProvider::Purge() {
   configuration_ = nil;
 }
 
+WKContentRuleListProvider*
+WKWebViewConfigurationProvider::GetContentRuleListProvider() {
+  return content_rule_list_provider_.get();
+}
+
 base::CallbackListSubscription
 WKWebViewConfigurationProvider::RegisterWebSiteDataStoreUpdatedCallback(
     WebSiteDataStoreUpdatedCallbackList::CallbackType callback) {
