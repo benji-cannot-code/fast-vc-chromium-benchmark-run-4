44b15f293c23a3db4815bc0882e61ee0a568c798
Jennifer Serrano <jennserrano@google.com>
2025-05-16 13:12:23 -0700

    Fix customize chrome theme preview appearance
    The preview appearance button is missing in first party NTPs because of the mojo changes introduced in http://crrev.com/c//6516213.
    
    Before:
    screen/6PbeCcwU467AfsD
    
    After:
    First party NTP with Default Chrome:
    screen/4LUxC4HzgxF8gaN
    
    First party NTP with installed theme:
    screen/4uVamegEYDAoDLz
    
    On Non-NTP:
    screen/9i8NfNW3zNJ7Lrf
    
    Bug: 417650227
    Change-Id: I0c6f6a36ab18ecad8811771aed51b5cddb84a142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550856
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461579}

--- chrome/browser/resources/side_panel/customize_chrome/appearance.ts
+++ chrome/browser/resources/side_panel/customize_chrome/appearance.ts
@@ -24,6 +24,7 @@ import {getCss} from './appearance.css.js';
 import {getHtml} from './appearance.html.js';
 import {CustomizeChromeAction, recordCustomizeChromeAction} from './common.js';
 import type {CustomizeChromePageCallbackRouter, CustomizeChromePageHandlerInterface, Theme} from './customize_chrome.mojom-webui.js';
+import {NewTabPageType} from './customize_chrome.mojom-webui.js';
 import {CustomizeChromeApiProxy} from './customize_chrome_api_proxy.js';
 
 export interface AppearanceElement {
@@ -79,7 +80,7 @@ export class AppearanceElement extends AppearanceElementBase {
       showSearchedImageButton_: {type: Boolean},
       showManagedDialog_: {type: Boolean},
       showEditTheme_: {type: Boolean},
-      isSourceTabFirstPartyNtp_: {type: Boolean},
+      newTabPageType_: {type: NewTabPageType},
 
       wallpaperSearchButtonEnabled_: {
         type: Boolean,
@@ -106,7 +107,8 @@ export class AppearanceElement extends AppearanceElementBase {
       loadTimeData.getBoolean('wallpaperSearchButtonEnabled');
   private accessor wallpaperSearchEnabled_: boolean =
       loadTimeData.getBoolean('wallpaperSearchEnabled');
-  protected accessor isSourceTabFirstPartyNtp_: boolean = true;
+  protected accessor newTabPageType_: NewTabPageType =
+      NewTabPageType.kFirstPartyWebUI;
   protected accessor showEditTheme_: boolean = true;
   protected ntpManagedByName_: string = '';
   private setThemeEditableId_: number|null = null;
@@ -135,8 +137,8 @@ export class AppearanceElement extends AppearanceElementBase {
     this.attachedTabStateUpdatedId_ =
         CustomizeChromeApiProxy.getInstance()
             .callbackRouter.attachedTabStateUpdated.addListener(
-                (isSourceTabFirstPartyNtp: boolean) => {
-                  this.isSourceTabFirstPartyNtp_ = isSourceTabFirstPartyNtp;
+                (newTabPageType: NewTabPageType) => {
+                  this.newTabPageType_ = newTabPageType;
                 });
     this.pageHandler_.updateAttachedTabState();
 
@@ -179,7 +181,7 @@ export class AppearanceElement extends AppearanceElementBase {
     this.editThemeButtonText_ = this.computeEditThemeButtonText_();
 
     if (changedPrivateProperties.has('theme_') ||
-        changedPrivateProperties.has('isSourceTabFirstPartyNtp_')) {
+        changedPrivateProperties.has('newTabPageType_')) {
       this.thirdPartyThemeId_ = this.computeThirdPartyThemeId_();
       this.thirdPartyThemeName_ = this.computeThirdPartyThemeName_();
       this.showClassicChromeButton_ = this.computeShowClassicChromeButton_();
@@ -260,7 +262,7 @@ export class AppearanceElement extends AppearanceElementBase {
            this.theme_.backgroundImage.isUploadedImage)) &&
         // TODO(crbug.com/404247286) Enable snapshots for extension NTP with 1P
         // theme.
-        this.isSourceTabFirstPartyNtp_;
+        this.isSourceTabFirstPartyNtp_();
   }
 
   private computeShowUploadedImageButton_(): boolean {
@@ -274,6 +276,10 @@ export class AppearanceElement extends AppearanceElementBase {
     return !!(
         this.theme_ && this.theme_.backgroundImage &&
         this.theme_.backgroundImage.localBackgroundId);
+  }
+
+  protected isSourceTabFirstPartyNtp_(): boolean {
+    return this.newTabPageType_ === NewTabPageType.kFirstPartyWebUI;
   }
 
   protected onEditThemeClicked_() {
