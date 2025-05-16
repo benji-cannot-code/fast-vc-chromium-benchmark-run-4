75923fec1413a0788ab59a1e840a1aec681d9401
Eshwar Stalin <estalin@chromium.org>
2025-05-16 12:21:30 -0700

    Fixing some bugs for enabling TabSearch in ToT
    There were a couple of issues with the previous CL which attempted to
    enable TabSearch in ToT. These included the country value was in lower
    case and so fixed that. Also GlicEnabling needed to use
    IsTabSearchMoving instead of directly checking the feature flag. These
    two issues are now fixed.
    
    Bug: 395951494
    Change-Id: Ice465e85aa81bcffa6e78c2a5592a015a4e826bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6557102
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461541}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -456,7 +456,7 @@ static std::string GetCountryCode() {
 
 bool IsTabSearchMoving() {
   static const bool is_tab_search_moving = [] {
-    if (GetCountryCode() == "US" &&
+    if (GetCountryCode() == "us" &&
         features::kLaunchedTabSearchToolbarButton.Get()) {
       return true;
     }
