1d74ce263de5b5ff22844a844d88d222ff225650
Salma Elmahallawy <esalma@google.com>
2025-05-16 12:29:36 -0700

    [NTPFooter] Update feature that the flag controls
    Bug: 401541301
    Change-Id: Ie11cca4aa877e9085380ed91a31e4a3cc4f47b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6556784
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461554}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4808,7 +4808,7 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnterpriseBadgingForNtpFooterName,
      flag_descriptions::kEnterpriseBadgingForNtpFooterDescription,
      kOsMac | kOsWin | kOsLinux,
-     FEATURE_VALUE_TYPE(features::kEnterpriseProfileBadgingForMenu)},
+     FEATURE_VALUE_TYPE(features::kEnterpriseBadgingForNtpFooter)},
     {"enable-enterprise-updated-profile-creation-screen",
      flag_descriptions::kEnterpriseUpdatedProfileCreationScreenName,
      flag_descriptions::kEnterpriseUpdatedProfileCreationScreenDescription,
