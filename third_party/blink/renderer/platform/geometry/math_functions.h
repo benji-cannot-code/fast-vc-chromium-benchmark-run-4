816a8ca4370147d6e8290220171d30d76c1da24b
Takuto Ikuta <tikuta@chromium.org>
2025-05-16 09:47:43 -0700

    blink: Add missing includes
    This is to fix build error with Clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=junov@chromium.org
    
    Bug: 40263312
    Change-Id: I7a3e8022bc22cb9d14bc00e21973167d7365e4bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550760
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461420}

--- third_party/blink/renderer/platform/geometry/math_functions.h
+++ third_party/blink/renderer/platform/geometry/math_functions.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cfloat>
 #include <cmath>
 #include <optional>
+#include <type_traits>
 #include <utility>
 
 #include "base/notreached.h"
