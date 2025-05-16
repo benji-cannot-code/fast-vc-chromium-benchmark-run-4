ffd0a899b8fc96cc2243e73d65b5a5e60f5e59e3
Kavita Soni <kavitasoni@chromium.org>
2025-05-16 10:46:59 -0700

    [A11y] Semibold legal message links in payments bottomsheet on ios.
    This CL makes the legal message links in Save Card and VCN Enrollment
    bottomsheets semibold so that the links are distinguishable even in
    greyscale mode.
    
    The VCN Enrollment bottomsheet still has an additional link (in its
    subtitle) that is not fixed by this CL.
    
    Save Card Bottomsheet Screenshot: http://screen/4Y7nVknE9VWP2h4
    Save Card Bottomsheet Greyscale screenshot:
    http://screen/AGvCixxwjoaUAJW
    Virtual Card Bottomsheet Screenshot: http://screen/AdwFe5vaTnnUgeR
    
    Bug: 415102316, 417637113
    Change-Id: I9e6f6a7248a6e19161ef9a43eb4f7605602feb4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6552582
    Commit-Queue: Kavita Soni <kavitasoni@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461465}

--- ios/chrome/browser/autofill/ui_bundled/autofill_credit_card_util.mm
+++ ios/chrome/browser/autofill/ui_bundled/autofill_credit_card_util.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/autofill/ui_bundled/autofill_credit_card_util.h"
 
+#import <UIKit/UIKit.h>
+
 #import "base/check.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/autofill/core/browser/autofill_type.h"
@@ -12,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/ui_bundled/autofill_credit_card_ui_type.h"
 #import "ios/chrome/browser/autofill/ui_bundled/autofill_ui_type_util.h"
 #import "ios/chrome/browser/net/model/crurl.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/text_view_util.h"
 
@@ -181,9 +184,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (!crurl || !crurl.gurl.is_valid()) {
         return;
       }
-      [attributedText addAttribute:NSLinkAttributeName
-                             value:crurl.nsurl
-                             range:rangeValue.rangeValue];
+      NSDictionary* linkAttributes = @{
+        NSLinkAttributeName : crurl.nsurl,
+        NSFontAttributeName : PreferredFontForTextStyle(UIFontTextStyleCaption2,
+                                                        UIFontWeightSemibold)
+      };
+      [attributedText addAttributes:linkAttributes range:rangeValue.rangeValue];
     }];
   }
   return attributedText;
