07cc59ed73c70f02e6efc3e674441369b0e38eb6
Roger Johannesson <roger.johannesson@xperi.com>
2025-05-16 01:36:38 -0700

    ScriptId should only be requested from source text modules
    V8 triggers a fatal error when requesting ScriptId on a
    non source text module. Add similar protection when tracing
    as is done further up in this function when evaluating the
    module.
    
    The fatal error was hit in some cases when importing
    synthetic modules (like JSON modules) while collecting
    performance trace data via devtools.
    
    Bug: 417673004
    Change-Id: I7564d97b0491dade7ddb6e6795ee3eb43cce768f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6545988
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Roger Johannesson <roger.johannesson@xperi.com>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461223}

--- third_party/blink/renderer/core/inspector/inspector_trace_events.cc
+++ third_party/blink/renderer/core/inspector/inspector_trace_events.cc
@@ -1229,6 +1229,10 @@ void inspector_target_rundown_event::Data(perfetto::TracedValue context,
                                           v8::Isolate* isolate,
                                           ScriptState* scriptState,
                                           int scriptId) {
+  if (scriptId == v8::UnboundScript::kNoScriptId) {
+    return;
+  }
+
   // Target related info
   LocalDOMWindow* window = DynamicTo<LocalDOMWindow>(execution_context);
   LocalFrame* frame = window ? window->GetFrame() : nullptr;
