030dfc9c2050fb6daf3f1f9225c5eb41cc50b693
Volodymyr Riazantsev <riazantsevv@google.com>
2025-05-16 09:57:19 -0700

    cast: streaming: Allow to specify a receiver port
    Allow to specify a Cast Streaming port on the receiver side.
    Currently the port is assigned randomly, but some systems can
    have a firewall configured, so the receiver configuration is expected to
    be static.
    
    Add a command line switch `--cast-streaming-receiver-port=<port>` to
    support such a use case.
    
          specified port.
    
    Bug: b:389728258
    Test: Run Cast receiver with the flag, ensure RTP stream is going to the
    Change-Id: I52ff5c5bade7759d733684ff113083b27d0e6830
    Signed-off-by: Volodymyr Riazantsev <riazantsevv@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508746
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Antonio Rivera <antoniori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461427}

---
+++ components/cast_streaming/browser/cast_streaming_switches.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CAST_STREAMING_BROWSER_CAST_STREAMING_SWITCHES_H_
+#define COMPONENTS_CAST_STREAMING_BROWSER_CAST_STREAMING_SWITCHES_H_
+
+namespace switches {
+
+// If set, allows to use a specific UDP port for Cast Streaming sessions on the
+// receiver side. Otherwise the Cast Streaming receiver is using a random
+// system port.
+inline constexpr char kCastStreamingReceiverPort[] =
+    "cast-streaming-receiver-port";
+
+}  // namespace switches
+
+#endif  // COMPONENTS_CAST_STREAMING_BROWSER_CAST_STREAMING_SWITCHES_H_
