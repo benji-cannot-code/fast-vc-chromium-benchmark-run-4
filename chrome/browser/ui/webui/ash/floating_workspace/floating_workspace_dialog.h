510742d56e1bc14337b84215dfe2ef59bbaa9532
Andrey Davydov <andreydav@google.com>
2025-05-16 07:22:35 -0700

    Reland "FWS: new startup UX"
    The original cl at https://crrev.com/c/6554262 exposed an issue with an
    uninitialized member variable which broke the msan build. The issue is
    fixed in https://crrev.com/c/6553929, this reland is done manually due
    to a merge conflict.
    
    Original description:
    
    Revamp UX/UI according to go/desk-sync: instead of notifications we now
    use a dedicated modal dialog.
    
    Bug: b:411121762
    Test: unit tests FloatingWorkspace*
    Change-Id: If72a2fc739b26c8264e7b26c56c91be8dcc16fde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6555872
    Reviewed-by: Michael Slutskii <slutskii@google.com>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461334}

--- chrome/browser/ui/webui/ash/floating_workspace/floating_workspace_dialog.h
+++ chrome/browser/ui/webui/ash/floating_workspace/floating_workspace_dialog.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_ASH_FLOATING_WORKSPACE_FLOATING_WORKSPACE_DIALOG_H_
 #define CHROME_BROWSER_UI_WEBUI_ASH_FLOATING_WORKSPACE_FLOATING_WORKSPACE_DIALOG_H_
 
+#include <optional>
+
 #include "chrome/browser/ui/webui/ash/system_web_dialog/system_web_dialog_delegate.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 
@@ -17,6 +19,9 @@ class FloatingWorkspaceDialogHandler;
 // workspace fetches the state.
 class FloatingWorkspaceDialog : public SystemWebDialogDelegate {
  public:
+  // This dialog can be in only one of three states.
+  enum class State { kDefault, kNetwork, kError };
+
   FloatingWorkspaceDialog(const FloatingWorkspaceDialog&) = delete;
   FloatingWorkspaceDialog& operator=(const FloatingWorkspaceDialog&) = delete;
   ~FloatingWorkspaceDialog() override;
@@ -24,7 +29,9 @@ class FloatingWorkspaceDialog : public SystemWebDialogDelegate {
   static void ShowDefaultScreen();
   static void ShowNetworkScreen();
   static void ShowErrorScreen();
-  static bool IsShown();
+  // Returns an empty optional if the dialog is not shown, otherwise returns
+  // it's current state.
+  static std::optional<State> IsShown();
 
   // Closes the dialog if it's currently opened.
   static void Close();
--- chrome/browser/ui/webui/ash/floating_workspace/floating_workspace_handler.h
+++ chrome/browser/ui/webui/ash/floating_workspace/floating_workspace_handler.h
@@ -8,15 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
+#include "chrome/browser/ui/webui/ash/floating_workspace/floating_workspace_dialog.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
 namespace ash {
 
 class FloatingWorkspaceDialogHandler : public content::WebUIMessageHandler {
  public:
-  // This dialog can be in only one of three states.
-  enum class DialogState { kDefault, kNetwork, kError };
-
   FloatingWorkspaceDialogHandler();
   ~FloatingWorkspaceDialogHandler() override;
 
@@ -27,6 +25,8 @@ class FloatingWorkspaceDialogHandler : public content::WebUIMessageHandler {
   void ShowNetworkScreen();
   void ShowErrorScreen();
 
+  FloatingWorkspaceDialog::State state() { return state_; }
+
  private:
   void Initialize(const base::Value::List& args);
   void ShowNetworkDetails(const base::Value::List& args);
@@ -35,7 +35,8 @@ class FloatingWorkspaceDialogHandler : public content::WebUIMessageHandler {
   void GetHostname(const base::Value::List& args);
   void Respond(const std::string& callback_id, base::ValueView response);
 
-  DialogState state_ = DialogState::kDefault;
+  FloatingWorkspaceDialog::State state_ =
+      FloatingWorkspaceDialog::State::kDefault;
   base::WeakPtrFactory<FloatingWorkspaceDialogHandler> weak_ptr_factory_{this};
 };
 
