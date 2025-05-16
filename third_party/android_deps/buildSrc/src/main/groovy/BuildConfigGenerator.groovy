12b647dc098885de6bf665f565ac7141dd0c9b87
Peter Wen <wnwen@chromium.org>
2025-05-16 12:01:11 -0700

    Revert "Roll android_deps from TZHdcfZjA4uMdOqea... to PHPiIFpY5eGci8_VC..."
    This reverts commit 0763910d8f291c9e14c8e8d26f58d62b1b15eaa7.
    
    Reason for revert: https://ci.chromium.org/ui/p/chrome/builders/try/test-o-emulator/21700/overview
    
    Bug: 417478928
    Original change's description:
    > Roll android_deps from TZHdcfZjA4uMdOqea... to PHPiIFpY5eGci8_VC...
    >
    > Fixed:
    > - Mark com_google_guava_guava_java as supports_android, as it uses the
    >   -android.jar already.
    > - Switch from using com_google_guava_guava_android_java to
    >   com_google_guava_guava_java as it is the same -android.jar.
    >
    > If this roll has caused a breakage, revert this CL and stop the roller
    > using the controls here:
    > https://autoroll.skia.org/r/android-deps-chromium
    > Please CC clank-build@google.com,clank-library-failures@google.com,wnwen@chromium.org,wnwen@google.com on the revert to ensure that a human
    > is aware of the problem.
    >
    > To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    >
    > To report a problem with the AutoRoller itself, please file a bug:
    > https://issues.skia.org/issues/new?component=1389291&template=1850622
    >
    > Documentation for the AutoRoller is here:
    > https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    >
    > Bug: 417478928
    > Change-Id: I9fe7890ff3541eff9935fd42fa7afc3f85401d37
    > Cq-Include-Trybots: luci.chrome.try:android-internal-binary-size;luci.chrome.try:android-internal-rel
    > Tbr: clank-library-failures@google.com,wnwen@chromium.org
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553404
    > Owners-Override: Peter Wen <wnwen@chromium.org>
    > Auto-Submit: Peter Wen <wnwen@chromium.org>
    > Commit-Queue: Peter Wen <wnwen@chromium.org>
    > Reviewed-by: Sam Maier <smaier@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1460993}
    
    Bug: 417478928
    Cq-Include-Trybots: luci.chrome.try:android-internal-binary-size;luci.chrome.try:android-internal-rel
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I713e41fb1564d419ddb623746adb56333730d101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6558368
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461532}

--- third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
@@ -886,7 +886,6 @@ No modifications.
     private static void addPreconditionsOverrideTreatment(StringBuilder sb, String dependencyId) {
         String targetName = translateTargetName(dependencyId)
         switch (targetName) {
-            case 'com_google_guava_guava':
             case 'com_google_guava_guava_android':
             case 'google_play_services_basement':
                 String libraryDep = '//third_party/android_deps/local_modifications/preconditions:' +
@@ -910,7 +909,6 @@ No modifications.
     private static String computePreconditionsStubLibraryForDep(String dependencyId) {
         String targetName = translateTargetName(dependencyId)
         switch (targetName) {
-            case 'com_google_guava_guava':
             case 'com_google_guava_guava_android':
                 return 'guava_stub_preconditions_java'
             case 'google_play_services_basement':
@@ -922,7 +920,6 @@ No modifications.
     private static String computePreconditionsClassForDep(String dependencyId) {
         String targetName = translateTargetName(dependencyId)
         switch (targetName) {
-            case 'com_google_guava_guava':
             case 'com_google_guava_guava_android':
                 return 'com/google/common/base/Preconditions.class'
             case 'google_play_services_basement':
