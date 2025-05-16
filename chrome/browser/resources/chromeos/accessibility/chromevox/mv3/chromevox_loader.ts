21ff5f12bcd702a90d494608f581dd5a17aff4d0
valerie young <spectranaut@igalia.com>
2025-05-16 09:04:59 -0700

    [ChromeVox] Update MV3 build to use non-closure MessageFormat
    The same logic applied in this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6469376
    
    To the MV3 directory.
    
    Bug: 388867840
    Change-Id: Ibc0b229a8c3b2a7aff6a34c67949f03914dad939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6554604
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1461403}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/chromevox_loader.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/chromevox_loader.ts
@@ -3,16 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Closure scripts. Each call to importScripts() depends on the scripts in the previous calls.
-// Can be found in //third_party/chromevox/third_party/closure_library/closure/goog/.
-importScripts('../closure/base.js');
-importScripts(
-    '../closure/debug/error.js','../closure/dom/nodetype.js', '../closure/i18n/ordinalrules.js',
-    '../closure/i18n/pluralrules.js', '../closure/string/string.js');
-importScripts('../closure/asserts/asserts.js');
-importScripts('../closure/i18n/messageformat.js');
-importScripts('common/closure_loader.js');
-
 // ChromeVox ES6 modules. Non-ES6 modules cannot depend on these modules.
 import 'background/es6_loader.js';
 
