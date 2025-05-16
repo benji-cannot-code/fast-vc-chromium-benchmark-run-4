2e0a6ba8ee6b1e2ebc871cbcc8df3853a2b19956
Min Qin <qinmin@chromium.org>
2025-05-16 09:54:53 -0700

    [Reland] Fix some issues in NotificationManagerProxy
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/6466664. The original
    CL was reverted because of failing tests. The test are using
    NotificationManager to get active notifications directly. However,
    since the CL will make NotificationManager to post notification
    on a background thread, we also need to get the active notification
    by posting a task.
    PS#1 is the original CL.
    PS#2 is the new code to fix the affected tests.
    
    this CL did the following:
    1. Wrong class is created in AsncNotificationManagerProxyImpl#getInstance()
    2. Didn't initialize sInstance in NotificationmanagerProxyImpl
    3. adding a trace event for ctors and move actual notificationManager
       creation to background thread for async version
    
    Bug: 411465437
    Change-Id: I3e8605ecf0f8090994960cf007380d4d495a779c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553481
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461426}

--- chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoNotificationServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoNotificationServiceTest.java
@@ -7,12 +7,10 @@ package org.chromium.chrome.browser.incognito;
 
 import static org.junit.Assert.assertTrue;
 
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.PendingIntent.CanceledException;
 import android.content.Context;
 import android.content.Intent;
-import android.service.notification.StatusBarNotification;
 import android.util.Pair;
 
 import androidx.test.core.app.ApplicationProvider;
@@ -24,7 +22,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.util.CommandLineFlags;
@@ -32,6 +29,7 @@ import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.PayloadCallbackHelper;
 import org.chromium.chrome.browser.customtabs.CustomTabsIntentTestUtils;
 import org.chromium.chrome.browser.customtabs.IncognitoCustomTabActivityTestRule;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -44,9 +42,12 @@ import org.chromium.chrome.browser.tabpersistence.TabStateFileManager;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
+import org.chromium.components.browser_ui.notifications.BaseNotificationManagerProxy.StatusBarNotificationProxy;
+import org.chromium.components.browser_ui.notifications.BaseNotificationManagerProxyFactory;
 import org.chromium.content_public.browser.LoadUrlParams;
 
 import java.io.File;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 /** Tests for the Incognito Notification service. */
@@ -96,11 +97,8 @@ public class IncognitoNotificationServiceTest {
                             Matchers.greaterThanOrEqualTo(1));
                 });
 
-        Context context = ContextUtils.getApplicationContext();
-        NotificationManager nm =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         boolean isIncognitoNotificationDisplayed = false;
-        for (StatusBarNotification statusBarNotification : nm.getActiveNotifications()) {
+        for (StatusBarNotificationProxy statusBarNotification : getActiveNotifications()) {
             if (IncognitoNotificationManager.INCOGNITO_TABS_OPEN_TAG.equals(
                     statusBarNotification.getTag())) {
                 isIncognitoNotificationDisplayed = true;
@@ -281,5 +279,12 @@ public class IncognitoNotificationServiceTest {
                                     .getCount(),
                             Matchers.is(expectedCount));
                 });
+    }
+
+    private static List<? extends StatusBarNotificationProxy> getActiveNotifications() {
+        PayloadCallbackHelper<List<? extends StatusBarNotificationProxy>> helper =
+                new PayloadCallbackHelper();
+        BaseNotificationManagerProxyFactory.create().getActiveNotifications(helper::notifyCalled);
+        return helper.getOnlyPayloadBlocking();
     }
 }
