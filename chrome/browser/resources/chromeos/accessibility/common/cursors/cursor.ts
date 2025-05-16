b5a1ccbb1de26895ced13fd5e174e9205ccb60ef
Alexander Surkov <asurkov@igalia.com>
2025-05-16 07:57:48 -0700

    [Accessibility Common] Fix AccessibilityExtensionCursorsTest tests in MV3
    Dynamic imports are disallowed in MV3. TestImportManager is used
    instead. accessibility/common tests run in an environment with
    the autoclick extension enabled, which is part of
    the accessibility_common extension. Adding required utility imports
    into autoclick script allows to workaround the test failures.
    
    Bug: 388867838
    Change-Id: Iee7c6c927362e5a085fa9962f227f6ebd695c11d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549278
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461356}

--- chrome/browser/resources/chromeos/accessibility/common/cursors/cursor.ts
+++ chrome/browser/resources/chromeos/accessibility/common/cursors/cursor.ts
@@ -687,3 +687,6 @@ export class WrappingCursor extends Cursor {
 }
 
 TestImportManager.exportForTesting(Cursor);
+TestImportManager.exportForTesting(['CursorMovement', CursorMovement]);
+TestImportManager.exportForTesting(['CursorUnit', CursorUnit]);
+TestImportManager.exportForTesting(WrappingCursor);
