96c7e3adc425dd0b9f71478185a078ce8242fb32
Michael Slutskii <slutskii@google.com>
2025-05-16 05:52:42 -0700

    Reland "Add message handler to the floating workspace dialog"
    This is a reland of commit 214a1479f69489c1b7c796cfbf4baaae6fd5dfbc
    
    The state member variable wasn't initialized by default in
    FloatingWorkspaceDialogHandler which broke the msan build.
    
    Original change's description:
    > Add message handler to the floating workspace dialog
    >
    > Add a message handler for the FW dialog and some functions,
    > so it would be possible to change dialog states depending
    > on the floating workspace service logic.
    >
    > Note: resources themselves will be added in a follow-up
    > CL.
    >
    > Screenshot: screenshot/BsYSNsuWnYzo7RY.png
    >
    > Bug: b:411121762
    > Change-Id: I029e2792c06e655591564bffcd1155834ee0799a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6544148
    > Reviewed-by: Andrey Davydov <andreydav@google.com>
    > Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    > Commit-Queue: Michael Slutskii <slutskii@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1460611}
    
    Bug: b:411121762
    Change-Id: I3e35d433891f833c73653b4407e7925c0d6deb8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553929
    Reviewed-by: Andrey Davydov <andreydav@google.com>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Michael Slutskii <slutskii@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461292}

--- chrome/browser/ui/webui/ash/floating_workspace/floating_workspace_ui.h
+++ chrome/browser/ui/webui/ash/floating_workspace/floating_workspace_ui.h
@@ -9,13 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/common/chrome_os_webui_config.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chromeos/services/network_config/public/mojom/cros_network_config.mojom-forward.h"
 #include "content/public/common/url_constants.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
+#include "ui/webui/mojo_web_ui_controller.h"
 
 namespace ash {
-
 class FloatingWorkspaceUI;
-
+class FloatingWorkspaceDialogHandler;
 // The WebUIConfig for the FloatingWorkspaceUI class.
 class FloatingWorkspaceUIConfig
     : public ChromeOSWebUIConfig<FloatingWorkspaceUI> {
@@ -23,17 +25,26 @@ class FloatingWorkspaceUIConfig
   FloatingWorkspaceUIConfig();
 };
 
-class FloatingWorkspaceUI : public ui::WebDialogUI {
+class FloatingWorkspaceUI : public ui::MojoWebDialogUI {
  public:
   explicit FloatingWorkspaceUI(content::WebUI* web_ui);
   FloatingWorkspaceUI(const FloatingWorkspaceUI&) = delete;
   FloatingWorkspaceUI& operator=(const FloatingWorkspaceUI&) = delete;
   ~FloatingWorkspaceUI() override;
 
- private:
-  base::WeakPtrFactory<FloatingWorkspaceUI> weak_factory_{this};
-};
+  // Instantiates implementation of the mojom::CrosNetworkConfig mojo interface
+  // passing the pending receiver that will be internally bound.
+  void BindInterface(
+      mojo::PendingReceiver<chromeos::network_config::mojom::CrosNetworkConfig>
+          receiver);
+  FloatingWorkspaceDialogHandler* GetMainHandler();
 
+ private:
+  raw_ptr<FloatingWorkspaceDialogHandler> main_handler_;
+  base::WeakPtrFactory<FloatingWorkspaceUI> weak_factory_{this};
+
+  WEB_UI_CONTROLLER_TYPE_DECL();
+};
 }  // namespace ash
 
 #endif  // CHROME_BROWSER_UI_WEBUI_ASH_FLOATING_WORKSPACE_FLOATING_WORKSPACE_UI_H_
