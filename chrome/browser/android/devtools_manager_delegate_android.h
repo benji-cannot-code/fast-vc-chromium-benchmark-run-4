c7fc712d5fa75b2c4c1237b3c7ba233d25f1635c
Lei Zhang <thestig@chromium.org>
2025-05-16 15:13:49 -0700

    Fix missing //base includes in various places
    Do IWYU to fix missing include problems that will creep in when a CL
    chained to this one removes //base includes in some random, unrelated
    header. For components/ui_devtools/devtools_server.h, remove a default
    argument so base::FilePath can be forwards declared. Then fix up the one
    caller that needed to pass in an empty base::FilePath.
    
    Change-Id: I7777f2acd3ebefc03f9fb687a4392457333a0c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6552180
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461647}

--- chrome/browser/android/devtools_manager_delegate_android.h
+++ chrome/browser/android/devtools_manager_delegate_android.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/flat_map.h"
 #include "content/public/browser/devtools_agent_host_observer.h"
 #include "content/public/browser/devtools_manager_delegate.h"
 
