19ac2dfde761df27454c4bf259f2f0f2d218d825
Sam Maier <smaier@chromium.org>
2025-05-16 12:04:09 -0700

    Revert "Reland "Using aidl generated structs where possible for buildinfo classes""
    This reverts commit 6e2b4c7f92502d91802e55b5a1f874932edebbeb.
    
    Reason for revert: Possible culprit of b/418103037, speculative revert
    
    Bug: 414609682, 411426303
    Original change's description:
    > Reland "Using aidl generated structs where possible for buildinfo classes"
    >
    > This reverts commit c8401a53d5ef88a6fa58472ba500fe921f5b9cd5.
    >
    > Reason for revert: C++ field construction order didn't match definition,
    > but now it does.
    >
    > Bug: 414609682, 411426303
    > Original change's description:
    > > Revert "Using aidl generated structs where possible for buildinfo classes"
    > >
    > > This reverts commit dd34250ee82d316bfa12cfdbbf484eed129c2eca.
    > >
    > > Reason for revert:
    > > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8714787181885935281
    > >
    > > Sample failed build: https://ci.chromium.org/b/8714787181885935281
    > >
    > > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6541466&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8714787181885935281&type=BUG
    > >
    > > Original change's description:
    > > > Using aidl generated structs where possible for buildinfo classes
    > > >
    > > > Bug: 414609682, 411426303
    > > > Change-Id: I2405127f0e93ba2d08b5c2bf74a348db6264ea88
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6541466
    > > > Owners-Override: Sam Maier <smaier@chromium.org>
    > > > Commit-Queue: Sam Maier <smaier@chromium.org>
    > > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1460803}
    > > >
    > >
    > > Bug: 414609682, 411426303
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Change-Id: I18318a6bd4c25396c44b8868832348780dd586dc
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6552227
    > > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/main@{#1460820}
    >
    > Bug: 414609682, 411426303
    > Change-Id: I7dd4479597697bd2e1a727b6ad8eca1cf1fdf9a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6552039
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Sam Maier <smaier@chromium.org>
    > Auto-Submit: Sam Maier <smaier@chromium.org>
    > Commit-Queue: Sam Maier <smaier@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1460893}
    
    Bug: 414609682, 411426303
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I8ce10eb87c800c87a3c5d43d78e5a48e6013e9de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6556863
    Owners-Override: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461537}

--- base/android/java/src/org/chromium/base/DeviceInfo.java
+++ base/android/java/src/org/chromium/base/DeviceInfo.java
@@ -36,7 +36,26 @@ public final class DeviceInfo {
 
     private static @Nullable String sGmsVersionCodeForTesting;
     private static boolean sInitialized;
-    private final IDeviceInfo mIDeviceInfo;
+
+    /** The versionCode of Play Services. Can be overridden in tests. */
+    private final String mGmsVersionCode;
+
+    /** Whether we're running on Android TV or not */
+    private final boolean mIsTv;
+
+    /** Whether we're running on an Android Automotive OS device or not. */
+    private final boolean mIsAutomotive;
+
+    /** Whether we're running on an Android Foldable OS device or not. */
+    private final boolean mIsFoldable;
+
+    /** Whether we're running on an Android Desktop OS device or not. */
+    private final boolean mIsDesktop;
+
+    /**
+     * version of the FEATURE_VULKAN_DEQP_LEVEL, if available. Queried only on Android T or above
+     */
+    private final int mVulkanDeqpLevel;
 
     @GuardedBy("CREATION_LOCK")
     private static @Nullable DeviceInfo sInstance;
@@ -50,20 +69,19 @@ public final class DeviceInfo {
     // function.
     @CalledByNative
     private static void nativeReadyForFields() {
-        IDeviceInfo info = getInstance().mIDeviceInfo;
         DeviceInfoJni.get()
                 .fillFields(
                         /* gmsVersionCode= */ getGmsVersionCode(),
-                        /* isTV= */ info.isTv,
-                        /* isAutomotive= */ info.isAutomotive,
-                        /* isFoldable= */ info.isFoldable,
-                        /* isDesktop= */ info.isDesktop,
-                        /* vulkanDeqpLevel= */ info.vulkanDeqpLevel);
+                        /* isTV= */ isTV(),
+                        /* isAutomotive= */ isAutomotive(),
+                        /* isFoldable= */ isFoldable(),
+                        /* isDesktop= */ isDesktop(),
+                        /* vulkanDeqpLevel= */ getVulkanDeqpLevel());
     }
 
     public static String getGmsVersionCode() {
         return sGmsVersionCodeForTesting == null
-                ? getInstance().mIDeviceInfo.gmsVersionCode
+                ? getInstance().mGmsVersionCode
                 : sGmsVersionCodeForTesting;
     }
 
@@ -74,23 +92,23 @@ public final class DeviceInfo {
     }
 
     public static boolean isTV() {
-        return getInstance().mIDeviceInfo.isTv;
+        return getInstance().mIsTv;
     }
 
     public static boolean isAutomotive() {
-        return getInstance().mIDeviceInfo.isAutomotive;
+        return getInstance().mIsAutomotive;
     }
 
     public static boolean isFoldable() {
-        return getInstance().mIDeviceInfo.isFoldable;
+        return getInstance().mIsFoldable;
     }
 
     public static boolean isDesktop() {
-        return getInstance().mIDeviceInfo.isDesktop;
+        return getInstance().mIsDesktop;
     }
 
     public static int getVulkanDeqpLevel() {
-        return getInstance().mIDeviceInfo.vulkanDeqpLevel;
+        return getInstance().mVulkanDeqpLevel;
     }
 
     public static boolean isInitializedForTesting() {
@@ -125,10 +143,9 @@ public final class DeviceInfo {
     }
 
     private DeviceInfo() {
-        mIDeviceInfo = new IDeviceInfo();
         sInitialized = true;
         PackageInfo gmsPackageInfo = PackageUtils.getPackageInfo("com.google.android.gms", 0);
-        mIDeviceInfo.gmsVersionCode =
+        mGmsVersionCode =
                 gmsPackageInfo != null
                         ? String.valueOf(packageVersionCode(gmsPackageInfo))
                         : "gms versionCode not available.";
@@ -137,7 +154,7 @@ public final class DeviceInfo {
         PackageManager pm = appContext.getPackageManager();
         // See https://developer.android.com/training/tv/start/hardware.html#runtime-check.
         UiModeManager uiModeManager = (UiModeManager) appContext.getSystemService(UI_MODE_SERVICE);
-        mIDeviceInfo.isTv =
+        mIsTv =
                 uiModeManager != null
                         && uiModeManager.getCurrentModeType()
                                 == Configuration.UI_MODE_TYPE_TELEVISION;
@@ -153,14 +170,14 @@ public final class DeviceInfo {
             // should not have such a modification.
             isAutomotive = false;
         }
-        mIDeviceInfo.isAutomotive = isAutomotive;
+        mIsAutomotive = isAutomotive;
 
         // Detect whether device is foldable.
-        mIDeviceInfo.isFoldable =
+        mIsFoldable =
                 Build.VERSION.SDK_INT >= VERSION_CODES.R
                         && pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_HINGE_ANGLE);
 
-        mIDeviceInfo.isDesktop =
+        mIsDesktop =
                 pm.hasSystemFeature(PackageManager.FEATURE_PC)
                         || CommandLine.getInstance().hasSwitch(BaseSwitches.FORCE_DESKTOP_ANDROID);
 
@@ -176,7 +193,7 @@ public final class DeviceInfo {
                 }
             }
         }
-        mIDeviceInfo.vulkanDeqpLevel = vulkanLevel;
+        mVulkanDeqpLevel = vulkanLevel;
     }
 
     @NativeMethods
