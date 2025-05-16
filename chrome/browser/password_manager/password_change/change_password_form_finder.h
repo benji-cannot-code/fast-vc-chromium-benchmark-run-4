610afd1a684b6e4c3792e2b5c46a25877a64784f
Anna Tsvirchkova <atsvirchkova@google.com>
2025-05-16 07:58:58 -0700

    [APC] Call MES server for the open password change form step
    The CL introduces calling MES server for the OPEN_FORM_STEP. The reply (clicking the dom node id from the response) will be handled in the follow-up CLs.
    
    Bug: 407486413
    Change-Id: I2c74d406183d87f8e8599c1fcaaa7ed8a4c1f9ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6552357
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461357}

--- chrome/browser/password_manager/password_change/change_password_form_finder.h
+++ chrome/browser/password_manager/password_change/change_password_form_finder.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/password_manager/password_change/change_password_form_waiter.h"
 #include "components/optimization_guide/content/browser/page_content_proto_provider.h"
 
@@ -49,6 +50,15 @@ class ChangePasswordFormFinder {
 
   void OnPageContentReceived(
       std::optional<optimization_guide::AIPageContentResult> content);
+
+  OptimizationGuideKeyedService* GetOptimizationService();
+
+  void OnExecutionResponseCallback(
+      optimization_guide::OptimizationGuideModelExecutionResult
+          execution_result,
+      std::unique_ptr<
+          optimization_guide::proto::PasswordChangeSubmissionLoggingData>
+          logging_data);
 
   base::WeakPtr<content::WebContents> web_contents_;
   ChangePasswordFormWaiter::PasswordFormFoundCallback callback_;
--- chrome/browser/password_manager/password_change/change_password_form_finder_unittest.cc
+++ chrome/browser/password_manager/password_change/change_password_form_finder_unittest.cc
@@ -9,12 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/types/pass_key.h"
+#include "chrome/browser/optimization_guide/mock_optimization_guide_keyed_service.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/password_manager/account_password_store_factory.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
 #include "chrome/browser/password_manager/profile_password_store_factory.h"
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/autofill/core/common/autofill_test_utils.h"
+#include "components/optimization_guide/core/mock_optimization_guide_model_executor.h"
+#include "components/optimization_guide/core/optimization_guide_proto_util.h"
 #include "components/os_crypt/sync/os_crypt_mocker.h"
 #include "components/password_manager/core/browser/fake_form_fetcher.h"
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
@@ -34,7 +38,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 using autofill::test::CreateTestFormField;
+using testing::Invoke;
 using testing::Return;
+using testing::WithArg;
 
 class FakeChromePasswordManagerClient : public ChromePasswordManagerClient {
  public:
@@ -54,6 +60,28 @@ class FakeChromePasswordManagerClient : public ChromePasswordManagerClient {
       : ChromePasswordManagerClient(web_contents) {}
 };
 
+std::unique_ptr<KeyedService> CreateOptimizationService(
+    content::BrowserContext* context) {
+  return std::make_unique<MockOptimizationGuideKeyedService>();
+}
+
+void PostResponse(
+    optimization_guide::OptimizationGuideModelExecutionResultCallback
+        callback) {
+  optimization_guide::proto::PasswordChangeResponse response;
+  response.mutable_open_form_data()->set_dom_node_id_to_click(0);
+  response.mutable_open_form_data()->set_page_type(
+      ::optimization_guide::proto::OpenFormResponseData_PageType::
+          OpenFormResponseData_PageType_SETTINGS_PAGE);
+
+  auto result = optimization_guide::OptimizationGuideModelExecutionResult(
+      optimization_guide::AnyWrapProto(response),
+      /*execution_info=*/nullptr);
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), std::move(result),
+                                /*log_entry=*/nullptr));
+}
+
 }  // namespace
 
 class ChangePasswordFormFinderTest : public ChromeRenderViewHostTestHarness {
@@ -72,7 +100,9 @@ class ChangePasswordFormFinderTest : public ChromeRenderViewHostTestHarness {
         base::BindRepeating(&password_manager::BuildPasswordStoreInterface<
                             content::BrowserContext,
                             password_manager::MockPasswordStoreInterface>));
-
+    OptimizationGuideKeyedServiceFactory::GetInstance()
+        ->SetTestingFactoryAndUse(
+            profile(), base::BindRepeating(&CreateOptimizationService));
     // `ChromePasswordManagerClient` observes `AutofillManager`s, so
     // `ChromeAutofillClient` needs to be set up, too.
     autofill::ChromeAutofillClient::CreateForWebContents(web_contents());
@@ -105,6 +135,11 @@ class ChangePasswordFormFinderTest : public ChromeRenderViewHostTestHarness {
 
   base::PassKey<class ChangePasswordFormFinderTest> pass_key() {
     return base::PassKey<class ChangePasswordFormFinderTest>();
+  }
+
+  MockOptimizationGuideKeyedService* optimization_service() {
+    return static_cast<MockOptimizationGuideKeyedService*>(
+        OptimizationGuideKeyedServiceFactory::GetForProfile(profile()));
   }
 
  private:
@@ -162,6 +197,8 @@ TEST_F(ChangePasswordFormFinderTest,
   static_cast<content::WebContentsObserver*>(form_finder->form_waiter())
       ->DocumentOnLoadCompletedInPrimaryMainFrame();
 
+  EXPECT_CALL(*optimization_service(), ExecuteModel)
+      .WillOnce(WithArg<3>(Invoke(&PostResponse)));
   EXPECT_CALL(capture_annotated_page_content, Run)
       .WillOnce(base::test::RunOnceCallback<0>(
           optimization_guide::AIPageContentResult()));
