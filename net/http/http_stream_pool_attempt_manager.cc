1e225411d8fdfebc7e39d679461a1422a2d918cb
Kenichi Ishibashi <bashi@chromium.org>
2025-05-16 14:16:30 -0700

    HttpStreamPool: Don't create HttpBasicStream if not needed
    This is a follow-up of https://crrev.com/c/6535141. When there is no
    job to notify StreamReady(), we should just put the stream as an
    idle stream in the corresponding group, rather than creating an
    HttpBasicStream which would have been released shortly after.
    
    In Group, move ProcessPendingRequest() call from ReleaseStreamSocket()
    to AddIdleStreamSocket() to ensure that putting an idle stream can
    process pending requests.
    
    Bug: 396998469
    Change-Id: I1c901c93b8ad3c8bc42a76189608f82e23640ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550996
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461631}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -282,8 +282,8 @@ void HttpStreamPool::AttemptManager::StartJob(Job* job) {
     // already took the ownership of the idle stream socket. If we don't create
     // an HttpBasicStream here, another call of this method might exceed the
     // per-group limit.
-    CreateTextBasedStreamAndMaybeNotify(std::move(stream_socket), reuse_type,
-                                        LoadTimingInfo::ConnectTiming());
+    CreateTextBasedStreamAndNotify(std::move(stream_socket), reuse_type,
+                                   LoadTimingInfo::ConnectTiming());
     return;
   }
 
@@ -479,8 +479,8 @@ void HttpStreamPool::AttemptManager::ProcessPendingJob() {
     if (stream_socket) {
       const StreamSocketHandle::SocketReuseType reuse_type =
           GetReuseTypeFromIdleStreamSocket(*stream_socket);
-      CreateTextBasedStreamAndMaybeNotify(std::move(stream_socket), reuse_type,
-                                          LoadTimingInfo::ConnectTiming());
+      CreateTextBasedStreamAndNotify(std::move(stream_socket), reuse_type,
+                                     LoadTimingInfo::ConnectTiming());
       return;
     }
   }
@@ -1548,10 +1548,12 @@ void HttpStreamPool::AttemptManager::NotifyJobOfPreconnectComplete(
   raw_job->OnPreconnectComplete(rv);
 }
 
-void HttpStreamPool::AttemptManager::CreateTextBasedStreamAndMaybeNotify(
+void HttpStreamPool::AttemptManager::CreateTextBasedStreamAndNotify(
     std::unique_ptr<StreamSocket> stream_socket,
     StreamSocketHandle::SocketReuseType reuse_type,
     LoadTimingInfo::ConnectTiming connect_timing) {
+  CHECK(!jobs_.empty());
+
   NextProto negotiated_protocol = stream_socket->GetNegotiatedProtocol();
   CHECK_NE(negotiated_protocol, NextProto::kProtoHTTP2);
 
@@ -1562,17 +1564,7 @@ void HttpStreamPool::AttemptManager::CreateTextBasedStreamAndMaybeNotify(
       << "active=" << group_->ActiveStreamSocketCount()
       << ", limit=" << pool()->max_stream_sockets_per_group();
 
-  if (jobs_.empty()) {
-    // The ownership of the underlying `stream_socket` of `http_stream` will be
-    // moved to the group as an idle stream.
-    // TODO(crbug.com/396998469): Better to move `stream_socket` directly to
-    // the group as an idle stream without creating an HttpStream. Currently we
-    // depends on the fact that the group processes pending preconnects when
-    // the handle of the HttpStream is returned to the group.
-    return;
-  }
   NotifyStreamReady(std::move(http_stream), negotiated_protocol);
-  // `this` may be deleted.
 }
 
 bool HttpStreamPool::AttemptManager::HasAvailableSpdySession() const {
@@ -1843,9 +1835,15 @@ void HttpStreamPool::AttemptManager::OnTcpBasedAttemptComplete(
   ProcessPreconnectsAfterAttemptComplete(rv,
                                          group_->ActiveStreamSocketCount() + 1);
 
-  CHECK_NE(stream_socket->GetNegotiatedProtocol(), NextProto::kProtoHTTP2);
-  CreateTextBasedStreamAndMaybeNotify(std::move(stream_socket), reuse_type,
-                                      std::move(connect_timing));
+  // If there is no request job, put the stream as an idle stream and try to
+  // process pending requests in the group/pool.
+  if (jobs_.empty()) {
+    group_->AddIdleStreamSocket(std::move(stream_socket));
+    pool()->ProcessPendingRequestsInGroups();
+  } else {
+    CreateTextBasedStreamAndNotify(std::move(stream_socket), reuse_type,
+                                   std::move(connect_timing));
+  }
 }
 
 void HttpStreamPool::AttemptManager::OnTcpBasedAttemptSlow(
