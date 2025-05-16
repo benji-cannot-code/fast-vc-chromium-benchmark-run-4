8a2e51eee132405b864a1e7c2b921fb8f95e1d4c
Mike Wittman <wittman@chromium.org>
2025-05-16 09:24:24 -0700

    Use a plain, hardcoded function to build response parsers
    Since the available response parsers are a fixed set within any
    Chrome release and don't need to be configured at runtime, just use
    a plain factory function with a switch statement over the parser
    kind enum to build them.
    
    This allows dependency injection of custom parser types
    in OnDeviceModelFeatureAdapter via callback to facilitate
    testing. That's not possible with the singleton registry and will
    be used in follow-on CLs.
    
    Bug: 383761415
    Change-Id: I2052b40e4878d48fb9b79961cd8f852542481bec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6554624
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461412}

--- components/optimization_guide/core/model_execution/on_device_model_feature_adapter.cc
+++ components/optimization_guide/core/model_execution/on_device_model_feature_adapter.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_execution/on_device_model_execution_proto_value_utils.h"
 #include "components/optimization_guide/core/model_execution/redactor.h"
 #include "components/optimization_guide/core/model_execution/response_parser.h"
-#include "components/optimization_guide/core/model_execution/response_parser_registry.h"
+#include "components/optimization_guide/core/model_execution/response_parser_factory.h"
 #include "components/optimization_guide/core/model_execution/simple_response_parser.h"
 #include "components/optimization_guide/core/optimization_guide_constants.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
@@ -27,11 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace optimization_guide {
 
 OnDeviceModelFeatureAdapter::OnDeviceModelFeatureAdapter(
-    proto::OnDeviceModelExecutionFeatureConfig config)
+    proto::OnDeviceModelExecutionFeatureConfig config,
+    ResponseParserFactory response_parser_factory)
     : config_(std::move(config)),
       redactor_(Redactor::FromProto(config_.output_config().redact_rules())),
-      parser_(
-          ResponseParserRegistry::Get().CreateParser(config_.output_config())) {
+      parser_(response_parser_factory
+                  ? response_parser_factory.Run(config_.output_config())
+                  : CreateResponseParser(config_.output_config())) {
   // Set limits values in `token_limits_`.
   auto& input_config = config_.input_config();
   auto& output_config = config_.output_config();
--- components/optimization_guide/core/model_execution/on_device_model_feature_adapter.h
+++ components/optimization_guide/core/model_execution/on_device_model_feature_adapter.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
+#include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -30,15 +31,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace optimization_guide {
 
 class Redactor;
+class ResponseParser;
 
 // Adapts the on-device model to be used for a particular feature, based on
 // a configuration proto.
 class OnDeviceModelFeatureAdapter final
     : public base::RefCounted<OnDeviceModelFeatureAdapter> {
  public:
+  using ResponseParserFactory =
+      base::RepeatingCallback<std::unique_ptr<ResponseParser>(
+          const proto::OnDeviceModelExecutionOutputConfig&)>;
+
   // Constructs an adapter from a configuration proto.
   explicit OnDeviceModelFeatureAdapter(
-      proto::OnDeviceModelExecutionFeatureConfig config);
+      proto::OnDeviceModelExecutionFeatureConfig config,
+      // Allows dependency injection for use in tests.
+      ResponseParserFactory response_parser_factory = ResponseParserFactory());
 
   // Constructs the model input from `request`.
   std::optional<SubstitutionResult> ConstructInputString(
