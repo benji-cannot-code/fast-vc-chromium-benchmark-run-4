12abf7dfc58efd4c6dc37518f96879bc7438e472
Daniel Cheng <dcheng@chromium.org>
2025-05-16 21:13:13 -0700

    Revert "Add IPH to PWA Page Action"
    This reverts commit bbd3220966aeb1f31a405d70b9756e874fc0c76e.
    
    Reason for revert:
    PwaInstallViewBrowserTestSuite/PwaInstallViewBrowserTest.PwaIntallIphSiteEngagement/1
    is failing on https://ci.chromium.org/ui/b/8714668102874113153
    
    Bug: 376283433
    Original change's description:
    > Add IPH to PWA Page Action
    >
    > This CL migrates the IPH logic [1] to the new framework.
    >
    > It also makes the necessary changes to fix and re-enable the IPH test
    > (see crbug.com/40796769). The bug says the test was flaky but, in my
    > experience, it was failing every time. It now passes every test, so the
    > problem seems to be fixed. If we see flakiness, I'll send a follow-up CL
    > to disable the test.
    >
    > As discussed offline, the OnIPHClosed/Shown methods are not tested, but
    > they will be simplified in a follow-up CL. Currently their main goal is
    > to record "ignore" metrics, but those are already recorded by the user
    > education system.
    >
    > [1]
    > https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/page_action/pwa_install_view.cc;l=150-166;drc=a48632411d7e7263e8fd4d273d24a80f668b73ec
    >
    > Bug: 376283433
    > Fixed: 40796769
    > Change-Id: I2f5abc99adb6cba42e34ffdb6e2ec1992d7f7dd1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6469512
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: André Bianchessi <abianchessi@chromium.org>
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1461672}
    
    Bug: 376283433
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I4b44ef17bdc6546e1f0e38a7fb41cfd6f028febb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6558400
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1461740}

--- chrome/browser/ui/web_applications/pwa_install_page_action_browsertest.cc
+++ chrome/browser/ui/web_applications/pwa_install_page_action_browsertest.cc
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/feature_engagement/public/feature_list.h"
-#include "components/feature_engagement/test/scoped_iph_feature_list.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
 #include "components/omnibox/browser/omnibox_view.h"
 #include "components/site_engagement/content/site_engagement_service.h"
@@ -174,7 +173,7 @@ class PwaInstallViewBrowserTest : public extensions::ExtensionBrowserTest,
            {{features::kPageActionsMigrationPwaInstall.name, "true"}}});
     }
 
-    features_.InitAndEnableFeaturesWithParameters(enabled_features, {});
+    features_.InitWithFeaturesAndParameters(enabled_features, {});
   }
 
   PwaInstallViewBrowserTest(const PwaInstallViewBrowserTest&) = delete;
@@ -429,7 +428,7 @@ class PwaInstallViewBrowserTest : public extensions::ExtensionBrowserTest,
 
  private:
   web_app::OsIntegrationManager::ScopedSuppressForTesting os_hooks_suppress_;
-  feature_engagement::test::ScopedIphFeatureList features_;
+  base::test::ScopedFeatureList features_;
 };
 
 INSTANTIATE_TEST_SUITE_P(PwaInstallViewBrowserTestSuite,
@@ -821,7 +820,9 @@ IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest,
       "Manifest listing related chrome app"));
 }
 
-IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest, PwaIntallIphSiteEngagement) {
+// TODO(crbug.com/40796769): Flaky.
+IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest,
+                       DISABLED_PwaIntallIphSiteEngagement) {
   GURL app_url = GetInstallableAppURL();
   bool installable = OpenTab(app_url).installable;
   ASSERT_TRUE(installable);
@@ -834,10 +835,8 @@ IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest, PwaIntallIphSiteEngagement) {
   site_engagement::SiteEngagementService::Get(profile())->AddPointsForTesting(
       app_url, web_app::kIphFieldTrialParamDefaultSiteEngagementThreshold + 1);
   OpenTab(app_url);
-  EXPECT_TRUE(browser()->window()->IsFeaturePromoQueued(
-                  feature_engagement::kIPHDesktopPwaInstallFeature) ||
-              browser()->window()->IsFeaturePromoActive(
-                  feature_engagement::kIPHDesktopPwaInstallFeature));
+  EXPECT_TRUE(browser()->window()->IsFeaturePromoActive(
+      feature_engagement::kIPHDesktopPwaInstallFeature));
 }
 
 IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest, PwaIntallIphIgnored) {
