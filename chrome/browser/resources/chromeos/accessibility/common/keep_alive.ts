2a5335e1ee292ef5c84100067e45e0e8bd808bed
Alexander Surkov <asurkov@igalia.com>
2025-05-16 18:47:26 -0700

    [Accessibility Common] Fix AccessibilityExtensionKeepAliveTest tests in MV3
    Dynamic imports are disallowed in MV3. TestImportManager is used
    instead. accessibility/common tests run in an environment with
    the autoclick extension enabled, which is part of
    the accessibility_common extension. Adding required utility imports
    into autoclick script allows to workaround the test failures.
    
    Bug: 388867838
    Change-Id: Id6358c41fef3d45f88b6b81ca9631fa7161f2575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550296
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1461716}

--- chrome/browser/resources/chromeos/accessibility/common/keep_alive.ts
+++ chrome/browser/resources/chromeos/accessibility/common/keep_alive.ts
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * chrome.runtime.getPlatformInfo() every 20 seconds.
  */
 
+import {TestImportManager} from './testing/test_import_manager.js';
+
 export class KeepAlive {
   static instance?: KeepAlive;
   private interval?: number;
@@ -38,3 +40,5 @@ export class KeepAlive {
     this.interval = setInterval(() => this.runHeartbeat(), 20 * 1000);
   }
 }
+
+TestImportManager.exportForTesting(KeepAlive);
