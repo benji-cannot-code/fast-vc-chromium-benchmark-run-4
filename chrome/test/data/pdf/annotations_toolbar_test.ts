ac898c10cc1ebbe99ee1e77075332076e9194b6f
Lei Zhang <thestig@chromium.org>
2025-05-19 12:57:07 -0700

    PDF: Separate Ink1 and Ink2 code
    In viewer_toolbar.ts, it is unclear if `pdfAnnotationsEnabled_` is for
    Ink1 or Ink2. Change its usage so it is only used for Ink1. Then wrap it
    in <if> as needed and rename it to `pdfInk1AnnotationsEnabled_`. On the
    C++ side, similarly change AddAdditionalData() to set the
    "pdfInk1AnnotationsEnabled" key only on Chrome OS, and add similarly
    structured logic for Ink2, based on the feature flag.
    
    Change-Id: I34fa7c8e73922d8ef0e872a53762279fbda4fa3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6556745
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462391}

--- chrome/test/data/pdf/annotations_toolbar_test.ts
+++ chrome/test/data/pdf/annotations_toolbar_test.ts
@@ -72,7 +72,10 @@ const tests = [
   async function testRotateOrTwoUpViewTriggersDialog() {
     const toolbar = createToolbar();
     toolbar.annotationAvailable = true;
-    toolbar.strings = {'pdfAnnotationsEnabled': true, 'printingEnabled': true};
+    toolbar.strings = {
+      'pdfInk1AnnotationsEnabled': true,
+      'printingEnabled': true,
+    };
     toolbar.rotated = false;
     toolbar.twoUpViewEnabled = false;
 
