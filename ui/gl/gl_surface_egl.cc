a009d758b735a1fe4db988d481172daae6b0dfab
Sahir Vellani <sahir.vellani@microsoft.com>
2025-05-19 11:00:10 -0700

    Revert "[Win] Set the gl surface handle to a Child Window when not using DComp"
    This reverts commit 02222b9ee2a18895a750f4b3a353dc0a3b4ef318.
    
    Reason for revert: Breaks enterprise screenshot prevention
    functionality. SetWindowDisplayAffinity incorrectly returns an OOM error
    when the top level window contains a layered child. This ends up
    breaking screenshot prevention controls set up by Chromium.
    Unfortunately, there is no workaround until the bug can be fixed in
    Windows. Therefore, prevent layered windows from being the rendering
    surface altogether.
    
    Bug: 400454999 Original change's description:
    > [Win] Set the gl surface handle to a Child Window when not using DComp
    >
    > If the Redirection bitmap is removed from the main window, ANGLE can't
    > render content when not using DComp. The reason being that ANGLE
    > EGLSurface swap chains use a bitBlit approach. The solution to this
    > problem is to supply a child hwnd with a Redirection bitmap.
    >
    > Bug: 400454999
    > Change-Id: Ib113d37696cef0355ba45c200d94e5c26e72874d
    > Skip-Clang-Tidy-Checks: modernize-use-nullptr
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426631
    > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1449042}
    
    Bug: 400454999
    Change-Id: I3f36e0b2cc534af7bb791638b23bdb854a4272f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6558308
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462324}

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -368,15 +368,6 @@ NativeViewGLSurfaceEGL::NativeViewGLSurfaceEGL(
   if (GetClientRect(window_, &windowRect)) {
     size_ = gfx::Rect(windowRect).size();
   }
-  // If the main window Redirection Bitmap is removed, there is no way for ANGLE
-  // swap chains to bitBlt content to the main window. Therefore, send a layered
-  // child window handle so that the swap chain can present in Flip mode.
-  if (((GetWindowLong(window_, GWL_EXSTYLE) & WS_EX_NOREDIRECTIONBITMAP) ==
-       WS_EX_NOREDIRECTIONBITMAP)) {
-    child_window_.Initialize(/*remove_redirection_bitmap=*/false);
-    window_ = child_window_.window();
-    child_window_.Resize(size_);
-  }
 #endif
 }
 #endif  // BUILDFLAG(IS_ANDROID)
@@ -760,10 +751,6 @@ bool NativeViewGLSurfaceEGL::Resize(const gfx::Size& size,
                                     bool has_alpha) {
   if (size == GetSize())
     return true;
-#if BUILDFLAG(IS_WIN)
-  // If `child_window_` has not been initialized, Resize() will simply fail.
-  child_window_.Resize(size);
-#endif  // BUILDFLAG(IS_WIN)
   size_ = size;
   GLContext* context = GLContext::GetCurrent();
   DCHECK(context);
@@ -837,10 +824,6 @@ EGLTimestampClient* NativeViewGLSurfaceEGL::GetEGLTimestampClient() {
 
 bool NativeViewGLSurfaceEGL::IsEGLTimestampSupported() const {
   return use_egl_timestamps_;
-}
-
-EGLNativeWindowType NativeViewGLSurfaceEGL::GetNativeWindow() const {
-  return window_;
 }
 
 bool NativeViewGLSurfaceEGL::GetFrameTimestampInfoIfAvailable(
