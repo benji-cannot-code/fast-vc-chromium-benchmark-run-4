aa58c46bf4144764abae239f28d4b3982f8d0251
Jacob Stanley <jacobstanley@google.com>
2025-05-19 10:21:16 -0700

    [Notice Framework] Add Notice EEA dialog
    Bug: 417700269
    Change-Id: I6d698979cc8b962cedc085174289a53071866881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553484
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462273}

---
+++ chrome/browser/resources/privacy_sandbox/protected_audience_measurement.html.ts
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ProtectedAudienceMeasurement} from './protected_audience_measurement.js';
+
+export function getHtml(this: ProtectedAudienceMeasurement) {
+  return html`
+    <div>Protected Audience Measurement Notice Placeholder</div>
+    <cr-button id="ackButton" @click="${this.onAck}">
+      Ack Placeholder
+    </cr-button>
+  `;
+}
---
+++ chrome/browser/resources/privacy_sandbox/protected_audience_measurement.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {BaseDialogMixin} from './base_dialog_mixin.js';
+import {PrivacySandboxNotice} from './notice.mojom-webui.js';
+import {getHtml} from './protected_audience_measurement.html.js';
+
+const ProtectedAudienceMeasurementBase = BaseDialogMixin(
+    CrLitElement, PrivacySandboxNotice.kProtectedAudienceMeasurementNotice);
+
+export class ProtectedAudienceMeasurement extends
+    ProtectedAudienceMeasurementBase {
+  static get is() {
+    return 'protected-audience-measurement';
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'protected-audience-measurement': ProtectedAudienceMeasurement;
+  }
+}
+
+customElements.define(
+    ProtectedAudienceMeasurement.is, ProtectedAudienceMeasurement);
