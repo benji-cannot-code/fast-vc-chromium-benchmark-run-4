adf68258f201bd2be2ef3cf8e24dc83e2d099670
Sky Malice <skym@chromium.org>
2025-05-19 10:46:49 -0700

    Revert "Clean up stale base::Feature LensOnQuickActionSearchWidget"
    This reverts commit 99641dfe03ee74030986144d144ddd3c2ac900c6.
    
    Reason for revert: Broke clank compile. See https://ci.chromium.org/ui/p/chrome/builders/ci/clang-release-builder-x86/45105/overview
    
    Original change's description:
    > Clean up stale base::Feature LensOnQuickActionSearchWidget
    >
    > Fixed: 356235211
    > Change-Id: Ifc0e4e62477e43f2a2c09b568619928f02756a60
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564604
    > Reviewed-by: Juan Mojica <juanmojica@google.com>
    > Commit-Queue: Nan Lin <linnan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1462249}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ic7ba9d79146d6a454f1a0357d01f2adc79cf9963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564058
    Auto-Submit: Sky Malice <skym@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Sky Malice <skym@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1462314}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1502,6 +1502,9 @@ extern const char kLensSearchSidePanelNewFeedbackDescription[];
 extern const char kLinkedServicesSettingName[];
 extern const char kLinkedServicesSettingDescription[];
 
+extern const char kLensOnQuickActionSearchWidgetName[];
+extern const char kLensOnQuickActionSearchWidgetDescription[];
+
 extern const char kLocationBarModelOptimizationsName[];
 extern const char kLocationBarModelOptimizationsDescription[];
 
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -307,6 +307,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kIncognitoScreenshot,
     &kInstanceSwitcherV2,
     &kKeyboardEscBackNavigation,
+    &kLensOnQuickActionSearchWidget,
     &kMagicStackAndroid,
     &kMayLaunchUrlUsesSeparateStoragePartition,
     &kMiniOriginBar,
@@ -863,6 +864,10 @@ BASE_FEATURE(kContextMenuSysUiMatchesActivity,
 BASE_FEATURE(kContextMenuTranslateWithGoogleLens,
              "ContextMenuTranslateWithGoogleLens",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kLensOnQuickActionSearchWidget,
+             "LensOnQuickActionSearchWidget",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kContextualSearchDisableOnlineDetection,
              "ContextualSearchDisableOnlineDetection",
