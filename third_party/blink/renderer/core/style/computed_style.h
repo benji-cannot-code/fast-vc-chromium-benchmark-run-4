23a880607e637578473dbc72fc6ea8f741c6fec6
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2025-05-19 16:05:50 -0700

    [gap-decorations] Rule color is transparent fix for repeater values
    This CL fixes an issue where we will crash when recomputing
    the column-rule-color style when it is set to a repeater value.
    
    In order to fix this, we consider the rule transparent on the case
    of repeater values only if every individual value is transparent.
    
    Bug: 357648037
    Change-Id: I7072e7e4035af7ddad3acd26a01406d50219d410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6536229
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Sam Davis Omekara <samomekarajr@microsoft.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1462485}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -1004,10 +1004,7 @@ class ComputedStyle final : public ComputedStyleBase {
            !HasAutoColumnHeight();
   }
   bool ColumnRuleIsTransparent() const {
-    return ColumnRuleColor()
-        .GetLegacyValue()
-        .Resolve(GetCurrentColor(), UsedColorScheme())
-        .IsFullyTransparent();
+    return GapRuleColorIsTransparent(ColumnRuleColor());
   }
   bool ColumnRuleEquivalent(const ComputedStyle& other_style) const;
   bool HasColumnRule() const {
@@ -1020,10 +1017,7 @@ class ComputedStyle final : public ComputedStyleBase {
   }
 
   bool RowRuleIsTransparent() const {
-    return RowRuleColor()
-        .GetLegacyValue()
-        .Resolve(GetCurrentColor(), UsedColorScheme())
-        .IsFullyTransparent();
+    return GapRuleColorIsTransparent(RowRuleColor());
   }
   bool HasRowRule() const {
     // `SpecifiesColumns()` signifies we are in a multicol context. Return false
@@ -2772,6 +2766,9 @@ class ComputedStyle final : public ComputedStyleBase {
 
   // Derived flags:
   bool CalculateIsStackingContextWithoutContainment() const;
+
+  bool GapRuleColorIsTransparent(
+      const GapDataList<StyleColor>& gap_rule_color) const;
 
   FRIEND_TEST_ALL_PREFIXES(ComputedStyleTest, CustomPropertiesEqual_Values);
   FRIEND_TEST_ALL_PREFIXES(ComputedStyleTest, CustomPropertiesEqual_Data);
