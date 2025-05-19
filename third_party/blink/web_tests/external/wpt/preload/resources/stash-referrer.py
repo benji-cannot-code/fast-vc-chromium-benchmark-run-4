07307a18c9dbf9f241e02b873bfb6af09085fb55
Takashi Nakayama <tnak@chromium.org>
2025-05-19 02:34:42 -0700

    Add WPT to check referrerpolicy in Link headers on subresources
    This CL adds WPT to ensure that referrerpolicy is set to `no-referrer`
    for `Link` headers on subresources, as a follow-up for a past change,
    https://crrev.com/c/6522070.
    
    Bug: 417529151
    Change-Id: I472594b7aa49f6f662f84c7b1af8f36c0538f567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542774
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462025}

---
+++ third_party/blink/web_tests/external/wpt/preload/resources/stash-referrer.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from wptserve.utils import isomorphic_decode
+
+def main(request, response):
+    response.headers.set(b'Access-Control-Allow-Origin', b'*')
+
+    # We assume this is a string representing a UUID
+    key = request.GET.first(b'key')
+    operation = request.GET.first(b'operation')
+
+    if operation == b'put':
+        referer = request.headers.get(b'referer') or 'NO-REFERER'
+        request.server.stash.put(key, referer)
+        return "Added value to stash"
+    elif operation == b'take':
+        value = request.server.stash.take(key)
+        return value or ''
+    else:
+        assert False
