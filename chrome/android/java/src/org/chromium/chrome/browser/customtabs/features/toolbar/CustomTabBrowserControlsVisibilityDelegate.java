fa10fffaaff00f52644b2fff11736eaf26af7d22
Zhe Li <zheliooo@google.com>
2025-05-19 00:27:05 -0700

    [NullAway] Add @NullMarked to chrome/browser/customtabs/features/toolbar
    Bug: 389129271
    Change-Id: I4091ffa1c94116972b776c875918ea0fd232d76e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6557745
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461967}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabBrowserControlsVisibilityDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabBrowserControlsVisibilityDelegate.java
@@ -5,13 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.customtabs.features.toolbar;
 
+import static org.chromium.build.NullUtil.assumeNonNull;
+
 import org.chromium.base.supplier.Supplier;
+import org.chromium.build.annotations.NullMarked;
 import org.chromium.cc.input.BrowserControlsState;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsVisibilityManager;
 import org.chromium.chrome.browser.browser_controls.BrowserStateBrowserControlsVisibilityDelegate;
 import org.chromium.components.browser_ui.util.BrowserControlsVisibilityDelegate;
 
 /** Implementation of {@link BrowserControlsVisibilityDelegate} for custom tabs. */
+@NullMarked
 public class CustomTabBrowserControlsVisibilityDelegate extends BrowserControlsVisibilityDelegate {
     private final Supplier<BrowserControlsVisibilityManager> mBrowserControlsVisibilityManager;
     private @BrowserControlsState int mBrowserControlsState = BrowserControlsState.BOTH;
@@ -35,7 +39,8 @@ public class CustomTabBrowserControlsVisibilityDelegate extends BrowserControlsV
     }
 
     private @BrowserControlsState int calculateVisibilityConstraints() {
-        @BrowserControlsState int defaultConstraints = getDefaultVisibilityDelegate().get();
+        @BrowserControlsState
+        int defaultConstraints = assumeNonNull(getDefaultVisibilityDelegate().get());
         if (defaultConstraints == BrowserControlsState.HIDDEN
                 || mBrowserControlsState == BrowserControlsState.HIDDEN) {
             return BrowserControlsState.HIDDEN;
