3d45a460a5d2c0fe2f9e62fbf055941aab62e2c7
Fiorella Barrientos Villalta <fiorellab@google.com>
2025-05-19 05:06:19 -0700

    Record latency of Model Execution Server call for each step.
    Change-Id: Ie411b97cf544e285f552b3a49ceac1f48cb3e558
    Bug: 407503334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6555869
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Fiorella Barrientos Villalta <fiorellab@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462075}

--- chrome/browser/password_manager/password_change/model_quality_logs_uploader.cc
+++ chrome/browser/password_manager/password_change/model_quality_logs_uploader.cc
@@ -19,6 +19,10 @@ using PasswordChangeOutcome = optimization_guide::proto ::
 using PageType = optimization_guide::proto::OpenFormResponseData_PageType;
 
 namespace {
+int64_t ComputeRequestLatencyMs(base::Time server_request_start_time) {
+  return (base::Time::Now() - server_request_start_time).InMilliseconds();
+}
+
 FinalModelStatus GetFinalModelStatus(
     const std::optional<optimization_guide::proto::PasswordChangeResponse>&
         response) {
@@ -68,7 +72,8 @@ ModelQualityLogsUploader::~ModelQualityLogsUploader() = default;
 
 void ModelQualityLogsUploader::SetOpenFormQuality(
     const optimization_guide::proto::PasswordChangeResponse& response,
-    std::unique_ptr<LoggingData> logging_data) {
+    std::unique_ptr<LoggingData> logging_data,
+    base::Time server_request_start_time) {
   PageType open_form = response.open_form_data().page_type();
   QualityStatus quality_status;
 
@@ -94,12 +99,19 @@ void ModelQualityLogsUploader::SetOpenFormQuality(
       ->mutable_quality()
       ->mutable_open_form()
       ->set_status(quality_status);
+  // Set latency
+  request.mutable_password_change_submission()
+      ->mutable_quality()
+      ->mutable_open_form()
+      ->set_request_latency_ms(
+          ComputeRequestLatencyMs(server_request_start_time));
   final_log_data_.MergeFrom(request);
 }
 
 void ModelQualityLogsUploader::SetSubmitFormQuality(
     const optimization_guide::proto::PasswordChangeResponse& response,
-    std::unique_ptr<LoggingData> logging_data) {
+    std::unique_ptr<LoggingData> logging_data,
+    base::Time server_request_start_time) {
   QualityStatus quality_status;
   if (response.submit_form_data().dom_node_id_to_click()) {
     quality_status = QualityStatus::
@@ -115,13 +127,20 @@ void ModelQualityLogsUploader::SetSubmitFormQuality(
       ->mutable_quality()
       ->mutable_submit_form()
       ->set_status(quality_status);
+  // Set latency
+  request.mutable_password_change_submission()
+      ->mutable_quality()
+      ->mutable_submit_form()
+      ->set_request_latency_ms(
+          ComputeRequestLatencyMs(server_request_start_time));
   final_log_data_.MergeFrom(request);
 }
 
 void ModelQualityLogsUploader::SetVerifySubmissionQuality(
     const std::optional<optimization_guide::proto::PasswordChangeResponse>&
         response,
-    std::unique_ptr<LoggingData> logging_data) {
+    std::unique_ptr<LoggingData> logging_data,
+    base::Time server_request_start_time) {
   FinalModelStatus final_model_status = GetFinalModelStatus(response);
   QualityStatus quality_status = GetVerifySubmissionQualityStatus(response);
   optimization_guide::proto::LogAiDataRequest request;
@@ -134,6 +153,12 @@ void ModelQualityLogsUploader::SetVerifySubmissionQuality(
       ->mutable_quality()
       ->mutable_verify_submission()
       ->set_status(quality_status);
+  // Set latency
+  request.mutable_password_change_submission()
+      ->mutable_quality()
+      ->mutable_verify_submission()
+      ->set_request_latency_ms(
+          ComputeRequestLatencyMs(server_request_start_time));
   final_log_data_.MergeFrom(request);
 }
 
--- chrome/browser/password_manager/password_change/password_change_submission_verifier.h
+++ chrome/browser/password_manager/password_change/password_change_submission_verifier.h
@@ -91,6 +91,8 @@ class PasswordChangeSubmissionVerifier {
   optimization_guide::proto::PasswordChangeRequest
       check_submission_successful_request_;
 
+  base::Time server_request_start_time_;
+
   base::WeakPtrFactory<PasswordChangeSubmissionVerifier> weak_ptr_factory_{
       this};
 };
