6eb49a36fe69251c198f560981e5eff2152c83ee
Nohemi Fernandez <fernandex@google.com>
2025-05-19 05:08:30 -0700

    [iOS] Add debug info to symbol lookup failure.
    Current debug message does not provide sufficient information to
    understand the issue in lookup failures for system symbols.
    
    https://ci.chromium.org/ui/p/chromium/builders/try/ios-simulator-full-configs/157839/overview
    
    Bug: N/A
    Change-Id: Ifd1d59248bec36d8b37c783dd65e9d5b88545138
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563484
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462077}

--- ios/chrome/browser/shared/ui/symbols/symbol_helpers.mm
+++ ios/chrome/browser/shared/ui/symbols/symbol_helpers.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbol_helpers.h"
 
 #import "base/check.h"
+#import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbol_configurations.h"
 
@@ -35,7 +36,8 @@ UIImage* SymbolWithConfiguration(NSString* symbol_name,
                         inBundle:nil
                withConfiguration:configuration];
   }
-  DCHECK(symbol);
+  DCHECK(symbol) << " symbol_name: " << base::SysNSStringToUTF8(symbol_name)
+                 << " is_system_symbol: " << system_symbol;
   return symbol;
 }
 
