28f81e98b227185442abd1aa7dc69cc883654a41
Grace Cham <hscham@google.com>
2025-05-19 11:31:49 -0700

    Re-enable ContextMenuTest.test{Share,Print}Page tests
    Set setSupportsPrecisionPointerForTesting to true so that empty space
    context menu feature is enabled. This is always true on physical desktop
    devices (touch pad) but is false on the desktop emulator.
    
    Although it seems not affecting automated tests pass/failure, disable
    occlusion which causes content menu flashing in manual tests.
    
    Bug: 415101606
    Test: run android-desktop-15-x64-fyi-rel and the two tests pass
    Include-Ci-Only-Tests: chromium.android.desktop.fyi:android-desktop-15-x64-fyi-rel|chrome_public_test_apk
    Change-Id: Ia1c1cb3a5f7303519c8735c8caffde6093a3a990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6554346
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462344}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -103,6 +103,8 @@ import org.chromium.printing.PrintingController;
 import org.chromium.printing.PrintingControllerImpl;
 import org.chromium.ui.base.Clipboard;
 import org.chromium.ui.base.DeviceFormFactor;
+import org.chromium.ui.base.DeviceInput;
+import org.chromium.ui.base.UiAndroidFeatures;
 import org.chromium.ui.mojom.MenuSourceType;
 import org.chromium.url.GURL;
 
@@ -1230,7 +1232,9 @@ public class ContextMenuTest {
     @SmallTest
     @Restriction(DeviceFormFactor.DESKTOP)
     @EnableFeatures({ChromeFeatureList.CONTEXT_MENU_EMPTY_SPACE})
+    @DisableFeatures({UiAndroidFeatures.ANDROID_WINDOW_OCCLUSION})
     public void testSharePage() throws Exception {
+        DeviceInput.setSupportsPrecisionPointerForTesting(true);
         Tab tab = sDownloadTestRule.getActivity().getActivityTab();
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -1275,7 +1279,9 @@ public class ContextMenuTest {
     @MediumTest
     @Restriction(DeviceFormFactor.DESKTOP)
     @EnableFeatures({ChromeFeatureList.CONTEXT_MENU_EMPTY_SPACE})
+    @DisableFeatures({UiAndroidFeatures.ANDROID_WINDOW_OCCLUSION})
     public void testPrintPage() throws Exception {
+        DeviceInput.setSupportsPrecisionPointerForTesting(true);
         Tab tab = sDownloadTestRule.getActivity().getActivityTab();
         ThreadUtils.runOnUiThreadBlocking(
                 // Set printing controller to use the mock instance.
