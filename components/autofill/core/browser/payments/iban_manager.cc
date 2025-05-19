41e646d87b290ef967f6c7186d3ecfe1d268ef4a
Karol Sygiet <sygiet@google.com>
2025-05-19 01:05:58 -0700

    Move the IbanSuggestionGenerator to the "payments" directory.
    Bug: 409962888
    Change-Id: I8e42b12e2806f5067ed20f026d395710a45e6522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553341
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461993}

--- components/autofill/core/browser/payments/iban_manager.cc
+++ components/autofill/core/browser/payments/iban_manager.cc
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/foundations/browser_autofill_manager.h"
 #include "components/autofill/core/browser/integrators/optimization_guide/autofill_optimization_guide.h"
 #include "components/autofill/core/browser/metrics/payments/iban_metrics.h"
+#include "components/autofill/core/browser/suggestions/payments/iban_suggestion_generator.h"
 #include "components/autofill/core/browser/suggestions/payments/payments_suggestion_generator.h"
-#include "components/autofill/core/browser/suggestions/single_fields/iban_suggestion_generator.h"
 #include "components/autofill/core/common/autofill_clock.h"
 
 namespace autofill {
