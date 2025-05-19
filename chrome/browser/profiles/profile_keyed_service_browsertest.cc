1d0ee45ca5258e480ec6ff53ad6bb7ac9283cd80
Jan Keitel <jkeitel@google.com>
2025-05-19 03:39:49 -0700

    Only create AutofillAI-related KeyedServices when they are needed
    This CL stops the creation of AutofillAI KeyedServices in guest mode
    and for ash internals, since they are not needed for either.
    
    Additionally, it now redirects the OTR version of the KeyedServices
    to the original profile for regular profiles and stops OTR support
    entire for AutofillAiModelExecutor. Effectively, this does not change
    the current product behavior:
    - The AutofillAiModelCache and the AutofillAiModelExecutor are not
      used (yet).
    - The EntityDataManager is used, but essentially just acts as a cache
      to make accessing the data store in the underlying WebDatabase
      synchronously possible. Prior to this CL, there existed different
      different instantiations of the EntityDataManager, but both used the
      same underlying database - as such, they had access to the same data.
      Changing the behavior to redirect to the original profile just saves
      one copy of the cache.
    
    Bug: 418691350
    Change-Id: I2fc0a2d9c72bf5ff7382e6a8658ae1bcd86af95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563464
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462043}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/profile_waiter.h"
-#include "components/autofill/core/common/autofill_features.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/enterprise/buildflags/buildflags.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -195,7 +194,6 @@ class ProfileKeyedServiceBrowserTest : public InProcessBrowserTest {
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
           features::kEnableCertManagementUIV2Write,
 #endif
-          autofill::features::kAutofillAiWithDataSchema,
           network::features::kBrowsingTopics,
           blink::features::kBuiltInAIAPI,
           extensions_features::kForceWebRequestProxyForTest,
@@ -365,7 +363,6 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
   std::set<std::string> guest_otr_active_services {
     "AlarmManager",
     "AXMainNodeAnnotatorController",
-    "AutofillEntityDataManager",
     "AutocompleteActionPredictor",
     "AutocompleteClassifier",
     "AutocompleteControllerEmitter",
@@ -572,7 +569,6 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
     "AutocompleteScoringModelService",
 #endif  // BUILDFLAG(BUILD_WITH_TFLITE_LIB)
     "AutofillClientProvider",
-    "AutofillEntityDataManager",
     "AutofillImageFetcher",
     "AutofillPrivateEventRouter",
     "AutofillStrikeDatabase",
