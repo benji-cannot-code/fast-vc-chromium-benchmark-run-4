519f2c8f828b767c9119ac804211c68f33485e53
Amelie Schneider <amelies@google.com>
2025-05-19 08:15:35 -0700

    [B4B] Reland "Make local other node default save location without account nodes"
    This relands commit 12d87b4ef73af5a1fecca996da67c2f89fab4d2a.
    
    The original CL changed the order in which folders are returned in
    `GetMostRecentlyModifiedUserFolders()`.
    
    It was reverted in builds without the field trial config because the
    flag `SyncEnableBookmarksInTransportMode` was not explicitly enabled in
    a test. This adds the flag to the class of the test which was failing
    (`BookmarkBrowsertest`).
    
    Test failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/100529/overview
    Revert CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6563609
    
    Bug: 416215585
    Change-Id: I93c2b799f9d78bf99218056ba17a63b7c19094b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563290
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462163}

--- components/bookmarks/browser/bookmark_model_unittest.cc
+++ components/bookmarks/browser/bookmark_model_unittest.cc
@@ -1563,9 +1563,9 @@ TEST_F(BookmarkModelTest, MoveWithUuidCollisionInDescendant) {
 // Tests the default node if no bookmarks have been added yet
 TEST_F(BookmarkModelTest, ParentForNewNodesWithEmptyModel) {
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-  ASSERT_EQ(model_->mobile_node(), GetParentForNewNodes(model_.get()));
+  EXPECT_EQ(model_->mobile_node(), GetParentForNewNodes(model_.get()));
 #else
-  ASSERT_EQ(model_->bookmark_bar_node(), GetParentForNewNodes(model_.get()));
+  EXPECT_EQ(model_->other_node(), GetParentForNewNodes(model_.get()));
 #endif
 }
 
@@ -1577,7 +1577,7 @@ TEST_F(BookmarkModelTest, ParentCanBeBookmarkBarOnAndroid) {
   const GURL kUrl("http://foo.com");
 
   model_->AddURL(model_->bookmark_bar_node(), 0, kTitle, kUrl);
-  ASSERT_EQ(model_->bookmark_bar_node(), GetParentForNewNodes(model_.get()));
+  EXPECT_EQ(model_->bookmark_bar_node(), GetParentForNewNodes(model_.get()));
 }
 #endif
 
@@ -1587,7 +1587,7 @@ TEST_F(BookmarkModelTest, ParentForNewNodes) {
   const GURL kUrl("http://foo.com");
 
   model_->AddURL(model_->other_node(), 0, kTitle, kUrl);
-  ASSERT_EQ(model_->other_node(), GetParentForNewNodes(model_.get()));
+  EXPECT_EQ(model_->other_node(), GetParentForNewNodes(model_.get()));
 }
 
 // Tests that adding a URL to a folder updates the last modified time.
@@ -1596,7 +1596,7 @@ TEST_F(BookmarkModelTest, ParentForNewMobileNodes) {
   const GURL kUrl("http://foo.com");
 
   model_->AddURL(model_->mobile_node(), 0, kTitle, kUrl);
-  ASSERT_EQ(model_->mobile_node(), GetParentForNewNodes(model_.get()));
+  EXPECT_EQ(model_->mobile_node(), GetParentForNewNodes(model_.get()));
 }
 
 // Make sure recently modified stays in sync when adding a URL.
