e29c03984832da4367026ae9f0cbd44c14ea5b3a
Nwokedi Idika <nwokedi@chromium.org>
2025-05-19 08:34:35 -0700

    [PhishGuard] Remove GetUserPopulationPref
    The method is unused.
    
    Fixed: 413354128
    Change-Id: Ie16cbe3380fc8c443d2e1e47c4ad3bf6c65f200b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6558129
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Commit-Queue: Nwokedi Idika <nwokedi@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462176}

--- ios/chrome/browser/safe_browsing/model/chrome_password_protection_service.h
+++ ios/chrome/browser/safe_browsing/model/chrome_password_protection_service.h
@@ -145,9 +145,6 @@ class ChromePasswordProtectionService
 
   AccountInfo GetAccountInfo() const override;
 
-  safe_browsing::ChromeUserPopulation::UserPopulation GetUserPopulationPref()
-      const override;
-
   AccountInfo GetAccountInfoForUsername(
       const std::string& username) const override;
 
