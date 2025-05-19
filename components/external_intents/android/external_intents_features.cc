ab1fd424e90c88997ff9cc04b3c4f2ce935cb9ee
Fabio Muratori <muratori@google.com>
2025-05-19 11:17:48 -0700

    [Clank Nav Cap] Auxiliary navigations from browser are not captured
    This CL implements a new navigation capturing scenario for desktop
    windowing mode Clank on Android. Every auxiliary browsing context
    navigation that starts in the browser environment should never lead to
    a new app opening. Instead, it should always open a new browser tab.
    
    This is the behaviour used when no apps are found within the scope of the
    clicked link. This CL takes care of expanding this logic to any auxiliary
    navigation case, regardless of available apps.
    
    Implementation:
    
    To achieve this, a condition has been added to
    InterceptNavigationDelegateClientImpl.shouldIgnoreNavigation, the entry
    point of the navigation capturing process. We prevent any capturing logic
    from being executed by preemptively terminating the process and allowing
    a navigation to continue if the necessary conditions are verified.
    
    Tests:
    
    Test auxiliary navigation in UrlOverridingTest. Navigation should never
    send an Intetn and a new tab should always be created in the browser tab.
    Enabled feature flag at class level and fixed minor bugs in affected
    tests.
    
    Bug: 383138896
    Change-Id: I8b9c8f70ca16df95e67dd00806cccfee3a276529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519515
    Commit-Queue: Fabio Muratori <muratori@google.com>
    Reviewed-by: Olga Korokhina <korokhina@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462335}

--- components/external_intents/android/external_intents_features.cc
+++ components/external_intents/android/external_intents_features.cc
@@ -27,7 +27,7 @@ namespace {
 // Array of features exposed through the Java ExternalIntentsFeatures API.
 const base::Feature* const kFeaturesExposedToJava[] = {
     &kExternalNavigationDebugLogs, &kBlockIntentsToSelf,
-    &kNavigationCaptureRefactorAndroid};
+    &kNavigationCaptureRefactorAndroid, &kAuxiliaryNavigationStaysInBrowser};
 
 }  // namespace
 
@@ -43,6 +43,10 @@ BASE_FEATURE(kBlockIntentsToSelf,
 
 BASE_FEATURE(kNavigationCaptureRefactorAndroid,
              "NavigationCaptureRefactorAndroid",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAuxiliaryNavigationStaysInBrowser,
+             "AuxiliaryNavigationStaysInBrowser",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 static jlong JNI_ExternalIntentsFeatures_GetFeature(JNIEnv* env, jint ordinal) {
