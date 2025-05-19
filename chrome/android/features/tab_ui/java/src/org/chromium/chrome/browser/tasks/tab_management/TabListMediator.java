81233b61af1a1bdb61707b9414e402ddfabb6223
Linyu He <linyuh@google.com>
2025-05-19 16:06:44 -0700

    Consolidate tab closure "allowUndo" logic into TabClosureParamsUtils
    Bug: 375468032
    Change-Id: I00815df978e7b3407231ff0b5e045e9bde2a09d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6557910
    Auto-Submit: Linyu He <linyuh@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462486}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -118,7 +118,6 @@ import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
-import org.chromium.ui.util.MotionEventUtils;
 import org.chromium.ui.util.XrUtils;
 import org.chromium.url.GURL;
 
@@ -1200,8 +1199,7 @@ class TabListMediator implements TabListNotificationHandler {
                         Tab currentTab = TabModelUtils.getCurrentTab(tabModel);
                         Tab nextTab = currentTab == closingTab ? getNextTab(tabId) : null;
                         boolean allowUndo =
-                                triggeringMotionEvent == null
-                                        || !MotionEventUtils.isMouseEvent(triggeringMotionEvent);
+                                TabClosureParamsUtils.shouldAllowUndo(triggeringMotionEvent);
                         TabClosureParams closureParams =
                                 TabClosureParams.closeTab(closingTab)
                                         .recommendedNextTab(nextTab)
