595145fea64801e8a5bca9a9319ed878060fcb39
Alison Gale <agale@chromium.org>
2025-05-19 14:48:59 -0700

    Remove uses of GetInctiveTabWidth from unit test
    This variable can be misleading so it is better to be explicit about
    which tab width you are getting.
    
    Bug: 418256399
    Change-Id: I552fa9aafa54c6274654c8e472b5bc5fb3c36843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6558748
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462451}

--- chrome/browser/ui/views/tabs/tab_container_unittest.cc
+++ chrome/browser/ui/views/tabs/tab_container_unittest.cc
@@ -371,6 +371,12 @@ class TabContainerTest : public ChromeViewsTestBase {
     tab_container_->SetSize(size);
   }
 
+  int GetWidthOfActiveTab() {
+    return tab_container_
+        ->GetTabAtModelIndex(tab_strip_controller_->GetActiveIndex().value())
+        ->width();
+  }
+
   std::unique_ptr<FakeBaseTabStripController> tab_strip_controller_;
   std::unique_ptr<FakeTabContainerController> tab_container_controller_;
   std::unique_ptr<FakeTabSlotController> tab_slot_controller_;
@@ -391,7 +397,7 @@ TEST_F(TabContainerTest, ExitsClosingModeAtStandardWidth) {
   // Create just enough tabs so tabs are not full size.
   const int standard_width =
       TabStyle::Get()->GetStandardWidth(/*is_split*/ false);
-  while (tab_container_->GetActiveTabWidth() == standard_width) {
+  while (tab_container_->GetTabAtModelIndex(0)->width() == standard_width) {
     AddTab(0);
     tab_container_->CompleteAnimationAndLayout();
   }
@@ -407,13 +413,13 @@ TEST_F(TabContainerTest, ExitsClosingModeAtStandardWidth) {
   // constraining tab widths to below full size.
   RemoveTab(tab_container_->GetTabCount() - 2);
   tab_container_->CompleteAnimationAndLayout();
-  ASSERT_LT(tab_container_->GetActiveTabWidth(), standard_width);
+  ASSERT_LT(GetWidthOfActiveTab(), standard_width);
 
   // Close the last tab; tab closing mode should allow tabs to resize to full
   // size.
   RemoveTab(tab_container_->GetTabCount() - 1);
   tab_container_->CompleteAnimationAndLayout();
-  EXPECT_EQ(tab_container_->GetActiveTabWidth(), standard_width);
+  EXPECT_EQ(GetWidthOfActiveTab(), standard_width);
 }
 
 TEST_F(TabContainerTest, StaysInClosingModeBelowStandardWidth) {
@@ -422,7 +428,7 @@ TEST_F(TabContainerTest, StaysInClosingModeBelowStandardWidth) {
   // Create just enough tabs so tabs are not full size.
   const int standard_width =
       TabStyle::Get()->GetStandardWidth(/*is_split*/ false);
-  while (tab_container_->GetActiveTabWidth() == standard_width) {
+  while (tab_container_->GetTabAtModelIndex(0)->width() == standard_width) {
     AddTab(0);
     tab_container_->CompleteAnimationAndLayout();
   }
@@ -442,13 +448,13 @@ TEST_F(TabContainerTest, StaysInClosingModeBelowStandardWidth) {
   // constraining tab widths to below full size.
   RemoveTab(tab_container_->GetTabCount() - 2);
   tab_container_->CompleteAnimationAndLayout();
-  ASSERT_LT(tab_container_->GetActiveTabWidth(), standard_width);
+  ASSERT_LT(GetWidthOfActiveTab(), standard_width);
 
   // Close the last tab; tab closing mode should remain active, as there isn't
   // enough room for tabs to be standard width.
   RemoveTab(tab_container_->GetTabCount() - 1);
   tab_container_->CompleteAnimationAndLayout();
-  EXPECT_LT(tab_container_->GetActiveTabWidth(), standard_width);
+  EXPECT_LT(GetWidthOfActiveTab(), standard_width);
 }
 
 TEST_F(TabContainerTest, ClosingModeAffectsMinWidth) {
@@ -457,7 +463,7 @@ TEST_F(TabContainerTest, ClosingModeAffectsMinWidth) {
   // Create just enough tabs so tabs are not full size.
   const int standard_width =
       TabStyle::Get()->GetStandardWidth(/*is_split*/ false);
-  while (tab_container_->GetActiveTabWidth() == standard_width) {
+  while (tab_container_->GetTabAtModelIndex(0)->width() == standard_width) {
     AddTab(0);
     tab_container_->CompleteAnimationAndLayout();
   }
@@ -492,7 +498,7 @@ TEST_F(TabContainerTest, RemoveTabInGroupWithTabClosingMode) {
   // Set a tab_counter to avoid infinite loop
   int tab_counter = 0;
   while ((tab_counter < 100) &&
-         (tab_container_->GetActiveTabWidth() == standard_width ||
+         (tab_container_->GetTabAtModelIndex(0)->width() == standard_width ||
           tab_container_->GetTabCount() < 10)) {
     AddTab(0);
     tab_container_->CompleteAnimationAndLayout();
