5c8657403d16fb56ad2a7b69c499e7d3e5d19249
Aman Verma <amanvr@google.com>
2025-05-19 09:44:57 -0700

    [InputVizard] Remove the crash keys for RenderInputRouter.
    This CL removes the crash keys added for investigating the UAF bug in
    RenderInputRouter. This issue has been fixed by
    https://crrev.com/c/6530325.
    
    Bug: 413442819
    Change-Id: Ibf1bfef46a39ed814184445fc6211120ab445681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563404
    Reviewed-by: Kartar Singh <kartarsingh@google.com>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462212}

--- components/input/render_input_router.cc
+++ components/input/render_input_router.cc
@@ -9,13 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/command_line.h"
-#include "base/debug/stack_trace.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/tracing/protos/chrome_track_event.pbzero.h"
 #include "cc/input/browser_controls_offset_tag_modifications.h"
-#include "components/crash/core/common/crash_key.h"
 #include "components/input/input_constants.h"
 #include "components/input/input_router_config_helper.h"
 #include "components/input/input_router_impl.h"
@@ -124,10 +122,6 @@ base::LazyInstance<UnboundWidgetInputHandler>::Leaky g_unbound_input_handler =
 
 RenderInputRouter::~RenderInputRouter() {
   TRACE_EVENT("input", "RenderInputRouter::~RenderInputRouter");
-  // TODO(413442819): Remove these crash keys after the UAF bug has been fixed.
-  static crash_reporter::CrashKeyString<1024> trace_key("crbug413442819");
-  crash_reporter::SetCrashKeyStringToStackTrace(&trace_key,
-                                                base::debug::StackTrace());
 }
 
 RenderInputRouter::RenderInputRouter(
