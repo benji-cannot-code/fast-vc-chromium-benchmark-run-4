f068e94c7874416e4ccc1630beebdd992284c3e6
Tomasz Jurkiewicz <tju@google.com>
2025-05-19 01:50:20 -0700

    Remove test only url filter setter, use a testing factory instead
    Changes signature of the supervised user service so that the url filter
    is injected. The filter was never changed during the lifetime of the
    service.
    
    Minor:
    * add supervised user service factory for test helper,
    * remove redundant friend declarations.
    
    Bug: 416249869
    Change-Id: I37bb41443c434e699517fcad15651967698abcb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6562381
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462008}

--- components/supervised_user/core/browser/supervised_user_service_unittest.cc
+++ components/supervised_user/core/browser/supervised_user_service_unittest.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
-#include "supervised_user_metrics_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -81,7 +80,8 @@ class SupervisedUserServiceTestBase : public ::testing::Test {
         identity_test_env_.identity_manager(),
         test_url_loader_factory_.GetSafeWeakWrapper(), syncable_pref_service_,
         settings_service_, &sync_service_,
-        std::make_unique<FakeURLFilterDelegate>(),
+        std::make_unique<SupervisedUserURLFilter>(
+            syncable_pref_service_, std::make_unique<FakeURLFilterDelegate>()),
         std::make_unique<FakePlatformDelegate>());
     service_->Init();
 
