2afab0d4cc77ed09579a1830cfbe3c79178e00f2
Takuto Ikuta <tikuta@chromium.org>
2025-05-18 23:52:44 -0700

    chrome: Add missing includes
    This is to fix build error with Clang modules build.
    
    Bug: 40263312
    Change-Id: I6b81f5d2e40aeed989cb48ce9c280b9919506bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6560922
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461958}

--- chrome/browser/vr/ui_renderer.h
+++ chrome/browser/vr/ui_renderer.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_VR_UI_RENDERER_H_
 #define CHROME_BROWSER_VR_UI_RENDERER_H_
 
+#include <vector>
+
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/vr/vr_ui_export.h"
 
