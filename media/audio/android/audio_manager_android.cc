8c7953ad9765342741a943f785455f715e586d0c
Diego Valenzuela <dvalenzuela@google.com>
2025-05-19 10:10:01 -0700

    Allow chromium to obtain the maximum number of channels for Android Auto
    The AudioManager provides methods to query audio devices and their
    properties. A key method is
    getDevices(AudioManager.GET_DEVICES_OUTPUTS). This returns an array of
    AudioDeviceInfo objects representing the available output devices. Each
    AudioDeviceInfo object has a method getChannelMask(), which returns an
    array of integers representing the supported masks for that specific
    device (e.g., [CHANNEL_OUT_STEREO, CHANNEL_OUT_5POINT1,
    CHANNEL_OUT_7POINT1).
    
    The maximum channel count for a device would be the highest value from
    the channel layout config channels.
    
    Note: This change applies automotive logic to grab the max number; given
    the filter by AudioDeviceInfo.TYPE_BUS, which was checked with the AAOS
    team; and its needed to support any media file with a config change that
    increases the initial number of channels will be affected by this
    change.
    
    Change-Id: I9d67c7f8c154ac7f87f282a13b0195266516a279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6485826
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Diego Valenzuela <dvalenzuela@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462254}

--- media/audio/android/audio_manager_android.cc
+++ media/audio/android/audio_manager_android.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/fake_audio_input_stream.h"
 #include "media/base/audio_parameters.h"
 #include "media/base/channel_layout.h"
+#include "media/base/media_switches.h"
 #include "media/media_buildflags.h"
 
 #if BUILDFLAG(USE_OPENSLES)
@@ -624,6 +625,14 @@ AudioParameters AudioManagerAndroid::GetPreferredOutputStreamParameters(
     }
   }
 
+  if (base::FeatureList::IsEnabled(kUseAudioManagerMaxChannelLayout)) {
+    // Since channel count never changes over the lifetime of an output stream,
+    // use the max number of channels supported. This can prevent down-sampling
+    // and loss of channel information (e.g. if a stream starts as stereo and
+    // changes to 5.1)
+    channel_layout_config = GetLayoutWithMaxChannels(channel_layout_config);
+  }
+
   int user_buffer_size = GetUserBufferSize();
   if (user_buffer_size) {
     buffer_size = user_buffer_size;
@@ -634,7 +643,6 @@ AudioParameters AudioManagerAndroid::GetPreferredOutputStreamParameters(
     return GetAudioFormatsSupportedBySinkDevice(
         output_device_id, channel_layout_config, sample_rate, buffer_size);
   }
-
   return AudioParameters(AudioParameters::AUDIO_PCM_LOW_LATENCY,
                          channel_layout_config, sample_rate, buffer_size);
 }
@@ -753,6 +761,22 @@ void AudioManagerAndroid::DoSetVolumeOnAudioThread(double volume) {
   for (auto stream : streams_) {
     stream->SetVolume(volume);
   }
+}
+
+ChannelLayoutConfig AudioManagerAndroid::GetLayoutWithMaxChannels(
+    ChannelLayoutConfig layout_configuration) {
+  JNIEnv* env = AttachCurrentThread();
+  int value = Java_AudioManagerAndroid_getLayoutWithMaxChannels(
+      env, GetJavaAudioManager());
+  CHECK_GT(value, 0);
+  CHECK_LE(value, CHANNEL_LAYOUT_MAX);
+  ChannelLayout channel_layout = static_cast<ChannelLayout>(value);
+  int number_channels = ChannelLayoutToChannelCount(channel_layout);
+  if (number_channels > layout_configuration.channels()) {
+    return ChannelLayoutConfig(channel_layout, number_channels);
+  }
+
+  return layout_configuration;
 }
 
 }  // namespace media
