579e7ac0a09f0504fc0614c3ae46b9c793e4a8d0
James Cook <jamescook@chromium.org>
2025-05-19 11:47:11 -0700

    extensions: Remove guest view JS if ENABLE_GUEST_VIEW is not set
    Follow up from code review:
    https://chromium-review.googlesource.com/c/chromium/src/+/6553482
    
    Bug: 415701970
    Change-Id: I1f4cc5c2b8c82c2fef6800403fb75bf67ea84305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551442
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462355}

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -520,7 +520,9 @@ void Dispatcher::DidCreateScriptContext(
   }
 #endif
 
+#if BUILDFLAG(ENABLE_GUEST_VIEW)
   RequireGuestViewModules(context);
+#endif
 
   const base::TimeDelta elapsed = base::TimeTicks::Now() - start_time;
   switch (context->context_type()) {
@@ -723,9 +725,11 @@ void Dispatcher::WillEvaluateServiceWorkerOnWorkerThread(
 
   worker_bindings_system->DidCreateScriptContext(context);
 
+#if BUILDFLAG(ENABLE_GUEST_VIEW)
   // TODO(lazyboy): Get rid of RequireGuestViewModules() as this doesn't seem
   // necessary for Extension SW.
   RequireGuestViewModules(context);
+#endif
 
   WorkerThreadDispatcher::GetServiceWorkerData()->Init();
   g_worker_script_context_set.Get().Insert(base::WrapUnique(context));
@@ -1525,6 +1529,7 @@ bool Dispatcher::IsWithinPlatformApp() {
   return false;
 }
 
+#if BUILDFLAG(ENABLE_GUEST_VIEW)
 void Dispatcher::RequireGuestViewModules(ScriptContext* context) {
   ModuleSystem* module_system = context->module_system();
   bool requires_guest_view_module = false;
@@ -1560,13 +1565,11 @@ void Dispatcher::RequireGuestViewModules(ScriptContext* context) {
   }
 #endif
 
-#if BUILDFLAG(ENABLE_GUEST_VIEW)
   // Require ExtensionOptions.
   if (context->GetAvailability("extensionOptionsInternal").is_available()) {
     requires_guest_view_module = true;
     module_system->Require("extensionOptionsElement");
   }
-#endif
 
   // Require WebView.
   if (context->GetAvailability("webViewInternal").is_available()) {
@@ -1590,6 +1593,7 @@ void Dispatcher::RequireGuestViewModules(ScriptContext* context) {
         ->SetForceMainWorldInitialization(true);
   }
 }
+#endif  // BUILDFLAG(ENABLE_GUEST_VIEW)
 
 std::unique_ptr<NativeExtensionBindingsSystem> Dispatcher::CreateBindingsSystem(
     NativeExtensionBindingsSystem::Delegate* delegate,
