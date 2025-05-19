eebc62fd3352f1193466fec5db4f9df1334a3390
Nidhi Jaju <nidhijaju@chromium.org>
2025-05-18 18:10:55 -0700

    Add a LRU cache for relative URLs resolved in Document
    This CL adds a LRU cache for relative URLs resolved in Document to
    reduce URL parsing and copies. Document::CompleteURL is updated with the
    memoization, and the cache is cleared when the base URL is updated to a
    different URL, as the resolved URLs will become invalid.
    
    Bug: 348442535
    Change-Id: I8716df87dc27c6685da73bebf56ab6e2893c39b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534472
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1461907}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1996,6 +1996,16 @@ BASE_FEATURE(kOpenAllUrlsOrFilesOnDrop,
              "OpenAllUrlsOrFilesOnDrop",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kOptimizeHTMLElementUrls,
+             "OptimizeHTMLElementUrls",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE_PARAM(size_t,
+                   kDocumentURLCacheSize,
+                   &kOptimizeHTMLElementUrls,
+                   "cache_size",
+                   100);
+
 BASE_FEATURE(kOriginAgentClusterDefaultEnabled,
              "OriginAgentClusterDefaultEnable",
              base::FEATURE_ENABLED_BY_DEFAULT);
