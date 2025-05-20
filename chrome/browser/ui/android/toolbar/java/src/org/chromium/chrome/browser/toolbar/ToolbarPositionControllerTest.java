97b327a661b7c713caf11747d03d042d03964b2f
Patrick Noland <pnoland@google.com>
2025-05-20 14:46:14 -0700

    [Anchor] Call setLayoutParams when changing progress bar params
    Changing them "out of band" risks CoordinatorLayout failing to resolve
    the anchor before the next layout pass, tripping an exception and
    crashing. Calling setLayoutParams should call requestLayout and prompt
    CoordinatorLayout to resolve the anchor view before the next layout
    pass.
    
    Bug: 417940201
    Change-Id: Ie28a0c53ff88f78d89944c9917754bc742e78619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569751
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463059}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionControllerTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionControllerTest.java
@@ -18,6 +18,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.FrameLayout.LayoutParams;
 
@@ -235,6 +236,7 @@ public class ToolbarPositionControllerTest {
     @Mock private ControlContainer mControlContainer;
     @Mock private View mControlContainerView;
     @Mock private View mProgressBarContainer;
+    @Mock private ViewGroup mProgressBarParent;
 
     private Context mContext;
     private final ObservableSupplierImpl<Boolean> mIsNtpShowing =
@@ -284,6 +286,7 @@ public class ToolbarPositionControllerTest {
         doReturn(mControlContainerView).when(mControlContainer).getView();
         doReturn(CONTROL_CONTAINER_ID).when(mControlContainerView).getId();
         doReturn(mProgressBarLayoutParams).when(mProgressBarContainer).getLayoutParams();
+        doReturn(mProgressBarParent).when(mProgressBarContainer).getParent();
         mContext = ContextUtils.getApplicationContext();
         doReturn(mContext.getResources()).when(mProgressBarContainer).getResources();
         mWindowAndroid = new WindowAndroid(mContext, false);
@@ -405,9 +408,11 @@ public class ToolbarPositionControllerTest {
     @EnableFeatures(ChromeFeatureList.ANDROID_BOTTOM_TOOLBAR + ":default_to_top/false")
     public void testDefaultBottom() {
         assertControlsAtBottom();
+        verify(mProgressBarContainer).setLayoutParams(mProgressBarLayoutParams);
 
         setUserToolbarAnchorPreference(/* showToolbarOnTop= */ true);
         assertControlsAtTop();
+        verify(mProgressBarContainer, times(2)).setLayoutParams(mProgressBarLayoutParams);
     }
 
     @Test
