951330dd3a1109d459f0e87f6c279108af3e66a4
Oleh Lamzin <lamzin@google.com>
2025-05-20 05:26:22 -0700

    Settings: Add missing icons import for do not track toggle
    do_not_track_toggle.html uses the following icon(s):
      - settings:forward
    
    do_not_track_toggle.ts does not import corresponding
    .html.js file(s):
      - chrome/browser/resources/settings/icons.html for
        - settings:* icon(s)
    
    Add missing icon import(s).
    
    Also sort existing imports.
    
    Bug: 365922159
    Change-Id: I601b1a984b0f7e9404b2d4fef0a0d0971dcff14d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565504
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Auto-Submit: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462717}

--- chrome/browser/resources/settings/privacy_page/do_not_track_toggle.ts
+++ chrome/browser/resources/settings/privacy_page/do_not_track_toggle.ts
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
-import '../settings_shared.css.js';
 import '../controls/settings_toggle_button.js';
+import '../icons.html.js';
+import '../settings_shared.css.js';
 
 import {focusWithoutInk} from 'chrome://resources/js/focus_without_ink.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
