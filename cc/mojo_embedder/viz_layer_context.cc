d2568e11fec712e6aa83cc26572bc624aeee1478
Vikas Soni <vikassoni@chromium.org>
2025-05-19 23:57:24 -0700

    [TreesInViz] Move viz::mojom::MissingTileReason to cc component.
    Move MissingTileReason enum from tiling.mojom in viz to its separate
    component missing_tile_reason.mojom in cc. This is because including any
    viz component in cc currently triggers UBSAN bots causing test failures
    and for TreesInViz development we require including viz component in cc.
    
    Moving the enum to separate component in cc will avoid triggering UBSAN
    bots for now. Once the UBSAN related issues are fixed, we can move the
    enum back to viz if needed. Also note that moving enum to its own
    separate component in cc is also required to avoid any circular
    dependencies between viz and cc.
    
    Bug: 417965009, 418120288
    Change-Id: Ia5059aa0581952d99e0f8cd88f138f5e08e27f16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565869
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462592}

--- cc/mojo_embedder/viz_layer_context.cc
+++ cc/mojo_embedder/viz_layer_context.cc
@@ -478,7 +478,7 @@ viz::mojom::TilePtr SerializeTile(
   switch (tile.draw_info().mode()) {
     case TileDrawInfo::OOM_MODE:
       wire->contents = viz::mojom::TileContents::NewMissingReason(
-          viz::mojom::MissingTileReason::kOutOfMemory);
+          mojom::MissingTileReason::kOutOfMemory);
       break;
 
     case TileDrawInfo::SOLID_COLOR_MODE:
@@ -493,7 +493,7 @@ viz::mojom::TilePtr SerializeTile(
             SerializeTileResource(tile, resource_provider, context_provider));
       } else {
         wire->contents = viz::mojom::TileContents::NewMissingReason(
-            viz::mojom::MissingTileReason::kResourceNotReady);
+            mojom::MissingTileReason::kResourceNotReady);
       }
       break;
   }
