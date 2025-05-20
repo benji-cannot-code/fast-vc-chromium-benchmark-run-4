a8f10279f6ff17c24909f51f26f5b08bec237467
Googler <noreply@google.com>
2025-05-20 02:36:16 -0700

    Sync feature protos.
    This CL was generated by a Copybara workflow. See the attached bug
    for more details.
    
    BbcpOrigin-RevId: 760062728
    Change-Id: Ie37c5eb2cfa9a42b2c1418139c58bd23624501ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549695
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Copybara Service <copybara-worker-blackhole@google.com>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462660}

--- components/autofill/core/browser/ml_model/autofill_ai/autofill_ai_model_executor_impl_unittest.cc
+++ components/autofill/core/browser/ml_model/autofill_ai/autofill_ai_model_executor_impl_unittest.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_proto_util.h"
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "components/optimization_guide/proto/features/forms_classifications.pb.h"
-#include "components/optimization_guide/proto/features/forms_predictions.pb.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
