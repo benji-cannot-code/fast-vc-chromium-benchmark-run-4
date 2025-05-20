ef98f99f533d5a6b5eeda5471815e07f3a01a671
Noémie St-Onge <noemies@google.com>
2025-05-20 12:22:23 -0700

    [iOS] Deflake testEditAddressFromOverflowMenu
    Fixed: 417957580
    Change-Id: Ic9b6c89bb92d25f003471ac525ca5dad93a571cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568571
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Auto-Submit: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462963}

--- ios/chrome/browser/autofill/ui_bundled/manual_fill/address_view_controller_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/manual_fill/address_view_controller_egtest.mm
@@ -608,6 +608,8 @@ void OpenAddressManualFillViewWithNoSavedAddresses() {
       performAction:grey_tap()];
 
   // Tap the "Done" button to dismiss the view.
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:NavigationBarDoneButton()];
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
       performAction:grey_tap()];
 
