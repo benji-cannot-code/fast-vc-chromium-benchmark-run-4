89eb71e36b92c7465ae9fa4c132a3a2fcfb2bb72
Colin Blundell <blundell@chromium.org>
2025-05-20 00:09:52 -0700

    [Blink] Make CanvasResourceHost::ClearLayerTexture() virtual
    This method is a no-op if `cc_layer_` is null. `cc_layer_` is non-null
    only for HTMLCanvasElement. However, we cannot easily move the method
    into HTMLCanvasElement in the immediate due to its callsites in
    BaseRenderingContext2D and CanvasHibernationHandler. This CL instead
    makes the method virtual with a default empty implementation and has
    HTMLCanvasElement override it to clear `cc_layer`'s texture.
    
    Bug: 399587138
    Change-Id: Iee11a117cb8400c01424458285c5657e36476530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563082
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462600}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.h
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.h
@@ -190,6 +190,8 @@ class CORE_EXPORT HTMLCanvasElement final
 
   cc::TextureLayer* GetOrCreateCcLayerIfNeeded();
   cc::TextureLayer* GetCcLayerForTesting() { return cc_layer_.get(); }
+  void ClearLayerTexture() override;
+
   Canvas2DLayerBridge* GetOrCreateCanvas2DLayerBridge();
 
   void SetNeedsPushProperties();
