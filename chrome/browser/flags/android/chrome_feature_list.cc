5c6bb1aee4a43e6bf1414cd27c961c9dd319d6f1
Jack Thiesen <jthiesen@chromium.org>
2025-05-20 09:10:35 -0700

    Feature flag for ShowHomeButton policy on Android.
    Bug: 400804628
    Change-Id: I86f05483711e95518a26c2129439ac315655996f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565890
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462841}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -334,6 +334,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kPwaRestoreUiAtStartup,
     &kOmahaMinSdkVersionAndroid,
     &kShortCircuitUnfocusAnimation,
+    &kShowHomeButtonPolicyAndroid,
     &kShowNewTabAnimations,
     &kPartnerCustomizationsUma,
     &kQuickDeleteAndroidSurvey,
@@ -1100,6 +1101,10 @@ BASE_FEATURE(kOmahaMinSdkVersionAndroid,
 
 BASE_FEATURE(kShortCircuitUnfocusAnimation,
              "ShortCircuitUnfocusAnimation",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kShowHomeButtonPolicyAndroid,
+             "ShowHomeButtonPolicyAndroid",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kShowNewTabAnimations,
