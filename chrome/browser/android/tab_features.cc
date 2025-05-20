8d62a375533ee468e326348735a019e64a6048ce
Kamil Jarosz <kjarosz@google.com>
2025-05-20 02:24:44 -0700

    [PS ACT Survey] Implement the survey for Android
    The ACT Survey is shown on the NTP in Incognito after a configurable delay.
    
    Bug: 397440908
    Change-Id: I9589931cf59c8c4537c08a6d348bb4804ce7a7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563690
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462652}

--- chrome/browser/android/tab_features.cc
+++ chrome/browser/android/tab_features.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/tab_features.h"
 
+#include "chrome/browser/privacy_sandbox/incognito/privacy_sandbox_incognito_tab_observer.h"
 #include "chrome/browser/sync/sessions/sync_sessions_router_tab_helper.h"
 #include "chrome/browser/sync/sessions/sync_sessions_web_contents_router_factory.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
@@ -24,6 +25,10 @@ TabFeatures::TabFeatures(content::WebContents* web_contents, Profile* profile) {
 
   dwa_web_contents_observer_ =
       std::make_unique<metrics::DwaWebContentsObserver>(web_contents);
+
+  privacy_sandbox_incognito_tab_observer_ =
+      std::make_unique<privacy_sandbox::PrivacySandboxIncognitoTabObserver>(
+          web_contents);
 }
 
 TabFeatures::~TabFeatures() = default;
--- chrome/browser/ui/hats/survey_config.cc
+++ chrome/browser/ui/hats/survey_config.cc
@@ -234,7 +234,6 @@ std::vector<hats::SurveyConfig> GetAllSurveyConfigs() {
       /*log_responses_to_uma=*/true,
       /*log_responses_to_ukm=*/true);
 
-#if !BUILDFLAG(IS_ANDROID)
   // Privacy sandbox ACT survey
   survey_configs.emplace_back(  //
       &privacy_sandbox::kPrivacySandboxActSurvey,
@@ -249,6 +248,7 @@ std::vector<hats::SurveyConfig> GetAllSurveyConfigs() {
       /*requested_browser_type=*/
       hats::SurveyConfig::RequestedBrowserType::kIncognito);
 
+#if !BUILDFLAG(IS_ANDROID)
   // Dev tools surveys.
   survey_configs.emplace_back(&features::kHaTSDesktopDevToolsIssuesCOEP,
                               "devtools-issues-coep",
