31bdadcfd05c41fbb95f4c4d251d6ad79b18c0ed
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-05-20 13:32:25 -0700

    Only synchronize the rounded corners of a window tree when required
    When dragging an overview item,
    `ScopedOverviewTransformWindow::UpdateRoundedCorners()` is called for
    each drag event. For many of these calls, only the position of overview
    item is changed. For these instances, it is wasteful to synchronize the
    window tree.
    
    Bug: b:415049664
    Change-Id: Icb2d454db507ab9193a69da7ee39ad06d73020ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565524
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463023}

--- ash/wm/overview/scoped_overview_transform_window.h
+++ ash/wm/overview/scoped_overview_transform_window.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_observer.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
+#include "ui/gfx/geometry/rrect_f.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/transform.h"
 
@@ -224,7 +225,7 @@ class ASH_EXPORT ScopedOverviewTransformWindow
   gfx::Rect original_clip_rect_;
 
   // Removes clipping on `window_` during destruction in the case it was not
-  // removed in `RestoreWindw()`. See destructor for more information.
+  // removed in `RestoreWindow()`. See destructor for more information.
   bool reset_clip_on_shutdown_ = true;
 
   std::unique_ptr<ScopedOverviewHideWindows> hidden_transient_children_;
@@ -234,6 +235,8 @@ class ASH_EXPORT ScopedOverviewTransformWindow
 
   std::unique_ptr<WindowTreeSynchronizer> window_tree_synchronizer_;
   std::unique_ptr<WindowTreeSynchronizer> window_tree_synchronizer_during_drag_;
+
+  std::optional<gfx::RRectF> synchronized_bounds_at_origin_;
 
   base::WeakPtrFactory<ScopedOverviewTransformWindow> weak_ptr_factory_{this};
 };
