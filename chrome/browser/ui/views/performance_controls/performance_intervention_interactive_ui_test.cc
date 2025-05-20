1f0b73b362c6d0b3b6afc9cf98f266eb553b53d2
stluong <stluong@chromium.org>
2025-05-20 12:39:36 -0700

    [Fixit] Deflake PerformanceInterventionInteractiveTests
    The PerformanceInterventionTest.TakeSuggestedAction and
    RemoveSuggestedTabFromList were flaking because a tab will sometimes
    fail to discard due to the contents being still visible when discard
    occurs. This CL fixes the test flakiness by removing the flaky step
    of switching tabs before discarding in favor of discarding the first
    two background tabs instead.
    
    #top-chrome-bug-fixit
    
    Fixed: 416350381
    Change-Id: I49ea69e4356b5a66264d482406ac06737806fc0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6567568
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462977}

--- chrome/browser/ui/views/performance_controls/performance_intervention_interactive_ui_test.cc
+++ chrome/browser/ui/views/performance_controls/performance_intervention_interactive_ui_test.cc
@@ -443,17 +443,18 @@ IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
 IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
                        TakeSuggestedAction) {
   RunTestSequence(
+      InstrumentTab(kFirstTab), NavigateWebContents(kFirstTab, GetURL()),
       AddInstrumentedTab(kSecondTab, GetURL()),
-      AddInstrumentedTab(kThirdTab, GetURL()), SelectTab(kTabStripElementId, 0),
-      TriggerOnActionableTabListChange({1, 2}),
+      AddInstrumentedTab(kThirdTab, GetURL()), WaitForShow(kThirdTab),
+      TriggerOnActionableTabListChange({0, 1}),
       WaitForShow(kToolbarPerformanceInterventionButtonElementId),
       WaitForShow(PerformanceInterventionBubble::
                       kPerformanceInterventionDialogDeactivateButton),
       ObserveState(kTabDiscardedState, 2),
       PressButton(PerformanceInterventionBubble::
                       kPerformanceInterventionDialogDeactivateButton),
-      WaitForState(kTabDiscardedState, true), CheckTabDiscardStatus(0, false),
-      CheckTabDiscardStatus(1, true), CheckTabDiscardStatus(2, true));
+      WaitForState(kTabDiscardedState, true), CheckTabDiscardStatus(0, true),
+      CheckTabDiscardStatus(1, true), CheckTabDiscardStatus(2, false));
 }
 
 // The dialog should discard tabs suggested in the tab list
@@ -463,9 +464,10 @@ IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
   const char kSuggestedCloseButton[] = "SuggestedCloseButton";
 
   RunTestSequence(
+      InstrumentTab(kFirstTab), NavigateWebContents(kFirstTab, GetURL()),
       AddInstrumentedTab(kSecondTab, GetURL()),
-      AddInstrumentedTab(kThirdTab, GetURL()), SelectTab(kTabStripElementId, 0),
-      TriggerOnActionableTabListChange({1, 2}),
+      AddInstrumentedTab(kThirdTab, GetURL()), WaitForShow(kThirdTab),
+      TriggerOnActionableTabListChange({0, 1}),
       WaitForShow(kToolbarPerformanceInterventionButtonElementId),
       WaitForShow(
           PerformanceInterventionBubble::kPerformanceInterventionTabList),
@@ -490,7 +492,7 @@ IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
       PressButton(PerformanceInterventionBubble::
                       kPerformanceInterventionDialogDeactivateButton),
       WaitForState(kTabDiscardedState, true), CheckTabDiscardStatus(0, false),
-      CheckTabDiscardStatus(1, false), CheckTabDiscardStatus(2, true));
+      CheckTabDiscardStatus(1, true), CheckTabDiscardStatus(2, false));
 }
 
 // Intervention dialog should only show when the performance intervention
