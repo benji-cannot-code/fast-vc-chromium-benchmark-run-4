354964e0bfe456231e8ff7044694414065d004d6
Jennifer Serrano <jennserrano@google.com>
2025-05-20 12:09:18 -0700

    [ntp-footer] Update footer visibility based on user pref
    Only surfaces the footer on extension NTPs if "prefs::kNtpFooterVisible" is set to true. As `kNtpFooterVisible` is changed through the customize chrome side panel the footer visibility gets updated accordingly.
    
    Bug: 399184119
    
    Change-Id: I0bc1d8e37b51eb06d455ce7f7bee40b5a399137a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512356
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462951}

--- chrome/browser/ui/views/new_tab_footer/footer_controller.h
+++ chrome/browser/ui/views/new_tab_footer/footer_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_NEW_TAB_FOOTER_FOOTER_CONTROLLER_H_
 
 #include "chrome/browser/ui/views/new_tab_footer/footer_web_view.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/tabs/public/tab_interface.h"
 #include "content/public/browser/web_contents_observer.h"
 
@@ -34,6 +35,8 @@ class NewTabFooterController : public content::WebContentsObserver {
   const raw_ptr<tabs::TabInterface> tab_;
   raw_ptr<new_tab_footer::NewTabFooterWebView> footer_web_view_;
   base::CallbackListSubscription tab_did_activate_callback_subscription_;
+  PrefChangeRegistrar pref_change_registrar_;
+  raw_ptr<Profile> profile_;
 
   base::WeakPtrFactory<NewTabFooterController> weak_factory_{this};
 };
