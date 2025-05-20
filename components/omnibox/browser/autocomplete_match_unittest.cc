79d5e26d308c9f455e37f4c0f513d4579eb77063
Orin Jaworski <orinj@google.com>
2025-05-19 17:51:27 -0700

    Map match icons from SuggestTemplateInfo::IconType to gfx::VectorIcon
    Bug: 413104741
    Change-Id: I50b585a22e642fc62d976b56e877114f817d3e07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6566253
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462518}

--- components/omnibox/browser/autocomplete_match_unittest.cc
+++ components/omnibox/browser/autocomplete_match_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/metrics_proto/omnibox_event.pb.h"
 #include "third_party/metrics_proto/omnibox_scoring_signals.pb.h"
 #include "third_party/omnibox_proto/entity_info.pb.h"
+#include "third_party/omnibox_proto/suggest_template_info.pb.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "url/gurl.h"
 
@@ -1134,6 +1135,16 @@ TEST_F(AutocompleteMatchTest, ValidateGetVectorIcons) {
       // All other suggestion types should result in non-empty vector icons.
       EXPECT_FALSE(match.GetVectorIcon(/*is_bookmark=*/false).is_empty());
     }
+  }
+
+  // When the match has a SuggestTemplateInfo, its icon should be set.
+  // This checks the full range to ensure any new additions get mapped.
+  match.suggest_template = omnibox::SuggestTemplateInfo();
+  for (int i = omnibox::SuggestTemplateInfo::IconType_MIN;
+       i <= omnibox::SuggestTemplateInfo::IconType_MAX; i++) {
+    match.suggest_template->set_type_icon(
+        static_cast<omnibox::SuggestTemplateInfo::IconType>(i));
+    EXPECT_FALSE(match.GetVectorIcon(false).is_empty());
   }
 }
 #endif
