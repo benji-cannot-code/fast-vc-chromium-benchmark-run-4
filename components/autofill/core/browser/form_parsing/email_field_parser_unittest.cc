4fd1dc20e1603e00394a116f57846b654406caf2
Norge Vizcay <vizcay@google.com>
2025-05-20 01:55:42 -0700

    Add parsing for email or loyalty card number fields.
    The CL extends the email field parser to also capture "email or loyalty
    card" fields and parse them as EMAIL_OR_LOYALTY_MEMBERSHIP_ID.
    
    Bug: 416664590
    Change-Id: Ia11c4968639990f3deb88f94f1735bc22f10626f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563491
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462640}

--- components/autofill/core/browser/form_parsing/email_field_parser_unittest.cc
+++ components/autofill/core/browser/form_parsing/email_field_parser_unittest.cc
@@ -16,7 +16,14 @@ namespace autofill {
 class EmailFieldParserTest : public FormFieldParserTestBase,
                              public ::testing::Test {
  public:
-  EmailFieldParserTest() = default;
+  EmailFieldParserTest() {
+    feature_list_.InitWithFeatures(
+        /*enabled_features=*/{features::kAutofillParseEmailLabelAndPlaceholder,
+                              features::kAutofillEnableLoyaltyCardsFilling,
+                              features::
+                                  kAutofillEnableEmailOrLoyaltyCardsFilling},
+        /*disabled_features=*/{});
+  }
   EmailFieldParserTest(const EmailFieldParserTest&) = delete;
   EmailFieldParserTest& operator=(const EmailFieldParserTest&) = delete;
 
@@ -27,8 +34,7 @@ class EmailFieldParserTest : public FormFieldParserTestBase,
   }
 
  private:
-  base::test::ScopedFeatureList feature_list_{
-      features::kAutofillParseEmailLabelAndPlaceholder};
+  base::test::ScopedFeatureList feature_list_;
 };
 
 // Tests that a field whose label has the format of an email address is parsed
@@ -36,6 +42,15 @@ class EmailFieldParserTest : public FormFieldParserTestBase,
 TEST_F(EmailFieldParserTest, ParseEmailAddressLabel) {
   AddTextFormFieldData(/*name=*/"username", /*label=*/"some@foo.com",
                        EMAIL_ADDRESS);
+  ClassifyAndVerify(ParseResult::kParsed);
+}
+
+// Tests that a field that accepts email or loyalty card fields are parsed as
+// `EMAIL_OR_LOYALTY_MEMBERSHIP_ID`.
+TEST_F(EmailFieldParserTest, ParseEmailOrLoyaltyCardAddressLabel) {
+  AddTextFormFieldData(/*name=*/"email-or-loyalty-card",
+                       /*label=*/"email-or-loyalty-card",
+                       EMAIL_OR_LOYALTY_MEMBERSHIP_ID);
   ClassifyAndVerify(ParseResult::kParsed);
 }
 
