3c54d3b15d4da73e1b3ccefc5188276728ee070a
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2025-05-20 08:50:12 -0700

    Revert CL for the high confidence crash culprit for http://crash/80c4953ffb6a4050
    The Suspect CL https://chromium-review.googlesource.com/q/I79b2e48f1b7438a5c27abd92f160aec5bc031ab1 is highly likely the cause of the crash. Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Change-Id: Idb2ec122dea030624a8ca9903d11ce5c7cf45526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564124
    Reviewed-by: Justin Green <greenjustin@google.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Justin Green <greenjustin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462819}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -339,7 +339,6 @@ class MailboxVideoFrameConverter;
 class MojoVideoEncodeAccelerator;
 class PaintCanvasVideoRenderer;
 class V4L2DevicePoller;  // TODO(crbug.com/41486289): remove this.
-class V4L2VideoEncodeAccelerator;
 }  // namespace media
 namespace memory_instrumentation {
 class OSMetrics;
@@ -781,7 +780,6 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
             WorkerStatus StatusWork>
   friend class media::CodecWorkerImpl;
   friend class media::MojoVideoEncodeAccelerator;
-  friend class media::V4L2VideoEncodeAccelerator;
   friend class mojo::core::ScopedIPCSupport;
   friend class net::MultiThreadedCertVerifierScopedAllowBaseSyncPrimitives;
   friend class rlz_lib::FinancialPing;
