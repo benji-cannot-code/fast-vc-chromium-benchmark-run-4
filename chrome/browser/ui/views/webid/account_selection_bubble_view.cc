67b0a47a2e40388646785353831222fd06378221
Christian Biesinger <cbiesinger@chromium.org>
2025-05-20 12:46:46 -0700

    [FedCM] Fix close button positioning
    Fixes a regression from https://crrev.com/c/6544686
    
    This matches what SetLabelProperties does.
    
    Also don't set a subtitle for the verifying dialog, since the
    title does not include any eTLD+1s.
    
    Bug: 418116096, 390581529
    Change-Id: I8fb4f432bd917c38782abead94a56758949b2b09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569729
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462989}

--- chrome/browser/ui/views/webid/account_selection_bubble_view.cc
+++ chrome/browser/ui/views/webid/account_selection_bubble_view.cc
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/layout/flex_layout.h"
+#include "ui/views/layout/flex_layout_types.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 
@@ -229,7 +230,7 @@ void AccountSelectionBubbleView::ShowVerifyingSheet(
     const IdentityRequestAccountPtr& account,
     const std::u16string& title) {
   UpdateHeader(account->identity_provider->idp_metadata.brand_decoded_icon,
-               title, webid::GetSubtitle(rp_data_),
+               title, u"",
                /*show_back_button=*/false,
                /*should_circle_crop_header_icon=*/true);
 
@@ -499,6 +500,11 @@ std::unique_ptr<views::View> AccountSelectionBubbleView::CreateHeaderView() {
       header->AddChildView(std::make_unique<views::View>());
   titles_container->SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical));
+  views::FlexSpecification flex_spec(views::LayoutOrientation::kHorizontal,
+                                     views::MinimumFlexSizeRule::kScaleToZero,
+                                     views::MaximumFlexSizeRule::kUnbounded);
+  titles_container->SetProperty(views::kFlexBehaviorKey, flex_spec);
+
   // Add the title.
   title_label_ = titles_container->AddChildView(std::make_unique<views::Label>(
       title_, views::style::CONTEXT_DIALOG_BODY_TEXT,
