914636b03c4fcbdbc8f6f03097d22cb27de46e39
Jacob Stanley <jacobstanley@google.com>
2025-05-20 09:47:55 -0700

    [Notice Framework] Add Restricted Notice Dialog
    Bug: 417700269
    Change-Id: I5d18372a86e0635df288873aa3dbf1e0c42db070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6566254
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1462845}

--- chrome/browser/resources/privacy_sandbox/base_dialog_app.html.ts
+++ chrome/browser/resources/privacy_sandbox/base_dialog_app.html.ts
@@ -26,6 +26,10 @@ export function getHtml(this: BaseDialogApp) {
       this.getNoticeId(
           PrivacySandboxNotice.kThreeAdsApisNotice)}" slot="view" fill-content>
       </three-ads-apis-notice>
+      <measurement-notice id="${
+      this.getNoticeId(
+          PrivacySandboxNotice.kMeasurementNotice)}" slot="view" fill-content>
+      </measurement-notice>
     </cr-view-manager>
   `;
 }
