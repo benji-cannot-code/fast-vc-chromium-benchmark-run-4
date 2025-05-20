72cda3bfe424a2850b5cddbda9398be356902c77
Luchen Peng <luchenpeng@google.com>
2025-05-20 05:54:58 -0700

    [KeyboardAccessory] Remove `UiConfiguration` class in `AllPasswordsBottomSheetViewBinder`.
    `UiConfiguration` only contains FaviconHelper (maybe it had other fields
    before), so it doesn't seem necessary to have it as a separate class.
    After this change, `FaviconHelper` will be directly passed to
    `AllPasswordsBottomSheetViewBinder`.
    
    Change-Id: If6add74bd38971b876a726d012b04e3deee1eef4
    Bug: 418951360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564472
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Luchen Peng <luchenpeng@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462728}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetCoordinator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetCoordinator.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.browser.autofill.helpers.FaviconHelper;
-import org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet.AllPasswordsBottomSheetViewBinder.UiConfiguration;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.ui.modelutil.ListModel;
@@ -67,13 +66,11 @@ class AllPasswordsBottomSheetCoordinator {
         ListModel<ListItem> listModel = new ListModel<>();
         mMediator.initialize(delegate, model, listModel);
 
-        UiConfiguration uiConfiguration = new UiConfiguration();
-        uiConfiguration.faviconHelper = FaviconHelper.create(context, profile);
         setUpView(
                 model,
                 listModel,
                 new AllPasswordsBottomSheetView(context, sheetController),
-                uiConfiguration);
+                FaviconHelper.create(context, profile));
     }
 
     /**
@@ -92,7 +89,7 @@ class AllPasswordsBottomSheetCoordinator {
             PropertyModel model,
             ListModel<ListItem> listModel,
             AllPasswordsBottomSheetView view,
-            UiConfiguration uiConfiguration) {
+            FaviconHelper faviconHelper) {
         view.setSheetItemListAdapter(
                 new RecyclerViewAdapter<>(
                         new SimpleRecyclerViewMcp<>(
@@ -101,7 +98,7 @@ class AllPasswordsBottomSheetCoordinator {
                                 AllPasswordsBottomSheetViewBinder::connectPropertyModel),
                         (parent, itemType) ->
                                 AllPasswordsBottomSheetViewBinder.createViewHolder(
-                                        parent, itemType, uiConfiguration)));
+                                        parent, itemType, faviconHelper)));
         PropertyModelChangeProcessor.create(
                 model, view, AllPasswordsBottomSheetViewBinder::bindAllPasswordsBottomSheet);
     }
