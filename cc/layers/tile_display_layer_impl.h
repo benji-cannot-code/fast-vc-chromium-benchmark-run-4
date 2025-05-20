7c259a6e648d52ea048b97c86a1a6f40fa8e5f7b
Vikas Soni <vikassoni@chromium.org>
2025-05-19 23:57:37 -0700

    [TreesInViz] Add reason to represent a tile with no contents.
    This is reland of the original CL after some changes to avoid
    triggering UBSAN bots.
    
    Introduces a new Reason enum inside NoContents to track why a tile has
    no content. Also adds a corresponding kTileDeleted value in the
    MissingTileReason mojom enum to represent tiles that were explicitly
    deleted by the client.
    
    This enables the Viz compositor to distinguish between different
    NoContents cases, such as tiles that are missing due to deletion,
    out-of-memory, etc.
    
    This is a required to improving tile lifecycle handling in Viz as well
    as to allow for checkerboard tiles which were OOMed.
    
    Future CLs will add renderer-side and Viz-side logic to make use of
    this information.
    
    Bug: 417965009
    Change-Id: I425bb7725282f17c5e3e05f73166bc6640b305ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565105
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462593}

--- cc/layers/tile_display_layer_impl.h
+++ cc/layers/tile_display_layer_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/tiling_data.h"
 #include "cc/cc_export.h"
 #include "cc/layers/layer_impl.h"
+#include "cc/mojom/missing_tile_reason.mojom.h"
 #include "cc/tiles/tile_index.h"
 #include "cc/tiles/tile_priority.h"
 #include "cc/tiles/tiling_coverage_iterator.h"
@@ -32,7 +33,13 @@ namespace cc {
 // layer down to Viz, and this layer uses that information to draw tile quads.
 class CC_EXPORT TileDisplayLayerImpl : public LayerImpl {
  public:
-  struct NoContents {};
+  struct NoContents {
+    mojom::MissingTileReason reason =
+        mojom::MissingTileReason::kResourceNotReady;
+
+    NoContents() = default;
+    explicit NoContents(mojom::MissingTileReason r) : reason(r) {}
+  };
 
   struct CC_EXPORT TileResource {
     TileResource(const viz::TransferableResource& resource,
