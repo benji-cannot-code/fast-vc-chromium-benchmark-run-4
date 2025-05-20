bf0d6640004cdb89a977226933450c402f120b37
Kent Tamura <tkent@chromium.org>
2025-05-19 18:56:50 -0700

    Autofill: Improve performance of FieldDataManager::UpdateFieldDataMap()
    It's called every time on updating text control values.
    
    It was unable to move the specified value because it was
    std::u16string_view. By changing it to `std::u16string`, the function
    can move the value if a caller specifies a r-value as the argument.
    
    This change improves typing delay by 1.3ms for a value including 3.3M
    characters.
    
    Bug: 341564372, 414858401
    Change-Id: I7f39df1ccac6caadbd1f4675513fe85a1c15fb22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6555303
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462541}

--- components/autofill/core/common/field_data_manager.cc
+++ components/autofill/core/common/field_data_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/common/field_data_manager.h"
 
+#include <utility>
+
 #include "base/check.h"
 #include "base/i18n/case_conversion.h"
 
@@ -50,16 +52,17 @@ bool FieldDataManager::FindMatchedValue(const std::u16string& value) const {
 }
 
 void FieldDataManager::UpdateFieldDataMap(FieldRendererId id,
-                                          std::u16string_view value,
+                                          std::u16string value,
                                           FieldPropertiesMask mask) {
+  const bool is_empty = value.empty();
   if (HasFieldData(id)) {
-    field_value_and_properties_map_[id].first = std::u16string(value);
+    field_value_and_properties_map_[id].first = std::move(value);
     field_value_and_properties_map_[id].second |= mask;
   } else {
-    field_value_and_properties_map_[id] = {std::u16string(value), mask};
+    field_value_and_properties_map_[id] = {std::move(value), mask};
   }
   // Reset kUserTyped and kAutofilled flags if the value is empty.
-  if (value.empty()) {
+  if (is_empty) {
     field_value_and_properties_map_[id].second &=
         ~(FieldPropertiesFlags::kUserTyped | FieldPropertiesFlags::kAutofilled);
   }
