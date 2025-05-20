937bf6dd78903319e3cdd91377ad97853852485f
Min Qin <qinmin@chromium.org>
2025-05-20 08:58:18 -0700

    Unshare a tab group if a tab group transitioning to saved is removed by remote
    If a tab group is transitioning to saved, it may never complete the
    unshare if user dismisses the unshare dialog. In that case, Chrome
    should finish the transition when being removed from sync.
    
    Bug: 416341201
    Change-Id: I4575aaabff75fb6f393b532597f741e1ed2b50a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533143
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462829}

--- components/saved_tab_groups/internal/saved_tab_group_model.cc
+++ components/saved_tab_groups/internal/saved_tab_group_model.cc
@@ -253,10 +253,7 @@ void SavedTabGroupModel::RemovedFromSync(const LocalTabGroupID tab_group_id) {
   }
 
   const std::optional<int> index = GetIndexOf(tab_group_id);
-  SavedTabGroup removed_group = RemoveImpl(index.value());
-  for (auto& observer : observers_) {
-    observer.SavedTabGroupRemovedFromSync(removed_group);
-  }
+  HandleTabGroupRemovedFromSync(index.value());
 }
 
 void SavedTabGroupModel::RemovedFromSync(const base::Uuid& id) {
@@ -265,10 +262,7 @@ void SavedTabGroupModel::RemovedFromSync(const base::Uuid& id) {
   }
 
   const std::optional<int> index = GetIndexOf(id);
-  SavedTabGroup removed_group = RemoveImpl(index.value());
-  for (auto& observer : observers_) {
-    observer.SavedTabGroupRemovedFromSync(removed_group);
-  }
+  HandleTabGroupRemovedFromSync(index.value());
 }
 
 void SavedTabGroupModel::UpdatedVisualDataFromSync(
@@ -1014,4 +1008,20 @@ void SavedTabGroupModel::UpdateArchivalStatus(const base::Uuid& id,
   }
 }
 
+void SavedTabGroupModel::HandleTabGroupRemovedFromSync(int index) {
+  // If this is a shared group that is transitioning to saved, make the
+  // transition complete and that will delete the shared group during the
+  // process.
+  SavedTabGroup* group = &saved_tab_groups_[index];
+  if (group->is_shared_tab_group() && group->is_transitioning_to_saved()) {
+    for (auto& observer : observers_) {
+      observer.TabGroupTransitioningToSavedRemovedFromSync(group->saved_guid());
+    }
+    return;
+  }
+  SavedTabGroup removed_group = RemoveImpl(index);
+  for (auto& observer : observers_) {
+    observer.SavedTabGroupRemovedFromSync(removed_group);
+  }
+}
 }  // namespace tab_groups
