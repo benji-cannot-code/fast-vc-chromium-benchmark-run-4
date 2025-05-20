b068add06fc713d824bd972b3f24785a177bb50a
Alexis Hetu <sugoi@chromium.org>
2025-05-20 07:47:05 -0700

    [iOS] Add csv_password implementation for ios
    As part of the "import from safari" project, the password import
    process will use the PasswordImporter class in order to properly
    handle conflicts. In order to use PasswordImporter, the CSV
    password parser service has to be implemented for iOS. Because
    iOS does not run Chromium using multiple processes, like other
    platforms do, the parser service has to:
    1) Use mojo in the same process
    2) Use a memory-safe language for parsing because of 1)
    
    So, this CL adds an iOS version of the CSV password parser service
    as a singleton which is created on demand to hold onto the parser
    while the importer goes through the password import flow.
    
    The parser itself is a Swift implementation of the parser already
    implemented in CSVPasswordSequence in order to have that parser
    available in a memory-safe language. The CSVPasswordParserImplTest
    tests are now running on all platforms, including iOS, which now
    also covers the new Swift version of the parser.
    
    Design doc: go/bling-import
    
    Bug: 407587751
    Change-Id: I8b4368b6f2f957cccb6f75efdb32a21ac0ae5627
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6541928
    Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462787}

---
+++ components/password_manager/services/csv_password/ios/csv_password_parser_service.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/services/csv_password/csv_password_parser_service.h"
+
+#include "components/password_manager/services/csv_password/ios/csv_password_parser_service_factory.h"
+
+namespace password_manager {
+
+mojo::Remote<password_manager::mojom::CSVPasswordParser>
+LaunchCSVPasswordParser() {
+  return password_manager::CSVPasswordParserServiceFactory::GetInstance()
+      ->LaunchCSVPasswordParser();
+}
+
+}  // namespace password_manager
