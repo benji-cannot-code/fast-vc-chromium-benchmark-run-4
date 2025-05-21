2b672a806977684ac6dc2bfead86458e9bc1b4ed
Christine Ying <chrstne@google.com>
2025-05-21 10:22:14 -0700

    [VM] Replace hardcoded notice with value returned from DialogView
    Change-Id: I368d77794fb90361e4d612e64c4c75d0b45b93c0
    Bug: 408016824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553127
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463539}

--- chrome/browser/ui/webui/privacy_sandbox/base_dialog_ui.cc
+++ chrome/browser/ui/webui/privacy_sandbox/base_dialog_ui.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/privacy_sandbox/base_dialog_ui.h"
 
-#include "chrome/browser/privacy_sandbox/notice/notice.mojom.h"
 #include "chrome/browser/privacy_sandbox/notice/notice_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/views/privacy_sandbox/dialog_view_context.h"
@@ -21,7 +20,6 @@ namespace privacy_sandbox {
 
 using dialog::mojom::BaseDialogPage;
 using dialog::mojom::BaseDialogPageHandler;
-using notice::mojom::PrivacySandboxNotice;
 
 BaseDialogUI::BaseDialogUI(content::WebUI* web_ui)
     : ui::MojoWebUIController(web_ui) {
@@ -40,12 +38,10 @@ BaseDialogUI::BaseDialogUI(content::WebUI* web_ui)
           web_ui->GetWebContents());
   if (view_context) {
     delegate_ = &view_context->GetDelegate();
+    source->AddInteger(
+        "noticeIdToShow",
+        static_cast<int32_t>(delegate_->GetPrivacySandboxNotice()));
   }
-  // TODO(crbug.com/398005782): Replace hard coded value once notice is passed
-  // in from constructor.
-  source->AddInteger(
-      "noticeIdToShow",
-      static_cast<int32_t>(PrivacySandboxNotice::kTopicsConsentNotice));
 }
 
 WEB_UI_CONTROLLER_TYPE_IMPL(BaseDialogUI)
