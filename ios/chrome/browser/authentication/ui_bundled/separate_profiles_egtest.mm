cce3f9e4b4e227ebc64683b418615782abd7de8b
Jood <jood@google.com>
2025-05-21 04:19:14 -0700

    [iOS] Update IDS_IOS_ENTERPRISE_PROFILE_CREATION_* strings
    This CL:
    Adds a full stop after IDS_IOS_ENTERPRISE_PROFILE_CREATION_SUBTITLE
    only when it is followed by another string.
    And updates IDS_IOS_ENTERPRISE_PROFILE_CREATION_LEARN_MORE_HEADER
    
    Bug: 405076601
    Change-Id: I7a6627fb0617b5649cab1a47aa3fd8dd5d691667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568771
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463318}

--- ios/chrome/browser/authentication/ui_bundled/separate_profiles_egtest.mm
+++ ios/chrome/browser/authentication/ui_bundled/separate_profiles_egtest.mm
@@ -46,7 +46,7 @@ id<GREYMatcher> MergeBrowsingDataCellMatcher() {
 id<GREYMatcher> ManagedProfileCreationSubtitleMergeByDefaultMatcher() {
   return grey_accessibilityLabel([NSString
       stringWithFormat:
-          @"%@\n\n%@",
+          @"%@.\n\n%@",
           l10n_util::GetNSString(IDS_IOS_ENTERPRISE_PROFILE_CREATION_SUBTITLE),
           l10n_util::GetNSString(
               IDS_IOS_ENTERPRISE_PROFILE_CREATION_ACCOUNT_KEEP_BROWSING_DATA_DESCRIPTION)]);
@@ -60,7 +60,7 @@ id<GREYMatcher> ManagedProfileCreationSubtitleMatcher() {
 id<GREYMatcher> ManagedProfileCreationDataMigrationDisabledSubtitleMatcher() {
   return grey_accessibilityLabel([NSString
       stringWithFormat:
-          @"%@\n\n%@",
+          @"%@.\n\n%@",
           l10n_util::GetNSString(IDS_IOS_ENTERPRISE_PROFILE_CREATION_SUBTITLE),
           l10n_util::GetNSString(
               IDS_IOS_ENTERPRISE_PROFILE_CREATION_ACCOUNT_KEEP_BROWSING_DATA_DISABLED_DESCRIPTION)]);
