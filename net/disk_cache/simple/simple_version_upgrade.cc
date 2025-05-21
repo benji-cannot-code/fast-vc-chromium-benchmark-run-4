fcdbbbe68be22ceb6a105e9b77cf2906d48688be
Maks Orlovich <morlovich@chromium.org>
2025-05-20 18:31:12 -0700

    Cleanup some unsafe_buffers in net/disk_cache
    This mostly involves spanify'ing blockfile stats, but also touches a few
    self-zero type things in a few spots.
    
    Bug: 40285824
    Change-Id: Ie6c3f8916f9edd452014f1750d77ea0338736e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518712
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463176}

--- net/disk_cache/simple/simple_version_upgrade.cc
+++ net/disk_cache/simple/simple_version_upgrade.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
-#pragma allow_unsafe_libc_calls
-#endif
-
 #include "net/disk_cache/simple/simple_version_upgrade.h"
 
 #include <cstring>
@@ -65,8 +60,9 @@ bool WriteFakeIndexFile(disk_cache::BackendFileOperations* file_operations,
 namespace disk_cache {
 
 FakeIndexData::FakeIndexData() {
-  // Make hashing repeatable: leave no padding bytes untouched.
-  std::memset(this, 0, sizeof(*this));
+  // We don't want unset holes in types stored to disk.
+  static_assert(std::has_unique_object_representations_v<FakeIndexData>,
+                "FakeIndexData should have no implicit padding bytes");
 }
 
 // Some points about the Upgrade process are still not clear:
