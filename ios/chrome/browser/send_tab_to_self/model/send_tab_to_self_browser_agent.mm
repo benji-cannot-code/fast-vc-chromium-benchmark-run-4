0a3b54ae9cbfe951d360396af22a8c0e8823a166
Sylvain Defresne <sdefresne@chromium.org>
2025-05-21 07:29:28 -0700

    [ios] Simplify BrowserUserData sub-classes
    Since BrowserUserData stores a pointer to the owning Browser,
    convert sub-classes to use this pointer instead of each of
    them maintaining their own copy of the pointer.
    
    This converts //ios/chrome/browser/send_tab_to_self directory.
    
    Bug: 409299519
    Change-Id: Ifbc20032617c054f6c07926aea34cf46b8845b43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6573735
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463412}

--- ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.mm
+++ ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.mm
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 SendTabToSelfBrowserAgent::SendTabToSelfBrowserAgent(Browser* browser)
     : BrowserUserData(browser),
-      browser_(browser),
       model_(
           SendTabToSelfSyncServiceFactory::GetForProfile(browser_->GetProfile())
               ->GetSendTabToSelfModel()) {
