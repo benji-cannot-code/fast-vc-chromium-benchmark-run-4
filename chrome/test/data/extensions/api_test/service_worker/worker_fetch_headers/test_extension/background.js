8c4dd556674b29b4d83e260f298209bf83f9ed90
Justin Lulejian <jlulejian@chromium.org>
2025-05-21 10:29:54 -0700

    [Extensions] Test extension fetch() requests with header modifications.
    Before this change there wasn't a test that explicitly confirmed the
    behavior of fetch() requests from extension contexts that attempt to
    overwrite forbidden and non-forbidden headers.
    
    After this change we confirm the current behavior of privileged contexts
    (background scripts, extension resource scripts) and non-privileged
    contexts (content scripts).
    
    Currently the behavior between these contexts is the same: non-forbidden
    headers can be set by the extension, but setting forbidden headers are
    overwritten by the browser before sending. This complies with the
    current web API spec for fetch(). In a future CL, we'll make it so that
    privileged contexts can set forbidden headers.
    
    Bug: 418811955
    Change-Id: I1561d3a6fa4a298bca24b9d64d57bd3a5b78e309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533740
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463543}

---
+++ chrome/test/data/extensions/api_test/service_worker/worker_fetch_headers/test_extension/background.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {getCommonFetchHeaderTests} from '/_test_resources/api_test/service_worker/worker_fetch_headers/fetch_common_tests.js';
+
+chrome.test.getConfig(function(config) {
+  var runBackgroundTests = config.customArg == 'run_background_tests';
+  if (runBackgroundTests) {
+    chrome.test.runTests(getCommonFetchHeaderTests());
+  }
+});
---
+++ chrome/test/data/extensions/api_test/service_worker/worker_fetch_headers/test_extension/popup.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This works-around that popups can't import because they aren't considered
+// modules.
+(async () => {
+  const src = chrome.runtime.getURL(
+      '/_test_resources/api_test/service_worker/worker_fetch_headers/' +
+      'fetch_common_tests.js');
+  const testsImport = await import(src);
+  chrome.test.runTests(testsImport.getCommonFetchHeaderTests());
+})();
