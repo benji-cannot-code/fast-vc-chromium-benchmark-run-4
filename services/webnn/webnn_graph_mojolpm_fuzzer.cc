c95c304c66d2f14b0aa53b82d4ec271fef6de19f
Bryan Bernhart <bryan.bernhart@intel.com>
2025-05-21 13:56:56 -0700

    Reland "WebNN: support constant tensors"
    This is a reland of commit fd00312e80a964f7f5dffee1f811f634f7fe731e
    
    Fixed a bug where constant DML buffers lacked the properties needed to
    write initialized data and could fail on UMA devices.
    
    Original change's description:
    > WebNN: support constant tensors
    >
    > Allows MLTensor to be input to constant() so weights can be
    > reused on-device between multiple builds on the same builder
    > or different builders. This eliminates the need to keep the original
    > JS input data and lowers CPU memory usage.
    >
    > To keep the CL size in check, only the DML backend was enabled.
    >
    > More specifically:
    > * Adds constant usage to MLTensor.
    > * Allows tensors to be initialized from a supplied JS buffer.
    > * Supports graph builds using weights from tensors.
    >
    > Restrictions:
    > * Constant tensors cannot be dispatched.
    > * Constant tensors must be initialized.
    > * Constant tensors must remain static.
    >
    > https://github.com/webmachinelearning/webnn/issues/760
    >
    > Bug: 332350952
    > Change-Id: Ib18dfe06ead6728172355f2a540e3faeec99917b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075601
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Bryan Bernhart <bryan.bernhart@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1460981}
    
    Bug: 332350952, 418078503
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Change-Id: If6dc4282ccc908444c2d266e41c2fca94ecfc371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6557622
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Bryan Bernhart <bryan.bernhart@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1463706}

--- services/webnn/webnn_graph_mojolpm_fuzzer.cc
+++ services/webnn/webnn_graph_mojolpm_fuzzer.cc
@@ -154,6 +154,7 @@ void BuildGraph(const mojolpm::webnn::mojom::GraphInfo& graph_info_proto,
     base::test::TestFuture<webnn::mojom::CreateTensorResultPtr>
         create_tensor_future;
     webnn_context_remote->CreateTensor(std::move(tensor_info),
+                                       mojo_base::BigBuffer(0),
                                        create_tensor_future.GetCallback());
     webnn::mojom::CreateTensorResultPtr create_tensor_result =
         create_tensor_future.Take();
@@ -189,6 +190,7 @@ void BuildGraph(const mojolpm::webnn::mojom::GraphInfo& graph_info_proto,
     base::test::TestFuture<webnn::mojom::CreateTensorResultPtr>
         create_tensor_future;
     webnn_context_remote->CreateTensor(std::move(tensor_info),
+                                       mojo_base::BigBuffer(0),
                                        create_tensor_future.GetCallback());
     webnn::mojom::CreateTensorResultPtr create_tensor_result =
         create_tensor_future.Take();
--- third_party/blink/renderer/modules/ml/webnn/ml_constant_operand.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_constant_operand.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/webnn/public/mojom/webnn_graph.mojom-blink.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_operand.h"
+#include "third_party/blink/renderer/modules/ml/webnn/ml_tensor.h"
 
 namespace blink {
 
@@ -17,9 +18,16 @@ MLConstantOperand::MLConstantOperand(MLGraphBuilder* builder,
                 webnn::mojom::blink::Operand::Kind::kConstant,
                 std::move(descriptor)) {}
 
+MLConstantOperand::MLConstantOperand(MLGraphBuilder* builder, MLTensor* tensor)
+    : MLOperand(builder,
+                webnn::mojom::blink::Operand::Kind::kConstant,
+                tensor->Descriptor()),
+      tensor_(tensor) {}
+
 MLConstantOperand::~MLConstantOperand() = default;
 
 void MLConstantOperand::Trace(Visitor* visitor) const {
+  visitor->Trace(tensor_);
   MLOperand::Trace(visitor);
 }
 
