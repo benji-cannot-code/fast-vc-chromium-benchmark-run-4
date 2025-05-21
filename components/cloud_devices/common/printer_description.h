130130222219c7341861b38980ea84f9dcf0d97d
Jan Keitel <jkeitel@google.com>
2025-05-21 09:53:17 -0700

    Modernize equality operators
    This CL is pure clean-up and contains no functionality changes.
    Depending on the files covered in the CL (since the CL was generated
    using git cl split), it does a subset of the following:
    - Remove unneeded operator!= declarations/definitions since C++20 can
      automatically derive those from operator==.
    - Default operator== where this is equivalent to the current behavior.
    - Default operator<=> where this is equivalent to the current
      behavior.
    
    This CL was uploaded by git cl split.
    
    R=vitalybuka@chromium.org
    
    Bug: 40256175
    Change-Id: I6d7787c182499fbfcb110c6f4a719d21c1de99cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513057
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463520}

--- components/cloud_devices/common/printer_description.h
+++ components/cloud_devices/common/printer_description.h
@@ -86,10 +86,8 @@ class RangeVendorCapability {
 
   RangeVendorCapability& operator=(RangeVendorCapability&& other);
 
-  bool operator==(const RangeVendorCapability& other) const;
-  bool operator!=(const RangeVendorCapability& other) const {
-    return !(*this == other);
-  }
+  friend bool operator==(const RangeVendorCapability&,
+                         const RangeVendorCapability&) = default;
 
   bool IsValid() const;
   bool LoadFrom(const base::Value::Dict& dict);
@@ -109,10 +107,8 @@ struct SelectVendorCapabilityOption {
   ~SelectVendorCapabilityOption();
 
   bool IsValid() const;
-  bool operator==(const SelectVendorCapabilityOption& other) const;
-  bool operator!=(const SelectVendorCapabilityOption& other) const {
-    return !(*this == other);
-  }
+  friend bool operator==(const SelectVendorCapabilityOption&,
+                         const SelectVendorCapabilityOption&) = default;
 
   std::string value;
   std::string display_name;
@@ -141,10 +137,8 @@ class TypedValueVendorCapability {
 
   TypedValueVendorCapability& operator=(TypedValueVendorCapability&& other);
 
-  bool operator==(const TypedValueVendorCapability& other) const;
-  bool operator!=(const TypedValueVendorCapability& other) const {
-    return !(*this == other);
-  }
+  friend bool operator==(const TypedValueVendorCapability&,
+                         const TypedValueVendorCapability&) = default;
 
   bool IsValid() const;
   bool LoadFrom(const base::Value::Dict& dict);
@@ -182,9 +176,6 @@ class VendorCapability {
   ~VendorCapability();
 
   bool operator==(const VendorCapability& other) const;
-  bool operator!=(const VendorCapability& other) const {
-    return !(*this == other);
-  }
 
   bool IsValid() const;
   bool LoadFrom(const base::Value::Dict& dict);
@@ -210,8 +201,7 @@ struct VendorItem {
   VendorItem(const std::string& id, const std::string& value);
 
   bool IsValid() const;
-  bool operator==(const VendorItem& other) const;
-  bool operator!=(const VendorItem& other) const { return !(*this == other); }
+  friend bool operator==(const VendorItem&, const VendorItem&) = default;
 
   std::string id;
   std::string value;
@@ -230,8 +220,7 @@ struct Color {
   explicit Color(ColorType type);
 
   bool IsValid() const;
-  bool operator==(const Color& other) const;
-  bool operator!=(const Color& other) const { return !(*this == other); }
+  friend bool operator==(const Color&, const Color&) = default;
 
   ColorType type;
   std::string vendor_id;
@@ -257,7 +246,6 @@ struct Margins {
   Margins(int32_t top_um, int32_t right_um, int32_t bottom_um, int32_t left_um);
 
   bool operator==(const Margins& other) const;
-  bool operator!=(const Margins& other) const { return !(*this == other); }
 
   int32_t top_um;
   int32_t right_um;
@@ -270,8 +258,7 @@ struct Dpi {
   Dpi(int32_t horizontal, int32_t vertical);
 
   bool IsValid() const;
-  bool operator==(const Dpi& other) const;
-  bool operator!=(const Dpi& other) const { return !(*this == other); }
+  friend bool operator==(const Dpi&, const Dpi&) = default;
 
   int32_t horizontal;
   int32_t vertical;
@@ -474,7 +461,6 @@ struct Media {
 
   bool IsValid() const;
   bool operator==(const Media& other) const;
-  bool operator!=(const Media& other) const { return !(*this == other); }
 
   MediaSize size_name;
   gfx::Size size_um;
@@ -553,8 +539,7 @@ struct Interval {
   Interval(int32_t start, int32_t end);
   explicit Interval(int32_t start);
 
-  bool operator==(const Interval& other) const;
-  bool operator!=(const Interval& other) const { return !(*this == other); }
+  friend bool operator==(const Interval&, const Interval&) = default;
 
   int32_t start;
   int32_t end;
@@ -567,8 +552,7 @@ struct MediaType {
   MediaType(const std::string& vendor_id,
             const std::string& custom_display_name);
 
-  bool operator==(const MediaType& other) const;
-  bool operator!=(const MediaType& other) const { return !(*this == other); }
+  friend bool operator==(const MediaType&, const MediaType&) = default;
 
   bool IsValid() const;
 
