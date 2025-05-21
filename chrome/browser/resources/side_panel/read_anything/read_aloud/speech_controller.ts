41785355a320bdee52ef7c9551544fd570801534
Kristi Saney <kristislee@google.com>
2025-05-21 14:39:15 -0700

    [Read aloud] Remove resetting the engine state
    This was causing an issue where going to a new page would make the play
    button unclickable.
    
    Change-Id: I07a8fae9bb695d7268f9a5d44b42a33caa9f6943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576041
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463734}

--- chrome/browser/resources/side_panel/read_anything/read_aloud/speech_controller.ts
+++ chrome/browser/resources/side_panel/read_anything/read_aloud/speech_controller.ts
@@ -789,7 +789,6 @@ export class SpeechController {
       isSpeechBeingRepositioned: false,
     };
     this.setState_(speechPlayingState);
-    this.setEngineState_(SpeechEngineState.NONE);
     this.setPreviewVoicePlaying_(null);
     this.model_.setFirstTextNode(null);
     this.model_.setResumeSpeechOnVoiceMenuClose(false);
