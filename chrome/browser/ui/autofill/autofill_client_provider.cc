a0d6a98c18d63c97b741697bfe2abae5412e8dce
Friedrich Horschig <friedrichh@chromium.org>
2025-05-21 08:40:34 -0700

    [Android][3PPWM] Add interface for a11y deprecation field trial
    The bug for details and the full plan. The tl;dr is that a11y will be
    deprecated for Autofill purposes.
    The deprecation happens with a server-side flag. To monitor signals
    about stability in UMA, we want clients receiving the change to
    become part of an Enabled group.
    
    The annotation kCurrentLog is acceptable since there should not
    be any data generated before the registering is called anyways.
    If there was (e.g. stability incidents), then it's useful, too.
    
    The implementation is introduced in https://crrev.com/i/8276823.
    The trial is added to the dict of trials at http://cl/760570261.
    
    This interface allows downstream implementations to enroll the
    client into a field trial when the decision to use 3P clients
    is made.
    
    Bug: 418159584
    Change-Id: I7df178d0e03868cdc217825e8ab28867505938d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6554132
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463468}

--- chrome/browser/ui/autofill/autofill_client_provider.cc
+++ chrome/browser/ui/autofill/autofill_client_provider.cc
@@ -14,8 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
 #include "chrome/browser/autofill/android/android_autofill_availability_status.h"
 #include "chrome/browser/autofill/android/jni_headers/AutofillClientProviderUtils_jni.h"
+#include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "components/android_autofill/browser/android_autofill_client.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
@@ -83,11 +85,23 @@ bool UsesVirtualViewStructureForAutofill(PrefService& prefs) {
 #endif  // BUILDFLAG(IS_ANDROID)
 }
 
+#if BUILDFLAG(IS_ANDROID)
+std::string GetTrialGroupForPackage() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  return base::android::ConvertJavaStringToUTF8(
+      env, Java_AutofillClientProviderUtils_getTrialGroupForPackage(env));
+}
+#endif
+
 }  // namespace
 
 AutofillClientProvider::AutofillClientProvider(PrefService* prefs)
     : uses_platform_autofill_(UsesVirtualViewStructureForAutofill(*prefs)) {
 #if BUILDFLAG(IS_ANDROID)
+  // Non-Android users are not affected by the configuration change.
+  ChromeMetricsServiceAccessor::RegisterSyntheticFieldTrial(
+      "SyntheticAutofillViaA11yDeprecated", GetTrialGroupForPackage(),
+      variations::SyntheticTrialAnnotationMode::kCurrentLog);
   RecordWhetherAndroidPrefResets(*prefs, uses_platform_autofill_);
   // Ensure the pref is reset if platform autofill is restricted.
   prefs->SetBoolean(prefs::kAutofillUsingVirtualViewStructure,
