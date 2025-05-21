9b18cf0f34a1a0e70fe020fbb6d5fba1b2dc83f7
Ewann Pelle <ewannpv@chromium.org>
2025-05-21 08:06:58 -0700

    [iOS] Stop observing in destructor
    Stop observing the browser when IOSCollaborationControllerDelegate's
    destructor is called.
    
    Fixed: 418010889
    Change-Id: Icd39ecf165b5676efb17beedb2632aa038506796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574496
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463442}

--- ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate.mm
+++ ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate.mm
@@ -130,7 +130,13 @@ IOSCollaborationControllerDelegate::IOSCollaborationControllerDelegate(
   CHECK(base_view_controller_);
 }
 
-IOSCollaborationControllerDelegate::~IOSCollaborationControllerDelegate() {}
+IOSCollaborationControllerDelegate::~IOSCollaborationControllerDelegate() {
+  if (IsInObserverList()) {
+    CHECK(browser_);
+    browser_->RemoveObserver(this);
+    browser_ = nullptr;
+  }
+}
 
 // CollaborationControllerDelegate.
 void IOSCollaborationControllerDelegate::PrepareFlowUI(
