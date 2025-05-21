7bedb2bdbfd8824fdceaf806f78037448c239b97
Lijin Shen <lazzzis@google.com>
2025-05-20 17:11:46 -0700

    Remove INSECURE_DOWNLOAD_INFOBAR_DELEGATE_ANDROID
    This infobar has been migrated to message ui and has not been used
    for quite a while.
    
    Bug: 375499412
    Change-Id: I09a2731379416725c5d76ee6a19333629f00743b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6548955
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463119}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -110,7 +110,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/android/download_utils.h"
 #include "chrome/browser/download/android/duplicate_download_dialog_bridge_delegate.h"
 #include "chrome/browser/download/android/insecure_download_dialog_bridge.h"
-#include "chrome/browser/download/android/insecure_download_infobar_delegate.h"
 #include "chrome/browser/download/android/new_navigation_observer.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/ui/android/pdf/pdf_jni_headers/PdfUtils_jni.h"
