9d66e68f386348acf19853cd2299a370d8caf724
Nicolás Peña <npm@chromium.org>
2025-05-21 14:48:22 -0700

    [FedCM] Add "Opens in a new tab" a11y text to login buttons
    This CL adds the a11y to the desktop login buttons, similar to what was
    done in the Android counterpart. To do so, HoverButton is modified since
    it computes the accessible name from the title, subtitle, and footer.
    Thus we need to add a method which appends some text to these to form
    the new accessible name.
    
    Fixed: 409125431
    Change-Id: If88d2d3100115b6b4486a3992af9f66ef965e205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576821
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463742}

--- chrome/browser/ui/views/controls/hover_button.h
+++ chrome/browser/ui/views/controls/hover_button.h
@@ -108,6 +108,10 @@ class HoverButton : public views::LabelButton {
   // Set the text context and style of the footer.
   void SetFooterTextStyle(int text_context, views::style::TextStyle text_style);
 
+  // Adds a11y text to the button, which will be read out when the button is
+  // focused.
+  void AddExtraAccessibleText(const std::u16string& text);
+
   void SetIconHorizontalMargins(int left, int right);
 
   PressedCallback& callback(base::PassKey<HoverButtonController>) {
@@ -156,6 +160,8 @@ class HoverButton : public views::LabelButton {
   raw_ptr<views::Label> footer_ = nullptr;
   raw_ptr<views::View> icon_view_ = nullptr;
   raw_ptr<views::View> secondary_view_ = nullptr;
+
+  std::u16string additional_accessible_text_;
 
   std::vector<base::CallbackListSubscription> text_changed_subscriptions_;
 
