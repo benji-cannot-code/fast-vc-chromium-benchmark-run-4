e22f94e279615d1f3cdd4e78352923c51a551fe2
Andy Phan <andyphan@chromium.org>
2025-05-21 16:28:04 -0700

    [PDF Ink Signatures] Move some methods to PDFiumDrawSelectionTestBase
    Create PDFiumDrawSelectionTestBase and move various test helpers from
    PDFiumEngineTest to it. This allows the methods to be used in other test
    fixtures, such as in https://crrev.com/c/6576063.
    
    Bug: 410777432
    Change-Id: Iaf6a3642360ca2e65e9a7c82259d2deaf3d68f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576062
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463789}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -664,6 +664,7 @@ class PDFiumEngine : public DocumentLoader::Client, public IFSDK_PAUSE {
   };
 
   friend class FormFillerTest;
+  friend class PDFiumDrawSelectionTestBase;
   friend class PDFiumEngineTabbingTest;
   friend class PDFiumEngineTest;
   friend class PDFiumFormFiller;
--- pdf/test/test_helpers.cc
+++ pdf/test/test_helpers.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "pdf/test/test_helpers.h"
 
+#include <string_view>
+
 #include "base/base_paths.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
@@ -101,6 +103,18 @@ base::FilePath::StringType GetTestDataPathWithPlatformSuffix(
 #else
   return base::FilePath(filename).value();
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+}
+
+base::FilePath GetReferenceFilePath(
+    base::FilePath::StringViewType sub_directory,
+    std::string_view test_filename,
+    bool use_platform_suffix) {
+  if (use_platform_suffix) {
+    return base::FilePath(sub_directory)
+        .Append(GetTestDataPathWithPlatformSuffix(test_filename));
+  }
+
+  return base::FilePath(sub_directory).AppendASCII(test_filename);
 }
 
 testing::AssertionResult MatchesPngFile(
