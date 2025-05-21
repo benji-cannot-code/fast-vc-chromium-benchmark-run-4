9650532a9ea0e922b519e7e1561f08abd554fb64
Joe Hattori <hattorij@google.com>
2025-05-20 19:32:42 -0700

    Record durations from TWA launch
    This commit records durations from TWA startup timestamp. Metrics suchas the duration from TWA launch to FCP would be measured. A pull request to android_browser_helper
    (https://github.com/GoogleChrome/android-browser-helper/pull/517) puts
    the corresponding extra data to the intent.
    
    Bug: 413583728
    Change-Id: I142ac5a74617435c0d1b7399126a3aea76f3ecfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520581
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Joe Hattori <hattorij@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463203}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/HiddenTabHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/HiddenTabHolder.java
@@ -231,7 +231,8 @@ public class HiddenTabHolder {
 
         TabObserverRegistrar registrar = new TabObserverRegistrar();
         CustomTabObserver customTabObserver =
-                new CustomTabObserver(/* openedByChrome= */ false, session);
+                new CustomTabObserver(
+                        /* openedByChrome= */ false, session, /* twaStartupUptimeMillis= */ null);
         CustomTabNavigationEventObserver customTabNavigationEventObserver =
                 new CustomTabNavigationEventObserver(session, /* forPrerender= */ true);
         CustomTabActivityTabController.addTabNavigationObservers(
@@ -396,7 +397,8 @@ public class HiddenTabHolder {
         TabObserverRegistrar registrar = new TabObserverRegistrar();
         SessionHolder<?> token = SessionHolder.getSessionHolderFromIntent(intent);
         CustomTabObserver customTabObserver =
-                new CustomTabObserver(/* openedByChrome= */ false, token);
+                new CustomTabObserver(
+                        /* openedByChrome= */ false, token, /* twaStartupUptimeMillis= */ null);
         CustomTabNavigationEventObserver customTabNavigationEventObserver =
                 new CustomTabNavigationEventObserver(token, /* forPrerender= */ false);
         CustomTabActivityTabController.addTabNavigationObservers(
