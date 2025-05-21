e142ce441756b37f2623f160ffa0cdfb91d43d52
Benjamin Williams <bwwilliams@google.com>
2025-05-21 09:15:50 -0700

    [iOS] Add SearchWhatYouSeePromoViewController strings
    This change introduces the necessary strings for the
    SearchWhatYouSeePromoViewController. These strings are the title,
    subtitle, and action button text designed to guide users on how to use
    the 'Search what you see' feature.
    
    Note: This CL focuses solely on adding the strings for the
    SearchWhatYouSeePromoViewController. The instructions overlay view and
    its associated strings are not part of this specific change.
    
    Demo screenshot: https://screenshot.googleplex.com/7ZbZukaB6MXCwmk.png
    
    Change-Id: Ic5fd7675b162cabd9f434ce7c23a983bc0f7794f
    Bug: 417690229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565101
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463494}

--- ios/chrome/browser/tips_notifications/ui/search_what_you_see_promo_view_controller.mm
+++ ios/chrome/browser/tips_notifications/ui/search_what_you_see_promo_view_controller.mm
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/tips_notifications/ui/search_what_you_see_promo_view_controller.h"
 
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac.h"
+
 namespace {
 
 // The name of the animation used for the Search What You See Lottie.
@@ -26,6 +29,12 @@ NSString* const kSearchWhatYouSeePromoAXID = @"kSearchWhatYouSeePromoAXID";
 - (void)viewDidLoad {
   self.animationName = kAnimationName;
   self.animationNameDarkMode = kAnimationNameDarkMode;
+  self.titleString =
+      l10n_util::GetNSString(IDS_IOS_SEARCH_WHAT_YOU_SEE_TIPS_PROMO_TITLE);
+  self.subtitleString =
+      l10n_util::GetNSString(IDS_IOS_SEARCH_WHAT_YOU_SEE_TIPS_PROMO_SUBTITLE);
+  self.secondaryActionString = l10n_util::GetNSString(
+      IDS_IOS_SEARCH_WHAT_YOU_SEE_TIPS_PROMO_SHOW_ME_HOW_ACTION);
 
   [super viewDidLoad];
 
