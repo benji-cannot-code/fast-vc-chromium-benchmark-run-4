5a561b92fe07a33df92129b80617b5c699bd2e08
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-05-20 17:34:58 -0700

    [Installer Downloader] Use IDS_LEARN_MORE string instead of new string
    Change-Id: I6a6d2bbcfe33298935f96617e3940db376369e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6570093
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463137}

--- chrome/browser/win/installer_downloader/installer_downloader_infobar_delegate.cc
+++ chrome/browser/win/installer_downloader/installer_downloader_infobar_delegate.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_manager.h"
 #include "components/omnibox/browser/vector_icons.h"
+#include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/vector_icon_types.h"
@@ -81,7 +82,7 @@ std::u16string InstallerDownloaderInfoBarDelegate::GetMessageText() const {
 }
 
 std::u16string InstallerDownloaderInfoBarDelegate::GetLinkText() const {
-  return l10n_util::GetStringUTF16(IDS_INSTALLER_DOWNLOADER_LINK_TEXT);
+  return l10n_util::GetStringUTF16(IDS_LEARN_MORE);
 }
 
 int InstallerDownloaderInfoBarDelegate::GetButtons() const {
