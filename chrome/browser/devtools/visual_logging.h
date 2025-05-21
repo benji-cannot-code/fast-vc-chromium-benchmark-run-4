b3cc340107f41e3b3a3522ad8eab81f3ad593d44
Danil Somsikov <dsv@chromium.org>
2025-05-21 04:14:02 -0700

    Fix the session tag name for GenAI without logging
    Also removed unnecessary bitmask check
    
    Bug: 417114401
    Change-Id: I733905b81fb4d178ebbf8f97874684383cf3b29d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6573755
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463315}

--- chrome/browser/devtools/visual_logging.h
+++ chrome/browser/devtools/visual_logging.h
@@ -68,7 +68,7 @@ struct SettingAccessEvent {
 enum SessionTags {
   kUserSignedIn = 1 << 0,
   kDevToolsGetAiEnterprisePolicyDisabled = 1 << 1,
-  kDevToolsGetAiEnterprisePolicyAllowWithLogging = 1 << 2,
+  kDevToolsGetAiEnterprisePolicyAllowWithoutLogging = 1 << 2,
   kDevToolsRemoteDebuggingDisabled = 1 << 3,
 };
 
