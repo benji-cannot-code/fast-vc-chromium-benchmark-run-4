ee3205b476c27d11eabc96c8be3c81012ea77a3c
mikt <mikt@google.com>
2025-05-20 19:52:28 -0700

    [PA] Cache ReservationOffsetTable instead of lookup by address
    This change will cache ReservationOffsetTable inside PartitionRoot.
    This can have slight perf improvement, but I assume it is not measurable
    since ReservationOffsetTable is mainly for direct-mapped allocations.
    
    True motivation here is to allow us to glue ReservationOffsetTable,
    which should be a follow-up CL later.
    That makes easier to enable BRP in multiple pools.
    
    Change-Id: I1a420b506512d9ee469235d1ebb0ed930875694a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6538607
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463208}

--- base/allocator/partition_allocator/src/partition_alloc/reservation_offset_table.cc
+++ base/allocator/partition_allocator/src/partition_alloc/reservation_offset_table.cc
@@ -10,9 +10,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace partition_alloc::internal {
 
 #if PA_BUILDFLAG(HAS_64_BIT_POINTERS)
-PA_CONSTINIT ReservationOffsetTable ReservationOffsetTable::singleton_;
+PA_CONSTINIT ReservationOffsetTable::_ReservationOffsetTable<
+    ReservationOffsetTable::kRegularOffsetTableLength>
+    ReservationOffsetTable::regular_pool_table_;
+PA_CONSTINIT ReservationOffsetTable::_ReservationOffsetTable<
+    ReservationOffsetTable::kBRPOffsetTableLength>
+    ReservationOffsetTable::brp_pool_table_;
+PA_CONSTINIT ReservationOffsetTable::_ReservationOffsetTable<
+    ReservationOffsetTable::kConfigurableOffsetTableLength>
+    ReservationOffsetTable::configurable_pool_table_;
+#if PA_BUILDFLAG(ENABLE_THREAD_ISOLATION)
+// If thread isolation support is enabled, we need to write-protect the tables
+// of the thread isolated pool. For this, the thread isolated ones start on a
+// page boundary.
+PA_THREAD_ISOLATED_ALIGN
+PA_CONSTINIT ReservationOffsetTable::_ReservationOffsetTable<
+    ReservationOffsetTable::kThreadIsolatedOffsetTableLength>
+    ReservationOffsetTable::thread_isolated_pool_table_;
+#endif
 #else
-PA_CONSTINIT ReservationOffsetTable::_ReservationOffsetTable
+// A single table for the entire 32-bit address space.
+PA_CONSTINIT ReservationOffsetTable::_ReservationOffsetTable<
+    ReservationOffsetTable::kReservationOffsetTableLength>
     ReservationOffsetTable::reservation_offset_table_;
 #endif  // PA_BUILDFLAG(HAS_64_BIT_POINTERS)
 
