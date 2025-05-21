7f7caa9374c1b3a352d5e0d908c827afc65158c6
Hailey Wang <haileywang@google.com>
2025-05-20 17:30:57 -0700

    [STG] Prevent IPH to be shown on top of collaboration flow UI
    Bug: 408263933
    Change-Id: Id87308aded8ac35b2e02e0c384d1c5edae17c97b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6567813
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463131}

--- chrome/browser/collaboration/android/java/src/org/chromium/chrome/browser/collaboration/CollaborationControllerDelegateImpl.java
+++ chrome/browser/collaboration/android/java/src/org/chromium/chrome/browser/collaboration/CollaborationControllerDelegateImpl.java
@@ -20,6 +20,7 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.data_sharing.DataSharingMetrics;
 import org.chromium.chrome.browser.data_sharing.DataSharingTabManager;
+import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.SettingsNavigationFactory;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
@@ -46,6 +47,7 @@ import org.chromium.components.data_sharing.GroupToken;
 import org.chromium.components.data_sharing.SharedTabGroupPreview;
 import org.chromium.components.data_sharing.configs.DataSharingCreateUiConfig;
 import org.chromium.components.data_sharing.configs.DataSharingJoinUiConfig;
+import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
 import org.chromium.components.tab_group_sync.LocalTabGroupId;
 import org.chromium.components.tab_group_sync.SavedTabGroup;
@@ -77,6 +79,9 @@ public class CollaborationControllerDelegateImpl implements CollaborationControl
 
     // Stores the runnable to close the current showing UI. Is null when there's no UI showing.
     private @Nullable Runnable mCloseScreenRunnable;
+
+    /** Used to suppress IPH UIs while a collaboration flow UI is on the screen. */
+    private Tracker.@Nullable DisplayLockHandle mFeatureEngagementLock;
 
     /**
      * Constructor for a new {@link CollaborationControllerDelegateImpl} object.
@@ -126,6 +131,11 @@ public class CollaborationControllerDelegateImpl implements CollaborationControl
     @CalledByNative
     void prepareFlowUI(long exitCallback, long resultCallback) {
         mExitCallback = exitCallback;
+
+        // Acquire lock to prevent IPH from being shown in a collaboration flow.
+        Tracker tracker = TrackerFactory.getTrackerForProfile(mDataSharingTabManager.getProfile());
+        mFeatureEngagementLock = tracker.acquireDisplayLock();
+
         Runnable onTabSwitcherShownRunnable =
                 () -> {
                     CollaborationControllerDelegateImplJni.get()
@@ -684,6 +694,9 @@ public class CollaborationControllerDelegateImpl implements CollaborationControl
         mDataSharingTabManager.onCollaborationDelegateFlowFinished();
         cleanUpPointers();
 
+        if (mFeatureEngagementLock != null) {
+            mFeatureEngagementLock.release();
+        }
         if (mExitCallback != 0) {
             CollaborationControllerDelegateImplJni.get().deleteExitCallback(mExitCallback);
         }
