26765ef7759acd73d2a4417ad3e499c45d8ca57d
Sky Malice <skym@chromium.org>
2025-05-21 16:02:11 -0700

    Stop casting findViewById in chrome/browser/ui/android.
    Bug: 352582031
    Change-Id: I55979d13d2e2e4ac5740db6b113c3fdb722a17e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575827
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463774}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionViewBinder.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionViewBinder.java
@@ -888,7 +888,7 @@ class AccountSelectionViewBinder {
                             model.get(HeaderProperties.RP_MODE) == RpMode.ACTIVE
                                     ? R.dimen.account_selection_active_mode_sheet_icon_size
                                     : R.dimen.account_selection_sheet_icon_size);
-            ImageView headerIconView = (ImageView) view.findViewById(R.id.header_icon);
+            ImageView headerIconView = view.findViewById(R.id.header_icon);
             if (shouldCircleCrop) {
                 Drawable croppedBrandIcon =
                         createBitmapWithMaskableIconSafeZone(resources, brandIcon, iconSize);
@@ -907,8 +907,8 @@ class AccountSelectionViewBinder {
             if (model.get(HeaderProperties.RP_MODE) == RpMode.PASSIVE) return;
 
             Bitmap brandIcon = model.get(HeaderProperties.RP_BRAND_ICON);
-            ImageView headerIconView = (ImageView) view.findViewById(R.id.header_rp_icon);
-            ImageView arrowRangeIcon = (ImageView) view.findViewById(R.id.arrow_range_icon);
+            ImageView headerIconView = view.findViewById(R.id.header_rp_icon);
+            ImageView arrowRangeIcon = view.findViewById(R.id.arrow_range_icon);
             if (brandIcon != null) {
                 int iconSize =
                         resources.getDimensionPixelSize(
