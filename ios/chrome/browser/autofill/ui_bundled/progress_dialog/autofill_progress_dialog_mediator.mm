b96a13b98a7362cfeb969fd13428b5f7cbc8d866
yiwenqian <yiwenqian@google.com>
2025-05-20 17:06:28 -0700

    Turn on Hagrid test config flag on IOS
    Bug:417491134, b/416069944
    
    Change-Id: I40fc6ddf616e6e76a984c7059fd1afbe63c644c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509345
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Yiwen Qian <yiwenqian@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463112}

--- ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_mediator.mm
+++ ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_mediator.mm
@@ -18,15 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/alert_view/ui_bundled/alert_action.h"
 #import "ios/chrome/browser/alert_view/ui_bundled/alert_consumer.h"
+#import "ios/chrome/browser/autofill/ui_bundled/autofill_ui_constants.h"
 #import "ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_mediator_delegate.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ui/base/l10n/l10n_util.h"
-
-namespace {
-// The delay between showing the confirmation and dismissing the progress
-// dialog.
-constexpr base::TimeDelta kConfirmationDismissDelay = base::Seconds(1);
-}  // namespace
 
 AutofillProgressDialogMediator::AutofillProgressDialogMediator(
     autofill::AutofillProgressDialogControllerImpl* model_controller,
@@ -65,7 +60,8 @@ void AutofillProgressDialogMediator::Dismiss(
                      weak_ptr_factory_.GetWeakPtr());
 
   base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
-      FROM_HERE, std::move(closure), kConfirmationDismissDelay);
+      FROM_HERE, std::move(closure),
+      autofill_ui_constants::kProgressDialogConfirmationDismissDelay);
 }
 
 void AutofillProgressDialogMediator::InvalidateControllerForCallbacks() {
