e4bed4c8ecf97035c03b9f40e2854f30e77b5cf0
jhtin <jhtin@google.com>
2025-05-21 06:09:01 -0700

    jp systempk: Make each dictionary entry chromevox text unique.
    Will now include a translated "Entry {number}" aria-description text.
    e.g. "Reading... Entry 1"
    
    see screenshot/8uAwypiA2YWhtMm
    
    Fixed: b:418058307
    Change-Id: I5b16ea238e50224c8b1e0e1bcee33f0758d2c2f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6567508
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Jon Htin <jhtin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463361}

--- chrome/browser/resources/ash/settings/os_languages_page/os_japanese_dictionary_entry_row.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/os_japanese_dictionary_entry_row.ts
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/ash/common/cr_elements/cr_input/cr_input.js';
 
 import type {CrInputElement} from 'chrome://resources/ash/common/cr_elements/cr_input/cr_input.js';
+import {I18nMixin} from 'chrome://resources/ash/common/cr_elements/i18n_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import type {JapaneseDictionaryEntry} from '../mojom-webui/user_data_japanese_dictionary.mojom-webui.js';
@@ -29,7 +30,8 @@ interface OsJapaneseDictionaryEntryRowElement {
   };
 }
 
-class OsJapaneseDictionaryEntryRowElement extends PolymerElement {
+class OsJapaneseDictionaryEntryRowElement extends I18nMixin
+(PolymerElement) {
   // LINT.IfChange(JpPosType)
   private readonly posTypeOptions_: DropdownOption[] = [
     {value: JpPosType.kNoPos, label: '品詞なし'},
@@ -200,6 +202,11 @@ class OsJapaneseDictionaryEntryRowElement extends PolymerElement {
   private dispatchSavedEvent_(): void {
     this.dispatchEvent(
         new CustomEvent('dictionary-saved', {bubbles: true, composed: true}));
+  }
+
+  private i18nEntryDescription_(): string {
+    // +1 to the index so that it starts at "1" instead of 0.
+    return this.i18n('japaneseDictionaryEntryPosition', this.index + 1);
   }
 }
 
