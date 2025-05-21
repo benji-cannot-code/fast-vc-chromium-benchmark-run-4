01ee8a3d4eff56ff25aa166bbc76cd93753b2b83
Salvador Guerrero <salg@google.com>
2025-05-21 12:18:08 -0700

    [Dino game] Migrate GameOverPanel class to typescript
    Bug: 373951324
    Change-Id: Ic0f406cb1c3115d99ef8f928d6114531ad2fc945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549135
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463640}

--- components/neterror/resources/dino_game/offline_sprite_definitions.ts
+++ components/neterror/resources/dino_game/offline_sprite_definitions.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {BackgroundElConfig, BackgroundElSpriteConfig} from './background_el.js';
+import type {AltGameEndConfig, AltGameModePanelDimensions} from './game_over_panel.js';
 import type {HorizonLineConfig} from './horizon_line.js';
 import type {SpritePosition} from './sprite_position.js';
 import type {AltGameModeSpriteConfig as AltTrexSpriteDefinition} from './trex.js';
@@ -88,7 +89,8 @@ export interface SpriteDefinition {
   };
   backgroundElConfig: BackgroundElConfig;
   lines: HorizonLineConfig[];
-  altGameOverTextConfig: {[key: string]: number|boolean};
+  altGameOverTextConfig: AltGameModePanelDimensions;
+  altGameEndConfig?: AltGameEndConfig;
 }
 
 interface SpriteDefinitionByType {
@@ -221,12 +223,12 @@ export const spriteDefinitionByType: SpriteDefinitionByType = {
       {sourceX: 2, sourceY: 52, width: 600, height: 12, yPos: 127},
     ],
     altGameOverTextConfig: {
-      TEXT_X: 32,
-      TEXT_Y: 0,
-      TEXT_WIDTH: 246,
-      TEXT_HEIGHT: 17,
-      FLASH_DURATION: 1500,
-      FLASHING: false,
+      textX: 32,
+      textY: 0,
+      textWidth: 246,
+      textHeight: 17,
+      flashDuration: 1500,
+      flashing: false,
     },
   },
 };
