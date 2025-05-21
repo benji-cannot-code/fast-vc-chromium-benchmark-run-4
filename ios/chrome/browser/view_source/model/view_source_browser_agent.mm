3524cc54b7abd420be1158629f21f658f79151c5
Sylvain Defresne <sdefresne@chromium.org>
2025-05-21 06:59:47 -0700

    [ios] Simplify BrowserUserData sub-classes
    Since BrowserUserData stores a pointer to the owning Browser,
    convert sub-classes to use this pointer instead of each of
    them maintaining their own copy of the pointer.
    
    This converts //ios/chrome/browser/view_source directory.
    
    Bug: 409299519
    Change-Id: I275c2ac9f50c05c64882a3e4114f5218038b8b1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6573693
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463388}

--- ios/chrome/browser/view_source/model/view_source_browser_agent.mm
+++ ios/chrome/browser/view_source/model/view_source_browser_agent.mm
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state.h"
 
 ViewSourceBrowserAgent::ViewSourceBrowserAgent(Browser* browser)
-    : BrowserUserData(browser), browser_(browser) {}
+    : BrowserUserData(browser) {}
 
 ViewSourceBrowserAgent::~ViewSourceBrowserAgent() {}
 
