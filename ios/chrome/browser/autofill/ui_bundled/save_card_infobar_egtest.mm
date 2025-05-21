4b33669d1c8cd2fac0e35efb0edce981096751fe
Vidhan Jain <vidhanj@google.com>
2025-05-21 05:43:05 -0700

    [iOS][Gardener] Disable flaky tests in SaveCardInfobarEGTest
    Bug: 419219302
    Change-Id: Ie817ff362108dd0fada5efe8f5af3213aa16210c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574880
    Owners-Override: Vidhan Jain <vidhanj@google.com>
    Auto-Submit: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463344}

--- ios/chrome/browser/autofill/ui_bundled/save_card_infobar_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/save_card_infobar_egtest.mm
@@ -551,7 +551,8 @@ void FillAndSubmitXframeCreditCardForm() {
 // with partial data (only card number and expiration date) and that Google
 // Payments server is queried to request card upload. Due to partial data,
 // instead of bottomsheet, infobar will be shown.
-- (void)testOfferUpstream_PartialData_PaymentsAccepts {
+// TODO(crbug.com/419219302): Test is flaky.
+- (void)DISABLED_testOfferUpstream_PartialData_PaymentsAccepts {
   [self fillAndSubmitFormWithID:kFillPartialFormId
                paymentsResponse:kResponseGetUploadDetailsSuccess
                       errorCode:net::HTTP_OK
@@ -567,7 +568,8 @@ void FillAndSubmitXframeCreditCardForm() {
 
 // Ensures that UMA metrics are correctly logged when the user declines upload
 // on a bottomsheet and an infobar.
-- (void)testUMA_Upstream_UserDeclinesBottomSheetAndInfobar {
+// TODO(crbug.com/419219302): Test is flaky.
+- (void)DISABLED_testUMA_Upstream_UserDeclinesBottomSheetAndInfobar {
   // Form submitted with full credit card data and no previous strikes offers
   // upstream save in a bottomsheet.
   [self fillAndSubmitFormWithID:kFillFullFormId
@@ -639,7 +641,8 @@ void FillAndSubmitXframeCreditCardForm() {
 // Ensures that UMA metrics are correctly logged when the user declines upload
 // on a bottomsheet and accepts when offered infobar. On accept, ensures that an
 // UploadCardRequest RPC is sent to Google Payments Server.
-- (void)testUMA_Upstream_UserDeclinesBottomSheetAcceptsInfobar {
+// TODO(crbug.com/419219302): Test is flaky.
+- (void)DISABLED_testUMA_Upstream_UserDeclinesBottomSheetAcceptsInfobar {
   // Form submitted with full credit card data and no previous strikes offers
   // card upload in a bottomsheet.
   [self fillAndSubmitFormWithID:kFillFullFormId
@@ -727,7 +730,9 @@ void FillAndSubmitXframeCreditCardForm() {
   }
 }
 
-- (void)testSaveCardBottomSheetShowsLoadingAndConfirmationAfterAcceptPushed {
+// TODO(crbug.com/419219302): Test is flaky.
+- (void)
+    DISABLED_testSaveCardBottomSheetShowsLoadingAndConfirmationAfterAcceptPushed {
   [self fillAndSubmitFormWithID:kFillFullFormId
                paymentsResponse:kResponseGetUploadDetailsSuccess
                       errorCode:net::HTTP_OK
