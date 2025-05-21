beee80bb383620be03c592b7a446531ff95445f6
Mike Wasserman <msw@chromium.org>
2025-05-21 13:32:52 -0700

    Summarizer API: Update 'tl;dr' to 'tldr'
    Fixed: 419272071
    Change-Id: Ieb5efb6672caa03fa0e89175acaf37cf30e76b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576261
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463673}

--- third_party/blink/web_tests/external/wpt/ai/summarizer/summarizer-availability-available.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/ai/summarizer/summarizer-availability-available.tentative.https.window.js
@@ -13,7 +13,7 @@ promise_test(async () => {
 
 promise_test(async () => {
   const availability = await Summarizer.availability({
-    type: 'tl;dr',
+    type: 'tldr',
     format: 'plain-text',
     length: 'medium',
     expectedInputLanguages: ['en-GB'],
@@ -25,7 +25,7 @@ promise_test(async () => {
 
 promise_test(async () => {
   const availability = await Summarizer.availability({
-    type: 'tl;dr',
+    type: 'tldr',
     format: 'plain-text',
     length: 'medium',
     expectedInputLanguages: ['es'], // not supported
