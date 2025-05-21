ddceec742e8940682580ee6d9ac1cb20a884fee8
Cheick Cisse <cheickcisse@google.com>
2025-05-21 10:37:25 -0700

    [FET][DeviceStorage] Add feature flag for enabling device storage
    This CL adds a feature flag for the addition of device storage to the FET.
    
    Bug: 417956087
    Change-Id: I1395cd1b54a6f70af19dfc1704786022580aef87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551437
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463547}

--- components/feature_engagement/public/feature_constants.h
+++ components/feature_engagement/public/feature_constants.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace feature_engagement {
 
+// Returns true if adding on-device storage is enabled.
+bool IsOnDeviceStorageEnabled();
+
 #define FEATURE_CONSTANTS_DECLARE_FEATURE(feature_name)  \
   COMPONENT_EXPORT(FEATURE_ENGAGEMENT_FEATURE_CONSTANTS) \
   BASE_DECLARE_FEATURE(feature_name)
