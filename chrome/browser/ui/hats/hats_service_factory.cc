2a01cb20930e3590765f350cc96302ef679ce023
Antonio Gomes <tonikitoo@igalia.com>
2025-05-21 10:57:08 -0700

    Componentize chrome/browser/ui/android/hats/
    This CL continues the effort to componentize the \hats\ directory within
    chrome/browser/ui. It takes care exclusively of the android-specific
    implementation.
    
    R=dljames@chromium.org
    
    Bug: 367929289
    Change-Id: I9134682edaa315fba402bd834884f15ab1aec2c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569134
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463580}

--- chrome/browser/ui/hats/hats_service_factory.cc
+++ chrome/browser/ui/hats/hats_service_factory.cc
@@ -9,9 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/variations/google_groups_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/ui/android/hats/hats_service_android.h"
 #include "chrome/browser/ui/hats/hats_service.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/android/hats/hats_service_android.h"
+#else
 #include "chrome/browser/ui/hats/hats_service_desktop.h"
+#endif
 
 // static
 HatsService* HatsServiceFactory::GetForProfile(Profile* profile,
