2067dddd987fe8719f640fe3f683ad704bda7620
Jan Keitel <jkeitel@google.com>
2025-05-21 05:14:23 -0700

    Modernize equality operators in //ui
    This CL is pure clean-up and contains no functionality changes.
    Depending on the files covered in the CL (since the CL was generated
    using git cl split), it does a subset of the following:
    the following:
    - Remove unneeded operator!= declarations/definitions since C++20 can
      automatically derive those from operator==.
    - Default operator== where this is equivalent to the current behavior.
    - Default operator<=> where this is equivalent to the current
      behavior.
    
    This CL was uploaded by git cl split.
    
    R=mrobinson@igalia.com
    
    AX-Relnotes: n/a
    
    Bug: 40256175
    Change-Id: Ib07164c683ad56cbb5824a810484b3fb93c8275c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6535910
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463330}

--- ui/accessibility/platform/browser_accessibility.h
+++ ui/accessibility/platform/browser_accessibility.h
@@ -235,7 +235,6 @@ class COMPONENT_EXPORT(AX_PLATFORM) BrowserAccessibility
       bool operator==(const Iterator& rhs) const {
         return parent_ == rhs.parent_ && index_ == rhs.index_;
       }
-      bool operator!=(const Iterator& rhs) const { return !operator==(rhs); }
       const BrowserAccessibility* operator*();
 
      private:
