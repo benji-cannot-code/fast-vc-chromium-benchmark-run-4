3c87082d10a050cdc5eeb5da1c2ff3b17ba2df8e
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-05-21 09:33:37 -0700

    Import wpt@8d2e9252aad5b5479c9699df064605dd267036cc
    https://github.com/web-platform-tests/wpt/compare/c12b79f80...8d2e9252a
    
    Using wpt-import in Chromium aa34d9d06f7040803bffdd58dcfa0c8793735b41.
    With Chromium commits locally applied on WPT:
      af3dd6b264 "[ident] Support ident() for anchor names"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    mkwst@chromium.org:
      external/wpt/subresource-integrity
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Id45d0aaf92bd66a6a61852899b2c479047e0eddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575300
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1463508}

--- third_party/blink/web_tests/external/wpt/custom-elements/registries/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/custom-elements/registries/idlharness.window.js
@@ -1,10 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
-// META: global=window,dedicatedworker,shadowrealm-in-window
 
 idl_test(
-  ["scoped-custom-elements-registry.tentative"],
+  ["scoped-custom-elements-registry"],
   ["html", "dom"],
   (idl_array) => {
     let element = document.createElement("div");
