227ff9e26d2918677b7e031569c0319d48a4b75c
Ryan Sultanem <rsult@google.com>
2025-05-21 06:01:15 -0700

    [B4B] Do not show upload icon in Signin Pending
    Hide the upload button for profiles that are in Signin Pending.
    Actually hide the button for any state that is not Signed in.
    
    Bug: 419025008
    Change-Id: I330a4aaf12c4cc7928ba95efe5c03a2fad88e00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569312
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463352}

--- chrome/browser/ui/webui/bookmarks/bookmarks_message_handler.cc
+++ chrome/browser/ui/webui/bookmarks/bookmarks_message_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "base/not_fatal_until.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/batch_upload/batch_upload_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_account_storage_move_dialog.h"
@@ -166,6 +168,8 @@ void BookmarksMessageHandler::RegisterMessages() {
 
 void BookmarksMessageHandler::OnJavascriptAllowed() {
   Profile* profile = Profile::FromWebUI(web_ui());
+  CHECK(!profile->IsGuestSession(),
+        base::NotFatalUntil(base::NotFatalUntil::M140));
   pref_change_registrar_.Init(profile->GetPrefs());
   pref_change_registrar_.Add(
       policy::policy_prefs::kIncognitoModeAvailability,
@@ -249,6 +253,14 @@ bool BookmarksMessageHandler::CanUploadBookmarkToAccountStorage(
   // Incognito profile should not show the upload button. The action is
   // possible, but it should not be promoted.
   if (profile->IsOffTheRecord()) {
+    return false;
+  }
+
+  // Identity manager should always be valid since Incognito and Guest mode are
+  // filtered out above.
+  // Only signed in users may see the upload button.
+  if (signin_util::GetSignedInState(IdentityManagerFactory::GetForProfile(
+          profile)) != signin_util::SignedInState::kSignedIn) {
     return false;
   }
 
