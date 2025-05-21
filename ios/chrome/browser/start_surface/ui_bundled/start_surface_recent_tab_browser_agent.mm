f95803ad89b6c5629b301536f4de8b9f9fe96418
Sylvain Defresne <sdefresne@chromium.org>
2025-05-21 07:21:19 -0700

    [ios] Simplify BrowserUserData sub-classes
    Since BrowserUserData stores a pointer to the owning Browser,
    convert sub-classes to use this pointer instead of each of
    them maintaining their own copy of the pointer.
    
    This converts //ios/chrome/browser/start_surface directory.
    
    Bug: 409299519
    Change-Id: Ic2c5529ce5dcd57ba5047a681675047a08d73f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6573954
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463405}

--- ios/chrome/browser/start_surface/ui_bundled/start_surface_recent_tab_browser_agent.mm
+++ ios/chrome/browser/start_surface/ui_bundled/start_surface_recent_tab_browser_agent.mm
@@ -16,9 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 StartSurfaceRecentTabBrowserAgent::StartSurfaceRecentTabBrowserAgent(
     Browser* browser)
-    : BrowserUserData(browser),
-      favicon_driver_observer_(this),
-      browser_(browser) {
+    : BrowserUserData(browser), favicon_driver_observer_(this) {
   browser_->AddObserver(this);
   browser_->GetWebStateList()->AddObserver(this);
 }
