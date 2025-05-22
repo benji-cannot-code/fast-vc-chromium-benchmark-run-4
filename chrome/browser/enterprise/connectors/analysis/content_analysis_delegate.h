2a93f1bbed3dd61f36d9f9dacd3ddbe62e7578eb
Haihan Chen <haihan@google.com>
2025-05-22 07:57:25 -0700

    [IFrame DLP Rules Support] Populate frame_url_chain for upload content analysis requests
    Add the chain of nested iFrames for upload trigger. Other triggers will be added later.
    
    This is entirely gated by a finch flag, will have subsequent CLs for unit tests/browsertests and UMA metrics.
    
    Bug: 397480137
    Change-Id: I269cbbabd2d73e8f839004136f35ad355b9bf742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575500
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464094}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.h
@@ -284,6 +284,8 @@ class ContentAnalysisDelegate : public ContentAnalysisDelegateBase,
   ContentAnalysisRequest::Reason reason() const override;
   google::protobuf::RepeatedPtrField<::safe_browsing::ReferrerChainEntry>
   referrer_chain() const override;
+  google::protobuf::RepeatedPtrField<std::string> frame_url_chain()
+      const override;
 
  protected:
   ContentAnalysisDelegate(content::WebContents* web_contents,
@@ -406,6 +408,9 @@ class ContentAnalysisDelegate : public ContentAnalysisDelegateBase,
 
   // The GURL corresponding to the page where the scan triggered.
   GURL url_;
+
+  // Parent URL chain of the frame from which the action was triggered.
+  google::protobuf::RepeatedPtrField<std::string> frame_url_chain_;
 
   // The title corresponding to the WebContents triggering the scan.
   std::string title_;
--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate_unittest.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate_unittest.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/buildflags/buildflags.h"
 #include "components/enterprise/common/proto/connectors.pb.h"
 #include "components/enterprise/connectors/core/analysis_settings.h"
+#include "components/enterprise/connectors/core/features.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/safe_browsing/core/common/features.h"
@@ -136,8 +137,10 @@ class BaseTest : public testing::Test {
     EXPECT_TRUE(profile_manager_.SetUp());
     profile_ = profile_manager_.CreateTestingProfile("test-user");
     ContentAnalysisDelegate::DisableUIForTesting();
-    scoped_feature_list_.InitAndEnableFeature(
-        safe_browsing::kEnhancedFieldsForSecOps);
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{safe_browsing::kEnhancedFieldsForSecOps,
+                              kEnterpriseIframeDlpRulesSupport},
+        /*disabled_features=*/{});
   }
 
   void ScanUpload(content::WebContents* web_contents,
