9489cfa68607991b85482f40b10d0bd99ec991eb
Tomasz Jurkiewicz <tju@google.com>
2025-05-22 08:27:02 -0700

    Enable supervision in tests as it's done in prod
    The current way of enabling supervision flips the incognito flag as
    intended, but as byproduct it's also putting the system in unexpected
    state: the url filtering preferences are also set, but supervision is
    not enabled.
    
    After this change, the whole supervision stack is correctly enabled,
    including the asserted incognito mode.
    
    Bug: 419438027
    Change-Id: I05e3ce306f162a782a2916559bf39de80e72fd5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574644
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464118}

--- chrome/browser/signin/chromeos_mirror_account_consistency_browsertest.cc
+++ chrome/browser/signin/chromeos_mirror_account_consistency_browsertest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/supervised_user/supervised_user_settings_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/account_id/account_id.h"
@@ -22,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_header_helper.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
-#include "components/supervised_user/core/browser/supervised_user_settings_service.h"
+#include "components/supervised_user/core/browser/supervised_user_preferences.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/test/browser_test.h"
@@ -126,21 +125,14 @@ IN_PROC_BROWSER_TEST_F(ChromeOsMirrorAccountConsistencyTest,
   signin::WaitForRefreshTokensLoaded(
       IdentityManagerFactory::GetForProfile(profile));
 
-  supervised_user::SupervisedUserSettingsService*
-      supervised_user_settings_service =
-          SupervisedUserSettingsServiceFactory::GetForKey(
-              profile->GetProfileKey());
-  supervised_user_settings_service->SetActive(true);
-
-  // Incognito is always disabled for child accounts.
-  PrefService* prefs = profile->GetPrefs();
-  prefs->SetInteger(
-      policy::policy_prefs::kIncognitoModeAvailability,
-      static_cast<int>(policy::IncognitoModeAvailability::kDisabled));
+  // EnableParentalControls is called when the browser discovers a child
+  // account. Among other thins, this disables the incognito mode.
+  supervised_user::EnableParentalControls(*profile->GetPrefs());
   ASSERT_EQ(1, signin::PROFILE_MODE_INCOGNITO_DISABLED);
 
   // TODO(http://crbug.com/1134144): This test seems to test supervised profiles
-  // instead of child accounts. With the current implementation,
+  // instead of child accounts (but the EnableParentalControls call closely
+  // simulates child account). With the current implementation,
   // X-Chrome-Connected header gets a supervised=true argument only for child
   // profiles. Verify if these tests needs to be updated to use child accounts
   // or whether supervised profiles need to be supported as well.
