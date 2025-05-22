af1e5cd730c2c995c9be0fe8c244fbc41a043ed7
Adem Derinel <derinel@google.com>
2025-05-22 06:31:58 -0700

    WebAuthn: Introduce early timeout for immediate requests
    This change terminates the immediate mediation within 500ms if the UI
    has not been shown yet by the delegate. The duration is configurable via
    Finch, if needed.
    
    Bug: 393055190
    Change-Id: Ibb1f195de192c0e85833f94a9058b190c0f83aea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565544
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464045}

--- chrome/browser/password_manager/chrome_webauthn_credentials_delegate_unittest.cc
+++ chrome/browser/password_manager/chrome_webauthn_credentials_delegate_unittest.cc
@@ -134,7 +134,7 @@ class ChromeWebAuthnCredentialsDelegateTest
     authenticator_request_delegate_->RegisterActionCallbacks(
         base::DoNothing(), base::DoNothing(), base::DoNothing(),
         base::DoNothing(), base::DoNothing(), base::DoNothing(),
-        base::DoNothing(), base::DoNothing());
+        base::DoNothing(), base::DoNothing(), base::DoNothing());
 #else
     delegate_ =
         WebAuthnRequestDelegateAndroid::GetRequestDelegate(web_contents());
--- chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
@@ -688,10 +688,10 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, FilterGoogleComPasskeys) {
 
     ChromeAuthenticatorRequestDelegate delegate(main_rfh());
     delegate.SetRelyingPartyId(test.rp_id);
-    delegate.RegisterActionCallbacks(base::DoNothing(), base::DoNothing(),
-                                     base::DoNothing(), base::DoNothing(),
-                                     base::DoNothing(), base::DoNothing(),
-                                     base::DoNothing(), base::DoNothing());
+    delegate.RegisterActionCallbacks(
+        base::DoNothing(), base::DoNothing(), base::DoNothing(),
+        base::DoNothing(), base::DoNothing(), base::DoNothing(),
+        base::DoNothing(), base::DoNothing(), base::DoNothing());
     delegate.OnTransportAvailabilityEnumerated(std::move(data));
 
     EXPECT_EQ(result.has_platform_authenticator_credential,
@@ -738,10 +738,10 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest,
 
   ChromeAuthenticatorRequestDelegate delegate(main_rfh());
   delegate.SetRelyingPartyId(kGoogleRpId);
-  delegate.RegisterActionCallbacks(base::DoNothing(), base::DoNothing(),
-                                   base::DoNothing(), base::DoNothing(),
-                                   base::DoNothing(), base::DoNothing(),
-                                   base::DoNothing(), base::DoNothing());
+  delegate.RegisterActionCallbacks(
+      base::DoNothing(), base::DoNothing(), base::DoNothing(),
+      base::DoNothing(), base::DoNothing(), base::DoNothing(),
+      base::DoNothing(), base::DoNothing(), base::DoNothing());
   delegate.OnTransportAvailabilityEnumerated(std::move(data));
 
   // Despite lacking the user ID prefix, credentials are not filtered from
@@ -954,10 +954,10 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest,
   auto raw_password_controller = password_controller.get();
   delegate.SetPasswordControllerForTesting(std::move(password_controller));
   base::MockCallback<base::OnceClosure> mock_closure;
-  delegate.RegisterActionCallbacks(base::DoNothing(), mock_closure.Get(),
-                                   base::DoNothing(), base::DoNothing(),
-                                   base::DoNothing(), base::DoNothing(),
-                                   base::DoNothing(), base::DoNothing());
+  delegate.RegisterActionCallbacks(
+      base::DoNothing(), mock_closure.Get(), base::DoNothing(),
+      base::DoNothing(), base::DoNothing(), base::DoNothing(),
+      base::DoNothing(), base::DoNothing(), base::DoNothing());
   delegate.SetUIPresentation(UIPresentation::kModalImmediate);
   delegate.SetCredentialTypes(kRequestPassword | kRequestPublicKey);
   delegate.SetRelyingPartyId(kRpId);
@@ -998,10 +998,10 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest,
   auto raw_password_controller = password_controller.get();
   delegate.SetPasswordControllerForTesting(std::move(password_controller));
   base::MockCallback<base::OnceClosure> mock_closure;
-  delegate.RegisterActionCallbacks(base::DoNothing(), mock_closure.Get(),
-                                   base::DoNothing(), base::DoNothing(),
-                                   base::DoNothing(), base::DoNothing(),
-                                   base::DoNothing(), base::DoNothing());
+  delegate.RegisterActionCallbacks(
+      base::DoNothing(), mock_closure.Get(), base::DoNothing(),
+      base::DoNothing(), base::DoNothing(), base::DoNothing(),
+      base::DoNothing(), base::DoNothing(), base::DoNothing());
   delegate.SetUIPresentation(UIPresentation::kModalImmediate);
   delegate.SetCredentialTypes(kRequestPassword | kRequestPublicKey);
   delegate.SetRelyingPartyId(kRpId);
@@ -1065,6 +1065,7 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, ImmediateMediationRateLimit) {
       /*account_preselected_callback=*/base::DoNothing(),
       /*password_selected_callback=*/base::DoNothing(),
       /*request_callback=*/base::DoNothing(),
+      /*cancel_ui_timeout_callback=*/base::DoNothing(),
       /*bluetooth_adapter_power_on_callback=*/base::DoNothing(),
       /*bluetooth_query_status_callback=*/base::DoNothing());
 
