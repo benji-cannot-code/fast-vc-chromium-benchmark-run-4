28f6c9ebe33cca93e7be6b279f11c9296005a872
Johannes Kron <kron@chromium.org>
2025-05-22 02:14:53 -0700

    Add support for kLoopbackWithoutChromeId to CatapAudioInputStream
    `kLoopbackWithoutChromeId` is used to specify that Chrome's own
    audio should be excluded from the capture.
    
    Search for all audio object IDs that belong to the Chrome audio
    service and exclude these in the capture configuration.
    
    Bug: 415953671
    Change-Id: Ice85bc590dd95e7a14aa6e10fe397581f1fc0c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6523462
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463958}

--- media/audio/mac/catap_api.h
+++ media/audio/mac/catap_api.h
@@ -26,6 +26,12 @@ class CatapApi {
       AudioDeviceIOProc proc,
       void* in_client_data,
       AudioDeviceIOProcID* out_proc_id) = 0;
+  virtual OSStatus AudioObjectGetPropertyDataSize(
+      AudioObjectID in_object_id,
+      const AudioObjectPropertyAddress* in_address,
+      UInt32 in_qualifier_data_size,
+      const void* in_qualifier_data,
+      UInt32* out_data_size) = 0;
   virtual OSStatus AudioObjectGetPropertyData(
       AudioObjectID in_object_id,
       const AudioObjectPropertyAddress* in_address,
@@ -70,6 +76,12 @@ class API_AVAILABLE(macos(14.2)) CatapApiImpl : public CatapApi {
                                      AudioDeviceIOProc proc,
                                      void* in_client_data,
                                      AudioDeviceIOProcID* out_proc_id) override;
+  OSStatus AudioObjectGetPropertyDataSize(
+      AudioObjectID in_object_id,
+      const AudioObjectPropertyAddress* in_address,
+      UInt32 in_qualifier_data_size,
+      const void* in_qualifier_data,
+      UInt32* out_data_size) override;
   OSStatus AudioObjectGetPropertyData(
       AudioObjectID in_object_id,
       const AudioObjectPropertyAddress* in_address,
