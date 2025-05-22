2936b0539ded89fbfb515e3b1a8b6b712f580770
Aashna Sheth <aashnas@google.com>
2025-05-22 14:47:33 -0700

    Add signin sync check to DidFinishNavigation
    Bug: b:408016824
    Change-Id: I331fa9baca5a347cbba607db1ffcc2f836f5cd4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576541
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464380}

--- chrome/browser/privacy_sandbox/notice/desktop_entrypoint_handlers.cc
+++ chrome/browser/privacy_sandbox/notice/desktop_entrypoint_handlers.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/privacy_sandbox/notice/desktop_entrypoint_handlers_helper.h"
 #include "chrome/browser/privacy_sandbox/notice/desktop_view_manager.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/profiles/profile_customization_bubble_sync_controller.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/tabs/public/tab_interface.h"
 #include "components/web_modal/web_contents_modal_dialog_host.h"
@@ -75,6 +77,22 @@ void NavigationHandler::HandleNewNavigation(
       browser_window_interface->GetWebContentsModalDialogHostForWindow();
   if (!web_contents || web_contents->GetMaximumDialogSize().height() <
                            kMinRequiredDialogHeight) {
+    return;
+  }
+
+  // If a sign-in dialog is being currently displayed or is about to be
+  // displayed, the prompt should not be shown to avoid conflict.
+  // TODO(crbug.com/370806609): When we add sign in notice to queue, put this
+  // behind flag / remove.
+  auto* browser = browser_window_interface->GetBrowserForMigrationOnly();
+  bool signin_dialog_showing =
+      browser->signin_view_controller()->ShowsModalDialog();
+#if !BUILDFLAG(IS_CHROMEOS)
+  signin_dialog_showing =
+      signin_dialog_showing ||
+      IsProfileCustomizationBubbleSyncControllerRunning(browser);
+#endif  // !BUILDFLAG(IS_CHROMEOS)
+  if (signin_dialog_showing) {
     return;
   }
 
