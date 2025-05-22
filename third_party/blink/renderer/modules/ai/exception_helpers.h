2ab11cbc6c8c0f43e45d58bd92289283ff2613bd
Brad Triebwasser <btriebw@chromium.org>
2025-05-21 17:09:25 -0700

    Reland "Add more supported bitmap types for LanguageModel Prompt API"
    This reverts commit 3c69e5a3cd70bcc6c3ebd0058410e91609c8e1c6.
    
    Reason for revert: Fixed memory leak
    
    Bug: 414906618
    Original change's description:
    > Revert "Add more supported bitmap types for LanguageModel Prompt API"
    >
    > This reverts commit 8e10a77ce4786031b354cd58acb277ac15fba66e.
    >
    > Reason for revert: Likely culprit for failing test,
    > wpt_internal/ai/language-model-api-prompt-context-destroyed.https.window.html,
    > which has failed in 2 builds starting on submit of this change.
    >
    > Bug: 414906618, 418812441
    > Original change's description:
    > > Add more supported bitmap types for LanguageModel Prompt API
    > >
    > > Adds Blob and ArrayBuffer support for images.
    > >
    > > Adds bitmap tests for Blob, ImageData, ImageBitmap, VideoFrame,
    > > OffscreenCanvas, HTMLCanvasElement and ArrayBuffer.
    > >
    > > Change-Id: I2ba9c851257ed4faeac2251c84c9d6dc53250c3e
    > > Bug: 414906618
    > > Fixed: 417797201
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534099
    > > Auto-Submit: Brad Triebwasser <btriebw@chromium.org>
    > > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > > Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    > > Commit-Queue: Mike Wasserman <msw@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1462327}
    >
    > Bug: 414906618
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ic44439a9b7c571926668c385d0aeb0a17a7faaa4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565163
    > Owners-Override: Katie Dektar <katie@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Katie Dektar <katie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1462376}
    
    Bug: 414906618, 418812441
    Bug: 414906618
    Change-Id: I78af50b5509878d2e0cf93eab7dd3ed93140c7ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575184
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463803}

--- third_party/blink/renderer/modules/ai/exception_helpers.h
+++ third_party/blink/renderer/modules/ai/exception_helpers.h
@@ -40,6 +40,7 @@ void ThrowAbortedException(ExceptionState& exception_state);
 void RejectPromiseWithInternalError(ScriptPromiseResolverBase* resolver);
 
 DOMException* CreateInternalErrorException();
+DOMException* CreateSessionDestroyedException();
 
 DOMException* ConvertModelStreamingResponseErrorToDOMException(
     ModelStreamingResponseStatus error);
