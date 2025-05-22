69cd56573354d4fe7c8b6bef42fa0918a66f6313
Sam Davis Omekara (from Dev Box) <samomekarajr@microsoft.com>
2025-05-22 16:30:47 -0700

    [Gap Decorations]: Implement Bidirectional rule shorthand
    This CL introduces the `rule` shorthand as part of the gap decorations
    feature [1]. The `rule` shorthand sets the `column-rule` and `row-rule`
    to the same value.
    
    The CL augments `css_parsing_utils::ConsumeGapDecorationsRuleShorthand`
    to accept corresponding width, style, and color values for hydration
    upon successful parsing. The task of adding the properties has been
    moved to the caller, not the consumption function. This approach
    simplifies adding properties for bidirectional rules and maintains the
    same convention as the Border shorthand.
    
    [1]: https://drafts.csswg.org/css-gaps-1/#propdef-rule
    
    Bug: 357648037
    Change-Id: I9ac1993e599236e99f018e458b6e38f2d437668e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543211
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1464436}

--- third_party/blink/renderer/core/css/css_gap_decoration_property_utils.h
+++ third_party/blink/renderer/core/css/css_gap_decoration_property_utils.h
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_GAP_DECORATION_PROPERTY_UTILS_H_
 
 #include "third_party/blink/renderer/core/css/css_property_names.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 
 namespace blink {
 
+class CSSPropertyValue;
+class CSSValueList;
 template <typename T>
 class GapDataList;
 class StyleColor;
@@ -37,6 +40,12 @@ class CORE_EXPORT CSSGapDecorationUtils {
       CSSGapDecorationPropertyType type);
   static CSSPropertyID GetShorthandProperty(
       CSSGapDecorationPropertyDirection direction);
+  static void AddProperties(CSSGapDecorationPropertyDirection direction,
+                            const CSSValueList& rule_widths,
+                            const CSSValueList& rule_styles,
+                            const CSSValueList& rule_colors,
+                            bool important,
+                            HeapVector<CSSPropertyValue, 64>& properties);
 };
 
 }  // namespace blink
--- third_party/blink/renderer/core/css/css_property_equality.cc
+++ third_party/blink/renderer/core/css/css_property_equality.cc
@@ -1294,6 +1294,7 @@ bool CSSPropertyEquality::PropertiesEqual(const PropertyHandle& property,
     case CSSPropertyID::kPlaceSelf:
     case CSSPropertyID::kPositionTry:
     case CSSPropertyID::kRowRule:
+    case CSSPropertyID::kRule:
     case CSSPropertyID::kRuleColor:
     case CSSPropertyID::kRuleWidth:
     case CSSPropertyID::kRuleStyle:
