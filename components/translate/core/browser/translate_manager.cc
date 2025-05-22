0190ddadb290f40e12b99e4483b13c63a480adfd
Aaron Selya <selya@google.com>
2025-05-22 13:57:06 -0700

    [Code Health] Remove Feature Flag: Translate.
    This removes the feature flag translate which has been enabled by default for almost 4 years.
    
    Bug: 356624303
    Change-Id: I5a45b913466a0e7153bf26f451c89cdef9c5df5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578953
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464348}

--- components/translate/core/browser/translate_manager.cc
+++ components/translate/core/browser/translate_manager.cc
@@ -159,15 +159,6 @@ void TranslateManager::InitiateTranslation(const std::string& page_lang) {
 bool TranslateManager::CanManuallyTranslate(bool menuLogging) {
   bool can_translate = true;
 
-  if (!base::FeatureList::IsEnabled(translate::kTranslate)) {
-    if (!menuLogging)
-      return false;
-    TranslateBrowserMetrics::ReportMenuTranslationUnavailableReason(
-        TranslateBrowserMetrics::MenuTranslationUnavailableReason::
-            kTranslateDisabled);
-    can_translate = false;
-  }
-
   if (net::NetworkChangeNotifier::IsOffline()) {
     if (!menuLogging)
       return false;
@@ -678,8 +669,7 @@ void TranslateManager::SetIgnoreMissingKeyForTesting(bool ignore) {
 // static
 bool TranslateManager::IsAvailable(const TranslatePrefs* prefs) {
   // These conditions mirror the conditions in InitiateTranslation.
-  return base::FeatureList::IsEnabled(translate::kTranslate) &&
-         (ignore_missing_key_for_testing_ ||
+  return (ignore_missing_key_for_testing_ ||
           ::google_apis::HasAPIKeyConfigured()) &&
          prefs->IsOfferTranslateEnabled();
 }
@@ -822,12 +812,6 @@ void TranslateManager::FilterIsTranslatePossible(
     decision->PreventAllTriggering();
     GetActiveTranslateMetricsLogger()->LogTriggerDecision(
         TriggerDecision::kDisabledDoesntNeedTranslation);
-  }
-
-  if (!base::FeatureList::IsEnabled(translate::kTranslate)) {
-    decision->PreventAllTriggering();
-    GetActiveTranslateMetricsLogger()->LogTriggerDecision(
-        TriggerDecision::kDisabledTranslationFeatureDisabled);
   }
 
   // Also, skip if the connection is currently offline - initiation doesn't make
--- components/translate/core/browser/translate_prefs.cc
+++ components/translate/core/browser/translate_prefs.cc
@@ -127,8 +127,6 @@ BASE_FEATURE(kTranslateRecentTarget,
              "TranslateRecentTarget",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kTranslate, "Translate", base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kMigrateAlwaysTranslateLanguagesFix,
              "MigrateAlwaysTranslateLanguagesFix",
              base::FEATURE_ENABLED_BY_DEFAULT);
