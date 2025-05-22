bed841b834788b6d2a56255215221e8f3ee035f0
Christoph Schwering <schwering@google.com>
2025-05-22 12:54:31 -0700

    [Autofill] Use base::span in determine_possible_field_types.{h,cc}
    Bug: 40100455
    Change-Id: If0d03c35dced7fc9c50810807c37787665319d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578584
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464320}

--- components/autofill/core/browser/crowdsourcing/determine_possible_field_types.cc
+++ components/autofill/core/browser/crowdsourcing/determine_possible_field_types.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_set.h"
+#include "base/containers/span.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
@@ -36,7 +37,7 @@ namespace {
 
 // Finds the first field in |form_structure| with |field.value|=|value|.
 AutofillField* FindFirstFieldWithValue(const FormStructure& form_structure,
-                                       const std::u16string& value) {
+                                       std::u16string_view value) {
   for (const auto& field : form_structure) {
     std::u16string trimmed_value;
     base::TrimWhitespace(field->value_for_import(), base::TRIM_ALL,
@@ -120,7 +121,7 @@ AutofillField* HeuristicallyFindCVCFieldForUpload(
 // search for the CVC field if any.
 AutofillField* GetBestPossibleCVCFieldForUpload(
     const FormStructure& form_structure,
-    std::u16string last_unlocked_credit_card_cvc) {
+    std::u16string_view last_unlocked_credit_card_cvc) {
   if (!last_unlocked_credit_card_cvc.empty()) {
     AutofillField* result =
         FindFirstFieldWithValue(form_structure, last_unlocked_credit_card_cvc);
@@ -129,7 +130,6 @@ AutofillField* GetBestPossibleCVCFieldForUpload(
     }
     return result;
   }
-
   return HeuristicallyFindCVCFieldForUpload(form_structure);
 }
 
@@ -139,9 +139,9 @@ AutofillField* GetBestPossibleCVCFieldForUpload(
 // found.
 void FindAndSetPossibleFieldTypesForField(
     AutofillField& field,
-    const std::vector<AutofillProfile>& profiles,
-    const std::vector<CreditCard>& credit_cards,
-    const std::vector<LoyaltyCard>& loyalty_cards,
+    base::span<const AutofillProfile> profiles,
+    base::span<const CreditCard> credit_cards,
+    base::span<const LoyaltyCard> loyalty_cards,
     const std::set<FieldGlobalId> fields_that_match_state,
     const std::string& app_locale) {
   std::u16string value_u16 = field.value_for_import();
@@ -185,11 +185,11 @@ void FindAndSetPossibleFieldTypesForField(
 // cards and sets the field's possible types accordingly. Special heuristics are
 // run for finding the CVC field.
 void FindAndSetPossibleFieldTypes(
-    const std::vector<AutofillProfile>& profiles,
-    const std::vector<CreditCard>& credit_cards,
-    const std::vector<LoyaltyCard>& loyalty_cards,
+    base::span<const AutofillProfile> profiles,
+    base::span<const CreditCard> credit_cards,
+    base::span<const LoyaltyCard> loyalty_cards,
     const std::set<FieldGlobalId> fields_that_match_state,
-    const std::u16string& last_unlocked_credit_card_cvc,
+    std::u16string_view last_unlocked_credit_card_cvc,
     const std::string& app_locale,
     FormStructure& form) {
   for (size_t i = 0; i < form.field_count(); ++i) {
@@ -237,7 +237,7 @@ std::vector<std::u16string> GetMatchingCompleteDateFormats(
 }  // namespace
 
 std::set<FieldGlobalId> PreProcessStateMatchingTypes(
-    const std::vector<AutofillProfile>& profiles,
+    base::span<const AutofillProfile> profiles,
     const FormStructure& form_structure,
     const std::string& app_locale) {
   std::set<FieldGlobalId> fields_that_match_state;
@@ -274,11 +274,11 @@ std::set<FieldGlobalId> PreProcessStateMatchingTypes(
 }
 
 void DeterminePossibleFieldTypesForUpload(
-    const std::vector<AutofillProfile>& profiles,
-    const std::vector<CreditCard>& credit_cards,
-    const std::vector<LoyaltyCard>& loyalty_cards,
+    base::span<const AutofillProfile> profiles,
+    base::span<const CreditCard> credit_cards,
+    base::span<const LoyaltyCard> loyalty_cards,
     const std::set<FieldGlobalId>& fields_that_match_state,
-    const std::u16string& last_unlocked_credit_card_cvc,
+    std::u16string_view last_unlocked_credit_card_cvc,
     const std::string& app_locale,
     FormStructure& form) {
   for (const std::unique_ptr<AutofillField>& field : form) {
