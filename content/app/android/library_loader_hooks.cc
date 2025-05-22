6ea8c6fcb81cf7f9d3fe6f09d70cea477115d5ff
Sam Maier <smaier@chromium.org>
2025-05-22 11:42:13 -0700

    Deleting unused Java env parameter from library loader
    Bug: 411426303
    Change-Id: I0534e732928af45feeabb93b6fe306e836a2d4e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579084
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464272}

--- content/app/android/library_loader_hooks.cc
+++ content/app/android/library_loader_hooks.cc
@@ -16,9 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-bool LibraryLoaded(JNIEnv* env,
-                   jclass clazz,
-                   base::android::LibraryProcessType library_process_type) {
+bool LibraryLoaded(base::android::LibraryProcessType library_process_type) {
   // Android's main browser loop is custom so we set the browser name here as
   // early as possible if this is the browser process or main webview process.
   if (library_process_type ==
