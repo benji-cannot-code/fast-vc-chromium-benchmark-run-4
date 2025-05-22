727c65de01a6fa81e929390b403dc051416a8d54
Chris Fredrickson <cfredric@chromium.org>
2025-05-22 06:12:33 -0700

    Remove unnecessary alias; construct flat_map efficiently
    This `using` alias is now only used in one place, so there's no real
    benefit to having it. Also, it's inefficient to construct flat_maps via
    repeated insertions; it's better to construct them all at once, to avoid
    unnecessary reallocations. MakeFlatMap is a useful helper for that.
    
    https://crsrc.org/c/base/containers/flat_map.h;drc=4f6cc657b0953fb353111d581c130858e90aec08;l=60
    
    Change-Id: I301cd7bfb3b477ac31e3d4150477f03416d7d463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576240
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464039}

--- content/public/browser/permission_overrides.cc
+++ content/public/browser/permission_overrides.cc
@@ -5,12 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/permission_overrides.h"
 
+#include "base/containers/contains.h"
 #include "base/types/optional_ref.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 
 namespace content {
-using PermissionOverridesMap =
-    base::flat_map<blink::PermissionType, blink::mojom::PermissionStatus>;
 using PermissionStatus = blink::mojom::PermissionStatus;
 
 PermissionOverrides::PermissionOverrides() = default;
@@ -62,13 +61,13 @@ void PermissionOverrides::Reset(base::optional_ref<const url::Origin> origin) {
 void PermissionOverrides::GrantPermissions(
     base::optional_ref<const url::Origin> origin,
     const std::vector<blink::PermissionType>& permissions) {
-  const std::vector<blink::PermissionType>& kAllPermissionTypes =
-      blink::GetAllPermissionTypes();
-  PermissionOverridesMap granted_overrides;
-  for (const auto& permission : kAllPermissionTypes)
-    granted_overrides[permission] = PermissionStatus::DENIED;
-  for (const auto& permission : permissions)
-    granted_overrides[permission] = PermissionStatus::GRANTED;
+  const auto granted_overrides =
+      base::MakeFlatMap<blink::PermissionType, PermissionStatus>(
+          blink::GetAllPermissionTypes(), {}, [&](blink::PermissionType type) {
+            return std::make_pair(type, base::Contains(permissions, type)
+                                            ? PermissionStatus::GRANTED
+                                            : PermissionStatus::DENIED);
+          });
   Reset(origin);
   for (const auto& setting : granted_overrides)
     Set(origin, setting.first, setting.second);
