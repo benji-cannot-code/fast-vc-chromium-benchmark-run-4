3aa2a3857b27465cb83a64a14ead98d7fba4fce4
Kaan Alsan <alsan@chromium.org>
2025-05-22 12:03:37 -0700

    Re-enable MultiContentsView DnD tests for Mac 12 and 13
    The tests have been flaky on Mac 11, 14, and 15. This CL re-enables the
    tests on some versions to ensure there's non-zero coverage.
    
    Bug: 414590951
    Change-Id: Idcc022a521a4fb9f876cdf3f6cbfa838750e9f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568997
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464286}

--- chrome/browser/ui/views/frame/multi_contents_view_interactive_uitest.cc
+++ chrome/browser/ui/views/frame/multi_contents_view_interactive_uitest.cc
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/views/test/views_test_utils.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kNewTab);
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSecondTab);
 
@@ -475,6 +479,15 @@ IN_PROC_BROWSER_TEST_F(MultiContentsViewUiTest,
 // on various platforms. These should be re-enabled as support is added.
 #if !BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_CHROMEOS)
 
+// DnD is flaky on some Mac versions. This allows us to at least have non-zero
+// coverage.
+#define MAYBE_SKIP_FOR_MAC_VERSION()                                          \
+  if (auto version = base::mac::MacOSMajorVersion();                          \
+      version != 12 && version != 13) {                                       \
+    GTEST_SKIP()                                                              \
+        << "DND testing is flaky on some Mac versions (crbug.com/415251142)"; \
+  }
+
 gfx::Point PointForDropTargetFromView(views::View* view) {
   return view->GetBoundsInScreen().right_center() - gfx::Vector2d(10, 0);
 }
@@ -575,10 +588,9 @@ class MultiContentsViewDragEntrypointsUiTest : public MultiContentsViewUiTest {
   net::EmbeddedTestServer http_server_;
 };
 
-// TODO(crbug.com/414590951): This test has been flaky on some MacOS versions,
-// and DnD testing isn't well-supported for other platforms.
 IN_PROC_BROWSER_TEST_F(MultiContentsViewDragEntrypointsUiTest,
-                       DISABLED_ShowsDropTargetOnLinkDragged) {
+                       ShowsDropTargetOnLinkDragged) {
+  MAYBE_SKIP_FOR_MAC_VERSION();
   RunTestSequence(
       AddInstrumentedTab(kNewTab, GetURL("/links.html"), 0),
       CheckTabIsActive(0),
@@ -635,10 +647,9 @@ class MultiContentsViewBookmarkDragEntrypointsUiTest
   }
 };
 
-// TODO(crbug.com/414590951): This test has been flaky on some MacOS versions,
-// and DnD testing isn't well-supported for other platforms.
 IN_PROC_BROWSER_TEST_F(MultiContentsViewBookmarkDragEntrypointsUiTest,
-                       DISABLED_ShowsDropTargetOnBookmarkedLinkDragged) {
+                       ShowsDropTargetOnBookmarkedLinkDragged) {
+  MAYBE_SKIP_FOR_MAC_VERSION();
   bookmarks::BookmarkModel* const model =
       BookmarkModelFactory::GetForBrowserContext(browser()->profile());
   const std::u16string bookmark_title = u"Bookmark";
