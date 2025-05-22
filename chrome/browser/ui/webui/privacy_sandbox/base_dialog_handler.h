2dc723ad8579678089f183a9b1cbfe6f2fd36b9b
Aashna Sheth <aashnas@google.com>
2025-05-22 08:07:22 -0700

    Move desktop view manager interface
    Moving the interface to its own file, so we can add it to interfaces
    target and other BUILD files don't have to rely on entirety of
    privacy_sandbox.
    
    Bug: b:408016824
    Change-Id: I546304670a1f87a276d864a65b07b7a6a5587793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576162
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464106}

--- chrome/browser/ui/webui/privacy_sandbox/base_dialog_handler.h
+++ chrome/browser/ui/webui/privacy_sandbox/base_dialog_handler.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_PRIVACY_SANDBOX_BASE_DIALOG_HANDLER_H_
 
 #include "base/scoped_observation.h"
-#include "chrome/browser/privacy_sandbox/notice/desktop_view_manager.h"
+#include "chrome/browser/privacy_sandbox/notice/desktop_view_manager_interface.h"
 #include "chrome/browser/privacy_sandbox/notice/notice.mojom-forward.h"
 #include "chrome/browser/ui/webui/privacy_sandbox/base_dialog.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
