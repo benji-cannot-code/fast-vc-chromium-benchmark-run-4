fb8ea390373fc3c744c839f375ff4518f2024940
Nick Harper <nharper@chromium.org>
2025-05-22 15:28:47 -0700

    Move 2-QWAC cert binding code to its own files.
    Change-Id: I87f68738de0b783ccf44b605c75753f2a53a358c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580339
    Auto-Submit: Nick Harper <nharper@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464402}

--- net/cert/qwac_fuzztest.cc
+++ net/cert/qwac_fuzztest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/containers/to_vector.h"
 #include "net/cert/qwac.h"
+#include "net/cert/two_qwac.h"
 #include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 
