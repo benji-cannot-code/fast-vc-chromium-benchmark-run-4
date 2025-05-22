ddcd6a254a621b96a5487e7ceaa4279661e78dc1
Victor Miura <vmiura@chromium.org>
2025-05-22 12:23:43 -0700

    [TreesInViz] Implement ScrollTree synced_scroll_offsets updates
    Wires updates to the cc::ScrollTree::synced_scroll_offset_map_ entries
    to Viz, which are required to compute scrollbar geometries.
    
    This resolves failures in fast/dom/focus-contenteditable.html
    
    Bug: 418116092
    Change-Id: I76fa220a2a3cbdbfe97327d6537633ab963d02cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569335
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Victor Miura <vmiura@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464295}

---
+++ cc/mojom/synced_scroll_offset_type_traits.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/mojom/synced_scroll_offset_type_traits.h"
+
+#include <utility>
+
+#include "cc/mojom/synced_scroll_offset.mojom.h"
+
+namespace mojo {
+
+bool StructTraits<cc::mojom::SyncedScrollOffsetDataView,
+                  scoped_refptr<cc::SyncedScrollOffset>>::
+    Read(cc::mojom::SyncedScrollOffsetDataView data,
+         scoped_refptr<cc::SyncedScrollOffset>* out) {
+  gfx::PointF scroll_offset;
+  if (!data.ReadScrollOffset(&scroll_offset)) {
+    return false;
+  }
+  *out = base::MakeRefCounted<cc::SyncedScrollOffset>();
+  (*out)->SetCurrent(scroll_offset);
+  return true;
+}
+
+}  // namespace mojo
