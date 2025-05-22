a6b3156df54a42b1f33ccde5d756001911cea005
Kelvin Jiang <kelvinjiang@chromium.org>
2025-05-22 12:25:03 -0700

    [Extensions] Port history API and some API tests to desktop Android
    Bug: 419057486
    Change-Id: I36f290f65d4b40eaa2b84318a87520000257dca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6570211
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464297}

--- chrome/browser/extensions/api/api_browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/api/api_browser_context_keyed_service_factories.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/bookmarks/bookmarks_api_watcher.h"
 #include "chrome/browser/extensions/api/cookies/cookies_api.h"
 #include "chrome/browser/extensions/api/developer_private/developer_private_api.h"
+#include "chrome/browser/extensions/api/history/history_api.h"
 #include "chrome/browser/extensions/api/notifications/extension_notification_display_helper_factory.h"
 #include "chrome/browser/extensions/commands/command_service.h"
 #include "chrome/common/buildflags.h"
@@ -24,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/braille_display_private/braille_display_private_api.h"
 #include "chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_private_event_router.h"
 #include "chrome/browser/extensions/api/font_settings/font_settings_api.h"
-#include "chrome/browser/extensions/api/history/history_api.h"
 #include "chrome/browser/extensions/api/identity/identity_api.h"
 #include "chrome/browser/extensions/api/image_writer_private/operation_manager.h"
 #include "chrome/browser/extensions/api/language_settings_private/language_settings_private_delegate_factory.h"
@@ -81,10 +81,11 @@ void EnsureApiBrowserContextKeyedServiceFactoriesBuilt() {
   // APIs supported on Win/Mac/Linux plus desktop Android go here.
   extensions::BookmarksAPI::GetFactoryInstance();
   extensions::BookmarksApiWatcher::EnsureFactoryBuilt();
-  extensions::CookiesAPI::GetFactoryInstance();
-  extensions::ExtensionNotificationDisplayHelperFactory::GetInstance();
-  extensions::DeveloperPrivateAPI::GetFactoryInstance();
   extensions::CommandService::GetFactoryInstance();
+  extensions::CookiesAPI::GetFactoryInstance();
+  extensions::DeveloperPrivateAPI::GetFactoryInstance();
+  extensions::ExtensionNotificationDisplayHelperFactory::GetInstance();
+  extensions::HistoryAPI::GetFactoryInstance();
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   extensions::ActivityLogAPI::GetFactoryInstance();
@@ -97,7 +98,6 @@ void EnsureApiBrowserContextKeyedServiceFactoriesBuilt() {
 #endif
   extensions::EnterpriseReportingPrivateEventRouterFactory::GetInstance();
   extensions::FontSettingsAPI::GetFactoryInstance();
-  extensions::HistoryAPI::GetFactoryInstance();
   extensions::IdentityAPI::GetFactoryInstance();
   extensions::IncognitoConnectability::EnsureFactoryBuilt();
 #if BUILDFLAG(IS_CHROMEOS)
