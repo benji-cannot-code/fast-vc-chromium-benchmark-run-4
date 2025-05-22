c66cf1892af17d4f43f881d2e96d52efa19b5009
Ho Cheung <hocheung@chromium.org>
2025-05-22 16:52:48 -0700

    [ui/android] Eliminate some unsafe buffers in the //ui/android directory
    Replace with something safer.
    
    Bug: 40285824
    Change-Id: I0bb312ad6e1b0cf0811e6654de3d6ecc2ed6264c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576946
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464448}

--- ui/android/resources/resource_manager_impl.h
+++ ui/android/resources/resource_manager_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ANDROID_RESOURCES_RESOURCE_MANAGER_IMPL_H_
 #define UI_ANDROID_RESOURCES_RESOURCE_MANAGER_IMPL_H_
 
+#include <array>
 #include <memory>
 #include <unordered_map>
 #include <unordered_set>
@@ -97,7 +98,7 @@ class UI_ANDROID_EXPORT ResourceManagerImpl
       std::unordered_map<SkColor, std::unique_ptr<ResourceMap>>;
 
   raw_ptr<cc::UIResourceManager> ui_resource_manager_;
-  ResourceMap resources_[ANDROID_RESOURCE_TYPE_COUNT];
+  std::array<ResourceMap, ANDROID_RESOURCE_TYPE_COUNT> resources_;
   TintedResourceMap tinted_resources_;
 
   // The set of tints that are used for resources in the current frame.
