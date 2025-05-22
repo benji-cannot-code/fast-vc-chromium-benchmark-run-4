cc0096298bb5f4d916f18ada04d1c8603992adca
Orin Jaworski <orinj@google.com>
2025-05-22 14:59:14 -0700

    Update contextual search match labels and support alts for lens action
    Bug: 406310461, 419351566
    Change-Id: I4771a5a0587cdd25c714bd303cb44bc2ca899424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568775
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464386}

--- components/omnibox/browser/actions/contextual_search_action.cc
+++ components/omnibox/browser/actions/contextual_search_action.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/omnibox/browser/actions/contextual_search_action.h"
 
+#include "components/omnibox/common/omnibox_feature_configs.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -13,6 +14,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/vector_icons.h"  // nogncheck
 #include "components/vector_icons/vector_icons.h"     // nogncheck
 #endif
+
+namespace {
+
+int GetOpenLensActionLabelId() {
+  switch (omnibox_feature_configs::ContextualSearch::Get()
+              .alternative_action_label) {
+    case 1:
+      return IDS_CONTEXTUAL_SEARCH_OPEN_LENS_ACTION_LABEL_ALT;
+    case 2:
+      return IDS_CONTEXTUAL_SEARCH_OPEN_LENS_ACTION_LABEL_ALT2;
+    case 0:
+    default:
+      return IDS_CONTEXTUAL_SEARCH_OPEN_LENS_ACTION_LABEL;
+  }
+}
+
+}  // namespace
 
 ContextualSearchFulfillmentAction::ContextualSearchFulfillmentAction(
     const GURL& url,
@@ -45,8 +63,7 @@ ContextualSearchFulfillmentAction::~ContextualSearchFulfillmentAction() =
 
 ContextualSearchOpenLensAction::ContextualSearchOpenLensAction()
     : OmniboxAction(OmniboxAction::LabelStrings(
-                        l10n_util::GetStringUTF16(
-                            IDS_CONTEXTUAL_SEARCH_OPEN_LENS_ACTION_LABEL),
+                        l10n_util::GetStringUTF16(GetOpenLensActionLabelId()),
                         u"",
                         u"",
                         u""),
