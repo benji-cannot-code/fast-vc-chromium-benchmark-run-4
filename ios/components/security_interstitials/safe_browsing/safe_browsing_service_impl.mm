ba0c317a50e902a101e557d453e6d15fa69205ef
Ernesto Izquierdo Clua <eic@google.com>
2025-05-22 08:02:21 -0700

    [ios] Disable access to allow list for enterprise url checks
    Fixes a bug in Enteprise Url Filtering. Enterprise Url checks should
    not use the Safe Browsing high confidence allow list, otherwise urls
    in the list can be marked as safe even when they match Enterprise Url
    filtering rules.
    
    This patch makes the iOS Safe Browsing implementation to only use
    the high confidence allow list for consumer url checks.
    
    This patch is behavior neutral when Enterprise Url Filtering is
    disabled.
    
    Fixed: 418031234
    Change-Id: I8bea216c439ca9aa09554a84a6a8f9a4739a3804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568573
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464100}

--- ios/components/security_interstitials/safe_browsing/safe_browsing_service_impl.mm
+++ ios/components/security_interstitials/safe_browsing/safe_browsing_service_impl.mm
@@ -179,6 +179,16 @@ SafeBrowsingServiceImpl::CreateUrlChecker(
               /*log_usage_histograms=*/true,
               /*are_background_lookups_allowed=*/false);
 
+  // Decide whether safe browsing database can be checked.
+  // If url_lookup_service_ is null, safe browsing database should be checked by
+  // default.
+  bool can_check_db =
+      url_lookup_service ? url_lookup_service->CanCheckSafeBrowsingDb() : true;
+  bool can_check_high_confidence_allowlist =
+      url_lookup_service
+          ? url_lookup_service->CanCheckSafeBrowsingHighConfidenceAllowlist()
+          : true;
+
   return std::make_unique<safe_browsing::SafeBrowsingUrlCheckerImpl>(
       /*headers=*/net::HttpRequestHeaders(), /*load_flags=*/0,
       /*has_user_gesture=*/false, url_checker_delegate,
@@ -190,8 +200,8 @@ SafeBrowsingServiceImpl::CreateUrlChecker(
       /*render_frame_token=*/std::nullopt,
       /*frame_tree_node_id=*/
       security_interstitials::UnsafeResource::kNoFrameTreeNodeId,
-      /*navigation_id=*/std::nullopt, can_perform_full_url_lookup,
-      /*can_check_db=*/true, /*can_check_high_confidence_allowlist=*/true,
+      /*navigation_id=*/std::nullopt, can_perform_full_url_lookup, can_check_db,
+      can_check_high_confidence_allowlist,
       /*url_lookup_service_metric_suffix=*/"", web::GetUIThreadTaskRunner({}),
       url_lookup_service ? url_lookup_service->GetWeakPtr() : nullptr,
       hash_real_time_service ? hash_real_time_service->GetWeakPtr() : nullptr,
@@ -225,6 +235,16 @@ SafeBrowsingServiceImpl::CreateAsyncChecker(
               /*log_usage_histograms=*/true,
               /*are_background_lookups_allowed=*/false);
 
+  // Decide whether safe browsing database can be checked.
+  // If url_lookup_service_ is null, safe browsing database should be checked by
+  // default.
+  bool can_check_db =
+      url_lookup_service ? url_lookup_service->CanCheckSafeBrowsingDb() : true;
+  bool can_check_high_confidence_allowlist =
+      url_lookup_service
+          ? url_lookup_service->CanCheckSafeBrowsingHighConfidenceAllowlist()
+          : true;
+
   return std::make_unique<safe_browsing::SafeBrowsingUrlCheckerImpl>(
       /*headers=*/net::HttpRequestHeaders(), /*load_flags=*/0,
       /*has_user_gesture=*/false, url_checker_delegate,
@@ -236,8 +256,8 @@ SafeBrowsingServiceImpl::CreateAsyncChecker(
       /*render_frame_token=*/std::nullopt,
       /*frame_tree_node_id=*/
       security_interstitials::UnsafeResource::kNoFrameTreeNodeId,
-      /*navigation_id=*/std::nullopt, can_perform_full_url_lookup,
-      /*can_check_db=*/true, /*can_check_high_confidence_allowlist=*/true,
+      /*navigation_id=*/std::nullopt, can_perform_full_url_lookup, can_check_db,
+      can_check_high_confidence_allowlist,
       /*url_lookup_service_metric_suffix=*/"", web::GetUIThreadTaskRunner({}),
       url_lookup_service ? url_lookup_service->GetWeakPtr() : nullptr,
       hash_real_time_service ? hash_real_time_service->GetWeakPtr() : nullptr,
