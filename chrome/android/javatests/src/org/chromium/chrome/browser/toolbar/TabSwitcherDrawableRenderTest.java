a2ff5778f2fa9fc17601829f188c0018c7f5e15f
Tomasz Wiszkowski <ender@google.com>
2025-05-22 11:53:08 -0700

    Re-enable TabSwitcherDrawableRenderTests disabled due to Parity update
    The change bumps render test revision to differentiate new screenshots
    which carry the search engine name.
    
    Change-Id: Iaf1706357d9ce3237bc8f310230eae0a5941ccae
    Fixed: 419257653, 419165813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580598
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464277}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/TabSwitcherDrawableRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/TabSwitcherDrawableRenderTest.java
@@ -47,7 +47,7 @@ public class TabSwitcherDrawableRenderTest {
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
                     .setBugComponent(RenderTestRule.Component.UI_BROWSER_MOBILE_TAB_GROUPS)
-                    .setRevision(3)
+                    .setRevision(4)
                     .build();
 
     @ClassRule
@@ -127,7 +127,6 @@ public class TabSwitcherDrawableRenderTest {
     @Test
     @MediumTest
     @Feature("RenderTest")
-    @DisabledTest(message = "https://crbug.com/419257653")
     public void testTabSwitcherDrawable_newTabPageIncognito() throws Exception {
         ChromeTabbedActivity activity = sActivityTestRule.getActivity();
 
