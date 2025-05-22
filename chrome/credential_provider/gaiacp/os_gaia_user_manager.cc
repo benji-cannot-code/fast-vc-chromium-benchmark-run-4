e1deed3333d944f00018afd8716ee9f32fb5673e
Krishna Kurapati <krkurapati@google.com>
2025-05-21 22:39:27 -0700

    Updating logging level at ChangeGaiaUserPasswordIfNeeded to INFO
    Errors in this method are benign.
    
    Change-Id: I851157fd3dc68c27b1f63262e4b4876d2bb7df9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569339
    Commit-Queue: Rakesh Soma <rakeshsoma@google.com>
    Reviewed-by: Rakesh Soma <rakeshsoma@google.com>
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463882}

--- chrome/credential_provider/gaiacp/os_gaia_user_manager.cc
+++ chrome/credential_provider/gaiacp/os_gaia_user_manager.cc
@@ -240,7 +240,7 @@ HRESULT OSGaiaUserManager::ChangeGaiaUserPasswordIfNeeded() {
   auto policy = ScopedLsaPolicy::Create(POLICY_ALL_ACCESS);
   if (!policy) {
     hr = HRESULT_FROM_WIN32(::GetLastError());
-    LOGFN(ERROR) << "ScopedLsaPolicy::Create hr=" << putHR(hr);
+    LOGFN(INFO) << "ScopedLsaPolicy::Create hr=" << putHR(hr);
     return hr;
   }
 
@@ -250,7 +250,7 @@ HRESULT OSGaiaUserManager::ChangeGaiaUserPasswordIfNeeded() {
   // gaia user must already exist.
   hr = IsGaiaUserSidDifferent(&is_sid_different);
   if (FAILED(hr)) {
-    LOGFN(ERROR) << "IsGaiaUserSidDifferent hr=" << putHR(hr);
+    LOGFN(INFO) << "IsGaiaUserSidDifferent hr=" << putHR(hr);
     return hr;
   }
 
@@ -260,7 +260,7 @@ HRESULT OSGaiaUserManager::ChangeGaiaUserPasswordIfNeeded() {
     hr = policy->RetrievePrivateData(kLsaKeyGaiaUsername, gaia_username,
                                      std::size(gaia_username));
     if (FAILED(hr)) {
-      LOGFN(ERROR) << "RetrievePrivateData for gaia username hr=" << putHR(hr);
+      LOGFN(INFO) << "RetrievePrivateData for gaia username hr=" << putHR(hr);
       return hr;
     }
 
@@ -268,7 +268,7 @@ HRESULT OSGaiaUserManager::ChangeGaiaUserPasswordIfNeeded() {
     hr = manager->GenerateRandomPassword(new_password, std::size(new_password));
     if (FAILED(hr)) {
       SecurelyClearBuffer(new_password, std::size(new_password));
-      LOGFN(ERROR) << "GenerateRandomPassword hr=" << putHR(hr);
+      LOGFN(INFO) << "GenerateRandomPassword hr=" << putHR(hr);
       return hr;
     }
 
@@ -277,7 +277,7 @@ HRESULT OSGaiaUserManager::ChangeGaiaUserPasswordIfNeeded() {
                                      std::size(current_password));
     if (FAILED(hr)) {
       SecurelyClearBuffer(new_password, std::size(current_password));
-      LOGFN(ERROR) << "RetrievePrivateData hr=" << putHR(hr);
+      LOGFN(INFO) << "RetrievePrivateData hr=" << putHR(hr);
       return hr;
     }
 
@@ -287,14 +287,14 @@ HRESULT OSGaiaUserManager::ChangeGaiaUserPasswordIfNeeded() {
     SecurelyClearBuffer(current_password, std::size(current_password));
     if (FAILED(hr)) {
       SecurelyClearBuffer(new_password, std::size(new_password));
-      LOGFN(ERROR) << "ChangeUserPassword hr=" << putHR(hr);
+      LOGFN(INFO) << "ChangeUserPassword hr=" << putHR(hr);
       return hr;
     }
 
     hr = policy->StorePrivateData(kLsaKeyGaiaPassword, new_password);
     SecurelyClearBuffer(new_password, std::size(new_password));
     if (FAILED(hr)) {
-      LOGFN(ERROR) << "StoreGaiaPassword hr=" << putHR(hr);
+      LOGFN(INFO) << "StoreGaiaPassword hr=" << putHR(hr);
       return hr;
     }
 
@@ -302,7 +302,7 @@ HRESULT OSGaiaUserManager::ChangeGaiaUserPasswordIfNeeded() {
 
     hr = StoreCurrentGaiaSid();
     if (FAILED(hr)) {
-      LOGFN(ERROR) << "StoreCurrentGaiaSid hr=" << putHR(hr);
+      LOGFN(INFO) << "StoreCurrentGaiaSid hr=" << putHR(hr);
       return hr;
     }
     LOGFN(INFO) << "Current SID stored for gaia user.";
