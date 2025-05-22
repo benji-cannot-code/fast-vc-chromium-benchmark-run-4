844da08879475ca67b1a671bce2ee03e79e9f300
James Lee <ljjlee@google.com>
2025-05-22 14:46:22 -0700

    Trigger onCreated/onRemoved on extension API when node visibility changes
    This builds on the changes in crrev.com/c/6395501 and sends
    onCreated/onRemoved events over the extension API as a result of changes
    to permanent folder visibility.
    
    This change is primarily relevant for users with separate local and
    account storages, where the node permanent folder visibility can change
    based on the visibility of other permanent folders, and whether the
    folder itself has children.
    
    Bug: 395071423
    Change-Id: Ie05d21ecfa0efd385def5a9b550bf88531a124cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394263
    Commit-Queue: James Lee <ljjlee@google.com>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464378}

--- chrome/browser/extensions/api/bookmarks/bookmarks_api.h
+++ chrome/browser/extensions/api/bookmarks/bookmarks_api.h
@@ -32,8 +32,9 @@ class FilePath;
 }
 
 namespace bookmarks {
-class BookmarkNode;
 class BookmarkModel;
+class BookmarkNode;
+class BookmarkPermanentNode;
 class ManagedBookmarkService;
 }  // namespace bookmarks
 
@@ -77,6 +78,8 @@ class BookmarkEventRouter : public bookmarks::BookmarkModelObserver {
   void BookmarkNodeFaviconChanged(const bookmarks::BookmarkNode* node) override;
   void BookmarkNodeChildrenReordered(
       const bookmarks::BookmarkNode* node) override;
+  void BookmarkPermanentNodeVisibilityChanged(
+      const bookmarks::BookmarkPermanentNode* node) override;
   void ExtensiveBookmarkChangesBeginning() override;
   void ExtensiveBookmarkChangesEnded() override;
 
