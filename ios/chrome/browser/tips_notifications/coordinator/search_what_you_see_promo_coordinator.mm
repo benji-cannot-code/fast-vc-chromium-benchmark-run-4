5abaa15ff9316d5cebd17555116755a2def7008c
Benjamin Williams <bwwilliams@google.com>
2025-05-22 05:31:59 -0700

    [iOS][Tips1.6] Create SearchWhatYouSeePromoInstructionsViewController
    This CL introduces SearchWhatYouSeePromoInstructionsViewController, a
    new overlay that educates users on how to use the "Search What You See"
    feature.
    
    Note: This change does not include the "Learn more" link at the bottom
    of the instructions view. (This will be added in a subsequent CL.)
    
    Demo Screenshot: https://screenshot.googleplex.com/5VShoibUJMC3uxP.png
    
    Change-Id: Iead5f01be4ee2d80cbf9c5a2ee971fc94b679100
    Bug: 418750898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569649
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464022}

--- ios/chrome/browser/tips_notifications/coordinator/search_what_you_see_promo_coordinator.mm
+++ ios/chrome/browser/tips_notifications/coordinator/search_what_you_see_promo_coordinator.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/public/commands/browser_coordinator_commands.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/chrome/browser/tips_notifications/ui/search_what_you_see_promo_instructions_view_controller.h"
 #import "ios/chrome/browser/tips_notifications/ui/search_what_you_see_promo_view_controller.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation SearchWhatYouSeePromoCoordinator {
   SearchWhatYouSeePromoViewController* _viewController;
+  SearchWhatYouSeePromoInstructionsViewController* _instructionsViewController;
 }
 
 #pragma mark - ChromeCoordinator
@@ -37,11 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
+  _instructionsViewController.actionHandler = nil;
   _viewController.actionHandler = nil;
 
   [_viewController.presentingViewController dismissViewControllerAnimated:YES
                                                                completion:nil];
 
+  _instructionsViewController = nil;
   _viewController = nil;
 }
 
@@ -52,10 +56,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)confirmationAlertSecondaryAction {
-  // TODO(crbug.com/418750898): Present the 'Show me how' steps.
+  _instructionsViewController =
+      [[SearchWhatYouSeePromoInstructionsViewController alloc] init];
+  _instructionsViewController.actionHandler = self;
+  _instructionsViewController.presentationController.delegate = self;
+  [_viewController presentViewController:_instructionsViewController
+                                animated:YES
+                              completion:nil];
 }
 
 - (void)confirmationAlertDismissAction {
+  if (_viewController.presentedViewController &&
+      _viewController.presentedViewController == _instructionsViewController) {
+    _instructionsViewController.actionHandler = nil;
+    _instructionsViewController = nil;
+
+    [_viewController dismissViewControllerAnimated:YES completion:nil];
+
+    return;
+  }
+
   [self dismiss];
 }
 
@@ -63,6 +83,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)presentationControllerDidDismiss:
     (UIPresentationController*)presentationController {
+  if (presentationController.presentedViewController &&
+      presentationController.presentedViewController ==
+          _instructionsViewController) {
+    _instructionsViewController.actionHandler = nil;
+    _instructionsViewController = nil;
+
+    return;
+  }
+
   [self dismiss];
 }
 
