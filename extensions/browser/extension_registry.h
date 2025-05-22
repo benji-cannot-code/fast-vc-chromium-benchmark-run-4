001fbc553a3c0d554ff2da970aa391ed803bf881
James Cook <jamescook@chromium.org>
2025-05-22 10:23:47 -0700

    extensions: Clean up ENABLE_EXTENSIONS in //extensions
    TestRuntimeApiDelegate can be safely added to the build now.
    Also, replace ENABLE_EXTENSIONS || ENABLE_DESKTOP_ANDROID_EXTENSIONS
    with ENABLE_EXTENSIONS_CORE, to be consistent with the rest of the
    codebase.
    
    Bug: 356905053
    Change-Id: I616391c2843c6c5e20188803f2a51c6f83165927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576511
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464202}

--- extensions/browser/extension_registry.h
+++ extensions/browser/extension_registry.h
@@ -20,8 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_id.h"
 #include "extensions/common/extension_set.h"
 
-static_assert(BUILDFLAG(ENABLE_EXTENSIONS) ||
-              BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS));
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace content {
 class BrowserContext;
--- extensions/common/extension.h
+++ extensions/common/extension.h
@@ -30,8 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "url/origin.h"
 
-static_assert(BUILDFLAG(ENABLE_EXTENSIONS) ||
-              BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS));
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 class HashedExtensionId;
