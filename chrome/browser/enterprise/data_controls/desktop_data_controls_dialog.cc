1e3151a1e3ca7fa288c3933cf3d003b8d0cec9a7
Greg Thompson <grt@chromium.org>
2025-05-22 08:02:55 -0700

    [data_controls] Fix two DesktopDataControlsDialog leaks
    WebContentsDestroyed and PrimaryPageChanged were failing to close the
    dialog, leading to leaked instances.
    
    Bug: none
    Change-Id: I76221164a52585957a12d218ebf16fbed9fe03fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578626
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464101}

--- chrome/browser/enterprise/data_controls/desktop_data_controls_dialog.cc
+++ chrome/browser/enterprise/data_controls/desktop_data_controls_dialog.cc
@@ -294,7 +294,7 @@ void DesktopDataControlsDialog::WebContentsDestroyed() {
   // was neither bypassed or accepted so it should close without calling
   // any callback.
   ClearCallbacks();
-  OnDialogButtonClicked(/*bypassed=*/false);
+  CloseDialog(views::Widget::ClosedReason::kAcceptButtonClicked);
 }
 
 void DesktopDataControlsDialog::PrimaryPageChanged(content::Page& page) {
@@ -304,7 +304,7 @@ void DesktopDataControlsDialog::PrimaryPageChanged(content::Page& page) {
   // that trigger on the new page, so callbacks must be cleared before closing
   // the dialog.
   ClearCallbacks();
-  OnDialogButtonClicked(/*bypassed=*/false);
+  CloseDialog(views::Widget::ClosedReason::kAcceptButtonClicked);
 }
 
 DesktopDataControlsDialog::DesktopDataControlsDialog(
