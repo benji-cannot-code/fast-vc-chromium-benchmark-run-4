fc0d8807dddc58a1f67099dc192a6321efa7ce44
Gauthier Ambard <gambard@chromium.org>
2025-05-22 01:35:40 -0700

    [iOS][SK] Update color helper for TabGroupView buttons
    Use a helper to be able to share the value with other components.
    
    Bug: 417382128
    Change-Id: I921a722276a6b13c126f71e53966a2c887279e79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574465
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463939}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.h"
 
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+
 NSString* const kCreateTabGroupViewIdentifier =
     @"kCreateTabGroupViewIdentifier";
 NSString* const kCreateTabGroupTextFieldIdentifier =
@@ -26,6 +28,10 @@ NSString* const kTabGroupCloseButtonIdentifier =
     @"kTabGroupCloseButtonIdentifier";
 NSString* const kTabGroupFacePileButtonIdentifier =
     @"kTabGroupFacePileButtonIdentifier";
+
+UIColor* TabGroupViewButtonBackgroundColor() {
+  return [[UIColor colorNamed:kGrey200Color] colorWithAlphaComponent:0.35];
+}
 
 NSString* const kTabGroupRecentActivityIdentifier =
     @"kTabGroupRecentActivityIdentifier";
