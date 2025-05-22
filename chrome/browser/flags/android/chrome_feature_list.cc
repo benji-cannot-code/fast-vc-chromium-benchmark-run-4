886e395163dc51062742a272ba5d28617d9f149b
Mark Schillaci <mschillaci@google.com>
2025-05-21 20:53:24 -0700

    [KeyboardEsc] Enabled feature by default for escape key navigation
    AX-Relnotes: N/A
    Bug: 398061359
    Change-Id: I9265785c3f1a859b8411ded4dda41698eb29bc64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575050
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1463859}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -997,7 +997,7 @@ BASE_FEATURE(kInstanceSwitcherV2,
 
 BASE_FEATURE(kKeyboardEscBackNavigation,
              "KeyboardEscBackNavigation",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMagicStackAndroid,
              "MagicStackAndroid",
