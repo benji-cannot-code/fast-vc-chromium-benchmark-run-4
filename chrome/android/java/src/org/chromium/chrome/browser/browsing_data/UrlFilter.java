bc1c6ff4d71b2f19bb649cb9e0ac95fb0de74ef5
Sinan Sahin <sinansahin@google.com>
2025-05-22 16:00:45 -0700

    Add @NullMarked to org.chromium.chrome.browser.browsing_data
    Bug: 389129271
    Change-Id: If357841acfb1d00ad2e7f1c229047815fc93adc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580099
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464425}

--- chrome/android/java/src/org/chromium/chrome/browser/browsing_data/UrlFilter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browsing_data/UrlFilter.java
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.browsing_data;
 
+import org.chromium.build.annotations.NullMarked;
+
 /** A URL->boolean filter used in browsing data deletions. */
+@NullMarked
 public interface UrlFilter {
     /**
      * @param url The url to be matched.
