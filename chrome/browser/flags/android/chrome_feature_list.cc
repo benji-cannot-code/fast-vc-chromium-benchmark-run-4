5162148bab118544a3b8b091e02b74f71fecdadb
Jack Thiesen <jthiesen@chromium.org>
2025-05-21 17:12:53 -0700

    Feature Flag for HomepageIsNewTabPage on Android
    Similar to: https://chromium-review.googlesource.com/c/chromium/src/+/6565890
    
    (also corrects an alphabetization mistake from that CL)
    
    Bug: 400800634
    Change-Id: Ie1130316ba4af52eda10fa51928d52e82cb51201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6571612
    Reviewed-by: Charles Hager <clhager@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463806}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -302,6 +302,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kHeadlessTabModel,
     &kHideTabletToolbarDownloadButton,
     &kHistoryPaneAndroid,
+    &kHomepageIsNewTabPagePolicyAndroid,
     &kLegacyTabStateDeprecation,
     &kLockBackPressHandlerAtStart,
     &kIncognitoScreenshot,
@@ -972,6 +973,10 @@ BASE_FEATURE(kHeadlessTabModel,
 
 BASE_FEATURE(kHistoryPaneAndroid,
              "HistoryPaneAndroid",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kHomepageIsNewTabPagePolicyAndroid,
+             "HomepageIsNewTabPagePolicyAndroid",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLegacyTabStateDeprecation,
