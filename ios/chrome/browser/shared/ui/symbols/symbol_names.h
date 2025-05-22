3d4e164bb276e5f557f0906e717f2733779c3b88
Christian Xu <christianxu@google.com>
2025-05-22 09:17:11 -0700

    [iOS] Add magnifying glass spark symbol
    Add magnifying glass spark symbol.
    
    Bug: None
    Change-Id: I1f970f4630bb6b03db1681b19ffc648483ed67ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578630
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464144}

--- ios/chrome/browser/shared/ui/symbols/symbol_names.h
+++ ios/chrome/browser/shared/ui/symbols/symbol_names.h
@@ -99,6 +99,7 @@ extern NSString* const kFamilylinkSymbol;
 extern NSString* const kMyDriveSymbol;
 extern NSString* const kSharedDrivesSymbol;
 extern NSString* const kEllipsisSquareFillSymbol;
+extern NSString* const kMagnifyingglassSparkSymbol;
 
 // Custom symbol names which can be configured with a color palette. iOS 15+
 // only.
