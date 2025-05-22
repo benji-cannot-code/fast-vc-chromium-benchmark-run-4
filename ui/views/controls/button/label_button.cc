bb1266167d35d02ce121032f3926e98fbc3367f3
Vansh Parikh <vanshparikh@microsoft.com>
2025-05-22 00:34:41 -0700

    Paint label button icons to layer by default on Windows
    This CL moves the icon painting logic back to the base `LabelButton`
    class, so that icons are painted to a layer by default for all label
    buttons.
    
    Background: A previous CL [1] shifted this logic from `LabelButton` to
    its sub-class `ToolbarButton`. While this was intended to solve issues
    specific to `ToolbarButton`, it inadvertently caused hover-based icon
    shifting across other `LabelButton`s. A subsequent CL [2] attempted to
    fix the regression only for bookmark folder buttons, but the issue still
    persisted for other label buttons, as seen in the reopened bug.
    
    Fix: The logic for painting icons to a layer by default is being moved
    back to the `LabelButton` class, effectively reverting changes in [1] &
    [2]. This ensures consistent behavior across all label buttons and
    resolves the icon shift issue on hover.
    
    Since this is impacting Ring 0 label buttons as well, restoring the
    previous behavior here makes sense. The original bug [3] that CL [1] was
    addressing will be reopened so a more specific solution can be explored
    separately.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6279834
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/6527368
    [3] https://issues.chromium.org/issues/394380766
    
    Please find the attached screencast below after the fix:
    https://drive.google.com/file/d/1H3szdYL1KF2LR_cH5OddChN3IVTn3mIP/view?usp=sharing
    
    Bug: 414783688
    Change-Id: I1e3f9bb8ace62d9490aafa18e64d353bb9d5b533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576165
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Vansh Parikh <vanshparikh@microsoft.com>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463916}

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -83,6 +83,13 @@ LabelButton::LabelButton(
   SetTextInternal(text);
   SetLayoutManager(std::make_unique<DelegatingLayoutManager>(this));
   GetViewAccessibility().SetIsDefault(is_default_);
+
+#if BUILDFLAG(IS_WIN)
+  // Paint image(s) to a layer so that the canvas is snapped to pixel
+  // boundaries.
+  image_container_view()->SetPaintToLayer();
+  image_container_view()->layer()->SetFillsBoundsOpaquely(false);
+#endif
 }
 
 LabelButton::~LabelButton() {
@@ -521,8 +528,10 @@ void LabelButton::UpdateImage() {
 
 void LabelButton::AddLayerToRegion(ui::Layer* new_layer,
                                    views::LayerRegion region) {
+#if !BUILDFLAG(IS_WIN)
   image_container_view()->SetPaintToLayer();
   image_container_view()->layer()->SetFillsBoundsOpaquely(false);
+#endif
   ink_drop_container()->SetVisible(true);
   ink_drop_container()->AddLayerToRegion(new_layer, region);
 }
@@ -530,7 +539,9 @@ void LabelButton::AddLayerToRegion(ui::Layer* new_layer,
 void LabelButton::RemoveLayerFromRegions(ui::Layer* old_layer) {
   ink_drop_container()->RemoveLayerFromRegions(old_layer);
   ink_drop_container()->SetVisible(false);
+#if !BUILDFLAG(IS_WIN)
   image_container_view()->DestroyLayer();
+#endif
 }
 
 std::unique_ptr<ActionViewInterface> LabelButton::GetActionViewInterface() {
