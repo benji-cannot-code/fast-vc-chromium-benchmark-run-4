0c1fcdb27f66c1d57714542c2629b0df5ab3aaf9
Aashna Sheth <aashnas@google.com>
2025-05-22 07:09:49 -0700

    Move IsUrlSuitableCheck to helper file
    Moving the check to its own file, so we can create a separate target for
    it and other BUILD files don't have to rely on entirety of
    privacy_sandbox.
    
    Bug: b:408016824
    Change-Id: Ia1f8c00bda7bee5903f56c66dfa5ff787f4c3c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576280
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464061}

---
+++ chrome/browser/privacy_sandbox/notice/desktop_entrypoint_handlers_helper.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef CHROME_BROWSER_PRIVACY_SANDBOX_NOTICE_DESKTOP_ENTRYPOINT_HANDLERS_HELPER_H_
+#define CHROME_BROWSER_PRIVACY_SANDBOX_NOTICE_DESKTOP_ENTRYPOINT_HANDLERS_HELPER_H_
+#include "url/gurl.h"
+namespace privacy_sandbox {
+// Returns whether |url| is suitable to display the Privacy Sandbox prompt
+// over. Only about:blank and certain chrome:// URLs are considered
+// suitable.
+bool IsUrlSuitableForPrompt(const GURL& url);
+}  // namespace privacy_sandbox
+#endif  // CHROME_BROWSER_PRIVACY_SANDBOX_NOTICE_DESKTOP_ENTRYPOINT_HANDLERS_HELPER_H_
