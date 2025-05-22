6a284524f6bb025ed7ebf6ee1e82cd4dfadeca73
James Cook <jamescook@chromium.org>
2025-05-22 14:58:57 -0700

    extensions: Port chrome.action API to desktop Android
    Most of the API functions work without modification. The only exception
    is openPopup(), which will need a separate Android implementation (or a
    new shared implementation when we have a cross-platform window
    interface).
    
    Split the API impl into shared, _android.cc, and _non_android.cc files
    to limit ifdef-ing. Provide stubs for openPopup() for Android.
    
    Port the unit test. We only need to test chrome.action on Android,
    as Android only supports manifest V3. The related chrome.browserAction
    and chrome.pageAction APIs are manifest <= V2 and won't be supported.
    
    The API test will come in a later CL, as it requires more work.
    
    Bug: 393179880
    Change-Id: I7d12657d06faa3821532b104bfa129601e996bfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580126
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464385}

--- chrome/browser/extensions/api/extension_action/extension_action_api_unittest.cc
+++ chrome/browser/extensions/api/extension_action/extension_action_api_unittest.cc
@@ -13,9 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension_test_util.h"
 #include "extensions/browser/extension_action.h"
 #include "extensions/browser/extension_action_manager.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/api/extension_action/action_info.h"
 #include "extensions/common/api/extension_action/action_info_test_util.h"
 #include "extensions/test/test_extension_dir.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 namespace {
@@ -124,11 +127,19 @@ TEST_P(ExtensionActionAPIUnitTest, ActionLocalization) {
             action->GetTitle(ExtensionAction::kDefaultTabId));
 }
 
+#if BUILDFLAG(IS_ANDROID)
+// Android only supports manifest V3, which has chrome.action but not
+// chrome.browserAction or chrome.pageAction.
+INSTANTIATE_TEST_SUITE_P(All,
+                         ExtensionActionAPIUnitTest,
+                         testing::Values(ActionInfo::Type::kAction));
+#else
 INSTANTIATE_TEST_SUITE_P(All,
                          ExtensionActionAPIUnitTest,
                          testing::Values(ActionInfo::Type::kBrowser,
                                          ActionInfo::Type::kPage,
                                          ActionInfo::Type::kAction));
+#endif  // BUILDFLAG(IS_ANDROID)
 
 }  // namespace
 }  // namespace extensions
