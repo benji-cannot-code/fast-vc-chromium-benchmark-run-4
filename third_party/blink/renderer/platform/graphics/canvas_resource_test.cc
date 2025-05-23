65a71238afae6332337237aabe13000206fb97cf
Colin Blundell <blundell@chromium.org>
2025-05-23 01:19:17 -0700

    [Blink] Augment tests of CanvasResource::PrepareTransferableResource()
    Add a test that the TransferableResource that is produced matches the
    premul/unpremul alpha type of the CanvasResource's SharedImage. The
    latter part of the test fails if [1] is reverted.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6573712
    
    Bug: 410591523
    Change-Id: I132f0a192a22b5cc000665f883d4a30b2087cdd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578603
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464612}

--- third_party/blink/renderer/platform/graphics/canvas_resource_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "components/viz/common/resources/transferable_resource.h"
+#include "components/viz/test/test_context_provider.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -16,12 +17,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/test/fake_gles2_interface.h"
 #include "third_party/blink/renderer/platform/graphics/test/fake_web_graphics_context_3d_provider.h"
 #include "third_party/blink/renderer/platform/graphics/test/gpu_memory_buffer_test_platform.h"
+#include "third_party/blink/renderer/platform/graphics/test/gpu_test_utils.h"
 #include "third_party/blink/renderer/platform/graphics/test/test_webgraphics_shared_image_interface_provider.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
+#include "third_party/blink/renderer/platform/testing/testing_platform_support.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/skia/include/core/SkSurface.h"
 
 namespace blink {
+
+namespace {
+
+class AcceleratedCompositingTestPlatform
+    : public blink::TestingPlatformSupport {
+ public:
+  bool IsGpuCompositingDisabled() const override { return false; }
+};
+
+}  // namespace
 
 TEST(CanvasResourceTest, PrepareTransferableResource_Software) {
   test::TaskEnvironment task_environment;
@@ -47,6 +60,43 @@ TEST(CanvasResourceTest, PrepareTransferableResource_Software) {
 
   std::move(release_callback)
       .Run(std::move(canvas_resource), gpu::SyncToken(), false);
+}
+
+TEST(CanvasResourceTest, PrepareTransferableResource_PreservesAlphaType) {
+  test::TaskEnvironment task_environment;
+  auto accelerated_compositing_platform = std::make_unique<
+      ScopedTestingPlatformSupport<AcceleratedCompositingTestPlatform>>();
+  auto test_context_provider = viz::TestContextProvider::CreateRaster();
+  InitializeSharedGpuContextRaster(test_context_provider.get());
+
+  viz::TransferableResource resource;
+  CanvasResource::ReleaseCallback release_callback;
+
+  scoped_refptr<CanvasResource> premul_canvas_resource =
+      CanvasResourceSharedImage::Create(
+          gfx::Size(10, 10), viz::SinglePlaneFormat::kRGBA_8888,
+          kPremul_SkAlphaType, gfx::ColorSpace::CreateSRGB(),
+          SharedGpuContext::ContextProviderWrapper(),
+          /*CanvasResourceProvider=*/nullptr,
+          /*is_accelerated=*/false, gpu::SHARED_IMAGE_USAGE_DISPLAY_READ);
+
+  ASSERT_TRUE(premul_canvas_resource->PrepareTransferableResource(
+      &resource, &release_callback, /*needs_verified_synctoken=*/false));
+  EXPECT_EQ(resource.alpha_type, kPremul_SkAlphaType);
+
+  scoped_refptr<CanvasResource> unpremul_canvas_resource =
+      CanvasResourceSharedImage::Create(
+          gfx::Size(10, 10), viz::SinglePlaneFormat::kRGBA_8888,
+          kUnpremul_SkAlphaType, gfx::ColorSpace::CreateSRGB(),
+          SharedGpuContext::ContextProviderWrapper(),
+          /*CanvasResourceProvider=*/nullptr,
+          /*is_accelerated=*/false,
+          gpu::SHARED_IMAGE_USAGE_DISPLAY_READ |
+              gpu::SHARED_IMAGE_USAGE_RASTER_WRITE);
+
+  ASSERT_TRUE(unpremul_canvas_resource->PrepareTransferableResource(
+      &resource, &release_callback, /*needs_verified_synctoken=*/false));
+  EXPECT_EQ(resource.alpha_type, kUnpremul_SkAlphaType);
 }
 
 }  // namespace blink
