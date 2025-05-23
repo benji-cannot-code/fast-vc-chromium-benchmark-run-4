e75c60fb7abd385b5dedd94ce67c74b65fe876eb
Christoph Schlosser <cschlosser@chromium.org>
2025-05-23 13:05:12 -0700

    rcaps: Drop keyboard layout information to get a valid country code
    Previously fallback data like us.intl would have been dropped as invalid
    data as it was longer than the 2 character country code.
    Now anything (usually keyboard layout info) after the dot is removed to
    get a valid country code.
    
    Bug: 419726281
    Change-Id: I57346b42fe940de6f7954dc7493eee2d815f497e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578394
    Auto-Submit: Christoph Schlosser <cschlosser@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464940}

--- chrome/browser/regional_capabilities/regional_capabilities_service_client.h
+++ chrome/browser/regional_capabilities/regional_capabilities_service_client.h
@@ -27,7 +27,8 @@ enum class ChromeOSFallbackCountry {
   kRegionTooShort = 3,
   kRegionTooLong = 4,
   kValidCountryCode = 5,
-  kMaxValue = kValidCountryCode,
+  kStrippedSubkeyInformation = 6,
+  kMaxValue = kStrippedSubkeyInformation,
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/chromeos/enums.xml:ChromeOSFallbackCountry)
 #endif
