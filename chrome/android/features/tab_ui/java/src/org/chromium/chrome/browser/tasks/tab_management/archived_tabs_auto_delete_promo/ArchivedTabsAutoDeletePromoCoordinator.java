97632899a282fd1babbd5a7241fba2794f4af46d
Hitarth Kothari <hitarthkothari@google.com>
2025-05-23 13:10:31 -0700

    [Declutter] Added User Action Metrics
    Bug: 415285268
    Change-Id: Iebab44591ea4091d34d2f9980f2b143ba3ded44b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6582048
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464943}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/archived_tabs_auto_delete_promo/ArchivedTabsAutoDeletePromoCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/archived_tabs_auto_delete_promo/ArchivedTabsAutoDeletePromoCoordinator.java
@@ -12,6 +12,7 @@ import android.view.View;
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.tab.TabArchiveSettings;
@@ -174,6 +175,12 @@ public class ArchivedTabsAutoDeletePromoCoordinator {
 
         mTabArchiveSettings.setAutoDeleteEnabled(!disableAutoDeleteFeature);
         mTabArchiveSettings.setAutoDeleteDecisionMade(true);
+
+        if (disableAutoDeleteFeature) {
+            RecordUserAction.record("Tabs.ArchivedTabAutoDeletePromo.No");
+        } else {
+            RecordUserAction.record("Tabs.ArchivedTabAutoDeletePromo.Yes");
+        }
 
         cleanupSheetResourcesOnly();
     }
