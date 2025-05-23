3f2ac90060cfa2f4dbdf517e93eecd8e2b637c81
Orin Jaworski <orinj@google.com>
2025-05-23 16:22:54 -0700

    Add and implement ContextualSuggestionsAblateOthersWhenPresent feature
    Also adds flags entry so users can try it:
    chrome://flags/#contextual-suggestions-ablate-others-when-present
    
    Bug: 416724838
    Change-Id: I7df5af5b9079bb58fd9410f90bd4d0685164b2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6583592
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465048}

--- components/omnibox/common/omnibox_feature_configs.h
+++ components/omnibox/common/omnibox_feature_configs.h
@@ -134,6 +134,7 @@ struct ContextualSearch : Config<ContextualSearch> {
   ContextualSearch& operator=(const ContextualSearch&);
   ~ContextualSearch();
 
+  DECLARE_FEATURE(kContextualSuggestionsAblateOthersWhenPresent);
   DECLARE_FEATURE(kOmniboxContextualSuggestions);
   DECLARE_FEATURE(kStarterPackPage);
   DECLARE_FEATURE(kContextualZeroSuggestLensFulfillment);
@@ -152,6 +153,11 @@ struct ContextualSearch : Config<ContextualSearch> {
   // Whether to enable prefetching to support this feature's synchronous
   // match production requirement.
   bool IsEnabledWithPrefetch() const;
+
+  // Whether to make contextual suggestions exclusive; that is, remove
+  // other kinds of zero suggest matches when there are any contextual
+  // search matches.
+  bool contextual_suggestions_ablate_others_when_present;
 
   // Whether the starter pack page scope is enabled.
   bool starter_pack_page;
