1ddffbbe7c3f590c0f5109ee4220b7bb6f2ba4a6
Vansh Parikh <vanshparikh@microsoft.com>
2025-05-22 18:46:01 -0700

    Fix: Restore download drag icon rendering
    This is a follow-up CL to the previous CL [1], which moved the icon
    painting logic back to `LabelButton`. The move was necessary as the
    earlier fix CL [2] for this bug introduced visual regressions in other
    `LabelButton`s, including some Ring 0 buttons.
    
    However, bringing the logic back to `LabelButton` caused this previously
    resolved bug to reappear — the icon stopped rendering when dragging an
    item from the downloads bubble.
    
    Fix: This CL disables icon layer painting logic specifically for the
    drag contents button. This restores the expected drag icon behavior
    without impacting other `LabelButton` instances.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6576165
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/6279834
    
    Please find the attached screencast below after the fix:
    https://drive.google.com/file/d/148X6lPYtpTQkhT-rtnmfR9wsYKbHKO4R/view?usp=sharing
    
    Bug: 394380766, 414783688
    Change-Id: I6584e1328072e7bb28bb47cfce76219f568d4ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580519
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Vansh Parikh <vanshparikh@microsoft.com>
    Reviewed-by: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464503}

--- ui/views/button_drag_utils.cc
+++ ui/views/button_drag_utils.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/color/color_id.h"
@@ -30,6 +32,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/paint_info.h"
 #include "ui/views/widget/widget.h"
 #include "url/gurl.h"
+
+namespace {
+
+class DragContentsButton : public views::LabelButton {
+  METADATA_HEADER(DragContentsButton, views::LabelButton)
+
+ public:
+  DragContentsButton(PressedCallback callback, std::u16string title)
+      : LabelButton(std::move(callback), title) {
+#if BUILDFLAG(IS_WIN)
+    // For windows, label button paints icon to a layer by default, which
+    // causes the drag image to not render correctly. Disable this behavior.
+    // This is a workaround for crbug.com/394380766
+    image_container_view()->DestroyLayer();
+#endif
+  }
+  ~DragContentsButton() override = default;
+};
+
+BEGIN_METADATA(DragContentsButton)
+END_METADATA
+
+}  // namespace
 
 namespace button_drag_utils {
 
@@ -81,8 +106,8 @@ void SetDragImage(const GURL& url,
   drag_widget->Init(std::move(params));
 
   // Create a button to render the drag image for us.
-  views::LabelButton* button =
-      drag_widget->SetContentsView(std::make_unique<views::LabelButton>(
+  DragContentsButton* button =
+      drag_widget->SetContentsView(std::make_unique<DragContentsButton>(
           views::Button::PressedCallback(),
           title.empty() ? base::UTF8ToUTF16(url.spec()) : title));
   button->SetTextSubpixelRenderingEnabled(false);
