3bf46e1c7e4c7051f5a5d2ddca7c904afb5d880a
Ankush Singh <ankushkush@google.com>
2025-05-23 16:30:17 -0700

    Update DemographicsMetricsProvider::CanUploadDemographicsToGoogle()
    The method currently checks `kReplaceSyncWithSignInPromo` feature flag
    to check if the type supports transport-mode and if not, checks
    IsSyncFeatureEnabled(). This is incorrect on Desktop platforms since
    preferences support in transport-mode is behind another flag. Moreover,
    syncer::GetUploadToGoogleState() should already return the correct state
    and thus we don't need this extra logic here.
    
    On Mobile: No-op because `kReplaceSyncWithSignInPromo` is always
    enabled.
    
    On Desktop: No change for syncing users since the user toggle check will
    control syncing of prefs. No change for implicitly signed-in users
    because of they cannot turn on syncing of preferences without explicitly
    signing in. For the rest signed-in non-syncing users, this will enable
    syncing of the demographics pref, which is the intended change here.
    
    Bug: 330677712,417132019
    Change-Id: I30a235eb82d6b2acc12db58fc586bc9a7fc0de37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564346
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465052}

--- components/metrics/demographics/demographic_metrics_provider.cc
+++ components/metrics/demographics/demographic_metrics_provider.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
-#include "components/sync/base/features.h"
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/service/sync_service.h"
 #include "components/sync/service/sync_service_utils.h"
@@ -36,40 +35,17 @@ bool IsValidUploadState(syncer::UploadState upload_state) {
 
 bool CanUploadDemographicsToGoogle(syncer::SyncService* sync_service) {
   CHECK(sync_service);
-
   // PRIORITY_PREFERENCES is the sync datatype used to propagate demographics
   // information to the client. In its absence, demographics info is unavailable
   // thus cannot be uploaded.
-  if (!IsValidUploadState(syncer::GetUploadToGoogleState(
-          sync_service, syncer::PRIORITY_PREFERENCES)) ||
-      // With `kSyncSupportAlwaysSyncingPriorityPreferences` feature enabled,
-      // PRIORITY_PREFERENCES will always be active (decoupled from sync user
-      // toggle). Thus, the preferences user toggle should be checked
-      // separately.
-      !sync_service->GetUserSettings()->GetSelectedTypes().Has(
-          syncer::UserSelectableType::kPreferences)) {
-    return false;
-  }
-
-  // Even if GetUploadToGoogleState() reports to be active, the user may be in
-  // transport mode or full-sync (aka sync-the-feature enabled) mode.
-  // If `kReplaceSyncPromosWithSignInPromos` is enabled, then
-  // PRIORITY_PREFERENCES being enabled (which implies the user is signed in) is
-  // enough, and the sync mode doesn't matter.
-  if (base::FeatureList::IsEnabled(
-          syncer::kReplaceSyncPromosWithSignInPromos)) {
-    return true;
-  }
-
-  // If `kReplaceSyncPromosWithSignInPromos` is NOT enabled, then demographics
-  // may only be uploaded for users who have opted in to Sync.
-  // TODO(crbug.com/40066949): Simplify once IsSyncFeatureEnabled() is deleted
-  // from the codebase.
-  if (sync_service->IsSyncFeatureEnabled()) {
-    return true;
-  }
-
-  return false;
+  return IsValidUploadState(syncer::GetUploadToGoogleState(
+             sync_service, syncer::PRIORITY_PREFERENCES)) &&
+         // With `kSyncSupportAlwaysSyncingPriorityPreferences` feature enabled,
+         // PRIORITY_PREFERENCES will always be active (decoupled from sync user
+         // toggle). Thus, the preferences user toggle should be checked
+         // separately.
+         sync_service->GetUserSettings()->GetSelectedTypes().Has(
+             syncer::UserSelectableType::kPreferences);
 }
 
 }  // namespace
