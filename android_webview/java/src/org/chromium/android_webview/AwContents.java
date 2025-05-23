c711327a0bd8e9a139f4526d668719c0bd5a8c70
Rupert Ben Wiser <bewise@google.com>
2025-05-23 02:16:26 -0700

    webview: Add javascript interface restrictions and validation
    We have settled more confidently on an API shape for js interfaces on
    the WebViewBuilder. We are going to add an explicit separate call to
    restrict the Javascript.
    
    Also adding validation to ensure the interface name isn't added again.
    
    Bug: 383099115
    Change-Id: I74208bc526fb1d14d589b2e406ddeff4fbde54ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563367
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Auto-Submit: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464631}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -510,6 +510,8 @@ public class AwContents implements SmartClipProvider {
     private boolean mContainerViewFocused;
     private boolean mWindowFocused;
 
+    private boolean mRestrictJavascriptInterface;
+
     // These come from the compositor and are updated synchronously (in contrast to the values in
     // RenderCoordinates, which are updated at end of every frame).
     private float mPageScaleFactor = 1.0f;
@@ -3540,9 +3542,21 @@ public class AwContents implements SmartClipProvider {
     }
 
     /**
+     * @see WebViewBuilder#restrictJavascriptInterface()
+     */
+    public void restrictJavascriptInterface() {
+        mRestrictJavascriptInterface = true;
+    }
+
+    /**
      * @see JavascriptInjector#addPossiblyUnsafeInterface(Object, String, Class)
      */
     public void addJavascriptInterface(Object object, String name) {
+        if (mRestrictJavascriptInterface) {
+            throw new IllegalStateException(
+                    "addJavascriptInterface cannot be called on a WebView built with "
+                            + "restrictJavascriptInterface()");
+        }
         addJavascriptInterface(object, name, List.of("*"));
     }
 
@@ -3571,8 +3585,15 @@ public class AwContents implements SmartClipProvider {
                 .addPossiblyUnsafeInterface(object, name, requiredAnnotation, originAllowlist);
     }
 
-    /** @see android.webkit.WebView#removeJavascriptInterface(String) */
+    /**
+     * @see android.webkit.WebView#removeJavascriptInterface(String)
+     */
     public void removeJavascriptInterface(String interfaceName) {
+        if (mRestrictJavascriptInterface) {
+            throw new IllegalStateException(
+                    "removeJavascriptInterface cannot be called on a WebView built with "
+                            + "restrictJavascriptInterface()");
+        }
         if (TRACE) Log.i(TAG, "%s removeInterface=%s", this, interfaceName);
         if (isDestroyed(WARN)) return;
 
