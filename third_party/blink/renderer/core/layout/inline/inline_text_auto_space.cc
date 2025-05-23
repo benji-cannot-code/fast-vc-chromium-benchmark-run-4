b31a9f0ab0bea84aaef0ae0e671332a855514a33
Koji Ishii <kojii@chromium.org>
2025-05-22 21:38:54 -0700

    [text-autospace] Change the code for testing
    `InlineTextAutoSpace::Apply()` had an optional vector for
    testing code to inspect its computation.
    
    This patch changes it to a callback. This reduces test-only
    code from the production, and makes it easier to change the
    core logic without changing tests.
    
    This patch has no behavior changes.
    
    Bug: 40275399
    Change-Id: I5371e936fce4e954c245d839315c49efb897fbef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6577875
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464556}

--- third_party/blink/renderer/core/layout/inline/inline_text_auto_space.cc
+++ third_party/blink/renderer/core/layout/inline/inline_text_auto_space.cc
@@ -102,6 +102,13 @@ class SpacingApplier {
     DCHECK(shape_result);
     shape_result->ApplyTextAutoSpacing(offsets_with_spacing_);
     item->SetUnsafeToReuseShapeResult();
+    if (callback_for_testing_) [[unlikely]] {
+      callback_for_testing_->DidApply(offsets_with_spacing_);
+    }
+  }
+
+  void SetCallbackForTesting(InlineTextAutoSpace::Callback* callback) {
+    callback_for_testing_ = callback;
   }
 
  private:
@@ -109,6 +116,7 @@ class SpacingApplier {
   // Stores the spacing (1/8 ic) and auto-space points's previous positions, for
   // the previous item.
   Vector<OffsetWithSpacing, 16> offsets_with_spacing_;
+  InlineTextAutoSpace::Callback* callback_for_testing_ = nullptr;
 };
 
 }  // namespace
@@ -152,8 +160,7 @@ void InlineTextAutoSpace::Initialize(const InlineItemsData& data) {
   }
 }
 
-void InlineTextAutoSpace::Apply(InlineItemsData& data,
-                                Vector<wtf_size_t>* offsets_out) {
+void InlineTextAutoSpace::Apply(InlineItemsData& data) {
   const String& text = data.text_content;
   DCHECK(!text.Is8Bit());
   DCHECK_EQ(text.length(), ranges_.back().end);
@@ -167,6 +174,7 @@ void InlineTextAutoSpace::Apply(InlineItemsData& data,
   // whether to add spacing into the bound of two items.
   TextDirection last_direction = TextDirection::kLtr;
   SpacingApplier applier;
+  applier.SetCallbackForTesting(callback_for_testing_);
   for (const Member<InlineItem>& item_ptr : data.items) {
     const InlineItem& item = *item_ptr;
     if (item.Type() != InlineItem::kText) {
@@ -276,11 +284,7 @@ void InlineTextAutoSpace::Apply(InlineItemsData& data,
       }
     } while (offset < item.EndOffset());
 
-    if (!offsets_out) {
-      applier.SetSpacing(offsets, &item, *style);
-    } else {
-      offsets_out->AppendVector(offsets);
-    }
+    applier.SetSpacing(offsets, &item, *style);
     offsets.Shrink(0);
   }
   // Apply the pending spacing for the last item if needed.
