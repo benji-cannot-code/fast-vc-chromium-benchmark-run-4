d044cdef07e368a85f4b1479e0748b66c5ba24f3
Steven Luong <stluong@chromium.org>
2025-05-23 09:31:47 -0700

    Move favicon and title logic from TabRendererData to TabUIHelper
    TabRendererData was using TabUIHelper and
    SavedTabGroupWebContentsListener to determine which favicon and title
    to use. This CL moves logic that uses SavedTabGroupWebContentsListener
    into the TabUIHelper so that all usage of favicon and titles will be
    consistent.
    
    Bug: 418030657
    Change-Id: Ide5dc5eb6be1b8048b6bd1188766b9c4bb650773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579784
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464809}

--- chrome/browser/ui/tab_ui_helper.cc
+++ chrome/browser/ui/tab_ui_helper.cc
@@ -10,7 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/favicon/favicon_utils.h"
 #include "chrome/browser/sessions/session_restore.h"
+#include "chrome/browser/ui/tabs/public/tab_features.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_web_contents_listener.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/tabs/public/tab_interface.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/resources/grit/ui_resources.h"
 
@@ -31,6 +34,17 @@ TabUIHelper::TabUIHelper(tabs::TabInterface& tab_interface)
 TabUIHelper::~TabUIHelper() = default;
 
 std::u16string TabUIHelper::GetTitle() const {
+  tabs::TabInterface* const tab_interface =
+      tabs::TabInterface::GetFromContents(web_contents());
+  const tab_groups::SavedTabGroupWebContentsListener* wc_listener =
+      tab_interface->GetTabFeatures()->saved_tab_group_web_contents_listener();
+  if (wc_listener) {
+    if (const std::optional<tab_groups::DeferredTabState>& deferred_tab_state =
+            wc_listener->deferred_tab_state()) {
+      return deferred_tab_state.value().title();
+    }
+  }
+
   const std::u16string& contents_title = web_contents()->GetTitle();
   if (!contents_title.empty()) {
     return contents_title;
@@ -44,6 +58,17 @@ std::u16string TabUIHelper::GetTitle() const {
 }
 
 ui::ImageModel TabUIHelper::GetFavicon() const {
+  tabs::TabInterface* const tab_interface =
+      tabs::TabInterface::GetFromContents(web_contents());
+  const tab_groups::SavedTabGroupWebContentsListener* wc_listener =
+      tab_interface->GetTabFeatures()->saved_tab_group_web_contents_listener();
+  if (wc_listener) {
+    if (const std::optional<tab_groups::DeferredTabState>& deferred_tab_state =
+            wc_listener->deferred_tab_state()) {
+      return deferred_tab_state.value().favicon();
+    }
+  }
+
   return ui::ImageModel::FromImage(
       favicon::TabFaviconFromWebContents(web_contents()));
 }
