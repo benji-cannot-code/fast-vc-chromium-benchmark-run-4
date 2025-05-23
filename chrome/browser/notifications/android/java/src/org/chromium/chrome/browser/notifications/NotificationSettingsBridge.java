789acc16556662c0d647b9d99d098b896fc59004
Min Qin <qinmin@chromium.org>
2025-05-23 08:55:01 -0700

    Listening to notification channel changes to modify content settings
    When a notificationChannel is changed by user, it will not be immediately reflected in the contentSettings because the contentSettings will lazily update its rule until a getRuleIterator() is called.
    This is causing some UI issues as changes in site setting is not immediately updated.
    This CL addresses the issue by listening to the NotificationManager
    on notificationChannel changes. Once a change takes place, Chrome will
    immediately update the content settings in native.
    
    Bug: 418116083
    Change-Id: I34174059f678a42a91eac8742591e682877be593
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6565487
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464784}

--- chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/NotificationSettingsBridge.java
+++ chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/NotificationSettingsBridge.java
@@ -17,6 +17,7 @@ import org.chromium.chrome.browser.notifications.channels.ChromeChannelDefinitio
 import org.chromium.chrome.browser.notifications.channels.SiteChannelsManager;
 import org.chromium.components.url_formatter.SchemeDisplay;
 import org.chromium.components.url_formatter.UrlFormatter;
+import org.chromium.content_public.browser.BrowserStartupController;
 
 /** Interface for native code to interact with Android notification channels. */
 @NullMarked
@@ -104,8 +105,19 @@ public class NotificationSettingsBridge {
         }
     }
 
+    public static void onNotificationChannelStateChanged(String channelId, boolean blocked) {
+        if (!BrowserStartupController.getInstance().isFullBrowserStarted()) {
+            return;
+        }
+        NotificationSettingsBridgeJni.get()
+                .onChannelStateChanged(
+                        channelId, SiteChannelsManager.toSiteOrigin(channelId), blocked);
+    }
+
     @NativeMethods
     interface Natives {
         void onGetSiteChannelsDone(long callbackId, SiteChannel[] channels);
+
+        void onChannelStateChanged(String channelId, String origin, boolean blocked);
     }
 }
