a08c24ac844489a40749d521ffcc68f9c3cd1770
Antonio Sartori <antoniosartori@chromium.org>
2025-05-23 10:10:02 -0700

    [DNS] Report daily UMA metrics on revocation entries
    This CL adds metrics for Disruptive Notification Revocations so that
    we daily report the state of revocation entry. This will make it
    easier to compute which percentage of revocations get regranted or
    acknowledged or ignored and how they correlate with site engagement.
    
    In order to do that, we need to introduce a new state "Acknowledged",
    that is reached when a revocation is acked by the user. The new state
    also makes it easier to restore acked revocations.
    
    Finally, since we need to track the days since revocation, this CL
    removes the "created_at" member of a RevocationEntry and instead
    consistently stores in the timestamp field the creation time of a
    revocation or proposed revocation.
    
    NO_IFTTT=not needed in this case
    
    Bug: 406475122
    Change-Id: I79916d15a56117880b62fc97964529212d8df9b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574455
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464832}

--- chrome/browser/notifications/platform_notification_service_unittest.cc
+++ chrome/browser/notifications/platform_notification_service_unittest.cc
@@ -425,7 +425,6 @@ TEST_F(PlatformNotificationServiceTest,
                   RevocationState::kProposed,
               .site_engagement = 0.0,
               .daily_notification_count = kDailyNotificationCount,
-              .lifetime = base::Days(30),
           });
 
   PlatformNotificationData data;
@@ -460,7 +459,6 @@ TEST_F(PlatformNotificationServiceTest,
                   RevocationState::kProposed,
               .site_engagement = 0.0,
               .daily_notification_count = kDailyNotificationCount,
-              .lifetime = base::Days(30),
           });
 
   PlatformNotificationData data;
