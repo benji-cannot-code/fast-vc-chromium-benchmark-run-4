d9d4405182775222da81c3e83da659c18aaa6ccc
Joe Downing <joedow@google.com>
2025-05-23 13:31:19 -0700

    Handle connection change events and set min bitrate
    This CL stops and cleans up the session objects if the connection
    state changes to closed or failed so that we prevent zombie clients
    from running forever.  I'm also setting a minimum bitrate of 1Mbps
    since we've seen that this improves the host-side BWE and overall
    experience.
    
    Change-Id: I44283d163a88c8481cdc354f2688ba5d1c0614a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6581426
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464963}

--- remoting/client/common/remoting_client.cc
+++ remoting/client/common/remoting_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/client/common/remoting_client.h"
 
+#include <cstdint>
 #include <memory>
 #include <string>
 #include <string_view>
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/passthrough_oauth_token_getter.h"
 #include "remoting/client/common/frame_consumer_wrapper.h"
 #include "remoting/client/common/logging.h"
+#include "remoting/proto/control.pb.h"
 #include "remoting/proto/remoting/v1/host_info.pb.h"
 #include "remoting/proto/remoting/v1/remote_support_host_messages.pb.h"
 #include "remoting/protocol/chromium_port_allocator_factory.h"
@@ -29,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/client_authentication_config.h"
 #include "remoting/protocol/connection_to_host.h"
 #include "remoting/protocol/errors.h"
+#include "remoting/protocol/host_stub.h"
 #include "remoting/protocol/ice_config_fetcher_default.h"
 #include "remoting/protocol/jingle_session.h"
 #include "remoting/protocol/jingle_session_manager.h"
@@ -46,6 +49,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/video_codecs/sdp_video_format.h"
 
 namespace remoting {
+
+namespace {
+constexpr std::int32_t kMinBitrateBps = 10485760;
+}
 
 RemotingClient::RemotingClient(
     base::OnceClosure quit_closure,
@@ -241,6 +248,15 @@ void RemotingClient::OnConnectionState(protocol::ConnectionToHost::State state,
                                        protocol::ErrorCode error) {
   CLIENT_LOG << "OnConnectionState change: "
              << protocol::ConnectionToHost::StateToString(state);
+  if (state == protocol::ConnectionToHost::State::CONNECTED &&
+      connection_->host_stub()) {
+    protocol::PeerConnectionParameters peer_connection_params;
+    peer_connection_params.set_preferred_min_bitrate_bps(kMinBitrateBps);
+    connection_->host_stub()->ControlPeerConnection(peer_connection_params);
+  } else if (state == protocol::ConnectionToHost::State::CLOSED ||
+             state == protocol::ConnectionToHost::State::FAILED) {
+    StopSession();
+  }
 }
 
 void RemotingClient::OnConnectionReady(bool ready) {
