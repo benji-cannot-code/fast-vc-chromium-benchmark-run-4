57654fa4ceac7c93e46f60b3c737906202805235
Kent Tamura <tkent@chromium.org>
2025-05-23 02:03:42 -0700

    WTF: Fix a DCHECK failure in ~StringView by StrCat()
        String foo = ...;
        foo = StrCat({"before ", foo, " after"});
    
    The above code caused a DCHECK failure because the initial StringImpl
    for `foo` is destructed before the destruction of a StringView created
    from `foo`.
    
    The runtime behavior has no problems because the StringView is not
    accessed after the StringImpl destruction. In order to avoid this DCHECK
    failure, this CL adds `Clear()` calls, and unexposes StrCat(base::span),
    which clears the passed StringViews.
    
    Bug: 419595599
    Change-Id: Id563b64885b3e9274bd2f671c3a995ab5bc136fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579165
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1464626}

--- third_party/blink/renderer/platform/wtf/text/strcat.cc
+++ third_party/blink/renderer/platform/wtf/text/strcat.cc
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
+namespace {
+
+// Caution: This function calls `Clear()` for items in `pieces` even though
+// the items are `const`, in order to pass checks in ~StringView().
 String StrCat(base::span<const StringView> pieces) {
   size_t size = 0;
   bool is_8bit = true;
@@ -30,6 +34,9 @@ String StrCat(base::span<const StringView> pieces) {
         DCHECK_LT(view[0], 0x0100);
         sub_buffer[0] = view[0];
       }
+#if DCHECK_IS_ON()
+      const_cast<StringView&>(view).Clear();
+#endif
     }
     return impl;
   }
@@ -42,8 +49,17 @@ String StrCat(base::span<const StringView> pieces) {
     } else {
       sub_buffer.copy_from(view.Span16());
     }
+#if DCHECK_IS_ON()
+    const_cast<StringView&>(view).Clear();
+#endif
   }
   return impl;
+}
+
+}  // namespace
+
+String StrCat(std::initializer_list<StringView> pieces) {
+  return StrCat(base::span(pieces));
 }
 
 }  // namespace WTF
