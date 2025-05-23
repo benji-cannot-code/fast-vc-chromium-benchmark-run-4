40e0b3e562d95675752f349f89928bd5988c737a
Sharon Yang <yangsharon@chromium.org>
2025-05-22 18:14:55 -0700

    Introduce default SiteInstanceGroup
    This CL introduces use of default SiteInstanceGroup in lieu of default
    SiteInstance. Sites that used to be added to the default SiteInstance
    will now each get their own SiteInstance and be placed in the default
    SiteInstanceGroup. The default SiteInstance has a placeholder site URL
    of unisolated.invalid. Now sites can all have their own site URL, and
    share the default SiteInstanceGroup. Like the default SiteInstance, the
    default SiteInstanceGroup's lock URL will be an empty URL.
    
    This feature is behind the feature flag kDefaultSiteInstanceGroups,
    which is disabled by default. As such there is no behaviour change in
    this CL.
    
    When enabled, this change does not have any effect on the process
    count. Default SiteInstanceGroup will be used in the same scenarios as
    default SiteInstance is currently used, e.g. on Android where we do not
    have full site isolation. All sites that used to share a process will
    continue to do so. They will each have their own SiteInstance, but will
    all be in the default SiteInstanceGroup.
    
    SiteInstanceGroups are closely tied to processes, so a SiteInstance
    will be assigned to the default SiteInstanceGroup at process assignment
    time, rather than being placed into the default SiteInstance at
    SiteInstance creation time. Like default SiteInstance, default
    SiteInstanceGroup is one per BrowsingInstance.
    
    Other effects of this feature include:
    - When navigating away from the initial empty document, it is not
      guaranteed to be a same-RenderFrameHost navigation.
    - Many SiteInstance comparisons are no longer accurate. These were
      previously equivalent and used in lieu of same-process comparisons,
      or seeing if two frames were local. This is no longer always true,
      so many of these comparisons have been updated to compare either
      SiteInstanceGroups or processes.
    
    
    Note: This change will only experiment on Clank to begin, and not
    Android WebView as the following tests are failing:
    - android.webkit.cts.WebViewTest#testSetInitialScale
    - org.chromium.android_webview.test.AwSettingsTest#testLoadWithOverviewModeWithTwoViews
    
    Doc: https://docs.google.com/document/d/1yjayNYun4ZcBmhFaaLEVjK7OpJr24CFxRLhMVchcm2o/edit?usp=sharing
    
    Test: DefaultSiteInstanceGroupTest, enable and update existing tests
    Bug: 390571607
    Change-Id: Ia918257416d6d0f38e7f5e9c3189e0f9d3558da4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6203249
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464487}

--- content/browser/renderer_host/render_frame_host_impl_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_impl_browsertest.cc
@@ -8605,7 +8605,9 @@ IN_PROC_BROWSER_TEST_F(
                                         ->RequiresDedicatedProcess();
 
   // `shell()` and `second_shell` opened different sites.
-  if (requires_dedicated_process) {
+  // TODO(crbug.com/419469455): Make sure metrics are updated correctly with the
+  // introduction of default SiteInstanceGroup.
+  if (requires_dedicated_process || ShouldUseDefaultSiteInstanceGroup()) {
     EXPECT_THAT(histogram.GetAllSamples(
                     "SiteIsolation."
                     "NewProcessUsedForNavigationWhenSameSiteProcessExists"),
@@ -8619,7 +8621,7 @@ IN_PROC_BROWSER_TEST_F(
 
   ASSERT_TRUE(NavigateToURL(second_shell, url));
   // Now `shell()` and `second_shell` opened the same site.
-  if (requires_dedicated_process) {
+  if (requires_dedicated_process || ShouldUseDefaultSiteInstanceGroup()) {
     EXPECT_THAT(
         histogram.GetAllSamples(
             "SiteIsolation."
