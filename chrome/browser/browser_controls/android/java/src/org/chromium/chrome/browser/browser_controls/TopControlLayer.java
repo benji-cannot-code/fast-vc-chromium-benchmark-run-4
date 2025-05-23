965d8cce91b4c02369c1be47811cb639c109bab3
Mark Schillaci <mschillaci@google.com>
2025-05-23 12:24:28 -0700

    [TopControls] Add a stubbed TopControlsStacker
    This CL adds a simple stubbed TopControlsStacker which will be used to
    keep track of height and y-offset for any top controls. Full
    functionality will be added in future CLs.
    
    AX-Relnotes: N/A
    Bug: 417238089
    Change-Id: Ib9070529bc3356137f7a3f2f72064a6504c02c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6556723
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464914}

---
+++ chrome/browser/browser_controls/android/java/src/org/chromium/chrome/browser/browser_controls/TopControlLayer.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.browser_controls;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.browser_controls.TopControlsStacker.TopControlType;
+import org.chromium.chrome.browser.browser_controls.TopControlsStacker.TopControlVisibility;
+
+/** Interface definition for any View registering itself as a top control. */
+@NullMarked
+public interface TopControlLayer {
+    /** Return the type of the layer. This should not change once the layer is created. */
+    @TopControlType
+    int getTopControlType();
+
+    /** Return the current height of the layer. */
+    int getHeight();
+
+    /** Whether the layer is visible in the UI. */
+    @TopControlVisibility
+    int getTopControlVisibility();
+
+    /**
+     * Return if the layer should contribute to the total height, which a view may not if it draws
+     * over other views, for example the progress bar.
+     */
+    boolean contributesToTotalHeight();
+}
