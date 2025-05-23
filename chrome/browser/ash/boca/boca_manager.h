99decab80e1bc798b78a7828cef80bdfae86ebca
aprilzhou@google.com <aprilzhou@google.com>
2025-05-23 15:00:52 -0700

    Add base framework for Spotlight native CRD client
    Viewing student screen for teacher will be using the native chromium CRD
    client instead of the remotedesktop.google.com website. This CL
    integrates the client into Boca App and subsequent CLs will add
    functionality to send frames to the UI.
    
    Bug: 409613172
    Change-Id: I30b42c4e40d2876d7191bfe3cdf29551f3548f73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579583
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465011}

--- chrome/browser/ash/boca/boca_manager.h
+++ chrome/browser/ash/boca/boca_manager.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_BOCA_BOCA_MANAGER_H_
 #define CHROME_BROWSER_ASH_BOCA_BOCA_MANAGER_H_
 
+#include <memory>
+
 #include "chrome/browser/ash/boca/boca_app_client_impl.h"
 #include "chromeos/ash/components/boca/babelorca/babel_orca_manager.h"
 #include "chromeos/ash/components/boca/babelorca/soda_installer.h"
@@ -14,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/boca/invalidations/invalidation_service_impl.h"
 #include "chromeos/ash/components/boca/on_task/on_task_session_manager.h"
 #include "chromeos/ash/components/boca/session_api/session_client_impl.h"
+#include "chromeos/ash/components/boca/spotlight/spotlight_oauth_token_fetcher.h"
 #include "chromeos/ash/components/boca/spotlight/spotlight_session_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_service.h"
@@ -69,6 +72,7 @@ class BocaManager : public KeyedService {
   void AddObservers(const user_manager::User* user);
 
   std::unique_ptr<babelorca::SodaInstaller> soda_installer_;
+  std::unique_ptr<boca::SpotlightOAuthTokenFetcher> spotlight_token_fetcher_;
   std::unique_ptr<boca::OnTaskSessionManager> on_task_session_manager_;
   std::unique_ptr<boca::SessionClientImpl> session_client_impl_;
   std::unique_ptr<boca::BocaSessionManager> boca_session_manager_;
