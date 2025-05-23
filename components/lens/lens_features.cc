fed4184b4261376a78422fb9c37091548c639a19
Duncan Mercer <mercerd@google.com>
2025-05-23 08:00:56 -0700

    [lensoverlay] Add gradient super G to Lens searchbox
    This adds the new gradient super G icon to the Lens searchbox behind a flag. The parma is default to true but the meta flag is default off.
    
    Bug: 417267919
    Change-Id: I92cdd05b77e90f29e341e844985c4de562bd48db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6581446
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464750}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -490,6 +490,10 @@ constexpr base::FeatureParam<bool>
         true};
 
 constexpr base::FeatureParam<bool>
+    kLensOverlayVisualSelectionUpdatesEnableGradientSuperG{
+        &kLensOverlayVisualSelectionUpdates, "enable-gradient-super-g", true};
+
+constexpr base::FeatureParam<bool>
     kLensOverlayVisualSelectionUpdatesCsbThumbnail{
         &kLensOverlayVisualSelectionUpdates, "enable-csb-thumbnail", true};
 
@@ -1046,6 +1050,11 @@ bool GetVisualSelectionUpdatesEnableGradientRegionStroke() {
 bool GetVisualSelectionUpdatesEnableRegionSelectedGlow() {
   return base::FeatureList::IsEnabled(kLensOverlayVisualSelectionUpdates) &&
          kLensOverlayVisualSelectionUpdatesEnableRegionSelectedGlow.Get();
+}
+
+bool GetVisualSelectionUpdatesEnableGradientSuperG() {
+  return base::FeatureList::IsEnabled(kLensOverlayVisualSelectionUpdates) &&
+         kLensOverlayVisualSelectionUpdatesEnableGradientSuperG.Get();
 }
 
 bool GetVisualSelectionUpdatesEnableCsbThumbnail() {
