dcf6610e2fdc585b12f5a03370e99be75af76930
Karol Sygiet <sygiet@google.com>
2025-05-23 02:05:09 -0700

    [IN] Disable fallback parsing logic in India
    Bug: 393294031
    Change-Id: I227ce79b60ee2ba8b78b89fb05e42527a5c3abd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578528
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464627}

--- components/autofill/core/browser/data_model/addresses/autofill_structured_address_component.cc
+++ components/autofill/core/browser/data_model/addresses/autofill_structured_address_component.cc
@@ -27,6 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_features.h"
 
 namespace autofill {
+namespace {
+
+// The list of countries where the fallback parsing is not supported.
+static constexpr auto countries_not_supporting_fallback_parsing =
+    base::MakeFixedFlatSet<AddressCountryCode>({AddressCountryCode("IN")});
+
+}  // namespace
 
 bool IsLessSignificantVerificationStatus(VerificationStatus left,
                                          VerificationStatus right) {
@@ -491,7 +498,11 @@ void AddressComponent::ParseValueAndAssignSubcomponents() {
   }
 
   // As a final fallback, parse using the fallback method.
-  ParseValueAndAssignSubcomponentsByFallbackMethod();
+  // In some countries (e.g. India), the parsing cannot be reliably implemented
+  // and the fallback method does more harm than good.
+  if (!countries_not_supporting_fallback_parsing.contains(GetCountryCode())) {
+    ParseValueAndAssignSubcomponentsByFallbackMethod();
+  }
 }
 
 bool AddressComponent::ParseValueAndAssignSubcomponentsByI18nParsingRules() {
