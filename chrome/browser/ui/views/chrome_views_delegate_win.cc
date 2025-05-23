a811e45618675cba3777cdb8b81970d20add393a
Keren Zhu <kerenzhu@chromium.org>
2025-05-23 12:42:08 -0700

    glic: use desktop widget for web modals
    Using desktop widgets allows the web modal dialogs to extend beyond the
    boundary of the glic window on Windows.
    
    This also kicks off the larger project that intends to use desktop
    widgets everywhere on desktop aura.
    
    screencast:
    https://drive.google.com/file/d/1TItT0ZqSfkYtvpWHMDpimKfaBftq1q0H/view?usp=sharing.
    It hacks to invoke DesktopDataControlsDialog during glic startup.
    
    Investigation doc (internal):
    https://docs.google.com/document/d/1rY7OVmEry6-SjuL8bWABXxJaQofrOphVblNcqoTamcg/edit?pli=1&tab=t.0#heading=h.9t8nfprvrbr2
    
    Bug: b:409382820, 346974105
    Change-Id: I15ff283dd46bef1519e64510e4ea6594131fa767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578297
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464928}

--- chrome/browser/ui/views/chrome_views_delegate_win.cc
+++ chrome/browser/ui/views/chrome_views_delegate_win.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/native_widget_factory.h"
 #include "chrome/browser/win/app_icon.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
+#include "ui/views/widget/widget_delegate.h"
 
 namespace {
 
@@ -152,6 +153,11 @@ views::NativeWidget* ChromeViewsDelegate::CreateNativeWidget(
       native_widget_type = NativeWidgetType::DESKTOP_NATIVE_WIDGET_AURA;
     }
   }
+
+  if (params->delegate && params->delegate->use_desktop_widget_override()) {
+    native_widget_type = NativeWidgetType::DESKTOP_NATIVE_WIDGET_AURA;
+  }
+
   return ::CreateNativeWidget(native_widget_type, params, delegate);
 }
 
