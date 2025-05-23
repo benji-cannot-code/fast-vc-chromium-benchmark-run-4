1c741b9added9187d7174925d0f8b921c5b5c639
Andrey Davydov <andreydav@google.com>
2025-05-23 08:44:09 -0700

    Support automatic sign-out when Floating SSO is enabled, but FWS is not
    When Floating SSO (i.e. cookie sync) is enabled, but Floating Workspace
    as a feature is not, we still use FloatingWorkspaceService to enable
    automatic sign-out behavior.
    
    Bug: b:360363770
    Test: e2e manually, plus new unit tests variations in this cl
    Change-Id: I1c6bd495131c7f6be549b503a8e418d903771957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578664
    Reviewed-by: Michael Slutskii <slutskii@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464776}

--- chrome/browser/ash/floating_workspace/floating_workspace_util.cc
+++ chrome/browser/ash/floating_workspace/floating_workspace_util.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/ash/floating_sso/floating_sso_service.h"
+#include "chrome/browser/ash/floating_sso/floating_sso_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/components/network/network_handler.h"
@@ -75,6 +77,18 @@ bool IsFloatingWorkspaceEnabled(const Profile* profile) {
 
   // TODO(crbug.com/297795546): Remove external ash feature flag.
   return features::IsFloatingWorkspaceV2Enabled();
+}
+
+bool IsFloatingSsoEnabled(Profile* profile) {
+  if (!ash::features::IsFloatingSsoAllowed()) {
+    return false;
+  }
+  ash::floating_sso::FloatingSsoService* floating_sso_service =
+      ash::floating_sso::FloatingSsoServiceFactory::GetForProfile(profile);
+  if (!floating_sso_service) {
+    return false;
+  }
+  return floating_sso_service->IsFloatingSsoEnabled();
 }
 
 bool IsInternetConnected() {
