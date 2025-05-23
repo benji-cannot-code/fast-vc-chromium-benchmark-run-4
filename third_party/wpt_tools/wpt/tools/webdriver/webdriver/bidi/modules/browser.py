08911b34edfb2e2986e1fc1da7e4d61a381605e0
Jonathan Lee <jonathanjlee@google.com>
2025-05-23 09:25:02 -0700

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 307dcacd9bccd9e66e062f3675c56e49efeca81d
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I93c88330f420099e91bab7bf9c54e37b1339ce03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6581305
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464803}

--- third_party/wpt_tools/wpt/tools/webdriver/webdriver/bidi/modules/browser.py
+++ third_party/wpt_tools/wpt/tools/webdriver/webdriver/bidi/modules/browser.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-from typing import Any, Mapping, MutableMapping
+from typing import Any, Mapping, MutableMapping, Optional
 
 from ._module import BidiModule, command
 
@@ -28,8 +28,19 @@ class Browser(BidiModule):
         return result["clientWindows"]
 
     @command
-    def create_user_context(self) -> Mapping[str, Any]:
-        return {}
+    def create_user_context(
+        self, accept_insecure_certs: Optional[bool] = None,
+        proxy: Optional[Mapping[str, Any]] = None
+    ) -> Mapping[str, Any]:
+        params: MutableMapping[str, Any] = {}
+
+        if accept_insecure_certs is not None:
+            params["acceptInsecureCerts"] = accept_insecure_certs
+
+        if proxy is not None:
+            params["proxy"] = proxy
+
+        return params
 
     @create_user_context.result
     def _create_user_context(self, result: Mapping[str, Any]) -> Any:
