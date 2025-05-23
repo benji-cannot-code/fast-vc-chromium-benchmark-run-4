9bae6505f2cb35b4808afdadcdd9a6606beeb19f
Dana Fried <dfried@chromium.org>
2025-05-23 11:20:28 -0700

    [User Education] Don't close tutorial bubble until after Tracker check.
    This was causing startup promos to cause tutorial tests to flake in a
    test environment. The flake was caused by the IPH getting all the way
    to trying to show, closing the tutorial bubble, and realizing that -
    since it was a test - the Tracker was blocking the promo.
    
    Now, closing outstanding bubbles happens after the Tracker check.
    
    Bug: 411232061
    Change-Id: I2c5a4bb24a909ba13c970bd6d6a2b1730f881f90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580521
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464874}

--- components/user_education/common/feature_promo/impl/feature_promo_controller_25.cc
+++ components/user_education/common/feature_promo/impl/feature_promo_controller_25.cc
@@ -384,9 +384,6 @@ FeaturePromoResult FeaturePromoController25::ShowPromo(PromoData& promo_data) {
                  : FeaturePromoClosedReason::kOverrideForPrecedence);
   }
 
-  // If the session policy allows overriding other help bubbles, close them.
-  CloseHelpBubbleIfPresent(anchor_element->context());
-
   // TODO(crbug.com/40200981): Currently this must be called before
   // ShouldTriggerHelpUI() below. See bug for details.
   if (build_params.spec->promo_type() !=
@@ -403,6 +400,9 @@ FeaturePromoResult FeaturePromoController25::ShowPromo(PromoData& promo_data) {
       !feature_engagement_tracker()->ShouldTriggerHelpUI(feature)) {
     return FeaturePromoResult::kBlockedByConfig;
   }
+
+  // If the session policy allows overriding other help bubbles, close them.
+  CloseHelpBubbleIfPresent(anchor_element->context());
 
   // Store the current promo.
   set_current_promo(std::move(lifecycle));
