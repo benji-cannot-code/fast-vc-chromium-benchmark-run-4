f1b43c2114bcb9ea30cc67d0217118f0db51a1ef
Justin Lulejian <jlulejian@chromium.org>
2025-05-23 11:06:13 -0700

    [Extensions] Conditionally CHECK valid UUIDs for destructing functions.
    Before this change, if an extension function failed to initialize (and
    therefore not proceed to set its UUID) it would then have an invalid
    (unset) base::Uuid. When the ExtensionFunction destructs though there
    is a CHECK assumes all service worker based functions have valid UUIDs.
    
    After this change we only perform the valid UUID check if an extension
    function should have initialized enough to have a valid UUID. This is
    done by checking a new member var that is set when the function
    should've finished initializing.
    
    We could've checked ExtensionFunction did_run_ and/or response_type_,
    but did_run_ doesn't cover all cases. response_type_ kSucceeded and
    kBadMessage seem like they should have always valid UUIDs, but kFailed
    sometimes has an valid UUID so the below seemed the most accurate way
    to do this check.
    
    Bug: 416377497
    Change-Id: I2e803ee0b42418067a21f262e48b474f6f0fd1b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579012
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464863}

--- extensions/browser/extension_function.h
+++ extensions/browser/extension_function.h
@@ -401,6 +401,9 @@ class ExtensionFunction : public base::RefCountedThreadSafe<
 
   bool is_from_service_worker() const { return worker_id_.has_value(); }
 
+  bool did_initialize() const { return did_initialize_; }
+  void set_did_initialize() { did_initialize_ = true; }
+
   ResponseType* response_type() const { return response_type_.get(); }
 
   // Whether this function has responded.
@@ -669,6 +672,11 @@ class ExtensionFunction : public base::RefCountedThreadSafe<
   // Any class that gets a malformed message should set this to true before
   // returning.  Usually we want to kill the message sending process.
   bool bad_message_ = false;
+
+  // Set to true when an extension function is created successfully without
+  // error. This implies all setup state should be valid on the function for
+  // some assertions made during destruction.
+  bool did_initialize_ = false;
 
   // Set to true when RunWithValidation() is called, to look for callers using
   // the method more than once on a single ExtensionFunction. Note that some
