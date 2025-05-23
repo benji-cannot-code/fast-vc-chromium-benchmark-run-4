9dc69a3406c1bc2cacafb02bea79eb2cbfe5c42f
Jameson Thies <jthies@google.com>
2025-05-23 14:54:46 -0700

    Add USB limit notification
    Add a notification to warn users when they have reached the maximum
    number of supported USB devices or endpoints. This will only be signaled
    on platforms with a low limit on the number of simultaneously connected
    USB devices.
    
    Bug: 416716383
    Test: UsbPeripheralNotificationControllerTest*,
    PeripheralNotificationManagerTest*, manually checked notification.
    
    Change-Id: Ie997a1795b4580de26724fe7d61262da5bd6f4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6569653
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Jameson Thies <jthies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465009}

--- chromeos/ash/components/peripheral_notification/peripheral_notification_manager_unittest.cc
+++ chromeos/ash/components/peripheral_notification/peripheral_notification_manager_unittest.cc
@@ -76,6 +76,10 @@ class FakeObserver : public PeripheralNotificationManager::Observer {
     return num_speed_limiting_cable_notification_calls_;
   }
 
+  size_t num_usb_device_or_endpoint_limit_notification_calls() const {
+    return num_usb_device_or_endpoint_limit_notification_calls_;
+  }
+
   bool is_current_guest_device_tbt_only() const {
     return is_current_guest_device_tbt_only_;
   }
@@ -118,6 +122,10 @@ class FakeObserver : public PeripheralNotificationManager::Observer {
     ++num_speed_limiting_cable_notification_calls_;
   }
 
+  void OnUsbDeviceOrEndpointLimit() override {
+    ++num_usb_device_or_endpoint_limit_notification_calls_;
+  }
+
  private:
   size_t num_limited_performance_notification_calls_ = 0u;
   size_t num_guest_notification_calls_ = 0u;
@@ -128,6 +136,7 @@ class FakeObserver : public PeripheralNotificationManager::Observer {
   size_t num_invalid_usb4_cable_notification_calls_ = 0u;
   size_t num_invalid_tbt_cable_notification_calls_ = 0u;
   size_t num_speed_limiting_cable_notification_calls_ = 0u;
+  size_t num_usb_device_or_endpoint_limit_notification_calls_ = 0u;
   bool is_current_guest_device_tbt_only_ = false;
 };
 
@@ -208,6 +217,10 @@ class PeripheralNotificationManagerTest : public testing::Test {
 
   size_t GetSpeedLimitingCableNotificationObserverCalls() {
     return fake_observer_.num_speed_limiting_cable_notification_calls();
+  }
+
+  size_t GetUsbDeviceOrEndpointLimitNotificationObserverCalls() {
+    return fake_observer_.num_usb_device_or_endpoint_limit_notification_calls();
   }
 
   bool GetIsCurrentGuestDeviceTbtOnly() {
@@ -605,6 +618,46 @@ TEST_F(PeripheralNotificationManagerTest, SpeedLimitingCableWarning) {
       "Ash.Peripheral.ConnectivityResults",
       PeripheralNotificationManager::PeripheralConnectivityResults::
           kSpeedLimitingCable,
+      1);
+}
+
+TEST_F(PeripheralNotificationManagerTest, UsbDeviceLimitWarning) {
+  InitializeManager(/*is_guest_profile=*/false,
+                    /*is_pcie_tunneling_allowed=*/false);
+  EXPECT_EQ(0u, GetUsbDeviceOrEndpointLimitNotificationObserverCalls());
+  histogram_tester_.ExpectBucketCount(
+      "Ash.Peripheral.ConnectivityResults",
+      PeripheralNotificationManager::PeripheralConnectivityResults::
+          kUsbDeviceOrEndpointLimit,
+      0);
+
+  typecd::UsbLimitType usb_limit_type = typecd::UsbLimitType::kDeviceLimit;
+  fake_typecd_client()->EmitUsbLimitSignal(usb_limit_type);
+  EXPECT_EQ(1u, GetUsbDeviceOrEndpointLimitNotificationObserverCalls());
+  histogram_tester_.ExpectBucketCount(
+      "Ash.Peripheral.ConnectivityResults",
+      PeripheralNotificationManager::PeripheralConnectivityResults::
+          kUsbDeviceOrEndpointLimit,
+      1);
+}
+
+TEST_F(PeripheralNotificationManagerTest, UsbEndpointLimitWarning) {
+  InitializeManager(/*is_guest_profile=*/false,
+                    /*is_pcie_tunneling_allowed=*/false);
+  EXPECT_EQ(0u, GetUsbDeviceOrEndpointLimitNotificationObserverCalls());
+  histogram_tester_.ExpectBucketCount(
+      "Ash.Peripheral.ConnectivityResults",
+      PeripheralNotificationManager::PeripheralConnectivityResults::
+          kUsbDeviceOrEndpointLimit,
+      0);
+
+  typecd::UsbLimitType usb_limit_type = typecd::UsbLimitType::kEndpointLimit;
+  fake_typecd_client()->EmitUsbLimitSignal(usb_limit_type);
+  EXPECT_EQ(1u, GetUsbDeviceOrEndpointLimitNotificationObserverCalls());
+  histogram_tester_.ExpectBucketCount(
+      "Ash.Peripheral.ConnectivityResults",
+      PeripheralNotificationManager::PeripheralConnectivityResults::
+          kUsbDeviceOrEndpointLimit,
       1);
 }
 
