c8766ae5eedda62e964c6bbaad67621b9abb964e
caott <caott@google.com>
2025-05-23 14:13:23 -0700

    [OnTask] stop processing notification after reaching the end time for countdown
    Bug: b:419370868
    Change-Id: I0133509cfd2d9e57d4093c472b84e1afa09ceef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578968
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Luke Cao <caott@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464995}

--- chromeos/ash/components/boca/on_task/on_task_notifications_manager.cc
+++ chromeos/ash/components/boca/on_task/on_task_notifications_manager.cc
@@ -227,9 +227,8 @@ void OnTaskNotificationsManager::CreateNotificationInternal(
       // When the countdown finishes, immediately clear the last countdown
       // notification.
       ClearNotification(params.id);
-    } else {
-      StopProcessingNotification(params.id);
     }
+    StopProcessingNotification(params.id);
     return;
   }
 
