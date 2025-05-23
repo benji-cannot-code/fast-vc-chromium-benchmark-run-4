9157faf19e20b0937ef478f50a6377fe860f5195
Mikel Astiz <mastiz@chromium.org>
2025-05-23 08:06:27 -0700

    [sync] Fix live tests waiting indefinitely for ConsentLevel::kSync
    This reverts https://crrev.com/c/6568752 and fixes the CHECKs that were
    failing, after verifying that some of the function calls, despite their
    name and signature, do not guarantee as postcondition that
    IdentityManager reports ConsentLevel::kSync.
    
    A TODO is added to revisit if such misleading functions should be
    redesigned.
    
    Bug: 41496149
    Change-Id: I5bdb758d62acfe8191def752f4b2dd2cbfc93c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578638
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464755}

--- chrome/browser/sync/test/integration/sync_service_impl_harness.cc
+++ chrome/browser/sync/test/integration/sync_service_impl_harness.cc
@@ -251,9 +251,12 @@ bool SyncServiceImplHarness::SignInPrimaryAccount(
 
   signin::IdentityManager* identity_manager =
       IdentityManagerFactory::GetForProfile(profile_);
+  // Note that `consent_level` is not actually guaranteed at this stage. In
+  // particular, live tests require closing the sync confirmation dialog before
+  // signin::ConsentLevel::kSync is granted.
   CHECK(identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin));
-  CHECK(identity_manager->HasPrimaryAccount(consent_level));
-  CHECK(identity_manager->HasPrimaryAccountWithRefreshToken(consent_level));
+  CHECK(identity_manager->HasPrimaryAccountWithRefreshToken(
+      signin::ConsentLevel::kSignin));
   CHECK(!service()->GetAccountInfo().IsEmpty());
 
   return true;
@@ -352,6 +355,14 @@ bool SyncServiceImplHarness::SetupSyncNoWaitForCompletion(
     return false;
   }
 
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(profile_);
+  // Note that `ConsentLevel::kSync` is not actually guaranteed at this stage.
+  // Namely, live tests require closing the sync confirmation dialog before
+  // `ConsentLevel::kSync` is granted. This is achieved later below with
+  // `ConfirmSyncUI()`.
+  CHECK(identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin));
+
   if (!AwaitEngineInitialization()) {
     return false;
   }
@@ -365,9 +376,16 @@ bool SyncServiceImplHarness::SetupSyncNoWaitForCompletion(
   // Notify SyncServiceImpl that we are done with configuration.
   FinishSyncSetup();
 
-  if (signin_type_ == SigninType::UI_SIGNIN) {
-    return signin_delegate_->ConfirmSyncUI(profile_);
+  if (signin_type_ == SigninType::UI_SIGNIN &&
+      !signin_delegate_->ConfirmSyncUI(profile_)) {
+    return false;
   }
+
+  CHECK(identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSync));
+  CHECK(identity_manager->HasPrimaryAccountWithRefreshToken(
+      signin::ConsentLevel::kSync));
+  CHECK(!service()->GetAccountInfo().IsEmpty());
+
   return true;
 }
 
