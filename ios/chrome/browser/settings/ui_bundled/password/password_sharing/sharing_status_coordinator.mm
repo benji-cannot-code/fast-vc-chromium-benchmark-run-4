262390b704e244b31f596b3c7585db975e7d083e
Briana McClure <bmcclure@google.com>
2025-05-23 07:58:47 -0700

    [iOS] Mark half the Welcome Back Features Used
    This CL marks Lens, Locked Incognito Tabs, Never Forget Passwords, and
    Share Passwords as used when the user interacts with them. This prevents
    the Welcome Back promo from showing features the user has already used.
    
    Bug: 410072328
    Change-Id: Ib92f0c97e273d5e4b6ca4018ac5c5758227c642a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575602
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Auto-Submit: Briana McClure <bmcclure@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464746}

--- ios/chrome/browser/settings/ui_bundled/password/password_sharing/sharing_status_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_sharing/sharing_status_coordinator.mm
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/password/password_sharing/sharing_status_coordinator.h"
 
 #import "ios/chrome/browser/favicon/model/ios_chrome_favicon_loader_factory.h"
+#import "ios/chrome/browser/first_run/ui_bundled/best_features/ui/best_features_item.h"
+#import "ios/chrome/browser/first_run/ui_bundled/features.h"
+#import "ios/chrome/browser/first_run/ui_bundled/welcome_back/model/welcome_back_prefs.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/password_sharing/password_sharing_constants.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/password_sharing/password_sharing_metrics.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/password_sharing/recipient_info.h"
@@ -125,6 +128,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)startPasswordSharing {
   [self.delegate startPasswordSharing];
+  // Notify Welcome Back to remove Share Passwords from the eligible features.
+  if (first_run::IsWelcomeBackInFirstRunEnabled()) {
+    MarkWelcomeBackFeatureUsed(BestFeaturesItemType::kSharePasswordsWithFamily);
+  }
 }
 
 - (void)changePasswordLinkWasTapped {
--- ios/chrome/browser/settings/ui_bundled/privacy/incognito/incognito_lock_mediator.mm
+++ ios/chrome/browser/settings/ui_bundled/privacy/incognito/incognito_lock_mediator.mm
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
 #import "components/prefs/pref_service.h"
+#import "ios/chrome/browser/first_run/ui_bundled/best_features/ui/best_features_item.h"
+#import "ios/chrome/browser/first_run/ui_bundled/features.h"
+#import "ios/chrome/browser/first_run/ui_bundled/welcome_back/model/welcome_back_prefs.h"
 #import "ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_constants.h"
 #import "ios/chrome/browser/settings/ui_bundled/privacy/incognito/incognito_lock_consumer.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_backed_boolean.h"
@@ -75,6 +78,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           IncognitoLockSettingInteraction::kHideWithReauthSelected);
       base::RecordAction(
           base::UserMetricsAction("IOS.Settings.IncognitoLock.HideWithReauth"));
+      // Notify Welcome Back to remove Locked Incognito from the eligible
+      // features.
+      if (first_run::IsWelcomeBackInFirstRunEnabled()) {
+        MarkWelcomeBackFeatureUsed(BestFeaturesItemType::kLockedIncognitoTabs);
+      }
       break;
     case IncognitoLockState::kSoftLock:
       _incognitoReauthPref.value = false;
