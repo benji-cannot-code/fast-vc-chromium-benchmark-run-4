b3594578560fd48e3b0beecb82f32c9d5a10e35f
Owen Min <zmin@chromium.org>
2025-05-23 15:26:16 -0700

    Fetch user remote command only when CEC is enabled
    For now, we rely on if DM server has set user remote command
    topic in Policy data.
    
    However, this solution is hacky as the topic field is not
    designed for this purpose and will be deprecated in the future.
    
    Long term, we should make DM server to use `cec_enabled` which is a more
    specific signal that indicates the CEC state of the domain.
    
    Bug: 419832738
    Change-Id: I3c67e3c5635f38463d0725699b42c4c72e86773d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579328
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465026}

--- components/policy/core/common/remote_commands/remote_commands_service.cc
+++ components/policy/core/common/remote_commands/remote_commands_service.cc
@@ -12,15 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_is_test.h"
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
 #include "components/policy/core/common/cloud/cloud_policy_store.h"
 #include "components/policy/core/common/cloud/cloud_policy_validator.h"
 #include "components/policy/core/common/cloud/enterprise_metrics.h"
+#include "components/policy/core/common/cloud/policy_invalidation_util.h"
+#include "components/policy/core/common/features.h"
 #include "components/policy/core/common/policy_logger.h"
 #include "components/policy/core/common/remote_commands/remote_commands_factory.h"
 #include "components/policy/core/common/remote_commands/remote_commands_fetch_reason.h"
@@ -215,13 +219,7 @@ RemoteCommandsService::~RemoteCommandsService() = default;
 
 bool RemoteCommandsService::FetchRemoteCommands(
     RemoteCommandsFetchReason reason) {
-  if (!client_->is_registered()) {
-    LOG_POLICY(WARNING, REMOTE_COMMANDS) << "Client is not registered.";
-    return false;
-  }
-
-  if (command_fetch_in_progress_) {
-    has_enqueued_fetch_request_ = true;
+  if (!CanFetchRemoteCommands()) {
     return false;
   }
 
@@ -369,6 +367,43 @@ void RemoteCommandsService::EnqueueCommand(
   RecordReceivedRemoteCommand(RemoteCommandMetricFromType(command.type()));
 
   queue_.AddJob(std::move(job));
+}
+
+bool RemoteCommandsService::CanFetchRemoteCommands() {
+  if (!client_->is_registered()) {
+    LOG_POLICY(WARNING, REMOTE_COMMANDS) << "Client is not registered.";
+    return false;
+  }
+
+#if !BUILDFLAG(IS_CHROMEOS)
+  // We need to check if CEC is enabled.
+  if (scope_ == PolicyInvalidationScope::kUser) {
+    const em::PolicyData* policy = store_->policy();
+    if (!policy) {
+      LOG_POLICY(WARNING, REMOTE_COMMANDS) << "Policy is not available.";
+      return false;
+    }
+
+    if (base::FeatureList::IsEnabled(features::kUseCECFlagInPolicyData)) {
+      if (!policy->cec_enabled()) {
+        LOG_POLICY(WARNING, REMOTE_COMMANDS) << "CEC is not enabled.";
+        return false;
+      }
+    } else {
+      invalidation::Topic topic;
+      if (!GetRemoteCommandTopicFromPolicy(*policy, &topic)) {
+        LOG_POLICY(WARNING, REMOTE_COMMANDS) << "CEC is not enabled.";
+        return false;
+      }
+    }
+  }
+#endif  // !BUILDFLAG(IS_CHROMEOS)
+
+  if (command_fetch_in_progress_) {
+    has_enqueued_fetch_request_ = true;
+    return false;
+  }
+  return true;
 }
 
 void RemoteCommandsService::OnJobStarted(RemoteCommandJob* command) {}
