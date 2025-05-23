137dc80c5da4aa96e20a8547cade10bf2af5f654
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-05-23 16:50:16 -0700

    [Code Health] Update InfobarViewTest to BrowserTest
    Per guidelines in BrowserWithTestWindowTest, InfobarViewTest is being
    updated to a BrowserTest.
    
    Bug: 384547121
    Change-Id: Ib4b9374332b6c8d7e9019723c6b0c10a98fe8afa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6581926
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465063}

--- chrome/browser/ui/views/infobars/infobar_view_browsertest.cc
+++ chrome/browser/ui/views/infobars/infobar_view_browsertest.cc
@@ -5,16 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/infobars/infobar_view.h"
 
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/infobars/infobar_container_view.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/base/in_process_browser_test.h"
 #include "components/infobars/content/content_infobar_manager.h"
+#include "content/public/test/browser_test.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/test/ax_event_counter.h"
 #include "ui/views/test/views_test_base.h"
-#include "ui/views/test/widget_test.h"
-#include "ui/views/widget/unique_widget_ptr.h"
 
 class TestInfoBarDelegate : public infobars::InfoBarDelegate {
  public:
@@ -28,82 +28,27 @@ class TestInfoBarDelegate : public infobars::InfoBarDelegate {
   InfoBarIdentifier GetIdentifier() const override { return TEST_INFOBAR; }
 };
 
-class InfoBarViewTest : public BrowserWithTestWindowTest {
- public:
-  InfoBarViewTest() = default;
-
-  InfoBarViewTest(const InfoBarViewTest&) = delete;
-  InfoBarViewTest& operator=(const InfoBarViewTest&) = delete;
-
-  ~InfoBarViewTest() override = default;
-
-  // ChromeViewsTestBase:
-  void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
-
-    AddTab(browser(), GURL("about:blank"));
-    widget_ = std::make_unique<views::Widget>();
-    views::Widget::InitParams params =
-        CreateParams(views::Widget::InitParams::CLIENT_OWNS_WIDGET,
-                     views::Widget::InitParams::TYPE_POPUP);
-    params.bounds = gfx::Rect(50, 50, 650, 650);
-    widget_->Init(std::move(params));
-    auto* root = widget_->GetRootView();
-
-    // Accessible events will not fire until View is added to a RootView's tree.
-    auto* added_container =
-        root->AddChildView(std::make_unique<InfoBarContainerView>(nullptr));
-
-    infobar_container_view_ = added_container;
-    infobar_container_view_->ChangeInfoBarManager(infobar_manager());
-
-    widget_->Show();
-  }
-
-  void TearDown() override {
-    DetachContainer();
-    infobar_container_view_ = nullptr;
-    widget_.reset();
-    BrowserWithTestWindowTest::TearDown();
-  }
-
+class InfoBarViewBrowserTest : public InProcessBrowserTest {
+ protected:
   infobars::ContentInfoBarManager* infobar_manager() {
     return infobars::ContentInfoBarManager::FromWebContents(
-        browser()->tab_strip_model()->GetWebContentsAt(0));
-  }
-
-  views::Widget::InitParams CreateParams(
-      views::Widget::InitParams::Ownership ownership,
-      views::Widget::InitParams::Type type) {
-    views::Widget::InitParams params(ownership, type);
-    params.context = GetContext();
-    return params;
+        browser()->tab_strip_model()->GetActiveWebContents());
   }
 
   InfoBarContainerView* info_bar_container_view() {
-    return infobar_container_view_;
+    return BrowserView::GetBrowserViewForBrowser(browser())
+        ->infobar_container();
   }
-
-  // Detaches |infobar_container_view_| from infobar_manager(), so that newly-
-  // created infobars will not be placed in a container.  This can be used to
-  // simulate creating an infobar in a background tab.
-  void DetachContainer() {
-    infobar_container_view_->ChangeInfoBarManager(nullptr);
-  }
-
- private:
-  raw_ptr<InfoBarContainerView> infobar_container_view_ = nullptr;
-  views::UniqueWidgetPtr widget_;
 };
 
-TEST_F(InfoBarViewTest, AlertAccessibleEvent) {
+IN_PROC_BROWSER_TEST_F(InfoBarViewBrowserTest, AlertAccessibleEvent) {
   views::test::AXEventCounter counter(views::AXUpdateNotifier::Get());
   EXPECT_EQ(0, counter.GetCount(ax::mojom::Event::kAlert));
   TestInfoBarDelegate::Create(infobar_manager());
   EXPECT_EQ(1, counter.GetCount(ax::mojom::Event::kAlert));
 }
 
-TEST_F(InfoBarViewTest, AccessibleProperties) {
+IN_PROC_BROWSER_TEST_F(InfoBarViewBrowserTest, AccessibleProperties) {
   // InfoBarView accessible properties test.
   InfoBarView* view = TestInfoBarDelegate::Create(infobar_manager());
   ui::AXNodeData data;
