fd04979caa0e0033996ab382cc224b36beac0138
Lei Zhang <thestig@chromium.org>
2025-05-22 18:45:11 -0700

    Remove viz::Selection::ToString()
    This was originally added for content::RenderWidgetSurfaceProperties in
    https://crrev.com/524326, but RenderWidgetSurfaceProperties got deleted
    in https://crrev.com/717799. So this has been dead code for several
    years now.
    
    Change-Id: I8409ceda179339ade348fc85e012f4f90fcd4b87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6577969
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1464502}

--- components/viz/common/quads/selection.h
+++ components/viz/common/quads/selection.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_COMMON_QUADS_SELECTION_H_
 #define COMPONENTS_VIZ_COMMON_QUADS_SELECTION_H_
 
-#include "base/strings/stringprintf.h"
-
 namespace viz {
 
 template <typename BoundType>
@@ -15,12 +13,8 @@ struct Selection {
   Selection() = default;
   ~Selection() = default;
 
-  BoundType start, end;
-
-  std::string ToString() const {
-    return base::StringPrintf("Selection(%s, %s)", start.ToString().c_str(),
-                              end.ToString().c_str());
-  }
+  BoundType start;
+  BoundType end;
 
   friend bool operator==(const Selection<BoundType>&,
                          const Selection<BoundType>&) = default;
