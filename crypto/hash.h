aaf8d7401f0e8578da8412912167527943a60a5e
Nick Harper <nharper@chromium.org>
2025-05-23 12:22:18 -0700

    Add SHA-384 to crypto::hash.
    Bug: 392929826
    Change-Id: Iec2b2e1d5d89aa993193b84843d01a143b7fcaae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579785
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Auto-Submit: Nick Harper <nharper@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464911}

--- crypto/hash.h
+++ crypto/hash.h
@@ -20,6 +20,7 @@ namespace crypto::hash {
 
 inline constexpr size_t kSha1Size = 20;
 inline constexpr size_t kSha256Size = 32;
+inline constexpr size_t kSha384Size = 48;
 inline constexpr size_t kSha512Size = 64;
 
 // Unless your code needs to be generic over HashKind, use one of these
@@ -41,6 +42,7 @@ CRYPTO_EXPORT std::array<uint8_t, kSha512Size> Sha512(std::string_view data);
 enum HashKind {
   kSha1,
   kSha256,
+  kSha384,
   kSha512,
 };
 
@@ -50,6 +52,8 @@ inline constexpr size_t DigestSizeForHashKind(HashKind k) {
       return kSha1Size;
     case kSha256:
       return kSha256Size;
+    case kSha384:
+      return kSha384Size;
     case kSha512:
       return kSha512Size;
   }
