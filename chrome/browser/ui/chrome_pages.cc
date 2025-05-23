d35e0350b60d26230e9614923478b942629794b7
Teresa Mao <temao@chromium.org>
2025-05-23 14:38:39 -0700

    data-sharing: Add a 3-dot menu button to Recent Activity.
    The menu contains a "See All Activity" option that opens the activity
    logs page.
    
    Fixed: 410609387
    Change-Id: Id7685aca10049b23fe2f7e6217f452fd58de973b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6478654
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465001}

--- chrome/browser/ui/chrome_pages.cc
+++ chrome/browser/ui/chrome_pages.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_node.h"
 #include "components/commerce/core/commerce_constants.h"
+#include "components/data_sharing/public/features.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/safe_browsing/core/common/safe_browsing_settings_metrics.h"
@@ -630,6 +631,12 @@ void ShowEnterpriseManagementPageInTabbedBrowser(Browser* browser) {
   // Management shows in a tab because it has a "back" arrow that takes the
   // user to the Chrome browser about page, which is part of browser settings.
   ShowSingletonTabIgnorePathOverwriteNTP(browser, GURL(kChromeUIManagementURL));
+}
+
+void ShowSharedTabGroupActivity(Profile* profile) {
+  Browser* browser = GetOrCreateBrowserForProfile(profile);
+  ShowSingletonTab(browser,
+                   GURL(data_sharing::features::kActivityLogsURL.Get()));
 }
 
 #if BUILDFLAG(IS_CHROMEOS)
