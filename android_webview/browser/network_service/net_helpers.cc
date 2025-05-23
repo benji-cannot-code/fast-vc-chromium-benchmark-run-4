83bd34919acd126d9af2db44abeff9ae9f8545ea
Abhijith Nair <abhijithnair@google.com>
2025-05-23 05:58:09 -0700

    [WV] Add histograms related to HTTP cache limit
    Bug: 417434323
    Change-Id: Id0726949c06910d3579df07ab7d319307f65a8dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578736
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464685}

--- android_webview/browser/network_service/net_helpers.cc
+++ android_webview/browser/network_service/net_helpers.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/common/url_constants.h"
 #include "base/android/path_utils.h"
 #include "base/check_op.h"
+#include "base/metrics/histogram_functions.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_request_headers.h"
 #include "url/gurl.h"
@@ -29,6 +30,7 @@ int UpdateCacheControlFlags(int load_flags, int cache_control_flags) {
   return load_flags;
 }
 
+// Guaranteed to return a valid cache size
 int GetHttpCacheSizeInternal() {
   constexpr int DEFAULT_CACHE_LIMIT = 20 * 1024 * 1024;  // 20MiB
 
@@ -133,11 +135,16 @@ bool ShouldBlockURL(const GURL& url,
 
 int GetHttpCacheSize() {
   // static so that `GetHttpCacheSizeInternal()` is called only once.
-  static int cache_limit{GetHttpCacheSizeInternal()};
-  // Using WARNING instead of INFO since usage of INFO is unallowed.
-  // This is semantically not a warning and is temporary till the
-  // HTTP Cache size experiment has landed.
-  LOG(WARNING) << "HTTP Cache size is: " << cache_limit;
+  static int cache_limit = -1;
+  if (cache_limit == -1) {
+    cache_limit = GetHttpCacheSizeInternal();
+    // Using WARNING instead of INFO since usage of INFO is unallowed.
+    // This is semantically not a warning and is temporary till the
+    // HTTP Cache size experiment has landed.
+    LOG(WARNING) << "HTTP Cache size is: " << cache_limit;
+    base::UmaHistogramCounts10000("Android.WebView.HttpCacheSizeLimit",
+                                  cache_limit / (1024 * 1024));
+  };
   return cache_limit;
 }
 
