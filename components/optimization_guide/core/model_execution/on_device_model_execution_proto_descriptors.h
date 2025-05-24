cd82a7f4980fbe57127b0d23a3a17489fe973378
Mike Wittman <wittman@chromium.org>
2025-05-23 17:40:12 -0700

    Support bool and numeric fields in FieldwiseResponseParser
    Adds support for parsing numeric values from model output and writing
    to response proto fields.  Enum field types remain to be implemented in
    a follow-on CL.
    
    Bug: 383761415
    Change-Id: Id7992cc1ecf0ca15685b653052ef1673951a2ca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6554463
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465083}

--- components/optimization_guide/core/model_execution/on_device_model_execution_proto_descriptors.h
+++ components/optimization_guide/core/model_execution/on_device_model_execution_proto_descriptors.h
@@ -13,15 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/notreached.h"
 #include "base/values.h"
+#include "components/optimization_guide/core/model_execution/on_device_model_execution_proto_status.h"
 #include "components/optimization_guide/proto/descriptors.pb.h"
 #include "components/optimization_guide/proto/model_execution.pb.h"
 
 namespace optimization_guide {
-
-enum class ProtoStatus {
-  kOk = 0,
-  kError = 1,
-};
 
 // Returns the value of `proto_field` from `msg`.
 // Returns nullopt when the `proto_field` does not reference a valid field, or
@@ -39,11 +35,12 @@ std::unique_ptr<google::protobuf::MessageLite> GetProtoFromAny(
 std::unique_ptr<google::protobuf::MessageLite> BuildMessage(
     const std::string& proto_name);
 
-// Sets `value` in `msg`'s `proto_field`. Returns kError if `proto_field` is not
-// a valid string type field.
-ProtoStatus SetProtoValue(google::protobuf::MessageLite* msg,
-                          const proto::ProtoField& proto_field,
-                          const std::string& value);
+// Sets `value` in `msg`'s `proto_field`. Converts to non-string fields using
+// ValueConverter<T>::TryConvertFromString(). Returns kError if the value is not
+// convertible to the `proto_field` type.
+ProtoStatus SetProtoValueFromString(google::protobuf::MessageLite* msg,
+                                    const proto::ProtoField& proto_field,
+                                    const std::string& value);
 
 // Get immutable value for a singular message field.
 // Analogous to google::protobuf::Reflection::GetMessage.
