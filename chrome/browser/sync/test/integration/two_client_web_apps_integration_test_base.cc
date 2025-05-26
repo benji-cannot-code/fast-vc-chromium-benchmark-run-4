ee467dda7e275346cdd5d76f2ecd08d1f1167780
Mikel Astiz <mastiz@chromium.org>
2025-05-26 06:38:14 -0700

    [sync] Improve integration test support for the advanced sync setup flow
    Callers to SyncServiceImplHarness::SetupSync() with a custom
    configuration (callback passed) are now required to explicitly invoke
    SyncUserSettings::SetInitialSyncFeatureSetupComplete().
    
    This also means, SetupSync() may also be used for cases where the test
    intentionally wants to avoid such call. Before this patch, such tests
    made a questionable use of
    SyncServiceImplHarness::SignInPrimaryAccount() with
    ConsentLevel::kSync, leading to a non-realistic state (and one that
    definitely cannot be reproduced in Live integration tests).
    
    The function that takes the customization callback is also renamed
    for extra clarity.
    
    Change-Id: Ic0c4891b6ae40f5bfc6b32843374e0d831e03cb5
    Bug: 41496149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6582553
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465491}

--- chrome/browser/sync/test/integration/two_client_web_apps_integration_test_base.cc
+++ chrome/browser/sync/test/integration/two_client_web_apps_integration_test_base.cc
@@ -137,7 +137,7 @@ void TwoClientWebAppsIntegrationTestBase::SetUpOnMainThread() {
       continue;
     }
 
-    ASSERT_TRUE(GetClient(i)->SetupSync(
+    ASSERT_TRUE(GetClient(i)->SetupSyncWithCustomSettings(
         base::BindLambdaForTesting([](syncer::SyncUserSettings* user_settings) {
 #if BUILDFLAG(IS_CHROMEOS)
           user_settings->SetSelectedTypes(/*sync_everything=*/false,
@@ -149,6 +149,8 @@ void TwoClientWebAppsIntegrationTestBase::SetUpOnMainThread() {
           user_settings->SetSelectedTypes(
               /*sync_everything=*/false,
               /*types=*/{syncer::UserSelectableType::kApps});
+          user_settings->SetInitialSyncFeatureSetupComplete(
+              syncer::SyncFirstSetupCompleteSource::ADVANCED_FLOW_CONFIRM);
 #endif  // BUILDFLAG(IS_CHROMEOS)
         })));
   }
