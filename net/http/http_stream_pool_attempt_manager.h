43abcc3602d89bca16fa54eec45479c8ce87a0a9
Kenichi Ishibashi <bashi@chromium.org>
2025-05-26 02:05:43 -0700

    HttpStreamPool: Separate IPEndPoint attempt state management
    We plan to make an AttemptManager enter "draining" state after all jobs
    are notified. Once an AttemptManager enters "draining" state, it won't
    handle further jobs. However, some tests in
    HttpStreamPoolAttemptManagerTest assume that AttemptManagers can handle
    jobs even after all jobs are notified. Some of these tests involve
    managing IPEndPoint attempt states. This CL extracts IPEndPoint attempt
    state management from AttemptManager to a new class, called
    IPEndpointStateTracker, so that we can update such tests.
    
    Moved following tests from HttpStreamPoolAttemptManagerTest to
    HttpStreamPoolIPEndPointStateTrackerTest:
    - GetIPEndPointToAttempt
    - IPEndPointSlowSuccessSlowFail
    
    Subsequent CLs will move further tests.
    
    No behavior changes.
    
    Bug: 414173943, 383606724
    Change-Id: I4e5777ec01bcd77e43ca72191c4420dedbd6a297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6577873
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465391}

--- net/http/http_stream_pool_attempt_manager.h
+++ net/http/http_stream_pool_attempt_manager.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/host_resolver.h"
 #include "net/dns/public/resolve_error_info.h"
 #include "net/http/http_stream_pool.h"
+#include "net/http/http_stream_pool_ip_endpoint_state_tracker.h"
 #include "net/http/http_stream_pool_job.h"
 #include "net/http/http_stream_request.h"
 #include "net/log/net_log_with_source.h"
@@ -63,7 +64,8 @@ class HttpStreamKey;
 // and destroyed when all jobs, in-flight attempts, and the QuicAttempt are
 // completed.
 class HttpStreamPool::AttemptManager
-    : public HostResolver::ServiceEndpointRequest::Delegate {
+    : public HostResolver::ServiceEndpointRequest::Delegate,
+      public IPEndPointStateTracker::Delegate {
  public:
   class NET_EXPORT_PRIVATE QuicAttempt;
   struct NET_EXPORT_PRIVATE QuicAttemptOutcome {
@@ -79,22 +81,6 @@ class HttpStreamPool::AttemptManager
     NetErrorDetails error_details;
     raw_ptr<QuicChromiumClientSession> session;
   };
-
-  // The state of an IPEndPoint. There is no success state. The absence of a
-  // state for an endpoint means that we haven't yet attempted to connect to the
-  // endpoint, or that a connection to the endpoint was successfully completed
-  // and was not slow. Public for testing.
-  enum class IPEndPointState {
-    // The endpoint has failed.
-    kFailed,
-    // The endpoint is considered slow and haven't timed out yet.
-    kSlowAttempting,
-    // The endpoint was slow to connect, but the connection establishment
-    // completed successfully.
-    kSlowSucceeded,
-  };
-
-  using IPEndPointStateMap = std::map<IPEndPoint, IPEndPointState>;
 
   // Time to delay connection attempts more than one when the destination is
   // known to support HTTP/2, to avoid unnecessary socket connection
@@ -142,6 +128,14 @@ class HttpStreamPool::AttemptManager
   void OnServiceEndpointsUpdated() override;
   void OnServiceEndpointRequestFinished(int rv) override;
 
+  // IPEndPointStateTracker::Delegate implementation:
+  HostResolver::ServiceEndpointRequest* GetServiceEndpointRequest() override;
+  bool IsSvcbOptional() override;
+  bool HasEnoughTcpBasedAttemptsForSlowIPEndPoint(
+      const IPEndPoint& ip_endpoint) override;
+  bool IsEndpointUsableForTcpBasedAttempt(const ServiceEndpoint& endpoint,
+                                          bool svcb_optional) override;
+
   // Tries to process a single pending request/preconnect.
   void ProcessPendingJob();
 
@@ -187,12 +181,6 @@ class HttpStreamPool::AttemptManager
   // Returns true when `this` is blocked by the pool's stream limit.
   bool IsStalledByPoolLimit();
 
-  // Returns whether attempts is "SVCB-optional". See
-  // https://www.rfc-editor.org/rfc/rfc9460.html#section-3-4
-  // Note that the result can be changed over time while the DNS resolution is
-  // still ongoing.
-  bool IsSvcbOptional();
-
   // Called when the QuicAttempt owned by `this` is completed.
   void OnQuicAttemptComplete(QuicAttemptOutcome result);
 
@@ -205,6 +193,13 @@ class HttpStreamPool::AttemptManager
   MultiplexedSessionCreationInitiator
   CalculateMultiplexedSessionCreationInitiator();
 
+  // TODO(crbug.com/383606724): Remove this once we move unittests from
+  // HttpStreamPoolAttemptManagerTest to
+  // HttpStreamPoolIPEndPointStateTrackerTest
+  const IPEndPointStateTracker& ip_endpoint_state_tracker() const {
+    return ip_endpoint_state_tracker_;
+  }
+
   std::optional<int> GetQuicAttemptResultForTesting() {
     return quic_attempt_result_;
   }
@@ -214,13 +209,6 @@ class HttpStreamPool::AttemptManager
   }
 
   QuicAttempt* quic_attempt_for_testing() const { return quic_attempt_.get(); }
-
-  IPEndPointStateMap& ip_endpoint_states_for_testing() {
-    return ip_endpoint_states_;
-  }
-  const IPEndPointStateMap& ip_endpoint_states_for_testing() const {
-    return ip_endpoint_states_;
-  }
 
   void SetOnCompleteCallbackForTesting(base::OnceClosure callback);
 
@@ -302,8 +290,6 @@ class HttpStreamPool::AttemptManager
 
   static std::string_view TcpBasedAttemptStateToString(
       TcpBasedAttemptState state);
-
-  static std::string_view IPEndPointStateToString(IPEndPointState state);
 
   const HttpStreamKey& stream_key() const;
 
@@ -405,26 +391,6 @@ class HttpStreamPool::AttemptManager
 
   // Helper method to calculate pending jobs.
   size_t PendingCountInternal(size_t pending_count) const;
-
-  // Returns an IPEndPoint to attempt a connection. If `exclude_ip_endpoint` is
-  // given, exclude the endpoint. Brief summary of the behavior are:
-  //  * Try preferred address family first.
-  //  * Prioritize unattempted or fast endpoints.
-  //  * Fall back to slow but succeeded endpoints.
-  //  * Use slow and attempting endpoints as the last option.
-  //  * For a slow endpoint, skip the endpoint if there are enough attempts for
-  //    the endpoint.
-  // TODO(crbug.com/383606724): The current logic relies on rather naive and not
-  // very well-founded heuristics. Write a design document and implement more
-  // appropriate algorithm to pick an IPEndPoint.
-  std::optional<IPEndPoint> GetIPEndPointToAttemptTcpBased(
-      std::optional<IPEndPoint> exclude_ip_endpoint = std::nullopt);
-  void FindBetterIPEndPoint(const std::vector<IPEndPoint>& ip_endpoints,
-                            std::optional<IPEndPoint> exclude_ip_endpoint,
-                            std::optional<IPEndPointState>& current_state,
-                            std::optional<IPEndPoint>& current_endpoint);
-  bool HasEnoughTcpBasedAttemptsForSlowIPEndPoint(
-      const IPEndPoint& ip_endpoint);
 
   // Returns a QUIC endpoint to make a connection attempt. See the comments in
   // QuicSessionPool::SelectQuicVersion() for the criteria to select a QUIC
@@ -531,10 +497,6 @@ class HttpStreamPool::AttemptManager
 
   bool IsEchEnabled() const;
 
-  // Returns true when `endpoint` can be used to attempt TCP/TLS connections.
-  bool IsEndpointUsableForTcpBasedAttempt(const ServiceEndpoint& endpoint,
-                                          bool svcb_optional);
-
   // Mark QUIC brokenness if QUIC attempts failed but TCP/TLS attempts succeeded
   // or not attempted.
   void MaybeMarkQuicBroken();
@@ -623,11 +585,8 @@ class HttpStreamPool::AttemptManager
   base::OneShotTimer spdy_throttle_timer_;
   bool spdy_throttle_delay_passed_ = false;
 
-  // When true, try to use IPv6 for the next attempt first.
-  bool prefer_ipv6_ = true;
-  // Updated when a stream attempt is completed or considered slow. Used to
-  // calculate next IPEndPoint to attempt.
-  IPEndPointStateMap ip_endpoint_states_;
+  // Tracks the states of IPEndPoints.
+  IPEndPointStateTracker ip_endpoint_state_tracker_{this};
 
   // The current state of TCP/TLS connection attempts.
   TcpBasedAttemptState tcp_based_attempt_state_ =
