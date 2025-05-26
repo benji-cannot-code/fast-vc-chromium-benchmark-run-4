6e0151736e6c9785dc802537d9b4a6f48ee7a5f5
Daniel Cheng <dcheng@chromium.org>
2025-05-26 12:47:49 -0700

    Remove base::NotFatalUntil::M124 usage
    As of May 2025, this is always fatal.
    
    Bug: 419922502
    Change-Id: Ia61260f309f1f5a8f2df5f186341eed2077e6229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6582592
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Owners-Override: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465633}

--- chrome/browser/ui/views/webauthn/authenticator_qr_centered_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_qr_centered_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
-#include "base/not_fatal_until.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/qr_code_generator/bitmap_generator.h"
@@ -64,7 +63,7 @@ AuthenticatorQrCenteredView::AuthenticatorQrCenteredView(
 
   // Success is guaranteed, because `qr_string`'s size is bounded and smaller
   // than QR code limits.
-  CHECK(qr_code.has_value(), base::NotFatalUntil::M124);
+  CHECK(qr_code.has_value());
 
   qr_code_image_->SetImage(ui::ImageModel::FromImageSkia(qr_code.value()));
   qr_code_image_->SetVisible(true);
