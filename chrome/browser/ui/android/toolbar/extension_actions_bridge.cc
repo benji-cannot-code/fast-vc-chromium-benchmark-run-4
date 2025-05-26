cf339b60d635eee6de89e373d8b81f2dcf95ce0c
Shuhei Takahashi <nya@chromium.org>
2025-05-25 18:00:46 -0700

    extensions: Add ExtensionActionsBridge::runAction
    When an extension button is clicked, this method should be called first
    to trigger the extension action. Eventually it should be wired to
    ExtensionActionRunner, but the JNI method is stubbed to always return
    kNone for now.
    
    Bug: 385985177
    Change-Id: Ie44eabc1f4d221672da85f8c5611a2e64f4d59e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6581678
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465294}

--- chrome/browser/ui/android/toolbar/extension_actions_bridge.cc
+++ chrome/browser/ui/android/toolbar/extension_actions_bridge.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/toolbar/extension_actions_bridge.h"
 
 #include "base/android/jni_string.h"
+#include "chrome/browser/extensions/extension_action_runner.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/android/toolbar/extension_actions_bridge_factory.h"
+#include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_action.h"
 #include "extensions/browser/extension_action_manager.h"
 #include "extensions/browser/extension_registry.h"
@@ -20,9 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::AttachCurrentThread;
 using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
+using content::WebContents;
 using extensions::Extension;
 using extensions::ExtensionAction;
 using extensions::ExtensionActionManager;
+using extensions::ExtensionActionRunner;
 using extensions::ExtensionRegistry;
 
 ExtensionActionsBridge::IconObserver::IconObserver(
@@ -107,6 +111,37 @@ ScopedJavaLocalRef<jobject> ExtensionActionsBridge::GetActionIcon(
 
   gfx::Image image = icon_observer->GetIcon(static_cast<int>(tab_id));
   return gfx::ConvertToJavaBitmap(*image.ToSkBitmap());
+}
+
+jint ExtensionActionsBridge::RunAction(
+    JNIEnv* env,
+    const std::string& action_id,
+    const JavaParamRef<jobject>& web_contents_java) {
+  ExtensionRegistry* registry = ExtensionRegistry::Get(profile_);
+  DCHECK(registry);
+  ExtensionActionManager* manager = ExtensionActionManager::Get(profile_);
+  DCHECK(manager);
+
+  const Extension* extension =
+      registry->enabled_extensions().GetByID(action_id);
+  if (extension == nullptr) {
+    return static_cast<jint>(ExtensionAction::ShowAction::kNone);
+  }
+
+  WebContents* web_contents =
+      WebContents::FromJavaWebContents(web_contents_java);
+  if (extension == nullptr) {
+    return static_cast<jint>(ExtensionAction::ShowAction::kNone);
+  }
+
+  ExtensionActionRunner* runner =
+      ExtensionActionRunner::GetForWebContents(web_contents);
+  if (runner == nullptr) {
+    return static_cast<jint>(ExtensionAction::ShowAction::kNone);
+  }
+
+  return static_cast<jint>(
+      runner->RunAction(extension, /*grant_tab_permissions=*/true));
 }
 
 void ExtensionActionsBridge::OnToolbarActionAdded(
