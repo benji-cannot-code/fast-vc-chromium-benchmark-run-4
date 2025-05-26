d14f7d863d52cc5f2637f0ddd7f0f132da5cddb9
Andrey Davydov <andreydav@google.com>
2025-05-26 07:24:20 -0700

    [FWS] Fix automatic sign-out when starting offline
    Until this cl FloatingWorkspaceService was writing a timestampt to
    Device Info only when device is online during FloatingWorkspaceService
    initialization and only if local device info provider is not initialized
    yet. This wasn't an intended behavior: now we make sure to always write
    the timestamp during FloatingWorkspaceService initialization.
    
    Unit tests started crashing due to the fact that
    FakeLocalDeviceInfoProvider used in tests is derived from
    LocalDeviceInfoProvider, while FloatingWorkspaceService was calling a
    method of MutableLocalDeviceInfoProvider. So this cl also changes
    FakeLocalDeviceInfoProvider to inherit from
    MutableLocalDeviceInfoProvider.
    
    Low-Coverage-Reason: OTHER warning is raised by FakeLocalDeviceInfoProvider
    
    Bug: b:382519556
    Change-Id: Ia63b1012515f312d548d38fba5838f99b98bfaa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578401
    Reviewed-by: Michael Slutskii <slutskii@google.com>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465517}

--- components/sync_device_info/fake_local_device_info_provider.h
+++ components/sync_device_info/fake_local_device_info_provider.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace syncer {
 
-class FakeLocalDeviceInfoProvider : public LocalDeviceInfoProvider {
+class FakeLocalDeviceInfoProvider : public MutableLocalDeviceInfoProvider {
  public:
   FakeLocalDeviceInfoProvider();
 
@@ -26,6 +26,17 @@ class FakeLocalDeviceInfoProvider : public LocalDeviceInfoProvider {
   const DeviceInfo* GetLocalDeviceInfo() const override;
   base::CallbackListSubscription RegisterOnInitializedCallback(
       const base::RepeatingClosure& callback) override;
+
+  // Overrides for MutableLocalDeviceInfoProvider:
+  void Initialize(const std::string& cache_guid,
+                  const std::string& client_name,
+                  const std::string& manufacturer_name,
+                  const std::string& model_name,
+                  const std::string& full_hardware_class,
+                  const DeviceInfo* device_info_restored_from_store) override;
+  void Clear() override;
+  void UpdateClientName(const std::string& client_name) override;
+  void UpdateRecentSignInTime(base::Time time) override;
 
   void SetReady(bool ready);
   DeviceInfo* GetMutableDeviceInfo();
