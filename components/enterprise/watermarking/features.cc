f03d000bfcc50b56e3ab624cac5e9c7ef1fc51b6
Nasser Al-shawwa <alshawwa@chromium.org>
2025-05-26 13:52:48 -0700

    Enable print watermark by default
    Bug: 383509620
    
    Change-Id: I73568128c23b38d9415c0c0281a4c24b08e764f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6583139
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465660}

--- components/enterprise/watermarking/features.cc
+++ components/enterprise/watermarking/features.cc
@@ -11,6 +11,6 @@ namespace enterprise_watermark::features {
 
 BASE_FEATURE(kEnablePrintWatermark,
              "EnablePrintWatermark",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace enterprise_watermark::features
