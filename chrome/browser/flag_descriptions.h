fec895c18b775d0ef39542a9c391aae481240d8b
Tomek Jurkiewicz <tju@google.com>
2025-05-26 07:45:10 -0700

    Revert "Cleanup kids profile flags for url classification."
    This reverts commit 81a3df81fdfb286444116d4deb1b7690204e1699.
    
    Reason for revert: Incomplete flag cleanup
    
    Bug: 378636321
    Original change's description:
    > Cleanup kids profile flags for url classification.
    >
    > Bug: 378636321
    > Change-Id: I988f667b3a1a0e6cb81acea499bcdb3fb85fc79f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553342
    > Reviewed-by: James Lee <ljjlee@google.com>
    > Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1461318}
    
    Bug: 378636321
    Change-Id: I22a393eaf1a6d762bb11344d9a8ae448d4eaa0f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6586991
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465527}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -5033,6 +5033,9 @@ extern const char kSupervisedProfileReauthForBlockedSiteDescription[];
 extern const char kSupervisedProfileSubframeReauthName[];
 extern const char kSupervisedProfileSubframeReauthDescription[];
 
+extern const char kSupervisedProfileFilteringFallbackName[];
+extern const char kSupervisedProfileFilteringFallbackDescription[];
+
 extern const char kSupervisedProfileCustomStringsName[];
 extern const char kSupervisedProfileCustomStringsDescription[];
 
