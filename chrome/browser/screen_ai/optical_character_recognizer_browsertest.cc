b6a50ff811035ecd2226801e42f8e781254a4f94
Ramin Halavati <rhalavati@google.com>
2025-05-26 01:23:06 -0700

    Increase waiting cycle counts for shutting down OCR service in tests.
    When OCR client is reset in browsertests and test needs to wait to
    check if the service has shut down, the following scenario may happen:
     1- The tests checks if service exists (and it exists) and schedules
        checking again in 3 seconds.
     2- The service shuts down and sets the last use to now, delaying
        shutdown on idle by 3 seconds.
     3- The test checks again if the service has shut down, and it's not
        yet.
     4- The service shuts down on idle.
    
    Hence `WaitForDisconnecting` should do try 3 times to ensure service
    has shut down. Update it from 2 to 3.
    
    AX-Relnotes: n/a
    Bug: 419953991
    Change-Id: Idbed8cb36072bf6b1456d2e526324ac770d185a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6586181
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465377}

--- chrome/browser/screen_ai/optical_character_recognizer_browsertest.cc
+++ chrome/browser/screen_ai/optical_character_recognizer_browsertest.cc
@@ -514,7 +514,7 @@ IN_PROC_BROWSER_TEST_P(OpticalCharacterRecognizerTest,
   // Release it and wait for shutdown due to being idle.
   ocr().reset();
   base::test::TestFuture<void> future;
-  WaitForDisconnecting(router, future.GetCallback(), /*remaining_tries=*/2);
+  WaitForDisconnecting(router, future.GetCallback(), /*remaining_tries=*/3);
   ASSERT_TRUE(future.Wait());
   ASSERT_FALSE(router->IsProcessRunningForTesting(
       screen_ai::ScreenAIServiceRouter::Service::kOCR));
@@ -834,7 +834,7 @@ IN_PROC_BROWSER_TEST_F(OpticalCharacterRecognizerResultsTest,
   screen_ai::ScreenAIServiceRouter* router =
       ScreenAIServiceRouterFactory::GetForBrowserContext(browser()->profile());
   base::test::TestFuture<void> future;
-  WaitForDisconnecting(router, future.GetCallback(), /*remaining_tries=*/2);
+  WaitForDisconnecting(router, future.GetCallback(), /*remaining_tries=*/3);
   ASSERT_TRUE(future.Wait());
   ASSERT_FALSE(router->IsProcessRunningForTesting(
       screen_ai::ScreenAIServiceRouter::Service::kOCR));
