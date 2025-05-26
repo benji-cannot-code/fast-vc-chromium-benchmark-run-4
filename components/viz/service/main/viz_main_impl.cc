439186a2e9e78487d6c8f8e441b529044a4452a0
Zhuang Xu <zhuangxu@microsoft.com>
2025-05-25 19:45:05 -0700

    Move LogMessageManager out of gpu_service_impl.cc.
    Now LogMessageManager is implemented in gpu_service_impl.cc and only
    used in GPUServiceImpl. Make LogMessageManager a public class interface
    so that other classes can interact with gpu LogMessageManager.
    
    Bug: 419128705
    Change-Id: Id47ab1b23d89b62460759be56c24873c0bcd6628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6572556
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Zhuang Xu <zhuangxu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1465316}

--- components/viz/service/main/viz_main_impl.cc
+++ components/viz/service/main/viz_main_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/buildflags.h"
 #include "components/viz/common/features.h"
 #include "components/viz/service/debugger/viz_debugger.h"
+#include "components/viz/service/gl/gpu_log_message_manager.h"
 #include "components/viz/service/performance_hint/hint_session.h"
 #include "gpu/command_buffer/common/shm_count.h"
 #include "gpu/config/gpu_finch_features.h"
@@ -182,7 +183,7 @@ void VizMainImpl::CreateGpuService(
 
   if (!gpu_init_->init_successful()) {
     LOG(ERROR) << "Exiting GPU process due to errors during initialization";
-    GpuServiceImpl::FlushPreInitializeLogMessages(gpu_host.get());
+    GpuLogMessageManager::GetInstance()->FlushMessages(gpu_host.get());
     gpu_service_.reset();
     gpu_host->DidFailInitialize();
     if (delegate_)
