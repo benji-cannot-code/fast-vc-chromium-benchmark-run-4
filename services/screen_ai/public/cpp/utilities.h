4f87253cb1f8bf094623b1d005e4f4b5a5da4ba8
Takuto Ikuta <tikuta@chromium.org>
2025-05-26 02:06:37 -0700

    services: Add missing includes
    This is to fix error in clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=mschillaci@google.com
    
    Bug: 40263312
    Change-Id: Ic5e18dd32015b98478b9b07aa56799573f2bc509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6586139
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465392}

--- services/screen_ai/public/cpp/utilities.h
+++ services/screen_ai/public/cpp/utilities.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_SCREEN_AI_PUBLIC_CPP_UTILITIES_H_
 #define SERVICES_SCREEN_AI_PUBLIC_CPP_UTILITIES_H_
 
+#include <stdint.h>
+
 #include "base/files/file_path.h"
 #include "services/screen_ai/buildflags/buildflags.h"
 
