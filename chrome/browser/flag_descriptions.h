85c7044266ac8184051e4e055e1485c4af6cb9fa
François Doray <fdoray@chromium.org>
2025-05-26 05:34:37 -0700

    [freezing] Add "Infinite Tabs Freezing" to about:flags.
    Bug: 415799027
    Change-Id: I5290b15e1b4a39c5c36961a02eb894ba7bdcc6d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6583186
    Commit-Queue: Alex Attar <aattar@google.com>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Alex Attar <aattar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465470}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3241,6 +3241,9 @@ extern const char kFreezingOnEnergySaverTestingDescription[];
 extern const char kImprovedPasswordChangeServiceName[];
 extern const char kImprovedPasswordChangeServiceDescription[];
 
+extern const char kInfiniteTabsFreezingName[];
+extern const char kInfiniteTabsFreezingDescription[];
+
 extern const char kKeyboardLockPromptName[];
 extern const char kKeyboardLockPromptDescription[];
 
