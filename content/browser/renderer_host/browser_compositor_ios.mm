aebe4681f1f21f059647b818d94077137b6fc725
Gyuyoung Kim <gyuyoung@igalia.com>
2025-05-26 04:02:57 -0700

    [iOS Blink] Include ui_base_features.h in browser_compositor_ios.mm
    As https://crrev.com/c/6579850 moved IsPixelCanvasRecordingEnabled()
    from //ui/compositor/compositor_switches.h to
    //ui/base/ui_base_features.h, browser_compositor_ios.mm also needs to
    apply the change.
    
    Bug: 41344902
    Change-Id: Id8452521303bd6f9f70cfce203298050576b53d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6585773
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465444}

--- content/browser/renderer_host/browser_compositor_ios.mm
+++ content/browser/renderer_host/browser_compositor_ios.mm
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/begin_frame_source_ios.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/context_factory.h"
-#include "ui/compositor/compositor_switches.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/dip_util.h"
 #include "ui/gfx/geometry/size_conversions.h"
 
@@ -251,7 +251,7 @@ void BrowserCompositorIOS::TransitionToState(State new_state) {
     compositor_ = std::make_unique<ui::Compositor>(
         context_factory->AllocateFrameSinkId(), context_factory,
         base::SingleThreadTaskRunner::GetCurrentDefault(),
-        ui::IsPixelCanvasRecordingEnabled(),
+        features::IsPixelCanvasRecordingEnabled(),
         /*use_external_begin_frame_control=*/true);
     begin_frame_source_ =
         std::make_unique<BeginFrameSourceIOS>(compositor_.get());
