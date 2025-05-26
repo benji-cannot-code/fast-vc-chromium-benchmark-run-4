47fd0d51765faff55bfed56088340b15caa7b267
François Beaufort <beaufort.francois@gmail.com>
2025-05-26 10:17:14 -0700

    WebGPU: Support GPUBuffer in GPUBindingResource
    Spec issue: https://github.com/gpuweb/gpuweb/issues/5095
    
    Change-Id: I162bd116ad862ce69f627ee56f5a6da0947eb280
    Bug: 419223794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6549638
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1465593}

--- third_party/blink/renderer/modules/webgpu/gpu_bind_group.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_bind_group.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_bind_group_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_bind_group_entry.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_buffer_binding.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_union_gpubufferbinding_gpuexternaltexture_gpusampler_gputextureview.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_union_gpubuffer_gpubufferbinding_gpuexternaltexture_gpusampler_gputextureview.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_conversions.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_bind_group_layout.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_buffer.h"
@@ -29,6 +29,11 @@ wgpu::BindGroupEntry AsDawnType(
   };
 
   switch (webgpu_binding->resource()->GetContentType()) {
+    case V8GPUBindingResource::ContentType::kGPUBuffer: {
+      GPUBuffer* buffer = webgpu_binding->resource()->GetAsGPUBuffer();
+      dawn_binding.buffer = AsDawnType(buffer);
+      break;
+    }
     case V8GPUBindingResource::ContentType::kGPUBufferBinding: {
       GPUBufferBinding* buffer =
           webgpu_binding->resource()->GetAsGPUBufferBinding();
