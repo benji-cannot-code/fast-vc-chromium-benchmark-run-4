1a2a2d38f78cd9114f153e3f2616c82d0884945d
Noam Rosenthal <nrosenthal@chromium.org>
2025-05-26 05:22:20 -0700

    shape(): support arc radius direction-agnostic size
    Spec: https://drafts.csswg.org/css-shapes-2/#direction-agnostic-size
    
    When an arc() value receives only one argument for a radius, compute
    percentages for this radius as "direction-agnostic size", i.e. a
    percentage from the diagonal length, creating a circular arc.
    
    Implemented behind a flag as it is a change to a shipped feature.
    
    At this point this would interpolate incorrectly, will fix interpolation
    in a followup CL, so the interpolation tests that test this are
    converted to use both radii for now. (this bug exists for WebKit as
    well, so removing these tests will not regress anything there).
    
    R=fs@opera.com
    
    Bug: 417852973
    Change-Id: I0baafefb5b33ede1a5a949b6f9be92e6480f456b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6573335
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1465464}

--- third_party/blink/renderer/core/style/style_shape.cc
+++ third_party/blink/renderer/core/style/style_shape.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/style/style_shape.h"
 
+#include <numbers>
 #include <variant>
 
 #include "base/memory/stack_allocated.h"
@@ -118,6 +119,12 @@ class SegmentVisitor {
                           FloatValueForLength(segment.y, box_size.height())}});
   }
 
+  gfx::SizeF BoxSizeForDirectionAgnosticSize() const {
+    const float square_dimension =
+        std::hypot(box_size.width(), box_size.height()) / std::numbers::sqrt2;
+    return gfx::SizeF(square_dimension, square_dimension);
+  }
+
   template <SVGPathSegType T>
   void Emit(const StyleShape::ArcSegment<T>& segment, SVGPathSegType command) {
     PathSegmentData arc_data{
@@ -125,7 +132,11 @@ class SegmentVisitor {
         .target_point = PointForLengthPoint(segment.target_point, box_size),
         .arc_sweep = segment.sweep,
         .arc_large = segment.large};
-    gfx::SizeF radius = SizeForLengthSize(segment.radius, box_size);
+
+    gfx::SizeF radius = SizeForLengthSize(
+        segment.radius, segment.has_direction_agnostic_radius
+                            ? BoxSizeForDirectionAgnosticSize()
+                            : box_size);
     arc_data.SetArcRadiusX(radius.width());
     arc_data.SetArcRadiusY(radius.height());
     arc_data.SetArcAngle(segment.angle);
