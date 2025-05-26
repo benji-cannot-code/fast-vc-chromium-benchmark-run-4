91d7a5649f793d88acaf5c6ecdb36d5c17a6d5bd
Chris Lu <thegreenfrog@google.com>
2025-05-26 02:31:07 -0700

    [ios] Release FirstRunProfileAgent after Guided Tour is completed
    After completion, the cleanup normally done after First Run needs to
    happen in order for FirstRunProfileAgent to be removed. In addition,
    the IPH display lock needs to be released.
    
    Bug: 413461470
    Change-Id: I0169f4d24cbabfb55c940d63084b3752d7d39aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580599
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465409}

--- ios/chrome/app/profile/first_run_profile_agent.mm
+++ ios/chrome/app/profile/first_run_profile_agent.mm
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/check_op.h"
+#import "components/feature_engagement/public/tracker.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/startup_information.h"
 #import "ios/chrome/app/profile/profile_init_stage.h"
 #import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin/signin_utils.h"
 #import "ios/chrome/browser/device_orientation/ui_bundled/scoped_force_portrait_orientation.h"
+#import "ios/chrome/browser/feature_engagement/model/tracker_factory.h"
 #import "ios/chrome/browser/first_run/ui_bundled/first_run_coordinator.h"
 #import "ios/chrome/browser/first_run/ui_bundled/first_run_screen_provider.h"
 #import "ios/chrome/browser/first_run/ui_bundled/guided_tour/guided_tour_coordinator.h"
@@ -61,6 +63,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Used to force the device orientation in portrait mode on iPhone.
   std::unique_ptr<ScopedForcePortraitOrientation> _scopedForceOrientation;
+
+  // Used to prevent other IPHs from showing during the Guided Tour.
+  std::unique_ptr<feature_engagement::DisplayLockHandle> _displayLock;
 }
 
 #pragma mark - Public
@@ -104,6 +109,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   AppState* appState = profileState.appState;
   if (appState.startupInformation.isFirstRun) {
     _scopedForceOrientation = ForcePortraitOrientationOnIphone(appState);
+    if (IsBestOfAppFREEnabled()) {
+      id<BrowserProvider> presentingInterface =
+          _presentingSceneState.browserProviderInterface.currentBrowserProvider;
+      Browser* browser = presentingInterface.browser;
+      feature_engagement::Tracker* engagementTracker =
+          feature_engagement::TrackerFactory::GetForProfile(
+              browser->GetProfile()->GetOriginalProfile());
+      _displayLock = engagementTracker->AcquireDisplayLock();
+    }
   }
 }
 
@@ -256,7 +270,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)guidedTourCompleted {
-  // TODO(crbug.com/413461470): Implement
+  _displayLock.reset();
+  _scopedForceOrientation.reset();
+  [self.profileState removeAgent:self];
 }
 
 #pragma mark - GuidedTourCoordinatorDelegate
@@ -289,6 +305,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)dismissGuidedTourPromo {
   [_guidedTourPromoCoordinator stopWithCompletion:nil];
+  [self guidedTourCompleted];
 }
 
 - (void)startGuidedTour {
