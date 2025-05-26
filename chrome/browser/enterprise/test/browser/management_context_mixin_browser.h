53de6208f2d87fd7332d8aef93f8f3f55f9b9d48
Iman Saboori <isaboori@google.com>
2025-05-26 13:22:48 -0700

    Revert "Add basic client cert provisioning browser tests"
    This reverts commit c7bc50efe9bd5a0038cd36115f703549354994d3.
    
    Reason for revert: Chrome build gardener: This CL causes a bunch to tests to fail b/420456724
    
    Bug: 416700532
    Original change's description:
    > Add basic client cert provisioning browser tests
    >
    > These tests cover basic scenarios (create, and load existing) for both
    > Profile and Browser managed client certs.
    >
    > Bug: 416700532
    > Change-Id: Ia9267562ca9fc1498911dec299ce21b6fceb42c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574559
    > Reviewed-by: Zonghan Xu <xzonghan@chromium.org>
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1465545}
    
    Bug: 416700532
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ie78e0806e40bbb9eed4b4bf992c9351023a6bdbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6588557
    Reviewed-by: Owen Min <zmin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465643}

--- chrome/browser/enterprise/test/browser/management_context_mixin_browser.h
+++ chrome/browser/enterprise/test/browser/management_context_mixin_browser.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "chrome/browser/enterprise/test/management_context_mixin.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
-#include "components/enterprise/browser/controller/fake_browser_dm_token_storage.h"
 
 namespace policy {
 class FakeBrowserDMTokenStorage;
@@ -38,6 +37,9 @@ class ManagementContextMixinBrowser : public ManagementContextMixin {
   // InProcessBrowserTestMixin:
   void SetUpOnMainThread() override;
 
+  // ManagementContextMixin:
+  void SetUpInProcessBrowserTestFixture() override;
+
 #if !BUILDFLAG(GOOGLE_CHROME_BRANDING)
   void SetUpDefaultCommandLine(base::CommandLine* command_line) override;
 #endif
@@ -49,7 +51,7 @@ class ManagementContextMixinBrowser : public ManagementContextMixin {
       override;
 
  private:
-  policy::FakeBrowserDMTokenStorage browser_dm_token_storage_;
+  std::unique_ptr<policy::FakeBrowserDMTokenStorage> browser_dm_token_storage_;
 };
 
 }  // namespace enterprise::test
