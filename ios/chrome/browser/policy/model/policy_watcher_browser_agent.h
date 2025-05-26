8df0443ddd3a73890fb8dcc9dd6e7b8311b6be38
Sylvain Defresne <sdefresne@chromium.org>
2025-05-26 08:33:40 -0700

    [ios] Simplify BrowserUserData sub-classes
    Since BrowserUserData stores a pointer to the owning Browser,
    convert sub-classes to use this pointer instead of each of
    them maintaining their own copy of the pointer.
    
    This converts //ios/chrome/browser/policy directory.
    
    Bug: 409299519
    Change-Id: I6343022a66868a660f98fc1b72de6ffc0bf6c797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574114
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465558}

--- ios/chrome/browser/policy/model/policy_watcher_browser_agent.h
+++ ios/chrome/browser/policy/model/policy_watcher_browser_agent.h
@@ -68,9 +68,6 @@ class PolicyWatcherBrowserAgent
   // AuthenticationServiceObserver implementation.
   void OnPrimaryAccountRestricted() override;
 
-  // The owning Browser.
-  raw_ptr<Browser> browser_ = nullptr;
-
   // The AuthenticationService.
   raw_ptr<AuthenticationService> auth_service_ = nullptr;
 
