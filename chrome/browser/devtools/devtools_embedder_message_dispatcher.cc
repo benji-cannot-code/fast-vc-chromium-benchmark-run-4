d5e3cdff19ffda940d5ea1fc9fc3e07b170d7b86
Wolfgang Beyer <wolfi@chromium.org>
2025-05-26 11:11:18 -0700

    Add FunctionCall structured metrics event for DevTools
    Corresponding DevTools CLs:
    https://crrev.com/c/6574354
    https://crrev.com/c/6574295
    
    Bug: 417878946
    Change-Id: Ia9c6e1c7844836ca525cd2a0efd61132823c4884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568811
    Reviewed-by: Mike West <mkwst@chromium.org>
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465605}

--- chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
+++ chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
@@ -267,6 +267,23 @@ bool GetValue(const base::Value& value, SettingAccessEvent* event) {
   return true;
 }
 
+bool GetValue(const base::Value& value, FunctionCallEvent* event) {
+  if (!value.is_dict()) {
+    return false;
+  }
+
+  std::optional<int> name = value.GetDict().FindInt("name");
+  if (name) {
+    event->name = *name;
+  }
+
+  std::optional<int> context = value.GetDict().FindInt("context");
+  if (context) {
+    event->context = *context;
+  }
+  return true;
+}
+
 template <typename T>
 struct StorageTraits {
   using StorageType = T;
@@ -459,6 +476,8 @@ DevToolsEmbedderMessageDispatcher::CreateForDevToolsFrontend(
   d->RegisterHandler("recordChange", &Delegate::RecordChange, delegate);
   d->RegisterHandler("recordKeyDown", &Delegate::RecordKeyDown, delegate);
   d->RegisterHandler("recordSettingAccess", &Delegate::RecordSettingAccess,
+                     delegate);
+  d->RegisterHandler("recordFunctionCall", &Delegate::RecordFunctionCall,
                      delegate);
   d->RegisterHandler("registerPreference", &Delegate::RegisterPreference,
                      delegate);
