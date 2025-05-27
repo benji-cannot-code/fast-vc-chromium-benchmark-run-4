71b4301a0774ab49980cb8721900ca40c80b6a4a
Florian Jacky <fjacky@google.com>
2025-05-26 23:11:16 -0700

    Make SurveyUiDelegateAndroid a member of DelayedSurveyTask
    Since we pass a pointer to the delegate to Android, the object should
    exist for the duration of the survey task.
    
    Bug: 420362737
    
    Change-Id: I8d3616645280f2a5dc3c63292adb2539dcea8a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6587113
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465738}

--- chrome/browser/ui/android/hats/hats_service_android.h
+++ chrome/browser/ui/android/hats/hats_service_android.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/ui/android/hats/survey_ui_delegate_android.h"
 #include "chrome/browser/ui/hats/hats_service.h"
 #include "components/messages/android/message_enums.h"
 #include "content/public/browser/web_contents.h"
@@ -80,6 +81,7 @@ class HatsServiceAndroid : public HatsService {
     raw_ptr<HatsServiceAndroid> hats_service_;
 
     std::unique_ptr<messages::MessageWrapper> message_;
+    std::unique_ptr<hats::SurveyUiDelegateAndroid> delegate_;
     std::string trigger_;
     SurveyBitsData product_specific_bits_data_;
     SurveyStringData product_specific_string_data_;
