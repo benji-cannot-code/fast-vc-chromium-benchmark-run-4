70818cfbae6cad971e8a76a2ccbcbc0f8fb1d8c4
Thomas Lukaszewicz <tluk@chromium.org>
2025-05-27 09:51:42 -0700

    [bedrock] Update GlicBorderView to use ScopedObservation
    This CL uses ScopedObservation when registering CompositorObserver
    and CompositorAnimationObserver on the associated ui::Compositor
    in GlicBorderView.
    
    This is generally best practice and guards against possible races
    where the border view is destroyed before the compositor during
    shutdown (resulting in UAF crashes).
    
    Bug: 413168662
    Change-Id: I238e9cf3fd1dd761eeefcd86c7963308e7ad2c23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6587328
    Auto-Submit: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465991}

--- chrome/browser/glic/browser_ui/glic_border_view.cc
+++ chrome/browser/glic/browser_ui/glic_border_view.cc
@@ -546,8 +546,8 @@ void GlicBorderView::Show() {
   }
 
   compositor_ = compositor;
-  compositor_->AddAnimationObserver(this);
-  compositor_->AddObserver(this);
+  compositor_animation_observation_.Observe(compositor_.get());
+  compositor_observation_.Observe(compositor_.get());
 
   if (tester_) [[unlikely]] {
     tester_->AnimationStarted();
@@ -559,8 +559,8 @@ void GlicBorderView::StopShowing() {
     return;
   }
 
-  compositor_->RemoveObserver(this);
-  compositor_->RemoveAnimationObserver(this);
+  compositor_observation_.Reset();
+  compositor_animation_observation_.Reset();
   compositor_ = nullptr;
   first_frame_time_ = base::TimeTicks{};
   first_emphasis_frame_ = base::TimeTicks{};
