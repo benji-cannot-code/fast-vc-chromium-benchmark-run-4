02e0c28870c3020c8c334431d068b4da747e7ca4
Rushan Suleymanov <rushans@google.com>
2025-05-27 02:14:36 -0700

    [Sharing] Clear authorized_key field from sharing prefs
    The field was used for Vapid messages which is not used anymore.
    
    This CL also stops registration for Vapid on iOS (it wasn't used there
    and hence it's safe to remove).
    
    Bug: 40253551
    Change-Id: If9467a95edc8b723e15d8bdeea661cf0bc38b9d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6587112
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465800}

--- components/sharing_message/sharing_service_unittest.cc
+++ components/sharing_message/sharing_service_unittest.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kDeviceName[] = "other_name";
-const char kAuthorizedEntity[] = "authorized_entity";
 constexpr base::TimeDelta kTimeout = base::Seconds(15);
 
 SharingTargetDeviceInfo CreateFakeSharingTargetDeviceInfo(
@@ -102,8 +101,8 @@ class MockSharingFCMHandler : public SharingFCMHandler {
  public:
   MockSharingFCMHandler()
       : SharingFCMHandler(/*gcm_driver=*/nullptr,
+                          /*device_info_tracker=*/nullptr,
                           /*sharing_fcm_sender=*/nullptr,
-                          /*sync_preference=*/nullptr,
                           /*handler_registry=*/nullptr) {}
   ~MockSharingFCMHandler() override = default;
 
@@ -647,8 +646,8 @@ TEST_F(SharingServiceTest, StartListeningToFCMAtConstructor) {
 
   // Create new SharingService instance with FCM already registered at
   // constructor.
-  sync_prefs_->SetFCMRegistration(SharingSyncPreference::FCMRegistration(
-      kAuthorizedEntity, base::Time::Now()));
+  sync_prefs_->SetFCMRegistration(
+      SharingSyncPreference::FCMRegistration(base::Time::Now()));
   EXPECT_CALL(*fcm_handler_, StartListening()).Times(1);
   GetSharingService();
 }
