87818a2d76e7ec5202873693fcc26548eb716834
Tomasz Wiszkowski <ender@google.com>
2025-05-27 13:54:56 -0700

    Suppress some UrlBar A11y events when talkback is off.
    This change adds and extra check to confirm talkback is enabled ahead
    of emitting AccessibilityEvent from the UrlBar.
    
    Change-Id: Ifc044d3420fff6c118555c0145a6aa165846745c
    Fixed: 41496500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594235
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466138}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextModelBase.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextModelBase.java
@@ -75,9 +75,9 @@ public interface AutocompleteEditTextModelBase {
         boolean isFocused();
 
         /**
-         * @see TextView#sendAccessibilityEventUnchecked(AccessibilityEvent)
+         * @see TextView#sendAccessibilityEvent(AccessibilityEvent)
          */
-        void sendAccessibilityEventUnchecked(AccessibilityEvent event);
+        void sendAccessibilityEvent(AccessibilityEvent event);
 
         /**
          * Call super.dispatchKeyEvent(KeyEvent).
