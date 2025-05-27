7d394c9cee891876cea883a33af8fbeb77dced5a
Patrick Monette <pmonette@chromium.org>
2025-05-27 09:25:15 -0700

    Reland "Remove the third-party blocking feature"
    This is a reland of commit 604eed61ac0324f999433efd947ac5adf6852cf4
    
    The original change still had references to the incompatible application
    settings WebUI page which no longer existed. Those references were
    removed.
    
    Original change's description:
    > Remove the third-party blocking feature
    >
    > Unfortunately the state of accessibility software on Windows never
    > allowed us to launch without significantly disrupting some users.
    >
    > Some obsolete prefs were moved to be migrated in browser_prefs.cc.
    >
    > After this CL, there will be some cleanup left in chrome/chrome_elf/.
    >
    > Bug: 40578141
    > Change-Id: Ib54768672827f6a4a5eb25bd9173fedc293f4d9c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553962
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1464067}
    
    Bug: 40578141
    Change-Id: I7654736dfa3ba4c305e61e2181789e8dfbce50e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580079
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465975}

--- chrome/browser/win/conflicts/module_database.h
+++ chrome/browser/win/conflicts/module_database.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
-#include "build/branding_buildflags.h"
-#include "chrome/browser/win/conflicts/installed_applications.h"
 #include "chrome/browser/win/conflicts/module_info.h"
 #include "chrome/browser/win/conflicts/module_inspector.h"
 #include "chrome/browser/win/conflicts/third_party_metrics_recorder.h"
@@ -27,16 +25,6 @@ namespace base {
 class FilePath;
 class SequencedTaskRunner;
 }  // namespace base
-
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-class ModuleLoadAttemptLogListener;
-class PrefRegistrySimple;
-class ThirdPartyConflictsManager;
-
-namespace base {
-struct OnTaskRunnerDeleter;
-}
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 // A class that keeps track of all modules loaded across Chrome processes.
 //
@@ -78,12 +66,6 @@ class ModuleDatabase : public ModuleDatabaseEventSource {
   // global instance and deliberately leaked, unless manually cleaned up. This
   // has no locking and should be called when Chrome is single threaded.
   static void SetInstance(std::unique_ptr<ModuleDatabase> module_database);
-
-  // Initializes the ModuleLoadAttemptLogListener instance. This function is a
-  // noop on non-GOOGLE_CHROME_BRANDING configurations because it is used only
-  // for third-party software blocking, which is only enabled in Google Chrome
-  // builds.
-  void StartDrainingModuleLoadAttemptsLog();
 
   // Returns true if the ModuleDatabase is idle. This means that no modules are
   // currently being inspected, and no new module events have been observed in
@@ -148,29 +130,6 @@ class ModuleDatabase : public ModuleDatabaseEventSource {
 
   void StartInspection();
 
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  // Similar with the GetInstance() but overwriting third party conflicts
-  // manager's installed_applications_ for testing.
-  static ModuleDatabase* GetInstanceForTesting(
-      std::unique_ptr<InstalledApplications>);
-
-  // Disables the blocking of third-party modules in the browser process. It is
-  // safe to invoke this function from any thread.
-  // This function is meant to be used only as a workaround for the in-process
-  // printing code that may require that third-party DLLs be successfully
-  // loaded into the process to work correctly.
-  // TODO(pmonette): Remove this workaround when printing is moved to a utility
-  //                 process. See https://crbug.com/892294.
-  static void DisableThirdPartyBlocking();
-
-  // Accessor for the third party conflicts manager.
-  // Returns null if both the tracking of incompatible applications and the
-  // blocking of third-party modules are disabled.
-  ThirdPartyConflictsManager* third_party_conflicts_manager() {
-    return third_party_conflicts_manager_.get();
-  }
-#endif
-
  private:
   friend class TestModuleDatabase;
   friend class ModuleDatabaseTest;
@@ -215,21 +174,6 @@ class ModuleDatabase : public ModuleDatabaseEventSource {
   // OnNewModuleFound().
   void NotifyLoadedModules(ModuleDatabaseObserver* observer);
 
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  // Called by DisableThirdPartyBlocking() to disable the analysis of loaded
-  // modules.
-  // TODO(pmonette): Remove this workaround when printing is moved to a utility
-  //                 process. See https://crbug.com/892294.
-  void OnThirdPartyBlockingDisabled();
-
-  // Initializes the ThirdPartyConflictsManager, which controls showing warnings
-  // for incompatible applications that inject into Chrome and the blocking of
-  // third-party modules. The manager is only initialized if either or both of
-  // the ThirdPartyModulesBlocking and IncompatibleApplicationsWarning features
-  // are enabled.
-  void MaybeInitializeThirdPartyConflictsManager();
-#endif
-
   // A map of all known modules.
   ModuleMap modules_;
 
@@ -244,20 +188,11 @@ class ModuleDatabase : public ModuleDatabaseEventSource {
   // Indicates if all input method editors have been enumerated.
   bool ime_enumerated_;
 
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  std::unique_ptr<ModuleLoadAttemptLogListener>
-      module_load_attempt_log_listener_;
-#endif
-
   // Inspects new modules on a blocking task runner.
   ModuleInspector module_inspector_;
 
   // Holds observers.
   base::ObserverList<ModuleDatabaseObserver>::Unchecked observer_list_;
-
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  std::unique_ptr<ThirdPartyConflictsManager> third_party_conflicts_manager_;
-#endif
 
   // Records metrics on third-party modules.
   ThirdPartyMetricsRecorder third_party_metrics_;
