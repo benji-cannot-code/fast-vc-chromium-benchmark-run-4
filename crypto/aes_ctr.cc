763a6c0f9a51cd2bd74fd4c401d86922a7f2844b
Takuto Ikuta <tikuta@chromium.org>
2025-05-27 08:08:56 -0700

    crypto: Add missing includes
    This is to fix error in clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=davidben@chromium.org
    
    Bug: 40263312
    Change-Id: I35f47d04d3b229780ccdf5a7adfba86d3394c140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6586418
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465924}

--- crypto/aes_ctr.cc
+++ crypto/aes_ctr.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "crypto/aes_ctr.h"
 
+#include <array>
 #include <vector>
 
 #include "base/containers/span.h"
--- crypto/test_support.cc
+++ crypto/test_support.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "crypto/test_support.h"
 
+#include <array>
+
 namespace crypto::test {
 
 // To generate these keys:
