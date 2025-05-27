64b1986c6913524c665ff685f39c76d86fdbb685
Joel Hockey <joelhockey@chromium.org>
2025-05-26 19:22:12 -0700

    Revert "[Blink] Coalesce metrics collection on CanvasResourceProvider creation"
    This reverts commit c12891ab68d3ba31fcba597bfa80631746da820b.
    
    Reason for revert: null pointer crash in
    GetOrCreateCanvasResourceProviderImpl()
    
    Bug: 420499905
    Bug: 352263194
    Original change's description:
    > [Blink] Coalesce metrics collection on CanvasResourceProvider creation
    >
    > No functional change. Followup will eliminate the usage of
    > `raw_provider` in this flow.
    >
    > Bug: 352263194
    > Change-Id: Ie6782a9fa2073ff518f204cdc7b7c42ba41eebdc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580331
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1465355}
    
    Bug: 352263194
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5b64188d41608dd77f41f9206295077aa5c735f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6587088
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1465712}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
@@ -141,12 +141,6 @@ CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl() {
     if (!provider) {
       did_fail_to_create_resource_provider_ = true;
     }
-    if (provider->IsValid()) {
-      base::UmaHistogramBoolean("Blink.Canvas.ResourceProviderIsAccelerated",
-                                provider->IsAccelerated());
-      base::UmaHistogramEnumeration("Blink.Canvas.ResourceProviderType",
-                                    provider->GetType());
-    }
   }
   return provider;
 }
@@ -161,6 +155,12 @@ CanvasRenderingContextHost::CreateCanvasResourceProviderWebGPU() {
   }
   auto* raw_provider = provider.get();
   ReplaceResourceProvider(std::move(provider));
+  if (raw_provider && raw_provider->IsValid()) {
+    base::UmaHistogramBoolean("Blink.Canvas.ResourceProviderIsAccelerated",
+                              ResourceProvider()->IsAccelerated());
+    base::UmaHistogramEnumeration("Blink.Canvas.ResourceProviderType",
+                                  ResourceProvider()->GetType());
+  }
   return raw_provider;
 }
 
@@ -248,6 +248,12 @@ CanvasRenderingContextHost::CreateCanvasResourceProviderWebGL() {
 
   auto* raw_provider = provider.get();
   ReplaceResourceProvider(std::move(provider));
+  if (raw_provider && raw_provider->IsValid()) {
+    base::UmaHistogramBoolean("Blink.Canvas.ResourceProviderIsAccelerated",
+                              ResourceProvider()->IsAccelerated());
+    base::UmaHistogramEnumeration("Blink.Canvas.ResourceProviderType",
+                                  ResourceProvider()->GetType());
+  }
   return raw_provider;
 }
 
@@ -351,6 +357,12 @@ CanvasRenderingContextHost::CreateCanvasResourceProvider2D() {
   ReplaceResourceProvider(std::move(provider));
 
   if (raw_provider) {
+    if (raw_provider->IsValid()) {
+      base::UmaHistogramBoolean("Blink.Canvas.ResourceProviderIsAccelerated",
+                                ResourceProvider()->IsAccelerated());
+      base::UmaHistogramEnumeration("Blink.Canvas.ResourceProviderType",
+                                    ResourceProvider()->GetType());
+    }
     raw_provider->SetResourceRecyclingEnabled(true);
   }
   return raw_provider;
