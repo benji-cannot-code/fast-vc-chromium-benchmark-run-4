a527da0200e0ec61af7085227032141033dd14b5
Jeff Yoon <jeffyoon@google.com>
2025-05-27 12:39:24 -0700

    [perf] add --remote argument for brya tester
    See if this allows adb to connect to the proper device so it does not
    have to search for it.
    
    Bug: 414619873
    Change-Id: I36bc109019a7b2e1bc18252b8dffb1b804f4130b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594491
    Commit-Queue: John Chen <johnchen@chromium.org>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Auto-Submit: Jeff Yoon <jeffyoon@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466100}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -618,6 +618,10 @@ BUILDERS = {
         'tests': [{
             'isolate':
             'performance_test_suite_android_trichrome_chrome_google_64_32_bundle',
+            'extra_args': [
+                '--device',
+                'variable_lab_dut_hostname',
+            ],
         }],
         'platform':
         'android-trichrome-chrome-google-64-32-bundle',
