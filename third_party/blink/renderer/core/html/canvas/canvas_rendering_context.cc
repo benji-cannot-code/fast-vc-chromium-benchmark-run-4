22b52f20ef1779cdb0518ba22dd1eae0f5b66eed
Colin Blundell <blundell@chromium.org>
2025-05-27 01:05:19 -0700

    [Blink] Rip out IsPaintableChecksResourceProviderInsteadOfBridge
    This killswitch shipped in 136 [1], which has now rolled through stable
    with no problems.
    
    [1] https://chromiumdash.appspot.com/commit/fcb352f82e1d879fa5b1765254c1de92f570c714
    
    Bug: 40280152
    Change-Id: If1fcd74a311c79484fceb258226bc09a2b1184d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580026
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465774}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
@@ -39,13 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// static
-bool CanvasRenderingContext::
-    CheckProviderInCanvas2DRenderingContextIsPaintable() {
-  return base::FeatureList::IsEnabled(
-      features::kIsPaintableChecksResourceProviderInsteadOfBridge);
-}
-
 CanvasRenderingContext::CanvasRenderingContext(
     CanvasRenderingContextHost* host,
     const CanvasContextCreationAttributesCore& attrs,
