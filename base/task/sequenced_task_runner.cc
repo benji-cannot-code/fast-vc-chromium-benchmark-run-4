81398878733fbc31700b15360dfcbe02942e2aa9
Gabriel Charette <gab@chromium.org>
2025-05-27 09:30:24 -0700

    [base] Fizzle DeleteOrReleaseSoon on shutdown and fix a dcheck in ~UpdateServiceProxyImpl
    It feels unnatural to add a ScopedFizzleBlockShutdownTasks to
    UpdateServiceProxyImpl itself. Instead this addresses the problem
    at a more generic layer where all callers of DeleteOrReleaseSoon
    shouldn't have to worry about doing so on BLOCK_SHUTDOWN task runners
    beyond shutdown.
    
    Fixed: 420259698
    Change-Id: Iae8510fca8db4484bb2da7081b501e09754f4d08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6589113
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465979}

--- base/task/sequenced_task_runner.cc
+++ base/task/sequenced_task_runner.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/task/default_delayed_task_handle_delegate.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/time/time.h"
 
 namespace base {
@@ -124,6 +125,11 @@ bool SequencedTaskRunner::DeleteOrReleaseSoonInternal(
     const Location& from_here,
     void (*deleter)(const void*),
     const void* object) {
+  // Allow memory to leak on shutdown. ScopedFizzleBlockShutdownTasks avoids a
+  // DCHECK about posting a task to a potentially BLOCK_SHUTDOWN task runner
+  // after shut down in cleanups that happen as things are reaped in the final
+  // phases of shutdown (ref. crbug.com/420259698; and other instances).
+  ThreadPoolInstance::ScopedFizzleBlockShutdownTasks fizzler;
   return PostNonNestableTask(from_here, BindOnce(deleter, object));
 }
 
