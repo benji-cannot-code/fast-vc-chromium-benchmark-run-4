1c4a03788f9a2054da6ae68bb4ae74da5d716b8f
Aashna Sheth <aashnas@google.com>
2025-05-27 15:13:48 -0700

    Add sync check to DidFinishNavigation
    This CL adds a check to see if sync process a running in which case
    we don't want to show the PS prompt. The signing check and sync check
    are distinct.
    
    Bug: b:408016824
    Change-Id: I574759b0f0c1daa3d54e74c251d5e819ddc71d45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6582505
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466181}

--- chrome/browser/privacy_sandbox/notice/desktop_entrypoint_handlers.cc
+++ chrome/browser/privacy_sandbox/notice/desktop_entrypoint_handlers.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/privacy_sandbox/notice/desktop_entrypoint_handlers_helper.h"
 #include "chrome/browser/privacy_sandbox/notice/desktop_view_manager.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/profiles/profile_customization_bubble_sync_controller.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/sync/service/sync_service.h"
 #include "components/tabs/public/tab_interface.h"
 #include "components/web_modal/web_contents_modal_dialog_host.h"
 #include "content/public/browser/navigation_handle.h"
@@ -94,6 +96,13 @@ void NavigationHandler::HandleNewNavigation(
 #endif  // !BUILDFLAG(IS_CHROMEOS)
   if (signin_dialog_showing) {
     return;
+  }
+
+  // If a Sync setup is in progress, the prompt should not be shown.
+  if (auto* sync_service = SyncServiceFactory::GetForProfile(profile)) {
+    if (sync_service->IsSetupInProgress()) {
+      return;
+    }
   }
 
   // TODO(crbug.com/408016824):  Add error-event histograms.
