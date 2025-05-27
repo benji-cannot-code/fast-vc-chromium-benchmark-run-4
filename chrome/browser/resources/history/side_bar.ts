3c450f6d456dac6cdcd68baaba906b505eb5fc63
Chase Hartsell <chazzy@chromium.org>
2025-05-27 14:10:48 -0700

    [Compare][Cleanup] Remove product specifications from history WebUI
    This patch removes the product specifications lists code from the
    history WebUI (chrome://history), as the new list of comparison tables
    in the Compare WebUI is enabled by default.
    
    Bug: 416565369
    Change-Id: I7388697a80558833d75064c0bdf2c543e3e0d40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6544129
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466150}

--- chrome/browser/resources/history/side_bar.ts
+++ chrome/browser/resources/history/side_bar.ts
@@ -97,11 +97,6 @@ export class HistorySideBarElement extends PolymerElement {
         computed: 'computeShowHistoryClusters_(' +
             'historyClustersEnabled, historyClustersVisible)',
       },
-
-      compareHistoryEnabled_: {
-        type: Boolean,
-        value: loadTimeData.getBoolean('compareHistoryEnabled'),
-      },
     };
   }
 
@@ -114,7 +109,6 @@ export class HistorySideBarElement extends PolymerElement {
   declare private historyClustersVisibleManagedByPolicy_: boolean;
   declare private showFooter_: boolean;
   declare private showHistoryClusters_: boolean;
-  declare private compareHistoryEnabled_: boolean;
 
   override ready() {
     super.ready();
--- chrome/test/data/webui/history/history_ui_browsertest.cc
+++ chrome/test/data/webui/history/history_ui_browsertest.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
-#include "components/commerce/core/commerce_constants.h"
-#include "components/commerce/core/commerce_feature_list.h"
 #include "components/history_clusters/core/features.h"
 #include "components/history_embeddings/history_embeddings_features.h"
 #include "content/public/test/browser_test.h"
@@ -178,38 +176,6 @@ IN_PROC_BROWSER_TEST_F(HistoryListTest, ScrollingLoadsMore) {
 
 IN_PROC_BROWSER_TEST_F(HistoryListTest, ResizingLoadsMore) {
   RunTestCase("ResizingLoadsMore");
-}
-
-class HistoryProductSpecificationsListTest : public WebUIMochaBrowserTest {
- protected:
-  HistoryProductSpecificationsListTest() {
-    set_test_loader_host(chrome::kChromeUIHistoryHost);
-    scoped_feature_list_.InitWithFeatures({commerce::kProductSpecifications},
-                                          {});
-  }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
-};
-
-IN_PROC_BROWSER_TEST_F(HistoryProductSpecificationsListTest, Load) {
-  RunTest("history/history_product_specifications_list_test.js", "mocha.run()");
-}
-
-class HistoryProductSpecificationsItemTest : public WebUIMochaBrowserTest {
- protected:
-  HistoryProductSpecificationsItemTest() {
-    set_test_loader_host(chrome::kChromeUIHistoryHost);
-    scoped_feature_list_.InitWithFeatures({commerce::kProductSpecifications},
-                                          {});
-  }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
-};
-
-IN_PROC_BROWSER_TEST_F(HistoryProductSpecificationsItemTest, Load) {
-  RunTest("history/history_product_specifications_item_test.js", "mocha.run()");
 }
 
 class HistoryWithHistoryEmbeddingsTest : public WebUIMochaBrowserTest {
