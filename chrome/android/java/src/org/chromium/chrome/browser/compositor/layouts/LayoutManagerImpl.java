1ba318a333d5d60334359768e6221660bf33780b
Aldo Culquicondor <acondor@chromium.org>
2025-05-27 07:55:05 -0700

    Remove function from LayoutTab model
    A function, as opposed to a value, could result in changes that aren't
    visible to the model. This was preventing proper rendering of contents
    when MvcUpdateViewWhenModelChanged was enabled.
    
    Tested on a phone and a tablet, switching tabs using the tab switcher, a
    tab swipe and using the tab strip (tablet only).
    
    Bug: 416752339
    Change-Id: I25f6e5469d452970752ddb2d2bca0fd88528ba52
    Fixed: 394893115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580131
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465916}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerImpl.java
@@ -1225,12 +1225,14 @@ public class LayoutManagerImpl
             if (oldLayout != null) {
                 oldLayout.forceAnimationToFinish();
                 oldLayout.detachViews();
+                oldLayout.setIsActive(false);
 
                 // TODO(crbug.com/40141330): hide oldLayout if it's not hidden.
             }
             layout.contextChanged(mHost.getContext());
             layout.attachViews(mContentContainer);
             mActiveLayout = layout;
+            mActiveLayout.setIsActive(true);
         }
 
         BrowserControlsVisibilityManager controlsVisibilityManager =
@@ -1292,11 +1294,6 @@ public class LayoutManagerImpl
     @Override
     public @LayoutType int getNextLayoutType() {
         return mNextActiveLayout != null ? mNextActiveLayout.getLayoutType() : LayoutType.NONE;
-    }
-
-    @Override
-    public boolean isActiveLayout(Layout layout) {
-        return layout == mActiveLayout;
     }
 
     @Override
--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/StaticLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/StaticLayout.java
@@ -70,7 +70,7 @@ public class StaticLayout extends Layout {
 
     private final Supplier<TopUiThemeColorProvider> mTopUiThemeColorProvider;
 
-    private boolean mIsActive;
+    private boolean mIsShowing;
 
     private static Integer sToolbarTextBoxBackgroundColorForTesting;
 
@@ -154,7 +154,7 @@ public class StaticLayout extends Layout {
                         .with(LayoutTab.Y, 0.0f)
                         .with(LayoutTab.RENDER_X, 0.0f)
                         .with(LayoutTab.RENDER_Y, 0.0f)
-                        .with(LayoutTab.IS_ACTIVE_LAYOUT_SUPPLIER, this::isActive)
+                        .with(LayoutTab.IS_ACTIVE_LAYOUT, false)
                         .build();
 
         mTopUiThemeColorProvider = topUiThemeColorProvider;
@@ -237,7 +237,7 @@ public class StaticLayout extends Layout {
                 new TabModelSelectorTabModelObserver(tabModelSelector) {
                     @Override
                     public void didSelectTab(Tab tab, int type, int lastId) {
-                        if (!mIsActive) return;
+                        if (!mIsShowing) return;
 
                         setStaticTab(tab);
                         requestFocus(tab);
@@ -306,7 +306,7 @@ public class StaticLayout extends Layout {
     public void show(long time, boolean animate) {
         super.show(time, animate);
 
-        mIsActive = true;
+        mIsShowing = true;
         Tab tab = mTabModelSelector.getCurrentTab();
         if (tab == null) return;
         setStaticTab(tab);
@@ -328,7 +328,7 @@ public class StaticLayout extends Layout {
 
     @Override
     public void doneHiding() {
-        mIsActive = false;
+        mIsShowing = false;
         mModel.set(LayoutTab.TAB_ID, Tab.INVALID_TAB_ID);
 
         // Call super last because it might re-show this layout. If we do any work after
@@ -348,7 +348,7 @@ public class StaticLayout extends Layout {
             return;
         }
 
-        if (mIsActive && tab.getView() != null) tab.getView().requestFocus();
+        if (mIsShowing && tab.getView() != null) tab.getView().requestFocus();
     }
 
     private void updateVisibleIdsCheckingLiveLayer(int tabId, boolean useLiveTexture) {
@@ -463,6 +463,12 @@ public class StaticLayout extends Layout {
     @Override
     public int getLayoutType() {
         return LayoutType.BROWSING;
+    }
+
+    @Override
+    protected void setIsActive(boolean active) {
+        super.setIsActive(active);
+        mModel.set(LayoutTab.IS_ACTIVE_LAYOUT, active);
     }
 
     @Override
