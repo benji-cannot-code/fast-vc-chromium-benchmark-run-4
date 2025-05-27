081b08a4f4112af5b57d5231c2257739e6d16469
Colin Blundell <blundell@chromium.org>
2025-05-27 06:12:27 -0700

    [Viz] Pass alpha type through directly in TransferableResource::Make()
    Historically the `alpha_type` passed to TransferableResource has been
    compressed to a "premul" bool with kOpaque_SkAlphaType being treated as
    kPremul_SkAlphaType on the service side. This CL eliminates this
    historical behavior under a killswitch.
    
    Bug: 410591523
    Change-Id: I0354704da991247a1981bd81790950d17371e3ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6590271
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465881}

--- components/viz/common/resources/transferable_resource.cc
+++ components/viz/common/resources/transferable_resource.cc
@@ -4,10 +4,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/viz/common/resources/transferable_resource.h"
+
+#include "base/feature_list.h"
 #include "components/viz/common/resources/returned_resource.h"
 #include "gpu/command_buffer/client/client_shared_image.h"
 
 namespace viz {
+
+namespace {
+
+BASE_FEATURE(kPassAlphaTypeDirectly,
+             "PassAlphaTypeDirectly",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+}  // namespace
 
 // static
 TransferableResource TransferableResource::MakeSoftwareSharedImage(
@@ -87,13 +97,18 @@ TransferableResource TransferableResource::Make(
   resource.origin = override.origin.value_or(shared_image->surface_origin());
   SkAlphaType alpha_type =
       override.alpha_type.value_or(shared_image->alpha_type());
-  // TODO(crbug.com/410591523): Set `resource.alpha_type` directly from
-  // `alpha_type` under a killswitch; this will result in kOpaque_SkAlphaType
-  // being passed through to the service side, whereas historically that has
-  // been compressed to a "premul" bool and treated as kPremul_SkAlphaType on
-  // the service side.
-  resource.alpha_type =
-      (alpha_type == kUnpremul_SkAlphaType) ? alpha_type : kPremul_SkAlphaType;
+  // Historically `alpha_type` has been compressed to a "premul" bool with
+  // kOpaque_SkAlphaType being treated as kPremul_SkAlphaType on the service
+  // side. Eliminate this historical behavior under a killswitch.
+  // TODO(crbug.com/410591523): Remove killswitch after it has safely rolled
+  // out.
+  if (base::FeatureList::IsEnabled(kPassAlphaTypeDirectly)) {
+    resource.alpha_type = alpha_type;
+  } else {
+    resource.alpha_type = (alpha_type == kUnpremul_SkAlphaType)
+                              ? alpha_type
+                              : kPremul_SkAlphaType;
+  }
   resource.set_texture_target(
       override.texture_target.value_or(shared_image->GetTextureTarget()));
 
