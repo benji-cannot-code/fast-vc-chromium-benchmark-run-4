cdf1dc33b03fe75a85069cac049325c3d06a9ad8
Takuto Ikuta <tikuta@chromium.org>
2025-05-27 01:46:18 -0700

    components: Add missing includes
    This is to fix error in clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=etienneb@chromium.org
    
    Bug: 40263312
    Change-Id: I82737fcdc08e056610b6b9403884bd539dd248a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6590492
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465792}

--- components/persistent_cache/sqlite/vfs/sqlite_database_vfs_file_set.cc
+++ components/persistent_cache/sqlite/vfs/sqlite_database_vfs_file_set.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/persistent_cache/sqlite/vfs/sqlite_database_vfs_file_set.h"
 
+#include <atomic>
+
 #include "base/files/file_path.h"
 #include "base/notreached.h"
 #include "base/strings/strcat.h"
