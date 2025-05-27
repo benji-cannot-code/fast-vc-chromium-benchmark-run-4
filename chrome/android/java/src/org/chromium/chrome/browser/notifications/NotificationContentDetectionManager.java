b406ac84a9101123f91bc86e6cb07927add56123
Sarah Krakowiak Criel <skrakowi@chromium.org>
2025-05-27 08:24:17 -0700

    Spamishv2 use cache flag to gate feature; add field trial testing config
    Bug: 390196590
    Change-Id: Ie9a8557a3349e0f95342cc1d17a3c42888cbecc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6582502
    Auto-Submit: Sarah Krakowiak <skrakowi@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465936}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationContentDetectionManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationContentDetectionManager.java
@@ -144,9 +144,8 @@ public class NotificationContentDetectionManager {
                                                     notificationOrigin,
                                                     SchemeDisplay.OMIT_HTTP_AND_HTTPS)))
                             .setSmallIconId(
-                                    ChromeFeatureList.isEnabled(
-                                                    ChromeFeatureList
-                                                            .REPORT_NOTIFICATION_CONTENT_DETECTION_DATA)
+                                    ChromeFeatureList.sReportNotificationContentDetectionData
+                                                    .isEnabled()
                                             ? R.drawable.ic_warning_red_24dp
                                             : R.drawable.report_octagon)
                             .setTicker(tickerText)
@@ -155,8 +154,7 @@ public class NotificationContentDetectionManager {
                             .setOrigin(
                                     UrlFormatter.formatUrlForSecurityDisplay(
                                             origin, SchemeDisplay.OMIT_HTTP_AND_HTTPS));
-            if (ChromeFeatureList.isEnabled(
-                    ChromeFeatureList.REPORT_NOTIFICATION_CONTENT_DETECTION_DATA)) {
+            if (ChromeFeatureList.sReportNotificationContentDetectionData.isEnabled()) {
                 // Don't show default icon on warning notification.
                 notificationBuilder.setSuppressShowingLargeIcon(true);
             }
@@ -375,8 +373,7 @@ public class NotificationContentDetectionManager {
     }
 
     static boolean shouldAllowReportingSpam(Bundle extras) {
-        if (ChromeFeatureList.isEnabled(
-                        ChromeFeatureList.REPORT_NOTIFICATION_CONTENT_DETECTION_DATA)
+        if (ChromeFeatureList.sReportNotificationContentDetectionData.isEnabled()
                 && extras.containsKey(
                         NotificationConstants
                                 .EXTRA_ALLOW_REPORTING_AS_SPAM_IS_NOTIFICATION_WARNED)) {
