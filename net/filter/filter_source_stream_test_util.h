008f54fa4ecfb14c7c557b2c97d44826efb7c9e3
Takuto Ikuta <tikuta@chromium.org>
2025-05-27 07:11:13 -0700

    net: Add missing includes
    This is to fix error in clang modules build.
    
    This CL was uploaded by git cl split.
    
    R=dschinazi@chromium.org
    
    Bug: 40263312
    Change-Id: I3ff0ea51942d0644f06cdb2cb01e854e7c68fdca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6590313
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465903}

--- net/filter/filter_source_stream_test_util.h
+++ net/filter/filter_source_stream_test_util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_FILTER_FILTER_SOURCE_STREAM_TEST_UTIL_H_
 #define NET_FILTER_FILTER_SOURCE_STREAM_TEST_UTIL_H_
 
+#include <stdint.h>
+
 #include <string_view>
 #include <vector>
 
