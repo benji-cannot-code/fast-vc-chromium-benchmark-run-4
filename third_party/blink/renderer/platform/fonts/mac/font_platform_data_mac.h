0282675e780c5e9ec65797851a356c295524f631
Kent Tamura <tkent@chromium.org>
2025-05-27 02:04:21 -0700

    Remove dead code in WTF::AtomicString
    * font_platform_data_mac.h
      CoreText.h is necessary for the `CTFontRef` usage.
      CoreText.h was included via base/apple/bridging.h.
    
    This CL has no behavior changes.
    
    Change-Id: I92ffa56de4a2b02028f71473047c246f658422dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6589351
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1465798}

--- third_party/blink/renderer/platform/fonts/mac/font_platform_data_mac.h
+++ third_party/blink/renderer/platform/fonts/mac/font_platform_data_mac.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_MAC_FONT_PLATFORM_DATA_MAC_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_MAC_FONT_PLATFORM_DATA_MAC_H_
 
+#include <CoreText/CoreText.h>
+
 #include "third_party/blink/renderer/platform/fonts/font_optical_sizing.h"
 #include "third_party/blink/renderer/platform/fonts/resolved_font_features.h"
 #include "third_party/blink/renderer/platform/fonts/text_rendering_mode.h"
--- third_party/blink/renderer/platform/wtf/text/atomic_string.h
+++ third_party/blink/renderer/platform/wtf/text/atomic_string.h
@@ -39,10 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/wtf_export.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 
-#ifdef __OBJC__
-#include "base/apple/bridging.h"
-#endif
-
 namespace WTF {
 class WTF_EXPORT AtomicString;
 }
@@ -208,7 +204,6 @@ class WTF_EXPORT AtomicString {
   unsigned Hash() const { return string_.Impl()->ExistingHash(); }
 
 #ifdef __OBJC__
-  AtomicString(NSString* s) : string_(Add(base::apple::NSToCFPtrCast(s))) {}
   operator NSString*() const { return string_; }
 #endif
   // AtomicString::fromUTF8 will return a null string if
@@ -253,9 +248,6 @@ class WTF_EXPORT AtomicString {
   }
   static scoped_refptr<StringImpl> AddSlowCase(scoped_refptr<StringImpl>&&);
   static scoped_refptr<StringImpl> AddSlowCase(StringImpl*);
-#if BUILDFLAG(IS_APPLE)
-  static scoped_refptr<StringImpl> Add(CFStringRef);
-#endif
 };
 
 inline bool operator==(const AtomicString& a, const AtomicString& b) {
