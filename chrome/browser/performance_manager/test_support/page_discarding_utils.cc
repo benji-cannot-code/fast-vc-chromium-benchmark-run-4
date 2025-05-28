bf6d9a83534adf65d32ec6bf9df69a89e42799e6
Christopher Grant <cjgrant@chromium.org>
2025-05-28 11:07:15 -0700

    MemorySaver: Deflake chip tests by ensuring reliable discarding
    Interactive tests that exercise UI chip state shouldn't have to worry
    about discards being blocked due to eligibility (there are other tests
    for that).  Per the associated bug, analysis shows that these chip
    tests become stable with this change (it both fixes currently flaky
    test(s) and allows previously-disabled tests to be re-enabled).  See
    associated bug for details, as well as crbug.com/420645248.  Existing
    disabled tests will be re-enabled in a separate CL in case there are
    more problems with them.
    
    Note that DiscardEligibilityPolicy is not public at this point, so a
    test helper exposes this hook to Memory Saver tests.
    
    Bug: 407785192
    Change-Id: I3905006547958177517639d779cfea1fb44497fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6425985
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466608}

--- chrome/browser/performance_manager/test_support/page_discarding_utils.cc
+++ chrome/browser/performance_manager/test_support/page_discarding_utils.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/decorators/page_live_state_decorator.h"
 #include "components/performance_manager/public/user_tuning/prefs.h"
 #include "components/performance_manager/test_support/graph_test_harness.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace performance_manager::testing {
 
@@ -46,6 +47,12 @@ void MakePageNodeDiscardable(PageNodeImpl* page_node,
            CanDiscardResult::kEligible);
   CHECK_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::SUGGESTED),
            CanDiscardResult::kEligible);
+}
+
+void SetAllPagesDiscardableForTest() {
+  auto* policy = policies::DiscardEligibilityPolicy::GetFromGraph();
+  ASSERT_TRUE(policy);
+  policy->set_always_discard_for_testing(true);
 }
 
 GraphTestHarnessWithDiscardablePage::GraphTestHarnessWithDiscardablePage()
--- chrome/browser/ui/views/performance_controls/memory_saver_interactive_ui_test.cc
+++ chrome/browser/ui/views/performance_controls/memory_saver_interactive_ui_test.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/performance_manager/public/user_tuning/user_performance_tuning_manager.h"
+#include "chrome/browser/performance_manager/test_support/page_discarding_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/resource_coordinator/lifecycle_unit_state.mojom-shared.h"
 #include "chrome/browser/resource_coordinator/tab_lifecycle_unit.h"
@@ -300,7 +301,11 @@ class MemorySaverChipInteractiveTest
 
   void SetUpOnMainThread() override {
     MemorySaverInteractiveTestMixin::SetUpOnMainThread();
+
     SetMemorySaverModeEnabled(true);
+
+    // Discard tabs unconditionally in Chip tests.
+    performance_manager::testing::SetAllPagesDiscardableForTest();
   }
 
   views::BubbleDialogDelegate* GetMemorySaverBubble() {
