1ec88d46ae4b0bbb6c7a95c084e0d78b28151c8c
Radu Nitescu <radunitescu@google.com>
2025-05-28 04:55:21 -0700

    [iOS][LU] Add action for URL opener (QR code scan)
    Bug: None
    Change-Id: I33a9e7c301769b130fbeb261302901f3f3783505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6588031
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466402}

--- ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_view_finder_coordinator.mm
@@ -170,6 +170,7 @@ LensViewFinderTransition TransitionFromPresentationStyle(
 
 - (void)lensController:(id<ChromeLensViewFinderController>)lensController
           didSelectURL:(GURL)url {
+  [_metricsRecorder recordLensViewFinderCameraURLOpen];
   __weak __typeof(self) weakSelf = self;
   [self exitLensViewFinderAnimated:YES
                         completion:^{
--- ios/chrome/browser/lens_overlay/model/lens_view_finder_metrics_recorder.mm
+++ ios/chrome/browser/lens_overlay/model/lens_view_finder_metrics_recorder.mm
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   RecordAction(base::UserMetricsAction("Mobile.LensViewFinder.DismissTapped"));
 }
 
+- (void)recordLensViewFinderCameraURLOpen {
+  RecordAction(base::UserMetricsAction("Mobile.LensViewFinder.CameraURLOpen"));
+}
+
 - (void)recordImageWithSource:(LensViewFinderImageSource)source {
   switch (source) {
     case LensViewFinderImageSource::kCamera:
