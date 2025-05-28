1f67cee726807deb7dedbbd207734214b2dc8609
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-05-28 07:58:06 -0700

    Replace an ASSERT in a helper function with a GoogleTest matcher
    The helper function `AssertNoFlagsSet` has an ASSERT statement. The
    purpose of ASSERT is to abort a test when an error would prevent the
    test from correctly running. ASSERT however aborts the test by virtue of
    a hidden `return` statement. It therefore cannot be used in a helper
    function because that would just abort that function and would let the
    test continue executing.
    
    More generally, we should avoid putting test expectations in helper
    functions because if the expectation fails, the test failure will lack
    the context needed to understand what instance of the helper function
    call is to blame (go/totw/206).
    
    Another issue with `AssertNoFlagsSet` was that some invocations should
    have been ASSERTs while others should have been EXCEPTs. This CL
    replaces `AssertNoFlagsSet` with a `HasNoFlagSet` matcher, allowing
    either EXPECT or ASSERT to be used directly in the test body. It also
    makes it clear that `deadline_` is the variable being validated.
    
    Bug: 413314866
    Change-Id: I788305a9be6f29c049bbf41c250c8bd1254bf5f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578465
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466466}

--- base/threading/hang_watcher_unittest.cc
+++ base/threading/hang_watcher_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/barrier_closure.h"
+#include "base/containers/fixed_flat_map.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -1152,20 +1153,29 @@ TEST_F(WatchHangsInScopeBlockingTest, MAYBE_NewScopeDoesNotBlockDuringCapture) {
 namespace internal {
 namespace {
 
-constexpr std::array<HangWatchDeadline::Flag, 3> kAllFlags{
-    {HangWatchDeadline::Flag::kMinValue,
-     HangWatchDeadline::Flag::kIgnoreCurrentWatchHangsInScope,
-     HangWatchDeadline::Flag::kShouldBlockOnHang}};
+// Matcher validating that the specified `HangWatchDeadline` has no flag set.
+MATCHER(HasNoFlagSet, /*description=*/"") {
+  static constexpr auto kAllFlags =
+      base::MakeFixedFlatMap<HangWatchDeadline::Flag, std::string_view>({
+          {HangWatchDeadline::Flag::kMinValue, "kMinValue"},
+          {HangWatchDeadline::Flag::kIgnoreCurrentWatchHangsInScope,
+           "kIgnoreCurrentWatchHangsInScope"},
+          {HangWatchDeadline::Flag::kShouldBlockOnHang, "kShouldBlockOnHang"},
+      });
+
+  for (const auto& [flag, description] : kAllFlags) {
+    if (arg.IsFlagSet(flag)) {
+      *result_listener << "where flag " << description << " is set";
+      return false;
+    }
+  }
+  return true;
+}
+
 }  // namespace
 
 class HangWatchDeadlineTest : public testing::Test {
  protected:
-  void AssertNoFlagsSet() const {
-    for (HangWatchDeadline::Flag flag : kAllFlags) {
-      ASSERT_FALSE(deadline_.IsFlagSet(flag));
-    }
-  }
-
   // Return a flag mask without one of the flags for test purposes. Use to
   // ignore that effect of setting a flag that was just set.
   uint64_t FlagsMinus(uint64_t flags, HangWatchDeadline::Flag flag) {
@@ -1187,7 +1197,7 @@ TEST_F(HangWatchDeadlineTest, BitsPreservedThroughExtract) {
 // side-effects. Neither the flags nor the deadline change concurrently in this
 // test.
 TEST_F(HangWatchDeadlineTest, SetAndClearPersistentFlag) {
-  AssertNoFlagsSet();
+  ASSERT_THAT(deadline_, HasNoFlagSet());
 
   // Grab the original values for flags and deadline.
   auto [old_flags, old_deadline] = deadline_.GetFlagsAndDeadline();
@@ -1228,7 +1238,7 @@ TEST_F(HangWatchDeadlineTest, SetAndClearPersistentFlag) {
 TEST_F(HangWatchDeadlineTest, SetDeadline) {
   TimeTicks ticks;
 
-  AssertNoFlagsSet();
+  ASSERT_THAT(deadline_, HasNoFlagSet());
   ASSERT_NE(deadline_.GetDeadline(), ticks);
 
   // Set the deadline and verify it stuck.
@@ -1236,14 +1246,14 @@ TEST_F(HangWatchDeadlineTest, SetDeadline) {
   EXPECT_EQ(deadline_.GetDeadline(), ticks);
 
   // Only the value was modified, no flags should be set.
-  AssertNoFlagsSet();
+  EXPECT_THAT(deadline_, HasNoFlagSet());
 }
 
 // Verify that setting a non-persistent flag (kShouldBlockOnHang)
 // when the TimeTicks value changed since calling the flag setting
 // function fails and has no side-effects.
 TEST_F(HangWatchDeadlineTest, SetShouldBlockOnHangDeadlineChanged) {
-  AssertNoFlagsSet();
+  ASSERT_THAT(deadline_, HasNoFlagSet());
 
   auto [flags, deadline] = deadline_.GetFlagsAndDeadline();
 
@@ -1268,7 +1278,7 @@ TEST_F(HangWatchDeadlineTest, SetShouldBlockOnHangDeadlineChanged) {
 // Verify that clearing a persistent (kIgnoreCurrentWatchHangsInScope) when
 // the value changed succeeds and has non side-effects.
 TEST_F(HangWatchDeadlineTest, ClearIgnoreHangsDeadlineChanged) {
-  AssertNoFlagsSet();
+  ASSERT_THAT(deadline_, HasNoFlagSet());
 
   auto [flags, deadline] = deadline_.GetFlagsAndDeadline();
 
@@ -1300,7 +1310,7 @@ TEST_F(HangWatchDeadlineTest, ClearIgnoreHangsDeadlineChanged) {
 // the deadline or flags changed succeeds and has non side-effects.
 TEST_F(HangWatchDeadlineTest,
        SetIgnoreCurrentHangWatchScopeEnableDeadlineChanged) {
-  AssertNoFlagsSet();
+  ASSERT_THAT(deadline_, HasNoFlagSet());
 
   auto [flags, deadline] = deadline_.GetFlagsAndDeadline();
 
