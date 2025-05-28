b66ee9c389d5fe2ffe6124e39582819d7378ba51
Christopher Grant <cjgrant@chromium.org>
2025-05-28 05:29:52 -0700

    Passwords: Create three-button save dialog field trial
    The few Skia test images affected by this change are being updated to
    match the new look.
    
    Bug: 419251689
    Change-Id: I1e923160ff333cbe80d7d58a4655afdebd190ca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6588555
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466416}

--- chrome/browser/ui/views/passwords/password_bubble_browsertest.cc
+++ chrome/browser/ui/views/passwords/password_bubble_browsertest.cc
@@ -74,6 +74,7 @@ class PasswordBubbleBrowserTest
 
 IN_PROC_BROWSER_TEST_P(PasswordBubbleBrowserTest,
                        InvokeUi_PendingPasswordBubble) {
+  set_baseline("6588555");
   ShowAndVerifyUi();
 }
 
