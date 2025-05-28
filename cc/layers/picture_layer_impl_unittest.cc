2dc69f81a2e1ba3c94984faeebae8b1650f1f119
Colin Blundell <blundell@chromium.org>
2025-05-27 23:54:48 -0700

    [//cc] Port PictureLayerImpl test away from creating low res tiles
    Ports UpdateLCDTextPushToActiveTreeWith2dScale in anticipation of
    removing low-res tiling altogether.
    
    Bug: 418234930
    Change-Id: Ic93bbd68ead4f153de334d86a3ab71c9d1f8c8ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6591100
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466318}

--- cc/layers/picture_layer_impl_unittest.cc
+++ cc/layers/picture_layer_impl_unittest.cc
@@ -5511,7 +5511,7 @@ TEST_F(NoLowResPictureLayerImplTest, UpdateLCDTextPushToActiveTree) {
     EXPECT_FALSE(tile->can_use_lcd_text());
 }
 
-TEST_F(LegacySWPictureLayerImplTest, UpdateLCDTextPushToActiveTreeWith2dScale) {
+TEST_F(NoLowResPictureLayerImplTest, UpdateLCDTextPushToActiveTreeWith2dScale) {
   SetupPendingTree(FakeRasterSource::CreateFilledWithText(gfx::Size(200, 200)));
   gfx::Vector2dF ideal_scale(4.f, 2.f);
   float page_scale = 4.f;
@@ -5526,14 +5526,12 @@ TEST_F(LegacySWPictureLayerImplTest, UpdateLCDTextPushToActiveTreeWith2dScale) {
   ActivateTree();
 
   EXPECT_TRUE(active_layer()->can_use_lcd_text());
-  ASSERT_EQ(2u, active_layer()->tilings()->num_tilings());
+  ASSERT_EQ(1u, active_layer()->tilings()->num_tilings());
   ASSERT_TRUE(active_layer()->HighResTiling()->has_tiles());
   std::vector<Tile*> tiles =
       active_layer()->HighResTiling()->AllTilesForTesting();
   for (Tile* tile : tiles)
     EXPECT_TRUE(tile->can_use_lcd_text());
-  for (Tile* tile : active_layer()->LowResTiling()->AllTilesForTesting())
-    EXPECT_FALSE(tile->can_use_lcd_text());
 
   SetupPendingTree(FakeRasterSource::CreateFilledWithText(gfx::Size(200, 200)));
   SetupDrawPropertiesAndUpdateTiles(pending_layer(), ideal_scale, 1.0f,
@@ -5545,11 +5543,9 @@ TEST_F(LegacySWPictureLayerImplTest, UpdateLCDTextPushToActiveTreeWith2dScale) {
   ActivateTree();
 
   EXPECT_FALSE(active_layer()->can_use_lcd_text());
-  ASSERT_EQ(2u, active_layer()->tilings()->num_tilings());
+  ASSERT_EQ(1u, active_layer()->tilings()->num_tilings());
   ASSERT_TRUE(active_layer()->HighResTiling()->has_tiles());
   for (Tile* tile : active_layer()->HighResTiling()->AllTilesForTesting())
-    EXPECT_FALSE(tile->can_use_lcd_text());
-  for (Tile* tile : active_layer()->LowResTiling()->AllTilesForTesting())
     EXPECT_FALSE(tile->can_use_lcd_text());
 }
 
