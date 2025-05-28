7ad1d4ab9bbd5534ff29b62173241d9822a04c23
Wan-Teh Chang <wtc@google.com>
2025-05-28 12:36:51 -0700

    Roll src/third_party/dav1d/libdav1d/ 8d9561809..7889ac760 (1 commit)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/8d956180934f..7889ac760317
    
    $ git log 8d9561809..7889ac760 --date=short --no-merges --format='%ad %ae %s'
    2025-05-28 gramner cdf: Remove unused eob_hi_bit entries
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: I4de2f42fae27c48a02316eca24e2e5333c2420b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598095
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466659}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.1-5-g8d95618"
+#define DAV1D_VERSION "1.5.1-6-g7889ac7"
