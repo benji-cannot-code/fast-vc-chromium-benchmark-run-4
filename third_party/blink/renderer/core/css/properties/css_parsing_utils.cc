5757225307a33f59f964bb4102890b79ac9bbeac
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2025-05-28 05:29:29 -0700

    Revert CL for the high confidence crash culprit for http://crash/8009eaf7e80a95d9
    The Suspect CL https://chromium-review.googlesource.com/q/I5bd643a24e82ff5f06af80c896e93842c278e1b2 is highly likely the cause of the crash. Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Change-Id: If77bfa375f56f752ff4c5e3face95645b00d7394
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6597354
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466415}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -1606,10 +1606,11 @@ CSSIdentifierValue* ConsumeIdentRange(CSSParserTokenStream& stream,
   return ConsumeIdent(stream);
 }
 
+namespace {
+
 // https://drafts.csswg.org/css-values-5/#ident
 CSSFunctionValue* ConsumeIdentFunction(CSSParserTokenStream& stream,
                                        const CSSParserContext& context) {
-  CHECK(RuntimeEnabledFeatures::CSSIdentFunctionEnabled());
   if (stream.Peek().FunctionId() != CSSValueID::kIdent) {
     return nullptr;
   }
@@ -1648,6 +1649,8 @@ CSSFunctionValue* ConsumeIdentFunction(CSSParserTokenStream& stream,
   return MakeGarbageCollected<CSSFunctionValue>(
       CSSValueID::kIdent, CSSValueList::kSpaceSeparator, std::move(values));
 }
+
+}  // namespace
 
 CSSCustomIdentValue* ConsumeCustomIdent(CSSParserTokenStream& stream,
                                         const CSSParserContext& context) {
