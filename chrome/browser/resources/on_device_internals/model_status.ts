59dcee4611d88e280120262349ff392db2f630ac
Zekun Jiang <zekunjiang@google.com>
2025-05-28 13:04:05 -0700

    Add feature adaptations display to on device internals.
    UI screenshot:
    https://screenshot.googleplex.com/AfrbQddJ2NjGRY9
    
    Bug: 375443504
    Change-Id: I4812e165c1f10e113ee95268a20481e31af51035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6581058
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466669}

--- chrome/browser/resources/on_device_internals/model_status.ts
+++ chrome/browser/resources/on_device_internals/model_status.ts
@@ -44,6 +44,7 @@ export class OnDeviceInternalsModelStatusElement extends CrLitElement {
     suppModels: [],
     modelCrashCount: 0,
     maxModelCrashCount: 0,
+    featureAdaptations: [],
   };
 
   protected accessor mayRestartBrowser_: boolean = false;
