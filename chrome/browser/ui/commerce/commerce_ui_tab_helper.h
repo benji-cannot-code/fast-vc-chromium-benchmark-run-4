f1f2c549ca00152a30c6e44882f27787497df578
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-05-28 15:46:58 -0700

    Price Insights page action: Provide TabInterface for WC lookup
    `CommerceUiTabHelper` inherits from `content::WebContentsObserver`, so
    `web_contents()` can point to non-tab WebContents or nullptr. Calling
    `TabInterface::GetFromContents` on a non-tab WebContents is unsafe and
    can crash.
    
    This CL updates the price-insights to get a TabInterface and get the web
    content from it.
    
    Bug: 420875194
    Change-Id: Iac17fc5dddbac4a74bad80110ddbac38901230c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6593455
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466764}

--- chrome/browser/ui/commerce/commerce_ui_tab_helper.h
+++ chrome/browser/ui/commerce/commerce_ui_tab_helper.h
@@ -33,8 +33,11 @@ class BookmarkModel;
 
 namespace content {
 class NavigationHandle;
-class WebContents;
 }  // namespace content
+
+namespace tabs {
+class TabInterface;
+}
 
 namespace image_fetcher {
 class ImageFetcher;
@@ -53,7 +56,7 @@ class ProductSpecificationsPageActionController;
 // features.
 class CommerceUiTabHelper : public content::WebContentsObserver {
  public:
-  CommerceUiTabHelper(content::WebContents* contents,
+  CommerceUiTabHelper(tabs::TabInterface& tab_interface,
                       ShoppingService* shopping_service,
                       bookmarks::BookmarkModel* model,
                       image_fetcher::ImageFetcher* image_fetcher,
@@ -217,6 +220,10 @@ class CommerceUiTabHelper : public content::WebContentsObserver {
 
   base::RepeatingClosure GetPageActionControllerNotificationCallback(
       base::RepeatingClosure page_action_icon_update_callback);
+
+  // Reference to the tab interface, which provides access to tab-specific
+  // features.
+  const raw_ref<tabs::TabInterface> tab_interface_;
 
   // The shopping service is tied to the lifetime of the browser context
   // which will always outlive this tab helper.
