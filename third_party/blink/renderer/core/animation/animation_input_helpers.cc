6616f8829103bbaf117acba726ad9e06f0ddc7d4
Fredrik Söderquist <fs@opera.com>
2025-05-28 15:24:04 -0700

    Remove unused includes in animation_input_helpers.cc
    Left-overs after the SVG WebAnimations removal.
    
    Bug: 40318405
    Change-Id: I7a47e476711b09c8a35fa06d12990330aa59094f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6596681
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466752}

--- third_party/blink/renderer/core/animation/animation_input_helpers.cc
+++ third_party/blink/renderer/core/animation/animation_input_helpers.cc
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/execution_context/security_context.h"
-#include "third_party/blink/renderer/core/svg/animation/svg_smil_element.h"
-#include "third_party/blink/renderer/core/svg/svg_element.h"
-#include "third_party/blink/renderer/core/svg_names.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
