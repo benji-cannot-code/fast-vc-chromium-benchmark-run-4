0ec6fdae61d8b68bd801d0d1b48d8573580bb451
Nicolas Dossou-Gbete <dgn@chromium.org>
2025-05-28 13:12:48 -0700

    rcaps_pipeline: Put built-in search engines branding assets behind flag
    Declares the new ENABLE_BUILTIN_SEARCH_PROVIDER_ASSETS buildflag that
    guards whether built-in logos and snippets should be used in search
    engine choice UIs.
    
    Bug: b:322753899
    Change-Id: I34fe37ade7d20d37de3cec4703eacc0fd30fc209
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6596815
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466678}

--- components/search_engines/template_url_data.cc
+++ components/search_engines/template_url_data.cc
@@ -19,12 +19,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/memory_usage_estimator.h"
 #include "base/uuid.h"
 #include "base/values.h"
+#include "build/branding_buildflags.h"
 #include "components/search_engines/regulatory_extension_type.h"
 #include "components/search_engines/search_engines_switches.h"
 #include "crypto/hash.h"
 #include "third_party/search_engines_data/resources/definitions/prepopulated_engines.h"
 
 namespace {
+
+constexpr bool kEnableBuiltinSearchProviderAssets =
+    !!BUILDFLAG(ENABLE_BUILTIN_SEARCH_PROVIDER_ASSETS);
 
 // Returns a GUID used for sync, which is random except for built-in search
 // engines. The latter benefit from using a deterministic GUID, to make sure
@@ -103,7 +107,11 @@ TemplateURLData::TemplateURLData(
       contextual_search_url(contextual_search_url),
       logo_url(logo_url),
       doodle_url(doodle_url),
-      base_builtin_resource_id(base_builtin_resource_id),
+      // Loading search engines resources is not supporting on non-branded
+      // builds.
+      base_builtin_resource_id(kEnableBuiltinSearchProviderAssets
+                                   ? base_builtin_resource_id
+                                   : std::string_view()),
       search_url_post_params(search_url_post_params),
       suggestions_url_post_params(suggest_url_post_params),
       image_url_post_params(image_url_post_params),
