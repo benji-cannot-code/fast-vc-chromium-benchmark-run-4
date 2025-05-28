9f49751a5c51ae2835de59f2e7f2140a9470b5b8
Joemer Ramos <joemerramos@google.com>
2025-05-28 01:14:46 -0700

    [iOS] Call PrivacySafeBrowsingCoordinator Stop To Ensure Cleanup
    Not calling stop is causing a crash related to PrefBackedBooleans not
    being cleaned up. This CL calls stop to ensure that this crash doesn't
    happen. This is the appropriate fix following the intended behavior in
    crrev.com/c/6433123.
    
    Fixed: 420390008
    Change-Id: I7e710f93f8a051ebbc1c99335b8a621752158c21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6592411
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466336}

--- ios/chrome/browser/settings/ui_bundled/settings_navigation_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/settings_navigation_controller.mm
@@ -797,6 +797,7 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
     return;
   }
   DCHECK(!self.privacySafeBrowsingCoordinator);
+  [self.privacySafeBrowsingCoordinator stop];
   self.privacySafeBrowsingCoordinator = [[PrivacySafeBrowsingCoordinator alloc]
       initWithBaseNavigationController:self
                                browser:self.browser];
