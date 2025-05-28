7dc5330c4a265e02ff78d76e9439b187e4e56e05
Yishui Liu <yishuil@google.com>
2025-05-28 14:50:22 -0700

    [Card Benefits][Capital One] Clean capital one benefits related logic
    This change removes capital one related logic from optimization guide
    since capital one benefits is rolled back.
    
    Bug: 419349165
    Change-Id: I684760890e9b939bba9ee8fc79a9a9d34ba183f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574439
    Commit-Queue: Yishui Liu <yishuil@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466733}

--- components/autofill/core/browser/integrators/optimization_guide/autofill_optimization_guide.cc
+++ components/autofill/core/browser/integrators/optimization_guide/autofill_optimization_guide.cc
@@ -92,18 +92,6 @@ GetCardBenefitsOptimizationTypesForCard(const CreditCard& card) {
         optimization_guide::proto::BMO_CREDIT_CARD_TRAVEL_BENEFITS);
     optimization_types.push_back(
         optimization_guide::proto::BMO_CREDIT_CARD_WHOLESALE_CLUB_BENEFITS);
-  } else if (card.issuer_id() == kCapitalOneCardIssuerId) {
-    optimization_types.push_back(
-        optimization_guide::proto::CAPITAL_ONE_CREDIT_CARD_DINING_BENEFITS);
-    optimization_types.push_back(
-        optimization_guide::proto::CAPITAL_ONE_CREDIT_CARD_GROCERY_BENEFITS);
-    optimization_types.push_back(
-        optimization_guide::proto::
-            CAPITAL_ONE_CREDIT_CARD_ENTERTAINMENT_BENEFITS);
-    optimization_types.push_back(
-        optimization_guide::proto::CAPITAL_ONE_CREDIT_CARD_STREAMING_BENEFITS);
-    optimization_types.push_back(
-        optimization_guide::proto::CAPITAL_ONE_CREDIT_CARD_BENEFITS_BLOCKED);
   }
   return optimization_types;
 }
@@ -305,16 +293,6 @@ AutofillOptimizationGuide::AttemptToGetEligibleCreditCardBenefitCategory(
         optimization_guide::proto::BMO_CREDIT_CARD_TRAVEL_BENEFITS);
     issuer_optimization_types.push_back(
         optimization_guide::proto::BMO_CREDIT_CARD_WHOLESALE_CLUB_BENEFITS);
-  } else if (issuer_id == kCapitalOneCardIssuerId) {
-    issuer_optimization_types.push_back(
-        optimization_guide::proto::CAPITAL_ONE_CREDIT_CARD_DINING_BENEFITS);
-    issuer_optimization_types.push_back(
-        optimization_guide::proto::CAPITAL_ONE_CREDIT_CARD_GROCERY_BENEFITS);
-    issuer_optimization_types.push_back(
-        optimization_guide::proto::
-            CAPITAL_ONE_CREDIT_CARD_ENTERTAINMENT_BENEFITS);
-    issuer_optimization_types.push_back(
-        optimization_guide::proto::CAPITAL_ONE_CREDIT_CARD_STREAMING_BENEFITS);
   }
 
   for (auto& optimization_type : issuer_optimization_types) {
