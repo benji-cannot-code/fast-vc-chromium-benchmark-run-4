33a4eea0eef9e3995b3f186448a519c6278ebc19
Minoru Chikamune <chikamune@chromium.org>
2025-05-27 17:26:26 -0700

    Add Android WebView testing config for TimedHTMLParserBudget
    Bug: 40279826
    Change-Id: I947a5010f7a2bbc4b48b31af56c20b859c09ba49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6590772
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466233}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1072,6 +1072,7 @@ public final class ProductionSupportedFlagList {
         Flag.baseFeature(
                 "PrefetchServiceWorker",
                 "Enables SpeculationRules prefetch to ServiceWorker-controlled URLs."),
+        Flag.baseFeature("TimedHTMLParserBudget"),
         // Add new commandline switches and features above. The final entry should have a
         // trailing comma for cleaner diffs.
     };
