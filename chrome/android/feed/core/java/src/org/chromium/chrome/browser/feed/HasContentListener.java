c3a7bdb408f7789316767aaa3667ac53a4c9b1e2
Shu Yang <shuyng@google.com>
2025-05-27 17:03:47 -0700

    Add @NullMarked to chrome/android/feed/core/j/s/o/c/chrome/browser/feed
    Bug: 389129271
    Change-Id: Id88bff98632ba1773c727304476b1c0acb4348a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6592775
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466227}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/HasContentListener.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/HasContentListener.java
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.feed;
 
+import org.chromium.build.annotations.NullMarked;
+
 /** Interface to listen to the hasContent events from FeedSurfaceMediator. */
+@NullMarked
 public interface HasContentListener {
     /** Called when the has content state changes. */
     void hasContentChanged(@StreamKind int kind, boolean hasContent);
