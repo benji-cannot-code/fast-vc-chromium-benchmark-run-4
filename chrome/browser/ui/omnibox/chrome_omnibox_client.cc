fe480c9c84d8cee16a2acc20a95996de796c6912
Marlon Facey <mfacey@chromium.org>
2025-05-28 14:14:08 -0700

    [omnibox] Trigger different surveys for different arms for On-focus ZPS
    Add ability to supply trigger IDs from Finch so that different arms can
    trigger different surveys.
    
    Bug: 
    Change-Id: I169653e77a98009a1c781b221d6e2d58f4459eaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6597368
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466717}

--- chrome/browser/ui/omnibox/chrome_omnibox_client.cc
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.cc
@@ -477,6 +477,15 @@ void ChromeOmniboxClient::CheckConditionsAndLaunchSurvey() {
       show_happiness_survey ? kHatsSurveyTriggerOnFocusZpsSuggestionsHappiness
                             : kHatsSurveyTriggerOnFocusZpsSuggestionsUtility;
 
+  const std::string& trigger_id =
+      show_happiness_survey
+          ? omnibox_feature_configs::
+                HappinessTrackingSurveyForOmniboxOnFocusZps::Get()
+                    .happiness_trigger_id
+          : omnibox_feature_configs::
+                HappinessTrackingSurveyForOmniboxOnFocusZps::Get()
+                    .utility_trigger_id;
+
   HatsService* hats_service =
       HatsServiceFactory::GetForProfile(profile_, /*create_if_necessary=*/true);
 
@@ -501,7 +510,8 @@ void ChromeOmniboxClient::CheckConditionsAndLaunchSurvey() {
         {{"page classification",
           metrics::OmniboxEventProto::PageClassification_Name(
               GetPageClassification(/*is_prefetch=*/false))},
-         {"channel", channel}});
+         {"channel", channel}},
+        trigger_id, HatsService::SurveyOptions());
   }
 }
 
