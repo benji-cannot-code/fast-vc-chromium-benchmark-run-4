358ffc7223b61cd26b6465212d10bb6b22cae91f
Jason Hu <hujasonx@google.com>
2025-05-28 11:23:38 -0700

    [lensoverlay]Send lns_surface param for all Lens queries
    Removes the gating on the kLensOverlayUpdatedClientContext flag.
    
    Bug: 419372379
    Change-Id: If9395e65949aa778461ccd9da4758bd5317ce490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6593771
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466617}

--- chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
+++ chrome/browser/ui/lens/lens_overlay_controller_browsertest.cc
@@ -3527,7 +3527,7 @@ IN_PROC_BROWSER_TEST_F(LensOverlayControllerBrowserTest,
   const GURL first_search_url(
       "https://www.google.com/"
       "search?source=chrome.cr.menu&vsint=CAMqDAoCCAcSAggDGAEgAg&q=oranges"
-      "&lns_fp=1&lns_mode=text&gsc=2&hl=en-US&cs=0");
+      "&lns_fp=1&lns_mode=text&lns_surface=42&gsc=2&hl=en-US&cs=0");
   controller->IssueTextSelectionRequestForTesting("oranges", 20, 200);
   EXPECT_TRUE(content::WaitForLoadStop(
       controller->GetSidePanelWebContentsForTesting()));
@@ -3578,7 +3578,7 @@ IN_PROC_BROWSER_TEST_F(LensOverlayControllerBrowserTest,
   const GURL third_search_url(
       "https://www.google.com/"
       "search?source=chrome.cr.menu&vsint=CAMqCgoCCAcSAggDIAI&q=kiwi&lns_fp=1"
-      "&lns_mode=text&gsc=2&hl=en-US&cs=0");
+      "&lns_mode=text&lns_surface=42&gsc=2&hl=en-US&cs=0");
   content::TestNavigationObserver third_search_observer(
       controller->GetSidePanelWebContentsForTesting());
   controller->IssueTextSelectionRequestForTesting("kiwi", 1, 100);
@@ -3727,7 +3727,7 @@ IN_PROC_BROWSER_TEST_F(LensOverlayControllerBrowserTest,
   const GURL second_search_url(
       "https://www.google.com/"
       "search?source=chrome.cr.ctxi&vsint=CAMqCgoCCAcSAggDIAI&q=kiwi&lns_fp="
-      "1&lns_mode=text&gsc=2&hl=en-US&cs=0");
+      "1&lns_mode=text&lns_surface=42&gsc=2&hl=en-US&cs=0");
   content::TestNavigationObserver second_observer(
       controller->GetSidePanelWebContentsForTesting());
   controller->IssueTextSelectionRequestForTesting("kiwi", 1, 100);
