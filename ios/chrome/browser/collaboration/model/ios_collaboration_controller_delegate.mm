9e4d6ddd7519315cab33e825de3011927cd1bd4a
Arthur Milchior <arthurmilchior@google.com>
2025-05-28 04:51:27 -0700

    [iOS]Ios collaboration controller delegate owns its signin coordinator
    There should be no visible change for users.
    
    Change-Id: Iea7aab50374b3de0f0d56b8ecb22ecb2c29fce32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6590818
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466401}

--- ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate.mm
+++ ios/chrome/browser/collaboration/model/ios_collaboration_controller_delegate.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/service/sync_service.h"
 #import "components/sync/service/sync_user_settings.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin/signin_constants.h"
+#import "ios/chrome/browser/authentication/ui_bundled/signin/signin_coordinator.h"
 #import "ios/chrome/browser/collaboration/model/collaboration_service_factory.h"
 #import "ios/chrome/browser/favicon/model/favicon_loader.h"
 #import "ios/chrome/browser/favicon/model/ios_chrome_favicon_loader_factory.h"
@@ -135,6 +136,7 @@ IOSCollaborationControllerDelegate::IOSCollaborationControllerDelegate(
 }
 
 IOSCollaborationControllerDelegate::~IOSCollaborationControllerDelegate() {
+  StopSigninCoordinator();
   if (IsInObserverList()) {
     CHECK(browser_);
     browser_->RemoveObserver(this);
@@ -191,6 +193,7 @@ void IOSCollaborationControllerDelegate::Cancel(ResultCallback result) {
   if (!browser_) {
     return;
   }
+  StopSigninCoordinator();
 
   if (dismiss_join_screen_callback_) {
     std::move(dismiss_join_screen_callback_).Run();
@@ -235,8 +238,6 @@ void IOSCollaborationControllerDelegate::ShowAuthenticationUi(
       break;
   }
 
-  id<ApplicationCommands> application_handler =
-      HandlerForProtocol(browser_->GetCommandDispatcher(), ApplicationCommands);
   auto completion_block = base::CallbackToBlock(base::BindOnce(
       &IOSCollaborationControllerDelegate::OnAuthenticationComplete,
       weak_ptr_factory_.GetWeakPtr(), std::move(result)));
@@ -272,8 +273,11 @@ void IOSCollaborationControllerDelegate::ShowAuthenticationUi(
   command.fullScreenPromo = fullScreenPromo;
   command.contextStyle = context_style;
 
-  [application_handler showSignin:command
-               baseViewController:base_view_controller_];
+  signin_coordinator_ =
+      [SigninCoordinator signinCoordinatorWithCommand:command
+                                              browser:browser_
+                                   baseViewController:base_view_controller_];
+  [signin_coordinator_ start];
 }
 
 void IOSCollaborationControllerDelegate::NotifySignInAndSyncStatusChange() {
@@ -479,6 +483,11 @@ void IOSCollaborationControllerDelegate::BrowserDestroyed(Browser* browser) {
 
 #pragma mark - Private
 
+void IOSCollaborationControllerDelegate::StopSigninCoordinator() {
+  [signin_coordinator_ stop];
+  signin_coordinator_ = nil;
+}
+
 void IOSCollaborationControllerDelegate::ShowLeaveOrDeleteDialog(
     const tab_groups::EitherGroupID& either_id,
     ResultCallback result) {
@@ -502,6 +511,7 @@ void IOSCollaborationControllerDelegate::OnAuthenticationComplete(
     ResultCallback result,
     SigninCoordinatorResult sign_in_result,
     id<SystemIdentity> completion_info) {
+  StopSigninCoordinator();
   if (sign_in_result == SigninCoordinatorResultCanceledByUser) {
     std::move(result).Run(CollaborationControllerDelegate::Outcome::kCancel);
     return;
