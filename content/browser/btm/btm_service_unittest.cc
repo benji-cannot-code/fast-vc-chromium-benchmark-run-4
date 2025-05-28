0a3bf0fa4b7d372e93e9bd7d0c9d98ff908f6f5d
Andrew Liu <liu@chromium.org>
2025-05-28 09:28:10 -0700

    [btm] Handle redirect chain with a single storage call
    In a follow-up CL, we will edit the BTM.Redirect UKMs to properly take
    user activation and webauthn assertion (rather than inaccurately setting
    site engagement level).
    
    Beyond removing the recursive `GotState` calls, this CL simplifies the
    signature of a `RecordBounceCallback` such that the
    `StatefulBounceCallback` is curried in earlier and there's less plumbing
    of parameters. The fact that `HandleRedirect` is static and used for
    tests forces some baseline degree of callback passing, so I wonder if we
    can make it non-static and simplify things further.
    
    Bug: 414361732
    Change-Id: I10c44c713a14a4bba5c44554da7f1f790a099dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594037
    Commit-Queue: Andrew Liu <liu@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466531}

--- content/browser/btm/btm_service_unittest.cc
+++ content/browser/btm/btm_service_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/callback_forward.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
@@ -66,7 +67,7 @@ class BtmServiceTest : public testing::Test {
       std::string_view final_url,
       base::Time time,
       bool stateful,
-      base::RepeatingCallback<void(const GURL&)> stateful_bounce_callback) {
+      BtmServiceImpl::StatefulBounceCallback stateful_bounce_callback) {
     BtmRedirectChainInfo chain(
         MakeUrlAndId(initial_url), MakeUrlAndId(final_url),
         /*length=*/3,
@@ -220,8 +221,7 @@ TEST_F(BtmServiceTest, EmptySiteEventsIgnored) {
   GURL url;
   base::Time bounce = base::Time::FromSecondsSinceUnixEpoch(2);
   RecordBounce(profile.get(), url.spec(), "https://initial.com",
-               "https://final.com", bounce, false,
-               base::BindRepeating([](const GURL& final_url) {}));
+               "https://final.com", bounce, false, base::DoNothing());
   WaitOnStorage(service);
 
   // Verify that an entry is not returned when querying for an empty URL,
@@ -317,7 +317,7 @@ class BtmServiceStateRemovalTest : public testing::Test {
       std::string_view final_url,
       base::Time time,
       bool stateful,
-      base::RepeatingCallback<void(const GURL&)> stateful_bounce_callback) {
+      BtmServiceImpl::StatefulBounceCallback stateful_bounce_callback) {
     BtmRedirectChainInfo chain(
         MakeUrlAndId(initial_url), MakeUrlAndId(final_url),
         /*length=*/3,
@@ -390,9 +390,9 @@ TEST_F(BtmServiceStateRemovalTest,
   btm::Populate3PcExceptions(GetProfile(), /*web_contents=*/nullptr,
                              complete_chain->initial_url.url,
                              complete_chain->final_url.url, complete_redirects);
-  GetService()->HandleRedirectChain(
-      std::move(complete_redirects), std::move(complete_chain),
-      base::BindRepeating([](const GURL& final_url) {}));
+  GetService()->HandleRedirectChain(std::move(complete_redirects),
+                                    std::move(complete_chain),
+                                    base::DoNothing());
   WaitOnStorage(GetService());
   // Expect one call to Observer.OnChainHandled when handling a complete chain.
   EXPECT_EQ(chain_counter.count(), 1u);
@@ -419,9 +419,9 @@ TEST_F(BtmServiceStateRemovalTest,
   btm::Populate3PcExceptions(GetProfile(), /*web_contents=*/nullptr,
                              partial_chain->initial_url.url,
                              partial_chain->final_url.url, partial_redirects);
-  GetService()->HandleRedirectChain(
-      std::move(partial_redirects), std::move(partial_chain),
-      base::BindRepeating([](const GURL& final_url) {}));
+  GetService()->HandleRedirectChain(std::move(partial_redirects),
+                                    std::move(partial_chain),
+                                    base::DoNothing());
   WaitOnStorage(GetService());
   // Expect no calls to Observer.OnChainHandled when handling a partial chain.
   EXPECT_EQ(chain_counter.count(), 0u);
@@ -441,7 +441,7 @@ TEST_F(BtmServiceStateRemovalTest, DISABLED_BrowsingDataDeletion_Enabled) {
   GURL url("https://example.com");
   base::Time bounce = base::Time::FromSecondsSinceUnixEpoch(2);
   RecordBounce(url.spec(), "https://initial.com", "https://final.com", bounce,
-               false, base::BindRepeating([](const GURL& final_url) {}));
+               false, base::DoNothing());
   WaitOnStorage(GetService());
   EXPECT_TRUE(GetBtmState(GetService(), url).has_value());
 
@@ -503,7 +503,7 @@ TEST_F(BtmServiceStateRemovalTest,
   browser_client_.GrantCookieAccessTo3pSite(excepted_3p_url);
 
   int stateful_bounce_count = 0;
-  base::RepeatingCallback<void(const GURL&)> increment_bounce =
+  BtmServiceImpl::StatefulBounceCallback increment_bounce =
       base::BindLambdaForTesting(
           [&](const GURL& final_url) { stateful_bounce_count++; });
 
@@ -551,7 +551,7 @@ TEST_F(BtmServiceStateRemovalTest,
   Add3PCException(scoped_excepted_1p_url, redirect_url_1);
 
   int stateful_bounce_count = 0;
-  base::RepeatingCallback<void(const GURL&)> increment_bounce =
+  BtmServiceImpl::StatefulBounceCallback increment_bounce =
       base::BindLambdaForTesting(
           [&](const GURL& final_url) { stateful_bounce_count++; });
 
@@ -642,7 +642,7 @@ TEST_F(BtmServiceStateRemovalTest,
       ContentSettingsType::TOP_LEVEL_STORAGE_ACCESS, CONTENT_SETTING_ALLOW);
   */
   int stateful_bounce_count = 0;
-  base::RepeatingCallback<void(const GURL&)> increment_bounce =
+  BtmServiceImpl::StatefulBounceCallback increment_bounce =
       base::BindLambdaForTesting(
           [&](const GURL& final_url) { stateful_bounce_count++; });
 
@@ -720,7 +720,7 @@ TEST_F(
   browser_client_.BlockThirdPartyCookies(redirect_url_1, scoped_blocked_1p_url);
 
   int stateful_bounce_count = 0;
-  base::RepeatingCallback<void(const GURL&)> increment_bounce =
+  BtmServiceImpl::StatefulBounceCallback increment_bounce =
       base::BindLambdaForTesting(
           [&](const GURL& final_url) { stateful_bounce_count++; });
 
@@ -783,7 +783,7 @@ TEST_F(BtmServiceStateRemovalTest, ImmediateEnforcement) {
   GURL url("https://example.com");
   base::Time bounce = Now();
   RecordBounce(url.spec(), "https://initial.com", "https://final.com", bounce,
-               false, base::BindRepeating([](const GURL& final_url) {}));
+               false, base::DoNothing());
   WaitOnStorage(GetService());
   EXPECT_TRUE(GetBtmState(GetService(), url).has_value());
 
@@ -871,7 +871,7 @@ TEST_F(BtmServiceHistogramTest, DeletionLatency) {
   GURL url("https://example.com");
   base::Time bounce = base::Time::FromSecondsSinceUnixEpoch(2);
   RecordBounce(url.spec(), "https://initial.com", "https://final.com", bounce,
-               false, base::BindRepeating([](const GURL& final_url) {}));
+               false, base::DoNothing());
   WaitOnStorage(GetService());
 
   // Set the current time to just after the bounce happened.
@@ -911,8 +911,7 @@ TEST_F(BtmServiceHistogramTest, Deletion_ExceptedAs1P) {
   browser_client_.AllowThirdPartyCookiesOnSite(excepted_1p_url);
   base::Time bounce_time = base::Time::FromSecondsSinceUnixEpoch(2);
   RecordBounce(url.spec(), excepted_1p_url.spec(), "https://final.com",
-               bounce_time, true,
-               base::BindRepeating([](const GURL& final_url) {}));
+               bounce_time, true, base::DoNothing());
   WaitOnStorage(GetService());
 
   // Time-travel to after the grace period has ended for the bounce.
@@ -945,8 +944,7 @@ TEST_F(BtmServiceHistogramTest, Deletion_ExceptedAs3P) {
   browser_client_.GrantCookieAccessTo3pSite(excepted_3p_url);
   base::Time bounce_time = base::Time::FromSecondsSinceUnixEpoch(2);
   RecordBounce(excepted_3p_url.spec(), "https://initial.com",
-               "https://final.com", bounce_time, true,
-               base::BindRepeating([](const GURL& final_url) {}));
+               "https://final.com", bounce_time, true, base::DoNothing());
   WaitOnStorage(GetService());
 
   // Time-travel to after the grace period has ended for the bounce.
@@ -978,8 +976,7 @@ TEST_F(BtmServiceHistogramTest, DISABLED_Deletion_Enforced) {
   GURL url("https://example.com");
   base::Time bounce_time = base::Time::FromSecondsSinceUnixEpoch(2);
   RecordBounce(url.spec(), "https://initial.com", "https://final.com",
-               bounce_time, true,
-               base::BindRepeating([](const GURL& final_url) {}));
+               bounce_time, true, base::DoNothing());
   WaitOnStorage(GetService());
 
   // Time-travel to after the grace period has ended for the bounce.
