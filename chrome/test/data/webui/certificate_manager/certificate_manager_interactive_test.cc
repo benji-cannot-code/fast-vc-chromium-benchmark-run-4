70bd250585176cda8e3a67cad0efaec6d005a7ac
dpapad <dpapad@chromium.org>
2025-05-28 16:58:46 -0700

    WebUI: Move certificate_manager out of cr_components/, part 2.
    In this part moving the test code.
    
    Bug: 390333881
    Change-Id: I47f22ac79c3fbe19da172e6a6b144bdf45736e75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598604
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466797}

---
+++ chrome/test/data/webui/certificate_manager/certificate_manager_interactive_test.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/test/browser_test.h"
+
+using CertificateManagerFocusTest = WebUIMochaBrowserTest;
+
+IN_PROC_BROWSER_TEST_F(CertificateManagerFocusTest, CertificateManager) {
+  set_test_loader_host(chrome::kChromeUICertificateManagerHost);
+  RunTest("certificate_manager/certificate_manager_v2_focus_test.js",
+          "mocha.run()");
+}
