01b2f920379097c4f2cbd127762b87d92d2ce62a
Aishwarya Rajesh <aishwaryarj@google.com>
2025-05-29 15:04:57 -0700

    Revert "[TSLO] Repurpose WindowInsetsUtils#getWidestUnoccludedRect() to"
    This reverts commit 713dd49a7a3c932387c48c268087573c06da336d.
    
    Reason for revert: Causing test failure in parent CL: https://ci.chromium.org/ui/p/chromium/builders/ci/android-15-tablet-landscape-x64-rel/2396/overview
    
    Bug: 418868898
    Original change's description:
    > [TSLO] Repurpose WindowInsetsUtils#getWidestUnoccludedRect() to
    > additionally return if the unoccluded region is complex
    >
    > Also update AppHeaderCoordinator heuristics checks to avoid customizing
    > the app header when the unoccluded region in the caption bar is complex.
    >
    > OBSOLETE_HISTOGRAM[Android.DesktopWindowHeuristicResult3]=Replaced by Android.DesktopWindowHeuristicResult4
    >
    > Bug: 418868898
    > Change-Id: I8cf3dc1ef5e466ed4464ffc83319360deeff3652
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574741
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1467189}
    
    Bug: 418868898
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I58fbc446fac861e245b1f8260389b57889a814d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6605638
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467206}

--- chrome/browser/ui/android/desktop_windowing/java/src/org/chromium/chrome/browser/ui/desktop_windowing/AppHeaderUtils.java
+++ chrome/browser/ui/android/desktop_windowing/java/src/org/chromium/chrome/browser/ui/desktop_windowing/AppHeaderUtils.java
@@ -38,7 +38,6 @@ public class AppHeaderUtils {
         DesktopWindowHeuristicResult.CAPTION_BAR_BOUNDING_RECT_INVALID_HEIGHT,
         DesktopWindowHeuristicResult.WIDEST_UNOCCLUDED_RECT_EMPTY,
         DesktopWindowHeuristicResult.DISALLOWED_ON_EXTERNAL_DISPLAY,
-        DesktopWindowHeuristicResult.COMPLEX_UNOCCLUDED_REGION,
         DesktopWindowHeuristicResult.NUM_ENTRIES,
     })
     public @interface DesktopWindowHeuristicResult {
@@ -48,10 +47,9 @@ public class AppHeaderUtils {
         int CAPTION_BAR_BOUNDING_RECT_INVALID_HEIGHT = 3;
         int WIDEST_UNOCCLUDED_RECT_EMPTY = 4;
         int DISALLOWED_ON_EXTERNAL_DISPLAY = 5;
-        int COMPLEX_UNOCCLUDED_REGION = 6;
 
         // Be sure to also update enums.xml when updating these values.
-        int NUM_ENTRIES = 7;
+        int NUM_ENTRIES = 6;
     }
 
     // These values are persisted to logs. Entries should not be renumbered and
@@ -133,7 +131,7 @@ public class AppHeaderUtils {
             @DesktopWindowHeuristicResult int result) {
         assert result != DesktopWindowHeuristicResult.UNKNOWN;
         RecordHistogram.recordEnumeratedHistogram(
-                "Android.DesktopWindowHeuristicResult4",
+                "Android.DesktopWindowHeuristicResult3",
                 result,
                 DesktopWindowHeuristicResult.NUM_ENTRIES);
     }
