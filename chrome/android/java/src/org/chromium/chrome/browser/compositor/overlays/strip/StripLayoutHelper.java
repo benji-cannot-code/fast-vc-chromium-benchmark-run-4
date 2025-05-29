be74b78bb77f4cec24f87a32a0a750abbeae3090
Sirisha Kavuluru <skavuluru@google.com>
2025-05-29 10:38:56 -0700

    [TabStrip] Support nullable hover card view
    We call SLH#onHoverEnter() immediately after hover card view inflate()
    call which throws a NPE if view is null. Inflation is an async process,
    so view might not be set during SLH#onHoverEnter()
    
    To get around, added null check for hover card view and also invoking
    force show hover card during setTabHoverCardView which is called
    onInflationComplete()
    
    Bug: 420974445
    Change-Id: I48baac15d386765460b93c27854fd8ff13ca177b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6602694
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467070}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -505,8 +505,8 @@ public class StripLayoutHelper
     @Nullable private final TabDragSource mTabDragSource;
 
     // Tab hover state.
-    private StripLayoutTab mLastHoveredTab;
-    private StripTabHoverCardView mTabHoverCardView;
+    private @Nullable StripLayoutTab mLastHoveredTab;
+    private @Nullable StripTabHoverCardView mTabHoverCardView;
     private long mLastHoverCardExitTime;
 
     // Tab Group Sync.
@@ -2477,6 +2477,10 @@ public class StripLayoutHelper
     @VisibleForTesting
     void setTabHoverCardView(StripTabHoverCardView tabHoverCardView) {
         mTabHoverCardView = tabHoverCardView;
+        // If onHoverEnter was already processed before this method call, show card now.
+        if (mLastHoveredTab != null && !mTabHoverCardView.isShown()) {
+            showTabHoverCardView(/* isDelayedCall= */ false);
+        }
     }
 
     StripTabHoverCardView getTabHoverCardViewForTesting() {
@@ -2506,18 +2510,21 @@ public class StripLayoutHelper
         if (mLastHoveredTab == null) {
             return;
         }
-
         // Clear close button hover state.
         mLastHoveredTab.setCloseHovered(false);
-
         // Remove the highlight from the last hovered tab.
         updateHoveredTabAttachedState(mLastHoveredTab, false);
-        mStripTabEventHandler.removeMessages(MESSAGE_HOVER_CARD);
-        if (mTabHoverCardView.isShown()) {
-            mLastHoverCardExitTime = SystemClock.uptimeMillis();
-        }
-        mTabHoverCardView.hide();
         mLastHoveredTab = null;
+
+        // Hide hover card view.
+        mStripTabEventHandler.removeMessages(MESSAGE_HOVER_CARD);
+        // Hover card view can be null if hover event was processed before view inflation completes.
+        if (mTabHoverCardView != null) {
+            if (mTabHoverCardView.isShown()) {
+                mLastHoverCardExitTime = SystemClock.uptimeMillis();
+            }
+            mTabHoverCardView.hide();
+        }
     }
 
     @VisibleForTesting
