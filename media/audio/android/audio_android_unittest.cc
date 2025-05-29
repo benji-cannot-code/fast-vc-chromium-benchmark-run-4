4d98ee4452a4b37cee3383cfed992ecc5257ab11
Maya Jelonkiewicz <mjel@google.com>
2025-05-29 00:40:02 -0700

    Correctly skip Android audio tests if a feature is unavailable
    GTEST_SKIP only directly stops execution of the current function, not
    the entire test. Tests which call InitFeatures themselves need to check
    the skipped state directly within the test body in order to stop test
    execution early. Note that this already worked correctly for TEST_P
    tests, where InitFeatures is called from the test class constructor and
    GTEST_SKIP skips running the test function in the first place.
    
    Test: run_media_unittests --gtest_filter="AudioAndroid*" -v
    Bug: 420877363
    Change-Id: Ic5655bcb6d80e82749b44e2ab89a1fe6c38590b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6597019
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Maya Jelonkiewicz <mjel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466910}

--- media/audio/android/audio_android_unittest.cc
+++ media/audio/android/audio_android_unittest.cc
@@ -931,6 +931,10 @@ TEST_P(AudioAndroidOutputTest, CreateAndCloseOutputStream) {
 // closed. This test is only relevant for AAudioWithPerStreamDeviceSelection.
 TEST_F(AudioAndroidOutputTest, CreateAndCloseOutputStreamWithDevice) {
   InitFeatures(AudioApi::AAudioWithPerStreamDeviceSelection);
+  if (IsSkipped()) {
+    return;
+  }
+
   std::optional<AudioDeviceDescription> device =
       GetFirstNonDefaultOutputDevice();
   if (!device.has_value()) {
@@ -992,6 +996,10 @@ TEST_P(AudioAndroidOutputTest, OpenAndCloseOutputStream) {
 // device ID. This test is only relevant for AAudioWithPerStreamDeviceSelection.
 TEST_F(AudioAndroidOutputTest, OpenAndCloseOutputStreamWithDevice) {
   InitFeatures(AudioApi::AAudioWithPerStreamDeviceSelection);
+  if (IsSkipped()) {
+    return;
+  }
+
   std::optional<AudioDeviceDescription> device =
       GetFirstNonDefaultOutputDevice();
   if (!device.has_value()) {
