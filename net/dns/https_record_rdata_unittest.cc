4f12f39fe6cc8a387cee885fb5b47774eabaa5a4
David Benjamin <davidben@chromium.org>
2025-05-29 08:46:35 -0700

    Stop storing unparsed_params in ServiceFormHttpsRecordRdata
    We never use this field except to check it in tests. Don't bother saving
    it at all and save some cycles.
    
    Change-Id: I0c8b0749941a757cbb5a7133637610d1fb49ef8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598178
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467018}

--- net/dns/https_record_rdata_unittest.cc
+++ net/dns/https_record_rdata_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <vector>
 
-#include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/containers/span.h"
 #include "net/base/ip_address.h"
@@ -120,9 +119,7 @@ TEST(HttpsRecordRdataTest, ParsesService) {
       false /* default_alpn */, std::optional<uint16_t>(46) /* port */,
       std::vector<IPAddress>({IPAddress(8, 8, 8, 8)}) /* ipv4_hint */,
       ech_config_testdata /* ech_config */,
-      std::vector<IPAddress>({expected_ipv6}) /* ipv6_hint */,
-      base::flat_map<uint16_t, std::string>(
-          {{7, "foo"}}) /* unparsed_params */);
+      std::vector<IPAddress>({expected_ipv6}) /* ipv6_hint */);
   EXPECT_TRUE(rdata->IsEqual(&expected));
 
   EXPECT_FALSE(rdata->IsAlias());
@@ -140,8 +137,6 @@ TEST(HttpsRecordRdataTest, ParsesService) {
   EXPECT_THAT(service_rdata->ech_config(),
               testing::ElementsAreArray(ech_config_testdata));
   EXPECT_THAT(service_rdata->ipv6_hint(), testing::ElementsAre(expected_ipv6));
-  EXPECT_THAT(service_rdata->unparsed_params(),
-              testing::ElementsAre(testing::Pair(7, "foo")));
   EXPECT_TRUE(service_rdata->IsCompatible());
 }
 
@@ -164,8 +159,7 @@ TEST(HttpsRecordRdataTest, AliasIsEqualRejectsWrongType) {
   ServiceFormHttpsRecordRdata service(
       1u /* priority */, "service.name.test", {} /* mandatory_keys */,
       {} /* alpn_ids */, true /* default_alpn */, std::nullopt /* port */,
-      {} /* ipv4_hint */, {} /* ech_config */, {} /* ipv6_hint */,
-      {} /* unparsed_params */);
+      {} /* ipv4_hint */, {} /* ech_config */, {} /* ipv6_hint */);
 
   EXPECT_TRUE(alias.IsEqual(&alias));
   EXPECT_FALSE(alias.IsEqual(&service));
@@ -176,8 +170,7 @@ TEST(HttpsRecordRdataTest, ServiceIsEqualRejectsWrongType) {
   ServiceFormHttpsRecordRdata service(
       1u /* priority */, "service.name.test", {} /* mandatory_keys */,
       {} /* alpn_ids */, true /* default_alpn */, std::nullopt /* port */,
-      {} /* ipv4_hint */, {} /* ech_config */, {} /* ipv6_hint */,
-      {} /* unparsed_params */);
+      {} /* ipv4_hint */, {} /* ech_config */, {} /* ipv6_hint */);
 
   EXPECT_FALSE(service.IsEqual(&alias));
   EXPECT_TRUE(service.IsEqual(&service));
