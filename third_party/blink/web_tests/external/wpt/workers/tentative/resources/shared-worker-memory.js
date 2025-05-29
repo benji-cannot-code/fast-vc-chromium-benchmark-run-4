59002fe0aa7d2bb1830742bac59d1725bdba6397
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-05-28 23:28:47 -0700

    Add WPT for SharedWorker extendedLifetime
    With the extendedLifetime option, SharedWorker is guaranteed to live
    after the final window unload for a certain period of time.
    
    Bug: 400473072
    Change-Id: I1481bfb6c8ef5860340f3318dada357525efbe31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6595553
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466898}

---
+++ third_party/blink/web_tests/external/wpt/workers/tentative/resources/shared-worker-memory.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+let stored_data = null;
+
+function processMessage(e) {
+  function respond(data) {
+    e.currentTarget.postMessage(Object.assign(data, {reqid: e.data.reqid}));
+  }
+
+  switch (e.data.op) {
+    case 'load': {
+      respond({ack: 'load', status: 'OK', data: stored_data});
+      break;
+    }
+    case 'store': {
+      try {
+        stored_data = e.data.data
+      } catch (err) {
+        respond({ack: 'store', status: 'ERROR', error: err.name});
+        return;
+      }
+      respond({ack: 'store', status: 'OK'});
+      break;
+    }
+  }
+}
+
+self.addEventListener('connect', e => {
+  e.ports[0].onmessage = processMessage;
+});
