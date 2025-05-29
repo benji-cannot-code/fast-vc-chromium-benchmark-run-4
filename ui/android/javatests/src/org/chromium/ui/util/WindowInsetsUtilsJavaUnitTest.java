d1193ac9ca0d15b4d7368dafa760bc2f310f1dd6
Aishwarya Rajesh <aishwaryarj@google.com>
2025-05-29 14:34:51 -0700

    [TSLO] Repurpose WindowInsetsUtils#getWidestUnoccludedRect() to additionally return if the unoccluded region is complex
    Also update AppHeaderCoordinator heuristics checks to avoid customizing
    the app header when the unoccluded region in the caption bar is complex.
    
    OBSOLETE_HISTOGRAM[Android.DesktopWindowHeuristicResult3]=Replaced by Android.DesktopWindowHeuristicResult4
    
    Bug: 418868898
    Change-Id: I8cf3dc1ef5e466ed4464ffc83319360deeff3652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574741
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467189}

--- ui/android/javatests/src/org/chromium/ui/util/WindowInsetsUtilsJavaUnitTest.java
+++ ui/android/javatests/src/org/chromium/ui/util/WindowInsetsUtilsJavaUnitTest.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import android.graphics.Rect;
 import android.os.Build.VERSION_CODES;
@@ -24,12 +26,13 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MaxAndroidSdkLevel;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
+import org.chromium.ui.util.WindowInsetsUtils.UnoccludedRegion;
 
 import java.util.List;
 
 /**
- * Test for {@link WindowInsetsUtils#getWidestUnoccludedRect} to fill the gap where Region does not
- * work properly in Robolectric tests.
+ * Test for {@link WindowInsetsUtils#getUnoccludedRegion} to fill the gap where Region does not work
+ * properly in Robolectric tests.
  */
 @RunWith(BaseJUnit4ClassRunner.class)
 @Batch(Batch.UNIT_TESTS)
@@ -40,9 +43,9 @@ public class WindowInsetsUtilsJavaUnitTest {
     public void testGetWidestUnoccludedRect_Horizontal() {
         Rect region = new Rect(0, 0, 600, 800);
         List<Rect> blocks = List.of(new Rect(0, 0, 100, 800), new Rect(400, 0, 600, 800));
-        assertEquals(
-                new Rect(100, 0, 400, 800),
-                WindowInsetsUtils.getWidestUnoccludedRect(region, blocks));
+        UnoccludedRegion unoccludedRegion = WindowInsetsUtils.getUnoccludedRegion(region, blocks);
+        assertEquals(new Rect(100, 0, 400, 800), unoccludedRegion.getWidestUnoccludedRect());
+        assertFalse("Unoccluded region should not be complex.", unoccludedRegion.isRegionComplex());
     }
 
     @Test
@@ -50,9 +53,9 @@ public class WindowInsetsUtilsJavaUnitTest {
     public void testGetWidestUnoccludedRect_NoVerticalBlocker() {
         Rect region = new Rect(0, 0, 600, 800);
         List<Rect> blocks = List.of(new Rect(0, 0, 100, 300), new Rect(400, 400, 600, 800));
-        assertEquals(
-                new Rect(0, 300, 600, 400),
-                WindowInsetsUtils.getWidestUnoccludedRect(region, blocks));
+        UnoccludedRegion unoccludedRegion = WindowInsetsUtils.getUnoccludedRegion(region, blocks);
+        assertEquals(new Rect(0, 300, 600, 400), unoccludedRegion.getWidestUnoccludedRect());
+        assertTrue("Unoccluded region should be complex.", unoccludedRegion.isRegionComplex());
     }
 
     @Test
@@ -121,6 +124,8 @@ public class WindowInsetsUtilsJavaUnitTest {
     public void testGetWidestUnoccludedRect_NoBlockedRects() {
         Rect region = new Rect(0, 0, 600, 800);
         List<Rect> blocks = List.of();
-        assertEquals(new Rect(), WindowInsetsUtils.getWidestUnoccludedRect(region, blocks));
+        assertEquals(
+                new Rect(),
+                WindowInsetsUtils.getUnoccludedRegion(region, blocks).getWidestUnoccludedRect());
     }
 }
