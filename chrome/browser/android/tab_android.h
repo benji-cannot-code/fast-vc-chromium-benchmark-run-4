01ffc031296e1bd68919f1884f40fe0829e24bf7
Calder Kitagawa <ckitagawa@chromium.org>
2025-05-29 12:42:02 -0700

    [Tabs] Implement GetParentCollection
    Add/update TODOs and implement the pointer reference to a tab's parent
    collection.
    
    Bug: 409366905
    Change-Id: I9a2844697bf6efcba3206739f2bcada9d0f40876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6603533
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467127}

--- chrome/browser/android/tab_android.h
+++ chrome/browser/android/tab_android.h
@@ -271,6 +271,8 @@ class TabAndroid : public tabs::TabInterface,
   // Holds tab-scoped state. Constructed after tab_helpers.
   std::unique_ptr<tabs::TabFeatures> tab_features_;
 
+  raw_ptr<tabs::TabCollection> parent_collection_ = nullptr;
+
   base::ObserverList<Observer> observers_;
 
   const base::WeakPtr<Profile> profile_;
