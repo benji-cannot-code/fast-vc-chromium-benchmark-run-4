451d2e1f17ffddfb6610443269ac39aaf9a8e05f
Wei Wang <wei4.wang@intel.com>
2025-05-28 20:47:52 -0700

    [WebNN] Fuse QDQ for resample2d on tflite
    This CL supports fusing `dq->resample2d->q` subgraph on tflite.
    
    Input and output operands have to be dequantized from ints8, the scale
    and zero point of input and output have to be scaler. Input and output
    must all have same scale and zero_point.
    
    Bug: 401281047
    Change-Id: I73f1cf987b283b682a7f86037a5afd6a541ce9bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6590131
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1466861}

--- services/webnn/tflite/graph_builder_tflite.h
+++ services/webnn/tflite/graph_builder_tflite.h
@@ -751,6 +751,8 @@ class GraphBuilderTflite final {
   std::optional<TensorInfo> CanFuseQuantizeAndGetOutput(
       const mojom::Reduce& reduce);
   std::optional<TensorInfo> CanFuseQuantizeAndGetOutput(
+      const mojom::Resample2d& resample2d);
+  std::optional<TensorInfo> CanFuseQuantizeAndGetOutput(
       const mojom::Reshape& reshape);
   std::optional<TensorInfo> CanFuseQuantizeAndGetOutput(
       const mojom::Slice& slice);
