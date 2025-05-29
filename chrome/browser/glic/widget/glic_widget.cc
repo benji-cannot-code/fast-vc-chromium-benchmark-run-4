7d0aa91e60e7d7e1be14bdecfdf3ecdf28c0e13f
Orko Garai <orko@igalia.com>
2025-05-29 13:06:20 -0700

    Fix/Skip GlicWindowResizeAnimationTests on wayland
    In `GlicWidget::Create()` don't set position for widget on platforms
    that don't support global coordinates.
    
    Some GlicWindowResizeAnimationTests tests rely on setting position from
    the client-side using global screen coordinates, which isn't possible in
    wayland. So check that in those tests and skip some of them and change
    the expectation in AllCallbacksRunInOrder.
    
    R=iwells
    
    Bug: 419581863
    Change-Id: If39553d88921a90e9775159ebe18de68bc89c28f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594275
    Commit-Queue: Orko Garai <orko@igalia.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467141}

--- chrome/browser/glic/widget/glic_widget.cc
+++ chrome/browser/glic/widget/glic_widget.cc
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/native_widget.h"
 #include "ui/views/widget/widget_delegate.h"
 
+#if BUILDFLAG(IS_OZONE)
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 #if BUILDFLAG(IS_WIN)
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/shell_util.h"
@@ -104,6 +108,15 @@ std::unique_ptr<GlicWidget> GlicWidget::Create(
       views::Widget::InitParams::CLIENT_OWNS_WIDGET,
       views::Widget::InitParams::TYPE_WINDOW_FRAMELESS);
   params.bounds = initial_bounds;
+#if BUILDFLAG(IS_OZONE)
+  // Some platforms don't allow accelerated widgets to be positioned from
+  // client-side. Don't set an origin in that case.
+  if (!ui::OzonePlatform::GetInstance()
+           ->GetPlatformProperties()
+           .supports_global_screen_coordinates) {
+    params.bounds.set_origin({});
+  }
+#endif
   if (user_resizable) {
     params.bounds.Outset(GetTargetOutsets(initial_bounds));
   }
