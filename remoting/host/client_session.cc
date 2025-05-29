6d35acf45d4eb797945806040d8400bfe67e890b
Jamie Walch <jamiewalch@chromium.org>
2025-05-29 10:10:00 -0700

    Defer more ClientSession stuff.
    This is split off from
    https://chromium-review.googlesource.com/c/chromium/src/+/6288607 but it
    seems mostly unrelated to Wayland. Perhaps on other platforms this code
    is never executed before the desktop environment is created, but that
    seems like a fragile assumption, especially since ClientSession already
    has a way to queue work that needs to wait for the desktop environment.
    
    Change-Id: I9ad11e8a1dd399c1531a40b0797a01946c2df729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598902
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467054}

--- remoting/host/client_session.cc
+++ remoting/host/client_session.cc
@@ -1377,6 +1377,12 @@ void ClientSession::OnDesktopDetached() {
 void ClientSession::CreateFileTransferMessageHandler(
     const std::string& channel_name,
     std::unique_ptr<protocol::MessagePipe> pipe) {
+  if (!desktop_environment_) {
+    desktop_environment_ready_callbacks_.push_back(base::BindOnce(
+        &ClientSession::CreateFileTransferMessageHandler,
+        weak_factory_.GetWeakPtr(), channel_name, std::move(pipe)));
+    return;
+  }
   // FileTransferMessageHandler manages its own lifetime and is tied to the
   // lifetime of |pipe|. Once |pipe| is closed, this instance will be cleaned
   // up.
@@ -1396,6 +1402,12 @@ void ClientSession::CreateActionMessageHandler(
     std::vector<ActionRequest::Action> capabilities,
     const std::string& channel_name,
     std::unique_ptr<protocol::MessagePipe> pipe) {
+  if (!desktop_environment_) {
+    desktop_environment_ready_callbacks_.push_back(base::BindOnce(
+        &ClientSession::CreateActionMessageHandler, weak_factory_.GetWeakPtr(),
+        std::move(capabilities), channel_name, std::move(pipe)));
+    return;
+  }
   std::unique_ptr<ActionExecutor> action_executor =
       desktop_environment_->CreateActionExecutor();
   if (!action_executor) {
@@ -1422,6 +1434,12 @@ void ClientSession::CreateRemoteOpenUrlMessageHandler(
 void ClientSession::CreateUrlForwarderControlMessageHandler(
     const std::string& channel_name,
     std::unique_ptr<protocol::MessagePipe> pipe) {
+  if (!desktop_environment_) {
+    desktop_environment_ready_callbacks_.push_back(base::BindOnce(
+        &ClientSession::CreateUrlForwarderControlMessageHandler,
+        weak_factory_.GetWeakPtr(), channel_name, std::move(pipe)));
+    return;
+  }
   // UrlForwarderControlMessageHandler manages its own lifetime and is tied to
   // the lifetime of |pipe|. Once |pipe| is closed, this instance will be
   // cleaned up.
@@ -1433,6 +1451,12 @@ void ClientSession::CreateUrlForwarderControlMessageHandler(
 void ClientSession::CreateRemoteWebAuthnMessageHandler(
     const std::string& channel_name,
     std::unique_ptr<protocol::MessagePipe> pipe) {
+  if (!desktop_environment_) {
+    desktop_environment_ready_callbacks_.push_back(base::BindOnce(
+        &ClientSession::CreateRemoteWebAuthnMessageHandler,
+        weak_factory_.GetWeakPtr(), channel_name, std::move(pipe)));
+    return;
+  }
   // RemoteWebAuthnMessageHandler manages its own lifetime and is tied to the
   // lifetime of |pipe|. Once |pipe| is closed, this instance will be cleaned
   // up.
