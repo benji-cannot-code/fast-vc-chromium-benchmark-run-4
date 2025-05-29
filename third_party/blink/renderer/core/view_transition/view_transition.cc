00544d2f732b36942f13a59558481f3664698e0f
Vladimir Levin <vmpstr@chromium.org>
2025-05-29 13:59:56 -0700

    VT: Don't block rendering if we don't have a document frame.
    Blocking rendering relies on checking if we're the local root. However,
    if there is no frame (startup/teardown) then that check crashes instead.
    
    Avoid this by not blocking rendering if there is no frame.
    
    R=skobes@chromium.org
    
    Test: clusterfuzz doesn't have a reliable repro, so not including test
    Bug: 419361216
    Change-Id: Id3e09e2262d07e6bd503dc553353c729ab2de5a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604798
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467172}

--- third_party/blink/renderer/core/view_transition/view_transition.cc
+++ third_party/blink/renderer/core/view_transition/view_transition.cc
@@ -51,8 +51,9 @@ namespace blink {
 ViewTransition::ScopedPauseRendering::ScopedPauseRendering(
     const Element& element) {
   const Document& document = element.GetDocument();
-  if (!document.GetFrame()->IsLocalRoot())
+  if (!document.GetFrame() || !document.GetFrame()->IsLocalRoot()) {
     return;
+  }
 
   if (!element.IsDocumentElement()) {
     return;
