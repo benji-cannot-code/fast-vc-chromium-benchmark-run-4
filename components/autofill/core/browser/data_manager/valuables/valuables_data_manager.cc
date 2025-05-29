aa3ce6537ff05eaf6cca3e929e7b5ace71eae67c
Norge Vizcay <vizcay@google.com>
2025-05-29 01:19:13 -0700

    Inline matching card function.
    Change-Id: I11a0526ff3f5eea8a2c13100bf35d94a728a99c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6596840
    Reviewed-by: Piotr Wrótniak <piotrwrotniak@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466916}

--- components/autofill/core/browser/data_manager/valuables/valuables_data_manager.cc
+++ components/autofill/core/browser/data_manager/valuables/valuables_data_manager.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/containers/to_vector.h"
 #include "components/autofill/core/browser/data_model/valuables/loyalty_card.h"
 #include "components/autofill/core/browser/ui/autofill_image_fetcher_base.h"
 #include "components/autofill/core/browser/webdata/autofill_change.h"
