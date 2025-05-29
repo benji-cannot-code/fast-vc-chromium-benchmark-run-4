30352b1eea8936fc1a70b01c61153ccedf124810
Joey Arhar <jarhar@chromium.org>
2025-05-29 16:02:28 -0700

    Fix select element container query ::highlight crash
    We skip style recalc for an element that looks like a size container at
    style recalc time, that we know will be reached during layout. If we
    figure out at layout tree building that we won't create a box for it
    after all, or if the box type for some reason is not of the type we
    predicted at style recalc time, we resume the skipped recalc at layout
    tree rebuild time.
    
    In this case, the select element's descendants have computed style but
    not layout objects because the select's picker popover is display:none
    but has style computed for it anyway via special code in
    Element::ShouldStoreComputedStyle.
    
    Fixed: 419167530
    Change-Id: Id65e60820e0572d2cc4215d1398b1e9bae822c97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598757
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467231}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -3554,8 +3554,9 @@ bool StyleEngine::RecalcHighlightStylesForContainer(Element& container) {
           container.ParentComputedStyle());
       new_style != &style) {
     container.SetComputedStyle(new_style);
-    container.GetLayoutObject()->SetStyle(new_style,
-                                          LayoutObject::ApplyStyleChanges::kNo);
+    if (LayoutObject* layout_object = container.GetLayoutObject()) {
+      layout_object->SetStyle(new_style, LayoutObject::ApplyStyleChanges::kNo);
+    }
   }
 
   return depends_on_container_queries;
