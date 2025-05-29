f96283a40f7572fecb6346142c4df3981347be8f
mikt <mikt@google.com>
2025-05-28 23:27:18 -0700

    Reland "[PA] Cache ReservationOffsetTable instead of lookup by address"
    This reverts commit 3d5fd3f483f1d8258c8446fb895fc0a653a2770f.
    
    Reason for revert: pkey failure fixed; see diff from PS1
    
    Original change's description:
    > Revert "[PA] Cache ReservationOffsetTable instead of lookup by address"
    >
    > This reverts commit 25c8be782e077706af5dbe9be7f45e5ef5044736.
    >
    > Reason for revert: crbug.com/419236586 Partition Alloc failures at process launch consistently on all Linux GPU bots. Blocking CLs landing
    >
    > Original change's description:
    > > [PA] Cache ReservationOffsetTable instead of lookup by address
    > >
    > > This change will cache ReservationOffsetTable inside PartitionRoot.
    > > This can have slight perf improvement, but I assume it is not measurable
    > > since ReservationOffsetTable is mainly for direct-mapped allocations.
    > >
    > > True motivation here is to allow us to glue ReservationOffsetTable,
    > > which should be a follow-up CL later.
    > > That makes easier to enable BRP in multiple pools.
    > >
    > > Change-Id: I1a420b506512d9ee469235d1ebb0ed930875694a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6538607
    > > Commit-Queue: Mikihito Matsuura <mikt@google.com>
    > > Reviewed-by: Takashi Sakamoto <tasak@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1463208}
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Id54cbb71a3561767a7668d1a5732cdf2f85bd92d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575301
    > Auto-Submit: Jonathan Ross <jonross@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1463379}
    
    Bug: 419236586
    Cq-Include-Trybots: luci.chromium.try:gpu-fyi-try-linux-wayland-intel-rel
    Change-Id: I84177ce3192245f68b463561c06d194be37b1579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575960
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466896}

--- base/allocator/partition_allocator/src/partition_alloc/partition_root.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_root.h
@@ -271,6 +271,7 @@ struct alignas(64) PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionRoot {
 #if PA_BUILDFLAG(HAS_64_BIT_POINTERS)
     internal::PoolOffsetLookup offset_lookup;
 #endif  // PA_BUILDFLAG(HAS_64_BIT_POINTERS)
+    internal::ReservationOffsetTable reservation_offset_table;
 
     bool eventually_zero_freed_memory = false;
     internal::SchedulerLoopQuarantineConfig
@@ -722,6 +723,10 @@ struct alignas(64) PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionRoot {
     return settings.offset_lookup;
   }
 #endif  // PA_BUILDFLAG(HAS_64_BIT_POINTERS)
+  PA_ALWAYS_INLINE const internal::ReservationOffsetTable&
+  GetReservationOffsetTable() const {
+    return settings.reservation_offset_table;
+  }
 
   PA_ALWAYS_INLINE static PAGE_ALLOCATOR_CONSTANTS_DECLARE_CONSTEXPR size_t
   GetDirectMapMetadataAndGuardPagesSize() {
@@ -1106,15 +1111,9 @@ struct SlotAddressAndSize {
 PA_ALWAYS_INLINE SlotAddressAndSize
 PartitionAllocGetDirectMapSlotStartAndSizeInBRPPool(uintptr_t address) {
   PA_DCHECK(IsManagedByPartitionAllocBRPPool(address));
-#if PA_BUILDFLAG(HAS_64_BIT_POINTERS)
-  // Use this variant of GetDirectMapReservationStart as it has better
-  // performance.
-  uintptr_t offset = OffsetInBRPPool(address);
   uintptr_t reservation_start =
-      GetDirectMapReservationStart(address, kBRPPoolHandle, offset);
-#else  // PA_BUILDFLAG(HAS_64_BIT_POINTERS)
-  uintptr_t reservation_start = GetDirectMapReservationStart(address);
-#endif
+      ReservationOffsetTable::Get(pool_handle::kBRPPoolHandle)
+          .GetDirectMapReservationStart(address);
   if (!reservation_start) {
     return SlotAddressAndSize{.slot_start = uintptr_t(0), .size = size_t(0)};
   }
@@ -1157,7 +1156,9 @@ PartitionAllocGetDirectMapSlotStartAndSizeInBRPPool(uintptr_t address) {
 // the in-slot metadata is in place for this allocation.
 PA_ALWAYS_INLINE SlotAddressAndSize
 PartitionAllocGetSlotStartAndSizeInBRPPool(uintptr_t address) {
-  PA_DCHECK(IsManagedByNormalBucketsOrDirectMap(address));
+  PA_DCHECK(
+      ReservationOffsetTable::Get(address).IsManagedByNormalBucketsOrDirectMap(
+          address));
   DCheckIfManagedByPartitionAllocBRPPool(address);
 
   auto directmap_slot_info =
@@ -1767,7 +1768,8 @@ PA_ALWAYS_INLINE PartitionRoot* PartitionRoot::FromSlotSpanMetadata(
 
 PA_ALWAYS_INLINE PartitionRoot* PartitionRoot::FromFirstSuperPage(
     uintptr_t super_page) {
-  PA_DCHECK(internal::IsReservationStart(super_page));
+  PA_DCHECK(internal::ReservationOffsetTable::Get(super_page)
+                .IsReservationStart(super_page));
   auto* extent_entry = internal::PartitionSuperPageToExtent(super_page);
   PartitionRoot* root = extent_entry->root;
   PA_DCHECK(root->inverted_self == ~reinterpret_cast<uintptr_t>(root));
@@ -1777,7 +1779,8 @@ PA_ALWAYS_INLINE PartitionRoot* PartitionRoot::FromFirstSuperPage(
 PA_ALWAYS_INLINE PartitionRoot* PartitionRoot::FromAddrInFirstSuperpage(
     uintptr_t address) {
   uintptr_t super_page = address & internal::kSuperPageBaseMask;
-  PA_DCHECK(internal::IsReservationStart(super_page));
+  PA_DCHECK(internal::ReservationOffsetTable::Get(super_page)
+                .IsReservationStart(super_page));
   return FromFirstSuperPage(super_page);
 }
 
