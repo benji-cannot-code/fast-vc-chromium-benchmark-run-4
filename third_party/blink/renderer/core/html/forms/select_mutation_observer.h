92a1b0656b4713d0f15d49e19c76588931d31c9b
David Baron <dbaron@chromium.org>
2025-05-28 17:11:53 -0700

    Add missing include guard to select_mutation_observer.h
    This fixes a presubmit that complains any time this file is modified.
    
    Change-Id: I217652bf51242ff5b259707a0115a89496b12709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598263
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466808}

--- third_party/blink/renderer/core/html/forms/select_mutation_observer.h
+++ third_party/blink/renderer/core/html/forms/select_mutation_observer.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FORMS_SELECT_MUTATION_OBSERVER_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FORMS_SELECT_MUTATION_OBSERVER_H_
+
 #include "third_party/blink/renderer/core/dom/mutation_observer.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/inspector/inspector_audits_issue.h"
@@ -57,3 +60,5 @@ class SelectMutationObserver : public MutationObserver::Delegate {
 };
 
 }  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FORMS_SELECT_MUTATION_OBSERVER_H_
