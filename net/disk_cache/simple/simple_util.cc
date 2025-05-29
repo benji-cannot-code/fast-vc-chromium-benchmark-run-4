111c3a1ca0af026d5f2d649e61a6393265165a79
Eriko Kurimoto <elkurin@google.com>
2025-05-29 01:38:28 -0700

    disk_cache: Update const value to constexpr in simple index
    This CL does not change the code behavior.
    
    Change-Id: Ib34334164792f6a2bf8657de8b8fa113871fcfa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6600932
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466922}

--- net/disk_cache/simple/simple_util.cc
+++ net/disk_cache/simple/simple_util.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Size of the uint64_t hash_key number in Hex format in a string.
-const size_t kEntryHashKeyAsHexStringSize = 2 * sizeof(uint64_t);
+constexpr size_t kEntryHashKeyAsHexStringSize = 2 * sizeof(uint64_t);
 
 }  // namespace
 
