65b3c2c70486ca6db5171fb4d0a9112d94b6be02
Nafis Abedin <nafisabedin@google.com>
2025-05-29 10:10:31 -0700

    [Binder] Off-load CCT Realtime Engagement Signals to BG
    In an effort to reduce Binder calls made on the UI thread, this CL moves
    sending Realtime Engagement signals for CCTs to a background thread
    using a sequenced task runner so events are still processed in order.
    
    Currently, even short (~10s) sessions can lead to ~2-4ms of time spent
    on synchronous IPCs in an Official build on a high-end phone. Just one
    of these callbacks can take as long as 5ms or even 20ms for low-end
    devices.
    
    Bug: 407591642
    Change-Id: I4a786525413343df5f9c2831304c308f774cd338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6502912
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467055}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -253,6 +253,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTOpenInBrowserButtonIfEnabledByEmbedder,
     &kCCTPredictiveBackGesture,
     &kCCTPrewarmTab,
+    &kCCTRealtimeEngagementEventsInBackground,
     &kCCTReportParallelRequestStatus,
     &kCCTReportPrerenderEvents,
     &kCCTResizableForThirdParties,
@@ -753,6 +754,10 @@ BASE_FEATURE(kGridTabSwitcherUpdate,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTPrewarmTab, "CCTPrewarmTab", base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kCCTRealtimeEngagementEventsInBackground,
+             "CCTRealtimeEngagementEventsInBackground",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTReportParallelRequestStatus,
              "CCTReportParallelRequestStatus",
