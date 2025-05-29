81b44591e3cdd04106d270c2ee695b609062f9fd
Slobodan Pejic <slobodan@chromium.org>
2025-05-29 08:51:42 -0700

    Document and CHECK the internal usage of UnboundKey.
    PasskeyBrowserBinder hands out UnboundKey from CreateUnboundKey() and
    expects the UnboundKey back in BindKeyForPasskey(); otherwise,
    UnboundKey cleans up the BrowserBoundKey during destruction. The caller
    of PasskeyBrowserBinder has access to the BrowserBoundKey through
    UnboundKey::Get().
    
    Additionally, there is an internal usage where the PasskeyBrowserBinder
    only provides the BBK id to UnboundKey leaving the `browser_bound_key_`
    member null. In this internal usage `UnboundKey::Get()` is not called.
    
    After this change the Get() method CHECKs that `browser_bound_key_` is
    not nullptr before returning the reference.
    
    Browser bound keys are behind the flag
    chrome://flags/#enable-secure-payment-confirmation-browser-bound-key.
    
    Bug: 377278827, b:420940392
    Change-Id: I8bcd0843fd5dce0bee422d387555f93c2814ebd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6601915
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467023}

--- components/payments/content/browser_binding/passkey_browser_binder.cc
+++ components/payments/content/browser_binding/passkey_browser_binder.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/barrier_callback.h"
 #include "base/base64.h"
 #include "base/check.h"
+#include "base/check_deref.h"
 #include "base/containers/contains.h"
 #include "base/containers/to_vector.h"
 #include "base/functional/callback.h"
@@ -157,6 +158,10 @@ PasskeyBrowserBinder::UnboundKey::~UnboundKey() {
   if (!browser_bound_key_id_.empty()) {
     key_store_->DeleteBrowserBoundKey(browser_bound_key_id_);
   }
+}
+
+BrowserBoundKey& PasskeyBrowserBinder::UnboundKey::Get() {
+  return CHECK_DEREF(browser_bound_key_.get());
 }
 
 void PasskeyBrowserBinder::UnboundKey::MarkKeyBoundAndReset() {
