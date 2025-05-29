df3016da4bab74d7ae4a808bc14990b4a24405f6
Jose Dapena Paz <jdapena@igalia.com>
2025-05-29 00:21:19 -0700

    ContainerTiming: introduce the containertiming-ignore attribute
    In the initial implementation of ContainerTiming, setting an
    attribute containertiming makes all the children nodes be
    listened for paint events (unless there is another containertiming
    attribute on a nested child).
    
    This can be inconvenient, specially if we set containertiming to the
    top node, and we show the feedback of the paints modifying the tree.
    
    This patchset introduces a new attribute, containertiming-ignore. When
    set, it will stop propagation of parent container timing affected
    nodes, so it will inhibit paint events for its children.
    
    If a node has both containertiming and containertiming-ignore, the
    ignore is... ignored. If a containertiming is nested in a
    containertiming-ignore, it will still work.
    
    WPT tests provided for the additional propagation rules.
    
    Explainer: https://github.com/bloomberg/container-timing/blob/main/README.md
    Chromestatus: https://chromestatus.com/feature/5110962817073152
    I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/J-WxY0w7bNk/m/VkqnomK-CAAJ
    
    Ticket for ignore specification discussion:
    https://github.com/bloomberg/container-timing/issues/15
    
    Bug: 382422286
    Change-Id: Ib5c793a9fa3d03650d2212adb36a7bb8f1c6c6da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6270777
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466907}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -11959,8 +11959,12 @@ Element* Element::ImplicitAnchorElement() const {
 
 bool Element::RecalcSelfOrAncestorHasContainerTiming() const {
   DCHECK(RuntimeEnabledFeatures::ContainerTimingEnabled());
-  if (IsHTMLElement() && FastHasAttribute(html_names::kContainertimingAttr)) {
-    return true;
+  if (IsHTMLElement()) {
+    if (FastHasAttribute(html_names::kContainertimingAttr)) {
+      return true;
+    } else if (FastHasAttribute(html_names::kContainertimingIgnoreAttr)) {
+      return false;
+    }
   }
   Node* parent = parentNode();
   if (parent && parent->SelfOrAncestorHasContainerTiming()) {
@@ -11974,7 +11978,8 @@ void Element::UpdateDescendantHasContainerTiming(bool has_container_timing) {
   Element* element = ElementTraversal::FirstChild(*this);
   while (element) {
     if (element->IsHTMLElement()) {
-      if (element->FastHasAttribute(html_names::kContainertimingAttr)) {
+      if (element->FastHasAttribute(html_names::kContainertimingAttr) ||
+          element->FastHasAttribute(html_names::kContainertimingIgnoreAttr)) {
         element = ElementTraversal::NextSkippingChildren(*element, this);
         continue;
       }
@@ -12001,7 +12006,8 @@ void Element::UpdateDescendantHasContainerTiming(bool has_container_timing) {
 bool Element::DoesChildContainerTimingNeedChange(const Node& node) const {
   auto* element = DynamicTo<Element>(node);
   if (element && element->IsHTMLElement() &&
-      (element->FastHasAttribute(html_names::kContainertimingAttr))) {
+      (element->FastHasAttribute(html_names::kContainertimingAttr) ||
+       element->FastHasAttribute(html_names::kContainertimingIgnoreAttr))) {
     return false;
   }
   return SelfOrAncestorHasContainerTiming() !=
