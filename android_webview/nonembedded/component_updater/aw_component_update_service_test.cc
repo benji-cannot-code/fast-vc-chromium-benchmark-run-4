2562d02ee283b93d169724ca92865c34735b1df6
Noah Rose Ledesma <noahrose@google.com>
2025-05-29 15:54:53 -0700

    Use Set-Cookie for NetworkFetcher responses
    http://crrev.com/c/6496984 used the wrong header name for responses
    indicating to set a cookie on the client. Fix the name and code
    references to it.
    
    Bug: 363046220
    Change-Id: I1947de06e86aee9c832b299d010a4d940cd2512a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6600029
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467226}

--- android_webview/nonembedded/component_updater/aw_component_update_service_test.cc
+++ android_webview/nonembedded/component_updater/aw_component_update_service_test.cc
@@ -104,7 +104,7 @@ class FailingNetworkFetcher : public update_client::NetworkFetcher {
              /* network_error= */ -2,
              /* header_etag= */ "",
              /* header_x_cup_server_proof= */ "",
-             /* header_cookie= */ "",
+             /* header_set_cookie= */ "",
              /* x_header_retry_after_sec= */ 0ll);
   }
 
@@ -146,7 +146,7 @@ class OnDemandNetworkFetcher : public update_client::NetworkFetcher {
              /* network_error= */ -2,
              /* header_etag= */ "",
              /* header_x_cup_server_proof= */ "",
-             /* header_cookie= */ "",
+             /* header_set_cookie= */ "",
              /* x_header_retry_after_sec= */ 0ll);
   }
 
@@ -203,7 +203,7 @@ class FakeCrxNetworkFetcher : public update_client::NetworkFetcher {
              /* network_error= */ network_error,
              /* header_etag= */ "",
              /* header_x_cup_server_proof= */ "",
-             /* header_cookie= */ "",
+             /* header_set_cookie= */ "",
              /* x_header_retry_after_sec= */ 0ll);
   }
 
@@ -391,8 +391,9 @@ class AwComponentUpdateServiceTest : public testing::Test {
   }
 
   void TearDown() override {
-    if (base::PathExists(component_install_dir_))
+    if (base::PathExists(component_install_dir_)) {
       ASSERT_TRUE(base::DeletePathRecursively(component_install_dir_));
+    }
 
     if (base::PathExists(component_provider_dir_)) {
       ASSERT_TRUE(base::DeletePathRecursively(component_provider_dir_));
--- chrome/updater/net/fallback_net_fetcher.cc
+++ chrome/updater/net/fallback_net_fetcher.cc
@@ -76,7 +76,7 @@ void FallbackNetFetcher::PostRequestDone(
     int net_error,
     const std::string& header_etag,
     const std::string& header_x_cup_server_proof,
-    const std::string& header_cookie,
+    const std::string& header_set_cookie,
     int64_t xheader_retry_after_sec) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   const int should_fallback = net_error || (http_status_code_ != 200);
@@ -91,7 +91,8 @@ void FallbackNetFetcher::PostRequestDone(
   }
   std::move(post_request_complete_callback)
       .Run(std::move(response_body), net_error, header_etag,
-           header_x_cup_server_proof, header_cookie, xheader_retry_after_sec);
+           header_x_cup_server_proof, header_set_cookie,
+           xheader_retry_after_sec);
 }
 
 base::OnceClosure FallbackNetFetcher::DownloadToFile(
