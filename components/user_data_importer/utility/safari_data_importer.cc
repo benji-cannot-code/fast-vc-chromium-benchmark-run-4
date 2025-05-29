ca0aa86c9f735528741a3ec87bb33800fe9e6bb6
Alexis Hetu <sugoi@chromium.org>
2025-05-29 13:50:18 -0700

    PasswordImport integration in SafariDataImporter class
    This CL adds support for receiving a ZIP file and importing
    the passwords contained in the CSV file within that zip file.
    
    The unzipper uses the Rust library and unzips a file from that
    zip file based on the imported data type requested, which is
    either:
    - Passwords
    - Bookmarks
    - History
    - Payment cards
    The unzipper returns a string containing the file contents.
    
    The PasswordImported integration is done in this CL, which
    allows the Safari data importer class to import passwords
    and potentially resolve conflicts.
    
    Other imported data types (bookmarks, history, payment cards)
    are not implemented in this CL.
    
    New unit tests were added to cover basic cases.
    
    See go/bling-import for details.
    
    Bug: 407587751
    Change-Id: I85649e6968ba99c841d9a2593955666ccde1053a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6570731
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467167}

--- components/user_data_importer/utility/safari_data_importer.cc
+++ components/user_data_importer/utility/safari_data_importer.cc
@@ -5,7 +5,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/user_data_importer/utility/safari_data_importer.h"
 
+#include "base/containers/span_rust.h"
+#include "base/strings/sys_string_conversions.h"
+#include "base/threading/scoped_blocking_call.h"
+#include "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
+#include "components/user_data_importer/utility/zip_ffi_glue.rs.h"
+
 namespace user_data_importer {
+
+// Returns the contents of the file of the desired type contained in the
+// provided zip file. Returns an empty string on failure.
+std::string unzipFile(const std::string& zip_filename, FileType filetype) {
+  // TODO(crbug.com/407587751): Add a base::ScopedBlockingCall object here, once
+  // this code has been moved to the IO thread.
+  const std::vector<uint8_t> zip_filename_span(zip_filename.begin(),
+                                               zip_filename.end());
+  rust::Slice<const uint8_t> rs_zip_filename =
+      base::SpanToRustSlice(zip_filename_span);
+  std::string output_bytes;
+  if (!unzip_using_rust(rs_zip_filename, filetype, output_bytes)) {
+    return std::string();
+  }
+  return output_bytes;
+}
 
 SafariDataImporter::SafariDataImporter(
     password_manager::SavedPasswordsPresenter* presenter)
@@ -31,12 +53,17 @@ void SafariDataImporter::Import(
     return;
   }
 
+  // TODO(crbug.com/407587751): Run the following code in the IO thread.
+
   // Passwords import may require conflict resolution, so it is done first.
-  ImportPasswords(zip_filename, std::move(passwords_callback));
+  ImportPasswords(unzipFile(zip_filename, FileType::Passwords),
+                  std::move(passwords_callback));
 
   // Launch payment cards and bookmarks import processes.
-  ImportPaymentCards(zip_filename, std::move(payment_cards_callback));
-  ImportBookmarks(zip_filename, std::move(bookmarks_callback));
+  ImportPaymentCards(unzipFile(zip_filename, FileType::PaymentCards),
+                     std::move(payment_cards_callback));
+  ImportBookmarks(unzipFile(zip_filename, FileType::Bookmarks),
+                  std::move(bookmarks_callback));
 
   // History import may require synchronously reading from the file, so it is
   // done last.
@@ -44,23 +71,43 @@ void SafariDataImporter::Import(
 }
 
 void SafariDataImporter::ImportPasswords(
-    const std::string& zip_filename,
+    std::string csv_data,
     password_manager::PasswordImporter::ImportResultsCallback
         passwords_callback) {
-  // TODO(crbug.com/407587751): Import passwords.
-  password_manager::ImportResults results;
-  std::move(passwords_callback).Run(std::move(results));
+  if (csv_data.empty()) {
+    password_manager::ImportResults results;
+    std::move(passwords_callback).Run(std::move(results));
+    return;
+  }
+
+  // TODO(crbug.com/407587751): Pick a store based on whether the user is
+  // signed in to their account.
+  password_manager::PasswordForm::Store to_store =
+      password_manager::PasswordForm::Store::kAccountStore;
+
+  password_importer_->Import(std::move(csv_data), to_store,
+                             std::move(passwords_callback));
 }
 
 void SafariDataImporter::ImportPaymentCards(
-    const std::string& zip_filename,
+    std::string json_data,
     ImportCallback payment_cards_callback) {
+  if (json_data.empty()) {
+    std::move(payment_cards_callback).Run(0);
+    return;
+  }
+
   // TODO(crbug.com/407587751): Import payment cards.
   std::move(payment_cards_callback).Run(0);
 }
 
-void SafariDataImporter::ImportBookmarks(const std::string& zip_filename,
+void SafariDataImporter::ImportBookmarks(std::string html_data,
                                          ImportCallback bookmarks_callback) {
+  if (html_data.empty()) {
+    std::move(bookmarks_callback).Run(0);
+    return;
+  }
+
   // TODO(crbug.com/407587751): Import bookmarks.
   std::move(bookmarks_callback).Run(0);
 }
@@ -68,6 +115,9 @@ void SafariDataImporter::ImportBookmarks(const std::string& zip_filename,
 void SafariDataImporter::ImportHistory(const std::string& zip_filename,
                                        ImportCallback history_callback) {
   // TODO(crbug.com/407587751): Import history.
+  // Note: Because the history file can be very large, the parsing will happen
+  // entirely in Rust, so that we can stream the unzipper's output to the JSON
+  // parser's input.
   std::move(history_callback).Run(0);
 }
 
@@ -75,9 +125,8 @@ void SafariDataImporter::ResolvePasswordConflicts(
     const std::vector<int>& selected_ids,
     password_manager::PasswordImporter::ImportResultsCallback
         passwords_callback) {
-  // TODO(crbug.com/407587751): Resolve conflicts.
-  password_manager::ImportResults results;
-  std::move(passwords_callback).Run(std::move(results));
+  password_importer_->ContinueImport(selected_ids,
+                                     std::move(passwords_callback));
 }
 
 }  // namespace user_data_importer
