a482bc38935513e81a0e914d560d764ead5cb0d5
Shibalik Mohapatra <shibalik@chromium.org>
2025-05-29 11:59:15 -0700

    [SxS] Constrain to index for attaching tab groups.
    Bug: 420788422
    Change-Id: I4fc0b080cf521d7cbd5bd6ebd13cae94291915d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6599565
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467107}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -549,6 +549,8 @@ gfx::Range TabStripModel::InsertDetachedTabGroupAt(
     static_cast<tabs::TabModel*>(tab)->OnAddedToModel(this);
   }
 
+  index = ConstrainInsertionIndex(index, false);
+
   return InsertDetachedCollectionImpl(
       group_collection, group->active_index_,
       base::BindOnce(&TabStripModel::InsertDetachedTabGroupImpl,
