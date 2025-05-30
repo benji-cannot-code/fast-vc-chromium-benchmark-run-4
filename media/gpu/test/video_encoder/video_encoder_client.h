ddd63346d1ca44207d58a50631240d9e20b95507
Eugene Zemtsov <eugene@chromium.org>
2025-05-30 16:24:54 -0700

    Fix video_encode_accelerator_tests to run on Android bots
    - Allow unknown flags
    - Don't create TestEnvironment on Android if the test already has it
    - Fix test expectations on older Androids and emulators
    
    Bug: 364860537
    Change-Id: I55272f49503be6ec59f400e77418c311fe192da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6599357
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467712}

--- media/gpu/test/video_encoder/video_encoder_client.h
+++ media/gpu/test/video_encoder/video_encoder_client.h
@@ -166,6 +166,8 @@ class VideoEncoderClient : public VideoEncodeAccelerator::Client {
   VideoEncoderStats GetStats() const;
   void ResetStats();
 
+  bool IsHardwareAccelerated();
+
   // VideoEncodeAccelerator::Client implementation
   void RequireBitstreamBuffers(unsigned int input_count,
                                const gfx::Size& input_coded_size,
@@ -283,6 +285,7 @@ class VideoEncoderClient : public VideoEncodeAccelerator::Client {
   bool force_keyframe_ = false;
 
   VideoEncoderStats current_stats_ GUARDED_BY(stats_lock_);
+  VideoEncoderInfo encoder_info_ GUARDED_BY(stats_lock_);
   mutable base::Lock stats_lock_;
 
   scoped_refptr<gpu::TestSharedImageInterface> test_sii_;
