ae6a7a698a47c02251a9ecee2877e89c55c46697
Erik Anderson <Erik.Anderson@microsoft.com>
2025-05-30 08:18:46 -0700

    Add more complete Windows OS version info to about:gpu
    Before this change, about:gpu only shows the first 3 components of the
    OS build number. This is generally insufficient to reason about how
    up-to-date the OS is.
    
    With this change, we now report two versions with all four components.
    
    Using base::win::OSInfo's version_number() method gives us the fourth
    component using a mechanism that should match what winver.exe reports.
    
    Additionally, we report the kernel32 version which reports an accurate
    version even if an OS version shim is being applied.
    
    Before the change, "Operating system" it reports this for 24H2:
    "Windows NT 10.0.26100"
    
    With this change, it reports a version that looks like this, which is
    sufficient to observe that the recent monthly update is installed:
    "Windows NT 10.0.26100.4061 (kernel32 10.0.26100.3912)"
    
    Change-Id: I76f480862f91343665e61a6ef65918f2e3ec4e99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604863
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Auto-Submit: Erik Anderson <Erik.Anderson@microsoft.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467486}

--- content/browser/gpu/gpu_internals_ui.cc
+++ content/browser/gpu/gpu_internals_ui.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gpu_switching_manager.h"
 
 #if BUILDFLAG(IS_WIN)
+#include "base/win/windows_version.h"
 #include "ui/gfx/win/physical_size.h"
 #endif
 
@@ -808,8 +809,22 @@ base::Value::Dict GpuMessageHandler::GetClientInfo() {
 #else
   dict.Set("command_line", command_line);
 #endif
+#if BUILDFLAG(IS_WIN)
+  const base::win::OSInfo& os_info = *base::win::OSInfo::GetInstance();
+  const base::win::OSInfo::VersionNumber os_version = os_info.version_number();
+  base::win::OSInfo::VersionNumber kernel32_version =
+      os_info.Kernel32VersionNumber();
+  dict.Set(
+      "operating_system",
+      base::StringPrintf("%s %u.%u.%u.%u (kernel32 %u.%u.%u.%u)",
+                         base::SysInfo::OperatingSystemName(), os_version.major,
+                         os_version.minor, os_version.build, os_version.patch,
+                         kernel32_version.major, kernel32_version.minor,
+                         kernel32_version.build, kernel32_version.patch));
+#else
   dict.Set("operating_system", base::SysInfo::OperatingSystemName() + " " +
                                    base::SysInfo::OperatingSystemVersion());
+#endif
   dict.Set("angle_commit_id", angle::GetANGLECommitHash());
   dict.Set("graphics_backend",
            std::string("Skia/" STRINGIZE(SK_MILESTONE) " " SKIA_COMMIT_HASH));
