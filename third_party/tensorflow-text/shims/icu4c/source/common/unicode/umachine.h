64af7ee940e445750dcf00d7eaa3d335a889e8ff
Robert Ogden <robertogden@chromium.org>
2025-05-30 11:26:07 -0700

    Pull TF-Text files for needed ops in the new Bert Safety Model
    Bug: 414865959
    Change-Id: I53f3acfcba77dc887eb7e406257af72ac9567a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576263
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467580}

---
+++ third_party/tensorflow-text/shims/icu4c/source/common/unicode/umachine.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_TENSORFLOW_TEXT_SHIMS_ICU4C_SOURCE_COMMON_UNICODE_UMACHINE_H_
+#define THIRD_PARTY_TENSORFLOW_TEXT_SHIMS_ICU4C_SOURCE_COMMON_UNICODE_UMACHINE_H_
+
+#include "third_party/icu/source/common/unicode/umachine.h"
+
+#endif  // THIRD_PARTY_TENSORFLOW_TEXT_SHIMS_ICU4C_SOURCE_COMMON_UNICODE_UMACHINE_H_
