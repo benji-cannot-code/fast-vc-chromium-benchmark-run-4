9ddd4531b7e5220a0c1b2871477e58245985c37c
Mikel Astiz <mastiz@chromium.org>
2025-05-30 00:52:31 -0700

    [sync] Remove unused test-only flag --password-file-for-test
    It has been unused for a long time and therefore it is better to clean
    up the code.
    
    Change-Id: Ib3816336683348afaf682876a780acc945c60fbe
    Bug: 368091420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6596957
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467348}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/command_line.h"
 #include "base/containers/to_vector.h"
-#include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
@@ -22,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -201,20 +199,17 @@ void SyncTest::SetUp() {
 #endif
 
   base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
-  if (cl->HasSwitch(switches::kPasswordFileForTest)) {
-    ReadPasswordFile();
-  } else {
-    // Decide on username to use or create one.
-    if (cl->HasSwitch(switches::kSyncUserForTest)) {
-      username_ = cl->GetSwitchValueASCII(switches::kSyncUserForTest);
-    } else if (server_type_ != EXTERNAL_LIVE_SERVER) {
-      username_ = kDefaultUserEmail;
-    }
-    // Decide on password to use.
-    password_ = cl->HasSwitch(switches::kSyncPasswordForTest)
-                    ? cl->GetSwitchValueASCII(switches::kSyncPasswordForTest)
-                    : "password";
+
+  // Decide on username to use or create one.
+  if (cl->HasSwitch(switches::kSyncUserForTest)) {
+    username_ = cl->GetSwitchValueASCII(switches::kSyncUserForTest);
+  } else if (server_type_ != EXTERNAL_LIVE_SERVER) {
+    username_ = kDefaultUserEmail;
   }
+  // Decide on password to use.
+  password_ = cl->HasSwitch(switches::kSyncPasswordForTest)
+                  ? cl->GetSwitchValueASCII(switches::kSyncPasswordForTest)
+                  : "password";
 
   if (username_.empty() || password_.empty()) {
     LOG(FATAL) << "Cannot run sync tests without GAIA credentials.";
@@ -945,25 +940,6 @@ void SyncTest::WaitForDataModels(Profile* profile) {
   // really about bookmarks.
   bookmarks::test::WaitForBookmarkModelToLoad(
       BookmarkModelFactory::GetForBrowserContext(profile));
-}
-
-void SyncTest::ReadPasswordFile() {
-  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
-  password_file_ = cl->GetSwitchValuePath(switches::kPasswordFileForTest);
-  if (password_file_.empty()) {
-    LOG(FATAL) << "Can't run live server test without specifying --"
-               << switches::kPasswordFileForTest << "=<filename>";
-  }
-  std::string file_contents;
-  base::ReadFileToString(password_file_, &file_contents);
-  ASSERT_NE(file_contents, "")
-      << "Password file \"" << password_file_.value() << "\" does not exist.";
-  std::vector<std::string> tokens = base::SplitString(
-      file_contents, "\r\n", base::KEEP_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
-  ASSERT_EQ(2U, tokens.size()) << "Password file \"" << password_file_.value()
-                               << "\" must contain exactly two lines of text.";
-  username_ = tokens[0];
-  password_ = tokens[1];
 }
 
 void SyncTest::SetupMockGaiaResponses() {
