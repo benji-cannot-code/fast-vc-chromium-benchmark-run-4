bd0311660b41a87c6ab7344518647cc86b7e1e57
Magdalena Skarbinska <mskarbinska@google.com>
2025-05-30 05:03:48 -0700

    [iwa]: Add early return if `KeepAliveRegistry` is shutting down
    This cl adds a check if `KeepAliveRegistry` is shutting down before
    registering a new keep-alive. If it is, then
    `IsolatedWebAppUpdateManager::DiscoverUpdatesForApp` returns early.
    
    Change-Id: I4c6ce76ee0a2c31b81e5aea9974783da4b46d422
    Bug: 421267965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606737
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467416}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
+#include "components/keep_alive_registry/keep_alive_registry.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/prefs/pref_service.h"
@@ -478,6 +479,9 @@ void IsolatedWebAppUpdateManager::DiscoverUpdatesForApp(
     bool allow_downgrades,
     const std::optional<base::Version>& pinned_version,
     bool dev_mode) {
+  if (KeepAliveRegistry::GetInstance()->IsShuttingDown()) {
+    return;
+  }
   auto keep_alive = std::make_unique<ScopedKeepAlive>(
       KeepAliveOrigin::ISOLATED_WEB_APP_UPDATE,
       KeepAliveRestartOption::DISABLED);
