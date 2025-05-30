40272471bc681e9dac1fc56146c8b086ede898d8
Jun Ishiguro <junis@google.com>
2025-05-30 00:35:56 -0700

    Remove browser_process dependency from c/b/ash/hats
    Fixed: b:404132060
    Change-Id: I1ac2d668887a2f70edcfa969d76ade5ea1af3f19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606234
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467340}

--- chrome/browser/ash/hats/hats_notification_controller.cc
+++ chrome/browser/ash/hats/hats_notification_controller.cc
@@ -25,16 +25,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/hats/hats_dialog.h"
 #include "chrome/browser/ash/hats/hats_finch_helper.h"
 #include "chrome/browser/ash/login/startup_utils.h"
-#include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/network/network_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
@@ -275,9 +273,8 @@ bool HatsNotificationController::ShouldShowSurveyToProfile(
   if (profile->IsChild())
     return false;
 
-  const bool is_enterprise_enrolled = g_browser_process->platform_part()
-                                          ->browser_policy_connector_ash()
-                                          ->IsDeviceEnterpriseManaged();
+  const bool is_enterprise_enrolled =
+      ash::InstallAttributes::Get()->IsEnterpriseManaged();
 
   HatsFinchHelper hats_finch_helper(profile, hats_config);
 
