2a81c92f1a3cecdce2e84c436e8d6aabfc46c42f
Kenichi Ishibashi <bashi@chromium.org>
2025-05-30 04:35:51 -0700

    HttpStreamPool: Reset ServiceEndpointRequest asynchronously
    ServiceEndpointRequest has a weird restriction upon destruction.
    Resetting it synchronously may cause a dangling pointer in the
    HostResolver layer (see comment #8 of crbug.com/397597592 for
    details). Reset ServiceEndpointRequest asynchronously to work around
    the issue.
    
    Bug: 421299722
    Change-Id: I5021f5fbb9d92a89a799c38237b73913ffb5f9c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6605360
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467410}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -319,6 +319,10 @@ void HttpStreamPool::AttemptManager::Preconnect(Job* job) {
 }
 
 void HttpStreamPool::AttemptManager::OnServiceEndpointsUpdated() {
+  if (is_shutting_down()) {
+    return;
+  }
+
   TRACE_EVENT_INSTANT("net.stream", "AttemptManager::OnServiceEndpointsUpdated",
                       track_);
   net_log().AddEvent(
@@ -332,6 +336,10 @@ void HttpStreamPool::AttemptManager::OnServiceEndpointsUpdated() {
 }
 
 void HttpStreamPool::AttemptManager::OnServiceEndpointRequestFinished(int rv) {
+  if (is_shutting_down()) {
+    return;
+  }
+
   TRACE_EVENT_INSTANT("net.stream",
                       "AttemptManager::OnServiceEndpointRequestFinished",
                       track_, "result", rv);
@@ -1044,6 +1052,18 @@ void HttpStreamPool::AttemptManager::ResolveServiceEndpoint(
   }
 }
 
+void HttpStreamPool::AttemptManager::ResetServiceEndpointRequestLater() {
+  CHECK(is_shutting_down());
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, base::BindOnce(&AttemptManager::ResetServiceEndpointRequest,
+                                weak_ptr_factory_.GetWeakPtr()));
+}
+
+void HttpStreamPool::AttemptManager::ResetServiceEndpointRequest() {
+  CHECK(is_shutting_down());
+  service_endpoint_request_.reset();
+}
+
 void HttpStreamPool::AttemptManager::RestrictAllowedProtocols(
     NextProtoSet allowed_alpns) {
   allowed_alpns_ = base::Intersection(allowed_alpns_, allowed_alpns);
@@ -1492,7 +1512,7 @@ void HttpStreamPool::AttemptManager::HandleFinalError(int error) {
   CHECK(!final_error_to_notify_jobs_.has_value());
   final_error_to_notify_jobs_ = error;
   availability_state_ = AvailabilityState::kFailing;
-  service_endpoint_request_.reset();
+  ResetServiceEndpointRequestLater();
 
   net_log_.AddEvent(
       NetLogEventType::HTTP_STREAM_POOL_ATTEMPT_MANAGER_NOTIFY_FAILURE, [&] {
@@ -1657,7 +1677,7 @@ void HttpStreamPool::AttemptManager::MaybeStartDraining() {
   }
 
   availability_state_ = AvailabilityState::kDraining;
-  service_endpoint_request_.reset();
+  ResetServiceEndpointRequestLater();
 
   // Cancel in-flight TCP based attempts so that draining AttemptManager won't
   // have active connecting streams.
