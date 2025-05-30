2312b3b481423b4ff4a54eacdaa3e7e51a8292bf
Sam Maier <smaier@chromium.org>
2025-05-30 11:00:39 -0700

    Revert "Partially revert SkipIsolatedSplitPreload experiment to determine issues"
    This reverts commit c7c258706dcdb1f8806c8339656e7a57d98e2e40.
    
    Reason for revert: Population seems back to normal now, want to try
    again
    
    Bug: 402155170
    Original change's description:
    > Partially revert SkipIsolatedSplitPreload experiment to determine issues
    >
    > We are seeing large population differences between these experiments.
    > We are now not doing anything in this experiment except for checking the
    > CachedFlag as the same time, to see if Finch is actually assigning our
    > groups the same amount of population. If this restores group sizes to
    > be the same on Control and Enabled, then the issue is in the
    > implementation of the experiment.
    >
    > Bug: 402155170
    > Change-Id: I94b8a1aec37c9af09dddbe0e89bb3acb953d28d9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543287
    > Auto-Submit: Sam Maier <smaier@chromium.org>
    > Commit-Queue: Sam Maier <smaier@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1459648}
    
    Bug: 402155170
    Change-Id: I371acf3c1937f8413c4e536c38206d45bc719bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606861
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467567}

--- chrome/android/java/src/org/chromium/chrome/browser/base/SplitCompatApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/base/SplitCompatApplication.java
@@ -202,8 +202,7 @@ public class SplitCompatApplication extends Application {
 
         maybeInitProcessType();
 
-        if (isBrowserProcess) {
-            ChromeFeatureList.sSkipIsolatedSplitPreload.isEnabled();
+        if (isBrowserProcess && !ChromeFeatureList.sSkipIsolatedSplitPreload.isEnabled()) {
             performBrowserProcessPreloading(context);
         }
 
@@ -297,8 +296,11 @@ public class SplitCompatApplication extends Application {
     }
 
     private void maybeInitChromeSplitAndPreloadNativeLibrary() {
-        if (isBrowserProcess()) {
-            ChromeFeatureList.sSkipIsolatedSplitPreload.isEnabled();
+        if (isBrowserProcess()
+                && ChromeFeatureList.sSkipIsolatedSplitPreload.isEnabled()
+                && !BuildConfig.IS_FOR_TEST) {
+            new Thread(() -> LibraryLoader.getInstance().loadNow()).start();
+            performBrowserProcessPreloading(this, true);
         }
     }
 
