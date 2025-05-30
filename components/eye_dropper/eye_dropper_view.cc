20f17e8b1f3c42d2ef334e2ac3ada0d37d5beb8a
David Sanders <dsanders11@ucsbalum.com>
2025-05-30 13:35:14 -0700

    Remove unused includes in render_widget_host.h
    Adds missing includes as needed to fix the build.
    
    Bug: 40318405
    Change-Id: I7fc23b0688ccd55c0f7aed02f50a75ecf49a5a5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6605640
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467651}

--- components/eye_dropper/eye_dropper_view.cc
+++ components/eye_dropper/eye_dropper_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "components/color/color_id.h"
 #include "components/eye_dropper/features.h"
