56cce573f1eceeec4e8dcaa08df2010bbae604e5
Alex Kallam <aakallam@chromium.org>
2025-05-30 12:58:36 -0700

    [MDL] Enable MaskedDomainListFlatbufferImpl by default
    Flatbuffers are now used for the default MDL implementation
    and the old implementation will be removed next milestone.
    
    Bug: 419829517
    Change-Id: I6a6a69641682dc74fbc26de15d7e49c6ebbd638e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578822
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Alex Kallam <aakallam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467630}

--- services/network/network_service_unittest.cc
+++ services/network/network_service_unittest.cc
@@ -182,8 +182,9 @@ TEST_F(NetworkServiceTest, CreateContextWithoutChannelID) {
 
 TEST_F(NetworkServiceTest, CreateContextWithMaskedDomainListProxyConfig) {
   base::test::ScopedFeatureList scoped_feature_list_;
-  scoped_feature_list_.InitAndEnableFeature(
-      net::features::kEnableIpProtectionProxy);
+  scoped_feature_list_.InitWithFeatures(
+      {net::features::kEnableIpProtectionProxy},
+      {network::features::kMaskedDomainListFlatbufferImpl});
 
   masked_domain_list::MaskedDomainList mdl;
   auto* resourceOwner = mdl.add_resource_owners();
@@ -209,8 +210,9 @@ TEST_F(NetworkServiceTest, CreateContextWithMaskedDomainListProxyConfig) {
 TEST_F(NetworkServiceTest,
        CreateContextWithCustomProxyConfig_MdlConfigIsNotUsed) {
   base::test::ScopedFeatureList scoped_feature_list_;
-  scoped_feature_list_.InitAndEnableFeature(
-      net::features::kEnableIpProtectionProxy);
+  scoped_feature_list_.InitWithFeatures(
+      {net::features::kEnableIpProtectionProxy},
+      {network::features::kMaskedDomainListFlatbufferImpl});
 
   masked_domain_list::MaskedDomainList mdl;
   auto* resourceOwner = mdl.add_resource_owners();
@@ -1114,7 +1116,7 @@ TEST_F(NetworkServiceTest, SetMaskedDomainList) {
   scoped_feature_list_.InitWithFeatures(
       {net::features::kEnableIpProtectionProxy,
        network::features::kMaskedDomainList},
-      {});
+      {network::features::kMaskedDomainListFlatbufferImpl});
 
   masked_domain_list::MaskedDomainList mdl;
   auto* resourceOwner = mdl.add_resource_owners();
