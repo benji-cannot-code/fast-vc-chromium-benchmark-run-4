bb8f958a0ba784d7b9f0993946034e553a996de6
Ananya <ananyaseelam@google.com>
2025-05-30 08:56:20 -0700

    [search aggregator] Add suggestion type slices to latency metric
    This CL adds suggestion type slices to the response time latency metric
    "Omnibox.SuggestRequestsSent.ResponseTime2.RequestState.
    EnterpriseSearchAggregatorSuggest" which logs latencies sliced by
    whether they were interrupted or completed. Prior to this CL, latencies
    were only logged once all requests were completed. Now, if the omnibox
    config feature param `multiple_requests` is true, we log latencies after
    each sub-request is completed.
    
    The new metrics logged for a given `SuggestionType` (People, Content,
    Query) are:
    - ...EnterpriseSearchAggregatorSuggest.<SuggestionType>.Interrupted
    - ...EnterpriseSearchAggregatorSuggest.<SuggestionType>.Completed
    - ...EnterpriseSearchAggregatorSuggest.<SuggestionType>
    
    NO_IFTTT=false positive on introduction of LINT.IfChange
    
    Bug: 415786421
    Change-Id: Id6849ca31c80a0320966b176fc61c95d581a2535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542926
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467501}

--- components/omnibox/browser/enterprise_search_aggregator_suggestions_service_unittest.cc
+++ components/omnibox/browser/enterprise_search_aggregator_suggestions_service_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
-
 variations::VariationID kVariationID = 123;
 
 const std::string& mock_response = base::StringPrintf({
