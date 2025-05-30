3d57a26c05c5ce528b2993f03aaf4a5a04774d0d
David Sanders <dsanders11@ucsbalum.com>
2025-05-30 14:04:48 -0700

    Remove unused includes in css_parser_context.h
    Bug: 40318405
    Change-Id: I2bc4646ac396b34add825b7640e1bcf6a12fb991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598264
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1467664}

--- third_party/blink/renderer/core/css/parser/css_parser_context.h
+++ third_party/blink/renderer/core/css/parser/css_parser_context.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_PARSER_CSS_PARSER_CONTEXT_H_
 
 #include "base/auto_reset.h"
-#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/css_resource_fetch_restriction.h"
@@ -15,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_feature_forward.h"
 #include "third_party/blink/renderer/platform/bindings/dom_wrapper_world.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/loader/fetch/resource_loader_options.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/weborigin/referrer.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_encoding.h"
