3c7465542a9c179eb53386cd04543a74ab2b3669
rbpotter <rbpotter@chromium.org>
2025-05-29 17:25:26 -0700

    [PDF Ink Signatures] Commit box on document escape or margin click
    Per UX feedback, commit textboxes for a few additional situations:
    - Escape key outside the textbox element (anywhere on the document,
      other than the <textarea>)
    - Click in the plugin area outside any pages or scrollbars.
    
    Bug: 402546155
    Change-Id: Ieb3b2335a3efb4590a0b632e59b2d39c3d25c3db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604836
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467260}

--- chrome/browser/resources/pdf/pdf_viewer.ts
+++ chrome/browser/resources/pdf/pdf_viewer.ts
@@ -469,7 +469,7 @@ export class PdfViewerElement extends PdfViewerBaseElement {
       case 'Enter':
         if ((e as ExtendedKeyEvent).fromPlugin &&
             this.isInTextAnnotationMode_()) {
-          Ink2Manager.getInstance().initializeTextAnnotation();
+          this.maybeCreateTextAnnotation_();
         }
         // </if>
     }
@@ -579,6 +579,16 @@ export class PdfViewerElement extends PdfViewerBaseElement {
   // </if>
 
   // <if expr="enable_pdf_ink2">
+  private maybeCreateTextAnnotation_(location?: Point) {
+    const created =
+        Ink2Manager.getInstance().initializeTextAnnotation(location);
+    if (!created && this.textboxState_ !== TextBoxState.INACTIVE) {
+      const textbox = this.shadowRoot.querySelector('ink-text-box');
+      assert(textbox);
+      textbox.commitTextAnnotation();
+    }
+  }
+
   private recordEnterExitAnnotationModeMetrics_(
       newAnnotationMode: AnnotationMode) {
     // Record exit metrics if annotation mode is being changed from one of
@@ -1091,7 +1101,14 @@ export class PdfViewerElement extends PdfViewerBaseElement {
           return;
         }
         const location = data as unknown as Point;
-        Ink2Manager.getInstance().initializeTextAnnotation(location);
+        // Clicks on a scrollbar should allow the plugin to take focus.
+        if (this.viewport.isPointOnScrollbar(location)) {
+          const textbox = this.shadowRoot.querySelector('ink-text-box');
+          assert(textbox);
+          textbox.blur();
+        } else {
+          this.maybeCreateTextAnnotation_(data as unknown as Point);
+        }
         return;
         // </if>
     }
