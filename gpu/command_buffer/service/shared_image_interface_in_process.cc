b4d12c86e1da77554b5c1361c3efbf4e4728b54d
Maggie Chen <magchen@chromium.org>
2025-05-30 14:59:25 -0700

    [Memory Tracking] Replace GpuCommandBufferMemoryTracker with MemoryTracker
    Delete gpu_command_buffer_memory_tracker.cc and
    gpu_command_buffer_memory_tracker.h.
    
    Set GpuPeakMemoryAllocationSource to COMMAND_BUFFER in MemoryTracker
    Ctor.
    
    Bug: 407874799, 421219211
    Change-Id: I216e8d58e2b64df1d1c6ca37c99b44ac400ea743
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6608036
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467688}

--- gpu/command_buffer/service/shared_image_interface_in_process.cc
+++ gpu/command_buffer/service/shared_image_interface_in_process.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/sync_token.h"
 #include "gpu/command_buffer/service/command_buffer_task_executor.h"
 #include "gpu/command_buffer/service/display_compositor_memory_and_task_controller_on_gpu.h"
-#include "gpu/command_buffer/service/gpu_command_buffer_memory_tracker.h"
 #include "gpu/command_buffer/service/gr_shader_cache.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_factory.h"
 #include "gpu/command_buffer/service/single_task_sequence.h"
