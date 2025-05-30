5b8ae67832a026aaee28d8dfa54a07c00473a63a
Issack John <issackjohn@microsoft.com>
2025-05-30 09:40:50 -0700

    [net] Recognize *+json MIME types as JSON behind a flag
    WHAT
    - Updates net::MatchesMimeType() to properly validate MIME types when
      wildcard patterns are used with validation enabled
    - Introduces Blink runtime flag SpecCompliantJsonMimeTypes which controls which pattern is used in IsJSONMimeType.
    
    WHY
    - Per WHATWG mimesniff §4.6, a JSON MIME type is one whose: essence is "application/json" or "text/json", OR any MIME type whose subtype ends in "+json"
    - Previous implementation missed legitimate JSON types like
    "text/html+json" or "image/svg+json".
    
    Bug: 399689607,362282752
    Change-Id: I25489288b352b84d0a1f8d4cc01dd74d850cc3ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6525687
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Issack John <issackjohn@microsoft.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467525}

--- third_party/blink/common/mime_util/mime_util_unittest.cc
+++ third_party/blink/common/mime_util/mime_util_unittest.cc
@@ -5,13 +5,61 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/mime_util/mime_util.h"
 
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "media/media_buildflags.h"
 #include "net/base/mime_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/buildflags.h"
+#include "third_party/blink/public/common/features.h"
 
 namespace blink {
+class JsonMimeTypeParamTest : public testing::TestWithParam<bool> {
+ protected:
+  void SetUp() override {
+    scoped_feature_list_.InitWithFeatureState(
+        blink::features::kSpecCompliantJsonMimeTypes, GetParam());
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+TEST_P(JsonMimeTypeParamTest, IsJSON) {
+  EXPECT_TRUE(IsJSONMimeType("application/json"));
+  EXPECT_TRUE(IsJSONMimeType("text/json"));
+  EXPECT_TRUE(IsJSONMimeType("application/blah+json"));
+  EXPECT_TRUE(IsJSONMimeType("Application/JSON"));
+  EXPECT_TRUE(IsJSONMimeType("Text/JSON"));
+  EXPECT_TRUE(IsJSONMimeType("application/json;x=1"));
+  EXPECT_TRUE(IsJSONMimeType("application/blah+json;x=1"));
+  EXPECT_TRUE(IsJSONMimeType("text/json;x=1"));
+
+  EXPECT_FALSE(IsJSONMimeType("json"));
+  EXPECT_FALSE(IsJSONMimeType("+json"));
+  EXPECT_FALSE(IsJSONMimeType("application/"));
+  EXPECT_FALSE(IsJSONMimeType("application/jsonabcd"));
+  EXPECT_FALSE(IsJSONMimeType("application/blahjson"));
+  EXPECT_FALSE(IsJSONMimeType("application/blah+jsonabcd"));
+  EXPECT_FALSE(IsJSONMimeType("application/foo+json bar"));
+  EXPECT_FALSE(IsJSONMimeType("application/foo+jsonbar;a=b"));
+  EXPECT_FALSE(IsJSONMimeType("application/json+blah"));
+  EXPECT_FALSE(IsJSONMimeType("application/problem+"));
+  EXPECT_FALSE(IsJSONMimeType("application/+"));
+  EXPECT_FALSE(IsJSONMimeType("text/html;+json"));
+  EXPECT_FALSE(IsJSONMimeType("text/html+json+xml"));
+  EXPECT_FALSE(IsJSONMimeType("text/json/json"));
+
+  if (GetParam()) {
+    EXPECT_TRUE(IsJSONMimeType("text/blah+json;x=1"));
+    EXPECT_TRUE(IsJSONMimeType("text/html+json"));
+    EXPECT_TRUE(IsJSONMimeType("image/svg+json"));
+  } else {
+    EXPECT_FALSE(IsJSONMimeType("text/blah+json;x=1"));
+    EXPECT_FALSE(IsJSONMimeType("text/html+json"));
+    EXPECT_FALSE(IsJSONMimeType("image/svg+json"));
+  }
+}
 
 TEST(MimeUtilTest, LookupTypes) {
   EXPECT_FALSE(IsUnsupportedTextMimeType("text/banana"));
@@ -56,26 +104,8 @@ TEST(MimeUtilTest, LookupTypes) {
   EXPECT_FALSE(IsSupportedMimeType("Application/X-JSON"));
   EXPECT_FALSE(IsSupportedNonImageMimeType("application/vnd.doc;x=y+json"));
   EXPECT_FALSE(IsSupportedNonImageMimeType("Application/VND.DOC;X=Y+JSON"));
-
-  EXPECT_TRUE(IsJSONMimeType("application/json"));
-  EXPECT_TRUE(IsJSONMimeType("text/json"));
-  EXPECT_TRUE(IsJSONMimeType("application/blah+json"));
-  EXPECT_TRUE(IsJSONMimeType("Application/JSON"));
-  EXPECT_TRUE(IsJSONMimeType("Text/JSON"));
-  EXPECT_TRUE(IsJSONMimeType("application/json;x=1"));
-  EXPECT_TRUE(IsJSONMimeType("application/blah+json;x=1"));
-  EXPECT_TRUE(IsJSONMimeType("text/json;x=1"));
-  EXPECT_FALSE(IsJSONMimeType("text/blah+json;x=1"));
-  EXPECT_FALSE(IsJSONMimeType("json"));
-  EXPECT_FALSE(IsJSONMimeType("+json"));
-  EXPECT_FALSE(IsJSONMimeType("application/"));
-  EXPECT_FALSE(IsJSONMimeType("application/jsonabcd"));
-  EXPECT_FALSE(IsJSONMimeType("application/blahjson"));
-  EXPECT_FALSE(IsJSONMimeType("application/blah+jsonabcd"));
-  EXPECT_FALSE(IsJSONMimeType("application/foo+json bar"));
-  EXPECT_FALSE(IsJSONMimeType("application/foo+jsonbar;a=b"));
-  EXPECT_FALSE(IsJSONMimeType("application/json+blah"));
-  EXPECT_FALSE(IsJSONMimeType("image/svg+json"));
 }
+
+INSTANTIATE_TEST_SUITE_P(All, JsonMimeTypeParamTest, ::testing::Bool());
 
 }  // namespace blink
