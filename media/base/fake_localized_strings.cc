fd9c3949f199371960e92e25f22ae83ff2d78b38
Maya Jelonkiewicz <mjel@google.com>
2025-05-30 04:30:19 -0700

    Move AudioManagerAndroid's JNI calls to delegate class
    This refactors and organizes interactions with the Java-side
    AudioManagerAndroid and allows for mocking JNI calls. Device
    enumeration-related tests are also updated to use such a JNI mock; most
    other tests cannot be updated in this way as they also exercise
    interactions with AAudio.
    
    Test: run_media_unittests --gtest_filter="AudioAndroid*" -v
    Bug: 417671787
    Change-Id: I72f6e8765923346e4cf036250023588b5654653b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553126
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Maya Jelonkiewicz <mjel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467409}

--- media/base/fake_localized_strings.cc
+++ media/base/fake_localized_strings.cc
@@ -9,10 +9,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 std::u16string FakeLocalizedStringProvider(MessageId message_id) {
-  if (message_id == DEFAULT_AUDIO_DEVICE_NAME)
-    return u"Default";
-
-  return u"FakeString";
+  switch (message_id) {
+    case DEFAULT_AUDIO_DEVICE_NAME:
+      return u"Default";
+#if BUILDFLAG(IS_WIN)
+    case COMMUNICATIONS_AUDIO_DEVICE_NAME:
+      return u"Communications";
+#endif
+#if BUILDFLAG(IS_ANDROID)
+    case GENERIC_AUDIO_DEVICE_NAME:
+      return u"Nameless audio device (generic)";
+    case INTERNAL_SPEAKER_AUDIO_DEVICE_NAME:
+      return u"Nameless audio device (internal speaker)";
+    case INTERNAL_MIC_AUDIO_DEVICE_NAME:
+      return u"Nameless audio device (internal mic)";
+    case WIRED_HEADPHONES_AUDIO_DEVICE_NAME:
+      return u"Nameless audio device (wired headphones)";
+    case BLUETOOTH_AUDIO_DEVICE_NAME:
+      return u"Nameless audio device (Bluetooth)";
+    case USB_AUDIO_DEVICE_NAME:
+      return u"Nameless audio device (USB)";
+    case HDMI_AUDIO_DEVICE_NAME:
+      return u"Nameless audio device (HDMI)";
+#endif
+  }
 }
 
 void SetUpFakeLocalizedStrings() {
