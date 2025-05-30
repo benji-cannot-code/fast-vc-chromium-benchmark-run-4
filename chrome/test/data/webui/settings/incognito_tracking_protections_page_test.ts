eeec679f48b9e9d4af33c042d939f9969e387f03
Maggie Jennings <mjenn@google.com>
2025-05-30 09:02:41 -0700

    [ACT] Add user action for IPP and FPP toggles
    Bug: 408036586
    Change-Id: I995665a242ea6983ee0aea4f65b796badb5f7bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6597562
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467506}

--- chrome/test/data/webui/settings/incognito_tracking_protections_page_test.ts
+++ chrome/test/data/webui/settings/incognito_tracking_protections_page_test.ts
@@ -77,6 +77,11 @@ suite('IncognitoTrackingProtectionsPageTest', function() {
             '#fingerprintingProtectionToggle');
     assertTrue(!!fingerprintingProtectionToggle);
     fingerprintingProtectionToggle.click();
+    const actionResult =
+        await testMetricsBrowserProxy.whenCalled('recordAction');
+    assertEquals(
+        actionResult,
+        'Settings.TrackingProtections.FingerprintingProtection.Enabled');
     const result =
         await testMetricsBrowserProxy.whenCalled('recordSettingsPageHistogram');
     assertEquals(PrivacyElementInteractions.FINGERPRINTING_PROTECTION, result);
@@ -124,6 +129,10 @@ suite('IncognitoTrackingProtectionsPageTest', function() {
             '#ipProtectionToggle');
     assertTrue(!!ipProtectionToggle);
     ipProtectionToggle.click();
+    const actionResult =
+        await testMetricsBrowserProxy.whenCalled('recordAction');
+    assertEquals(
+        actionResult, 'Settings.TrackingProtections.IpProtection.Enabled');
     const result =
         await testMetricsBrowserProxy.whenCalled('recordSettingsPageHistogram');
     assertEquals(PrivacyElementInteractions.IP_PROTECTION, result);
