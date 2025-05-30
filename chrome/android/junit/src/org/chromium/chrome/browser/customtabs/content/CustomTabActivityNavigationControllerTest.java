5dd90d2d836ac46e3f6b24e3d05e4c8d06b1e2e2
Mohamed Adel <adelm@google.com>
2025-05-29 20:01:49 -0700

    Add an enumerated histogram to record the finish reason for CCTs
    This change adds an enumerated histogram that is used to record the
    finish reason when a CCT navigation causes it to finish.
    
    Bug: 40285983
    Change-Id: I5795883eb9ee3b99deb613eb1ae3645a3ebce0c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604456
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Moe Adel <adelm@google.com>
    Commit-Queue: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467292}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityNavigationControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityNavigationControllerTest.java
@@ -140,6 +140,9 @@ public class CustomTabActivityNavigationControllerTest {
                                 MinimizeAppAndCloseTabBackPressHandler
                                         .getCustomTabSameTaskHistogramNameForTesting(),
                                 MinimizeAppAndCloseTabType.MINIMIZE_APP_AND_CLOSE_TAB)
+                        .expectIntRecord(
+                                CustomTabActivityNavigationController.HISTOGRAM_FINISH_REASON,
+                                FinishReason.USER_NAVIGATION)
                         .expectNoRecords(
                                 BackPressManager.getCustomTabSeparateTaskHistogramForTesting())
                         .expectNoRecords(BackPressManager.getCustomTabSameTaskHistogramForTesting())
@@ -180,6 +183,9 @@ public class CustomTabActivityNavigationControllerTest {
                                 MinimizeAppAndCloseTabBackPressHandler
                                         .getCustomTabSameTaskHistogramNameForTesting(),
                                 MinimizeAppAndCloseTabType.MINIMIZE_APP_AND_CLOSE_TAB)
+                        .expectIntRecord(
+                                CustomTabActivityNavigationController.HISTOGRAM_FINISH_REASON,
+                                FinishReason.USER_NAVIGATION)
                         .expectNoRecords(
                                 BackPressManager.getCustomTabSeparateTaskHistogramForTesting())
                         .expectNoRecords(BackPressManager.getCustomTabSameTaskHistogramForTesting())
@@ -346,10 +352,18 @@ public class CustomTabActivityNavigationControllerTest {
 
     @Test
     public void whenCallbackInvoked_FinishesWithReasonHandledByOS() {
+        HistogramWatcher histogramWatcher =
+                HistogramWatcher.newBuilder()
+                        .expectIntRecord(
+                                CustomTabActivityNavigationController.HISTOGRAM_FINISH_REASON,
+                                FinishReason.HANDLED_BY_OS)
+                        .expectNoRecords(BackPressManager.getHistogramForTesting())
+                        .build();
         when(mTabController.onlyOneTabRemaining()).thenReturn(true);
         when(mTabController.dispatchBeforeUnloadIfNeeded()).thenReturn(true);
         CustomTabActivityNavigationController.enablePredictiveBackGestureForTesting();
         mNavigationController.onSystemNavigation();
+        histogramWatcher.assertExpected();
 
         verify(mFinishHandler).onFinish(FinishReason.HANDLED_BY_OS, true);
     }
