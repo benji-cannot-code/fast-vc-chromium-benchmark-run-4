d1b816047e9d38fb047fef1289d70ba34d3ae840
Aishwarya Rajesh <aishwaryarj@google.com>
2025-05-31 19:19:24 -0700

    [TSLO] Fix InsetsRectProvider.Consumer callback invocation on startup
    In, https://chromium-review.googlesource.com/c/chromium/src/+/6604745,
    we updated the insets consumption logic in InsetsRectProvider to
    delegate the consumption decision to the Consumer. If the Consumer is
    not set when #maybeUpdateWidestUnoccludedRect() is called during
    startup, we will not accurately consume the insets if needed since
    AppHeaderCoordinator currently simply invokes the callback
    implementation to populate its own initial internal state. If
    consumption condition is different, then in this case the
    InsetsRectProvider would have incorrectly "not consumed" the insets
    because it was invoked before the Consumer is set.
    
    This CL fixes this gap by having AHC re-trigger window insets
    application on startup that will not only set its own state but also
    give the InsetsRectProvider a chance to reconsider inset consumption
    after the consumer is set.
    
    Bug: 418868898
    Change-Id: Ie9e7edf98752a2229291477b6a367f3d8e7ada3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6605999
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1467820}

--- ui/android/java/src/org/chromium/ui/insets/InsetsRectProvider.java
+++ ui/android/java/src/org/chromium/ui/insets/InsetsRectProvider.java
@@ -65,7 +65,7 @@ public class InsetsRectProvider implements WindowInsetsConsumer {
     private @Nullable WindowInsetsCompat mCachedInsets;
     private List<Rect> mBoundingRects;
     private Rect mWidestUnoccludedRect = new Rect();
-    private boolean mUnoccludedRectUpdateConsumed;
+    private @Nullable Boolean mUnoccludedRectUpdateConsumed;
     private boolean mIsUnoccludedRegionComplex;
 
     /**
@@ -86,18 +86,22 @@ public class InsetsRectProvider implements WindowInsetsConsumer {
         mInsetType = insetType;
         mBoundingRects = List.of();
         mInsetObserver = insetObserver;
+        mCachedInsets = initialInsets;
 
         assert VERSION.SDK_INT >= VERSION_CODES.R;
         mInsetObserver.addInsetsConsumer(this, insetConsumerSource);
-        if (initialInsets != null) {
-            maybeUpdateWidestUnoccludedRect(initialInsets);
-        }
     }
 
-    /** Sets the consumer for insets of type |mInsetType|. */
+    /**
+     * Sets the consumer for insets of type |mInsetType|. Expected to be called soon after
+     * instantiation.
+     */
     @Initializer
     public void setConsumer(Consumer consumer) {
         mInsetsRectConsumer = consumer;
+        if (mCachedInsets != null) {
+            maybeUpdateWidestUnoccludedRect(mCachedInsets);
+        }
     }
 
     /** Return the list of bounding rect from the window insets. */
@@ -196,8 +200,11 @@ public class InsetsRectProvider implements WindowInsetsConsumer {
         if (windowSize.getWidth() == 0 && windowSize.getHeight() == 0) return false;
 
         Rect windowRect = new Rect(0, 0, windowSize.getWidth(), windowSize.getHeight());
-        if (windowInsetsCompat.equals(mCachedInsets) && windowRect.equals(mWindowRect)) {
-            // Avoid repeated processing if the insets have already been processed.
+        if (windowInsetsCompat.equals(mCachedInsets)
+                && windowRect.equals(mWindowRect)
+                && mUnoccludedRectUpdateConsumed != null) {
+            // Avoid repeated processing if the insets have already been processed and / or
+            // potentially consumed.
             return mUnoccludedRectUpdateConsumed;
         }
 
@@ -205,9 +212,6 @@ public class InsetsRectProvider implements WindowInsetsConsumer {
         mWindowRect.set(windowRect);
 
         updateWidestUnoccludedRect(windowInsetsCompat);
-
-        // Consumer can be null at the time of instantiation of this class.
-        if (mInsetsRectConsumer == null) return false;
 
         // Notify the consumer about the update.
         mUnoccludedRectUpdateConsumed =
