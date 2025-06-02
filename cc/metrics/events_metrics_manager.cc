a3ecb158d5cd686fb4693ba73a524d85e12f05ac
Jonathan Ross <jonross@chromium.org>
2025-06-02 07:21:15 -0700

    Reland "ZeroScrollMetricsUpdate Implemented in V3 ScrollJank"
    This reverts commit 4ccc33484e76e6126531124f03be902e02679469.
    
    Reason for revert: Relanding with fix
    
    Bug: 404502127
    Original change's description:
    > Revert "ZeroScrollMetricsUpdate Implemented in V3 ScrollJank"
    >
    > This reverts commit 06c27d50801ee322795d5985a9006b3bf0373827.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6142867098566656
    >
    > Sample build with failed test: https://ci.chromium.org/b/8713520342998157217
    > Affected test(s):
    > [ninja://content/test:content_browsertests/ScrollTracingBrowserTest.ScrollingMetricsParity](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fcontent%2Ftest:content_browsertests%2FScrollTracingBrowserTest.ScrollingMetricsParity?q=VHash%3A6e0f9c1d8add64dd)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6142867098566656&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6597364&type=BUG
    >
    > Original change's description:
    > > ZeroScrollMetricsUpdate Implemented in V3 ScrollJank
    > >
    > > We validated via ZeroScrollMetricsUpdate that the zero scroll metrics
    > > have an effect. This change merges when into only the V3 ScrollJank
    > > metrics.
    > >
    > > The original feature is removed. We now always save the EventMetrics.
    > > To differentiate between the V3 metric, which includes zero delta
    > > updates, and the V2, which does not, we now mark if we actually
    > > scrolled on the metric
    > >
    > > Bug: 404502127
    > > Change-Id: If08c045b07d1ebd3f2a4cc9c00006a5cbe1c0954
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6597364
    > > Commit-Queue: Jonathan Ross <jonross@chromium.org>
    > > Reviewed-by: Victor Miura <vmiura@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1467035}
    > >
    >
    > Bug: 404502127
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I9399464dcaa6767f402e6dbe2c72b00238d6ae15
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604737
    > Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Owners-Override: Alex Mitra <alexmitra@chromium.org>
    > Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1467411}
    
    Bug: 404502127
    Change-Id: Ife032d8dbd09dcd65741c66ea30efb545e41618f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6607787
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468092}

--- cc/metrics/events_metrics_manager.cc
+++ cc/metrics/events_metrics_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
+#include "cc/metrics/event_metrics.h"
 
 namespace cc {
 
@@ -91,8 +92,14 @@ void EventsMetricsManager::OnScopedMonitorEnded(
   DCHECK_GT(active_scoped_monitors_.size(), 0u);
   active_scoped_monitors_.pop_back();
 
-  if (metrics)
+  if (metrics) {
+    if (metrics->type() == EventMetrics::EventType::kGestureScrollUpdate) {
+      auto* scroll_update = metrics->AsScrollUpdate();
+      scroll_update->set_did_scroll(did_scroll_);
+    }
     saved_events_.push_back(std::move(metrics));
+  }
+  did_scroll_ = false;
 }
 
 }  // namespace cc
