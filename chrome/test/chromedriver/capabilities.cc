c0a813c6589585bf57177125e3d19b8da26d47f5
Jonathan Lee <jonathanjlee@google.com>
2025-06-02 14:48:50 -0700

    [chromedriver] Coalesce multiple `--js-flags` args
    Merge the values like for `--enable-features` instead of just taking the
    last one (not as useful).
    
    Fixed: 421941798
    Change-Id: I0ba29535853ebb1cd593ab88dc5003021d75f8ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612042
    Reviewed-by: Alex N. Jose <alexnj@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468375}

--- chrome/test/chromedriver/capabilities.cc
+++ chrome/test/chromedriver/capabilities.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/contains.h"
-#include "base/containers/fixed_flat_set.h"
+#include "base/containers/fixed_flat_map.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/json/string_escape.h"
@@ -984,17 +984,18 @@ void Switches::SetSwitch(const std::string& name, const base::FilePath& value) {
 }
 
 void Switches::SetMultivaluedSwitch(const std::string& name,
-                                    const std::string& value) {
+                                    const std::string& value,
+                                    const std::string_view& delimiter) {
 #if BUILDFLAG(IS_WIN)
   auto native_value = base::UTF8ToWide(value);
-  auto delimiter = L',';
+  auto native_delimiter = base::UTF8ToWide(delimiter);
 #else
   const auto& native_value = value;
-  const auto delimiter = ',';
+  const auto& native_delimiter = delimiter;
 #endif
   NativeString& switch_value = switch_map_[name];
-  if (switch_value.size() > 0 && switch_value.back() != delimiter) {
-    switch_value += delimiter;
+  if (switch_value.size() > 0 && !switch_value.ends_with(native_delimiter)) {
+    switch_value += native_delimiter;
   }
   switch_value += native_value;
 }
@@ -1007,9 +1008,16 @@ void Switches::SetFromSwitches(const Switches& switches) {
 }
 
 namespace {
-constexpr auto kMultivaluedSwitches = base::MakeFixedFlatSet<std::string_view>(
-    {"enable-blink-features", "disable-blink-features", "enable-features",
-     "disable-features"});
+
+constexpr auto kMultivaluedSwitches =
+    base::MakeFixedFlatMap<std::string_view, std::string_view>({
+        {"enable-blink-features", ","},
+        {"disable-blink-features", ","},
+        {"enable-features", ","},
+        {"disable-features", ","},
+        {"js-flags", " "},
+    });
+
 }  // namespace
 
 void Switches::SetUnparsedSwitch(const std::string& unparsed_switch) {
@@ -1024,10 +1032,12 @@ void Switches::SetUnparsedSwitch(const std::string& unparsed_switch) {
     start_index = 2;
   name = unparsed_switch.substr(start_index, equals_index - start_index);
 
-  if (kMultivaluedSwitches.contains(name))
-    SetMultivaluedSwitch(name, value);
-  else
+  const auto iter = kMultivaluedSwitches.find(name);
+  if (iter != kMultivaluedSwitches.end()) {
+    SetMultivaluedSwitch(name, value, iter->second);
+  } else {
     SetSwitch(name, value);
+  }
 }
 
 void Switches::RemoveSwitch(const std::string& name) {
