fd2e0906e41244af579bfeaede97fd44afa65f93
Paul Adedeji <pauladedeji@google.com>
2025-06-02 11:18:26 -0700

    [ntp footer] Record clicks on extension names.
    Change-Id: I080a145b73726482c6c4623d5b0de19b357c2ac9
    Bug: 420953568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6611479
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468251}

--- chrome/browser/resources/new_tab_footer/app.ts
+++ chrome/browser/resources/new_tab_footer/app.ts
@@ -47,6 +47,7 @@ export enum FooterCustomizeChromeEntryPoint {
 export enum FooterElement {
   OTHER = 0,
   CUSTOMIZE_BUTTON = 1,
+  EXTENSION_NAME = 2,
   MAX_VALUE = CUSTOMIZE_BUTTON,
 }
 
@@ -156,6 +157,7 @@ export class NewTabFooterAppElement extends CrLitElement {
 
   protected onExtensionNameClick_(e: Event) {
     e.preventDefault();
+    recordClick(FooterElement.EXTENSION_NAME);
     this.handler_.openExtensionOptionsPageWithFallback();
   }
 
--- chrome/test/data/webui/new_tab_footer/app_test.ts
+++ chrome/test/data/webui/new_tab_footer/app_test.ts
@@ -118,6 +118,11 @@ suite('NewTabFooterAppTest', () => {
       // Assert.
       assertEquals(
           1, handler.getCallCount('openExtensionOptionsPageWithFallback'));
+      assertEquals(1, metrics.count('NewTabPage.Footer.Click'));
+      assertEquals(
+          1,
+          metrics.count(
+              'NewTabPage.Footer.Click', FooterElement.EXTENSION_NAME));
     });
   });
 
