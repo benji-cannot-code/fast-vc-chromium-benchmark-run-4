858db0adf23a2caee5a0b00ffc6886a2025e2e05
Ananya <ananyaseelam@google.com>
2025-06-02 08:55:37 -0700

    [ntp-composebox] Add composebox entrypoint
    This CL adds a cr-button as an entrypoint to the composebox. This button
    currently does not do anything except emit an event. It is a basic
    button that will be styled in a future CL. It is also gated behind the
    `kNtpSearchboxShowComposeButton` flag.
    
    Before: https://screenshot.googleplex.com/AXX9EvgKc7zBqyK
    After: https://screenshot.googleplex.com/4x6p3TmrMxnjawF
    
    Bug: 421165591
    Change-Id: If8862af71ce43bb9e9768c6277abad4fd34d0972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612178
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468142}

--- ui/webui/resources/cr_components/searchbox/searchbox.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.ts
@@ -142,6 +142,12 @@ export class SearchboxElement extends SearchboxElementBase {
         reflectToAttribute: true,
       },
 
+      composeButtonEnabled: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('searchboxShowComposeButton'),
+        reflectToAttribute: true,
+      },
+
       //========================================================================
       // Private properties
       //========================================================================
@@ -286,6 +292,7 @@ export class SearchboxElement extends SearchboxElementBase {
   declare searchboxLensSearchEnabled: boolean;
   declare searchboxChromeRefreshTheming: boolean;
   declare searchboxSteadyStateShadow: boolean;
+  declare composeButtonEnabled: boolean;
   declare showThumbnail: boolean;
   declare private inputAriaLive_: string;
   declare private isLensSearchbox_: boolean;
@@ -814,6 +821,10 @@ export class SearchboxElement extends SearchboxElementBase {
   private onLensSearchClick_() {
     this.dropdownIsVisible = false;
     this.dispatchEvent(new Event('open-lens-search'));
+  }
+
+  private onComposeButtonClick_() {
+    this.dispatchEvent(new CustomEvent('open-compose-box'));
   }
 
   private onRemoveThumbnailClick_() {
