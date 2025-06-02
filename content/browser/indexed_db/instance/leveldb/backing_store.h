a6e488d98d3484b0016ebd4b58b8c35bc925c807
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2025-06-01 23:13:28 -0700

    IDB: Move Cursor::IteratorState to the leveldb backing store
    Seems to be a leveldb-specific enum that is not relevant to callers.
    
    Bug: 419208481
    Change-Id: I2d56f41579c19e803abd4476d91a3f24b444cd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606918
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1467962}

--- content/browser/indexed_db/instance/leveldb/backing_store.h
+++ content/browser/indexed_db/instance/leveldb/backing_store.h
@@ -341,6 +341,8 @@ class CONTENT_EXPORT BackingStore : public indexed_db::BackingStore,
   // avoid needless git churn.
   class Cursor : public indexed_db::BackingStore::Cursor {
    public:
+    enum IteratorState { READY = 0, SEEK };
+
     struct CursorOptions {
       CursorOptions();
       CursorOptions(const CursorOptions& other);
@@ -370,10 +372,13 @@ class CONTENT_EXPORT BackingStore : public indexed_db::BackingStore,
     blink::IndexedDBKey TakeKey() && override;
     bool Continue(const blink::IndexedDBKey& key,
                   const blink::IndexedDBKey& primary_key,
-                  IteratorState state,
                   Status*) override;
     bool Advance(uint32_t count, Status*) override;
 
+    bool Continue(const blink::IndexedDBKey& key,
+                  const blink::IndexedDBKey& primary_key,
+                  IteratorState state,
+                  Status*);
     bool FirstSeek(Status*);
 
    protected:
