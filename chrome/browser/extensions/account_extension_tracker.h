48d4b26930cdb9836bb85c7d068fe1b413ecee78
Keigo Oka <oka@google.com>
2025-06-02 10:44:27 -0700

    extensions: Add static assertion to files included in desktop android
    Bug: 419460299
    Change-Id: Ibca617796a3c050b22553c7d20b29b33bd5f1f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606919
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468223}

--- chrome/browser/extensions/account_extension_tracker.h
+++ chrome/browser/extensions/account_extension_tracker.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_id.h"
 
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
 class BrowserContextKeyedServiceFactory;
 class Profile;
 
