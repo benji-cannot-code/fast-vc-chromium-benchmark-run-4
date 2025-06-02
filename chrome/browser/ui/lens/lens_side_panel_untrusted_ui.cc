cb4ceec0d4330d86656730c9fb21107efafcd7d5
Duncan Mercer <mercerd@google.com>
2025-06-02 12:17:39 -0700

    [lensoverlay] Fix thumbnail sizing not working
    The Searchbox handler also sets this flag, and since its created below
    this, the handler flag overrides this one to always be false. This Cl
    moves this flag lower to fix this issue.
    
    Change-Id: I1f9f9213089bc519b40f6f76adc246e731d591ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6603987
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468286}

--- chrome/browser/ui/lens/lens_side_panel_untrusted_ui.cc
+++ chrome/browser/ui/lens/lens_side_panel_untrusted_ui.cc
@@ -113,9 +113,6 @@ LensSidePanelUntrustedUI::LensSidePanelUntrustedUI(content::WebUI* web_ui)
       "enableCloseButtonTweaks",
       lens::features::GetVisualSelectionUpdatesEnableCloseButtonTweaks());
   html_source->AddBoolean(
-      "enableThumbnailSizingTweaks",
-      lens::features::GetVisualSelectionUpdatesEnableThumbnailSizingTweaks());
-  html_source->AddBoolean(
       "enableSummarizeSuggestionHint",
       lens::features::ShouldEnableSummarizeHintForContextualSuggest());
 
@@ -164,6 +161,9 @@ LensSidePanelUntrustedUI::LensSidePanelUntrustedUI(content::WebUI* web_ui)
       "forceHideEllipsis",
       lens::features::GetVisualSelectionUpdatesHideCsbEllipsis());
   html_source->AddBoolean("queryAutocompleteOnEmptyInput", true);
+  html_source->AddBoolean(
+    "enableThumbnailSizingTweaks",
+    lens::features::GetVisualSelectionUpdatesEnableThumbnailSizingTweaks());
 }
 
 void LensSidePanelUntrustedUI::BindInterface(
