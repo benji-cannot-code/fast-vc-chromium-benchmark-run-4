cb97133ecbb785e038acca313e49b1b3fed8579f
Adenilson Cavalcanti <cavalcantii@chromium.org>
2025-06-02 10:59:56 -0700

    [zlib] Implement deflate wide writes
    All the way back to 2017, we implemented wide reads/writes in *inflate*
    on 64-bit Arm and x86 CPUs to speed up data decompression.
    
    It turns out that the same idea can be exploited to speed up deflate
    encoding on 64-bit CPUs.
    
    This patch changes put_short & send_bits to write more data with less
    instructions while performing compression by widening the bitstream
    buffer from 16 to 64 bits. On x86-64 it uses a single 'MOV WORD' instead
    of a pair of 'MOV BYTE' (similarly on aarch64: a single 'strh' instead
    of a 'str' plus 'strb' pair).
    
    The optimization is little endian specific, so the use of naming of the
    macro (i.e. DEFLATE_CHUNK_WRITE_64LE). In theory it could be ported to
    32bit, but we no longer add new optimizations to the 32bit code path,
    only try to not break it.
    
    The improvement will be more evident on lower compression levels (1 to
    5) than on higher levels, as then more cycles are spent on searching
    matches than writing data.
    
    For Xeon 6th gen (Granite Rapids: GNR) the average compression speed
    improvement ranges from +4.3% (default) to +8.5% (level 1). For Zen 5
    the numbers are similar and so is for aarch64. For RISC-V it was a
    lesser gain (+2.8% default compression up to +4.5% for level 1).
    
    Original patch by Noel Gordon.
    
    Bug: 419596999
    Change-Id: Idb9a6f39c1ef4cc6f22bfb6fbaeec4d87ea17dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580406
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468234}

--- third_party/zlib/deflate.h
+++ third_party/zlib/deflate.h
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEFLATE_H
 #define DEFLATE_H
 
+#if defined(DEFLATE_CHUNK_WRITE_64LE)
+#include <stdint.h>
+#endif
+
 #include "zutil.h"
 
 /* define NO_GZIP when compiling if you want to disable gzip header and
@@ -53,7 +57,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MAX_BITS 15
 /* All codes must not exceed MAX_BITS bits */
 
+#if defined(DEFLATE_CHUNK_WRITE_64LE)
+#define Buf_size 64
+#else
 #define Buf_size 16
+#endif
 /* size of bit buffer in bi_buf */
 
 #define INIT_STATE    42    /* zlib header -> BUSY_STATE */
@@ -262,7 +270,11 @@ typedef struct internal_state {
     ulg bits_sent;      /* bit length of compressed data sent mod 2^32 */
 #endif
 
+#if defined(DEFLATE_CHUNK_WRITE_64LE)
+    uint64_t bi_buf;
+#else
     ush bi_buf;
+#endif
     /* Output buffer. bits are inserted starting at the bottom (least
      * significant bits).
      */
