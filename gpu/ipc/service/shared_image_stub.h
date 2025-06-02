2836d881a28b2f8f376a411194f8b03f4353eaaa
Maggie Chen <magchen@chromium.org>
2025-06-02 13:15:55 -0700

    [Memory Tracking] Include MemoryTracker within class SharedImageStub
    Remove MemoryTracker inheritance. Add MemoryTracker as a class member.
    
    Bug: 407874799, 421219211
    Change-Id: I839180ef9957032ffd37fdb28223a5a798e7610e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6608136
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468307}

--- gpu/ipc/service/shared_image_stub.h
+++ gpu/ipc/service/shared_image_stub.h
@@ -37,9 +37,9 @@ class GpuChannel;
 class GpuChannelSharedImageInterface;
 class SharedImageFactory;
 
-class GPU_IPC_SERVICE_EXPORT SharedImageStub : public MemoryTracker {
+class GPU_IPC_SERVICE_EXPORT SharedImageStub {
  public:
-  ~SharedImageStub() override;
+  ~SharedImageStub();
 
   using SharedImageDestructionCallback =
       base::OnceCallback<void(const gpu::SyncToken&)>;
@@ -56,12 +56,8 @@ class GPU_IPC_SERVICE_EXPORT SharedImageStub : public MemoryTracker {
                                     gfx::Size& size,
                                     gfx::BufferUsage& buffer_usage);
 
-  // MemoryTracker implementation:
-  void TrackMemoryAllocatedChange(int64_t delta) override;
-  uint64_t GetSize() const override;
-  uint64_t ClientTracingId() const override;
-  int ClientId() const override;
-  uint64_t ContextGroupTracingId() const override;
+  // Get memory size from MemoryTracker.
+  uint64_t GetSize() const;
 
   SequenceId sequence() const { return sequence_; }
   SharedImageFactory* factory() const { return factory_.get(); }
@@ -106,6 +102,8 @@ class GPU_IPC_SERVICE_EXPORT SharedImageStub : public MemoryTracker {
   void SetGpuExtraInfo(const gfx::GpuExtraInfo& gpu_extra_info);
 
   bool MakeContextCurrent(bool needs_gl = false);
+
+  MemoryTracker* memory_tracker() { return memory_tracker_.get(); }
 
  private:
   SharedImageStub(GpuChannel* channel, int32_t route_id);
@@ -158,7 +156,8 @@ class GPU_IPC_SERVICE_EXPORT SharedImageStub : public MemoryTracker {
   const SequenceId sequence_;
   scoped_refptr<SharedContextState> context_state_;
   std::unique_ptr<SharedImageFactory> factory_;
-  uint64_t size_ = 0;
+
+  std::unique_ptr<MemoryTracker> memory_tracker_;
 
   // Holds shared memory used in initial data uploads.
   base::ReadOnlySharedMemoryRegion upload_memory_;
