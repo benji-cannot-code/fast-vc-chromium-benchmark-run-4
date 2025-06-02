7966b5e5521a0882d3c8702509e26c28293010e6
Michael Wilson <mjwilson@chromium.org>
2025-06-02 09:00:04 -0700

    Don't expose timeline_ in AudioParamHandler
    AudioParamTimeline is an internal implementation detail of
    AudioParamHandler, so make passthrough APIs and remove the API that
    allows getting a direct reference to the timeline object.
    
    Change-Id: Ieafb542eb41aa1098b76ef2211344ca27caec486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606864
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468149}

--- third_party/blink/renderer/modules/webaudio/audio_param.h
+++ third_party/blink/renderer/modules/webaudio/audio_param.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_typed_array.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_param_handler.h"
-#include "third_party/blink/renderer/modules/webaudio/audio_param_timeline.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_summing_junction.h"
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
 #include "third_party/blink/renderer/modules/webaudio/inspector_helper_mixin.h"
