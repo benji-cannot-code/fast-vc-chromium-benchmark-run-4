e8a475b25ddfeca7ba2aa7043718edbca504b9d3
Keren Zhu <kerenzhu@chromium.org>
2025-06-02 10:47:44 -0700

    mac: fix missing mouse up and down event in fullscreen top-chrome
    Mouse down and up events were routed to NSWindow's contentView after
    https://crrev.com/c/6574464. This breaks fullscreen top-chrome area
    because the contentView there is an AppKit-managed view, not the NSView
    that hosts views::RootView.
    
    Bug: 421469621
    Change-Id: I002bd87324d1a416cc1e9f0b1307b41963f4e638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6605819
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468227}

--- ui/views/cocoa/native_widget_mac_ns_window_host.mm
+++ ui/views/cocoa/native_widget_mac_ns_window_host.mm
@@ -1258,7 +1258,7 @@ bool NativeWidgetMacNSWindowHost::GetHitTestResult(
     return true;
   }
 
-  *hit_test_result = remote_cocoa::mojom::HitTestResult::kContentView;
+  *hit_test_result = remote_cocoa::mojom::HitTestResult::kRootView;
   return true;
 }
 
