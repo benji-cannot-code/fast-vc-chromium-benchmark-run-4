96f61b9562c139a9f4b71e132e0702478017b2cb
Kenichi Ishibashi <bashi@chromium.org>
2025-06-01 23:40:47 -0700

    HttpStreamPool: Record netlog/trace events for slow attempts
    These would be useful to investigate regression like issue 414173943.
    
    Also update TcpBasedAttempt to use its own method for slow timer.
    
    Bug: 385170278, 414173943
    Change-Id: I7d78dab9afc862dacae8e98da5da2121c2ce88ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6607949
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467966}

--- net/http/http_stream_pool_tcp_based_attempt.h
+++ net/http/http_stream_pool_tcp_based_attempt.h
@@ -49,7 +49,6 @@ class HttpStreamPool::TcpBasedAttempt : public TlsStreamAttempt::Delegate {
   const IPEndPoint& ip_endpoint() const { return attempt_->ip_endpoint(); }
 
   bool is_slow() const { return is_slow_; }
-  void set_is_slow(bool is_slow) { is_slow_ = is_slow; }
 
   // Set to true when the attempt is aborted. When true, the attempt will fail
   // but not be considered as an actual failure.
@@ -72,6 +71,7 @@ class HttpStreamPool::TcpBasedAttempt : public TlsStreamAttempt::Delegate {
   base::Value::Dict GetInfoAsValue() const;
 
  private:
+  void OnAttemptSlow();
   void OnAttemptComplete(int rv);
 
   const raw_ptr<AttemptManager> manager_;
