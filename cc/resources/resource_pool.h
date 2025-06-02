95bfebeae8e48de416461cdc2c1a1399bbd0d6e8
Colin Blundell <blundell@chromium.org>
2025-06-02 00:28:08 -0700

    [//cc] Remove gpu_memory_buffer.h inclusion
    No longer needed.
    
    Bug: 404905709
    Change-Id: Ide16fb851f0b83fc87c2b6152b8d105129b16557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6607728
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467973}

--- cc/resources/resource_pool.h
+++ cc/resources/resource_pool.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/sync_token.h"
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/gpu_memory_buffer.h"
 
 namespace base {
 class SingleThreadTaskRunner;
