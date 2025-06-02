4705c03b55223ef7b33d36eabec600c9ac7de513
Johannes Henkel <johannes@chromium.org>
2025-06-02 09:25:25 -0700

    (Partially) fix paint-timing IDL harness test.
    This test currently fails with an error regarding DomHighResTimeStamp
    missing, and that's because the test, idlharness.window.js, doesn't
    declare a dependency on the 'hr-time'. Fixing that in this cl.
    
    However, this doesn't actually make the test pass, because the spec for
    the performance timeline is broken: 1) It requires that there be an id
    attribute on every performance entry, of type uint64. This is
    impossible, because LargestContentFulPaint entries have also an id
    attribute, and it's a string. So this cannot work (and that's the error
    now in the expected file). 2) There's also the (still masked)
    navigationId error, another attribute of PerformanceEntry. This is
    spec'd as a uint64, in Chrome it's currently a string. I'll try to fix
    that one.
    
    Just to demonstrate the two points above, I'll upload a first patchset
    with this change, which shows how to make the idlharness.window.js test
    actually pass, but this requires commenting out a couple lines from the
    spec IDL, which I can't do in this (Chromium) change. So then I'll undo
    that, in patchset 2, and just fix the test with a new expected output.
    
    Should you later want to see these two (Chromium) patchsets, you may
    find this change at crrev.com/c/6604075
    
    Bug: 420166099
    Change-Id: Iab6a6185f77853e2380a4e11fe235e8ab61bcff5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604075
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468173}

--- third_party/blink/web_tests/external/wpt/paint-timing/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/paint-timing/idlharness.window.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['paint-timing'],
-  ['performance-timeline'],
+  ['performance-timeline', 'hr-time'],
   (idl_array, t) => {
     idl_array.add_objects({
       PerformancePaintTiming: ['paintTiming'],
