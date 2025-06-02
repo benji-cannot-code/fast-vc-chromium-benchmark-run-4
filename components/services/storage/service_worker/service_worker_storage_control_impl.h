945cc5cc9a17c44081d4f5aa2ccb684fe83509ca
Minoru Chikamune <chikamune@chromium.org>
2025-06-02 00:53:16 -0700

    Introduce kBackgroundUpdateForRegisteredStorageKeys feature
    The initial navigation on browser startup tends to be blocked by
    checking ServiceWorker registration. Checking the registration of
    ServiceWorker runs in the thread pool of the browser process, so the DB
    operation itself can be run without being blocked by other busy tasks.
    But, to receive the result, a task must be run on the UI thread
    currently. Since the UI thread is usually very clouded, receiving
    results can be delayed a lot.
    
    This CL introduces kBackgroundUpdateForRegisteredStorageKeys feature to
    mitigate this issue. With this feature, it makes it possible to receive
    the `registered_storage_keys_` information from the thread pool without
    waiting for running the receiving task. To receive the information in
    the thread pool from UI thread, this CL introduces StorageSharedBuffer
    class.
    
    Bug: 421530699
    Change-Id: I8bc703bad94fa0373a80243379f8d828d7ec7a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606856
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467981}

--- components/services/storage/service_worker/service_worker_storage_control_impl.h
+++ components/services/storage/service_worker/service_worker_storage_control_impl.h
@@ -37,9 +37,13 @@ class ServiceWorkerStorageControlImpl
  public:
   static mojo::SelfOwnedReceiverRef<mojom::ServiceWorkerStorageControl> Create(
       mojo::PendingReceiver<mojom::ServiceWorkerStorageControl> receiver,
-      const base::FilePath& user_data_directory);
+      const base::FilePath& user_data_directory,
+      scoped_refptr<storage::ServiceWorkerStorage::StorageSharedBuffer>
+          storage_shared_buffer);
   ServiceWorkerStorageControlImpl(
       const base::FilePath& user_data_directory,
+      scoped_refptr<storage::ServiceWorkerStorage::StorageSharedBuffer>
+          storage_shared_buffer,
       mojo::PendingReceiver<mojom::ServiceWorkerStorageControl> receiver);
 
   ServiceWorkerStorageControlImpl(const ServiceWorkerStorageControlImpl&) =
@@ -54,8 +58,10 @@ class ServiceWorkerStorageControlImpl
   void LazyInitializeForTest();
 
  private:
-  explicit ServiceWorkerStorageControlImpl(
-      const base::FilePath& user_data_directory);
+  ServiceWorkerStorageControlImpl(
+      const base::FilePath& user_data_directory,
+      scoped_refptr<storage::ServiceWorkerStorage::StorageSharedBuffer>
+          storage_shared_buffer);
   // mojom::ServiceWorkerStorageControl implementations:
   void Disable(DisableCallback callback) override;
   void Delete(DeleteCallback callback) override;
--- content/browser/service_worker/service_worker_context_wrapper.h
+++ content/browser/service_worker/service_worker_context_wrapper.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/observer_list.h"
 #include "base/observer_list_threadsafe.h"
+#include "components/services/storage/service_worker/service_worker_storage.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_context_core_observer.h"
 #include "content/browser/service_worker/service_worker_identifiability_metrics.h"
@@ -433,6 +434,11 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
       mojo::PendingReceiver<storage::mojom::ServiceWorkerStorageControl>
           receiver);
 
+  scoped_refptr<storage::ServiceWorkerStorage::StorageSharedBuffer>&
+  storage_shared_buffer() {
+    return storage_shared_buffer_;
+  }
+
   using StorageControlBinder = base::RepeatingCallback<void(
       mojo::PendingReceiver<storage::mojom::ServiceWorkerStorageControl>)>;
   // Sets a callback to bind ServiceWorkerStorageControl for testing.
@@ -608,6 +614,9 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
   // ServiceWorkerStorage is sandboxed. An instance of this impl should live in
   // the storage service, not here.
   std::unique_ptr<storage::ServiceWorkerStorageControlImpl> storage_control_;
+  scoped_refptr<storage::ServiceWorkerStorage::StorageSharedBuffer>
+      storage_shared_buffer_;
+
   // These fields are used to (re)create `storage_control_`.
   base::FilePath user_data_directory_;
   scoped_refptr<storage::QuotaManagerProxy> quota_manager_proxy_;
