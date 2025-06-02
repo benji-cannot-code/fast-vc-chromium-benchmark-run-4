37d823e779f4ea01a70f1c3373ffb66cec9fb547
Eugene Zemtsov <eugene@chromium.org>
2025-06-02 16:03:22 -0700

    Revert "Remove ScopedTestingCrosSettings in c/b/ash/policy/*browsertest part 3"
    This reverts commit 9988986f1cf3eef5d82c2c7c7c20bd9887ecc54f.
    
    Reason for revert: crbug.com/421973488
    
    Original change's description:
    > Remove ScopedTestingCrosSettings in c/b/ash/policy/*browsertest part 3
    >
    > These two tests are actually unittests, rather than browsertests.
    > According to the comment, the concerning is the env var overriding,
    > so this CL uses a utility to unset for each test.
    > Then, moved these tests back to unittest, and removed hacks
    > to run these tests in browser_tests binary.
    >
    > BUG=414927997
    > TEST=Tryjob
    >
    > Change-Id: I2a3252e7ab4255a0a6edca0139fb048e9ba60c93
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6611398
    > Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    > Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1468116}
    
    Bug: 414927997, 421973488
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I01f16ab7f04205bfb93ed91de941d839ca1929a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6613606
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Owners-Override: Eugene Zemtsov <ezemtsov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468420}

--- chrome/browser/ash/policy/status_collector/child_status_collector_browsertest.cc
+++ chrome/browser/ash/policy/status_collector/child_status_collector_browsertest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
-#include "base/scoped_environment_variable_override.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -46,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/test/base/chrome_unit_test_suite.h"
 #include "chrome/test/base/scoped_testing_local_state.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
@@ -174,6 +174,9 @@ bool GetFakeAndroidStatus(const std::string& status,
 
 }  // namespace
 
+// Though it is a unit test, this test is linked with browser_tests so that it
+// runs in a separate process. The intention is to avoid overriding the timezone
+// environment variable for other tests.
 class ChildStatusCollectorTest : public testing::Test {
  public:
   ChildStatusCollectorTest()
@@ -186,8 +189,19 @@ class ChildStatusCollectorTest : public testing::Test {
     // rely on mojo will explode.
     mojo::core::Init();
 
+    // Although this is really a unit test which runs in the browser_tests
+    // binary, it doesn't get the unit setup which normally happens in the unit
+    // test binary.
+    ChromeUnitTestSuite::InitializeProviders();
+    ChromeUnitTestSuite::InitializeResourceBundle();
+
     content::SetContentClient(&content_client_);
     content::SetBrowserClientForTesting(&browser_content_client_);
+
+    // Run this test with a well-known timezone so that Time::LocalMidnight()
+    // returns the same values on all machines.
+    std::unique_ptr<base::Environment> env(base::Environment::Create());
+    env->SetVar("TZ", "UTC");
 
     // Use FakeUpdateEngineClient.
     ash::UpdateEngineClient::InitializeFakeForTest();
@@ -336,9 +350,8 @@ class ChildStatusCollectorTest : public testing::Test {
   }
 
   void OnStatusReceived(StatusCollectorParams callback_params) {
-    if (callback_params.child_status) {
+    if (callback_params.child_status)
       child_status_ = *callback_params.child_status;
-    }
     EXPECT_TRUE(run_loop_);
     run_loop_->Quit();
   }
@@ -402,9 +415,9 @@ class ChildStatusCollectorTest : public testing::Test {
   Profile* testing_profile() { return testing_profile_.get(); }
   PrefService* pref_service() { return testing_profile_->GetPrefs(); }
 
-  // Run this test with a well-known timezone so that Time::LocalMidnight()
-  // returns the same values on all machines.
-  base::ScopedEnvironmentVariableOverride timezone_override_{"TZ", "UTC"};
+  // Since this is a unit test running in browser_tests we must do additional
+  // unit test setup and make a TestingBrowserProcess. Must be first member.
+  TestingBrowserProcessInitializer initializer_;
 
   content::BrowserTaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
