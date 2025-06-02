0f65378ea8c48f81a64e2e5786e37de4845258a3
Ian Wells <iwells@chromium.org>
2025-06-02 11:28:36 -0700

    Add GlicExplicitBackgroundColor flag
    When the flag is enabled:
    * don't copy the glic client background color, instead use one from
      Chrome. In the future there will be an API that allows the client to
      set this color explicitly.
    * Use SetWinPaintColor() to match, so resizing looks better on Windows.
    
    This doesn't completely solve the gutter issue when resizing glic on
    Windows but it helps.
    
    Screen recordings:
      Light mode without flag: shortn/_U68F6EsP2j
      Light mode with flag: shortn/_Pum9u9qs2h
      Dark mode without flag: shortn/_NlsfMYtIyB
      Dark mode with flag: shortn/_yg7lGtYTqy
    
    Bug: 419477329
    Change-Id: I9147b54c0d1246ed6885bd092a04ed9c8be4e4f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578618
    Reviewed-by: Sana Akbani <sanaakbani@google.com>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468257}

--- chrome/browser/glic/widget/glic_view.cc
+++ chrome/browser/glic/widget/glic_view.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
 #include "chrome/browser/ui/views/tabs/glic_button.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/interaction/element_identifier.h"
@@ -67,11 +68,29 @@ void GlicView::UpdatePrimaryDraggableAreaOnResize() {
 }
 
 void GlicView::UpdateBackgroundColor() {
-  std::optional<SkColor> client_background = GetClientBackgroundColor();
-  if (client_background) {
-    SetBackground(views::CreateSolidBackground(*client_background));
-  } else {
-    SetBackground(views::CreateSolidBackground(kColorGlicBackground));
+  const bool explicit_background =
+      base::FeatureList::IsEnabled(features::kGlicExplicitBackgroundColor);
+
+  std::unique_ptr<views::Background> background;
+  if (!explicit_background) {
+    std::optional<SkColor> client_background = GetClientBackgroundColor();
+    if (client_background) {
+      background = views::CreateSolidBackground(*client_background);
+    }
+  }
+
+  if (!background) {
+    background = views::CreateSolidBackground(kColorGlicBackground);
+  }
+
+  SetBackground(std::move(background));
+
+  if (views::Widget* widget = GetWidget(); explicit_background && widget) {
+    // Set the native widget background color if needed.
+    widget->SetColorModeOverride(
+        /*color_mode_override=*/std::nullopt,
+        ui::ColorVariant(kColorGlicBackground)
+            .ResolveToSkColor(widget->GetColorProvider()));
   }
 }
 
