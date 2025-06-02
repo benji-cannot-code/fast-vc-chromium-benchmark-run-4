d10fc8bbd0de988f1a82189d9105bbe373896547
Julia Sobiech <jsobiech@google.com>
2025-06-02 05:37:58 -0700

    [H/W] Expand AutofillProfileRecordTypeCategory
    This change introduces new record types specific to Home and Work
    profiles allowing related metrics to log information about such types.
    
    Affected metrics:
     - Autofill.Leipzig.FillingReadinessCategory
     - Autofill.Leipzig.FillingAssistanceCategory
     - Autofill.Leipzig.FillingCorrectness.{Category}
     - Autofill.StoredProfileCount.{Category}
    
    Bug: 421353034
    Change-Id: I74d1cc8999a09bd09c31e0ed113c340af589b96f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6607137
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468058}

--- components/autofill/core/browser/metrics/form_events/address_form_event_logger.cc
+++ components/autofill/core/browser/metrics/form_events/address_form_event_logger.cc
@@ -48,6 +48,10 @@ CategoryResolvedKeyMetricBucket ProfileCategoriesToMetricBucket(
       return CategoryResolvedKeyMetricBucket::kAccountChrome;
     case AutofillProfileRecordTypeCategory::kAccountNonChrome:
       return CategoryResolvedKeyMetricBucket::kAccountNonChrome;
+    case AutofillProfileRecordTypeCategory::kAccountHome:
+      return CategoryResolvedKeyMetricBucket::kAccountHome;
+    case AutofillProfileRecordTypeCategory::kAccountWork:
+      return CategoryResolvedKeyMetricBucket::kAccountWork;
   }
 }
 
--- components/autofill/core/browser/metrics/form_events/address_form_event_logger_unittest.cc
+++ components/autofill/core/browser/metrics/form_events/address_form_event_logger_unittest.cc
@@ -94,6 +94,10 @@ TEST_F(CategoryResolvedKeyMetricsTest, NoAutofill) {
   histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.AccountNonChrome", 0);
   histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountHome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountWork", 0);
+  histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.Mixed", 0);
 }
 
@@ -136,6 +140,10 @@ TEST_P(CategoryResolvedKeyMetricsEditTest, kLocalOrSyncable) {
   histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.AccountNonChrome", 0);
   histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountHome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountWork", 0);
+  histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.Mixed", 0);
 }
 
@@ -164,6 +172,10 @@ TEST_P(CategoryResolvedKeyMetricsEditTest, kAccountChrome) {
   histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.AccountNonChrome", 0);
   histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountHome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountWork", 0);
+  histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.Mixed", 0);
 }
 
@@ -191,6 +203,72 @@ TEST_P(CategoryResolvedKeyMetricsEditTest, kAccountNonChrome) {
   histogram_tester_.ExpectUniqueSample(
       "Autofill.Leipzig.FillingCorrectness.AccountNonChrome",
       !ShouldEditField(), 1);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountHome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountWork", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.Mixed", 0);
+}
+
+TEST_P(CategoryResolvedKeyMetricsEditTest, kAccountHome) {
+  FormData form = CreateAndSeeForm();
+  FillFormWithProfile(
+      form,
+      CreateProfileOfCategory(AutofillProfileRecordTypeCategory::kAccountHome));
+  if (ShouldEditField()) {
+    SimulateUserChangedField(form, form.fields().front());
+  }
+  SubmitForm(form);
+
+  ResetDriverToCommitMetrics();
+  histogram_tester_.ExpectUniqueSample(
+      "Autofill.Leipzig.FillingReadinessCategory",
+      CategoryResolvedKeyMetricBucket::kAccountHome, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Autofill.Leipzig.FillingAssistanceCategory",
+      CategoryResolvedKeyMetricBucket::kAccountHome, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.Legacy", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountChrome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountNonChrome", 0);
+  histogram_tester_.ExpectUniqueSample(
+      "Autofill.Leipzig.FillingCorrectness.AccountHome", !ShouldEditField(), 1);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountWork", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.Mixed", 0);
+}
+
+TEST_P(CategoryResolvedKeyMetricsEditTest, kAccountWork) {
+  FormData form = CreateAndSeeForm();
+  FillFormWithProfile(
+      form,
+      CreateProfileOfCategory(AutofillProfileRecordTypeCategory::kAccountWork));
+  if (ShouldEditField()) {
+    SimulateUserChangedField(form, form.fields().front());
+  }
+  SubmitForm(form);
+
+  ResetDriverToCommitMetrics();
+  histogram_tester_.ExpectUniqueSample(
+      "Autofill.Leipzig.FillingReadinessCategory",
+      CategoryResolvedKeyMetricBucket::kAccountWork, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Autofill.Leipzig.FillingAssistanceCategory",
+      CategoryResolvedKeyMetricBucket::kAccountWork, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.Legacy", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountChrome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountNonChrome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountHome", 0);
+  histogram_tester_.ExpectUniqueSample(
+      "Autofill.Leipzig.FillingCorrectness.AccountWork", !ShouldEditField(), 1);
   histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.Mixed", 0);
 }
@@ -232,6 +310,10 @@ TEST_P(CategoryResolvedKeyMetricsEditTest, Mixed) {
       "Autofill.Leipzig.FillingCorrectness.AccountChrome", 0);
   histogram_tester_.ExpectTotalCount(
       "Autofill.Leipzig.FillingCorrectness.AccountNonChrome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountHome", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.Leipzig.FillingCorrectness.AccountWork", 0);
   histogram_tester_.ExpectUniqueSample(
       "Autofill.Leipzig.FillingCorrectness.Mixed", !ShouldEditField(), 1);
 }
