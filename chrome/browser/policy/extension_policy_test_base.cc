868abf26640a4ce5c2389b01acd440f96459a9fe
James Cook <jamescook@chromium.org>
2025-06-02 14:26:38 -0700

    policy: Add ExtensionPolicyTest to desktop Android
    Desktop Android supports extensions, and several extension management
    policies have been added to the build. While the unit tests were added,
    we're missing some browser test coverage.
    
    Add the parts of ExtensionPolicyTest that work on desktop Android to
    the build. The unsupported parts (in particular policies that we have
    not wired up yet) are wrapped with ENABLE_EXTENSIONS ifdefs, as is our
    convention in the extensions code. (ENABLE_EXTENSIONS_CORE includes
    desktop Android, whereas ENABLE_EXTENSIONS is Win/Mac/Linux/CrOS.)
    
    Bug: 394876083
    Change-Id: Iadd4aa7f4c03730b7cac84f011f39c9ef739ba6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594219
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468356}

--- chrome/browser/policy/extension_policy_test_base.cc
+++ chrome/browser/policy/extension_policy_test_base.cc
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/policy/extension_policy_test_base.h"
 
-#include "base/path_service.h"
 #include "chrome/browser/extensions/chrome_test_extension_loader.h"
+#include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/test/base/ui_test_utils.h"
 
 namespace policy {
 
@@ -23,9 +21,10 @@ ExtensionPolicyTestBase::~ExtensionPolicyTestBase() = default;
 scoped_refptr<const extensions::Extension>
 ExtensionPolicyTestBase::LoadUnpackedExtension(
     const base::FilePath::StringType& name) {
-  base::FilePath extension_path(ui_test_utils::GetTestFilePath(
-      base::FilePath(kTestExtensionsDir), base::FilePath(name)));
-  extensions::ChromeTestExtensionLoader loader(browser()->profile());
+  base::FilePath extension_path =
+      GetTestFilePath(base::FilePath(kTestExtensionsDir), base::FilePath(name));
+  auto* profile = chrome_test_utils::GetProfile(this);
+  extensions::ChromeTestExtensionLoader loader(profile);
   return loader.LoadExtension(extension_path);
 }
 
