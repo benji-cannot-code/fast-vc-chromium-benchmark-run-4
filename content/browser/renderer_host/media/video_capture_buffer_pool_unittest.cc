33504ff37282d885878e85c7d4266ed02ae24d4f
Colin Blundell <blundell@chromium.org>
2025-06-02 23:37:49 -0700

    [//content] Remove gpu_memory_buffer.h includes
    These only use GpuMemoryBufferHandle.
    
    Bug: 404905709
    Change-Id: Ie1237a2ea363e3db48d25fd4d07998aa61f8d485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606160
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468539}

--- content/browser/renderer_host/media/video_capture_buffer_pool_unittest.cc
+++ content/browser/renderer_host/media/video_capture_buffer_pool_unittest.cc
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/win/scoped_handle.h"
 #include "media/base/win/dxgi_device_manager.h"
-#include "ui/gfx/gpu_memory_buffer.h"
+#include "ui/gfx/gpu_memory_buffer_handle.h"
 #endif
 
 namespace content {
--- content/renderer/media/win/dcomp_texture_wrapper_unittest.cc
+++ content/renderer/media/win/dcomp_texture_wrapper_unittest.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/test_helpers.h"
 #include "media/base/win/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/gfx/gpu_memory_buffer.h"
+#include "ui/gfx/gpu_memory_buffer_handle.h"
 
 using base::RunLoop;
 using Microsoft::WRL::ComPtr;
