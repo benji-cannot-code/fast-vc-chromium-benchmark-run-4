1da31223b510c04608737b8394d345541f34d42c
Jonathan Ross <jonross@chromium.org>
2025-06-02 18:07:38 -0700

    Remove Graphics.Exo.Smoothness.FrameArrivalToSubmission*
    This metric is no longer being used. Exo has been stable for a while.
    
    OBSOLETE_HISTOGRAM[Graphics.Exo.Smoothness.FrameArrivalToSubmission]=No longer used
    OBSOLETE_HISTOGRAM[Graphics.Exo.Smoothness.BeginFrameArrivalToSubmission]=No longer used
    
    Bug: 420442957
    Change-Id: I3b32bf5e54d68fdb4dd5903e042dda95c40a3461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6614417
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468475}

--- components/exo/layer_tree_frame_sink_holder.cc
+++ components/exo/layer_tree_frame_sink_holder.cc
@@ -116,15 +116,6 @@ void LayerTreeFrameSinkHolder::SubmitCompositorFrame(viz::CompositorFrame frame,
   }
 
   DiscardCachedFrame(&frame);
-
-  // Needs to be after DiscardCachedFrame(), because discarding a frame will
-  // reset the frame arrival information in `frame_timing_history_`.
-  if (frame_timing_history_) {
-    frame_timing_history_->FrameArrived();
-
-    frame_timing_history_->MayRecordDidNotProduceToFrameArrvial(/*valid=*/true);
-  }
-
   ObserveBeginFrameSource(true);
 
   if (!ShouldSubmitFrameNow() && !submit_now) {
@@ -309,9 +300,6 @@ bool LayerTreeFrameSinkHolder::OnBeginFrameDerivedImpl(
 
   base::TimeDelta timing_estimate = base::Milliseconds(0);
   if (frame_timing_history_) {
-    frame_timing_history_->BeginFrameArrived(args.frame_id);
-    frame_timing_history_->MayRecordDidNotProduceToFrameArrvial(
-        /*valid=*/false);
     timing_estimate = frame_timing_history_->GetFrameTransferDurationEstimate();
   }
 
@@ -418,9 +406,6 @@ void LayerTreeFrameSinkHolder::DiscardCachedFrame(
         });
   }
   cached_frame_.reset();
-  if (frame_timing_history_) {
-    frame_timing_history_->FrameDiscarded();
-  }
 }
 
 void LayerTreeFrameSinkHolder::SendDiscardedFrameNotifications(
@@ -470,12 +455,6 @@ void LayerTreeFrameSinkHolder::OnSendDeadlineExpired(bool update_timer) {
              ? (frame_timing_history_->consecutive_did_not_produce_count() >=
                 kPauseBeginFrameThreshold)
              : false);
-
-    if ((!pending_begin_frames_.empty() || should_pause_begin_frame) &&
-        frame_timing_history_) {
-      frame_timing_history_->MayRecordDidNotProduceToFrameArrvial(
-          /*valid=*/false);
-    }
 
     if (should_pause_begin_frame) {
       ObserveBeginFrameSource(false);
