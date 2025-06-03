ad343a27fb681fd0fa1581238eb9184319b34cba
Luciano Pacheco <lucmult@chromium.org>
2025-06-02 19:14:57 -0700

    Revert "Implement 2-QWAC link header processing and 2-QWAC fetching"
    This reverts commit 9e2e159159157281df11fd2f569cddd86fc6cf9b.
    
    Reason for revert: Suspect of breaking the bot: linux-bfcache-rel 
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/b8713130622980753393/test-results?q=ID%3Aninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FQwacWebContentsObserverBrowserTest.TestSuccessAndCachedGoBack
    
    Bug: 392931069
    Original change's description:
    > Implement 2-QWAC link header processing and 2-QWAC fetching
    >
    > Bug: 392931069
    > Change-Id: I09e7c9d35ff078067041a59e2554411ead1a294d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6554703
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Chris Mullins <crmullins@chromium.org>
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Matt Mueller <mattm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1468410}
    
    Bug: 392931069
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I0b701f61585afc351d047ac0342b20b39b833936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6615378
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468502}

--- chrome/browser/ui/tabs/public/tab_features.h
+++ chrome/browser/ui/tabs/public/tab_features.h
@@ -29,7 +29,6 @@ class SidePanelRegistry;
 class TabResourceUsageTabHelper;
 class TabUIHelper;
 class TranslatePageActionController;
-class QwacWebContentsObserver;
 
 namespace commerce {
 class CommerceUiTabHelper;
@@ -403,8 +402,6 @@ class TabFeatures {
   std::unique_ptr<TabAlertController> tab_alert_controller_;
 
   std::unique_ptr<TabUIHelper> tab_ui_helper_;
-
-  std::unique_ptr<QwacWebContentsObserver> qwac_web_contents_observer_;
 
   // Must be the last member.
   base::WeakPtrFactory<TabFeatures> weak_factory_{this};
