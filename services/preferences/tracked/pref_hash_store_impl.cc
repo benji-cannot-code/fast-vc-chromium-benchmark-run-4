7173949c3746dc72c8074dbc0d41d227a75e18a0
Zack Han <zackhan@chromium.org>
2025-06-02 17:13:24 -0700

    [Harden Pref][Bug Fix] Keep the legacy mac entry when storing EHs
    Previously, when we store a new encrypted hash, the code removes both
    old entries, HMAC entry and the encrypted hash entry, which causes some
    test failures due to removed legacy HMAC. At this point, we do not
    need to remove the legacy HMAC when writing a new encrypted hash to the
    hash store. This CL corrects this code and adds a new unit test to
    verify the pref hash store behavior.
    
    Bug: 346830807
    Change-Id: I0caa53cb9b2d7fd2409ef547fbecdfb044bc7d54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612363
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468452}

--- services/preferences/tracked/pref_hash_store_impl.cc
+++ services/preferences/tracked/pref_hash_store_impl.cc
@@ -99,7 +99,7 @@ class PrefHashStoreImpl::PrefHashStoreTransactionImpl
 
   // Stores the new split Encrypted Hashes. Requires the encryptor.
   void StoreSplitEncryptedHash(const std::string& path,
-                               const base::Value::Dict* split_value);
+                               const base::Value::Dict* split_value) override;
 
   // Clears only the Encrypted Hash for the path.
   void ClearEncryptedHash(const std::string& path);
@@ -575,8 +575,6 @@ void PrefHashStoreImpl::PrefHashStoreTransactionImpl::StoreSplitEncryptedHash(
     return;
   }
 
-  // Remove any existing single MAC entry for the base path.
-  contents_->RemoveEntry(path);
   // Also remove any existing single *encrypted hash* entry for the base path
   contents_->RemoveEntry(GetEncryptedHashKey(path));
 
