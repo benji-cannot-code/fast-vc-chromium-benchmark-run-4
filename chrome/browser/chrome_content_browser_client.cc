53ebfaf0db63210f2d5b678530e457695316d5d1
Di Zhang <dizhangg@chromium.org>
2025-06-03 10:32:02 -0700

    Remove enterprise policy for KeyboardFocusableScrollers at 138
    We decided to remove this enterprise policy in 138 to match the end
    of its opt-out origin trial.
    
    chromestatus:
    https://chromestatus.com/feature/5231964663578624
    
    Change-Id: I8567bf1a32e0c148f4603eea71cd9f50c66e1308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612449
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468825}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1480,8 +1480,6 @@ void ChromeContentBrowserClient::RegisterProfilePrefs(
       /*default_value=*/true);
 
   registry->RegisterBooleanPref(
-      policy::policy_prefs::kKeyboardFocusableScrollersEnabled, true);
-  registry->RegisterBooleanPref(
       policy::policy_prefs::kStandardizedBrowserZoomEnabled, true);
 
   registry->RegisterBooleanPref(
@@ -2784,11 +2782,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
             blink::switches::kDisableBlobUrlPartitioning);
       }
 
-      if (!prefs->GetBoolean(
-              policy::policy_prefs::kKeyboardFocusableScrollersEnabled)) {
-        command_line->AppendSwitch(
-            blink::switches::kKeyboardFocusableScrollersOptOut);
-      }
       if (!prefs->GetBoolean(
               policy::policy_prefs::kStandardizedBrowserZoomEnabled)) {
         command_line->AppendSwitch(
