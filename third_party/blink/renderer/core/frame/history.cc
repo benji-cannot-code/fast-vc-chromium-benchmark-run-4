d924f1bf7b1d3448dd19cade11555057e9131ffd
Anton Bershanskyi <bershanskyi@gmail.com>
2025-06-03 12:14:12 -0700

    [History] Standard way for handling empty URL
    This CL removes special case handling empty string URL in
    History API pushState() and replaceState(), aligning Chrome
    with Firefox and Safari. Also add config to rollback this
    change in case of compatibility issues. For details see
    https://github.com/whatwg/html/pull/9370
    
    Bug: 40915318
    Change-Id: I933ece53c993fbd4487749913e4ac77ea9e5c213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576127
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468915}

--- third_party/blink/renderer/core/frame/history.cc
+++ third_party/blink/renderer/core/frame/history.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/v8_private_property.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/task_attribution_info.h"
 #include "third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
@@ -319,8 +320,11 @@ void History::replaceState(ScriptState* script_state,
 }
 
 KURL History::UrlForState(const String& url_string) {
-  if (url_string.IsNull())
+  if (url_string.IsNull() ||
+      (url_string.empty() &&
+       RuntimeEnabledFeatures::StandardHistoryStateEmptyUrlHandlingEnabled())) {
     return DomWindow()->Url();
+  }
   if (url_string.empty())
     return DomWindow()->BaseURL();
 
