6d1a9407c90e55ff26d908da2d35ed7663b20550
Christian Xu <christianxu@chromium.org>
2025-06-03 10:30:54 -0700

    [iOS][LO] Enable lens overlay entrypoint by default
    Enable the lens overlay entrypoint by default. This will also disable
    the pan IPH pan gesture recognizer for this IPH which was causing
    crashes.
    
    Bug: 413590628
    Change-Id: I4c82232ee77dae1cea09907f7aea4ac97994d63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6613830
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468823}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1096,7 +1096,7 @@ BASE_FEATURE(kIPHAblation, "IPHAblation", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensOverlayDisableIPHPanGesture,
              "LensOverlayDisableIPHPanGesture",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsIPHAblationEnabled() {
   return base::FeatureList::IsEnabled(kIPHAblation);
