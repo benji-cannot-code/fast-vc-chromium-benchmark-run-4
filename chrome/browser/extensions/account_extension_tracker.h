bb8367dc47c55febcd6be0ef48ec2a94f0c09ab4
Keigo Oka <oka@google.com>
2025-06-02 21:07:31 -0700

    extensions: Include buildflags.h directly in a header file
    Follow up https://crrev.com/c/6606919
    
    Bug: 419460299
    Change-Id: If78d713cd29d0e4aac24d3da55efcce01b68673e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6615497
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468518}

--- chrome/browser/extensions/account_extension_tracker.h
+++ chrome/browser/extensions/account_extension_tracker.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_id.h"
 
