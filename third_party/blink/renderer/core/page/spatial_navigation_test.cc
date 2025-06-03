6d7f84c6ec5e556a80b986533fda596eac90ebc3
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-06-02 19:16:59 -0700

    Revert "Element::VisibleBoundsInLocalRoot performs clipping"
    This reverts commit ec7c29e63ffa06ddafd5c623146347e5a07c73bb.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5426639660908544
    
    Sample build with failed test: https://ci.chromium.org/b/8713137394515927985
    Affected test(s):
    [ninja://:blink_web_tests/fast/forms/validation-bubble-appearance-escape.html](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_web_tests%2Ffast%2Fforms%2Fvalidation-bubble-appearance-escape.html?q=VHash%3Afd4ea9131c944098)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5426639660908544&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6512367&type=BUG
    
    Original change's description:
    > Element::VisibleBoundsInLocalRoot performs clipping
    >
    > VisibleBoundsRespectingClipsInLocalRoot was added in
    > https://crrev.com/c/6525508 as it was urgent but the real desire is for
    > VisibleBoundsInLocalRoot to have the overflow clipping behavior. This CL
    > makes the change to unify these two method behind a flag (as there may
    > be compat issues). Assuming this lands safely we can merge the two
    > methods.
    >
    > VisibleBoundsInLocalRoot clips to ancestor frame rects but not to any
    > element clips (e.g. overflow:hidden). This change performs the entire
    > mapping using MapToVisualRectInAncestorSpace which does apply these
    > clips. The change is flag guarded in case of compatibility issues.
    >
    > Making this change broke HTMLSelectElementTest.VisibleBoundsInLocalRoot
    > This is because MapToVisualRectInAncestorSpace causes the
    > subpixel-positioned rect to expand which can cause a select drop down to
    > appear detached from the element. This is why this function was using a
    > rounded absolute rect. This comes from https://crrev.com/1268243002. The
    > bug here appears to be coming from LayoutBox::ApplyBoxClips which
    > unnecessarily(?) snaps to pixels.
    >
    > The spat-nav changes came from https://crrev.com/c/1243246 - the
    > IsOffscreen function no longer checks VisibleContentRect so only the
    > TODO is removed there stale test expectations can now be removed.
    > Technically SpatNav's NodeRectInRootFrame could now just be replaced by
    > VisibleBoundsInLocalRoot except that it has an edge-case for border
    > insets; its necessity is unclear.
    >
    > Bug: 41417572,415110566
    > Change-Id: I94dcb816552e83c85f1cd6b9c9c7f1520ffa7e6c
    > Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512367
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1468348}
    >
    
    Bug: 41417572,415110566
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6bb11e6d7067472d9b90d67c9d395c20e500018a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6614786
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468503}

--- third_party/blink/renderer/core/page/spatial_navigation_test.cc
+++ third_party/blink/renderer/core/page/spatial_navigation_test.cc
@@ -204,7 +204,12 @@ TEST_F(SpatialNavigationTest,
   Element* container = GetDocument().getElementById(AtomicString("container"));
   Node* enclosing_container = ScrollableAreaOrDocumentOf(content);
 
-  EXPECT_TRUE(content->VisibleBoundsInLocalRoot().IsEmpty());
+  // TODO(crbug.com/889840):
+  // VisibleBoundsInLocalRoot does not (yet) take div-clipping into
+  // account. The node is off screen, but nevertheless VBIVV returns a non-
+  // empty rect. If you fix VisibleBoundsInLocalRoot, change to
+  // EXPECT_TRUE here and stop using LayoutObject in IsOffscreen().
+  EXPECT_FALSE(content->VisibleBoundsInLocalRoot().IsEmpty());  // EXPECT_TRUE.
 
   EXPECT_TRUE(IsOffscreen(content));
   EXPECT_FALSE(IsOffscreen(container));
@@ -213,7 +218,7 @@ TEST_F(SpatialNavigationTest,
   EXPECT_TRUE(IsScrollableAreaOrDocument(enclosing_container));
 }
 
-TEST_F(SpatialNavigationTest, ZoomPutsElementOffScreen) {
+TEST_F(SpatialNavigationTest, ZooomPutsElementOffScreen) {
   SetBodyInnerHTML(
       "<!DOCTYPE html>"
       "<button id='a'>hello</button><br>"
@@ -368,7 +373,12 @@ TEST_F(SpatialNavigationTest, StartAtContainersEdge) {
       GetDocument().getElementById(AtomicString("container"));
   const PhysicalRect container_box = NodeRectInRootFrame(container);
 
-  EXPECT_TRUE(b->VisibleBoundsInLocalRoot().IsEmpty());
+  // TODO(crbug.com/889840):
+  // VisibleBoundsInLocalRoot does not (yet) take div-clipping into
+  // account. The node is off screen, but nevertheless VBIVV returns a non-
+  // empty rect. If you fix VisibleBoundsInLocalRoot, change to
+  // EXPECT_TRUE here and stop using LayoutObject in IsOffscreen().
+  EXPECT_FALSE(b->VisibleBoundsInLocalRoot().IsEmpty());  // EXPECT_TRUE.
   EXPECT_TRUE(IsOffscreen(b));
 
   // Go down.
@@ -561,7 +571,12 @@ TEST_F(SpatialNavigationTest, DivsCanClipIframes) {
   Element* link = ChildDocument().QuerySelector(AtomicString("a"));
   EXPECT_FALSE(IsOffscreen(div));
 
-  EXPECT_TRUE(iframe->VisibleBoundsInLocalRoot().IsEmpty());
+  // TODO(crbug.com/889840):
+  // VisibleBoundsInLocalRoot does not (yet) take div-clipping into
+  // account. The node is off screen, but nevertheless VBIVV returns a non-
+  // empty rect. If you fix VisibleBoundsInLocalRoot, change to
+  // EXPECT_TRUE here and stop using LayoutObject in IsOffscreen().
+  EXPECT_FALSE(iframe->VisibleBoundsInLocalRoot().IsEmpty());  // EXPECT_TRUE.
 
   // The <iframe> is not displayed in the visual viewport because it is clipped
   // by the div. In other words, it is being offscreen. And so is also its
