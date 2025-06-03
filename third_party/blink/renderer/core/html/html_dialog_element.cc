b272c520d8dbe40a718f7b6ecaac045fc1a638a9
Joey Arhar <jarhar@chromium.org>
2025-06-03 11:11:57 -0700

    Implement ToggleEvent.source for dialog requestclose
    I recently added this to the spec PR after incorporating spec changes
    for the addition of dialog.requestClose:
    https://github.com/whatwg/html/pull/11186
    
    Bug: 408018828
    Change-Id: Ia6a3a2560349760219a863dba814c5addfa60165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598554
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468857}

--- third_party/blink/renderer/core/html/html_dialog_element.cc
+++ third_party/blink/renderer/core/html/html_dialog_element.cc
@@ -221,14 +221,16 @@ void HTMLDialogElement::close(const String& return_value,
   }
 }
 
-void HTMLDialogElement::requestClose(const String& return_value,
-                                     ExceptionState& exception_state) {
+void HTMLDialogElement::RequestCloseInternal(const String& return_value,
+                                             Element* invoker,
+                                             ExceptionState& exception_state) {
   if (!IsOpenAndActive()) {
     return;
   }
   CHECK(close_watcher_);
   close_watcher_->setEnabled(true);
   request_close_return_value_ = return_value;
+  request_close_source_element_ = invoker;
   close_watcher_->RequestClose(CloseWatcher::AllowCancel::kAlways);
   SetCloseWatcherEnabledState();
 }
@@ -382,7 +384,7 @@ bool HTMLDialogElement::HandleCommandInternal(HTMLElement& invoker,
   } else if (command == CommandEventType::kRequestClose) {
     CHECK(RuntimeEnabledFeatures::HTMLCommandRequestCloseEnabled());
     if (open) {
-      requestClose(return_value, ASSERT_NO_EXCEPTION);
+      RequestCloseInternal(return_value, &invoker, ASSERT_NO_EXCEPTION);
       return true;
     } else {
       AddConsoleMessage(
@@ -636,7 +638,8 @@ void HTMLDialogElement::CloseWatcherFiredCancel(Event* close_watcher_event) {
 void HTMLDialogElement::CloseWatcherFiredClose() {
   // https://wicg.github.io/close-watcher/#patch-dialog closeAction
 
-  close(request_close_return_value_);
+  close(request_close_return_value_, request_close_source_element_);
+  request_close_source_element_ = nullptr;
 }
 
 // https://html.spec.whatwg.org#dialog-focusing-steps
@@ -725,6 +728,7 @@ void HTMLDialogElement::DispatchPendingToggleEvent() {
 }
 
 void HTMLDialogElement::Trace(Visitor* visitor) const {
+  visitor->Trace(request_close_source_element_);
   visitor->Trace(previously_focused_element_);
   visitor->Trace(close_watcher_);
   visitor->Trace(pending_toggle_event_);
