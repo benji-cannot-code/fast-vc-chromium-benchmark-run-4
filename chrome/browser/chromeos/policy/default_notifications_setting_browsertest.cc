26308fbaa5d7c8688480c9c6a3853ca5237ba3e9
Yifan Luo <lyf@chromium.org>
2025-06-03 02:53:34 -0700

    [Site settings] Update tests for PermissionSiteSettingsRadioButton feature.
    This CL only update related tests for PermissionSiteSettingsRadioButton
    tests. A follow-up CL will enable the feature flag in `testing/variations/fieldtrial_testing_config.json`
    
    Bug: 41494473, 418276074
    Change-Id: I0ef58c535e82ac933797cc9878234f88fc59c5b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6557087
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Auto-Submit: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468610}

--- chrome/browser/chromeos/policy/default_notifications_setting_browsertest.cc
+++ chrome/browser/chromeos/policy/default_notifications_setting_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/permissions/features.h"
 #include "components/policy/policy_constants.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -31,6 +32,11 @@ class DefaultNotificationsSettingBrowserTest
     : public policy::PolicyTest,
       public testing::WithParamInterface<int> {
  public:
+  DefaultNotificationsSettingBrowserTest() {
+    feature_list_.InitWithFeatures(
+        {}, {permissions::features::kPermissionSiteSettingsRadioButton});
+  }
+
   void SetUpInProcessBrowserTestFixture() override {
     policy::PolicyTest::SetUpInProcessBrowserTestFixture();
 
