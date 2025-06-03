6910a05e178311387aeef464982c6ee282b26e85
Alexei Svitkine <asvitkine@chromium.org>
2025-06-03 12:49:17 -0700

    Simplify optional logic for VariationsPlatformOverride
    - Use std::optional instead of separate bool and value
    - Also initialized primitive members in the header.
    
    Change-Id: I1106bf0f0737f7c2aa2d8fc1176513991ab4de21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6617925
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468931}

--- components/variations/service/variations_field_trial_creator_base.h
+++ components/variations/service/variations_field_trial_creator_base.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 #include <memory>
+#include <optional>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -325,20 +326,16 @@ class VariationsFieldTrialCreatorBase {
 
   // Tracks whether |CreateTrialsFromSeed| has been called, to ensure that it is
   // called at most once.
-  bool create_trials_from_seed_called_;
+  bool create_trials_from_seed_called_ = false;
 
   // The application locale won't change after the startup, so we cache the
   // value the first time when GetApplicationLocale() is called in the
   // constructor.
   std::string application_locale_;
 
-  // Indicate if OverrideVariationsPlatform has been used to set
-  // |platform_override_|.
-  bool has_platform_override_;
-
   // Platform to be used for variations filtering, overriding the current
   // platform.
-  Study::Platform platform_override_;
+  std::optional<Study::Platform> platform_override_;
 
   // Caches the UI strings which need to be overridden in the resource bundle.
   // These strings are cached before the resource bundle is initialized.
