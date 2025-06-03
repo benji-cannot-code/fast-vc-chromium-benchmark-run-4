d62c2fe2fcdd293cb0e0e4109689361bbf209ec0
Arthur Milchior <arthurmilchior@google.com>
2025-06-03 03:18:11 -0700

    [iOS]Correct mocks
    Checking the mock allowed to find the expected call was not actually called.
    
    Bug: 421873301
    Change-Id: Ieb3c191d346b04e9c99bab2ea335a98b690e86e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6611063
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468618}

--- ios/chrome/browser/bookmarks/ui_bundled/cells/bookmark_text_field_item_unittest.mm
+++ ios/chrome/browser/bookmarks/ui_bundled/cells/bookmark_text_field_item_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/gtest_mac.h"
 #import "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
+#import "third_party/ocmock/gtest_support.h"
 
 namespace {
 
@@ -27,8 +28,8 @@ TEST_F(BookmarkTextFieldItemTest, DelegateGetsTextFieldEvents) {
   [item configureCell:cell withStyler:styler];
   EXPECT_EQ(mockDelegate, cell.textField.delegate);
 
-  [[mockDelegate expect] textDidChangeForItem:item];
   cell.textField.text = @"Foo";
+  EXPECT_OCMOCK_VERIFY(mockDelegate);
 }
 
 TEST_F(BookmarkTextFieldItemTest, TextFieldGetsText) {
