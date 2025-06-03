4a244fea09145e49fb7a576f1bb5d516f74b0aaf
Colin Blundell <blundell@chromium.org>
2025-06-02 23:38:11 -0700

    [//services] Remove gpu_memory_buffer.h includes
    These only use GpuMemoryBufferHandle.
    
    Bug: 404905709
    Change-Id: I4b91a4817ff5c8277de0e058a137f21f18c8a4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604980
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468540}

--- services/video_capture/ash/video_frame_handler_ash.cc
+++ services/video_capture/ash/video_frame_handler_ash.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_transformation.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "mojo/public/cpp/system/buffer.h"
-#include "ui/gfx/gpu_memory_buffer.h"
+#include "ui/gfx/gpu_memory_buffer_handle.h"
 
 namespace crosapi {
 
