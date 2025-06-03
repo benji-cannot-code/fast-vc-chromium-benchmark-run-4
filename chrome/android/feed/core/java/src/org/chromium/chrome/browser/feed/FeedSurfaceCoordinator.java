23f53300096f3aaf9be8422725f433db39068493
Yanling <yanlinghu@google.com>
2025-06-03 12:23:12 -0700

    [NTP Customization] Use semantic color for better maintenance
    This CL replaces the background of the button to a semantic color, which
    will make the code easier to maintain during future design system
    updates.
    
    The UI remains visually unchanged, as demonstrated by the following
    screenshot:
    https://docs.google.com/document/d/1bMRVyyqOd0-cAmyTbL9IhKNU9uSvDdpdCngSzSXIHnk/edit?tab=t.0#bookmark=id.xxp34w72xqdy
    
    Bug: 376238770
    Change-Id: Iadfd1830bb631d79c7f9940b1a0cff4bf16cecdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606134
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468919}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.feed;
 
+import static org.chromium.components.browser_ui.styles.SemanticColorUtils.getDefaultIconColor;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.ColorStateList;
@@ -77,7 +79,6 @@ import org.chromium.chrome.browser.xsurface.feed.FeedUserInteractionReliabilityL
 import org.chromium.chrome.browser.xsurface.feed.FeedUserInteractionReliabilityLogger.ClosedReason;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.edge_to_edge.EdgeToEdgePadAdjuster;
-import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.displaystyle.UiConfig;
 import org.chromium.components.feature_engagement.EventConstants;
 import org.chromium.components.feature_engagement.Tracker;
@@ -492,8 +493,7 @@ public class FeedSurfaceCoordinator
             mNtpCustomizationButton.setBackgroundResource(R.drawable.edit_icon_circle_background);
             ImageViewCompat.setImageTintList(
                     mNtpCustomizationButton,
-                    ColorStateList.valueOf(
-                            SemanticColorUtils.getColorOnSurface(mRootView.getContext())));
+                    ColorStateList.valueOf(getDefaultIconColor(mRootView.getContext())));
             int size =
                     mActivity
                             .getResources()
