12906e7020900d64708971ab06a8b349555d1b65
Liza Bipin <mlbipin@google.com>
2025-06-03 03:25:16 -0700

    Revert "[PS ACT Survey] Add custom invitation on Android"
    This reverts commit 048370733cb266f1128f9089658f4b7b5f5cfb86.
    
    Reason for revert: Caused compile failure of android-arm-rel-ready
    
    Bug: 422077952
    Original change's description:
    > [PS ACT Survey] Add custom invitation on Android
    >
    > Fixed: 420883644
    > Change-Id: I995f5d4f1d69e4142aedca276a0fba5bd9fcf6df
    > Low-Coverage-Reason: HARD_TO_TEST HaTS test support is not available on Android
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6597116
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Reviewed-by: Matt Dembski <dembski@google.com>
    > Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1468608}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I29269c4dab0790b91b477d44acb66fac8cff47e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6613831
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Owners-Override: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468621}

--- chrome/browser/privacy_sandbox/incognito/privacy_sandbox_incognito_survey_service.cc
+++ chrome/browser/privacy_sandbox/incognito/privacy_sandbox_incognito_survey_service.cc
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/hats/hats_service.h"
 #include "chrome/browser/ui/hats/hats_service_factory.h"
 #include "chrome/browser/ui/hats/survey_config.h"
-#include "chrome/grit/branded_strings.h"
 #include "privacy_sandbox_incognito_features.h"
-#include "ui/base/l10n/l10n_util.h"
 
 namespace privacy_sandbox {
 
@@ -68,20 +66,6 @@ PrivacySandboxIncognitoSurveyService::GetActSurveyPsd(int delay_ms) {
   return {{"Survey Trigger Delay", base::NumberToString(delay_ms)}};
 }
 
-HatsService::SurveyOptions
-PrivacySandboxIncognitoSurveyService::GetActSurveyOptions() {
-  HatsService::SurveyOptions options;
-
-#if BUILDFLAG(IS_ANDROID)
-  if (kPrivacySandboxActSurveyCustomInvitation.Get()) {
-    options.custom_invitation =
-        l10n_util::GetStringUTF16(IDS_MESSAGE_ACT_SURVEY_INVITATION);
-  }
-#endif
-
-  return options;
-}
-
 void PrivacySandboxIncognitoSurveyService::RecordActSurveyStatus(
     ActSurveyStatus status) {
   base::UmaHistogramEnumeration("PrivacySandbox.ActSurvey.Status", status);
@@ -118,9 +102,7 @@ void PrivacySandboxIncognitoSurveyService::MaybeShowActSurvey(
                      weak_ptr_factory_.GetWeakPtr()),
       /*failure_callback=*/
       base::BindOnce(&PrivacySandboxIncognitoSurveyService::OnActSurveyFailure,
-                     weak_ptr_factory_.GetWeakPtr()),
-      /*supplied_trigger_id=*/std::nullopt,
-      /*survey_options=*/GetActSurveyOptions());
+                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void PrivacySandboxIncognitoSurveyService::OnActSurveyShown() {
