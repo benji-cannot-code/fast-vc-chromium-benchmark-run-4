e02293ec581abd890b45cddf32f2bcf68daea066
Kai Ninomiya <kainino@chromium.org>
2025-06-03 15:49:40 -0700

    Handle Dawn breaking change moving Compat limits to an extension
    Needed in order to update Dawn to match the standard webgpu.h, here:
    https://dawn-review.googlesource.com/c/dawn/+/244294
    
    ComboLimits is kind of a duplicate of dawn::utils::ComboLimits
    and should be deduplicated at some point if possible.
    
    Bug: 416304914
    Change-Id: I945d597e278351f97c82f54568f4e4beee48728a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598226
    Reviewed-by: Loko Kung <lokokung@google.com>
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Loko Kung <lokokung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469025}

--- third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
@@ -115,8 +115,8 @@ GPUAdapter::GPUAdapter(
 
   features_ = MakeFeatureNameSet(GetHandle());
 
-  wgpu::Limits limits = {};
-  GetHandle().GetLimits(&limits);
+  GPUSupportedLimits::ComboLimits limits;
+  GetHandle().GetLimits(limits.GetLinked());
   limits_ = MakeGarbageCollected<GPUSupportedLimits>(limits);
 
   info_ = CreateAdapterInfoForAdapter();
@@ -275,10 +275,9 @@ ScriptPromise<GPUDevice> GPUAdapter::requestDevice(
 
   wgpu::DeviceDescriptor dawn_desc = {};
 
-  wgpu::Limits required_limits = {};
+  GPUSupportedLimits::ComboLimits required_limits;
   if (descriptor->hasRequiredLimits()) {
-    dawn_desc.requiredLimits = &required_limits;
-    GPUSupportedLimits::MakeUndefined(&required_limits);
+    dawn_desc.requiredLimits = required_limits.GetLinked();
     if (!GPUSupportedLimits::Populate(&required_limits,
                                       descriptor->requiredLimits(), resolver)) {
       return promise;
