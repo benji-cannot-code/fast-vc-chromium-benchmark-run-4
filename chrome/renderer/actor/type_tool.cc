e61ec12707faec544cb58d6539f719882ece19e4
David Bokan <bokan@chromium.org>
2025-06-03 12:37:34 -0700

    Add move constructor to TypeTool::TargetAndKeys
    Errata for https://crrev.com/c/6602273
    
    Since we provide a copy constructor the move constructor needs to be
    explicit as well.
    
    Change-Id: Ie6e6af01d780092526e0250a985dce98db9cd8e3
    Bug: 409558980
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6616385
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468926}

--- chrome/renderer/actor/type_tool.cc
+++ chrome/renderer/actor/type_tool.cc
@@ -128,6 +128,9 @@ TypeTool::TargetAndKeys::~TargetAndKeys() = default;
 TypeTool::TargetAndKeys::TargetAndKeys(const TargetAndKeys&) = default;
 TypeTool::TargetAndKeys& TypeTool::TargetAndKeys::operator=(
     const TargetAndKeys&) = default;
+TypeTool::TargetAndKeys::TargetAndKeys(TargetAndKeys&&) = default;
+TypeTool::TargetAndKeys& TypeTool::TargetAndKeys::operator=(TargetAndKeys&&) =
+    default;
 
 TypeTool::KeyParams::KeyParams() = default;
 TypeTool::KeyParams::~KeyParams() = default;
