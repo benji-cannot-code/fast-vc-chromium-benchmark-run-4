aab19833c38a02f9b6d742f08703652b9e7726b4
Akihiro Ota <akihiroota@chromium.org>
2025-06-03 11:58:59 -0700

    Third party: remove ChromeVox closure files
    There is a ChromeVox-specific checkout of closure, which is no longer
    needed after this CL [1]. Here is the thread [2] asking us to remove
    the unused files.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6469376
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/6469376/13/chrome/browser/resources/chromeos/accessibility/chromevox/mv2/BUILD.gn#b188
    
    Bug: N/A
    Change-Id: I2d7a5333d7b28289e196e094e5c57fd0e82dcce4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533498
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468904}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/testing/fake_dom.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/testing/fake_dom.js
@@ -7,8 +7,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 window = {};
 window.setTimeout = () => {};
 globalThis = window;
-goog = {};
-goog.provide = () => {};
-goog.require = () => {};
-goog.addDependency = () => {};
-goog.isDef = val => val !== undefined;
