2bffa2045f6db2cbee4b5f8ba2b830730cce69dd
Tomek Jurkiewicz <tju@google.com>
2025-06-03 10:10:22 -0700

    Lazy load the SupervisedUserURLFilter inside the ClassifyUrlNavigationThrottle
    This reduces the count of references to components of the supervised
    user service.
    
    Change-Id: Iba4c8ec342eb4c5040b55a927bfa4d37cb667c4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6616828
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1468809}

--- chrome/browser/supervised_user/classify_url_navigation_throttle.cc
+++ chrome/browser/supervised_user/classify_url_navigation_throttle.cc
@@ -137,7 +137,7 @@ void ClassifyUrlNavigationThrottle::CheckURL() {
   ClassifyUrlCheckList::Key key = list_.NewCheck();
 
   if (navigation_handle()->IsInPrimaryMainFrame()) {
-    url_filter_->GetFilteringBehaviorWithAsyncChecks(
+    url_filter()->GetFilteringBehaviorWithAsyncChecks(
         url,
         base::BindOnce(&ClassifyUrlNavigationThrottle::OnURLCheckDone,
                        weak_ptr_factory_.GetWeakPtr(), key),
@@ -146,7 +146,7 @@ void ClassifyUrlNavigationThrottle::CheckURL() {
         FilteringContext::kNavigationThrottle,
         navigation_handle()->GetPageTransition());
   } else {
-    url_filter_->GetFilteringBehaviorForSubFrameWithAsyncChecks(
+    url_filter()->GetFilteringBehaviorForSubFrameWithAsyncChecks(
         url, navigation_handle()->GetWebContents()->GetVisibleURL(),
         base::BindOnce(&ClassifyUrlNavigationThrottle::OnURLCheckDone,
                        weak_ptr_factory_.GetWeakPtr(), key),
@@ -262,6 +262,13 @@ const GURL& ClassifyUrlNavigationThrottle::currently_navigated_url() const {
   return navigation_handle()->GetURL();
 }
 
+SupervisedUserURLFilter* ClassifyUrlNavigationThrottle::url_filter() const {
+  return SupervisedUserServiceFactory::GetForProfile(
+             Profile::FromBrowserContext(
+                 navigation_handle()->GetWebContents()->GetBrowserContext()))
+      ->GetURLFilter();
+}
+
 void MaybeCreateAndAddClassifyUrlNavigationThrottle(
     content::NavigationThrottleRegistry& registry) {
   Profile* profile = Profile::FromBrowserContext(
@@ -282,13 +289,10 @@ void MaybeCreateAndAddClassifyUrlNavigationThrottle(
     return;
   }
 
-  SupervisedUserURLFilter* filter = supervised_user_service->GetURLFilter();
-  CHECK(filter) << "Supervised user service for child users is expected to "
-                   "have the URL filter present";
   base::UmaHistogramEnumeration(
       kClassifyUrlThrottleUseCaseHistogramName,
       ClassifyUrlThrottleUseCase::kFamilyLinkSupervisedUser);
-  ClassifyUrlNavigationThrottle::CreateAndAdd(registry, filter);
+  ClassifyUrlNavigationThrottle::CreateAndAdd(registry);
 }
 
 std::optional<ClassifyUrlNavigationThrottle::ThrottleCheckResult>
@@ -356,11 +360,9 @@ void ClassifyUrlNavigationThrottle::CancelDeferredNavigation(
 }
 
 void ClassifyUrlNavigationThrottle::CreateAndAdd(
-    content::NavigationThrottleRegistry& registry,
-    SupervisedUserURLFilter* url_filter) {
+    content::NavigationThrottleRegistry& registry) {
   registry.AddThrottle(
-      base::WrapUnique(
-      new ClassifyUrlNavigationThrottle(registry, url_filter)));
+      base::WrapUnique(new ClassifyUrlNavigationThrottle(registry)));
 }
 
 const char* ClassifyUrlNavigationThrottle::GetNameForLogging() {
@@ -368,9 +370,8 @@ const char* ClassifyUrlNavigationThrottle::GetNameForLogging() {
 }
 
 ClassifyUrlNavigationThrottle::ClassifyUrlNavigationThrottle(
-    content::NavigationThrottleRegistry& registry,
-    SupervisedUserURLFilter* url_filter)
-    : content::NavigationThrottle(registry), url_filter_(url_filter) {}
+    content::NavigationThrottleRegistry& registry)
+    : content::NavigationThrottle(registry) {}
 ClassifyUrlNavigationThrottle::~ClassifyUrlNavigationThrottle() = default;
 
 ClassifyUrlNavigationThrottle::ClassifyUrlCheckList::ClassifyUrlCheckList() =
