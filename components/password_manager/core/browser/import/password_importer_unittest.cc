fe1bb6499a0135f7bace16ab2774680b832b5d3e
Alexis Hetu <sugoi@chromium.org>
2025-06-03 14:16:39 -0700

    [safari-import] Force user confirmation when importing passwords
    This CL adds a setting to PasswordImporter which forces the importer
    to wait for user confirmation before executing the password import.
    
    Also, when waiting for user confirmation, the password importer now
    sends the number of passwords to import as part of the information
    sent to the results callback.
    
    See go/bling-import for details.
    
    Bug: 407587751
    Change-Id: I67390c937ef5d6e0365e32a6992054d29b7c2d59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612982
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468953}

--- components/password_manager/core/browser/import/password_importer_unittest.cc
+++ components/password_manager/core/browser/import/password_importer_unittest.cc
@@ -106,7 +106,7 @@ class PasswordImporterTest : public testing::Test {
   }
 
   void AssertConflictsState() {
-    ASSERT_TRUE(importer_.IsState(PasswordImporter::kConflicts));
+    ASSERT_TRUE(importer_.IsState(PasswordImporter::kUserInteractionRequired));
   }
 
   void ContinueImportAndWaitForCompletion(
