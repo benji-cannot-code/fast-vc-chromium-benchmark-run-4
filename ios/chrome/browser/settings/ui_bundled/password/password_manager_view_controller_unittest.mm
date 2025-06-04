32b266ea556cf01b873e437cc1c1dd1f1b3b02a7
Yurii Lahodiuk <ylahodiuk@google.com>
2025-06-04 05:46:56 -0700

    [iOS] Update the Trusted Vault widget promo image
    In the scope of this change we are updating the Trusted Vault widget
    promo image according to the recommendations of UX/UI.
    
    Screenshots:
    
    1) Light mode:
    - Enabled: https://drive.google.com/file/d/1Kx5SK768ZXOU7sF6ZzcRfgVlJ1zi-awB/view?usp=sharing&resourcekey=0--3KP0t-DS50s5WwkQK5Flg
    - Disabled: https://drive.google.com/file/d/1aLKjQ9O2JkY2ZZ0YKZGj_nyhrNftRSYU/view?usp=sharing&resourcekey=0-nWQa69vfYx7y_1BjlGpV-A
    
    2) Dark mode:
    - Enabled: https://drive.google.com/file/d/1_dbPH3xYjpA6eAj9QvTF2jKGJqwAFmcm/view?usp=sharing&resourcekey=0-Jy9beqBv5_1NrE-msSYc6g
    - Disabled: https://drive.google.com/file/d/1lEx6hOyG37HcNcjAuzdtCVHM5m7q9LhP/view?usp=sharing&resourcekey=0-n78cYL-jpIgIMkvszldCfA
    
    Bug: 407605858
    Change-Id: I35c1d70fea4a9fc679f5ae7e2c14d2ef2c3321b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6614864
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469232}

--- ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/legacy_chrome_table_view_controller_test.h"
@@ -1360,7 +1361,9 @@ TEST_F(PasswordManagerViewControllerTest,
   InlinePromoItem* item = static_cast<InlinePromoItem*>(GetTableViewItem(
       GetSectionIndex(SectionIdentifierTrustedVaultWidgetPromo), 0));
 
-  EXPECT_NSEQ(item.promoImage, [UIImage imageNamed:WidgetPromoImageName()]);
+  EXPECT_NSEQ(
+      item.promoImage,
+      [UIImage imageNamed:kPasswordManagerTrustedVaultWidgetPromoImage]);
   EXPECT_NSEQ(
       item.promoText,
       l10n_util::GetNSStringF(
@@ -1375,8 +1378,10 @@ TEST_F(PasswordManagerViewControllerTest,
 
   SetEditing(true);
   EXPECT_FALSE(item.enabled);
-  EXPECT_NSEQ(item.promoImage,
-              [UIImage imageNamed:WidgetPromoDisabledImageName()]);
+  EXPECT_NSEQ(
+      item.promoImage,
+      [UIImage
+          imageNamed:kPasswordManagerTrustedVaultWidgetPromoDisabledImage]);
   SetEditing(false);
 
   [GetPasswordManagerViewController() settingsWillBeDismissed];
