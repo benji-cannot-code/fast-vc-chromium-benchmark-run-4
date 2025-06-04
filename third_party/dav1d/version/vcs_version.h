2fb1abeb16ed6c83012faf918e0d7236032fa768
Wan-Teh Chang <wtc@google.com>
2025-06-04 14:20:24 -0700

    Roll src/third_party/dav1d/libdav1d/ fe0ab5146..63bf075aa (1 commit)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/fe0ab5146024..63bf075aada9
    
    $ git log fe0ab5146..63bf075aa --date=short --no-merges --format='%ad %ae %s'
    2025-06-02 gramner recon: Fix level index calculation optimization for 2D transforms
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Ie71aa2236d8722b66b4c9b20fc473cab9e4d37ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6621922
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469618}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.1-9-gfe0ab51"
+#define DAV1D_VERSION "1.5.1-10-g63bf075"
