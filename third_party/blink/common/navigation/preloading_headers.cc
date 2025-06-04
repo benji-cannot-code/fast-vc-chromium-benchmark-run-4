4c18d64bce04ce0a3f701f1a4229a931a6ba36c0
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-06-03 18:00:05 -0700

    Introduce IsSecPurposeForPrefetch
    This CL adds a helper method `IsSecPurposeForPrefetch` to check if
    `Sec-Purpose` request header set in Chromium is for prefetch. It is
    collocated with `Sec-Purpose` header value definitions to help kept
    consistent when new header values are added in the future.
    
    Preparation for
    https://chromium-review.googlesource.com/c/chromium/src/+/6607183
    
    Bug: 420603369
    Change-Id: I413c0615df1be9bb60b33edecc012ddbebffb5dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6616000
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469073}

---
+++ third_party/blink/common/navigation/preloading_headers.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/public/common/navigation/preloading_headers.h"
+
+namespace blink {
+
+bool IsSecPurposeForPrefetch(std::optional<std::string> sec_purpose_header) {
+  // All values used in Chromium and defined in `preloading_headers.h` start
+  // with "prefetch".
+  return sec_purpose_header && sec_purpose_header->starts_with("prefetch");
+}
+
+}  // namespace blink
