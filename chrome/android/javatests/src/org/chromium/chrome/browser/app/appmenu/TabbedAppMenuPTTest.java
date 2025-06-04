c0f8f0407dafe1c942eff2d72b989770e6b8452c
Yanling <yanlinghu@google.com>
2025-06-04 07:52:02 -0700

    [NTP Customization] Display customization entry only on NTP menu
    Previously, the NTP Customization entry appeared on both NTP and non-NTP
    menus.
    
    This CL removes the entry from non-NTP menus, meaning it will no longer
    display in Incognito, native pages, or web pages, as shown in the video
    below:
    https://drive.google.com/file/d/1rAkFsohf9F-lacNemcHRTNa5W3RecmAU/view?usp=sharing&resourcekey=0-dDs17N3T7a0mAA3GcumnFw
    
    Bug: 421968090
    Change-Id: I02c371be6f191d3991457b5f1f1ad94e9442fa0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6614179
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469273}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuPTTest.java
@@ -152,7 +152,7 @@ public class TabbedAppMenuPTTest {
         WebPageStation blankPage = mCtaTestRule.startOnBlankPage();
         RegularWebPageAppMenuFacility menu = blankPage.openRegularTabAppMenu();
 
-        mRenderTestRule.render(menu.menuListElement.get(), "regular_webpage_app_menu_v2");
+        mRenderTestRule.render(menu.menuListElement.get(), "regular_webpage_app_menu_v3");
         menu.verifyPresentItems();
         assertFinalDestination(blankPage, menu);
 
