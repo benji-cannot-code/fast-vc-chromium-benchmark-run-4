b2b2a26d3ffd2ace876f11c9763b1e34c7564196
Arthur Milchior <arthurmilchior@google.com>
2025-06-04 07:12:02 -0700

    [iOS]Ensure that OCMock are verified
    As of june 2025, hundreds of tests uses Mock without verifying
    them. While GMock don’t require verification, OCMock does. Those CL
    correct the tests if they are easy to correct and otherwise disable
    them.
    
    This CL was uploaded by git cl split.
    
    R=slobodan@chromium.org
    
    Bug: 421873301
    Change-Id: I62c7668e1599ca1505fd71138c644a60975e7983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620945
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469249}

--- ios/chrome/browser/autofill/ui_bundled/authentication/otp_input_dialog_mediator_unittest.mm
+++ ios/chrome/browser/autofill/ui_bundled/authentication/otp_input_dialog_mediator_unittest.mm
@@ -66,6 +66,11 @@ class OtpInputDialogMediatorTest
         model_controller_->GetImplWeakPtr(), delegate_);
   }
 
+  ~OtpInputDialogMediatorTest() {
+    EXPECT_OCMOCK_VERIFY(consumer_);
+    EXPECT_OCMOCK_VERIFY(delegate_);
+  }
+
   id<OtpInputDialogConsumer> consumer_;
   id<OtpInputDialogMediatorDelegate> delegate_;
   testing::NiceMock<MockOtpUnmaskDelegate> unmask_delegate_;
@@ -95,7 +100,8 @@ TEST_P(OtpInputDialogMediatorTest, SetConsumer) {
   EXPECT_OCMOCK_VERIFY((id)consumer_);
 }
 
-TEST_P(OtpInputDialogMediatorTest, DidTapConfirmButton) {
+// TODO(crbug.com/421873301): re-enable.
+TEST_P(OtpInputDialogMediatorTest, DISABLED_DidTapConfirmButton) {
   NSString* otp = @"123456";
   OCMExpect([consumer_ showPendingState]);
   EXPECT_CALL(unmask_delegate_,
@@ -112,7 +118,8 @@ TEST_P(OtpInputDialogMediatorTest, DidTapCancelButton) {
   [mediator_->AsMutator() didTapCancelButton];
 }
 
-TEST_P(OtpInputDialogMediatorTest, OnOtpInputChanges) {
+// TODO(crbug.com/421873301): re-enable
+TEST_P(OtpInputDialogMediatorTest, DISABLED_OnOtpInputChanges) {
   OCMExpect([consumer_ setConfirmButtonEnabled:NO]);
 
   [mediator_->AsMutator() onOtpInputChanges:@"12345"];
