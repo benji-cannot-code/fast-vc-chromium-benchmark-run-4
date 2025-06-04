84763ea586d6a35ba028e805bf9d01e1aa1f708e
Takashi Toyoshima <toyoshim@chromium.org>
2025-06-03 23:23:31 -0700

    NavigationThrottleRunner2: Remove DefaultNavigationThrottle
    This cast specific throttle doesn't seem to be used in the repository.
    
    Bug: 412524375
    Change-Id: I61ad210df8effdf01b3c7adde6d712c60546514f
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm64-rel,fuchsia-x64-cast-receiver-rel,linux-cast-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604276
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469140}

--- chromecast/browser/cast_content_browser_client.cc
+++ chromecast/browser/cast_content_browser_client.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/browser/cast_web_contents.h"
 #include "chromecast/browser/cast_web_preferences.h"
 #include "chromecast/browser/cast_web_service.h"
-#include "chromecast/browser/default_navigation_throttle.h"
 #include "chromecast/browser/devtools/cast_devtools_manager_delegate.h"
 #include "chromecast/browser/general_audience_browsing_navigation_throttle.h"
 #include "chromecast/browser/general_audience_browsing_service.h"
