12f85a1b0ffde38335c18e0e529cf2854eb5d80e
Christian Xu <christianxu@chromium.org>
2025-06-04 08:24:23 -0700

    [iOS] Add fieldtrial config for chromnient on iPad
    Add fieldtrial config for chromnient on iPad and update the tests for
    it.
    
    Change-Id: I79593ef5fc5bc76fae1e555797688bba72ba1572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6616226
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469298}

--- ios/chrome/browser/browser_view/ui_bundled/key_commands_provider_unittest.mm
+++ ios/chrome/browser/browser_view/ui_bundled/key_commands_provider_unittest.mm
@@ -922,7 +922,8 @@ TEST_F(KeyCommandsProviderTest, BackForward) {
       web_state->GetNavigationManager();
   int initial_index = navigation_manager->GetLastCommittedItemIndex();
 
-  if (IsLensOverlayAvailable(profile_->GetPrefs())) {
+  if (IsLensOverlayAvailable(profile_->GetPrefs()) &&
+      IsLensOverlaySameTabNavigationEnabled(profile_->GetPrefs())) {
     OCMExpect([mock_page_side_swipe_commands_handler_
         navigateBackWithSideSwipeAnimationIfNeeded]);
   }
@@ -930,7 +931,8 @@ TEST_F(KeyCommandsProviderTest, BackForward) {
   [provider_ keyCommand_back];
   EXPECT_EQ(navigation_manager->GetLastCommittedItemIndex(), initial_index - 1);
 
-  if (IsLensOverlayAvailable(profile_->GetPrefs())) {
+  if (IsLensOverlayAvailable(profile_->GetPrefs()) &&
+      IsLensOverlaySameTabNavigationEnabled(profile_->GetPrefs())) {
     OCMExpect([mock_page_side_swipe_commands_handler_
         navigateBackWithSideSwipeAnimationIfNeeded]);
   }
@@ -944,7 +946,8 @@ TEST_F(KeyCommandsProviderTest, BackForward) {
   [provider_ keyCommand_forward];
   EXPECT_EQ(navigation_manager->GetLastCommittedItemIndex(), initial_index);
 
-  if (IsLensOverlayAvailable(profile_->GetPrefs())) {
+  if (IsLensOverlayAvailable(profile_->GetPrefs()) &&
+      IsLensOverlaySameTabNavigationEnabled(profile_->GetPrefs())) {
     EXPECT_OCMOCK_VERIFY(mock_page_side_swipe_commands_handler_);
   }
 }
