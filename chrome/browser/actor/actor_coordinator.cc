fbe525ee3160c8af011e51998d069455a13f5c5e
Kevin Graney <kmg@google.com>
2025-06-04 09:08:36 -0700

    Add a UMA histogram to record ActorResultCode
    BUG=390211362
    
    Change-Id: Ia201dcc8ec1fadad2e946f6705a3e8d045a1d335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6608323
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469344}

--- chrome/browser/actor/actor_coordinator.cc
+++ chrome/browser/actor/actor_coordinator.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/notimplemented.h"
 #include "base/types/id_type.h"
 #include "chrome/browser/actor/browser_action_util.h"
@@ -47,6 +48,8 @@ namespace {
 
 void PostTaskForActCallback(ActorCoordinator::ActionResultCallback callback,
                             mojom::ActionResultPtr result) {
+  UMA_HISTOGRAM_ENUMERATION("Actor.ActorCoordinator.Action.ResultCode",
+                            result->code);
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), std::move(result)));
 }
--- chrome/browser/actor/actor_coordinator_unittest.cc
+++ chrome/browser/actor/actor_coordinator_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/actor/actor_test_util.h"
@@ -30,8 +31,9 @@ namespace actor {
 using ::optimization_guide::proto::BrowserAction;
 
 namespace {
-
 constexpr int kFakeContentNodeId = 123;
+constexpr char kActionResultHistogram[] =
+    "Actor.ActorCoordinator.Action.ResultCode";
 
 class FakeChromeRenderFrame : public chrome::mojom::ChromeRenderFrame {
  public:
@@ -128,6 +130,8 @@ class ActorCoordinatorTest : public ChromeRenderViewHostTestHarness {
   void AssociateTabInterface() { tab_state_.emplace(web_contents()); }
   void ClearTabInterface() { tab_state_.reset(); }
 
+  base::HistogramTester histograms_;
+
  private:
   struct TabState {
     explicit TabState(content::WebContents* web_contents) : weak_factory(&tab) {
@@ -149,6 +153,8 @@ TEST_F(ActorCoordinatorTest, ActSucceedsOnSupportedUrl) {
       Act(GURL("http://localhost/"), base::BindLambdaForTesting([this]() {
             return MakeClick(*main_rfh(), kFakeContentNodeId);
           })));
+  histograms_.ExpectUniqueSample(kActionResultHistogram,
+                                 mojom::ActionResultCode::kOk, 1);
 }
 
 TEST_F(ActorCoordinatorTest, ActFailsOnUnsupportedUrl) {
@@ -175,6 +181,8 @@ TEST_F(ActorCoordinatorTest, ActFailsWhenTabDestroyed) {
   DeleteContents();
 
   ExpectErrorResult(result, mojom::ActionResultCode::kTabWentAway);
+  histograms_.ExpectUniqueSample(kActionResultHistogram,
+                                 mojom::ActionResultCode::kTabWentAway, 1);
 }
 
 TEST_F(ActorCoordinatorTest, CrossOriginNavigationBeforeAction) {
@@ -197,6 +205,9 @@ TEST_F(ActorCoordinatorTest, CrossOriginNavigationBeforeAction) {
   // TODO(mcnee): We currently just fail, but this should do something more
   // graceful.
   ExpectErrorResult(result, mojom::ActionResultCode::kCrossOriginNavigation);
+  histograms_.ExpectUniqueSample(
+      kActionResultHistogram, mojom::ActionResultCode::kCrossOriginNavigation,
+      1);
 }
 
 }  // namespace
