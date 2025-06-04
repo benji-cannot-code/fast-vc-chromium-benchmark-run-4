808c969a8e5e52325aca1a6b982686214f36207b
Stephen Nusko <nuskos@chromium.org>
2025-06-04 00:22:11 -0700

    Fix up a couple issues in remove-unneeded-pragmas
    Apparently not using is_official_build in android-binary-size causes
    different code to be linked so we don't properly build and notice some
    errors.
    
    Fix typo in win_chromium_compile_dbg_ng 'x64' should be 'x86' this
    caused it to fail to properly build some code hidden behind macros.
    
    Remove cast linux never got it compiling locally.
    
    Fix a typo in the function name and variable passed that were edited in
    the final patchset of the prior CL.
    
    Change-Id: Ia59e9697c14db3b57dbaf0a61869172e25b9477d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619227
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469157}

--- tools/clang/spanify/gnconfigs.py
+++ tools/clang/spanify/gnconfigs.py
@@ -84,36 +84,6 @@ class GnConfigsImpl:
             'target_cpu="x64"',
             'target_os="linux"',
         ] + current_exec
-        self.linux_configs["linux-cast-arm-rel"] = [
-            'cast_is_debug=false',
-            'cast_streaming_enable_remoting=true',
-            'chrome_pgo_phase=0',
-            'dcheck_always_on=false',
-            'enable_arcore=false',
-            'enable_cardboard=false',
-            'enable_cast_receiver=true',
-            'enable_extensions=true',
-            'enable_guest_view=true',
-            'enable_openxr=false',
-            'enable_pdf=true',
-            'enable_plugins=true',
-            'enable_printing=true',
-            'enable_vr=false',
-            'enable_widevine=true',
-            'ffmpeg_branding="Chrome"',
-            'is_cast_desktop_build=true',
-            'is_castos=true',
-            'is_clang=true',
-            'is_component_build=false',
-            'is_debug=false',
-            'media_use_ffmpeg=true',
-            'proprietary_codecs=true',
-            'symbol_level=1',
-            'target_cpu="arm"',
-            'target_os="linux"',
-            'use_chromecast_cdms=false',
-            'use_dummy_lastchange=false',
-        ] + current_exec
         # Finally Official linux builder args
         # See https://ci.chromium.org/ui/p/chrome/builders/official/linux64
         self.linux_configs["linux-official"] = [
@@ -185,7 +155,7 @@ class GnConfigsImpl:
             'is_debug=true',
             'proprietary_codecs=true',
             'symbol_level=0',
-            'target_cpu="x64"',
+            'target_cpu="x86"',
             'target_os="win"',
         ] + current_exec
 
@@ -200,8 +170,7 @@ class GnConfigsImpl:
             #'android_static_analysis="off"', gn warns this has no effect.
             # Coverage file is removed
             'dcheck_always_on=true',
-            # For local builds this generates a warning saying it has no effect.
-            #'debuggable_apks=false',
+            'debuggable_apks=false',
             'enable_android_secondary_abi=true',
             'fail_on_android_expectations=true',
             'ffmpeg_branding="Chrome"',
@@ -212,7 +181,7 @@ class GnConfigsImpl:
             'symbol_level=0',
             'system_webview_package_name="com.google.android.chrome"',
             'target_cpu="arm64"',
-            'use_clange_coverage=true',
+            'use_clang_coverage=true',
         ] + current_exec
         self.android_configs['android-binary-size'] = [
             'android_channel="stable"',
@@ -265,7 +234,7 @@ class GnConfigsImpl:
             'is_component_build=false',
             'is_debug=false',
             'is_high_end_android=false',
-            #'is_official_build=true', Disabled to save compile time.
+            'is_official_build=true',
             'proprietary_codecs=true',
             # locally 2 generates to many debug symbols.
             'symbol_level=0',
@@ -305,7 +274,7 @@ class GnConfigsImpl:
             'is_chrome_branded=true',
             #'is_chromeos_device=true', Not building on a chromeos device.
             'is_debug=false',
-            # 'is_official_build=true', Disabled for build speed
+            'is_official_build=true',
             'use_cfi_cast=true',
             'enable_nacl=true',
             'enable_pseudolocales=true',
