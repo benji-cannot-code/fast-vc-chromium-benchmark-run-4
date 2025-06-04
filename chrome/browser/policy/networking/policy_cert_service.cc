afd1c736b99c830a6bf3905ea6a66af12396e6e3
Andrew Williams <awillia@chromium.org>
2025-06-04 09:11:10 -0700

    Add ProfileNetworkContextService::Shutdown and cleanup state
    ProfileNetworkContextService should remove itself as a content_settings
    observer when it's shutting down to avoid the case where
    HostContentSettingsMap mistakenly calls into a
    ProfileNetworkContextService instance after it has been deleted.
    
    This may be related to suspected
    network::CookieSettings::set_content_settings UaFs / memory corruption
    we are seeing, and/or HostContentSettingsMap destruction shutdown hangs
    
    Bug: 414863913, 383931513
    Change-Id: I52240394552df502341b5a427fe56ba48f9a0a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598421
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469348}

--- chrome/browser/policy/networking/policy_cert_service.cc
+++ chrome/browser/policy/networking/policy_cert_service.cc
@@ -28,9 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace policy {
 
-PolicyCertService::~PolicyCertService() {
-  StopListeningToPolicyCertificateProvider();
-}
+PolicyCertService::~PolicyCertService() = default;
 
 PolicyCertService::PolicyCertService(
     Profile* profile,
@@ -39,8 +37,8 @@ PolicyCertService::PolicyCertService(
     : profile_(profile),
       policy_certificate_provider_(policy_certificate_provider),
       may_use_profile_wide_trust_anchors_(may_use_profile_wide_trust_anchors) {
-  DCHECK(policy_certificate_provider_);
-  DCHECK(profile_);
+  CHECK(policy_certificate_provider_);
+  CHECK(profile_);
 }
 
 PolicyCertService::PolicyCertService(Profile* profile)
@@ -62,7 +60,19 @@ void PolicyCertService::StartObservingCertChanges(
   profile_wide_trust_anchors_ = GetAllowedProfileWideTrustAnchors();
 }
 
+void PolicyCertService::StopObservingCertChanges() {
+  // If `on_policy_provided_certs_changed_callback_` is not set then the
+  // observer was never added or has already been removed.
+  if (!on_policy_provided_certs_changed_callback_) {
+    return;
+  }
+  CHECK(policy_certificate_provider_);
+  policy_certificate_provider_->RemovePolicyProvidedCertsObserver(this);
+  on_policy_provided_certs_changed_callback_.Reset();
+}
+
 void PolicyCertService::OnPolicyProvidedCertsChanged() {
+  CHECK(policy_certificate_provider_);
   profile_wide_all_server_and_authority_certs_ =
       policy_certificate_provider_->GetAllServerAndAuthorityCertificates(
           chromeos::onc::CertificateScope::Default());
@@ -87,7 +97,10 @@ void PolicyCertService::OnPolicyProvidedCertsChanged() {
 }
 
 void PolicyCertService::OnPolicyCertificateProviderDestroying() {
-  StopListeningToPolicyCertificateProvider();
+  StopObservingCertChanges();
+  // Set our PolicyCertificateProvider pointer to null so that it doesn't become
+  // dangling.
+  policy_certificate_provider_ = nullptr;
 }
 
 void PolicyCertService::GetPolicyCertificatesForStoragePartition(
@@ -98,6 +111,7 @@ void PolicyCertService::GetPolicyCertificatesForStoragePartition(
       profile_wide_all_server_and_authority_certs_;
   *out_trust_anchors = profile_wide_trust_anchors_;
 
+  CHECK(policy_certificate_provider_);
   if (policy_certificate_provider_->GetExtensionIdsWithPolicyCertificates()
           .empty()) {
     return;
@@ -174,6 +188,7 @@ net::CertificateList PolicyCertService::GetAllowedProfileWideTrustAnchors() {
     return {};
   }
 
+  CHECK(policy_certificate_provider_);
   return policy_certificate_provider_->GetWebTrustedCertificates(
       chromeos::onc::CertificateScope::Default());
 }
@@ -192,14 +207,6 @@ void PolicyCertService::SetPolicyTrustAnchorsForTesting(
 
   profile_wide_all_server_and_authority_certs_ = trust_anchors;
   profile_wide_trust_anchors_ = trust_anchors;
-}
-
-void PolicyCertService::StopListeningToPolicyCertificateProvider() {
-  if (!policy_certificate_provider_) {
-    return;
-  }
-  policy_certificate_provider_->RemovePolicyProvidedCertsObserver(this);
-  policy_certificate_provider_ = nullptr;
 }
 
 }  // namespace policy
