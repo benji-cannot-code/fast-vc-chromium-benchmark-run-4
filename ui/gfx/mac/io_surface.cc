2e97b5c72a099197d5505b38ad3951173d398949
Sunny Sachanandani <sunnyps@chromium.org>
2025-06-04 12:15:19 -0700

    [ios blink] Use mach_port_deallocate for IOSurface mach port
    mach_port_deallocate is strictly better than mach_port_mod_refs since it
    can deal with invalid mach ports in addition to changing the ref count - base::ScopedMachSendRight also uses it.
    
    Also includes other minor cleanup in io_surface.h/cc:
    
    - Use the ScopedIOSurface type alias in function return type
    
    - Stop using base::bits::AlignUpDeprecatedDoNotUse
    
    - Remove unnecessary non-null check in IOSurfaceSetColorSpace
    
    These were found while working on a larger CL to workaround IOSurface
    mapping sandbox restrictions on iOS.
    
    Bug: 414330687
    Change-Id: I4cfbd6dd275eddad289e6aae76b78f27f458fc77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619580
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469543}

--- ui/gfx/mac/io_surface.cc
+++ ui/gfx/mac/io_surface.cc
@@ -139,10 +139,9 @@ mach_port_t IOSurfaceMachPortTraits::Retain(mach_port_t port) {
 
 // static
 void IOSurfaceMachPortTraits::Release(mach_port_t port) {
-  kern_return_t kr =
-      mach_port_mod_refs(mach_task_self(), port, MACH_PORT_RIGHT_SEND, -1);
+  kern_return_t kr = mach_port_deallocate(mach_task_self(), port);
   MACH_LOG_IF(ERROR, kr != KERN_SUCCESS, kr)
-      << "IOSurfaceMachPortTraits::Release mach_port_mod_refs";
+      << "IOSurfaceMachPortTraits::Release mach_port_deallocate";
 }
 
 // Common method used by IOSurfaceSetColorSpace and IOSurfaceCanSetColorSpace.
@@ -203,10 +202,8 @@ bool IOSurfaceSetColorSpace(IOSurfaceRef io_surface,
   }
 
   if (color_space_name) {
-    if (io_surface) {
-      IOSurfaceSetValue(io_surface, CFSTR("IOSurfaceColorSpace"),
-                        color_space_name);
-    }
+    IOSurfaceSetValue(io_surface, CFSTR("IOSurfaceColorSpace"),
+                      color_space_name);
     return true;
   }
 
@@ -303,11 +300,11 @@ base::apple::ScopedCFTypeRef<IOSurfaceRef> CreateIOSurface(
     const size_t bytes_per_element = BytesPerElement(format, 0);
     const size_t bytes_per_row = IOSurfaceAlignProperty(
         kIOSurfaceBytesPerRow,
-        base::bits::AlignUpDeprecatedDoNotUse(size.width(), 2) *
+        base::bits::AlignUp(static_cast<size_t>(size.width()), size_t{2}) *
             bytes_per_element);
     const size_t bytes_alloc = IOSurfaceAlignProperty(
         kIOSurfaceAllocSize,
-        base::bits::AlignUpDeprecatedDoNotUse(size.height(), 2) *
+        base::bits::AlignUp(static_cast<size_t>(size.height()), size_t{2}) *
             bytes_per_row);
     AddIntegerValue(properties.get(), kIOSurfaceBytesPerElement,
                     bytes_per_element);
@@ -355,8 +352,7 @@ void IOSurfaceSetColorSpace(IOSurfaceRef io_surface,
   }
 }
 
-COMPONENT_EXPORT(GFX)
-base::apple::ScopedCFTypeRef<IOSurfaceRef> IOSurfaceMachPortToIOSurface(
+ScopedIOSurface IOSurfaceMachPortToIOSurface(
     ScopedRefCountedIOSurfaceMachPort io_surface_mach_port) {
   base::apple::ScopedCFTypeRef<IOSurfaceRef> io_surface;
   if (!io_surface_mach_port) {
