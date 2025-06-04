5c2494c11953436e1d58fa9835af220de2e807a1
Noémie St-Onge <noemies@google.com>
2025-06-04 09:28:01 -0700

    [iOSPasswordRecovery] Add feature flag
    This CL adds a feature flag to be used for the password recovery
    autofill feature on iOS.
    
    Fixed: 422205880
    Change-Id: Id686b2350113252287852fc58421371099086fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6617101
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469369}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -94,6 +94,10 @@ BASE_FEATURE(kIOSPasswordBottomSheetV2,
 BASE_FEATURE(kIOSProactivePasswordGenerationBottomSheet,
              "kIOSProactivePasswordGenerationBottomSheet",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kIOSFillRecoveryPassword,
+             "IOSFillRecoveryPassword",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif  // IS_IOS
 
 BASE_FEATURE(kPasswordFormGroupedAffiliations,
