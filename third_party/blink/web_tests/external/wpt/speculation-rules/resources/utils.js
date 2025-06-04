8d39ce1e10f4912a5b9f29943c7477d03a9e5799
Domenic Denicola <domenic@chromium.org>
2025-06-03 22:31:27 -0700

    Refactor preloading remote context helpers
    This is part of an effort to share more test infrastructure between
    prerender and prefetch, for certain tests which should be running for
    both.
    
    * Modify the base RemoteContextHelper class to allow subclasses to choose custom RemoteContextWrappers. This requires moving the RemoteContextHelper declaration below the RemoteContextWrapper declaration, so the diff looks larger, but only two lines were changed.
    
    * Introduce PreloadingRemoteContextWrapper/Helper classes to speculation-rules/resources/utils.js. For now it only has the addPreload() method, but it will be a place to introduce functionality that is common between prefetch and prerender tests.
    
    * Introduce PrerenderingRemoteContextWrapper/Helper classes to speculation-rules/prerender/resources/utils.js. We move the two previously global functions, addPrerenderRC() and activatePrerenderRC(), to this class, with new names addPrerender() and navigateExpectingPrerenderingActivation(). The majority of smaller updates in this CL are due to these methods moving.
    
    * Fix a couple of missing awaits before navigateTo() in the cancel-prerendering-* tests. These are not technically required as long as you await something in the new context afterward, which we did. But that code was fragile.
    
    Bug: 40273170
    Change-Id: Ia2e63830f69a78f3fe688723d43729073fce4df2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6615401
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469125}

--- third_party/blink/web_tests/external/wpt/speculation-rules/resources/utils.js
+++ third_party/blink/web_tests/external/wpt/speculation-rules/resources/utils.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-window.assertSpeculationRulesIsSupported = () => {
+globalThis.assertSpeculationRulesIsSupported = () => {
   assert_implements(
       'supports' in HTMLScriptElement,
       'HTMLScriptElement.supports must be supported');
@@ -7,3 +7,49 @@ window.assertSpeculationRulesIsSupported = () => {
       HTMLScriptElement.supports('speculationrules'),
       '<script type="speculationrules"> must be supported');
 };
+
+// If you want access to these, be sure to include
+// /html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js.
+// So as to avoid requiring everyone to do that, we only conditionally define this infrastructure.
+if (globalThis.RemoteContextHelper) {
+  class PreloadingRemoteContextWrapper extends RemoteContextHelper.RemoteContextWrapper {
+    /**
+    * Starts preloading a page with this `PreloadingRemoteContextWrapper` as the
+    * referrer, using `<script type="speculationrules">`.
+    *
+    * @param {"prefetch"|"prerender"} preloadType - The preload type to use.
+    * @param {object} [extrasInSpeculationRule] - Additional properties to add
+    *     to the speculation rule JSON.
+    * @param {RemoteContextConfig|object} [extraConfig] - Additional remote
+    *     context configuration for the preloaded context.
+    * @returns {Promise<PreloadingRemoteContextWrapper>}
+    */
+    addPreload(preloadType, { extrasInSpeculationRule = {}, ...extraConfig } = {}) {
+      const referrerRemoteContext = this;
+
+      return this.helper.createContext({
+        executorCreator(url) {
+          return referrerRemoteContext.executeScript((url, preloadType, extrasInSpeculationRule) => {
+            const script = document.createElement("script");
+            script.type = "speculationrules";
+            script.textContent = JSON.stringify({
+              [preloadType]: [
+                {
+                  source: "list",
+                  urls: [url],
+                  ...extrasInSpeculationRule
+                }
+              ]
+            });
+            document.head.append(script);
+          }, [url, preloadType, extrasInSpeculationRule]);
+        },
+        extraConfig
+      });
+    }
+  }
+
+  globalThis.PreloadingRemoteContextHelper = class extends RemoteContextHelper {
+    static RemoteContextWrapper = PreloadingRemoteContextWrapper;
+  };
+}
