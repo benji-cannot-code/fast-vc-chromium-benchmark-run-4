4947028dd6db9b651c91cf80d86359b6e8f89717
Kartar Singh <kartarsingh@google.com>
2025-06-04 09:21:37 -0700

    [InputVizard] Do not try creating child surface if parent is null
    We had received some crashes from field where the clients are crashing
    with `nullptr passed as parent argument`, which seems to be happening
    when parent can be nullptr. This is coming only from handful of clients.
    In this change we are just early returning from the method and log in
    uma if parent surface was null for some reason.
    
    Bug: 40073296
    Change-Id: Ia62c731565d43930505b5da74218d6fa39fdcffb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6616621
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469361}

--- components/viz/service/input/input_manager.cc
+++ components/viz/service/input/input_manager.cc
@@ -96,7 +96,7 @@ constexpr char kStateProcessingResultHistogram[] =
 enum class CreateAndroidInputReceiverResult {
   kSuccessfullyCreated = 0,
   kFailedUnknown = 1,
-  kFailedNullSurfaceControl = 2,
+  // kFailedNullSurfaceControl = 2,
   kFailedNullLooper = 3,
   kFailedNullInputTransferToken = 4,
   kFailedNullCallbacks = 5,
@@ -107,7 +107,9 @@ enum class CreateAndroidInputReceiverResult {
   kRootCompositorFrameSinkDestroyed = 10,
   kFailedChoreographerNotSupported = 11,
   kFailedNullChoreographer = 12,
-  kMaxValue = kFailedNullChoreographer,
+  kFailedNullParentSurfaceControl = 13,
+  kFailedNullChildSurfaceControl = 14,
+  kMaxValue = kFailedNullChildSurfaceControl,
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/android/enums.xml:CreateAndroidInputReceiverResult)
 
@@ -779,13 +781,20 @@ void InputManager::CreateOrReuseAndroidInputReceiver(
     return;
   }
 
+  if (!parent_input_surface->surface()) {
+    UMA_HISTOGRAM_ENUMERATION(
+        kInputReceiverCreationResultHistogram,
+        CreateAndroidInputReceiverResult::kFailedNullParentSurfaceControl);
+    return;
+  }
+
   scoped_refptr<gfx::SurfaceControl::Surface> input_surface =
       base::MakeRefCounted<gfx::SurfaceControl::Surface>(*parent_input_surface,
                                                          kInputSCName);
-  if (!parent_input_surface->surface() || !input_surface->surface()) {
+  if (!input_surface->surface()) {
     UMA_HISTOGRAM_ENUMERATION(
         kInputReceiverCreationResultHistogram,
-        CreateAndroidInputReceiverResult::kFailedNullSurfaceControl);
+        CreateAndroidInputReceiverResult::kFailedNullChildSurfaceControl);
     return;
   }
 
