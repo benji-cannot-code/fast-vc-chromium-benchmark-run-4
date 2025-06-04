b34fad60008cb823a25d729844f34bd50fc2e77e
Ameur Hosni <ameurhosni@google.com>
2025-06-04 04:25:06 -0700

    [IOS][OR] Use text model state in the edit model
    This CL prepares for the edit model removal by moving some of its state
    management in the omniboxTextModel so that it is easier to move logic
    out of the edit model in the future CLs.
    
    Bug: 418975029
    Change-Id: I139207da56ce944732852149e82d0876e46a70cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6611323
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469217}

--- ios/chrome/browser/omnibox/model/omnibox_text_model.mm
+++ ios/chrome/browser/omnibox/model/omnibox_text_model.mm
@@ -5,16 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/omnibox/model/omnibox_text_model.h"
 
-@implementation OmniboxTextModel
+OmniboxTextModel::OmniboxTextModel()
+    : focus_state(OMNIBOX_FOCUS_NONE),
+      user_input_in_progress(false),
+      user_text(u""),
+      focus_resulted_in_navigation(false),
+      just_deleted_text(false),
+      inline_autocompletion(u""),
+      paste_state(OmniboxPasteState::kNone) {}
 
-- (instancetype)init {
-  self = [super init];
-  if (self) {
-    _focusState = OMNIBOX_FOCUS_NONE;
-    _pasteState = OmniboxPasteState::kNone;
-  }
-
-  return self;
-}
-
-@end
+OmniboxTextModel::~OmniboxTextModel() = default;
