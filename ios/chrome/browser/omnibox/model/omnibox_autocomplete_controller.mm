1c507f6815b2c11b810f38ca6d3bcc264f02e994
Christian Xu <christianxu@google.com>
2025-06-04 07:28:59 -0700

    [iOS][OR] Move updateWithSortedResults to private
    Now that the suggestion wrapping has been moved outside of
    OmniboxMediator `updateWithSortedResults` is no longer used outside of
    OmniboxAutocompleteController. This CL moves the method to private.
    
    Bug: None
    Change-Id: I62836d28370a5affc0e41c3c7285a91b26bdee64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620080
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469258}

--- ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
+++ ios/chrome/browser/omnibox/model/omnibox_autocomplete_controller.mm
@@ -183,6 +183,15 @@ using base::UserMetricsAction;
   }
 }
 
+#pragma mark - AutocompleteResultWrapperDelegate
+
+- (void)autocompleteResultWrapper:(AutocompleteResultWrapper*)wrapper
+              didInvalidatePedals:(NSArray<id<AutocompleteSuggestionGroup>>*)
+                                      nonPedalSuggestionsGroups {
+  [self.delegate omniboxAutocompleteController:self
+                    didUpdateSuggestionsGroups:nonPedalSuggestionsGroups];
+}
+
 #pragma mark - Boolean Observer
 
 - (void)booleanDidChange:(id<ObservableBoolean>)observableBoolean {
@@ -332,24 +341,6 @@ using base::UserMetricsAction;
                                   isFirstUpdate:isFirstUpdate];
 }
 
-#pragma mark - OmniboxAutocomplete event
-
-- (void)updateWithSortedResults:(const AutocompleteResult&)results {
-  NSArray<id<AutocompleteSuggestionGroup>>* suggestionGroups =
-      [self.autocompleteResultWrapper wrapAutocompleteResultInGroups:results];
-  [self.delegate omniboxAutocompleteController:self
-                    didUpdateSuggestionsGroups:suggestionGroups];
-}
-
-#pragma mark - AutocompleteResultWrapperDelegate
-
-- (void)autocompleteResultWrapper:(AutocompleteResultWrapper*)wrapper
-              didInvalidatePedals:(NSArray<id<AutocompleteSuggestionGroup>>*)
-                                      nonPedalSuggestionsGroups {
-  [self.delegate omniboxAutocompleteController:self
-                    didUpdateSuggestionsGroups:nonPedalSuggestionsGroups];
-}
-
 #pragma mark - Private
 
 /// Opens a match created outside of autocomplete controller.
@@ -363,6 +354,14 @@ using base::UserMetricsAction;
   OmniboxPopupSelection selection(
       autocompleteController->InjectAdHocMatch(match.value()));
   _omniboxEditModel->OpenSelection(selection, timestamp, disposition);
+}
+
+/// Wraps the suggestions and send them to the delegate.
+- (void)updateWithSortedResults:(const AutocompleteResult&)results {
+  NSArray<id<AutocompleteSuggestionGroup>>* suggestionGroups =
+      [self.autocompleteResultWrapper wrapAutocompleteResultInGroups:results];
+  [self.delegate omniboxAutocompleteController:self
+                    didUpdateSuggestionsGroups:suggestionGroups];
 }
 
 #pragma mark Clipboard match handling
