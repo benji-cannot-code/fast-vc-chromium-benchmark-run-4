bff4a02911a62c28646384cbeb43d5b0999ab287
Salvador Guerrero <salg@google.com>
2025-06-03 18:51:15 -0700

    [Dino game] Migrate horizon class to typescript
    Bug: 373951324
    Change-Id: I43d39f68b56d705a5ad03b85945cb1235087818e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6593856
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469083}

--- components/neterror/resources/dino_game/offline_sprite_definitions.ts
+++ components/neterror/resources/dino_game/offline_sprite_definitions.ts
@@ -57,7 +57,7 @@ export interface ObstacleType {
   frameRate?: number;
 }
 
-interface SpritePositions {
+export interface SpritePositions {
   backgroundEl: SpritePosition;
   cactusLarge: SpritePosition;
   cactusSmall: SpritePosition;
