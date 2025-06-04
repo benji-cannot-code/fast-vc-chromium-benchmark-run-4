1bff2e9234c8fd742304992ff3821b64d610ae00
Piotr Kotynia <piotrkotynia@google.com>
2025-06-04 08:53:29 -0700

    Delete unused sorted_normalized_tokens_ member
    Bug: 40100455
    Change-Id: I996d8b14443ceb439f98e182d4664f32a55290e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618870
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469323}

--- components/autofill/core/browser/data_model/addresses/autofill_structured_address_component.cc
+++ components/autofill/core/browser/data_model/addresses/autofill_structured_address_component.cc
@@ -136,7 +136,6 @@ void AddressComponent::CopyFrom(const AddressComponent& other) {
   if (other.IsValueAssigned()) {
     value_ = other.value_;
     value_verification_status_ = other.value_verification_status_;
-    sorted_normalized_tokens_ = other.sorted_normalized_tokens_;
   } else {
     UnsetValue();
   }
@@ -261,7 +260,6 @@ void AddressComponent::SetValue(std::u16string value,
 void AddressComponent::UnsetValue() {
   value_.reset();
   value_verification_status_ = VerificationStatus::kNoStatus;
-  sorted_normalized_tokens_.reset();
 }
 
 bool AddressComponent::IsSupportedType(FieldType field_type) const {
