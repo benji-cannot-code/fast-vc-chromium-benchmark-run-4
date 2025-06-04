11c114622c19be93fa1f30c10ce6dc52f084d530
Ho Cheung <hocheung@chromium.org>
2025-06-04 09:56:11 -0700

    Replace base::Overloaded with absl::Overload
    We are allowing absl::Overload; we should replace base::Overloaded
    with it and remove.
    
    This CL was uploaded by git cl split.
    
    Bug: 392929829
    Change-Id: Ief5797fe14d5490cd4c237298f11fcbf7bc76919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620159
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Ho Cheung <hocheung@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469402}

--- chrome/updater/certificate_tag.cc
+++ chrome/updater/certificate_tag.cc
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
-#include "base/functional/overloaded.h"
 #include "chrome/updater/certificate_tag_internal.h"
+#include "third_party/abseil-cpp/absl/functional/overload.h"
 #include "third_party/boringssl/src/include/openssl/bytestring.h"
 #include "third_party/boringssl/src/include/openssl/crypto.h"
 
@@ -524,7 +524,7 @@ std::optional<std::vector<uint8_t>> PEBinary::SetTag(
 PEBinary::PEBinary() = default;
 
 bool PEBinary::ParseTag() {
-  return std::visit(base::Overloaded{
+  return std::visit(absl::Overload{
                         [](FailedParse unused) { return false; },
                         [](SuccessfulEmptyParse unused) { return true; },
                         [this](SuccessfulParse tag) {
@@ -978,7 +978,7 @@ std::optional<std::vector<uint8_t>> MSIBinary::SetTag(
 }
 
 bool MSIBinary::ParseTag() {
-  return std::visit(base::Overloaded{
+  return std::visit(absl::Overload{
                         [](FailedParse unused) { return false; },
                         [](SuccessfulEmptyParse unused) { return true; },
                         [this](SuccessfulParse tag) {
