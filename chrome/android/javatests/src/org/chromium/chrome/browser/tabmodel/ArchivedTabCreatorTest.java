1a37cf973361b1b8a2cd02a1eda4138b4d2cbc7a
Henrique Nakashima <hnakashima@chromium.org>
2025-06-05 15:04:59 -0700

    [Android] Migrate .tabmodel to Public Transit rules
    Migrate the tests shallowly to use Public Transit entry points.
    
    Bug: 399877997
    Change-Id: I0253a23d0d666f027c965461367f64d5e0d875b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6605817
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470220}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/ArchivedTabCreatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/ArchivedTabCreatorTest.java
@@ -15,7 +15,6 @@ import androidx.test.filters.MediumTest;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,11 +32,11 @@ import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tab.TabState;
 import org.chromium.chrome.browser.tab.TabStateExtractor;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
-import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
+import org.chromium.chrome.test.transit.AutoResetCtaTransitTestRule;
+import org.chromium.chrome.test.transit.ChromeTransitTestRules;
+import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.net.test.EmbeddedTestServer;
-import org.chromium.net.test.EmbeddedTestServerRule;
 
 /** Tests for ChromeTabCreator. */
 @RunWith(ChromeJUnit4ClassRunner.class)
@@ -45,15 +44,9 @@ import org.chromium.net.test.EmbeddedTestServerRule;
 @Batch(Batch.PER_CLASS)
 @EnableFeatures({ChromeFeatureList.ANDROID_TAB_DECLUTTER_RESCUE_KILLSWITCH})
 public class ArchivedTabCreatorTest {
-    @ClassRule
-    public static ChromeTabbedActivityTestRule sActivityTestRule =
-            new ChromeTabbedActivityTestRule();
-
     @Rule
-    public BlankCTATabInitialStateRule mBlankCTATabInitialStateRule =
-            new BlankCTATabInitialStateRule(sActivityTestRule, false);
-
-    @ClassRule public static EmbeddedTestServerRule sTestServerRule = new EmbeddedTestServerRule();
+    public AutoResetCtaTransitTestRule mActivityTestRule =
+            ChromeTransitTestRules.fastAutoResetCtaActivityRule();
 
     private static final String TEST_PATH = "/chrome/test/data/android/about.html";
 
@@ -61,14 +54,16 @@ public class ArchivedTabCreatorTest {
     private Profile mProfile;
     private ArchivedTabModelOrchestrator mOrchestrator;
     private TabCreator mTabCreator;
+    private WebPageStation mInitialPage;
 
     @Before
     public void setUp() throws Exception {
-        mTestServer = sTestServerRule.getServer();
+        mTestServer = mActivityTestRule.getTestServer();
+        mInitialPage = mActivityTestRule.startOnBlankPage();
         runOnUiThreadBlocking(
                 () -> {
                     mProfile =
-                            sActivityTestRule
+                            mInitialPage
                                     .getActivity()
                                     .getProfileProviderSupplier()
                                     .get()
@@ -86,14 +81,13 @@ public class ArchivedTabCreatorTest {
     @Test
     @MediumTest
     public void testCreateFrozenTab() throws Exception {
-        Tab tab =
-                sActivityTestRule.loadUrlInNewTab(
-                        mTestServer.getURL(TEST_PATH), /* incognito= */ false);
+        WebPageStation testPage = mInitialPage.openFakeLinkToWebPage(mTestServer.getURL(TEST_PATH));
+        Tab tab = testPage.loadedTabElement.get();
         Tab frozenTab =
                 runOnUiThreadBlocking(
                         () -> {
                             TabState state = TabStateExtractor.from(tab);
-                            sActivityTestRule
+                            mActivityTestRule
                                     .getActivity()
                                     .getCurrentTabModel()
                                     .getTabRemover()
--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/ContextMenuLoadUrlParamsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/ContextMenuLoadUrlParamsTest.java
@@ -20,7 +20,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -44,9 +43,10 @@ import org.chromium.chrome.browser.tabmodel.NextTabPolicy.NextTabPolicySupplier;
 import org.chromium.chrome.browser.tabwindow.TabModelSelectorFactory;
 import org.chromium.chrome.browser.tabwindow.WindowId;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
-import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
+import org.chromium.chrome.test.transit.AutoResetCtaTransitTestRule;
+import org.chromium.chrome.test.transit.ChromeTransitTestRules;
+import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.chrome.test.util.browser.contextmenu.ContextMenuUtils;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -59,13 +59,9 @@ import java.util.regex.Pattern;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @Batch(Batch.PER_CLASS)
 public class ContextMenuLoadUrlParamsTest {
-    @ClassRule
-    public static ChromeTabbedActivityTestRule sActivityTestRule =
-            new ChromeTabbedActivityTestRule();
-
     @Rule
-    public BlankCTATabInitialStateRule mBlankCTATabInitialStateRule =
-            new BlankCTATabInitialStateRule(sActivityTestRule, true);
+    public AutoResetCtaTransitTestRule mActivityTestRule =
+            ChromeTransitTestRules.autoResetCtaActivityRule();
 
     private static final String HTML_PATH =
             "/chrome/test/data/android/contextmenu/context_menu_test.html";
@@ -73,6 +69,7 @@ public class ContextMenuLoadUrlParamsTest {
 
     // Load parameters of the last call to openNewTab().
     private static LoadUrlParams sOpenNewTabLoadUrlParams;
+    private WebPageStation mInitialPage;
 
     // Records parameters of calls to TabModelSelector methods and otherwise behaves like
     // TabModelSelectorImpl.
@@ -145,6 +142,7 @@ public class ContextMenuLoadUrlParamsTest {
                 () -> {
                     FirstRunStatus.setFirstRunFlowComplete(true);
                 });
+        mInitialPage = mActivityTestRule.startOnBlankPage();
     }
 
     @After
@@ -164,13 +162,13 @@ public class ContextMenuLoadUrlParamsTest {
     @Feature({"Browser"})
     public void testOpenInNewTabReferrer() throws TimeoutException {
         triggerContextMenuLoad(
-                sActivityTestRule.getTestServer().getURL(HTML_PATH),
+                mActivityTestRule.getTestServer().getURL(HTML_PATH),
                 "testLink",
                 R.id.contextmenu_open_in_new_tab);
 
         assertNotNull(sOpenNewTabLoadUrlParams);
         assertEquals(
-                sActivityTestRule.getTestServer().getURL(HTML_PATH),
+                mActivityTestRule.getTestServer().getURL(HTML_PATH),
                 sOpenNewTabLoadUrlParams.getReferrer().getUrl());
 
         assertNotNull(sOpenNewTabLoadUrlParams.getAdditionalNavigationParams());
@@ -188,7 +186,7 @@ public class ContextMenuLoadUrlParamsTest {
     @Feature({"Browser"})
     public void testOpenInIncognitoTabNoReferrer() throws TimeoutException {
         triggerContextMenuLoad(
-                sActivityTestRule.getTestServer().getURL(HTML_PATH),
+                mActivityTestRule.getTestServer().getURL(HTML_PATH),
                 "testLink",
                 R.id.contextmenu_open_in_incognito_tab);
 
@@ -202,7 +200,7 @@ public class ContextMenuLoadUrlParamsTest {
     @MediumTest
     @Feature({"Browser"})
     public void testOpenInNewTabSanitizeReferrer() throws TimeoutException {
-        String testUrl = sActivityTestRule.getTestServer().getURL(HTML_PATH);
+        String testUrl = mActivityTestRule.getTestServer().getURL(HTML_PATH);
         String[] schemeAndUrl = SCHEME_SEPARATOR_RE.split(testUrl, 2);
         assertEquals(2, schemeAndUrl.length);
         String testUrlUserPass = schemeAndUrl[0] + "://user:pass@" + schemeAndUrl[1];
@@ -213,12 +211,12 @@ public class ContextMenuLoadUrlParamsTest {
 
     private void triggerContextMenuLoad(String url, String openerDomId, int menuItemId)
             throws TimeoutException {
-        sActivityTestRule.loadUrl(url);
-        sActivityTestRule.assertWaitForPageScaleFactorMatch(0.5f);
-        Tab tab = sActivityTestRule.getActivity().getActivityTab();
+        mActivityTestRule.loadUrl(url);
+        mActivityTestRule.assertWaitForPageScaleFactorMatch(0.5f);
+        Tab tab = mActivityTestRule.getActivity().getActivityTab();
         ContextMenuUtils.selectContextMenuItem(
                 InstrumentationRegistry.getInstrumentation(),
-                sActivityTestRule.getActivity(),
+                mActivityTestRule.getActivity(),
                 tab,
                 openerDomId,
                 menuItemId);
