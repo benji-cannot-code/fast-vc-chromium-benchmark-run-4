a0a1e8b0fb0ebf6182cc9a5a01b6777698dd5c38
Peilin Wang <peilinwang@google.com>
2025-06-05 12:20:29 -0700

    Reland "[BCIV] Fix bug where tabstrip disappears during scroll"
    This is a reland of commit 78633658189ddcde448701cf2497fbce5239812b
    
    Original change's description:
    > [BCIV] Fix bug where tabstrip disappears during scroll
    >
    > Currently, when StripLayoutHelperManager submits a frame to update the
    > tabstrip, it passes the offset from the renderer to the compositor. If
    > BCIV is enabled, we check if we're in a scroll, and if we are, we set
    > the pass 0 as the offset to the compositor, and let viz apply the
    > offset. However, it's not correct to check if we're in a scroll. We
    > should be checking for the visibility constraints (as we are doing with
    > other UI elements.)
    >
    > Checking if we're in a scroll was working fine, until a change was made
    > that makes StripLayoutHelperManager submit a frame right after scrolling
    > finished. This updated the offset to -height, which makes the tabstrip
    > invisible, which causes the layer tree to hide the layers for the
    > tabstrip. Then when we scroll back the tabstrip into view, the
    > renderer's offset is applied to -height, instead of 0, which means we
    > won't see the tabstrip on the screen.
    >
    > Bug: 418046470
    > Change-Id: I2fe5ac6d0733bb10b96a502e8269263550aa833e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6614117
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Peilin Wang <peilinwang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1468966}
    
    Bug: 418046470, 422256802
    Change-Id: I8cfe69f497a0c570421a2070f581cd382ffbecbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6617496
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470122}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/HeadlessBrowserControlsStateProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/HeadlessBrowserControlsStateProvider.java
@@ -90,4 +90,9 @@ public class HeadlessBrowserControlsStateProvider implements BrowserControlsStat
     public @ControlsPosition int getControlsPosition() {
         return ControlsPosition.NONE;
     }
+
+    @Override
+    public boolean isVisibilityForced() {
+        return false;
+    }
 }
