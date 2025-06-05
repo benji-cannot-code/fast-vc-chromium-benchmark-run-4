8cf567ef157b012049018569ecba77f89cc2c4dc
Keigo Oka <oka@google.com>
2025-06-05 10:21:53 -0700

    extensions: Disable on managed profiles for real on desktop android
    The previous attempt https://crrev.com/c/6619754 didn't work on the
    first run, because ExtensionRegistrar being a profile-keyed service
    is initialized before the profile is managed, enabling the flag to
    enable extensions.
    
    In this patch we do the check of the managed status in
    ExtensionManagement, addressing the timing issue. Also, with this
    approach all the extensions that have been installed will be also gone.
    
    To keep many tests from failing, we limit the affected users to
    @google.com and @managedchrome.com only.
    
          @managedchrome.com account.
          installed. Then launch chrome with this patch, and confirm
          extensions are gone.
    
    Bug: 422307625
    Test: Forced extensions are not installed, as tested on
    Test: Launch chrome without this patch, confirm extensions are
    Change-Id: Ie03701081ff48a068299b980bf864526a824299e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6622732
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470041}

--- chrome/browser/extensions/extension_management.cc
+++ chrome/browser/extensions/extension_management.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "base/version.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/enterprise/util/managed_browser_utils.h"
 #include "chrome/browser/extensions/cws_info_service.h"
 #include "chrome/browser/extensions/extension_management_constants.h"
 #include "chrome/browser/extensions/extension_management_internal.h"
@@ -61,6 +62,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/permissions/permission_set.h"
 #include "extensions/common/url_pattern.h"
 #include "url/gurl.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "google_apis/gaia/gaia_auth_util.h"
+#endif
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/themes/theme_service.h"
@@ -170,6 +175,25 @@ ManagedInstallationMode ExtensionManagement::GetInstallationMode(
 ManagedInstallationMode ExtensionManagement::GetInstallationMode(
     const ExtensionId& extension_id,
     const std::string& update_url) {
+// Block extensions for managed profiles on Desktop Android. This is
+// temporary until extensions are ready for dogfooding.
+// TODO(crbug.com/422307625): Remove this check once extensions are ready for
+// dogfooding.
+#if BUILDFLAG(IS_ANDROID)
+  if (enterprise_util::IsBrowserManaged(profile_)) {
+    // Disable extensions only for specific managed accounts.
+    // This check keeps many tests from failing.
+    std::string user_name = profile_->GetProfileUserName();
+    // Crude check to avoid passing invalid strings to `ExtractDomainName`.
+    if (base::Contains(user_name, "@")) {
+      std::string domain = gaia::ExtractDomainName(user_name);
+      if (domain == "google.com" || domain == "managedchrome.com") {
+        return ManagedInstallationMode::kRemoved;
+      }
+    }
+  }
+#endif  // BUILDFLAG(IS_ANDROID)
+
   // Check per-extension installation mode setting first.
   auto* setting = GetSettingsForId(extension_id);
   if (setting)
