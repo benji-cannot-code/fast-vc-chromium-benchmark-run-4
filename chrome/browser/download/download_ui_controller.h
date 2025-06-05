47bf8d30f2e1d12a788f1d6e6d9a890ead02e472
Kyra Seevers <kyraseevers@chromium.org>
2025-06-05 09:10:34 -0700

    Fix typo and simplify DownloadUIController description
    This CL fixes a typo (hte->the) and simplifies the class description for
    DownloadUIController.
    
    Change-Id: Iaf93f53d27725c37c2c33243ac4a32b092a83a0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624393
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Kyra Seevers <kyraseevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469987}

--- chrome/browser/download/download_ui_controller.h
+++ chrome/browser/download/download_ui_controller.h
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/download/content/public/all_download_item_notifier.h"
 
-// This class handles the task of observing a single DownloadManager for
-// notifying the UI when a new download should be displayed in the UI.
-// It invokes the OnNewDownloadReady() method of hte Delegate when the
-// target path is available for a new download.
+// This class handles the task of observing a single DownloadManager to
+// notify the UI when a new download should be displayed. It invokes the
+// OnNewDownloadReady() method of the Delegate when the target path is available
+// for a new download.
 class DownloadUIController
     : public download::AllDownloadItemNotifier::Observer {
  public:
