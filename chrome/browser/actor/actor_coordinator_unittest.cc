cba94ac3bcc69126d7339cf7f961784dadc4887c
Kevin McNee <mcnee@chromium.org>
2025-06-05 12:16:50 -0700

    Don't delay ActorCoordinator unit tests
    Skip the observation delay in the unit tests as is done in the browser
    tests.
    
    Bug: None
    Change-Id: I54d18675c365a54970257b42badc9b7e5cca8eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624937
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470116}

--- chrome/browser/actor/actor_coordinator_unittest.cc
+++ chrome/browser/actor/actor_coordinator_unittest.cc
@@ -99,6 +99,10 @@ class ActorCoordinatorTest : public ChromeRenderViewHostTestHarness {
 
     ChromeRenderViewHostTestHarness::SetUp();
 
+    // TODO(crbug.com/409564704): Mock the delay so that tests can run at
+    // reasonable speed. Remove once there is a more permanent approach.
+    OverrideActionObservationDelay(base::TimeDelta());
+
     AssociateTabInterface();
   }
 
