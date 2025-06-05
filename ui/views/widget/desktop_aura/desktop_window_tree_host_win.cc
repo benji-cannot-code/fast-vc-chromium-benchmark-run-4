eb483d210f678cdff1220a2179bf2899d80ee800
Paul Vazquez <paulva@microsoft.com>
2025-06-05 16:15:46 -0700

    Revert "Schedule a browser exit when a SC_CLOSE is received from Windows TaskManager"
    This reverts commit 745982d509317b6c8709b775aded7f6c2aa4954c. We received reports from VisualStudio users that when ending a debug session in a particular window all browser windows close. This is because VS is also sending a SC_CLOSE command, so it is incorrectly interpreted as a TaskManager EndTask action. This was reported in: https://issues.chromium.org/issues/422218337
    
    Bug: 407549281
    Change-Id: I8edf7ef922fb4670d17e9fd791b437ec7cf4f4ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624958
    Commit-Queue: Paul Vazquez <paulva@microsoft.com>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470250}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
@@ -1002,8 +1002,6 @@ void DesktopWindowTreeHostWin::HandleClose() {
   }
 }
 
-void DesktopWindowTreeHostWin::HandleRequestClose() {}
-
 bool DesktopWindowTreeHostWin::HandleCommand(int command) {
   if (Widget* widget = GetWidget(); widget && widget->widget_delegate()) {
     return widget->widget_delegate()->ExecuteWindowsCommand(command);
