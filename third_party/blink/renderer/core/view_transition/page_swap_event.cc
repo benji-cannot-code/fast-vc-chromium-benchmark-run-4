800309639425a04ddcfe2666aaeb356d2439d116
Johannes Henkel <johannes@chromium.org>
2025-06-05 12:33:10 -0700

    page_swap_event.cc: Add include for uuid.h
    The file uses WTF::CreateCanonicalUUIDString via some other include,
    which a change I'm working on is probably removing from there.
    
    Change-Id: I5b074654774dcff8f608b9372fb62fb0437e4c93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624045
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470131}

--- third_party/blink/renderer/core/view_transition/page_swap_event.cc
+++ third_party/blink/renderer/core/view_transition/page_swap_event.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/navigation_api/navigation_api.h"
 #include "third_party/blink/renderer/core/view_transition/dom_view_transition.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/wtf/uuid.h"
 
 namespace blink {
 namespace {
