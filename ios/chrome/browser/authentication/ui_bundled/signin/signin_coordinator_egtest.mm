a37301ffe14cc4f317ecab9320c19b0e6b176b8f
Noémie St-Onge <noemies@google.com>
2025-06-05 08:36:31 -0700

    [iOS] Disable failing SigninCoordinatorTestCase tests on iPad
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-fieldtrial-rel/44317/overview
    
    Bug: 422731851
    Change-Id: I507ac00603dbe50a0d4361855f015b0759df6c13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624412
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Owners-Override: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469971}

--- ios/chrome/browser/authentication/ui_bundled/signin/signin_coordinator_egtest.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/signin_coordinator_egtest.mm
@@ -418,6 +418,11 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
 // Sign-in opened from: tab switcher.
 // Interrupted at: user consent.
 - (void)testDismissSigninFromTabSwitcher {
+  // TODO(crbug.com/422731851): Test is failing on iPad. Re-enable when fixed.
+  if ([ChromeEarlGrey isIPadIdiom]) {
+    EARL_GREY_TEST_DISABLED(@"Failing on iPad.");
+  }
+
   // When Tab Groups is the third panel (i.e. when Tab Group Sync is enabled),
   // Recent Tabs is not reachable from the Tab Grid. So the sign-in flow is not
   // supported with Tab Group Sync enabled.
@@ -433,6 +438,11 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
 // Sign-in opened from: tab switcher.
 // Interrupted at: identity picker.
 - (void)testDismissSigninFromTabSwitcherFromIdentityPicker {
+  // TODO(crbug.com/422731851): Test is failing on iPad. Re-enable when fixed.
+  if ([ChromeEarlGrey isIPadIdiom]) {
+    EARL_GREY_TEST_DISABLED(@"Failing on iPad.");
+  }
+
   // When Tab Groups is the third panel (i.e. when Tab Group Sync is enabled),
   // Recent Tabs is not reachable from the Tab Grid. So the sign-in flow is not
   // supported with Tab Group Sync enabled.
