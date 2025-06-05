2ebb07b2fd987aee7890e6406f17e145fec9d517
Takuto Ikuta <tikuta@chromium.org>
2025-06-05 01:13:10 -0700

    Adding <set> include for std::set
    This CL fixes a breakage due to missing include in
    session_service_commands.h
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-modules-compile-fyi-rel/35391/overview
    
    Bug: 40263312
    Change-Id: I4f47cc3b288b62791ded2895c8b2befd0275c717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619197
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469842}

--- components/sessions/core/session_service_commands.h
+++ components/sessions/core/session_service_commands.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <optional>
+#include <set>
 #include <string>
 
 #include "components/sessions/core/command_storage_manager.h"
