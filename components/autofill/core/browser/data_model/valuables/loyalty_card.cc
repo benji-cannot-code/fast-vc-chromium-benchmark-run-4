49fb23f773495d6a9b035a59f31b5559244e28fc
Norge Vizcay <vizcay@google.com>
2025-06-05 04:51:39 -0700

    Filter out loyalty cards with empty merchant domains.
    Bug: 404436027
    Change-Id: I5bd8630bfcca45cd21fe301cc11dec63707e4727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619080
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469886}

--- components/autofill/core/browser/data_model/valuables/loyalty_card.cc
+++ components/autofill/core/browser/data_model/valuables/loyalty_card.cc
@@ -32,6 +32,7 @@ LoyaltyCard::~LoyaltyCard() = default;
 
 bool LoyaltyCard::IsValid() const {
   return !id_->empty() && !loyalty_card_number_.empty() &&
+         !merchant_name_.empty() &&
          (program_logo_.is_empty() || program_logo_.is_valid());
 }
 
