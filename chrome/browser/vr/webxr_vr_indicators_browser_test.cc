608f5b3d70b44fd70e691651e44fa501570669ba
Alexander Cooper <alcooper@chromium.org>
2025-06-04 17:28:49 -0700

    Run Mock XRDevice on its own thread
    On Android the browser thread, device thread, and test thread are all
    the same thing. This leads to issues where the test can be waiting on
    the device to do something or for a message to get posted through the
    browser process, but it doesn't happen because the test is blocking the
    thread. This change thus introduces a new thread for the MockXRDevice,
    which seems to by-and-large unblock these tests that were failing (some
    flakily, some fairly consistently). There's still a bit of potential
    concern with the browser process, as sometimes running the RunLoop
    didn't seem to resolve everything, but no flakes are observed. On
    Windows, the device process can still need to query the test thread, but
    this seems to be much less of a concern; however, it doesn't hurt
    anything to run an extra thread on Windows and reduces the complexity
    for debugging, so Windows also gets an extra thread as well (and this
    may help with the far more transient flaky failures we've off and on
    seen on Windows over the years).
    
    Bug: 381000093
    Change-Id: I54f620fde0431729215356424c90401aa04b88d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618607
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469713}

--- chrome/browser/vr/webxr_vr_indicators_browser_test.cc
+++ chrome/browser/vr/webxr_vr_indicators_browser_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/vr/test/mock_xr_device_hook_base.h"
 #include "chrome/browser/vr/test/multi_class_browser_test.h"
 #include "chrome/browser/vr/test/ui_utils.h"
 #include "chrome/browser/vr/test/webxr_vr_browser_test.h"
@@ -17,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Browser tests for indicators shown at various phases of an immersive session.
 
-// TODO(https://crbug.com/381000093): Fix tests on Android
-#if !BUILDFLAG(IS_ANDROID)
 namespace vr {
 
 namespace {
@@ -82,6 +81,7 @@ void TestIndicatorOnAccessForContentType(
     ContentSettingsType content_setting_type,
     const std::string& script,
     UserFriendlyElementName element_name) {
+  MockXRDeviceHookBase mock_device;
   // Enter VR while the content setting is CONTENT_SETTING_ASK to suppress
   // its corresponding indicator from initially showing up.
   LoadGenericPageChangeDefaultPermissionAndEnterVr(
@@ -104,6 +104,7 @@ void TestForInitialIndicatorForContentType(
     WebXrVrBrowserTestBase* t,
     const std::vector<TestIndicatorSetting>& test_indicator_settings) {
   DCHECK(!test_indicator_settings.empty());
+  MockXRDeviceHookBase mock_device;
   // Enter VR while the content setting is CONTENT_SETTING_ASK to suppress
   // its corresponding indicator from initially showing up.
   LoadGenericPageChangeDefaultPermissionAndEnterVr(
@@ -222,4 +223,3 @@ WEBXR_VR_ALL_RUNTIMES_BROWSER_TEST_F(
 }
 
 }  // namespace vr
-#endif  // if !BUILDFLAG(IS_ANDROID)
