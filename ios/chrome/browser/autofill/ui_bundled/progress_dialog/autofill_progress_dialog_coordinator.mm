951c5db62e08b942fe9955fa9ea99b44f180c807
yiwenqian <yiwenqian@google.com>
2025-06-05 12:18:08 -0700

    IOS Hagrid UI polish: make progress dialog fade in
    Bug: 422231511, b/422782575
    
    Change-Id: Ice7ddc2a3f8a824d313f48bdb41459e9dd0173cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618787
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Yiwen Qian <yiwenqian@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470118}

--- ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_coordinator.mm
+++ ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_coordinator.mm
@@ -61,6 +61,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   AlertViewController* alertViewController = [[AlertViewController alloc] init];
   alertViewController.modalPresentationStyle =
       UIModalPresentationOverFullScreen;
+  alertViewController.modalTransitionStyle =
+      UIModalTransitionStyleCrossDissolve;
   _alertViewController = alertViewController;
   _mediator->SetConsumer(alertViewController);
   [self.baseViewController presentViewController:alertViewController
