2c1230ad651690b4d0db9e733a6b718dce69fb7e
Rune Lillesveen <futhark@chromium.org>
2025-06-05 00:50:18 -0700

    Revert "[ident] Support ident() within var() functions (reland)"
    This reverts commit 33cb042440a11663e7535398291b97a4741725dc.
    
    Reason for revert: crash issue 422592875
    
    Bug: 384930424, 422592875
    
    Original change's description:
    > [ident] Support ident() within var() functions (reland)
    >
    > This CL allows e.g. var(ident("--myprop" 2)), such that it behaves
    > identically to var(--myprop2).
    >
    > Bug: 384930424
    > Change-Id: I815131ac7d430c4fbd1556db420faa075165da1d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620081
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Auto-Submit: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1469378}
    
    Bug: 384930424
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6c806c1655890e0f0b5e722880a3e45d59898124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618538
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469829}

--- third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/execution_context/security_context.h"
 #include "third_party/blink/renderer/core/testing/null_execution_context.h"
-#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 
 namespace blink {
@@ -1421,17 +1420,6 @@ TEST(CSSParserImplTest, ParseNestedRule) {
             To<StyleRule>(nested)
                 ->FirstSelector()
                 ->SelectorTextExpandingPseudoReferences(/*scope_id=*/0));
-}
-
-TEST(CSSParserImplTest, UnexpectedTokenInVar_IdentFunctionDisabled) {
-  test::TaskEnvironment task_environment;
-  ScopedNullExecutionContext execution_context;
-  ScopedCSSIdentFunctionForTest scoped_feature(false);
-  Document* document =
-      Document::CreateForTest(execution_context.GetExecutionContext());
-  // Don't crash:
-  css_test_helpers::ParseRule(*document, ".a { color: var(42); }");
-  css_test_helpers::ParseRule(*document, ".a { color: var(ident('thing')); }");
 }
 
 }  // namespace blink
