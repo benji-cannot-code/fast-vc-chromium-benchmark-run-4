f40d12726a82483dea5ae01655ae3567401df8d0
Christoph Schwering <schwering@google.com>
2025-06-05 10:36:31 -0700

    [Autofill] Remove AttributeInstance::GetSupportedTypes()
    This CL removes AttributeInstance::GetSupportedTypes() from the public
    API. For unsupported types,
    AttributeType::{Set,Get}[Raw]Info[WithVerificationStatus]() become a
    no-op / returns the empty string.
    
    The CL replaces all references references to `a.GetSupportedTypes()`
    with `a.type().field_subtypes()`. This is equivalence-preserving in all
    those cases.
    
    Bug: 422716363
    Change-Id: I38f626946c8e7a7e4a337b4966c434f20fa1a132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624052
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470056}

--- components/autofill/core/browser/crowdsourcing/determine_possible_field_types.cc
+++ components/autofill/core/browser/crowdsourcing/determine_possible_field_types.cc
@@ -179,7 +179,7 @@ FieldTypeSet GetAvailableAutofillAiFieldTypes(
   FieldTypeSet types;
   for (const EntityInstance& entity : entities) {
     for (const AttributeInstance& attribute : entity.attributes()) {
-      for (FieldType field_type : attribute.GetSupportedTypes()) {
+      for (FieldType field_type : attribute.type().field_subtypes()) {
         bool is_empty = comparator.HasOnlySkippableCharacters(attribute.GetInfo(
             field_type, comparator.app_locale(), std::nullopt));
         if (!is_empty) {
@@ -214,7 +214,7 @@ FieldTypeSet GetPossibleAutofillAiFieldTypes(
   FieldTypeSet types;
   for (const EntityInstance& entity : entities) {
     for (const AttributeInstance& attribute : entity.attributes()) {
-      for (FieldType field_type : attribute.GetSupportedTypes()) {
+      for (FieldType field_type : attribute.type().field_subtypes()) {
         bool matches = comparator.Compare(
             value,
             attribute.GetInfo(field_type, comparator.app_locale(),
@@ -246,7 +246,7 @@ void FindAndSetPossibleDateFieldTypes(
 
   for (const EntityInstance& entity : entities) {
     for (const AttributeInstance& attribute : entity.attributes()) {
-      for (const FieldType field_type : attribute.GetSupportedTypes()) {
+      for (const FieldType field_type : attribute.type().field_subtypes()) {
         if (!IsDateFieldType(field_type)) {
           continue;
         }
