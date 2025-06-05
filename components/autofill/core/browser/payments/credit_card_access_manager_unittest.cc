1fd65df7ae922cfa5174ce29f9c15e4c8b1ccfcb
Olivia Saul <jsaul@google.com>
2025-06-04 17:47:58 -0700

    [AF] Remove UsedCached[Server|Virtual]Card metrics
    OBSOLETE_HISTOGRAMS=Deemed obsolete by annual team histogram review
    
    Change-Id: Ib7566fa0f0539474b10366feb200b09beb23eb43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6617747
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469718}

--- components/autofill/core/browser/payments/credit_card_access_manager_unittest.cc
+++ components/autofill/core/browser/payments/credit_card_access_manager_unittest.cc
@@ -1650,14 +1650,12 @@ TEST_F(CreditCardAccessManagerTest, FetchCreditCardUsesUnmaskedCardCache) {
   // version.
   FetchCreditCard(&masked_card);
 
-  histogram_tester.ExpectBucketCount("Autofill.UsedCachedServerCard", 1, 1);
   histogram_tester.ExpectUniqueSample(
       "Autofill.ServerCardUnmask.ServerCard.Result.UnspecifiedFlowType",
       autofill_metrics::ServerCardUnmaskResult::kLocalCacheHit, 1);
 
   FetchCreditCard(&masked_card);
 
-  histogram_tester.ExpectBucketCount("Autofill.UsedCachedServerCard", 2, 1);
   histogram_tester.ExpectUniqueSample(
       "Autofill.ServerCardUnmask.ServerCard.Result.UnspecifiedFlowType",
       autofill_metrics::ServerCardUnmaskResult::kLocalCacheHit, 2);
@@ -1674,7 +1672,6 @@ TEST_F(CreditCardAccessManagerTest, FetchCreditCardUsesUnmaskedCardCache) {
   masked_card.set_record_type(CreditCard::RecordType::kVirtualCard);
   FetchCreditCard(&masked_card);
 
-  histogram_tester.ExpectBucketCount("Autofill.UsedCachedVirtualCard", 1, 1);
   histogram_tester.ExpectUniqueSample(
       "Autofill.ServerCardUnmask.VirtualCard.Result.UnspecifiedFlowType",
       autofill_metrics::ServerCardUnmaskResult::kLocalCacheHit, 1);
