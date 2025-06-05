1ecfb73c0dba25a70f0d30df11048a6ba34e56fe
Shakti Sahu <shaktisahu@chromium.org>
2025-06-05 14:33:55 -0700

    [Versioning] Add enum for versioning to CollaborationEvent
    This CL adds enum CollaborationEvent::VERSION_OUT_OF_DATE
    for versioning. No functional changes. The following CL will add
    the business logic.
    
    Bug: 420675818
    Change-Id: I9776a1c97e65c0d28fbf8498b373a5ee14b0117d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620764
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470207}

--- ios/chrome/browser/collaboration/model/messaging/infobar/collaboration_group_infobar_delegate.mm
+++ ios/chrome/browser/collaboration/model/messaging/infobar/collaboration_group_infobar_delegate.mm
@@ -100,6 +100,7 @@ Browser* GetBrowserFromInstantMessage(
     case CollaborationEvent::TAB_GROUP_REMOVED:
     case CollaborationEvent::UNDEFINED:
     case CollaborationEvent::COLLABORATION_REMOVED:
+    case CollaborationEvent::VERSION_OUT_OF_DATE:
       use_first_available_browser = true;
       break;
   }
@@ -246,6 +247,8 @@ std::u16string CollaborationGroupInfoBarDelegate::GetButtonLabel(
     case CollaborationEvent::COLLABORATION_ADDED:
     case CollaborationEvent::COLLABORATION_REMOVED:
     case CollaborationEvent::COLLABORATION_MEMBER_REMOVED:
+    case CollaborationEvent::VERSION_OUT_OF_DATE:
+      // TODO(crbug.com/422424386): Implement for versioning.
       return l10n_util::GetStringUTF16(
           IDS_IOS_COLLABORATION_GROUP_DEFAULT_PRIMARY_TOOLBAR_BUTTON);
   }
@@ -269,6 +272,8 @@ bool CollaborationGroupInfoBarDelegate::Accept() {
     case CollaborationEvent::COLLABORATION_ADDED:
     case CollaborationEvent::COLLABORATION_REMOVED:
     case CollaborationEvent::COLLABORATION_MEMBER_REMOVED:
+    case CollaborationEvent::VERSION_OUT_OF_DATE:
+      // TODO(crbug.com/422424386): Implement for versioning.
       break;
   }
 
@@ -343,6 +348,8 @@ UIImage* CollaborationGroupInfoBarDelegate::GetSymbolImage() {
       break;
     case CollaborationEvent::TAB_GROUP_REMOVED:
     case CollaborationEvent::COLLABORATION_REMOVED:
+    case CollaborationEvent::VERSION_OUT_OF_DATE:
+      // TODO(crbug.com/422424386): Implement for versioning.
       symbolName = kTabGroupsSymbol;
       break;
   }
