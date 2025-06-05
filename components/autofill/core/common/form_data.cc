b55f2ae1d2069f133ae1149e2a7dbb114fe8871e
Christoph Schwering <schwering@google.com>
2025-06-05 03:36:46 -0700

    [Autofill] Make operator<<() for Form[Field]Data more digestible
    The serialization of FormData() to std::ostream is hardly readable,
    even though it skips many members.
    
    This CL changes the format from a one-line, no-separator format to a
    JSON-like format:
    - Old: http://shortn/_TAVHLrXhob
    - New: http://shortn/_2J5FLZCSJ2
    
    It also adds FormData::global_id() and
    FormFieldData::is_{visible,focusable}() to the output.
    
    Bug: 422683631
    Change-Id: Ia7d7861f6aa17ab43fde254c5d9d8e8b425092bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6621189
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1469875}

--- components/autofill/core/common/form_data.cc
+++ components/autofill/core/common/form_data.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/logging/log_buffer.h"
+#include "components/autofill/core/common/logging/stream_operator_util.h"
 
 namespace autofill {
 
@@ -130,13 +131,45 @@ bool FormHasNonEmptyPasswordField(const FormData& form) {
 }
 
 std::ostream& operator<<(std::ostream& os, const FormData& form) {
-  os << base::UTF16ToUTF8(form.name()) << " " << form.url() << " "
-     << form.action() << " " << form.main_frame_origin() << " " << "Fields:";
-  for (const FormFieldData& field : form.fields()) {
-    os << field << ",";
+  return internal::PrintWithIndentation(os, form, /*indentation=*/0);
+}
+
+namespace internal {
+
+std::ostream& PrintWithIndentation(std::ostream& os,
+                                   const FormData& form,
+                                   int indentation,
+                                   std::string_view title) {
+  std::string space = std::string(indentation, ' ');
+  os << space << "{";
+  if (!title.empty()) {
+    os << " /*" << title << "*/";
   }
+  os << '\n';
+#define PRINT_PROPERTY(property)                                            \
+  os << space << "  " << #property << ": " << PrintWrapper(form.property()) \
+     << ",\n"
+  PRINT_PROPERTY(global_id);
+  PRINT_PROPERTY(name);
+  PRINT_PROPERTY(url);
+  PRINT_PROPERTY(main_frame_origin);
+#undef PRINT_PROPERTY
+  os << space << "  fields: [ /*length " << form.fields().size() << "*/\n";
+  for (size_t i = 0; i < form.fields().size(); ++i) {
+    internal::PrintWithIndentation(
+        os, form.fields()[i], /*indentation=*/indentation + 4,
+        base::StrCat({"FormFieldData index ", base::NumberToString(i)}));
+    if (i < form.fields().size()) {
+      os << ",";
+    }
+    os << '\n';
+  }
+  os << space << "  ]\n";
+  os << space << "}";
   return os;
 }
+
+}  // namespace internal
 
 const FormFieldData* FormData::FindFieldByGlobalId(
     const FieldGlobalId& global_id) const {
