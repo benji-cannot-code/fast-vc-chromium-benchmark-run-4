52580fcba1059c68a1e05ad6d75266e733beac51
Alex Chen <alexwchen@chromium.org>
2025-06-05 11:53:56 -0700

    [omnibox] Add enterprise policy for AI Mode
    Adding a new Enterprise Policy to control AI Mode. This
    policy will replace AIModeSearchSuggestSettings which was released and
    will be deprecated in M138 (never in stable). This new policy is also
    tied with GenAiDefaultSettings.
    
    Later CLs will deprecate AIModeSearchSuggestSettings and implement the
    logic to use AIModeSettings.
    
    Bug: 422386761
    Change-Id: I9e7c7a60023471cc29e5368af104343e702a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624672
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470102}

--- ios/chrome/browser/policy/model/configuration_policy_handler_list_factory.mm
+++ ios/chrome/browser/policy/model/configuration_policy_handler_list_factory.mm
@@ -173,6 +173,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { policy::key::kAIModeSearchSuggestSettings,
     omnibox::kAIModeSearchSuggestSettings,
     base::Value::Type::INTEGER },
+  { policy::key::kAIModeSettings,
+    omnibox::kAIModeSettings,
+    base::Value::Type::INTEGER },
 };
 // clang-format on
 
@@ -251,6 +254,10 @@ std::unique_ptr<policy::ConfigurationPolicyHandlerList> BuildPolicyHandlerList(
   gen_ai_default_policies.emplace_back(
       policy::key::kAIModeSearchSuggestSettings,
       omnibox::kAIModeSearchSuggestSettings,
+      policy::GenAiDefaultSettingsPolicyHandler::PolicyValueToPrefMap(
+          {{0, 0}, {1, 0}, {2, 1}}));
+  gen_ai_default_policies.emplace_back(
+      policy::key::kAIModeSettings, omnibox::kAIModeSettings,
       policy::GenAiDefaultSettingsPolicyHandler::PolicyValueToPrefMap(
           {{0, 0}, {1, 0}, {2, 1}}));
   handlers->AddHandler(
