9bc8e7159dbcd8f6f9419160ff0afd2622868074
James Cook <jamescook@chromium.org>
2025-06-05 13:48:09 -0700

    extensions: Add chrome.dom API tests to desktop Android
    The underlying chrome.dom API is already hooked up (via bindings in the
    renderer). We just need to add the tests.
    
    Fixed: 422796100
    Change-Id: Iaeba83d6c818fdb8136b6b90f4295401e387865a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626012
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470167}

--- chrome/browser/extensions/extension_dom_apitest.cc
+++ chrome/browser/extensions/extension_dom_apitest.cc
@@ -4,9 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/extensions/extension_apitest.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/test/result_catcher.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -23,7 +26,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest,
       "dom/closed_shadow_root_from_content_script")));
 
   ResultCatcher catcher;
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+  ASSERT_TRUE(NavigateToURL(url));
+  ASSERT_TRUE(content::WaitForLoadStop(GetActiveWebContents()));
   EXPECT_TRUE(catcher.GetNextResult());
 }
 
