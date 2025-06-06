4ef2b53a1203406be1245d0c9ad808d80918294d
Wenyu Fu <wenyufu@chromium.org>
2025-06-06 16:37:04 -0700

    Fix the gravity for toolbar progress bar container
    Follow up from crrev.com/c/6599131 which changes the gravity in the
    layout.
    
    Bug: 409407482
    Change-Id: I76950c5a06047e1dac37ce8891af2ae911454369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6628048
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470844}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionController.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionController.java
@@ -424,7 +424,7 @@ public class ToolbarPositionController implements OnSharedPreferenceChangeListen
                                 (LayoutParams) mToolbarProgressBarContainer.getLayoutParams();
                         progressBarLayoutParams.setAnchorId(mControlContainer.getView().getId());
                         progressBarLayoutParams.anchorGravity = Gravity.BOTTOM;
-                        progressBarLayoutParams.gravity = Gravity.TOP;
+                        progressBarLayoutParams.gravity = Gravity.CENTER;
                         mToolbarProgressBarContainer.setLayoutParams(progressBarLayoutParams);
                     };
 
