7eeeba718bcf20ce6ed1b4be2c8d72ac78069a15
Ho Cheung <hocheung@chromium.org>
2025-06-06 01:20:57 -0700

    Replace base::Overloaded with absl::Overload
    We are allowing absl::Overload; we should replace base::Overloaded
    with it and remove.
    
    This CL was uploaded by git cl split.
    
    Bug: 392929829
    Change-Id: Ib83cfc6345fd1691845005223ccc81e0bb2a9255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6621481
    Auto-Submit: Ho Cheung <hocheung@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470403}

--- chrome/browser/digital_credentials/digital_identity_provider_desktop.cc
+++ chrome/browser/digital_credentials/digital_identity_provider_desktop.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <variant>
 
 #include "base/containers/span.h"
-#include "base/functional/overloaded.h"
 #include "base/values.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/digital_credentials/digital_identity_low_risk_origins.h"
@@ -29,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/random.h"
 #include "device/fido/cable/v2_constants.h"
 #include "device/fido/cable/v2_handshake.h"
+#include "third_party/abseil-cpp/absl/functional/overload.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/dialog_model.h"
 #include "ui/views/accessibility/view_accessibility.h"
@@ -169,7 +169,7 @@ void DigitalIdentityProviderDesktop::Transact(
 
 void DigitalIdentityProviderDesktop::OnEvent(const std::string& qr_url,
                                              Event event) {
-  std::visit(base::Overloaded{
+  std::visit(absl::Overload{
                  [this, qr_url](SystemEvent event) {
                    switch (event) {
                      case SystemEvent::kBluetoothNotPowered:
@@ -226,7 +226,7 @@ void DigitalIdentityProviderDesktop::OnFinished(
   }
 
   std::visit(
-      base::Overloaded{
+      absl::Overload{
           [this](SystemError error) {
             EndRequestWithError(RequestStatusForMetrics::kErrorOther);
           },
