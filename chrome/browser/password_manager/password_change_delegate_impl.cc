126a5dd67ded64cbd76fa854538f73a6479e53b8
Rafal Godlewski <rgod@google.com>
2025-06-06 06:21:04 -0700

    [PasswordChange] Replace trouble signin-in bubble with dialog
    Bug: 417390080
    Change-Id: I3ea5087f0d51c092f3295b4e9217a5e6ef2adbe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618844
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470491}

--- chrome/browser/password_manager/password_change_delegate_impl.cc
+++ chrome/browser/password_manager/password_change_delegate_impl.cc
@@ -330,12 +330,12 @@ void PasswordChangeDelegateImpl::UpdateState(
     case State::kChangingPassword:
     case State::kOfferingPasswordChange:
     case State::kPasswordChangeFailed:
+    case State::kChangePasswordFormNotFound:
       return;
     case State::kPasswordSuccessfullyChanged:
       NotifyPasswordChangeFinishedSuccessfully(originator_);
       // Fallthrough to trigger bubble display.
       [[fallthrough]];
-    case State::kChangePasswordFormNotFound:
     case State::kWaitingForAgreement:
     case State::kOtpDetected:
       DisplayChangePasswordBubbleAutomatically(originator_);
