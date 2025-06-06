96d521137084afbb05c42e6d15a03878e9023a4d
Keigo Oka <oka@google.com>
2025-06-06 10:11:26 -0700

    extensions: Support opening options from details page on desktop android
    A new method is added to ExtensionTabUtil to allow opening the options
    page from the given WebContents, and we make it work on desktop android.
    
    Bug: 412226610
    Test: Install an extension that has options_page, click the link in
    details page on chrome://extensions, and confirm the page opens in a new
    tab.
    Test: Install an extension that has options_ui, click the link in
    details page on chrome://extensions, and confirm the URL shown in
    omnibox is changed as expected, while the options are not shown due to a
    separate issue 422872704.
    
    Change-Id: I31ad03093ab3db5d49c85eaffeb7814ed4ed53d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626799
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470618}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_desktop.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_desktop.cc
@@ -88,7 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/install_warning.h"
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
-#include "extensions/common/manifest_handlers/options_page_info.h"
 #include "extensions/common/manifest_url_handlers.h"
 #include "extensions/common/mojom/context_type.mojom.h"
 #include "extensions/common/permissions/permissions_data.h"
@@ -609,32 +608,6 @@ DeveloperPrivateLoadDirectoryFunction::DeveloperPrivateLoadDirectoryFunction()
 
 DeveloperPrivateLoadDirectoryFunction::
     ~DeveloperPrivateLoadDirectoryFunction() {}
-
-DeveloperPrivateShowOptionsFunction::~DeveloperPrivateShowOptionsFunction() =
-    default;
-
-ExtensionFunction::ResponseAction DeveloperPrivateShowOptionsFunction::Run() {
-  std::optional<developer::ShowOptions::Params> params =
-      developer::ShowOptions::Params::Create(args());
-  EXTENSION_FUNCTION_VALIDATE(params);
-  const Extension* extension = GetEnabledExtensionById(params->extension_id);
-  if (!extension) {
-    return RespondNow(Error(kNoSuchExtensionError));
-  }
-
-  if (OptionsPageInfo::GetOptionsPage(extension).is_empty()) {
-    return RespondNow(Error(kNoOptionsPageForExtensionError));
-  }
-
-  content::WebContents* web_contents = GetSenderWebContents();
-  if (!web_contents) {
-    return RespondNow(Error(kCouldNotFindWebContentsError));
-  }
-
-  ExtensionTabUtil::OpenOptionsPage(extension,
-                                    chrome::FindBrowserWithTab(web_contents));
-  return RespondNow(NoArguments());
-}
 
 DeveloperPrivateShowPathFunction::~DeveloperPrivateShowPathFunction() = default;
 
