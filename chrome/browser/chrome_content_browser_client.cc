fb7bb93dc95e1453b52ba60e18c29abf529b5270
Joey Arhar <jarhar@chromium.org>
2025-06-06 14:56:29 -0700

    Deprecate the SelectParserRelaxationEnabled enterprise policy
    Now that this policy is past its end date for support, we can remove it.
    
    Bug: 335456114
    Change-Id: I83ba0e89d8b27d06beae473097ec8bb0c78ed582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568239
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470803}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1477,10 +1477,6 @@ void ChromeContentBrowserClient::RegisterProfilePrefs(
       /*default_value=*/false);
 
   registry->RegisterBooleanPref(
-      policy::policy_prefs::kSelectParserRelaxationEnabled,
-      /*default_value=*/true);
-
-  registry->RegisterBooleanPref(
       policy::policy_prefs::kStandardizedBrowserZoomEnabled, true);
 
   registry->RegisterBooleanPref(
@@ -2792,11 +2788,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
               policy::policy_prefs::kCSSCustomStateDeprecatedSyntaxEnabled)) {
         command_line->AppendSwitch(
             blink::switches::kCSSCustomStateDeprecatedSyntaxEnabled);
-      }
-      if (!prefs->GetBoolean(
-              policy::policy_prefs::kSelectParserRelaxationEnabled)) {
-        command_line->AppendSwitch(
-            blink::switches::kDisableSelectParserRelaxation);
       }
 
       if (prefs->GetBoolean(policy::policy_prefs::
