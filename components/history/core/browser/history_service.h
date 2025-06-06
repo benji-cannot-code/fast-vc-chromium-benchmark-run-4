0a45642620b89519348218686db22f78c6a77e5f
Lei Zhang <thestig@chromium.org>
2025-06-06 10:11:57 -0700

    Include file_path.h in history_service.h
    In components/history/core/browser/history_service.h,
    `HistoryService::history_dir_` from https://crrev.com/1274286 requires
    the definition of base::FilePath. So replace for the forward declaration
    with file_path.h, instead of relying on a transitive file_path.h include
    from elsewhere.
    
    Bug: 40216326
    Change-Id: I0e70fc2b6b5601e1c70fe16b63512ca128e25776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624837
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470619}

--- components/history/core/browser/history_service.h
+++ components/history/core/browser/history_service.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/containers/flat_set.h"
 #include "base/feature_list.h"
+#include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
@@ -52,10 +53,6 @@ class GURL;
 class HistoryQuickProviderTest;
 class InMemoryURLIndexTest;
 class SkBitmap;
-
-namespace base {
-class FilePath;
-}  // namespace base
 
 namespace favicon {
 class FaviconServiceImpl;
