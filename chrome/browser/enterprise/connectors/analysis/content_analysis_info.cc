a0b56ba57da3fe6bb5bf547a3650b80f47acf473
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-06-06 11:05:07 -0700

    Add content_area_account_email field to sensitive data events
    This CL populates the new `web_app_signed_in_account` field added in
    crrev.com/c/6616249 with the same `content_area_account_email` passed in
    content analysis requests.
    
    Bug: 293360492
    Change-Id: I434e0817e5d57dff9835906b0ae17ebc89184edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624600
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470663}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_info.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_info.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/fixed_flat_set.h"
 #include "base/feature_list.h"
 #include "base/strings/string_number_conversions.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/enterprise/connectors/core/features.h"
 #include "components/enterprise/connectors/core/reporting_utils.h"
 #include "components/safe_browsing/core/common/features.h"
@@ -147,5 +148,65 @@ std::string ContentAnalysisInfo::GetContentAreaAccountEmail() const {
 
   return accounts.GetAllAccounts()[user_index].email;
 }
+
+// static
+std::string ContentAreaUserProvider::GetUser(Profile* profile,
+                                             const GURL& tab_url) {
+  return ContentAreaUserProvider(IdentityManagerFactory::GetForProfile(profile),
+                                 tab_url)
+      .GetContentAreaAccountEmail();
+}
+
+const GURL& ContentAreaUserProvider::tab_url() const {
+  return *tab_url_;
+}
+
+signin::IdentityManager* ContentAreaUserProvider::identity_manager() const {
+  return im_;
+}
+
+const enterprise_connectors::AnalysisSettings&
+ContentAreaUserProvider::settings() const {
+  NOTREACHED();
+}
+
+int ContentAreaUserProvider::user_action_requests_count() const {
+  NOTREACHED();
+}
+
+std::string ContentAreaUserProvider::tab_title() const {
+  NOTREACHED();
+}
+
+std::string ContentAreaUserProvider::user_action_id() const {
+  NOTREACHED();
+}
+
+std::string ContentAreaUserProvider::email() const {
+  NOTREACHED();
+}
+
+std::string ContentAreaUserProvider::url() const {
+  NOTREACHED();
+}
+
+enterprise_connectors::ContentAnalysisRequest::Reason
+ContentAreaUserProvider::reason() const {
+  NOTREACHED();
+}
+
+google::protobuf::RepeatedPtrField<::safe_browsing::ReferrerChainEntry>
+ContentAreaUserProvider::referrer_chain() const {
+  NOTREACHED();
+}
+
+google::protobuf::RepeatedPtrField<std::string>
+ContentAreaUserProvider::frame_url_chain() const {
+  NOTREACHED();
+}
+
+ContentAreaUserProvider::ContentAreaUserProvider(signin::IdentityManager* im,
+                                                 const GURL& tab_url)
+    : im_(im), tab_url_(tab_url) {}
 
 }  // namespace enterprise_connectors
--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/enterprise/connectors/analysis/content_analysis_info.h"
 #include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/enterprise/connectors/connectors_service.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
@@ -491,6 +492,12 @@ void SafeBrowsingPrivateEventRouter::OnSensitiveDataEvent(
   if (!content_transfer_method.empty()) {
     event.Set(kKeyContentTransferMethod, content_transfer_method);
   }
+  std::string content_area_account_email =
+      enterprise_connectors::ContentAreaUserProvider::GetUser(
+          Profile::FromBrowserContext(context_), tab_url);
+  if (!content_area_account_email.empty()) {
+    event.Set(kKeyWebAppSignedInAccount, content_area_account_email);
+  }
 
   AddAnalysisConnectorVerdictToEvent(result, event);
 
@@ -550,6 +557,12 @@ void SafeBrowsingPrivateEventRouter::OnAnalysisConnectorWarningBypassed(
   }
   if (!content_transfer_method.empty()) {
     event.Set(kKeyContentTransferMethod, content_transfer_method);
+  }
+  std::string content_area_account_email =
+      enterprise_connectors::ContentAreaUserProvider::GetUser(
+          Profile::FromBrowserContext(context_), tab_url);
+  if (!content_area_account_email.empty()) {
+    event.Set(kKeyWebAppSignedInAccount, content_area_account_email);
   }
 
   AddAnalysisConnectorVerdictToEvent(result, event);
@@ -775,6 +788,12 @@ void SafeBrowsingPrivateEventRouter::OnDataControlsSensitiveDataEvent(
     event.Set(kKeyContentSize, base::Int64ToValue(content_size));
   }
   event.Set(kKeyTrigger, trigger);
+  std::string content_area_account_email =
+      enterprise_connectors::ContentAreaUserProvider::GetUser(
+          Profile::FromBrowserContext(context_), tab_url);
+  if (!content_area_account_email.empty()) {
+    event.Set(kKeyWebAppSignedInAccount, content_area_account_email);
+  }
   event.Set(kKeyEventResult,
             enterprise_connectors::EventResultToString(event_result));
 
