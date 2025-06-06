cf360366eeebee5a8869e18a1882887618c04644
Eliot Courtney <edcourtney@google.com>
2025-06-05 21:26:49 -0700

    Plumb activity result from the media capture picker dialog.
    This CL plumbs the ActivityResult from MediaProjection from the media
    capture picker dialog to the ScreenCapture code.
    
    Bug: 352187279
    Test: CQ
    Change-Id: I2aa4d4f5612bc79001e7bc54efeb514a02361206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579088
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470352}

--- chrome/android/java/src/org/chromium/chrome/browser/media/MediaCapturePickerDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/MediaCapturePickerDialog.java
@@ -24,6 +24,7 @@ import org.chromium.chrome.browser.media.MediaCapturePickerHeadlessFragment.Capt
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLoadIfNeededCaller;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.content_public.browser.media.capture.ScreenCapture;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
@@ -196,8 +197,12 @@ public class MediaCapturePickerDialog implements AllTabObserver.Observer {
     private void startAndroidCapturePrompt() {
         var fragment = MediaCapturePickerHeadlessFragment.getInstanceForCurrentActivity();
         fragment.startAndroidCapturePrompt(
-                result -> {
-                    switch (result) {
+                (action, result) -> {
+                    if (action != CaptureAction.CAPTURE_CANCELLED) {
+                        ScreenCapture.onPick(result);
+                    }
+
+                    switch (action) {
                         case CaptureAction.CAPTURE_CANCELLED:
                             mDelegate.onCancel();
                             break;
