7c3bab36435a2786f5f0d8e6f0a5a55d0b105145
Jérôme Lebel <jlebel@chromium.org>
2025-06-06 15:56:20 -0700

    [iOS][rcaps_pipeline] Add test for Google search icon
    This patch adds tests for crrev.com/c/6628202.
    
    Fixed: b:422441516
    Change-Id: I6422878721253e52507efb3f097555555a2b0f0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627684
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470826}

--- ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_ui_util.mm
+++ ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_ui_util.mm
@@ -27,6 +27,8 @@ namespace {
 
 const CGFloat kSearchEngineMinFaviconSizePt = 8;
 
+}  // namespace
+
 int GetResourceIdFromTemplateURL(const TemplateURL& template_url) {
 #if BUILDFLAG(ENABLE_BUILTIN_SEARCH_PROVIDER_ASSETS)
   // This would be better served by ResourcesUtil::GetThemeResourceId(), but
@@ -54,7 +56,6 @@ int GetResourceIdFromTemplateURL(const TemplateURL& template_url) {
 
   return IDR_DEFAULT_FAVICON;
 }
-}  // namespace
 
 UIImage* SearchEngineFaviconFromTemplateURL(const TemplateURL& template_url) {
   // Only works for prepopulated search engines.
