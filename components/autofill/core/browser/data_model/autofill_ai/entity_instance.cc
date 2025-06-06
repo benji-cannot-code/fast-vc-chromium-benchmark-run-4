4ab32d544c9e18b9bd85e2a32fd9810a40bea6c1
Christoph Schwering <schwering@google.com>
2025-06-06 11:02:49 -0700

    [Autofill] Update documentation in entity_instance.h
    Bug: 422716363
    Change-Id: I1604a87fd2fb19b7454b255e90b758d417373bff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625656
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470660}

--- components/autofill/core/browser/data_model/autofill_ai/entity_instance.cc
+++ components/autofill/core/browser/data_model/autofill_ai/entity_instance.cc
@@ -25,25 +25,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-AttributeInstance::AttributeInstance(AttributeType type) : type_(type) {
-  switch (type.data_type()) {
-    case AttributeType::DataType::kName:
-      info_ = NameInfo();
-      break;
-    case AttributeType::DataType::kCountry:
-      info_ = CountryInfo();
-      break;
-    case AttributeType::DataType::kDate:
-      info_ = DateInfo();
-      break;
-    case AttributeType::DataType::kState:
-      info_ = StateInfo();
-      break;
-    case AttributeType::DataType::kString:
-      info_ = u"";
-      break;
-  }
-}
+AttributeInstance::AttributeInstance(AttributeType type)
+    : type_(type), info_([&]() -> InfoStructure {
+        switch (type.data_type()) {
+          case AttributeType::DataType::kName:
+            return NameInfo();
+          case AttributeType::DataType::kCountry:
+            return CountryInfo();
+          case AttributeType::DataType::kDate:
+            return DateInfo();
+          case AttributeType::DataType::kState:
+            return StateInfo();
+          case AttributeType::DataType::kString:
+            return u"";
+        }
+      }()) {}
 
 AttributeInstance::AttributeInstance(const AttributeInstance&) = default;
 AttributeInstance& AttributeInstance::operator=(const AttributeInstance&) =
@@ -143,14 +139,13 @@ void AttributeInstance::SetInfo(FieldType field_type,
   std::visit(
       absl::Overload{
           [&](CountryInfo& country) {
-            // We assume that the given `value` is either a valid
-            // country code or a valid country name localized to the
-            // provided `app_locale`.
+            // We assume that the given `value` is either a valid country code
+            // or a valid country name localized to the provided `app_locale`.
             if (!country.SetCountryFromCountryCode(value) &&
                 !country.SetCountryFromCountryName(value, app_locale)) {
-              // In case `value` turns out to be neither of the two
-              // options mentioned above, we reset the country value to
-              // indicate failure.
+              // In case `value` turns out to be neither of the two options
+              // mentioned above, we reset the country value to indicate
+              // failure.
               country = CountryInfo();
             }
           },
@@ -178,9 +173,8 @@ void AttributeInstance::SetRawInfo(FieldType field_type,
   std::visit(absl::Overload{
                  [&](CountryInfo& country) {
                    if (!country.SetCountryFromCountryCode(value)) {
-                     // In case `value` isn't a valid country
-                     // code, we reset the country value to
-                     // indicate failure.
+                     // In case `value` isn't a valid country code, we reset the
+                     // country value to indicate failure.
                      country = CountryInfo();
                    }
                  },
