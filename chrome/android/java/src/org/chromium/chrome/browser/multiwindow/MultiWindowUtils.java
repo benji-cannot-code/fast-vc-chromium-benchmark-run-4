a243916fddce6417a54309e3c3dfcf3e3ee7afa0
Aishwarya Rajesh <aishwaryarj@google.com>
2025-06-05 17:15:48 -0700

    [UW] Create method to get set of persisted instance IDs
    Create a new method MultiInstanceManagerApi31#getPersistedInstanceIds()
    that extracts persisted instance ids from the existing lastAccessedTime
    SharedPreferences KeyPrefix. Use this set to loop over instance ids in
    MultiWindowUtils#getInstanceCount().
    
    Bug: 420917692
    Change-Id: If5c0448c1d55b2efeb1f10fa928ee50fb4f7a28d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604620
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470274}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtils.java
@@ -64,6 +64,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 /**
  * Utilities for detecting multi-window/multi-instance support.
@@ -407,8 +408,9 @@ public class MultiWindowUtils implements ActivityStateListener {
     public static int getInstanceCount() {
         if (sInstanceCountForTesting != null) return sInstanceCountForTesting;
         int count = 0;
-        for (int i = 0; i < getMaxInstances(); ++i) {
-            if (MultiInstanceManagerApi31.instanceEntryExists(i) && isRestorableInstance(i)) {
+        Set<Integer> ids = MultiInstanceManagerApi31.getPersistedInstanceIds();
+        for (Integer id : ids) {
+            if (isRestorableInstance(id)) {
                 count++;
             }
         }
