1abb1da07194f4089e6cd07be4e20af83b0a5bfa
Adam Kassi Lahlou <adamkl@google.com>
2025-06-06 07:37:59 -0700

    [Watermark 2.0] Add WatermarkStyle policy and handler for customization
    This CL introduces the WatermarkStyle policy to allow administrators to
    customize the appearance of enterprise watermarks.
    
    Key changes include:
    - Definition of the new `WatermarkStyle` policy, which will allow
      configuration of attributes such as fill opacity, outline opacity,
    and font size
    - Implementation of a policy handler responsible for reading the
      WatermarkStyle policy values.
    - Integration of the policy handler to set the corresponding preferences.
    - Implementation of tests for the policy and the policy handler.
    
    Bug: 421436420
    Change-Id: Id58d429248780ae3d8ef3c56001da5909db2a79e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578101
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Adam Kassi Lahlou <adamkl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470529}

---
+++ chrome/browser/enterprise/watermark/watermark_style_policy_handler.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_WATERMARK_WATERMARK_STYLE_POLICY_HANDLER_H_
+#define CHROME_BROWSER_ENTERPRISE_WATERMARK_WATERMARK_STYLE_POLICY_HANDLER_H_
+
+#include "components/policy/core/browser/configuration_policy_handler.h"
+
+class WatermarkStylePolicyHandler : public policy::CloudOnlyPolicyHandler {
+ public:
+  explicit WatermarkStylePolicyHandler(policy::Schema schema);
+  WatermarkStylePolicyHandler(WatermarkStylePolicyHandler&) = delete;
+  WatermarkStylePolicyHandler& operator=(WatermarkStylePolicyHandler&) = delete;
+  ~WatermarkStylePolicyHandler() override;
+
+  // policy::CloudOnlyPolicyHandler:
+  void ApplyPolicySettings(const policy::PolicyMap& policies,
+                           PrefValueMap* prefs) override;
+};
+
+#endif  // CHROME_BROWSER_ENTERPRISE_WATERMARK_WATERMARK_STYLE_POLICY_HANDLER_H_
