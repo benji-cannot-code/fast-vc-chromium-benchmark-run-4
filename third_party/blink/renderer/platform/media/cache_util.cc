c3eb18f9db35c0a9e478af31f7dbc3bb8416e811
Daniel Cheng <dcheng@chromium.org>
2025-06-06 15:53:59 -0700

    Remove base::MakeStringPiece() use in //third_party/blink/renderer/platform/media
    Just use std:;string_view::substr() instead; also change some string
    constants to std::string_view to avoid the awkward length calculation.
    
    Bug: 422940464
    Change-Id: I1c449ae13601beb8a69f074c5991395e6590171d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625821
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470825}

--- third_party/blink/renderer/platform/media/cache_util.cc
+++ third_party/blink/renderer/platform/media/cache_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <string>
+#include <string_view>
 
 #include "base/containers/contains.h"
 #include "base/strings/string_number_conversions.h"
@@ -65,13 +66,11 @@ uint32_t GetReasonsForUncacheability(const WebURLResponse& response) {
   const base::TimeDelta kMinimumAgeForUsefulness =
       base::Seconds(3600);  // Arbitrary value.
 
-  const char kMaxAgePrefix[] = "max-age=";
-  const size_t kMaxAgePrefixLen = std::size(kMaxAgePrefix) - 1;
+  constexpr std::string_view kMaxAgePrefix = "max-age=";
   if (cache_control_header.starts_with(kMaxAgePrefix)) {
     int64_t max_age_seconds;
     base::StringToInt64(
-        base::MakeStringPiece(cache_control_header.begin() + kMaxAgePrefixLen,
-                              cache_control_header.end()),
+        std::string_view(cache_control_header).substr(kMaxAgePrefix.size()),
         &max_age_seconds);
     if (base::Seconds(max_age_seconds) < kMinimumAgeForUsefulness) {
       reasons |= kShortMaxAge;
@@ -103,13 +102,11 @@ base::TimeDelta GetCacheValidUntil(const WebURLResponse& response) {
   // Max cache timeout ~= 1 month.
   base::TimeDelta ret = base::Days(30);
 
-  const char kMaxAgePrefix[] = "max-age=";
-  const size_t kMaxAgePrefixLen = std::size(kMaxAgePrefix) - 1;
+  constexpr std::string_view kMaxAgePrefix = "max-age=";
   if (cache_control_header.starts_with(kMaxAgePrefix)) {
     int64_t max_age_seconds;
     base::StringToInt64(
-        base::MakeStringPiece(cache_control_header.begin() + kMaxAgePrefixLen,
-                              cache_control_header.end()),
+        std::string_view(cache_control_header).substr(kMaxAgePrefix.size()),
         &max_age_seconds);
 
     ret = std::min(ret, base::Seconds(max_age_seconds));
