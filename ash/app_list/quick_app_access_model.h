d1106a8e2e5dc20f3235e44f4dc2e25d06756c68
Lei Zhang <thestig@chromium.org>
2025-06-05 18:07:59 -0700

    Fix nits in ash/app_list/quick_app_access_model.h
    - Include time.h, because std::optional<base::TimeTicks> requires the
      definition of TimeTicks.
    - Move ctors/dtor closer together.
    - Make getters const.
    
    Change-Id: I8aa1b821bfd63bed79509f174ba5607a861079be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598559
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470295}

--- ash/app_list/quick_app_access_model.h
+++ ash/app_list/quick_app_access_model.h
@@ -13,10 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_controller_observer.h"
 #include "base/observer_list.h"
 #include "base/scoped_observation.h"
-
-namespace base {
-class TimeTicks;
-}  // namespace base
+#include "base/time/time.h"
 
 namespace gfx {
 class ImageSkia;
@@ -44,11 +41,10 @@ class QuickAppAccessModel : public AppListItemObserver,
     // Called when the default icon for the quick app icon changes.
     virtual void OnQuickAppIconChanged() = 0;
   };
-  QuickAppAccessModel();
 
+  QuickAppAccessModel();
   QuickAppAccessModel(const QuickAppAccessModel&) = delete;
   QuickAppAccessModel& operator=(const QuickAppAccessModel&) = delete;
-
   ~QuickAppAccessModel() override;
 
   void AddObserver(Observer* observer);
@@ -68,8 +64,10 @@ class QuickAppAccessModel : public AppListItemObserver,
   // Returns the quick app's display name.
   const std::u16string GetAppName() const;
 
-  const std::string& quick_app_id() { return quick_app_id_; }
-  bool quick_app_should_show_state() { return quick_app_should_show_state_; }
+  const std::string& quick_app_id() const { return quick_app_id_; }
+  bool quick_app_should_show_state() const {
+    return quick_app_should_show_state_;
+  }
 
  private:
   // AppListItemObserver:
