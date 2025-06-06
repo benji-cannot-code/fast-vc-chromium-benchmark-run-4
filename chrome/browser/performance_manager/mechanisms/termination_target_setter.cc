95707d4818f7771dde0e6f69524d5fec68204e25
Francois Pierre Doray <fdoray@chromium.org>
2025-06-06 07:30:44 -0700

    [partition_alloc] Add mechanism to terminate a process on commit failure (Windows).
    Based on an idea from lizeb@. Recently motivated by the desire to
    realize these gains go/memory-pressure-based-on-commit-stable1 with
    no effect on browser process stability.
    
    This CL adds a partition_alloc API to set the handle of a process to
    terminate on commit failure. On commit failure, if the
    "PageAllocatorRetryOnCommitFailure" feature is enabled,
    partition_alloc will repeatedly:
    
    1. Terminate the process provided via the new API, if any.
    2. Wait 50ms. Local experiments on Win11 show that a process'
       commit charge is not immediately relinquished on termination, but
       it is after 50ms (this is on 1 machine without CPU contention, we
       don't know what conditions can affect this result).
    3. Retry the commit.
    
    We plan to run a 3-arm experiment:
    - Control
    - Retry loop, without Chrome providing a process to terminate
    - Retry loop, with Chrome providing a process to terminate
    We'll check how effective each of the experiment arms is at trading
    browser crashes (very bad) for unimportant renderer crashes
    (less bad), and decide whether to ship one arm, taking into account
    effectiveness and code complexity involved.
    
    The policy which selects the termination target and provides it to
    partition_alloc via the new API is implemented separately:
    crrev.com/c/6542709
    
    Bug: 40880528
    Change-Id: I8d6662bd7b6c0809efedc417c667a5896ed6887b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576584
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470527}

---
+++ chrome/browser/performance_manager/mechanisms/termination_target_setter.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/performance_manager/mechanisms/termination_target_setter.h"
+
+#include "base/process/process.h"
+#include "components/performance_manager/public/graph/process_node.h"
+#include "partition_alloc/page_allocator.h"
+
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_types.h"
+#endif
+
+namespace performance_manager {
+
+void TerminationTargetSetter::SetTerminationTarget(
+    const ProcessNode* process_node) {
+#if BUILDFLAG(IS_WIN)
+  if (process_node == nullptr) {
+    partition_alloc::SetProcessToTerminateOnCommitFailure(nullptr);
+  } else {
+    const base::Process& process = process_node->GetProcess();
+    partition_alloc::SetProcessToTerminateOnCommitFailure(
+        process.Duplicate().Release());
+  }
+#endif  // BUILDFLAG(IS_WIN)
+}
+
+}  // namespace performance_manager
