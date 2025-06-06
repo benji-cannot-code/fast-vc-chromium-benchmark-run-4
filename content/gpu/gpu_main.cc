dfbb0f4da15f95da8e8efd4eb7a5fec95ba849c4
Colin Blundell <blundell@chromium.org>
2025-06-06 02:02:36 -0700

    [//content] Remove gpu_memory_buffer_support.h includes
    All of these are stale.
    
    Bug: 404905709
    Change-Id: I65a1f6348edb96bf88db924588ba1f064ae26796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618661
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470423}

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/config/gpu_preferences.h"
 #include "gpu/config/gpu_switches.h"
 #include "gpu/config/gpu_util.h"
-#include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "gpu/ipc/service/gpu_config.h"
 #include "gpu/ipc/service/gpu_init.h"
 #include "gpu/ipc/service/gpu_watchdog_thread.h"
--- content/renderer/pepper/pepper_graphics_2d_host.cc
+++ content/renderer/pepper/pepper_graphics_2d_host.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/capabilities.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/ipc/client/client_shared_image_interface.h"
-#include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "ppapi/c/pp_bool.h"
 #include "ppapi/c/pp_errors.h"
 #include "ppapi/c/pp_rect.h"
