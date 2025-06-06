9e7578aa6c59ce17f46bd7aa4cd1db83ece6070e
Duncan Mercer <mercerd@google.com>
2025-06-06 12:59:02 -0700

    [lensoverlay] Fix ghost loader not resizing to less than 5 suggestions
    This CL fixes an issue with the new ghost loader suggestions which
    caused the ghost loader to be shown behind the real suggestions. To do
    so, I am passing the number of suggestions dynamically to the ghost
    loader so it can respond and only render the correct number of
    suggestions. I also discovered the cross fade wasn't fully working, as
    the CSS was trying to style across the shadow DOM boundary, so fixed
    that.
    
    https://screencast.googleplex.com/cast/NTM2MTIzMTcyMzQ5NTQyNHw2ZTNkNGU1Ni1iNw
    
    NO_IFTTT=Did not touch the ghost loader strings.
    
    Bug: 422795246
    Change-Id: Idfdfe3f88bf0d50da4b2c74c06fba9513e37b4c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625197
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470725}

--- chrome/browser/resources/lens/overlay/side_panel/side_panel_app.ts
+++ chrome/browser/resources/lens/overlay/side_panel/side_panel_app.ts
@@ -173,6 +173,10 @@ export class LensSidePanelAppElement extends LensSidePanelAppElementBase {
         type: String,
         value: '',
       },
+      searchboxSuggestionCount: {
+        type: Number,
+        value: 0,
+      },
     };
   }
 
@@ -228,7 +232,12 @@ export class LensSidePanelAppElement extends LensSidePanelAppElementBase {
   private pageHandler: LensSidePanelPageHandlerInterface;
   declare private wasBackArrowAvailable: boolean;
   declare private toastMessage: string;
+  // The number of suggestions currently being shown to the user.
+  declare private searchboxSuggestionCount: number;
   private eventTracker_: EventTracker = new EventTracker();
+
+  private searchboxBoundingClientRectObserver: ResizeObserver =
+      new ResizeObserver(this.onSearchboxBoundsChanged.bind(this));
 
   constructor() {
     super();
@@ -421,6 +430,11 @@ export class LensSidePanelAppElement extends LensSidePanelAppElementBase {
     this.notifyHelpBubbleAnchorCustomEvent(
         'kLensSidePanelSearchBoxElementId',
         'kLensSidePanelSearchBoxFocusedEventId');
+
+    // Setup a listener on the suggestions container to adjust the ghost loader
+    // number of suggestions.
+    this.searchboxBoundingClientRectObserver.observe(
+        this.$.searchbox.getSuggestionsElement());
   }
 
   private onSearchboxFocusOut_(event: FocusEvent) {
@@ -437,6 +451,14 @@ export class LensSidePanelAppElement extends LensSidePanelAppElementBase {
     this.isSearchboxFocused = false;
     this.autocompleteRequestStarted = false;
     this.showErrorState = false;
+
+    // Disconnect the ResizeObserver.
+    this.searchboxBoundingClientRectObserver.disconnect();
+  }
+
+  private onSearchboxBoundsChanged() {
+    this.searchboxSuggestionCount =
+        this.$.searchbox.getSuggestionsElement().selectableMatchElements.length;
   }
 
   private computeShowGhostLoader(): boolean {
--- ui/webui/resources/cr_components/searchbox/searchbox.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.ts
@@ -359,7 +359,7 @@ export class SearchboxElement extends SearchboxElementBase {
     performance.measure('realbox-creation', 'realbox-creation-start');
   }
 
-  getSuggestionsElement(): HTMLElement {
+  getSuggestionsElement(): SearchboxDropdownElement {
     return this.$.matches;
   }
 
