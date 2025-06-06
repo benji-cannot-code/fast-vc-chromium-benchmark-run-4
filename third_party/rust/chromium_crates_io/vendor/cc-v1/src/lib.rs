245162a003d5439e8cafb3eeb586f73bfeaed9ff
Liza Burakova <liza@chromium.org>
2025-06-06 13:28:31 -0700

    Roll cc: 1.2.24 => 1.2.26 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * cc: 1.2.24 => 1.2.26 (https://docs.rs/crate/cc/1.2.26)
    
    Bug: None
    Change-Id: I6a641aefd898f2bbab5c290cb30bbf1315e2e291
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626602
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470747}

--- third_party/rust/chromium_crates_io/vendor/cc-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/cc-v1/src/lib.rs
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `cc-1.2.24` crate.
+// `cc-1.2.26` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
