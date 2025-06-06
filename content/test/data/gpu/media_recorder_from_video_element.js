4e85d589d8a374990cc8d9057040420195a5780f
Dale Curtis <dalecurtis@chromium.org>
2025-06-06 16:49:16 -0700

    Improve errors messages for media_recorder_from_video_element test
    $exception.message doesn't exist, only $element.error.message does,
    so use that instead when printing why the test failed.
    
    R=lowkey
    
    Change-Id: If3fcac0fd165f15f22081d96137216a678c6e71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624781
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470847}

--- content/test/data/gpu/media_recorder_from_video_element.js
+++ content/test/data/gpu/media_recorder_from_video_element.js
@@ -48,8 +48,8 @@ function startPlayback() {
     logOutput('Playback complete.');
     sendResult('SUCCESS');
   }
-  dstVideo.onerror = e => {
-    logOutput(`Test failed: ${e.message}`);
+  dstVideo.onerror = _ => {
+    logOutput(`Test failed: ${dstVideo.error.message}`);
     abort = true;
     sendResult('FAIL');
   };
@@ -95,8 +95,8 @@ function main() {
   dstVideo.muted = true;  // No need to exercise audio paths.
   setVideoSize();
 
-  srcVideo.onerror = e => {
-    logOutput(`Test failed: ${e.message}`);
+  srcVideo.onerror = _ => {
+    logOutput(`Test failed: ${dstVideo.error.message}`);
     abort = true;
     sendResult('FAIL');
   };
