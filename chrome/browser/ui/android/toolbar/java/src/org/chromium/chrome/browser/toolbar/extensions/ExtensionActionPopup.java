d0b35852de62a0ec20692b30ad244ee20772d9ad
Shuhei Takahashi <nya@chromium.org>
2025-06-06 17:17:05 -0700

    extensions: Set elevation to the popup
    Bug: 385987224
    Change-Id: Ibe490026245c8e7b0e53d63e63b46913bc79ba9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627252
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470857}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionActionPopup.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionActionPopup.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.toolbar.extensions;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.View;
@@ -17,6 +18,7 @@ import org.chromium.base.lifetime.Destroyable;
 import org.chromium.base.version_info.VersionInfo;
 import org.chromium.build.NullUtil;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.toolbar.R;
 import org.chromium.components.embedder_support.view.ContentView;
 import org.chromium.components.thinwebview.ThinWebView;
 import org.chromium.components.thinwebview.ThinWebViewConstraints;
@@ -40,6 +42,9 @@ import org.chromium.ui.widget.ViewRectProvider;
  */
 @NullMarked
 class ExtensionActionPopup implements Destroyable {
+    /** The context to use for creating views. */
+    private final Context mContext;
+
     /** The ID of the extension action this popup is associated with. */
     private final String mActionId;
 
@@ -73,6 +78,7 @@ class ExtensionActionPopup implements Destroyable {
             View anchorView,
             String actionId,
             ExtensionActionPopupContents contents) {
+        mContext = context;
         mActionId = actionId;
         mContents = contents;
 
@@ -107,11 +113,16 @@ class ExtensionActionPopup implements Destroyable {
                 new AnchoredPopupWindow(
                         context,
                         decorView,
-                        new ColorDrawable(Color.TRANSPARENT),
+                        new ColorDrawable(Color.WHITE),
                         frame,
                         new ViewRectProvider(anchorView));
+
         mPopupWindow.setHorizontalOverlapAnchor(true);
         mPopupWindow.setOutsideTouchable(true);
+
+        Resources resources = mContext.getResources();
+        mPopupWindow.setElevation(
+                resources.getDimensionPixelSize(R.dimen.extension_action_popup_elevation));
 
         contents.setDelegate(new ContentsDelegate());
     }
