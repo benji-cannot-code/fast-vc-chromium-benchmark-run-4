787f3509fe555853a8bd301b0d1e4f95ce86d516
Salma Elmahallawy <esalma@google.com>
2025-06-07 13:16:51 -0700

    [NTPFooter] Making management notice text clickable
    The management notice text should redirect to the chrome://management
    page when clicked.
    
    Screenshot: https://screenshot.googleplex.com/69kusWFQycLn94N
    Screen recording:
    https://drive.google.com/file/d/1Id7iXjuwnFoN2CftaYeygbFSKHe8uTED/view?usp=sharing
    
    Bug: 419335688
    Change-Id: Ia681a8048b41c30d4bb0585d6870324418c193c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6599018
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1470966}

--- chrome/test/data/webui/new_tab_footer/app_test.ts
+++ chrome/test/data/webui/new_tab_footer/app_test.ts
@@ -135,10 +135,11 @@ suite('NewTabFooterAppTest', () => {
       const managementNoticeContainer =
           element.shadowRoot.querySelector('#managementNoticeContainer');
       assertTrue(!!managementNoticeContainer);
-      let managementNoticeText = $$(element, '#managementNoticeContainer p');
-      assertTrue(!!managementNoticeText);
+      let managementNoticeLink =
+          $$(element, '#managementNoticeContainer [role="link"]');
+      assertTrue(!!managementNoticeLink);
       assertEquals(
-          managementNoticeText.innerText, 'Managed by your organization');
+          managementNoticeLink.innerText, 'Managed by your organization');
       let managementNoticeLogo =
           $$<HTMLImageElement>(element, '#managementNoticeLogo');
       assertTrue(!!managementNoticeLogo);
@@ -151,9 +152,10 @@ suite('NewTabFooterAppTest', () => {
       await callbackRouter.$.flushForTesting();
 
       // Assert.
-      managementNoticeText = $$(element, '#managementNoticeContainer p');
+      managementNoticeLink =
+          $$(element, '#managementNoticeContainer [role="link"]');
       managementNoticeLogo = $$(element, '#managementNoticeLogo');
-      assertFalse(!!managementNoticeText);
+      assertFalse(!!managementNoticeLink);
       assertFalse(!!managementNoticeLogo);
     });
 
@@ -189,6 +191,24 @@ suite('NewTabFooterAppTest', () => {
       assertEquals(logoContainter.classList.length, 0);
     });
 
+    test('Click manageemnt notice link', async () => {
+      // Arrange.
+      const managementNotice: ManagementNotice = {
+        text: 'Managed by your organization',
+        bitmapDataUrl: {url: 'chrome://resources/images/chrome_logo_dark.svg'},
+        isCustomLogo: false,
+      };
+      callbackRouter.setManagementNotice(managementNotice);
+      await callbackRouter.$.flushForTesting();
+
+      // Act.
+      const link = $$(element, '#managementNoticeContainer [role="link"]');
+      assertTrue(!!link);
+      link.click();
+
+      // Assert.
+      assertEquals(1, handler.getCallCount('openManagementPage'));
+    });
   });
 
   suite('CustomizeChromeButton', () => {
