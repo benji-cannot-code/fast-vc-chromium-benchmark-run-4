aa163feee6c1df5c6b6bf69f35527b81c10a8afa
Ho Cheung <hocheung@chromium.org>
2025-06-07 13:51:06 -0700

    [gpu] Replace base::Overloaded with absl::Overload
    We are allowing absl::Overload; we should replace base::Overloaded
    with it and remove.
    
    Bug: 392929829
    Change-Id: I848c9dce277170c81b25ed2a645fc836305798e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627039
    Auto-Submit: Ho Cheung <hocheung@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470970}

--- gpu/ipc/common/gpu_surface_tracker.cc
+++ gpu/ipc/common/gpu_surface_tracker.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/containers/contains.h"
-#include "base/functional/overloaded.h"
 #include "build/build_config.h"
+#include "third_party/abseil-cpp/absl/functional/overload.h"
 #include "ui/gl/android/scoped_java_surface.h"
 
 namespace gpu {
@@ -57,7 +57,7 @@ SurfaceRecord GpuSurfaceTracker::AcquireJavaSurface(
                          /*can_be_used_with_surface_control=*/false);
 
   return std::visit(
-      base::Overloaded{
+      absl::Overload{
           [&](const gl::ScopedJavaSurface& surface) {
             DCHECK(surface.IsValid());
             return SurfaceRecord(surface.CopyRetainOwnership(),
