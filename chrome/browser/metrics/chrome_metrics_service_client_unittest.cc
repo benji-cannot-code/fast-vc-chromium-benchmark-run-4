bb5923c84470e6101916df1629aa5574a8a2e5c2
Zifan Zhang <zifanzhang@google.com>
2025-06-09 13:34:33 -0700

    Cleanup the K12AgeClassification feature flag since it is launched
    Bug: 412743953
    Change-Id: Ia3337e2f2bba8d1ebdec34c1b8f31ce3717cf699
    Fix: 412743953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6613263
    Commit-Queue: Zifan Zhang <zifanzhang@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471409}

--- chrome/browser/metrics/chrome_metrics_service_client_unittest.cc
+++ chrome/browser/metrics/chrome_metrics_service_client_unittest.cc
@@ -97,7 +97,6 @@ class ChromeMetricsServiceClientTest : public testing::Test {
 #if BUILDFLAG(IS_CHROMEOS)
     scoped_feature_list_.InitWithFeatures(
         {features::kUmaStorageDimensions,
-         features::kK12AgeClassificationMetricsProvider,
          features::kClassManagementEnabledMetricsProvider,
          metrics::dwa::kDwaFeature},
         {});
