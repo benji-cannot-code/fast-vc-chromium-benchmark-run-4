cdb606342378348bc399206de0891ad4311d64b0
Hidehiko Abe <hidehiko@chromium.org>
2025-06-09 12:18:50 -0700

    Disallow stacking CrosSettings.
    BUG=414927997
    TEST=Tryjob
    
    Change-Id: I73158e23f208afa4ecbcd669682089f4fb9f21a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627395
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471364}

--- chrome/browser/ash/settings/cros_settings_holder.cc
+++ chrome/browser/ash/settings/cros_settings_holder.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/check_is_test.h"
 #include "base/check_op.h"
 #include "base/functional/bind.h"
 #include "chrome/browser/ash/settings/device_settings_provider.h"
@@ -34,22 +33,13 @@ CrosSettingsHolder::CrosSettingsHolder(
   cros_settings_->AddSettingsProvider(
       std::make_unique<SystemSettingsProvider>(notify_callback));
 
-  if (CrosSettings::IsInitialized()) {
-    // On tests, CrosSettings may be overwritten to inject testing behavior.
-    // Keep the existing one.
-    LOG(WARNING)
-        << "CrosSettings instance was injected. Skipping initialization.";
-    CHECK_IS_TEST();
-  } else {
-    CrosSettings::SetInstance(cros_settings_.get());
-  }
+  CrosSettings::SetInstance(cros_settings_.get());
 }
 
 CrosSettingsHolder::~CrosSettingsHolder() {
-  if (CrosSettings::IsInitialized() &&
-      CrosSettings::Get() == cros_settings_.get()) {
-    CrosSettings::SetInstance(nullptr);
-  }
+  // Nobody should override the global instance.
+  CHECK_EQ(CrosSettings::Get(), cros_settings_.get());
+  CrosSettings::SetInstance(nullptr);
 }
 
 }  // namespace ash
