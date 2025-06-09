9036e57a542e0833baa20727fa26900f5342196e
Mason Freed <masonf@chromium.org>
2025-06-09 10:04:20 -0700

    Clean up unnecessary TreeScope check code
    This bug (crbug.com/40260784) was fixed [1] but a few checks never
    got cleaned up. This cleans them up.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4450333
    
    Bug: 40260784
    Change-Id: If021f2724d9779d04ea6ad6ff00a1af6e67d3d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626450
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471269}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -1704,12 +1704,7 @@ void Element::DefaultEventHandler(Event& event) {
 }
 
 Element* Element::anchorElement() const {
-  // TODO(crbug.com/1425215): Fix GetElementAttribute() for out-of-tree-scope
-  // elements, so that we can remove the hack below.
   if (!RuntimeEnabledFeatures::HTMLAnchorAttributeEnabled()) {
-    return nullptr;
-  }
-  if (!IsInTreeScope()) {
     return nullptr;
   }
   return GetElementAttributeResolvingReferenceTarget(html_names::kAnchorAttr);
@@ -1718,14 +1713,7 @@ Element* Element::anchorElement() const {
 // For JavaScript binding, return the anchor element without resolving the
 // reference target, to avoid exposing shadow root content to JS.
 Element* Element::anchorElementForBinding() const {
-  // TODO(crbug.com/1425215): Fix GetElementAttribute() for out-of-tree-scope
-  // elements, so that we can remove the hack below.
-  if (!RuntimeEnabledFeatures::HTMLAnchorAttributeEnabled()) {
-    return nullptr;
-  }
-  if (!IsInTreeScope()) {
-    return nullptr;
-  }
+  CHECK(RuntimeEnabledFeatures::HTMLAnchorAttributeEnabled());
   return GetElementAttribute(html_names::kAnchorAttr);
 }
 
