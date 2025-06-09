a609a6ab10dcb2e67da83ccba2c1e3db1f4c9138
Daniel Cheng <dcheng@chromium.org>
2025-06-09 13:53:01 -0700

    Remove vector use in base/containers/span.h
    constexpr evaluation disallows comparing pointers from
    two different objects, but `copy_from()` uses pointer comparisons to
    determine which, if any parts, of the source and destination range
    overlap.
    
    The initial workaround copied the elements to a local std::vector to
    guarantee no overlap; efficiency-wise, this doesn't matter, since it's
    just accounting at compile-time. However, <vector> is quite large and
    no other part of the span implementation needs std::vector.
    
    Instead, just "allocate" a buffer for storing the copied elements and
    manage it manually.
    
    Bug: 423048781
    Change-Id: I369b11a9d13f35b437b1f8ffcc0d3076ddcde118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6628957
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471422}

--- ppapi/shared_impl/vpn_provider_util.h
+++ ppapi/shared_impl/vpn_provider_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PPAPI_SHARED_IMPL_VPN_PROVIDER_UTIL_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/memory/shared_memory_mapping.h"
 #include "base/memory/unsafe_shared_memory_region.h"
