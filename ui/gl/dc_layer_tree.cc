fd2074ed3a589921f89b328aa754ccc5d0c21220
Michael Tang <tangm@microsoft.com>
2025-06-09 12:00:50 -0700

    Add histogram to track number of OS compositor layers touched per frame
    This CL adds GPU.OsCompositor.NumLayersModified, which tracks the number
    of OS compositor layers (e.g. DComp visual subtrees) modified in a
    commit. This estimates the amount of work the OS compositor needs to do
    per frame and can be used to compare incremental tree updating
    algorithms.
    
    Bug: 324460866
    Change-Id: I8782600989d720c3469fb06bd722c400f4ba178a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618128
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1471356}

--- ui/gl/dc_layer_tree.cc
+++ ui/gl/dc_layer_tree.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
@@ -925,6 +926,8 @@ base::expected<void, CommitError> DCLayerTree::VisualTree::BuildTree(
     }
   }
 
+  size_t num_layers_modified = 0;
+
   // This loop walks the overlays and builds or updates the visual subtree for
   // each overlay. |left_sibling_visual| is required to properly stack visual
   // subtrees that are detached from the root visual.
@@ -979,7 +982,7 @@ base::expected<void, CommitError> DCLayerTree::VisualTree::BuildTree(
     const bool allow_antialiasing = !layers_with_multiple_overlays.contains(
         overlays[i].layer_id.shared_quad_state_layer_id());
 
-    needs_commit |= visual_subtrees[i]->Update(
+    const bool visual_needs_commit = visual_subtrees[i]->Update(
         dc_layer_tree_->dcomp_device_.Get(), dcomp_visual_content,
         dcomp_surface_serial, image_size, overlays[i].content_rect,
         background_color_surface,
@@ -992,9 +995,13 @@ base::expected<void, CommitError> DCLayerTree::VisualTree::BuildTree(
       HRESULT hr = dc_layer_tree_->dcomp_root_visual_.Get()->AddVisual(
           visual_subtree->container_visual(), TRUE, left_sibling_visual);
       CHECK_EQ(hr, S_OK);
-      needs_commit = true;
     }
     left_sibling_visual = visual_subtree->container_visual();
+
+    if (visual_needs_commit || !subtree_attached_to_root) {
+      num_layers_modified++;
+      needs_commit = true;
+    }
 
     layer_ids_for_testing.push_back(overlays[i].layer_id);
   }
@@ -1003,6 +1010,9 @@ base::expected<void, CommitError> DCLayerTree::VisualTree::BuildTree(
   subtree_map_ = std::move(subtree_map);
   visual_subtrees_ = std::move(visual_subtrees);
   layer_ids_for_testing_ = std::move(layer_ids_for_testing);
+
+  UMA_HISTOGRAM_COUNTS("GPU.OsCompositor.NumLayersModified",
+                       num_layers_modified);
 
   if (needs_commit) {
     TRACE_EVENT0("gpu", "DCLayerTree::CommitAndClearPendingOverlays::Commit");
--- ui/gl/os_compositor_tree_base.h
+++ ui/gl/os_compositor_tree_base.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/containers/span.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/gfx/overlay_layer_id.h"
@@ -373,6 +374,9 @@ bool OsCompositorTreeBase<OverlayParams, Layer>::UpdateTree(
   DVLOG(1) << "layers: modified = " << num_layers_modified
            << " / total = " << layers_.size();
   num_layers_modified_last_frame_ = num_layers_modified;
+
+  UMA_HISTOGRAM_COUNTS("GPU.OsCompositor.NumLayersModified",
+                       num_layers_modified);
 
   if (num_layers_modified > 0) {
     TRACE_EVENT("gpu", "Commit overlay layers");
