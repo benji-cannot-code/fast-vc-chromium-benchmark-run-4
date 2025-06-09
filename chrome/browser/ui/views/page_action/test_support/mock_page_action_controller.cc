3ea9b762243208d140fb762a843a0892c6afe7eb
Kaan Alsan <alsan@chromium.org>
2025-06-09 13:10:52 -0700

    Define a mock PageActionController for testing
    Change-Id: I36ad4935f261917b6b9f818084bd70f42d99aeef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6628314
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471397}

---
+++ chrome/browser/ui/views/page_action/test_support/mock_page_action_controller.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/page_action/test_support/mock_page_action_controller.h"
+
+namespace page_actions {
+
+MockPageActionController::MockPageActionController() = default;
+MockPageActionController::~MockPageActionController() = default;
+
+}  // namespace page_actions
