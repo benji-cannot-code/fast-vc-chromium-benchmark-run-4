28b464495b69efec18ccf3788a0a699cdfc3c560
Peter Kvitek <kvitekp@chromium.org>
2025-06-09 13:06:35 -0700

    [headless] Fix maximized/fullscreen window size in headless shell
    Headless shell used to have extremely minimalist window state support,
    essentially it would just store and report back the requested windows
    state. This was enough because until recently headless did not have a
    concept of headless screen so there was no screen size to zoom a window
    to.
    
    http://crrev.com/c/6021359 added headless screen configuration to
    headless shell, so this CL refactors headless shell window state
    handling to use headless screen work area for maximized/fullscreen
    windows.
    
    Bug: 422844474
    Change-Id: I6210d2e975d3e8c51b907b7465160299a9997a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625635
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471392}

--- headless/lib/browser/headless_web_contents_impl.h
+++ headless/lib/browser/headless_web_contents_impl.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "content/public/browser/render_process_host_observer.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "headless/lib/browser/headless_window.h"
+#include "headless/lib/browser/headless_window_delegate.h"
 #include "headless/lib/browser/headless_window_tree_host.h"
 #include "headless/public/headless_export.h"
 #include "headless/public/headless_web_contents.h"
@@ -34,7 +36,8 @@ namespace headless {
 class HeadlessBrowserImpl;
 
 // Exported for tests.
-class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents {
+class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents,
+                                                public HeadlessWindowDelegate {
  public:
   HeadlessWebContentsImpl(const HeadlessWebContentsImpl&) = delete;
   HeadlessWebContentsImpl& operator=(const HeadlessWebContentsImpl&) = delete;
@@ -67,10 +70,14 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents {
     return window_tree_host_.get();
   }
   int window_id() const { return window_id_; }
-  HeadlessWindowState window_state() const { return window_state_; }
 
-  // Set the WebContent's platform window state and visibility.
+  // Set the WebContent's platform window visibility.
+  void SetVisible(bool visible);
+
+  // Set the WebContent's platform window state.
   void SetWindowState(HeadlessWindowState window_state);
+
+  HeadlessWindowState GetWindowState() const;
 
   // Set bounds of WebContent's platform window.
   void SetBounds(const gfx::Rect& bounds);
@@ -90,6 +97,10 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents {
                   bool capture_screenshot,
                   FrameFinishedCallback frame_finished_callback);
 
+  // HeadlessWindowDelegate:
+  void OnVisibilityChanged() override;
+  void OnBoundsChanged(const gfx::Rect& old_bounds) override;
+
  private:
   explicit HeadlessWebContentsImpl(
       std::unique_ptr<content::WebContents> web_contents);
@@ -104,8 +115,8 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl : public HeadlessWebContents {
   class Delegate;
   std::unique_ptr<Delegate> web_contents_delegate_;
   std::unique_ptr<HeadlessWindowTreeHost> window_tree_host_;
+  std::unique_ptr<HeadlessWindow> headless_window_;
   int window_id_ = 0;
-  HeadlessWindowState window_state_ = HeadlessWindowState::kNormal;
   std::unique_ptr<content::WebContents> const web_contents_;
 
   class PendingFrame;
