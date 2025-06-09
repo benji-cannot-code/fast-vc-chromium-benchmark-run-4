e493f37d89e683c749d2cac1be0f2b0cfab1ecb3
Christoph Schwering <schwering@google.com>
2025-06-09 13:37:18 -0700

    [Autofill] Clean up AutofillDisallowSlashDotLabels
    Bug: 396325496
    Change-Id: Id0438a1b7c9e7b8c9048f70d65fc7bc845920065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624441
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1471410}

--- components/autofill/ios/form_util/autofill_form_features_injector_unittest.mm
+++ components/autofill/ios/form_util/autofill_form_features_injector_unittest.mm
@@ -63,7 +63,6 @@ TEST_F(AutofillFormInjectorTest, InjectFlagsWebFrames) {
       {kAutofillIsolatedWorldForJavascriptIos,
        autofill::features::kAutofillAcrossIframesIos,
        autofill::features::kAutofillAcrossIframesIosThrottling,
-       autofill::features::kAutofillDisallowSlashDotLabels,
        kAutofillCorrectUserEditedBitInParsedField,
        kAutofillAllowDefaultPreventedSubmission, kAutofillDedupeFormSubmission},
       /* disabled_features= */ {});
@@ -83,8 +82,6 @@ TEST_F(AutofillFormInjectorTest, InjectFlagsWebFrames) {
                     u"setAutofillAcrossIframes(true);",
                     u"__gCrWeb.autofill_form_features."
                     u"setAutofillAcrossIframesThrottling(true);",
-                    u"__gCrWeb.autofill_form_features."
-                    u"setAutofillDisallowSlashDotLabels(true);",
                     u"__gCrWeb.autofill_form_features."
                     u"setAutofillCorrectUserEditedBitInParsedField(true);",
                     u"__gCrWeb.autofill_form_features."
