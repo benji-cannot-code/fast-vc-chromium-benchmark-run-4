a7ab6d2679da6a3152633533fb4384e4ef37fcf8
Will Harris <wfh@chromium.org>
2025-06-09 07:20:20 -0700

    Convert StoreAndReplyWithKey to take pair rather than tuple
    Previously StoreAndReplyWithKey would take a tuple of a vector and a
    const ref to an optional vector.
    
    This didn't work because of an unexpected interaction between
    base::expected and std::tuple when passing refs - see
    crbug.com/423313075 - so switch this to be a pair instead with no refs,
    and instead std::move the pair containing the two vectors between the
    Decrypt call and the StoreAndReplyWithKey function, and update the other
    call.
    
    Since this change fixes the bug, the speculative revert introduced in
    StoreKey in crrev.com/c/6621542 is removed, and replaced with the better
    function suggested by grt@ in the review.
    
    This means the disabled test can be re-enabled. In addition, one more
    expectation is added to the browser test to verify that these empty
    ciphertexts are never stored.
    
    Further CLs will add more integration tests to verify the complete
    end-to-end behavior but this CL is believed sufficient to fix the
    current issue.
    
    BUG=415979068
    
    Change-Id: I740438f664b621dc58306801dc115c9dee71dcdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629710
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471180}

--- chrome/browser/os_crypt/app_bound_encryption_provider_win.h
+++ chrome/browser/os_crypt/app_bound_encryption_provider_win.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 #include <string>
-#include <tuple>
+#include <utility>
+#include <vector>
 
 #include "base/containers/span.h"
 #include "base/gtest_prod_util.h"
@@ -85,12 +86,11 @@ class AppBoundEncryptionProviderWin : public os_crypt_async::KeyProvider {
   RetrieveEncryptedKey();
   void HandleEncryptedKey(ReadWriteKeyData decrypted_key,
                           KeyCallback callback,
-                          const OptionalReadOnlyKeyData& encrypted_key);
+                          OptionalReadOnlyKeyData encrypted_key);
   void StoreAndReplyWithKey(
       KeyCallback callback,
-      base::expected<
-          std::tuple<ReadWriteKeyData, const OptionalReadOnlyKeyData&>,
-          KeyProvider::KeyError> key_pair);
+      base::expected<std::pair<ReadWriteKeyData, OptionalReadOnlyKeyData>,
+                     KeyProvider::KeyError> key_pair);
   void StoreKey(base::span<const uint8_t> encrypted_key);
 
   raw_ptr<PrefService> local_state_ GUARDED_BY_CONTEXT(sequence_checker_);
