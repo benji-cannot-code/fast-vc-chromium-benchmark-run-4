a47fc178434f9e555abbcd960d0a0b365c325803
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2025-06-09 09:46:34 -0700

    [views-ax] Remove dead code WidgetAXTreeIDMap
    Found this unused code added in 2020 while working on ViewsAX. Cleaning
    it up.
    
    Bug: 40672441
    Change-Id: I22146d8cbfb68a366b5e148a6b42fe8de7f65613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619701
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1471256}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/atomic_view_ax_tree_manager.h"
 #include "ui/views/accessibility/ax_update_notifier.h"
 #include "ui/views/accessibility/ax_virtual_view.h"
-#include "ui/views/accessibility/widget_ax_tree_id_map.h"
 #include "ui/views/view.h"
 #include "ui/views/view_utils.h"
 #include "ui/views/widget/root_view.h"
