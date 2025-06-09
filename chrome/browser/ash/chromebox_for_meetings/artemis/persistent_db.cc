7b8b0beb7504a304856dbfae5ecda8bd114cd801
Daniel Cheng <dcheng@chromium.org>
2025-06-09 14:24:32 -0700

    Fix another lingering IWYU issue in linux-cfm-rel
    Bug: 423048781
    Change-Id: Iea752494b5f1a05758b2bed1ea32cd8e279216be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624843
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1471429}

--- chrome/browser/ash/chromebox_for_meetings/artemis/persistent_db.cc
+++ chrome/browser/ash/chromebox_for_meetings/artemis/persistent_db.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/chromebox_for_meetings/artemis/persistent_db.h"
 
+#include <ostream>
+
 #include "base/check.h"
 
 namespace ash::cfm {
