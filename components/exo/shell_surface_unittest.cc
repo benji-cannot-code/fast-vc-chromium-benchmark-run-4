5979c5a58d9495e557e2d66b50e7099efcf43199
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-06-09 15:50:42 -0700

    Remove to usage of feature flag `IsRoundedWindowsEnabled()`
    Bug: b:410475201
    Change-Id: I50e1c1556984cfbec2c41fe21edf819708e58f95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625236
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471482}

--- components/exo/shell_surface_unittest.cc
+++ components/exo/shell_surface_unittest.cc
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
 #include "base/test/mock_callback.h"
-#include "base/test/scoped_feature_list.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/base/app_types.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "components/app_restore/window_properties.h"
@@ -3242,12 +3240,6 @@ TEST_F(ShellSurfaceTest, ShadowRoundedCorners) {
   constexpr gfx::Point kOrigin(20, 20);
   constexpr int kWindowCornerRadius = 12;
 
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures(
-      {chromeos::features::kRoundedWindows,
-       chromeos::features::kFeatureManagementRoundedWindows},
-      /*disabled_features=*/{});
-
   std::unique_ptr<ShellSurface> shell_surface =
       test::ShellSurfaceBuilder({256, 256})
           .SetOrigin(kOrigin)
@@ -3289,12 +3281,6 @@ TEST_F(ShellSurfaceTest, ShadowRoundedCorners) {
 TEST_F(ShellSurfaceTest, RoundedWindows) {
   constexpr gfx::Point kOrigin(20, 20);
   constexpr int kWindowCornerRadius = 12;
-
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures(
-      {chromeos::features::kRoundedWindows,
-       chromeos::features::kFeatureManagementRoundedWindows},
-      /*disabled_features=*/{});
 
   std::unique_ptr<ShellSurface> shell_surface =
       test::ShellSurfaceBuilder({256, 256})
