c9193c58c1aea80ded290b4181a844eee271e4a6
Slobodan Pejic <slobodan@chromium.org>
2025-06-09 12:54:56 -0700

    Fix clientData serialization when creating payment credentials.
    Before this change, an extrenous quotation mark was added to the
    serialized client data json when creating a credential with the payment
    extension enabled and the browser bound key flag enabled. This resulted
    in the serialized json being malformed. After this change, the client
    data is serialized correctly without the extreanous quotation mark.
    
    Browser bound keys are behind the flag
    chrome://flags/#enable-secure-payment-confirmation-browser-bound-key.
    
    Bug: 423415330
    Change-Id: I9e0b3c3931162ea04596b232ea2a9798feebba34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629334
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471383}

--- content/public/android/javatests/src/org/chromium/content_public/browser/ClientDataJsonTest.java
+++ content/public/android/javatests/src/org/chromium/content_public/browser/ClientDataJsonTest.java
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content_public.browser;
 
 import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import static org.chromium.base.test.util.Matchers.containsString;
+
+import android.util.JsonReader;
+import android.util.JsonToken;
 
 import androidx.test.filters.SmallTest;
 
@@ -26,6 +30,9 @@ import org.chromium.payments.mojom.PaymentCurrencyAmount;
 import org.chromium.url.GURL;
 import org.chromium.url.Origin;
 import org.chromium.url.mojom.Url;
+
+import java.io.IOException;
+import java.io.StringReader;
 
 /** Unit tests for ClientDataJson */
 @RunWith(BaseJUnit4ClassRunner.class)
@@ -58,6 +65,7 @@ public class ClientDataJsonTest {
                         RELYING_PARTY_ID,
                         TOP_ORIGIN);
 
+        assertParsesJson(output);
         // Test that the output has the expected fields.
         assertThat(output, containsString("\"type\":\"payment.get\""));
         assertThat(output, containsString("\"challenge\":\"AAAA\""));
@@ -95,6 +103,8 @@ public class ClientDataJsonTest {
                         payment,
                         RELYING_PARTY_ID,
                         TOP_ORIGIN);
+
+        assertParsesJson(output);
         assertThat(output, containsString("\"type\":\"webauthn.create\""));
         assertThat(output, containsString("\"challenge\":\"AAAA\""));
         assertThat(output, containsString(String.format("\"origin\":\"%s\"", ORIGIN)));
@@ -117,6 +127,7 @@ public class ClientDataJsonTest {
                         RELYING_PARTY_ID,
                         TOP_ORIGIN);
 
+        assertParsesJson(output);
         assertThat(output, containsString("\"paymentEntitiesLogos\":[]"));
     }
 
@@ -139,6 +150,7 @@ public class ClientDataJsonTest {
                         RELYING_PARTY_ID,
                         TOP_ORIGIN);
 
+        assertParsesJson(output);
         assertThat(
                 output,
                 containsString(
@@ -166,10 +178,26 @@ public class ClientDataJsonTest {
                         RELYING_PARTY_ID,
                         TOP_ORIGIN);
 
+        assertParsesJson(output);
         assertThat(
                 output,
                 containsString(
                         "\"paymentEntitiesLogos\":[{\"url\":\"https://www.example.test/logo_one.png\",\"label\":\"logo_one_label\"},{\"url\":\"https://www.example.test/logo_two.png\",\"label\":\"logo_two_label\"}]"));
+    }
+
+    private void assertParsesJson(String serializedJson) {
+        try {
+            JsonReader jsonReader = new JsonReader(new StringReader(serializedJson));
+            // skipValue() throws a subclass of IOException when the serializedJson cannot be
+            // parsed.
+            jsonReader.skipValue();
+            assertEquals(
+                    "There is more than one value in the serialized json: " + serializedJson,
+                    JsonToken.END_DOCUMENT,
+                    jsonReader.peek());
+        } catch (IOException e) {
+            throw new AssertionError("Could not parse the serialized json: " + serializedJson, e);
+        }
     }
 
     private PaymentOptions createSamplePaymentOptions() {
