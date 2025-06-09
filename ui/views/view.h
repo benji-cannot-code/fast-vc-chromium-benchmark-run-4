34b7934b2eb5444e6f4c7a54e41786d9236fadd0
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2025-06-09 11:46:08 -0700

    Reland "[views-ax] Cache Widget on View"
    This is a reland of commit 9ee0346611064af030d36d07a21233a251dbe5cf
    
    What changed: We're now closing the widget at the end of
    ViewTest.WidgetCachedOnViews and manually freeing the memory of
    `view_1`. Bug that led to the revert: https://crbug.com/422903251
    
    Original change's description:
    > [views-ax] Cache Widget on View
    >
    > This CL introduces a raw_ptr<Widget> member in View to store the owning
    > Widget when the view is added to it. This replaces the need to walk up
    > the view hierarchy to the RootView, which overrides GetWidget() to
    > return the associated Widget. Improves performance for frequent widget
    > lookups in large view trees. Pointer is cleared before Widget
    > destruction to avoid dangling references.
    >
    > Change-Id: I43b39ffa929c35701e25b88e54faecace82a2bad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604284
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1470280}
    
    Bug: 40672441
    Change-Id: Iac347ade6a6a4aecef9a3eed7954d60275a2dab5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626668
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1471341}

--- ui/views/view.h
+++ ui/views/view.h
@@ -2037,6 +2037,8 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   friend class internal::RootView;
   friend class internal::ScopedChildrenLock;
   friend class FocusManager;
+  // TODO(https://crbug.com/422382420): unfriend this.
+  friend class TestBubbleFrameView;
   friend class ViewDebugWrapperImpl;
   friend class ViewLayerTest;
   friend class ViewLayerPixelCanvasTest;
@@ -2130,6 +2132,8 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
 
   // Calls ViewHierarchyChanged() and notifies observers.
   void ViewHierarchyChangedImpl(const ViewHierarchyChangedDetails& details);
+
+  void SetWidget(Widget* widget);
 
   // Size and disposition ------------------------------------------------------
 
@@ -2376,6 +2380,10 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   int group_ = -1;
 
   // Tree operations -----------------------------------------------------------
+
+  // The widget that this view is attached to. This is null if the view is not
+  // attached to a widget.
+  raw_ptr<Widget> widget_ = nullptr;
 
   // This view's parent.
   raw_ptr<View> parent_ = nullptr;
