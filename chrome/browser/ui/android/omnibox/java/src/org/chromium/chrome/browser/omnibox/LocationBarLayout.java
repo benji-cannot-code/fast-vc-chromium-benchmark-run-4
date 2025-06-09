ecd26d83139bb28c2b5ddb93f1629526763c68e2
Neil Coronado <nemco@google.com>
2025-06-09 14:06:08 -0700

    [LocationBarMerge] Remove left/right Spaces in phone location bar
    Remove left/right Spaces in phone location bar.
    -Previously used for animations, but have since been replaced.
    
    Bug: 422837255
    Change-Id: Iaba678708d705045cef029e7d33e5a983de8cd39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625414
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1471428}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -49,8 +49,6 @@ public class LocationBarLayout extends FrameLayout {
     protected ImageButton mLensButton;
     protected ImageButton mComposeplateButton;
     protected UrlBar mUrlBar;
-    protected View mStatusViewLeftSpace;
-    protected View mStatusViewRightSpace;
 
     protected UrlBarCoordinator mUrlCoordinator;
     protected AutocompleteCoordinator mAutocompleteCoordinator;
@@ -91,8 +89,6 @@ public class LocationBarLayout extends FrameLayout {
         mLensButton = findViewById(R.id.lens_camera_button);
         mComposeplateButton = findViewById(R.id.composeplate_button);
         mUrlActionContainer = findViewById(R.id.url_action_container);
-        mStatusViewLeftSpace = findViewById(R.id.location_bar_status_view_left_space);
-        mStatusViewRightSpace = findViewById(R.id.location_bar_status_view_right_space);
         mMinimumUrlBarWidthPx =
                 context.getResources().getDimensionPixelSize(R.dimen.location_bar_min_url_width);
         mStatusIconAndUrlBarOffset =
@@ -409,7 +405,7 @@ public class LocationBarLayout extends FrameLayout {
     }
 
     /**
-     * Expand the left and right space besides the status view, and increase the location bar
+     * Expand the left and right margins besides the status view, and increase the location bar
      * vertical padding based on current animation progress percent.
      *
      * @param ntpSearchBoxScrollFraction The degree to which the omnibox has expanded to full width
@@ -424,16 +420,15 @@ public class LocationBarLayout extends FrameLayout {
             boolean isUrlFocusChangeInProgress) {
         mIsUrlFocusChangeInProgress = isUrlFocusChangeInProgress;
         mUrlFocusPercentage = Math.max(ntpSearchBoxScrollFraction, urlFocusChangeFraction);
-        setStatusViewLeftSpacePercent(
+        setStatusViewLeftMarginPercent(
                 ntpSearchBoxScrollFraction, urlFocusChangeFraction, isUrlFocusChangeInProgress);
-        setStatusViewRightSpacePercent(
+        setStatusViewRightMarginPercent(
                 ntpSearchBoxScrollFraction, urlFocusChangeFraction, isUrlFocusChangeInProgress);
     }
 
     /**
-     * Set the "left space width" based on current animation progress percent. This can either
-     * mutate the width of a Space view to the left of the status view or use translation to
-     * accomplish the same thing without triggering a relayout.
+     * Set the "left margin width" based on current animation progress percent. This uses
+     * translation to avoid triggering a relayout.
      *
      * @param ntpSearchBoxScrollFraction The degree to which the omnibox has expanded to full width
      *     in NTP due to the NTP search box is being scrolled up.
@@ -441,13 +436,12 @@ public class LocationBarLayout extends FrameLayout {
      *     getting focused.
      * @param isUrlFocusChangeInProgress True if the url focus change is in progress.
      */
-    protected void setStatusViewLeftSpacePercent(
+    protected void setStatusViewLeftMarginPercent(
             float ntpSearchBoxScrollFraction,
             float urlFocusChangeFraction,
             boolean isUrlFocusChangeInProgress) {
         float maxPercent = Math.max(ntpSearchBoxScrollFraction, urlFocusChangeFraction);
         boolean isOnTablet = DeviceFormFactor.isNonMultiDisplayContextOnTablet(getContext());
-        // The tablet UI doesn't have status view spacer elements so must use translation.
         float translationX;
         if (!isOnTablet && isUrlFocusChangeInProgress && ntpSearchBoxScrollFraction == 1) {
             translationX =
@@ -463,9 +457,8 @@ public class LocationBarLayout extends FrameLayout {
     }
 
     /**
-     * Set the "right space width" based on current animation progress percent. This can either
-     * mutate the width of a Space view to the right of the status view or use translation to
-     * accomplish the same thing without triggering a relayout.
+     * Set the "right margin width" based on current animation progress percent. This uses
+     * translation to avoid triggering a relayout.
      *
      * @param ntpSearchBoxScrollFraction The degree to which the omnibox has expanded to full width
      *     in NTP due to the NTP search box is being scrolled up.
@@ -473,11 +466,10 @@ public class LocationBarLayout extends FrameLayout {
      *     getting focused.
      * @param isUrlFocusChangeInProgress True if the url focus change is in progress.
      */
-    protected void setStatusViewRightSpacePercent(
+    protected void setStatusViewRightMarginPercent(
             float ntpSearchBoxScrollFraction,
             float urlFocusChangeFraction,
             boolean isUrlFocusChangeInProgress) {
-        // The tablet UI doesn't have status view spacer elements so must use translation.
         float translationX;
         if (mUrlBarLaidOutAtFocusedWidth) {
             translationX =
