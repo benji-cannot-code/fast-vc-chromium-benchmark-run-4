c5ae63d29cd10eab5d43459ed65f54f5681ca5e9
Minseop Choi <minsubb13@gmail.com>
2025-06-09 15:17:57 -0700

    components/nearby: migrate to new crypto/hash API
    Migrate crypto of nearby in components/cross_device from crypto/sha2 to
    crypto/hash. The new interfaces are a bit more efficient due to
    making fewer heap allocations and a bit more resistant to misuse.
    They are also not deprecated.
    
    Bug: 374310081
    Change-Id: Idd37c111bf425c79d5e456db32f9da425d3e30c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6589491
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Euisang Lim <eui-sang.lim@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1471467}

--- components/cross_device/nearby/crypto.cc
+++ components/cross_device/nearby/crypto.cc
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/nearby/src/internal/platform/implementation/crypto.h"
 
+#include <vector>
+
 #include "base/containers/span.h"
 #include "base/hash/md5.h"
 #include "base/memory/ptr_util.h"
-#include "crypto/sha2.h"
-
-#include <vector>
-
+#include "base/strings/string_view_util.h"
+#include "crypto/hash.h"
 namespace nearby {
 
 void Crypto::Init() {}
@@ -29,7 +29,8 @@ ByteArray Crypto::Sha256(std::string_view input) {
   if (input.empty())
     return ByteArray();
 
-  return ByteArray(crypto::SHA256HashString(std::string(input)));
+  return ByteArray(
+      std::string(base::as_string_view(crypto::hash::Sha256(input))));
 }
 
 }  // namespace nearby
