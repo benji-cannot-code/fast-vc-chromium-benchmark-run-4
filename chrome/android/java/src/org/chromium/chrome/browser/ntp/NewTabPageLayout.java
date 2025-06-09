8d5f6dcf5a9a54579101a1d22e4d0808c7ad5032
Xinyi Ji <xinyiji@google.com>
2025-06-09 07:09:50 -0700

    Fix "NewTabPage.SearchBox.LensShown" UserAction
    This CL fixes a bug that caused a single user action to be logged multiple times. With this change, the action is now logged only once per display.
    
    Change-Id: Ia506ef7e1132dfa9c97194a0a4d50dabf3018429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629694
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471178}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
@@ -912,7 +912,6 @@ public class NewTabPageLayout extends LinearLayout
                 mSearchBoxCoordinator.isLensEnabled(LensEntryPoint.NEW_TAB_PAGE);
         if (!mIsComposeplateEnabled) {
             mSearchBoxCoordinator.setVoiceSearchButtonVisibility(shouldShowVoiceSearchButton);
-            LensMetrics.recordShown(LensEntryPoint.NEW_TAB_PAGE, shouldShowLensButton);
             mSearchBoxCoordinator.setLensButtonVisibility(shouldShowLensButton);
             updatePreviousButtonVisibilityAndRecordMetrics(
                     shouldShowVoiceSearchButton,
@@ -931,9 +930,6 @@ public class NewTabPageLayout extends LinearLayout
         mComposeplateCoordinator.setVisibility(
                 shouldShowComposeplateButton, mManager.isCurrentPage());
 
-        // The lens button will be shown either in the fake search box or the composeplate view.
-        LensMetrics.recordShown(
-                LensEntryPoint.NEW_TAB_PAGE, isLensButtonVisible || shouldShowComposeplateButton);
         updatePreviousButtonVisibilityAndRecordMetrics(
                 isVoiceSearchButtonVisible, isLensButtonVisible, shouldShowComposeplateButton);
     }
@@ -956,6 +952,14 @@ public class NewTabPageLayout extends LinearLayout
                         && mPreviousLensButtonVisible != null
                         && isLensButtonVisible == mPreviousLensButtonVisible)) {
             return;
+        }
+
+        if (mPreviousLensButtonVisible == null
+                || isLensButtonVisible != mPreviousLensButtonVisible) {
+            // The lens button will be shown either in the fake search box or the composeplate view.
+            LensMetrics.recordShown(
+                    LensEntryPoint.NEW_TAB_PAGE,
+                    isLensButtonVisible || isComposeplateButtonVisible);
         }
 
         ComposeplateMetricsUtils.recordFakeSearchBoxImpression();
