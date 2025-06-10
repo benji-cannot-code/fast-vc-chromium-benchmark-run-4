411ea63faf566ee6e46b9d4c6f35c83b0129de1d
Peter KH <pkotwicz@google.com>
2025-06-10 09:19:41 -0700

    [Settings] Update strings for V8-optimizer-settings on "Security Page"
    This CL:
    - Updates the strings in the V8-optimizer row on the settings "security page".
    - Makes the sublabel for the V8-optimizer row dynamic based on the setting state similar to the "Ad privacy" settings page.
    
    Bug:416087291
    Screenshots: https://crbug.com/416087291#c2
    
    Change-Id: I825d2f6af1300475a605d42e8f49df72922483a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576621
    Reviewed-by: Awad Osman <awado@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471846}

--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -689,7 +689,6 @@ IN_PROC_BROWSER_TEST_F(SettingsTest, SyncAccountControl) {
 }
 #endif
 
-
 IN_PROC_BROWSER_TEST_F(SettingsTest, TabDiscardExceptionDialog) {
   RunTest("settings/tab_discard_exception_dialog_test.js", "mocha.run()");
 }
@@ -935,8 +934,7 @@ IN_PROC_BROWSER_TEST_F(SettingsBrowserTest, MemorySaverAggressiveness) {
           "runMochaSuite('MemorySaverAggressiveness')");
 }
 
-class SettingsPersonalizationOptionsTest : public SettingsBrowserTest {
-};
+class SettingsPersonalizationOptionsTest : public SettingsBrowserTest {};
 
 IN_PROC_BROWSER_TEST_F(SettingsPersonalizationOptionsTest, AllBuilds) {
   RunTest("settings/personalization_options_test.js",
@@ -1468,6 +1466,11 @@ IN_PROC_BROWSER_TEST_F(SettingsSecurityPageTest,
                        SecurityPageHappinessTrackingSurveys) {
   RunTest("settings/security_page_test.js",
           "runMochaSuite('SecurityPageHappinessTrackingSurveys')");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsSecurityPageTest, JavascriptOptimizer) {
+  RunTest("settings/security_page_test.js",
+          "runMochaSuite('JavascriptOptimizer')");
 }
 
 // TODO(crbug/338155508): Enable this flaky test. This is flaky on Linux debug
