305ecfb2827f314dba1084fca89bcba443dff4b5
Valerie Young <spectranaut@igalia.com>
2025-06-10 09:32:44 -0700

    [ChromeVox] Fix ChromeVoxISearchTest.Simple Test for MV3
    Removed unnecessary logic that replied on deprecated API.
    
    Bug: 388867840
    Change-Id: Ic989024f966b80cab04ea0e6336fcc33ce217bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6613836
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1471855}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/testing/chromevox_e2e_test_base.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/testing/chromevox_e2e_test_base.js
@@ -17,17 +17,6 @@ GEN_INCLUDE(['mock_feedback.js']);
 ChromeVoxE2ETest = class extends E2ETestBase {
   constructor() {
     super();
-
-    if (this.runtimeDeps.length > 0) {
-      chrome.extension.getViews().forEach(view => {
-        this.runtimeDeps.forEach(dep => {
-          if (view[dep]) {
-            window[dep] = view[dep];
-          }
-        });
-      });
-    }
-
     this.originalOutputContextValues_ = {};
   }
 
@@ -124,14 +113,6 @@ ChromeVoxE2ETest = class extends E2ETestBase {
   doGesture(gesture, opt_x, opt_y) {
     return () => GestureCommandHandler.instance.onAccessibilityGesture_(
                gesture, opt_x, opt_y);
-  }
-
-  /**
-   * Dependencies defined on a background window other than this one.
-   * @type {!Array<string>}
-   */
-  get runtimeDeps() {
-    return [];
   }
 
   /** @override */
