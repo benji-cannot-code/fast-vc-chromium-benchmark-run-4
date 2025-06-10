c6ceb26869e7f7358c7310741ca72b04d676d248
Piotr Kotynia <piotrkotynia@google.com>
2025-06-10 11:29:28 -0700

    [Android] Pass payments-related params through the suggestion payload
    The Payload class has recently been reflected on the Java side, enabling
    AutofillProfilePayload to be passed through C++ code. This change aims
    to remove payments-related code from the AutofillSuggestion class,
    making it feature-agnostic. To achieve this, the PaymentsPayload Java
    class has been added. This particular change is a no-op.
    
    Change-Id: Ib354e7ef15e707c379f969b306b855484ed86c23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624145
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1471955}

--- components/autofill/core/browser/suggestions/suggestion.cc
+++ components/autofill/core/browser/suggestions/suggestion.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "components/autofill/android/main_autofill_jni_headers/AutofillProfilePayload_jni.h"
+#include "components/autofill/android/main_autofill_jni_headers/PaymentsPayload_jni.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
 namespace autofill {
@@ -300,6 +301,16 @@ Suggestion::PaymentsPayload& Suggestion::PaymentsPayload::operator=(
     PaymentsPayload&&) = default;
 
 Suggestion::PaymentsPayload::~PaymentsPayload() = default;
+
+#if BUILDFLAG(IS_ANDROID)
+base::android::ScopedJavaLocalRef<jobject>
+Suggestion::PaymentsPayload::CreateJavaObject() const {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  return Java_PaymentsPayload_Constructor(
+      env, main_text_content_description, should_display_terms_available,
+      guid.value(), is_local_payments_method);
+}
+#endif  // BUILDFLAG(IS_ANDROID)
 
 Suggestion::IPHMetadata::IPHMetadata() = default;
 
