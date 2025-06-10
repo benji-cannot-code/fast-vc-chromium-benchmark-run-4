5be029327a74f80ac6ad2043049b77d0c32704e9
Eshwar Stalin <estalin@chromium.org>
2025-06-10 08:35:20 -0700

    [SxS] Cleaning up interface between BrowserView and MultiContentsView
    This CL cleans up the interface between BrowserView and
    MultiContentsView into a delegate. It also makes a number of BrowserView
    methods for managing the split view private so its only accessible
    internally and isn't available to external clients. Using a delegate
    instead of separate callbacks to streamline things for easier
    extensibility for the future.
    
    Change-Id: Ib5bafa0f9b06ca7d70f4fbf8212948f0a87f8632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6631671
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471822}

--- chrome/browser/ui/views/frame/multi_contents_view.cc
+++ chrome/browser/ui/views/frame/multi_contents_view.cc
@@ -27,13 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(MultiContentsView,
                                       kMultiContentsViewElementId);
 
-MultiContentsView::MultiContentsView(
-    BrowserView* browser_view,
-    WebContentsFocusedCallback inactive_contents_focused_callback,
-    WebContentsResizeCallback contents_resize_callback)
+MultiContentsView::MultiContentsView(BrowserView* browser_view,
+                                     std::unique_ptr<Delegate> delegate)
     : browser_view_(browser_view),
-      inactive_contents_focused_callback_(inactive_contents_focused_callback),
-      contents_resize_callback_(contents_resize_callback),
+      delegate_(std::move(delegate)),
       start_contents_view_inset_(
           gfx::Insets(kSplitViewContentInset).set_top(0).set_right(0)),
       end_contents_view_inset_(
@@ -147,7 +144,7 @@ void MultiContentsView::ExecuteOnEachVisibleContentsView(
 
 void MultiContentsView::OnSwap() {
   CHECK(IsInSplitView());
-  browser_view_->ReverseWebContents();
+  delegate_->ReverseWebContents();
 }
 
 void MultiContentsView::OnResize(int resize_amount, bool done_resizing) {
@@ -164,7 +161,7 @@ void MultiContentsView::OnResize(int resize_amount, bool done_resizing) {
                         contents_container_views_[0]->GetInsets().width() +
                         static_cast<double>(resize_amount)) /
                        total_width;
-  contents_resize_callback_.Run(start_ratio);
+  delegate_->ResizeWebContents(start_ratio);
 
   if (done_resizing) {
     initial_start_width_on_resize_ = std::nullopt;
@@ -191,7 +188,7 @@ void MultiContentsView::OnWebContentsFocused(views::WebView* web_view) {
     // inactive web contents gets focus. See crbug.com/419335827
     if (GetInactiveContentsView()->web_contents() == web_view->web_contents() &&
         GetWidget()->IsVisible()) {
-      inactive_contents_focused_callback_.Run(web_view->web_contents());
+      delegate_->WebContentsFocused(web_view->web_contents());
     }
   }
 }
--- chrome/browser/ui/views/frame/multi_contents_view.h
+++ chrome/browser/ui/views/frame/multi_contents_view.h
@@ -46,10 +46,14 @@ class MultiContentsView : public views::View,
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kMultiContentsViewElementId);
 
-  using WebContentsFocusedCallback =
-      base::RepeatingCallback<void(content::WebContents*)>;
+  class Delegate {
+   public:
+    virtual ~Delegate() = default;
 
-  using WebContentsResizeCallback = base::RepeatingCallback<void(double)>;
+    virtual void WebContentsFocused(content::WebContents* contents) = 0;
+    virtual void ResizeWebContents(double ratio) = 0;
+    virtual void ReverseWebContents() = 0;
+  };
 
   struct ViewWidths {
     double start_width = 0;
@@ -59,10 +63,8 @@ class MultiContentsView : public views::View,
 
   static constexpr int kSplitViewContentInset = 8;
 
-  MultiContentsView(
-      BrowserView* browser_view,
-      WebContentsFocusedCallback inactive_contents_focused_callback,
-      WebContentsResizeCallback contents_resize_callback);
+  MultiContentsView(BrowserView* browser_view,
+                    std::unique_ptr<Delegate> delegate);
   MultiContentsView(const MultiContentsView&) = delete;
   MultiContentsView& operator=(const MultiContentsView&) = delete;
   ~MultiContentsView() override;
@@ -159,6 +161,7 @@ class MultiContentsView : public views::View,
   void UpdateContentsBorderAndOverlay();
 
   raw_ptr<BrowserView> browser_view_;
+  std::unique_ptr<Delegate> delegate_;
 
   // Holds ContentsContainerViews, when not in a split view the second
   // ContentsContainerView is not visible.
@@ -185,11 +188,6 @@ class MultiContentsView : public views::View,
   // The index in contents_views_ of the active contents view.
   int active_index_ = 0;
 
-  // Callback to be executed when the user focuses the inactive contents view.
-  WebContentsFocusedCallback inactive_contents_focused_callback_;
-
-  // Callback to be executed when the user resizes the contents.
-  WebContentsResizeCallback contents_resize_callback_;
 
   // Current ratio of |contents_views_|'s first ContentsContainerView's width /
   // overall contents view width.
