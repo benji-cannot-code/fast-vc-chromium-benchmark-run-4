1bf7c63eb66eca28df26462de528c2dddb805a6b
Awad Osman <awado@google.com>
2025-06-10 10:43:09 -0700

    [Bundled Security Settings] Create a new preference for the bundles
    Creates a new preference that will control the standard and enhanced
    bundles on the new Bundled Security Settings page. We also add a
    generated preference because this bundle preference will end up setting
    the values of other preferences on the chrome://settings/security page.
    The UI for bundled security settings is not finalized yet so I just used
    two standard collapse radio buttons for simplicity. The main point of
    this CL is to create a new preference and to be able update its value
    based on user selection on the UI.
    
    Change-Id: I3b2ca3e9c18e95758fb56a1cce9b65beb4bc544f
    Bug: 416284626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594411
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471928}

--- chrome/browser/extensions/api/settings_private/generated_prefs.cc
+++ chrome/browser/extensions/api/settings_private/generated_prefs.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/settings_private/prefs_util_enums.h"
 #include "chrome/browser/password_manager/generated_password_leak_detection_pref.h"
 #include "chrome/browser/safe_browsing/generated_safe_browsing_pref.h"
+#include "chrome/browser/safe_browsing/generated_security_settings_bundle_pref.h"
 #include "chrome/browser/ssl/generated_https_first_mode_pref.h"
 #include "chrome/common/extensions/api/settings_private.h"
 #include "components/content_settings/core/common/content_settings_types.h"
@@ -104,6 +105,9 @@ void GeneratedPrefs::CreatePrefs() {
       std::make_unique<GeneratedPasswordLeakDetectionPref>(profile_);
   prefs_[safe_browsing::kGeneratedSafeBrowsingPref] =
       std::make_unique<safe_browsing::GeneratedSafeBrowsingPref>(profile_);
+  prefs_[safe_browsing::kGeneratedSecuritySettingsBundlePref] =
+      std::make_unique<safe_browsing::GeneratedSecuritySettingsBundlePref>(
+          profile_);
   prefs_[content_settings::kGeneratedNotificationPref] = std::make_unique<
       content_settings::GeneratedPermissionPromptingBehaviorPref>(
       profile_, ContentSettingsType::NOTIFICATIONS);
