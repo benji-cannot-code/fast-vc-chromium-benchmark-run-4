3231e292b3118f8a4096ca5f10b3239ab7af9ef4
Evan Stade <evanstade@microsoft.com>
2025-06-10 16:47:26 -0700

    IDB: improve BackingStore::Cursor signatures
    Bug: 418962704
    Change-Id: I88f00098e4268f37d397740af9463c1833c8a727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619152
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1472137}

--- content/browser/indexed_db/instance/cursor.cc
+++ content/browser/indexed_db/instance/cursor.cc
@@ -116,13 +116,19 @@ Status Cursor::AdvanceOperation(
     blink::mojom::IDBCursor::AdvanceCallback callback,
     Transaction* /*transaction*/) {
   TRACE_EVENT0("IndexedDB", "Cursor::AdvanceOperation");
-  Status s = Status::OK();
-  if (!cursor_ || !cursor_->Advance(count, &s)) {
+
+  if (!cursor_) {
+    std::move(callback).Run(blink::mojom::IDBCursorResult::NewEmpty(true));
+    return Status::OK();
+  }
+
+  if (StatusOr<bool> result = cursor_->Advance(count);
+      !result.has_value() || !*result) {
     cursor_.reset();
 
-    if (s.ok()) {
+    if (result.has_value()) {
       std::move(callback).Run(blink::mojom::IDBCursorResult::NewEmpty(true));
-      return s;
+      return Status::OK();
     }
 
     // CreateError() needs to be called before calling Close() so
@@ -132,7 +138,7 @@ Status Cursor::AdvanceOperation(
     Close();
     std::move(callback).Run(blink::mojom::IDBCursorResult::NewErrorResult(
         blink::mojom::IDBError::New(error.code(), error.message())));
-    return s;
+    return result.error();
   }
 
   blink::mojom::IDBValuePtr mojo_value;
@@ -153,7 +159,7 @@ Status Cursor::AdvanceOperation(
   std::move(callback).Run(blink::mojom::IDBCursorResult::NewValues(
       blink::mojom::IDBCursorValue::New(
           std::move(keys), std::move(primary_keys), std::move(values))));
-  return s;
+  return Status::OK();
 }
 
 void Cursor::Continue(IndexedDBKey key,
@@ -188,13 +194,19 @@ Status Cursor::ContinueOperation(
     blink::mojom::IDBCursor::ContinueCallback callback,
     Transaction* /*transaction*/) {
   TRACE_EVENT0("IndexedDB", "Cursor::ContinueOperation");
-  Status s = Status::OK();
-  if (!cursor_ || !cursor_->Continue(key, primary_key, &s)) {
+
+  if (!cursor_) {
+    std::move(callback).Run(blink::mojom::IDBCursorResult::NewEmpty(true));
+    return Status::OK();
+  }
+
+  if (StatusOr<bool> result = cursor_->Continue(key, primary_key);
+      !result.has_value() || !*result) {
     cursor_.reset();
-    if (s.ok()) {
+    if (result.has_value()) {
       // This happens if we reach the end of the iterator and can't continue.
       std::move(callback).Run(blink::mojom::IDBCursorResult::NewEmpty(true));
-      return s;
+      return Status::OK();
     }
 
     // |transaction_| must be valid for CreateError(), so we can't call
@@ -204,7 +216,7 @@ Status Cursor::ContinueOperation(
     Close();
     std::move(callback).Run(blink::mojom::IDBCursorResult::NewErrorResult(
         blink::mojom::IDBError::New(error.code(), error.message())));
-    return s;
+    return result.error();
   }
 
   blink::mojom::IDBValuePtr mojo_value;
@@ -225,7 +237,7 @@ Status Cursor::ContinueOperation(
   std::move(callback).Run(blink::mojom::IDBCursorResult::NewValues(
       blink::mojom::IDBCursorValue::New(
           std::move(keys), std::move(primary_keys), std::move(values))));
-  return s;
+  return Status::OK();
 }
 
 void Cursor::Prefetch(int number_to_fetch,
@@ -274,9 +286,14 @@ Status Cursor::PrefetchIterationOperation(
   //                 properly fail, caller will not know why, and any corruption
   //                 will be ignored.
   for (int i = 0; i < number_to_fetch; ++i) {
-    if (!cursor_ || !cursor_->Continue(&s)) {
+    if (!cursor_) {
+      break;
+    }
+
+    if (StatusOr<bool> result = cursor_->Continue();
+        !result.has_value() || !*result) {
       cursor_.reset();
-      if (s.ok()) {
+      if (result.has_value()) {
         // We've reached the end, so just return what we have.
         break;
       }
@@ -288,7 +305,7 @@ Status Cursor::PrefetchIterationOperation(
       Close();
       std::move(callback).Run(blink::mojom::IDBCursorResult::NewErrorResult(
           blink::mojom::IDBError::New(error.code(), error.message())));
-      return s;
+      return result.error();
     }
 
     if (i == 0) {
@@ -322,7 +339,7 @@ Status Cursor::PrefetchIterationOperation(
 
   if (found_keys.empty()) {
     std::move(callback).Run(blink::mojom::IDBCursorResult::NewEmpty(true));
-    return s;
+    return Status::OK();
   }
 
   DCHECK_EQ(found_keys.size(), found_primary_keys.size());
@@ -340,7 +357,7 @@ Status Cursor::PrefetchIterationOperation(
       blink::mojom::IDBCursorValue::New(std::move(found_keys),
                                         std::move(found_primary_keys),
                                         std::move(mojo_values))));
-  return s;
+  return Status::OK();
 }
 
 void Cursor::PrefetchReset(int used_prefetches) {
@@ -354,11 +371,8 @@ void Cursor::PrefetchReset(int used_prefetches) {
   // First prefetched result is always used.
   if (cursor_) {
     DCHECK_GT(used_prefetches, 0);
-    for (int i = 0; i < used_prefetches - 1; ++i) {
-      Status unused;
-      bool ok = cursor_->Continue(&unused);
-      DCHECK(ok);
-    }
+    auto result = cursor_->Advance(used_prefetches - 1);
+    DCHECK(!result.has_value() || result.value());
   }
 }
 
