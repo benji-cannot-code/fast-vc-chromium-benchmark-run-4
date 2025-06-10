1bbf3fd784e2a421c31ac078a79e5059c1dc45bb
Kamila Hasanbega <hkamila@google.com>
2025-06-10 06:36:19 -0700

    DBD revamp: Add password manager linkout
    Add a link out to Password manager in  the Other Google Data page.
    
    Bug: 397187800
    Change-Id: I579be5b922239f245d6d92b3ee3caf8f6ebb66fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6615038
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1471748}

---
+++ chrome/test/data/webui/settings/other_google_data_dialog_test.ts
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://settings/lazy_load.js';
+
+import type {SettingsOtherGoogleDataDialogElement} from 'chrome://settings/lazy_load.js';
+import {PasswordManagerImpl, PasswordManagerPage} from 'chrome://settings/settings.js';
+import {assertEquals} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+
+import {TestPasswordManagerProxy} from './test_password_manager_proxy.js';
+
+// TODO(crbug.com/422340428): Add tests for back & cancel buttons.
+suite('OtherGoogleDataDialog', function() {
+  let dialog: SettingsOtherGoogleDataDialogElement;
+  let passwordManagerProxy: TestPasswordManagerProxy;
+
+  setup(function() {
+    passwordManagerProxy = new TestPasswordManagerProxy();
+    PasswordManagerImpl.setInstance(passwordManagerProxy);
+
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    dialog = document.createElement('settings-other-google-data-dialog');
+    document.body.appendChild(dialog);
+    return flushTasks();
+  });
+
+  test('PasswordManagerLinkClick', async function() {
+    dialog.$.passwordManagerLink.click();
+
+    assertEquals(
+        PasswordManagerPage.PASSWORDS,
+        await passwordManagerProxy.whenCalled('showPasswordManager'));
+  });
+});
