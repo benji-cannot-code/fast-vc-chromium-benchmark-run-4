b2879c39b32c178add193f3237f7a0f7c546e163
Daniel Cheng <dcheng@chromium.org>
2025-06-10 12:46:50 -0700

    Remove base::MakeStringPiece() use from //net
    Where possible, simplify the code, e.g. by using base::SplitStringOnce()
    or std::string_view::substr() where possible. Otherwise, just translate
    it into a direct call to the corresponding std::string_view constructor.
    
    Bug: 422940464
    Change-Id: I3337c101a4ee9fab6736f74f0d88c63c560a6c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6628352
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472015}

--- net/disk_cache/simple/simple_index_file.cc
+++ net/disk_cache/simple/simple_index_file.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/disk_cache/simple/simple_index_file.h"
 
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -16,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/pickle.h"
-#include "base/strings/string_util.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/thread_restrictions.h"
@@ -152,8 +152,8 @@ void ProcessEntryFile(BackendFileOperations* file_operations,
 
   if (file_name.size() != kEntryFilesLength)
     return;
-  const auto hash_string = base::MakeStringPiece(
-      file_name.begin(), file_name.begin() + kEntryFilesHashLength);
+  const auto hash_string =
+      std::string_view(file_name).substr(0, kEntryFilesHashLength);
   uint64_t hash_key = 0;
   if (!simple_util::GetEntryHashKeyFromHexString(hash_string, &hash_key)) {
     LOG(WARNING) << "Invalid entry hash key filename while restoring index from"
