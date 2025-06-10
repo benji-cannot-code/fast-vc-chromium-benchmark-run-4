6265df6907efbd42cdc26c3c174d6bb3bae3d5ba
Verina Armanyous <averina@google.com>
2025-06-10 11:18:47 -0700

    [Save and Fill] Implement card validation feedback
    This change introduces new logic to the SaveAndFillDialog to provide
    real-time validation feedback for card number input.
    
    key changes:
    1. Dynamic Error Message Display: Added logic to
    dynamically display an error message when the entered credit card number
    is invalid. This message appears and disappears as the user types,
    leveraging the existing IsValidCreditCardNumber validation logic.
    
    2. Stable Dialog Height: Introduced a padding view that hides and shows,
    ensuring the dialog's height remains constant. This prevents the dialog
    from unexpectedly shifting or stretching when the error message appears
    or disappears.
    
    Mocks: https://screenshot.googleplex.com/76zVhv6cb9MV4Ci.png (note:
    labels are missing in this mock)
    Implementation:
    https://screencast.googleplex.com/cast/NDU1MzMzNTQ5MDIxNTkzNnwzZjg4MGU1Ny0xZg
    
    Bug: 378165002, 378163937
    Change-Id: Ib28354988d597372a3fd1cb004e8d6bca91305fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574236
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471948}

--- chrome/browser/ui/views/autofill/payments/save_and_fill_dialog.h
+++ chrome/browser/ui/views/autofill/payments/save_and_fill_dialog.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_AUTOFILL_PAYMENTS_SAVE_AND_FILL_DIALOG_H_
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_PAYMENTS_SAVE_AND_FILL_DIALOG_H_
 
+#include "chrome/browser/ui/views/autofill/payments/payments_view_util.h"
 #include "components/autofill/core/browser/ui/payments/save_and_fill_dialog_view.h"
+#include "ui/views/controls/textfield/textfield_controller.h"
 #include "ui/views/window/dialog_delegate.h"
 
 namespace autofill {
@@ -15,7 +17,8 @@ class SaveAndFillDialogController;
 
 // The dialog delegate view implementation for the Save and Fill dialog view.
 // This is owned by the view hierarchy.
-class SaveAndFillDialog : public views::DialogDelegateView {
+class SaveAndFillDialog : public views::DialogDelegateView,
+                          public views::TextfieldController {
  public:
   explicit SaveAndFillDialog(
       base::WeakPtr<SaveAndFillDialogController> controller);
@@ -27,11 +30,17 @@ class SaveAndFillDialog : public views::DialogDelegateView {
   void AddedToWidget() override;
   std::u16string GetWindowTitle() const override;
 
+  // views::TextfieldController
+  void ContentsChanged(views::Textfield* sender,
+                       const std::u16string& new_contents) override;
+
  private:
   // Initialize the dialog's contents.
   void InitViews();
 
   base::WeakPtr<SaveAndFillDialogController> controller_;
+
+  LabeledTextfieldWithErrorMessage card_number_data_;
 };
 
 }  // namespace autofill
