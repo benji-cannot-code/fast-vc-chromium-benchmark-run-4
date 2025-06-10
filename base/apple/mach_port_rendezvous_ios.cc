637155c709cd9f5f515a4f650230dac380338766
Sunny Sachanandani <sunnyps@chromium.org>
2025-06-09 21:40:27 -0700

    [ios blink] Fix compile error due to missing include
    Bug: 40254930
    Change-Id: I6a6a6964a60e187ae373889840f9a091e76bc2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6622167
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471581}

--- base/apple/mach_port_rendezvous_ios.cc
+++ base/apple/mach_port_rendezvous_ios.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/apple/scoped_mach_port.h"
+#include "base/check_op.h"
 #include "base/containers/buffer_iterator.h"
 #include "base/mac/scoped_mach_msg_destroy.h"
 #include "base/numerics/byte_conversions.h"
