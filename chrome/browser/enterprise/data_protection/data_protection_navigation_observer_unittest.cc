c37659c90e79f2cdfd4658e985909a06292766dc
Nasser Al-shawwa <alshawwa@chromium.org>
2025-06-10 12:57:41 -0700

    Add back IsSameDocument() check.
    Same document navigation code path was inadvertently removed in
    https://crrev.com/c/6604816 and this was not caught by tests. Re-adding
    with tests.
    
    Bug: 423921730
    Change-Id: I2b059265d97a2474ebb172f0f72ebda04d26beea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624609
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472026}

--- chrome/browser/enterprise/data_protection/data_protection_navigation_observer_unittest.cc
+++ chrome/browser/enterprise/data_protection/data_protection_navigation_observer_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/proto/realtimeapi.pb.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
@@ -550,6 +551,61 @@ TEST_F(DataProtectionNavigationObserverTest,
     ASSERT_EQ(navigation_observer, nullptr);
     ASSERT_EQ(future.Get(), UrlSettings());
   }
+}
+
+class SameDocumentNavigationWebContentsObserver
+    : public content::WebContentsObserver {
+ public:
+  explicit SameDocumentNavigationWebContentsObserver(
+      content::WebContents* web_contents,
+      FakeRealTimeUrlLookupService* lookup_service,
+      content::BrowserContext* browser_context)
+      : content::WebContentsObserver(web_contents),
+        lookup_service_(lookup_service),
+        browser_context_(browser_context) {}
+
+  MOCK_METHOD(void,
+              DidFinishNavigation,
+              (content::NavigationHandle*),
+              (override));
+
+  void DidStartNavigation(
+      content::NavigationHandle* navigation_handle) override {
+    base::test::TestFuture<const UrlSettings&> future;
+
+    FakeDataProtectionNavigationController controller(
+        web_contents(), lookup_service_, future.GetCallback());
+
+    auto navigation_observer =
+        DataProtectionNavigationObserver::CreateForNavigationIfNeeded(
+            &controller, Profile::FromBrowserContext(browser_context_),
+            navigation_handle, future.GetCallback());
+
+    ASSERT_EQ(navigation_observer, nullptr);
+  }
+
+ private:
+  raw_ptr<content::WebContents> web_contents_;
+  raw_ptr<FakeRealTimeUrlLookupService> lookup_service_;
+  raw_ptr<content::BrowserContext> browser_context_;
+};
+
+TEST_F(DataProtectionNavigationObserverTest,
+       SkipSameDocumentNavigation_CreateForNavigationIfNeeded) {
+  SetContents(CreateTestWebContents());
+  NavigateAndCommit(GURL("https://example.com"));
+  SameDocumentNavigationWebContentsObserver observer(
+      web_contents(), &lookup_service_, browser_context());
+
+  auto simulator = content::NavigationSimulator::CreateRendererInitiated(
+      GURL("https://example.com#fragment"), main_rfh());
+
+  // Ensure that the navigation callbacks are invoked, since the assertion is
+  // outside the test body. If DidFinishNavigation() was called, then it is
+  // guaranteed that DidStartNavigation() was called prior, thereby checking the
+  // same document assertion.
+  EXPECT_CALL(observer, DidFinishNavigation);
+  simulator->CommitSameDocument();
 }
 
 TEST_F(DataProtectionNavigationObserverTest,
