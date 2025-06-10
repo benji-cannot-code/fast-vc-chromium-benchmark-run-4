4877183344d14cc38c0c001d31d33b788a5924dc
Elly <ellyjones@chromium.org>
2025-06-10 15:41:18 -0700

    base/task: drop include of base/hash/md5
    This code never actually depended on base/hash/md5, yay :)
    
    Bug: 406729261
    Change-Id: Ied4120ccfc1fa17f0291690b4ccd4801b8161dd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635513
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472104}

--- base/task/common/task_annotator.cc
+++ base/task/common/task_annotator.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
 #include "base/debug/alias.h"
-#include "base/hash/md5.h"
 #include "base/logging.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/time/time.h"
