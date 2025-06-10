1725e20948292f99baa479ea20abe0f609dff386
Calder Kitagawa <ckitagawa@chromium.org>
2025-06-10 06:55:11 -0700

    [Tab Model] Create IncognitoTabGroupModelFilterImpl
    TabCollectionTabModelImpl implements TabModel and TabGroupModelFilter.
    For the incognito model TabCollectionTabModelImpl will be destroyed when
    the last incognito tab is closed.
    
    The existing implementation assumes TabGroupModelFilter for both the
    regular and incognito models will always exists.
    
    Do what we do for the regular tab model and create a wrapper class
    like IncognitoTabModelImpl that delegates calls when a real instance
    of the TabModel/TabGroupModelFilter exists and otherwise performs
    default behaviors.
    
    BYPASS_LARGE_CHANGE_WARNING - boilerplate.
    
    Bug: 423043938
    Change-Id: I1abde52dc9ae27ccffd87fee3593af697d998731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624602
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471752}

---
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabUngrouper.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tabmodel;
+
+import org.chromium.base.Token;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.tab.Tab;
+
+import java.util.List;
+
+/** Empty implementation of {@link TabUngrouper}. */
+@NullMarked
+public class EmptyTabUngrouper implements TabUngrouper {
+    @Override
+    public void ungroupTabs(
+            List<Tab> tabs,
+            boolean trailing,
+            boolean allowDialog,
+            @Nullable TabModelActionListener listener) {}
+
+    @Override
+    public void ungroupTabGroup(
+            Token tabGroupId,
+            boolean trailing,
+            boolean allowDialog,
+            @Nullable TabModelActionListener listener) {}
+}
