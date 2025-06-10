30daab86c3ff0c8d41b728f341968d4bf66356c2
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-06-10 14:41:32 -0700

    [opt_guide] Extracted SecurityOriginSerializer out of /content subfolder
    Ios cannot depend on Blink, extracted out SecurityOriginSerializer to a
    more suitable location so that ios can depend on it.
    
    Bug: 423698182
    Change-Id: I9a5bef99b8f979b4b4ca8b3b8b05e24872d6991f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6630762
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472079}

---
+++ components/optimization_guide/core/page_content_proto_serializer.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/optimization_guide/core/page_content_proto_serializer.h"
+
+#include "base/unguessable_token.h"
+#include "components/optimization_guide/proto/features/common_quality_data.pb.h"
+#include "url/origin.h"
+
+namespace optimization_guide {
+
+void SecurityOriginSerializer::Serialize(
+    const url::Origin& origin,
+    optimization_guide::proto::SecurityOrigin* proto_origin) {
+  proto_origin->set_opaque(origin.opaque());
+
+  if (origin.opaque()) {
+    proto_origin->set_value(origin.GetNonceForSerialization()->ToString());
+  } else {
+    proto_origin->set_value(origin.Serialize());
+  }
+}
+
+}  // namespace optimization_guide
