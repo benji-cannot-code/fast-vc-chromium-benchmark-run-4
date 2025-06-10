2bae4cdcc7131df16de58f965adbea6986ecf062
David Sanders <dsanders11@ucsbalum.com>
2025-06-10 12:23:24 -0700

    Remove unused include in chrome_browsing_data_remover_constants.h
    Adds missing includes as needed to fix the build.
    
    Bug: 40318405
    Change-Id: I72c120228d8007ceb2a6de75b87b6fbb048a8d1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618954
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1471995}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_constants.h
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_constants.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "chrome/common/buildflags.h"
-#include "content/public/browser/browsing_data_filter_builder.h"
 #include "content/public/browser/browsing_data_remover.h"
 #include "extensions/buildflags/buildflags.h"
 
