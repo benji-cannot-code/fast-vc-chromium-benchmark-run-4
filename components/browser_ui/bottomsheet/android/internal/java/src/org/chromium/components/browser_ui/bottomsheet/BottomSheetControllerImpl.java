46311d3a0bbe35082cf9e242558fcd4874a8bbe8
Hitarth Kothari <hitarthkothari@google.com>
2025-06-10 16:46:32 -0700

    [Bugfix] Fix crash if promo tries to show after root ui coorinator has been destroyed
    Bug: 423825517
    Change-Id: If30fd3d42a297a99f784fc7c48ddb602b049c4c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635117
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472136}

--- components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetControllerImpl.java
+++ components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetControllerImpl.java
@@ -203,6 +203,7 @@ class BottomSheetControllerImpl implements ManagedBottomSheetController, ScrimCo
 
     /**
      * Do the actual initialization of the bottom sheet.
+     *
      * @param initializedCallback A callback for the creation of the sheet.
      * @param window A means of accessing the screen size.
      * @param keyboardDelegate A means of hiding the keyboard.
@@ -214,6 +215,9 @@ class BottomSheetControllerImpl implements ManagedBottomSheetController, ScrimCo
             KeyboardVisibilityDelegate keyboardDelegate,
             Supplier<ViewGroup> root) {
         mBottomSheetContainer = root.get();
+        if (mBottomSheetContainer == null) {
+            return;
+        }
         mBottomSheetContainer.setVisibility(View.VISIBLE);
 
         LayoutInflater.from(root.get().getContext())
@@ -334,7 +338,6 @@ class BottomSheetControllerImpl implements ManagedBottomSheetController, ScrimCo
             mBottomSheet.addObserver(mPendingSheetObservers.get(i));
         }
         mPendingSheetObservers.clear();
-
         mSheetInitializer = null;
     }
 
@@ -535,9 +538,10 @@ class BottomSheetControllerImpl implements ManagedBottomSheetController, ScrimCo
         if (content == null) {
             throw new RuntimeException("Attempting to show null content in the sheet!");
         }
-
         if (mBottomSheet == null) assumeNonNull(mSheetInitializer).run();
-        assumeNonNull(mBottomSheet);
+        if (mBottomSheet == null) {
+            return false;
+        }
         assumeNonNull(mContentQueue);
 
         // If already showing (or queued to show) the requested content, do nothing.
