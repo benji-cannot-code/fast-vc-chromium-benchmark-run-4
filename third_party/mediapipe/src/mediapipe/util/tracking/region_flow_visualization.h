b7a38ffc7deb9b5bcd15736d971b3255488f845b
Brad Triebwasser <btriebw@chromium.org>
2025-06-10 10:44:04 -0700

    Re import mediapipe.
    Change-Id: Ie9655f3ad95dc598b2f73240254b8a7d0e82520f
    Fixed: 401218336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635309
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471929}

--- third_party/mediapipe/src/mediapipe/util/tracking/region_flow_visualization.h
+++ third_party/mediapipe/src/mediapipe/util/tracking/region_flow_visualization.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
+#include <deque>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
