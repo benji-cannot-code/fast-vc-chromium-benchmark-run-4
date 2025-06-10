74db2fd3516bbfffb95c04ab51df7e14ac012235
David Sanders <dsanders11@ucsbalum.com>
2025-06-10 14:07:13 -0700

    Remove unused include in web_url_request.h
    Bug: 40318405
    Change-Id: Ifca58c50753c6c596b756845ec312ac074fdda29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625603
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472054}

--- third_party/blink/public/platform/web_url_request.h
+++ third_party/blink/public/platform/web_url_request.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "net/base/request_priority.h"
 #include "third_party/blink/public/platform/web_common.h"
-#include "ui/base/page_transition_types.h"
 
 // TODO(crbug.com/922875): Need foo.mojom.shared-forward.h.
 namespace network {
