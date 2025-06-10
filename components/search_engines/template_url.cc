65fbd57581d670b58dbe1312429377699b35ba21
Daniel Cheng <dcheng@chromium.org>
2025-06-10 11:44:57 -0700

    Remove base::MakeStringPiece use in //components/search_engines
    std::string_view::substr() is a bit simpler than the original. Note the
    explicit conversion to a std::string_view first; unfortunately,
    std::string::substr() copies, while std::string_view::substr() does not.
    
    Bug: 422940464
    Change-Id: I766356620894e4de2a93bf5977d7b34f3f8cc894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625822
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1471968}

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
-#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/memory_usage_estimator.h"
@@ -744,9 +743,8 @@ bool TemplateURLRef::ParseParameter(size_t start,
     length--;
   }
 
-  const auto parameter =
-      base::MakeStringPiece(original_url.begin() + start + 1,
-                            original_url.begin() + start + 1 + length);
+  const std::string_view parameter =
+      std::string_view(original_url).substr(start + 1, length);
   // Remove the parameter from the string.  For parameters who replacement is
   // constant and already known, just replace them directly.  For other cases,
   // like parameters whose values may change over time, use |replacements|.
