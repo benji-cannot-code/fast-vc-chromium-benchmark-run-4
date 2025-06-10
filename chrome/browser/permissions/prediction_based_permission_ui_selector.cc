b428c2c35b4eab1cfdc30098a1764ec95d64e06f
Elias Klim <elklm@chromium.org>
2025-06-10 11:44:10 -0700

    Record PermissionRequestRelevance and partition by request type and model
    This CL introduces new histograms `Permissions.AIv{1, 3}.{Notifications,
    Geolocation}.PermissionRequestRelevance` that partition the existing
    `Permissions.AIv{1, 3}.PermissionRequestRelevance` by request type. The
    existing histograms are kept for now to avoid disrupting ongoing
    analyses. The idea is to migrate to the new histograms as soon as enough
    data has been collected.
    
    Bug: 423830729
    Change-Id: Ia1aefe95c1234ce9553ab73882e4c070af64106d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633370
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471967}

--- chrome/browser/permissions/prediction_based_permission_ui_selector.cc
+++ chrome/browser/permissions/prediction_based_permission_ui_selector.cc
@@ -295,8 +295,9 @@ void PredictionBasedPermissionUiSelector::OnDeviceAiv3ModelExecutionCallback(
             << static_cast<int>(relevance.value());
     last_permission_request_relevance_ = relevance.value();
     features.permission_relevance = relevance.value();
-    base::UmaHistogramEnumeration("Permissions.AIv3.PermissionRequestRelevance",
-                                  features.permission_relevance);
+
+    PermissionUmaUtil::RecordPermissionRequestRelevance(
+        request_metadata.request_type, features.permission_relevance, "AIv3");
   } else {
     last_permission_request_relevance_ =
         PermissionRequestRelevance::kUnspecified;
@@ -532,7 +533,7 @@ void PredictionBasedPermissionUiSelector::OnDeviceAiv1ModelExecutionCallback(
   }
   features.permission_relevance = last_permission_request_relevance_.value();
   PermissionUmaUtil::RecordPermissionRequestRelevance(
-      features.permission_relevance);
+      request_metadata.request_type, features.permission_relevance, "AIv1");
   InquireServerModel(features, std::move(request_metadata),
                      /*record_source=*/!response.has_value());
 }
