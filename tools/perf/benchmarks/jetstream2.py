d3bd0072f64becae806ffe691ad4bcf0245cb675
Camillo Bruni <cbruni@chromium.org>
2025-06-10 10:05:17 -0700

    [perf] Add Speedometer and JetStream no-field-trial variants
    TODO: Currently the flag is ignored in telemetry and needs
          additional changes there to make this work properly.
    
    Drive-by-fix:
    - Use more consistent naming
    - Better hint that some benchmarks use the latest version
    
    Bug: 423737975
    Change-Id: I62f3823ecbef3e3466b1a2a65a28c8edec7beff9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6632928
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471888}

--- tools/perf/benchmarks/jetstream2.py
+++ tools/perf/benchmarks/jetstream2.py
@@ -31,7 +31,7 @@ from benchmarks import press
 class _JetStream2Base(press._PressBenchmark):  # pylint:disable=protected-access
   """JetStream2, a combination of JavaScript and Web Assembly benchmarks.
 
-  Run all the JetStream 2 benchmarks by default.
+  Run all the JetStream 2.x benchmarks by default.
   """
   @classmethod
   def AddBenchmarkCommandLineArgs(cls, parser):
@@ -87,7 +87,7 @@ class JetStream22(_JetStream2Base):
     component='Blink>JavaScript',
     documentation_url='https://browserbench.org/JetStream2.0/in-depth.html')
 class JetStream2(_JetStream2Base):
-  """Latest JetStream 2 """
+  """Latest JetStream 2.x """
   @classmethod
   def Name(cls):
     return 'jetstream2'
@@ -101,7 +101,7 @@ class JetStream2(_JetStream2Base):
     component='Blink>JavaScript>GarbageCollection',
     documentation_url='https://browserbench.org/JetStream2.0/in-depth.html')
 class JetStream2MinorMS(JetStream2):
-  """Latest JetStream2 with the MinorMS flag.
+  """Latest JetStream 2.x with the MinorMS flag.
 
   Shows the performance with MinorMS young generation GC in V8.
   """
@@ -111,3 +111,21 @@ class JetStream2MinorMS(JetStream2):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs('--js-flags=--minor-ms')
+
+
+@benchmark.Info(
+    emails=['vahl@chromium.org', 'cbruni@chromium.org'],
+    component='Blink>JavaScript',
+    documentation_url='https://browserbench.org/JetStream2.0/in-depth.html')
+class JetStream2NoFieldTrial(JetStream2):
+  """Latest JetStream 2.x without field-trials
+  """
+
+  SCHEDULED = False
+
+  @classmethod
+  def Name(cls):
+    return 'jetstream2-no-field-trials'
+
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs('--disable-field-trial-config')
--- tools/perf/benchmarks/speedometer3.py
+++ tools/perf/benchmarks/speedometer3.py
@@ -24,9 +24,9 @@ _SPEEDOMETER_DIR = os.path.join(path_util.GetChromiumSrcDir(), 'third_party',
 
 
 class _Speedometer3(press._PressBenchmark):  # pylint: disable=protected-access
-  """Abstract base Speedometer3 Benchmark class.
+  """Abstract base Speedometer 3.x Benchmark class.
 
-  Runs all the speedometer 2 suites by default. Add --suite=<regex> to filter
+  Runs all the speedometer 3 suites by default. Add --suite=<regex> to filter
   out suites, and only run suites whose names are matched by the regular
   expression provided.
   """
@@ -71,11 +71,10 @@ class _Speedometer3(press._PressBenchmark):  # pylint: disable=protected-access
     if self.take_memory_measurement:
       cat_filter.AddDisabledByDefault('disabled-by-default-memory-infra')
 
-    # "blink.console" is used for marking ranges in
-    # cache_temperature.MarkTelemetryInternal.
+    # Used for marking ranges in cache_temperature.MarkTelemetryInternal.
     cat_filter.AddIncludedCategory('blink.console')
 
-    # "toplevel" category is used to capture TaskQueueManager events.
+    # Used to capture TaskQueueManager events.
     cat_filter.AddIncludedCategory('toplevel')
 
     if self.extra_chrome_categories:
@@ -140,9 +139,9 @@ class _Speedometer3(press._PressBenchmark):  # pylint: disable=protected-access
 
 @benchmark.Info(emails=['cbruni@chromium.org', 'vahl@chromium.org'],
                 component='Blink>JavaScript',
-                documentation_url='https://browserbench.org/Speedometer3.0')
+                documentation_url='https://github.com/WebKit/Speedometer')
 class Speedometer30(_Speedometer3):
-  """Speedometer3.0 benchmark.
+  """Speedometer 3.0 benchmark.
   Explicitly named version."""
 
   SCHEDULED = False
@@ -159,9 +158,9 @@ class Speedometer30(_Speedometer3):
 
 @benchmark.Info(emails=['cbruni@chromium.org', 'vahl@chromium.org'],
                 component='Blink>JavaScript',
-                documentation_url='https://browserbench.org/Speedometer3.1')
+                documentation_url='https://github.com/WebKit/Speedometer')
 class Speedometer31(_Speedometer3):
-  """Speedometer3.1 benchmark.
+  """Speedometer 3.1 benchmark.
   Explicitly named version."""
 
   SCHEDULED = False
@@ -180,7 +179,7 @@ class Speedometer31(_Speedometer3):
                 component='Blink>JavaScript',
                 documentation_url='https://github.com/WebKit/Speedometer')
 class Speedometer3(Speedometer31):
-  """The latest version of the Speedometer3 benchmark."""
+  """The latest version of the Speedometer 3.x benchmark."""
   SCHEDULED = True
 
   @classmethod
@@ -195,8 +194,8 @@ class Speedometer3(Speedometer31):
 @benchmark.Info(emails=['cbruni@chromium.org', 'vahl@chromium.org'],
                 component='Blink>JavaScript',
                 documentation_url='https://github.com/WebKit/Speedometer')
-class V8Speedometer3Future(Speedometer3):
-  """The latest Speedometer3 benchmark with the V8 flag --future.
+class Speedometer3Future(Speedometer3):
+  """The latest Speedometer 3.x benchmark with the V8 flag --future.
 
   Shows the performance of upcoming V8 VM features.
   """
@@ -212,7 +211,7 @@ class V8Speedometer3Future(Speedometer3):
                 component='Blink>JavaScript>GarbageCollection',
                 documentation_url='https://github.com/WebKit/Speedometer')
 class Speedometer3MinorMS(Speedometer3):
-  """The latest Speedometer3 benchmark without the MinorMS flag.
+  """The latest Speedometer 3.x benchmark without the MinorMS flag.
 
   Shows the performance of Scavenger young generation GC in V8.
   """
@@ -226,9 +225,9 @@ class Speedometer3MinorMS(Speedometer3):
 
 @benchmark.Info(emails=['rasikan@google.com', 'wnwen@google.com'],
                 component='Blink>JavaScript',
-                documentation_url='https://browserbench.org/Speedometer3.0')
+                documentation_url='https://github.com/WebKit/Speedometer')
 class Speedometer3Predictable(Speedometer3):
-  """The latest Speedometer3 benchmark with V8's `predictable` mode.
+  """The latest Speedometer 3.x benchmark with V8's `predictable` mode.
 
   This should (hopefully) help reduce variance in the score.
   """
@@ -239,3 +238,20 @@ class Speedometer3Predictable(Speedometer3):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs('--js-flags=--predictable')
+
+
+@benchmark.Info(emails=['cbruni@chromium.org', 'vahl@chromium.org'],
+                component='Blink>JavaScript',
+                documentation_url='https://github.com/WebKit/Speedometer')
+class Speedometer3NoFieldTrials(Speedometer3):
+  """The latest Speedometer 3.x benchmark without field-trials.
+  """
+
+  SCHEDULED = False
+
+  @classmethod
+  def Name(cls):
+    return 'speedometer3-no-field-trials'
+
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs('--disable-field-trial-config')
