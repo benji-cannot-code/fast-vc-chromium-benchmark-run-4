5c4fe279ed3e596154b64e31c9d376546a0c548e
Jun Ishiguro <junis@google.com>
2025-06-09 19:27:51 -0700

    Remove browser_process dependency from report_controller_initializer
    Fixed: b:404133862
    Change-Id: I64250c490ebdc3f5ddac88019967163a4d77ccfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629891
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471552}

--- chrome/browser/ash/report_controller_initializer/report_controller_initializer_unittest.cc
+++ chrome/browser/ash/report_controller_initializer/report_controller_initializer_unittest.cc
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
+#include "chrome/browser/browser_process_platform_part.h"
+#include "chrome/test/base/scoped_testing_local_state.h"
+#include "chrome/test/base/testing_browser_process.h"
+#include "chromeos/ash/components/install_attributes/stub_install_attributes.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
@@ -22,7 +26,12 @@ class ReportControllerInitializerValidateSegment
 
     // Create ReportControllerInitializer for unit testing.
     report_controller_initializer_ =
-        std::make_unique<ReportControllerInitializer>();
+        std::make_unique<ReportControllerInitializer>(
+            TestingBrowserProcess::GetGlobal()->local_state(),
+            TestingBrowserProcess::GetGlobal()->shared_url_loader_factory(),
+            TestingBrowserProcess::GetGlobal()
+                ->platform_part()
+                ->browser_policy_connector_ash());
   }
 
   void TearDown() override {
@@ -46,6 +55,12 @@ class ReportControllerInitializerValidateSegment
   }
 
  private:
+  ScopedTestingLocalState testing_local_state_{
+      TestingBrowserProcess::GetGlobal()};
+
+  // Needed for `browser_policy_connector_ash()`.
+  ScopedStubInstallAttributes scoped_stub_install_attributes_;
+
   std::unique_ptr<ReportControllerInitializer> report_controller_initializer_;
 };
 
