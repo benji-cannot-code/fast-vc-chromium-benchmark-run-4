c3b02151a69308b396580752f21eb1219aaa2ec3
Wan-Teh Chang <wtc@google.com>
2025-06-10 06:07:34 -0700

    Remove Blink.ImageDecoders.Avif.CleanAperture
    OBSOLETE_HISTOGRAM[Blink.ImageDecoders.Avif.CleanAperture]=Removed
    
    Bug: 417216401
    Change-Id: I165099db250f8c682fbeec478836bd8dbb591290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626058
    Reviewed-by: Vignesh Venkat <vigneshv@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1471733}

--- third_party/blink/renderer/platform/image-decoders/avif/crabbyavif_image_decoder.cc
+++ third_party/blink/renderer/platform/image-decoders/avif/crabbyavif_image_decoder.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/timer/elapsed_timer.h"
 #include "build/build_config.h"
@@ -990,7 +989,6 @@ bool CrabbyAVIFImageDecoder::UpdateDemuxer() {
   // If the image is cropped, pass the size of the cropped image (the clean
   // aperture) to SetSize().
   if (container->transformFlags & crabbyavif::AVIF_TRANSFORM_CLAP) {
-    AVIFCleanApertureType clap_type;
     crabbyavif::avifCropRect crop_rect;
     crabbyavif::avifDiagnostics diag;
     crabbyavif::avifBool valid_clap =
@@ -1000,7 +998,6 @@ bool CrabbyAVIFImageDecoder::UpdateDemuxer() {
     if (!valid_clap) {
       DVLOG(1) << "Invalid 'clap' property: " << diag.error
                << "; showing the full image.";
-      clap_type = AVIFCleanApertureType::kInvalid;
       ignore_clap_ = true;
     } else if (crop_rect.x != 0 || crop_rect.y != 0) {
       // To help discourage the creation of files with privacy risks, also
@@ -1009,15 +1006,12 @@ bool CrabbyAVIFImageDecoder::UpdateDemuxer() {
       // https://github.com/AOMediaCodec/av1-avif/issues/189.
       DVLOG(1) << "Origin of 'clap' property anchored to (" << crop_rect.x
                << ", " << crop_rect.y << "); showing the full image.";
-      clap_type = AVIFCleanApertureType::kNonzeroOrigin;
       ignore_clap_ = true;
     } else {
-      clap_type = AVIFCleanApertureType::kZeroOrigin;
       clap_origin_.SetPoint(crop_rect.x, crop_rect.y);
       width = crop_rect.width;
       height = crop_rect.height;
     }
-    clap_type_ = clap_type;
   }
   return SetSize(width, height);
 }
@@ -1062,16 +1056,9 @@ crabbyavif::avifResult CrabbyAVIFImageDecoder::DecodeImage(wtf_size_t index) {
     CropDecodedImage();
   }
 
-  if (ret == crabbyavif::AVIF_RESULT_OK) {
-    if (IsAllDataReceived() && update_bpp_histogram_callback_) {
-      std::move(update_bpp_histogram_callback_).Run(Size(), data_->size());
-    }
-
-    if (clap_type_.has_value()) {
-      base::UmaHistogramEnumeration("Blink.ImageDecoders.Avif.CleanAperture",
-                                    clap_type_.value());
-      clap_type_.reset();
-    }
+  if (ret == crabbyavif::AVIF_RESULT_OK && IsAllDataReceived() &&
+      update_bpp_histogram_callback_) {
+    std::move(update_bpp_histogram_callback_).Run(Size(), data_->size());
   }
   return ret;
 }
