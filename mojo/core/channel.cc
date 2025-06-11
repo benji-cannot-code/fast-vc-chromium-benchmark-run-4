c0232f505a946199c058301b18690f208c0d87f7
Ho Cheung <hocheung@chromium.org>
2025-06-11 16:52:28 -0700

    [cleanup] Remove superfluous includes
    Most base::Overloaded in Chromium have been replaced with
    absl::Overload, or related usage has been removed from the code, and we
    no longer need to include header files related to base::Overloaded.
    
    This CL was uploaded by git cl split.
    
    Bug: 392929829
    Change-Id: Ia2b82bea1a72d30595863812ab3cb5923b646c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636671
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472758}

--- mojo/core/channel.cc
+++ mojo/core/channel.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
-#include "base/functional/overloaded.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
--- mojo/core/ipcz_driver/transport.cc
+++ mojo/core/ipcz_driver/transport.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_op.h"
-#include "base/functional/overloaded.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
