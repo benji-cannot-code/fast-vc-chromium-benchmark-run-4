9d7896b715d4f4df7480db73e4e3d6f1c52228d2
Xiyuan Xia <xiyuan@chromium.org>
2025-06-11 08:46:02 -0700

    Reland "extensions: Add `WillPrepareForEvaluation` to setup MojoJS"
    This is a reland of commit da2268c07b811ff8c1ce3a43279ce2cfbe066dc1
    
    Changes since last land: Move `PrepareForEvaluation` under the
    protection of `pause_evaluation_` so that `Dispatcher` sees a consistent
    state of extensions loaded in `WillPrepareForEvaluation` and
    `DidStartServiceWorkerContextOnWorkerThread`.
    
    Original change's description:
    > extensions: Add `WillPrepareForEvaluation` to setup MojoJS
    >
    > - Add `WillPrepareForEvaluation` to create `ModuleSystem` before
    >   v8 `InstallConditionalFeatures` so that MojoJS is enabled properly
    > - Make `ModuleSystem` EnableMojoJS for service worker;
    > - Allow `WebV8Features::EnableMojoJS` for IsWorkerOrWorkletWorld()
    > - Add a test to verify component extension service worker can
    >   have MojoJS enabled;
    >
    > Bug: 419101455
    > Change-Id: I5dce1aad58e82d92bdf90c72550713717417b769
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6605256
    > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1470766}
    
    Bug: 419101455
    Change-Id: I06de48ae77b6eb44cdc480d0fc151f1eb96356a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6630056
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472445}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -1753,7 +1753,7 @@ void ChromeContentRendererClient::
 #endif
 }
 
-void ChromeContentRendererClient::WillEvaluateServiceWorkerOnWorkerThread(
+void ChromeContentRendererClient::WillPrepareForEvaluationOnWorkerThread(
     blink::WebServiceWorkerContextProxy* context_proxy,
     v8::Local<v8::Context> v8_context,
     int64_t service_worker_version_id,
@@ -1763,9 +1763,17 @@ void ChromeContentRendererClient::WillEvaluateServiceWorkerOnWorkerThread(
 #if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   extensions::ExtensionsRendererClient::Get()
       ->dispatcher()
-      ->WillEvaluateServiceWorkerOnWorkerThread(
+      ->WillPrepareForEvaluationOnWorkerThread(
           context_proxy, v8_context, service_worker_version_id,
           service_worker_scope, script_url, service_worker_token);
+#endif
+}
+
+void ChromeContentRendererClient::WillEvaluateServiceWorkerOnWorkerThread() {
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+  extensions::ExtensionsRendererClient::Get()
+      ->dispatcher()
+      ->WillEvaluateServiceWorkerOnWorkerThread();
 #endif
 }
 
