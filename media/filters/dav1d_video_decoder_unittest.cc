03e8a5696c38aeb2d812b11955e7c6855e038dc2
Elly <ellyjones@chromium.org>
2025-06-11 08:40:16 -0700

    media/filters: stop using md5 in tests
    It's used for checking whether data changes between runs. This change
    swaps these tests to using SHA-256 for that purpose instead, and
    incidentally spanifies some of the involved code.
    
    Bug: 406729261
    Change-Id: Ieb32719cda81411b12ef757df348311cda0f56d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635498
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472441}

--- media/filters/dav1d_video_decoder_unittest.cc
+++ media/filters/dav1d_video_decoder_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/hash/md5.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
