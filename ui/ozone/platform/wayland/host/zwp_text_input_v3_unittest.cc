d6c225bc26a1b39845593b2ecc13eff5de6eb33e
Orko Garai <orko@igalia.com>
2025-06-10 18:03:00 -0700

    ozone/wayland: Fix regression in input field selection
    Fix regression caused by https://crrev.com/c/6626557 which made text
    input empty on re-focus.
    
    Cause for regression: If empty preedit is notified when there is no
    existing preedit, it clears the surrounding text internally in chromium.
    
    So ensure preedit is notified only when it is different from previously
    applied preedit.
    
    R=fangzhoug
    
    Bug: 409716545
    Fixed: 423207116
    Change-Id: I6426d89962e3ceaa35a64724c654491c8cc9e6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634635
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1472161}

--- ui/ozone/platform/wayland/host/zwp_text_input_v3_unittest.cc
+++ ui/ozone/platform/wayland/host/zwp_text_input_v3_unittest.cc
@@ -734,6 +734,17 @@ TEST_F(ZwpTextInputV3Test, OnDoneWithCommitAndPreedit) {
     zwp_text_input_v3_send_done(text_input->resource(), 0);
   });
   VerifyAndClearExpectations();
+
+  // Sending done again should report empty preedit.
+  EXPECT_CALL(test_client_, OnDeleteSurroundingText(_, _)).Times(0);
+  EXPECT_CALL(test_client_, OnCommitString(_)).Times(0);
+  EXPECT_CALL(test_client_,
+              OnPreeditString("", std::vector<SpanStyle>{}, gfx::Range()));
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    auto* text_input = server->text_input_manager_v3()->text_input();
+    zwp_text_input_v3_send_done(text_input->resource(), 1);
+  });
+  VerifyAndClearExpectations();
 }
 
 TEST_F(ZwpTextInputV3Test, OnDoneWithDeleteSurroundingAndCommit) {
@@ -746,8 +757,7 @@ TEST_F(ZwpTextInputV3Test, OnDoneWithDeleteSurroundingAndCommit) {
   InSequence s;
   EXPECT_CALL(test_client_, OnDeleteSurroundingText(10, 29));
   EXPECT_CALL(test_client_, OnCommitString(kCommitString));
-  EXPECT_CALL(test_client_,
-              OnPreeditString("", std::vector<SpanStyle>{}, gfx::Range()));
+  EXPECT_CALL(test_client_, OnPreeditString(_, _, _)).Times(0);
   PostToServerAndWait([kCommitString](wl::TestWaylandServerThread* server) {
     auto* text_input = server->text_input_manager_v3()->text_input();
     zwp_text_input_v3_send_delete_surrounding_text(text_input->resource(), 1,
@@ -816,7 +826,7 @@ TEST_F(ZwpTextInputV3Test, OnDoneWithDeleteSurroundingCommitAndPreedit) {
   VerifyAndClearExpectations();
 }
 
-TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnEnable) {
+TEST_F(ZwpTextInputV3Test, PendingInputEventsClearedOnEnable) {
   constexpr std::string kCommitString("CommitString");
   constexpr std::string_view kPreeditString("PreeditString");
   constexpr gfx::Range kPreeditCursor{0, 13};
@@ -842,11 +852,10 @@ TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnEnable) {
   text_input_v3_->Enable();
   VerifyAndClearExpectations();
 
-  // Sending done should only report empty preedit.
+  // Sending done should have no effect.
   EXPECT_CALL(test_client_, OnDeleteSurroundingText(_, _)).Times(0);
   EXPECT_CALL(test_client_, OnCommitString(_)).Times(0);
-  EXPECT_CALL(test_client_,
-              OnPreeditString("", std::vector<SpanStyle>{}, gfx::Range()));
+  EXPECT_CALL(test_client_, OnPreeditString(_, _, _)).Times(0);
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     auto* text_input = server->text_input_manager_v3()->text_input();
     zwp_text_input_v3_send_done(text_input->resource(), 1);
@@ -854,7 +863,7 @@ TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnEnable) {
   VerifyAndClearExpectations();
 }
 
-TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnDisable) {
+TEST_F(ZwpTextInputV3Test, PendingInputEventsClearedOnDisable) {
   constexpr std::string kCommitString("CommitString");
   constexpr std::string_view kPreeditString("PreeditString");
   constexpr gfx::Range kPreeditCursor{0, 13};
@@ -870,7 +879,7 @@ TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnDisable) {
                                                    1);
   });
 
-  // Disable should reset input events state to default.
+  // Disable should clear pending requests.
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     auto* zwp_text_input = server->text_input_manager_v3()->text_input();
     InSequence s;
@@ -880,11 +889,10 @@ TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnDisable) {
   text_input_v3_->Disable();
   VerifyAndClearExpectations();
 
-  // Sending done should report empty preedit.
+  // Sending done should have no effect.
   EXPECT_CALL(test_client_, OnDeleteSurroundingText(_, _)).Times(0);
   EXPECT_CALL(test_client_, OnCommitString(_)).Times(0);
-  EXPECT_CALL(test_client_,
-              OnPreeditString("", std::vector<SpanStyle>{}, gfx::Range()));
+  EXPECT_CALL(test_client_, OnPreeditString(_, _, _)).Times(0);
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     auto* text_input = server->text_input_manager_v3()->text_input();
     zwp_text_input_v3_send_done(text_input->resource(), 1);
@@ -892,7 +900,7 @@ TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnDisable) {
   VerifyAndClearExpectations();
 }
 
-TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnReset) {
+TEST_F(ZwpTextInputV3Test, PendingInputEventsClearedOnReset) {
   constexpr std::string kCommitString("CommitString");
   constexpr std::string_view kPreeditString("PreeditString");
   constexpr gfx::Range kPreeditCursor{0, 13};
@@ -919,12 +927,10 @@ TEST_F(ZwpTextInputV3Test, PendingInputEventsEmptiedOnReset) {
   text_input_v3_->Reset();
   VerifyAndClearExpectations();
 
-  // Sending done should only report empty preedit.
+  // Sending done should have no effect.
   EXPECT_CALL(test_client_, OnDeleteSurroundingText(_, _)).Times(0);
   EXPECT_CALL(test_client_, OnCommitString(_)).Times(0);
-  EXPECT_CALL(test_client_,
-              OnPreeditString("", std::vector<SpanStyle>{}, gfx::Range()))
-      .Times(2);
+  EXPECT_CALL(test_client_, OnPreeditString(_, _, _)).Times(0);
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     auto* text_input = server->text_input_manager_v3()->text_input();
     zwp_text_input_v3_send_done(text_input->resource(), 1);
