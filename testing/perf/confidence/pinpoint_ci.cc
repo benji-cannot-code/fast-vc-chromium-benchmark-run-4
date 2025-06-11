85113edb61d5a67caaf71febfb34795589529329
Kalvin Lee <kdlee@chromium.org>
2025-06-11 01:24:04 -0700

    pinpoint_ci: Use `base/logging.h`
    In lieu of `fprintf(stderr, "WARNING: ...`, use the `WARNING` log level.
    This makes logging noisier:
    
    ```
    // line breaks added for legibility
    [0611/165039.902222:WARNING:testing/perf/confidence/pinpoint_ci.cc:67]
      /usr/local/google/home/kdlee/Downloads/klaus.csv:
      Line had 27 columns, expected 28
    ```
    
    but slightly easier to use.
    
    The primary emission at the bottom using `printf()` is left alone for
    now.
    
    Change-Id: I937c773366460579934621c39408c97e555e0363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636535
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472252}

--- testing/perf/confidence/pinpoint_ci.cc
+++ testing/perf/confidence/pinpoint_ci.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_util.h"
+#include "base/logging.h"
 #include "base/rand_util.h"
 #include "base/strings/string_split.h"
 #include "testing/perf/confidence/ratio_bootstrap_estimator.h"
@@ -56,7 +57,7 @@ vector<unordered_map<string, string>> ReadCSV(const char* filename) {
       contents, "\n", base::KEEP_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
   vector<string_view> headers = SplitCSVLine(lines[0]);
   if (headers.empty()) {
-    fprintf(stderr, "%s: Empty header line!\n", filename);
+    LOG(WARNING) << filename << ": Empty header line!";
     exit(1);
   }
 
@@ -64,8 +65,8 @@ vector<unordered_map<string, string>> ReadCSV(const char* filename) {
   for (unsigned i = 1; i < lines.size(); ++i) {
     vector<string_view> line = SplitCSVLine(lines[i]);
     if (line.size() != headers.size()) {
-      fprintf(stderr, "%s: Line had %zu columns, expected %zu\n", filename,
-              line.size(), headers.size());
+      LOG(WARNING) << filename << ": Line had " << line.size()
+                   << " columns, expected " << headers.size();
       break;
     }
 
@@ -83,7 +84,7 @@ vector<unordered_map<string, string>> ReadCSV(const char* filename) {
 
 int main(int argc, char** argv) {
   if (argc < 2 || argc > 3) {
-    fprintf(stderr, "USAGE: pinpoint_ci CSV_FILE [CONFIDENCE_LEVEL]\n");
+    LOG(WARNING) << "USAGE: pinpoint_ci CSV_FILE [CONFIDENCE_LEVEL]";
     exit(1);
   }
 
@@ -118,7 +119,7 @@ int main(int argc, char** argv) {
     string story;
     if (name == "motionmark") {
       if (line.count("stories") == 0) {
-        fprintf(stderr, "WARNING: Could not find MotionMark story\n");
+        LOG(WARNING) << "Could not find MotionMark story";
         continue;
       }
       story = line["stories"];
@@ -131,8 +132,7 @@ int main(int argc, char** argv) {
     } else if (display_label.find("exp:") != string::npos) {
       samples[story].second.push_back(avg);
     } else {
-      fprintf(stderr, "WARNING: Unknown display_label %s\n",
-              display_label.c_str());
+      LOG(WARNING) << "Unknown display_label " << display_label;
     }
   }
 
