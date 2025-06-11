4ed4dd6e7eee31493a8dddffd209c732a86761bf
Aishwarya Rajesh <aishwaryarj@google.com>
2025-06-11 09:46:00 -0700

    [UW] Update condition to display +New window on the instance switcher
    Bug: 423061240
    Change-Id: I449133514ef3aeb39aac704a3771a008a9889fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6628840
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472492}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
@@ -158,7 +158,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManagerImpl implements Acti
                         cleanupSyncedTabGroupsIfLastInstance();
                     },
                     () -> openNewWindow("Android.WindowManager.NewWindow"),
-                    info.size() < MultiWindowUtils.getMaxInstances(),
+                    MultiWindowUtils.getMaxInstances(),
                     info);
 
             if (AppHeaderUtils.isAppInDesktopWindow(mDesktopWindowStateManagerSupplier.get())) {
