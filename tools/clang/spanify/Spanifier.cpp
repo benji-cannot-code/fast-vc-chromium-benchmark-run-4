aeb52b6933e7b0bb33644a813e84a71dd3a5980c
Kalvin Lee <kdlee@chromium.org>
2025-06-10 20:04:49 -0700

    spanify: Lower priority of `.data()` edit
    `.data()` should be the "furthest" away from the target. Adjust the
    position of the corresponding `Precedence` variant to match.
    
    Bug: 412395846
    Change-Id: Ic3ba34eca8e53d09a912bbcc575b2c945d30f029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626724
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472195}

--- tools/clang/spanify/Spanifier.cpp
+++ tools/clang/spanify/Spanifier.cpp
@@ -79,11 +79,11 @@ enum Precedence {
   kNeutralPrecedence = 0,
 
   // Lower priority (weaker ties to the target)
+  kAppendDataCallPrecedence,
   kDecaySpanToPointerPrecedence,
   kAdaptBinaryOperationPrecedence,
   kEmitSingleVariableSpanPrecedence,
   kAdaptBinaryPlusEqOperationPrecedence,
-  kAppendDataCallPrecedence,
   // Higher priority (stronger ties to the target)
 };
 
