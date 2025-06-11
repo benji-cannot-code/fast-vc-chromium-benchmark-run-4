1f4d438956b972435410f772e099ac9087f64aed
Saifuddin Hitawala <hitawala@chromium.org>
2025-06-11 13:52:47 -0700

    [gpu] Update GPU.Vulkan UMA to track lazy allocated memory
    Update gpu/vulkan memory dumps and GPU.Vulkan UMA to track lazily
    allocated vulkan memory separately for both Ganesh and Graphite after
    both Ganesh and Graphite have been updated to track these metrics.
    
    Bug: 407730048
    Change-Id: I41c5ae006122f13d189c4d2f87b9f8886ed58680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624543
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472663}

--- gpu/command_buffer/service/dawn_context_provider.cc
+++ gpu/command_buffer/service/dawn_context_provider.cc
@@ -1034,12 +1034,18 @@ bool DawnSharedContext::OnMemoryDump(
     // `allocated_size` is memory allocated from the device, used is what is
     // actually used.
     dump->AddScalar("allocated_size", MemoryAllocatorDump::kUnitsBytes,
-                    allocator_usage.totalAllocatedMemory);
-    dump->AddScalar("used_size", MemoryAllocatorDump::kUnitsBytes,
-                    allocator_usage.totalUsedMemory);
+                    allocator_usage.totalAllocatedMemory -
+                        allocator_usage.totalLazyAllocatedMemory);
+    dump->AddScalar(
+        "used_size", MemoryAllocatorDump::kUnitsBytes,
+        allocator_usage.totalUsedMemory - allocator_usage.totalLazyUsedMemory);
     dump->AddScalar(
         "fragmentation_size", MemoryAllocatorDump::kUnitsBytes,
         allocator_usage.totalAllocatedMemory - allocator_usage.totalUsedMemory);
+    dump->AddScalar("lazy_allocated_size", MemoryAllocatorDump::kUnitsBytes,
+                    allocator_usage.totalLazyAllocatedMemory);
+    dump->AddScalar("lazy_used_size", MemoryAllocatorDump::kUnitsBytes,
+                    allocator_usage.totalLazyUsedMemory);
   }
 
   return true;
