0f8e069106278b5d4dd3f6318999a5955d602dd3
Jenna Himawan <jhimawan@google.com>
2025-06-11 11:14:37 -0700

    Reland "Make app menu dividers not focusable"
    This reverts commit 9fd0a0cafd87b7e2a6193dfc4b0b6be1f78fa09d.
    
    Reason for revert: Fix test and reland
    
    Original change's description:
    > Revert "Make app menu dividers not focusable"
    >
    > This reverts commit 70f148efa93910da45e89beb15eb8645996df859.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5475279125348352
    >
    > Sample build with failed test: https://ci.chromium.org/b/8712407334286527841
    > Affected test(s):
    > [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.app.appmenu.TabbedAppMenuTest#testBookmarkMenuItem](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.app.appmenu.TabbedAppMenuTest%23testBookmarkMenuItem?q=VHash%3A42715ebb540ede1b)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5475279125348352&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6621371&type=BUG
    >
    > Original change's description:
    > > Make app menu dividers not focusable
    > >
    > > Demo: https://screencast.googleplex.com/cast/NDk5MDI1Njg3NjA5MzQ0MHxhY2JhNWFmZi05MQ
    > >
    > > Fixed: 421794377
    > > Change-Id: I41a9ce540ddea1a417e2c3b5dc267d7b01f7fb48
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6621371
    > > Commit-Queue: Jenna Himawan <jhimawan@google.com>
    > > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1472114}
    > >
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I95067f3db167d53c48368687e11a9d3e2d76ed03
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636940
    > Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    > Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    > Owners-Override: Andrew Rayskiy <greengrape@google.com>
    > Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1472249}
    
    Change-Id: Ia4f1d0b9e56aa5b6be0e3a746a73ccbf43caae35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6638609
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472560}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuTest.java
@@ -9,6 +9,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import static org.chromium.base.test.util.Batch.PER_CLASS;
+import static org.chromium.chrome.browser.ui.appmenu.AppMenuHandler.AppMenuItemType.DIVIDER;
+
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ListView;
@@ -31,6 +34,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
@@ -69,6 +73,7 @@ import java.io.IOException;
 import java.util.concurrent.Callable;
 
 /** Tests tabbed mode app menu popup. */
+@Batch(PER_CLASS)
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class TabbedAppMenuTest {
@@ -360,11 +365,12 @@ public class TabbedAppMenuTest {
                     Assert.assertTrue(mAppMenuHandler.isAppMenuShowing());
                 });
 
+        pressKey(KeyEvent.KEYCODE_DPAD_DOWN);
+
         // Make sure the menu is ready to be selected.
         CriteriaHelper.pollUiThread(
                 () -> {
-                    getListView().setSelection(0);
-                    Criteria.checkThat(getCurrentFocusedRow(), Matchers.is(0));
+                    Criteria.checkThat(getCurrentFocusedRow(), Matchers.not(-1));
                 });
     }
 
@@ -379,9 +385,15 @@ public class TabbedAppMenuTest {
         // Move to the boundary.
         final int end = towardsTop ? 0 : getCount() - 1;
         int increment = towardsTop ? -1 : 1;
-        for (int index = getCurrentFocusedRow(); index != end; index += increment) {
+        ListView listView = getListView();
+        int index = getCurrentFocusedRow();
+        while (index != end) {
             pressKey(towardsTop ? KeyEvent.KEYCODE_DPAD_UP : KeyEvent.KEYCODE_DPAD_DOWN);
-            final int expectedPosition = index + increment;
+            do {
+                index += increment;
+            } while (listView.getAdapter().getItemViewType(index) == DIVIDER);
+
+            final int expectedPosition = index;
             CriteriaHelper.pollInstrumentationThread(
                     () -> {
                         Criteria.checkThat(getCurrentFocusedRow(), Matchers.is(expectedPosition));
