65b3749d07b3ee0c04873b69c2590bd90bbf4b10
Keren Zhu <kerenzhu@chromium.org>
2025-06-11 10:28:32 -0700

    tabs_api: Rename TabId to NodeId
    This change is because the TabId identifier is not only identifying
    Tabs, but also collections. This makes the identifier more generic, and
    not only tied to Tabs.
    
    Bug: 412935316
    Change-Id: I29c013ae2285810b3c78416dd0d616d5936ea030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633494
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472528}

--- chrome/browser/ui/tabs/tab_strip_api/node_id.cc
+++ chrome/browser/ui/tabs/tab_strip_api/node_id.cc
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/tabs/tab_strip_api/tab_id.h"
+#include "chrome/browser/ui/tabs/tab_strip_api/node_id.h"
 
 namespace tabs_api {
 
-bool operator==(const TabId& a, const TabId& b) {
+bool operator==(const NodeId& a, const NodeId& b) {
   return a.Type() == b.Type() && a.Id() == b.Id();
 }
 
--- chrome/browser/ui/tabs/tab_strip_api/node_id_traits_unittest.cc
+++ chrome/browser/ui/tabs/tab_strip_api/node_id_traits_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/tabs/tab_strip_api/tab_id_traits.h"
+#include "chrome/browser/ui/tabs/tab_strip_api/node_id_traits.h"
 
 #include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_api.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -12,12 +12,12 @@ namespace tabs_api {
 namespace {
 
 TEST(TabsStripServiceMojoTraitsTest, Convert) {
-  TabId original(TabId::Type::kCollection, "super_secret_id");
+  NodeId original(NodeId::Type::kCollection, "super_secret_id");
 
-  auto serialized = mojom::TabId::Serialize(&original);
+  auto serialized = mojom::NodeId::Serialize(&original);
 
-  TabId deserialized;
-  mojom::TabId::Deserialize(serialized, &deserialized);
+  NodeId deserialized;
+  mojom::NodeId::Deserialize(serialized, &deserialized);
 
   ASSERT_TRUE(original == deserialized);
 }
