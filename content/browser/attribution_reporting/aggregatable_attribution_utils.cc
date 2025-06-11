5abca98d7367d0839ae77457d83e1d0a2233ae2b
Ho Cheung <hocheung@chromium.org>
2025-06-11 08:12:12 -0700

    [cleanup] Remove superfluous includes
    Most base::Overloaded in Chromium have been replaced with
    absl::Overload, or related usage has been removed from the code, and we
    no longer need to include header files related to base::Overloaded.
    
    This CL was uploaded by git cl split.
    
    Bug: 392929829
    Change-Id: I60282d6281892670d5de829233a283724e2dc274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635593
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472406}

--- content/browser/attribution_reporting/aggregatable_attribution_utils.cc
+++ content/browser/attribution_reporting/aggregatable_attribution_utils.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/containers/flat_tree.h"
-#include "base/functional/overloaded.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/numerics/checked_math.h"
 #include "base/numerics/clamped_math.h"
--- content/browser/scheduler/responsiveness/watcher.cc
+++ content/browser/scheduler/responsiveness/watcher.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/functional/overloaded.h"
 #include "base/pending_task.h"
 #include "base/power_monitor/power_monitor.h"
 #include "build/build_config.h"
