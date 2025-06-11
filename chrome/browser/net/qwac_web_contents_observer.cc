08f968dafb2b808162bbf45e6b6211090a575f90
Matt Mueller <mattm@chromium.org>
2025-06-11 11:42:10 -0700

    Plumb Verify2QwacBinding and hook it up in QwacWebContentsObserver
    Bug: 392931070
    Cq-Include-Trybots: luci.chromium.try:linux-bfcache-rel
    Change-Id: Id1694a9153a27c2dd1cec2711e2433b4394627c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624719
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472582}

--- chrome/browser/net/qwac_web_contents_observer.cc
+++ chrome/browser/net/qwac_web_contents_observer.cc
@@ -9,12 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/link_header_util/link_header_util.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/render_process_host.h"
+#include "content/public/browser/storage_partition.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "net/base/features.h"
 #include "net/cert/cert_status_flags.h"
 #include "net/http/http_response_headers.h"
 #include "net/url_request/referrer_policy.h"
 #include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "url/origin.h"
 #include "url/scheme_host_port.h"
@@ -62,11 +65,14 @@ PAGE_USER_DATA_KEY_IMPL(QwacWebContentsObserver::QwacStatus);
 
 QwacWebContentsObserver::QwacStatus::QwacStatus(
     content::Page& page,
+    std::string hostname,
     scoped_refptr<net::X509Certificate> tls_cert,
     GURL qwac_url,
     const url::Origin& initiator,
     mojo::Remote<network::mojom::URLLoaderFactory> url_loader_factory)
-    : content::PageUserData<QwacStatus>(page), tls_cert_(std::move(tls_cert)) {
+    : content::PageUserData<QwacStatus>(page),
+      hostname_(std::move(hostname)),
+      tls_cert_(std::move(tls_cert)) {
   auto resource_request = std::make_unique<network::ResourceRequest>();
   resource_request->url = std::move(qwac_url);
   resource_request->request_initiator = initiator;
@@ -115,11 +121,27 @@ QwacWebContentsObserver::QwacStatus::RegisterCallback(
 
 void QwacWebContentsObserver::QwacStatus::On2QwacDownloadComplete(
     std::optional<std::string> response_body) {
-  if (response_body) {
-    response_body_ = std::move(*response_body);
+  if (!response_body) {
+    is_finished_ = true;
+    callback_list_.Notify();
+    return;
   }
 
+  page()
+      .GetMainDocument()
+      .GetProcess()
+      ->GetStoragePartition()
+      ->GetNetworkContext()
+      ->Verify2QwacCertBinding(
+          *response_body, hostname_, tls_cert_,
+          base::BindOnce(&QwacStatus::On2QwacVerificationComplete,
+                         weak_ptr_factory_.GetWeakPtr()));
+}
+
+void QwacWebContentsObserver::QwacStatus::On2QwacVerificationComplete(
+    const scoped_refptr<net::X509Certificate>& verified_2qwac) {
   is_finished_ = true;
+  verified_2qwac_ = verified_2qwac;
   callback_list_.Notify();
 }
 
@@ -268,7 +290,8 @@ void QwacWebContentsObserver::DidFinishNavigation(
       url_loader_factory.BindNewPipeAndPassReceiver());
 
   QwacStatus::CreateForPage(
-      page, navigation_handle->GetSSLInfo()->cert, std::move(full_qwac_url),
+      page, navigation_handle->GetURL().host(),
+      navigation_handle->GetSSLInfo()->cert, std::move(full_qwac_url),
       /*initiator=*/render_frame_host->GetLastCommittedOrigin(),
       std::move(url_loader_factory));
 }
