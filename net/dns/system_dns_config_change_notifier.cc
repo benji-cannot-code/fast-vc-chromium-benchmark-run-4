9a8df9bf49f94c77ad9b99a7224ca82f0473ca8a
Thiabaud Engelbrecht <thiabaud@google.com>
2025-06-11 09:29:32 -0700

    [net] Add threading annotations
    Change-Id: Ifbc45ff50090e317b107ca569490b439555e5ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627939
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472480}

--- net/dns/system_dns_config_change_notifier.cc
+++ net/dns/system_dns_config_change_notifier.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "base/thread_annotations.h"
 #include "net/dns/dns_config_service.h"
 
 namespace net {
@@ -88,7 +89,7 @@ class SystemDnsConfigChangeNotifier::Core {
     DCHECK(wrapped_observers_.empty());
   }
 
-  void AddObserver(Observer* observer) {
+  void AddObserver(Observer* observer) LOCKS_EXCLUDED(lock_) {
     // Create wrapped observer outside locking in case construction requires
     // complex side effects.
     auto wrapped_observer = std::make_unique<WrappedObserver>(observer);
@@ -108,7 +109,7 @@ class SystemDnsConfigChangeNotifier::Core {
     }
   }
 
-  void RemoveObserver(Observer* observer) {
+  void RemoveObserver(Observer* observer) LOCKS_EXCLUDED(lock_) {
     // Destroy wrapped observer outside locking in case destruction requires
     // complex side effects.
     std::unique_ptr<WrappedObserver> removed_wrapped_observer;
@@ -155,7 +156,7 @@ class SystemDnsConfigChangeNotifier::Core {
         &Core::OnConfigChanged, weak_ptr_factory_.GetWeakPtr()));
   }
 
-  void OnConfigChanged(const DnsConfig& config) {
+  void OnConfigChanged(const DnsConfig& config) LOCKS_EXCLUDED(lock_) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     base::AutoLock lock(lock_);
 
@@ -186,8 +187,9 @@ class SystemDnsConfigChangeNotifier::Core {
   mutable base::Lock lock_;
   // Only stores valid configs. |std::nullopt| if most recent config was
   // invalid (or no valid config has yet been read).
-  std::optional<DnsConfig> config_;
-  std::map<Observer*, std::unique_ptr<WrappedObserver>> wrapped_observers_;
+  std::optional<DnsConfig> config_ GUARDED_BY(lock_);
+  std::map<Observer*, std::unique_ptr<WrappedObserver>> wrapped_observers_
+      GUARDED_BY(lock_);
 
   // Fields valid only on |task_runner_|.
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
