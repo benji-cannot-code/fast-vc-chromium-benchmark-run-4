9e24ecf0fc1729af4f28ecb7f412b2525810936a
Irina <irfedorova@google.com>
2025-06-11 01:36:53 -0700

    iwa: create keep alive only when the update discovery task is started
    Previously, keep alive was created for many update discovery tasks
    which are still in a queue. But all these tasks cannot be finished
    within 3 seconds which are given for keep alive. This means, the
    tasks will be destroyed on shutdown and cause shutdown crashes.
    
    To prevent this, now the update discovery task creates keep alive
    when it is started. Which means it is more likely to be finished
    within 3 seconds and avoid the shutdown crash.
    
    Change-Id: I28d6b24c99dbffa13f35dd332981194e4cf83d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6617030
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Magdalena Skarbińska <mskarbinska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472256}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_discovery_task.h
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_discovery_task.h
@@ -81,7 +81,9 @@ class IsolatedWebAppUpdateDiscoveryTask {
     kBundleDownloadError,
 
     // Update dry run errors
-    kUpdateDryRunFailed
+    kUpdateDryRunFailed,
+
+    kSystemShutdown,
   };
 
   static std::string SuccessToString(Success success);
@@ -95,8 +97,7 @@ class IsolatedWebAppUpdateDiscoveryTask {
       WebAppCommandScheduler& command_scheduler,
       WebAppRegistrar& registrar,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-      std::unique_ptr<ScopedKeepAlive> optional_keep_alive,
-      std::unique_ptr<ScopedProfileKeepAlive> optional_profile_keep_alive);
+      Profile& profile);
   ~IsolatedWebAppUpdateDiscoveryTask();
 
   IsolatedWebAppUpdateDiscoveryTask(const IsolatedWebAppUpdateDiscoveryTask&) =
@@ -147,8 +148,10 @@ class IsolatedWebAppUpdateDiscoveryTask {
   raw_ref<WebAppCommandScheduler> command_scheduler_;
   raw_ref<WebAppRegistrar> registrar_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
-  std::unique_ptr<ScopedKeepAlive> optional_keep_alive_;
-  std::unique_ptr<ScopedProfileKeepAlive> optional_profile_keep_alive_;
+  // Set on Start:
+  std::unique_ptr<ScopedKeepAlive> keep_alive_;
+  std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive_;
+  const raw_ref<Profile> profile_;
 
   ScopedTempWebBundleFile bundle_;
 
--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
-#include "components/keep_alive_registry/keep_alive_registry.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/prefs/pref_service.h"
@@ -479,25 +478,12 @@ void IsolatedWebAppUpdateManager::DiscoverUpdatesForApp(
     bool allow_downgrades,
     const std::optional<base::Version>& pinned_version,
     bool dev_mode) {
-  if (KeepAliveRegistry::GetInstance()->IsShuttingDown()) {
-    return;
-  }
-  auto keep_alive = std::make_unique<ScopedKeepAlive>(
-      KeepAliveOrigin::ISOLATED_WEB_APP_UPDATE,
-      KeepAliveRestartOption::DISABLED);
-  auto profile_keep_alive =
-      profile_->IsOffTheRecord()
-          ? nullptr
-          : std::make_unique<ScopedProfileKeepAlive>(
-                &*profile_, ProfileKeepAliveOrigin::kIsolatedWebAppUpdate);
-
   task_queue_.Push(std::make_unique<IsolatedWebAppUpdateDiscoveryTask>(
       IwaUpdateDiscoveryTaskParams(update_manifest_url, update_channel,
                                    allow_downgrades, pinned_version, url_info,
                                    dev_mode),
       provider_->scheduler(), provider_->registrar_unsafe(),
-      profile_->GetURLLoaderFactory(), std::move(keep_alive),
-      std::move(profile_keep_alive)));
+      profile_->GetURLLoaderFactory(), *profile_));
 
   task_queue_.MaybeStartNextTask();
 }
@@ -1060,6 +1046,8 @@ IsolatedWebAppUpdateError IsolatedWebAppUpdateManager::FromDiscoveryTaskError(
       return IsolatedWebAppUpdateError::kBundleDownloadError;
     case IsolatedWebAppUpdateDiscoveryTask::Error::kUpdateDryRunFailed:
       return IsolatedWebAppUpdateError::kUpdateDryRunFailed;
+    case IsolatedWebAppUpdateDiscoveryTask::Error::kSystemShutdown:
+      return IsolatedWebAppUpdateError::kSystemShutdown;
   }
 }
 
