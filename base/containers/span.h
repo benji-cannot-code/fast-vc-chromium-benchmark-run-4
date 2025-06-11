ba38691ceffca31769eaf6334efe1f0fc6ed1060
Takuto Ikuta <tikuta@chromium.org>
2025-06-10 20:19:12 -0700

    Revert "Remove <functional> header from //base/containers/span.h"
    This reverts commit d0fd486087dba56d5f99f66b6df602f3d5305202.
    
    Reason for revert:
    <functional> is necessary for `std::less_equal`.
    e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/linux-modules-compile-fyi-rel/36236/overview
    
    ref: https://cppreference.com/w/cpp/utility/functional/less_equal.html
    
    
    Bug: 423048781 40263312
    Original change's description:
    > Remove <functional> header from //base/containers/span.h
    >
    > It appears to be unused.
    >
    > Bug: 423048781
    > Change-Id: I407bb11a55781b7583173fa563ccf05f5c0901c8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629303
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1471893}
    
    Bug: 423048781
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I53fd56a75ade55a47ca329a720ebbe314e068ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636509
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472198}

--- base/containers/span.h
+++ base/containers/span.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <concepts>
+#include <functional>
 #include <initializer_list>
 #include <iterator>
 #include <limits>
