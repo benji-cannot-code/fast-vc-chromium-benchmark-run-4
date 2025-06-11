b6767fcbb0ab285e67d12e39e9989ddeca1540ab
Verina Armanyous <averina@google.com>
2025-06-11 11:41:26 -0700

    [Save and Fill] Implement credit card formatting
    Automatically format cc number when the focus
    switches from the credit card number text field to another field.
    
    Mock:https://screenshot.googleplex.com/3ow8HQigcvLSesf
    Implementation:
    http://screencast/cast/NDkzMTIyMjgxOTA0NTM3Nnw0ZGUyNzJmZi1hOQ
    
    Bug: 378165002
    Change-Id: Ie6c56bd0655cd0057caedb0f26c6804891fe337e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6606740
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472581}

--- chrome/browser/ui/views/autofill/payments/save_and_fill_dialog.cc
+++ chrome/browser/ui/views/autofill/payments/save_and_fill_dialog.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "components/autofill/core/browser/ui/payments/save_and_fill_dialog_controller.h"
+#include "components/autofill/core/common/credit_card_number_validation.h"
 #include "components/grit/components_scaled_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -41,6 +42,11 @@ SaveAndFillDialog::SaveAndFillDialog(
 SaveAndFillDialog::~SaveAndFillDialog() = default;
 
 void SaveAndFillDialog::AddedToWidget() {
+  focus_manager_ = GetFocusManager();
+  if (focus_manager_) {
+    focus_manager_->AddFocusChangeListener(this);
+  }
+
   if (controller_->IsUploadSaveAndFill()) {
     GetBubbleFrameView()->SetTitleView(
         std::make_unique<TitleWithIconAfterLabelView>(
@@ -54,6 +60,13 @@ void SaveAndFillDialog::AddedToWidget() {
   }
 }
 
+void SaveAndFillDialog::RemovedFromWidget() {
+  if (focus_manager_) {
+    focus_manager_->RemoveFocusChangeListener(this);
+    focus_manager_ = nullptr;
+  }
+}
+
 std::u16string SaveAndFillDialog::GetWindowTitle() const {
   return controller_ ? controller_->GetWindowTitle() : std::u16string();
 }
@@ -64,6 +77,15 @@ void SaveAndFillDialog::ContentsChanged(views::Textfield* sender,
     card_number_data_.SetErrorState(
         /*is_valid_input=*/controller_->IsValidCreditCardNumber(new_contents),
         /*error_message=*/controller_->GetInvalidCardNumberErrorMessage());
+  }
+}
+
+void SaveAndFillDialog::OnDidChangeFocus(views::View* before,
+                                         views::View* now) {
+  if (before == &card_number_data_.GetInputTextField()) {
+    card_number_data_.GetInputTextField().SetText(
+        GetFormattedCardNumberForDisplay(
+            card_number_data_.GetInputTextField().GetText()));
   }
 }
 
