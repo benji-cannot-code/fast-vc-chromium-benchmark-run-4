211f75bf619f50bcf82ba02a3b9a77aebcc813a2
David Sanders <dsanders11@ucsbalum.com>
2025-06-11 09:13:30 -0700

    Remove unused include in //cc/paint/draw_looper.h
    Bug: 40318405
    Change-Id: Ib5acf14a480c8dafd04f7bcc06435415c6767e12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635894
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1472462}

--- cc/paint/draw_looper.h
+++ cc/paint/draw_looper.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/adapters.h"
-#include "base/stl_util.h"
 #include "cc/paint/paint_export.h"
 #include "third_party/skia/include/core/SkBlendMode.h"
 #include "third_party/skia/include/core/SkCanvas.h"
