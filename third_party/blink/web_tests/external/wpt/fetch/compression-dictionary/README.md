db66ae53a6c254ae15b3bce82415f56ece87495b
Patrick Meenan <pmeenan@chromium.org>
2025-06-11 16:35:31 -0700

    Improve WPT coverage for Compression Dictionary Transport
    This adds several web platform tests for compression dictionary
    transport to better test some of the edge conditions.
    
    Specifically:
    * Expired dictionaries don't generate available-dictionary.
    * Compressed dictionary responses work as expected.
    * Dictionary-compressing dictionary updates works.
    * Crossorigin no-cors requests do not advertise available-dictionary.
    * A "vary" response that includes use-as-dictionary doesn't invalidate
    existing cache entries.
    
    Bug: 422734684
    Change-Id: Ic7294e4b725cec456026b06ed4a97f1c2dcbe1ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634910
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Patrick Meenan <pmeenan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472752}

---
+++ third_party/blink/web_tests/external/wpt/fetch/compression-dictionary/README.md
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+These are the tests for the [Compression Dictionary Transport](https://datatracker.ietf.org/doc/draft-ietf-httpbis-compression-dictionary/) standard (currently in IETF draft state, approved for publication). The tests are marked as tentative, pending the publication of the RFC.
+
+The MDN reference is [here](https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Compression_dictionary_transport).
---
+++ third_party/blink/web_tests/external/wpt/fetch/compression-dictionary/resources/handler_utils.py
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import json
+
+def create_echo_response(request, response):
+    response.headers.set(b"Access-Control-Allow-Origin", b"*")
+    headers = {}
+    for header in request.headers:
+        key = header.decode('utf-8')
+        value = request.headers.get(header).decode('utf-8')
+        headers[key] = value
+    result = json.dumps(headers)
+    # If there is a callback, treat it as JSONP and wrap the result in the provided callback
+    if b'callback' in request.GET:
+        callback = request.GET.first(b"callback").decode('utf-8')
+        result = callback + '(' + result + ');'
+
+    return result
