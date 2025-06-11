22338b8353b4a5f9256b132c3fe986ca0688247a
Kaan Alsan <alsan@chromium.org>
2025-06-11 10:26:09 -0700

    [SxS] Move MultiContentsView::Delegate implementation to standalone file
    The BrowserView-internal implementation is moved to a new
    `multi_contents_view_delegate.h/cc` file, which only depends on tab
    strip model.
    
    Change-Id: I22fc49a74e273063ea0eb69d7f091a9436eb1530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635635
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472527}

--- chrome/browser/ui/views/frame/multi_contents_view.h
+++ chrome/browser/ui/views/frame/multi_contents_view.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_list.h"
 #include "base/functional/callback_forward.h"
 #include "chrome/browser/ui/views/frame/contents_container_view.h"
-#include "chrome/browser/ui/views/frame/multi_contents_drop_target_view.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/resize_area_delegate.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BrowserView;
 class ContentsWebView;
 class MultiContentsResizeArea;
+class MultiContentsViewDelegate;
 class MultiContentsViewDropTargetController;
 class MultiContentsViewMiniToolbar;
 class MultiContentsDropTargetView;
@@ -47,15 +47,6 @@ class MultiContentsView : public views::View,
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kMultiContentsViewElementId);
 
-  class Delegate : public MultiContentsDropTargetView::DropDelegate {
-   public:
-    ~Delegate() override = default;
-
-    virtual void WebContentsFocused(content::WebContents* contents) = 0;
-    virtual void ResizeWebContents(double ratio) = 0;
-    virtual void ReverseWebContents() = 0;
-  };
-
   struct ViewWidths {
     double start_width = 0;
     double resize_width = 0;
@@ -65,7 +56,7 @@ class MultiContentsView : public views::View,
   static constexpr int kSplitViewContentInset = 8;
 
   MultiContentsView(BrowserView* browser_view,
-                    std::unique_ptr<Delegate> delegate);
+                    std::unique_ptr<MultiContentsViewDelegate> delegate);
   MultiContentsView(const MultiContentsView&) = delete;
   MultiContentsView& operator=(const MultiContentsView&) = delete;
   ~MultiContentsView() override;
@@ -162,7 +153,7 @@ class MultiContentsView : public views::View,
   void UpdateContentsBorderAndOverlay();
 
   raw_ptr<BrowserView> browser_view_;
-  std::unique_ptr<Delegate> delegate_;
+  std::unique_ptr<MultiContentsViewDelegate> delegate_;
 
   // Holds ContentsContainerViews, when not in a split view the second
   // ContentsContainerView is not visible.
