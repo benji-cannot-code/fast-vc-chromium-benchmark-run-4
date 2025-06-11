f578b7585ba7368cf3afd3a7ae9b1195f9c5f5ad
Arthur Milchior <arthurmilchior@google.com>
2025-06-11 03:25:27 -0700

    [iOS]Ensure that OCMock are verified
    As of june 2025, hundreds of tests uses Mock without verifying
    them. While GMock don’t require verification, OCMock does. Those CL
    correct the tests if they are easy to correct and otherwise disable
    them.
    
    This CL was uploaded by git cl split.
    
    R=davidjm@chromium.org
    
    Bug: 421873301
    Change-Id: Ie210ee322465e876ae8950387bc14737d64f6528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619517
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472297}

--- ios/chrome/browser/commerce/model/push_notification/commerce_push_notification_client_unittest.mm
+++ ios/chrome/browser/commerce/model/push_notification/commerce_push_notification_client_unittest.mm
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/gtest_mac.h"
 #import "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
+#import "third_party/ocmock/gtest_support.h"
 
 namespace {
 
@@ -213,6 +214,11 @@ class CommercePushNotificationClientTest : public PlatformTest {
             : std::make_unique<CommercePushNotificationClient>();
   }
 
+  void TearDown() override {
+    EXPECT_OCMOCK_VERIFY((id)application_handler_);
+    PlatformTest::TearDown();
+  }
+
   CommercePushNotificationClient* GetCommercePushNotificationClient() {
     return commerce_push_notification_client_.get();
   }
@@ -334,7 +340,9 @@ TEST_F(CommercePushNotificationClientTest, TestHintKeyRemovedUponNotification) {
   commerce_push_notification_client_->HandleNotificationReception(dict);
 }
 
-TEST_F(CommercePushNotificationClientTest, TestNotificationInteraction) {
+// TODO(crbug.com/422437406): Re-enable the test by correcting the expectation.
+TEST_F(CommercePushNotificationClientTest,
+       DISABLED_TestNotificationInteraction) {
   NSDictionary* user_info = SerializeOptGuideCommercePayload();
 
   // Simulate user clicking 'visit site'.
@@ -416,7 +424,8 @@ TEST_F(CommercePushNotificationClientTest, TestUntrackPriceFailed) {
                                      /*sample=*/true, /*expected_count=*/1);
 }
 
-TEST_F(CommercePushNotificationClientTest, TestBrowserInitialization) {
+// TODO(crbug.com/422436433): Re-enable the test by correcting the expectation.
+TEST_F(CommercePushNotificationClientTest, DISABLED_TestBrowserInitialization) {
   browser_list_->RemoveBrowser(GetBrowser());
   NSDictionary* user_info = SerializeOptGuideCommercePayload();
 
