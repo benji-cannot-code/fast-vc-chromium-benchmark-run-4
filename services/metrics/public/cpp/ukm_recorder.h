2b6989fd22f98ac3d421c2fe06d7cb27697bae99
David Sanders <dsanders11@ucsbalum.com>
2025-06-11 10:05:06 -0700

    Remove unused include in ukm_recorder.h
    Bug: 40318405
    Change-Id: I63dc2822ba85139419c6871c046bf463bb9c81d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635238
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472506}

--- services/metrics/public/cpp/ukm_recorder.h
+++ services/metrics/public/cpp/ukm_recorder.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
-#include "base/threading/thread_checker.h"
 #include "base/types/pass_key.h"
 #include "services/metrics/public/cpp/metrics_export.h"
 #include "services/metrics/public/cpp/ukm_source.h"
