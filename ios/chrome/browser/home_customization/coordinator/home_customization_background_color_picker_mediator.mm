a81837f75540e0581221949cf09e7d5977b66253
Pier-Alexandre Bouchard <pabouchard@google.com>
2025-06-11 14:05:40 -0700

    [iOS] Implement Background Color Picker Selection
    This CL introduces selection functionality in the background color
    picker by highlighting the chosen background color with a blue circular
    border. Additionally, it supports initializing the picker with a
    pre-selected background color, allowing persistence of the user’s
    previous selection.
    
    Old experience: https://screenshot.googleplex.com/4YHEDpvSADrNpQG
    New experience: https://screenshot.googleplex.com/BvH2Bmxcis8ky76
    
    Bug: 418004123
    
    Change-Id: I03a0940b54626c3493562758603cd234ec22f53d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6631706
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472671}

--- ios/chrome/browser/home_customization/coordinator/home_customization_background_color_picker_mediator.mm
+++ ios/chrome/browser/home_customization/coordinator/home_customization_background_color_picker_mediator.mm
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [configs addObject:CreateColorPaletteConfigurationFromSeedColor(seedColor)];
   }];
 
-  [_consumer setColorPaletteConfigurations:configs];
+  [_consumer setColorPaletteConfigurations:configs selectedColorIndex:nil];
 }
 
 @end
--- ios/chrome/browser/home_customization/ui/home_customization_background_color_picker_consumer.h
+++ ios/chrome/browser/home_customization/ui/home_customization_background_color_picker_consumer.h
@@ -13,11 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // A consumer protocol that receives updates about background color palettes.
 @protocol HomeCustomizationBackgroundColorPickerConsumer
 
-// Sets the available background color palette configurations, indexed by seed
-// color index.
+// Sets the background color palette configurations for each seed color,
+// and specifies the index of the currently selected background color.
 - (void)setColorPaletteConfigurations:
-    (NSArray<HomeCustomizationColorPaletteConfiguration*>*)
-        colorPaletteConfigurations;
+            (NSArray<HomeCustomizationColorPaletteConfiguration*>*)
+                colorPaletteConfigurations
+                   selectedColorIndex:(NSNumber*)selectedColorIndex;
 @end
 
 #endif  // IOS_CHROME_BROWSER_HOME_CUSTOMIZATION_UI_HOME_CUSTOMIZATION_BACKGROUND_COLOR_PICKER_CONSUMER_H_
