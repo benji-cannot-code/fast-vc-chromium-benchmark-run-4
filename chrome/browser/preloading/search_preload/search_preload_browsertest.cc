f454332e9578ffc5a7dff9f05aa8699b2b29c48b
kenoss <kenoss@chromium.org>
2025-06-10 20:28:12 -0700

    DsePreload2: Disable on low memory devices
    This CL adds threshold for low memory devices:
    `kDsePreload2DeviceMemoryThresholdMiB`.
    
    For more details, see
    https://docs.google.com/document/d/1f4dcNYP3O_Ft4yMmC42ETxGC5lM7YF5FDbEgnxUua7M/edit?pli=1&tab=t.38v8gca76tmi
    
    Fixed: 418252600
    Change-Id: Iff03ab73b6b56b3bf65015fe7437bd168c15d34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624792
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472202}

--- chrome/browser/preloading/search_preload/search_preload_browsertest.cc
+++ chrome/browser/preloading/search_preload/search_preload_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <limits>
 #include <string>
 
 #include "base/functional/bind.h"
@@ -436,7 +437,9 @@ class SearchPreloadBrowserTest : public SearchPreloadBrowserTestBase {
             },
             {
                 features::kDsePreload2,
-                {},
+                {
+                    {"kDsePreload2DeviceMemoryThresholdMiB", "0"},
+                },
             },
             {
                 features::kDsePreload2OnPress,
@@ -947,6 +950,41 @@ IN_PROC_BROWSER_TEST_F(
             ParseNoVarySearchData(R"(key-order, params, except=("q"))"));
 }
 
+class SearchPreloadBrowserTest_DeviceMemoryThreshold
+    : public SearchPreloadBrowserTestBase {
+  void InitFeatures(
+      base::test::ScopedFeatureList& scoped_feature_list) override {
+    scoped_feature_list.InitWithFeaturesAndParameters(
+        {
+            {
+                features::kPrefetchPrerenderIntegration,
+                {},
+            },
+            {
+                features::kDsePreload2,
+                {
+                    {"kDsePreload2DeviceMemoryThresholdMiB",
+                     base::NumberToString(std::numeric_limits<int>::max())},
+                },
+            },
+            {
+                features::kDsePreload2OnPress,
+                {
+                    {"kDsePreload2OnPressMouseDown", "true"},
+                    {"kDsePreload2OnPressUpOrDownArrowButton", "true"},
+                    {"kDsePreload2OnPressTouchDown", "true"},
+                },
+            },
+        },
+        /*disabled_features=*/{});
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(SearchPreloadBrowserTest_DeviceMemoryThreshold,
+                       FeatureIsDisabledIfDeviceMemoryIsSmallerThanThreshold) {
+  ASSERT_FALSE(features::IsDsePreload2Enabled());
+}
+
 class SearchPreloadBrowserTest_Limit : public SearchPreloadBrowserTestBase {
   void InitFeatures(
       base::test::ScopedFeatureList& scoped_feature_list) override {
@@ -959,6 +997,7 @@ class SearchPreloadBrowserTest_Limit : public SearchPreloadBrowserTestBase {
             {
                 features::kDsePreload2,
                 {
+                    {"kDsePreload2DeviceMemoryThresholdMiB", "0"},
                     {"kDsePreload2MaxPrefetch", "2"},
                 },
             },
@@ -1140,6 +1179,7 @@ class SearchPreloadBrowserTest_Ttl : public SearchPreloadBrowserTestBase {
             {
                 features::kDsePreload2,
                 {
+                    {"kDsePreload2DeviceMemoryThresholdMiB", "0"},
                     {"kDsePreload2MaxPrefetch", "2"},
                     {"kDsePreload2PrefetchTtl", "1000ms"},
                 },
@@ -1304,6 +1344,7 @@ class SearchPreloadBrowserTest_SharedDictionary
             {
                 features::kDsePreload2,
                 {
+                    {"kDsePreload2DeviceMemoryThresholdMiB", "0"},
                     {"kDsePreload2OnSuggestSharedDictionaryTtl", "10ms"},
                 },
             },
