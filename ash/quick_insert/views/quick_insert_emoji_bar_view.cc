5ae0a5ddd5a7255cd7333ca0fd3500fa5bd12e1c
Ho Cheung <hocheung@chromium.org>
2025-06-11 16:52:37 -0700

    [cleanup] Remove superfluous includes
    Most base::Overloaded in Chromium have been replaced with
    absl::Overload, or related usage has been removed from the code, and we
    no longer need to include header files related to base::Overloaded.
    
    This CL was uploaded by git cl split.
    
    Bug: 392929829
    Change-Id: I7225829064cb55f7c72d49882d31e5ea0442419e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636949
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472759}

--- ash/quick_insert/views/quick_insert_emoji_bar_view.cc
+++ ash/quick_insert/views/quick_insert_emoji_bar_view.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
-#include "base/functional/overloaded.h"
 #include "base/notreached.h"
 #include "ui/base/emoji/emoji_panel_helper.h"
 #include "ui/base/l10n/l10n_util.h"
