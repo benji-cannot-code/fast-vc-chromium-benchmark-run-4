e3c7f89b3e165123980d8a2f7140832bf8ea1961
Ho Cheung <hocheung@chromium.org>
2025-06-11 16:49:54 -0700

    [cleanup] Remove superfluous includes
    Most base::Overloaded in Chromium have been replaced with
    absl::Overload, or related usage has been removed from the code, and we
    no longer need to include header files related to base::Overloaded.
    
    This CL was uploaded by git cl split.
    
    Bug: 392929829
    Change-Id: Ie406e4bfa663ab5a439a1bc5204d9d852d00557b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636350
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472756}

--- cc/layers/tile_display_layer_impl.cc
+++ cc/layers/tile_display_layer_impl.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <variant>
 
 #include "base/check.h"
-#include "base/functional/overloaded.h"
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "cc/layers/append_quads_data.h"
