7a741d01b39a47a0c61242442fe97d23d9e4d7ce
Yurii Lahodiuk <ylahodiuk@google.com>
2025-06-12 08:37:47 -0700

    [iOS] Don't show GPM Widget and Trusted Vault promos together
    This change allows to ensure that we will not display both promos at
    the same time.
    
    NO_IFTTT=Refactoring without changing the logic
    
    Bug: 407605858
    Change-Id: Ia444d1df0483530ddb6e702981b2204b8ec59c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6632590
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473075}

--- ios/chrome/browser/settings/ui_bundled/password/passwords_mediator.mm
+++ ios/chrome/browser/settings/ui_bundled/password/passwords_mediator.mm
@@ -170,6 +170,13 @@ struct PasswordManagerActiveWidgetPromoData
 }
 
 - (void)askFETToShowPasswordManagerWidgetPromo {
+  if (password_manager::features::
+          IsPasswordManagerTrustedVaultWidgetEnabled() &&
+      [self shouldTrustedVaultPromoBeShown]) {
+    // We don't display the password manager widget promo because the trusted
+    // vault promo should be shown.
+    return;
+  }
   if (self.tracker && !_shouldNotifyFETToDismissPasswordManagerWidgetPromo) {
     [self.consumer setShouldShowPasswordManagerWidgetPromo:
                        [self shouldShowPasswordManagerWidgetPromo]];
@@ -413,6 +420,14 @@ struct PasswordManagerActiveWidgetPromoData
 }
 
 // LINT.IfChange(IsTrustedVaultKeyRequiredForPreferredDataTypes)
+// Decides whether the Trusted Vault widget promo should be displayed.
+- (BOOL)shouldTrustedVaultPromoBeShown {
+  CHECK(_syncService);
+  return _syncService->GetUserSettings()
+      ->IsTrustedVaultKeyRequiredForPreferredDataTypes();
+}
+// LINT.ThenChange(/ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_mediator.mm:IsTrustedVaultKeyRequiredForPreferredDataTypes)
+
 // Decides whether the Trusted Vault widget promo should be displayed and asks
 // consumer to do so. This code should be in sync with the code that decides
 // whether the error badge should be displayed for the GPM icon in the overflow
@@ -421,11 +436,9 @@ struct PasswordManagerActiveWidgetPromoData
   if (password_manager::features::
           IsPasswordManagerTrustedVaultWidgetEnabled()) {
     [self.consumer setShouldShowTrustedVaultWidgetPromo:
-                       _syncService->GetUserSettings()
-                           ->IsTrustedVaultKeyRequiredForPreferredDataTypes()];
-  }
+                       [self shouldTrustedVaultPromoBeShown]];
+  };
 }
-// LINT.ThenChange(/ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_mediator.mm:IsTrustedVaultKeyRequiredForPreferredDataTypes)
 
 #pragma mark - SavedPasswordsPresenterObserver
 
@@ -463,6 +476,7 @@ struct PasswordManagerActiveWidgetPromoData
           password_manager::sync_util::GetPasswordSyncState(_syncService) !=
           password_manager::sync_util::SyncState::kNotActive];
   [self displayOrHideTrustedVaultPasswordManagerWidgetPromo];
+  [self askFETToShowPasswordManagerWidgetPromo];
 }
 
 @end
