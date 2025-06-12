dfd162a422302f88d624703643a8eb10e525fded
Evan Luo <evanluo@google.com>
2025-06-12 16:02:30 -0700

    [declutter] Add metrics for tab group declutter
    Goal is capturing all relevant metrics related to tab group declutter.
    Full detailed list of metric definitions can be found attached in issue.
    
    Bug: 417470833
    Change-Id: Ia0a9259da9dbe1a96906500f81e599dece161cd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635279
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473243}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
@@ -66,6 +66,7 @@ import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
 import org.chromium.components.tab_group_sync.LocalTabGroupId;
 import org.chromium.components.tab_group_sync.SavedTabGroup;
+import org.chromium.components.tab_group_sync.SavedTabGroupTab;
 import org.chromium.components.tab_group_sync.TabGroupSyncService;
 
 import java.util.Arrays;
@@ -313,6 +314,9 @@ public class TabArchiverTest {
         String syncId = "sync_id";
         SavedTabGroup savedTabGroup = new SavedTabGroup();
         savedTabGroup.syncId = syncId;
+        SavedTabGroupTab savedTabGroupTab1 = new SavedTabGroupTab();
+        SavedTabGroupTab savedTabGroupTab2 = new SavedTabGroupTab();
+        savedTabGroup.savedTabs = Arrays.asList(savedTabGroupTab1, savedTabGroupTab2);
         when(mTabGroupSyncService.getGroup(any(LocalTabGroupId.class))).thenReturn(savedTabGroup);
 
         Tab tab =
@@ -336,6 +340,8 @@ public class TabArchiverTest {
         HistogramWatcher watcher =
                 HistogramWatcher.newBuilder()
                         .expectIntRecords("Tabs.TabArchived.TabCount", 1)
+                        .expectIntRecords("TabGroups.TabGroupDeclutter.ArchivedTabGroups", 1)
+                        .expectIntRecords("TabGroups.TabGroupDeclutter.ArchivedTabGroupTabCount", 2)
                         .build();
         runOnUiThreadBlocking(
                 () ->
@@ -390,6 +396,8 @@ public class TabArchiverTest {
         HistogramWatcher watcher =
                 HistogramWatcher.newBuilder()
                         .expectIntRecords("Tabs.TabArchived.TabCount", 1)
+                        .expectNoRecords("TabGroups.TabGroupDeclutter.ArchivedTabGroups")
+                        .expectNoRecords("TabGroups.TabGroupDeclutter.ArchivedTabGroupTabCount")
                         .build();
         // The grouped tab should be skipped.
         runOnUiThreadBlocking(
@@ -408,6 +416,14 @@ public class TabArchiverTest {
     @MediumTest
     @EnableFeatures(ChromeFeatureList.ANDROID_TAB_DECLUTTER_ARCHIVE_TAB_GROUPS)
     public void testGroupedTabsAreArchived() {
+        String syncId = "sync_id";
+        SavedTabGroup savedTabGroup = new SavedTabGroup();
+        savedTabGroup.syncId = syncId;
+        SavedTabGroupTab savedTabGroupTab1 = new SavedTabGroupTab();
+        SavedTabGroupTab savedTabGroupTab2 = new SavedTabGroupTab();
+        savedTabGroup.savedTabs = Arrays.asList(savedTabGroupTab1, savedTabGroupTab2);
+        when(mTabGroupSyncService.getGroup(any(LocalTabGroupId.class))).thenReturn(savedTabGroup);
+
         sActivityTestRule.loadUrlInNewTab(
                 sActivityTestRule.getTestServer().getURL(TEST_PATH), /* incognito= */ false);
 
@@ -441,6 +457,8 @@ public class TabArchiverTest {
         HistogramWatcher watcher =
                 HistogramWatcher.newBuilder()
                         .expectIntRecords("Tabs.TabArchived.TabCount", 1)
+                        .expectIntRecords("TabGroups.TabGroupDeclutter.ArchivedTabGroups", 1)
+                        .expectIntRecords("TabGroups.TabGroupDeclutter.ArchivedTabGroupTabCount", 2)
                         .build();
         // The grouped tab should not be added to the archived tab model and have been closed from
         // the regular tab model.
