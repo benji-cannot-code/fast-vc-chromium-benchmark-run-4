ba8cd02cfcd39d2f6dca94e66b9f6b71d0377da1
Vincent Chiang <vincentchiang@chromium.org>
2025-06-11 20:19:19 -0700

    Fix the insets for mirror window transformer
    The vertical and horizontal insets are set to be the opposite of the
    transform by mistake. This change makes sure insets_ and transform_
    agrees with one another.
    
    Bug: b:422780925
    Change-Id: I3d4888423a2215a6116a148e3a5012b49984fe72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634754
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472821}

--- ash/display/root_window_transformers.cc
+++ ash/display/root_window_transformers.cc
@@ -251,7 +251,7 @@ class MirrorRootWindowTransformer : public RootWindowTransformer {
       int margin = static_cast<int>((mirror_display_rect.height() -
                                      root_bounds_.height() * inverted_scale) /
                                     2);
-      insets_ = gfx::Insets::TLBR(0, margin, 0, margin);
+      insets_ = gfx::Insets::TLBR(margin, 0, margin, 0);
 
       transform_.Translate(0, margin);
       transform_.Scale(inverted_scale, inverted_scale);
@@ -263,7 +263,7 @@ class MirrorRootWindowTransformer : public RootWindowTransformer {
       int margin = static_cast<int>((mirror_display_rect.width() -
                                      root_bounds_.width() * inverted_scale) /
                                     2);
-      insets_ = gfx::Insets::TLBR(margin, 0, margin, 0);
+      insets_ = gfx::Insets::TLBR(0, margin, 0, margin);
 
       transform_.Translate(margin, 0);
       transform_.Scale(inverted_scale, inverted_scale);
