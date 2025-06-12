2684f7f55e7a284f22c486e5e945174d82fffb45
David Sanders <dsanders11@ucsbalum.com>
2025-06-12 07:02:54 -0700

    Remove unused include in shape_cache.h
    Bug: 40318405
    Change-Id: Ic782ba5093353d53050632d660218de3db2641f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6639089
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473025}

--- third_party/blink/renderer/platform/fonts/shaping/shape_cache.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_cache.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/hash.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
-#include "third_party/blink/renderer/platform/heap/weak_cell.h"
 #include "third_party/blink/renderer/platform/text/text_run.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/hash_functions.h"
