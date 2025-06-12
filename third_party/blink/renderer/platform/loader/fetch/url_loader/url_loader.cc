e65c08d7c5be2a79853a77129b2e255a9d848c7d
François Doray <fdoray@chromium.org>
2025-06-12 14:31:14 -0700

    [blink] Add histogram for duration of synchronous loads.
    Synchronous loads are frequently observed in traces of long (>1 second)
    Interaction to Next Paint. This CL adds an histogram to track their
    duration. We'll run custom queries on UMA tables to estimate to what
    extent synchronous loads affect Interaction to Next Paint, without the
    same biases as traces from the field.
    
    Bug: 424432184
    Change-Id: I0ab12864f11a327a8bf54e993d07b6ca26e8954a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6638278
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473164}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/url_loader.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/url_loader.cc
@@ -18,9 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
+#include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
 #include "mojo/public/cpp/base/big_buffer.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -39,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/url_loader_factory.mojom-forward.h"
 #include "services/network/public/mojom/url_response_head.mojom-forward.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 #include "third_party/blink/public/common/loader/mime_sniffing_throttle.h"
 #include "third_party/blink/public/common/loader/referrer_utils.h"
 #include "third_party/blink/public/common/loader/url_loader_throttle.h"
@@ -443,6 +446,12 @@ void URLLoader::LoadSynchronously(
   if (!context_) {
     return;
   }
+
+  base::ElapsedTimer timer;
+  absl::Cleanup record_time = [&]() {
+    base::UmaHistogramMediumTimes("Blink.LoadURLSynchronouslyDuration",
+                                  timer.Elapsed());
+  };
 
   TRACE_EVENT0("loading", "URLLoader::loadSynchronously");
   SyncLoadResponse sync_load_response;
