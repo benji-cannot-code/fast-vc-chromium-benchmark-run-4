2b8ce28c909b6b2904306495da0e8734f15da86f
Aishwarya Rajesh <aishwaryarj@google.com>
2025-06-11 17:19:13 -0700

    [NullAway] Add @NullMarked to tab_management files
    Bug: 389129271
    Change-Id: I6e180c0a516d3e798d04f1bb7feca7f29834fc5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6640268
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472767}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupCreationDialogManager.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupCreationDialogManager.java
@@ -42,7 +42,7 @@ public class TabGroupCreationDialogManager {
         private final TabGroupModelFilter mTabGroupModelFilter;
 
         private TabGroupCreationDialogController(
-                Token tabGroupId, TabGroupModelFilter tabGroupModelFilter) {
+                @Nullable Token tabGroupId, TabGroupModelFilter tabGroupModelFilter) {
             assert tabGroupId != null;
 
             mRootId = tabGroupModelFilter.getRootIdFromTabGroupId(tabGroupId);
@@ -129,7 +129,7 @@ public class TabGroupCreationDialogManager {
      * @param tabGroupId The destination tab group id of the new tab group that has been created.
      * @param filter The current TabGroupModelFilter that this group is created on.
      */
-    public void showDialog(Token tabGroupId, TabGroupModelFilter filter) {
+    public void showDialog(@Nullable Token tabGroupId, TabGroupModelFilter filter) {
         mTabGroupCreationDialogController =
                 new TabGroupCreationDialogController(tabGroupId, filter);
         mTabGroupVisualDataDialogManager.showDialog(
