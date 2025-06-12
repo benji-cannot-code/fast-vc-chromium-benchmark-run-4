2e825eb7ccc7a1730487bd7067e86a9ba8725069
Akihiro Ota <akihiroota@chromium.org>
2025-06-12 07:33:53 -0700

    Accessibility: Remove imports from chromevox tests
    ChromeVox has access to an object called TestImportManager, which can
    be used to access the main ChromeVox classes. As a result, we no longer
    need the importModule calls in ChromeVox tests. This change uses
    TestImportManager in favor of importModule. In a follow-up CL, we will
    add a single method to globalize a ChromeVox module and remove duplicate
    code.
    
    Bug: N/A
    Change-Id: I48b49820f843a484b8b6028e1cd913b8339a5d4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634475
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473039}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv2/background/math_handler.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv2/background/math_handler.ts
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import {AutomationPredicate} from '/common/automation_predicate.js';
 import type {CursorRange} from '/common/cursors/range.js';
+import {TestImportManager} from '/common/testing/test_import_manager.js';
 
 import {Msgs} from '../common/msgs.js';
 import {QueueMode} from '../common/tts_types.js';
@@ -99,3 +100,5 @@ export class MathHandler {
     return false;
   }
 }
+
+TestImportManager.exportForTesting(MathHandler);
