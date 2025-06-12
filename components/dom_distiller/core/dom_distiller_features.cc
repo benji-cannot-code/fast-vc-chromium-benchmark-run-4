c7e9a59377db6878d604dc5a8c31645820e1caf7
Brandon Wylie <wylieb@chromium.org>
2025-06-12 16:50:16 -0700

    [reader mode] Add feature flag for readability
    This feature flag will control if readability vs dom_distiller is used
    for dom distillation.
    
    Bug: 417490653
    Change-Id: I8771d32c1c504b5ec3e2f71e1240b2598f4f3026
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629164
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473269}

--- components/dom_distiller/core/dom_distiller_features.cc
+++ components/dom_distiller/core/dom_distiller_features.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "components/dom_distiller/core/dom_distiller_switches.h"
 #include "components/dom_distiller/core/pref_names.h"
@@ -30,6 +31,28 @@ bool ShouldStartDistillabilityService() {
       switches::kEnableDistillabilityService);
 }
 
+BASE_FEATURE(kReaderModeUseReadability,
+             "ReaderModeUseReadability",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+constexpr base::FeatureParam<bool> kReaderModeUseReadabilityUseDisiller{
+    &kReaderModeUseReadability, /*name=*/"use_distiller",
+    /*default_value=*/false};
+
+bool ShouldReadabilityDistiller() {
+  return base::FeatureList::IsEnabled(kReaderModeUseReadability) &&
+         kReaderModeUseReadabilityUseDisiller.Get();
+}
+
+constexpr base::FeatureParam<bool> kReaderModeUseReadabilityUseHeuristic{
+    &kReaderModeUseReadability, /*name=*/"use_heuristic",
+    /*default_value=*/false};
+
+bool ShouldReadabilityHeuristic() {
+  return base::FeatureList::IsEnabled(kReaderModeUseReadability) &&
+         kReaderModeUseReadabilityUseHeuristic.Get();
+}
+
 #if BUILDFLAG(IS_ANDROID)
 // Feature declarations below -- alphabetical order.
 BASE_FEATURE(kReaderModeAutoDistill,
@@ -43,7 +66,8 @@ BASE_FEATURE(kReaderModeImprovements,
 namespace android {
 static jlong JNI_DomDistillerFeatureMap_GetNativeMap(JNIEnv* env) {
   static const base::Feature* const kFeaturesExposedToJava[] = {
-      &kReaderModeAutoDistill, &kReaderModeImprovements};
+      &kReaderModeAutoDistill, &kReaderModeImprovements,
+      &kReaderModeUseReadability};
   static base::NoDestructor<base::android::FeatureMap> kFeatureMap(
       kFeaturesExposedToJava);
   return reinterpret_cast<jlong>(kFeatureMap.get());
