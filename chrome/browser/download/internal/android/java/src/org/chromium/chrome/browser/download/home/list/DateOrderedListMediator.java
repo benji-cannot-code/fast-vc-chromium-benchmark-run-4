b5f3bbc932c81bb6e1d0ae0b8bc2be7bece0ea4e
Lily Chen <chlily@chromium.org>
2025-06-12 08:30:15 -0700

    [MaliciousApkDownloadCheck] Move util function
    This relocates the util function with logic for when to display
    downloads as dangerous for MaliciousApkDownloadCheck. This is in
    preparation for using it in other parts of the UI aside from Download
    Home.
    
    Bug: 397407934
    Change-Id: I5024cf38c998a3f9cda3e039fad2fc7afcbd074b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635851
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473069}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListMediator.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListMediator.java
@@ -44,6 +44,7 @@ import org.chromium.chrome.browser.profiles.OtrProfileId;
 import org.chromium.chrome.browser.thumbnail.generator.ThumbnailProvider;
 import org.chromium.chrome.browser.thumbnail.generator.ThumbnailProvider.ThumbnailRequest;
 import org.chromium.chrome.browser.thumbnail.generator.ThumbnailProviderImpl;
+import org.chromium.components.browser_ui.util.DownloadUtils;
 import org.chromium.components.browser_ui.widget.gesture.BackPressHandler;
 import org.chromium.components.browser_ui.widget.selectable_list.SelectionDelegate;
 import org.chromium.components.offline_items_collection.LaunchLocation;
@@ -366,7 +367,7 @@ class DateOrderedListMediator implements BackPressHandler {
     }
 
     private void onOpenItem(OfflineItem item) {
-        if (UiUtils.shouldDisplayAsDangerous(item)) {
+        if (DownloadUtils.shouldDisplayDownloadAsDangerous(item.dangerType, item.state)) {
             onShowWarningBypassDialog(item);
             return;
         }
@@ -404,7 +405,9 @@ class DateOrderedListMediator implements BackPressHandler {
     }
 
     private void onShowWarningBypassDialog(OfflineItem item) {
-        if (!UiUtils.shouldDisplayAsDangerous(item)) return;
+        if (!DownloadUtils.shouldDisplayDownloadAsDangerous(item.dangerType, item.state)) {
+            return;
+        }
         mWarningBypassDialogController.showWarningBypassDialog(
                 item.title,
                 (didValidate) -> {
