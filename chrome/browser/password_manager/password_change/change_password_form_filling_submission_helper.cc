5ab413b99c92fd14f38f5b23abb59eab7a4eb96e
Anna Tsvirchkova <atsvirchkova@google.com>
2025-06-12 08:42:00 -0700

    Reland "[APC] Click on submit button using MES response"
    This is a reland of commit 16ad6c4e031364f90f8846781ee917ec2cf76515
    There was a merge conflict between the reverted CL and
    crrev.com/c/6574301, which caused the build failure. The conflict was
    fixed in the patch set 3.
    
    Original change's description:
    > [APC] Click on submit button using MES response
    >
    > Executes the click on the button specified by the dom node id in ChangePasswordFormFillingSubmissionHelper.
    >
    > Bug: 407487665
    > Change-Id: I7b7c97f334d82feb24e07215ae1d6454b68aed35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620823
    > Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1472389}
    
    Bug: 407487665
    Change-Id: Id1e097fa40b445857fc2652c70634a1959a39881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633344
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473076}

--- chrome/browser/password_manager/password_change/change_password_form_filling_submission_helper.cc
+++ chrome/browser/password_manager/password_change/change_password_form_filling_submission_helper.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
+#include "chrome/browser/password_manager/password_change/button_click_helper.h"
 #include "chrome/browser/password_manager/password_change/password_change_submission_verifier.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/optimization_guide/core/model_quality/model_execution_logging_wrappers.h"
@@ -233,12 +234,27 @@ void ChangePasswordFormFillingSubmissionHelper::OnExecutionResponseCallback(
     return;
   }
 
-  // TODO(crbug.com/407487665): Click the button specified in execution_result.
+  int dom_node_id = response.value().submit_form_data().dom_node_id_to_click();
+  click_helper_ = std::make_unique<ButtonClickHelper>(
+      web_contents_.get(), dom_node_id,
+      base::BindOnce(
+          &ChangePasswordFormFillingSubmissionHelper::OnButtonClicked,
+          weak_ptr_factory_.GetWeakPtr()));
 }
 
 void ChangePasswordFormFillingSubmissionHelper::OnFormSubmitted() {
   submission_verifier_ = std::make_unique<PasswordChangeSubmissionVerifier>(
       web_contents_, logs_uploader_);
+}
+
+void ChangePasswordFormFillingSubmissionHelper::OnButtonClicked(bool result) {
+  click_helper_.reset();
+
+  if (!result || !web_contents_) {
+    return;
+  }
+
+  OnFormSubmitted();
 }
 
 void ChangePasswordFormFillingSubmissionHelper::
