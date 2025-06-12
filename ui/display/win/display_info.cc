d44ce41b10bd79fff7f3715333e3ba4f25d9d526
Brad Triebwasser <btriebw@chromium.org>
2025-06-12 15:26:19 -0700

    Remove LegacyDisplayIdAlgorithm histogram(s)
    Removes LegacyDisplayIdAlgorithm and LegacyDisplayIdAlgorithm2. They are
    expired and unowned.
    
    Change-Id: If8a4b54c286c242a91b4f168798979ac85b695c0
    Fixed: 421901699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635229
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473223}

--- ui/display/win/display_info.cc
+++ ui/display/win/display_info.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/hash/hash.h"
-#include "base/metrics/histogram_functions.h"
-#include "base/rand_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/display/win/display_config_helper.h"
@@ -87,15 +85,6 @@ int64_t DisplayInfo::DisplayIdFromMonitorInfo(const MONITORINFOEX& monitor) {
   std::optional<DISPLAYCONFIG_PATH_INFO> config_path =
       GetDisplayConfigPathInfo(monitor);
   // Record if DISPLAYCONFIG_PATH_INFO is available or not.
-  // TODO(anandrv): Remove the non-subsampled metric when there is enough data
-  // from the subsampled metric.
-  base::UmaHistogramBoolean("Windows.LegacyDisplayIdAlgorithm",
-                            !config_path.has_value());
-  if (base::ShouldRecordSubsampledMetric(0.01)) {
-    base::UmaHistogramBoolean("Windows.LegacyDisplayIdAlgorithm2",
-                              !config_path.has_value());
-  }
-
   if (config_path.has_value()) {
     return static_cast<int64_t>(base::PersistentHash(base::StringPrintf(
         "%lu/%li/%u", config_path->targetInfo.adapterId.LowPart,
