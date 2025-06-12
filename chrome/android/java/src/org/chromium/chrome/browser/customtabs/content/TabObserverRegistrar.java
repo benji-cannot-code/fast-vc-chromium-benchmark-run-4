5c30c5920416527f1ac5726229718fef84a19bdd
Shu Yang <shuyng@google.com>
2025-06-11 18:16:34 -0700

    Add @NullMarked to a set of chrome_java files
    Bug: 389129271
    Change-Id: I902e843439d9ade2768378100e3b1b2525fadc3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6637640
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472788}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/TabObserverRegistrar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/TabObserverRegistrar.java
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.customtabs.content;
 
-import androidx.annotation.NonNull;
-
 import org.chromium.base.ObserverList;
+import org.chromium.build.annotations.Initializer;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.DestroyObserver;
 import org.chromium.chrome.browser.page_load_metrics.PageLoadMetrics;
@@ -30,6 +31,7 @@ import java.util.Set;
  * Different than the regular managed {@link TabObserver}, this new type of observer will only
  * attach to the current active tab.
  */
+@NullMarked
 public class TabObserverRegistrar implements TabModelObserver, DestroyObserver {
     private CustomTabActivityTabProvider mTabProvider;
     private final Set<PageLoadMetrics.Observer> mPageLoadMetricsObservers = new HashSet<>();
@@ -42,12 +44,12 @@ public class TabObserverRegistrar implements TabModelObserver, DestroyObserver {
      * Caches the {@link CustomTabActivityTabProvider}'s active tab so that TabObservers can be
      * removed from the previous active tab when the active tab changes.
      */
-    private Tab mTabProviderTab;
+    private @Nullable Tab mTabProviderTab;
 
     private final CustomTabActivityTabProvider.Observer mActivityTabProviderObserver =
             new CustomTabActivityTabProvider.Observer() {
                 @Override
-                public void onInitialTabCreated(@NonNull Tab tab, @TabCreationMode int mode) {
+                public void onInitialTabCreated(Tab tab, @TabCreationMode int mode) {
                     onTabProviderTabUpdated();
                     for (CustomTabTabObserver observer : mActivityTabObservers) {
                         observer.onAttachedToInitialTab(tab);
@@ -55,7 +57,7 @@ public class TabObserverRegistrar implements TabModelObserver, DestroyObserver {
                 }
 
                 @Override
-                public void onTabSwapped(@NonNull Tab tab) {
+                public void onTabSwapped(Tab tab) {
                     onTabProviderTabUpdated();
                     for (CustomTabTabObserver observer : mActivityTabObservers) {
                         observer.onObservingDifferentTab(tab);
@@ -72,6 +74,16 @@ public class TabObserverRegistrar implements TabModelObserver, DestroyObserver {
             };
 
     public TabObserverRegistrar() {}
+
+    @Initializer
+    public void associateWithActivity(
+            ActivityLifecycleDispatcher lifecycleDispatcher,
+            CustomTabActivityTabProvider tabProvider) {
+        mTabProvider = tabProvider;
+        mTabProvider.addObserver(mActivityTabProviderObserver);
+
+        lifecycleDispatcher.register(this);
+    }
 
     /** Registers a {@link PageLoadMetrics.Observer} to be managed by this Registrar. */
     public void registerPageLoadMetricsObserver(PageLoadMetrics.Observer observer) {
@@ -116,15 +128,6 @@ public class TabObserverRegistrar implements TabModelObserver, DestroyObserver {
         if (activeTab != null) {
             activeTab.removeObserver(observer);
         }
-    }
-
-    public void associateWithActivity(
-            ActivityLifecycleDispatcher lifecycleDispatcher,
-            CustomTabActivityTabProvider tabProvider) {
-        mTabProvider = tabProvider;
-        mTabProvider.addObserver(mActivityTabProviderObserver);
-
-        lifecycleDispatcher.register(this);
     }
 
     @Override
@@ -201,17 +204,18 @@ public class TabObserverRegistrar implements TabModelObserver, DestroyObserver {
      */
     public abstract static class CustomTabTabObserver extends EmptyTabObserver {
         /**
-         * Called when the initial tab is created or the observer is registered with
-         * {@link TabObserverRegistrar}, whichever occurs last.
+         * Called when the initial tab is created or the observer is registered with {@link
+         * TabObserverRegistrar}, whichever occurs last.
          */
-        protected void onAttachedToInitialTab(@NonNull Tab tab) {}
+        protected void onAttachedToInitialTab(Tab tab) {}
 
         /**
          * A notification that the observer has switched to observing a different tab. This will not
          * be called for the initial tab being attached to after creation.
+         *
          * @param tab The tab that the observer is now observing.
          */
-        protected void onObservingDifferentTab(@NonNull Tab tab) {}
+        protected void onObservingDifferentTab(Tab tab) {}
 
         /**
          * A notification that the observer has been removed from the tab, as all the tabs are
