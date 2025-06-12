57215bd2aa19d55db3059ba2a9c0c170212f645b
Riley Tatum <rtatum@google.com>
2025-06-11 17:24:33 -0700

    [ntp][enterprise] Move module sign-in requirement
    Move the sign-in check into individual module checks so that Microsoft
    cards don't require a sign-in check.
    * Removes sign-in check in new_tab_page_modules.cc when flag enabled.
    * Adds sign-in check for Google Calendar, Drive, and Tab Resumption
      modules when flag enabled.
    
    Bug: 424180940
    Change-Id: Idb47d9b6122f4ac9b1ccebdf7f692e3ac2d1f213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635531
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472769}

--- chrome/browser/new_tab_page/new_tab_page_util.cc
+++ chrome/browser/new_tab_page/new_tab_page_util.cc
@@ -10,12 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/new_tab_page/modules/modules_switches.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/common/pref_names.h"
 #include "components/optimization_guide/core/optimization_guide_logger.h"
 #include "components/page_content_annotations/core/page_content_annotations_features.h"
 #include "components/prefs/pref_service.h"
 #include "components/search/ntp_features.h"
+#include "components/signin/public/identity_manager/accounts_in_cookie_jar_info.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/sync/service/sync_service.h"
 #include "components/variations/service/variations_service.h"
 
@@ -71,6 +74,11 @@ bool IsDriveModuleEnabledForProfile(bool is_managed_profile, Profile* profile) {
     return false;
   }
 
+  if (!IsProfileSignedIn(profile)) {
+    LogModuleEnablement(ntp_features::kNtpDriveModule, false, "not signed in");
+    return false;
+  }
+
   if (!base::FeatureList::IsEnabled(
           ntp_features::kNtpDriveModuleNoSyncRequirement)) {
     auto* sync_service = SyncServiceFactory::GetForProfile(profile);
@@ -115,7 +123,13 @@ bool IsFeatureForceEnabled(const base::Feature& feature) {
   return force_enabled;
 }
 
-bool IsGoogleCalendarModuleEnabled(bool is_managed_profile) {
+bool IsGoogleCalendarModuleEnabled(bool is_managed_profile, Profile* profile) {
+  if (!IsProfileSignedIn(profile)) {
+    LogModuleEnablement(ntp_features::kNtpCalendarModule, false,
+                        "not signed in");
+    return false;
+  }
+
   if (!is_managed_profile) {
     LogModuleEnablement(ntp_features::kNtpCalendarModule, false,
                         "account not managed");
@@ -133,7 +147,13 @@ bool IsGoogleCalendarModuleEnabled(bool is_managed_profile) {
   return IsFeatureEnabled(ntp_features::kNtpCalendarModule);
 }
 
-bool IsMostRelevantTabResumeModuleEnabled() {
+bool IsMostRelevantTabResumeModuleEnabled(Profile* profile) {
+  if (!IsProfileSignedIn(profile)) {
+    LogModuleEnablement(ntp_features::kNtpMostRelevantTabResumptionModule,
+                        false, "not signed in");
+    return false;
+  }
+
   return g_browser_process &&
          page_content_annotations::features::
              ShouldExecutePageVisibilityModelOnPageContent(
@@ -171,6 +191,15 @@ bool IsOutlookCalendarModuleEnabledForProfile(Profile* profile) {
 bool IsMicrosoftModuleEnabledForProfile(Profile* profile) {
   return IsMicrosoftFilesModuleEnabledForProfile(profile) ||
          IsOutlookCalendarModuleEnabledForProfile(profile);
+}
+
+bool IsProfileSignedIn(Profile* profile) {
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
+  return !base::FeatureList::IsEnabled(
+             ntp_features::kNtpModuleSignInRequirement) ||
+         (identity_manager && identity_manager->GetAccountsInCookieJar()
+                                      .GetPotentiallyInvalidSignedInAccounts()
+                                      .size() > 0);
 }
 
 std::string GetVariationsServiceCountryCode(
