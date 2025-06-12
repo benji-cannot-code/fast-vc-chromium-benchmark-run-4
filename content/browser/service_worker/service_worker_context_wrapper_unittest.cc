ac0fee4434bf4930de493a6238851a5c4aec2056
Minoru Chikamune <chikamune@chromium.org>
2025-06-11 22:36:15 -0700

    ServiceWorker: Moves registered_storage_keys_ field from ServiceWorkerContextCore to ServiceWorkerRegistry
    This CL moves registered_storage_keys_ field from
    ServiceWorkerContextCore to ServiceWorkerRegistry.
    
    Previously, CL (https://crrev.com/c/3495262) moved
    registered_storage_keys_ from ServiceWorkerContextWrapper to
    ServiceWorkerContextCore. It was because we wanted to refresh
    registered_storage_keys_ by recreating ServiceWorkerContextCore when the
    storage gets corrupted.
    
    In this CL, we want to move registered_storage_keys_ from
    ServiceWorkerContextCore to ServiceWorkerRegistry.
    ServiceWorkerContextCore and ServiceWorkerRegistry have a 1:1
    relationship. Hence the lifecycle of these objects are the same. The
    reason for moving registered_storage_keys_ is that we want to merge two
    GetRegisteredStorageKeys() mojo calls into a single call. In the
    upcoming CL, we will merge these mojo calls.
    
    OBSOLETE_HISTOGRAM[ServiceWorker.Storage.RegisteredStorageKeyCacheInitialization.Time]=Replaced by ServiceWorker.Storage.RegisteredStorageKeyCacheInitialization.Time2
    
    Bug: 422348336
    Bug: 40156373
    Change-Id: Ia571400a0787401e3f5273bfc9e65032a4f25d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618051
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472851}

--- content/browser/service_worker/service_worker_context_wrapper_unittest.cc
+++ content/browser/service_worker/service_worker_context_wrapper_unittest.cc
@@ -153,7 +153,7 @@ TEST_F(ServiceWorkerContextWrapperTest, HasRegistration) {
   InitWrapper();
 
   // Now test that registrations are recognized.
-  wrapper_->context()->WaitForRegistrationsInitializedForTest();
+  wrapper_->context()->registry().WaitForRegistrationsInitializedForTest();
   EXPECT_TRUE(wrapper_->MaybeHasRegistrationForStorageKey(key));
   EXPECT_FALSE(wrapper_->MaybeHasRegistrationForStorageKey(
       blink::StorageKey::CreateFromStringForTesting("https://example.org")));
@@ -165,7 +165,7 @@ TEST_F(ServiceWorkerContextWrapperTest, HasRegistration) {
 // key, and should only return FALSE when ALL registrations for that key
 // have been deleted from storage.
 TEST_F(ServiceWorkerContextWrapperTest, DeleteRegistrationsForSameKey) {
-  wrapper_->context()->WaitForRegistrationsInitializedForTest();
+  wrapper_->context()->registry().WaitForRegistrationsInitializedForTest();
 
   // Make two registrations for same origin.
   GURL scope1("https://example1.com/abc/");
@@ -220,7 +220,7 @@ TEST_F(ServiceWorkerContextWrapperTest, DeleteRegistrationsForPartitionedKeys) {
   scope_feature_list_.InitAndEnableFeature(
       net::features::kThirdPartyStoragePartitioning);
 
-  wrapper_->context()->WaitForRegistrationsInitializedForTest();
+  wrapper_->context()->registry().WaitForRegistrationsInitializedForTest();
 
   // Make two registrations for same origin, but different top-level site.
   GURL scope("https://example1.com/abc/");
@@ -278,7 +278,7 @@ TEST_F(ServiceWorkerContextWrapperTest, DeleteRegistrationsForPartitionedKeys) {
 // registrations may exist, MaybeHasRegistrationForStorageKey correctly returns
 // FALSE since the registrations do not exist in storage.
 TEST_F(ServiceWorkerContextWrapperTest, DeleteRegistration) {
-  wrapper_->context()->WaitForRegistrationsInitializedForTest();
+  wrapper_->context()->registry().WaitForRegistrationsInitializedForTest();
 
   // Make registration.
   GURL scope1("https://example2.com/");
