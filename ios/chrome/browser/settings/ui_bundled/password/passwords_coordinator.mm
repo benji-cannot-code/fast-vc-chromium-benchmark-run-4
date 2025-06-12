946e160a6de75185ad55f6e6aa6186779eb955b4
Yurii Lahodiuk <ylahodiuk@google.com>
2025-06-12 09:59:39 -0700

    [iOS] Dismiss TrustedVaultReauthenticationCoordinator when PasswordsCoordinator is stopped
    Bug: 407605858
    Change-Id: I5734d061662d6a8313de5984e7082f2492e9fe1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634181
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473124}

--- ios/chrome/browser/settings/ui_bundled/password/passwords_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/password/passwords_coordinator.mm
@@ -228,6 +228,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.reauthCoordinator.delegate = nil;
   self.reauthCoordinator = nil;
   [self dismissActionSheetCoordinator];
+  [self dismissTrustedVaultReauthenticationCoordinator];
 
   [self.mediator disconnect];
 }
