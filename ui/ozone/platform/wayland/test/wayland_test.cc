f54265054036fbc212269fd8e4f5573063afad31
Kramer Ge <fangzhoug@chromium.org>
2025-06-12 15:13:58 -0700

    Revert "ozone/wayland: send first set_window_geometry at correct timing"
    This reverts commit 14941d9d38af4baf14e5120f4c59bc2710a5d9ef.
    
    Reason for revert: causing crbug.com/416760621. Attempts to force
    full damage to the popup but does not work on user machine. Setting
    initial state to kUnknown has other side effects, need to fix
    crbug.com/361631328 in a different way.
    
    We can maybe suppress SetWindowGeometry calls when window is not
    processing a sequence_point that will map the surface.
    
    Bug: 361631328
    Original change's description:
    > ozone/wayland: send first set_window_geometry at correct timing
    >
    > Per the spec [1]:
    >
    > > When applied, the effective window geometry will be the set window
    > > geometry clamped to the bounding rectangle of the combined geometry of
    > > the surface of the xdg_surface and the associated subsurfaces.
    >
    > Thus, geometry must be set only when a non-empty buffer gets attached.
    > Otherwise, it's equivalent to setting a 0x0 rectangle. This fixes it by
    > initializing both toplevel and popup window state to `unknown`. Which is
    > set to something else only when the first configure sequence is
    > received (ie: xdg_(toplevel|popup).configure + xdg_surface.configure).
    > That state change is then used to determine when the first
    > xdg_surface.set_window_geometry must be sent.
    >
    > Significant part of this CL is about updating ozone/wayland test
    > expectations as well as test logic in some cases. Remarkably:
    >
    > - Now emulating the first "surface activation" configure sequence is
    >   no longer enough to put the xdg surface in "configured" state.
    >   Instead, it will only happen once the first buffer is committed.
    >   Which, depending on the purpose of the test, is done either (1) by
    >   triggering WaylandWindow::OnSequencePoint(), or (2) by creating and
    >   attaching a real (shm) buffer. WaylandWindow tests use approach (1),
    >   for example, while BufferManager tests use method (2).
    > - Some test logic had to be modified, as they were taking assumptions
    >   about the xdg surface state (which no longer apply) and setting
    >   expectations upon them.
    >
    > Bug: 361631328
    > Test: Manually with gnome 48.1 as well as ozone_unittests
    > Change-Id: Iad3d0a26edef5c8d227533cef1b9227b07cfd1e3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6451729
    > Reviewed-by: Orko Garai <orko@igalia.com>
    > Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    > Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    > Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1447216}
    
    Bug: 361631328, 416760621
    Change-Id: Ieeab5f258578985f5feb9455e8082cd1be314d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6640290
    Reviewed-by: Orko Garai <orko@igalia.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473214}

--- ui/ozone/platform/wayland/test/wayland_test.cc
+++ ui/ozone/platform/wayland/test/wayland_test.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using ::testing::_;
-using ::testing::Mock;
 using ::testing::SaveArg;
 
 namespace ui {
@@ -90,10 +89,9 @@ void WaylandTestBase::SetUp() {
 
   // Wait for the client to flush all pending requests from initialization.
   SyncDisplay();
-  Mock::VerifyAndClearExpectations(&delegate_);
 
   // The surface must be activated before buffers are attached.
-  ActivateSurface(delegate_);
+  ActivateSurface(window_->root_surface()->get_surface_id());
 
   EXPECT_EQ(0u,
             DeviceDataManager::GetInstance()->GetTouchscreenDevices().size());
@@ -187,58 +185,10 @@ void WaylandTestBase::SendConfigureEvent(uint32_t surface_id,
       });
 }
 
-void WaylandTestBase::ActivateSurface(
-    MockWaylandPlatformWindowDelegate& window_delegate,
-    std::optional<uint32_t> serial) {
-  WaylandWindow* window = window_delegate.window();
-  CHECK(window_delegate.window());
-
-  ASSERT_FALSE(window->IsSurfaceConfigured());
-  EXPECT_CALL(window_delegate,
-              OnWindowStateChanged(PlatformWindowState::kUnknown,
-                                   PlatformWindowState::kNormal));
-
+void WaylandTestBase::ActivateSurface(uint32_t surface_id,
+                                      std::optional<uint32_t> serial) {
   wl::ScopedWlArray state({XDG_TOPLEVEL_STATE_ACTIVATED});
-  SendConfigureEvent(window->root_surface()->get_surface_id(), gfx::Size(0, 0),
-                     state, serial);
-
-  Mock::VerifyAndClearExpectations(&window_delegate);
-  EXPECT_NE(PlatformWindowState::kUnknown, window->GetPlatformWindowState());
-  EXPECT_FALSE(window->IsSurfaceConfigured());
-}
-
-void WaylandTestBase::MapSurface(
-    MockWaylandPlatformWindowDelegate& window_delegate) {
-  CHECK(window_delegate.window());
-  SyncDisplay();
-
-  WaylandWindow* window = window_delegate.window();
-  ASSERT_FALSE(window->IsSurfaceConfigured());
-  ASSERT_NE(PlatformWindowState::kUnknown, window->GetPlatformWindowState());
-  ASSERT_EQ(0U, window->root_surface()->buffer_id());
-
-  uint32_t surface_id = window->root_surface()->get_surface_id();
-  PostToServerAndWait([surface_id, geometry = window->GetBoundsInDIP()](
-                          wl::TestWaylandServerThread* server) {
-    auto* surface = server->GetObject<wl::MockSurface>(surface_id);
-    ASSERT_TRUE(surface);
-    ASSERT_TRUE(surface->xdg_surface());
-    EXPECT_CALL(*surface->xdg_surface(), SetWindowGeometry(geometry));
-    EXPECT_CALL(*surface->xdg_surface(), AckConfigure(_));
-  });
-
-  // This emulates a buffer attachment to `window` by simply triggering
-  // OnSequencePoint, which is the entry point in WaylandWindow for it.
-  // In production, it's called by frame manager, just before attaching the
-  // buffer.
-  window->OnSequencePoint(window_delegate.viz_seq());
-
-  Mock::VerifyAndClearExpectations(&window_delegate);
-  ASSERT_TRUE(window->IsSurfaceConfigured());
-  PostToServerAndWait([surface_id](wl::TestWaylandServerThread* server) {
-    auto* surface = server->GetObject<wl::MockSurface>(surface_id);
-    Mock::VerifyAndClearExpectations(surface->xdg_surface());
-  });
+  SendConfigureEvent(surface_id, {0, 0}, state, serial);
 }
 
 void WaylandTestBase::MaybeSetUpXkb() {
