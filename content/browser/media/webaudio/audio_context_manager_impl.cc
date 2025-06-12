7e1f6e839bcee86aa44367d6a19c2552f40bd0df
Alvin Ji <alvinji@chromium.org>
2025-06-12 15:53:23 -0700

    webaudio: Add WebAudio.AudioContext.ConcurrentAudioContexts
    This CL introduces a new UMA histogram,
    `WebAudio.AudioContext.ConcurrentAudioContexts`, to record the maximum
    number of concurrent active `AudioContext`s within a single render
    frame's lifetime.
    
    Bug: 422491427
    Change-Id: If9cc1976c67394e489237b9c52d6cc97b0bbb572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627194
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473238}

--- content/browser/media/webaudio/audio_context_manager_impl.cc
+++ content/browser/media/webaudio/audio_context_manager_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/metrics/histogram_macros.h"
 #include "base/time/default_tick_clock.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/public/browser/render_frame_host.h"
@@ -59,10 +60,13 @@ AudioContextManagerImpl::~AudioContextManagerImpl() {
       RecordAudibleTime(now - entry.second);
   }
   pending_audible_durations_.clear();
+  UMA_HISTOGRAM_EXACT_LINEAR("WebAudio.AudioContext.ConcurrentAudioContexts",
+                             max_concurrent_audio_contexts_,
+                             /*exclusive_max=*/101);
 }
 
 void AudioContextManagerImpl::AudioContextAudiblePlaybackStarted(
-    int32_t audio_context_id) {
+    uint32_t audio_context_id) {
   DCHECK(pending_audible_durations_[audio_context_id].is_null());
 
   // Keeps track of the start audible time for this context.
@@ -73,7 +77,7 @@ void AudioContextManagerImpl::AudioContextAudiblePlaybackStarted(
 }
 
 void AudioContextManagerImpl::AudioContextAudiblePlaybackStopped(
-    int32_t audio_context_id) {
+    uint32_t audio_context_id) {
   base::TimeTicks then = pending_audible_durations_[audio_context_id];
   DCHECK(!then.is_null());
 
@@ -104,6 +108,16 @@ void AudioContextManagerImpl::RecordAudibleTime(base::TimeDelta audible_time) {
       .SetIsMainFrame(render_frame_host().IsInPrimaryMainFrame())
       .SetAudibleTime(GetBucketedTimeInMilliseconds(audible_time))
       .Record(ukm_recorder);
+}
+
+void AudioContextManagerImpl::AudioContextCreated(uint32_t audio_context_id) {
+  concurrent_audio_context_ids_.insert(audio_context_id);
+  max_concurrent_audio_contexts_ = std::max(
+      max_concurrent_audio_contexts_, concurrent_audio_context_ids_.size());
+}
+
+void AudioContextManagerImpl::AudioContextClosed(uint32_t audio_context_id) {
+  concurrent_audio_context_ids_.erase(audio_context_id);
 }
 
 }  // namespace content
