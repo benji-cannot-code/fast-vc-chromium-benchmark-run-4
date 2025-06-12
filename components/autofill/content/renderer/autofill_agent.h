f5e011368cdb0be1cb36a571780598c3ebc0a68c
Piotr Kotynia <piotrkotynia@google.com>
2025-06-12 04:14:25 -0700

    [Autofill] Delete unused AutofillAgent function declarations
    Bug: 40100455
    Change-Id: Ie208fc502c966d66f0564bb7d6528cf64dd4f3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633360
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472966}

--- components/autofill/content/renderer/autofill_agent.h
+++ components/autofill/content/renderer/autofill_agent.h
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_mode.h"
 
 namespace blink {
-class WebNode;
 class WebFormControlElement;
 class WebFormElement;
 }  // namespace blink
@@ -205,10 +204,6 @@ class AutofillAgent : public content::RenderFrameObserver,
   void GetPotentialLastFourCombinationsForStandaloneCvc(
       base::OnceCallback<void(const std::vector<std::string>&)>
           potential_matches) override;
-
-  base::WeakPtr<AutofillAgent> GetWeakPtr() {
-    return weak_ptr_factory_.GetWeakPtr();
-  }
 
   // Called after updating the last interacted element in FormTracker because of
   // `reason`. It is always the case that `form` or `element` are non-null. If
@@ -389,11 +384,6 @@ class AutofillAgent : public content::RenderFrameObserver,
       const blink::WebElement& element,
       AutofillSuggestionTriggerSource trigger_source);
 
-  // Sets the selected value of the the field identified by `field_id` to
-  // `suggested_value`.
-  void DoAcceptDataListSuggestion(FieldRendererId field_id,
-                                  const std::u16string& suggested_value);
-
   // Set `element` to display the given `value`.
   void DoFillFieldWithValue(std::u16string_view value,
                             blink::WebFormControlElement& element,
@@ -477,10 +467,6 @@ class AutofillAgent : public content::RenderFrameObserver,
   // Stores immutable configuration this agent was created with. It contains
   // features and settings that are specific to the client using this agent.
   const Config config_;
-
-  // Return the next web node of `current_node` in the DOM. `next` determines
-  // the direction to traverse in.
-  blink::WebNode NextWebNode(const blink::WebNode& current_node, bool next);
 
   // Contains the forms of the document.
   FormCache form_cache_{this};
