97ef43f814f2646d4dfc026ff05db29c0791725b
Divyansh Mangal <dmangal@microsoft.com>
2025-06-13 03:04:10 -0700

    [SVG] Granular handling of mutations when attributes are changed on referenced svg element.
    According to the spec [1], any event listeners defined on an
    element in the referenced graphics must also listen for the same
    event, at the same capture phase, on each instance of that element
    in a use-element shadow tree. In Chromium, we handle this by copying
    the event listeners of the target element to the corresponding
    shadow DOM element.
    
    However, if multiple events are defined and we mutate the referenced
    element using those events, an issue arises where subsequent event
    states get lost. The reason for this is the way we currently handle
    mutations in Chromium. We handle them by marking all the related
    use-instances as dirty (and eventually deleting them), which means
    all state relevant to specific nodes gets lost.
    
    To remedy this in this CL, we introduced a more fine-grained
    handling of mutations instead of rebuilding the use-shadow DOM
    tree from scratch. Here, when attributes on an referenced svg
    element are changed or modified, we pass or supply the same change
    to all the shadow instances of the element as well.
    This way, the original problem of events getting lost will also be
    fixed as we are not rebuilding the use-shadow tree.
    
    [1] https://svgwg.org/svg2-draft/struct.html#UseEventHandling
    
    Bug: 416091474
    Change-Id: I33c1133faf0c6664b2500b67383bd2c2a8c61e58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6623901
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1473451}

--- third_party/blink/renderer/core/svg/svg_element.cc
+++ third_party/blink/renderer/core/svg/svg_element.cc
@@ -722,7 +722,7 @@ void SVGElement::AttributeChanged(const AttributeModificationParams& params) {
 
   if (property) {
     SvgAttributeChanged({*property, params.name, params.reason});
-    InvalidateInstances();
+    SynchronizeAttributeInShadowInstances(params.name, params.new_value);
     return;
   }
 
@@ -741,7 +741,7 @@ void SVGElement::AttributeChanged(const AttributeModificationParams& params) {
       CssPropertyIdForSVGAttributeName(GetExecutionContext(), params.name);
   if (prop_id > CSSPropertyID::kInvalid) {
     UpdatePresentationAttributeStyle(prop_id, params.name, params.new_value);
-    InvalidateInstances();
+    SynchronizeAttributeInShadowInstances(params.name, params.new_value);
     return;
   }
 }
@@ -1201,6 +1201,21 @@ SMILTimeContainer* SVGElement::GetTimeContainer() const {
   }
 
   return ownerSVGElement()->TimeContainer();
+}
+
+// TODO: When implementing <use> scoping rules this may need to be applied more
+// widely. (crbug.com/40550039)
+void SVGElement::SynchronizeAttributeInShadowInstances(
+    const QualifiedName& name,
+    const AtomicString& value) {
+  if (RuntimeEnabledFeatures::SvgUseInstancesAttributeSyncEnabled()) {
+    const HeapHashSet<WeakMember<SVGElement>>& set = InstancesForElement();
+    for (SVGElement* instance : set) {
+      instance->SetAttributeWithoutValidation(name, value);
+    }
+  } else {
+    InvalidateInstances();
+  }
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/svg/svg_use_element_test.cc
+++ third_party/blink/renderer/core/svg/svg_use_element_test.cc
@@ -127,9 +127,9 @@ TEST_F(SVGUseElementTest, NullInstanceRootWhenShadowTreePendingRebuild) {
   GetDocument().body()->setInnerHTML(R"HTML(
     <svg>
       <defs>
-        <rect id="r" width="100" height="100" fill="blue"/>
+        <g id="g"/>
       </defs>
-      <use id="target" href="#r"/>
+      <use id="target" href="#g"/>
     </svg>
   )HTML");
   UpdateAllLifecyclePhasesForTest();
@@ -140,8 +140,10 @@ TEST_F(SVGUseElementTest, NullInstanceRootWhenShadowTreePendingRebuild) {
   ASSERT_TRUE(target->InstanceRoot());
 
   GetDocument()
-      .getElementById(AtomicString("r"))
-      ->setAttribute(html_names::kWidthAttr, AtomicString("50"));
+      .getElementById(AtomicString("g"))
+      ->appendChild(GetDocument().createElementNS(
+          AtomicString("http://www.w3.org/2000/svg"), AtomicString("text"),
+          ASSERT_NO_EXCEPTION));
 
   ASSERT_FALSE(target->InstanceRoot());
 }
