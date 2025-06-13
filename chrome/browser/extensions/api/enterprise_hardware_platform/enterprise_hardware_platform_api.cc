12e100b1fbc1556e8e2050ba0902788ca869fd25
James Cook <jamescook@chromium.org>
2025-06-13 10:50:35 -0700

    extensions: Port chrome.enterprise.hardwarePlatform to desktop Android
    This API is used by the Hangouts Services component extension. There's
    also a policy to enable it, which is not part of this CL. I'll enable
    the policy on Android in a followup.
    
    Bug: 405218977
    Change-Id: I7d8a0acefa0e4038ed2783756dcbe25f4f5240f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642319
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473676}

--- chrome/browser/extensions/api/enterprise_hardware_platform/enterprise_hardware_platform_api.cc
+++ chrome/browser/extensions/api/enterprise_hardware_platform/enterprise_hardware_platform_api.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/enterprise_hardware_platform.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
+#include "extensions/buildflags/buildflags.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
--- chrome/browser/extensions/api/enterprise_hardware_platform/enterprise_hardware_platform_api_unittest.cc
+++ chrome/browser/extensions/api/enterprise_hardware_platform/enterprise_hardware_platform_api_unittest.cc
@@ -13,8 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "extensions/browser/api_test_utils.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_builder.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
