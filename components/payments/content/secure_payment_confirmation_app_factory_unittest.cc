4016b8dd050978cb4dee0376141aa881ea786265
Darwin Yang <darwinyang@google.com>
2025-06-13 11:31:15 -0700

    [spc] Filter payment entities logos on SPC app
    Change GetPaymentEntitiesLogos() to return a vector of payment entities
    logos that filters out any empty bitmaps. Note that since this would
    modify the original vector of logos, a new vector is returned with
    pointers to the logos. The member vector of logos contains the unique
    pointers of the logos as the SPC app should retain ownership and the
    lifecycle of the logos are directly linked to the payment app.
    
    Bug: 416515989
    Change-Id: I38e010b9d94a6c4bd22e424c2890256c44195e2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6641241
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473707}

--- components/payments/content/secure_payment_confirmation_app_factory_unittest.cc
+++ components/payments/content/secure_payment_confirmation_app_factory_unittest.cc
@@ -51,6 +51,7 @@ using ::testing::Field;
 using ::testing::IsEmpty;
 using ::testing::IsNull;
 using ::testing::Matcher;
+using ::testing::Pointee;
 using ::testing::Pointer;
 using ::testing::Property;
 using ::testing::Return;
@@ -610,7 +611,7 @@ class SecurePaymentConfirmationAppFactoryNetworkAndIssuerIconsTest
     }
 
     ASSERT_TRUE(static_cast<content::TestWebContents*>(web_contents_.get())
-                    ->TestDidDownloadImage(image_url, /*https_status_code=*/200,
+                    ->TestDidDownloadImage(image_url, /*http_status_code=*/200,
                                            std::move(icon_bitmaps),
                                            std::move(icon_sizes)));
   }
@@ -624,12 +625,13 @@ Matcher<const SkBitmap*> IsSkBitmapWithHeight(int height) {
   return Pointer(Property("height", &SkBitmap::height, height));
 }
 
-Matcher<const PaymentApp::PaymentEntityLogo&> IsPaymentEntityLogo(
+Matcher<PaymentApp::PaymentEntityLogo*> IsPaymentEntityLogo(
     const std::u16string& label,
     Matcher<const SkBitmap*> icon_matcher) {
-  return AllOf(Field("label", &PaymentApp::PaymentEntityLogo::label, label),
-               Field("icon", &PaymentApp::PaymentEntityLogo::icon,
-                     Pointer(icon_matcher)));
+  return Pointer(
+      AllOf(Field("label", &PaymentApp::PaymentEntityLogo::label, label),
+            Field("icon", &PaymentApp::PaymentEntityLogo::icon,
+                  Pointer(icon_matcher))));
 }
 
 // Tests that when neither the network nor issuer icons are specified, they are
@@ -723,8 +725,7 @@ TEST_F(SecurePaymentConfirmationAppFactoryNetworkAndIssuerIconsTest,
   EXPECT_FALSE(created_payment_app->network_bitmap());
   EXPECT_TRUE(created_payment_app->issuer_bitmap());
   EXPECT_THAT(created_payment_app->GetPaymentEntitiesLogos(),
-              ElementsAre(IsPaymentEntityLogo(u"", IsNull()),
-                          IsPaymentEntityLogo(u"Issuer Name",
+              ElementsAre(IsPaymentEntityLogo(u"Issuer Name",
                                               IsSkBitmapWithHeight(60))));
 }
 
@@ -802,8 +803,7 @@ TEST_F(SecurePaymentConfirmationAppFactoryNetworkAndIssuerIconsTest,
   EXPECT_FALSE(created_payment_app->network_bitmap());
   EXPECT_TRUE(created_payment_app->issuer_bitmap());
   EXPECT_THAT(created_payment_app->GetPaymentEntitiesLogos(),
-              ElementsAre(IsPaymentEntityLogo(u"Network Name", IsNull()),
-                          IsPaymentEntityLogo(u"Issuer Name",
+              ElementsAre(IsPaymentEntityLogo(u"Issuer Name",
                                               IsSkBitmapWithHeight(60))));
 }
 
