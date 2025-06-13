f0d3b0552bea8bc370adb029b6ec12bdd9d9d119
Kamila Hasanbega <hkamila@google.com>
2025-06-13 07:10:24 -0700

    DBD revamp: Add my activity linkout
    Adds the linkout to MyActivity in Other Google Data dialog.
    
    Bug: 397187800
    Change-Id: I0b4a357f31729034cb213c6fe403ddd4394e22ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6632812
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473546}

--- chrome/test/data/webui/settings/other_google_data_dialog_test.ts
+++ chrome/test/data/webui/settings/other_google_data_dialog_test.ts
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://settings/lazy_load.js';
 
 import type {SettingsOtherGoogleDataDialogElement} from 'chrome://settings/lazy_load.js';
-import {PasswordManagerImpl, PasswordManagerPage} from 'chrome://settings/settings.js';
+import {loadTimeData, OpenWindowProxyImpl, PasswordManagerImpl, PasswordManagerPage} from 'chrome://settings/settings.js';
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {TestOpenWindowProxy} from 'chrome://webui-test/test_open_window_proxy.js';
 
 import {TestPasswordManagerProxy} from './test_password_manager_proxy.js';
 
@@ -16,10 +17,14 @@ import {TestPasswordManagerProxy} from './test_password_manager_proxy.js';
 suite('OtherGoogleDataDialog', function() {
   let dialog: SettingsOtherGoogleDataDialogElement;
   let passwordManagerProxy: TestPasswordManagerProxy;
+  let testOpenWindowProxy: TestOpenWindowProxy;
 
   setup(function() {
     passwordManagerProxy = new TestPasswordManagerProxy();
     PasswordManagerImpl.setInstance(passwordManagerProxy);
+
+    testOpenWindowProxy = new TestOpenWindowProxy();
+    OpenWindowProxyImpl.setInstance(testOpenWindowProxy);
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     dialog = document.createElement('settings-other-google-data-dialog');
@@ -33,5 +38,13 @@ suite('OtherGoogleDataDialog', function() {
     assertEquals(
         PasswordManagerPage.PASSWORDS,
         await passwordManagerProxy.whenCalled('showPasswordManager'));
+  });
+
+  test('MyActivityLinkClick', async function() {
+    dialog.$.myActivityLink.click();
+
+    const url = await testOpenWindowProxy.whenCalled('openUrl');
+    assertEquals(
+        loadTimeData.getString('deleteBrowsingDataMyActivityUrl'), url);
   });
 });
