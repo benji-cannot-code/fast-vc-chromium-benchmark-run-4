ce9b6a202d50ee3caecccc7db382308e465299be
Colin Blundell <blundell@chromium.org>
2025-06-13 02:25:16 -0700

    [Blink] Introduce and use GetResourceProviderForWebGPU()
    Replaces ResourceProviderDEPRECATED() for callsites where the context is
    WebGPU.
    
    Bug: 352263194
    Change-Id: I30b8c219a07f0fdd890876409f0dc084521d843a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6641537
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473443}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h
@@ -142,6 +142,10 @@ class CORE_EXPORT CanvasRenderingContextHost : public GarbageCollectedMixin,
     CHECK(IsWebGL());
     return ResourceProviderDEPRECATED();
   }
+  CanvasResourceProvider* GetResourceProviderForWebGPU() const {
+    CHECK(IsWebGPU());
+    return ResourceProviderDEPRECATED();
+  }
   CanvasResourceProvider* GetResourceProviderForCanvas2D() const {
     CHECK(IsRenderingContext2D());
     return ResourceProviderDEPRECATED();
