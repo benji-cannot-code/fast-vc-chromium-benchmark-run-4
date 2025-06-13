0b810cb1b52d730e24dc982cf514c554fd41e309
Robbie Gibson <rkgibson@google.com>
2025-06-13 11:55:35 -0700

    [iOS] Don't retain PopupMenuHelpController after stopping
    PopupMenuHelpCoordinator crashes because it accesses self.profile after
    stop is called. My hunch is this is it isn't set to nil after stopping,
    so it doesn't deallocate.
    
    This solution does that, but also changes the self.profile accesses to
    self.browser accesses, which don't crash (but will eventually CHECK). If
    these are not triggered, self.profile can return because that is
    clearer.
    
    Bug: 423868267, 424761561
    Change-Id: Ib20f64e061c174c97decba253ccf62ff92b7853d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642198
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473720}

--- ios/chrome/browser/popup_menu/ui_bundled/popup_menu_help_coordinator.mm
+++ ios/chrome/browser/popup_menu/ui_bundled/popup_menu_help_coordinator.mm
@@ -81,6 +81,10 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
 @implementation PopupMenuHelpCoordinator {
   raw_ptr<segmentation_platform::DeviceSwitcherResultDispatcher>
       _deviceSwitcherResultDispatcher;
+
+  // Whether the coordinator has been stopped.
+  // TODO(crbug.com/424761561): Remove when crashes stop.
+  BOOL _stopped;
 }
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
@@ -99,9 +103,8 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
 #pragma mark - Getters
 
 - (feature_engagement::Tracker*)featureEngagementTracker {
-  if (!self.profile) {
-    return nullptr;
-  }
+  CHECK(!_stopped, base::NotFatalUntil::M147)
+      << "PopupMenuHelpCoordinator used after -stop";
   feature_engagement::Tracker* tracker =
       feature_engagement::TrackerFactory::GetForProfile(self.profile);
   DCHECK(tracker);
@@ -135,6 +138,8 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
 }
 
 - (void)stop {
+  _stopped = YES;
+
   SceneState* sceneState = self.browser->GetSceneState();
   [sceneState removeObserver:self];
 }
@@ -261,10 +266,8 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
 
 // Returns whether blue dot should be shown.
 - (BOOL)shouldShowBlueDot {
-  // Don't show blue dot if cannot make a decision.
-  if (!self.profile) {
-    return NO;
-  }
+  CHECK(!_stopped, base::NotFatalUntil::M147)
+      << "PopupMenuHelpCoordinator used after -stop";
 
   // As sync error takes precendence on blue dot for settings destination in the
   // overflow menu. In that case don't show blue dot as the full path from
