a79e448d2cdbd655499f4b5dea6ee31ad8cb3138
Thomas Guilbert <tguilbert@chromium.org>
2025-06-12 17:17:14 -0700

    Add originating media timestamps to transcriptions
    This CL uses the SpeechTimestampEstimator retrieve the media timestamps
    which originated a speech transcription, and forwards the timestamps to
    parties interested in receiving transcriptions
    
    Bug: 413823334
    Change-Id: I0fd9500f50587d3cad0e692bee912072ef46643c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619192
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473282}

--- chrome/services/speech/speech_timestamp_estimator.h
+++ chrome/services/speech/speech_timestamp_estimator.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/circular_deque.h"
 #include "base/time/time.h"
 #include "base/types/strong_alias.h"
+#include "media/mojo/mojom/speech_recognition_result.h"
 
 namespace speech {
 
@@ -43,8 +44,7 @@ class SpeechTimestampEstimator {
 
   // Note: these `base::TimeDelta`s are `MediaTimestamp`s, but avoid forcing
   // callers to deal with the `base::StrongAlias`.
-  using MediaTimestampRange = std::pair<base::TimeDelta, base::TimeDelta>;
-  using MediaRanges = std::vector<MediaTimestampRange>;
+  using MediaRanges = std::vector<media::MediaTimestampRange>;
 
   // Represents a chunk of originating media played for a certain amount of
   // time, and its position in the queue of all audio sent to the speech
