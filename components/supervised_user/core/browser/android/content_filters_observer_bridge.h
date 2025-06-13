e637e5ec99415439cdb30bd1b78ba3b1d1eb735d
Jan Keitel <jkeitel@google.com>
2025-06-13 03:27:17 -0700

    Revert "Add log statements for the Content Filter Observer bridge."
    This reverts commit f1f63f1265b693359589e9a5ed6b3cb75e2db53a.
    
    Reason for revert: Suspect for causing tree closure.
    
    Bug: 424071314
    Original change's description:
    > Add log statements for the Content Filter Observer bridge.
    >
    > Bug: 424071314
    > Change-Id: Iac5a249dfb77250abe03da9058ea6afb312479a5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633706
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1473458}
    
    Bug: 424071314
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iab1aaedb4cc8937ce10db0158fe9328e092fc267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6632821
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473459}

--- components/supervised_user/core/browser/android/content_filters_observer_bridge.h
+++ components/supervised_user/core/browser/android/content_filters_observer_bridge.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SUPERVISED_USER_CORE_BROWSER_ANDROID_CONTENT_FILTERS_OBSERVER_BRIDGE_H_
 #define COMPONENTS_SUPERVISED_USER_CORE_BROWSER_ANDROID_CONTENT_FILTERS_OBSERVER_BRIDGE_H_
 
-#include <string>
-
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/functional/callback.h"
@@ -25,9 +23,6 @@ class ContentFiltersObserverBridge {
   explicit ContentFiltersObserverBridge(std::string_view setting_name,
                                         base::RepeatingClosure on_enabled,
                                         base::RepeatingClosure on_disabled);
-  ContentFiltersObserverBridge(const ContentFiltersObserverBridge&) = delete;
-  ContentFiltersObserverBridge& operator=(
-      const ContentFiltersObserverBridge&) = delete;
   ~ContentFiltersObserverBridge();
 
   // Called after creating the bridge and when the setting is enabled or
@@ -35,10 +30,9 @@ class ContentFiltersObserverBridge {
   void OnChange(JNIEnv* env, bool enabled);
 
  private:
-  std::string setting_name_;
+  base::android::ScopedJavaGlobalRef<jobject> bridge_;
   base::RepeatingClosure on_enabled_;
   base::RepeatingClosure on_disabled_;
-  base::android::ScopedJavaGlobalRef<jobject> bridge_;
 };
 
 }  // namespace supervised_user
