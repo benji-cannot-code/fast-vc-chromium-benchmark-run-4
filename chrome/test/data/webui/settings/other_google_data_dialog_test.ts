86bd99947de6c03874019c3b50a72eda3157012f
Kamila Hasanbega <hkamila@google.com>
2025-06-13 11:13:16 -0700

    DBD revamp: Add Search History linkout
    Adds linkout to Other Google Data dialog.
    
    Bug: 397187800
    Change-Id: I450d6c568342718ac264e69ba144dafab7dab591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633618
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473699}

--- chrome/test/data/webui/settings/other_google_data_dialog_test.ts
+++ chrome/test/data/webui/settings/other_google_data_dialog_test.ts
@@ -47,4 +47,12 @@ suite('OtherGoogleDataDialog', function() {
     assertEquals(
         loadTimeData.getString('deleteBrowsingDataMyActivityUrl'), url);
   });
+
+  test('SearchHistoryLinkClick', async function() {
+    dialog.$.searchHistoryLink.click();
+
+    const url = await testOpenWindowProxy.whenCalled('openUrl');
+    assertEquals(
+        loadTimeData.getString('deleteBrowsingDataSearchHistoryUrl'), url);
+  });
 });
