d87dff06c8015b0639a85b3369bfec4ad477b155
Lei Zhang <thestig@chromium.org>
2025-06-13 01:25:21 -0700

    Reland "Conditionally include base/logging.h in base/feature_list.h"
    This is a reland of commit 64b33935a2d6965357b5c616eb7a95ac7fba57e6
    
    Some //base header cleanups collided and the original CL revealed yet
    another missing include, which https://crrev.com/c/6628957 fixes.
    
    With all the //base header cleanups, do more IWYU to fix the build.
    
    Original change's description:
    > Conditionally include base/logging.h in base/feature_list.h
    >
    > In base/feature_list.h, base/logging.h is effectively only needed on
    > Chrome OS. Make this include conditional, and then do IWYU to fix the
    > build.
    >
    > Change-Id: I02371f4873a62d7bf66d5460fa731a652cb0ac9a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626024
    > Owners-Override: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1470949}
    
    Change-Id: I8432c2080e482e79d7e16b68511ed963887e46cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629437
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473417}

--- components/autofill/core/common/credit_card_number_validation_unittest.cc
+++ components/autofill/core/common/credit_card_number_validation_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 #include <string>
 
+#include "base/strings/utf_ostream_operators.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
--- components/policy/core/common/remote_commands/remote_commands_constants.cc
+++ components/policy/core/common/remote_commands/remote_commands_constants.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <ostream>
+
 #include "base/feature_list.h"
 #include "base/notreached.h"
 #include "components/invalidation/invalidation_constants.h"
