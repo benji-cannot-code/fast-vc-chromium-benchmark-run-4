5691e66005c38e26715b074e9a16aae31db93970
Arthur Milchior <arthurmilchior@google.com>
2025-06-13 06:41:21 -0700

    [iOS]Remove iOS16 condition
    Chrome support starts at iOS16 so the condition always hold.
    
    Bug: None
    Change-Id: Idbfe36f77e1f0e007f665ad3b93aaddd7d4fcea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6632745
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473530}

--- ios/web/navigation/crw_wk_navigation_handler.mm
+++ ios/web/navigation/crw_wk_navigation_handler.mm
@@ -199,10 +199,8 @@ void LogPresentingErrorPageFailedWithError(NSError* error) {
   if (!self.beingDestroyed) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-      if (@available(iOS 16.0, *)) {
-        web::GetWebClient()->SetOSLockdownModeEnabled(
-            preferences.lockdownModeEnabled);
-      }
+      web::GetWebClient()->SetOSLockdownModeEnabled(
+          preferences.lockdownModeEnabled);
     });
   }
 
@@ -251,25 +249,22 @@ void LogPresentingErrorPageFailedWithError(NSError* error) {
   BOOL isMainFrameNavigationAction = [self isMainFrameNavigationAction:action];
   auto decisionHandler = ^(WKNavigationActionPolicy policy) {
     preferences.preferredContentMode = contentMode;
-    if (@available(iOS 16.0, *)) {
+    if ((policy == WKNavigationActionPolicyAllow) &&
+        isMainFrameNavigationAction) {
+      UMA_HISTOGRAM_BOOLEAN("IOS.MainFrameNavigationIsInLockdownMode",
+                            preferences.lockdownModeEnabled);
+    }
+
+    if (!self.beingDestroyed) {
+      bool browser_lockdown_mode_enabled =
+          web::GetWebClient()->IsBrowserLockdownModeEnabled();
       if ((policy == WKNavigationActionPolicyAllow) &&
           isMainFrameNavigationAction) {
-        UMA_HISTOGRAM_BOOLEAN("IOS.MainFrameNavigationIsInLockdownMode",
-                              preferences.lockdownModeEnabled);
+        UMA_HISTOGRAM_BOOLEAN("IOS.MainFrameNavigationIsInBrowserLockdownMode",
+                              browser_lockdown_mode_enabled);
       }
-
-      if (!self.beingDestroyed) {
-        bool browser_lockdown_mode_enabled =
-            web::GetWebClient()->IsBrowserLockdownModeEnabled();
-        if ((policy == WKNavigationActionPolicyAllow) &&
-            isMainFrameNavigationAction) {
-          UMA_HISTOGRAM_BOOLEAN(
-              "IOS.MainFrameNavigationIsInBrowserLockdownMode",
-              browser_lockdown_mode_enabled);
-        }
-        if (browser_lockdown_mode_enabled) {
-          preferences.lockdownModeEnabled = true;
-        }
+      if (browser_lockdown_mode_enabled) {
+        preferences.lockdownModeEnabled = true;
       }
     }
     handler(policy, preferences);
