0bffbef42612dea85b993f65e50e8d830ccf4583
Maks Orlovich <morlovich@chromium.org>
2025-06-13 09:08:09 -0700

    net: avoid memcpy in sqlite_persistent_shared_dictionary_store.cc
    Bug: 40285824
    Change-Id: I8cdb51417b49c66c66983318a5fdfcc4e1a99493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6640273
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473614}

--- net/extras/sqlite/sqlite_persistent_shared_dictionary_store.cc
+++ net/extras/sqlite/sqlite_persistent_shared_dictionary_store.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "net/extras/sqlite/sqlite_persistent_shared_dictionary_store.h"
 
 #include "base/containers/span.h"
@@ -137,7 +132,7 @@ std::optional<SHA256HashValue> ToSHA256HashValue(
   if (sha256_bytes.size() != sha256_hash.size()) {
     return std::nullopt;
   }
-  memcpy(sha256_hash.data(), sha256_bytes.data(), sha256_bytes.size());
+  base::span(sha256_hash).copy_from_nonoverlapping(sha256_bytes);
   return sha256_hash;
 }
 
