2530ff2dc7c8ac7c86f10c23e1395a028207bc5d
Chuong Ho <hdchuong@google.com>
2025-06-13 16:57:15 -0700

    quick-answer: add magic boost consent view
    This CL adds a consent view in case the Magic Boost consent flow is enabled. The view contains:
    * The magic boost header + the settings button
    * The intent chip (currently displaying the selected text, and will be updated later)
    
    This CL also migrates some components (Magic Boost header, some constants) to files that can be shared across different classes.
    
    Bug: b:414391121
    
    Change-Id: I7c8c0c5e1c9cf5eaf4045bec4908391aff7ee5e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6615801
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473831}

--- chrome/browser/ui/ash/quick_answers/quick_answers_ui_controller.cc
+++ chrome/browser/ui/ash/quick_answers/quick_answers_ui_controller.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/quick_answers/quick_answers_controller_impl.h"
+#include "chrome/browser/ui/ash/quick_answers/ui/magic_boost_user_consent_view.h"
 #include "chrome/browser/ui/ash/quick_answers/ui/quick_answers_util.h"
 #include "chrome/browser/ui/ash/quick_answers/ui/quick_answers_view.h"
 #include "chrome/browser/ui/ash/quick_answers/ui/rich_answers_definition_view.h"
@@ -281,23 +282,36 @@ void QuickAnswersUiController::CreateUserConsentViewInternal(
   CHECK_EQ(controller_->GetQuickAnswersVisibility(),
            QuickAnswersVisibility::kPending);
 
-  auto* view = GetReadWriteCardsUiController().SetQuickAnswersUi(
-      views::Builder<quick_answers::UserConsentView>(
-          std::make_unique<quick_answers::UserConsentView>(
-              use_refreshed_design, GetReadWriteCardsUiController()))
-          .SetIntentType(intent_type)
-          .SetIntentText(intent_text)
-          // It is safe to do `base::Unretained(this)`. UIs are destructed
-          // before a UI controller gets destructed. See
-          // `~QuickAnswersUiController`.
-          .SetNoThanksButtonPressed(base::BindRepeating(
-              &QuickAnswersUiController::OnUserConsentNoThanksPressed,
-              base::Unretained(this)))
-          .SetAllowButtonPressed(base::BindRepeating(
-              &QuickAnswersUiController::OnUserConsentAllowPressed,
-              base::Unretained(this)))
-          .Build());
-  user_consent_view_.SetView(view);
+  if (chromeos::features::IsMagicBoostRevampForQuickAnswersEnabled() &&
+      QuickAnswersState::GetFeatureType() ==
+          QuickAnswersState::FeatureType::kHmr) {
+    user_consent_view_.SetView(
+        GetReadWriteCardsUiController().SetQuickAnswersUi(
+            views::Builder<quick_answers::MagicBoostUserConsentView>(
+                std::make_unique<quick_answers::MagicBoostUserConsentView>(
+                    // TODO: crbug.com/414391121 - Populate the button label
+                    // with the correct text.
+                    intent_text, GetReadWriteCardsUiController()))
+                .Build()));
+  } else {
+    user_consent_view_.SetView(
+        GetReadWriteCardsUiController().SetQuickAnswersUi(
+            views::Builder<quick_answers::UserConsentView>(
+                std::make_unique<quick_answers::UserConsentView>(
+                    use_refreshed_design, GetReadWriteCardsUiController()))
+                .SetIntentType(intent_type)
+                .SetIntentText(intent_text)
+                // It is safe to do `base::Unretained(this)`. UIs are destructed
+                // before a UI controller gets destructed. See
+                // `~QuickAnswersUiController`.
+                .SetNoThanksButtonPressed(base::BindRepeating(
+                    &QuickAnswersUiController::OnUserConsentNoThanksPressed,
+                    base::Unretained(this)))
+                .SetAllowButtonPressed(base::BindRepeating(
+                    &QuickAnswersUiController::OnUserConsentAllowPressed,
+                    base::Unretained(this)))
+                .Build()));
+  }
 
   // `ViewAccessibility::AnnounceText` requires a root view. Announce text after
   // a view gets attached to a widget.
---
+++ chrome/browser/ui/ash/quick_answers/ui/magic_boost_header.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ASH_QUICK_ANSWERS_UI_MAGIC_BOOST_HEADER_H_
+#define CHROME_BROWSER_UI_ASH_QUICK_ANSWERS_UI_MAGIC_BOOST_HEADER_H_
+
+#include "ui/views/layout/box_layout_view.h"
+
+namespace quick_answers {
+
+views::Builder<views::BoxLayoutView> GetMagicBoostHeader();
+
+}
+
+#endif  // CHROME_BROWSER_UI_ASH_QUICK_ANSWERS_UI_MAGIC_BOOST_HEADER_H_
