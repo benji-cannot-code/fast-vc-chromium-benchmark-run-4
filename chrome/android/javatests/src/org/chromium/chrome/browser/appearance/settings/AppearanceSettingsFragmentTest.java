7dadc9855c2a5c2aae2c03f122fd62d281f21f3e
Mark Schillaci <mschillaci@google.com>
2025-06-13 14:48:39 -0700

    [Bookmarks Bar] Update settings page to list to pref changes directly
    This CL continues the refactor work for Bookmarks Bar visibility and
    settings.
    
    With this CL we remove the dependency of the AppearanceSettingsFragment
    on the BookmarkBarSettingProvider, and instead have the existing profile
    of the fragment register itself as a pref observer to accomplish the
    same.
    
    Bug: 424456738
    Change-Id: I3d445a286d77012ca348858bb29d20d811e8f519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635524
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473786}

--- chrome/android/javatests/src/org/chromium/chrome/browser/appearance/settings/AppearanceSettingsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/appearance/settings/AppearanceSettingsFragmentTest.java
@@ -22,7 +22,7 @@ import androidx.preference.Preference;
 import androidx.test.annotation.UiThreadTest;
 import androidx.test.filters.SmallTest;
 
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -50,9 +50,11 @@ import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarStatePredicto
 import org.chromium.chrome.browser.toolbar.adaptive.settings.AdaptiveToolbarSettingsFragment;
 import org.chromium.components.browser_ui.settings.BlankUiTestActivitySettingsTestRule;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
+import org.chromium.components.prefs.PrefChangeRegistrar;
 import org.chromium.components.prefs.PrefChangeRegistrar.PrefObserver;
 import org.chromium.components.prefs.PrefChangeRegistrarJni;
 import org.chromium.components.prefs.PrefService;
+import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.components.user_prefs.UserPrefsJni;
 
 import java.util.HashSet;
@@ -69,10 +71,10 @@ public class AppearanceSettingsFragmentTest {
 
     @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
 
-    @Mock private PrefChangeRegistrarJni mPrefChangeRegistrarJni;
+    @Mock private PrefChangeRegistrar.Natives mPrefChangeRegistrarJni;
     @Mock private PrefService mPrefService;
     @Mock private Profile mProfile;
-    @Mock private UserPrefsJni mUserPrefsJni;
+    @Mock private UserPrefs.Natives mUserPrefsJni;
 
     private Set<PrefObserver> mBookmarkBarSettingObserverCache;
     private ObservableSupplierImpl<Boolean> mBookmarkBarSettingSupplier;
@@ -108,8 +110,8 @@ public class AppearanceSettingsFragmentTest {
                 .setBoolean(eq(Pref.SHOW_BOOKMARK_BAR), anyBoolean());
     }
 
-    @After
-    public void tearDown() {
+    @AfterClass
+    public static void tearDown() {
         PrefChangeRegistrarJni.setInstanceForTesting(null);
         UserPrefsJni.setInstanceForTesting(null);
     }
@@ -232,6 +234,7 @@ public class AppearanceSettingsFragmentTest {
                 /* fragmentArgs= */ null,
                 (fragment) -> ((AppearanceSettingsFragment) fragment).setProfile(mProfile));
         mSettings = (AppearanceSettingsFragment) mSettingsTestRule.getPreferenceFragment();
+        mBookmarkBarSettingObserverCache.add(mSettings.getPrefObserverForTesting());
     }
 
     private @NonNull <T> Answer<Void> runCallbackWithValueAtIndex(
