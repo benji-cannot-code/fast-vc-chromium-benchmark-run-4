efe35191cb79a8e9e2ace50957604622fbd720bd
Lei Zhang <thestig@chromium.org>
2025-06-12 17:46:31 -0700

    Fix DEVTOOLS_INSTRUMENTATION_DUMPING enabled builds
    Builders with this build flag enabled stopped working due to
    https://crrev.com/1472457. Add the missing include to fix it.
    
    Bug: 424479309
    Change-Id: Iecd82fcffc1addf81136d433857e66cfce2c7ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6640802
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Fumitoshi Ukai <ukai@google.com>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473300}

--- base/android/orderfile/orderfile_instrumentation.cc
+++ base/android/orderfile/orderfile_instrumentation.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(DEVTOOLS_INSTRUMENTATION_DUMPING)
 #include <sstream>
 
+#include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "base/trace_event/memory_dump_manager.h"   // no-presubmit-check
 #include "base/trace_event/memory_dump_provider.h"  // no-presubmit-check
