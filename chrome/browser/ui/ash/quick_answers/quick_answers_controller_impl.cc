30b6630ed36a728feb730f5be88f991e43350634
Chuong Ho <hdchuong@google.com>
2025-06-13 16:57:45 -0700

    quick-answer: adjust condition to show quick answers in magic boost mode
    Currently, quick answer UI is hidden if the quick answer state is in HMR
    mode.
    
    With the magic boost revamp flag enabled, this CL enables quick answers
    UI to be visible when quick answer state is in either HMR or Quick
    Answer.
    
    Bug: b:414391121
    Change-Id: Ice233d1db1819379a50307c554ec67600723a033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635591
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473832}

--- chrome/browser/ui/ash/quick_answers/quick_answers_controller_impl.cc
+++ chrome/browser/ui/ash/quick_answers/quick_answers_controller_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_state.h"
 #include "chromeos/components/quick_answers/quick_answers_client.h"
 #include "chromeos/components/quick_answers/quick_answers_model.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
@@ -88,23 +89,22 @@ bool ShouldShowQuickAnswers() {
     return true;
   }
 
-  // If feature type is `kQuickAnswers`, return `true` for the case `kUnknown`
-  // to show a consent UI.
   if (QuickAnswersState::GetFeatureType() ==
-      QuickAnswersState::FeatureType::kQuickAnswers) {
-    base::expected<quick_answers::prefs::ConsentStatus,
-                   QuickAnswersState::Error>
-        maybe_consent_status = QuickAnswersState::GetConsentStatus();
-    if (!maybe_consent_status.has_value()) {
-      return false;
-    }
-
-    if (maybe_consent_status.value() ==
-        quick_answers::prefs::ConsentStatus::kUnknown) {
-      return true;
-    }
+          QuickAnswersState::FeatureType::kHmr &&
+      !chromeos::features::IsMagicBoostRevampForQuickAnswersEnabled()) {
+    return false;
   }
 
+  base::expected<quick_answers::prefs::ConsentStatus, QuickAnswersState::Error>
+      maybe_consent_status = QuickAnswersState::GetConsentStatus();
+  if (!maybe_consent_status.has_value()) {
+    return false;
+  }
+
+  if (maybe_consent_status.value() ==
+      quick_answers::prefs::ConsentStatus::kUnknown) {
+    return true;
+  }
   return false;
 }
 
@@ -576,10 +576,9 @@ QuickAnswersControllerImpl::GetWeakPtr() {
 bool QuickAnswersControllerImpl::MaybeShowUserConsent(
     IntentType intent_type,
     const std::u16string& intent_text) {
-  // For non-QuickAnswers case (i.e., HMR), user consent is handled outside of
-  // QuickAnswers code.
-  if (QuickAnswersState::GetFeatureType() !=
-      QuickAnswersState::FeatureType::kQuickAnswers) {
+  if (QuickAnswersState::GetFeatureType() ==
+          QuickAnswersState::FeatureType::kHmr &&
+      !chromeos::features::IsMagicBoostRevampForQuickAnswersEnabled()) {
     return false;
   }
 
