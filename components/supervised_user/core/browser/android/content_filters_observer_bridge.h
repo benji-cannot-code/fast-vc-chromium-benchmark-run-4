31d8879b14769a2f9b801961dd194aeeb76791c1
Tomek Jurkiewicz <tju@google.com>
2025-06-13 03:19:49 -0700

    Add log statements for the Content Filter Observer bridge.
    Bug: 424071314
    Change-Id: Iac5a249dfb77250abe03da9058ea6afb312479a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633706
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473458}

--- components/supervised_user/core/browser/android/content_filters_observer_bridge.h
+++ components/supervised_user/core/browser/android/content_filters_observer_bridge.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SUPERVISED_USER_CORE_BROWSER_ANDROID_CONTENT_FILTERS_OBSERVER_BRIDGE_H_
 #define COMPONENTS_SUPERVISED_USER_CORE_BROWSER_ANDROID_CONTENT_FILTERS_OBSERVER_BRIDGE_H_
 
+#include <string>
+
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/functional/callback.h"
@@ -23,6 +25,9 @@ class ContentFiltersObserverBridge {
   explicit ContentFiltersObserverBridge(std::string_view setting_name,
                                         base::RepeatingClosure on_enabled,
                                         base::RepeatingClosure on_disabled);
+  ContentFiltersObserverBridge(const ContentFiltersObserverBridge&) = delete;
+  ContentFiltersObserverBridge& operator=(
+      const ContentFiltersObserverBridge&) = delete;
   ~ContentFiltersObserverBridge();
 
   // Called after creating the bridge and when the setting is enabled or
@@ -30,9 +35,10 @@ class ContentFiltersObserverBridge {
   void OnChange(JNIEnv* env, bool enabled);
 
  private:
-  base::android::ScopedJavaGlobalRef<jobject> bridge_;
+  std::string setting_name_;
   base::RepeatingClosure on_enabled_;
   base::RepeatingClosure on_disabled_;
+  base::android::ScopedJavaGlobalRef<jobject> bridge_;
 };
 
 }  // namespace supervised_user
