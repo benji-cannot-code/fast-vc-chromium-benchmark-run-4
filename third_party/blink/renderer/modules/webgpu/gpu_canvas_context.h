bfbbaaa89f89e8226b7870a64bd8d948bd9ce352
Colin Blundell <blundell@chromium.org>
2025-06-13 02:18:28 -0700

    [Blink] Streamline PaintRenderingResultsToCanvas() impls
    This method no longer needs to be on CanvasRenderingContext. This CL
    moves it to be private in the various contexts, inlining it in the
    contexts for which the implementation is trivial.
    
    Bug: 352263194
    Change-Id: Ifb08073b5f2c822aee19503d726ee3fa2a7071d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633572
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473432}

--- third_party/blink/renderer/modules/webgpu/gpu_canvas_context.h
+++ third_party/blink/renderer/modules/webgpu/gpu_canvas_context.h
@@ -69,8 +69,6 @@ class GPUCanvasContext : public ScriptWrappable,
   // Produces a snapshot of the current contents of the swap chain if possible
   // or else a snapshot of the most-recently presented contents.
   scoped_refptr<StaticBitmapImage> GetImage(FlushReason) final;
-  CanvasResourceProvider* PaintRenderingResultsToCanvas(
-      SourceDrawingBuffer) final;
   scoped_refptr<StaticBitmapImage> PaintRenderingResultsToSnapshot(
       SourceDrawingBuffer source_buffer,
       FlushReason reason) override;
@@ -116,6 +114,7 @@ class GPUCanvasContext : public ScriptWrappable,
   bool IsGPUDeviceDestroyed() override;
 
  private:
+  CanvasResourceProvider* PaintRenderingResultsToCanvas(SourceDrawingBuffer);
   scoped_refptr<WebGPUMailboxTexture> GetFrontBufferMailboxTexture();
   void DetachSwapBuffers();
   void ReplaceDrawingBuffer(bool destroy_swap_buffers);
