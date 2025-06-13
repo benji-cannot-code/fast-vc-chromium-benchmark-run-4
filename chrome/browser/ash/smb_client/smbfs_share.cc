5485918f60f70696b007ef1ff15bb3c1b75dc06a
Minseop Choi <minsubb13@gmail.com>
2025-06-13 00:19:36 -0700

    chrome/smb_client: migrate to new crypto/hash API
    Migrate smbfs_share in chrome/browser/ash/smb_client from crypto/sha2 to
    crypto/hash. The new interfaces are a bit more efficient due to making
    fewer heap allocations and a bit more resistant to misuse. They are also
    not deprecated.
    
    Bug: 374310081
    Change-Id: Ifac52282a2f5da4a46a0eb456d167eb74346cc4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634150
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473401}

--- chrome/browser/ash/smb_client/smbfs_share.cc
+++ chrome/browser/ash/smb_client/smbfs_share.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/smb_client/smb_service_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_credentials_dialog.h"
-#include "crypto/sha2.h"
+#include "crypto/hash.h"
 #include "storage/browser/file_system/external_mount_points.h"
 
 namespace ash::smb_client {
@@ -312,9 +312,8 @@ void SmbFsShare::SetMounterCreationCallbackForTest(
 }
 
 std::string SmbFsShare::GenerateStableMountId() const {
-  std::string hash_input = GenerateStableMountIdInput();
-  return base::ToLowerASCII(base::HexEncode(
-      crypto::SHA256HashString(hash_input).c_str(), crypto::kSHA256Length));
+  const auto input = GenerateStableMountIdInput();
+  return base::ToLowerASCII(base::HexEncode(crypto::hash::Sha256(input)));
 }
 
 std::string SmbFsShare::GenerateStableMountIdInput() const {
