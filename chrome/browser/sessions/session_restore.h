19a22ac92c4ddc44d37a042a4c46682003b8a12c
David Sanders <dsanders11@ucsbalum.com>
2025-06-12 23:45:59 -0700

    Remove unused include in session_restore.h
    Bug: 40318405
    Change-Id: I10c6c4deea689d3e2a913c0fcc89b1b0e82047c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6641109
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473391}

--- chrome/browser/sessions/session_restore.h
+++ chrome/browser/sessions/session_restore.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/observer_list.h"
 #include "chrome/browser/sessions/session_restore_observer.h"
-#include "components/history/core/browser/history_service.h"
 #include "components/sessions/core/session_types.h"
 #include "ui/base/window_open_disposition.h"
 
