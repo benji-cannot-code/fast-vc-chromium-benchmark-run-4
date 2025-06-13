2f29d74f3769ed9469b2af394f41d3e36678ff45
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-06-13 16:49:12 -0700

    Remove feature flag `kComplexOccluderForQuadsWithRoundedCorners`
    Bug: b:294589500
    Change-Id: Ib8b87b56813603bd7844063e0debf377fbe3e7eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6644001
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473829}

--- components/viz/service/display/occlusion_culler_unittest.cc
+++ components/viz/service/display/occlusion_culler_unittest.cc
@@ -3372,10 +3372,6 @@ class QuadsWithComplexOccluderTest
 };
 
 TEST_P(QuadsWithComplexOccluderTest, OcclusionCullingWithRoundedCorner) {
-  if (!features::IsComplexOccluderForQuadsWithRoundedCornersEnabled()) {
-    GTEST_SKIP();
-  }
-
   RendererSettings::OcclusionCullerSettings settings;
   settings.generate_complex_occluder_for_rounded_corners = true;
   settings.minimum_fragments_reduced = 0;
@@ -3508,10 +3504,6 @@ INSTANTIATE_TEST_SUITE_P(
 // inscribed rectangle within the rounded rectangle, rather than a more complex
 // occluder.
 TEST_F(OcclusionCullerTest, OcclusionCullingWithRoundedCornerOnSmallQuads) {
-  if (!features::IsComplexOccluderForQuadsWithRoundedCornersEnabled()) {
-    GTEST_SKIP();
-  }
-
   RendererSettings::OcclusionCullerSettings settings;
   settings.generate_complex_occluder_for_rounded_corners = true;
   settings.quad_split_limit = 10;
