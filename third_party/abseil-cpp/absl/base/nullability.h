5ec57ff5c60b57fd3508f931ef388357f8946396
Lei Zhang <thestig@chromium.org>
2025-06-12 23:38:49 -0700

    Remove Abseil deprecated nullability patch
    After https://crrev.com/1473185, this is no longer needed as all code
    has migrated away from the deprecated form of Abseil's nullability code.
    
    Bug: 412943761
    Change-Id: I852efa53f45b623b51d4b02f70751ba294383bd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635041
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473389}

--- third_party/abseil-cpp/absl/base/nullability.h
+++ third_party/abseil-cpp/absl/base/nullability.h
@@ -185,7 +185,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ABSL_BASE_NULLABILITY_H_
 
 #include "absl/base/config.h"
-#include "absl/base/internal/nullability_deprecated.h"
 
 // ABSL_POINTERS_DEFAULT_NONNULL
 //
