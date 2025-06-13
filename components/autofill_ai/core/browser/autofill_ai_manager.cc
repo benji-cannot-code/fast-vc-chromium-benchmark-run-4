c173b4ae6ac4c88f473b1a28d8f4044da7485c1d
Jihad Hanna <jihadghanna@google.com>
2025-06-13 10:52:55 -0700

    Remove AutofillAiClient::GetEntityDataManager()
    The same function exists in AutofillClient and AutofillAiClient has a
    public getter to AutofillClient.
    
    Bug: 411316204
    Change-Id: Id59aa64872ea42f3e1329207216f76cb9bb90f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633423
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473680}

--- components/autofill_ai/core/browser/autofill_ai_manager.cc
+++ components/autofill_ai/core/browser/autofill_ai_manager.cc
@@ -222,7 +222,8 @@ void AutofillAiManager::OnFormSeen(const FormStructure& form) {
     return;
   }
 
-  autofill::EntityDataManager* entity_manager = client_->GetEntityDataManager();
+  autofill::EntityDataManager* entity_manager =
+      client_->GetAutofillClient().GetEntityDataManager();
   if (!entity_manager) {
     return;
   }
@@ -245,7 +246,8 @@ void AutofillAiManager::OnDidFillSuggestion(
   for (const autofill::AutofillField* const field : filled_fields) {
     logger_.OnDidFillField(form, CHECK_DEREF(field), ukm_source_id);
   }
-  autofill::EntityDataManager* entity_manager = client_->GetEntityDataManager();
+  autofill::EntityDataManager* entity_manager =
+      client_->GetAutofillClient().GetEntityDataManager();
   if (!entity_manager) {
     return;
   }
@@ -278,7 +280,8 @@ bool AutofillAiManager::MaybeImportForm(const FormStructure& form) {
     return false;
   }
 
-  autofill::EntityDataManager* entity_manager = client_->GetEntityDataManager();
+  autofill::EntityDataManager* entity_manager =
+      client_->GetAutofillClient().GetEntityDataManager();
   if (!entity_manager) {
     LOG_AF(GetCurrentLogManager())
         << LoggingScope::kAutofillAi << LogMessage::kAutofillAi
@@ -338,7 +341,8 @@ void AutofillAiManager::HandleSavePromptResult(
     return;
   }
 
-  autofill::EntityDataManager* entity_manager = client_->GetEntityDataManager();
+  autofill::EntityDataManager* entity_manager =
+      client_->GetAutofillClient().GetEntityDataManager();
   if (!entity_manager) {
     return;
   }
@@ -357,7 +361,8 @@ void AutofillAiManager::HandleUpdatePromptResult(
     return;
   }
 
-  autofill::EntityDataManager* entity_manager = client_->GetEntityDataManager();
+  autofill::EntityDataManager* entity_manager =
+      client_->GetAutofillClient().GetEntityDataManager();
   if (!entity_manager) {
     return;
   }
@@ -375,7 +380,8 @@ std::vector<autofill::Suggestion> AutofillAiManager::GetSuggestions(
     return {};
   }
 
-  autofill::EntityDataManager* entity_manager = client_->GetEntityDataManager();
+  autofill::EntityDataManager* entity_manager =
+      client_->GetAutofillClient().GetEntityDataManager();
   if (!entity_manager) {
     return {};
   }
