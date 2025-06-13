556bb9770c4cc4faf85024c2a4ad9ea652210f82
Tushar <tushartushar@google.com>
2025-06-12 18:11:42 -0700

    Moving all authentication mock values to util.
    Bug: 412481752
    Change-Id: I5c5805c1cdff234883d9c9a53984ee0f3deab8ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636539
    Commit-Queue: Tushar <tushartushar@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473312}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_egtest.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "components/url_formatter/elide_url.h"
 #import "ios/chrome/browser/autofill/model/features.h"
+#import "ios/chrome/browser/autofill/ui_bundled/authentication/authentication_egtest_util.h"
 #import "ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.h"
 #import "ios/chrome/browser/autofill/ui_bundled/autofill_ui_constants.h"
 #import "ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_matchers.h"
@@ -38,17 +39,10 @@ using chrome_test_util::TextFieldForCellWithLabelId;
 
 namespace {
 
-const char kCreditCardUrl[] = "/credit_card.html";
 const char kCreditCardWithAutofocusUrl[] = "/credit_card_autofocused.html";
-const char kFormCardName[] = "CCName";
 const char kFormCardNumber[] = "CCNo";
 const char kFormCardExpirationMonth[] = "CCExpiresMonth";
 const char kFormCardExpirationYear[] = "CCExpiresYear";
-NSString* const kTriggeringRequestUrl =
-    @"https://payments.google.com/payments/apis-secure/creditcardservice/"
-    @"getrealpan?s7e_suffix=chromewallet";
-NSString* const kSuccessResponseNoAuthNeeded =
-    @"{ \"pan\": \"5411111111112109\" }";
 
 // Matcher for the credit card suggestion chip.
 id<GREYMatcher> KeyboardAccessoryCreditCardSuggestionChip() {
@@ -513,9 +507,10 @@ void CheckAutofillSuggestionAcceptedIndexMetricsCount(
                       chrome_test_util::StaticTextWithAccessibilityLabelId(
                           IDS_AUTOFILL_CARD_UNMASK_PROGRESS_DIALOG_TITLE)];
   // Fake the successful server response that triggers Dismiss.
-  [AutofillAppInterface setPaymentsResponse:kSuccessResponseNoAuthNeeded
-                                 forRequest:kTriggeringRequestUrl
-                              withErrorCode:net::HTTP_OK];
+  [AutofillAppInterface
+      setPaymentsResponse:kUnmaskCardSuccessResponseNoAuthNeeded
+               forRequest:kUnmaskCardRequestUrl
+            withErrorCode:net::HTTP_OK];
   // This delay is the autodismiss delay (1 second) + extra time to avoid
   // flakiness on the simulators (2 seconds).
   const base::TimeDelta total_delay_for_dismiss =
