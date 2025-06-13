59cb80cf31596addb60282733cf630b754b3fa12
Jan Keitel <jkeitel@google.com>
2025-06-13 05:22:09 -0700

    Avoid string copy in TtsControllerDelegate
    No functionality changes - just a tiny clean-up that I came across
    while transitioning some ui code to use std::string_view.
    
    Bug: 423355571
    Change-Id: Ifbad74562de748c0804f2fdf9e9a34ab95f9fae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6641264
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473490}

--- content/public/browser/tts_controller_delegate.cc
+++ content/public/browser/tts_controller_delegate.cc
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/tts_controller_delegate.h"
 
+#include <string>
+#include <utility>
+
 namespace content {
 
-TtsControllerDelegate::PreferredVoiceId::PreferredVoiceId(
-    const std::string& name,
-    const std::string& id)
-    : name(name), id(id) {}
+TtsControllerDelegate::PreferredVoiceId::PreferredVoiceId(std::string name,
+                                                          std::string id)
+    : name(std::move(name)), id(std::move(id)) {}
 
 TtsControllerDelegate::PreferredVoiceId::PreferredVoiceId() = default;
 
