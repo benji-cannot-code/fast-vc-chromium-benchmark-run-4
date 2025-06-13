80f0f9ddc2058d5ec47abd4e6db7425a843943d1
Kent Tamura <tkent@chromium.org>
2025-06-13 03:46:04 -0700

    Switch text_codec_utf16.* to "blink" namespace
    * Renames TextCodecUTF16 to TextCodecUtf16 to comply with Google C++
      coding style.
    
    This CL has no behavior changes.
    
    Bug: 422768753
    Change-Id: Ib09e1f15bd9506f26378362854a915a3b7d566be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636462
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473464}

--- third_party/blink/renderer/platform/wtf/text/text_codec_utf16.h
+++ third_party/blink/renderer/platform/wtf/text/text_codec_utf16.h
@@ -29,14 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/wtf/text/text_codec.h"
 
-namespace WTF {
+namespace blink {
 
-class TextCodecUTF16 final : public TextCodec {
+class TextCodecUtf16 final : public TextCodec {
  public:
   static void RegisterEncodingNames(EncodingNameRegistrar);
   static void RegisterCodecs(TextCodecRegistrar);
 
-  TextCodecUTF16(bool little_endian) : little_endian_(little_endian) {}
+  TextCodecUtf16(bool little_endian) : little_endian_(little_endian) {}
 
   String Decode(base::span<const uint8_t> data,
                 FlushBehavior,
@@ -53,6 +53,6 @@ class TextCodecUTF16 final : public TextCodec {
   UChar lead_surrogate_;
 };
 
-}  // namespace WTF
+}  // namespace blink
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_TEXT_TEXT_CODEC_UTF16_H_
