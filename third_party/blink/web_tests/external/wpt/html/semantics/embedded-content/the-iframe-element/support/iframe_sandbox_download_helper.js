1299cf5ad92dc866aa93a5be2f0d89c158670576
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-06-13 13:24:39 -0700

    Import wpt@02825a00f3df890670c27bfbeb35172a71c1cea1
    https://github.com/web-platform-tests/wpt/compare/13345105f...02825a00f
    
    Using wpt-import in Chromium 78870a08037cd16bc7c117b13e476195d1aabe95.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    vogelheim@chromium.org:
      external/wpt/trusted-types
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ib6ed3a043d35376233868f4ecc0aba2861d08280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642580
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1473758}

--- third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/support/iframe_sandbox_download_helper.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/support/iframe_sandbox_download_helper.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function StreamDownloadFinishDelay() {
-    return 1000;
+    return 2000;
 }
 
 function DownloadVerifyDelay() {
