d42e9fab6ff8fec8af8de11ab7ed289c57610ec6
Etienne Dechamps <edechamps@google.com>
2025-06-13 17:53:02 -0700

    Sprinkle tracing events around ProxyChangeListener
    This code can keep the main thread and the Cronet init thread busy, but
    traces currently won't show anything at all; this CL fixes that.
    
    The impact on trace sizes/performance is negligible because proxy change
    events are very rare.
    
    Change-Id: I2a1ac0d4a5db9eefa65be669d5783e6fec597bb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633532
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473850}

--- net/android/java/src/org/chromium/net/ProxyBroadcastReceiver.java
+++ net/android/java/src/org/chromium/net/ProxyBroadcastReceiver.java
@@ -13,6 +13,7 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
+import org.chromium.base.TraceEvent;
 import org.chromium.build.annotations.NullMarked;
 
 @NullMarked
@@ -26,8 +27,10 @@ final class ProxyBroadcastReceiver extends BroadcastReceiver {
     @Override
     @RequiresApi(Build.VERSION_CODES.M)
     public void onReceive(Context context, final Intent intent) {
-        if (Proxy.PROXY_CHANGE_ACTION.equals(intent.getAction())) {
-            mListener.updateProxyConfigFromConnectivityManager(intent);
+        try (TraceEvent e = TraceEvent.scoped("ProxyBroadcastReceiver#onReceive")) {
+            if (Proxy.PROXY_CHANGE_ACTION.equals(intent.getAction())) {
+                mListener.updateProxyConfigFromConnectivityManager(intent);
+            }
         }
     }
 }
