1e89172bf8281e6ebe8f765a6ae415d9a33f83e6
Emily Stark <estark@google.com>
2025-06-13 23:39:40 -0700

    Configure Trust Anchor IDs on QUIC connections
    This CL hooks up TLS Trust Anchor IDs support for QUIC. To do so, we
    compute the intersection of server-provided TLS Trust Anchor IDs with
    client-trusted Trust Anchor IDs when establishing the connection, and
    we set the result on QuicSSLConfig to be sent in the handshake (when
    the feature flag is enabled).
    
    Bug: 415694849
    Change-Id: I3840b36334c2b8fe44b467270ac9234bc5a3fa41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642009
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473902}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
@@ -1024,6 +1025,7 @@ QuicChromiumClientSession::QuicChromiumClientSession(
       http3_logger_(std::make_unique<QuicHttp3Logger>(net_log_)),
       path_validation_writer_delegate_(this, task_runner_),
       ech_config_list_(metadata.ech_config_list),
+      trust_anchor_ids_(metadata.trust_anchor_ids),
       allow_server_preferred_address_(allow_server_preferred_address),
       session_creation_initiator_(session_creation_initiator) {
   default_network_ = default_network;
@@ -1683,10 +1685,22 @@ void QuicChromiumClientSession::OnCanCreateNewOutgoingStream(
 
 quic::QuicSSLConfig QuicChromiumClientSession::GetSSLConfig() const {
   quic::QuicSSLConfig config = quic::QuicSpdyClientSessionBase::GetSSLConfig();
-  if (ssl_config_service_->GetSSLContextConfig().ech_enabled) {
+  SSLContextConfig ssl_context_config =
+      ssl_config_service_->GetSSLContextConfig();
+  if (ssl_context_config.ech_enabled) {
     config.ech_grease_enabled = true;
     config.ech_config_list.assign(ech_config_list_.begin(),
                                   ech_config_list_.end());
+  }
+  if (base::FeatureList::IsEnabled(features::kTLSTrustAnchorIDs)) {
+    if (!trust_anchor_ids_.empty() &&
+        !ssl_context_config.trust_anchor_ids.empty()) {
+      std::vector<uint8_t> selected_trust_anchor_ids =
+          SSLConfig::SelectTrustAnchorIDs(trust_anchor_ids_,
+                                          ssl_context_config.trust_anchor_ids);
+      config.trust_anchor_ids = std::string(selected_trust_anchor_ids.begin(),
+                                            selected_trust_anchor_ids.end());
+    }
   }
   return config;
 }
