03f2924d2745873e2b90d259c62edb7523404811
Emily Stark <estark@google.com>
2025-06-13 22:37:34 -0700

    Implement retry flow for Trust Anchor IDs
    This CL adds a retry to SSLConnectJob when the client advertises Trust
    Anchor IDs in the handshake but then gets an error. The server can
    provide its up-to-date Trust Anchor IDs in the handshake; if the
    handshake then fails, the client retries with those Trust Anchor IDs
    rather than the ones advertised in DNS.
    
    Bug: 415720498
    Change-Id: Ic75929c01ced3a7707c60dcfbbbdc6509b4305e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627510
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473891}

--- net/socket/ssl_client_socket_impl.h
+++ net/socket/ssl_client_socket_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/containers/lru_cache.h"
+#include "base/containers/span_reader.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -49,8 +50,9 @@ class SSLPrivateKey;
 class SSLKeyLogger;
 class X509Certificate;
 
-class SSLClientSocketImpl : public SSLClientSocket,
-                            public SocketBIOAdapter::Delegate {
+class NET_EXPORT_PRIVATE SSLClientSocketImpl
+    : public SSLClientSocket,
+      public SocketBIOAdapter::Delegate {
  public:
   // Takes ownership of |stream_socket|, which may already be connected.
   // The given hostname will be compared with the name(s) in the server's
@@ -74,6 +76,7 @@ class SSLClientSocketImpl : public SSLClientSocket,
 
   // SSLClientSocket implementation.
   std::vector<uint8_t> GetECHRetryConfigs() override;
+  std::vector<std::vector<uint8_t>> GetServerTrustAnchorIDsForRetry() override;
 
   // SSLSocket implementation.
   int ExportKeyingMaterial(std::string_view label,
@@ -123,6 +126,7 @@ class SSLClientSocketImpl : public SSLClientSocket,
   class SSLContext;
   friend class SSLClientSocket;
   friend class SSLContext;
+  FRIEND_TEST_ALL_PREFIXES(SSLClientSocketTest, ParseServerTrustAnchorIDs);
 
   int Init();
   void DoReadCallback(int result);
@@ -144,6 +148,9 @@ class SSLClientSocketImpl : public SSLClientSocket,
   // pending Connect, Read or Write calls, if any. Retries all state machines
   // and, if complete, runs the respective callbacks.
   void RetryAllOperations();
+
+  static std::vector<std::vector<uint8_t>> ParseServerTrustAnchorIDs(
+      base::SpanReader<const uint8_t>* reader);
 
   // Callback from the SSL layer when a certificate needs to be verified. This
   // is called when establishing new (fresh) connections and when evaluating
