d16266a1f0485871ce227e3a31b08be72b42b0fe
Kartar Singh <kartarsingh@google.com>
2025-06-16 13:58:27 -0700

    [InputVizard] Cancel InputReceiver creation task if frame sink destroyed
    While processing CreateRootCompositorFrameSink call a task is posted to
    create input receiver. There's a bug in the current implementation where
    we still try to run the task even if a DestroyCompositorFrameSink call
    comes in for root sink. In this change, we are now cancelling the task
    in these cases avoiding.
    
    Bug: 424169457
    Change-Id: I98a053c0e9aa1598762248084b81633f7edc75f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6640995
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474560}

--- components/viz/service/input/input_manager.h
+++ components/viz/service/input/input_manager.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/android_info.h"
+#include "base/cancelable_callback.h"
 #include "components/input/android/input_receiver_data.h"
 #include "components/viz/service/input/android_state_transfer_handler.h"
 #include "components/viz/service/input/render_input_router_support_android.h"
@@ -199,6 +200,11 @@ class VIZ_SERVICE_EXPORT InputManager
   AndroidStateTransferHandler android_state_transfer_handler_;
 
   std::unique_ptr<input::InputReceiverData> receiver_data_;
+
+  // Allow cancelling the creation task, since it's possible for
+  // DestroyCompositorFrameSink call to come before the callback is ran.
+  base::flat_map<FrameSinkId, std::unique_ptr<base::CancelableOnceClosure>>
+      pending_create_input_receiver_callback_;
 #endif  // BUILDFLAG(IS_ANDROID)
 
   friend class MockInputManager;
