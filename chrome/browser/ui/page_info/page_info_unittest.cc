1cafd2c57d0a3e0c1d1085a501af4ed4ed05146a
Aisulu R <aisulu@google.com>
2025-06-16 14:30:28 -0700

    [T&S Sentiment] Make PageInfo related tests use mock sentiment service
    This CL adds mock TrustSafetySentimentService to tests that instantiate
    ChromePageInfoDelegate that in turn create a real sentiment service.
    
    This is to avoid test crashes resulted by creation of real sentiment
    services once TrustSafetySentimentSurveyV2 flag is enabled by default.
    
    Bug: 425350548
    Change-Id: I019ce2a2638222bf5faf9508632b45878b6a37a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6645699
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474588}

--- chrome/browser/ui/page_info/page_info_unittest.cc
+++ chrome/browser/ui/page_info/page_info_unittest.cc
@@ -74,6 +74,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/android/android_theme_resources.h"
 #else
+#include "chrome/browser/ui/hats/mock_trust_safety_sentiment_service.h"
+#include "chrome/browser/ui/hats/trust_safety_sentiment_service_factory.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -191,9 +193,19 @@ class PageInfoTest : public ChromeRenderViewHostTestHarness {
   }
 
   TestingProfile::TestingFactories GetTestingFactories() const override {
-    return {TestingProfile::TestingFactory{
-        StatefulSSLHostStateDelegateFactory::GetInstance(),
-        StatefulSSLHostStateDelegateFactory::GetDefaultFactoryForTesting()}};
+    return {
+        TestingProfile::TestingFactory{
+            StatefulSSLHostStateDelegateFactory::GetInstance(),
+            StatefulSSLHostStateDelegateFactory::GetDefaultFactoryForTesting()},
+#if !BUILDFLAG(IS_ANDROID)
+        // Setup mock sentiment service for ChromePageInfoDelegate.
+        TestingProfile::TestingFactory {
+          TrustSafetySentimentServiceFactory::GetInstance(),
+          base::BindRepeating(&BuildMockTrustSafetySentimentService)
+        }
+#endif  // !BUILDFLAG(IS_ANDROID)
+
+    };
   }
 
   void SetDefaultUIExpectations(MockPageInfoUI* mock_ui) {
