9c5e37c0bdd6c24b8f2440c3567f2666e588fdae
Hubert Chao <hchao@chromium.org>
2025-06-16 12:11:11 -0700

    [LNA] fix test for local network access permission
    Fix settings test in preparation for changing
    LocalNetworkAccessChecksWarn from true to false in
    fieldtrial_testing_config.json
    
    Bug: 395895368
    Change-Id: I10e2b2d82ebf8b13f9284a4425939c78c16cff7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6646932
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474499}

--- chrome/test/data/webui/settings/site_details_test.ts
+++ chrome/test/data/webui/settings/site_details_test.ts
@@ -54,6 +54,9 @@ suite('SiteDetails', function() {
   let websiteUsageProxy: TestWebsiteUsageBrowserProxy;
 
   // Initialize a site-details before each test.
+  //
+  // It may also be required to add the ContentSettingsType in the constructor
+  // in test_site_settings_prefs_browser_proxy.ts.
   setup(function() {
     loadTimeData.overrideValues({
       enableWebPrintingContentSetting: true,
@@ -191,6 +194,9 @@ suite('SiteDetails', function() {
               [createRawSiteException('https://foo.com:443')]),
           createContentSettingTypeToValuePair(
               ContentSettingsTypes.POINTER_LOCK,
+              [createRawSiteException('https://foo.com:443')]),
+          createContentSettingTypeToValuePair(
+              ContentSettingsTypes.LOCAL_NETWORK_ACCESS,
               [createRawSiteException('https://foo.com:443')]),
         ],
         [
