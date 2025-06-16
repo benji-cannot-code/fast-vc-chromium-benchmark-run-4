83f7478e9fc3a0c6b516bdb51ab4d07fdc12d70f
Justin Lulejian <jlulejian@chromium.org>
2025-06-16 15:04:32 -0700

    [Extensions] Fix multi-listener regression cause by promise return work.
    Before this change OneTimeMessageHandler's
    CheckAndHandleAsyncListenerReply() method would return for all boolean
    values a listener returned. This means that if a synchronous listener
    returned false before an asynchronous listener returned true we would
    proceed to close the port and (in the browser) the channel since it
    appeared the extension didn't indicate it would call sendResponse()
    asynchronously.
    
    This is a regression from previous behavior where we only check if the
    listener returned `true` in the JS.
    
    After this change we correctly ignore the listener return if it is false
    and indicate async reply if it is `true`. This also adds test cases to
    prevent this from regressing in the future.
    
    Bug: 424560420
    Change-Id: I107689b59dd7caa52d9edcc7e8b24380ac33cbc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6643546
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474601}

---
+++ extensions/test/data/api_test/messaging/on_message_multi_listener/async_listener_called_first/background.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// NOTE: the ordering is important here as first listener registered must be
+// asynchronous to test the opposite scenario from the bug.
+
+async function asyncCallSendResponse(sendResponse) {
+  setTimeout(() => {
+    sendResponse('Async response from background script');
+  }, 1);
+}
+
+function onMessageAsyncListener(message, sender, sendResponse) {
+  asyncCallSendResponse(sendResponse);
+  return true;
+}
+
+chrome.runtime.onMessage.addListener(onMessageAsyncListener);
+
+function onMessageSyncListener(message, sender, sendResponse) {
+  return false;
+}
+
+chrome.runtime.onMessage.addListener(onMessageSyncListener);
---
+++ extensions/test/data/api_test/messaging/on_message_multi_listener/async_listener_called_first/content_script.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+
+  // Tests that an async listener registering first will still provide it's
+  // response to the message sender.
+  async function sendMessageWithAsyncListenerCalledFirst() {
+    const response = await chrome.runtime.sendMessage('');
+    chrome.test.assertEq('Async response from background script', response);
+    chrome.test.succeed();
+  },
+
+]);
