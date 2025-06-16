8afe9486b1f4eb671b53c3c6e2c8eccb97e6b571
Nidhi Jaju <nidhijaju@chromium.org>
2025-06-15 21:01:53 -0700

    Revert "[gpu] Add raster usage dump will be checks in CSI::BeginRasterAccess"
    This reverts commit 3ac5eded4089acec6287530c9787ac702908d149.
    
    Reason for revert: Causing consistent TSan failures on Linux
    i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20TSan%20Tests/95891/test-results
    
    Bug: 420632430
    Original change's description:
    > [gpu] Add raster usage dump will be checks in CSI::BeginRasterAccess
    >
    > Enforce raster usage over ClientSharedImage::BeginRasterAccess.
    >
    > Also update client callsites for android, webgpu, paint canvas and
    > unittests that were not passing the needed read usage.
    >
    > Bug: 420632430
    > Change-Id: I8430634611fd37b812a5491b200b673f4f3732d9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633794
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1473749}
    
    Bug: 420632430
    Change-Id: Ie2c10a95d97f3b46b17802bf728a4b57858aba59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642841
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474145}

--- media/renderers/video_resource_updater_unittest.cc
+++ media/renderers/video_resource_updater_unittest.cc
@@ -249,20 +249,12 @@ class VideoResourceUpdaterTest : public testing::Test {
   scoped_refptr<VideoFrame> CreateTestHardwareVideoFrame(
       viz::SharedImageFormat si_format,
       VideoPixelFormat format,
-      unsigned target,
-      bool needs_raster_access) {
+      unsigned target) {
     const int kDimension = 10;
     gfx::Size size(kDimension, kDimension);
 
-    gpu::SharedImageMetadata metadata;
-    metadata.format = si_format;
-    metadata.color_space = gfx::ColorSpace::CreateSRGB();
-    metadata.surface_origin = kTopLeft_GrSurfaceOrigin;
-    metadata.alpha_type = kOpaque_SkAlphaType;
-    metadata.usage = needs_raster_access ? gpu::SHARED_IMAGE_USAGE_RASTER_READ
-                                         : gpu::SharedImageUsageSet();
     scoped_refptr<gpu::ClientSharedImage> shared_image =
-        gpu::ClientSharedImage::CreateForTesting(metadata, target);
+        gpu::ClientSharedImage::CreateForTesting(si_format, target);
     scoped_refptr<VideoFrame> video_frame = VideoFrame::WrapSharedImage(
         format, shared_image, kMailboxSyncToken,
         base::BindOnce(&VideoResourceUpdaterTest::SetReleaseSyncToken,
@@ -277,15 +269,14 @@ class VideoResourceUpdaterTest : public testing::Test {
 
   scoped_refptr<VideoFrame> CreateTestRGBAHardwareVideoFrame() {
     return CreateTestHardwareVideoFrame(viz::SinglePlaneFormat::kRGBA_8888,
-                                        PIXEL_FORMAT_ARGB, GL_TEXTURE_2D,
-                                        /*needs_raster_access=*/false);
+                                        PIXEL_FORMAT_ARGB, GL_TEXTURE_2D);
   }
 
   scoped_refptr<VideoFrame> CreateTestStreamTextureHardwareVideoFrame(
       bool needs_copy) {
     scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
         viz::SinglePlaneFormat::kRGBA_8888, PIXEL_FORMAT_ARGB,
-        GL_TEXTURE_EXTERNAL_OES, /*needs_raster_access=*/needs_copy);
+        GL_TEXTURE_EXTERNAL_OES);
     video_frame->metadata().copy_required = needs_copy;
     return video_frame;
   }
@@ -294,7 +285,7 @@ class VideoResourceUpdaterTest : public testing::Test {
   scoped_refptr<VideoFrame> CreateTestDCompSurfaceVideoFrame() {
     scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
         viz::SinglePlaneFormat::kRGBA_8888, PIXEL_FORMAT_ARGB,
-        GL_TEXTURE_EXTERNAL_OES, /*needs_raster_access=*/false);
+        GL_TEXTURE_EXTERNAL_OES);
     video_frame->metadata().dcomp_surface = true;
     return video_frame;
   }
@@ -729,9 +720,9 @@ TEST_F(VideoResourceUpdaterTest, ChangeResourceizeSoftwareCompositor) {
 
 TEST_F(VideoResourceUpdaterTest, CreateForHardwarePlanes_SharedImageFormat) {
   std::unique_ptr<VideoResourceUpdater> updater = CreateUpdaterForHardware();
-  scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
-      viz::MultiPlaneFormat::kI420, PIXEL_FORMAT_I420, GL_TEXTURE_RECTANGLE_ARB,
-      /*needs_raster_access=*/false);
+  scoped_refptr<VideoFrame> video_frame =
+      CreateTestHardwareVideoFrame(viz::MultiPlaneFormat::kI420,
+                                   PIXEL_FORMAT_I420, GL_TEXTURE_RECTANGLE_ARB);
   VideoFrameExternalResource resource =
       updater->CreateExternalResourceFromVideoFrame(video_frame);
   EXPECT_EQ(VideoFrameResourceType::RGB, resource.type);
@@ -740,9 +731,9 @@ TEST_F(VideoResourceUpdaterTest, CreateForHardwarePlanes_SharedImageFormat) {
   EXPECT_EQ(resource.resource.synchronization_type,
             viz::TransferableResource::SynchronizationType::kSyncToken);
 
-  video_frame = CreateTestHardwareVideoFrame(
-      viz::MultiPlaneFormat::kI420, PIXEL_FORMAT_I420, GL_TEXTURE_RECTANGLE_ARB,
-      /*needs_raster_access=*/false);
+  video_frame =
+      CreateTestHardwareVideoFrame(viz::MultiPlaneFormat::kI420,
+                                   PIXEL_FORMAT_I420, GL_TEXTURE_RECTANGLE_ARB);
   video_frame->metadata().read_lock_fences_enabled = true;
 
   resource = updater->CreateExternalResourceFromVideoFrame(video_frame);
@@ -925,8 +916,7 @@ TEST_F(VideoResourceUpdaterTest, CreateForHardwarePlanes_SingleNV12) {
   std::unique_ptr<VideoResourceUpdater> updater = CreateUpdaterForHardware();
   EXPECT_EQ(0u, GetSharedImageCount());
   scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
-      viz::MultiPlaneFormat::kNV12, PIXEL_FORMAT_NV12, GL_TEXTURE_EXTERNAL_OES,
-      /*needs_raster_access=*/false);
+      viz::MultiPlaneFormat::kNV12, PIXEL_FORMAT_NV12, GL_TEXTURE_EXTERNAL_OES);
   VideoFrameExternalResource resource =
       updater->CreateExternalResourceFromVideoFrame(video_frame);
   EXPECT_EQ(VideoFrameResourceType::RGB, resource.type);
@@ -940,9 +930,9 @@ TEST_F(VideoResourceUpdaterTest,
        CreateForHardwarePlanes_DualNV12_SharedImageFormat) {
   std::unique_ptr<VideoResourceUpdater> updater = CreateUpdaterForHardware();
   EXPECT_EQ(0u, GetSharedImageCount());
-  scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
-      viz::MultiPlaneFormat::kNV12, PIXEL_FORMAT_NV12, GL_TEXTURE_RECTANGLE_ARB,
-      /*needs_raster_access=*/false);
+  scoped_refptr<VideoFrame> video_frame =
+      CreateTestHardwareVideoFrame(viz::MultiPlaneFormat::kNV12,
+                                   PIXEL_FORMAT_NV12, GL_TEXTURE_RECTANGLE_ARB);
   VideoFrameExternalResource resource =
       updater->CreateExternalResourceFromVideoFrame(video_frame);
   // Setting to kSharedImageFormat, resource type should bo RGB.
@@ -953,8 +943,7 @@ TEST_F(VideoResourceUpdaterTest,
   EXPECT_EQ(0u, GetSharedImageCount());
 
   video_frame = CreateTestHardwareVideoFrame(
-      viz::MultiPlaneFormat::kNV12, PIXEL_FORMAT_NV12, GL_TEXTURE_EXTERNAL_OES,
-      /*needs_raster_access=*/false);
+      viz::MultiPlaneFormat::kNV12, PIXEL_FORMAT_NV12, GL_TEXTURE_EXTERNAL_OES);
 
   resource = updater->CreateExternalResourceFromVideoFrame(video_frame);
   EXPECT_EQ(VideoFrameResourceType::RGB, resource.type);
@@ -977,7 +966,7 @@ TEST_F(VideoResourceUpdaterTest, CreateForHardwarePlanes_SingleP010HDR) {
   EXPECT_EQ(0u, GetSharedImageCount());
   scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
       viz::MultiPlaneFormat::kP010, PIXEL_FORMAT_P010LE,
-      GL_TEXTURE_EXTERNAL_OES, /*needs_raster_access=*/false);
+      GL_TEXTURE_EXTERNAL_OES);
   video_frame->set_color_space(kHDR10ColorSpace);
   video_frame->set_hdr_metadata(hdr_metadata);
 
