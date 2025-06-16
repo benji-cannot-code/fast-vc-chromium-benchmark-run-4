98a2d8df6994bfa189258a8fe90639aa2b23c0e0
Nick Birnie <birnie@google.com>
2025-06-16 15:20:01 -0700

    Cleanup SyntheticTrialManager Conflict Handling
    Makes changes requested in https://buganizer.corp.google.com/issues/414697584#comment10 bullet point 3.2
    
    Bug: 414455578
    Change-Id: If7226839ddaa9e09b6d22be61b68632d7c14a24f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6643352
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474613}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/chrome_features.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/metrics/metrics_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "content/public/browser/web_contents.h"
