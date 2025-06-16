eda4b95e8e5c0e0d9b35c706373b48b8f6221551
Jan Keitel <jkeitel@google.com>
2025-06-16 03:37:10 -0700

    Forward declare more classes in payments_autofill_table.h
    The include of autofill_specifics.pb.h is the 8th most expensive
    include in all of //components in terms of build time. This CL uses a
    forward declaration instead.
    
    Bug: 40100455
    Change-Id: If2195a42334d0dbec2b03b96030bbd2f65cf2053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6643105
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474240}

--- components/autofill/core/browser/webdata/payments/payments_autofill_table.h
+++ components/autofill/core/browser/webdata/payments/payments_autofill_table.h
@@ -14,11 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "components/autofill/core/browser/data_model/payments/credit_card_benefit.h"
-#include "components/sync/base/data_type.h"
-#include "components/sync/protocol/autofill_specifics.pb.h"
 #include "components/webdata/common/web_database_table.h"
 
 class WebDatabase;
+
+namespace sync_pb {
+class PaymentInstrument;
+class PaymentInstrumentCreationOption;
+}  // namespace sync_pb
 
 namespace autofill {
 
