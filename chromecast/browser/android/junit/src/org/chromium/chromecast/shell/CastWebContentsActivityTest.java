c430f62f19f6843246be8a93813ed6eceaa2725c
Peter Wen <wnwen@chromium.org>
2025-06-16 12:10:01 -0700

    android: Remove O-specific cast tests
    This fixes cast CQ bots when the minsdk is raised.
    
    R=hnakashima@chromium.org
    
    Bug: 422521169
    Change-Id: Id69c73538fb93ebb5745a04f3384d4e8e34fb721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6648009
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474497}

--- chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsActivityTest.java
+++ chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsActivityTest.java
@@ -32,7 +32,6 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.PatternMatcher;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
@@ -283,23 +282,6 @@ public class CastWebContentsActivityTest {
     }
 
     @Test
-    @Config(sdk = {Build.VERSION_CODES.O})
-    public void testTurnsScreenOnIfTurnOnScreen_AndroidO() {
-        mActivityLifecycle =
-                Robolectric.buildActivity(
-                        CastWebContentsActivity.class,
-                        CastWebContentsIntentUtils.requestStartCastActivity(
-                                mWebContents, true, false, true, false, "0"));
-        mActivity = mActivityLifecycle.get();
-        mActivity.testingModeForTesting();
-        mActivityLifecycle.create();
-
-        Assert.assertTrue(
-                Shadows.shadowOf(mActivity.getWindow())
-                        .getFlag(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON));
-    }
-
-    @Test
     @Config(shadows = {ExtendedShadowActivity.class})
     public void testDoesNotTurnScreenOnIfNotTurnOnScreen() {
         mActivityLifecycle =
@@ -314,23 +296,6 @@ public class CastWebContentsActivityTest {
 
         Assert.assertFalse(shadowActivity.getTurnScreenOn());
         Assert.assertFalse(shadowActivity.getShowWhenLocked());
-    }
-
-    @Test
-    @Config(sdk = {Build.VERSION_CODES.O})
-    public void testDoesNotTurnScreenOnIfNotTurnOnScreen_AndroidO() {
-        mActivityLifecycle =
-                Robolectric.buildActivity(
-                        CastWebContentsActivity.class,
-                        CastWebContentsIntentUtils.requestStartCastActivity(
-                                mWebContents, true, false, true, false, "0"));
-        mActivity = mActivityLifecycle.get();
-        mActivity.testingModeForTesting();
-        mActivityLifecycle.create();
-
-        Assert.assertTrue(
-                Shadows.shadowOf(mActivity.getWindow())
-                        .getFlag(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON));
     }
 
     @Test
@@ -465,9 +430,7 @@ public class CastWebContentsActivityTest {
     }
 
     @Test
-    @Config(
-            shadows = {ExtendedShadowActivity.class},
-            sdk = {Build.VERSION_CODES.O})
+    @Config(shadows = {ExtendedShadowActivity.class})
     public void testStopWhileNotInPipModeDoesNotCloseActivity() {
         mShadowActivityManager.setLockTaskModeState(ActivityManager.LOCK_TASK_MODE_NONE);
         mActivityLifecycle.create().start().resume();
@@ -481,9 +444,7 @@ public class CastWebContentsActivityTest {
     }
 
     @Test
-    @Config(
-            shadows = {ExtendedShadowActivity.class},
-            sdk = {Build.VERSION_CODES.O})
+    @Config(shadows = {ExtendedShadowActivity.class})
     public void testStopWhileInPipModeDoesNotClosesActivity() {
         mShadowActivityManager.setLockTaskModeState(ActivityManager.LOCK_TASK_MODE_NONE);
         mActivityLifecycle.create().start().resume();
