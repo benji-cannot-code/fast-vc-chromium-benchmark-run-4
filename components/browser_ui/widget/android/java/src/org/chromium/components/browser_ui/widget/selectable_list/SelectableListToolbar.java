277288ceef44f68c546118298fe8d7dea49dbe70
Patrick Noland <pnoland@google.com>
2025-06-16 14:40:42 -0700

    [a11y] Request a11y focus for selectable list search text on clear
    Focus should return to the edit text for proper context/logical flow;
    using the clear button conveys an intent to continue editing the search
    text.
    
    Bug: 394398255
    Change-Id: Ic3efc3bd4dcdd8cf8e929206e476737aa4c28c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6649584
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474591}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/selectable_list/SelectableListToolbar.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/selectable_list/SelectableListToolbar.java
@@ -20,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.view.accessibility.AccessibilityEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
@@ -329,7 +330,11 @@ public class SelectableListToolbar<E> extends Toolbar
                 });
 
         mClearTextButton = findViewById(R.id.clear_text_button);
-        mClearTextButton.setOnClickListener(v -> mSearchEditText.setText(""));
+        mClearTextButton.setOnClickListener(
+                v -> {
+                    mSearchEditText.setText("");
+                    mSearchEditText.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
+                });
     }
 
     @Override
