5211e615dd4d053f8a4f7c3ebeccc22f12de7a9a
Maks Orlovich <morlovich@chromium.org>
2025-06-16 14:10:42 -0700

    Spanify some tests in net/proxy_resolution
    Bug: 40285824
    Change-Id: Iab2b3a3c8fd98a79403a1d65514ae3d675df5254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6643849
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474568}

--- net/proxy_resolution/win/proxy_config_service_win_unittest.cc
+++ net/proxy_resolution/win/proxy_config_service_win_unittest.cc
@@ -3,12 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "net/proxy_resolution/win/proxy_config_service_win.h"
+
+#include <array>
 
 #include "net/base/net_errors.h"
 #include "net/proxy_resolution/proxy_config.h"
@@ -25,7 +22,7 @@ TEST(ProxyConfigServiceWinTest, SetFromIEConfig) {
     const wchar_t* proxy;
     const wchar_t* proxy_bypass;
   };
-  const struct {
+  struct Test {
     // Input.
     IEProxyConfig ie_config;
 
@@ -34,7 +31,9 @@ TEST(ProxyConfigServiceWinTest, SetFromIEConfig) {
     GURL pac_url;
     ProxyRulesExpectation proxy_rules;
     const char* proxy_bypass_list;  // newline separated
-  } tests[] = {
+  };
+
+  auto tests = std::to_array<Test>({
       // Auto detect.
       {
           {
@@ -209,7 +208,7 @@ TEST(ProxyConfigServiceWinTest, SetFromIEConfig) {
           GURL(),  // pac_url
           ProxyRulesExpectation::EmptyWithBypass("foo.com,google.com"),
       },
-  };
+  });
 
   for (size_t i = 0; i < std::size(tests); ++i) {
     WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ie_config = {
