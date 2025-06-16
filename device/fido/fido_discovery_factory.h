f90ea10a2b15e2c0a84d85053903f4237c248e7c
David Sanders <dsanders11@ucsbalum.com>
2025-06-16 09:37:18 -0700

    Remove unused include in //device/fido/fido_discovery_factory.h
    Bug: 40318405
    Change-Id: I8f14dcff77122c93398aed50ae16e49c9326f414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642859
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474388}

--- device/fido/fido_discovery_factory.h
+++ device/fido/fido_discovery_factory.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
-#include "components/sync/protocol/webauthn_credential_specifics.pb.h"
 #include "device/fido/cable/cable_discovery_data.h"
 #include "device/fido/cable/v2_constants.h"
 #include "device/fido/ctap_get_assertion_request.h"
