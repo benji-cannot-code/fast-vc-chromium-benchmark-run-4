33ad9f353f461828dd460dbc1ce2b82a94a303a8
Mei Liang <meiliang@chromium.org>
2025-06-16 11:27:08 -0700

    Add @NullMarked to a set of chrome_java files
    Bug: 389129271
    Change-Id: Ic92a5a1b599c16b75fd52f2a0361ddf9337c0ceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6637865
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474467}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateReportAbuseDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateReportAbuseDialog.java
@@ -46,7 +46,7 @@ public class WebApkUpdateReportAbuseDialog implements ModalDialogProperties.Cont
     private final ModalDialogManager mModalDialogManager;
 
     // The short name of the app the user is uninstalling.
-    private final String mAppShortName;
+    private final @Nullable String mAppShortName;
 
     // The package name for the app the user is uninstalling.
     private final String mAppPackageName;
@@ -64,7 +64,7 @@ public class WebApkUpdateReportAbuseDialog implements ModalDialogProperties.Cont
             Context activityContext,
             ModalDialogManager manager,
             String appPackageName,
-            String appShortName,
+            @Nullable String appShortName,
             boolean showAbuseCheckbox,
             Callback callback) {
         mActivityContext = activityContext;
