677a73a4e8855e9c7dd4ac6f118a3844d934d8ec
Fumitoshi Ukai <ukai@google.com>
2025-06-15 20:39:15 -0700

    rpm/merge_package_deps.py use action_helpers.actomic_output
    restat: changed but not modified "out/siso/rpm_common.deps" ea09ea7823b20ee5a0de06c9cfa54f2d09dbcf2590571ad9c1d49744bb610ec7/2426 2025-06-12 16:45:21.526890846 +0900 JST->2025-06-13 11:25:11.955944568 +0900 JST
    
    ukai@ukai ...src/chromium/src % siso query rule -C out/siso rpm_common.deps
    ninja_required_version = 1.7.2
    
    pool build_toolchain_action_pool
      depth = 128
    
    rule __chrome_installer_linux_merge_rpm_dependencies___build_toolchain_linux_clang_x64__rule
      command = python3 ../../chrome/installer/linux/rpm/merge_package_deps.py rpm_common.deps ../../chrome/installer/linux/rpm/additional_deps rpm_chrome.deps rpm_chrome_crashpad_handler.deps rpm_chrome_management_service.deps rpm_chrome_sandbox.deps rpm_libEGL.deps rpm_libGLESv2.deps rpm_libvulkan.so.deps rpm_libvk_swiftshader.deps
      description = ACTION //chrome/installer/linux:merge_rpm_dependencies(//build/toolchain/linux:clang_x64)
      restat = 1
      pool = build_toolchain_action_pool
    
    build rpm_common.deps $
     : __chrome_installer_linux_merge_rpm_dependencies___build_toolchain_linux_clang_x64__rule $
      | $
        ../../chrome/installer/linux/rpm/merge_package_deps.py $
        ../../chrome/installer/linux/rpm/additional_deps $
        ../../build/action_helpers.py $
        phony/chrome/installer/linux/calculate_rpm_dependencies
    
    Bug: 424653546
    Change-Id: Iacba2ccb25393674eeaa72cefe31e317614efc29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636332
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474142}

--- chrome/installer/linux/rpm/merge_package_deps.py
+++ chrome/installer/linux/rpm/merge_package_deps.py
@@ -4,7 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             os.path.pardir, os.path.pardir, os.path.pardir,
+                             os.path.pardir, 'build'))
+import action_helpers
 
 if len(sys.argv) < 3:
   print ('Usage: %s output_deps_file input1_deps_file input2_deps_file ...' %
@@ -25,5 +31,5 @@ for input_filename in input_filenames:
       continue
     requires.add(line)
 
-with open(output_filename, 'w') as output_file:
+with action_helpers.atomic_output(output_filename, mode='w') as output_file:
   output_file.write(''.join(sorted(list(requires))))
