1d3af9765af59ee9cd1869a0ae1d4bf3d5cf03c6
David Sanders <dsanders11@ucsbalum.com>
2025-06-16 13:01:02 -0700

    Remove unused include in optimization_guide_features.h
    Adds a missing include to fix the build.
    
    Bug: 40318405
    Change-Id: I57bd10e13943a62144d33c53af4fba22ca750db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6645268
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1474534}

--- chrome/renderer/accessibility/phrase_segmentation/dependency_parser_model.cc
+++ chrome/renderer/accessibility/phrase_segmentation/dependency_parser_model.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/accessibility/phrase_segmentation/dependency_parser_model.h"
 
+#include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/histogram_macros_local.h"
