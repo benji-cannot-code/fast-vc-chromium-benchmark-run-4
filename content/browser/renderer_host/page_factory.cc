13e1b72c7299f7246e37bf1f521f34935f654d79
David Sanders <dsanders11@ucsbalum.com>
2025-06-16 14:01:54 -0700

    Do IWYU for page_factory.cc
    Bug: 40318405
    Change-Id: I682a070f2a344905e1e607f0de3d4163370106ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635902
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1474561}

--- content/browser/renderer_host/page_factory.cc
+++ content/browser/renderer_host/page_factory.cc
@@ -7,10 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "content/browser/renderer_host/render_view_host_impl.h"
-#include "content/browser/renderer_host/render_widget_host_factory.h"
-#include "content/browser/site_instance_group.h"
-#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "base/check.h"
+#include "content/browser/renderer_host/page_impl.h"
 
 namespace content {
 
