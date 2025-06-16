d1c4d986f84985e55a019f1167336cc00f21af6b
Xinghui Lu <xinghuilu@chromium.org>
2025-06-16 13:04:15 -0700

    [CLAMS] Log OnDeviceModelEligibilityReason within IsOnDeviceModelAvailable.
    This change will simplify the new IntelligentScanDelegate interface.
    
    This CL is pure refactoring. No behavioral change.
    
    Bug: 424104358
    Change-Id: Iacd7a898491238abcf5fca314e884ad43f7cd0e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6639511
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474536}

--- chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
@@ -178,7 +178,6 @@ class MockClientSideDetectionService : public ClientSideDetectionService {
       (std::string,
        base::OnceCallback<void(
            std::optional<optimization_guide::proto::ScamDetectionResponse>)>));
-  MOCK_METHOD0(LogOnDeviceModelEligibilityReason, void());
 };
 
 class MockSafeBrowsingUIManager : public SafeBrowsingUIManager {
@@ -2744,7 +2743,6 @@ TEST_F(ClientSideDetectionHostScamDetectionTest,
   SetFeatures({kClientSideDetectionBrandAndIntentForScamDetection}, {});
   raw_delegate_->ForceEmptyInnerText();
   // Because the inner text is empty, we will NOT inquire the on-device model.
-  EXPECT_CALL(*csd_service_, LogOnDeviceModelEligibilityReason()).Times(0);
   EXPECT_CALL(*csd_service_, InquireOnDeviceModel(_, _)).Times(0);
   SetSendClientReportPhishingRequestCallback(
       /*has_expected_brand_and_intent=*/false,
@@ -2777,7 +2775,6 @@ TEST_F(ClientSideDetectionHostScamDetectionTest,
   SetFeatures({kClientSideDetectionBrandAndIntentForScamDetection}, {});
   // Because the URL is on the HC allowlist, we will NOT inquire the
   // on-device model.
-  EXPECT_CALL(*csd_service_, LogOnDeviceModelEligibilityReason()).Times(0);
   EXPECT_CALL(*csd_service_, InquireOnDeviceModel(_, _)).Times(0);
   SetSendClientReportPhishingRequestCallback(
       /*has_expected_brand_and_intent=*/false,
@@ -2813,7 +2810,6 @@ TEST_F(ClientSideDetectionHostScamDetectionTest,
   csd_service_->SetOnDeviceAvailabilityForTesting(false);
   // Because the on-device model is unavailable, we will NOT inquire the
   // on-device model.
-  EXPECT_CALL(*csd_service_, LogOnDeviceModelEligibilityReason()).Times(1);
   EXPECT_CALL(*csd_service_, InquireOnDeviceModel(_, _)).Times(0);
   SetSendClientReportPhishingRequestCallback(
       /*has_expected_brand_and_intent=*/false,
@@ -2848,7 +2844,6 @@ TEST_F(ClientSideDetectionHostScamDetectionTest,
 
   // Because the client side detection type is POINTER_LOCK_REQUESTED, we will
   // NOT inquire the on-device model.
-  EXPECT_CALL(*csd_service_, LogOnDeviceModelEligibilityReason()).Times(0);
   EXPECT_CALL(*csd_service_, InquireOnDeviceModel(_, _)).Times(0);
   SetSendClientReportPhishingRequestCallback(
       /*has_expected_brand_and_intent=*/false,
--- components/safe_browsing/content/browser/client_side_detection_host.cc
+++ components/safe_browsing/content/browser/client_side_detection_host.cc
@@ -1309,7 +1309,8 @@ void ClientSideDetectionHost::MaybeInquireOnDeviceForScamDetection(
       return;
     }
 
-    bool on_device_model_available = csd_service_->IsOnDeviceModelAvailable();
+    bool on_device_model_available = csd_service_->IsOnDeviceModelAvailable(
+        /*log_failed_eligibility_reason=*/true);
 
     base::UmaHistogramBoolean(
         "SBClientPhishing.IsOnDeviceModelAvailableAtInquiryTime",
@@ -1320,10 +1321,6 @@ void ClientSideDetectionHost::MaybeInquireOnDeviceForScamDetection(
         on_device_model_available);
 
     if (!on_device_model_available) {
-      // When the model is not available at the time of inquiry, we want to log
-      // the current status of the model fetch.
-      csd_service_->LogOnDeviceModelEligibilityReason();
-
       IntelligentScanInfo intelligent_scan_info;
       intelligent_scan_info.set_no_info_reason(
           IntelligentScanInfo::ON_DEVICE_MODEL_UNAVAILABLE);
