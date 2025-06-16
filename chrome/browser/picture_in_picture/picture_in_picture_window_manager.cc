761679c5ecea94fde440bb6ab78a74779ef48341
Dominik Bylica <bylica@google.com>
2025-06-16 03:06:22 -0700

    [IWA] Enable picture-in-picture API in IWAs.
    Aka stop navigation capturing from getting in the way.
    
    Bug: 424412274
    Change-Id: I983ecbb9b33c18dcab1c56f7c66549ff48d9e4c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633982
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Dominik Bylica <bylica@google.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Auto-Submit: Dominik Bylica <bylica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474233}

--- chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/picture_in_picture/picture_in_picture_bounds_cache.h"
 #include "chrome/browser/picture_in_picture/picture_in_picture_occlusion_tracker.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
+#include "chrome/common/url_constants.h"
 #include "content/public/browser/document_picture_in_picture_window_controller.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/picture_in_picture_window_controller.h"
@@ -570,7 +571,8 @@ bool PictureInPictureWindowManager::IsSupportedForDocumentPictureInPicture(
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
   return url.SchemeIs(url::kHttpsScheme) || url.SchemeIsFile() ||
-         net::IsLocalhost(url) || url.SchemeIs(content::kChromeUIScheme);
+         net::IsLocalhost(url) || url.SchemeIs(content::kChromeUIScheme) ||
+         url.SchemeIs(chrome::kIsolatedAppScheme);
 #else
   return false;
 #endif  // !BUILDFLAG(IS_ANDROID)
