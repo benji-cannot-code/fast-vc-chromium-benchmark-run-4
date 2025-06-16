6bbc8e5d9c4184c29c32919f580d10c7d5086509
Hira Mahmood <hiramahmood@google.com>
2025-06-16 09:05:40 -0700

    [iOS] Create view controller for Interactive Lens Overlay Promo
    This CL creates the view controller for the interactive Lens promo in
    the FRE. Addition of the actual Lens view will be added in a later CL.
    Screen recordings are attached below (the Lens view is given a gray
    background for the sake of the recordings so that the view's position is
    visible).
    
    Regular font:
    https://screencast.googleplex.com/cast/NjAxNTkyNzAxMDE5NzUwNHxjZDdjNDg0Ny1jMw
    Larger fonts:
    https://screencast.googleplex.com/cast/NDcwMDI3MjQyODMxODcyMHxhNjYxODA4NC02MQ
    
    Fixed: 417701357
    Change-Id: Ib39bd357f0dc7bbe54bdde73bd36940aaed1b8a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6643236
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474372}

--- ios/chrome/browser/first_run/ui_bundled/interactive_lens/coordinator/interactive_lens_promo_coordinator.mm
+++ ios/chrome/browser/first_run/ui_bundled/interactive_lens/coordinator/interactive_lens_promo_coordinator.mm
@@ -5,9 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/first_run/ui_bundled/interactive_lens/coordinator/interactive_lens_promo_coordinator.h"
 
+#import "base/check.h"
 #import "ios/chrome/browser/first_run/ui_bundled/first_run_screen_delegate.h"
+#import "ios/chrome/browser/first_run/ui_bundled/interactive_lens/ui/interactive_lens_overlay_promo_view_controller.h"
 
-@implementation InteractiveLensPromoCoordinator
+@interface InteractiveLensPromoCoordinator () <InteractiveLensPromoDelegate>
+@end
+
+@implementation InteractiveLensPromoCoordinator {
+  // View controller for the Interactive Lens promo screen.
+  InteractiveLensOverlayPromoViewController* _promoViewController;
+  // Container view controller for the Lens view.
+  UIViewController* _lensContainerViewController;
+}
 
 @synthesize baseNavigationController = _baseNavigationController;
 
@@ -25,12 +35,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   [super start];
-  // TODO(crbug.com/416480202): Present screen.
+  // TODO(crbug.com/416480202): Trigger the Lens UI presentation with
+  // `_lensContainerViewController` as the base view controller before
+  // presenting `_promoViewController`.
+  _lensContainerViewController = [[UIViewController alloc] init];
+  _promoViewController = [[InteractiveLensOverlayPromoViewController alloc]
+      initWithLensView:_lensContainerViewController.view];
+  _promoViewController.delegate = self;
+  _promoViewController.modalInPresentation = YES;
+  BOOL animated = self.baseNavigationController.topViewController != nil;
+  [self.baseNavigationController setViewControllers:@[ _promoViewController ]
+                                           animated:animated];
 }
 
 - (void)stop {
   self.firstRunDelegate = nil;
+  _lensContainerViewController = nil;
+  _promoViewController = nil;
   [super stop];
+}
+
+#pragma mark - InteractiveLensPromoDelegate
+
+- (void)didTapContinueButton {
+  CHECK(self.firstRunDelegate);
+  [self.firstRunDelegate screenWillFinishPresenting];
 }
 
 @end
