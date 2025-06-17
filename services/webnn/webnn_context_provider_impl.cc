a20ac6c25a74b5bcc1f1c50b0ac5bbf909498064
Reilly Grant <reillyg@chromium.org>
2025-06-17 15:37:51 -0700

    Remove unnecessary CHECK_IS_TEST() checks in WebNNContextProviderImpl
    These checks are unnecessary because they are in functions already named
    "ForTesting". There is a presubmit check which makes such these
    functions aren't called from non-test functions.
    
    Change-Id: I8bca1dba56239326139dce735338f7c1e06a339d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653687
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1475272}

--- services/webnn/webnn_context_provider_impl.cc
+++ services/webnn/webnn_context_provider_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/check_is_test.h"
 #include "base/metrics/histogram_functions.h"
 #include "gpu/command_buffer/service/scheduler.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
@@ -124,8 +123,6 @@ WebNNContextProviderImpl::CreateForTesting(
     mojo::PendingReceiver<mojom::WebNNContextProvider> receiver,
     WebNNStatus status,
     LoseAllContextsCallback lose_all_contexts_callback) {
-  CHECK_IS_TEST();
-
   gpu::GpuFeatureInfo gpu_feature_info;
   gpu::GPUInfo gpu_info;
 
@@ -279,7 +276,6 @@ void WebNNContextProviderImpl::CreateWebNNContext(
 base::optional_ref<WebNNContextImpl>
 WebNNContextProviderImpl::GetWebNNContextImplForTesting(
     const blink::WebNNContextToken& handle) {
-  CHECK_IS_TEST();
   const auto it = impls_.find(handle);
   if (it == impls_.end()) {
     mojo::ReportBadMessage(kBadMessageInvalidContext);
