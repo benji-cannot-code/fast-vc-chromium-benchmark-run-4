2443b04ff9491908f62d1d90fe700de69914fab5
Simon Hangl <simonha@google.com>
2025-06-17 10:36:20 -0700

    Add new usage indicator class outside of ash for multi screen capture
    Based on partner feedback, this CL implements a new usage indicator
    design for the the getAllScreensMedia API. It also implements the new
    architectural requirement to move the usage indicator code outside of
    `ash/`
    
    DD: go/screencapture-notification-allowlist
    Change-Id: I8e95095acf85d1ee822e9a074cb7c3bd5adead70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563685
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475076}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -393,6 +393,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/enterprise/cloud_storage/one_drive_pref_observer.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_download_observer_factory.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
+#include "chrome/browser/media/webrtc/multi_capture/multi_capture_usage_indicator_service_factory.h"
 #include "chrome/browser/nearby_sharing/nearby_sharing_service_factory.h"
 #include "chrome/browser/policy/networking/policy_cert_service_factory.h"
 #include "chrome/browser/policy/networking/user_network_configuration_updater_factory.h"
@@ -798,6 +799,10 @@ void ChromeBrowserMainExtraPartsProfiles::
   ClipboardRestrictionServiceFactory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS)
   policy::MultiScreenCapturePolicyServiceFactory::GetInstance();
+  if (base::FeatureList::IsEnabled(
+          chromeos::features::kMultiCaptureReworkedUsageIndicators)) {
+    multi_capture::MultiCaptureUsageIndicatorServiceFactory::GetInstance();
+  }
   if (chromeos::features::
           IsMicrosoftOneDriveIntegrationForEnterpriseEnabled()) {
     chromeos::cloud_storage::OneDrivePrefObserverFactory::GetInstance();
