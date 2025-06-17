9fe0d0e4ce5a5fa73cb95abbe103d01aaa849e35
Brad Triebwasser <btriebw@chromium.org>
2025-06-17 14:34:50 -0700

    Resolve early when processing empty prompt input & fix up WPTs
    Adds an early exit for when prompt messages are empty. Also fixes up
    WPTs and fixes tests which weren't awaiting promise_rejects_dom.
    
    
    Fixed: 425446560
    Change-Id: I550f8379df2daf771bdad5b2a251c2aa55ac58e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652603
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475237}

--- third_party/blink/web_tests/wpt_internal/ai/language-model-api-prompt-multimodal.https.window.js
+++ third_party/blink/web_tests/wpt_internal/ai/language-model-api-prompt-multimodal.https.window.js
@@ -33,7 +33,7 @@ promise_test(async t => {
   newImage.src = kValidImagePath;
   const session = await createLanguageModel(kImageOptions);
   // TODO(crbug.com/409615288): Expect a TypeError according to the spec.
-  promise_rejects_dom(
+  return promise_rejects_dom(
       t, 'SyntaxError',
       session.prompt(messageWithContent(kPrompt, 'text', newImage)));
 }, 'Prompt with type:"text" and image content should reject');
@@ -43,7 +43,7 @@ promise_test(async t => {
   const newImage = new Image();
   newImage.src = kValidImagePath;
   const session = await createLanguageModel(kImageOptions);
-  promise_rejects_dom(t, 'NotSupportedError', session.prompt([
+  return promise_rejects_dom(t, 'NotSupportedError', session.prompt([
     {role: 'assistant', content: [{type: 'image', value: newImage}]}
   ]));
 }, 'Prompt with assistant role should reject with multimodal input');
@@ -57,7 +57,7 @@ promise_test(async (t) => {
   const newImage = new Image();
   newImage.src = kValidImagePath;
   const session = await createLanguageModel();
-  promise_rejects_dom(
+  return promise_rejects_dom(
       t, 'NotSupportedError',
       session.prompt(messageWithContent(kPrompt, 'image', newImage)));
 }, 'Prompt image without `image` expectedInput');
@@ -224,7 +224,7 @@ promise_test(async (t) => {
   await ensureLanguageModel();
   const blob = await (await fetch(kValidAudioPath)).blob();
   const session = await createLanguageModel();
-  promise_rejects_dom(
+  return promise_rejects_dom(
       t, 'NotSupportedError',
       session.prompt(messageWithContent(kPrompt, 'audio', blob)));
 }, 'Prompt audio without `audio` expectedInput');
@@ -256,7 +256,7 @@ promise_test(async (t) => {
   const blob = await (await fetch(kValidImagePath)).blob();
   const session = await createLanguageModel(kAudioOptions);
   // TODO(crbug.com/409615288): Expect a TypeError according to the spec.
-  promise_rejects_dom(
+  return promise_rejects_dom(
       t, 'DataError',
       session.prompt(messageWithContent(kPrompt, 'audio', blob)));
 }, 'Prompt audio with blob containing invalid audio data.');
