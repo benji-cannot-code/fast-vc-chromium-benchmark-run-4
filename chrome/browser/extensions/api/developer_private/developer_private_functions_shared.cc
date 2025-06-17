4420cdd0a95de5418d106a6ebb622b28dc1360ee
Keigo Oka <oka@google.com>
2025-06-16 23:26:00 -0700

    extensions: Remove if def in updateExtensionConfiguration for android
    Now we have SitePermissionHelper and ToolbarActionModel available, we
    can remove the if-def around the use of them.
    
    Bug: 392777363
    
    Change-Id: I007cfc1a173e585c1d421e5573716f3f298d8298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650183
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474765}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_shared.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_shared.cc
@@ -27,12 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/manifest_v2_experiment_manager.h"
 #include "chrome/browser/extensions/permissions/permissions_updater.h"
 #include "chrome/browser/extensions/permissions/scripting_permissions_modifier.h"
+#include "chrome/browser/extensions/permissions/site_permissions_helper.h"
 #include "chrome/browser/extensions/updater/extension_updater.h"
 #include "chrome/browser/extensions/webstore_reinstaller.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/supervised_user/supervised_user_browser_utils.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
 #include "chrome/browser/ui/safety_hub/menu_notification_service_factory.h"
+#include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/common/drop_data.h"
 #include "extensions/browser/disable_reason.h"
@@ -57,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/extensions/permissions/site_permissions_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -588,9 +589,6 @@ DeveloperPrivateUpdateExtensionConfigurationFunction::Run() {
       event_router->OnExtensionConfigurationChanged(extension->id());
     }
   }
-// TODO(crbug.com/392777363): Enable this code when toolbars are supported on
-// desktop Android.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
   if (update.show_access_requests_in_toolbar) {
     SitePermissionsHelper(Profile::FromBrowserContext(browser_context()))
         .SetShowAccessRequestsInToolbar(
@@ -610,7 +608,6 @@ DeveloperPrivateUpdateExtensionConfigurationFunction::Run() {
                                                  !is_action_pinned);
     }
   }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
   return RespondNow(NoArguments());
 }
