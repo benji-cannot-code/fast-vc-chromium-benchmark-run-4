f6d56b341f1e20d6fa0f9cfe7517bdeba09af6c7
Julia Sobiech <jsobiech@google.com>
2025-06-17 14:42:11 -0700

    [H/W] Add metrics for Home/Work superset import
    This CL adds metrics to track the user decision for the
    H/W superset save prompt.
    
    Bug: 421353034
    Change-Id: Ic66bce5b473f7964d895831c34172eae8bf8bc8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6647804
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475244}

--- components/autofill/core/browser/metrics/profile_import_metrics.cc
+++ components/autofill/core/browser/metrics/profile_import_metrics.cc
@@ -228,6 +228,12 @@ void LogProfileUpdateImportDecision(
   }
 }
 
+void LogHomeWorkSupersetImportDecision(
+    AutofillClient::AddressPromptUserDecision decision) {
+  base::UmaHistogramEnumeration(
+      "Autofill.ProfileImport.HomeWorkSupersetProfileDecision", decision);
+}
+
 // static
 void LogRemovedSettingInaccessibleFields(bool did_remove) {
   base::UmaHistogramBoolean(
