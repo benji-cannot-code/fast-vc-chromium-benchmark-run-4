382b6a73a7df4a5cc78375af0a0fe9d424f800fb
Nohemi Fernandez <fernandex@google.com>
2025-06-17 06:08:18 -0700

    [iOS] Disable multiple reminder tests in ReminderNotificationClientTest.
    Bots are failing in ios-asan:
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-asan
    
    Bug: 425616989, 425618273
    Change-Id: I693d7cf6d0d895dde45ad633d1927a799e516561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651515
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Auto-Submit: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474911}

--- ios/chrome/browser/reminder_notifications/model/reminder_notification_client_unittests.mm
+++ ios/chrome/browser/reminder_notifications/model/reminder_notification_client_unittests.mm
@@ -200,7 +200,7 @@ TEST_F(ReminderNotificationClientTest, OneReminderInPrefs) {
 }
 
 // Test scheduling multiple notifications from prefs.
-TEST_F(ReminderNotificationClientTest, MultipleRemindersInPrefs) {
+TEST_F(ReminderNotificationClientTest, DISABLED_MultipleRemindersInPrefs) {
   SetupMockNotificationCenter();
   SetReminderPermission(true);
   GURL url1("http://example.com/page1");
@@ -321,7 +321,7 @@ TEST_F(ReminderNotificationClientTest, OneReminderInPrefsHasPrefRemoved) {
 
 // Test scheduling multiple notifications from Prefs and verifies Prefs removal.
 TEST_F(ReminderNotificationClientTest,
-       MultipleRemindersInPrefsHavePrefsRemoved) {
+       DISABLED_MultipleRemindersInPrefsHavePrefsRemoved) {
   SetupMockNotificationCenter();
   SetReminderPermission(true);
   GURL url1("http://example.com/page1");
