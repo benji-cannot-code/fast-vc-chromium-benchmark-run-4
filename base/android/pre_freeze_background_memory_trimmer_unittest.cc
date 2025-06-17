f8e15c7ed56a1773e5477d9304a7cce6b27ebebf
Thiabaud Engelbrecht <thiabaud@google.com>
2025-06-17 07:03:14 -0700

    [self_compact] Refactor
    This CL moves most of the declarations into their own header, and begins
    moving the implementation into its own file.
    
    Change-Id: I3317889ad2e70fec3fc32856c3f5dddcd913b58c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6638283
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474940}

--- base/android/pre_freeze_background_memory_trimmer_unittest.cc
+++ base/android/pre_freeze_background_memory_trimmer_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "base/android/self_compaction_manager.h"
 #include "base/compiler_specific.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_file.h"
