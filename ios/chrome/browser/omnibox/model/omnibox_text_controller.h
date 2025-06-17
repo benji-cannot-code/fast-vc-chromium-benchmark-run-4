5ea32cad0051bd487b29201161da7a34877a485b
Ameur Hosni <ameurhosni@google.com>
2025-06-17 09:24:19 -0700

    [IOS][OR] Move onDidChange and willChange to the text controller
    This CL aims to move text state handling from the omniboxViewIOS to
    the text controller. This will help removing the view IOS in a future
    CL.
    Some tests were relocated to the text model unittest since it's now
    handling the text state changes computation.
    
    Bug: 409730560
    Change-Id: I7fe01b0212b068c8854fe4540fdab22285d696b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6643557
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475023}

--- ios/chrome/browser/omnibox/model/omnibox_text_controller.h
+++ ios/chrome/browser/omnibox/model/omnibox_text_controller.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <string>
 
+#import "ios/chrome/browser/omnibox/model/omnibox_text_model.h"
+
 @protocol AutocompleteSuggestion;
 @class OmniboxAutocompleteController;
 class OmniboxControllerIOS;
@@ -17,7 +19,6 @@ class OmniboxEditModelIOS;
 @protocol OmniboxFocusDelegate;
 @protocol OmniboxTextControllerDelegate;
 @class OmniboxTextFieldIOS;
-struct OmniboxTextModel;
 class OmniboxViewIOS;
 
 /// Controller of the omnibox text.
@@ -35,6 +36,9 @@ class OmniboxViewIOS;
 
 /// Omnibox textfield.
 @property(nonatomic, weak) OmniboxTextFieldIOS* textField;
+
+/// Returns the current selection range.
+@property(nonatomic, assign, readonly) NSRange currentSelection;
 
 /// Temporary initializer, used during the refactoring. crbug.com/390409559
 - (instancetype)initWithOmniboxController:
@@ -64,8 +68,11 @@ class OmniboxViewIOS;
 /// Inserts text into the omnibox without triggering autocomplete.
 - (void)insertTextToOmnibox:(NSString*)text;
 
-// Notifies the client about input changes.
+/// Notifies the client about input changes.
 - (void)notifyClientOnUserInputInProgressChange:(BOOL)changedToUserInProgress;
+
+/// Retrieves the current textfield selection bounds.
+- (void)getSelectionBounds:(size_t*)start end:(size_t*)end;
 
 #pragma mark - Autocomplete event
 
--- ios/chrome/browser/omnibox/model/test_omnibox_view_ios.mm
+++ ios/chrome/browser/omnibox/model/test_omnibox_view_ios.mm
@@ -11,17 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/omnibox/browser/test_omnibox_client.h"
 #import "ui/gfx/native_widget_types.h"
 
-// static
-OmniboxViewIOS::State TestOmniboxViewIOS::CreateState(std::string text,
-                                                      size_t sel_start,
-                                                      size_t sel_end) {
-  OmniboxViewIOS::State state;
-  state.text = base::UTF8ToUTF16(text);
-  state.sel_start = sel_start;
-  state.sel_end = sel_end;
-  return state;
-}
-
 std::u16string TestOmniboxViewIOS::GetText() const {
   return text_;
 }
@@ -32,11 +21,6 @@ void TestOmniboxViewIOS::SetWindowTextAndCaretPos(const std::u16string& text,
                                                   bool notify_text_changed) {
   text_ = text;
   selection_ = gfx::Range(caret_pos);
-}
-
-void TestOmniboxViewIOS::GetSelectionBounds(size_t* start, size_t* end) const {
-  *start = selection_.start();
-  *end = selection_.end();
 }
 
 void TestOmniboxViewIOS::OnInlineAutocompleteTextMaybeChanged(
@@ -52,8 +36,4 @@ void TestOmniboxViewIOS::OnInlineAutocompleteTextMaybeChanged(
   if (text_changed) {
     selection_ = gfx::Range(text_.size(), inline_autocompletion.size());
   }
-}
-
-bool TestOmniboxViewIOS::OnAfterPossibleChange() {
-  return false;
 }
