2b18492cb912e8b6da4c7e5b08c9ccedcf6f9de5
Lily Chen <chlily@chromium.org>
2025-06-17 14:27:42 -0700

    Enable SafeBrowsingApiHandler(Bridge) to retrieve SafetyNet ID
    This CL adds the ability for SafeBrowsingApiHandler(Bridge) to retrieve
    the device's SafetyNet shared UUID. This is needed for
    MaliciousApkDownloadCheck, which requires this value to be populated in
    ClientDownloadRequest pings.
    
    This CL adds the method to the interface on the Chromium side and adds a
    dummy implementation. The real implementation will be landed
    downstream/internally.
    
    Bug: 397407934
    Cq-Include-Trybots: luci.chrome.try:android-internal-binary-size;luci.chrome.try:android-internal-rel;luci.chrome.try:android-internal-dbg
    Change-Id: I763b1705833862f7a9771a5f6759b8cfdff70650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6648869
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475231}

--- components/safe_browsing/android/java/src/org/chromium/components/safe_browsing/SafeBrowsingApiBridge.java
+++ components/safe_browsing/android/java/src/org/chromium/components/safe_browsing/SafeBrowsingApiBridge.java
@@ -47,6 +47,10 @@ public final class SafeBrowsingApiBridge {
     private static boolean sSafetyNetApiHandlerInitCalled;
 
     @GuardedBy("sSafetyNetApiHandlerLock")
+    private static @SafetyNetApiHandler.SafetyNetApiState int sSafetyNetApiState =
+            SafetyNetApiHandler.SafetyNetApiState.NOT_AVAILABLE;
+
+    @GuardedBy("sSafetyNetApiHandlerLock")
     private static @Nullable SafetyNetApiHandler sSafetyNetApiHandler;
 
     @GuardedBy("sSafeBrowsingApiHandlerLock")
@@ -93,6 +97,7 @@ public final class SafeBrowsingApiBridge {
         synchronized (sSafetyNetApiHandlerLock) {
             sSafetyNetApiHandlerInitCalled = false;
             sSafetyNetApiHandler = null;
+            sSafetyNetApiState = SafetyNetApiHandler.SafetyNetApiState.NOT_AVAILABLE;
         }
         synchronized (sSafeBrowsingApiHandlerLock) {
             sSafeBrowsingApiHandler = null;
@@ -108,10 +113,19 @@ public final class SafeBrowsingApiBridge {
      * @return true iff the initialization succeeded.
      */
     @CalledByNative
-    public static boolean ensureSafetyNetApiInitialized() {
+    public static boolean ensureSafetyNetApiInitialized(boolean requireFirstParty) {
         synchronized (sSafetyNetApiHandlerLock) {
-            return getSafetyNetApiHandler() != null;
+            return getSafetyNetApiHandler() != null
+                    && (!requireFirstParty
+                            || sSafetyNetApiState
+                                    == SafetyNetApiHandler.SafetyNetApiState
+                                            .INITIALIZED_FIRST_PARTY);
         }
+    }
+
+    @CalledByNative
+    public static boolean ensureSafetyNetApiInitialized() {
+        return ensureSafetyNetApiInitialized(/* requireFirstParty= */ false);
     }
 
     /**
@@ -156,9 +170,10 @@ public final class SafeBrowsingApiBridge {
     }
 
     /**
-     * Initializes the SafetyNetApiHandler, if supported.
+     * Initializes the SafetyNetApiHandler, if supported. Sets {@link sSafetyNetApiState} to
+     * indicate which operations are available from the handler.
      *
-     * The caller must {@link #setSafetyNetApiHandler(SafetyNetApiHandler)} first.
+     * <p>The caller must {@link #setSafetyNetApiHandler(SafetyNetApiHandler)} first.
      *
      * @return the handler if it is usable, or null if the API is not supported.
      */
@@ -169,9 +184,12 @@ public final class SafeBrowsingApiBridge {
                 Log.i(TAG, "initSafetyNetApiHandler");
             }
             if (sSafetyNetApiHandler == null) return null;
-            return sSafetyNetApiHandler.init(new SafetyNetApiLookupDoneObserver())
-                    ? sSafetyNetApiHandler
-                    : null;
+            sSafetyNetApiState =
+                    sSafetyNetApiHandler.initialize(new SafetyNetApiLookupDoneObserver());
+            if (sSafetyNetApiState == SafetyNetApiHandler.SafetyNetApiState.NOT_AVAILABLE) {
+                return null;
+            }
+            return sSafetyNetApiHandler;
         }
     }
 
@@ -180,6 +198,13 @@ public final class SafeBrowsingApiBridge {
         public void onVerifyAppsEnabledDone(long callbackId, int result) {
             synchronized (sSafetyNetApiHandlerLock) {
                 SafeBrowsingApiBridgeJni.get().onVerifyAppsEnabledDone(callbackId, result);
+            }
+        }
+
+        @Override
+        public void onGetSafetyNetIdDone(String result) {
+            synchronized (sSafetyNetApiHandlerLock) {
+                SafeBrowsingApiBridgeJni.get().onGetSafetyNetIdDone(result);
             }
         }
     }
@@ -229,6 +254,7 @@ public final class SafeBrowsingApiBridge {
         synchronized (sSafetyNetApiHandlerLock) {
             assert sSafetyNetApiHandlerInitCalled;
             assert sSafetyNetApiHandler != null;
+            assert sSafetyNetApiState != SafetyNetApiHandler.SafetyNetApiState.NOT_AVAILABLE;
             try (TraceEvent t = TraceEvent.scoped("SafeBrowsingApiBridge.startAllowlistLookup")) {
                 return sSafetyNetApiHandler.startAllowlistLookup(uri, threatType);
             }
@@ -275,6 +301,7 @@ public final class SafeBrowsingApiBridge {
         synchronized (sSafetyNetApiHandlerLock) {
             assert sSafetyNetApiHandlerInitCalled;
             assert sSafetyNetApiHandler != null;
+            assert sSafetyNetApiState != SafetyNetApiHandler.SafetyNetApiState.NOT_AVAILABLE;
             sSafetyNetApiHandler.isVerifyAppsEnabled(callbackId);
         }
     }
@@ -289,7 +316,24 @@ public final class SafeBrowsingApiBridge {
         synchronized (sSafetyNetApiHandlerLock) {
             assert sSafetyNetApiHandlerInitCalled;
             assert sSafetyNetApiHandler != null;
+            assert sSafetyNetApiState != SafetyNetApiHandler.SafetyNetApiState.NOT_AVAILABLE;
             sSafetyNetApiHandler.enableVerifyApps(callbackId);
+        }
+    }
+
+    /**
+     * Gets the device's shared UUID from the SafetyNet API.
+     *
+     * <p>Must only be called if {@code #ensureSafetyNetApiInitialized(true)} returns true.
+     */
+    @CalledByNative
+    public static void getSafetyNetId() {
+        synchronized (sSafetyNetApiHandlerLock) {
+            assert sSafetyNetApiHandlerInitCalled;
+            assert sSafetyNetApiHandler != null;
+            assert sSafetyNetApiState
+                    == SafetyNetApiHandler.SafetyNetApiState.INITIALIZED_FIRST_PARTY;
+            sSafetyNetApiHandler.getSafetyNetId();
         }
     }
 
@@ -304,5 +348,7 @@ public final class SafeBrowsingApiBridge {
                 long checkDeltaUs);
 
         void onVerifyAppsEnabledDone(long callbackId, int result);
+
+        void onGetSafetyNetIdDone(String result);
     }
 }
