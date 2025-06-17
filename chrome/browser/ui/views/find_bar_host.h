226145ef0da5d12f7cf01fb65b3007212cbb82b8
Thomas Lukaszewicz <tluk@chromium.org>
2025-06-17 13:22:51 -0700

    [bedrock] Reset FindBarController pre BrowserWindow destruction
    FindBarController has a dependency on BrowserWindow (specifically
    BrowserView) via its owned FindBarHost. This CL destroys
    BrowserWindowFeatures::find_bar_controller_ before BrowserWindow
    destruction to avoid dangling references and UAF crashes.
    
    This CL also updates FindBarHost to leverage a ScopedObservation
    for its FocusManager observation.
    
    Bug: 413168662, 423956131
    Change-Id: Ib4a0af4c72e2e90bc29e510c3fa3a38434dbc340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650022
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475187}

--- chrome/browser/ui/views/find_bar_host.h
+++ chrome/browser/ui/views/find_bar_host.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ui/find_bar/find_bar.h"
 #include "chrome/browser/ui/find_bar/find_bar_controller.h"
 #include "chrome/browser/ui/views/find_bar_view.h"
@@ -221,6 +222,10 @@ class FindBarHost : public FindBar,
   // or any of its children. Used to restore focus once the FindBarHost's view
   // is closed.
   std::unique_ptr<views::ExternalFocusTracker> focus_tracker_;
+
+  // Observation over the host's FocusManager.
+  base::ScopedObservation<views::FocusManager, views::FocusChangeListener>
+      focus_manager_observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_FIND_BAR_HOST_H_
