fc29d7c58f790f97731b35d9a01b5fe01951376c
Dan Harrington <harringtond@chromium.org>
2025-06-17 13:29:18 -0700

    Revert "[Settings][DBD] Add a signout link to settings checkbox sublabel"
    This reverts commit f795fd2f532d5139daef2bb628810c807d4bbe58.
    
    Reason for revert: crbug.com/425711771
    
    Bug: 397187800
    Original change's description:
    > [Settings][DBD] Add a signout link to settings checkbox sublabel
    >
    > This change adds a signout link to the 'Cookies and site data' checkbox
    > in the 'Delete browsing data' settings page (On non-ChromeOS platforms).
    > The string that contains the linkout is constructed on the C++ side and
    > displayed in the dialog once 'browsing-data-counter-text-update' is
    > called with the corresponding pref.
    >
    > To allow for anchor elements with custom click behaviors in the
    > settings-checkbox element. This change also propagates valid click
    > events from `subLabelHtml` outside of the 'settings-checkbox' element by
    > firing a 'sub-label-link-clicked' event, this gives clients the
    > flexibility with handling the events accordingly.
    >
    > This does mean that the default behavior of an anchor element (opening a
    > link), will not work if the clients do not handle the
    > 'sub-label-link-clicked' event. However, this does not break any
    > existing behavior as the only client of the `subLabelHtml` attribute is
    > `settings-clear-browsing-data-dialog` which does not pass any action
    > links.
    >
    > Screenshot: https://screenshot.googleplex.com/9ovXw3zcFaouWZ7
    > Bug: 397187800
    > Change-Id: I24a9acd598b561b34888ff3e4af16c89e888b4be
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6638641
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1475000}
    
    Bug: 397187800
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I8412a176179bb6defaaf421c3a743a6b4a4d8255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651464
    Owners-Override: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475195}

--- chrome/test/data/webui/settings/checkbox_test.ts
+++ chrome/test/data/webui/settings/checkbox_test.ts
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://settings/lazy_load.js';
 
-import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import type {SettingsCheckboxElement} from 'chrome://settings/lazy_load.js';
-import {assertEquals, assertFalse, assertNotReached, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {eventToPromise} from 'chrome://webui-test/test_util.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 /** @fileoverview Suite of tests for settings-checkbox. */
 suite('SettingsCheckbox', function() {
@@ -87,78 +85,5 @@ suite('SettingsCheckbox', function() {
     await testElement.$.checkbox.updateComplete;
     assertTrue(testElement.checked);
     assertEquals(1, prefNum.value);
-  });
-
-  test('sub label should be able to have an id', () => {
-    testElement.subLabelHtml = `<a id="subLabelWithLink"></a>`;
-    flush();
-
-    const actionLink =
-        testElement.$.subLabel.querySelector('#subLabelWithLink');
-    assertTrue(!!actionLink);
-  });
-
-  test('sub label should be able to have an aria-label', () => {
-    testElement.subLabelHtml = `<a aria-label="Label"></a>`;
-    flush();
-
-    const actionLink = testElement.$.subLabel.querySelector('a');
-    assertTrue(!!actionLink);
-    assertEquals(actionLink.getAttribute('aria-label'), 'Label');
-  });
-
-  test(
-      'click on sub label link should not toggle the button', async function() {
-        testElement.checked = true;
-        testElement.subLabelHtml = `<a href="#"></a>`;
-        flush();
-
-        const actionLink = testElement.$.subLabel.querySelector('a');
-        assertTrue(!!actionLink);
-
-        assertTrue(testElement.checked);
-
-        actionLink.click();
-        await testElement.$.checkbox.updateComplete;
-
-        assertTrue(testElement.checked);
-      });
-
-  test('click on sub label text should toggle the button', async function() {
-    testElement.checked = true;
-    testElement.subLabelHtml = `<a href="#"></a>`;
-    flush();
-
-    assertTrue(testElement.checked);
-
-    testElement.$.subLabel.click();
-    await testElement.$.checkbox.updateComplete;
-
-    assertFalse(testElement.checked);
-  });
-
-  test('click on sub label link should fire a custom event', async function() {
-    testElement.subLabelHtml = `<a href="#" id="subLabelWithLink"></a>`;
-    flush();
-
-    const actionLink = testElement.$.subLabel.querySelector('a');
-    assertTrue(!!actionLink);
-
-    const clickEventPromise =
-        eventToPromise('sub-label-link-clicked', testElement);
-    actionLink.click();
-    const clickEvent = await clickEventPromise;
-    assertEquals('subLabelWithLink', clickEvent.detail.id);
-  });
-
-  test('click on sub label text should not fire a custom event', () => {
-    testElement.subLabelHtml = `<a href="#" id="subLabelWithLink"></a>`;
-    testElement.addEventListener('sub-label-link-clicked', () => {
-      assertNotReached(
-          'custom event should not be triggered for non action link clicks.');
-    });
-    flush();
-
-    testElement.$.subLabel.click();
   });
 });
