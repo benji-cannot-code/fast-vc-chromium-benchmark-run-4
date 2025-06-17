f15a71e0b02e7f07650cee0949fbd21d9b6d752a
David Sanders <dsanders11@ucsbalum.com>
2025-06-17 05:42:46 -0700

    Remove unused include in mouse_wheel_event_queue.h
    Bug: 40318405
    Change-Id: I522e5088b8a82303b8c6981f14ba99ce46ad4e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6648169
    Reviewed-by: Aman Verma <amanvr@google.com>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474896}

--- components/input/mouse_wheel_event_queue.h
+++ components/input/mouse_wheel_event_queue.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/circular_deque.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "base/trace_event/trace_event.h"
 #include "components/input/dispatch_to_renderer_callback.h"
 #include "components/input/event_with_latency_info.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
