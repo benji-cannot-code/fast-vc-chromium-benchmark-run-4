822c24cf099c8f44f12fe3cff438737e6f5e39d5
Ho Cheung <hocheung@chromium.org>
2025-06-17 13:21:59 -0700

    [components] Apply clang-tidy modernize-use-nullptr on //components
    This CL removes the ability to use NULL as an address type in favor of
    nullptr.
    
    This modernization prevents various bugs when refactoring code.
    
    Bug: 40546877
    Change-Id: Ic41c7c5732b3648bd34478a1f3a828c80b37756c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636697
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475186}

--- components/cronet/native/generated/cronet.idl_impl_interface_unittest.cc
+++ components/cronet/native/generated/cronet.idl_impl_interface_unittest.cc
@@ -67,7 +67,7 @@ Cronet_RawDataPtr TestCronet_Buffer_GetData(Cronet_BufferPtr self) {
   CHECK(test);
   test->GetData_called_ = true;
 
-  return static_cast<Cronet_RawDataPtr>(0);
+  return static_cast<Cronet_RawDataPtr>(nullptr);
 }
 }  // namespace
 
@@ -284,7 +284,7 @@ Cronet_String TestCronet_Engine_GetVersionString(Cronet_EnginePtr self) {
   CHECK(test);
   test->GetVersionString_called_ = true;
 
-  return static_cast<Cronet_String>(0);
+  return static_cast<Cronet_String>(nullptr);
 }
 Cronet_String TestCronet_Engine_GetDefaultUserAgent(Cronet_EnginePtr self) {
   CHECK(self);
@@ -293,7 +293,7 @@ Cronet_String TestCronet_Engine_GetDefaultUserAgent(Cronet_EnginePtr self) {
   CHECK(test);
   test->GetDefaultUserAgent_called_ = true;
 
-  return static_cast<Cronet_String>(0);
+  return static_cast<Cronet_String>(nullptr);
 }
 void TestCronet_Engine_AddRequestFinishedListener(
     Cronet_EnginePtr self,
