2751c45ae0bd66624eed5f6e2b9a12f49d2811af
David Sanders <dsanders11@ucsbalum.com>
2025-06-17 12:25:10 -0700

    Correct include in //crypto/obsolete/md5.h
    Bug: 40318405
    Change-Id: I52434af8888d3947078315db552f7b0406aae542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6648053
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1475154}

--- crypto/obsolete/md5.h
+++ crypto/obsolete/md5.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/gtest_prod_util.h"
 #include "crypto/crypto_export.h"
-#include "third_party/boringssl/src/include/openssl/evp.h"
+#include "third_party/boringssl/src/include/openssl/digest.h"
 
 namespace crypto::obsolete {
 class Md5;
