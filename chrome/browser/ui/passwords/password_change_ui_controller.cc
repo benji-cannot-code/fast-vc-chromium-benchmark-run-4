6048736236bcddc3fbb64717ec038e67cc81c233
Rafal Godlewski <rgod@google.com>
2025-06-18 13:35:50 -0700

    [PasswordChange] Remove toast inheritance from DialogDelegate
    Creates the delegate separately from the view in
    PasswordChangeUIController.
    
    https://drive.google.com/file/d/1iOAyNdQhk60gk_RA2XppEkbQhn-k5A8Z
    
    Bug: 417938283
    Change-Id: I068ee860d167a4c978cece52f57558b37efb2d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651898
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475833}

--- chrome/browser/ui/passwords/password_change_ui_controller.cc
+++ chrome/browser/ui/passwords/password_change_ui_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/tabs/public/tab_dialog_manager.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
+#include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/passwords/password_change/password_change_toast.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
@@ -140,8 +141,12 @@ void PasswordChangeUIController::UpdateState(
 
   if (std::holds_alternative<ToastOptions>(configuration)) {
     if (toast_view_) {
+      CHECK(toast_delegate_);
       toast_view_->UpdateLayout(
           std::move(std::get<ToastOptions>(configuration)));
+      toast_delegate_->set_margins(toast_view_->CalculateMargins());
+      toast_view_->GetWidget()->SetBounds(
+          toast_delegate_->GetDesiredWidgetBounds());
       return;
     }
     ShowToast(std::move(std::get<ToastOptions>(configuration)));
@@ -232,6 +237,8 @@ PasswordChangeUIController::GetDialogOrToastConfiguration(
 
 void PasswordChangeUIController::ShowToast(ToastOptions options) {
   CHECK(tab_interface_);
+
+  std::u16string title = options.text;
   auto toast_view = std::make_unique<PasswordChangeToast>(std::move(options));
   toast_view_ = toast_view.get();
   auto params = std::make_unique<tabs::TabDialogManager::Params>();
@@ -239,10 +246,34 @@ void PasswordChangeUIController::ShowToast(ToastOptions options) {
   params->close_on_detach = false;
   params->disable_input = false;
 
+  toast_delegate_ = std::make_unique<views::DialogDelegate>();
+  toast_delegate_->SetModalType(ui::mojom::ModalType::kChild);
+  toast_delegate_->SetOwnershipOfNewWidget(
+      views::Widget::InitParams::CLIENT_OWNS_WIDGET);
+  toast_delegate_->set_use_custom_frame(true);
+  toast_delegate_->SetShowCloseButton(false);
+  toast_delegate_->SetButtons(static_cast<int>(ui::mojom::DialogButton::kNone));
+  toast_delegate_->SetContentsView(std::move(toast_view));
+  toast_delegate_->set_corner_radius(
+      ChromeLayoutProvider::Get()->GetDistanceMetric(
+          DISTANCE_TOAST_BUBBLE_HEIGHT));
+  toast_delegate_->set_margins(toast_view_->CalculateMargins());
+  toast_delegate_->SetAccessibleWindowRole(ax::mojom::Role::kAlert);
+  toast_delegate_->SetAccessibleTitle(title);
+  toast_delegate_->RegisterWidgetInitializedCallback(base::BindOnce(
+      [](views::DialogDelegate* delegate) {
+        views::BubbleFrameView* frame_view = delegate->GetBubbleFrameView();
+        if (frame_view) {
+          frame_view->SetBackgroundColor(ui::kColorToastBackgroundProminent);
+          frame_view->bubble_border()->set_draw_border_stroke(false);
+        }
+      },
+      base::Unretained(toast_delegate_.get())));
+
   toast_widget_ =
       tab_interface_->GetTabFeatures()
           ->tab_dialog_manager()
-          ->CreateAndShowDialog(toast_view.release(), std::move(params));
+          ->CreateAndShowDialog(toast_delegate_.get(), std::move(params));
   toast_widget_->MakeCloseSynchronous(
       base::BindOnce(&PasswordChangeUIController::CloseToastWidget,
                      weak_ptr_factory_.GetWeakPtr()));
@@ -325,4 +356,5 @@ void PasswordChangeUIController::CloseToastWidget(
     views::Widget::ClosedReason reason) {
   toast_view_ = nullptr;
   toast_widget_.reset();
+  toast_delegate_.reset();
 }
--- chrome/browser/ui/passwords/password_change_ui_controller.h
+++ chrome/browser/ui/passwords/password_change_ui_controller.h
@@ -20,6 +20,10 @@ namespace ui {
 class DialogModel;
 }  // namespace ui
 
+namespace views {
+class DialogDelegate;
+}  // namespace views
+
 // Responsible for creating and displaying appropriate views based on the
 // current state of the password change flow.
 class PasswordChangeUIController {
@@ -64,6 +68,9 @@ class PasswordChangeUIController {
 
   // View displaying the progress of password change.
   raw_ptr<PasswordChangeToast> toast_view_;
+
+  // Delegate for the `toast_view_`.
+  std::unique_ptr<views::DialogDelegate> toast_delegate_;
 
   // Widget containing the currently open toast, if any.
   std::unique_ptr<views::Widget> toast_widget_;
