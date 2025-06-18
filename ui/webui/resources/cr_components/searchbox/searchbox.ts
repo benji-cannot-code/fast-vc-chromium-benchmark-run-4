9ad8086907a736b2dff7eb4160f53a2709f9b340
Nihar Majmudar <niharm@google.com>
2025-06-18 13:09:32 -0700

    [composebox] Entrypoint click with text should navigate to AIM.
    Currently, this opens the composeplate and clears the text out. Should
    follow what web does and navigate the user to AIM.
    
    Change-Id: If2498f86ef076d58b0842b8c399a7dc5d039524a
    Bug: 425913575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653053
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475816}

--- ui/webui/resources/cr_components/searchbox/searchbox.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.ts
@@ -836,7 +836,7 @@ export class SearchboxElement extends SearchboxElementBase {
   }
 
   private onComposeButtonClick_(e: CustomEvent<ComposeClickEventDetail>) {
-    if (!this.composeboxEnabled) {
+    if (!this.composeboxEnabled || this.$.input.value.trim()) {
       // Construct navigation url.
       const searchParams = new URLSearchParams();
       searchParams.append('sourceid', 'chrome');
