d42aba34fb05fb6aaec3c9257e8519ebb626db74
quacksort <quacksort@google.com>
2025-06-18 09:15:58 -0700

    [Signin][Android] Remove expired metric MobileFre.FragmentInflationSpeed
    The histogram expired in 2023 and was superseded by
    MobileFre.SlowestLoadPoint and other histograms. This also removes
    AppRestrictionSupplier#mCompletionTimeCallbacks.
    
    Bug: 420877297
    Change-Id: Ie60f4d5efad596e24a3112cb7694a8cb63015398
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6607206
    Commit-Queue: Lucia Giorgi <quacksort@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475641}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AppRestrictionSupplier.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AppRestrictionSupplier.java
@@ -25,9 +25,7 @@ import org.chromium.components.policy.AbstractAppRestrictionsProvider;
 import org.chromium.components.policy.AppRestrictionsProvider;
 import org.chromium.components.policy.PolicySwitches;
 
-import java.util.LinkedList;
 import java.util.Locale;
-import java.util.Queue;
 import java.util.concurrent.RejectedExecutionException;
 
 /**
@@ -42,10 +40,8 @@ import java.util.concurrent.RejectedExecutionException;
 public class AppRestrictionSupplier implements OneshotSupplier<Boolean> {
     private static final String TAG = "AppRestriction";
 
-    private boolean mInitialized;
     private long mCompletionElapsedRealtimeMs;
     private final OneshotSupplierImpl<Boolean> mSupplier = new OneshotSupplierImpl<>();
-    private final Queue<Callback<Long>> mCompletionTimeCallbacks = new LinkedList<>();
 
     private @Nullable AsyncTask<Boolean> mFetchAppRestrictionAsyncTask;
 
@@ -66,22 +62,6 @@ public class AppRestrictionSupplier implements OneshotSupplier<Boolean> {
     @SuppressWarnings("NullAway")
     public @Nullable Boolean get() {
         return mSupplier.get();
-    }
-
-    /**
-     * Registers a callback for the timestamp from {@link SystemClock#elapsedRealtime} when the app
-     * restrictions call finished. If the restrictions have already been fetched, the callback will
-     * be invoked immediately.
-     *
-     * @param callback Callback to run with the timestamp of completing the fetch.
-     */
-    public void getCompletionElapsedRealtimeMs(Callback<Long> callback) {
-        ThreadUtils.assertOnUiThread();
-        if (mInitialized) {
-            callback.onResult(mCompletionElapsedRealtimeMs);
-        } else {
-            mCompletionTimeCallbacks.add(callback);
-        }
     }
 
     /** Start fetching app restriction on an async thread. */
@@ -130,8 +110,6 @@ public class AppRestrictionSupplier implements OneshotSupplier<Boolean> {
     }
 
     private void onRestrictionDetected(boolean isAppRestricted, long startTime) {
-        mInitialized = true;
-
         // Only record histogram when startTime is valid.
         if (startTime > 0) {
             mCompletionElapsedRealtimeMs = SystemClock.elapsedRealtime();
@@ -145,8 +123,5 @@ public class AppRestrictionSupplier implements OneshotSupplier<Boolean> {
                             isAppRestricted));
         }
         mSupplier.set(isAppRestricted);
-        while (!mCompletionTimeCallbacks.isEmpty()) {
-            mCompletionTimeCallbacks.remove().onResult(mCompletionElapsedRealtimeMs);
-        }
     }
 }
