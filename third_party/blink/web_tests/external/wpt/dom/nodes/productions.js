af8fba15ce2a0f6ebd4d641400cc5e859846136c
Joey Arhar <jarhar@chromium.org>
2025-06-18 09:25:18 -0700

    Update tests for relaxed DOM name/attribute parsing
    Spec PR: https://github.com/whatwg/dom/pull/1079
    
    Bug: 40122442, 40228234
    Change-Id: I42c40c3f3acdfcfc4647c6d87ffcbfadc6de13be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6570951
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475652}

--- third_party/blink/web_tests/external/wpt/dom/nodes/productions.js
+++ third_party/blink/web_tests/external/wpt/dom/nodes/productions.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var invalid_names = ["", "invalid^Name", "\\", "'", '"', "0", "0:a"] // XXX
-var valid_names = ["x", "X", ":", "a:0"]
-var invalid_qnames = [":a", "b:", "x:y:z"] // XXX
+var invalid_names = [""] // XXX
+var valid_names = ["x", "X", ":", "a:0", "invalid^Name", "\\", "'", '"', "0", "0:a", ":a", "x:y:x", "~"]
+var invalid_qnames = ["b:"] // XXX
