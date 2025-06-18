41fd28dd7f297b6c182a1929e1171ce2ff5181b7
Masa Fujita <massan@google.com>
2025-06-18 15:31:29 -0700

    extensions: Refactor ExtensionToolbarManager to ExtensionService
    Even though the original purpose for `ExtensionToolbarManager` was to
    handle only the extensions in the toolbar, we realized that other things
    outside of toolbar that require this manager. For example, buttons in
    the three dot menu require a method inside this manager to tell whether
    extensions are enabled. Therefore, we have decided to abstract the
    the manager.
    
    This CL renames `ExtensionToolbarManager` to `ExtensionService` and
    moves it from /toolbar/extensions to /extensions, and changes the owner
    from `ToolbarManager` to `RootUiCoordinator` so that it can be accessed
    from outside toolbar components.
    
    There should not be any functional change with this CL.
    
    Bug: 424414252
    Change-Id: I42fa7795d97e0cb4c670f474c19c00f7c60b677a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636282
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Masa Fujita <massan@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475885}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -153,6 +153,7 @@ import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeControllerFactory;
 import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeUtils;
 import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeUtils.EdgeToEdgeDebuggingInfo;
 import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeUtils.MissingNavbarInsetsReason;
+import org.chromium.chrome.browser.ui.extensions.ExtensionService;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.ui.native_page.NativePage;
 import org.chromium.chrome.browser.ui.system.StatusBarColorController;
@@ -241,6 +242,8 @@ public class RootUiCoordinator
 
     protected @Nullable FindToolbarManager mFindToolbarManager;
     private @Nullable FindToolbarObserver mFindToolbarObserver;
+
+    private @Nullable ExtensionService mExtensionService;
 
     private OverlayPanelManager mOverlayPanelManager;
     private OverlayPanelManager.OverlayPanelManagerObserver mOverlayPanelManagerObserver;
@@ -646,6 +649,11 @@ public class RootUiCoordinator
             mToolbarManager = null;
         }
 
+        if (mExtensionService != null) {
+            mExtensionService.destroy();
+            mExtensionService = null;
+        }
+
         if (mAdaptiveToolbarUiCoordinator != null) {
             mAdaptiveToolbarUiCoordinator.destroy();
             mAdaptiveToolbarUiCoordinator = null;
@@ -786,6 +794,7 @@ public class RootUiCoordinator
     public void onInflationComplete() {
         mScrimManager = buildScrimWidget();
         mScrimManagerSupplier.set(mScrimManager);
+        mExtensionService = ExtensionService.maybeCreate(mProfileSupplier);
         initFindToolbarManager();
         initializeToolbar();
     }
@@ -1506,6 +1515,7 @@ public class RootUiCoordinator
                             mActivityTabProvider,
                             mScrimManager,
                             mActionModeControllerCallback,
+                            mExtensionService,
                             mFindToolbarManager,
                             mProfileSupplier,
                             mBookmarkModelSupplier,
