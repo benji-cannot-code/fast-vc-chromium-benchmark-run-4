1db87b27d6fd04d1839dffedb35c33ad67e792b3
Lei Zhang <thestig@chromium.org>
2025-06-17 17:23:39 -0700

    Add locking to GLibLogHandler()
    Avoid a race condition where multiple threads call
    g_log_set_always_fatal() or g_log_set_fatal_mask() at the same time. In
    the unlucky situation, a second thread will get the `G_LOG_LEVEL_MASK`
    value set by the first thread, before the first thread had a chance to
    restore the original value.
    
    This can happen with ~QEventDispatcherGlib calling g_log() on multiple
    threads. Since libQt6Core.so.6 is not something Chromium can control,
    GLibLogHandler() has to tolerate this somehow.
    
    Change-Id: Icbf3f680d7ae66db7707b9885d6ca969b0a1a6d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651436
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475327}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -211,6 +211,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_GLIB)
 #include <glib-object.h>
+
+#include "base/synchronization/lock.h"
 #endif
 
 #if BUILDFLAG(IS_WIN)
@@ -268,11 +270,17 @@ static void GLibLogHandler(const gchar* log_domain,
   if (!message)
     message = "<no message>";
 
-  GLogLevelFlags always_fatal_flags = g_log_set_always_fatal(G_LOG_LEVEL_MASK);
-  g_log_set_always_fatal(always_fatal_flags);
-  GLogLevelFlags fatal_flags =
-      g_log_set_fatal_mask(log_domain, G_LOG_LEVEL_MASK);
-  g_log_set_fatal_mask(log_domain, fatal_flags);
+  GLogLevelFlags always_fatal_flags;
+  GLogLevelFlags fatal_flags;
+  {
+    static base::NoDestructor<base::Lock> lock;
+    base::AutoLock auto_lock(*lock);
+    always_fatal_flags = g_log_set_always_fatal(G_LOG_LEVEL_MASK);
+    g_log_set_always_fatal(always_fatal_flags);
+    fatal_flags = g_log_set_fatal_mask(log_domain, G_LOG_LEVEL_MASK);
+    g_log_set_fatal_mask(log_domain, fatal_flags);
+  }
+
   if ((always_fatal_flags | fatal_flags) & log_level) {
     LOG(DFATAL) << log_domain << ": " << message;
   } else if (log_level & (G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL)) {
