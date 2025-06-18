3facceb5506230ef6e5db7fb654d86140bff6e7a
Jason <cgqaq@chromium.org>
2025-06-18 01:23:32 -0700

    Ensure full OTS error messages are displayed
    Previously, every BlinkOTSContext::Message call will override the
    previous messages, causing incomplete error messages in the console.
    
    This CL keeps the old messages by concat every single
    BlinkOTSContext::Message with '\n'
    
    Bug: 40773784
    Change-Id: I426089b885a4cebdbf1f9f10a385b265b9061d90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624899
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Auto-Submit: Jason Leo <cgqaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475440}

--- third_party/blink/renderer/platform/fonts/web_font_decoder.cc
+++ third_party/blink/renderer/platform/fonts/web_font_decoder.cc
@@ -62,10 +62,12 @@ class BlinkOTSContext final : public ots::OTSContext {
  public:
   void Message(int level, const char* format, ...) override;
   ots::TableAction GetTableAction(uint32_t tag) override;
-  const String& GetErrorString() { return error_string_; }
+  const String& GetErrorString() { return accumulated_error_string_; }
 
  private:
-  String error_string_;
+  void AppendErrorMessage(const String&& new_error_string);
+
+  String accumulated_error_string_;
 };
 
 void BlinkOTSContext::Message(int level, const char* format, ...) {
@@ -81,7 +83,7 @@ void BlinkOTSContext::Message(int level, const char* format, ...) {
   va_end(args);
 
   if (result <= 0) {
-    error_string_ = String("OTS Error");
+    AppendErrorMessage(String("Unspecified OTS Error"));
   } else {
     Vector<char, 256> buffer;
     unsigned len = result;
@@ -90,7 +92,21 @@ void BlinkOTSContext::Message(int level, const char* format, ...) {
     va_start(args, format);
     UNSAFE_TODO(vsnprintf(buffer.data(), buffer.size(), format, args));
     va_end(args);
-    error_string_ = StringImpl::Create(base::span(buffer).first(len));
+
+    AppendErrorMessage(
+        String(StringImpl::Create(base::span(buffer).first(len))));
+  }
+}
+
+void BlinkOTSContext::AppendErrorMessage(const String&& new_error_string) {
+  if (accumulated_error_string_.empty()) {
+    accumulated_error_string_ = new_error_string;
+  } else {
+    if (accumulated_error_string_.Contains(new_error_string)) {
+      return;
+    }
+    accumulated_error_string_ =
+        accumulated_error_string_ + "\n" + new_error_string;
   }
 }
 
