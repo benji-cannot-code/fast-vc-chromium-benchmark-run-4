346663116391c01dd1fca73552d3d5551fc159be
Jeremy Roman <jbroman@chromium.org>
2025-06-17 21:11:59 -0700

    glic: Use AccountManagedStatusFinder to determine if a status RPC is required
    Policy management status may take time to load and doesn't offer a
    convenient notification mechanism. Whether the account is managed is
    knowable from the extended account information, and there is an existing
    class which can monitor this. It does, however, have to be kicked off
    once the account to check is known.
    
    To do this cleanly, move the IdentityManager notifications required by
    the fetcher all into the fetcher itself, so we can just branch on which
    mechanism is used. (The prior mechanism is left in place for now with a
    feature param, so we can revert this via Finch if some deficiency in
    this logic is found.)
    
    Browser test included.
    
    Bug: 419170785
    Change-Id: I0cc5a3eb88a9fcba37f47ab8c85e543879b639b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642501
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475383}

--- chrome/browser/glic/glic_enabling.h
+++ chrome/browser/glic/glic_enabling.h
@@ -194,8 +194,6 @@ class GlicEnabling : public signin::IdentityManager::Observer {
   void OnExtendedAccountInfoUpdated(const AccountInfo& info) override;
   void OnExtendedAccountInfoRemoved(const AccountInfo& info) override;
   void OnRefreshTokensLoaded() override;
-  void OnRefreshTokenUpdatedForAccount(
-      const CoreAccountInfo& account_info) override;
   void OnRefreshTokenRemovedForAccount(
       const CoreAccountId& account_id) override;
 
@@ -209,13 +207,8 @@ class GlicEnabling : public signin::IdentityManager::Observer {
       signin_metrics::SourceForRefreshTokenOperation token_operation_source)
       override;
 
-  void OnIdentityManagerShutdown(
-      signin::IdentityManager* identity_manager) override;
-
   void UpdateEnabledStatus();
   void UpdateConsentStatus();
-
-  void UpdateUserStatus(const signin::PrimaryAccountChangeEvent& event_details);
 
   raw_ptr<Profile> profile_;
   raw_ptr<ProfileAttributesStorage> profile_attributes_storage_;
@@ -232,10 +225,6 @@ class GlicEnabling : public signin::IdentityManager::Observer {
   base::ScopedObservation<signin::IdentityManager,
                           signin::IdentityManager::Observer>
       identity_manager_observation_{this};
-
-  // Stores the previous value of `prefs::kGeminiSettings` to detect
-  // transitions.
-  std::optional<glic::prefs::SettingsPolicyState> cached_gemini_settings_value_;
 };
 
 }  // namespace glic
