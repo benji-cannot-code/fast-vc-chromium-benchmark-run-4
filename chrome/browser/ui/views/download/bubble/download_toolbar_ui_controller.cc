32b8ce31842a19fadd7a0d79cb98337acce86fae
Thomas Lukaszewicz <tluk@chromium.org>
2025-06-18 11:08:36 -0700

    [bedrock] Mitigate dangling pointers in DownloadDisplayController
    DownloadDisplayController has a dependency on BrowserView. This CL
    ensures DownloadDisplayController is destroyed before BrowserView
    to prevent risks of dangling references and UAF crashes.
    
    Bug: 413168662
    Change-Id: Ib76e60f7c50d5e2ccd0e4a2185369b4b4b6d3bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654457
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475719}

--- chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
+++ chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
@@ -475,6 +475,8 @@ void DownloadToolbarUIController::Init() {
 
 void DownloadToolbarUIController::TearDownPreBrowserWindowDestruction() {
   immersive_revealed_lock_.reset();
+  // DownloadDisplayController depends on BrowserView.
+  controller_.reset();
   browser_view_ = nullptr;
 }
 
