232191887f8e29b6e977a07dded17c981147339d
Colin Blundell <blundell@chromium.org>
2025-06-18 06:54:12 -0700

    [//cc] Remove dead code in TileDisplayLayerImpl
    These places check whether `tilings_` is empty where code just above
    has early-out'd if it's empty or CHECK'd that it's not empty.
    
    Change-Id: I44302f499d5bbfc7a0af7067f05860c504073376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651475
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475551}

--- cc/layers/tile_display_layer_impl.cc
+++ cc/layers/tile_display_layer_impl.cc
@@ -199,8 +199,7 @@ void TileDisplayLayerImpl::AppendQuads(const AppendQuadsContext& context,
     return;
   }
 
-  const float max_contents_scale =
-      tilings_.empty() ? 1.0f : tilings_.front()->contents_scale_key();
+  const float max_contents_scale = tilings_.front()->contents_scale_key();
 
   // If this layer is used as a backdrop filter, don't create and append a quad
   // as that will be done in RenderSurfaceImpl::AppendQuads.
@@ -316,8 +315,7 @@ void TileDisplayLayerImpl::GetContentsResourceId(
   CHECK(is_backdrop_filter_mask_);
   CHECK_EQ(tilings_.size(), 1u);
 
-  const float max_contents_scale =
-      tilings_.empty() ? 1.0f : tilings_.front()->contents_scale_key();
+  const float max_contents_scale = tilings_.front()->contents_scale_key();
   gfx::Rect content_rect =
       gfx::ScaleToEnclosingRect(gfx::Rect(bounds()), max_contents_scale);
   const auto ideal_scale = GetIdealContentsScale();
