0a80d2c599e7d2cff808049cdb9c7d39e1d0ee45
Elly <ellyjones@chromium.org>
2025-06-18 09:49:41 -0700

    device/fido: stop using ECSignatureCreator and friends
    This CL drops all uses of the deprecated ECSignatureCreator and
    ECPrivateKey APIs in devices/fido, replacing them with uses of
    crypto::sign and crypto::keypair respectively.
    
    This CL also drops a couple of stray uses of the deprecated crypto/sha2
    interfaces by migrating them to use crypto/hash.
    
    Bug: 372283556
    Change-Id: Ifdb780c002117ee0674a3f297dc4e5e298a0a126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653904
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475667}

--- device/fido/enclave/enclave_discovery.h
+++ device/fido/enclave/enclave_discovery.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "crypto/ec_private_key.h"
 #include "device/fido/enclave/types.h"
 #include "device/fido/fido_discovery_base.h"
 #include "services/network/public/mojom/network_context.mojom.h"
--- device/fido/virtual_fido_device.h
+++ device/fido/virtual_fido_device.h
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/public_key_credential_user_entity.h"
 #include "third_party/boringssl/src/include/openssl/base.h"
 
-namespace crypto {
-class ECPrivateKey;
-}
-
 namespace device {
 
 struct PublicKey;
@@ -405,10 +401,6 @@ class COMPONENT_EXPORT(DEVICE_FIDO) VirtualFidoDevice : public FidoDevice {
   static std::vector<uint8_t> GetAttestationKey();
 
   scoped_refptr<State> NewReferenceToState() const { return state_; }
-
-  static bool Sign(crypto::ECPrivateKey* private_key,
-                   base::span<const uint8_t> sign_buffer,
-                   std::vector<uint8_t>* signature);
 
   // Constructs certificate encoded in X.509 format to be used for packed
   // attestation statement and FIDO-U2F attestation statement.
