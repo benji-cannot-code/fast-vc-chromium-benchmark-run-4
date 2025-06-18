eb811d4f5a9201552d964ef7f5c2c08f2f3fec02
Chengwei Hsieh <chengweih@google.com>
2025-06-18 11:37:18 -0700

    shape_detection: Use ChromeShapeDetectionAPI for barcode detection
    This moves barcode detection on ChromeOS to use ChromeShapeDetectionAPI
    library from Barhopper library.
    
    Because shape detection service runs on utility process on ChromeOS, it
    is required to create a new sandbox type for shape detection service in
    order to load the ChromeShapeDetectionAPI library dynamically.
    
    This CL also adds a mapping from services/shape_detection/internal to
    https://chrome-internal.googlesource.com/chrome/services/shape_detection
    
    Bug: 418576397
    Change-Id: Ibcf79785cdd4d2a5f5897398b21d0a5c7d76defc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6582202
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475744}

--- services/shape_detection/barcode_detection_impl_chrome.h
+++ services/shape_detection/barcode_detection_impl_chrome.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_SHAPE_DETECTION_BARCODE_DETECTION_IMPL_CHROME_H_
 #define SERVICES_SHAPE_DETECTION_BARCODE_DETECTION_IMPL_CHROME_H_
 
+#include "services/shape_detection/chrome_shape_detection_api.h"
 #include "services/shape_detection/public/mojom/barcodedetection.mojom.h"
 #include "services/shape_detection/public/mojom/barcodedetection_provider.mojom.h"
-#include "third_party/barhopper/barhopper/barhopper.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
 namespace shape_detection {
@@ -30,7 +30,7 @@ class BarcodeDetectionImplChrome : public mojom::BarcodeDetection {
   GetSupportedFormats();
 
  private:
-  barhopper::RecognitionOptions recognition_options_;
+  const ChromeBarcodeFormat expected_formats_;
 };
 
 }  // namespace shape_detection
