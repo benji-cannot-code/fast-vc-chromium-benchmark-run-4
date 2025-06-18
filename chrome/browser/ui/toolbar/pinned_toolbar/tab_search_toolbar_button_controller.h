f071688c3051aae9d9fc92f13264244a9b91a048
Antonio Gomes <tonikitoo@igalia.com>
2025-06-18 13:13:49 -0700

    Factor TabSearchBubbleHost::Observer out
    ... temporarily, into its own class and header.
    
    This will unblock the componentization of
    tab_search_toolbar_button_controller.h. This header includes
    tab_search_bubble_host.h, that includes various other headers not
    componentized yet.
    
    By temporarily placing TabSearchBubbleHost::Observer class into its
    own outer class / header (namely TabSearchBubbleHostObserver and
    tab_search_bubble_host_observer.h respectively),
    tab_search_toolbar_button_controller.h can include it and not
    (the problematic) tab_search_bubble_host.h.
    
    R=dljames@chromium.org
    
    Bug: 369436587
    Change-Id: Ida94dddc9c418608500492fc92f0ba5dc2e268cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651292
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1475819}

--- chrome/browser/ui/toolbar/pinned_toolbar/tab_search_toolbar_button_controller.h
+++ chrome/browser/ui/toolbar/pinned_toolbar/tab_search_toolbar_button_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/ui/views/tab_search_bubble_host.h"
+#include "chrome/browser/ui/views/tab_search_bubble_host_observer.h"
 
 namespace actions {
 class ActionItem;
@@ -17,7 +18,7 @@ class ActionItem;
 
 class BrowserView;
 
-class TabSearchToolbarButtonController : public TabSearchBubbleHost::Observer {
+class TabSearchToolbarButtonController : public TabSearchBubbleHostObserver {
  public:
   TabSearchToolbarButtonController(BrowserView* browser_view,
                                    TabSearchBubbleHost* tab_search_bubble_host);
@@ -28,7 +29,7 @@ class TabSearchToolbarButtonController : public TabSearchBubbleHost::Observer {
   TabSearchToolbarButtonController& operator=(
       const TabSearchToolbarButtonController&) = delete;
 
-  // TabSearchBubbleHost::Observer:
+  // TabSearchBubbleHostObserver:
   void OnBubbleInitializing() override;
   void OnBubbleDestroying() override;
 
@@ -45,7 +46,7 @@ class TabSearchToolbarButtonController : public TabSearchBubbleHost::Observer {
   // into the controller.
   const raw_ptr<BrowserView> browser_view_;
 
-  base::ScopedObservation<TabSearchBubbleHost, TabSearchBubbleHost::Observer>
+  base::ScopedObservation<TabSearchBubbleHost, TabSearchBubbleHostObserver>
       tab_search_bubble_host_observation_{this};
 
   base::WeakPtrFactory<TabSearchToolbarButtonController> weak_ptr_factory_{
--- chrome/browser/ui/views/tab_search_bubble_host.cc
+++ chrome/browser/ui/views/tab_search_bubble_host.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/tab_search_bubble_host_observer.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
 #include "chrome/browser/ui/webui/tab_search/tab_search_prefs.h"
@@ -220,11 +221,12 @@ void TabSearchBubbleHost::BeforeBubbleWidgetShowed(views::Widget* widget) {
           base::TimeTicks::Now()));
 }
 
-void TabSearchBubbleHost::AddObserver(Observer* observer) {
+void TabSearchBubbleHost::AddObserver(TabSearchBubbleHostObserver* observer) {
   observers_.AddObserver(observer);
 }
 
-void TabSearchBubbleHost::RemoveObserver(Observer* observer) {
+void TabSearchBubbleHost::RemoveObserver(
+    TabSearchBubbleHostObserver* observer) {
   observers_.RemoveObserver(observer);
 }
 
