a8494e388709be05c2193b5563d922b59cc7ffe2
Christopher Whelan <chwhelan@google.com>
2025-06-18 01:53:36 -0700

    [fuchsia] Update reboot APIs part 2
    crrev.com/c/6651293 updated the reboot APIs in source but neglected to
    update the tests.
    
    Bug: 416304988
    Change-Id: Icd5ca73c80fe694645ea38618d0732a76ddffb3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652121
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475451}

--- chromecast/system/reboot/reboot_fuchsia_test.cc
+++ chromecast/system/reboot/reboot_fuchsia_test.cc
@@ -44,8 +44,9 @@ using ::testing::Eq;
 using ::testing::Ne;
 
 using fuchsia::feedback::RebootReason;
+using fuchsia::hardware::power::statecontrol::RebootOptions;
 using StateControlRebootReason =
-    fuchsia::hardware::power::statecontrol::RebootReason;
+    fuchsia::hardware::power::statecontrol::RebootReason2;
 
 struct RebootReasonParam {
   RebootReason reason;
@@ -104,8 +105,12 @@ class FakeAdmin
   }
 
  private:
-  void Reboot(StateControlRebootReason reason, RebootCallback callback) final {
-    last_reboot_reason_ = reason;
+  void PerformReboot(RebootOptions options,
+                     PerformRebootCallback callback) final {
+    if (options.has_reasons() && !options.reasons().empty()) {
+      last_reboot_reason_ = options.reasons()[0];
+    }
+
     callback(fpromise::ok());
   }
 
