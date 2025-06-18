e063b0a911d6d03f53648add60a390b54d07973f
Hidehiko Abe <hidehiko@chromium.org>
2025-06-17 23:52:03 -0700

    Remove MultiCaptureServiceAsh crosapi
    The implementation is used only by capture_policy_utils, and
    this CL simplified the code, too.
    
    BUG=354842935
    TEST=Tryjob
    
    Change-Id: I9dafb19875b54c3375784d00e64e119754645c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642818
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475415}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/local_printer_ash.h"
 #include "chrome/browser/ash/crosapi/login_ash.h"
 #include "chrome/browser/ash/crosapi/media_ui_ash.h"
-#include "chrome/browser/ash/crosapi/multi_capture_service_ash.h"
 #include "chrome/browser/ash/crosapi/parent_access_ash.h"
 #include "chrome/browser/ash/crosapi/vpn_service_ash.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_factory.h"
@@ -53,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/local_printer.mojom.h"
 #include "chromeos/crosapi/mojom/magic_boost.mojom.h"
 #include "chromeos/crosapi/mojom/mahi.mojom.h"
-#include "chromeos/crosapi/mojom/multi_capture_service.mojom.h"
 #include "chromeos/crosapi/mojom/telemetry_diagnostic_routine_service.mojom.h"
 #include "chromeos/services/chromebox_for_meetings/public/cpp/service_connection.h"
 #include "chromeos/services/chromebox_for_meetings/public/mojom/cfm_service_manager.mojom.h"
@@ -104,7 +102,6 @@ CrosapiAsh::CrosapiAsh()
       local_printer_ash_(std::make_unique<LocalPrinterAsh>()),
       login_ash_(std::make_unique<LoginAsh>()),
       media_ui_ash_(std::make_unique<MediaUIAsh>()),
-      multi_capture_service_ash_(std::make_unique<MultiCaptureServiceAsh>()),
       parent_access_ash_(std::make_unique<ParentAccessAsh>()),
       telemetry_diagnostic_routine_service_ash_(
           std::make_unique<ash::TelemetryDiagnosticsRoutineServiceAsh>()),
@@ -235,11 +232,6 @@ void CrosapiAsh::BindMediaSessionController(
         receiver) {
   content::GetMediaSessionService().BindMediaControllerManager(
       std::move(receiver));
-}
-
-void CrosapiAsh::BindMultiCaptureService(
-    mojo::PendingReceiver<mojom::MultiCaptureService> receiver) {
-  multi_capture_service_ash_->BindReceiver(std::move(receiver));
 }
 
 void CrosapiAsh::BindNetworkChange(
