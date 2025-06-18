88a818b63b20133e1b5c98d8ff79f2f5e940dcab
Haihan Chen <haihan@google.com>
2025-06-18 12:30:25 -0700

    Query last replaced clipboard types due to Data controls policy
    Fix issue where Data Controls rule could prevent pasting complex content by ensuring `ClipboardHostImpl` is aware of clipboard types of the actual paste content before it got replaced.
    
    Bug: 413062244
    Change-Id: I9dbd9359752d813a68c9178a1ea778bd8eee31d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6622036
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475781}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -233,6 +233,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/common/proto/connectors.pb.h"
 #include "components/enterprise/content/clipboard_restriction_service.h"
 #include "components/enterprise/content/pref_names.h"
+#include "components/enterprise/data_controls/content/browser/last_replaced_clipboard_data.h"
 #include "components/error_page/common/error.h"
 #include "components/error_page/common/error_page_switches.h"
 #include "components/error_page/common/localized_error.h"
@@ -8800,4 +8801,17 @@ ChromeContentBrowserClient::MaybeCreateKeepAliveRequestTracker(
         is_context_detached_callback) {
   return ChromeKeepAliveRequestTracker::MaybeCreateKeepAliveRequestTracker(
       request, ukm_source_id, std::move(is_context_detached_callback));
+}
+
+std::optional<std::vector<std::u16string>>
+ChromeContentBrowserClient::GetClipboardTypesIfPolicyApplied(
+    const ui::ClipboardSequenceNumberToken& seqno) {
+  const data_controls::LastReplacedClipboardData& last_replaced_data =
+      data_controls::GetLastReplacedClipboardData();
+
+  if (last_replaced_data.seqno == seqno) {
+    return last_replaced_data.GetAvailableTypes();
+  }
+
+  return std::nullopt;
 }
