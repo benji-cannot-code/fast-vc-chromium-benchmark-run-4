74ff8505eb29b1877363c2e63c7e987161551d61
Noah Rose Ledesma <noahrose@google.com>
2025-06-18 09:29:43 -0700

    Import //third_party/enterprise_companion
    Add a third-party package for the Chrome Enterprise Companion App to
    permit integration testing against previously released versions of the
    binary.
    
    Bug: 406578163
    Change-Id: I10f221958b3a96d93a0ce4fec094ef475a6236d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6405936
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475656}

---
+++ third_party/enterprise_companion/chromium_mac_amd64/3pp/fetch.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import fetch_lib
+import sys
+
+PLATFORM = 'Mac'
+MIN_VERSION = 1439396
+
+if __name__ == '__main__':
+    sys.exit(fetch_lib.main(PLATFORM, MIN_VERSION))
