abbe1e7a6f1077615118a5aa8d593cb2f8e20d33
Eshwar Stalin <estalin@chromium.org>
2025-06-18 10:22:12 -0700

    Fixing a potential crash in incognito mode
    Change-Id: I264d2c1dc73768b339a8fe4be43d9e742e5b646d
    Fixed: 424382564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652317
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475690}

--- chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.cc
+++ chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.cc
@@ -481,10 +481,13 @@ BookmarkManagerPrivateCanPasteFunction::RunOnReady() {
 
 ExtensionFunction::ResponseValue
 BookmarkManagerPrivateIsActiveTabInSplitFunction::RunOnReady() {
-  Browser* browser = ChromeExtensionFunctionDetails(this)
-                         .GetCurrentWindowController()
-                         ->GetBrowser();
+  WindowController* window_controller =
+      ChromeExtensionFunctionDetails(this).GetCurrentWindowController();
+  if (!window_controller) {
+    return Error(ExtensionTabUtil::kNoCurrentWindowError);
+  }
 
+  Browser* browser = window_controller->GetBrowser();
   if (!browser) {
     return Error(kInvalidBrowserError);
   }
@@ -772,9 +775,13 @@ BookmarkManagerPrivateOpenInNewTabGroupFunction::RunOnReady() {
     return BadMessage();
   }
 
-  Browser* browser = ChromeExtensionFunctionDetails(this)
-                         .GetCurrentWindowController()
-                         ->GetBrowser();
+  WindowController* window_controller =
+      ChromeExtensionFunctionDetails(this).GetCurrentWindowController();
+  if (!window_controller) {
+    return Error(ExtensionTabUtil::kNoCurrentWindowError);
+  }
+
+  Browser* browser = window_controller->GetBrowser();
   if (!browser) {
     return Error(kInvalidBrowserError);
   }
