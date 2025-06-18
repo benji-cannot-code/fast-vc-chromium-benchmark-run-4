a5da6a2196ac2a41427dd0b3c18815e4a9a39740
Diana Qu <xiaqu@microsoft.com>
2025-06-18 14:39:14 -0700

    Update ValidateCommitOriginTest params with more validate value.
    Make sure ValidateCommitOriginTest use reasonable DescribeParams.
    
    Bug: 41492620, 425427368
    Change-Id: Ib5795b562e73a904e6becc21cdcfeb9a8160bb98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651304
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Diana Qu <xiaqu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1475862}

--- content/browser/renderer_host/navigation_controller_impl_browsertest.cc
+++ content/browser/renderer_host/navigation_controller_impl_browsertest.cc
@@ -23495,7 +23495,7 @@ INSTANTIATE_TEST_SUITE_P(
     ValidateCommitOriginTest,
     testing::Combine(testing::ValuesIn(RenderDocumentFeatureLevelValues()),
                      testing::Bool()),
-    ValidateCommitOriginTest::DescribeParams);
+    NavigationControllerBrowserTest::DescribeParams);
 INSTANTIATE_TEST_SUITE_P(
     All,
     LoadDataWithBaseURLWithPossiblyEmptyURLsBrowserTest,
