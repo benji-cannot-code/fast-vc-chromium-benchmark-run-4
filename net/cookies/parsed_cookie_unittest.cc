6db21e477840a4bd342994c4fc06effe0d124ebc
Chris Fredrickson <cfredric@google.com>
2025-06-18 14:28:11 -0700

    Remove Has* methods from ParsedCookie
    We can support the same functionality/semantics via a simpler API (fewer
    methods with richer return values), and also remove undocumented
    DCHECKs.
    
    This CL also modernizes to use std::string_view instead of const
    std::string& in a few places.
    
    Change-Id: I4399418421b1452c736aa82a01addf05c1a68b65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651629
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475857}

--- net/cookies/parsed_cookie_unittest.cc
+++ net/cookies/parsed_cookie_unittest.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/parsed_cookie.h"
 
 #include <array>
+#include <optional>
 #include <string>
+#include <string_view>
 
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
@@ -26,10 +28,10 @@ TEST(ParsedCookieTest, TestBasic) {
   EXPECT_FALSE(pc1.IsPartitioned());
   EXPECT_EQ("a", pc1.Name());
   EXPECT_EQ("b", pc1.Value());
-  EXPECT_FALSE(pc1.HasPath());
-  EXPECT_FALSE(pc1.HasDomain());
-  EXPECT_FALSE(pc1.HasExpires());
-  EXPECT_FALSE(pc1.HasMaxAge());
+  EXPECT_FALSE(pc1.Path());
+  EXPECT_FALSE(pc1.Domain());
+  EXPECT_FALSE(pc1.Expires());
+  EXPECT_FALSE(pc1.MaxAge());
   EXPECT_EQ(CookieSameSite::UNSPECIFIED, pc1.SameSite());
   EXPECT_EQ(CookiePriority::COOKIE_PRIORITY_DEFAULT, pc1.Priority());
 
@@ -42,12 +44,9 @@ TEST(ParsedCookieTest, TestBasic) {
   EXPECT_TRUE(pc2.IsPartitioned());
   EXPECT_EQ("c", pc2.Name());
   EXPECT_EQ("d", pc2.Value());
-  EXPECT_TRUE(pc2.HasPath());
   EXPECT_EQ("/foo", pc2.Path());
-  EXPECT_TRUE(pc2.HasDomain());
   EXPECT_EQ("bar.test", pc2.Domain());
-  EXPECT_FALSE(pc2.HasExpires());
-  EXPECT_TRUE(pc2.HasMaxAge());
+  EXPECT_FALSE(pc2.Expires());
   EXPECT_EQ("60", pc2.MaxAge());
   EXPECT_EQ(CookieSameSite::LAX_MODE, pc2.SameSite());
   EXPECT_EQ(CookiePriority::COOKIE_PRIORITY_HIGH, pc2.Priority());
@@ -190,7 +189,6 @@ TEST(ParsedCookieTest, TestQuoted) {
     EXPECT_TRUE(pc.IsValid());
     EXPECT_FALSE(pc.IsSecure());
     EXPECT_TRUE(pc.IsHttpOnly());
-    EXPECT_TRUE(pc.HasPath());
     EXPECT_EQ("aBc", pc.Name());
     EXPECT_EQ(test.expected, pc.Value());
 
@@ -208,7 +206,6 @@ TEST(ParsedCookieTest, TestNameless) {
   ParsedCookie pc("BLAHHH; path=/; secure;");
   EXPECT_TRUE(pc.IsValid());
   EXPECT_TRUE(pc.IsSecure());
-  EXPECT_TRUE(pc.HasPath());
   EXPECT_EQ("/", pc.Path());
   EXPECT_EQ("", pc.Name());
   EXPECT_EQ("BLAHHH", pc.Value());
@@ -224,7 +221,6 @@ TEST(ParsedCookieTest, TestAttributeCase) {
   EXPECT_TRUE(pc.IsHttpOnly());
   EXPECT_TRUE(pc.IsPartitioned());
   EXPECT_EQ(CookieSameSite::LAX_MODE, pc.SameSite());
-  EXPECT_TRUE(pc.HasPath());
   EXPECT_EQ("/", pc.Path());
   EXPECT_EQ("", pc.Name());
   EXPECT_EQ("BLAH", pc.Value());
@@ -236,7 +232,6 @@ TEST(ParsedCookieTest, TestDoubleQuotedNameless) {
   ParsedCookie pc("\"BLA\\\"HHH\"; path=/; secure;");
   EXPECT_TRUE(pc.IsValid());
   EXPECT_TRUE(pc.IsSecure());
-  EXPECT_TRUE(pc.HasPath());
   EXPECT_EQ("/", pc.Path());
   EXPECT_EQ("", pc.Name());
   EXPECT_EQ("\"BLA\\\"HHH\"", pc.Value());
@@ -287,7 +282,6 @@ TEST(ParsedCookieTest, MissingValue) {
   EXPECT_TRUE(pc.IsValid());
   EXPECT_EQ("ABC", pc.Name());
   EXPECT_EQ("", pc.Value());
-  EXPECT_TRUE(pc.HasPath());
   EXPECT_EQ("/wee", pc.Path());
   EXPECT_EQ(COOKIE_PRIORITY_DEFAULT, pc.Priority());
   EXPECT_EQ(1U, pc.NumberOfAttributes());
@@ -302,8 +296,8 @@ TEST(ParsedCookieTest, Whitespace) {
   EXPECT_TRUE(pc.IsValid());
   EXPECT_EQ("A", pc.Name());
   EXPECT_EQ("BC", pc.Value());
-  EXPECT_FALSE(pc.HasPath());
-  EXPECT_FALSE(pc.HasDomain());
+  EXPECT_FALSE(pc.Path());
+  EXPECT_FALSE(pc.Domain());
   EXPECT_TRUE(pc.IsSecure());
   EXPECT_FALSE(pc.IsHttpOnly());
   EXPECT_EQ(CookieSameSite::LAX_MODE, pc.SameSite());
@@ -317,8 +311,8 @@ TEST(ParsedCookieTest, MultipleEquals) {
   EXPECT_TRUE(pc.IsValid());
   EXPECT_EQ("A", pc.Name());
   EXPECT_EQ("== BC", pc.Value());
-  EXPECT_FALSE(pc.HasPath());
-  EXPECT_FALSE(pc.HasDomain());
+  EXPECT_FALSE(pc.Path());
+  EXPECT_FALSE(pc.Domain());
   EXPECT_TRUE(pc.IsSecure());
   EXPECT_TRUE(pc.IsHttpOnly());
   EXPECT_EQ(CookieSameSite::UNSPECIFIED, pc.SameSite());
@@ -335,8 +329,7 @@ TEST(ParsedCookieTest, QuotedTrailingWhitespace) {
   EXPECT_EQ("ANCUUID", pc.Name());
   // Stripping whitespace after the quotes matches all other major browsers.
   EXPECT_EQ("\"zohNumRKgI0oxyhSsV3Z7D\"", pc.Value());
-  EXPECT_TRUE(pc.HasExpires());
-  EXPECT_TRUE(pc.HasPath());
+  EXPECT_TRUE(pc.Expires());
   EXPECT_EQ("/", pc.Path());
   EXPECT_EQ(COOKIE_PRIORITY_DEFAULT, pc.Priority());
   EXPECT_EQ(2U, pc.NumberOfAttributes());
@@ -350,8 +343,7 @@ TEST(ParsedCookieTest, TrailingWhitespace) {
   EXPECT_TRUE(pc.IsValid());
   EXPECT_EQ("ANCUUID", pc.Name());
   EXPECT_EQ("zohNumRKgI0oxyhSsV3Z7D", pc.Value());
-  EXPECT_TRUE(pc.HasExpires());
-  EXPECT_TRUE(pc.HasPath());
+  EXPECT_TRUE(pc.Expires());
   EXPECT_EQ("/", pc.Path());
   EXPECT_EQ(COOKIE_PRIORITY_DEFAULT, pc.Priority());
   EXPECT_EQ(2U, pc.NumberOfAttributes());
@@ -449,12 +441,11 @@ TEST(ParsedCookieTest, EnforceSizeConstraints) {
 
   ParsedCookie pc20("name=value; path=" + max_path);
   EXPECT_TRUE(pc20.IsValid());
-  EXPECT_TRUE(pc20.HasPath());
   EXPECT_EQ("/" + almost_max_path, pc20.Path());
 
   ParsedCookie pc21("name=value; path=" + too_long_path, &status);
   EXPECT_TRUE(pc21.IsValid());
-  EXPECT_FALSE(pc21.HasPath());
+  EXPECT_FALSE(pc21.Path());
   EXPECT_TRUE(
       status.HasWarningReason(CookieInclusionStatus::WarningReason::
                                   WARN_ATTRIBUTE_VALUE_EXCEEDS_MAX_SIZE));
@@ -469,12 +460,11 @@ TEST(ParsedCookieTest, EnforceSizeConstraints) {
 
   ParsedCookie pc30("name=value; domain=" + max_domain);
   EXPECT_TRUE(pc30.IsValid());
-  EXPECT_TRUE(pc30.HasDomain());
   EXPECT_EQ(max_domain, pc30.Domain());
 
   ParsedCookie pc31("name=value; domain=" + too_long_domain);
   EXPECT_TRUE(pc31.IsValid());
-  EXPECT_FALSE(pc31.HasDomain());
+  EXPECT_FALSE(pc31.Domain());
   EXPECT_TRUE(
       status.HasWarningReason(CookieInclusionStatus::WarningReason::
                                   WARN_ATTRIBUTE_VALUE_EXCEEDS_MAX_SIZE));
@@ -722,13 +712,13 @@ TEST(ParsedCookieTest, SetAttributes) {
 
   // Clear an unset attribute.
   EXPECT_TRUE(pc.SetDomain(std::string()));
-  EXPECT_FALSE(pc.HasDomain());
+  EXPECT_FALSE(pc.Domain());
   EXPECT_EQ("name=value", pc.ToCookieLine());
   EXPECT_TRUE(pc.IsValid());
 
   // Set a string containing an invalid character
   EXPECT_FALSE(pc.SetDomain("foo;bar"));
-  EXPECT_FALSE(pc.HasDomain());
+  EXPECT_FALSE(pc.Domain());
   EXPECT_EQ("name=value", pc.ToCookieLine());
   EXPECT_TRUE(pc.IsValid());
 
@@ -748,10 +738,10 @@ TEST(ParsedCookieTest, SetAttributes) {
       "expires=Sun, 18-Apr-2027 21:06:29 GMT; max-age=12345; secure; "
       "httponly; samesite=LAX; priority=HIGH; partitioned",
       pc.ToCookieLine());
-  EXPECT_TRUE(pc.HasDomain());
-  EXPECT_TRUE(pc.HasPath());
-  EXPECT_TRUE(pc.HasExpires());
-  EXPECT_TRUE(pc.HasMaxAge());
+  EXPECT_TRUE(pc.Domain());
+  EXPECT_TRUE(pc.Path());
+  EXPECT_TRUE(pc.Expires());
+  EXPECT_TRUE(pc.MaxAge());
   EXPECT_TRUE(pc.IsSecure());
   EXPECT_TRUE(pc.IsHttpOnly());
   EXPECT_EQ(CookieSameSite::LAX_MODE, pc.SameSite());
@@ -759,10 +749,9 @@ TEST(ParsedCookieTest, SetAttributes) {
 
   // Modify one attribute in the middle.
   EXPECT_TRUE(pc.SetPath("/foo"));
-  EXPECT_TRUE(pc.HasDomain());
-  EXPECT_TRUE(pc.HasPath());
+  EXPECT_TRUE(pc.Domain());
   EXPECT_EQ("/foo", pc.Path());
-  EXPECT_TRUE(pc.HasExpires());
+  EXPECT_TRUE(pc.Expires());
   EXPECT_TRUE(pc.IsSecure());
   EXPECT_TRUE(pc.IsHttpOnly());
   EXPECT_EQ(
@@ -800,10 +789,10 @@ TEST(ParsedCookieTest, SetAttributes) {
   EXPECT_TRUE(pc.SetName("name2"));
   EXPECT_TRUE(pc.SetValue("value2"));
   EXPECT_TRUE(pc.SetPriority(std::string()));
-  EXPECT_FALSE(pc.HasDomain());
-  EXPECT_FALSE(pc.HasPath());
-  EXPECT_FALSE(pc.HasExpires());
-  EXPECT_FALSE(pc.HasMaxAge());
+  EXPECT_FALSE(pc.Domain());
+  EXPECT_FALSE(pc.Path());
+  EXPECT_FALSE(pc.Expires());
+  EXPECT_FALSE(pc.MaxAge());
   EXPECT_FALSE(pc.IsSecure());
   EXPECT_FALSE(pc.IsHttpOnly());
   EXPECT_EQ(CookieSameSite::UNSPECIFIED, pc.SameSite());
