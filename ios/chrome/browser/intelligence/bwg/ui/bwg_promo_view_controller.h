9bc7430a6e6a06f46ee5826a704c8171298e0b9f
“Prasanaa” <prasanaa@google.com>
2025-06-18 07:09:20 -0700

    [iOS] Implement BWG Promo UI
    This CL implements the BWG Promo UI.
    
    Bug: 414375434
    Change-Id: If85980cda00ffcb1fbc58636c317dab2ce469cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650970
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475554}

--- ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller.h
+++ ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol BWGPromoViewControllerDelegate;
 
 // BWG consent View Controller.
-@interface BWGPromoViewController : PromoStyleViewController
+@interface BWGPromoViewController : UIViewController
 
 // The mutator for this view controller to communicate to the mediator.
 @property(nonatomic, weak) id<BWGConsentMutator> mutator;
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // NavigationController.
 @property(nonatomic, weak) id<BWGPromoViewControllerDelegate> BWGPromoDelegate;
 
-// Content height of the bottom sheet.
+// Content height of the BWG Promo UI.
 - (CGFloat)contentHeight;
 
 @end
