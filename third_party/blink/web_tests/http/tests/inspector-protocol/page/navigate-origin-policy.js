3d9d8e9c404bfe5b893839785fc8ab17a7346d0c
Alex Rudenko <alexrudenko@chromium.org>
2025-06-18 01:48:38 -0700

    DevTools: fix referrerPolicy validation in Page.navigate
    Invalid values were causing a DCHECK failure. Instead
    we should validate and return an error message to the
    client.
    
    Bug: none
    Change-Id: I6f6c555d21c30326bb8bb47e0abb51286f65f6f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651518
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Commit-Queue: Maksim Sadym <sadym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475449}

--- third_party/blink/web_tests/http/tests/inspector-protocol/page/navigate-origin-policy.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/navigate-origin-policy.js
@@ -1,20 +1,27 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
-  var {page, session, dp} = await testRunner.startBlank(
+  const { dp } = await testRunner.startBlank(
       `Tests that Page.navigate() returns the results that match referrer-policy configs`);
 
   await dp.Page.enable();
   await dp.Network.enable();
-  var navigatePromise = dp.Page.navigate({
+
+  testRunner.log(await dp.Page.navigate({
+    url: 'http://example.com',
+    referrer: 'https://www.google.com/search?q=bing',
+    referrerPolicy: 'no-referrer',
+  }), 'Invalid policy: ');
+
+  dp.Page.navigate({
     url: 'http://example.com',
     referrer: 'https://www.google.com/search?q=bing',
     referrerPolicy: 'origin',
   });
-  var requestWillBeSent = dp.Network.onRequestWillBeSent(event => {
+  dp.Network.onRequestWillBeSent(event => {
     const request = event.params.request;
     testRunner.log('Request referrer: ' + request.headers.Referer);
     testRunner.log('Request referrer-policy: ' + request.referrerPolicy);
   });
-  const params = (await dp.Network.onceRequestWillBeSent()).params;
+  await dp.Network.onceRequestWillBeSent();
   testRunner.completeTest();
 })
