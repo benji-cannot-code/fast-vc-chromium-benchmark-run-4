20f41bd42eec663fea57fd144a771cb4b556234c
Lauren Winston <lwinston@google.com>
2025-06-18 14:33:10 -0700

    [Read anything] Fix variable rename to receive scroll events.
    Fixed: 425944147
    Change-Id: I5aa465476ebda61501fdc44bef6a8391c1daa345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6655278
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475859}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -40,8 +40,8 @@ export interface AppElement {
     toolbar: ReadAnythingToolbarElement,
     appFlexParent: HTMLElement,
     container: HTMLElement,
-    containerParent: HTMLElement,
     languageToast: LanguageToastElement,
+    containerScroller: HTMLElement,
   };
 }
 
@@ -240,7 +240,7 @@ export class AppElement extends AppElementBase implements
       }
     };
 
-    this.$.containerParent.onscroll = () => {
+    this.$.containerScroller.onscroll = () => {
       chrome.readingMode.onScroll(this.scrollingOnSelection_);
       this.scrollingOnSelection_ = false;
       if (this.isReadAloudEnabled_) {
