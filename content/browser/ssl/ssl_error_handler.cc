52438720d329ab2761093540df0d355632f9f059
David Sanders <dsanders11@ucsbalum.com>
2025-06-18 07:56:40 -0700

    Remove unused includes in ssl_error_handler.cc
    Bug: 40318405
    Change-Id: Ibebf425addcc4274d8fdaa6ba1905105ac9c878b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654226
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1475582}

--- content/browser/ssl/ssl_error_handler.cc
+++ content/browser/ssl/ssl_error_handler.cc
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/ssl/ssl_error_handler.h"
 
-#include "content/browser/renderer_host/navigation_controller_impl.h"
-#include "content/public/browser/browser_task_traits.h"
+#include "base/memory/weak_ptr.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/net_errors.h"
-#include "net/cert/cert_status_flags.h"
 
 using net::SSLInfo;
 
