caf6b5b2c95438d559820030084cee3506c8fa44
Harry Nguyen <nguyenharry@google.com>
2025-06-18 12:27:59 -0700

    [PS Internals] Make page always visible and gate TPCD_METADATA_GRANTS
    Key Changes:
    - `chrome/browser/chrome_browser_interface_binders_webui.cc`: Remove feature flag check for Privacy Sandbox Internals. The Privacy Sandbox Internals page is now enabled by default and no longer requires a flag to be visible.While the main page is now always shown, the display of sensitive information within it, such as TPCD_METADATA_GRANTS, remains controlled by its own feature flag.
    
    - `chrome/browser/resources/privacy_sandbox/internals/index.ts`: Introduced a flag check to control the rendering and loading of the "TPCD Metadata" tab. This ensures the tab is only visible and loaded when the relevant feature flag is enabled.
    
    - `chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.cc`: Exposed the value of a feature flag to the UI. This enables the frontend to dynamically adapt its behavior based on the flag's state.
    
    - Tests: Added new tests specifically for the logic governing the TPCD tab's visibility and functionality. Existing tests were also updated to reflect these changes and ensure continued correctness.
    
    **Before:** https://screenshot.googleplex.com/955MTXuL8HkPV8S
    
    **After:** https://screenshot.googleplex.com/4hn8SGCtLZ8ivq4
    https://screenshot.googleplex.com/7PyWoW4myhFxFEg
    https://screenshot.googleplex.com/AQNwat4Zbh7UgKU
    
    Bug: 420639987
    Change-Id: I4466425e4640eb2cc30df272282c5b9074421f3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612220
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Harry Nguyen <nguyenharry@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475777}

--- chrome/browser/resources/privacy_sandbox/internals/privacy_sandbox_internals_browser_proxy.ts
+++ chrome/browser/resources/privacy_sandbox/internals/privacy_sandbox_internals_browser_proxy.ts
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import '/strings.m.js';
+
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import type {PageHandlerRemote} from './privacy_sandbox_internals.mojom-webui.js';
 import {PageHandler} from './privacy_sandbox_internals.mojom-webui.js';
@@ -11,6 +14,10 @@ export class PrivacySandboxInternalsBrowserProxy {
 
   constructor() {
     this.handler = PageHandler.getRemote();
+  }
+
+  shouldShowTpcdMetadataGrants(): boolean {
+    return loadTimeData.getBoolean('isPrivacySandboxInternalsDevUIEnabled');
   }
 
   static getInstance(): PrivacySandboxInternalsBrowserProxy {
