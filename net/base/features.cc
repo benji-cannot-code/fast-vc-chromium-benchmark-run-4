8449f9de5ce28e5eb4b7812b9c1bf18c3a5bd3f6
Antonio Alphonse <alphonsea@google.com>
2025-06-18 08:39:17 -0700

    [IPP Devtools] Add Feature Param for IPP DevTools UI Changes.
    In order to slowly test out IP Protection information within DevTools,
    we create a feature param to make sure we can conditionally use it in
    Finch. This also ensures that DevTools IPP changes can only be enabled
    if set within kEnableIpProtectionProxy.
    
    See go/ipp-devtools-design for more info.
    Tracked at https://crbug.com/425645896.
    
    Change-Id: If95bf0759a7f18a11f5fa6cff4e269fd63381504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612182
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Antonio Alphonse <alphonsea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475619}

--- net/base/features.cc
+++ net/base/features.cc
@@ -534,6 +534,11 @@ const base::FeatureParam<bool> kIpPrivacyDisableForEnterpriseByDefault{
     /*name=*/"IpPrivacyDisableForEnterpriseByDefault",
     /*default_value=*/false};
 
+const base::FeatureParam<bool> kIpPrivacyEnableIppInDevTools{
+    &kEnableIpProtectionProxy,
+    /*name=*/"IpPrivacyEnableIppInDevTools",
+    /*default_value=*/false};
+
 BASE_FEATURE(kExcludeLargeBodyReports,
              "ExcludeLargeReportBodies",
              base::FEATURE_ENABLED_BY_DEFAULT);
