55ae82986b4ef375dca00231c4fa95e419c114ac
Michael Tatarski <mtatarski@google.com>
2025-06-19 01:45:04 -0700

    Allow more detailed UMA metric logging for sync errors
    This patch adds an enum value called model_error_type to the ModelError
    class. This allows more granular UMA reporting for sync errors,
    which helps us to identify specific errors that are causing sync to
    fail. It is optional to use this field, to allow for backwards
    compatibility.
    
    The new value value will be logged in UMA only if set in the ModelError.
    
    In subsequent CLs we need to adjust the existing sync datatypes to
    support this new error model interface.
    
    Bug: 40886237
    
    Change-Id: I20b27d01f1c6c76fd7d4c9a9398013b7ab8a5992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6641908
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1476060}

--- components/sync/service/data_type_controller.cc
+++ components/sync/service/data_type_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "components/sync/base/data_type.h"
@@ -332,6 +333,7 @@ DataTypeControllerDelegate* DataTypeController::GetDelegateForTesting(
 
 void DataTypeController::ReportModelError(const ModelError& error) {
   DCHECK(CalledOnValidThread());
+  LogModelErrorToHistogram(error);
 
   switch (state_) {
     case MODEL_LOADED:
@@ -384,6 +386,13 @@ void DataTypeController::RecordRunFailure() const {
   DCHECK(CalledOnValidThread());
   UMA_HISTOGRAM_ENUMERATION("Sync.DataTypeRunFailures2",
                             DataTypeHistogramValue(type()));
+}
+
+void DataTypeController::LogModelErrorToHistogram(
+    const ModelError& error) const {
+  DCHECK(CalledOnValidThread());
+  // TODO(crbug.com/40886237): add a histogram per sync data type in subsequent
+  // CL.
 }
 
 void DataTypeController::OnDelegateStarted(
