00d46450247401f3e06299d25da266c2be3d60b5
wangxiao <wangxiao@bytedance.com>
2025-06-18 20:58:58 -0700

    net/tools/cachetools: base/hash/md5 -> crypto/obsolete/md5
    Bug: 406729261
    Change-Id: Ib2ba5831c75d173733864925f2c048b3bdb57fb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651571
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Peng Zhou <zhoupeng.1996@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1475988}

--- net/tools/cachetool/cachetool.cc
+++ net/tools/cachetool/cachetool.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string_view>
 #include <unordered_map>
+#include <vector>
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_executor.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
+#include "crypto/obsolete/md5.h"
 #include "net/base/io_buffer.h"
 #include "net/base/test_completion_callback.h"
 #include "net/disk_cache/disk_cache.h"
@@ -35,6 +37,12 @@ using disk_cache::Backend;
 using disk_cache::BackendResult;
 using disk_cache::Entry;
 using disk_cache::EntryResult;
+
+namespace cachetool {
+crypto::obsolete::Md5 MakeMd5HasherForCachetools() {
+  return {};
+}
+}  // namespace cachetool
 
 namespace {
 
@@ -241,7 +249,8 @@ class StreamCommandMarshal final : public CommandMarshal {
       return "";
     std::cout.flush();
     size_t command_id = static_cast<size_t>(std::cin.get());
-    if (command_id >= std::size(kCommandNames)) {
+
+    if (command_id >= kCommandNames.size()) {
       ReturnFailure("Unknown command.");
       return "";
     }
@@ -384,8 +393,7 @@ std::string GetMD5ForResponseBody(disk_cache::Entry* entry) {
       base::MakeRefCounted<net::IOBufferWithSize>(kInitBufferSize);
   net::TestCompletionCallback cb;
 
-  base::MD5Context ctx;
-  base::MD5Init(&ctx);
+  crypto::obsolete::Md5 hasher = cachetool::MakeMd5HasherForCachetools();
 
   int bytes_read = 0;
   while (true) {
@@ -398,13 +406,11 @@ std::string GetMD5ForResponseBody(disk_cache::Entry* entry) {
     }
 
     if (rv == 0) {
-      base::MD5Digest digest;
-      base::MD5Final(&digest, &ctx);
-      return base::MD5DigestToBase16(digest);
+      return base::ToLowerASCII(base::HexEncode(hasher.Finish()));
     }
 
     bytes_read += rv;
-    base::MD5Update(&ctx, std::string_view(buffer->data(), rv));
+    hasher.Update(buffer->span());
   }
 
   NOTREACHED();
