598dd4fcff1d9efc126a380f203fa2cb2667c792
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-06-19 08:22:07 -0700

    Move `GetContentsView` to `ContentAnalysisDialogDelegate`
    Follow-up to crrev.com/c/6638114
    
    This CL moves the last `views::DialogDelegate` override from the
    controller class to `ContentAnalysisDialogDelegate`. The next few CLs
    should be able to break the dependency completely and remove the
    inheritance.
    
    Bug: 422111748
    Change-Id: If655f94e88771d35dffb5001a45c96d414348756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656659
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476173}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_dialog_controller.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_dialog_controller.h
@@ -107,9 +107,6 @@ class ContentAnalysisDialogController
           FinalContentAnalysisResult::SUCCESS,
       download::DownloadItem* download_item = nullptr);
 
-  // views::DialogDelegate:
-  views::View* GetContentsView() override;
-
   // content::WebContentsObserver:
   void WebContentsDestroyed() override;
   void PrimaryPageChanged(content::Page& page) override;
@@ -153,9 +150,6 @@ class ContentAnalysisDialogController
 
   // Helper function to determine whether dialog should be shown immediately.
   bool ShouldShowDialogNow();
-
-  // Returns a newly created side icon.
-  std::unique_ptr<views::View> CreateSideIcon();
 
   void AcceptButtonCallback();
   void CancelButtonCallback();
