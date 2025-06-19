8d6750a9e00df53229c4396bf791b6bd01aec8bb
Jeremy Roman <jbroman@chromium.org>
2025-06-18 19:09:18 -0700

    glic: Also throttle user status requests immediately after a throttled one
    carlosk@ identified a subtle (but minor) interaction which meant that
    it was previously possible for the intended rate limit to be exceeded
    (albeit only slightly). This addresses that issue.
    
    Bug: b:416561303
    Change-Id: I1c3245764d2f20e860fca7c1ed92d24a5fb23725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652640
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475972}

--- chrome/browser/glic/glic_user_status_fetcher.cc
+++ chrome/browser/glic/glic_user_status_fetcher.cc
@@ -355,7 +355,7 @@ void GlicUserStatusFetcher::UpdateUserStatusWithThrottling() {
         [](GlicUserStatusFetcher* self) {
           if (self->update_was_throttled_) {
             self->update_was_throttled_ = false;
-            self->UpdateUserStatus();
+            self->UpdateUserStatusWithThrottling();
           }
         },
         base::Unretained(this));
