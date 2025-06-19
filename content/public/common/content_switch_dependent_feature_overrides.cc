04484b24df1a451d990f54cd04a9e5cd9c6ca122
Domenic Denicola <domenic@chromium.org>
2025-06-18 18:19:36 -0700

    Make Cookie-Indices experimental
    To help reduce the number of prefetch-related virtual test suites, we
    mark the Cookie-Indices related features as dependent on experimental
    web platform features, so that they are on by default when running web
    platform tests.
    
    This requires moving the base::Feature declarations from
    prefetch_features.h to content_features.h so they can be referenced from
    content_switch_dependent_feature_overrides.cc.
    
    Bug: 328628231
    Change-Id: If48e2e8f48e01793d22a3c9549bb73d1421e2031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650063
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475954}

--- content/public/common/content_switch_dependent_feature_overrides.cc
+++ content/public/common/content_switch_dependent_feature_overrides.cc
@@ -70,6 +70,12 @@ GetSwitchDependentFeatureOverrides(const base::CommandLine& command_line) {
       {switches::kEnableExperimentalWebPlatformFeatures,
        std::cref(blink::features::kPartitionedPopins),
        base::FeatureList::OVERRIDE_ENABLE_FEATURE},
+      {switches::kEnableExperimentalWebPlatformFeatures,
+       std::cref(network::features::kCookieIndicesHeader),
+       base::FeatureList::OVERRIDE_ENABLE_FEATURE},
+      {switches::kEnableExperimentalWebPlatformFeatures,
+       std::cref(features::kPrefetchCookieIndices),
+       base::FeatureList::OVERRIDE_ENABLE_FEATURE},
 
       // Overrides for --enable-experimental-cookie-features.
       {switches::kEnableExperimentalCookieFeatures,
