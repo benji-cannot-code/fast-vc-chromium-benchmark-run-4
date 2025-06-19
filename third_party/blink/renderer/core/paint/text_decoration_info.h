371dc3ea80bf13e92adb18f1afe8ed0ac249a526
Fredrik Söderquist <fs@opera.com>
2025-06-19 01:27:44 -0700

    Move TextDecorationInfo::Bounds() to DecorationLinePainter
    This is where we want to end up eventually, and moving it will allow to
    reuse some of the helpers from the latter.
    
    Change the new function to use StrokeStyle values instead of
    ETextDecorationStyle. Fold BoundsForWavy() and make
    BoundsForDottedOrDashed() public for the time being (to be removed in a
    follow-up CL).
    
    Bug: 425866014
    Change-Id: I0ec092b5efcb25a6fbe0fb6d745c77beb478089a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653659
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1476050}

--- third_party/blink/renderer/core/paint/text_decoration_info.h
+++ third_party/blink/renderer/core/paint/text_decoration_info.h
@@ -145,6 +145,7 @@ class CORE_EXPORT TextDecorationInfo {
 
   // Compute bounds for the given line and the current decoration.
   gfx::RectF Bounds() const;
+  gfx::RectF BoundsForDottedOrDashed() const;
 
   // Returns tile record and coordinates for wavy decorations.
   cc::PaintRecord WavyTileRecord() const;
@@ -166,8 +167,6 @@ class CORE_EXPORT TextDecorationInfo {
   void ComputeWavyLineData(gfx::RectF& pattern_rect,
                            cc::PaintRecord& tile_record) const;
 
-  gfx::RectF BoundsForDottedOrDashed() const;
-  gfx::RectF BoundsForWavy() const;
   Path PrepareDottedOrDashedStrokePath() const;
   bool IsSpellingOrGrammarError() const {
     return line_data_.line == TextDecorationLine::kSpellingError ||
