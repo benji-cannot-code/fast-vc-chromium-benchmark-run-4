716304887b2366851af3e21c157f762eb176b71d
“Prasanaa” <prasanaa@google.com>
2025-06-19 01:06:00 -0700

    [iOS] Add logic for checking if the account is managed in BWG FRE
    This CL adds the logic for checking if the account is managed in BWG
    FRE.
    
    Bug: 422232977
    Change-Id: Idc02188567dc8d0607b596f96acdd7229767057d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651931
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1476040}

--- ios/chrome/browser/intelligence/bwg/coordinator/bwg_coordinator.mm
+++ ios/chrome/browser/intelligence/bwg/coordinator/bwg_coordinator.mm
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/bwg_commands.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/public/commands/help_commands.h"
+#import "ios/chrome/browser/signin/model/authentication_service.h"
+#import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 
 @interface BWGCoordinator () <UISheetPresentationControllerDelegate,
                               BWGMediatorDelegate,
@@ -112,7 +114,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   _navigationController =
-      [[BWGNavigationController alloc] initWithPromo:showPromo];
+      [[BWGNavigationController alloc] initWithPromo:showPromo
+                                    isAccountManaged:[self isManagedAccount]];
   _navigationController.sheetPresentationController.delegate = self;
   _navigationController.BWGNavigationDelegate = self;
   _navigationController.mutator = _mediator;
@@ -188,6 +191,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_helpCommandsHandler
         presentInProductHelpWithType:InProductHelpType::kPageActionMenu];
   }
+}
+
+// Returns YES if the account is managed.
+- (BOOL)isManagedAccount {
+  raw_ptr<AuthenticationService> authService =
+      AuthenticationServiceFactory::GetForProfile(self.profile);
+  return authService->HasPrimaryIdentityManaged(signin::ConsentLevel::kSignin);
 }
 
 @end
--- ios/chrome/browser/intelligence/bwg/ui/bwg_consent_view_controller.h
+++ ios/chrome/browser/intelligence/bwg/ui/bwg_consent_view_controller.h
@@ -12,8 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @protocol BWGConsentMutator;
 
-// BWG consent View Controller.
+// BWG consent View Controller (VC).
 @interface BWGConsentViewController : UIViewController
+
+// Initializer for the VC whether the account is managed.
+- (instancetype)initWithIsAccountManaged:(BOOL)isAccountManaged;
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithNibName:(NSString*)nibNameOrNil
+                         bundle:(NSBundle*)nibBundleOrNil NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
 
 // The mutator for this view controller to communicate to the mediator.
 @property(nonatomic, weak) id<BWGConsentMutator> mutator;
