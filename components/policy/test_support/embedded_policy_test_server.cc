880515e860c37cdc6b3e93d907c2ef6c4713adaf
Austin Zhang <austinzzr@google.com>
2025-06-20 14:13:25 -0700

    Add integration test infrastructure for PromotionEligibility.
    Change-Id: Ia873a6a30676126391ca75ba6d81fe42c121637d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626556
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Austin Zhang <austinzzr@google.com>
    Auto-Submit: Austin Zhang <austinzzr@google.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1476736}

--- components/policy/test_support/embedded_policy_test_server.cc
+++ components/policy/test_support/embedded_policy_test_server.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/test_support/request_handler_for_check_android_management.h"
 #include "components/policy/test_support/request_handler_for_chrome_desktop_report.h"
 #include "components/policy/test_support/request_handler_for_client_cert_provisioning.h"
+#include "components/policy/test_support/request_handler_for_determine_promotion_eligibility.h"
 #include "components/policy/test_support/request_handler_for_device_attribute_update.h"
 #include "components/policy/test_support/request_handler_for_device_attribute_update_permission.h"
 #include "components/policy/test_support/request_handler_for_device_initial_enrollment_state.h"
@@ -122,6 +123,8 @@ EmbeddedPolicyTestServer::EmbeddedPolicyTestServer()
   RegisterHandler(std::make_unique<RequestHandlerForChromeDesktopReport>(this));
   RegisterHandler(
       std::make_unique<RequestHandlerForClientCertProvisioning>(this));
+  RegisterHandler(
+      std::make_unique<RequestHandlerForDeterminePromotionEligibility>(this));
   RegisterHandler(
       std::make_unique<RequestHandlerForDeviceAttributeUpdate>(this));
   RegisterHandler(
