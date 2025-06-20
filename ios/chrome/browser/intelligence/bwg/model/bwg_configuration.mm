800f0fc048728a23f23941196a64b4865436abc7
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-06-20 13:41:48 -0700

    [iOS][BWG] Create config to pass to new BWG API.
    Also added OWNERS file for i/c/test/providers/*
    
    Four-CL change:
    -> crrev.com/c/6652423
       crrev.com/i/8363036
       crrev.com/c/6652424
       crrev.com/i/8363037
    
    Bug: 422506000
    Change-Id: I7dc4e4257d7e852e74e6d2a7c86646509a6eeb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652423
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476723}

---
+++ ios/chrome/browser/intelligence/bwg/model/bwg_configuration.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/intelligence/bwg/model/bwg_configuration.h"
+
+#import "base/check.h"
+#import "components/optimization_guide/proto/features/common_quality_data.pb.h"
+
+@implementation BWGConfiguration {
+  // The current configuration's PageContext.
+  std::unique_ptr<optimization_guide::proto::PageContext> _pageContext;
+}
+
+- (std::unique_ptr<optimization_guide::proto::PageContext>)pageContext {
+  CHECK(_pageContext.get());
+  return std::move(_pageContext);
+}
+
+- (void)setPageContext:
+    (std::unique_ptr<optimization_guide::proto::PageContext>)pageContext {
+  _pageContext = std::move(pageContext);
+}
+
+@end
