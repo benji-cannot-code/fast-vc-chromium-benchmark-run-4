fca116bc141c464bc78abb8a093469aba29e6396
dljames <dljames@chromium.org>
2025-06-20 13:09:07 -0700

    Centralize window title layout logic into BrowserFrame
    The `LayoutWebAppWindowTitle` method was previously a virtual function
    on BrowserNonClientFrameView. This led to implementations being
    scattered across various platform-specific subclasses.
    
    This CL consolidates all this platform-specific logic into a single
    method, `BrowserFrame::LayoutWebAppWindowTitle`, using `BUILDFLAG`s to
    differentiate between platforms (Win, Mac, Linux, CrOS).
    
    This refactoring simplifies the `BrowserNonClientFrameView` hierarchy
    and the `BrowserViewLayoutDelegate` interface by removing the
    now-redundant virtual method. Callsites are updated to use the new
    central function.
    
    Change-Id: I1a5321d382a520b3d3b4b30fd45bd44e7cab67c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651876
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476703}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
@@ -21,10 +21,6 @@ namespace base {
 class OneShotTimer;
 }
 
-namespace views {
-class Label;
-}
-
 namespace remote_cocoa::mojom {
 enum class ToolbarVisibilityStyle;
 }
@@ -52,8 +48,6 @@ class BrowserNonClientFrameViewMac : public BrowserNonClientFrameView,
       const gfx::Size& tabstrip_minimum_size) const override;
   gfx::Rect GetBoundsForWebAppFrameToolbar(
       const gfx::Size& toolbar_preferred_size) const override;
-  void LayoutWebAppWindowTitle(const gfx::Rect& available_space,
-                               views::Label& window_title_label) const override;
   int GetTopInset(bool restored) const override;
   void UpdateFullscreenTopUI() override;
   bool ShouldHideTopUIForFullscreen() const override;
