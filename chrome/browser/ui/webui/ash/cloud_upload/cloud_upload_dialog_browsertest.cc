b581582553c50a0257bcbb1522440a1999ebaf10
Daniel Cheng <dcheng@chromium.org>
2025-06-20 12:26:23 -0700

    Remove C++ JSON parser
    This was only needed to support NaCl builds, but that is no longer a
    requirement.
    
    Bug: 40511454
    Bug: 40811643
    Change-Id: I1782c1d17c06aa17e03b6185c6ae6a69a840a1f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6655303
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476678}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog_browsertest.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog_browsertest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/callback_helpers.h"
-#include "base/json/json_parser.h"
 #include "base/json/json_reader.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
--- components/omnibox/browser/enterprise_search_aggregator_suggestions_service_unittest.cc
+++ components/omnibox/browser/enterprise_search_aggregator_suggestions_service_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/enterprise_search_aggregator_suggestions_service.h"
 
 #include "base/functional/bind.h"
-#include "base/json/json_parser.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/strings/string_number_conversions.h"
