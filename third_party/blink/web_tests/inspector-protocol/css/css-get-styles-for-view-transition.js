849cbfafbdd419483cef84c384be22e0db76f5e5
Eric Leese <leese@chromium.org>
2025-06-20 05:19:14 -0700

    Include view transition classes when getting matched styles
    Currently DevTools is not reporting matched styles on view transition
    pseudo elements that use view transition classes. This change adds the
    view transition classes to the style request so these are collected and
    reported to DevTools.
    
    Fixed: 423867354
    Change-Id: I7206f0b8645d8972a3a22cd6693154900cdac059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651397
    Commit-Queue: Eric Leese <leese@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Auto-Submit: Eric Leese <leese@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476444}

--- third_party/blink/web_tests/inspector-protocol/css/css-get-styles-for-view-transition.js
+++ third_party/blink/web_tests/inspector-protocol/css/css-get-styles-for-view-transition.js
@@ -3,7 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const {page, session, dp} = await testRunner.startHTML(`
       <style> ::view-transition-new(shared) {animation-duration: 300s;} </style>
       <style> ::view-transition-old(*) {background: red;} </style>
-      <div style='width:100px; height:100px; view-transition-name: shared; contain:paint;'></div>`,
+      <style> ::view-transition-group(.transition-group) {font-size: 10px;} </style>
+      <style> ::view-transition-image-pair(shared.transition-group) {left: 100px;} </style>
+      <div style='width:100px; height:100px; view-transition-name: shared; view-transition-class: transition-group; contain:paint;'></div>`,
       'The test verifies functionality of querying style information for view-transition pseudo elements');
 
   await session.evaluateAsync(`
