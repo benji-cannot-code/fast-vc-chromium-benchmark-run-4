a438180ddea26414f9933da508c43b5b10297344
Kent Tamura <tkent@chromium.org>
2025-06-20 07:22:51 -0700

    WTF: Remove the `additional_data` argument from TextCodec factory
    No codec uses it.
    This CL has no behavior changes.
    
    Change-Id: I56b06d4e8ef18c6c85ba4e05450f76f9319e6cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656238
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476504}

--- third_party/blink/renderer/platform/wtf/text/text_codec_utf8.cc
+++ third_party/blink/renderer/platform/wtf/text/text_codec_utf8.cc
@@ -56,8 +56,7 @@ ALWAYS_INLINE size_t LengthOfNonCharacter(int character) {
   return -character;
 }
 
-std::unique_ptr<TextCodec> TextCodecUtf8::Create(const TextEncoding&,
-                                                 const void*) {
+std::unique_ptr<TextCodec> TextCodecUtf8::Create(const TextEncoding&) {
   return base::WrapUnique(new TextCodecUtf8());
 }
 
@@ -80,7 +79,7 @@ void TextCodecUtf8::RegisterEncodingNames(EncodingNameRegistrar registrar) {
 }
 
 void TextCodecUtf8::RegisterCodecs(TextCodecRegistrar registrar) {
-  registrar("UTF-8", Create, nullptr);
+  registrar("UTF-8", Create);
 }
 
 static constexpr std::array<uint8_t, 256> kNonASCIISequenceLength = {
