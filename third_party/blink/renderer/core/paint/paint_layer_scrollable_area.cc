4e80d4394fe73decf02f7b52b31a846c53573d6a
Perry <perryuwang@gmail.com>
2025-06-20 09:04:48 -0700

    Overscroll behavior should be respected on all scroll containers
    If a scroll container is not scrollable, it will not create a scroll
    node. Therefore, the scroll events that occur on it will be propagated
    without being limited by the overscroll-behavior. This CL ensures that
    overscroll-behavior is respected on all scroll containers.
    
    Bug: 41371072
    Change-Id: I70cbcb05acfaa58411801dfdb5929a3501ba8d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618817
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Perry <perryuwang@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1476548}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/checked_math.h"
 #include "base/task/single_thread_task_runner.h"
 #include "cc/animation/animation_timeline.h"
+#include "cc/base/features.h"
 #include "cc/input/main_thread_scrolling_reason.h"
 #include "cc/input/scroll_snap_data.h"
 #include "cc/input/scroll_utils.h"
@@ -1373,6 +1374,24 @@ bool PaintLayerScrollableArea::HasVerticalOverflow() const {
   return ScrollHeight().Round() > client_height.Round();
 }
 
+bool PaintLayerScrollableArea::CanPropagateScroll() const {
+  if (!base::FeatureList::IsEnabled(
+          ::features::kOverscrollBehaviorRespectedOnAllScrollContainers)) {
+    return true;
+  }
+  auto* box = GetLayoutBox();
+  // TODO(crbug.com/425353152): Remove the visibility check.
+  if (!box || !box->IsScrollContainer() ||
+      box->StyleRef().Visibility() != EVisibility::kVisible) {
+    return true;
+  }
+  if (box->StyleRef().OverscrollBehaviorX() != EOverscrollBehavior::kAuto ||
+      box->StyleRef().OverscrollBehaviorY() != EOverscrollBehavior::kAuto) {
+    return false;
+  }
+  return true;
+}
+
 // This function returns true if the given box requires overflow scrollbars (as
 // opposed to the viewport scrollbars managed by VisualViewport).
 static bool CanHaveOverflowScrollbars(const LayoutBox& box) {
@@ -2556,6 +2575,8 @@ void PaintLayerScrollableArea::UpdateScrollableAreaSet() {
     frame_view->RemoveScrollAnchoringScrollableArea(this);
   }
 
+  // TODO(crbug.com/425353152): Should be able to scroll invisible scroll
+  // containers.
   bool is_visible =
       GetLayoutBox()->StyleRef().Visibility() == EVisibility::kVisible;
   bool did_scroll_overflow = scrolls_overflow_;
@@ -2569,6 +2590,17 @@ void PaintLayerScrollableArea::UpdateScrollableAreaSet() {
   }
 
   scrolls_overflow_ = has_overflow && is_visible;
+
+  if (GetLayoutBox()->IsScrollContainer() && !scrolls_overflow_ &&
+      (GetLayoutBox()->StyleRef().OverscrollBehaviorX() !=
+           EOverscrollBehavior::kAuto ||
+       GetLayoutBox()->StyleRef().OverscrollBehaviorY() !=
+           EOverscrollBehavior::kAuto)) {
+    UseCounter::Count(
+        GetLayoutBox()->GetDocument(),
+        WebFeature::kOverscrollBehaviorOnNonScrollableScrollContainer);
+  }
+
   if (did_scroll_overflow == ScrollsOverflow())
     return;
 
--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -464,6 +464,9 @@ static bool NeedsScrollAndScrollTranslation(
       !scrollable_area->GetScrollOffset().IsZero()) {
     return true;
   }
+  if (!scrollable_area->CanPropagateScroll()) {
+    return true;
+  }
   return false;
 }
 
