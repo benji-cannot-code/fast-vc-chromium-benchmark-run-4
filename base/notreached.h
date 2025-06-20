ad7de9d37c53456d6cc32d561f57796b4051122d
Daniel Cheng <dcheng@chromium.org>
2025-06-20 11:47:52 -0700

    Remove notimplemented.h include from notreached.h
    Bug: 41493641
    Change-Id: Ifa084f07ebfac97d465708dc43bc74835ef4ec31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6657459
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476655}

--- base/notreached.h
+++ base/notreached.h
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/dcheck_is_on.h"
 
-// TODO(crbug.com/41493641): Remove once NOTIMPLEMENTED() call sites include
-// base/notimplemented.h.
-#include "base/notimplemented.h"
-
 namespace logging {
 
 #if CHECK_WILL_STREAM()
