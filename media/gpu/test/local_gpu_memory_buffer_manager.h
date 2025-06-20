31fa5d7f98b68893fec9b60d9e5b79614c49ee0a
Colin Blundell <blundell@chromium.org>
2025-06-19 23:01:21 -0700

    [//media] Remove dead code from LocalGpuMemoryBufferManager
    Bug: 404763491
    Change-Id: If93e29abfdf983c008f485e1a82e5c4fbd0805bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656420
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476371}

--- media/gpu/test/local_gpu_memory_buffer_manager.h
+++ media/gpu/test/local_gpu_memory_buffer_manager.h
@@ -16,13 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/gpu_memory_buffer_handle.h"
 #include "ui/gfx/linux/scoped_gbm_device.h"
 
-namespace base {
-namespace trace_event {
-class ProcessMemoryDump;
-class MemoryAllocatorDumpGuid;
-}  // namespace trace_event
-}  // namespace base
-
 namespace gfx {
 class GpuMemoryBuffer;
 struct NativePixmapHandle;
@@ -88,16 +81,8 @@ class GpuMemoryBufferImplGbm {
   void* memory(size_t plane);
   void Unmap();
   gfx::Size GetSize() const;
-  gfx::BufferFormat GetFormat() const;
   int stride(size_t plane) const;
-  gfx::GpuMemoryBufferId GetId() const;
-  gfx::GpuMemoryBufferType GetType() const;
   gfx::GpuMemoryBufferHandle CloneHandle() const;
-  void OnMemoryDump(
-      base::trace_event::ProcessMemoryDump* pmd,
-      const base::trace_event::MemoryAllocatorDumpGuid& buffer_dump_guid,
-      uint64_t tracing_process_id,
-      int importance) const;
 
  private:
   struct MappedPlane {
@@ -105,7 +90,6 @@ class GpuMemoryBufferImplGbm {
     raw_ptr<void> mapped_data;
   };
 
-  gfx::BufferFormat format_;
   raw_ptr<gbm_bo> buffer_object_;
   gfx::GpuMemoryBufferHandle handle_;
   bool mapped_;
