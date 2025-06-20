b6fc83f1c979b0496b163b3af10df31f3d3f5667
Sam Maier <smaier@chromium.org>
2025-06-20 11:40:25 -0700

    Migrate PlatformServiceBridge to use ServiceLoaderUtil
    Bug: 380092098
    Change-Id: Ib3f15e8772af3e02f890550e6dd3d04efefcdd2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654834
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476648}

---
+++ android_webview/java/src/org/chromium/android_webview/common/NoOpPlatformServiceBridge.java
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.common;
+
+/**
+ * The default implementation of {@link PlatformServiceBridge}.
+ *
+ * <p>This can be overridden by defining a service implementation for {@link PlatformServiceBridge}.
+ */
+public class NoOpPlatformServiceBridge extends PlatformServiceBridge {}
