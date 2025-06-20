eeeea7ea48240175c63c71d57ec82879a9b8361d
Peter Kvitek <kvitekp@chromium.org>
2025-06-20 11:56:01 -0700

    [headelss] Prevent download bubble from showing on Macs
    This CL disables download bubbles when in headless mode. No one will see
    them anyway.
    
    Bug: 379994807
    Change-Id: Ic5e2f39f6ac10620041030d2bb94c113816297aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654808
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476658}

--- chrome/browser/download/bubble/download_bubble_prefs.cc
+++ chrome/browser/download/bubble/download_bubble_prefs.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "chrome/browser/download/download_core_service.h"
 #include "chrome/browser/download/download_core_service_factory.h"
+#include "chrome/browser/headless/headless_mode_util.h"
 #include "chrome/common/pref_names.h"
 #include "components/enterprise/buildflags/buildflags.h"
 #include "components/prefs/pref_service.h"
@@ -30,6 +31,11 @@ bool IsDownloadBubbleEnabled() {
 }
 
 bool ShouldShowDownloadBubble(Profile* profile) {
+  // There is no need to display download bubble in headless mode. This prevents
+  // http://crbug.com/379994807 but also makes sense on other platforms.
+  if (headless::IsHeadlessMode()) {
+    return false;
+  }
   // If the download UI is disabled by at least one extension, do not show the
   // bubble and the toolbar icon.
   return DownloadCoreServiceFactory::GetForBrowserContext(
