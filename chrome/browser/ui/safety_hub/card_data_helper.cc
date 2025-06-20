6ab52f3d19812b767a4472d93b47c88f084dae33
Aisulu R <aisulu@google.com>
2025-06-20 07:32:38 -0700

    [SHv2] Extract Result class for notification permissions review service
    This CL extracts NotificationPermissionsResult class and its tests from
    NotificationPermissionsReview service into separate files.
    
    While at it also:
    - resolve a TODO about NotificationPermissionsResult's private methods
    - change a return type of GetNotificationPermissions() since every time
    it's called the result is being cast to NotificationPermissionsResult.
    
    Bug: 419315328
    Change-Id: Ic23a122b364cae94a0af3f0b67dcd1083630b20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651402
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476507}

--- chrome/browser/ui/safety_hub/card_data_helper.cc
+++ chrome/browser/ui/safety_hub/card_data_helper.cc
@@ -149,8 +149,7 @@ SafetyHubCardState GetOverallState(Profile* profile) {
   std::optional<std::unique_ptr<SafetyHubService::Result>> opt_npr_result =
       npr_service->GetCachedResult();
   if (opt_npr_result.has_value()) {
-    auto* result = static_cast<
-        NotificationPermissionsReviewService::NotificationPermissionsResult*>(
+    auto* result = static_cast<NotificationPermissionsReviewResult*>(
         opt_npr_result.value().get());
     if (!result->GetSortedNotificationPermissions().empty()) {
       return SafetyHubCardState::kWarning;
