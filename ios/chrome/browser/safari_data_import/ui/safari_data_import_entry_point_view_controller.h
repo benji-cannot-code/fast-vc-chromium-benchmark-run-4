0a395668e605abab133a5b7c813149ec260bb6f7
ginnnnnnny <ginnyhuang@chromium.org>
2025-06-20 08:03:07 -0700

    [iOS][safari-import] Add strings and image resources for entry point
    ... and also added the view controller using these resources. A future
    CL will hook the view controller up with the entry point coordinator in
    crrev.com/c/6653580.
    
    Bug: 420703283
    Change-Id: I504bb7e0717cfd7203f23bab169b01512beee964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634718
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476518}

---
+++ ios/chrome/browser/safari_data_import/ui/safari_data_import_entry_point_view_controller.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_ENTRY_POINT_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_ENTRY_POINT_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
+
+/// Screen presented for `SafariDataImportScreen::kEntryPoint`.
+@interface SafariDataImportEntryPointViewController
+    : ConfirmationAlertViewController
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_ENTRY_POINT_VIEW_CONTROLLER_H_
