48dc0e1f6557ec462a21a89db8c737f8aeefa15e
Michelle Abreo <michelleabreo@google.com>
2025-06-20 16:04:48 -0700

    Update Throbber start/stop logic to fix race condition.
    This cl fixes a race condition where the Throbber's animation timer
    could fire after the object was destroyed, which sometimes happened when
    the throbber was still active in a dialog during page reloads. This
    change replaces it with a WeakPtr to ensure the animation callback is
    safely invalidated when the Throbber is destroyed.
    
    Bug b:425927824
    
    Change-Id: Iaa1aa8f835bf916f78cd1966b518b3193bbdf53b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652302
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476787}

--- ui/views/controls/throbber.h
+++ ui/views/controls/throbber.h
@@ -62,6 +62,8 @@ class VIEWS_EXPORT Throbber : public View {
 
   // Overrides the default color, ui::kColorThrobber, if set.
   std::optional<ui::ColorId> color_id_;
+
+  base::WeakPtrFactory<Throbber> weak_ptr_factory_{this};
 };
 
 BEGIN_VIEW_BUILDER(VIEWS_EXPORT, Throbber, View)
