f1e25af0729197399dc3cbfd587f23a521ed5fb3
Di Wu <diwux@google.com>
2025-06-20 17:40:10 -0700

    Ash: Fix crash in MahiPanelViewTest.ClickMetrics
    The `MahiPanelViewTest.ClickMetrics` test was failing with a `DCHECK`
    failure in `AshTestBase::LeftClickOn`. The test attempted to find and
    click a view with the ID `kLearnMoreLink`.
    
    This view does not exist, as the "Learn More" link is part of a
    `views::StyledLabel` and does not have its own `View` instance or ID, as
    shown here
    https://source.chromium.org/chromium/chromium/src/+/main:ash/system/mahi/mahi_panel_view.cc;l=764;drc=b49c254c612709bbf6efee73667c827613f1876a;bpv=0;bpt=1.
    Consequently, `GetViewByID` returned `nullptr`, causing the crash.
    
    This CL fixes the test by:
    - Finding the parent `StyledLabel` by its ID (`kFooterLabel`).
    - Using `ClickFirstLinkForTesting()` to correctly simulate the link click.
    - Removing the now-unused `kLearnMoreLink` from the `ViewId` enum in
      `mahi_constants.h`.
    - Re-enabling the test and adding a missing histogram check for
      completeness.
    
    Bug: b:281717553, 333800096
    Test: MahiPanelViewTest.ClickMetrics
    Change-Id: Ib0fbfaa9fee57dbf9e4c2d08527c03c71c4bfbdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6655751
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1476829}

--- ash/system/mahi/mahi_constants.h
+++ ash/system/mahi/mahi_constants.h
@@ -25,7 +25,6 @@ enum ViewId {
   kThumbsUpButton,
   kThumbsDownButton,
   kFooterLabel,
-  kLearnMoreLink,
   kRefreshView,
   kMahiPanelView,
   kSummaryOutlinesSection,
--- ash/system/mahi/mahi_panel_view_unittest.cc
+++ ash/system/mahi/mahi_panel_view_unittest.cc
@@ -1755,15 +1755,16 @@ TEST_F(MahiPanelViewTest, RefreshSummaryContents_TransitionToSummaryView) {
   EXPECT_TRUE(question_answer_view->children().empty());
 }
 
-// TODO(crbug.com/333800096): Re-enable this test
-TEST_F(MahiPanelViewTest, DISABLED_ClickMetrics) {
+TEST_F(MahiPanelViewTest, ClickMetrics) {
   base::HistogramTester histogram;
 
   // Learn more button.
   histogram.ExpectBucketCount(mahi_constants::kMahiButtonClickHistogramName,
                               mahi_constants::PanelButton::kLearnMoreLink, 0);
-  LeftClickOn(
-      panel_view()->GetViewByID(mahi_constants::ViewId::kLearnMoreLink));
+  views::test::RunScheduledLayout(widget());
+  views::AsViewClass<views::StyledLabel>(
+      panel_view()->GetViewByID(mahi_constants::ViewId::kFooterLabel))
+      ->ClickFirstLinkForTesting();
   histogram.ExpectBucketCount(mahi_constants::kMahiButtonClickHistogramName,
                               mahi_constants::PanelButton::kLearnMoreLink, 1);
   histogram.ExpectTotalCount(mahi_constants::kMahiButtonClickHistogramName, 1);
@@ -1816,6 +1817,7 @@ TEST_F(MahiPanelViewTest, DISABLED_ClickMetrics) {
   histogram.ExpectBucketCount(
       mahi_constants::kMahiButtonClickHistogramName,
       mahi_constants::PanelButton::kGoToQuestionAndAnswerButton, 1);
+  histogram.ExpectTotalCount(mahi_constants::kMahiButtonClickHistogramName, 4);
 
   // Close button.
   views::test::RunScheduledLayout(widget());
