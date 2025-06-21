d30577b9361be3032e8ecf4aed31678e1e8880b9
Taehee Yoo <ap420073@gmail.com>
2025-06-21 06:10:56 -0700

    Remove unnecessary enum Style in hover_button.h
    The enum Style in the hover_button.h is no longer used.
    So remove it.
    
    Bug: 41446220
    Change-Id: Iecb76bd38c16f0cb24e785d5c68ccb3d2f3ef254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651992
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Jinho Bang <zino@chromium.org>
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476901}

--- chrome/browser/ui/views/controls/hover_button.h
+++ chrome/browser/ui/views/controls/hover_button.h
@@ -48,8 +48,6 @@ class HoverButton : public views::LabelButton {
   METADATA_HEADER(HoverButton, views::LabelButton)
 
  public:
-  enum Style { STYLE_PROMINENT, STYLE_ERROR };
-
   HoverButton();
 
   // Creates a single line hover button with no icon.
