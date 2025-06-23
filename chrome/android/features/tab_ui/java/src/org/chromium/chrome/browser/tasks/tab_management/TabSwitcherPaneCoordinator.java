c076390da608d438e2c8ba613648794265d36ea8
Fiaz Muhammad <mfiaz@google.com>
2025-06-23 16:45:32 -0700

    [Tab Group Suggest] Add test mode functionality
    - Adds test mode functionality (forces a group suggestion using the
    	last 3 tabs in the tab switcher)
    
    Bug: 426246058
    Change-Id: I1ae0a0a5f372e8b34aacd78bdcde9ab198cfdd8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663822
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477631}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinator.java
@@ -750,7 +750,11 @@ public class TabSwitcherPaneCoordinator implements BackPressHandler {
     private void onTabSwitcherShown() {
         if (ChromeFeatureList.sTabSwitcherGroupSuggestionsAndroid.isEnabled()) {
             assert mTabSwitcherGroupSuggestionService != null;
-            mTabSwitcherGroupSuggestionService.maybeShowSuggestions();
+            if (ChromeFeatureList.sTabSwitcherGroupSuggestionsTestModeAndroid.isEnabled()) {
+                mTabSwitcherGroupSuggestionService.forceTabGroupSuggestion();
+            } else {
+                mTabSwitcherGroupSuggestionService.maybeShowSuggestions();
+            }
         }
 
         mTabListCoordinator.attachEmptyView();
