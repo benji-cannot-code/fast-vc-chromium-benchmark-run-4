d2af338dc2c549fe63a0b3452e070d3adb210b5c
James Cook <jamescook@chromium.org>
2025-06-23 14:22:26 -0700

    Add new extension platform "desktop_android" to json schema compiler
    This allows files like _api_features.json to explicitly list (or
    explicitly *not* list) desktop_android support.
    
    Once we update api_features and permissions_features we'll be able
    to prevent CHECK failures in the renderer when extensions try
    to call APIs that haven't been ported to desktop Android yet.
    
    Bug: 424382482
    Change-Id: I5a2b36acf28fad06e1028d910515e9c587041352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664198
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477561}

--- extensions/common/features/feature.cc
+++ extensions/common/features/feature.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest.h"
 
@@ -30,6 +31,8 @@ Feature::Platform Feature::GetCurrentPlatform() {
   return WIN_PLATFORM;
 #elif BUILDFLAG(IS_FUCHSIA)
   return FUCHSIA_PLATFORM;
+#elif BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS)
+  return DESKTOP_ANDROID_PLATFORM;
 #else
   return UNSPECIFIED_PLATFORM;
 #endif
