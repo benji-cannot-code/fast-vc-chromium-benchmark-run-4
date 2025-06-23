d1217eaab31b6d8b7a129ce6b4abb967ab949318
Victor Miura <vmiura@chromium.org>
2025-06-23 11:04:56 -0700

    [TreesInViz] Plumb IsDirectlyCompositedImage and nearest_neighbor to Viz.
    This change plumbs |IsDirectlyCompositedImage()| and |nearest_neighbor()|
    from PictureLayerImpl to TileDisplayLayerImpl, and updates the logic in
    |TileDisplayLayerImpl::AppendQuads()| to match what existed before the
    change to use mojom for display tree construction.
    
    When using directly composited images, we must set |shared_quad_state->clip_rect|
    to the intersection of the bounds of the layer and any existing clip. This is
    required because in these cases, the tiling can have fractional dimensions due to
    the fact that raster scale is not applied to both dimensions equally, when the
    aspect ratio of the layer does not match that of the image.
    
    This change also ensures that the nearest_neighbor property of directly
    composited image layers is correctly propagated to quads.
    
    Bug: 421890757
    Change-Id: I852dc4e205e689d5a9973e32ad90a8ea585f6581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656016
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477401}

--- cc/layers/picture_layer_impl.h
+++ cc/layers/picture_layer_impl.h
@@ -203,6 +203,9 @@ class CC_EXPORT PictureLayerImpl
   using TileUpdateSet = std::map<float, std::set<TileIndex>>;
   TileUpdateSet TakeUpdatedTiles();
 
+  bool IsDirectlyCompositedImage() const;
+  bool nearest_neighbor() const { return nearest_neighbor_; }
+
  protected:
   friend class RasterizeAndRecordBenchmarkImpl;
 
@@ -243,7 +246,6 @@ class CC_EXPORT PictureLayerImpl
       const PaintWorkletRecordMap* pending_paint_worklet_records,
       const DiscardableImageMap* pending_discardable_image_map);
 
-  bool IsDirectlyCompositedImage() const;
   void UpdateDirectlyCompositedImageFromRasterSource();
 
   void SanityCheckTilingState() const;
