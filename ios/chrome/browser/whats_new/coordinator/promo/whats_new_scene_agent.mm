826dbc643f583cee0f0397a229873bb536de0409
Cheick Cisse <cheickcisse@google.com>
2025-06-23 10:04:44 -0700

    [iOS][WhatsNew] Remove Price Insights flag from What's New
    Change-Id: I33653dbd0ad6fd0bc659d477956f2fef768b99f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6659761
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477362}

--- ios/chrome/browser/whats_new/coordinator/promo/whats_new_scene_agent.mm
+++ ios/chrome/browser/whats_new/coordinator/promo/whats_new_scene_agent.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/whats_new/coordinator/promo/whats_new_scene_agent.h"
 
-#import "ios/chrome/browser/price_insights/model/price_insights_feature.h"
 #import "ios/chrome/browser/promos_manager/model/constants.h"
 #import "ios/chrome/browser/promos_manager/model/features.h"
 #import "ios/chrome/browser/promos_manager/model/promos_manager.h"
@@ -38,17 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     transitionedToActivationLevel:(SceneActivationLevel)level {
   switch (level) {
     case SceneActivationLevelForegroundActive: {
-      if (WasWhatsNewUsed()) {
-        return;
-      }
-
-      // Special case for What's New M132 for Price Insights. Only register a
-      // promo is Price Insights is enabled.
-      if (!IsPriceInsightsRegionEnabled()) {
-        self.promosManager->DeregisterPromo(promos_manager::Promo::WhatsNew);
-        return;
-      }
-
       DCHECK(self.promosManager);
       self.promosManager->RegisterPromoForContinuousDisplay(
           promos_manager::Promo::WhatsNew);
