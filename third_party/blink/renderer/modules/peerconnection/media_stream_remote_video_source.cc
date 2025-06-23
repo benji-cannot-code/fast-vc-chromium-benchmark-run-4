3cd1fb59259078639461dba19f61094f9db2f7a4
Ilya Nikolaevskiy <ilnik@chromium.org>
2025-06-23 02:46:18 -0700

    Always set color space in webrtc remote video source
    The default seems to be BT601, as videos like that look color accurate.
    
    Also guess the correct color space if the passed one is only partially set.
    
    Bug: 40269844
    Change-Id: I550fa25faec089929d21eaabbc7038d2b75f6a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6642770
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477187}

--- third_party/blink/renderer/modules/peerconnection/media_stream_remote_video_source.cc
+++ third_party/blink/renderer/modules/peerconnection/media_stream_remote_video_source.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
+#include "media/base/media_switches.h"
 #include "media/base/timestamp_constants.h"
+#include "media/base/video_color_space.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_util.h"
 #include "third_party/blink/public/common/features.h"
@@ -205,7 +207,7 @@ void MediaStreamRemoteVideoSource::RemoteVideoSourceDelegate::OnFrame(
   // WebRtcIgnoreUnspecifiedColorSpace. If the feature is enabled we won't try
   // to guess a color space if the webrtc::ColorSpace is unspecified. If the
   // feature is disabled (default), an unspecified color space will get
-  // converted into a gfx::ColorSpace set to BT709.
+  // converted into a gfx::ColorSpace set to BT601.
   if (incoming_frame.color_space() &&
       !(ignore_unspecified_color_space_ &&
         incoming_frame.color_space()->primaries() ==
@@ -214,8 +216,19 @@ void MediaStreamRemoteVideoSource::RemoteVideoSourceDelegate::OnFrame(
             webrtc::ColorSpace::TransferID::kUnspecified &&
         incoming_frame.color_space()->matrix() ==
             webrtc::ColorSpace::MatrixID::kUnspecified)) {
-    video_frame->set_color_space(
-        WebRtcToGfxColorSpace(*incoming_frame.color_space()));
+    gfx::ColorSpace color_space =
+        WebRtcToGfxColorSpace(*incoming_frame.color_space());
+    if (!color_space.IsValid()) {
+      color_space = media::VideoColorSpace::FromGfxColorSpace(color_space)
+                        .GuessGfxColorSpace();
+    }
+    if (color_space.IsValid()) {
+      video_frame->set_color_space(color_space);
+    }
+  }
+  if (base::FeatureList::IsEnabled(media::kWebRTCColorAccuracy) &&
+      !incoming_frame.color_space()) {
+    video_frame->set_color_space(gfx::ColorSpace::CreateREC601());
   }
 
   // Run render smoothness algorithm only when we don't have to render
