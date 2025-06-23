15224d61e9fa3d693e0d3f9d645b7e6afe776d8f
S Ganesh <ganesh@chromium.org>
2025-06-23 15:58:21 -0700

    Revert "[views-ax] Introduce ViewAccessibilityAXTreeSource"
    This reverts commit 3dddcfc1d5f20b624ed926de140c9a29c8e74614.
    
    Reason for revert: Step "views_unittests (retry shards) on Ubuntu-22.04" failing on builder "chromium/ci/Linux UBSan Tests"
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20UBSan%20Tests/7723/overview
    
    Bug: 40672441
    Original change's description:
    > [views-ax] Introduce ViewAccessibilityAXTreeSource
    >
    > This CL creates a new AXTreeSource for ViewAccessibility, which holds
    > the active views-based tree. For now, we only include a barebone
    > implementation. We'll implement it in follow-up CLs and add tests.
    >
    > Bug: 40672441
    > Change-Id: I936b15310e9213c5d4bc8950a51aeaef70c69775
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656003
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1477465}
    
    Bug: 40672441
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I73d423939dcf0822232d1f5a5bf131a1bd9302e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664047
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Owners-Override: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1477614}

--- ui/views/accessibility/tree/widget_ax_manager.h
+++ ui/views/accessibility/tree/widget_ax_manager.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_id_forward.h"
 #include "ui/accessibility/ax_tree_id.h"
 #include "ui/accessibility/platform/ax_mode_observer.h"
-#include "ui/views/accessibility/tree/view_accessibility_ax_tree_source.h"
 #include "ui/views/views_export.h"
 
 namespace views {
@@ -60,10 +59,6 @@ class VIEWS_EXPORT WidgetAXManager : public ui::AXModeObserver {
 
   // The AXTreeID of the parent widget's accessibility tree, if any.
   ui::AXTreeID parent_ax_tree_id_;
-
-  // Holds the active views-based tree. A tree consists of all the views in the
-  // widget.
-  std::unique_ptr<ViewAccessibilityAXTreeSource> tree_source_;
 
   // Indicates whether we're actively serializing widget accessibility data.
   bool is_enabled_ = false;
