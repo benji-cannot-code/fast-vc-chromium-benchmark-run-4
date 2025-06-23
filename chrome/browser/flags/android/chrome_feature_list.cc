fdebcaf98c3682f8b1b1ba85915a12671693f542
Jinsuk Kim <jinsukkim@chromium.org>
2025-06-23 12:20:11 -0700

    [mtb-cct] Test switch
    Introduced a new feature flag cct-adaptive-button-test-switch to
    make it easy to test functionalities.
    
    - Simulate a smaller toolbar width to have MTB hidden. This makes
      the menu dot appear on the overflow menu whenever contextual
      page action is triggered, indicating the fallback UI is available
      via overflow menu.
    - The contextual page action chip animation follows the IPH
      frequency condition. This CL adds a switch to override it so
      the animation is always performed.
    
    Bug: 414984846
    Change-Id: If8152e61f30f9a514f090a86db358969b70a9968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6661777
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477470}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -245,6 +245,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCacheIsMultiInstanceApi31Enabled,
     &kCastDeviceFilter,
     &kCCTAdaptiveButton,
+    &kCCTAdaptiveButtonTestSwitch,
     &kCCTAuthTab,
     &kCCTAuthTabDisableAllExternalIntents,
     &kCCTAuthTabEnableHttpsRedirects,
@@ -718,6 +719,10 @@ BASE_FEATURE(kCastDeviceFilter,
 
 BASE_FEATURE(kCCTAdaptiveButton,
              "CCTAdaptiveButton",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kCCTAdaptiveButtonTestSwitch,
+             "CCTAdaptiveButtonTestSwitch",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTAuthTab, "CCTAuthTab", base::FEATURE_ENABLED_BY_DEFAULT);
