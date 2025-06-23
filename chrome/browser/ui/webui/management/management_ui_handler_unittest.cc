9305bf071bfb38e4c7c1c449942320f4dd3685f8
Jun Ishiguro <junis@google.com>
2025-06-22 21:05:27 -0700

    Clean up TestingProfileManager (part 1)
    ScopedTestingLocalState is now deprecated and it has no effect. This CL
    removes a constructor of TestingProfileManager that takes
    ScopedTestingLocalState.
    
    Bug: 422039036
    Change-Id: I532d4224904fd6eda53c104fbee1bc3d0580b9b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6646948
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477106}

--- chrome/browser/ui/webui/management/management_ui_handler_unittest.cc
+++ chrome/browser/ui/webui/management/management_ui_handler_unittest.cc
@@ -490,7 +490,7 @@ class ManagementUIHandlerTests :
 #if BUILDFLAG(IS_CHROMEOS)
   void SetUp() override {
     profile_manager_ = std::make_unique<TestingProfileManager>(
-        TestingBrowserProcess::GetGlobal(), &local_state_);
+        TestingBrowserProcess::GetGlobal());
     ASSERT_TRUE(profile_manager_->SetUp());
     fake_user_manager_.Reset(
         std::make_unique<user_manager::FakeUserManager>(local_state_.Get()));
@@ -631,7 +631,7 @@ class ManagementUIHandlerTests :
 #else
   void SetUp() override {
     profile_manager_ = std::make_unique<TestingProfileManager>(
-        TestingBrowserProcess::GetGlobal(), &local_state_);
+        TestingBrowserProcess::GetGlobal());
     ASSERT_TRUE(profile_manager_->SetUp());
   }
   void TearDown() override {
--- chrome/test/base/testing_profile_manager.h
+++ chrome/test/base/testing_profile_manager.h
@@ -41,8 +41,6 @@ class PrefServiceSyncable;
 class TestingProfileManager : public ProfileObserver {
  public:
   explicit TestingProfileManager(TestingBrowserProcess* browser_process);
-  TestingProfileManager(TestingBrowserProcess* browser_process,
-                        ScopedTestingLocalState* local_state);
   TestingProfileManager(const TestingProfileManager&) = delete;
   TestingProfileManager& operator=(const TestingProfileManager&) = delete;
   ~TestingProfileManager() override;
