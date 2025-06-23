c298254c87f3d49678fd89d5c449a3626106ba22
Salma Elmahallawy <esalma@google.com>
2025-06-23 11:25:44 -0700

    [NTPFooter] Mark `kNTPFooterManagementNoticeEnabled` as sensitive policy
    Bug: 423645915
    Change-Id: I3d7a88aee1b6d8916e06504d007a2a25ca9bf51b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663878
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477424}

--- components/policy/core/common/policy_loader_common.cc
+++ components/policy/core/common/policy_loader_common.cc
@@ -62,6 +62,7 @@ const char* kSensitivePolicies[] = {
     key::kCommandLineFlagSecurityWarningsEnabled,
     key::kEnterpriseCustomLabelForBrowser,
     key::kEnterpriseLogoUrlForBrowser,
+    key::kNTPFooterManagementNoticeEnabled,
 #endif
 #if !BUILDFLAG(IS_IOS)
     key::kFirstPartySetsOverrides,
