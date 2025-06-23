a4913eaa6ee6cf9d27d4132a7f5cf03f850a8c4d
Ahmed Nasr <anasr@google.com>
2025-06-23 12:21:25 -0700

    Add Noto Sans Arabic UI to the list of fallback fonts.
    This is to address the problem of some RTL languages not displayed
    correctly in the caption bubble in chrome os.
    
    AX-Relnotes: n/a.
    
    Bug: b:426599907
    Change-Id: Ia2fb11f29912e53462a31a66901ebada5d087fa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6655433
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477472}

--- components/live_caption/views/caption_bubble.h
+++ components/live_caption/views/caption_bubble.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/live_caption/views/translation_view_wrapper_base.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/gfx/animation/slide_animation.h"
+#include "ui/gfx/font.h"
 #include "ui/gfx/font_list.h"
 #include "ui/native_theme/caption_style.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
@@ -78,6 +79,12 @@ class CaptionBubble : public views::BubbleDialogDelegateView,
   METADATA_HEADER(CaptionBubble, views::BubbleDialogDelegateView)
 
  public:
+  using NewFontListGetter = base::RepeatingCallback<gfx::FontList(
+      const std::vector<std::string>& font_names,
+      int font_style,
+      int font_size,
+      gfx::Font::Weight font_weight)>;
+
   CaptionBubble(
       CaptionBubbleSettings* caption_bubble_settings,
       std::unique_ptr<TranslationViewWrapperBase> translation_view_wrapper,
@@ -112,6 +119,7 @@ class CaptionBubble : public views::BubbleDialogDelegateView,
   views::MdTextButton* GetScrollLockButtonForTesting();
   views::View* GetHeaderForTesting();
   TranslationViewWrapperBase* GetTranslationViewWrapperForTesting();
+  void SetNewFontListGetterForTesting(NewFontListGetter callback);
 
   void SetCaptionBubbleStyle();
 
@@ -308,6 +316,8 @@ class CaptionBubble : public views::BubbleDialogDelegateView,
   std::unique_ptr<CaptionBubbleEventObserver> caption_bubble_event_observer_;
 
   base::CallbackListSubscription title_text_changed_callback_;
+
+  NewFontListGetter new_font_list_getter_;
 
   base::WeakPtrFactory<CaptionBubble> weak_ptr_factory_{this};
 };
