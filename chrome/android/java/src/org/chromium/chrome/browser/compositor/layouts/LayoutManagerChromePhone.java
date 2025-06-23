4c466ce5647a8c873b3266cd1da8cd096568ded1
Calder Kitagawa <ckitagawa@chromium.org>
2025-06-23 11:05:54 -0700

    [Layout] Delete a bunch of unused Layout methods
    These methods are unused and not overridden. They were used by legacy
    tab switcher code. LayoutManagerImpl still needs stubs for some of these
    events as subclasses care about the events and override the stubs.
    
    Bug: N/A
    Change-Id: I4d0ea1bf1c5516f39325a439ba7784b2c95b6145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663459
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477402}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChromePhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChromePhone.java
@@ -150,7 +150,6 @@ public class LayoutManagerChromePhone extends LayoutManagerChrome {
             // overview mode when the animation is finished.
             if (getActiveLayoutType() == LayoutType.SIMPLE_ANIMATION) {
                 setNextLayout(getLayoutForType(LayoutType.TAB_SWITCHER), true);
-                getActiveLayout().onTabClosed(time(), id, nextId, incognito);
             } else {
                 super.tabClosed(id, nextId, incognito, tabRemoved);
             }
