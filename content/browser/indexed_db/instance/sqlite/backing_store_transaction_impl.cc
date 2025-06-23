ac90537bd500ad086316030863511b69182e7c05
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2025-06-23 06:32:03 -0700

    IDB: SQLite store - implement index creation and cursors
    createIndex() and cursors (both key-only and with value) work.
    count() is implemented but needs triggers to function correctly.
    Triggers are needed also to remove "tombstones", i.e., the outdated
    index entries from when an object store record has been updated.
    
    Bug: 419203258
    Change-Id: I989ecb13327e74b3e44dfc6d464af510d625ecb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6657621
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1477242}

--- content/browser/indexed_db/instance/sqlite/backing_store_transaction_impl.cc
+++ content/browser/indexed_db/instance/sqlite/backing_store_transaction_impl.cc
@@ -71,8 +71,7 @@ Status BackingStoreTransactionImpl::ClearObjectStore(int64_t object_store_id) {
 Status BackingStoreTransactionImpl::CreateIndex(
     int64_t object_store_id,
     blink::IndexedDBIndexMetadata index) {
-  NOTIMPLEMENTED();
-  return Status::InvalidArgument("Not implemented");
+  return db_->CreateIndex(PassKey(), object_store_id, std::move(index));
 }
 
 Status BackingStoreTransactionImpl::DeleteIndex(int64_t object_store_id,
@@ -135,8 +134,8 @@ Status BackingStoreTransactionImpl::PutIndexDataForRecord(
     int64_t index_id,
     const blink::IndexedDBKey& key,
     const BackingStore::RecordIdentifier& record) {
-  NOTIMPLEMENTED();
-  return Status::InvalidArgument("Not implemented");
+  return db_->PutIndexDataForRecord(PassKey(), object_store_id, index_id, key,
+                                    record);
 }
 
 StatusOr<blink::IndexedDBKey>
@@ -144,8 +143,8 @@ BackingStoreTransactionImpl::GetFirstPrimaryKeyForIndexKey(
     int64_t object_store_id,
     int64_t index_id,
     const blink::IndexedDBKey& key) {
-  NOTIMPLEMENTED();
-  return base::unexpected(Status::InvalidArgument("not implemented"));
+  return db_->GetFirstPrimaryKeyForIndexKey(PassKey(), object_store_id,
+                                            index_id, key);
 }
 
 StatusOr<uint32_t> BackingStoreTransactionImpl::GetObjectStoreKeyCount(
@@ -159,8 +158,8 @@ StatusOr<uint32_t> BackingStoreTransactionImpl::GetIndexKeyCount(
     int64_t object_store_id,
     int64_t index_id,
     blink::IndexedDBKeyRange key_range) {
-  NOTIMPLEMENTED();
-  return base::unexpected(Status::InvalidArgument("Not implemented"));
+  return db_->GetIndexKeyCount(PassKey(), object_store_id, index_id,
+                               std::move(key_range));
 }
 
 StatusOr<std::unique_ptr<BackingStore::Cursor>>
@@ -186,9 +185,9 @@ BackingStoreTransactionImpl::OpenIndexKeyCursor(
     int64_t object_store_id,
     int64_t index_id,
     const blink::IndexedDBKeyRange& key_range,
-    blink::mojom::IDBCursorDirection) {
-  NOTIMPLEMENTED();
-  return base::unexpected(Status::InvalidArgument("Not implemented"));
+    blink::mojom::IDBCursorDirection direction) {
+  return db_->OpenIndexCursor(PassKey(), object_store_id, index_id, key_range,
+                              direction, /*key_only=*/true);
 }
 
 StatusOr<std::unique_ptr<indexed_db::BackingStore::Cursor>>
@@ -196,9 +195,9 @@ BackingStoreTransactionImpl::OpenIndexCursor(
     int64_t object_store_id,
     int64_t index_id,
     const blink::IndexedDBKeyRange& key_range,
-    blink::mojom::IDBCursorDirection) {
-  NOTIMPLEMENTED();
-  return base::unexpected(Status::InvalidArgument("Not implemented"));
+    blink::mojom::IDBCursorDirection direction) {
+  return db_->OpenIndexCursor(PassKey(), object_store_id, index_id, key_range,
+                              direction, /*key_only=*/false);
 }
 
 blink::mojom::IDBValuePtr BackingStoreTransactionImpl::BuildMojoValue(
--- content/browser/indexed_db/instance/sqlite/record_iterator.cc
+++ content/browser/indexed_db/instance/sqlite/record_iterator.cc
@@ -5,13 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/indexed_db/instance/sqlite/record_iterator.h"
 
-#include <memory>
-#include <optional>
-#include <string>
-#include <utility>
-
 #include "base/check.h"
-#include "base/functional/callback.h"
 #include "base/types/expected.h"
 #include "content/browser/indexed_db/instance/record.h"
 #include "content/browser/indexed_db/status.h"
@@ -35,18 +29,13 @@ StatusOr<std::unique_ptr<Record>> RecordIterator::Iterate(
   }
 
   statement->Reset(/*clear_bound_vars=*/false);
-  BindParameters(*statement, key, primary_key,
-                 /*offset=*/0);
+  BindParameters(*statement, key, primary_key, /*offset=*/0);
   if (!statement->Step()) {
     TRANSIENT_CHECK(statement->Succeeded());
     // End of range.
-    current_position_.reset();
     return nullptr;
   }
-  return ReadRow(*statement).transform([this](PositionAndRecord result) {
-    current_position_ = std::move(result.first);
-    return std::move(result.second);
-  });
+  return ReadRow(*statement);
 }
 
 StatusOr<std::unique_ptr<Record>> RecordIterator::Iterate(uint32_t count) {
@@ -68,14 +57,10 @@ StatusOr<std::unique_ptr<Record>> RecordIterator::Iterate(uint32_t count) {
   if (!statement->Step()) {
     TRANSIENT_CHECK(statement->Succeeded());
     // End of range.
-    current_position_.reset();
     return nullptr;
   }
 
-  return ReadRow(*statement).transform([this](PositionAndRecord result) {
-    current_position_ = std::move(result.first);
-    return std::move(result.second);
-  });
+  return ReadRow(*statement);
 }
 
 }  // namespace content::indexed_db::sqlite
