5d9f1a9de3da8cadea1008bdb20269f588409f3b
Fredrik Söderquist <fs@opera.com>
2025-06-23 09:35:07 -0700

    Factor out DecorationGeometry from TextDecorationInfo::LineData
    Rename TextDecorationInfo::LineData to DecorationGeometry and move it to
    beside DecorationLinePainter, and have it act as the main data source
    for painting decoration lines. The coupling to TextDecorationInfo will
    be further reduced in follow up CLs.
    
    Add DecorationGeometry::Make() and call that from
    TextDecorationInfo::SetLineData() when resolving the decoration
    geometry. Fold some more code that is only needed for the geometry setup
    into that function, such as ShouldAntialias() and DecorationStyle().
    
    Relocate wavy-related code to an anonymous namespace in
    decoration_line_painter.cc with minimal changes. ComputeWavyLineData()
    need to take a WavyParams and a DecorationGeometry object to populate.
    
    Drop the DrawLineForText() overload that is no longer needed, and call
    DrawLineAsRect() directly when drawing "double" lines.
    
    Bug: 425866014
    Change-Id: I5367ced3db4216c5964b5a5112a13a08b62b5754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650925
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1477342}

--- third_party/blink/renderer/core/paint/text_painter.cc
+++ third_party/blink/renderer/core/paint/text_painter.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/decoration_line_painter.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/svg_object_painter.h"
+#include "third_party/blink/renderer/core/paint/text_decoration_info.h"
 #include "third_party/blink/renderer/core/paint/text_paint_style.h"
 #include "third_party/blink/renderer/core/paint/timing/paint_timing_detector.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
