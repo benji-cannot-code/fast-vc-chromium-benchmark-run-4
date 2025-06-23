c2f901853737086155cbf1118f8e1bce10cc12c5
Ewann Pelle <ewannpv@chromium.org>
2025-06-23 01:44:05 -0700

    [iOS] Fixes: '+X' Label in Face Pile
    Improves '+X' label sizing with content priorities, removes corner
    radius animation, and fixes text color.
    
    Screenshot:
    https://drive.google.com/file/d/1UflpoD4ZTTmKezrFxNoHyc3L1wXWimDS/view?usp=sharing&resourcekey=0-VFBroRKC-e4QuWGZNn6Xpg
    
    Bug: 422737259
    Change-Id: Iad27c33fd19d1cdbcd556e840615475239deba39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6657007
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477163}

--- ios/chrome/browser/saved_tab_groups/ui/face_pile_view.mm
+++ ios/chrome/browser/saved_tab_groups/ui/face_pile_view.mm
@@ -136,7 +136,9 @@ const CGFloat kPlusXlabelContainerBackgroundAlpha = 0.2;
     // This is needed to avoid anti-aliasing artifacts around the border itself.
     UIView* plusXContainerView =
         [self createCircularContainerWithSize:containerSize];
-    plusXContainerView.layer.cornerRadius = containerSize / 2.0;
+    [UIView performWithoutAnimation:^{
+      plusXContainerView.layer.cornerRadius = containerSize / 2.0;
+    }];
     [plusXContainerView addSubview:plusXLabel];
 
     [_facesStackView addArrangedSubview:plusXContainerView];
@@ -175,7 +177,7 @@ const CGFloat kPlusXlabelContainerBackgroundAlpha = 0.2;
         [isDarkMode ? [UIColor colorNamed:kSolidWhiteColor]
                     : [UIColor colorNamed:kSolidBlackColor]
             colorWithAlphaComponent:kPlusXlabelContainerBackgroundAlpha];
-    _plusXLabel.textColor = [UIColor colorNamed:kTextPrimaryColor];
+    _plusXLabel.textColor = [UIColor colorNamed:kSolidWhiteColor];
     return;
   }
   if (isDarkMode) {
@@ -232,12 +234,19 @@ const CGFloat kPlusXlabelContainerBackgroundAlpha = 0.2;
   plusXLabel.textAlignment = NSTextAlignmentCenter;
   plusXLabel.font = [UIFont systemFontOfSize:kPlusXlabelFontSize
                                       weight:UIFontWeightMedium];
+  [plusXLabel setContentHuggingPriority:UILayoutPriorityRequired
+                                forAxis:UILayoutConstraintAxisHorizontal];
+  [plusXLabel
+      setContentCompressionResistancePriority:UILayoutPriorityRequired
+                                      forAxis:UILayoutConstraintAxisHorizontal];
 
   // Configure a container in order to add an inner horizontal margin around the
   // label.
   UIView* plusXLabelContainer = [[UIView alloc] init];
   plusXLabelContainer.translatesAutoresizingMaskIntoConstraints = NO;
-  plusXLabelContainer.layer.cornerRadius = _avatarSize / 2.0;
+  [UIView performWithoutAnimation:^{
+    plusXLabelContainer.layer.cornerRadius = _avatarSize / 2.0;
+  }];
   plusXLabelContainer.layer.masksToBounds = YES;
   [plusXLabelContainer addSubview:plusXLabel];
 
