374ee6452adc4c69affb2839991ee87ee9ad0170
Siddhartha S <ssid@google.com>
2025-06-23 15:02:34 -0700

    Integrate chrome_user_engagement model on iOS
    It was missed in https://chromium-review.googlesource.com/c/chromium/src/+/6612834
    
    Change-Id: Id2d62c1ddeea2127c0aa5e3f5730975154165aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664706
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477584}

--- ios/chrome/browser/segmentation_platform/model/segmentation_platform_config.mm
+++ ios/chrome/browser/segmentation_platform/model/segmentation_platform_config.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/feature_list.h"
 #import "base/metrics/field_trial_params.h"
 #import "base/time/time.h"
+#import "components/segmentation_platform/embedder/default_model/chrome_user_engagement.h"
 #import "components/segmentation_platform/embedder/default_model/cross_device_user_segment.h"
 #import "components/segmentation_platform/embedder/default_model/device_switcher_model.h"
 #import "components/segmentation_platform/embedder/default_model/feed_user_segment.h"
@@ -52,6 +53,7 @@ std::vector<std::unique_ptr<Config>> GetSegmentationPlatformConfig(
   configs.emplace_back(TabResumptionRanker::GetConfig());
   configs.emplace_back(PasswordManagerUserModel::GetConfig());
   configs.emplace_back(ShoppingUserModel::GetConfig());
+  configs.emplace_back(ChromeUserEngagement::GetConfig());
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           "test-ios-module-ranker")) {
     configs.emplace_back(TestIosModuleRanker::GetConfig());
