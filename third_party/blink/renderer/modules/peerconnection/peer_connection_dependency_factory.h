27308fb6e07207da35d3711fcae84bcce47376a8
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2025-06-24 07:44:10 -0700

    LNA: Request LNA permission for candidates with less public address spaces
    Implements LocalNetworkAccessPermissionFactory and
    LocalNetworkAccessPermission in blink. These interfaces are used by
    WebRTC to request for permission once an ICE Candidate's address has
    been resolved.
    
    The WebRTC change to introduce
    LocalNetworkAccessPermissionFactoryInterface and
    LocalNetworkAccessPermissionInterface is here:
    
    https://webrtc-review.googlesource.com/c/src/+/390520
    
    Screenshot: http://screen/7oK8g6EsA9Ck8S8
    
    Bug: 421223919
    
    Change-Id: I932c018916aed0b60d6a5a0f724ab3ba7a794a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501482
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477914}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.h
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
 #include "base/types/pass_key.h"
+#include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -149,6 +150,10 @@ class MODULES_EXPORT PeerConnectionDependencyFactory
   // passed to the WebRTC PeerConnection, allowing blink events to be coalesced
   // around the same ticks.
   virtual std::unique_ptr<webrtc::Metronome> CreateDecodeMetronome();
+
+  void BindPermissionService(
+      mojo::PendingReceiver<mojom::blink::PermissionService>
+          permission_service);
 
   void Trace(Visitor*) const override;
 
