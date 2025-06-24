07d022dc04fef6f8b8746bd6eb338388efe79f26
Wenyu Fu <wenyufu@chromium.org>
2025-06-24 01:37:08 -0700

    [SurfaceColor] Update tab group favicon empty fill color in dark
    Light: scLow (unchanged)
    Dark: scLow -> scHigh
    
    Before: https://screenshot.googleplex.com/6H3Vex4rCfFKKpM
    After: https://screenshot.googleplex.com/8ghbftf9Pvma8XS
    
    Fixed: 426014888
    Change-Id: Ic3de1d0736fad9a3be54be8d34d44d772af8fcac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6665781
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Eleanor Lee <eleanorlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477775}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiThemeProvider.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiThemeProvider.java
@@ -517,7 +517,7 @@ public class TabUiThemeProvider {
             Context context, boolean showFavicon) {
         return showFavicon
                 ? SemanticColorUtils.getColorSurfaceBright(context)
-                : SemanticColorUtils.getColorSurfaceContainerLow(context);
+                : ContextCompat.getColor(context, R.color.tab_group_favicon_quater_empty_fill_color);
     }
 
     /**
