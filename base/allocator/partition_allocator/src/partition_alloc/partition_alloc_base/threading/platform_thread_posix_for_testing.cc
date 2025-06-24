5dcc0834dcc0e92619ac4c111afb38d440100ce6
Takuto Ikuta <tikuta@chromium.org>
2025-06-24 10:39:28 -0700

    partition_alloc: Add a missing include for std::ignore
    Bug: 40263312
    Change-Id: Iee64934754620407da212c52abc82e124a524207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667551
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478032}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/threading/platform_thread_posix_for_testing.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/threading/platform_thread_posix_for_testing.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <tuple>
 
 #include "partition_alloc/build_config.h"
 #include "partition_alloc/buildflags.h"
