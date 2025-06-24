cb6940aabdb7c3a19b97cfee85a65c35d9d9d9de
Arthur Milchior <arthurmilchior@google.com>
2025-06-24 09:07:29 -0700

    [iOS]Manage Accounts Coordinator’s stop close the view
    The `stop` method should usually close its own view controllers.
    
    Before this CL, the view was dismissed either:
    * in `closeSettings`, which was called when this view is opened in the settings,
    * when the parent view is dismissed by its own parent view, in the case of the account-menu,
    
    This second case is not the way coordinators are suppsoed to works,
    and also stops working with the fix in crrev.com/c/6662560.
    
    After this CL, we guarantee that the view controller is always
    dismissed exactly once. Either by `closeSettings` if in the settings,
    or by `stop` otherwise.
    
    Bug: 425899803
    Change-Id: I6dc5a01a6f3e2ca1be9523cceac04cf5beed4d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6666860
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477956}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_coordinator.mm
@@ -174,8 +174,7 @@ using signin_metrics::PromoAction;
   }
   [_signoutCoordinator stop];
   _signoutCoordinator = nil;
-  _viewController.modelIdentityDataSource = nil;
-  _viewController = nil;
+  [self closeViewController];
   _mediator.consumer = nil;
   [_mediator disconnect];
   _mediator = nil;
@@ -186,17 +185,7 @@ using signin_metrics::PromoAction;
 
 - (void)closeSettings {
   base::RecordAction(base::UserMetricsAction("Signin_AccountsTableView_Close"));
-  if ([_viewController respondsToSelector:@selector(settingsWillBeDismissed)]) {
-    [_viewController performSelector:@selector(settingsWillBeDismissed)];
-  }
-  if (_closeSettingsOnAddAccount) {
-    [base::apple::ObjCCastStrict<SettingsNavigationController>(
-        _viewController.navigationController)
-        popViewControllerOrCloseSettingsAnimated:YES];
-  } else {
-    [_viewController.navigationController dismissViewControllerAnimated:YES
-                                                             completion:nil];
-  }
+  [self closeViewController];
   [self requestStop];
 }
 
@@ -292,6 +281,22 @@ using signin_metrics::PromoAction;
 }
 
 #pragma mark - Private
+
+- (void)closeViewController {
+  if ([_viewController respondsToSelector:@selector(settingsWillBeDismissed)]) {
+    [_viewController performSelector:@selector(settingsWillBeDismissed)];
+  }
+  if (_closeSettingsOnAddAccount) {
+    [base::apple::ObjCCastStrict<SettingsNavigationController>(
+        _viewController.navigationController)
+        popViewControllerOrCloseSettingsAnimated:YES];
+  } else {
+    [_viewController.navigationController dismissViewControllerAnimated:YES
+                                                             completion:nil];
+  }
+  _viewController.modelIdentityDataSource = nil;
+  _viewController = nil;
+}
 
 - (void)stopAddAccountCoordinator {
   [_addAccountSigninCoordinator stop];
