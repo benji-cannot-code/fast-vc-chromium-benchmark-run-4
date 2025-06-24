553c22bfecb618214045f3726afff180bdba286e
François Beaufort <beaufort.francois@gmail.com>
2025-06-24 02:30:11 -0700

    WebGPU: Remove GPUAdapter isFallbackAdapter attribute
    Intent to remove: https://groups.google.com/a/chromium.org/g/blink-dev/c/Wzr22XXV3s8
    
    Bug: 409259074
    Change-Id: I165643920bcfcac7ccba66d6b75562edb170b4e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533260
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1477803}

--- third_party/blink/renderer/modules/webgpu/gpu_adapter.h
+++ third_party/blink/renderer/modules/webgpu/gpu_adapter.h
@@ -43,7 +43,6 @@ class GPUAdapter final : public ScriptWrappable, DawnObject<wgpu::Adapter> {
   GPUSupportedFeatures* features() const;
   GPUSupportedLimits* limits() const { return limits_.Get(); }
   GPUAdapterInfo* info() const;
-  bool isFallbackAdapter() const;
   wgpu::BackendType backendType() const;
   bool SupportsMultiPlanarFormats() const;
 
@@ -74,7 +73,6 @@ class GPUAdapter final : public ScriptWrappable, DawnObject<wgpu::Adapter> {
   }
 
   Member<GPU> gpu_;
-  bool is_fallback_adapter_;
   wgpu::BackendType backend_type_;
   wgpu::AdapterType adapter_type_;
   bool is_consumed_ = false;
