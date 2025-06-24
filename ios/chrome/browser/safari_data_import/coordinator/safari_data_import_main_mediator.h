5dd549ae204bdabdd0932caceb462bf09c3b9c76
ginnnnnnny <ginnyhuang@chromium.org>
2025-06-24 05:33:14 -0700

    [iOS] Add mediator for Safari import main coordinator
    The only two things this mediator does is to
    
    1. Add a reminder promo for the workflow
    2. Manage UI blocker
    
    (Unit test powered by Gemini.)
    
    Bug: 420703283
    Change-Id: Ibe91596e043adf63ffe6cf198e0b5f5d096b2d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6658580
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477859}

---
+++ ios/chrome/browser/safari_data_import/coordinator/safari_data_import_main_mediator.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_COORDINATOR_SAFARI_DATA_IMPORT_MAIN_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_COORDINATOR_SAFARI_DATA_IMPORT_MAIN_MEDIATOR_H_
+
+#import <Foundation/Foundation.h>
+
+class PromosManager;
+@protocol UIBlockerTarget;
+
+/// A mediator for the safari data import screen entry point.
+@interface SafariDataImportMainMediator : NSObject
+
+/// Initializer.
+- (instancetype)initWithUIBlockerTarget:(id<UIBlockerTarget>)target
+                          promosManager:(PromosManager*)promosManager
+    NS_DESIGNATED_INITIALIZER;
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Displays the entry point again in a few days.
+- (void)registerReminder;
+
+/// Disconnects mediator dependencies; should be called when stopping the
+/// coordinator.
+- (void)disconnect;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_COORDINATOR_SAFARI_DATA_IMPORT_MAIN_MEDIATOR_H_
