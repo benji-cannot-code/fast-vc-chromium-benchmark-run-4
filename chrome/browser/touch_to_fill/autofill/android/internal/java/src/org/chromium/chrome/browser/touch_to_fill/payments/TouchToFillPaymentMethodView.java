a430224f06240554698714bb59f24d83acde7755
Timofey Chudakov <tchudakov@google.com>
2025-06-24 13:01:48 -0700

    [Wallet] Create all loyalty cards screen header.
    This CL creates the second screen for loyalty card filling TTF bottom
    sheet. This screen is opened when the user clicks the "All your loyalty
    cards" item on the home screen.
    
    The second screen will be populated with loyalty cards in the next CL.
    
    Screencast: http://shortn/_P4Z0kVRwCA
    
    Bug: 420957826
    Change-Id: Ie1f2cfe402ca99de02fd0a808f71a0190c72d375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6658624
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1478142}

--- chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodView.java
+++ chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodView.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.touch_to_fill.payments;
 
 import static org.chromium.build.NullUtil.assumeNonNull;
+import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillPaymentMethodProperties.ScreenId.ALL_LOYALTY_CARDS_SCREEN;
 import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillPaymentMethodProperties.ScreenId.HOME_SCREEN;
 
 import android.content.Context;
@@ -161,6 +162,8 @@ class TouchToFillPaymentMethodView extends TouchToFillViewBase {
         switch (screenId) {
             case HOME_SCREEN:
                 return 0;
+            case ALL_LOYALTY_CARDS_SCREEN:
+                return 1;
         }
         assert false : "Undefined ScreenId: " + screenId;
         return 0;
@@ -170,6 +173,8 @@ class TouchToFillPaymentMethodView extends TouchToFillViewBase {
         switch (screenId) {
             case HOME_SCREEN:
                 return R.id.touch_to_fill_payment_method_home_screen;
+            case ALL_LOYALTY_CARDS_SCREEN:
+                return R.id.touch_to_fill_all_loyalty_cards_list;
         }
         assert false : "Undefined ScreenId: " + screenId;
         return 0;
