7c268ab680456aafd6958916900977b5a4122105
Karthika Pai <karthikapai@google.com>
2025-06-24 16:02:30 -0700

    Allowlist internal extension
    Allowlist an internal extension for prototyping purposes.
    
    Change-Id: I69992f90994e1e79629fbbe95e255309e546e624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664141
    Commit-Queue: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Auto-Submit: Karthika Pai <karthikapai@google.com>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478241}

--- chrome/browser/ai/ai_data_keyed_service.cc
+++ chrome/browser/ai/ai_data_keyed_service.cc
@@ -781,7 +781,9 @@ bool AiDataKeyedService::IsExtensionAllowlistedForData(
                                        // https://issues.chromium.org/403366603
                                        "abdciamfdmknaeggbnmafmbdfdmhfgfa",
                                        // https://issues.chromium.org/414437025
-                                       "fiamdfnbelfkjlacoaeiclobkdmckaoa"});
+                                       "fiamdfnbelfkjlacoaeiclobkdmckaoa",
+                                       // Internal extension.
+                                       "mofldjifenhadohlkkngamgbifiofbnd"});
   if (base::Contains(*kHardcodedAllowlistedExtensions, extension_id)) {
     return true;
   }
@@ -831,6 +833,7 @@ bool AiDataKeyedService::IsExtensionAllowlistedForStable(
 
   // And the extension must be on this list.
   static const base::NoDestructor<std::vector<std::string>>
-      kStableChannelAllowlistedIds({});
+      kStableChannelAllowlistedIds({// Internal extension
+                                    "mofldjifenhadohlkkngamgbifiofbnd"});
   return base::Contains(*kStableChannelAllowlistedIds, extension_id);
 }
