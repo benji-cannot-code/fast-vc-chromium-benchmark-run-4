be1656d4fa5cee8eb72fbcec92f1ee9605e29af9
James Cook <jamescook@chromium.org>
2025-06-24 02:12:21 -0700

    Revert "sync: Enable extensions sync on desktop Android"
    This reverts commit 15bbc8e10024c54828635a882e79f6584b122cfc.
    
    Reason for revert: A better approach to enabling extensions sync
    on desktop Android was landed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/6656719
    
    Bug: 424217517
    Original change's description:
    > sync: Enable extensions sync on desktop Android
    >
    > This CL ties extension sync to the transport layer on desktop Android,
    > similar to bookmarks, reading list, etc. This is a temporary measure,
    > as there's an ongoing migration of desktop sync that will likely break
    > this code. That's OK, we just need something for desktop Android
    > dogfooding.
    >
    > Bug: 424217517
    > Test: existing components_unittests and sync_integration_tests
    > Change-Id: I480a1fc72401f6f914ad235fa573d8cf872f03e2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654893
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1476694}
    
    Bug: 424217517
    Change-Id: I989200374e33d17f5138686eb247bfc6b5e4fd76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663818
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477794}

--- components/sync/service/sync_prefs_unittest.cc
+++ components/sync/service/sync_prefs_unittest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/protocol/nigori_specifics.pb.h"
 #include "components/sync/service/glue/sync_transport_data_prefs.h"
-#include "extensions/buildflags/buildflags.h"
 #include "google_apis/gaia/gaia_id.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
