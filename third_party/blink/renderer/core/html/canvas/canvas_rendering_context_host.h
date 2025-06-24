02973efb6c59c4949114e85e705c52cddce83a53
Colin Blundell <blundell@chromium.org>
2025-06-23 23:34:48 -0700

    [Blink] Eliminate CanvasRCHost::GetOrCreateCRP2D()
    Inline it in its only caller in HTMLCanvasElement.
    
    Bug: 352263194
    Change-Id: I674abd29b6ad30479265016eb22dd7548ea2b9e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6662358
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477738}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h
@@ -105,8 +105,6 @@ class CORE_EXPORT CanvasRenderingContextHost : public GarbageCollectedMixin,
 
   // Partial CanvasResourceHost implementation
   void InitializeForRecording(cc::PaintCanvas*) const final;
-  virtual CanvasResourceProvider*
-  GetOrCreateCanvasResourceProviderForCanvas2D();
   virtual void PageVisibilityChanged();
 
   bool IsWebGL() const;
@@ -186,6 +184,8 @@ class CORE_EXPORT CanvasRenderingContextHost : public GarbageCollectedMixin,
 
   bool ContextHasOpenLayers(const CanvasRenderingContext*) const;
 
+  void CreateCanvasResourceProvider2D();
+
   // Computes the digest that corresponds to the "input" of this canvas,
   // including the context type, and if applicable, canvas digest, and taint
   // bits.
@@ -199,7 +199,6 @@ class CORE_EXPORT CanvasRenderingContextHost : public GarbageCollectedMixin,
   bool did_fail_to_create_resource_provider_ = false;
 
  private:
-  void CreateCanvasResourceProvider2D();
 
   std::unique_ptr<CanvasResourceProvider> resource_provider_for_canvas2d_;
   std::unique_ptr<CanvasResourceProvider> resource_provider_for_image_bitmap_;
