00425aebb53d7b47f9923008734de3c08cfdc44d
Tom Sepez <tsepez@chromium.org>
2025-06-24 10:25:24 -0700

    Remove now-unused IPC_STRUCT_{BEGIN,END,MEMBER} macros.
    Legacy IPC is no longer marshaling through these macros, however
    the IPC_STRUCT_TRAITS_* macros are still used to serialize some
    legacy types under mojom.
    
    Change-Id: Iadeebdef4edb52bb6e1ee7b0b656be7ba908c618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6665139
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478016}

--- ipc/param_traits_log_macros.h
+++ ipc/param_traits_log_macros.h
@@ -11,15 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Null out all the macros that need nulling.
 #include "ipc/ipc_message_null_macros.h"
 
-// STRUCT declarations cause corresponding STRUCT_TRAITS declarations to occur.
-#undef IPC_STRUCT_BEGIN_WITH_PARENT
-#undef IPC_STRUCT_MEMBER
-#undef IPC_STRUCT_END
-#define IPC_STRUCT_BEGIN_WITH_PARENT(struct_name, parent) \
-  IPC_STRUCT_TRAITS_BEGIN(struct_name)
-#define IPC_STRUCT_MEMBER(type, name, ...) IPC_STRUCT_TRAITS_MEMBER(name)
-#define IPC_STRUCT_END() IPC_STRUCT_TRAITS_END()
-
 // Set up so next include will generate log methods.
 #undef IPC_STRUCT_TRAITS_BEGIN
 #undef IPC_STRUCT_TRAITS_MEMBER
