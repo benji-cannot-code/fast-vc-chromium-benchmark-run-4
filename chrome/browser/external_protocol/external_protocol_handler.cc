d381a52855edf86f7f9f003791caa612894e82cb
Andrew Rayskiy <greengrape@google.com>
2025-06-24 02:53:21 -0700

    Close initial tab after protocol navigation on ChromeOS
    This CL aligns the ChromeOS behavior with desktop behavior for protocol
    handling and closes the tab that the protocol navigation took place in
    if it's still in the initial navigation phase (aka NTP).
    
    Bug: 302328814
    Change-Id: I9ccadd45651c5f014686f23f5c91b1a5f423e194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653675
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477819}

--- chrome/browser/external_protocol/external_protocol_handler.cc
+++ chrome/browser/external_protocol/external_protocol_handler.cc
@@ -58,6 +58,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/constants/chromeos_features.h"
+#endif
+
 namespace {
 
 // Anti-flood protection controls whether we accept requests for launching
@@ -196,7 +200,7 @@ void LaunchUrlWithoutSecurityCheckWithDelegate(
 #endif
       url);
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS)
+#if !BUILDFLAG(IS_ANDROID)
   // If the protocol navigation occurs in a new tab, close it.
   // Avoid calling CloseContents if the tab is not in this browser's tab strip
   // model; this can happen if the protocol was initiated by something
@@ -205,7 +209,11 @@ void LaunchUrlWithoutSecurityCheckWithDelegate(
   if (browser && web_contents->GetController().IsInitialNavigation() &&
       browser->tab_strip_model()->count() > 1 &&
       browser->tab_strip_model()->GetIndexOfWebContents(web_contents) !=
-          TabStripModel::kNoTab) {
+          TabStripModel::kNoTab
+#if BUILDFLAG(IS_CHROMEOS)
+      && chromeos::features::IsWebAppManifestProtocolHandlerSupportEnabled()
+#endif
+  ) {
     // Defer destruction of `WebContents` to avoid synchronously destroying
     // NavigationURLLoader(Impl) here. See https://issues.chromium.org/361600654
     content::GetUIThreadTaskRunner({})->PostTask(
