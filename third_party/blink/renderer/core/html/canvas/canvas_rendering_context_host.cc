12a8f0ed336b382834abc1ede04485fb1ee1ad2a
Colin Blundell <blundell@chromium.org>
2025-06-23 23:34:34 -0700

    [Blink] Move WebGPU resource provider from CRCHost to WebGPU context
    The only subtle part is ensuring that the resource provider continues to
    be reset at every relevant place where DiscardResources() is called.
    They are:
    
    * Dispose() [1][2]
    * size change [3]
    * from within WebGPU context itself on detecting a size mismatch [4]
      (note: potentially redundant with [3], but outside the scope of this
       CL)
    
    The other calls are all for other context types.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/canvas/html_canvas_element.cc;l=456?q=html_canvas_element.cc&ss=chromium
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc;l=123?q=offscreen_canvas.cc&ss=chromium
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/canvas/html_canvas_element.cc;l=1320?q=html_canvas_element.cc&ss=chromium
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc;l=205?q=gpu_canvas_context.cc&ss=chromium
    
    Bug: 352263194
    Change-Id: I77fb543d3e0e306047f54cd772a7b31d8e8d0a42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6658161
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477737}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
@@ -398,7 +398,6 @@ CanvasRenderingContextHost::ReplaceResourceProviderForCanvas2D(
 void CanvasRenderingContextHost::DiscardResources() {
   resource_provider_for_canvas2d_ = nullptr;
   resource_provider_for_image_bitmap_ = nullptr;
-  resource_provider_for_webgpu_ = nullptr;
   UpdateMemoryUsage();
 }
 
