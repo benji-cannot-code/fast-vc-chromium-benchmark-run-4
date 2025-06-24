7bb6dc75581e7ef53f9972fbd450a035a59ff955
Lei Zhang <thestig@chromium.org>
2025-06-24 00:24:09 -0700

    Remove url/origin_debug.h from url/origin.h
    Separate the two headers and do IWYU to fix the build.
    
    Change-Id: I4d68cb276ae21d8a24622461bca64cab293645ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6665325
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477751}

--- chrome/browser/web_applications/isolated_web_apps/commands/remove_obsolete_bundle_versions_cache_command.cc
+++ chrome/browser/web_applications/isolated_web_apps/commands/remove_obsolete_bundle_versions_cache_command.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/task/thread_pool.h"
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
--- url/origin.h
+++ url/origin.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/buildflag.h"
 #include "build/robolectric_buildflags.h"
-#include "url/origin_debug.h"  // TODO(thestig): Remove.
 #include "url/scheme_host_port.h"
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_ROBOLECTRIC)
