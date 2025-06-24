95df5e2eb7b91378d5fd9ab02392641963a4d804
ginnnnnnny <ginnyhuang@chromium.org>
2025-06-23 17:11:35 -0700

    [iOS] Make CHECK on -[PrefBackedBoolean dealloc] not fatal until M140
    The root cause of the issue in Settings is so complicated that just
    stopping individual `PrefBackedBoolean` objects will not really resolve
    the issue. It will just trigger the same CHECK somewhere else.
    
    Pushing the FATAL milestone to M140 so we have time to get to the bottom
    of this.
    
    Bug: 426584854
    Change-Id: I8516020593e097a77b27b697d836348a8b5fb694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663884
    Reviewed-by: Harry Souders <harrysouders@google.com>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Owners-Override: Harry Souders <harrysouders@google.com>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477639}

--- ios/chrome/browser/shared/model/prefs/pref_backed_boolean.mm
+++ ios/chrome/browser/shared/model/prefs/pref_backed_boolean.mm
@@ -31,7 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)dealloc {
-  CHECK(_stopped) << "-stop should be called on PrefBackedBoolean";
+  CHECK(_stopped, base::NotFatalUntil::M140)
+      << "-stop should be called on PrefBackedBoolean";
 }
 
 - (BOOL)value {
