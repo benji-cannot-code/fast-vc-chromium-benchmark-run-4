d7d6cf0b1c9aeeb9044f7d37cdd904f6c7489e2e
Robbie McElrath <rmcelrath@chromium.org>
2025-06-24 14:31:20 -0700

    ondevice: Prefill model path in on-device-internals
    This prefills the "Model directory" text input with the path of the
    default base model.
    
    Change-Id: I5513b91a609df5c6544791fb827eb9115493fbf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6655319
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478192}

--- chrome/browser/resources/on_device_internals/tools.ts
+++ chrome/browser/resources/on_device_internals/tools.ts
@@ -14,6 +14,7 @@ import type {CrInputElement} from '//resources/cr_elements/cr_input/cr_input.js'
 import {assert} from '//resources/js/assert.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
+import type {FilePath} from '//resources/mojo/mojo/public/mojom/base/file_path.mojom-webui.js';
 
 import {BrowserProxy} from './browser_proxy.js';
 import type {AudioData, Capabilities, InputPiece, ResponseChunk, ResponseSummary} from './on_device_model.mojom-webui.js';
@@ -84,6 +85,16 @@ function textToInputPieces(text: string): InputPiece[] {
   return input;
 }
 
+function filePathToString(filePath: FilePath): string {
+  if (typeof filePath.path === 'string') {
+    return filePath.path;
+  }
+
+  const decoder = new TextDecoder('utf-16');
+  const buffer = new Uint16Array(filePath.path);
+  return decoder.decode(buffer);
+}
+
 class OnDeviceInternalsToolsElement extends CrLitElement {
   static get is() {
     return 'on-device-internals-tools';
@@ -99,6 +110,7 @@ class OnDeviceInternalsToolsElement extends CrLitElement {
 
   static override get properties() {
     return {
+      defaultModelPath_: {type: String},
       modelPath_: {type: String},
       error_: {type: String},
       imageError_: {type: String},
@@ -128,6 +140,7 @@ class OnDeviceInternalsToolsElement extends CrLitElement {
   protected accessor currentResponse_: Response|null = null;
   protected accessor error_: string = '';
   protected accessor imageError_: string = '';
+  protected accessor defaultModelPath_: string = '';
   private loadModelDuration_: number = -1;
   private accessor loadModelStart_: number = 0;
   private accessor modelPath_: string = '';
@@ -150,6 +163,7 @@ class OnDeviceInternalsToolsElement extends CrLitElement {
 
   override firstUpdated() {
     this.getPerformanceClass_();
+    this.getDefaultModelPath_();
     this.$.temperatureInput.inputElement.step = '0.1';
     this.$.imageInput.addEventListener(
         'change', this.onImageChange_.bind(this));
@@ -173,6 +187,14 @@ class OnDeviceInternalsToolsElement extends CrLitElement {
     this.performanceClassText_ = getPerformanceClassText(
         (await this.proxy_.handler.getDevicePerformanceInfo())
             .performanceInfo.performanceClass);
+  }
+
+  private async getDefaultModelPath_() {
+    const defaultModelPath = await this.proxy_.handler.getDefaultModelPath();
+    if (defaultModelPath.modelPath === null) {
+      return;
+    }
+    this.defaultModelPath_ = filePathToString(defaultModelPath.modelPath);
   }
 
   private onModelOrErrorChanged_() {
