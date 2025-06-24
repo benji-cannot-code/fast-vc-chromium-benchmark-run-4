6ca35ebb3b78405c04f9c15a76b9d103a351a4ad
Patrick Noland <pnoland@google.com>
2025-06-24 09:06:04 -0700

    [DSE Parity] Don't skip for neutral state when flag is enabled
    Skipping for neutral state without an incognito badge means we get no
    status icon at all on chrome:// pages (and other pages with a security
    level of NONE).
    
    Bug: 426663492
    Change-Id: I47aed15f1d5eb585cd8366b8ecd7ae40c530d9c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664022
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477954}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/LocationBarModel.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/LocationBarModel.java
@@ -46,6 +46,7 @@ import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.components.metrics.OmniboxEventProtos.OmniboxEventProto.PageClassification;
 import org.chromium.components.omnibox.AutocompleteSchemeClassifier;
+import org.chromium.components.omnibox.OmniboxFeatures;
 import org.chromium.components.omnibox.OmniboxUrlEmphasizer;
 import org.chromium.components.omnibox.SecurityStatusIcon;
 import org.chromium.components.security_state.ConnectionSecurityLevel;
@@ -695,7 +696,8 @@ public class LocationBarModel implements ToolbarDataProvider, LocationBarDataPro
         boolean skipIconForNeutralState =
                 (mProfile != null
                                 && !SearchEngineUtils.getForProfile(mProfile)
-                                        .shouldShowSearchEngineLogo())
+                                        .shouldShowSearchEngineLogo()
+                                && !OmniboxFeatures.sOmniboxMobileParityUpdate.isEnabled())
                         || mNtpDelegate.isCurrentlyVisible();
 
         return SecurityStatusIcon.getSecurityIconResource(
