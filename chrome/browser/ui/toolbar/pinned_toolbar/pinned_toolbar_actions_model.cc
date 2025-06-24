c6b6558b49eca8564a19b4801cc7edc6ba468475
Antonio Gomes <tonikitoo@igalia.com>
2025-06-23 19:49:54 -0700

    Componentize chrome/browser/ui/toolbar (2/x)
    This CL componentizes the remaining .cc files in chrome/browser/ui/toolbar by
    building them remaining from the proper BUILD.gn files.
    
    The following targets are complete:
      - //chrome/browser/ui/toolbar:impl,
      - //chrome/browser/ui/tollbar/pinned_toolbar:imp
      - //chrome/browser/ui/tollbar:test_support
    
    In a follow up step, unit_tests, browser_tests and interactive_ui_tests
    will be handled.
    
    Bug: 369436587
    
    R=dljames@chromium.org
    
    Change-Id: Ic6d91bd7a5fc51388d6cae2d371cde76519bfeed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6662069
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477683}

--- chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.cc
+++ chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model_factory.h"
 #include "chrome/browser/ui/toolbar/toolbar_pref_names.h"
 #include "chrome/browser/ui/ui_features.h"
