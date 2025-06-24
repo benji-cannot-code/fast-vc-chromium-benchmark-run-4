9581d522f03e478ad94d01e51bd854bdd155cf1d
Rohit Rao <rohitrao@chromium.org>
2025-06-24 10:53:26 -0700

    Revert "[tvOS] Make iOS test runner infrastructure support tvOS"
    This reverts commit dfe1e62ce3515e9cf3d46055a7dfa98302eaff91.
    
    Reason for revert: Breaks internal device bots
    
    Traceback (most recent call last):
      File "/Volumes/Work/s/w/ir/out/e8e0-device-builder/../../ios/build/bots/scripts/run.py", line 208, in run
        tr = xcodebuild_runner.DeviceXcodeTestRunner(
             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Volumes/Work/s/w/ir/ios/build/bots/scripts/xcodebuild_runner.py", line 592, in __init__
        self.all_eg_test_names = self.fetch_test_names()
                                 ^^^^^^^^^^^^^^^^^^^^^^^
      File "/Volumes/Work/s/w/ir/ios/build/bots/scripts/xcodebuild_runner.py", line 361, in fetch_test_names
        xctestrun = self._create_xctest_run_enum_tests(include_disabled)
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Volumes/Work/s/w/ir/ios/build/bots/scripts/xcodebuild_runner.py", line 350, in _create_xctest_run_enum_tests
        test_app = self.get_launch_test_app()
                   ^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Volumes/Work/s/w/ir/ios/build/bots/scripts/xcodebuild_runner.py", line 457, in get_launch_test_app
        self.platform_type,
        ^^^^^^^^^^^^^^^^^^
    AttributeError: 'DeviceXcodeTestRunner' object has no attribute 'platform_type'
    
    Bug: 400486355
    Original change's description:
    > [tvOS] Make iOS test runner infrastructure support tvOS
    >
    > This makes it possible to call e.g. <out-dir>/bin/run_base_unittests
    > and other run.py wrappers on tvOS builds.
    >
    > tl;dr: Using run_*tests on both tvOS and iOS should Just Work without
    > any user-visible changes.
    >
    > Running tests on an actual device is still not supported, so the device
    > test runners still all hardcode iOS as the only supported platform.
    >
    > The actual required changes are quite simple:
    > - iossim_util needs to accept tvOS runtimes when parsing `simctl
    >   list`'s  output.
    > - We need to pass different paths to DYLD_{LIBRARY,FRAMEWORK}_PATH
    >   depending on whether this is a tvOS or an iOS build.
    >
    > The latter is achieved by deriving the intended target platform from the
    > value passed to --platform since the platform name will have a fixed
    > device type identifier we can use.
    >
    > Other relevant changes:
    > - wpr_runner_test.py and xcodebuild_runner_test.py: Make some test
    >   classes derive from test_runner_test.SimulatorTestRunnerTest so that
    >   they can call setUp() there, which adds the appropriate iossim_util
    >   mock for the new call in SimulatorTestRunner's constructor. This also
    >   allows us to remove some duplicated mock calls that are all
    >   centralized in the parent class's setUp() implementation.
    >
    > Note that this CL does not change xcode_util, which remains unable to
    > download tvOS SDKs and images. This must be done separately as part of
    > bug 400486355.
    >
    > With help from Abhijeet Kandalkar <abhijeet@igalia.com>
    >
    > Bug: 400486355
    > Cq-Include-Trybots: luci.chromium.try:ios-blink-rel-fyi
    > Change-Id: Ie3574ce0a818adc4bf19c024e79621cecf87f512
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368755
    > Reviewed-by: Yue She <yueshe@google.com>
    > Commit-Queue: Raphael Kubo da Costa <kubo@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1477888}
    
    Bug: 400486355
    Cq-Include-Trybots: luci.chromium.try:ios-blink-rel-fyi
    Change-Id: I87437d43896d57e345b3fe4675ef9c610e3f5f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668893
    Auto-Submit: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Yue She <yueshe@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478041}

--- ios/build/bots/scripts/constants.py
+++ ios/build/bots/scripts/constants.py
@@ -2,9 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2022 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Constants and types used in various modules."""
-
-import enum
+"""Constants used in various modules."""
 
 # Default Timeout (seconds) to kill a test process when it doesn't have output.
 READLINE_TIMEOUT = 180
@@ -22,13 +20,3 @@ CRASH_MESSAGE = 'App crashed and disconnected.'
 ASAN_ERROR = 'ERROR: AddressSanitizer'
 
 LAYOUT_CONSTRAINT_MSG = 'Omitting layout constraint warnings'
-
-
-class IOSPlatformType(enum.Enum):
-  """The iOS-based platform being targeted"""
-
-  # This matches the possible values for `target_platform` in
-  # //build/config/apple/mobile_config.gni.
-
-  IPHONEOS = enum.auto()
-  TVOS = enum.auto()
