3b524a08dde12282f7b2afca0e0f9c353e4fe457
Akihiro Ota <akihiroota@chromium.org>
2025-06-24 11:00:29 -0700

    a11y-mv3: Add mv3/ directory for enhanced network tts
    This change duplicates the contents of the mv2/ directory into a new
    mv3/ directory. This will allow us to make changes for the mv3
    migration without impacting the mv2/ version of enhanced network tts.
    
    Bug: 388867658
    Change-Id: I6c07a569e983b0b2e4cba27c1789f8d0cbd3bc53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668193
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478046}

---
+++ chrome/browser/resources/chromeos/accessibility/enhanced_network_tts/mv3/enhanced_network_tts_e2e_test_base.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+GEN_INCLUDE(['../../common/testing/e2e_test_base.js']);
+GEN_INCLUDE(['mock_mojo_private.js']);
+
+/**
+ * Test fixture for Enhanced Network TTS E2E tests.
+ */
+EnhancedNetworkTE2ETestBase = class extends E2ETestBase {
+  /** @override */
+  constructor() {
+    super();
+    this.mockMojoPrivate = MockMojoPrivate;
+    chrome.mojoPrivate = this.mockMojoPrivate;
+    this.onSpeakWithAudioStreamListeners = [];
+    this.onStopListeners = [];
+    chrome.ttsEngine = {
+      onSpeakWithAudioStream: {
+        addListener: callback => {
+          this.onSpeakWithAudioStreamListeners.push(callback);
+        },
+      },
+      onStop: {
+        addListener: callback => {
+          this.onStopListeners.push(callback);
+        },
+      },
+    };
+  }
+
+  /** @override */
+  testGenPreamble() {
+    super.testGenPreamble();
+    GEN(`
+    base::OnceClosure load_cb =
+        base::BindOnce(
+            &ash::AccessibilityManager::LoadEnhancedNetworkTtsForTest,
+            base::Unretained(ash::AccessibilityManager::Get()));
+    `);
+  }
+};
