7e3d471741f9693aafca19c710ea737412c12db3
Yuwei Huang <yuweih@chromium.org>
2025-06-24 14:41:32 -0700

    [crd webauthn] Health-checks IPC with timeout
    Once the WebAuthn remote is bound, we rely on the fact that the
    disconnect handler will be called whenever the IPC disconnects or the
    remote session ends to tell if the remote is actually valid. However,
    crbug.com/425759818 shows that this is not necessarily reliable. Even
    though the issue has been mitigated, reliability can still be improved.
    
    getRemoteState sort of does an explicit IPC "health check" by calling
    QueryVersion on the remote, but it isn't applied to all other requests
    and it doesn't have a timeout logic. This CL improves this by moving the
    QueryVersion to EnsureIpcConnection (and renaming it) and adding a
    timeout logic that treats the remote as disconnected if no response is
    received in 5 seconds.
    
    With this change, the NMH will consider the IPC connection to be
    disconnected if the host hasn't bound the pending receiver to a
    `WebAuthnProxy` receiver within 5 seconds.
    
    Bug: 425759818
    Change-Id: I708762545108caaf4c30291cca19b9eda398880d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6659065
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478198}

--- remoting/host/webauthn/remote_webauthn_native_messaging_host_unittest.cc
+++ remoting/host/webauthn/remote_webauthn_native_messaging_host_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "remoting/host/webauthn/remote_webauthn_native_messaging_host.h"
+
 #include <memory>
 
 #include "base/check_deref.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/gmock_callback_support.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "extensions/browser/api/messaging/native_message_host.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -24,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/native_messaging/log_message_handler.h"
 #include "remoting/host/native_messaging/native_messaging_constants.h"
 #include "remoting/host/webauthn/remote_webauthn_constants.h"
-#include "remoting/host/webauthn/remote_webauthn_native_messaging_host.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -74,7 +76,7 @@ class MockWebAuthnProxy : public mojom::WebAuthnProxy {
  public:
   MOCK_METHOD(void,
               IsUserVerifyingPlatformAuthenticatorAvailable,
-              (IsUserVerifyingPlatformAuthenticatorAvailableCallback),
+              (IsUvpaaCallback),
               (override));
   MOCK_METHOD(void,
               Create,
@@ -131,18 +133,23 @@ class RemoteWebAuthnNativeMessagingHostTest
 
   void ResetReceiver();
 
+  base::test::TaskEnvironment task_environment_{
+      base::test::TaskEnvironment::MainThreadType::IO,
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
+
   MockWebAuthnProxy webauthn_proxy_;
-  raw_ptr<MockChromotingHostServicesProvider, DanglingUntriaged> api_provider_;
   MockChromotingSessionServices api_;
 
  private:
-  base::test::TaskEnvironment task_environment_{
-      base::test::TaskEnvironment::MainThreadType::IO};
   std::unique_ptr<RemoteWebAuthnNativeMessagingHost> host_;
   std::unique_ptr<base::RunLoop> response_run_loop_;
   mojo::Receiver<mojom::WebAuthnProxy> webauthn_proxy_receiver_{
       &webauthn_proxy_};
   base::Value::Dict latest_message_;
+
+ protected:
+  // `api_provider_` must be deleted before `host_`.
+  raw_ptr<MockChromotingHostServicesProvider> api_provider_;
 };
 
 RemoteWebAuthnNativeMessagingHostTest::RemoteWebAuthnNativeMessagingHostTest() {
@@ -275,14 +282,31 @@ TEST_F(RemoteWebAuthnNativeMessagingHostTest, IsUvpaa) {
 }
 
 TEST_F(RemoteWebAuthnNativeMessagingHostTest,
-       ClientDisconnectedWhenRequestIsPending_MessageSent) {
+       ClientDisconnectedWhenRequestIsPending_ClientDisconnectedMessageSent) {
   ExpectGetSessionServices();
   ExpectBindWebAuthnProxy();
   EXPECT_CALL(webauthn_proxy_, IsUserVerifyingPlatformAuthenticatorAvailable(_))
-      .WillOnce([&](mojom::WebAuthnProxy::
-                        IsUserVerifyingPlatformAuthenticatorAvailableCallback
-                            callback) { ResetReceiver(); });
+      .WillOnce([&](IsUvpaaCallback callback) { ResetReceiver(); });
   SendMessage(CreateRequestMessage(kIsUvpaaMessageType));
+
+  const base::Value::Dict& response = ReadMessage();
+  ASSERT_EQ(CHECK_DEREF(response.FindString(kMessageType)),
+            kClientDisconnectedMessageType);
+}
+
+TEST_F(RemoteWebAuthnNativeMessagingHostTest,
+       PendingReceiverNotBoundWithinTimeout_ClientDisconnectedMessageSent) {
+  ExpectGetSessionServices();
+  mojo::PendingReceiver<mojom::WebAuthnProxy> pending_receiver;
+  EXPECT_CALL(api_, BindWebAuthnProxy(_))
+      .WillOnce([&](mojo::PendingReceiver<mojom::WebAuthnProxy> pr) {
+        pending_receiver = std::move(pr);
+      });
+  EXPECT_CALL(webauthn_proxy_, IsUserVerifyingPlatformAuthenticatorAvailable(_))
+      .Times(0);
+
+  SendMessage(CreateRequestMessage(kIsUvpaaMessageType));
+  task_environment_.FastForwardBy(base::Minutes(1));
 
   const base::Value::Dict& response = ReadMessage();
   ASSERT_EQ(CHECK_DEREF(response.FindString(kMessageType)),
