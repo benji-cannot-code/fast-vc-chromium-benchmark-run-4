9444e1c24fc53ba9d9f3ee3ee382fbaac6e07592
Saifuddin Hitawala <hitawala@chromium.org>
2025-06-24 09:43:47 -0700

    [gpu] Remove GMBImplSharedMemory::IsConfigurationSupported
    Remove GpuMemoryBufferImplSharedMemory::IsConfigurationSupported
    and directly call IsUsageSupported from the callsite.
    
    Bug: 404958317
    Change-Id: Iad6608abede04c442e44b5fff005732921e058ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668332
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477983}

--- gpu/ipc/common/gpu_memory_buffer_support.cc
+++ gpu/ipc/common/gpu_memory_buffer_support.cc
@@ -216,8 +216,7 @@ bool GpuMemoryBufferSupport::IsConfigurationSupportedForTest(
   }
 
   if (type == gfx::SHARED_MEMORY_BUFFER) {
-    return GpuMemoryBufferImplSharedMemory::IsConfigurationSupported(format,
-                                                                     usage);
+    return GpuMemoryBufferImplSharedMemory::IsUsageSupported(usage);
   }
 
   NOTREACHED();
