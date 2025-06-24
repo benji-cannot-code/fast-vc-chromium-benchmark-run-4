fb79a84ae354e7aa4bf57f676a58bb1983bd4d25
Xinghui Lu <xinghuilu@chromium.org>
2025-06-24 14:58:01 -0700

    Add ClientSideDetectionIntelligentScanDelegateAndroidTest
    Bug: 424104358
    Change-Id: Ie52f505e942775274ca408948ffb06ea1e3403ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663962
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478211}

---
+++ chrome/browser/safe_browsing/android/client_side_detection_intelligent_scan_delegate_android_unittest.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/safe_browsing/android/client_side_detection_intelligent_scan_delegate_android.h"
+
+#include "components/safe_browsing/core/common/proto/csd.pb.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace safe_browsing {
+
+class ClientSideDetectionIntelligentScanDelegateAndroidTest
+    : public testing::Test {
+ public:
+  ClientSideDetectionIntelligentScanDelegateAndroidTest() = default;
+};
+
+TEST_F(ClientSideDetectionIntelligentScanDelegateAndroidTest,
+       ShouldRequestIntelligentScan) {
+  ClientSideDetectionIntelligentScanDelegateAndroid delegate;
+  ClientPhishingRequest verdict;
+  EXPECT_FALSE(delegate.ShouldRequestIntelligentScan(&verdict));
+}
+
+}  // namespace safe_browsing
