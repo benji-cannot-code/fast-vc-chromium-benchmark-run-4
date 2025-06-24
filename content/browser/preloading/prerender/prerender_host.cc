8c2e45339d48ef35815900d80d261c5006d7e749
Jiacheng Guo <gjc@google.com>
2025-06-24 01:57:32 -0700

    Add UMA for measuring potential same-origin prerender process reuse
    The CL adds a UMA for recording if there is a prerender host under the
    same origin when the navigation happens. We may reuse the prerender
    process in this case.
    
    Bug: 427054641
    Change-Id: Ida57b79e8284d000ef40e5b183ffb4ed8e62ad0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654328
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477787}

--- content/browser/preloading/prerender/prerender_host.cc
+++ content/browser/preloading/prerender/prerender_host.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/prerender/prerender_final_status.h"
 #include "content/browser/preloading/prerender/prerender_host_registry.h"
 #include "content/browser/preloading/prerender/prerender_metrics.h"
+#include "content/browser/preloading/prerender/prerender_navigation_utils.h"
 #include "content/browser/renderer_host/frame_tree.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/renderer_host/navigation_controller_impl.h"
@@ -1374,6 +1375,15 @@ bool PrerenderHost::IsNoVarySearchHintUrlMatch(const GURL& url) const {
   }
 
   return false;
+}
+
+bool PrerenderHost::IsUrlSameOrigin(const GURL& url) const {
+  return url::IsSameOriginWith(GetInitialUrl(), url);
+}
+
+bool PrerenderHost::IsUrlSameSite(const GURL& url) const {
+  return prerender_navigation_utils::IsSameSite(
+      url, url::Origin::Create(GetInitialUrl()));
 }
 
 void PrerenderHost::OnAcceptClientHintChanged(
