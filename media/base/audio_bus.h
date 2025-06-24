4de44e9eb8a06abbd679b981df86813d7339a50a
David Sanders <dsanders11@ucsbalum.com>
2025-06-24 16:59:00 -0700

    Remove unused include in //media/base/audio_bus.h
    Bug: 40318405
    Change-Id: I34a9e9318ce9cac411f33e02ba1393c364bbe7a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635634
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1478267}

--- media/base/audio_bus.h
+++ media/base/audio_bus.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/aligned_memory.h"
 #include "base/memory/raw_ptr_exclusion.h"
-#include "media/base/audio_sample_types.h"
 #include "media/base/media_shmem_export.h"
 
 namespace media {
