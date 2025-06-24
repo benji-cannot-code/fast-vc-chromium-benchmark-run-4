100fb1dbcc7db264f6c3a7622a76448244dfc43f
Jan Keitel <jkeitel@google.com>
2025-06-23 22:51:23 -0700

    Allow transparent look-ups in base::LRUCache
    It is currently possible to construct a base::LRUCache with for which
    the underlying container supports transparent comparison:
    base::LRUCache<std::string, int> cache(10);
    
    However, transparent look-ups do not work. The following does not
    compile:
    cache.Get(std::string_view("asd"));
    
    This CL allows transparent Get and Peek in LRUCache.
    
    Change-Id: I67b039c779e58a913551f22cd789609d2721a139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6655128
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477719}

--- base/containers/lru_cache_unittest.cc
+++ base/containers/lru_cache_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cstddef>
+#include <functional>
 #include <iterator>
 #include <memory>
 #include <string>
@@ -638,6 +639,13 @@ TEST(LRUCacheIndexOrderTest, IndexIteration) {
       [](const auto& key_value_pair) -> int { return key_value_pair.first; });
   EXPECT_THAT(unordered_keys, testing::UnorderedElementsAre(
                                   kItem1Key, kItem2Key, kItem3Key, kItem4Key));
+}
+
+TEST(LRUCacheSimpleTest, TransparentLookup) {
+  LRUCache<std::string, int, std::less<>> cache(10);
+  cache.Put("some string", 4);
+  EXPECT_EQ(cache.Get(std::string_view("some string"))->second, 4);
+  EXPECT_EQ(cache.Peek(std::string_view("some string"))->second, 4);
 }
 
 }  // namespace base
