3356b9dba4680b61669237232f678793868fb365
Kevin Graney <kmg@google.com>
2025-06-24 13:05:27 -0700

    Add UiEventDispatcher skeleton, and wire to ExecutionEngine
    ToolRequests will be passed into this new object pre- and post-
    invocation of ToolController. This object will be responsible for
    finding the correct per-profile ActorStateUiManager and passing to it a
    list of events that may update UI state.
    
    This object will be responsible for translating ToolRequests to a
    sequence of events. It will also be responsible for providing status
    back to ExecutionEngine.
    
    ExecutionEngine now maintains a basic state machine of its callback
    sequence, used only for logging and DCHECKs.
    
    Bug: 425784083
    Change-Id: I6c2a1080a47676a5f9c94df809044073e6f6d7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653412
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1478147}

---
+++ chrome/browser/actor/ui/mock_event_dispatcher.h
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ACTOR_UI_MOCK_EVENT_DISPATCHER_H_
+#define CHROME_BROWSER_ACTOR_UI_MOCK_EVENT_DISPATCHER_H_
+
+#include "chrome/browser/actor/ui/event_dispatcher.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+class Profile;
+namespace actor {
+class ToolRequest;
+
+class MockUiEventDispatcher : public UiEventDispatcher {
+ public:
+  MockUiEventDispatcher();
+  ~MockUiEventDispatcher() override;
+  MOCK_METHOD(void,
+              OnPreTool,
+              (Profile * profile,
+               const ToolRequest& tool_request,
+               UiCompleteCallback callback),
+              (override));
+
+  MOCK_METHOD(void,
+              OnPostTool,
+              (Profile * profile,
+               const ToolRequest& tool_request,
+               UiCompleteCallback callback),
+              (override));
+};
+
+std::unique_ptr<UiEventDispatcher> NewMockUiEventDispatcher();
+
+}  // namespace actor
+
+#endif  // CHROME_BROWSER_ACTOR_UI_MOCK_EVENT_DISPATCHER_H_
