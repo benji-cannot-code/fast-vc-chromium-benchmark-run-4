0a0dff07e2c52f1649af840ef723171fb52a3db2
Fredrik Söderquist <fs@opera.com>
2025-06-25 06:07:56 -0700

    Move specialized GraphicsContext::DrawLine() to BoxBorderPainter
    This function is highly specialized around painting of borders. Move it
    to BoxBorderPainter with minimal changes and rename it to
    DrawLineWithStyle().
    
    Bug: 425866014
    Change-Id: Ia3273c622c4a5516d671ef3ec38dfd68189ebd8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6671729
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1478498}

--- third_party/blink/renderer/core/paint/box_border_painter.h
+++ third_party/blink/renderer/core/paint/box_border_painter.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ComputedStyle;
+class StyledStrokeData;
 
 typedef unsigned BorderEdgeFlags;
 
@@ -49,6 +50,15 @@ class BoxBorderPainter {
                           Color color,
                           EBorderStyle style,
                           const AutoDarkMode& auto_dark_mode);
+
+  // DrawLineWithStyle() only operates on horizontal or vertical lines and uses
+  // the current stroke color. For dotted or dashed stroke, the line need to be
+  // top-to-down or left-to-right to get correct interval of dots/dashes.
+  static void DrawLineWithStyle(GraphicsContext& context,
+                                const gfx::Point&,
+                                const gfx::Point&,
+                                const StyledStrokeData&,
+                                const AutoDarkMode& auto_dark_mode);
 
  private:
   // For PaintBorder().
