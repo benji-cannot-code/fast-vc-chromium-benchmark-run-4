7313901eb587ecb3f3914c9dedba8600edbfd6e6
Antonio Gomes <tonikitoo@igalia.com>
2025-06-25 13:17:29 -0700

    Componentize chrome/browser/ui/supervised_user
    This CL adds a BUILD.gn to drive the compilation of
    c/b/ui/supervised_user.
    No behavior change expected.
    
    R=dljames@chromium.org
    
    Bug: 427681153
    Change-Id: I26a5070240355a5fb013be8dfe001143fe27f1f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6675467
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478748}

--- chrome/browser/ui/views/supervised_user/parent_permission_dialog_view.cc
+++ chrome/browser/ui/views/supervised_user/parent_permission_dialog_view.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/supervised_user/supervised_user_browser_utils.h"
 #include "chrome/browser/supervised_user/supervised_user_service_factory.h"
-#include "chrome/browser/ui/supervised_user/parent_permission_dialog.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/browser/ui/views/extensions/extension_permissions_view.h"
