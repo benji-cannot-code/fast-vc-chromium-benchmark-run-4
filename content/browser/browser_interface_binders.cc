eaa58cfa17fd55adcf1479fc6abffe35db51806c
Gyuyoung Kim <gyuyoung@igalia.com>
2025-06-25 14:28:01 -0700

    [tvOS] Unbind the date time chooser
    Since tvOS doesn’t support UIDatePicker, which is used by
    iOS’s date time picker implementation, tvOS needs a custom
    picker built with components supported by tvOS. Until that
    is implemented, this CL unbinds the date time chooser.
    
    Bug: 391914246
    Change-Id: I9d98736c2287ca5d14ac5242482be9d565b65e62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6670880
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478797}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -368,7 +368,7 @@ void BindSharedWorkerConnector(
   SharedWorkerConnectorImpl::Create(host->GetGlobalId(), std::move(receiver));
 }
 
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
+#if BUILDFLAG(IS_ANDROID) || (BUILDFLAG(IS_IOS) && !BUILDFLAG(IS_IOS_TVOS))
 void BindDateTimeChooserForFrame(
     RenderFrameHost* host,
     mojo::PendingReceiver<blink::mojom::DateTimeChooser> receiver) {
@@ -1261,7 +1261,7 @@ void PopulateBinderMapWithContext(
   RegisterWebUIControllerInterfaceBinder<webxr::mojom::WebXrInternalsHandler,
                                          WebXrInternalsUI>(map);
 #endif
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
+#if BUILDFLAG(IS_ANDROID) || (BUILDFLAG(IS_IOS) && !BUILDFLAG(IS_IOS_TVOS))
   map->Add<blink::mojom::DateTimeChooser>(
       base::BindRepeating(&BindDateTimeChooserForFrame));
 #endif
