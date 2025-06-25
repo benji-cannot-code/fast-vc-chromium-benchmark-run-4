b0bf948732e021f684b883ef956a46ff569b5e98
Anna Sato <annasato@chromium.org>
2025-06-24 20:35:42 -0700

    Replace bug number in page lifecycle TODOs.
    The original bug (crbug.com/1378279) is fixed. This replaces it with a
    new tracking bug for the remaining refactoring work to make the page
    lifecycle state updates more explicit.
    
    Bug: 427130212
    Change-Id: Id09aaa8bbfe17c7cea1492b9e71abbed2d474799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6666923
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Auto-Submit: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478333}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -2541,7 +2541,7 @@ void WebViewImpl::SetPageLifecycleStateInternal(
     SetVisibilityState(new_state->visibility, /*is_initial_state=*/false);
   }
   if (storing_in_bfcache) {
-    // TODO(https://crbug.com/1378279): Consider moving this to happen earlier
+    // TODO(https://crbug.com/427130212): Consider moving this to happen earlier
     // and together with other page state updates so that the ordering is clear.
     Scheduler()->SetPageBackForwardCached(new_state->is_in_back_forward_cache);
   }
@@ -2554,7 +2554,7 @@ void WebViewImpl::SetPageLifecycleStateInternal(
       }
     }
 
-    // TODO(https://crbug.com/1378279): Consider moving this to happen earlier
+    // TODO(https://crbug.com/427130212): Consider moving this to happen earlier
     // and together with other page state updates so that the ordering is clear.
     SetPageFrozen(true);
   }
@@ -2570,7 +2570,7 @@ void WebViewImpl::SetPageLifecycleStateInternal(
   if (eviction_changed)
     HookBackForwardCacheEviction(new_state->eviction_enabled);
   if (resuming_page) {
-    // TODO(https://crbug.com/1378279): Consider moving this to happen earlier
+    // TODO(https://crbug.com/427130212): Consider moving this to happen earlier
     // and together with other page state updates so that the ordering is clear.
     SetPageFrozen(false);
   }
@@ -2583,7 +2583,7 @@ void WebViewImpl::SetPageLifecycleStateInternal(
 
     DispatchPersistedPageshow(page_restore_params->navigation_start);
 
-    // TODO(https://crbug.com/1378279): Consider moving this to happen earlier
+    // TODO(https://crbug.com/427130212): Consider moving this to happen earlier
     // and together with other page state updates so that the ordering is clear.
     Scheduler()->SetPageBackForwardCached(new_state->is_in_back_forward_cache);
     if (MainFrame()->IsWebLocalFrame()) {
@@ -2606,7 +2606,7 @@ void WebViewImpl::SetPageLifecycleStateInternal(
   // move SchedulerTrackedFeatures to core/ and remove the back and forth.
   ReportActiveSchedulerTrackedFeatures();
 
-  // TODO(https://crbug.com/1378279): Consider moving this to happen earlier
+  // TODO(https://crbug.com/427130212): Consider moving this to happen earlier
   // and together with other page state updates so that the ordering is clear.
   GetPage()->SetPageLifecycleState(std::move(new_state));
 
