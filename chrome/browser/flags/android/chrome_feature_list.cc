401f6680013ee165ed1f1d468132b984141d6987
Michael Thiessen <mthiesse@chromium.org>
2025-06-25 14:43:23 -0700

    Fix CCT observers missing Tab Show for pre-warmed tabs
    When fixing a bug around tab visibility during startup we accidentally
    caused TAB_SHOW notifications to be dropped as the tab was now being
    shown before the CCT observers were attached.
    
    Make sure to show the tab after the observers are attached.
    
    Bug: 427139504
    Change-Id: Ie693aae81a48c006201a0b135933efca8e47eda3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6674253
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478805}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -277,6 +277,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTReportPrerenderEvents,
     &kCCTResizableForThirdParties,
     &kCCTRevampedBranding,
+    &kCCTShowTabFix,
     &kCCTTabModalDialog,
     &kCCTToolbarRefactor,
     &kChangeUnfocusedPriority,
@@ -850,6 +851,8 @@ BASE_FEATURE(kCCTResizableForThirdParties,
 BASE_FEATURE(kCCTRevampedBranding,
              "CCTRevampedBranding",
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kCCTShowTabFix, "CCTShowTabFix", base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTTabModalDialog,
              "CCTTabModalDialog",
