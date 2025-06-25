951db0d65bdccf8a4ecac1a335148984f3dc41f6
Robbie McElrath <rmcelrath@chromium.org>
2025-06-25 15:01:55 -0700

    ondevice: Fix LoadModel error handling in on-device-internals
    This wraps the LoadModel callback with
    WrapCallbackWithDefaultInvokeIfNotRun to ensure it gets run if the call
    fails, and updates the ModelFile destructor to delete the File member
    on a blockable thread.
    
    Bug: 427784684
    Change-Id: I06cf1b4b65828782ab3c0aa15c20d46ad2e02814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6675783
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478814}

--- services/on_device_model/public/cpp/model_assets.cc
+++ services/on_device_model/public/cpp/model_assets.cc
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/functional/callback_helpers.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/default_construct_tag.h"
 
@@ -90,7 +93,12 @@ ModelFile& ModelFile::operator=(const ModelFile& other) {
 
 ModelFile::ModelFile(ModelFile&&) = default;
 ModelFile& ModelFile::operator=(ModelFile&&) = default;
-ModelFile::~ModelFile() = default;
+ModelFile::~ModelFile() {
+  if (IsFile() && file().IsValid()) {
+    base::ThreadPool::PostTask(FROM_HERE, {base::MayBlock()},
+                               base::DoNothingWithBoundArgs(std::move(file())));
+  }
+}
 
 base::File& ModelFile::file() {
   CHECK(std::holds_alternative<base::File>(file_));
