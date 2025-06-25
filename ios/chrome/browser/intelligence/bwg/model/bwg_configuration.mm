bbc33ecfda01c68d43c32b740d0f1a5b6b31e1f0
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-06-25 13:29:20 -0700

    [iOS][BWG] Remove bwg_config CHECK
    Removed check since the page context can be null within the config.
    Renamed the property to make it clear that it's a unique_ptr and that
    subsequent calls will be null. (The comment already said this).
    
    Bug: 427681837
    Change-Id: Iacae969ae5a40f960238de810f8695adc454d413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6674978
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1478764}

--- ios/chrome/browser/intelligence/bwg/model/bwg_configuration.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_configuration.mm
@@ -10,17 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation BWGConfiguration {
   // The current configuration's PageContext.
-  std::unique_ptr<optimization_guide::proto::PageContext> _pageContext;
+  std::unique_ptr<optimization_guide::proto::PageContext> _uniquePageContext;
 }
 
-- (std::unique_ptr<optimization_guide::proto::PageContext>)pageContext {
-  CHECK(_pageContext.get());
-  return std::move(_pageContext);
+- (std::unique_ptr<optimization_guide::proto::PageContext>)uniquePageContext {
+  return std::move(_uniquePageContext);
 }
 
-- (void)setPageContext:
-    (std::unique_ptr<optimization_guide::proto::PageContext>)pageContext {
-  _pageContext = std::move(pageContext);
+- (void)setUniquePageContext:
+    (std::unique_ptr<optimization_guide::proto::PageContext>)uniquePageContext {
+  _uniquePageContext = std::move(uniquePageContext);
 }
 
 @end
