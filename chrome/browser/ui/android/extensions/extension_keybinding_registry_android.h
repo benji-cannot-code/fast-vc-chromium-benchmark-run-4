ae841693b0b44d99792941cf7b72ff52f4cc6bd1
Keigo Oka <oka@google.com>
2025-06-25 09:36:24 -0700

    extensions: Add static asserts for enable_extensions_core in two files
    Also remove a include using forward declaration in a file.
    
    This is a follow up for https://crrev.com/c/6657758 and
    https://crrev.com/c/6656218.
    
    Bug: None
    Change-Id: Ie1b14d3e218ff30b5134af820cd9b8c7f628d3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667424
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478609}

--- chrome/browser/ui/android/extensions/extension_keybinding_registry_android.h
+++ chrome/browser/ui/android/extensions/extension_keybinding_registry_android.h
@@ -8,10 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_keybinding_registry.h"
 #include "ui/base/accelerators/accelerator.h"
-#include "ui/events/android/key_event_android.h"
 
 namespace content {
 class BrowserContext;
+}
+
+namespace ui {
+class KeyEventAndroid;
 }
 
 namespace extensions {
