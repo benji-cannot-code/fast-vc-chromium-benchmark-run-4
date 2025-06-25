905784820dcc84aae8b1ebd466c706706ab768ae
Greg Thompson <grt@chromium.org>
2025-06-25 03:00:15 -0700

    [A11yPerformance] Improve use of containers
    - Use try_emplace and similar to avoid dual lookups where possible.
    - Erase via an iterator rather than a key where convenient.
    - Prefer range-based for loops with vectors.
    - Use a single instance of AXNode's End iterators when looping.
    
    AX-Relnotes: n/a.
    Bug: 391797366, 40919326
    Change-Id: I91b902336b66e559c8b88f7d37b4dec0380968bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651738
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478440}

--- ui/accessibility/ax_table_info.cc
+++ ui/accessibility/ax_table_info.cc
@@ -40,8 +40,9 @@ namespace {
 // nodes that are ignored, but we don't skp any other roles
 // in-between a table row and its cells.
 void FindCellsInRow(AXNode* node, std::vector<AXNode*>* cell_nodes) {
-  for (auto iter = node->UnignoredChildrenBegin();
-       iter != node->UnignoredChildrenEnd(); ++iter) {
+  for (auto iter = node->UnignoredChildrenBegin(),
+            end = node->UnignoredChildrenEnd();
+       iter != end; ++iter) {
     AXNode* child = iter.get();
     if (child->GetRole() == ax::mojom::Role::kGenericContainer) {
       FindCellsInRow(child, cell_nodes);
@@ -52,8 +53,9 @@ void FindCellsInRow(AXNode* node, std::vector<AXNode*>* cell_nodes) {
 }
 
 bool isRowlessTable(AXNode* node) {
-  for (auto iter = node->UnignoredChildrenBegin();
-       iter != node->UnignoredChildrenEnd(); ++iter) {
+  for (auto iter = node->UnignoredChildrenBegin(),
+            end = node->UnignoredChildrenEnd();
+       iter != end; ++iter) {
     AXNode* child = iter.get();
     if (child->GetRole() == ax::mojom::Role::kGenericContainer ||
         child->GetRole() == ax::mojom::Role::kGroup ||
@@ -82,8 +84,9 @@ bool isRowlessTable(AXNode* node) {
 void FindRows(AXNode* node,
               std::vector<raw_ptr<AXNode, VectorExperimental>>* row_node_list,
               AXNodeID& caption_node_id) {
-  for (auto iter = node->UnignoredChildrenBegin();
-       iter != node->UnignoredChildrenEnd(); ++iter) {
+  for (auto iter = node->UnignoredChildrenBegin(),
+            end = node->UnignoredChildrenEnd();
+       iter != end; ++iter) {
     AXNode* child = iter.get();
     if (child->GetRole() == ax::mojom::Role::kGenericContainer ||
         child->GetRole() == ax::mojom::Role::kGroup ||
@@ -137,8 +140,9 @@ void FindCellsForRowlessTable(
   int current_row = -1;
   int current_index = -1;
   base::queue<AXNode*> child_queue;
-  for (auto iter = grid_node->UnignoredChildrenBegin();
-       iter != grid_node->UnignoredChildrenEnd(); ++iter) {
+  for (auto iter = grid_node->UnignoredChildrenBegin(),
+            end = grid_node->UnignoredChildrenEnd();
+       iter != end; ++iter) {
     child_queue.push(iter.get());
     while (!child_queue.empty()) {
       auto* child = child_queue.front();
@@ -146,8 +150,9 @@ void FindCellsForRowlessTable(
       if (child->GetRole() == ax::mojom::Role::kGenericContainer ||
           child->GetRole() == ax::mojom::Role::kGroup) {
         // Add children of the container to the queue
-        for (auto container_itr = child->UnignoredChildrenBegin();
-             container_itr != child->UnignoredChildrenEnd(); ++container_itr) {
+        for (auto container_itr = child->UnignoredChildrenBegin(),
+                  container_end = child->UnignoredChildrenEnd();
+             container_itr != container_end; ++container_itr) {
           child_queue.push(container_itr.get());
         }
         continue;
