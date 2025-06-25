211fa6532f8d0668d0ab6f57ff20942085e18bb5
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-06-25 04:01:16 -0700

    Introduce kOffloadAcceptCHFrameCheck feature flag
    I forgot to introduce this in the previous CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6656395
    However, it should be there.
    
    Bug: 406407746
    Change-Id: I6d651c911de1e5f314496361859091c3d310a543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6670539
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478452}

--- services/network/accept_ch_frame_interceptor_unittest.cc
+++ services/network/accept_ch_frame_interceptor_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/accept_ch_frame_interceptor.h"
 
+#include "base/test/scoped_feature_list.h"
+#include "services/network/public/cpp/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -26,6 +28,8 @@ class AcceptCHFrameInterceptorTest : public testing::Test {
   void TearDown() override { interceptor_.reset(); }
 
   std::unique_ptr<AcceptCHFrameInterceptor> interceptor_;
+  base::test::ScopedFeatureList feature_list_{
+      features::kOffloadAcceptCHFrameCheck};
 };
 
 TEST_F(AcceptCHFrameInterceptorTest, NeedsObserverCheckNullOpt) {
