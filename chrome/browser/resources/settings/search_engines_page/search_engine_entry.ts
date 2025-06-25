288c4de89f48deb04f3d9a23c728d809d60b8bbd
Alex Chen <alexwchen@chromium.org>
2025-06-25 12:15:37 -0700

    [Site search policy] Show disabled three-dot-menu for non-overridable engines
    Currently, non-overridable engines hide the three-dot-menu. As a result,
    the icons in the search engines page are not aligned. Per UX feedback,
    the three-dot-menu should be shown in a disabled greyed out state for
    non-overridable engines instead.
    
    Before: https://screenshot.googleplex.com/8TreYknons5mnWN
    After: https://screenshot.googleplex.com/9F4ujYxGsxEjqdL
    
    Bug: b:427549302
    Change-Id: I291d5623a0522e475b6391ff24acdc8037ff87e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667707
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1478710}

--- chrome/browser/resources/settings/search_engines_page/search_engine_entry.ts
+++ chrome/browser/resources/settings/search_engines_page/search_engine_entry.ts
@@ -78,9 +78,9 @@ export class SettingsSearchEngineEntryElement extends
         computed: 'computeShowSecondaryButton_(engine)',
       },
 
-      showDots_: {
+      disableDots_: {
         type: Boolean,
-        computed: 'computeShowDots_(engine)',
+        computed: 'computeDisableDots_(engine)',
       },
     };
   }
@@ -94,7 +94,7 @@ export class SettingsSearchEngineEntryElement extends
   declare private showEditIcon_: boolean;
   declare private showDownloadedIcon_: boolean;
   declare private showSecondaryButton_: boolean;
-  declare private showDots_: boolean;
+  declare private disableDots_: boolean;
   private timeoutId_: number|null = null;
 
   private onEngineChanged_(
@@ -135,9 +135,10 @@ export class SettingsSearchEngineEntryElement extends
         (this.engine.isManaged && !this.engine.canBeEdited);
   }
 
-  private computeShowDots_(): boolean {
-    return !this.engine.isManaged || this.engine.canBeActivated ||
-        this.engine.canBeDeactivated || this.engine.canBeRemoved;
+  private computeDisableDots_(): boolean {
+    return this.engine.default ||
+        (this.engine.isManaged && !this.engine.canBeActivated &&
+         !this.engine.canBeDeactivated && !this.engine.canBeRemoved);
   }
 
   private onDeleteClick_(e: Event) {
