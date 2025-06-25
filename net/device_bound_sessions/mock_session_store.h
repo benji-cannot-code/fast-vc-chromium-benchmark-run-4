f9523a2fa0788f421638595bcfc628d0f2be2e3b
Daniel Rubery <drubery@chromium.org>
2025-06-25 07:25:40 -0700

    Combine separate site and id parameters into SessionKey
    This is a general refactor to make it more natural to use SessionKey. No
    behavior changes are expected.
    
    Also remove some unused headers causing presubmit warnings
    
    Bug: 402020386
    Change-Id: Id7738597eb92ed595f99945323b521756e0ba1a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6669112
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478540}

--- net/device_bound_sessions/mock_session_store.h
+++ net/device_bound_sessions/mock_session_store.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_DEVICE_BOUND_SESSIONS_MOCK_SESSION_STORE_H_
 #define NET_DEVICE_BOUND_SESSIONS_MOCK_SESSION_STORE_H_
 
-#include <string>
-#include <utility>
-
 #include "base/functional/callback.h"
 #include "net/base/schemeful_site.h"
 #include "net/device_bound_sessions/session.h"
@@ -31,8 +28,7 @@ class SessionStoreMock : public SessionStore {
   MOCK_METHOD(SessionStore::SessionsMap, GetAllSessions, (), (const, override));
   MOCK_METHOD(void,
               RestoreSessionBindingKey,
-              (const SchemefulSite& site,
-               const Session::Id& session_id,
+              (const SessionKey& session_key,
                RestoreSessionBindingKeyCallback callback),
               (override));
 };
