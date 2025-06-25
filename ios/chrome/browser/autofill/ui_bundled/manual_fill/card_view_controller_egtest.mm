7ad8a43c314f5349d1e8eb1bb23c14e726a1fb87
Daniel White <danieltwhite@google.com>
2025-06-25 09:39:59 -0700

    [iOS] Fix test CreditCardViewControllerTestCases on iOS 26
    This test was failing on iOS 26 because multiple elements were matched:
     - _UIInterfaceActionCustomViewRepresentationView
     - _UIAlertControllerActionView
    This CL updates the selection so that only
    _UIInterfaceActionCustomViewRepresentationView is matched.
    
    This CL fixes the testcases:
     - testCreditCardLocalNumberDoesntInjectOnHttp
     - testCreditCardLocalNumberShowsWarningOnHttp
    
    Fixed: 427421171
    Change-Id: I04dcdc38be5a20c4c713d084217427e7aff9ddaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668212
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478614}

--- ios/chrome/browser/autofill/ui_bundled/manual_fill/card_view_controller_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/manual_fill/card_view_controller_egtest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/wait_util.h"
 #import "components/autofill/core/browser/test_utils/autofill_test_utils.h"
 #import "components/autofill/core/common/autofill_payments_features.h"
+#import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.h"
 #import "ios/chrome/browser/autofill/ui_bundled/form_input_accessory/form_input_accessory_app_interface.h"
 #import "ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_constants.h"
@@ -938,8 +939,9 @@ void DismissPaymentBottomSheet() {
                         doesInjectValue:@""];
 
   // Dismiss the warning alert.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::OKButton()]
-      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:
+                 chrome_test_util::ActionSheetItemWithAccessibilityLabelId(
+                     IDS_OK)] performAction:grey_tap()];
 
   [ChromeEarlGreyUI cleanupAfterShowingAlert];
 }
@@ -954,8 +956,9 @@ void DismissPaymentBottomSheet() {
       assertWithMatcher:grey_not(grey_nil())];
 
   // Dismiss the alert.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::OKButton()]
-      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:
+                 chrome_test_util::ActionSheetItemWithAccessibilityLabelId(
+                     IDS_OK)] performAction:grey_tap()];
 
   [ChromeEarlGreyUI cleanupAfterShowingAlert];
 }
