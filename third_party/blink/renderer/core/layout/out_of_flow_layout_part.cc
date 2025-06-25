68b582f630a523b04e1249319de16611c664564c
Morten Stenshorne <mstensho@chromium.org>
2025-06-25 02:25:56 -0700

    [anchor] Always initialize style in OOFCandidateStyleIterator.
    Re-initializing via Reset() would fail with null style_ if we had
    exhausted all fallbacks, if the last fallback was non-existent.
    
    Add a correctness test, that would also crash without this fix.
    
    Bug: 427134601
    Change-Id: I59d3a4b4cd944a429fd5ed066067b347f678f644
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664363
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478435}

--- third_party/blink/renderer/core/layout/out_of_flow_layout_part.cc
+++ third_party/blink/renderer/core/layout/out_of_flow_layout_part.cc
@@ -115,7 +115,7 @@ class OOFCandidateStyleIterator {
       AnchorEvaluatorImpl& anchor_evaluator,
       WritingDirectionMode container_writing_direction)
       : element_(DynamicTo<Element>(object.GetNode())),
-        style_(object.Style()),
+        original_style_(object.StyleRef()),
         anchor_evaluator_(anchor_evaluator),
         container_writing_direction_(container_writing_direction) {
     Initialize();
@@ -241,6 +241,9 @@ class OOFCandidateStyleIterator {
 
  private:
   void Initialize() {
+    style_ = &original_style_;
+
+    // Not all OOFs have an element. LayoutViewTransitionRoot is one example.
     if (element_) {
       position_try_fallbacks_ = style_->GetPositionTryFallbacks();
 
@@ -320,6 +323,9 @@ class OOFCandidateStyleIterator {
   }
 
   Element* element_ = nullptr;
+
+  // The ComputedStyle stored on LayoutObject at construction time.
+  const ComputedStyle& original_style_;
 
   // The current candidate style if no auto anchor fallback is triggered.
   // Otherwise, the base style for generating auto anchor fallbacks.
