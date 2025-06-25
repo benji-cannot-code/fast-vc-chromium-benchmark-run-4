c771144983587a4cc86734ab7108b019a7cafe15
Sky Malice <skym@chromium.org>
2025-06-25 15:38:24 -0700

    Move optional token/id handling to token_id.h
    Bug: None
    Change-Id: I38ced7aea2b332b7a9c80949b05edfe8de64b620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6672927
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478828}

--- chrome/browser/ui/android/tab_model/tab_collection_tab_model_impl.cc
+++ chrome/browser/ui/android/tab_model/tab_collection_tab_model_impl.cc
@@ -37,14 +37,6 @@ namespace {
 
 constexpr int kInvalidTabIndex = -1;
 
-std::optional<TabGroupId> ToTabGroupId(
-    const std::optional<base::Token>& token) {
-  if (!token) {
-    return std::nullopt;
-  }
-  return TabGroupId::FromRawToken(*token);
-}
-
 // Converts the `tab_android` to a `unique_ptr<TabInterface>`. Under the hood we
 // use a wrapper class `TabInterfaceAndroid` which takes a weak ptr to
 // `TabAndroid` to avoid memory management issues.
@@ -114,7 +106,8 @@ int TabCollectionTabModelImpl::MoveTabRecursive(
     size_t new_index,
     const std::optional<base::Token>& token,
     bool new_is_pinned) {
-  std::optional<TabGroupId> new_tab_group_id = ToTabGroupId(token);
+  std::optional<TabGroupId> new_tab_group_id =
+      tab_groups::TabGroupId::FromOptionalToken(token);
   new_index = GetSafeIndex(/*is_move=*/true, new_index, new_tab_group_id,
                            new_is_pinned);
 
@@ -131,7 +124,8 @@ void TabCollectionTabModelImpl::AddTabRecursive(
     bool is_pinned) {
   CHECK(tab_android);
 
-  std::optional<TabGroupId> tab_group_id = ToTabGroupId(token);
+  std::optional<TabGroupId> tab_group_id =
+      tab_groups::TabGroupId::FromOptionalToken(token);
 
   index = GetSafeIndex(/*is_move=*/false, index, tab_group_id, is_pinned);
 
