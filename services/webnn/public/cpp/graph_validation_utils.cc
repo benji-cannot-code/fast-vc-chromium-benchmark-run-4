6ab133c727f6d2c4ab7c2a7030849bdb624eb858
Takuto Ikuta <tikuta@chromium.org>
2025-06-24 23:58:23 -0700

    services: Add a missing header for std::array
    Bug: 40263312
    Change-Id: If79a16f0374e1a85ee7c8d83405777119e86b8ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6669727
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478381}

--- services/webnn/public/cpp/graph_validation_utils.cc
+++ services/webnn/public/cpp/graph_validation_utils.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/webnn/public/cpp/graph_validation_utils.h"
 
 #include <algorithm>
+#include <array>
 #include <numeric>
 #include <set>
 #include <variant>
