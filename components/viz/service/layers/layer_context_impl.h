8e8c0cfe8b0a08053b0edbea64f9b6b2629a8851
Colin Blundell <blundell@chromium.org>
2025-06-24 23:48:34 -0700

    [Viz] Pass `enable_edge_anti_alising` bool into LayerContextImpl
    This CL extends LayerContextImpl to take in an
    `enable_edge_anti_aliasing` bool and forward it on to the settings used
    by the LayerTreeHostImpl it creates.
    
    A followup CL will thread this bool from the client's settings to
    LayerContextImpl via BindLayerContext().
    
    Bug: 421891105
    Change-Id: Ie112967ba54898b7f7c4fa1aabc0f28563fa0d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6657173
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478364}

--- components/viz/service/layers/layer_context_impl.h
+++ components/viz/service/layers/layer_context_impl.h
@@ -44,13 +44,15 @@ class VIZ_SERVICE_EXPORT LayerContextImpl : public cc::LayerTreeHostImplClient,
   // `compositor_sink` with client connection details given by `context`.
   LayerContextImpl(CompositorFrameSinkSupport* compositor_sink,
                    mojom::PendingLayerContext& context,
-                   bool draw_mode_is_gpu);
+                   bool draw_mode_is_gpu,
+                   bool enable_edge_anti_aliasing);
 
   // Static factory method for testing purposes. The created object's lifetime
   // is not managed by this function.
   static std::unique_ptr<LayerContextImpl> CreateForTesting(
       CompositorFrameSinkSupport* compositor_sink,
-      bool draw_mode_is_gpu);
+      bool draw_mode_is_gpu,
+      bool enable_edge_anti_aliasing);
 
   ~LayerContextImpl() override;
 
@@ -74,6 +76,7 @@ class VIZ_SERVICE_EXPORT LayerContextImpl : public cc::LayerTreeHostImplClient,
   LayerContextImpl(
       CompositorFrameSinkSupport* compositor_sink,
       bool draw_mode_is_gpu,
+      bool enable_edge_anti_aliasing,
       mojo::PendingAssociatedReceiver<mojom::LayerContext> receiver_pipe,
       mojo::PendingAssociatedRemote<mojom::LayerContextClient> client_pipe);
 
