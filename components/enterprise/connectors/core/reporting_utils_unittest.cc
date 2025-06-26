b15f3655edf5c4ae19d062b0e43a6a5332a91b2f
Nancy Xiao <nancylanxiao@google.com>
2025-06-26 10:29:03 -0700

    [Proto migration] Use proto format to report password changed event
    Fixed: 396437063
    Change-Id: I794000b979be4a824dc4db8498d7ba50b46f7695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6678008
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1479280}

--- components/enterprise/connectors/core/reporting_utils_unittest.cc
+++ components/enterprise/connectors/core/reporting_utils_unittest.cc
@@ -101,8 +101,11 @@ TEST(ReportingUtilsTest, GetPasswordReuseEventWithoutWarning) {
 }
 
 TEST(ReportingUtilsTest, GetPasswordChangedEvent) {
-  auto event = GetPasswordChangedEvent(kUsername);
+  auto event =
+      GetPasswordChangedEvent(kUsername, "identifier", "profile_username");
   ASSERT_EQ(event.user_name(), kUsername);
+  ASSERT_EQ(event.profile_identifier(), "identifier");
+  ASSERT_EQ(event.profile_user_name(), "profile_username");
 }
 
 TEST(ReportingUtilsTest, GetLoginEvent) {
