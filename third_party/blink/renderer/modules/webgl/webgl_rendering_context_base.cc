00074557e4be91a5b756879d0a09957e2e9583a9
Colin Blundell <blundell@chromium.org>
2025-06-25 23:28:00 -0700

    [Blink] Eliminate calling WebGLRenderingContextBase::IsAccelerated()
    The one callsite of this method for WebGL is here [1]. That method's
    one production callsite is here [2]. The WebGL implementation of
    IsAccelerated() checks whether the context's CanvasResourceProvider
    exists and is accelerated. This works as expected when the WebGL canvas
    is an HTMLCanvasElement, as in that case
    PaintRenderingResultsToSnapshot() will have been called via
    HTMLCanvasElement::GetSourceImageForCanvas() just prior to the call to
    [2], and it will have created an accelerated CRP iff the
    StaticBitmapImage that was returned by calling Snapshot() on that CRP
    is accelerated. However, it does not work as intended when the WebGL
    canvas is an offscreen canvas:
    
    * Copying from offscreen canvas with placeholder: It only potentially
      accelerates if the source image is accelerated (PushFrame() will be
      getting called on the offscreen canvas and might end up creating the
      context's CRP but also might not end up doing so)
    * Copying from offscreen canvas without placeholder: It will never
      accelerate, as the WebGL context's CRP is never created in this case
    
    This CL changes [2] to use the info of whether the created image is
    texture-backed as the source of truth for whether a WebGL canvas is
    accelerated. This leaves the current behavior unchanged for the case
    where the WebGL canvas is an HTMLCanvasElement and aligns the behavior
    for the case where the WebGL canvas is an OffscreenCanvas to match the
    current behavior for the case of it being an HTMLCanvasElement.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc;l=348;drc=b8b5768e5d0d1c2a84fe4896eae884d97fd1131e
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/canvas/html_canvas_element.cc;l=1929;drc=b8b5768e5d0d1c2a84fe4896eae884d97fd1131e
    
    Bug: 352263194
    Change-Id: I7c831ba8d2f77baaaa360f5f1c54800d1195eb5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668796
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478945}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1967,9 +1967,8 @@ void WebGLRenderingContextBase::MarkLayerComposited() {
 }
 
 bool WebGLRenderingContextBase::IsAccelerated() const {
-  auto* resource_provider = resource_provider_.get();
-  return resource_provider ? resource_provider->IsAccelerated()
-                           : Host()->ShouldTryToUseGpuRaster();
+  // This method is not supported for WebGL and should not be called.
+  NOTREACHED();
 }
 
 bool WebGLRenderingContextBase::UsingSwapChain() const {
