2a1f8f38ecdcc57fbc8e6418823257e830d9df80
Devon Loehr <dloehr@google.com>
2025-06-26 08:32:19 -0700

    Explicitly include <cstdlib> in buffer.cc
    After https://github.com/llvm/llvm-project/pull/139586, this header is
    no longer transitively included by <cmath>, so we need to include it
    directly.
    
    Bug: 427774670
    Change-Id: I9d892d30f410ee55d55afddf33c7aabc3be21344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6679831
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Devon Loehr <dloehr@google.com>
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1479187}

--- components/cronet/native/buffer.cc
+++ components/cronet/native/buffer.cc
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/cronet/native/generated/cronet.idl_impl_interface.h"
+#include <cstdlib>
 
 #include "base/no_destructor.h"
 #include "base/numerics/safe_conversions.h"
+#include "components/cronet/native/generated/cronet.idl_impl_interface.h"
 
 namespace {
 
