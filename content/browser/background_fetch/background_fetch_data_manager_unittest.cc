f54b68d2f301741b062a640fef3d408c747b45ea
Alex Gough <ajgo@chromium.org>
2025-06-26 07:27:32 -0700

    Use result<> for OpenResult in cache storage mojom
    No functional changes.
    
    Bug: 40841428
    Change-Id: Iec9607fc1ffe9537c5a2424fd06d287945eabf24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6675479
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479141}

--- content/browser/background_fetch/background_fetch_data_manager_unittest.cc
+++ content/browser/background_fetch/background_fetch_data_manager_unittest.cc
@@ -485,13 +485,14 @@ class BackgroundFetchDataManagerTest
     base::RunLoop run_loop;
     background_fetch_data_manager_->OpenCache(
         storage_key(), kExampleUniqueId, trace_id,
-        base::BindLambdaForTesting([&](blink::mojom::OpenResultPtr result) {
-          EXPECT_FALSE(result->is_status());
+        base::BindLambdaForTesting([&](blink::mojom::CacheStorage::OpenResult
+                                           result) {
+          EXPECT_TRUE(result.has_value());
 
           auto match_options = blink::mojom::CacheQueryOptions::New();
           match_options->ignore_search = true;
 
-          cache.Bind(std::move(result->get_cache()));
+          cache.Bind(std::move(result.value()));
           cache->Match(
               BackgroundFetchSettledFetch::CloneRequest(request),
               std::move(match_options),
@@ -513,8 +514,9 @@ class BackgroundFetchDataManagerTest
         storage_key(),
         /* unique_id= */ kExampleUniqueId,
         /* trace_id= */ 0,
-        base::BindLambdaForTesting([&](blink::mojom::OpenResultPtr result) {
-          EXPECT_TRUE(result->is_cache());
+        base::BindLambdaForTesting([&](blink::mojom::CacheStorage::OpenResult
+                                           result) {
+          EXPECT_TRUE(result.has_value());
 
           std::vector<blink::mojom::BatchOperationPtr> operation_ptr_vec;
           operation_ptr_vec.push_back(blink::mojom::BatchOperation::New());
@@ -526,7 +528,7 @@ class BackgroundFetchDataManagerTest
               blink::mojom::CacheQueryOptions::New();
           operation_ptr_vec[0]->match_options->ignore_search = true;
 
-          cache.Bind(std::move(result->get_cache()));
+          cache.Bind(std::move(result.value()));
           cache->Batch(
               std::move(operation_ptr_vec), /* trace_id= */ 0,
               base::BindOnce(&BackgroundFetchDataManagerTest::DidBatchOperation,
@@ -543,8 +545,9 @@ class BackgroundFetchDataManagerTest
         storage_key(),
         /* unique_id= */ kExampleUniqueId,
         /* trace_id= */ 0,
-        base::BindLambdaForTesting([&](blink::mojom::OpenResultPtr result) {
-          EXPECT_TRUE(result->is_cache());
+        base::BindLambdaForTesting([&](blink::mojom::CacheStorage::OpenResult
+                                           result) {
+          EXPECT_TRUE(result.has_value());
 
           std::vector<blink::mojom::BatchOperationPtr> operation_ptr_vec;
           operation_ptr_vec.push_back(blink::mojom::BatchOperation::New());
@@ -554,7 +557,7 @@ class BackgroundFetchDataManagerTest
               BackgroundFetchSettledFetch::CloneRequest(request);
           operation_ptr_vec[0]->response = std::move(response);
 
-          cache.Bind(std::move(result->get_cache()));
+          cache.Bind(std::move(result.value()));
           cache->Batch(
               std::move(operation_ptr_vec), /* trace_id= */ 0,
               base::BindOnce(&BackgroundFetchDataManagerTest::DidBatchOperation,
--- content/browser/background_fetch/storage/database_task.h
+++ content/browser/background_fetch/storage/database_task.h
@@ -158,7 +158,7 @@ class DatabaseTask : public DatabaseTaskHost {
                             blink::ServiceWorkerStatusCode status);
   void DidOpenCache(
       base::OnceCallback<void(blink::mojom::CacheStorageError)> callback,
-      blink::mojom::OpenResultPtr result);
+      blink::mojom::CacheStorage::OpenResult result);
 
   base::WeakPtr<DatabaseTaskHost> GetWeakPtr() override;
 
