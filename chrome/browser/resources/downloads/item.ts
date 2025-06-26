d5f14df9a643a1a76f16962e13c02e2ad96f369a
Lily Chen <chlily@chromium.org>
2025-06-26 10:31:54 -0700

    [Downloads] Add failure toast for copy download link button
    The "copy" button on chrome://downloads allows the user to copy the
    download's URL to their clipboard. This uses the JS call
    navigator.clipboard.writeText(), which (for security reasons) may be
    prohibited e.g. if the page is not focused. A normal user would not
    typically encounter this, but this happens sometimes if the button is
    clicked programmatically.
    
    This CL adds explicit handling of this exception state, and adds a
    "toast" to give the user UI feedback when the copy operation failed.
    
    Screenshot:
    https://drive.google.com/drive/folders/1KaFUcmRn2vBwTxbhS6Oq9PmfHzAk2cEs
    
    Fixed: 401516234
    Change-Id: Ie615ba5e9aff22a0faa0e284af4a8f4ab3d248f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6673962
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479284}

--- chrome/browser/resources/downloads/item.ts
+++ chrome/browser/resources/downloads/item.ts
@@ -1123,8 +1123,13 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
     if (!this.data || !this.data.url) {
       return;
     }
-    navigator.clipboard.writeText(this.data.url.url);
-    this.displayCopyToast_(e);
+    let copied = true;
+    navigator.clipboard.writeText(this.data.url.url)
+        .catch(error => {
+          console.error('Unable to copy to clipboard:', error);
+          copied = false;
+        })
+        .finally(() => this.displayCopyToast_(e, copied));
   }
 
   protected onMoreActionsClick_() {
@@ -1227,19 +1232,24 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
     this.getMoreActionsMenu().close();
   }
 
-  private displayCopyToast_(e: Event) {
+  private displayCopyToast_(e: Event, copied: boolean) {
     if (!this.data || !this.data.url) {
       return;
     }
-
-    const pieces = loadTimeData.getSubstitutedStringPieces(
-                       loadTimeData.getString('toastCopiedDownloadLink'),
-                       this.data.url.url) as unknown as
-        Array<{collapsible: boolean, value: string, arg: string}>;
-    pieces.forEach(p => {
-      p.collapsible = !!p.arg;
-    });
-    getToastManager().showForStringPieces(pieces, /*hideSlotted=*/ true);
+    if (copied) {
+      const pieces = loadTimeData.getSubstitutedStringPieces(
+                         loadTimeData.getString('toastCopiedDownloadLink'),
+                         this.data.url.url) as unknown as
+          Array<{collapsible: boolean, value: string, arg: string}>;
+      pieces.forEach(p => {
+        p.collapsible = !!p.arg;
+      });
+      getToastManager().showForStringPieces(pieces, /*hideSlotted=*/ true);
+    } else {
+      getToastManager().show(
+          loadTimeData.getString('toastCopyDownloadLinkFailed'),
+          /*hideSlotted=*/ true);
+    }
 
     e.stopPropagation();
     e.preventDefault();
