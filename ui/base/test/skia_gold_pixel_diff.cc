bd231a69efc9104952b20f47a73bad95f2818ecb
Michael Maltsev <michaelm@island.io>
2025-06-26 12:22:23 -0700

    Windows: Update NormalizeFilePath to support network paths
    NetworkProfileBubble::CheckNetworkProfile was adjusted to work correctly
    with the updated implementation. Previously, it was assumed that any
    path that can't be normalized is a network path, which is no longer the
    case.
    
    The SkiaGoldPixelDiff::GenerateLocalDiff test function was updated to
    log the correct file URL for network paths.
    
    Bug: 421933204
    Change-Id: I650b99763c3501dd8befc58ca473e622f47918a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6607671
    Commit-Queue: Michael Maltsev (R&D) <michaelm@island.io>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479370}

--- ui/base/test/skia_gold_pixel_diff.cc
+++ ui/base/test/skia_gold_pixel_diff.cc
@@ -620,7 +620,8 @@ void SkiaGoldPixelDiff::GenerateLocalDiff(
         return {path->png_path.MaybeAsASCII()};
       }
 
-      return {std::string("file:///") +
+      return {(path_normalized.IsNetwork() ? std::string("file:")
+                                           : std::string("file:///")) +
               path_normalized.NormalizePathSeparatorsTo(FILE_PATH_LITERAL('/'))
                   .MaybeAsASCII()};
     } else {
