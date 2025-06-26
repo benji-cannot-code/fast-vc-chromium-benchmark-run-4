313fb082e60e715bb5c02edee34e70a912656895
Robbie McElrath <rmcelrath@chromium.org>
2025-06-26 12:56:25 -0700

    controlledframe: Update authrequired event API
    This updates the authrequired event to match the recent changes in the
    spec. Specifically, the event.setCredentials call now supports taking
    a promise and an options dictionary with an AbortSignal that will let
    the developer cancel the request.
    
    See https://github.com/WICG/controlled-frame/pull/138
    
    Bug: 419101396
    Change-Id: I3d086e8073e809a178bbc6b4a6719bc477e138cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653108
    Reviewed-by: Simon Hangl <simonha@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479392}

--- chrome/test/data/controlled_frame/webrequest_auth.window.js
+++ chrome/test/data/controlled_frame/webrequest_auth.window.js
@@ -6,6 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META script=resources/controlled_frame_helpers.js
 // META script=resources/event_handler_helpers.js
 
+// This file contains authrequired event related tests.
+//
+// Successful authentications are cached and can mess with subsequent tests.
+// To get around this, the test uses separate partitions for tests that
+// successfully authenticate, but a shared partition for others to speed up the
+// tests because partition creation is expensive.
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 promise_test(async (test) => {
   const controlledframe = await createControlledFrame('/simple.html', 'auth1');
   controlledframe.stop();
@@ -18,10 +29,45 @@ promise_test(async (test) => {
   controlledframe.request.createWebRequestInterceptor({
     urlPatterns: [targetUrl.toString()],
     blocking: true,
-  }).addEventListener('authrequired', async (e) => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1);
-    });
+  }).addEventListener('authrequired', (e) => {
+    e.setCredentials(new Promise((resolve) => {
+      // Sleep to ensure the network request is paused until the
+      // promise resolves.
+      sleep(1);
+      resolve({
+        username: '',
+        password: 'PASS',
+      });
+    }));
+  });
+
+  targetUrl.search = 'password=PASS&realm=REALM';
+  await navigateControlledFrame(controlledframe, targetUrl.toString());
+
+  verifyWebRequestEvents([
+    'beforerequest',
+    'beforesendheaders',
+    'sendheaders',
+    'headersreceived',
+    'authrequired',
+    'responsestarted',
+    'completed',
+  ]);
+}, 'WebRequest Auth Async');
+
+promise_test(async (test) => {
+  const controlledframe = await createControlledFrame('/simple.html', 'auth2');
+  controlledframe.stop();
+
+  const targetUrl = new URL(controlledframe.src);
+  targetUrl.pathname = '/auth-basic';
+  targetUrl.search = '*';
+  addWebRequestListeners(controlledframe, targetUrl.toString());
+
+  controlledframe.request.createWebRequestInterceptor({
+    urlPatterns: [targetUrl.toString()],
+    blocking: true,
+  }).addEventListener('authrequired', (e) => {
     e.setCredentials({
       username: '',
       password: 'PASS',
@@ -40,10 +86,10 @@ promise_test(async (test) => {
     'responsestarted',
     'completed',
   ]);
-}, 'WebRequest Auth');
+}, 'WebRequest Auth Sync');
 
 promise_test(async (test) => {
-  const controlledframe = await createControlledFrame('/simple.html', 'auth2');
+  const controlledframe = await createControlledFrame('/simple.html', 'noauth');
   controlledframe.stop();
 
   const targetUrl = new URL(controlledframe.src);
@@ -72,10 +118,10 @@ promise_test(async (test) => {
       window.events.slice(-1)[0], 'erroroccurred', 'erroroccurred fired');
   assert_equals(
       window.occurredErrors[0], 'net::ERR_TOO_MANY_RETRIES', 'error code');
-}, 'WebRequest Auth Error');
+}, 'WebRequest Auth Fail');
 
 promise_test(async (test) => {
-  const controlledframe = await createControlledFrame('/simple.html', 'auth3');
+  const controlledframe = await createControlledFrame('/simple.html', 'noauth');
   controlledframe.stop();
 
   const targetUrl = new URL(controlledframe.src);
@@ -87,10 +133,7 @@ promise_test(async (test) => {
     urlPatterns: [targetUrl.toString()],
     blocking: true,
   });
-  interceptor.addEventListener('authrequired', async (e) => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1);
-    });
+  interceptor.addEventListener('authrequired', (e) => {
     e.preventDefault();
   });
 
@@ -112,10 +155,90 @@ promise_test(async (test) => {
     'completed',
   ]);
   assert_equals(statusCode, 401);
-}, 'WebRequest Auth Cancel');
+}, 'WebRequest Auth Cancel Sync');
 
 promise_test(async (test) => {
-  const controlledframe = await createControlledFrame('/simple.html', 'auth4');
+  const controlledframe = await createControlledFrame('/simple.html', 'noauth');
+  controlledframe.stop();
+
+  const targetUrl = new URL(controlledframe.src);
+  targetUrl.pathname = '/auth-basic';
+  targetUrl.search = '*';
+  addWebRequestListeners(controlledframe, targetUrl.toString());
+
+  const interceptor = controlledframe.request.createWebRequestInterceptor({
+    urlPatterns: [targetUrl.toString()],
+    blocking: true,
+  });
+  interceptor.addEventListener('authrequired', (e) => {
+    const controller = new AbortController();
+    e.setCredentials(new Promise(() => {}), {signal: controller.signal});
+    sleep(1).then(() => {
+      controller.abort();
+    });
+  });
+
+  let statusCode = -1;
+  interceptor.addEventListener('completed', (e) => {
+    statusCode = e.response.statusCode;
+  });
+
+  targetUrl.search = 'password=PASS&realm=REALM';
+  await navigateControlledFrame(controlledframe, targetUrl.toString());
+
+  verifyWebRequestEvents([
+    'beforerequest',
+    'beforesendheaders',
+    'sendheaders',
+    'headersreceived',
+    'authrequired',
+    'responsestarted',
+    'completed',
+  ]);
+  assert_equals(statusCode, 401);
+}, 'WebRequest Auth Cancel AbortSignal');
+
+promise_test(async (test) => {
+  const controlledframe = await createControlledFrame('/simple.html', 'noauth');
+  controlledframe.stop();
+
+  const targetUrl = new URL(controlledframe.src);
+  targetUrl.pathname = '/auth-basic';
+  targetUrl.search = '*';
+  addWebRequestListeners(controlledframe, targetUrl.toString());
+
+  const interceptor = controlledframe.request.createWebRequestInterceptor({
+    urlPatterns: [targetUrl.toString()],
+    blocking: true,
+  });
+  interceptor.addEventListener('authrequired', (e) => {
+    const controller = new AbortController();
+    controller.abort();
+    e.setCredentials(new Promise(() => {}), {signal: controller.signal});
+  });
+
+  let statusCode = -1;
+  interceptor.addEventListener('completed', (e) => {
+    statusCode = e.response.statusCode;
+  });
+
+  targetUrl.search = 'password=PASS&realm=REALM';
+  await navigateControlledFrame(controlledframe, targetUrl.toString());
+
+  verifyWebRequestEvents([
+    'beforerequest',
+    'beforesendheaders',
+    'sendheaders',
+    'headersreceived',
+    'authrequired',
+    'responsestarted',
+    'completed',
+  ]);
+  assert_equals(statusCode, 401);
+}, 'WebRequest Auth Cancel Preaborted AbortSignal');
+
+promise_test(async (test) => {
+  const controlledframe = await createControlledFrame('/simple.html', 'noauth');
   controlledframe.stop();
 
   const targetUrl = new URL(controlledframe.src);
