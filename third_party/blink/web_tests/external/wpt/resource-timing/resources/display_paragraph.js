3a4df237b3418e5d849b994a82d225ce67b4f284
Guohui Deng <guohuideng@microsoft.com>
2025-06-26 07:40:58 -0700

    [WPT] Fix test document-initiated.html
    1) Ahem.ttf must not be retrieved from the cache, otherwise it results
    in a failure. 2) Function testResourceInitiatorUrl() must either resolve
    or reject, because it was waited by the promise_test(). 3) It's better
    to properly use the preloaded resource so the test doesn't emit a
    warning message.
    
    Will monitor document-initiated.html and make sure it's no longer flaky
    before we completely enable it again.
    
    Bug: 425936609
    Change-Id: I18b846b5a4b924b773c16a5635eb9a9ac76832ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656129
    Reviewed-by: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Guohui Deng <guohuideng@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1479147}

---
+++ third_party/blink/web_tests/external/wpt/resource-timing/resources/display_paragraph.js
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function display_paragraph() {
+   const paragraph = document.createElement('p');
+   paragraph.textContent = "Please display me.";
+   document.body.appendChild(paragraph);
+}
