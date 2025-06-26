d861384bb7ded6a901bb24c646a3aadc1abc38e0
Yuki Shiino <yukishiino@chromium.org>
2025-06-26 01:59:54 -0700

    gardening: Disable TabSearchContainerBrowserTest on Win
    Bug: 413441658
    Change-Id: Id0020d5243583de528e86a2762c1a2215071841b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6677874
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478998}

--- chrome/browser/ui/views/tabs/tab_search_container_browsertest.cc
+++ chrome/browser/ui/views/tabs/tab_search_container_browsertest.cc
@@ -276,8 +276,16 @@ IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest, DelaysHide) {
   EXPECT_TRUE(ExpansionAnimationIsClosing());
 }
 
+// TODO(crbug.com/413441658): Flaky on Windows 10 builds.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_ImmediatelyHidesWhenOrganizeButtonClicked \
+  DISABLED_ImmediatelyHidesWhenOrganizeButtonClicked
+#else
+#define MAYBE_ImmediatelyHidesWhenOrganizeButtonClicked \
+  ImmediatelyHidesWhenOrganizeButtonClicked
+#endif
 IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
-                       ImmediatelyHidesWhenOrganizeButtonClicked) {
+                       MAYBE_ImmediatelyHidesWhenOrganizeButtonClicked) {
   tab_search_container()->ShowTabOrganization(
       tab_search_container()->auto_tab_group_button());
   ResetAnimation(1);
@@ -292,8 +300,16 @@ IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
   EXPECT_TRUE(ExpansionAnimationIsClosing());
 }
 
+// TODO(crbug.com/413441658): Flaky on Windows 10 builds.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_ImmediatelyHidesWhenOrganizeButtonDismissed \
+  DISABLED_ImmediatelyHidesWhenOrganizeButtonDismissed
+#else
+#define MAYBE_ImmediatelyHidesWhenOrganizeButtonDismissed \
+  ImmediatelyHidesWhenOrganizeButtonDismissed
+#endif
 IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
-                       ImmediatelyHidesWhenOrganizeButtonDismissed) {
+                       MAYBE_ImmediatelyHidesWhenOrganizeButtonDismissed) {
   tab_search_container()->ShowTabOrganization(
       tab_search_container()->auto_tab_group_button());
   ResetAnimation(1);
@@ -465,8 +481,16 @@ IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
       "Tab.Organization.Declutter.Trigger.Outcome", 2, 1);
 }
 
+// TODO(crbug.com/413441658): Flaky on Windows 10 builds.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_HidesAutoTabGroupButtonFromHalfway \
+  DISABLED_HidesAutoTabGroupButtonFromHalfway
+#else
+#define MAYBE_HidesAutoTabGroupButtonFromHalfway \
+  HidesAutoTabGroupButtonFromHalfway
+#endif
 IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
-                       HidesAutoTabGroupButtonFromHalfway) {
+                       MAYBE_HidesAutoTabGroupButtonFromHalfway) {
   ASSERT_FALSE(tab_search_container()->animation_session_for_testing());
 
   tab_search_container()->ShowTabOrganization(
@@ -521,8 +545,14 @@ IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
                   ->IsShowing());
 }
 
+// TODO(crbug.com/413441658): Flaky on Windows 10 builds.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_ShowsAndHidesDeclutterChip DISABLED_ShowsAndHidesDeclutterChip
+#else
+#define MAYBE_ShowsAndHidesDeclutterChip ShowsAndHidesDeclutterChip
+#endif
 IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
-                       ShowsAndHidesDeclutterChip) {
+                       MAYBE_ShowsAndHidesDeclutterChip) {
   ASSERT_FALSE(tab_search_container()->animation_session_for_testing());
 
   tab_search_container()->ShowTabOrganization(
@@ -544,8 +574,17 @@ IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
   EXPECT_TRUE(ExpansionAnimationIsClosing());
 }
 
-IN_PROC_BROWSER_TEST_F(TabSearchContainerBrowserTest,
-                       DoesNotShowDeclutterChipWhenAutoTabGroupChipIsShown) {
+// TODO(crbug.com/413441658): Flaky on Windows 10 builds.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_DoesNotShowDeclutterChipWhenAutoTabGroupChipIsShown \
+  DISABLED_DoesNotShowDeclutterChipWhenAutoTabGroupChipIsShown
+#else
+#define MAYBE_DoesNotShowDeclutterChipWhenAutoTabGroupChipIsShown \
+  DoesNotShowDeclutterChipWhenAutoTabGroupChipIsShown
+#endif
+IN_PROC_BROWSER_TEST_F(
+    TabSearchContainerBrowserTest,
+    MAYBE_DoesNotShowDeclutterChipWhenAutoTabGroupChipIsShown) {
   ASSERT_FALSE(tab_search_container()->animation_session_for_testing());
 
   // Show the auto-tab group chip.
