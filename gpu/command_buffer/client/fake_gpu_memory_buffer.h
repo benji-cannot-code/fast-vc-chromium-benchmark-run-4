e8105587a62528369d20d9ea025f9c74ca0ee98f
Takuto Ikuta <tikuta@chromium.org>
2025-06-26 10:14:39 -0700

    gpu: Fix missing include for std::vector.
    Bug: 40263312
    Change-Id: Idd44db62a1c979fcdd4d7f0f5741fff3ed0e8bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680507
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479266}

--- gpu/command_buffer/client/fake_gpu_memory_buffer.h
+++ gpu/command_buffer/client/fake_gpu_memory_buffer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GPU_COMMAND_BUFFER_CLIENT_FAKE_GPU_MEMORY_BUFFER_H_
 
 #include <memory>
+#include <vector>
 
 #include "media/base/video_types.h"
 #include "ui/gfx/gpu_memory_buffer.h"
