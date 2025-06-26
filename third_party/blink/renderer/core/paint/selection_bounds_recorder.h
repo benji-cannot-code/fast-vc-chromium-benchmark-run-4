a70246c970027a9ed1d526c6aabc3b96e09a6aa5
Xianzhu Wang <wangxianzhu@chromium.org>
2025-06-26 14:56:02 -0700

    Clean up killswitch SelectionVisibilityAfterPaint
    It has been stable since M136.
    
    Bug: 376381581
    Change-Id: Iff0a58ef39563c29ee98b28b15271f4ec3615042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681567
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479475}

--- third_party/blink/renderer/core/paint/selection_bounds_recorder.h
+++ third_party/blink/renderer/core/paint/selection_bounds_recorder.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class FrameSelection;
-class LayoutObject;
 class PaintController;
 
 // This class is used for recording painted selection bounds. Based on the
@@ -32,16 +31,11 @@ class SelectionBoundsRecorder {
                           PhysicalRect,
                           PaintController&,
                           TextDirection,
-                          WritingMode,
-                          const LayoutObject&);
+                          WritingMode);
 
   ~SelectionBoundsRecorder();
 
   static bool ShouldRecordSelection(const FrameSelection&, SelectionState);
-
-  static bool IsVisible(const LayoutObject& rect_layout_object,
-                        const PhysicalOffset& edge_start_in_layer,
-                        const PhysicalOffset& edge_end_in_layer);
 
  private:
   const SelectionState state_;
@@ -49,7 +43,6 @@ class SelectionBoundsRecorder {
   PaintController& paint_controller_;
   TextDirection text_direction_;
   WritingMode writing_mode_;
-  const LayoutObject& selection_layout_object_;
 };
 
 }  // namespace blink
