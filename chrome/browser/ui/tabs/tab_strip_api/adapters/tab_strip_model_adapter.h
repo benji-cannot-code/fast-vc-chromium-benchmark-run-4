d0ff591154c489f4bc9f26fd849ca94370384f61
David Yeung <dayeung@chromium.org>
2025-06-26 11:37:41 -0700

    Typemap Position struct in TabStripApi
    Position should be typemapped for ease of use on the C++ side.
    Also the existing Position POD can not support an optional parent_id.
    
    Bug: 425390972
    Change-Id: I7936cff6e152e4f864723ae185bed63126cb5978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6675844
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479344}

--- chrome/browser/ui/tabs/tab_strip_api/adapters/tab_strip_model_adapter.h
+++ chrome/browser/ui/tabs/tab_strip_api/adapters/tab_strip_model_adapter.h
@@ -13,12 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tabs_api {
 
-// POD representation of a position within a collection. May be passed by
-// reference or by value.
-struct Position {
-  const size_t index;
-};
-
 // Tab strip has a large API service that is difficult to implement under test.
 // We only need a subset of the API, so an adapter is used to proxy those
 // methods. This makes it easier to swap in a fake for test.
