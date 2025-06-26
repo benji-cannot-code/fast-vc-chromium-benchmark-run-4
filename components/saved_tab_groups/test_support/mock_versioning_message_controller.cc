6328a366aca3272991eba29840548edec1d7a2a6
Louis Romero <lpromero@google.com>
2025-06-26 15:07:11 -0700

    Move MockVersioningMessageController to test_support
    This CL moves MockVersioningMessageController to be reusable by all
    platforms.
    
    Bug: 422424386
    Change-Id: I96a913586b8aafaa316eeb3607c7a2e0a3c04b14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6678554
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1479482}

---
+++ components/saved_tab_groups/test_support/mock_versioning_message_controller.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/saved_tab_groups/test_support/mock_versioning_message_controller.h"
+
+namespace tab_groups {
+
+MockVersioningMessageController::MockVersioningMessageController() = default;
+MockVersioningMessageController::~MockVersioningMessageController() = default;
+
+}  // namespace tab_groups
