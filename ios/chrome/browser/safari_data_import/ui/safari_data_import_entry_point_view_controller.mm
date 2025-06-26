edba2179bf0be78f3858012be298b3c645e2af37
ginnnnnnny <ginnyhuang@chromium.org>
2025-06-26 11:16:06 -0700

    [iOS][safari-data-import] Initial view implementation for import screen
    Unlike the entry point view controller, this view uses the PromoStyle-
    ViewController for easier UI layout formatting for the are between the
    subtitle and the button.
    
    Bug: 420703283
    Change-Id: I18493f5ebd1d5d54e415552b613e38dc984c130f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6669954
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479329}

--- ios/chrome/browser/safari_data_import/ui/safari_data_import_entry_point_view_controller.mm
+++ ios/chrome/browser/safari_data_import/ui/safari_data_import_entry_point_view_controller.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/safari_data_import/ui/safari_data_import_entry_point_view_controller.h"
 
-#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
---
+++ ios/chrome/browser/safari_data_import/ui/safari_data_import_import_view_controller.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_IMPORT_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_IMPORT_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/common/ui/promo_style/promo_style_view_controller.h"
+
+/// Main screen presented for `SafariDataImportImportCoordinator`.
+@interface SafariDataImportImportViewController : PromoStyleViewController
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_IMPORT_VIEW_CONTROLLER_H_
