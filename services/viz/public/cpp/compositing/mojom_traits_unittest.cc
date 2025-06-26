6c09e706e1679ed57ea9689849fa4a80746b94df
Aaron Moss <aaronmoss@google.com>
2025-06-26 06:51:29 -0700

    Replace TextureResults with SharedImage
    All instances of `CopyOutputTextureResults` in `CopyOutputResults`
    and callers replaced by `CopyOutputSharedImageResults` for more-
    consistent API around `Mailbox` + metadata shared images.
    
    Bug: 427238674
    Change-Id: I34bebe32db932230edce0665fd918fde6548a515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668495
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Aaron Moss <aaronmoss@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479109}

--- services/viz/public/cpp/compositing/mojom_traits_unittest.cc
+++ services/viz/public/cpp/compositing/mojom_traits_unittest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/viz/common/frame_sinks/copy_output_result.h"
 #ifdef UNSAFE_BUFFERS_BUILD
 // TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
 #pragma allow_unsafe_buffers
@@ -372,9 +373,8 @@ TEST_F(StructTraitsTest, CopyOutputRequest_TextureRequest) {
       run_loop_for_release.QuitClosure(), sync_token));
 
   output->SendResult(std::make_unique<CopyOutputTextureResult>(
-      result_format, result_rect,
-      CopyOutputResult::TextureResult(mailbox, gfx::ColorSpace::CreateSRGB()),
-      std::move(release_callbacks)));
+      result_format, result_rect, mailbox, gfx::ColorSpace::CreateSRGB(),
+      "CopyOutputRequest_TextureRequest", std::move(release_callbacks)));
 
   // Wait for the result to be delivered to the other side: The
   // CopyOutputRequest callback will be called, at which point
@@ -1433,8 +1433,8 @@ TEST_F(StructTraitsTest, CopyOutputResult_Texture) {
   mailbox.SetName(mailbox_name);
   std::unique_ptr<CopyOutputResult> input =
       std::make_unique<CopyOutputTextureResult>(
-          CopyOutputResult::Format::RGBA, result_rect,
-          CopyOutputResult::TextureResult(mailbox, result_color_space),
+          CopyOutputResult::Format::RGBA, result_rect, mailbox,
+          result_color_space, "CopyOutputResult_Texture",
           std::move(release_callbacks));
 
   std::unique_ptr<CopyOutputResult> output;
