0f210451d74db10d9b1abdc84507beac5bbca0e8
Asami Doi <asamidoi@chromium.org>
2025-06-26 02:40:49 -0700

    [ios] Fix NotificationsTestCase/testSafetyCheckSwitch and NotificationsTestCase/testTipsSwitch with iOS 26
    The following tests are failing with iOS 26 because multiple elements
    are matched.
    - NotificationsTestCase/testSafetyCheckSwitch
    - NotificationsTestCase/testTipsSwitch
    
    This CL adds a helper function to match only one item and fixes the
    tests.
    
    Fixed: 427712294
    Change-Id: I706a40496db38cf2c98934e2e3f80d64c1b4bf81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6674293
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479013}

--- ios/chrome/test/earl_grey/chrome_matchers_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_matchers_app_interface.h
@@ -66,6 +66,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // corresponding to `messageID`.
 + (id<GREYMatcher>)actionSheetItemWithAccessibilityLabelID:(int)messageID;
 
+// Matcher for an alert item with accessibility label corresponding to
+// `messageID`.
++ (id<GREYMatcher>)alertItemWithAccessibilityLabelId:(int)messageID;
+
 // Matcher for element with an image corresponding to `image`.
 + (id<GREYMatcher>)imageViewWithImage:(UIImage*)image;
 
