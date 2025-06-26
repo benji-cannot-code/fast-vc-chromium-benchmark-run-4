f6c4671832559f089d25bcb59fdc2e7c3cb6a38d
Mitsuru Oshima <oshima@chromium.org>
2025-06-26 11:23:20 -0700

    Add trusted-pinned state support to TestAppWindow
    * Add toggle button to pin/unpin the window after 5 seconds.
    * The delay is added so that we can test multiple app requested
      pinned state.
    
    Bug: None
    Change-Id: Ia18586412369774a9b8567025ed15591c879f5a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681587
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479333}

--- ash/examples/test_app_window.cc
+++ ash/examples/test_app_window.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/examples/client_controlled_state_util.h"
 #include "ash/shell.h"
+#include "ash/wm/window_pin_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
@@ -145,6 +146,25 @@ class TestView : public views::View {
                                            ? ui::ZOrderLevel::kFloatingWindow
                                            : ui::ZOrderLevel::kNormal);
                  }));
+    add_checkbox(this, u"Trusted Pinned After 5 seconds", u"trusted pinned",
+                 /*initial_state=*/false,
+                 base::BindRepeating(
+                     [](TestView* this_, const ui::Event& event) {
+                       views::Checkbox* cb =
+                           static_cast<views::Checkbox*>(event.target());
+                       auto* window = cb->GetWidget()->GetNativeWindow();
+                       bool pinned = cb->GetChecked();
+                       this_->RunAfterFiveSeconds(base::BindRepeating(
+                           [](aura::Window* window, bool pinned) {
+                             if (pinned) {
+                               PinWindow(window, /*trusted=*/true);
+                             } else {
+                               UnpinWindow(window);
+                             }
+                           },
+                           base::Unretained(window), pinned));
+                     },
+                     base::Unretained(this)));
 
     AddChildView(std::make_unique<views::LabelButton>(
         base::BindRepeating(
