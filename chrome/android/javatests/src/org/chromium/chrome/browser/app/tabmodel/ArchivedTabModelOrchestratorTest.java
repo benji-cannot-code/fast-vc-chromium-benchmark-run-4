49fa8d7720f7ed2cf5f88eb194246c2dc2eb87f8
Sky Malice <skym@chromium.org>
2025-06-26 09:29:15 -0700

    Implement VersioningMessageBanner with a stub dialog.
    Bug: 422514006
    Change-Id: I9d5887bf573e2f866f56d9f3ba1cc1b977f6afad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6674616
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479230}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestratorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestratorTest.java
@@ -66,6 +66,7 @@ import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
 import org.chromium.components.tab_group_sync.SavedTabGroup;
 import org.chromium.components.tab_group_sync.TabGroupSyncService;
+import org.chromium.components.tab_group_sync.VersioningMessageController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -114,6 +115,7 @@ public class ArchivedTabModelOrchestratorTest {
     @Mock private TabPersistentStore mNormalTabPersistentStore;
     @Mock private TabModelSelectorBase mTabModelSelector;
     @Mock private TabGroupSyncService mTabGroupSyncService;
+    @Mock private VersioningMessageController mVersioningMessageController;
 
     private Profile mProfile;
     private FakeDeferredStartupHandler mDeferredStartupHandler;
@@ -128,6 +130,8 @@ public class ArchivedTabModelOrchestratorTest {
     public void setUp() throws Exception {
         TabGroupSyncServiceFactory.setForTesting(mTabGroupSyncService);
         when(mTabGroupSyncService.getAllGroupIds()).thenReturn(new String[] {});
+        when(mTabGroupSyncService.getVersioningMessageController())
+                .thenReturn(mVersioningMessageController);
 
         mDeferredStartupHandler = new FakeDeferredStartupHandler();
         DeferredStartupHandler.setInstanceForTests(mDeferredStartupHandler);
