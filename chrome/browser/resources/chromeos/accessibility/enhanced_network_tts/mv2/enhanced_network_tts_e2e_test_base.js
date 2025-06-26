6193ed670d7c700a3477a071970b05d6f7871819
Akihiro Ota <akihiroota@chromium.org>
2025-06-26 08:23:43 -0700

    a11y-mv3: Ensure manifest v3 is off for mv2 tests
    This change explicitly forces manifest v3 off for mv2 tests for
    enhanced network tts. This is important because it will keep these
    tests stable when we roll mv3 out using finch.
    
    Bug: 388867658
    Change-Id: Ib0c33750a80ca6a03eb658e7b7c0114799565645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6673955
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479183}

--- chrome/browser/resources/chromeos/accessibility/enhanced_network_tts/mv2/enhanced_network_tts_e2e_test_base.js
+++ chrome/browser/resources/chromeos/accessibility/enhanced_network_tts/mv2/enhanced_network_tts_e2e_test_base.js
@@ -32,6 +32,14 @@ EnhancedNetworkTE2ETestBase = class extends E2ETestBase {
   }
 
   /** @override */
+  testGenCppIncludes() {
+    super.testGenCppIncludes();
+    GEN(`
+#include "ui/accessibility/accessibility_features.h"
+    `);
+  }
+
+  /** @override */
   testGenPreamble() {
     super.testGenPreamble();
     GEN(`
@@ -40,5 +48,10 @@ EnhancedNetworkTE2ETestBase = class extends E2ETestBase {
             &ash::AccessibilityManager::LoadEnhancedNetworkTtsForTest,
             base::Unretained(ash::AccessibilityManager::Get()));
     `);
+  }
+
+  /** @override */
+  get featureList() {
+    return {disabled: ['features::kAccessibilityManifestV3EnhancedNetworkTts']};
   }
 };
