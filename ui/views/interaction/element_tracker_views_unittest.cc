8753cf65291c7d856acd99b0e2ff853621652e8d
Dana Fried <dfried@chromium.org>
2025-06-26 07:11:33 -0700

    [Views] Element Tracker (Views) Optimizations
    ElementTrackerViews watches, among other signals,
    OnViewVisibilityChanged(), which is called when a view, any of its
    ancestors in the hierarchy, or its widget changes visibility.
    
    Previously, this did not include information on whether the item in
    question had become visible or not-visible, which then required the
    code to walk the view's hierarchy to determine if it was actually being
    drawn. Furthermore, the visibility of a widget did not immediately
    update on all platforms, leading to a situation where a callback
    might be received when a widget was shown or hidden but the widget
    could report previous visibility state - which then required additional
    handling to avoid reporting an incorrect state.
    
    This CL uses the fact that OnViewVisibilityChanged() now specifies
    whether the source became visible or not-visible; on transition to
    not-visible, we know the target View is no longer visible, so all of
    that additional logic can be bypassed. This should also simplify some
    corner cases, and additional special case tests have been added to
    try to catch the most pathological ones.
    
    Bug: 426560497
    Change-Id: Iddfe292dcaed1afabf32c14caf94de3e5b2e8d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652872
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479124}

--- ui/views/interaction/element_tracker_views_unittest.cc
+++ ui/views/interaction/element_tracker_views_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/element_test_util.h"
 #include "ui/base/interaction/element_tracker.h"
+#include "ui/base/interaction/expect_call_in_scope.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/events/base_event_utils.h"
@@ -884,6 +885,9 @@ TEST_F(ElementTrackerViewsTest, WidgetShownAfterAdd) {
       kTestElementID, context));
 }
 
+// ------------------------------------------------------------------
+// Corner Cases
+
 // This is a gross corner case where a Widget might not report IsVisible()
 // during show, but we're still showing views and could conceivably add another
 // view as part of a callback.
@@ -932,6 +936,66 @@ TEST_F(ElementTrackerViewsTest, AddedDuringWidgetShow) {
   widget->Hide();
   EXPECT_TRUE(called);
 }
+
+TEST_F(ElementTrackerViewsTest, AddedBeforeShowDeletedDuringShow) {
+  auto widget = CreateWidget();
+  const auto context = ElementTrackerViews::GetContextForWidget(widget.get());
+  UNCALLED_MOCK_CALLBACK(ui::ElementTracker::Callback, shown);
+  UNCALLED_MOCK_CALLBACK(ui::ElementTracker::Callback, hidden);
+  auto shown_subscription =
+      ui::ElementTracker::GetElementTracker()->AddElementShownCallback(
+          kTestElementID, context, shown.Get());
+  auto hidden_subscription =
+      ui::ElementTracker::GetElementTracker()->AddElementHiddenCallback(
+          kTestElementID, context, hidden.Get());
+  auto* const contents = widget->SetContentsView(std::make_unique<View>());
+  contents->SetProperty(kElementIdentifierKey, kTestElementID);
+  EXPECT_CALL_IN_SCOPE(shown, Run, widget->Show());
+  EXPECT_CALL_IN_SCOPE(hidden, Run, widget.reset());
+}
+
+TEST_F(ElementTrackerViewsTest, AddedAfterShowDeletedDuringShow) {
+  auto widget = CreateWidget();
+  const auto context = ElementTrackerViews::GetContextForWidget(widget.get());
+  UNCALLED_MOCK_CALLBACK(ui::ElementTracker::Callback, shown);
+  UNCALLED_MOCK_CALLBACK(ui::ElementTracker::Callback, hidden);
+  auto shown_subscription =
+      ui::ElementTracker::GetElementTracker()->AddElementShownCallback(
+          kTestElementID, context, shown.Get());
+  auto hidden_subscription =
+      ui::ElementTracker::GetElementTracker()->AddElementHiddenCallback(
+          kTestElementID, context, hidden.Get());
+  auto* const contents = widget->SetContentsView(std::make_unique<View>());
+  EXPECT_CALL_IN_SCOPE(shown, Run, {
+    widget->Show();
+    contents->SetProperty(kElementIdentifierKey, kTestElementID);
+  });
+  EXPECT_CALL_IN_SCOPE(hidden, Run, widget.reset());
+}
+
+TEST_F(ElementTrackerViewsTest, AddedDuringHideThenDeleted) {
+  auto widget = CreateWidget();
+  const auto context = ElementTrackerViews::GetContextForWidget(widget.get());
+  UNCALLED_MOCK_CALLBACK(ui::ElementTracker::Callback, shown);
+  UNCALLED_MOCK_CALLBACK(ui::ElementTracker::Callback, hidden);
+  auto shown_subscription =
+      ui::ElementTracker::GetElementTracker()->AddElementShownCallback(
+          kTestElementID, context, shown.Get());
+  auto hidden_subscription =
+      ui::ElementTracker::GetElementTracker()->AddElementHiddenCallback(
+          kTestElementID, context, hidden.Get());
+  auto contents = std::make_unique<View>();
+  contents->SetProperty(kElementIdentifierKey, kTestElementID);
+  test::WidgetVisibleWaiter waiter(widget.get());
+  widget->Show();
+  waiter.Wait();
+  widget->Hide();
+  widget->SetContentsView(std::move(contents));
+  widget.reset();
+}
+
+// End Corner Cases
+// ------------------------------------------------------------------
 
 TEST_F(ElementTrackerViewsTest, CleansUpWidgetTrackers) {
   auto widget1 = CreateWidget();
