8e06a743a26b9201fa1c8c3e2f8c27f8ce6bad49
Colin Blundell <blundell@chromium.org>
2025-06-25 23:30:24 -0700

    [Blink] Have CanvasRenderingContextHost::Commit() return bool
    Aligns its signature with PushFrame(), which will facilitate pulling out
    similar flows into a helper in the WebGL context in a followup.
    
    Bug: 352263194
    Change-Id: Ic8e1690ba0b63f85260de4d097657167a7c320c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6672888
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478949}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
@@ -124,7 +124,7 @@ class CORE_EXPORT OffscreenCanvas final
                  const SkIRect& damage_rect) override;
   void DidDraw(const SkIRect&) override;
   using CanvasRenderingContextHost::DidDraw;
-  void Commit(scoped_refptr<CanvasResource>&& bitmap_image,
+  bool Commit(scoped_refptr<CanvasResource>&& bitmap_image,
               const SkIRect& damage_rect) override;
   bool ShouldAccelerate2dContext() const override;
   CanvasResourceDispatcher* GetOrCreateResourceDispatcher() override;
