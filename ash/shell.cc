1bd64a08202f79ba6df335b4aaf9538a6a1e8a46
Mitsuru Oshima <oshima@chromium.org>
2025-06-27 16:43:41 -0700

    Close all app like windows before snapgroup controller is deleted
    Fixed: 423973589
    Test: covered by unittest
    Change-Id: I383172862af2cb6bb182578db8486ff0afe86387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654804
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480152}

--- ash/shell.cc
+++ ash/shell.cc
@@ -253,6 +253,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm_mode/wm_mode_controller.h"
 #include "base/check.h"
 #include "base/command_line.h"
+#include "base/containers/adapters.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ptr_util.h"
@@ -824,6 +825,10 @@ Shell::~Shell() {
   views::ViewsTextServicesContextMenuChromeos::SetImplFactory(
       base::NullCallback());
 
+  // Close and destroy all application windows here, so that the window manager
+  // related objects, which app windows relies on, can be sefely deleted.
+  CloseAllAppWindows();
+
   wm_mode_controller_.reset();
 
   // `shortcut_input_handler_` must be cleaned up before
@@ -981,7 +986,8 @@ Shell::~Shell() {
   // are needed for proper deletion of RoundedDisplayProviders.
   window_tree_host_manager_->ShutdownRoundedDisplays();
 
-  // Close all widgets (including the shelf) and destroy all window containers.
+  // Close all windows and associated widgets (including system UI) and destroy
+  // all containers.
   CloseAllRootWindowChildWindows();
 
   glanceables_controller_.reset();
@@ -1967,6 +1973,21 @@ void Shell::InitRootWindow(aura::Window* root_window) {
   ::wm::SetWindowMoveClient(root_window, toplevel_window_event_handler_.get());
   root_window->AddPreTargetHandler(toplevel_window_event_handler_.get());
   root_window->AddPostTargetHandler(toplevel_window_event_handler_.get());
+}
+
+void Shell::CloseAllAppWindows() {
+  auto list = mru_window_tracker_->BuildAppWindowList(DesksMruType::kAllDesks);
+  aura::WindowTracker tracker;
+  for (auto window : list) {
+    tracker.Add(window.get());
+  }
+  // Delete from the bottom of mru list so that it won't affect activation.
+  for (auto window : base::Reversed(list)) {
+    // Make sure that the window in the `list` is still alive.
+    if (tracker.Contains(window)) {
+      delete window;
+    }
+  }
 }
 
 void Shell::CloseAllRootWindowChildWindows() {
