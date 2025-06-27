9f1958e0c22e96e42d5f1273a23163eecfbe3a8c
Colin Blundell <blundell@chromium.org>
2025-06-26 23:34:04 -0700

    [Blink] Eliminate usage of CanvasResourceProviderPassThrough
    The current flow for usage of CanvasResourceProviderPassThrough by the
    WebGL context is as follows:
    
    * Create CRPPassThrough if possible in GetOrCreateCRP()
    * If a passthrough CRP was created, export ExternalCanvasResource from
      the DrawingBuffer into the CRP in PaintRenderingResultsToCanvas()
    * Use that ExternalCanvasResource via the CRP at the callsites of
      PaintRenderingResultsToCanvas()
    
    This CL changes the flow to work as follows:
    
    * Add a new ExportLowLatencyResource() method that serves as an
      alternative to PaintRenderingResultsToCanvas() for the case where the
      DrawingBuffer's SI can be exported without copy. This method works
      analogously to the current flow in PaintRenderingResultsToCanvas() [1]
      but just returns the ExternalCanvasResource that it creates.
    * Have all callsites of PaintRenderingResultsToCanvas() check whether
      the DrawingBuffer's SI can be exported without copy, call
      ExportLowLatencyResource() in that case rather than
      PaintRenderingResultsToCanvas(), and use the created
      ExternalCanvasResource directly.
    * Change PaintRenderingResultsToCanvas() and
      GetOrCreateCanvasResourceProvider() to be *only* for the case where
      the DrawingBuffer's SI cannot be exported directly.
    
    This comes with one behavioral change:
    
    * As CanUseDrawingBufferSiWithoutCopyForLowLatency() returns false if
      the shared GPU context is lost, we will now be checking for that
      context loss at the current callsites of
      PaintRenderingResultsToCanvas(), whereas before we would elide that
      check if we had already created a CRPPassThrough via an earlier call
      to GetOrCreateCanvasResourceProvider(). This would cause a behavioral
      difference only in the case where there is a difference in the state
      of the context being lost between those two calls. As
      (a) PaintRenderingResultsToCanvas() already checks for the WebGL
      context being lost, (b) if one context is lost, the other will be lost
      as well, and (c) if one context can be restored, the other will be
      restored as well, any differences in flow caused by this behavioral
      change will be transient.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc;l=2124;drc=13075886efc851f658b6b14500272e62e5521614
    
    Bug: 352263194
    Change-Id: I3c1269bd20c45aa62b35a0bb776684d65ddbcb96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6679014
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479662}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
@@ -1963,6 +1963,10 @@ class MODULES_EXPORT WebGLRenderingContextBase
                                 Platform::ContextType context_type,
                                 Platform::GraphicsInfo* graphics_info);
 
+  scoped_refptr<ExternalCanvasResource> ExportLowLatencyCanvasResource(
+      SourceDrawingBuffer source_buffer,
+      bool export_only_if_update);
+
   CanvasResourceProvider* GetOrCreateCanvasResourceProvider();
   CanvasResourceProvider* PaintRenderingResultsToCanvas(
       SourceDrawingBuffer source_buffer,
