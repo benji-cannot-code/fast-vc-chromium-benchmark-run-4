7fd4aa921934d5994ae758bc39f90ed2e2be6d7f
James Lee <ljjlee@google.com>
2025-06-27 04:59:56 -0700

    Clean up kRemoveSearchEngineChoiceAttribution feature flag
    This flag is fully launched, and the disabled path is no longer required.
    
    Bug: 424428814
    Change-Id: I753a15f2b8d21065b5c02ee5284323b84bf4dd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680396
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1479742}

--- components/search_engines/template_url_unittest.cc
+++ components/search_engines/template_url_unittest.cc
@@ -2497,13 +2497,6 @@ TEST_F(TemplateURLTest, GenerateSearchURL) {
 }
 
 TEST_F(TemplateURLTest, GenerateURL_NoRegulatoryExtensions) {
-#if BUILDFLAG(IS_ANDROID)
-  // Disable the chrome_dse_attribution parameter from being added on Android.
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      switches::kRemoveSearchEngineChoiceAttribution);
-#endif
-
   TemplateURLData data;
   data.SetURL("https://search?q={searchTerms}");
   data.suggestions_url = "https://suggest?q={searchTerms}";
@@ -2533,13 +2526,6 @@ TEST_F(TemplateURLTest, GenerateURL_NoRegulatoryExtensions) {
 }
 
 TEST_F(TemplateURLTest, GenerateURL_WithEmptyRegulatoryExtensions) {
-#if BUILDFLAG(IS_ANDROID)
-  // Disable the chrome_dse_attribution parameter from being added on Android.
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      switches::kRemoveSearchEngineChoiceAttribution);
-#endif
-
   TemplateURLData::RegulatoryExtension default_ext{
       .variant = RegulatoryExtensionType::kDefault,
   };
@@ -2579,13 +2565,6 @@ TEST_F(TemplateURLTest, GenerateURL_WithEmptyRegulatoryExtensions) {
 }
 
 TEST_F(TemplateURLTest, GenerateURL_WithFullRegulatoryExtensions) {
-#if BUILDFLAG(IS_ANDROID)
-  // Disable the chrome_dse_attribution parameter from being added on Android.
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      switches::kRemoveSearchEngineChoiceAttribution);
-#endif
-
   TemplateURLData::RegulatoryExtension default_ext{
       .variant = RegulatoryExtensionType::kDefault,
       .search_params = "default_search_param=123",
@@ -2656,13 +2635,6 @@ TEST_F(TemplateURLTest, GenerateURL_WithFullRegulatoryExtensions) {
 
 TEST_F(TemplateURLTest,
        ReplaceSearchTerms_NoRegulatoryExpansionForUnsupportedUrls) {
-#if BUILDFLAG(IS_ANDROID)
-  // Disable the chrome_dse_attribution parameter from being added on Android.
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      switches::kRemoveSearchEngineChoiceAttribution);
-#endif
-
   TemplateURLData::RegulatoryExtension default_ext{
       .variant = RegulatoryExtensionType::kDefault,
       .search_params = "default_search_param=123",
