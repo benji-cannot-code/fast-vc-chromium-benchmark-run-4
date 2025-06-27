1cb227cfe86b7d9b8400933665194b761ffd6fc8
Timofey Chudakov <tchudakov@google.com>
2025-06-27 00:55:19 -0700

    [Wallet] Open Google Wallet passes settings.
    Before this CL, "Wallet settings" button opened the loyalty card
    preference in Chrome Payment settings. This CL changes the logic
    to open the passes settings page in Google Wallet.
    
    Screencast: http://shortn/_CxPbbyTOAw
    
    Bug: 420957826
    Change-Id: I8eb141fd312b2ac31c4212ec7519279e42053306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680391
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1479678}

--- chrome/android/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodControllerBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodControllerBridge.java
@@ -16,7 +16,7 @@ import org.jni_zero.NativeMethods;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.autofill.AutofillFallbackSurfaceLauncher;
-import org.chromium.chrome.browser.autofill.settings.SettingsNavigationHelper;
+import org.chromium.chrome.browser.autofill.GoogleWalletLauncher;
 import org.chromium.ui.base.WindowAndroid;
 
 import java.lang.ref.WeakReference;
@@ -78,7 +78,8 @@ class TouchToFillPaymentMethodControllerBridge
     @Override
     public void showGoogleWalletSettings() {
         if (mContext.get() != null) {
-            SettingsNavigationHelper.showGoogleWalletSettings(mContext.get());
+            GoogleWalletLauncher.openGoogleWalletPassesSettings(
+                    mContext.get(), mContext.get().getPackageManager());
         }
     }
 
