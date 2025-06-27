5abd39a96ce765950a2eaa91b51e75ff9d6fdddb
Joemer Ramos <joemerramos@google.com>
2025-06-27 02:48:24 -0700

    [iOS] Polish BWG Navigation View and Promo UI
    This CL does the following:
    - Prevents the FRE modal from being dismissed by tapping the
    background or by dragging it down.
    - Lightens the separator line color between the two icon rows
    - Increases icon size
    
    UI Screenshot: http://screen/8r23bpM2nfpUocy
    
    Bug: 414375434
    Change-Id: Ib9c8fbf5b18c814e8f64553e3b67ce5f9321ba10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681440
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479709}

--- ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller.mm
+++ ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller.mm
@@ -25,7 +25,7 @@ const CGFloat kMainStackHorizontalInset = 20.0;
 const CGFloat kMainStackSpacing = 8.0;
 
 // Icons size.
-const CGFloat kIconSize = 16.0;
+const CGFloat kIconSize = 24.0;
 const CGFloat kWhiteInnerSize = 32.0;
 
 // Corner radius of icons.
@@ -110,7 +110,7 @@ const CGFloat kSpacingPrimarySecondaryButtons = 0.0;
   UIView* wrapperContainer = [[UIView alloc] init];
   wrapperContainer.translatesAutoresizingMaskIntoConstraints = NO;
   UIView* separator = [[UIView alloc] init];
-  separator.backgroundColor = [UIColor colorNamed:kGrey500Color];
+  separator.backgroundColor = [UIColor colorNamed:kGrey200Color];
   separator.translatesAutoresizingMaskIntoConstraints = NO;
   [wrapperContainer addSubview:separator];
   [NSLayoutConstraint activateConstraints:@[
