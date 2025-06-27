9f1d5fa6400c164175c767e3827e3608db762a64
Aisulu R <aisulu@google.com>
2025-06-27 09:38:31 -0700

    [T&S Sentiment] Improve prod and test code as a default enablement prep
    This CL is a preparation for graduating the TrustSafetySentimentSurveyV2
    from Finch and enabling the survey by default in the code.
    
    Enabling the feature by default for desktop crashes a lot of tests
    (approximately 600 tests across ~50 test suites). This indicates there
    is a problem within a survey code itself.
    
    In addition, existing TrustSafetySentimentService tests crash as well -
    due to an unearthed latent bug and assumptions that V2 is disabled.
    
    This CL:
    - adds missing checks to prod so all unrelated tests run smoothly when
    V2 is enabled by default
    - fixes a latent bug in unittests
    - updates related tests to accommodate "V2 enabled by default"
    - while at it, improves service code by using ScopedObserver (suggested
    by reviewers, thanks!)
    
    Bug: 333526530
    Change-Id: I5e10f8f35afa61c01610ddd893dcdc24f869891b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650988
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479893}

--- chrome/browser/ui/hats/trust_safety_sentiment_service_browsertest.cc
+++ chrome/browser/ui/hats/trust_safety_sentiment_service_browsertest.cc
@@ -36,7 +36,7 @@ class TrustSafetySentimentServiceBrowserTest : public InProcessBrowserTest {
  public:
   TrustSafetySentimentServiceBrowserTest() {
     feature_list_.InitAndEnableFeatureWithParameters(
-        features::kTrustSafetySentimentSurvey,
+        features::kTrustSafetySentimentSurveyV2,
         {{"trusted-surface-probability", "1.0"}});
   }
 
@@ -109,7 +109,7 @@ IN_PROC_BROWSER_TEST_F(TrustSafetySentimentServiceBrowserTest,
   SurveyBitsData expected_product_specific_data = {
       {"Interacted with Page Info", false}};
   EXPECT_CALL(*mock_hats_service_,
-              LaunchSurvey(kHatsSurveyTriggerTrustSafetyTrustedSurface, _, _,
+              LaunchSurvey(kHatsSurveyTriggerTrustSafetyV2TrustedSurface, _, _,
                            expected_product_specific_data, _, _, _));
   {
     base::subtle::ScopedTimeClockOverrides override(
@@ -153,7 +153,7 @@ IN_PROC_BROWSER_TEST_F(TrustSafetySentimentServiceBrowserTest,
   SurveyBitsData expected_product_specific_data = {
       {"Interacted with Page Info", true}};
   EXPECT_CALL(*mock_hats_service_,
-              LaunchSurvey(kHatsSurveyTriggerTrustSafetyTrustedSurface, _, _,
+              LaunchSurvey(kHatsSurveyTriggerTrustSafetyV2TrustedSurface, _, _,
                            expected_product_specific_data, _, _, _));
 
   {
--- chrome/browser/ui/hats/trust_safety_sentiment_service_factory.h
+++ chrome/browser/ui/hats/trust_safety_sentiment_service_factory.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TrustSafetySentimentServiceFactory : public ProfileKeyedServiceFactory {
  public:
   static TrustSafetySentimentServiceFactory* GetInstance();
+  static void ShutDownForTesting(content::BrowserContext* context);
   static TrustSafetySentimentService* GetForProfile(Profile* profile);
 
  private:
