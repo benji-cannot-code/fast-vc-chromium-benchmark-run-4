01fa0fd919d7a70ae28573906a84db61daae1265
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-06-27 14:48:49 -0700

    [iOS][BWG] Hookup sessions delegate handler with BWG service
    New BWG sessions handler and hooking it up with the gateway in upstream.
    
    Bug: 419070203
    Change-Id: Ibbea3c1c8468759d525aeb7af9491dcf520bdf52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685846
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480105}

--- ios/chrome/browser/intelligence/bwg/model/bwg_service.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_service.mm
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_configuration.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_delegate.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_handler.h"
+#import "ios/chrome/browser/intelligence/bwg/model/bwg_session_delegate.h"
+#import "ios/chrome/browser/intelligence/bwg/model/bwg_session_handler.h"
 #import "ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
@@ -47,6 +49,9 @@ BwgService::BwgService(AuthenticationService* auth_service,
   if (bwg_gateway_) {
     bwg_link_opening_handler_ = [[BWGLinkOpeningHandler alloc] init];
     bwg_gateway_.linkOpeningHandler = bwg_link_opening_handler_;
+
+    bwg_session_handler_ = [[BWGSessionHandler alloc] init];
+    bwg_gateway_.sessionHandler = bwg_session_handler_;
   }
 }
 
