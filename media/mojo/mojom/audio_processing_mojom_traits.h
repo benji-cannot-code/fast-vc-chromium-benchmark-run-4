baf2cb46852c2634362958d451dcddd91f65a32a
Fredrik Hernqvist <fhernqvist@google.com>
2025-06-27 04:08:31 -0700

    Use system loopback as AEC reference depending on settings flag
    This makes the AudioService use the `use_loopback_aec_reference` flag in
    AudioProcessingSettings to decide if chrome-wide AEC or loopback AEC
    should be used. (this flag used to be `needs_loopback_aec_reference`,
    this CL renames it to `use_loopback_aec_reference`.
    
    This changes the role of the SystemLoopbackAsAecReference flag. Instead
    of forcing the use of loopback AEC (which was the previous behaviour),
    it now only decides if LoopbackReferenceManager should be created.
    
    To still allow us to test loopback AEC, we now add an option to force
    the use of loopback AEC to the command line. Specifying
    `--enable-features=SystemLoopbackAsAecReference:forced_on/true` now uses
    loopback AEC whenever chrome-wide AEC would be used.
    
    Note that this CL is intended to be landed after
    https://chromium-review.googlesource.com/c/chromium/src/+/6670033
    
    Bug: 415954529
    
    Change-Id: I6dfcf4f8692e8d311998433c5e51ae819618a5cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668134
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479730}

--- media/mojo/mojom/audio_processing_mojom_traits.h
+++ media/mojo/mojom/audio_processing_mojom_traits.h
@@ -53,6 +53,10 @@ struct StructTraits<media::mojom::AudioProcessingSettingsDataView,
       const media::AudioProcessingSettings& s) {
     return s.multi_channel_capture_processing;
   }
+  static bool use_loopback_aec_reference(
+      const media::AudioProcessingSettings& s) {
+    return s.use_loopback_aec_reference;
+  }
   static bool Read(media::mojom::AudioProcessingSettingsDataView input,
                    media::AudioProcessingSettings* out_settings);
 };
