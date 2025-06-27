9ba29996f3170c6e6dbb4a996e2d1eab1853f7b2
Andrew Williams <awillia@chromium.org>
2025-06-27 13:03:19 -0700

    [Blob URL] Bypass partitioning for media navigation resource loads
    Navigating to Blob URLs with a media mime type results in a resource
    load for the URL that we don't want to enforce partitioning on.
    This CL adds a method of detecting this case (when a resource load
    for a blob URL occurs in a top-level document with that same URL)
    and bypasses partitioning in this case. For more info see the
    corresponding bug.
    
    This CL also re-enables the partitioning feature flags for ChromeOS.
    
    Bug: 426787402, 421810301
    Change-Id: Ib354612ecec212d7967089e06dfdd3195992cdc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656174
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480048}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -12799,6 +12799,12 @@ void RenderFrameHostImpl::BindBlobUrlStoreAssociatedReceiver(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   auto* storage_partition_impl =
       static_cast<StoragePartitionImpl*>(GetStoragePartition());
+
+  std::optional<GURL> top_level_blob_document_url;
+  if (GetLastCommittedURL().SchemeIsBlob() && IsOutermostMainFrame()) {
+    top_level_blob_document_url = GetLastCommittedURL();
+  }
+
   storage_partition_impl->GetBlobUrlRegistry()->AddReceiver(
       GetStorageKey(), GetLastCommittedOrigin(),
       GetProcess()->GetDeprecatedID(), std::move(receiver),
@@ -12813,6 +12819,7 @@ void RenderFrameHostImpl::BindBlobUrlStoreAssociatedReceiver(
             return frame->IsFullCookieAccessAllowed();
           },
           weak_ptr_factory_.GetWeakPtr()),
+      std::move(top_level_blob_document_url),
       !(GetContentClient()->browser()->IsBlobUrlPartitioningEnabled(
           GetBrowserContext())));
 }
--- storage/browser/blob/blob_url_store_impl.h
+++ storage/browser/blob/blob_url_store_impl.h
@@ -43,6 +43,8 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) BlobURLStoreImpl
           partitioning_blob_url_closure = base::DoNothing(),
       base::RepeatingCallback<bool()> storage_access_check_closure =
           base::BindRepeating([]() -> bool { return false; }),
+      std::optional<GURL> top_level_blob_document_url =
+          std::nullopt,
       bool partitioning_disabled_by_policy = false);
 
   BlobURLStoreImpl(const BlobURLStoreImpl&) = delete;
@@ -112,6 +114,10 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) BlobURLStoreImpl
       partitioning_blob_url_closure_;
 
   base::RepeatingCallback<bool()> storage_access_check_callback_;
+
+  // Set when this BlobURLStoreImpl corresponds to a top-level document created
+  // by navigating to a blob URL.
+  std::optional<GURL> top_level_blob_document_url_;
 
   const bool partitioning_disabled_by_policy_;
 
