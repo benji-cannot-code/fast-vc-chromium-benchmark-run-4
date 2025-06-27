ccaa88fadfe6507986bcd94f3d92d298561598cc
Radu Nitescu <radunitescu@google.com>
2025-06-27 10:13:52 -0700

    [iOS][MIA] Reset button stack trailing margin in landscape
    Landcape does not proportionally adjusts the trailing space. For this
    reason it needs to be reset when the orientation changes, independently
    from the scroll percent.
    
    Fixed: 428196469
    Change-Id: I9d54ee132b96d29970160f4bf7ef0283d3f18b89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681793
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1479921}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
@@ -627,6 +627,9 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
 
     self.separator.alpha = 0;
 
+    _buttonStack.directionalLayoutMargins = NSDirectionalEdgeInsetsMake(
+        0, 0, 0, [self endButtonFakeboxTrailingSpace]);
+
     _lastAnimationPercent = percent;
     return;
   }
