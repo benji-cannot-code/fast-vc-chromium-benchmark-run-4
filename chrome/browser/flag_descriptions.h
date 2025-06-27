988d3fc696534da1806833d8fbde6e3b0180506e
Norge Vizcay <vizcay@google.com>
2025-06-27 06:07:19 -0700

    [Wallet] Add AutofillEnableEmailOrLoyaltyCardsFilling to chrome://flags.
    Bug: 404436679
    Change-Id: I4f8301145a506c9be63122ed62db8458d8a36e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6682123
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1479764}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -503,6 +503,9 @@ extern const char kAutofillEnableCardInfoRuntimeRetrievalDescription[];
 extern const char kAutofillEnableDownstreamCardAwarenessIphName[];
 extern const char kAutofillEnableDownstreamCardAwarenessIphDescription[];
 
+extern const char kAutofillEnableEmailOrLoyaltyCardsFillingName[];
+extern const char kAutofillEnableEmailOrLoyaltyCardsFillingDescription[];
+
 extern const char kAutofillEnableFlatRateCardBenefitsFromCurinosName[];
 extern const char kAutofillEnableFlatRateCardBenefitsFromCurinosDescription[];
 
