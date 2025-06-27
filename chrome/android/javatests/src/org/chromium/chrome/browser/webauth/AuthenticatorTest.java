b89ece90a1a89e43ade540ed3b07c8ba3fe86ba6
Henrique Nakashima <hnakashima@chromium.org>
2025-06-27 09:16:02 -0700

    [Android] Migrate chrome_test_java_various to PT rules (part 18)
    Bug: 399877997
    Change-Id: Icf24b045576513b08430d399b879d54f1642575c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680818
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479879}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webauth/AuthenticatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webauth/AuthenticatorTest.java
@@ -25,7 +25,9 @@ import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.chrome.test.transit.ChromeTransitTestRules;
+import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
+import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.components.webauthn.AuthenticatorImpl;
 import org.chromium.components.webauthn.MockFido2CredentialRequest;
 import org.chromium.components.webauthn.WebauthnMode;
@@ -48,11 +50,13 @@ import org.chromium.ui.test.util.DeviceRestriction;
 @Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO)
 public class AuthenticatorTest {
     @Rule
-    public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
+    public FreshCtaTransitTestRule mActivityTestRule =
+            ChromeTransitTestRules.freshChromeTabbedActivityRule();
 
     private static final String TEST_FILE = "/content/test/data/android/authenticator.html";
     private EmbeddedTestServer mTestServer;
     private String mUrl;
+    private WebPageStation mPage;
     private Tab mTab;
     private AuthenticatorUpdateWaiter mUpdateWaiter;
     private MockFido2CredentialRequest mMockCredentialRequest;
@@ -84,13 +88,13 @@ public class AuthenticatorTest {
 
     @Before
     public void setUp() throws Exception {
-        mActivityTestRule.startMainActivityOnBlankPage();
+        mPage = mActivityTestRule.startOnBlankPage();
         mTestServer =
                 EmbeddedTestServer.createAndStartHTTPSServer(
                         InstrumentationRegistry.getInstrumentation().getContext(),
                         ServerCertificate.CERT_OK);
         mUrl = mTestServer.getURLWithHostName("subdomain.example.test", TEST_FILE);
-        mTab = mActivityTestRule.getActivity().getActivityTab();
+        mTab = mPage.getTab();
         mUpdateWaiter = new AuthenticatorUpdateWaiter();
         ThreadUtils.runOnUiThreadBlocking(() -> mTab.addObserver(mUpdateWaiter));
         WebauthnModeProvider.getInstance().setGlobalWebauthnMode(WebauthnMode.CHROME);
