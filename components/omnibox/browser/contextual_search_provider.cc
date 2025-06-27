8394304f1241413ff1222ea8f2707a6d7543e95b
manukh <manukh@chromium.org>
2025-06-27 11:52:54 -0700

    [omnibox][toolbelt] Show lens entry match if toolbelt doesn't have lens
    Today, lens entry match is hidden if the toolbelt is present, even if
    the toolbelt doesn't have a lens action.
    
    Before:
    1. if toolbelt is not added,                 lens entry match is shown
    2. if toolbelt is added without lens action, lens entry match is HIDDEN
    3. if toolbelt is added with    lens action, lens entry match is hidden
    
    With this CL:
    1. if toolbelt is not added,                 lens entry match is shown
    2. if toolbelt is added without lens action, lens entry match is SHOWN
    3. if toolbelt is added with    lens action, lens entry match is hidden
    
    This CL fixes case (2).
    
    Bug: 427369065, 425987352, 426652236
    Change-Id: Iaa8f5ddf6290300f20ac609d986fe07446f68afb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681408
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479983}

--- components/omnibox/browser/contextual_search_provider.cc
+++ components/omnibox/browser/contextual_search_provider.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/search/search.h"
 #include "components/search_engines/search_engine_type.h"
+#include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_starter_pack_data.h"
 #include "components/strings/grit/components_strings.h"
@@ -120,13 +121,13 @@ void ContextualSearchProvider::Start(
 
   const auto [input, starter_pack_engine] = AdjustInputForStarterPackKeyword(
       autocomplete_input, client()->GetTemplateURLService());
-  const bool toolbelted = MaybeAddToolbeltMatch(input, starter_pack_engine);
+  const bool toolbelted_with_lens =
+      MaybeAddToolbeltMatch(input, starter_pack_engine);
 
-  // Note, the dedicated entrypoint match is not added if the toolbelt is
-  // included because the toolbelt will already have an action to serve as the
-  // Lens entrypoint.
+  // Note, the dedicated entrypoint match is not added if the toolbelt with
+  // lens.
   if (omnibox_feature_configs::ContextualSearch::Get().show_open_lens_action &&
-      !toolbelted && IsLensEntrypointAvailable(input, client())) {
+      !toolbelted_with_lens && IsLensEntrypointAvailable(input, client())) {
     AddLensEntrypointMatch(input);
   }
 
@@ -434,12 +435,14 @@ bool ContextualSearchProvider::MaybeAddToolbeltMatch(
 
   // Lens is only allowed if the DSE is google, but that's already checked in
   // `client->IsLensEnabled()`. Lens is not restricted by locale.
+  bool lens_added = false;
   if (ActionEnabledForPageContext(input, config.show_lens_action_on_non_ntp,
                                   config.show_lens_action_on_ntp) &&
       (config.always_include_lens_action ||
        IsLensEntrypointAvailable(input, client()))) {
     match.actions.push_back(
         base::MakeRefCounted<ContextualSearchOpenLensAction>());
+    lens_added = true;
   }
 
   // Add the starter pack entry actions only if the given starter pack keyword
@@ -486,7 +489,7 @@ bool ContextualSearchProvider::MaybeAddToolbeltMatch(
         template_url_starter_pack_data::StarterPackId::kTabs);
   }
   matches_.push_back(match);
-  return true;
+  return lens_added;
 }
 
 const TemplateURL* ContextualSearchProvider::GetKeywordTemplateURL() const {
