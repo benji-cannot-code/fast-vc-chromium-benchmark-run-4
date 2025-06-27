4ed962bc8b8e2ff51485ba1d9ee523033060c7f3
Nico Weber <thakis@chromium.org>
2025-06-27 04:48:58 -0700

    Remove components/nacl/common/buildflags.h
    Bug: 40511454
    Change-Id: I7ed78333b31fe1900e820989438489e607a037e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680050
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479738}

--- components/nacl/browser/nacl_host_message_filter.cc
+++ components/nacl/browser/nacl_host_message_filter.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/nacl/browser/nacl_file_host.h"
 #include "components/nacl/browser/nacl_process_host.h"
 #include "components/nacl/browser/pnacl_host.h"
-#include "components/nacl/common/buildflags.h"
 #include "components/nacl/common/nacl_host_messages.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -97,40 +96,11 @@ void NaClHostMessageFilter::OnChannelClosing() {
 void NaClHostMessageFilter::OverrideThreadForMessage(
     const IPC::Message& message,
     content::BrowserThread::ID* thread) {
-#if BUILDFLAG(ENABLE_NACL)
-  if (message.type() == NaClHostMsg_LaunchNaCl::ID) {
-    *thread = content::BrowserThread::UI;
-  } else if (message.type() == NaClHostMsg_GetReadonlyPnaclFD::ID ||
-             message.type() == NaClHostMsg_NaClCreateTemporaryFile::ID ||
-             message.type() == NaClHostMsg_NexeTempFileRequest::ID ||
-             message.type() == NaClHostMsg_ReportTranslationFinished::ID) {
-    *thread = content::BrowserThread::UI;
-  }
-#endif
 }
 
 bool NaClHostMessageFilter::OnMessageReceived(const IPC::Message& message) {
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(NaClHostMessageFilter, message)
-#if BUILDFLAG(ENABLE_NACL)
-    IPC_MESSAGE_HANDLER_DELAY_REPLY(NaClHostMsg_LaunchNaCl, OnLaunchNaCl)
-    IPC_MESSAGE_HANDLER_DELAY_REPLY(NaClHostMsg_GetReadonlyPnaclFD,
-                                    OnGetReadonlyPnaclFd)
-    IPC_MESSAGE_HANDLER_DELAY_REPLY(NaClHostMsg_NaClCreateTemporaryFile,
-                                    OnNaClCreateTemporaryFile)
-    IPC_MESSAGE_HANDLER(NaClHostMsg_NexeTempFileRequest,
-                        OnGetNexeFd)
-    IPC_MESSAGE_HANDLER(NaClHostMsg_ReportTranslationFinished,
-                        OnTranslationFinished)
-    IPC_MESSAGE_HANDLER(NaClHostMsg_MissingArchError,
-                        OnMissingArchError)
-    IPC_MESSAGE_HANDLER_DELAY_REPLY(NaClHostMsg_OpenNaClExecutable,
-                                    OnOpenNaClExecutable)
-    IPC_MESSAGE_HANDLER(NaClHostMsg_NaClGetNumProcessors,
-                        OnNaClGetNumProcessors)
-    IPC_MESSAGE_HANDLER(NaClHostMsg_NaClDebugEnabledForURL,
-                        OnNaClDebugEnabledForURL)
-#endif
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
 
