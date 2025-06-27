bdf6357c1f82389ff5620224df5856f2b8cbce37
Kelvin Jiang <kelvinjiang@chromium.org>
2025-06-27 16:09:37 -0700

    [Extensions] Migrate reading list api tests to desktop Android
    This CL ports the API tests.
    
    Fixed: 425962655
    Change-Id: I248a774d15f3927a7ae6ea7a37197a019251b5fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6677932
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480139}

--- chrome/browser/extensions/api/reading_list/reading_list_apitest.cc
+++ chrome/browser/extensions/api/reading_list/reading_list_apitest.cc
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/reading_list/reading_list_model_factory.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/common/extensions/api/reading_list.h"
 #include "components/reading_list/core/reading_list_model.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/test_event_router_observer.h"
+#include "extensions/buildflags/buildflags.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -36,7 +38,7 @@ IN_PROC_BROWSER_TEST_F(ReadingListApiTest,
   // this observer will catch events for each.
   TestEventRouterObserver event_observer(EventRouter::Get(profile()));
 
-  // Add a Reading List entry in a normal browser.
+  // Add a Reading List entry in a normal profile.
   ReadingListModel* const reading_list_model =
       ReadingListModelFactory::GetForBrowserContext(profile());
   reading_list_model->AddOrReplaceEntry(
@@ -54,12 +56,12 @@ IN_PROC_BROWSER_TEST_F(ReadingListApiTest,
   ASSERT_FALSE(base::Contains(event_observer.events(),
                               api::reading_list::OnEntryAdded::kEventName));
 
-  const Browser* const incognito_browser = CreateIncognitoBrowser(profile());
+  Profile* incognito_profile =
+      profile()->GetPrimaryOTRProfile(/*create_if_needed=*/true);
 
-  // Add a Reading List entry in an incognito browser.
+  // Add a Reading List entry in an off-the-record profile.
   ReadingListModel* const incognito_reading_list_model =
-      ReadingListModelFactory::GetForBrowserContext(
-          incognito_browser->profile());
+      ReadingListModelFactory::GetForBrowserContext(incognito_profile);
   incognito_reading_list_model->AddOrReplaceEntry(
       GURL("https://www.example.com"), "example of title",
       reading_list::EntrySource::ADDED_VIA_CURRENT_APP, base::TimeDelta());
@@ -69,8 +71,7 @@ IN_PROC_BROWSER_TEST_F(ReadingListApiTest,
   Event* incognito_event = event_observer.events()
                                .at(api::reading_list::OnEntryAdded::kEventName)
                                .get();
-  EXPECT_EQ(incognito_event->restrict_to_browser_context,
-            incognito_browser->profile());
+  EXPECT_EQ(incognito_event->restrict_to_browser_context, incognito_profile);
 }
 
 }  // namespace
