42cfa5eb316599328258ac9e537bc036f0ba6e0a
Colin Blundell <blundell@chromium.org>
2025-06-26 23:25:25 -0700

    [//gpu] Remove friending in GpuMemoryBufferImplDXGI
    No longer needed following [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6668859
    
    Bug: 40263579
    Change-Id: I20a41f9b1d1a5ba2c45ab24bfbaa83aaa22e5759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6678571
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479647}

--- gpu/ipc/common/gpu_memory_buffer_impl_dxgi.h
+++ gpu/ipc/common/gpu_memory_buffer_impl_dxgi.h
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/gpu_memory_buffer.h"
 
-namespace media {
-class GpuMemoryBufferTrackerWin;
-}
-
 namespace gpu {
 
 class GpuMemoryBufferSupport;
@@ -78,8 +74,6 @@ class GPU_IPC_COMMON_EXPORT GpuMemoryBufferImplDXGI
   const gfx::DXGIHandleToken& GetToken() const;
 
  private:
-  // TODO(crbug.com/40264379): Remove the need for this.
-  friend media::GpuMemoryBufferTrackerWin;
   friend GpuMemoryBufferSupport;
 
   static std::unique_ptr<GpuMemoryBufferImplDXGI> CreateFromHandle(
