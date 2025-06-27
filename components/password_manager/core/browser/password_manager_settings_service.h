47da9f08c4fc0d0cb6415effe0c4f256a9174e0c
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-06-27 08:45:52 -0700

    Fix auto reauthn broken outside of Android
    Broke in crrev.com/c/6657168. Before that CL,
    FederatedIdentityAutoReauthnPermissionContext would get a reference to
    PasswordManagerSettingsService on-demand (wrong pattern for keyed
    service dependencies). After the CL, PasswordManagerSettingsService
    injects itself as a cyclic dependency, but this was only done in the
    Android code path.
    
    This CL fixes the issue by hooking the dependency on all platforms.
    It also adds a new null check for
    FederatedIdentityAutoReauthnPermissionContext to be on the safe side.
    
    Fixed: 428112191
    Change-Id: Ia6c9b2a7c7365cddd23a4a56bec0e0083f4df2de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680317
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479856}

--- components/password_manager/core/browser/password_manager_settings_service.h
+++ components/password_manager/core/browser/password_manager_settings_service.h
@@ -13,6 +13,8 @@ namespace password_manager {
 // Service used to access the password manager settings.
 class PasswordManagerSettingsService : public KeyedService {
  public:
+  ~PasswordManagerSettingsService() override = default;
+
   // Checks if `setting` is enabled. It ensures that the correct pref is checked
   // on Android, which depends on the unified password manager status.
   virtual bool IsSettingEnabled(
@@ -24,9 +26,6 @@ class PasswordManagerSettingsService : public KeyedService {
   // Sets the auto sign in setting to off. Used by the auto sign in first run
   // dialog.
   virtual void TurnOffAutoSignIn() = 0;
-
- protected:
-  ~PasswordManagerSettingsService() override = default;
 };
 
 }  // namespace password_manager
