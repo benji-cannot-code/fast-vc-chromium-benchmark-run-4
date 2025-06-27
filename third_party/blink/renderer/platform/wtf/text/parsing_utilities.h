064e3028d0215f9c5b96ee20c15a33509ae622d4
Suyeon Ji <zeesuyeon@gmail.com>
2025-06-26 18:41:19 -0700

    Switch wtf/text/parsing_utilities.h to "blink" namespace
    This CL has no behavior changes.
    
    Bug: 422768753
    Change-Id: I010b9afadab4eb4787f4288a3a632a323b127608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680207
    Reviewed-by: Jinho Bang <zino@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479574}

--- third_party/blink/renderer/platform/wtf/text/parsing_utilities.h
+++ third_party/blink/renderer/platform/wtf/text/parsing_utilities.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
 
-namespace WTF {
+namespace blink {
 
 template <typename CharType>
 bool SkipExactly(base::span<const CharType> chars,
@@ -128,12 +128,6 @@ template <typename CharType, bool predicate(CharType)>
   return position;
 }
 
-}  // namespace WTF
-
-using WTF::SkipExactly;
-using WTF::SkipToken;
-using WTF::SkipUntil;
-using WTF::SkipWhile;
-using WTF::ReverseSkipWhile;
+}  // namespace blink
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_TEXT_PARSING_UTILITIES_H_
