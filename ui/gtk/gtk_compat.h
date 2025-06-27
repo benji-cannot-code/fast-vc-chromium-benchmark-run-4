c9cfc09b697d1de70ec7263a57784e7b99ef7b4c
Tom Anderson <thomasanderson@chromium.org>
2025-06-27 16:39:33 -0700

    [GTK] Revert back to GTK3 on GNOME+X11
    IMEs are still immature on GTK4 as we've received reports using numerous
    IMEs (fcitx, ibus, scim, xim) and locales (PT, CN, JP, etc). Many
    issues stem from a packaging bug on Ubuntu 22.04 and earlier where GTK3
    modules would be installed, but not GTK4. Other issues are bugs in the
    IME implementation, and have been addressed in later releases. Because
    of the difficulty in managing so many configurations, this CL simply
    reverts back to GTK3 for GNOME+X11. Wayland doesn't load the GTK IM
    modules, so that configuration doesn't have this issue.
    
    R=thestig
    
    Bug: 408449463, 421194904
    Change-Id: I261ea5f106cf9ef07065d70647c98a8a6f4de30e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681508
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480151}

--- ui/gtk/gtk_compat.h
+++ ui/gtk/gtk_compat.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/glib/scoped_gobject.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gtk/gtk_types.h"
+#include "ui/linux/linux_ui_delegate.h"
 
 #if GTK_CHECK_VERSION(4, 1, 1)
 #define UI_GTK_CONST const
@@ -43,7 +44,7 @@ using SkColor = uint32_t;
 namespace gtk {
 
 // Loads libgtk and related libraries and returns true on success.
-bool LoadGtk();
+bool LoadGtk(ui::LinuxUiBackend backend);
 
 const base::Version& GtkVersion();
 
