cb38e038b654c4f6cce10654328e8bfbb1232263
Henrique Nakashima <hnakashima@chromium.org>
2025-06-26 18:59:44 -0700

    [Android] Migrate chrome_test_java_various to PT rules (part 14)
    Bug: 399877997
    Change-Id: Ide97ac25f4dea2c0f38a331e33ed118d2454ff46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680852
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479585}

--- chrome/android/javatests/src/org/chromium/chrome/browser/storage/BlobUrlStoreTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/storage/BlobUrlStoreTest.java
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.storage;
 
+import static org.junit.Assert.assertTrue;
+
 import android.content.Context;
 
 import androidx.test.filters.SmallTest;
 
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -18,10 +19,11 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
-import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.TestContentProvider;
+import org.chromium.chrome.test.transit.ChromeTransitTestRules;
+import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
+import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 
 import java.io.File;
@@ -32,7 +34,8 @@ import java.io.FileWriter;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class BlobUrlStoreTest {
     @Rule
-    public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
+    public FreshCtaTransitTestRule mActivityTestRule =
+            ChromeTransitTestRules.freshChromeTabbedActivityRule();
 
     @Rule public TemporaryFolder mFolder = new TemporaryFolder();
 
@@ -59,10 +62,15 @@ public class BlobUrlStoreTest {
         writer.close();
         TestContentProvider.resetResourceRequestCounts(context);
         TestContentProvider.setDataFilePath(context, mFolder.getRoot().getPath());
-        mActivityTestRule.startMainActivityWithURL(
-                TestContentProvider.createContentUrl("blob.html"));
-        Tab tab = mActivityTestRule.getActivity().getActivityTab();
-        Assert.assertEquals("A blob", ChromeTabUtils.getTitleOnUiThread(tab));
-        Assert.assertTrue(ChromeTabUtils.getUrlStringOnUiThread(tab).startsWith("blob:null/"));
+        WebPageStation page =
+                mActivityTestRule
+                        .startOnUrlTo(TestContentProvider.createContentUrl("blob.html"))
+                        .arriveAt(
+                                WebPageStation.newBuilder()
+                                        .withEntryPoint()
+                                        .withExpectedTitle("A blob")
+                                        .withExpectedUrlSubstring("blob:null/")
+                                        .build());
+        assertTrue(ChromeTabUtils.getUrlStringOnUiThread(page.getTab()).startsWith("blob:null/"));
     }
 }
