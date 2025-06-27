15ed4998931f43f7b764009ebf8356a88efa058e
Nicolas Dossou-Gbété <dgn@chromium.org>
2025-06-27 03:20:11 -0700

    waffle: Record eligibility through the service
    This will allow internal service state to be used in follow-ups to tweak
    the recorded histogram.
    
    Change-Id: Ie871e49735b7ed977b77e48d14c2cf8d9962d43c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6658618
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479718}

--- chrome/browser/ui/search_engine_choice/search_engine_choice_tab_helper.cc
+++ chrome/browser/ui/search_engine_choice/search_engine_choice_tab_helper.cc
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_dialog_service.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_dialog_service_factory.h"
+#include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "components/search_engines/search_engine_choice/search_engine_choice_service.h"
 #include "components/search_engines/search_engine_choice/search_engine_choice_utils.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
@@ -94,7 +96,11 @@ void SearchEngineChoiceTabHelper::MaybeShowDialog() {
 
   search_engines::SearchEngineChoiceScreenConditions conditions =
       search_engine_choice_dialog_service->ComputeDialogConditions(*browser);
-  search_engines::RecordChoiceScreenNavigationCondition(conditions);
+
+  search_engines::SearchEngineChoiceService* search_engine_choice_service =
+      search_engines::SearchEngineChoiceServiceFactory::GetForProfile(
+          browser->profile());
+  search_engine_choice_service->RecordDynamicEligibility(conditions);
 
   if (conditions !=
       search_engines::SearchEngineChoiceScreenConditions::kEligible) {
