176456b972fef17525d6e74ac98e6230b469766a
Colin Blundell <blundell@chromium.org>
2025-06-26 23:28:02 -0700

    [//cc] Remove LOW_RES from TilingSetRasterQueueAll
    Everything referring to this is dead code.
    
    Bug: 418234930
    Change-Id: If865f9752a1e452289b20c6e292c26cdcf609db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6678974
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479652}

--- cc/tiles/tiling_set_raster_queue_all.h
+++ cc/tiles/tiling_set_raster_queue_all.h
@@ -24,7 +24,7 @@ namespace cc {
 class PictureLayerTilingSet;
 
 // This queue returns all tiles required to be rasterized from HIGH_RESOLUTION
-// and LOW_RESOLUTION tilings.
+// tilings.
 class CC_EXPORT TilingSetRasterQueueAll {
  public:
   static std::unique_ptr<TilingSetRasterQueueAll> Create(
@@ -193,7 +193,6 @@ class CC_EXPORT TilingSetRasterQueueAll {
   };
 
   enum IteratorType {
-    LOW_RES,
     HIGH_RES,
     ACTIVE_NON_IDEAL_PENDING_HIGH_RES,
     NUM_ITERATORS
@@ -201,7 +200,6 @@ class CC_EXPORT TilingSetRasterQueueAll {
 
   TilingSetRasterQueueAll(
       PictureLayerTiling* high_res_tiling,
-      PictureLayerTiling* low_res_tiling,
       PictureLayerTiling* active_non_ideal_pending_high_res_tiling,
       bool is_drawing_layer);
 
