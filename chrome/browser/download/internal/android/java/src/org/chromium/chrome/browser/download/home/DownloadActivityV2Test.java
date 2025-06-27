5d299b32772cc93af33471ab2c107be94f714b79
Peter Pakkenberg <pbirk@chromium.org>
2025-06-27 03:28:49 -0700

    Gardener: Disable testDeleteDangerousUsing* on Auto
    Bug: 428127167
    Change-Id: Id5f3b296c642fb855adacad92dbdce82e92d8206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681332
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Owners-Override: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479719}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/DownloadActivityV2Test.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/DownloadActivityV2Test.java
@@ -40,6 +40,8 @@ import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.filters.MediumTest;
 
+import org.chromium.base.test.util.Restriction;
+import org.chromium.ui.test.util.DeviceRestriction;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.junit.Assert;
@@ -483,7 +485,7 @@ public class DownloadActivityV2Test {
 
     @Test
     @MediumTest
-    @DisabledTest(message="crbug.com/428127167")
+    @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO}) // crbug.com/428127167
     public void testDeleteDangerousUsingMenu() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -505,6 +507,7 @@ public class DownloadActivityV2Test {
 
     @Test
     @MediumTest
+    @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO}) // crbug.com/428127167
     public void testDeleteDangerousUsingSelection() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
