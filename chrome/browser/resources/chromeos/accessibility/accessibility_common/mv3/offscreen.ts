fd14a98e8a3f5a3574ad0e09e8824d343744c790
Xiyuan Xia <xiyuan@chromium.org>
2025-06-27 11:21:59 -0700

    [Accessibility Common] Share message processing
    Ensure only one chrome.runtime.onMessage listener exists in
    the offscreen document. Route incoming messages to appropriate handlers
    to maintain modular logic.
    
    In Chrome extension messaging, only the first registered onMessage
    listener is invoked. If it returns false or nothing and doesn’t call
    sendResponse synchronously, Chrome treats the message as handled and
    closes the port. This affects both synchronous and asynchronous
    messages, and can lead to the error: "The message port closed before a
    response was received." To avoid dropped responses and ensure consistent
    behavior, message handling should be centralized.
    
    Changes on top of crrev.com/c/6563668:
    - Move message handling into `Messenger` so that service worker side
      could share message processing too.
    
    AX-Relnotes: n/a.
    Bug: 388867838
    Change-Id: I1f2146a307e331be74693e37948f83029dd75124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680169
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479963}

---
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/mv3/offscreen.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {OffscreenAudio} from './dictation/offscreen_audio.js';
+import {OffscreenPumpkinWorker} from './dictation/offscreen_pumpkin_worker.js';
+import {Messenger} from './messenger.js';
+
+/**
+ * Offscreen to manage all offscreen processors in the extension.
+ */
+class Offscreen {
+  static instance?: Offscreen;
+
+  private offscreens_: Set<Object> = new Set();
+
+  constructor() {
+    this.offscreens_.add(new OffscreenAudio());
+    this.offscreens_.add(new OffscreenPumpkinWorker());
+  }
+
+  static init(): void {
+    if (Offscreen.instance) {
+      throw 'Error: trying to create two instances of singleton Offscreen.';
+    }
+    Messenger.init();
+    Offscreen.instance = new Offscreen();
+  }
+}
+
+Offscreen.init();
