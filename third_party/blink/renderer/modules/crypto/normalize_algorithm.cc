44d60b23e9dee6333dad7a9701f8d55048843684
Javier Fernández García-Boente <jfernandez@igalia.com>
2025-06-28 06:48:17 -0700

    Remove the WebCryptoEd25519 runtime flag
    The WebCryptoEd25519 flag has been set to stable since M137, so I think
    we can remove the flag now.
    
    Bug: 40241089
    Change-Id: Ib47fa802bee8bac0fc4334b9055e8df70bdbc8b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6686186
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480219}

--- third_party/blink/renderer/modules/crypto/normalize_algorithm.cc
+++ third_party/blink/renderer/modules/crypto/normalize_algorithm.cc
@@ -201,12 +201,6 @@ std::optional<WebCryptoAlgorithmId> LookupAlgorithmIdByName(
     return std::nullopt;
 
   WebCryptoAlgorithmId id = it->algorithm_id;
-
-  if (!RuntimeEnabledFeatures::WebCryptoEd25519Enabled()) {
-    if (id == kWebCryptoAlgorithmIdEd25519) {
-      return std::nullopt;
-    }
-  }
   return id;
 }
 
