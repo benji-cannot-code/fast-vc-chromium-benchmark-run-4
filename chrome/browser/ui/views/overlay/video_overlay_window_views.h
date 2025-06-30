ffb6145870a8b3013d8f5fb4d2aa11daa7fdb9cd
Tommy Steimel <steimel@chromium.org>
2025-06-30 10:30:57 -0700

    [video pip] Add fade in/out animation to controls visibility changes
    This CL adds a fade animation when the video pip controls change
    visibility.
    
    Bug: 360357715
    Change-Id: I3f0a152ea7d4f6756981a7a6b04c05529ab8fee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6675864
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480604}

--- chrome/browser/ui/views/overlay/video_overlay_window_views.h
+++ chrome/browser/ui/views/overlay/video_overlay_window_views.h
@@ -39,6 +39,7 @@ class FrameSinkId;
 class BackToTabLabelButton;
 class CloseImageButton;
 class HangUpButton;
+class OverlayControlsFadeAnimation;
 class OverlayWindowBackToTabButton;
 class OverlayWindowLiveCaptionDialog;
 class OverlayWindowMinimizeButton;
@@ -163,7 +164,9 @@ class VideoOverlayWindowViews : public content::VideoOverlayWindow,
   // Updates the controls view::Views to reflect |is_visible|. If the window is
   // currently in motion, the update is queued until the end of motion. If
   // multiple updates are requested, only the last update will be applied.
-  void UpdateControlsVisibility(bool is_visible);
+  // When `should_animate` is true, there will be fade animation to the new
+  // state.
+  void UpdateControlsVisibility(bool is_visible, bool should_animate = true);
 
   // Gets the bounds of the controls.
   gfx::Rect GetBackToTabControlsBounds();
@@ -477,6 +480,9 @@ class VideoOverlayWindowViews : public content::VideoOverlayWindow,
   // Used to tuck/untuck this widget into the side of the screen.
   std::unique_ptr<PictureInPictureTucker> tucker_;
   bool is_tucking_forced_ = false;
+
+  // Used for when the controls change visibility.
+  std::unique_ptr<OverlayControlsFadeAnimation> fade_animation_;
 
   // Callback to get / create an overlay view.  This is a callback to let tests
   // provide alternate implementations.
