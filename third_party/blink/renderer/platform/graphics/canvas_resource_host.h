9e37bec2ddfd1b113fec789a77f4b20431fdfdad
Colin Blundell <blundell@chromium.org>
2025-06-30 01:03:19 -0700

    [Blink] Move methods down from CanvasResourceHost
    These are the final methods not needed on CanvasResourceHost.
    
    Bug: 352263194
    Change-Id: Ibb10895ef6078b3967c3fd05191a6111457d9b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6683634
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480362}

--- third_party/blink/renderer/platform/graphics/canvas_resource_host.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_host.h
@@ -22,11 +22,7 @@ class PLATFORM_EXPORT CanvasResourceHost {
 
   virtual void NotifyGpuContextLost() = 0;
   virtual void InitializeForRecording(cc::PaintCanvas* canvas) const = 0;
-
   virtual bool IsPrinting() const { return false; }
-  virtual bool IsHibernating() const { return false; }
-
-  virtual void SetTransferToGPUTextureWasInvoked() {}
   virtual bool TransferToGPUTextureWasInvoked() { return false; }
 };
 
