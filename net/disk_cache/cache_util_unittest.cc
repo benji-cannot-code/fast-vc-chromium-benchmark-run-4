e5a6ecdad93b16df53a5bd63f4f341fd60e2802e
Olivier Li <olivierli@chromium.org>
2025-06-30 11:59:22 -0700

    Repurpose ChangeDiskCacheSize experiment.
    Originally the experiment was introduce to evaluate changes to HTTP disk
    cache size. This evaluation was completed and the default size
    multiplicators chosen in go/disk-cache-size-results-2024.
    
    Changing the behavior of the feature to affect the generated code
    cache lets us do the same process again.
    
    Bug: 356624787,428703077
    Change-Id: I0c87e6c386820a82cd2d72f52f255bb3770da29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685067
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480665}

--- net/disk_cache/cache_util_unittest.cc
+++ net/disk_cache/cache_util_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "base/threading/platform_thread.h"
 #include "build/build_config.h"
+#include "net/base/cache_type.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
@@ -255,13 +256,14 @@ TEST_F(CacheUtilTest, PreferredCacheSize) {
             PreferredCacheSize(50000 * kMiBInBytes,
                                net::GENERATED_NATIVE_CODE_CACHE));
 
-  for (int cache_size_exeriment : {100, 200, 250, 300}) {
+  for (int cache_size_exeriment : {100, 200, 250, 300, 400}) {
     base::test::ScopedFeatureList scoped_feature_list;
     std::map<std::string, std::string> field_trial_params;
     field_trial_params["percent_relative_size"] =
         base::NumberToString(cache_size_exeriment);
     scoped_feature_list.InitAndEnableFeatureWithParameters(
-        disk_cache::kChangeDiskCacheSizeExperiment, field_trial_params);
+        disk_cache::kChangeGeneratedCodeCacheSizeExperiment,
+        field_trial_params);
 
     for (const auto& test_case : kTestCases) {
       int expected = 0;
@@ -278,16 +280,23 @@ TEST_F(CacheUtilTest, PreferredCacheSize) {
         case 300:
           expected = test_case.expected_300percent;
           break;
+        case 400:
+          expected = test_case.expected_400percent;
+          break;
         default:
           NOTREACHED();
       }
 
-      EXPECT_EQ(expected, PreferredCacheSize(test_case.available));
+      // Generate byte code cache size is appropriately scaled.
+      EXPECT_EQ(expected, PreferredCacheSize(test_case.available,
+                                             net::GENERATED_BYTE_CODE_CACHE));
 
-      // For caches other than disk cache, the size is not scaled.
-      EXPECT_EQ(test_case.expected_100percent,
-                PreferredCacheSize(test_case.available,
-                                   net::GENERATED_BYTE_CODE_CACHE));
+      // For caches other than generated code cache, the size is not scaled
+      // through `kChangeGeneratedCodeCacheSizeExperiment`.
+      int64_t default_expected = kHTTPCacheSizeIsIncreased
+                                     ? test_case.expected_400percent
+                                     : test_case.expected_100percent;
+      EXPECT_EQ(default_expected, PreferredCacheSize(test_case.available));
 
       // Preferred size for WebUI code cache is not scaled by the trial, and
       // should never be more than 5 MB.
@@ -310,10 +319,11 @@ TEST_F(CacheUtilTest, PreferredCacheSize) {
   {
     base::test::ScopedFeatureList scoped_feature_list;
     scoped_feature_list.InitAndEnableFeature(
-        disk_cache::kChangeDiskCacheSizeExperiment);
+        disk_cache::kChangeGeneratedCodeCacheSizeExperiment);
     for (const auto& test_case : kTestCases) {
       EXPECT_EQ(test_case.expected_400percent,
-                PreferredCacheSize(test_case.available));
+                PreferredCacheSize(test_case.available,
+                                   net::GENERATED_BYTE_CODE_CACHE));
     }
     // Check that the cache size cap is 50% higher for native code caches.
     EXPECT_EQ(((320 * kMiBInBytes) / 2) * 3,
