313b9d275812f17fdc87569b898605bd573c94d2
Adam Rice <ricea@chromium.org>
2025-06-30 03:30:43 -0700

    Add CHECKs for crashes in CompressionStream
    blink::CompressionStream has unexplained crashes. Add CHECKs to diagnose
    them.
    
    BUG=427166012
    
    Change-Id: Ic8f883c9c47ef5ab87212ec9faf1d99130bad661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6688646
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480423}

--- third_party/blink/renderer/modules/compression/compression_stream.cc
+++ third_party/blink/renderer/modules/compression/compression_stream.cc
@@ -20,10 +20,12 @@ CompressionStream* CompressionStream::Create(ScriptState* script_state,
 }
 
 ReadableStream* CompressionStream::readable() const {
+  CHECK(initialized_);
   return transform_->Readable();
 }
 
 WritableStream* CompressionStream::writable() const {
+  CHECK(initialized_);
   return transform_->Writable();
 }
 
@@ -51,6 +53,8 @@ CompressionStream::CompressionStream(ScriptState* script_state,
                               MakeGarbageCollected<DeflateTransformer>(
                                   script_state, deflate_format, deflate_level),
                               exception_state);
+  CHECK(transform_);
+  initialized_ = true;
 }
 
 }  // namespace blink
