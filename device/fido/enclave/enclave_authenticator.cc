bc8f727363db1e4e783a18b12b59717fea1ae181
Ambika Mohapatra <ambikaraj@google.com>
2025-06-30 14:43:09 -0700

    webauthn: MakeCredential-side implementation of large blob for Google Password Manager.
    Bug: 426192000
    Change-Id: Ie510a922b194631a100a11530caf60bb447e9cf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6682938
    Auto-Submit: Ambika Mohapatra <ambikaraj@google.com>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480753}

--- device/fido/enclave/enclave_authenticator.cc
+++ device/fido/enclave/enclave_authenticator.cc
@@ -19,11 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/device_event_log/device_event_log.h"
 #include "components/sync/protocol/webauthn_credential_specifics.pb.h"
 #include "crypto/random.h"
+#include "device/fido/authenticator_get_assertion_response.h"
+#include "device/fido/authenticator_make_credential_response.h"
 #include "device/fido/discoverable_credential_metadata.h"
 #include "device/fido/enclave/constants.h"
 #include "device/fido/enclave/metrics.h"
 #include "device/fido/enclave/types.h"
 #include "device/fido/fido_parsing_utils.h"
+#include "device/fido/large_blob.h"
 #include "device/fido/public_key_credential_descriptor.h"
 
 namespace device::enclave {
@@ -76,6 +79,7 @@ AuthenticatorSupportedOptions EnclaveAuthenticatorOptions() {
   options.supports_resident_key = true;
   options.user_verification_availability = AuthenticatorSupportedOptions::
       UserVerificationAvailability::kSupportedAndConfigured;
+  options.large_blob_type = LargeBlobSupportType::kBespoke;
   options.supports_user_presence = false;
   return options;
 }
