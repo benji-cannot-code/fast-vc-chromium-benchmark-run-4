f682c68823430985d45ae5b33736bc4729a638bf
Peilin Wang <peilinwang@google.com>
2025-06-30 13:06:08 -0700

    [BCIV] Add unit tests for visibility constraints changing
    Bug: 399466053
    Change-Id: I1467bf32b35a6e216c49298212ca41ec5fbcbab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667563
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480697}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabBrowserControlsConstraintsHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabBrowserControlsConstraintsHelper.java
@@ -217,7 +217,6 @@ public class TabBrowserControlsConstraintsHelper implements UserData {
         update(BrowserControlsState.BOTH, getConstraints() != BrowserControlsState.HIDDEN);
     }
 
-    /** Unregister all OffsetTags (for now, only the top controls have an OffsetTag.) */
     private void unregisterOffsetTags() {
         updateOffsetTags(new BrowserControlsOffsetTagsInfo(null, null, null), getConstraints());
     }
