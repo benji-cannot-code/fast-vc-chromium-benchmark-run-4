d98a0a24761a85dbabc45fa00f50630597a261ec
mikt <mikt@google.com>
2025-06-30 01:37:10 -0700

    [E-LUD] Fix compile failure on raw_ptr's NoOpImpl
    It is broken on mac-arm-no-brp-builder-perf.
    https://ci.chromium.org/ui/p/chrome/builders/ci/mac-arm-no-brp-builder-perf/740/overview
    
    Change-Id: I13c9b81171624648a53d68a0fd9d656db24706c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6670859
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Sergei Glazunov <glazunov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480374}

--- components/gwp_asan/client/extreme_lightweight_detector_quarantine.cc
+++ components/gwp_asan/client/extreme_lightweight_detector_quarantine.cc
@@ -10,6 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gwp_asan::internal {
 
+// Making this non-trivial dtor to allow use of `base::NoDestructor`.
+// This explicit dtor is not needed in most build configurations because
+// `raw_ptr<T>` has a non-trivial dtor. However, `raw_ptr<T>` does not guarantee
+// it and we want to avoid the code here getting affected by `raw_ptr<T>`'s
+// internal implementation.
+// TODO(yukishiino): Make this trivially destructible.
+// NOLINTNEXTLINE(modernize-use-equals-default)
+ExtremeLightweightDetectorQuarantineRoot::
+    ~ExtremeLightweightDetectorQuarantineRoot() {}
+
 ExtremeLightweightDetectorQuarantineBranch
 ExtremeLightweightDetectorQuarantineRoot::CreateBranch(
     const ExtremeLightweightDetectorQuarantineBranchConfig& config) {
