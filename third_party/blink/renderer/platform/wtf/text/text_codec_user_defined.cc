3186fe60f613c1906d7ad8e76326b529ba5b5efa
Jason <cgqaq@chromium.org>
2025-06-30 03:04:12 -0700

    Remove allow_unsafe_buffers in text_codec_user_defined.cc
    Bug: 351564777
    Change-Id: I050752c899c45ea0de2ff9eef3d1aefa8bf3af19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652576
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1480412}

--- third_party/blink/renderer/platform/wtf/text/text_codec_user_defined.cc
+++ third_party/blink/renderer/platform/wtf/text/text_codec_user_defined.cc
@@ -24,11 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/wtf/text/text_codec_user_defined.h"
 
 #include <memory>
@@ -72,7 +67,6 @@ static std::string EncodeComplexUserDefined(
     base::span<const CharType> char_data,
     UnencodableHandling handling) {
   DCHECK_NE(handling, UnencodableHandling::kNoUnencodables);
-  const auto* characters = char_data.data();
   const wtf_size_t length = base::checked_cast<wtf_size_t>(char_data.size());
   wtf_size_t target_length = length;
   std::string result;
@@ -81,7 +75,7 @@ static std::string EncodeComplexUserDefined(
   for (wtf_size_t i = 0; i < length;) {
     UChar32 c;
     // TODO(jsbell): Will the input for x-user-defined ever be LChars?
-    U16_NEXT(characters, i, length, c);
+    U16_NEXT(char_data, i, length, c);
     // If the input was a surrogate pair (non-BMP character) then we
     // overestimated the length.
     if (c > 0xffff)
