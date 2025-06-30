90b2d4be8ac67ab7dcf178e435c8af7b969f8e32
James Lee <ljjlee@google.com>
2025-06-30 05:25:53 -0700

    Hardcode kSearchEngineChoiceMaximumSkipCount
    This no longer needs to be server-configured, which will then allow for
    kSearchEngineChoiceTrigger feature flag to be removed.
    
    Bug: 424428814
    Change-Id: Ie534ab11abeb0cac7994d973a446948ff98d8d6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6679297
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480448}

--- ios/chrome/browser/search_engine_choice/model/search_engine_choice_util_unittest.mm
+++ ios/chrome/browser/search_engine_choice/model/search_engine_choice_util_unittest.mm
@@ -95,11 +95,11 @@ TEST_F(SearchEngineChoiceUtilTest,
 TEST_F(
     SearchEngineChoiceUtilTest,
     ShowChoiceScreenIfPoliciesAreNotSetStartedByExternalIntentAlmostMaxCount) {
-  ASSERT_GT(switches::kSearchEngineChoiceMaximumSkipCount.Get(), 0);
+  ASSERT_GT(kSearchEngineChoiceMaximumSkipCount, 0);
   PrefService* pref_service = profile().GetPrefs();
   pref_service->SetInteger(
       prefs::kDefaultSearchProviderChoiceScreenSkippedCount,
-      switches::kSearchEngineChoiceMaximumSkipCount.Get() - 1);
+      kSearchEngineChoiceMaximumSkipCount - 1);
 
   EXPECT_FALSE(ShouldDisplaySearchEngineChoiceScreen(
       profile(), /*is_first_run_entrypoint=*/false,
@@ -117,7 +117,7 @@ TEST_F(
   PrefService* pref_service = profile().GetPrefs();
   pref_service->SetInteger(
       prefs::kDefaultSearchProviderChoiceScreenSkippedCount,
-      switches::kSearchEngineChoiceMaximumSkipCount.Get());
+      kSearchEngineChoiceMaximumSkipCount);
 
   EXPECT_TRUE(ShouldDisplaySearchEngineChoiceScreen(
       profile(), /*is_first_run_entrypoint=*/false,
