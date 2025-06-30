af59c6a900ce070e990f6fac8d53957b55030ef3
Colin Blundell <blundell@chromium.org>
2025-06-30 02:09:30 -0700

    [Blink] Streamline WebGLRenderingContextBase::PushFrame()
    This method can be called only by OffscreenCanvas [1], for which
    LowLatencyEnabled() always returns false [2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc;l=652;drc=e65323be17a0ba06198d18b0a8bf5ae594e10348
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h;l=112?q=CanvasRenderingContextHost::Low&sq=&ss=chromium
    
    Bug: 352263194
    Change-Id: Ia74de86a8fdc0cee35a86fe7238429a79780faaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680315
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480392}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1652,11 +1652,8 @@ bool WebGLRenderingContextBase::PushFrame() {
   if (!must_paint_to_canvas_ && !must_clear_now)
     return false;
 
-  if (!Host()->LowLatencyEnabled() &&
-      GetDrawingBuffer()->IsUsingGpuCompositing()) {
-    // If LowLatency is not enabled, and it's using Gpu Compositing, it will try
-    // to export the mailbox, synctoken and callback mechanism for the
-    // compositor to present the frame in the offscrencanvas.
+  if (GetDrawingBuffer()->IsUsingGpuCompositing()) {
+    // Export the DrawingBuffer's SI directly if possible.
     if (PushFrameNoCopy())
       return true;
   }
@@ -1690,19 +1687,13 @@ bool WebGLRenderingContextBase::PushFrameWithCopy() {
   scoped_refptr<CanvasResource> resource = nullptr;
   bool produced_frame = false;
 
-  if (CanUseDrawingBufferSIWithoutCopyForLowLatency()) {
-    resource = ExportLowLatencyCanvasResource(kBackBuffer,
-                                              /*export_only_if_update=*/true);
-    produced_frame = !!resource;
-  } else {
-    bool resource_provider_was_updated = false;
-    auto* resource_provider = PaintRenderingResultsToCanvas(
-        kBackBuffer, &resource_provider_was_updated);
-    produced_frame = resource_provider && resource_provider_was_updated;
-    if (produced_frame) {
-      resource =
-          resource_provider->ProduceCanvasResource(FlushReason::kNon2DCanvas);
-    }
+  bool resource_provider_was_updated = false;
+  auto* resource_provider = PaintRenderingResultsToCanvas(
+      kBackBuffer, &resource_provider_was_updated);
+  produced_frame = resource_provider && resource_provider_was_updated;
+  if (produced_frame) {
+    resource =
+        resource_provider->ProduceCanvasResource(FlushReason::kNon2DCanvas);
   }
 
   if (produced_frame) {
