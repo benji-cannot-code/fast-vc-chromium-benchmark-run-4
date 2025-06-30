4cd0e37268447db76a208610c716ff495132f17c
Fredrik Söderquist <fs@opera.com>
2025-06-30 09:29:45 -0700

    Fix TextDecorationInfo::ComputeUnderlineThickness() with font override
    With a font override in effect we could still end using the font data
    from the "decorating box style". For SVG text this will be wrong because
    it's using a font with additional scaling applied.
    
    Only use the "decorating box style" if the use_decorating_box_ flag is
    set to true.
    
    Guarded by the "SvgTextCentralBaselineTextDecorationFix" flag.
    
    Fixed: 389845192
    Change-Id: I1cfbab6a63fe0117fa8960d4bf20df41ba020477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6678196
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480559}

--- third_party/blink/renderer/core/paint/text_decoration_info.cc
+++ third_party/blink/renderer/core/paint/text_decoration_info.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/text_paint_style.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/geometry/length_functions.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -455,7 +456,9 @@ float TextDecorationInfo::ComputeUnderlineThickness(
     // decorating box.
     // Only for non-Roman for now for the performance implications.
     // https:// drafts.csswg.org/css-text-decor-3/#decorating-box
-    if (decorating_box_style) {
+    if (RuntimeEnabledFeatures::SvgTextCentralBaselineTextDecorationFixEnabled()
+            ? use_decorating_box_
+            : !!decorating_box_style) {
       thickness = ComputeDecorationThickness(
           applied_decoration_thickness,
           decorating_box_style->ComputedFontSize(),
