161099f0c203f221374d859bcf00645e42ceafe1
Noah Rose Ledesma <noahrose@google.com>
2025-06-30 12:38:01 -0700

    Fix CECA uninstallation in O4 tests
    In some O4 tests the UninstallBrokenEnterpriseCompanionApp helper is
    used despite the test using a non-broken version of the companion app.
    Omit this instruction and allow the test fixture tear down to use the
    typical uninstall flow.
    
    Change-Id: I9054671e4856118fde6f820c5b36f6cb065e5a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6690958
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480679}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -2732,7 +2732,6 @@ TEST_F(IntegrationTestDeviceManagement,
   ASSERT_NO_FATAL_FAILURE(InstallAppViaService(kApp1.appid));
   ASSERT_NO_FATAL_FAILURE(ExpectRegistered(kApp1.appid));
   ASSERT_NO_FATAL_FAILURE(ExpectUninstallPing(test_server_.get()));
-  ASSERT_NO_FATAL_FAILURE(UninstallBrokenEnterpriseCompanionApp());
   ASSERT_NO_FATAL_FAILURE(Uninstall());
 }
 
@@ -2765,7 +2764,6 @@ TEST_F(IntegrationTestDeviceManagement, PolicyFetchFailedButAppUpdatedAnyway) {
 
   ASSERT_NO_FATAL_FAILURE(ExpectAppVersion(kApp1.appid, kApp1.v2));
   ASSERT_NO_FATAL_FAILURE(ExpectUninstallPing(test_server_.get()));
-  ASSERT_NO_FATAL_FAILURE(UninstallBrokenEnterpriseCompanionApp());
   ASSERT_NO_FATAL_FAILURE(Uninstall());
 }
 
