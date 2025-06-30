29e98a4dc6c1f54c5978ba20b8186475425ac551
Aishwarya Rajesh <aishwaryarj@google.com>
2025-06-30 15:23:14 -0700

    [UW] Make positive button on instance switcher filled
    Before: https://screenshot.googleplex.com/9bfBiZJfex7jASc
    After: https://screenshot.googleplex.com/9Ebnuqhat8rvr38
    
    Bug: 427872354
    Change-Id: Icd3d76cf8ed437812eaad252152e9e113c51ada7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6689135
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480768}

--- chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherCoordinator.java
+++ chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherCoordinator.java
@@ -47,6 +47,7 @@ import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogManager.ModalDialogType;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
+import org.chromium.ui.modaldialog.ModalDialogProperties.ButtonStyles;
 import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.ModelListAdapter;
@@ -308,7 +309,10 @@ public class InstanceSwitcherCoordinator {
                                 R.string.cancel)
                         .with(
                                 ModalDialogProperties.DIALOG_STYLES,
-                                ModalDialogProperties.DialogStyles.DIALOG_WHEN_LARGE);
+                                ModalDialogProperties.DialogStyles.DIALOG_WHEN_LARGE)
+                        .with(
+                                ModalDialogProperties.BUTTON_STYLES,
+                                ButtonStyles.PRIMARY_FILLED_NEGATIVE_OUTLINE);
 
         if (UiUtils.isInstanceSwitcherV2Enabled()) {
             builder.with(
