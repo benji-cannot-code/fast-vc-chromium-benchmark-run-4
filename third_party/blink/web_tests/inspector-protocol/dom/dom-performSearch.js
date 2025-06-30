83dd574c5a710ca7987b82787137a37768c0b3e4
Wolfgang Beyer <wolfi@chromium.org>
2025-06-30 04:04:05 -0700

    [DevTools] Let DOM.performSearch search pierce shadow DOM when searching for selector
    Bug: 422019184
    Change-Id: I547ec660c4df3852335e54daa01dd321a6e214d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6678958
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480430}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-performSearch.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {dp} = await testRunner.startURL('resources/dom-perform-search.html', 'Tests DOM.performSearch command.');
+  await dp.DOM.getDocument();
+
+  let performSearchResult = await dp.DOM.performSearch({query: '.find-me'});
+  let nodesResponse = await dp.DOM.getSearchResults({searchId: performSearchResult.result.searchId, fromIndex: 0, toIndex: 4});
+
+  testRunner.log("Nodes:");
+
+  for (const nodeId of nodesResponse.result.nodeIds) {
+    const nodeResponse = await dp.DOM.describeNode({ nodeId });
+    testRunner.log(nodeResponse);
+  }
+
+  performSearchResult = await dp.DOM.performSearch({query: 'html'});
+  nodesResponse = await dp.DOM.getSearchResults({searchId: performSearchResult.result.searchId, fromIndex: 0, toIndex: 2});
+
+  for (const nodeId of nodesResponse.result.nodeIds) {
+    const nodeResponse = await dp.DOM.describeNode({ nodeId });
+    testRunner.log(nodeResponse);
+  }
+  testRunner.completeTest();
+})
