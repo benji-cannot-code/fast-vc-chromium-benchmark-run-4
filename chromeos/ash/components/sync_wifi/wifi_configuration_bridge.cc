2c647d2c6d3da4919a7f7020b5c9f6017381c6a8
Mahmoud Rashad <mmrashad@google.com>
2025-06-30 13:23:02 -0700

    [CodeHealth] Clean up WifiSyncAllowDeletes
    Fixed: 356235002
    Change-Id: Ibfb7eb73fd191296ca6e05e843624520e0be5f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6677091
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480710}

--- chromeos/ash/components/sync_wifi/wifi_configuration_bridge.cc
+++ chromeos/ash/components/sync_wifi/wifi_configuration_bridge.cc
@@ -559,10 +559,6 @@ void WifiConfigurationBridge::OnNetworkConfiguredDelayComplete(
 void WifiConfigurationBridge::OnBeforeConfigurationRemoved(
     const std::string& service_path,
     const std::string& guid) {
-  if (!base::FeatureList::IsEnabled(features::kWifiSyncAllowDeletes)) {
-    return;
-  }
-
   std::optional<NetworkIdentifier> id =
       local_network_collector_->GetNetworkIdentifierFromGuid(guid);
   if (!id) {
@@ -577,10 +573,6 @@ void WifiConfigurationBridge::OnBeforeConfigurationRemoved(
 void WifiConfigurationBridge::OnConfigurationRemoved(
     const std::string& service_path,
     const std::string& network_guid) {
-  if (!base::FeatureList::IsEnabled(features::kWifiSyncAllowDeletes)) {
-    return;
-  }
-
   if (!pending_deletes_.contains(network_guid)) {
     NET_LOG(EVENT) << "Configuration " << network_guid
                    << " removed with no matching saved metadata.";
@@ -598,9 +590,6 @@ void WifiConfigurationBridge::OnConfigurationRemoved(
 
 void WifiConfigurationBridge::RemoveNetworkFromSync(
     const std::string& storage_key) {
-  if (!base::FeatureList::IsEnabled(features::kWifiSyncAllowDeletes)) {
-    return;
-  }
   if (!entries_.contains(storage_key)) {
     return;  // Network is not synced.
   }
