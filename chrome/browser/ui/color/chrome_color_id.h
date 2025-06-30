82c5d6eb1a11e508362e1d9f22681b8ae6fa6e1d
Riley Tatum <rtatum@google.com>
2025-06-30 16:47:21 -0700

    [composebox] Set colors for upload buttons
    Screenshots:
    http://screenshot/A69jmb9zBuepcfe
    http://screenshot/BqmFD2x55ib23Xy
    
    NO_IFTTT=It is in both files
    
    Bug: 422561574
    Change-Id: Ie11381101d693e737a6a4a4a54bdf3af76e759a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6691533
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480801}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -267,6 +267,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageComposeboxCancelButton) \
   E_CPONLY(kColorNewTabPageComposeboxHover) \
   E_CPONLY(kColorNewTabPageComposeboxSubmitButton) \
+  E_CPONLY(kColorNewTabPageComposeboxUploadButton) \
   E_CPONLY(kColorNewTabPageControlBackgroundHovered) \
   E_CPONLY(kColorNewTabPageControlBackgroundSelected) \
   E_CPONLY(kColorNewTabPageDialogBackground) \
