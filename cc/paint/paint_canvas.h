a0e8863af2ba4e5b4c1f9982dec2c8a89a00acf6
Sam Fortiner <samfort@microsoft.com>
2025-06-30 14:25:01 -0700

    Fix dangling PaintPreviewTracker ptr in PaintCanvas
    In CapturePaintPreviewInternal(), a PaintPreviewTracker is created and
    set on the canvas via SetPaintPreviewTracker(). Later, the tracker is
    moved to FinishRecordingOnUIThread() via std::move(tracker). From there
    it may get further moved to SerializeFileRecording or
    SerializeMemoryBufferRecording (and eventually to BuildAndSendResponse)
    where it'll get released when that function returns. But, the canvas
    still holds a raw_ptr to the now-moved and released tracker object.
    
    The fix clears the tracker pointer from the canvas before moving the
    tracker object to prevent the dangling pointer condition. With this
    change we can remove DanglingUntriaged from PaintCanvas::tracker_.
    
    Bug: 40212619
    Change-Id: I63bf079ee26d95ce1a3a0a6e082a329659c884ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681455
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sam Fortiner <samfort@microsoft.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480740}

--- cc/paint/paint_canvas.h
+++ cc/paint/paint_canvas.h
@@ -259,8 +259,7 @@ class CC_PAINT_EXPORT PaintCanvas {
 
  private:
   raw_ptr<printing::MetafileSkia> metafile_ = nullptr;
-  raw_ptr<paint_preview::PaintPreviewTracker, DanglingUntriaged> tracker_ =
-      nullptr;
+  raw_ptr<paint_preview::PaintPreviewTracker> tracker_ = nullptr;
 };
 
 class CC_PAINT_EXPORT PaintCanvasAutoRestore {
