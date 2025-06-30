ad2fd2236e01fd35f09cd9cae1114275f32539ef
Akihiro Ota <akihiroota@chromium.org>
2025-06-30 11:12:40 -0700

    ChromeVox: Use SRE in manifest v3
    This CL imports SRE into manifest v3 ChromeVox. We mostly follow the
    pattern established by the mv2 code in https://crrev.com/c/6534810,
    but with the key difference of calling SRE from the offscreen
    document. For context, we cannot run SRE in the service worker
    context because the dependency contains various web API calls that
    are not allowed in a service worker. As a result, we also need to wire
    up the messaging between the service worker and the offscreen
    document to handle the calls to SRE.move() and SRE.walk().
    
    Currently ChromeVox mv3 tests are not enabled in CQ. However, I have
    uploaded a downstream change [1] that enables and runs the mv3
    tests in CQ, which show no failures for any of the math-related tests.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6684988
    
    Bug: 388867840
    Change-Id: I3f540e483b5fb1f08109c30ea99877bd3849c071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681310
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480628}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/background/math_handler.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/background/math_handler.ts
@@ -8,17 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import {AutomationPredicate} from '/common/automation_predicate.js';
 import type {CursorRange} from '/common/cursors/range.js';
+import {TestImportManager} from '/common/testing/test_import_manager.js';
 
 import type {InternalKeyEvent} from '../common/internal_key_event.js'
 import {Msgs} from '../common/msgs.js';
+import {OffscreenCommandType} from '../common/offscreen_command_type.js';
 import {QueueMode} from '../common/tts_types.js';
 
 import {ChromeVox} from './chromevox.js';
 
 import AutomationNode = chrome.automation.AutomationNode;
-
-// Speech Rule Engine is included as a global variable in background.html.
-declare let SRE: any;
 
 /**
  * Handles specialized code to navigate, announce, and interact with math
@@ -35,9 +34,9 @@ export class MathHandler {
 
   /**
    * Speaks the current node.
-   * @return Boolean indicating whether any math was spoken.
+   * @return Promise indicating whether any math was spoken.
    */
-  speak(): boolean {
+  async speak(): Promise<boolean> {
     let mathml = this.node_.mathContent;
     if (!mathml) {
       return false;
@@ -47,13 +46,9 @@ export class MathHandler {
       mathml = '<math>' + mathml + '</math>';
     }
 
-    let text: string|null = null;
-
-    try {
-      text = SRE.walk(mathml);
-    } catch (e) {
-      // Swallow exceptions from third party library.
-    }
+    const text = await chrome.runtime.sendMessage(
+        /*extensionId=*/ undefined,
+        /*message=*/ {command: OffscreenCommandType.SRE_WALK, mathml});
 
     if (!text) {
       return false;
@@ -82,7 +77,7 @@ export class MathHandler {
    * Handles key events.
    * @return Boolean indicating whether an event should propagate.
    */
-  static onKeyDown(evt: InternalKeyEvent): boolean {
+  static async onKeyDown(evt: InternalKeyEvent): Promise<boolean> {
     if (!MathHandler.instance) {
       return true;
     }
@@ -92,10 +87,18 @@ export class MathHandler {
       return true;
     }
 
-    const output: string = SRE.move(evt.keyCode);
+    const output = await chrome.runtime.sendMessage(
+        /*extensionId=*/ undefined,
+        /*message=*/ {
+          command: OffscreenCommandType.SRE_MOVE,
+          keyCode: evt.keyCode
+        });
+
     if (output) {
       ChromeVox.tts.speak(output, QueueMode.FLUSH);
     }
     return false;
   }
 }
+
+TestImportManager.exportForTesting(MathHandler);
--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/offscreen/offscreen.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/offscreen/offscreen.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {SRE} from '/chromevox/mv3/third_party/sre/sre_browser.js';
+
 import {EarconEngine} from '../background/earcon_engine.js';
 import {BackgroundBridge} from '../common/background_bridge.js';
 import {InternalKeyEvent} from '../common/internal_key_event.js';
@@ -54,6 +56,12 @@ class OffscreenMessageHandler {
         break;
       case OffscreenCommandType.LEARN_MODE_REMOVE_LISTENERS:
         OffscreenLearnModeKeyboardHandler.instance!.removeListeners();
+        break;
+      case OffscreenCommandType.SRE_MOVE:
+        OffscreenMathHandler.instance!.sreMove(sendResponse, message.keyCode);
+        break;
+      case OffscreenCommandType.SRE_WALK:
+        OffscreenMathHandler.instance!.sreWalk(sendResponse, message.mathml);
         break;
       case OffscreenCommandType.ON_CLIPBOARD_DATA_CHANGED:
         const forceRead = message.forceRead as boolean;
@@ -303,11 +311,32 @@ class OffscreenBrailleDisplayManager {
   }
 }
 
+class OffscreenMathHandler {
+  static instance?: OffscreenMathHandler;
+
+  static init(): void {
+    if (OffscreenMathHandler.instance) {
+      throw 'Error: trying to create two instances of singleton ' +
+          'OffscreenMathHandler.';
+    }
+    OffscreenMathHandler.instance = new OffscreenMathHandler();
+  }
+
+  sreMove(sendResponse: SendResponse, keyCode: number): void {
+    sendResponse(SRE.move(keyCode));
+  }
+
+  sreWalk(sendResponse: SendResponse, mathml: string): void {
+    sendResponse(SRE.walk(mathml));
+  }
+}
+
 OffscreenBackgroundKeyboardHandler.init();
 OffscreenLearnModeKeyboardHandler.init();
 OffscreenClipboardHandler.init();
 OffscreenSpeechSynthesis.init();
 OffscreenBrailleDisplayManager.init();
+OffscreenMathHandler.init();
 EarconEngine.init();
 LibLouisWorker.init();
 
