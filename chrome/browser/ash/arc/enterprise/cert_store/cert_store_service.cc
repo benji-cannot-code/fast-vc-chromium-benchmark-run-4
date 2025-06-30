8544ea6c86f58ffd4d79f166917baab80a7f7ad0
Elly <ellyjones@chromium.org>
2025-06-30 08:31:40 -0700

    cros platform keys: use crypto::evp parsing functions
    The crypto::evp parsing functions don't require the caller to construct
    a CBS themselves and are a little more robust to errors.
    
    In the process, I noticed that PublicKeyInfo::public_key_spki_der being
    a std::string meant that all the places using it had to convert to/from
    std::vector<uint8_t>. Since most of the crypto APIs want to deal in byte
    spans or byte vectors rather than strings anyway, I changed the type of
    that field in PublicKeyInfo, then removed the variant of
    GetSubjectPublicKeyInfo() that returns a std::string since it became
    unused.
    
    Bug: 372283556
    Change-Id: Idd7c9bb89193d620db4c38855744e1d4b27fdee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6674534
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Michael Slutskii <slutskii@google.com>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480523}

--- chrome/browser/ash/arc/enterprise/cert_store/cert_store_service.cc
+++ chrome/browser/ash/arc/enterprise/cert_store/cert_store_service.cc
@@ -228,7 +228,7 @@ void IsCertificateAllowed(IsCertificateAllowedCallback callback,
   ash::platform_keys::KeyPermissionsServiceFactory::GetForBrowserContext(
       context)
       ->IsCorporateKey(
-          chromeos::platform_keys::GetSubjectPublicKeyInfoBlob(cert),
+          chromeos::platform_keys::GetSubjectPublicKeyInfo(cert),
           base::BindOnce(&CheckCorporateFlag, std::move(callback)));
 }
 
