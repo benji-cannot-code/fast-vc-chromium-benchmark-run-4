aff5edb59db930a697c12fec93aabd532b868de2
Eric Seckler <eseckler@chromium.org>
2025-06-30 06:14:57 -0700

    TracingControllerAndroid: Run buffer usage callback on UI thread
    Tentative fix for DCHECK violations in weak_ptr dereferences around
    TracingControllerAndroid, whose methods should only run on the UI
    thread.
    
    Also adds some thread checkers around TCA.
    
    Bug: 424641444
    Change-Id: I2f7cb6b7c0de86a76118d39e71c76e4fc2fd4cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685951
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480471}

--- content/browser/android/tracing_controller_android.cc
+++ content/browser/android/tracing_controller_android.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/trace_event/trace_event.h"
 #include "content/browser/tracing/tracing_controller_impl.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/tracing_controller.h"
 #include "services/tracing/public/cpp/perfetto/perfetto_config.h"
 #include "services/tracing/public/cpp/perfetto/perfetto_session.h"
@@ -95,6 +96,7 @@ bool TracingControllerAndroid::StartTracing(
     const JavaParamRef<jstring>& jcategories,
     const JavaParamRef<jstring>& jtraceoptions,
     bool use_protobuf) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   std::string categories =
       base::android::ConvertJavaStringToUTF8(env, jcategories);
   std::string options =
@@ -123,6 +125,7 @@ void TracingControllerAndroid::StopTracing(
     bool compress_file,
     bool use_protobuf,
     const base::android::JavaParamRef<jobject>& callback) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   base::FilePath file_path(
       base::android::ConvertJavaStringToUTF8(env, jfilepath));
   ScopedJavaGlobalRef<jobject> global_callback(env, callback);
@@ -170,6 +173,7 @@ void TracingControllerAndroid::StopTracing(
 
 base::FilePath TracingControllerAndroid::GenerateTracingFilePath(
     const std::string& basename) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   JNIEnv* env = base::android::AttachCurrentThread();
   ScopedJavaLocalRef<jstring> jfilename =
       Java_TracingControllerAndroidImpl_generateTracingFilePath(
@@ -180,6 +184,7 @@ base::FilePath TracingControllerAndroid::GenerateTracingFilePath(
 
 void TracingControllerAndroid::OnTracingStopped(
     const base::android::ScopedJavaGlobalRef<jobject>& callback) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   JNIEnv* env = base::android::AttachCurrentThread();
   base::android::ScopedJavaLocalRef<jobject> obj = weak_java_object_.get(env);
   if (obj.obj())
@@ -190,6 +195,7 @@ bool TracingControllerAndroid::GetKnownCategoriesAsync(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& callback) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   ScopedJavaGlobalRef<jobject> global_callback(env, callback);
   // TODO(skyostil): Get the categories from Perfetto instead.
   return TracingController::GetInstance()->GetCategories(
@@ -200,6 +206,7 @@ bool TracingControllerAndroid::GetKnownCategoriesAsync(
 void TracingControllerAndroid::OnKnownCategoriesReceived(
     const ScopedJavaGlobalRef<jobject>& callback,
     const std::set<std::string>& categories_received) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   base::Value::List category_list;
   for (const std::string& category : categories_received)
     category_list.Append(category);
@@ -237,6 +244,7 @@ bool TracingControllerAndroid::GetTraceBufferUsageAsync(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& callback) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   ScopedJavaGlobalRef<jobject> global_callback(env, callback);
   auto weak_callback =
       base::BindOnce(&TracingControllerAndroid::OnTraceBufferUsageReceived,
@@ -265,8 +273,10 @@ bool TracingControllerAndroid::GetTraceBufferUsageAsync(
         }
         // TODO(skyostil): Remove approximate_event_count since no-one is using
         // it.
-        std::move(shared_callback->data)
-            .Run(percent_full, /*approximate_event_count=*/0);
+        GetUIThreadTaskRunner({})->PostTask(
+            FROM_HERE,
+            base::BindOnce(std::move(shared_callback->data), percent_full,
+                           /*approximate_event_count=*/0));
       });
   return true;
 }
@@ -275,6 +285,7 @@ void TracingControllerAndroid::OnTraceBufferUsageReceived(
     const ScopedJavaGlobalRef<jobject>& callback,
     float percent_full,
     size_t approximate_event_count) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   JNIEnv* env = base::android::AttachCurrentThread();
   base::android::ScopedJavaLocalRef<jobject> obj = weak_java_object_.get(env);
   if (obj.obj()) {
