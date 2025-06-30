59eb4b0445b7f5461ba08dbd8a96632c2535da09
David Sanders <dsanders11@ucsbalum.com>
2025-06-30 16:04:23 -0700

    Remove unused include in dmserver_job_configurations.h
    Adds missing includes as needed to fix the build.
    
    Bug: 40318405
    Change-Id: Iafa2393c85d57f638c1ee705f8721a834eafb267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6690727
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1480787}

--- components/policy/core/common/cloud/dmserver_job_configurations.h
+++ components/policy/core/common/cloud/dmserver_job_configurations.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/device_management_service.h"
 #include "components/policy/core/common/cloud/dm_auth.h"
 #include "components/policy/policy_export.h"
-#include "components/policy/proto/cloud_policy.pb.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "url/gurl.h"
 
