cd7d1b3b17a8a1ac43606cdeb7ecf3868f5b1f6a
Maggie Chen <magchen@chromium.org>
2025-06-30 08:38:54 -0700

    Disable DrDC for MacOS version <= 12
    Bug:407874799, 427774385
    
    Change-Id: I766e3914e1c4885eea111b354e922e3c8043ed58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6688466
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480525}

--- gpu/config/gpu_finch_features.cc
+++ gpu/config/gpu_finch_features.cc
@@ -720,6 +720,9 @@ bool IsDrDcEnabled() {
   if (!IsSkiaGraphiteEnabled(base::CommandLine::ForCurrentProcess())) {
     return false;
   }
+  if (base::mac::MacOSVersion() <= 12) {
+    return false;
+  }
 #endif
 
   return base::FeatureList::IsEnabled(kEnableDrDc);
