9f9a4c93b0a39bfbf7bfb2c2b452847e4ade56f4
Colin Blundell <blundell@chromium.org>
2025-06-30 01:37:38 -0700

    [//ui] Remove GpuMemoryBuffer::GetId()
    Nothing uses this now.
    
    I'll look at removing `id_` ivars in followup; it looks like at least
    some of them are still used (e.g., for CloneHandle()).
    
    Bug: 404905709
    Change-Id: I43d2ec5d90bb890d80165e30d274c01324d613a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680417
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480375}

--- gpu/command_buffer/client/fake_gpu_memory_buffer.h
+++ gpu/command_buffer/client/fake_gpu_memory_buffer.h
@@ -64,7 +64,6 @@ class FakeGpuMemoryBuffer : public gfx::GpuMemoryBuffer {
   gfx::Size GetSize() const override;
   gfx::BufferFormat GetFormat() const override;
   int stride(size_t plane) const override;
-  gfx::GpuMemoryBufferId GetId() const override;
   gfx::GpuMemoryBufferType GetType() const override;
   gfx::GpuMemoryBufferHandle CloneHandle() const override;
 
--- gpu/command_buffer/client/test_gpu_memory_buffer_manager.cc
+++ gpu/command_buffer/client/test_gpu_memory_buffer_manager.cc
@@ -73,7 +73,6 @@ class GpuMemoryBufferImpl : public gfx::GpuMemoryBuffer {
   gfx::GpuMemoryBufferType GetType() const override {
     return gfx::SHARED_MEMORY_BUFFER;
   }
-  gfx::GpuMemoryBufferId GetId() const override { return id_; }
   gfx::GpuMemoryBufferHandle CloneHandle() const override {
     gfx::GpuMemoryBufferHandle handle(region_.Duplicate());
     handle.offset = base::checked_cast<uint32_t>(offset_);
@@ -109,7 +108,6 @@ class GpuMemoryBufferFromClient : public gfx::GpuMemoryBuffer {
   int stride(size_t plane) const override {
     return client_buffer_->stride(plane);
   }
-  gfx::GpuMemoryBufferId GetId() const override { return id_; }
   gfx::GpuMemoryBufferType GetType() const override {
     return client_buffer_->GetType();
   }
