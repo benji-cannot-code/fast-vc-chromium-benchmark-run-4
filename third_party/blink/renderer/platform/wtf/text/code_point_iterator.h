44108bbbe064954710771c52c19eca62a0778f67
Jason <cgqaq@chromium.org>
2025-06-30 07:25:18 -0700

    Remove allow_unsafe_buffers in string_impl.{h,cc}
    Bug: 351564777
    Change-Id: I059639e3856138e827ce33c53157e58db10a1e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6657439
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1480502}

--- third_party/blink/renderer/platform/wtf/text/code_point_iterator.h
+++ third_party/blink/renderer/platform/wtf/text/code_point_iterator.h
@@ -90,7 +90,9 @@ class CodePointIterator {
   // Create a `begin()` iterator.
   template <class T>
   explicit CodePointIterator(const T& string)
-      : CodePointIterator(string.Is8Bit(), string.Bytes(), string.length()) {}
+      : CodePointIterator(string.Is8Bit(),
+                          string.RawByteSpan().data(),
+                          string.length()) {}
 
   // Create an `end()` iterator.
   template <class T>
--- third_party/blink/renderer/platform/wtf/text/string_statics.cc
+++ third_party/blink/renderer/platform/wtf/text/string_statics.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/containers/span.h"
 #include "third_party/blink/renderer/platform/wtf/dynamic_annotations.h"
 #include "third_party/blink/renderer/platform/wtf/static_constructors.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
