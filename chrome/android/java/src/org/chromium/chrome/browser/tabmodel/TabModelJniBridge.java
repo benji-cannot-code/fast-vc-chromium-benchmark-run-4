853c8643d66577a1cdb7f09cac22e75081229670
Dan Polanco <polardz@google.com>
2025-06-30 12:31:59 -0700

    [Tabs] Implement MoveGroupTo in Tab Model
    Adds implementation to TabListInterface.
    
    Bug: 415351293
    Change-Id: I99a75d48a47e1687b7f9f5a19a53831452064c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680491
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480676}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelJniBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelJniBridge.java
@@ -434,6 +434,10 @@ public abstract class TabModelJniBridge implements TabModelInternal {
     protected abstract void moveTabToIndex(int index, int newIndex);
 
     @CalledByNative
+    protected abstract void moveGroupToIndex(
+            @JniType("base::Token") Token tabGroupId, int newIndex);
+
+    @CalledByNative
     protected abstract @JniType("std::vector<TabAndroid*>") List<Tab> getAllTabs();
 
     @CalledByNative
