8c1ba1c08e7387e71336813084d02a8dfd099bae
Louis Romero <lpromero@google.com>
2025-07-01 02:10:56 -0700

    Add unit test to BrowserCoordinator
    This CL adds a unit test to prevent a regression on crbug.com/425992726.
    
    Fixed: 428109792
    Change-Id: I24487c249505546f40c74650d0098a79e04060f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6688927
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480927}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator+Testing.h
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator+Testing.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_BROWSER_VIEW_UI_BUNDLED_BROWSER_COORDINATOR_TESTING_H_
 
 #import "ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.h"
+#import "ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_controller.h"
 #import "ios/chrome/browser/save_to_photos/ui_bundled/save_to_photos_coordinator.h"
 #import "ios/chrome/browser/shared/public/commands/activity_service_commands.h"
 
@@ -14,7 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Coordinator for BrowserViewController. Testing category to expose properties
 // used for tests only.
-@interface BrowserCoordinator (Testing) <ActivityServiceCommands>
+@interface BrowserCoordinator (Testing) <ActivityServiceCommands,
+                                         OverscrollActionsControllerDelegate>
 
 // The coordinator used for the New Tab Page.
 @property(nonatomic, strong, readonly) NewTabPageCoordinator* NTPCoordinator;
