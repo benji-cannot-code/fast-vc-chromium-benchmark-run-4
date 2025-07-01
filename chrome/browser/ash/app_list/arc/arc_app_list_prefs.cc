15953100a0585f0cb1c259a958e9a2eb54bd7f05
Jun Ishiguro <junis@google.com>
2025-06-30 23:17:20 -0700

    Remove c/b/image_decoder dependency from c/b/ash/app_list
    This CL removes the dependency and directly uses
    data_decoder::DecodeImage() instead.
    
    Bug: b:428654878
    Change-Id: I31932ae8b2997ade1924491bbf0c62a0acbfcc58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6688369
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480872}

--- chrome/browser/ash/app_list/arc/arc_app_list_prefs.cc
+++ chrome/browser/ash/app_list/arc/arc_app_list_prefs.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/session/arc_initial_optin_metrics_recorder_factory.h"
 #include "chrome/browser/ash/arc/session/arc_session_manager.h"
 #include "chrome/browser/ash/login/session/user_session_manager.h"
-#include "chrome/browser/image_decoder/image_decoder.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
