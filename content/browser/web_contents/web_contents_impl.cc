bfbeb7590e8f27aac9b9713057dddd1be256286b
Greg Thompson <grt@chromium.org>
2025-07-01 07:29:20 -0700

    [A11yPerformance] Remove AXNode::AllChildIterator
    Replace all uses of it with range-based for loops over a node's
    children, which is drastically more efficient.
    
    AX-Relnotes: n/a.
    Bug: 40919326
    Change-Id: Ice194734cadc401a9e831495d3e94c4c30cd58b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694890
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481037}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -9443,9 +9443,8 @@ void WebContentsImpl::RecursivelyConstructAXTree(
     std::vector<ui::AXNodeData>& nodes) {
   nodes.push_back(node->data());
 
-  for (auto iter = node->AllChildrenBegin(); iter != node->AllChildrenEnd();
-       ++iter) {
-    RecursivelyConstructAXTree(&(*iter), nodes);
+  for (ui::AXNode* child : node->GetAllChildren()) {
+    RecursivelyConstructAXTree(child, nodes);
   }
 }
 
