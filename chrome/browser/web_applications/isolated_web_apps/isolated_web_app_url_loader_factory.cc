cbce882b2fcb1c8d204b6d77637bbe38578e9ab3
Mahmoud Rashad <mmrashad@google.com>
2025-07-01 07:13:11 -0700

    Revert "[iwa]: Notify user if IWA opens multiple windows without user gesture"
    This reverts commit 8e458818b642d5b432bb7728388f0e8eae51c7ed.
    
    Reason for revert: Some of MultipleWebContentsOpenedServiceBrowserTest tests are consistently failing on bunch of builders, e.g. "chromium/ci/Linux ChromiumOS MSan Tests", "chromium/ci/Linux MSan Tests", "chromium/ci/Linux ASan LSan Tests (1)" http://crbug.com/428917141
    
    Bug: 400677358, 428917141
    Original change's description:
    > [iwa]: Notify user if IWA opens multiple windows without user gesture
    >
    > This CL implements a system notification for Isolated Web Apps (IWAs) in
    > unmanaged (BYOD) scenarios when they programmatically open multiple
    > windows without an explicit user gesture.
    >
    > Previously, `window.open()` calls generally consumed a user gesture,
    > limiting apps to opening one new window at a time without further
    > interaction. To enable an app to open multiple windows programmatically
    > (e.g., in bulk or from automated processes), explicit user permission
    > via the Pop-ups and Redirects Content Setting was required. While this
    > could be defaulted for listed URLs via policy in managed enterprise
    > environments, it led to a poor user experience for IWAs in unmanaged
    > scenarios (e.g., during auto-launch or session restoration).
    >
    > This change ensures IWAs can seamlessly open multiple new windows by
    > automatically setting the Pop-ups and Redirects Content Setting to
    > "allow" upon installation, including for unmanaged apps. To mitigate
    > concerns about unexpected window creation, a system-level notification
    > is introduced. This notification informs the user about the creation of
    > multiple windows by an IWA and provides an action button to update
    > content settings if needed.
    >
    > This feature is implemented via a new keyed service that observes all
    > tab strips and windows - `MultipleWebContentsOpenedService`. This
    > approach was chosen because it allows to accurately track the complete
    > lifecycle of all child windows and tabs—from creation to closure—which
    > is essential for maintaining a correct count of opened web contents in
    > the notification, even when tabs are moved. This modular design also
    > establishes clear code ownership for the IWA team, simplifying
    > maintenance and ensuring the feature is robust against future changes in
    > the browser's windowing system.
    >
    > This is the first part of the implementation. The next ones will
    > include:
    > - `Close windows` button to close all of the windows that have
    > been opened by the app;
    > - Showing the notification for the IWA no more than N times; or
    > until the user acknowledges it
    >
    > Examples of the notification:
    > https://screenshot.googleplex.com/A3Vehvx6KchEDWG Context:
    > go/window-open-without-gesture-for-iwas
    >
    > Reason for skipping tsan test validation: crbug.com/405912587
    >
    > Bug: 400677358
    > Change-Id: I42686c3fb0b21ec11a01b5c9d8d903cc8c5a4068
    > Validate-Test-Flakiness: skip
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449966
    > Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    > Reviewed-by: Ryan Sultanem <rsult@google.com>
    > Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1480948}
    
    Bug: 400677358, 428917141
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I7911e1d79b81d865bcf662de910f72d4d7b697cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695014
    Owners-Override: Mahmoud Rashad <mmrashad@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481029}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_loader_factory.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_loader_factory.cc
@@ -645,7 +645,6 @@ void IsolatedWebAppURLLoaderFactory::CreateLoaderAndStart(
                      LogErrorAndFail(std::move(error),
                                      std::move(loader_client));
                    });
-
   auto location = IwaSourceWithMode::FromStorageLocation(
       profile_->GetPath(), iwa.isolation_data()->location());
 
