b5fb0f688638a2f6a5ea41c65dfaad4db8f586e7
Shin Kawamura <kawasin@google.com>
2025-07-01 05:32:32 -0700

    Default the number of max-deferred-bindings to 10
    This is to match the number in
    testing/variations/fieldtrial_testing_config.json
    
    Bug: 416153046
    Change-Id: Ib3283242c01be7e0e3643246bf7b693b2a6eae17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6693019
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480988}

--- base/android/java/src/org/chromium/base/BaseFeatureList.java
+++ base/android/java/src/org/chromium/base/BaseFeatureList.java
@@ -25,5 +25,5 @@ public class BaseFeatureList {
                     false);
 
     public static final MutableIntParamWithSafeDefault sMaxDeferredSharedRebindServiceConnection =
-            sUseSharedRebindServiceConnection.newIntParam("max-deferred-bindings", 0);
+            sUseSharedRebindServiceConnection.newIntParam("max-deferred-bindings", 10);
 }
