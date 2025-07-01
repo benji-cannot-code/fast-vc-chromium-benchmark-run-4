183adc03542a485375894fc0a4f0d3e25cdecedd
David Sanders <dsanders11@ucsbalum.com>
2025-07-01 02:08:52 -0700

    Remove unused include in iwa_key_distribution_info_provider.h
    Bug: 40318405
    Change-Id: I1c226dfc0368c584dcd72d0fe7e4a3afc1746284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6691990
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480924}

--- components/webapps/isolated_web_apps/iwa_key_distribution_info_provider.h
+++ components/webapps/isolated_web_apps/iwa_key_distribution_info_provider.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "base/version.h"
 #include "components/webapps/isolated_web_apps/iwa_key_distribution_histograms.h"
-#include "components/webapps/isolated_web_apps/proto/key_distribution.pb.h"
 
 namespace base {
 class FilePath;
