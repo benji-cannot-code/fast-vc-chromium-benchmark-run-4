590b1de721157d1a15e663884cf1c5ecf1f1cf45
Haiyang Pan <hypan@google.com>
2025-07-01 09:24:55 -0700

    Enable PRE tests for android_browsertests
    As well as the suites like android_sync_integration_tests. These test
    suites use ChromeBrowserTestsActivity.java on the java side to launch
    AndroidBrowserTest on the c++ side.
    
    This CL explicitly passes a "--user-data-dir" to c++ test launcher so
    that the latter one won't create a temp data dir during
    AndroidBrowserTest::SetUp.
    
    By doing so, we are able to clean the data dir on demand (for PRE tests)
    on the java side, without touching/changing the c++ test runner.
    
    Also patch https://crrev.com/c/6652520 which adds explicit tests
    AndroidBrowserTest::StatePreserved to verify state are preserved
    between PRE_ stages.
    
    Set "Validate-Test-Flakiness: skip" as it does not work well with
    PRE tests.
    
    Bug: 40200835
    Validate-Test-Flakiness: skip
    Change-Id: I3e27092a1f69b77c65850da555b08fc3301762f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6655512
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481108}

--- chrome/test/base/android/android_browser_test_browsertest_android.cc
+++ chrome/test/base/android/android_browser_test_browsertest_android.cc
@@ -3,7 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #include "chrome/test/base/android/android_browser_test.h"
@@ -17,4 +20,42 @@ IN_PROC_BROWSER_TEST_F(AndroidBrowserTest, Smoke) {
   const TabModel* tab_model = TabModelList::models()[0];
   EXPECT_TRUE(content::NavigateToURL(tab_model->GetActiveWebContents(),
                                      GURL("about:blank")));
+}
+
+// Verifies that state is preserved between PRE_ stages on Android.
+// Historically PRE_ was not supported, which is why there's an explicit test
+// for this behavior.
+IN_PROC_BROWSER_TEST_F(AndroidBrowserTest, PRE_PRE_StatePreserved) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  Profile* profile = GetProfile();
+  ASSERT_FALSE(base::DirectoryExists(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_PRE_DIR"))));
+  ASSERT_FALSE(base::DirectoryExists(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_DIR"))));
+
+  ASSERT_TRUE(base::CreateDirectory(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_PRE_DIR"))));
+  ASSERT_TRUE(base::DirectoryExists(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_PRE_DIR"))));
+}
+
+IN_PROC_BROWSER_TEST_F(AndroidBrowserTest, PRE_StatePreserved) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  Profile* profile = GetProfile();
+  ASSERT_TRUE(base::DirectoryExists(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_PRE_DIR"))));
+  ASSERT_FALSE(base::DirectoryExists(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_DIR"))));
+
+  ASSERT_TRUE(base::CreateDirectory(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_DIR"))));
+}
+
+IN_PROC_BROWSER_TEST_F(AndroidBrowserTest, StatePreserved) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  Profile* profile = GetProfile();
+  ASSERT_TRUE(base::DirectoryExists(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_PRE_DIR"))));
+  ASSERT_TRUE(base::DirectoryExists(
+      profile->GetPath().Append(FILE_PATH_LITERAL("PRE_DIR"))));
 }
