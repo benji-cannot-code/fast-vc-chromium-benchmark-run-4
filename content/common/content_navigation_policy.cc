589310a20dd071a2e359e30745f2babe7ab03b56
Liam Brady <lbrady@google.com>
2025-07-01 15:01:25 -0700

    Add ContentClient to disable default SiteInstanceGroups
    Introduce a ContentClient function that lets the embedder decide if
    they want to disable default SiteInstanceGroups. This lets us enable
    the feature by default, but leave it disabled on Android WebView while
    fixing remaining tests and blocking issues.
    
    Test: Config only change
    Bug: 390571607, 419595581
    Change-Id: I461fad2d1bba88a6bd315cc45288192194092a61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6682215
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481277}

--- content/common/content_navigation_policy.cc
+++ content/common/content_navigation_policy.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "build/build_config.h"
 #include "content/common/features.h"
+#include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "net/base/features.h"
@@ -179,7 +180,8 @@ bool ShouldCreateSiteInstanceForDataUrls() {
 }
 
 bool ShouldUseDefaultSiteInstanceGroup() {
-  return base::FeatureList::IsEnabled(features::kDefaultSiteInstanceGroups);
+  return GetContentClient()->ShouldAllowDefaultSiteInstanceGroup() &&
+         base::FeatureList::IsEnabled(features::kDefaultSiteInstanceGroups);
 }
 
 }  // namespace content
