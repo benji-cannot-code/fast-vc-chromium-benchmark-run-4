7a2b66ab610a13518eaaee51032b4e1bddae889c
Jennifer Serrano <jennserrano@google.com>
2025-07-01 15:55:32 -0700

    [ntp-composebox] Disable submit button
    The submit button should be disabled when input consists of only
    whitespace characters. Also, the composebox should be closed instead of
    cleared when the input is only whitespace characters.
    
    Bug: 428042283
    Change-Id: I49a9f9da42e51f08dc402a1d9d6cccab380509bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695543
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481294}

--- chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
+++ chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
@@ -290,6 +290,7 @@ suite('NewTabPageComposeboxTest', () => {
     // Arrange.
     composeboxElement.$.input.value = 'test';
     composeboxElement.$.input.dispatchEvent(new Event('input'));
+    await microtasksFinished();
     const submitIcon = $$<HTMLElement>(composeboxElement, '#submitIcon');
     assertTrue(!!submitIcon);
     submitIcon.click();
@@ -307,6 +308,7 @@ suite('NewTabPageComposeboxTest', () => {
     // Arrange.
     composeboxElement.$.input.value = '';
     composeboxElement.$.input.dispatchEvent(new Event('input'));
+    await microtasksFinished();
     const submitIcon = $$<HTMLElement>(composeboxElement, '#submitIcon');
     assertTrue(!!submitIcon);
     submitIcon.click();
@@ -314,5 +316,17 @@ suite('NewTabPageComposeboxTest', () => {
 
     // Assert call does not occur.
     assertEquals(handler.getCallCount('submitQuery'), 0);
+  });
+
+  test('submit button is disabled', async () => {
+    // Arrange.
+    composeboxElement.$.input.value = ' ';
+    composeboxElement.$.input.dispatchEvent(new Event('input'));
+    await microtasksFinished();
+    const submitIcon = $$<HTMLElement>(composeboxElement, '#submitIcon');
+    assertTrue(!!submitIcon);
+
+    // Assert.
+    assertTrue(submitIcon.hasAttribute('disabled'));
   });
 });
