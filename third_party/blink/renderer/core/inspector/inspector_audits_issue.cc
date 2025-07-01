2654084f61cb3312a8ac676fde9b4fd6cb7fb7dc
David Baron <dbaron@chromium.org>
2025-07-01 08:58:40 -0700

    Null check execution context in ReportElementAccessibilityIssue.
    Other AuditsIssue methods that take a document all appear to null-check
    the execution context.
    
    I haven't been able to make a reliable test for this; I was only able to
    reproduce the crash once.
    
    This also removes a CHECK() about feature flags that is no longer true.
    
    Fixed: 428754698
    Bug: 427172874
    Change-Id: Ice6cfe19365523c997d5bd13d775347a3c020a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695646
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481085}

--- third_party/blink/renderer/core/inspector/inspector_audits_issue.cc
+++ third_party/blink/renderer/core/inspector/inspector_audits_issue.cc
@@ -869,7 +869,9 @@ void AuditsIssue::ReportElementAccessibilityIssue(
     DOMNodeId node_id,
     ElementAccessibilityIssueReason issue_reason,
     bool has_disallowed_attributes) {
-  CHECK(HTMLSelectElement::CustomizableSelectEnabled(document));
+  if (!document->GetExecutionContext()) {
+    return;
+  }
 
   auto select_accessibility_issue_details =
       protocol::Audits::ElementAccessibilityIssueDetails::create()
