702b7a2c68869c845c95ab343416cc406cb02b29
David Sanders <dsanders11@ucsbalum.com>
2025-06-30 23:19:04 -0700

    Remove unused include in v8_per_isolate_data.h
    Updates interface.py to add a missing include needed
    for the usage of ActiveScriptWrappable.
    
    Bug: 40318405
    Change-Id: I1d2c9c75d90cb90e02d485c77cc9cda37b71cc54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6692033
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1480874}

--- third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h
+++ third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "gin/public/gin_embedders.h"
 #include "gin/public/isolate_holder.h"
-#include "third_party/blink/renderer/platform/bindings/active_script_wrappable_manager.h"
 #include "third_party/blink/renderer/platform/bindings/dom_wrapper_world.h"
 #include "third_party/blink/renderer/platform/bindings/runtime_call_stats.h"
 #include "third_party/blink/renderer/platform/bindings/script_regexp.h"
