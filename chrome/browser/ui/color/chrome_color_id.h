ea7bac485d431598a364d45d85e07a3d0bdc560b
Marlon Facey <mfacey@chromium.org>
2025-07-01 10:42:12 -0700

    [NTP composebox] Fix OGB theming, logo theming and update background
    OGB and logo were not updating correctly when there is a background.
    Update OGB and background when the composebox is opened. Also
    sets the background of the scrim to match the specs.
    
    Dark: http://screenshot/4nooBzuDnPjMLL5
    Light: http://screenshot/6yepddqkuyDxyzj
    
    NO_IFTTT=Both files were updated.
    
    Bug: 420679912
    Change-Id: I5169972cb3d0a41178b6eee20cc89de3797251b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695787
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481145}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -266,6 +266,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageComposeboxFont) \
   E_CPONLY(kColorNewTabPageComposeboxCancelButton) \
   E_CPONLY(kColorNewTabPageComposeboxHover) \
+  E_CPONLY(kColorNewTabPageComposeboxScrimBackground) \
   E_CPONLY(kColorNewTabPageComposeboxSubmitButton) \
   E_CPONLY(kColorNewTabPageComposeboxUploadButton) \
   E_CPONLY(kColorNewTabPageControlBackgroundHovered) \
