710fc79abe1bf383499222d25458699faa0be47a
Kristi Saney <kristislee@google.com>
2025-07-01 15:31:15 -0700

    [Read aloud] Fix flashing of voices across different profiles
    The language installation status is profile-dependent, but the signal
    from the tts engine about a language is not, so when we were
    uninstalling in one profile, the other profile would incorrectly receive
    the "not installed" status for that language, even though it's still
    installed in that profile.
    
    This fixes it by checking the profile before forwarding the language
    status to the delegate.
    
    Fixed: 413719511
    Change-Id: I33da8d9001ae1f97b6228683352dbaf8ed7edd20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6692851
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481285}

--- chrome/browser/speech/extension_api/tts_extension_apitest.cc
+++ chrome/browser/speech/extension_api/tts_extension_apitest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/speech/extension_api/tts_engine_extension_api.h"
 #include "chrome/browser/speech/extension_api/tts_extension_api.h"
 #include "chrome/common/chrome_switches.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/tts_controller.h"
 #include "content/public/browser/tts_platform.h"
 #include "content/public/test/browser_test.h"
@@ -63,15 +64,18 @@ class MockUpdateLanguageStatusDelegate
   MockUpdateLanguageStatusDelegate() = default;
   ~MockUpdateLanguageStatusDelegate() override = default;
 
-  void OnUpdateLanguageStatus(const std::string& lang,
+  void OnUpdateLanguageStatus(content::BrowserContext* browser_context,
+                              const std::string& lang,
                               content::LanguageInstallStatus install_status,
                               const std::string& error) override {
+    this->on_update_language_status_params.browser_context = browser_context;
     this->on_update_language_status_params.lang = lang;
     this->on_update_language_status_params.install_status = install_status;
     this->on_update_language_status_params.error = error;
   }
 
   struct OnUpdateLanguageStatusParams {
+    raw_ptr<content::BrowserContext> browser_context;
     std::string lang;
     content::LanguageInstallStatus install_status;
     std::string error;
@@ -674,6 +678,8 @@ IN_PROC_BROWSER_TEST_P(TtsApiTest, UpdateLanguageCallsDelegate) {
       &delegate_);
 
   ASSERT_TRUE(RunExtensionTest("tts_engine/call_update_language")) << message_;
+  ASSERT_EQ(delegate_.GetLastUpdateLanguageStatusParams().browser_context,
+            profile());
   ASSERT_EQ(delegate_.GetLastUpdateLanguageStatusParams().install_status,
             content::LanguageInstallStatus::INSTALLING);
   ASSERT_EQ(delegate_.GetLastUpdateLanguageStatusParams().lang, "fr");
