a913392b9f338c90c10e9604a8fa213944a2a0ec
Orin Jaworski <orinj@google.com>
2025-07-01 12:20:49 -0700

    Implement KeepToolbeltInKeywordMode OmniboxToolbelt feature parameter
    With this CL, by default the toolbelt will be dropped when any keyword
    mode is entered. Set KeepToolbeltInKeywordMode true to preserve then.
    
    Fixed: 428795855
    Change-Id: Ic6cd06aa993a5f1c07e3398c72ce881d5c0e48ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6693451
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481197}

--- components/omnibox/common/omnibox_feature_configs.cc
+++ components/omnibox/common/omnibox_feature_configs.cc
@@ -198,6 +198,11 @@ Toolbelt::Toolbelt() {
       base::FeatureParam<bool>(&kOmniboxToolbelt, "KeepToolbeltAfterInput",
                                enabled)
           .Get();
+  keep_toolbelt_in_keyword_mode =
+      base::FeatureParam<bool>(&kOmniboxToolbelt, "KeepToolbeltInKeywordMode",
+                               false)
+          .Get();
+
   always_include_lens_action =
       base::FeatureParam<bool>(&kOmniboxToolbelt, "AlwaysIncludeLensAction",
                                false)
