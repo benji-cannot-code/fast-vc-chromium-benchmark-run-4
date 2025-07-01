687aecb21feda3c3121eab1f11fbbeabfd51c5a6
Jason <cgqaq@chromium.org>
2025-07-01 08:26:28 -0700

    Remove allow_unsafe_buffers in string_statics.cc
    Bug: 351564777
    Change-Id: I86483fdd8acc83af94886441dd6306449dde4e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694773
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1481068}

--- third_party/blink/renderer/platform/wtf/text/atomic_string_table.cc
+++ third_party/blink/renderer/platform/wtf/text/atomic_string_table.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string_table.h"
 
+#include <cstdint>
+
 #include "base/containers/heap_array.h"
 #include "base/containers/span.h"
 #include "base/notreached.h"
@@ -39,10 +41,11 @@ class UCharBuffer {
       // This is a very common case from HTML parsing, so we take
       // the size penalty from inlining.
       return StringHasher::ComputeHashAndMaskTop8BitsInline<
-          WTF::ConvertTo8BitHashReader>((const char*)chars, len);
+          WTF::ConvertTo8BitHashReader>(
+          {reinterpret_cast<const uint8_t*>(chars), len});
     } else {
-      return StringHasher::ComputeHashAndMaskTop8Bits((const char*)chars,
-                                                      len * 2);
+      return StringHasher::ComputeHashAndMaskTop8Bits(
+          reinterpret_cast<const char*>(chars), len * 2);
     }
   }
 
@@ -348,9 +351,7 @@ class LCharBuffer {
       : characters_(chars.data()),
         length_(chars.size()),
         // This is a common path from V8 strings, so inlining is worth it.
-        hash_(StringHasher::ComputeHashAndMaskTop8BitsInline(
-            base::as_chars(chars).data(),
-            chars.size())) {}
+        hash_(StringHasher::ComputeHashAndMaskTop8BitsInline(chars)) {}
 
   base::span<const LChar> characters() const { return {characters_, length_}; }
   unsigned hash() const { return hash_; }
