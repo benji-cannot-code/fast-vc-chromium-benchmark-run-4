1c5372223d8d1ebd40026934ce94cfec59eda1f7
Christopher Cameron <ccameron@chromium.org>
2025-07-01 10:04:11 -0700

    Update HDR headroom query API to match spec
    The spec uses hdrHeadroom instead of highDynamicRangeHeadroom, and uses
    the log2 headroom value ("stops" in photography-speak) instead of linear
    values. It also has a separate onhdrheadroomchange event for changes to
    HDR headroom.
    
    Add a test to verify that:
    - this property is propagated to the ScreenDetailed interface
    - the onhdrheadroomchange fires when HDR headroom changes
    - the change event does not fire when HDR headroom changes
    - the onhdrheadroomchange does not fire for other changes
    
    Bug: 428357629
    Change-Id: Ic65736d702235ab60c151f4ec32289a044467ea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6668855
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481128}

--- third_party/blink/renderer/modules/screen_details/screen_detailed.cc
+++ third_party/blink/renderer/modules/screen_details/screen_detailed.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/screen_details/screen_detailed.h"
 
+#include <cmath>
+
 #include "base/numerics/safe_conversions.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -93,6 +95,13 @@ bool ScreenDetailed::AreWebExposedScreenDetailedPropertiesEqual(
   return true;
 }
 
+// static
+bool ScreenDetailed::AreHdrHeadroomEqual(const display::ScreenInfo& prev,
+                                         const display::ScreenInfo& current) {
+  return prev.display_color_spaces.GetHDRMaxLuminanceRelative() ==
+         current.display_color_spaces.GetHDRMaxLuminanceRelative();
+}
+
 int ScreenDetailed::left() const {
   if (!DomWindow())
     return 0;
@@ -127,6 +136,12 @@ String ScreenDetailed::label() const {
   if (!DomWindow())
     return String();
   return String::FromUTF8(GetScreenInfo().label);
+}
+
+float ScreenDetailed::hdrHeadroom() const {
+  return std::log2(std::max(
+      GetScreenInfo().display_color_spaces.GetHDRMaxLuminanceRelative(),
+      1.f));
 }
 
 float ScreenDetailed::highDynamicRangeHeadroom() const {
