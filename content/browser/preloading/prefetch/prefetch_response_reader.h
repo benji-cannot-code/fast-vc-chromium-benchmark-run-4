e0778dbda3bc3812431762cff6ddf49a89f63919
kenoss <kenoss@chromium.org>
2025-07-01 03:28:38 -0700

    PrefetchReusable: Remove feature flag (Part 2/4)
    This CL removes the use of the feature flag `kPrefetchReusbale`.
    
    No behavioral change.
    
    Bug: 397882995
    Fixed: 373553133
    Change-Id: Ia66235e4e23ff10bfa9d9e4dd8f2bb20fc6881bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656639
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480954}

--- content/browser/preloading/prefetch/prefetch_response_reader.h
+++ content/browser/preloading/prefetch/prefetch_response_reader.h
@@ -54,11 +54,6 @@ class CONTENT_EXPORT PrefetchResponseReader final
     : public network::mojom::URLLoader,
       public base::RefCounted<PrefetchResponseReader> {
  public:
-  // TODO(crbug.com/373553133): Stop using two different constructors once we
-  // find how we enable `is_reusable_` path.
-  explicit PrefetchResponseReader(bool is_reusable);
-  // For
-  // //content/browser/preloading/prefetch/prefetch_streaming_url_loader_unittest.cc
   explicit PrefetchResponseReader();
 
   void SetStreamingURLLoader(
@@ -125,8 +120,6 @@ class CONTENT_EXPORT PrefetchResponseReader final
     return weak_ptr_factory_.GetWeakPtr();
   }
 
-  bool is_reusable() const { return is_reusable_; }
-
  private:
   // Identifies a client in `serving_url_loader_clients_`.
   using ServingUrlLoaderClientId = mojo::RemoteSetElementId;
@@ -184,9 +177,6 @@ class CONTENT_EXPORT PrefetchResponseReader final
   // stored into `head_` (for non-redirect responses) or `event_queue_` (or
   // redirect responses).
   void StoreInfoFromResponseHead(const network::mojom::URLResponseHead& head);
-
-  // If true, the body is reusable with help of `PrefetchDataPipeTee`.
-  const bool is_reusable_;
 
   // All URLLoader events are queued up here.
   std::vector<EventCallback> event_queue_;
@@ -274,9 +264,6 @@ class CONTENT_EXPORT PrefetchResponseReader final
 
   // The prefetched data and metadata. Not set for a redirect response.
   network::mojom::URLResponseHeadPtr head_;
-  // `body_` is set/used only when `!is_reusable_`.
-  mojo::ScopedDataPipeConsumerHandle body_;
-  // `body_tee_` is set/used only when `is_reusable_`.
   scoped_refptr<PrefetchDataPipeTee> body_tee_;
   std::optional<network::URLLoaderCompletionStatus> completion_status_;
   // Recorded on `OnComplete` and used to check if the prefetch data is still
