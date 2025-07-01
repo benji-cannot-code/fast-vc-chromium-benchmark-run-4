77dfc29c74930d8e09d56f9121ee2b9f4b496b93
Federica Germinario <fedegermi@google.com>
2025-07-01 07:33:37 -0700

    [ios] Use gaia as identifier instead of the email on widgets
    This change fixes an issue happening when "No account" is selected and
    the language is changed. In this case the account was considered
    as removed (so the deleted ui was displayed).
    
    By using the gaia as id this issue is fixed.
    
    Fixed: 428668929
    Change-Id: Ib7a49b510ff736ba222b8da1b87e46bed8f03fe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6688494
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481038}

--- ios/chrome/widget_kit_extension/select_profile_intent.swift
+++ ios/chrome/widget_kit_extension/select_profile_intent.swift
@@ -21,7 +21,7 @@ struct AccountQuery: EntityQuery {
     let noAccountName = String(
       localized: "IDS_IOS_WIDGET_KIT_EXTENSION_NO_ACCOUNT_LABEL")
 
-    let noAccount = AccountDetail(id: noAccountName, gaia: "Default")
+    let noAccount = AccountDetail(id: "Default", email: noAccountName)
 
     guard let accounts = try? await suggestedEntities()
     else { return noAccount }
@@ -32,8 +32,8 @@ struct AccountQuery: EntityQuery {
     guard let primaryAccount = sharedDefaults.object(forKey: "ios.primary_account") as? String
     else { return noAccount }
     for account in accounts {
-      if account.gaia == primaryAccount {
-        return AccountDetail(id: account.id, gaia: account.gaia)
+      if account.id == primaryAccount {
+        return AccountDetail(id: account.id, email: account.email)
       }
     }
     return noAccount
@@ -41,14 +41,15 @@ struct AccountQuery: EntityQuery {
 }
 
 struct AccountDetail: AppEntity {
+  // Id contains the gaia id of the account or "Default" for the "No account" case.
   let id: String
-  let gaia: String
+  let email: String
 
   static let typeDisplayRepresentation: TypeDisplayRepresentation = "Account"
   static let defaultQuery = AccountQuery()
 
   var displayRepresentation: DisplayRepresentation {
-    DisplayRepresentation(title: "\(id)")
+    DisplayRepresentation(title: "\(email)")
   }
 
   static func allAccounts() -> [AccountDetail] {
@@ -63,11 +64,11 @@ struct AccountDetail: AppEntity {
 
     let noAccountName = String(
       localized: "IDS_IOS_WIDGET_KIT_EXTENSION_NO_ACCOUNT_LABEL")
-    accountsDetail.append(AccountDetail(id: noAccountName, gaia: "Default"))
+    accountsDetail.append(AccountDetail(id: "Default", email: noAccountName))
 
     for (key, value) in accounts {
       if let email = value["email"] as? String {
-        accountsDetail.append(AccountDetail(id: email, gaia: key))
+        accountsDetail.append(AccountDetail(id: key, email: email))
       }
     }
     return accountsDetail
@@ -85,7 +86,7 @@ struct SelectAccountIntent: WidgetConfigurationIntent {
 
   // Returns the avatar linked to the account.
   func avatar() -> Image? {
-    guard let gaia = account?.gaia
+    guard let gaia = account?.id
     else { return nil }
 
     let avatarFilePath =
@@ -99,7 +100,7 @@ struct SelectAccountIntent: WidgetConfigurationIntent {
 
   // Returns the gaiaID linked to the account.
   func gaia() -> String? {
-    guard let gaia = account?.gaia
+    guard let gaia = account?.id
     else { return nil }
 
     return gaia
@@ -107,6 +108,6 @@ struct SelectAccountIntent: WidgetConfigurationIntent {
 
   // Returns a boolean used to check if the account was deleted from device.
   func deleted() -> Bool {
-    return account?.gaia == nil
+    return account?.id == nil
   }
 }
