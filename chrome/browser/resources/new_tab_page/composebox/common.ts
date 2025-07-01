2862674d05dd23dc183b45a108156fdccec70132
Ananya <ananyaseelam@google.com>
2025-07-01 06:28:52 -0700

    [ntp-composebox] Send file info to controller
    This CL collects files from the UI through the `onFileChange_` method
    which sends file data to the `composebox_handler` which subsequently
    initiates the upload file flow in the `composebox_query_controller
    
    Bug: 427547161
    Change-Id: Ia9ef055d932fbabaf6e0d0f7a7fe21e65aacb634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6673637
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481010}

--- chrome/browser/resources/new_tab_page/composebox/common.ts
+++ chrome/browser/resources/new_tab_page/composebox/common.ts
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 export interface ComposeboxFile {
+  // TODO(crbug.com/427994425): Keep uuid until we get base::Unguessable token
+  //   through a callback.
   uuid: string;
   name: string;
   objectUrl: string|null;
--- chrome/browser/ui/webui/new_tab_page/composebox/composebox_handler_unittest.cc
+++ chrome/browser/ui/webui/new_tab_page/composebox/composebox_handler_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_navigation_observer.h"
+#include "mojo/public/cpp/base/unguessable_token_mojom_traits.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -39,6 +40,11 @@ class MockQueryController : public ComposeboxQueryController {
 
   MOCK_METHOD(void, NotifySessionStarted, ());
   MOCK_METHOD(void, NotifySessionAbandoned, ());
+  MOCK_METHOD(
+      void,
+      StartFileUploadFlow,
+      (std::unique_ptr<ComposeboxQueryController::FileInfo> file_info_mojom,
+       scoped_refptr<base::RefCountedBytes> file_data));
 };
 
 class TestWebContentsDelegate : public content::WebContentsDelegate {
@@ -142,4 +148,34 @@ TEST_F(ComposeboxHandlerTest, SubmitQuery) {
   // Ensure navigation occurred.
   EXPECT_EQ(expected_url,
             web_contents()->GetController().GetLastCommittedEntry()->GetURL());
+}
+
+TEST_F(ComposeboxHandlerTest, AddFile_Pdf) {
+  composebox::mojom::SelectedFileInfoPtr file_info =
+      composebox::mojom::SelectedFileInfo::New();
+  file_info->file_name = "test.pdf";
+  file_info->selection_time = base::Time::Now();
+  file_info->mime_type = "application/pdf";
+
+  std::vector<uint8_t> test_data = {1, 2, 3, 4};
+  auto test_data_span = base::span<const uint8_t>(test_data);
+  mojo_base::BigBuffer file_data(test_data_span);
+
+  EXPECT_CALL(query_controller(), StartFileUploadFlow).Times(1);
+  handler().AddFile(std::move(file_info), std::move(file_data));
+}
+
+TEST_F(ComposeboxHandlerTest, AddFile_Image) {
+  composebox::mojom::SelectedFileInfoPtr file_info =
+      composebox::mojom::SelectedFileInfo::New();
+  file_info->file_name = "test.png";
+  file_info->selection_time = base::Time::Now();
+  file_info->mime_type = "application/image";
+
+  std::vector<uint8_t> test_data = {1, 2, 3, 4};
+  auto test_data_span = base::span<const uint8_t>(test_data);
+  mojo_base::BigBuffer file_data(test_data_span);
+
+  EXPECT_CALL(query_controller(), StartFileUploadFlow).Times(1);
+  handler().AddFile(std::move(file_info), std::move(file_data));
 }
