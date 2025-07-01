7f7871f1b606eac6550ed0fa52daef6596b3251d
Hailey Wang <haileywang@google.com>
2025-06-30 17:05:19 -0700

    [STG] Removed obsolete CiC flag
    Automotive behavior has been QA tested, bug fixed and enabled in the
    STG stable experiment. Deleting the flag now.
    
    Bug: 40623513
    Change-Id: Id8ad8cfbe292df4838e2d249ca634ac35f6a4824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6690504
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480810}

--- components/data_sharing/public/features.h
+++ components/data_sharing/public/features.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace data_sharing::features {
 
-// Feature flag for enabling collaboration on automotive.
-BASE_DECLARE_FEATURE(kCollaborationAutomotive);
-
 // Feature flag for enabling collaboration in entreprise v2.
 BASE_DECLARE_FEATURE(kCollaborationEntrepriseV2);
 
