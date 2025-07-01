b7a75378d57a8d7b8af321ef21cce0d31cefa4a3
Benjamin Zielinski <bzielinski@google.com>
2025-07-01 09:52:12 -0700

    Remove flag for projector dvs
    This flag is obsolete since the feature is already launched. This
    change also removes the metadata for the projector gm3 flag since it
    was missed when the rest of the flag was cleaned up.
    
    Bug: 373522475
    Change-Id: I1170b8d15331d3efb44d4f68765be797772d94dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6692071
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Reviewed-by: Dorian Brandon <dorianbrandon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481123}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4509,9 +4509,6 @@ extern const char kProjectorServerSideSpeechRecognitionDescription[];
 extern const char kProjectorServerSideUsmName[];
 extern const char kProjectorServerSideUsmDescription[];
 
-extern const char kProjectorUseDVSPlaybackEndpointName[];
-extern const char kProjectorUseDVSPlaybackEndpointDescription[];
-
 extern const char kReleaseNotesNotificationAllChannelsName[];
 extern const char kReleaseNotesNotificationAllChannelsDescription[];
 
