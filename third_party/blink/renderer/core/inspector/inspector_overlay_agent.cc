e09c653d092a35f00a37461f0fcc26c43ee28bd9
Philip Pfaffe <pfaffe@chromium.org>
2025-07-01 05:32:10 -0700

    Remove unused distance tool
    The distance tool was planned to be added to the inspector overlay, but
    it was never used in devtools. Original issue: crbug.com/41449675
    
    Bug: 425562188
    Change-Id: I49072cd4649934c13e9158a5c873614add77f773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696345
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480987}

--- third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
@@ -1581,8 +1581,7 @@ protocol::Response InspectorOverlayAgent::setInspectMode(
   if (mode != protocol::Overlay::InspectModeEnum::None &&
       mode != protocol::Overlay::InspectModeEnum::SearchForNode &&
       mode != protocol::Overlay::InspectModeEnum::SearchForUAShadowDOM &&
-      mode != protocol::Overlay::InspectModeEnum::CaptureAreaScreenshot &&
-      mode != protocol::Overlay::InspectModeEnum::ShowDistances) {
+      mode != protocol::Overlay::InspectModeEnum::CaptureAreaScreenshot) {
     return protocol::Response::ServerError(
         StrCat({"Unknown mode \"", mode, "\" was provided."}).Utf8());
   }
@@ -1622,10 +1621,6 @@ void InspectorOverlayAgent::PickTheRightTool() {
   } else if (inspect_mode ==
              protocol::Overlay::InspectModeEnum::CaptureAreaScreenshot) {
     inspect_tool = MakeGarbageCollected<ScreenshotTool>(this, GetFrontend());
-  } else if (inspect_mode ==
-             protocol::Overlay::InspectModeEnum::ShowDistances) {
-    inspect_tool =
-        MakeGarbageCollected<NearbyDistanceTool>(this, GetFrontend());
   } else if (!paused_in_debugger_message_.Get().IsNull()) {
     inspect_tool = MakeGarbageCollected<PausedInDebuggerTool>(
         this, GetFrontend(), v8_session_, paused_in_debugger_message_.Get());
