a2dfb3a44bfd0ab4a6a2602589bd1f301916f5ef
Radu Nitescu <radunitescu@google.com>
2025-07-01 08:01:01 -0700

    [iOS][MIA] Avoid hint label overlap with the MIA animation
    Add an offset on the trailing side of the label when the MIA animation
    is shown to avoid a visual overlap.
    
    Fixed: 428922528
    Change-Id: I6a3f903f646959474c75d5e391f9489acd6748f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694735
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481058}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
@@ -461,16 +461,6 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
     self.fakeLocationBarHeightConstraint,
   ]];
 
-  UIView* referenceView = _buttonStack.arrangedSubviews.firstObject;
-  self.hintLabelTrailingConstraint = [self.searchHintLabel.trailingAnchor
-      constraintLessThanOrEqualToAnchor:referenceView.leadingAnchor];
-  self.hintLabelTrailingConstraint.priority = UILayoutPriorityDefaultHigh;
-  [NSLayoutConstraint activateConstraints:@[
-    [referenceView.centerYAnchor
-        constraintEqualToAnchor:self.fakeLocationBar.centerYAnchor],
-    self.hintLabelTrailingConstraint,
-  ]];
-
   [self addSearchEngineLogoIfNeededToSearchField:searchField];
 }
 
@@ -598,7 +588,11 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
   CGFloat hintLabelScalingExtraOffset =
       (_currentHintLabelScale - 1) *
       self.searchHintLabel.intrinsicContentSize.width * 0.5;
-  self.hintLabelTrailingConstraint.constant = -hintLabelScalingExtraOffset;
+
+  // If MIA animation view is shown then add an aditional spacing to avoid any
+  // overlap with the label.
+  self.hintLabelTrailingConstraint.constant =
+      -hintLabelScalingExtraOffset - [self miaButtonHintLabelOffset];
 
   CGFloat fakeOmniboxHeight = content_suggestions::FakeOmniboxHeight();
   CGFloat locationBarHeight = content_suggestions::PinnedFakeOmniboxHeight();
@@ -934,6 +928,7 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
                                                .userInterfaceStyle];
 
   [self addActionsToFakeboxButtons];
+  [self updateHintLabelTrailingConstraint];
 }
 
 // Registers the actions for the fakebox buttons.
@@ -950,6 +945,25 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
   [self.miaButton addTarget:self
                      action:@selector(openMIA)
            forControlEvents:UIControlEventTouchUpInside];
+}
+
+// Updates the trailing constraint of the label to the nearest button stack
+// element.
+- (void)updateHintLabelTrailingConstraint {
+  UIView* referenceView = _buttonStack.arrangedSubviews.firstObject;
+  if (!referenceView) {
+    return;
+  }
+
+  self.hintLabelTrailingConstraint = [self.searchHintLabel.trailingAnchor
+      constraintLessThanOrEqualToAnchor:referenceView.leadingAnchor
+                               constant:-[self miaButtonHintLabelOffset]];
+  self.hintLabelTrailingConstraint.priority = UILayoutPriorityDefaultHigh;
+  [NSLayoutConstraint activateConstraints:@[
+    [referenceView.centerYAnchor
+        constraintEqualToAnchor:self.fakeLocationBar.centerYAnchor],
+    self.hintLabelTrailingConstraint,
+  ]];
 }
 
 // Gets the fonts for the pinned and unpinned fakebox hint label, and sets
@@ -1205,6 +1219,14 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
   [self.miaButton addSubview:_miaAnimationView];
   AddSameCenterConstraints(_miaAnimationView, self.miaButton);
   AddSizeConstraints(_miaAnimationView, [self miaAnimationSize]);
+}
+
+- (CGFloat)miaButtonHintLabelOffset {
+  if (self.useSingleButtonMIA && _miaAnimationView) {
+    return [self miaAnimationSize].width / 2;
+  }
+
+  return 0;
 }
 
 // The size for the animation view dependant on the fakebox size.
