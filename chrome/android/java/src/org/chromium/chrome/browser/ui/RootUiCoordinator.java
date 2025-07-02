de1cce584e1f1558a9d1826efcd1d3b2ac04af6e
Mark Schillaci <mschillaci@google.com>
2025-07-02 13:20:13 -0700

    [TopControls] Make TopControlsStacker observe height changes via BrowserControlsManager
    This CL continues work on creating a TopControlsStacker. With this CL
    we update the Stacker to implement onTopControlsHeightChanged to get
    updates from the BrowserControlSizer that it adds itself to as an
    observer during construction. The Stacker will inform each of the
    TopControlLayers of the changes (with a default no-op implementation)
    so that in the future TopControlLayers do not need to separately observe
    the BrowserControlsManager, and all offset and height changes/logic
    will flow through the TopControlsStacker. We update the BookmarkBar to
    listen for height changes via the BookmarkBarCoordinator as the only
    TopControlLayer currently interested in these updates.
    
    Bug: 417238089
    Change-Id: Ie3d81d06547facaca6287e13c8a170acb8b8b832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694971
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481843}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -565,7 +565,7 @@ public class RootUiCoordinator
         mEdgeToEdgeManager = edgeToEdgeManager;
         mBottomControlsStacker =
                 new BottomControlsStacker(mBrowserControlsManager, mActivity, mWindowAndroid);
-        mTopControlsStacker = new TopControlsStacker();
+        mTopControlsStacker = new TopControlsStacker(mBrowserControlsManager);
         mXrSpaceModeObservableSupplier = xrSpaceModeObservableSupplier;
     }
 
