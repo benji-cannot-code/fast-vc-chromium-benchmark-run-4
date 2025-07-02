82553d516fc9eab3841e13fde26110c9156f0800
Wilson Low <wilsonlow@google.com>
2025-07-02 14:33:58 -0700

    [BNPL][Klarna] Add UpdateBnplPaymentInstrumentCall
    This CL implements the UpdateBnplPaymentInstrumentCall and integrates it
    with the Payments Network Interface. This request is used to indicate
    whether the user successfully completed the ToS flow for a linked BNPL
    partner.
    
    Relevant server-side change: cl/776784917
    
    Bug: 424259928
    Fixed: b:424599247
    Change-Id: I9c84690856d2ca70e0b309ce16d4c24a3a0e52ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6692214
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481897}

--- components/autofill/core/browser/payments/payments_network_interface.cc
+++ components/autofill/core/browser/payments/payments_network_interface.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_requests/select_challenge_option_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/unmask_card_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/unmask_iban_request.h"
+#include "components/autofill/core/browser/payments/payments_requests/update_bnpl_payment_instrument_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/update_virtual_card_enrollment_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/upload_card_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/upload_iban_request.h"
@@ -249,6 +250,16 @@ void PaymentsNetworkInterface::GetDetailsForUpdateBnplPaymentInstrument(
           /*full_sync_enabled=*/
           account_info_getter_->IsSyncFeatureEnabledForPaymentsServerMetrics(),
           std::move(callback)));
+}
+
+void PaymentsNetworkInterface::UpdateBnplPaymentInstrument(
+    const UpdateBnplPaymentInstrumentRequestDetails& request_details,
+    base::OnceCallback<void(PaymentsRpcResult)> callback) {
+  IssueRequest(std::make_unique<UpdateBnplPaymentInstrumentRequest>(
+      request_details,
+      /*full_sync_enabled=*/
+      account_info_getter_->IsSyncFeatureEnabledForPaymentsServerMetrics(),
+      std::move(callback)));
 }
 
 }  // namespace autofill::payments
