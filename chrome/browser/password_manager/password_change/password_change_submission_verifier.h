ac12894d71697f03579476637bb582c14f6cfbf3
Viktor Semeniuk <vsemeniuk@google.com>
2025-07-02 06:48:18 -0700

    Avoid sending A11y tree to the server
    This CL simplifies the code to avoid sending A11y tree to the server
    as now Annotated Page Context can be used by default.
    
    Bug: 409946698
    Change-Id: I85189e1cc93f81940b5e92b9fc5a94851fe4083a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695191
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481568}

--- chrome/browser/password_manager/password_change/password_change_submission_verifier.h
+++ chrome/browser/password_manager/password_change/password_change_submission_verifier.h
@@ -16,13 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/content/browser/page_content_proto_provider.h"
 #include "components/optimization_guide/core/optimization_guide_model_executor.h"
 #include "components/password_manager/core/browser/password_form.h"
-#include "ui/accessibility/ax_tree_update.h"
 
 namespace content {
 class WebContents;
 }
 class ModelQualityLogsUploader;
-class OptimizationGuideKeyedService;
 
 // Helper class which verifies whether password change was successful or not.
 class PasswordChangeSubmissionVerifier {
@@ -68,30 +66,21 @@ class PasswordChangeSubmissionVerifier {
 #endif
 
  private:
-  void OnAnnotatedPageContentReceived(
+  void CheckSubmissionSuccessful(
       std::optional<optimization_guide::AIPageContentResult> page_content);
-  void OnAxTreeReceived(ui::AXTreeUpdate& ax_tree_update);
-  void CheckSubmissionSuccessful();
   void OnExecutionResponseCallback(
+      base::Time request_time,
       optimization_guide::OptimizationGuideModelExecutionResult
           execution_result,
       std::unique_ptr<
           optimization_guide::proto::PasswordChangeSubmissionLoggingData>
           logging_data);
 
-  OptimizationGuideKeyedService* GetOptimizationService() const;
-
   const raw_ptr<content::WebContents> web_contents_;
   base::OnceCallback<void(optimization_guide::OnAIPageContentDone)>
       capture_annotated_page_content_;
   FormSubmissionResultCallback callback_;
   raw_ptr<ModelQualityLogsUploader> logs_uploader_;
-  // TODO(crbug.com/409946698): Delete this when removing support for AX tree
-  // prompts.
-  optimization_guide::proto::PasswordChangeRequest
-      check_submission_successful_request_;
-
-  base::Time server_request_start_time_;
 
   base::WeakPtrFactory<PasswordChangeSubmissionVerifier> weak_ptr_factory_{
       this};
