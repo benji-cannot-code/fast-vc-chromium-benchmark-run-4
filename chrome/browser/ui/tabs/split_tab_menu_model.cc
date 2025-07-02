b2b8c73fc9cf5b1b4db1f5ecce50c151feba239f
Vince Lugli <lugli@google.com>
2025-07-02 12:25:34 -0700

    Fixes close tab order in RTL locales
    Ensures the correct tab is closed when using the "Close Left Tab" and "Close Right Tab" options in RTL locales.
    
    Adds a new test to verify the fix.
    
    Bug: 424476637
    Change-Id: I625b74825ba467a080639358e55ff99b1a50dd23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697733
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Vince Lugli <lugli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481806}

--- chrome/browser/ui/tabs/split_tab_menu_model.cc
+++ chrome/browser/ui/tabs/split_tab_menu_model.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/check.h"
+#include "base/i18n/rtl.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -175,12 +176,17 @@ void SplitTabMenuModel::ExecuteCommand(int command_id, int event_flags) {
     case CommandId::kCloseSpecifiedTab:
       CloseTabAtIndex(split_tab_index_.value());
       break;
-    case CommandId::kCloseStartTab:
-      CloseTabAtIndex(tab_strip_model_->GetIndexOfTab(tabs_in_split[0]));
+    case CommandId::kCloseStartTab: {
+      int startIndex = base::i18n::IsRTL() ? 1 : 0;
+      CloseTabAtIndex(
+          tab_strip_model_->GetIndexOfTab(tabs_in_split[startIndex]));
       break;
-    case CommandId::kCloseEndTab:
-      CloseTabAtIndex(tab_strip_model_->GetIndexOfTab(tabs_in_split[1]));
+    }
+    case CommandId::kCloseEndTab: {
+      int endIndex = base::i18n::IsRTL() ? 0 : 1;
+      CloseTabAtIndex(tab_strip_model_->GetIndexOfTab(tabs_in_split[endIndex]));
       break;
+    }
     case CommandId::kExitSplit:
       tab_strip_model_->RemoveSplit(split_id);
       break;
