e242721acaef8a826ebe49faae5e4a70834478d7
Tomek Jurkiewicz <tju@google.com>
2025-07-02 06:48:47 -0700

    Use a weak pointer for Content Filter observers in tests
    Before this patch, the test environment holds a bare pointer to the
    observers created by the service. This is a potential use-after-free,
    because the service has ownership of those objects. This patch uses a
    weak pointer to break the dependency on the observer life-cycle.
    
    Also: moves the observer variables to protected, to be accessed from
    test classes that add extra test specific interface.
    
    Bug: 405328417
    
    Change-Id: I5caba462ff98c696242bb865a8e6e0f000f6e4aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695015
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481569}

--- components/supervised_user/core/browser/supervised_user_service.h
+++ components/supervised_user/core/browser/supervised_user_service.h
@@ -171,6 +171,12 @@ class SupervisedUserService : public KeyedService {
 #endif
   );
 
+ protected:
+#if BUILDFLAG(IS_ANDROID)
+  ContentFiltersObserverBridge* browser_content_filters_observer();
+  ContentFiltersObserverBridge* search_content_filters_observer();
+#endif  // BUILDFLAG(IS_ANDROID)
+
  private:
   // Activates the service which controls managed settings of url filtering and
   // incognito mode.
@@ -255,6 +261,7 @@ class SupervisedUserService : public KeyedService {
       browser_content_filters_observer_;
   std::unique_ptr<ContentFiltersObserverBridge>
       search_content_filters_observer_;
+
 #endif  // BUILDFLAG(IS_ANDROID)
 
   // True only when |Shutdown()| method has been called.
@@ -268,8 +275,6 @@ class SupervisedUserService : public KeyedService {
 #if BUILDFLAG(IS_CHROMEOS)
   bool signout_required_after_supervision_enabled_ = false;
 #endif
-
-  base::WeakPtrFactory<SupervisedUserService> weak_ptr_factory_{this};
 };
 
 }  // namespace supervised_user
