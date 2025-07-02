aca8d7402ce912caa9af16f283ed68891e9bd4bb
Asami Doi <asamidoi@chromium.org>
2025-07-02 07:24:40 -0700

    [ios] Fix RequestDesktopMobileSiteTestCase/testRequestDesktopByDefault on iOS 26
    Scrolling in EG tests doesn't work well in some cases. This CL disables
    the timer in this scope to avoid infinite spinner loop. See
    crbug.com/40138424 for more details.
    
    Fixed: 428896261
    Change-Id: Id256b4bfcfba8866e9e1c7d89a99a8db4495c7fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695950
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481591}

--- ios/chrome/browser/popup_menu/ui_bundled/request_desktop_mobile_site_egtest.mm
+++ ios/chrome/browser/popup_menu/ui_bundled/request_desktop_mobile_site_egtest.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
+#import "ios/chrome/test/earl_grey/scoped_disable_timer_tracking.h"
 #import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/web/common/features.h"
@@ -134,13 +135,18 @@ class UserAgentResponseProvider : public web::DataResponseProvider {
 // Sets the default mode to the passed `defaultMode`.
 - (void)selectDefaultMode:(NSString*)defaultMode {
   [ChromeEarlGreyUI openSettingsMenu];
-  [[[EarlGrey
-      selectElementWithMatcher:grey_allOf(grey_accessibilityID(
-                                              kSettingsContentSettingsCellId),
-                                          grey_sufficientlyVisible(), nil)]
-         usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 150)
-      onElementWithMatcher:chrome_test_util::SettingsCollectionView()]
-      performAction:grey_tap()];
+  {
+    // Disable the timer in this scope to avoid infinite spinner loop. EarlGrey
+    // tests wait for scroll bars to disappear after a scroll operation.
+    ScopedDisableTimerTracking disabler;
+    [[[EarlGrey
+        selectElementWithMatcher:grey_allOf(grey_accessibilityID(
+                                                kSettingsContentSettingsCellId),
+                                            grey_sufficientlyVisible(), nil)]
+           usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 200)
+        onElementWithMatcher:chrome_test_util::SettingsCollectionView()]
+        performAction:grey_tap()];
+  }
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
                                           kSettingsDefaultSiteModeCellId)]
       performAction:grey_tap()];
