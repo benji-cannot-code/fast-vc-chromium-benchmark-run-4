d1e8bb5bb374f1643bdd0894af6689c1c38b9208
Arthur Milchior <arthurmilchior@google.com>
2025-07-02 07:13:31 -0700

    [iOS]closeManageSyncSettings stop the children
    When `closeManageSyncSettings` was called, it was closing view
    controller but not properly dismissing children coordinator, such as
    AccountMenuCoordinator. This could occur, for example, if the user gets
    signed-out.
    
    This CL ensures that the children view controllers are properly
    dismissed.
    
    Bug: 425899803
    Change-Id: I4a0934f3ca6cb389eacd522baeca184468b6e2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6662458
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481581}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_mediator.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_mediator.mm
@@ -161,6 +161,7 @@ constexpr CGFloat kBatchUploadSymbolPointSize = 22.;
 - (void)disconnect {
   _syncObserver.reset();
   _syncService = nullptr;
+  _consumer = nullptr;
   _identityManager = nullptr;
   _identityManagerObserver.reset();
   _authenticationService = nullptr;
