7e3a721bde02ae3f1206be1ec3ad310d93ad17a9
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-07-02 10:10:36 -0700

    [iOS][BWG] Creating and updating prefs for BWG session persistence
    Create, update and remove dicts in prefs for session storage and
    persistence. Also call the correct state updaters when delegate calls
    are executed.
    
    Bug: 419070203
    Change-Id: Iefe77b8b931c1a210d83e0d5193f48a49eae414d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695770
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481704}

--- ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller.mm
+++ ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller.mm
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller.h"
 
 #import "ios/chrome/browser/intelligence/bwg/ui/bwg_consent_mutator.h"
-#import "ios/chrome/browser/intelligence/bwg/ui/bwg_constants.h"
 #import "ios/chrome/browser/intelligence/bwg/ui/bwg_promo_view_controller_delegate.h"
 #import "ios/chrome/browser/intelligence/bwg/ui/bwg_ui_utils.h"
+#import "ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
--- ios/chrome/browser/intelligence/bwg/utils/bwg_constants.mm
+++ ios/chrome/browser/intelligence/bwg/utils/bwg_constants.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/intelligence/bwg/ui/bwg_constants.h"
+#import "ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h"
 
 NSString* const kBWGPromoConsentPartialDetentIdentifier =
     @"BWGConsentPartialDetentIdentifier";
@@ -11,3 +11,6 @@ NSString* const kBWGPromoConsentFullDetentIdentifier =
     @"BWGConsentFullDetentIdentifier";
 
 NSString* const kLottieAnimationFREBannerName = @"FRE_Banner";
+
+const char kLastInteractionTimestampDictKey[] = "last_interaction_timestamp";
+const char kServerIDDictKey[] = "server_id";
