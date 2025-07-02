b97dbcebe2c77994a9037770c2bfbd786765b131
James Cook <jamescook@chromium.org>
2025-07-02 11:41:03 -0700

    extensions: Improve chrome.tabs.create() stub on desktop Android
    The goal here is to get enough of the tabs API working on desktop
    Android that we can use the tabs API in tests of *other* APIs.
    
    This CL is all throwaway code. It will eventually be replaced by a
    cross-platform tab and window API.
    
    Bug: 371432155
    Test: android_browsertests
    Change-Id: Ieb158919fb84cec1e35a6df8c949b5a2de843c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696684
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481767}

---
+++ chrome/test/data/extensions/api_test/tabs/smoke/worker.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function testCreateTab() {
+  chrome.tabs.create({url: 'about:blank'}, function(tab) {
+    chrome.test.assertNoLastError();
+    // Check that some tab properties are set. The exact values are not
+    // important, just ensure they are valid.
+    chrome.test.assertTrue(tab.id >= 0, 'id not valid');
+    chrome.test.assertTrue(tab.windowId >= 0, 'windowId not valid');
+    chrome.test.succeed();
+  });
+}
+
+chrome.test.runTests([testCreateTab]);
