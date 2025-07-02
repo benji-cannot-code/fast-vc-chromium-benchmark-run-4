e977ab9baf308509edba04fa88fadb3a9e3ef43a
Ashutosh Singhal <macinashutosh@google.com>
2025-07-02 05:40:48 -0700

    Introduce KioskAppLevelLogsSaver class
    * Kiosk logs saver handles the logic of saving the logs in the
      kiosk_apps.log file using SYSLOG.
    * Kiosk logs saver only saved a limited log entries controlled
      by the logs manager.
    
    Bug: b:425623334
    Test: KioskAppLevelLogsSaverTest* unit tests.
    Change-Id: Ie5a9541120806474ba94cc9989903f38fdfb804b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6672669
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481533}

--- chrome/browser/chromeos/app_mode/kiosk_app_level_logs_manager.cc
+++ chrome/browser/chromeos/app_mode/kiosk_app_level_logs_manager.cc
@@ -5,12 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/app_mode/kiosk_app_level_logs_manager.h"
 
+#include "chrome/browser/chromeos/app_mode/kiosk_app_level_logs_saver.h"
 #include "chrome/browser/profiles/profile.h"
+#include "third_party/blink/public/mojom/devtools/console_message.mojom-data-view.h"
 
 namespace chromeos {
 
 KioskAppLevelLogsManager::KioskAppLevelLogsManager(Profile* profile) {}
 
 KioskAppLevelLogsManager::~KioskAppLevelLogsManager() = default;
+
+void KioskAppLevelLogsManager::SaveLog(
+    const KioskAppLevelLogsSaver::KioskLogMessage& log) {
+  logs_saver_.SaveLog(log);
+}
 
 }  // namespace chromeos
