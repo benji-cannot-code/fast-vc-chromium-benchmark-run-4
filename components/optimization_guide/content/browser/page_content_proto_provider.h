4bb3168e0555a80eaa41c4a2d3083dc594b09f6d
David Sanders <dsanders11@ucsbalum.com>
2025-07-01 17:31:36 -0700

    Remove some unused includes in page_content_proto_provider.h
    Adds other missing includes to fix the build.
    
    Bug: 40318405
    Change-Id: I8f2ab93ff93a85cea075b37ffec67959f641e6bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697992
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481327}

--- components/optimization_guide/content/browser/page_content_proto_provider.h
+++ components/optimization_guide/content/browser/page_content_proto_provider.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTENT_PROTO_PROVIDER_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTENT_PROTO_PROVIDER_H_
 
+#include <optional>
 #include <string>
-#include <vector>
 
 #include "base/containers/flat_map.h"
 #include "base/functional/callback.h"
-#include "components/optimization_guide/proto/features/model_prototyping.pb.h"
+#include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "content/public/browser/document_user_data.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/weak_document_ptr.h"
