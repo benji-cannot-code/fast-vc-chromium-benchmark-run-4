f083b30fabb5ac7954f8cb07b5a746a4c2a29efd
Andrea Gomez <andreaxg@google.com>
2025-07-02 14:48:49 -0700

    Dynamic settings spark
    Bug: 422275745
    Change-Id: Ied3287616acd99224dc51415a2b44333e6eb0cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695247
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481909}

--- chrome/browser/resources/settings/glic_page/glic_page.ts
+++ chrome/browser/resources/settings/glic_page/glic_page.ts
@@ -147,6 +147,11 @@ export class SettingsGlicPageElement extends SettingsGlicPageElementBase {
         computed: `computeTabAccessLearnMoreUrl_(prefs.${
             SettingsGlicPageFeaturePrefName.USER_STATUS}.value)`,
       },
+
+      spark_: {
+        type: String,
+        computed: `computeSpark_()`,
+      },
     };
   }
 
@@ -175,6 +180,7 @@ export class SettingsGlicPageElement extends SettingsGlicPageElementBase {
   declare private microphoneSubLabel_: string;
   declare private tabAccessSubLabel_: string;
   declare private tabAccessLearnMoreUrl_: string;
+  declare private spark_: string;
 
   override async connectedCallback() {
     super.connectedCallback();
@@ -387,6 +393,12 @@ export class SettingsGlicPageElement extends SettingsGlicPageElementBase {
             userStatus?.isEnterpriseAccountDataProtected ?
         this.i18n('glicTabAccessToggleLearnMoreUrlDataProtected') :
         this.i18n('glicTabAccessToggleLearnMoreUrl');
+  }
+
+  private computeSpark_() {
+    return loadTimeData.getBoolean('glicAssetsV2Enabled') ?
+        'settings-internal:sparkv2' :
+        'settings-internal:spark';
   }
 }
 
