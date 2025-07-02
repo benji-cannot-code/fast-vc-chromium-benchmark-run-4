4ff0250ed50298cef59015c609b4db5c2f7038b0
Charles Hager <clhager@google.com>
2025-07-02 11:39:36 -0700

    Use backup navbar insets in E2ELayout
    The edge-to-edge layout is used to wrap the content view when the
    edge-to-edge-everywhere feature is enabled. This layout relies on the
    window insets to know how much to pad the content view, which causes
    visual issues if the OS sends buggy / inconsistent window insets.
    
    This allows the edge-to-edge layout to get backup insets if the
    navigation bar insets are missing, using the same utility method as the
    EdgeToEdgeController. These backup insets are informed by the other
    window insets, such as the tappableElement or the
    mandatorySystemGestures.
    
    This also coordinates usage of these backup insets between the
    EdgeToEdgeControllerImpl and the EdgeToEdgeLayoutCoordinator.
    
    Gesture nav, using backup insets for missing navbar:
    https://screencast.googleplex.com/cast/NDc2NzQwMTQ5MjA4Njc4NHxjYmMyOTAzNC05Ng
    
    Tappable nav, using backup insets for missing navbar:
    https://screencast.googleplex.com/cast/NTY0NDIzNTM3MzAxOTEzNnw5ZjE1Y2E0ZS03Mw
    
    Bug: 412867399
    Change-Id: I53343b4f1bd463d6889beccd796c2ca6b60f9d1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680592
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481765}

--- components/browser_ui/edge_to_edge/android/java/src/org/chromium/components/browser_ui/edge_to_edge/layout/EdgeToEdgeLayoutViewTest.java
+++ components/browser_ui/edge_to_edge/android/java/src/org/chromium/components/browser_ui/edge_to_edge/layout/EdgeToEdgeLayoutViewTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.browser_ui.edge_to_edge.layout;
 
+import static org.mockito.Mockito.doReturn;
+
 import static org.chromium.base.test.util.Batch.PER_CLASS;
 
 import android.app.Activity;
@@ -22,6 +24,9 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseActivityTestRule;
@@ -29,6 +34,7 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
+import org.chromium.components.browser_ui.edge_to_edge.EdgeToEdgeFieldTrial;
 import org.chromium.components.browser_ui.edge_to_edge.R;
 import org.chromium.ui.test.util.BlankUiTestActivity;
 import org.chromium.ui.test.util.RenderTestRule;
@@ -46,6 +52,8 @@ public class EdgeToEdgeLayoutViewTest {
             new BaseActivityTestRule<>(BlankUiTestActivity.class);
 
     private static Activity sActivity;
+
+    @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     @Rule
     public RenderTestRule mRenderTestRule =
@@ -65,6 +73,8 @@ public class EdgeToEdgeLayoutViewTest {
     private static final int NAV_BAR_DIVIDER_COLOR = Color.BLUE;
     private static final int BG_COLOR = Color.GRAY;
 
+    @Mock private EdgeToEdgeFieldTrial mUseBackupNavbarInsetsFieldTrial;
+
     private EdgeToEdgeLayoutCoordinator mEdgeToEdgeLayoutCoordinator;
     private FrameLayout mContentView;
     private View mEdgeToEdgeLayout;
@@ -78,7 +88,18 @@ public class EdgeToEdgeLayoutViewTest {
     public void setUp() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    mEdgeToEdgeLayoutCoordinator = new EdgeToEdgeLayoutCoordinator(sActivity, null);
+                    doReturn(true)
+                            .when(mUseBackupNavbarInsetsFieldTrial)
+                            .isEnabledForManufacturerVersion();
+
+                    mEdgeToEdgeLayoutCoordinator =
+                            new EdgeToEdgeLayoutCoordinator(
+                                    sActivity,
+                                    null,
+                                    /* useBackupNavbarInsetsEnabled= */ true,
+                                    /* useBackupNavbarInsetsFieldTrial= */ mUseBackupNavbarInsetsFieldTrial,
+                                    /* canUseTappableElementInsets= */ true,
+                                    /* canUseMandatoryGesturesInsets= */ true);
 
                     mContentView = new FrameLayout(sActivity, null);
                     sActivity.setContentView(
