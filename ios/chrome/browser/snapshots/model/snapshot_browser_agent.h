a91c2ead57bf56cb3b445d351f4b28b640a2b2d2
Sylvain Defresne <sdefresne@chromium.org>
2025-07-02 03:14:26 -0700

    [ios] Expose a method to fetch snapshot of unrealized WebStates
    When CreateTabHelperOnlyForRealizedWebStates() is defined, TabHelpers
    will not be created for unrealized WebStates.
    
    Add a method to SnapshotBrowserAgent allowing to retrieve the snapshot
    for unrealized WebStates without using the SnapshotTabHelper (though
    it will require knowing which Browser owns a given WebState, which is
    not a problem since snapshot are presented by the TabGrid which has
    that information).
    
    Bug: 422362996
    Change-Id: I2d47d66e7b34f82324f30c4007c8d68e6af82ecc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696649
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481494}

--- ios/chrome/browser/snapshots/model/snapshot_browser_agent.h
+++ ios/chrome/browser/snapshots/model/snapshot_browser_agent.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_SNAPSHOTS_MODEL_SNAPSHOT_BROWSER_AGENT_H_
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #import <string>
 #import <vector>
@@ -16,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser_user_data.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.h"
+#import "ios/chrome/browser/snapshots/model/snapshot_types.h"
 
-@class SnapshotIDWrapper;
 @protocol SnapshotStorage;
 
 // Associates a SnapshotStorage to a Browser.
@@ -42,6 +43,12 @@ class SnapshotBrowserAgent : public BrowserUserData<SnapshotBrowserAgent>,
   void RemoveAllSnapshots();
 
   id<SnapshotStorage> snapshot_storage() { return snapshot_storage_; }
+
+  // Retrieves snapshot of `snapshot_kind` for `snapshot_id` invoking
+  // `completion` with the image retrieved (or nil in case of failure).
+  void RetrieveSnapshotWithID(SnapshotID snapshot_id,
+                              SnapshotKind snapshot_kind,
+                              SnapshotRetrievedBlock completion);
 
  private:
   friend class BrowserUserData<SnapshotBrowserAgent>;
@@ -72,9 +79,6 @@ class SnapshotBrowserAgent : public BrowserUserData<SnapshotBrowserAgent>,
 
   // Purges the snapshots folder of unused snapshots.
   void PurgeUnusedSnapshots();
-
-  // Returns the snapshot IDs of all the WebStates in the Browser.
-  NSArray<SnapshotIDWrapper*>* GetSnapshotIDs();
 
   __strong id<SnapshotStorage> snapshot_storage_;
 
