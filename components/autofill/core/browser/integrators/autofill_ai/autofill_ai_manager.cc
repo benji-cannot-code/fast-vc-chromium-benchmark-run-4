1a6c9774ef95898e3ce9835c7f3dd6988b95ed69
Jan Keitel <jkeitel@google.com>
2025-07-02 08:14:16 -0700

    Avoid redundant code in AutofillAiManager
    No functionality change - just rename a helper and use it in another
    place.
    
    Bug: 429109328
    Change-Id: I1f24fe548c4b4ad8f2e534d6502300d46f214512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694765
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481626}

--- components/autofill/core/browser/integrators/autofill_ai/autofill_ai_manager.cc
+++ components/autofill/core/browser/integrators/autofill_ai/autofill_ai_manager.cc
@@ -174,7 +174,7 @@ std::vector<std::string> GetAttributeStrikeKeys(const EntityInstance& entity,
   return base::ToVector(entity.type().strike_keys(), value_for_strike_key);
 }
 
-bool IsFormEligibleForFilling(const FormStructure& form) {
+bool IsFormRelevantForAutofillAi(const FormStructure& form) {
   return std::ranges::any_of(
       form.fields(), [](const std::unique_ptr<AutofillField>& field) {
         return field->GetAutofillAiServerTypePredictions().has_value();
@@ -206,7 +206,7 @@ void AutofillAiManager::OnSuggestionsShown(const FormStructure& form,
 }
 
 void AutofillAiManager::OnFormSeen(const FormStructure& form) {
-  bool is_eligible = IsFormEligibleForFilling(form);
+  bool is_eligible = IsFormRelevantForAutofillAi(form);
   logger_.OnFormEligibilityAvailable(form.global_id(), is_eligible);
   if (!is_eligible) {
     return;
@@ -250,10 +250,7 @@ void AutofillAiManager::OnEditedAutofilledField(const FormStructure& form,
 
 bool AutofillAiManager::OnFormSubmitted(const FormStructure& form,
                                         ukm::SourceId ukm_source_id) {
-  if (std::ranges::any_of(
-          form.fields(), [](const std::unique_ptr<AutofillField>& field) {
-            return field->GetAutofillAiServerTypePredictions() != std::nullopt;
-          })) {
+  if (IsFormRelevantForAutofillAi(form)) {
     logger_.RecordFormMetrics(form, ukm_source_id, /*submission_state=*/true,
                               GetAutofillAiOptInStatus(*client_));
   }
