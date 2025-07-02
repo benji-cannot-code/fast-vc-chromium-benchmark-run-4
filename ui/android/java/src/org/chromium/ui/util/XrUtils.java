7c90f90d9e81f5b22a72887a15a20f182ea28aa1
Alexander Cooper <alcooper@chromium.org>
2025-07-02 13:55:03 -0700

    Remove android.software.xr.immersive feature check
    The android.software.xr.immersive feature has been deprecated for some
    time now. It was left in and checked alongside the new feature in order
    to ensure backwards compatibility. At this time, the deprecated feature
    should no longer be needed. As such, this change removes the old
    deprecated feature.
    
    Bug: 398275913
    Change-Id: Ib192e4a9e7942c9dada2ffc8064d7786b194b3e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6665238
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481868}

--- ui/android/java/src/org/chromium/ui/util/XrUtils.java
+++ ui/android/java/src/org/chromium/ui/util/XrUtils.java
@@ -29,7 +29,6 @@ public class XrUtils {
     }
 
     private static boolean isXrDeviceInternal() {
-        return PackageManagerUtils.hasSystemFeature(PackageManagerUtils.XR_IMMERSIVE_FEATURE_NAME)
-                || PackageManagerUtils.hasSystemFeature(PackageManagerUtils.XR_OPENXR_FEATURE_NAME);
+        return PackageManagerUtils.hasSystemFeature(PackageManagerUtils.XR_OPENXR_FEATURE_NAME);
     }
 }
