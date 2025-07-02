7d3a31b3b038725a37d5e8714013cd5bcd2d756b
Vasiliy Telezhnikov <vasilyt@chromium.org>
2025-07-02 06:33:26 -0700

    Don't set ycbcr on the VideoFrame for fuchsia hardware decoder
    If it's not set, gpu service will create it based on other metadata [1].
    It's essentially the same code as in the decoder.
    
    Format will be derived from SharedImageFormat via
    ToVkFormatExternalSampler() [2][3]. Conversion matches current code [4].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/skia_utils.cc;drc=e65323be17a0ba06198d18b0a8bf5ae594e10348;l=444
    [2] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/shared_image/external_vk_image_backing.cc;drc=ee862142a057e9bf45f2f7d0fc30345980893e1a;l=294
    [3] https://source.chromium.org/chromium/chromium/src/+/main:components/viz/service/display_embedder/skia_output_surface_impl.cc;drc=e65323be17a0ba06198d18b0a8bf5ae594e10348;l=1497
    [4] https://source.chromium.org/chromium/chromium/src/+/main:media/fuchsia/video/fuchsia_video_decoder.cc;drc=e65323be17a0ba06198d18b0a8bf5ae594e10348;l=565
    
    Bug: 40282154
    Change-Id: I0c3f1e3f5b744af8c39c8a3956c21ce709c0fd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681691
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481559}

--- gpu/vulkan/vulkan_image.cc
+++ gpu/vulkan/vulkan_image.cc
@@ -100,7 +100,6 @@ std::unique_ptr<VulkanImage> VulkanImage::Create(
     VkImageTiling image_tiling,
     VkDeviceSize device_size,
     uint32_t memory_type_index,
-    std::optional<VulkanYCbCrInfo>& ycbcr_info,
     VkImageUsageFlags usage,
     VkImageCreateFlags flags) {
   auto image = std::make_unique<VulkanImage>(base::PassKey<VulkanImage>());
@@ -112,7 +111,6 @@ std::unique_ptr<VulkanImage> VulkanImage::Create(
                                 static_cast<uint32_t>(size.height()), 1};
   image->create_info_.format = format;
   image->create_info_.tiling = image_tiling;
-  image->ycbcr_info_ = ycbcr_info;
   image->create_info_.usage = usage;
   image->create_info_.flags = flags;
   return image;
