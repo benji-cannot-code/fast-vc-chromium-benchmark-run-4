6e286af31640b2a608857c963fb0be9c92d2a6ae
Teo Lamort de Gail <lamort@google.com>
2025-07-02 01:22:47 -0700

    [autofill_ml] Connect model handler to log router
    At this point, we can check that the placeholder ML log objects are
    making their way to the frontend.
    
    Page: https://screenshot.googleplex.com/69mGnXgyfJfmiwQ
    
    Console: https://screenshot.googleplex.com/3HQni6JTeV7uvKc
    
    Bug: 428686605
    
    Change-Id: I1526013b318057456ff1652166bad5fe8bb3b53d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651558
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Teo Lamort de Gail <lamort@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481456}

--- components/autofill/core/browser/ml_model/field_classification_model_handler.h
+++ components/autofill/core/browser/ml_model/field_classification_model_handler.h
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/lru_cache.h"
 #include "base/functional/callback_forward.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/ml_model/field_classification_model_encoder.h"
+#include "components/autofill/core/browser/ml_model/logging/ml_log_router.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/core/delivery/optimization_guide_model_provider.h"
 #include "components/optimization_guide/core/inference/model_handler.h"
@@ -40,7 +42,8 @@ class FieldClassificationModelHandler
 
   FieldClassificationModelHandler(
       optimization_guide::OptimizationGuideModelProvider* model_provider,
-      optimization_guide::proto::OptimizationTarget optimization_target);
+      optimization_guide::proto::OptimizationTarget optimization_target,
+      autofill::MLLogRouter* log_router = nullptr);
   ~FieldClassificationModelHandler() override;
 
   // This function asynchronously queries predictions for the `form_structure`
@@ -132,6 +135,8 @@ class FieldClassificationModelHandler
 
   // Cached model classifications.
   base::LRUCache<ModelInputHash, std::vector<FieldType>> predictions_cache_;
+
+  raw_ptr<autofill::MLLogRouter> log_router_ = nullptr;
 
   base::WeakPtrFactory<FieldClassificationModelHandler> weak_ptr_factory_{this};
 };
