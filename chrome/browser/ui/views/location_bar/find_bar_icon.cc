044653a7b0bba7d5421b4d09626ec154922c125f
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-07-02 12:54:22 -0700

    Add element identifier to the find bar icon for testability
    Bug: 376283618
    Change-Id: Ibeb8d282a599cb8e9010c58109a5397ae09b776a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6690960
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481827}

--- chrome/browser/ui/views/location_bar/find_bar_icon.cc
+++ chrome/browser/ui/views/location_bar/find_bar_icon.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/ink_drop.h"
+#include "ui/views/view_class_properties.h"
+
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(FindBarIcon, kElementId);
 
 FindBarIcon::FindBarIcon(
     Browser* browser,
@@ -27,6 +30,7 @@ FindBarIcon::FindBarIcon(
                          "Find"),
       browser_(browser) {
   DCHECK(browser_);
+  SetProperty(views::kElementIdentifierKey, kElementId);
   GetViewAccessibility().SetName(l10n_util::GetStringUTF16(IDS_TOOLTIP_FIND));
 }
 
