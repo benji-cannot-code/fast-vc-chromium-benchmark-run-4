7208da4db8b2e98e347ddd166759d13030ec8733
Florian Leimgruber <fleimgruber@google.com>
2025-07-02 02:07:23 -0700

    Deprecate import logic for incomplete profiles
    Upon form submission, FormDataImporter extracts an AutofillProfile from
    the submitted FormStructure. If at least one profile is found,
    AutofillProfileImportProcess is asked to determine the import type, by
    looking at existing profiles, strikes, etc. This is represented by
    AutofillProfileImportType.
    
    After FormDataImporter extracted a profile, it determines if the profile
    meets Autofill's completeness requirements [1]. This CL's main
    observation is that incomplete profiles are dropped. Yet, a few lines
    after the early return, ExtractedAddressProfile has a
    all_requirements_fulfilled property, that consequently is always true.
    This CL removes all dead code around incomplete imports.
    
    In the import process the import types kSilentUpdateForIncompleteProfile
    and kUnusableIncompleteProfile are meant to deal with incomplete
    imports. According to metrics [2], both are dead. However, looking at
    the code, it appears that they can be triggered through a rarely
    occurring special case:
    - If FormDataImporter cannot show a prompt (e.g. because a credit card
      import is possible), it attempts to perform silent updates only [3].
      In this case, kNewProfile/kConfirmableMerge import types are converted
      to kUnusableIncompleteProfile/kSilentUpdateForIncompleteProfile [4].
      This seems semantically wrong.
    To remove the two incomplete profile import types, this CL instead
    treats the case above as kSuppressedNewProfile and
    kSuppressedConfirmableMerge.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/form_import/form_data_importer.cc;l=715-716,740-742;drc=43cfaa57fbbef87d4daa61e8c707326dea4efe7c
    [2] https://uma.googleplex.com/p/chrome/timeline_v2?sid=51c6e1a3404976e1ebbedfa974b4d47b
    [3] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/form_import/form_data_importer.cc;l=784,802;drc=43cfaa57fbbef87d4daa61e8c707326dea4efe7c
    [4] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/form_import/addresses/autofill_profile_import_process.cc;l=219,235-239;drc=30382c187a4dadbd3624660808311c4da276f888
    
    Bug: 354706653, 40100455
    Change-Id: Ie73719ea91b0c0a5b5e8ad303724c2bedcbbd95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6656471
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481468}

--- components/autofill/core/browser/form_import/addresses/address_profile_save_manager.cc
+++ components/autofill/core/browser/form_import/addresses/address_profile_save_manager.cc
@@ -70,11 +70,9 @@ void AddressProfileSaveManager::MaybeOfferSavePrompt(
     // process without initiating a user prompt
     case AutofillProfileImportType::kDuplicateImport:
     case AutofillProfileImportType::kSilentUpdate:
-    case AutofillProfileImportType::kSilentUpdateForIncompleteProfile:
     case AutofillProfileImportType::kSuppressedNewProfile:
     case AutofillProfileImportType::kSuppressedConfirmableMergeAndSilentUpdate:
     case AutofillProfileImportType::kSuppressedConfirmableMerge:
-    case AutofillProfileImportType::kUnusableIncompleteProfile:
       import_process->AcceptWithoutPrompt();
       FinalizeProfileImport(std::move(import_process));
       return;
