0c2a6d8146d3454c9be4952d563c40adbbd01cb5
valerie young <spectranaut@igalia.com>
2025-07-02 09:43:18 -0700

    [ChromeVox] ChromeVoxBackgroundTtsTest fix for new PrimaryTts
    Bug: 388867840
    Change-Id: I9ab3dc384c4a1d5efdd7b3bb9c6c54b554720f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695970
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1481681}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/background/tts_background_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/background/tts_background_test.js
@@ -13,7 +13,7 @@ ChromeVoxBackgroundTtsTest = class extends ChromeVoxE2ETest {
   /** @override */
   async setUpDeferred() {
     await super.setUpDeferred();
-    globalThis.tts = new PrimaryTts();
+    globalThis.tts = new PrimaryTts(/*skipOnVoicesHandlerForTesting=*/ true);
   }
 
   expectUtteranceQueueIsLike(expectedObjects) {
