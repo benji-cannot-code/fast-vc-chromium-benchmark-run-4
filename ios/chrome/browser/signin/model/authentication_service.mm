a3f2bf9d99f321a7680d94b67ec984b1ae3ba60f
Tanmoy Mollik <triploblastic@google.com>
2025-07-03 03:48:40 -0700

    [iOS] Remove FireIdentityAccessTokenRefreshFailed
    This cl removes the 2 argument version of
    FireIdentityAccessTokenRefreshFailed.
    
    This cl also plumbs scopes argument from
    FireIdentityAccessTokenRefreshFailed all the way to authentication
    service. In a follow-up cl the scopes argument will be used to filter
    mdm errors for allowlisted scopes.
    
    Bug: 350461111
    Change-Id: I11eae902fb516faed3134a606f7c4b05a99e0eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667908
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482152}

--- ios/chrome/browser/signin/model/authentication_service.mm
+++ ios/chrome/browser/signin/model/authentication_service.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/signin/ios/browser/features.h"
 #import "components/signin/public/base/gaia_id_hash.h"
 #import "components/signin/public/base/signin_pref_names.h"
+#import "components/signin/public/base/signin_switches.h"
 #import "components/signin/public/identity_manager/account_info.h"
 #import "components/signin/public/identity_manager/device_accounts_synchronizer.h"
 #import "components/signin/public/identity_manager/primary_account_mutator.h"
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/features/system_flags.h"
 #import "ios/chrome/browser/signin/model/authentication_service_delegate.h"
 #import "ios/chrome/browser/signin/model/authentication_service_observer.h"
+#import "ios/chrome/browser/signin/model/mdm_allowlisted_scopes.h"
 #import "ios/chrome/browser/signin/model/refresh_access_token_error.h"
 #import "ios/chrome/browser/signin/model/signin_util.h"
 #import "ios/chrome/browser/signin/model/system_identity.h"
@@ -645,7 +647,8 @@ void AuthenticationService::OnRefreshTokenUpdated(id<SystemIdentity> identity) {
 
 void AuthenticationService::OnAccessTokenRefreshFailed(
     id<SystemIdentity> identity,
-    id<RefreshAccessTokenError> error) {
+    id<RefreshAccessTokenError> error,
+    const std::set<std::string>& scopes) {
   if (!identity) {
     DLOG(ERROR)
         << "Unexpected call of OnAccessTokenRefreshFailed with null identity";
