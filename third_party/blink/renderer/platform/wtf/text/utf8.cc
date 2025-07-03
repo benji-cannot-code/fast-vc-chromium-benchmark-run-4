c399e7cdb715f08a223a95733b9adeb9b4b6c08c
Takuto Ikuta <tikuta@chromium.org>
2025-07-03 02:02:42 -0700

    blink: Add <array> include for error in modules build
    This is to fix
    https://ci.chromium.org/ui/p/chromium/builders/try/linux_chromium_asan_rel_ng/2402862/overview
    
    Bug: 40263312
    Change-Id: I620c5253c8636672d0fb0f50e028b0b09a669a86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702557
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482111}

--- third_party/blink/renderer/platform/wtf/text/utf8.cc
+++ third_party/blink/renderer/platform/wtf/text/utf8.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <unicode/utf16.h>
 
+#include <array>
+
 #include "base/check.h"
 #include "base/notreached.h"
 #include "third_party/blink/renderer/platform/wtf/text/ascii_ctype.h"
