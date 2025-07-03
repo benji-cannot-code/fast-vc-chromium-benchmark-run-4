68c91deacb60391b0e5ba4e2f5f6c9d094f40e8e
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-07-03 14:15:07 -0700

    Inject page action controller in the price insight controller
    Tab features that depend on others tab features should not call
    `GetFeatures`, instead, they should inject it.
    
    This CL updates the price insights page action controller by injecting
    the page action controller.
    
    Change-Id: I06c00b6b87656b4a79a37a206bdaeccea2556013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6704504
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482384}

--- chrome/browser/ui/views/commerce/price_insights_page_action_view_controller.cc
+++ chrome/browser/ui/views/commerce/price_insights_page_action_view_controller.cc
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace commerce {
 
 PriceInsightsPageActionViewController::PriceInsightsPageActionViewController(
-    tabs::TabInterface& tab_interface)
-    : tab_interface_(tab_interface) {
+    tabs::TabInterface& tab_interface,
+    page_actions::PageActionController& page_action_controller)
+    : tab_interface_(tab_interface),
+      page_action_controller_(page_action_controller) {
   CHECK(IsPageActionMigrated(PageActionIconType::kPriceInsights));
 }
 
@@ -28,20 +30,16 @@ void PriceInsightsPageActionViewController::UpdatePageActionIcon(
     bool should_shown_icon,
     bool should_expand_icon,
     PriceInsightsIconLabelType label_type) {
-  page_actions::PageActionController* page_action_controller =
-      tab_interface_->GetTabFeatures()->page_action_controller();
-  CHECK(page_action_controller);
-
   if (!should_shown_icon) {
     // Suggestion chip may be previously shown, ensure that the state is
     // cleared.
-    page_action_controller->HideSuggestionChip(kActionCommercePriceInsights);
-    page_action_controller->Hide(kActionCommercePriceInsights);
+    page_action_controller_->HideSuggestionChip(kActionCommercePriceInsights);
+    page_action_controller_->Hide(kActionCommercePriceInsights);
     scoped_window_call_to_action_ptr_.reset();
     return;
   }
 
-  page_action_controller->Show(kActionCommercePriceInsights);
+  page_action_controller_->Show(kActionCommercePriceInsights);
 
   if (!should_expand_icon) {
     return;
@@ -56,22 +54,22 @@ void PriceInsightsPageActionViewController::UpdatePageActionIcon(
 
   switch (label_type) {
     case PriceInsightsIconLabelType::kPriceIsLow:
-      page_action_controller->OverrideText(
+      page_action_controller_->OverrideText(
           kActionCommercePriceInsights,
           l10n_util::GetStringUTF16(
               IDS_SHOPPING_INSIGHTS_ICON_EXPANDED_TEXT_LOW_PRICE));
       break;
     case PriceInsightsIconLabelType::kPriceIsHigh:
-      page_action_controller->OverrideText(
+      page_action_controller_->OverrideText(
           kActionCommercePriceInsights,
           l10n_util::GetStringUTF16(
               IDS_SHOPPING_INSIGHTS_ICON_EXPANDED_TEXT_HIGH_PRICE));
       break;
     default:
-      page_action_controller->ClearOverrideText(kActionCommercePriceInsights);
+      page_action_controller_->ClearOverrideText(kActionCommercePriceInsights);
   }
 
-  page_action_controller->ShowSuggestionChip(kActionCommercePriceInsights);
+  page_action_controller_->ShowSuggestionChip(kActionCommercePriceInsights);
 }
 
 }  // namespace commerce
--- chrome/browser/ui/views/commerce/price_insights_page_action_view_controller.h
+++ chrome/browser/ui/views/commerce/price_insights_page_action_view_controller.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class ScopedWindowCallToAction;
 
+namespace page_actions {
+class PageActionController;
+}
+
 namespace tabs {
 class TabInterface;
 }
@@ -26,7 +30,9 @@ namespace commerce {
 class PriceInsightsPageActionViewController {
  public:
   explicit PriceInsightsPageActionViewController(
-      tabs::TabInterface& tab_interface);
+      tabs::TabInterface& tab_interface,
+      page_actions::PageActionController& page_action_controller);
+
   PriceInsightsPageActionViewController(
       const PriceInsightsPageActionViewController&) = delete;
   PriceInsightsPageActionViewController& operator=(
@@ -46,6 +52,10 @@ class PriceInsightsPageActionViewController {
   // Reference to the tab interface, which provides access to tab-specific
   // features.
   const raw_ref<tabs::TabInterface> tab_interface_;
+
+  // Unowned reference to the page action controller that will coordinate
+  // requests from this object.
+  const raw_ref<page_actions::PageActionController> page_action_controller_;
 
   std::unique_ptr<ScopedWindowCallToAction> scoped_window_call_to_action_ptr_;
 };
