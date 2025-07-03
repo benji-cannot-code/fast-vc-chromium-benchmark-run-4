5ee31e4aa005e1d99647593f44818bae77a0430e
Takuto Ikuta <tikuta@chromium.org>
2025-07-03 03:10:27 -0700

    media: Add <array> include for error in modules build
    This is to fix
    https://ci.chromium.org/ui/p/chromium/builders/try/linux_chromium_tsan_rel_ng/2391862/overview
    
    Bug: 40263312
    Change-Id: Ic0bd16e1ba59bc6ae92bde1d00f66dc702d7cd58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701564
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482139}

--- media/base/picture_in_picture_events_info.cc
+++ media/base/picture_in_picture_events_info.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/picture_in_picture_events_info.h"
 
+#include <array>
 #include <string>
 
 #include "base/notreached.h"
