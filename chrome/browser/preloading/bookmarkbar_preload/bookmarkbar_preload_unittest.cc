a187a26774c5e18d4a3f4e55374d8ddee39c2792
Robert Lin <robertlin@chromium.org>
2025-07-02 20:52:26 -0700

    Add a proper TearDown to BookmarkBarPreloadPipelineManagerTest
    This CL adds a TearDown function to
    BookmarkBarPreloadPipelineManagerTest in
    c/b/p/b/bookmarkbar_preload_unittest.cc
    
    Bug: 413259638
    Change-Id: I522b96a59eeee0cff283a5d3aa06a2ec577bbed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697044
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482030}

--- chrome/browser/preloading/bookmarkbar_preload/bookmarkbar_preload_unittest.cc
+++ chrome/browser/preloading/bookmarkbar_preload/bookmarkbar_preload_unittest.cc
@@ -60,6 +60,8 @@ class BookmarkBarPreloadPipelineManagerTest
                   "&oq=" + original_query);
   }
 
+  void TearDown() override { ChromeRenderViewHostTestHarness::TearDown(); }
+
  protected:
   GURL GetUrl(const std::string& path) { return test_server_.GetURL(path); }
 
