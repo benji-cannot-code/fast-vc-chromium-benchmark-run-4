1abc91ab444f03f55d77285810249885725b2ea3
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-07-02 21:55:50 -0700

    [Page action] migrate/delete find
    Bug: 376283618
    Change-Id: Ib330513fa6e491886db57e95648b559d3a1ce487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6690962
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482039}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1499,7 +1499,10 @@ void Browser::FullscreenTopUIStateChanged() {
 }
 
 void Browser::OnFindBarVisibilityChanged() {
-  window()->UpdatePageActionIcon(PageActionIconType::kFind);
+  if (!IsPageActionMigrated(PageActionIconType::kFind)) {
+    window()->UpdatePageActionIcon(PageActionIconType::kFind);
+  }
+
   command_controller_->FindBarVisibilityChanged();
 }
 
--- chrome/browser/ui/page_action/page_action_icon_type.cc
+++ chrome/browser/ui/page_action/page_action_icon_type.cc
@@ -41,6 +41,8 @@ const base::FeatureParam<bool>* GetPageActionMigrationParam(
       return &features::kPageActionsMigrationCookieControls;
     case PageActionIconType::kAutofillAddress:
       return &features::kPageActionsMigrationAutofillAddress;
+    case PageActionIconType::kFind:
+      return &features::kPageActionsMigrationFind;
     default:
       return nullptr;
   }
