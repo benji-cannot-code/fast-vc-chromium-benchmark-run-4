1f0a0124ebba0ba19f598d2502ed27679a21dbd4
Yang Guo <yangguo@chromium.org>
2025-07-03 02:02:25 -0700

    Handle devtools:// URLs based on policy
    Fixed: 407099042
    Change-Id: I93162b7e6207ddfe85be048c95884228fc0e525b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694991
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482110}

--- chrome/browser/ui/webui/devtools/devtools_ui.cc
+++ chrome/browser/ui/webui/devtools/devtools_ui.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "chrome/browser/devtools/devtools_availability_checker.h"
 #include "chrome/browser/devtools/url_constants.h"
 #include "chrome/browser/ui/webui/devtools/devtools_ui_data_source.h"
 #include "chrome/common/chrome_switches.h"
@@ -68,13 +69,16 @@ bool DevToolsUI::IsFrontendResourceURL(const GURL& url) {
 DevToolsUI::DevToolsUI(content::WebUI* web_ui)
     : WebUIController(web_ui), bindings_(web_ui->GetWebContents()) {
   web_ui->SetBindings(content::BindingsPolicySet());
-  auto factory = web_ui->GetWebContents()
-                     ->GetBrowserContext()
-                     ->GetDefaultStoragePartition()
-                     ->GetURLLoaderFactoryForBrowserProcess();
-  content::URLDataSource::Add(
-      web_ui->GetWebContents()->GetBrowserContext(),
-      std::make_unique<DevToolsDataSource>(std::move(factory)));
+  content::BrowserContext* browser_context =
+      web_ui->GetWebContents()->GetBrowserContext();
+  if (IsInspectionAllowed(Profile::FromBrowserContext(browser_context),
+                          static_cast<content::WebContents*>(nullptr))) {
+    auto factory = browser_context->GetDefaultStoragePartition()
+                       ->GetURLLoaderFactoryForBrowserProcess();
+    content::URLDataSource::Add(
+        browser_context,
+        std::make_unique<DevToolsDataSource>(std::move(factory)));
+  }
 }
 
 DevToolsUI::~DevToolsUI() = default;
