9f28d1bbc51832f5d96e484bc259bc94ffad4bd1
Rafal Godlewski <rgod@google.com>
2025-07-03 04:08:51 -0700

    [PasswordChange] Add HaTS for successful change
    Adds config for a survey that should be triggered when password change
    flow ends with success.
    
    There will be 1 or 2 more surveys for different results of password
    change flow added in follow-up CLs. Some product-specific data passed
    along is now filled with empty placeholders, will also be set in
    follow-ups (left TODOs).
    
    HaTS plan is linked in the bug.
    
    Bug: 383079813
    Change-Id: Ic18288e78f2d63a5c0fa1f614ee33daad97f7e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685956
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482159}

--- chrome/browser/ui/hats/survey_config.cc
+++ chrome/browser/ui/hats/survey_config.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/download/download_warning_desktop_hats_utils.h"
 #include "components/password_manager/core/browser/features/password_features.h"  // nogncheck
+#include "components/password_manager/core/browser/features/password_manager_features_util.h"  // nogncheck
 #include "components/performance_manager/public/features.h"  // nogncheck
 #include "components/permissions/constants.h"                // nogncheck
 #include "components/safe_browsing/core/common/features.h"   // nogncheck
@@ -77,6 +78,8 @@ constexpr char kHatsSurveyTriggerLensOverlayResults[] = "lens-overlay-results";
 constexpr char kHatsSurveyTriggerNtpModules[] = "ntp-modules";
 constexpr char kHatsSurveyTriggerNtpPhotosModuleOptOut[] =
     "ntp-photos-module-opt-out";
+constexpr char kHatsSurveyTriggerPasswordChangeSuccess[] =
+    "password-change-success";
 constexpr char kHatsSurveyTriggerPerformanceControlsPPM[] = "performance-ppm";
 // The permission prompt trigger permits configuring multiple triggers
 // simultaneously. Each trigger increments a counter at the end -->
@@ -633,6 +636,22 @@ std::vector<hats::SurveyConfig> GetAllSurveyConfigs() {
   merchant_trust_learn_survey_config.probability =
       page_info::kMerchantTrustLearnSurveyProbability.Get();
   survey_configs.push_back(merchant_trust_learn_survey_config);
+
+  // Automated password change surveys.
+  survey_configs.emplace_back(
+      &password_manager::features::kImprovedPasswordChangeService,
+      kHatsSurveyTriggerPasswordChangeSuccess,
+      password_manager::features::kPasswordChangeSuccessSurveyTriggerId.Get(),
+      /*product_specific_bits_data_fields=*/
+      std::vector<std::string>{password_manager::features_util::
+                                   kPasswordChangeSuggestedPasswordsAdoption},
+      /*product_specific_string_data_fields=*/
+      std::vector<std::string>{
+          password_manager::features_util::
+              kPasswordChangeBreachedPasswordsCount,
+          password_manager::features_util::kPasswordChangeSavedPasswordsCount,
+          password_manager::features_util::kPasswordChangeRuntime});
+
 #else
   survey_configs.emplace_back(&chrome::android::kChromeSurveyNextAndroid,
                               kHatsSurveyTriggerAndroidStartupSurvey);
