e2648aec1e9f3e3a00535a7da7fcb56a69ddd13c
weidongliu <weidongliu@chromium.org>
2025-07-02 21:34:04 -0700

    Reland "Eliminate allow_unsafe_buffers in //media/audio."
    This is a reland of commit d65a7128bcbb5ff37a71fcfe66e4f7e05520eee6
    
    The revert is due to a logical error. During the conversion,
    `flac_data_.data() + cursor_` was mistakenly written as
    `base::as_byte_span(flac_data_).first()`.
    
    Original change's description:
    > Eliminate allow_unsafe_buffers in //media/audio.
    >
    > Bug: 40284755
    > Change-Id: Iaba7521c232272bda88bcd980311ddd484626fe2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654246
    > Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1481346}
    
    Bug: 40284755
    Change-Id: I6e684b1835dbf689bc89dc9ff8594020c6519076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695455
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482036}

--- media/audio/simple_sources.cc
+++ media/audio/simple_sources.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "media/audio/simple_sources.h"
 
 #include <stddef.h>
@@ -286,7 +281,7 @@ int BeepingSource::OnMoreData(base::TimeDelta /* delay */,
   // Accumulate the time from the last beep.
   interval_from_last_beep_ += base::TimeTicks::Now() - last_callback_time_;
 
-  memset(buffer_.data(), 128, buffer_.size());
+  std::ranges::fill(buffer_, 128);
   bool should_beep = false;
   BeepContext* beep_context = GetBeepContext();
   if (beep_context->automatic_beep()) {
@@ -314,7 +309,7 @@ int BeepingSource::OnMoreData(base::TimeDelta /* delay */,
     size_t position = 0;
     while (position + high_bytes <= buffer_.size()) {
       // Write high values first.
-      memset(buffer_.data() + position, 255, high_bytes);
+      std::ranges::fill(buffer_.subspan(position, high_bytes), 255);
       // Then leave low values in the buffer with |high_bytes|.
       position += high_bytes * 2;
     }
