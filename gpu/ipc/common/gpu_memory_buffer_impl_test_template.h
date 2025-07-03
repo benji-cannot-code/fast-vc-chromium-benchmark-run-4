6542871af0fd0fa00b7005b6168416d6d96a63a7
Colin Blundell <blundell@chromium.org>
2025-07-03 02:33:57 -0700

    [//gpu] Have GMB test template do its own creation of GMBs
    Dupes [1] into gpu_memory_buffer_impl_test_template.h in order to
    facilitate moving [1] into client_shared_image.cc in followup and
    allowing them each to evolve separately as dictated by the ongoing work
    in folding GpuMemoryBuffer into SharedImage.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/ipc/common/gpu_memory_buffer_support.cc;l=225-264?q=GpuMemoryBufferSupport::CreateGpuMemoryBuffer&ss=chromium
    
    Bug: 404905709
    Change-Id: I22705435a6fe12f21a6ee0741a3ef20f6564a1ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694570
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482124}

--- gpu/ipc/common/gpu_memory_buffer_impl_test_template.h
+++ gpu/ipc/common/gpu_memory_buffer_impl_test_template.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
 #include "components/viz/test/test_gpu_service_holder.h"
+#include "gpu/ipc/common/gpu_memory_buffer_impl_shared_memory.h"
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "mojo/public/cpp/base/shared_memory_mojom_traits.h"
 #include "mojo/public/cpp/test_support/test_utils.h"
@@ -41,12 +42,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/ozone_platform.h"
 #endif
 
+#if BUILDFLAG(IS_OZONE)
+#include "gpu/ipc/common/gpu_memory_buffer_impl_native_pixmap.h"
+#include "ui/ozone/public/client_native_pixmap_factory_ozone.h"
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
+#if BUILDFLAG(IS_MAC)
+#include "gpu/ipc/common/gpu_memory_buffer_impl_io_surface.h"
+#endif
+
+#if BUILDFLAG(IS_OZONE)
+#include "gpu/ipc/common/gpu_memory_buffer_impl_native_pixmap.h"
+#include "ui/ozone/public/client_native_pixmap_factory_ozone.h"
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
+#if BUILDFLAG(IS_WIN)
+#include "gpu/ipc/common/gpu_memory_buffer_impl_dxgi.h"
+#endif
+
 namespace gpu {
 
 template <typename GpuMemoryBufferImplType>
 class GpuMemoryBufferImplTest : public testing::Test {
  public:
-  GpuMemoryBufferImplTest() = default;
+  GpuMemoryBufferImplTest() {
+#if BUILDFLAG(IS_OZONE)
+    client_native_pixmap_factory_ = ui::CreateClientNativePixmapFactoryOzone();
+#endif
+  }
 
   GpuMemoryBufferImpl::DestructionCallback CreateGpuMemoryBuffer(
       const gfx::Size& size,
@@ -67,9 +92,29 @@ class GpuMemoryBufferImplTest : public testing::Test {
       gfx::BufferFormat format,
       gfx::BufferUsage usage,
       GpuMemoryBufferImpl::DestructionCallback callback) {
-    return gpu_memory_buffer_support_
-        .CreateGpuMemoryBufferImplFromHandleForTesting(
+    switch (handle.type) {
+      case gfx::SHARED_MEMORY_BUFFER:
+        return GpuMemoryBufferImplSharedMemory::CreateFromHandleForTesting(
             std::move(handle), size, format, usage, std::move(callback));
+#if BUILDFLAG(IS_MAC)
+      case gfx::IO_SURFACE_BUFFER:
+        return GpuMemoryBufferImplIOSurface::CreateFromHandleForTesting(
+            std::move(handle), size, format, usage, std::move(callback));
+#endif
+#if BUILDFLAG(IS_OZONE)
+      case gfx::NATIVE_PIXMAP:
+        return GpuMemoryBufferImplNativePixmap::CreateFromHandleForTesting(
+            client_native_pixmap_factory_.get(), std::move(handle), size,
+            format, usage, std::move(callback));
+#endif
+#if BUILDFLAG(IS_WIN)
+      case gfx::DXGI_SHARED_HANDLE:
+        return GpuMemoryBufferImplDXGI::CreateFromHandleForTesting(
+            std::move(handle), size, format, usage, std::move(callback));
+#endif
+      default:
+        NOTREACHED();
+    }
   }
 
   GpuMemoryBufferSupport* gpu_memory_buffer_support() {
@@ -139,6 +184,9 @@ class GpuMemoryBufferImplTest : public testing::Test {
   bool run_gpu_test_ = false;
   GpuMemoryBufferSupport gpu_memory_buffer_support_;
   raw_ptr<gl::GLDisplay> display_ = nullptr;
+#if BUILDFLAG(IS_OZONE)
+  std::unique_ptr<gfx::ClientNativePixmapFactory> client_native_pixmap_factory_;
+#endif
 
   void FreeGpuMemoryBuffer(base::OnceClosure free_callback, bool* destroyed) {
     std::move(free_callback).Run();
