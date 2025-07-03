3b023c3c950e6bf8a63e0c7fbfec735bba3b2324
Colin Blundell <blundell@chromium.org>
2025-07-03 02:37:59 -0700

    [//media] Remove unneeded include to gpu_memory_buffer.h
    Bug: 40263579
    Change-Id: If5adc127bef5b0ab7258e5d25ca2133b49e869ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695198
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482128}

--- components/chromeos_camera/mjpeg_decode_accelerator_unittest.cc
+++ components/chromeos_camera/mjpeg_decode_accelerator_unittest.cc
@@ -70,8 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
-// TODO(crbug.com/404741060): Eliminate
-#include "ui/gfx/gpu_memory_buffer.h"
+#include "ui/gfx/gpu_memory_buffer_handle.h"
 #include "ui/gfx/native_pixmap_handle.h"
 
 #if BUILDFLAG(USE_VAAPI)
