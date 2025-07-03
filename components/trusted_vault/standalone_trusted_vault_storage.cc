e1e1853854950c93fd874fc8f6488ef1f8976ddd
Stacy Gaikovaia <gaiko@google.com>
2025-07-03 09:53:54 -0700

    components/trusted_vault: start writing sha256
    Writepath changes: Start writing SHA256 alongside MD5. Readpath changes:
    Read MD5 and optionally SHA256 if it's there.
    
    After a couple of milestones, intended change is to stop writing the md5
    but continue reading both. A few milestones after that, stop reading
    md5.
    
    Include `kEnableTrustedVaultSHA256` in case this cl needs to be disabled
    via go/finch-killswitch.
    
    Bug: 425990763
    Change-Id: I3f82186ed4dfe470def1061f983ab0119f898574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702027
    Commit-Queue: Stacy Gaikovaia <gaiko@google.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482320}

--- components/trusted_vault/standalone_trusted_vault_storage.cc
+++ components/trusted_vault/standalone_trusted_vault_storage.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/base64.h"
 #include "base/files/file_util.h"
 #include "base/files/important_file_writer.h"
 #include "base/logging.h"
@@ -14,10 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "components/trusted_vault/features.h"
 #include "components/trusted_vault/proto_string_bytes_conversion.h"
 #include "components/trusted_vault/standalone_trusted_vault_server_constants.h"
 #include "components/trusted_vault/trusted_vault_histograms.h"
 #include "components/trusted_vault/trusted_vault_server_constants.h"
+#include "crypto/hash.h"
 #include "crypto/obsolete/md5.h"
 
 namespace trusted_vault {
@@ -79,6 +82,18 @@ trusted_vault_pb::LocalTrustedVault ReadDataFromDiskImpl(
         security_domain_id,
         TrustedVaultFileReadStatusForUMA::kMD5DigestMismatch);
     return data_proto;
+  }
+
+  if (base::FeatureList::IsEnabled(kEnableTrustedVaultSHA256)) {
+    if (file_proto.has_sha256_digest_hex_string() &&
+        (base::Base64Encode(crypto::hash::Sha256(base::as_byte_span(
+             file_proto.serialized_local_trusted_vault()))) !=
+         file_proto.sha256_digest_hex_string())) {
+      RecordTrustedVaultFileReadStatus(
+          security_domain_id,
+          TrustedVaultFileReadStatusForUMA::kSHA256DigestMismatch);
+      return data_proto;
+    }
   }
 
   if (!data_proto.ParseFromString(
@@ -182,6 +197,11 @@ void WriteDataToDiskImpl(const trusted_vault_pb::LocalTrustedVault& data,
   file_proto.set_serialized_local_trusted_vault(data.SerializeAsString());
   file_proto.set_md5_digest_hex_string(
       MD5StringForTrustedVault(file_proto.serialized_local_trusted_vault()));
+  if (base::FeatureList::IsEnabled(kEnableTrustedVaultSHA256)) {
+    file_proto.set_sha256_digest_hex_string(
+        base::Base64Encode(crypto::hash::Sha256(
+            base::as_byte_span(file_proto.serialized_local_trusted_vault()))));
+  }
   bool success = base::ImportantFileWriter::WriteFileAtomically(
       file_path, file_proto.SerializeAsString(), "TrustedVault");
   if (!success) {
