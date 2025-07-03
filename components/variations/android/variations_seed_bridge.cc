b30d5e401faf8a64172e488ee3f1e2d46877d6fd
Ramon Cano Aparicio <rcanoaparicio@google.com>
2025-07-03 07:12:49 -0700

    Remove unused imports in variations_seed_bridge.
    `variations_seed_bridge.h`: not using anything from jni.
    `variations_seed_bridge.cc`: the used methods are already imported from
    "base/android/jni_android.h"
    
    Bug: 429354807
    Change-Id: I7eb5e3df1e2c5468dd3cbce067a5b9ce3c9c7ac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702632
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482246}

--- components/variations/android/variations_seed_bridge.cc
+++ components/variations/android/variations_seed_bridge.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/variations/android/variations_seed_bridge.h"
 
-#include <jni.h>
-#include <stdint.h>
-
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
--- components/variations/android/variations_seed_bridge.h
+++ components/variations/android/variations_seed_bridge.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VARIATIONS_ANDROID_VARIATIONS_SEED_BRIDGE_H_
 #define COMPONENTS_VARIATIONS_ANDROID_VARIATIONS_SEED_BRIDGE_H_
 
-#include <jni.h>
 #include <stdint.h>
 
 #include <memory>
