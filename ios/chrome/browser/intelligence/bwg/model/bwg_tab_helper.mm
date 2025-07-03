75cbb59da8756e7cfcc1f5559e4787b42a726312
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-07-02 20:54:28 -0700

    [iOS][BWG] Add shouldAnimatePresentation field in BWG configuration
    Added new config bit for presenting the UI with or without animation.
    Also added small check for empty IDs.
    
    Bug: 429250847
    Change-Id: Iaa8d184405ebc94cfe39447d256cb38b5764a6ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701171
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482031}

--- ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
@@ -100,6 +100,10 @@ void BwgTabHelper::WebStateDestroyed(web::WebState* web_state) {
 
 void BwgTabHelper::CreateOrUpdateSessionInPrefs(std::string client_id,
                                                 std::string server_id) {
+  if (client_id.empty() || server_id.empty()) {
+    return;
+  }
+
   base::Value::Dict session_info_dict;
   session_info_dict.Set(kServerIDDictKey, server_id);
   session_info_dict.Set(
@@ -113,6 +117,10 @@ void BwgTabHelper::CreateOrUpdateSessionInPrefs(std::string client_id,
 }
 
 void BwgTabHelper::CleanupSessionFromPrefs(std::string session_id) {
+  if (session_id.empty()) {
+    return;
+  }
+
   ProfileIOS* profile =
       ProfileIOS::FromBrowserState(web_state_->GetBrowserState());
   ScopedDictPrefUpdate update(profile->GetPrefs(), prefs::kBwgSessionMap);
