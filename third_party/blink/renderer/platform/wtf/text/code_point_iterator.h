6960788ceb330020456dfdd4d74713788a6c16cb
Jason <cgqaq@chromium.org>
2025-07-03 04:52:14 -0700

    Remove allow_unsafe_buffers in code_point_iterator.h
    Bug: 351564777
    Change-Id: Id6e7c2e8870bd403f71bef47a72ca867a7e72c6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697176
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482172}

--- third_party/blink/renderer/platform/wtf/text/code_point_iterator.h
+++ third_party/blink/renderer/platform/wtf/text/code_point_iterator.h
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_TEXT_CODE_POINT_ITERATOR_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_TEXT_CODE_POINT_ITERATOR_H_
 
@@ -58,7 +53,12 @@ class CodePointIterator {
     }
 
     // Returns a `end` iterator for `this` iterator.
-    Utf16 EndForThis() const { return Utf16{data_ + length_, 0}; }
+    Utf16 EndForThis() const {
+      // SAFETY: safe if `data_` points to at least `length_` code units, which
+      // should be guaranteed by the constructor or operator++
+      auto* end = UNSAFE_BUFFERS(data_ + length_);
+      return Utf16{end, 0};
+    }
 
     bool operator==(const Utf16& other) const { return data_ == other.data_; }
     bool operator!=(const Utf16& other) const { return !(*this == other); }
@@ -106,7 +106,15 @@ class CodePointIterator {
   }
 
   UChar32 operator*() const { return is_8bit_ ? *Data() : *utf16_; }
-  void operator++() { is_8bit_ ? static_cast<void>(++DataRef()) : ++utf16_; }
+  void operator++() {
+    is_8bit_
+        ? static_cast<void>(
+              // SAFETY: safe to increment as we're not deref, caller
+              // should check the returned value is not equal to
+              // `CodePointIterator::End(<same string>)` before dereferencing.
+              UNSAFE_BUFFERS(++DataRef()))
+        : ++utf16_;
+  }
 
   bool operator==(const CodePointIterator& other) const {
     DCHECK_EQ(is_8bit_, other.is_8bit_);
@@ -137,13 +145,17 @@ inline UChar32 CodePointIterator::Utf16::operator*() const {
   // Get a code point, and cache its length to `code_point_length_`.
   UChar32 ch;
   code_point_length_ = 0;
-  U16_NEXT(data_, code_point_length_, length_, ch);
+  // SAFETY: call into icu functions. Consumes at least one code unit, and
+  // checks against `length_`.
+  UNSAFE_BUFFERS(U16_NEXT(data_, code_point_length_, length_, ch));
   return ch;
 }
 
 inline void CodePointIterator::Utf16::AdvanceByCodeUnits(wtf_size_t by) {
   CHECK_LE(by, length_);
-  data_ += by;
+  // SAFETY: `data_` is safe to increment by `by` as long as `by` <= `length_`,
+  // which is checked at the top of the function.
+  UNSAFE_BUFFERS(data_ += by);
   length_ -= by;
   code_point_length_ = 0;
 }
@@ -151,11 +163,10 @@ inline void CodePointIterator::Utf16::AdvanceByCodeUnits(wtf_size_t by) {
 inline void CodePointIterator::Utf16::operator++() {
   if (!code_point_length_) [[unlikely]] {
     // `code_point_length_` is cached by `operator*()`. If not, compute it.
-    U16_FWD_1(data_, code_point_length_, length_);
+    // SAFETY: call into icu functions.
+    UNSAFE_BUFFERS(U16_FWD_1(data_, code_point_length_, length_););
   }
-  data_ += code_point_length_;
-  length_ -= code_point_length_;
-  code_point_length_ = 0;
+  AdvanceByCodeUnits(code_point_length_);
 }
 
 }  // namespace blink
