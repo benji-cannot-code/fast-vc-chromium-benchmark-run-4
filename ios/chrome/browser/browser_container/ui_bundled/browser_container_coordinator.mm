45fe5fb4fc277ac4bcab64a243f0e4fed19d0d1a
Olivier ROBIN <olivierrobin@google.com>
2025-07-03 06:02:27 -0700

    [IOS] Use IsReaderModeWebStateAvailable in BrowserContainerCoordinator
    Use `IsReaderModeWebStateAvailable` instead of `IsActive` to check for reader mode. This is a more robust way to check for the reader mode state before using it.
    
    Also, update the comment for `ReaderModeTabHelper::IsActive` to clarify that the web state may not be available even when active.
    
    Change-Id: I2f493cee3575ff700b2b323850cc142e68864b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694587
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482207}

--- ios/chrome/browser/browser_container/ui_bundled/browser_container_coordinator.mm
+++ ios/chrome/browser/browser_container/ui_bundled/browser_container_coordinator.mm
@@ -232,12 +232,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Check if there is an alternate webState.
   ReaderModeTabHelper* readerModeTabHelper =
       ReaderModeTabHelper::FromWebState(webState);
-  if (readerModeTabHelper && readerModeTabHelper->IsActive()) {
-    web::WebState* readerModeWebState =
-        readerModeTabHelper->GetReaderModeWebState();
-    if (readerModeWebState) {
-      webState = readerModeWebState;
-    }
+  if (readerModeTabHelper &&
+      readerModeTabHelper->IsReaderModeWebStateAvailable()) {
+    webState = readerModeTabHelper->GetReaderModeWebState();
   }
   [_browserEditMenuHandler buildEditMenuWithBuilder:builder
                                          inWebState:webState];
