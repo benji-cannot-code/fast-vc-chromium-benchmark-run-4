36017aed287b66acc15bef1f260b264488b07531
Jason <cgqaq@chromium.org>
2025-07-03 03:39:00 -0700

    Remove allow_unsafe_buffers in atomic_string.cc
    Bug: 351564777
    Change-Id: I564e68627967668aa885ecfbe878b3e91be60f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696251
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1482148}

--- third_party/blink/renderer/platform/wtf/text/atomic_string.cc
+++ third_party/blink/renderer/platform/wtf/text/atomic_string.cc
@@ -22,11 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 #include "base/numerics/safe_conversions.h"
@@ -56,7 +51,8 @@ AtomicString::AtomicString(base::span<const UChar> chars,
 AtomicString::AtomicString(const UChar* chars)
     : string_(AtomicStringTable::Instance().Add(
           chars,
-          chars ? LengthOfNullTerminatedString(chars) : 0,
+          // SAFETY: safe when `chars` points to a null-terminated cstring.
+          chars ? UNSAFE_BUFFERS(LengthOfNullTerminatedString(chars)) : 0,
           AtomicStringUCharEncoding::kUnknown)) {}
 
 AtomicString::AtomicString(const StringView& string_view)
