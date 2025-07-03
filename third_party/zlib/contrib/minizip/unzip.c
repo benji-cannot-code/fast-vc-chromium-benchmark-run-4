8496d91f5b97178cd92614ef794af7dc5996864d
Hans Wennborg <hans@chromium.org>
2025-07-03 10:40:30 -0700

    [minizip] Fix Unicode Path Extra Field filename length overflow
    If dataSize is too small, fileNameSize would overflow.
    
    Bug: 428744375
    Change-Id: I714fc1e30cb1634c31cb97ce87be225518368e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701714
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482331}

--- third_party/zlib/contrib/minizip/unzip.c
+++ third_party/zlib/contrib/minizip/unzip.c
@@ -1013,7 +1013,15 @@ local int unz64local_GetCurrentFileInfoInternal(unzFile file,
             {
                 int version = 0;
 
-                if (unz64local_getByte(&s->z_filefunc, s->filestream, &version) != UNZ_OK)
+                if (dataSize < 1 + 4)
+                {
+                    /* dataSize includes version (1 byte), uCrc (4 bytes), and
+                     * the filename data. If it's too small, fileNameSize below
+                     * would overflow. */
+                    err = UNZ_ERRNO;
+                    break;
+                }
+                else if (unz64local_getByte(&s->z_filefunc, s->filestream, &version) != UNZ_OK)
                 {
                     err = UNZ_ERRNO;
                 }
@@ -1033,7 +1041,7 @@ local int unz64local_GetCurrentFileInfoInternal(unzFile file,
                         err = UNZ_ERRNO;
                     }
                     uHeaderCrc = crc32(0, (const unsigned char *)szFileName, file_info.size_filename);
-                    fileNameSize = dataSize - (2 * sizeof (short) + 1);
+                    fileNameSize = dataSize - (1 + 4);  /* 1 for version, 4 for uCrc */
                     /* Check CRC against file name in the header. */
                     if (uHeaderCrc != uCrc)
                     {
