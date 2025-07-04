928d4612b1e9a9323309f4e1f35957ff22cc7a33
Mark Cogan <marq@google.com>
2025-07-04 09:39:23 -0700

    [iOS] Update HttpsOnlyModeUpgradeTabHelper to stop observing.
    The tab helper now makes a weak ptr to its observed webstate and
    stops observing in the destructor.
    
    Methods in the .mm are grouped and reordered to match the header.
    
    Fixed: 414684531
    Change-Id: Ib40f2c976842b90257de669efd518685c962e93a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513588
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482662}

--- ios/chrome/browser/https_upgrades/model/https_only_mode_upgrade_tab_helper.h
+++ ios/chrome/browser/https_upgrades/model/https_only_mode_upgrade_tab_helper.h
@@ -74,7 +74,7 @@ class HttpsOnlyModeUpgradeTabHelper
   // allowlisted).
   bool IsHttpAllowedForUrl(const GURL& url) const;
   // Called when the upgrade timer times out.
-  void OnHttpsLoadTimeout(base::WeakPtr<web::WebState> weak_web_state);
+  void OnHttpsLoadTimeout();
   // Stops the current navigation and sets the state so that an upgrade will be
   // started.
   void StopToUpgrade(
@@ -122,6 +122,7 @@ class HttpsOnlyModeUpgradeTabHelper
 
   base::OneShotTimer timer_;
 
+  raw_ptr<web::WebState> web_state_;
   raw_ptr<PrefService> prefs_;
   raw_ptr<PrerenderService> prerender_service_;
   raw_ptr<HttpsUpgradeService> service_;
