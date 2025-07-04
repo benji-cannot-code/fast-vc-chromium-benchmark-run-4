63704077e6bcecbbdb20e58a37703569f1f8d2d5
Xinyi Ji <xinyiji@google.com>
2025-07-04 08:57:11 -0700

    [CustomizationV2] Add NTP appearance bottom sheet (Part 4)
    This CL changes the method used to hide the trailing icon in each section. Instead of setting the icon's visibility to View.GONE, it now uses View.INVISIBLE.
    This change is necessary to prevent the section title from wrapping from one line to two when the icon is displayed.
    The behavior before and after this change is demonstrated in the videos below:
    Before: https://drive.google.com/file/d/1-JJ1DATORNxSj2ctq6IpVz5Qg0yR8arn/view?usp=sharing&resourcekey=0-pPdjNJqkxuLUTGfmxG02rA
    After: https://drive.google.com/file/d/1BiBQqOYUMqjIy2R-_UNSIkDnOrPqc_xw/view?usp=sharing&resourcekey=0-IKofVLTkXDXGgy4SVsbWnA
    
    Bug: 423579377
    Change-Id: I223d5064dcaf6d5039ff9088ba2ef44180b3988e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6704115
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482649}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/NtpThemeListItemViewUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/NtpThemeListItemViewUnitTest.java
@@ -58,6 +58,6 @@ public class NtpThemeListItemViewUnitTest {
 
         mTrailingIcon.setVisibility(View.VISIBLE);
         mNtpThemeListItemView.setTrailingIconVisibility(false);
-        assertEquals(View.GONE, mTrailingIcon.getVisibility());
+        assertEquals(View.INVISIBLE, mTrailingIcon.getVisibility());
     }
 }
