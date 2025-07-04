e276e9d09705bafde92180f7ea6a0185044895ef
Radu Nitescu <radunitescu@google.com>
2025-07-04 05:07:30 -0700

    [iOS][Lens] Cancel Omnibox edit when Lens Overlay is resized
    This CL fixes a bug causing the popup omnibox to remain displayed when
    Lens Overlay is resized to compact.
    
    Fixed: 429279746
    Change-Id: Ieadfbc688c91e27bb8f514d698d2e05a6ad09fa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701745
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482550}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_presentation_environment.h
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_presentation_environment.h
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Notifies the embedder that Lens Overlay is about to disappear.
 - (void)lensOverlayWillDisappear;
 
+// Notifies the embedder that Lens Overlay readjusted its presentation.
+- (void)lensOverlayDidReadjustPresentation;
+
 // Returns the required insets for the Lens Overlay presentation.
 - (NSDirectionalEdgeInsets)presentationInsetsForLensOverlay;
 
