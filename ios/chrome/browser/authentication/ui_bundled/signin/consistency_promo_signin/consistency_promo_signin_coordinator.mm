470c178619b03a6bdaaf785b18fdb348a567947b
Boris Sazonov <bsazonov@chromium.org>
2025-07-04 05:34:37 -0700

    [Signin][iOS] Support metrics recording for explicit reauth flows
    Adds ReauthAccessPoint do identify different entry points for
    explicit reauthentication flow UIs (for example, an error card in
    the account menu) and adds code to record
    Signin.Reauth.InExplicitFlow.{} histograms when a reauth flow started
    from those explicit access points is started (completed, cancelled,
    etc.).
    
    NO_IFTTT=Moved ReauthFlowEvent without any changes
    
    Bug: 391342053, 424251652
    Change-Id: If7a60df0eea0e6a33f09657e9fe9684c4d81fd00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700810
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482560}

--- ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -288,7 +288,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       initWithBaseViewController:self.navigationController
                          browser:self.browser
                          account:account
-                     accessPoint:self.accessPoint];
+               signinAccessPoint:self.accessPoint];
   self.reauthCoordinator.delegate = self;
   [self.reauthCoordinator start];
 }
--- ios/chrome/browser/authentication/ui_bundled/signin/reauth/reauth_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/reauth/reauth_coordinator.mm
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/ui_bundled/signin/reauth/reauth_coordinator.h"
 
 #import <string>
+#import <variant>
 
+#import "absl/functional/overload.h"
 #import "base/logging.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/signin/public/base/signin_metrics.h"
@@ -22,7 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation ReauthCoordinator {
   raw_ptr<Browser> _browser;
   CoreAccountInfo _account;
-  signin_metrics::AccessPoint _accessPoint;
+  std::variant<signin_metrics::ReauthAccessPoint, signin_metrics::AccessPoint>
+      _accessPoint;
   id<SystemIdentityInteractionManager> _identityInteractionManager;
 }
 
@@ -31,8 +34,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
                                    browser:(Browser*)browser
                                    account:(const CoreAccountInfo&)account
-                               accessPoint:
-                                   (signin_metrics::AccessPoint)accessPoint {
+                         reauthAccessPoint:
+                             (signin_metrics::ReauthAccessPoint)accessPoint {
+  self = [super initWithBaseViewController:viewController browser:browser];
+  if (self) {
+    _account = account;
+    _accessPoint = accessPoint;
+  }
+  return self;
+}
+
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+                                   browser:(Browser*)browser
+                                   account:(const CoreAccountInfo&)account
+                         signinAccessPoint:
+                             (signin_metrics::AccessPoint)accessPoint {
   self = [super initWithBaseViewController:viewController browser:browser];
   if (self) {
     _account = account;
@@ -109,9 +125,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)recordReauthFlowEvent:(signin_metrics::ReauthFlowEvent)event {
-  // TODO(crbug.com/391342053): Add logging for reauth flows that aren't started
-  // from a sign-in flow.
-  signin_metrics::RecordReauthFlowEventInSigninFlow(_accessPoint, event);
+  std::visit(absl::Overload{
+                 [event](signin_metrics::ReauthAccessPoint ap) {
+                   signin_metrics::RecordReauthFlowEventInExplicitFlow(ap,
+                                                                       event);
+                 },
+                 [event](signin_metrics::AccessPoint ap) {
+                   signin_metrics::RecordReauthFlowEventInSigninFlow(ap, event);
+                 },
+             },
+             _accessPoint);
 }
 
 @end
