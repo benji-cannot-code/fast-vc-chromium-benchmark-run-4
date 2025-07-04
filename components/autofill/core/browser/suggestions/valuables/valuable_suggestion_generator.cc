0645189a48903cc8a490a37d33e9b87e7b61160c
Norge Vizcay <vizcay@google.com>
2025-07-04 09:46:10 -0700

    Refactor suggestion generation util in browser autofill manager.
    The CL renames the valuables suggestion generator function
    GetLoyaltyCardSuggestions -> GetSuggestionsForLoyaltyCards to match the
    same naming on other generators. It also introduces a
    GetLoyaltyCardSuggestions helper in BrowserAutofillManager.
    
    Bug: 429370505
    Change-Id: I5d523f973a49b3c9d4b054bc6d807f799bb7b9c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700769
    Reviewed-by: Piotr Wrótniak <piotrwrotniak@google.com>
    Commit-Queue: Piotr Wrótniak <piotrwrotniak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482664}

--- components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
@@ -129,7 +129,7 @@ std::vector<Suggestion> GetLoyaltyCardsFooterSuggestions(
 
 }  // namespace
 
-std::vector<Suggestion> GetLoyaltyCardSuggestions(
+std::vector<Suggestion> GetSuggestionsForLoyaltyCards(
     const ValuablesDataManager& valuables_manager,
     const GURL& url,
     bool trigger_field_is_autofilled) {
