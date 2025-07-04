dbb1c99d7b5d3b6531b22d5ace32b9da3489c8da
Guillaume Jenkins <gujen@google.com>
2025-07-04 08:35:01 -0700

    [iOS] Enable the Default Status API check by default
    The Default Status API integration is ready for full launch. This
    enables the RunDefaultStatusCheck base::Feature by default.
    
    Bug: 414434695
    Change-Id: I8b51215a50ae072ca34898eb63081222fb5ea8b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701033
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482643}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1233,7 +1233,7 @@ bool IsNTPBackgroundCustomizationEnabled() {
 
 BASE_FEATURE(kRunDefaultStatusCheck,
              "RunDefaultStatusCheck",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsRunDefaultStatusCheckEnabled() {
   return base::FeatureList::IsEnabled(kRunDefaultStatusCheck);
