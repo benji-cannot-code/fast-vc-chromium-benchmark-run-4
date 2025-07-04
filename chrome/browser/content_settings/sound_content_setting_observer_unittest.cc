030dbe3b016b3502d5e0925caf229ce1276d3922
Jagadesh P <jagadeshjai1999@gmail.com>
2025-07-04 10:59:41 -0700

    Fix dangling pointer in |SoundContentSettingObserverTest|
    Bug: 40212619
    Change-Id: I3899e0bb8c1c57dcf43b604dddc3789bc6bbfae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6508289
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482686}

--- chrome/browser/content_settings/sound_content_setting_observer_unittest.cc
+++ chrome/browser/content_settings/sound_content_setting_observer_unittest.cc
@@ -66,6 +66,11 @@ class SoundContentSettingObserverTest : public ChromeRenderViewHostTestHarness {
     NavigateAndCommit(GURL(kURL1));
   }
 
+  void TearDown() override {
+    host_content_settings_map_ = nullptr;
+    ChromeRenderViewHostTestHarness::TearDown();
+  }
+
  protected:
   void ChangeSoundContentSettingTo(ContentSetting setting) {
     GURL url = web_contents()->GetLastCommittedURL();
@@ -110,7 +115,7 @@ class SoundContentSettingObserverTest : public ChromeRenderViewHostTestHarness {
 #endif
 
  private:
-  raw_ptr<HostContentSettingsMap, DanglingUntriaged> host_content_settings_map_;
+  raw_ptr<HostContentSettingsMap> host_content_settings_map_;
   std::unique_ptr<ukm::TestUkmRecorder> test_ukm_recorder_;
   base::test::ScopedFeatureList scoped_feature_list_;
 };
