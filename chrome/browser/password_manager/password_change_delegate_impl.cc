d00415f2d18ab1f590b3e5354855d23c1b276dae
Rafal Godlewski <rgod@google.com>
2025-07-04 03:56:13 -0700

    [PasswordChange] Add time spent metrics
    Adds logging:
    * time spent on the initial dialog (with/without privacy notice split)
    * time spent on change password toast (i.e. time between finding the
    change password form and the flow outcome)
    
    Bug: 425323880
    Change-Id: Id888cde340a81ec5bce9cf221ff1c5be51da36ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702304
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482535}

--- chrome/browser/password_manager/password_change_delegate_impl.cc
+++ chrome/browser/password_manager/password_change_delegate_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_change_delegate_impl.h"
 
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/strcat.h"
 #include "base/strings/to_string.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -57,6 +58,9 @@ using ::password_manager::BrowserSavePasswordProgressLogger;
 
 constexpr base::TimeDelta kToastDisplayTime = base::Seconds(8);
 
+constexpr char kLeakDialogTimeSpentHistogram[] =
+    "PasswordManager.PasswordChange.LeakDetectionDialog.TimeSpent";
+
 void LogPasswordFormDetectedMetric(bool form_detected,
                                    base::TimeDelta time_delta) {
   base::UmaHistogramBoolean("PasswordManager.ChangePasswordFormDetected",
@@ -65,6 +69,19 @@ void LogPasswordFormDetectedMetric(bool form_detected,
     base::UmaHistogramMediumTimes(
         "PasswordManager.ChangePasswordFormDetectionTime", time_delta);
   }
+}
+
+void LogLeakDialogTimeSpent(PasswordChangeDelegate::State state,
+                            base::TimeDelta time_delta) {
+  CHECK(state == PasswordChangeDelegate::State::kWaitingForAgreement ||
+        state == PasswordChangeDelegate::State::kOfferingPasswordChange);
+
+  std::string suffix =
+      state == PasswordChangeDelegate::State::kWaitingForAgreement
+          ? ".WithPrivacyNotice"
+          : ".WithoutPrivacyNotice";
+  base::UmaHistogramMediumTimes(
+      base::StrCat({kLeakDialogTimeSpentHistogram, suffix}), time_delta);
 }
 
 std::u16string GeneratePassword(
@@ -198,6 +215,7 @@ PasswordChangeDelegateImpl::PasswordChangeDelegateImpl(
 
   UpdateState(IsPrivacyNoticeAcknowledged() ? State::kOfferingPasswordChange
                                             : State::kWaitingForAgreement);
+  leak_dialog_display_time_ = base::Time::Now();
 }
 
 PasswordChangeDelegateImpl::~PasswordChangeDelegateImpl() {
@@ -212,6 +230,8 @@ PasswordChangeDelegateImpl::~PasswordChangeDelegateImpl() {
 
 void PasswordChangeDelegateImpl::StartPasswordChangeFlow() {
   flow_start_time_ = base::Time::Now();
+  LogLeakDialogTimeSpent(current_state_,
+                         flow_start_time_ - leak_dialog_display_time_);
   UpdateState(State::kWaitingForChangePasswordForm);
 
   executor_ = CreateWebContents(profile_, change_password_url_);
@@ -239,8 +259,10 @@ void PasswordChangeDelegateImpl::OnPasswordChangeFormFound(
     password_manager::PasswordFormManager* form_manager) {
   form_finder_.reset();
 
-  LogPasswordFormDetectedMetric(/*form_detected=*/form_manager,
-                                base::Time::Now() - flow_start_time_);
+  change_password_form_found_time_ = base::Time::Now();
+  LogPasswordFormDetectedMetric(
+      /*form_detected=*/form_manager,
+      change_password_form_found_time_ - flow_start_time_);
   if (!form_manager) {
     UpdateState(State::kChangePasswordFormNotFound);
     return;
@@ -412,10 +434,15 @@ void PasswordChangeDelegateImpl::UpdateState(State new_state) {
 }
 
 void PasswordChangeDelegateImpl::OnChangeFormSubmissionVerified(bool result) {
-  base::TimeDelta password_change_duration =
-      base::Time::Now() - flow_start_time_;
+  base::Time time_now = base::Time::Now();
+  base::TimeDelta password_change_duration_overall =
+      time_now - flow_start_time_;
+  base::UmaHistogramMediumTimes(
+      "PasswordManager.ChangingPasswordToast.TimeSpent",
+      time_now - change_password_form_found_time_);
   base::UmaHistogramMediumTimes("PasswordManager.PasswordChangeTimeOverall",
-                                password_change_duration);
+                                password_change_duration_overall);
+
   if (!result) {
     UpdateState(State::kPasswordChangeFailed);
   } else {
@@ -425,7 +452,7 @@ void PasswordChangeDelegateImpl::OnChangeFormSubmissionVerified(bool result) {
     NotifyPasswordChangeFinishedSuccessfully(originator_);
     UpdateState(State::kPasswordSuccessfullyChanged);
     MaybeLaunchSurvey(kHatsSurveyTriggerPasswordChangeSuccess,
-                      password_change_duration, profile_, originator_);
+                      password_change_duration_overall, profile_, originator_);
   }
   // TODO(crbug.com/407503334): Upload final log on destructor.
   logs_uploader_->UploadFinalLog();
