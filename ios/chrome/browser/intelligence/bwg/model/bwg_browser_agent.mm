cd6f5b73bddb5313ed6301a3fb64e33873c1613e
adamta <adamta@google.com>
2025-07-04 08:39:14 -0700

    [iOS] Set favicon to BWG configuration
    Sets the cached favicon from the web state to the BWG configuration, to
    be used by the floaty. If the favicon is unavailable, resorts to a
    default favicon instead.
    
    Bug: 428191060
    Change-Id: I98629220603457613add8aea5318b85540c836d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701677
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482644}

--- ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.h"
 
 #import "base/strings/sys_string_conversions.h"
+#import "components/favicon/ios/web_favicon_driver.h"
 #import "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_configuration.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_delegate.h"
@@ -18,10 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_browser_agent.h"
 #import "ios/public/provider/chrome/browser/bwg/bwg_api.h"
 #import "ios/public/provider/chrome/browser/bwg/bwg_gateway_protocol.h"
+#import "ui/gfx/image/image.h"
 
 namespace {
 
@@ -58,6 +61,8 @@ void BwgBrowserAgent::PresentBwgOverlay(
     UIViewController* base_view_controller,
     base::expected<std::unique_ptr<optimization_guide::proto::PageContext>,
                    PageContextWrapperError> expected_page_context) {
+  web::WebState* web_state = browser_->GetWebStateList()->GetActiveWebState();
+
   BWGConfiguration* config = [[BWGConfiguration alloc] init];
   config.baseViewController = base_view_controller;
   config.authService =
@@ -66,8 +71,9 @@ void BwgBrowserAgent::PresentBwgOverlay(
       GetApplicationContext()->GetSingleSignOnService();
   config.gateway = bwg_gateway_;
 
-  BwgTabHelper* bwg_tab_helper = BwgTabHelper::FromWebState(
-      browser_->GetWebStateList()->GetActiveWebState());
+  // Use the tab helper to set the initial floaty state, which includes the chat
+  // IDs and whether it was backgrounded.
+  BwgTabHelper* bwg_tab_helper = BwgTabHelper::FromWebState(web_state);
   config.clientID = base::SysUTF8ToNSString(bwg_tab_helper->GetClientId());
   std::optional<std::string> maybe_server_id = bwg_tab_helper->GetServerId();
   config.serverID =
@@ -76,6 +82,8 @@ void BwgBrowserAgent::PresentBwgOverlay(
       !bwg_tab_helper->GetIsBwgSessionActiveInBackground();
   config.shouldShowZeroState = bwg_tab_helper->ShouldShowZeroState();
 
+  // Set the page context and page state for the current web state. If the page
+  // context is unavailable, the page state represents the error.
   std::unique_ptr<optimization_guide::proto::PageContext> pageContext = nullptr;
   if (expected_page_context.has_value()) {
     pageContext = std::move(expected_page_context.value());
@@ -87,6 +95,24 @@ void BwgBrowserAgent::PresentBwgOverlay(
   }
   config.uniquePageContext = std::move(pageContext);
 
+  // Use the cached favicon of the web state. If it's not available, use a
+  // default favicon instead.
+  gfx::Image cached_favicon =
+      favicon::WebFaviconDriver::FromWebState(web_state)->GetFavicon();
+  UIImage* page_favicon;
+  if (!cached_favicon.IsEmpty()) {
+    page_favicon = cached_favicon.ToUIImage();
+  } else {
+    UIImageConfiguration* configuration = [UIImageSymbolConfiguration
+        configurationWithPointSize:gfx::kFaviconSize
+                            weight:UIImageSymbolWeightBold
+                             scale:UIImageSymbolScaleMedium];
+    page_favicon =
+        DefaultSymbolWithConfiguration(kGlobeAmericasSymbol, configuration);
+  }
+  config.favicon = page_favicon;
+
+  // Start the overlay and update the tab helper to reflect this.
   ios::provider::StartBwgOverlay(config);
   bwg_tab_helper->SetBwgUiShowing(true);
 }
