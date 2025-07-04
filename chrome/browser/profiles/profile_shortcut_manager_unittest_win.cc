defb4d97b9f0cc51517c25d8c899cef93cffd1d5
Shelley Vohr <shelley.vohr@gmail.com>
2025-07-04 14:19:17 -0700

    Remove dual mode from shortcuts
    Remove dual mode related code indicating that an application supports
    dual desktop and immersive modes.
    
    Refs https://chromium-review.googlesource.com/c/chromium/src/+/6633298/comment/093bdc60_85092da8
    
    Bug: 40445378
    
    Change-Id: Ib9766455ec756465916c1aab13b3ef07c1a00dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6632693
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482729}

--- chrome/browser/profiles/profile_shortcut_manager_unittest_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_unittest_win.cc
@@ -202,7 +202,6 @@ class ProfileShortcutManagerTest : public testing::Test {
         shell_integration::win::GetAppUserModelIdForBrowser(profile_path));
     expected_properties.set_target(GetExePath());
     expected_properties.set_description(InstallUtil::GetAppDescription());
-    expected_properties.set_dual_mode(false);
     expected_properties.set_arguments(
         profiles::internal::CreateProfileShortcutFlags(profile_path));
     expected_properties.set_icon(icon_path, 0);
@@ -230,7 +229,6 @@ class ProfileShortcutManagerTest : public testing::Test {
         profiles::internal::CreateProfileShortcutFlags(profile_path));
     expected_properties.set_icon(GetExePath(), 0);
     expected_properties.set_description(InstallUtil::GetAppDescription());
-    expected_properties.set_dual_mode(false);
     PostValidateShortcut(location, shortcut_path, expected_properties);
   }
 
@@ -251,7 +249,6 @@ class ProfileShortcutManagerTest : public testing::Test {
     expected_properties.set_arguments(std::wstring());
     expected_properties.set_icon(GetExePath(), 0);
     expected_properties.set_description(InstallUtil::GetAppDescription());
-    expected_properties.set_dual_mode(false);
     PostValidateShortcut(location, shortcut_path, expected_properties);
   }
 
