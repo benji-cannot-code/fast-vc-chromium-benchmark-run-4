2f3193e089d61c49068a2e93ef1c2a1e7ee5d2d5
Norge Vizcay <vizcay@google.com>
2025-07-04 09:47:19 -0700

    Remove leftover TODO.
    Change-Id: I684f10bf9f5d0cb4acaa1f6e6d41dbaa4bdd839a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700121
    Reviewed-by: Piotr Wrótniak <piotrwrotniak@google.com>
    Commit-Queue: Piotr Wrótniak <piotrwrotniak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482666}

--- components/autofill/core/browser/metrics/form_events/loyalty_card_form_event_logger.cc
+++ components/autofill/core/browser/metrics/form_events/loyalty_card_form_event_logger.cc
@@ -29,7 +29,7 @@ AffiliationCategoryMetricBucket AffiliationCategoriesToMetricBucket(
   if (affiliation_categories.empty()) {
     return AffiliationCategoryMetricBucket::kNone;
   }
-  if (affiliation_categories.size() > 1) {
+  if (affiliation_categories.size() > 1u) {
     return AffiliationCategoryMetricBucket::kMixed;
   }
   switch (*affiliation_categories.begin()) {
@@ -57,7 +57,6 @@ std::string_view GetAffiliationCategoriesSuffix(
 
 }  // namespace
 
-// TODO(crbug.com/422366498): Complete the class implementation.
 LoyaltyCardFormEventLogger::LoyaltyCardFormEventLogger(
     BrowserAutofillManager* owner)
     : FormEventLoggerBase("LoyaltyCard", owner) {}
