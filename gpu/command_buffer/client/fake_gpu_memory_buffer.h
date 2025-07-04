221ec0b021c1dc7fa514dee0a0d4e92f0d4c7865
Colin Blundell <blundell@chromium.org>
2025-07-03 23:49:58 -0700

    [//gpu] Eliminate unneeded FakeGpuMemoryBuffer code
    In advance of folding this class behind ClientSharedImage.
    
    Bug: 404905709
    Change-Id: I1dbe02b2c9c76136e39b28b4e826e7fc4c9bc898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701593
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482461}

--- gpu/command_buffer/client/fake_gpu_memory_buffer.h
+++ gpu/command_buffer/client/fake_gpu_memory_buffer.h
@@ -42,10 +42,6 @@ class FakeGpuMemoryBuffer : public gfx::GpuMemoryBuffer {
 
   FakeGpuMemoryBuffer() = delete;
 
-  FakeGpuMemoryBuffer(const gfx::Size& size, gfx::BufferFormat format);
-  FakeGpuMemoryBuffer(const gfx::Size& size,
-                      gfx::BufferFormat format,
-                      uint64_t modifier);
   FakeGpuMemoryBuffer(const gfx::Size& size,
                       gfx::BufferFormat format,
                       bool premapped,
