f7ad5fdd8429fc787f67f9f202c6e88b45d1d692
Magdalena Skarbinska <mskarbinska@google.com>
2025-07-04 09:58:35 -0700

    IWA: Implement notifying users when IWA opens multiple contents
    Ad revert
    https://chromium-review.googlesource.com/c/chromium/src/+/6695014 -
    tests tear down was updated to avoid use-after-free scenario.
    
    This CL implements a system notification for Isolated Web Apps (IWAs) in
    unmanaged (BYOD) scenarios when they programmatically open multiple
    windows without an explicit user gesture.
    
    Previously, `window.open()` calls generally consumed a user gesture,
    limiting apps to opening one new window at a time without further
    interaction. To enable an app to open multiple windows programmatically
    (e.g., in bulk or from automated processes), explicit user permission
    via the Pop-ups and Redirects Content Setting was required. While this
    could be defaulted for listed URLs via policy in managed enterprise
    environments, it led to a poor user experience for IWAs in unmanaged
    scenarios (e.g., during auto-launch or session restoration).
    
    This change ensures IWAs can seamlessly open multiple new windows by
    automatically setting the Pop-ups and Redirects Content Setting to
    "allow" upon installation, including for unmanaged apps. To mitigate
    concerns about unexpected window creation, a system-level notification
    is introduced. This notification informs the user about the creation of
    multiple windows by an IWA and provides an action button to update
    content settings if needed.
    
    This feature is implemented via a new keyed service that observes all
    tab strips and windows - `MultipleWebContentsOpenedService`. This
    approach was chosen because it allows to accurately track the complete
    lifecycle of all child windows and tabs—from creation to closure—which
    is essential for maintaining a correct count of opened web contents in
    the notification, even when tabs are moved. This modular design also
    establishes clear code ownership for the IWA team, simplifying
    maintenance and ensuring the feature is robust against future changes in
    the browser's windowing system.
    
    Examples of the notification:
    https://screenshot.googleplex.com/A3Vehvx6KchEDWG Context:
    go/window-open-without-gesture-for-iwas
    
    Change-Id: Icc56d218c13511e6c4a758e5754da11f4453dd39
    Bug: 400677358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694764
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482670}

--- chrome/browser/web_applications/isolated_web_apps/window_management/isolated_web_apps_opened_tabs_counter_service_factory.cc
+++ chrome/browser/web_applications/isolated_web_apps/window_management/isolated_web_apps_opened_tabs_counter_service_factory.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 BASE_FEATURE(kIsolatedWebAppsOpenedTabsCounterServiceNotification,
              "IsolatedWebAppsOpenedTabsCounterServiceNotification",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 }
 
 IsolatedWebAppsOpenedTabsCounterServiceFactory*
