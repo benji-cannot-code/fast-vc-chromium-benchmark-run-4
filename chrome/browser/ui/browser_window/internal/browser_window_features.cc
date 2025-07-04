701e007c15797a34243a0a71b527d53f759795f1
Dana Fried <dfried@chromium.org>
2025-07-04 12:40:58 -0700

    Fix bad merge that caused an object to be deleted early
    This was part of a bad merge in this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6569870
    
    The CL above restored an old line that deleted an object early; it
    has been removed to avoid this exact UAF.
    
    More specifically, while the above patch was in development, the line
    was evidently added and then removed by these two CLs, the latter of
    which is a revert:
    https://source.chromium.org/chromium/chromium/src/+/77eefce0d4a746a9b1aac30f46a9ad0179bacbd1
    https://source.chromium.org/chromium/chromium/src/+/efab2242b8683262c2bbfa88f21944cad6bd6bfe
    
    The second change was evidently lost during a local merge.
    
    This CL removes the erroneously-preserved line.
    
    Change-Id: Ic7e18ba26330e2966664decad205c20752a9216c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702031
    Reviewed-by: David Neto <dneto@google.com>
    Commit-Queue: David Neto <dneto@google.com>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482712}

--- chrome/browser/ui/browser_window/internal/browser_window_features.cc
+++ chrome/browser/ui/browser_window/internal/browser_window_features.cc
@@ -510,8 +510,6 @@ void BrowserWindowFeatures::TearDownPreBrowserWindowDestruction() {
   windows_taskbar_icon_updater_.reset();
 #endif
 
-  exclusive_access_manager_.reset();
-
   if (user_education_) {
     user_education_->TearDown();
   }
