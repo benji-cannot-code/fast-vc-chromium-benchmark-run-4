96b11999d27e3a5ab197fde752740bae1565f70b
Manuel Rego Casasnovas <rego@igalia.com>
2025-07-04 05:36:43 -0700

    Remove WindowOnMove feature flag
    The CSSWG has decided to remove this event and doesn't make sense
    to keep it separated from the bigger effort related to AWC
    (Additional Windowing Controls).
    See https://github.com/w3c/csswg-drafts/issues/7693.
    
    So this is somehow a revert of crrev.com/c/6035050 so the WindowOnMove
    runtime feature is removed and the event remains under
    DesktopPWAsAdditionalWindowingControls flag.
    
    Bug: 379542213,40276534
    Change-Id: I70944a99408ceea1b162fc064809044f6556dc0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6705234
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482561}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -4898,7 +4898,8 @@ bool WebFrameWidgetImpl::UpdateScreenRects(
 }
 
 void WebFrameWidgetImpl::EnqueueMoveEvent() {
-  if (!RuntimeEnabledFeatures::WindowOnMoveEventEnabled()) {
+  if (!RuntimeEnabledFeatures::
+          DesktopPWAsAdditionalWindowingControlsEnabled()) {
     return;
   }
 
