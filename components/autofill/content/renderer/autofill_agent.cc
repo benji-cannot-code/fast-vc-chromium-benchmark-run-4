1d1c94d8ba7a1ab59c9157feab0f1871e499e7f2
Florian Leimgruber <fleimgruber@google.com>
2025-07-04 00:18:27 -0700

    Expose AX node ID in verbose autofill-information
    When verbose autofill-information [1] is enabled, more details about
    the form are exposed through the autofill-information attribute. This CL
    adds the AX node ID to it. This is useful for data collection.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/common/autofill_features.cc;l=915;drc=17b4c42cfb3512836655464b0d41d319e819c0b6
    
    Bug: 389631485
    Change-Id: Icb9d3e628510563f0f6b2cb232a5bfa51157507a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701713
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482470}

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -349,6 +349,8 @@ bool ShowPredictions(const WebDocument& document,
           option_labels,
           "\noption values: ",
           option_values,
+          "\nax node id: ",
+          base::NumberToString(field_data.form_control_ax_id()),
       });
     }
 
