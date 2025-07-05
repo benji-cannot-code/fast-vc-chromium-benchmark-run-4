4b37c117db76cae080bc0fc505aef7c84942821e
Orko Garai <orko@igalia.com>
2025-07-04 21:54:41 -0700

    ozone/wayland: Fix high resolution scroll handling
    The handling of wl_pointer::axis_value120 event was a little inaccurate
    as the compositor sends a value of 120 for a single scroll step or
    detent. But then it was being multiplied by detent angle of 15 and
    divided by axis value scale of 10 which is required for the axis event
    and not applicable in this case. This resulted in a scroll delta of 180
    pixels per wheel click in wayland as opposed to the 120 pixels in X11
    and Windows.
    
    So make the following changes:
    - Don't use detent angle as the value passed in the event corresponds to
      a scroll step or wheel detent regardless of detent angle [1].
    - Only scale down the value by 10 in the case of the axis event and not
      the axis_value120 event.
    
    [1] https://wayland.app/protocols/wayland#wl_pointer:event:axis_value120
    
    Test: Tested on gnome that the scrolled pixels per wheel click in
    wayland is now the same as in x11 (and windows)
    
    Bug: 40720099, 40887377
    Change-Id: If2a21a37751a5646a4fa2c4d3aa01cbb45d04434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700422
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1482769}

--- ui/ozone/platform/wayland/host/wayland_pointer.cc
+++ ui/ozone/platform/wayland/host/wayland_pointer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/host/wayland_pointer.h"
 
 #include <linux/input.h>
+#include <wayland-util.h>
 
 #include <optional>
 
@@ -166,8 +167,14 @@ void WaylandPointer::OnAxis(void* data,
                             uint32_t time,
                             uint32_t axis,
                             wl_fixed_t value) {
-  const double delta =
-      -wl_fixed_to_double(value) * MouseWheelEvent::kWheelDelta;
+  // Wayland compositors send axis events with values in the surface coordinate
+  // space. They send a value of 10 per mouse wheel click by convention, so
+  // clients (e.g. GTK+) typically scale down by this amount to convert to
+  // discrete step coordinates. wl_pointer version 5 improves the situation by
+  // adding axis sources and discrete axis events.
+  const double kAxisValueScale = 10.0;
+  const double delta = -wl_fixed_to_double(value) / kAxisValueScale *
+                       MouseWheelEvent::kWheelDelta;
   const auto timestamp = wl::EventMillisecondsToTimeTicks(time);
   auto* self = static_cast<WaylandPointer*>(data);
   self->OnAxisImpl(delta, axis, timestamp, /*is_high_resolution=*/false);
@@ -220,8 +227,7 @@ void WaylandPointer::OnAxisValue120(void* data,
                                     wl_pointer* pointer,
                                     uint32_t axis,
                                     int32_t value120) {
-  static const double kDetentAngleDegrees = 15.0;
-  const double delta = -value120 * kDetentAngleDegrees;
+  const double delta = -value120;
   auto* self = static_cast<WaylandPointer*>(data);
   self->OnAxisImpl(delta, axis, /*timestamp=*/std::nullopt,
                    /*is_high_resolution=*/true);
