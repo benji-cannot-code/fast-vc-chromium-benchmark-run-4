036b0d7478b1f3dae95bdb2bdc184b203a2b9762
Sirisha Kavuluru <skavuluru@google.com>
2025-07-05 17:47:14 -0700

    Skip showing Desktop site setting IPH on desktop
    Desktop site setting is default enabled on desktop, we should not show
    the IPH for it.
    
    Also remove unused function.
    
    Fixed: 424271684
    Change-Id: I52ca8338f04f6e8900cd6d55c5c17bc90e64b818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700419
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Auto-Submit: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1482853}

--- chrome/android/java/src/org/chromium/chrome/browser/desktop_site/DesktopSiteSettingsIphController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/desktop_site/DesktopSiteSettingsIphController.java
@@ -16,6 +16,7 @@ import android.view.View;
 
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.DeviceInfo;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
@@ -73,13 +74,15 @@ public class DesktopSiteSettingsIphController {
      * @param toolbarMenuButton The toolbar menu button to which the IPH will be anchored.
      * @param appMenuHandler The app menu handler.
      */
-    public static DesktopSiteSettingsIphController create(
+    public static @Nullable DesktopSiteSettingsIphController create(
             Activity activity,
             WindowAndroid windowAndroid,
             ActivityTabProvider activityTabProvider,
             Profile profile,
             View toolbarMenuButton,
             AppMenuHandler appMenuHandler) {
+        // Desktop site settings are default enabled on desktop. Do not show IPH.
+        if(DeviceInfo.isDesktop()) return null;
         return new DesktopSiteSettingsIphController(
                 windowAndroid,
                 activityTabProvider,
