9da0ab1e86a17f3bff59aacb6a1878ddc3ae7bb0
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-07-07 07:34:04 -0700

    Create ActiveUserTestMixin
    This CL moves the test setup code added in crrev.com/c/6652381 to its
    own mixin class so it can be reused in future CLs.
    
    Bug: 415002299
    Change-Id: I3b9868a1ae8539b99d5f849998605f4dbc3e23d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700303
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483144}

---
+++ chrome/browser/enterprise/connectors/test/active_user_test_mixin.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_TEST_ACTIVE_USER_TEST_MIXIN_H_
+#define CHROME_BROWSER_ENTERPRISE_CONNECTORS_TEST_ACTIVE_USER_TEST_MIXIN_H_
+
+#include "chrome/test/base/mixin_based_in_process_browser_test.h"
+#include "google_apis/gaia/fake_gaia.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+
+namespace enterprise_connectors::test {
+
+// Helper class that allows a simple setup for active content area user
+// identity. This should be used to help validate that DLP requests and reported
+// events are detecting the correct user.
+class ActiveUserTestMixin : public InProcessBrowserTestMixin {
+ public:
+  ActiveUserTestMixin(InProcessBrowserTestMixinHost* host,
+                      InProcessBrowserTest* test,
+                      net::EmbeddedTestServer* test_server,
+                      std::vector<const char*> emails);
+  ~ActiveUserTestMixin() override;
+
+  void SetFakeCookieValue();
+
+ protected:
+  // InProcessBrowserTestMixin:
+  void SetUp() override;
+  void SetUpOnMainThread() override;
+  void SetUpCommandLine(base::CommandLine* command_line) override;
+  void SetUpInProcessBrowserTestFixture() override;
+
+ private:
+  const raw_ptr<InProcessBrowserTest> test_;
+  const raw_ptr<net::EmbeddedTestServer> test_server_;
+  std::vector<const char*> emails_;
+
+  FakeGaia fake_gaia_;
+};
+
+}  // namespace enterprise_connectors::test
+
+#endif  // CHROME_BROWSER_ENTERPRISE_CONNECTORS_TEST_ACTIVE_USER_TEST_MIXIN_H_
