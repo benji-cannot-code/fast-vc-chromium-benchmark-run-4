40f172644234ce800ed9c1f84dca868ad8355256
Morten Stenshorne <mstensho@chromium.org>
2025-07-07 01:57:47 -0700

    Let scaling affect media query matching when printing.
    It used to work like this prior to crrev.com/c/4853339 , which
    unintentionally changed it. Although there's no spec for guidance, it
    seems like a good idea to honor scaling here.
    
    Add some documentation, and also rename the function. What it does is
    very specific now, and nobody else (apart from media query evaluation)
    currently calls it.
    
    Bug: 424552574
    Change-Id: I032bb6e81d74916df86a032faf8684b202cafb5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6662378
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483038}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -945,7 +945,7 @@ gfx::SizeF LocalFrameView::ViewportSizeForMediaQueries() const {
   }
   if (frame_->ShouldUsePaginatedLayout()) {
     if (const LayoutView* layout_view = GetLayoutView()) {
-      return layout_view->DefaultPageAreaSize();
+      return layout_view->PaginationViewportSizeForMediaQueries();
     }
   }
   gfx::SizeF viewport_size(layout_size_);
