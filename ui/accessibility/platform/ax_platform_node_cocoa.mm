22c395a37fb07708f9ec12eb83378061dc9e7348
Aaron leventhal <aleventhal@google.com>
2025-07-07 09:53:26 -0700

    [A11yPerformance] No attributes should be exposed on ignored elements
    Bug: 425758499
    Change-Id: I4995944c74fb70795a1c2741664f7b3b2839bee0
    Fixed: 428022217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6707790
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483216}

--- ui/accessibility/platform/ax_platform_node_cocoa.mm
+++ ui/accessibility/platform/ax_platform_node_cocoa.mm
@@ -991,7 +991,7 @@ const ui::CocoaActionList& GetCocoaActionListForTesting() {
     case ax::mojom::Role::kPreDeprecated:
     case ax::mojom::Role::kPortalDeprecated:
     case ax::mojom::Role::kRubyAnnotation:
-      NOTREACHED();
+      NOTREACHED() << "The following role should not be present: " << role;
   }
 }
 
@@ -1725,6 +1725,10 @@ const ui::CocoaActionList& GetCocoaActionListForTesting() {
   TRACE_EVENT1("accessibility",
                "AXPlatformNodeCocoa::accessibilityAttributeNames",
                "role=", ui::ToString([self internalRole]));
+
+  if (![self isAccessibilityElement]) {
+    return @[];
+  }
 
   // Exclude attributes available through the new accessibility API.
   NSMutableArray* attributes = [self internalAccessibilityAttributeNames];
