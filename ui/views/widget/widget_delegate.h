cb9ef9fe11feaf6bb3f468d00dbf872074d7fedb
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-07-07 11:59:35 -0700

    Remove OwnedByWidgetPassKey usage from content analysis dialog tests
    This CL follows-up on the cleanups done for crbug.com/422111748 and
    removes test fixtures from WidgetDelegate::OwnedByWidgetPassKey's
    private friends.
    
    Bug: 422111748
    Change-Id: Icacd61714bd4008d48892cb8e3614f06253fec9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702959
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483298}

--- ui/views/widget/widget_delegate.h
+++ ui/views/widget/widget_delegate.h
@@ -166,11 +166,6 @@ namespace crostini {
 class AppRestartDialog;
 }
 
-namespace enterprise_connectors {
-class ContentAnalysisDialogController;
-class ContentAnalysisDialogBehaviorBrowserTest;
-}  // namespace enterprise_connectors
-
 namespace exo {
 class ShellSurfaceBase;
 }
@@ -438,9 +433,6 @@ class VIEWS_EXPORT WidgetDelegate {
     friend class ::constrained_window::BrowserModalHelper;
     friend class ::content::ShellPlatformDelegate;
     friend class ::crostini::AppRestartDialog;
-    friend class ::enterprise_connectors::ContentAnalysisDialogController;
-    friend class ::enterprise_connectors::
-        ContentAnalysisDialogBehaviorBrowserTest;
     friend class ::exo::ShellSurfaceBase;
     friend class ::extensions::WebFileHandlersPermissionHandler;
     friend class ::javascript_dialogs::AppModalDialogViewViews;
