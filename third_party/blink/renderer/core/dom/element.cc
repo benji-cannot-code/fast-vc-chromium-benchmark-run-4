8b56bcd58c9f70cf3f71840487d50d416a20e8ea
Divyansh Mangal <dmangal@microsoft.com>
2025-07-07 01:37:02 -0700

    [SVG] Removing runtime flag `RestrictGetBoundingClientRectForHiddenSVGElements`
    Removing this feature flag as two releases have passed.
    
    Bug: 40348501
    Change-Id: Ibe91ebcb8ae8ad590d213b3d6c22fff6885497fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6707260
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Reviewed-by: Vinay Singh <vinaysingh@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1483032}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -3076,9 +3076,7 @@ void Element::ClientQuads(Vector<gfx::QuadF>& quads) const {
     // If stroke is desired, we can update this to use AbsoluteQuads, below.
     if (const auto* svg_graphics_element =
             DynamicTo<SVGGraphicsElement>(this)) {
-      if (RuntimeEnabledFeatures::
-              RestrictGetBoundingClientRectForHiddenSVGElementsEnabled() &&
-          svg_graphics_element->IsNonRendered(element_layout_object)) {
+      if (svg_graphics_element->IsNonRendered(element_layout_object)) {
         return;
       }
 
