0f92a9782d0e0f934d3b4a33d79a4a10e381d9b8
Jocelyn Tran <jocelyntran@google.com>
2025-07-07 10:43:38 -0700

    [A11y] Add null check for event recorder creation
    When a recorder is created for AXEventServer (used for ax_dump_events),
    nullptr is passed in for the manager. This null check prevents a
    segmentation fault.
    
    Bug: 429216094
    Change-Id: I7d4e63117c67b18ea6be96d0f1d5b65257e94906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685671
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483247}

--- content/public/browser/ax_inspect_factory_auralinux.cc
+++ content/public/browser/ax_inspect_factory_auralinux.cc
@@ -54,7 +54,7 @@ std::unique_ptr<ui::AXEventRecorder> AXInspectFactory::CreateRecorder(
   switch (type) {
     case ui::AXApiType::kLinux:
       return std::make_unique<ui::AXEventRecorderAuraLinux>(
-          manager->GetWeakPtr(), pid, selector);
+          manager ? manager->GetWeakPtr() : nullptr, pid, selector);
     default:
       NOTREACHED() << "Unsupported API type " << static_cast<std::string>(type);
   }
