3e82b45dd761a0de2c1ab2219efe0727e54b4ba5
Steven 'Steve' Kendall <skend@google.com>
2025-07-07 15:48:36 -0700

    Add UserInitiatedFlexSystemFirmwareUpdate policy
    This policy will toggle filtering firmware updates for
    the local device itself. Also remove redundant "atomic_groups:"
    in policies.yaml erroneously added by previous policy.
    
    TEST=browser builds, no UI elements yet to test
    BUG=b:294273852
    
    Change-Id: Ia5b4b7c509bcb558a8d613a1506d9946de379fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6631042
    Commit-Queue: Steve Kendall <skend@google.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483403}

--- chrome/browser/ash/settings/device_settings_provider.cc
+++ chrome/browser/ash/settings/device_settings_provider.cc
@@ -119,6 +119,7 @@ constexpr auto kKnownSettings = base::MakeFixedFlatSet<std::string_view>({
     kDeviceSecondFactorAuthenticationMode,
     kDeviceUnaffiliatedCrostiniAllowed,
     kDeviceUserInitiatedFirmwareUpdatesEnabled,
+    kDeviceUserInitiatedFlexSystemFirmwareUpdatesEnabled,
     kDeviceWebBasedAttestationAllowedUrls,
     kDeviceWiFiAllowed,
     kDisplayRotationDefault,
@@ -1377,6 +1378,16 @@ void DecodeGenericPolicies(const em::ChromeDeviceSettingsProto& policy,
     if (container.has_value()) {
       new_values_cache->SetValue(kDeviceUserInitiatedFirmwareUpdatesEnabled,
                                  base::Value(container.value()));
+    }
+  }
+
+  if (policy.has_deviceuserinitiatedflexsystemfirmwareupdatesenabled()) {
+    const em::BooleanPolicyProto& container(
+        policy.deviceuserinitiatedflexsystemfirmwareupdatesenabled());
+    if (container.has_value()) {
+      new_values_cache->SetValue(
+          kDeviceUserInitiatedFlexSystemFirmwareUpdatesEnabled,
+          base::Value(container.value()));
     }
   }
 
