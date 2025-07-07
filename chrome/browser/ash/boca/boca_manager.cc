a70558a59b14bdc53c29899a500f3ddba52e701b
Ahmed Nasr <anasr@google.com>
2025-07-07 16:56:35 -0700

    [BabelOrca] Use new speech recognizer client controller by finch.
    Bug: b:416494658
    Change-Id: I6ed0df36fd6de7c7333a3861a8f4f7a9e39eab65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700753
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483437}

--- chrome/browser/ash/boca/boca_manager.cc
+++ chrome/browser/ash/boca/boca_manager.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
+#include "chrome/browser/ash/boca/babelorca/babel_orca_speech_recognizer_client.h"
 #include "chrome/browser/ash/boca/babelorca/babel_orca_speech_recognizer_impl.h"
 #include "chrome/browser/ash/boca/babelorca/caption_bubble_context_boca.h"
 #include "chrome/browser/ash/boca/on_task/on_task_extensions_manager_impl.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chromeos/ash/components/boca/babelorca/babel_orca_manager.h"
+#include "chromeos/ash/components/boca/babelorca/babel_orca_speech_recognizer.h"
 #include "chromeos/ash/components/boca/babelorca/babel_orca_translation_dispatcher_impl.h"
 #include "chromeos/ash/components/boca/babelorca/soda_installer.h"
 #include "chromeos/ash/components/boca/boca_metrics_manager.h"
@@ -90,9 +92,16 @@ std::unique_ptr<boca::BabelOrcaManager> CreateBabelOrcaManager(
     return nullptr;
   }
 
-  auto speech_recognizer =
-      std::make_unique<babelorca::BabelOrcaSpeechRecognizerImpl>(
-          profile, soda_installer, application_locale, caption_language);
+  std::unique_ptr<babelorca::BabelOrcaSpeechRecognizer> speech_recognizer;
+  if (features::IsBocaMigrateSpeechRecognizerClientEnabled()) {
+    speech_recognizer =
+        std::make_unique<babelorca::BabelOrcaSpeechRecognizerClient>(
+            profile, soda_installer, application_locale, caption_language);
+  } else {
+    speech_recognizer =
+        std::make_unique<babelorca::BabelOrcaSpeechRecognizerImpl>(
+            profile, soda_installer, application_locale, caption_language);
+  }
   auto babel_orca_manager = boca::BabelOrcaManager::CreateAsProducer(
       IdentityManagerFactory::GetForProfile(profile),
       profile->GetURLLoaderFactory(), std::move(caption_bubble_context),
