540743227546d99ba7cc3188b440d3261f7233d8
Fergal Daly <fergal@chromium.org>
2025-07-06 23:58:17 -0700

    More crash keys and also prevent inlining to try get a good stack trace.
    The `NOT_TAIL_CALLED` is probably not useful based on where this is
    called from but it's harmless.
    
    Bug: 427426299
    Change-Id: Ie3d0960366ab8e77c3e1a218271d6753a4eeff57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702797
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483007}

--- content/browser/renderer_host/back_forward_cache_metrics.cc
+++ content/browser/renderer_host/back_forward_cache_metrics.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/back_forward_cache_metrics.h"
 
+#include "base/debug/alias.h"
 #include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_functions.h"
@@ -232,6 +233,7 @@ void BackForwardCacheMetrics::DidCommitNavigation(
                  page_store_result_->ToString());
     RecordHistoryNavigationUMA(navigation, back_forward_cache_allowed);
     RecordHistoryNavigationUKM(navigation);
+    SCOPED_CRASH_KEY_BOOL("crbug/427426299", "pstr", !!page_store_tree_result_);
     if (!navigation->IsServedFromBackForwardCache()) {
       devtools_instrumentation::BackForwardCacheNotUsed(
           navigation, page_store_result_.get(), page_store_tree_result_.get());
@@ -242,10 +244,12 @@ void BackForwardCacheMetrics::DidCommitNavigation(
           std::move(page_store_tree_result_));
     }
   }
+  SCOPED_CRASH_KEY_BOOL("crbug/427426299", "before_gni", true);
   // Save the information about the last cross-document main frame navigation
   // that uses this metrics object.
   last_committed_cross_document_main_frame_navigation_id_ =
       navigation->GetNavigationId();
+  SCOPED_CRASH_KEY_BOOL("crbug/427426299", "after_gni", true);
 
   // BackForwardCacheMetrics can be reused in some cases. Reset fields for UKM
   // for the next navigation.
