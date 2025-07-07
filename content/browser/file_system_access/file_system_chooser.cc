ddb841ab5b9c153a0f1f9628030d957d305a8118
Joel Hockey <joelhockey@chromium.org>
2025-07-07 12:52:31 -0700

    Add perfetto trace event for File System Access file chooser
    Bug: b/388981133
    Change-Id: I0789081336b5846e2ec23d666c8c557f61799d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6705696
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483316}

--- content/browser/file_system_access/file_system_chooser.cc
+++ content/browser/file_system_access/file_system_chooser.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "content/browser/file_system_access/file_system_access_error.h"
 #include "content/public/browser/browser_thread.h"
@@ -287,6 +288,7 @@ void FileSystemChooser::CreateAndShow(
     ResultCallback callback,
     base::ScopedClosureRunner fullscreen_block) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  TRACE_EVENT0("FileSystem", "FileSystemChooser::CreateAndShow");
   // `listener` deletes itself.
   auto* listener = new FileSystemChooser(options.type(), std::move(callback),
                                          std::move(fullscreen_block));
