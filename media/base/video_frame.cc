d182ed250d7a347ec32769c309b1e2864f0eb1d0
Colin Blundell <blundell@chromium.org>
2025-07-07 02:37:47 -0700

    [//media] Make WrapExternalGpuMemoryBuffer() ChromeOS-only
    This is conceptually part of [1]; at some point in a rebase I somehow
    lost the changes to video_frame.* in that CL :P.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6680612
    
    Bug: 40263579
    Change-Id: I544db13158096f032c74cb00f7bd65fbd0fc02b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700541
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483060}

--- media/base/video_frame.cc
+++ media/base/video_frame.cc
@@ -790,6 +790,7 @@ scoped_refptr<VideoFrame> VideoFrame::WrapExternalYuvaData(
   return frame;
 }
 
+#if BUILDFLAG(IS_CHROMEOS)
 // static
 scoped_refptr<VideoFrame> VideoFrame::WrapExternalGpuMemoryBuffer(
     const gfx::Rect& visible_rect,
@@ -825,6 +826,7 @@ scoped_refptr<VideoFrame> VideoFrame::WrapExternalGpuMemoryBuffer(
   }
   return frame;
 }
+#endif
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 // static
