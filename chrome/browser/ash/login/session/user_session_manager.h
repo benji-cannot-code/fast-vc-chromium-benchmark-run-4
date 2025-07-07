ff212d62311f2078feb24a9f317576b911be8e7b
Elie Maamari <emaamari@google.com>
2025-07-07 05:04:55 -0700

    Extract legacy token handle fetching code
    We factor out the legacy code for token handle fetching in a
    separate method `UserSessionManager::FetchTokenHandleLegacy`. This is
    in preparation to adding the new code that uses `TokenHandleService` in
    the next downstream CL.
    
    Bug: b:383733245
    Change-Id: Ic567db73e7dbbed527ca588a57343e5988be0a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627620
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483096}

--- chrome/browser/ash/login/session/user_session_manager.h
+++ chrome/browser/ash/login/session/user_session_manager.h
@@ -479,7 +479,7 @@ class UserSessionManager
   // Returns `true` if token handles should be used on this device.
   bool TokenHandlesEnabled();
 
-  void CreateTokenUtilIfMissing();
+  void CreateTokenHandleStoreIfMissing();
 
   // Update token handle if the existing token handle is missing/invalid.
   void UpdateTokenHandleIfRequired(Profile* const profile,
@@ -517,6 +517,8 @@ class UserSessionManager
   // doesn't exist.
   HelpAppNotificationController* GetHelpAppNotificationController(
       Profile* profile);
+
+  void FetchTokenHandleLegacy(Profile* profile, const user_manager::User* user);
 
   base::WeakPtr<UserSessionManagerDelegate> delegate_;
 
