ddecc22cd624775222e4edd6f39292c796cae09f
Sylvain Defresne <sdefresne@chromium.org>
2025-07-07 09:24:27 -0700

    [ios] Add missing include in utils_unittest.mm
    The following line depends on a full definition of the type
    TabGroupVisualData ({} is constructing a TabGroupVisualData),
    but the definition is not directly included.
    
      active_web_state_list->CreateGroup(
          {0}, {}, TabGroupId::GenerateNew());
    
    Currently this builds because the include comes from the
    ios/chrome/browser/snapshots/model/snapshot_browser_agent.h
    file transitively but this may break when the file is modified.
    
    Bug: none
    Change-Id: Ie181fcfd722ea4ec44e0457b74b55016623e1109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6708785
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483198}

--- ios/chrome/browser/tabs/model/inactive_tabs/utils_unittest.mm
+++ ios/chrome/browser/tabs/model/inactive_tabs/utils_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/task_environment.h"
 #import "components/prefs/pref_service.h"
 #import "components/tab_groups/tab_group_id.h"
+#import "components/tab_groups/tab_group_visual_data.h"
 #import "ios/chrome/browser/ntp/model/new_tab_page_tab_helper.h"
 #import "ios/chrome/browser/ntp/model/new_tab_page_tab_helper_delegate.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
