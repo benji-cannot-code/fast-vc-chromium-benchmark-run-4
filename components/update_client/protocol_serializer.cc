fb778966a314001e4512cc7340e86a30029fdf6b
Anton Bershanskyi <bershanskyi@gmail.com>
2025-07-07 13:03:05 -0700

    [Extensions] Remove NaCl arch info from Update Client URLs
    NaCl is deprecated since M139 which disabled NaCl entirely in CL 6632728
    and removed support for nacl_modules manifest key in CL 6659079. This CL
    updates Update Client to stop setting nacl_arch request parameter when
    downloading extension updates.
    
    Bug: 40511454
    Change-Id: If128bf3dd7ea585aeaf0600b9e34bb69ff3d65c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701466
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483323}

--- components/update_client/protocol_serializer.cc
+++ components/update_client/protocol_serializer.cc
@@ -132,7 +132,6 @@ protocol_request::Request MakeProtocolRequest(
   request.prodchannel = channel;
   request.operating_system = UpdateQueryParams::GetOS();
   request.arch = UpdateQueryParams::GetArch();
-  request.nacl_arch = UpdateQueryParams::GetNaclArch();
   request.dlpref = download_preference;
   request.domain_joined = domain_joined;
   request.additional_attributes = additional_attributes;
