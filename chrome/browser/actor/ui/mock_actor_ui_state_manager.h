79e92a42272c977e0a790b018324eb646ecde57d
chrstne <chrstne@google.com>
2025-07-07 09:45:10 -0700

    Reland "[AUSM] Add tab scoped ui changes due to actor task state changes"
    This reverts commit d3bbf7d167d9b649880721fed031d8d86328a731.
    
    Reason for revert: current_ui_tab_state_ in ActorUiTabController was not initialized before being used, applied a fix
    
    successful run: https://ci.chromium.org/ui/inv/u-chrstne-2025-07-07-14-28-00-606b69bfc0e99438
    
    Bug: 424495020
    Original change's description:
    > Revert "[AUSM] Add tab scoped ui changes due to actor task state changes"
    >
    > This reverts commit 18e6096f6a72aee50c4248ae9c1ca2d1f6636867.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6595448908283904
    >
    > Sample build with failed test: https://ci.chromium.org/b/8710398830125946369
    > Affected test(s):
    > [ninja://chrome/test:interactive_ui_tests/GlicActorControllerUiTest.PauseActorTask](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FGlicActorControllerUiTest.PauseActorTask?q=VHash%3A0b7dea373e451e6a)
    > [ninja://chrome/test:interactive_ui_tests/GlicActorControllerUiTest.PauseThenResumeActorTask](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FGlicActorControllerUiTest.PauseThenResumeActorTask?q=VHash%3A0b7dea373e451e6a)
    > [ninja://chrome/test:interactive_ui_tests/GlicActorControllerUiTest.PauseThenStopActorTask](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FGlicActorControllerUiTest.PauseThenStopActorTask?q=VHash%3A0b7dea373e451e6a)
    > [ninja://chrome/test:interactive_ui_tests/GlicActorControllerUiTest.ResumeActorTaskWhenAlreadyResumed](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FGlicActorControllerUiTest.ResumeActorTaskWhenAlreadyResumed?q=VHash%3A0b7dea373e451e6a)
    > [ninja://chrome/test:interactive_ui_tests/GlicActorControllerUiTest.ResumeActorTaskWithoutATask](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FGlicActorControllerUiTest.ResumeActorTaskWithoutATask?q=VHash%3A0b7dea373e451e6a)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6595448908283904&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6683062&type=BUG
    >
    > Original change's description:
    > > [AUSM] Add tab scoped ui changes due to actor task state changes
    > >
    > > Bug: 424495020
    > > Change-Id: I6fb5afabacd8f915f8d951d99db9c075e54b7172
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6683062
    > > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > > Reviewed-by: Kevin Graney <kmg@google.com>
    > > Commit-Queue: Christine Ying <chrstne@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1482006}
    > >
    >
    > Bug: 424495020
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I0349377ce80541a2fb82118a990c8a2527bca28a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701562
    > Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    > Owners-Override: Maksim Moskvitin <mmoskvitin@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1482095}
    
    Bug: 424495020
    Change-Id: Ia77998ca38031492bcb0671e639a11c60e8bd017
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6707787
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483213}

--- chrome/browser/actor/ui/mock_actor_ui_state_manager.h
+++ chrome/browser/actor/ui/mock_actor_ui_state_manager.h
@@ -9,7 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/task_id.h"
 #include "chrome/browser/actor/ui/actor_ui_state_manager_interface.h"
+#include "chrome/browser/actor/ui/actor_ui_tab_controller_interface.h"
 #include "testing/gmock/include/gmock/gmock.h"
+
+namespace tabs {
+class TabInterface;
+}
 
 namespace actor::ui {
 
@@ -25,6 +30,10 @@ class MockActorUiStateManager : public ActorUiStateManagerInterface {
   MOCK_METHOD(void,
               OnUiEvent,
               (UiEvent event, UiCompleteCallback callback),
+              (override));
+  MOCK_METHOD(void,
+              NotifyUiTabController,
+              (tabs::TabInterface & tab, const UiTabState& ui_tab_state),
               (override));
   MOCK_METHOD(void, MaybeShowToast, (), (override));
 };
