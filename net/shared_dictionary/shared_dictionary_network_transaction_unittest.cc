5d8fd3f5b6d243536c9ecfcf00a90aadb4626500
Elly <ellyjones@chromium.org>
2025-07-07 08:19:58 -0700

    net/sdch: use new hash API in unit tests
    Bug: 372283556
    Change-Id: Id6f625e1aa6f84454747feb4e15a3cbbe81e941e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701485
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483173}

--- net/shared_dictionary/shared_dictionary_network_transaction_unittest.cc
+++ net/shared_dictionary/shared_dictionary_network_transaction_unittest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
 #include "base/test/scoped_feature_list.h"
-#include "crypto/secure_hash.h"
+#include "crypto/hash.h"
 #include "net/base/features.h"
 #include "net/base/hash_value.h"
 #include "net/base/io_buffer.h"
@@ -90,18 +90,14 @@ class DummySyncDictionary : public SharedDictionary {
   explicit DummySyncDictionary(const std::string& data_string,
                                const std::string& id = "")
       : data_(base::MakeRefCounted<StringIOBuffer>(data_string)),
-        size_(data_string.size()),
         id_(id) {
-    std::unique_ptr<crypto::SecureHash> secure_hash =
-        crypto::SecureHash::Create(crypto::SecureHash::SHA256);
-    secure_hash->Update(data_->data(), size_);
-    secure_hash->Finish(hash_);
+    hash_ = crypto::hash::Sha256(data_->span());
   }
 
   // SharedDictionary
   int ReadAll(base::OnceCallback<void(int)> callback) override { return OK; }
   scoped_refptr<IOBuffer> data() const override { return data_; }
-  size_t size() const override { return size_; }
+  size_t size() const override { return data_->size(); }
   const SHA256HashValue& hash() const override { return hash_; }
   const std::string& id() const override { return id_; }
 
@@ -109,8 +105,7 @@ class DummySyncDictionary : public SharedDictionary {
   ~DummySyncDictionary() override = default;
 
  private:
-  const scoped_refptr<IOBuffer> data_;
-  const size_t size_;
+  const scoped_refptr<StringIOBuffer> data_;
   const std::string id_;
   SHA256HashValue hash_;
 };
