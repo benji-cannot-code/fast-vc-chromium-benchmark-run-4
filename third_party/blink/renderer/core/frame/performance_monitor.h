5966a4d30e7d2e2983f315ff558e35f33e9716a7
David Sanders <dsanders11@ucsbalum.com>
2025-07-07 10:35:03 -0700

    Remove unused include in performance_monitor.h
    Bug: 40318405
    Change-Id: I13cac0f25bda8543a4d825f3ce222580a46a93e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6707444
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483242}

--- third_party/blink/renderer/core/frame/performance_monitor.h
+++ third_party/blink/renderer/core/frame/performance_monitor.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/prefinalizer.h"
-#include "third_party/blink/renderer/platform/loader/fetch/resource.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "v8/include/v8.h"
