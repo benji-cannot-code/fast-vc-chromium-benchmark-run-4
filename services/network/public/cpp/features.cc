ead6d0b156440570d7c760009611939ee3245b64
Aldo Culquicondor <acondor@chromium.org>
2025-07-07 16:13:29 -0700

    Enable IncreaseCookieAccessCacheSize by default
    The feature showed a mild but positive impact on scroll jank and memory
    usage.
    
    Bug: 380864710, 408329628
    Change-Id: Iaed0219908b527c302641ea04de0eb0420908928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6710485
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483418}

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -595,7 +595,7 @@ BASE_FEATURE(kGetCookiesOnSet,
 
 BASE_FEATURE(kIncreaseCookieAccessCacheSize,
              "IncreaseCookieAccessCacheSize",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE_PARAM(int,
                    kCookieAccessCacheSize,
