985c9bdbed6e1a0285a073f69f1fb83f91393103
Arthur Milchior <arthurmilchior@google.com>
2025-07-08 02:26:25 -0700

    [iOS]Stopping the Add Account Coordinator propagates to its coordinators
    If the add account coordinator was opened, the alert coordinator was not
    properly dismissed. The History sync popup coordinator had the same
    issue, so I also solve this issue here.
    
    This CL remove the DCHECK as its quite certain they will now be valid
    given that the stop/interrupt function was called above and put them to
    nil.
    
    Fixed: 424623612
    Change-Id: If8d5eeb3a1b9ca55ceac7a40ce9b676ba7712f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701606
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483632}

--- ios/chrome/browser/authentication/ui_bundled/signin/add_account_signin/add_account_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -151,18 +151,14 @@ using signin_metrics::PromoAction;
 
   [self stopPostSigninManagerCoordinatorAnimated:animated];
   [self interruptAddAccountSigninManager:animated];
+  [self stopAlertCoordinator];
+  [self stopHistorySyncPopupCoordinator];
 
   _accountManagerService = nullptr;
   _identityManager = nullptr;
   _authenticationService = nil;
   _continuationProvider.Reset();
   _syncService = nil;
-  // If one of those 3 DCHECK() fails, -[AddAccountSigninCoordinator
-  // runCompletionWithSigninResult] has not been called.
-  DCHECK(!self.addAccountSigninManager);
-  DCHECK(!self.alertCoordinator);
-  DCHECK(!self.postSigninManagerCoordinator);
-  DCHECK(!self.historySyncPopupCoordinator);
 }
 
 #pragma mark - AddAccountSigninManagerDelegate
@@ -196,8 +192,7 @@ using signin_metrics::PromoAction;
       DCHECK(error);
       __weak AddAccountSigninCoordinator* weakSelf = self;
       ProceduralBlock dismissAction = ^{
-        [weakSelf.alertCoordinator stop];
-        weakSelf.alertCoordinator = nil;
+        [weakSelf stopAlertCoordinator];
         [weakSelf
             addAccountDoneWithSigninResult:SigninCoordinatorResultCanceledByUser
                                   identity:nil];
@@ -238,6 +233,11 @@ using signin_metrics::PromoAction;
 }
 
 #pragma mark - Private
+
+- (void)stopAlertCoordinator {
+  [self.alertCoordinator stop];
+  self.alertCoordinator = nil;
+}
 
 - (void)interruptAddAccountSigninManager:(BOOL)animated {
   [self.addAccountSigninManager interruptAnimated:animated];
@@ -295,8 +295,7 @@ using signin_metrics::PromoAction;
   [self.alertCoordinator
       addItemWithTitle:l10n_util::GetNSString(IDS_OK)
                 action:^{
-                  [weakSelf.alertCoordinator stop];
-                  weakSelf.alertCoordinator = nil;
+                  [weakSelf stopAlertCoordinator];
                   [weakSelf continueAddAccountFlowWithSigninResult:
                                 SigninCoordinatorResultCanceledByUser
                                                           identity:nil];
