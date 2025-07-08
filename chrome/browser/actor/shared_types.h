3d15240c08073a43f3fa1db7849dbbe077f59cbe
Takuto Ikuta <tikuta@chromium.org>
2025-07-08 00:53:56 -0700

    chrome: Add <variant> include for error in modules build
    Bug: 40263312
    Change-Id: Ide08afbb6d01086462ccddf65591683f6c7e3e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6712856
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483594}

--- chrome/browser/actor/shared_types.h
+++ chrome/browser/actor/shared_types.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ACTOR_SHARED_TYPES_H_
 #define CHROME_BROWSER_ACTOR_SHARED_TYPES_H_
 
+#include <variant>
+
 #include "chrome/common/actor.mojom-data-view.h"
 #include "ui/gfx/geometry/point.h"
 
