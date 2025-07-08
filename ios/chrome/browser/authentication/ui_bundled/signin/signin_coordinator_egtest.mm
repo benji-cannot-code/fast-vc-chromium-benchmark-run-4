54718aded922553b4dbe87dc07f48ff2a6356d20
Asami Doi <asamidoi@chromium.org>
2025-07-08 08:04:08 -0700

    [ios] Fix SigninCoordinatorTestCase on iOS 26
    On iOS 26, the cancel button in the "Sign in to Chromium" screen becomes
    a button with the 'Close' label instead of the 'Cancel' label.
    
    Use an accessibility ID instead of the label ID to match an element.
    
    Below iOS 18, it used to be:
    ```
    <_UIButtonBarButton:0x11637ea0000; isAccessible=Y; AX.id='WebSigninSkipButtonAccessibilityIdentifier'; AX.label='Cancel'; AX.frame={{695.5, 583}, {60.5, 44}}; AX.isModal=N; AX.activationPoint={725.75, 605}; AX.traits='UIAccessibilityTraitButton'; AX.focused='N'; frame={{0, 0}, {60.5, 44}}; opaque; alpha=1>
    ```
    
    On iOS 26, it becomes:
    ```
    <_UIButtonBarButton:0x13e37da8c00; isAccessible=Y; AX.id='WebSigninSkipButtonAccessibilityIdentifier'; AX.label='Close'; AX.frame={{352.33333333333331, 640.66666666666674}, {41.666666666666686, 44}}; AX.isModal=N; AX.activationPoint={373.16666666666663, 662.66666666666674}; AX.traits='UIAccessibilityTraitButton'; AX.focused='N'; frame={{0, 0}, {41.666666666666664, 44}}; opaque; alpha=1>
    ```
    
    Fixed: 429403548
    Change-Id: I0d9c0d8559d95476ef9fa7e0b6448a0613c7a192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714567
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483770}

--- ios/chrome/browser/authentication/ui_bundled/signin/signin_coordinator_egtest.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/signin_coordinator_egtest.mm
@@ -390,10 +390,8 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
       assertWithMatcher:grey_notNil()];
 
   // Close sign-in screen and Settings.
-  [[EarlGrey selectElementWithMatcher:
-                 grey_allOf(ButtonWithAccessibilityLabelId(
-                                IDS_IOS_ACCOUNT_CONSISTENCY_SETUP_SKIP_BUTTON),
-                            grey_userInteractionEnabled(), nil)]
+  [[EarlGrey
+      selectElementWithMatcher:chrome_test_util::WebSigninSkipButtonMatcher()]
       performAction:grey_tap()];
   [[EarlGrey selectElementWithMatcher:SettingsDoneButton()]
       performAction:grey_tap()];
@@ -578,10 +576,8 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
       assertWithMatcher:grey_notNil()];
 
   // Close sign-in screen and Settings.
-  [[EarlGrey selectElementWithMatcher:
-                 grey_allOf(ButtonWithAccessibilityLabelId(
-                                IDS_IOS_ACCOUNT_CONSISTENCY_SETUP_SKIP_BUTTON),
-                            grey_userInteractionEnabled(), nil)]
+  [[EarlGrey
+      selectElementWithMatcher:chrome_test_util::WebSigninSkipButtonMatcher()]
       performAction:grey_tap()];
   [[EarlGrey selectElementWithMatcher:SettingsDoneButton()]
       performAction:grey_tap()];
