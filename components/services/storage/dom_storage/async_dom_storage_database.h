9455c9f62beeb0fdd71673180e37ae5cd62d8a89
Evan Stade <evanstade@microsoft.com>
2025-07-08 11:43:03 -0700

    Remove DOMStorage.CommitSizeBytes histograms
    DOMStorage.CommitSizeBytes and DOMStorage.CommitSizeBytesAggregated are
    nearing expiration and are removed because the experiments they were
    used for have already concluded.
    
    They should have already been expired, but the Chrome Metrics Logs bots
    automatically extended them (and then they were also manually extended
    due to nag emails).
    
    Bug: 428705334
    Change-Id: I48d256e58696614556550586238a396cc735d85d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697774
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1483905}

--- components/services/storage/dom_storage/async_dom_storage_database.h
+++ components/services/storage/dom_storage/async_dom_storage_database.h
@@ -85,9 +85,6 @@ class AsyncDomStorageDatabase {
     std::vector<DomStorageDatabase::Key> keys_to_delete;
     std::optional<DomStorageDatabase::Key> copy_to_prefix;
     std::vector<base::TimeTicks> timestamps;
-
-    // For metrics.
-    size_t data_size;
   };
 
   // An interface that represents a source of commits. Practically speaking,
