aac6217a63e5c1574d220e76e03197222038fc99
Colin Blundell <blundell@chromium.org>
2025-07-08 10:10:52 -0700

    [//gpu] Remove GpuServiceFactory::gpu_memory_buffer_factory_
    Credit to vasilyt@ for noticing that this is unused.
    
    Bug: 404905709
    Change-Id: Ibaa0de20e53f28d7612647632a053cb061646745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6708963
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483854}

--- content/gpu/gpu_service_factory.h
+++ content/gpu/gpu_service_factory.h
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/mojom/media_service.mojom.h"
 #include "services/service_manager/public/mojom/service.mojom.h"
 
-namespace gpu {
-class GpuMemoryBufferFactory;
-}
-
 namespace media {
 class MediaGpuChannelManager;
 }
@@ -38,7 +34,6 @@ class GpuServiceFactory {
       const gpu::GpuFeatureInfo& gpu_feature_info,
       const gpu::GPUInfo& gpu_info,
       base::WeakPtr<media::MediaGpuChannelManager> media_gpu_channel_manager,
-      gpu::GpuMemoryBufferFactory* gpu_memory_buffer_factory,
       media::AndroidOverlayMojoFactoryCB android_overlay_factory_cb);
 
   GpuServiceFactory(const GpuServiceFactory&) = delete;
@@ -58,8 +53,6 @@ class GpuServiceFactory {
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   base::WeakPtr<media::MediaGpuChannelManager> media_gpu_channel_manager_;
   media::AndroidOverlayMojoFactoryCB android_overlay_factory_cb_;
-  // Indirectly owned by GpuChildThread.
-  raw_ptr<gpu::GpuMemoryBufferFactory> gpu_memory_buffer_factory_;
   gpu::GpuPreferences gpu_preferences_;
   gpu::GpuDriverBugWorkarounds gpu_workarounds_;
   gpu::GpuFeatureInfo gpu_feature_info_;
