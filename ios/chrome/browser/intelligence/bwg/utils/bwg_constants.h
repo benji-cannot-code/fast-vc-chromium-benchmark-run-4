b4a056ee103b8e8f09dabd6a42ccbce8df3ef9a4
“Prasanaa” <prasanaa@google.com>
2025-07-08 06:21:35 -0700

    [iOS] Remove unused detent identifier in BWG Constants
    This CL removes unused detent identifier
    `kBWGPromoConsentPartialDetentIdentifier` in BWG Constants
    
    Bug: 429229749
    Change-Id: I4301e9ce20e908e2e6b0033234e7e2d07805c5d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713577
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483712}

--- ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h
+++ ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-// BWG UI sheet detents.
-extern NSString* const kBWGPromoConsentPartialDetentIdentifier;
+// BWG UI sheet detent identifier.
 extern NSString* const kBWGPromoConsentFullDetentIdentifier;
 
 // BWG UI Lottie Animation name for FRE Banner.
--- ios/chrome/browser/intelligence/bwg/utils/bwg_constants.mm
+++ ios/chrome/browser/intelligence/bwg/utils/bwg_constants.mm
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h"
 
-NSString* const kBWGPromoConsentPartialDetentIdentifier =
-    @"BWGConsentPartialDetentIdentifier";
 NSString* const kBWGPromoConsentFullDetentIdentifier =
-    @"BWGConsentFullDetentIdentifier";
+    @"BWGPromoConsentFullDetentIdentifier";
 
 NSString* const kLottieAnimationFREBannerName = @"FRE_Banner";
 
