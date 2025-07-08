41ce6f586873b6807db6a003e6c5ebf0e4debfba
Qiu Jianlin <jianlin.qiu@intel.com>
2025-07-07 21:44:19 -0700

    [DX12 Enc] Pack color space into encoded AV1 bitstream.
    When input frame's color space is Rec.709/Rec.601, pack color space
    information into AV1 bitstream.
    
    Bug: 40275246, 40269844
    Change-Id: I930569602f7100b1451b6fd57ea0f2b2ead3da8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6706958
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1483536}

--- media/gpu/av1_builder.h
+++ media/gpu/av1_builder.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "media/gpu/media_gpu_export.h"
+#include "third_party/libgav1/src/src/gav1/decoder_buffer.h"
 #include "third_party/libgav1/src/src/utils/constants.h"
 #include "third_party/libgav1/src/src/utils/types.h"
 
@@ -87,6 +88,15 @@ struct MEDIA_GPU_EXPORT AV1BitstreamBuilder::SequenceHeader {
   bool enable_superres = false;
   bool enable_cdef = false;
   bool enable_restoration = false;
+  bool color_description_present_flag = false;
+  Libgav1ColorPrimary color_primaries = kLibgav1ColorPrimaryBt709;
+  Libgav1TransferCharacteristics transfer_characteristics =
+      kLibgav1TransferCharacteristicsBt709;
+  Libgav1MatrixCoefficients matrix_coefficients =
+      kLibgav1MatrixCoefficientsBt709;
+  Libgav1ColorRange color_range = kLibgav1ColorRangeStudio;
+  Libgav1ChromaSamplePosition chroma_sample_position =
+      kLibgav1ChromaSamplePositionUnknown;
 };
 
 struct MEDIA_GPU_EXPORT AV1BitstreamBuilder::FrameHeader {
