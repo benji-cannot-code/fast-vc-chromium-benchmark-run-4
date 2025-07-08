1468768822334ff0d2d3ee48bda52d3170a2ad00
Jiacheng Guo <gjc@google.com>
2025-07-07 18:13:40 -0700

    Remove ForTesting suffix for anonymous functions in prerender_host.cc
    This is a refactor CL derived from the discussions in
    crrev.com/c/6687011.
    
    Bug: None
    Change-Id: Ie0ec6bf9ff8d53b9f9b65236beb26f03e8942a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6703036
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483479}

--- content/browser/preloading/prerender/prerender_host.cc
+++ content/browser/preloading/prerender/prerender_host.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 
+#include "base/check_is_test.h"
 #include "base/debug/alias.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
@@ -58,10 +59,10 @@ namespace content {
 
 namespace {
 
-base::OnceCallback<void(FrameTreeNodeId)>& GetHostCreationCallbackForTesting() {
+base::OnceCallback<void(FrameTreeNodeId)>& GetHostCreationCallback() {
   static base::NoDestructor<base::OnceCallback<void(FrameTreeNodeId)>>
-      host_creation_callback_for_testing;
-  return *host_creation_callback_for_testing;
+      host_creation_callback;
+  return *host_creation_callback;
 }
 
 #if BUILDFLAG(IS_ANDROID)
@@ -192,7 +193,7 @@ bool PrerenderHost::AreHttpRequestHeadersCompatible(
 // static
 void PrerenderHost::SetHostCreationCallbackForTesting(
     base::OnceCallback<void(FrameTreeNodeId host_id)> callback) {
-  GetHostCreationCallbackForTesting() = std::move(callback);  // IN-TEST
+  GetHostCreationCallback() = std::move(callback);
 }
 
 PrerenderHost::PrerenderHost(
@@ -268,9 +269,9 @@ PrerenderHost::PrerenderHost(
 
   frame_tree_node_id_ = frame_tree_->root()->frame_tree_node_id();
 
-  if (GetHostCreationCallbackForTesting()) {
-    std::move(GetHostCreationCallbackForTesting())  // IN-TEST
-        .Run(frame_tree_node_id_);
+  if (GetHostCreationCallback()) {
+    CHECK_IS_TEST();
+    std::move(GetHostCreationCallback()).Run(frame_tree_node_id_);
   }
 }
 
