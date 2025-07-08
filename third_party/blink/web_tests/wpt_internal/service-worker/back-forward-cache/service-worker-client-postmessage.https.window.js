52d191e17ee671a6d17cbc7e7ce14e67de92ad08
Anna Sato <annasato@chromium.org>
2025-07-07 22:36:50 -0700

    Isolate helpers for ServiceWorker + BFCache WPTs
    The previous CL (https://crrev.com/c/6680929) modified a helper in
    external/wpt, which caused Firefox stability failures. This CL moves the
    logic into a new internal helper to fix the issue.
    
    Bug: 428057368
    Change-Id: Ie6927110c7523cb57d7618ec0d2a45e4b115c76d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6703072
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483549}

--- third_party/blink/web_tests/wpt_internal/service-worker/back-forward-cache/service-worker-client-postmessage.https.window.js
+++ third_party/blink/web_tests/wpt_internal/service-worker/back-forward-cache/service-worker-client-postmessage.https.window.js
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper-tests/resources/test-helper.js
 // META: script=/service-workers/service-worker/resources/test-helpers.sub.js
+// META: script=./resources/helper.js
 // META: timeout=long
 
 'use strict';
