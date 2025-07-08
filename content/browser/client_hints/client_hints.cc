80b13c316bd38705bde63893e2bf63d8ba014526
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-07-07 21:23:39 -0700

    Rename GetAddedClientHints to GetEnabledClientHints
    This is a follow up to https://chromium-review.googlesource.com/c/chromium/src/+/6677314/comment/781e24f0_4b7eebc7
    This CL renames GetAddedClientHints to GetEnabledClientHints.
    
    Bug: 406407746
    Change-Id: I5cc1e8ce8afc5f1d7cbd04545e6d5fbe4dd1b844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6703028
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483529}

--- content/browser/client_hints/client_hints.cc
+++ content/browser/client_hints/client_hints.cc
@@ -1062,7 +1062,7 @@ bool AreCriticalHintsMissing(
   return false;
 }
 
-std::vector<network::mojom::WebClientHintsType> GetAddedClientHints(
+std::vector<network::mojom::WebClientHintsType> GetEnabledClientHints(
     const url::Origin& origin,
     FrameTreeNode* frame_tree_node,
     ClientHintsControllerDelegate* delegate) {
--- content/browser/client_hints/client_hints_unittest.cc
+++ content/browser/client_hints/client_hints_unittest.cc
@@ -314,7 +314,7 @@ TEST_F(ClientHintsTest, SubFrame) {
   EXPECT_EQ(existing_hints, current_hints.GetEnabledHints());
 }
 
-TEST_F(ClientHintsTest, GetAddedClientHints) {
+TEST_F(ClientHintsTest, GetEnabledClientHints) {
   url::Origin origin = url::Origin::Create(GURL(ClientHintsTest::kOriginUrl));
 
   FrameTree& frame_tree = contents()->GetPrimaryFrameTree();
@@ -335,7 +335,7 @@ TEST_F(ClientHintsTest, GetAddedClientHints) {
     }
   }
   std::vector<network::mojom::WebClientHintsType> actual_types =
-      GetAddedClientHints(origin, main_frame_node, &delegate);
+      GetEnabledClientHints(origin, main_frame_node, &delegate);
 
   // We do not care the order of contents.
   EXPECT_THAT(actual_types, testing::UnorderedElementsAreArray(expected_types));
