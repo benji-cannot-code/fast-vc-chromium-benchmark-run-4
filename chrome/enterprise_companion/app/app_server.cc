d1750fff262a3cbb20db082c6296e3bcdcde0df2
Noah Rose Ledesma <noahrose@google.com>
2025-07-08 11:27:51 -0700

    Fix member/method decl order
    Members should be declared after methods per style guide.
    
    Fixed: 353269840
    Change-Id: Ia6bbf05d824c725f8eed4f3f264f9d5a67a3a558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715757
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483898}

--- chrome/enterprise_companion/app/app_server.cc
+++ chrome/enterprise_companion/app/app_server.cc
@@ -138,20 +138,6 @@ class AppServer : public App {
   }
 
  private:
-  SEQUENCE_CHECKER(sequence_checker_);
-
-#if !BUILDFLAG(IS_MAC)
-  base::Thread net_thread_{"Network"};
-#endif
-
-#if BUILDFLAG(IS_WIN)
-  base::win::ScopedCOMInitializer com_initializer_{
-      base::win::ScopedCOMInitializer::kMTA};
-#endif
-  base::SequenceBound<URLLoaderFactoryProvider> url_loader_factory_provider_;
-  std::unique_ptr<ScopedLock> lock_;
-  std::unique_ptr<mojom::EnterpriseCompanion> stub_;
-
   void OnUrlLoaderFactoryReceived(
       std::unique_ptr<network::PendingSharedURLLoaderFactory>
           pending_url_loader_factory) {
@@ -170,6 +156,18 @@ class AppServer : public App {
             base::BindOnce(&AppServer::Shutdown, weak_ptr_factory_.GetWeakPtr(),
                            EnterpriseCompanionStatus::Success())));
   }
+
+  SEQUENCE_CHECKER(sequence_checker_);
+#if !BUILDFLAG(IS_MAC)
+  base::Thread net_thread_{"Network"};
+#endif
+#if BUILDFLAG(IS_WIN)
+  base::win::ScopedCOMInitializer com_initializer_{
+      base::win::ScopedCOMInitializer::kMTA};
+#endif
+  base::SequenceBound<URLLoaderFactoryProvider> url_loader_factory_provider_;
+  std::unique_ptr<ScopedLock> lock_;
+  std::unique_ptr<mojom::EnterpriseCompanion> stub_;
 
   base::WeakPtrFactory<AppServer> weak_ptr_factory_{this};
 };
