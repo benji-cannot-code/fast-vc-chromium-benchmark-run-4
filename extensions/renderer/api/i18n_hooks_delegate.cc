2f5a89645163293879a6d76212204d93f13304bb
Clemens Backes <clemensb@chromium.org>
2025-07-08 01:53:53 -0700

    [extensions] Avoid calls to deprecated GetIsolate methods
    Those methods just return the value from TLS and do not read from the
    object any more. Hence they should be replaced by
    `v8::Isolate::GetCurrent()`.
    
    After this landed https://crrev.com/c/6613837 will advance the
    deprecation of the methods in the V8 API.
    
    Bug: 427885584
    
    R=rdevlin.cronin@chromium.org
    
    Change-Id: I5b943607c10f1da6a3972de2cb309b1934f9a838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6703733
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483615}

--- extensions/renderer/api/i18n_hooks_delegate.cc
+++ extensions/renderer/api/i18n_hooks_delegate.cc
@@ -91,7 +91,7 @@ v8::Local<v8::Value> DetectedLanguage::ToV8(v8::Isolate* isolate) const {
 
 v8::Local<v8::Value> LanguageDetectionResult::ToV8(
     v8::Local<v8::Context> context) const {
-  v8::Isolate* isolate = context->GetIsolate();
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
   DCHECK(isolate->GetCurrentContext() == context);
 
   v8::Local<v8::Array> v8_languages = v8::Array::New(isolate, languages.size());
@@ -156,7 +156,7 @@ v8::Local<v8::Value> GetI18nMessage(const std::string& message_name,
                                     v8::Local<v8::Value> v8_options,
                                     SharedL10nMap::IPCTarget* ipc_target,
                                     v8::Local<v8::Context> context) {
-  v8::Isolate* isolate = context->GetIsolate();
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
   std::string message = SharedL10nMap::GetInstance().GetMessage(
       extension_id, message_name, ipc_target);
