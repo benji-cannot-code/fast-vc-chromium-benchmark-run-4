d04fa1ac10684d31fd181499b4ae79f24b2e86bf
Benedikt Meurer <bmeurer@chromium.org>
2025-07-08 02:33:18 -0700

    [DevTools] Remove remaining uses of `requestContent`.
    Bug: 425280773
    Change-Id: I994590c56bae3f5e0a7a704b1b06a0dd1b25a129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713418
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483635}

--- third_party/blink/web_tests/http/tests/devtools/network/network-xhr-sync.js
+++ third_party/blink/web_tests/http/tests/devtools/network/network-xhr-sync.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TestRunner} from 'test_runner';
 import {NetworkTestRunner} from 'network_test_runner';
 
+import * as TextUtils from 'devtools/models/text_utils/text_utils.js';
+
 (async function() {
   TestRunner.addResult(`Tests XHR network resource type and content for synchronous requests. Bug 61205\n`);
   await TestRunner.showPanel('network');
@@ -18,7 +20,7 @@ import {NetworkTestRunner} from 'network_test_runner';
     TestRunner.addResult(request1.url());
     TestRunner.addResult('resource.type: ' + request1.resourceType());
     TestRunner.assertTrue(!request1.failed, 'Resource loading failed.');
-    request1.requestContent().then(step3);
+    request1.requestContentData().then(TextUtils.ContentData.ContentData.asDeferredContent).then(step3);
   }
 
   function step3({ content, error, isEncoded }) {
