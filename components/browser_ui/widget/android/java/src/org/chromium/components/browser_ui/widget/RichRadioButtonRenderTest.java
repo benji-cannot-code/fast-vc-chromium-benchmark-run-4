b4db87f18d72b04696562b4bcde19d287baa55ee
Nicola Tommasi <tommasin@chromium.org>
2025-07-08 12:42:38 -0700

    [Clank][widget] RichRadioButton - Add support for vertical layout
    This CL enhances the RichRadioButton class to support vertical layouts
    for its items.
    
    Screenshots: https://screenshot.googleplex.com/4acdrPf7zeF2Rk7,
    https://screenshot.googleplex.com/C2pbbBGekmWSDCv
    
    Bug: 428698459
    Change-Id: I0c7c1342cb04307a2d75f8212043af236ad96433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6689123
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483935}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RichRadioButtonRenderTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RichRadioButtonRenderTest.java
@@ -70,6 +70,7 @@ public class RichRadioButtonRenderTest {
     private RichRadioButton mRichRbHorizontalFullUnchecked;
     private RichRadioButton mRichRbHorizontalTitleChecked;
     private RichRadioButton mRichRbHorizontalMinimalUnchecked;
+    private RichRadioButton mRichRbVerticalFullUnchecked;
 
     private final int mFakeBgColor;
 
@@ -115,6 +116,8 @@ public class RichRadioButtonRenderTest {
                             mLayout.findViewById(R.id.rich_rb_horizontal_title_checked);
                     mRichRbHorizontalMinimalUnchecked =
                             mLayout.findViewById(R.id.rich_rb_horizontal_minimal_unchecked);
+                    mRichRbVerticalFullUnchecked =
+                            mLayout.findViewById(R.id.rich_rb_vertical_full_unchecked);
                 });
 
         Assert.assertNotNull(mLayout);
@@ -179,5 +182,18 @@ public class RichRadioButtonRenderTest {
                     mRichRbHorizontalFullUnchecked.setChecked(true);
                 });
         mRenderTestRule.render(mRichRbHorizontalFullUnchecked, "rich_rb_horizontal_full_checked");
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"RenderTest", "RichRadioButton"})
+    public void testRichRbVerticalFullUnchecked() throws Exception {
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    mRichRbVerticalFullUnchecked.setItemData(
+                            R.drawable.test_location_precise, "Title", "Vertical item", true);
+                    mRichRbVerticalFullUnchecked.setChecked(false);
+                });
+        mRenderTestRule.render(mRichRbVerticalFullUnchecked, "rich_rb_vertical_full_unchecked");
     }
 }
