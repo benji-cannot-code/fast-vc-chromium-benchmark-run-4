e4d6c40c51fa07e7d4b009c9f5b76820f45de7f3
Stefan Zager <szager@chromium.org>
2025-07-08 12:42:10 -0700

    Abandon printing if lifecycle update failed
    Speculative fix for crash.
    
    Bug: chromium:408411675
    Change-Id: I127c095fc165c5a38f750d4dde95603ebaff3444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6712908
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483934}

--- third_party/blink/renderer/core/frame/web_local_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_local_frame_impl.cc
@@ -452,7 +452,9 @@ class ChromePrintContext : public PrintContext {
 
     auto* frame_view = GetFrame()->View();
     DCHECK(frame_view);
-    frame_view->UpdateLifecyclePhasesForPrinting();
+    if (!frame_view->UpdateLifecyclePhasesForPrinting()) {
+      return;
+    }
 
     if (!IsFrameValid() || page_index >= PageCount()) {
       // TODO(crbug.com/452672): The number of pages may change after layout for
@@ -576,7 +578,9 @@ class PaintPreviewContext : public PrintContext {
     if (!GetFrame()->GetDocument() ||
         !GetFrame()->GetDocument()->GetLayoutView())
       return false;
-    GetFrame()->View()->UpdateLifecyclePhasesForPrinting();
+    if (!GetFrame()->View()->UpdateLifecyclePhasesForPrinting()) {
+      return false;
+    }
     if (!GetFrame()->GetDocument() ||
         !GetFrame()->GetDocument()->GetLayoutView())
       return false;
