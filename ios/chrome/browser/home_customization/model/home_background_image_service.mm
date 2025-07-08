5cd254611f73e88c2cced8bddb8aec9c76663644
Cheick Cisse <cheickcisse@google.com>
2025-07-08 12:09:37 -0700

    [iOS] Fixes a crash that occurs during NtpBackgroundService shutdown.
    This change relocates the NTPBackgroundService cleanup process in
    HomeBackgroundImageService from the destructor to
    OnNtpBackgroundServiceShuttingDown method to ensure it is handled
    correctly.
    
    Bug: 428687430
    Change-Id: I819d625ca7587b3bb8f0c9370d01dc868a9700cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6708484
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483915}

--- ios/chrome/browser/home_customization/model/home_background_image_service.mm
+++ ios/chrome/browser/home_customization/model/home_background_image_service.mm
@@ -21,10 +21,7 @@ HomeBackgroundImageService::HomeBackgroundImageService(
   ntp_background_service_->AddObserver(this);
 }
 
-HomeBackgroundImageService::~HomeBackgroundImageService() {
-  ntp_background_service_->RemoveObserver(this);
-  ntp_background_service_ = nullptr;
-}
+HomeBackgroundImageService::~HomeBackgroundImageService() = default;
 
 void HomeBackgroundImageService::FetchDefaultCollectionImages(
     CollectionsImagesCallback callback) {
@@ -135,4 +132,7 @@ void HomeBackgroundImageService::OnCollectionInfoAvailable() {
 
 void HomeBackgroundImageService::OnCollectionImagesAvailable() {}
 void HomeBackgroundImageService::OnNextCollectionImageAvailable() {}
-void HomeBackgroundImageService::OnNtpBackgroundServiceShuttingDown() {}
+void HomeBackgroundImageService::OnNtpBackgroundServiceShuttingDown() {
+  ntp_background_service_->RemoveObserver(this);
+  ntp_background_service_ = nullptr;
+}
