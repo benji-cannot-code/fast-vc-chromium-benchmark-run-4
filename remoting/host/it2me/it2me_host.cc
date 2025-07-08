3c777b779d0735c1f7e37be61d341a47c7dc4f61
Yuwei Huang <yuweih@chromium.org>
2025-07-08 14:12:19 -0700

    [crd it2me] Remove useCorpSessionAuthz flag
    The flag has been superseded by `isCorpUser`, which is set with the
    it2me_corp_directory_apis rollout, which has been fully rolled out and
    folded. So this CL removes the useCorpSessionAuthz flag.
    
    Bug: 417567187
    Change-Id: Id3667150182081293a9250afdb4ba09975bb0d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715241
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484000}

--- remoting/host/it2me/it2me_host.cc
+++ remoting/host/it2me/it2me_host.cc
@@ -314,8 +314,7 @@ void It2MeHost::ConnectOnNetworkThread(
     }
   }
 
-  if (connection_context->is_corp_user ||
-      connection_context->use_corp_session_authz) {
+  if (connection_context->is_corp_user) {
     use_corp_session_authz_ = true;
   }
 
