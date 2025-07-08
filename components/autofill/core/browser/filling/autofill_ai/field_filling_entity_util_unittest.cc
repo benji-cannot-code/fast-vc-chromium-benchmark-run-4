bc4b033d51f1f13cc028f7d6c703cda7070d9394
Jihad Hanna <jihadghanna@google.com>
2025-07-08 10:20:17 -0700

    Give the default test passport a more meaningful number
    Bug: 40100455
    Change-Id: I3bbac4e76b8d4d424052c1da21f80b585c00c607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714789
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483860}

--- components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util_unittest.cc
+++ components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util_unittest.cc
@@ -327,7 +327,7 @@ TEST_F(GetFillValueForEntityTest, NumbersWithMaxLength) {
             u"12312345");
   EXPECT_EQ(GetFillValueForEntity(passport, passport_number_field,
                                   mojom::ActionPersistence::kFill),
-            u"123");
+            u"LR1234567");
   EXPECT_EQ(GetFillValueForEntity(vehicle, license_plate_field,
                                   mojom::ActionPersistence::kFill),
             u"123456");
@@ -349,7 +349,7 @@ TEST_F(GetFillValueForEntityTest, NumbersWithMaxLength) {
             u"345");
   EXPECT_EQ(GetFillValueForEntity(passport, passport_number_field,
                                   mojom::ActionPersistence::kFill),
-            u"123");
+            u"567");
   EXPECT_EQ(GetFillValueForEntity(vehicle, license_plate_field,
                                   mojom::ActionPersistence::kFill),
             u"456");
@@ -371,7 +371,7 @@ TEST_F(GetFillValueForEntityTest, NumbersWithMaxLength) {
             u"12312345");
   EXPECT_EQ(GetFillValueForEntity(passport, passport_number_field,
                                   mojom::ActionPersistence::kFill),
-            u"123");
+            u"LR1234567");
   EXPECT_EQ(GetFillValueForEntity(vehicle, license_plate_field,
                                   mojom::ActionPersistence::kFill),
             u"123456");
