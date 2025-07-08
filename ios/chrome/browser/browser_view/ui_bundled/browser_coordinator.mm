afb1ca4adcd826441874e29fc6a3fc7aa2322061
Quentin Pubert <qpubert@google.com>
2025-07-08 06:46:43 -0700

    [iOS] Fix null FindTabHelper crash in -[BrowserCoordinator hideFindUI]
    It appears the activeWebState can sometimes have no FindTabHelper
    attached when trying to hide the Find UI (maybe the WebState is being
    destroyed). This CL adds an if-statement so that the tab helper is only
    used if non-null.
    
    Bug: 427201321
    Change-Id: I17b3e03c2bd33e2566e7028234aa262dbf401110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713397
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483724}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -2822,7 +2822,9 @@ enum class ToolbarKind {
     web::WebState* activeWebState = self.activeWebState;
     DCHECK(activeWebState);
     auto* helper = FindTabHelper::FromWebState(activeWebState);
-    helper->DismissFindNavigator();
+    if (helper) {
+      helper->DismissFindNavigator();
+    }
   } else {
     [self.findBarCoordinator stop];
     self.findBarCoordinator = nil;
