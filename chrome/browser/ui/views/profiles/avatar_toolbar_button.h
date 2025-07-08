739b51dd9f65a01ff95155206c3521a3d9e32640
Ernest Nguyen Hung <ernn@google.com>
2025-07-08 00:38:36 -0700

    [Profiles] Remove AvatarToolbarButtonDelegate class
    It removes `AvatarToolbarButtonDelegate` and makes `AvatarToolbarButton`
    to use `internal::AvatarToolbarButtonStateManager` directly. Remaining
    logic of `AvatarToolbarButtonDelegate` as moved to either
    `AvatarToolbarButton` or `internal::AvatarToolbarButtonStateManager`.
    
    This reduces unnecessary indirection and enables unit testing of the
    `internal::AvatarToolbarButtonStateManager`.
    
    Fixed: 421821832
    Change-Id: Ib936ba918cbe45ff9ff9d2ecfd36a1a6d276dc71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6702475
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483590}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -17,10 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "components/signin/public/base/signin_buildflags.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/events/event.h"
 
-class AvatarToolbarButtonDelegate;
+class AvatarToolbarButtonStateManager;
 class Browser;
 class BrowserView;
 struct AccountInfo;
@@ -42,9 +43,10 @@ enum class AvatarDelayType {
 // This class takes care the Profile Avatar Button.
 // Primarily applies UI configuration.
 // It's data (text, icon, etc...) content are computed through the
-// `AvatarToolbarButtonDelegate`, when relying on Chrome and Profile changes in
-// order to adapt the expected content shown in the button.
-class AvatarToolbarButton : public ToolbarButton {
+// `AvatarToolbarButtonStateManager`, when relying on Chrome and Profile changes
+// in order to adapt the expected content shown in the button.
+class AvatarToolbarButton : public ToolbarButton,
+                            signin::IdentityManager::Observer {
   METADATA_HEADER(AvatarToolbarButton, ToolbarButton)
 
  public:
@@ -161,6 +163,16 @@ class AvatarToolbarButton : public ToolbarButton {
   FRIEND_TEST_ALL_PREFIXES(AvatarToolbarButtonTest,
                            HighlightMeetsMinimumContrast);
 
+  // signin::IdentityManager::Observer:
+  void OnPrimaryAccountChanged(
+      const signin::PrimaryAccountChangeEvent& event_details) override;
+  void OnExtendedAccountInfoUpdated(const AccountInfo& info) override;
+  void OnErrorStateOfRefreshTokenUpdatedForAccount(
+      const CoreAccountInfo& account_info,
+      const GoogleServiceAuthError& error,
+      signin_metrics::SourceForRefreshTokenOperation token_operation_source)
+      override;
+
   // ui::PropertyHandler:
   void AfterPropertyChange(const void* key, int64_t old_value) override;
 
@@ -176,7 +188,7 @@ class AvatarToolbarButton : public ToolbarButton {
   // Lists of observers.
   base::ObserverList<Observer, true> observer_list_;
 
-  std::unique_ptr<AvatarToolbarButtonDelegate> delegate_;
+  std::unique_ptr<AvatarToolbarButtonStateManager> state_manager_;
 
   const raw_ptr<Browser> browser_;
 
@@ -191,6 +203,15 @@ class AvatarToolbarButton : public ToolbarButton {
   // Setting this to true will stop the button reaction but the button will
   // remain in active state, not affecting it's UI in any way.
   bool button_action_disabled_ = false;
+
+  // Gaia Id of the account that was signed in from having it's choice
+  // remembered following a web sign-in event but waiting for the available
+  // account information to be fetched in order to show the sign in IPH.
+  GaiaId gaia_id_for_signin_choice_remembered_;
+
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observation_{this};
 
   base::WeakPtrFactory<AvatarToolbarButton> weak_ptr_factory_{this};
 };
