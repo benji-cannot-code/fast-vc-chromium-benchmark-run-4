d1cea849d68843bd922cf7543d054d3f82c17596
Brandon Wylie <wylieb@chromium.org>
2025-07-08 13:42:13 -0700

    [reader mode] Add bottom sheet scaffolding
    Initial scaffolding for the new reading mode bottom sheet UI. Eventually
    this will hold reading mode prefs and some other features.
    
    Bug: 428967967
    Change-Id: I86d46495f998a6dd618d9bc532c948a06a0beac8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697157
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483977}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeBottomSheetViewBinder.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.dom_distiller;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Binds the reader mode bottom sheet properties to the view. */
+@NullMarked
+public class ReaderModeBottomSheetViewBinder {
+    /**
+     * Binds PropertyKeys to View properties for the reader mode bottom sheet.
+     *
+     * @param model The PropertyModel for the View.
+     * @param view The View to be bound.
+     * @param key The key that's being bound.
+     */
+    public static void bind(PropertyModel model, ReaderModeBottomSheetView view, PropertyKey key) {}
+}
