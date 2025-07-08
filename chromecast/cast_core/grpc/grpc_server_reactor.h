0bb43a590c1d072e90dcc2daf49b61373343b9dd
Claudio DeSouza <cdesouza@chromium.org>
2025-07-08 16:31:13 -0700

    [CodeHealth] Clean up exit destructors in `GrpcServerReactor`
    This CL removes two static objects in the body of `GrpcServerReactor`,
    and enables `-Wexit-time-destructors` for this target to prevent other
    occurrences of it in the future. This complies with the style guide
    requirement that static objects should not run complex destructors
    during termination.
    
    https://google.github.io/styleguide/cppguide.html#Static_and_Global_Variables
    
    Bug: 40031409
    Change-Id: I3bef59ec213a64119d855db22fffe61b9132f724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6703737
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484065}

--- chromecast/cast_core/grpc/grpc_server_reactor.h
+++ chromecast/cast_core/grpc/grpc_server_reactor.h
@@ -19,6 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cast {
 namespace utils {
 
+// Returns a standard error for failed reads.
+const grpc::Status& GetStandardReadsFailedError();
+
+// Returns a standard error for failed writes.
+const grpc::Status& GetStandardWritesFailedError();
+
 // A base class for gRPC server reactors.
 template <typename TRequest, typename TResponse>
 class GrpcServerReactor : public grpc::ServerGenericBidiReactor {
@@ -94,12 +100,10 @@ class GrpcServerReactor : public grpc::ServerGenericBidiReactor {
 
   // Implements grpc::ServerGenericBidiReactor APIs.
   void OnReadDone(bool ok) override {
-    static const grpc::Status kReadsFailedError(grpc::StatusCode::ABORTED,
-                                                "Reads failed");
     DVLOG(1) << "Reads done: " << name() << ", ok=" << ok;
     if (!ok) {
       DVLOG(1) << "Reads failed: " << name();
-      OnRequestDone(kReadsFailedError);
+      OnRequestDone(GetStandardReadsFailedError());
       return;
     }
 
@@ -109,13 +113,11 @@ class GrpcServerReactor : public grpc::ServerGenericBidiReactor {
   }
 
   void OnWriteDone(bool ok) override {
-    static const grpc::Status kWritesFailedError(grpc::StatusCode::ABORTED,
-                                                 "Writes failed");
     DVLOG(1) << "Writes done: " << name() << ", ok=" << ok;
     response_byte_buffer_.reset();
     if (!ok) {
       DVLOG(1) << "Writes failed: " << name();
-      OnResponseDone(kWritesFailedError);
+      OnResponseDone(GetStandardWritesFailedError());
       return;
     }
 
