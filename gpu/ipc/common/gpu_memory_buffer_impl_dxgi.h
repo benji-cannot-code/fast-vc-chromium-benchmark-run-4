164aaa661ac826f32b714d51de1f0a161f9eeb27
Colin Blundell <blundell@chromium.org>
2025-07-08 08:10:48 -0700

    [//gpu] Remove GMBImplDXGI taking in GpuMemoryBufferId
    Bug: 404905709
    Change-Id: I26dd6c712b005c461f89d0e5b29cf658e5cdb1dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713415
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483775}

--- gpu/ipc/common/gpu_memory_buffer_impl_dxgi.h
+++ gpu/ipc/common/gpu_memory_buffer_impl_dxgi.h
@@ -97,7 +97,6 @@ class GPU_IPC_COMMON_EXPORT GpuMemoryBufferImplDXGI
       scoped_refptr<base::UnsafeSharedMemoryPool> pool = nullptr);
 
   GpuMemoryBufferImplDXGI(
-      gfx::GpuMemoryBufferId id,
       const gfx::Size& size,
       gfx::BufferFormat format,
       DestructionCallback callback,
