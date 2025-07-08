e11a9975b262eccd0db7ab57cc2abd7ea0ed8c6e
Ankush Singh <ankushkush@google.com>
2025-07-08 06:15:32 -0700

    Implement dice migration dialog in DiceMigrationService
    This CL adds in logic to display the DICe migration offer dialog.
    
    Dialog screenshot(Googlers-only):
    https://screenshot.googleplex.com/4BLK2fjStUZXKtY.png
    
    Bug: 399838468
    Change-Id: I228a9811fa7079d1a83ba62fd81940447f766f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6688495
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483708}

--- chrome/browser/ui/signin/dice_migration_service_factory.cc
+++ chrome/browser/ui/signin/dice_migration_service_factory.cc
@@ -6,11 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/signin/dice_migration_service_factory.h"
 
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/signin/dice_migration_service.h"
 #include "components/signin/public/base/signin_switches.h"
 
 DiceMigrationServiceFactory::DiceMigrationServiceFactory()
-    : ProfileKeyedServiceFactory("DiceMigrationService") {}
+    : ProfileKeyedServiceFactory("DiceMigrationService") {
+  DependsOn(IdentityManagerFactory::GetInstance());
+  DependsOn(SyncServiceFactory::GetInstance());
+}
 
 DiceMigrationServiceFactory::~DiceMigrationServiceFactory() = default;
 
