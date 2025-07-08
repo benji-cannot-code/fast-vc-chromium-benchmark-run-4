51bb4b38411768f6e75bcf99b0c6a8036edcdca5
Shin Kawamura <kawasin@google.com>
2025-07-08 06:38:45 -0700

    Add Android.ChildProcessRanking.{HighRank,LowRank}.Count UMA metrics
    We will monitor the number of high rank process to decide whether we
    need to optimize ChildProcessRanking.rebindHighRankConnections() more.
    
    Bug: 326330076
    Change-Id: I337b1a6419033b1765e4321c8704e678b8e245fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6712311
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1483722}

--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
@@ -515,7 +515,12 @@ public final class ChildProcessLauncherHelperImpl {
         LauncherThread.postDelayed(sDelayedBackgroundTask, delay);
         LauncherThread.post(
                 () -> {
-                    if (sBindingManager != null) sBindingManager.onSentToBackground();
+                    if (sBindingManager != null) {
+                        sBindingManager.onSentToBackground();
+                    }
+                    if (sSandboxedChildConnectionRanking != null) {
+                        sSandboxedChildConnectionRanking.recordProcessRanking();
+                    }
                 });
     }
 
