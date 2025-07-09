a68c7bf74aa141e203cd58bacd3592d7a243230f
Andrea Orru <andreaorru@chromium.org>
2025-07-08 20:12:32 -0700

    [Extensions] Ensure OnStopping called before DidStopServiceWorkerContext
    Always call OnStopping/OnStopped regardless of whether the service
    worker is tracked inside running_service_workers_.
    
    This ensures OnStopping/OnStopped events are issued to
    ServiceWorkerState even if the worker is shut down early.
    
    Bug: 40936639
    Change-Id: I09bcc6c6abc9dbfe7cb49364579763846a8f5254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716099
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484134}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -946,11 +946,7 @@ void ServiceWorkerContextCore::RemoveLiveVersion(int64_t id) {
     observer_list_->Notify(FROM_HERE,
                            &ServiceWorkerContextCoreObserver::OnStopped, id);
     for (auto& observer : sync_observer_list_->observers) {
-      const std::optional<ServiceWorkerRunningInfo> running_info =
-          wrapper_->GetRunningServiceWorkerInfo(id);
-      if (running_info.has_value()) {
-        observer.OnStopped(id, version->scope());
-      }
+      observer.OnStopped(id, version->scope());
     }
   }
 
@@ -1019,11 +1015,7 @@ void ServiceWorkerContextCore::DeleteAndStartOver(StatusCallback callback) {
   for (const auto& live_version_itr : live_versions_) {
     ServiceWorkerVersion* live_version = live_version_itr.second;
     for (auto& observer : sync_observer_list_->observers) {
-      const std::optional<ServiceWorkerRunningInfo> running_info =
-          wrapper_->GetRunningServiceWorkerInfo(live_version->version_id());
-      if (running_info.has_value()) {
-        observer.OnStopped(live_version->version_id(), live_version->scope());
-      }
+      observer.OnStopped(live_version->version_id(), live_version->scope());
     }
   }
 
@@ -1236,11 +1228,7 @@ void ServiceWorkerContextCore::OnRunningStateChanged(
                              &ServiceWorkerContextCoreObserver::OnStopped,
                              version->version_id());
       for (auto& observer : sync_observer_list_->observers) {
-        const std::optional<ServiceWorkerRunningInfo> running_info =
-            wrapper_->GetRunningServiceWorkerInfo(version->version_id());
-        if (running_info.has_value()) {
-          observer.OnStopped(version->version_id(), version->scope());
-        }
+        observer.OnStopped(version->version_id(), version->scope());
       }
       break;
     case blink::EmbeddedWorkerStatus::kStarting:
@@ -1260,11 +1248,7 @@ void ServiceWorkerContextCore::OnRunningStateChanged(
                              &ServiceWorkerContextCoreObserver::OnStopping,
                              version->version_id());
       for (auto& observer : sync_observer_list_->observers) {
-        const std::optional<ServiceWorkerRunningInfo> running_info =
-            wrapper_->GetRunningServiceWorkerInfo(version->version_id());
-        if (running_info.has_value()) {
-          observer.OnStopping(version->version_id(), version->scope());
-        }
+        observer.OnStopping(version->version_id(), version->scope());
       }
       break;
   }
