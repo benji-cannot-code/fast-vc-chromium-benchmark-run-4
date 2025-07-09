46e6b01428335480d1a73b9ae53e6fca459243a0
Asami Doi <asamidoi@chromium.org>
2025-07-09 09:39:50 -0700

    [ios] Turn down the LargeCapacityInSnapshotLRUCache experiment
    The issue is not reproducible anymore because it was fixed by
    refactoring the tab grid side.
    https://chromium-review.googlesource.com/c/chromium/src/+/6592101
    
    Remove the code for the LargeCapacityInSnapshotLRUCache experiment.
    
    Bug: 382129600
    Change-Id: I1e1ff659bbc3b1d693171013792d0d4755b60ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719328
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484412}

--- ios/chrome/browser/snapshots/model/features.mm
+++ ios/chrome/browser/snapshots/model/features.mm
@@ -8,11 +8,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 BASE_FEATURE(kSnapshotInSwift,
              "SnapshotInSwift",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kLargeCapacityInSnapshotLRUCache,
-             "LargeCapacityInSnapshotLRUCache",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-bool IsLargeCapacityInSnapshotLRUCacheEnabled() {
-  return base::FeatureList::IsEnabled(kLargeCapacityInSnapshotLRUCache);
-}
