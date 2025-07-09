f74003a81d9c4f06cee4003695869889f71711e5
Haihan Chen <haihan@google.com>
2025-07-09 08:23:19 -0700

    [Iframe DLP Rules Support] Populate frame_url_chain for download content analysis requests
    - Move helper method to a more appropriate place for use by download content analysis
    - Added a check to skip chrome-extension:// urls, this is for chrome pdf viewer especially where the frame url is a blob url. For getting the parent frame we also change it to `GetParentOrOuterDocumentOrEmbedder()`  so that we can traverse the frame tree upwards from guest views (i.e. chrome pdf viewer) and fenced frames. Similarly, we also skip blob and about:blank urls as these are not useful urls for scanning.
    
    Bug: 397480585
    Change-Id: I54e1c7e04513d18a1b0b68df3076d70a5f993829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700675
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484358}

--- chrome/browser/safe_browsing/download_protection/file_system_access_metadata.h
+++ chrome/browser/safe_browsing/download_protection/file_system_access_metadata.h
@@ -47,6 +47,8 @@ class FileSystemAccessMetadata : public DeepScanningMetadata {
   void SetCallback(CheckDownloadCallback callback);
   void ProcessScanResult(DownloadCheckResultReason reason,
                          DownloadCheckResult deep_scan_result) override;
+  google::protobuf::RepeatedPtrField<std::string> CollectFrameUrls()
+      const override;
   base::WeakPtr<FileSystemAccessMetadata> GetWeakPtr();
 
  private:
