4246aef7cf3eeec30553afe3c671f56e1728a777
Andreas Haas <ahaas@chromium.org>
2025-07-09 15:59:35 -0700

    Migrate PluginPlaceholder to gin::Wrappable
    gin::Wrappable gets refactored to improve security and memory
    management. This CL migrates PluginPlaceholder from the old
    gin::Wrappable to the new gin::Wrappable.
    
    Note that `PluginPlaceholder` needs a `Persistent` self
    reference now. The reason is that for `PluginPlaceholder`, C++
    does the memory management through RenderFrameObserver, even though the
    assumption with gin::Wrappable is that the GC does the memory
    management. So far this probably caused a dangling pointer that was
    never dereferenced, but with this CL `PluginPlaceholder` is also
    a garbage collected object. With the `Persistent` we make sure that the
    object stays alive as long as the C++ owner owns it.
    
    Bug: 345640553
    Change-Id: Ibd7ae58254d5f314f0a27f6c00ff557b9ef149c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714626
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484654}

--- gin/public/wrappable_pointer_tags.h
+++ gin/public/wrappable_pointer_tags.h
@@ -28,6 +28,7 @@ enum WrappablePointerTag : uint16_t {
   kGpuBenchmarking,                    // content::GpuBenchmarking
   kNetErrorPageController,             // NetErrorPageController
   kPDFPluginPlaceholder,               // PDFPluginPlaceholder
+  kPluginPlaceholder,                  // plugins::PluginPlaceholder
   kReadAnythingAppController,          // ReadAnythingAppController
   kSearchBoxBindings,                  // SearchBoxBindings
   kSkiaBenchmarking,                   // content::SkiaBenchmarking
