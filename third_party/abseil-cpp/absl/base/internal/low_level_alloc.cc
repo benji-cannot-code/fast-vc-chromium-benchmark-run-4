f1d22b6722345a9bf9f70f8ea78d1a2f2defc309
Mirko Bonadei <mbonadei@chromium.org>
2025-07-09 04:47:44 -0700

    Roll abseil_revision ddbb5170bb..cf2b48cf45
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/ddbb5170bb..cf2b48cf45
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/ddbb5170bb..cf2b48cf45
    
    Bug: None
    Change-Id: I07e5b64bab55ee9d741a84d882e79391d3e9a7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6717992
    Auto-Submit: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484255}

--- third_party/abseil-cpp/absl/base/internal/low_level_alloc.cc
+++ third_party/abseil-cpp/absl/base/internal/low_level_alloc.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "absl/base/internal/low_level_alloc.h"
 
+#include <optional>
 #include <type_traits>
 
 #include "absl/base/call_once.h"
