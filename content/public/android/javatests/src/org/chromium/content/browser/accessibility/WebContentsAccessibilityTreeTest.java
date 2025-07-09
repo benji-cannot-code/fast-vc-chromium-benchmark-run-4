fe709ffb369016773f82555c662c6080861a75d3
Addison Luh <aluh@chromium.org>
2025-07-09 08:51:33 -0700

    a11y: Re-enable ariaOption and nestedlist web contents tree tests
    Re-enable these tests:
    * WebContentsAccessibilityTreeTest#test_ariaOption
    * WebContentsAccessibilityTreeTest#nestedlist
    
    They are passing consistently locally with x64.
    
    Bug: crbug.com/349962563
    Change-Id: I55dfd21f8a5211a8458d2a90b9a3443cf1201192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716632
    Auto-Submit: Addison Luh <aluh@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484376}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
@@ -781,7 +781,6 @@ public class WebContentsAccessibilityTreeTest {
 
     @Test
     @SmallTest
-    @DisableIf.Build(supported_abis_includes = "x86_64", message = "https://crbug.com/349962563")
     public void test_ariaOption() {
         performAriaTest("aria-option.html");
     }
@@ -2275,7 +2274,6 @@ public class WebContentsAccessibilityTreeTest {
 
     @Test
     @SmallTest
-    @DisableIf.Build(supported_abis_includes = "x86_64", message = "https://crbug.com/349962563")
     public void test_nestedlist() {
         performHtmlTest("nestedlist.html");
     }
