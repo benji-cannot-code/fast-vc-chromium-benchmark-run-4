598163e54cb141a6eb8b4fafea52e1096733c81c
Mustaq Ahmed <mustaq@google.com>
2025-07-09 12:49:32 -0700

    End paint-holding before input to deflake UrlOverridingTest's.
    To mitigate a bug with user interactions with a stale page, we are
    planning to hold back input events from a page while paint-holding is
    active. Paint-holding is the browser feature to continue to show a
    snapshot of the pre-navigation page (instead of a blank page) until the
    renderer for the post-navigation page has pushed content to the GPU.
    
    The tests modified here send user input to the pages without waiting
    for renderer content. Because the tests are agnostic to paint-holding,
    we are ending paint-holding early to keep the tests working.
    
    Fixed: 428702162
    Bug: 40057499
    Change-Id: I9f17d0e65ec3841e345b7202a5e290f1ca2dabfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6705813
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484538}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -101,6 +101,7 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
+import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.components.browser_ui.modaldialog.ModalDialogView;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.external_intents.ExternalIntentsFeatures;
@@ -123,7 +124,6 @@ import org.chromium.content_public.browser.WebContentsObserver;
 import org.chromium.content_public.browser.test.util.DOMUtils;
 import org.chromium.content_public.browser.test.util.FencedFrameUtils;
 import org.chromium.content_public.browser.test.util.TouchCommon;
-import org.chromium.content_public.browser.test.util.WebContentsUtils;
 import org.chromium.net.NetError;
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.util.TestWebServer;
@@ -567,6 +567,7 @@ public class UrlOverridingTest {
                 () -> {
                     tab.loadUrl(loadParams);
                 });
+        ChromeTabUtils.waitForInteractable(tab);
 
         int preClickFinishTarget = params.willLoadSubframe ? 2 : 1;
         if (finishCallback.getCallCount() < preClickFinishTarget) {
@@ -1850,9 +1851,6 @@ public class UrlOverridingTest {
                                 + 1) // xhr callback
                 .when(mSpyRedirectHandler)
                 .currentRealtime();
-
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> WebContentsUtils.simulateEndOfPaintHolding(tab.getWebContents()));
 
         TouchCommon.singleClickView(tab.getView());
         // Wait for blocked Message to show.
