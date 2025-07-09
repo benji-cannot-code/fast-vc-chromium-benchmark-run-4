aa4f6516e5f58506fc4f5a612e22cc8e0545d28b
Xinghui Lu <xinghuilu@chromium.org>
2025-07-09 10:59:05 -0700

    [CLAMS] Only log OnDeviceModelSessionAlive histogram on new classification.
    Previously OnDeviceModelSessionAliveOnNewRequest is logged at both
    preclassification and inquiry time. This CL replaces the reset session
    call at inquiry time with a CHECK and clarifies that the caller is
    responsible to call reset session before calling inquire on-device model
    again.
    
    OBSOLETE_HISTOGRAMS=Replaced with SBClientPhishing.OnDeviceModelSessionAliveOnNewPreclassification
    
    Bug: 380928557
    Change-Id: Id6195bb92420c436d6f100be3d5aa177797f0cdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6709622
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484464}

--- chrome/browser/safe_browsing/android/client_side_detection_intelligent_scan_delegate_android.cc
+++ chrome/browser/safe_browsing/android/client_side_detection_intelligent_scan_delegate_android.cc
@@ -27,9 +27,8 @@ void ClientSideDetectionIntelligentScanDelegateAndroid::InquireOnDeviceModel(
   return;
 }
 
-void ClientSideDetectionIntelligentScanDelegateAndroid::ResetOnDeviceSession(
-    bool inquiry_complete) {
-  return;
+bool ClientSideDetectionIntelligentScanDelegateAndroid::ResetOnDeviceSession() {
+  return false;
 }
 
 }  // namespace safe_browsing
