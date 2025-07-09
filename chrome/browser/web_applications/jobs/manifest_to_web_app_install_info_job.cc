05ae7024b798c0455c0bf1186feaa950969ebe57
Dibyajyoti Pal <dibyapal@google.com>
2025-07-09 11:40:53 -0700

    [PWA] Remove UpdateWebAppInfoFromManifest(), cleanup unused functions
    This is the final CL in the migration of all commands using the job to
    generate the WebAppInstallInfo from the manifest. All code has been
    migrated, so this CL ends up removing the unused public APIs are other
    dependent functions.
    
    Fixed: 427565907
    Change-Id: I7132892c68fc990403994d81b83ecdf69593668d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719612
    Reviewed-by: May Siem <msiem@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484497}

--- chrome/browser/web_applications/jobs/manifest_to_web_app_install_info_job.cc
+++ chrome/browser/web_applications/jobs/manifest_to_web_app_install_info_job.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_icon_operations.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
-// TODO(crbug.com/427565907): Remove dependency on install utils after all
-// functions have been migrated to this file.
 #include "chrome/browser/web_applications/web_app_install_utils.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/browser/web_applications/web_contents/web_app_data_retriever.h"
@@ -574,7 +572,6 @@ void ManifestToWebAppInstallInfoJob::ParseManifestAndPopulateInfo() {
                                                    install_info_.get());
 
   // TODO(crbug.com/40185556): Confirm incoming icons to write to install_info_.
-  // TODO(crbug.com/427565907): Move from web_app_install_utils.cc to here.
   PopulateFileHandlerInfoFromManifest(
       manifest_->file_handlers, install_info_->scope, install_info_.get());
 
--- chrome/browser/web_applications/web_app_install_utils.h
+++ chrome/browser/web_applications/web_app_install_utils.h
@@ -23,12 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 
-namespace blink {
-namespace mojom {
-class Manifest;
-}  // namespace mojom
-}  // namespace blink
-
 namespace content {
 class WebContents;
 }
@@ -65,16 +59,6 @@ void PopulateFileHandlerInfoFromManifest(
         manifest_file_handlers,
     const GURL& app_scope,
     WebAppInstallInfo* web_app_info);
-
-// Update the given WebAppInstallInfo with information from the manifest.
-// Will sanitise the manifest fields to be suitable for installation to prevent
-// sites from using arbitrarily large amounts of disk space.
-void UpdateWebAppInfoFromManifest(const blink::mojom::Manifest& manifest,
-                                  WebAppInstallInfo* web_app_info);
-
-// Same as above, but returns a fresh WebAppInstallInfo.
-WebAppInstallInfo CreateWebAppInfoFromManifest(
-    const blink::mojom::Manifest& manifest);
 
 // Populate non-product icons in WebAppInstallInfo using the IconsMap. This
 // currently covers shortcut item icons and file handler icons. It ignores
