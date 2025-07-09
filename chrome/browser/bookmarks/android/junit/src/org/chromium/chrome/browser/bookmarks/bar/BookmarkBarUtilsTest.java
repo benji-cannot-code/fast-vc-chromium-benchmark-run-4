3678920f31da1bc4a7639d97b7bb3b08190228b4
Mark Schillaci <mschillaci@google.com>
2025-07-09 13:37:53 -0700

    [Bookmarks Bar] Remove add/set observer methods from BookmarkBarUtils
    This CL removes two methods that were previously used by the settings
    flow for Bookmark Bar but are not used anymore. Removing them was
    missed before, but they are only used in testing code so they can be
    removed.
    
    Bug: 424456738
    Change-Id: If03d99f7ab406e45ee09bfb51e4306cd7ce5ab7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716188
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484571}

--- chrome/browser/bookmarks/android/junit/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarUtilsTest.java
+++ chrome/browser/bookmarks/android/junit/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarUtilsTest.java
@@ -14,7 +14,6 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
@@ -250,20 +249,6 @@ public class BookmarkBarUtilsTest {
 
         BookmarkBarUtils.toggleSettingEnabled(mProfile);
         assertFalse(BookmarkBarUtils.isSettingEnabled(mProfile));
-    }
-
-    @Test
-    @SmallTest
-    public void testAddAndRemoveSettingObserver() {
-        verifyNoMoreInteractions(mPrefChangeRegistrar);
-
-        BookmarkBarUtils.addSettingObserver(mPrefChangeRegistrar, mPrefObserver);
-        verify(mPrefChangeRegistrar).addObserver(Pref.SHOW_BOOKMARK_BAR, mPrefObserver);
-        verifyNoMoreInteractions(mPrefChangeRegistrar);
-
-        BookmarkBarUtils.removeSettingObservers(mPrefChangeRegistrar);
-        verify(mPrefChangeRegistrar).removeObserver(Pref.SHOW_BOOKMARK_BAR);
-        verifyNoMoreInteractions(mPrefChangeRegistrar);
     }
 
     @Test
