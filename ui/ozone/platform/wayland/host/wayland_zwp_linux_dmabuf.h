acfa87699992bf9e0e888298678402a4df886e79
Kramer Ge <fangzhoug@chromium.org>
2025-07-09 14:09:38 -0700

    Implement dmabuf feedback main_device in v4
    Several compositors do not implement wl_drm v2 but implement
    linux_dmabuf at v4 or above, which is another indicator for what device
    to composite on.
    
    Note that wl_drm device path is still useful as Mir implements wl_drm
    but not linux_dmabuf.
    
    Change-Id: I0fa68bc830fb090c97db77c193b01b1705fecd37
    Bug: 396434686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6707684
    Reviewed-by: Orko Garai <orko@igalia.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484591}

--- ui/ozone/platform/wayland/host/wayland_zwp_linux_dmabuf.h
+++ ui/ozone/platform/wayland/host/wayland_zwp_linux_dmabuf.h
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 #include "ui/ozone/platform/wayland/common/wayland_util.h"
 
-struct zwp_linux_dmabuf_v1;
 struct zwp_linux_buffer_params_v1;
+struct zwp_linux_dmabuf_v1;
+struct zwp_linux_dmabuf_feedback_v1;
 
 namespace gfx {
 enum class BufferFormat : uint8_t;
@@ -99,10 +100,31 @@ class WaylandZwpLinuxDmabuf
                         wl_buffer* new_buffer);
   static void OnFailed(void* data, zwp_linux_buffer_params_v1* params);
 
+  // zwp_linux_dmabuf_feedback_v1_listener callbacks:
+  static void OnDone(void* data, zwp_linux_dmabuf_feedback_v1* feedback);
+  static void OnFormatTable(void* data,
+                            zwp_linux_dmabuf_feedback_v1* feedback,
+                            int32_t fd,
+                            uint32_t size);
+  static void OnMainDevice(void* data,
+                           zwp_linux_dmabuf_feedback_v1* feedback,
+                           struct wl_array* device);
+  static void OnTrancheDone(void* data, zwp_linux_dmabuf_feedback_v1* feedback);
+  static void OnTrancheTargetDevice(void* data,
+                                    zwp_linux_dmabuf_feedback_v1* feedback,
+                                    struct wl_array* device);
+  static void OnTrancheFormats(void* data,
+                               zwp_linux_dmabuf_feedback_v1* feedback,
+                               struct wl_array* indices);
+  static void OnTrancheFlags(void* data,
+                             zwp_linux_dmabuf_feedback_v1* feedback,
+                             uint32_t flags);
+
   // Holds pointer to the
   // zwp_linux_dmabuf_v1 Wayland
   // factory.
   const wl::Object<zwp_linux_dmabuf_v1> zwp_linux_dmabuf_;
+  wl::Object<zwp_linux_dmabuf_feedback_v1> zwp_linux_dmabuf_feedback_;
 
   // Non-owned.
   const raw_ptr<WaylandConnection> connection_;
