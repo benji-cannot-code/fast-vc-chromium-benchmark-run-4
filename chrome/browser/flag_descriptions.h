3b621d3c01028ff171e254e8247f71b49bd01794
Dibyajyoti Pal <dibyapal@google.com>
2025-07-09 08:01:51 -0700

    [App Update] Rename feature flag to predictable app updating
    The app updating design has changed directions from using an update
    token from being parsed to using changes in icon urls for making them
    predictable. As such, rename the feature flag to make the intentions of
    the flag clearer.
    
    Bug: 403252297
    Change-Id: I0a61c9522b71a89f28d7911c9a9e9d4fb7413a38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715817
    Reviewed-by: May Siem <msiem@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484343}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1133,8 +1133,8 @@ extern const char kEnableResamplingScrollEventsExperimentalPredictionName[];
 extern const char
     kEnableResamplingScrollEventsExperimentalPredictionDescription[];
 
-extern const char kEnableWebAppUpdateTokenParsingName[];
-extern const char kEnableWebAppUpdateTokenParsingDescription[];
+extern const char kEnableWebAppPredictableAppUpdatingName[];
+extern const char kEnableWebAppPredictableAppUpdatingDescription[];
 
 extern const char kEnableZeroCopyTabCaptureName[];
 extern const char kEnableZeroCopyTabCaptureDescription[];
