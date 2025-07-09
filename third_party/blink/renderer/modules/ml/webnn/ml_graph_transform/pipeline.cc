1f54e8f9971b989eca218c4b4ced719d1919702d
JianxiaoLuIntel <jianxiao.lu@intel.com>
2025-07-08 20:03:07 -0700

    WebNN: Detect and transform QDQ
    This detects potential QDQ (Quantize-Dequantize) patterns in the graph
    and transform it to suitable form for backend fusion.
    For example, here is the original graph:
    
        ...               ...
          \               /
           DQ(0)         DQ(1)
            \           /
        transpose(0)  transpose(1)
              \      /
               conv2d(center operator)
                 \
              transpose(2)
                   \
                    Q
    
    The TFlite(LiteRT) backend can fuse conv2d with DQ inputs and Q outputs,
    so we should swap the position of DQ(Q) and transpose to make it
    suitable for backend fusion. Like this:
    
        ...               ...
          \               /
       transpose(0)     transpose(1)
            \           /
            DQ(0)      DQ(1)
              \      /
               conv2d(center operator)
                 \
                  Q
                   \
                transpose(2)
    
    Bug: 414330683
    Change-Id: I09f94483e48b88a9d4ee487b9861dd877103ec02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634994
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Jianxiao Lu <jianxiao.lu@intel.com>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484129}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_transform/pipeline.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_transform/pipeline.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_transform/pipeline.h"
 
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_transform/layout_transformer.h"
+#include "third_party/blink/renderer/modules/ml/webnn/ml_graph_transform/qdq_detection_transformer.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_transform/transpose_elimination_transformer.h"
 
 namespace blink {
@@ -19,8 +20,13 @@ void MLGraphTransformPipeline::Trace(Visitor* visitor) const {
 }
 
 void MLGraphTransformPipeline::InitTransformers(MLGraphBuilder* graph_builder) {
+  // Required transformers for backends to consume the graph.
   transformers_.push_back(
       MakeGarbageCollected<LayoutTransformer>(graph_builder));
+
+  // Non-essential transformers. For better performance.
+  transformers_.push_back(
+      MakeGarbageCollected<QDQDetectionTransformer>(graph_builder));
   transformers_.push_back(
       MakeGarbageCollected<TransposeEliminationTransformer>(graph_builder));
 }
