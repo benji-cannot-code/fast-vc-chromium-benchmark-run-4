810c1adedc69604d18ec1c71433d01a3cd981087
Addison Luh <aluh@chromium.org>
2025-07-09 14:36:43 -0700

    a11y: Support simple contenteditable text formatting in clank
    For contenteditables with only static text children, we can simply
    iterate through them to get text formatting information.
    
    Bug: 427578244
    Change-Id: I2b4a82f7512b7c75a4059eae8cb937d093c5d202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6669358
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Addison Luh <aluh@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484610}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -887,7 +887,8 @@ void BrowserAccessibilityAndroid::AccumulateSubstringTextContentUTF16(
 
   // First, always return the `value` attribute if this is an input field.
   std::u16string value = GetValueForControl();
-  if (ShouldExposeValueAsName(value)) {
+  const bool is_non_atomic_text_field = IsNonAtomicTextField();
+  if (ShouldExposeValueAsName(value) && !is_non_atomic_text_field) {
     text = std::move(value);
     return;
   }
@@ -904,7 +905,9 @@ void BrowserAccessibilityAndroid::AccumulateSubstringTextContentUTF16(
   // In the case of accessible name from kAttribute, the aria-label will be
   // mapped to one of the container title, content description or supplemental
   // description, we should exclude aria-label from mapping to text.
-  text = IsAccessibleNameFromAttribute() ? u"" : GetNameAsString16();
+  if (!IsAccessibleNameFromAttribute() && !is_non_atomic_text_field) {
+    text = GetNameAsString16();
+  }
   if (ui::IsRangeValueSupported(GetRole())) {
     // For controls that support range values such as sliders, when a non-empty
     // name is present (e.g. a label), append this to the value so both the
@@ -921,7 +924,7 @@ void BrowserAccessibilityAndroid::AccumulateSubstringTextContentUTF16(
     } else if (!value.empty()) {
       text = std::move(value);
     }
-  } else if (text.empty()) {
+  } else if (text.empty() && !is_non_atomic_text_field) {
     // When a node does not have a name (e.g. a label), use its value instead.
     text = std::move(value);
   }
@@ -972,7 +975,7 @@ void BrowserAccessibilityAndroid::AccumulateSubstringTextContentUTF16(
   // Only for roles that do not support naming with child content, we loop
   // through the children, in order to populate the visual content (use Android
   // text API), in addition to populating the aria label information.
-  if (text.size() == 0 && !ui::SupportsNamingWithChildContent(GetRole()) &&
+  if (text.empty() && !ui::SupportsNamingWithChildContent(GetRole()) &&
       ((HasOnlyTextChildren() && !HasListMarkerChild()) ||
        (IsFocusable() && HasOnlyTextAndImageChildren()))) {
     for (auto it = InternalChildrenBegin(); it != InternalChildrenEnd(); ++it) {
@@ -982,6 +985,10 @@ void BrowserAccessibilityAndroid::AccumulateSubstringTextContentUTF16(
         break;
       }
     }
+  }
+
+  if (is_non_atomic_text_field && text.empty()) {
+    text = std::move(value);
   }
 
   if (text.empty() &&
