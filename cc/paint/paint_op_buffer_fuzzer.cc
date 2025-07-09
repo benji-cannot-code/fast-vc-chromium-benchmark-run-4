61069aad25eb4de0cafb3d0e9657551cbbabc347
Liza Burakova <liza@chromium.org>
2025-07-09 07:25:46 -0700

    Add ServicePaintCache to paint op buffer fuzzer.
    The fuzzer sometimes crashes prematurely because the ServicePaintCache
    is a nullptr and it attempts to dereference it.
    
    Bug: 353557023
    Change-Id: Ic572fdf35375e038c3eca24574e1150f761f64a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716118
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484320}

--- cc/paint/paint_op_buffer_fuzzer.cc
+++ cc/paint/paint_op_buffer_fuzzer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/logging.h"
+#include "cc/paint/paint_cache.h"
 #include "cc/paint/paint_op.h"
 #include "cc/test/transfer_cache_test_helper.h"
 
@@ -21,8 +22,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   std::vector<uint8_t> scratch_buffer;
   cc::TransferCacheTestHelper transfer_cache_helper;
+  cc::ServicePaintCache service_paint_cache;
   cc::PaintOp::DeserializeOptions options{
       .transfer_cache = &transfer_cache_helper,
+      .paint_cache = &service_paint_cache,
       .scratch_buffer = scratch_buffer};
   cc::PaintOpBuffer::MakeFromMemory(data, size, options);
 
