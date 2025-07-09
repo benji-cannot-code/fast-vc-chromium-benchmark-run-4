208139dbd93f283415db2330e2660e3de767fe1c
Philip Jägenstedt <foolip@chromium.org>
2025-07-09 09:29:10 -0700

    Add WebDX use counter for font metric overrides
    Bug: 366475997
    Change-Id: Ife130e08748ed3813b8342f157952d1a8b92510e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715126
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484404}

--- third_party/blink/renderer/core/css/parser/at_rule_descriptor_parser.cc
+++ third_party/blink/renderer/core/css/parser/at_rule_descriptor_parser.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/parser/css_variable_parser.h"
 #include "third_party/blink/renderer/core/css/properties/css_parsing_utils.h"
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 
 namespace blink {
 
@@ -391,6 +392,9 @@ CSSValue* AtRuleDescriptorParser::ParseFontFaceDescriptor(
     case AtRuleDescriptorID::DescentOverride:
     case AtRuleDescriptorID::LineGapOverride:
       parsed_value = ConsumeFontMetricOverride(stream, context);
+      if (parsed_value && IsUseCounterEnabledForMode(context.Mode())) {
+        context.Count(WebDXFeature::kFontMetricOverrides);
+      }
       break;
     case AtRuleDescriptorID::SizeAdjust:
       parsed_value = css_parsing_utils::ConsumePercent(
