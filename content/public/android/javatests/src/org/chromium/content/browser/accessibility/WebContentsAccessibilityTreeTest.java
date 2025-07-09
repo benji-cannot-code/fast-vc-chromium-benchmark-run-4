ea84d69d133fb91945d238caaa0b4015dd6f1671
Addison Luh <aluh@chromium.org>
2025-07-09 08:52:41 -0700

    a11y: Re-enable textColorsAndStyles web contents tree test
    Re-enable WebContentsAccessibilityTreeTest.test_textColorsAndStyles.
    It's passing consistently locally.
    
    Bug: crbug.com/1224422
    Change-Id: Ibc898599f6ad5893488c59294416044ef6302506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715624
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Addison Luh <aluh@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484377}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
@@ -2665,7 +2665,6 @@ public class WebContentsAccessibilityTreeTest {
 
     @Test
     @SmallTest
-    @DisableIf.Build(supported_abis_includes = "x86", message = "https://crbug.com/1224422")
     public void test_textColorsAndStyles() {
         performHtmlTest("text-colors-and-styles.html");
     }
