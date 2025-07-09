e78f6f71fcf19222dfbc7a473a0c1baadd8b4383
Peter Kvitek <kvitekp@chromium.org>
2025-07-09 13:19:58 -0700

    [headless] Move virtual time related tests to a dedicated sub-directory
    This CL moves all tests that use virtual time to the 'virtual-time'
    sub-directory to keep them separate from the regular tests.
    
    Bug: 429039219
    Change-Id: Ic1b52b9191cf0cefbe12046b161c7e67ccf36dc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715839
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484561}

---
+++ headless/test/data/protocol/virtual-time/renderer-override-title-js-enabled.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startWithFrameControl(
+      'Tests renderer: override title with JavaScript enabled.');
+
+  let RendererTestHelper =
+      await testRunner.loadScript('../helpers/renderer-test-helper.js');
+  let {httpInterceptor, frameNavigationHelper, virtualTimeController} =
+      await (new RendererTestHelper(testRunner, dp, page)).init();
+
+  await dp.Emulation.setScriptExecutionDisabled({value: false});
+
+  httpInterceptor.addResponse(`http://example.com/foobar`, `<html>
+        <head>
+          <title>JavaScript is off</title>
+          <script language="JavaScript">
+            function settitle() {
+              document.title = 'JavaScript is on';
+            }
+            </script>
+          </head>
+        <body onload="settitle()">
+          Hello, World!
+        </body>
+      </html>`);
+
+
+  await virtualTimeController.initialize(1000);
+  await frameNavigationHelper.navigate('http://example.com/foobar');
+  await virtualTimeController.grantTime(500);
+
+  testRunner.log(await session.evaluate('document.title'));
+  testRunner.completeTest();
+})
