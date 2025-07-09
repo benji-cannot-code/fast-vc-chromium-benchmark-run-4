00f2fead5c2aed98196a787fc3aa2abf4dba5b47
Nathan Memmott <memmott@chromium.org>
2025-07-09 13:26:42 -0700

    Writing Assistance: Correctly determine if component is installed
    We were using ComponentUpdateService's GetComponentIDs to determine if a
    component is installed or not, but this only tells us if a component is
    registered or not. This caused issues where we weren't reporting
    download progress for components mid download.
    
    This fixes it by using ComponentUpdateService's GetComponentDetails
    which returns the current status of a component.
    
    Fixed: 427101721
    Change-Id: I48a6cc3cf3369f40f55f49b3be453c1862e62747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6711474
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484563}

--- chrome/browser/ai/ai_test_utils.cc
+++ chrome/browser/ai/ai_test_utils.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ai/ai_test_utils.h"
 
+#include <cstdint>
+#include <utility>
+
 #include "chrome/browser/ai/ai_manager.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "third_party/blink/public/mojom/ai/model_download_progress_observer.mojom.h"
@@ -46,16 +49,35 @@ AITestUtils::FakeMonitor::BindNewPipeAndPassRemote() {
 
 void AITestUtils::FakeMonitor::ExpectReceivedUpdate(
     uint64_t expected_downloaded_bytes,
+    uint64_t expected_total_bytes,
+    base::OnceClosure callback) {
+  EXPECT_CALL(mock_monitor_, OnDownloadProgressUpdate(testing::_, testing::_))
+      .WillOnce([callback = std::move(callback), expected_downloaded_bytes,
+                 expected_total_bytes](uint64_t downloaded_bytes,
+                                       uint64_t total_bytes) mutable {
+        EXPECT_EQ(downloaded_bytes, expected_downloaded_bytes);
+        EXPECT_EQ(total_bytes, expected_total_bytes);
+        std::move(callback).Run();
+      });
+}
+
+void AITestUtils::FakeMonitor::ExpectReceivedUpdate(
+    uint64_t expected_downloaded_bytes,
     uint64_t expected_total_bytes) {
   base::RunLoop download_progress_run_loop;
-  EXPECT_CALL(mock_monitor_, OnDownloadProgressUpdate(testing::_, testing::_))
-      .WillOnce(
-          testing::Invoke([&](uint64_t downloaded_bytes, uint64_t total_bytes) {
-            EXPECT_EQ(downloaded_bytes, expected_downloaded_bytes);
-            EXPECT_EQ(total_bytes, expected_total_bytes);
-            download_progress_run_loop.Quit();
-          }));
+  ExpectReceivedUpdate(expected_downloaded_bytes, expected_total_bytes,
+                       download_progress_run_loop.QuitClosure());
   download_progress_run_loop.Run();
+}
+
+void AITestUtils::FakeMonitor::ExpectReceivedNormalizedUpdate(
+    uint64_t expected_downloaded_bytes,
+    uint64_t expected_total_bytes,
+    base::OnceClosure callback) {
+  ExpectReceivedUpdate(AIUtils::NormalizeModelDownloadProgress(
+                           expected_downloaded_bytes, expected_total_bytes),
+                       AIUtils::kNormalizedDownloadProgressMax,
+                       std::move(callback));
 }
 
 void AITestUtils::FakeMonitor::ExpectReceivedNormalizedUpdate(
