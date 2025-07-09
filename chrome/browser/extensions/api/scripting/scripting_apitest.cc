30f012e6f464cdd6b5edbb24e2f6f4bb2a13ec5c
James Cook <jamescook@chromium.org>
2025-07-09 15:23:49 -0700

    extensions: Expand chrome.tabs.query stub on desktop Android
    In particular, handle querying tabs by URL. With this addition several
    tests in ScriptingAPITest now pass.
    
    This is still throwaway code, pending a real implementation of
    chrome.tabs for desktop Android. But it gets us some more coverage in
    the short term.
    
    Bug: 371432155
    Test: android_browsertests --gtest_filter=*ScriptingAPITest*
    Change-Id: Ifa8b84dd3f68433185be9ec9908d7cade2d03e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719676
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484633}

--- chrome/browser/extensions/api/scripting/scripting_apitest.cc
+++ chrome/browser/extensions/api/scripting/scripting_apitest.cc
@@ -120,10 +120,6 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPITest, GetContentScripts) {
   ASSERT_TRUE(RunExtensionTest("scripting/get_scripts")) << message_;
 }
 
-// TODO(crbug.com/371432155): Most of the tests in this file are skipped on
-// desktop Android because they use the chrome.tabs API, which hasn't been
-// ported yet.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 IN_PROC_BROWSER_TEST_F(ScriptingAPITest, MainFrameTests) {
   OpenURLInCurrentTab(embedded_test_server()->GetURL(
       "example.com", "/extensions/main_world_script_flag.html"));
@@ -135,6 +131,10 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPITest, MainFrameTests) {
       << message_;
 }
 
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+// TODO(crbug.com/371432404): Most of the tests in this file are skipped on
+// desktop Android because they use the chrome.webNavigation API, which hasn't
+// been ported yet.
 IN_PROC_BROWSER_TEST_F(ScriptingAPITest, SubFramesTests) {
   OpenURLInCurrentTab(
       embedded_test_server()->GetURL("a.com", "/iframe_cross_site.html"));
@@ -153,8 +153,8 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPITest, NestedWebContents) {
   OpenURLInCurrentTab(
       embedded_test_server()->GetURL("a.com", "/iframe_about_blank.html"));
 
-  content::RenderFrameHost* iframe_host = content::ChildFrameAt(
-      browser()->tab_strip_model()->GetActiveWebContents(), 0);
+  content::RenderFrameHost* iframe_host =
+      content::ChildFrameAt(GetActiveWebContents(), 0);
   ASSERT_TRUE(iframe_host);
   content::WebContents* inner_web_contents =
       content::CreateAndAttachInnerContents(iframe_host);
@@ -165,6 +165,7 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPITest, NestedWebContents) {
   // From there, the test continues in the JS.
   ASSERT_TRUE(RunExtensionTest("scripting/nested_web_contents")) << message_;
 }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 #if BUILDFLAG(ENABLE_PDF)
 class ScriptingAPIOopifPdfTest : public ScriptingAPITest {
@@ -188,6 +189,9 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPIOopifPdfTest, PdfFrames) {
 }
 #endif  // BUILDFLAG(ENABLE_PDF)
 
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+// TODO(crbug.com/371432404): Skipped on desktop Android because the test uses
+// the chrome.webNavigation API, which hasn't been ported yet.
 IN_PROC_BROWSER_TEST_F(ScriptingAPITest, CSSInjection) {
   OpenURLInCurrentTab(
       embedded_test_server()->GetURL("example.com", "/simple.html"));
@@ -204,6 +208,7 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPITest, CSSInjection) {
 IN_PROC_BROWSER_TEST_F(ScriptingAPITest, CSSRemoval) {
   ASSERT_TRUE(RunExtensionTest("scripting/remove_css")) << message_;
 }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 IN_PROC_BROWSER_TEST_F(ScriptingAPITest, RegisterContentScripts) {
   ASSERT_TRUE(RunExtensionTest("scripting/register_scripts")) << message_;
@@ -228,7 +233,6 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPITest, DynamicContentScriptsMainWorld) {
   ASSERT_TRUE(RunExtensionTest("scripting/dynamic_scripts_main_world"))
       << message_;
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 // Unregisters a pending script and verifies that the script is unregistered
 // and doesn't inject.
