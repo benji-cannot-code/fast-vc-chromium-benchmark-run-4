994ed5a7c4bc40c94518256d9fcd94ceb2b280c1
Gauthier Ambard <gambard@chromium.org>
2025-07-09 02:01:33 -0700

    [iOS] Remove processpool checks from WebView creation utils
    The property is deprecated because it is no longer relevant.
    
    Bug: None
    Change-Id: Icfeddf3bedc398e48d7735fffa2470de8efdda97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6717950
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484221}

--- ios/web/web_state/web_view_internal_creation_util.mm
+++ ios/web/web_state/web_view_internal_creation_util.mm
@@ -15,27 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
-namespace {
-
-// Verifies the preconditions for creating a WKWebView. Must be called before
-// a WKWebView is allocated. Not verifying the preconditions before creating
-// a WKWebView will lead to undefined behavior.
-void VerifyWKWebViewCreationPreConditions(
-    BrowserState* browser_state,
-    WKWebViewConfiguration* configuration) {
-  DCHECK(browser_state);
-  DCHECK(configuration);
-  WKWebViewConfigurationProvider& config_provider =
-      WKWebViewConfigurationProvider::FromBrowserState(browser_state);
-  DCHECK_EQ([config_provider.GetWebViewConfiguration() processPool],
-            [configuration processPool]);
-}
-
-}  // namespace
-
 WKWebView* BuildWKWebViewForQueries(WKWebViewConfiguration* configuration,
                                     BrowserState* browser_state) {
-  VerifyWKWebViewCreationPreConditions(browser_state, configuration);
+  CHECK(browser_state);
+  CHECK(configuration);
   return [[WKWebView alloc] initWithFrame:CGRectZero
                             configuration:configuration];
 }
@@ -46,7 +29,8 @@ WKWebView* BuildWKWebView(CGRect frame,
                           UserAgentType user_agent_type,
                           id<CRWInputViewProvider> input_view_provider,
                           id<CRWEditMenuBuilder> edit_menu_builder) {
-  VerifyWKWebViewCreationPreConditions(browser_state, configuration);
+  CHECK(browser_state);
+  CHECK(configuration);
 
   GetWebClient()->PreWebViewCreation();
 
