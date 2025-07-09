bde1f594917750656914ae12c10e951641155e60
Hailey Wang <haileywang@google.com>
2025-07-09 14:15:58 -0700

    [STG][Enterprise] Add sharing disabled ui config
    Bug: 429036115
    Change-Id: Iecaa470751b4995b1f2b9dd4c80a1da6651564a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6711416
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484599}

--- components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/DataSharingManageUiConfig.java
+++ components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/DataSharingManageUiConfig.java
@@ -25,6 +25,7 @@ public class DataSharingManageUiConfig {
     private final @Nullable GURL mLearnAboutBlockedAccounts;
     private final @Nullable GURL mActivityLogsUrl;
     private final @Nullable DataSharingUiConfig mCommonConfig;
+    private final boolean mIsSharingDisabled;
 
     /** Callback interface for data sharing Manage UI events. */
     public interface ManageCallback {
@@ -58,6 +59,7 @@ public class DataSharingManageUiConfig {
         this.mActivityLogsUrl = builder.mActivityLogsUrl;
         this.mManageCallback = builder.mManageCallback;
         this.mCommonConfig = builder.mCommonConfig;
+        this.mIsSharingDisabled = builder.mIsSharingDisabled;
     }
 
     public @Nullable GroupToken getGroupToken() {
@@ -80,6 +82,10 @@ public class DataSharingManageUiConfig {
         return mCommonConfig;
     }
 
+    public boolean getIsSharingDisabled() {
+        return mIsSharingDisabled;
+    }
+
     // Builder class
     public static class Builder {
         private @Nullable GroupToken mGroupToken;
@@ -87,6 +93,7 @@ public class DataSharingManageUiConfig {
         private @Nullable GURL mActivityLogsUrl;
         private @Nullable ManageCallback mManageCallback;
         private @Nullable DataSharingUiConfig mCommonConfig;
+        private boolean mIsSharingDisabled;
 
         /**
          * Sets the group token for the data sharing group.
@@ -135,6 +142,16 @@ public class DataSharingManageUiConfig {
          */
         public Builder setCommonConfig(DataSharingUiConfig commonConfig) {
             this.mCommonConfig = commonConfig;
+            return this;
+        }
+
+        /**
+         * Sets whether sharing is disabled.
+         *
+         * @param isSharingDisabled True if sharing is disabled.
+         */
+        public Builder setIsSharingDisabled(boolean isSharingDisabled) {
+            this.mIsSharingDisabled = isSharingDisabled;
             return this;
         }
 
