1e2462bd10962af78556002b73228d7a29ac7b71
Gauthier Ambard <gambard@chromium.org>
2025-07-09 04:03:30 -0700

    [iOS][Diamond] Move prototype files to dedicated folder
    It would be easier to cleanup if all the prototype files are in the
    same folder.
    
    Bug: 429955447
    Change-Id: Ie1184869320c8722d331227196758d2baf91eed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718189
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484248}

--- ios/chrome/browser/shared/public/prototypes/diamond/chrome_app_bar_prototype.mm
+++ ios/chrome/browser/shared/public/prototypes/diamond/chrome_app_bar_prototype.mm
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/chrome_app_bar_prototype.h"
+#import "ios/chrome/browser/shared/public/prototypes/diamond/chrome_app_bar_prototype.h"
 
-#import "ios/chrome/browser/shared/public/features/diamond_prototype_utils.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/browser/shared/public/prototypes/diamond/utils.h"
 #import "ios/chrome/browser/shared/ui/symbols/buildflags.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
--- ios/chrome/browser/shared/public/prototypes/diamond/utils.mm
+++ ios/chrome/browser/shared/public/prototypes/diamond/utils.mm
@@ -3,6 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/shared/public/features/diamond_prototype_utils.h"
+#import "ios/chrome/browser/shared/public/prototypes/diamond/utils.h"
 
 const CGFloat kChromeAppBarPrototypeHeight = 58;
