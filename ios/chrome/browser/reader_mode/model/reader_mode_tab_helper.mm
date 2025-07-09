584a3ee45e75a5ba2c1935eb51715591671d5c0e
Nohemi Fernandez <fernandex@google.com>
2025-07-09 09:34:27 -0700

    [iOS] Migrate Reading Mode distiller metrics to independent class.
    Moves distillation state and latency metrics to ReaderModeMetricsHelper
    and tracks the relevant states { DistillationStarted,
    DistillationCompleted, ReaderShown } in "IOS.ReaderMode.State".
    
    Bug: 429174292
    Change-Id: Ieccd5ba440759ff13e225ce7e319bb91b069ee20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6709703
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484406}

--- ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.mm
+++ ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.mm
@@ -33,41 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "net/base/apple/url_conversions.h"
-#import "services/metrics/public/cpp/ukm_builders.h"
 
 namespace {
 
-// Records the time elapsed from the execution of the distillation JavaScript to
-// the result callback.
-void RecordReaderModeDistillationLatency(base::TimeDelta elapsed,
-                                         ukm::SourceId source_id) {
-  UMA_HISTOGRAM_TIMES(kReaderModeDistillerLatencyHistogram, elapsed);
-  if (source_id != ukm::kInvalidSourceId) {
-    ukm::builders::IOS_ReaderMode_Distiller_Latency(source_id)
-        .SetLatency(elapsed.InMilliseconds())
-        .Record(ukm::UkmRecorder::Get());
-  }
-}
-
-// Records whether the given source ID for a navigation is distillable or not.
-void RecordReaderModeDistillationResult(bool is_distillable,
-                                        ukm::SourceId source_id) {
-  if (source_id != ukm::kInvalidSourceId) {
-    ukm::builders::IOS_ReaderMode_Distiller_Result(source_id)
-        .SetResult(static_cast<int64_t>(
-            is_distillable ? ReaderModeDistillerResult::kPageIsDistillable
-                           : ReaderModeDistillerResult::kPageIsNotDistillable))
-        .Record(ukm::UkmRecorder::Get());
-  }
-}
-
 // Helper function to generate the snackbar message.
-NSString* GenerateSnackbarMessage(bool is_distillable_page,
-                                  base::TimeDelta distillation_latency) {
+NSString* GenerateSnackbarMessage(bool is_distillable_page) {
   std::string message = "\nDistillation Result: ";
   message += (is_distillable_page ? "Distillable" : "Not Distillable");
-  message += "\nDistillation Latency: " +
-             base::NumberToString(distillation_latency.InMilliseconds()) + "ms";
   return base::SysUTF8ToNSString(message);
 }
 
@@ -231,6 +203,10 @@ void ReaderModeTabHelper::ResetUrlEligibility(const GURL& url) {
   if (trigger_reader_mode_timer_.IsRunning()) {
     trigger_reader_mode_timer_.Stop();
     metrics_helper_.CancelReaderHeuristicRecording();
+  } else {
+    // If there is no trigger in progress ensure any metrics related to a
+    // past navigation have been recorded.
+    metrics_helper_.Flush();
   }
 
   // Do not reset URL eligibility for same-page navigations.
@@ -241,6 +217,8 @@ void ReaderModeTabHelper::ResetUrlEligibility(const GURL& url) {
 
 void ReaderModeTabHelper::ReaderModeContentDidLoadData(
     ReaderModeContentTabHelper* reader_mode_content_tab_helper) {
+  metrics_helper_.RecordReaderShown();
+
   // Generic snapshot image generation on side-swipe has a long tail latency.
   // Force update the snapshot storage to ensure that the latest snapshot is
   // presented before a transition.
@@ -331,7 +309,6 @@ void ReaderModeTabHelper::TriggerReaderModeHeuristic(const GURL& url) {
 }
 
 void ReaderModeTabHelper::PageDistillationCompleted(
-    base::TimeTicks start_time,
     const GURL& page_url,
     const std::string& html,
     const std::vector<DistillerViewerInterface::ImageInfo>& images,
@@ -342,21 +319,17 @@ void ReaderModeTabHelper::PageDistillationCompleted(
   if (!web_state_ || web_state_->IsBeingDestroyed()) {
     return;
   }
-  const ukm::SourceId source_id =
-      ukm::GetSourceIdForWebStateDocument(web_state_);
-  const base::TimeDelta distillation_latency =
-      base::TimeTicks::Now() - start_time;
-  RecordReaderModeDistillationLatency(distillation_latency, source_id);
 
   bool is_distillable_page = !html.empty();
-  RecordReaderModeDistillationResult(is_distillable_page, source_id);
+  metrics_helper_.RecordReaderDistillerCompleted(
+      is_distillable_page ? ReaderModeDistillerResult::kPageIsDistillable
+                          : ReaderModeDistillerResult::kPageIsNotDistillable);
 
   if (IsReaderModeSnackbarEnabled()) {
     // Show a snackbar with the heuristic result, latency and page distillation
     // result and latency.
     MDCSnackbarMessage* message = [MDCSnackbarMessage
-        messageWithText:GenerateSnackbarMessage(is_distillable_page,
-                                                distillation_latency)];
+        messageWithText:GenerateSnackbarMessage(is_distillable_page)];
     message.duration = MDCSnackbarMessageDurationMax;
     [snackbar_handler_ showSnackbarMessage:message];
   }
@@ -380,6 +353,8 @@ void ReaderModeTabHelper::PageDistillationCompleted(
 }
 
 void ReaderModeTabHelper::CreateReaderModeWebState() {
+  metrics_helper_.RecordReaderDistillerTriggered();
+
   web::WebState::CreateParams create_params = web::WebState::CreateParams(
       ProfileIOS::FromBrowserState(web_state_->GetBrowserState())
           ->GetOffTheRecordProfile());
@@ -397,8 +372,7 @@ void ReaderModeTabHelper::CreateReaderModeWebState() {
       reader_mode_web_state_.get(), distiller_service_,
       std::move(distiller_page), web_state_->GetLastCommittedURL(),
       base::BindRepeating(&ReaderModeTabHelper::PageDistillationCompleted,
-                          weak_ptr_factory_.GetWeakPtr(),
-                          base::TimeTicks::Now())));
+                          weak_ptr_factory_.GetWeakPtr())));
 }
 
 void ReaderModeTabHelper::DestroyReaderModeWebState() {
