4165d5f100e495148cd2f2eb2fc474704921ea6f
Munira Tursunova <moonira@google.com>
2025-07-09 05:40:08 -0700

    Revert "Refactor RuleSet creation."
    This reverts commit 5e152e4dbac2abda05b7ec0734e1c02688087bf9.
    
    Reason for revert: This CL breaks editing container queries in DevTools.
    
    Bug: 430484028
    
    Original change's description:
    > Refactor RuleSet creation.
    >
    > We now extract all style sheets first, and create RuleSets in a separate
    > pass second. This is because it will be required by mixins shortly,
    > but it also reduces the code duplication slightly. In particular, we can
    > replace RuleSetScope with a more explicit construction.
    >
    > Note that this now means that adopted style sheets also go through
    > the same process, but this is a no-op (see added comment).
    >
    > Change-Id: I3be885ef4afc2e132b813f4dad3639ad44ec4b18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696133
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1482230}
    
    Change-Id: Ib2152d2f43e47b8686e890228f3cd79785e9204f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718230
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484271}

--- third_party/blink/renderer/core/css/document_style_sheet_collection.cc
+++ third_party/blink/renderer/core/css/document_style_sheet_collection.cc
@@ -52,6 +52,8 @@ DocumentStyleSheetCollection::DocumentStyleSheetCollection(
 void DocumentStyleSheetCollection::CollectStyleSheetsFromCandidates(
     StyleEngine& engine,
     DocumentStyleSheetCollector& collector) {
+  StyleEngine::RuleSetScope rule_set_scope;
+
   for (Node* n : style_sheet_candidate_nodes_) {
     StyleSheetCandidate candidate(*n);
 
@@ -72,7 +74,13 @@ void DocumentStyleSheetCollection::CollectStyleSheetsFromCandidates(
     }
 
     CSSStyleSheet* css_sheet = To<CSSStyleSheet>(sheet);
-    collector.AppendActiveStyleSheet(css_sheet);
+    collector.AppendActiveStyleSheet(std::make_pair(
+        css_sheet, rule_set_scope.RuleSetForSheet(engine, css_sheet)));
+
+    if (css_sheet->Contents()->GetRuleSetDiff()) {
+      collector.AppendRuleSetDiff(css_sheet->Contents()->GetRuleSetDiff());
+      css_sheet->Contents()->ClearRuleSetDiff();
+    }
   }
 
   const TreeScope& tree_scope = GetTreeScope();
@@ -88,7 +96,8 @@ void DocumentStyleSheetCollection::CollectStyleSheetsFromCandidates(
     }
     DCHECK_EQ(GetDocument(), sheet->ConstructorDocument());
     collector.AppendSheetForList(sheet);
-    collector.AppendActiveStyleSheet(sheet);
+    collector.AppendActiveStyleSheet(
+        std::make_pair(sheet, engine.RuleSetForSheet(*sheet)));
   }
 }
 
@@ -97,14 +106,17 @@ void DocumentStyleSheetCollection::CollectStyleSheets(
     DocumentStyleSheetCollector& collector) {
   for (auto& sheet :
        GetDocument().GetStyleEngine().InjectedAuthorStyleSheets()) {
-    collector.AppendActiveStyleSheet(sheet.second);
+    collector.AppendActiveStyleSheet(std::make_pair(
+        sheet.second,
+        GetDocument().GetStyleEngine().RuleSetForSheet(*sheet.second)));
   }
   CollectStyleSheetsFromCandidates(engine, collector);
   for (CSSStyleSheet* inspector_sheet :
        GetDocument().GetStyleEngine().InspectorStyleSheets()) {
-    collector.AppendActiveStyleSheet(inspector_sheet);
+    collector.AppendActiveStyleSheet(std::make_pair(
+        inspector_sheet,
+        GetDocument().GetStyleEngine().RuleSetForSheet(*inspector_sheet)));
   }
-  collector.FinishCollectingStylesheets(engine);
 }
 
 void DocumentStyleSheetCollection::UpdateActiveStyleSheets(
--- third_party/blink/renderer/core/css/document_style_sheet_collector.h
+++ third_party/blink/renderer/core/css/document_style_sheet_collector.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_DOCUMENT_STYLE_SHEET_COLLECTOR_H_
 
 #include "third_party/blink/renderer/core/css/active_style_sheets.h"
-#include "third_party/blink/renderer/core/css/style_rule.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -37,8 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class MediaQueryEvaluator;
-class StyleEngine;
 class StyleSheet;
 class StyleSheetCollection;
 class RuleSetDiff;
@@ -55,14 +52,9 @@ class DocumentStyleSheetCollector {
   DocumentStyleSheetCollector(StyleSheetCollection*,
                               HeapVector<Member<StyleSheet>>*);
 
-  // NOTE: You will need to call FinishCollectingStylesheets()
-  // after this.
-  void AppendActiveStyleSheet(CSSStyleSheet*);
+  void AppendActiveStyleSheet(const ActiveStyleSheet&);
   void AppendSheetForList(StyleSheet*);
   void AppendRuleSetDiff(RuleSetDiff*);
-
-  // Can only be called once.
-  void FinishCollectingStylesheets(StyleEngine& engine);
 
  private:
   StyleSheetCollection* collection_;
@@ -72,7 +64,7 @@ class DocumentStyleSheetCollector {
 class ActiveDocumentStyleSheetCollector final
     : public DocumentStyleSheetCollector {
  public:
-  explicit ActiveDocumentStyleSheetCollector(StyleSheetCollection&);
+  ActiveDocumentStyleSheetCollector(StyleSheetCollection&);
 };
 
 class ImportedDocumentStyleSheetCollector final
