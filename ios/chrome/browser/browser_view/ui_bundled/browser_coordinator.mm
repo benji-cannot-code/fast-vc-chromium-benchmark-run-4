4d048ce7f38bffc1e00faac3a072a985298acb5e
Quentin Pubert <qpubert@google.com>
2025-07-09 05:37:15 -0700

    [iOS] Fix null activeWebState crash in -[BrowserCoordinator hideFindUI]
    It appears the activeWebState can sometimes be null when trying to hide
    the Find UI. This CL adds an if-statement to return early if so.
    
    Bug: 427201321
    Change-Id: If5fe79892b4a360e32ab080299155279be67d8d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714652
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484270}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -2819,9 +2819,11 @@ enum class ToolbarKind {
 }
 
 - (void)hideFindUI {
+  web::WebState* activeWebState = self.activeWebState;
+  if (!activeWebState) {
+    return;
+  }
   if (IsNativeFindInPageAvailable()) {
-    web::WebState* activeWebState = self.activeWebState;
-    DCHECK(activeWebState);
     auto* helper = FindTabHelper::FromWebState(activeWebState);
     helper->DismissFindNavigator();
   } else {
