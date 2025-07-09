627afab4094b8aa7457d7643af1ab07046745b1d
“Kavita <kavitasoni@chromium.org>
2025-07-09 10:25:02 -0700

    Cleanup flag autofill-vcn-enroll-request-timeout
    Bug: 394151939, 353441324
    Change-Id: I10b1b4e7d456d2c0837f59d8f56a908522df4d4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700143
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Kavita Soni <kavitasoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484440}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -572,9 +572,6 @@ extern const char kAutofillUnmaskCardRequestTimeoutDescription[];
 extern const char kAutofillUploadCardRequestTimeoutName[];
 extern const char kAutofillUploadCardRequestTimeoutDescription[];
 
-extern const char kAutofillVcnEnrollRequestTimeoutName[];
-extern const char kAutofillVcnEnrollRequestTimeoutDescription[];
-
 extern const char kAutofillVcnEnrollStrikeExpiryTimeName[];
 extern const char kAutofillVcnEnrollStrikeExpiryTimeDescription[];
 
--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -131,9 +131,6 @@ extern const char kAutofillUploadCardRequestTimeoutDescription[];
 extern const char kAutofillUseRendererIDsName[];
 extern const char kAutofillUseRendererIDsDescription[];
 
-extern const char kAutofillVcnEnrollRequestTimeoutName[];
-extern const char kAutofillVcnEnrollRequestTimeoutDescription[];
-
 extern const char kAutofillVcnEnrollStrikeExpiryTimeName[];
 extern const char kAutofillVcnEnrollStrikeExpiryTimeDescription[];
 
