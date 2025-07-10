6e109b3c703f131a152d2f86dfe709cac79813c9
Patrick Monette <pmonette@chromium.org>
2025-07-09 17:17:49 -0700

    [Memory Coordinator] Add the //base/ API
    This change introduces the MemoryConsumer class and its associated
    traits. It also adds the MemoryConsumerRegistry interface for
    registering the consumers with a global instance.
    
    This new class will be used as a replacement for
    MemoryPressureListener, with additional capabilities and flexibility.
    
    Bug: 406578344
    Change-Id: I35b7afd2bb6742bc47894f8106c2085fc3d4dff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6648870
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484687}

---
+++ base/memory_coordinator/mock_memory_consumer.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MEMORY_COORDINATOR_MOCK_MEMORY_CONSUMER_H_
+#define BASE_MEMORY_COORDINATOR_MOCK_MEMORY_CONSUMER_H_
+
+#include "base/memory_coordinator/memory_consumer.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace base {
+
+class MockMemoryConsumer : public MemoryConsumer {
+ public:
+  MockMemoryConsumer();
+  ~MockMemoryConsumer() override;
+
+  MOCK_METHOD(void, OnUpdateMemoryLimit, (), (override));
+  MOCK_METHOD(void, OnReleaseMemory, (), (override));
+};
+
+}  // namespace base
+
+#endif  // BASE_MEMORY_COORDINATOR_MOCK_MEMORY_CONSUMER_H_
