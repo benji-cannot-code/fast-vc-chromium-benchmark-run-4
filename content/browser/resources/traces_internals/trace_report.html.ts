f3efc134c5a192e035dc46d830dcd8a8136620b8
Etienne Pierre-doray <etiennep@chromium.org>
2025-07-10 14:38:34 -0700

    [tracing] Plumb trace uuid to recorder in chrome://traces
    This CL forces a uuid and exposes it to chrome://traces UI;
    this is used to name file when downloading a trace.
    Drive-by: lift common functions to ./trace_util.ts
    
    Bug: 413057996
    Change-Id: I893db52db08fc42ff600a0ab89930f50bf3df908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714671
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485217}

--- content/browser/resources/traces_internals/trace_report.html.ts
+++ content/browser/resources/traces_internals/trace_report.html.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {html, nothing} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {TraceReportElement} from './trace_report.js';
+import {getTokenAsUuidString} from './trace_util.js';
 import {ReportUploadState} from './traces_internals.mojom-webui.js';
 
 export function getHtml(this: TraceReportElement) {
@@ -15,9 +16,9 @@ export function getHtml(this: TraceReportElement) {
     html`
     <div>
       <button class="clickable-field copiable"
-          title="${this.getTokenAsUuidString_()}"
+          title="${getTokenAsUuidString(this.trace.uuid)}"
           @click="${this.onCopyUuidClick_}">
-        ${this.getTokenAsUuidString_()}
+        ${getTokenAsUuidString(this.trace.uuid)}
       </button>
       <div class="info">Trace ID</div>
     </div>
--- content/browser/tracing/traces_internals/traces_internals_handler.h
+++ content/browser/tracing/traces_internals/traces_internals_handler.h
@@ -94,7 +94,8 @@ class CONTENT_EXPORT TracesInternalsHandler
   void OnTracingError(perfetto::TracingError error);
   void OnTracingStop();
   void OnTracingStart();
-  void OnTraceComplete(std::optional<mojo_base::BigBuffer>);
+  void OnTraceComplete(std::optional<mojo_base::BigBuffer>,
+                       const std::optional<base::Token>&);
   void OnBufferUsage(bool success, float percent_full, bool data_loss);
 
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
@@ -107,6 +108,7 @@ class CONTENT_EXPORT TracesInternalsHandler
   const raw_ptr<TracingDelegate> tracing_delegate_;
 
   base::UnguessableToken session_unguessable_name_;
+  base::Token session_id_;
   std::unique_ptr<perfetto::TracingSession> tracing_session_;
   StartTraceSessionCallback start_callback_;
   StopTraceSessionCallback stop_callback_;
