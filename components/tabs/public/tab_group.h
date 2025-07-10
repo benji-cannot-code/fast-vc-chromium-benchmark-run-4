b79a34a40f8e092e9836bb69257882c290688262
David Yeung <dayeung@chromium.org>
2025-07-10 14:53:38 -0700

    Fetch TabCollection Handle from TabGroup
    Expose a getter in TabGroup to return the TabCollection handle.
    
    Bug: 425390972
    Change-Id: I40b24f036eed79ad0c43cbdac379e1a7ebdce2fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714832
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485229}

--- components/tabs/public/tab_group.h
+++ components/tabs/public/tab_group.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
+#include "components/tabs/public/tab_collection.h"
 #include "ui/gfx/range/range.h"
 
 class Profile;
@@ -83,6 +84,9 @@ class TabGroup {
   // Get the last tab in the Tab Group or nullptr if the group is currently
   // empty.
   tabs::TabInterface* GetLastTab() const;
+
+  // Returns the TabCollection handle associated with this group.
+  tabs::TabCollection::Handle GetCollectionHandle() const;
 
   // Returns the range of tab model indices this group contains. Notably
   // does not rely on the TabGroup's internal metadata, but rather
