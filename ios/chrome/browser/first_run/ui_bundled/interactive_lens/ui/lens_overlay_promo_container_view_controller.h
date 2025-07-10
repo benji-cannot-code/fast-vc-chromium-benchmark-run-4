1ef35ecc0fad986cfe3b2e5a4a5a9b7eaaa1a56a
Hira Mahmood <hiramahmood@google.com>
2025-07-10 09:06:12 -0700

    [iOS] Handle interactions on InteractiveLensPromoViewController
    Currently, "circle" gestures on the Lens view don't work as expected
    because the promo view controller's "pull-down" effect kicks in
    instead (see screen recordings).
    This CL:
     * Fixes the broken "circling" by disabling the pan gesture
       recognizers on the sheet-style view controller so that the
       pull-down effect is not registered.
     * Adds gesture recognizers to the Lens subview to hide the tip bubble
       upon any interaction.
    
    Before (attempting to circle here):
    https://screencast.googleplex.com/cast/NjcyMzU2MDg4OTM4NDk2MHw0Zjc0YjI0Ni02Yw
    After:
    https://screencast.googleplex.com/cast/NDc2NDAyNDk2NDEyMDU3Nnw0MjNkZjEyMC05Yw
    
    Fixed: 427997004
    Change-Id: Ic17202cc8342fb9f7e17058957163c22fd025666
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6681748
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485015}

---
+++ ios/chrome/browser/first_run/ui_bundled/interactive_lens/ui/lens_overlay_promo_container_view_controller.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_FIRST_RUN_UI_BUNDLED_INTERACTIVE_LENS_UI_LENS_OVERLAY_PROMO_CONTAINER_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_FIRST_RUN_UI_BUNDLED_INTERACTIVE_LENS_UI_LENS_OVERLAY_PROMO_CONTAINER_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+@class LensOverlayPromoContainerViewController;
+
+// Delegate for the LensOverlayPromoContainerViewController.
+@protocol LensOverlayPromoContainerViewControllerDelegate <NSObject>
+
+// Called when the user interacts with the Lens view.
+- (void)lensOverlayPromoContainerViewControllerDidReceiveInteraction:
+    (LensOverlayPromoContainerViewController*)viewController;
+
+@end
+
+// A view controller that displays the Lens view for the interactive Lens promo.
+@interface LensOverlayPromoContainerViewController : UIViewController
+
+// Delegate for the view controller.
+@property(nonatomic, weak) id<LensOverlayPromoContainerViewControllerDelegate>
+    delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_FIRST_RUN_UI_BUNDLED_INTERACTIVE_LENS_UI_LENS_OVERLAY_PROMO_CONTAINER_VIEW_CONTROLLER_H_
