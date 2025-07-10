c0ea9a760703564736f0ded2c4c9b8ecd554fa1b
Christian Dullweber <dullweber@chromium.org>
2025-07-10 02:33:54 -0700

    [geo] Register PermissionSetting by default
    This content settings was only registered behind a flag to fix a couple
    of issues related to PermissionSettings.
    
    Bug: 425642101
    Change-Id: If7477c3236183400533e324943e971c4f435c232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718202
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484857}

--- components/content_settings/core/browser/permission_settings_registry.cc
+++ components/content_settings/core/browser/permission_settings_registry.cc
@@ -9,16 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/contains.h"
-#include "base/feature_list.h"
 #include "base/values.h"
-#include "build/blink_buildflags.h"
-#include "build/build_config.h"
 #include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/content_settings/core/browser/geolocation_setting_delegate.h"
 #include "components/content_settings/core/browser/permission_settings_info.h"
 #include "components/content_settings/core/browser/website_settings_registry.h"
 #include "components/content_settings/core/common/content_settings.h"
-#include "components/content_settings/core/common/features.h"
 
 namespace content_settings {
 
@@ -79,8 +75,6 @@ void PermissionSettingsRegistry::Init() {
   // If a permission is DELETED, please update
   // PrefProvider::DiscardOrMigrateObsoletePreferences() and
   // DefaultProvider::DiscardOrMigrateObsoletePreferences() accordingly.
-  if (base::FeatureList::IsEnabled(
-          content_settings::features::kApproximateGeolocationPermission)) {
     Register(ContentSettingsType::GEOLOCATION_WITH_OPTIONS,
              "geolocation-with-options",
              GeolocationSetting(PermissionOption::kAsk, PermissionOption::kAsk),
@@ -91,7 +85,6 @@ void PermissionSettingsRegistry::Init() {
                  WebsiteSettingsRegistry::DESKTOP,
              PermissionSettingsInfo::EXCEPTIONS_ON_SECURE_ORIGINS_ONLY,
              std::make_unique<GeolocationSettingDelegate>());
-  }
 }
 
 void PermissionSettingsRegistry::Register(
