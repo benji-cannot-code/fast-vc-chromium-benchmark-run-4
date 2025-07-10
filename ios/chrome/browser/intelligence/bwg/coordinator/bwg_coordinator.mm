878e5adc5e4e6415fc3578bdb1aa48a82be2b941
Joemer Ramos <joemerramos@google.com>
2025-07-09 20:09:17 -0700

    [iOS] Add Logic to Force BWG Promo
    Add logic to force BWG Promo.
    
    Bug: 414791798
    Change-Id: I75d3f7c92c9e38f1b59153e085f5bf40bda302e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715258
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484749}

--- ios/chrome/browser/intelligence/bwg/coordinator/bwg_coordinator.mm
+++ ios/chrome/browser/intelligence/bwg/coordinator/bwg_coordinator.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/bwg/coordinator/bwg_mediator_delegate.h"
 #import "ios/chrome/browser/intelligence/bwg/metrics/bwg_metrics.h"
 #import "ios/chrome/browser/intelligence/bwg/ui/bwg_navigation_controller.h"
+#import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
@@ -185,11 +186,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // If YES, BWG Promo should be shown.
 - (BOOL)shouldShowBWGPromo {
   BOOL promoShownManually = _prefService->GetBoolean(prefs::kIOSBWGManualPromo);
+  BOOL forcePromo = ShouldForceBWGPromo();
   BOOL promoTriggered = _tracker->HasEverTriggered(
       feature_engagement::kIPHIOSBWGPromoFeature, true);
   BOOL isPromoEntry = _entryPoint == bwg::EntryPointPromo;
 
-  return isPromoEntry || (!promoTriggered && !promoShownManually);
+  return isPromoEntry || (!promoTriggered && !promoShownManually) || forcePromo;
 }
 
 // Presents the page action menu IPH.
