f467c7ad5b8f2e4e4b263d6374218c513870e912
Eliot Courtney <edcourtney@google.com>
2025-07-09 20:04:16 -0700

    Add ScreenCaptureTest
    This CL adds ScreenCaptureTest and some basic tests.
    
    Bug: 352187279
    Test: CQ
    Change-Id: I105984bc53214c33847daacdb5b3ef22a107b013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6686952
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484747}

--- content/public/android/java/src/org/chromium/content_public/browser/media/capture/ScreenCapture.java
+++ content/public/android/java/src/org/chromium/content_public/browser/media/capture/ScreenCapture.java
@@ -21,6 +21,7 @@ import android.os.Looper;
 import android.view.WindowManager;
 
 import androidx.activity.result.ActivityResult;
+import androidx.annotation.VisibleForTesting;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
@@ -84,6 +85,12 @@ public class ScreenCapture implements ImageHandler.Delegate {
         }
     }
 
+    @VisibleForTesting
+    interface ImageHandlerFactory {
+        ImageHandler create(
+                CaptureState captureState, ImageHandler.Delegate delegate, Handler handler);
+    }
+
     // Starting a MediaProjection session involves plumbing the results from the content picker,
     // which is done via ActivityResult. This class does not handle how that is achieved, but
     // requires this state to begin the session.
@@ -98,6 +105,7 @@ public class ScreenCapture implements ImageHandler.Delegate {
     private long mNativeDesktopCapturerAndroid;
 
     private final Handler mHandler;
+    private final ImageHandlerFactory mImageHandlerFactory;
 
     private @Nullable MediaProjection mMediaProjection;
 
@@ -111,8 +119,14 @@ public class ScreenCapture implements ImageHandler.Delegate {
     private @Nullable WebContents mWebContents;
 
     private ScreenCapture(long nativeDesktopCapturerAndroid) {
+        this(nativeDesktopCapturerAndroid, ImageHandler::new);
+    }
+
+    @VisibleForTesting
+    ScreenCapture(long nativeDesktopCapturerAndroid, ImageHandlerFactory imageHandlerFactory) {
         mNativeDesktopCapturerAndroid = nativeDesktopCapturerAndroid;
         mHandler = new Handler(assumeNonNull(Looper.myLooper()));
+        mImageHandlerFactory = imageHandlerFactory;
     }
 
     public static void onForegroundServiceRunning(boolean running) {
@@ -247,7 +261,7 @@ public class ScreenCapture implements ImageHandler.Delegate {
     }
 
     private ImageHandler createImageHandler(CaptureState captureState) {
-        final var imageHandler = new ImageHandler(captureState, this, mHandler);
+        final var imageHandler = mImageHandlerFactory.create(captureState, this, mHandler);
         mImageHandlerQueue.add(imageHandler);
         return imageHandler;
     }
