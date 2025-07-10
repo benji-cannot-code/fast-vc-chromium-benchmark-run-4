01e1921173b4a77ca1db9e1c42e944a3e3b09857
Stanislav Mikheyev <mikhe@google.com>
2025-07-10 04:42:08 -0700

    Make minor enhancements to autocomplete table code (label sensitive version).
    - Prefer std::move over const references in constructors.
    - Prefer `std::u16string_view` over const references.
    - Prefer `size_t` over `int` for sizes and limits.
    - Use backticks to reference code in comments.
    
    Bug: 346507576
    Change-Id: I4af50517aba19cfb6c3edc31e4a923d284c1fb01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664539
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Stanislav Mikheyev <mikhe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484893}

--- components/autofill/core/browser/webdata/autocomplete/autocomplete_entry_label_sensitive.cc
+++ components/autofill/core/browser/webdata/autocomplete/autocomplete_entry_label_sensitive.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/autocomplete/autocomplete_entry_label_sensitive.h"
 
 #include <string>
+#include <utility>
 
 #include "base/strings/utf_string_conversions.h"
 
@@ -18,15 +19,17 @@ namespace autofill {
 AutocompleteKeyLabelSensitive::AutocompleteKeyLabelSensitive() = default;
 
 AutocompleteKeyLabelSensitive::AutocompleteKeyLabelSensitive(
-    const std::u16string& name,
-    const std::u16string& label,
-    const std::u16string& value)
-    : name_(name), label_(label), value_(value) {}
+    std::u16string name,
+    std::u16string label,
+    std::u16string value)
+    : name_(std::move(name)),
+      label_(std::move(label)),
+      value_(std::move(value)) {}
 
 AutocompleteKeyLabelSensitive::AutocompleteKeyLabelSensitive(
-    const std::string& name,
-    const std::string& label,
-    const std::string& value)
+    std::string_view name,
+    std::string_view label,
+    std::string_view value)
     : name_(base::UTF8ToUTF16(name)),
       label_(base::UTF8ToUTF16(label)),
       value_(base::UTF8ToUTF16(value)) {}
