ffa1be0488deaba0c1e865a59201666b54a42bdb
Calder Kitagawa <ckitagawa@chromium.org>
2025-07-10 08:12:31 -0700

    [NullAway] Null mark automotive toolbar and background task
    Bug: 389129271
    Change-Id: I8f82c9445c7360b9704773ca60ccd84ad25ecce3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722212
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484980}

--- chrome/android/java/src/org/chromium/chrome/browser/automotivetoolbar/AutomotiveBackButtonToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/automotivetoolbar/AutomotiveBackButtonToolbarCoordinator.java
@@ -16,6 +16,7 @@ import android.widget.FrameLayout;
 import androidx.annotation.AnimRes;
 import androidx.appcompat.widget.Toolbar;
 
+import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.back_press.BackPressManager;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -31,6 +32,7 @@ import org.chromium.ui.display.DisplayUtil;
  * The automotive back button toolbar allows users to navigate backwards. This coordinator supports
  * the back button toolbar disappearing on fullscreen, appearing on swipe.
  */
+@NullMarked
 public class AutomotiveBackButtonToolbarCoordinator {
     /** Duration automotive back button toolbar is visible after a valid swipe */
     private static final long SHOW_TOOLBAR_ON_SWIPE_DURATION_MS = 10000;
@@ -200,6 +202,7 @@ public class AutomotiveBackButtonToolbarCoordinator {
     }
 
     /** Destroy the Automotive Back Button Toolbar coordinator and its components. */
+    @SuppressWarnings("NullAway")
     public void destroy() {
         mHideOnSwipeToolbarAnimation.cancel();
         mShowOnSwipeToolbarAnimation.cancel();
