4fb6284db79ec2fac32b82a93741a80043145727
Shweta Bindal <shwetabindal@microsoft.com>
2025-07-09 23:19:16 -0700

    [Editing] Use upstream text affinity to find previous line
    The |SelectionModifier::PreviousLinePosition| function was previously
    using a downstream text affinity. Due to this,
    the visible position at the end of a soft-wrapped line is
    considered the same as the start of the next line.
    
    Because of the downstream affinity, the cursor was incorrectly
    associated with the start of the next line, causing
    |PreviousLinePosition| to fail to find the correct previous line.
    
    This patch resolves the issue by using an |kUpstreamIfPossible| text
    affinity to find the line box. This ensures that a position at a wrap
    point is correctly identified as being on the current line, allowing
    for proper navigation to the previous line. The original affinity is
    preserved when the cursor is already at the beginning of a line.
    
    Bug: 40679713
    Change-Id: I8ffcef396dea1a600b7d2742c84d6062d06985a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650558
    Reviewed-by: Utkarsh Pathak <utpathak@microsoft.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Shweta Bindal <shwetabindal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1484785}

--- third_party/blink/renderer/core/editing/selection_modifier_line.cc
+++ third_party/blink/renderer/core/editing/selection_modifier_line.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/inline_box_position.h"
+#include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/editing/visible_position.h"
 #include "third_party/blink/renderer/core/editing/visible_units.h"
 #include "third_party/blink/renderer/core/layout/geometry/logical_rect.h"
@@ -42,6 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace {
+
+static LayoutUnit AbsoluteDifference(LayoutUnit a, LayoutUnit b) {
+  return (a - b).Abs();
+}
 
 // TODO(1229581): Get rid of this.
 class AbstractLineBox {
@@ -119,6 +124,10 @@ class AbstractLineBox {
       bool only_editable_leaves) const {
     return PositionForPoint(cursor_, point_in_container, only_editable_leaves);
   }
+  const LayoutBlockFlow& GetBlock() const {
+    DCHECK(IsNotNull());
+    return *cursor_.GetLayoutBlockFlow();
+  }
 
  private:
   explicit AbstractLineBox(const InlineCursor& cursor)
@@ -126,10 +135,6 @@ class AbstractLineBox {
     DCHECK(cursor_.Current().IsLineBox());
   }
 
-  const LayoutBlockFlow& GetBlock() const {
-    DCHECK(IsNotNull());
-    return *cursor_.GetLayoutBlockFlow();
-  }
 
   LayoutUnit PhysicalBlockOffset() const {
     DCHECK(IsNotNull());
@@ -379,6 +384,27 @@ PositionInFlatTree NextRootInlineBoxCandidatePosition(
 }  // namespace
 
 // static
+bool SelectionModifier::ShouldUseUpstreamPositionForLineNavigation(
+    const PositionInFlatTree& p,
+    LayoutUnit line_direction_point) {
+  const PositionInFlatTreeWithAffinity upstream_position_with_affinity(
+      p, TextAffinity::kUpstream);
+  const VisiblePositionInFlatTree& visible_upstream_position =
+      CreateVisiblePosition(upstream_position_with_affinity);
+  const LayoutUnit x_upstream = LineDirectionPointForBlockDirectionNavigationOf(
+      visible_upstream_position);
+
+  const VisiblePositionInFlatTree& visible_downstream_position =
+      CreateVisiblePosition(p, TextAffinity::kDownstream);
+  const LayoutUnit x_downstream =
+      LineDirectionPointForBlockDirectionNavigationOf(
+          visible_downstream_position);
+
+  return AbsoluteDifference(line_direction_point, x_upstream) <
+         AbsoluteDifference(line_direction_point, x_downstream);
+}
+
+// static
 PositionInFlatTreeWithAffinity SelectionModifier::PreviousLinePosition(
     const PositionInFlatTreeWithAffinity& position,
     LayoutUnit line_direction_point) {
@@ -396,9 +422,23 @@ PositionInFlatTreeWithAffinity SelectionModifier::PreviousLinePosition(
 
   AbstractLineBox line = AbstractLineBox::CreateFor(position);
   if (line) {
+    if (RuntimeEnabledFeatures::
+            ConsiderUpstreamPositionForFindingPreviousLineEnabled()) {
+      if (ShouldUseUpstreamPositionForLineNavigation(p, line_direction_point)) {
+        const PositionInFlatTreeWithAffinity upstream_position(
+            p, TextAffinity::kUpstream);
+        if (upstream_position.IsNotNull()) {
+          if (auto upstream_line =
+                  AbstractLineBox::CreateFor(upstream_position)) {
+            line = upstream_line;
+          }
+        }
+      }
+    }
     line = line.PreviousLine();
-    if (!line || !line.CanBeCaretContainer())
+    if (!line || !line.CanBeCaretContainer()) {
       line = AbstractLineBox();
+    }
   }
 
   if (!line) {
