6eae1a25af7e81dd9f827d105029f0e69b1100af
Rafał Godlewski <rgod@google.com>
2025-07-10 08:47:37 -0700

    [iOS][Pwd Settings] Clean up kIOSPasskeysM2 feature flag
    The feature was enabled by default in M138.
    
    Bug: b:396694707
    Change-Id: I4c6b4e022e512829b2957d789652a41efd5a7ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722234
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485004}

--- ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
-#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_item.h"
@@ -1534,9 +1533,7 @@ TEST_F(PasswordManagerViewControllerTest, ManageAccountHeaderIsBeingUpdated) {
   [GetPasswordManagerViewController() setSavingPasswordsToAccount:YES];
 
   EXPECT_NSEQ(l10n_util::GetNSString(
-                  IOSPasskeysM2Enabled()
-                      ? IDS_IOS_SAVE_PASSWORDS_PASSKEYS_MANAGE_ACCOUNT_HEADER
-                      : IDS_IOS_SAVE_PASSWORDS_MANAGE_ACCOUNT_HEADER),
+                  IDS_IOS_SAVE_PASSWORDS_PASSKEYS_MANAGE_ACCOUNT_HEADER),
               header.text);
   EXPECT_EQ(1U, [header.urls count]);
   CrURL* expectedHeaderUrl = [[CrURL alloc]
