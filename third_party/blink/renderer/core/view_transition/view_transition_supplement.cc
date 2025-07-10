29e2954f24b94cc7a93ed365f8b642108c8488a7
Kevin Ellis <kevers@google.com>
2025-07-10 13:09:27 -0700

    Force a stacking context for the scope of a view transition.
    Bug: 426218225
    Change-Id: Ifdad680f295c82181fbac1547c32d5bb3020eaa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714984
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485169}

--- third_party/blink/renderer/core/view_transition/view_transition_supplement.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_supplement.cc
@@ -10,14 +10,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/frame/frame.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_view_transition_callback.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_view_transition_options.h"
+#include "third_party/blink/renderer/core/css/style_change_reason.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/pseudo_element.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/page_animator.h"
+#include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/view_transition/dom_view_transition.h"
 #include "third_party/blink/renderer/core/view_transition/page_swap_event.h"
@@ -261,12 +264,25 @@ void ViewTransitionSupplement::StartTransition(
 void ViewTransitionSupplement::OnTransitionFinished(
     ViewTransition* transition) {
   CHECK(transition);
+
   // Clear the transition so it can be garbage collected if needed (and to
   // prevent callers of GetTransition thinking there's an ongoing transition).
   if (transition == document_transition_) {
     document_transition_ = nullptr;
   } else {
-    element_transitions_.erase(transition->Scope());
+    Element* scope = transition->Scope();
+    element_transitions_.erase(scope);
+    LayoutObject* layout_object = scope->GetLayoutObject();
+    if (scope != scope->GetDocument().documentElement() && layout_object &&
+        !(layout_object->StyleRef().Contain() & kContainsViewTransition) &&
+        layout_object->HasLayer()) {
+      // Element may have had an added stacking context purely for being the
+      // scope of a view transition. Ensure correctness of the adjusted style.
+      layout_object->EnclosingLayer()->SetNeedsCompositingInputsUpdate();
+      scope->SetNeedsStyleRecalc(kLocalStyleChange,
+                                 StyleChangeReasonForTracing::Create(
+                                     style_change_reason::kViewTransition));
+    }
   }
 
   // Notify the animator if the set of active view transitions is empty.
