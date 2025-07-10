b7c670003d1c6938ddb9a64be766002c0998e066
Jian Li <jianli@chromium.org>
2025-07-09 18:21:01 -0700

    Remove checking group ID in getSuggestionDescription
    This is not longer needed after the fix at
    https://chromium-review.googlesource.com/c/chromium/src/+/6672918
    
    Change-Id: Id2191fd9d67050218fe1f16a717df28752bdb58f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701419
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484707}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
@@ -111,6 +111,7 @@ public class OmniboxPedalsRenderTest {
         return AutocompleteMatchBuilder.searchWithType(OmniboxSuggestionType.SEARCH_SUGGEST)
                 .setDisplayText(name)
                 .setActions(List.of(new OmniboxPedal(0, "hint", "accessibility", id)))
+                .setDescription(null)
                 .build();
     }
 
