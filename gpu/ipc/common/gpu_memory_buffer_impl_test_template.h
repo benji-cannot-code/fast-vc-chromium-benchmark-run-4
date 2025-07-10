8e8a1c11d600050ccd761843195d9854a1764a1d
Saifuddin Hitawala <hitawala@chromium.org>
2025-07-10 13:45:20 -0700

    [gpu] Remove BufferUsage param from GMBImplDXGI::CreateFromHandle
    The parameter was unused.
    
    Bug: 404958317
    Change-Id: I9eea711440fec89f490eb7a77f9f58dcd09c9bfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6723093
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485190}

--- gpu/ipc/common/gpu_memory_buffer_impl_test_template.h
+++ gpu/ipc/common/gpu_memory_buffer_impl_test_template.h
@@ -110,7 +110,7 @@ class GpuMemoryBufferImplTest : public testing::Test {
 #if BUILDFLAG(IS_WIN)
       case gfx::DXGI_SHARED_HANDLE:
         return GpuMemoryBufferImplDXGI::CreateFromHandleForTesting(
-            std::move(handle), size, format, usage, std::move(callback));
+            std::move(handle), size, format, std::move(callback));
 #endif
       default:
         NOTREACHED();
