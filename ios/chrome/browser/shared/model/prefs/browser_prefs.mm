e8c088ab7b4195fe15672b4ddd1a1cd2f86e8d86
Joemer Ramos <joemerramos@google.com>
2025-07-10 08:32:19 -0700

    [iOS] Create BWG Setting Browser Prefs
    Creates preferences for the BWG settings page.
    
    Bug: 427226904
    Change-Id: I0931dd7f39bef12deb3250a0aaac160e0d02ec6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722572
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484992}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -1049,6 +1049,10 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
   registry->RegisterBooleanPref(prefs::kIOSBwgConsent, false);
 
+  registry->RegisterBooleanPref(prefs::kIOSBWGPreciseLocationSetting, false);
+
+  registry->RegisterBooleanPref(prefs::kIOSBWGPageContentSetting, true);
+
   registry->RegisterBooleanPref(prefs::kIOSBWGManualPromo, false);
 
   registry->RegisterTimePref(prefs::kIosSyncInfobarErrorLastDismissedTimestamp,
