1aa4c3192089ccd4a0cc96fb68d249d5e528f395
weidongliu <weidongliu@chromium.org>
2025-07-09 18:29:34 -0700

    Eliminate allow_unsafe_buffers in //media/audio/[alsa,android,pulse].
    Bug: 40284755
    Change-Id: I49dfd3902bb64fb30ebdc50d3f19f6dea77d89f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651568
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484712}

--- media/audio/android/opensles_wrapper.cc
+++ media/audio/android/opensles_wrapper.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 // The file defines the symbols from OpenSLES that android is using. It then
 // loads the library dynamically on first use.
 
@@ -25,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // untouched.
 #include <stdint.h>
 
+#include <array>
+
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 
 #define const
@@ -72,23 +70,25 @@ base::NativeLibrary IntializeLibraryHandle() {
   };
 
   // The list of defined symbols.
-  const SymbolDefinition kSymbols[] = {
-      {"SL_IID_ENGINE", &SL_IID_ENGINE},
-      {"SL_IID_ANDROIDSIMPLEBUFFERQUEUE", &SL_IID_ANDROIDSIMPLEBUFFERQUEUE},
-      {"SL_IID_ANDROIDCONFIGURATION", &SL_IID_ANDROIDCONFIGURATION},
-      {"SL_IID_RECORD", &SL_IID_RECORD},
-      {"SL_IID_BUFFERQUEUE", &SL_IID_BUFFERQUEUE},
-      {"SL_IID_VOLUME", &SL_IID_VOLUME},
-      {"SL_IID_PLAY", &SL_IID_PLAY}};
+  auto kSymbols = std::to_array<const SymbolDefinition>(
+      {{"SL_IID_ENGINE", &SL_IID_ENGINE},
+       {"SL_IID_ANDROIDSIMPLEBUFFERQUEUE", &SL_IID_ANDROIDSIMPLEBUFFERQUEUE},
+       {"SL_IID_ANDROIDCONFIGURATION", &SL_IID_ANDROIDCONFIGURATION},
+       {"SL_IID_RECORD", &SL_IID_RECORD},
+       {"SL_IID_BUFFERQUEUE", &SL_IID_BUFFERQUEUE},
+       {"SL_IID_VOLUME", &SL_IID_VOLUME},
+       {"SL_IID_PLAY", &SL_IID_PLAY}});
 
-  for (size_t i = 0; i < sizeof(kSymbols) / sizeof(kSymbols[0]); ++i) {
+  for (auto& symbol : kSymbols) {
     void* func_ptr =
-        base::GetFunctionPointerFromNativeLibrary(handle, kSymbols[i].name);
+        base::GetFunctionPointerFromNativeLibrary(handle, symbol.name);
     if (!func_ptr) {
-      DLOG(ERROR) << "Unable to find symbol for " << kSymbols[i].name;
+      DLOG(ERROR) << "Unable to find symbol for " << symbol.name;
       return nullptr;
     }
-    memcpy(kSymbols[i].sl_iid, func_ptr, sizeof(SLInterfaceID));
+    base::byte_span_from_ref(*symbol.sl_iid)
+        .copy_from(base::byte_span_from_ref(
+            *reinterpret_cast<SLInterfaceID*>(func_ptr)));
   }
   return handle;
 }
