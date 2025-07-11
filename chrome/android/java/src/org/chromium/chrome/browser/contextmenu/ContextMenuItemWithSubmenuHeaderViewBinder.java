6764fcd1ded492ea3d3c45498ca599ee158aaff3
Gazal <agazal@google.com>
2025-07-11 09:13:45 -0700

    Handle property ListMenuItemProperties.ENABLED
    Bug: 418807464
    Change-Id: I6651a4a487e0f2ad41c100655961c50576f4edba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6724976
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485615}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuItemWithSubmenuHeaderViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuItemWithSubmenuHeaderViewBinder.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.contextmenu;
 
 import static org.chromium.ui.listmenu.ContextMenuSubmenuHeaderItemProperties.TITLE;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.CLICK_LISTENER;
+import static org.chromium.ui.listmenu.ListMenuItemProperties.ENABLED;
 
 import android.view.View;
 import android.widget.TextView;
@@ -28,6 +29,8 @@ class ContextMenuItemWithSubmenuHeaderViewBinder {
         TextView textView = view.findViewById(R.id.menu_row_text);
         if (propertyKey == TITLE) {
             textView.setText(model.get(TITLE));
+        }  else if (propertyKey == ENABLED) {
+            textView.setEnabled(model.get(ENABLED));
         } else if (propertyKey == CLICK_LISTENER) {
             view.setOnClickListener(model.get(CLICK_LISTENER));
         }
