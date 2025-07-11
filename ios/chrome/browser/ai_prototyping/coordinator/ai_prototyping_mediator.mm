47355526538518ebbf8357f74b0583fa8170cda1
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-07-11 13:08:30 -0700

    [iOS][Evals] Added PDF extraction as default for Bling prototyping menu
    Added PDf extraction as default for Bling prototyping menu for future
    evals PageContext extraction.
    
    Change-Id: I73d3e13e6920865e9f57d831a3a9d089be88b738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6728956
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485779}

--- ios/chrome/browser/ai_prototyping/coordinator/ai_prototyping_mediator.mm
+++ ios/chrome/browser/ai_prototyping/coordinator/ai_prototyping_mediator.mm
@@ -170,6 +170,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       completionCallback:std::move(page_context_completion_callback)];
   [_pageContextWrapper setShouldGetInnerText:YES];
   [_pageContextWrapper setShouldGetSnapshot:YES];
+  [_pageContextWrapper setShouldGetFullPagePDF:YES];
   [_pageContextWrapper populatePageContextFieldsAsync];
 }
 
