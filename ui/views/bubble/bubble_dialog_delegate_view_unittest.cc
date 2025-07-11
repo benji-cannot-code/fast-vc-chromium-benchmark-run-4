9236e580438c496da8c77eb92c72809cffed1fb0
David Yeung <dayeung@chromium.org>
2025-07-11 11:30:27 -0700

    Enabling BubbleMetricsApi everywhere
    This CL will enable BubbleMetricsApi by default by removing the
    BubbleMetricsApi flag.
    
    Fixed: 362247364
    Change-Id: Icc6c9c27665b7345079b72eb4683f0382e15bb34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6723366
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485719}

--- ui/views/bubble/bubble_dialog_delegate_view_unittest.cc
+++ ui/views/bubble/bubble_dialog_delegate_view_unittest.cc
@@ -176,10 +176,7 @@ class WidgetWithNonNullThemeProvider : public Widget {
 
 class BubbleDialogDelegateViewTest : public ViewsTestBase {
  public:
-  BubbleDialogDelegateViewTest() {
-    feature_list_.InitAndEnableFeature(features::kBubbleMetricsApi);
-  }
-
+  BubbleDialogDelegateViewTest() = default;
   BubbleDialogDelegateViewTest(const BubbleDialogDelegateViewTest&) = delete;
   BubbleDialogDelegateViewTest& operator=(const BubbleDialogDelegateViewTest&) =
       delete;
@@ -202,10 +199,7 @@ class BubbleDialogDelegateViewTest : public ViewsTestBase {
 
 class BubbleUmaLoggerTest : public ViewsTestBase {
  public:
-  BubbleUmaLoggerTest() {
-    feature_list_.InitAndEnableFeature(features::kBubbleMetricsApi);
-  }
-
+  BubbleUmaLoggerTest() = default;
   BubbleUmaLoggerTest(const BubbleUmaLoggerTest&) = delete;
   BubbleUmaLoggerTest& operator=(const BubbleUmaLoggerTest&) = delete;
 
