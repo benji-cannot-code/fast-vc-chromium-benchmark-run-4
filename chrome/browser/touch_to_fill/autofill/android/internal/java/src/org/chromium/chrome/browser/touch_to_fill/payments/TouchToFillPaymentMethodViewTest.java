6630472faa10b7cd3f00cd8a223bf3412ed3702f
Timofey Chudakov <tchudakov@google.com>
2025-07-11 02:09:09 -0700

    [Wallet] Make all loyalty cards screen title focusable.
    Bug: 431049794
    Change-Id: Id6390d9f9fec56a779a127be8dd3cfc58e38a49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6727214
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485430}

--- chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodViewTest.java
+++ chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodViewTest.java
@@ -1135,6 +1135,7 @@ public class TouchToFillPaymentMethodViewTest {
         assertThat(
                 allLoyaltyCardsScreenTitle.getText().toString(),
                 is(getString(R.string.autofill_bottom_sheet_all_loyalty_cards_screen_title)));
+        assertTrue(allLoyaltyCardsScreenTitle.isFocusable());
 
         // Verify that 1 loyalty card is displayed.
         RecyclerView allLoyaltyCardsContainer =
