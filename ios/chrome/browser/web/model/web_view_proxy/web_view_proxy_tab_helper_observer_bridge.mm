43913826fe5ba1cc7777ed201bae70f4356cd5b3
Quentin Pubert <qpubert@google.com>
2025-07-11 02:16:07 -0700

    [iOS] Observe WebViewProxyTabHelper in main content UI forwarding code
    This change introduces an Objective-C bridge for the
    `WebViewProxyTabHelper` observer, allowing Objective-C classes to be
    notified when the web view proxy changes.
    
    The `WebScrollViewMainContentUIForwarder` is updated to use this new
    bridge to observe the `WebViewProxyTabHelper` and update its proxy.
    
    Bug: 417688581
    Change-Id: I893430668645e6365ce3ea8999e457ae826bcb11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6710803
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485433}

---
+++ ios/chrome/browser/web/model/web_view_proxy/web_view_proxy_tab_helper_observer_bridge.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/web/model/web_view_proxy/web_view_proxy_tab_helper_observer_bridge.h"
+
+WebViewProxyTabHelperObserverBridge::WebViewProxyTabHelperObserverBridge(
+    id<WebViewProxyTabHelperObserving> observer)
+    : observer_(observer) {}
+
+WebViewProxyTabHelperObserverBridge::~WebViewProxyTabHelperObserverBridge() =
+    default;
+
+void WebViewProxyTabHelperObserverBridge::WebViewProxyChanged(
+    WebViewProxyTabHelper* tab_helper) {
+  [observer_ webViewProxyDidChange:tab_helper];
+}
+
+void WebViewProxyTabHelperObserverBridge::WebViewProxyTabHelperDestroyed(
+    WebViewProxyTabHelper* tab_helper) {
+  [observer_ webViewProxyTabHelperWasDestroyed:tab_helper];
+}
