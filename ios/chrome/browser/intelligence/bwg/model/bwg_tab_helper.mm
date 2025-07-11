9c5c13f7f949d5006a29ea7f49cb8a25c7881880
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-07-11 07:34:07 -0700

    [iOS][BWG] Added BWG overlay screenshots for backgrounded BWG tabs
    Added functionality for enabling screenshots of the entire window
    (instead of the content area only) for tabs that are backgrounded with
    the BWG overlay showing. This way the tab grid previews include the BWG
    overlay in them.
    
    Bug: 422175388
    Change-Id: I1f220ef8bb7900e98a219750759661010c2b88a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713963
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485559}

--- ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
@@ -13,12 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/prefs/scoped_user_pref_update.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_service.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_service_factory.h"
+#import "ios/chrome/browser/intelligence/bwg/model/bwg_snapshot_utils.h"
 #import "ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/url/url_util.h"
 #import "ios/chrome/browser/shared/public/commands/bwg_commands.h"
+#import "ios/chrome/browser/snapshots/model/snapshot_tab_helper.h"
 #import "ios/web/public/navigation/navigation_context.h"
 #import "ios/web/public/web_state.h"
 #import "ios/web/util/content_type_util.h"
@@ -71,8 +73,15 @@ BwgTabHelper::~BwgTabHelper() {}
 void BwgTabHelper::SetBwgUiShowing(bool showing) {
   is_bwg_ui_showing_ = showing;
 
+  // UI was foregrounded, so can no longer be active in background.
   if (is_bwg_ui_showing_) {
     is_bwg_session_active_in_background_ = false;
+  }
+
+  // UI was hidden but the session is not active, so update the snapshot to
+  // remove the overlay from it.
+  if (!is_bwg_ui_showing_ && !is_bwg_session_active_in_background_) {
+    UpdateWebStateSnapshotInStorage();
   }
 }
 
@@ -117,7 +126,7 @@ bool BwgTabHelper::IsBwgAvailableForWebState() {
   const bool is_profile_eligible =
       !profile->IsOffTheRecord() && bwg_service->IsEligibleForBwg();
 
-  // The web state is eligible for HTML and images that use http/https schemas,
+  // The web state is eligible for HTML and images that use http/https schemes.
   const GURL& url = web_state_->GetVisibleURL();
   const std::string mime_type = web_state_->GetContentsMimeType();
   const BOOL is_web_state_eligible =
@@ -164,6 +173,10 @@ void BwgTabHelper::WasShown(web::WebState* web_state) {
 void BwgTabHelper::WasHidden(web::WebState* web_state) {
   if (is_bwg_ui_showing_) {
     is_bwg_session_active_in_background_ = true;
+
+    // Update the snapshot before backgrounding BWG.
+    UpdateWebStateSnapshotInStorage();
+
     [bwg_commands_handler_ dismissBWGFlow];
   }
 }
@@ -223,4 +236,23 @@ std::optional<std::string> BwgTabHelper::GetURLOnLastInteraction() {
   }
 
   return std::nullopt;
+}
+
+void BwgTabHelper::UpdateWebStateSnapshotInStorage() {
+  SnapshotTabHelper* snapshot_tab_helper =
+      SnapshotTabHelper::FromWebState(web_state_);
+
+  if (!snapshot_tab_helper) {
+    return;
+  }
+
+  if (is_bwg_ui_showing_) {
+    UIImage* snapshot =
+        bwg_snapshot_utils::GetCroppedFullscreenSnapshot(web_state_->GetView());
+    if (snapshot) {
+      snapshot_tab_helper->UpdateSnapshotStorageWithImage(snapshot);
+    }
+  } else {
+    snapshot_tab_helper->UpdateSnapshotWithCallback(nil);
+  }
 }
