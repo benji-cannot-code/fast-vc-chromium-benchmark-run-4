c2c2c23caf5b380e3afa6629fa1e9c0df546c608
Mike Dougherty <michaeldo@chromium.org>
2025-07-11 14:03:55 -0700

    Revert "[iOS] Observe WebViewProxyTabHelper in FullscreenWebStateObserver"
    This reverts commit fb26b7c3948ca65ee4b6f6ce440fd671f8e1aebd.
    
    Reason for revert: Breaks FullscreenBrowserAgentTest.* tests -> https://ci.chromium.org/ui/p/chrome/builders/ci/smoke/157731/overview
    
    Bug: 417688581
    Original change's description:
    > [iOS] Observe WebViewProxyTabHelper in FullscreenWebStateObserver
    >
    > If `kSmoothScrollingDefault` is disabled then FullscreenWebStateObserver
    > is responsible for updating the FullscreenModel through its
    > FullscreenWebViewProxyObserver.
    >
    > This CL makes FullscreenWebStateObserver observer the WebState's
    > WebViewProxyTabHelper so it can feed the correct proxy to the proxy
    > observer.
    >
    > Bug: 417688581
    > Change-Id: Ida497debe8e174f73a83fe3b5922f5aa515171fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718039
    > Auto-Submit: Quentin Pubert <qpubert@google.com>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1485593}
    
    Bug: 417688581
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Idcbef44013bba876c1afd41a688a7b768441fef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6729934
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485813}

--- ios/chrome/browser/fullscreen/ui_bundled/fullscreen_web_state_observer_unittest.mm
+++ ios/chrome/browser/fullscreen/ui_bundled/fullscreen_web_state_observer_unittest.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/fullscreen/ui_bundled/test/test_fullscreen_mediator.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
-#import "ios/chrome/browser/web/model/web_view_proxy/web_view_proxy_tab_helper.h"
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/security/ssl_status.h"
 #import "ios/web/public/test/fakes/fake_navigation_context.h"
@@ -31,7 +30,6 @@ class FullscreenWebStateObserverTest : public PlatformTest {
         TestFullscreenController::FromBrowser(browser_.get());
     mediator_ = std::make_unique<TestFullscreenMediator>(
         controller, controller->getModel());
-    WebViewProxyTabHelper::CreateForWebState(&web_state_);
     observer_ = std::make_unique<FullscreenWebStateObserver>(
         controller, controller->getModel(), mediator_.get());
     // Set up a FakeNavigationManager.
