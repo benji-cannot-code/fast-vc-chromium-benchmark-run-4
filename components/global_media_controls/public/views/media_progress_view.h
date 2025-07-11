4a18ecf8806a27c7985cf6bd51199fa61c71e11d
Tommy Steimel <steimel@chromium.org>
2025-07-11 16:51:52 -0700

    [global media controls][video pip] Ensure video is paused for dragging
    Both the global media controls and video pip use the MediaProgressView
    to allow users to seek media playback. If a video is playing when the
    user starts dragging the progress bar, we pause the video while they
    seek. However, if the video is already paused but then starts playing
    after a drag has started, then the video doesn't get paused.
    
    This CL changes the behavior to re-pause the video if it starts playing
    again. This fixes an issue where if the user clicks/drags quickly we
    can get into a race where the video is still paused from the previous
    drag and never re-paused for the new drag.
    
    Bug: 430216824
    Change-Id: Ia23686349c3e3df8ed9d92c324c181393762027b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6730640
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485885}

--- components/global_media_controls/public/views/media_progress_view.h
+++ components/global_media_controls/public/views/media_progress_view.h
@@ -114,6 +114,7 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaProgressView
   void OnProgressDragStarted(double location);
   void DelayedProgressDragStarted(double location);
   void OnProgressDragEnded();
+  void PauseForDragging();
 
   // Updates the colors of the progress view based on whether the media is
   // paused.
@@ -186,6 +187,9 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaProgressView
   // Whether the media is currently paused due to the user dragging the progress
   // line.
   bool paused_for_dragging_ = false;
+
+  // True if the user is currently dragging the progress line.
+  bool is_dragging_ = false;
 
   // Whether we should use the paused colors for the progress view.
   bool use_paused_colors_ = true;
