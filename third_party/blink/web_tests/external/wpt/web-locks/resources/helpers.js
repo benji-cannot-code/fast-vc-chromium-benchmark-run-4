7f431388d79bbd38b606ed7f6687ef82f74d7e0d
Anna Sato <annasato@chromium.org>
2025-07-10 22:54:14 -0700

    Add BackForwardCacheWithSharedWorker in fieldtrial_testing_config.json
    This CL removes unnecessary WPT expectation files, as pages with a
    SharedWorker can now enter BFCache.
    
    This also changes the behavior of tests where a SharedWorker holding a
    Web Lock was expected to block the page from being cached. We believe
    simply blocking the cache could not be the right long-term solution,
    because the ideal behavior for a multi-client SharedWorker is more
    complex and deserves a separate discussion.
    
    To unblock the main goal of enabling BFCache for SharedWorker, this
    change allows pages to be cached for now, even with a lock. We can
    address the ideal Web Lock interaction separately.
    
    Bug: 406420935
    Change-Id: I2ae84ef459877f3e449a9bab3945581cbd1cc4b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713496
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485376}

--- third_party/blink/web_tests/external/wpt/web-locks/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/web-locks/resources/helpers.js
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   };
   self.uniqueNameByQuery = () => {
     const prefix = new URL(location.href).searchParams.get('prefix');
-    return `${prefix}-${++res_num}`;
+    // Add randomness to prevent tests from timing out on leaked locks from
+    // previous sub-tests in the same file.
+    return `${prefix}-${++res_num}-${Math.random()}`;
   }
 
   // Inject an iframe showing the given url into the page, and resolve
