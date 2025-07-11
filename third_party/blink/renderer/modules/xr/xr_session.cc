f4f5f60f2508df83470d4aa822ee979de280ea77
Fredrik Söderquist <fs@opera.com>
2025-07-11 03:53:24 -0700

    Remove IDLEnumAsString()
    Everything has been migrated to bindings enumeration types, and most of
    the uses of this function has been replaced with better alternatives, so
    just fold the "new" variant into the remaining users.
    
    Bug: 430171134
    Change-Id: I512a8af5fef77cc92b4e2c27fded01eba3171ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718053
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485480}

--- third_party/blink/renderer/modules/xr/xr_session.cc
+++ third_party/blink/renderer/modules/xr/xr_session.cc
@@ -1443,7 +1443,7 @@ ScriptPromise<XRLightProbe> XRSession::requestLightProbe(
     exception_state.ThrowDOMException(
         DOMExceptionCode::kNotSupportedError,
         "Reflection format \"" +
-            IDLEnumAsString(light_probe_init->reflectionFormat()) +
+            light_probe_init->reflectionFormat().AsString() +
             "\" not supported.");
     return EmptyPromise();
   }
