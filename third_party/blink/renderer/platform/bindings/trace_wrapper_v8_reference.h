34de67255a425495900661a51f8ba6efd66553c0
Kent Tamura <tkent@chromium.org>
2025-07-11 01:55:16 -0700

    Move wtf/vector_traits.h to "blink" namespace
    All VectorTraits specializations are moved as well.
    
    This CL has no behavior changes.
    
    Bug: 422768753
    Change-Id: I6f3fcdd9734e95d23d19cad4c42787378b44ce5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6726056
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1485422}

--- third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h
+++ third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h
@@ -39,9 +39,12 @@ struct IsTraceable<blink::TraceWrapperV8Reference<T>> {
   static const bool value = true;
 };
 
+}  // namespace WTF
+namespace blink {
+
 template <typename T>
-struct VectorTraits<blink::TraceWrapperV8Reference<T>>
-    : VectorTraitsBase<blink::TraceWrapperV8Reference<T>> {
+struct VectorTraits<TraceWrapperV8Reference<T>>
+    : VectorTraitsBase<TraceWrapperV8Reference<T>> {
   STATIC_ONLY(VectorTraits);
 
   static constexpr bool kNeedsDestruction =
@@ -68,6 +71,6 @@ struct VectorTraits<blink::TraceWrapperV8Reference<T>>
                 "TraceWrapperV8Reference should be trivially destructible.");
 };
 
-}  // namespace WTF
+}  // namespace blink
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_TRACE_WRAPPER_V8_REFERENCE_H_
