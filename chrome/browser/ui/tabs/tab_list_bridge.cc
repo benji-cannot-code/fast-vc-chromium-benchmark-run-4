ccdaeeaf3742494943c21b6795e454561f016f85
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-07-11 12:29:50 -0700

    [Desktop Android] Add TabListInterface::From()
    Add a static getter for a TabListInterface to be retrieved from a given
    BrowserWindowInterface. This will allow using TabListInterface::From()
    on the experimental desktop android platform.
    
    The getter will be defined in platform-specific files. Since desktop
    android does not yet support a (real) BrowserWindowInterface
    implementation, the getter is left if-def'd out for now.
    
    This will enable pre-work to move more code to these general interfaces.
    
    Bug: 415961057
    Change-Id: I76bfed6118d8d3bd09c5f9d2dbbe668a4fdc585b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6723558
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485756}

--- chrome/browser/ui/tabs/tab_list_bridge.cc
+++ chrome/browser/ui/tabs/tab_list_bridge.cc
@@ -17,13 +17,6 @@ TabListBridge::TabListBridge(TabStripModel& tab_strip_model,
 
 TabListBridge::~TabListBridge() = default;
 
-// static
-TabListInterface* TabListBridge::From(
-    BrowserWindowInterface* browser_window_interface) {
-  return ui::ScopedUnownedUserData<TabListBridge>::Get(
-      browser_window_interface->GetUnownedUserDataHost());
-}
-
 int TabListBridge::GetTabCount() const {
   return tab_strip_->count();
 }
@@ -65,3 +58,11 @@ std::optional<tab_groups::TabGroupId> TabListBridge::AddTabsToGroup(
 void TabListBridge::Ungroup(const std::set<tabs::TabHandle>& tabs) {}
 
 void TabListBridge::MoveGroupTo(tab_groups::TabGroupId group_id, int index) {}
+
+// static
+// From //chrome/browser/ui/tabs/tab_list_interface.h
+TabListInterface* TabListInterface::From(
+    BrowserWindowInterface* browser_window_interface) {
+  return ui::ScopedUnownedUserData<TabListBridge>::Get(
+      browser_window_interface->GetUnownedUserDataHost());
+}
--- chrome/browser/ui/tabs/tab_list_interface.h
+++ chrome/browser/ui/tabs/tab_list_interface.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/tabs/public/tab_interface.h"
 #include "url/gurl.h"
 
+class BrowserWindowInterface;
+
 // Interface for supporting a basic set of tab operations on Android and
 // Desktop.
 class TabListInterface {
@@ -24,6 +26,13 @@ class TabListInterface {
 
   TabListInterface(const TabListInterface& other) = delete;
   void operator=(const TabListInterface& other) = delete;
+
+  // TODO(https://crbug.com/427503497): Include this on Android when the
+  // implementation is get-able from a BrowserWindowInterface.
+#if !BUILDFLAG(IS_ANDROID)
+  // Returns the TabListInterface associated with the given `browser`.
+  static TabListInterface* From(BrowserWindowInterface* browser);
+#endif
 
   // Returns the count of tabs within the tab list.
   virtual int GetTabCount() const = 0;
