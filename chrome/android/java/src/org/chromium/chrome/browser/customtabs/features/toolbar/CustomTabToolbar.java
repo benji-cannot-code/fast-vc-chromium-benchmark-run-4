5d2e6c1e81bd37528cf5248516e0cb7978978b97
Jinsuk Kim <jinsukkim@chromium.org>
2025-07-11 13:09:04 -0700

    [CCT] Make minimizer visibility change no-op when entering PiP mode
    Minimizer icon visibility need not altering when going in and out of
    PiP mode as the icon won't be visible in the mode anyway. This CL
    addresses it in CustomTabToolbar#isInMultiWindowMode so that it returns
    true only when in real split-screen mode, not in PiP mode.
    
    Bug: 428924144
    Change-Id: I65aa513dfc6e593f993bed9a89c246c4eacbd5de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6728187
    Reviewed-by: Moe Adel <adelm@google.com>
    Commit-Queue: Moe Adel <adelm@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485780}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
@@ -816,7 +816,9 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
 
     private boolean isInMultiWindowMode() {
         Activity activity = getActivityFromCurrentTab();
-        return MultiWindowUtils.getInstance().isInMultiWindowMode(activity);
+        if (activity == null) return false;
+        return !activity.isInPictureInPictureMode()
+                && MultiWindowUtils.getInstance().isInMultiWindowMode(activity);
     }
 
     /** Returns {@code true} if the optional button will be shown. */
