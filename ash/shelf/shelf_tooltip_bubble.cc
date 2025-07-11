648791278a76f67bb331d6dc3f03e463d124dff8
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-07-11 10:03:34 -0700

    Remove unnecessary layer for ShelfTooltipBubble
    After crrev.com/c/6501740, the client view of BubbleDialogs is always
    painted to a layer.
    
    Current layer hierarchy is:
    Widget [TEXTURED]
    +--ClientView [TEXTURED]
    
    However, ShelfTooltipBubble paints a transparent bubble (with no
    shadow), therefore there is no need paint widget and client view to a
    TEXTURED layer. We can only paint the contents view to a TEXTURED layer,
    which reduces overdraw and overhead related to an extra TEXTURED layer.
    
    New layer hierarchy is:
    Widget [NOT_DRAWN]
    +--ClientView [NOT_DRAWN]
    +----contents_view [TEXTURED]
    
    Bug: b:383933622
    Change-Id: I4c6c6f3365d0c63547c38812516f83654f231f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697695
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485650}

--- ash/shelf/shelf_tooltip_bubble.cc
+++ ash/shelf/shelf_tooltip_bubble.cc
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_id.h"
 #include "ash/style/style_util.h"
 #include "ash/wm/collision_detection/collision_detection_utils.h"
-#include "ui/aura/window.h"
-#include "ui/color/color_id.h"
+#include "ui/compositor/layer_type.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/corewm/tooltip_view_aura.h"
 #include "ui/views/layout/fill_layout.h"
@@ -23,6 +22,8 @@ ShelfTooltipBubble::ShelfTooltipBubble(
     const std::u16string& text,
     std::optional<views::BubbleBorder::Arrow> arrow_position)
     : ShelfBubble(anchor, alignment, /*for_tooltip=*/true, arrow_position) {
+  // BubbleDialog is used as a transparent container.
+  set_layer_type(ui::LAYER_NOT_DRAWN);
   set_close_on_deactivate(false);
   SetCanActivate(false);
   set_accept_events(false);
@@ -30,6 +31,7 @@ ShelfTooltipBubble::ShelfTooltipBubble(
   SetLayoutManager(std::make_unique<views::FillLayout>());
 
   set_margins(gfx::Insets(0));
+  SetPaintToLayer(ui::LAYER_TEXTURED);
   auto* tooltip_view = AddChildView(StyleUtil::CreateAshStyleTooltipView());
   tooltip_view->SetText(text);
 
