dc0341aae4fc1498d5d1826feeb69ce4f4c6c297
Sarah Krakowiak Criel <skrakowi@chromium.org>
2025-07-11 07:26:14 -0700

    Record histograms for tracking show original notifications accuracy
    Bug: 427251614
    Change-Id: Ic363eae511fb8581c2e783d5b7fee9f4fb92b6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715333
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485553}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationContentDetectionManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationContentDetectionManager.java
@@ -215,6 +215,8 @@ public class NotificationContentDetectionManager {
                     NotificationConstants.EXTRA_NOTIFICATION_INFO_WEBAPK_PACKAGE, mWebApkPackage);
             originalNotificationBackup.putString(
                     NotificationConstants.EXTRA_NOTIFICATION_INFO_CHANNEL_ID, mChannelId);
+            originalNotificationBackup.putInt(
+                    NotificationConstants.EXTRA_SUSPICIOUS_NOTIFICATION_COUNT, warningCount);
             notificationBuilder.setExtras(originalNotificationBackup);
 
             // Closing the notification should delete it.
@@ -331,7 +333,8 @@ public class NotificationContentDetectionManager {
             }
         }
 
-        void displayOriginalNotificationsFromBackups() {
+        int displayOriginalNotificationsFromBackups() {
+            int actualNumSuspiciousBackupsDisplayed = 0;
             for (String notificationId : mOriginalNotificationBackups.keySet()) {
                 // Skip the `mWarningNotificationId` because this will be displayed using the
                 // warning notification's extra Bundle.
@@ -349,7 +352,9 @@ public class NotificationContentDetectionManager {
                         notificationAttributes.mIncognito,
                         notificationAttributes.mWebApkPackage,
                         notificationAttributes.mChannelId);
+                actualNumSuspiciousBackupsDisplayed += 1;
             }
+            return actualNumSuspiciousBackupsDisplayed;
         }
     }
 
@@ -604,6 +609,17 @@ public class NotificationContentDetectionManager {
                                         warningNotificationExtras,
                                         NotificationConstants.EXTRA_NOTIFICATION_INFO_CHANNEL_ID);
 
+                        int expectedNumSuspiciousNotificationsLeft = 1;
+                        if (warningNotificationExtras != null
+                                && warningNotificationExtras.containsKey(
+                                        NotificationConstants
+                                                .EXTRA_SUSPICIOUS_NOTIFICATION_COUNT)) {
+                            expectedNumSuspiciousNotificationsLeft =
+                                    warningNotificationExtras.getInt(
+                                            NotificationConstants
+                                                    .EXTRA_SUSPICIOUS_NOTIFICATION_COUNT);
+                        }
+
                         // Display the original notification, whose backup is stored in the warning
                         // notification's backup `Bundle`.
                         displayOriginalNotification(
@@ -616,14 +632,33 @@ public class NotificationContentDetectionManager {
                                 webApkPackage,
                                 channelId);
 
-                        // Display the rest of the original notifications.
+                        // Record the number of suppressed suspicious notifications that the warning
+                        // displays to the user, then subtract 1 from
+                        // `expectedNumSuspiciousNotificationsLeft` because the first suspicious
+                        // notification is displayed using the backup stored in the `Bundle`.
+                        NotificationUmaTracker.recordSuspiciousNotificationCountOnShowOriginals(
+                                expectedNumSuspiciousNotificationsLeft);
+                        expectedNumSuspiciousNotificationsLeft -= 1;
+
+                        // Display the rest of the original notifications and record the number of
+                        // suspicious notifications that the user expected to be delivered but were
+                        // not.
                         if (sWarningNotificationAttributesByOrigin.containsKey(
                                 warningNotificationOrigin)) {
-                            sWarningNotificationAttributesByOrigin
-                                    .get(warningNotificationOrigin)
-                                    .displayOriginalNotificationsFromBackups();
+                            int actualNumSuspiciousBackupsDisplayed =
+                                    sWarningNotificationAttributesByOrigin
+                                            .get(warningNotificationOrigin)
+                                            .displayOriginalNotificationsFromBackups();
+                            NotificationUmaTracker
+                                    .recordSuspiciousNotificationsDroppedCountOnShowOriginals(
+                                            expectedNumSuspiciousNotificationsLeft
+                                                    - actualNumSuspiciousBackupsDisplayed);
                             sWarningNotificationAttributesByOrigin.remove(
                                     warningNotificationOrigin);
+                        } else {
+                            NotificationUmaTracker
+                                    .recordSuspiciousNotificationsDroppedCountOnShowOriginals(
+                                            expectedNumSuspiciousNotificationsLeft);
                         }
                     }
                 });
