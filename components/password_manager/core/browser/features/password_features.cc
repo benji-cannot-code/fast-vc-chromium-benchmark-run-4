70ffb3ed7b77c3dea6c47f31a0ffedd8420e8b35
Ioana Pandele <ioanap@chromium.org>
2025-07-11 04:13:06 -0700

    Add flag for actor login
    Bug: 429997678
    Change-Id: I4c157b270bb1f0030e5fc79b2451d22d17463c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6723331
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485488}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_buildflags.h"
 
 namespace password_manager::features {
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)  // Desktop
+BASE_FEATURE(kActorLogin, "ActorLogin", base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kAndroidSmsOtpFilling,
