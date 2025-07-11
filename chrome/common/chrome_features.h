85595f389837fce38376eca19ed0157d6d81df41
Maggie Jennings <mjenn@google.com>
2025-07-11 07:58:14 -0700

    [Task Icon] Add Chrome feature for the Glic Task Icon
    Bug: 422439931
    Change-Id: I83200bd0a61fb22150dca9ca4bf3dc0258c24aef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6725014
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485571}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -206,6 +206,7 @@ extern const base::FeatureParam<base::TimeDelta>(
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::FeatureParam<base::TimeDelta>(
     kGlicActorPageStabilityTimeout);
+COMPONENT_EXPORT(CHROME_FEATURES) BASE_DECLARE_FEATURE(kGlicActorTaskIcon);
 
 #if BUILDFLAG(ENABLE_GLIC)
 COMPONENT_EXPORT(CHROME_FEATURES) BASE_DECLARE_FEATURE(kGlic);
