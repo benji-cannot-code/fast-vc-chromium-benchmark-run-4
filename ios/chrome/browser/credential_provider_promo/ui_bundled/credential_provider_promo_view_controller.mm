948f487dee2e7d58fcf0d51053e624c63fdea808
Rafał Godlewski <rgod@google.com>
2025-07-11 14:21:28 -0700

    [iOS][CPE promo] Clean up kIOSPasskeysM2 flag
    The feature was enabled by default in M138.
    
    Bug: b:396694707
    Change-Id: Ia736ef56234c6859bf1d56688d1ee9874b31f89f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722174
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485824}

--- ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_view_controller.mm
+++ ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_view_controller.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_view_controller.h"
 
 #import "base/values.h"
-#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
@@ -141,8 +140,7 @@ NSString* const kCredentialProviderPromoAccessibilityId =
   self.alertScreen.titleTextStyle = UIFontTextStyleTitle2;
   self.alertScreen.topAlignedLayout = YES;
 
-  if ((!self.alertScreen.image && self.shouldShowAnimation) ||
-      (!self.shouldShowAnimation && IOSPasskeysM2Enabled())) {
+  if (!self.shouldShowAnimation || !self.alertScreen.image) {
     self.alertScreen.customSpacingBeforeImageIfNoNavigationBar =
         kCustomSpacingAtTopIfNoNavigationBar;
   }
@@ -203,8 +201,7 @@ NSString* const kCredentialProviderPromoAccessibilityId =
       self.alertScreen.sheetPresentationController;
   presentationController.prefersEdgeAttachedInCompactHeight = YES;
   presentationController.detents = @[
-    IOSPasskeysM2Enabled() ? self.alertScreen.preferredHeightDetent
-                           : [UISheetPresentationControllerDetent mediumDetent],
+    self.alertScreen.preferredHeightDetent,
     [UISheetPresentationControllerDetent largeDetent]
   ];
   presentationController.preferredCornerRadius = kPreferredCornerRadius;
