48ecd104bb5bef581be7ab531dbaaa2edfc5a806
Christian Xu <christianxu@google.com>
2025-07-11 03:23:23 -0700

    [iOS] Add AIM prototype flag
    Bug: None
    Change-Id: Ia600dd3ef48ecd5c246272891549c2ff84d79734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722377
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485465}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -24,6 +24,9 @@ namespace flag_descriptions {
 extern const char kAddAddressManuallyName[];
 extern const char kAddAddressManuallyDescription[];
 
+extern const char kAIMPrototypeName[];
+extern const char kAIMPrototypeDescription[];
+
 extern const char kAppBackgroundRefreshName[];
 extern const char kAppBackgroundRefreshDescription[];
 
