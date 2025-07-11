b3625fa2615ad16c4675bbbf9faf66bbdfebb0e8
Fred Shih <ffred@chromium.org>
2025-07-10 23:01:49 -0700

    Reland "[mojo] Add a verification step for typemap config"
    This is a reland of commit cbfb7d5ab0d7bfddd63cdd159f1fc618ef247791
    
    The breakage was caused by SerializedArrayBufferContents traits having
    an implicit dependency on //third_party/blink/renderer/core, which is
    not ok, because the source file is under platform. If we added the
    dep, it would cause a circular dep of platform -> core -> platform.
    
    This is fixed by moving array_buffer_contents.mojom to the mojom_core
    target, which seems correct to me anyway, because clients should have
    already used this target if they are using the typemap.
    
    To repro this, I had to explicitly build the target:
    obj/third_party/blink/public/mojom/mojom_platform_blink/array_buffer_contents.mojom-blink.o
    
    With this change, I verified that the build succeeds by building the
    target:
    obj/third_party/blink/public/mojom/mojom_core_blink/array_buffer_contents.mojom-blink.o
    
    Original change's description:
    > Add a verification step for typemap config
    >
    > If there is a typo in the typemap config, this will raise a warning
    > telling the developer that a typemap has not been referenced.
    >
    > Most of the change is mechanical (i.e.: removing dead typemaps that have
    > no effect).
    >
    > This change will force typemaps to be declared in the mojom rule that
    > contains the mojom module where the type is declared, which is probably
    > better because having another target declare the typemaps can lead to
    > some very... interesting.. side effects. One of these side effects is
    > that the type of the generated code will look different, depending on
    > your deps.
    >
    > Bug: 428039213
    > Change-Id: I75b40055cf9cc045b5e7890365550d846fb71145
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6692233
    > Owners-Override: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Fred Shih <ffred@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1484659}
    
    Bug: 428039213
    Change-Id: Iad8dd6d11c6ff4814ca187e39ca360c69fd5ae76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6724490
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485380}

--- third_party/blink/renderer/modules/serial/serial_port.cc
+++ third_party/blink/renderer/modules/serial/serial_port.cc
@@ -95,7 +95,7 @@ SerialPortInfo* SerialPort::getInfo() {
   if (info_->bluetooth_service_class_id) {
     info->setBluetoothServiceClassId(
         MakeGarbageCollected<V8UnionStringOrUnsignedLong>(
-            info_->bluetooth_service_class_id->uuid));
+            info_->bluetooth_service_class_id));
   }
   return info;
 }
