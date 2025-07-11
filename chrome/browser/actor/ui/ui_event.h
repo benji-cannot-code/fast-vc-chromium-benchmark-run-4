796d995f46eaf4b457b1fd8fd32f87a1ea2e6c4f
Kevin Graney <kmg@google.com>
2025-07-10 19:15:10 -0700

    Reland "Add TaskStateChanged UiEvent"
    This reverts commit 1d5dd3ac79c36a06ace8d72c240763008625dd92.
    
    Reason for revert: added missing variant that was merged in the wrong order.
    
    Bug: 425784083
    Original change's description:
    > Revert "Add TaskStateChanged UiEvent"
    >
    > This reverts commit d1057fb0460aa565ccfe5945dc7be3d924b96828.
    >
    > Reason for revert: suspected to cause compile failure:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/mac-arm64-dbg/64546/overview
    >
    > Bug: 425784083
    > Original change's description:
    > > Add TaskStateChanged UiEvent
    > >
    > > Also documented which ones are sent so far, and which aren't.
    > >
    > > Bug: 425784083
    > > Change-Id: I7cfda959b813f50f07c0e4ae2daf35399d9c1831
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722208
    > > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > > Commit-Queue: Kevin Graney <kmg@google.com>
    > > Reviewed-by: Abe Boujane <boujane@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1485297}
    >
    > Bug: 425784083
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I202a6102a991e7439c94cebdfd7d37d87b7d0e21
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6725039
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Auto-Submit: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Reviewed-by: Kevin Graney <kmg@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1485300}
    
    Bug: 425784083
    Change-Id: I3e8a01d85656b8332236f9bba75733bac306fd0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6726134
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485323}

--- chrome/browser/actor/ui/ui_event.h
+++ chrome/browser/actor/ui/ui_event.h
@@ -9,11 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <variant>
 
+#include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/shared_types.h"
 #include "chrome/browser/actor/task_id.h"
 #include "components/tabs/public/tab_interface.h"
 
 namespace actor::ui {
+// STATUS: Dispatched on first action from a task.  Will be refactored to
+// dispatch at a different point in the actuation flow.
 struct StartTask {
   actor::TaskId task_id;
 
@@ -22,6 +25,18 @@ struct StartTask {
   ~StartTask();
 };
 
+// STATUS: Not yet dispatched anywhere.
+struct TaskStateChanged {
+  actor::TaskId task_id;
+  ActorTask::State state;
+
+  TaskStateChanged(actor::TaskId, ActorTask::State);
+  TaskStateChanged(const TaskStateChanged&);
+  ~TaskStateChanged();
+};
+
+// STATUS: Dispatched on first action from a task.  Will be refactored to
+// dispatch at a different point in the actuation flow.
 struct StartingToActOnTab {
   tabs::TabInterface::Handle tab_handle;
   actor::TaskId task_id;
@@ -31,6 +46,7 @@ struct StartingToActOnTab {
   ~StartingToActOnTab();
 };
 
+// STATUS: Not yet dispatched anywhere.
 struct StoppedActingOnTab {
   tabs::TabInterface::Handle tab_handle;
 
@@ -39,6 +55,7 @@ struct StoppedActingOnTab {
   ~StoppedActingOnTab();
 };
 
+// STATUS: Dispatched pre-tool invocation.
 struct MouseMove {
   tabs::TabInterface::Handle tab_handle;
   PageTarget target;
@@ -48,6 +65,7 @@ struct MouseMove {
   ~MouseMove();
 };
 
+// STATUS: Dispatched pre-tool invocation.
 struct MouseClick {
   tabs::TabInterface::Handle tab_handle;
   MouseClickType click_type;
@@ -61,6 +79,7 @@ struct MouseClick {
 using UiEvent = std::variant<StartTask,
                              StartingToActOnTab,
                              StoppedActingOnTab,
+                             TaskStateChanged,
                              MouseClick,
                              MouseMove>;
 
