eff18760d1a2ae59522b3d3ec09156bdd3a9db32
Andreas Haas <ahaas@chromium.org>
2025-07-11 07:45:41 -0700

    Migrate MojoHandle to gin::Wrappable
    gin::Wrappable gets refactored to improve security and memory
    management. This CL migrates MojoHandle from the old
    gin::Wrappable to the new gin::Wrappable.
    
    Bug: 345640553
    
    Change-Id: I43ef574e238596e561624de7807d18c2a0128a04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718348
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485566}

--- gin/public/wrappable_pointer_tags.h
+++ gin/public/wrappable_pointer_tags.h
@@ -27,6 +27,7 @@ enum WrappablePointerTag : uint16_t {
   kChromePluginPlaceholder,            // ChromePluginPlaceholder
   kDomAutomationController,            // content::DomAutomationController
   kGpuBenchmarking,                    // content::GpuBenchmarking
+  kMojoHandle,                         // ax::MojoHandle
   kNetErrorPageController,             // NetErrorPageController
   kPDFPluginPlaceholder,               // PDFPluginPlaceholder
   kPluginPlaceholder,                  // plugins::PluginPlaceholder
