19ec6c6da720afcadab3320cf443e2bc8741f3d1
Judith Hemp <hempjudith@google.com>
2025-07-11 02:04:18 -0700

    [Permissions AI] Use threshold from metadata if model has it.
    Bug: 382447738
    Change-Id: Ic11f5d096bb348390757fddc8c76c25c2dd4179a
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627614
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485427}

--- components/permissions/prediction_service/permissions_aiv3_handler.h
+++ components/permissions/prediction_service/permissions_aiv3_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/inference/model_handler.h"
 #include "components/optimization_guide/proto/models.pb.h"
 #include "components/permissions/prediction_service/permissions_aiv3_encoder.h"
+#include "components/permissions/prediction_service/permissions_aiv3_model_metadata.pb.h"
 #include "components/permissions/prediction_service/prediction_model_metadata.pb.h"
 #include "components/permissions/prediction_service/prediction_service_messages.pb.h"
 
@@ -29,6 +30,7 @@ class PermissionsAiv3Handler : public optimization_guide::ModelHandler<
       RequestType request_type,
       scoped_refptr<base::SequencedTaskRunner> model_executor_task_runner,
       std::unique_ptr<PermissionsAiv3Encoder> model_executor);
+  ~PermissionsAiv3Handler() override;
 
   PermissionsAiv3Handler(const PermissionsAiv3Handler&) = delete;
   PermissionsAiv3Handler& operator=(const PermissionsAiv3Handler&) = delete;
@@ -38,9 +40,11 @@ class PermissionsAiv3Handler : public optimization_guide::ModelHandler<
       base::optional_ref<const optimization_guide::ModelInfo> model_info)
       override;
 
-  virtual void ExecuteModel(
-      ExecutionCallback callback,
-      std::unique_ptr<PermissionsAiv3Encoder::ModelInput> snapshot);
+  virtual void ExecuteModel(ExecutionCallback callback,
+                            std::unique_ptr<SkBitmap> snapshot);
+
+ private:
+  std::optional<PermissionsAiv3ModelMetadata> model_metadata_;
 };
 
 }  // namespace permissions
