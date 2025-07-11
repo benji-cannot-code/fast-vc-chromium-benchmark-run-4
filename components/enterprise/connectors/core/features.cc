ba8c7303744916ca2aba3681bc67be6f8492c921
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-07-11 08:55:38 -0700

    Clean up URLF reporting for Clank Finch experiment
    Change-Id: I2b10fa8ffaea1b1737d4520826fb292003533783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6728475
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485602}

--- components/enterprise/connectors/core/features.cc
+++ components/enterprise/connectors/core/features.cc
@@ -11,10 +11,6 @@ BASE_FEATURE(kEnterpriseSecurityEventReportingOnAndroid,
              "EnterpriseSecurityEventReportingOnAndroid",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kEnterpriseUrlFilteringEventReportingOnAndroid,
-             "EnterpriseUrlFilteringEventReportingOnAndroid",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kEnterpriseRealtimeEventReportingOnIOS,
              "EnterpriseRealtimeEventReportingOnIOS",
              base::FEATURE_ENABLED_BY_DEFAULT);
