8893dbf5603af82e274e2546e15555953cc998f3
Michael Tang <tangm@microsoft.com>
2025-07-11 14:01:31 -0700

    Allow --force_nv12_overlay_support=1 to actually force swap chain format
    This change fixes force_nv12_overlay_support to actually make
    SwapChainPresenter use a NV12 format, when applicable.
    
    This allows one to test the TryDisableDesktopPlane optimization with
    IDXGIDecodeSwapChains on devices that do not support YUV MPOs.
    
    force_nv12_overlay_support is a flag that affects the result of
    GetDirectCompositionSDROverlayFormat, used in
    SwapChainPresenter::GetSwapChainFormat. Previously GetSwapChainFormat
    exited early if !DirectCompositionHardwareOverlaysSupported() and fell
    back to BGRA8 if too may frames were reported as "composed" by DWM.
    
    This will not affect any existing devices because
    force_nv12_overlay_support is not actively being used by any GPU driver
    bug workaround.
    
    Change-Id: I46b92d2cf28ae835335e270306879804672cc562
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6723915
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1485811}

--- ui/gl/swap_chain_presenter.cc
+++ ui/gl/swap_chain_presenter.cc
@@ -526,7 +526,7 @@ DXGI_FORMAT SwapChainPresenter::GetSwapChainFormat(
     return sdr_yuv_overlay_format;
   }
 
-  if (!presentation_history_.Valid()) {
+  if (!presentation_history_.Valid() || IsSwapChainYuvFormatForced()) {
     // Prefer P010 swapchain when playing P010 SDR content on SDR system with
     // P010 MPO supported.
     return sdr_yuv_overlay_format;
