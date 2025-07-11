f4528a1d50ec15fe4475077fa1cdc920e64ed3d0
Etienne Pierre-doray <etiennep@chromium.org>
2025-07-10 17:05:13 -0700

    [tracing] Add wildcard disabled categories in chrome://traces recorder
    Otherwise all categories are enabled.
    
    Bug: 413057996
    Change-Id: Id2b6a2af738ba704e1a9863c5bb951172db49847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6723798
    Reviewed-by: Alex Attar <aattar@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485283}

--- content/browser/resources/traces_internals/trace_recorder.ts
+++ content/browser/resources/traces_internals/trace_recorder.ts
@@ -467,7 +467,7 @@ export class TraceRecorderElement extends CrLitElement {
   private createDefaultTrackEventConfig_(): TrackEventConfig {
     return {
       enabledCategories: [],
-      disabledCategories: [],
+      disabledCategories: ['*'],
       enabledTags: [],
       disabledTags: ['slow', 'debug', 'sensitive'],
     };
