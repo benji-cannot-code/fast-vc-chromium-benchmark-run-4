ddecacde9942472b45ddd6cf6348f903aec1708f
Gazal Agarwal <agazal@google.com>
2025-07-11 08:56:47 -0700

    Add submenu and back press handling
    Screenshot (sample): https://screenshot.googleplex.com/7DLVRDer5kmyUQi.png
    http://screencast/cast/NjQ3OTIzNzkxNjY1NTYxNnwyZmJmMWUwYy1lMg
    
    Bug: 418807464
    Change-Id: I4102bc7dbaf4f645490d9f404b0d1f73778ca539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719600
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485604}

--- ui/android/java/src/org/chromium/ui/listmenu/ListItemType.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListItemType.java
@@ -27,6 +27,7 @@ import java.lang.annotation.RetentionPolicy;
     ListItemType.CONTEXT_MENU_ITEM_WITH_CHECKBOX,
     ListItemType.CONTEXT_MENU_ITEM_WITH_RADIO_BUTTON,
     ListItemType.CONTEXT_MENU_ITEM_WITH_SUBMENU,
+    ListItemType.CONTEXT_MENU_SUBMENU_HEADER,
 })
 public @interface ListItemType {
     int DIVIDER = 0;
@@ -36,4 +37,5 @@ public @interface ListItemType {
     int CONTEXT_MENU_ITEM_WITH_CHECKBOX = 4;
     int CONTEXT_MENU_ITEM_WITH_RADIO_BUTTON = 5;
     int CONTEXT_MENU_ITEM_WITH_SUBMENU = 6;
+    int CONTEXT_MENU_SUBMENU_HEADER = 7;
 }
