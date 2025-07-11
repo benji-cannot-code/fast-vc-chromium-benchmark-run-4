f89ae8d05a31edf49fcdf5439ec85a4e07d04fb4
Ernest Nguyen Hung <ernn@google.com>
2025-07-11 01:56:13 -0700

    [UNO-d] Add on sign-in identity pill
    It adds a new identity pill state 'Making Chrome Yours...'. It
    substitutes the existing show identity name state (greetings) on
    sign-in. The greetings remain displayed as they are on startup.
    
    The new state is shown for 30 seconds or till the user clicks on the
    pill (whatever comes first). The state is consistently displayed across
    multiple browser windows. Upon clicking the pill while the state is
    active, the profile menu is open.
    
    Screenshot (internal only):
    https://screenshot.googleplex.com/AvVLrDxvGtJwCaK
    
    Bug: 417928515
    Change-Id: Ib975444553b90705bb08f5883015f7eb52aff52f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6707843
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485423}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_state_manager.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_state_manager.h
@@ -149,6 +149,7 @@ class AvatarToolbarButtonStateManager
     kGuestSession,
     kIncognitoProfile,
     kExplicitTextShowing,
+    kOnSignin,
     kShowIdentityName,
     kSigninPending,
     kSyncPaused,
