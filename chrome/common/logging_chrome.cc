00b4ca9b184071671867c8a1bbf2ffcfcb4d1068
Tom Sepez <tsepez@chromium.org>
2025-07-11 09:48:51 -0700

    Remove chrome/common/all_messages.h
    There are no more legacy IPC messages in chrome.
    
    Bug: 40621529
    Change-Id: Iffb83fee229a8edb92f4160889348c95ea2554cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6720194
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485638}

--- chrome/common/logging_chrome.cc
+++ chrome/common/logging_chrome.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_ipc_logging.h"
 #define IPC_LOG_TABLE_ADD_ENTRY(msg_id, logger) \
   content::RegisterIPCLogger(msg_id, logger)
-#include "chrome/common/all_messages.h"
 #endif
 
 #if BUILDFLAG(IS_WIN)
