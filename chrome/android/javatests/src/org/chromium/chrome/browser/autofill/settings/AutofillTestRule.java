222336c9f08e85f70d43c00cca5f29f42461bc6a
Joshua Tan <joshuatan@google.com>
2025-07-10 18:41:49 -0700

    Add NonCardPaymentMethodsManagement fragment.
    This is part of the refactoring described in go/a2a-settings-page-impl-1-pg.
    It initially contains only eWallet settings, but in the future will contain
    other non-card payment settings e.g. A2A.
    
    Follow-up changes will complete the refactoring by switching
    AutofillPaymentMethodsFragment to use this new fragment.
    
    Mock: https://screenshot.googleplex.com/6TPSAvVKjggnTaN
    Screenshot: https://screenshot.googleplex.com/94adD8SUsJeVLn4
    DD: go/a2a-settings-page-impl-1-pg
    
    Bug: 427314316
    Change-Id: Ie101eea3b0f208876d8e75ebe444209c28229af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716883
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Joshua Tan <joshuatan@google.com>
    Reviewed-by: Junhui He <junhuihe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1485303}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillTestRule.java
@@ -60,6 +60,7 @@ class AutofillTestRule extends ChromeBrowserTestRule
                 AutofillLocalCardEditor.setObserverForTest(AutofillTestRule.this);
                 AutofillLocalIbanEditor.setObserverForTest(AutofillTestRule.this);
                 FinancialAccountsManagementFragment.setObserverForTest(AutofillTestRule.this);
+                NonCardPaymentMethodsManagementFragment.setObserverForTest(AutofillTestRule.this);
                 statement.evaluate();
             }
         };
