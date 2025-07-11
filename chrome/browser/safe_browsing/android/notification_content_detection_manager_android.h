9e2be4cb6b1bca1043c2d51edfdbbb96860eed09
Sarah Krakowiak Criel <skrakowi@chromium.org>
2025-07-11 07:25:50 -0700

    Collapse notification warnings front end
    This CL suppresses notification warnings for an origin if one for that
    origin is already being shown. The string shown on the warning will also
    show the number of suppressed notifications from that origin and tapping
    “Show notification(s)” will make a best attempt at showing all
    suppressed notifications. We will also log the histogram in the event
    that a notification is suppressed.
    
    Bug: 412379309
    Change-Id: I6af1859c13cf0ec694379d9768b8615dfcd09715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663170
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485552}

--- chrome/browser/safe_browsing/android/notification_content_detection_manager_android.h
+++ chrome/browser/safe_browsing/android/notification_content_detection_manager_android.h
@@ -27,7 +27,8 @@ enum class SuspiciousNotificationWarningInteractions {
   kReportAsSafe = 5,
   kReportWarnedNotificationAsSpam = 6,
   kReportUnwarnedNotificationAsSpam = 7,
-  kMaxValue = kReportUnwarnedNotificationAsSpam,
+  kSuppressDuplicateWarning = 8,
+  kMaxValue = kSuppressDuplicateWarning,
 };
 
 void UpdateSuspiciousNotificationIds(HostContentSettingsMap* hcsm,
