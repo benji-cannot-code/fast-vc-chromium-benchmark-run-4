e9d7279fc66536849210eeb899a700b88d4130b6
James Cook <jamescook@chromium.org>
2025-07-13 09:24:55 -0700

    extensions: Rename desktop_android_extensions_browser_client.cc
    Rename it to chrome_extensions_browser_client_android.cc. This matches
    chrome_extensions_browser_client_non_android.cc and our other
    android file names as well.
    
    Bug: none
    Test: compiles
    Change-Id: Ib3922504f061fcfb58c50a72ca35958b074e3771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732019
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486105}

--- chrome/browser/extensions/chrome_extensions_browser_client_android.cc
+++ chrome/browser/extensions/chrome_extensions_browser_client_android.cc
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/extensions/chrome_extensions_browser_client.h"
+
 #include <memory>
 #include <utility>
 
-#include "chrome/browser/extensions/chrome_extensions_browser_client.h"
 #include "chrome/browser/extensions/desktop_android/desktop_android_extension_host_delegate.h"
 #include "chrome/browser/extensions/error_console/error_console.h"
 #include "chrome/browser/extensions/user_script_listener.h"
