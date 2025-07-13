8eb9875f4723707d5e3fc16e4553ea7f5e125029
Eshwar Stalin <estalin@chromium.org>
2025-07-13 14:09:07 -0700

    [SxS] Minor fixes to contents corner radius and scrim
    Fixing an issue where the content's corner radius on Windows isn't
    rounded as it is on macOS. Additionally, updating the bounds of the
    contents scrims to cover NTP footer view as well.
    
    Change-Id: If729c14dc94e688245e1d751540b0363728f9822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6731680
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486123}

--- chrome/browser/ui/views/frame/contents_container_view.cc
+++ chrome/browser/ui/views/frame/contents_container_view.cc
@@ -75,6 +75,7 @@ void ContentsContainerView::UpdateBorderAndOverlay(bool is_in_split,
   // split.
   if (!is_in_split) {
     SetBorder(nullptr);
+    contents_view_->holder()->SetCornerRadii(gfx::RoundedCornersF{0});
     contents_view_->SetBackgroundRadii(gfx::RoundedCornersF{0});
     contents_scrim_view_->SetRoundedCorners(gfx::RoundedCornersF{0});
     mini_toolbar_->SetVisible(false);
@@ -95,6 +96,7 @@ void ContentsContainerView::UpdateBorderAndOverlay(bool is_in_split,
       gfx::Insets(kSplitViewContentPadding)));
 
   if (contents_view_->GetBackgroundRadii() != kContentCornerRadius) {
+    contents_view_->holder()->SetCornerRadii(kContentCornerRadius);
     contents_view_->SetBackgroundRadii(kContentCornerRadius);
   }
   if (contents_scrim_view_->layer()->rounded_corner_radii() !=
@@ -148,10 +150,11 @@ views::ProposedLayout ContentsContainerView::CalculateProposedLayout(
   layouts.child_layouts.emplace_back(
       contents_view_.get(), contents_view_->GetVisible(), contents_rect);
 
+  // The scrim view should cover the entire contents bounds.
   CHECK(contents_scrim_view_);
   layouts.child_layouts.emplace_back(contents_scrim_view_.get(),
                                      contents_scrim_view_->GetVisible(),
-                                     contents_rect);
+                                     contents_bounds);
 
   // The scrim view should cover the entire contents bounds.
   if (inactive_split_scrim_view_) {
