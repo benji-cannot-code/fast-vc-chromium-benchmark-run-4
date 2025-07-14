02f60daf0a9cab6ae6838a446cc62f3f285537e4
Thomas Quintanilla <tquintanilla@google.com>
2025-07-14 09:57:30 -0700

    Report delivery retries with one navigation-based attempt.
    This is a follow-up to crrev.com/c/6172810.
    
    Instead of sending primarily on navigation, we will continue with our
    normal implementation using the NetworkConnectionTracker. Instead, we
    will adjust the retry system so that the final retry attempt made will
    be navigation-based, meaning that the report won't attempt to send again
    until a navigation successfully commits. This is Finch gated and the
    number of retries before this final navigation-based retry will be
    tested in a multi-arm experiment. The experimental values for this are:
     - 0 timed retries, 1 navigation retry.
     - 1 timed retry, 1 navigation retry.
     - 2 timed retries, 1 navigation retry.
    
    OBSOLETE_HISTOGRAM[Conversions.EventLevelReport.ReportRetriesTillSuccess
    OrFailure]=Bumped
    version to
    Conversions.EventLevelReport.ReportRetriesTillSuccessOrFailure2
    OBSOLETE_HISTOGRAM[Conversions.AggregatableReport.ReportRetriesTillSucce
    ssOrFailure]=Bumped
    version to
    Conversions.AggregatableReport.ReportRetriesTillSuccessOrFailure2
    
    Bug: 384870263
    Change-Id: I6dbfe58dcf800db5f6f7d33af78f8d2198472958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594632
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Auto-Submit: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486372}

--- content/browser/attribution_reporting/attribution_storage_sql.h
+++ content/browser/attribution_reporting/attribution_storage_sql.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/enum_set.h"
+#include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/containers/span.h"
 #include "base/files/file_path.h"
@@ -77,11 +78,11 @@ enum class RateLimitResult : int;
 class CONTENT_EXPORT AttributionStorageSql {
  public:
   // Version number of the database.
-  static constexpr int kCurrentVersionNumber = 69;
+  static constexpr int kCurrentVersionNumber = 70;
 
   // Earliest version which can use a `kCurrentVersionNumber` database
   // without failing.
-  static constexpr int kCompatibleVersionNumber = 69;
+  static constexpr int kCompatibleVersionNumber = 70;
 
   // Latest version of the database that cannot be upgraded to
   // `kCurrentVersionNumber` without razing the database.
@@ -224,6 +225,9 @@ class CONTENT_EXPORT AttributionStorageSql {
                                   base::Time new_report_time);
   bool AdjustOfflineReportTimes(base::TimeDelta min_delay,
                                 base::TimeDelta max_delay);
+  base::flat_map<AttributionReport::Type, int> AdjustNavigationRetryReportTimes(
+      base::TimeDelta min_delay,
+      base::TimeDelta max_delay);
   void ClearAllDataAllTime(bool delete_rate_limit_data);
   void ClearDataWithFilter(
       base::Time delete_begin,
