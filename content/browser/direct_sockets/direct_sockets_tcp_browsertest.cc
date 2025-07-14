065f731502d3f4aab746c656e3484ad514441974
Chris Fredrickson <cfredric@chromium.org>
2025-07-14 15:39:51 -0700

    Stop ignoring errors in ConnectToJsSocket in browsertests
    https://crrev.com/c/6728599 rewrote these assertions from
    `EXPECT_TRUE(result.value.is_none())` into the current form. But note
    that neither form checks for the presence of an error; see
    https://crsrc.org/c/content/public/test/browser_test_utils.h;drc=cb9896e7ae4688dc32a9315fee224e7688ed8087;l=844.
    EvalJsResult::value is default-constructed in the case of an error, so
    `result.value.is_none()` would still be true in that case.
    
    I suspect that these callsites meant to expect a successful "void" value
    from the JavaScript, i.e. meant to compare the whole result against
    `base::Value()`.
    
    Change-Id: I4ed14e84b33d2bd09dca500c8b4dff8873fe5ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6731368
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486617}

--- content/browser/direct_sockets/direct_sockets_tcp_browsertest.cc
+++ content/browser/direct_sockets/direct_sockets_tcp_browsertest.cc
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The tests in this file use the Network Service implementation of
 // NetworkContext, to test sending and receiving of data over TCP sockets.
 
-using testing::AnyOf;
 using testing::StartsWith;
 
 namespace content {
@@ -258,10 +257,8 @@ class DirectSocketsTcpBrowserTest : public ContentBrowserTest {
         )",
         kLocalhostAddress, port);
 
-    ASSERT_THAT(
-        EvalJs(shell(), content::test::WrapAsync(open_socket)),
-        AnyOf(EvalJsResult::IsError(),
-              EvalJsResult::IsOkAndHolds(base::test::IsJson(base::Value()))));
+    ASSERT_EQ(EvalJs(shell(), content::test::WrapAsync(open_socket)),
+              base::Value());
   }
 
  protected:
