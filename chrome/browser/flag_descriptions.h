160120094b0d76ddba7b8e3d3edb5324c4bcb2f1
Morten Stenshorne <mstensho@chromium.org>
2025-07-14 04:26:39 -0700

    [CodeHealth] Remove stale base::Feature AllowWgcWindowCapturer
    Enabled by default since M123.
    
    Bug: 362247185
    Change-Id: Ieb95b665833d4c82ddb7cde98a2ef47d85dd1437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6727096
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486244}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3580,9 +3580,6 @@ extern const char kUseWinrtMidiApiDescription[];
 extern const char kWebRtcAllowWgcScreenCapturerName[];
 extern const char kWebRtcAllowWgcScreenCapturerDescription[];
 
-extern const char kWebRtcAllowWgcWindowCapturerName[];
-extern const char kWebRtcAllowWgcWindowCapturerDescription[];
-
 extern const char kWebRtcWgcRequireBorderName[];
 extern const char kWebRtcWgcRequireBorderDescription[];
 
