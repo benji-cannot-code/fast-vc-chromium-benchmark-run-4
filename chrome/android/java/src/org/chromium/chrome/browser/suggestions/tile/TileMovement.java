ff1422bde3172bb072fa67b395282deeb2f9c875
Samuel Huang <huangs@chromium.org>
2025-07-14 09:37:30 -0700

    [MVT Customization] Implement auto-scroll for drag-and-drop to reorder.
    Previously, Custom Tiles (CTs) could be reordered with drag-and-drop.
    However, attempting to move a Tile to a position that's not currently
    visible will require multiple drag-and-drop states. This CL improves
    polish by having the MVT automatically scroll when a CT is dragged to
    the edge of MVT container. Behavior:
    * Dragging a Tile near / beyond the left and right edges of the MVT
      container (the "sense zones") triggers horizontal scroll.
    * Scrolling reveals more MVT content, but the Tile being dragged should
      appear stationary.
    * Scroll continues while the pointer is held, and stops when the pointer
      is released, is moved outside of the sense zone, or when scroll
      bounds have been reached.
    * During scroll, transient drag-and-drop animation work as before; on
      pointer release, the drop action takes place.
    
    Refactoring: Getting TileDragSession to use setX(): Previously the
    "from" Tile (being dragged) is moved setTranslationX() to simplify
    (e.g., passing 0 resets position; and drag anchor can be ignored).
    However, for auto-scroll it makes more sense to use setX() (e.g., to
    enforce constraints), so this CL makes the change.
    
    Details:
    * During drag-and-drop / auto-scroll we have 3 nested Views of interest:
      "Outer" (container fixture), "Inner" (content with all tiles), "Tile".
    * MostVisitedTilesLayout: Add getScrollView() to return "Outer".
    * TileDragSession.Delegate (Impl = TileDragDelegateImpl):
      * Add isAutoScrollEnabled(). Impl: Enable always (for now).
      * Add getOuterView(). Impl: Forward getScrollView() from above.
    * When Inner is shifted by scroll, the Tile would move as well. For it
      to appear stationary, it will need "counter-shift".
    * Add TileDragAutoScroll.Delegate (Impl = TileDragSession):
      * Add getScrollInnerX(), getScrollTileX().
      * Add scrollInnerXBy().
      * Add onAutoScroll(). Impl: Performs "counter-shift" above.
    * Add TileDragAutoScroll: Main logic for auto-shift.
      * To ensure proper scaling, distances params (sense zone and scroll
        speed) are defined relative to Tile width.
      * Sense zones: Specify position and width. Position requires Tile's
        bulk to stick out beyond left / right boundaries, to reduce the
        likelihood of unintended scroll. Width defines region where the
        scroll speed increases as pointer into the sense zones. The increase
        can be nonlinear, defined by easing() function.
      * CTOR: Define sense zone and scroll params
      * Add computeScrollFactor() for sense zone detection and scroll
        velocity calculation.
      * Add run() for main flow, and potentially trigger self-call after
        delayed to create animation.
      * Add stop() to stop existing delay loop.
      * Tweakable configs (target for tweaking in follow-ups).
        * SCROLL_SENSE_X_START_RATIO: Location of sense zones.
        * SCROLL_SENSE_WIDTH_RATIO: Width of sense zones.
        * SCROLL_DELAY_MS: Auto-scroll animation delay.
        * SCROLL_SPEED_RATIO: Max auto-scroll speed.
        * easing(): [0, 1] -> [0, 1] function for scroll speed scaling.
    * TileDragSession:
      * Make the setTransitionX() -> setX() refactor.
      * Implement TileDragAutoScroll.Delegate.
      * start(): Instantiate TileDragAutoScroll if enabled.
      * updateFromView(): Entrance to auto-scroll logic.
      * finish(): Remember to stop auto-scroll.
    
    Bug: 423661092, 388782412, 431765443
    Change-Id: I2705b7ea87becedfd509abdeb9b8f9dadaaa8d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716039
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486365}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/TileMovement.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/TileMovement.java
@@ -207,7 +207,7 @@ public class TileMovement {
                 oldAnimator.cancel();
                 mAnimators.set(i, null);
             }
-            mTileViews.get(i).setTranslationX(0f);
+            mTileViews.get(i).setX(mOriginalX.get(i));
         }
     }
 }
