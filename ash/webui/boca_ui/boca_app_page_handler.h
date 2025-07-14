dbc037e2b3121e023832af1f6cb41af62cb492fd
aprilzhou@google.com <aprilzhou@google.com>
2025-07-14 11:05:58 -0700

    boca: Disconnect when no frame received for 5 seconds.
    Entire CRD path is not unit test covered yet, will be addressed in
    b/424867979
    
    Bug: b:427814956
    Change-Id: Ia60b652b961d7270d0b1df140310bfaff6c324da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6724938
    Reviewed-by: Dorian Brandon <dorianbrandon@google.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Dorian Brandon <dorianbrandon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1486424}

--- ash/webui/boca_ui/boca_app_page_handler.h
+++ ash/webui/boca_ui/boca_app_page_handler.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/thread_annotations.h"
+#include "base/timer/timer.h"
 #include "chromeos/ash/components/boca/boca_session_manager.h"
 #include "chromeos/ash/components/boca/on_task/on_task_system_web_app_manager.h"
 #include "chromeos/ash/components/boca/proto/roster.pb.h"
@@ -63,6 +64,7 @@ class BocaAppHandler : public mojom::PageHandler,
 
   ~BocaAppHandler() override;
   // Static
+  inline static constexpr int kSpotlightFrameTimeout = 5;
   static void SetFloatModeAndBoundsForWindow(bool is_float_mode,
                                              aura::Window* window,
                                              SetFloatModeCallback callback);
@@ -250,6 +252,8 @@ class BocaAppHandler : public mojom::PageHandler,
 
   void OnUpdateSessionBlockingRequestCompleted();
 
+  void OnSpotlightFrameTimeout();
+
   BocaSessionManager* GetSessionManager();
 
   void SetAccountImage(user_manager::User* user);
@@ -272,6 +276,8 @@ class BocaAppHandler : public mojom::PageHandler,
   mojo::Receiver<boca::mojom::PageHandler> receiver_;
   mojo::Remote<boca::mojom::Page> remote_;
   raw_ptr<SpotlightService> spotlight_service_;
+  base::OneShotTimer spotlight_frame_timeout_timer_
+      GUARDED_BY_CONTEXT(sequence_checker_);
   const raw_ptr<OnTaskSystemWebAppManager> system_web_app_manager_;
   raw_ptr<SessionClientImpl> session_client_impl_;
   raw_ptr<content::WebUI> web_ui_;
