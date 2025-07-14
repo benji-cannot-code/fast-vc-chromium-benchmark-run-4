6df65b9b25af5eb01089460467a63f3d9ed6ea9d
Maggie Jennings <mjenn@google.com>
2025-07-14 10:07:21 -0700

    [Task Icon] Add task icon controller skeleton
    Bug: 422439931
    Change-Id: I5b119990872844ecbe0de236eb03e772abd824f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6728681
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486379}

--- chrome/browser/ui/browser_window/internal/browser_window_features.cc
+++ chrome/browser/ui/browser_window/internal/browser_window_features.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/performance_controls/memory_saver_opt_in_iph_controller.h"
 #include "chrome/browser/ui/signin/signin_view_controller.h"
 #include "chrome/browser/ui/sync/browser_synced_window_delegate.h"
+#include "chrome/browser/ui/tabs/glic_actor_task_icon_controller.h"
 #include "chrome/browser/ui/tabs/glic_nudge_controller.h"
 #include "chrome/browser/ui/tabs/organization/tab_declutter_controller.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/most_recent_shared_tab_update_store.h"
@@ -193,6 +194,11 @@ void BrowserWindowFeatures::Init(BrowserWindowInterface* browser) {
       glic_iph_controller_ = std::make_unique<glic::GlicIphController>(browser);
       glic_nudge_controller_ =
           std::make_unique<tabs::GlicNudgeController>(browser);
+
+      if (base::FeatureList::IsEnabled(features::kGlicActorTaskIcon)) {
+        glic_actor_task_icon_controller_ =
+            std::make_unique<tabs::GlicActorTaskIconController>(browser);
+      }
     }
 #endif  // BUILDFLAG(ENABLE_GLIC)
   }
