fc3c556a7cc69dba7fed233e56f1c3fd6d0199f1
Evan Liu <evliu@google.com>
2025-07-14 15:01:20 -0700

    Fix soda-linux builder
    This CL fixes the soda-linux builder.
    
    Fixed: 380489471
    Change-Id: Ic3cf5dd59258f2991a4fa55d5c8a85f068e63f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734815
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Auto-Submit: Evan Liu <evliu@google.com>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486595}

--- chrome/browser/speech/speech_recognition_service_browsertest.cc
+++ chrome/browser/speech/speech_recognition_service_browsertest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_device_description.h"
 #include "media/audio/wav_audio_handler.h"
 #include "media/base/audio_bus.h"
+#include "media/base/audio_sample_types.h"
 #include "media/mojo/mojom/audio_data.mojom.h"
 #include "media/mojo/mojom/audio_data_pipe.mojom.h"
 #include "media/mojo/mojom/audio_input_stream.mojom.h"
@@ -338,7 +339,7 @@ void SpeechRecognitionServiceTest::SendAudioChunk(
     }
 
     speech_recognition_recognizer_->SendAudioToSpeechRecognitionService(
-        std::move(signed_buffer));
+        std::move(signed_buffer), std::nullopt);
     chunk_start += chunk_size;
 
     // Sleep for 20ms to simulate real-time audio. SODA requires audio
--- chrome/services/speech/soda/soda_client_impl_unittest.cc
+++ chrome/services/speech/soda/soda_client_impl_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/speech/soda/soda_test_paths.h"
 #include "media/audio/wav_audio_handler.h"
 #include "media/base/audio_bus.h"
+#include "media/base/audio_sample_types.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 #if BUILDFLAG(IS_WIN)
