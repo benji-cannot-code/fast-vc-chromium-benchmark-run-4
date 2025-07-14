706ca428038e84627337ab219eb577d493f110a8
Vasiliy Telezhnikov <vasilyt@chromium.org>
2025-07-14 10:14:21 -0700

    Remove SurfaceTexture code paths
    Not used anymore.
    
    Bug: 40276517
    Change-Id: Ifdd830affcb9c898129eae3fdf288983925d5bf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6728780
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486382}

--- gpu/command_buffer/service/shared_image/android_video_image_backing.cc
+++ gpu/command_buffer/service/shared_image/android_video_image_backing.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/ref_counted_lock.h"
 #include "gpu/command_buffer/service/shared_context_state.h"
 #include "gpu/command_buffer/service/shared_image/video_image_reader_image_backing.h"
-#include "gpu/command_buffer/service/shared_image/video_surface_texture_image_backing.h"
 #include "gpu/command_buffer/service/texture_owner.h"
 #include "gpu/vulkan/vulkan_device_queue.h"
 #include "gpu/vulkan/vulkan_implementation.h"
--- gpu/command_buffer/service/texture_owner.cc
+++ gpu/command_buffer/service/texture_owner.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/decoder_context.h"
 #include "gpu/command_buffer/service/feature_info.h"
 #include "gpu/command_buffer/service/image_reader_gl_owner.h"
-#include "gpu/command_buffer/service/surface_texture_gl_owner.h"
 #include "gpu/command_buffer/service/texture_base.h"
 #include "ui/gl/scoped_binders.h"
 #include "ui/gl/scoped_make_current.h"
