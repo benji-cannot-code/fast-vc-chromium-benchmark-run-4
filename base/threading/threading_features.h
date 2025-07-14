977cf51ffe94edae46c1512318b29b64a59062d0
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-07-14 08:00:20 -0700

    Enable HangWatcher for sandboxed GPU process
    For unsandboxed processes, the HangWatcher is started from
    RunOtherNamedProcessTypeMain or RunZygote in
    content/app/content_main_runner_impl.cc. For sandboxed, the startup is
    delayed to after the sandbox is initialized, in each individual
    process' main function, in renderer_main.cc, utility_main.cc or
    gpu_main.cc.
    
    This CL enables HangWatcher in gpu_main.cc. This change can't be tested
    from a sibling test file though (e.g. gpu_main_browsertest.cc) because
    gpu_main.cc lives under src/content and
    HangWatcher::InitializeOnMainThread is only called for chrome, in
    chrome/app/chrome_main_delegate.cc. The test is therefore done in the
    associated test file chrome_main_delegate_browsertest.cc
    
    Bug: 413314866
    Change-Id: Ibb77ec9a7e726032f7873a187955f55e46984997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6646926
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486305}

--- base/threading/threading_features.h
+++ base/threading/threading_features.h
@@ -30,6 +30,7 @@ BASE_EXPORT BASE_DECLARE_FEATURE(kUserInteractiveCompositingMac);
 
 BASE_EXPORT BASE_DECLARE_FEATURE(kEnableHangWatcher);
 BASE_EXPORT BASE_DECLARE_FEATURE(kEnableHangWatcherOnGpuProcess);
+BASE_EXPORT extern const char kHangWatcherMonitoringPeriodParam[];
 
 // Hang watcher log levels.
 BASE_EXPORT extern const char kBrowserProcessIoThreadLogLevelParam[];
