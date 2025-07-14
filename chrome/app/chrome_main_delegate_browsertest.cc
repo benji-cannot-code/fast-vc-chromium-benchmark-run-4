a11b7a7c85517645bdcb4d3596dfe8bb434b5f96
Xinghui Lu <xinghuilu@chromium.org>
2025-07-14 12:47:08 -0700

    Disable test RecordsUncoveredTime under ASan/LSan.
    Failure under Linux ASan LSan Tests:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/135051/test-results
    
    Bug: 431802107
    Change-Id: I11402e784badaa2c88a7c0af9f5a01763d80f907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6735097
    Owners-Override: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486504}

--- chrome/app/chrome_main_delegate_browsertest.cc
+++ chrome/app/chrome_main_delegate_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_timeouts.h"
 #include "base/threading/threading_features.h"
 #include "base/timer/elapsed_timer.h"
+#include "build/build_config.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/metrics/content/subprocess_metrics_provider.h"
 #include "content/public/test/browser_test.h"
@@ -45,8 +46,14 @@ class ChromeMainDelegateHangWatcherTest : public InProcessBrowserTest {
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 // Tests that UncoveredStartupTime histograms are recorded for all watched
 // sandboxed processes.
+// TODO(crbug.com/431802107): Re-enable this test once the failure is fixed.
+#if defined(LEAK_SANITIZER) || defined(ADDRESS_SANITIZER)
+#define MAYBE_RecordsUncoveredTime DISABLED_RecordsUncoveredTime
+#else
+#define MAYBE_RecordsUncoveredTime RecordsUncoveredTime
+#endif
 IN_PROC_BROWSER_TEST_F(ChromeMainDelegateHangWatcherTest,
-                       RecordsUncoveredTime) {
+                       MAYBE_RecordsUncoveredTime) {
   metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
   EXPECT_THAT(
       histogram_tester_.GetAllSamplesForPrefix("HangWatcher"),
