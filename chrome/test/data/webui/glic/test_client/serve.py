d3c866ea2ed03e97cb9d1a80e3c81eaa91ce48ba
Carlos Knippschild <carlosk@chromium.org>
2025-07-14 15:58:42 -0700

    Serve now prints usable command line arguments for local Glic URLs
    Change-Id: I81771c8a84bf2f1829150eba166ab893e3da0386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6727802
    Auto-Submit: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486621}

--- chrome/test/data/webui/glic/test_client/serve.py
+++ chrome/test/data/webui/glic/test_client/serve.py
@@ -27,6 +27,7 @@ from google.protobuf.message import DecodeError
 from google.protobuf import json_format
 from google.protobuf import text_format
 
+
 def build(outdir: str):
     subprocess.run([
         shutil.which('autoninja'), '-C', outdir,
@@ -35,6 +36,7 @@ def build(outdir: str):
                    stdout=sys.stdout,
                    stderr=sys.stderr,
                    check=True)
+
 
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
     directory = None
@@ -143,7 +145,8 @@ def main():
               file=sys.stderr)
         sys.exit(1)
 
-    # Allows us to import generated proto bindings for common_quality_data.proto.
+    # Allows us to import generated proto bindings for
+    # common_quality_data.proto.
     sys.path.insert(
         0,
         os.path.join(args.outdir, 'pyproto', 'components',
@@ -153,7 +156,11 @@ def main():
 
     with socketserver.ThreadingTCPServer((server_addr, args.port),
                                          RequestHandler) as httpd:
-        print("Server started at localhost:" + str(args.port))
+        url_prefix = f"http://localhost:{args.port}/glic/test_client"
+        print(f"Server started on port {args.port}.",
+              "Use the following command line arguments to connect to it:")
+        print(f'--glic-fre-url="{url_prefix}/fre.html"',
+              f'--glic-guest-url="{url_prefix}/index.html"')
         httpd.serve_forever()
 
 
