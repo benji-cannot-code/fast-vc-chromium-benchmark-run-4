8da0c4a8b19b49d3ad6aa99222ba251d45f6dbc0
Shiyi Zou <shiyi.zou@intel.com>
2025-07-14 18:13:45 -0700

    webnn: let ORT release the external weights that are no longer needed
    This CL introduces `ExternalWeightsManager` which inherits
    `OrtAllocator` and is passed into ORT via
    `CreateTensorWithDataAndDeleterAsOrtValue()` to let ORT manage the
    lifecycle of the external weights.
    
    Bug: 411465403
    Change-Id: I19279694292c4857e06821bb0efa60703ca3217f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701729
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Shiyi Zou <shiyi.zou@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1486676}

--- services/webnn/ort/model_editor.h
+++ services/webnn/ort/model_editor.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webnn::ort {
 
+class ExternalWeightsManager;
+
 class COMPONENT_EXPORT(WEBNN_SERVICE) ModelEditor {
  public:
   struct COMPONENT_EXPORT(WEBNN_SERVICE) ModelInfo {
@@ -30,9 +32,13 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) ModelEditor {
     ModelInfo& operator=(const ModelInfo&) = delete;
     ~ModelInfo();
 
+    // `external_weights_manager` should be prior to `model` since
+    // `external_weights_manager` will be called by ORT to release the external
+    // weights during `model` destruction.
+    std::unique_ptr<ExternalWeightsManager> external_weights_manager;
+
     ScopedOrtModel model;
-    // The external data should be kept alive during graph inferencing.
-    std::vector<base::HeapArray<uint8_t>> external_data;
+
     base::flat_map<std::string, std::string>
         operand_input_name_to_onnx_input_name;
     base::flat_map<std::string, std::string>
@@ -87,14 +93,12 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) ModelEditor {
                                base::span<const int64_t> shape,
                                base::span<const uint8_t> data);
 
-  // Add an initializer and transfer the data into `ModelInfo::external_data`.
+  // Add an initializer and transfer the data into
+  // `model_info_->external_weights_manager`.
   void AddInitializerAsExternalData(base::cstring_view name,
                                     ONNXTensorElementDataType data_type,
                                     base::span<const int64_t> shape,
                                     base::HeapArray<uint8_t> data);
-
-  // Describes where the constant buffer resides in memory.
-  ScopedOrtMemoryInfo memory_info_;
 
   std::vector<ScopedOrtValueInfo> inputs_;
   std::vector<ScopedOrtValueInfo> outputs_;
