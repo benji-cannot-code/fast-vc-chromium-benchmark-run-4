fe21c9f5b14ee088e8826e59941a22b998fce50c
Brandon Wylie <wylieb@chromium.org>
2025-07-14 19:25:28 -0700

    Reland "[declutter] Support active tab groups for archive all flag"
    This is a reland of commit ab78d8f5a3304d5cfd91f32abf4ae3f89c608818
    
    Original change's description:
    > [declutter] Support active tab groups for archive all flag
    >
    > Bug: 430616630
    > Change-Id: Ib79f1a90b9b07ea77be90f351d308d9ec1e96a9a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6729834
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Commit-Queue: Brandon Wylie <wylieb@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1485810}
    
    Bug: 430616630
    Change-Id: Id5011aa2a32e918090fec34422e0c64b0846b95a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6735402
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486700}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
@@ -26,6 +26,7 @@ import static org.chromium.chrome.browser.preferences.ChromePreferenceKeys.UI_TH
 import static org.chromium.chrome.browser.tab.Tab.INVALID_TAB_ID;
 import static org.chromium.chrome.browser.tabmodel.TabList.INVALID_TAB_INDEX;
 
+import androidx.test.annotation.UiThreadTest;
 import androidx.test.filters.MediumTest;
 
 import org.junit.After;
@@ -73,6 +74,7 @@ import org.chromium.components.tab_group_sync.SavedTabGroup;
 import org.chromium.components.tab_group_sync.SavedTabGroupTab;
 import org.chromium.components.tab_group_sync.TabGroupSyncService;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -188,6 +190,22 @@ public class TabArchiverTest {
     @AfterClass
     public static void tearDownTestSuite() {
         ActivityFinisher.finishAll();
+    }
+
+    @Test
+    @MediumTest
+    @UiThreadTest
+    public void testGetTabsToArchive_emptyTabModel() {
+        mRegularTabModel.getTabRemover().forceCloseTabs(TabClosureParams.closeAllTabs().build());
+        assertEquals(0, mRegularTabModel.getCount());
+        assertEquals(0, mArchivedTabModel.getCount());
+
+        assertEquals(
+                mTabArchiver.getTabsToArchive(
+                        mRegularTabModelSelector
+                                .getTabGroupModelFilterProvider()
+                                .getCurrentTabGroupModelFilter()),
+                new ArrayList<>());
     }
 
     @Test
