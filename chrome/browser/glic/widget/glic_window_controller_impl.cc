8e3f2c8a6393bd3b573298a6c4d4d84e7598e459
Andrea Gomez <andreaxg@google.com>
2025-07-15 10:47:45 -0700

    Record Glic position relative to the Chrome window on open/close
    b: 419035236
    Change-Id: Idfc62a956e7a31244d0bb426042d862a8d3825aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6571136
    Auto-Submit: Andrea Gomez <andreaxg@google.com>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487065}

--- chrome/browser/glic/widget/glic_window_controller_impl.cc
+++ chrome/browser/glic/widget/glic_window_controller_impl.cc
@@ -656,7 +656,8 @@ void GlicWindowControllerImpl::Show(Browser* browser,
       GetGlicWidget()->GetNearestDisplay();
   gfx::Point glic_center_point =
       GetGlicWidget()->GetWindowBoundsInScreen().CenterPoint();
-  glic_service_->metrics()->OnGlicWindowShown(display, glic_center_point);
+  glic_service_->metrics()->OnGlicWindowShown(browser, display,
+                                              glic_center_point);
 }
 
 void GlicWindowControllerImpl::SetupGlicWidget(Browser* browser) {
@@ -1081,7 +1082,8 @@ void GlicWindowControllerImpl::Close() {
       GetGlicWidget()->GetNearestDisplay();
   gfx::Point glic_center_point =
       GetGlicWidget()->GetWindowBoundsInScreen().CenterPoint();
-  glic_service_->metrics()->OnGlicWindowClose(display, glic_center_point);
+  glic_service_->metrics()->OnGlicWindowClose(
+      BrowserList::GetInstance()->GetLastActive(), display, glic_center_point);
   base::UmaHistogramEnumeration("Glic.PanelWebUiState.FinishState2",
                                 host().GetPrimaryWebUiState());
 
