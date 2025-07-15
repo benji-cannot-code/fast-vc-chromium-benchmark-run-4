7b31f5ddc70ae7d088318f4bf9b4f5316ccd9322
Xi Han <hanxi@google.com>
2025-07-15 14:09:41 -0700

    [Theme] Set NTP background type before notifying observers.
    Bug: 423579377
    Change-Id: I2f6e7b7a5e5114c17205ea1cf31294b8118781b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734655
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487233}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationConfigManager.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationConfigManager.java
@@ -112,6 +112,11 @@ public class NtpCustomizationConfigManager {
      * @param bitmap : The NTP's background image.
      */
     public void onBackgroundChanged(@Nullable Bitmap bitmap) {
+        NtpCustomizationUtils.setNtpBackgroundImageType(
+                bitmap == null
+                        ? NtpBackgroundImageType.DEFAULT
+                        : NtpBackgroundImageType.IMAGE_FROM_DISK);
+
         notifyBackgroundImageChanged(bitmap);
 
         NtpCustomizationUtils.updateBackgroundImageFile(bitmap);
