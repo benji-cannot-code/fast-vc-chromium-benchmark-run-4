ad9482c5a809d3871916adb9d60aa2c15af51591
Jeremy Roman <jbroman@chromium.org>
2025-07-15 07:49:40 -0700

    actor: Revert 2798ebca and make renderer tools async again.
    Some tools (notably typing) do need to complete asynchronously.
    
    Bug: b:424264856
    Change-Id: Ib261dd61f5d9e1619219d53972c603c89c7a45d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6730756
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486923}

--- chrome/renderer/actor/mouse_move_tool.h
+++ chrome/renderer/actor/mouse_move_tool.h
@@ -33,7 +33,7 @@ class MouseMoveTool : public ToolBase {
   ~MouseMoveTool() override;
 
   // actor::ToolBase
-  mojom::ActionResultPtr Execute() override;
+  void Execute(ToolFinishedCallback callback) override;
   std::string DebugString() const override;
 
  private:
