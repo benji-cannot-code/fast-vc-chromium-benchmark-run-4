a1fd6e4a6da1cdd5bda20d11c6a8468e00096ffa
Slobodan Pejic <slobodan@chromium.org>
2025-07-15 09:10:02 -0700

    Add browser bound key tests for secure payment confirmation.
    This change adds web platform tests for browser bound key.
    
    Tested: Manually on Chrome Canary for Android
    Change-Id: I5bb962c15a4ebf37d18d8477580f2bee9e664894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685068
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486970}

--- third_party/blink/web_tests/external/wpt/secure-payment-confirmation/utils.sub.js
+++ third_party/blink/web_tests/external/wpt/secure-payment-confirmation/utils.sub.js
@@ -19,8 +19,16 @@ const INVALID_ICON_DATA_URL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACA
 // Creates and returns a WebAuthn credential, optionally with the payment
 // extension set.
 //
+// `options` is object containing additional options for creating the request.
+// The options include the following:
+//
+// * "browserBoundPubKeyCredParams": The credential creation parameters to set
+//   on the payment extension. set_payment_extension must be set to true in
+//   order for this option to apply.
+//
 // Assumes that a virtual authenticator has already been created.
-async function createCredential(set_payment_extension=true) {
+async function createCredential(set_payment_extension = true, options = {}) {
+  options = Object.assign({browserBoundPubKeyCredParams: []}, options);
   const challengeBytes = new Uint8Array(16);
   window.crypto.getRandomValues(challengeBytes);
 
@@ -48,7 +56,10 @@ async function createCredential(set_payment_extension=true) {
 
   if (set_payment_extension) {
     publicKey.extensions = {
-      payment: { isPayment: true },
+      payment: {
+        isPayment: true,
+        browserBoundPubKeyCredParams: options.browserBoundPubKeyCredParams
+      },
     };
   }
 
@@ -101,4 +112,3 @@ function base64UrlEncode(data) {
   let result = btoa(data);
   return result.replace(/=+$/g, '').replace(/\+/g, "-").replace(/\//g, "_");
 }
-
