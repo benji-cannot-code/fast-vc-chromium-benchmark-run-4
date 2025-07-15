34337515aa7ce7c1e641eda7234a4c03e2549f3d
Daniel Rubery <drubery@chromium.org>
2025-07-15 10:54:16 -0700

    Refactor SessionInclusionRules to use DBSC-specific host patterns
    SchemeHostPortMatcherRules aren't well-represented in the spec, so we've
    restricted to a small set of host patterns. These are used to match URLs
    (for session scoping) and to match the hosts of origins (for mitigating
    timing side channels). SchemeHostPortMatcherRule is a poor fit for
    origins, so add DBSC-specific host pattern matching.
    
    Note that the validation in SessionInclusionRules::AddUrlRuleIfValid is
    far less relevant after https://crrev.com/c/6444326. Then, the
    validation was the only thing keeping arbitrary URLs from being in
    scope. Today, origin scoping (or site scoping) takes precedence, so the
    validation is only helping site owners know about possible
    accidental misconfiguration.
    
    Bug: 401184805
    Change-Id: Id773859706b41c0c08943b0ad1e08d2e29c27766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715461
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487076}

---
+++ net/device_bound_sessions/host_patterns_unittest.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/device_bound_sessions/host_patterns.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace net::device_bound_sessions {
+
+namespace {
+
+TEST(HostPatterns, IsValidHostPattern) {
+  EXPECT_FALSE(IsValidHostPattern(""));
+  EXPECT_FALSE(IsValidHostPattern("a.*.test"));
+  EXPECT_FALSE(IsValidHostPattern("*a.test"));
+
+  EXPECT_TRUE(IsValidHostPattern("*"));
+  EXPECT_TRUE(IsValidHostPattern("example.test"));
+  EXPECT_TRUE(IsValidHostPattern("*.example.test"));
+  EXPECT_TRUE(IsValidHostPattern("[1:abcd::3:4:ff]"));
+}
+
+TEST(HostPatterns, MatchesHostPattern) {
+  EXPECT_TRUE(MatchesHostPattern("*", GURL("https://example.test").host()));
+  EXPECT_TRUE(
+      MatchesHostPattern("example.test", GURL("https://example.test").host()));
+  EXPECT_FALSE(MatchesHostPattern("*.example.test",
+                                  GURL("https://example.test").host()));
+  EXPECT_TRUE(MatchesHostPattern(
+      "*.example.test", GURL("https://subdomain.example.test").host()));
+  EXPECT_FALSE(MatchesHostPattern("notexample.test",
+                                  GURL("https://example.test/").host()));
+  EXPECT_TRUE(MatchesHostPattern("[1:abcd::3:4:ff]",
+                                 GURL("https://[1:abcd::3:4:ff]/").host()));
+}
+
+}  // namespace
+
+}  // namespace net::device_bound_sessions
