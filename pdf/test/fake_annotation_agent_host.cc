dda67afa7c9d58154abb240fe23942e563dba1c1
William Liu <liuwilliam@chromium.org>
2025-07-15 13:28:31 -0700

    Introduce PDFAnnotationAgent
    PDFAnnotationAgent is the renderer endpoint of the annotation IPC. It
    highlights text fragments of PDFs and is able to scroll the highlighted
    text fragment into the viewport.
    
    It is owned uniquely by the PDFViewWebPlugin (in the next CL).
    
    Bug: b:395859365
    Change-Id: Icb49d0b5bb43cbb734027959301bd4e18c9726ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732465
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487199}

---
+++ pdf/test/fake_annotation_agent_host.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "pdf/test/fake_annotation_agent_host.h"
+
+namespace chrome_pdf {
+
+FakeAnnotationAgentHost::FakeAnnotationAgentHost() = default;
+
+FakeAnnotationAgentHost::~FakeAnnotationAgentHost() = default;
+
+void FakeAnnotationAgentHost::DidFinishAttachment(
+    const gfx::Rect& document_relative_rect,
+    blink::mojom::AttachmentResult attachment_result) {
+  attachment_result_ = attachment_result;
+  run_loop_.Quit();
+}
+
+void FakeAnnotationAgentHost::ScrollIntoView() {
+  annotation_agent_remote_->ScrollIntoView(/*applies_focus=*/true);
+  annotation_agent_remote_.FlushForTesting();
+}
+
+blink::mojom::AttachmentResult
+FakeAnnotationAgentHost::WaitForAttachmentResult() {
+  run_loop_.Run();
+  return *attachment_result_;
+}
+
+}  // namespace chrome_pdf
