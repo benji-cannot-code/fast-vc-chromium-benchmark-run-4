d8d89250b2c7506f7e431e3e8dbe7b1fc9817ae7
Stephen Chenney <schenney@chromium.org>
2025-07-14 18:34:04 -0700

    Check for null style in caret-blinking code
    The crash bug revealed the problem of
    GetComputedStyleForElementOrLayoutObject returning null when getting the
    caret-shape style. Fix it.
    
    Bug: 431138376
    Change-Id: Ic3e34b39b81d0f1ee402f30a6c2bd201877ba3cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734947
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486683}

--- third_party/blink/renderer/core/editing/frame_caret.cc
+++ third_party/blink/renderer/core/editing/frame_caret.cc
@@ -134,10 +134,12 @@ PositionWithAffinity FrameCaret::UpdateAppearance() {
 
   SetBlinkingDisabled(false);
   if (RuntimeEnabledFeatures::CSSCaretAnimationEnabled() &&
-      caret_position.AnchorNode() &&
-      GetComputedStyleForElementOrLayoutObject(*caret_position.AnchorNode())
-              ->CaretAnimation() == ECaretAnimation::kManual) {
-    SetBlinkingDisabled(true);
+      caret_position.AnchorNode()) {
+    const auto* style =
+        GetComputedStyleForElementOrLayoutObject(*caret_position.AnchorNode());
+    if (style && style->CaretAnimation() == ECaretAnimation::kManual) {
+      SetBlinkingDisabled(true);
+    }
   }
 
   // Start blinking with a black caret. Be sure not to restart if we're
@@ -223,9 +225,11 @@ CaretShape FrameCaret::GetCaretShape() const {
   PositionWithAffinity caret_position = CaretPosition();
   CaretShape caret_shape = CaretShape::kBar;
   if (caret_position.AnchorNode() && IsEditable(*caret_position.AnchorNode())) {
-    caret_shape = GetCaretShapeFromComputedStyle(
-        *GetComputedStyleForElementOrLayoutObject(
-            *caret_position.AnchorNode()));
+    const auto* style =
+        GetComputedStyleForElementOrLayoutObject(*caret_position.AnchorNode());
+    if (style) {
+      caret_shape = GetCaretShapeFromComputedStyle(*style);
+    }
   }
   return caret_shape;
 }
