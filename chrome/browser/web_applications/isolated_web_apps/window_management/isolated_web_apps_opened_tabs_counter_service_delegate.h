eca34dada9d99e40843ffee11fa997c1269ff1f6
Magdalena Skarbinska <mskarbinska@google.com>
2025-07-15 06:48:09 -0700

    IWA: Add Close button to IsolatedWebAppsOpenedTabsCounterService notification
    This button enables the user to close all tabs/windows opened by the IWA
    that is mentioned in the notification.
    
    Change-Id: I65d3f4cef96886c1e263a8f320932b2a797cc17b
    Bug: 400677358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700788
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1486887}

--- chrome/browser/web_applications/isolated_web_apps/window_management/isolated_web_apps_opened_tabs_counter_service_delegate.h
+++ chrome/browser/web_applications/isolated_web_apps/window_management/isolated_web_apps_opened_tabs_counter_service_delegate.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_ISOLATED_WEB_APPS_WINDOW_MANAGEMENT_ISOLATED_WEB_APPS_OPENED_TABS_COUNTER_SERVICE_DELEGATE_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_ISOLATED_WEB_APPS_WINDOW_MANAGEMENT_ISOLATED_WEB_APPS_OPENED_TABS_COUNTER_SERVICE_DELEGATE_H_
 
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
+#include "chrome/browser/web_applications/isolated_web_apps/window_management/isolated_web_apps_opened_tabs_counter_service.h"
 #include "components/webapps/common/web_app_id.h"
 #include "ui/message_center/public/cpp/notification_delegate.h"
 
@@ -15,8 +16,15 @@ class Profile;
 class IsolatedWebAppsOpenedTabsCounterServiceDelegate
     : public message_center::NotificationDelegate {
  public:
-  IsolatedWebAppsOpenedTabsCounterServiceDelegate(Profile* profile,
-                                                  const webapps::AppId& app_id);
+  IsolatedWebAppsOpenedTabsCounterServiceDelegate(
+      Profile* profile,
+      const webapps::AppId& app_id,
+      IsolatedWebAppsOpenedTabsCounterService::CloseWebContentsCallback
+          close_web_contents_callback,
+      IsolatedWebAppsOpenedTabsCounterService::NotificationAcknowledgedCallback
+          notification_acknowledged_callback,
+      IsolatedWebAppsOpenedTabsCounterService::CloseNotificationCallback
+          close_notification_callback);
 
   IsolatedWebAppsOpenedTabsCounterServiceDelegate(
       const IsolatedWebAppsOpenedTabsCounterServiceDelegate&) = delete;
@@ -24,15 +32,23 @@ class IsolatedWebAppsOpenedTabsCounterServiceDelegate
       const IsolatedWebAppsOpenedTabsCounterServiceDelegate&) = delete;
 
  protected:
-  ~IsolatedWebAppsOpenedTabsCounterServiceDelegate() override = default;
+  ~IsolatedWebAppsOpenedTabsCounterServiceDelegate() override;
 
   // message_center::NotificationDelegate:
   void Click(const std::optional<int>& button_index,
              const std::optional<std::u16string>& reply) override;
+  void Close(bool by_user) override;
 
  private:
   const raw_ref<Profile> profile_;
   const webapps::AppId app_id_;
+
+  IsolatedWebAppsOpenedTabsCounterService::CloseWebContentsCallback
+      close_web_contents_callback_;
+  IsolatedWebAppsOpenedTabsCounterService::NotificationAcknowledgedCallback
+      notification_acknowledged_callback_;
+  IsolatedWebAppsOpenedTabsCounterService::CloseNotificationCallback
+      close_notification_callback_;
 };
 
 #endif  // CHROME_BROWSER_WEB_APPLICATIONS_ISOLATED_WEB_APPS_WINDOW_MANAGEMENT_ISOLATED_WEB_APPS_OPENED_TABS_COUNTER_SERVICE_DELEGATE_H_
