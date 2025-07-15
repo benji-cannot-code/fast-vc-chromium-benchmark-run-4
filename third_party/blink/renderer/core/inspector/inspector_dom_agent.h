ffa5d509ddf2fb6f070c4efcbe1786a2ac376bdf
Philip Pfaffe <pfaffe@chromium.org>
2025-07-15 02:34:50 -0700

    A DevTools API to force-open popovers
    Debugging popovers is hard since they often automatically close when
    moving focus to the devtools window (be it interest-invoked or manually
    opened ones). This CL adds a devtools API to force a popover open. Any
    hidePopover call is suppressed and the popover can only be hidden from
    the devtools API once opened this way.
    
    Bug: 413580647
    Change-Id: I98412fbd6f2128b22f6fe92fe188758618d20194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6486638
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486783}

--- third_party/blink/renderer/core/inspector/inspector_dom_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_dom_agent.h
@@ -58,6 +58,7 @@ class Document;
 class DocumentLoader;
 class DummyExceptionStateForTesting;
 class Element;
+class HTMLElement;
 class HTMLFrameOwnerElement;
 class HTMLSlotElement;
 class InspectedFrames;
@@ -274,6 +275,12 @@ class CORE_EXPORT InspectorDOMAgent final
                                       std::optional<String> anchor_specifier,
                                       int* out_node_id) override;
 
+  protocol::Response forceShowPopover(
+      int node_id,
+      bool enable,
+      std::unique_ptr<protocol::Array<int>>* out_nodeIds) override;
+  void WillHidePopover(HTMLElement* element, bool* force_open);
+
   bool Enabled() const;
   IncludeWhitespaceEnum IncludeWhitespace() const;
   void ReleaseDanglingNodes();
@@ -402,6 +409,7 @@ class CORE_EXPORT InspectorDOMAgent final
   Node* NodeForPath(const String& path);
 
   void DiscardFrontendBindings();
+  void ReleaseForcedPopovers();
 
   InspectorRevalidateDOMTask* RevalidateTask();
 
@@ -421,6 +429,8 @@ class CORE_EXPORT InspectorDOMAgent final
   HashSet<int> children_requested_;
   HashSet<int> distributed_nodes_requested_;
   HashMap<int, int> cached_child_count_;
+  HeapHashSet<WeakMember<Node>> forced_popovers_;
+  HeapHashSet<WeakMember<Node>> popovers_currently_being_hidden_;
   int last_node_id_;
   Member<Document> document_;
   using SearchResults =
