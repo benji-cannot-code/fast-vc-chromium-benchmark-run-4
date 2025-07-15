096add6c75ece63e2feaf6efecb8c6e52d49d133
Mohamed Amir Yosef <mamir@chromium.org>
2025-07-15 05:02:26 -0700

    [DC] Simplify credential extraction code to use PendingIntentHandler
    This change cleans up the implementation of the code used to retrieve
    digital credentials from the credential manager by making use of
    PendingIntentHandler.retrieveGetCredentialResponse. That simplifies
    code, removes some duplicated code and makes it easier to follow and to
    update in the future.
    
    This CL shouldn't introduce any behavioral change.
    
    Bug: 373841707
    Change-Id: I56877358ad577ec6f5576f9454dfaa551f3fc992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6733483
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486844}

--- chrome/browser/webid/android/junit/src/org/chromium/chrome/browser/webid/DigitalCredentialsPresentationDelegateTest.java
+++ chrome/browser/webid/android/junit/src/org/chromium/chrome/browser/webid/DigitalCredentialsPresentationDelegateTest.java
@@ -15,8 +15,6 @@ import static org.mockito.Mockito.when;
 import static org.chromium.chrome.browser.webid.DigitalCredentialsPresentationDelegate.BUNDLE_KEY_IDENTITY_TOKEN;
 import static org.chromium.chrome.browser.webid.DigitalCredentialsPresentationDelegate.BUNDLE_KEY_PROVIDER_DATA;
 import static org.chromium.chrome.browser.webid.DigitalCredentialsPresentationDelegate.BUNDLE_KEY_REQUEST_JSON;
-import static org.chromium.chrome.browser.webid.DigitalCredentialsPresentationDelegate.EXTRA_CREDENTIAL_DATA;
-import static org.chromium.chrome.browser.webid.DigitalCredentialsPresentationDelegate.EXTRA_GET_CREDENTIAL_RESPONSE;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -45,6 +43,14 @@ public class DigitalCredentialsPresentationDelegateTest {
             "androidx.identitycredentials.EXTRA_CREDENTIAL_TYPE";
     private static final String INTENT_HELPER_EXTRA_CREDENTIAL_DATA =
             "androidx.identitycredentials.EXTRA_CREDENTIAL_DATA";
+    private static final String EXTRA_CREDENTIAL_TYPE =
+            "androidx.credentials.provider.extra.EXTRA_CREDENTIAL_TYPE";
+    private static final String EXTRA_CREDENTIAL_DATA =
+            "androidx.credentials.provider.extra.EXTRA_CREDENTIAL_DATA";
+    private static final String EXTRA_GET_CREDENTIAL_RESPONSE =
+            "android.service.credentials.extra.GET_CREDENTIAL_RESPONSE";
+    private static final String CREDENTIAL_TYPE_DIGITAL_CREDENTIAL =
+            "androidx.credentials.TYPE_DIGITAL_CREDENTIAL";
     private static final String JSON_PROTOCOL = "openid4vp";
     private static final String JSON_DATA = "{\"test_key\":\"test_value\"}";
     private static final String JSON_WITH_PROTOCOL =
@@ -73,6 +79,7 @@ public class DigitalCredentialsPresentationDelegateTest {
         credentialBundle.putString(BUNDLE_KEY_REQUEST_JSON, json);
 
         Bundle dataBundle = new Bundle();
+        dataBundle.putString(EXTRA_CREDENTIAL_TYPE, CREDENTIAL_TYPE_DIGITAL_CREDENTIAL);
         dataBundle.putBundle(EXTRA_CREDENTIAL_DATA, credentialBundle);
 
         intent.putExtra(EXTRA_GET_CREDENTIAL_RESPONSE, dataBundle);
@@ -85,7 +92,7 @@ public class DigitalCredentialsPresentationDelegateTest {
         // Credential and GetCredentialResponse classes don't exist in Robolectric test and hence we
         // mock them.
         Credential credential = mock(Credential.class);
-        when(credential.getType()).thenReturn("androidx.credentials.TYPE_DIGITAL_CREDENTIAL");
+        when(credential.getType()).thenReturn(CREDENTIAL_TYPE_DIGITAL_CREDENTIAL);
         when(credential.getData()).thenReturn(credentialBundle);
 
         GetCredentialResponse getCredentialResponse = mock(GetCredentialResponse.class);
@@ -117,7 +124,7 @@ public class DigitalCredentialsPresentationDelegateTest {
     }
 
     @Test
-    public void testExtractDigitalCredentialFromGetResponse_NewFormat_ProcotolInResponse()
+    public void testExtractDigitalCredentialFromGetResponse_NewFormat_ProtocolInResponse()
             throws JSONException, NullPointerException, GetCredentialException {
         Intent intent = new Intent();
         packageResponseJsonInNewFormat(JSON_WITH_PROTOCOL, intent);
@@ -133,7 +140,7 @@ public class DigitalCredentialsPresentationDelegateTest {
     }
 
     @Test
-    public void testExtractDigitalCredentialFromGetResponse_BothFormats_ProcotolInResponse()
+    public void testExtractDigitalCredentialFromGetResponse_BothFormats_ProtocolInResponse()
             throws JSONException, NullPointerException, GetCredentialException {
         Intent intent = new Intent();
         packageResponseJsonInNewFormat(JSON_WITH_PROTOCOL, intent);
@@ -151,7 +158,7 @@ public class DigitalCredentialsPresentationDelegateTest {
     }
 
     @Test
-    public void testExtractDigitalCredentialFromGetResponse_BothFormats_NoProcotolInResponse()
+    public void testExtractDigitalCredentialFromGetResponse_BothFormats_NoProtocolInResponse()
             throws JSONException, NullPointerException, GetCredentialException {
         Intent intent = new Intent();
         packageResponseJsonInNewFormat(JSON_WITHOUT_PROTOCOL, intent);
