c16b8fd350ac76895960fda72b8f4c36b3978284
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-07-15 05:28:37 -0700

    spanification: remove `#pragma allow_unsafe_buffers` where unneeded
    This is a clean up of any files that now compile without the pragma.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    //tools/clang/spanify/remove-unneeded-pragmas.py
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --expect-owners-override --no-reviewers -d ~/description.txt`
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 414858406
    Change-Id: Ib2817661fd0d61df1637514fcfd0b5cd814e0d0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6733252
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486857}

--- ui/ozone/platform/flatland/client_native_pixmap_factory_flatland.cc
+++ ui/ozone/platform/flatland/client_native_pixmap_factory_flatland.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include "ui/ozone/platform/flatland/client_native_pixmap_factory_flatland.h"
 
