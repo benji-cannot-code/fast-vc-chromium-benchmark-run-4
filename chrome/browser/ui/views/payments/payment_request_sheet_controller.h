3fb0535af602e08415f57f7f401d2c987dcd3280
Rouslan Solomakhin <rouslan@google.com>
2025-07-15 08:12:49 -0700

    Efficient and accessible Payment Handler UI updates
    This patch updates the Payment Handler UI to prevent inefficient header
    rebuilds that were previously triggered by color or text changes. These
    rebuilds caused accessibility failures due to losing keyboard focus.
    
    PaymentHandlerWebFlowViewController keeps track of navigations to
    different pages, which may have different theme colors. If a page does
    not define its own theme, then PaymentHandlerWebFlowViewController uses
    Chrome's default theme color.
    
    The new approach updates header elements in-place, which resolves the
    accessibility issue.
    
    Fixed: 418001134
    Change-Id: Idbcdad8ad5d4c25263b2b067def9b4038f8dbfcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732014
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486947}

--- chrome/browser/ui/views/payments/payment_request_sheet_controller.h
+++ chrome/browser/ui/views/payments/payment_request_sheet_controller.h
@@ -202,6 +202,9 @@ class PaymentRequestSheetController {
   // issues.
   virtual base::WeakPtr<PaymentRequestSheetController> GetWeakPtr() = 0;
 
+ protected:
+  views::View* header_view() { return header_view_; }
+
  private:
   // Add the primary/secondary buttons to |container|.
   void AddPrimaryButton(views::View* container);
