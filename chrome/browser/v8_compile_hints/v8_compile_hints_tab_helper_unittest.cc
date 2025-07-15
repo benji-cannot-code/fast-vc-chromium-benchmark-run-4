94194a531217ba20033e42886240c98698183d04
Morten Stenshorne <mstensho@chromium.org>
2025-07-15 08:12:15 -0700

    [CodeHealth] Remove stale base::Feature ConsumeCompileHints
    Enabled by default since M122.
    
    Bug: 362246572
    Change-Id: Ie80e0af4d797bde2bc1744e8b3867026761a0878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6733256
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486946}

--- chrome/browser/v8_compile_hints/v8_compile_hints_tab_helper_unittest.cc
+++ chrome/browser/v8_compile_hints/v8_compile_hints_tab_helper_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_proto_util.h"
 #include "content/public/test/navigation_simulator.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/common/features.h"
 
 using ::testing::_;
 using ::testing::An;
@@ -49,9 +48,7 @@ void V8CompileHintsTabHelperTest::SetUp() {
   ChromeRenderViewHostTestHarness::SetUp();
   CreateSessionServiceTabHelper(web_contents());
   scoped_feature_list_.InitWithFeatures(
-      {blink::features::kConsumeCompileHints,
-       optimization_guide::features::kOptimizationHints},
-      {});
+      {optimization_guide::features::kOptimizationHints}, {});
 
   mock_optimization_guide_keyed_service_ =
       static_cast<NiceMock<MockOptimizationGuideKeyedService>*>(
