0982fdd8bfed8cc9d4b01f03c5009c47a97065e6
Ramon Cano Aparicio <rcanoaparicio@google.com>
2025-07-15 00:59:15 -0700

    ChromeBrowserFieldTrialsTest call CreateFallbackSamplingTrials.
    The test was calling to
    ChromeBrowserFieldTrials::SetUpClientSideFieldTrials() directly when
    trying to create again the client-side sampling trials. That created
    some unintended behavior, re-creating also other client-side trials that
    may be created in the SetUpClientSideFieldTrials() method.
    
    This forced those other client-side trials to support being created
    multiple times, and that's not correct.
    
    Call CreateFallbackSamplingTrialsIfNeeded() and
    CreateFallbackUkmSamplingTrialIfNeeded() directly instead.
    
    Change-Id: I413550284cd5a99f66ac0ec97473c2bc721c145e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734082
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Cr-Commit-Position: refs/heads/main@{#1486734}

--- chrome/browser/chrome_browser_field_trials_unittest.cc
+++ chrome/browser/chrome_browser_field_trials_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/feature_list.h"
+#include "chrome/browser/metrics/chrome_browser_sampling_trials.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/ukm/ukm_recorder_impl.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -49,10 +50,12 @@ TEST(ChromeBrowserFieldTrialsTest, SamplingTrials) {
 #endif  // BUILDFLAG(IS_ANDROID)
   EXPECT_TRUE(base::FieldTrialList::TrialExists(kUkmSamplingTrialName));
 
-  // Call SetUpClientSideFieldTrials() again. This should be a no-op,
+  // Try to create the sampling trials again. This should be a no-op,
   // since the sampling trials already exist. If the trials are created again,
   // a CHECK will be triggered and this will crash.
-  chrome_browser_field_trials.SetUpClientSideFieldTrials(
-      /*has_seed=*/false, entropy_providers, feature_list.get());
+  metrics::CreateFallbackSamplingTrialsIfNeeded(
+      entropy_providers.default_entropy(), feature_list.get());
+  metrics::CreateFallbackUkmSamplingTrialIfNeeded(
+      entropy_providers.default_entropy(), feature_list.get());
 }
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_ANDROID)
