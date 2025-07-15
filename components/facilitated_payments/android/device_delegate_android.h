2fe6b99df341cb648e98d854bdd60d44b69d6163
Vishwas Uppoor <vishwasuppoor@google.com>
2025-07-14 17:09:55 -0700

    [PixAL] Create test API for DeviceDelegateAndroid
    Create a test API for DeviceDelegateAndroid before friending tests for
    accessing private members gets out of hand.
    
    Bug: 431344394
    Change-Id: Iba9dcf7ebd42eeeb4c384ab29d5dba80a5a78d36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732678
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1486644}

--- components/facilitated_payments/android/device_delegate_android.h
+++ components/facilitated_payments/android/device_delegate_android.h
@@ -10,13 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/application_status_listener.h"
 #include "base/functional/callback.h"
-#include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "components/facilitated_payments/core/browser/device_delegate.h"
 #include "components/facilitated_payments/core/browser/facilitated_payments_app_info_list.h"
 #include "content/public/browser/web_contents.h"
 
 namespace payments::facilitated {
+
+class DeviceDelegateAndroidTestApi;
 
 // Android implementation of `DeviceDelegate`.
 class DeviceDelegateAndroid : public DeviceDelegate {
@@ -42,21 +43,7 @@ class DeviceDelegateAndroid : public DeviceDelegate {
       const GURL& payment_link_url) override;
 
  private:
-  friend class DeviceDelegateAndroidTest;
-
-  FRIEND_TEST_ALL_PREFIXES(DeviceDelegateAndroidTest,
-                           ChromeGoesToBackgroundThenForeground_CallbackRun);
-  FRIEND_TEST_ALL_PREFIXES(
-      DeviceDelegateAndroidTest,
-      ChromeGoesToForegroundWithoutGoingToBackground_CallbackNotRun);
-  FRIEND_TEST_ALL_PREFIXES(DeviceDelegateAndroidTest,
-                           ChromeGoesToBackground_CallbackNotRun);
-  FRIEND_TEST_ALL_PREFIXES(
-      DeviceDelegateAndroidTest,
-      MultipleBackgroundForegroundCycles_CallbackRunOnlyOnce);
-  FRIEND_TEST_ALL_PREFIXES(
-      DeviceDelegateAndroidTest,
-      CallbackSetAfterChromeAlreadyInBackground_ThenForeground_CallbackNotRun);
+  friend class DeviceDelegateAndroidTestApi;
 
   // Called when the Chrome app's state changes.
   void OnApplicationStateChanged(base::android::ApplicationState state);
