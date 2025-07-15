add9f12a7e5a4f1cedd7d89a3b27ffe70f0743d4
Jenna Himawan <jhimawan@google.com>
2025-07-15 09:22:34 -0700

    Fix icons for extension-injected context menu items
    - When icon is null, don't throw an error
    - Provide icons for submenu parent rows
    - Fix icon colors
    - Update render test
    
    Bug: 418807464
    Change-Id: Iae39e7328dd1a4eaaaa9ce85f9758b681c8db74d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6733434
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1486990}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuItemWithSubmenuViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuItemWithSubmenuViewBinder.java
@@ -5,17 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.contextmenu;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 import static org.chromium.ui.listmenu.ContextMenuSubmenuItemProperties.ON_HOVER;
 import static org.chromium.ui.listmenu.ContextMenuSubmenuItemProperties.TITLE;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.CLICK_LISTENER;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.ENABLED;
+import static org.chromium.ui.listmenu.ListMenuItemProperties.START_ICON_BITMAP;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.R;
 import org.chromium.ui.listmenu.ContextMenuSubmenuItemProperties;
+import org.chromium.ui.listmenu.ListMenuItemProperties;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -30,7 +39,17 @@ class ContextMenuItemWithSubmenuViewBinder {
         TextView textView = view.findViewById(R.id.menu_row_text);
         if (propertyKey == TITLE) {
             textView.setText(model.get(TITLE));
-        }  else if (propertyKey == ENABLED) {
+        } else if (propertyKey == START_ICON_BITMAP) {
+            ImageView icon = view.findViewById(org.chromium.ui.R.id.menu_item_icon);
+            Bitmap bitmap = model.get(ListMenuItemProperties.START_ICON_BITMAP);
+            if (bitmap == null) {
+                icon.setVisibility(GONE);
+            } else {
+                Drawable drawable = new BitmapDrawable(view.getResources(), bitmap);
+                icon.setImageDrawable(drawable);
+                icon.setVisibility(VISIBLE);
+            }
+        } else if (propertyKey == ENABLED) {
             textView.setEnabled(model.get(ENABLED));
         } else if (propertyKey == ON_HOVER) {
             // TODO(crbug.com/424580483): Implement flyout submenus.
--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuRenderTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.contextmenu;
 
+import static org.chromium.chrome.browser.contextmenu.ContextMenuCoordinator.createAdapter;
+
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -37,9 +39,10 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.components.embedder_support.contextmenu.ContextMenuSwitches;
+import org.chromium.ui.listmenu.ContextMenuSubmenuHeaderItemProperties;
+import org.chromium.ui.listmenu.ContextMenuSubmenuItemProperties;
 import org.chromium.ui.listmenu.ListItemType;
 import org.chromium.ui.listmenu.ListMenuItemProperties;
-import org.chromium.ui.modelutil.LayoutViewBuilder;
 import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.ModelListAdapter;
@@ -94,7 +97,7 @@ public class ContextMenuRenderTest {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     mListItems = new ModelList();
-                    mAdapter = new ModelListAdapter(mListItems);
+                    mAdapter = createAdapter(mListItems);
 
                     sActivity.setContentView(R.layout.context_menu_fullscreen_container);
                     mView = sActivity.findViewById(android.R.id.content);
@@ -102,23 +105,6 @@ public class ContextMenuRenderTest {
                     mFrame = mView.findViewById(R.id.context_menu_frame);
                     ContextMenuListView listView = mView.findViewById(R.id.context_menu_list_view);
                     listView.setAdapter(mAdapter);
-
-                    mAdapter.registerType(
-                            ListItemType.HEADER,
-                            new LayoutViewBuilder(R.layout.context_menu_header),
-                            ContextMenuHeaderViewBinder::bind);
-                    mAdapter.registerType(
-                            ListItemType.DIVIDER,
-                            new LayoutViewBuilder(R.layout.list_section_divider),
-                            (m, v, p) -> {});
-                    mAdapter.registerType(
-                            ListItemType.CONTEXT_MENU_ITEM,
-                            new LayoutViewBuilder(R.layout.context_menu_row),
-                            ContextMenuItemViewBinder::bind);
-                    mAdapter.registerType(
-                            ListItemType.CONTEXT_MENU_ITEM_WITH_ICON_BUTTON,
-                            new LayoutViewBuilder(R.layout.context_menu_row),
-                            ContextMenuItemViewBinder::bind);
                 });
     }
 
@@ -172,6 +158,18 @@ public class ContextMenuRenderTest {
         Bitmap testBitmap = drawableToBitmap(sActivity.getDrawable(R.drawable.lens_icon));
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
+                    // Submenu back header
+                    mListItems.add(
+                            new ListItem(
+                                    ListItemType.CONTEXT_MENU_SUBMENU_HEADER,
+                                    new PropertyModel.Builder(
+                                                    ContextMenuSubmenuHeaderItemProperties.ALL_KEYS)
+                                            .with(
+                                                    ContextMenuSubmenuHeaderItemProperties.TITLE,
+                                                    EXAMPLE_LABEL)
+                                            .with(ListMenuItemProperties.ENABLED, true)
+                                            .build()));
+                    // Command type items
                     mListItems.add(
                             new ListItem(
                                     ListItemType.CONTEXT_MENU_ITEM,
@@ -190,6 +188,30 @@ public class ContextMenuRenderTest {
                                             .with(
                                                     ListMenuItemProperties.START_ICON_BITMAP,
                                                     testBitmap)
+                                            .with(ListMenuItemProperties.ENABLED, false)
+                                            .build()));
+                    // Submenu parent items
+                    mListItems.add(
+                            new ListItem(
+                                    ListItemType.CONTEXT_MENU_ITEM_WITH_SUBMENU,
+                                    new PropertyModel.Builder(
+                                                    ContextMenuSubmenuItemProperties.ALL_KEYS)
+                                            .with(
+                                                    ContextMenuSubmenuItemProperties.TITLE,
+                                                    EXAMPLE_LABEL)
+                                            .with(
+                                                    ListMenuItemProperties.START_ICON_BITMAP,
+                                                    testBitmap)
+                                            .with(ListMenuItemProperties.ENABLED, true)
+                                            .build()));
+                    mListItems.add(
+                            new ListItem(
+                                    ListItemType.CONTEXT_MENU_ITEM_WITH_SUBMENU,
+                                    new PropertyModel.Builder(
+                                                    ContextMenuSubmenuItemProperties.ALL_KEYS)
+                                            .with(
+                                                    ContextMenuSubmenuItemProperties.TITLE,
+                                                    EXAMPLE_LABEL)
                                             .with(ListMenuItemProperties.ENABLED, false)
                                             .build()));
                 });
