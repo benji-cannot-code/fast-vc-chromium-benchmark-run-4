c8fe9af5b7423ab7eeb11e357015b58f441e78db
Jiacheng Guo <gjc@google.com>
2025-07-14 18:20:11 -0700

    Stop creating Android spare render when NTP finishes loading
    The CL adds a feature parameter to filter out non-web page loading
    finished event when creating spare renders on Android. Since the
    RenderProcessHost for NTP will be reused when the navigation happens,
    creating a spare renderer will not improve loading speed in this case.
    
    Bug: 350646322
    Change-Id: Iad4475956253a39cbf786039aee742b3c5bce0a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6726517
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1486680}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -12203,6 +12203,10 @@ void WebContentsImpl::WarmUpAndroidSpareRenderer() {
   if (GetBrowserContext()->ShutdownStarted()) {
     return;
   }
+  if (!GetLastCommittedURL().SchemeIsHTTPOrHTTPS() &&
+      features::kAndroidSpareRendererOnlyWarmupAfterWebPageLoaded.Get()) {
+    return;
+  }
   int renderer_timeout_seconds =
       features::kAndroidSpareRendererTimeoutSeconds.Get();
   if (renderer_timeout_seconds < 0) {
