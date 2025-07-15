f09268a1db0805e6d15d500d3dd3d836faaed361
Jihad Hanna <jihadghanna@google.com>
2025-07-15 09:58:39 -0700

    Remove safe_filled_fields from BAM::OnDidFillOrPreviewForm
    The only use case of `safe_filled_fields` in the BAM function is to pass
    it to AutofillHandler so that it has access to the latest field values.
    
    After having fixed crbug.com/40227496, there is no need anymore for
    passing FormFieldData objects along with AutofillField objects just to
    obtain the latest value, since AutofillField now contains this
    information.
    
    Bug: 40227071
    Change-Id: I84fce531b0a89d5fce0caff1aaf6dcf1cc6ee2aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734629
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487017}

--- chrome/browser/devtools/protocol/autofill_handler.h
+++ chrome/browser/devtools/protocol/autofill_handler.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/content/browser/content_autofill_driver_factory.h"
 #include "components/autofill/core/browser/foundations/autofill_manager.h"
-#include "components/autofill/core/common/form_field_data.h"
 #include "content/public/browser/web_contents.h"
 
 using protocol::String;
@@ -58,11 +57,11 @@ class AutofillHandler : public protocol::Autofill::Backend,
       autofill::AutofillManager& manager,
       autofill::AutofillManager::LifecycleState old_state,
       autofill::AutofillManager::LifecycleState new_state) override;
-  void OnFillOrPreviewDataModelForm(
+  void OnFillOrPreviewForm(
       autofill::AutofillManager& manager,
-      autofill::FormGlobalId form,
+      autofill::FormGlobalId form_id,
       autofill::mojom::ActionPersistence action_persistence,
-      base::span<const autofill::FormFieldData* const> filled_fields,
+      const base::flat_set<autofill::FieldGlobalId>& filled_field_ids,
       const autofill::FillingPayload& filling_payload) override;
 
   // ContentAutofillDriverFactory::Observer:
