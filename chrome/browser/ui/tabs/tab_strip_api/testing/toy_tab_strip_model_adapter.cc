aa7e6bd8bc59f4cd6959ec5585322fea970f2713
David Yeung <dayeung@chromium.org>
2025-07-15 09:18:59 -0700

    [TabStripApi] Move Tab into a TabCollection
    Add the capability to move a Tab into a TabCollection using the position
    struct's parent_id. This is a wrapper in the adapter layer of the
    TabStripService before using standard apis in TabStripModel.
    
    There are 3 TabCollections that support move.
    - Unpinned (default case)
    - Pinned
    - TabGroup
    TabStrip and SplitTab both do not allow tabs to be moved. There is a
    future TODO to add errors if that happens.
    
    Bug: 412709271
    Change-Id: I7c220e521b45a3a7a65915a40589d729f739d5e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6733295
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486984}

--- chrome/browser/ui/tabs/tab_strip_api/testing/toy_tab_strip_model_adapter.cc
+++ chrome/browser/ui/tabs/tab_strip_api/testing/toy_tab_strip_model_adapter.cc
@@ -40,7 +40,7 @@ void ToyTabStripModelAdapter::ActivateTab(size_t idx) {
 }
 
 void ToyTabStripModelAdapter::MoveTab(tabs::TabHandle handle,
-                                      Position position) {
+                                      const Position& position) {
   tab_strip_->MoveTab(handle, position.index());
 }
 
