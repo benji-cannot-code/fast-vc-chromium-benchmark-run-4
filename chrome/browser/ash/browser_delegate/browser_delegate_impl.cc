bedefdcaafe729a5aca10a6108a69401863f43a2
Georg Neis <neis@chromium.org>
2025-07-15 23:05:54 -0700

    ash: Add BrowserDelegate::MoveTab and use it in CoralDelegate
    This removes a direct access to TabStripModel.
    
    Bug: b:369688254
    Change-Id: Iece4d2cabd450f54e5e2c12603a4634bf5f13c6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6736466
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487409}

--- chrome/browser/ash/browser_delegate/browser_delegate_impl.cc
+++ chrome/browser/ash/browser_delegate/browser_delegate_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/tabs/tab_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/web_app_launch_utils.h"
 #include "components/tab_groups/tab_group_id.h"
@@ -73,6 +74,10 @@ void BrowserDelegateImpl::Show() {
   browser_->window()->Show();
 }
 
+void BrowserDelegateImpl::ShowInactive() {
+  browser_->window()->ShowInactive();
+}
+
 void BrowserDelegateImpl::Activate() {
   browser_->window()->Activate();
 }
@@ -124,6 +129,22 @@ void BrowserDelegateImpl::CreateTabGroup(
 void BrowserDelegateImpl::PinTab(size_t tab_index) {
   browser_->tab_strip_model()->SetTabPinned(static_cast<int>(tab_index),
                                             /*pinned=*/true);
+}
+
+void BrowserDelegateImpl::MoveTab(size_t tab_index,
+                                  BrowserDelegate& target_browser) {
+  TabStripModel* source_tab_strip = browser_->tab_strip_model();
+  TabStripModel* target_tab_strip =
+      static_cast<BrowserDelegateImpl&>(target_browser)
+          .browser_->tab_strip_model();
+
+  const bool was_pinned = source_tab_strip->IsTabPinned(tab_index);
+
+  std::unique_ptr<tabs::TabModel> detached_tab =
+      source_tab_strip->DetachTabAtForInsertion(tab_index);
+  target_tab_strip->InsertDetachedTabAt(
+      TabStripModel::kNoTab, std::move(detached_tab),
+      was_pinned ? AddTabTypes::ADD_PINNED : AddTabTypes::ADD_ACTIVE);
 }
 
 }  // namespace ash
