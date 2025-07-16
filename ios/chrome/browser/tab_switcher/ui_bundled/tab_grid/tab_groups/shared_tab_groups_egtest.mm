c760ef1afd728420f3380007e128dbe70ee8a8cb
Asami Doi <asamidoi@chromium.org>
2025-07-16 07:47:47 -0700

    [ios] Fix SharedTabGroupsTestCase on iOS26
    The "Delete browsing data" button may be hidden when the tools menu is
    opened depending on the size of a device and the iOS version. Use
    `tapToolsMenuAction:` to scroll down to find the button if necessary.
    
    Fixed: 429601495
    Change-Id: I5018f84b032790c9bf66e61c3baadc5ff7021f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6760436
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487630}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_egtest.mm
@@ -1098,11 +1098,9 @@ void WaitForFakeJoinFlowView() {
 
   // Open clear browsing data page.
   [ChromeEarlGreyUI openToolsMenu];
-  [[EarlGrey
-      selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabel(
-                                   l10n_util::GetNSString(
-                                       IDS_IOS_TOOLS_MENU_CLEAR_BROWSING_DATA))]
-      performAction:grey_tap()];
+  [ChromeEarlGreyUI
+      tapToolsMenuAction:grey_accessibilityLabel(l10n_util::GetNSString(
+                             IDS_IOS_TOOLS_MENU_CLEAR_BROWSING_DATA))];
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabel(
                                    l10n_util::GetNSString(
