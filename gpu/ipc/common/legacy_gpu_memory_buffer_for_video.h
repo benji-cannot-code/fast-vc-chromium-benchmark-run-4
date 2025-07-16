c45eb79067c2f30d038f37914d81a3886e50e888
Colin Blundell <blundell@chromium.org>
2025-07-15 23:57:50 -0700

    [//gpu] Remove dead include
    This class no longer references gfx::GpuMemoryBuffer.
    
    Bug: 404905709
    Change-Id: I38824358d24d248fc380a9ba8fe63ca054348f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732633
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487425}

--- gpu/ipc/common/legacy_gpu_memory_buffer_for_video.h
+++ gpu/ipc/common/legacy_gpu_memory_buffer_for_video.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/common/gpu_ipc_common_export.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/size.h"
-#include "ui/gfx/gpu_memory_buffer.h"
 #include "ui/gfx/gpu_memory_buffer_handle.h"
 
 namespace gfx {
