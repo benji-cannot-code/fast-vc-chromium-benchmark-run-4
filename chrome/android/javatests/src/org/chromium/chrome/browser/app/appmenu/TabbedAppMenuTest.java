f20969ee3d4fd081cb82d347b6df62403abefee4
Fiaz Muhammad <mfiaz@google.com>
2025-07-16 14:57:56 -0700

    Disable flaky tests in TabbedAppMenuTest
    Disabled flaky tests, testSettingsMenuItem_BadgeShownForSignedInUsersOnIdentityError and testSettingsMenuItem_NoBadgeShownForSignedInUsersIfNoError.
    
    Bug: 432304126
    Change-Id: If4db40915f528d6dbd828b443c6842b66588afb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6760797
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Owners-Override: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1487912}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuTest.java
@@ -38,6 +38,7 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -318,6 +319,7 @@ public class TabbedAppMenuTest {
     @Test
     @LargeTest
     @Feature({"Browser", "Main", "RenderTest"})
+    @DisabledTest(message = "crbug.com/432304126")
     public void testSettingsMenuItem_BadgeShownForSignedInUsersOnIdentityError()
             throws IOException {
         ThreadUtils.runOnUiThreadBlocking(() -> mAppMenuHandler.hideAppMenu());
@@ -346,6 +348,7 @@ public class TabbedAppMenuTest {
     @Test
     @LargeTest
     @Feature({"Browser", "Main", "RenderTest"})
+    @DisabledTest(message = "crbug.com/432304126")
     public void testSettingsMenuItem_NoBadgeShownForSignedInUsersIfNoError() throws IOException {
         ThreadUtils.runOnUiThreadBlocking(() -> mAppMenuHandler.hideAppMenu());
         // Sign in and wait for sync machinery to be active.
