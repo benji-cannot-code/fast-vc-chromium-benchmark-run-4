28b8438767c07c42da40f14d92b0c0e81746f27c
Devon Loehr <dloehr@google.com>
2025-07-16 10:37:42 -0700

    Directly include <vector> in scheduler.h
    The newest version libcxx no longer includes this header transitively,
    so we need to include it directly instead.
    
    Bug: 431755915
    Change-Id: I1330bb8c0f649fa88920c135bc2306abfb573454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6761592
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1487762}

--- ios/chrome/browser/download/model/auto_deletion/scheduler.h
+++ ios/chrome/browser/download/model/auto_deletion/scheduler.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_DOWNLOAD_MODEL_AUTO_DELETION_SCHEDULER_H_
 #define IOS_CHROME_BROWSER_DOWNLOAD_MODEL_AUTO_DELETION_SCHEDULER_H_
 
+#include <vector>
+
 #import "base/memory/raw_ptr.h"
 
 namespace auto_deletion {
