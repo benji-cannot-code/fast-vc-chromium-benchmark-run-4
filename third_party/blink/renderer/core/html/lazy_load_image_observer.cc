07bc50a5076e8b38ec08b69b2bbe00f9c2c2e8e0
Philip Rogers <pdr@chromium.org>
2025-07-15 17:27:48 -0700

    Cleanup LazyLoadScrollMargin flags
    These shipped a long time ago.
    
    Change-Id: I93ae71bf75e533700e0f1f8f48ffe82ebb8efa96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6757898
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487317}

--- third_party/blink/renderer/core/html/lazy_load_image_observer.cc
+++ third_party/blink/renderer/core/html/lazy_load_image_observer.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/network/network_state_notifier.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -64,14 +63,10 @@ void LazyLoadImageObserver::StartMonitoringNearViewport(Document* root_document,
   if (!lazy_load_intersection_observer_) {
     int margin = GetLazyLoadingImageMarginPx(*root_document);
     IntersectionObserver::Params params = {
+        .scroll_margin = {{/* top & bottom */ Length::Fixed(margin),
+                           /* right & left */ Length::Fixed(margin / 2)}},
         .thresholds = {std::numeric_limits<float>::min()},
     };
-    if (RuntimeEnabledFeatures::LazyLoadScrollMarginEnabled()) {
-      params.scroll_margin = {{/* top & bottom */ Length::Fixed(margin),
-                               /* right & left */ Length::Fixed(margin / 2)}};
-    } else {
-      params.margin = {Length::Fixed(margin)};
-    }
     lazy_load_intersection_observer_ = IntersectionObserver::Create(
         *root_document,
         WTF::BindRepeating(&LazyLoadImageObserver::LoadIfNearViewport,
