9309f885a794cb767149752425092fed4da5e879
Baran Erfani <baranerf@google.com>
2025-07-16 11:23:58 -0700

    Added a UUID to the feedback description
    Feedbacks submitted from the same user, with identical descriptions, and
    in a short window of time might get rejected (deduplicated). This change
    will ensure the description is different for all thumbs down feedbacks.
    
    Bug: 431791541
    Change-Id: I0358ab1748b815e163f9667d659f678ab7e92fb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732939
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487799}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "base/uuid.h"
 #include "base/version_info/version_info.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/aggregated_journal.h"
@@ -75,6 +76,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/mojom/geometry.mojom.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/widget/widget.h"
+
+namespace FeedbackConstants {
+const char kThumbsDownFeedbackPrefix[] = "Response feedback thumbs down - ";
+}
 
 namespace mojo {
 
@@ -1309,8 +1314,10 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
             std::move(uploader), ContentTracingManager::Get());
     auto journal = journal_handler_.GetSnapshot(false);
 
-    // TODO(430054430): Fetch and include system data to the feedback.
-    feedback_data->set_description("Response feedback thumbs down.");
+    // TODO(b/430054430): Fetch and include system data to the feedback.
+    feedback_data->set_description(
+        FeedbackConstants::kThumbsDownFeedbackPrefix +
+        base::Uuid::GenerateRandomV4().AsLowercaseString());
     feedback_data->set_product_id(feedback::kGeminiWebProductId);
     feedback_data->set_category_tag(
         std::string(feedback::kGeminiWebJournalCategoryTag));
