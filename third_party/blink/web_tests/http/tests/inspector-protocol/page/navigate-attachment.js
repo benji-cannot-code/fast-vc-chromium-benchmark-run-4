7206a7b98baa6c4216713ce825765c06a1c21ac1
Simon Knott <simon.knott@microsoft.com>
2025-07-16 15:20:51 -0700

    [CDP] Detect `Page.navigate` download
    Before this CL, navigating to a download URL yields "net::ERR_ABORTED",
    and is impossible to distinguish from any other error.
    To determine whether the error stems from a download,
    one needs to wait for the `Browser.downloadWillBegin` event.
    But since that can be emitted *after* `Page.navigate` returns,
    this introduces unnecessary waiting.
    
    To prevent this waiting, this CL adds a flag that
    shows whether the navigation turned into a download.
    
    Change-Id: I90d81d289df6621d0cdb977ba6ed7e7d74ee475e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696011
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487924}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/navigate-attachment.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests that Page.navigate returns isDownload when server responds with Content-Disposition: attachment.`);
+
+  await dp.Page.enable();
+  const response = await dp.Page.navigate({ url: testRunner.url('./resources/httpAttachment.php')});
+  testRunner.log(response);
+  testRunner.completeTest();
+})
