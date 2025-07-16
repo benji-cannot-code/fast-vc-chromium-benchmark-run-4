1fa2bd6a5ed4008b6a81e7a13b7dda8261cc54e8
Daniel Murphy <dmurph@chromium.org>
2025-07-16 08:53:56 -0700

    [ProtoExtras] Add optional support for proto-based serialization
    Adds a helper method MaybeSerialize() to handle the case when a field of
    type optional<ProtoMessage> is being serialized to a base::DictValue.
    
    This simplifies the calling sites by not having to do a check on the
    optionality of the member prior to setting it in the dictionary.
    
    Change-Id: I410570d82e72e520a8fa24e81dcd19c259092f38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6756617
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487677}

--- components/proto_extras/proto_extras_lib.h
+++ components/proto_extras/proto_extras_lib.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/values.h"
 
 namespace google::protobuf {
 class MessageLite;
