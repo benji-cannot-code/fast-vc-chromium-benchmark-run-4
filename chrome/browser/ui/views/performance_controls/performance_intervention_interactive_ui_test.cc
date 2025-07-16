1b22044feabb6331e375f059378d63c1a1e164c0
Steven Luong <stluong@chromium.org>
2025-07-16 15:38:56 -0700

    [Fixit] Deflake Performance PerformanceInterventionInteractiveTests
    The PerformanceInterventionTest.TakeSuggestedAction and
    RemoveSuggestedTabFromList were flaking because a tab will sometimes
    fail to discard. Since these tests are for testing the performance
    intervention UI and not discard conditions, this CL forces discard
    to succeed for tests which should deflake the tests.
    
    Fixed: 416350381
    Change-Id: I1a7e1bb61c69d4cac293f4ef5ce3bdc80e56b7f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618906
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487934}

--- chrome/browser/ui/views/performance_controls/performance_intervention_interactive_ui_test.cc
+++ chrome/browser/ui/views/performance_controls/performance_intervention_interactive_ui_test.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/performance_manager/public/user_tuning/performance_detection_manager.h"
+#include "chrome/browser/performance_manager/test_support/page_discarding_utils.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_test_util.h"
 #include "chrome/browser/resource_coordinator/lifecycle_unit.h"
@@ -59,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 
 namespace {
+using ::performance_manager::testing::ScopedSetAllPagesDiscardableForTesting;
 
 class DiscardObserver : public resource_coordinator::LifecycleUnitObserver,
                         public ui::test::StateObserver<bool> {
@@ -119,6 +121,13 @@ class PerformanceInterventionInteractiveTest
     InteractiveFeaturePromoTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
     ASSERT_TRUE(embedded_test_server()->Start());
+    unconditionally_discard_pages_ =
+        std::make_unique<ScopedSetAllPagesDiscardableForTesting>();
+  }
+
+  void TearDownOnMainThread() override {
+    unconditionally_discard_pages_.reset();
+    InteractiveFeaturePromoTest::TearDownOnMainThread();
   }
 
   Profile* CreateTestProfile() {
@@ -205,6 +214,10 @@ class PerformanceInterventionInteractiveTest
                                enabled);
     });
   }
+
+ private:
+  std::unique_ptr<ScopedSetAllPagesDiscardableForTesting>
+      unconditionally_discard_pages_;
 };
 
 IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
@@ -440,14 +453,8 @@ IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
 }
 
 // The dialog should discard tabs suggested in the tab list
-// TODO(crbug.com/416350381): Re-enable this test
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_TakeSuggestedAction DISABLED_TakeSuggestedAction
-#else
-#define MAYBE_TakeSuggestedAction TakeSuggestedAction
-#endif
 IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
-                       MAYBE_TakeSuggestedAction) {
+                       TakeSuggestedAction) {
   RunTestSequence(
       InstrumentTab(kFirstTab), NavigateWebContents(kFirstTab, GetURL()),
       AddInstrumentedTab(kSecondTab, GetURL()),
@@ -464,14 +471,8 @@ IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
 }
 
 // The dialog should discard tabs suggested in the tab list
-// TODO(crbug.com/416350381): Re-enable this test
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_RemoveSuggestedTabFromList DISABLED_RemoveSuggestedTabFromList
-#else
-#define MAYBE_RemoveSuggestedTabFromList RemoveSuggestedTabFromList
-#endif
 IN_PROC_BROWSER_TEST_F(PerformanceInterventionInteractiveTest,
-                       MAYBE_RemoveSuggestedTabFromList) {
+                       RemoveSuggestedTabFromList) {
   const char kTabListRow[] = "TabListRow";
   const char kSuggestedCloseButton[] = "SuggestedCloseButton";
 
