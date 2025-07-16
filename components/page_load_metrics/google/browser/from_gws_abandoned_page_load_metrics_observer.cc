5a67468dd15716068a3d877f4bb304580164688d
Keita Suzuki <suzukikeita@chromium.org>
2025-07-16 00:40:44 -0700

    Update handling of extended error code for abandoned plmo
    This CL updates the extended error code handling in the
    FromGWSAbandonedPageLoadMetricsObserver to record the field separately
    from the net error codes.
    
    Bug: 390216631
    Change-Id: Ifb3af3abe598b43a951aeb4d541b38c5c6e89bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6704389
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487451}

--- components/page_load_metrics/google/browser/from_gws_abandoned_page_load_metrics_observer.cc
+++ components/page_load_metrics/google/browser/from_gws_abandoned_page_load_metrics_observer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/named_trigger.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
 #include "components/page_load_metrics/google/browser/google_url_util.h"
+#include "content/public/browser/error_navigation_trigger.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "services/metrics/public/cpp/metrics_utils.h"
@@ -60,9 +61,13 @@ FromGWSAbandonedPageLoadMetricsObserver::OnNavigationHandleTimingUpdated(
 
   if (navigation_handle->GetNetErrorCode() < 0) {
     CHECK(!net_error_.has_value());
-    CHECK(!net_extended_error_code_.has_value());
     net_error_ = navigation_handle->GetNetErrorCode();
-    net_extended_error_code_ = navigation_handle->GetNetExtendedErrorCode();
+  }
+
+  if (navigation_handle->GetErrorNavigationTrigger().has_value()) {
+    CHECK(!error_navigation_trigger_.has_value());
+    error_navigation_trigger_ =
+        navigation_handle->GetErrorNavigationTrigger().value();
   }
 
   // Set the request / response time of the second redirect by checking:
@@ -125,9 +130,14 @@ void FromGWSAbandonedPageLoadMetricsObserver::OnFailedProvisionalLoad(
   // through `OnNavigationHandleTimingUpdated`.
   if (!net_error_.has_value()) {
     net_error_ = failed_provisional_load_info.error;
-    net_extended_error_code_ =
-        failed_provisional_load_info.net_extended_error_code;
   }
+
+  if (failed_provisional_load_info.error_navigation_trigger.has_value()) {
+    CHECK(!error_navigation_trigger_.has_value());
+    error_navigation_trigger_ =
+        failed_provisional_load_info.error_navigation_trigger.value();
+  }
+
   AbandonedPageLoadMetricsObserver::OnFailedProvisionalLoad(
       failed_provisional_load_info);
 }
@@ -218,11 +228,13 @@ void FromGWSAbandonedPageLoadMetricsObserver::LogUKMHistograms(
   }
 
   if (net_error_.has_value()) {
-    CHECK(net_extended_error_code_.has_value());
     builder.SetNet_ErrorCode(
         std::abs(static_cast<int64_t>(net_error_.value())));
+  }
+
+  if (error_navigation_trigger_.has_value()) {
     builder.SetNet_ExtendedErrorCode(
-        std::abs(net_extended_error_code_.value()));
+        std::abs(static_cast<int64_t>(error_navigation_trigger_.value())));
   }
 
   LogUKMHistogramsForAbandonMetrics(builder, abandon_reason, milestone,
