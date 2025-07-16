a6b7ede97cd049aa1f87a5cc81c9c2fef6102315
Lukasz Anforowicz <lukasza@chromium.org>
2025-07-16 12:16:21 -0700

    [rust] Make `-Amismatched_lifetime_syntaxes` suppression less global.
    This CL moves the `-Amismatched_lifetime_syntaxes` suppression into the
    currently affected 3 targets.  This allows removing the global
    suppression and "holding the line" for the other Rust targets.
    
    Fixed: 424424323
    Change-Id: I1c94010a1699569b8567a293ed46b7240ec55f15
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700277
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487828}

--- mojo/public/rust/system/data_pipe.rs
+++ mojo/public/rust/system/data_pipe.rs
@@ -300,7 +300,7 @@ impl<T> Consumer<T> {
 
     /// Begin two-phase read. Returns a ReadDataBuffer to perform read and
     /// commit.
-    pub fn begin(&self) -> Result<ReadDataBuffer<T>, MojoResult> {
+    pub fn begin(&self) -> Result<ReadDataBuffer<'_, '_, T>, MojoResult> {
         let mut buffer_num_bytes: u32 = 0;
         let mut buffer_ptr: *const ffi::c_void = ptr::null();
         let r = MojoResult::from_code(unsafe {
@@ -373,7 +373,7 @@ impl<T> Producer<T> {
 
     /// Begin two-phase write. Returns a WriteDataBuffer to perform write and
     /// commit.
-    pub fn begin(&self) -> Result<WriteDataBuffer<T>, MojoResult> {
+    pub fn begin(&self) -> Result<WriteDataBuffer<'_, '_, T>, MojoResult> {
         let mut buffer_num_bytes: u32 = 0;
         let mut buffer_ptr: *mut ffi::c_void = ptr::null_mut();
         let r = MojoResult::from_code(unsafe {
