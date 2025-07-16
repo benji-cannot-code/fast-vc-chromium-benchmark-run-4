c7b552203e8afae682dcb2dbd69cce3e8d70609e
Ioana Pandele <ioanap@chromium.org>
2025-07-16 03:14:17 -0700

    Implement MVP Actor Login API
    The current implementation always returns an empty list for
    getCredentials and `false` for attempt login.
    
    Skipping test flakiness check due to TSAN bot issues.
    
    Validate-Test-Flakiness: skip
    Bug: 427170123
    Change-Id: I0b1e4a55a95713d9d9baae209627593c65ee08a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718394
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487509}

---
+++ chrome/browser/password_manager/actor_login/actor_login_service_impl.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PASSWORD_MANAGER_ACTOR_LOGIN_ACTOR_LOGIN_SERVICE_IMPL_H_
+#define CHROME_BROWSER_PASSWORD_MANAGER_ACTOR_LOGIN_ACTOR_LOGIN_SERVICE_IMPL_H_
+
+#include "chrome/browser/password_manager/actor_login/actor_login_service.h"
+
+namespace actor_login {
+
+class ActorLoginServiceImpl : public ActorLoginService {
+ public:
+  ActorLoginServiceImpl();
+  ~ActorLoginServiceImpl() override;
+
+  ActorLoginServiceImpl(const ActorLoginServiceImpl&) = delete;
+  ActorLoginServiceImpl& operator=(const ActorLoginServiceImpl&) = delete;
+
+  // `ActorLoginService` implementation:
+  void GetCredentials(tabs::TabInterface* tab,
+                      CredentialsOrErrorReply callback) override;
+  void AttemptLogin(tabs::TabInterface* tab,
+                    const Credential& credential,
+                    LoginStatusResultOrErrorReply callback) override;
+};
+
+}  // namespace actor_login
+
+#endif  // CHROME_BROWSER_PASSWORD_MANAGER_ACTOR_LOGIN_ACTOR_LOGIN_SERVICE_IMPL_H_
