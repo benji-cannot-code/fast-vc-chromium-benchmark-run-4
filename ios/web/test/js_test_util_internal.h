13750e60f2275ac87d060ffa9c4f37bad9758707
Mike Dougherty <michaeldo@chromium.org>
2025-07-16 16:47:29 -0700

    Add JavaScript helper functions for side effects
    These helpers don't return a result which allows for optimizations
    around the kAssertOnJavaScriptErrors flag.
    
    Bug: None
    Change-Id: I53b391e5265aac72cc9017069c35281c73ce7083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6720053
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487957}

--- ios/web/test/js_test_util_internal.h
+++ ios/web/test/js_test_util_internal.h
@@ -12,10 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web {
 namespace test {
 
-// Synchronously executes `script` in `content_world` and returns result.
-// NOTE: Generally, tests should not deal with raw WKContentWorlds. Instead,
-// prefer specifying the associated JavaScriptFeature instance using
+// NOTE: The following accept a pointer to a `WKContentWorld`. However, tests
+// should generally not deal with raw WKContentWorlds. Instead, prefer
+// specifying the associated JavaScriptFeature instance using
 // WebTestWithWebState::ExecuteJavaScriptForFeature.
+
+// Synchronously executes `script` in `content_world` and waits for the
+// execution to complete.
+void ExecuteJavaScriptInWebViewAndWorld(WKWebView* web_view,
+                                        WKContentWorld* content_world,
+                                        NSString* script);
+
+// Synchronously executes `script` in `content_world` and returns result.
 id ExecuteJavaScript(WKWebView* web_view,
                      WKContentWorld* content_world,
                      NSString* script);
--- ios/web/test/js_test_util_internal.mm
+++ ios/web/test/js_test_util_internal.mm
@@ -7,8 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebKit/WebKit.h>
 
+#import "base/check.h"
+#import "base/feature_list.h"
+#import "base/logging.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
+#import "ios/web/common/features.h"
 #import "testing/gtest/include/gtest/gtest.h"
 
 using base::test::ios::kWaitForJSCompletionTimeout;
@@ -17,10 +21,13 @@ using base::test::ios::WaitUntilConditionOrTimeout;
 namespace web {
 namespace test {
 
-id ExecuteJavaScript(WKWebView* web_view,
-                     WKContentWorld* content_world,
-                     NSString* script) {
-  __block id result;
+namespace {
+
+void ExecuteJavaScript(WKWebView* web_view,
+                       WKContentWorld* content_world,
+                       NSString* script,
+                       id __autoreleasing* result) {
+  __block id block_result;
   __block bool completed = false;
   __block NSError* block_error = nil;
   SCOPED_TRACE(base::SysNSStringToUTF8(script));
@@ -28,19 +35,61 @@ id ExecuteJavaScript(WKWebView* web_view,
                        inFrame:nil
                 inContentWorld:content_world
              completionHandler:^(id script_result, NSError* script_error) {
-               result = [script_result copy];
+               block_result = [script_result copy];
                block_error = [script_error copy];
                completed = true;
              }];
   BOOL success = WaitUntilConditionOrTimeout(kWaitForJSCompletionTimeout, ^{
     return completed;
   });
+
   // Log stack trace to provide some context.
   EXPECT_TRUE(success)
       << base::SysNSStringToUTF8(block_error.description)
       << "\nWKWebView failed to complete javascript execution.\n"
       << base::SysNSStringToUTF8(
              [[NSThread callStackSymbols] componentsJoinedByString:@"\n"]);
+
+  if (block_error) {
+    DLOG(WARNING) << "\nWKWebView javascript execution failed.\n"
+                  << base::SysNSStringToUTF8(block_error.description);
+
+    BOOL unsupportedResultError =
+        [block_error.domain isEqualToString:WKErrorDomain] &&
+        block_error.code == WKErrorJavaScriptResultTypeIsUnsupported;
+    if (  // Caller cares about the result but it was of an unsupported type.
+        (result && unsupportedResultError) ||
+        // Error is a real failure
+        !unsupportedResultError) {
+      DLOG(WARNING) << "\nWKWebView javascript execution failed.\n"
+                    << base::SysNSStringToUTF8(block_error.description);
+
+      if (base::FeatureList::IsEnabled(
+              web::features::kAssertOnJavaScriptErrors)) {
+        CHECK(false) << "JavaScript error occurred with "
+                        "kAssertOnJavaScriptErrors enabled.";
+      }
+    }
+  }
+
+  if (result) {
+    *result = block_result;
+  }
+}
+
+}  // namespace
+
+void ExecuteJavaScriptInWebViewAndWorld(WKWebView* web_view,
+                                        WKContentWorld* content_world,
+                                        NSString* script) {
+  ExecuteJavaScript(web_view, content_world, script, nil);
+}
+
+id ExecuteJavaScript(WKWebView* web_view,
+                     WKContentWorld* content_world,
+                     NSString* script) {
+  id result;
+  ExecuteJavaScript(web_view, content_world, script, &result);
   return result;
 }
 
