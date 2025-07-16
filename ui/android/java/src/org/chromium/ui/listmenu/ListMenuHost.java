73fe19c0e80ccf7864a2826aa02757cc6a58ff09
Henrique Nakashima <hnakashima@chromium.org>
2025-07-16 09:55:11 -0700

    [Android] Rewrite testOpenMostVisitedItemInIncognitoTab
    This test was removed from NewTabPageTest as it didn't work with
    TileContextMenuRefactor enabled. Rewrite it in Public Transit adding
    initial support for context menus.
    
    Also add testOpenItemInNewTab.
    
    Bug: 421250783,420700079
    Change-Id: I79e2443386f97919b08df2440d50f94485218cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6731352
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487721}

--- ui/android/java/src/org/chromium/ui/listmenu/ListMenuHost.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListMenuHost.java
@@ -16,6 +16,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.ObserverList;
 import org.chromium.base.ResettersForTesting;
 import org.chromium.build.annotations.EnsuresNonNull;
@@ -38,8 +40,9 @@ public class ListMenuHost implements AnchoredPopupWindow.LayoutObserver {
         default void onPopupMenuDismissed() {}
     }
 
+    @VisibleForTesting
     @FunctionalInterface
-    interface PopupMenuHelper {
+    public interface PopupMenuHelper {
         /**
          * Called when the popup menu is requested to be shown or dismissed, then inject a test
          * value.
@@ -248,7 +251,7 @@ public class ListMenuHost implements AnchoredPopupWindow.LayoutObserver {
         }
     }
 
-    static void setMenuChangedListenerForTesting(PopupMenuHelper listener) {
+    public static void setMenuChangedListenerForTesting(PopupMenuHelper listener) {
         sPopupMenuHelperForTesting = listener;
         ResettersForTesting.register(() -> sPopupMenuHelperForTesting = null);
     }
