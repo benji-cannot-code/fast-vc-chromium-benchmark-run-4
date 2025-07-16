21480968785fed5dee119de301aa18ff22c7b939
Kristi Saney <kristislee@google.com>
2025-07-16 13:20:27 -0700

    [Reading mode] Add missing closing tag for the rate menu
    Change-Id: I2b26c9bc0c5f94de15b7ab1f7f6ec84169835f21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6762174
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1487860}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.html.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.html.ts
@@ -168,6 +168,7 @@ export function getHtml(this: ReadAnythingToolbarElement) {
       id="rateMenu"
       .settingsPrefs="${this.settingsPrefs}"
       @rate-change="${this.onRateChange_}">
+  </rate-menu>
   <highlight-menu
       id="highlightMenu"
       .settingsPrefs="${this.settingsPrefs}"
