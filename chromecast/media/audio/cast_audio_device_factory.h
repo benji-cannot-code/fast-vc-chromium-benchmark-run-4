201bfe8a96c380eb8375f3b8b8e1d2124b7f0560
Sandeep Vijayasekar <sandv@google.com>
2025-07-16 15:41:13 -0700

    [Cast] Pass SupportedBitstreamAudioCodecs to renderer
    Bug: Internal 428791750
    Test: Test EAC-3 content in HDMI passthrough mode.
    
    Change-Id: I346e2d4dbaf29a386c339c7f7fb282040cd4312f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716298
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Auto-Submit: Sandeep Vijayasekar <sandv@google.com>
    Commit-Queue: Sandeep Vijayasekar <sandv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1487936}

--- chromecast/media/audio/cast_audio_device_factory.h
+++ chromecast/media/audio/cast_audio_device_factory.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
+#include "chromecast/base/bitstream_audio_codecs.h"
 #include "media/audio/audio_sink_parameters.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/platform/audio/web_audio_device_source_type.h"
@@ -30,6 +31,9 @@ class CastAudioDeviceFactory final : public blink::AudioDeviceFactory {
       const blink::LocalFrameToken& frame_token,
       const blink::FrameToken& main_frame_token,
       const ::media::AudioSinkParameters& params) override;
+
+  void SetSupportedBitstreamAudioCodec(BitstreamAudioCodecsInfo);
+  BitstreamAudioCodecsInfo supported_bitstream_audio_codecs_info_;
 };
 
 }  // namespace media
