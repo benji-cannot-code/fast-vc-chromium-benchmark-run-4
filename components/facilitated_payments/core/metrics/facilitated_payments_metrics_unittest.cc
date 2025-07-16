7683854170fcd2a0958604dbfbb528ac9ec01b96
Junhui He <junhuihe@google.com>
2025-07-16 15:54:03 -0700

    Add momo as a supported payment link scheme
    Updated payment link spec:
    https://github.com/WICG/paymentlink/pull/18
    
    Updated public design (Security section):
    https://bit.ly/html-payment-link-dd
    
    Fixed: 432091020
    Change-Id: Ieeb3b02a72cd6c22ae7473df729abd61ab0127ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6758256
    Commit-Queue: Junhui He <junhuihe@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487942}

--- components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
+++ components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
@@ -40,6 +40,8 @@ std::string GetSchemeString(PaymentLinkValidator::Scheme scheme) {
     case PaymentLinkValidator::Scheme::kPromptPay:
       // TODO(crbug.com/427319124): Add tests for kPromptPay when adding metrics.
       NOTREACHED();
+    case PaymentLinkValidator::Scheme::kMomo:
+      return "Momo";
     case PaymentLinkValidator::Scheme::kInvalid:
       NOTREACHED();
   }
@@ -323,7 +325,8 @@ INSTANTIATE_TEST_SUITE_P(
                         EwalletFlowExitedReason::kMaxStrikes),
         testing::Values(PaymentLinkValidator::Scheme::kDuitNow,
                         PaymentLinkValidator::Scheme::kShopeePay,
-                        PaymentLinkValidator::Scheme::kTngd)));
+                        PaymentLinkValidator::Scheme::kTngd,
+                        PaymentLinkValidator::Scheme::kMomo)));
 
 class FacilitatedPaymentsMetricsPixExitedReasonTest
     : public testing::TestWithParam<PixFlowExitedReason> {};
@@ -393,7 +396,8 @@ TEST_F(FacilitatedPaymentsMetricsUkmTest, LogEwalletFopSelectorShownUkm) {
   for (PaymentLinkValidator::Scheme scheme :
        {PaymentLinkValidator::Scheme::kDuitNow,
         PaymentLinkValidator::Scheme::kShopeePay,
-        PaymentLinkValidator::Scheme::kTngd}) {
+        PaymentLinkValidator::Scheme::kTngd,
+        PaymentLinkValidator::Scheme::kMomo}) {
     LogEwalletFopSelectorShownUkm(ukm::UkmRecorder::GetNewSourceID(), scheme);
 
     auto ukm_entries = ukm_recorder_.GetEntries(
@@ -439,7 +443,8 @@ TEST_F(FacilitatedPaymentsMetricsUkmTest, LogEwalletFopSelectorResult) {
     for (PaymentLinkValidator::Scheme scheme :
          {PaymentLinkValidator::Scheme::kDuitNow,
           PaymentLinkValidator::Scheme::kShopeePay,
-          PaymentLinkValidator::Scheme::kTngd}) {
+          PaymentLinkValidator::Scheme::kTngd,
+          PaymentLinkValidator::Scheme::kMomo}) {
       LogEwalletFopSelectorResultUkm(
           accepted, ukm::UkmRecorder::GetNewSourceID(), scheme);
 
@@ -515,7 +520,8 @@ INSTANTIATE_TEST_SUITE_P(
                                      FacilitatedPaymentsType::kPix),
                      testing::Values(PaymentLinkValidator::Scheme::kDuitNow,
                                      PaymentLinkValidator::Scheme::kShopeePay,
-                                     PaymentLinkValidator::Scheme::kTngd)));
+                                     PaymentLinkValidator::Scheme::kTngd,
+                                     PaymentLinkValidator::Scheme::kMomo)));
 
 TEST_P(FacilitatedPaymentsMetricsParameterizedTest,
        LogApiAvailabilityCheckResultAndLatency_Success) {
@@ -794,7 +800,8 @@ INSTANTIATE_TEST_SUITE_P(
                                      FacilitatedPaymentsType::kPix),
                      testing::Values(PaymentLinkValidator::Scheme::kDuitNow,
                                      PaymentLinkValidator::Scheme::kShopeePay,
-                                     PaymentLinkValidator::Scheme::kTngd),
+                                     PaymentLinkValidator::Scheme::kTngd,
+                                     PaymentLinkValidator::Scheme::kMomo),
                      testing::Values(UiState::kFopSelector,
                                      UiState::kProgressScreen,
                                      UiState::kErrorScreen)));
