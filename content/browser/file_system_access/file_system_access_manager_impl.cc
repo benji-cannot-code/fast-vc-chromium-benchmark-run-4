00f80c6df88148d35b411e0c65bbebb4f99c23eb
Ming-Ying Chung <mych@chromium.org>
2025-07-16 02:36:07 -0700

    [FSA] Replace `request_writable` with a new enum `FileSystemAccessPermissionMode`.
    The mojom bool field `DirectoryPickerOptions::request_writable`
    currently represents read & readwrite modes, directly mapped from the
    idl [`V8FileSystemPermissionMode`][1]. This CL replaces the bool with
    an enum such that more than 2 modes can be added in the future.
    
    No functional change.
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/file_system_access/directory_picker_options.idl;l=9;drc=047c7dc4ee1ce908d7fea38ca063fa2f80f92c77
    
    Bug: 40276567
    Change-Id: I7a8621baa9e18e9233c7164c9510862fc308c3a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6735979
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487497}

--- content/browser/file_system_access/file_system_access_manager_impl.cc
+++ content/browser/file_system_access/file_system_access_manager_impl.cc
@@ -542,8 +542,10 @@ void FileSystemAccessManagerImpl::ChooseEntries(
             context.storage_key.origin()) ||
         ((options->type_specific_options->is_save_file_picker_options() ||
           (options->type_specific_options->is_directory_picker_options() &&
+           // TODO(crbug.com/40276567): Support kWrite.
            options->type_specific_options->get_directory_picker_options()
-               ->request_writable)) &&
+                   ->permission_mode ==
+               blink::mojom::FileSystemAccessPermissionMode::kReadWrite)) &&
          !permission_context_->CanObtainWritePermission(
              context.storage_key.origin()))) {
       std::move(callback).Run(
@@ -703,10 +705,12 @@ void FileSystemAccessManagerImpl::SetDefaultPathAndShowPicker(
         context.storage_key.origin());
   }
 
-  auto request_directory_write_access =
+  // TODO(crbug.com/40276567): Support kWrite.
+  bool request_directory_write_access =
       options->type_specific_options->is_directory_picker_options() &&
       options->type_specific_options->get_directory_picker_options()
-          ->request_writable;
+              ->permission_mode ==
+          blink::mojom::FileSystemAccessPermissionMode::kReadWrite;
 
   auto suggested_name =
       options->type_specific_options->is_save_file_picker_options()
@@ -1548,7 +1552,7 @@ void FileSystemAccessManagerImpl::DidChooseEntries(
     const BindingContext& binding_context,
     const FileSystemChooser::Options& options,
     const std::string& starting_directory_id,
-    const bool request_directory_write_access,
+    bool request_directory_write_access,
     ChooseEntriesCallback callback,
     blink::mojom::FileSystemAccessErrorPtr result,
     std::vector<PathInfo> entries) {
@@ -1593,7 +1597,7 @@ void FileSystemAccessManagerImpl::DidVerifySensitiveDirectoryAccess(
     const BindingContext& binding_context,
     const FileSystemChooser::Options& options,
     const std::string& starting_directory_id,
-    const bool request_directory_write_access,
+    bool request_directory_write_access,
     ChooseEntriesCallback callback,
     std::vector<PathInfo> entries,
     SensitiveEntryResult result) {
