ab58926baf0b70ef1103828af6ebb76db4114e0c
Zhenyao Mo <zmo@chromium.org>
2025-07-15 19:11:25 -0700

    Record a few variables to understand why RWH_INVALID_FRAME_TOKEN.
    TEST=bots
    
    Bug: 431761865,431671391
    Change-Id: I163fdd4ae683af1260f8edfa88730960e12254db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6757973
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487374}

--- content/browser/renderer_host/frame_token_message_queue.cc
+++ content/browser/renderer_host/frame_token_message_queue.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/frame_token_message_queue.h"
 
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
+#include "base/strings/string_number_conversions.h"
 
 namespace content {
 
@@ -36,6 +39,16 @@ void FrameTokenMessageQueue::DidProcessFrame(uint32_t frame_token,
   if ((frame_token <= last_received_frame_token_) &&
       !(last_received_frame_token_reset_ &&
         last_received_frame_token_reset_ != frame_token)) {
+    // TODO(crbug.com/431761865): Remove after the bug is fixed.
+    SCOPED_CRASH_KEY_STRING32("content", "Frame token",
+                              base::NumberToString(frame_token));
+    SCOPED_CRASH_KEY_STRING32("content", "Frame token (last)",
+                              base::NumberToString(last_received_frame_token_));
+    if (last_received_frame_token_reset_) {
+      SCOPED_CRASH_KEY_STRING32(
+          "content", "Frame token (last reset)",
+          base::NumberToString(last_received_frame_token_reset_));
+    }
     client_->OnInvalidFrameToken(frame_token);
     return;
   }
