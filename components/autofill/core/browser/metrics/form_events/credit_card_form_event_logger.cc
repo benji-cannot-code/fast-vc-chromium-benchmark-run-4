958ea5e4791690c158e8ed81646a36d51f97e093
Verina Armanyous <averina@google.com>
2025-07-15 17:45:31 -0700

    [Save and Fill] Add Save and Fill suggestion histogram
    Add a form event histogram that logs when the Save and Fill suggestion
    is shown to the user. Another form event will be added in a followup CL
    for when the user accepts the suggestion. Added entry to go/payments-autofill-uma
    
    Design doc: go/payments-autofill-save-and-fill-desktop-design
    
    Bug: 431815179
    Change-Id: Id6779b777babd50f619bed0d28a044590191aaff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732043
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1487327}

--- components/autofill/core/browser/metrics/form_events/credit_card_form_event_logger.cc
+++ components/autofill/core/browser/metrics/form_events/credit_card_form_event_logger.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/payments/bnpl_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/card_info_retrieval_enrolled_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/card_unmask_flow_metrics.h"
+#include "components/autofill/core/browser/metrics/payments/save_and_fill_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/virtual_card_standalone_cvc_suggestion_metrics.h"
 #include "components/autofill/core/browser/payments/autofill_offer_manager.h"
 #include "components/autofill/core/browser/payments/bnpl_manager.h"
@@ -572,6 +573,13 @@ void CreditCardFormEventLogger::OnDidAcceptBnplSuggestion() {
   if (!has_logged_bnpl_suggestion_accepted_) {
     LogBnplFormEvent(BnplFormEvent::kBnplSuggestionAccepted);
     has_logged_bnpl_suggestion_accepted_ = true;
+  }
+}
+
+void CreditCardFormEventLogger::OnSaveAndFillSuggestionShown() {
+  if (!has_logged_save_and_fill_suggestion_shown_) {
+    LogSaveAndFillFormEvent(SaveAndFillFormEvent::kSuggestionShown);
+    has_logged_save_and_fill_suggestion_shown_ = true;
   }
 }
 
