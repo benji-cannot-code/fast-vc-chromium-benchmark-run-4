744d8a230cba1a0890305cea0e8dfe5b366f7521
Ameur Hosni <ameurhosni@google.com>
2025-07-16 10:35:10 -0700

    [IOS][AIM] Add a popup row trailing button for AIM
    Change-Id: I5fc10ded7e1df24bd3bdae297108f3a757c3203c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6729455
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487758}

--- ios/chrome/browser/omnibox/public/omnibox_popup_accessibility_identifier_constants.mm
+++ ios/chrome/browser/omnibox/public/omnibox_popup_accessibility_identifier_constants.mm
@@ -43,6 +43,9 @@ NSString* const kReviewsActionHighlightedIdentifier =
 
 NSString* const kReviewsActionIdentifier = @"kReviewsActionIdentifier";
 
+NSString* const kOmniboxPopupRowSearchWithAimAccessibilityIdentifier =
+    @"OmniboxPopupRowSearchWithAimAccessibilityIdentifier";
+
 @implementation OmniboxPopupAccessibilityIdentifierHelper
 
 + (NSString*)accessibilityIdentifierForRowAtIndexPath:(NSIndexPath*)indexPath {
