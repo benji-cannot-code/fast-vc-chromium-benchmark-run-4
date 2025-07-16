b78ec8ad840497d213af457bedd6e9cca83ca72a
Devon Loehr <dloehr@google.com>
2025-07-16 11:02:18 -0700

    Directly include <vector> in associated_user_validator.h
    The newest version libcxx no longer includes this header transitively,
    so we need to include it directly instead.
    
    Bug: 431755915
    Change-Id: Ibdfa73e6af693923d8711fb378f453092108668f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6761593
    Auto-Submit: Devon Loehr <dloehr@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487776}

--- chrome/credential_provider/gaiacp/associated_user_validator.h
+++ chrome/credential_provider/gaiacp/associated_user_validator.h
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/memory/raw_ptr.h"
 #include "base/synchronization/lock.h"
 #include "base/time/time.h"
 #include "base/win/scoped_handle.h"
-
 #include "chrome/credential_provider/gaiacp/gaia_credential_provider_i.h"
 
 namespace credential_provider {
