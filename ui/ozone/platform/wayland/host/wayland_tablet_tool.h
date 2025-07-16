a8eab1ad2d96e317555a8aa4261e6f2817e66f71
Tom Anderson <thomasanderson@chromium.org>
2025-07-16 14:59:07 -0700

    [Wayland] Support tab dragging with tablet tools
    * Include tablet button state in synthesized motion events.
    * Reset button state in ProximityIn instead of ProximityOut, so that
      buttons are still treated as pressed during DnD.
    * Include released button in MouseEvent flags.
    * Set PointerDetails::pointer_type to kPen.
    * Treat ProximityIn/ProximityOut as focus changes. Use same dispatch
      policy as for mouse events.
    * Use kMousePress for serial updates instead of kTouchPress. The latter
      should be for touch screens, not tablet devices.
    
    R=fangzhoug
    
    Bug: 264637885
    Change-Id: I16e1dbb84c4d62159b8bb59aa3d008dc8bcd01a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6757892
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487914}

--- ui/ozone/platform/wayland/host/wayland_tablet_tool.h
+++ ui/ozone/platform/wayland/host/wayland_tablet_tool.h
@@ -9,11 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <vector>
 
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
-#include "ui/events/event.h"
 #include "ui/events/pointer_details.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
+#include "ui/ozone/platform/wayland/host/wayland_pointer.h"
 
 namespace ui {
 
@@ -29,7 +30,8 @@ class WaylandTabletTool {
   WaylandTabletTool(zwp_tablet_tool_v2* tool,
                     WaylandTabletSeat* seat,
                     WaylandConnection* connection,
-                    Delegate* delegate);
+                    Delegate* delegate,
+                    WaylandPointer::Delegate* pointer_delegate);
   WaylandTabletTool(const WaylandTabletTool&) = delete;
   WaylandTabletTool& operator=(const WaylandTabletTool&) = delete;
   ~WaylandTabletTool();
@@ -91,6 +93,7 @@ class WaylandTabletTool {
   const raw_ptr<WaylandConnection> connection_;
   const raw_ptr<WaylandTabletSeat> seat_;
   const raw_ptr<Delegate> delegate_;
+  const raw_ptr<WaylandPointer::Delegate> pointer_delegate_;
 
   wl::Object<zwp_tablet_tool_v2> tool_;
 
