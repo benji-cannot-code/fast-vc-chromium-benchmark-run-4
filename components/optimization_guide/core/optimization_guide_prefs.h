af2620fb4a450b79f2586df0e096d145bd89fc39
Raj T <rajendrant@chromium.org>
2025-07-16 10:32:57 -0700

    Update ModelFetcher prefs to be Local State instead of profile pref
    This allows it to be global for all profiles on a given browser instance.
    
    Bug: 432026124
    Change-Id: I342784ffa259a3ecdbda7dd405d21b1362890822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6757252
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487755}

--- components/optimization_guide/core/optimization_guide_prefs.h
+++ components/optimization_guide/core/optimization_guide_prefs.h
@@ -19,10 +19,6 @@ namespace prefs {
 COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
 extern const char kHintsFetcherLastFetchAttempt[];
 COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
-extern const char kModelAndFeaturesLastFetchAttempt[];
-COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
-extern const char kModelLastFetchSuccess[];
-COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
 extern const char kHintsFetcherHostsSuccessfullyFetched[];
 COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
 extern const char kPendingHintsProcessingVersion[];
@@ -53,6 +49,10 @@ std::string GetSettingEnabledPrefName(UserVisibleFeatureKey feature);
 namespace localstate {
 
 // Local state prefs.
+COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
+extern const char kModelLastFetchAttempt[];
+COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
+extern const char kModelLastFetchSuccess[];
 COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
 extern const char kModelStoreMetadata[];
 COMPONENT_EXPORT(OPTIMIZATION_GUIDE_FEATURES)
