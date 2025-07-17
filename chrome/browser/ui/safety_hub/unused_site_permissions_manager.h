41bfffb892ccee8c484e12bd21e1c4042df479bc
Aisulu R <aisulu@google.com>
2025-07-17 03:53:04 -0700

    [SHv2] Extract unused site permissions related code - migration code
    This is a continuation of extracting UnusedSitePermissions logic into
    its own manager started in https://crrev.com/c/6734239.
    
    This CL moves migration code [1] (`UpdateIntegerValuesToGroupName` and
    its 2 helpers) to the new manager and calls it from the constructor.
    
    Also migration specific tests are moved as is (% updating how
    `UpdateIntegerValuesToGroupName` is called) to a new unittest file.
    
    While I could leave the migration code here due to its temporary nature,
    I think it would be better to move it too: it is used only for unused
    site permissions and is somewhat coupled with its code - moving it will
    result in a better encapsulation. This matters given ongoing bigger
    refactoring effort and the fact that it might take some time till it's
    deleted (given we don't have any metrics yet we can use to decide).
    
    [1] Migration of revoked unused site permissions' format from int to
    string introduced in https://crrev.com/c/5509595.
    
    Bug: 419321803
    Change-Id: I69cbbb77a8fed5f849cc9ddbc4d19e62843ebbd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734448
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488150}

--- chrome/browser/ui/safety_hub/unused_site_permissions_manager.h
+++ chrome/browser/ui/safety_hub/unused_site_permissions_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_SAFETY_HUB_UNUSED_SITE_PERMISSIONS_MANAGER_H_
 #define CHROME_BROWSER_UI_SAFETY_HUB_UNUSED_SITE_PERMISSIONS_MANAGER_H_
 
+#include <memory>
 #include <string>
 
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
@@ -13,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "content/public/browser/browser_context.h"
+
+class PrefChangeRegistrar;
+class PrefService;
 
 namespace url {
 class Origin;
@@ -23,7 +27,8 @@ class Origin;
 class UnusedSitePermissionsManager {
  public:
   explicit UnusedSitePermissionsManager(
-      content::BrowserContext* browser_context);
+      content::BrowserContext* browser_context,
+      PrefService* prefs);
 
   UnusedSitePermissionsManager(const UnusedSitePermissionsManager&) = delete;
   UnusedSitePermissionsManager& operator=(const UnusedSitePermissionsManager&) =
@@ -47,6 +52,19 @@ class UnusedSitePermissionsManager {
       const ContentSettingsPattern& primary_pattern);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(UnusedSitePermissionsManagerTest,
+                           UpdateIntegerValuesToGroupName_AllContentSettings);
+  FRIEND_TEST_ALL_PREFIXES(
+      UnusedSitePermissionsManagerTest,
+      UpdateIntegerValuesToGroupName_SubsetOfContentSettings);
+  FRIEND_TEST_ALL_PREFIXES(
+      UnusedSitePermissionsManagerTest,
+      UpdateIntegerValuesToGroupName_UnknownContentSettings);
+
+  // Convert all integer permission values to string, if there is any
+  // permission represented by integer.
+  void UpdateIntegerValuesToGroupName();
+
   // Pointer to an object that allows us to manage site permissions.
   HostContentSettingsMap* hcsm() {
     return HostContentSettingsMapFactory::GetForProfile(browser_context_.get());
@@ -54,6 +72,9 @@ class UnusedSitePermissionsManager {
 
   // Pointer to a browser context whose permissions are being updated.
   raw_ptr<content::BrowserContext> browser_context_;
+
+  // Observes user profile prefs.
+  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
 };
 
 #endif  // CHROME_BROWSER_UI_SAFETY_HUB_UNUSED_SITE_PERMISSIONS_MANAGER_H_
