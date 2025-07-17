1e7943e2c853f207a1643313341169a124f5276b
Colin Blundell <blundell@chromium.org>
2025-07-16 23:52:55 -0700

    [//gpu] Remove remaining references to gfix::GpuMemoryBuffer
    This class no longer exists :).
    
    Bug: 404905709
    Change-Id: I034ea8593f14b36c43de2a1931a196ae9f704dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6745971
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488073}

--- gpu/ipc/common/gpu_memory_buffer_impl_io_surface.h
+++ gpu/ipc/common/gpu_memory_buffer_impl_io_surface.h
@@ -46,7 +46,7 @@ class GPU_IPC_COMMON_EXPORT GpuMemoryBufferImplIOSurface
       gfx::BufferUsage usage,
       gfx::GpuMemoryBufferHandle* handle);
 
-  // Overridden from gfx::GpuMemoryBuffer:
+  // Overridden from GpuMemoryBufferImpl:
   bool Map() override;
   void* memory(size_t plane) override;
   void Unmap() override;
