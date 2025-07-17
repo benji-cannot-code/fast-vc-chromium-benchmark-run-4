37db780cba8fdb42525ec1f6d6fe7524077acd3d
Federica Germinario <fedegermi@google.com>
2025-07-17 02:08:58 -0700

    [ios] Remove prefs deprecated on 07/2024 from browserPrefs
    These prefs expired one year ago, code can now be removed.
    kTabPickupEnabled was not removed because the default value was 'true',
    this CL updates it to 'false'.
    
    Change-Id: I3c3492c9573cad3070752238897114bea6e85a7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6760952
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488118}

--- ios/chrome/browser/shared/model/prefs/browser_prefs_unittest.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs_unittest.mm
@@ -134,10 +134,6 @@ TEST_F(BrowserPrefsTest, VerifyLocalStatePrefsMigration) {
 
   // Set initial values in local_state
 
-  // Safety Check Manager and Settings
-  local_state()->SetString(prefs::kIosSafetyCheckManagerPasswordCheckResult,
-                           "Example");
-
   // Account Info
   local_state()->SetDict(prefs::kIosPreRestoreAccountInfo,
                          dict_example.Clone());
@@ -162,14 +158,6 @@ TEST_F(BrowserPrefsTest, VerifyLocalStatePrefsMigration) {
                             99);
 
   // Verify initial state before migration
-
-  // Check Safety Check Manager and Settings
-  EXPECT_EQ(
-      pref_service_.GetString(prefs::kIosSafetyCheckManagerPasswordCheckResult),
-      NameForSafetyCheckState(PasswordSafetyCheckState::kDefault));
-  EXPECT_EQ(local_state()->GetString(
-                prefs::kIosSafetyCheckManagerPasswordCheckResult),
-            "Example");
 
   // Check Account Info
   EXPECT_EQ(pref_service_.GetDict(prefs::kIosPreRestoreAccountInfo).size(),
@@ -237,14 +225,6 @@ TEST_F(BrowserPrefsTest, VerifyLocalStatePrefsMigration) {
   MigrateObsoleteProfilePrefs(&pref_service_);
 
   // Verify state after migration
-
-  // Check Safety Check Manager and Settings
-  EXPECT_EQ(
-      pref_service_.GetString(prefs::kIosSafetyCheckManagerPasswordCheckResult),
-      "Example");
-  EXPECT_EQ(local_state()->GetString(
-                prefs::kIosSafetyCheckManagerPasswordCheckResult),
-            NameForSafetyCheckState(PasswordSafetyCheckState::kDefault));
 
   // Check Account Info
   EXPECT_EQ(pref_service_.GetDict(prefs::kIosPreRestoreAccountInfo),
