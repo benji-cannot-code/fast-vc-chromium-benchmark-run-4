5fa370c87266602c5303ebf03f685376f9bc567f
David Sanders <dsanders11@ucsbalum.com>
2025-07-16 17:16:56 -0700

    Forward declare AXNodeData in //ui/views/view.h
    Should save ~31 MiB of compiler input size.
    
    Bug: 429365675
    Change-Id: I2e09b53a2d6d703f85282a6c0461c9883a49ae41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6759063
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487972}

--- ui/views/view.h
+++ ui/views/view.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/accessibility/ax_enums.mojom.h"
-#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/base/class_property.h"
 #include "ui/base/clipboard/clipboard_format_type.h"
@@ -98,6 +97,7 @@ class Insets;
 
 namespace ui {
 struct AXActionData;
+struct AXNodeData;
 class ColorProvider;
 class Compositor;
 class InputMethod;
