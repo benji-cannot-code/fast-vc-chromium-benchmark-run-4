651ce1fbfd5f3e3e0d79f0c725fb96c8714a09b3
Christian Xu <christianxu@google.com>
2025-07-17 11:26:17 -0700

    [iOS] Show AIM prototype view controller when tapping on entrypoint
    Add AIM prototype which is shown when tapping the entrypoint on NTP
    when the prototype flag is enabled.
    
    Bug: None
    Change-Id: Ie23d2b2d44666bcf09a0ae344c09d90f2975f50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6765645
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488438}

---
+++ ios/chrome/browser/aim/prototype/coordinator/aim_prototype_coordinator.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AIM_PROTOTYPE_COORDINATOR_AIM_PROTOTYPE_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_AIM_PROTOTYPE_COORDINATOR_AIM_PROTOTYPE_COORDINATOR_H_
+
+#import "ios/chrome/browser/aim/prototype/ui/aim_prototype_view_controller.h"
+#import "ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.h"
+
+@class AIMPrototypeCoordinator;
+
+// Delegate for the AIM prototype coordinator.
+@protocol AIMPrototypeCoordinatorDelegate
+- (void)aimPrototypeCoordinatorDidFinish:(AIMPrototypeCoordinator*)coordinator;
+@end
+
+// LensOverlayCoordinator presents the public interface for the Lens Overlay.
+@interface AIMPrototypeCoordinator
+    : ChromeCoordinator <AIMPrototypeViewControllerDelegate>
+
+@property(nonatomic, weak) id<AIMPrototypeCoordinatorDelegate> delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_AIM_PROTOTYPE_COORDINATOR_AIM_PROTOTYPE_COORDINATOR_H_
--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/profile/profile_init_stage.h"
 #import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/app/profile/profile_state_observer.h"
+#import "ios/chrome/browser/aim/prototype/coordinator/aim_prototype_coordinator.h"
 #import "ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_constants.h"
 #import "ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_coordinator.h"
 #import "ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_coordinator_delegate.h"
@@ -146,6 +147,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/l10n/l10n_util_mac.h"
 
 @interface NewTabPageCoordinator () <AccountMenuCoordinatorDelegate,
+                                     AIMPrototypeCoordinatorDelegate,
                                      AuthenticationServiceObserving,
                                      ContentSuggestionsDelegate,
                                      DiscoverFeedObserverBridgeDelegate,
@@ -275,6 +277,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @implementation NewTabPageCoordinator {
+  // Coordinator for the AIM prototype.
+  AIMPrototypeCoordinator* _aimPrototypeCoordinator;
   // Coordinator in charge of handling sharing use cases.
   SharingCoordinator* _sharingCoordinator;
   // Coordinator for presenting the Home customization menu.
@@ -388,6 +392,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!self.started) {
     return;
   }
+
+  [self stopAimPrototypeCoordinator];
 
   _webState = nullptr;
 
@@ -1572,7 +1578,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self stopAccountMenuCoordinator];
 }
 
+#pragma mark - AIMPrototypeCoordinatorDelegate
+
+- (void)aimPrototypeCoordinatorDidFinish:(AIMPrototypeCoordinator*)coordinator {
+  [self stopAimPrototypeCoordinator];
+}
+
 #pragma mark - Private
+
+- (void)startAimPrototypeCoordinator {
+  if (_aimPrototypeCoordinator) {
+    return;
+  }
+  _aimPrototypeCoordinator = [[AIMPrototypeCoordinator alloc]
+      initWithBaseViewController:self.baseViewController
+                         browser:self.browser];
+  _aimPrototypeCoordinator.delegate = self;
+  [_aimPrototypeCoordinator start];
+}
+
+- (void)stopAimPrototypeCoordinator {
+  if (!_aimPrototypeCoordinator) {
+    return;
+  }
+  [_aimPrototypeCoordinator stop];
+  _aimPrototypeCoordinator = nil;
+}
 
 - (void)stopSharingCoordinator {
   [_sharingCoordinator stop];
@@ -1935,6 +1966,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)openMIA {
+  if (base::FeatureList::IsEnabled(kAIMPrototype)) {
+    [self startAimPrototypeCoordinator];
+    return;
+  }
   [self.NTPMetricsRecorder recordMIATapped];
   OpenNewTabCommand* command = [OpenNewTabCommand
       commandWithURLFromChrome:GetUrlForAim(
