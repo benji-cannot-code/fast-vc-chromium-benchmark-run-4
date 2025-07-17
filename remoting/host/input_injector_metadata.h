380a03578b58ded4c2be896b06f3eb3220af0a38
Philipp Hancke <philipp.hancke@googlemail.com>
2025-07-17 11:48:49 -0700

    remoting: directly include pipewire/webrtc xdg_session_details
    which unblocks IWYU in WebRTC:
      https://webrtc-review.googlesource.com/c/src/+/399820
    
    Bug: None
    Change-Id: Ica08237b5f4e36867074bd164c9a7b8eeb53e8bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6763702
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488449}

--- remoting/host/input_injector_metadata.h
+++ remoting/host/input_injector_metadata.h
@@ -7,14 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REMOTING_HOST_INPUT_INJECTOR_METADATA_H_
 
 #if defined(WEBRTC_USE_PIPEWIRE)
-#include "third_party/webrtc/modules/portal/xdg_desktop_portal_utils.h"
+#include "third_party/webrtc/modules/portal/xdg_session_details.h"
 #endif  // defined(WEBRTC_USE_PIPEWIRE)
 
 namespace remoting {
 
 struct InputInjectorMetadata {
 #if defined(WEBRTC_USE_PIPEWIRE)
-  // Details of the XDG desktop portal session (required by the wayalnd input
+  // Details of the XDG desktop portal session (required by the wayland input
   // injector).
   webrtc::xdg_portal::SessionDetails session_details;
 #endif  // defined(WEBRTC_USE_PIPEWIRE)
