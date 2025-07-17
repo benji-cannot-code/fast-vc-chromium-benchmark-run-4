bd422be5e55a2bad1198cf37066b6d2e9b42a4ed
Maria Kazinova <kazinova@google.com>
2025-07-17 02:26:31 -0700

    [OTPs] Fetch and offer OTP suggestions on user field focus.
    This CL enables showing OTP suggestions when the user focuses a
    field parsed as an OTP field.
    This supports the pre-kAutofillNewSuggestionGeneration state, when
    suggestions that need to provided by integrators asynchronously are
    queried one by one.
    So far, only SMS OTP suggestions Android are supported, and the
    fetch is happening on the page load, and is very quick, so this flow
    is appropriate.
    
    Follow-up work is still needed to support post-
    kAutofillNewSuggestionGeneration state.
    
    Suggestion screenshot: https://screenshot.googleplex.com/AsU9FudCUALHHfq
    Suggestion mocks: https://screenshot.googleplex.com/4k6HFcm25nneAAJ
    (from go/gpm-sms-otp slide 13)
    
    Bug: 415273276
    Change-Id: Ia6f6165f90e06cc42a8d1129c312aff7e31e67cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718060
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488128}

---
+++ components/autofill/core/browser/suggestions/one_time_passwords/otp_suggestion_generator.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "components/autofill/core/browser/suggestions/one_time_passwords/otp_suggestion_generator.h"
+
+#include "base/strings/utf_string_conversions.h"
+
+namespace autofill {
+namespace {
+
+// Builds Suggestion for given `otp_value`.
+Suggestion BuildOtpSuggestion(const std::string& otp_value) {
+  Suggestion suggestion = Suggestion(base::UTF8ToUTF16(otp_value),
+                                     SuggestionType::kOneTimePasswordEntry);
+  suggestion.icon = Suggestion::Icon::kAndroidMessages;
+  return suggestion;
+}
+
+}  // namespace
+
+std::vector<Suggestion> BuildOtpSuggestions(
+    std::vector<std::string> one_time_passwords) {
+  std::vector<Suggestion> suggestions;
+  for (const std::string& otp_value : one_time_passwords) {
+    suggestions.push_back(BuildOtpSuggestion(otp_value));
+  }
+  return suggestions;
+}
+
+}  // namespace autofill
---
+++ components/autofill/core/browser/suggestions/one_time_passwords/otp_suggestion_generator.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_SUGGESTIONS_ONE_TIME_PASSWORDS_OTP_SUGGESTION_GENERATOR_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_SUGGESTIONS_ONE_TIME_PASSWORDS_OTP_SUGGESTION_GENERATOR_H_
+
+#include <vector>
+
+#include "components/autofill/core/browser/suggestions/suggestion.h"
+
+namespace autofill {
+
+// Generates OTP suggestions from the provided vector of retrieved OTP values.
+// TODO(crbug.com/409962888): Cleanup once AutofillNewSuggestionGeneration is
+// launched.
+std::vector<Suggestion> BuildOtpSuggestions(
+    std::vector<std::string> one_time_passwords);
+
+}  // namespace autofill
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_SUGGESTIONS_ONE_TIME_PASSWORDS_OTP_SUGGESTION_GENERATOR_H_
