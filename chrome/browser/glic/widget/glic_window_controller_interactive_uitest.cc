9e4e7362ebd39cdce86058c726f2cf67e9fc3264
Mike Wasserman <msw@chromium.org>
2025-07-16 17:11:49 -0700

    [Gardener] Disable GlicWindowControllerUiTest.TestPositionMetrics on Mac
    Bug: 426542319
    Change-Id: I11429ce09acc71d341a7a5fa745152f5b349081b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6762417
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487968}

--- chrome/browser/glic/widget/glic_window_controller_interactive_uitest.cc
+++ chrome/browser/glic/widget/glic_window_controller_interactive_uitest.cc
@@ -451,9 +451,9 @@ IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, TestInitialBounds) {
   }
 }
 
-// TODO(b/426542319): Fix and enable tests on non-mac platforms.
-#if BUILDFLAG(IS_MAC)
-IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, TestPositionMetrics) {
+// TODO(crbug.com/426542319): Fix and re-enable.
+IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest,
+                       DISABLED_TestPositionMetrics) {
   // The GlicButton and Tabstrip are not actually shown until a tab is created.
   chrome::AddTabAt(browser(), GURL("about:blank"), 0, true);
   gfx::Rect work_area_bounds =
@@ -527,7 +527,6 @@ IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, TestPositionMetrics) {
   // ChromeRelativePosition::kChromeOnOtherDisplay isn't being tested since
   // tests involving moving Glic to another display are flaky.
 }
-#endif
 
 class GlicWindowControllerWithPreviousPostionUiTest
     : public GlicWindowControllerUiTest {
