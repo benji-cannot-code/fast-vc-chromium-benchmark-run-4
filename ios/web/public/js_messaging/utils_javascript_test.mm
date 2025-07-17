3289a5fede0c2e4547f928e17c893393474d8113
Mike Dougherty <michaeldo@chromium.org>
2025-07-17 03:02:52 -0700

    Migrate test usage of ExecuteJavaScript in ios/web
    Call sites which are not interested in the return value of the executed
    JavaScript are being migrated to functions which do not return a value.
    
    This will allow the addition of `[[nodiscard]]` on the helpers which
    return values and improved error handling in tests by enabling
    `kAssertOnJavaScriptErrors` more widely in future follow-up CLs.
    
    Bug: None
    Change-Id: I6ccdaa5b3b9fa9f903dd1ceb46776ec743ea7fa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764027
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488139}

--- ios/web/public/js_messaging/utils_javascript_test.mm
+++ ios/web/public/js_messaging/utils_javascript_test.mm
@@ -98,7 +98,7 @@ TEST_F(
     UtilsJavaScriptTest,
     RemoveQueryAndReferenceFromURL_WithCorruptedURLPrototype_MissingProperty) {
   // Replace the window.URL prototype.
-  web::test::ExecuteJavaScript(
+  web::test::ExecuteJavaScriptInWebView(
       web_view(), @"window.URL = function() { return { weird_field: 1 }; };");
 
   NSString* apiCall =
@@ -115,9 +115,9 @@ TEST_F(
 TEST_F(UtilsJavaScriptTest,
        RemoveQueryAndReferenceFromURL_WithCorruptedURLPrototype_WrongType) {
   // Replace the window.URL prototype.
-  web::test::ExecuteJavaScript(web_view(),
-                               @"window.URL = function() { return {"
-                                "origin: 'o', path: 'pa', protocol: 3 }; };");
+  web::test::ExecuteJavaScriptInWebView(
+      web_view(), @"window.URL = function() { return {"
+                   "origin: 'o', path: 'pa', protocol: 3 }; };");
 
   NSString* apiCall =
       @"__gCrWeb.utils_tests.removeQueryAndReferenceFromURL('%@')";
@@ -173,7 +173,7 @@ TEST_F(UtilsJavaScriptTest, SendWebKitMessage) {
     NSString* js = [NSString
         stringWithFormat:@"__gCrWeb.utils_tests.sendWebKitMessage('%@', %@)",
                          kUtilsSampleMessageHandlerName, data.input];
-    web::test::ExecuteJavaScript(web_view(), js);
+    web::test::ExecuteJavaScriptInWebView(web_view(), js);
 
     ASSERT_TRUE(base::test::ios::WaitUntilConditionOrTimeout(
         base::test::ios::kWaitForJSCompletionTimeout, ^bool() {
