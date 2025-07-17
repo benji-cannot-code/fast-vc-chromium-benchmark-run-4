b19993157c89b9d7855777baca44316749ba2054
Longsheng Yin <longsheng@google.com>
2025-07-17 13:14:20 -0700

    [Pix Account Linking] Log prompt accepted action.
    Design: go/pix-account-linking-on-chrome-dd
    
    Bug: 419108993
    Change-Id: Iad63e1ec97937b5cacefc2a584c8ce4bb8ffefb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6723773
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Kavita Soni <kavitasoni@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488495}

--- components/facilitated_payments/core/browser/pix_account_linking_manager.cc
+++ components/facilitated_payments/core/browser/pix_account_linking_manager.cc
@@ -121,7 +121,7 @@ void PixAccountLinkingManager::DismissPrompt() {
 }
 
 void PixAccountLinkingManager::OnAccepted() {
-  // TODO(crbug.com/419108993): Add metrics.
+  LogPixAccountLinkingPromptAccepted();
   DismissPrompt();
   client_->GetDeviceDelegate()->LaunchPixAccountLinkingPage();
 }
