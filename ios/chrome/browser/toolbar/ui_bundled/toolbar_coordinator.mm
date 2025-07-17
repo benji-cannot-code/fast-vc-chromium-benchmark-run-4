a093a0265275b6eda95a9f3253f74e0826bdb031
Stepan Khapugin <stkhapugin@chromium.org>
2025-07-17 10:25:03 -0700

    [iOS][Omnibox] Create DRS prototype.
    Implements a skeleton UI for Dynamic Response System Prototype, where
    multiple response types can be shown in the omnibox dynamically.
    
    Bug: none
    Change-Id: I6a3098acd649e160fc5db8d5b6dae8fb6d5558fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6759922
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488386}

--- ios/chrome/browser/toolbar/ui_bundled/toolbar_coordinator.mm
+++ ios/chrome/browser/toolbar/ui_bundled/toolbar_coordinator.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ntp/model/new_tab_page_tab_helper.h"
 #import "ios/chrome/browser/ntp/model/new_tab_page_util.h"
 #import "ios/chrome/browser/omnibox/model/omnibox_position/omnibox_position_browser_agent.h"
+#import "ios/chrome/browser/omnibox/ui/omnibox_drs_view_controller.h"
 #import "ios/chrome/browser/orchestrator/ui_bundled/omnibox_focus_orchestrator.h"
 #import "ios/chrome/browser/orchestrator/ui_bundled/omnibox_focus_orchestrator_parity.h"
 #import "ios/chrome/browser/overlays/model/public/overlay_presentation_context.h"
@@ -70,6 +71,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) OmniboxFocusOrchestrator* orchestrator;
 /// Whether the omnibox is currently focused.
 @property(nonatomic, assign) BOOL locationBarFocused;
+/// Dynamic response system view controller is an omnibox presenter. Only
+/// defined  when kOmniboxDRSPrototype is set.
+@property(nonatomic, strong) OmniboxDRSViewController* drsViewController;
 
 @end
 
@@ -131,6 +135,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self.browser->GetCommandDispatcher()
         startDispatchingToTarget:self
                      forProtocol:@protocol(GuidedTourCommands)];
+  }
+
+  if (base::FeatureList::IsEnabled(kOmniboxDRSPrototype)) {
+    self.drsViewController = [[OmniboxDRSViewController alloc] init];
+    self.drsViewController.proxiedPresenterDelegate =
+        self.popupPresenterDelegate;
+    self.popupPresenterDelegate = self.drsViewController;
   }
 
   segmentation_platform::DeviceSwitcherResultDispatcher* deviceSwitcherResult =
@@ -328,16 +339,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   __weak __typeof(self) weakSelf = self;
   BOOL toolbarExpanded = focused && !CanShowTabStrip(self.traitEnvironment);
-  [self.orchestrator
-      transitionToStateOmniboxFocused:focused
-                      toolbarExpanded:toolbarExpanded
-                              trigger:[self omniboxFocusTrigger]
-                             animated:animateTransition
-                           completion:^{
-                             [weakSelf focusTransitionDidComplete:focused
-                                                       completion:completion];
-                           }];
+  if (base::FeatureList::IsEnabled(kOmniboxDRSPrototype) && focused) {
+    [self.baseViewController presentViewController:self.drsViewController
+                                          animated:YES
+                                        completion:nil];
 
+    return;
+
+  } else {
+    [self.orchestrator
+        transitionToStateOmniboxFocused:focused
+                        toolbarExpanded:toolbarExpanded
+                                trigger:[self omniboxFocusTrigger]
+                               animated:animateTransition
+                             completion:^{
+                               [weakSelf focusTransitionDidComplete:focused
+                                                         completion:completion];
+                             }];
+  }
   self.locationBarFocused = focused;
 }
 
