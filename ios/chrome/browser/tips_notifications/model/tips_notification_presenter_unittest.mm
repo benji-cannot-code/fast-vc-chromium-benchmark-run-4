56b79c43a60b5fc4e1e89463c3233cf12040c8b6
Yurii Lahodiuk <ylahodiuk@google.com>
2025-07-17 06:20:20 -0700

    [iOS] Add metrics for presenter of the Trusted Vault notification
    In the scope of this change we are adding metrics for monitoring cases
    when the Trusted Vault key retrieval flow has been started or has not
    been started (which should be rare) upon opening the notification.
    
    Bug: b:413671723
    Change-Id: I5e9b8be1d367269fff5d23a1f527121469f3247c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6727774
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488221}

--- ios/chrome/browser/tips_notifications/model/tips_notification_presenter_unittest.mm
+++ ios/chrome/browser/tips_notifications/model/tips_notification_presenter_unittest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/tips_notifications/model/tips_notification_presenter.h"
 
+#import "base/test/metrics/histogram_tester.h"
 #import "components/sync/service/sync_service.h"
 #import "components/sync/service/sync_service_utils.h"
 #import "components/sync/service/sync_user_settings.h"
@@ -76,6 +77,7 @@ class TipsNotificationPresenterTest : public PlatformTest {
   std::unique_ptr<TestBrowser> browser_;
   raw_ptr<syncer::MockSyncService> sync_service_mock_ = nullptr;
   id application_handler_;
+  const base::HistogramTester histogram_tester_;
 };
 
 #pragma mark - Test Cases
@@ -189,6 +191,9 @@ TEST_F(TipsNotificationPresenterTest, TestStartTrustedVaultKeyRetrievalFlow) {
   TipsNotificationPresenter::Present(
       browser_->AsWeakPtr(), TipsNotificationType::kTrustedVaultKeyRetrieval);
   EXPECT_OCMOCK_VERIFY(mock_handler);
+  histogram_tester_.ExpectUniqueSample(
+      "IOS.PasswordManager.TrustedVaultNotification.Events",
+      TrustedVaultNotificationEvents::kKeyRetrievalFlowStarted, 1);
 }
 
 // Tests that the presenter doesn't start the trusted vault key retrieval flow
@@ -202,4 +207,7 @@ TEST_F(TipsNotificationPresenterTest,
   TipsNotificationPresenter::Present(
       browser_->AsWeakPtr(), TipsNotificationType::kTrustedVaultKeyRetrieval);
   EXPECT_OCMOCK_VERIFY(mock_handler);
+  histogram_tester_.ExpectUniqueSample(
+      "IOS.PasswordManager.TrustedVaultNotification.Events",
+      TrustedVaultNotificationEvents::kTrustedVaultKeyAlreadyAvailable, 1);
 }
