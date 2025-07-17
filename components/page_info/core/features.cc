360e3968e8fa0d2b884cbedd7cadf663e5b45e33
Nicola Tommasi <tommasin@chromium.org>
2025-07-17 04:18:53 -0700

    Merchant Trust: Enable 100% on ToT
    Bug: 430212213
    Change-Id: Ic9f5e59c4674c2b027d3a4d55bbc63e553f9b836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713337
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488164}

--- components/page_info/core/features.cc
+++ components/page_info/core/features.cc
@@ -53,7 +53,7 @@ BASE_FEATURE(kPageInfoHideSiteSettings,
 
 BASE_FEATURE(kMerchantTrust,
              "MerchantTrust",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const char kMerchantTrustEnabledWithSampleDataName[] =
     "enabled-with-sample-data";
