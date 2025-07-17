ac9b6689a078c4917b0a6706bcfc5dab15acdd35
Tom Sepez <tsepez@chromium.org>
2025-07-17 13:07:55 -0700

    Remove IPC::SyncMessageFilter.
    It is unused in Chrome.
    
    Bug: 40621529
    Change-Id: I7a1d210869c6a419867cec77a166acd76b99e7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6762255
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488490}

--- components/viz/service/gl/gpu_service_impl.cc
+++ components/viz/service/gl/gpu_service_impl.cc
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/buildflags.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ipc/ipc_sync_channel.h"
-#include "ipc/ipc_sync_message_filter.h"
 #include "media/base/media_log.h"
 #include "media/base/media_switches.h"
 #include "media/gpu/buildflags.h"
