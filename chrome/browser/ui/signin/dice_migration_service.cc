8b11abb83aa26945021bef7f11f96b78eb281122
Ankush Singh <ankushkush@google.com>
2025-07-17 04:01:09 -0700

    Add timer to trigger the DICe migration dialog
    The timer starts upon the service creation if the user is considered
    eligible for migration. The timer is set for the standard session grace
    period, upon which we will show the dialog.
    
    Bug: 399838468
    Change-Id: I6fe4a29147fdeaccaa68b6eb0f9edc6b8370b70a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6759901
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488155}

--- chrome/browser/ui/signin/dice_migration_service.cc
+++ chrome/browser/ui/signin/dice_migration_service.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_utils.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/user_education/common/user_education_features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/dialog_model.h"
 #include "ui/views/bubble/bubble_dialog_model_host.h"
@@ -122,7 +123,15 @@ DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(DiceMigrationService,
                                       kCancelButtonElementId);
 
 DiceMigrationService::DiceMigrationService(Profile* profile)
-    : profile_(profile) {}
+    : profile_(profile) {
+  if (IsUserEligibleForDiceMigration(profile_)) {
+    dialog_trigger_timer_.Start(
+        FROM_HERE, user_education::features::GetSessionStartGracePeriod(),
+        base::BindOnce(
+            &DiceMigrationService::ShowDiceMigrationOfferDialogIfUserEligible,
+            base::Unretained(this)));
+  }
+}
 
 DiceMigrationService::~DiceMigrationService() {
   if (dialog_widget_) {
@@ -216,6 +225,10 @@ bool DiceMigrationService::IsDialogShowing() {
 
 views::Widget* DiceMigrationService::GetDialogWidgetForTesting() {
   return dialog_widget_.get();
+}
+
+base::OneShotTimer& DiceMigrationService::GetDialogTriggerTimerForTesting() {
+  return dialog_trigger_timer_;
 }
 
 void DiceMigrationService::OnWidgetDestroying(views::Widget* widget) {
--- chrome/browser/ui/signin/dice_migration_service_factory.h
+++ chrome/browser/ui/signin/dice_migration_service_factory.h
@@ -14,6 +14,7 @@ class DiceMigrationService;
 class DiceMigrationServiceFactory : public ProfileKeyedServiceFactory {
  public:
   static DiceMigrationService* GetForProfile(Profile* profile);
+  static DiceMigrationService* GetForProfileIfExists(Profile* profile);
 
   // Returns an instance of the DiceMigrationServiceFactory singleton.
   static DiceMigrationServiceFactory* GetInstance();
