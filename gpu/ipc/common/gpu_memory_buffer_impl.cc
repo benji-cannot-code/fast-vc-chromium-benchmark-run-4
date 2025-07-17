792bbd6f474d525bc7522c0e36d6de8964174ab3
Colin Blundell <blundell@chromium.org>
2025-07-16 23:47:40 -0700

    [//ui] Remove gfx::GpuMemoryBuffer
    The only entity that now has knowledge of gfx::GpuMemoryBuffer is
    ClientSharedImage(/TestSharedImageInterface), and it has knowledge of
    it as the concrete gpu::GpuMemoryBufferImpl subclass.
    
    Future work will do renaming, decomposition, etc.
    
    Bug: 404905709
    Change-Id: I090482598e36f10bc438cfb4754497c80e1586fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734765
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488069}

--- gpu/ipc/common/gpu_memory_buffer_impl.cc
+++ gpu/ipc/common/gpu_memory_buffer_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/ipc/common/gpu_memory_buffer_impl.h"
 
+#include "ui/gfx/buffer_format_util.h"
+
 namespace gpu {
 
 GpuMemoryBufferImpl::GpuMemoryBufferImpl(const gfx::Size& size,
@@ -33,6 +35,31 @@ void GpuMemoryBufferImpl::AssertMapped() {
   base::AutoLock auto_lock(map_lock_);
   DCHECK_GT(map_count_, 0u);
 #endif
+}
+
+void GpuMemoryBufferImpl::MapAsync(base::OnceCallback<void(bool)> result_cb) {
+  std::move(result_cb).Run(Map());
+}
+
+bool GpuMemoryBufferImpl::AsyncMappingIsNonBlocking() const {
+  return false;
+}
+
+base::span<uint8_t> GpuMemoryBufferImpl::memory_span(size_t plane) {
+  uint8_t* data = static_cast<uint8_t*>(memory(plane));
+  if (!data) {
+    return {};
+  }
+  size_t size = 0;
+  if (!PlaneSizeForBufferFormatChecked(GetSize(), GetFormat(), plane, &size)) {
+    return {};
+  }
+
+  // SAFETY: The safety is ensured by the contract of the `GpuMemoryBuffer`.
+  // `data` is a pointer to memory that has been mapped by `Map()` and
+  // the `size` is calculated using the buffer utility method used by all
+  // `GpuMemoryBuffer` clients already.
+  return UNSAFE_BUFFERS(base::span<uint8_t>(data, size));
 }
 
 }  // namespace gpu
