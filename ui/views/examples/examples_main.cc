7d95acb33017f407c2a3c36bdbaba23ac3696c91
Robert Liao <robliao@chromium.org>
2025-07-16 17:23:46 -0700

    Views Examples Mac App Bundle
    Converts the views_examples executable on Mac to an app bundle.
    
    Change-Id: Ic4d8c61411265b8e7fea829d1617957c88d3b17e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629439
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487975}

--- ui/views/examples/examples_main.cc
+++ ui/views/examples/examples_main.cc
@@ -3,12 +3,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ui/views/examples/examples_main.h"
+
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/test/test_timeouts.h"
+#include "build/build_config.h"
+#include "build/buildflag.h"
 #include "ui/views/examples/examples_main_proc.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "ui/views/examples/examples_main_mac_support.h"
+#endif
+
+#if BUILDFLAG(IS_MAC)
+int ViewsExamplesMain(int argc, char** argv) {
+#else
 int main(int argc, char** argv) {
+#endif  // BUILDFLAG(IS_MAC)
   base::CommandLine::Init(argc, argv);
 
   // The use of base::test::TaskEnvironment in the following function relies on
@@ -16,6 +28,10 @@ int main(int argc, char** argv) {
   TestTimeouts::Initialize();
 
   base::AtExitManager at_exit;
+
+#if BUILDFLAG(IS_MAC)
+  UpdateFrameworkBundlePath();
+#endif
 
   return static_cast<int>(views::examples::ExamplesMainProc());
 }
