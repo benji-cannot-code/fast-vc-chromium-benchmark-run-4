4b737ad5df3e8062d70df4c7aa4d79e182e7468e
Saifuddin Hitawala <hitawala@chromium.org>
2025-07-17 13:43:08 -0700

    [media] Use format from shared image in FrameResources
    There's no need to store video pixel format in FrameResources as we
    can use that from shared image.
    
    Also add a utility method for converting VideoPixelFormat to
    SharedImageFormat that can be used as we phase out BufferFormat usages.
    
    Bug: 425634684, 356649879
    Change-Id: I561b8ea4568aa96d40d09e644a85a7eb198be35b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6759633
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488509}

--- media/base/format_utils.h
+++ media/base/format_utils.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "components/viz/common/resources/shared_image_format.h"
 #include "media/base/media_export.h"
 #include "media/base/video_types.h"
 #include "ui/gfx/buffer_types.h"
@@ -19,6 +20,9 @@ MEDIA_EXPORT std::optional<VideoPixelFormat> GfxBufferFormatToVideoPixelFormat(
 
 MEDIA_EXPORT std::optional<gfx::BufferFormat> VideoPixelFormatToGfxBufferFormat(
     VideoPixelFormat pixel_format);
+
+MEDIA_EXPORT std::optional<viz::SharedImageFormat>
+VideoPixelFormatToSharedImageFormat(VideoPixelFormat pixel_format);
 
 }  // namespace media
 
