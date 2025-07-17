dd71f632ad742ab3f97c2b7132fc60a4dcdd72c3
William Liu <liuwilliam@chromium.org>
2025-07-17 14:39:26 -0700

    Put HostCapability::kScrollToPdf behind ENABLE_PDF guard
    This is to be consistent with the implementation of the PDF scrollTo
    machinery (https://crrev.com/c/6618222 ), where support is only
    implemented behind the ENABLE_PDF guard
    
    Bug: b:395859365, b:429639179
    Change-Id: I2c7dd8bb9332f7cf63030a848d1b6fa1788abedb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6766206
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488535}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/guest_view/web_view/web_view_guest.h"
 #include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "mojo/public/cpp/bindings/message.h"
+#include "pdf/buildflags.h"
 #include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/geometry/mojom/geometry.mojom.h"
@@ -590,9 +591,11 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
         features::kGlicUserStatusRefreshApi.Get();
     state->enable_multi_tab =
         base::FeatureList::IsEnabled(glic::mojom::features::kGlicMultiTab);
+#if BUILDFLAG(ENABLE_PDF)
     if (features::kGlicScrollToPDF.Get()) {
       state->host_capabilities.push_back(mojom::HostCapability::kScrollToPdf);
     }
+#endif
 
     std::move(callback).Run(std::move(state));
   }
