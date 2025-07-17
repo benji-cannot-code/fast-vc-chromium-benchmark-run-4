fab8a840539edd28bad650d4f271d945dbde7abc
Dan Harrington <harringtond@google.com>
2025-07-17 12:34:38 -0700

    Add glic/public directory
    I'm moving a single file here to start. I think we can
    just iteratively move files here over time. Even with
    a small set of files, I think this can help clarify
    what's intended to be externally accessible, and so is
    a useful thing to do.
    
    Bug: b:430371855
    Change-Id: Ie11bdcdb97b7a0ed8476b974272028c39ac0e7dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764078
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488466}

---
+++ chrome/browser/glic/public/README.md
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+All Glic code/interfaces intended for external belong here.
+
+This is a work in progress. See b/430371855.
--- chrome/browser/glic/public/context/glic_sharing_manager.h
+++ chrome/browser/glic/public/context/glic_sharing_manager.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_GLIC_HOST_CONTEXT_GLIC_SHARING_MANAGER_H_
-#define CHROME_BROWSER_GLIC_HOST_CONTEXT_GLIC_SHARING_MANAGER_H_
+#ifndef CHROME_BROWSER_GLIC_PUBLIC_CONTEXT_GLIC_SHARING_MANAGER_H_
+#define CHROME_BROWSER_GLIC_PUBLIC_CONTEXT_GLIC_SHARING_MANAGER_H_
 
 #include "base/containers/span.h"
 #include "chrome/browser/glic/host/context/glic_tab_data.h"
@@ -74,4 +74,4 @@ class GlicSharingManager {
 
 }  // namespace glic
 
-#endif  // CHROME_BROWSER_GLIC_HOST_CONTEXT_GLIC_SHARING_MANAGER_H_
+#endif  // CHROME_BROWSER_GLIC_PUBLIC_CONTEXT_GLIC_SHARING_MANAGER_H_
