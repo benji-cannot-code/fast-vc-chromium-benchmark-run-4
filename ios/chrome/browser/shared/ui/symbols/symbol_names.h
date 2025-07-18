1eda8eb48c22da26b1a80c9314cde416a535b135
Jérôme Lebel <jlebel@chromium.org>
2025-07-18 04:09:11 -0700

    [iOS] Add kGoogleSearchEngineLogoImage in symbol_names.h
    This image already exists in:
    ios_internal/chrome/browser/ui/icons/resources/Assets.xcassets/google_logo.symbolset
    
    Bug: 423883582
    Change-Id: Id7608714f185cb0338659683c33e1cf48474dd64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764011
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488755}

--- ios/chrome/browser/shared/ui/symbols/symbol_names.h
+++ ios/chrome/browser/shared/ui/symbols/symbol_names.h
@@ -30,6 +30,7 @@ extern NSString* const kGeminiBrandedLogoImage;
 extern NSString* const kGoogleDriveSymbol;
 extern NSString* const kGoogleFullSymbol;
 extern NSString* const kGoogleIconSymbol;
+extern NSString* const kGoogleSearchEngineLogoImage;
 extern NSString* const kGoogleShieldSymbol;
 extern NSString* const kGoogleMapsSymbol;
 extern NSString* const kGooglePasswordManagerWidgetPromoImage;
