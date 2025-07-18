90d853ad648fd8aa473fa4e2dfe6b1d3639a558b
Yeonghan Kim <soosungp33@gmail.com>
2025-07-18 00:47:10 -0700

    Replace base::BindOnce in video_track_adapter_unittest.cc
    This patch replaces base::BindOnce with CrossThreadBindOnce in
    video_track_adapter_unittest.cc for cross-thread handling.
    And add necessary copiers to header file and reuse them.
    
    Bug: 40797264
    Change-Id: I52934cd88ca40d2b68dfdb67f7c290e0705919ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6733117
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Reviewed-by: Jinho Bang <zino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488686}

--- third_party/blink/renderer/modules/mediastream/video_track_adapter_unittest.cc
+++ third_party/blink/renderer/modules/mediastream/video_track_adapter_unittest.cc
@@ -24,9 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/mediastream/mock_encoded_video_frame.h"
 #include "third_party/blink/renderer/modules/mediastream/mock_media_stream_video_source.h"
 #include "third_party/blink/renderer/modules/mediastream/video_track_adapter_settings.h"
+#include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/testing/io_task_runner_testing_platform_support.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/video_frame_utils.h"
+#include "third_party/blink/renderer/platform/wtf/cross_thread_copier_media.h"
+#include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/webrtc_overrides/low_precision_timer.h"
 #include "third_party/webrtc_overrides/metronome_source.h"
 
@@ -233,9 +237,10 @@ class VideoTrackAdapterFixtureTest : public ::testing::Test {
 
   void TearDown() override {
     if (track_added_) {
-      testing_render_thread_.task_runner()->PostTask(
-          FROM_HERE, base::BindOnce(&VideoTrackAdapter::RemoveTrack, adapter_,
-                                    null_track_.get()));
+      PostCrossThreadTask(
+          *testing_render_thread_.task_runner(), FROM_HERE,
+          CrossThreadBindOnce(&VideoTrackAdapter::RemoveTrack, adapter_,
+                              CrossThreadUnretained(null_track_.get())));
     }
     base::WaitableEvent source_deleted;
     testing_render_thread_.task_runner()->PostTask(
@@ -271,10 +276,11 @@ class VideoTrackAdapterFixtureTest : public ::testing::Test {
       AddTrackInternal(null_track_.get(), adapter_settings);
       track_added_ = true;
     } else {
-      testing_render_thread_.task_runner()->PostTask(
-          FROM_HERE,
-          base::BindOnce(&VideoTrackAdapter::ReconfigureTrack, adapter_,
-                         null_track_.get(), adapter_settings));
+      PostCrossThreadTask(
+          *testing_render_thread_.task_runner(), FROM_HERE,
+          CrossThreadBindOnce(&VideoTrackAdapter::ReconfigureTrack, adapter_,
+                              CrossThreadUnretained(null_track_.get()),
+                              adapter_settings));
     }
   }
 
@@ -293,10 +299,12 @@ class VideoTrackAdapterFixtureTest : public ::testing::Test {
         &VideoTrackAdapterFixtureTest::OnFrameSettings, base::Unretained(this));
     video_stream_fallbacks.sub_capture_target_version_cb = base::DoNothing();
     video_stream_fallbacks.format_cb = base::DoNothing();
-    testing_render_thread_.task_runner()->PostTask(
-        FROM_HERE,
-        base::BindOnce(&VideoTrackAdapter::AddTrack, adapter_, track,
-                       std::move(video_stream_fallbacks), adapter_settings));
+    PostCrossThreadTask(
+        *testing_render_thread_.task_runner(), FROM_HERE,
+        CrossThreadBindOnce(&VideoTrackAdapter::AddTrack, adapter_,
+                            CrossThreadUnretained(track),
+                            std::move(video_stream_fallbacks),
+                            adapter_settings));
   }
 
   void SetFrameValidationCallback(VideoCaptureDeliverFrameCB callback) {
@@ -316,11 +324,10 @@ class VideoTrackAdapterFixtureTest : public ::testing::Test {
   void DeliverAndValidateFrame(scoped_refptr<media::VideoFrame> frame,
                                base::TimeTicks estimated_capture_time) {
     auto deliver_frame = [&]() {
-      platform_support_->GetIOTaskRunner()->PostTask(
-          FROM_HERE,
-          base::BindOnce(&VideoTrackAdapter::DeliverFrameOnVideoTaskRunner,
-                         adapter_, frame,
-                         estimated_capture_time));
+      PostCrossThreadTask(
+          *platform_support_->GetIOTaskRunner(), FROM_HERE,
+          CrossThreadBindOnce(&VideoTrackAdapter::DeliverFrameOnVideoTaskRunner,
+                              adapter_, frame, estimated_capture_time));
     };
 
     frame_processed_.Reset();
@@ -660,10 +667,11 @@ TEST_F(VideoTrackAdapterFixtureTest,
       base::BindLambdaForTesting(check_dimensions);
   video_stream_fallbacks.sub_capture_target_version_cb = base::DoNothing();
   video_stream_fallbacks.format_cb = base::DoNothing();
-  testing_render_thread_.task_runner()->PostTask(
-      FROM_HERE,
-      base::BindOnce(&VideoTrackAdapter::AddTrack, adapter_, second_track.get(),
-                     std::move(video_stream_fallbacks), adapter_settings));
+  PostCrossThreadTask(
+      *testing_render_thread_.task_runner(), FROM_HERE,
+      CrossThreadBindOnce(&VideoTrackAdapter::AddTrack, adapter_,
+                          CrossThreadUnretained(second_track.get()),
+                          std::move(video_stream_fallbacks), adapter_settings));
   settings_callback_run_.Wait();
 }
 
