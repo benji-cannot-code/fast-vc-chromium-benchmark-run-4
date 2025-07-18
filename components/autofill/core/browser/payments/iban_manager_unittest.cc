16c3c1466f07be644c865ec9833dbed54dbfa43c
Greg Thompson <grt@chromium.org>
2025-07-18 12:25:54 -0700

    [payments] Use actual images in Iban tests
    Include the 100% scaled resources in components_tests_pak so that the
    autofill IBAN images are available for tests, and remove the workarounds
    in tests introduced to handle the lack of images.
    
    Bug: 432395473
    Change-Id: I1bef5fcbde8b352d4d456ea74469780f012d6a21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6768373
    Reviewed-by: Olivia Saul <osaul@google.com>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488995}

--- components/autofill/core/browser/payments/iban_manager_unittest.cc
+++ components/autofill/core/browser/payments/iban_manager_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/resource/mock_resource_bundle_delegate.h"
 #include "ui/gfx/image/image_unittest_util.h"
 
 using testing::_;
@@ -84,17 +83,6 @@ class IbanManagerTest : public testing::Test,
     test_api(*form_structure_).SetFieldTypes({IBAN_VALUE});
     autofill_field_ = form_structure_->field(0);
 
-    ui::ResourceBundle::InitSharedInstanceWithLocale(
-        "en-US", &mock_resource_delegate_,
-        ui::ResourceBundle::DO_NOT_LOAD_COMMON_RESOURCES);
-    if (IsNewFopDisplayEnabled()) {
-      ON_CALL(mock_resource_delegate_, GetImageNamed(IDR_AUTOFILL_IBAN))
-          .WillByDefault(testing::Return(gfx::test::CreateImage(100, 50)));
-    } else {
-      ON_CALL(mock_resource_delegate_, GetImageNamed(IDR_AUTOFILL_IBAN_OLD))
-          .WillByDefault(testing::Return(gfx::test::CreateImage(100, 50)));
-    }
-
     ON_CALL(*autofill_client_.GetAutofillOptimizationGuide(),
             ShouldBlockSingleFieldSuggestions)
         .WillByDefault(testing::Return(false));
@@ -106,10 +94,6 @@ class IbanManagerTest : public testing::Test,
 #else
     return GetParam();
 #endif
-  }
-
-  void TearDown() override {
-    ui::ResourceBundle::CleanupSharedInstance();
   }
 
   // Sets up the TestPersonalDataManager with a local IBAN.
@@ -186,8 +170,6 @@ class IbanManagerTest : public testing::Test,
   raw_ptr<AutofillField> autofill_field_;
   IbanManager iban_manager_{
       &autofill_client_.GetPersonalDataManager().payments_data_manager()};
-  testing::NiceMock<ui::MockResourceBundleDelegate> mock_resource_delegate_;
-  ui::ResourceBundle::SharedInstanceSwapperForTesting resource_bundle_swapper_;
   base::test::ScopedFeatureList feature_list_metadata_;
 };
 
