047754554e2796616896e2d1945d733c3f6eb76d
Colin Blundell <blundell@chromium.org>
2025-07-17 23:54:34 -0700

    [//gpu] Remove GpuChannel::GetGpuMemoryBufferHandleInfo()
    Nothing calls this method.
    
    Bug: 404905709
    Change-Id: If9b9bac09e0e28cffb74c6e8e8f2771747c993e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764818
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488671}

--- gpu/ipc/service/gpu_channel.h
+++ gpu/ipc/service/gpu_channel.h
@@ -151,9 +151,6 @@ class GPU_IPC_SERVICE_EXPORT GpuChannel : public IPC::Listener,
   // scheduled by the scheduler.
   void ExecuteDeferredRequest(mojom::DeferredRequestParamsPtr params,
                               FenceSyncReleaseDelegate* release_delegate);
-  void GetGpuMemoryBufferHandleInfo(
-      const gpu::Mailbox& mailbox,
-      mojom::GpuChannel::GetGpuMemoryBufferHandleInfoCallback callback);
   void PerformImmediateCleanup();
 
   void WaitForTokenInRange(
