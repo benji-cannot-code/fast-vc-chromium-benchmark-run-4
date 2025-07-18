6cdf6f39be4568f453d88eec5972fbe40e94d7f9
Adem Derinel <derinel@google.com>
2025-07-18 07:20:09 -0700

    Cleanup feature flag "suppress-autofill-via-accessibility"
    Change-Id: I8156bd323fcbe53d0393cba78bd246e2d553082d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769283
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Auto-Submit: Adem Derinel <derinel@google.com>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488839}

--- components/android_autofill/browser/android_autofill_features.h
+++ components/android_autofill/browser/android_autofill_features.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill::features {
 
-BASE_DECLARE_FEATURE(kAndroidAutofillDeprecateAccessibilityApi);
-
 BASE_DECLARE_FEATURE(kAutofillVirtualViewStructureAndroidInCct);
 
 BASE_DECLARE_FEATURE(kAutofillVirtualViewStructureAndroidPasskeyLongPress);
