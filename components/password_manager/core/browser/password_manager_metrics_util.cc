b26cda15dc8906855c40c6571e330770fb78595f
Fiorella Barrientos Villalta <fiorellab@google.com>
2025-07-18 06:06:38 -0700

    [APC] Record UMA metric when annotated page context capture fails.
    This CL adds and records a new UMA metric,
    `PasswordManager.PasswordChange.FailedCapturingPageContent`, to track
    failures in capturing annotated page content during the steps of the
    password change flow.
    
    Bug: 413318086
    Change-Id: I9f7eecf8e577dccbd05898bcfd0a384c34f0141e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734907
    Commit-Queue: Fiorella Barrientos Villalta <fiorellab@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488805}

--- components/password_manager/core/browser/password_manager_metrics_util.cc
+++ components/password_manager/core/browser/password_manager_metrics_util.cc
@@ -667,4 +667,9 @@ void LogCumulativeGetCredentialsMetrics(
       error == password_manager::CredentialManagerError::SUCCESS);
 }
 
+void LogPageContentCaptureFailure(PasswordChangeFlowStep step) {
+  base::UmaHistogramEnumeration(
+      "PasswordManager.PasswordChange.FailedCapturingPageContent", step);
+}
+
 }  // namespace password_manager::metrics_util
