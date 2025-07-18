1ad0c8d8e8a9a5f3a64c327df40f8b61370f7fc8
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-07-18 13:09:12 -0700

    [Desktop Android] Add a stub TabListInterface::From() implementation
    Add a stub implementation for TabListInterface::From() that returns
    nullptr. We're almost to the point where we can implement this, but
    still need the implementation of an UnownedUserDataHost in
    AndroidBrowserWindow. For now, add a stub implementation so we can
    unblock compiling other code that will use this.
    
    Bug: 415961057
    Change-Id: I9d77762248753350ecc0089e8ce084e1a7dbb4e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6770809
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489014}

--- chrome/browser/ui/android/tab_model/tab_model.cc
+++ chrome/browser/ui/android/tab_model/tab_model.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 
 #include "base/metrics/histogram_functions.h"
+#include "base/notimplemented.h"
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
@@ -127,4 +128,14 @@ void TabModel::RecordActualSyncedTabsHistogram() {
   int percent_synced = synced_tabs_count * 100 / eligible_tabs_count;
   base::UmaHistogramPercentage("Android.Sync.ActualSyncedTabCountPercentage",
                                percent_synced);
+}
+
+// static
+// From //chrome/browser/ui/tabs/tab_list_interface.h
+TabListInterface* TabListInterface::From(
+    BrowserWindowInterface* browser_window_interface) {
+  // TODO(https://crbug.com/415961057): Implement this once AndroidBrowserWindow
+  // has a get-able UnownedUserDataHost.
+  NOTIMPLEMENTED();
+  return nullptr;
 }
