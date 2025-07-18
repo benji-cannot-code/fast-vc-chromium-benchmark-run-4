7e31c91492e5e2580be2bfee54b6059dbd352efd
Weidong Liu <weidongliu@chromium.org>
2025-07-17 23:06:23 -0700

    Temporarily relax the check constraints on ReadBits.
    base::checked_cast is too strict now and may cause too many crashes
    affecting users. Let's relax this check until we change the constraint
    to base::bits::UnsignedInteger.
    
    Bug: 432404775
    Change-Id: I67cb207a063d73e562484a42550afbe17a450144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769020
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488655}

--- media/base/bit_reader.h
+++ media/base/bit_reader.h
@@ -58,7 +58,7 @@ class MEDIA_EXPORT BitReader {
     uint64_t temp = 0;
     bool ret = ReadBitsInternal(num_bits, &temp);
     if (ret) {
-      *out = base::checked_cast<T>(temp);
+      *out = static_cast<T>(temp);
     }
     return ret;
   }
