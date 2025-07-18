786c6302ca4019fac0c373ad56e050a615b371d6
Gauthier Ambard <gambard@chromium.org>
2025-07-18 08:24:25 -0700

    [iOS][Diamond] UI polish on AppBar
    Update the background color and the corner radius of the animations.
    
    Also make sure that the omnibox default state is correct.
    
    Bug: 429955447
    Change-Id: Idd75a733ea9098af98d0f984701e44f3f9286839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769545
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488868}

--- ios/chrome/browser/main/ui_bundled/bvc_container_view_controller.mm
+++ ios/chrome/browser/main/ui_bundled/bvc_container_view_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check_op.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/browser/shared/public/prototypes/diamond/utils.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 
 @interface BVCContainerViewController ()
@@ -62,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)viewDidLoad {
   [super viewDidLoad];
   if (IsDiamondPrototypeEnabled()) {
-    self.view.layer.cornerRadius = 18;
+    self.view.layer.cornerRadius = kDiamondBrowserCornerRadius;
     self.view.layer.maskedCorners =
         kCALayerMinXMaxYCorner | kCALayerMaxXMaxYCorner;
     self.view.layer.masksToBounds = YES;
