3ce2358b782ec7768edf12ec06932ea93e82e4dc
Brijesh Giri <brijeshvgiri@gmail.com>
2025-07-18 14:36:33 -0700

    [DebugUITabStrip] Add backend to serve tab-strip-internals
    This CL introduces Phase 1 of the 'tab-strip-internals' page, a debug
    WebUI intended to visualize the live state of the TabStrip model.
    
    Changes:
    - Added WebUI folder 'tab-strip-internals' in chrome/browser/ui/webui/
    - Updated BUILD.gn in chrome/browser/ui to register tab_strip_internals_ui
    - Updated chrome_web_ui_configs.cc to register TabStripInternalsUIConfig
    - Updated webui_url_constants.h in chrome/common to define WebUI URL
    
    Future phases will render the live state of the TabStrip model on the
    'tab-strip-internals' page.
    
    Project doc:
    https://docs.google.com/document/d/1EKJicM4grBpp2nA4SOPzUvsKVLa_Ndj5L0HrIsGJNUI
    
    Bug: 427204855
    Change-Id: I03482388617438b1313a3df066c721025d4b4f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6684695
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Brijesh Giri <brijeshvgiri@gmail.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489049}

--- chrome/browser/ui/webui/chrome_web_ui_configs.cc
+++ chrome/browser/ui/webui/chrome_web_ui_configs.cc
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/inspect/inspect_ui.h"
 #if BUILDFLAG(ENABLE_SESSION_SERVICE)
 #include "chrome/browser/ui/webui/internals/internals_ui.h"
+#include "chrome/browser/ui/webui/tab_strip_internals/tab_strip_internals_ui.h"
 #endif  // BUILDFLAG(ENABLE_SESSION_SERVICE)
 #include "chrome/browser/ui/webui/management/management_ui.h"
 #include "chrome/browser/ui/webui/media_router/media_router_internals_ui.h"
@@ -310,6 +311,7 @@ void RegisterChromeWebUIConfigs() {
   map.AddWebUIConfig(std::make_unique<InspectUIConfig>());
 #if BUILDFLAG(ENABLE_SESSION_SERVICE)
   map.AddWebUIConfig(std::make_unique<InternalsUIConfig>());
+  map.AddWebUIConfig(std::make_unique<TabStripInternalsUIConfig>());
 #endif  // BUILDFLAG(ENABLE_SESSION_SERVICE)
   map.AddWebUIConfig(std::make_unique<ManagementUIConfig>());
   map.AddWebUIConfig(
