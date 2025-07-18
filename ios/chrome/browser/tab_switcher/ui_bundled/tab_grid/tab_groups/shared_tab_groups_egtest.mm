ca7988a2e0a1811799a4f7e9aff499b049fdbc4e
Asami Doi <asamidoi@chromium.org>
2025-07-18 02:38:31 -0700

    [ios] Fix SharedTabGroupsTestCase on iOS 26
    On iOS 26, tapping the overflow menu button again to close the overflow
    menu doesn't work well. It seems the area of the overflow menu is
    slightly overlapped with the area of the face pile button, which causes
    opening the share/manage page. See the screen recording:
    http://screencast/cast/NjYyMDcxNjQ1NDY0MTY2NHwwMjNjNjMzNC1kZA
    
    This CL fixes it by tapping another area to close the overflow menu.
    
    Fixed: 429600189, 429600196
    Change-Id: Id6ef45f16efb7764e419354483d001db161bc609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764754
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488725}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_egtest.mm
@@ -340,17 +340,13 @@ void WaitForFakeJoinFlowView() {
       performAction:grey_tap()];
 
   // Open the menu and check elements while not shared.
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(
-                                   kTabGroupOverflowMenuButtonIdentifier)]
+  [[EarlGrey selectElementWithMatcher:TabGroupOverflowMenuButton()]
       performAction:grey_tap()];
-  [[EarlGrey
-      selectElementWithMatcher:ContextMenuItemWithAccessibilityLabelId(
-                                   IDS_IOS_CONTENT_CONTEXT_SHARELOCALGROUP)]
+  [[EarlGrey selectElementWithMatcher:ShareGroupButton()]
       assertWithMatcher:grey_sufficientlyVisible()];
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(
-                                   kTabGroupOverflowMenuButtonIdentifier)]
+
+  // Close the menu by tapping outside the menu area.
+  [[EarlGrey selectElementWithMatcher:CloseTabGroupButton()]
       performAction:grey_tap()];
 
   // Tap on the face pile to share the group.
@@ -369,21 +365,15 @@ void WaitForFakeJoinFlowView() {
   [ChromeEarlGrey waitForUIElementToDisappearWithMatcher:FakeShareFlowView()];
 
   // Open the menu and check elements while shared.
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(
-                                   kTabGroupOverflowMenuButtonIdentifier)]
+  [[EarlGrey selectElementWithMatcher:TabGroupOverflowMenuButton()]
       performAction:grey_tap()];
-  [[EarlGrey
-      selectElementWithMatcher:ContextMenuItemWithAccessibilityLabelId(
-                                   IDS_IOS_CONTENT_CONTEXT_MANAGESHAREDGROUP)]
+  [[EarlGrey selectElementWithMatcher:ManageGroupButton()]
       assertWithMatcher:grey_sufficientlyVisible()];
-  [[EarlGrey
-      selectElementWithMatcher:ContextMenuItemWithAccessibilityLabelId(
-                                   IDS_IOS_CONTENT_CONTEXT_RECENTACTIVITY)]
+  [[EarlGrey selectElementWithMatcher:RecentActivityButton()]
       assertWithMatcher:grey_sufficientlyVisible()];
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(
-                                   kTabGroupOverflowMenuButtonIdentifier)]
+
+  // Close the menu by tapping outside the menu area.
+  [[EarlGrey selectElementWithMatcher:CloseTabGroupButton()]
       performAction:grey_tap()];
 
   // Tap on the face pile to manage the group.
