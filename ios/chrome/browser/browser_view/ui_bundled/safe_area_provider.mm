603dbfe38cf94621fbf23e04ff03623ad70c412d
Ewann Pelle <ewannpv@chromium.org>
2025-07-18 07:58:18 -0700

    [iOS][26] Updates fullscreen height on iPad
    Demo:
    https://drive.google.com/file/d/1uQ3lsi1BAatU6ZYHGkTEaKf80InvJKRn/view?usp=sharing&resourcekey=0-Mg6Cuk6CmUP4PFcNpKM_oQ
    
    Fixed: 432245003
    Change-Id: I9e4025d288af7d56599210c6fd4bb81ca68582ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6759838
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488856}

--- ios/chrome/browser/browser_view/ui_bundled/safe_area_provider.mm
+++ ios/chrome/browser/browser_view/ui_bundled/safe_area_provider.mm
@@ -34,8 +34,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (UIEdgeInsets)safeArea {
-  return self.sceneState.window.rootViewController.view.safeAreaInsets;
+  return self.rootView.safeAreaInsets;
 }
+
+#if defined(__IPHONE_26_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_26_0
+- (NSDirectionalEdgeInsets)directionalEdgeInsetsForLayoutRegion:
+    (UIViewLayoutRegion*)layoutRegion API_AVAILABLE(ios(26.0)) {
+  return [self.rootView directionalEdgeInsetsForLayoutRegion:layoutRegion];
+}
+#endif
 
 #pragma mark - Private properties
 
@@ -45,6 +52,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return nil;
   }
   return browser->GetSceneState();
+}
+
+// Root view of the window.
+- (UIView*)rootView {
+  return self.sceneState.window.rootViewController.view;
 }
 
 @end
