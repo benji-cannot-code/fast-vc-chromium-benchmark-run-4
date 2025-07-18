a2475aa7861ba1f74404b7dcc8e7b13acb38c31c
Vishwas Uppoor <vishwasuppoor@google.com>
2025-07-18 12:24:43 -0700

    [PixAL] Update DeviceDelegateAndroidTest setup
    ApplicationStatusListener provides a static method to simulate changes
    in the app status which triggers the callback passed by the callers.
    Currently, the app status changes are simulated by directly triggering
    the callback which is not ideal.
    
    Use NotifyApplicationStateChange to simulate app status changes in
    DeviceDelegateAndroidTest.
    
    Bug: 425191286
    Change-Id: Ie9cf247c02bb938a6b9c34df1ee693f4b002c543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6758693
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488993}

--- components/facilitated_payments/android/device_delegate_android_test_api.h
+++ components/facilitated_payments/android/device_delegate_android_test_api.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/application_status_listener.h"
 #include "base/check_deref.h"
+#include "base/functional/callback.h"
 #include "base/memory/raw_ref.h"
 #include "components/facilitated_payments/android/device_delegate_android.h"
 
@@ -22,10 +23,12 @@ class DeviceDelegateAndroidTestApi {
       delete;
   ~DeviceDelegateAndroidTestApi() = default;
 
-  // Calls the underlying DeviceDelegateAndroid's private methods.
-  void OnApplicationStateChanged(base::android::ApplicationState state) {
-    delegate_->OnApplicationStateChanged(state);
+  void SetOnApplicationStateChangedCallbackForTesting(
+      base::OnceClosure callback) {
+    delegate_->on_application_state_changed_callback_for_testing_ =
+        std::move(callback);
   }
+
   base::android::ApplicationStatusListener* app_status_listener() {
     return delegate_->app_status_listener_.get();
   }
