b581417a499719ab2c97dfd510ef434ccd9c2499
James Cook <jamescook@chromium.org>
2025-07-18 11:07:24 -0700

    extensions: Remove //c/b/e/desktop_android directory
    We've migrated to using the existing ChromeFooClient interfaces instead
    of DesktopAndroidFooClient. This directory now only contains smoke tests
    for desktop Android. Move the test file up a level and delete the
    directory.
    
    Bug: none
    Test: android_browsertests
    Change-Id: Icd8b071503deb93647ad53c720b97dec15c4a771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6770826
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488955}

--- chrome/browser/extensions/desktop_android_extensions_browsertest.cc
+++ chrome/browser/extensions/desktop_android_extensions_browsertest.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+// Smoke tests for the experimental desktop Android build, which supports
+// extensions. See https://crbug.com/356905053
 class DesktopAndroidExtensionsBrowserTest : public AndroidBrowserTest {
  public:
   DesktopAndroidExtensionsBrowserTest() = default;
