e241234ae8e8894c833550a9417c472829eb6537
Sylvain Defresne <sdefresne@chromium.org>
2025-07-18 01:45:59 -0700

    [ios] Use WebState's unique identifier for BreadcrumbManagerTabHelper
    When the feature kCreateTabHelperOnlyForRealizedWebStates is enabled,
    tab helpers are not created for unrealized WebState. Use the WebState
    identifier as the identifier for the breadcrumbs as they are unique.
    
    This changes the output of the breadcrumbs since the tab identifier
    will no longer be starting at one, so instead of "Tab 1", "Tab 2", ...
    the breadcrumbs will use e.g. "Tab 1532320382", "Tab 1532321492", ...
    
    Fixed: 431697948
    Change-Id: I0ebe2e61938de5b661bf0fed3635266781eaa4ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6765069
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488711}

--- ios/chrome/browser/crash_report/model/breadcrumbs/breadcrumb_manager_browser_agent.mm
+++ ios/chrome/browser/crash_report/model/breadcrumbs/breadcrumb_manager_browser_agent.mm
@@ -21,11 +21,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/tab_group_range.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
+#import "ios/web/public/web_state.h"
+#import "ios/web/public/web_state_id.h"
 
 namespace {
 
-int GetTabId(const web::WebState* const web_state) {
-  return BreadcrumbManagerTabHelper::FromWebState(web_state)->GetUniqueId();
+// Returns the identifier for `web_state`.
+int GetTabId(const web::WebState* web_state) {
+  const web::WebStateID web_state_id = web_state->GetUniqueIdentifier();
+  if (auto* tab_helper = BreadcrumbManagerTabHelper::FromWebState(web_state)) {
+    // If the tab helper has been created, it should be using the WebState
+    // unique identifier too, so check this is the case.
+    const int identifier = tab_helper->GetUniqueId();
+    CHECK_EQ(identifier, web_state_id.identifier());
+    return identifier;
+  }
+
+  return web_state_id.identifier();
 }
 
 }  // namespace
