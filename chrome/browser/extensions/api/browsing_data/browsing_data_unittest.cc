46b7d5dd807ac230b5eba841341e722ceefec01c
Christian Dullweber <dullweber@chromium.org>
2025-07-18 12:01:52 -0700

    BrowsingData: Fix parsing 'since' == 0 in extension API
    Passing zero as starting time should be converted to base::Time() to
    ensure that logic that clears all data is triggered correctly.
    
    Bug: 432200328
    Change-Id: I0a622a0cf1f8d2a728d899bb363fc976ac59792a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769725
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488985}

--- chrome/browser/extensions/api/browsing_data/browsing_data_unittest.cc
+++ chrome/browser/extensions/api/browsing_data/browsing_data_unittest.cc
@@ -296,7 +296,7 @@ class BrowsingDataApiTest : public ExtensionServiceTestBase {
   void VerifyFilterBuilder(const std::string& options,
                            content::BrowsingDataFilterBuilder* filter_builder) {
     delegate()->ExpectCall(
-        base::Time::UnixEpoch(), base::Time::Max(),
+        base::Time(), base::Time::Max(),
         content::BrowsingDataRemover::DATA_TYPE_LOCAL_STORAGE, UNPROTECTED_WEB,
         filter_builder);
     auto function = base::MakeRefCounted<BrowsingDataRemoveFunction>();
@@ -617,7 +617,7 @@ TEST_F(BrowsingDataApiTest, RemoveCookiesWithFilter) {
   auto filter_builder = content::BrowsingDataFilterBuilder::Create(
       content::BrowsingDataFilterBuilder::Mode::kPreserve);
   filter_builder->AddRegisterableDomain("example.com");
-  delegate()->ExpectCall(base::Time::UnixEpoch(), base::Time::Max(),
+  delegate()->ExpectCall(base::Time(), base::Time::Max(),
                          content::BrowsingDataRemover::DATA_TYPE_COOKIES,
                          UNPROTECTED_WEB, filter_builder.get());
 
@@ -636,7 +636,7 @@ TEST_F(BrowsingDataApiTest, RemoveCookiesAndStorageWithFilter) {
   auto filter_builder1 = content::BrowsingDataFilterBuilder::Create(
       content::BrowsingDataFilterBuilder::Mode::kDelete);
   filter_builder1->AddRegisterableDomain("example.com");
-  delegate()->ExpectCall(base::Time::UnixEpoch(), base::Time::Max(),
+  delegate()->ExpectCall(base::Time(), base::Time::Max(),
                          content::BrowsingDataRemover::DATA_TYPE_COOKIES,
                          UNPROTECTED_WEB, filter_builder1.get());
 
@@ -644,7 +644,7 @@ TEST_F(BrowsingDataApiTest, RemoveCookiesAndStorageWithFilter) {
       content::BrowsingDataFilterBuilder::Mode::kDelete);
   filter_builder2->AddOrigin(
       url::Origin::Create(GURL("http://www.example.com")));
-  delegate()->ExpectCall(base::Time::UnixEpoch(), base::Time::Max(),
+  delegate()->ExpectCall(base::Time(), base::Time::Max(),
                          content::BrowsingDataRemover::DATA_TYPE_LOCAL_STORAGE,
                          UNPROTECTED_WEB, filter_builder2.get());
 
