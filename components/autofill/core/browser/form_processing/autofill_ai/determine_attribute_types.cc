9b893684069f79710e6d2e82dde6f1f79fca439a
Jan Keitel <jkeitel@google.com>
2025-07-18 06:11:18 -0700

    Use attribute types instead of AutofillAI types to determine relevance
    Using only AutofillAI types instead of attribute assignment is
    incorrect in two ways:
    - It mistakenly classifies forms as relevant even if they only have
      *_NAME predictions.
    - It mistakenly classifies forms as relevant even if the entities
      that could be filled on those forms are disabled.
    
    Bug: 429109328
    Change-Id: I8c943ba99a6c829ea66d35b7cb510ef5280be60b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769681
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1488807}

--- components/autofill/core/browser/form_processing/autofill_ai/determine_attribute_types.cc
+++ components/autofill/core/browser/form_processing/autofill_ai/determine_attribute_types.cc
@@ -294,4 +294,10 @@ SectionMap DetermineAttributeTypes(
   return r;
 }
 
+bool AreFieldsRelevantForAutofillAi(
+    base::span<const std::unique_ptr<AutofillField>> fields) {
+  return !std::ranges::all_of(GetAttributeTypes(fields),
+                              &DenseSet<AttributeType>::empty);
+}
+
 }  // namespace autofill
