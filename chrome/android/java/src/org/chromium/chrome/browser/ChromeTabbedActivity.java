067e4cb3907c327ec5f7287b116c71033b3a5500
Yue Zhang <yuezhanggg@chromium.org>
2025-07-18 15:27:27 -0700

    [tab suggestions] Block bottom sheet UI by default
    Bug: 432796257
    Change-Id: If1055afa3b3025175a361b1e2df64c2398d0f70b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771525
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489067}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1396,7 +1396,7 @@ public class ChromeTabbedActivity extends ChromeActivity {
                 if (ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
                         ChromeFeatureList.GROUP_SUGGESTION_SERVICE,
                         GroupSuggestionsPromotionCoordinator.CREATE_SUGGESTIONS_PROMOTION_UI_PARAM,
-                        true)) {
+                        false)) {
                     mGroupSuggestionsPromotionCoordinator =
                             new GroupSuggestionsPromotionCoordinator(
                                     this,
