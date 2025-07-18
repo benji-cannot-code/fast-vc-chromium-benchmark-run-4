6a8f2a4cde5649036d3ce91ab2b203397f3c8bef
Sana Akbani <sanaakbani@google.com>
2025-07-18 09:57:00 -0700

    Flip Closed Captions to enabled by default
    Bug: 421186711
    Change-Id: If6e49ca9a096235b6a11d63ee8f32a5612ac5dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6770825
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488912}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -650,7 +650,7 @@ BASE_FEATURE(kGlicUserStatusCheck,
 
 BASE_FEATURE(kGlicClosedCaptioning,
              "GlicClosedCaptioning",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGlicUnloadOnClose,
              "GlicUnloadOnClose",
