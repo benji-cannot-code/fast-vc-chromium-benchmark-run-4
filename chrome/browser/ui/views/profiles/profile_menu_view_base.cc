f2eed0e0e771001eda136f3e2768ee96084f1271
Thomas Lukaszewicz <tluk@chromium.org>
2025-07-17 19:19:18 -0700

    [bedrock] Null-check anchor_view_ in ProfileMenuViewBase
    anchor_view_ typically belongs to a different View hierarchy to
    ProfileMenuViewBase and there is a risk that the host is
    destroyed before the menu.
    
    This CL mitigates this risk by tracking and null-checking the
    anchor as appropriate.
    
    Bug: 413168662
    Change-Id: Ie90960f4d5969f424f23a5edf3df811ee5ced6d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6767692
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488616}

--- chrome/browser/ui/views/profiles/profile_menu_view_base.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view_base.cc
@@ -750,11 +750,11 @@ void ProfileMenuViewBase::Init() {
 }
 
 void ProfileMenuViewBase::OnWindowClosing() {
-  if (!anchor_button()) {
+  if (!anchor_button_) {
     return;
   }
 
-  views::InkDrop::Get(anchor_button())
+  views::InkDrop::Get(anchor_button_.view())
       ->AnimateToState(views::InkDropState::DEACTIVATED, nullptr);
 }
 
