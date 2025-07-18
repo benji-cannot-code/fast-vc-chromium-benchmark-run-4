c570d0be60a48af10887d7fb154ce549d0de55ea
Kene Okoye <kenok@google.com>
2025-07-18 13:39:16 -0700

    [AUSM] Disable GlicActorUi feature by default and gate UI logic with it
    Bug: 432315310,425952887
    Change-Id: Id439df307487ed923c3489ad60f0d16b0d33fae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6760605
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489023}

--- chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
+++ chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
@@ -2,10 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #include "chrome/browser/actor/ui/actor_ui_state_manager.h"
 
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/actor_keyed_service_factory.h"
 #include "chrome/browser/actor/execution_engine.h"
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/actor.mojom-forward.h"
 #include "chrome/common/actor/action_result.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/tabs/public/mock_tab_interface.h"
@@ -91,6 +92,9 @@ class ActorUiStateManagerTest : public testing::Test {
 
   // testing::Test:
   void SetUp() override {
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{features::kGlicActorUi},
+        /*disabled_features=*/{});
     profile_ = TestingProfile::Builder()
                    .AddTestingFactory(
                        ActorKeyedServiceFactory::GetInstance(),
@@ -140,6 +144,7 @@ class ActorUiStateManagerTest : public testing::Test {
  private:
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfile> profile_;
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(ActorUiStateManagerTest, NoTask_ReturnsInactiveUiState) {
