1d7757ccba841fb641fd7e3dcc07da0c7b7e4286
David Sanders <dsanders11@ucsbalum.com>
2025-07-19 01:30:01 -0700

    Use more specific include for perfetto::NamedTrack
    The include was added in
    https://chromium-review.googlesource.com/c/chromium/src/+/6490487 and
    adds a significant amount to the total compiler input size.
    
    Should save ~145 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: I3fe4ae5b3ff2a0136d61a79cd27e7d1425bc28a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772132
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1489170}

--- net/base/network_change_notifier.h
+++ net/base/network_change_notifier.h
@@ -16,10 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list_threadsafe.h"
 #include "base/strings/cstring_view.h"
 #include "base/time/time.h"
-#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "net/base/net_export.h"
 #include "net/base/network_handle.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "net/base/address_map_linux.h"
