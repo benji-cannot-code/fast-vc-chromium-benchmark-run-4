7f1e9c6db32eaf7c57061685bf7b7eccbd1aab43
David Sanders <dsanders11@ucsbalum.com>
2025-07-18 18:06:28 -0700

    Remove unused include in cast_socket_service.h
    Should save ~27 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: I5c6795fd75be2bf58e420f65ea989f9cf0cc10d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6762355
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1489119}

--- components/media_router/common/providers/cast/channel/cast_socket_service.h
+++ components/media_router/common/providers/cast/channel/cast_socket_service.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "components/media_router/common/providers/cast/channel/cast_socket.h"
 #include "services/network/public/cpp/network_context_getter.h"
-#include "services/network/public/mojom/network_context.mojom.h"
 
 namespace cast_channel {
 
