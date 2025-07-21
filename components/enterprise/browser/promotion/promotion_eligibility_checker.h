83a8d345e262f6ecaf2ccdcd2c44cc46e82b8db4
David Sanders <dsanders11@ucsbalum.com>
2025-07-21 13:05:14 -0700

    Forward declare CloudPolicyClient in promotion_eligibility_checker.h
    Should save ~20 MiB of compiler input size.
    
    Bug: 429365675
    Change-Id: I3a75555de54f132a2c2ff89cf16715daae1de2e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771504
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489735}

--- components/enterprise/browser/promotion/promotion_eligibility_checker.h
+++ components/enterprise/browser/promotion/promotion_eligibility_checker.h
@@ -11,13 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "components/policy/core/common/cloud/cloud_policy_client.h"
+#include "base/memory/weak_ptr.h"
+#include "components/policy/proto/device_management_backend.pb.h"
 #include "components/signin/public/identity_manager/access_token_fetcher.h"
 
+namespace policy {
+class CloudPolicyClient;
+}  // namespace policy
+
 namespace signin {
-
 class IdentityManager;
-
 }  // namespace signin
 
 namespace enterprise_promotion {
