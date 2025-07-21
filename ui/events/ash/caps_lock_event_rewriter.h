3049c4e07bb5713f5e0e3c07288b40becabb1b13
Bao-Duy Tran <tranbaoduy@chromium.org>
2025-07-21 12:41:21 -0700

    Rename RightAlt->QuickInsert in CapsLockEventRewriter comment.
    This should've been done together in
    https://crrev.com/c/5994078
    but was accidentally missed out.
    
    Bug: None
    Change-Id: If3bd20f8959f68af9f67dd2d6a577819c666a1b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774421
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Auto-Submit: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489719}

--- ui/events/ash/caps_lock_event_rewriter.h
+++ ui/events/ash/caps_lock_event_rewriter.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-// CapsLockEventRewriter rewrites Fn + RightAlt events into CapsLock.
+// CapsLockEventRewriter rewrites Fn + QuickInsert events into CapsLock.
 class CapsLockEventRewriter : public EventRewriter {
  public:
   struct RemappedKey {
