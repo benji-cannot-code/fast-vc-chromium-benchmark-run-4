2ff1d718fd460940c9fa6cff85fdebe23e3c4d0a
Jack Thiesen <jthiesen@chromium.org>
2025-07-21 07:18:58 -0700

    Enterprise Policies Can Be Recommendations on Android
    Enterprise admins gain the ability to recommend settings alongside existing policy management for Android Devices. Users can override the recommendations.
    
    Example:
    https://screenshot.googleplex.com/374HiUATw75A7vc.png
    
    Bug: 429018995
    Bug: 400800634
    Bug: 400804628
    Change-Id: I6ee8e102eb14fb34ff5abeb9b44679a6ec6a42f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695869
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489501}

--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ChromeBasePreferenceTest.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ChromeBasePreferenceTest.java
@@ -137,7 +137,7 @@ public class ChromeBasePreferenceTest {
         onView(withId(android.R.id.title)).check(matches(allOf(withText(TITLE), isDisplayed())));
         onView(withId(android.R.id.summary))
                 .check(matches(allOf(withText(R.string.managed_by_your_parent), isDisplayed())));
-        onView(withId(R.id.managed_disclaimer_text)).check(doesNotExist());
+        onView(withId(R.id.managed_disclaimer_text)).check(matches(not(isDisplayed())));
         onView(withId(android.R.id.icon)).check(matches(isDisplayed()));
     }
 
@@ -155,7 +155,7 @@ public class ChromeBasePreferenceTest {
         onView(withId(android.R.id.title)).check(matches(allOf(withText(TITLE), isDisplayed())));
         onView(withId(android.R.id.summary))
                 .check(matches(allOf(withText(R.string.managed_by_your_parents), isDisplayed())));
-        onView(withId(R.id.managed_disclaimer_text)).check(doesNotExist());
+        onView(withId(R.id.managed_disclaimer_text)).check(matches(not(isDisplayed())));
         onView(withId(android.R.id.icon)).check(matches(isDisplayed()));
     }
 
