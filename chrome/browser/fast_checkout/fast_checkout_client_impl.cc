595f24cbf2ce842a0a0d526ee61d010fa853dfe9
Jihad Hanna <jihadghanna@google.com>
2025-07-21 05:33:07 -0700

    Remove kAddressFieldTypes in fast_checkout_client_impl.cc
    Bug: 432827626
    Change-Id: I4466d50c2efc5bbcb24ac5a58223130cd325d0c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6773585
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489462}

--- chrome/browser/fast_checkout/fast_checkout_client_impl.cc
+++ chrome/browser/fast_checkout/fast_checkout_client_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
 #include "components/autofill/core/browser/data_model/addresses/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/payments/credit_card.h"
+#include "components/autofill/core/browser/field_type_utils.h"
 #include "components/autofill/core/browser/integrators/fast_checkout/fast_checkout_enums.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics_utils.h"
 #include "components/autofill/core/browser/payments/credit_card_cvc_authenticator.h"
@@ -48,12 +49,6 @@ constexpr base::TimeDelta kTimeout = base::Minutes(30);
 constexpr auto kSupportedFormTypes = autofill::DenseSet<autofill::FormType>(
     {autofill::FormType::kAddressForm, autofill::FormType::kCreditCardForm});
 
-constexpr auto kAddressFieldTypes =
-    autofill::DenseSet<autofill::FieldTypeGroup>(
-        {autofill::FieldTypeGroup::kName, autofill::FieldTypeGroup::kEmail,
-         autofill::FieldTypeGroup::kPhone, autofill::FieldTypeGroup::kAddress,
-         autofill::FieldTypeGroup::kCompany});
-
 bool IsVisibleTextField(const autofill::AutofillField& field) {
   return field.IsFocusable() && field.IsTextInputElement();
 }
@@ -64,7 +59,7 @@ const autofill::AutofillField* GetFieldToFill(
   for (const std::unique_ptr<autofill::AutofillField>& field : fields) {
     if (IsVisibleTextField(*field) && field->value().empty() &&
         ((!is_credit_card_form &&
-          kAddressFieldTypes.contains(field->Type().group())) ||
+          autofill::IsAddressType(field->Type().GetStorableType())) ||
          (is_credit_card_form &&
           field->Type().GetStorableType() == autofill::CREDIT_CARD_NUMBER))) {
       return field.get();
