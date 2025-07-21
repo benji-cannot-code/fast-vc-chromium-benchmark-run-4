66ee7d8bcf085a8f71c1136c8102b7d5caa72d1d
Kevin Graney <kmg@google.com>
2025-07-21 10:59:44 -0700

    Change StoppedActingOnTab to a SyncUiEvent
    This is in preparation for dispatching it when tasks finish.
    
    Bug: 425784083
    Change-Id: I311a632a0d279515ef4be2b071588ca48ecc1d5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774228
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489652}

--- chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
+++ chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/ui/event_dispatcher.h"
 #include "chrome/browser/actor/ui/mock_actor_ui_tab_controller.h"
 #include "chrome/browser/actor/ui/mock_event_dispatcher.h"
+#include "chrome/browser/actor/ui/ui_event.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/actor.mojom-forward.h"
 #include "chrome/common/actor/action_result.h"
@@ -364,6 +365,11 @@ class ActorUiStateManagerUiEventUiTabScopedTest
  public:
   void VerifyUiEvent(AsyncUiEvent event, UiTabState expected_state) {
     OnUiEventComplete(event);
+    EXPECT_EQ(actor_ui_state_manager()->GetUiTabState(), expected_state);
+  }
+
+  void VerifyUiEvent(SyncUiEvent event, UiTabState expected_state) {
+    actor_ui_state_manager()->OnUiEvent(event);
     EXPECT_EQ(actor_ui_state_manager()->GetUiTabState(), expected_state);
   }
 
