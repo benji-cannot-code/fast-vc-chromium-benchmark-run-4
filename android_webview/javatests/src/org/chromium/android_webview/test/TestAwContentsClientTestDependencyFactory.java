599deece39519d9de06ce210e644233481a93126
Ho Cheung <hocheung@chromium.org>
2025-07-21 09:32:05 -0700

    [aw] Extract TestAwContentsClientTestDependencyFactory into shared file
    Extracts the duplicate TestAwContentsClientTestDependencyFactory class
    from ContextMenuTest and LoadUrlTest into a separate shared file to
    eliminate code duplication.
    
    Bug: 382654667
    Change-Id: Iaf307a714690c13031b4a537960b274b539e15ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6773536
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489587}

---
+++ android_webview/javatests/src/org/chromium/android_webview/test/TestAwContentsClientTestDependencyFactory.java
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.test;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import org.chromium.android_webview.AwBrowserContext;
+import org.chromium.android_webview.AwContents;
+import org.chromium.android_webview.AwContents.DependencyFactory;
+import org.chromium.android_webview.AwContents.InternalAccessDelegate;
+import org.chromium.android_webview.AwContentsClient;
+import org.chromium.android_webview.AwSettings;
+import org.chromium.android_webview.gfx.AwDrawFnImpl;
+
+/**
+ * A test dependency factory that creates TestAwContents instances instead of regular AwContents.
+ * This allows tests to use the enhanced TestAwContents class which provides additional testing
+ * capabilities.
+ */
+public class TestAwContentsClientTestDependencyFactory
+        extends AwActivityTestRule.TestDependencyFactory {
+    @Override
+    public AwContents createAwContents(
+            AwBrowserContext browserContext,
+            ViewGroup containerView,
+            Context context,
+            InternalAccessDelegate internalAccessAdapter,
+            AwDrawFnImpl.DrawFnAccess drawFnAccess,
+            AwContentsClient contentsClient,
+            AwSettings settings,
+            DependencyFactory dependencyFactory) {
+        return new TestAwContents(
+                browserContext,
+                containerView,
+                context,
+                internalAccessAdapter,
+                drawFnAccess,
+                contentsClient,
+                settings,
+                dependencyFactory);
+    }
+}
