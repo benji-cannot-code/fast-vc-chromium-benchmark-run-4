63a378b24e996daa9ea0b34dc9a6faa615854692
Marc Treib <treib@chromium.org>
2025-07-21 01:30:57 -0700

    Introduce feature flag AndroidDataImporterService
    ...including about:flags entry and Java-side CachedFlag.
    Not used for anything yet.
    
    Bug: 431218724
    Change-Id: I81edcbe54735555111efb016d71cfe72084003a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6770179
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489397}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2622,6 +2622,9 @@ extern const char kAnimatedImageDragShadowDescription[];
 extern const char kAnimateSuggestionsListAppearanceName[];
 extern const char kAnimateSuggestionsListAppearanceDescription[];
 
+extern const char kAndroidDataImporterServiceName[];
+extern const char kAndroidDataImporterServiceDescription[];
+
 extern const char kAndroidElegantTextHeightName[];
 extern const char kAndroidElegantTextHeightDescription[];
 
