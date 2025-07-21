84324ddcc2dfaa7ae23db40da60c2927e5095836
Andrew Rayskiy <greengrape@google.com>
2025-07-21 10:27:58 -0700

    Break response reader registry's dependency on the WebAppRegistrar
    The information about keys and web bundle ids is already stored in
    the registry cache.
    
    Bug: 432251929
    Change-Id: Ic5cd89825368677dc4919bbe46a51ccc38d04366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772974
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Luke Klimek <zgroza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489629}

--- components/web_package/signed_web_bundles/identity_validator.h
+++ components/web_package/signed_web_bundles/identity_validator.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/types/expected.h"
 #include "base/version.h"
+#include "components/web_package/signed_web_bundles/signed_web_bundle_integrity_block.h"
 #include "components/web_package/signed_web_bundles/types.h"
 
 namespace web_package {
@@ -32,6 +33,10 @@ class IdentityValidator {
   virtual base::expected<void, std::string> ValidateWebBundleIdentity(
       const std::string& web_bundle_id,
       const std::vector<PublicKey>& public_keys) const;
+
+  // Shorthand for calling the above when the caller has an `integrity_block`.
+  base::expected<void, std::string> ValidateWebBundleIdentity(
+      const SignedWebBundleIntegrityBlock& integrity_block) const;
 
  protected:
   IdentityValidator();
