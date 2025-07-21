9449ba8949586e4257edc2b2cb3682554daa1405
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2025-07-21 09:22:23 -0700

    Revert CL for the high confidence crash culprit for http://crash/40d6cb9b9892c546
    The Suspect CL https://chromium-review.googlesource.com/q/Iddba09800b703074a83581c4210b24812a101ef8 is highly likely the cause of the crash. Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Fixed: 432227597
    Change-Id: Ia440b5c93f8c50c85e1fc1e7dd33e66fee731d8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6761050
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489583}

--- net/quic/quic_session_pool_job.cc
+++ net/quic/quic_session_pool_job.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/quic_session_pool_job.h"
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/memory/weak_ptr.h"
 #include "base/trace_event/trace_event.h"
 #include "net/base/completion_once_callback.h"
@@ -62,12 +61,6 @@ QuicSessionPool::Job::~Job() {
 }
 
 void QuicSessionPool::Job::AddRequest(QuicSessionRequest* request) {
-  // We suspect that requests are being added to jobs that are being deleted
-  // which would leave the requests orphaned.
-  // TODO(crbug.com/404586727): Remove once crash reason is confirmed.
-  if (is_deleting) {
-    base::debug::DumpWithoutCrashing();
-  }
   request->AddedToJob();
   requests_.insert(request);
   SetRequestExpectations(request);
