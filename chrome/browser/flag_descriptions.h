493ca192bf5ff4a55bdad987fc760677f47ca4e9
Judith Hemp <hempjudith@google.com>
2025-07-21 07:49:53 -0700

    [PermissionsAI] Add new feature flag for AIv4.
    Bug: 382447738
    Change-Id: I8b0cab8f471ac3d24dd93986f28382f84008c221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771650
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489515}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1846,6 +1846,9 @@ extern const char kPermissionsAIv1Description[];
 extern const char kPermissionsAIv3Name[];
 extern const char kPermissionsAIv3Description[];
 
+extern const char kPermissionsAIv4Name[];
+extern const char kPermissionsAIv4Description[];
+
 extern const char kPermissionSiteSettingsRadioButtonName[];
 extern const char kPermissionSiteSettingsRadioButtonDescription[];
 
