ec1b5c194c1581cb8b17adcd0cc0237dd1f9288c
Eshwar Stalin <estalin@chromium.org>
2025-07-21 10:31:40 -0700

    Add a comment to ContentsLayoutManager file
    ContentsContainerView was added to support Split Views. This is
    available even without the SxS flag enabled where a single instance of
    ContentsContainerView would exist. Going forward we want developers to
    added new overlay views in the contents area to the
    ContentsContainerView. To guard against new overlay views added without
    proper support for Split Views adding a warning comment to
    ContentsLayoutManager.
    
    Change-Id: Ie8144b6ada1cc56675d37e802710a0669821999d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771557
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489632}

--- chrome/browser/ui/views/frame/contents_layout_manager.h
+++ chrome/browser/ui/views/frame/contents_layout_manager.h
@@ -11,7 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_manager_base.h"
 #include "ui/views/layout/proposed_layout.h"
 
-// ContentsLayoutManager positions the WebContents and devtools WebContents.
+// WARNING: DO NOT ADD NEW FEATURES HERE. New views in the content area
+// should be added to ContentsContainerView. This is so that we can
+// support Split View where there will be multiple instances of Contents
+// WebView and associated overlay views. The layout for the remaining
+// views in this file are being moved and this class will be deleted soon.
+// Please consult estalin@ and corising@ for any additional guidance.
 class ContentsLayoutManager : public views::LayoutManagerBase {
  public:
   ContentsLayoutManager(views::View* devtools_view,
