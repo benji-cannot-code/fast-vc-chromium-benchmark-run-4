8abf997de92561ae4c709280a4e9d8fdedaf0384
Jenna Himawan <jhimawan@google.com>
2025-07-21 13:52:46 -0700

    Make ListMenu style more like context menus
    - See context_menu_row_item_horizontal_padding: https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/res/values/dimens.xml;l=312;drc=308644f929fca5d209fb5aecc64a486f329ee2c0
    - See TextAppearance.BrowserUIListMenuItem: https://source.chromium.org/chromium/chromium/src/+/main:components/browser_ui/widget/android/java/res/values/styles.xml;l=54;drc=b3a8b64872aa916b920498aeeab9933c171fde41
    
    - See discussion + approval by UX:
      - https://screenshot.googleplex.com/8rxEFGv8AZSE5a7
      - https://screenshot.googleplex.com/4ZUPuT9jddxgKLE
    
    Bug: 432981212
    Change-Id: Ibd7e9f1f4aea339f77c2eb573b6353384c1e8aec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772685
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489762}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/adaptive/AdaptiveButtonActionMenuRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/adaptive/AdaptiveButtonActionMenuRenderTest.java
@@ -59,7 +59,7 @@ public class AdaptiveButtonActionMenuRenderTest {
     @Rule
     public ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(2)
+                    .setRevision(3)
                     .setBugComponent(ChromeRenderTestRule.Component.UI_BROWSER_TOOLBAR)
                     .build();
 
