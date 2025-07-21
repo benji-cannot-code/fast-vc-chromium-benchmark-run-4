635f982c6b99dc3f247c5cb03b533a3cf991fdc6
Mei Liang <meiliang@chromium.org>
2025-07-21 12:38:36 -0700

    [UrlItemGrid] Add UrlItemElement
    Add the UrlItemElement that displays a single URL item with its favicon,
    title, thumbnail, and a close button. Thumbnail updates will be
    handled in another CL.
    
    Bug: 424506384
    Change-Id: I35cc825dd7406b680e990c82ce69ac90c6a19d46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6716650
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489717}

---
+++ chrome/browser/resources/tab_group_home/url_item_grid/url_item.html.ts
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import type {UrlItemElement} from './url_item.js';
+
+export function getHtml(this: UrlItemElement) {
+  return html`
+    <div class="header">
+      <div id="favicon" style="background-image: ${this.faviconImageSet_}">
+      </div>
+      <div id="title">${this.item.title}</div>
+      <cr-icon-button id="closeButton" iron-icon="cr:close" title="$i18n{close}"
+          @click="${this.onCloseButtonClick_}"></cr-icon-button>
+    </div>
+    <div class="thumbnail-container">
+      <img id="thumbnail" aria-hidden="true">
+    </div>
+  `;
+}
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -689,7 +689,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "META": {"sizes": {"includes": [30]}},
     "includes": [5020],
   }, "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/tab_group_home/resources.grd": {
-    "META": {"sizes": {"includes": [10]}},
+    "META": {"sizes": {"includes": [20]}},
     "includes": [5030],
   },
   "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/tab_search/resources.grd": {
