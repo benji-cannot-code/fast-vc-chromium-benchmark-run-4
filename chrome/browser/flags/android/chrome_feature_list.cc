7a4b0520e5e97e91f21675713c98a7f180166bca
Jack Thiesen <jthiesen@chromium.org>
2025-07-21 08:58:22 -0700

    Enabled ShowHomeButton Policy Flag By Default
    Bug: 400804628
    Change-Id: Ieddfd51472adea30390cd3b172a789e42de410d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636050
    Reviewed-by: Charles Hager <clhager@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489561}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1171,7 +1171,7 @@ BASE_FEATURE(kShortCircuitUnfocusAnimation,
 
 BASE_FEATURE(kShowHomeButtonPolicyAndroid,
              "ShowHomeButtonPolicyAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kShowNewTabAnimations,
              "ShowNewTabAnimations",
