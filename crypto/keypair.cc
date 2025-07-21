d9a1f84424ce9c9f2213eef3d74bb4e824f8e571
Elly <ellyjones@chromium.org>
2025-07-21 09:54:56 -0700

    crypto/{keypair, sign}: support Ed25519
    Specifically:
    * Add Ed25519 keypair support to crypto/keypair, including generating
      random keys, importing/exporting private keys, and importing public
      points to form PublicKeys;
    * Add Ed25519 signature support to crypto/sign; only the one-shot
      signing and verification functions can be used with these types;
    * Refactor the internals of Sign() and Verify() to not use Signer and
      Verifier respectively, since this doesn't work for one-shot-only
      schemes
    * Have CanUseKeyForSignatureKind() actually enforce something
    
    Fixed: 432056904
    Change-Id: Idce87e049aa33a8cd2d9cccc3a5cce5673f94bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6758127
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489603}

--- crypto/keypair.cc
+++ crypto/keypair.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/rsa_private_key.h"
 #include "third_party/boringssl/src/include/openssl/bn.h"
 #include "third_party/boringssl/src/include/openssl/bytestring.h"
+#include "third_party/boringssl/src/include/openssl/curve25519.h"
 #include "third_party/boringssl/src/include/openssl/ec.h"
 #include "third_party/boringssl/src/include/openssl/evp.h"
 #include "third_party/boringssl/src/include/openssl/mem.h"
@@ -38,7 +39,8 @@ bssl::UniquePtr<EVP_PKEY> GenerateRsa(size_t bits) {
 }
 
 bool IsSupportedEvpId(int evp_id) {
-  return evp_id == EVP_PKEY_RSA || evp_id == EVP_PKEY_EC;
+  return evp_id == EVP_PKEY_RSA || evp_id == EVP_PKEY_EC ||
+         evp_id == EVP_PKEY_ED25519;
 }
 
 std::vector<uint8_t> ExportEVPPublicKey(EVP_PKEY* pkey) {
@@ -99,6 +101,20 @@ PrivateKey PrivateKey::GenerateEcP256() {
 }
 
 // static
+PrivateKey PrivateKey::GenerateEd25519() {
+  OpenSSLErrStackTracer err_tracer(FROM_HERE);
+
+  std::array<uint8_t, ED25519_PUBLIC_KEY_LEN> unused_pubkey;
+  std::array<uint8_t, ED25519_PRIVATE_KEY_LEN> privkey;
+
+  ED25519_keypair(unused_pubkey.data(), privkey.data());
+
+  // EVP_PKEY_new_raw_public_key() takes only the 32-byte RFC 8032 "seed" at the
+  // start of the private key, not the BoringSSL-format "full" private key.
+  return FromEd25519PrivateKey(base::span(privkey).first<32>());
+}
+
+// static
 std::optional<PrivateKey> PrivateKey::FromPrivateKeyInfo(
     base::span<const uint8_t> pki) {
   OpenSSLErrStackTracer err_tracer(FROM_HERE);
@@ -124,6 +140,15 @@ PrivateKey PrivateKey::FromDeprecatedRSAPrivateKey(RSAPrivateKey* key) {
   return PrivateKey(bssl::UpRef(key->key()));
 }
 
+// static
+PrivateKey PrivateKey::FromEd25519PrivateKey(
+    base::span<const uint8_t, 32> key) {
+  bssl::UniquePtr<EVP_PKEY> pkey(EVP_PKEY_new_raw_private_key(
+      EVP_PKEY_ED25519, nullptr, key.data(), key.size()));
+  CHECK(pkey);
+  return PrivateKey(std::move(pkey));
+}
+
 std::vector<uint8_t> PrivateKey::ToPrivateKeyInfo() const {
   OpenSSLErrStackTracer err_tracer(FROM_HERE);
   bssl::ScopedCBB cbb;
@@ -143,6 +168,15 @@ std::vector<uint8_t> PrivateKey::ToPrivateKeyInfo() const {
   return result;
 }
 
+std::array<uint8_t, 32> PrivateKey::ToEd25519PrivateKey() const {
+  CHECK(IsEd25519());
+  std::array<uint8_t, 32> result;
+  size_t len = std::size(result);
+  CHECK(EVP_PKEY_get_raw_private_key(key_.get(), result.data(), &len));
+  CHECK(len == std::size(result));
+  return result;
+}
+
 std::vector<uint8_t> PrivateKey::ToSubjectPublicKeyInfo() const {
   return ExportEVPPublicKey(key_.get());
 }
@@ -159,6 +193,15 @@ std::vector<uint8_t> PrivateKey::ToUncompressedForm() const {
   return buf;
 }
 
+std::array<uint8_t, 32> PrivateKey::ToEd25519PublicKey() const {
+  CHECK(IsEd25519());
+  std::array<uint8_t, 32> result;
+  size_t len = std::size(result);
+  CHECK(EVP_PKEY_get_raw_public_key(key_.get(), result.data(), &len));
+  CHECK(len == std::size(result));
+  return result;
+}
+
 PrivateKey::PrivateKey(bssl::UniquePtr<EVP_PKEY> key) : key_(std::move(key)) {}
 
 bool PrivateKey::IsRsa() const {
@@ -167,6 +210,10 @@ bool PrivateKey::IsRsa() const {
 
 bool PrivateKey::IsEc() const {
   return EVP_PKEY_id(key_.get()) == EVP_PKEY_EC;
+}
+
+bool PrivateKey::IsEd25519() const {
+  return EVP_PKEY_id(key_.get()) == EVP_PKEY_ED25519;
 }
 
 PublicKey::PublicKey(bssl::UniquePtr<EVP_PKEY> key, crypto::SubtlePassKey)
@@ -249,6 +296,16 @@ std::optional<PublicKey> PublicKey::FromEcP256Point(
   return PublicKey(std::move(pkey));
 }
 
+// static
+PublicKey PublicKey::FromEd25519PublicKey(base::span<const uint8_t, 32> key) {
+  static_assert(std::size(key) == ED25519_PUBLIC_KEY_LEN);
+
+  bssl::UniquePtr<EVP_PKEY> pkey(EVP_PKEY_new_raw_public_key(
+      EVP_PKEY_ED25519, nullptr, key.data(), key.size()));
+  CHECK(pkey);
+  return PublicKey(std::move(pkey));
+}
+
 std::vector<uint8_t> PublicKey::ToSubjectPublicKeyInfo() const {
   return ExportEVPPublicKey(key_.get());
 }
@@ -259,6 +316,10 @@ bool PublicKey::IsRsa() const {
 
 bool PublicKey::IsEc() const {
   return EVP_PKEY_id(key_.get()) == EVP_PKEY_EC;
+}
+
+bool PublicKey::IsEd25519() const {
+  return EVP_PKEY_id(key_.get()) == EVP_PKEY_ED25519;
 }
 
 PublicKey::PublicKey(bssl::UniquePtr<EVP_PKEY> key) : key_(std::move(key)) {}
--- crypto/sign_unittest.cc
+++ crypto/sign_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "crypto/sign.h"
 
+#include "base/test/gtest_util.h"
 #include "crypto/test_support.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -38,6 +39,21 @@ TEST(Sign, RoundTripSignVerify) {
   auto ec_pub = PublicKey::FromPrivateKey(ec_priv);
 
   expect_roundtrip(ec_priv, ec_pub, SignatureKind::ECDSA_SHA256);
+
+  auto ed25519_priv = PrivateKey::GenerateEd25519();
+  auto ed25519_pub = PublicKey::FromPrivateKey(ed25519_priv);
+
+  expect_roundtrip(ed25519_priv, ed25519_pub, SignatureKind::ED25519);
+}
+
+TEST(Sign, CantUseEd25519ForStreaming) {
+  auto priv = PrivateKey::GenerateEd25519();
+  auto pub = PublicKey::FromPrivateKey(priv);
+
+  std::array<uint8_t, 64> sig = {};
+
+  EXPECT_CHECK_DEATH(crypto::sign::Signer(SignatureKind::ED25519, priv));
+  EXPECT_CHECK_DEATH(crypto::sign::Verifier(SignatureKind::ED25519, pub, sig));
 }
 
 }  // namespace
