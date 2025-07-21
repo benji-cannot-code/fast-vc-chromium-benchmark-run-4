332d04269ed71b63cf0fd6b88ffefdf3ff1f3dfc
Xiaohui Chen <xiaohuic@google.com>
2025-07-21 09:54:15 -0700

    assistant: add tests for gemini button in launcher
    * Add similar tests that assistant button used to have.
    * Move `UpdateSearchBoxUiVisibilities()` to a new location as the
      previous one is only called from the fullscreen presenter, not the
      bubble presenter.
    
    Bug: 430994906
    Change-Id: Iefebd76747baf765a7fd281661ce4217a6289e80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6730576
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489602}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -1365,8 +1365,6 @@ void AppListControllerImpl::InvokeSearchResultAction(
 }
 
 void AppListControllerImpl::ViewShown(int64_t display_id) {
-  UpdateSearchBoxUiVisibilities();
-
   // Note that IsHomeScreenVisible() might still return false at this point, as
   // the home screen visibility takes into account whether the app list view is
   // obscured by an app window, or overview UI. This method gets called when the
@@ -1829,6 +1827,7 @@ void AppListControllerImpl::OnVisibilityWillChange(bool visible,
     }
 
     if (real_target_visibility) {
+      UpdateSearchBoxUiVisibilities();
       // The virtual keyboard should be hidden before the bubble launcher
       // calculating the work area.
       keyboard::KeyboardUIController::Get()->HideKeyboardExplicitlyBySystem();
