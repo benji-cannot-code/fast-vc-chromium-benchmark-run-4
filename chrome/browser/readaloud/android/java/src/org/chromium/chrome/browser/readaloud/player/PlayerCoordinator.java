c7be341c50bc048a46508d5daca5c6b93f513211
Andrew Grieve <agrieve@chromium.org>
2025-07-21 12:13:58 -0700

    Android: Update "google3" split name references to "on_demand"
    Bug: 433183653, 432671263, 433171655
    Change-Id: I7966b0574e2265d3a7eed6ab68d3add16d0666f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774167
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489699}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/PlayerCoordinator.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/PlayerCoordinator.java
@@ -18,6 +18,7 @@ import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.readaloud.ReadAloudPrefs;
 import org.chromium.chrome.browser.readaloud.player.expanded.ExpandedPlayerCoordinator;
 import org.chromium.chrome.browser.readaloud.player.mini.MiniPlayerCoordinator;
+import org.chromium.chrome.modules.on_demand.OnDemandModule;
 import org.chromium.chrome.modules.readaloud.Playback;
 import org.chromium.chrome.modules.readaloud.PlaybackArgs.PlaybackMode;
 import org.chromium.chrome.modules.readaloud.PlaybackListener;
@@ -63,7 +64,8 @@ public class PlayerCoordinator implements Player {
                         .build();
         // This Context can be used to inflate views from the split.
         Context contextForInflation =
-                BundleUtils.createContextForInflation(delegate.getActivity(), "google3");
+                BundleUtils.createContextForInflation(
+                        delegate.getActivity(), OnDemandModule.SPLIT_NAME);
         mMiniPlayer =
                 new MiniPlayerCoordinator(
                         delegate.getActivity(),
