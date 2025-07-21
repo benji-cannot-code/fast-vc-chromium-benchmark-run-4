9abe9826745ff0823b63654cc30776a04f359db8
Marijn Kruisselbrink <mek@chromium.org>
2025-07-21 13:18:49 -0700

    Separate remote_cocoa and "local" cocoa versions of EventMonitor
    Since the remote_cocoa (~more or less PWAs) version of EventMonitor
    doesn't share any significant code with the regular/local cocoa
    version, both implementations get more readable by separating them
    out into their own classes.
    
    Bug: 426181639
    Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests|browser_tests
    Change-Id: I6330b4e50cda3bbcbc090f4113d7b1ddb87e8de4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6762468
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489747}

--- chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.h"
 
+#include "base/check_is_test.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -60,10 +61,15 @@ LocationBarBubbleDelegateView::WebContentMouseHandler::WebContentMouseHandler(
     : bubble_(bubble), web_contents_(web_contents) {
   DCHECK(bubble_);
   DCHECK(web_contents_);
-  event_monitor_ = views::EventMonitor::CreateWindowMonitor(
-      this, web_contents_->GetTopLevelNativeWindow(),
-      {ui::EventType::kMousePressed, ui::EventType::kKeyPressed,
-       ui::EventType::kTouchPressed});
+  // In unittests `web_contents_` might not have a containing top-level window.
+  if (web_contents_->GetTopLevelNativeWindow()) {
+    event_monitor_ = views::EventMonitor::CreateWindowMonitor(
+        this, web_contents_->GetTopLevelNativeWindow(),
+        {ui::EventType::kMousePressed, ui::EventType::kKeyPressed,
+         ui::EventType::kTouchPressed});
+  } else {
+    CHECK_IS_TEST();
+  }
 }
 
 LocationBarBubbleDelegateView::WebContentMouseHandler::
