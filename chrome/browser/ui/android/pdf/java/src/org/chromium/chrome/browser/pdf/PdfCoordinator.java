2fa624045794e1ae84336febe9710608d55aaef5
Henrique Nakashima <hnakashima@chromium.org>
2025-07-21 11:21:21 -0700

    [Android] Convert PdfPageTest to Public Transit
    Add PdfCtaPageStation to Transit Layer.
    
    Bug: 399877997,413111192
    Change-Id: I4bb8fe98b87a2879f0286655285eae8ba76a0a5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6770149
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489670}

--- chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
@@ -71,7 +71,7 @@ public class PdfCoordinator {
     /** Uri of the pdf document. Generated when the pdf is ready to load. */
     private @Nullable Uri mUri;
 
-    @VisibleForTesting ChromePdfViewerFragment mChromePdfViewerFragment;
+    @VisibleForTesting public ChromePdfViewerFragment mChromePdfViewerFragment;
 
     private int mFindInPageCount;
 
@@ -125,7 +125,7 @@ public class PdfCoordinator {
     /** The class responsible for rendering pdf document. */
     public static class ChromePdfViewerFragment extends PdfViewerFragment {
         /** Whether the pdf has been loaded successfully. */
-        boolean mIsLoadDocumentSuccess;
+        @VisibleForTesting public boolean mIsLoadDocumentSuccess;
 
         /** Whether the pdf has emitted any load error. */
         boolean mIsLoadDocumentError;
@@ -162,7 +162,8 @@ public class PdfCoordinator {
     }
 
     /** Returns the intended view for PdfPage tab. */
-    View getView() {
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public View getView() {
         return mView;
     }
 
