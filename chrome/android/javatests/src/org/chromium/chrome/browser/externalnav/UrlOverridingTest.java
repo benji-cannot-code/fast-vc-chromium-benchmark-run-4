eba9b3dd8e2897aa6d881baea1edf2ab6007a7ed
Fabio Muratori <muratori@google.com>
2025-07-21 10:43:19 -0700

    [Clank Nav Cap] Enable desktop windowing flags by default
    The navigation changes for desktop windowing behind flags:
    - ReparentAuxiliaryNavigationFromPWA
    - AuxiliaryNavigationStaysInBrowser
    - ReparentTopLevelNavigationFromPWA
    are ready for release on stable 100%
    
    Based on finch best practices first we should enable them by default in
    chromium.
    
    Because flag AuxiliaryNavigationStaysInBrowser is multi value, minor
    changes have been made to when the flag is enabled, specifically for
    desktop windowing Clank.
    
    Tests have been adapted to reflect this.
    
    https://launch.corp.google.com/launch/4385297
    
    Bug: 431942688
    Change-Id: I3ec63658ed61789241969e8966be1646c8d244c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772909
    Commit-Queue: Fabio Muratori <muratori@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Bartosz Chomiński <chominskib@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489636}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -2299,6 +2299,8 @@ public class UrlOverridingTest {
     @EnableFeatures({ExternalIntentsFeatures.AUXILIARY_NAVIGATION_STAYS_IN_BROWSER_NAME})
     @LargeTest
     public void testAuxiliaryNavigationShouldStayInBrowser() throws Exception {
+        InterceptNavigationDelegateClientImpl.setIsDesktopWindowingModeForTesting(true);
+
         mTabbedActivityTestRule.startOnBlankPage();
 
         String url_external = EXTERNAL_APP_SCHEME + "://example.com";
@@ -2328,6 +2330,8 @@ public class UrlOverridingTest {
     @EnableFeatures({ExternalIntentsFeatures.AUXILIARY_NAVIGATION_STAYS_IN_BROWSER_NAME})
     @LargeTest
     public void testTopLevelNavigationShouldBeIntercepted() throws Exception {
+        InterceptNavigationDelegateClientImpl.setIsDesktopWindowingModeForTesting(true);
+
         mTabbedActivityTestRule.startOnBlankPage();
 
         String url_external = EXTERNAL_APP_SCHEME + "://example.com";
@@ -2356,6 +2360,8 @@ public class UrlOverridingTest {
     @EnableFeatures({ExternalIntentsFeatures.AUXILIARY_NAVIGATION_STAYS_IN_BROWSER_NAME})
     @LargeTest
     public void testSelfNavigationInAuxiliaryPage() throws Exception {
+        InterceptNavigationDelegateClientImpl.setIsDesktopWindowingModeForTesting(true);
+
         String page_with_self_link =
                 getUrlWithParam(
                         NAVIGATION_FROM_TARGET_SELF_LINK, EXTERNAL_APP_SCHEME + "://example.com");
