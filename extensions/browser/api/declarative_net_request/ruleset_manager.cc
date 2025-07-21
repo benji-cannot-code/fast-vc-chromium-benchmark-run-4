676ca0e6c7b2b3b4a3824e653cbe3e11c3c07f20
Christoph Schwering <schwering@google.com>
2025-07-21 14:47:31 -0700

    [Extensions] Remove DCHECKs in RuleSetManager, RulesMonitorService
    This CL replaces two crashing DCHECKs with LOG_IFs and histograms.
    
    Bug: 358617943
    Change-Id: I3e8e6d8f0b16c8221f6834c7b663c7f6284da6c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6647245
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489794}

--- extensions/browser/api/declarative_net_request/ruleset_manager.cc
+++ extensions/browser/api/declarative_net_request/ruleset_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/time/time.h"
@@ -84,8 +85,13 @@ RulesetManager::~RulesetManager() {
 void RulesetManager::AddRuleset(const ExtensionId& extension_id,
                                 std::unique_ptr<CompositeMatcher> matcher) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(!GetMatcherForExtension(extension_id))
+  // TODO(crbug.com/358617943): Replace LOG_IF() and histogram with CHECK().
+  const bool called_twice = GetMatcherForExtension(extension_id);
+  LOG_IF(ERROR, called_twice)
       << "AddRuleset called twice in succession for " << extension_id;
+  base::UmaHistogramBoolean(
+      "Extensions.DeclarativeNetRequest.AddRulesetCalledTwiceInSuccession",
+      called_twice);
 
   base::Time update_time = GetLastUpdateTime(prefs_, extension_id);
   rulesets_.emplace(extension_id, update_time, std::move(matcher));
