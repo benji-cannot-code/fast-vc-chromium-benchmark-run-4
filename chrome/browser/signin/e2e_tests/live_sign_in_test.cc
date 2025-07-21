03399d1410f0d6a23b5ac568414fd8148dc94c14
anthie@google.com <anthie@google.com>
2025-07-21 10:17:08 -0700

    Update the dice tab helper in the profile picker signin provider
    This CL updates only the necessary parts of the dice tab helper state
    when the flow is redirected in a browser tab.
    
    Previously, the tab helper would be re-initialized, and have it's state
    reset, but we want to avoid that since there are use cases that should
    maintain the running timer that waits for the sync header.
    
    In passing, updated condition on a failing live test to wait until
    the browser primary account is set.
    
    Bug: 429524688
    Change-Id: I50817555cf49dbe76d20db71615188e9cef3b58d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764512
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489619}

--- chrome/browser/signin/e2e_tests/live_sign_in_test.cc
+++ chrome/browser/signin/e2e_tests/live_sign_in_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/strings/stringprintf.h"
+#include "base/task/current_thread.h"
 #include "base/test/bind.h"
 #include "base/test/test_future.h"
 #include "base/test/with_feature_override.h"
@@ -612,6 +613,7 @@ IN_PROC_BROWSER_TEST_F(LiveSignInTest,
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 IN_PROC_BROWSER_TEST_F(LiveSignInTest, MANUAL_CreateSignedInProfile) {
+  base::HistogramTester histogram_tester;
   std::optional<TestAccountSigninCredentials> test_account =
       GetTestAccounts()->GetAccount("TEST_ACCOUNT_1");
   CHECK(test_account.has_value());
@@ -645,15 +647,17 @@ IN_PROC_BROWSER_TEST_F(LiveSignInTest, MANUAL_CreateSignedInProfile) {
   // User is signed in, but Sync is off.
   signin::IdentityManager* identity_manager =
       IdentityManagerFactory::GetForProfile(new_profile);
-  EXPECT_EQ(GetPrimaryAccountConsentLevel(identity_manager),
-            signin::ConsentLevel::kSignin);
+  base::test::RunUntil([&identity_manager]() {
+    return GetPrimaryAccountConsentLevel(identity_manager) ==
+           signin::ConsentLevel::kSignin;
+  });
 
   // Confirm Sync.
   ui_test_utils::BrowserChangeObserver browser_added_observer(
       nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-  GURL sync_confirmation_url = AppendSyncConfirmationQueryParams(
-      GURL("chrome://sync-confirmation/"), SyncConfirmationStyle::kWindow,
-      /*is_sync_promo=*/true);
+  GURL sync_confirmation_url =
+      AppendSyncConfirmationQueryParams(GURL("chrome://sync-confirmation/"),
+                                        SyncConfirmationStyle::kWindow, true);
   profiles::testing::WaitForPickerLoadStop(sync_confirmation_url);
   LoginUIServiceFactory::GetForProfile(new_profile)
       ->SyncConfirmationUIClosed(LoginUIService::SYNC_WITH_DEFAULT_SETTINGS);
@@ -664,6 +668,11 @@ IN_PROC_BROWSER_TEST_F(LiveSignInTest, MANUAL_CreateSignedInProfile) {
   EXPECT_EQ(new_browser->profile(), new_profile);
   EXPECT_EQ(GetPrimaryAccountConsentLevel(identity_manager),
             signin::ConsentLevel::kSync);
+
+  // Both LST and Sync Header are received so their time difference must be
+  // recorded.
+  histogram_tester.ExpectTotalCount(
+      "Signin.SigninManager.SyncHeaderArrivalTimeWindow", 1);
 }
 
 class LiveSignInGaiaIntegrationTest
