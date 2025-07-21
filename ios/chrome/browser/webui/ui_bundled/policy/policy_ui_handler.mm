22b2e62e487adbe9af26d7472c16d3eef8d4f629
David Sanders <dsanders11@ucsbalum.com>
2025-07-21 12:34:52 -0700

    Add some forward declares to remove includes
    Should save ~63 MiB of compiler input size.
    
    Adds missing includes as needed to fix the build.
    
    Bug: 429365675
    Change-Id: I0d211d8413058159b5aba45f7c3761ac38682f89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772764
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1489713}

--- ios/chrome/browser/webui/ui_bundled/policy/policy_ui_handler.mm
+++ ios/chrome/browser/webui/ui_bundled/policy/policy_ui_handler.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/enterprise/browser/controller/browser_dm_token_storage.h"
 #import "components/enterprise/browser/controller/chrome_browser_cloud_management_controller.h"
 #import "components/enterprise/browser/reporting/common_pref_names.h"
+#import "components/enterprise/browser/reporting/report_scheduler.h"
 #import "components/policy/core/browser/policy_conversions.h"
 #import "components/policy/core/browser/webui/json_generation.h"
 #import "components/policy/core/browser/webui/machine_level_user_cloud_policy_status_provider.h"
