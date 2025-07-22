55572a457e1b954e73f0a794414cd1f8e89dc919
Thomas Nguyen <tungnh@chromium.org>
2025-07-22 01:49:33 -0700

    [PEPC] Introduce a new <geolocation> element
    This is the first implementation of the new HTML Geolocation Element.
    This initial implementation does not define all properties/features
    that will be added in the future.
    
    Change-Id: I685bf5ae87e9c570674c712b0ad01c0febf0e9b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769244
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490010}

---
+++ third_party/blink/renderer/core/html/html_geolocation_element.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/html/html_geolocation_element.h"
+
+#include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/html/html_permission_element.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+
+namespace blink {
+
+HTMLGeolocationElement::HTMLGeolocationElement(Document& document)
+    : HTMLPermissionElement(document) {
+  CHECK(RuntimeEnabledFeatures::GeolocationElementEnabled(
+      document.GetExecutionContext()));
+  setType(AtomicString("geolocation"));
+}
+
+void HTMLGeolocationElement::Trace(Visitor* visitor) const {
+  HTMLPermissionElement::Trace(visitor);
+}
+}  // namespace blink
---
+++ third_party/blink/renderer/core/html/html_geolocation_element.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_GEOLOCATION_ELEMENT_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_GEOLOCATION_ELEMENT_H_
+
+#include "third_party/blink/renderer/core/dom/events/event_target.h"
+#include "third_party/blink/renderer/core/html/html_permission_element.h"
+#include "third_party/blink/renderer/platform/heap/member.h"
+
+namespace blink {
+
+class CORE_EXPORT HTMLGeolocationElement final : public HTMLPermissionElement {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  explicit HTMLGeolocationElement(Document&);
+
+  DEFINE_ATTRIBUTE_EVENT_LISTENER(location, kLocation)
+
+  void Trace(Visitor*) const override;
+};
+
+}  // namespace blink
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_GEOLOCATION_ELEMENT_H_
