cd5769e1c1c99170114c563b440315f1971b6235
Tom Anderson <thomasanderson@chromium.org>
2025-07-22 08:06:18 -0700

    [DBus Types] Implement Variant
    Bug: 429203538
    Change-Id: I9ede942298924c707e6f4c6218f95a0a6f1570dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697158
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490168}

--- components/dbus/utils/signature_unittest.cc
+++ components/dbus/utils/signature_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 #include <vector>
 
+#include "components/dbus/utils/variant.h"
 #include "dbus/object_path.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -39,6 +40,7 @@ TEST(DBusSignatureTest, GetSignature) {
   EXPECT_EQ(GetSignature<uint8_t>(), "y");
   EXPECT_EQ(GetSignature<std::string>(), "s");
   EXPECT_EQ(GetSignature<dbus::ObjectPath>(), "o");
+  EXPECT_EQ(GetSignature<Variant>(), "v");
   EXPECT_EQ(GetSignature<base::ScopedFD>(), "h");
 
   EXPECT_EQ(GetSignature<std::vector<int32_t>>(), "ai");
