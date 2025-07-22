cf3490db10c36f069ccc32efe8049911541252d8
Wei Wang <wei4.wang@intel.com>
2025-07-21 18:04:40 -0700

    [WebNN] Introduce MLNumber
    Adds support for MLNumber unions in the WebIDL bindings and uses
    MLNumber with one operator: clamp. Use of MLNumber with other ops
    will be added in follow-up CLs
    
    Spec: https://github.com/webmachinelearning/webnn/pull/647
    
    Bug: 335909582
    Change-Id: I99387b221bb1147c441ae1e789dc58e9e20bf419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6720936
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489877}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "base/types/expected.h"
 #include "mojo/public/cpp/bindings/type_converter.h"
+#include "services/webnn/public/cpp/ml_number.h"
+#include "services/webnn/public/cpp/operand_descriptor.h"
 #include "services/webnn/public/cpp/webnn_types.h"
 #include "services/webnn/public/mojom/webnn_context_provider.mojom-blink.h"
 #include "services/webnn/public/mojom/webnn_graph.mojom-blink.h"
@@ -19,6 +22,7 @@ namespace blink {
 
 class MLOperand;
 class MLOperator;
+class V8UnionBigintOrUnrestrictedDouble;
 
 // Add operand to `graph_info` and return its operand id.
 webnn::OperandId AddOperand(webnn::mojom::blink::GraphInfo& graph_info,
@@ -30,6 +34,10 @@ void SerializeMojoOperation(
     const webnn::ContextProperties& context_properties,
     const MLOperator* op,
     webnn::mojom::blink::GraphInfo* graph_info);
+
+base::expected<webnn::MLNumber, String> ToMLNumberAsType(
+    const V8UnionBigintOrUnrestrictedDouble& number,
+    webnn::OperandDataType type);
 
 }  // namespace blink
 
