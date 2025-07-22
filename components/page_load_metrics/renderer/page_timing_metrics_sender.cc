2ab57c61d70475bf9d9cd008cfc6d0a58f649f4d
Johannes Henkel <johannes@chromium.org>
2025-07-22 10:22:17 -0700

    [soft navs] Remove soft_navigation_metrics_type_converter.h.
    This appears to be unused.
    
    Change-Id: I9b4390de0cff497d9b1d36a745ef81fdef4a95d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6767933
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490267}

--- components/page_load_metrics/renderer/page_timing_metrics_sender.cc
+++ components/page_load_metrics/renderer/page_timing_metrics_sender.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_load_metrics/common/page_load_metrics_util.h"
 #include "components/page_load_metrics/common/page_load_timing.h"
 #include "components/page_load_metrics/renderer/page_timing_sender.h"
-#include "components/page_load_metrics/renderer/soft_navigation_metrics_type_converter.h"
 #include "services/network/public/cpp/url_loader_completion_status.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "third_party/blink/public/common/features.h"
