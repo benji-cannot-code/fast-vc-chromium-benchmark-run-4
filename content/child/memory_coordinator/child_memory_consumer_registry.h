a017c1cc7793a9e859a5239c59d6d9d054d816f9
Patrick Monette <pmonette@chromium.org>
2025-07-22 10:16:59 -0700

    [Memory Coordinator] Allow binding the ChildMemoryConsumerRegistry later
    This will allow instantiating the ChildMemoryConsumerRegistry much
    earlier than when Mojo is initialized, which thus allow MemoryConsumers
    to register very early in the process's lifetime.
    
    Bug: 406578344
    Change-Id: I04a3dca9c3cc74270fb6d6a05d706a00289de2b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774623
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490264}

--- content/child/memory_coordinator/child_memory_consumer_registry.h
+++ content/child/memory_coordinator/child_memory_consumer_registry.h
@@ -31,9 +31,7 @@ class CONTENT_EXPORT ChildMemoryConsumerRegistry
     : public base::MemoryConsumerRegistry,
       public mojom::ChildMemoryConsumer {
  public:
-  explicit ChildMemoryConsumerRegistry(
-      mojo::PendingRemote<mojom::BrowserMemoryConsumerRegistry>
-          browser_memory_consumer_registry);
+  ChildMemoryConsumerRegistry();
   ~ChildMemoryConsumerRegistry() override;
 
   // mojom::ChildMemoryConsumer:
@@ -57,6 +55,15 @@ class CONTENT_EXPORT ChildMemoryConsumerRegistry
 
   // Returns the number of consumers with different IDs.
   size_t size() const { return consumer_groups_.size(); }
+
+  // Allows connecting this process's global instance with the browser process.
+  static mojo::PendingReceiver<mojom::BrowserMemoryConsumerRegistry>
+  BindAndPassReceiver();
+
+  // Non-static version of the above, allowing to connect the browser and child
+  // registries if the caller has a pointer to the registry.
+  mojo::PendingReceiver<mojom::BrowserMemoryConsumerRegistry>
+  BindAndPassReceiverForTesting();
 
  private:
   // An implementation of MemoryConsumer that groups all consumers with the same
@@ -92,6 +99,9 @@ class CONTENT_EXPORT ChildMemoryConsumerRegistry
   void OnMemoryConsumerRemoved(
       std::string_view consumer_id,
       base::RegisteredMemoryConsumer consumer) override;
+
+  mojo::PendingReceiver<mojom::BrowserMemoryConsumerRegistry>
+  BindAndPassReceiverImpl();
 
   // Used to register consumers in the child process with the browser process.
   mojo::Remote<mojom::BrowserMemoryConsumerRegistry> browser_registry_;
