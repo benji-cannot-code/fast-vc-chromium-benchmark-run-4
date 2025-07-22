acb0e300df6e6887dd282c9c050c9830b422c7a3
Calder Kitagawa <ckitagawa@chromium.org>
2025-07-22 13:38:38 -0700

    [Tab Switcher] Disable swipe and drag auto-scroll on boundary for GTS
    When a mouse is being used for input disable the auto-scroll and swipe
    to close behaviors on the GTS. Add a touch helper before the
    ItemTouchHelper to detect this and notify the callback as to whether or
    not this is a mouse input.
    
    Fixed: 433481618
    Change-Id: I8b6c42c282de2caa4700438e2b97b8a3981d899c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6776980
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490403}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
@@ -12,6 +12,7 @@ import android.app.Activity;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.util.Size;
+import android.view.InputDevice;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -145,8 +146,9 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
     private @Nullable OnLayoutChangeListener mListLayoutListener;
     private boolean mLayoutListenerRegistered;
     private @Nullable TabStripSnapshotter mTabStripSnapshotter;
-    private ItemTouchHelper2 mItemTouchHelper;
-    private @Nullable OnItemTouchListener mOnItemTouchListener;
+    private @Nullable ItemTouchHelper2 mItemTouchHelper;
+    private @Nullable OnItemTouchListener mOnBeforeItemTouchHelperItemTouchListener;
+    private @Nullable OnItemTouchListener mOnAfterItemTouchHelperItemTouchListener;
     private @Nullable TabListEmptyCoordinator mTabListEmptyCoordinator;
     private boolean mIsEmptyViewInitialized;
     private @Nullable Runnable mAwaitingLayoutRunnable;
@@ -252,6 +254,7 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
                             for (DragObserver observer : mDragObserverList) {
                                 observer.onDragStart();
                             }
+                            assumeNonNull(mItemTouchHelper);
                             mItemTouchHelper.onExternalDragStart(
                                     xPx, yPx, /* hideItemWhileDragging= */ true);
                             return true;
@@ -259,6 +262,7 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
 
                         @Override
                         public boolean handleDragLocation(float xPx, float yPx) {
+                            assumeNonNull(mItemTouchHelper);
                             mItemTouchHelper.onExternalDragLocation(xPx, yPx);
                             return true;
                         }
@@ -268,6 +272,7 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
                             for (DragObserver observer : mDragObserverList) {
                                 observer.onDragEnd();
                             }
+                            assumeNonNull(mItemTouchHelper);
                             mItemTouchHelper.onExternalDragStop(/* recoverItem= */ false);
                             return true;
                         }
@@ -587,7 +592,9 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
         boolean modeAllowsDragAndDrop = mMode == TabListMode.GRID;
         boolean actionStateAllowsDragAndDrop = mTabActionState != TabActionState.SELECTABLE;
         if (mAllowDragAndDrop && modeAllowsDragAndDrop && actionStateAllowsDragAndDrop) {
-            if (mItemTouchHelper == null || mOnItemTouchListener == null) {
+            if (mOnBeforeItemTouchHelperItemTouchListener == null
+                    || mItemTouchHelper == null
+                    || mOnAfterItemTouchHelperItemTouchListener == null) {
                 TabGridItemTouchHelperCallback callback =
                         (TabGridItemTouchHelperCallback)
                                 mMediator.getItemTouchHelperCallback(
@@ -605,8 +612,29 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
                     longPressHandler = new LongPressHandler();
                 }
 
+                // Detects if inputs are coming from a mouse or not. This is used to modify
+                // behaviors of the TabGridItemTouchHelperCallback.
+                mOnBeforeItemTouchHelperItemTouchListener =
+                        new OnItemTouchListener() {
+                            @Override
+                            public boolean onInterceptTouchEvent(
+                                    RecyclerView recyclerView, MotionEvent event) {
+                                callback.setIsMouseInputSource(
+                                        event.getSource() == InputDevice.SOURCE_MOUSE);
+                                return false;
+                            }
+
+                            @Override
+                            public void onTouchEvent(
+                                    RecyclerView recyclerView, MotionEvent event) {}
+
+                            @Override
+                            public void onRequestDisallowInterceptTouchEvent(
+                                    boolean disallowIntercept) {}
+                        };
+
                 // Creates an instance of the ItemTouchHelper using TabGridItemTouchHelperCallback
-                // and attach a downsteam mOnItemTouchListener that watches for
+                // and attach a downstream mOnAfterItemTouchHelperItemTouchListener that watches for
                 // TabGridItemTouchHelperCallback#shouldBlockAction() to occur. This determines if
                 // on a longpress the final MOTION_UP event should be intercepted if it should have
                 // been filtered in the ItemTouchHelper, but was not handled. This then allows
@@ -615,7 +643,7 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
                 //
                 // See similar comments in TabGridItemTouchHelperCallback for more details.
                 mItemTouchHelper = new ItemTouchHelper2(callback, longPressHandler);
-                mOnItemTouchListener =
+                mOnAfterItemTouchHelperItemTouchListener =
                         new OnItemTouchListener() {
                             @Override
                             public boolean onInterceptTouchEvent(
@@ -656,12 +684,16 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
                             }
                         };
             }
+            mRecyclerView.addOnItemTouchListener(mOnBeforeItemTouchHelperItemTouchListener);
             mItemTouchHelper.attachToRecyclerView(mRecyclerView);
-            mRecyclerView.addOnItemTouchListener(mOnItemTouchListener);
+            mRecyclerView.addOnItemTouchListener(mOnAfterItemTouchHelperItemTouchListener);
         } else {
-            if (mItemTouchHelper != null && mOnItemTouchListener != null) {
+            if (mOnBeforeItemTouchHelperItemTouchListener != null
+                    && mItemTouchHelper != null
+                    && mOnAfterItemTouchHelperItemTouchListener != null) {
+                mRecyclerView.addOnItemTouchListener(mOnBeforeItemTouchHelperItemTouchListener);
                 mItemTouchHelper.attachToRecyclerView(null);
-                mRecyclerView.removeOnItemTouchListener(mOnItemTouchListener);
+                mRecyclerView.removeOnItemTouchListener(mOnAfterItemTouchHelperItemTouchListener);
             }
         }
     }
@@ -827,11 +859,14 @@ public class TabListCoordinator implements PriceWelcomeMessageProvider, DestroyO
         if (mTabStripSnapshotter != null) {
             mTabStripSnapshotter.destroy();
         }
+        if (mOnBeforeItemTouchHelperItemTouchListener != null) {
+            mRecyclerView.removeOnItemTouchListener(mOnBeforeItemTouchHelperItemTouchListener);
+        }
         if (mItemTouchHelper != null) {
             mItemTouchHelper.attachToRecyclerView(null);
         }
-        if (mOnItemTouchListener != null) {
-            mRecyclerView.removeOnItemTouchListener(mOnItemTouchListener);
+        if (mOnAfterItemTouchHelperItemTouchListener != null) {
+            mRecyclerView.removeOnItemTouchListener(mOnAfterItemTouchHelperItemTouchListener);
         }
         if (mTabSwitcherDragHandler != null) {
             mTabSwitcherDragHandler.destroy();
