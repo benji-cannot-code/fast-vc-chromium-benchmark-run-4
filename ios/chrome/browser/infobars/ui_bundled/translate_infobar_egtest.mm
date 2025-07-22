10d78e5f3d441a5b8c0ee8ec70d7663cdb30d6ea
Asami Doi <asamidoi@chromium.org>
2025-07-22 10:23:27 -0700

    [ios] Fix TranslateInfobarTestCase on iOS 26
    On iOS 26, it seems that `isNewOverflowMenuEnabled` is true. We should
    use `kPopupMenuToolsMenuActionListId` to match an overflow menu element.
    
    Fixed: 429444249
    Change-Id: Icf24c1ae3d948167e0643533c2c14cd925148147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772333
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490269}

--- ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
+++ ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
@@ -1059,11 +1059,16 @@ void TestResponseProvider::GetLanguageResponse(
 
   // Make sure the Translate manual trigger button is enabled and tap it.
   [ChromeEarlGreyUI openToolsMenu];
+
+  id<GREYMatcher> tableViewMatcher =
+      [ChromeEarlGrey isNewOverflowMenuEnabled]
+          ? grey_accessibilityID(kPopupMenuToolsMenuActionListId)
+          : grey_accessibilityID(kPopupMenuToolsMenuTableViewId);
   [[[[EarlGrey selectElementWithMatcher:grey_allOf(grey_accessibilityID(
                                                        kToolsMenuTranslateId),
                                                    grey_interactable(), nil)]
          usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 250)
-      onElementWithMatcher:chrome_test_util::ToolsMenuView()]
+      onElementWithMatcher:tableViewMatcher]
       assertWithMatcher:grey_not(grey_accessibilityTrait(
                             UIAccessibilityTraitNotEnabled))]
       performAction:grey_tap()];
