05be316a7f03b5756f9c50956d3b5e1e4e5dd6e1
Mitsuru Oshima <oshima@chromium.org>
2025-07-21 17:51:02 -0700

    EventGenerator improvements.
    * Centerize Mouse Source Device ID update
    * Support "plain" scroll sequence which happens with TrackPoint.
    
    Bug: None
    Change-Id: Id7a28df670eb317c12271bae39a2b06f39a20de1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6773786
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489873}

--- ash/wm/overview/overview_session_unittest.cc
+++ ash/wm/overview/overview_session_unittest.cc
@@ -4623,7 +4623,7 @@ class ContinuousOverviewAnimationTest
         /*end_state=*/
         complete_scroll
             ? ui::test::EventGenerator::ScrollSequenceType::UpToFling
-            : ui::test::EventGenerator::ScrollSequenceType::ScrollOnly);
+            : ui::test::EventGenerator::ScrollSequenceType::StartAndScroll);
   }
 
   void SetShowDeskButton(bool visible) {
--- ui/events/test/event_generator.cc
+++ ui/events/test/event_generator.cc
@@ -153,7 +153,6 @@ void EventGenerator::PressButton(int flag) {
     gfx::Point location = GetLocationInCurrentRoot();
     ui::MouseEvent mouseev(ui::EventType::kMousePressed, location, location,
                            ui::EventTimeForNow(), flags_, flag);
-    mouseev.set_source_device_id(mouse_source_device_id_);
     Dispatch(&mouseev);
   }
 }
@@ -163,7 +162,6 @@ void EventGenerator::ReleaseButton(int flag) {
     gfx::Point location = GetLocationInCurrentRoot();
     ui::MouseEvent mouseev(ui::EventType::kMouseReleased, location, location,
                            ui::EventTimeForNow(), flags_, flag);
-    mouseev.set_source_device_id(mouse_source_device_id_);
     Dispatch(&mouseev);
     flags_ ^= flag;
   }
@@ -208,7 +206,6 @@ void EventGenerator::MoveMouseWheel(int delta_x, int delta_y) {
   gfx::Point location = GetLocationInCurrentRoot();
   ui::MouseWheelEvent wheelev(gfx::Vector2d(delta_x, delta_y), location,
                               location, ui::EventTimeForNow(), flags_, 0);
-  wheelev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&wheelev);
 }
 
@@ -217,7 +214,6 @@ void EventGenerator::SendMouseEnter() {
   delegate()->ConvertPointToTarget(current_target_, &enter_location);
   ui::MouseEvent mouseev(ui::EventType::kMouseEntered, enter_location,
                          enter_location, ui::EventTimeForNow(), flags_, 0);
-  mouseev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&mouseev);
 }
 
@@ -226,7 +222,6 @@ void EventGenerator::SendMouseExit() {
   delegate()->ConvertPointToTarget(current_target_, &exit_location);
   ui::MouseEvent mouseev(ui::EventType::kMouseExited, exit_location,
                          exit_location, ui::EventTimeForNow(), flags_, 0);
-  mouseev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&mouseev);
 }
 
@@ -241,7 +236,6 @@ void EventGenerator::MoveMouseToWithNative(const gfx::Point& point_in_host,
       new ui::MouseEvent(ui::EventType::kMouseMoved, point_in_host,
                          point_in_host, ui::EventTimeForNow(), flags_, 0));
   ui::MouseEvent mouseev(native_event.get());
-  mouseev.set_source_device_id(mouse_source_device_id_);
   native_event->set_location(point_for_native);
   Dispatch(&mouseev);
 
@@ -256,7 +250,6 @@ void EventGenerator::MoveMouseToInHost(const gfx::Point& point_in_host) {
                                        : ui::EventType::kMouseMoved;
   ui::MouseEvent mouseev(event_type, point_in_host, point_in_host,
                          ui::EventTimeForNow(), flags_, 0);
-  mouseev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&mouseev);
 
   SetCurrentScreenLocation(point_in_host);
@@ -281,7 +274,6 @@ void EventGenerator::MoveMouseTo(const gfx::Point& point_in_screen,
     delegate()->ConvertPointToTarget(current_target_, &move_point);
     ui::MouseEvent mouseev(event_type, move_point, move_point,
                            ui::EventTimeForNow(), flags_, 0);
-    mouseev.set_source_device_id(mouse_source_device_id_);
     Dispatch(&mouseev);
   }
   SetCurrentScreenLocation(point_in_screen);
@@ -642,22 +634,25 @@ void EventGenerator::ScrollSequence(const gfx::Point& start,
   UpdateCurrentDispatcher(start);
 
   base::TimeTicks timestamp = ui::EventTimeForNow();
-  ui::ScrollEvent fling_cancel(ui::EventType::kScrollFlingCancel, start,
-                               timestamp, 0, 0, 0, 0, 0, num_fingers);
-  Dispatch(&fling_cancel);
+  if (end_state != ScrollSequenceType::ScrollOnly) {
+    ui::ScrollEvent fling_cancel(ui::EventType::kScrollFlingCancel, start,
+                                 timestamp, 0, 0, 0, 0, 0, num_fingers);
+    Dispatch(&fling_cancel);
+    timestamp += step_delay;
+  }
 
   float dx = x_offset / steps;
   float dy = y_offset / steps;
   for (int i = 0; i < steps; ++i) {
-    timestamp += step_delay;
     ui::ScrollEvent move(ui::EventType::kScroll, start, timestamp, 0, dx, dy,
                          dx, dy, num_fingers);
     Dispatch(&move);
+    timestamp += step_delay;
   }
 
   // End the scroll sequence early if we want to end with the fingers rested on
   // the trackpad.
-  if (end_state == ScrollSequenceType::ScrollOnly) {
+  if (end_state != ScrollSequenceType::UpToFling) {
     return;
   }
 
@@ -744,6 +739,10 @@ void EventGenerator::PressAndReleaseKeyAndModifierKeys(KeyboardCode key_code,
 }
 
 void EventGenerator::Dispatch(ui::Event* event) {
+  if (event->IsMouseEvent() || event->IsScrollEvent()) {
+    event->set_source_device_id(mouse_source_device_id_);
+  }
+
   if (event->IsTouchEvent()) {
     ui::TouchEvent* touch_event = static_cast<ui::TouchEvent*>(event);
     touch_pointer_details_.id = touch_event->pointer_details().id;
