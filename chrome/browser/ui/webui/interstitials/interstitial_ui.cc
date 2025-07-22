48fb6b1a07593f0edeb484ba5d6b3c6724e9abb0
Elly <ellyjones@chromium.org>
2025-07-22 10:06:38 -0700

    net/cert: get rid of CreateKeyAndSelfSignedCert()
    This function was very dangerous:
    * It returns a self-signed cert which is difficult to use safely
    * It generated a 1024-bit RSA key which was too short for security even
      if used safely
    
    Fortunately all of the clients were only using this function to generate
    test / dummy certificates, and in fact all of them were immediately
    discarding the generated private key without using it.
    
    This change replaces that function with a new function:
    
      net::x509_util::CreateUnusableCert()
    
    which generates a random EC P-256 key, uses it to self-sign a cert, then
    discards the private key and returns only the cert. Using EC P-256 is
    much more efficient than using RSA, and deliberately not returning the
    key means the certificate can never be misused because there's no way to
    actually sign anything with it.
    
    Bug: 372283556
    Change-Id: I314ebf2ea96ce8423b2b28113e0e3b6a153a0c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764696
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490256}

--- chrome/browser/ui/webui/interstitials/interstitial_ui.cc
+++ chrome/browser/ui/webui/interstitials/interstitial_ui.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <utility>
 
-#include "base/atomic_sequence_num.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted_memory.h"
@@ -63,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
-#include "crypto/rsa_private_key.h"
 #include "net/base/net_errors.h"
 #include "net/base/url_util.h"
 #include "net/cert/x509_certificate.h"
@@ -96,23 +94,11 @@ InterstitialUIConfig::InterstitialUIConfig()
 
 namespace {
 
-// NSS requires that serial numbers be unique even for the same issuer;
-// as all fake certificates will contain the same issuer name, it's
-// necessary to ensure the serial number is unique, as otherwise
-// NSS will fail to parse.
-base::AtomicSequenceNumber g_serial_number;
-
 scoped_refptr<net::X509Certificate> CreateFakeCert() {
-  std::unique_ptr<crypto::RSAPrivateKey> unused_key;
-  std::string cert_der;
-  if (!net::x509_util::CreateKeyAndSelfSignedCert(
-          "CN=Error", static_cast<uint32_t>(g_serial_number.GetNext()),
-          base::Time::Now() - base::Minutes(5),
-          base::Time::Now() + base::Minutes(5), &unused_key, &cert_der)) {
-    return nullptr;
-  }
+  std::vector<uint8_t> cert_der =
+      net::x509_util::CreateUnusableCert("CN=Error");
 
-  return net::X509Certificate::CreateFromBytes(base::as_byte_span(cert_der));
+  return net::X509Certificate::CreateFromBytes(cert_der);
 }
 
 // Implementation of chrome://interstitials demonstration pages. This code is
