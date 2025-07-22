d735c3a793520c41c926888f143a0ba79132dae6
Charles Hager <clhager@google.com>
2025-07-22 14:01:03 -0700

    Pass IncognitoStateProvider to ToolbarLayout
    It would be useful for ToolbarLayout and its subclasses to have access
    to a IncognitoStateProvider, both to observe incognito state (as opposed
    to directly observing the TabModelSelector) and because ToolbarChild
    takes in an IncognitoStateProvider in its constructor.
    
    Bug: 427258660
    Change-Id: I8befc8c4dd1f0a4228241f9e7bda9144b379b1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6775991
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1490417}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarLayout.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarLayout.java
@@ -42,6 +42,7 @@ import org.chromium.chrome.browser.omnibox.OmniboxFocusReason;
 import org.chromium.chrome.browser.omnibox.UrlBarData;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.IncognitoStateProvider;
 import org.chromium.chrome.browser.theme.ThemeColorProvider;
 import org.chromium.chrome.browser.theme.ThemeColorProvider.ThemeColorObserver;
 import org.chromium.chrome.browser.theme.ThemeColorProvider.TintObserver;
@@ -93,6 +94,7 @@ public abstract class ToolbarLayout extends FrameLayout
     private boolean mFindInPageToolbarShowing;
 
     protected ThemeColorProvider mThemeColorProvider;
+    protected IncognitoStateProvider mIncognitoStateProvider;
     private MenuButtonCoordinator mMenuButtonCoordinator;
     private @Nullable AppMenuButtonHelper mAppMenuButtonHelper;
 
@@ -220,6 +222,15 @@ public abstract class ToolbarLayout extends FrameLayout
         mThemeColorProvider = themeColorProvider;
         mThemeColorProvider.addTintObserver(this);
         mThemeColorProvider.addThemeColorObserver(this);
+    }
+
+    /**
+     * @param incognitoStateProvider The {@link IncognitoStateProvider} for observing incognito
+     *     state.
+     */
+    @Initializer
+    void setIncognitoStateProvider(IncognitoStateProvider incognitoStateProvider) {
+        mIncognitoStateProvider = incognitoStateProvider;
     }
 
     /**
