caa0edfe19d3fb22a1a79246b96f0174e9be3b24
Simon Hangl <simonha@google.com>
2025-07-22 11:17:46 -0700

    Map url pattern to match pattern for context menus create
    This CL maps the url pattern parameters provided to contextMenus.create
    (for controlled frame) to a match pattern such that the webview
    implementation can handle the input correctly.
    
    This concerns the `documentURLPatterns` and `targetURLPatterns`
    parameters.
    
    Bug: 433466036, 433465937
    Change-Id: I74e5347ea28b6ede2c55ee38163856e9aecdb9a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772023
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Dominik Bylica <bylica@google.com>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490311}

--- chrome/renderer/resources/controlled_frame/controlled_frame_context_menus.js
+++ chrome/renderer/resources/controlled_frame/controlled_frame_context_menus.js
@@ -18,9 +18,22 @@ const $Headers = require('safeMethods').SafeMethods.$Headers;
 const WebViewContextMenusImpl =
     require('chromeWebView').WebViewContextMenusImpl;
 const ControlledFrameInternal = getInternalApi('controlledFrameInternal');
+const WebUrlPatternNatives = requireNative('WebUrlPatternNatives');
 
 function identity(value) {
   return value;
+}
+
+function mapUrlPatternsToMatchPatterns(urlPatternsStrs) {
+  let matchPatterns = [];
+  for (const urlPatternStr of urlPatternsStrs) {
+    matchPatterns = $Array.concat(
+      matchPatterns,
+      WebUrlPatternNatives.URLPatternToMatchPatterns(
+        new URLPattern(urlPatternStr))
+    );
+  };
+  return matchPatterns;
 }
 
 function ensureString(value) {
@@ -139,10 +152,10 @@ function unwebifyContextMenusProperties(properties) {
   const unwebifiedProperties = extractAndMapValues(properties, {
     checked: identity,
     contexts: identity,
-    documentURLPatterns: identity,
+    documentURLPatterns: $Function.bind(mapUrlPatternsToMatchPatterns, null),
     enabled: identity,
     parentId: identity,
-    targetURLPatterns: identity,
+    targetURLPatterns: $Function.bind(mapUrlPatternsToMatchPatterns, null),
     title: identity,
     type: identity,
   });
@@ -160,10 +173,10 @@ function unwebifyContextMenusCreateProperties(properties) {
     id: identity,
     checked: identity,
     contexts: identity,
-    documentURLPatterns: identity,
+    documentURLPatterns: $Function.bind(mapUrlPatternsToMatchPatterns, null),
     enabled: identity,
     parentId: identity,
-    targetURLPatterns: identity,
+    targetURLPatterns: $Function.bind(mapUrlPatternsToMatchPatterns, null),
     title: identity,
     type: identity,
   });
