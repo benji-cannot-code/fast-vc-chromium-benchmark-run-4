13175cf905c0e49e002e55f593da3d8412885a29
Jeff Yoon <jeffyoon@google.com>
2025-07-22 08:51:40 -0700

    [perf] add js2, mm, sp2 to brya, remove rendering.mobile
    rendering.mobile is failing, hogging resources. add js2, mm, sp2
    (press benchmarks) to brya
    
    Change-Id: Id06ff67242397cc9224ec8715c6532a1cfc5d49c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6776501
    Auto-Submit: Jeff Yoon <jeffyoon@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490198}

--- tools/perf/core/bot_platforms.py
+++ tools/perf/core/bot_platforms.py
@@ -561,6 +561,11 @@ _CROSSBENCH_PIXEL9 = frozenset([
     ]),
 ])
 
+_CROSSBENCH_ANDROID_AL_BRYA = frozenset([
+    _speedometer3_crossbench(arguments=['--fileserver', '--debug']),
+    _motionmark1_3_crossbench(arguments=['--fileserver', '--debug']),
+])
+
 _CROSSBENCH_ANDROID_AL = frozenset([
     _speedometer3_crossbench(arguments=['--fileserver', '--debug']),
 ])
@@ -827,6 +832,10 @@ _ANDROID_PIXEL_TANGOR_BENCHMARK_CONFIGS = PerfSuite(
         _GetBenchmarkConfig('speedometer3-minorms')
     ])
 # Android Desktop (AL)
+_ANDROID_AL_BRYA_BENCHMARK_CONFIGS = PerfSuite([
+    _GetBenchmarkConfig('jetstream2'),
+    _GetBenchmarkConfig('speedometer2'),
+])
 _ANDROID_AL_BENCHMARK_CONFIGS = PerfSuite([
     _GetBenchmarkConfig('rendering.mobile'),
 ])
@@ -991,7 +1000,7 @@ ANDROID_BRYA = PerfPlatform(
     name='android-brya-kano-i5-8gb-perf',
     description='Brya SKU kano_12th_Gen_IntelR_CoreTM_i5_1235U_8GB',
     num_shards=7,
-    benchmark_configs=_ANDROID_AL_BENCHMARK_CONFIGS,
+    benchmark_configs=_ANDROID_AL_BRYA_BENCHMARK_CONFIGS,
     platform_os='android',
     executables=None,
     crossbench=_CROSSBENCH_ANDROID_AL)
