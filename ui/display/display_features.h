908a46e419d7ed75c6ec7a0467eeef513aa95c6e
Vincent Chiang <vincentchiang@chromium.org>
2025-07-22 12:59:08 -0700

    Display: Add a feature flag for maximum display notification
    Add a placeholder display feature that will be used to enable a new
    display notification indicating maximum display supported count has
    been reached.
    
    Bug: b:425437937,b:433318990
    Change-Id: I534b94d69b5bdefd66160e37da3ad1d72f487637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6775225
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490369}

--- ui/display/display_features.h
+++ ui/display/display_features.h
@@ -83,6 +83,12 @@ BASE_DECLARE_FEATURE(kScreenWinDisplayLookupByHMONITOR);
 COMPONENT_EXPORT(DISPLAY_FEATURES)
 bool IsScreenWinDisplayLookupByHMONITOREnabled();
 
+COMPONENT_EXPORT(DISPLAY_FEATURES)
+BASE_DECLARE_FEATURE(kMaximumDisplaySupportedNotification);
+
+COMPONENT_EXPORT(DISPLAY_FEATURES)
+bool IsMaximumDisplaySupportedNotifacationEnabled();
+
 }  // namespace features
 }  // namespace display
 
