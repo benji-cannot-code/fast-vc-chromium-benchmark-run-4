af3df7087765132b0bc6ed628ea1a3f3f7760df9
Scott Haseley <shaseley@chromium.org>
2025-07-23 09:59:21 -0700

    PaintTiming: Remove redundant MarkPaintTimingInternal() call
    PaintTiming::MarkPaintTimingInternal() can be called twice per window
    per animation frame:
     1. Via FramePaintTiming d'tor, if the frame is painted (not called
        unless actually painted).
     2. Via PaintTimingDetector::NotifyPaintFinished(), unconditionally for
        each FrameView for each animation frame
    
    (2) was added in crrev.com/c/6038116 as part of the PaintTimingMixin
    work. Since (1) occurs before (2), (2) should mostly be a no-op aside
    from the added overhead --- but for soft navs, (2) might do meaningful
    work since SoftNavigationHeuristics::OnPaintFinished (also called in
    NotifyPaintFinished) affects whether or not soft navs provides a paint
    timing callback. So we end up in a scenario where two presentation
    callbacks get queued for the same frame, and each one might do
    meaningful soft nav work (one updating LCP candidates, one updating
    FCP).
    
    This CL removes (1), since (2) was added to align with the spec and that
    timing is needed for soft navs. This should be low risk since (2)
    typically happens soon after (1) (% iframes), but I'm putting it behind
    an enabled-by-default flag just in case.
    
    Bug: 378827535
    Change-Id: I5a245ce25837b78632aef1e7cafe4c26448f725e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6777944
    Reviewed-by: Noam Rosenthal <nrosenthal@google.com>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490906}

--- third_party/blink/renderer/core/paint/timing/paint_timing.cc
+++ third_party/blink/renderer/core/paint/timing/paint_timing.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/functional/callback_forward.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -65,6 +66,12 @@ struct PendingPaintTimingRecord {
   HashSet<PaintEvent> paint_events;
   base::TimeTicks rendering_update_end_time;
 };
+
+// When enabled, `PaintTiming::MarkPaintTimingInternal()` is only called from
+// `PaintTiming::NotifyPaintFinished()`.
+BASE_FEATURE(kMarkPaintTimingInternalOnlyOnFinish,
+             "MarkPaintTimingInternalOnlyOnFinish",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace
 
@@ -233,7 +240,10 @@ void PaintTiming::NotifyPaint(bool is_first_paint,
 
   if (is_first_paint)
     GetFrame()->OnFirstPaint(text_painted, image_painted);
-  MarkPaintTimingInternal();
+
+  if (!base::FeatureList::IsEnabled(kMarkPaintTimingInternalOnlyOnFinish)) {
+    MarkPaintTimingInternal();
+  }
 }
 
 // https://w3c.github.io/paint-timing/#mark-paint-timing
