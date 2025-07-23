2fa5d664a434d44ffc35250492a28c18dca9a226
Christian Dullweber <dullweber@chromium.org>
2025-07-23 09:37:43 -0700

    [geo] Pass GeolocationSettings from c++ to java site settings
    Bug: 418938557
    Change-Id: I9799c8df59235cd5d66a60d2b8edf9d07553c6d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774786
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490871}

---
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/GeolocationSetting.java
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.browser_ui.site_settings;
+
+import androidx.annotation.NonNull;
+
+import org.jni_zero.CalledByNative;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.components.content_settings.ContentSettingValues;
+
+import java.util.Objects;
+
+@NullMarked
+public final class GeolocationSetting {
+    public final @ContentSettingValues int mApproximate;
+    public final @ContentSettingValues int mPrecise;
+
+    @CalledByNative
+    public GeolocationSetting(
+            @ContentSettingValues int approximate, @ContentSettingValues int precise) {
+        mApproximate = approximate;
+        mPrecise = precise;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        return o instanceof GeolocationSetting that
+                && mApproximate == that.mApproximate
+                && mPrecise == that.mPrecise;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mApproximate, mPrecise);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "GeolocationSetting{" + mApproximate + ", " + mPrecise + "}";
+    }
+}
--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
@@ -19,6 +19,8 @@ import org.chromium.components.browsing_data.content.BrowsingDataInfo;
 import org.chromium.components.content_settings.ContentSettingValues;
 import org.chromium.components.content_settings.ContentSettingsType;
 import org.chromium.components.content_settings.SessionModel;
+import org.chromium.components.permissions.PermissionsAndroidFeatureList;
+import org.chromium.components.permissions.PermissionsAndroidFeatureMap;
 import org.chromium.content_public.browser.BrowserContextHandle;
 import org.chromium.content_public.browser.ContentFeatureList;
 import org.chromium.content_public.browser.ContentFeatureMap;
@@ -116,9 +118,16 @@ public class WebsitePermissionsFetcher {
             case ContentSettingsType.SERIAL_GUARD:
             case ContentSettingsType.USB_GUARD:
                 return WebsitePermissionsType.CHOSEN_OBJECT_INFO;
+            case ContentSettingsType.GEOLOCATION_WITH_OPTIONS:
+                if (PermissionsAndroidFeatureMap.isEnabled(
+                        PermissionsAndroidFeatureList.APPROXIMATE_GEOLOCATION_PERMISSION)) {
+                    return WebsitePermissionsType.PERMISSION_INFO;
+                }
+                break;
             default:
                 return null;
         }
+        return null;
     }
 
     /**
