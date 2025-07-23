11eb70e443cdb2ba08ea6618a8060a53bd615bde
Jonathan Ross <jonross@chromium.org>
2025-07-23 07:35:54 -0700

    Update DisplayScheduler to handle pushed BeginFrames
    We are looking to eliminate Missed BeginFrames. As the arrive in clients
    too late to make deadlines. Resulting in various backpressure mechanisms
    slowing down frame production.
    
    However `ExternalBeginFrameSourceMojo` is used without a `remote_client`
    in AR mode. In this mode the `ArCompositorFrameSink::RequestBeginFrame`
    pushes one BeginFrame at a time to Viz. Using GPU fences as backpressure
    to prevent future frames.
    
    `ExternalBeginFrameSourceMojo::IssueExternalBeginFrame` is reliant on
    the Missed BeginFrames as a side-effect of
    `viz::Display::SetNeedsOneBeginFrame` to trigger frame production. As
    otherwise the `DisplayScheduler` will subscribe to BeginFrames and wait
    for the next one.
    
    To handle this we are adding `BeginFrameArgs` to the
    `SetNeedsOneBeginFrame`. So that we can immediately use the args if we
    are not already subscribed. In all future VSyncs the `DisplayScheduler`
    will already be subscribed and trigger frame production through
    normal notifications.
    
    Bug: 40900977
    Change-Id: I84c8faedda7b1106c9539cafc528c027de34b427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6674264
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490804}

--- components/viz/service/display/display_scheduler.h
+++ components/viz/service/display/display_scheduler.h
@@ -49,7 +49,8 @@ class VIZ_SERVICE_EXPORT DisplayScheduler
   void SetDamageTracker(DisplayDamageTracker* damage_tracker) override;
   void SetVisible(bool visible) override;
   void ForceImmediateSwapIfPossible() override;
-  void SetNeedsOneBeginFrame(bool needs_draw) override;
+  void SetNeedsOneBeginFrame(const BeginFrameArgs& args,
+                             bool needs_draw) override;
   void DidSwapBuffers() override;
   void DidReceiveSwapBuffersAck() override;
   void OutputSurfaceLost() override;
