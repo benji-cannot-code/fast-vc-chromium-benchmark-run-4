c453b6b289f43f8c357938490bc41479be46ac1b
Nicola Tommasi <tommasin@chromium.org>
2025-07-23 13:05:31 -0700

    [Clank][widget] Adjust RichRadioButton dimensions to match the mocks
    This CL just fixes some RichRadioButton padding / margins to match the
    mocks.
    
    Mocks: https://screenshot.googleplex.com/6KCN76cdddMHctV
    
    Bug: 432411749
    Change-Id: I1e71ccc96680510dc0f8bf6cfc2e9e70324bd127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6765323
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491052}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RichRadioButton.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RichRadioButton.java
@@ -10,8 +10,10 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Checkable;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -49,6 +51,14 @@ public class RichRadioButton extends ConstraintLayout implements Checkable {
 
     private boolean mIsChecked;
     private boolean mIsVerticalLayout;
+
+    private int mDefaultRootLayoutPaddingStart;
+    private int mDefaultRootLayoutPaddingTop;
+    private int mDefaultRootLayoutPaddingEnd;
+    private int mDefaultRootLayoutPaddingBottom;
+
+    private ViewGroup.MarginLayoutParams mDefaultTitleLayoutParams;
+    private ViewGroup.MarginLayoutParams mDefaultRadioButtonLayoutParams;
 
     public RichRadioButton(@NonNull Context context) {
         super(context);
@@ -95,6 +105,19 @@ public class RichRadioButton extends ConstraintLayout implements Checkable {
                         info.setClassName(RadioButton.class.getName());
                     }
                 });
+
+        mDefaultRootLayoutPaddingStart = mRootItemLayout.getPaddingStart();
+        mDefaultRootLayoutPaddingTop = mRootItemLayout.getPaddingTop();
+        mDefaultRootLayoutPaddingEnd = mRootItemLayout.getPaddingEnd();
+        mDefaultRootLayoutPaddingBottom = mRootItemLayout.getPaddingBottom();
+
+        mDefaultTitleLayoutParams =
+                new LinearLayout.LayoutParams(
+                        (LinearLayout.LayoutParams) mItemTitle.getLayoutParams());
+
+        mDefaultRadioButtonLayoutParams =
+                new ConstraintLayout.LayoutParams(
+                        (ConstraintLayout.LayoutParams) mItemRadioButton.getLayoutParams());
     }
 
     /**
@@ -115,9 +138,11 @@ public class RichRadioButton extends ConstraintLayout implements Checkable {
             mItemIcon.setImageResource(iconResId);
             mItemIcon.setVisibility(VISIBLE);
             mIconContainer.setVisibility(VISIBLE);
+            resetLayoutWithIcon();
         } else {
             mItemIcon.setVisibility(GONE);
             mIconContainer.setVisibility(GONE);
+            adjustLayoutWithoutIcon();
         }
         mItemTitle.setText(title);
         if (description != null && !description.isEmpty()) {
@@ -127,6 +152,52 @@ public class RichRadioButton extends ConstraintLayout implements Checkable {
             mItemDescription.setVisibility(GONE);
         }
         setOrientation(isInternalVertical);
+    }
+
+    private void adjustLayoutWithoutIcon() {
+        int layoutVerticalPaddingPx =
+                getContext()
+                        .getResources()
+                        .getDimensionPixelSize(
+                                R.dimen.rich_radio_button_without_icon_vertical_padding);
+        mRootItemLayout.setPaddingRelative(
+                0,
+                layoutVerticalPaddingPx,
+                mRootItemLayout.getPaddingEnd(),
+                layoutVerticalPaddingPx);
+
+        int titleMarginPx =
+                getContext()
+                        .getResources()
+                        .getDimensionPixelSize(R.dimen.rich_radio_button_title_margin);
+
+        LinearLayout.LayoutParams currentTitleParams =
+                (LinearLayout.LayoutParams) mItemTitle.getLayoutParams();
+        LinearLayout.LayoutParams newTitleParams =
+                new LinearLayout.LayoutParams(currentTitleParams);
+        newTitleParams.setMargins(titleMarginPx, titleMarginPx, 0, titleMarginPx);
+        mItemTitle.setLayoutParams(newTitleParams);
+        int radioButtonMarginPx =
+                getContext()
+                        .getResources()
+                        .getDimensionPixelSize(R.dimen.rich_radio_button_radio_button_margin_end);
+        ConstraintLayout.LayoutParams currentRadioButtonParams =
+                (ConstraintLayout.LayoutParams) mItemRadioButton.getLayoutParams();
+        ConstraintLayout.LayoutParams newRadioButtonParams =
+                new ConstraintLayout.LayoutParams(currentRadioButtonParams);
+        newRadioButtonParams.setMarginEnd(radioButtonMarginPx);
+        mItemRadioButton.setLayoutParams(newRadioButtonParams);
+    }
+
+    private void resetLayoutWithIcon() {
+        mRootItemLayout.setPaddingRelative(
+                mDefaultRootLayoutPaddingStart,
+                mDefaultRootLayoutPaddingTop,
+                mDefaultRootLayoutPaddingEnd,
+                mDefaultRootLayoutPaddingBottom);
+
+        mItemTitle.setLayoutParams(mDefaultTitleLayoutParams);
+        mItemRadioButton.setLayoutParams(mDefaultRadioButtonLayoutParams);
     }
 
     /**
