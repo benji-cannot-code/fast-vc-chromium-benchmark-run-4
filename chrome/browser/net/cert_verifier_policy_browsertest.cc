a90124ec85e9c3ba51725c3b76bfb6b8912abee5
Matt Mueller <mattm@chromium.org>
2025-07-23 10:19:55 -0700

    clean up EnableCertManagementUIV2 flags in chrome/browser/net
    Bug: 390333881
    Change-Id: I582b82a6a0740cd0f00abbe5f59cc7d65f3b97ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6776166
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490941}

--- chrome/browser/net/cert_verifier_policy_browsertest.cc
+++ chrome/browser/net/cert_verifier_policy_browsertest.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/server_certificate_database_service_factory.h"  // nogncheck
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/server_certificate_database/server_certificate_database.h"  // nogncheck
@@ -66,7 +65,7 @@ class CertVerifierServicePolicyTest : public policy::PolicyTest {
   // In some cases, we may need to wait until the certificate policy updates
   // propagate.
   void UpdateProviderPolicyAndWaitForUpdate(const policy::PolicyMap& policies) {
-    // If features::kEnableCertManagementUIV2Write is enabled, the cert verifier
+    // If the ServerCertificateDatabaseService is enabled, the cert verifier
     // service update is asynchronous and the test needs to wait for the update
     // to complete.
     // Otherwise, cert changes may not make it to the verifier in time to clear
@@ -914,14 +913,7 @@ class CertVerifierServicePolicyAndUserRootsTest
     : public CertVerifierServicePolicyTest,
       public testing::WithParamInterface<bool> {
  public:
-  CertVerifierServicePolicyAndUserRootsTest() {
-    feature_list_.InitWithFeatures({features::kEnableCertManagementUIV2,
-                                    features::kEnableCertManagementUIV2Write},
-                                   {});
-  }
   bool add_certs() const { return GetParam(); }
-
-  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_P(CertVerifierServicePolicyAndUserRootsTest,
--- chrome/browser/net/cert_verifier_service_browsertest.cc
+++ chrome/browser/net/cert_verifier_service_browsertest.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_test_util.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/server_certificate_database/server_certificate_database.h"  // nogncheck
@@ -240,12 +239,6 @@ IN_PROC_BROWSER_TEST_F(CertVerifierTestCrsConstraintsSwitchTest,
 
 class CertVerifierUserSettingsTest : public PlatformBrowserTest {
  public:
-  CertVerifierUserSettingsTest() {
-    feature_list_.InitWithFeatures({features::kEnableCertManagementUIV2,
-                                    features::kEnableCertManagementUIV2Write},
-                                   {});
-  }
-
   testing::AssertionResult AddCertificateToDatabaseAndWaitForVerifierUpdate(
       net::ServerCertificateDatabase::CertInformation cert_info) {
     return AddCertificateToProfileDatabaseAndWaitForVerifierUpdate(
@@ -275,9 +268,6 @@ class CertVerifierUserSettingsTest : public PlatformBrowserTest {
     }
     return testing::AssertionSuccess();
   }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(CertVerifierUserSettingsTest, TestUserSettingsUsed) {
