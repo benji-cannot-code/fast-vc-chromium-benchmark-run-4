62809f5e3711de7b60a569b9dc38753931987383
Hsin-yu Chao <hychao@google.com>
2025-07-23 03:37:42 -0700

    Revert "android audio: Set communication mode when using Bluetooth microphone"
    This reverts commit 78d0de138346e6edfc8c33bdd22c45b3eec4f3d0.
    
    Flag use_bt_sco_for_media is the replacement of the original short-term fix.
    See the document of the original bug.
    
    call in separate window and verify YouTube can be resumed and its audio
    is playing through BT headset in HFP/SCO mode.
    
    Bug: 426458377
    Test: Connect BT headset and launch YouTube for streaming. Join Meet
    Change-Id: I03f6ba4667fafe14682fbdf649c1ed8d3f661193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774461
    Commit-Queue: Hsinyu Chao <hychao@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490712}

--- media/base/android/java/src/org/chromium/media/CommunicationDeviceSelectorPreS.java
+++ media/base/android/java/src/org/chromium/media/CommunicationDeviceSelectorPreS.java
@@ -81,11 +81,6 @@ class CommunicationDeviceSelectorPreS extends CommunicationDeviceSelector {
     }
 
     @Override
-    public boolean isBluetoothScoOn() {
-        return mAudioManager.isBluetoothScoOn();
-    }
-
-    @Override
     public void maybeSetBluetoothScoState(boolean state) {
         if (state) {
             startBluetoothSco();
