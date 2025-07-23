09dad52aefa32795278f8d0571aaed9b444face2
AbdAlRahman Gad <agad@igalia.com>
2025-07-23 05:25:41 -0700

    [ozone/wayland] Fix dangling pointer in `WaylandCursor`
    Declare `seat_` before `cursor_` to prevent a dangling pointer issue.
    This issue happens because the `cursor_` member holds a pointer to
    `seat_->pointer_`, so `seat_` must be declared first to ensure it will
    be destroyed last.
    
    Reset `cursor_` in WaylandPointer's destructor to prevent dangling
    references.
    
    Bug: 324294360
    Change-Id: I0801545c62ad121349e05a4a39cbad0fc2105c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6757952
    Reviewed-by: Orko Garai <orko@igalia.com>
    Reviewed-by: Max Ihlenfeldt <max@igalia.com>
    Commit-Queue: AbdAlRahman Gad <agad@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1490739}

--- ui/ozone/platform/wayland/host/wayland_connection.cc
+++ ui/ozone/platform/wayland/host/wayland_connection.cc
@@ -306,6 +306,11 @@ void WaylandConnection::SetCursorBitmap(const std::vector<SkBitmap>& bitmaps,
   cursor_->UpdateBitmap(bitmaps, hotspot_in_dips, buffer_scale);
 }
 
+void WaylandConnection::ResetCursor() {
+  cursor_.reset();
+  cursor_position_.reset();
+}
+
 bool WaylandConnection::IsDragInProgress() const {
   // An active drag requires a seat exist.
   return seat_ && data_device_manager_ &&
@@ -427,8 +432,7 @@ void WaylandConnection::UpdateCursor() {
       zwp_pointer_gestures_->Init();
     }
   } else {
-    cursor_.reset();
-    cursor_position_.reset();
+    ResetCursor();
   }
 }
 
--- ui/ozone/platform/wayland/host/wayland_connection.h
+++ ui/ozone/platform/wayland/host/wayland_connection.h
@@ -150,6 +150,8 @@ class WaylandConnection {
                        const gfx::Point& hotspot_in_dips,
                        int buffer_scale);
 
+  void ResetCursor();
+
   WaylandEventSource* event_source() const { return event_source_.get(); }
 
   WaylandSeat* seat() const { return seat_.get(); }
@@ -438,6 +440,7 @@ class WaylandConnection {
   // capabilities to create wl_buffers.
   std::unique_ptr<WaylandBufferFactory> buffer_factory_;
 
+  std::unique_ptr<WaylandSeat> seat_;
   std::unique_ptr<WaylandCursor> cursor_;
   std::unique_ptr<WaylandDataDeviceManager> data_device_manager_;
   std::unique_ptr<WaylandOutputManager> output_manager_;
@@ -449,7 +452,6 @@ class WaylandConnection {
   std::unique_ptr<WaylandZwpRelativePointerManager>
       zwp_relative_pointer_manager_;
   std::unique_ptr<WaylandZwpPointerGestures> zwp_pointer_gestures_;
-  std::unique_ptr<WaylandSeat> seat_;
   std::unique_ptr<WaylandBufferManagerHost> buffer_manager_host_;
   std::unique_ptr<XdgActivation> xdg_activation_;
   std::unique_ptr<XdgForeignWrapper> xdg_foreign_;
