013e7043a2a22705f1bde7e026b84e89d45d570f
Sinan Sahin <sinansahin@google.com>
2025-07-23 14:01:46 -0700

    Clean up CustomTabs.PostMessage.OnMessage
    Bug: 420443230
    Change-Id: I8a9dbd95ded4ef7613ba5d752720af84f3965e86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781525
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491088}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/PostMessageHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/PostMessageHandler.java
@@ -17,7 +17,6 @@ import androidx.browser.customtabs.PostMessageBackend;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.TerminationStatus;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.build.annotations.NullMarked;
@@ -89,8 +88,6 @@ public class PostMessageHandler implements OriginVerificationListener {
                     }
                     assumeNonNull(messagePayload.getAsString());
                     mPostMessageBackend.onPostMessage(messagePayload.getAsString(), bundle);
-                    RecordHistogram.recordBooleanHistogram(
-                            "CustomTabs.PostMessage.OnMessage", true);
                 };
     }
 
