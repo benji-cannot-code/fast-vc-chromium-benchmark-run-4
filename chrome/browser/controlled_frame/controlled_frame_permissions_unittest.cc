706b7d9e29d7a0d1fc941719a554e9ec764de656
Patryk Chodur <pchodur@google.com>
2025-07-23 13:19:48 -0700

    Add content setting for Device Attributes API.
    This commit adds content setting for the Device Attributes API, but
    without using it yet.
    
    Bug: 372467879
    Change-Id: I64ef14c65d01447715f6725e012983a7edfb3003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6703734
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Patryk Chodur <pchodur@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491059}

--- chrome/browser/controlled_frame/controlled_frame_permissions_unittest.cc
+++ chrome/browser/controlled_frame/controlled_frame_permissions_unittest.cc
@@ -205,6 +205,7 @@ TEST_F(ControlledFramePermissionsTest, Verify) {
           ON_DEVICE_SPEECH_RECOGNITION_LANGUAGES_DOWNLOADED:
       case ContentSettingsType::INITIALIZED_TRANSLATIONS:
       case ContentSettingsType::SUSPICIOUS_NOTIFICATION_IDS:
+      case ContentSettingsType::DEVICE_ATTRIBUTES:
         break;
 
       default:
