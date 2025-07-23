134fe369d55c4ec4752ae000ad86cb01d1309430
Bryan Nguyen <nguyenbryan@google.com>
2025-07-23 12:37:10 -0700

    [lensoverlay] Fix focus behavior when showing and hiding overlay.
    This change ensures that, when the overlay is shown, focus is taken by
    the overlay view prior to the tab contents web view being disabled, and
    when the overlay is hidden, focus is taken by the tab contents web
    view prior to the overlay view being disabled. This avoids focus being
    moved elsewhere in between. On Mac, the tab strip was taking focus,
    causing the hovercard of the first tab in the strip to appear
    momentarily when the overlay was invoked from the homework action chip,
    or when switching between the overlay tab and other tabs.
    
    Bug: 432499615
    Change-Id: Id744785499500cecfcfb1549f71a79c902b6a416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779016
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491031}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -1552,10 +1552,8 @@ void LensOverlayController::SetLiveBlur(bool enabled) {
 }
 
 void LensOverlayController::ShowOverlay() {
-  // Grab the tab contents web view and disable mouse and keyboard inputs to it.
   auto* contents_web_view = tab_->GetBrowserWindowInterface()->GetWebView();
   CHECK(contents_web_view);
-  contents_web_view->SetEnabled(false);
 
   // If the view already exists, we just need to reshow it.
   if (overlay_view_) {
@@ -1570,6 +1568,12 @@ void LensOverlayController::ShowOverlay() {
     // The overlay needs to be focused on show to immediately begin
     // receiving key events.
     overlay_web_view_->RequestFocus();
+
+    // Disable mouse and keyboard inputs to the tab contents web view. Do this
+    // after the overlay takes focus. If it is done before, focus will move from
+    // the contents web view to another Chrome UI element before the overlay can
+    // take focus.
+    contents_web_view->SetEnabled(false);
     return;
   }
 
@@ -1603,6 +1607,12 @@ void LensOverlayController::ShowOverlay() {
   // receiving key events.
   CHECK(overlay_web_view_);
   overlay_web_view_->RequestFocus();
+
+  // Disable mouse and keyboard inputs to the tab contents web view. Do this
+  // after the overlay takes focus. If it is done before, focus will move from
+  // the contents web view to another Chrome UI element before the overlay can
+  // take focus.
+  contents_web_view->SetEnabled(false);
 
   // Listen to the render process housing out overlay.
   overlay_web_view_->GetWebContents()
@@ -2559,6 +2569,15 @@ void LensOverlayController::HandlePageContentUploadProgress(uint64_t position,
 }
 
 void LensOverlayController::HideOverlay() {
+  // Re-enable mouse and keyboard events to the tab contents web view, and take
+  // focus before the overlay view is hidden. If it is done after, focus will
+  // move from the overlay view to another Chrome UI element before the contents
+  // web view can take focus.
+  auto* contents_web_view = tab_->GetBrowserWindowInterface()->GetWebView();
+  CHECK(contents_web_view);
+  contents_web_view->SetEnabled(true);
+  contents_web_view->RequestFocus();
+
   // Hide the overlay view, but keep the web view attached to the overlay view
   // so that the overlay can be re-shown without creating a new web view.
   preselection_widget_anchor_->SetVisible(false);
@@ -2567,10 +2586,6 @@ void LensOverlayController::HideOverlay() {
 
   SetLiveBlur(false);
   HidePreselectionBubble();
-  // Re-enable mouse and keyboard events to the tab contents web view.
-  auto* contents_web_view = tab_->GetBrowserWindowInterface()->GetWebView();
-  CHECK(contents_web_view);
-  contents_web_view->SetEnabled(true);
 }
 
 void LensOverlayController::HideOverlayAndMaybeSetLivePageState() {
