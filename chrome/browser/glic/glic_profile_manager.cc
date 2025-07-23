d6e409f570c5f5a46e063584a5b24c6acaa173cd
Glenn Hartmann <hartmanng@chromium.org>
2025-07-23 13:09:47 -0700

    [bedrock] Migrate glic_profile_manager.cc to GetBrowserWindowInterfacesOrderedByActivation()
    This migrates from BrowserList::OrderedByActivation() to
    GetBrowserWindowInterfacesOrderedByActivation() as part of project
    bedrock (https://crbug.com/421758609 ).
    
    Bug: 431671320
    Change-Id: I68ae121325336af6b6e74ff7c778de20a011de9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778513
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491054}

--- chrome/browser/glic/glic_profile_manager.cc
+++ chrome/browser/glic/glic_profile_manager.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/profiles/profile_picker.h"
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/common/chrome_features.h"
@@ -86,9 +86,10 @@ Profile* GlicProfileManager::GetProfileForLaunch() const {
   }
 
   // Look for a profile to based on most recently used browser windows
-  for (Browser* browser : BrowserList::GetInstance()->OrderedByActivation()) {
-    if (GlicEnabling::IsEnabledAndConsentForProfile(browser->profile())) {
-      return browser->profile();
+  for (BrowserWindowInterface* browser :
+       GetBrowserWindowInterfacesOrderedByActivation()) {
+    if (GlicEnabling::IsEnabledAndConsentForProfile(browser->GetProfile())) {
+      return browser->GetProfile();
     }
   }
 
