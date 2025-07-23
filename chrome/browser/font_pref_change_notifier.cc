c337c2d64644abe78ad67591ce0055e35bbe616c
Sylvain Defresne <sdefresne@chromium.org>
2025-07-23 10:19:43 -0700

    Implement PrefService observation on base::RepeatingCallbackList
    The API to observe preference changes from a PrefService was difficult
    to use as it required both the observer to stop the observation of the
    PrefService before its destruction but also to be removed from the list
    of observers before being destroyed.
    
    PrefMember and PrefChangeRegistrar helped with the latter (by removing
    themselves from the list of observer from destruction) but not with the
    former leading to many crashes if those objects outlived the service.
    
    This is especially problematic on iOS. The UI observes the preferences
    and update when they changes. However, the UI is mostly implemented in
    Objective-C and even if the UI is destroyed before the `PrefService`
    it is possible for the objects themselves to have their lifetime to
    be extended (e.g. by an animation or the autorelease pool). Thus they
    need to have an additional `-shutdown` method added, and client have
    to call it before the UI is destroyed, which is error-prone.
    
    Since the observer only has one single method, change the registration
    mechanism to use a base::RepeatingCallbackList<...> as this class handle
    both cases (i.e. the observer being destroyed before the observee, and
    vice versa).
    
    This re-implementation keeps PrefMember, and PrefChangeRegistrar in
    order to limit the size of the changelist, even if PrefChangeRegistrar
    is now mostly redundant (i.e. PrefService::AddPrefObserver(...) can be
    made public and client code converted to use that directly).
    
    Fixed: 390506652, 41447167, 41449546, 41451523, 432929675
    Change-Id: If5ebee3db7fa3c3a69c2b4804419596690c3ac91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771900
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490940}

--- chrome/browser/font_pref_change_notifier.cc
+++ chrome/browser/font_pref_change_notifier.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/font_pref_change_notifier.h"
 
+#include "base/callback_list.h"
 #include "base/check.h"
 #include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
 #include "base/observer_list.h"
 #include "base/strings/string_util.h"
 #include "chrome/common/pref_names_util.h"
@@ -36,14 +38,14 @@ void FontPrefChangeNotifier::Registrar::Unregister() {
   callback_ = FontPrefChangeNotifier::Callback();
 }
 
-FontPrefChangeNotifier::FontPrefChangeNotifier(PrefService* pref_service)
-    : pref_service_(pref_service) {
-  pref_service_->AddPrefObserverAllPrefs(this);
+FontPrefChangeNotifier::FontPrefChangeNotifier(PrefService* pref_service) {
+  subscription_ =
+      pref_service->AddAllPrefsChangedCallback(base::IgnoreArgs<PrefService*>(
+          base::BindRepeating(&FontPrefChangeNotifier::OnPreferenceChanged,
+                              base::Unretained(this))));
 }
 
 FontPrefChangeNotifier::~FontPrefChangeNotifier() {
-  pref_service_->RemovePrefObserverAllPrefs(this);
-
   // There could be a shutdown race between this class and the objects
   // registered with it. We don't want the registrars to call back into us
   // when we're deleted, so tell them to unregister now.
@@ -59,8 +61,7 @@ void FontPrefChangeNotifier::RemoveRegistrar(Registrar* registrar) {
   registrars_.RemoveObserver(registrar);
 }
 
-void FontPrefChangeNotifier::OnPreferenceChanged(PrefService* pref_service,
-                                                 std::string_view pref_name) {
+void FontPrefChangeNotifier::OnPreferenceChanged(std::string_view pref_name) {
   if (base::StartsWith(pref_name, pref_names_util::kWebKitFontPrefPrefix,
                        base::CompareCase::SENSITIVE)) {
     const std::string pref_name_string(pref_name);
