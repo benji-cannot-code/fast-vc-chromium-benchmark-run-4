7be31416a1deedea5a7eacda1755b8afe54ceb8d
Jocelyn Tran <jocelyntran@google.com>
2025-07-23 09:56:03 -0700

    [A11yPerformance] Send AXBitset data over Mojo
    Add two optional uint32 fields, bool_attributes_set_bits and
    bool_attributes_values, for efficient serialization of AXBitset data.
    
    The use of AXBitset as boolean attributes storage is controlled by the
    kAccessibilityUseAXBitset flag. When the flag is on, AXNodeData is
    serialized using the new, compact uint32 fields. When the flag is off,
    the existing map<BoolAttribute, bool> field is used.
    
    Also, change AXBitset to hold uint32_t instead of uint64_t since
    ax::mojom::BoolAttribute only has 22 values.
    
    Bug: b/422234724
    Change-Id: Ib099f4a6aeea5ba8348946fd5ba7b3f199a8c52e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6761893
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Cr-Commit-Position: refs/heads/main@{#1490901}

--- ui/accessibility/ax_node_data.h
+++ ui/accessibility/ax_node_data.h
@@ -61,6 +61,8 @@ class AX_BASE_EXPORT AXBoolStore {
   virtual void Merge(const AXBoolStore& other) = 0;
   virtual void ForEach(base::FunctionRef<void(ax::mojom::BoolAttribute, bool)>
                            callback) const = 0;
+  virtual void PopulateFromBitset(
+      const AXBitset<ax::mojom::BoolAttribute>& source_bitset) = 0;
   virtual void PopulateFromMap(
       const base::flat_map<ax::mojom::BoolAttribute, bool>& source_map) = 0;
 
@@ -98,6 +100,8 @@ class AX_BASE_EXPORT AXVectorBoolStore : public AXBoolStore {
   void ForEach(base::FunctionRef<void(ax::mojom::BoolAttribute, bool)> callback)
       const override;
   void Merge(const AXBoolStore& other) override;
+  void PopulateFromBitset(
+      const AXBitset<ax::mojom::BoolAttribute>& source_bitset) override;
   void PopulateFromMap(const base::flat_map<ax::mojom::BoolAttribute, bool>&
                            source_map) override;
   const AXBitset<ax::mojom::BoolAttribute>& GetBitsetStore() const override;
@@ -130,6 +134,8 @@ class AX_BASE_EXPORT AXBitsetBoolStore : public AXBoolStore {
   void Merge(const AXBoolStore& other) override;
   void ForEach(base::FunctionRef<void(ax::mojom::BoolAttribute, bool)> callback)
       const override;
+  void PopulateFromBitset(
+      const AXBitset<ax::mojom::BoolAttribute>& source_bitset) override;
   void PopulateFromMap(const base::flat_map<ax::mojom::BoolAttribute, bool>&
                            source_map) override;
   const AXBitset<ax::mojom::BoolAttribute>& GetBitsetStore() const override;
