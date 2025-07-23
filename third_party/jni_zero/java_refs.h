c6b0b91a55619c1cf215355c71587eb6e8c67d67
Andrew Grieve <agrieve@chromium.org>
2025-07-23 08:39:42 -0700

    JNI Zero: Add #define to allow breaking APIs without updating WebRTC uses
    Hoping to fix the roller break:
    https://ci.chromium.org/ui/p/webrtc/builders/try/android_arm64_rel/84823/overview
    
    Bug: 40173007
    Change-Id: I32bc0e962b7b6e14238276e86c7b4c07d50466f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778004
    Reviewed-by: Philip Eliasson <philipel@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490861}

--- third_party/jni_zero/java_refs.h
+++ third_party/jni_zero/java_refs.h
@@ -18,6 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/jni_zero/jni_export.h"
 #include "third_party/jni_zero/logging.h"
 
+#if !defined(JNI_ZERO_ENABLE_COMPAT_API) && defined(WEBRTC_ANDROID)
+#define JNI_ZERO_ENABLE_COMPAT_API 1
+#else
+#define JNI_ZERO_ENABLE_COMPAT_API 0
+#endif
+
 namespace jni_zero {
 
 namespace internal {
@@ -86,7 +92,9 @@ class JNI_ZERO_COMPONENT_BUILD_EXPORT JavaRef<jobject> {
     return JavaRef<jobject>(env, obj);
   }
 
+#if !JNI_ZERO_ENABLE_COMPAT_API
  protected:
+#endif
 // Takes ownership of the |obj| reference passed; requires it to be a local
 // reference type.
 #if JNI_ZERO_DCHECK_IS_ON()
@@ -94,6 +102,10 @@ class JNI_ZERO_COMPONENT_BUILD_EXPORT JavaRef<jobject> {
   JavaRef(JNIEnv* env, jobject obj);
 #else
   JavaRef(JNIEnv* env, jobject obj) : obj_(obj) {}
+#endif
+
+#if JNI_ZERO_ENABLE_COMPAT_API
+ protected:
 #endif
 
   // Used for move semantics. obj_ must have been released first if non-null.
@@ -157,7 +169,9 @@ class JavaRef : public JavaRef<jobject> {
     return JavaRef<T>(env, obj);
   }
 
+#if !JNI_ZERO_ENABLE_COMPAT_API
  protected:
+#endif
   JavaRef(JNIEnv* env, T obj) : JavaRef<jobject>(env, obj) {}
 };
 
