56e9854f74e523d6b71e3ca41388d9af2d4afc2a
Takuto Ikuta <tikuta@chromium.org>
2025-07-23 01:03:31 -0700

    Add missing includes
    This is to fix
    https://ci.chromium.org/ui/p/chromium/builders/try/linux-libfuzzer-asan-rel/2314057/overview
    and
    https://ci.chromium.org/ui/p/chromium/builders/try/linux_chromium_tsan_rel_ng/2414034/overview
    
    Bug: 40263312
    Change-Id: I6f5be6398e3d9b03b4607a343d97221bf1675710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6780026
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490654}

--- crypto/keypair.h
+++ crypto/keypair.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CRYPTO_KEYPAIR_H_
 #define CRYPTO_KEYPAIR_H_
 
+#include <array>
 #include <vector>
 
 #include "base/containers/span.h"
