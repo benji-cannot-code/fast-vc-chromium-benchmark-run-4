5f1d82197094c1d4c35a9b38f279e84a101c03cc
Elly <ellyjones@chromium.org>
2025-07-23 15:49:43 -0700

    crypto: remove RSAPrivateKey::Create
    There are no remaining clients of it; key generation now has to go via
    PrivateKey::GenerateRsa*() (which only allows generating fixed key sizes
    chosen by //crypto owners).
    
    Bug: 425863216
    Change-Id: I6aeb93048e2fd9984eab3a6d9686fa63babde921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781523
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491152}

--- crypto/rsa_private_key.h
+++ crypto/rsa_private_key.h
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crypto {
 
-// Encapsulates an RSA private key. Can be used to generate new keys, export
-// keys to other formats, or to extract a public key.
+// Encapsulates an RSA private key. Can be used to export keys to other formats
+// or to extract a public key.
 // TODO(https://crbug.com/425863216): Delete this.
 class CRYPTO_EXPORT RSAPrivateKey {
  public:
@@ -31,9 +31,6 @@ class CRYPTO_EXPORT RSAPrivateKey {
   RSAPrivateKey& operator=(const RSAPrivateKey&) = delete;
 
   ~RSAPrivateKey();
-
-  // Create a new random instance. Can return NULL if initialization fails.
-  static std::unique_ptr<RSAPrivateKey> Create(uint16_t num_bits);
 
   // Create a new instance by importing an existing private key. The format is
   // an ASN.1-encoded PrivateKeyInfo block from PKCS #8. This can return NULL if
