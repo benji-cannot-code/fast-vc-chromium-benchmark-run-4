d2321cd151a094e866ee61d35bb4e7fd36a74e4d
Mina Prapakamol <minap@google.com>
2025-07-23 09:37:28 -0700

    Use notifications global cache list for notification telemetry allowlist.
    This CL updates the notification telemetry service to check against
    global cache list instead of the high confidence allowlist.
    
    Bug: 433300912
    Change-Id: Ib4033dde8d1ddc7af2e90f5afd47414e7dc1c33b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6765751
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Commit-Queue: Mina Prapakamol <minap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490870}

--- chrome/browser/safe_browsing/notification_telemetry/notification_telemetry_service.cc
+++ chrome/browser/safe_browsing/notification_telemetry/notification_telemetry_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/push_messaging/push_messaging_service_factory.h"
 #include "chrome/browser/push_messaging/push_messaging_service_impl.h"
 #include "chrome/browser/safe_browsing/notification_telemetry/notification_telemetry_service_factory.h"
+#include "components/safe_browsing/content/browser/notification_content_detection/notifications_global_cache_list.h"
 #include "components/safe_browsing/core/browser/db/database_manager.h"
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
 #include "components/safe_browsing/core/common/utils.h"
@@ -156,9 +157,18 @@ void NotificationTelemetryService::OnRegistrationStored(
     service_worker_info.scope = scope;
     service_worker_info.registration_id = registration_id;
     service_worker_info.resources = service_worker_registration_info.resources;
-    database_manager_->CheckUrlForHighConfidenceAllowlist(
-        scope, base::BindOnce(&NotificationTelemetryService::DatabaseCheckDone,
-                              weak_factory_.GetWeakPtr(), service_worker_info));
+
+    // TODO(crbug.com/433543634): Clean up the use of `database_manager_` post
+    // GlobalCacheListForGatingNotificationProtections launch.
+    if (database_manager_ == nullptr) {
+      MaybeStoreServiceWorkerInfo(
+          service_worker_info, IsDomainInNotificationsGlobalCacheList(scope));
+    } else {
+      database_manager_->CheckUrlForHighConfidenceAllowlist(
+          scope,
+          base::BindOnce(&NotificationTelemetryService::DatabaseCheckDone,
+                         weak_factory_.GetWeakPtr(), service_worker_info));
+    }
   }
 }
 
@@ -173,6 +183,12 @@ void NotificationTelemetryService::DatabaseCheckDone(
     std::optional<
         SafeBrowsingDatabaseManager::HighConfidenceAllowlistCheckLoggingDetails>
         logging_details) {
+  MaybeStoreServiceWorkerInfo(service_worker_info, allow_listed);
+}
+
+void NotificationTelemetryService::MaybeStoreServiceWorkerInfo(
+    ServiceWorkerTelemetryInfo service_worker_info,
+    bool allow_listed) {
   base::UmaHistogramBoolean(
       "SafeBrowsing.NotificationTelemetry.ServiceWorkerScopeURL.IsAllowlisted",
       allow_listed);
