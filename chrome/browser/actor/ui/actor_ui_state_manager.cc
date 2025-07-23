400c1b73490a70e1e25676d12d5c1d2e05959308
Ken Okada <kenoss@chromium.org>
2025-07-22 17:31:54 -0700

    Revert "[AUSM] Show Toast"
    This reverts commit 51808635dd91cfcbda53fcfc001a7fe837135081.
    
    Reason for revert: Failure on linux-win-cross-rel bot https://ci.chromium.org/ui/p/chromium/builders/ci/linux-win-cross-rel
    
    Bug: b:428014205
    Original change's description:
    > [AUSM] Show Toast
    >
    > This CL adds the necessary wiring such that the toast shows under the
    > correct conditions.
    >
    > Demo: https://screencast.googleplex.com/cast/NjA2NTk0MzQxNDMwODg2NHw2NTEyMmJlZS0yZA
    >
    > DD: go/toast-one-pager
    >
    > Bug: b:428014205
    > Change-Id: I105845dae5e3f83a712f0489a85318608f646e58
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732160
    > Reviewed-by: Kevin Graney <kmg@google.com>
    > Commit-Queue: Aashna Sheth <aashnas@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1490390}
    
    Bug: b:428014205
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iabddda2042e9c0c115cde1d3a1bedcce99ea7204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6780080
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Owners-Override: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490519}

--- chrome/browser/actor/ui/actor_ui_state_manager.cc
+++ chrome/browser/actor/ui/actor_ui_state_manager.cc
@@ -9,18 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/execution_engine.h"
-#include "chrome/browser/actor/ui/actor_ui_state_manager_prefs.h"
 #include "chrome/browser/actor/ui/actor_ui_tab_controller.h"
 #include "chrome/browser/actor/variant_visitor.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser_window/public/browser_window_features.h"
-#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
-#include "chrome/browser/ui/toasts/api/toast_id.h"
-#include "chrome/browser/ui/toasts/toast_controller.h"
 #include "chrome/common/actor/action_result.h"
 #include "chrome/common/chrome_features.h"
-#include "components/prefs/pref_service.h"
 #include "components/tabs/public/tab_interface.h"
 #if BUILDFLAG(ENABLE_GLIC)
 #include "chrome/browser/glic/glic_keyed_service.h"
@@ -29,9 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace actor::ui {
 namespace {
-// The maximum number of times the closing toast should be shown for a profile.
-constexpr int kToastShownMax = 2;
-
 using tabs::TabInterface;
 
 // TODO(crbug.com/424495020): Hardcoded states; Move this out to it's own file
@@ -99,14 +89,6 @@ void LogUiChangeError(bool result) {
     LOG(DFATAL)
         << "Unexpected error when trying to update actor ui components.";
   }
-}
-
-bool MaybeShowToastViaController(BrowserWindowInterface* bwi) {
-  if (auto* controller = bwi->GetFeatures().toast_controller()) {
-    return controller->MaybeShowToast(
-        ToastParams(ToastId::kGeminiWorkingOnTask));
-  }
-  return false;
 }
 
 }  // namespace
@@ -233,11 +215,10 @@ void ActorUiStateManager::OnUiEvent(SyncUiEvent event) {
 
 #if BUILDFLAG(ENABLE_GLIC)
 void ActorUiStateManager::OnGlicUpdateFloatyState(
-    glic::GlicWindowController::State floaty_state,
-    BrowserWindowInterface* bwi) {
+    glic::GlicWindowController::State floaty_state) {
   switch (floaty_state) {
     case glic::GlicWindowController::State::kClosed:
-      MaybeShowToast(bwi);
+      MaybeShowToast();
       break;
     case glic::GlicWindowController::State::kOpen:
     case glic::GlicWindowController::State::kWaitingForGlicToLoad:
@@ -255,23 +236,8 @@ ActorUiStateManager::RegisterFloatyTaskStateChange(
 }
 #endif
 
-void ActorUiStateManager::MaybeShowToast(BrowserWindowInterface* bwi) {
-  PrefService* pref_service = actor_service_->GetProfile()->GetPrefs();
-  int toast_shown_count = pref_service->GetInteger(kToastShown);
-  if (toast_shown_count >= kToastShownMax) {
-    return;
-  }
-
-  const auto& active_tasks = actor_service_->GetActiveTasks();
-  const bool has_acting_or_reflecting_task = std::any_of(
-      active_tasks.begin(), active_tasks.end(), [](const auto& task_pair) {
-        return task_pair.second->GetState() == ActorTask::State::kActing ||
-               task_pair.second->GetState() == ActorTask::State::kReflecting;
-      });
-
-  if (has_acting_or_reflecting_task && MaybeShowToastViaController(bwi)) {
-    pref_service->SetInteger(kToastShown, toast_shown_count + 1);
-  }
+void ActorUiStateManager::MaybeShowToast() {
+  // TODO(crbug.com/428014205): Implement this function.
 }
 
 void ActorUiStateManager::MaybeUpdateProfileScopedUiState() {
