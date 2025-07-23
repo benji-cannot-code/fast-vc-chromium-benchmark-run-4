f86ab2bdad9715ae0e12bdc6aacfa1b640715fb9
Christian Dullweber <dullweber@chromium.org>
2025-07-23 13:06:12 -0700

    [geo] Rename CanBeAutoRevoked to IsAnyPermissionAllowed
    The function will be useful for pageinfo as well.
    
    Bug: 418938557
    Change-Id: Ic1a3784f396a52eb4470c6952871f8920f4217b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781045
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491053}

--- components/content_settings/core/browser/geolocation_setting_delegate.cc
+++ components/content_settings/core/browser/geolocation_setting_delegate.cc
@@ -86,7 +86,7 @@ std::optional<PermissionSetting> GeolocationSettingDelegate::FromValue(
   return setting;
 }
 
-bool GeolocationSettingDelegate::CanBeAutoRevoked(
+bool GeolocationSettingDelegate::IsAnyPermissionAllowed(
     PermissionSetting setting) const {
   return std::get<GeolocationSetting>(setting).approximate ==
          PermissionOption::kAllowed;
