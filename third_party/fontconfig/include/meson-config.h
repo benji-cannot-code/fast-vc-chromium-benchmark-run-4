46debc5d49edd1d1774ce252a5a3cf12029fa565
Dominik Röttsches <drott@chromium.org>
2025-07-23 09:41:22 -0700

    Roll src/third_party/fontconfig/src/ c527fe145..8f169b6a9 (30 commits)
    Includes build fix for building Fontations indexing
    and Rust syntax fix to address rustc warning.
    
    https://chromium.googlesource.com/external/fontconfig.git/+log/c527fe1452d4..8f169b6a9c6b
    
    $ git log c527fe145..8f169b6a9 --date=short --no-merges --format='%ad %ae %s'
    2025-07-22 drott Fix memory leak after path canonicalization in e42188283f0ee
    2025-07-22 drott [Fontations] Fix use-after-free in handling exclusive lang
    2025-07-21 drott Fix rustc warning for mismatching referred lifetimes
    2025-07-09 akira do not mix up a slash and a backslash in file object on Win32
    2025-07-08 akira test: update to pass test cases on Win32
    2025-06-12 akira test: port basic functionality check to Python
    2025-06-12 akira test: add common helper class
    2025-07-04 akira fc-lang: Add suz.orth for Sunuwar
    2025-07-03 akira test: Fix a build issue with musl libc
    2025-07-03 akira ci: install test fonts for Alpine
    2025-07-03 akira ci: Add extra setup hook
    2025-07-02 akira ci: Use venv to avoid externally-managed-environment error on Alpine
    2025-07-02 akira ci: make sure build.sh is running on bash
    2025-07-02 akira ci: Fix a typo
    2025-07-02 akira ci: Add Alpine Linux
    2025-07-02 akira ci: Update ci-tamplates
    2025-07-02 akira ci: Add FreeBSD 14.2 and drop 14.1
    2025-07-02 akira ci: Add Fedora 42 and drop 40
    2025-07-02 akira Bump version to 2.17.1
    2025-07-02 akira Bump the libtool version
    2025-07-01 akira meson: Add 'noinstall' to default-hinting, default-sub-pixel-rendering, bitmap-conf build options
    2025-06-30 akira Fix a heap buffer overflow
    2025-06-27 akira Bump version to 2.17.0
    2025-06-27 akira Fix release script
    2025-06-27 akira Bump libtool version for autotools build
    2025-05-20 trisk Avoid conflict between dgettext macro and declaration in fcint.h
    2025-06-25 drott [Fontations] Fix downstream build of indexing with Fontations
    2025-06-25 akira ci: add some usage text to build script
    2025-06-25 akira ci: fix pipeline fail of subproject build on forked project
    2025-06-23 heyuming fix: Skip empty entries in XDG_DATA_DIRS parsing
    
    Created with:
      src/third_party/fontconfig/update.sh
    
    Bug: chromium:349952802, chromium:429255379
    Change-Id: Iaa9ee3fce303eee942572c9d8223966546cfb35d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771309
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490876}

--- third_party/fontconfig/include/meson-config.h
+++ third_party/fontconfig/include/meson-config.h
@@ -149,13 +149,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define PACKAGE_NAME "fontconfig"
 
-#define PACKAGE_STRING "fontconfig 2.16.2"
+#define PACKAGE_STRING "fontconfig 2.17.1"
 
 #define PACKAGE_TARNAME "fontconfig"
 
 #define PACKAGE_URL ""
 
-#define PACKAGE_VERSION "2.16.2"
+#define PACKAGE_VERSION "2.17.1"
 
 #define SIZEOF_VOID_P 8
 
