d7cce4da6946e72af977f6c22eb51aaa940dab13
Andrii Natiahlyi <natiahlyi@google.com>
2025-07-23 07:45:31 -0700

    Add the flag for enabling the mojo API in the password manager web UI
    The flag is added as an emergency switch.
    It will be used in the following CL: https://crrev.com/c/6727218
    
    Bug: 432409279
    Change-Id: Ibec6b382740a7f71ee5cf22278109d59fa350491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779426
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1490810}

--- chrome/browser/ui/webui/password_manager/password_manager_ui.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/theme_resources.h"
 #include "components/favicon_base/favicon_url_parser.h"
 #include "components/grit/components_scaled_resources.h"
+#include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
 #include "components/password_manager/core/common/password_manager_constants.h"
 #include "components/strings/grit/components_strings.h"
@@ -634,6 +635,11 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
   source->AddBoolean("passwordChangeAvailable",
                      PasswordChangeServiceFactory::GetForProfile(profile)
                          ->IsPasswordChangeAvailable());
+
+  source->AddBoolean(
+      "enablePasswordManagerMojoApi",
+      base::FeatureList::IsEnabled(
+          password_manager::features::kEnablePasswordManagerMojoApi));
 
   content::URLDataSource::Add(
       profile, std::make_unique<FaviconSource>(
