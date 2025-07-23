eae65c4d5a911bd0113d1bf289677d7472bafd05
Arthur Milchior <arthurmilchior@google.com>
2025-07-23 07:36:49 -0700

    [iOS]Replace Browser by SceneState parameter
    The browser was not actually used. This will slightly simplify a future CL where the sign-out occur from a scene state without a browser trivially available.
    
    Bug: 432549813
    Change-Id: Ia868e0aa48675b702ce92d9e00bea52ce9a6df63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779765
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490805}

--- ios/chrome/browser/authentication/ui_bundled/signin/signin_utils.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/signin_utils.mm
@@ -145,11 +145,11 @@ bool ShouldSwitchProfileAtSignout(AuthenticationService* authentication_service,
 
 // Post an asynchronous request to switch to `profile`, running `continuation`
 // when the change completes.
-void SwitchToProfile(Browser* browser,
+void SwitchToProfile(SceneState* scene_state,
                      const std::string& profile_name,
                      ChangeProfileReason reason,
                      ChangeProfileContinuation continuation) {
-  __weak SceneState* weak_scene_state = browser->GetSceneState();
+  __weak SceneState* weak_scene_state = scene_state;
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
       base::BindOnce(&SwitchToProfileSynchronously, profile_name,
@@ -158,7 +158,7 @@ void SwitchToProfile(Browser* browser,
 
 // Post an asynchronous request to switch from a managed profile to the
 // personal profile, running `continuation` when the change completes.
-void SwitchToPersonalProfile(Browser* browser,
+void SwitchToPersonalProfile(SceneState* scene_state,
                              ChangeProfileReason reason,
                              ChangeProfileContinuation continuation) {
   ProfileManagerIOS* profile_manager =
@@ -167,7 +167,7 @@ void SwitchToPersonalProfile(Browser* browser,
       profile_manager->GetProfileAttributesStorage()->GetPersonalProfileName();
   CHECK(profile_manager->HasProfileWithName(personal_profile_name));
 
-  SwitchToProfile(browser, personal_profile_name, reason,
+  SwitchToProfile(scene_state, personal_profile_name, reason,
                   std::move(continuation));
 }
 
@@ -505,7 +505,8 @@ void ProfileSignoutRequest::Run(Browser* browser) && {
   }
 
   std::move(prepare_callback_).Run(/*will_change_profile=*/true);
-  SwitchToPersonalProfile(browser, ChangeProfileReason::kManagedAccountSignOut,
+  SwitchToPersonalProfile(scene_state,
+                          ChangeProfileReason::kManagedAccountSignOut,
                           std::move(continuation));
 }
 
@@ -544,7 +545,7 @@ void MultiProfileSignOutForProfile(
             signout_source, /*force_snackbar_over_toolbar=*/false,
             /*should_record_metrics=*/false, /*snackbar_message =*/nil,
             base::IgnoreArgs<SceneState*>(barrier));
-    SwitchToPersonalProfile(browser,
+    SwitchToPersonalProfile(browser->GetSceneState(),
                             ChangeProfileReason::kManagedAccountSignOut,
                             std::move(continuation));
   }
