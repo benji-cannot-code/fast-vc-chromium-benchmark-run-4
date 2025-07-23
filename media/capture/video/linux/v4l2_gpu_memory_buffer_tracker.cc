2c6c54d4045f84508e907d2fe0dd0a9c6a59e40b
Saifuddin Hitawala <hitawala@chromium.org>
2025-07-23 10:23:30 -0700

    [media] Remove usages of BufferFormat from V4L2GMBTracker
    Remove usages of BufferFormat to instead use SharedImageFormat in
    V4L2GpuMemoryBufferTracker.
    
    Also set the SharedImageFormat directly as const instead of
    converting const BufferFormat in couple places.
    
    Bug: 356649879
    Change-Id: Iec703f37f16fd4d8b280bdfaaa76219e7677221e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774208
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490946}

--- media/capture/video/linux/v4l2_gpu_memory_buffer_tracker.cc
+++ media/capture/video/linux/v4l2_gpu_memory_buffer_tracker.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
-#include "components/viz/common/resources/shared_image_format_utils.h"
+#include "components/viz/common/resources/shared_image_format.h"
 #include "gpu/command_buffer/client/client_shared_image.h"
 #include "gpu/command_buffer/client/shared_image_interface.h"
 #include "media/capture/video/video_capture_buffer_handle.h"
@@ -17,24 +17,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 namespace {
-// Converts the video pixel format |pixel_format| to gfx::BufferFormat.
-std::optional<gfx::BufferFormat> ToBufferFormat(VideoPixelFormat pixel_format) {
+// Converts the video pixel format |pixel_format| to viz::SharedImageFormat.
+std::optional<viz::SharedImageFormat> ToSharedImageFormat(
+    VideoPixelFormat pixel_format) {
   switch (pixel_format) {
     case PIXEL_FORMAT_NV12:
-      return gfx::BufferFormat::YUV_420_BIPLANAR;
+      return viz::MultiPlaneFormat::kNV12;
     default:
       return std::nullopt;
   }
 }
 
-gfx::BufferUsage GetBufferUsage(gfx::BufferFormat format) {
-  switch (format) {
-    case gfx::BufferFormat::YUV_420_BIPLANAR:
-      return gfx::BufferUsage::GPU_READ_CPU_READ_WRITE;
-    default:
-      // Default usage for YUV camera buffer.
-      return gfx::BufferUsage::SCANOUT_CPU_READ_WRITE;
+gfx::BufferUsage GetBufferUsage(viz::SharedImageFormat format) {
+  if (format == viz::MultiPlaneFormat::kNV12) {
+    return gfx::BufferUsage::GPU_READ_CPU_READ_WRITE;
   }
+  // Default usage for YUV camera buffer.
+  return gfx::BufferUsage::SCANOUT_CPU_READ_WRITE;
 }
 }  // namespace
 
@@ -46,13 +45,13 @@ V4L2GpuMemoryBufferTracker::~V4L2GpuMemoryBufferTracker() {
 bool V4L2GpuMemoryBufferTracker::Init(const gfx::Size& dimensions,
                                       VideoPixelFormat format,
                                       const mojom::PlaneStridesPtr& strides) {
-  std::optional<gfx::BufferFormat> gfx_format = ToBufferFormat(format);
-  if (!gfx_format) {
+  std::optional<viz::SharedImageFormat> si_format = ToSharedImageFormat(format);
+  if (!si_format) {
     DLOG(ERROR) << "Unsupported VideoPixelFormat "
                 << VideoPixelFormatToString(format);
     return false;
   }
-  gfx::BufferUsage usage = GetBufferUsage(*gfx_format);
+  gfx::BufferUsage usage = GetBufferUsage(*si_format);
 
   auto sii =
       VideoCaptureGpuChannelHost::GetInstance().GetSharedImageInterface();
@@ -64,9 +63,8 @@ bool V4L2GpuMemoryBufferTracker::Init(const gfx::Size& dimensions,
   // Setting some default usage in order to get a mappable shared image.
   const auto si_usage = gpu::SHARED_IMAGE_USAGE_CPU_WRITE_ONLY |
                         gpu::SHARED_IMAGE_USAGE_DISPLAY_READ;
-  const auto si_format = viz::GetSharedImageFormat(*gfx_format);
   shared_image_ = sii->CreateSharedImage(
-      {si_format, dimensions, gfx::ColorSpace(),
+      {*si_format, dimensions, gfx::ColorSpace(),
        gpu::SharedImageUsageSet(si_usage), "V4L2GpuMemoryBufferTracker"},
       gpu::kNullSurfaceHandle, usage);
   if (!shared_image_) {
@@ -87,11 +85,11 @@ bool V4L2GpuMemoryBufferTracker::IsReusableForFormat(
     return false;
   }
 
-  std::optional<gfx::BufferFormat> gfx_format = ToBufferFormat(format);
-  if (!gfx_format) {
+  std::optional<viz::SharedImageFormat> si_format = ToSharedImageFormat(format);
+  if (!si_format) {
     return false;
   }
-  return (viz::GetSharedImageFormat(*gfx_format) == shared_image_->format() &&
+  return (*si_format == shared_image_->format() &&
           dimensions == shared_image_->size());
 }
 
--- media/video/video_encode_accelerator_adapter.cc
+++ media/video/video_encode_accelerator_adapter.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
-#include "components/viz/common/resources/shared_image_format_utils.h"
+#include "components/viz/common/resources/shared_image_format.h"
 #include "media/base/bitstream_buffer.h"
 #include "media/base/encoder_status.h"
 #include "media/base/media_log.h"
@@ -174,8 +174,7 @@ class VideoEncodeAcceleratorAdapter::GpuMemoryBufferVideoFramePool
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     DCHECK(gfx::Rect(coded_size_).Contains(gfx::Rect(visible_size)));
 
-    const auto buffer_format = gfx::BufferFormat::YUV_420_BIPLANAR;
-    const auto si_format = viz::GetSharedImageFormat(buffer_format);
+    const auto si_format = viz::MultiPlaneFormat::kNV12;
     const auto buffer_usage =
         gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE;
 
