9e017fec06077fbd110f6ed5251eebe1cf3cddd6
Keigo Oka <oka@google.com>
2025-07-22 18:27:25 -0700

    Refactor EventForwarder to use MotionEventAndroidSource [10/n]
    This refactors `EventForwarder` to use the new `MotionEventAndroidSource`
    abstraction instead of direct JNI calls. This simplifies the code in
    `EventForwarder` and encapsulates the JNI logic within
    `MotionEventAndroidSourceJava`.
    
    This is a pure refactoring with no behavior change.
    
    Bug: 417078839
    Change-Id: Ibedbbe2c0b5b3c31668b8059738832109e8f3f74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734349
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490554}

--- ui/events/android/motion_event_android_source_java.cc
+++ ui/events/android/motion_event_android_source_java.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/android/motion_event_android_source_java.h"
 
 #include "base/android/jni_android.h"
+#include "base/memory/ptr_util.h"
 #include "ui/events/android/events_android_utils.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
@@ -15,6 +16,14 @@ using base::android::AttachCurrentThread;
 using base::android::ScopedJavaLocalRef;
 
 namespace ui {
+
+// static
+std::unique_ptr<MotionEventAndroidSource> MotionEventAndroidSourceJava::Create(
+    const base::android::JavaRef<jobject>& event,
+    bool is_latest_event_time_resampled) {
+  return base::WrapUnique(
+      new MotionEventAndroidSourceJava(event, is_latest_event_time_resampled));
+}
 
 MotionEventAndroidSourceJava::MotionEventAndroidSourceJava(
     const base::android::JavaRef<jobject>& event,
@@ -124,8 +133,7 @@ int MotionEventAndroidSourceJava::GetMetaState() const {
 
 std::unique_ptr<MotionEventAndroidSource> MotionEventAndroidSourceJava::Clone()
     const {
-  return std::make_unique<MotionEventAndroidSourceJava>(
-      event_, is_latest_event_time_resampled_);
+  return Create(event_, is_latest_event_time_resampled_);
 }
 
 }  // namespace ui
