4467a2a594fe3894961e320f55c8148e5acbd4b1
Chris Lu <thegreenfrog@google.com>
2025-07-23 09:58:12 -0700

    [ios] Disable failing Account EGTests
    Bug: 433726717
    Change-Id: If79f1173ff9b6e84ea6cc8a22f2a60a7a5c7ab1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781393
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Owners-Override: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490904}

--- ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_egtest.mm
+++ ios/chrome/browser/authentication/ui_bundled/account_menu/account_menu_egtest.mm
@@ -478,6 +478,11 @@ id<GREYMatcher> snackbarMessageMatcher(FakeSystemIdentity* identity) {
 // to be changed and the account menu view to be closed after showing managed
 // account sign-in dialog.
 - (void)testSwitchToManagedAccount {
+  // TODO(crbug.com/433726717): Test disabled on iPhones.
+  if (![ChromeEarlGrey isIPadIdiom]) {
+    EARL_GREY_TEST_DISABLED(@"Fails on iPhones.");
+  }
+
   [SigninEarlGrey signinWithFakeIdentity:kPrimaryIdentity];
   [SigninEarlGrey addFakeIdentity:kManagedIdentity1];
   [self selectIdentityDisc];
@@ -510,6 +515,11 @@ id<GREYMatcher> snackbarMessageMatcher(FakeSystemIdentity* identity) {
 }
 
 - (void)testSwitchFromManagedAccountToManagedAccount {
+  // TODO(crbug.com/433726717): Test disabled on iPhones.
+  if (![ChromeEarlGrey isIPadIdiom]) {
+    EARL_GREY_TEST_DISABLED(@"Fails on iPhones.");
+  }
+
   [SigninEarlGrey
       signinWithFakeManagedIdentityInPersonalProfile:kManagedIdentity1];
   [ChromeEarlGreyUI waitForAppToIdle];
