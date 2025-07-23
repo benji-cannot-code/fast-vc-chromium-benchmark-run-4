1b467e7c6e8d7a04b95231a80c45108463cfdb29
Linyu He <linyuh@google.com>
2025-07-23 16:14:28 -0700

    Set up ExtensionWindowControllerBridge
    ExtensionWindowControllerBridge will give Java code access to the
    cross-platform extension windowing code.
    
    Changes:
    (1) Set up a modular directory
        (browser/ui/android/extension/windowing/)
        with the skeletons of:
        ExtensionWindowControllerBridge (.java & .cc), and
        ExtensionWindowControllerBridgeFactory (.java).
    
    (2) Connect the Java ExtensionWindowControllerBridge with its native
        counterpart.
    
    (3) Compile new code into the "desktop Android" build.
        See:
        `chrome/android/BUILD.gn` (Java)
        `chrome/browser/ui/BUILD.gn` (C++)
    
    (4) Add unit tests.
    
    Bug: 424854148
    Change-Id: I9a016c90e805c990d1b1108d8a884343727a9f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6775982
    Commit-Queue: Linyu He <linyuh@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491165}

---
+++ chrome/browser/ui/android/extensions/windowing/java/src/org/chromium/chrome/browser/ui/extensions/windowing/ExtensionWindowControllerBridge.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.extensions.windowing;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.ui.browser_window.ChromeAndroidTaskFeature;
+
+/**
+ * Provides access to cross-platform extension windowing code for Java.
+ *
+ * <p>This interface extends {@link ChromeAndroidTaskFeature} so that the lifecycle of an instance
+ * will be in sync with that of a {@code ChromeAndroidTask}.
+ */
+@NullMarked
+public interface ExtensionWindowControllerBridge extends ChromeAndroidTaskFeature {}
