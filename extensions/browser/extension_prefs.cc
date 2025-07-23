e20fd39d6193da25a085390074bb899c440f577f
James Cook <jamescook@chromium.org>
2025-07-22 17:09:04 -0700

    extensions: Remove unneeded ENABLE_EXTENSIONS_CORE check
    There's a block of code in //extensions that checks both for
    ENABLE_SUPERVISED_USERS and ENABLE_EXTENSIONS_CORE. The check for
    ENABLE_EXTENSIONS_CORE isn't needed, so remove it.
    
    Bug: none
    Change-Id: Ib4e50c8d7cc04681bb63954eb1e77a569039a20b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779534
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490513}

--- extensions/browser/extension_prefs.cc
+++ extensions/browser/extension_prefs.cc
@@ -2262,7 +2262,7 @@ void ExtensionPrefs::RegisterProfilePrefs(
   // defined.
   registry->RegisterIntegerPref(kCorruptedDisableCount.name, 0);
 
-#if BUILDFLAG(ENABLE_SUPERVISED_USERS) && BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+#if BUILDFLAG(ENABLE_SUPERVISED_USERS)
   registry->RegisterBooleanPref(
       prefs::kSupervisedUserExtensionsMayRequestPermissions, false);
   registry->RegisterBooleanPref(prefs::kSkipParentApprovalToInstallExtensions,
@@ -2272,8 +2272,7 @@ void ExtensionPrefs::RegisterProfilePrefs(
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterDictionaryPref(
       prefs::kSupervisedUserLocallyParentApprovedExtensions);
-#endif  // #if BUILDFLAG(ENABLE_SUPERVISED_USERS) &&
-        // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+#endif
 
 #if !BUILDFLAG(IS_MAC)
   registry->RegisterBooleanPref(pref_names::kAppFullscreenAllowed, true);
