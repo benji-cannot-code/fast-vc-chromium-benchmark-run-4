9bd333973bdb38e8e10598d3bd600a9fafbb8288
Baran Erfani <baranerf@google.com>
2025-07-23 14:19:17 -0700

    Set the email value in listnr feedback
    This change gets the user email from profile to set the email field in
    the feedback data. Moreover, Listnr's deduplicating mechanism seems to
    be marking the feedback reports with identical first line of description
    as duplicates. Removed the new lines and added a " - " instead.
    
    Bug: b:433239418
    Change-Id: I6b56e086b89bfddfec76342a8843f44b1019d8b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6777422
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Auto-Submit: Baran Erfani <baranerf@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491103}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/chrome_features.h"
@@ -404,12 +405,22 @@ class JournalHandler {
 
     // TODO(b/430054430): Fetch and include system data to the feedback.
     feedback_data->set_description(
-        reason + "\n\n" + base::Uuid::GenerateRandomV4().AsLowercaseString());
+        reason + " - " + base::Uuid::GenerateRandomV4().AsLowercaseString());
     feedback_data->set_product_id(feedback::kGeminiWebProductId);
     feedback_data->set_category_tag(
         std::string(feedback::kGeminiWebJournalCategoryTag));
     feedback_data->set_is_offensive_or_unsafe(false);
     feedback_data->AddFile("actor-journal", journal);
+
+    signin::IdentityManager* identity_manager =
+        IdentityManagerFactory::GetForProfile(
+            actor_keyed_service_->GetProfile());
+    if (identity_manager &&
+        identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin)) {
+      feedback_data->set_user_email(
+          identity_manager->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin)
+              .email);
+    }
 
     feedback_data->CompressSystemInfo();
     feedback_data->OnFeedbackPageDataComplete();
