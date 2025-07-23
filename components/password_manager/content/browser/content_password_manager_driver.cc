b3e06c69780c28ccc733ca3f8ffbd0577c296dec
Friedrich Horschig <friedrichh@chromium.org>
2025-07-22 23:59:51 -0700

    [Autofill] Remove Filled Field type metric
    As discussed in https://crrev.com/c/6770021/comment/967d0733_2dc83808/,
    the metric isn't recorded as expected and is not used anymore.
    Therefore, obsolete it which also allows to remove the field caching the
    last triggered field in http://crrev.com/c/6770021.
    
    OBSOLETE_HISTOGRAM[Autofill.FilledFieldType.Password]=Unreliable &
    unused.
    
    Bug: 399131928
    Change-Id: I7ee59ebf9ef42d7c3a0545baebca86ad22afbdce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778180
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1490619}

--- components/password_manager/content/browser/content_password_manager_driver.cc
+++ components/password_manager/content/browser/content_password_manager_driver.cc
@@ -243,7 +243,6 @@ void ContentPasswordManagerDriver::FillField(
     const std::u16string& value,
     autofill::AutofillSuggestionTriggerSource suggestion_source) {
   if (const auto& agent = GetPasswordAutofillAgent()) {
-    LogFilledFieldType();
     agent->FillField(last_triggering_field_id_, value, suggestion_source);
   }
 }
@@ -271,7 +270,6 @@ void ContentPasswordManagerDriver::FillChangePasswordForm(
     base::OnceCallback<void(const std::optional<autofill::FormData>&)>
         form_data_callback) {
   if (const auto& agent = GetPasswordAutofillAgent()) {
-    LogFilledFieldType();
     agent->FillChangePasswordForm(
         password_element_id, new_password_element_id,
         confirm_password_element_id, old_password, new_password,
@@ -293,7 +291,6 @@ void ContentPasswordManagerDriver::FillSuggestion(
     const std::u16string& username,
     const std::u16string& password,
     base::OnceCallback<void(bool)> success_callback) {
-  LogFilledFieldType();
   GetPasswordAutofillAgent()->FillPasswordSuggestion(
       username, password, std::move(success_callback));
 }
@@ -304,7 +301,6 @@ void ContentPasswordManagerDriver::FillSuggestionById(
     const std::u16string& username,
     const std::u16string& password,
     autofill::AutofillSuggestionTriggerSource suggestion_source) {
-  LogFilledFieldType();
   GetPasswordAutofillAgent()->FillPasswordSuggestionById(
       username_element_id, password_element_id, username, password,
       suggestion_source);
@@ -314,7 +310,6 @@ void ContentPasswordManagerDriver::FillIntoFocusedField(
     bool is_password,
     const std::u16string& credential) {
   if (const auto& agent = GetPasswordAutofillAgent()) {
-    LogFilledFieldType();
     agent->FillIntoFocusedField(is_password, credential);
   }
 }
@@ -644,14 +639,6 @@ void ContentPasswordManagerDriver::LogFirstFillingResult(
           render_frame_host_))
     return;
   GetPasswordManager()->LogFirstFillingResult(this, form_renderer_id, result);
-}
-
-void ContentPasswordManagerDriver::LogFilledFieldType() {
-  bool field_classified_as_target_filling_password =
-      GetPasswordManager()->GetPasswordFormCache()->GetPasswordForm(
-          this, last_triggering_field_id_);
-  base::UmaHistogramBoolean("Autofill.FilledFieldType.Password",
-                            field_classified_as_target_filling_password);
 }
 
 const mojo::AssociatedRemote<autofill::mojom::AutofillAgent>&
--- components/password_manager/content/browser/content_password_manager_driver.h
+++ components/password_manager/content/browser/content_password_manager_driver.h
@@ -191,8 +191,6 @@ class ContentPasswordManagerDriver final
                              int32_t result) override;
 
  private:
-  void LogFilledFieldType();
-
   const mojo::AssociatedRemote<autofill::mojom::AutofillAgent>&
   GetAutofillAgent();
 
