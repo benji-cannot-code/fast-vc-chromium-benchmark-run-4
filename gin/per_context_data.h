d7e5d145fa82e8b0bc27f9e6203f904ee8eb9d55
Andreas Haas <ahaas@chromium.org>
2025-07-24 02:45:35 -0700

    [gin][cleanup]Remove unused PerContextData::runner
    Change-Id: Iba57c6318ad16d84c2c603c1d898457980fc6090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779127
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491335}

--- gin/per_context_data.h
+++ gin/per_context_data.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gin {
 
 class ContextHolder;
-class Runner;
 
 // There is one instance of PerContextData per v8::Context managed by Gin. This
 // class stores all the Gin-related data that varies per context. Arbitrary data
@@ -32,16 +31,10 @@ class GIN_EXPORT PerContextData : public base::SupportsUserData {
   // Can return NULL after the ContextHolder has detached from context.
   static PerContextData* From(v8::Local<v8::Context> context);
 
-  // The Runner associated with this context. To execute script in this context,
-  // please use the appropriate API on Runner.
-  Runner* runner() const { return runner_; }
-  void set_runner(Runner* runner) { runner_ = runner; }
-
   ContextHolder* context_holder() { return context_holder_; }
 
  private:
   raw_ptr<ContextHolder, DanglingUntriaged> context_holder_;
-  raw_ptr<Runner> runner_;
 };
 
 }  // namespace gin
