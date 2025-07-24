2bd193934cf37eba9839022b0fd80f5e30416fb9
Mihai Sardarescu <msarda@chromium.org>
2025-07-24 03:01:35 -0700

    Enable chrome.identity API unit tests on Android Desktop
    This CL compiles and runs the identity api unittests on Android Desktop.
    
    Bug: 403867969
    
    Change-Id: Ifac90536c4c6f4dba6b3a90fb5715c7fe9181155
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6780900
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491339}

--- chrome/browser/extensions/api/identity/identity_api_unittest.cc
+++ chrome/browser/extensions/api/identity/identity_api_unittest.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
 using testing::_;
 using testing::Mock;
 constexpr char kTestAccount[] = "test@example.com";
