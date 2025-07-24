ddf749bb85a55a0be276813dd60d8196c2ed2ba4
Keigo Oka <oka@google.com>
2025-07-23 18:07:01 -0700

    Remove redundant parameters from SendMouseWheelEvent [12/n]
    This CL is a part of a series of CLs to refactor how motion events are
    handled in `ContentUiEventHandler`.
    
    This CL removes redundant `x`, `y`, `ticks_x`, and `ticks_y`
    parameters from `SendMouseWheelEvent` and its JNI wrapper. The values
    are now obtained from the `MotionEventAndroid` object.
    
    Bug: 417078839
    Change-Id: Ieec0ca978ea58154c0d026cb43324ee262038fa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6736131
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491195}

--- content/public/android/java/src/org/chromium/content/browser/ContentUiEventHandler.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentUiEventHandler.java
@@ -106,11 +106,7 @@ public class ContentUiEventHandler implements UserData {
                 .sendMouseWheelEvent(
                         mNativeContentUiEventHandler,
                         event,
-                        MotionEventUtils.getEventTimeNanos(event),
-                        event.getX(),
-                        event.getY(),
-                        event.getAxisValue(MotionEvent.AXIS_HSCROLL),
-                        event.getAxisValue(MotionEvent.AXIS_VSCROLL));
+                        MotionEventUtils.getEventTimeNanos(event));
     }
 
     private boolean onMouseEvent(MotionEvent event, boolean shouldConvertToMouseEvent) {
@@ -222,14 +218,7 @@ public class ContentUiEventHandler implements UserData {
     interface Natives {
         long init(ContentUiEventHandler self, WebContents webContents);
 
-        void sendMouseWheelEvent(
-                long nativeContentUiEventHandler,
-                MotionEvent event,
-                long timeNs,
-                float x,
-                float y,
-                float ticksX,
-                float ticksY);
+        void sendMouseWheelEvent(long nativeContentUiEventHandler, MotionEvent event, long timeNs);
 
         void sendMouseEvent(
                 long nativeContentUiEventHandler,
