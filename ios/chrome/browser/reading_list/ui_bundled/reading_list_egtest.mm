5a5190dc26fdfbd955b0db18dcca76f3dda51aa4
Ernesto Izquierdo Clua <eic@google.com>
2025-07-24 16:23:00 -0700

    [ios] Mark ReadingList egtests as disabled on iPhone simulator
    Bug: 433982582
    Change-Id: Idb6311a6cad2a54a271cf9cd4867cb617cfb4163
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785267
    Owners-Override: Ernesto Izquierdo Clua <eic@google.com>
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491706}

--- ios/chrome/browser/reading_list/ui_bundled/reading_list_egtest.mm
+++ ios/chrome/browser/reading_list/ui_bundled/reading_list_egtest.mm
@@ -1171,6 +1171,12 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
 // Tests display and selection of 'Open in New Incognito Tab' in a context menu
 // on a history entry.
 - (void)testContextMenuOpenInIncognito {
+#if TARGET_IPHONE_SIMULATOR
+  // TODO(crbug.com/433982582): Flaky on an iPhone simulator.
+  if ([ChromeEarlGrey isIPhoneIdiom]) {
+    EARL_GREY_TEST_DISABLED(@"Flakes on iPhone.");
+  }
+#endif
   GURL distillablePageURL(self.testServer->GetURL(kDistillableURL));
   [self addURLToReadingList:distillablePageURL];
   LongPressEntry(kDistillableTitle);
@@ -1231,6 +1237,12 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
 
 // Tests the Share context menu action for a reading list entry.
 - (void)testContextMenuShare {
+#if TARGET_IPHONE_SIMULATOR
+  // TODO(crbug.com/433982582): Flaky on an iPhone simulator.
+  if ([ChromeEarlGrey isIPhoneIdiom]) {
+    EARL_GREY_TEST_DISABLED(@"Flakes on iPhone.");
+  }
+#endif
   GURL distillablePageURL(self.testServer->GetURL(kDistillableURL));
   [self addURLToReadingList:distillablePageURL];
   LongPressEntry(kDistillableTitle);
@@ -1241,6 +1253,12 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
 
 // Tests the Delete context menu action for a reading list entry.
 - (void)testContextMenuDelete {
+#if TARGET_IPHONE_SIMULATOR
+  // TODO(crbug.com/433982582): Flaky on an iPhone simulator.
+  if ([ChromeEarlGrey isIPhoneIdiom]) {
+    EARL_GREY_TEST_DISABLED(@"Flakes on iPhone.");
+  }
+#endif
   GURL distillablePageURL(self.testServer->GetURL(kDistillableURL));
   [self addURLToReadingList:distillablePageURL];
   LongPressEntry(kDistillableTitle);
