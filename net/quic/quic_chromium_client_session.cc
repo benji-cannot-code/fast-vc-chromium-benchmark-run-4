d27c89757185a14adf72cd23adff6351c685e830
Yoichi Osato <yoichio@chromium.org>
2025-07-23 23:59:18 -0700

    Roll src/net/third_party/quiche/src/ 12f908334..d76b2b7a8 (18 commits)
    https://quiche.googlesource.com/quiche.git/+log/12f908334b43..d76b2b7a880b
    
    $ git log 12f908334..d76b2b7a8 --date=short --no-merges --format='%ad %ae %s'
    2025-07-23 elburrito BlindSignAuth: Remove `UnpackTo` usage in tests
    2025-07-21 quiche-dev Fix build error in Chromium
    2025-07-21 vasilvv Simplify the way bitrate adjuster provides bitrate feedback.
    2025-07-21 elburrito BlindSignAuth: Serialize `AndroidAttestationData` into an `Any` proto instead of using `PackFrom`. QUICHE uses lite protos and doesn't have reflection or descriptors.
    2025-07-21 abhisinghx Modify Masque Client to create a CONNECT-UDP-Bind client
    2025-07-18 martinduke Terminate MoQT subscription with TooFarBehind if largest group is abandoned and DELIVERY_TIMEOUT is infinite.
    2025-07-18 martinduke Update version number to MoQT draft-12.
    2025-07-18 martinduke Update MoQT AuthorizationTag parameter to draft-12.
    2025-07-17 martinduke Remove `kGroupDoesNotExist` Moqt status.
    2025-07-16 rch Remove unused method QuicSpdySession::CreateOutgoingUnidirectionalStream and QuicSpdySession::ShouldCreateOutgoingUnidirectionalStream.
    2025-07-16 vasilvv Use C++20 default operators in MoQT code.
    2025-07-15 asedeno Fix up minor issues in quiche export.
    2025-07-15 martinduke Update MoQT datagrams to draft-11.
    2025-07-14 martinduke Deprecate --gfe2_reloadable_flag_quic_heapless_static_parser.
    2025-07-14 martinduke Use the default operator== in moqt_messages.h.
    2025-07-14 haoyuewang Deprecate --gfe2_reloadable_flag_quic_record_tos_byte
    2025-07-14 martinduke Update Subgroup Stream Header formats to draft-11.
    2025-07-14 elburrito BlindSignAuth: Implement GetAttestationTokens API.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: Iebdeb7c736de8ec35a7188f1ea39b458fe0df196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6780426
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491276}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -1409,10 +1409,6 @@ bool QuicChromiumClientSession::ShouldCreateOutgoingBidirectionalStream() {
   return true;
 }
 
-bool QuicChromiumClientSession::ShouldCreateOutgoingUnidirectionalStream() {
-  NOTREACHED() << "Try to create outgoing unidirectional streams";
-}
-
 bool QuicChromiumClientSession::WasConnectionEverUsed() {
   const quic::QuicConnectionStats& stats = connection()->GetStats();
   return stats.bytes_sent > 0 || stats.bytes_received > 0;
@@ -1421,11 +1417,6 @@ bool QuicChromiumClientSession::WasConnectionEverUsed() {
 QuicChromiumClientStream*
 QuicChromiumClientSession::CreateOutgoingBidirectionalStream() {
   NOTREACHED() << "CreateOutgoingReliableStreamImpl should be called directly";
-}
-
-QuicChromiumClientStream*
-QuicChromiumClientSession::CreateOutgoingUnidirectionalStream() {
-  NOTREACHED() << "Try to create outgoing unidirectional stream";
 }
 
 QuicChromiumClientStream*
--- net/quic/quic_chromium_client_session.h
+++ net/quic/quic_chromium_client_session.h
@@ -746,7 +746,6 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
 
   // quic::QuicSession methods:
   QuicChromiumClientStream* CreateOutgoingBidirectionalStream() override;
-  QuicChromiumClientStream* CreateOutgoingUnidirectionalStream() override;
   const quic::QuicCryptoClientStream* GetCryptoStream() const override;
   quic::QuicCryptoClientStream* GetMutableCryptoStream() override;
   void SetDefaultEncryptionLevel(quic::EncryptionLevel level) override;
@@ -984,7 +983,6 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
   // quic::QuicSession methods:
   bool ShouldCreateIncomingStream(quic::QuicStreamId id) override;
   bool ShouldCreateOutgoingBidirectionalStream() override;
-  bool ShouldCreateOutgoingUnidirectionalStream() override;
 
   QuicChromiumClientStream* CreateIncomingStream(
       quic::QuicStreamId id) override;
