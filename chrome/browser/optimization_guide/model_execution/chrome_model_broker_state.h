da0f313c060b7bb1068019005ae279db37b6e1e2
Steven Holte <holte@chromium.org>
2025-07-24 16:21:19 -0700

    Extract shared ModelBrokerState to components/.
    This allows re-using this object in tests.
    
    Also change unique_ptrs to inline objects.
    
    Bug: 432041523
    Change-Id: Ic90b29bca45237e0ef45756047ef4841fc1aa2dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781013
    Commit-Queue: Steven Holte <holte@chromium.org>
    Auto-Submit: Steven Holte <holte@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491703}

--- chrome/browser/optimization_guide/model_execution/chrome_model_broker_state.h
+++ chrome/browser/optimization_guide/model_execution/chrome_model_broker_state.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/optimization_guide/chrome_prediction_model_store.h"
 #include "components/optimization_guide/core/delivery/optimization_guide_model_provider.h"
+#include "components/optimization_guide/core/model_execution/model_broker_state.h"
 #include "components/optimization_guide/core/model_execution/on_device_asset_manager.h"
-#include "components/optimization_guide/core/model_execution/on_device_model_service_controller.h"
 #include "components/optimization_guide/core/optimization_guide_enums.h"
 
 namespace optimization_guide {
@@ -30,11 +30,11 @@ class ChromeModelBrokerState final
   static scoped_refptr<ChromeModelBrokerState> CreateOrGet();
 
   OnDeviceModelComponentStateManager& component_state_manager() {
-    return *component_state_manager_;
+    return model_broker_state_.component_state_manager();
   }
 
   OnDeviceModelServiceController& service_controller() {
-    return *service_controller_;
+    return model_broker_state_.service_controller();
   }
 
   ChromePredictionModelStore& prediction_model_store() {
@@ -43,15 +43,16 @@ class ChromeModelBrokerState final
 
   // Create a new asset manager to provide extra models/configs to the broker.
   std::unique_ptr<OnDeviceAssetManager> CreateAssetManager(
-      OptimizationGuideModelProvider* provider);
+      OptimizationGuideModelProvider* provider) {
+    return model_broker_state_.CreateAssetManager(provider);
+  }
 
  private:
   friend base::RefCounted<ChromeModelBrokerState>;
   ChromeModelBrokerState();
   ~ChromeModelBrokerState();
 
-  std::unique_ptr<OnDeviceModelComponentStateManager> component_state_manager_;
-  std::unique_ptr<OnDeviceModelServiceController> service_controller_;
+  ModelBrokerState model_broker_state_;
 
   ChromePredictionModelStore prediction_model_store_;
 
