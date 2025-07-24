b8f35e9db28ecbe00fcd43787d8133c933ca85cb
David Sanders <dsanders11@ucsbalum.com>
2025-07-23 23:29:02 -0700

    Remove unused include in preconnect_manager_impl.cc
    Should save ~5 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: Iad00848e039d3e6c494b1a24d9ffe5540507f6b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6782507
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491262}

--- chrome/browser/predictors/preconnect_manager_impl.cc
+++ chrome/browser/predictors/preconnect_manager_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "base/types/optional_util.h"
 #include "chrome/browser/predictors/predictors_features.h"
-#include "chrome/browser/predictors/resource_prefetch_predictor.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
