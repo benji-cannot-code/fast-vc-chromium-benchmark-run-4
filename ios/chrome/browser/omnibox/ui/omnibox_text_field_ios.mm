6a4b0be9c42bc860806939ecb305c73af0a7495e
Louis Romero <lpromero@google.com>
2025-07-23 22:12:54 -0700

    Forward the return key to the omniboxKeyboardDelegate
    This CL fixes a bug where the copied-link suggestion was not navigated
    to when hitting the return key.
    Prior to this CL, the return key was not triggering any call to
    OmniboxTextFieldIOS nor its delegate. This CL adds the same explicit
    handling as for arrow keys.
    
    Fixed: 40836724
    Change-Id: I1f444e9e7ec6c14468064102c02a9047b15f4c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6773725
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491240}

--- ios/chrome/browser/omnibox/ui/omnibox_text_field_ios.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_text_field_ios.mm
@@ -634,6 +634,8 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
     return [self.omniboxKeyboardDelegate canPerformKeyboardAction:kLeftArrow];
   } else if (action == @selector(forwardKeyCommandRight)) {
     return [self.omniboxKeyboardDelegate canPerformKeyboardAction:kRightArrow];
+  } else if (action == @selector(forwardKeyCommandReturn)) {
+    return [self.omniboxKeyboardDelegate canPerformKeyboardAction:kReturnKey];
   }
 
   // Handle pre-edit shortcuts.
@@ -739,6 +741,10 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
   [self.omniboxKeyboardDelegate performKeyboardAction:kRightArrow];
 }
 
+- (void)forwardKeyCommandReturn {
+  [self.omniboxKeyboardDelegate performKeyboardAction:kReturnKey];
+}
+
 // Arrow keys are forwarded to the main OmniboxKeyboardDelegate that will
 // dispatch them to OmniboxPopupViewController or OmniboxViewController, if they
 // don't handle them, default behavior of UITextField applies.
@@ -760,11 +766,17 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
                           modifierFlags:0
                                  action:@selector(forwardKeyCommandRight)];
 
+  UIKeyCommand* commandReturn =
+      [UIKeyCommand keyCommandWithInput:@"\r"
+                          modifierFlags:0
+                                 action:@selector(forwardKeyCommandReturn)];
+
   commandUp.wantsPriorityOverSystemBehavior = YES;
   commandDown.wantsPriorityOverSystemBehavior = YES;
   commandLeft.wantsPriorityOverSystemBehavior = YES;
   commandRight.wantsPriorityOverSystemBehavior = YES;
-  return @[ commandUp, commandDown, commandLeft, commandRight ];
+  commandReturn.wantsPriorityOverSystemBehavior = YES;
+  return @[ commandUp, commandDown, commandLeft, commandRight, commandReturn ];
 }
 
 #pragma mark - UIAccessibilityElement
