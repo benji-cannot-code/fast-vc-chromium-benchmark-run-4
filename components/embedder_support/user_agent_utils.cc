e7ce968ca8648fc2f298af0baee7980e66553760
Gurmeet Kalra <gurmeetk@google.com>
2025-07-24 16:37:54 -0700

    [XR] Use desktop user agent client hints by default for XR devices
    This change modifies the user agent client hints for XR devices to align
    with a desktop form factor.
    
    Previously, search requests from XR devices sent the default mobile user
    agent, causing some web services to incorrectly identify the XR device
    as a mobile phone. This resulted in a degraded user experience, such as
    3D models links not appearing in initial search results.
    
    The user agent for XR devices now correctly reflects the user agent
    string, similar to for a desktop (crrev.com/c/6639016). This is achieved
    by setting the platform info mobile flag and specifying device-specific
    details like architecture, CPU, and bit count.
    
    This change also resolves the issue of Chrome not playing videos hosted
    on a drive on XR devices.
    
    Test: Verified on a Moohan device by searching for "Golden Eagle". The
    "View in 3D" option now appears in the initial search results. A screen
    recording of the successful test is available at
    https://drive.google.com/file/d/14BcygWXpdrA7E8Ewu3ADNIbYPOo2XZ66/view?usp=drive_link&resourcekey=0-Gi2OiD2fMlYe-2UAd-4Qtw
    
    Bug: 381541583
    Change-Id: I45c1e2205cc0058bbc0c0f787a16cfe3841a468c
    Fixed: 380746545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769955
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Gurmeet Kalra <gurmeetk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491731}

--- components/embedder_support/user_agent_utils.cc
+++ components/embedder_support/user_agent_utils.cc
@@ -344,7 +344,10 @@ std::string GetUnifiedPlatform() {
   constexpr char kUnifiedPlatformLinuxX64[] = "X11; Linux x86_64";
 #endif
 #if BUILDFLAG(IS_ANDROID)
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP) {
+  // The Android XR device by default also has the unified platform of desktop
+  // form factor.
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP ||
+      device::features::IsXrDevice()) {
     return kUnifiedPlatformLinuxX64;
   }
   return "Linux; Android 10; K";
@@ -604,7 +607,8 @@ bool GetMobileBitForUAMetadata() {
   // Android and not a desktop form factor, AND the kUseMobileUserAgent switch
   // is present.
 #if BUILDFLAG(IS_ANDROID)
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP) {
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP ||
+      device::features::IsXrDevice()) {
     return false;
   }
 #endif
@@ -626,7 +630,8 @@ std::string GetPlatformVersion() {
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP) {
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP ||
+      device::features::IsXrDevice()) {
     return std::string();
   }
 #endif
@@ -643,7 +648,8 @@ std::string GetPlatformVersion() {
 
 std::string GetPlatformForUAMetadata() {
 #if BUILDFLAG(IS_ANDROID)
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP) {
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP ||
+      device::features::IsXrDevice()) {
     return "Linux";
   }
 #endif
@@ -788,7 +794,11 @@ std::string GetCpuArchitecture() {
 #elif BUILDFLAG(IS_IOS)
   return "arm";
 #elif BUILDFLAG(IS_ANDROID)
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP) {
+  // TODO(crbug.com/433345971) The user agent string should contain the actual
+  // cpu type information obtained from the Android device. Same for the cpu bit
+  // count in #GetCpuBitness below.
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP ||
+      device::features::IsXrDevice()) {
     return "x86";
   }
   return std::string();
@@ -827,7 +837,8 @@ std::string GetCpuBitness() {
 #elif BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_FUCHSIA)
   return "64";
 #elif BUILDFLAG(IS_ANDROID)
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP) {
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP ||
+      device::features::IsXrDevice()) {
     return "64";
   }
   return std::string();
