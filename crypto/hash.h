12ef6ce27153e43c43fa5463615dbfdf4492990e
Elly <ellyjones@chromium.org>
2025-07-23 17:23:17 -0700

    crypto: deduplicate EVPMDForHashKind
    This function has 3 copies in //crypto already and several elsewhere in
    the codebase. This change moves it onto the crypto/hash interface and
    has the other clients in //crypto call that.
    
    Fixed: 430635196
    Change-Id: Icfdf3edd365690af63afa72d586bb36d54b1767a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778234
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491186}

--- crypto/hash.h
+++ crypto/hash.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/notreached.h"
 #include "crypto/crypto_export.h"
+#include "third_party/boringssl/src/include/openssl/base.h"
 #include "third_party/boringssl/src/include/openssl/digest.h"
 
 namespace crypto::hash {
@@ -45,6 +46,13 @@ enum HashKind {
   kSha384,
   kSha512,
 };
+
+// Free functions to convert to/from bssl EVP_MDs. The functions to convert to
+// HashKind return optionals because HashKind can only represent a subset of the
+// algorithms BoringSSL supports - specifically the ones the //crypto OWNERS
+// recommend people use.
+CRYPTO_EXPORT const EVP_MD* EVPMDForHashKind(HashKind k);
+CRYPTO_EXPORT std::optional<HashKind> HashKindForEVPMD(const EVP_MD* evp_md);
 
 inline constexpr size_t DigestSizeForHashKind(HashKind k) {
   switch (k) {
