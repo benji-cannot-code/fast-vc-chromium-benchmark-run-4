d8f60e14e2a6807a0b7cf4334e27c9181f5fdf8f
Louis Romero <lpromero@google.com>
2025-07-24 00:46:35 -0700

    Fix typo in a method name
    Bug: none
    Change-Id: Ia61cecce045eaa0a633a6dda8c294d2dd4e1d081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779786
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491287}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_view_controller.mm
@@ -262,7 +262,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   if (@available(iOS 17, *)) {
     NSArray<UITrait>* traits = TraitCollectionSetForTraits(nil);
     [self registerForTraitChanges:traits
-                       withAction:@selector(updateConstraitsOnTraitChange)];
+                       withAction:@selector(updateConstraintsOnTraitChange)];
   }
 }
 
@@ -305,7 +305,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
     return;
   }
 
-  [self updateConstraitsOnTraitChange];
+  [self updateConstraintsOnTraitChange];
 }
 #endif
 
@@ -1534,7 +1534,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   self.topToolbar.pageControl.userInteractionEnabled = NO;
 }
 
-- (void)updateConstraitsOnTraitChange {
+- (void)updateConstraintsOnTraitChange {
   if (IsPinnedTabsEnabled()) {
     [self updatePinnedTabsViewControllerConstraints];
   }
