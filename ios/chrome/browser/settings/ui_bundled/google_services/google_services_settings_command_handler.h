8aa4d457ba66ce6e3c10bb420d9a4b180971deaa
Arthur Milchior <arthurmilchior@google.com>
2025-07-24 09:16:49 -0700

    [iOS]Google settings asks the user to confirm sign-out more often
    The user is asked to confirm they want to sign-out if any of the
    profile has a signed account.
    
    Also, if there is another profil with a signed-in account, the user is
    warned that this may lead to loss of unsynced data. We also show it if
    there is unsynced data in the current account.
    
    Bug: 433201414
    Change-Id: I2541d5a84150b51ab92a82004949e1f42949e344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771851
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491517}

--- ios/chrome/browser/settings/ui_bundled/google_services/google_services_settings_command_handler.h
+++ ios/chrome/browser/settings/ui_bundled/google_services/google_services_settings_command_handler.h
@@ -12,12 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Protocol to communicate user actions from the mediator to its coordinator.
 @protocol GoogleServicesSettingsCommandHandler <NSObject>
 
-// Presents the sign-out dialog to the user.
+// Presents the sign-out dialog to the user if any profile is signed-in in
+// another scene. Otherwise directly call completion with success=YES.
 // `targetRect` rect in table view system coordinate to display the signout
 // popover dialog.
-- (void)showSignOutFromTargetRect:(CGRect)targetRect
-                       completion:
-                           (signin_ui::SignoutCompletionCallback)completion;
+- (void)maybeShowSignOutFromTargetRect:(CGRect)targetRect
+                            completion:(signin_ui::SignoutCompletionCallback)
+                                           completion;
 
 @end
 
