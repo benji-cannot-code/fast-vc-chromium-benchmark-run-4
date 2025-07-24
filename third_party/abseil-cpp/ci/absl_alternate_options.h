41c7fbfa785506ee2d4644d0f3b530935953bce0
Mirko Bonadei <mbonadei@chromium.org>
2025-07-24 09:41:04 -0700

    Roll abseil_revision af00a9295f..64a9eafe33
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/af00a9295f..64a9eafe33
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/af00a9295f..64a9eafe33
    
    Bug: None
    Change-Id: I07dbfd85ac9750f4863df827a7e6003b92f896e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779281
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Auto-Submit: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491529}

--- third_party/abseil-cpp/ci/absl_alternate_options.h
+++ third_party/abseil-cpp/ci/absl_alternate_options.h
@@ -16,13 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Alternate options.h file, used in continuous integration testing to exercise
 // option settings not used by default.
 
+// SKIP_ABSL_INLINE_NAMESPACE_CHECK
+
 #ifndef ABSL_CI_ABSL_ALTERNATE_OPTIONS_H_
 #define ABSL_CI_ABSL_ALTERNATE_OPTIONS_H_
 
-#define ABSL_OPTION_USE_STD_ANY 0
-#define ABSL_OPTION_USE_STD_OPTIONAL 0
 #define ABSL_OPTION_USE_STD_STRING_VIEW 0
-#define ABSL_OPTION_USE_STD_VARIANT 0
 #define ABSL_OPTION_USE_STD_ORDERING 0
 #define ABSL_OPTION_USE_INLINE_NAMESPACE 1
 #define ABSL_OPTION_INLINE_NAMESPACE_NAME ns
