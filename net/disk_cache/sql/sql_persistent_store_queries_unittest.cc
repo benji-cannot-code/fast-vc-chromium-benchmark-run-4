930f8260b3ceb076677d17bdfd38f89ae3de2ae4
Tsuyoshi Horo <horo@chromium.org>
2025-07-24 16:53:14 -0700

    disk_cache: Hardcode 'doomed' status in SQL queries
    This change refactors the SQL queries in SqlPersistentStore to hardcode
    the doomed status (0 for live, 1 for doomed) directly into the query
    strings, rather than passing it as a bind parameter.
    
    As part of this change:
    - SQL query constants and the Query enum have been renamed for clarity
      (e.g., kOpenEntry_SelectResources -> kOpenEntry_SelectLiveResources).
    - C++ code using these queries has been updated to remove the
      now-unnecessary BindBool() calls for the doomed parameter.
    
    Bug: 422065015
    Change-Id: I7d97ee0f228e6a7e9375ec5085e7fcc9f7500d5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6780860
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491757}

--- net/disk_cache/sql/sql_persistent_store_queries_unittest.cc
+++ net/disk_cache/sql/sql_persistent_store_queries_unittest.cc
@@ -140,7 +140,7 @@ TEST_F(SqlPersistentStoreQueriesTest, AllQueriesHaveValidPlan) {
            {Query::kIndex_ResourcesDoomedLastUsed, ""},
            {Query::kIndex_ResourcesDoomedResId, ""},
            {Query::kIndex_BlobsTokenStart, ""},
-           {Query::kOpenEntry_SelectResources,
+           {Query::kOpenEntry_SelectLiveResources,
             "`--SEARCH resources USING "
             "INDEX index_resources_cache_key_doomed "
             "(cache_key=? AND doomed=?)"},
@@ -153,7 +153,7 @@ TEST_F(SqlPersistentStoreQueriesTest, AllQueriesHaveValidPlan) {
             "`--SEARCH resources USING "
             "INDEX index_resources_token "
             "(token_high=? AND token_low=?)"},
-           {Query::kDeleteDoomedEntries_SelectResources,
+           {Query::kDeleteDoomedEntries_SelectDoomedResources,
             "`--SEARCH resources USING "
             "INDEX index_resources_doomed_res_id "
             "(doomed=?)"},
@@ -163,7 +163,7 @@ TEST_F(SqlPersistentStoreQueriesTest, AllQueriesHaveValidPlan) {
             "(cache_key=? AND doomed=?)"},
            {Query::kDeleteAllEntries_DeleteFromResources, ""},
            {Query::kDeleteAllEntries_DeleteFromBlobs, ""},
-           {Query::kDeleteLiveEntriesBetween_SelectResourcesForEviction,
+           {Query::kDeleteLiveEntriesBetween_SelectLiveResources,
             "`--SEARCH resources USING "
             "INDEX index_resources_doomed_last_used "
             "(doomed=? AND last_used>? AND last_used<?)"},
@@ -210,15 +210,15 @@ TEST_F(SqlPersistentStoreQueriesTest, AllQueriesHaveValidPlan) {
             "`--SEARCH blobs USING "
             "INDEX index_blobs_token_start "
             "(token_high=? AND token_low=? AND start<?)"},
-           {Query::kCalculateSizeOfEntriesBetween_SelectFromResources,
+           {Query::kCalculateSizeOfEntriesBetween_SelectLiveResources,
             "`--SEARCH resources USING "
             "INDEX index_resources_doomed_last_used "
             "(doomed=? AND last_used>? AND last_used<?)"},
-           {Query::kOpenLatestEntryBeforeResId_SelectResources,
+           {Query::kOpenLatestEntryBeforeResId_SelectLiveResources,
             "`--SEARCH resources USING "
             "INDEX index_resources_doomed_res_id "
             "(doomed=? AND res_id<?)"},
-           {Query::kRunEviction_SelectResourcesForEviction,
+           {Query::kRunEviction_SelectLiveResources,
             "`--SEARCH resources USING "
             "INDEX index_resources_doomed_last_used "
             "(doomed=?)"},
@@ -226,11 +226,11 @@ TEST_F(SqlPersistentStoreQueriesTest, AllQueriesHaveValidPlan) {
             "`--SEARCH resources USING "
             "INDEX index_resources_token "
             "(token_high=? AND token_low=?)"},
-           {Query::kCalculateResourceEntryCount_SelectCountFromResources,
+           {Query::kCalculateResourceEntryCount_SelectCountFromLiveResources,
             "`--SEARCH resources USING "
             "COVERING INDEX index_resources_doomed_res_id "
             "(doomed=?)"},
-           {Query::kCalculateTotalSize_SelectTotalSizeFromResources,
+           {Query::kCalculateTotalSize_SelectTotalSizeFromLiveResources,
             "`--SEARCH resources USING "
             "INDEX index_resources_doomed_res_id "
             "(doomed=?)"}});
