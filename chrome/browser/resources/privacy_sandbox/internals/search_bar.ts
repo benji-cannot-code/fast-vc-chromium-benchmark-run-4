b2116cfc56f94313f13641cd0cfbd3e4e938b86f
Harry Nguyen <nguyenharry@google.com>
2025-07-24 16:13:32 -0700

    [PS Internals] Create Search bar UI
    This change introduces the foundational UI for a new search bar on the
    Privacy Sandbox Internals page (chrome://privacy-sandbox-internals).
    This is the first of several changes to implement a full search and
    filtering capability.
    
    UI Changes:
    
    - A new reusable <search-bar> Web Component has been created
    (search_bar.html, search_bar.ts).
    
    - This component is now included at the top of the main content area in
    internals_page.html, styled to align with Chromium's WebUI standards.
    
    - The page's routing logic has been corrected to ensure it reliably
    defaults to the "Tracking Protection" tab and updates the URL on initial
    load, fixing a race condition.
    
    **Demo**: https://screenshot.googleplex.com/6tKGuwv95XztrDx
    
    Bug: 432111868
    Change-Id: I80a609e5b63ab0e7e62e5ce3b467b4fa90488e05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6735953
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Harry Nguyen <nguyenharry@google.com>
    Reviewed-by: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491690}

---
+++ chrome/browser/resources/privacy_sandbox/internals/search_bar.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import { CustomElement } from 'chrome://resources/js/custom_element.js';
+
+import { getTemplate } from './search_bar.html.js';
+
+export class SearchBarElement extends CustomElement {
+  static override get template() {
+    return getTemplate();
+  }
+
+  static get is() {
+    return 'search-bar';
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'search-bar': SearchBarElement;
+  }
+}
+
+customElements.define(SearchBarElement.is, SearchBarElement);
