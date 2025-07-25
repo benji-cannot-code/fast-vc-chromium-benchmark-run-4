e00fcdd332d1b5e5e0c6f656d6c644d242550cf0
Scott Haseley <shaseley@chromium.org>
2025-07-25 10:23:47 -0700

    [soft navs] Refactor AddPaintedArea to take PaintTimingRecord
    Follow-up to crrev.com/c/6781530. Refactor AddPaintedArea() to take
    PaintTimingRecord* instead of specializations for TextRecord* and
    ImageRecord*, since we can now share more code.
    
    Bug: 434130450
    
    Change-Id: Id67d074b1f71a602145cc6d624c766ef0e6be5c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6782230
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492155}

--- third_party/blink/renderer/core/timing/soft_navigation_context.h
+++ third_party/blink/renderer/core/timing/soft_navigation_context.h
@@ -22,11 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class Node;
-class TextRecord;
 class ImageRecord;
+class Node;
 class LargestContentfulPaintCalculator;
 struct LargestContentfulPaintDetails;
+class PaintTimingRecord;
+class TextRecord;
 
 class CORE_EXPORT SoftNavigationContext
     : public GarbageCollected<SoftNavigationContext> {
@@ -90,8 +91,7 @@ class CORE_EXPORT SoftNavigationContext
   // for this Node*.
   bool IsNeededForTiming(Node* node);
   // Reports a new contentful paint area to this context, and the Node painted.
-  bool AddPaintedArea(TextRecord*);
-  bool AddPaintedArea(ImageRecord*);
+  bool AddPaintedArea(PaintTimingRecord*);
   // Returns true if we update the total attributed area this animation frame.
   // Used to check if it is worthwhile to call `SatisfiesSoftNavPaintCriteria`.
   bool OnPaintFinished();
@@ -112,8 +112,6 @@ class CORE_EXPORT SoftNavigationContext
   void Trace(Visitor* visitor) const;
 
  private:
-  bool AddPaintedAreaInternal(Node* node, const gfx::RectF& rect);
-
   // Pre-Increment `last_context_id_` such that the newest context uses the
   // largest value and can be used to identify the most recent context.
   const uint64_t context_id_ = ++last_context_id_;
