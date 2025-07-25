483a0d1d03cd1c10493955ca35f16324aab9e0f8
Ben Chin <lungc@chromium.org>
2025-07-25 04:59:36 -0700

    Add hover states to Recent Tabs list
    Implements a hover effect for individual items in the recent tabs list
    and for the collapse/expand icon, providing better visual feedback to
    the user. Hover state after changed: https://photos.app.goo.gl/ttBaw8t67Fjp6wGk6
    
    Bug: 422042079
    Test: Recent tabs on LFF devices should have hover state
    Change-Id: Ifd36e36a221e7f12b0d82c1158b63907fa2b4c53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6768663
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: ben chin <lungc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491972}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
@@ -45,6 +45,7 @@ import org.chromium.chrome.browser.ui.favicon.FaviconHelper.DefaultFaviconHelper
 import org.chromium.chrome.browser.ui.favicon.FaviconHelper.FaviconImageCallback;
 import org.chromium.chrome.browser.ui.favicon.FaviconUtils;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
+import org.chromium.components.browser_ui.widget.HoverHighlightViewListener;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
 import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.components.tab_groups.TabGroupColorId;
@@ -141,6 +142,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
     private final int mFaviconSize;
     private boolean mHasForeignDataRecorded;
     private final RoundedIconGenerator mIconGenerator;
+    private final HoverHighlightViewListener mHoverListener;
 
     /**
      * A generic group of objects to be shown in the RecentTabsRowAdapter, such as the list of
@@ -223,6 +225,8 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                                 LayoutInflater.from(mActivity)
                                         .inflate(R.layout.recent_tabs_group_item, parent, false);
             }
+            groupView.setOnHoverListener(mHoverListener);
+            groupView.getExpandCollapseIcon().setOnHoverListener(mHoverListener);
             configureGroupView(groupView, isExpanded);
             return groupView;
         }
@@ -654,6 +658,8 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
             viewHolder.textView.setContentDescription(null);
             // Reset the icon view.
             viewHolder.iconView.setVisibility(View.GONE);
+            // Explicitly telling this specific row's layout should react to hover events.
+            viewHolder.itemLayout.setOnHoverListener(mHoverListener);
             Resources res = mActivity.getResources();
             if (isHistoryLink(childPosition)) {
                 viewHolder.textView.setText(R.string.show_full_history);
@@ -715,7 +721,6 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                 }
             } else {
                 RecentlyClosedTab tab = (RecentlyClosedTab) entry;
-
                 String title = TitleUtil.getTitleForDisplay(tab.getTitle(), tab.getUrl());
                 viewHolder.textView.setText(title);
 
@@ -882,6 +887,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
         mFaviconSize = resources.getDimensionPixelSize(R.dimen.default_favicon_size);
 
         mIconGenerator = FaviconUtils.createCircularIconGenerator(activity);
+        mHoverListener = new HoverHighlightViewListener();
 
         RecordHistogram.recordEnumeratedHistogram(
                 "HistoryPage.OtherDevicesMenu",
