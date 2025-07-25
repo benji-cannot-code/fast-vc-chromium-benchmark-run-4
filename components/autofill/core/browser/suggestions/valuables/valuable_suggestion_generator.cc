b3ed5e1034ca8b561280c31f607c4066db4766b8
Norge Vizcay <vizcay@google.com>
2025-07-25 04:14:42 -0700

    Set up proper includes in valuables suggestion generator.
    Bug: 434122759
    Change-Id: Ic8228177e31c00bb5c045ddecd212d58c5bf6bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787908
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491960}

--- components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
@@ -4,8 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.h"
 
+#include <algorithm>
+#include <iterator>
 #include <vector>
 
+#include "base/i18n/case_conversion.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/data_model/valuables/loyalty_card.h"
 #include "components/autofill/core/browser/suggestions/suggestion.h"
@@ -13,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "url/origin.h"
+#include "ui/gfx/image/image.h"
+#include "url/gurl.h"
 
 namespace autofill {
 namespace {
