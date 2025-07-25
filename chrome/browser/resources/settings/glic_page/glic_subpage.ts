8961bf483b5d55f334b541b624c42610950f2742
Andrea Gomez <andreaxg@google.com>
2025-07-25 15:41:25 -0700

    Add extensions management link behind a flag
    http://screen/783DFeUuepSfLis of UI added.
    
    Bug: 421183189
    Change-Id: I828cd042b08c05df74d0c34159fdb5006a0ed408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778443
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492330}

--- chrome/browser/resources/settings/glic_page/glic_subpage.ts
+++ chrome/browser/resources/settings/glic_page/glic_subpage.ts
@@ -111,6 +111,13 @@ export class SettingsGlicSubpageElement extends SettingsGlicSubpageElementBase {
         },
       },
 
+      glicExtensionsFeatureEnabled_: {
+        type: Boolean,
+        value: () => {
+          return loadTimeData.getBoolean('glicExtensionsFeatureEnabled');
+        },
+      },
+
       glicUserStatusCheckFeatureEnabled_: {
         type: Boolean,
         value: () =>
@@ -173,6 +180,7 @@ export class SettingsGlicSubpageElement extends SettingsGlicSubpageElementBase {
       MetricsBrowserProxyImpl.getInstance();
   declare private tabAccessToggleExpanded_: boolean;
   declare private closedCaptionsFeatureEnabled_: boolean;
+  declare private glicExtensionsFeatureEnabled_: boolean;
   declare private glicUserStatusCheckFeatureEnabled_: boolean;
   declare private locationSubLabel_: string;
   declare private locationLearnMoreUrl_: string;
@@ -304,6 +312,12 @@ export class SettingsGlicSubpageElement extends SettingsGlicSubpageElementBase {
   private onActivityRowClick_() {
     OpenWindowProxyImpl.getInstance().openUrl(
         this.i18n('glicActivityButtonUrl'));
+  }
+
+  private onExtensionsRowClick_() {
+    // TODO(crbug.com/434213151): Append url param when ready.
+    const url = new URL(this.i18n('glicExtensionsManagementUrl'));
+    OpenWindowProxyImpl.getInstance().openUrl(url.toString());
   }
 
   private onShortcutsLearnMoreClick_() {
