b4f0ebd15b8fad28a6454408c4bce2ea60c751e0
Federica Germinario <fedegermi@google.com>
2025-07-25 08:55:46 -0700

    [ios] Fix AddressViewControllerKeyboardAccessoryUpgradeDisabledTestCase
    This test is failing because some idling resources are busy.
    
    Fixed: 434130235
    Change-Id: I84ce3ff28d64184827d51e830de0a9d376c45dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787789
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492084}

--- ios/chrome/browser/autofill/ui_bundled/manual_fill/address_view_controller_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/manual_fill/address_view_controller_egtest.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/chrome/test/scoped_eg_synchronization_disabler.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/web/public/test/element_selector.h"
 #import "net/test/embedded_test_server/embedded_test_server.h"
@@ -614,6 +615,9 @@ void OpenAddressManualFillViewWithNoSavedAddresses() {
       waitForUIElementToAppearWithMatcher:NavigationBarDoneButton()];
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
       performAction:grey_tap()];
+
+  // Synchronization off due to an infinite spinner.
+  ScopedSynchronizationDisabler disabler;
 
   // Check that the address details page is no longer visible.
   [[EarlGrey selectElementWithMatcher:AddressDetailsPage()]
