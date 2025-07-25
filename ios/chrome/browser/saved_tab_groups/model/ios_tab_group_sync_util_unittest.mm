15db520ad54ad9258766e6b44ebc17d44e3ca241
Sylvain Defresne <sdefresne@chromium.org>
2025-07-25 05:05:02 -0700

    [ios] Convert incorrect use of FakeBrowserState
    Code in //ios/chrome/browser *must not* use FakeBrowserState because
    it does not sub-class ProfileIOS* and thus using it is unsafe as all
    code in //ios/chrome/browser assumes all web::BrowserState* can be
    safely cast to ProfileIOS*.
    
    Bug: none
    Change-Id: I541642a2bffcc6b633fd362d974df70d8e4096a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6783408
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491978}

--- ios/chrome/browser/saved_tab_groups/model/ios_tab_group_sync_util_unittest.mm
+++ ios/chrome/browser/saved_tab_groups/model/ios_tab_group_sync_util_unittest.mm
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/snapshots/model/snapshot_browser_agent.h"
 #import "ios/chrome/browser/snapshots/model/snapshot_tab_helper.h"
 #import "ios/web/public/navigation/navigation_item.h"
-#import "ios/web/public/test/fakes/fake_browser_state.h"
 #import "ios/web/public/test/fakes/fake_navigation_context.h"
 #import "ios/web/public/test/fakes/fake_navigation_manager.h"
 #import "ios/web/public/test/fakes/fake_web_state.h"
@@ -333,23 +332,33 @@ TEST_F(TabGroupSyncUtilTest, ShouldUpdateHistory) {
   auto navigation_manager = std::make_unique<web::FakeNavigationManager>();
   navigation_manager->SetLastCommittedItem(item.get());
 
+  auto profile = TestProfileIOS::Builder().Build();
   auto web_state = std::make_unique<web::FakeWebState>();
   web_state->SetNavigationManager(std::move(navigation_manager));
-
-  auto profile = std::make_unique<web::FakeBrowserState>();
-  profile->SetOffTheRecord(false);
   web_state->SetBrowserState(profile.get());
 
   web::FakeNavigationContext navigation;
   navigation.SetWebState(web_state.get());
   navigation.SetHasCommitted(true);
 
+  std::unique_ptr<web::NavigationItem> otr_item = web::NavigationItem::Create();
+  otr_item->SetTransitionType(ui::PAGE_TRANSITION_TYPED);
+
+  auto otr_navigation_manager = std::make_unique<web::FakeNavigationManager>();
+  otr_navigation_manager->SetLastCommittedItem(otr_item.get());
+
+  auto otr_web_state = std::make_unique<web::FakeWebState>();
+  otr_web_state->SetNavigationManager(std::move(otr_navigation_manager));
+  otr_web_state->SetBrowserState(profile->GetOffTheRecordProfile());
+
+  web::FakeNavigationContext otr_navigation;
+  otr_navigation.SetWebState(otr_web_state.get());
+  otr_navigation.SetHasCommitted(true);
+
   EXPECT_TRUE(ShouldUpdateHistory(&navigation));
 
   // Off the record navigation.
-  profile->SetOffTheRecord(true);
-  EXPECT_FALSE(ShouldUpdateHistory(&navigation));
-  profile->SetOffTheRecord(false);
+  EXPECT_FALSE(ShouldUpdateHistory(&otr_navigation));
 
   // Back / forward navigation.
   EXPECT_TRUE(ShouldUpdateHistory(&navigation));
@@ -374,11 +383,9 @@ TEST_F(TabGroupSyncUtilTest, IsSaveableNavigation) {
   auto navigation_manager = std::make_unique<web::FakeNavigationManager>();
   navigation_manager->SetLastCommittedItem(item.get());
 
+  auto profile = TestProfileIOS::Builder().Build();
   auto web_state = std::make_unique<web::FakeWebState>();
   web_state->SetNavigationManager(std::move(navigation_manager));
-
-  auto profile = std::make_unique<web::FakeBrowserState>();
-  profile->SetOffTheRecord(false);
   web_state->SetBrowserState(profile.get());
 
   web::FakeNavigationContext navigation;
@@ -452,8 +459,21 @@ TEST_F(TabGroupSyncUtilTest, IsSaveableNavigation) {
 
   // Off the record navigation.
   EXPECT_TRUE(IsSaveableNavigation(&navigation));
-  profile->SetOffTheRecord(true);
-  EXPECT_FALSE(IsSaveableNavigation(&navigation));
+
+  std::unique_ptr<web::NavigationItem> otr_item = web::NavigationItem::Create();
+  otr_item->SetTransitionType(ui::PAGE_TRANSITION_TYPED);
+
+  auto otr_navigation_manager = std::make_unique<web::FakeNavigationManager>();
+  otr_navigation_manager->SetLastCommittedItem(item.get());
+
+  auto otr_web_state = std::make_unique<web::FakeWebState>();
+  otr_web_state->SetNavigationManager(std::move(otr_navigation_manager));
+  otr_web_state->SetBrowserState(profile->GetOffTheRecordProfile());
+
+  web::FakeNavigationContext otr_navigation;
+  otr_navigation.SetWebState(web_state.get());
+
+  EXPECT_FALSE(IsSaveableNavigation(&otr_navigation));
 }
 
 // Tests the `IsTabGroupShared` method with a shared group.
--- ios/chrome/browser/saved_tab_groups/model/tab_group_local_update_observer_unittest.mm
+++ ios/chrome/browser/saved_tab_groups/model/tab_group_local_update_observer_unittest.mm
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/web_state_list/test/web_state_list_builder_from_description.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/web/public/navigation/navigation_item.h"
-#import "ios/web/public/test/fakes/fake_browser_state.h"
 #import "ios/web/public/test/fakes/fake_navigation_context.h"
 #import "ios/web/public/test/fakes/fake_navigation_manager.h"
 #import "ios/web/public/test/fakes/fake_web_state.h"
