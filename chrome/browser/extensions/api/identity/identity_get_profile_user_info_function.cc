d4a633bbfe5e4e4fbe292e69109b24b87a588e20
David Roger <droger@chromium.org>
2025-07-25 01:26:05 -0700

    [extensions] Avoid a crash in chrome.identity.getProfileUserInfo
    SyncService can be null in certain scenarios (e.g. when using
    --enable-local-sync-backend). In this case, the code should not attempt
    to read its settings.
    
    Fixed: 433499860
    Change-Id: Id7d38e034b05489107a940b52c3c016155ce1ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779372
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491907}

--- chrome/browser/extensions/api/identity/identity_get_profile_user_info_function.cc
+++ chrome/browser/extensions/api/identity/identity_get_profile_user_info_function.cc
@@ -42,8 +42,9 @@ CoreAccountInfo GetAccountInfoFromProfileDetails(
       return primary_account_info;
     case api::identity::AccountStatus::kNone:
     case api::identity::AccountStatus::kSync:
-      return sync_service->GetUserSettings()->GetSelectedTypes().Has(
-                 syncer::UserSelectableType::kExtensions)
+      return sync_service &&
+                     sync_service->GetUserSettings()->GetSelectedTypes().Has(
+                         syncer::UserSelectableType::kExtensions)
                  ? primary_account_info
                  : CoreAccountInfo();
   }
