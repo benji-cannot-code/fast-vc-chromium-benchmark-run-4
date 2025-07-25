1da0d3d8ee8e3129e2176a016b2a7b8cad1767a3
Michael Crouse <mcrouse@chromium.org>
2025-07-25 09:55:25 -0700

    Added `isMediaActive` and `isTabContentCaptured` to TabData.
    These new properties correspond to tab indicators: media content is
    playing and/or screen content is being captured.
    
    This updates the states alongside basic tab changes that are already
    tracked. A future change can update the state when audio and screen
    capture change.
    
    Change-Id: I551c7fff7f4a639ce631cd93bf9f3445ccab6d0a
    Bug: b:425683537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778447
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492125}

--- chrome/browser/resources/glic/glic_api_impl/glic_api_host.ts
+++ chrome/browser/resources/glic/glic_api_impl/glic_api_host.ts
@@ -1324,6 +1324,8 @@ function tabDataToClient(tabData: TabDataMojo|null, extras: ResponseExtras):
   }
 
   const isObservable = optionalToClient(tabData.isObservable);
+  const isMediaActive = optionalToClient(tabData.isMediaActive);
+  const isTabContentCaptured = optionalToClient(tabData.isTabContentCaptured);
   return {
     tabId: tabIdToClient(tabData.tabId),
     windowId: windowIdToClient(tabData.windowId),
@@ -1333,6 +1335,8 @@ function tabDataToClient(tabData: TabDataMojo|null, extras: ResponseExtras):
     faviconUrl,
     documentMimeType: tabData.documentMimeType,
     isObservable,
+    isMediaActive,
+    isTabContentCaptured,
   };
 }
 
