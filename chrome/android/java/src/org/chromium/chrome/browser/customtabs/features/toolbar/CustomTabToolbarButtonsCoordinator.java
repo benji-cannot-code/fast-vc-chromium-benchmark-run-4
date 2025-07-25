6831aa6364d97bb1df866181ef8e5323e0da60c5
Jinsuk Kim <jinsukkim@chromium.org>
2025-07-25 10:39:18 -0700

    [CCT][ToolbarRefactor] Show MTB in toolbar
    MTB starts showning up in the toolbar. Follow-up CL will handle:
     - CPA background aligning
     - Migrating VisibilityRule#adjustMinimizeButtonPriority
     - Fallback UI (menu dot)
     - Metrics
    
    Bug: 428261559
    Change-Id: I1cb4db0cdab1cdbb899c4441ae1d60f4c76fed07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6784003
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492169}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbarButtonsCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbarButtonsCoordinator.java
@@ -55,6 +55,7 @@ public class CustomTabToolbarButtonsCoordinator
             mCustomActionButtonsMcp;
     private final PropertyModel mModel;
     private final CustomTabToolbarButtonsMediator mMediator;
+    private final boolean mIsOptionalButtonSupported;
 
     public CustomTabToolbarButtonsCoordinator(
             Activity activity,
@@ -81,6 +82,7 @@ public class CustomTabToolbarButtonsCoordinator
                         // #setCloseButtonClickHandler.
                         : new CloseButtonData();
 
+        mIsOptionalButtonSupported = intentDataProvider.isOptionalButtonSupported();
         int toolbarWidth = CustomTabDimensionUtils.getInitialWidth(activity, intentDataProvider);
         boolean omniboxEnabled = omniboxParams != null;
         boolean titleVisible =
@@ -138,12 +140,12 @@ public class CustomTabToolbarButtonsCoordinator
 
     @Override
     public void setOptionalButtonData(@Nullable ButtonData buttonData) {
-        mMediator.setOptionalButtonData(buttonData);
+        if (mIsOptionalButtonSupported) mMediator.setOptionalButtonData(buttonData);
     }
 
     @Override
     public boolean isOptionalButtonVisible() {
-        return false;
+        return mIsOptionalButtonSupported ? mMediator.isOptionalButtonVisible() : false;
     }
 
     /**
