2bd4f329006fda5e9465f766f7c239f00a10d4bd
Darryl James <dljames@chromium.org>
2025-07-25 15:17:12 -0700

    Revert "SmartCard: Add some more coverage to SmartCardReaderTrackerImpl"
    This reverts commit 92ffad1909a8ee8d6c29c7af0ca0e61f63a0a61d.
    
    Reason for revert: Causing failures on Linux Chromium OS ASan LSan Tests (1)
    
    See crbug.com/434250431 for more details.
    
    Bug: 434182533, 434250431
    Original change's description:
    > SmartCard: Add some more coverage to SmartCardReaderTrackerImpl
    >
    > This:
    > 1) Adds test coverage to some yet-uncovered corner cases
    > 2) Refactors existing unit tests a bit
    >
    > Bug: 434182533
    > Change-Id: I307eef2b07ce2c5c428d04186438432b03aeebbd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787935
    > Commit-Queue: Luke Klimek <zgroza@chromium.org>
    > Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1492204}
    
    Bug: 434182533
    Change-Id: I332cc211e7d2a4e52cef1daf3b686a4bfa8acd62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6789371
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Luke Klimek <zgroza@chromium.org>
    Owners-Override: Darryl James <dljames@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492322}

--- content/browser/smart_card/mock_smart_card_context_factory.cc
+++ content/browser/smart_card/mock_smart_card_context_factory.cc
@@ -18,16 +18,6 @@ MockSmartCardContextFactory::MockSmartCardContextFactory() {
   context_receivers_.set_disconnect_handler(
       base::BindRepeating(&MockSmartCardContextFactory::ContextDisconnected,
                           base::Unretained(this)));
-
-  ON_CALL(*this, CreateContext)
-      .WillByDefault([this](CreateContextCallback callback) {
-        mojo::PendingRemote<device::mojom::SmartCardContext> context_remote;
-        context_receivers_.Add(this,
-                               context_remote.InitWithNewPipeAndPassReceiver());
-
-        std::move(callback).Run(SmartCardCreateContextResult::NewContext(
-            std::move(context_remote)));
-      });
 }
 
 MockSmartCardContextFactory::~MockSmartCardContextFactory() = default;
@@ -37,6 +27,15 @@ MockSmartCardContextFactory::GetRemote() {
   mojo::PendingRemote<device::mojom::SmartCardContextFactory> pending_remote;
   receivers_.Add(this, pending_remote.InitWithNewPipeAndPassReceiver());
   return pending_remote;
+}
+
+void MockSmartCardContextFactory::CreateContext(
+    CreateContextCallback callback) {
+  mojo::PendingRemote<device::mojom::SmartCardContext> context_remote;
+  context_receivers_.Add(this, context_remote.InitWithNewPipeAndPassReceiver());
+
+  std::move(callback).Run(
+      SmartCardCreateContextResult::NewContext(std::move(context_remote)));
 }
 
 void MockSmartCardContextFactory::ExpectConnectFakeReaderSharedT1(
@@ -78,13 +77,6 @@ void MockSmartCardContextFactory::ExpectListReadersError(
   EXPECT_CALL(*this, ListReaders(_))
       .WillOnce(RunOnceCallback<0>(
           device::mojom::SmartCardListReadersResult::NewError(error)));
-}
-
-void MockSmartCardContextFactory::ExpectCreateContextError(
-    device::mojom::SmartCardError error) {
-  EXPECT_CALL(*this, CreateContext(_))
-      .WillOnce(RunOnceCallback<0>(
-          device::mojom::SmartCardCreateContextResult::NewError(error)));
 }
 
 void MockSmartCardContextFactory::ClearContextReceivers() {
--- content/browser/smart_card/mock_smart_card_context_factory.h
+++ content/browser/smart_card/mock_smart_card_context_factory.h
@@ -23,7 +23,7 @@ class MockSmartCardContextFactory
   mojo::PendingRemote<device::mojom::SmartCardContextFactory> GetRemote();
 
   // `device::mojom::SmartCardContextFactory` overrides:
-  MOCK_METHOD(void, CreateContext, (CreateContextCallback), (override));
+  void CreateContext(CreateContextCallback) override;
 
   // `device::mojom::SmartCardContext` overrides:
   MOCK_METHOD(void, ListReaders, (ListReadersCallback callback), (override));
@@ -56,8 +56,6 @@ class MockSmartCardContextFactory
   // Expect a ListReaders() call. Will return `readers`.
   void ExpectListReaders(std::vector<std::string> readers);
   void ExpectListReadersError(device::mojom::SmartCardError error);
-
-  void ExpectCreateContextError(device::mojom::SmartCardError error);
 
   void ClearContextReceivers();
 
