4c48c93c9a7c3cea931abc3c55d164f402cbc84d
Louis Romero <lpromero@google.com>
2025-07-25 10:28:23 -0700

    Remove tab grid related APIs from RecentTabs{Coordinator|Mediator}
    Recent Tabs is no longer shown from Tab Grid, so remove all arguments
    and codepaths related to Tab Grid in Recent Tabs.
    
    Bug: 433919353
    Change-Id: Ie59b0055a24320a576c842952bb61303c0645614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6783849
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492157}

--- ios/chrome/browser/recent_tabs/ui_bundled/recent_tabs_coordinator.h
+++ ios/chrome/browser/recent_tabs/ui_bundled/recent_tabs_coordinator.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 enum class UrlLoadStrategy;
 
 @protocol RecentTabsCoordinatorDelegate;
-@class TabGridModeHolder;
 
 // Coordinator that presents Recent Tabs.
 @interface RecentTabsCoordinator : ChromeCoordinator
@@ -22,9 +21,6 @@ enum class UrlLoadStrategy;
 
 // Opaque instructions on how to open urls.
 @property(nonatomic) UrlLoadStrategy loadStrategy;
-
-// Holder for the current Tab Grid mode.
-@property(nonatomic, strong) TabGridModeHolder* tabGridModeHolder;
 
 @end
 
