beb0adce72bbdc408d225bec1417574cf34ef034
Olivier ROBIN <olivierrobin@google.com>
2025-07-25 10:10:12 -0700

    Check subject before adding it to the share extension item
    Fixed: 433824534
    Change-Id: Id16f4823ea6683cfbe591d071ab20f4e19c75ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788474
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492143}

--- ios/chrome/browser/sharing/ui_bundled/activity_services/data/chrome_activity_url_source.mm
+++ ios/chrome/browser/sharing/ui_bundled/activity_services/data/chrome_activity_url_source.mm
@@ -94,7 +94,9 @@ BASE_FEATURE(kShareNSExtensionItemKillSwitch,
   };
   NSExtensionItem* item = [[NSExtensionItem alloc] init];
   item.attachments = @[ provider ];
-  item.attributedTitle = [[NSAttributedString alloc] initWithString:_subject];
+  if (_subject) {
+    item.attributedTitle = [[NSAttributedString alloc] initWithString:_subject];
+  }
   return item;
 }
 
