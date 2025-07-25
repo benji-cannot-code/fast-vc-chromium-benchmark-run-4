350c1c79cb720ba938c436f9b78e60905b367ba5
Simon Hangl <simonha@google.com>
2025-07-25 07:45:47 -0700

    Enable dynamic adjustment of multi capture notifications
    This CL dynamically changes the notifications if an allowlisted
    app is installed or uninstalled. This will change the notification
    message (i.e. the app will be added / removed).
    
    Bug: 424103935
    Change-Id: Ieeafd1f5698eeb48674d8ec567ffb1769a103fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787913
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Magdalena Skarbińska <mskarbinska@google.com>
    Auto-Submit: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492047}

--- chrome/browser/media/webrtc/multi_capture/multi_capture_data_service.h
+++ chrome/browser/media/webrtc/multi_capture/multi_capture_data_service.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <set>
 #include <string>
-#include <vector>
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
@@ -17,7 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
+#include "base/scoped_observation.h"
 #include "base/values.h"
+#include "chrome/browser/web_applications/web_app_install_manager.h"
+#include "chrome/browser/web_applications/web_app_install_manager_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/webapps/common/web_app_id.h"
 
@@ -34,7 +37,8 @@ namespace multi_capture {
 // data that constrains access to the `getAllScreensMedia` API. This is
 // intentional to prevent that additional apps can use the API after the user
 // was informed on login.
-class MultiCaptureDataService : public KeyedService {
+class MultiCaptureDataService : public KeyedService,
+                                public web_app::WebAppInstallManagerObserver {
  public:
   class Observer : public base::CheckedObserver {
    public:
@@ -59,6 +63,13 @@ class MultiCaptureDataService : public KeyedService {
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
 
+  // web_app::WebAppInstallManagerObserver:
+  void OnWebAppInstalled(const webapps::AppId& app_id) override;
+  void OnWebAppUninstalled(
+      const webapps::AppId& app_id,
+      webapps::WebappUninstallSource uninstall_source) override;
+  void OnWebAppInstallManagerDestroyed() override;
+
  private:
   FRIEND_TEST_ALL_PREFIXES(
       MultiCaptureUsageIndicatorBrowserTest,
@@ -66,12 +77,15 @@ class MultiCaptureDataService : public KeyedService {
   FRIEND_TEST_ALL_PREFIXES(
       MultiCaptureUsageIndicatorBrowserTest,
       YouAreCapturedNotificationShowsIfAppInstalledAndAllowlisted);
+  FRIEND_TEST_ALL_PREFIXES(MultiCaptureUsageIndicatorDynamicAppBrowserTest,
+                           AllowlistedAppAddedAndDeleted);
 
   MultiCaptureDataService(web_app::WebAppProvider* provider,
                           PrefService* prefs);
 
   void Init();
   void LoadData();
+  bool MaybeAddAppToCaptureAppLists(const webapps::AppId& app_id);
 
   const raw_ref<web_app::IwaKeyDistributionInfoProvider> info_provider_;
   const raw_ptr<web_app::WebAppProvider> provider_;
@@ -80,10 +94,14 @@ class MultiCaptureDataService : public KeyedService {
   bool is_initialized_ = false;
 
   base::Value::List multi_screen_capture_allowlist_on_login_;
-  std::vector<std::string> app_without_notification_bundle_ids_;
+  std::set<std::string> app_without_notification_bundle_ids_;
 
   std::map<webapps::AppId, std::string> capture_apps_with_notification_;
   std::map<webapps::AppId, std::string> capture_apps_without_notification_;
+
+  base::ScopedObservation<web_app::WebAppInstallManager,
+                          web_app::WebAppInstallManagerObserver>
+      install_manager_observation_{this};
 
   base::ObserverList<Observer> observers_;
 
