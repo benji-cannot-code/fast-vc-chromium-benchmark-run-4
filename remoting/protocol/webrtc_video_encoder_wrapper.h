4ed3f00b972a77900402995ef5d4496305054472
Yuwei Huang <yuweih@chromium.org>
2025-07-25 15:15:54 -0700

    [crd host][wayland] Top-off & keep-alive extrapolations
    For the X11 capturer, video top-off is driven by
    WebrtcFrameSchedulerConstantRate that schedules frame capturing at a
    constant rate. However, the Wayland pipewire capturer is push-based,
    meaning top-off won't happen if the desktop remains static, since the
    encoder isn't fed with a constant stream of frames. Also, WebRTC expects
    a video frame to be sent in a ~10s interval, otherwise it will request a
    key frame, so it is also broken with the pipewire capturer.
    
    This CL adds frame extrapolation logic to the encode wrapper such that
    the last capturer-fed frame will be re-encoded:
    
    1. In a rate slightly lower than the measured FPS when top-off is
       active
    1. In the keep-alive interval
    
    Extrapolation is suppressed if a frame is encoded within the timers'
    designated interval.
    
    Bug: 393627522
    Change-Id: Iaecb3f1472fc5da6c92cdfab9cf7ee3e8608966c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778328
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492320}

--- remoting/protocol/webrtc_video_encoder_wrapper.h
+++ remoting/protocol/webrtc_video_encoder_wrapper.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/thread_annotations.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "remoting/base/constants.h"
 #include "remoting/base/running_samples.h"
 #include "remoting/base/session_options.h"
@@ -22,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/video_codecs/sdp_video_format.h"
 #include "third_party/webrtc/api/video_codecs/video_encoder.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_capture_types.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
+#include "third_party/webrtc/modules/desktop_capture/shared_desktop_frame.h"
 
 namespace remoting::protocol {
 
@@ -58,6 +62,8 @@ class WebrtcVideoEncoderWrapper : public webrtc::VideoEncoder {
   void OnRttUpdate(int64_t rtt_ms) override;
   webrtc::VideoEncoder::EncoderInfo GetEncoderInfo() const override;
 
+  static base::TimeDelta GetKeepAliveIntervalForTesting();
+
  private:
   static constexpr int kStatsWindow = 5;
 
@@ -86,6 +92,16 @@ class WebrtcVideoEncoderWrapper : public webrtc::VideoEncoder {
   // |pending_frame_| contains valid frame data.
   void DropPendingFrame();
 
+  // Encodes a desktop frame. Only one frame can be encoded at a time.
+  void EncodeDesktopFrame(std::unique_ptr<webrtc::DesktopFrame> desktop_frame);
+
+  // If `top_off_active_` is true, (re)starts the top-off extrapolation timer
+  // with a recalculated interval, otherwise stops the timer.
+  void UpdateTopOffExtrapolationTimer();
+
+  // Re-encodes the last capturer-fed frame.
+  void ExtrapolateFrame();
+
   std::unique_ptr<WebrtcVideoEncoder> encoder_
       GUARDED_BY_CONTEXT(sequence_checker_);
 
@@ -99,7 +115,8 @@ class WebrtcVideoEncoderWrapper : public webrtc::VideoEncoder {
   uint32_t rtp_timestamp_ GUARDED_BY_CONTEXT(sequence_checker_);
 
   // FrameStats taken from the input VideoFrameAdapter, then added to the
-  // EncodedFrame when encoding is complete.
+  // EncodedFrame when encoding is complete. This is also used for top-off and
+  // keep-alive extrapolation.
   std::unique_ptr<WebrtcVideoEncoder::FrameStats> frame_stats_;
 
   // Bandwidth estimate from SetRates(), which is expected to be called before
@@ -112,6 +129,23 @@ class WebrtcVideoEncoderWrapper : public webrtc::VideoEncoder {
 
   // True when encoding unchanged frames for top-off.
   bool top_off_active_ GUARDED_BY_CONTEXT(sequence_checker_) = false;
+
+  // Timer to extrapolate top-off frames in a reasonable interval, until
+  // `top_off_active_` is false. It will be suppressed if either a capturer-fed
+  // frame or a keep-alive frame is encoded within the top-off interval.
+  base::RetainingOneShotTimer top_off_timer_
+      GUARDED_BY_CONTEXT(sequence_checker_);
+
+  // Timer to extrapolate keep-alive frames. It will be suppressed if either a
+  // capturer-fed frame or a top-off frame is encoded within the keep-alive
+  // interval.
+  base::RetainingOneShotTimer keep_alive_timer_
+      GUARDED_BY_CONTEXT(sequence_checker_);
+
+  // The last capturer-fed desktop frame, used for top-off and keep-alive
+  // extrapolation.
+  std::unique_ptr<webrtc::SharedDesktopFrame> last_capturer_fed_frame_
+      GUARDED_BY_CONTEXT(sequence_checker_);
 
   webrtc::VideoCodecType codec_type_ GUARDED_BY_CONTEXT(sequence_checker_);
 
