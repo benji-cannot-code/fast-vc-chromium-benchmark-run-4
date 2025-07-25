a48689eb90172dfe28170074e4ba3e229e728629
Clark DuVall <cduvall@chromium.org>
2025-07-25 11:28:08 -0700

    Fix crash computing performance class when service can't launch
    This was happening on Android which can't launch the service.
    
    Bug: 421610868
    Change-Id: I4df96a270fe48f86eca4a16cd75140bf8ac2a3b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785874
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492198}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller.cc
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller.cc
@@ -789,6 +789,11 @@ void OnDeviceModelServiceController::Solution::ReportHealthyCompletion() {
 
 void OnDeviceModelServiceController::EnsurePerformanceClassAvailable(
     base::OnceClosure complete) {
+  if (!features::CanLaunchOnDeviceModelService()) {
+    std::move(complete).Run();
+    return;
+  }
+
   if (ListenForPerformanceClassAvailable(std::move(complete))) {
     return;
   }
