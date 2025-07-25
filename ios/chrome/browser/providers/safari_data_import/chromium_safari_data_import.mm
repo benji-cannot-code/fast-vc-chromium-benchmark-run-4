505365c59f5f0b19688446495b748b8d141e4793
ginnnnnnny <ginnyhuang@chromium.org>
2025-07-25 12:46:09 -0700

    [iOS][safari-data-import] Public interface to open Settings app
    This CL declares the function to open Settings app from the Safari
    data import workflow, so that the user could export their data there.
    Implementation is in crrev.com/i/8452219.
    
    Bug: 420703283
    Change-Id: Iae10bf9a3b5642e6e6467450af0222f9a1b00ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786906
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492247}

---
+++ ios/chrome/browser/providers/safari_data_import/chromium_safari_data_import.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+
+#import "ios/public/provider/chrome/browser/safari_data_import/safari_data_import_api.h"
+
+namespace ios {
+namespace provider {
+
+void OpenSettingsToExportDataFromSafari() {
+  NSURL* url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+  if ([[UIApplication sharedApplication] canOpenURL:url]) {
+    [[UIApplication sharedApplication] openURL:url
+                                       options:@{}
+                             completionHandler:nil];
+  }
+}
+
+}  // namespace provider
+}  // namespace ios
---
+++ ios/public/provider/chrome/browser/safari_data_import/safari_data_import_api.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_SAFARI_DATA_IMPORT_SAFARI_DATA_IMPORT_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_SAFARI_DATA_IMPORT_SAFARI_DATA_IMPORT_API_H_
+
+namespace ios {
+namespace provider {
+
+// Opens the Settings app to export data from Safari.
+void OpenSettingsToExportDataFromSafari();
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_SAFARI_DATA_IMPORT_SAFARI_DATA_IMPORT_API_H_
