30b7b15fb273469903ec61db10036d96dffba14e
Tomasz Wiszkowski <ender@google.com>
2025-07-25 12:46:54 -0700

    Add Site Search feature flag.
    Bug: 399443172
    Change-Id: I1320b1fd84c778000e80b00e973313bcdcf0628b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785030
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492248}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1686,6 +1686,9 @@ extern const char kOmniboxStarterPackIPHDescription[];
 extern const char kOmniboxSearchAggregatorName[];
 extern const char kOmniboxSearchAggregatorDescription[];
 
+extern const char kOmniboxSiteSearchName[];
+extern const char kOmniboxSiteSearchDescription[];
+
 extern const char kContextualSearchBoxUsesContextualSearchProviderName[];
 extern const char kContextualSearchBoxUsesContextualSearchProviderDescription[];
 
