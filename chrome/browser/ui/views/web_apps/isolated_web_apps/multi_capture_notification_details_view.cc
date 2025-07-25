f38ccb2ed2378e4c9506646f5394f1adbff9ad82
Simon Hangl <simonha@google.com>
2025-07-25 08:57:33 -0700

    Minor app name length fix for multi capture details
    Change-Id: I40a77ccecef8073b7b6d0e1b75c2e79502b784be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786130
    Auto-Submit: Simon Hangl <simonha@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Reviewed-by: Magdalena Skarbińska <mskarbinska@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492086}

--- chrome/browser/ui/views/web_apps/isolated_web_apps/multi_capture_notification_details_view.cc
+++ chrome/browser/ui/views/web_apps/isolated_web_apps/multi_capture_notification_details_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/image_model.h"
 #include "ui/color/color_id.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/text_elider.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/md_text_button.h"
@@ -30,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace multi_capture {
 
 namespace {
+
+constexpr size_t kAppMaxNameLength = 18;
 
 enum class DetailsMode {
   kOnlyAppsWithNotification = 0,
@@ -101,7 +104,9 @@ std::unique_ptr<views::View> CreateAppList(
         vector_icons::kScreenRecordIcon, ui::kColorIconSecondary, 16));
 
     app_row->AddChildView(std::make_unique<views::Label>(
-        base::UTF8ToUTF16(app_name), views::style::CONTEXT_LABEL));
+        gfx::TruncateString(base::UTF8ToUTF16(app_name), kAppMaxNameLength,
+                            gfx::BreakType::WORD_BREAK),
+        views::style::CONTEXT_LABEL));
   }
   app_list_container->SetProperty(
       views::kMarginsKey,
