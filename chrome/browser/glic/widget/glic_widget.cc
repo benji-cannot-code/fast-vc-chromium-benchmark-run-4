20604ad1934cb5f977eb0a15629538a8a591fad1
Kevin Graney <kmg@google.com>
2025-07-25 11:14:16 -0700

    Set wayland_app_id for Glic widget
    Bug: 430277800
    Change-Id: I1ac3bad0391e3b5862b8e7127c7adcb47d21a4a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788537
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492194}

--- chrome/browser/glic/widget/glic_widget.cc
+++ chrome/browser/glic/widget/glic_widget.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/glic/widget/glic_view.h"
+#include "chrome/browser/shell_integration_linux.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/views/chrome_widget_sublevel.h"
@@ -127,6 +128,10 @@ std::unique_ptr<GlicWidget> GlicWidget::Create(
   // Don't change this name. This is used by other code to identify the glic
   // window. See b/404947780.
   params.name = "GlicWidget";
+#if BUILDFLAG(IS_LINUX)
+  params.wm_class_class = shell_integration_linux::GetProgramClassClass();
+  params.wayland_app_id = params.wm_class_class + "-glic";
+#endif
   // Support of rounded corners varies across platforms. See
   // Widget::InitParams::rounded_corners. DO NOT apply this radius using
   // views::Background or in the web client because it will mismatch with
