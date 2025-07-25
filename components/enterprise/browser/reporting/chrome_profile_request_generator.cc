c317252853450420f628548588d7cbc268abba79
hamda mare <hmare@google.com>
2025-07-25 10:17:10 -0700

    [CAA-Crowdstrike] Add agent signals to report request
    Adds the collection of detected agent signals to the chrome profile
    signals report request.
    
    Bug: 430135330
    Change-Id: Ibe870f3d306411dc5f5de6d172819b06e1b06c1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785407
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492150}

--- components/enterprise/browser/reporting/chrome_profile_request_generator.cc
+++ components/enterprise/browser/reporting/chrome_profile_request_generator.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "build/build_config.h"
 #include "components/device_signals/core/browser/signals_aggregator.h"
+#include "components/device_signals/core/common/signals_features.h"
 #include "components/enterprise/browser/reporting/os_report_generator.h"
 #include "components/enterprise/browser/reporting/report_type.h"
 #include "components/enterprise/browser/reporting/report_util.h"
@@ -154,6 +155,13 @@ void ChromeProfileRequestGenerator::OnBaseReportsReady(
   signals_request.signal_names.emplace(device_signals::SignalName::kOsSignals);
   signals_request.signal_names.emplace(
       device_signals::SignalName::kBrowserContextSignals);
+
+  if (enterprise_signals::features::IsDetectedAgentSignalCollectionEnabled()) {
+    signals_request.signal_names.emplace(device_signals::SignalName::kAgent);
+    signals_request.agent_signal_parameters.emplace(
+        device_signals::AgentSignalCollectionType::kDetectedAgents);
+  }
+
 #if BUILDFLAG(IS_WIN)
   signals_request.signal_names.emplace(device_signals::SignalName::kAntiVirus);
   signals_request.signal_names.emplace(device_signals::SignalName::kHotfixes);
@@ -282,6 +290,17 @@ void ChromeProfileRequestGenerator::OnAggregatedSignalsReceived(
 
     if (profile_signals.profile_id) {
       profile_report->set_profile_id(profile_signals.profile_id.value());
+    }
+  }
+
+  if (response.agent_signals_response) {
+    const auto& agent_signals = response.agent_signals_response.value();
+    for (auto detected_agent : agent_signals.detected_agents) {
+      switch (detected_agent) {
+        case (device_signals::Agents::kCrowdStrikeFalcon):
+          os_report->add_detected_agents(em::Agent::CROWDSTRIKE_FALCON);
+          break;
+      }
     }
   }
 
