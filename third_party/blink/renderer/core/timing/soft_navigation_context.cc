258d3c7488ebc5477f13af3b7c3f57b730734a92
Scott Haseley <shaseley@chromium.org>
2025-07-25 12:54:57 -0700

    [soft navs] Refactor FCP presentation time callback logic
    SoftNavigationHeuristics waits for FCP presentation time before
    emitting the soft nav performance entry. This CL refactors some of that
    logic, which is a step towards recording paints before URL change for
    pre-paint-based attribution mode.
    
    Specifically, this removes the callback mechanism for recording FCP,
    instead storing the PaintTimingRecord for the FCP candidate, which is
    updated asynchronously by the PaintTimingMixing layer. We can then
    detect when the FCP has updated when updating LCP candidates, and emit
    the soft nav entry there if its pending the FCP info.
    
    The behavior is mostly the same, but changes in the following ways:
    
     1. FCP is now the first contentful paint after URL change, rather than
        the first paint after the conditions are met. This paint is often
        likely to match the first ICP, but won't if the FCP wasn't
        sufficient to meet the paint area requirement.
    
     2. Don't emit ICP until SN entry has been emitted, as opposed to after
        navigation ID has been set. This shouldn't really change anything
        now that crrev.com/c/6777944 has landed, but I think it makes the
        ordering a bit more obvious.
    
        Note: This CL adds a direct call to emit the buffered LCP entry
        after the soft nav entry, since this is what we want for
        paints-before-url (and it doesn't hurt to do that here).
    
    Bug: 424448145
    Change-Id: Iadbbc40d4bf9877b0608ba99ee28b4f2ba1ea07e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779158
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492257}

--- third_party/blink/renderer/core/timing/soft_navigation_context.cc
+++ third_party/blink/renderer/core/timing/soft_navigation_context.cc
@@ -106,6 +106,13 @@ bool SoftNavigationContext::AddPaintedArea(PaintTimingRecord* record) {
       rect.height(), "paintedAreaThisAnimationFrame",
       painted_area_ - painted_area_last_animation_frame_);
 
+  // TODO(crbug.com/434159332): This doesn't currently match hard-FCP semantics
+  // because we aren't notified about images paints until they are "sufficiently
+  // loaded", which is needed for LCP/ICP.
+  if (!first_image_or_text_) {
+    first_image_or_text_ = record;
+  }
+
   if (record->IsImageRecord()) {
     if (!largest_image_ ||
         largest_image_->RecordedSize() < record->RecordedSize()) {
@@ -208,9 +215,10 @@ void SoftNavigationContext::UpdateWebExposedLargestContentfulPaintIfNeeded() {
 }
 
 bool SoftNavigationContext::TryUpdateLcpCandidate() {
-  // After we are ready to start measuring LCP (`HasNavigationId()`) and
-  // before we want to stop (input or scroll), we update LCP candidate.
-  if (!HasNavigationId() || !first_input_or_scroll_time_.is_null()) {
+  // After we are ready to start measuring LCP (after the soft nav entry was
+  // emitted) and before we want to stop (input or scroll), we update LCP
+  // candidate.
+  if (!was_emitted_ || !first_input_or_scroll_time_.is_null()) {
     return false;
   }
 
@@ -254,7 +262,7 @@ void SoftNavigationContext::WriteIntoTrace(
   dict.Add("mostRecentURL", most_recent_url_);
 
   dict.Add("interactionTimestamp", user_interaction_timestamp_);
-  dict.Add("firstContentfulPaint", first_contentful_paint_);
+  dict.Add("firstContentfulPaint", FirstContentfulPaint());
 
   dict.Add("domModifications", num_modified_dom_nodes_);
   dict.Add("paintedArea", painted_area_);
@@ -268,6 +276,7 @@ void SoftNavigationContext::Trace(Visitor* visitor) const {
   visitor->Trace(lcp_calculator_);
   visitor->Trace(largest_text_);
   visitor->Trace(largest_image_);
+  visitor->Trace(first_image_or_text_);
 }
 
 }  // namespace blink
