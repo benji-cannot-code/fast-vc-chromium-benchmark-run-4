85c495656d898b87297c3344fecdad5e683f6012
Elly <ellyjones@chromium.org>
2025-07-25 06:31:47 -0700

    spellcheck: stop gathering SpellCheck.UniqueWords
    This allows dropping a dep on base/hash/md5 and saves some memory.
    
    OBSOLETE_HISTOGRAM[SpellCheck.UniqueWords]=Data no longer necessary.
    
    Bug: 406729261
    Change-Id: I4ee88f54197ef95383138deb8f29425e8e33aaa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785808
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492004}

--- components/spellcheck/browser/spellcheck_host_metrics.cc
+++ components/spellcheck/browser/spellcheck_host_metrics.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/containers/span.h"
-#include "base/hash/md5.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/safe_conversions.h"
@@ -23,7 +22,6 @@ SpellCheckHostMetrics::SpellCheckHostMetrics()
       last_suggestion_show_count_(-1),
       replaced_word_count_(0),
       last_replaced_word_count_(-1),
-      last_unique_word_count_(0),
       start_time_(base::TimeTicks::Now()) {
   const uint64_t kHistogramTimerDurationInMinutes = 30;
   recording_timer_.Start(FROM_HERE,
@@ -50,11 +48,6 @@ void SpellCheckHostMetrics::RecordCheckedWordStats(const std::u16string& word,
     if (misspelled_word_count_ == 1)
       RecordReplacedWordStats(0);
   }
-
-  // Collects actual number of checked words, excluding duplication.
-  base::MD5Digest digest;
-  base::MD5Sum(base::as_byte_span(word), &digest);
-  checked_word_hashes_.insert(base::MD5DigestToBase16(digest));
 
   RecordWordCounts();
 }
@@ -112,14 +105,6 @@ void SpellCheckHostMetrics::RecordWordCounts() {
     DCHECK(replaced_word_count_ > last_replaced_word_count_);
     UMA_HISTOGRAM_COUNTS_1M("SpellCheck.ReplacedWords", replaced_word_count_);
     last_replaced_word_count_ = replaced_word_count_;
-  }
-
-  if (checked_word_hashes_.size() != last_unique_word_count_) {
-    DCHECK(checked_word_hashes_.size() > last_unique_word_count_);
-    UMA_HISTOGRAM_COUNTS_1M(
-        "SpellCheck.UniqueWords",
-        base::saturated_cast<int>(checked_word_hashes_.size()));
-    last_unique_word_count_ = checked_word_hashes_.size();
   }
 
   if (suggestion_show_count_ != last_suggestion_show_count_) {
