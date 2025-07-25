7af006f358d874b8a53e95c5ba12c63ec014da20
Robbie McElrath <rmcelrath@chromium.org>
2025-07-25 09:59:14 -0700

    controlledframe: Convert URLPattern to match pattern in content scripts
    Bug: 432827518
    Change-Id: I8d364cce537018d34f378d72f4d4ac574748b4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785850
    Auto-Submit: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492128}

--- chrome/renderer/resources/controlled_frame/controlled_frame_url_patterns_helper.js
+++ chrome/renderer/resources/controlled_frame/controlled_frame_url_patterns_helper.js
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const WebUrlPatternNatives = requireNative('WebUrlPatternNatives');
 
 function convertURLPatternsToMatchPatterns(urlPatternsStrsOrObjs) {
+  if (urlPatternsStrsOrObjs === undefined) {
+    return undefined;
+  }
+
   let matchPatterns = [];
   for (const urlPatternStrOrObj of urlPatternsStrsOrObjs) {
     matchPatterns = $Array.concat(
