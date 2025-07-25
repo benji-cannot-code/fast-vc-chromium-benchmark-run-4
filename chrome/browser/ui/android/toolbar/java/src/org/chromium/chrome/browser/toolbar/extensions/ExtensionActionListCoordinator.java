2a1e43da1de2e662574007143a8f095b1658e962
Shuhei Takahashi <nya@chromium.org>
2025-07-24 18:39:36 -0700

    extensions: Hide overflowed extension action buttons from the toolbar
    When the user has many extensions, their action buttons can overflow the
    toolbar. This change introduces ExtensionActionListContainer to hide
    buttons that do not fit in the available space, preventing the toolbar
    from becoming cluttered.
    
    Bug: 430705052
    Change-Id: Id6db448860f7173ea9ff1af775e884d861648be8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6726216
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491792}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionActionListCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionActionListCoordinator.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.toolbar.extensions;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.widget.LinearLayout;
 
 import org.chromium.base.lifetime.Destroyable;
 import org.chromium.base.lifetime.LifetimeAssert;
@@ -29,7 +28,7 @@ import org.chromium.ui.modelutil.ViewGroupAdapter;
  */
 @NullMarked
 public class ExtensionActionListCoordinator implements Destroyable {
-    private final LinearLayout mContainer;
+    private final ExtensionActionListContainer mContainer;
     private final ModelList mModels;
     private final ExtensionActionListMediator mMediator;
     private final ViewGroupAdapter mAdapter;
@@ -37,7 +36,7 @@ public class ExtensionActionListCoordinator implements Destroyable {
 
     public ExtensionActionListCoordinator(
             Context context,
-            LinearLayout container,
+            ExtensionActionListContainer container,
             WindowAndroid windowAndroid,
             ObservableSupplier<Profile> profileSupplier,
             ObservableSupplier<Tab> currentTabSupplier) {
