c4fb5e5ecd5f2929d3acc374ecda959ce4a59932
Nicola Tommasi <tommasin@chromium.org>
2025-07-25 04:44:22 -0700

    [Clank][Geo]: Expose arms to flags UI to simplify manual testing
    Bug: 433890566
    Change-Id: I37132b580ad333846c7066eb5a91274506b32a5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781232
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491967}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -156,6 +156,11 @@ extern const char kAnnotatorModeName[];
 extern const char kAnnotatorModeDescription[];
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
+#if BUILDFLAG(IS_ANDROID)
+extern const char kApproximateGeolocationPermissionName[];
+extern const char kApproximateGeolocationPermissionDescription[];
+#endif  // BUILDFLAG(IS_ANDROID)
+
 extern const char kAriaElementReflectionName[];
 extern const char kAriaElementReflectionDescription[];
 
