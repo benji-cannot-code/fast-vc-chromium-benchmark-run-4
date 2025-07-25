5666478bc680633743762afa294e66691e4637d2
Takuto Ikuta <tikuta@chromium.org>
2025-07-24 18:46:35 -0700

    chromecast: Fix a missing include
    This is to fix build error like
    https://ci.chromium.org/ui/p/chromium/builders/try/linux-cast-x64-rel/2042/overview
    
    Bug: 40263312
    Change-Id: Ic66f8138a696f3ba9b25ad5ab833fd62e48e0808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778608
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491797}

--- chromecast/media/audio/cast_audio_bus.cc
+++ chromecast/media/audio/cast_audio_bus.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/media/audio/cast_audio_bus.h"
 
+#include <algorithm>
 #include <cstring>
 
 #include "base/memory/ptr_util.h"
