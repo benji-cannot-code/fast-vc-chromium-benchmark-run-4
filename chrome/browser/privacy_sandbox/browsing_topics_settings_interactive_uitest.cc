e2809e255d0be5f4fe1de2fd94a58409f44b1f54
Jood <jood@google.com>
2025-07-25 03:42:31 -0700

    [Gardener] Disable BlockFirstTopicOnManageTopicsPage
    Bug: 430518830
    Change-Id: Ice9f32b442ce1e3482300b0d40c8a5261d743cde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788153
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1491945}

--- chrome/browser/privacy_sandbox/browsing_topics_settings_interactive_uitest.cc
+++ chrome/browser/privacy_sandbox/browsing_topics_settings_interactive_uitest.cc
@@ -124,7 +124,7 @@ IN_PROC_BROWSER_TEST_F(PrivacySandboxSettingsTopicsInteractiveTest,
   BlockFirstTopicOnManageTopicsPage
 #endif
 IN_PROC_BROWSER_TEST_F(PrivacySandboxSettingsTopicsInteractiveTest,
-                       BlockFirstTopicOnManageTopicsPage) {
+                       MAYBE_BlockFirstTopicOnManageTopicsPage) {
   RunTestSequence(
       InstrumentTab(kPrivacySandboxTopicsElementId),
       NavigateWebContents(kPrivacySandboxTopicsElementId,
