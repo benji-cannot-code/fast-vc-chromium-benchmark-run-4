b8262db89603162f65b608a58b0cb082731509a9
Guillaume Jenkins <gujen@google.com>
2025-07-25 17:37:17 -0700

    [iOS] Add necessary flags for the Default Browser off-cycle promo
    In preparation for implementing the Default Browser off-cycle promo,
    this CL adds the necessary base::Features and the Chrome about flag.
    
    Bug: 434214574
    Change-Id: I334681c24d21a8ad2a24c38605058e9632dc1ea7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788539
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492373}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -249,6 +249,9 @@ extern const char kDataSharingSharedDataTypesEnabledWithUi[];
 extern const char kDefaultBrowserBannerPromoName[];
 extern const char kDefaultBrowserBannerPromoDescription[];
 
+extern const char kDefaultBrowserOffCyclePromoName[];
+extern const char kDefaultBrowserOffCyclePromoDescription[];
+
 extern const char kDefaultBrowserPromoPropensityModelName[];
 extern const char kDefaultBrowserPromoPropensityModelDescription[];
 
