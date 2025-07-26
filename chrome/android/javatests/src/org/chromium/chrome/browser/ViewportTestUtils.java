3b9ad1c6e5bdee84a4890fa59617e10576f98794
Lijin Shen <lazzzis@google.com>
2025-07-25 17:39:47 -0700

    Re-enable startBackNavWithTopControlHidden after fix
    1. The previous getPageHeight() was not accounting for the
    deviceScaleFactor. See
    https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
    
    2. It uses edgeToEdgeController.getBottomInset() to decide the amount of
    space blocked by Bottom Controls, which doesn't account for Bottom
    Controls height.
    
    Bug: 431044324
    Change-Id: I938fbd7f79009f4ea5395202c5177fede6f1cda2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785987
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492375}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ViewportTestUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ViewportTestUtils.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
+import androidx.annotation.Px;
+
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 
@@ -16,7 +18,6 @@ import org.chromium.base.test.util.CriteriaNotSatisfiedException;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.fullscreen.FullscreenManagerTestUtils;
 import org.chromium.chrome.browser.tab.TabStateBrowserControlsVisibilityDelegate;
-import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeController;
 import org.chromium.chrome.test.transit.BaseCtaTransitTestRule;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.test.util.Coordinates;
@@ -72,12 +73,12 @@ public final class ViewportTestUtils {
                 mActivityTestRule.getActivity().getActivityTab());
         waitForFramePresented();
         int initialPageHeight = getPageInnerHeightPx();
-        int initialBottomInset = getBottomInsetHeightDp();
+        int initialBottomMargin = getBottomMargins();
         FullscreenManagerTestUtils.waitForBrowserControlsToBeMoveable(
                 mActivityTestRule.getActivity(), /* showControls= */ false);
 
         // Also wait for the browser controls to resize Blink before returning.
-        int finalHeight = initialPageHeight + getTopControlsHeightDp() + initialBottomInset;
+        int finalHeight = initialPageHeight + getTopControlsHeightPx() + initialBottomMargin;
         waitForExpectedPageHeight(finalHeight);
     }
 
@@ -156,23 +157,29 @@ public final class ViewportTestUtils {
         return (int) Math.floor(getTopControlsHeightPx() / getDeviceScaleFactor());
     }
 
-    public int getBottomInsetHeightDp() {
-        EdgeToEdgeController e2eController =
-                mActivityTestRule.getActivity().getEdgeToEdgeControllerSupplierForTesting().get();
-        // Returns zero if the inset is not scrollable.
-        return e2eController != null ? e2eController.getBottomInset() : 0;
+    public @Px int getBottomMargins() {
+        BrowserControlsStateProvider browserControlsStateProvider =
+                mActivityTestRule.getActivity().getBrowserControlsManager();
+        return browserControlsStateProvider.getBottomControlsHeight()
+                - browserControlsStateProvider.getBottomControlOffset();
     }
 
     public int getPageInnerHeightPx() throws Throwable {
-        return Integer.parseInt(
-                JavaScriptUtils.executeJavaScriptAndWaitForResult(
-                        getWebContents(), "window.innerHeight"));
+        return (int)
+                Math.round(
+                        getDeviceScaleFactor()
+                                * Integer.parseInt(
+                                        JavaScriptUtils.executeJavaScriptAndWaitForResult(
+                                                getWebContents(), "window.innerHeight")));
     }
 
-    public double getVisualViewportHeightPx() throws Throwable {
-        return Float.parseFloat(
-                JavaScriptUtils.executeJavaScriptAndWaitForResult(
-                        getWebContents(), "window.visualViewport.height"));
+    public int getVisualViewportHeightPx() throws Throwable {
+        return (int)
+                Math.round(
+                        getDeviceScaleFactor()
+                                * Float.parseFloat(
+                                        JavaScriptUtils.executeJavaScriptAndWaitForResult(
+                                                getWebContents(), "window.visualViewport.height")));
     }
 
     private WebContents getWebContents() {
