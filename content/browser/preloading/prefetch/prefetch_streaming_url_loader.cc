8af5f23b564a9ce19967aa51171acebfabaf5e5a
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-07-27 16:14:14 -0700

    Pass OnServiceWorkerStateDetermined callback for tests
    Preparation for
    https://chromium-review.googlesource.com/c/chromium/src/+/6786519
    
    To make `PrefetchServiceWorkerState` transitions correctly in tests.
    Otherwise, unit tests crashes due to incorrect
    `PrefetchServiceWorkerState` when `kPrefetchServiceWorker` is enabled.
    
    Bug: 40947546, 420310104
    Change-Id: Ic3c0d17d72927792adacaa733e9fb59cc8e20f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786518
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492553}

--- content/browser/preloading/prefetch/prefetch_streaming_url_loader.cc
+++ content/browser/preloading/prefetch/prefetch_streaming_url_loader.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/preloading/prefetch/prefetch_streaming_url_loader.h"
 
+#include "base/check_is_test.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/trace_event.h"
@@ -310,6 +311,19 @@ void PrefetchStreamingURLLoader::StartServiceWorkerInterceptor(
     const network::ResourceRequest& request,
     const net::NetworkTrafficAnnotationTag& network_traffic_annotation,
     base::TimeDelta timeout_duration) {
+  auto callback = base::BindOnce(
+      &PrefetchStreamingURLLoader::ServiceWorkerInterceptorLoaderCallback,
+      GetWeakPtr(), network_url_loader_factory, request,
+      network_traffic_annotation, std::move(timeout_duration));
+
+  if (!browser_context) {
+    // In tests, `browser_context` can be null. Emulate as if there are no
+    // service workers without going through the interceptor.
+    CHECK_IS_TEST();
+    std::move(callback).Run(std::nullopt);
+    return;
+  }
+
   // TODO(https://crbug.com/40947546): Set this FetchEvent's Client ID.
   std::string fetch_event_client_id;
 
@@ -331,11 +345,7 @@ void PrefetchStreamingURLLoader::StartServiceWorkerInterceptor(
       request, service_worker_handle_->AsWeakPtr(), network_url_loader_factory);
 
   interceptor_->MaybeCreateLoader(
-      request, browser_context,
-      base::BindOnce(
-          &PrefetchStreamingURLLoader::ServiceWorkerInterceptorLoaderCallback,
-          GetWeakPtr(), network_url_loader_factory, request,
-          network_traffic_annotation, std::move(timeout_duration)),
+      request, browser_context, std::move(callback),
       base::BindOnce(
           [](scoped_refptr<network::SharedURLLoaderFactory>
                  network_url_loader_factory,
