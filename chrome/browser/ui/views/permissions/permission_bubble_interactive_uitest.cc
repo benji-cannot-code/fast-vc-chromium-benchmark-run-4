2ca2caaeb41dd6051c1f6653eb7b4f1aa7a7ef56
Thomas Lukaszewicz <tluk@chromium.org>
2025-07-27 13:31:41 -0700

    [win] Refactor PermissionBubbleInteractiveUITest::CmdWClosesWindow
    This CL removes the NoWidgetsWaiter from CmdWClosesWindow and
    replaces it with a RunUntil check.
    
    views::test::WidgetTest::GetAllWidgets() uses tracked NativeWidgets
    to work backwards and determine all Widgets. NoWidgetsWaiter would
    wait for the reported count to reach 0 only when OnWidgetDestroyed()
    events were observed.
    
    However this only works for the NATIVE_WIDGET_OWNS_WIDGET ownership
    scheme since the NativeWidget will always outlive the widget. This
    will not work for CLIENT_OWNS_WIDGET since OnWidgetDestroyed() will
    be observed before the NativeWidget is destroyed (typically async)
    and thus the waiter could end up stuck and time out.
    
    Bug: 40502806, 413168662
    Change-Id: I8009d6bf10c04778af92d89f8db15d095fd6d7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6790628
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492546}

--- chrome/browser/ui/views/permissions/permission_bubble_interactive_uitest.cc
+++ chrome/browser/ui/views/permissions/permission_bubble_interactive_uitest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/run_loop.h"
+#include "base/test/run_until.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -170,38 +171,13 @@ IN_PROC_BROWSER_TEST_F(PermissionBubbleInteractiveUITest,
                        MAYBE_CmdWClosesWindow) {
   EXPECT_TRUE(browser()->window()->IsVisible());
 
-  class NoWidgetsWaiter : public views::WidgetObserver {
-   public:
-    NoWidgetsWaiter() {
-      EXPECT_NE(views::test::WidgetTest::GetAllWidgets().size(), 0U);
-      for (views::Widget* widget : views::test::WidgetTest::GetAllWidgets()) {
-        widget->AddObserver(this);
-      }
-    }
-
-    void Wait() {
-      run_loop_.Run();
-      EXPECT_EQ(views::test::WidgetTest::GetAllWidgets().size(), 0U);
-    }
-
-   private:
-    // views::WidgetObserver:
-    void OnWidgetDestroyed(views::Widget*) override {
-      if (views::test::WidgetTest::GetAllWidgets().empty()) {
-        run_loop_.Quit();
-      }
-    }
-
-    base::RunLoop run_loop_;
-  };
-
   // On Windows, the WM_NCDESTROY message triggering Widget destruction may not
   // have been processed by the time `SendAcceleratorSync` returns (only waits
   // for WM_KEYDOWN). For that reason, wait until there are no more widgets
   // instead of checking immediately that there are no more widgets.
-  NoWidgetsWaiter waiter;
   SendAcceleratorSync(ui::VKEY_W, false, false);
-  waiter.Wait();
+  EXPECT_TRUE(base::test::RunUntil(
+      [&] { return views::test::WidgetTest::GetAllWidgets().empty(); }));
 }
 
 #if BUILDFLAG(IS_MAC)
