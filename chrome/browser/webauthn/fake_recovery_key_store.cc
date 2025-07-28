2562da077f649cc3382b9580bde6499812760d0f
Nina Satragno <nsatragno@chromium.org>
2025-07-28 16:00:06 -0700

    [webauthn] Set wrapped cohort details for new user
    Store the cohort public key and cert.xml serial number with the wrapped
    Google Password Manager PIN when a user is enrolling to use passkeys for
    the first time.
    
    See go/robust-gpm-pin-refreshes
    
    Bug: 399818721
    Change-Id: I582afaa6ef96d7dded47ad0a469ead739e29bfb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6708727
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493162}

--- chrome/browser/webauthn/fake_recovery_key_store.cc
+++ chrome/browser/webauthn/fake_recovery_key_store.cc
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "components/trusted_vault/proto/recovery_key_store.pb.h"
 #include "components/trusted_vault/proto/vault.pb.h"
+#include "crypto/evp.h"
 #include "crypto/sha2.h"
 #include "device/fido/enclave/constants.h"
+#include "net/cert/asn1_util.h"
 #include "net/cert/x509_util.h"
 #include "services/network/public/cpp/data_element.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -307,7 +309,7 @@ class FakeRecoveryKeyStoreImpl : public FakeRecoveryKeyStore {
         R"(<?xml version="1.0" encoding="UTF-8"?>
 <certificate>
   <metadata>
-    <serial>2</serial>
+    <serial>$3</serial>
     <creation-time>$1</creation-time>
     <refresh-interval>2592000</refresh-interval>
     <previous>
@@ -323,7 +325,8 @@ class FakeRecoveryKeyStoreImpl : public FakeRecoveryKeyStore {
 </certificate>
 )",
         {base::NumberToString(kTestTime),
-         base::Base64Encode(endpoint_cert_der_)},
+         base::Base64Encode(endpoint_cert_der_),
+         base::NumberToString(kTestSerialNumber)},
         /*offsets=*/nullptr);
 
     const auto certs_xml_hash =
@@ -381,6 +384,21 @@ class FakeRecoveryKeyStoreImpl : public FakeRecoveryKeyStore {
         result.data(), result.size(),
         EC_KEY_get0_private_key(EVP_PKEY_get0_EC_KEY(endpoint_key_.get()))));
     return result;
+  }
+
+  std::vector<uint8_t> endpoint_public_key_bytes() const override {
+    std::vector<uint8_t> spki =
+        crypto::evp::PublicKeyToBytes(endpoint_key_.get());
+    std::string_view spki_view = std::string_view(
+        reinterpret_cast<const char*>(spki.data()), spki.size());
+    // Extract the public key from the SPKI.
+    std::string_view ec_point_oct;
+    CHECK(net::asn1::ExtractSubjectPublicKeyFromSPKI(spki_view, &ec_point_oct));
+    // ExtractSubjectPublicKeyFromSPKI does not remove the initial octet
+    // encoding the number of unused bits in the ASN.1 BIT STRING so we do it
+    // here. The public key is always byte-aligned.
+    ec_point_oct.remove_prefix(1);
+    return std::vector<uint8_t>(ec_point_oct.begin(), ec_point_oct.end());
   }
 
  private:
--- chrome/browser/webauthn/fake_recovery_key_store.h
+++ chrome/browser/webauthn/fake_recovery_key_store.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "base/containers/span.h"
 #include "base/functional/callback_forward.h"
@@ -28,6 +29,9 @@ class Vault;
 // records uploaded vaults like the real service.
 class FakeRecoveryKeyStore {
  public:
+  // The cert.xml serial number.
+  static constexpr int kTestSerialNumber = 2;
+
   // If present, values of this type contain an HTTP status code (e.g. 200) and
   // the body of the response.
   using MaybeResponse =
@@ -52,6 +56,9 @@ class FakeRecoveryKeyStore {
 
   // Returns the cohort private scalar as 32 bytes in big-endian order.
   virtual std::array<uint8_t, 32> endpoint_private_key_bytes() const = 0;
+
+  // Returns the cohort public key bytes.
+  virtual std::vector<uint8_t> endpoint_public_key_bytes() const = 0;
 };
 
 #endif  // CHROME_BROWSER_WEBAUTHN_FAKE_RECOVERY_KEY_STORE_H_
