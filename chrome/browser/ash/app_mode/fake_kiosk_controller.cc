ba2732383d6dedb753f9b30775fda28c8ab1eeb6
Edman Anjos <edmanp@google.com>
2025-07-28 11:36:03 -0700

    kiosk: Remove the KioskVision component
    The KioskVision feature was cancelled before launch.
    
    Fixed: 433211220
    Change-Id: Ia79c47b93cff4749b2fec01b4c6c1ef8cd41c9a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779349
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492986}

--- chrome/browser/ash/app_mode/fake_kiosk_controller.cc
+++ chrome/browser/ash/app_mode/fake_kiosk_controller.cc
@@ -58,14 +58,4 @@ KioskSystemSession* FakeKioskController::GetKioskSystemSession() {
   return nullptr;
 }
 
-kiosk_vision::TelemetryProcessor*
-FakeKioskController::GetKioskVisionTelemetryProcessor() {
-  return nullptr;
-}
-
-kiosk_vision::InternalsPageProcessor*
-FakeKioskController::GetKioskVisionInternalsPageProcessor() {
-  return nullptr;
-}
-
 }  // namespace ash
--- chrome/browser/ash/app_mode/kiosk_controller.h
+++ chrome/browser/ash/app_mode/kiosk_controller.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/login_accelerators.h"
 #include "chrome/browser/ash/app_mode/kiosk_app.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_types.h"
-#include "chromeos/ash/components/kiosk/vision/internals_page_processor.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "content/public/browser/web_contents.h"
@@ -25,13 +24,6 @@ class AppLaunchSplashScreen;
 class KioskProfileLoadFailedObserver;
 class KioskSystemSession;
 class LoginDisplayHost;
-
-namespace kiosk_vision {
-
-class TelemetryProcessor;
-class InternalsPageProcessor;
-
-}  // namespace kiosk_vision
 
 // Public interface for Kiosk.
 class KioskController {
@@ -78,25 +70,6 @@ class KioskController {
   // Returns the `KioskSystemSession`. Can be `nullptr` if called outside a
   // Kiosk session, or before `InitializeSystemSession`.
   virtual KioskSystemSession* GetKioskSystemSession() = 0;
-
-  // Returns the `kiosk_vision::TelemetryProcessor`.
-  // Can be `nullptr` in the following cases:
-  // * Outside a Kiosk session.
-  // * Before `InitializeSystemSession`.
-  // * When the Kiosk Vision framework is disabled by policy.
-  // * When the Kiosk Vision framework is not yet initialized.
-  virtual kiosk_vision::TelemetryProcessor*
-  GetKioskVisionTelemetryProcessor() = 0;
-
-  // Returns the `InternalsPageProcessor`.
-  // Can be `nullptr` in the following cases:
-  // * Outside a Kiosk session.
-  // * Before `InitializeSystemSession`.
-  // * When the Kiosk Vision framework is disabled by policy.
-  // * When the Kiosk Vision framework is not yet initialized.
-  // * When the internals page feature flag is disabled.
-  virtual kiosk_vision::InternalsPageProcessor*
-  GetKioskVisionInternalsPageProcessor() = 0;
 
   // Registers local state prefs relevant for Kiosk.
   static void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
