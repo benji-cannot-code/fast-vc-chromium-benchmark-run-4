2052d3b62eae2d49074a96c5c1c29fe59cc39542
Lijin Shen <lazzzis@google.com>
2025-07-28 11:32:12 -0700

    Fix: Search Engine Choice Service tests are missing
    The test was not added in the junit library.
    
    This CL also fixes an issue that the supplier was not given a value yet
    when used.
    
    Bug: 433496320
    Change-Id: I4b7c793cf8b804898266c76dc2bb5bca6787648e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788786
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492983}

--- components/search_engines/android/java/src/org/chromium/components/search_engines/SearchEngineChoiceServiceUnitTest.java
+++ components/search_engines/android/java/src/org/chromium/components/search_engines/SearchEngineChoiceServiceUnitTest.java
@@ -41,6 +41,7 @@ import org.chromium.components.search_engines.SearchEngineChoiceService.RefreshR
 import org.chromium.components.search_engines.SearchEngineCountryDelegate.DeviceChoiceEventType;
 
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(BaseRobolectricTestRunner.class)
 public class SearchEngineChoiceServiceUnitTest {
@@ -88,6 +89,7 @@ public class SearchEngineChoiceServiceUnitTest {
 
         var supplier = service.getIsDeviceChoiceRequiredSupplier();
         ShadowLooper.runUiThreadTasks();
+        ShadowLooper.idleMainLooper(3000, TimeUnit.MILLISECONDS);
         assertTrue(supplier.get());
 
         // The calls below should be fine to run without triggering anything.
