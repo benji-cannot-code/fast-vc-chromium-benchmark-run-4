f7da9ccf3ebc6fbabd5b0bf7adc1ec8e102f4544
Tanmoy Mollik <triploblastic@google.com>
2025-07-28 07:42:24 -0700

    [Android] Use FakeIdentityManager in SigninManagerImplTest
    SigninManagerImpl no longer observes IdentityManager as it doesn't use
    any of the observer methods.
    SigninManagerImplTest uses FakeIdentityManager.
    
    Bug: 374683682
    Change-Id: I09ef570dd8b254e31166c95270a11ecf279545f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722352
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492805}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
@@ -79,7 +79,7 @@ import java.util.Objects;
  * <p>See chrome/browser/android/signin/signin_manager_android.h for more details.
  */
 @NullMarked
-class SigninManagerImpl implements IdentityManager.Observer, SigninManager, AccountsChangeObserver {
+class SigninManagerImpl implements SigninManager, AccountsChangeObserver {
     private static final String TAG = "SigninManager";
 
     private static final Duration MANAGED_STATUS_TIMEOUT = Duration.ofSeconds(10);
@@ -134,7 +134,6 @@ class SigninManagerImpl implements IdentityManager.Observer, SigninManager, Acco
                 new SigninManagerImpl(
                         nativeSigninManagerAndroid, profile, identityManager, identityMutator);
 
-        identityManager.addObserver(signinManager);
         AccountInfoServiceProvider.init(identityManager);
 
         return signinManager;
@@ -174,7 +173,6 @@ class SigninManagerImpl implements IdentityManager.Observer, SigninManager, Acco
     @CalledByNative
     void destroy() {
         AccountInfoServiceProvider.get().destroy();
-        mIdentityManager.removeObserver(this);
         mAccountManagerFacade.removeObserver(this);
         mPrefChangeRegistrar.destroy();
         mNativeSigninManagerAndroid = 0;
@@ -486,10 +484,7 @@ class SigninManagerImpl implements IdentityManager.Observer, SigninManager, Acco
         }
     }
 
-    /**
-     * Initialize SignOutState, and call identity mutator to revoke the sync consent.  Processing
-     * will complete asynchronously in the {@link #onPrimaryAccountChanged()} callback.
-     */
+    /** Initialize SignOutState, and call identity mutator to revoke the sync consent. */
     @Override
     public void revokeSyncConsent(
             @SignoutReason int signoutSource,
