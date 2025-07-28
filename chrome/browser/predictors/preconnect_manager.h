2513be96f636dc263ac337e135f22f896b815a92
David Sanders <dsanders11@ucsbalum.com>
2025-07-27 20:31:15 -0700

    Forward declare network::mojom::NetworkContext in preconnect_manager.h
    Should save ~82 MiB of compiler input size.
    
    Adds missing includes as needed to fix the build.
    
    Bug: 429365675
    Change-Id: I8ef751f9dbd4276f074e4aba85e6c53f38a66e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6789763
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492581}

--- chrome/browser/predictors/preconnect_manager.h
+++ chrome/browser/predictors/preconnect_manager.h
@@ -15,7 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_anonymization_key.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/mojom/connection_change_observer_client.mojom.h"
-#include "services/network/public/mojom/network_context.mojom.h"
+#include "url/gurl.h"
+
+namespace network::mojom {
+class NetworkContext;
+}
 
 namespace predictors {
 
