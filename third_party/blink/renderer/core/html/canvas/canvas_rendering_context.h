dcf5980671a3e2d47f09fa36c23a16a171b58433
Colin Blundell <blundell@chromium.org>
2025-07-28 04:13:57 -0700

    [Blink] Have low-latency WebGL paint only if the dirty rect is non-empty
    PaintRenderingResultsToResource() already returns nullptr for the
    resource if it needs to use its CRP to create the resource, but it
    doesn't do so if it can export the resource directly. Whether it can
    export the resource directly or not is an internal detail. This CL
    aligns these two behaviors by hoisting the "don't paint if the dirty
    rect is empty" out to the callsite.  This also aligns low-latency WebGL
    behavior with that of Canvas2D just below.
    
    As with [1], this does have a side effect of not creating the
    CRP/resource in this case. The arguments on that CL also apply here.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6787151
    
    Bug: 352263194
    Change-Id: I80a71ef1700ce4e6cae7357538c3f1920b527d2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787890
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492708}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h
@@ -218,7 +218,6 @@ class CORE_EXPORT CanvasRenderingContext
   // WebGL-specific methods
   virtual void ClearMarkedCanvasDirty() {}
   virtual scoped_refptr<CanvasResource> PaintRenderingResultsToResource(
-      bool was_dirty,
       SourceDrawingBuffer source_buffer,
       FlushReason reason) {
     NOTREACHED();
