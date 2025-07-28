6400e2ea10f8d6108068334b53bebf0092dcdd08
Slobodan Pejic <slobodan@chromium.org>
2025-07-28 07:18:18 -0700

    Avoid std::move into a const-ref parameter.
    IsValidPaymentValidationErrorsFormat takes a const-ref, so
    JNI_PaymentValidator_ValidatePaymentValidationErrorsAndroid should not
    std::move errors.
    
    This is unlikely to be related to the bug.
    
    Bug: 434130458
    Change-Id: I9c444ff0388d5733b3e822d02a9e4d2e7c4c4c29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786830
    Reviewed-by: Kavita Soni <kavitasoni@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492793}

--- components/payments/content/android/payment_validator_android.cc
+++ components/payments/content/android/payment_validator_android.cc
@@ -47,7 +47,7 @@ jboolean JNI_PaymentValidator_ValidatePaymentValidationErrorsAndroid(
   }
   std::string unused_error_message;
   return PaymentsValidators::IsValidPaymentValidationErrorsFormat(
-      std::move(errors), &unused_error_message);
+      errors, &unused_error_message);
 }
 
 }  // namespace payments
