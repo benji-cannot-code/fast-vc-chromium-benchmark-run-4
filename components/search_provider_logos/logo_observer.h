886f4514d3865c4e68baefe06fd04dcb1a2959b6
Jérôme Lebel <jlebel@chromium.org>
2025-07-28 07:17:06 -0700

    [iOS] Add comments to deprecate APIs
    <LogoVendor>, <DoodleObserver>, CreateLogoVendor() and LogoObserver
    need to be removed after migrating to SearchEngineLogoMediator.
    
    Bug: 423883582
    Change-Id: I839e88daf6996814f500edc095cbf2264710cafe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786937
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492791}

--- components/search_provider_logos/logo_observer.h
+++ components/search_provider_logos/logo_observer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace search_provider_logos {
 
 // Receives updates when the search provider's logo is available.
+// TODO(crbug.com/423883582): Need to be removed once LogoController is removed.
 class LogoObserver {
  public:
   virtual ~LogoObserver() = default;
