b477899ad445c7b1961131be1c30dc9079feebab
Thomas Lukaszewicz <tluk@chromium.org>
2025-07-28 12:54:08 -0700

    [bedrock] Clear Browser references in BrowserFrameMac
    This CL clears Browser dependencies from BrowserFrameMac, Mac's
    Browser NativeWidget, when the Browser's Widget has been destroyed.
    
    Widget and NativeWidget do not have strictly overlapping lifetimes,
    it is possible for a Widget to exist without its NativeWidget and
    vice-versa.
    
    This CL ensures that Browser refs are cleared from BrowserFrameMac
    when its Widget is destroyed to mitigate the risks of dangling
    refs.
    
    Bug: 413168662
    Change-Id: I8a819db130fb8e1793a11f4ba833381000190688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6791145
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493027}

--- chrome/browser/ui/views/frame/browser_frame_mac.h
+++ chrome/browser/ui/views/frame/browser_frame_mac.h
@@ -39,6 +39,7 @@ class BrowserFrameMac : public views::NativeWidgetMac,
   void OnFocusWindowToolbar() override;
   void OnWindowFullscreenTransitionStart() override;
   void OnWindowFullscreenTransitionComplete() override;
+  void OnWidgetDestroyed(views::Widget* widget) override;
 
   // Overridden from NativeBrowserFrame:
   views::Widget::InitParams GetWidgetParams(
