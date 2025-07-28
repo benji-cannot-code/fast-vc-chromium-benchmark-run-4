7ced8f7a21b241dfea18db5569f7de767177bd24
Kent Tamura <tkent@chromium.org>
2025-07-28 01:22:33 -0700

    Move wtf/allocator/allocator.h to "blink" namespace
    This CL has no behavior changes.
    
    Bug: 422768753
    Change-Id: I174c7559a6611018934911036c2bdff464ea9930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6791246
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492633}

--- third_party/blink/renderer/platform/wtf/allocator/allocator.h
+++ third_party/blink/renderer/platform/wtf/allocator/allocator.h
@@ -12,13 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
 #include "third_party/blink/renderer/platform/wtf/type_traits.h"
 
-namespace WTF {
+namespace blink::internal {
 
-
-namespace internal {
 // A dummy class used in following macros.
 class __thisIsHereToForceASemicolonAfterThisMacro;
-}  // namespace internal
+
+}  // namespace blink::internal
 
 // Classes that contain references to garbage-collected objects but aren't
 // themselves garbaged allocated, have some extra macros available which
@@ -48,7 +47,7 @@ class __thisIsHereToForceASemicolonAfterThisMacro;
   void* operator new(size_t) = delete;                           \
                                                                  \
  public:                                                         \
-  friend class ::WTF::internal::__thisIsHereToForceASemicolonAfterThisMacro
+  friend class ::blink::internal::__thisIsHereToForceASemicolonAfterThisMacro
 
 #define STATIC_ONLY(Type)                                       \
   Type() = delete;                                              \
@@ -126,9 +125,7 @@ class __thisIsHereToForceASemicolonAfterThisMacro;
   }                                                              \
                                                                  \
  private:                                                        \
-  friend class ::WTF::internal::__thisIsHereToForceASemicolonAfterThisMacro
-
-}  // namespace WTF
+  friend class ::blink::internal::__thisIsHereToForceASemicolonAfterThisMacro
 
 // This version of placement new omits a 0 check.
 inline void* operator new(size_t, base::NotNullTag, void* location) {
