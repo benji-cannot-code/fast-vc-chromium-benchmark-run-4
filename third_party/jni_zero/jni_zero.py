80a3325fc0622fd3e71e5e29904cfc3764b2706f
Łukasz Anforowicz <lukasza@chromium.org>
2025-07-28 14:47:42 -0700

    Revert "JNI Zero: Add --include-path-prefix, --enable-legacy-natives, --allow-private-called-by-natives"
    This reverts commit 39aa3778e3de2a41e8e1b8ebcd90d80bb2bf9439.
    
    Reason for revert: speculatively suspecting that this is the cause of the Chromium Tree closure - see the build errors in https://ci.chromium.org/ui/p/chromium/builders/ci/android-archive-rel/60583
    
    Bug: 40253109, 346596940
    Original change's description:
    > JNI Zero: Add --include-path-prefix, --enable-legacy-natives, --allow-private-called-by-natives
    >
    > --include-path-prefix: For anyone clients that import the library at
    >   a location other than "third_party/jni_zero".
    > --enable-legacy-natives: Will allow us to disable non-proxy natives
    >   without immediately disabling it outside of chromium
    > --allow-private-called-by-natives: Will allow us to experiment with
    >   proxy called-by-natives.
    >
    > Bug: 40253109, 346596940
    > Change-Id: I50300bd5103de1f84e03c816037b0b0005e59ca6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788898
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Peter Wen <wnwen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1493074}
    
    Bug: 40253109, 346596940
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iab5811594c554403dc285187f927cd9c1a889b01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6795044
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493104}

--- third_party/jni_zero/jni_zero.py
+++ third_party/jni_zero/jni_zero.py
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import argparse
 import os
-import posixpath
 import shutil
 import sys
 
@@ -92,20 +91,6 @@ def _add_codegen_args(parser, *, is_final=False, is_javap=False):
   group.add_argument(
       '--module-name',
       help='Only look at natives annotated with a specific module name.')
-  default_path_prefix = os.path.relpath(
-      os.path.join(posixpath.dirname(__file__), ''), '../..') + '/'
-  group.add_argument('--include-path-prefix',
-                     help='Value for #include "${PREFIX}jni_zero.h" '
-                     f'(default={default_path_prefix})',
-                     default=default_path_prefix)
-  group.add_argument('--extra-include',
-                     action='append',
-                     dest='extra_includes',
-                     help='Header file to #include in the generated header.')
-  group.add_argument(
-      '--enable-legacy-natives',
-      action='store_true',
-      help='Whether to generate code from "native" java methods.')
   if is_final:
     group.add_argument(
         '--add-stubs-for-missing-native',
@@ -129,6 +114,10 @@ def _add_codegen_args(parser, *, is_final=False, is_javap=False):
                        action='store_true',
                        help='Whether to maintain ForTesting JNI methods.')
   else:
+    group.add_argument('--extra-include',
+                       action='append',
+                       dest='extra_includes',
+                       help='Header file to #include in the generated header.')
     group.add_argument(
         '--split-name',
         help='Split name that the Java classes should be loaded from.')
@@ -141,9 +130,6 @@ def _add_codegen_args(parser, *, is_final=False, is_javap=False):
         '--enable-definition-macros',
         action='store_true',
         help='Generate JNI glue code in DEFINE_JNI_FOR_MyClass() macros')
-    group.add_argument('--allow-private-called-by-natives',
-                       action='store_true',
-                       help='Whether to allow private @CalledByNative symbols.')
 
   if is_javap:
     group.add_argument('--unchecked-exceptions',
@@ -231,7 +217,6 @@ def main():
     parser.parse_args(sys.argv[1:] + ['-h'])
   else:
     args = parser.parse_args()
-
     bool_arg = lambda name: getattr(args, name, False)
     jni_mode = common.JniMode(is_hashing=bool_arg('use_proxy_hash'),
                               is_muxing=bool_arg('enable_jni_multiplexing'),
