7a388cdaca4671f458ac10c94c36dd4a0b17f446
John Palmer <jopalmer@chromium.org>
2025-07-27 18:42:46 -0700

    Revert "Enable field testing for pwa install page action migration"
    This reverts commit 98a28f3418129c94abf7b8e2537f2c677dc00a75.
    
    Reason for revert: Almost certainly broke apps.Sharesheet Tast Tast test based on https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-amd64-generic-rel-tast/23492/blamelist (to be confirmed - see https://crosperf.googleplex.com/b/9d358ee1-49ed-499a-aea6-6440bc06be61)
    
    Bug: 426246067
    Original change's description:
    > Enable field testing for pwa install page action migration
    >
    > There were some failing tests that were also updated to handle the new
    > migration path.
    >
    > Bug: 426246067
    > Change-Id: I556f9f0eded53613eb67f34ec2361010277612d5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6783427
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Kaan Alsan <alsan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1492288}
    
    Bug: 426246067
    Change-Id: I71abbede644eaabd345234d59d49af4fe029bfb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6791085
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492566}

--- chrome/browser/ui/views/page_action/page_action_properties_provider.cc
+++ chrome/browser/ui/views/page_action/page_action_properties_provider.cc
@@ -72,7 +72,6 @@ constexpr auto kPageActionProperties =
             {
                 .histogram_name = "PwaInstall",
                 .type = PageActionIconType::kPwaInstall,
-                .element_identifier = kInstallPwaElementId,
             },
         },
         {
