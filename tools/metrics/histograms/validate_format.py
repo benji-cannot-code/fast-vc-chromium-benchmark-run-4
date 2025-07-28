1330f341b3ac84ffd69a890b00182f6fc7230275
Alicja Opalinska <aopalinska@google.com>
2025-07-28 07:13:49 -0700

    Moved xml_utils.py to common folder.
    This file is not specific to histograms, it can also be useful for other
    modules with xml files.
    The only changes here are related to importing this file from its new
    location.
    
    Change-Id: Ifa5a149ee37b804679f99de5dcc11424a69e06d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6783105
    Reviewed-by: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492787}

--- tools/metrics/histograms/validate_format.py
+++ tools/metrics/histograms/validate_format.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import argparse
 import logging
+import os
 import re
 import sys
 from typing import List
@@ -15,6 +16,8 @@ import xml.dom.minidom
 import extract_histograms
 import histogram_paths
 import merge_xml
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 import xml_utils
 
 # The allowlist of namespaces (histogram prefixes, case insensitive) that are
