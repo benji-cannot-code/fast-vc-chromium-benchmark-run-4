779a871d17ea35f1d82209145b0062981e3776c8
“Prasanaa” <prasanaa@google.com>
2025-07-28 08:15:59 -0700

    [iOS] Make UITextView not selectable in BWG FRE
    Makes UITextView not selectable in BWG FRE.
    
    Fixed: 434615657
    Change-Id: Ia0ae0562d2207b411159aaad072c4a2eec10ab8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6790949
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492840}

--- ios/chrome/browser/intelligence/bwg/ui/bwg_consent_view_controller.mm
+++ ios/chrome/browser/intelligence/bwg/ui/bwg_consent_view_controller.mm
@@ -456,6 +456,7 @@ NSString* const kSecondBoxLink2ActionNonManagedAccount =
   bodyTextView.backgroundColor = [UIColor clearColor];
   bodyTextView.scrollEnabled = NO;
   bodyTextView.editable = NO;
+  bodyTextView.selectable = NO;
   bodyTextView.delegate = self;
   bodyTextView.textContainerInset = UIEdgeInsetsZero;
   bodyTextView.textContainer.lineFragmentPadding = 0;
@@ -473,6 +474,7 @@ NSString* const kSecondBoxLink2ActionNonManagedAccount =
   footNoteTextView.backgroundColor = [UIColor clearColor];
   footNoteTextView.scrollEnabled = NO;
   footNoteTextView.editable = NO;
+  footNoteTextView.selectable = NO;
   footNoteTextView.delegate = self;
 
   footNoteTextView.textContainerInset = UIEdgeInsetsZero;
