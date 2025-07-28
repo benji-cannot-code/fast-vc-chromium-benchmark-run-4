82e581598a20430ea5904cc713bd4e55eee0fffc
Tommy Steimel <steimel@chromium.org>
2025-07-28 09:35:59 -0700

    [video pip] Better display state on live caption button
    Currently, on video picture-in-picture the live caption button does
    not change its accessibility state depending on whether the live caption
    dialog is open. This makes it harder for screen reader users to
    understand whether the button will open or close a dialog. This CL adds
    state to the button to say whether or not the dialog is expanded or
    collapsed.
    
    Additionally, when it is expanded, this keeps the inkdrop highlight on
    to better indicate to the user that it can be clicked again to close
    the dialog.
    
    Bug: 432792364
    Change-Id: I1452f709f66772d5c9e86935f7bcc723c69454ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787420
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1492894}

--- chrome/browser/ui/views/overlay/video_overlay_window_views_unittest.cc
+++ chrome/browser/ui/views/overlay/video_overlay_window_views_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/overlay/close_image_button.h"
 #include "chrome/browser/ui/views/overlay/hang_up_button.h"
 #include "chrome/browser/ui/views/overlay/minimize_button.h"
+#include "chrome/browser/ui/views/overlay/overlay_window_live_caption_button.h"
 #include "chrome/browser/ui/views/overlay/overlay_window_live_caption_dialog.h"
 #include "chrome/browser/ui/views/overlay/playback_image_button.h"
 #include "chrome/browser/ui/views/overlay/simple_overlay_window_image_button.h"
@@ -51,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/test/event_generator.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/vector2d.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/widget_fade_animator.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/button/toggle_button.h"
@@ -868,7 +870,7 @@ TEST_F(VideoOverlayWindowViewsTest, OriginNotDrawnWhen2024UIIsDisabled) {
 TEST_F(VideoOverlayWindowViewsTest,
        LiveCaptionButtonNotDrawnWhen2024UIIsDisabled) {
   overlay_window().ForceControlsVisibleForTesting(true);
-  SimpleOverlayWindowImageButton* live_caption_button =
+  OverlayWindowLiveCaptionButton* live_caption_button =
       overlay_window().live_caption_button_for_testing();
   ASSERT_EQ(nullptr, live_caption_button);
 }
@@ -1317,13 +1319,23 @@ TEST_F(VideoOverlayWindowViewsWith2024UITest, VideoConferencingUI) {
 TEST_F(VideoOverlayWindowViewsWith2024UITest, LiveCaption) {
   overlay_window().ForceControlsVisibleForTesting(true);
   profile().GetPrefs()->SetBoolean(prefs::kLiveCaptionEnabled, false);
-  SimpleOverlayWindowImageButton* live_caption_button =
+  OverlayWindowLiveCaptionButton* live_caption_button =
       overlay_window().live_caption_button_for_testing();
   OverlayWindowLiveCaptionDialog* live_caption_dialog =
       overlay_window().live_caption_dialog_for_testing();
 
   ASSERT_NE(nullptr, live_caption_button);
   ASSERT_NE(nullptr, live_caption_dialog);
+
+  {
+    // The accessible data of the toggle button should show the state as
+    // collapsed.
+    ui::AXNodeData node_data;
+    live_caption_button->GetViewAccessibility().GetAccessibleNodeData(
+        &node_data);
+    EXPECT_FALSE(node_data.HasState(ax::mojom::State::kExpanded));
+    EXPECT_TRUE(node_data.HasState(ax::mojom::State::kCollapsed));
+  }
 
   // The live caption button should start visible and the live caption dialog
   // should start invisible.
@@ -1338,6 +1350,16 @@ TEST_F(VideoOverlayWindowViewsWith2024UITest, LiveCaption) {
   live_caption_button_clicker.NotifyClick(dummy_event);
   WaitForLayout();
   EXPECT_TRUE(live_caption_dialog->IsDrawn());
+
+  {
+    // The accessible data of the toggle button should show the state as
+    // expanded once the dialog is there.
+    ui::AXNodeData node_data;
+    live_caption_button->GetViewAccessibility().GetAccessibleNodeData(
+        &node_data);
+    EXPECT_TRUE(node_data.HasState(ax::mojom::State::kExpanded));
+    EXPECT_FALSE(node_data.HasState(ax::mojom::State::kCollapsed));
+  }
 
   // The live caption button should be enabled and toggled off, while the live
   // translate button should be disabled and toggled off.
@@ -1365,7 +1387,7 @@ TEST_F(VideoOverlayWindowViewsWith2024UITest, LiveCaption) {
 
 TEST_F(VideoOverlayWindowViewsWith2024UITest, LiveCaption_MouseClickOutside) {
   overlay_window().ForceControlsVisibleForTesting(true);
-  SimpleOverlayWindowImageButton* live_caption_button =
+  OverlayWindowLiveCaptionButton* live_caption_button =
       overlay_window().live_caption_button_for_testing();
   OverlayWindowLiveCaptionDialog* live_caption_dialog =
       overlay_window().live_caption_dialog_for_testing();
@@ -1400,7 +1422,7 @@ TEST_F(VideoOverlayWindowViewsWith2024UITest, LiveCaption_MouseClickOutside) {
 
 TEST_F(VideoOverlayWindowViewsWith2024UITest, LiveCaption_GestureTapOutside) {
   overlay_window().ForceControlsVisibleForTesting(true);
-  SimpleOverlayWindowImageButton* live_caption_button =
+  OverlayWindowLiveCaptionButton* live_caption_button =
       overlay_window().live_caption_button_for_testing();
   OverlayWindowLiveCaptionDialog* live_caption_dialog =
       overlay_window().live_caption_dialog_for_testing();
