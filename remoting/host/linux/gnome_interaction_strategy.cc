c7a398267a9f1b1f71e68d9ad8abc57f6467859c
Lambros Lambrou <lambroslambrou@chromium.org>
2025-07-28 11:27:43 -0700

    [remoting] Remove unused method from GnomeInteractionStrategy.
    The OnSessionInit() method was not called anywhere. The callback that
    runs is actually a lambda inside the Create() method.
    
    Change-Id: Ie47c7febb4f1f578aecbe3ef2f8bf4cc09f24982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792645
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492978}

--- remoting/host/linux/gnome_interaction_strategy.cc
+++ remoting/host/linux/gnome_interaction_strategy.cc
@@ -467,7 +467,8 @@ void GnomeInteractionStrategyFactory::Create(
       [](std::unique_ptr<GnomeInteractionStrategy> session,
          CreateCallback callback, base::expected<void, std::string> result) {
         if (!result.has_value()) {
-          LOG(ERROR) << result.error();
+          LOG(ERROR) << "Failed to initialize Gnome Remote Desktop session: "
+                     << result.error();
           std::move(callback).Run(nullptr);
           return;
         }
@@ -475,20 +476,6 @@ void GnomeInteractionStrategyFactory::Create(
         std::move(callback).Run(std::move(session));
       },
       std::move(session), std::move(callback)));
-}
-
-void GnomeInteractionStrategyFactory::OnSessionInit(
-    std::unique_ptr<GnomeInteractionStrategy> session,
-    CreateCallback callback,
-    base::expected<void, std::string> result) {
-  if (!result.has_value()) {
-    LOG(ERROR) << "Failed to initialize Gnome Remote Desktop session: "
-               << result.error();
-    std::move(callback).Run(nullptr);
-    return;
-  }
-
-  std::move(callback).Run(std::move(session));
 }
 
 }  // namespace remoting
