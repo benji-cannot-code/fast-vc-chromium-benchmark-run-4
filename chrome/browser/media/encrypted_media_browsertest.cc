a3bacc8a72a74b9ba4dc3c81549cdaeccaf6706f
Nic Champagne Williamson <champnic@microsoft.com>
2025-07-28 14:57:18 -0700

    Cleanup some usage of MFTEnum2 and MFTEnumEx and fix leaks
    Ensures returned 'activates' array is properly CoTaskMemFree'd by using
    base::win::ScopedCoMem. Additionally, properly maintains ref count by
    adding missing Release() calls.
    
    Bug: 431004496
    Change-Id: I1a898ae39ce6df60af0cba9feed63b64a0d072c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6729399
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Nic Williamson <champnic@microsoft.com>
    Reviewed-by: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1493116}

--- chrome/browser/media/encrypted_media_browsertest.cc
+++ chrome/browser/media/encrypted_media_browsertest.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include <mfapi.h>
 
+#include "base/win/scoped_co_mem.h"
 #include "base/win/windows_version.h"
 #include "chrome/browser/media/media_foundation_service_monitor.h"
 #include "content/public/browser/gpu_data_manager.h"
@@ -1557,7 +1558,7 @@ class MediaFoundationEncryptedMediaTest : public EncryptedMediaTestBase {
 #if BUILDFLAG(ENABLE_PLATFORM_ENCRYPTED_DOLBY_VISION)
   bool IsVideoDecoderSupported(const GUID& video_decoder_guid) {
     MFT_REGISTER_TYPE_INFO inputInfo{MFMediaType_Video, video_decoder_guid};
-    IMFActivate** activates;
+    base::win::ScopedCoMem<IMFActivate*> activates;
     unsigned int numActivates = 0;
     auto result = MFTEnumEx(MFT_CATEGORY_VIDEO_DECODER, MFT_ENUM_FLAG_SYNCMFT,
                             &inputInfo, nullptr, &activates, &numActivates);
@@ -1566,12 +1567,17 @@ class MediaFoundationEncryptedMediaTest : public EncryptedMediaTestBase {
       return false;
     }
 
+    // Clean up the activates
+    for (unsigned int i = 0; i < numActivates; ++i) {
+      activates[i]->Release();
+    }
+
     return true;
   }
 
   bool IsVideoRendererEffectSupported(const wchar_t* profile) {
     bool supported = false;
-    IMFActivate** activates = nullptr;
+    base::win::ScopedCoMem<IMFActivate*> activates;
     unsigned int numActivates = 0;
     auto result = MFTEnumEx(MFT_CATEGORY_VIDEO_RENDERER_EFFECT,
                             MFT_ENUM_FLAG_SORTANDFILTER, nullptr, nullptr,
@@ -1599,8 +1605,9 @@ class MediaFoundationEncryptedMediaTest : public EncryptedMediaTestBase {
       PropVariantClear(&var);
     }
 
-    if (*activates) {
-      (*activates)->Release();
+    // Clean up the activates
+    for (unsigned int i = 0; i < numActivates; ++i) {
+      activates[i]->Release();
     }
 
     return supported;
