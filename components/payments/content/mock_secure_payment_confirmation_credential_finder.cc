2c5d8451b42c4d1feb1d6f9d3e225d248c85163d
Stephen McGruer <smcgruer@chromium.org>
2025-07-28 13:24:07 -0700

    Extract a credential finder out of SecurePaymentConfirmationAppFactory
    This CL refactors SecurePaymentConfirmationAppFactory to extract
    the credential finding logic into a new class,
    SecurePaymentConfirmationCredentialFinder. This simplifies the
    tests for SecurePaymentConfirmationAppFactory, which no longer need
    to care about setting up the underlying user database and/or
    credential store APIs, and will also allow us to more easily change
    the logic for credential finding in the future (e.g., to use *both*
    the credential store APIs and the database together).
    
    Changes in SecurePaymentConfirmationAppFactory unittests:
      - Functionality from test subclasses moved to the top-level
        SecurePaymentConfirmationAppFactoryTest class.
      - Tests now use a mock SecurePaymentConfirmationCredentialFinder
        instead of talking to mock database or authenticators.
      - Third-party payment bit related tests move to unittests for the
        new credential finder class.
      - AppDisabledIfCredentialStoreAPIsUnavailable test changed to be
        more generic, renamed to AppDisabledIfCredentialFetchingFails.
      - SecureConfirmationPaymentRequest_PaymentInstrumentDetails moved
        to the base test class.
      - Fallback_NoAuthenticator renamed to match its actual
        implementation, that there is no *user-verifying platform*
        authenticator available.
    
    Bug: None
    Change-Id: Ic11849baa0f3fa1b875e9c86c539e88f3d395b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785107
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493045}

---
+++ components/payments/content/mock_secure_payment_confirmation_credential_finder.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/payments/content/mock_secure_payment_confirmation_credential_finder.h"
+
+namespace payments {
+
+MockSecurePaymentConfirmationCredentialFinder::
+    MockSecurePaymentConfirmationCredentialFinder() = default;
+MockSecurePaymentConfirmationCredentialFinder::
+    ~MockSecurePaymentConfirmationCredentialFinder() = default;
+
+}  // namespace payments
