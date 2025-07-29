df7fc4d1a92325dd377f6966860a3742a56434af
Takuto Ikuta <tikuta@chromium.org>
2025-07-28 23:13:24 -0700

    Update Hunspell 6d7d19f -> 96466a6
    https://github.com/hunspell/hunspell/commit/96466a6
    
    This is mainly for https://github.com/hunspell/hunspell/pull/1054.
    
    Bug: 428778692
    Change-Id: I8272a5834d43ac634a5a3aced175f9f032d01d7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797030
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493301}

--- third_party/hunspell/src/hunspell/hunspell.cxx
+++ third_party/hunspell/src/hunspell/hunspell.cxx
@@ -122,7 +122,7 @@ public:
   int add_with_affix(const std::string& word, const std::string& example);
   int remove(const std::string& word);
   const std::string& get_version_cpp() const;
-  struct cs_info* get_csconv();
+  const struct cs_info* get_csconv() const;
 
   int spell(const char* word, int* info = NULL, char** root = NULL);
   int suggest(char*** slst, const char* word);
@@ -146,7 +146,7 @@ private:
   std::string affixpath;
 #endif
   std::string encoding;
-  struct cs_info* csconv;
+  const struct cs_info* csconv;
   int langnum;
   int utf8;
   int complexprefixes;
@@ -1559,7 +1559,7 @@ const std::string& HunspellImpl::get_version_cpp() const {
   return pAMgr->get_version();
 }
 
-struct cs_info* HunspellImpl::get_csconv() {
+const struct cs_info* HunspellImpl::get_csconv() const {
   return csconv;
 }
 
@@ -2247,7 +2247,7 @@ const std::string& Hunspell::get_version_cpp() const {
   return m_Impl->get_version_cpp();
 }
 
-struct cs_info* Hunspell::get_csconv() {
+const struct cs_info* Hunspell::get_csconv() const {
   return m_Impl->get_csconv();
 }
 
--- third_party/hunspell/src/hunspell/suggestmgr.hxx
+++ third_party/hunspell/src/hunspell/suggestmgr.hxx
@@ -104,7 +104,7 @@ class SuggestMgr {
 
   AffixMgr* pAMgr;
   unsigned int maxSug;
-  struct cs_info* csconv;
+  const struct cs_info* csconv;
   int utf8;
   int langnum;
   int nosplitsugs;
