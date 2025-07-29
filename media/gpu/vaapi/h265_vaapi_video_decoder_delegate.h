5b75750bd57f1540ee6b9de8463847d390a9f490
Stephen Nusko <nuskos@chromium.org>
2025-07-28 20:10:35 -0700

    spanification: automatically spanify various pointers
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    This CL has no behavior changes.
    
    All changes should be correct and no behaviour changes in patchset 1, however the plan is to follow up and remove the redundant size parameters. To this effect in patchset 2
    we've added CHECKS and renames to note the fact the size is believed to be redundant. This was done through AI and should be carefully checked, but the CHECKS are not fatal and thus if unsure should be fine to land and remove after reports of the crashes.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    
    and then split with
    `git cl split --target-range 8 32 --max-depth=2 --no-reviewers -d ~/description.txt`
    
    This covers directories:
    media/gpu/vaapi
    
    This is part of auto spanification see internal doc about it:
    https://docs.google.com/document/u/1/d/1poEhNARfFas2osqPoXGzVZtBgnOQGQtQNUFTA-MlADs/edit?tab=t.0
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 431824299
    Change-Id: I56711e3deabe6ffca682c4ac78bfbc39c071aa11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734598
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493258}

--- media/gpu/vaapi/h265_vaapi_video_decoder_delegate.h
+++ media/gpu/vaapi/h265_vaapi_video_decoder_delegate.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <va/va.h>
 
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "media/gpu/h265_decoder.h"
@@ -72,7 +73,7 @@ class H265VaapiVideoDecoderDelegate : public H265Decoder::H265Accelerator,
  private:
   void FillVAPicture(VAPictureHEVC* va_pic, scoped_refptr<H265Picture> pic);
   void FillVARefFramesFromRefList(const H265Picture::Vector& ref_pic_list,
-                                  VAPictureHEVC* va_pics);
+                                  base::span<VAPictureHEVC> va_pics);
 
   // Returns |kInvalidRefPicIndex| if it cannot find a picture.
   int GetRefPicIndex(int poc);
--- media/gpu/vaapi/test/h265_vaapi_wrapper.h
+++ media/gpu/vaapi/test/h265_vaapi_wrapper.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_VAAPI_TEST_H265_VAAPI_WRAPPER_H_
 #define MEDIA_GPU_VAAPI_TEST_H265_VAAPI_WRAPPER_H_
 
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
 #include "media/gpu/vaapi/test/h265_dpb.h"
@@ -56,7 +57,7 @@ class H265VaapiWrapper {
  private:
   void FillVAPicture(VAPictureHEVC* va_pic, scoped_refptr<H265Picture> pic);
   void FillVARefFramesFromRefList(const H265Picture::Vector& ref_pic_list,
-                                  VAPictureHEVC* va_pics);
+                                  base::span<VAPictureHEVC> va_pics);
 
   // Returns |kInvalidRefPicIndex| if it cannot find a picture.
   int GetRefPicIndex(int poc);
