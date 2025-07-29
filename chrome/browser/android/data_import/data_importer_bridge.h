5e027c33f36f38d257af3e82129007f2bdd49156
Marc Treib <treib@chromium.org>
2025-07-29 03:09:55 -0700

    DataImporterService: Add JNI bridge to call the actual importer
    ...plus code in DataImporterServiceImpl to create and call the bridge.
    
    For now, the bridge forwards importBookmarks and importReadingList calls
    to the native side (StablePortabilityDataImporter). Reporting results
    back to Java, as well as importHistory, as left for followup CLs.
    
    Bug: 430254294
    Change-Id: I1911f24914e9573dfac1ebf44466ebba5c3bea9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6783024
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493403}

---
+++ chrome/browser/android/data_import/data_importer_bridge.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_DATA_IMPORT_DATA_IMPORTER_BRIDGE_H_
+#define CHROME_BROWSER_ANDROID_DATA_IMPORT_DATA_IMPORTER_BRIDGE_H_
+
+#include <memory>
+
+#include "base/android/jni_android.h"
+#include "base/memory/raw_ptr.h"
+#include "components/user_data_importer/content/stable_portability_data_importer.h"
+
+class Profile;
+
+class DataImporterBridge {
+ public:
+  explicit DataImporterBridge(Profile* profile);
+
+  DataImporterBridge(const DataImporterBridge&) = delete;
+  DataImporterBridge& operator=(const DataImporterBridge&) = delete;
+  ~DataImporterBridge();
+
+  void Destroy(JNIEnv* env);
+
+  void ImportBookmarks(JNIEnv* env, jint owned_fd);
+
+  void ImportReadingList(JNIEnv* env, jint owned_fd);
+
+ private:
+  raw_ptr<Profile> profile_;
+
+  std::unique_ptr<user_data_importer::StablePortabilityDataImporter> importer_;
+};
+
+#endif  // CHROME_BROWSER_ANDROID_DATA_IMPORT_DATA_IMPORTER_BRIDGE_H_
