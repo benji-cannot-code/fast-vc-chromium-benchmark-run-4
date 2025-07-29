792f69f720db8a9bafbece2472abf7c810d19ace
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-07-29 11:14:28 -0700

    Forward kFeatureManagementRoundedWindows to guest mode
    Bug: b:434761128
    Change-Id: Ic3d49d0af3afae6e1d3595726f36b7ae59389657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797887
    Reviewed-by: Vincent Chiang <vincentchiang@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493672}

--- chrome/browser/ash/login/chrome_restart_request.cc
+++ chrome/browser/ash/login/chrome_restart_request.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/cryptohome/cryptohome_parameters.h"
 #include "chromeos/ash/components/dbus/dbus_thread_manager.h"
 #include "chromeos/ash/components/dbus/session_manager/session_manager_client.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/constants/dbus_switches.h"
 #include "components/account_id/account_id.h"
 #include "components/policy/core/common/policy_switches.h"
@@ -248,6 +249,7 @@ void DeriveFeatures(base::CommandLine* out_command_line) {
       &::features::kAccessibilityManifestV3EspeakNGTts,
       &::features::kAccessibilityManifestV3GoogleTts,
       &features::kAutoNightLight,
+      &chromeos::features::kFeatureManagementRoundedWindows,
       &ash::features::kSeamlessRefreshRateSwitching,
       &::features::kPluginVm,
       &display::features::kCtmColorManagement,
