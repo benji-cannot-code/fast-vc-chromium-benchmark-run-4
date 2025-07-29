2ad841b510a3ea56bc4ba408d1dc67d9cb203050
Colin Blundell <blundell@chromium.org>
2025-07-28 23:29:07 -0700

    [Blink] Make WebGL BitmapCanvasResourceProvider usage configurable
    WebGL falls back to creating a BitmapCRP if all other options fail. We
    would like to eliminate this, but each individual use case needs to be
    analyzed separately. This CL allows for doing so by adding a boolean
    that internal callers of PaintRenderingResultsToResourceProvider() can
    pass to configure whether the bitmap resource provider should be created
    or not. In this initial CL, all callers pass true to avoid changing any
    behavior.
    
    Bug: 352263194
    Change-Id: I5e8d73995b6eaf5f59cece015dbeb86f30faeb9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787214
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493304}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
@@ -713,7 +713,8 @@ class MODULES_EXPORT WebGLRenderingContextBase
   bool CanUseDrawingBufferSIWithoutCopyForLowLatency();
   void PageVisibilityChanged() override;
   void SizeChanged() override;
-  std::unique_ptr<CanvasResourceProvider> CreateCanvasResourceProvider();
+  std::unique_ptr<CanvasResourceProvider> CreateCanvasResourceProvider(
+      bool use_bitmap_provider);
   scoped_refptr<StaticBitmapImage> PaintRenderingResultsToSnapshot(
       SourceDrawingBuffer source_buffer,
       FlushReason reason) override;
@@ -1960,9 +1961,11 @@ class MODULES_EXPORT WebGLRenderingContextBase
       SourceDrawingBuffer source_buffer,
       bool export_only_if_update);
 
-  CanvasResourceProvider* GetOrCreateCanvasResourceProvider();
+  CanvasResourceProvider* GetOrCreateCanvasResourceProvider(
+      bool use_bitmap_provider);
   CanvasResourceProvider* PaintRenderingResultsToResourceProvider(
       SourceDrawingBuffer source_buffer,
+      bool use_bitmap_provider,
       bool* resource_provider_was_updated = nullptr);
   void TexImageHelperMediaVideoFrame(
       TexImageParams,
