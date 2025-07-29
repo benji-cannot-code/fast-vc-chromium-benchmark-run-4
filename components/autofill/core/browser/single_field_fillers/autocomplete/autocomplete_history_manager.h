ed3eb9fd9969046f03f4eb5923fa5059bfb50937
Karol Sygiet <sygiet@google.com>
2025-07-29 07:25:54 -0700

    [RSG] Add AutocompleteSuggestionGenerator to BAM::suggestion_generators
    Bug: 409962888
    Change-Id: I5a3a03bacd07e33aa980bb8bbb37ee3aeee7c6c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772491
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1493525}

--- components/autofill/core/browser/single_field_fillers/autocomplete/autocomplete_history_manager.h
+++ components/autofill/core/browser/single_field_fillers/autocomplete/autocomplete_history_manager.h
@@ -93,6 +93,10 @@ class AutocompleteHistoryManager : public KeyedService {
   void OnAutofillCleanupReturned(WebDataServiceBase::Handle current_handle,
       std::unique_ptr<WDTypedResult> result);
 
+  scoped_refptr<AutofillWebDataService> GetProfileDatabase() {
+    return profile_database_;
+  }
+
  private:
   friend class AutocompleteHistoryManagerTest;
 
