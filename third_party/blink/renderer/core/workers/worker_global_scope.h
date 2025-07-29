995a414ce99ca45549a53447394f569499feb2db
David Sanders <dsanders11@ucsbalum.com>
2025-07-29 16:08:57 -0700

    Forward declare WorkerMainScriptLoadParameters in worker_global_scope.h
    Should save ~84 MiB of compiler input size.
    
    Adds missing includes as needed to fix the build.
    
    Bug: 429365675
    Change-Id: I5fb00dea04574d92e3a7ff935704e35085842f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799862
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1493854}

--- third_party/blink/renderer/core/workers/worker_global_scope.h
+++ third_party/blink/renderer/core/workers/worker_global_scope.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "services/network/public/mojom/fetch_api.mojom-blink-forward.h"
-#include "third_party/blink/public/common/loader/worker_main_script_load_parameters.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/mojom/loader/code_cache.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/script/script_type.mojom-blink-forward.h"
@@ -75,6 +74,7 @@ class WorkerResourceTimingNotifier;
 class TrustedTypePolicyFactory;
 class V8UnionTrustedScriptURLOrUSVString;
 class WorkerLocation;
+struct WorkerMainScriptLoadParameters;
 class WorkerNavigator;
 class WorkerThread;
 
