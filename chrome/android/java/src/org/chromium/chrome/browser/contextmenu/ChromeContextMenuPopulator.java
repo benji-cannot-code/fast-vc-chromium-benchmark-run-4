0ee7b218d60a660603f7917b091ea9478a5be6b3
Mohamed Adel <adelm@google.com>
2025-07-29 14:35:11 -0700

    Add more metrics for custom context menu items
    This change adds an entry under CustomTabsFeature to see developer usage
    of the newly added AndroidX API as well as a metric to see how many
    times a custom menu item has been displayed in a shown menu.
    
    Bug: 430661408
    Change-Id: I7bf628e7c7e78106c787dc3a33f21e09fa1d9f8c
    Fixed: 430661408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798981
    Commit-Queue: Moe Adel <adelm@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1493813}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
@@ -113,6 +113,8 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
             1512; // Random id to avoid possible collisions.
     private static final int MAX_CUSTOM_MENU_ITEMS = 4;
     private static final String TAG = "CCMenuPopulator";
+    private static final String UMA_CONTEXTUAL_CUSTOM_ACTION_TYPE_DISPLAYED =
+            "CustomTabs.ContextMenu.DisplayedContextualCustomActionType";
     private static final String UMA_CONTEXTUAL_CUSTOM_ACTION_TYPE_SELECTED =
             "CustomTabs.ContextMenu.SelectedContextualCustomActionType";
     private final Context mContext;
@@ -484,12 +486,20 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
                 }
                 if (ChromeFeatureList.sCctContextualMenuItems.isEnabled()
                         && mMode == ContextMenuMode.CUSTOM_TAB) {
+                    boolean displayedAction = false;
                     for (CustomContentAction action : mCustomContentActions) {
                         if (action.getTargetType() == CustomTabsIntent.CONTENT_TARGET_TYPE_LINK) {
+                            displayedAction = true;
                             mCustomActionMap.put(nextCustomMenuItemId, action);
                             linkGroup.add(createCustomListItem(action, nextCustomMenuItemId));
                             nextCustomMenuItemId++;
                         }
+                    }
+                    if (displayedAction) {
+                        RecordHistogram.recordEnumeratedHistogram(
+                                UMA_CONTEXTUAL_CUSTOM_ACTION_TYPE_DISPLAYED,
+                                ContextualCustomActionType.LINK,
+                                ContextualCustomActionType.COUNT);
                     }
                 }
                 if (!mParams.isImage()
@@ -561,12 +571,20 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
 
             if (ChromeFeatureList.sCctContextualMenuItems.isEnabled()
                     && mMode == ContextMenuMode.CUSTOM_TAB) {
+                boolean displayedAction = false;
                 for (CustomContentAction action : mCustomContentActions) {
                     if (action.getTargetType() == CustomTabsIntent.CONTENT_TARGET_TYPE_IMAGE) {
+                        displayedAction = true;
                         mCustomActionMap.put(nextCustomMenuItemId, action);
                         imageGroup.add(createCustomListItem(action, nextCustomMenuItemId));
                         nextCustomMenuItemId++;
                     }
+                }
+                if (displayedAction) {
+                    RecordHistogram.recordEnumeratedHistogram(
+                            UMA_CONTEXTUAL_CUSTOM_ACTION_TYPE_DISPLAYED,
+                            ContextualCustomActionType.IMAGE,
+                            ContextualCustomActionType.COUNT);
                 }
             }
 
@@ -1490,6 +1508,10 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
 
     public static int getCustomMenuItemIdStartForTesting() {
         return CUSTOM_MENU_ITEM_ID_START;
+    }
+
+    public static String getCustomActionTypeDisplayedHistogramForTesting() {
+        return UMA_CONTEXTUAL_CUSTOM_ACTION_TYPE_DISPLAYED;
     }
 
     public static String getContextualCustomActionTypeSelectedHistogramForTesting() {
--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsFeatureUsage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsFeatureUsage.java
@@ -22,7 +22,7 @@ public class CustomTabsFeatureUsage {
     public static final String CUSTOM_TABS_FEATURE_USAGE_HISTOGRAM = "CustomTabs.FeatureUsage";
 
     // NOTE: This must be kept in sync with the definition |CustomTabsFeatureUsed|
-    // in tools/metrics/histograms/enums.xml.
+    // in tools/metrics/histograms/metadata/custom_tabs/enums.xml.
     // LINT.IfChange(CustomTabsFeature)
     @IntDef({
         CustomTabsFeature.CTF_SESSIONS,
@@ -89,6 +89,7 @@ public class CustomTabsFeatureUsage {
         CustomTabsFeature.EXTRA_OPEN_IN_BROWSER_STATE,
         CustomTabsFeature.EXTRA_LAUNCH_HANDLER,
         CustomTabsFeature.EXTRA_FILE_HANDLERS,
+        CustomTabsFeature.EXTRA_CUSTOM_CONTENT_ACTIONS,
         CustomTabsFeature.COUNT
     })
     @Retention(RetentionPolicy.SOURCE)
@@ -160,9 +161,10 @@ public class CustomTabsFeatureUsage {
         int EXTRA_OPEN_IN_BROWSER_STATE = 63;
         int EXTRA_LAUNCH_HANDLER = 64;
         int EXTRA_FILE_HANDLERS = 65;
+        int EXTRA_CUSTOM_CONTENT_ACTIONS = 66;
 
         /** Total count of entries. */
-        int COUNT = 66;
+        int COUNT = 67;
     }
 
     // LINT.ThenChange(/tools/metrics/histograms/metadata/custom_tabs/enums.xml:CustomTabsFeatureUsed)
