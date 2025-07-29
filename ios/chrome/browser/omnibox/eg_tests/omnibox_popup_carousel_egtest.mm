8c0aba39522697c935dcdcb68b42d8f8e67d38b9
Federica Germinario <fedegermi@google.com>
2025-07-29 04:39:31 -0700

    [ios] Fix test OmniboxPopupCarouselTestCase/* on iOS 26
    This test is failing because some idling resources are busy.
    
    Fixed: 434843402
    Change-Id: I2b3e5528a362de19937808a2c786ab7902726eb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797230
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493442}

--- ios/chrome/browser/omnibox/eg_tests/omnibox_popup_carousel_egtest.mm
+++ ios/chrome/browser/omnibox/eg_tests/omnibox_popup_carousel_egtest.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/chrome/test/scoped_eg_synchronization_disabler.h"
 #import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "net/test/embedded_test_server/embedded_test_server.h"
@@ -283,6 +284,11 @@ id<GREYMatcher> CarouselMatcher() {
 /// Long press on `tile` and select delete in the context menu.
 - (void)deleteMostVisitedTile:(id<GREYMatcher>)tile {
   [self longPressMostVisitedTile:tile];
+
+#if TARGET_OS_SIMULATOR
+  // Synchronization off due to an infinite spinner.
+  ScopedSynchronizationDisabler disabler;
+#endif
   // Tap on remove.
   [[EarlGrey selectElementWithMatcher:
                  chrome_test_util::ContextMenuItemWithAccessibilityLabelId(
