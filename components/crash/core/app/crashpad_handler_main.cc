0b6fc2dcd734364f21c0ce56295f02e0a27acc94
Mark Mentovai <mark@chromium.org>
2025-07-29 04:07:15 -0700

    crash: include command output in minidumps to debug a net problem
    When the CHECK tracked by bug https://crbug.com/40064248 trips, a magic
    crash key (net-crbug_40064248) is set, which chrome_crashpad_handler
    will act on by recording a special CommandOutputUserStream in the
    minidump. This stream can contain the output of multiple commands. In
    this case, the commands that are executed are system_profiler, ifconfig,
    systemextensionsctl, and lsof.
    
    This is intended to be temporary, and will only be used to collect
    information about systems that are susceptible to the bug, to determine
    whether they have an unusual configuration in common.
    
    https://chromium-review.googlesource.com/c/6791935 implements a decoder
    for the new temporary command output stream (type 0x4b6b0005).
    
    Bug: 40064248
    Change-Id: Ie6c0d921228482b6d3f27216fa949ba8a3fef9f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788541
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493431}

--- components/crash/core/app/crashpad_handler_main.cc
+++ components/crash/core/app/crashpad_handler_main.cc
@@ -15,6 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/stability_report/user_stream_data_source_posix.h"
 #endif
 
+#if BUILDFLAG(IS_MAC)
+// TODO(crbug.com/40064248): Remove this once sufficient information is
+// collected.
+#include "components/crash/core/app/command_output_user_stream.h"
+#endif
+
 #if BUILDFLAG(ENABLE_GWP_ASAN)
 #include "components/gwp_asan/crash_handler/crash_handler.h"  // nogncheck
 #endif
@@ -37,6 +43,13 @@ __attribute__((visibility("default"), used)) int CrashpadHandlerMain(
       std::make_unique<stability_report::UserStreamDataSourcePosix>());
 #endif
 
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/40064248): Remove this once sufficient information is
+  // collected.
+  user_stream_data_sources.push_back(
+      std::make_unique<crash_reporter::CommandOutputUserStream>());
+#endif
+
 #if BUILDFLAG(ENABLE_GWP_ASAN)
   user_stream_data_sources.push_back(
       std::make_unique<gwp_asan::UserStreamDataSource>());
@@ -46,8 +59,8 @@ __attribute__((visibility("default"), used)) int CrashpadHandlerMain(
   user_stream_data_sources.push_back(
       std::make_unique<allocation_recorder::crash_handler::
                            AllocationRecorderStreamDataSource>(
-          base::MakeRefCounted<allocation_recorder::crash_handler::
-                                   AllocationRecorderHolder>(),
+          base::MakeRefCounted<
+              allocation_recorder::crash_handler::AllocationRecorderHolder>(),
           base::MakeRefCounted<
               allocation_recorder::crash_handler::StreamDataSourceFactory>()));
 #endif
