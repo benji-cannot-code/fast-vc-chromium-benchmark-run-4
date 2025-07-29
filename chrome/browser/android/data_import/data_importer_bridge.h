4064844a5469d20493ca87d2fc35a7371e7f0f61
Marc Treib <treib@chromium.org>
2025-07-29 08:19:29 -0700

    DataImporterService: Plumb callbacks through the native bridge
    This CL extends DataImporterBridge to take completion callbacks, pass
    them on to the native side, and call them once the import completes.
    Also DataImporterServiceImpl now waits for this completion signal before
    reporting that an importItem() call is done.
    
    Still TBD is reporting back the actual import result (success or
    failure) - for now, "success" is always reported.
    
    Bug: 430254294
    Change-Id: I8d1b8f63bbe749293283b0c504a5402634ccf472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785378
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493553}

--- chrome/browser/android/data_import/data_importer_bridge.h
+++ chrome/browser/android/data_import/data_importer_bridge.h
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/android/jni_android.h"
+#include "base/android/scoped_java_ref.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "components/user_data_importer/content/stable_portability_data_importer.h"
 
 class Profile;
@@ -24,14 +26,27 @@ class DataImporterBridge {
 
   void Destroy(JNIEnv* env);
 
-  void ImportBookmarks(JNIEnv* env, jint owned_fd);
+  void ImportBookmarks(JNIEnv* env,
+                       jint owned_fd,
+                       const base::android::JavaRef<jobject>& j_callback);
 
-  void ImportReadingList(JNIEnv* env, jint owned_fd);
+  void ImportReadingList(JNIEnv* env,
+                         jint owned_fd,
+                         const base::android::JavaRef<jobject>& j_callback);
 
  private:
+  void ImportBookmarksDone(base::android::ScopedJavaGlobalRef<jobject> callback,
+                           int count);
+
+  void ImportReadingListDone(
+      base::android::ScopedJavaGlobalRef<jobject> callback,
+      int count);
+
   raw_ptr<Profile> profile_;
 
   std::unique_ptr<user_data_importer::StablePortabilityDataImporter> importer_;
+
+  base::WeakPtrFactory<DataImporterBridge> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_ANDROID_DATA_IMPORT_DATA_IMPORTER_BRIDGE_H_
