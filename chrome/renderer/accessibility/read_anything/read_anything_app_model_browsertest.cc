2ae78cb24e327f8f9ade129c57cb4e492d1fcb85
Shari Trewin <trewin@google.com>
2025-07-29 11:49:05 -0700

    [Reading mode] Upgrade to new Atkinson Hyperlegible Next font
    The Atkinson Hyperlegible font is designed for readability with low
    visual acuity.  A new version of the font, Atkinson Hyperlegible Next,
    accommodates many more languages, expanding the set of font options for
    Reading mode for documents in more languages.
    
    This CL upgrades the version of Atkinson Hyperlegible without changing
    the logging for this option.
    
    
    Fixed: 412332398
    Change-Id: I535d439154bcae62e7bf9d6ea7c38a03bcdbb94a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6795488
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Shari Trewin <trewin@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1493703}

--- chrome/renderer/accessibility/read_anything/read_anything_app_model_browsertest.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_model_browsertest.cc
@@ -1648,7 +1648,7 @@ TEST_F(ReadAnythingAppModelTest,
   EXPECT_THAT(model().supported_fonts(),
               ElementsAre("Poppins", "Sans-serif", "Serif", "Comic Neue",
                           "Lexend Deca", "EB Garamond", "STIX Two Text",
-                          "Andika", "Atkinson Hyperlegible"));
+                          "Andika", "Atkinson Hyperlegible Next"));
 }
 
 TEST_F(ReadAnythingAppModelTest,
@@ -1658,7 +1658,7 @@ TEST_F(ReadAnythingAppModelTest,
   EXPECT_THAT(model().supported_fonts(),
               ElementsAre("Poppins", "Sans-serif", "Serif", "Comic Neue",
                           "Lexend Deca", "EB Garamond", "STIX Two Text",
-                          "Andika", "Atkinson Hyperlegible"));
+                          "Andika", "Atkinson Hyperlegible Next"));
 
   // Bulgarian
   model().SetBaseLanguageCode("bg");
@@ -1670,6 +1670,11 @@ TEST_F(ReadAnythingAppModelTest,
   model().SetBaseLanguageCode("hi");
   EXPECT_THAT(model().supported_fonts(),
               ElementsAre("Poppins", "Sans-serif", "Serif"));
+
+  // Welsh
+  model().SetBaseLanguageCode("cy");
+  EXPECT_THAT(model().supported_fonts(),
+              ElementsAre("Sans-serif", "Serif", "Atkinson Hyperlegible Next"));
 }
 
 TEST_F(ReadAnythingAppModelTest, PdfEvents_SetRequiresDistillation) {
