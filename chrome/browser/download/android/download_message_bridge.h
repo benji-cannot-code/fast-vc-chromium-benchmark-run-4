921af6d49ab779a24100f2378ac81be8690d2fc4
Ho Cheung <hocheung@chromium.org>
2025-07-28 21:47:14 -0700

    [download] Refactor using base::android::ToJniCallback
    This removes manual heap allocation and JNI boilerplate by switching
    from the old pattern of allocating callbacks to the heap and passing
    raw pointers to Java, to using the new base::android::ToJniCallback
    bridge method.
    
    This shouldn't change the logic.
    
    Bug: 394337126
    Change-Id: Ib2b6869550c50babc904aa07258464fac4556a86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787110
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493279}

--- chrome/browser/download/android/download_message_bridge.h
+++ chrome/browser/download/android/download_message_bridge.h
@@ -6,10 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DOWNLOAD_ANDROID_DOWNLOAD_MESSAGE_BRIDGE_H_
 #define CHROME_BROWSER_DOWNLOAD_ANDROID_DOWNLOAD_MESSAGE_BRIDGE_H_
 
-#include "base/android/jni_android.h"
-#include "base/android/scoped_java_ref.h"
-#include "chrome/browser/download/android/download_callback_validator.h"
-#include "chrome/browser/download/download_target_determiner_delegate.h"
+#include "base/functional/callback.h"
 #include "components/download/public/common/download_item.h"
 
 namespace content {
@@ -31,15 +28,6 @@ class DownloadMessageBridge : public download::DownloadItem::Observer {
   void ShowIncognitoDownloadMessage(DownloadMessageRequestCallback callback);
   virtual void ShowUnsupportedDownloadMessage(
       content::WebContents* web_contents);
-
-  // Called from Java via JNI.
-  void OnConfirmed(JNIEnv* env, jlong callback_id, jboolean accepted);
-
- private:
-  // Validator for all JNI callbacks.
-  DownloadCallbackValidator validator_;
-  // The corresponding java object.
-  base::android::ScopedJavaGlobalRef<jobject> java_object_;
 };
 
 #endif  // CHROME_BROWSER_DOWNLOAD_ANDROID_DOWNLOAD_MESSAGE_BRIDGE_H_
