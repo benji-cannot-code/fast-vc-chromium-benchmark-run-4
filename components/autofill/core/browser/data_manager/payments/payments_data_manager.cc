cff280012cc747f3faff7897c575544130eb76e9
Nidhi Jaju <nidhijaju@chromium.org>
2025-07-28 19:21:02 -0700

    Revert "[BNPL] Exclude externally linked issuer from settings page"
    This reverts commit 62ead38582d16fca617436e3d862fec86d78bacc.
    
    Reason for revert: In a CL chain that's suspicious for causing test failures
    See https://chromium-review.googlesource.com/c/chromium/src/+/6796329
    
    Bug: 424259928, b:433264714
    Original change's description:
    > [BNPL] Exclude externally linked issuer from settings page
    >
    > This CL adds logic to exclude externally linked Klarna issuer to be
    > shown on the settings page.
    >
    > Design Doc: go/chrome_bnpl_klarna_desktop_design
    >
    > Bug: 424259928, b:433264714
    > Change-Id: Ifd127c95fbf4183ccb9f4251645aa9062e65ff6f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779072
    > Reviewed-by: Vinny Persky <vinnypersky@google.com>
    > Commit-Queue: Yishui Liu <yishuil@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1493029}
    
    Bug: 424259928, b:433264714
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ic22277b10682f6bb49caf84007b8a5d7e87a84cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6796528
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493243}

--- components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
+++ components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
@@ -1010,10 +1010,6 @@ void PaymentsDataManager::SetPrefService(PrefService* pref_service) {
         // BUILDFLAG(IS_CHROMEOS)
 }
 
-bool PaymentsDataManager::IsAutofillBnplPrefEnabled() const {
-  return prefs::IsAutofillBnplEnabled(pref_service_);
-}
-
 void PaymentsDataManager::NotifyObservers() {
   if (!HasPendingPaymentQueries()) {
     for (Observer& o : observers_) {
@@ -1060,6 +1056,10 @@ bool PaymentsDataManager::IsCardBenefitsPrefEnabled() const {
 bool PaymentsDataManager::IsCardBenefitsSyncEnabled() const {
   return base::FeatureList::IsEnabled(
       features::kAutofillEnableCardBenefitsSync);
+}
+
+bool PaymentsDataManager::IsAutofillBnplPrefEnabled() const {
+  return prefs::IsAutofillBnplEnabled(pref_service_);
 }
 
 bool PaymentsDataManager::IsAutofillPaymentMethodsEnabled() const {
