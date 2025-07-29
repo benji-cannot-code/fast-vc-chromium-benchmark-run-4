bfecda82f00f2b13360f31cbb0df2351d1e4ae74
Weipeng Li <weipengli@microsoft.com>
2025-07-29 06:05:56 -0700

    [iOS] Add kDownloadList feature flag with UIType parameter
    This CL introduces a feature flag for the in-app download list on iOS, allowing UI customization via a UIType parameter.
    
    It implements the design discussed in the following doc:
    https://docs.google.com/document/d/1IqH9PqZnb2562KhjRNhx5QNlaunVzrX5cor4uVHxwrk/edit?usp=sharing
    
    Bug: none
    Change-Id: I65de5de5861fcd7c34b75abfeb644db262b9f702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786772
    Commit-Queue: weipeng li <weipengli@microsoft.com>
    Reviewed-by: Ran Sun <collinssun@microsoft.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493475}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -273,6 +273,9 @@ extern const char kDownloadAutoDeletionDescription[];
 extern const char kDownloadedPDFOpeningName[];
 extern const char kDownloadedPDFOpeningDescription[];
 
+extern const char kDownloadListName[];
+extern const char kDownloadListDescription[];
+
 extern const char kEnableCompromisedPasswordsMutingName[];
 extern const char kEnableCompromisedPasswordsMutingDescription[];
 
