7c2e8cad11bd16f2b3ee3aa50a3fd9d1a1996c34
Kent Tamura <tkent@chromium.org>
2025-07-28 21:10:10 -0700

    Move ::LChar to "blink" namespace
    We don't move `::UChar` and `::UChar32` because of ICU compatibility.
    This CL has no behavior changes.
    
    Bug: 422768753
    Change-Id: Id4cfd335ddec405dde523d3b539adc0acf4979d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6791126
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493269}

--- third_party/blink/renderer/core/html/parser/literal_buffer.h
+++ third_party/blink/renderer/core/html/parser/literal_buffer.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // UCharLiteralBuffer.
 template <typename T, blink::wtf_size_t kInlineSize>
 class LiteralBufferBase {
-  static_assert(std::is_same<LChar, T>::value || std::is_same<UChar, T>::value,
+  static_assert(std::is_same_v<blink::LChar, T> || std::is_same_v<UChar, T>,
                 "T must be a character type");
 
  public:
@@ -183,7 +183,7 @@ class LiteralBufferBase {
 };
 
 template <blink::wtf_size_t kInlineSize>
-class LCharLiteralBuffer : public LiteralBufferBase<LChar, kInlineSize> {
+class LCharLiteralBuffer : public LiteralBufferBase<blink::LChar, kInlineSize> {
  public:
   LCharLiteralBuffer() = default;
   LCharLiteralBuffer(const LCharLiteralBuffer& other) { *this = other; }
@@ -208,7 +208,7 @@ class LCharLiteralBuffer : public LiteralBufferBase<LChar, kInlineSize> {
   // Clear without freeing any storage.
   ALWAYS_INLINE void clear() { this->ClearImpl(); }
 
-  ALWAYS_INLINE void AddChar(LChar val) { this->AddCharImpl(val); }
+  ALWAYS_INLINE void AddChar(blink::LChar val) { this->AddCharImpl(val); }
 
   blink::String AsString() const { return blink::String(*this); }
 };
