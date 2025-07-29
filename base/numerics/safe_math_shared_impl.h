bb962386feee82d5c9bfb1eb75aea8d7a4f32364
郑苏波 (Super Zheng) <superzheng@tencent.com>
2025-07-29 14:21:56 -0700

    Keep base/numerics isolated.
    It shouldn't implicitly depend on build/build_config.h.
    
    Bug: none
    Change-Id: Id1a95eb737ef55f8146a83e7b789be86fb901bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6790591
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493791}

--- base/numerics/safe_math_shared_impl.h
+++ base/numerics/safe_math_shared_impl.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/numerics/safe_conversions.h"
-#include "build/build_config.h"
 
 #if defined(__asmjs__) || defined(__wasm__)
 // Optimized safe math instructions are incompatible with asmjs.
