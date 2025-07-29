e87be069570e9e6be2b3e69c09a3cdf6ae860cf2
Takuto Ikuta <tikuta@google.com>
2025-07-29 04:44:52 -0700

    components: Add a missing include
    Bug: 40263312
    Change-Id: Ibe090a9354613fa4f0dc94162930fbe572e44040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6796838
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493444}

--- components/password_manager/ios/test_helpers.h
+++ components/password_manager/ios/test_helpers.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_IOS_TEST_HELPERS_H_
 #define COMPONENTS_PASSWORD_MANAGER_IOS_TEST_HELPERS_H_
 
+#include <cstdint>
 #include <string>
 
 namespace autofill {
