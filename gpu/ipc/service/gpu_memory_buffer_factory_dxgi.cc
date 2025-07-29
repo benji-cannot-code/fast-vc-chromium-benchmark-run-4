6caf569ef187fa05b3e1e31e2dd4674a403cc4ed
Colin Blundell <blundell@chromium.org>
2025-07-28 23:33:58 -0700

    [//gpu] Remove GpuMemoryBufferFactory::DestroyGpuMemoryBuffer()
    As this CL shows, the only remaining caller of this method calls it
    immediately after creating the handle, making the caching behavior in
    GpuMemoryBufferFactoryNativePixmap a no-op: it adds a reference to the
    NativePixmap in its cache and then immediately drops it.
    
    Bug: 404905709
    Change-Id: I53c9469b99f29c995474a83c6bab8305a10b1dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788165
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493309}

--- gpu/ipc/service/gpu_memory_buffer_factory_dxgi.cc
+++ gpu/ipc/service/gpu_memory_buffer_factory_dxgi.cc
@@ -219,10 +219,6 @@ gfx::GpuMemoryBufferHandle GpuMemoryBufferFactoryDXGI::CreateGpuMemoryBuffer(
   return handle;
 }
 
-void GpuMemoryBufferFactoryDXGI::DestroyGpuMemoryBuffer(
-    gfx::GpuMemoryBufferId id,
-    int client_id) {}
-
 bool GpuMemoryBufferFactoryDXGI::FillSharedMemoryRegionWithBufferContents(
     gfx::GpuMemoryBufferHandle buffer_handle,
     base::UnsafeSharedMemoryRegion shared_memory) {
--- gpu/ipc/service/gpu_memory_buffer_factory_io_surface.cc
+++ gpu/ipc/service/gpu_memory_buffer_factory_io_surface.cc
@@ -52,10 +52,6 @@ GpuMemoryBufferFactoryIOSurface::CreateGpuMemoryBuffer(
   return gfx::GpuMemoryBufferHandle(std::move(io_surface));
 }
 
-void GpuMemoryBufferFactoryIOSurface::DestroyGpuMemoryBuffer(
-    gfx::GpuMemoryBufferId id,
-    int client_id) {}
-
 bool GpuMemoryBufferFactoryIOSurface::FillSharedMemoryRegionWithBufferContents(
     gfx::GpuMemoryBufferHandle buffer_handle,
     base::UnsafeSharedMemoryRegion shared_memory) {
