3fbc90819e171eadcb18cb5ce7f4b9015a301ba9
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-07-29 14:07:21 -0700

    Update OnAnalysisConnectorWarningBypassed with ContentAnalysisInfo
    This CL refactors OnAnalysisConnectorWarningBypassed similar to how
    MaybeReportDeepScanningVerdict were changed in crrev.com/c/6788314. For
    download code that doesn't have access to a proper implementation of
    ContentAnalysisInfo, a new DownloadContentAreaUserProvider helper was
    added alongside the existing overrides of ContentAnalysisInfoBase.
    
    Bug: 433762782
    Change-Id: Ief8fe362517ab566e6ff7628f83032b3a5d428b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6796445
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493787}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_info.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_info.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
 #include "content/public/browser/clipboard_types.h"
 
+namespace download {
+class DownloadItem;
+}  // namespace download
+
 namespace enterprise_connectors {
 
 // Implementation of `ContentAnalysisInfoBase` for chrome/ platforms.
@@ -30,8 +34,10 @@ class ContentAnalysisInfo : public ContentAnalysisInfoBase {
   std::string GetContentAreaAccountEmail() const;
 };
 
-// Simple implementation of `ContentAnalysisInfo` meant to be used for
-// `GetContentAreaAccountEmail` only
+// Simple implementation of `ContentAnalysisInfo` meant to obtain the value
+// returned by `GetContentAreaAccountEmail`. This class shouldn't expose
+// non-static methods unless every other override required to support it are
+// implemented.
 class ContentAreaUserProvider : public ContentAnalysisInfo {
  public:
   static std::string GetUser(Profile* profile, const GURL& tab_url);
@@ -56,6 +62,36 @@ class ContentAreaUserProvider : public ContentAnalysisInfo {
 
   raw_ptr<signin::IdentityManager> im_;
   raw_ref<const GURL> tab_url_;
+};
+
+// Download-specific implementation of `ContentAnalysisInfo`. This is meant to
+// be used only for reporting, so only public fields should be called.
+class DownloadContentAreaUserProvider : public ContentAnalysisInfo {
+ public:
+  explicit DownloadContentAreaUserProvider(
+      const download::DownloadItem& download_item);
+
+  // ContentAnalysisInfo:
+  std::string url() const override;
+  const GURL& tab_url() const override;
+  signin::IdentityManager* identity_manager() const override;
+
+ private:
+  // ContentAnalysisInfo:
+  const AnalysisSettings& settings() const override;
+  int user_action_requests_count() const override;
+  std::string tab_title() const override;
+  std::string user_action_id() const override;
+  std::string email() const override;
+  ContentAnalysisRequest::Reason reason() const override;
+  google::protobuf::RepeatedPtrField<::safe_browsing::ReferrerChainEntry>
+  referrer_chain() const override;
+  google::protobuf::RepeatedPtrField<std::string> frame_url_chain()
+      const override;
+
+  GURL url_;
+  GURL tab_url_;
+  raw_ptr<signin::IdentityManager> im_;
 };
 
 }  // namespace enterprise_connectors
