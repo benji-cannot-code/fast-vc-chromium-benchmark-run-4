b380bf3204add3a9432283c66f2c31da6f2e1594
Elly <ellyjones@chromium.org>
2025-07-29 12:43:18 -0700

    crypto/sign: fix broken oneshot functions for RSA-PSS
    In https://crrev.com/c/6758127 these functions accidentally lost their
    RSA-PSS setup code, so they didn't generate or validate correct RSA-PSS
    signatures. Unfortunately the testing for them involved round-tripping
    the one-shot functions into each other, so the tests still passed.
    
    This change:
    * Adds the missing setup code to the one-shot functions
    * Adds a new set of roundtrip tests to ensure the streaming and oneshot
      APIs are mutually compatible
    
    Fixed: 434641514
    Change-Id: Ie991305d6b0c74ee2e1ab22a553c42b1e6a9e1f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792756
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493735}

--- crypto/sign.cc
+++ crypto/sign.cc
@@ -57,6 +57,14 @@ const EVP_MD* DigestForSignatureKind(SignatureKind kind) {
   }
 }
 
+void ConfigurePkeyCtx(EVP_PKEY_CTX* pkctx, SignatureKind kind) {
+  if (kind == RSA_PSS_SHA256) {
+    CHECK(EVP_PKEY_CTX_set_rsa_padding(pkctx, RSA_PKCS1_PSS_PADDING));
+    CHECK(EVP_PKEY_CTX_set_rsa_mgf1_md(pkctx, DigestForSignatureKind(kind)));
+    CHECK(EVP_PKEY_CTX_set_rsa_pss_saltlen(pkctx, RSA_PSS_SALTLEN_DIGEST));
+  }
+}
+
 }  // namespace
 
 std::vector<uint8_t> Sign(SignatureKind kind,
@@ -69,6 +77,7 @@ std::vector<uint8_t> Sign(SignatureKind kind,
   bssl::UniquePtr<EVP_MD_CTX> context(EVP_MD_CTX_new());
   CHECK(EVP_DigestSignInit(context.get(), &pkctx, md, nullptr,
                            const_cast<EVP_PKEY*>(key.key())));
+  ConfigurePkeyCtx(pkctx, kind);
 
   size_t len = 0;
   CHECK(EVP_DigestSign(context.get(), nullptr, &len, data.data(), data.size()));
@@ -90,6 +99,7 @@ bool Verify(SignatureKind kind,
   bssl::UniquePtr<EVP_MD_CTX> context(EVP_MD_CTX_new());
   CHECK(EVP_DigestVerifyInit(context.get(), &pkctx, md, nullptr,
                              const_cast<EVP_PKEY*>(key.key())));
+  ConfigurePkeyCtx(pkctx, kind);
 
   return EVP_DigestVerify(context.get(), signature.data(), signature.size(),
                           data.data(), data.size()) == 1;
@@ -104,13 +114,7 @@ Signer::Signer(SignatureKind kind, crypto::keypair::PrivateKey key)
   EVP_PKEY_CTX* pkctx;
   CHECK(
       EVP_DigestSignInit(sign_context_.get(), &pkctx, md, nullptr, key.key()));
-
-  if (kind == RSA_PSS_SHA256) {
-    CHECK(EVP_PKEY_CTX_set_rsa_padding(pkctx, RSA_PKCS1_PSS_PADDING));
-    CHECK(EVP_PKEY_CTX_set_rsa_mgf1_md(pkctx, md));
-    // -1 here means "use digest's length"
-    CHECK(EVP_PKEY_CTX_set_rsa_pss_saltlen(pkctx, -1));
-  }
+  ConfigurePkeyCtx(pkctx, kind);
 }
 Signer::~Signer() = default;
 
@@ -144,13 +148,7 @@ Verifier::Verifier(SignatureKind kind,
   const EVP_MD* const md = DigestForSignatureKind(kind);
   CHECK(EVP_DigestVerifyInit(verify_context_.get(), &pkctx, md, nullptr,
                              key.key()));
-
-  if (kind == RSA_PSS_SHA256) {
-    CHECK(EVP_PKEY_CTX_set_rsa_padding(pkctx, RSA_PKCS1_PSS_PADDING));
-    CHECK(EVP_PKEY_CTX_set_rsa_mgf1_md(pkctx, md));
-    // -1 here means "use digest's length"
-    CHECK(EVP_PKEY_CTX_set_rsa_pss_saltlen(pkctx, -1));
-  }
+  ConfigurePkeyCtx(pkctx, kind);
 }
 Verifier::~Verifier() = default;
 
