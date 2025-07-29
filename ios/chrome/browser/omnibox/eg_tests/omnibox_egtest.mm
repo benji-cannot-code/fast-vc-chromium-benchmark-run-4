6900b3b5b807ace3a93c86b08fe2f7f426183d10
Asami Doi <asamidoi@chromium.org>
2025-07-29 02:54:17 -0700

    [ios] Fix testOmniboxMenuPasteTextToSearch on iOS 26
    On some devices on iOS 26, the menu opened by the long pressed omnibox
    has the forward button instead of full menu items in one line. See
    screenshot: https://screenshot.googleplex.com/6TPmkTQdcbPSEtY.png
    
    Tap the forward button if it exists to display the search text button.
    
    Fixed: 434645859
    Change-Id: I25c12285ba1d26c1bc42dbf6b714a906faf9e4bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6793370
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1493393}

--- ios/chrome/browser/omnibox/eg_tests/omnibox_egtest.mm
+++ ios/chrome/browser/omnibox/eg_tests/omnibox_egtest.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "build/build_config.h"
 #import "components/feature_engagement/public/feature_constants.h"
 #import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/browser_container/ui_bundled/edit_menu_app_interface.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/ntp_home_constant.h"
 #import "ios/chrome/browser/omnibox/eg_tests/omnibox_app_interface.h"
 #import "ios/chrome/browser/omnibox/eg_tests/omnibox_earl_grey.h"
@@ -359,16 +360,28 @@ void FocusFakebox() {
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
       performAction:grey_longPress()];
 
+  NSError* error = nil;
+  [[EarlGrey
+      selectElementWithMatcher:[EditMenuAppInterface editMenuNextButtonMatcher]]
+      assertWithMatcher:grey_sufficientlyVisible()
+                  error:&error];
+  if (error == nil) {
+    // Tap the forward button if it's visible. It depends on the device size.
+    [[EarlGrey selectElementWithMatcher:[EditMenuAppInterface
+                                            editMenuNextButtonMatcher]]
+        performAction:grey_tap()];
+  }
+
   // Wait for UIMenuController to appear or timeout after 2 seconds.
   GREYCondition* SearchTextButtonIsDisplayed = [GREYCondition
       conditionWithName:@"Search Copied Text button display condition"
                   block:^BOOL {
-                    NSError* error = nil;
+                    NSError* e = nil;
                     [[EarlGrey
                         selectElementWithMatcher:SearchCopiedTextButton()]
                         assertWithMatcher:grey_notNil()
-                                    error:&error];
-                    return error == nil;
+                                    error:&e];
+                    return e == nil;
                   }];
   GREYAssertTrue([SearchTextButtonIsDisplayed
                      waitWithTimeout:kWaitForUIElementTimeout.InSecondsF()],
