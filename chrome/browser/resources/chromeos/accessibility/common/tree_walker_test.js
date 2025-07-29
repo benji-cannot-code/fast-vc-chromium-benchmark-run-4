1ec904f433784c5f22c065e2139c1e1186460902
Xiyuan Xia <xiyuan@chromium.org>
2025-07-29 14:43:13 -0700

    Reland "Reland "a11y-common-mv3: Add to field trial config""
    This is a reland of commit 5fc1c6cd81b017d484ba624968fd1baf9e9c6c80
    
    Changes:
    Fix typo on MV2 code path (e.g. linux-chromeos-chrome bot).
    
    Original change's description:
    > Reland "a11y-common-mv3: Add to field trial config"
    >
    > This is a reland of commit b3f7e1c2119d8db4db053ca2aa244d95e80d10a1
    >
    > Changes:
    > AccessibilityExtensionAutomationTreeWalkerTest.Backward/Forward
    > failure on MSAN is because they have an iteration that runs more
    > than 30s. Long running tasks would cause the service worker to
    > be considered as stalled and gets killed. This is fixed by calling
    > an API and using `await` to yield control to service worker message
    > loop.
    >
    > Original change's description:
    > > a11y-common-mv3: Add to field trial config
    > >
    > > - Add AccessibilityManifestV3AccessibilityCommon fieldtrial config
    > >   to prepare for a finch roll out;
    > > - Skip AccessibilityExtensionLocalStorageTest.Migration test in service
    > >   worker because localStorage is not available;
    > >
    > > AX-Relnotes: n/a.
    > > Bug: 388867838
    > > Change-Id: I90b2fb51cefbb1dedb529586e1b89ea58cf24d10
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719634
    > > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1490225}
    >
    > Cq-Include-Trybots: luci.chromium.try:linux_chromium_chromeos_msan_rel_ng
    > Bug: 388867838
    > Change-Id: I3936cd9a37cdba0ab0bfc374d0ff3bd30a51f220
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781374
    > Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1491691}
    
    Bug: 388867838
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome;luci.chromium.try:linux_chromium_chromeos_msan_rel_ng
    Change-Id: Ie35bdf29e108260c31ce4b2244e2bd21cf3910c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788258
    Auto-Submit: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493824}

--- chrome/browser/resources/chromeos/accessibility/common/tree_walker_test.js
+++ chrome/browser/resources/chromeos/accessibility/common/tree_walker_test.js
@@ -61,7 +61,7 @@ AccessibilityExtensionAutomationTreeWalkerTest =
 TEST_F(
     'AccessibilityExtensionAutomationTreeWalkerTest', 'MAYBE_Forward',
     function() {
-      chrome.automation.getDesktop(this.newCallback(function(d) {
+      chrome.automation.getDesktop(this.newCallback(async function(d) {
         const resultList = [];
         this.flattenTree(d, resultList);
         let it = new AutomationTreeWalker(d, 'forward');
@@ -71,6 +71,10 @@ TEST_F(
         assertEquals(null, it.next().node);
 
         for (let j = 0; j < resultList.length; j++) {
+          // This is needed because this iteration could take more than 30s on
+          // MSAN bots.
+          await keepServiceWorkerAlive();
+
           it = new AutomationTreeWalker(resultList[j], 'forward');
           const start = it.node;
           let cur = it.next().node;
@@ -92,7 +96,7 @@ TEST_F(
 TEST_F(
     'AccessibilityExtensionAutomationTreeWalkerTest', 'MAYBE_Backward',
     function() {
-      chrome.automation.getDesktop(this.newCallback(function(d) {
+      chrome.automation.getDesktop(this.newCallback(async function(d) {
         const resultList = [];
         this.flattenTree(d, resultList);
         let it = new AutomationTreeWalker(
@@ -102,6 +106,10 @@ TEST_F(
         }
 
         for (let j = resultList.length - 1; j >= 0; j--) {
+          // This is needed because this iteration could take more than 30s on
+          // MSAN bots.
+          await keepServiceWorkerAlive();
+
           it = new AutomationTreeWalker(resultList[j], 'backward');
           const start = it.node;
           let cur = it.next().node;
