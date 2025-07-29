b5cd811291ffe259e9b4bcddf904a451da4431da
Christine Hollingsworth <christinesm@chromium.org>
2025-07-29 06:54:41 -0700

    macOS 26 - Disable `KeepAliveURLLoader...` tests with unexpected timeout
    Temporarily disables tests in KeepAliveURLLoaderServiceRetryTest
    unittests due to unexpected timeout. Based on local testing, this
    is due to flakiness in calls to
    `task_environment()->FastForwardBy()` where the time fast-
    forwarded is >= 24h.
    
    Bug: 434660312
    Change-Id: Ia1a44751766fa2044b19a6c794971253e8eab110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792802
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493498}

--- content/browser/loader/keep_alive_url_loader_service_unittest.cc
+++ content/browser/loader/keep_alive_url_loader_service_unittest.cc
@@ -52,6 +52,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/loader/fetch_later.mojom.h"
 #include "url/gurl.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace content {
 namespace {
 
@@ -1899,6 +1903,14 @@ TEST_F(KeepAliveURLLoaderServiceRetryTest, NoRetryOptionsWillNotBeRetried) {
 
 // Test that failing a request to non-HTTPs will not be retried.
 TEST_F(KeepAliveURLLoaderServiceRetryTest, NonHTTPSWillNotBeRetried) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
+
   FakeRemoteURLLoaderFactory renderer_loader_factory;
   MockReceiverURLLoaderClient renderer_loader_client;
   BindKeepAliveURLLoaderFactory(renderer_loader_factory);
@@ -1935,6 +1947,14 @@ TEST_F(KeepAliveURLLoaderServiceRetryTest, NonHTTPSWillNotBeRetried) {
 // retry options doesn't specify it wants to retry non-idempotent failures.
 TEST_F(KeepAliveURLLoaderServiceRetryTest,
        POSTWillNotBeRetriedUnlessRequested) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
+
   FakeRemoteURLLoaderFactory renderer_loader_factory;
   MockReceiverURLLoaderClient renderer_loader_client;
   BindKeepAliveURLLoaderFactory(renderer_loader_factory);
@@ -1968,9 +1988,16 @@ TEST_F(KeepAliveURLLoaderServiceRetryTest,
   EXPECT_FALSE(loader.get());
 }
 
-// Test that failing all the attmpte
+// Test that fail all attempts will forward the last error.
 TEST_F(KeepAliveURLLoaderServiceRetryTest,
        FailedMaxAttemptWillForwardLastError) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   FakeRemoteURLLoaderFactory renderer_loader_factory;
   MockReceiverURLLoaderClient renderer_loader_client;
   BindKeepAliveURLLoaderFactory(renderer_loader_factory);
@@ -2046,6 +2073,13 @@ TEST_F(KeepAliveURLLoaderServiceRetryTest, ReceivedResponseWillNotBeRetried) {
 // Test that hitting the redirect limit won't trigger a retry.
 TEST_F(KeepAliveURLLoaderServiceRetryTest,
        ExceededRedirectLimitWillNotBeRetried) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   FakeRemoteURLLoaderFactory renderer_loader_factory;
   MockReceiverURLLoaderClient renderer_loader_client;
   BindKeepAliveURLLoaderFactory(renderer_loader_factory);
