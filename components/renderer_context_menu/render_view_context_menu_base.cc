7a62acf6dc631d14cb56d60d3954165e78e8fba0
Kim-Anh Tran <kimanh@chromium.org>
2025-07-30 10:11:14 -0700

    [DevTools] Enable new badges on context submenu items
    On top of normal menu items, we also want to be able to show the
    new badge on submenu items.
    
    Bug: 427371633
    Change-Id: I37934c0d90132753ed238e6a6f19d8dc2b170f47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797876
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494308}

--- components/renderer_context_menu/render_view_context_menu_base.cc
+++ components/renderer_context_menu/render_view_context_menu_base.cc
@@ -158,6 +158,11 @@ void RenderViewContextMenuBase::AddCustomItemsToMenu(
             RenderViewContextMenuBase::ConvertToContentCustomCommandId(
                 item->action),
             item->label, submenu);
+        if (!item->feature_name.empty()) {
+          menu_model->SetIsNewFeatureAt(
+              menu_model->GetItemCount() - 1,
+              GetIsNewFeatureAtValue(base::UTF16ToUTF8(item->feature_name)));
+        }
         break;
       }
       default:
