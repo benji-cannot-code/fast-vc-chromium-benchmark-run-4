07603fec60611450a5d8730df8558232496d8f12
Gauthier Ambard <gambard@chromium.org>
2025-07-30 06:32:10 -0700

    [iOS] Disable ManageSyncSettingsTestCase tests
    The tests are failing on devices.
    
    Bug: 435139218
    Change-Id: I75dcb4f01db6d06e7f8c202c327f0f56262dcffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797816
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494146}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_egtest.mm
@@ -968,7 +968,11 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
 
 // Tests that the batch upload button description in the account settings
 // contains the correct string for reading list.
+// TODO(crbug.com/435139218): Reenable this test.
 - (void)testBulkUploadDescriptionTextForReadingList {
+#if !TARGET_OS_SIMULATOR
+  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
+#endif
   // Add local data.
   reading_list_test_utils::AddURLToReadingListWithSnackbarDismiss(
       GURL("https://example.com"), nil);
@@ -992,7 +996,11 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
 
 // Tests that the batch upload button description in the account settings
 // contains the correct string for passwords and other data type.
+// TODO(crbug.com/435139218): Reenable this test.
 - (void)testBulkUploadDescriptionTextForPasswordsAndOthers {
+#if !TARGET_OS_SIMULATOR
+  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
+#endif
   // Add local data.
   password_manager_test_utils::SavePasswordFormToProfileStore(
       @"password", @"user", @"https://example.com");
@@ -1021,7 +1029,11 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
 // - Passwords
 // - Bookmarks
 // - Reading list
+// TODO(crbug.com/435139218): Reenable this test.
 - (void)testBulkUploadPageForAllDataTypes {
+#if !TARGET_OS_SIMULATOR
+  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
+#endif
   // Add local data.
   password_manager_test_utils::SavePasswordFormToProfileStore(
       @"password", @"user", @"https://example.com");
@@ -1171,12 +1183,7 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
 // Tests that bulk upload moves the following data types to account:
 // - Passwords
 // TODO(crbug.com/407020882): Remove FLAKY_ from this test.
-#if TARGET_OS_SIMULATOR
-#define MAYBE_testBulkUploadForPasswords FLAKY_testBulkUploadForPasswords
-#else
-#define MAYBE_testBulkUploadForPasswords testBulkUploadForPasswords
-#endif
-- (void)MAYBE_testBulkUploadForPasswords {
+- (void)FLAKY_testBulkUploadForPasswords {
   // Add local data.
   password_manager_test_utils::SavePasswordFormToProfileStore(
       @"password", @"user", @"https://example.com");
@@ -1266,7 +1273,11 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
 // Tests that bulk upload moves the following data types to account:
 // - Bookmarks
 // - Reading List
+// TODO(crbug.com/435139218): Reenable this test.
 - (void)testBulkUploadForBookmarksAndReadingList {
+#if !TARGET_OS_SIMULATOR
+  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
+#endif
   // Add local data.
   password_manager_test_utils::SavePasswordFormToProfileStore(
       @"password", @"user", @"https://example.com");
@@ -1342,7 +1353,11 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
 // - Passwords
 // - Bookmarks
 // - Reading List
+// TODO(crbug.com/435139218): Reenable this test.
 - (void)testBulkUploadForAllDataTypes {
+#if !TARGET_OS_SIMULATOR
+  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
+#endif
   // Add local data.
   password_manager_test_utils::SavePasswordFormToProfileStore(
       @"password", @"user", @"https://example.com");
