84c17eabb58114035dbbeac72110064311734c2f
Tom Anderson <thomasanderson@chromium.org>
2025-07-30 07:55:08 -0700

    Remove CallMethodWithErrorCallback() in //chromeos
    R=hashimoto
    
    Bug: 41328956
    Change-Id: I53ec74eebee16436b3db3ed0ecc74993c7330455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799051
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494199}

--- chromeos/ash/components/dbus/shill/shill_client_helper.cc
+++ chromeos/ash/components/dbus/shill/shill_client_helper.cc
@@ -55,17 +55,31 @@ namespace {
 const char kInvalidResponseErrorName[] = "";  // No error name.
 const char kInvalidResponseErrorMessage[] = "Invalid response.";
 
+// Handles running appropriate error callbacks.
+void OnError(ShillClientHelper::ErrorCallback error_callback,
+             dbus::ErrorResponse* response) {
+  std::string error_name;
+  std::string error_message;
+  if (response) {
+    // Error message may contain the error message as string.
+    dbus::MessageReader reader(response);
+    error_name = response->GetErrorName();
+    reader.PopString(&error_message);
+  }
+  std::move(error_callback).Run(error_name, error_message);
+}
+
 // Note: here and below, |ref_holder| is unused in the function body. It only
 // exists so that it will be destroyed (and the reference released) with the
 // Callback object once completed.
-void OnBooleanMethodWithErrorCallback(
+void OnBooleanMethodWithErrorResponse(
     ShillClientHelper::RefHolder* ref_holder,
     ShillClientHelper::BooleanCallback callback,
     ShillClientHelper::ErrorCallback error_callback,
-    dbus::Response* response) {
+    dbus::Response* response,
+    dbus::ErrorResponse* error_response) {
   if (!response) {
-    std::move(error_callback)
-        .Run(kInvalidResponseErrorName, kInvalidResponseErrorMessage);
+    OnError(std::move(error_callback), error_response);
     return;
   }
   dbus::MessageReader reader(response);
@@ -78,14 +92,14 @@ void OnBooleanMethodWithErrorCallback(
   std::move(callback).Run(result);
 }
 
-void OnStringMethodWithErrorCallback(
+void OnStringMethodWithErrorResponse(
     ShillClientHelper::RefHolder* ref_holder,
     ShillClientHelper::StringCallback callback,
     ShillClientHelper::ErrorCallback error_callback,
-    dbus::Response* response) {
+    dbus::Response* response,
+    dbus::ErrorResponse* error_response) {
   if (!response) {
-    std::move(error_callback)
-        .Run(kInvalidResponseErrorName, kInvalidResponseErrorMessage);
+    OnError(std::move(error_callback), error_response);
     return;
   }
   dbus::MessageReader reader(response);
@@ -110,10 +124,10 @@ void OnObjectPathMethodWithoutStatus(
     ShillClientHelper::RefHolder* ref_holder,
     chromeos::ObjectPathCallback callback,
     ShillClientHelper::ErrorCallback error_callback,
-    dbus::Response* response) {
+    dbus::Response* response,
+    dbus::ErrorResponse* error_response) {
   if (!response) {
-    std::move(error_callback)
-        .Run(kInvalidResponseErrorName, kInvalidResponseErrorMessage);
+    OnError(std::move(error_callback), error_response);
     return;
   }
   dbus::MessageReader reader(response);
@@ -183,19 +197,31 @@ void OnDictValueMethod(ShillClientHelper::RefHolder* ref_holder,
 }
 
 // Handles responses for methods without results.
-void OnVoidMethodWithErrorCallback(ShillClientHelper::RefHolder* ref_holder,
-                                   base::OnceClosure callback,
-                                   dbus::Response* response) {
-  std::move(callback).Run();
+void OnVoidMethodWithErrorResponse(
+    ShillClientHelper::RefHolder* ref_holder,
+    base::OnceClosure callback,
+    ShillClientHelper::ErrorCallback error_callback,
+    dbus::Response* response,
+    dbus::ErrorResponse* error_response) {
+  if (response) {
+    std::move(callback).Run();
+  } else {
+    OnError(std::move(error_callback), error_response);
+  }
 }
 
 // Handles responses for methods with base::Value::Dict results.
-// Used by CallDictValueMethodWithErrorCallback().
-void OnDictValueMethodWithErrorCallback(
+// Used by CallDictValueMethodWithErrorResponse().
+void OnDictValueMethodWithErrorResponse(
     ShillClientHelper::RefHolder* ref_holder,
     base::OnceCallback<void(base::Value::Dict result)> callback,
     ShillClientHelper::ErrorCallback error_callback,
-    dbus::Response* response) {
+    dbus::Response* response,
+    dbus::ErrorResponse* error_response) {
+  if (!response) {
+    OnError(std::move(error_callback), error_response);
+    return;
+  }
   dbus::MessageReader reader(response);
   base::Value value(dbus::PopDataAsValue(&reader));
   if (!value.is_dict()) {
@@ -207,11 +233,16 @@ void OnDictValueMethodWithErrorCallback(
 }
 
 // Handles responses for methods with ListValue results.
-void OnListValueMethodWithErrorCallback(
+void OnListValueMethodWithErrorResponse(
     ShillClientHelper::RefHolder* ref_holder,
     ShillClientHelper::ListValueCallback callback,
     ShillClientHelper::ErrorCallback error_callback,
-    dbus::Response* response) {
+    dbus::Response* response,
+    dbus::ErrorResponse* error_response) {
+  if (!response) {
+    OnError(std::move(error_callback), error_response);
+    return;
+  }
   dbus::MessageReader reader(response);
   base::Value value(dbus::PopDataAsValue(&reader));
   if (!value.is_list()) {
@@ -220,20 +251,6 @@ void OnListValueMethodWithErrorCallback(
     return;
   }
   std::move(callback).Run(value.GetList());
-}
-
-// Handles running appropriate error callbacks.
-void OnError(ShillClientHelper::ErrorCallback error_callback,
-             dbus::ErrorResponse* response) {
-  std::string error_name;
-  std::string error_message;
-  if (response) {
-    // Error message may contain the error message as string.
-    dbus::MessageReader reader(response);
-    error_name = response->GetErrorName();
-    reader.PopString(&error_message);
-  }
-  std::move(error_callback).Run(error_name, error_message);
 }
 
 }  // namespace
@@ -313,13 +330,11 @@ void ShillClientHelper::CallObjectPathMethodWithErrorCallback(
     ErrorCallback error_callback) {
   DCHECK(!callback.is_null());
   DCHECK(!error_callback.is_null());
-  auto split_callback = base::SplitOnceCallback(std::move(error_callback));
-  proxy_->CallMethodWithErrorCallback(
+  proxy_->CallMethodWithErrorResponse(
       method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
       base::BindOnce(&OnObjectPathMethodWithoutStatus,
                      base::Owned(new RefHolder(weak_ptr_factory_.GetWeakPtr())),
-                     std::move(callback), std::move(split_callback.first)),
-      base::BindOnce(&OnError, std::move(split_callback.second)));
+                     std::move(callback), std::move(error_callback)));
 }
 
 void ShillClientHelper::CallValueMethod(
@@ -350,12 +365,11 @@ void ShillClientHelper::CallVoidMethodWithErrorCallback(
     ErrorCallback error_callback) {
   DCHECK(!callback.is_null());
   DCHECK(!error_callback.is_null());
-  proxy_->CallMethodWithErrorCallback(
+  proxy_->CallMethodWithErrorResponse(
       method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-      base::BindOnce(&OnVoidMethodWithErrorCallback,
+      base::BindOnce(&OnVoidMethodWithErrorResponse,
                      base::Owned(new RefHolder(weak_ptr_factory_.GetWeakPtr())),
-                     std::move(callback)),
-      base::BindOnce(&OnError, std::move(error_callback)));
+                     std::move(callback), std::move(error_callback)));
 }
 
 void ShillClientHelper::CallBooleanMethodWithErrorCallback(
@@ -364,13 +378,11 @@ void ShillClientHelper::CallBooleanMethodWithErrorCallback(
     ErrorCallback error_callback) {
   DCHECK(!callback.is_null());
   DCHECK(!error_callback.is_null());
-  auto split_callback = base::SplitOnceCallback(std::move(error_callback));
-  proxy_->CallMethodWithErrorCallback(
+  proxy_->CallMethodWithErrorResponse(
       method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-      base::BindOnce(&OnBooleanMethodWithErrorCallback,
+      base::BindOnce(&OnBooleanMethodWithErrorResponse,
                      base::Owned(new RefHolder(weak_ptr_factory_.GetWeakPtr())),
-                     std::move(callback), std::move(split_callback.first)),
-      base::BindOnce(&OnError, std::move(split_callback.second)));
+                     std::move(callback), std::move(error_callback)));
 }
 
 void ShillClientHelper::CallStringMethodWithErrorCallback(
@@ -379,13 +391,11 @@ void ShillClientHelper::CallStringMethodWithErrorCallback(
     ErrorCallback error_callback) {
   DCHECK(!callback.is_null());
   DCHECK(!error_callback.is_null());
-  auto split_callback = base::SplitOnceCallback(std::move(error_callback));
-  proxy_->CallMethodWithErrorCallback(
+  proxy_->CallMethodWithErrorResponse(
       method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-      base::BindOnce(&OnStringMethodWithErrorCallback,
+      base::BindOnce(&OnStringMethodWithErrorResponse,
                      base::Owned(new RefHolder(weak_ptr_factory_.GetWeakPtr())),
-                     std::move(callback), std::move(split_callback.first)),
-      base::BindOnce(&OnError, std::move(split_callback.second)));
+                     std::move(callback), std::move(error_callback)));
 }
 
 void ShillClientHelper::CallDictValueMethodWithErrorCallback(
@@ -394,13 +404,11 @@ void ShillClientHelper::CallDictValueMethodWithErrorCallback(
     ErrorCallback error_callback) {
   DCHECK(!callback.is_null());
   DCHECK(!error_callback.is_null());
-  auto split_callback = base::SplitOnceCallback(std::move(error_callback));
-  proxy_->CallMethodWithErrorCallback(
+  proxy_->CallMethodWithErrorResponse(
       method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-      base::BindOnce(&OnDictValueMethodWithErrorCallback,
+      base::BindOnce(&OnDictValueMethodWithErrorResponse,
                      base::Owned(new RefHolder(weak_ptr_factory_.GetWeakPtr())),
-                     std::move(callback), std::move(split_callback.first)),
-      base::BindOnce(&OnError, std::move(split_callback.second)));
+                     std::move(callback), std::move(error_callback)));
 }
 
 void ShillClientHelper::CallListValueMethodWithErrorCallback(
@@ -409,13 +417,11 @@ void ShillClientHelper::CallListValueMethodWithErrorCallback(
     ErrorCallback error_callback) {
   DCHECK(!callback.is_null());
   DCHECK(!error_callback.is_null());
-  auto split_callback = base::SplitOnceCallback(std::move(error_callback));
-  proxy_->CallMethodWithErrorCallback(
+  proxy_->CallMethodWithErrorResponse(
       method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-      base::BindOnce(&OnListValueMethodWithErrorCallback,
+      base::BindOnce(&OnListValueMethodWithErrorResponse,
                      base::Owned(new RefHolder(weak_ptr_factory_.GetWeakPtr())),
-                     std::move(callback), std::move(split_callback.first)),
-      base::BindOnce(&OnError, std::move(split_callback.second)));
+                     std::move(callback), std::move(error_callback)));
 }
 
 namespace {
