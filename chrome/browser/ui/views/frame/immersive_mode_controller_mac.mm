74697bb36f641d4cf5446bb582b4d56baaafa4f4
Thomas Lukaszewicz <tluk@chromium.org>
2025-07-29 23:11:47 -0700

    [mac] Disable immersive mode when the Browser closes
    ImmersiveModeControllerMac::SetEnabled() makes the assumption that
    OverlayWidget and TabOverlayWidget outlive the BrowserFrame -
    dereferencing these widgets without null-checking and resetting
    observations.
    
    This logic relies on BrowserFrame::OnWidgetDestroying() being received
    before the OverlayWidget and TabOverlayWidget themselves are destroyed
    (which otherwise results in UAFs).
    
    This is very fragile and code risks UAFs if widget destruction order or
    ordering of widget destruction notifications were to ever change.
    
    There are also observed issues where the child overlay widgets will
    return a valid widget for GetWidget(), but nullptr for GetFocusManager()
    during destruction, causing issues (the path to the top-level Widget
    appears to break on Mac at some hard to diagnose point).
    
    This CL updates ImmersiveModeControllerMac to instead listen for and
    disable immersive mode when the browser-closed event is observed. This
    is emitted to give clients a chance to tear down browser-related state
    before Widget state / Widget hierarchies are destroyed.
    
    Bug: 413168662, 348369180
    Change-Id: I824b597851d3155658f2cbcaa15c9812f4d39fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6800722
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493960}

--- chrome/browser/ui/views/frame/immersive_mode_controller_mac.mm
+++ chrome/browser/ui/views/frame/immersive_mode_controller_mac.mm
@@ -92,6 +92,10 @@ ImmersiveModeControllerMac::~ImmersiveModeControllerMac() {
 void ImmersiveModeControllerMac::Init(BrowserView* browser_view) {
   browser_view_ = browser_view;
   focus_search_ = std::make_unique<ImmersiveModeFocusSearchMac>(browser_view);
+  browser_close_subscription_ =
+      browser_view_->browser()->RegisterBrowserDidClose(
+          base::BindRepeating(&ImmersiveModeControllerMac::BrowserDidClose,
+                              base::Unretained(this)));
 }
 
 void ImmersiveModeControllerMac::SetEnabled(bool enabled) {
@@ -124,7 +128,6 @@ void ImmersiveModeControllerMac::SetEnabled(bool enabled) {
       SetTabNativeWidgetID(tab_overlay_host->bridged_native_widget_id());
     }
     top_container_observation_.Observe(browser_view_->top_container());
-    browser_frame_observation_.Observe(browser_view_->GetWidget());
     overlay_widget_observation_.Observe(browser_view_->overlay_widget());
 
     // Capture the overlay content view before enablement. Once enabled the view
@@ -199,7 +202,6 @@ void ImmersiveModeControllerMac::SetEnabled(bool enabled) {
           browser_view_->tab_strip_region_view(), 0);
     }
     top_container_observation_.Reset();
-    browser_frame_observation_.Reset();
     overlay_widget_observation_.Reset();
 
     // Notify BrowserView about the fullscreen exit so that the top container
@@ -242,6 +244,11 @@ gfx::Insets ImmersiveModeControllerMac::GetTabStripRegionViewInsets() {
   return browser_view_->frame()->GetFrameView()->CaptionButtonsOnLeadingEdge()
              ? gfx::Insets::TLBR(1, right_left_inset, 0, 0)
              : gfx::Insets::TLBR(1, 0, 0, right_left_inset);
+}
+
+void ImmersiveModeControllerMac::BrowserDidClose(
+    BrowserWindowInterface* browser) {
+  SetEnabled(false);
 }
 
 bool ImmersiveModeControllerMac::IsEnabled() const {
@@ -363,10 +370,6 @@ void ImmersiveModeControllerMac::OnViewBoundsChanged(
   if (auto* window = GetNSWindowMojo()) {
     window->OnTopContainerViewBoundsChanged(bounds);
   }
-}
-
-void ImmersiveModeControllerMac::OnWidgetDestroying(views::Widget* widget) {
-  SetEnabled(false);
 }
 
 void ImmersiveModeControllerMac::LockDestroyed() {
