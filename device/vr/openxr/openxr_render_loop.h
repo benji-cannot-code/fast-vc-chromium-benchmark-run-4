d8300e5853c5c71e3362d140a5a03fd01b180a67
Alexander Cooper <alcooper@chromium.org>
2025-07-30 14:11:52 -0700

    Allow framebuffer scaling for OpenXR on Android
    Switches OpenXR on Android to by default send both the maximum size
    texture per-eye specified by the OpenXR system as well as a
    default_framebufer_scale representing the ratio of the recommended size
    to this maximum size. The end result is that by default pages will still
    render at the recommended resolution; however, pages may now scale up
    their resolution if they so desire (understanding that it will likely
    come at the cost of decreased framerate or other performance).
    
    Note that this also fixes a race condition with UpdateLayerBounds where
    the page could receive an unexpected size texture if the overlay had
    requested a frame before the page did (and before the page could call
    UpdateLayerBounds). This ensures that the shared image is now updated to
    the appropriate size if `UpdateLayerBounds` is called and it looks like
    the page has not yet received the frame.
    
    Bug: 40918787
    Change-Id: I60cb4f3464971e5e08cfb45e733cec66d39885df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799462
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494457}

--- device/vr/openxr/openxr_render_loop.h
+++ device/vr/openxr/openxr_render_loop.h
@@ -265,6 +265,8 @@ class OpenXrRenderLoop : public XRThread,
                             GLuint id,
                             std::unique_ptr<gfx::GpuFence> gpu_fence);
 
+  void PopulateSharedImageData(mojom::XRFrameData& frame_data);
+
   void MaybeRejectSessionCallback();
 
   gfx::Transform mojo_from_local() {
