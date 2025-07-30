f282e4cdc7ef8b07f13376166c50040f67c7c6c4
Saifuddin Hitawala <hitawala@chromium.org>
2025-07-30 07:21:38 -0700

    Reland [media] Use SharedImageFormat in CameraBufferFactory, GMBTrackerCros
    Difference From Original:
    We earlier removed PixFormatVideoToGfx util method and instead
    updated callers to call util method from media/base/format_util.
    But it did not handle JPEG formats so we handled it separately in
    couple places but not in StreamBufferManager which caused tast test
    failures. This change adds back util method that is used by chromeos
    capture code, but avoids adding JPEG support to media/base/ util
    method to avoid polluting it as JPEG <-> R8 conversion should be
    rare.
    
    Original CL Description:
    Update CameraBufferFactory, GpuMemoryBufferTrackerCros and utils
    to talk in terms of SharedImageFormat instead of BufferFormat as part
    of BufferFormat conversions.
    
    Note: This is my first attempt at using gemini-cli. It involved asking
    it to read these specific files and then performing conversions in
    place but needed some manual intervention for clean-up and updating
    tests.
    
    Bug: 356649879
    Change-Id: I8b3b1d475a42a48dd2c6ad4e19c35f8f5ded87a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799137
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494164}

--- media/capture/video/chromeos/camera_buffer_factory.h
+++ media/capture/video/chromeos/camera_buffer_factory.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 
+#include "components/viz/common/resources/shared_image_format.h"
 #include "media/capture/video/chromeos/mojom/camera3.mojom.h"
 #include "media/capture/video/chromeos/pixel_format_utils.h"
 #include "media/capture/video_capture_types.h"
@@ -30,14 +31,14 @@ class CAPTURE_EXPORT CameraBufferFactory {
 
   virtual scoped_refptr<gpu::ClientSharedImage> CreateSharedImage(
       const gfx::Size& size,
-      gfx::BufferFormat format,
+      viz::SharedImageFormat format,
       gfx::BufferUsage usage,
       const gfx::ColorSpace& color_space = gfx::ColorSpace());
 
   virtual scoped_refptr<gpu::ClientSharedImage> CreateSharedImageFromGmbHandle(
       gfx::GpuMemoryBufferHandle buffer_handle,
       const gfx::Size& size,
-      gfx::BufferFormat format,
+      viz::SharedImageFormat format,
       gfx::BufferUsage usage,
       const gfx::ColorSpace& color_space = gfx::ColorSpace());
 
