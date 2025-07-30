85b96698d6d536940edef205e80a9cdafe4a182d
Eshwar Stalin <estalin@chromium.org>
2025-07-30 12:25:35 -0700

    [SxS] Ensure NewSplitTab has the correct pinned state
    NewSplitTab uses the correct group affiliation when creating the split.
    However, the pinned state isn't retained and so updating the logic to
    ensure this is handled correctly. Also adding new browser tests for the
    delegate class for split APIs.
    
    Fixed: 427556960
    Change-Id: I51e8c41f8626b5046a60a47c5d149231a7cbaadc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6800846
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494388}

--- chrome/browser/ui/browser_tab_strip_model_delegate_browsertest.cc
+++ chrome/browser/ui/browser_tab_strip_model_delegate_browsertest.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
+#include "chrome/browser/ui/tabs/split_tab_metrics.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_utils.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/saved_tab_groups/public/features.h"
@@ -334,6 +336,81 @@ IN_PROC_BROWSER_TEST_F(BrowserTabStripModelDelegateTest,
   EXPECT_FALSE(groups.at(0).local_group_id().has_value());
   // Current group is open
   EXPECT_TRUE(groups.at(1).local_group_id().has_value());
+}
+
+class BrowserTabStripModelDelegateWithSideBySide
+    : public BrowserTabStripModelDelegateTest {
+ public:
+  BrowserTabStripModelDelegateWithSideBySide() {
+    feature_list_.InitWithFeatures({features::kSideBySide}, {});
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(BrowserTabStripModelDelegateWithSideBySide,
+                       NewSplitTabWithActiveTabPinned) {
+  std::unique_ptr<TabStripModelDelegate> delegate =
+      std::make_unique<BrowserTabStripModelDelegate>(browser());
+
+  GURL url1("chrome://about");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url1));
+
+  browser()->tab_strip_model()->SetTabPinned(0, true);
+  delegate->NewSplitTab({}, split_tabs::SplitTabCreatedSource::kToolbarButton);
+
+  ASSERT_EQ(browser()->tab_strip_model()->count(), 2);
+  ASSERT_TRUE(browser()->tab_strip_model()->IsTabPinned(0));
+  ASSERT_TRUE(browser()->tab_strip_model()->IsTabPinned(1));
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserTabStripModelDelegateWithSideBySide,
+                       NewSplitTabWithActiveTabGroupped) {
+  std::unique_ptr<TabStripModelDelegate> delegate =
+      std::make_unique<BrowserTabStripModelDelegate>(browser());
+
+  GURL url1("chrome://about");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url1));
+
+  tab_groups::TabGroupId group_id =
+      browser()->tab_strip_model()->AddToNewGroup({0});
+  delegate->NewSplitTab({}, split_tabs::SplitTabCreatedSource::kToolbarButton);
+
+  ASSERT_EQ(browser()->tab_strip_model()->count(), 2);
+  ASSERT_EQ(browser()->tab_strip_model()->GetTabGroupForTab(0).value(),
+            group_id);
+  ASSERT_EQ(browser()->tab_strip_model()->GetTabGroupForTab(1).value(),
+            group_id);
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserTabStripModelDelegateWithSideBySide,
+                       DuplicateSplitTab) {
+  std::unique_ptr<TabStripModelDelegate> delegate =
+      std::make_unique<BrowserTabStripModelDelegate>(browser());
+
+  GURL url1("chrome://about");
+  GURL url2("chrome://version");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url1));
+  ASSERT_TRUE(AddTabAtIndex(1, url2, ui::PAGE_TRANSITION_LINK));
+
+  delegate->NewSplitTab({0}, split_tabs::SplitTabCreatedSource::kToolbarButton);
+
+  ASSERT_EQ(browser()->tab_strip_model()->count(), 2);
+  std::optional<split_tabs::SplitTabId> split_id1 =
+      browser()->tab_strip_model()->GetSplitForTab(0);
+  ASSERT_TRUE(split_id1.has_value());
+  ASSERT_EQ(browser()->tab_strip_model()->GetSplitForTab(1).value(),
+            split_id1.value());
+
+  delegate->DuplicateSplit(split_id1.value());
+
+  ASSERT_EQ(browser()->tab_strip_model()->count(), 4);
+  std::optional<split_tabs::SplitTabId> split_id2 =
+      browser()->tab_strip_model()->GetSplitForTab(2);
+  ASSERT_TRUE(split_id2.has_value());
+  ASSERT_EQ(browser()->tab_strip_model()->GetSplitForTab(3).value(),
+            split_id2.value());
 }
 
 }  // namespace chrome
