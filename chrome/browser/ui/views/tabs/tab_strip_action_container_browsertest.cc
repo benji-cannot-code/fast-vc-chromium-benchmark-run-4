a8a8fc9f136807e29c1a2b926f44060d22eaca1b
Maggie Jennings <mjenn@google.com>
2025-07-30 16:46:44 -0700

    [Task Icon] Update task icon nudge text
    Update the "Check your task" text based on UX feedback.
    
    Bug: 422439931
    Change-Id: I7dba0cdba03ffa89588ab4eb15d4b3c4ff689423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6802566
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494514}

--- chrome/browser/ui/views/tabs/tab_strip_action_container_browsertest.cc
+++ chrome/browser/ui/views/tabs/tab_strip_action_container_browsertest.cc
@@ -569,7 +569,7 @@ IN_PROC_BROWSER_TEST_F(TabStripActionContainerBrowserTest,
   EXPECT_TRUE(GlicActorTaskIcon()->GetVisible());
   EXPECT_TRUE(GlicActorTaskIcon()->GetIsShowingNudge());
   // TODO(crbug.com/431015299): Replace with finalized strings when ready.
-  EXPECT_EQ(GlicActorTaskIcon()->GetText(), u"Check your task");
+  EXPECT_EQ(GlicActorTaskIcon()->GetText(), u"Your task needs attention");
 
   ResetAnimation(1);
 
