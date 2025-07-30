475795d3df044f097b15c14cd1d42ed0cc1a680e
Karol Sygiet <sygiet@google.com>
2025-07-30 04:15:22 -0700

    [RSG] Add explicit includes for AutocompleteEntry in ASG
    Bug: 409962888
    Change-Id: I2aaa8057d21d9e8e009f39c232f66808bdc6759a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799319
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1494080}

--- components/autofill/core/browser/suggestions/autocomplete_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/autocomplete_suggestion_generator.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/to_vector.h"
 #include "components/autofill/core/browser/single_field_fillers/autocomplete/autocomplete_history_manager.h"
 #include "components/autofill/core/browser/studies/autofill_experiments.h"
+#include "components/autofill/core/browser/webdata/autocomplete/autocomplete_entry.h"
 
 namespace autofill {
 
--- components/autofill/core/browser/suggestions/autocomplete_suggestion_generator_unittests.cc
+++ components/autofill/core/browser/suggestions/autocomplete_suggestion_generator_unittests.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "components/autofill/core/browser/foundations/test_autofill_client.h"
 #include "components/autofill/core/browser/test_utils/autofill_test_utils.h"
+#include "components/autofill/core/browser/webdata/autocomplete/autocomplete_entry.h"
 #include "components/autofill/core/browser/webdata/mock_autofill_webdata_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
