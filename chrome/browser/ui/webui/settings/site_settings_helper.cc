ea3da604fe1e7f09e168872f196cfcd88a49f880
Christian Dullweber <dullweber@chromium.org>
2025-07-30 03:24:11 -0700

    ContentSettings: Add method to get default PermissionSetting
    Provide a way to query the default PermissionSetting.
    
    Bug: 425642101
    Change-Id: Id290ac8359c6a26596859f3bc90933c44265e86a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781044
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494065}

--- chrome/browser/ui/webui/settings/site_settings_helper.cc
+++ chrome/browser/ui/webui/settings/site_settings_helper.cc
@@ -768,7 +768,7 @@ std::string ProviderToDefaultSettingSourceString(const ProviderType provider) {
     case ProviderType::kNotificationAndroidProvider:
     case ProviderType::kProviderForTests:
     case ProviderType::kOtherProviderForTests:
-      NOTREACHED();
+      NOTREACHED() << provider;
   }
 }
 
