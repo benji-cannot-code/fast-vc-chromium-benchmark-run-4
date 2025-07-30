41eb78fd33add31a7cd078dd2006004b965980a3
Tiffany Shen <tishen@google.com>
2025-07-30 09:08:26 -0700

    [Save PDF to Drive] Adds more functionality to AccountChooserView.
    Changes include:
    - Allowing the dialog to switch between single account view (https://screenshot.googleplex.com/9vdyianCkjY2cbY) and multi account view (https://screenshot.googleplex.com/7DjuhCM9wnyewXz) without creating a new dialog.
    - Adding callbacks to the buttons
    - Adding a scrollbar view for lots of accounts.
    
    Change-Id: Ia1c61ea2e27a48385a91a986ca9f939ba4bac450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792907
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Tiffany Shen <tishen@google.com>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494255}

---
+++ chrome/browser/ui/save_to_drive/mock_account_chooser_controller_delegate.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/save_to_drive/mock_account_chooser_controller_delegate.h"
+
+namespace save_to_drive {
+MockAccountChooserControllerDelegate::MockAccountChooserControllerDelegate() =
+    default;
+MockAccountChooserControllerDelegate::~MockAccountChooserControllerDelegate() =
+    default;
+}  // namespace save_to_drive
