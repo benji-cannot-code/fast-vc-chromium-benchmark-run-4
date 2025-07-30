255d955bbe5424f63010a22c166f207f972136b9
Adem Derinel <derinel@google.com>
2025-07-30 07:41:21 -0700

    Revert "[iOS] Disable SupervisedUserWithParentalControlsTestCase"
    This reverts commit 57136dc0e8cbadbdb3e18e8f0fae3d04c544e01b.
    
    Reason for revert: Tree closure 
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-device/435105/overview
    
    Bug: 435140688
    Original change's description:
    > [iOS] Disable SupervisedUserWithParentalControlsTestCase
    >
    > Tests are failing on device.
    >
    > Bug: 435140688
    > Change-Id: Icf8ac30d6fccbcb7710ab597d953206f790818b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798802
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    > Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1494163}
    
    Bug: 435140688
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibc8ce62b980febba92dccba099685f99302305d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6801803
    Auto-Submit: Adem Derinel <derinel@google.com>
    Owners-Override: Adem Derinel <derinel@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1494186}

--- ios/chrome/browser/supervised_user/model/supervised_user_with_parental_controls_egtest.mm
+++ ios/chrome/browser/supervised_user/model/supervised_user_with_parental_controls_egtest.mm
@@ -221,11 +221,7 @@ static const char* kInterstitialDetails = "Details";
 // when a filtering for them is triggered. Also tests that the filtering logic
 // on existing tabs does not force-realize unrealized states. This is a
 // regression test for bug: 1486459.
-// TODO(crbug.com/435140688): Reenable this test.
 - (void)testSupervisedUserURLFilteringReloadsOnlyRealizedExistingWebStates {
-#if !TARGET_OS_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
-#endif
   // Signing in the user and allow all sites.
   [self signInSupervisedUser];
   [SupervisedUserSettingsAppInterface setFilteringToAllowAllSites];
@@ -440,11 +436,7 @@ static const char* kInterstitialDetails = "Details";
 
 // Tests that block-listing a url, results in showing immediately the
 // interstitial if the user has the url open in a tab.
-// TODO(crbug.com/435140688): Reenable this test.
 - (void)testSupervisedUserWithAllowAllFilteringIsBlockedOnURLBlockListing {
-#if !TARGET_OS_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
-#endif
   [self signInSupervisedUser];
   [SupervisedUserSettingsAppInterface setFilteringToAllowAllSites];
 
@@ -461,12 +453,8 @@ static const char* kInterstitialDetails = "Details";
 // Tests that users who have the filtering behaviour changed from "Allow all"
 // to "Allow approved" websites, will be shown the interstitial as soon as
 // the filtering behaviour changes.
-// TODO(crbug.com/435140688): Reenable this test.
 - (void)
     testSupervisedUserWithAllowApprovedSitesFilteringIsBlockedOnFilterChange {
-#if !TARGET_OS_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
-#endif
   [self signInSupervisedUser];
   GURL safeURL = self.testServer->GetURL(kEchoPath);
   [ChromeEarlGrey loadURL:safeURL];
@@ -483,11 +471,7 @@ static const char* kInterstitialDetails = "Details";
 // Tests that for users who have the filtering behaviour changed from "Allow
 // approved" to "Allow all" websites, a blocked pages will be refreshed and
 // unblocks as soon as the filtering behaviour changes.
-// TODO(crbug.com/435140688): Reenable this test.
 - (void)testSupervisedUserWithAllowAllSitesFilteringIsUnblockedOnFilterChange {
-#if !TARGET_OS_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
-#endif
   [self signInSupervisedUser];
   [SupervisedUserSettingsAppInterface setFilteringToAllowApprovedSites];
 
@@ -637,13 +621,9 @@ static const char* kInterstitialDetails = "Details";
   }
 }
 
-// Test that the when Local Web Approval is disabled, the "Back" button of the
-// interstitial gets us to the previous page.
-// TODO(crbug.com/435140688): Reenable this test.
+// Test that the when Local Web Approval is disabled, the "Back" button of the interstitial
+// gets us to the previous page.
 - (void)testSupervisedUserInterstitialOnBackButton {
-#if !TARGET_OS_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
-#endif
   [self signInSupervisedUser];
   [SupervisedUserSettingsAppInterface setFakePermissionCreator];
   [SupervisedUserSettingsAppInterface setFilteringToAllowAllSites];
@@ -670,13 +650,9 @@ static const char* kInterstitialDetails = "Details";
   [ChromeEarlGrey waitForWebStateContainingText:kDefaultContent];
 }
 
-// Test that the when Local Web Approval is enabled, users can request a local
-// web approval from the waiting screen.
-// TODO(crbug.com/435140688): Reenable this test.
+// Test that the when Local Web Approval is enabled, users can request a local web
+// approval from the waiting screen.
 - (void)testSupervisedUserInterstitialOnLocalApprovalRequestFromWaitingScreen {
-#if !TARGET_OS_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Disabled on devices");
-#endif
   [self signInSupervisedUser];
   [SupervisedUserSettingsAppInterface setFakePermissionCreator];
   [SupervisedUserSettingsAppInterface setFilteringToAllowAllSites];
