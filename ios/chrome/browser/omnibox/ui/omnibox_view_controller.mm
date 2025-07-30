19da504d47d891720905b0637e2d6c9d64eb8455
Krishna Govind <govind@chromium.org>
2025-07-29 17:44:59 -0700

    Revert "Reload the omnibox textfield after changing allowsReturnKeyWithEmptyText"
    This reverts commit 000b00f7f3120f4cd251fb12868d74b324841323.
    
    Reason for revert: Causing crashes - https://g-issues.chromium.org/issues/434738250
    
    Original change's description:
    > Reload the omnibox textfield after changing allowsReturnKeyWithEmptyText
    >
    > This CL fixes a bug where the copied-link suggestion was not navigated
    > to when hitting the return key.
    > Prior to this CL, the return key was enabled but the text field needed a
    > call to -reloadInputViews to take it into effect.
    >
    > Fixed: 40836724
    > Change-Id: If37cce23bbf40865529f288dec952ab9959534e3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787470
    > Reviewed-by: Christian Xu <christianxu@chromium.org>
    > Auto-Submit: Louis Romero <lpromero@google.com>
    > Commit-Queue: Christian Xu <christianxu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1491969}
    
    Change-Id: Ib470d0e077247cd01019f3aee2a88f28e8b5cfc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799071
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1493889}

--- ios/chrome/browser/omnibox/ui/omnibox_view_controller.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_view_controller.mm
@@ -462,7 +462,6 @@ using base::UserMetricsAction;
       !!self.view.thumbnailImage ||
       [self.popupKeyboardDelegate
           canPerformKeyboardAction:OmniboxKeyboardAction::kReturnKey];
-  [self.textField reloadInputViews];
 }
 
 - (void)setPlaceholderText:(NSString*)placeholderText {
