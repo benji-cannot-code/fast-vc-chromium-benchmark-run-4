b0249df6ba56adb6fad013a2742c24988443b4ed
David Sanders <dsanders11@ucsbalum.com>
2025-07-29 18:06:31 -0700

    Remove unused include in partition_address_space.h
    Should save ~36 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: I70f6f5f3effc6797a4b230490b69923138b456e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6800088
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1493903}

--- base/allocator/partition_allocator/src/partition_alloc/partition_address_space.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_address_space.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "partition_alloc/partition_alloc_base/bits.h"
 #include "partition_alloc/partition_alloc_base/compiler_specific.h"
 #include "partition_alloc/partition_alloc_base/component_export.h"
-#include "partition_alloc/partition_alloc_base/files/platform_file.h"
 #include "partition_alloc/partition_alloc_base/notreached.h"
 #include "partition_alloc/partition_alloc_check.h"
 #include "partition_alloc/partition_alloc_config.h"
