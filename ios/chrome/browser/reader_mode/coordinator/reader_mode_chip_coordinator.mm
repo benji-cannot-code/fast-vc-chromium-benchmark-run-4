1ee90bf674c399c0255d030c06d8f46507b698ba
Quentin Pubert <qpubert@google.com>
2025-07-30 07:24:00 -0700

    [iOS] Fix ReaderModeOptionsCommands dispatching crash
    This CL ensures that HandlerForProtocol is not used on the command
    dispatcher for ReaderModeOptionsCommands if it is not dispatching for
    that protocol. Skipping this verification step caused a crash which
    should now be fixed.
    
    Bug: 428434715
    Change-Id: I1fe92446da2a8324f5d8bcda6dfda40ef6161a2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798801
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1494168}

--- ios/chrome/browser/reader_mode/coordinator/reader_mode_chip_coordinator.mm
+++ ios/chrome/browser/reader_mode/coordinator/reader_mode_chip_coordinator.mm
@@ -45,8 +45,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)showReaderModeChip {
   [self.visibilityDelegate readerModeChipCoordinator:self
                           didSetReaderModeChipHidden:NO];
-  _viewController.readerModeOptionsHandler = HandlerForProtocol(
-      self.browser->GetCommandDispatcher(), ReaderModeOptionsCommands);
+  CommandDispatcher* dispatcher = self.browser->GetCommandDispatcher();
+  if ([dispatcher
+          dispatchingForProtocol:@protocol(ReaderModeOptionsCommands)]) {
+    _viewController.readerModeOptionsHandler =
+        HandlerForProtocol(dispatcher, ReaderModeOptionsCommands);
+  }
 }
 
 - (void)hideReaderModeChip {
