cada8ffc02d055cec0dabb2f5afa6e5998ab856a
Tiffany Shen <tishen@google.com>
2025-07-30 10:00:19 -0700

    [Save PDF to Drive] Adds ability to select an account from the AccountChooserRadioGroupView.
    Change-Id: I7f051299e7fcc63bd97ac468b5f1ffb57c889370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792906
    Commit-Queue: Tiffany Shen <tishen@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494300}

--- chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.cc
+++ chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.h"
 
+#include "chrome/browser/ui/save_to_drive/account_chooser_view_delegate.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/save_to_drive/account_chooser_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -78,8 +79,10 @@ BEGIN_METADATA(AccountChooserRadioButtonRow)
 END_METADATA
 
 AccountChooserRadioGroupView::AccountChooserRadioGroupView(
+    AccountChooserViewDelegate& parent_dialog,
     const std::vector<AccountInfo>& accounts,
-    std::optional<CoreAccountId> primary_account_id) {
+    std::optional<CoreAccountId> primary_account_id)
+    : parent_dialog_(parent_dialog) {
   CHECK(accounts.size() > 1) << "Account chooser radio group view should only "
                                 "be used for multi-account cases.";
   SetCascadingRadioGroupView(this, views::kCascadingRadioGroupView);
@@ -120,8 +123,7 @@ AccountChooserRadioGroupView::~AccountChooserRadioGroupView() = default;
 void AccountChooserRadioGroupView::SelectAccount(const AccountInfo& account) {
   auto it = accounts_.find(account);
   CHECK(it != accounts_.end());
-  // TODO(crbug.com/434686397): future implementation - update account selected
-  // field.
+  parent_dialog_->OnAccountSelected(account);
 }
 
 BEGIN_METADATA(AccountChooserRadioGroupView)
--- chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.h
+++ chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
+#include "chrome/browser/ui/save_to_drive/account_chooser_view_delegate.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/views/controls/button/radio_button.h"
@@ -57,7 +58,8 @@ class AccountChooserRadioGroupView : public views::BoxLayoutView,
   METADATA_HEADER(AccountChooserRadioGroupView, views::BoxLayoutView)
  public:
   // Precondition: accounts.size() > 1
-  AccountChooserRadioGroupView(const std::vector<AccountInfo>& accounts,
+  AccountChooserRadioGroupView(AccountChooserViewDelegate& parent_dialog,
+                               const std::vector<AccountInfo>& accounts,
                                std::optional<CoreAccountId> primary_account_id);
   AccountChooserRadioGroupView(const AccountChooserRadioGroupView&) = delete;
   AccountChooserRadioGroupView& operator=(const AccountChooserRadioGroupView&) =
@@ -76,6 +78,7 @@ class AccountChooserRadioGroupView : public views::BoxLayoutView,
   // Sorted by account display name.
   std::map<AccountInfo, AccountChooserRadioButtonRow*, AccountInfoCmp>
       accounts_;
+  raw_ref<AccountChooserViewDelegate> parent_dialog_;  // can never be null
 };
 }  // namespace save_to_drive
 #endif  // CHROME_BROWSER_UI_VIEWS_SAVE_TO_DRIVE_ACCOUNT_CHOOSER_RADIO_GROUP_VIEW_H_
