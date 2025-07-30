2fa149eef788670935e51362218c9c3609412e77
Alex Mitra <alexmitra@chromium.org>
2025-07-30 08:26:11 -0700

    Calculate bottom inset using Window methods instead of screen methods
    This approach takes supports multi-window layouts. This CL also adds
    unit tests for the logic and tests cases where the WebView is obscured
    by the edge of its parent Window. The calculation logic has been moved
    to getViewportInsetBottom. This CL also fixes the case where a WebView
    is used inside a bottom sheet by calling onViewportInsetBottomChanged
    inside getViewportInsetBottom.
    
    Bug: 40287394, 403584257, 430149984, 430024081, 429362514, 429917440
    Change-Id: I8aba8b131cd2c5d1c8961ff3fd5436ccc05dc201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6794364
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494220}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -55,6 +55,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewQuicConnectionTimeout,
     &features::kWebViewRecordAppCacheHistograms,
     &features::kWebViewReduceUAAndroidVersionDeviceModel,
+    &features::kWebViewReportImeInsets,
     &features::kWebViewSafeAreaIncludesSystemBars,
     &features::kWebViewSeparateResourceContext,
     &features::kWebViewShortCircuitShouldInterceptRequest,
