085929ebb32d176256bda5edd75a13dd69770710
Evan Stade <evanstade@microsoft.com>
2025-07-30 08:17:10 -0700

    IDB: fix DecodeVarInt corner case [2]
    As with 2044bff6cb3e6587b01889f00de75945b109bf67, DecodeVarInt()
    successfully decodes an input that can't have been generated by
    EncodeVarInt. When the encoded integer takes up all 10 bytes, the last
    byte has to be exactly 0x01. This is because only 1 bit is used and 7
    are ignored, and the one that's used must be 1 or the encoding would
    have stopped before the 10th byte.
    
    (This hints that the last byte is actually useless and could be dropped
    altogether, since the top bit of the 9th byte implies what the 10th
    byte must be. But since these encodings are persisted to disk, it's
    not super easy to change this.)
    
    Again, this bug is harmless (or at least, it is not obvious how it
    could be harmful), but it did trip up a fuzzer which had generated an
    input stream which was being decoded into something valid, but for which
    re-encoding generated a different byte stream.
    
    Fixed: 434331334
    Change-Id: I059d67968fa05366d1456581e7e70a1d3b86fd03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6800018
    Reviewed-by: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1494218}

--- components/services/storage/indexed_db/scopes/varint_coding.cc
+++ components/services/storage/indexed_db/scopes/varint_coding.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 #include "components/services/storage/indexed_db/scopes/varint_coding.h"
 
 #include <array>
@@ -50,9 +49,18 @@ bool DecodeVarInt(std::string_view* from, int64_t* into) {
       return false;
     }
 
-    ret |= static_cast<uint64_t>(c & 0x7f) << shift;
+    uint64_t pre_shift = static_cast<uint64_t>(c & 0x7f);
+    uint64_t shifted = pre_shift << shift;
+    if ((shifted >> shift) != pre_shift) {
+      // Make sure that no bits are shifted off the left, which would be another
+      // form of invalid input (which can occur in the last byte).
+      return false;
+    }
+
+    ret |= shifted;
     shift += 7;
   } while (*it++ & 0x80);
+
   *into = static_cast<int64_t>(ret);
   from->remove_prefix(it - from->begin());
   return true;
