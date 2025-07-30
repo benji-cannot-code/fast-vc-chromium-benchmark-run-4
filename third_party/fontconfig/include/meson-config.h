3a3950699816057b82bac945b8eb67057aac3b7b
Dominik Röttsches <drott@chromium.org>
2025-07-30 07:33:43 -0700

    Roll src/third_party/fontconfig/src/ 8f169b6a9..86b48ec01 (7 commits)
    Reland after revert in:
    https://crrev.com/c/6799105
    Architecture dependent compile error in format string is now fixed.
    
    https://chromium.googlesource.com/external/fontconfig.git/+log/8f169b6a9c6b..86b48ec01ece
    
    $ git log 8f169b6a9..86b48ec01 --date=short --no-merges --format='%ad %ae %s'
    2025-07-29 drott [Fontations] Don't leak PatternElement values
    2025-07-30 drott Architecture-dependent hex formatting of int64_t in fccache.c
    2025-07-30 akira Improve a warning message
    2025-07-28 wl doc: Fix two typos
    2025-07-24 drott [Fontations] Remaining fix for UaF
    2025-07-18 akira Add fontconfig version in FcCache
    2025-07-18 akira meson: Add a missing fontconfig architecture test case
    
    Created with:
      roll-dep src/third_party/fontconfig/src
    
    Bug: chromium:349952802
    Cq-Include-Trybots: luci.chromium.try:linux-cast-arm-rel
    Change-Id: I8b58cd7bf9cd98a500f6fca8c504bda194a0e457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799324
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1494182}

--- third_party/fontconfig/include/meson-config.h
+++ third_party/fontconfig/include/meson-config.h
@@ -27,6 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define FC_TEMPLATEDIR "/usr/share/fontconfig/conf.avail"
 
+#define FC_VERSION_MAJOR 2
+
+#define FC_VERSION_MICRO 1
+
+#define FC_VERSION_MINOR 17
+
 #define FLEXIBLE_ARRAY_MEMBER /**/
 
 #define FONTCONFIG_PATH "/etc/fonts"
