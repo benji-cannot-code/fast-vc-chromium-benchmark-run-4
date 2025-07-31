f3914b3802c706b4e71a93cdfaa62d9bf8a29e8b
Gauthier Ambard <gambard@chromium.org>
2025-07-31 03:11:10 -0700

    [iOS] Update FacePile color
    Make sure that the FacePile color is matching the buttons in the
    Tab Group view.
    Also updates the share button height to be the same as the buttons.
    
    Fixed: 432230177
    Change-Id: I2f8b1240fac2f6c4b32268464181b16b672167ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6805315
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494709}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.mm
@@ -32,6 +32,7 @@ NSString* const kTabGroupFacePileButtonIdentifier =
 UIColor* TabGroupViewButtonBackgroundColor() {
   return [[UIColor colorNamed:kGrey200Color] colorWithAlphaComponent:0.35];
 }
+const CGFloat kTabGroupButtonHeight = 26;
 
 NSString* const kTabGroupRecentActivityIdentifier =
     @"kTabGroupRecentActivityIdentifier";
