048571c0b47f2d1c81807c5764ecc0f653e6aa7e
Gauthier Ambard <gambard@chromium.org>
2025-07-31 01:39:29 -0700

    [iOS] Disable flaky tests
    Those tests are always failing on official bots.
    
    Bug: 435333773, 435327953, 435334012
    Change-Id: Ia93331cb28d04627d8bd3d3a42910c0f3cd1ddb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6802842
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494673}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_settings_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_settings_egtest.mm
@@ -791,7 +791,8 @@ id<GREYMatcher> SettingsToolbarDoneButton() {
 }
 
 // Tests that the local profile is migrated to account.
-- (void)testMigrateToAccount {
+// TODO(crbug.com/435334012): Reenable this test.
+- (void)FLAKY_testMigrateToAccount {
   [SigninEarlGrey signinWithFakeIdentity:[FakeSystemIdentity fakeIdentity1]];
   [AutofillAppInterface saveExampleProfile];
   [self
