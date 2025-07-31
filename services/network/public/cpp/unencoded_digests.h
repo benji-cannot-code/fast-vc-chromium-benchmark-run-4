4e26a7652505320c11b5c5a9ccf862c4e0d95cea
Mike West <mkwst@chromium.org>
2025-07-31 01:56:49 -0700

    [Unencoded-Digest] Copy `unencoded-digest` parsing to //services/network
    This is one of a chain of CLs moving `unencoded-digest` parsing and
    error reporting to the network service:
    
    1. https://crrev.com/c/6734707 [You are here.]
    2. https://crrev.com/c/6734019 (`URLResponseHead` integration)
    3. https://crrev.com/c/6771890 (DevTools integration)
    4. https://crrev.com/c/6798148 (Wire up enforcement in Blink)
    
    Bug: 431760410
    Change-Id: Ifaa6c0c1d265eeec96919b77fc388fe5d5f9b533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6734707
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494678}

---
+++ services/network/public/cpp/unencoded_digests.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_PUBLIC_CPP_UNENCODED_DIGESTS_H_
+#define SERVICES_NETWORK_PUBLIC_CPP_UNENCODED_DIGESTS_H_
+
+#include "base/component_export.h"
+#include "services/network/public/mojom/unencoded_digest.mojom.h"
+
+namespace net {
+class HttpResponseHeaders;
+}
+
+namespace network {
+
+// Parses the Unencoded-Digest response header.
+//
+// https://www.ietf.org/archive/id/draft-ietf-httpbis-unencoded-digest-00.html
+COMPONENT_EXPORT(NETWORK_CPP)
+mojom::UnencodedDigestsPtr ParseUnencodedDigestsFromHeaders(
+    const net::HttpResponseHeaders& headers);
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_PUBLIC_CPP_UNENCODED_DIGESTS_H_
