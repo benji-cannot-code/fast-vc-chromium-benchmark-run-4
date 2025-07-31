dea39cfe6e40af4c0f4b452015472909ead2186e
Aldo Culquicondor <acondor@chromium.org>
2025-07-31 07:49:19 -0700

    Cleanup IncrementLocalSurfaceIdForMainframeSameDocNavigationName
    Added a field in WebPreference, so the feature can be disabled for
    Android WebView, where it is not needed.
    
    Change-Id: I3d4cdf847cfd3570ef60f91e4ca4bd06da4ece8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6794285
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494815}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1414,10 +1414,6 @@ extern const char kHttpsUpgradesDescription[];
 extern const char kIgnoreGpuBlocklistName[];
 extern const char kIgnoreGpuBlocklistDescription[];
 
-extern const char kIncrementLocalSurfaceIdForMainframeSameDocNavigationName[];
-extern const char
-    kIncrementLocalSurfaceIdForMainframeSameDocNavigationDescription[];
-
 extern const char kIncognitoScreenshotName[];
 extern const char kIncognitoScreenshotDescription[];
 
