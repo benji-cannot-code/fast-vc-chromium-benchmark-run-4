75b6933d42e182996f4b4feefc25884dd02177e5
Justin Cohen <justincohen@google.com>
2025-07-31 11:52:40 -0700

    ios: Remove ScopedNativeScreen from BaseEarlGreyTestCase
    The display::ScopedNativeScreen was being created in the rest Runner,
    not in the test host. This is redundant, as the test Runner does not
    need a display::Screen instance.
    
    This change removes the unnecessary setup/teardown logic, simplifying
    the base test case.
    
    Change-Id: I53fc619adbb00e06c193433a3f0314274a626bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6807091
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494990}

--- ios/testing/earl_grey/base_earl_grey_test_case.mm
+++ ios/testing/earl_grey/base_earl_grey_test_case.mm
@@ -17,11 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/testing/earl_grey/coverage_utils.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/testing/earl_grey/system_alert_handler.h"
-#import "ui/display/screen.h"
-
-#if DCHECK_IS_ON()
-#import "ui/display/screen_base.h"
-#endif
 
 namespace {
 
@@ -29,7 +24,6 @@ namespace {
 // ensure that +setUpForTestCase is called exactly once per unique XCTestCase
 // and is reset in +tearDown.
 bool g_needs_set_up_for_test_case = true;
-std::unique_ptr<display::ScopedNativeScreen> g_screen;
 }  // namespace
 
 @implementation BaseEarlGreyTestCase
@@ -68,8 +62,6 @@ std::unique_ptr<display::ScopedNativeScreen> g_screen;
 - (void)setUp {
   [super setUp];
 
-  g_screen = std::make_unique<display::ScopedNativeScreen>();
-
   // Before starting a new test, relaunch the app and wipe the profile.
   AppLaunchConfiguration config = [self appConfigurationForTestCase];
   if ([BaseEarlGreyTestCase forceRestartAndWipe]) {
@@ -97,17 +89,7 @@ std::unique_ptr<display::ScopedNativeScreen> g_screen;
 }
 
 + (void)tearDown {
-#if DCHECK_IS_ON()
-  // Make sure that all display observers are removed at the end of each
-  // test.
-  if (display::Screen::HasScreen()) {
-    display::ScreenBase* screen =
-        static_cast<display::ScreenBase*>(display::Screen::GetScreen());
-    DCHECK(!screen->HasDisplayObservers());
-  }
-#endif
   g_needs_set_up_for_test_case = true;
-  g_screen.reset();
   [super tearDown];
 }
 
