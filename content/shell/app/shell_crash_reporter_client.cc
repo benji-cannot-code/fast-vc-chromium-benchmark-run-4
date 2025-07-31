18bb87cbbd053010ef609f453b636a0f9c21c52b
Raphael Kubo da Costa <kubo@igalia.com>
2025-07-31 03:10:55 -0700

    iOS, tvOS: Finish integrating Crashpad into content_shell
    Most of the integration is already working, except for two things:
    
    * The crash dump location used to be taken from the --crash-dumps-dir
      argument, which does not work in this case because the location needs
      to be inside the app's installation directory, which cannot be derived
      beforehand.
      Instead, do what Chrome-on-iOS and use a "Crashpad/" directory inside
      base::DIR_CACHE and stop erquiring --crash-dumps-dir. The latter's
      value is still preferred if it is specified though.
    
    * Saved intermediate dumps were never being processed, as Crashpad's
      ProcessIntermediateDumps() was not being called.
    
    Bug: 40254930, 391914246
    Cq-Include-Trybots: luci.chromium.try:ios-blink-rel-fyi,tvos-rel-fyi
    Change-Id: I80517e61cfd7506d67d35131949813605327ea2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718388
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <kubo@igalia.com>
    Auto-Submit: Raphael Kubo da Costa <kubo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1494708}

--- content/shell/app/shell_crash_reporter_client.cc
+++ content/shell/app/shell_crash_reporter_client.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "content/public/common/content_switches.h"
@@ -20,6 +21,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace content {
+
+namespace {
+
+base::FilePath GetCrashDumpLocationInternal() {
+  base::FilePath default_dir;
+#if BUILDFLAG(IS_IOS)
+  CHECK(base::PathService::Get(base::DIR_CACHE, &default_dir));
+  default_dir = default_dir.Append("Crashpad");
+#endif  // BUILDFLAG(IS_IOS)
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kCrashDumpsDir)) {
+    return default_dir;
+  }
+  return base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+      switches::kCrashDumpsDir);
+}
+
+}  // namespace
 
 ShellCrashReporterClient::ShellCrashReporterClient() {}
 ShellCrashReporterClient::~ShellCrashReporterClient() {}
@@ -49,12 +68,10 @@ bool ShellCrashReporterClient::GetCrashDumpLocation(std::wstring* crash_dir) {
 #else
 bool ShellCrashReporterClient::GetCrashDumpLocation(base::FilePath* crash_dir) {
 #endif
-  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kCrashDumpsDir))
+  base::FilePath crash_directory = GetCrashDumpLocationInternal();
+  if (crash_directory.empty()) {
     return false;
-  base::FilePath crash_directory =
-      base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
-          switches::kCrashDumpsDir);
+  }
 #if BUILDFLAG(IS_WIN)
   *crash_dir = crash_directory.value();
 #else
