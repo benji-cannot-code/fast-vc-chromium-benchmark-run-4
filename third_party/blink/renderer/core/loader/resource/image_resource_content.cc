270df3bc4f2ef32fe1e533cc3bce88bb9dba0761
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-07-31 15:38:56 -0700

    Remove ImageResourceInfo::HasCacheControlNoStoreHeader
    No longer used.
    
    Bug: N/A
    Change-Id: I74595de08c1dfa62457abca1e83c2dd4afe98a31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673473
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495134}

--- third_party/blink/renderer/core/loader/resource/image_resource_content.cc
+++ third_party/blink/renderer/core/loader/resource/image_resource_content.cc
@@ -57,7 +57,6 @@ class NullImageResourceInfo final
       DoesCurrentFrameHaveSingleSecurityOrigin) const override {
     return true;
   }
-  bool HasCacheControlNoStoreHeader() const override { return false; }
   std::optional<ResourceError> GetResourceError() const override {
     return std::nullopt;
   }
@@ -713,10 +712,6 @@ base::TimeTicks ImageResourceContent::LoadEnd() const {
 
 base::TimeTicks ImageResourceContent::LoadResponseEnd() const {
   return info_->LoadResponseEnd();
-}
-
-bool ImageResourceContent::HasCacheControlNoStoreHeader() const {
-  return info_->HasCacheControlNoStoreHeader();
 }
 
 float ImageResourceContent::DevicePixelRatioHeaderValue() const {
