7b212098979e6723d7e59a294bf311c45275218b
Jacob Stanley <jacobstanley@google.com>
2025-07-30 19:35:48 -0700

    [Ad Topics Content Parity] Enable feature by default
    Received approval for full launch
    (https://launch.corp.google.com/launch/4333481)
    
    Bug: b:347055994
    Change-Id: I6136cc55efbe0e38fd97250b7ce7c9942f68bc75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6803523
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Auto-Submit: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1494574}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -205,7 +205,7 @@ const base::FeatureParam<bool>
 
 BASE_FEATURE(kPrivacySandboxAdTopicsContentParity,
              "PrivacySandboxAdTopicsContentParity",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivacySandboxNoticeQueue,
              "PrivacySandboxNoticeQueue",
