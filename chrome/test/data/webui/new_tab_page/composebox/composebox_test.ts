245661016dae834416c0203f02fc57939ed86818
Nihar Majmudar <niharm@google.com>
2025-07-31 13:14:55 -0700

    [composebox] Hook up omnibox controller to composebox.
    This CL gives the ComposeboxHandler and instance of an
    OmniboxController. This allows the composebox WebUI to start receiving
    zps suggestions. The composebox now queries autocomplete when the it is
    first created and stores this autocomplete result.
    
    This CL doesn't actually render these results or make any UI changes.
    
    Change-Id: Id594c7b1a07455e8e978e4e8479354b9c0f03074
    Bug: 434734199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6794166
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495043}

--- chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
+++ chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
@@ -28,6 +28,7 @@ function generateZeroId(): string {
 suite('NewTabPageComposeboxTest', () => {
   let composeboxElement: ComposeboxElement;
   let handler: TestMock<PageHandlerRemote>;
+  let searchboxHandler: TestMock<SearchboxPageHandlerRemote>;
   let callbackRouterRemote: PageRemote;
   let metrics: MetricsTracker;
 
@@ -40,6 +41,9 @@ suite('NewTabPageComposeboxTest', () => {
             new SearchboxPageCallbackRouter())));
     callbackRouterRemote = ComposeboxProxyImpl.getInstance()
                                .callbackRouter.$.bindNewPipeAndPassRemote();
+    searchboxHandler = installMock(
+        SearchboxPageHandlerRemote,
+        mock => ComposeboxProxyImpl.getInstance().searchboxHandler = mock);
     metrics = fakeMetricsPrivate();
   });
 
@@ -571,5 +575,17 @@ suite('NewTabPageComposeboxTest', () => {
     assertEquals(
         composeboxElement.$.cancelIcon.getAttribute('title'),
         loadTimeData.getString('composeboxCancelButtonTitleInput'));
+  });
+
+  test('composebox queries autocomplete on load', async () => {
+    loadTimeData.overrideValues({composeboxShowZps: true});
+    createComposeboxElement();
+    await microtasksFinished();
+
+    // Autocomplete should be queried when the composebox is created.
+    assertEquals(searchboxHandler.getCallCount('queryAutocomplete'), 1);
+
+    // Restore.
+    loadTimeData.overrideValues({composeboxShowZps: false});
   });
 });
