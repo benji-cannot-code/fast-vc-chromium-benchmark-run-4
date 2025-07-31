5dc8d53437ac7c1fb223e3befe757ac9df76f80e
Christoph Schwering <schwering@google.com>
2025-07-31 08:52:40 -0700

    [Autofill] Generalize AutofillType to union type
    This CL repurposes the AutofillType class to represent union types.
    An AutofillType essentially represents a set of FieldTypes.
    
    This set is subject to constraints: the "AutofillType constraints"
    stipulate that the intersections of the AutofillType's FieldTypeSet
    with certain other FieldTypeSets must have at most one element.
    For example, an AutofillType may hold at most one address FieldType.
    
    These constraints are defined from scratch -- the existing concepts of
    FieldTypeGroup, FormType, or FillingProduct do not suit them well. The
    main reason is that some FieldTypes occur in multiple constraints.
    For example, EMAIL_ADDRESS occurs in the FieldTypeSet of the
    constraints for addresses, for identity credentials, and for loyalty
    cards. Similarly, NAME_FULL occurs in the constraints for addresses
    and certain EntityTypes.
    
    Future CL will migrate the existing calls of GetStorableType() to the
    new getters GetTypes(), GetAddressType(), etc. Same for groups().
    
    An AutofillType can still hold an HtmlFieldType as an alternative to
    a set of FieldType. A future CL will remove that.
    
    This CL is a no-op because all existing AutofillTypes only hold a
    singleton FieldTypeSet.
    
    Bug: 432645177
    Change-Id: I7f9c6d52ce74c82054fc1ccd494109c2c7796f33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798048
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494862}

--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/containers/fixed_flat_map.h"
+#include "base/containers/to_vector.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
+#include "base/strings/string_util.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
 
@@ -346,6 +348,12 @@ std::string_view FieldTypeToStringView(FieldType type) {
 
 std::string FieldTypeToString(FieldType type) {
   return std::string(FieldTypeToStringView(type));
+}
+
+std::string FieldTypeSetToString(FieldTypeSet s) {
+  return base::JoinString(
+      base::ToVector(s, [](FieldType t) { return FieldTypeToStringView(t); }),
+      ", ");
 }
 
 FieldType TypeNameToFieldType(std::string_view type_name) {
