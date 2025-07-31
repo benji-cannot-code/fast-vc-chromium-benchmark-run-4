8794d6d0829fc62a32932867fab2eef079554094
HuanPo Lin <robertlin@chromium.org>
2025-07-30 18:49:14 -0700

    gardener: Disable ClientSideDetectionHostScamDetectionTest.OnDeviceLLMWithEmptyResponse
    Bug: 435277952
    Change-Id: Ibcf7d6f9ab61ca51e062e6a46b19cb37484d302f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804723
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Huanpo Lin <robertlin@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494562}

--- chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
@@ -2718,7 +2718,14 @@ TEST_F(ClientSideDetectionHostScamDetectionTest,
       IntelligentScanVerdict::INTELLIGENT_SCAN_VERDICT_SAFE);
 }
 
-TEST_F(ClientSideDetectionHostScamDetectionTest, OnDeviceLLMWithEmptyResponse) {
+// TODO(crbug.com/434649088): Re-enable this test
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_OnDeviceLLMWithEmptyResponse DISABLED_OnDeviceLLMWithEmptyResponse
+#else
+#define MAYBE_OnDeviceLLMWithEmptyResponse OnDeviceLLMWithEmptyResponse
+#endif
+TEST_F(ClientSideDetectionHostScamDetectionTest,
+       MAYBE_OnDeviceLLMWithEmptyResponse) {
   if (base::FeatureList::IsEnabled(kClientSideDetectionKillswitch)) {
     GTEST_SKIP();
   }
