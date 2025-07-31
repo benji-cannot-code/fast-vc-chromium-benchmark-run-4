3680ecb7165b489baf25b4b9163b6f26759bcdab
David Sanders <dsanders11@ucsbalum.com>
2025-07-30 23:24:38 -0700

    Replace heavy include in bucket_init_params.h
    Switches to directly including the header which defines
    `blink::mojom::BucketDurability`.
    
    Should save ~73 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: I815494889ebe5fd07bc15c3b80818ed6c94c7def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804976
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494623}

--- components/services/storage/public/cpp/buckets/bucket_init_params.h
+++ components/services/storage/public/cpp/buckets/bucket_init_params.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/services/storage/public/cpp/buckets/bucket_id.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
-#include "third_party/blink/public/mojom/buckets/bucket_manager_host.mojom.h"
+#include "third_party/blink/public/mojom/quota/quota_types.mojom-forward.h"
 
 namespace storage {
 
