2775eeb02efa00068a1cb0e2ae5b519a834f539f
Adithya Srinivasan <adithyas@chromium.org>
2025-07-31 08:19:34 -0700

    [Gardener] Disable flaky IWA tests
    Bug: 435371796
    Change-Id: I65f0c331243d20054c235015fb9e0ba3df5ff951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6806257
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Auto-Submit: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Owners-Override: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494834}

--- chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_manager_ash_browsertest.cc
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_manager_ash_browsertest.cc
@@ -363,8 +363,9 @@ class IsolatedWebAppPolicyManagerAshBrowserTest
       const IsolatedWebAppPolicyManagerAshBrowserTest&) = delete;
 };
 
+// TODO(crbug.com/435371796): Test is flaky.
 IN_PROC_BROWSER_TEST_P(IsolatedWebAppPolicyManagerAshBrowserTest,
-                       InstallIsolatedWebAppOnLogin) {
+                       DISABLED_InstallIsolatedWebAppOnLogin) {
   AddUser(/*set_iwa_policy_on_login=*/true);
 
   // Log in in the managed guest session.
@@ -384,8 +385,9 @@ IN_PROC_BROWSER_TEST_P(IsolatedWebAppPolicyManagerAshBrowserTest,
   EXPECT_EQ(GetIsolatedWebAppVersion(kAppId1).GetString(), "7.0.6");
 }
 
+// TODO(crbug.com/435371796): Test is flaky.
 IN_PROC_BROWSER_TEST_P(IsolatedWebAppPolicyManagerAshBrowserTest,
-                       PolicyUpdate) {
+                       DISABLED_PolicyUpdate) {
   AddUser();
 
   SetPolicyWithOneApp();
