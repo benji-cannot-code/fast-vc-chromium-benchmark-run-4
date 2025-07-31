f24c043e9713919ceadaced2f55a4f1b3ad7f6e0
Takuto Ikuta <tikuta@chromium.org>
2025-07-30 20:36:00 -0700

    mojo: Include variant instead of utility
    Needed for std::monostate.
    
    Bug: 40263312
    Change-Id: I8444b839524635dade3577008051cb661cd9735b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6803194
    Reviewed-by: Matt Stark <msta@google.com>
    Commit-Queue: Matt Stark <msta@google.com>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494589}

--- mojo/public/cpp/base/empty_mojom_traits.h
+++ mojo/public/cpp/base/empty_mojom_traits.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_PUBLIC_CPP_BASE_EMPTY_MOJOM_TRAITS_H_
 #define MOJO_PUBLIC_CPP_BASE_EMPTY_MOJOM_TRAITS_H_
 
-#include <utility>
+#include <variant>
 
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "mojo/public/mojom/base/empty.mojom-shared.h"
