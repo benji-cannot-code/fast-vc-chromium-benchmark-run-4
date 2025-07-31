0911405d4ac8233657bbe8fdbc048f32c364c61d
David Sanders <dsanders11@ucsbalum.com>
2025-07-31 12:01:11 -0700

    Switch to ax_enums.mojom-forward.h header in web_contents.h
    Should save ~182 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: Ia8938b0ddf0aec0db80c9513a20e7972ccd42135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804980
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495001}

--- content/public/browser/web_contents.h
+++ content/public/browser/web_contents.h
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/picture_in_picture_window_options/picture_in_picture_window_options.mojom.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 #include "third_party/skia/include/core/SkColor.h"
-#include "ui/accessibility/ax_enums.mojom-shared.h"
+#include "ui/accessibility/ax_enums.mojom-forward.h"
 #include "ui/accessibility/ax_mode.h"
 #include "ui/accessibility/platform/inspect/ax_api_type.h"
 #include "ui/color/color_provider_key.h"
