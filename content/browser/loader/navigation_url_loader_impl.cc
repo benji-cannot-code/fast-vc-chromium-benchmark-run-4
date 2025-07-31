12efe90ca7d253a87787af6e3f78ad9f663032f2
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-07-31 08:46:33 -0700

    Call CancelWithError before Unbind() in NavigationURLLoaderImpl
    Currently, upon receiving HTTP 304,
    `NavigationURLLoaderImpl::OnReceiveResponse()` calls
    `ThrottlingURLLoader::Unbind()` and then calls
    `ThrottlingURLLoader::CancelWithError()`, which calls
    `NavigationURLLoaderImpl::OnComplete()`.
    
    This is confusing, because we expect a call from `ThrottlingURLLoader`
    to its client even after `ThrottlingURLLoader::Unbind()`. This also
    blocks https://chromium-review.googlesource.com/c/chromium/src/+/6776124
    to CHECK() that no loader activity should happen on
    `NavigationURLLoaderImpl` after `Unbind()`.
    
    This CL calls the `CancelWithError()` for HTTP 304 without calling
    `Unbind()`. This should preserve the behavior, because
    `ThrottlingURLLoader::CancelWithError()` resets the mojo endpoints,
    which is equivalent to calling `ThrottlingURLLoader::Unbind()` and then
    discarding the returned endpoints.
    
    The `Unbind()` call was intentionally moved before the cancellation in
    https://chromium-review.googlesource.com/c/chromium/src/+/1252684 (see
    the diff between Patch Sets 1 and 2), "to avoid receiving unexpected
    call". At that time, `NavigationURLLoaderImpl::OnComplete()` was called
    (which didn't reset the mojo endpoints), but now we call
    `CancelWithError()` (which resets the mojo endpoints), so the ordering
    is no longer needed.
    
    Bug: 434182226
    Change-Id: Ie4f395660aabbdf508967a175750908a23b58c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804628
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494853}

--- content/browser/loader/navigation_url_loader_impl.cc
+++ content/browser/loader/navigation_url_loader_impl.cc
@@ -1239,13 +1239,7 @@ void NavigationURLLoaderImpl::OnReceiveResponse(
     return;
   }
 
-  network::mojom::URLLoaderClientEndpointsPtr url_loader_client_endpoints =
-      loader_holder_.Unbind();
-
   // 304 responses should abort the navigation, rather than display the page.
-  // This needs to be after the URLLoader has been moved to
-  // `url_loader_client_endpoints` in order to abort the request, to avoid
-  // receiving unexpected call.
   if (head->headers &&
       head->headers->response_code() == net::HTTP_NOT_MODIFIED) {
     // Call CancelWithError instead of OnComplete so that if there is an
@@ -1255,6 +1249,9 @@ void NavigationURLLoaderImpl::OnReceiveResponse(
         std::string_view(base::NumberToString(net::ERR_ABORTED)));
     return;
   }
+
+  network::mojom::URLLoaderClientEndpointsPtr url_loader_client_endpoints =
+      loader_holder_.Unbind();
 
   bool must_download = download_utils::MustDownload(
       browser_context_, url_, head->headers.get(), head->mime_type);
