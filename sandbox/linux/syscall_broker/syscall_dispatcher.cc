53674f30485b3c5306ed1be24512ec9b74607d3b
Matt Stark <msta@google.com>
2025-07-30 18:50:25 -0700

    Fix missing includes
    Bug: 40263312
    
    Change-Id: I6a6a6964253e04099f3d93df4c94da3962a3610e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6801289
    Auto-Submit: Matt Stark <msta@google.com>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Matt Stark <msta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1494564}

--- sandbox/linux/syscall_broker/syscall_dispatcher.cc
+++ sandbox/linux/syscall_broker/syscall_dispatcher.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fcntl.h>
 
+#include <cerrno>
+
 #include "base/check.h"
 #include "base/logging.h"
 #include "sandbox/linux/system_headers/linux_syscalls.h"
