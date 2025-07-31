bb3978f90362b07b093e817e34c283bab74af4d1
Brandon Wylie <wylieb@chromium.org>
2025-07-31 15:14:43 -0700

    [reader mode] Include the always-on entrypoint when disitlling in-app
    Bug: 435259543
    Change-Id: I55c00d11347664cf79c18d29bd22dee4916133c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804058
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495118}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegate.java
@@ -683,7 +683,9 @@ public class TabbedAppMenuPropertiesDelegate extends AppMenuPropertiesDelegateIm
     }
 
     private boolean shouldShowReaderModeItem(@Nullable Tab currentTab) {
-        return currentTab != null && DomDistillerFeatures.showAlwaysOnEntryPoint();
+        return currentTab != null
+                && (DomDistillerFeatures.showAlwaysOnEntryPoint()
+                        || DomDistillerFeatures.sReaderModeDistillInApp.isEnabled());
     }
 
     private MVCListAdapter.ListItem buildReaderModeItem(Tab currentTab) {
