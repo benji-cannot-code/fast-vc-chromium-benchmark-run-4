5c37b191d3c7e6cdee5c3eb3472661a271ce96b2
Marijn Kruisselbrink <mek@chromium.org>
2025-07-31 12:39:19 -0700

    Enable `kPwaNavigationCapturing` reimpl on Chrome OS
    In preparation of rolling this out to 100% of stable channel, change the
    default for this feature to be enabled on all platforms.
    
    Bug: 427427432
    Change-Id: Idfe3876d47065dfc9622b624e8c6b14b9bf2579f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799043
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495024}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -1596,11 +1596,7 @@ const base::FeatureParam<int> kNavigationCapturingIPHGuardrailStorageDuration{
 
 BASE_FEATURE(kPwaNavigationCapturing,
              "PwaNavigationCapturing",
-#if BUILDFLAG(IS_CHROMEOS)
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
              base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 const base::FeatureParam<CapturingState>::Option kNavigationCapturingParams[] =
     {{CapturingState::kDefaultOn, "on_by_default"},
