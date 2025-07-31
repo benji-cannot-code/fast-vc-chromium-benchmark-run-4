112069afd3b2d80228f9320420aaa401b267d963
Liza Bipin <mlbipin@google.com>
2025-07-31 06:32:40 -0700

    Add no-op PlatformAccounts interface to components/signin
    Implementation will be added in following Cls according to design doc in
    bug.
    
    Bug: 429143376
    Change-Id: Id5be5b02396907c2dad0ed435b6041af83cbe4ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798068
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1494773}

---
+++ components/signin/public/android/java/src/org/chromium/components/signin/PlatformAccount.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.components.signin;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.google_apis.gaia.GaiaId;
+
+/** Provides details about an account. */
+@NullMarked
+public interface PlatformAccount {
+    /** Returns gaiaId of the PlatformAccount. */
+    GaiaId getId();
+
+    /** Returns email of the PlatformAccount. */
+    String getEmail();
+
+    /**
+     * Returns a {@link CapabilityResponse} that indicates whether the account has the requested or
+     * has exception.
+     */
+    @AccountManagerDelegate.CapabilityResponse
+    int hasCapability();
+}
