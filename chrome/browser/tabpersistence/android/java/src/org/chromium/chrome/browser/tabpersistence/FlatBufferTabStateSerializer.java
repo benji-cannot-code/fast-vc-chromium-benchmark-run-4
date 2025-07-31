88c1d790c635b907de539fc64bfac6878ee586c6
Jenna Himawan <jhimawan@google.com>
2025-07-31 16:47:17 -0700

    Add FROM_LINK_CREATING_NEW_WINDOW TabLaunchType
    Bug: 40916071
    Change-Id: I24fced09ab8d5315e1f07acb273e3286336a9758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804755
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495164}

--- chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
+++ chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
@@ -233,6 +233,8 @@ public class FlatBufferTabStateSerializer implements TabStateSerializer {
                 return TabLaunchType.FROM_LONGPRESS_FOREGROUND_IN_GROUP;
             case TabLaunchTypeAtCreation.FROM_TAB_LIST_INTERFACE:
                 return TabLaunchType.FROM_TAB_LIST_INTERFACE;
+            case TabLaunchTypeAtCreation.FROM_LINK_CREATING_NEW_WINDOW:
+                return TabLaunchType.FROM_LINK_CREATING_NEW_WINDOW;
             case TabLaunchTypeAtCreation.SIZE:
                 return TabLaunchType.SIZE;
             case TabLaunchTypeAtCreation.UNKNOWN:
@@ -313,6 +315,8 @@ public class FlatBufferTabStateSerializer implements TabStateSerializer {
                 return TabLaunchTypeAtCreation.FROM_LONGPRESS_FOREGROUND_IN_GROUP;
             case TabLaunchType.FROM_TAB_LIST_INTERFACE:
                 return TabLaunchTypeAtCreation.FROM_TAB_LIST_INTERFACE;
+            case TabLaunchType.FROM_LINK_CREATING_NEW_WINDOW:
+                return TabLaunchTypeAtCreation.FROM_LINK_CREATING_NEW_WINDOW;
             case TabLaunchType.SIZE:
                 return TabLaunchTypeAtCreation.SIZE;
             default:
