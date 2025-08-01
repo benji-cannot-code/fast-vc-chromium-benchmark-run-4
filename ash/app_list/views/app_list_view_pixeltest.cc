a571f43497d9208cffc3bdbf1ffb2c2f596cdd58
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-08-01 15:51:57 -0700

    Fix the black line visual glitch for AppListBubble
    For certain device scale factors, a black line appears for AppListBubble
    when background blur is not applied to the bubble. Black line usually
    means that pixels are not painted for that texture.
    
    We can fix it by painting a non-rounded background. (The layer itself
    is being rounded so it is unnecessary to begin with)
    
    Bug: b:435718259, b:435722727
    Change-Id: I30a91d5a8038de18a098d8676fbb8e76e479de53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814030
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495793}

--- ash/app_list/views/app_list_view_pixeltest.cc
+++ ash/app_list/views/app_list_view_pixeltest.cc
@@ -188,7 +188,7 @@ TEST_P(AppListViewPixelRTLTest, AnswerCardSearchResult) {
 
   UseFixedPlaceholderTextAndHideCursor(test_helper->GetSearchBoxView());
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_answer_card_search_results", /*revision_number=*/16,
+      "bubble_launcher_answer_card_search_results", /*revision_number=*/17,
       GetAppListTestHelper()->GetBubbleView(),
       GetPrimaryShelf()->navigation_widget()));
 }
@@ -210,7 +210,7 @@ TEST_P(AppListViewPixelRTLTest, URLSearchResult) {
 
   UseFixedPlaceholderTextAndHideCursor(test_helper->GetSearchBoxView());
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_url_search_results", /*revision_number=*/15,
+      "bubble_launcher_url_search_results", /*revision_number=*/16,
       GetAppListTestHelper()->GetBubbleView(),
       GetPrimaryShelf()->navigation_widget()));
 }
@@ -232,7 +232,7 @@ TEST_P(AppListViewPixelRTLTest, KeyboardShortcutSearchResult) {
 
   UseFixedPlaceholderTextAndHideCursor(test_helper->GetSearchBoxView());
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_ks_search_results", /*revision_number=*/6,
+      "bubble_launcher_ks_search_results", /*revision_number=*/7,
       GetAppListTestHelper()->GetBubbleView()));
 }
 
@@ -245,7 +245,7 @@ TEST_P(AppListViewPixelRTLTest, Basics) {
   UseFixedPlaceholderTextAndHideCursor(
       GetAppListTestHelper()->GetSearchBoxView());
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_basics", /*revision_number=*/18,
+      "bubble_launcher_basics", /*revision_number=*/19,
       GetAppListTestHelper()->GetBubbleView(),
       GetPrimaryShelf()->navigation_widget()));
 }
@@ -267,7 +267,7 @@ TEST_P(AppListViewPixelRTLTest, GradientZone) {
                                 /*position=*/20);
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_gradient_zone", /*revision_number=*/18,
+      "bubble_launcher_gradient_zone", /*revision_number=*/19,
       GetAppListTestHelper()->GetBubbleView(),
       GetPrimaryShelf()->navigation_widget()));
 }
@@ -282,7 +282,7 @@ TEST_P(AppListViewPixelRTLTest, GeminiButton) {
       GetAppListTestHelper()->GetSearchBoxView());
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "bubble_launcher_gemini_button", /*revision_number=*/1,
+      "bubble_launcher_gemini_button", /*revision_number=*/2,
       GetAppListTestHelper()->GetBubbleView(),
       GetPrimaryShelf()->navigation_widget()));
 }
