4c29ebaac3da3683059eade7cde1371f86cc5478
Peng Zhou <zhoupeng.1996@bytedance.com>
2025-08-01 09:50:15 -0700

    Remove invalid guest_url_spec parameter from WebViewInteractiveTest::SetupTest
    The `guest_url_spec` parameter was unused and no longer needed in
    `WebViewInteractiveTest::SetupTest`.
    
    This CL has no behavior changes.
    
    Bug: 428258029
    Change-Id: I7d52acaff4a2af5f8aa12ac8ecbbc884c01329fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6810731
    Commit-Queue: Peng Zhou <zhoupeng.1996@bytedance.com>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495541}

--- chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
@@ -300,8 +300,7 @@ class WebViewInteractiveTest : public extensions::PlatformAppBrowserTest {
         base::StringPrintf("onAppMessage('%s');", message.c_str())));
   }
 
-  void SetupTest(const std::string& app_name,
-                 const std::string& guest_url_spec) {
+  void SetupTest(const std::string& app_name) {
     ASSERT_TRUE(StartEmbeddedTestServer());
 
     LoadAndLaunchPlatformApp(app_name.c_str(), "connected");
@@ -573,8 +572,7 @@ class DISABLED_WebViewPopupInteractiveTest : public WebViewInteractiveTest {};
 #define MAYBE_PointerLock DISABLED_PointerLock
 #endif
 IN_PROC_BROWSER_TEST_F(WebViewPointerLockInteractiveTest, MAYBE_PointerLock) {
-  SetupTest("web_view/pointer_lock",
-            "/extensions/platform_apps/web_view/pointer_lock/guest.html");
+  SetupTest("web_view/pointer_lock");
 
   // Move the mouse over the Lock Pointer button.
   ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(
@@ -644,8 +642,7 @@ IN_PROC_BROWSER_TEST_F(WebViewPointerLockInteractiveTest, MAYBE_PointerLock) {
 #endif
 IN_PROC_BROWSER_TEST_F(WebViewPointerLockInteractiveTest,
                        MAYBE_PointerLockFocus) {
-  SetupTest("web_view/pointer_lock_focus",
-            "/extensions/platform_apps/web_view/pointer_lock_focus/guest.html");
+  SetupTest("web_view/pointer_lock_focus");
 
   // Move the mouse over the Lock Pointer button.
   ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(
@@ -880,9 +877,7 @@ IN_PROC_BROWSER_TEST_F(WebViewInteractiveTest, EditCommands) {
 // Tests that guests receive edit commands and respond appropriately.
 IN_PROC_BROWSER_TEST_F(WebViewInteractiveTest, EditCommandsNoMenu) {
   ExtensionTestMessageListener focus_listener("Focused");
-  SetupTest("web_view/edit_commands_no_menu",
-            "/extensions/platform_apps/web_view/edit_commands_no_menu/"
-            "guest.html");
+  SetupTest("web_view/edit_commands_no_menu");
   ASSERT_TRUE(ui_test_utils::ShowAndFocusNativeWindow(GetPlatformAppWindow()));
   // Ensure that an input gets focused before sending a key event.
   ASSERT_TRUE(focus_listener.WaitUntilSatisfied());
