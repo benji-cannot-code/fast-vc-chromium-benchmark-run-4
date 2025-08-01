b050c0b53d78a8f44f322ba672bdfc7f8e8568b9
Colin Blundell <blundell@chromium.org>
2025-08-01 05:39:58 -0700

    Revert "[Blink] Use CanvasResourceProviderSI for low-latency Canvas2D on Windows"
    This reverts commit ce194959d0bfe3799c7b54b1785e2c489ff9cc21.
    
    Reason for revert: crbug.com/435607273
    
    Bug: 415968760
    Original change's description:
    > [Blink] Use CanvasResourceProviderSI for low-latency Canvas2D on Windows
    >
    > Enables killswitch added in [1].
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/6769103
    >
    > Bug: 415968760
    > Change-Id: I2015eec008e0636dcacb8703f521fc92d9e83340
    > Cq-Do-Not-Cancel-Tryjobs: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6768726
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1494039}
    
    Bug: 415968760
    Change-Id: I0e035a5bbcd6f7ad92d8c8d164ce32a626bb531b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6811754
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495373}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1541,7 +1541,7 @@ bool CanvasResourceProvider::CanvasImageProvider::IsHardwareDecodeCache()
 #if BUILDFLAG(IS_WIN)
 BASE_FEATURE(kUseCRPSIForLowLatencyOnWindows,
              "UseCRPSIForLowLatencyOnWindows",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kCanvas2DAutoFlushParams,
