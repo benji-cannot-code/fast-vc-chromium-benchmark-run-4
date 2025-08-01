fbca337fe6dfc440bc61743769992a4309be17ce
Xiaohui Chen <xiaohuic@google.com>
2025-08-01 14:25:43 -0700

    assistant: cleanup code 9 of N
    * remove most logic in c/b/ui/ash/assistant files
    * keep assistant_browser_delegate_impl for now to limit the size of the
      cl.
    
    Bug: 388361414
    Change-Id: Ib90e9a7263386ca9112494a3914368b325a0e464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6803272
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Reviewed-by: Aiden Chiavatti <chiav@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495739}

--- chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.cc
@@ -182,7 +182,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/task_manager/task_manager_interface.h"
 #include "chrome/browser/tracing/chrome_tracing_delegate.h"
 #include "chrome/browser/ui/ash/assistant/assistant_browser_delegate_impl.h"
-#include "chrome/browser/ui/ash/assistant/assistant_state_client.h"
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client.h"
 #include "chrome/browser/ui/ash/session/session_controller_client_impl.h"
 #include "chrome/browser/ui/webui/ash/emoji/emoji_ui.h"
@@ -980,13 +979,6 @@ void ChromeBrowserMainPartsAsh::PreProfileInit() {
   // Has to be initialized before |assistant_delegate_|;
   image_downloader_ = std::make_unique<ImageDownloaderImpl>();
 
-  // Requires UserManager.
-  assistant_state_client_ = std::make_unique<AssistantStateClient>();
-
-  // Assistant has to be initialized before
-  // ChromeBrowserMainExtraPartsAsh::session_controller_client_ to avoid race of
-  // SessionChanged event and assistant_client initialization. It must come
-  // after AssistantStateClient.
   assistant_delegate_ = std::make_unique<AssistantBrowserDelegateImpl>();
 
   quick_pair_delegate_ =
@@ -1595,8 +1587,6 @@ void ChromeBrowserMainPartsAsh::PostMainMessageLoopRun() {
   // Assistant has to shut down before voice interaction controller client to
   // correctly remove the observer.
   assistant_delegate_.reset();
-
-  assistant_state_client_.reset();
 
   if (pre_profile_init_called_) {
     Shell::Get()->RemovePreTargetHandler(MagnificationManager::Get());
