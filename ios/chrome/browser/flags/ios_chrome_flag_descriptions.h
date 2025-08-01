6db2befe2a51a605600e5bec1360bd9d8d7fb06f
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-08-01 09:40:03 -0700

    [iOS][PageContext] iOS Page Context anchor tags retrieval
    Added anchor tags retrieval in Page Context for iOS. Also added a new
    flag for it. If the flag is enabled, the anchor tags script is injected
    into the main script, and the result is parsed + added to the
    AnnotatePageContent tree structure. All the anchor tags of a given frame
    are queried for and added as direct children of the frame's root node,
    alongside the entire innerText (TextInfo node).
    
    Bug: 435151709
    Change-Id: I0c9a5005cd6089d37cc0593ea33bc834427ed52f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6802944
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495530}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -763,6 +763,9 @@ extern const char
 extern const char kPageContentAnnotationsRemotePageMetadataName[];
 extern const char kPageContentAnnotationsRemotePageMetadataDescription[];
 
+extern const char kPageContextAnchorTagsName[];
+extern const char kPageContextAnchorTagsDescription[];
+
 extern const char kPageImageServiceSalientImageName[];
 extern const char kPageImageServiceSalientImageDescription[];
 
