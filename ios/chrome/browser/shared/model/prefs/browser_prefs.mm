7ef5e01772b709ae61cf8d5d7397227507e9a95e
Federica Germinario <fedegermi@google.com>
2025-08-01 06:50:43 -0700

    [ios] Remove prefs deprecated on 08/2024 from BrowserPrefs
    These prefs expired one year ago, code can now be removed.
    
    Bug: none
    Change-Id: I5fb90545ba241957292442ff569bbe6d8307cf92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797955
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Owners-Override: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495409}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -138,13 +138,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Deprecated 08/2024.
-const char kTrialPrefName[] = "trending_queries.trial_version";
-
-// Deprecated 08/2024.
-constexpr char kSafeBrowsingEsbOptInWithFriendlierSettings[] =
-    "safebrowsing.esb_opt_in_with_friendlier_settings";
-
 // Deprecated 09/2024.
 constexpr char kContentSettingsWindowLastTabIndex[] =
     "content_settings_window.last_tab_index";
@@ -570,11 +563,6 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   // `kIOSDockingPromoForEligibleUsersOnly` is enabled).
   registry->RegisterBooleanPref(prefs::kIosDockingPromoEligibilityMet, false);
 
-  // Pref related to the Enhanced Safe Browsing Opt-in with new friendlier
-  // settings UI on chrome://settings/security.
-  registry->RegisterBooleanPref(kSafeBrowsingEsbOptInWithFriendlierSettings,
-                                false);
-
   registry->RegisterTimePref(prefs::kLensLastOpened, base::Time());
 
   // Register pref used to determine if OS Lockdown Mode is enabled.
@@ -629,9 +617,6 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kMultiProfileForcedMigrationDone, false);
 
   registry->RegisterTimePref(prefs::kNextSSORecallTime, base::Time());
-
-  // Deprecated 08/2024.
-  registry->RegisterIntegerPref(kTrialPrefName, 0);
 
   // Deprecated 09/2024.
   registry->RegisterBooleanPref(kBrowsingDataMigrationHasBeenPossible, false);
@@ -1132,12 +1117,6 @@ void MigrateObsoleteLocalStatePrefs(PrefService* prefs) {
   // This function is not allowed to block.
   base::ScopedDisallowBlocking disallow_blocking;
 
-  // Added 08/2024.
-  prefs->ClearPref(kTrialPrefName);
-
-  // Added 08/2024.
-  prefs->ClearPref(kSafeBrowsingEsbOptInWithFriendlierSettings);
-
   // Added 09/2024.
   prefs->ClearPref(kBrowsingDataMigrationHasBeenPossible);
 
@@ -1338,9 +1317,6 @@ void MigrateObsoleteProfilePrefs(PrefService* prefs) {
 
 void MigrateObsoleteUserDefault() {
   NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-
-  // Added 08/2024.
-  [defaults removeObjectForKey:@"userHasInteractedWithWhatsNew"];
 
   // Added 11/2024.
   [defaults removeObjectForKey:@"DisplaySwitchProfile"];
