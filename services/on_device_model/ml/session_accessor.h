354e42a537899353d3d130ed09c094c39c14b8de
Brad Triebwasser <btriebw@chromium.org>
2025-07-31 19:23:53 -0700

    Add ASR Streaming functions to OnDevice Model Session.
    Adds ASR streaming support to an on-device session. Supports
    streaming audio chunks and receiving transcription results.
    
    Change-Id: Ibd8a8ead3cda9e552ea9397a82200876bee9750d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6725434
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495214}

--- services/on_device_model/ml/session_accessor.h
+++ services/on_device_model/ml/session_accessor.h
@@ -47,6 +47,9 @@ class COMPONENT_EXPORT(ON_DEVICE_MODEL_ML) SessionAccessor {
                                 ChromeMLGetProbabilitiesBlockingFn get_prob_fn);
   void SizeInTokens(on_device_model::mojom::InputPtr input,
                     ChromeMLSizeInTokensFn size_in_tokens_fn);
+  void CreateAsrStream(on_device_model::mojom::AsrStreamOptionsPtr options,
+                       const ChromeMLASRStreamOutputFn output_fn);
+  void AsrAddAudioChunk(on_device_model::mojom::AudioDataPtr data);
 
  private:
   class Canceler;
@@ -74,11 +77,16 @@ class COMPONENT_EXPORT(ON_DEVICE_MODEL_ML) SessionAccessor {
       ChromeMLGetProbabilitiesBlockingFn get_prob_fn);
   void SizeInTokensInternal(on_device_model::mojom::InputPtr input,
                             ChromeMLSizeInTokensFn size_in_tokens_fn);
+  void CreateAsrStreamInternal(
+      on_device_model::mojom::AsrStreamOptionsPtr asr_options,
+      const ChromeMLASRStreamOutputFn output_fn);
+  void AsrAddAudioChunkInternal(on_device_model::mojom::AudioDataPtr data);
 
   const raw_ref<const ChromeML> chrome_ml_;
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
   ChromeMLModel model_;
   ChromeMLSession session_ = 0;
+  ChromeMLASRStream asr_stream_ = 0;
 };
 
 }  // namespace ml
