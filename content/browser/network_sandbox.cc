bf31a2a1b18fe685212c67c93c482128c1042c32
Will Harris <wfh@chromium.org>
2025-08-01 03:51:47 -0700

    [Win] Grant access to parent directory of cache dir
    In order to support rename/delete operations of the cache directory the
    LPAC ACLs must be emplaced to the parent directory.
    
    This unfortunately regressed in 2022 so this CL fixes the glitch.
    
    The existing browser test is augmented to check the cache size after a
    reset. It should be 0, but before this CL it was returning ERR_FAILED.
    This verifies the fix should not regress in the future.
    
    The CacheResetFailure test which is only run on POSIX does expect a
    ERR_FAILED so to check this it is now passed as a parameter to the test
    helper function.
    
    BUG=435126164
    
    Change-Id: Id06307cfa550c0e5146c3907ec3751cad995abe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6801147
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495339}

--- content/browser/network_sandbox.cc
+++ content/browser/network_sandbox.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/win/security_util.h"
 #include "base/win/sid.h"
+#include "content/browser/network_service_instance_impl.h"
 #include "content/common/features.h"
 #include "sandbox/policy/features.h"
 #endif  // BUILDFLAG(IS_WIN)
@@ -235,15 +236,22 @@ bool MaybeGrantAccessToDataPath(const SandboxParameters& sandbox_params,
   static constexpr DWORD kInheritance =
       CONTAINER_INHERIT_ACE | OBJECT_INHERIT_ACE;
 
+  // For cache dir only, ACL the parent above the "Cache_Data" directory to
+  // ensure that rename/delete operations can take place correctly.
+  base::FilePath directory_to_acl = directory->path();
+
+  if (directory_to_acl.BaseName() == base::FilePath(kCacheDataDirectoryName)) {
+    directory_to_acl = directory_to_acl.DirName();
+  }
   // If LPAC capability already has access to the directory then avoid
   // granting access again. This is a performance optimization.
-  if (HasAccessToPath(directory->path(), ac_sids, kAccessMask, kInheritance)) {
+  if (HasAccessToPath(directory_to_acl, ac_sids, kAccessMask, kInheritance)) {
     return true;
   }
 
   // Grant recursive access to directory. This also means new files in the
   // directory will inherit the ACE.
-  return base::win::GrantAccessToPath(directory->path(), ac_sids, kAccessMask,
+  return base::win::GrantAccessToPath(directory_to_acl, ac_sids, kAccessMask,
                                       kInheritance, /*recursive=*/true);
 #else
   if (directory->IsOpenForTransferRequired()) {
--- content/browser/network_service_instance_impl.h
+++ content/browser/network_service_instance_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/command_line.h"
+#include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -18,6 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_context.mojom-forward.h"
 
 namespace content {
+
+// A directory name that is created below the http cache path and passed to the
+// network context when creating a network context with cache enabled.
+// This must be a directory below the main cache path so operations such as
+// resetting the cache via HttpCacheParams.reset_cache can function correctly
+// as they rely on having access to the parent directory of the cache.
+inline constexpr base::FilePath::CharType kCacheDataDirectoryName[] =
+    FILE_PATH_LITERAL("Cache_Data");
 
 // Creates the network::NetworkService object on the IO thread directly instead
 // of trying to go through the ServiceManager.
