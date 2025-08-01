03da993d1c7ebee3c86a3af562d81b15304a1f85
Verina Armanyous <averina@google.com>
2025-08-01 13:40:55 -0700

    [Save and Fill] Add pending state throbber
    This CL introduces a pending state with a throbber shown during the
    pre-flight response fetching. Upon result, the dialog transitions to
    either a local version (on preflight failure) or a server dialog (on
    preflight success).
    
    Mock: https://screenshot.googleplex.com/9mtoeZRBGM8Cj6L.png
    w/o the GPay logo
    Implementation (when preflight fails):
    http://screencast/cast/NTA2NzA4NzgwMTIyMTEyMHwxZTlhNDBjYy00ZQ
    Implementation (when preflight succeeds):
    http://screencast/cast/NjM0NDk4NTU2MDQ4MTc5MnxkMmM4ZGUzYy05YQ
    
    Design doc: go/payments-autofill-save-and-fill-desktop-design
    
    Bug: 378164165
    Change-Id: I3d44efb672e87ccce030176c96d94b66c58d9665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6794465
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495716}

--- chrome/browser/ui/views/autofill/payments/save_and_fill_dialog.h
+++ chrome/browser/ui/views/autofill/payments/save_and_fill_dialog.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_autofill_client.h"
 #include "components/autofill/core/browser/ui/payments/save_and_fill_dialog_view.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
+#include "ui/views/controls/throbber.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/window/dialog_delegate.h"
 
@@ -55,6 +56,10 @@ class SaveAndFillDialog : public views::DialogDelegateView,
   // Create a view with a legal message.
   std::unique_ptr<views::View> CreateLegalMessageView();
 
+  void CreateMainContentView();
+  void CreatePendingView();
+  void ToggleThrobberVisibility(bool visible);
+
   base::WeakPtr<SaveAndFillDialogController> controller_;
   base::RepeatingCallback<void(const GURL&)> on_legal_message_link_clicked_;
 
@@ -66,6 +71,11 @@ class SaveAndFillDialog : public views::DialogDelegateView,
   LabeledTextfieldWithErrorMessage cvc_data_;
   LabeledTextfieldWithErrorMessage expiration_date_data_;
   LabeledTextfieldWithErrorMessage name_on_card_data_;
+
+  raw_ptr<views::View> container_view_ = nullptr;
+  raw_ptr<views::BoxLayoutView> main_view_ = nullptr;
+  raw_ptr<views::BoxLayoutView> pending_view_ = nullptr;
+  raw_ptr<views::Throbber> throbber_ = nullptr;
 };
 
 }  // namespace autofill
--- components/autofill/core/browser/ui/payments/save_and_fill_dialog_controller_impl_unittest.cc
+++ components/autofill/core/browser/ui/payments/save_and_fill_dialog_controller_impl_unittest.cc
@@ -21,8 +21,8 @@ class SaveAndFillDialogControllerImplTest : public testing::Test {
   SaveAndFillDialogControllerImplTest() = default;
   ~SaveAndFillDialogControllerImplTest() override = default;
 
-  void SetIsUploadSaveAndFill(bool is_upload) {
-    controller_->is_upload_save_and_fill_ = is_upload;
+  void SetDialogState(SaveAndFillDialogState dialog_state) {
+    controller_->dialog_state_ = dialog_state;
   }
 
   void SetUp() override {
@@ -80,7 +80,7 @@ TEST_F(SaveAndFillDialogControllerImplTest, CorrectStringsAreReturned) {
   EXPECT_EQ(controller()->GetWindowTitle(),
             l10n_util::GetStringUTF16(IDS_AUTOFILL_SAVE_AND_FILL_DIALOG_TITLE));
 
-  SetIsUploadSaveAndFill(false);
+  SetDialogState(SaveAndFillDialogState::kLocalDialog);
   EXPECT_EQ(controller()->GetExplanatoryMessage(),
             l10n_util::GetStringUTF16(
                 IDS_AUTOFILL_SAVE_AND_FILL_DIALOG_EXPLANATION_LOCAL));
@@ -110,7 +110,7 @@ TEST_F(SaveAndFillDialogControllerImplTest, CorrectStringsAreReturned) {
                 IDS_AUTOFILL_SAVE_AND_FILL_DIALOG_INVALID_CARD_NUMBER));
 
   // Test for upload Save and Fill explanatory message.
-  SetIsUploadSaveAndFill(true);
+  SetDialogState(SaveAndFillDialogState::kUploadDialog);
   EXPECT_EQ(controller()->GetExplanatoryMessage(),
             l10n_util::GetStringUTF16(
                 IDS_AUTOFILL_SAVE_AND_FILL_DIALOG_EXPLANATION_UPLOAD));
