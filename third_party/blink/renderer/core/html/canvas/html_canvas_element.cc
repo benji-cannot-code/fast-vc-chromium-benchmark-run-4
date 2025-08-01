24bade2c878570ab8a57d098731be5761adbc477
Philip Rogers <pdr@chromium.org>
2025-08-01 14:48:56 -0700

    [html-in-canvas] Rename IsInCanvasSubtree -> IsCanvasOrInCanvasSubtree
    This patch updates the name of an element flag to make it clear that
    the flag is set for both canvas element and descendants. A simple unit
    test has been added for the behavior.
    
    Bug: 435609887
    Change-Id: I6a1befed49d530bef7577b2c609f9491f00789a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6812055
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495756}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -478,7 +478,7 @@ LayoutObject* HTMLCanvasElement::CreateLayoutObject(
 
 Node::InsertionNotificationRequest HTMLCanvasElement::InsertedInto(
     ContainerNode& node) {
-  SetIsInCanvasSubtree(true);
+  SetIsCanvasOrInCanvasSubtree(true);
   ColorSchemeMayHaveChanged();
   return HTMLElement::InsertedInto(node);
 }
