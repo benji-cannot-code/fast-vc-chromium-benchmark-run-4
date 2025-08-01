ae3b083bb98c17b358e5890e8abcd99951419c43
Nohemi Fernandez <fernandex@google.com>
2025-08-01 02:56:57 -0700

    [iOS] Fix crash in Overflow menu when the Reading Mode is unavailable.
    Updates the check in the overflow menu to remove the assumption that
    the ReaderModeTabHelper is present when the action toggle is activated
    and removes the Reading Mode from customizable actions when the feature
    is disabled.
    
    Fixed: 434171862
    Change-Id: I0b28a80b3997c6edb756cbb1ec026ba25ec45dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6802464
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495320}

--- ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_orderer.mm
+++ ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_orderer.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_action_provider.h"
 #import "ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_metrics.h"
 #import "ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_swift.h"
+#import "ios/chrome/browser/reader_mode/model/features.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_backed_boolean.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/public/features/system_flags.h"
@@ -602,6 +603,12 @@ base::Value::Dict DictFromBadgeData(const BadgeData badgeData) {
       if (actionsSet.contains(*actionType)) {
         continue;
       }
+      if (*actionType == overflow_menu::ActionType::ReaderMode &&
+          !IsReaderModeAvailable()) {
+        // Reader mode may have been disabled since the last update, if so do
+        // not add it to `actionsSet`.
+        continue;
+      }
 
       actionsSet.insert(*actionType);
       actionOrderData.shownActions.push_back(*actionType);
@@ -621,6 +628,12 @@ base::Value::Dict DictFromBadgeData(const BadgeData badgeData) {
         continue;
       }
       if (actionsSet.contains(*actionType)) {
+        continue;
+      }
+      if (*actionType == overflow_menu::ActionType::ReaderMode &&
+          !IsReaderModeAvailable()) {
+        // Reader mode may have been disabled since the last update, if so do
+        // not add it to `actionsSet`.
         continue;
       }
 
