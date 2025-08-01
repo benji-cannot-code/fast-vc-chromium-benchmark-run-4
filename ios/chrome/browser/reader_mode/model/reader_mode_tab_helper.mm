45bfb98df64918ef9892f6663062814256538238
Quentin Pubert <qpubert@google.com>
2025-08-01 08:44:19 -0700

    [iOS] Make Reader mode availability in AI hub consistent with tools menu
    Reader mode should be available in the AI hub according to the same rule
    used to determine availability in the tools menu. This CL ensures it is
    the case.
    
    This change also renames:
    - `CurrentPageSupportsReaderMode()`
      to `CurrentPageIsDistillable()`
    - `FetchLastCommittedUrlEligibilityResult()`
      to `FetchLastCommittedUrlDistillabilityResult()`
    in `ReaderModeTabHelper`, as the naming is currently confusing.
    
    Bug: 435578403
    Change-Id: Idcbcc0f482199192a6a2bac6db5beba288e80dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6811045
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495478}

--- ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.mm
+++ ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.mm
@@ -151,7 +151,7 @@ bool ReaderModeTabHelper::CurrentPageIsEligibleForReaderMode() const {
          !kGoogleWorkspaceBlocklist.contains(current_url.host_piece());
 }
 
-bool ReaderModeTabHelper::CurrentPageSupportsReaderMode() const {
+bool ReaderModeTabHelper::CurrentPageIsDistillable() const {
   return CurrentPageIsEligibleForReaderMode() &&
          last_committed_url_eligibility_ready_ &&
          last_committed_url_without_ref_.is_valid() &&
@@ -170,10 +170,10 @@ void ReaderModeTabHelper::RecordDistillationFailure() {
   }
 }
 
-void ReaderModeTabHelper::FetchLastCommittedUrlEligibilityResult(
+void ReaderModeTabHelper::FetchLastCommittedUrlDistillabilityResult(
     base::OnceCallback<void(std::optional<bool>)> callback) {
   if (last_committed_url_eligibility_ready_) {
-    std::move(callback).Run(CurrentPageSupportsReaderMode());
+    std::move(callback).Run(CurrentPageIsDistillable());
     return;
   }
   last_committed_url_eligibility_callbacks_.push_back(std::move(callback));
@@ -332,7 +332,7 @@ void ReaderModeTabHelper::HandleReaderModeHeuristicResult(
       result == ReaderModeHeuristicResult::kReaderModeEligible ? url : GURL();
   if (last_committed_url_without_ref_.EqualsIgnoringRef(url)) {
     last_committed_url_eligibility_ready_ = true;
-    CallLastCommittedUrlEligibilityCallbacks(CurrentPageSupportsReaderMode());
+    CallLastCommittedUrlEligibilityCallbacks(CurrentPageIsDistillable());
   }
 }
 
--- ios/chrome/browser/reader_mode/model/reader_mode_tab_helper_unittest.mm
+++ ios/chrome/browser/reader_mode/model/reader_mode_tab_helper_unittest.mm
@@ -252,7 +252,7 @@ TEST_F(ReaderModeTabHelperTest, ReaderModeNotEligibleOnNtp) {
   LoadWebpage(web_state(), ntp_url);
   WaitForPageLoadDelayAndRunUntilIdle();
 
-  ASSERT_FALSE(reader_mode_tab_helper()->CurrentPageSupportsReaderMode());
+  ASSERT_FALSE(reader_mode_tab_helper()->CurrentPageIsDistillable());
 }
 
 // Tests that reader mode is eligible on a regular page.
@@ -281,7 +281,7 @@ TEST_F(ReaderModeTabHelperTest, ReaderModeNotEligibleOnNonHTML) {
   web_state()->SetContentIsHTML(false);
   WaitForPageLoadDelayAndRunUntilIdle();
 
-  ASSERT_FALSE(reader_mode_tab_helper()->CurrentPageSupportsReaderMode());
+  ASSERT_FALSE(reader_mode_tab_helper()->CurrentPageIsDistillable());
 }
 
 // Tests that reader mode page eligibility supports same-page navigations.
@@ -303,13 +303,13 @@ TEST_F(ReaderModeTabHelperTest, ReaderModeEligibleForSamePageNavigation) {
                                     @"<html><body>Content</body></html>");
   web_state()->OnNavigationFinished(&navigation_context);
 
-  ASSERT_TRUE(reader_mode_tab_helper()->CurrentPageSupportsReaderMode());
+  ASSERT_TRUE(reader_mode_tab_helper()->CurrentPageIsDistillable());
 }
 
 // Tests that
-// ReaderModeTabHelper::FetchLastCommittedUrlEligibilityResult calls
+// ReaderModeTabHelper::FetchLastCommittedUrlDistillabilityResult calls
 // its completion once the page Reader mode eligibility has been determined.
-TEST_F(ReaderModeTabHelperTest, FetchLastCommittedUrlEligibilityResult) {
+TEST_F(ReaderModeTabHelperTest, FetchLastCommittedUrlDistillabilityResult) {
   GURL test_url("https://test.url/ref");
   SetReaderModeState(web_state(), test_url,
                      ReaderModeHeuristicResult::kReaderModeEligible, "");
@@ -317,7 +317,7 @@ TEST_F(ReaderModeTabHelperTest, FetchLastCommittedUrlEligibilityResult) {
   LoadWebpage(web_state(), test_url);
   __block std::optional<bool>
       current_page_supports_reader_mode_completion_result;
-  reader_mode_tab_helper()->FetchLastCommittedUrlEligibilityResult(
+  reader_mode_tab_helper()->FetchLastCommittedUrlDistillabilityResult(
       base::BindOnce(^(std::optional<bool> current_page_supports_reader_mode) {
         current_page_supports_reader_mode_completion_result =
             std::move(current_page_supports_reader_mode);
@@ -598,7 +598,7 @@ TEST_P(ReaderModeTabHelperWithEligibilityTest, TriggerHeuristicOnPageLoad) {
   WaitForPageLoadDelayAndRunUntilIdle();
 
   ASSERT_EQ(eligibility == ReaderModeHeuristicResult::kReaderModeEligible,
-            reader_mode_tab_helper()->CurrentPageSupportsReaderMode());
+            reader_mode_tab_helper()->CurrentPageIsDistillable());
 
   // The metrics for the navigation are recorded.
   FlushMetrics();
@@ -641,7 +641,7 @@ TEST_P(ReaderModeTabHelperWithEligibilityTest,
   WaitForPageLoadDelayAndRunUntilIdle();
 
   ASSERT_EQ(eligibility == ReaderModeHeuristicResult::kReaderModeEligible,
-            reader_mode_tab_helper()->CurrentPageSupportsReaderMode());
+            reader_mode_tab_helper()->CurrentPageIsDistillable());
 
   FlushMetrics();
   EXPECT_THAT(histogram_tester_.GetAllSamples(kReaderModeStateHistogram),
