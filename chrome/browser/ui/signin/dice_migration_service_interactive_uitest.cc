d20c69a96dd97c6070661b12ceb38e38d495d45b
Ankush Singh <ankushkush@google.com>
2025-08-01 07:52:25 -0700

    Add metrics to track count user migration and toast triggers
    This CL adds the following metrics:
    1. Signin.DiceMigrationDialog.Migrated: Upon user accepting the migration dialog, tracks whether the user was successfully migrated.
    2. Signin.DiceMigrationDialog.ToastTriggered: Upon a successful migration, tracks whether the toast was triggered.
    
    Bug: 399838468
    Change-Id: I74558790b343b0c1095f655d6747fe7135f34d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6811266
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495444}

--- chrome/browser/ui/signin/dice_migration_service_interactive_uitest.cc
+++ chrome/browser/ui/signin/dice_migration_service_interactive_uitest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_browser_test_base.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/signin/dice_migration_service.h"
 #include "chrome/browser/ui/signin/dice_migration_service_factory.h"
+#include "chrome/browser/ui/toasts/api/toast_id.h"
 #include "chrome/browser/ui/toasts/toast_controller.h"
 #include "chrome/browser/ui/toasts/toast_view.h"
 #include "chrome/browser/ui/toolbar/app_menu_model.h"
@@ -31,6 +33,14 @@ constexpr char kTestEmail[] = "test@gmail.com";
 
 constexpr char kDiceMigrationDialogCloseReasonHistogram[] =
     "Signin.DiceMigrationDialog.CloseReason";
+constexpr char kToastTriggeredHistogram[] =
+    "Signin.DiceMigrationDialog.ToastTriggered";
+constexpr char kToastTriggerToShowHistogram[] = "Toast.TriggeredToShow";
+constexpr char kToastDismissedHistogram[] = "Toast.DiceUserMigrated.Dismissed";
+constexpr char kToastActionButtonUserAction[] =
+    "Toast.ActionButtonClicked.DiceUserMigrated";
+constexpr char kToastCloseButtonUserAction[] =
+    "Toast.CloseButtonClicked.DiceUserMigrated";
 
 // Utility macro to implicitly sign in the user in a PRE test.
 // NOTE: `test_suite` must be a subclass of
@@ -97,6 +107,7 @@ class DiceMigrationServiceInteractiveUiTest : public InteractiveBrowserTest {
   const base::test::ScopedFeatureList scoped_feature_list_{
       switches::kOfferMigrationToDiceUsers};
   base::HistogramTester histogram_tester_;
+  base::UserActionTester user_action_tester_;
 };
 
 DICE_MIGRATION_TEST_F(DiceMigrationServiceInteractiveUiTest,
@@ -346,6 +357,10 @@ DICE_MIGRATION_TEST_F(DiceMigrationServiceInteractiveUiTest, ShowToast) {
                   // The toast should auto dismiss when the timer goes off.
                   FireToastCloseTimer(),
                   WaitForHide(toasts::ToastView::kToastViewId));
+
+  histogram_tester_.ExpectUniqueSample(kToastTriggeredHistogram, true, 1);
+  histogram_tester_.ExpectUniqueSample(kToastTriggerToShowHistogram,
+                                       ToastId::kDiceUserMigrated, 1);
 }
 
 DICE_MIGRATION_TEST_F(DiceMigrationServiceInteractiveUiTest,
@@ -378,6 +393,11 @@ DICE_MIGRATION_TEST_F(DiceMigrationServiceInteractiveUiTest,
           kActiveTab, chrome::GetSettingsUrl(chrome::kSyncSetupSubPage)),
 
       WaitForHide(toasts::ToastView::kToastViewId));
+
+  histogram_tester_.ExpectUniqueSample(
+      kToastDismissedHistogram, toasts::ToastCloseReason::kActionButton, 1);
+  EXPECT_EQ(user_action_tester_.GetActionCount(kToastActionButtonUserAction),
+            1);
 }
 
 DICE_MIGRATION_TEST_F(DiceMigrationServiceInteractiveUiTest, ToastCloseButton) {
@@ -401,6 +421,10 @@ DICE_MIGRATION_TEST_F(DiceMigrationServiceInteractiveUiTest, ToastCloseButton) {
                   PressButton(toasts::ToastView::kToastCloseButton),
 
                   WaitForHide(toasts::ToastView::kToastViewId));
+
+  histogram_tester_.ExpectUniqueSample(
+      kToastDismissedHistogram, toasts::ToastCloseReason::kCloseButton, 1);
+  EXPECT_EQ(user_action_tester_.GetActionCount(kToastCloseButtonUserAction), 1);
 }
 
 DICE_MIGRATION_TEST_F(DiceMigrationServiceInteractiveUiTest,
