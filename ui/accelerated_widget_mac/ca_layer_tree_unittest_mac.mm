a629537d8d9e8f509159b23749c39d6776aa32e1
Christopher Cameron <ccameron@chromium.org>
2025-07-31 23:50:19 -0700

    metal::HDRCopierLayer: Enable for all HDR layers
    There was a check that only enabled HDR for SRGB_HDR, and then a recent
    change (crrev.com/1493694) caused us to use the space CUSTOM_HDR.
    
    Ideally, we should not use HDRCopierLayer, because we should be able to
    make a CALayer with an IOSurface be HDR (it appears that the beta
    preferredDynamicRange API might do this), without copying its contents
    to a CAMetalLayer. Beyond that, we shouldn't indicate HDR-ness via
    gfx::ColorSpace.
    
    Add tests for this.
    
    Bug: 40268540
    Change-Id: I85dfad544abe80217fb309e8bec46bad8955dd0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6806965
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495259}

--- ui/accelerated_widget_mac/ca_layer_tree_unittest_mac.mm
+++ ui/accelerated_widget_mac/ca_layer_tree_unittest_mac.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/test/scoped_feature_list.h"
+#include "components/metal_util/hdr_copier_layer.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
@@ -1148,6 +1149,32 @@ TEST_F(CALayerTreeTest, HDRTrigger) {
 #pragma clang diagnostic ignored "-Wunguarded-availability-new"
   EXPECT_TRUE([content_layer wantsExtendedDynamicRangeContent]);
 #pragma clang diagnostic pop
+}
+
+TEST(HDRCoperLayerTest, Formats) {
+  gfx::HDRMetadata metadata_empty;
+  auto io_surface_8888 =
+      gfx::CreateIOSurface(gfx::Size(256, 256), gfx::BufferFormat::BGRA_8888);
+  auto io_surface_f16 =
+      gfx::CreateIOSurface(gfx::Size(256, 256), gfx::BufferFormat::RGBA_F16);
+  auto cs_extended_linear = gfx::ColorSpace(
+      gfx::ColorSpace::PrimaryID::P3, gfx::ColorSpace::TransferID::LINEAR_HDR);
+  auto cs_g22 = gfx::ColorSpace(gfx::ColorSpace::PrimaryID::BT709,
+                                gfx::ColorSpace::TransferID::GAMMA22);
+  auto cs_g22_hdr = cs_g22.GetAsHDR();
+
+  EXPECT_TRUE(metal::ShouldUseHDRCopier(io_surface_f16.get(), metadata_empty,
+                                        cs_extended_linear));
+  EXPECT_TRUE(metal::ShouldUseHDRCopier(io_surface_f16.get(), metadata_empty,
+                                        cs_g22_hdr));
+  EXPECT_FALSE(
+      metal::ShouldUseHDRCopier(io_surface_f16.get(), metadata_empty, cs_g22));
+  EXPECT_FALSE(metal::ShouldUseHDRCopier(io_surface_8888.get(), metadata_empty,
+                                         cs_extended_linear));
+  EXPECT_FALSE(metal::ShouldUseHDRCopier(io_surface_8888.get(), metadata_empty,
+                                         cs_g22_hdr));
+  EXPECT_FALSE(
+      metal::ShouldUseHDRCopier(io_surface_8888.get(), metadata_empty, cs_g22));
 }
 
 }  // namespace gpu
