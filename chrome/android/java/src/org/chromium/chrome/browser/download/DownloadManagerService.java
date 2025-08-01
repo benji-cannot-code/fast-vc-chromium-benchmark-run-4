4e6d624e649f547f985c3e0cc5293dbdc775734d
Shu Yang <shuyng@google.com>
2025-08-01 13:22:02 -0700

    Add @NullMarked to chrome/android/j/s/o/c/chrome/browser/download part6
    Bug: 389129271
    Change-Id: Ib09234dd0e3d13218c9493d8b360cea5cd10281d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804745
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495700}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -953,7 +953,7 @@ public class DownloadManagerService implements DownloadServiceDelegate, ProfileM
      * @param mimeType MIME type of the file.
      * @return Whether the download is openable by the browser.
      */
-    public boolean isDownloadOpenableInBrowser(String mimeType) {
+    public boolean isDownloadOpenableInBrowser(@Nullable String mimeType) {
         // TODO(qinmin): for audio and video, check if the codec is supported by Chrome.
         return isSupportedMimeType(mimeType);
     }
@@ -1324,7 +1324,7 @@ public class DownloadManagerService implements DownloadServiceDelegate, ProfileM
         DownloadInfo downloadInfo = downloadItem.getDownloadInfo();
         boolean canOpen =
                 DownloadUtils.openFile(
-                        downloadInfo.getFilePath(),
+                        assertNonNull(downloadInfo.getFilePath()),
                         downloadInfo.getMimeType(),
                         downloadInfo.getDownloadGuid(),
                         downloadInfo.getOtrProfileId(),
