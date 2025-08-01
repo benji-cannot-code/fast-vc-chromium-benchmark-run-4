019059b615e89785db57e8b987e5883396b2a9fd
Vidhan Jain <vidhanj@google.com>
2025-08-01 09:49:02 -0700

    Add chrome://flags for AutofillShowBubblesBasedOnPriorities
    Bug: 432429605
    Change-Id: Ie8da2297a5a0a5043ba113f4d98108869de8d510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6811232
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495539}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -578,6 +578,9 @@ extern const char kAutofillSyncEwalletAccountsDescription[];
 extern const char kAutofillPaymentsFieldSwappingName[];
 extern const char kAutofillPaymentsFieldSwappingDescription[];
 
+extern const char kAutofillShowBubblesBasedOnPrioritiesName[];
+extern const char kAutofillShowBubblesBasedOnPrioritiesDescription[];
+
 extern const char kAutofillUnmaskCardRequestTimeoutName[];
 extern const char kAutofillUnmaskCardRequestTimeoutDescription[];
 
