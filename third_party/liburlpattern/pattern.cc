6a649fe8c9e92b3479f0ccf6a51dd8774a6ab290
Tom Sepez <tsepez@chromium.org>
2025-08-01 10:09:30 -0700

    Enforce unsafe buffer usage on liburlpattern and jni_zero
    Add these "first party" third-party directories to the list
    enforced by unsafe_buffers_paths.txt and suppress on a per-file
    basis as needed.
    
    Bug: 393091624
    Change-Id: I38f3b93cc8a5e99e61abf95576ceda353348779e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6216799
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495559}

--- third_party/liburlpattern/pattern.cc
+++ third_party/liburlpattern/pattern.cc
@@ -4,6 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/393091624): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/liburlpattern/pattern.h"
 
 #include <algorithm>
