7caa033e29d789755c7fce891d38a6306d3c20be
adamta <adamta@google.com>
2025-08-01 10:53:11 -0700

    [iOS] Disable Lens in AI Hub for non-Google DSE
    Checks the default search engine and disables the Lens entry point in
    the AI Hub if it's not Google.
    
    Also updates the background color of the AI Hub small buttons as per
    new UX reqs.
    
    Fixed: 435687403
    Change-Id: I417378c454f13707a82cb145f5702d5c02bfd825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6812723
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495583}

--- ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_coordinator.mm
+++ ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_coordinator.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_coordinator.h"
 
+#import "components/search/search.h"
+#import "components/search_engines/template_url_service.h"
 #import "ios/chrome/browser/dom_distiller/model/distiller_service_factory.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_service.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_service_factory.h"
@@ -19,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.h"
 #import "ios/chrome/browser/reader_mode/ui/reader_mode_options_controls_view.h"
 #import "ios/chrome/browser/reader_mode/ui/reader_mode_options_view_controller.h"
+#import "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
@@ -93,7 +96,9 @@ const CGFloat kMenuCornerRadius = 20;
         HandlerForProtocol(self.browser->GetCommandDispatcher(), BWGCommands);
   }
 
-  if (IsLensOverlayAvailable(self.profile->GetPrefs())) {
+  if (IsLensOverlayAvailable(self.profile->GetPrefs()) &&
+      search::DefaultSearchProviderIsGoogle(
+          ios::TemplateURLServiceFactory::GetForProfile(self.profile))) {
     _viewController.lensOverlayHandler = HandlerForProtocol(
         self.browser->GetCommandDispatcher(), LensOverlayCommands);
   }
