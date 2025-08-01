cf0b9d562a59536a1ab687ea3f72fbde723f22a1
Chidera Olibie <colibie@google.com>
2025-08-01 02:45:30 -0700

    [wv][nit] Rename UI thread startup mode constants
    This is to better fit into the AndroidX API structure
    as requested by their API council in
    https://r.android.com/3703515.
    
    Bug: 397372092
    Change-Id: I8ec464f0041a5e152b46aa4edb5c2c0c9a5eca9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6807150
    Auto-Submit: Chidera Olibie <colibie@google.com>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495317}

--- android_webview/glue/java/src/com/android/webview/chromium/AndroidXProcessGlobalConfig.java
+++ android_webview/glue/java/src/com/android/webview/chromium/AndroidXProcessGlobalConfig.java
@@ -32,7 +32,7 @@ public final class AndroidXProcessGlobalConfig {
     private String mDataDirectoryBasePath;
     private String mCacheDirectoryBasePath;
     private Boolean mPartitionedCookiesEnabled;
-    private Integer mUiThreadStartupMode = ProcessGlobalConfigConstants.DEFAULT_UI_THREAD_STARTUP;
+    private int mUiThreadStartupMode = ProcessGlobalConfigConstants.UI_THREAD_STARTUP_MODE_DEFAULT;
     private static AndroidXProcessGlobalConfig sGlobalConfig;
 
     private AndroidXProcessGlobalConfig(@NonNull Map<String, Object> configMap) {
@@ -82,7 +82,7 @@ public final class AndroidXProcessGlobalConfig {
                                         + entry.getKey());
                     }
 
-                    mUiThreadStartupMode = (Integer) configValue;
+                    mUiThreadStartupMode = (int) configValue;
                     break;
                 default:
                     throw new RuntimeException(
@@ -142,7 +142,7 @@ public final class AndroidXProcessGlobalConfig {
         return mPartitionedCookiesEnabled;
     }
 
-    public Integer getUiThreadStartupMode() {
+    public int getUiThreadStartupMode() {
         return mUiThreadStartupMode;
     }
 }
