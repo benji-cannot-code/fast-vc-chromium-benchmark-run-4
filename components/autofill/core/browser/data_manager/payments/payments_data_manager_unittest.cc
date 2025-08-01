b9d29e371be627a65d3179be606046f28d3285bb
Yishui Liu <yishuil@google.com>
2025-08-01 15:20:31 -0700

    [BNPL] Rename feature flags
    This CL rename 'AutofillEnableBuyNowPayLaterForExternallyLinkedKlarna`
    to `AutofillEnableBuyNowPayLaterForExternallyLinked` to match the usage.
    
    Bug: 424259928, b:424600709
    Change-Id: Ib97f105c65d9b5db3ecc254f545f333b0ffdfc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6793159
    Commit-Queue: Yishui Liu <yishuil@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495781}

--- components/autofill/core/browser/data_manager/payments/payments_data_manager_unittest.cc
+++ components/autofill/core/browser/data_manager/payments/payments_data_manager_unittest.cc
@@ -2557,7 +2557,7 @@ TEST_F(PaymentsDataManagerTest, GetLinkedBnplIssuers_NonUsdPriceRangeRejected) {
 }
 
 // Tests that `action_required` is not set for BNPL issuers if flag
-// `AutofillEnableBuyNowPayLaterForExternallyLinkedKlarna` is disabled.
+// `AutofillEnableBuyNowPayLaterForExternallyLinked` is disabled.
 TEST_F(PaymentsDataManagerTest,
        GetLinkedBnplIssuers_IssuerLinkedExternally_FlagDisabled) {
   base::test::ScopedFeatureList scoped_feature_list;
@@ -2565,7 +2565,7 @@ TEST_F(PaymentsDataManagerTest,
       /*enabled_features=*/{features::kAutofillEnableBuyNowPayLaterSyncing,
                             features::kAutofillEnableBuyNowPayLaterForKlarna},
       /*disabled_features=*/{
-          features::kAutofillEnableBuyNowPayLaterForExternallyLinkedKlarna});
+          features::kAutofillEnableBuyNowPayLaterForExternallyLinked});
   sync_pb::PaymentInstrument payment_instrument =
       test::CreatePaymentInstrumentWithLinkedBnplIssuer(
           /*instrument_id=*/1234L, std::string(kBnplKlarnaIssuerId), "USD",
@@ -2593,14 +2593,14 @@ TEST_F(PaymentsDataManagerTest,
 }
 
 // Tests that `action_required` is set for BNPL issuers if flag
-// `AutofillEnableBuyNowPayLaterForExternallyLinkedKlarna` is enabled.
+// `AutofillEnableBuyNowPayLaterForExternallyLinked` is enabled.
 TEST_F(PaymentsDataManagerTest, GetLinkedBnplIssuers_IssuerLinkedExternally) {
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitWithFeatures(
       /*enabled_features=*/
       {features::kAutofillEnableBuyNowPayLaterSyncing,
        features::kAutofillEnableBuyNowPayLaterForKlarna,
-       features::kAutofillEnableBuyNowPayLaterForExternallyLinkedKlarna},
+       features::kAutofillEnableBuyNowPayLaterForExternallyLinked},
       /*disabled_features=*/{});
   sync_pb::PaymentInstrument payment_instrument =
       test::CreatePaymentInstrumentWithLinkedBnplIssuer(
