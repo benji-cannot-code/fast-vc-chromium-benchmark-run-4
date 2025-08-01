328279f8f7b546ce92ae0a9b70433c53358b018b
Avi Drissman <avi@chromium.org>
2025-08-01 10:42:23 -0700

    Use base::Bytes in content constants
    Bug: 429140103
    Change-Id: I904ce9156a0c7e975ea2005a72b4bdbab90cfcbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6812417
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495575}

--- content/common/content_constants_internal.h
+++ content/common/content_constants_internal.h
@@ -9,14 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/byte_count.h"
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "media/media_buildflags.h"
 
 namespace content {
 
-// The maximum length of string as data url: 20MiB
-inline constexpr size_t kMaxLengthOfDataURLString = 1024 * 1024 * 20;
+// The maximum length of string as data url.
+inline constexpr base::ByteCount kMaxLengthOfDataURLString = base::MiB(20);
 
 // Accept header used for frame requests.
 #if BUILDFLAG(ENABLE_AV1_DECODER)
