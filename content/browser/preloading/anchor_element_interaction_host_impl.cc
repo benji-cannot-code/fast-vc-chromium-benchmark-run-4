edf9cc9c948ecdf4f2382bb1feb7c892d865d2eb
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-08-01 12:28:16 -0700

    Revert "Preloading: Introduce "eager" eagerness heuristic for Desktop"
    This reverts commit b00b2ac5354a3ef83420fd0fe392040071a7a738.
    
    Reason for revert:
    [Gardener] Causes test "PrerenderEagernessBrowserTest.kEager" to fail
    https://crbug.com/435622290
    
    Bug: 40287486, 435622290
    Original change's description:
    > Preloading: Introduce "eager" eagerness heuristic for Desktop
    >
    > This CL introduces mouse hover heuristics for the "eager" eagerness of the Speculation Rules API, enabled by the `kPreloadingEagerHeuristics` flag.
    >
    > After this change, "eager" is no longer an immediate eagerness and has its own mouse hover heuristics to trigger preloading. Additionally, clicking on links also triggers preloading for the "eager" eagerness.
    >
    > The remaining histogram updates will be completed in
    > https://crrev.com/c/6677854.
    >
    > Design Doc: https://docs.google.com/document/d/1YPbtUPfZIDElzBZNx8IQMzRFvy8oauLG_i1XIr6jgTs/edit
    >
    > Bug: 40287486
    > Change-Id: Id2fe755760cd5f64c3cc4b5fa15e815fa0de1ad2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778982
    > Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    > Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    > Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    > Reviewed-by: Domenic Denicola <domenic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1495255}
    
    Bug: 40287486
    Change-Id: If9dfcd0cce4904f7bfe8b28347e211501ee6bb12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813293
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495656}

--- content/browser/preloading/anchor_element_interaction_host_impl.cc
+++ content/browser/preloading/anchor_element_interaction_host_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_client.h"
 #include "content/public/common/origin_util.h"
 #include "third_party/blink/public/common/features.h"
-#include "third_party/blink/public/mojom/speculation_rules/speculation_rules.mojom-data-view.h"
 
 namespace content {
 
@@ -149,10 +148,7 @@ void AnchorElementInteractionHostImpl::OnPointerDown(const GURL& url) {
 void AnchorElementInteractionHostImpl::OnPointerHoverEager(
     const GURL& url,
     blink::mojom::AnchorElementPointerDataPtr mouse_data) {
-  auto* preloading_decider =
-      PreloadingDecider::GetOrCreateForCurrentDocument(&render_frame_host());
-  preloading_decider->OnPointerHover(
-      url, std::move(mouse_data), blink::mojom::SpeculationEagerness::kEager);
+  // TODO(https://crbug.com/40287486): pipe this to PreloadingDecider.
 }
 
 void AnchorElementInteractionHostImpl::OnPointerHoverModerate(
@@ -160,9 +156,7 @@ void AnchorElementInteractionHostImpl::OnPointerHoverModerate(
     blink::mojom::AnchorElementPointerDataPtr mouse_data) {
   auto* preloading_decider =
       PreloadingDecider::GetOrCreateForCurrentDocument(&render_frame_host());
-  preloading_decider->OnPointerHover(
-      url, std::move(mouse_data),
-      blink::mojom::SpeculationEagerness::kModerate);
+  preloading_decider->OnPointerHover(url, std::move(mouse_data));
   MaybePrewarmHttpDiskCache(url, render_frame_host());
   MaybeWarmUpServiceWorkerOnPointerHover(url, render_frame_host());
 }
--- content/browser/preloading/preloading_decider.cc
+++ content/browser/preloading/preloading_decider.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <vector>
 
-#include "base/check_is_test.h"
 #include "base/check_op.h"
 #include "base/containers/enum_set.h"
 #include "base/feature_list.h"
@@ -35,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/preloading/anchor_element_interaction_host.mojom.h"
-#include "third_party/blink/public/mojom/speculation_rules/speculation_rules.mojom-data-view.h"
-#include "third_party/blink/public/mojom/speculation_rules/speculation_rules.mojom-forward.h"
 
 namespace content {
 
@@ -106,14 +103,6 @@ class PreloadingDecider::BehaviorConfig {
 
     pointer_hover_eagerness_ =
         EagernessSet{blink::mojom::SpeculationEagerness::kModerate};
-
-    if (base::FeatureList::IsEnabled(
-            blink::features::kPreloadingEagerHeuristics)) {
-      pointer_down_eagerness_.Put(blink::mojom::SpeculationEagerness::kEager);
-      pointer_hover_eagerness_.Put(blink::mojom::SpeculationEagerness::kEager);
-    }
-
-    CHECK(pointer_down_eagerness_.HasAll(pointer_hover_eagerness_));
 
     static const base::FeatureParam<std::string> kViewportHeuristicEagerness{
         &blink::features::kPreloadingViewportHeuristics,
@@ -306,17 +295,9 @@ void PreloadingDecider::OnPreloadingHeuristicsModelDone(const GURL& url,
 
 void PreloadingDecider::OnPointerHover(
     const GURL& url,
-    blink::mojom::AnchorElementPointerDataPtr mouse_data,
-    blink::mojom::SpeculationEagerness target_eagerness) {
-  // In non-test code, target eagerness must be either "moderate" or "eager".
-  if (target_eagerness != blink::mojom::SpeculationEagerness::kModerate &&
-      target_eagerness != blink::mojom::SpeculationEagerness::kEager) {
-    CHECK_IS_TEST();
-    return;
-  }
-
+    blink::mojom::AnchorElementPointerDataPtr mouse_data) {
   if (observer_for_testing_) {
-    observer_for_testing_->OnPointerHover(url, target_eagerness);
+    observer_for_testing_->OnPointerHover(url);
   }
 
   WebContents* web_contents =
@@ -334,16 +315,9 @@ void PreloadingDecider::OnPointerHover(
   // Preconnecting on hover events should not be done if the link is not safe
   // to prefetch or prerender.
   constexpr bool fallback_to_preconnect = false;
-  // Filter `kModerate` for the "eager" mouse hover to prevent false preloading.
-  EagernessSet eagerness_to_exclude;
-  if (base::FeatureList::IsEnabled(
-          blink::features::kPreloadingEagerHeuristics)) {
-    eagerness_to_exclude = EagernessSet::All();
-    eagerness_to_exclude.Remove(target_eagerness);
-  }
   MaybeEnactCandidate(url, preloading_predictor::kUrlPointerHoverOnAnchor,
                       PreloadingConfidence{100}, fallback_to_preconnect,
-                      eagerness_to_exclude);
+                      /*eagerness_to_exclude=*/{});
 }
 
 void PreloadingDecider::OnViewportHeuristicTriggered(const GURL& url) {
