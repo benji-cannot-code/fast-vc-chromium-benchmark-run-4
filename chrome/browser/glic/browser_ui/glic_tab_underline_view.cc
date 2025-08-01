df7fdcae546642c062d2af063c62e6fbccc77d39
Anthony Cui <cuianthony@google.com>
2025-08-01 11:16:55 -0700

    Re-animate glic tab underlines when new user input is submitted in the client
    Bug: b:431790330
    Change-Id: Ibcb4a5875e42a3f82b2f8b75de047f3328bba023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6807219
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495602}

--- chrome/browser/glic/browser_ui/glic_tab_underline_view.cc
+++ chrome/browser/glic/browser_ui/glic_tab_underline_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
+#include "base/functional/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/glic/browser_ui/theme_util.h"
 #include "chrome/browser/glic/host/context/glic_tab_data.h"
@@ -167,6 +168,12 @@ class GlicTabUnderlineView::UnderlineViewUpdater
 
     // Observe changes in the floaty state.
     glic_service->window_controller().AddStateObserver(this);
+
+    // Subscribe to when new requests are made by glic.
+    user_input_submitted_subscription_ =
+        glic_service->AddUserInputSubmittedCallback(base::BindRepeating(
+            &GlicTabUnderlineView::UnderlineViewUpdater::OnUserInputSubmitted,
+            base::Unretained(this)));
   }
   UnderlineViewUpdater(const UnderlineViewUpdater&) = delete;
   UnderlineViewUpdater& operator=(const UnderlineViewUpdater&) = delete;
@@ -272,6 +279,10 @@ class GlicTabUnderlineView::UnderlineViewUpdater
             : UpdateUnderlineReason::kPanelStateChanged_PanelShowing);
   }
 
+  void OnUserInputSubmitted() {
+    UpdateUnderlineView(UpdateUnderlineReason::kUserInputSubmitted);
+  }
+
  private:
   // Types of updates to the tab underline UI effect given changes in relevant
   // triggering signals, including tab focus, glic sharing controls, pinned tabs
@@ -297,6 +308,8 @@ class GlicTabUnderlineView::UnderlineViewUpdater
     // Events related to the glic panel's state.
     kPanelStateChanged_PanelShowing,
     kPanelStateChanged_PanelHidden,
+
+    kUserInputSubmitted,
   };
 
   GlicKeyedService* GetGlicKeyedService() {
@@ -449,6 +462,11 @@ class GlicTabUnderlineView::UnderlineViewUpdater
           HideUnderline();
         }
         break;
+      case UpdateUnderlineReason::kUserInputSubmitted:
+        if (underline_view_->IsShowing()) {
+          AnimateUnderline();
+        }
+        break;
     }
   }
 
@@ -514,6 +532,8 @@ class GlicTabUnderlineView::UnderlineViewUpdater
         return "PanelShowing";
       case UpdateUnderlineReason::kPanelStateChanged_PanelHidden:
         return "PanelHidden";
+      case UpdateUnderlineReason::kUserInputSubmitted:
+        return "UserInputSubmitted";
     }
   }
 
@@ -544,6 +564,7 @@ class GlicTabUnderlineView::UnderlineViewUpdater
   bool context_access_indicator_enabled_ = false;
   base::CallbackListSubscription indicator_change_subscription_;
   base::CallbackListSubscription pinned_tabs_change_subscription_;
+  base::CallbackListSubscription user_input_submitted_subscription_;
 
   static constexpr size_t kNumReasonsToKeep = 10u;
   std::list<std::string> underline_update_reasons_;
--- chrome/browser/glic/public/glic_keyed_service.h
+++ chrome/browser/glic/public/glic_keyed_service.h
@@ -168,6 +168,14 @@ class GlicKeyedService : public KeyedService {
       const mojom::GetTabContextOptions& context_options,
       glic::mojom::WebClientHandler::ResumeActorTaskCallback callback);
 
+  void OnUserInputSubmitted(glic::mojom::WebClientMode mode);
+
+  // Registers a callback to be called any time user input is submitted in the
+  // client. This is used to update UI effects on tabs that are being shared
+  // with glic.
+  base::CallbackListSubscription AddUserInputSubmittedCallback(
+      base::RepeatingClosure callback);
+
   void CaptureScreenshot(
       glic::mojom::WebClientHandler::CaptureScreenshotCallback callback);
 
@@ -221,6 +229,9 @@ class GlicKeyedService : public KeyedService {
       context_access_indicator_callback_list_;
   // The state of the context access indicator as set by the client.
   bool is_context_access_indicator_enabled_ = false;
+
+  // List of callbacks to be notified when user input has been submitted.
+  base::RepeatingClosureList user_input_submitted_callback_list_;
 
   raw_ptr<Profile> profile_;
 
