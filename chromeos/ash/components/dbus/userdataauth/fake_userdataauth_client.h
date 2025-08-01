20d88d92485de0f6ab06529613c2b3647dcdca25
Xiyuan Xia <xiyuan@chromium.org>
2025-08-01 14:27:14 -0700

    Revert "Add a new dbus method GenerateFreshRecoveryId to rotate recovery id."
    This reverts commit c7711a73ca09966a22d2ed10e21c52f952448fc5.
    
    Reason for revert: 
    Breaks `chromeos-betty-chrome-dchecks`.
    e.g.
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-chrome-dchecks/3419/overview
    
    Fails DCHECK in CryptohomeContext::GetAuthFactorsConfiguration.
    https://source.chromium.org/chromium/chromium/src/+/main:chromeos/ash/components/login/auth/public/user_context.cc;drc=c7711a73ca09966a22d2ed10e21c52f952448fc5;l=143
    
    ```
    2025-08-01T14:42:22.632168Z WARNING chrome[12745:12745]: [ash/login/login_screen_controller.cc:140] crbug.com/1339004 : started authentication
    2025-08-01T14:42:22.633387Z VERBOSE1 chrome[12745:12745]: [chrome/browser/ash/login/existing_user_controller.cc:556] Setting flow from PerformLogin
    2025-08-01T14:42:22.939477Z FATAL chrome[12745:12745]: [chromeos/ash/components/login/auth/public/user_context.cc:143] DCHECK failed: false. AuthFactorsConfiguration has not been set
    ```
    
    Original change's description:
    > Add a new dbus method GenerateFreshRecoveryId to rotate recovery id.
    >
    > This change introduces a new D-Bus method,  to rotate recovery id after
    > user authenticates with recovery factor. This method should only be
    > used after the user directory is mounted.
    >
    > Bug=b:420743396
    >
    > Change-Id: Iada26018cc40c3b07cfa4c816686cc9eaa9659de
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781900
    > Commit-Queue: Mohammed Abdon <mohammedabdon@chromium.org>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1495388}
    
    Bug: b:420743396
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ide95f631068eac4e89c35a3ec520256194d7f938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813459
    Auto-Submit: Xiyuan Xia <xiyuan@chromium.org>
    Owners-Override: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1495740}

--- chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h
+++ chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h
@@ -50,7 +50,6 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeUserDataAuthClient
     kRemove,
     kGetRecoverableKeyStores,
     kLockFactorUntilReboot,
-    kGenerateFreshRecoveryId,
   };
 
   // The method by which a user's home directory can be encrypted.
@@ -302,9 +301,6 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeUserDataAuthClient
   void GetAuthFactorExtendedInfo(
       const ::user_data_auth::GetAuthFactorExtendedInfoRequest& request,
       GetAuthFactorExtendedInfoCallback callback) override;
-  void GenerateFreshRecoveryId(
-      const ::user_data_auth::GenerateFreshRecoveryIdRequest& request,
-      GenerateFreshRecoveryIdCallback callback) override;
   void GetAuthSessionStatus(
       const ::user_data_auth::GetAuthSessionStatusRequest& request,
       GetAuthSessionStatusCallback callback) override;
@@ -363,8 +359,6 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeUserDataAuthClient
   FUDAC_OPERATION_TYPES(kGetRecoverableKeyStores,
                         GetRecoverableKeyStoresRequest);
   FUDAC_OPERATION_TYPES(kLockFactorUntilReboot, LockFactorUntilRebootRequest);
-  FUDAC_OPERATION_TYPES(kGenerateFreshRecoveryId,
-                        GenerateFreshRecoveryIdRequest);
 
 #undef FUDAC_OPERATION_TYPES
 
