fa145ae8306508cdb0a65be62fa1edc3df8279f9
Mohammed Abdon <mohammedabdon@google.com>
2025-08-01 06:08:40 -0700

    Add a new dbus method GenerateFreshRecoveryId to rotate recovery id.
    This change introduces a new D-Bus method,  to rotate recovery id after
    user authenticates with recovery factor. This method should only be
    used after the user directory is mounted.
    
    Bug=b:420743396
    
    Change-Id: Iada26018cc40c3b07cfa4c816686cc9eaa9659de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781900
    Commit-Queue: Mohammed Abdon <mohammedabdon@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495388}

--- chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h
+++ chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h
@@ -50,6 +50,7 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeUserDataAuthClient
     kRemove,
     kGetRecoverableKeyStores,
     kLockFactorUntilReboot,
+    kGenerateFreshRecoveryId,
   };
 
   // The method by which a user's home directory can be encrypted.
@@ -301,6 +302,9 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeUserDataAuthClient
   void GetAuthFactorExtendedInfo(
       const ::user_data_auth::GetAuthFactorExtendedInfoRequest& request,
       GetAuthFactorExtendedInfoCallback callback) override;
+  void GenerateFreshRecoveryId(
+      const ::user_data_auth::GenerateFreshRecoveryIdRequest& request,
+      GenerateFreshRecoveryIdCallback callback) override;
   void GetAuthSessionStatus(
       const ::user_data_auth::GetAuthSessionStatusRequest& request,
       GetAuthSessionStatusCallback callback) override;
@@ -359,6 +363,8 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeUserDataAuthClient
   FUDAC_OPERATION_TYPES(kGetRecoverableKeyStores,
                         GetRecoverableKeyStoresRequest);
   FUDAC_OPERATION_TYPES(kLockFactorUntilReboot, LockFactorUntilRebootRequest);
+  FUDAC_OPERATION_TYPES(kGenerateFreshRecoveryId,
+                        GenerateFreshRecoveryIdRequest);
 
 #undef FUDAC_OPERATION_TYPES
 
