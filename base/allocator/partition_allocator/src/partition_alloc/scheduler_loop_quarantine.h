ee69aa85698499bbe0257fdb943760882b06fa8b
Tom Sepez <tsepez@chromium.org>
2025-08-01 13:05:23 -0700

    Remove base/allocator from unsafe_buffers_paths.txt file.
    Blatantly suppress the unsafe buffer warnings in every file by
    automatically inserting the header. This avoids breaking the build
    and allows removal of the suppression from, e.g. raw_ptr.h as desired
    in a follow-up CL.
    
    Fully automated change.
    
    Bug: 435068772
    Change-Id: I020180ede0495622df2dfe6a44c63f14551d3716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6807180
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495686}

--- base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine.h
+++ base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine.h
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
+#pragma allow_unsafe_buffers
+#endif
+
 // Scheduler-loop Quarantine is a quarantine pool behind PartitionAlloc with
 // Advanced Checks and `ADVANCED_MEMORY_SAFETY_CHECKS()`.
 // Both requests to prevent `free()`d allocation getting released to free-list,
