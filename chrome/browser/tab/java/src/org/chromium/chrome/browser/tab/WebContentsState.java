23dafd25e3996949eda0835f595ea9d16e68fc87
Sky Malice <skym@chromium.org>
2025-08-01 12:21:02 -0700

    Stop copying web contents state, take the protobuf's copy.
    Calls release to take ownership of the std::string from the protobuf
    instead of allocating a new std::string. Should be a faster
    implementation with less copies. Still likely leaking memory, as the
    Java DirectByteBuffer that will use this does not know about how we
    allocated this memory.
    
    Bug: 427255040
    Change-Id: Ib9c36a8100591da2d24a4025025d084e4b40b510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813369
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495650}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/WebContentsState.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/WebContentsState.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.tab;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
 import java.nio.ByteBuffer;
 
@@ -35,7 +34,7 @@ public class WebContentsState {
     private static @Nullable WebContentsState sEmptyWebContentsState;
 
     public WebContentsState(ByteBuffer buffer) {
-        assert buffer.isDirect() || ChromeFeatureList.sTabStorageSqlitePrototype.isEnabled();
+        assert buffer.isDirect();
         mBuffer = buffer;
         sEmptyWebContentsState = null;
     }
