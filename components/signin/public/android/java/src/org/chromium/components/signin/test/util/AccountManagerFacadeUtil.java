cead64795862a7944498620f3874597eaba0e067
Tanmoy Mollik <triploblastic@google.com>
2025-08-01 02:29:56 -0700

    [Android] Use FakeAccountManagerFacade in AccountManagerFacadeUtil
    Bug: 40276391
    Change-Id: Id3b0935e9018da95638200dcd29509ed35cda65b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722550
    Reviewed-by: Menghan Yang <myuu@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495309}

--- components/signin/public/android/java/src/org/chromium/components/signin/test/util/AccountManagerFacadeUtil.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/test/util/AccountManagerFacadeUtil.java
@@ -6,20 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.signin.test.util;
 
 import org.jni_zero.CalledByNative;
-import org.mockito.Mockito;
 
-import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
 
 /** Util class to set java AccountManagerFacade for native tests. */
 final class AccountManagerFacadeUtil {
     /** Stubs AccountManagerFacade for native tests. */
     @CalledByNative
-    private static void setUpMockFacade(boolean useFakeImpl) {
-        // TODO(crbug.com/40276391): Remove Mockito and use FakeAccountManagerFacade instead.
-        AccountManagerFacadeProvider.setInstanceForTests(
-                useFakeImpl
-                        ? new FakeAccountManagerFacade()
-                        : Mockito.mock(AccountManagerFacade.class));
+    private static void setUpMockFacade() {
+        AccountManagerFacadeProvider.setInstanceForTests(new FakeAccountManagerFacade());
     }
 }
