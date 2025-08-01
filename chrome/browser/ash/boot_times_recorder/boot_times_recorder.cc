78165e62596110c9f00a11756632378fcdede5b8
Xiyuan Xia <xiyuan@chromium.org>
2025-08-01 13:51:44 -0700

    ash: Speculative crash fix in BootTimesRecorder
    This change addresses a suspected crash in BootTimesRecorder.
    
    In `BootTimesRecorder::LoginDone()`, `RemoveAllObservations()` is
    called to stop recording login tab paint metrics since they are no
    longer needed after login is complete.
    
    In `BootTimesRecorder::RenderFrameHostChanged()`, a check is added
    to see if the new host's `RenderWidgetHost` is already being
    observed before adding a new observation. This should fix the CHECK
    crash if it is indeed from BootTimesRecorder.
    
    Bug: 429300263
    Change-Id: I3866fc889b604ac786c52e57e9cd2af89aa19051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813752
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495725}

--- chrome/browser/ash/boot_times_recorder/boot_times_recorder.cc
+++ chrome/browser/ash/boot_times_recorder/boot_times_recorder.cc
@@ -108,6 +108,7 @@ void BootTimesRecorder::LoginDone(bool is_user_new) {
   LoginEventRecorder::Get()->ScheduleWriteLoginTimes(
       kLoginTimes, (is_user_new ? kUmaLoginNewUser : kUmaLogin),
       kUmaLoginPrefix);
+  render_widget_host_observations_.RemoveAllObservations();
 }
 
 void BootTimesRecorder::WriteLogoutTimes() {
@@ -243,8 +244,10 @@ void BootTimesRecorder::RenderFrameHostChanged(
   RenderWidgetHost* old_rwh = old_host->GetRenderWidgetHost();
   if (render_widget_host_observations_.IsObservingSource(old_rwh)) {
     render_widget_host_observations_.RemoveObservation(old_rwh);
-    render_widget_host_observations_.AddObservation(
-        new_host->GetRenderWidgetHost());
+    RenderWidgetHost* new_rwh = new_host->GetRenderWidgetHost();
+    if (!render_widget_host_observations_.IsObservingSource(new_rwh)) {
+      render_widget_host_observations_.AddObservation(new_rwh);
+    }
   }
 }
 
