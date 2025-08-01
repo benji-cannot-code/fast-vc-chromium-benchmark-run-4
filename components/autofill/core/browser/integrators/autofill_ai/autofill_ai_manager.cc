90379b5d0ad0a26f1c1593944263e0f0f7c4eda4
Karol Sygiet <sygiet@google.com>
2025-08-01 08:03:55 -0700

    [RSG] Move suggestion generation logic to AutofillAISuggestionGenerator
    Bug: 409962888
    Change-Id: Idf821c35ddf2d7e8a969b1c7910a78214b8b5e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792125
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495456}

--- components/autofill/core/browser/integrators/autofill_ai/autofill_ai_manager.cc
+++ components/autofill/core/browser/integrators/autofill_ai/autofill_ai_manager.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/foundations/autofill_client.h"
 #include "components/autofill/core/browser/integrators/autofill_ai/autofill_ai_import_utils.h"
-#include "components/autofill/core/browser/integrators/autofill_ai/autofill_ai_suggestions.h"
 #include "components/autofill/core/browser/integrators/autofill_ai/metrics/autofill_ai_logger.h"
 #include "components/autofill/core/browser/logging/log_manager.h"
 #include "components/autofill/core/browser/ml_model/autofill_ai/autofill_ai_model_executor.h"
