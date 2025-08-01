250c078af17308f59dcca50912f5770dd773b12d
Christoph Schwering <schwering@google.com>
2025-08-01 03:39:15 -0700

    [Autofill] Migrate GetStorableType() calls to GetCreditCardType()
    This CL migrates many calls GetStorableType() calls to
    GetCreditCardType().
    
    This CL is a no-op.
    
    Bug: 432645177
    Change-Id: I73e1ac53fd346376797e3f15b8b7acab79c35830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6802320
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495336}

--- ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_tab_helper.mm
+++ ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_tab_helper.mm
@@ -572,7 +572,7 @@ void AutofillBottomSheetTabHelper::AttachListenersForPaymentsForm(
         fields_by_frame;
     for (const auto& field : form_structure->fields()) {
       if (IsPaymentsBottomSheetTriggeringField(
-              field->Type().GetStorableType())) {
+              field->Type().GetCreditCardType())) {
         autofill::FieldGlobalId field_id = field->global_id();
         fields_by_frame[field_id.frame_token].push_back(field_id.renderer_id);
       }
@@ -588,7 +588,7 @@ void AutofillBottomSheetTabHelper::AttachListenersForPaymentsForm(
     std::vector<autofill::FieldRendererId> renderer_ids;
     for (const auto& field : form_structure->fields()) {
       if (IsPaymentsBottomSheetTriggeringField(
-              field->Type().GetStorableType())) {
+              field->Type().GetCreditCardType())) {
         renderer_ids.push_back(field->renderer_id());
       }
     }
