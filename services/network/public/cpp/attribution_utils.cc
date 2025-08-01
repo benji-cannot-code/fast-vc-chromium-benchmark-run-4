b9ba3f0569fa92429521c7ba1ac6d78ad333efa0
Xiyuan Xia <xiyuan@chromium.org>
2025-08-01 12:42:26 -0700

    Replace NOTREACHED for AttributionSupport::kUnset
    The AttributionSupport::kUnset value is not expected to be passed to
    these utils. However, this could happen for random sites.
    
    This change replaces NOTREACHED() with a `return false` to avoid
    NOTREACHED crash. `LogAuditIssue` should be called to log it in
    devtools when it happens.
    
    Bug: 434916791
    Change-Id: Id07fc402adcd2ca14ddf98442217e2b59ef7fe8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813590
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495667}

--- services/network/public/cpp/attribution_utils.cc
+++ services/network/public/cpp/attribution_utils.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/cpp/attribution_utils.h"
 
-#include "base/notreached.h"
 #include "services/network/public/mojom/attribution.mojom.h"
 
 namespace network {
@@ -17,9 +16,8 @@ bool HasAttributionOsSupport(mojom::AttributionSupport attribution_support) {
       return true;
     case mojom::AttributionSupport::kWeb:
     case mojom::AttributionSupport::kNone:
-      return false;
     case mojom::AttributionSupport::kUnset:
-      NOTREACHED();
+      return false;
   }
 }
 
@@ -30,9 +28,8 @@ bool HasAttributionWebSupport(mojom::AttributionSupport attribution_support) {
       return true;
     case mojom::AttributionSupport::kOs:
     case mojom::AttributionSupport::kNone:
-      return false;
     case mojom::AttributionSupport::kUnset:
-      NOTREACHED();
+      return false;
   }
 }
 
