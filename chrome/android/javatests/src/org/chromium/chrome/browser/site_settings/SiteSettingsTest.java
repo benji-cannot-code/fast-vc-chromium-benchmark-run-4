2056ba9aad1f97db53fea69283aee293b0f35114
Henrique Nakashima <hnakashima@chromium.org>
2025-08-01 14:04:53 -0700

    [Android] Migrate PermissionInfoTest to Public Transit rules
    Previously reverted in http://crrev.com/c/6614579.
    
    Reason for the revert was that the two suites sharing a batch but
    not the test rule was causing the CTA to be finished between them.
    Splitting into PER_CLASS batching avoids this.
    
    Bug: 421977814,399877997
    Change-Id: I8da1fdb943dae7b0c60e0d3ec712340d2ab75536
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814270
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495734}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
@@ -187,7 +187,6 @@ import java.util.concurrent.TimeoutException;
 })
 // TODO(crbug.com/344672098): Failing when batched, batch this again.
 public class SiteSettingsTest {
-    public static final String SITE_SETTINGS_BATCH_NAME = "site_settings";
 
     @ClassRule public static PermissionTestRule mPermissionRule = new PermissionTestRule(true);
 
