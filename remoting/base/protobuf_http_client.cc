210637617055465ead78af2d1aa09fe311e81f6d
Doug Turner <dft@google.com>
2025-08-01 13:29:05 -0700

    Use SetHeader instead of AddHeadersFromString
    This CL refactors instances of `net::HttpRequestHeaders::AddHeadersFromString`
    that use compile-time constant string literals to instead use individual `SetHeader` calls.
    
    This improves readability and avoids the overhead of parsing the header string at runtime.
    
    Bug: 41400723
    
    Change-Id: I4485a4a4b97c6db9258a279c387ca8d22e73624b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6807922
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Doug Turner <dft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495707}

--- remoting/base/protobuf_http_client.cc
+++ remoting/base/protobuf_http_client.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/common/api_key_request_util.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
+#include "net/http/http_request_headers.h"
 #include "remoting/base/http_status.h"
 #include "remoting/base/oauth_token_getter.h"
 #include "remoting/base/protobuf_http_request_base.h"
@@ -20,12 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "third_party/protobuf/src/google/protobuf/message_lite.h"
 #include "url/gurl.h"
-
-namespace {
-
-constexpr char kAuthorizationHeaderFormat[] = "Authorization: Bearer %s";
-
-}  // namespace
 
 namespace remoting {
 
@@ -128,8 +123,8 @@ ProtobufHttpClient::CreateSimpleUrlLoader(
   resource_request->method = config.method;
 
   if (!access_token.empty()) {
-    resource_request->headers.AddHeaderFromString(
-        base::StringPrintf(kAuthorizationHeaderFormat, access_token.c_str()));
+    resource_request->headers.SetHeader(net::HttpRequestHeaders::kAuthorization,
+                                        "Bearer " + access_token);
   } else {
     VLOG(1) << "Attempting to execute request without access token";
   }
