887676af9ab70bc6a1a4bfc87e8a832be859b5a4
Guillaume Jenkins <gujen@google.com>
2025-08-01 18:23:23 -0700

    [iOS] Update Default Browser promo strings for Default Apps destination
    Updates the Default Browser promo instructions view controller to use
    the updated strings when the Default Apps destination variant is in
    use. Also tweaks the Lottie animations to use the correct string key
    for correct localized string replacement.
    
    Bug: 435720622
    Change-Id: I9dac9a5ae07ce4f9a66a7883fef7562ba5c65c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814090
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495840}

--- ios/chrome/browser/default_promo/ui_bundled/default_browser_instructions_view_controller.mm
+++ ios/chrome/browser/default_promo/ui_bundled/default_browser_instructions_view_controller.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/default_promo/ui_bundled/default_browser_instructions_view_controller.h"
 
+#import "base/check.h"
 #import "base/i18n/rtl.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -36,6 +37,7 @@ NSString* const kDefaultBrowserDefaultAppsAnimationRtlDarkmode =
     @"default_browser_default_apps_animation_rtl_darkmode";
 
 // Keys in the lottie assets.
+NSString* const kBrowserAppKeypath = @"IDS_BROWSER_APP";
 NSString* const kDefaultBrowserAppKeypath = @"IDS_DEFAULT_BROWSER_APP";
 NSString* const kChromeKeypath = @"IDS_CHROME";
 
@@ -78,6 +80,7 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
     [self addVideoSection:useDefaultAppsDestination];
     [self addInformationSectionWithDismissButton:hasDismissButton
                                 hasRemindMeLater:hasRemindMeLater
+                       useDefaultAppsDestination:useDefaultAppsDestination
                                         hasSteps:hasSteps
                                    actionHandler:actionHandler
                                        titleText:titleText];
@@ -142,6 +145,8 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
 
   // Set the text localization.
   NSDictionary* textProvider = @{
+    kBrowserAppKeypath :
+        l10n_util::GetNSString(IDS_IOS_DEFAULT_BROWSER_VIDEO_PROMO_BROWSER_APP),
     kDefaultBrowserAppKeypath : l10n_util::GetNSString(
         IDS_IOS_DEFAULT_BROWSER_VIDEO_PROMO_DEFAULT_BROWSER_APP),
     kChromeKeypath : l10n_util::GetNSString(IDS_IOS_SHORT_PRODUCT_NAME)
@@ -215,6 +220,7 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
 // If `titleText` is nil, default title will be used.
 - (void)addInformationSectionWithDismissButton:(BOOL)hasDismissButton
                               hasRemindMeLater:(BOOL)hasRemindMeLater
+                     useDefaultAppsDestination:(BOOL)useDefaultAppsDestination
                                       hasSteps:(BOOL)hasSteps
                                  actionHandler:
                                      (id<ConfirmationAlertActionHandler>)
@@ -245,6 +251,10 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
 
   // The view can have either instruction steps or subtitles.
   if (hasSteps) {
+    // TODO(crbug.com/435722549): Add strings for steps for the new Default Apps
+    // destination, and use thme heere if useDefaultAppsDestination is YES.
+    // Remove the CHECK when done.
+    CHECK(!useDefaultAppsDestination);
     NSArray* defaultBrowserSteps = @[
       l10n_util::GetNSString(
           IDS_IOS_FIRST_RUN_DEFAULT_BROWSER_SCREEN_FIRST_STEP),
@@ -261,8 +271,13 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
     alertScreen.underTitleView = instructionView;
     alertScreen.shouldFillInformationStack = YES;
   } else {
-    alertScreen.subtitleString = l10n_util::GetNSString(
-        IDS_IOS_DEFAULT_BROWSER_VIDEO_PROMO_SUBTITLE_TEXT);
+    if (useDefaultAppsDestination) {
+      alertScreen.subtitleString = l10n_util::GetNSString(
+          IDS_IOS_DEFAULT_BROWSER_VIDEO_PROMO_DEFAULT_APPS_SUBTITLE_TEXT);
+    } else {
+      alertScreen.subtitleString = l10n_util::GetNSString(
+          IDS_IOS_DEFAULT_BROWSER_VIDEO_PROMO_SUBTITLE_TEXT);
+    }
   }
 
   if (hasDismissButton) {
