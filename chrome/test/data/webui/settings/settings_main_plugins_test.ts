edbe4607712d2c45e3bc5075d43aece19714cc48
dpapad <dpapad@chromium.org>
2025-08-03 22:20:07 -0700

    Settings: Deflake SettingsTest.SettingsMain.
    The tests were flaking because there wasn't a reliable signal to wait
    for after currentRouteChanged() calls to signify that cr-view-manager
    had finished transitioning to the newly displayed view. The test code
    was calling flushTasks() which was not sufficient.
    
    Fixed by exposed a whenViewSwitchingDone() method which returns a
    Promise that can reliably signal when the test can make assertions. A
    similar approach is being used at https://crrev.com/c/6784912 for the
    same reason.
    
    Bug: 435560735
    Change-Id: I2eb3a5f0b6ee01f074578ce0cd0968093c290a98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813306
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496137}

--- chrome/test/data/webui/settings/settings_main_plugins_test.ts
+++ chrome/test/data/webui/settings/settings_main_plugins_test.ts
@@ -48,6 +48,7 @@ suite('SettingsMain', function() {
 
   setup(function() {
     createSettingsMain();
+    return settingsMain.whenViewSwitchingDone();
   });
 
   test('UpdatesActiveViewWhenRouteChanges', async function() {
@@ -87,7 +88,7 @@ suite('SettingsMain', function() {
 
     for (const {route, pluginTag} of routesToVisit) {
       Router.getInstance().navigateTo(route);
-      await flushTasks();
+      await settingsMain.whenViewSwitchingDone();
       assertActive(pluginTag, route.path);
     }
   });
@@ -175,7 +176,7 @@ suite('SettingsMain', function() {
 
     // Case2: Guest mode.
     createSettingsMain({isGuest: true});
-    await flushTasks();
+    await settingsMain.whenViewSwitchingDone();
     active = settingsMain.$.switcher.querySelector<HTMLElement>(
         '.active[slot=view]');
     assertTrue(!!active);
