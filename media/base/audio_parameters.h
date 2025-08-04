88b1d3bfbffd34ca7743e3cb3be4527c62f92805
Venkatesh Srinivas <venkateshs@chromium.org>
2025-08-04 09:07:05 -0700

    Audio/Media: Convert legacy atomics -> std::atomic
    No behavior change expected.
    
    Bug: 40259737
    Bug: 40175832
    Change-Id: Iacc0929f6fac9945c867219e456facb9f32e25f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166126
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Venkatesh Srinivas <venkateshs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496388}

--- media/base/audio_parameters.h
+++ media/base/audio_parameters.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <atomic>
 #include <optional>
 #include <string>
 #include <vector>
@@ -47,10 +48,7 @@ struct MEDIA_EXPORT alignas(kParametersAlignment) AudioInputBufferParameters {
   uint32_t glitch_count;
   uint32_t size;
   uint32_t id;
-  // Intentionally using deprecated Atomic32 instead of std::atomic to keep the
-  // struct as a trivial type.
-  // TODO(https://crbug.com/40259737): Switch to atomic_ref once it's available.
-  base::subtle::Atomic32 has_unread_data;
+  uint32_t has_unread_data;
 };
 struct MEDIA_EXPORT alignas(kParametersAlignment) AudioOutputBufferParameters {
   int64_t delay_us;            // base::TimeDelta in microseconds.
