8e73628cbd74d67cc54062ef005e187aade071df
Quentin Pubert <qpubert@google.com>
2025-08-04 03:07:51 -0700

    [iOS] Fix LocationBarBadgesContainerView stack view element visibility
    Showing/hiding views in a stack view has a bug such that settings the
    value of `.hidden` to a given value e.g. YES is not idempotent. This
    results in issues where the following code would crash:
    ```
    myStackSubview.hidden = YES;
    CHECK(myStackSubview.hidden);
    ```
    To address this issue, a `SetViewHiddenIfNecessary` helper is used to
    set the value of `.hidden` only if necessary, so that views inside of
    LocationBarBadgesContainerView will never be shown or hidden twice in a
    row.
    
    Bug: 430927989
    Change-Id: If76430c74506d39e7c513860539f1d50f8df47e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6815500
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496213}

--- ios/chrome/browser/location_bar/ui_bundled/badges_container_view.mm
+++ ios/chrome/browser/location_bar/ui_bundled/badges_container_view.mm
@@ -10,6 +10,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 
+namespace {
+
+// Sets `view.hidden` to `hidden` if necessary. This helper is useful to address
+// a bug where the number of times `.hidden` is set in a view accumulates if it
+// is presented inside of a stack view. As a result, setting `.hidden = YES`
+// twice does not have the same effect as only settings it once.
+void SetViewHiddenIfNecessary(UIView* view, BOOL hidden) {
+  if (view.hidden != hidden) {
+    view.hidden = hidden;
+  }
+}
+
+}  // namespace
+
 @implementation LocationBarBadgesContainerView {
   UIStackView* _containerStackView;
 
@@ -109,7 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _incognitoBadgeView.translatesAutoresizingMaskIntoConstraints = NO;
   _incognitoBadgeView.isAccessibilityElement = NO;
   [_containerStackView insertArrangedSubview:_incognitoBadgeView atIndex:0];
-  _incognitoBadgeView.hidden = YES;
+  SetViewHiddenIfNecessary(_incognitoBadgeView, YES);
 
   [NSLayoutConstraint activateConstraints:@[
     [_incognitoBadgeView.heightAnchor
@@ -125,7 +139,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _badgeView.translatesAutoresizingMaskIntoConstraints = NO;
   _badgeView.isAccessibilityElement = NO;
   [_containerStackView addArrangedSubview:_badgeView];
-  _badgeView.hidden = YES;
+  SetViewHiddenIfNecessary(_badgeView, YES);
 
   [NSLayoutConstraint activateConstraints:@[
     [_badgeView.heightAnchor
@@ -144,7 +158,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _contextualPanelEntrypointView = contextualPanelEntrypointView;
   _contextualPanelEntrypointView.translatesAutoresizingMaskIntoConstraints = NO;
   _contextualPanelEntrypointView.isAccessibilityElement = NO;
-  _contextualPanelEntrypointView.hidden = YES;
+  SetViewHiddenIfNecessary(_contextualPanelEntrypointView, YES);
   // The Contextual Panel entrypoint view should be first in its containing
   // stackview, regardless of when it was added.
   [_containerStackView insertArrangedSubview:_contextualPanelEntrypointView
@@ -166,7 +180,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _readerModeChipView = readerModeChipView;
   _readerModeChipView.translatesAutoresizingMaskIntoConstraints = NO;
   _readerModeChipView.isAccessibilityElement = NO;
-  _readerModeChipView.hidden = YES;
+  SetViewHiddenIfNecessary(_readerModeChipView, YES);
   // Reading Mode chip should be shown to the right of the incognito badge
   // view.
   int index = _incognitoBadgeView ? 1 : 0;
@@ -193,7 +207,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _placeholderView = placeholderView;
   if (_placeholderView) {
     _placeholderView.translatesAutoresizingMaskIntoConstraints = NO;
-    _placeholderView.hidden = YES;
+    SetViewHiddenIfNecessary(_placeholderView, YES);
     [_containerStackView addArrangedSubview:_placeholderView];
     [NSLayoutConstraint activateConstraints:@[
       [_placeholderView.heightAnchor
@@ -207,16 +221,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Updates the hidden state of the views.
 - (void)updateViewsVisibility {
-  self.readerModeChipView.hidden = !_readerModeChipShouldBeVisible;
-  self.incognitoBadgeView.hidden = !_incognitoBadgeViewShouldBeVisible;
-  self.badgeView.hidden =
-      !_badgeViewShouldBeVisible || _readerModeChipShouldBeVisible;
+  SetViewHiddenIfNecessary(self.readerModeChipView,
+                           !_readerModeChipShouldBeVisible);
+  SetViewHiddenIfNecessary(self.incognitoBadgeView,
+                           !_incognitoBadgeViewShouldBeVisible);
+  SetViewHiddenIfNecessary(self.badgeView, !_badgeViewShouldBeVisible ||
+                                               _readerModeChipShouldBeVisible);
   if (IsDiamondPrototypeEnabled()) {
-    self.badgeView.hidden = YES;
+    SetViewHiddenIfNecessary(self.badgeView, YES);
   }
-  self.contextualPanelEntrypointView.hidden =
-      !_contextualPanelEntrypointShouldBeVisible ||
-      _readerModeChipShouldBeVisible;
+  SetViewHiddenIfNecessary(self.contextualPanelEntrypointView,
+                           !_contextualPanelEntrypointShouldBeVisible ||
+                               _readerModeChipShouldBeVisible);
 
   BOOL placeholderHidden =
       (self.contextualPanelEntrypointView &&
@@ -231,7 +247,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                               (self.badgeView && self.badgeView.hidden);
     placeholderHidden = !placeholderVisible;
     if (placeholderVisible) {
-      self.contextualPanelEntrypointView.hidden = YES;
+      SetViewHiddenIfNecessary(self.contextualPanelEntrypointView, YES);
     }
   }
 
@@ -239,7 +255,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
-  _placeholderView.hidden = placeholderHidden;
+  SetViewHiddenIfNecessary(_placeholderView, placeholderHidden);
 
   // Records why the placeholder view is hidden. These are not mutually
   // exclusive, price tracking will take precedence over messages.
