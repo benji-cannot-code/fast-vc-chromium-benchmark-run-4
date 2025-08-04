1df6f54814cebeca8396d7774950f967b817a957
Ivana Žužić <izuzic@google.com>
2025-08-04 09:47:05 -0700

    Reland "[3PAutofillSettings] Disable the settings in "Payment methods""
    This is a reland of commit b8fac33dff3ebeaec4ca8608ef20e694d87e0856
    
    The tests about mandatory reauth preference are not supposed to run on
    automotive. This CL adds a restriction to the test that was failing on
    android-automotive-12l-x64-rel-tests, as described in
    crrev.com/c/6818482.
    
    Original change's description:
    > [3PAutofillSettings] Disable the settings in "Payment methods"
    >
    > This change is disabling the settings when we are in third party mode.
    >
    > Design doc: go/3ppwm-disable-autofill-settings-screen
    >
    > Bug: 428917607
    > Change-Id: I46c78447584ce3f9c09ff91bc8a51edb24060327
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722374
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1496286}
    
    Bug: 428917607
    Change-Id: I10aa701257e1907fe8ded7aa813e3a08619d5a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819039
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496409}

--- components/payments/content/android/java/src/org/chromium/components/payments/PackageManagerDelegate.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/PackageManagerDelegate.java
@@ -60,7 +60,7 @@ public class PackageManagerDelegate {
      * returned.
      *
      * @param uid The uid of an installed application.
-     * @return The package information of installed appliacations with the input uid, or null if no
+     * @return The package information of installed applications with the input uid, or null if no
      *     matching applications are found.
      */
     @SuppressLint("PackageManagerGetSignatures")
@@ -94,7 +94,7 @@ public class PackageManagerDelegate {
 
     /**
      * Retrieves the list of activities that can respond to the given intent. And returns the
-     * activites' meta data in ResolveInfo.
+     * activities' meta data in ResolveInfo.
      *
      * @param intent The intent to query.
      * @return The list of activities that can respond to the intent.
