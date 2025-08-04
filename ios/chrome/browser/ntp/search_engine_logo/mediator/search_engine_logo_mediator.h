70bac0246f47e3e1d1b72f362e469be0a8b3ff64
Jérôme Lebel <jlebel@chromium.org>
2025-08-04 08:29:28 -0700

    [iOS] Update SearchEngineLogoMediator init method
    Update the init method for SearchEngineLogoMediator to not give
    the browser, and give all the services instead.
    
    Bug: 423883582
    Change-Id: Id2347fcf5183f7f8e389bc41cf0fe2b2d017fe8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6817736
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496361}

--- ios/chrome/browser/ntp/search_engine_logo/mediator/search_engine_logo_mediator.h
+++ ios/chrome/browser/ntp/search_engine_logo/mediator/search_engine_logo_mediator.h
@@ -8,15 +8,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "base/memory/scoped_refptr.h"
 #import "url/gurl.h"
 
+class GoogleLogoService;
 @protocol SearchEngineLogoConsumer;
 enum class SearchEngineLogoState;
+class TemplateURLService;
+class UrlLoadingBrowserAgent;
 
-class Browser;
+namespace network {
+class SharedURLLoaderFactory;
+}  // namespace network
 namespace web {
 class WebState;
-}
+}  // namespace web
 
 @interface SearchEngineLogoMediator : NSObject
 
@@ -29,11 +35,14 @@ class WebState;
 @property(nonatomic, strong, readonly) UIView* view;
 
 // Designated initializer.
-// TODO(crbug.com/423883582): Need remove browser parameter and all the service
-// parameters.
-- (instancetype)initWithBrowser:(Browser*)browser
-                       webState:(web::WebState*)webState
-    NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithWebState:(web::WebState*)webState
+              templateURLService:(TemplateURLService*)templateURLService
+                     logoService:(GoogleLogoService*)logoService
+          URLLoadingBrowserAgent:(UrlLoadingBrowserAgent*)URLLoadingBrowserAgent
+          sharedURLLoaderFactory:
+              (scoped_refptr<network::SharedURLLoaderFactory>)
+                  sharedURLLoaderFactory
+                    offTheRecord:(BOOL)offTheRecord NS_DESIGNATED_INITIALIZER;
 - (instancetype)init NS_UNAVAILABLE;
 
 // Disconnect the instance.
--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/feed/core/v2/public/common_enums.h"
 #import "components/feed/core/v2/public/ios/pref_names.h"
 #import "components/feed/feed_feature_list.h"
+#import "components/image_fetcher/ios/ios_image_data_fetcher_wrapper.h"
 #import "components/omnibox/common/omnibox_features.h"
 #import "components/policy/policy_constants.h"
 #import "components/pref_registry/pref_registry_syncable.h"
@@ -55,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/follow/model/follow_browser_agent.h"
 #import "ios/chrome/browser/follow/model/followed_web_site.h"
 #import "ios/chrome/browser/follow/model/followed_web_site_state.h"
+#import "ios/chrome/browser/google/model/google_logo_service_factory.h"
 #import "ios/chrome/browser/home_customization/coordinator/home_customization_coordinator.h"
 #import "ios/chrome/browser/home_customization/coordinator/home_customization_delegate.h"
 #import "ios/chrome/browser/home_customization/utils/home_customization_constants.h"
@@ -143,6 +145,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "ios/web/public/web_state_observer_bridge.h"
+#import "services/network/public/cpp/shared_url_loader_factory.h"
 #import "ui/base/l10n/l10n_util_mac.h"
 
 @interface NewTabPageCoordinator () <AccountMenuCoordinatorDelegate,
@@ -666,11 +669,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Creates all the NTP components.
 - (void)initializeNTPComponents {
   Browser* browser = self.browser;
+  ProfileIOS* profile = browser->GetProfile();
+  web::WebState* webState = browser->GetWebStateList()->GetActiveWebState();
+  TemplateURLService* templateURLService =
+      ios::TemplateURLServiceFactory::GetForProfile(profile);
+  GoogleLogoService* logoService =
+      GoogleLogoServiceFactory::GetForProfile(profile);
+  UrlLoadingBrowserAgent* URLLoadingBrowserAgent =
+      UrlLoadingBrowserAgent::FromBrowser(browser);
+  scoped_refptr<network::SharedURLLoaderFactory> sharedURLLoaderFactory =
+      profile->GetSharedURLLoaderFactory();
+  BOOL offTheRecord = profile->IsOffTheRecord();
+  _searchEngineLogoMediator =
+      [[SearchEngineLogoMediator alloc] initWithWebState:webState
+                                      templateURLService:templateURLService
+                                             logoService:logoService
+                                  URLLoadingBrowserAgent:URLLoadingBrowserAgent
+                                  sharedURLLoaderFactory:sharedURLLoaderFactory
+                                            offTheRecord:offTheRecord];
   id<NewTabPageComponentFactoryProtocol> componentFactory =
       self.componentFactory;
-  _searchEngineLogoMediator =
-      [[SearchEngineLogoMediator alloc] initWithBrowser:browser
-                                               webState:self.webState];
   self.NTPViewController = [componentFactory NTPViewController];
   self.headerViewController =
       [componentFactory headerViewControllerForProfile:self.profile];
