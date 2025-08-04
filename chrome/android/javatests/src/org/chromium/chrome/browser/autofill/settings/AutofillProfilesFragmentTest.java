83c1d80ffb2aba6403a9b96175d75bade2a17217
Piotr Kotynia <piotrkotynia@google.com>
2025-08-04 08:35:08 -0700

    [Android] Add link entry to Google Account in Home/Work address editors
    This change adds a link to the non-editable editor view. This link,
    labeled "Edit" with an external link icon, opens the appropriate Google
    Account address management page in a Custom Tab, allowing the user to
    edit their address at its source.
    
    Mocks:
    https://www.figma.com/design/IwB272Z5KxJyjfSwryZ0ms/Chrome-Autofill--Home---Work?node-id=4191-16869&t=OyspcmqGMFlJ2NXh-0
    
    UI: https://screenshot.googleplex.com/7ahg3tuUiSVWzZS.png UI video:
    http://screencast/cast/NTcwMTIzMjUwODI3MjY0MHxjNDYyMDNkZC0wMQ
    
    Bug: 430218067
    Change-Id: I1868239b0956cb973edc849119b5f914ca2c4a36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6802063
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496364}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragmentTest.java
@@ -6,10 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill.settings;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -17,8 +23,12 @@ import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -294,13 +304,35 @@ public class AutofillProfilesFragmentTest {
     @Feature({"Preferences"})
     public void testHomeEntry() throws Exception {
         mHelper.setProfile(sHomeProfile);
+        AutofillProfilesFragment autofillProfileFragment = sSettingsActivityTestRule.getFragment();
 
         AutofillProfileEditorPreference homeProfilePreference =
-                findPreference(sHomeProfile.getInfo(FieldType.NAME_FULL));
+                autofillProfileFragment.findPreference(sHomeProfile.getInfo(FieldType.NAME_FULL));
         assertNotNull(homeProfilePreference);
         assertEquals(
                 R.layout.autofill_settings_home_profile_icon,
                 homeProfilePreference.getWidgetLayoutResource());
+
+        // Edit a profile.
+        ThreadUtils.runOnUiThreadBlocking(homeProfilePreference::performClick);
+
+        // Define a fake result to return immediately when the intent is caught.
+        // This prevents the actual urls from being launched.
+        Instrumentation.ActivityResult ok_result =
+                new Instrumentation.ActivityResult(Activity.RESULT_OK, null);
+        var homeIntentMatcher =
+                allOf(
+                        hasAction(Intent.ACTION_VIEW),
+                        hasData(
+                                Uri.parse(
+                                        AutofillProfilesFragment
+                                                .GOOGLE_ACCOUNT_HOME_ADDRESS_EDIT_URL)));
+        intending(homeIntentMatcher).respondWith(ok_result);
+
+        // Try to find a view with "Link" and click on it.
+        Context context = autofillProfileFragment.getContext();
+        onView(withText(context.getString(R.string.autofill_edit_address_label))).perform(click());
+        intended(homeIntentMatcher);
     }
 
     @Test
@@ -308,13 +340,35 @@ public class AutofillProfilesFragmentTest {
     @Feature({"Preferences"})
     public void testWorkEntry() throws Exception {
         mHelper.setProfile(sWorkProfile);
+        AutofillProfilesFragment autofillProfileFragment = sSettingsActivityTestRule.getFragment();
 
         AutofillProfileEditorPreference workProfilePreference =
-                findPreference(sWorkProfile.getInfo(FieldType.NAME_FULL));
+                autofillProfileFragment.findPreference(sWorkProfile.getInfo(FieldType.NAME_FULL));
         assertNotNull(workProfilePreference);
         assertEquals(
                 R.layout.autofill_settings_work_profile_icon,
                 workProfilePreference.getWidgetLayoutResource());
+
+        // Edit a profile.
+        ThreadUtils.runOnUiThreadBlocking(workProfilePreference::performClick);
+
+        // Define a fake result to return immediately when the intent is caught.
+        // This prevents the actual urls from being launched.
+        Instrumentation.ActivityResult ok_result =
+                new Instrumentation.ActivityResult(Activity.RESULT_OK, null);
+        var workIntentMatcher =
+                allOf(
+                        hasAction(Intent.ACTION_VIEW),
+                        hasData(
+                                Uri.parse(
+                                        AutofillProfilesFragment
+                                                .GOOGLE_ACCOUNT_WORK_ADDRESS_EDIT_URL)));
+        intending(workIntentMatcher).respondWith(ok_result);
+
+        // Try to find a view with "Link" and click on it.
+        Context context = autofillProfileFragment.getContext();
+        onView(withText(context.getString(R.string.autofill_edit_address_label))).perform(click());
+        intended(workIntentMatcher);
     }
 
     @Test
