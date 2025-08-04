630022e0557aea06c29b7af031f12257bfeb5885
Sorin Jianu <sorin@chromium.org>
2025-08-04 10:47:50 -0700

    updater: enable partition_alloc::SetRetryOnCommitFailure
    Fixed: 435743974
    Change-Id: Id67f686cc86708400d7517bcbfe41d2b06e50178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813757
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496449}

--- chrome/updater/updater.cc
+++ chrome/updater/updater.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "chrome/updater/app/server/win/updater_service_delegate.h"
 #include "chrome/updater/util/win_util.h"
+#include "partition_alloc/page_allocator.h"
 #endif
 
 // Instructions For Windows.
@@ -116,6 +117,9 @@ int HandleUpdaterCommands(UpdaterScope updater_scope,
   base::EnableTerminationOnHeapCorruption();
   base::EnableTerminationOnOutOfMemory();
   logging::RegisterAbslAbortHook();
+#if BUILDFLAG(IS_WIN)
+  partition_alloc::SetRetryOnCommitFailure(true);
+#endif
 
   InitializeThreadPool("updater");
   const base::ScopedClosureRunner shutdown_thread_pool(base::BindOnce([] {
