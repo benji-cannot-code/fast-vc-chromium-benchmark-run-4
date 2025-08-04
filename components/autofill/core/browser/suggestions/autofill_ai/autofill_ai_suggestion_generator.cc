73aa1ee18fcb041256d084e81162d852878c250f
Christoph Schwering <schwering@google.com>
2025-08-04 06:16:57 -0700

    [Autofill] Remove AutofillType::GetStorableType(), group()
    This CL
    - migrates the last references to AutofillType::GetStorableType() and
      AutofillType::group() to the new getters, and
    - removes AutofillType::GetStorableType() and AutofillType::group().
    
    Bug: 432645177
    Change-Id: I1f6509d863ab12a0b4dd8ee682e508a5b32567b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6768332
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496289}

--- components/autofill/core/browser/suggestions/autofill_ai/autofill_ai_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/autofill_ai/autofill_ai_suggestion_generator.cc
@@ -276,8 +276,9 @@ bool EntityShouldProduceSuggestion(
     return false;
   }
   std::u16string trigger_value = trigger_attribute->GetInfo(
-      trigger_field.field->Type().GetStorableType(), app_locale,
-      trigger_field.field->format_string());
+      trigger_field.field->Type().GetAutofillAiTypeAndResolveTagTypes(
+          entity.type()),
+      app_locale, trigger_field.field->format_string());
   if (trigger_value.empty()) {
     return false;
   }
@@ -309,10 +310,13 @@ bool CanFillSomeField(const EntityInstance& entity,
         DCHECK_EQ(entity.type(), f.type.entity_type());
         base::optional_ref<const AttributeInstance> attribute =
             entity.attribute(f.type);
-        return attribute && !attribute
-                                 ->GetInfo(f.field->Type().GetStorableType(),
-                                           app_locale, f.field->format_string())
-                                 .empty();
+        return attribute &&
+               !attribute
+                    ->GetInfo(
+                        f.field->Type().GetAutofillAiTypeAndResolveTagTypes(
+                            entity.type()),
+                        app_locale, f.field->format_string())
+                    .empty();
       });
 }
 
@@ -335,7 +339,8 @@ SuggestionWithMetadata GetSuggestionForEntity(
     }
 
     std::u16string attribute_value = attribute->GetInfo(
-        field->Type().GetStorableType(), app_locale, field->format_string());
+        field->Type().GetAutofillAiTypeAndResolveTagTypes(entity.type()),
+        app_locale, field->format_string());
 
     if (attribute_value.empty()) {
       continue;
@@ -355,11 +360,12 @@ SuggestionWithMetadata GetSuggestionForEntity(
                                          std::move(full_attribute_value));
   }
 
-  Suggestion suggestion =
-      Suggestion(trigger_attribute.GetInfo(
-                     trigger_field.field->Type().GetStorableType(), app_locale,
-                     trigger_field.field->format_string()),
-                 SuggestionType::kFillAutofillAi);
+  Suggestion suggestion = Suggestion(
+      trigger_attribute.GetInfo(
+          trigger_field.field->Type().GetAutofillAiTypeAndResolveTagTypes(
+              trigger_attribute.type().entity_type()),
+          app_locale, trigger_field.field->format_string()),
+      SuggestionType::kFillAutofillAi);
   suggestion.payload = Suggestion::AutofillAiPayload(entity.guid());
   suggestion.icon = GetSuggestionIcon(entity.type());
   return SuggestionWithMetadata(suggestion, raw_ref(entity), trigger_field.type,
