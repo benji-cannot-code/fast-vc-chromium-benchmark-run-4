1152388fee8e4c4641d2fffad426e7f3f79119ae
Kenichi Ishibashi <bashi@chromium.org>
2025-08-04 04:34:02 -0700

    HttpStreamPool: Record endpoints for trace events
    This change adds service endpoints to the trace event for
    `OnServiceEndpointsUpdated` and `OnServiceEndpointRequestFinished` to
    provide more debugging information in traces, which can be used to
    understand which endpoints could be used.
    
    Bug: 383824591
    Change-Id: I20575cf2ce8f65b42024e51e185a05550474f95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814775
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496244}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -271,8 +271,11 @@ void HttpStreamPool::AttemptManager::OnServiceEndpointsUpdated() {
     return;
   }
 
-  TRACE_EVENT_INSTANT("net.stream", "AttemptManager::OnServiceEndpointsUpdated",
-                      track_);
+  CHECK(service_endpoint_request_);
+  TRACE_EVENT_INSTANT(
+      "net.stream", "AttemptManager::OnServiceEndpointsUpdated", track_,
+      "endpoints",
+      GetServiceEndpointRequestAsValue(service_endpoint_request_.get()));
   net_log().AddEvent(
       NetLogEventType::HTTP_STREAM_POOL_ATTEMPT_MANAGER_DNS_RESOLUTION_UPDATED,
       [&] {
@@ -288,11 +291,12 @@ void HttpStreamPool::AttemptManager::OnServiceEndpointRequestFinished(int rv) {
     return;
   }
 
-  TRACE_EVENT_INSTANT("net.stream",
-                      "AttemptManager::OnServiceEndpointRequestFinished",
-                      track_, "result", rv);
   CHECK(!service_endpoint_request_finished_);
   CHECK(service_endpoint_request_);
+  TRACE_EVENT_INSTANT(
+      "net.stream", "AttemptManager::OnServiceEndpointRequestFinished", track_,
+      "result", rv, "endpoints",
+      GetServiceEndpointRequestAsValue(service_endpoint_request_.get()));
 
   service_endpoint_request_finished_ = true;
   dns_resolution_end_time_ = base::TimeTicks::Now();
