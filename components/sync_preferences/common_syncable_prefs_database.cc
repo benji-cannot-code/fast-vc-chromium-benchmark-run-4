9e71a5c31c3797933ea2d347c7a550c4005992e2
Ron Sailer <rsailer@google.com>
2025-08-04 07:34:11 -0700

    [FPF] Register kFingerprintingProtectionEnabled pref on iOS.
    This is part of the effort to empower users to toggle fingerprinting
    protection.
    
    The FingerprintingProtectionEnabled was previously only registered on
    Desktop/Clank and in order to bring it to iOS we move it to the `common`
    prefs section and deprecate the current entry.
    
    Bug: 434992778
    Change-Id: Icc56172f27df75a5af99460dd139d9c64f7edfb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798961
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Ron Sailer <rsailer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496328}

--- components/sync_preferences/common_syncable_prefs_database.cc
+++ components/sync_preferences/common_syncable_prefs_database.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/payments/core/payment_prefs.h"
 #include "components/plus_addresses/plus_address_prefs.h"
+#include "components/privacy_sandbox/tracking_protection_prefs.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/saved_tab_groups/public/pref_names.h"
 #include "components/sharing_message/pref_names.h"
@@ -138,6 +139,7 @@ enum {
   kAutofillWorkMetadata = 89,
   kFacilitatedPaymentsA2AEnabled = 90,
   kFacilitatedPaymentsA2ATriggeredOnce = 91,
+  kFingerprintingProtectionEnabled = 92
   // See components/sync_preferences/README.md about adding new entries here.
   // vvvvv IMPORTANT! vvvvv
   // Note to the reviewer: IT IS YOUR RESPONSIBILITY to ensure that new syncable
@@ -367,6 +369,9 @@ constexpr auto kCommonSyncablePrefsAllowlist =
           syncer::PRIORITY_PREFERENCES,
           PrefSensitivity::kExemptFromUserControlWhileSignedIn,
           MergeBehavior::kNone}},
+        {prefs::kFingerprintingProtectionEnabled,
+         {syncable_prefs_ids::kFingerprintingProtectionEnabled,
+          syncer::PREFERENCES, PrefSensitivity::kNone, MergeBehavior::kNone}},
     });
 
 }  // namespace
