4e66f27217c475d1e5deeddae037fd26465d52d4
Christoph Schwering <schwering@google.com>
2025-08-04 06:15:59 -0700

    [Autofill] Handle non-default union type, tag type configs in tests
    This CL modifies test expectations to handle non-default combinations
    of kAutofillAiNoTagTypes and kAutofillUnionTypesForAutofillAi
    
    - Tag types enabled,  union types disabled: unsupported.
    - Tag types disabled, union types enabled:  default behavior.
    - Tag types disabled, union types disabled: addressed by this CL.
    - Tag types enabled,  union types enabled:  addressed by this CL.
    
    Bug: 432645177
    Change-Id: I07596e05ed4858f284d6db4f3fb16132af1367a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6815498
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496288}

--- components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
@@ -7158,6 +7158,12 @@ TEST_F(BrowserAutofillManagerTest, ComposeSuggestionsAreQueriedForTextareas) {
 class BrowserAutofillManagerTest_AutofillAi
     : public BrowserAutofillManagerTest {
  public:
+  BrowserAutofillManagerTest_AutofillAi() {
+    feature_list_.InitWithFeatures({features::kAutofillAiWithDataSchema,
+                                    features::kAutofillUnionTypesForAutofillAi},
+                                   {});
+  }
+
   void SetUp() override {
     BrowserAutofillManagerTest::SetUp();
     client().set_entity_data_manager(std::make_unique<EntityDataManager>(
@@ -7245,8 +7251,7 @@ class BrowserAutofillManagerTest_AutofillAi
   }
 
  private:
-  base::test::ScopedFeatureList feature_list_{
-      features::kAutofillAiWithDataSchema};
+  base::test::ScopedFeatureList feature_list_;
   AutofillWebDataServiceTestHelper webdata_helper_{
       std::make_unique<EntityTable>()};
   FormData passport_form_;
@@ -7493,10 +7498,12 @@ TEST_F(BrowserAutofillManagerTest_AutofillAi_WithModel, CacheResultUsed) {
   // Check that we set predictions on the form.
   const FormStructure* const fs = manager().FindCachedFormById(form_id);
   ASSERT_TRUE(fs);
-  EXPECT_THAT(fs->field(0)->Type().GetAutofillAiTypes(),
-              ElementsAre(NAME_FIRST));
-  EXPECT_THAT(fs->field(1)->Type().GetAutofillAiTypes(),
-              ElementsAre(NAME_LAST));
+  if (base::FeatureList::IsEnabled(features::kAutofillAiNoTagTypes)) {
+    EXPECT_THAT(fs->field(0)->Type().GetAutofillAiTypes(),
+                ElementsAre(NAME_FIRST));
+    EXPECT_THAT(fs->field(1)->Type().GetAutofillAiTypes(),
+                ElementsAre(NAME_LAST));
+  }
   EXPECT_THAT(fs->field(2)->Type().GetAutofillAiTypes(),
               ElementsAre(PASSPORT_NUMBER));
   EXPECT_THAT(fs->field(3)->Type().GetAutofillAiTypes(),
