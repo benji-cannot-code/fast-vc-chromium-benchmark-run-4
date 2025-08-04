8152ad3c63ecd124187ed381bf21148268372b08
Andy Phan <andyphan@chromium.org>
2025-08-04 13:46:25 -0700

    [PDF Ink Signatures] Do not prevent default for key events
    When caret browsing mode is enabled, do not prevent default for arrow
    keydown events. As a result, the PDF extension will still receive the
    key event, and the PDF plugin will also receive it for future caret
    browsing key handling.
    
    Bug: 435623341
    Change-Id: I3a51611f74834f440a845f9fb0fd5461f8a10db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814894
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496558}

--- chrome/browser/resources/pdf/pdf_internal_plugin_wrapper.ts
+++ chrome/browser/resources/pdf/pdf_internal_plugin_wrapper.ts
@@ -28,6 +28,7 @@ if (parentOrigin === 'chrome-untrusted://print') {
 
 // Plugin-to-parent message handlers. All messages are passed through, but some
 // messages may affect this frame, too.
+let caretBrowsingEnabled: boolean = false;
 let isFormFieldFocused: boolean = false;
 plugin.addEventListener('message', e => {
   const message = (e as MessageEvent).data;
@@ -37,6 +38,11 @@ plugin.addEventListener('message', e => {
       // interesting keyboard events first.
       const focusedData = convertFormFocusChangeMessage(message);
       isFormFieldFocused = focusedData.focused !== FormFieldFocusType.NONE;
+      break;
+    case 'rendererPreferencesUpdated':
+      const caretBrowsingEnabledData =
+          message as unknown as {caretBrowsingEnabled: boolean};
+      caretBrowsingEnabled = caretBrowsingEnabledData.caretBrowsingEnabled;
       break;
   }
 
@@ -195,6 +201,12 @@ document.addEventListener('keydown', e => {
     case 'ArrowLeft':
     case 'ArrowRight':
     case 'ArrowUp':
+      if (caretBrowsingEnabled) {
+        // Do not prevent default, otherwise the plugin will not handle
+        // directional key events.
+        break;
+      }
+
       // Don't prevent arrow navigation in form fields, or if modified.
       if (!isFormFieldFocused && !hasKeyModifiers(e)) {
         e.preventDefault();
