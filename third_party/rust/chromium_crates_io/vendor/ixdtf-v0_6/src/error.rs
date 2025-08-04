b7d552a8aa75897505a339ed93483fad5e463e00
Manish Goregaokar <manishearth@google.com>
2025-08-04 11:20:52 -0700

    Roll ixdtf: 0.6.1 => 0.6.2 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * ixdtf: 0.6.1 => 0.6.2; https://docs.rs/crate/ixdtf/0.6.2
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: I52bf962a12bdb84eafb4e65e5ff01e41766f6435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819477
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Manish Goregaokar <manishearth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496469}

--- third_party/rust/chromium_crates_io/vendor/ixdtf-v0_6/src/error.rs
+++ third_party/rust/chromium_crates_io/vendor/ixdtf-v0_6/src/error.rs
@@ -31,7 +31,7 @@ pub enum ParseError {
     DateYear,
     #[displaydoc("Invalid character while parsing extended year value.")]
     DateExtendedYear,
-    #[displaydoc("Invalid caracter while parsing month value.")]
+    #[displaydoc("Invalid character while parsing month value.")]
     DateMonth,
     #[displaydoc("Invalid character while parsing day value.")]
     DateDay,
@@ -93,6 +93,8 @@ pub enum ParseError {
     IanaChar,
     #[displaydoc("Invalid time zone character after '/'.")]
     UtcTimeSeparator,
+    #[displaydoc("UTC offset needs a sign")]
+    OffsetNeedsSign,
 
     // MonthDay Errors
     #[displaydoc("MonthDay must begin with a month or '--'")]
