b773de2425ea2b9a7c1ef209dd87aff65217fbee
Kenichi Ishibashi <bashi@chromium.org>
2025-08-04 04:41:34 -0700

    HttpStreamPool: Don't throttle SPDY for slow attempts
    This CL modifies ShouldThrottleAttemptForSpdy() to avoid throttling new
    connection attempts when all existing attempts are slow. Previously, if
    an attempt was slow, it would cause throttling, preventing new attempts
    from being made until the throttle delay passed. This could lead to
    unnecessary delays if the initial attempt was stuck.
    
    With this change, if the only in-flight attempts are slow, new attempts
    are allowed immediately, improving connection latency in cases where
    the initial endpoint is unresponsive.
    
    A TRACE_EVENT is added for future debugging.
    
    Bug: 383606724, 414173943
    Change-Id: I8f62a178919c64190c5e347c2eef015f776ac74d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6816519
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496250}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -1472,8 +1472,9 @@ bool HttpStreamPool::AttemptManager::ShouldThrottleAttemptForSpdy() const {
 
   CHECK(UsingTls());
 
-  // The first attempt should not be blocked.
-  if (tcp_based_attempts_.empty()) {
+  // If there are no non-slow attempts, don't throttle new attempts.
+  CHECK_GE(tcp_based_attempts_.size(), slow_tcp_based_attempt_count_);
+  if (tcp_based_attempts_.size() - slow_tcp_based_attempt_count_ == 0) {
     return false;
   }
 
@@ -1958,6 +1959,8 @@ void HttpStreamPool::AttemptManager::HandleTcpBasedAttemptFailure(
 }
 
 void HttpStreamPool::AttemptManager::OnSpdyThrottleDelayPassed() {
+  TRACE_EVENT_INSTANT("net.stream", "AttemptManager::OnSpdyThrottleDelayPassed",
+                      track_);
   CHECK(!spdy_throttle_delay_passed_);
   spdy_throttle_delay_passed_ = true;
   MaybeAttemptTcpBased();
