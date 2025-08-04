bb81db7e96cdc3c4bcfa3c045fe843c81fd5cf33
Claudio DeSouza <cdesouza@chromium.org>
2025-08-04 02:38:50 -0700

    Adding missing `AffiliationService` forward declaration
    This is a simple fix to add the missing forward declaration for
    `affiliations::AffiliationService`.
    
    Bug: 416293264
    Change-Id: I6a00f75b78a6a719f0d81674673e96ef45200a0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6816698
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496180}

--- ios/web_view/internal/autofill/cwv_autofill_data_manager_internal.h
+++ ios/web_view/internal/autofill/cwv_autofill_data_manager_internal.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/public/cwv_autofill_data_manager.h"
 
+namespace affiliations {
+class AffiliationService;
+}  // namespace affiliations
+
 namespace autofill {
 class PersonalDataManager;
 }  // namespace autofill
