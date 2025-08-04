b0e0511025d534642331fd4eb0c7190b7704f41a
Yiren Wang <yrw@chromium.org>
2025-08-04 13:48:03 -0700

    Add GetTranscriptsForFrame for GlicMediaIntegration
    This CL introduces a new GetTranscriptsForFrame method to the
    GlicMediaIntegration interface. This method allows consumers to retrieve
    the current transcript for a render frame host, including both final and
    non-final speech recognition results. The returned transcript is a
    vector of MediaTranscript protos, each containing the text of a
    transcript chunk and an optional start timestamp.
    
    This CL also includes a new GetTranscriptChunks method in
    GlicMediaContext to correctly order and return final and non-final
    transcript chunks.
    
    Bug: 409427125
    Change-Id: I8810262b8b1530d27b6410bc096bf7c47179791d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6803193
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496559}

--- chrome/browser/glic/media/glic_media_integration.h
+++ chrome/browser/glic/media/glic_media_integration.h
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback.h"
-#include "chrome/browser/glic/host/glic.mojom.h"
 
 namespace optimization_guide {
 namespace proto {
 class ContentNode;
+class MediaTranscript;
 }  // namespace proto
 }  // namespace optimization_guide
 
@@ -46,6 +46,11 @@ class GlicMediaIntegration {
   virtual void AppendContextForFrame(
       content::RenderFrameHost* rfh,
       optimization_guide::proto::ContentNode* context_root) = 0;
+
+  // Returns the current transcript as a list of `MediaTranscript`s for the
+  // given frame only.
+  virtual std::vector<optimization_guide::proto::MediaTranscript>
+  GetTranscriptsForFrame(content::RenderFrameHost* rfh) = 0;
 
   // Pretend that a peer connection has been added.
   virtual void OnPeerConnectionAddedForTesting(content::RenderFrameHost*) = 0;
--- chrome/browser/glic/media/glic_media_page_cache_unittest.cc
+++ chrome/browser/glic/media/glic_media_page_cache_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/live_caption/live_caption_controller.h"
 #include "components/live_caption/pref_names.h"
-#include "components/optimization_guide/content/browser/page_content_proto_provider.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
