99aeb396c399fcf8a3f858adf070567aec184b2e
Doug Turner <dft@google.com>
2025-08-04 15:03:25 -0700

    Use SetHeader instead of AddHeadersFromString in WebView
    This CL refactors instances of `net::HttpRequestHeaders::AddHeadersFromString`
    to instead use individual `SetHeader` calls.  As part of
    this, we are away from storing strings of headers to net::HttpRequestHeaders.
    
    Bug: 41400723
    Change-Id: Id828d9ff577f8bb48a516e866f9f9a8ed94fb07c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813592
    Commit-Queue: Doug Turner <dft@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496600}

--- content/browser/browser_context.cc
+++ content/browser/browser_context.cc
@@ -468,8 +468,8 @@ BrowserContext::GetOriginTrialsControllerDelegate() {
 }
 
 #if BUILDFLAG(IS_ANDROID)
-std::string BrowserContext::GetExtraHeadersForUrl(const GURL& url) {
-  return std::string();
+net::HttpRequestHeaders BrowserContext::GetExtraHeadersForUrl(const GURL& url) {
+  return net::HttpRequestHeaders();
 }
 #endif  // BUILDFLAG(IS_ANDROID)
 
