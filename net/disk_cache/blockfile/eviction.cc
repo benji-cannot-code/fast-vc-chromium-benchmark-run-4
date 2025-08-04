b22fa180380fc1bac063dd90620239c76e2513e2
Adam Rice <ricea@chromium.org>
2025-08-04 06:18:26 -0700

    Add histograms for blockfile eviction
    One possible explanation for the regressions associated with the
    HttpCacheNoVarySearch experiment on Windows is a difference in eviction
    behavior. Add temporary histograms for blockfile cache eviction to
    investigate whether there is any difference.
    
    DiskCache.0.EvictedRank: This will determine if there is a difference in
    rank in resources that are evicted between the experiment group and the
    control group.
    
    DiskCache.0.EvictedAge: This will determine if there is a difference in
    the age of entries that are getting evicted. It is clamped to 1 day, as
    old entries being evicted wouldn't lead to the issues we are seeing.
    
    DiskCache.0.TrimmedDeletedEntries: The number of entries trimmed in a
    single call to Eviction::TrimDeleted(). This will determine if there is
    some difference in the batching of deletions taking place.
    
    All these histograms implicitly rely on the fact that the "V2" ranking
    algorithm is only used for the HTTP disk cache. Since they are intended
    to only exist for the duration of the investigation, it is reasonable to
    assume that won't change during their lifetime.
    
    BUG=433551601
    
    Change-Id: Ibee39c615182ac4693a509b080ffd45069dd1119
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778609
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496292}

--- net/disk_cache/blockfile/eviction.cc
+++ net/disk_cache/blockfile/eviction.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
@@ -341,6 +342,27 @@ void Eviction::TrimCacheV2(bool empty) {
         // This entry is not being used by anybody.
         // Do NOT use node as an iterator after this point.
         rankings_->TrackRankingsBlock(node.get(), false);
+        if (!empty) {
+          // Eviction algorithm V2 is only used for the HTTP disk cache. These
+          // histograms are logged for every stored request, so they use the
+          // histogram macros.
+          // TODO(crbug.com/433551601): Remove these histograms once issue
+          // 433551601 is resolved.
+          UMA_HISTOGRAM_EXACT_LINEAR("DiskCache.0.EvictedRank", list,
+                                     kListsToSearch);
+
+          const Time used_time =
+              Time::FromInternalValue(node->Data()->last_used);
+          const base::TimeDelta age = Time::Now() - used_time;
+          // For the purposes of this investigation we don't care about old
+          // entries getting evicted, so just ignore them so that they don't
+          // mess up the averages.
+          if (age < base::Days(1)) {
+            UMA_HISTOGRAM_CUSTOM_TIMES("DiskCache.0.EvictedAge", age,
+                                       base::Milliseconds(1), base::Days(1),
+                                       50);
+          }
+        }
         if (EvictEntry(node.get(), empty, static_cast<Rankings::List>(list)))
           deleted_entries++;
 
@@ -471,13 +493,14 @@ void Eviction::TrimDeleted(bool empty) {
   if (backend_->disabled_)
     return;
 
+  static constexpr int kMaxDeletedPerCall = 20;
   TimeTicks start = TimeTicks::Now();
   Rankings::ScopedRankingsBlock node(rankings_);
   Rankings::ScopedRankingsBlock next(
     rankings_, rankings_->GetPrev(node.get(), Rankings::DELETED));
   int deleted_entries = 0;
   while (next.get() &&
-         (empty || (deleted_entries < 20 &&
+         (empty || (deleted_entries < kMaxDeletedPerCall &&
                     (TimeTicks::Now() - start).InMilliseconds() < 20))) {
     node.reset(next.release());
     next.reset(rankings_->GetPrev(node.get(), Rankings::DELETED));
@@ -485,6 +508,13 @@ void Eviction::TrimDeleted(bool empty) {
       deleted_entries++;
     if (test_mode_)
       break;
+  }
+
+  if (!empty && new_eviction_) {
+    // TODO(crbug.com/433551601): Remove this histogram once issue 433551601 is
+    // resolved.
+    base::UmaHistogramExactLinear("DiskCache.0.TrimmedDeletedRankingEntries",
+                                  deleted_entries, kMaxDeletedPerCall + 1);
   }
 
   if (deleted_entries && !empty && ShouldTrimDeleted()) {
