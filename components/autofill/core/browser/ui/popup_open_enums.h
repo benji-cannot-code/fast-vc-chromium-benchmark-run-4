f65933756a0c11b7fd46fd9b5da0bad1dd62956d
Dominic Battré <battre@chromium.org>
2025-08-04 03:06:38 -0700

    Clarify the logic for placing the popup on the screen
    This CL adds clarifying comments to the `popup_view_utils` and `popup_base_view`. It also fixes a typo.
    
    No functional changes are intended.
    
    Bug: 430555440
    Change-Id: Ic8d16472c34e23738d34a2cee1eab88e4c1ce016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788649
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496211}

--- components/autofill/core/browser/ui/popup_open_enums.h
+++ components/autofill/core/browser/ui/popup_open_enums.h
@@ -8,13 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-// Specifies where the popup wil be anchoring on. This is used to define
+// Specifies where the popup will be anchoring on. This is used to define
 // specific UI behaviours, for example in Desktop, when the element is `kField`
 // the UI will check whether there is enough horizontal space to have a popup
 // with vertical arrow (above or below the target bounds). When the element is
 // `kCaret`, this check is ignored.
 enum class PopupAnchorType {
+  // The popup is anchored on an <input> or <textarea> field.
   kField,
+  // The popup is anchored on the caret, i.e., the cursor marker in the input
+  // element. E.g. for a <textarea> element, the "Help me write" feature
+  // renders a popup above the current cursor position inside the <textarea>.
   kCaret,
   // Android suggestions are displayed anchored on the keyboard accessory.
   kKeyboardAccessory,
