92a47e78b3ec346bf82855c527e0d8c525ce06a7
Steinar H. Gunderson <sesse@chromium.org>
2025-08-05 08:06:11 -0700

    Reapply "Refactor RuleSet creation."
    Now that we have a fix for devtools, reapply this patch.
    This reverts commit 1d8410ec42c16cf7c59d3684c4fab19317fda6f8.
    
    Original change's description:
    
    > Refactor RuleSet creation.
    >
    > We now extract all style sheets first, and create RuleSets in a separate
    > pass second. This is because it will be required by mixins shortly,
    > but it also reduces the code duplication slightly. In particular, we can
    > replace RuleSetScope with a more explicit construction.
    >
    > Note that this now means that adopted style sheets also go through
    > the same process, but this is a no-op (see added comment).
    >
    > Change-Id: I3be885ef4afc2e132b813f4dad3639ad44ec4b18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696133
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1482230}
    
    Bug: 430484028
    Change-Id: I90b9a43e90292fbd6d331f887c62f2897b392b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819104
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496921}

--- third_party/blink/renderer/core/css/document_style_sheet_collector.h
+++ third_party/blink/renderer/core/css/document_style_sheet_collector.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_DOCUMENT_STYLE_SHEET_COLLECTOR_H_
 
 #include "third_party/blink/renderer/core/css/active_style_sheets.h"
+#include "third_party/blink/renderer/core/css/style_rule.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -36,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class MediaQueryEvaluator;
+class StyleEngine;
 class StyleSheet;
 class StyleSheetCollection;
 class RuleSetDiff;
@@ -52,9 +55,14 @@ class DocumentStyleSheetCollector {
   DocumentStyleSheetCollector(StyleSheetCollection*,
                               HeapVector<Member<StyleSheet>>*);
 
-  void AppendActiveStyleSheet(const ActiveStyleSheet&);
+  // NOTE: You will need to call FinishCollectingStylesheets()
+  // after this.
+  void AppendActiveStyleSheet(CSSStyleSheet*);
   void AppendSheetForList(StyleSheet*);
   void AppendRuleSetDiff(RuleSetDiff*);
+
+  // Can only be called once.
+  void FinishCollectingStylesheets(StyleEngine& engine);
 
  private:
   StyleSheetCollection* collection_;
@@ -64,7 +72,7 @@ class DocumentStyleSheetCollector {
 class ActiveDocumentStyleSheetCollector final
     : public DocumentStyleSheetCollector {
  public:
-  ActiveDocumentStyleSheetCollector(StyleSheetCollection&);
+  explicit ActiveDocumentStyleSheetCollector(StyleSheetCollection&);
 };
 
 class ImportedDocumentStyleSheetCollector final
