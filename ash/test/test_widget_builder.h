4f2e3fa58ff01123b66d80b7ff1e80250e9d07af
Mitsuru Oshima <oshima@chromium.org>
2025-08-05 14:44:40 -0700

    Introduce aggregation struct for TestWidgetBuilder
    so that test can create a builder with designated initializer.
    Widget::InitPrams can't be part of it because it's has a constructor
    and designated initializer can't be used.
    
    Bug: None
    Change-Id: I86f8242807a2391058354f161c9580f5174a5403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819788
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497207}

--- ash/test/test_widget_builder.h
+++ ash/test/test_widget_builder.h
@@ -18,6 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+struct WidgetBuilderParams {
+  int window_id = aura::Window::kInitialId;
+  std::u16string window_title = std::u16string();
+  bool show = true;
+};
+
 // A builder to create a views::Widget for testing purpose. There are three
 // ways to create a widget: `BuildOwnedByNativeWidget()`,
 // `BuildOwnsNativeWidget()`, and `BuildClientOwnsWidget()`.  Please refer
@@ -25,7 +31,7 @@ namespace ash {
 // test cases as there are subtle differences.
 class ASH_EXPORT TestWidgetBuilder {
  public:
-  TestWidgetBuilder();
+  explicit TestWidgetBuilder(WidgetBuilderParams params = {});
   TestWidgetBuilder(const TestWidgetBuilder& other) = delete;
   TestWidgetBuilder& operator=(const TestWidgetBuilder& other) = delete;
   ~TestWidgetBuilder();
@@ -102,11 +108,11 @@ class ASH_EXPORT TestWidgetBuilder {
   [[nodiscard]] std::unique_ptr<views::Widget> BuildWidgetWithOwnership(
       views::Widget::InitParams::Ownership ownership);
 
+  // Note: ownership is a placeholder. It is overridden when building
+  // a widget.
   views::Widget::InitParams widget_init_params_{
-      views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET};
-  int window_id_ = aura::Window::kInitialId;
-  std::u16string window_title_ = std::u16string();
-  bool show_ = true;
+      views::Widget::InitParams::CLIENT_OWNS_WIDGET};
+  WidgetBuilderParams params_;
   bool built_ = false;
 };
 
