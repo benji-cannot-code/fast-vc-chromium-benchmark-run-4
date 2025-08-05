a11a3d131d5b94b187b7c7b84b86cd4cb3d08906
Charles Hager <clhager@google.com>
2025-08-05 13:09:14 -0700

    Draw HistoryActivity edge-to-edge
    https://screenshot.googleplex.com/nurwB7PJMx9Jcjf.png
    https://screenshot.googleplex.com/8v5eWBc3D5i4wzi.png
    https://screencast.googleplex.com/cast/NTkxMjcwMDQ1NzU4MjU5Mnw5ODAzN2IwMS0wZg
    
    Bug: 433563241
    Change-Id: I3a587a21b8a7cc2ec1ceeef3fad0cc8cc334d498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792749
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497131}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/selectable_list/SelectableListLayout.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/selectable_list/SelectableListLayout.java
@@ -597,4 +597,8 @@ public class SelectableListLayout<E> extends FrameLayout
         mBackPressStateSupplier.set(
                 mToolbar.getSelectionDelegate().isSelectionEnabled() || mToolbar.isSearching());
     }
+
+    public RecyclerView getRecyclerViewForTesting() {
+        return mRecyclerView;
+    }
 }
