2b1785871e24cc454e65cd58b013afd76b5cd386
Calder Kitagawa <ckitagawa@chromium.org>
2025-08-05 14:05:01 -0700

    Cleanup Tab Group Sync flags in flag-metadata
    I missed this when originally cleaning up the flags.
    
    Fixed: 427497482, 427497864
    Change-Id: Ife98eaabdeb5b0b69c1af96946dd5f19310f2b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6817910
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497173}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3099,9 +3099,6 @@ extern const char kSwapNewTabAndNewTabInGroupAndroidDescription[];
 extern const char kHistoryPaneAndroidName[];
 extern const char kHistoryPaneAndroidDescription[];
 
-extern const char kTabGroupSyncDisableNetworkLayerName[];
-extern const char kTabGroupSyncDisableNetworkLayerDescription[];
-
 extern const char kAndroidPinnedTabsName[];
 extern const char kAndroidPinnedTabsDescription[];
 
