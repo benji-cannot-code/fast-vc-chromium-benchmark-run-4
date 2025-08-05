1f575dcd340565414a61c8260a23f83c2f259313
Christopher Grant <cjgrant@chromium.org>
2025-08-05 16:37:23 -0700

    NTP Promos: Make "has promos" check consistent
    The original check considered only promo elgibility when determining if
    there are promos to show. The actual generation of promo lists can
    suppress promos for several reasons, so rather than constructing a web
    component that might not have anything to show, just align the two
    methods.
    
    Efficiency is not a concern here with a small number of promos, but
    conditionally skip ordering the list (the heaviest part) for peace
    of mind.
    
    Bug: 433606749
    Change-Id: Ie2852dcddb1eb2b2f09cc5e8f54bc9d7356a8acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6806480
    Auto-Submit: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497258}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -1060,7 +1060,7 @@ void NewTabPageUI::OnLoad() {
       module_id_details_, IdentityManagerFactory::GetForProfile(profile_));
   update.Set("modulesEnabled", modules_enabled);
 
-  const auto* ntp_promo_controller =
+  auto* ntp_promo_controller =
       UserEducationServiceFactory::GetForBrowserContext(profile_)
           ->ntp_promo_controller();
   const bool show_ntp_promos =
--- components/user_education/common/ntp_promo/ntp_promo_controller_unittest.cc
+++ components/user_education/common/ntp_promo/ntp_promo_controller_unittest.cc
@@ -267,4 +267,10 @@ TEST_F(NtpPromoControllerTest, ShownCallbackInvoked) {
   controller_.OnPromosShown({kPromoId}, {});
 }
 
+TEST_F(NtpPromoControllerTest, HasShowablePromos) {
+  EXPECT_FALSE(controller_.HasShowablePromos(nullptr));
+  RegisterPromo(kPromoId, kEligible);
+  EXPECT_TRUE(controller_.HasShowablePromos(nullptr));
+}
+
 }  // namespace user_education
