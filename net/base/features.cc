af9eda8973ae16cd218c6d01dde78589ca8ed840
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-08-05 04:34:49 -0700

    Revert "[IPP Devtools] Update kIpPrivacyEnableIppInDevTools Default State to True"
    This reverts commit b721be253e571e41013c38d9c174d3d5567b36b6.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5625803350999040
    
    Sample build with failed test: https://ci.chromium.org/b/8707429438486591953
    Affected test(s):
    [ninja://:blink_web_tests/http/tests/inspector-protocol/fetch-later/activate-and-redirect-cross-site-when-document-alive.https.js](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_web_tests%2Fhttp%2Ftests%2Finspector-protocol%2Ffetch-later%2Factivate-and-redirect-cross-site-when-document-alive.https.js?q=VHash%3A9b30675a77baf900)
    [ninja://:blink_web_tests/http/tests/inspector-protocol/fetch-later/activate-and-redirect-when-document-alive.https.js](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_web_tests%2Fhttp%2Ftests%2Finspector-protocol%2Ffetch-later%2Factivate-and-redirect-when-document-alive.https.js?q=VHash%3A9b30675a77baf900)
    [ninja://:blink_web_tests/http/tests/inspector-protocol/fetch-later/activate-post-when-document-alive.https.js](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_web_tests%2Fhttp%2Ftests%2Finspector-protocol%2Ffetch-later%2Factivate-post-when-document-alive.https.js?q=VHash%3A9b30675a77baf900)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5625803350999040&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6805978&type=BUG
    
    Original change's description:
    > [IPP Devtools] Update kIpPrivacyEnableIppInDevTools Default State to True
    >
    > In preparation for launching IP Protection, this CL changes the default
    > state of kIpPrivacyEnableIppInDevTools from false to true. This is to
    > minimize the number of non-default flags for the feature.
    >
    > Also updates related tests to reflect for the updated default behavior.
    >
    > Change-Id: I806def190d42f8cf4202fc04a1c7039de9edba3f
    > Bug: 435481631
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6805978
    > Reviewed-by: David Schinazi <dschinazi@chromium.org>
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1496344}
    >
    
    Bug: 435481631
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ifc7f6967e0a4fdb752b2d8286f5e6fe56496c6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819809
    Owners-Override: Andrii Natiahlyi <natiahlyi@google.com>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1496841}

--- net/base/features.cc
+++ net/base/features.cc
@@ -546,7 +546,7 @@ const base::FeatureParam<bool> kIpPrivacyDisableForEnterpriseByDefault{
 const base::FeatureParam<bool> kIpPrivacyEnableIppInDevTools{
     &kEnableIpProtectionProxy,
     /*name=*/"IpPrivacyEnableIppInDevTools",
-    /*default_value=*/true};
+    /*default_value=*/false};
 
 const base::FeatureParam<bool> kIpPrivacyEnableIppPanelInDevTools{
     &kEnableIpProtectionProxy,
