d9112060c614dc077217ddc062c807cd155447f5
Tom Sepez <tsepez@chromium.org>
2025-08-05 14:00:00 -0700

    Add UNSAFE_BUFFER macro equivalents for third_party/ipcz
    Do this as a small CL prior to using it. Maintain the chromium /
    standalone split by using chromium's defines when present.
    
    Change-Id: Idf49fc62fd70cd7a855d0cd56e536e4af7f9dc45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819873
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497165}

---
+++ third_party/ipcz/src/util/unsafe_buffers.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IPCZ_SRC_UTIL_UNSAFE_BUFFERS_H_
+#define IPCZ_SRC_UTIL_UNSAFE_BUFFERS_H_
+
+// Version of safe buffer macros for ipcz.
+
+#if defined(IPCZ_STANDALONE)
+#include "standalone/base/unsafe_buffers.h"  // nogncheck
+#else  // IPCZ_STANDALONE
+#include "base/compiler_specific.h" // nogncheck
+
+#define IPCZ_UNSAFE_BUFFER_USAGE UNSAFE_BUFFER_USAGE
+#define IPCZ_UNSAFE_BUFFERS(...) UNSAFE_BUFFERS(__VA_ARGS__)
+#define IPCZ_UNSAFE_TODO(...) UNSAFE_TODO(__VA_ARGS__)
+#endif  // IPCZ_STANDALONE
+
+#endif  // IPCZ_SRC_UTIL_UNSAFE_BUFFERS_H_
