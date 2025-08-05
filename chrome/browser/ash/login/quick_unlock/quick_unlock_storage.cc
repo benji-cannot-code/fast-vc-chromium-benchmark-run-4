f4dcd8bd8a11a352b720697baa8812af8f45ca86
Istvan Nagy <iscsi@google.com>
2025-08-05 04:35:30 -0700

    Fix PinBackend::SetWithContext crash
    Bug: b:432820759
    Change-Id: I34f5e21897d85aa71cfc8cce1d9d3d4cb4700c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6796229
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496842}

--- chrome/browser/ash/login/quick_unlock/quick_unlock_storage.cc
+++ chrome/browser/ash/login/quick_unlock/quick_unlock_storage.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_pref_names.h"
 #include "ash/public/cpp/login_types.h"
+#include "base/check.h"
 #include "chrome/browser/ash/login/quick_unlock/auth_token.h"
 #include "chrome/browser/ash/login/quick_unlock/fingerprint_storage.h"
 #include "chrome/browser/ash/login/quick_unlock/pin_storage_prefs.h"
@@ -31,7 +32,9 @@ base::TimeDelta GetStrongAuthTimeout(PrefService* pref_service) {
 
 QuickUnlockStorage::QuickUnlockStorage(Profile* profile)
     : profile_(profile), clock_(base::DefaultClock::GetInstance()) {
+  CHECK(profile);
   fingerprint_storage_ = std::make_unique<FingerprintStorage>(profile);
+  CHECK(profile->GetPrefs());
   pin_storage_prefs_ = std::make_unique<PinStoragePrefs>(profile->GetPrefs());
 }
 
