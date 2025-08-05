cd05bdbf67fba47bd7f2d52f766fb0c682572940
Marc Treib <treib@chromium.org>
2025-08-05 02:06:20 -0700

    DataImporterServiceImpl: Report success vs error
    ...based on the "count" reported by the native code, which is negative
    in case of an error.
    
    This is essentially a reland of crrev.com/c/6811896 which was
    accidentally undone (due to a bad rebase probably) by
    crrev.com/c/6798451.
    
    Bug: 435386347
    Change-Id: Id4679a43213720017ebb8a0b1bf656404bae609f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818500
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496789}

--- chrome/android/modules/on_demand/java/src/org/chromium/chrome/browser/data_import/DataImporterServiceImpl.java
+++ chrome/android/modules/on_demand/java/src/org/chromium/chrome/browser/data_import/DataImporterServiceImpl.java
@@ -426,13 +426,7 @@ public class DataImporterServiceImpl extends DataImporterService.Impl {
                         recordImportDoneMetrics(
                                 sessionId, BrowserFileType.BROWSER_FILE_TYPE_BOOKMARKS, count);
                         Log.i(TAG, "Bookmarks imported: %d", count);
-                        synchronized (mPendingImportsLock) {
-                            ImportResults importResults = mPendingImports.get(sessionId);
-                            assert (importResults != null);
-                            // TODO(crbug.com/435386347): Plumb the actual import result (success or
-                            // failure) back here, so it can be properly reported.
-                            importResults.successItemCount++;
-                        }
+                        updateImportResults(sessionId, count);
                         responseObserver.onNext(ImportItemResponse.newBuilder().build());
                         responseObserver.onCompleted();
                     });
@@ -453,13 +447,7 @@ public class DataImporterServiceImpl extends DataImporterService.Impl {
                         recordImportDoneMetrics(
                                 sessionId, BrowserFileType.BROWSER_FILE_TYPE_READING_LIST, count);
                         Log.i(TAG, "ReadingList imported: %d", count);
-                        synchronized (mPendingImportsLock) {
-                            ImportResults importResults = mPendingImports.get(sessionId);
-                            assert (importResults != null);
-                            // TODO(crbug.com/435386347): Plumb the actual import result (success or
-                            // failure) back here, so it can be properly reported.
-                            importResults.successItemCount++;
-                        }
+                        updateImportResults(sessionId, count);
                         responseObserver.onNext(ImportItemResponse.newBuilder().build());
                         responseObserver.onCompleted();
                     });
@@ -482,13 +470,7 @@ public class DataImporterServiceImpl extends DataImporterService.Impl {
                                 BrowserFileType.BROWSER_FILE_TYPE_BROWSING_HISTORY,
                                 count);
                         Log.i(TAG, "History imported: %d", count);
-                        synchronized (mPendingImportsLock) {
-                            ImportResults importResults = mPendingImports.get(sessionId);
-                            assert (importResults != null);
-                            // TODO(crbug.com/435386347): Plumb the actual import result (success or
-                            // failure) back here, so it can be properly reported.
-                            importResults.successItemCount++;
-                        }
+                        updateImportResults(sessionId, count);
                         responseObserver.onNext(ImportItemResponse.newBuilder().build());
                         responseObserver.onCompleted();
                     });
@@ -560,6 +542,18 @@ public class DataImporterServiceImpl extends DataImporterService.Impl {
             // Record `True` to report the `Completed` bucket.
             RecordHistogram.recordBooleanHistogram(
                     "UserDataImporter.OSMigration." + getHistogramSuffix(fileType) + ".Flow", true);
+        }
+
+        private void updateImportResults(ByteString sessionId, int count) {
+            synchronized (mPendingImportsLock) {
+                ImportResults importResults = mPendingImports.get(sessionId);
+                assert (importResults != null);
+                if (count >= 0) {
+                    importResults.successItemCount++;
+                } else {
+                    importResults.failedItemCount++;
+                }
+            }
         }
     }
 }
