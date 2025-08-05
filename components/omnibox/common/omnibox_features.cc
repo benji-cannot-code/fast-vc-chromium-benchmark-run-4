2e0c4e9e05d4ddadc37d7e61775807fadf5fc4de
Tomasz Wiszkowski <ender@google.com>
2025-08-05 11:27:22 -0700

    Launch Omnibox Parity Update
    Change-Id: I82fd0f061442a6a9a40e3d71cc149d3095c03657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6821330
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497057}

--- components/omnibox/common/omnibox_features.cc
+++ components/omnibox/common/omnibox_features.cc
@@ -316,7 +316,7 @@ BASE_FEATURE(kOmniboxShortcutsAndroid, "OmniboxShortcutsAndroid", ENABLED);
 
 // Updates various NTP/Omnibox assets and descriptions for visual alignment on
 // Android and iOS.
-BASE_FEATURE(kOmniboxMobileParityUpdate, "OmniboxMobileParityUpdate", DISABLED);
+BASE_FEATURE(kOmniboxMobileParityUpdate, "OmniboxMobileParityUpdate", ENABLED);
 
 // Updates various NTP/Omnibox assets and descriptions for visual alignment on
 // Android and iOS, V2.
