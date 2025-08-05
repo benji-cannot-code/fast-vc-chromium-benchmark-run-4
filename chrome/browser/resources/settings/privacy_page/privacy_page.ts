7659170af0547e7f8b3d15edfea14d16b903a6c3
Fiona Macintosh <fmacintosh@google.com>
2025-08-05 09:33:58 -0700

    [RWS] V2 settings cleanup
    Since we are not proceeding with the V2 UI launch I'm removing the tech
    debt/dead code
    
    Bug: b:430556889
    Change-Id: Ie3012b8719560b33978f22588619495375b42d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6803780
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496974}

--- chrome/browser/resources/settings/privacy_page/privacy_page.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_page.ts
@@ -250,11 +250,7 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
         },
       },
 
-      searchFilter_: {
-        type: String,
-        value: '',
-        observer: 'updateAllSitesPageTitle_',
-      },
+      searchFilter_: String,
 
       /**
        * Expose ContentSettingsTypes enum to HTML bindings.
@@ -297,11 +293,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
         value: () => loadTimeData.getBoolean('enableWebAppInstallation'),
       },
 
-      enableRelatedWebsiteSetsV2Ui_: {
-        type: Boolean,
-        value: () => loadTimeData.getBoolean('isRelatedWebsiteSetsV2UiEnabled'),
-      },
-
       enableLocalNetworkAccessSetting_: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('enableLocalNetworkAccessSetting'),
@@ -329,8 +320,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
         type: Boolean,
         value: false,
       },
-
-      allSitesPageTitle_: String,
     };
   }
 
@@ -371,8 +360,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
   private safetyHubBrowserProxy_: SafetyHubBrowserProxy =
       SafetyHubBrowserProxyImpl.getInstance();
   declare private enableKeyboardLockPrompt_: boolean;
-  declare private enableRelatedWebsiteSetsV2Ui_: boolean;
-  declare private allSitesPageTitle_: string;
   declare private enableIncognitoTrackingProtections_: boolean;
   declare private enableBundledSecuritySettings_: boolean;
   declare private dbdDeletionConfirmationToastLabel_: string;
@@ -394,8 +381,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
         'onBlockAutoplayStatusChanged',
         (status: BlockAutoplayStatus) =>
             this.onBlockAutoplayStatusChanged_(status));
-
-    this.updateAllSitesPageTitle_();
   }
 
   override currentRouteChanged() {
@@ -536,19 +521,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
         return this.i18n('thirdPartyCookiesLinkRowSublabelDisabled');
       default:
         assertNotReached();
-    }
-  }
-
-  private updateAllSitesPageTitle_(): void {
-    const rwsPrefix = 'related:';
-    if (this.enableRelatedWebsiteSetsV2Ui_ &&
-        this.searchFilter_.length > rwsPrefix.length &&
-        this.searchFilter_.startsWith(rwsPrefix)) {
-      this.allSitesPageTitle_ = loadTimeData.getStringF(
-          'allSitesRwsFilterViewTitle',
-          this.searchFilter_.substring(rwsPrefix.length));
-    } else {
-      this.allSitesPageTitle_ = this.i18n('siteSettingsAllSites');
     }
   }
 
