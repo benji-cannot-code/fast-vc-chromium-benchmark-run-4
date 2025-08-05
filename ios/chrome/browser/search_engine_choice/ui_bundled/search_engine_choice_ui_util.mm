82dfa34b0b27665e83d3068b0585130c8b72a15c
Tomasz Wiszkowski <ender@google.com>
2025-08-05 16:58:11 -0700

    Fix DSE icon handling discrepancy on IOS
    Screen: http://screen/C48QGybamuMhF6C.png
    Change-Id: I25a342932b8d414f64fe9a5a33ee92221ef4b780
    Fixed: 436591107, 422992330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6821916
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Owners-Override: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497272}

--- ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_ui_util.mm
+++ ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_ui_util.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/strings/utf_string_conversions.h"
 #import "build/branding_buildflags.h"
-#import "components/grit/components_scaled_resources.h"
 #import "components/regional_capabilities/regional_capabilities_service.h"
 #import "components/search_engines/template_url.h"
 #import "components/search_engines/template_url_service.h"
@@ -42,15 +41,6 @@ int GetResourceIdFromTemplateURL(const TemplateURL& template_url) {
   // engine that was selected from a country outside of EEA countries.
   if (resource_it != std::end(kSearchEnginesScaledResources)) {
     return resource_it->id;
-  }
-
-  if (resource_name == "IDR_SEARCH_ENGINE_GOOGLE_IMAGE") {
-    // Unlike the other logos which are in `kSearchEnginesScaledResources`,
-    // the Google logo is included via
-    // `components/resources/search_engine_choice_scaled_resources.grdp`
-    // TODO(crbug.com/422992330): Fix this discrepancy now that all OSE assets
-    // are restricted to branded builds.
-    return IDR_SEARCH_ENGINE_GOOGLE_IMAGE;
   }
 #endif
 
