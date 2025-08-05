3ceb5f49f55f0ca1154e370081e32e26bd2bf87c
Kartar Singh <kartarsingh@google.com>
2025-08-05 07:22:37 -0700

    [Sheriff] Disable ContextMenu tests failing on android-12 builders
    Some ContextMenuTests are failing on these bots:
     - android-12l-landscape-x64-dbg-tests
     - android-automotive-12l-x64-rel-tests
    
    The tests were disabled on automotive bot here:
    https://crrev.com/i/8480157.
    
    The test filter for `android-12l-landscape-x64-dbg-tests` is being
    updated in this change.
    
    Bug: 436445484
    Change-Id: I00f5d966c2916abc9bd2e94622a3002c0d49437f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818328
    Owners-Override: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>
    Auto-Submit: Kartar Singh <kartarsingh@google.com>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496900}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -713,6 +713,8 @@ public class ContextMenuTest {
         Assert.assertEquals("Copied tel number is not correct", "10000000000", getClipboardText());
     }
 
+    // TODO(436445484): The test is disabled for some Android 12 builders, please remove comment and
+    // update the filters to re-enable test.
     @Test
     @LargeTest
     @Restriction(DeviceFormFactor.ONLY_TABLET)
@@ -1267,6 +1269,8 @@ public class ContextMenuTest {
                 chromeExtrasCaptor.getValue().saveLastUsed());
     }
 
+    // TODO(436445484): The test is disabled for some Android 12 builders, please remove comment and
+    // update the filters to re-enable test.
     @Test
     @SmallTest
     @Restriction(DeviceFormFactor.ONLY_TABLET)
@@ -1361,6 +1365,8 @@ public class ContextMenuTest {
                 chromeExtrasCaptor.getValue().saveLastUsed());
     }
 
+    // TODO(436445484): The test is disabled for some Android 12 builders, please remove comment and
+    // update the filters to re-enable test.
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.ONLY_TABLET)
