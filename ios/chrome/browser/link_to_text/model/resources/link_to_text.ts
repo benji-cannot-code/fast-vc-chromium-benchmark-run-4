632b68a136329fec041bb801def174788b0a3449
Alexandra Pereira <alexandrasp@google.com>
2025-08-05 13:46:39 -0700

    [iOS][gCrWeb] Migrate link_to_test.ts to new gCrWeb API
    Update `link_to_test.ts` Typescript resource from the deprecated
    `gCrWebLegacy` object to the new `gCrWeb` API. No functional change is
    intended.
    
    Bug: 399666983
    Change-Id: Idf8fb93341c313cd6322c397b222804ee57f09c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813473
    Auto-Submit: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497157}

--- ios/chrome/browser/link_to_text/model/resources/link_to_text.ts
+++ ios/chrome/browser/link_to_text/model/resources/link_to_text.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {gCrWebLegacy} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
 import * as utils from '//third_party/text-fragments-polyfill/src/src/fragment-generation-utils.js';
 
 /**
@@ -43,4 +43,6 @@ function getLinkToText() {
   };
 }
 
-gCrWebLegacy.linkToText = {getLinkToText};
+const linkToTextApi = new CrWebApi();
+linkToTextApi.addFunction('getLinkToText', getLinkToText);
+gCrWeb.registerApi('linkToText', linkToTextApi);
