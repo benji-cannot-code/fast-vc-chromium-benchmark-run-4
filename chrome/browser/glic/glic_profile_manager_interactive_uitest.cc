ebe4524659d1e60f020e99ec2d8ef22779e438dd
Justin DeWitt <dewittj@chromium.org>
2025-08-05 09:28:44 -0700

    Add Glic fre prewarming on impression of the whats-new GlicIntro module.
    Also adds a source for fre prewarming and a histogram tracking prewarm
    triggering per source.
    
    Bug: 430325799
    Change-Id: Idb36517d86d2ca37fd6c8a382fd953f3ddaf9b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6820118
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496971}

--- chrome/browser/glic/glic_profile_manager_interactive_uitest.cc
+++ chrome/browser/glic/glic_profile_manager_interactive_uitest.cc
@@ -137,7 +137,8 @@ class GlicProfileManagerUiTest
           GetTestEnvForSecondProfile().SetFRECompletion(
               prefs::FreStatus::kNotStarted);
         }
-        GetService(primary_profile)->TryPreloadFre();
+        GetService(primary_profile)
+            ->TryPreloadFre(glic::GlicPrewarmingFreSource::kTest);
       } else {
         GetService(primary_profile)->TryPreload();
       }
--- chrome/browser/ui/webui/whats_new/whats_new_handler.cc
+++ chrome/browser/ui/webui/whats_new/whats_new_handler.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/hats/hats_service_factory.h"
 #include "chrome/browser/ui/webui/whats_new/whats_new_fetcher.h"
 #include "chrome/browser/ui/webui/whats_new/whats_new_util.h"
+#include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_version.h"
 #include "chrome/common/webui_url_constants.h"
@@ -26,6 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/service/variations_service.h"
 #include "components/variations/service/variations_service_utils.h"
 #include "url/gurl.h"
+
+#if BUILDFLAG(ENABLE_GLIC)
+#include "chrome/browser/glic/public/glic_keyed_service.h"
+#include "chrome/browser/glic/public/glic_keyed_service_factory.h"
+#endif
 
 WhatsNewHandler::WhatsNewHandler(
     mojo::PendingReceiver<whats_new::mojom::PageHandler> receiver,
@@ -96,6 +102,15 @@ void WhatsNewHandler::RecordModuleImpression(
   std::string histogram_name = "UserEducation.WhatsNew.ModuleShown.";
   histogram_name.append(module_name);
   base::UmaHistogramEnumeration(histogram_name, position);
+
+#if BUILDFLAG(ENABLE_GLIC)
+  if (module_name == "GlicIntro") {
+    if (auto* glic_service =
+            glic::GlicKeyedServiceFactory::GetGlicKeyedService(profile_)) {
+      glic_service->TryPreloadFre(glic::GlicPrewarmingFreSource::kWhatsNew);
+    }
+  }
+#endif  // BUILDFLAG(ENABLE_GLIC)
 }
 
 void WhatsNewHandler::RecordExploreMoreToggled(bool expanded) {
