967c1f539300ed258fd700ca7d6d351ed2c34c19
Louis Romero <lpromero@google.com>
2025-08-05 02:52:46 -0700

    Reload the omnibox textfield after changing allowsReturnKeyWithEmptyText
    This CL fixes a bug where the copied-link suggestion was not navigated
    to when hitting the return key.
    Prior to this CL, the return key was enabled but the text field needed a
    call to -reloadInputViews to take it into effect.
    
    This reverts commit e9e35f50a622d7864c8ad7692d90cd9225e67013 and relands commit 000b00f7f3120f4cd251fb12868d74b324841323.
    
    Fixed: 40836724
    Change-Id: I743757700ecbcee29b69687f62970bdcda14ed31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6817157
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496809}

--- ios/chrome/browser/omnibox/ui/omnibox_text_field_ios.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_text_field_ios.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/not_fatal_until.h"
 #import "base/notreached.h"
 #import "base/strings/sys_string_conversions.h"
+#import "base/task/sequenced_task_runner.h"
 #import "components/grit/components_scaled_resources.h"
 #import "components/omnibox/browser/autocomplete_input.h"
 #import "components/open_from_clipboard/clipboard_async_wrapper_ios.h"
@@ -157,6 +158,21 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 
 - (void)dealloc {
   [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)setAllowsReturnKeyWithEmptyText:(BOOL)allowsReturnKeyWithEmptyText {
+  _allowsReturnKeyWithEmptyText = allowsReturnKeyWithEmptyText;
+
+  // To make sure the keyboard is correctly taking the new value into account,
+  // call `-reloadInputViews`. That being said, `-reloadInputViews` can
+  // update the input mode, which can itself call again this method.
+  // `-reloadInputViews` being non-reentrant (contention on
+  // `+[UIKeyboardAutomatic sharedInstance]`), call this asynchronously.
+  __weak __typeof(self) weakSelf = self;
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, base::BindOnce(^{
+        [weakSelf reloadInputViews];
+      }));
 }
 
 - (void)setText:(NSAttributedString*)text
