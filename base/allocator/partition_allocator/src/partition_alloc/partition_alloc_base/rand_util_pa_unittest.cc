21e69d82071e0a44ca703bdf5aea129161aba2e3
Tom Sepez <tsepez@chromium.org>
2025-08-04 17:01:47 -0700

    Remove file-wide unsafe buffer pragamas in partition alloc.
    Automated using tools/clang/unsafe_pragam_rewriter scripts.
    
    The previous bulk edit modified all files, even those that might have
    been compliant, so many of these will be no-op changes.
    
    Change-Id: Ie0614a41d0162adc9f5bea8b8d74d1726bc87055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819800
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496646}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/rand_util_pa_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/rand_util_pa_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <algorithm>
 #include <cmath>
 #include <cstddef>
@@ -17,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "partition_alloc/partition_alloc_base/check.h"
+#include "partition_alloc/partition_alloc_base/compiler_specific.h"
 #include "partition_alloc/partition_alloc_base/logging.h"
 #include "partition_alloc/partition_alloc_base/rand_util.h"
 #include "partition_alloc/partition_alloc_base/time/time.h"
@@ -27,12 +23,13 @@ namespace partition_alloc::internal::base {
 TEST(PartitionAllocBaseRandUtilTest, RandBytes) {
   const size_t buffer_size = 50;
   char buffer[buffer_size];
-  memset(buffer, 0, buffer_size);
+  PA_UNSAFE_TODO(memset(buffer, 0, buffer_size));
   base::RandBytes(buffer, buffer_size);
-  std::sort(buffer, buffer + buffer_size);
+  std::sort(buffer, PA_UNSAFE_TODO(buffer + buffer_size));
   // Probability of occurrence of less than 25 unique bytes in 50 random bytes
   // is below 10^-25.
-  EXPECT_GT(std::unique(buffer, buffer + buffer_size) - buffer, 25);
+  PA_UNSAFE_TODO(
+      EXPECT_GT(std::unique(buffer, buffer + buffer_size) - buffer, 25));
 }
 
 // Verify that calling base::RandBytes with an empty buffer doesn't fail.
--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/ranges/ranges_pa_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/ranges/ranges_pa_unittest.cc
@@ -3,15 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <array>
 #include <initializer_list>
 #include <vector>
 
+#include "partition_alloc/partition_alloc_base/compiler_specific.h"
 #include "partition_alloc/partition_alloc_base/ranges/ranges.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -54,7 +50,8 @@ template <size_t N>
 constexpr std::array<int, N> GenerateArray() {
   std::array<int, N> arr{};
   int i = 0;
-  for (auto* it = ranges::begin(arr); it != ranges::end(arr); ++it) {
+  for (auto* it = ranges::begin(arr); it != ranges::end(arr);
+       PA_UNSAFE_TODO(++it)) {
     *it = i++;
   }
 
@@ -94,13 +91,13 @@ TEST(RangesTest, EndPrefersMember) {
 
 TEST(RangesTest, EndConstexprContainers) {
   int arr[1]{};
-  static_assert(arr + 1 == ranges::end(arr), "");
+  static_assert(PA_UNSAFE_TODO(arr + 1) == ranges::end(arr), "");
 
   static constexpr std::initializer_list<int> il = {1, 2, 3};
   static_assert(il.end() == ranges::end(il), "");
 
   static constexpr std::array<int, 3> array = {1, 2, 3};
-  static_assert(&array[0] + 3 == ranges::end(array), "");
+  static_assert(PA_UNSAFE_TODO(&array[0] + 3) == ranges::end(array), "");
 }
 
 TEST(RangesTest, EndRegularContainers) {
