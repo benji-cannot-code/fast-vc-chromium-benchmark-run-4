107f933c29479bb16ddd00f8fb08cb1a0c161ebb
Sasha Kunitskaya <saskekun@google.com>
2025-08-05 09:07:27 -0700

    Show history opt-in in "Tabs from other devices"
    This CL is a WIP-prototype. There will be three variants of the "Tabs
    from other devices" screen depending on a user's state: not signed in /
    signed in in Web / signed in in Chrome
    
    In this CL I've:
    - introduced the strings for all screens (and uploaded the screenshots)
    - added the screens' html "frames" for all screens (with images)
    
    To be done:
    - finalize the "IDS_HISTORY_SIGNED_IN_SYNC_HISTORY_PROMO_DESC" string to include a user's email
    - retrieve user's data to show it on the appropriate screens (avatar, name and email)
    - add the logic to select which of the three screens to display based on the user's state
    - add an action for a "Sync history" button
    
    Bug: 418144047
    Change-Id: I37fb8bb41d3afb92d8bff24d0145a165db4ff349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792123
    Commit-Queue: Sasha Kunitskaya <saskekun@google.com>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496956}

--- chrome/browser/ui/webui/history/history_ui.cc
+++ chrome/browser/ui/webui/history/history_ui.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history_embeddings/history_embeddings_utils.h"
 #include "chrome/browser/page_image_service/image_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_ui_util.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
@@ -59,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/sync/base/features.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -71,6 +73,10 @@ namespace {
 content::WebUIDataSource* CreateAndAddHistoryUIHTMLSource(Profile* profile) {
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       profile, chrome::kChromeUIHistoryHost);
+
+  source->AddBoolean(
+      "useHistorySyncOptinScreen",
+      base::FeatureList::IsEnabled(syncer::kReplaceSyncPromosWithSignInPromos));
 
   HistoryUtil::PopulateSourceForSidePanelHistory(source, profile);
 
@@ -86,8 +92,19 @@ content::WebUIDataSource* CreateAndAddHistoryUIHTMLSource(Profile* profile) {
       {"noSyncedResults", IDS_HISTORY_NO_SYNCED_RESULTS},
       {"turnOnSyncPromo", IDS_HISTORY_TURN_ON_SYNC_PROMO},
       {"turnOnSyncPromoDesc", IDS_HISTORY_TURN_ON_SYNC_PROMO_DESC},
+      {"turnOnSyncHistoryPromo", IDS_HISTORY_SYNC_HISTORY_PROMO},
+      {"turnOnSyncHistoryPromoDesc", IDS_HISTORY_SYNC_HISTORY_PROMO_DESC},
+      {"turnOnSignedInSyncHistoryPromo",
+       IDS_HISTORY_SIGNED_IN_SYNC_HISTORY_PROMO},
   };
   source->AddLocalizedStrings(kStrings);
+
+  source->AddLocalizedString("turnOnSyncHistoryButton",
+                             IDS_HISTORY_SYNC_HISTORY_BUTTON);
+  source->AddLocalizedString("turnOnSignedInSyncHistoryPromoDesc",
+                             IDS_HISTORY_SIGNED_IN_SYNC_HISTORY_PROMO_DESC);
+  source->AddString("accountPictureUrl",
+                    profiles::GetPlaceholderAvatarIconUrl());
 
   source->AddString(
       "sidebarFooter",
