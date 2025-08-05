f265e194c5665a03bd2190eacbfa6153d8ccb199
Yishui Liu <yishuil@google.com>
2025-08-04 17:37:00 -0700

    [BNPL] Add feature flag for loading allowlist after syncing
    This CL adds the feature flag for loading BNPL allowlist after BNPL
    issuers are loaded.
    
    This flag will be used as a kill-switch flag, so we are not exposing it
    to the chrome://flags UI.
    
    Bug: 356443046, b:436358918
    Change-Id: Ifc37b79c48f2f3461d7d7743ece3171e5ce1d281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6820698
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Yishui Liu <yishuil@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496666}

--- components/autofill/core/common/autofill_payments_features.h
+++ components/autofill/core/common/autofill_payments_features.h
@@ -61,6 +61,8 @@ COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableFlatRateCardBenefitsFromCurinos);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableFpanRiskBasedAuthentication);
+COMPONENT_EXPORT(AUTOFILL)
+BASE_DECLARE_FEATURE(kAutofillEnableLoadBnplAllowlistAfterSyncing);
 
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableLogFormEventsToAllParsedFormTypes);
