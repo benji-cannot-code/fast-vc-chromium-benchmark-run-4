f871a6c7c1114b82181f3c74aa19eac297506e12
Reilly Grant <reillyg@chromium.org>
2025-08-05 16:10:43 -0700

    Revert "Hide Shadow when a window is fully occluded"
    This reverts commit 261ba32da681612e7611f8532e045a03cb16cca6.
    
    Reason for revert: Consistently failing on https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel.
    
    Bug: 435669630
    Original change's description:
    > Hide Shadow when a window is fully occluded
    >
    > Bug: 435669630
    > Test: covered by unit test. updated on pixel test.
    > Change-Id: Ic597abf7337919afd5a494eb1e240892c92fdea3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814159
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1496941}
    
    Bug: 435669630
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ifb033a87f55d63fbb9466a134868922cb8a0a91b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6822542
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497245}

--- ui/wm/core/shadow_controller.cc
+++ ui/wm/core/shadow_controller.cc
@@ -101,7 +101,6 @@ class ShadowController::Impl :
                              const gfx::Rect& new_bounds,
                              ui::PropertyChangeReason reason) override;
   void OnWindowDestroyed(aura::Window* window) override;
-  void OnWindowOcclusionChanged(aura::Window* window) override;
 
  private:
   friend class base::RefCounted<Impl>;
@@ -267,10 +266,6 @@ void ShadowController::Impl::OnWindowActivated(ActivationReason reason,
 
 bool ShadowController::Impl::ShouldShowShadowForWindow(
     aura::Window* window) const {
-  if (window->GetOcclusionState() == aura::Window::OcclusionState::OCCLUDED) {
-    return false;
-  }
-
   if (delegate_) {
     const bool should_show = delegate_->ShouldShowShadowForWindow(window);
     if (should_show)
@@ -286,14 +281,6 @@ bool ShadowController::Impl::ShouldShowShadowForWindow(
   }
 
   return GetShadowElevationConvertDefault(window) > 0;
-}
-
-void ShadowController::Impl::OnWindowOcclusionChanged(aura::Window* window) {
-  ui::Shadow* shadow = GetShadowForWindow(window);
-  if (!shadow) {
-    return;
-  }
-  HandlePossibleShadowVisibilityChange(window);
 }
 
 void ShadowController::Impl::MaybeSetShadowRadiusForWindow(
@@ -344,8 +331,6 @@ void ShadowController::Impl::CreateShadowForWindow(aura::Window* window) {
   shadow->layer()->SetVisible(ShouldShowShadowForWindow(window));
   window->layer()->Add(shadow->layer());
   window->layer()->StackAtBottom(shadow->layer());
-
-  window->TrackOcclusionState();
 
   if (delegate_) {
     delegate_->ApplyColorThemeToWindowShadow(window);
