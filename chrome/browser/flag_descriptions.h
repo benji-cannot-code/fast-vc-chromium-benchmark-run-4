8435d764686c9fd9f6fc7637dc4e14d451f823b2
Yann Dago <ydago@chromium.org>
2025-08-05 13:57:28 -0700

    [EM2] Add enforce-management-disclaimer flag to chrome://flags
    Bug: 394887710
    Change-Id: I02682e71c4c9323352e247cbec21b847f0fb87a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6820350
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497162}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1604,6 +1604,9 @@ extern const char kNotificationsSystemFlagName[];
 extern const char kNotificationsSystemFlagDescription[];
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
+extern const char kEnforceManagementDisclaimerName[];
+extern const char kEnforceManagementDisclaimerDescription[];
+
 extern const char kOfferMigrationToDiceUsersName[];
 extern const char kOfferMigrationToDiceUsersDescription[];
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
