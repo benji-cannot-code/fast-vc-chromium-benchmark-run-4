2a405d430f9457c2f20e5bb94322d26a6f683c81
Brandon Fong <bjfong@google.com>
2025-08-05 09:14:37 -0700

    [hub] Enable the search loupe on tab groups pane for hub search
    Enable the search loupe of the tab groups pane for hub search, through
    decoupling the menu button from the search loupe. The visibility used
    to be controlled via the container for the 3-dot menu button but will
    now be controlled via the button wrapper.
    
    Bug: 436529097
    Change-Id: Ia4022c1d60e0db4e99765bba44880f623a9a0882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819264
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496961}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewUnitTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewUnitTest.java
@@ -40,7 +40,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 import androidx.core.content.ContextCompat;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -67,8 +66,10 @@ import org.chromium.base.test.BaseRobolectricTestRule;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.hub.HubToolbarProperties.PaneButtonLookup;
+import org.chromium.chrome.browser.toolbar.menu_button.MenuButton;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.ui.base.TestActivity;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -113,7 +114,7 @@ public class HubToolbarViewUnitTest {
     private FrameLayout mToolbarContainer;
     private Button mActionButton;
     private TabLayout mPaneSwitcher;
-    private LinearLayout mMenuButtonContainer;
+    private MenuButton mMenuButtonWrapper;
     private View mSearchBox;
     private View mSearchLoupe;
     private EditText mSearchBoxText;
@@ -137,7 +138,7 @@ public class HubToolbarViewUnitTest {
         mToolbarContainer = (FrameLayout) inflater.inflate(layoutId, null, false);
         mActionButton = mToolbarContainer.findViewById(R.id.toolbar_action_button);
         mPaneSwitcher = mToolbarContainer.findViewById(R.id.pane_switcher);
-        mMenuButtonContainer = mToolbarContainer.findViewById(R.id.menu_button_container);
+        mMenuButtonWrapper = mToolbarContainer.findViewById(R.id.menu_button_wrapper);
         mSearchBox = mToolbarContainer.findViewById(R.id.search_box);
         mSearchLoupe = mToolbarContainer.findViewById(R.id.search_loupe);
         mSearchBoxText = mToolbarContainer.findViewById(R.id.search_box_text);
@@ -236,10 +237,10 @@ public class HubToolbarViewUnitTest {
     @Test
     public void testMenuButtonVisibility() {
         mPropertyModel.set(MENU_BUTTON_VISIBLE, false);
-        assertEquals(View.INVISIBLE, mMenuButtonContainer.getVisibility());
+        assertEquals(View.INVISIBLE, mMenuButtonWrapper.getVisibility());
 
         mPropertyModel.set(MENU_BUTTON_VISIBLE, true);
-        assertEquals(View.VISIBLE, mMenuButtonContainer.getVisibility());
+        assertEquals(View.VISIBLE, mMenuButtonWrapper.getVisibility());
     }
 
     @Test
