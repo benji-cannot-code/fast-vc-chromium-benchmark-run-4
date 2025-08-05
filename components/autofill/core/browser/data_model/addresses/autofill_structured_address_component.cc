ac01191e05856f0e604757d6ae76960b8ffe2ae6
Piotr Domaradzki <doland@google.com>
2025-08-05 02:57:22 -0700

    Remove NormalizeValue function.
    As I'm reading the code, I see that there are multiple functions
    responsible for roughly the same thing and it's not obvious which to
    use: NormalizeForComparison, NormalizeValue and GetValueForComparison.
    
    Due to inheritance further simplification requires more thought, but IMO
    such a change is a step in the right direction of making the interfaces
    easier to reason about.
    
    Change-Id: Ia25c71bfd80562fa4bad85a993898033e9fdc6b0
    Bug: 429537991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798636
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Commit-Queue: Piotr Domaradzki <doland@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496812}

--- components/autofill/core/browser/data_model/addresses/autofill_structured_address_component.cc
+++ components/autofill/core/browser/data_model/addresses/autofill_structured_address_component.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/zip.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/data_model/addresses/autofill_i18n_api.h"
+#include "components/autofill/core/browser/data_model/addresses/autofill_profile_comparator.h"
 #include "components/autofill/core/browser/data_model/addresses/autofill_structured_address_format_provider.h"
 #include "components/autofill/core/browser/data_model/addresses/autofill_structured_address_utils.h"
 #include "components/autofill/core/browser/field_types.h"
@@ -240,7 +241,7 @@ const std::u16string& AddressComponent::GetValue() const {
 std::u16string AddressComponent::GetValueForComparison(
     const std::u16string& value,
     const AddressCountryCode& common_country_code) const {
-  return NormalizeValue(value);
+  return AutofillProfileComparator::NormalizeForComparison(value);
 }
 
 std::optional<std::u16string> AddressComponent::GetCanonicalizedValue() const {
