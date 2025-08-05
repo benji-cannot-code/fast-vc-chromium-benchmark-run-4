d701f10affab38644e80fa2457aafe027eb9134a
Etienne Pierre-Doray <etiennep@chromium.org>
2025-08-05 13:31:30 -0700

    [tracing] Fix include to public track.h
    Change-Id: I67bb3bf3094e0ca87276784efff41413825e8fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6821975
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497148}

--- base/power_monitor/power_monitor.h
+++ base/power_monitor/power_monitor.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
-#include "third_party/perfetto/include/perfetto/tracing/internal/track_event_internal.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace base {
 
