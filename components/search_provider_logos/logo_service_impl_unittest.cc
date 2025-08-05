894de88cd6dd9b8512d2b0c97e1131742f387071
Jérôme Lebel <jlebel@chromium.org>
2025-08-05 04:28:21 -0700

    [iOS] Remove dependance of LogoObserver in ios
    Remove unused logo_observer.h and remove on LogoObserver.
    // TODO(crbug.com/423883582): Need to be removed once LogoController is removed.
    
    LogoObserver is still used on Android:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/android/logo/logo_bridge.cc;l=79;drc=88b4c5fb1f18c7687b26828ca741049b48c4786e
    
    Bug: 423883582
    Change-Id: I02e853fd4fde9a1af3e6657057c3bb4c25874033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818760
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496837}

--- components/search_provider_logos/logo_service_impl_unittest.cc
+++ components/search_provider_logos/logo_service_impl_unittest.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_provider_logos/fixed_logo_api.h"
 #include "components/search_provider_logos/google_logo_api.h"
 #include "components/search_provider_logos/logo_cache.h"
-#include "components/search_provider_logos/logo_observer.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "net/base/url_util.h"
 #include "net/http/http_response_headers.h"
